f220e4df46e4179283b7b522dff3cf1838a05667
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-09-28 19:39:19 +0000

    Bug 704490 - Abstract all bookmark/history access behing a common API (r=blassey)
    UltraBlame original commit: 6b6331924850348c3fa359e91c92a02763797261

--- mobile/android/base/GlobalHistory.java
+++ mobile/android/base/GlobalHistory.java
@@ -1,39 +1,39 @@
-/* -*- Mode: Java; c-basic-offset: 4; tab-width: 20; indent-tabs-mode: nil; -*-
- * ***** BEGIN LICENSE BLOCK *****
- * Version: MPL 1.1/GPL 2.0/LGPL 2.1
- *
- * The contents of this file are subject to the Mozilla Public License Version
- * 1.1 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- * http://www.mozilla.org/MPL/
- *
- * Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- * for the specific language governing rights and limitations under the
- * License.
- *
- * The Original Code is Mozilla Android code.
- *
- * The Initial Developer of the Original Code is Mozilla Foundation.
- * Portions created by the Initial Developer are Copyright (C) 2011
- * the Initial Developer. All Rights Reserved.
- *
- * Contributor(s):
- *   Kartikaya Gupta <kgupta@mozilla.com>
- *
- * Alternatively, the contents of this file may be used under the terms of
- * either the GNU General Public License Version 2 or later (the "GPL"), or
- * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
- * in which case the provisions of the GPL or the LGPL are applicable instead
- * of those above. If you wish to allow use of your version of this file only
- * under the terms of either the GPL or the LGPL, and not to allow others to
- * use your version of this file under the terms of the MPL, indicate your
- * decision by deleting the provisions above and replace them with the notice
- * and other provisions required by the GPL or the LGPL. If you do not delete
- * the provisions above, a recipient may use your version of this file under
- * the terms of any one of the MPL, the GPL or the LGPL.
- *
- * ***** END LICENSE BLOCK ***** */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 package org.mozilla.gecko;
 
@@ -43,11 +43,12 @@ import java.util.Queue;
 import java.util.Set;
 import java.lang.ref.SoftReference;
 
-import android.content.ContentValues;
+import android.content.ContentResolver;
 import android.database.Cursor;
 import android.os.Handler;
-import android.provider.Browser;
 import android.util.Log;
+
+import org.mozilla.gecko.db.BrowserDB;
 
 class GlobalHistory {
     private static final String LOGTAG = "GeckoGlobalHistory";
@@ -58,16 +59,16 @@ class GlobalHistory {
         return sInstance;
     }
 
-    // this is the delay between receiving a URI check request and processing it.
-    // this allows batching together multiple requests and processing them together,
-    // which is more efficient.
+    
+    
+    
     private static final long BATCHING_DELAY_MS = 100;
 
-    private final Handler mHandler;                     // a background thread on which we can process requests
-    private final Queue<String> mPendingUris;           // URIs that need to be checked
-    private SoftReference<Set<String>> mVisitedCache;   // cache of the visited URI list
-    private final Runnable mNotifierRunnable;           // off-thread runnable used to check URIs
-    private boolean mProcessing; // = false             // whether or not the runnable is queued/working
+    private final Handler mHandler;                     
+    private final Queue<String> mPendingUris;           
+    private SoftReference<Set<String>> mVisitedCache;   
+    private final Runnable mNotifierRunnable;           
+    private boolean mProcessing; 
 
     private GlobalHistory() {
         mHandler = GeckoAppShell.getHandler();
@@ -77,15 +78,10 @@ class GlobalHistory {
             public void run() {
                 Set<String> visitedSet = mVisitedCache.get();
                 if (visitedSet == null) {
-                    // the cache was wiped away, repopulate it
+                    
                     Log.w(LOGTAG, "Rebuilding visited link set...");
                     visitedSet = new HashSet<String>();
-                    Cursor c = GeckoApp.mAppContext.getContentResolver().query(Browser.BOOKMARKS_URI,
-                                    new String[] { Browser.BookmarkColumns.URL },
-                                    Browser.BookmarkColumns.BOOKMARK + "=0 AND " +
-                                    Browser.BookmarkColumns.VISITS + ">0",
-                                    null,
-                                    null);
+                    Cursor c = BrowserDB.getAllVisitedHistory(GeckoApp.mAppContext.getContentResolver());
                     if (c.moveToFirst()) {
                         do {
                             visitedSet.add(c.getString(0));
@@ -95,8 +91,8 @@ class GlobalHistory {
                     c.close();
                 }
 
-                // this runs on the same handler thread as the checkUriVisited code,
-                // so no synchronization needed
+                
+                
                 while (true) {
                     String uri = mPendingUris.poll();
                     if (uri == null) {
@@ -112,7 +108,7 @@ class GlobalHistory {
     }
 
     public void add(String uri) {
-        Browser.updateVisitedHistory(GeckoApp.mAppContext.getContentResolver(), uri, true);
+        BrowserDB.updateVisitedHistory(GeckoApp.mAppContext.getContentResolver(), uri);
         Set<String> visitedSet = mVisitedCache.get();
         if (visitedSet != null) {
             visitedSet.add(uri);
@@ -121,24 +117,19 @@ class GlobalHistory {
     }
 
     public void update(String uri, String title) {
-        ContentValues values = new ContentValues();
-        values.put(Browser.BookmarkColumns.TITLE, title);
-        GeckoApp.mAppContext.getContentResolver().update(
-                Browser.BOOKMARKS_URI,
-                values,
-                Browser.BookmarkColumns.URL + " = ?",
-                new String[] { uri });
+        ContentResolver resolver = GeckoApp.mAppContext.getContentResolver();
+        BrowserDB.updateHistoryTitle(resolver, uri, title);
     }
 
     public void checkUriVisited(final String uri) {
         mHandler.post(new Runnable() {
             public void run() {
-                // this runs on the same handler thread as the processing loop,
-                // so synchronization needed
+                
+                
                 mPendingUris.add(uri);
                 if (mProcessing) {
-                    // there's already a runnable queued up or working away, so
-                    // no need to post another
+                    
+                    
                     return;
                 }
                 mProcessing = true;
