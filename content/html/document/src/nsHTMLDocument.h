f3bf5b57788020a8a3c39977a8c9e6d2e6de08ca
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-09-28 20:46:16 +0000

    Backing out bf0a78471e5e:dc7022e2d807 for inexplicable windows crashes.
    UltraBlame original commit: e1bef8037d364093de996c9fbd8af78fbb1945d4

--- content/html/document/src/nsHTMLDocument.h
+++ content/html/document/src/nsHTMLDocument.h
@@ -1,40 +1,40 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=2 sw=2 et tw=80: */
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is mozilla.org code.
+ *
+ * The Initial Developer of the Original Code is
+ * Netscape Communications Corporation.
+ * Portions created by the Initial Developer are Copyright (C) 1998
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either of the GNU General Public License Version 2 or later (the "GPL"),
+ * or the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
 #ifndef nsHTMLDocument_h___
 #define nsHTMLDocument_h___
 
@@ -51,7 +51,7 @@
 #include "nsIHttpChannel.h"
 #include "nsHTMLStyleSheet.h"
 
-
+// Document.Write() related
 #include "nsIWyciwygChannel.h"
 #include "nsILoadGroup.h"
 #include "nsNetUtil.h"
@@ -117,21 +117,20 @@ public:
  
   virtual NS_HIDDEN_(nsContentList*) GetFormControls();
  
-  
+  // nsIDOMDocument interface
   NS_FORWARD_NSIDOMDOCUMENT(nsDocument::)
 
-  
+  // nsIDOMNode interface
   NS_FORWARD_NSIDOMNODE(nsDocument::)
 
-  
-  using nsDocument::GetPlugins;
+  // nsIDOMHTMLDocument interface
   NS_DECL_NSIDOMHTMLDOCUMENT
 
-  
-
-
-
-
+  /**
+   * Returns the result of document.all[aID] which can either be a node
+   * or a nodelist depending on if there are multiple nodes with the same
+   * id.
+   */
   nsISupports *GetDocumentAllResult(const nsAString& aID,
                                     nsWrapperCache **aCache,
                                     nsresult *aResult);
@@ -207,7 +206,7 @@ public:
   virtual nsXPCClassInfo* GetClassInfo();
 
   virtual void DocSizeOfExcludingThis(nsWindowSizes* aWindowSizes) const;
-  
+  // DocSizeOfIncludingThis is inherited from nsIDocument.
 
 protected:
   nsresult GetBodySize(PRInt32* aWidth,
@@ -233,9 +232,9 @@ protected:
   nsresult CreateAndAddWyciwygChannel(void);
   nsresult RemoveWyciwygChannel(void);
 
-  
-
-
+  /**
+   * Like IsEditingOn(), but will flush as needed first.
+   */
   bool IsEditingOnAfterFlush();
 
   void *GenerateParserKey(void);
@@ -249,7 +248,7 @@ protected:
   nsRefPtr<nsContentList> mForms;
   nsRefPtr<nsContentList> mFormControls;
 
-  
+  /** # of forms in the document, synchronously set */
   PRInt32 mNumForms;
 
   static PRUint32 gWyciwygSessionCnt;
@@ -264,7 +263,7 @@ protected:
   static bool TryCacheCharset(nsICachingChannel* aCachingChannel,
                                 PRInt32& aCharsetSource,
                                 nsACString& aCharset);
-  
+  // aParentDocument could be null.
   bool TryParentCharset(nsIDocShell*  aDocShell,
                           nsIDocument* aParentDocument,
                           PRInt32& charsetSource, nsACString& aCharset);
@@ -274,16 +273,16 @@ protected:
                                   PRInt32& aCharsetSource,
                                   nsACString& aCharset);
 
-  
+  // Override so we can munge the charset on our wyciwyg channel as needed.
   virtual void SetDocumentCharacterSet(const nsACString& aCharSetID);
 
-  
-  
-  
-  
+  // Tracks if we are currently processing any document.write calls (either
+  // implicit or explicit). Note that if a write call writes out something which
+  // would block the parser, then mWriteLevel will be incorrect until the parser
+  // finishes processing that script.
   PRUint32 mWriteLevel;
 
-  
+  // Load flags of the document's channel
   PRUint32 mLoadFlags;
 
   bool mIsFrameset;
@@ -296,7 +295,7 @@ protected:
 
   nsCOMPtr<nsIWyciwygChannel> mWyciwygChannel;
 
-  
+  /* Midas implementation */
   nsresult   GetMidasCommandManager(nsICommandManager** aCommandManager);
 
   nsCOMPtr<nsICommandManager> mMidasCommandManager;
@@ -312,7 +311,7 @@ protected:
   static jsid        sCutCopyInternal_id;
   static jsid        sPasteInternal_id;
 
-  
+  // When false, the .cookies property is completely disabled
   bool mDisableCookieAccess;
 };
 
@@ -321,4 +320,4 @@ protected:
     NS_INTERFACE_TABLE_ENTRY(_class, nsIHTMLDocument)                         \
     NS_INTERFACE_TABLE_ENTRY(_class, nsIDOMHTMLDocument)
 
-#endif 
+#endif /* nsHTMLDocument_h___ */
