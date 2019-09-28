e8f1de5a0fe1ffa9bc3313ceaaf46ef90a098eea
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-09-28 17:57:25 +0000

    Bug 670003: Fix yarr on OS/2. r=dmandelin
    UltraBlame original commit: e9245c104f52e81db8022719891d91a8bfc20d3a

--- js/src/yarr/PageBlock.cpp
+++ js/src/yarr/PageBlock.cpp
@@ -1,31 +1,31 @@
-/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
- * vim: set ts=8 sw=4 et tw=99 ft=cpp:
- *
- * ***** BEGIN LICENSE BLOCK *****
- * Copyright (C) 2010 Apple Inc. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY APPLE INC. AND ITS CONTRIBUTORS ``AS IS''
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL APPLE INC. OR ITS CONTRIBUTORS
- * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
- * THE POSSIBILITY OF SUCH DAMAGE.
- *
- * ***** END LICENSE BLOCK ***** */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #include "PageBlock.h"
 #include "wtf/Assertions.h"
@@ -44,11 +44,15 @@
 #include <e32std.h>
 #endif
 
+#if WTF_OS_OS2
+#include <stdlib.h>
+#endif
+
 namespace WTF {
 
 static size_t s_pageSize;
 
-#if WTF_OS_UNIX && !WTF_OS_SYMBIAN
+#if (WTF_OS_UNIX && !WTF_OS_SYMBIAN) || WTF_OS_OS2
 
 inline size_t systemPageSize()
 {
@@ -85,4 +89,4 @@ size_t pageSize()
     return s_pageSize;
 }
 
-} // namespace WTF
+} 
