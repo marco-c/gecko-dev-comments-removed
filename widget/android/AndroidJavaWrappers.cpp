94f9434bcdb1442e534d7f321e7321b572612e27
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-09-28 20:30:15 +0000

    Bug 724928 - Tell Gecko not to draw areas we aren't interested in. r=kats
    Alter GeckoSoftwareLayerClient.beginDrawing so that it can return a rectangle
    of the buffer that it's interested in. Gecko then uses this to clip the dirty
    region, which often saves on unnecessary drawing during flings.
    
    UltraBlame original commit: 1a01259eb5c8daa732b44db34cba0f6af07e22a2

--- widget/android/AndroidJavaWrappers.cpp
+++ widget/android/AndroidJavaWrappers.cpp
@@ -1,39 +1,39 @@
-/* -*- Mode: c++; c-basic-offset: 4; tab-width: 20; indent-tabs-mode: nil; -*-
- * ***** BEGIN LICENSE BLOCK *****
- * Version: MPL 1.1/GPL 2.0/LGPL 2.1
- *
- * The contents of this file are subject to the Mozilla Public License Version
- * 1.1 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- * http://www.mozilla.org/MPL/
- *
- * Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- * for the specific language governing rights and limitations under the
- * License.
- *
- * The Original Code is Mozilla Android code.
- *
- * The Initial Developer of the Original Code is Mozilla Foundation.
- * Portions created by the Initial Developer are Copyright (C) 2010
- * the Initial Developer. All Rights Reserved.
- *
- * Contributor(s):
- *   Vladimir Vukicevic <vladimir@pobox.com>
- *
- * Alternatively, the contents of this file may be used under the terms of
- * either the GNU General Public License Version 2 or later (the "GPL"), or
- * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
- * in which case the provisions of the GPL or the LGPL are applicable instead
- * of those above. If you wish to allow use of your version of this file only
- * under the terms of either the GPL or the LGPL, and not to allow others to
- * use your version of this file under the terms of the MPL, indicate your
- * decision by deleting the provisions above and replace them with the notice
- * and other provisions required by the GPL or the LGPL. If you do not delete
- * the provisions above, a recipient may use your version of this file under
- * the terms of any one of the MPL, the GPL or the LGPL.
- *
- * ***** END LICENSE BLOCK ***** */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #include "AndroidJavaWrappers.h"
 #include "AndroidBridge.h"
@@ -126,7 +126,7 @@ jmethodID AndroidGeckoSurfaceView::jGetHolderMethod = 0;
 
 #define initInit() jclass jClass
 
-// note that this also sets jClass
+
 #define getClassGlobalRef(cname) \
     (jClass = jclass(jEnv->NewGlobalRef(jEnv->FindClass(cname))))
 
@@ -331,7 +331,7 @@ AndroidGeckoSoftwareLayerClient::InitGeckoSoftwareLayerClientClass(JNIEnv *jEnv)
 
     jLockBufferMethod = getMethod("lockBuffer", "()Ljava/nio/ByteBuffer;");
     jUnlockBufferMethod = getMethod("unlockBuffer", "()V");
-    jBeginDrawingMethod = getMethod("beginDrawing", "(IIIILjava/lang/String;Z)Z");
+    jBeginDrawingMethod = getMethod("beginDrawing", "(IIIILjava/lang/String;Z)Landroid/graphics/Rect;");
     jEndDrawingMethod = getMethod("endDrawing", "(IIII)V");
 #endif
 }
@@ -392,8 +392,8 @@ AndroidGeckoEvent::ReadRectField(JNIEnv *jenv)
     if (!r.isNull()) {
         mRect.SetRect(r.Left(),
                       r.Top(),
-                      r.Right() - r.Left(),
-                      r.Bottom() - r.Top());
+                      r.Width(),
+                      r.Height());
     } else {
         mRect.SetEmpty();
     }
@@ -576,7 +576,7 @@ AndroidGeckoEvent::Init(AndroidGeckoEvent *aResizeEvent)
 
     mType = FORCED_RESIZE;
     mTime = aResizeEvent->mTime;
-    mPoints = aResizeEvent->mPoints; // x,y coordinates
+    mPoints = aResizeEvent->mPoints; 
 }
 
 void
@@ -690,7 +690,7 @@ AndroidGeckoSoftwareLayerClient::UnlockBuffer()
 }
 
 bool
-AndroidGeckoSoftwareLayerClient::BeginDrawing(int aWidth, int aHeight, int aTileWidth, int aTileHeight, const nsAString &aMetadata, bool aHasDirectTexture)
+AndroidGeckoSoftwareLayerClient::BeginDrawing(int aWidth, int aHeight, int aTileWidth, int aTileHeight, nsIntRect &aDirtyRect, const nsAString &aMetadata, bool aHasDirectTexture)
 {
     NS_ASSERTION(!isNull(), "BeginDrawing() called on null software layer client!");
     JNIEnv *env = AndroidBridge::GetJNIEnv();
@@ -699,7 +699,20 @@ AndroidGeckoSoftwareLayerClient::BeginDrawing(int aWidth, int aHeight, int aTile
 
     AndroidBridge::AutoLocalJNIFrame(env, 1);
     jstring jMetadata = env->NewString(nsPromiseFlatString(aMetadata).get(), aMetadata.Length());
-    return env->CallBooleanMethod(wrapped_obj, jBeginDrawingMethod, aWidth, aHeight, aTileWidth, aTileHeight, jMetadata, aHasDirectTexture);
+
+    jobject rectObject = env->CallObjectMethod(wrapped_obj, jBeginDrawingMethod,
+                                               aWidth, aHeight, aTileWidth, aTileHeight,
+                                               jMetadata, aHasDirectTexture);
+
+    if (rectObject == nsnull)
+        return false;
+
+    AndroidRect rect(env, rectObject);
+    nsIntRect newDirtyRect = aDirtyRect.Intersect(nsIntRect(rect.Top(), rect.Left(),
+                                                            rect.Width(), rect.Height()));
+    aDirtyRect.SetRect(newDirtyRect.x, newDirtyRect.y, newDirtyRect.width, newDirtyRect.height);
+
+    return true;
 }
 
 void
--- widget/android/AndroidJavaWrappers.h
+++ widget/android/AndroidJavaWrappers.h
@@ -1,39 +1,39 @@
-/* -*- Mode: c++; c-basic-offset: 4; tab-width: 20; indent-tabs-mode: nil; -*-
- * ***** BEGIN LICENSE BLOCK *****
- * Version: MPL 1.1/GPL 2.0/LGPL 2.1
- *
- * The contents of this file are subject to the Mozilla Public License Version
- * 1.1 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- * http://www.mozilla.org/MPL/
- *
- * Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- * for the specific language governing rights and limitations under the
- * License.
- *
- * The Original Code is Mozilla Android code.
- *
- * The Initial Developer of the Original Code is Mozilla Foundation.
- * Portions created by the Initial Developer are Copyright (C) 2010
- * the Initial Developer. All Rights Reserved.
- *
- * Contributor(s):
- *   Vladimir Vukicevic <vladimir@pobox.com>
- *
- * Alternatively, the contents of this file may be used under the terms of
- * either the GNU General Public License Version 2 or later (the "GPL"), or
- * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
- * in which case the provisions of the GPL or the LGPL are applicable instead
- * of those above. If you wish to allow use of your version of this file only
- * under the terms of either the GPL or the LGPL, and not to allow others to
- * use your version of this file under the terms of the MPL, indicate your
- * decision by deleting the provisions above and replace them with the notice
- * and other provisions required by the GPL or the LGPL. If you do not delete
- * the provisions above, a recipient may use your version of this file under
- * the terms of any one of the MPL, the GPL or the LGPL.
- *
- * ***** END LICENSE BLOCK ***** */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #ifndef AndroidJavaWrappers_h__
 #define AndroidJavaWrappers_h__
@@ -46,7 +46,7 @@
 #include "nsRect.h"
 #include "nsString.h"
 
-//#define FORCE_ALOG 1
+
 
 #ifndef ALOG
 #if defined(DEBUG) || defined(FORCE_ALOG)
@@ -60,14 +60,14 @@ namespace mozilla {
 
 void InitAndroidJavaWrappers(JNIEnv *jEnv);
 
-/*
- * Note: do not store global refs to any WrappedJavaObject;
- * these are live only during a particular JNI method, as
- * NewGlobalRef is -not- called on the jobject.
- *
- * If this is needed, WrappedJavaObject can be extended to
- * handle it.
- */
+
+
+
+
+
+
+
+
 
 class WrappedJavaObject {
 public:
@@ -135,6 +135,8 @@ public:
     int Left() { return mLeft; }
     int Right() { return mRight; }
     int Top() { return mTop; }
+    int Width() { return mRight - mLeft; }
+    int Height() { return mBottom - mTop; }
 
 protected:
     int mBottom;
@@ -161,7 +163,7 @@ public:
     jobject LockBuffer();
     unsigned char *LockBufferBits();
     void UnlockBuffer();
-    bool BeginDrawing(int aWidth, int aHeight, int aTileWidth, int aTileHeight, const nsAString &aMetadata, bool aHasDirectTexture);
+    bool BeginDrawing(int aWidth, int aHeight, int aTileWidth, int aTileHeight, nsIntRect &aDirtyRect, const nsAString &aMetadata, bool aHasDirectTexture);
     void EndDrawing(const nsIntRect &aRect);
 
 private:
@@ -203,8 +205,8 @@ public:
 
     jobject GetSurface();
 
-    // must have a JNI local frame when calling this,
-    // and you'd better know what you're doing
+    
+    
     jobject GetSurfaceHolder();
 
 protected:
