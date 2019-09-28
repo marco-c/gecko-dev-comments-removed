190fc7f39c0a1406191cf15157708f45a2d6ca6c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-09-28 17:08:23 +0000

    6. test patch 2 - Bug 386760 - directory removal with software update. r=dolske
    UltraBlame original commit: 35e27b34c7c515f8f3b76962ca5521fa5ab64bee

--- toolkit/mozapps/update/test/unit/test_0182_rmrfdirFileInUse_xp_win_complete.js
+++ toolkit/mozapps/update/test/unit/test_0182_rmrfdirFileInUse_xp_win_complete.js
@@ -1,55 +1,193 @@
-/* Any copyright is dedicated to the Public Domain.
- * http://creativecommons.org/publicdomain/zero/1.0/
- */
 
-/* File in use complete MAR file patch apply success test */
+
+
+
+
 
 const TEST_ID = "0182";
 
-// The files are in the same order as they are applied from the mar
+
+
+
 const TEST_FILES = [
 {
-  fileName         : "00png0.png",
-  relPathDir       : "0/00/",
-  originalContents : null,
-  compareContents  : null,
-  originalFile     : null,
-  compareFile      : "data/complete.png"
-}, {
-  fileName         : "00text0",
-  relPathDir       : "0/00/",
-  originalContents : "ToBeReplacedWithToBeModified\n",
-  compareContents  : "ToBeModified\n",
+  description      : "Only added by update.manifest for complete updates " +
+                     "when there is a channel change (add-cc)",
+  fileName         : "channel-prefs.js",
+  relPathDir       : "a/b/defaults/pref/",
+  originalContents : "ShouldNotBeReplaced\n",
+  compareContents  : "ShouldNotBeReplaced\n",
   originalFile     : null,
   compareFile      : null
 }, {
-  fileName         : "00text1",
-  relPathDir       : "0/00/",
-  originalContents : "ToBeReplacedWithToBeDeleted\n",
-  compareContents  : "ToBeDeleted\n",
-  originalFile     : null,
-  compareFile      : null
-}, {
-  fileName         : "0exe0.exe",
-  relPathDir       : "0/",
-  originalContents : null,
-  compareContents  : null,
-  originalFile     : HELPER_BIN_FILE,
-  compareFile      : "data/complete.png"
-}, {
-  fileName         : "10text0",
-  relPathDir       : "1/10/",
-  originalContents : "ToBeReplacedWithToBeDeleted\n",
-  compareContents  : "ToBeDeleted\n",
-  originalFile     : null,
-  compareFile      : null
-}, {
-  fileName         : "exe0.exe",
+  description      : "Added by update.manifest (add)",
+  fileName         : "precomplete",
   relPathDir       : "",
   originalContents : null,
   compareContents  : null,
+  originalFile     : "data/partial_precomplete",
+  compareFile      : "data/complete_precomplete"
+}, {
+  description      : "Added by update.manifest (add)",
+  fileName         : "searchpluginstext0",
+  relPathDir       : "a/b/searchplugins/",
+  originalContents : "ToBeReplacedWithFromComplete\n",
+  compareContents  : "FromComplete\n",
+  originalFile     : null,
+  compareFile      : null
+}, {
+  description      : "Added by update.manifest (add)",
+  fileName         : "searchpluginspng1.png",
+  relPathDir       : "a/b/searchplugins/",
+  originalContents : null,
+  compareContents  : null,
+  originalFile     : null,
+  compareFile      : "data/complete.png"
+}, {
+  description      : "Added by update.manifest (add)",
+  fileName         : "searchpluginspng0.png",
+  relPathDir       : "a/b/searchplugins/",
+  originalContents : null,
+  compareContents  : null,
+  originalFile     : "data/partial.png",
+  compareFile      : "data/complete.png"
+}, {
+  description      : "Added by update.manifest (add)",
+  fileName         : "removed-files",
+  relPathDir       : "a/b/",
+  originalContents : null,
+  compareContents  : null,
+  originalFile     : "data/partial_removed-files",
+  compareFile      : "data/complete_removed-files"
+}, {
+  description      : "Added by update.manifest if the parent directory " +
+                     "exists (add-if)",
+  fileName         : "extensions1text0",
+  relPathDir       : "a/b/extensions/extensions1/",
+  originalContents : null,
+  compareContents  : "FromComplete\n",
+  originalFile     : null,
+  compareFile      : null
+}, {
+  description      : "Added by update.manifest if the parent directory " +
+                     "exists (add-if)",
+  fileName         : "extensions1png1.png",
+  relPathDir       : "a/b/extensions/extensions1/",
+  originalContents : null,
+  compareContents  : null,
+  originalFile     : "data/partial.png",
+  compareFile      : "data/complete.png"
+}, {
+  description      : "Added by update.manifest if the parent directory " +
+                     "exists (add-if)",
+  fileName         : "extensions1png0.png",
+  relPathDir       : "a/b/extensions/extensions1/",
+  originalContents : null,
+  compareContents  : null,
+  originalFile     : null,
+  compareFile      : "data/complete.png"
+}, {
+  description      : "Added by update.manifest if the parent directory " +
+                     "exists (add-if)",
+  fileName         : "extensions0text0",
+  relPathDir       : "a/b/extensions/extensions0/",
+  originalContents : "ToBeReplacedWithFromComplete\n",
+  compareContents  : "FromComplete\n",
+  originalFile     : null,
+  compareFile      : null
+}, {
+  description      : "Added by update.manifest if the parent directory " +
+                     "exists (add-if)",
+  fileName         : "extensions0png1.png",
+  relPathDir       : "a/b/extensions/extensions0/",
+  originalContents : null,
+  compareContents  : null,
+  originalFile     : null,
+  compareFile      : "data/complete.png"
+}, {
+  description      : "Added by update.manifest if the parent directory " +
+                     "exists (add-if)",
+  fileName         : "extensions0png0.png",
+  relPathDir       : "a/b/extensions/extensions0/",
+  originalContents : null,
+  compareContents  : null,
+  originalFile     : null,
+  compareFile      : "data/complete.png"
+}, {
+  description      : "Added by update.manifest (add)",
+  fileName         : "exe0.exe",
+  relPathDir       : "a/b/",
+  originalContents : null,
+  compareContents  : null,
   originalFile     : HELPER_BIN_FILE,
   compareFile      : "data/complete.png"
+}, {
+  description      : "Added by update.manifest (add)",
+  fileName         : "10text0",
+  relPathDir       : "a/b/1/10/",
+  originalContents : "ToBeReplacedWithFromComplete\n",
+  compareContents  : "FromComplete\n",
+  originalFile     : null,
+  compareFile      : null
+}, {
+  description      : "Added by update.manifest (add) file in use",
+  fileName         : "0exe0.exe",
+  relPathDir       : "a/b/0/",
+  originalContents : null,
+  compareContents  : null,
+  originalFile     : HELPER_BIN_FILE,
+  compareFile      : "data/complete.png"
+}, {
+  description      : "Added by update.manifest (add)",
+  fileName         : "00text1",
+  relPathDir       : "a/b/0/00/",
+  originalContents : "ToBeReplacedWithFromComplete\n",
+  compareContents  : "FromComplete\n",
+  originalFile     : null,
+  compareFile      : null
+}, {
+  description      : "Added by update.manifest (add)",
+  fileName         : "00text0",
+  relPathDir       : "a/b/0/00/",
+  originalContents : "ToBeReplacedWithFromComplete\n",
+  compareContents  : "FromComplete\n",
+  originalFile     : null,
+  compareFile      : null
+}, {
+  description      : "Added by update.manifest (add)",
+  fileName         : "00png0.png",
+  relPathDir       : "a/b/0/00/",
+  originalContents : null,
+  compareContents  : null,
+  originalFile     : null,
+  compareFile      : "data/complete.png"
+}, {
+  description      : "Removed by precomplete (remove)",
+  fileName         : "20text0",
+  relPathDir       : "a/b/2/20/",
+  originalContents : "ToBeDeleted\n",
+  compareContents  : null,
+  originalFile     : null,
+  compareFile      : null
+}, {
+  description      : "Removed by precomplete (remove)",
+  fileName         : "20png0.png",
+  relPathDir       : "a/b/2/20/",
+  originalContents : "ToBeDeleted\n",
+  compareContents  : null,
+  originalFile     : null,
+  compareFile      : null
+}];
+
+ADDITIONAL_TEST_DIRS = [
+{
+  description  : "Removed by precomplete (rmdir)",
+  relPathDir   : "a/b/2/20/",
+  dirRemoved   : true
+}, {
+  description  : "Removed by precomplete (rmdir)",
+  relPathDir   : "a/b/2/",
+  dirRemoved   : true
 }];
 
 function run_test() {
@@ -66,8 +204,8 @@ function run_test() {
   let fileInUseBin = getApplyDirFile(TEST_DIRS[4].relPathDir +
                                      TEST_DIRS[4].subDirs[0] +
                                      TEST_DIRS[4].subDirFiles[0]);
-  // Remove the empty file created for the test so the helper application can
-  // replace it.
+  
+  
   fileInUseBin.remove(false);
 
   let helperBin = do_get_file(HELPER_BIN_FILE);
@@ -75,8 +213,8 @@ function run_test() {
                                     TEST_DIRS[4].subDirs[0]);
   helperBin.copyTo(fileInUseDir, TEST_DIRS[4].subDirFiles[0]);
 
-  // Launch an existing file so it is in use during the update
-  let args = [getApplyDirPath(), "input", "output", "-s", "20"];
+  
+  let args = [getApplyDirPath() + "a/b/", "input", "output", "-s", "20"];
   let fileInUseProcess = AUS_Cc["@mozilla.org/process/util;1"].
                          createInstance(AUS_Ci.nsIProcess);
   fileInUseProcess.init(fileInUseBin);
@@ -86,7 +224,7 @@ function run_test() {
 }
 
 function doUpdate() {
-  // apply the complete mar
+  
   let exitValue = runUpdate();
   logTestInfo("testing updater binary process exitValue for success when " +
               "applying a complete mar");
@@ -96,17 +234,15 @@ function doUpdate() {
 }
 
 function checkUpdate() {
-  let updatesDir = do_get_file(TEST_ID + UPDATES_DIR_SUFFIX);
-  let applyToDir = getApplyDirFile();
-
   logTestInfo("testing update.status should be " + STATE_SUCCEEDED);
+  let updatesDir = do_get_file(TEST_ID + UPDATES_DIR_SUFFIX);
   do_check_eq(readStatusFile(updatesDir), STATE_SUCCEEDED);
 
   checkFilesAfterUpdateSuccess();
+  checkUpdateLogContains(ERR_BACKUP_DISCARD);
 
   logTestInfo("testing tobedeleted directory exists");
-  let toBeDeletedDir = applyToDir.clone();
-  toBeDeletedDir.append("tobedeleted");
+  let toBeDeletedDir = getApplyDirFile("tobedeleted", true);
   do_check_true(toBeDeletedDir.exists());
 
   checkCallbackAppLog();
