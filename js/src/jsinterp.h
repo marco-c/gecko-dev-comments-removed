3be5804ba3ecb81e160c2c8d23b8c8f8cb33a73d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-09-28 13:07:18 +0000

    Merge from TM
    UltraBlame original commit: 95dc54180c543b4674c6cb4761f84baa29eb2820

--- js/src/jsinterp.h
+++ js/src/jsinterp.h
@@ -386,7 +386,7 @@ ValueToId(JSContext *cx, const Value &v, jsid *idp);
 
 
 extern const js::Value &
-js_GetUpvar(JSContext *cx, uintN level, uintN cookie);
+js_GetUpvar(JSContext *cx, uintN level, js::UpvarCookie cookie);
 
 
 
