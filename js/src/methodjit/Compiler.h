ec7b4f5b7cc3785ece9ac80ae87853e648084b7c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-09-28 18:16:31 +0000

    Merge from tracemonkey.
    UltraBlame original commit: f2383940c993d2fbb57d8fc54dda5ffd8742ed8e

--- js/src/methodjit/Compiler.h
+++ js/src/methodjit/Compiler.h
@@ -1,42 +1,42 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ * vim: set ts=4 sw=4 et tw=99:
+ *
+ * ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is Mozilla SpiderMonkey JavaScript 1.9 code, released
+ * May 28, 2008.
+ *
+ * The Initial Developer of the Original Code is
+ *   Brendan Eich <brendan@mozilla.org>
+ *
+ * Contributor(s):
+ *   David Anderson <danderson@mozilla.com>
+ *   David Mandelin <dmandelin@mozilla.com>
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either of the GNU General Public License Version 2 or later (the "GPL"),
+ * or the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
 #if !defined jsjaeger_compiler_h__ && defined JS_METHODJIT
 #define jsjaeger_compiler_h__
 
@@ -60,6 +60,7 @@ class Compiler
     typedef JSC::MacroAssembler::RegisterID RegisterID;
     typedef JSC::MacroAssembler::FPRegisterID FPRegisterID;
     typedef JSC::MacroAssembler::Address Address;
+    typedef JSC::MacroAssembler::AbsoluteAddress AbsoluteAddress;
     typedef JSC::MacroAssembler::BaseIndex BaseIndex;
     typedef JSC::MacroAssembler::Jump Jump;
     typedef JSC::MacroAssembler::JumpList JumpList;
@@ -104,17 +105,17 @@ class Compiler
         } u;
     };
 
-    
+    /* InlineFrameAssembler wants to see this. */
   public:
     struct CallGenInfo {
         CallGenInfo(uint32 argc)
           : argc(argc)
         { }
 
-        
-
-
-
+        /*
+         * These members map to members in CallICInfo. See that structure for
+         * more comments.
+         */
         jsbytecode   *pc;
         uint32       argc;
         DataLabelPtr funGuard;
@@ -223,8 +224,8 @@ class Compiler
     bool addTraceHints;
 
   public:
-    
-    
+    // Special atom index used to indicate that the atom is 'length'. This
+    // follows interpreter usage in JSOP_LENGTH.
     enum { LengthAtomIndex = uint32(-2) };
 
     Compiler(JSContext *cx, JSScript *script, JSFunction *fun, JSObject *scopeChain);
@@ -244,14 +245,14 @@ class Compiler
     CompileStatus generateEpilogue();
     CompileStatus finishThisUp();
 
-    
+    /* Non-emitting helpers. */
     uint32 fullAtomIndex(jsbytecode *pc);
     void jumpInScript(Jump j, jsbytecode *pc);
     JSC::ExecutablePool *getExecPool(size_t size);
     bool compareTwoValues(JSContext *cx, JSOp op, const Value &lhs, const Value &rhs);
     void addCallSite(uint32 id, bool stub);
 
-    
+    /* Emitting helpers. */
     RegisterID takeHWReturnAddress(Assembler &masm);
     void restoreReturnAddress(Assembler &masm);
     void restoreFrameRegs(Assembler &masm);
@@ -262,7 +263,7 @@ class Compiler
     void iterEnd();
     MaybeJump loadDouble(FrameEntry *fe, FPRegisterID fpReg);
 
-    
+    /* Opcode handlers. */
     void jumpAndTrace(Jump j, jsbytecode *target, Jump *slowOne = NULL, Jump *slowTwo = NULL);
     void jsop_bindname(uint32 index);
     void jsop_setglobal(uint32 index);
@@ -300,7 +301,7 @@ class Compiler
     void jsop_instanceof();
     void jsop_name(JSAtom *atom);
 
-    
+    /* Fast arithmetic. */
     void jsop_binary(JSOp op, VoidStub stub);
     void jsop_binary_full(FrameEntry *lhs, FrameEntry *rhs, JSOp op, VoidStub stub);
     void jsop_binary_full_simple(FrameEntry *fe, JSOp op, VoidStub stub);
@@ -324,7 +325,7 @@ class Compiler
     bool tryBinaryConstantFold(JSContext *cx, FrameState &frame, JSOp op,
                                FrameEntry *lhs, FrameEntry *rhs);
 
-    
+    /* Fast opcodes. */
     void jsop_bitop(JSOp op);
     void jsop_rsh();
     RegisterID rightRegForShift(FrameEntry *rhs);
@@ -390,8 +391,8 @@ class Compiler
     Call stubCall(void *ptr);
 };
 
-} 
-} 
+} /* namespace js */
+} /* namespace mjit */
 
 #endif
 
