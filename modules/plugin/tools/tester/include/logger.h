6dae1e3ad181467f1684c162625c2ab2759800fc
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-09-28 00:21:46 +0000

    A bunch of plugin SDK build fixes. b=471759 r=josh sr=jst
    UltraBlame original commit: d813cc048225cc775142d55dea1d379a4903ba8d

--- modules/plugin/tools/tester/include/logger.h
+++ modules/plugin/tools/tester/include/logger.h
@@ -1,39 +1,39 @@
-/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
-/* ***** BEGIN LICENSE BLOCK *****
- * Version: MPL 1.1/GPL 2.0/LGPL 2.1
- *
- * The contents of this file are subject to the Mozilla Public License Version
- * 1.1 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- * http://www.mozilla.org/MPL/
- *
- * Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- * for the specific language governing rights and limitations under the
- * License.
- *
- * The Original Code is mozilla.org code.
- *
- * The Initial Developer of the Original Code is
- * Netscape Communications Corporation.
- * Portions created by the Initial Developer are Copyright (C) 1998
- * the Initial Developer. All Rights Reserved.
- *
- * Contributor(s):
- *
- * Alternatively, the contents of this file may be used under the terms of
- * either the GNU General Public License Version 2 or later (the "GPL"), or
- * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
- * in which case the provisions of the GPL or the LGPL are applicable instead
- * of those above. If you wish to allow use of your version of this file only
- * under the terms of either the GPL or the LGPL, and not to allow others to
- * use your version of this file under the terms of the MPL, indicate your
- * decision by deleting the provisions above and replace them with the notice
- * and other provisions required by the GPL or the LGPL. If you do not delete
- * the provisions above, a recipient may use your version of this file under
- * the terms of any one of the MPL, the GPL or the LGPL.
- *
- * ***** END LICENSE BLOCK ***** */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #ifndef __LOGGER_H__
 #define __LOGGER_H__
@@ -41,7 +41,7 @@
 #include "plugbase.h"
 #include "action.h"
 #include "log.h"
-#include "logFile.h"
+#include "logfile.h"
 
 class CLogger
 {
@@ -101,10 +101,10 @@ public:
 #define LOGGER_DEFAULT_STRING_WRAP    32
 #define LOGGER_DEFAULT_TARGET "_npapi_Log"
 
-// Preferences profile stuff
+
 #define SECTION_LOG           "Log"
 #define KEY_RECORD_SEPARATOR  "RecordSeparator"
 #define KEY_STRING_WRAP       "StringDataWrap"
 #define KEY_FILE_NAME         "FileName"
 
-#endif // __LOGGER_H__
+#endif 
