205b8583be4678e0492c8c3727fe448c1b17ea97
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-09-28 00:48:03 +0000

    Bug 469718 - browser javascript reftests, r=ted.mielczarek, r=sayrer, r=dbaron.
    UltraBlame original commit: 7b082354192019885f696e600ad1e60f836fed4d

---
+++ js/src/tests/e4x/Types/9.1.1.7.js
@@ -0,0 +1,53 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+gTestfile = '9.1.1.7.js';
+
+START("9.1.1.7 - XML [[DeepCopy]]");
+
+var actual = 'Test not implemented';
+var expect = 'PASS';
+
+TEST(1, expect, actual);
+
+END();
+
