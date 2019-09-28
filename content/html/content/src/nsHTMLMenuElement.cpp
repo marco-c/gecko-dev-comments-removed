fd5c93b5ed3e014ce3a53aeed1d4c1ad3e5a5acf
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-09-28 20:42:40 +0000

    Bug 737976: Remove nsINodeInfo. rs=bent
    UltraBlame original commit: b00bf7f3869c471073f6ade173b50617904066e0

--- content/html/content/src/nsHTMLMenuElement.cpp
+++ content/html/content/src/nsHTMLMenuElement.cpp
@@ -1,38 +1,38 @@
-/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
-/* ***** BEGIN LICENSE BLOCK *****
- * Version: MPL 1.1/GPL 2.0/LGPL 2.1
- *
- * The contents of this file are subject to the Mozilla Public License Version
- * 1.1 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- * http://www.mozilla.org/MPL/
- *
- * Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- * for the specific language governing rights and limitations under the
- * License.
- *
- * The Original Code is Mozilla.
- *
- * The Initial Developer of the Original Code is Mozilla Foundation
- * Portions created by the Initial Developer are Copyright (C) 2011
- * the Initial Developer. All Rights Reserved.
- *
- * Contributor(s):
- *
- * Alternatively, the contents of this file may be used under the terms of
- * either of the GNU General Public License Version 2 or later (the "GPL"),
- * or the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
- * in which case the provisions of the GPL or the LGPL are applicable instead
- * of those above. If you wish to allow use of your version of this file only
- * under the terms of either the GPL or the LGPL, and not to allow others to
- * use your version of this file under the terms of the MPL, indicate your
- * decision by deleting the provisions above and replace them with the notice
- * and other provisions required by the GPL or the LGPL. If you do not delete
- * the provisions above, a recipient may use your version of this file under
- * the terms of any one of the MPL, the GPL or the LGPL.
- *
- * ***** END LICENSE BLOCK ***** */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #include "nsHTMLMenuElement.h"
 
@@ -70,7 +70,7 @@ enum SeparatorType
 NS_IMPL_NS_NEW_HTML_ELEMENT(Menu)
 
 
-nsHTMLMenuElement::nsHTMLMenuElement(already_AddRefed<nsINodeInfo> aNodeInfo)
+nsHTMLMenuElement::nsHTMLMenuElement(already_AddRefed<nsNodeInfo> aNodeInfo)
   : nsGenericHTMLElement(aNodeInfo), mType(MENU_TYPE_LIST)
 {
 }
@@ -86,7 +86,7 @@ NS_IMPL_RELEASE_INHERITED(nsHTMLMenuElement, nsGenericElement)
 
 DOMCI_NODE_DATA(HTMLMenuElement, nsHTMLMenuElement)
 
-// QueryInterface implementation for nsHTMLMenuElement
+
 NS_INTERFACE_TABLE_HEAD(nsHTMLMenuElement)
   NS_HTML_CONTENT_INTERFACE_TABLE2(nsHTMLMenuElement,
                                    nsIDOMHTMLMenuElement,
@@ -198,7 +198,7 @@ nsHTMLMenuElement::BuildSubmenu(const nsAString& aLabel,
   aBuilder->CloseContainer();
 }
 
-// static
+
 bool
 nsHTMLMenuElement::CanLoadIcon(nsIContent* aContent, const nsAString& aIcon)
 {
