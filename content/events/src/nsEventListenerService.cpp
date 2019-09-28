6fea0b7ec2f513a1d7404b39a6b02b80c166b14a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-09-28 20:50:37 +0000

    Bug 741245 - Remove nsresult return value from nsXPConnect::GetSafeJSContext(); r=bholley
    UltraBlame original commit: 4b0cc175806e1b0b37f342d8a55c242568df7514

--- content/events/src/nsEventListenerService.cpp
+++ content/events/src/nsEventListenerService.cpp
@@ -1,39 +1,39 @@
-/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
-/* ***** BEGIN LICENSE BLOCK *****
- * Version: MPL 1.1/GPL 2.0/LGPL 2.1
- *
- * The contents of this file are subject to the Mozilla Public License Version
- * 1.1 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- * http://www.mozilla.org/MPL/
- *
- * Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- * for the specific language governing rights and limitations under the
- * License.
- *
- * The Original Code is mozilla.org code.
- *
- * The Initial Developer of the Original Code is Mozilla Foundation.
- * Portions created by the Initial Developer are Copyright (C) 2009
- * the Initial Developer. All Rights Reserved.
- *
- * Contributor(s):
- *   Olli Pettay <Olli.Pettay@helsinki.fi> (Original Author)
- *
- * Alternatively, the contents of this file may be used under the terms of
- * either of the GNU General Public License Version 2 or later (the "GPL"),
- * or the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
- * in which case the provisions of the GPL or the LGPL are applicable instead
- * of those above. If you wish to allow use of your version of this file only
- * under the terms of either the GPL or the LGPL, and not to allow others to
- * use your version of this file under the terms of the MPL, indicate your
- * decision by deleting the provisions above and replace them with the notice
- * and other provisions required by the GPL or the LGPL. If you do not delete
- * the provisions above, a recipient may use your version of this file under
- * the terms of any one of the MPL, the GPL or the LGPL.
- *
- * ***** END LICENSE BLOCK ***** */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #include "nsEventListenerService.h"
 #include "nsCOMArray.h"
 #include "nsEventListenerManager.h"
@@ -97,7 +97,7 @@ nsEventListenerInfo::GetInSystemEventGroup(bool* aInSystemEventGroup)
 
 NS_IMPL_ISUPPORTS1(nsEventListenerService, nsIEventListenerService)
 
-// Caller must root *aJSVal!
+
 bool
 nsEventListenerInfo::GetJSVal(JSContext* aCx, JSAutoEnterCompartment& aAc, jsval* aJSVal)
 {
@@ -134,11 +134,10 @@ nsEventListenerInfo::ToSource(nsAString& aResult)
   nsCOMPtr<nsIThreadJSContextStack> stack =
     nsContentUtils::ThreadJSContextStack();
   if (stack) {
-    JSContext* cx = nsnull;
-    stack->GetSafeJSContext(&cx);
+    JSContext* cx = stack->GetSafeJSContext();
     if (cx && NS_SUCCEEDED(stack->Push(cx))) {
       {
-        // Extra block to finish the auto request before calling pop
+        
         JSAutoRequest ar(cx);
         JSAutoEnterCompartment ac;
         jsval v = JSVAL_NULL;
@@ -177,11 +176,10 @@ nsEventListenerInfo::GetDebugObject(nsISupports** aRetVal)
   nsCOMPtr<nsIThreadJSContextStack> stack =
     nsContentUtils::ThreadJSContextStack();
   if (stack) {
-    JSContext* cx = nsnull;
-    stack->GetSafeJSContext(&cx);
+    JSContext* cx = stack->GetSafeJSContext();
     if (cx && NS_SUCCEEDED(stack->Push(cx))) {
       {
-        // Extra block to finish the auto request before calling pop
+        
         JSAutoRequest ar(cx);
         JSAutoEnterCompartment ac;
         jsval v = JSVAL_NULL;
