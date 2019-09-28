6c56161f75e8df3521b01bd0fa9de7d3f016a7db
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-09-28 18:35:56 +0000

    Merge MC -> JM
    UltraBlame original commit: 1f767f82d1c309e5f69364f1ddcb24d1f5eff0f9

--- js/src/jsbuiltins.h
+++ js/src/jsbuiltins.h
@@ -231,7 +231,6 @@ struct ClosureVarInfo;
 #define _JS_CTYPE_CHARPTR           _JS_CTYPE(char *,                 _JS_PTR, --, --, INFALLIBLE)
 #define _JS_CTYPE_CVIPTR            _JS_CTYPE(const ClosureVarInfo *, _JS_PTR, --, --, INFALLIBLE)
 #define _JS_CTYPE_FRAMEINFO         _JS_CTYPE(FrameInfo *,            _JS_PTR, --, --, INFALLIBLE)
-#define _JS_CTYPE_PICTABLE          _JS_CTYPE(PICTable *,             _JS_PTR, --, --, INFALLIBLE)
 #define _JS_CTYPE_UINTN             _JS_CTYPE(uintN,                  _JS_PTR, --, --, INFALLIBLE)
  
 
@@ -290,7 +289,7 @@ struct ClosureVarInfo;
 #define _JS_DEFINE_CALLINFO(linkage, name, crtype, cargtypes, argtypes, isPure, storeAccSet)      \
     _JS_TN_LINKAGE(linkage, crtype) FASTCALL name cargtypes;                                      \
     _JS_CI_LINKAGE(linkage) const nanojit::CallInfo _JS_CALLINFO(name) =                          \
-        { (intptr_t) &name, argtypes, nanojit::ABI_FASTCALL, isPure, storeAccSet _JS_CI_NAME(name) }; \
+        { (uintptr_t) &name, argtypes, nanojit::ABI_FASTCALL, isPure, storeAccSet _JS_CI_NAME(name) }; \
     JS_STATIC_ASSERT_IF(isPure, (storeAccSet) == nanojit::ACCSET_NONE);
 #endif
 
--- js/src/jscompartment.cpp
+++ js/src/jscompartment.cpp
@@ -47,6 +47,7 @@
 #include "jsproxy.h"
 #include "jsscope.h"
 #include "jstracer.h"
+#include "jswatchpoint.h"
 #include "jswrapper.h"
 #include "assembler/wtf/Platform.h"
 #include "yarr/BumpPointerAllocator.h"
@@ -92,7 +93,8 @@ JSCompartment::JSCompartment(JSRuntime *rt)
     initialRegExpShape(NULL),
     initialStringShape(NULL),
     debugMode(rt->debugMode),
-    mathCache(NULL)
+    mathCache(NULL),
+    watchpointMap(NULL)
 {
     JS_INIT_CLIST(&scripts);
 
@@ -114,6 +116,7 @@ JSCompartment::~JSCompartment()
 #endif
 
     Foreground::delete_(mathCache);
+    Foreground::delete_(watchpointMap);
 
 #ifdef DEBUG
     for (size_t i = 0; i != JS_ARRAY_LENGTH(scriptsToGC); ++i)
@@ -294,7 +297,7 @@ JSCompartment::wrap(JSContext *cx, Value *vp)
         if (vp->isObject()) {
             JSObject *obj = &vp->toObject();
             JS_ASSERT(IsCrossCompartmentWrapper(obj));
-            if (obj->getParent() != global) {
+            if (global->getJSClass() != &js_dummy_class && obj->getParent() != global) {
                 do {
                     obj->setParent(global);
                     obj = obj->getProto();
@@ -486,7 +489,7 @@ JSCompartment::markTypes(JSTracer *trc)
 
     for (JSCList *cursor = scripts.next; cursor != &scripts; cursor = cursor->next) {
         JSScript *script = reinterpret_cast<JSScript *>(cursor);
-        js_TraceScript(trc, script);
+        js_TraceScript(trc, script, NULL);
     }
 
     gc::ArenaHeader *aheader = arenas[gc::FINALIZE_TYPE_OBJECT].getHead();
@@ -495,14 +498,14 @@ JSCompartment::markTypes(JSTracer *trc)
     for (; aheader; aheader = aheader->next) {
         gc::Arena *arena = aheader->getArena();
         gc::FreeSpan firstSpan(aheader->getFirstFreeSpan());
-        gc::FreeSpan *span = &firstSpan;
+        const gc::FreeSpan *span = &firstSpan;
 
         for (uintptr_t thing = arena->thingsStart(thingSize); ; thing += thingSize) {
             JS_ASSERT(thing <= arena->thingsEnd());
-            if (thing == span->start) {
+            if (thing == span->first) {
                 if (!span->hasNext())
                     break;
-                thing = span->end;
+                thing = span->last;
                 span = span->nextSpan();
             } else {
                 types::TypeObject *object = reinterpret_cast<types::TypeObject *>(thing);
