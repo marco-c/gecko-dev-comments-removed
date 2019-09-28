de1cb64439ac7958aa41b6634fab11c2587797e3
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-09-28 20:33:39 +0000

    Ensure that iterators are closed when an exception is thrown (bug 729797, r=luke).
    UltraBlame original commit: aec23eb06a3e8b1b194b05a970e8e813ba6adf05

--- js/src/jswrapper.cpp
+++ js/src/jswrapper.cpp
@@ -1,43 +1,43 @@
-/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
- * vim: set ts=4 sw=4 et tw=99:
- *
- * ***** BEGIN LICENSE BLOCK *****
- * Version: MPL 1.1/GPL 2.0/LGPL 2.1
- *
- * The contents of this file are subject to the Mozilla Public License Version
- * 1.1 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- * http://www.mozilla.org/MPL/
- *
- * Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- * for the specific language governing rights and limitations under the
- * License.
- *
- * The Original Code is Mozilla SpiderMonkey JavaScript 1.9 code, released
- * May 28, 2008.
- *
- * The Initial Developer of the Original Code is
- *   Mozilla Foundation
- * Portions created by the Initial Developer are Copyright (C) 2010
- * the Initial Developer. All Rights Reserved.
- *
- * Contributor(s):
- *   Andreas Gal <gal@mozilla.com>
- *
- * Alternatively, the contents of this file may be used under the terms of
- * either of the GNU General Public License Version 2 or later (the "GPL"),
- * or the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
- * in which case the provisions of the GPL or the LGPL are applicable instead
- * of those above. If you wish to allow use of your version of this file only
- * under the terms of either the GPL or the LGPL, and not to allow others to
- * use your version of this file under the terms of the MPL, indicate your
- * decision by deleting the provisions above and replace them with the notice
- * and other provisions required by the GPL or the LGPL. If you do not delete
- * the provisions above, a recipient may use your version of this file under
- * the terms of any one of the MPL, the GPL or the LGPL.
- *
- * ***** END LICENSE BLOCK ***** */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #include "jsapi.h"
 #include "jscntxt.h"
@@ -118,7 +118,7 @@ bool
 Wrapper::getPropertyDescriptor(JSContext *cx, JSObject *wrapper, jsid id, bool set,
                                PropertyDescriptor *desc)
 {
-    desc->obj = NULL; // default result if we refuse to perform this action
+    desc->obj = NULL; 
     CHECKED(JS_GetPropertyDescriptorById(cx, wrappedObject(wrapper), id, JSRESOLVE_QUALIFIED, desc),
             set ? SET : GET);
 }
@@ -126,8 +126,8 @@ Wrapper::getPropertyDescriptor(JSContext *cx, JSObject *wrapper, jsid id, bool s
 static bool
 GetOwnPropertyDescriptor(JSContext *cx, JSObject *obj, jsid id, uintN flags, JSPropertyDescriptor *desc)
 {
-    // If obj is a proxy, we can do better than just guessing. This is
-    // important for certain types of wrappers that wrap other wrappers.
+    
+    
     if (obj->isProxy())
         return Proxy::getOwnPropertyDescriptor(cx, obj, id, flags & JSRESOLVE_ASSIGNING, desc);
 
@@ -142,7 +142,7 @@ bool
 Wrapper::getOwnPropertyDescriptor(JSContext *cx, JSObject *wrapper, jsid id, bool set,
                                   PropertyDescriptor *desc)
 {
-    desc->obj = NULL; // default result if we refuse to perform this action
+    desc->obj = NULL; 
     CHECKED(GetOwnPropertyDescriptor(cx, wrappedObject(wrapper), id, JSRESOLVE_QUALIFIED, desc),
             set ? SET : GET);
 }
@@ -157,7 +157,7 @@ Wrapper::defineProperty(JSContext *cx, JSObject *wrapper, jsid id, PropertyDescr
 bool
 Wrapper::getOwnPropertyNames(JSContext *cx, JSObject *wrapper, AutoIdVector &props)
 {
-    // if we refuse to perform this action, props remains empty
+    
     jsid id = JSID_VOID;
     GET(GetPropertyNames(cx, wrappedObject(wrapper), JSITER_OWNONLY | JSITER_HIDDEN, &props));
 }
@@ -172,7 +172,7 @@ ValueToBoolean(Value *vp, bool *bp)
 bool
 Wrapper::delete_(JSContext *cx, JSObject *wrapper, jsid id, bool *bp)
 {
-    *bp = true; // default result if we refuse to perform this action
+    *bp = true; 
     Value v;
     SET(JS_DeletePropertyById2(cx, wrappedObject(wrapper), id, &v) &&
         ValueToBoolean(&v, bp));
@@ -181,7 +181,7 @@ Wrapper::delete_(JSContext *cx, JSObject *wrapper, jsid id, bool *bp)
 bool
 Wrapper::enumerate(JSContext *cx, JSObject *wrapper, AutoIdVector &props)
 {
-    // if we refuse to perform this action, props remains empty
+    
     static jsid id = JSID_VOID;
     GET(GetPropertyNames(cx, wrappedObject(wrapper), 0, &props));
 }
@@ -203,7 +203,7 @@ Cond(JSBool b, bool *bp)
 bool
 Wrapper::has(JSContext *cx, JSObject *wrapper, jsid id, bool *bp)
 {
-    *bp = false; // default result if we refuse to perform this action
+    *bp = false; 
     JSBool found;
     GET(JS_HasPropertyById(cx, wrappedObject(wrapper), id, &found) &&
         Cond(found, bp));
@@ -212,7 +212,7 @@ Wrapper::has(JSContext *cx, JSObject *wrapper, jsid id, bool *bp)
 bool
 Wrapper::hasOwn(JSContext *cx, JSObject *wrapper, jsid id, bool *bp)
 {
-    *bp = false; // default result if we refuse to perform this action
+    *bp = false; 
     PropertyDescriptor desc;
     JSObject *wobj = wrappedObject(wrapper);
     GET(JS_GetPropertyDescriptorById(cx, wobj, id, JSRESOLVE_QUALIFIED, &desc) &&
@@ -222,7 +222,7 @@ Wrapper::hasOwn(JSContext *cx, JSObject *wrapper, jsid id, bool *bp)
 bool
 Wrapper::get(JSContext *cx, JSObject *wrapper, JSObject *receiver, jsid id, Value *vp)
 {
-    vp->setUndefined(); // default result if we refuse to perform this action
+    vp->setUndefined(); 
     GET(wrappedObject(wrapper)->getGeneric(cx, receiver, id, vp));
 }
 
@@ -230,14 +230,14 @@ bool
 Wrapper::set(JSContext *cx, JSObject *wrapper, JSObject *receiver, jsid id, bool strict,
                Value *vp)
 {
-    // FIXME (bug 596351): Need deal with strict mode.
+    
     SET(wrappedObject(wrapper)->setGeneric(cx, id, vp, false));
 }
 
 bool
 Wrapper::keys(JSContext *cx, JSObject *wrapper, AutoIdVector &props)
 {
-    // if we refuse to perform this action, props remains empty
+    
     const jsid id = JSID_VOID;
     GET(GetPropertyNames(cx, wrappedObject(wrapper), JSITER_OWNONLY, &props));
 }
@@ -245,7 +245,7 @@ Wrapper::keys(JSContext *cx, JSObject *wrapper, AutoIdVector &props)
 bool
 Wrapper::iterate(JSContext *cx, JSObject *wrapper, uintN flags, Value *vp)
 {
-    vp->setUndefined(); // default result if we refuse to perform this action
+    vp->setUndefined(); 
     const jsid id = JSID_VOID;
     GET(GetIterator(cx, wrappedObject(wrapper), flags, vp));
 }
@@ -253,7 +253,7 @@ Wrapper::iterate(JSContext *cx, JSObject *wrapper, uintN flags, Value *vp)
 bool
 Wrapper::call(JSContext *cx, JSObject *wrapper, uintN argc, Value *vp)
 {
-    vp->setUndefined(); // default result if we refuse to perform this action
+    vp->setUndefined(); 
     const jsid id = JSID_VOID;
     CHECKED(ProxyHandler::call(cx, wrapper, argc, vp), CALL);
 }
@@ -261,7 +261,7 @@ Wrapper::call(JSContext *cx, JSObject *wrapper, uintN argc, Value *vp)
 bool
 Wrapper::construct(JSContext *cx, JSObject *wrapper, uintN argc, Value *argv, Value *vp)
 {
-    vp->setUndefined(); // default result if we refuse to perform this action
+    vp->setUndefined(); 
     const jsid id = JSID_VOID;
     GET(ProxyHandler::construct(cx, wrapper, argc, argv, vp));
 }
@@ -276,7 +276,7 @@ Wrapper::nativeCall(JSContext *cx, JSObject *wrapper, Class *clasp, Native nativ
 bool
 Wrapper::hasInstance(JSContext *cx, JSObject *wrapper, const Value *vp, bool *bp)
 {
-    *bp = false; // default result if we refuse to perform this action
+    *bp = false; 
     const jsid id = JSID_VOID;
     JSBool b = JS_FALSE;
     GET(JS_HasInstance(cx, wrappedObject(wrapper), *vp, &b) && Cond(b, bp));
@@ -300,7 +300,7 @@ Wrapper::obj_toString(JSContext *cx, JSObject *wrapper)
     bool status;
     if (!enter(cx, wrapper, JSID_VOID, GET, &status)) {
         if (status) {
-            // Perform some default behavior that doesn't leak any information.
+            
             return JS_NewStringCopyZ(cx, "[object Object]");
         }
         return NULL;
@@ -316,7 +316,7 @@ Wrapper::fun_toString(JSContext *cx, JSObject *wrapper, uintN indent)
     bool status;
     if (!enter(cx, wrapper, JSID_VOID, GET, &status)) {
         if (status) {
-            // Perform some default behavior that doesn't leak any information.
+            
             if (wrapper->isCallable())
                 return JS_NewStringCopyZ(cx, "function () {\n    [native code]\n}");
             js::Value v = ObjectValue(*wrapper);
@@ -404,7 +404,7 @@ Wrapper::New(JSContext *cx, JSObject *obj, JSObject *proto, JSObject *parent, Wr
                           obj->isCallable() ? obj : NULL, NULL);
 }
 
-/* Compartments. */
+
 
 namespace js {
 
@@ -412,7 +412,7 @@ extern JSObject *
 TransparentObjectWrapper(JSContext *cx, JSObject *obj, JSObject *wrappedProto, JSObject *parent,
                          uintN flags)
 {
-    // Allow wrapping outer window proxies.
+    
     JS_ASSERT(!obj->isWrapper() || obj->getClass()->ext.innerObject);
     return Wrapper::New(cx, obj, wrappedProto, parent, &CrossCompartmentWrapper::singleton);
 }
@@ -510,7 +510,7 @@ ErrorCopier::~ErrorCopier()
     }
 }
 
-/* Cross compartment wrappers. */
+
 
 CrossCompartmentWrapper::CrossCompartmentWrapper(uintN flags)
   : Wrapper(CROSS_COMPARTMENT | flags)
@@ -639,10 +639,10 @@ CrossCompartmentWrapper::keys(JSContext *cx, JSObject *wrapper, AutoIdVector &pr
            call.origin->wrap(cx, props));
 }
 
-/*
- * We can reify non-escaping iterator objects instead of having to wrap them. This
- * allows fast iteration over objects across a compartment boundary.
- */
+
+
+
+
 static bool
 CanReify(Value *vp)
 {
@@ -656,7 +656,7 @@ struct AutoCloseIterator
 {
     AutoCloseIterator(JSContext *cx, JSObject *obj) : cx(cx), obj(obj) {}
 
-    ~AutoCloseIterator() { if (obj) js_CloseIterator(cx, obj); }
+    ~AutoCloseIterator() { if (obj) CloseIterator(cx, obj); }
 
     void clear() { obj = NULL; }
 
@@ -673,16 +673,16 @@ Reify(JSContext *cx, JSCompartment *origin, Value *vp)
 
     AutoCloseIterator close(cx, iterObj);
 
-    /* Wrap the iteratee. */
+    
     JSObject *obj = ni->obj;
     if (!origin->wrap(cx, &obj))
         return false;
 
-    /*
-     * Wrap the elements in the iterator's snapshot.
-     * N.B. the order of closing/creating iterators is important due to the
-     * implicit cx->enumerators state.
-     */
+    
+
+
+
+
     size_t length = ni->numKeys();
     bool isKeyIter = ni->isKeyIter();
     AutoIdVector keys(cx);
@@ -701,7 +701,7 @@ Reify(JSContext *cx, JSCompartment *origin, Value *vp)
     }
 
     close.clear();
-    if (!js_CloseIterator(cx, iterObj))
+    if (!CloseIterator(cx, iterObj))
         return false;
 
     if (isKeyIter)
@@ -877,7 +877,7 @@ CrossCompartmentWrapper::trace(JSTracer *trc, JSObject *wrapper)
 
 CrossCompartmentWrapper CrossCompartmentWrapper::singleton(0u);
 
-/* Security wrappers. */
+
 
 template <class Base>
 SecurityWrapper<Base>::SecurityWrapper(uintN flags)
@@ -889,10 +889,10 @@ bool
 SecurityWrapper<Base>::nativeCall(JSContext *cx, JSObject *wrapper, Class *clasp, Native native,
                                   CallArgs args)
 {
-    /*
-     * Let this through until compartment-per-global lets us have stronger
-     * invariants wrt document.domain (bug 714547).
-     */
+    
+
+
+
     return Base::nativeCall(cx, wrapper, clasp, native, args);
 }
 
@@ -900,10 +900,10 @@ template <class Base>
 bool
 SecurityWrapper<Base>::objectClassIs(JSObject *obj, ESClassValue classValue, JSContext *cx)
 {
-    /*
-     * Let this through until compartment-per-global lets us have stronger
-     * invariants wrt document.domain (bug 714547).
-     */
+    
+
+
+
     return Base::objectClassIs(obj, classValue, cx);
 }
 
