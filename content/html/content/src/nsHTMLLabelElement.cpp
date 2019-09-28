7972a0f844830cb826e06534581216153f1752d1
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-09-28 00:11:18 +0000

    Fix for bug 457897 (Remove QI on 'this' object when calling from JS to C++). Patch by jorendorff and me, r/sr=jst.
    UltraBlame original commit: 66aa5b4d2b31ca3214e37c940af4a24c42498465

--- content/html/content/src/nsHTMLLabelElement.cpp
+++ content/html/content/src/nsHTMLLabelElement.cpp
@@ -1,39 +1,39 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is Mozilla Communicator client code.
+ *
+ * The Initial Developer of the Original Code is
+ * Netscape Communications Corporation.
+ * Portions created by the Initial Developer are Copyright (C) 1998
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either of the GNU General Public License Version 2 or later (the "GPL"),
+ * or the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
 #include "nsCOMPtr.h"
 #include "nsIDOMHTMLLabelElement.h"
 #include "nsIDOMHTMLFormElement.h"
@@ -61,28 +61,28 @@ public:
   nsHTMLLabelElement(nsINodeInfo *aNodeInfo);
   virtual ~nsHTMLLabelElement();
 
-  
+  // nsISupports
   NS_DECL_ISUPPORTS_INHERITED
 
-  
+  // nsIDOMNode
   NS_FORWARD_NSIDOMNODE(nsGenericHTMLFormElement::)
 
-  
+  // nsIDOMElement
   NS_FORWARD_NSIDOMELEMENT(nsGenericHTMLFormElement::)
 
-  
+  // nsIDOMHTMLElement
   NS_FORWARD_NSIDOMHTMLELEMENT(nsGenericHTMLFormElement::)
 
-  
+  // nsIDOMHTMLLabelElement
   NS_DECL_NSIDOMHTMLLABELELEMENT
 
-  
+  // nsIFormControl
   NS_IMETHOD_(PRInt32) GetType() const { return NS_FORM_LABEL; }
   NS_IMETHOD Reset();
   NS_IMETHOD SubmitNamesValues(nsIFormSubmission* aFormSubmission,
                                nsIContent* aSubmitElement);
 
-  
+  // nsIContent
   virtual nsresult BindToTree(nsIDocument* aDocument, nsIContent* aParent,
                               nsIContent* aBindingParent,
                               PRBool aCompileEventHandlers);
@@ -110,12 +110,12 @@ protected:
   already_AddRefed<nsIContent> GetForContent();
   already_AddRefed<nsIContent> GetFirstFormControl(nsIContent *current);
 
-  
+  // XXX It would be nice if we could use an event flag instead.
   PRPackedBool mHandlingEvent;
   PRPackedBool mInSetFocus;
 };
 
-
+// construction, destruction
 
 
 NS_IMPL_NS_NEW_HTML_ELEMENT(Label)
@@ -132,21 +132,22 @@ nsHTMLLabelElement::~nsHTMLLabelElement()
 {
 }
 
-
+// nsISupports 
 
 
 NS_IMPL_ADDREF_INHERITED(nsHTMLLabelElement, nsGenericElement) 
 NS_IMPL_RELEASE_INHERITED(nsHTMLLabelElement, nsGenericElement) 
 
 
-
-NS_HTML_CONTENT_INTERFACE_TABLE_HEAD(nsHTMLLabelElement,
-                                     nsGenericHTMLFormElement)
-  NS_INTERFACE_TABLE_INHERITED1(nsHTMLLabelElement, nsIDOMHTMLLabelElement)
+// QueryInterface implementation for nsHTMLLabelElement
+NS_INTERFACE_TABLE_HEAD(nsHTMLLabelElement)
+  NS_HTML_CONTENT_INTERFACE_TABLE1(nsHTMLLabelElement, nsIDOMHTMLLabelElement)
+  NS_HTML_CONTENT_INTERFACE_TABLE_TO_MAP_SEGUE(nsHTMLLabelElement,
+                                               nsGenericHTMLFormElement)
 NS_HTML_CONTENT_INTERFACE_TABLE_TAIL_CLASSINFO(HTMLLabelElement)
 
 
-
+// nsIDOMHTMLLabelElement
 
 
 NS_IMPL_ELEMENT_CLONE(nsHTMLLabelElement)
@@ -226,40 +227,40 @@ nsHTMLLabelElement::PostHandleEvent(nsEventChainPostVisitor& aVisitor)
       case NS_MOUSE_CLICK:
         if (NS_IS_MOUSE_LEFT_CLICK(aVisitor.mEvent)) {
           if (ShouldFocus(this)) {
-            
+            // Focus the for content.
             aVisitor.mPresContext->EventStateManager()->
               ChangeFocusWith(content, nsIEventStateManager::eEventFocusedByKey);
           }
 
-          
-          
-          
-          
-          
-          
+          // Dispatch a new click event to |content|
+          //    (For compatibility with IE, we do only left click.  If
+          //    we wanted to interpret the HTML spec very narrowly, we
+          //    would do nothing.  If we wanted to do something
+          //    sensible, we might send more events through like
+          //    this.)  See bug 7554, bug 49897, and bug 96813.
           nsEventStatus status = aVisitor.mEventStatus;
-          
-          
+          // Ok to use aVisitor.mEvent as parameter because DispatchClickEvent
+          // will actually create a new event.
           DispatchClickEvent(aVisitor.mPresContext,
                              static_cast<nsInputEvent*>(aVisitor.mEvent),
                              content, PR_FALSE, &status);
-          
+          // Do we care about the status this returned?  I don't think we do...
         }
         break;
       case NS_FOCUS_CONTENT:
-        
-        
-        
-        
-        
-        
+        // Since we don't have '-moz-user-focus: normal', the only time
+        // the event type will be NS_FOCUS_CONTENT will be when the accesskey
+        // is activated.  We've already redirected the |SetFocus| call in that
+        // case.
+        // Since focus doesn't bubble, this is basically the second part
+        // of redirecting |SetFocus|.
         {
           nsEvent event(NS_IS_TRUSTED_EVENT(aVisitor.mEvent), NS_FOCUS_CONTENT);
           event.flags |= NS_EVENT_FLAG_CANT_BUBBLE;
           nsEventStatus status = aVisitor.mEventStatus;
           DispatchEvent(aVisitor.mPresContext, &event,
                         content, PR_TRUE, &status);
-          
+          // Do we care about the status this returned?  I don't think we do...
         }
         break;
     }
@@ -338,7 +339,7 @@ nsHTMLLabelElement::PerformAccesskey(PRBool aKeyCausesActivation,
     if (!presContext)
       return;
 
-    
+    // Click on it if the users prefs indicate to do so.
     nsMouseEvent event(aIsTrustedEvent, NS_MOUSE_CLICK,
                        nsnull, nsMouseEvent::eReal);
 
@@ -361,11 +362,11 @@ nsHTMLLabelElement::GetForContent()
 {
   nsresult rv;
 
-  
+  // Get the element that this label is for
   nsAutoString elementId;
   rv = GetHtmlFor(elementId);
   if (NS_SUCCEEDED(rv) && !elementId.IsEmpty()) {
-    
+    // We have a FOR attribute.
     nsCOMPtr<nsIDOMDocument> domDoc;
     GetOwnerDocument(getter_AddRefs(domDoc));
     if (domDoc) {
@@ -375,14 +376,14 @@ nsHTMLLabelElement::GetForContent()
       if (domElement) {
         CallQueryInterface(domElement, &result);
         if (result && !IsNonLabelFormControl(result)) {
-          NS_RELEASE(result); 
+          NS_RELEASE(result); // assigns null
         }
       }
       return result;
     }
   } else {
-    
-    
+    // No FOR attribute, we are a label for our first form control element.
+    // do a depth-first traversal to look for the first form control element
     return GetFirstFormControl(this);
   }
   return nsnull;
