c3427c2d01471818962e0b1259f35f85cf4f9c86
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-09-28 18:35:31 +0000

    [INFER] Fix merge botch, infinite loop.
    UltraBlame original commit: 37894495d77ddee74fdc66bd3fc2e30f55ab6741

--- js/src/xpconnect/src/xpcpublic.h
+++ js/src/xpconnect/src/xpcpublic.h
@@ -1,46 +1,47 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ * vim: set ts=8 sw=4 et tw=78:
+ *
+ * ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is Mozilla XPConnect  code, released
+ * June 30, 2009.
+ *
+ * The Initial Developer of the Original Code is
+ *    The Mozilla Foundation
+ *
+ * Contributor(s):
+ *    Andreas Gal <gal@mozilla.com>
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either of the GNU General Public License Version 2 or later (the "GPL"),
+ * or the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
 
 #ifndef xpcpublic_h
 #define xpcpublic_h
 
 #include "jsapi.h"
+#include "jsfriendapi.h"
 #include "jsobj.h"
 #include "jsgc.h"
 #include "jspubtd.h"
@@ -66,7 +67,7 @@ xpc_CreateMTGlobalObject(JSContext *cx, JSClass *clasp,
                          nsISupports *ptr, JSObject **global,
                          JSCompartment **compartment);
 
-
+// XXX where should this live?
 NS_EXPORT_(void)
 xpc_LocalizeContext(JSContext *cx);
 
@@ -87,21 +88,21 @@ DebugCheckWrapperClass(JSObject* obj)
     return JS_TRUE;
 }
 
+// If IS_WRAPPER_CLASS for the JSClass of an object is true, the object can be
+// a slim wrapper, holding a native in its private slot, or a wrappednative
+// wrapper, holding the XPCWrappedNative in its private slot. A slim wrapper
+// also holds a pointer to its XPCWrappedNativeProto in a reserved slot, we can
+// check that slot for a non-void value to distinguish between the two.
 
-
-
-
-
-
-
+// Only use these macros if IS_WRAPPER_CLASS(obj->getClass()) is true.
 #define IS_WN_WRAPPER_OBJECT(obj)                                             \
     (DebugCheckWrapperClass(obj) && obj->getSlot(0).isUndefined())
 #define IS_SLIM_WRAPPER_OBJECT(obj)                                           \
     (DebugCheckWrapperClass(obj) && !obj->getSlot(0).isUndefined())
 
-
-
-
+// Use these macros if IS_WRAPPER_CLASS(obj->getClass()) might be false.
+// Avoid calling them if IS_WRAPPER_CLASS(obj->getClass()) can only be
+// true, as we'd do a redundant call to IS_WRAPPER_CLASS.
 #define IS_WN_WRAPPER(obj)                                                    \
     (IS_WRAPPER_CLASS(obj->getClass()) && IS_WN_WRAPPER_OBJECT(obj))
 #define IS_SLIM_WRAPPER(obj)                                                  \
@@ -146,30 +147,30 @@ xpc_FastGetCachedWrapper(nsWrapperCache *cache, JSObject *scope)
     return xpc_FastGetCachedWrapper(cache, scope, &dummy);
 }
 
-
-
-
-
-
+// The JS GC marks objects gray that are held alive directly or
+// indirectly by an XPConnect root. The cycle collector explores only
+// this subset of the JS heap.  JSStaticAtoms cause this to crash,
+// because they are statically allocated in the data segment and thus
+// are not really GCThings.
 inline JSBool
 xpc_IsGrayGCThing(void *thing)
 {
     return js_GCThingIsMarked(thing, XPC_GC_COLOR_GRAY);
 }
 
-
-
-
-
+// The cycle collector only cares about JS objects and XML objects that
+// are held alive directly or indirectly by an XPConnect root.  This
+// version is preferred to xpc_IsGrayGCThing when it isn't known if thing
+// is a JSString or not. Implemented in nsXPConnect.cpp.
 extern JSBool
 xpc_GCThingIsGrayCCThing(void *thing);
 
-
+// Implemented in nsXPConnect.cpp.
 extern void
 xpc_UnmarkGrayObjectRecursive(JSObject* obj);
 
-
-
+// Remove the gray color from the given JSObject and any other objects that can
+// be reached through it.
 inline void
 xpc_UnmarkGrayObject(JSObject *obj)
 {
@@ -219,7 +220,7 @@ struct CompartmentStats
     PRInt64 tjitDataAllocatorsMain;
     PRInt64 tjitDataAllocatorsReserve;
 #endif
-    JSCompartment::TypeInferenceMemoryStats typeInferenceMemory;
+    TypeInferenceMemoryStats typeInferenceMemory;
 };
 
 struct IterateData
@@ -245,8 +246,8 @@ ReportJSStackSizeForRuntime(JSRuntime *rt, const nsACString &pathPrefix,
                             nsIMemoryMultiReporterCallback *callback,
                             nsISupports *closure);
 
-} 
-} 
-} 
+} // namespace memory
+} // namespace xpconnect
+} // namespace mozilla
 
 #endif
