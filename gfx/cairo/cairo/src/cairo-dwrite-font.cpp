623f0812c049caca9c3132a7142a89a33b0aef8d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 08:34:52 +0000

    bug 675383 - add a tiny delta to x-coordinates to avoid erratic spacing of DWrite font using GDI rendering mode when a transform (e.g. synthetic italics) is in effect. r=bas
    UltraBlame original commit: f1dbc0a637030a7ca266d7ea63f5c974ff1dc640

--- gfx/cairo/cairo/src/cairo-dwrite-font.cpp
+++ gfx/cairo/cairo/src/cairo-dwrite-font.cpp
@@ -371,10 +371,21 @@ _cairo_dwrite_glyph_run_from_glyphs(cairo_glyph_t *glyphs,
 	}
     } else {
 	*transformed = 1;
-
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        const double EPSILON = 0.0001;
 	for (int i = 0; i < num_glyphs; i++) {
 	    indices[i] = (WORD) glyphs[i].index;
-	    double x = glyphs[i].x;
+	    double x = glyphs[i].x + EPSILON;
 	    double y = glyphs[i].y;
 	    cairo_matrix_transform_point(&scaled_font->mat_inverse, &x, &y);
 	    
@@ -1353,10 +1364,11 @@ _cairo_dwrite_show_glyphs_on_surface(void			*surface,
 	run.fontEmSize = (FLOAT)scaled_font->font_matrix.yy;
     } else {
 	transform = TRUE;
-
+        
+        const double EPSILON = 0.0001;
 	for (int i = 0; i < num_glyphs; i++) {
 	    indices[i] = (WORD) glyphs[i].index;
-	    double x = glyphs[i].x - fontArea.left;
+	    double x = glyphs[i].x - fontArea.left + EPSILON;
 	    double y = glyphs[i].y - fontArea.top;
 	    cairo_matrix_transform_point(&dwritesf->mat_inverse, &x, &y);
 	    
