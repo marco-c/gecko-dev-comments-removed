5777ab4f8097089824bcf61f4c431524f460ba15
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 17:29:53 +0000

    bug 846690 - add a memory-pressure notification observer to the hyphenation manager, to discard dictionaries when we're low on memory. r=smontagu
    UltraBlame original commit: 5040d4861a8d36ba5d9da1b746e7e87136125fb5

--- intl/hyphenation/src/nsHyphenationManager.cpp
+++ intl/hyphenation/src/nsHyphenationManager.cpp
@@ -1,39 +1,39 @@
-/* ***** BEGIN LICENSE BLOCK *****
- * Version: MPL 1.1/GPL 2.0/LGPL 2.1
- *
- * The contents of this file are subject to the Mozilla Public License Version
- * 1.1 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- * http://www.mozilla.org/MPL/
- *
- * Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- * for the specific language governing rights and limitations under the
- * License.
- *
- * The Original Code is Mozilla Hyphenation Service.
- *
- * The Initial Developer of the Original Code is
- * Mozilla Foundation.
- * Portions created by the Initial Developer are Copyright (C) 2011
- * the Initial Developer. All Rights Reserved.
- *
- * Contributor(s):
- *   Jonathan Kew <jfkthame@gmail.com>
- *
- * Alternatively, the contents of this file may be used under the terms of
- * either the GNU General Public License Version 2 or later (the "GPL"), or
- * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
- * in which case the provisions of the GPL or the LGPL are applicable instead
- * of those above. If you wish to allow use of your version of this file only
- * under the terms of either the GPL or the LGPL, and not to allow others to
- * use your version of this file under the terms of the MPL, indicate your
- * decision by deleting the provisions above and replace them with the notice
- * and other provisions required by the GPL or the LGPL. If you do not delete
- * the provisions above, a recipient may use your version of this file under
- * the terms of any one of the MPL, the GPL or the LGPL.
- *
- * ***** END LICENSE BLOCK ***** */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #include "nsHyphenationManager.h"
 #include "nsHyphenator.h"
@@ -48,18 +48,48 @@
 #include "nsUnicharUtils.h"
 #include "mozilla/Preferences.h"
 #include "nsZipArchive.h"
+#include "mozilla/Services.h"
+#include "nsIObserverService.h"
+#include "nsCRT.h"
 
 using namespace mozilla;
 
-#define INTL_HYPHENATIONALIAS_PREFIX "intl.hyphenation-alias."
+static const char kIntlHyphenationAliasPrefix[] = "intl.hyphenation-alias.";
+static const char kMemoryPressureNotification[] = "memory-pressure";
 
 nsHyphenationManager *nsHyphenationManager::sInstance = nullptr;
+
+NS_IMPL_ISUPPORTS1(nsHyphenationManager::MemoryPressureObserver,
+                   nsIObserver)
+
+NS_IMETHODIMP
+nsHyphenationManager::MemoryPressureObserver::Observe(nsISupports *aSubject,
+                                                      const char *aTopic,
+                                                      const PRUnichar *aData)
+{
+  if (!nsCRT::strcmp(aTopic, kMemoryPressureNotification)) {
+    
+    
+    
+    
+    if (nsHyphenationManager::sInstance) {
+      nsHyphenationManager::sInstance->mHyphenators.Clear();
+    }
+  }
+  return NS_OK;
+}
 
 nsHyphenationManager*
 nsHyphenationManager::Instance()
 {
   if (sInstance == nullptr) {
     sInstance = new nsHyphenationManager();
+
+    nsCOMPtr<nsIObserverService> obs = mozilla::services::GetObserverService();
+    if (obs) {
+        obs->AddObserver(new MemoryPressureObserver,
+                         kMemoryPressureNotification, false);
+    }
   }
   return sInstance;
 }
@@ -106,9 +136,9 @@ nsHyphenationManager::GetHyphenator(nsIAtom *aLocale)
       }
     }
     if (!uri) {
-      // In the case of a locale such as "de-DE-1996", we try replacing
-      // successive trailing subtags with "-*" to find fallback patterns,
-      // so "de-DE-1996" -> "de-DE-*" (and then recursively -> "de-*")
+      
+      
+      
       nsAtomCString localeStr(aLocale);
       if (StringEndsWith(localeStr, NS_LITERAL_CSTRING("-*"))) {
         localeStr.Truncate(localeStr.Length() - 2);
@@ -210,8 +240,8 @@ nsHyphenationManager::LoadPatternListFromOmnijar(Omnijar::Type aType)
       continue;
     }
     ToLowerCase(locale);
-    locale.SetLength(locale.Length() - 4); // strip ".dic"
-    locale.Cut(0, locale.RFindChar('/') + 1); // strip directory
+    locale.SetLength(locale.Length() - 4); 
+    locale.Cut(0, locale.RFindChar('/') + 1); 
     if (StringBeginsWith(locale, NS_LITERAL_CSTRING("hyph_"))) {
       locale.Cut(0, 5);
     }
@@ -268,7 +298,7 @@ nsHyphenationManager::LoadPatternListFromDir(nsIFile *aDir)
     if (StringBeginsWith(locale, NS_LITERAL_CSTRING("hyph_"))) {
       locale.Cut(0, 5);
     }
-    locale.SetLength(locale.Length() - 4); // strip ".dic"
+    locale.SetLength(locale.Length() - 4); 
     for (uint32_t i = 0; i < locale.Length(); ++i) {
       if (locale[i] == '_') {
         locale.Replace(i, 1, '-');
@@ -296,14 +326,14 @@ nsHyphenationManager::LoadAliases()
   }
   uint32_t prefCount;
   char **prefNames;
-  nsresult rv = prefRootBranch->GetChildList(INTL_HYPHENATIONALIAS_PREFIX,
+  nsresult rv = prefRootBranch->GetChildList(kIntlHyphenationAliasPrefix,
                                              &prefCount, &prefNames);
   if (NS_SUCCEEDED(rv) && prefCount > 0) {
     for (uint32_t i = 0; i < prefCount; ++i) {
       nsAdoptingCString value = Preferences::GetCString(prefNames[i]);
       if (value) {
         nsAutoCString alias(prefNames[i]);
-        alias.Cut(0, strlen(INTL_HYPHENATIONALIAS_PREFIX));
+        alias.Cut(0, sizeof(kIntlHyphenationAliasPrefix) - 1);
         ToLowerCase(alias);
         ToLowerCase(value);
         nsCOMPtr<nsIAtom> aliasAtom = do_GetAtom(alias);
