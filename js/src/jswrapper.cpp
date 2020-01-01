a2a7da4c23a57cbda4182848dc67104325b31066
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 11:01:21 +0000

    Back out e4c82a6b298c, 036194408a50, 39acc9f51df8 (bug 714647 and followups) because of test_finalizer.js crashes in Linux64 PGO builds
    UltraBlame original commit: 906aa73122d98b423452bdca8d93d31a92a77525

--- js/src/jswrapper.cpp
+++ js/src/jswrapper.cpp
@@ -265,7 +265,7 @@ Wrapper::iterate(JSContext *cx, JSObject *wrapper, unsigned flags, Value *vp)
 {
     vp->setUndefined(); 
     const jsid id = JSID_VOID;
-    GET(GetIterator(cx, RootedVarObject(cx, wrappedObject(wrapper)), flags, vp));
+    GET(GetIterator(cx, wrappedObject(wrapper), flags, vp));
 }
 
 bool
@@ -366,7 +366,7 @@ Wrapper::defaultValue(JSContext *cx, JSObject *wrapper, JSType hint, Value *vp)
 bool
 Wrapper::iteratorNext(JSContext *cx, JSObject *wrapper, Value *vp)
 {
-    if (!js_IteratorMore(cx, RootedVarObject(cx, wrappedObject(wrapper)), vp))
+    if (!js_IteratorMore(cx, wrappedObject(wrapper), vp))
         return false;
 
     if (vp->toBoolean()) {
@@ -692,8 +692,8 @@ Reify(JSContext *cx, JSCompartment *origin, Value *vp)
     AutoCloseIterator close(cx, iterObj);
 
     
-    RootedVarObject obj(cx, ni->obj);
-    if (!origin->wrap(cx, obj.address()))
+    JSObject *obj = ni->obj;
+    if (!origin->wrap(cx, &obj))
         return false;
 
     
