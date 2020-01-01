8f9b71a5235cc548edda1b1759793f7155479932
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 05:28:13 +0000

    Bug 558861 - Compartmental GC (r=gal)
    UltraBlame original commit: 1c913526c597640d9da597d180b0fe1e2fe2d2ad

--- js/src/jsxml.h
+++ js/src/jsxml.h
@@ -1,46 +1,47 @@
-/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 4 -*-
- *
- * ***** BEGIN LICENSE BLOCK *****
- * Version: MPL 1.1/GPL 2.0/LGPL 2.1
- *
- * The contents of this file are subject to the Mozilla Public License Version
- * 1.1 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- * http://www.mozilla.org/MPL/
- *
- * Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- * for the specific language governing rights and limitations under the
- * License.
- *
- * The Original Code is SpiderMonkey E4X code, released August, 2004.
- *
- * The Initial Developer of the Original Code is
- * Netscape Communications Corporation.
- * Portions created by the Initial Developer are Copyright (C) 1998
- * the Initial Developer. All Rights Reserved.
- *
- * Contributor(s):
- *
- * Alternatively, the contents of this file may be used under the terms of
- * either of the GNU General Public License Version 2 or later (the "GPL"),
- * or the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
- * in which case the provisions of the GPL or the LGPL are applicable instead
- * of those above. If you wish to allow use of your version of this file only
- * under the terms of either the GPL or the LGPL, and not to allow others to
- * use your version of this file under the terms of the MPL, indicate your
- * decision by deleting the provisions above and replace them with the notice
- * and other provisions required by the GPL or the LGPL. If you do not delete
- * the provisions above, a recipient may use your version of this file under
- * the terms of any one of the MPL, the GPL or the LGPL.
- *
- * ***** END LICENSE BLOCK ***** */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #ifndef jsxml_h___
 #define jsxml_h___
 
 #include "jspubtd.h"
 #include "jsobj.h"
+#include "jscell.h"
 
 extern const char js_AnyName_str[];
 extern const char js_AttributeName_str[];
@@ -113,23 +114,17 @@ struct JSXMLArrayCursor
         return root = array->vector[index];
     }
 
-    void trace(JSTracer *trc) {
-#ifdef DEBUG
-        size_t index = 0;
-#endif
-        for (JSXMLArrayCursor *cursor = this; cursor; cursor = cursor->next)
-            js::MarkGCThing(trc, cursor->root, "cursor_root", index++);
-    }
+    void trace(JSTracer *trc);
 };
 
 #define JSXML_PRESET_CAPACITY   JS_BIT(31)
 #define JSXML_CAPACITY_MASK     JS_BITMASK(31)
 #define JSXML_CAPACITY(array)   ((array)->capacity & JSXML_CAPACITY_MASK)
 
-/*
- * NB: don't reorder this enum without changing all array initializers that
- * depend on it in jsxml.c.
- */
+
+
+
+
 typedef enum JSXMLClass {
     JSXML_CLASS_LIST,
     JSXML_CLASS_ELEMENT,
@@ -151,38 +146,18 @@ typedef enum JSXMLClass {
 #endif
 
 typedef struct JSXMLListVar {
-    JSXMLArray          kids;           /* NB: must come first */
+    JSXMLArray          kids;           
     JSXML               *target;
     JSObject            *targetprop;
 } JSXMLListVar;
 
 typedef struct JSXMLElemVar {
-    JSXMLArray          kids;           /* NB: must come first */
+    JSXMLArray          kids;           
     JSXMLArray          namespaces;
     JSXMLArray          attrs;
 } JSXMLElemVar;
 
-struct JSXML {
-#ifdef DEBUG_notme
-    JSCList             links;
-    uint32              serial;
-#endif
-    JSObject            *object;
-    void                *domnode;       /* DOM node if mapped info item */
-    JSXML               *parent;
-    JSObject            *name;
-    uint32              xml_class;      /* discriminates u, below */
-    uint32              xml_flags;      /* flags, see below */
-    union {
-        JSXMLListVar    list;
-        JSXMLElemVar    elem;
-        JSString        *value;
-    } u;
-};
 
-JS_STATIC_ASSERT(sizeof(JSXML) % JS_GCTHING_ALIGN == 0);
-
-/* union member shorthands */
 #define xml_kids        u.list.kids
 #define xml_target      u.list.target
 #define xml_targetprop  u.list.targetprop
@@ -190,10 +165,7 @@ JS_STATIC_ASSERT(sizeof(JSXML) % JS_GCTHING_ALIGN == 0);
 #define xml_attrs       u.elem.attrs
 #define xml_value       u.value
 
-/* xml_flags values */
-#define XMLF_WHITESPACE_TEXT    0x1
 
-/* xml_class-testing macros */
 #define JSXML_HAS_KIDS(xml)     JSXML_CLASS_HAS_KIDS((xml)->xml_class)
 #define JSXML_HAS_VALUE(xml)    JSXML_CLASS_HAS_VALUE((xml)->xml_class)
 #define JSXML_HAS_NAME(xml)     JSXML_CLASS_HAS_NAME((xml)->xml_class)
@@ -201,14 +173,45 @@ JS_STATIC_ASSERT(sizeof(JSXML) % JS_GCTHING_ALIGN == 0);
                                  ? (xml)->xml_kids.length                     \
                                  : 0)
 
+struct JSXML : js::gc::Cell {
+#ifdef DEBUG_notme
+    JSCList             links;
+    uint32              serial;
+#endif
+    JSObject            *object;
+    void                *domnode;       
+    JSXML               *parent;
+    JSObject            *name;
+    uint32              xml_class;      
+    uint32              xml_flags;      
+    union {
+        JSXMLListVar    list;
+        JSXMLElemVar    elem;
+        JSString        *value;
+    } u;
+    
+    void finalize(JSContext *cx, unsigned thingKind) {
+        if (JSXML_HAS_KIDS(this)) {
+            xml_kids.finish(cx);
+            if (xml_class == JSXML_CLASS_ELEMENT) {
+                xml_namespaces.finish(cx);
+                xml_attrs.finish(cx);
+            }
+        }
+#ifdef DEBUG_notme
+        JS_REMOVE_LINK(&links);
+#endif
+    }
+};
+
+
+#define XMLF_WHITESPACE_TEXT    0x1
+
 extern JSXML *
 js_NewXML(JSContext *cx, JSXMLClass xml_class);
 
 extern void
 js_TraceXML(JSTracer *trc, JSXML *xml);
-
-extern void
-js_FinalizeXML(JSContext *cx, JSXML *xml);
 
 extern JSObject *
 js_NewXMLObject(JSContext *cx, JSXMLClass xml_class);
@@ -223,9 +226,9 @@ extern JS_FRIEND_DATA(js::Class) js_AttributeNameClass;
 extern JS_FRIEND_DATA(js::Class) js_AnyNameClass;
 extern js::Class                 js_XMLFilterClass;
 
-/*
- * Methods to test whether an object or a value is of type "xml" (per typeof).
- */
+
+
+
 inline bool
 JSObject::isXML() const
 {
@@ -285,10 +288,10 @@ js_InitXMLClasses(JSContext *cx, JSObject *obj);
 extern JSBool
 js_GetFunctionNamespace(JSContext *cx, js::Value *vp);
 
-/*
- * If obj is QName corresponding to function::name, set *funidp to name's id,
- * otherwise set *funidp to void.
- */
+
+
+
+
 JSBool
 js_IsFunctionQName(JSContext *cx, JSObject *obj, jsid *funidp);
 
@@ -298,11 +301,11 @@ js_GetDefaultXMLNamespace(JSContext *cx, jsval *vp);
 extern JSBool
 js_SetDefaultXMLNamespace(JSContext *cx, const js::Value &v);
 
-/*
- * Return true if v is a XML QName object, or if it converts to a string that
- * contains a valid XML qualified name (one containing no :), false otherwise.
- * NB: This function is an infallible predicate, it hides exceptions.
- */
+
+
+
+
+
 extern JSBool
 js_IsXMLName(JSContext *cx, jsval v);
 
@@ -329,9 +332,9 @@ js_ConstructXMLQNameObject(JSContext *cx, const js::Value & nsval,
 extern JSBool
 js_GetAnyName(JSContext *cx, jsid *idp);
 
-/*
- * Note: nameval must be either QName, AttributeName, or AnyName.
- */
+
+
+
 extern JSBool
 js_FindXMLProperty(JSContext *cx, const js::Value &nameval, JSObject **objp, jsid *idp);
 
@@ -366,7 +369,7 @@ js_MakeXMLCommentString(JSContext *cx, JSString *str);
 extern JSString *
 js_MakeXMLPIString(JSContext *cx, JSString *name, JSString *str);
 
-/* The caller must ensure that either v1 or v2 is an object. */
+
 extern JSBool
 js_TestXMLEquality(JSContext *cx, const js::Value &v1, const js::Value &v2,
                    JSBool *bp);
@@ -374,4 +377,4 @@ js_TestXMLEquality(JSContext *cx, const js::Value &v1, const js::Value &v2,
 extern JSBool
 js_ConcatenateXML(JSContext *cx, JSObject *obj1, JSObject *obj2, js::Value *vp);
 
-#endif /* jsxml_h___ */
+#endif 
