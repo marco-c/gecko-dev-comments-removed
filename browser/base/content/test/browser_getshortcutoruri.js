af5f69052c05a6594effc0cb6cd7aec39bfedb5b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 07:29:26 +0000

    Bug 359809: properly escape keyword searches when alternative character sets are specified, r=gavin
    UltraBlame original commit: f4c738892af6d400c361372909c1189c285e0929

--- browser/base/content/test/browser_getshortcutoruri.js
+++ browser/base/content/test/browser_getshortcutoruri.js
@@ -74,6 +74,14 @@ var testData = [
   
   [new bmKeywordData("bmget-escaping2", "http://bmget/?esc=%s&raw=%S&mozcharset=ISO-8859-1", null, "foé"),
    new keywordResult("http://bmget/?esc=fo%E9&raw=foé", null)],
+
+  
+  
+  [new bmKeywordData("bmget-escaping", "http://bmget/?esc=%s&raw=%S", null, "+/@"),
+   new keywordResult("http://bmget/?esc=%2B%2F%40&raw=+/@", null)],
+  
+  [new bmKeywordData("bmget-escaping2", "http://bmget/?esc=%s&raw=%S&mozcharset=ISO-8859-1", null, "+/@"),
+   new keywordResult("http://bmget/?esc=%2B%2F%40&raw=+/@", null)],
 ];
 
 function test() {
