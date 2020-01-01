e4702a5729c8f7fbb8502b5ad87ed106bc3b6cb3
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 13:55:05 +0000

    Bug 765834: Rework and unify keyPath handling. r=bent
    UltraBlame original commit: 968c0a2b5045366843c6915a4c373df3d088da89

--- dom/indexedDB/KeyPath.h
+++ dom/indexedDB/KeyPath.h
@@ -1,4 +1,4 @@
-
+﻿
 
 
 
@@ -9,13 +9,20 @@
 
 #include "mozilla/dom/indexedDB/IndexedDatabase.h"
 
+namespace IPC {
+
+template <class T> struct ParamTraits;
+
+} 
+
 BEGIN_INDEXEDDB_NAMESPACE
 
 class Key;
 
 class KeyPath
 {
-public:
+  template<class T> friend struct IPC::ParamTraits;
+
   enum KeyPathType {
     NONEXISTENT,
     STRING,
@@ -28,6 +35,7 @@ public:
   
   bool AppendStringWithValidation(JSContext* aCx, const nsAString& aString);
 
+public:
   explicit KeyPath(int aDummy)
   : mType(NONEXISTENT)
   {
@@ -91,6 +99,7 @@ public:
 
   bool IsAllowedForObjectStore(bool aAutoIncrement) const;
 
+private:
   KeyPathType mType;
 
   nsTArray<nsString> mStrings;
