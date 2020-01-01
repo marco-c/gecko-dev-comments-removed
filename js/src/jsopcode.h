7bc97026d94d5a8b6776e2cb672664772d20a613
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 05:56:09 +0000

    Fix warnings compiling with gcc 4.5.  rs=brendan
    UltraBlame original commit: 0c72e196383980b9f1ae15eeccab42d5708c0dd7

--- js/src/jsopcode.h
+++ js/src/jsopcode.h
@@ -61,7 +61,16 @@ typedef enum JSOp {
     op = val,
 #include "jsopcode.tbl"
 #undef OPDEF
-    JSOP_LIMIT
+    JSOP_LIMIT,
+
+    
+
+
+
+
+    JSOP_GETPROP2 = JSOP_LIMIT,
+    JSOP_GETELEM2 = JSOP_LIMIT + 1,
+    JSOP_FAKE_LIMIT = JSOP_GETELEM2
 } JSOp;
 
 
