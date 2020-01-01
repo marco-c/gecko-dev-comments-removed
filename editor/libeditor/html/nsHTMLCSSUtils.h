ba675a14113745706bfb820bfaad30e7c02adc9b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 13:54:07 +0000

    Merge m-c to Ionmonkey.
    UltraBlame original commit: 56ba58a77e8f11cff724a9ddb50839a0c5663e2c

--- editor/libeditor/html/nsHTMLCSSUtils.h
+++ editor/libeditor/html/nsHTMLCSSUtils.h
@@ -187,6 +187,14 @@ public:
 
 
 
+
+
+  bool IsCSSEquivalentToHTMLInlineStyleSet(nsIContent* aContent,
+                                           nsIAtom* aProperty,
+                                           const nsAString* aAttribute,
+                                           const nsAString& aValue,
+                                           PRUint8 aStyleType);
+
   nsresult    IsCSSEquivalentToHTMLInlineStyleSet(nsIDOMNode * aNode,
                                                   nsIAtom * aHTMLProperty,
                                                   const nsAString * aAttribute,
@@ -205,6 +213,14 @@ public:
 
 
 
+
+
+
+  PRInt32     SetCSSEquivalentToHTMLStyle(mozilla::dom::Element* aElement,
+                                          nsIAtom* aProperty,
+                                          const nsAString* aAttribute,
+                                          const nsAString* aValue,
+                                          bool aSuppressTransaction);
   nsresult    SetCSSEquivalentToHTMLStyle(nsIDOMNode * aNode,
                                           nsIAtom * aHTMLProperty,
                                           const nsAString * aAttribute,
@@ -259,6 +275,8 @@ public:
 
 
 
+  bool ElementsSameStyle(mozilla::dom::Element* aFirstNode,
+                         mozilla::dom::Element* aSecondNode);
   bool ElementsSameStyle(nsIDOMNode *aFirstNode, nsIDOMNode *aSecondNode);
 
   
