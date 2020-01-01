48689c1f8c793dfd62755ccb8ac69ef8a8cb3d7a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 09:23:40 +0000

    Merge MC -> JM
    UltraBlame original commit: 60f879bef90ef16c268b0230cf14ea20ae31c234

--- js/src/jsfriendapi.h
+++ js/src/jsfriendapi.h
@@ -210,6 +210,30 @@ GetCompartmentShapeTableSize(JSCompartment *c, JSUsableSizeFun usf);
 extern JS_FRIEND_API(bool)
 CheckUndeclaredVarAssignment(JSContext *cx, JSString *propname);
 
+struct WeakMapTracer;
+
+
+
+
+
+
+
+typedef void
+(* WeakMapTraceCallback)(WeakMapTracer *trc, JSObject *m,
+                         void *k, JSGCTraceKind kkind,
+                         void *v, JSGCTraceKind vkind);
+
+struct WeakMapTracer {
+    JSContext            *context;
+    WeakMapTraceCallback callback;
+
+    WeakMapTracer(JSContext *cx, WeakMapTraceCallback cb) 
+        : context(cx), callback(cb) {}
+};
+
+extern JS_FRIEND_API(void)
+TraceWeakMaps(WeakMapTracer *trc);
+
 
 
 
@@ -401,9 +425,6 @@ StringIsArrayIndex(JSLinearString *str, jsuint *indexp);
 #define JSITER_KEYVALUE   0x4   /* destructuring for-in wants [key, value] */
 #define JSITER_OWNONLY    0x8   /* iterate over obj's own properties only */
 #define JSITER_HIDDEN     0x10  /* also enumerate non-enumerable properties */
-
-
-#define JSFUN_TRCINFO     0x2000
 
 } 
 #endif
