02a9337a213abb95a0ddb536ce7eb25e270905ac
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 04:05:19 +0000

    Move 'source' property from IDBRequest to success/error events
    UltraBlame original commit: 1789a859699ea4e92b47074c74466d3b7afbbf2f

--- dom/indexedDB/IDBRequest.h
+++ dom/indexedDB/IDBRequest.h
@@ -1,42 +1,42 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=2 et sw=2 tw=80: */
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is Indexed Database.
+ *
+ * The Initial Developer of the Original Code is
+ * The Mozilla Foundation.
+ * Portions created by the Initial Developer are Copyright (C) 2010
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *   Shawn Wilsher <me@shawnwilsher.com>
+ *   Ben Turner <bent.mozilla@gmail.com>
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
 
 #ifndef mozilla_dom_indexeddb_idbrequest_h__
 #define mozilla_dom_indexeddb_idbrequest_h__
@@ -48,8 +48,6 @@
 
 #include "nsDOMEventTargetHelper.h"
 #include "nsCycleCollectionParticipant.h"
-
-class nsIIDBDatabaseError;
 
 BEGIN_INDEXEDDB_NAMESPACE
 
@@ -92,7 +90,7 @@ public:
       }
 
     private:
-      
+      // XXXbent Assuming infallible nsTArray here, make sure it lands!
       nsAutoTArray<IDBRequest*, 1> mLiveRequests;
   };
 
@@ -103,14 +101,20 @@ public:
   NS_DECL_CYCLE_COLLECTION_CLASS_INHERITED(IDBRequest,
                                            nsDOMEventTargetHelper)
 
+  already_AddRefed<nsISupports> GetGenerator()
+  {
+    nsCOMPtr<nsISupports> generator(mGenerator);
+    return generator.forget();
+  }
+
 private:
-  
+  // Only called by IDBRequestGenerator::Generate().
   IDBRequest(Generator* aGenerator);
 
   nsRefPtr<Generator> mGenerator;
 
 protected:
-  
+  // Called by Release().
   ~IDBRequest();
 
   virtual void NoteDyingGenerator() {
@@ -124,4 +128,4 @@ protected:
 
 END_INDEXEDDB_NAMESPACE
 
-#endif 
+#endif // mozilla_dom_indexeddb_idbrequest_h__
