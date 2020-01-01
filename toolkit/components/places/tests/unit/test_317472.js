8a7b9964a49abba1b06573fc2e34f6337f91c71b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 15:16:45 +0000

    Bug 775495 - Replace waitForClearHistory and waitForAsyncUpdates with versions that return promises, and remove waitForFrecency. r=mak
    UltraBlame original commit: d785c6a18a3eef270b234dda56642d54ae6453ac

--- toolkit/components/places/tests/unit/test_317472.js
+++ toolkit/components/places/tests/unit/test_317472.js
@@ -1,13 +1,13 @@
-
-
-
-
-
+/* -*- Mode: Java; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim:set ts=2 sw=2 sts=2 et: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 const charset = "UTF-8";
 const CHARSET_ANNO = "URIProperties/characterSet";
 
-
+// Get history service
 try {
   var histsvc = Cc["@mozilla.org/browser/nav-history-service;1"].
                 getService(Ci.nsINavHistoryService);
@@ -26,7 +26,7 @@ const TEST_BOOKMARKED_URI = uri("http://bar.com");
 function run_test() {
   var now = Date.now();
 
-  
+  // add pages to history
   histsvc.addVisit(TEST_URI, now, null,
                    Ci.nsINavHistoryService.TRANSITION_TYPED,
                    false, 0);
@@ -34,7 +34,7 @@ function run_test() {
                    Ci.nsINavHistoryService.TRANSITION_TYPED,
                    false, 0);
 
-  
+  // create bookmarks on TEST_BOOKMARKED_URI
   var bm1 = bmsvc.insertBookmark(bmsvc.unfiledBookmarksFolder,
                                  TEST_BOOKMARKED_URI, bmsvc.DEFAULT_INDEX,
                                  TEST_BOOKMARKED_URI.spec);
@@ -42,38 +42,38 @@ function run_test() {
                                  TEST_BOOKMARKED_URI, bmsvc.DEFAULT_INDEX,
                                  TEST_BOOKMARKED_URI.spec);
 
-  
+  // set charset on not-bookmarked page
   histsvc.setCharsetForURI(TEST_URI, charset);
-  
+  // set charset on bookmarked page
   histsvc.setCharsetForURI(TEST_BOOKMARKED_URI, charset);
 
-  
+  // check that we have created a page annotation
   do_check_eq(annosvc.getPageAnnotation(TEST_URI, CHARSET_ANNO), charset);
 
-  
+  // get charset from not-bookmarked page
   do_check_eq(histsvc.getCharsetForURI(TEST_URI), charset);
-  
+  // get charset from bookmarked page
   do_check_eq(histsvc.getCharsetForURI(TEST_BOOKMARKED_URI), charset);
 
-  waitForClearHistory(continue_test);
+  promiseClearHistory().then(continue_test);
 
   do_test_pending();
 }
 
 function continue_test() {
-  
+  // ensure that charset has gone for not-bookmarked page
   do_check_neq(histsvc.getCharsetForURI(TEST_URI), charset);
 
-  
+  // check that page annotation has been removed
   try {
     annosvc.getPageAnnotation(TEST_URI, CHARSET_ANNO);
     do_throw("Charset page annotation has not been removed correctly");
   } catch (e) {}
 
-  
+  // ensure that charset still exists for bookmarked page
   do_check_eq(histsvc.getCharsetForURI(TEST_BOOKMARKED_URI), charset);
 
-  
+  // remove charset from bookmark and check that has gone
   histsvc.setCharsetForURI(TEST_BOOKMARKED_URI, "");
   do_check_neq(histsvc.getCharsetForURI(TEST_BOOKMARKED_URI), charset);
 
--- toolkit/components/places/tests/unit/test_384370.js
+++ toolkit/components/places/tests/unit/test_384370.js
@@ -1,8 +1,8 @@
-
-
-
-
-
+/* -*- Mode: Java; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim:set ts=2 sw=2 sts=2 et: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 const LOAD_IN_SIDEBAR_ANNO = "bookmarkProperties/loadInSidebar";
 const DESCRIPTION_ANNO = "bookmarkProperties/description";
@@ -10,42 +10,42 @@ const POST_DATA_ANNO = "bookmarkProperties/POSTData";
 
 do_check_eq(typeof PlacesUtils, "object");
 
-
+// main
 function run_test() {
   do_test_pending();
 
-  
+  /*
+    HTML+FEATURES SUMMARY:
+    - import legacy bookmarks
+    - export as json, import, test (tests integrity of html > json)
+    - export as html, import, test (tests integrity of json > html)
 
+    BACKUP/RESTORE SUMMARY:
+    - create a bookmark in each root
+    - tag multiple URIs with multiple tags
+    - export as json, import, test
+  */
 
-
-
-
-
-
-
-
-
-
-  
+  // import the importer
   Cu.import("resource://gre/modules/BookmarkHTMLUtils.jsm");
 
-  
-  
+  // file pointer to legacy bookmarks file
+  //var bookmarksFileOld = do_get_file("bookmarks.large.html");
   var bookmarksFileOld = do_get_file("bookmarks.preplaces.html");
-  
+  // file pointer to a new places-exported json file
   var jsonFile = Services.dirsvc.get("ProfD", Ci.nsILocalFile);
   jsonFile.append("bookmarks.exported.json");
 
-  
+  // create bookmarks.exported.json
   if (jsonFile.exists())
     jsonFile.remove(false);
   jsonFile.create(Ci.nsILocalFile.NORMAL_FILE_TYPE, 0600);
   if (!jsonFile.exists())
     do_throw("couldn't create file: bookmarks.exported.json");
 
-  
-  
-  
+  // Test importing a pre-Places canonical bookmarks file.
+  // 1. import bookmarks.preplaces.html
+  // Note: we do not empty the db before this import to catch bugs like 380999
   try {
     BookmarkHTMLUtils.importFromFile(bookmarksFileOld, true, after_import);
   } catch(ex) { do_throw("couldn't import legacy bookmarks file: " + ex); }
@@ -57,29 +57,29 @@ function run_test() {
 
     populate();
 
-    
+    // 2. run the test-suite
     validate();
   
-    waitForAsyncUpdates(function testJsonExport() {
-      
-      
+    promiseAsyncUpdates().then(function testJsonExport() {
+      // Test exporting a Places canonical json file.
+      // 1. export to bookmarks.exported.json
       try {
         PlacesUtils.backups.saveBookmarksToJSONFile(jsonFile);
       } catch(ex) { do_throw("couldn't export to file: " + ex); }
       LOG("exported json");
 
-      
-      
+      // 2. empty bookmarks db
+      // 3. import bookmarks.exported.json
       try {
         PlacesUtils.restoreBookmarksFromJSONFile(jsonFile);
       } catch(ex) { do_throw("couldn't import the exported file: " + ex); }
       LOG("imported json");
 
-      
+      // 4. run the test-suite
       validate();
       LOG("validated import");
   
-      waitForAsyncUpdates(do_test_finished);
+      promiseAsyncUpdates().then(do_test_finished);
     });
   }
 }
@@ -94,22 +94,22 @@ var bookmarkData = [
   { uri: uri("http://en.wikipedia.org/wiki/Diplodocus"), title: "dinosaur, dj, rad word" }
 ];
 
-
-
-
-
+/*
+populate data in each folder
+(menu is populated via the html import)
+*/
 function populate() {
-  
+  // add tags
   for each(let {uri: u, tags: t} in tagData)
     PlacesUtils.tagging.tagURI(u, t);
   
-  
+  // add unfiled bookmarks
   for each(let {uri: u, title: t} in bookmarkData) {
     PlacesUtils.bookmarks.insertBookmark(PlacesUtils.bookmarks.unfiledBookmarksFolder,
                                          u, PlacesUtils.bookmarks.DEFAULT_INDEX, t);
   }
 
-  
+  // add to the toolbar
   for each(let {uri: u, title: t} in bookmarkData) {
     PlacesUtils.bookmarks.insertBookmark(PlacesUtils.bookmarks.toolbarFolder,
                                          u, PlacesUtils.bookmarks.DEFAULT_INDEX, t);
@@ -123,85 +123,85 @@ function validate() {
   testTags();
 }
 
-
-
+// Tests a bookmarks datastore that has a set of bookmarks, etc
+// that flex each supported field and feature.
 function testCanonicalBookmarks() {
-  
+  // query to see if the deleted folder and items have been imported
   var query = PlacesUtils.history.getNewQuery();
   query.setFolders([PlacesUtils.bookmarks.bookmarksMenuFolder], 1);
   var result = PlacesUtils.history.executeQuery(query, PlacesUtils.history.getNewQueryOptions());
   var rootNode = result.root;
   rootNode.containerOpen = true;
 
-  
+  // Count expected bookmarks in the menu root.
   do_check_eq(rootNode.childCount, 3);
 
-  
+  // check separator
   var testSeparator = rootNode.getChild(1);
   do_check_eq(testSeparator.type, testSeparator.RESULT_TYPE_SEPARATOR);
 
-  
+  // get test folder
   var testFolder = rootNode.getChild(2);
   do_check_eq(testFolder.type, testFolder.RESULT_TYPE_FOLDER);
   do_check_eq(testFolder.title, "test");
 
-  
-
-
-
-
-
+  /*
+  // add date 
+  do_check_eq(PlacesUtils.bookmarks.getItemDateAdded(testFolder.itemId)/1000000, 1177541020);
+  // last modified
+  do_check_eq(PlacesUtils.bookmarks.getItemLastModified(testFolder.itemId)/1000000, 1177541050);
+  */
 
   testFolder = testFolder.QueryInterface(Ci.nsINavHistoryQueryResultNode);
   do_check_eq(testFolder.hasChildren, true);
-  
+  // folder description
   do_check_true(PlacesUtils.annotations.itemHasAnnotation(testFolder.itemId,
                                                           DESCRIPTION_ANNO));
   do_check_eq("folder test comment",
               PlacesUtils.annotations.getItemAnnotation(testFolder.itemId, DESCRIPTION_ANNO));
-  
+  // open test folder, and test the children
   testFolder.containerOpen = true;
   var cc = testFolder.childCount;
-  
-  
+  // XXX Bug 380468
+  // do_check_eq(cc, 2);
   do_check_eq(cc, 1);
 
-  
+  // test bookmark 1
   var testBookmark1 = testFolder.getChild(0);
-  
+  // url
   do_check_eq("http://test/post", testBookmark1.uri);
-  
+  // title
   do_check_eq("test post keyword", testBookmark1.title);
-  
+  // keyword
   do_check_eq("test", PlacesUtils.bookmarks.getKeywordForBookmark(testBookmark1.itemId));
-  
+  // sidebar
   do_check_true(PlacesUtils.annotations.itemHasAnnotation(testBookmark1.itemId,
                                                           LOAD_IN_SIDEBAR_ANNO));
-  
+  /*
+  // add date 
+  do_check_eq(testBookmark1.dateAdded/1000000, 1177375336);
 
+  // last modified
+  do_check_eq(testBookmark1.lastModified/1000000, 1177375423);
+  */
 
-
-
-
-
-
-  
+  // post data
   do_check_true(PlacesUtils.annotations.itemHasAnnotation(testBookmark1.itemId, POST_DATA_ANNO));
   do_check_eq("hidden1%3Dbar&text1%3D%25s",
               PlacesUtils.annotations.getItemAnnotation(testBookmark1.itemId, POST_DATA_ANNO));
 
-  
+  // last charset
   var testURI = PlacesUtils._uri(testBookmark1.uri);
   do_check_eq("ISO-8859-1", PlacesUtils.history.getCharsetForURI(testURI));
 
-  
+  // description 
   do_check_true(PlacesUtils.annotations.itemHasAnnotation(testBookmark1.itemId,
                                                           DESCRIPTION_ANNO));
   do_check_eq("item description",
               PlacesUtils.annotations.getItemAnnotation(testBookmark1.itemId,
                                                         DESCRIPTION_ANNO));
 
-  
+  // clean up
   testFolder.containerOpen = false;
   rootNode.containerOpen = false;
 }
@@ -214,12 +214,12 @@ function testToolbarFolder() {
   var toolbar = result.root;
   toolbar.containerOpen = true;
 
-  
+  // child count (add 2 for pre-existing items)
   do_check_eq(toolbar.childCount, bookmarkData.length + 2);
   
-  
+  // livemark
   var livemark = toolbar.getChild(1);
-  
+  // title
   do_check_eq("Latest Headlines", livemark.title);
 
   PlacesUtils.livemarks.getLivemark(
@@ -233,7 +233,7 @@ function testToolbarFolder() {
     }
   );
 
-  
+  // test added bookmark data
   var child = toolbar.getChild(2);
   do_check_eq(child.uri, bookmarkData[0].uri.spec);
   do_check_eq(child.title, bookmarkData[0].title);
@@ -250,17 +250,17 @@ function testUnfiledBookmarks() {
   var result = PlacesUtils.history.executeQuery(query, PlacesUtils.history.getNewQueryOptions());
   var rootNode = result.root;
   rootNode.containerOpen = true;
-  
+  // child count (add 1 for pre-existing item)
   do_check_eq(rootNode.childCount, bookmarkData.length + 1);
   for (var i = 1; i < rootNode.childCount; i++) {
     var child = rootNode.getChild(i);
     dump(bookmarkData[i - 1].uri.spec + " == " + child.uri + "?\n");
     do_check_true(bookmarkData[i - 1].uri.equals(uri(child.uri)));
     do_check_eq(child.title, bookmarkData[i - 1].title);
-    
-
-
-
+    /* WTF
+    if (child.tags)
+      do_check_eq(child.tags, bookmarkData[i].title);
+    */
   }
   rootNode.containerOpen = false;
 }
