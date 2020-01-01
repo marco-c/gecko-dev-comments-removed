950ed2610162ae104354bd0fae81140b76197cca
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 15:02:02 +0000

    Bug 798491: Add an option to stick all chrome JSMs/JS components in the same compartment. r=mrbkap,philikon
    UltraBlame original commit: 1545e91c658ec9cd25d9750a43bbe7afd8057cf9

--- dom/base/ConsoleAPI.js
+++ dom/base/ConsoleAPI.js
@@ -1,29 +1,29 @@
-/* -*- Mode: js2; js2-basic-offset: 2; indent-tabs-mode: nil; -*- */
-/* vim: set ft=javascript ts=2 et sw=2 tw=80: */
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+
+
+
+
 
 let Cu = Components.utils;
 let Ci = Components.interfaces;
 let Cc = Components.classes;
 
-// The maximum allowed number of concurrent timers per page.
+
 const MAX_PAGE_TIMERS = 10000;
 
-// The regular expression used to parse %s/%d and other placeholders for
-// variables in strings that need to be interpolated.
+
+
 const ARGUMENT_PATTERN = /%\d*\.?\d*([osdif])\b/g;
 
-// The maximum stacktrace depth when populating the stacktrace array used for
-// console.trace().
+
+
 const DEFAULT_MAX_STACKTRACE_DEPTH = 200;
 
-// The console API methods are async and their action is executed later. This
-// delay tells how much later.
-const CALL_DELAY = 15; // milliseconds
 
-// This constant tells how many messages to process in a single timer execution.
+
+const CALL_DELAY = 15; 
+
+
 const MESSAGES_IN_INTERVAL = 1500;
 
 Cu.import("resource://gre/modules/XPCOMUtils.jsm");
@@ -31,10 +31,10 @@ Cu.import("resource://gre/modules/Services.jsm");
 Cu.import("resource://gre/modules/ConsoleAPIStorage.jsm");
 Cu.import("resource://gre/modules/PrivateBrowsingUtils.jsm");
 
-/**
- * The window.console API implementation. One instance is lazily created for
- * every inner window, when the window.console object is accessed.
- */
+
+
+
+
 function ConsoleAPI() {}
 ConsoleAPI.prototype = {
 
@@ -52,7 +52,7 @@ ConsoleAPI.prototype = {
   _windowDestroyed: false,
   _timer: null,
 
-  // nsIDOMGlobalPropertyInitializer
+  
   init: function CA_init(aWindow) {
     Services.obs.addObserver(this, "inner-window-destroyed", true);
 
@@ -69,7 +69,7 @@ ConsoleAPI.prototype = {
 
     let self = this;
     let chromeObject = {
-      // window.console API
+      
       log: function CA_log() {
         self.queueCall("log", arguments);
       },
@@ -88,7 +88,7 @@ ConsoleAPI.prototype = {
       trace: function CA_trace() {
         self.queueCall("trace", arguments);
       },
-      // Displays an interactive listing of all the properties of an object.
+      
       dir: function CA_dir() {
         self.queueCall("dir", arguments);
       },
@@ -108,8 +108,8 @@ ConsoleAPI.prototype = {
         self.queueCall("timeEnd", arguments);
       },
       profile: function CA_profile() {
-        // Send a notification picked up by the profiler if installed.
-        // This must happen right away otherwise we will miss samples
+        
+        
         let consoleEvent = {
           action: "profile",
           arguments: arguments
@@ -119,8 +119,8 @@ ConsoleAPI.prototype = {
                                      null);  
       },
       profileEnd: function CA_profileEnd() {
-        // Send a notification picked up by the profiler if installed.
-        // This must happen right away otherwise we will miss samples
+        
+        
         let consoleEvent = {
           action: "profileEnd",
           arguments: arguments
@@ -147,8 +147,8 @@ ConsoleAPI.prototype = {
       }
     };
 
-    // We need to return an actual content object here, instead of a wrapped
-    // chrome object. This allows things like console.log.bind() to work.
+    
+    
     let contentObj = Cu.createObjectIn(aWindow);
     function genPropDesc(fun) {
       return { enumerable: true, configurable: true, writable: true,
@@ -199,14 +199,14 @@ ConsoleAPI.prototype = {
     }
   },
 
-  /**
-   * Queue a call to a console method. See the CALL_DELAY constant.
-   *
-   * @param string aMethod
-   *        The console method the code has invoked.
-   * @param object aArguments
-   *        The arguments passed to the console method.
-   */
+  
+
+
+
+
+
+
+
   queueCall: function CA_queueCall(aMethod, aArguments)
   {
     let metaForCall = {
@@ -224,10 +224,10 @@ ConsoleAPI.prototype = {
     }
   },
 
-  /**
-   * Timer callback used to process each of the queued calls.
-   * @private
-   */
+  
+
+
+
   _timerCallback: function CA__timerCallback()
   {
     this._queuedCalls.splice(0, MESSAGES_IN_INTERVAL)
@@ -244,13 +244,13 @@ ConsoleAPI.prototype = {
     }
   },
 
-  /**
-   * Process a queued call to a console method.
-   *
-   * @private
-   * @param array aCall
-   *        Array that holds information about the queued call.
-   */
+  
+
+
+
+
+
+
   _processQueuedCall: function CA__processQueuedItem(aCall)
   {
     let [method, args, meta] = aCall;
@@ -289,26 +289,26 @@ ConsoleAPI.prototype = {
         notifyArguments = this.stopTimer(args[0], meta.timeStamp);
         break;
       default:
-        // unknown console API method!
+        
         return;
     }
 
     this.notifyObservers(method, notifyArguments, notifyMeta);
   },
 
-  /**
-   * Notify all observers of any console API call.
-   *
-   * @param string aLevel
-   *        The message level.
-   * @param mixed aArguments
-   *        The arguments given to the console API call.
-   * @param object aMeta
-   *        Object that holds metadata about the console API call:
-   *        - isPrivate - Whether the window is in private browsing mode.
-   *        - frame - the youngest content frame in the call stack.
-   *        - timeStamp - when the console API call occurred.
-   */
+  
+
+
+
+
+
+
+
+
+
+
+
+
   notifyObservers: function CA_notifyObservers(aLevel, aArguments, aMeta) {
     let consoleEvent = {
       ID: this._outerID,
@@ -323,7 +323,7 @@ ConsoleAPI.prototype = {
 
     consoleEvent.wrappedJSObject = consoleEvent;
 
-    // Store non-private messages for which the inner window was not destroyed.
+    
     if (!aMeta.isPrivate) {
       ConsoleAPIStorage.recordEvent(this._innerID, consoleEvent);
     }
@@ -332,16 +332,16 @@ ConsoleAPI.prototype = {
                                  this._outerID);
   },
 
-  /**
-   * Process the console API call arguments in order to perform printf-like
-   * string substitution.
-   *
-   * TODO: object substitution should take into account width and precision
-   * qualifiers (bug 685813).
-   *
-   * @param mixed aArguments
-   *        The arguments given to the console API call.
-   **/
+  
+
+
+
+
+
+
+
+
+
   processArguments: function CA_processArguments(aArguments) {
     if (aArguments.length < 2 || typeof aArguments[0] != "string") {
       return aArguments;
@@ -352,7 +352,7 @@ ConsoleAPI.prototype = {
     let splitter = "%" + format.length + Date.now() + "%";
     let objects = [];
 
-    // Format specification regular expression.
+    
     let processed = format.replace(ARGUMENT_PATTERN, function CA_PA_substitute(match, submatch) {
       switch (submatch) {
         case "o":
@@ -384,15 +384,15 @@ ConsoleAPI.prototype = {
     return result.concat(args);
   },
 
-  /**
-   * Build the stacktrace array for the console.trace() call.
-   *
-   * @param number [aMaxDepth=DEFAULT_MAX_STACKTRACE_DEPTH]
-   *        Optional maximum stacktrace depth.
-   * @return array
-   *         Each element is a stack frame that holds the following properties:
-   *         filename, lineNumber, functionName and language.
-   */
+  
+
+
+
+
+
+
+
+
   getStackTrace: function CA_getStackTrace(aMaxDepth) {
     if (!aMaxDepth) {
       aMaxDepth = DEFAULT_MAX_STACKTRACE_DEPTH;
@@ -418,34 +418,34 @@ ConsoleAPI.prototype = {
     return stack;
   },
 
-  /**
-   * Begin a new group for logging output together.
-   **/
+  
+
+
   beginGroup: function CA_beginGroup() {
     return Array.prototype.join.call(arguments[0], " ");
   },
 
-  /*
-   * A registry of started timers. Timer maps are key-value pairs of timer
-   * names to timer start times, for all timers defined in the page. Timer
-   * names are prepended with the inner window ID in order to avoid conflicts
-   * with Object.prototype functions.
-   */
+  
+
+
+
+
+
   timerRegistry: null,
 
-  /**
-   * Create a new timer by recording the current time under the specified name.
-   *
-   * @param string aName
-   *        The name of the timer.
-   * @param number [aTimestamp=Date.now()]
-   *        Optional timestamp that tells when the timer was originally started.
-   * @return object
-   *        The name property holds the timer name and the started property
-   *        holds the time the timer was started. In case of error, it returns
-   *        an object with the single property "error" that contains the key
-   *        for retrieving the localized error message.
-   **/
+  
+
+
+
+
+
+
+
+
+
+
+
+
   startTimer: function CA_startTimer(aName, aTimestamp) {
     if (!aName) {
         return;
@@ -460,17 +460,17 @@ ConsoleAPI.prototype = {
     return { name: aName, started: this.timerRegistry[key] };
   },
 
-  /**
-   * Stop the timer with the specified name and retrieve the elapsed time.
-   *
-   * @param string aName
-   *        The name of the timer.
-   * @param number [aTimestamp=Date.now()]
-   *        Optional timestamp that tells when the timer was originally stopped.
-   * @return object
-   *        The name property holds the timer name and the duration property
-   *        holds the number of milliseconds since the timer was started.
-   **/
+  
+
+
+
+
+
+
+
+
+
+
   stopTimer: function CA_stopTimer(aName, aTimestamp) {
     if (!aName) {
         return;
@@ -485,4 +485,4 @@ ConsoleAPI.prototype = {
   }
 };
 
-let NSGetFactory = XPCOMUtils.generateNSGetFactory([ConsoleAPI]);
+this.NSGetFactory = XPCOMUtils.generateNSGetFactory([ConsoleAPI]);
