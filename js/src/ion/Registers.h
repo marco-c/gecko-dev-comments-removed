75abad0c485e3c2f00865edae9a26ad8da31ea34
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 15:10:01 +0000

    Bug 792085. Fix js build on ppc, broken since ionmonkey merge. r=dvander
    Sprinkle some #ifdef JS_IOn/JS_METHODJIT around ion includes.
    Fix part of #746112, where pagesize was set to 64k on all ppcs,
    while it seems it's only on ppc64.
    
    UltraBlame original commit: 5d6ef2feaa936465af08b0e15344db5bb7659190

--- js/src/ion/Registers.h
+++ js/src/ion/Registers.h
@@ -1,9 +1,9 @@
-
-
-
-
-
-
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ * vim: set ts=4 sw=4 et tw=99:
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #ifndef jsion_cpu_registers_h__
 #define jsion_cpu_registers_h__
@@ -19,8 +19,8 @@
 #endif
 #include "FixedArityList.h"
 
-
-#ifndef JS_CPU_ARM
+// ARM defines the RegisterID within Architecture-arm.h
+#if !defined(JS_CPU_ARM) && defined(JS_METHODJIT)
 #include "assembler/assembler/MacroAssembler.h"
 #endif
 
@@ -85,7 +85,7 @@ struct FloatRegister {
     }
 };
 
-
+// Information needed to recover machine register state.
 class MachineState
 {
     FixedArityList<uintptr_t *, Registers::Total> regs_;
@@ -116,8 +116,8 @@ class MachineState
     }
 };
 
-} 
-} 
+} // namespace ion
+} // namespace js
 
-#endif 
+#endif // jsion_cpu_registers_h__
 
