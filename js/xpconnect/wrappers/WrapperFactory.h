f45307e33c4aaca5b16afb834613ff3393bf5d91
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 15:00:49 +0000

    Bug 803068 - Merge DirectWrapper and Wrapper. r=ejpbruel
    UltraBlame original commit: 44d874f51c28266628fd367311002929b8c8197a

--- js/xpconnect/wrappers/WrapperFactory.h
+++ js/xpconnect/wrappers/WrapperFactory.h
@@ -1,9 +1,9 @@
-/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
- * vim: set ts=4 sw=4 et tw=99 ft=cpp:
- *
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+
+
+
+
+
 
 #ifndef _xpc_WRAPPERFACTORY_H
 #define _xpc_WRAPPERFACTORY_H
@@ -20,13 +20,13 @@ class WrapperFactory {
            SCRIPT_ACCESS_ONLY_FLAG = IS_XRAY_WRAPPER_FLAG << 1,
            SOW_FLAG                = SCRIPT_ACCESS_ONLY_FLAG << 1,
 
-           // Prevent scripts from shadowing native properties.
-           // NB: Applies only to Xray wrappers.
-           // NB: This will prevent scriptable helpers from defining special
-           //     handlers for properties defined in IDL. Use with caution.
+           
+           
+           
+           
            SHADOWING_FORBIDDEN     = SOW_FLAG << 1 };
 
-    // Return true if any of any of the nested wrappers have the flag set.
+    
     static bool HasWrapperFlag(JSObject *wrapper, unsigned flag) {
         unsigned flags = 0;
         js::UnwrapObject(wrapper, true, &flags);
@@ -51,50 +51,50 @@ class WrapperFactory {
 
     static JSObject *DoubleWrap(JSContext *cx, JSObject *obj, unsigned flags);
 
-    // Prepare a given object for wrapping in a new compartment.
+    
     static JSObject *PrepareForWrapping(JSContext *cx,
                                         JSObject *scope,
                                         JSObject *obj,
                                         unsigned flags);
 
-    // Rewrap an object that is about to cross compartment boundaries.
+    
     static JSObject *Rewrap(JSContext *cx,
                             JSObject *obj,
                             JSObject *wrappedProto,
                             JSObject *parent,
                             unsigned flags);
 
-    // Wrap an object for same-compartment access.
+    
     static JSObject *WrapForSameCompartment(JSContext *cx,
                                             JSObject *obj);
 
-    // Return true if this is a location object.
+    
     static bool IsLocationObject(JSObject *obj);
 
-    // Wrap a location object.
+    
     static JSObject *WrapLocationObject(JSContext *cx, JSObject *obj);
 
-    // Wrap wrapped object into a waiver wrapper and then re-wrap it.
+    
     static bool WaiveXrayAndWrap(JSContext *cx, jsval *vp);
 
-    // Wrap a (same compartment) object in a SOW.
+    
     static JSObject *WrapSOWObject(JSContext *cx, JSObject *obj);
 
-    // Return true if this is a Components object.
+    
     static bool IsComponentsObject(JSObject *obj);
 
-    // Wrap a (same compartment) Components object.
+    
     static JSObject *WrapComponentsObject(JSContext *cx, JSObject *obj);
 
-    // Wrap a same-compartment object for Xray inspection.
+    
     static JSObject *WrapForSameCompartmentXray(JSContext *cx, JSObject *obj);
 
-    // Returns true if the wrapper is in not shadowing mode for the id.
+    
     static bool XrayWrapperNotShadowing(JSObject *wrapper, jsid id);
 };
 
-extern js::DirectWrapper XrayWaiver;
+extern js::Wrapper XrayWaiver;
 
 }
 
-#endif /* _xpc_WRAPPERFACTORY_H */
+#endif 
