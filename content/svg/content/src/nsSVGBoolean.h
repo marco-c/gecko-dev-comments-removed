32546b5910742660231d7ee7c5090c1f1411911d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 03:37:50 +0000

    Fix for bug 533637 (Speed up unwrapping a node in quickstubs that use nsINode (dromaeo)). Pair every DOMCI to a specific C++ class. r=jst.
    UltraBlame original commit: 8e6cbbc07c71d3b003d33e326601e168a42c6c47

--- content/svg/content/src/nsSVGBoolean.h
+++ content/svg/content/src/nsSVGBoolean.h
@@ -1,38 +1,38 @@
-/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
-/* ***** BEGIN LICENSE BLOCK *****
- * Version: MPL 1.1/GPL 2.0/LGPL 2.1
- *
- * The contents of this file are subject to the Mozilla Public License Version
- * 1.1 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- * http://www.mozilla.org/MPL/
- *
- * Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- * for the specific language governing rights and limitations under the
- * License.
- *
- * The Original Code is the Mozilla SVG project.
- *
- * The Initial Developer of the Original Code is Robert Longson.
- * Portions created by the Initial Developer are Copyright (C) 2007
- * the Initial Developer. All Rights Reserved.
- *
- * Contributor(s):
- *
- * Alternatively, the contents of this file may be used under the terms of
- * either of the GNU General Public License Version 2 or later (the "GPL"),
- * or the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
- * in which case the provisions of the GPL or the LGPL are applicable instead
- * of those above. If you wish to allow use of your version of this file only
- * under the terms of either the GPL or the LGPL, and not to allow others to
- * use your version of this file under the terms of the MPL, indicate your
- * decision by deleting the provisions above and replace them with the notice
- * and other provisions required by the GPL or the LGPL. If you do not delete
- * the provisions above, a recipient may use your version of this file under
- * the terms of any one of the MPL, the GPL or the LGPL.
- *
- * ***** END LICENSE BLOCK ***** */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #ifndef __NS_SVGBOOLEAN_H__
 #define __NS_SVGBOOLEAN_H__
@@ -67,17 +67,18 @@ public:
   nsresult ToDOMAnimatedBoolean(nsIDOMSVGAnimatedBoolean **aResult,
                                 nsSVGElement* aSVGElement);
 #ifdef MOZ_SMIL
-  // Returns a new nsISMILAttr object that the caller must delete
+  
   nsISMILAttr* ToSMILAttr(nsSVGElement* aSVGElement);
-#endif // MOZ_SMIL
+#endif 
 
 private:
 
   PRPackedBool mAnimVal;
   PRPackedBool mBaseVal;
   PRPackedBool mIsAnimated;
-  PRUint8 mAttrEnum; // element specified tracking for attribute
+  PRUint8 mAttrEnum; 
 
+public:
   struct DOMAnimatedBoolean : public nsIDOMSVGAnimatedBoolean
   {
     NS_DECL_CYCLE_COLLECTING_ISUPPORTS
@@ -86,7 +87,7 @@ private:
     DOMAnimatedBoolean(nsSVGBoolean* aVal, nsSVGElement *aSVGElement)
       : mVal(aVal), mSVGElement(aSVGElement) {}
 
-    nsSVGBoolean* mVal; // kept alive because it belongs to content
+    nsSVGBoolean* mVal; 
     nsRefPtr<nsSVGElement> mSVGElement;
 
     NS_IMETHOD GetBaseVal(PRBool* aResult)
@@ -94,8 +95,8 @@ private:
     NS_IMETHOD SetBaseVal(PRBool aValue)
       { mVal->SetBaseValue(aValue, mSVGElement); return NS_OK; }
 
-    // Script may have modified animation parameters or timeline -- DOM getters
-    // need to flush any resample requests to reflect these modifications.
+    
+    
     NS_IMETHOD GetAnimVal(PRBool* aResult)
     {
 #ifdef MOZ_SMIL
@@ -113,13 +114,13 @@ private:
     SMILBool(nsSVGBoolean* aVal, nsSVGElement* aSVGElement)
       : mVal(aVal), mSVGElement(aSVGElement) {}
     
-    // These will stay alive because a nsISMILAttr only lives as long
-    // as the Compositing step, and DOM elements don't get a chance to
-    // die during that.
+    
+    
+    
     nsSVGBoolean* mVal;
     nsSVGElement* mSVGElement;
     
-    // nsISMILAttr methods
+    
     virtual nsresult ValueFromString(const nsAString& aStr,
                                      const nsISMILAnimationElement* aSrcElement,
                                      nsSMILValue& aValue,
@@ -128,6 +129,6 @@ private:
     virtual void ClearAnimValue();
     virtual nsresult SetAnimValue(const nsSMILValue& aValue);
   };
-#endif // MOZ_SMIL
+#endif 
 };
-#endif //__NS_SVGBOOLEAN_H__
+#endif 
