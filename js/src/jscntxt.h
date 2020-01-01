f74ac25958a616f1acb820c705c0f0e2b48dc0a2
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 08:23:33 +0000

    Merge TM -> JM
    UltraBlame original commit: 17cbc8fed5784d1d4d7c0e5ef79ad5299f2f0f5b

--- js/src/jscntxt.h
+++ js/src/jscntxt.h
@@ -1075,6 +1075,11 @@ struct JSRuntime {
     JSObject           *gcWeakMapList;
 
     
+    void                *gcMarkStackObjs[js::OBJECT_MARK_STACK_SIZE];
+    void                *gcMarkStackXMLs[js::XML_MARK_STACK_SIZE];
+    void                *gcMarkStackLarges[js::LARGE_MARK_STACK_SIZE];
+
+    
 
 
 
--- js/src/jsstr.cpp
+++ js/src/jsstr.cpp
@@ -124,7 +124,7 @@ Untag(JSString *str)
     return (JSRope *)(size_t(str) & ~size_t(1));
 }
 
-JS_ALWAYS_INLINE void
+void
 JSLinearString::mark(JSTracer *)
 {
     JSLinearString *str = this;
@@ -132,7 +132,7 @@ JSLinearString::mark(JSTracer *)
         str = str->asDependent().base();
 }
 
-JS_ALWAYS_INLINE void
+void
 JSString::mark(JSTracer *trc)
 {
     if (isLinear()) {
@@ -188,12 +188,6 @@ JSString::mark(JSTracer *trc)
         parent = nextParent;
         goto finish_node;
     }
-}
-
-void
-js::gc::TypedMarker(JSTracer *trc, JSString *str)
-{
-    str->mark(trc);
 }
 
 static JS_ALWAYS_INLINE size_t
