6ac4a336964f2b8bfdcc4c7ca8236d3e1e9aca01
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 11:33:38 +0000

    Bug 716478 - update line numbers in JS reftests, necessary because of updating licence to MPL 2 with new, shorter boilerplate.
    UltraBlame original commit: b81020e8ca5c5c237ad3721f49b51f204ffff0a3

--- js/src/tests/js1_5/Regress/regress-167328.js
+++ js/src/tests/js1_5/Regress/regress-167328.js
@@ -1,9 +1,9 @@
-/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-//-----------------------------------------------------------------------------
+
+
+
+
+
 var BUGNUMBER = 167328;
 var summary = 'Normal error reporting code should fill Error object properties';
 var actual = '';
@@ -12,7 +12,7 @@ var expect = '';
 printBugNumber(BUGNUMBER);
 printStatus (summary);
 
-expect = 'TypeError:52';
+expect = 'TypeError:19';
 try
 {
   var obj = {toString: function() {return new Object();}};
