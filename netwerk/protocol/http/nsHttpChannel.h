1d51e10e4c8763cb53de26eec810495247466b4b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 12:13:35 +0000

    Bug 763342 -  Fix device-specific cache hit rate telemetry regression, r=michal
    UltraBlame original commit: 121f87144909f6086aa372d456f3e98414e40ab9

--- netwerk/protocol/http/nsHttpChannel.h
+++ netwerk/protocol/http/nsHttpChannel.h
@@ -375,12 +375,6 @@ protected:
     virtual void DoNotifyListenerCleanup();
 
 private: 
-    enum {
-        kCacheHit = 1,
-        kCacheHitViaReval = 2,
-        kCacheMissedViaReval = 3,
-        kCacheMissed = 4
-    };
     bool mDidReval;
 };
 
