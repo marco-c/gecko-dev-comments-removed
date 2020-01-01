d24f7b855e79be951a09006289c062a2d1741f14
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 00:21:46 +0000

    Bug 386640: ClearStyleDataAndReflow is fundamentally broken. r+sr=bz
    UltraBlame original commit: e200b1563539ae8347f5166a58b04f1cbb466387

--- layout/base/nsPresShell.cpp
+++ layout/base/nsPresShell.cpp
@@ -1794,11 +1794,10 @@ PresShell::SetPreferenceStyleRules(PRBool aForceReflow)
     }
 #ifdef DEBUG_attinasi
     printf( "Preference Style Rules set: error=%ld\n", (long)result);
-#endif    
+#endif
 
-    if (aForceReflow){
-      mPresContext->ClearStyleDataAndReflow();
-    }
+    
+    
 
     return result;
   }
