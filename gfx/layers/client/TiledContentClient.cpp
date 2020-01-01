9097b9ad8d17a64432a70eb64eaba0951987bd5a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 22:02:12 +0000

    Bug 747811 - Make the tiles backend safe across processes. r=nrc,BenWa
    The tiles backend passes raw pointers to transfer tiled buffers between the
    main thread and the compositor. This patch changes that to use shared
    memory and tile descriptors instead, as well as changing the memory management
    slightly to facilitate that.
    
    UltraBlame original commit: 426f477baba5c239c79ff84e59185848a7395484

--- gfx/layers/client/TiledContentClient.cpp
+++ gfx/layers/client/TiledContentClient.cpp
@@ -1,7 +1,7 @@
-/* -*- Mode: C++; tab-width: 20; indent-tabs-mode: nil; c-basic-offset: 2 -*-
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+
+
+
 
 #include "mozilla/layers/TiledContentClient.h"
 #include "mozilla/gfx/2D.h"
@@ -16,17 +16,17 @@ static void DrawDebugOverlay(gfxASurface* imgSurf, int x, int y)
 {
   gfxContext c(imgSurf);
 
-  // Draw border
+  
   c.NewPath();
   c.SetDeviceColor(gfxRGBA(0.0, 0.0, 0.0, 1.0));
   c.Rectangle(gfxRect(gfxPoint(0,0),imgSurf->GetSize()));
   c.Stroke();
 
-  // Build tile description
+  
   std::stringstream ss;
   ss << x << ", " << y;
 
-  // Draw text using cairo toy text API
+  
   cairo_t* cr = c.GetCairo();
   cairo_set_font_size(cr, 25);
   cairo_text_extents_t extents;
@@ -73,17 +73,16 @@ TiledContentClient::TiledContentClient(ClientTiledThebesLayer* aThebesLayer,
 void
 TiledContentClient::LockCopyAndWrite(TiledBufferType aType)
 {
-  // Create a heap copy owned and released by the compositor. This is needed
-  // since we're sending this over an async message and content needs to be
-  // be able to modify the tiled buffer in the next transaction.
-  // TODO: Remove me once Bug 747811 lands.
   BasicTiledLayerBuffer* buffer = aType == LOW_PRECISION_TILED_BUFFER
     ? &mLowPrecisionTiledBuffer
     : &mTiledBuffer;
 
-  BasicTiledLayerBuffer* heapCopy = new BasicTiledLayerBuffer(buffer->DeepCopy());
+  
+  
+  
   buffer->ReadLock();
-  mForwarder->PaintedTiledLayerBuffer(this, heapCopy);
+
+  mForwarder->PaintedTiledLayerBuffer(this, buffer->GetSurfaceDescriptorTiles());
   buffer->ClearPaintedRegion();
 }
 
@@ -113,6 +112,53 @@ BasicTiledLayerBuffer::GetContentType() const
 }
 
 
+BasicTileDescriptor
+BasicTiledLayerTile::GetTileDescriptor()
+{
+  return BasicTileDescriptor(GetSurface()->GetShmem());
+}
+
+ BasicTiledLayerTile
+BasicTiledLayerTile::OpenDescriptor(ISurfaceAllocator *aAllocator, const BasicTileDescriptor& aDesc)
+{
+  nsRefPtr<gfxReusableSurfaceWrapper> surface =
+    gfxReusableSurfaceWrapper::Open(aAllocator, aDesc.reusableSurface());
+  return BasicTiledLayerTile(
+    new DeprecatedTextureClientTile(nullptr, TextureInfo(BUFFER_TILED), surface));
+}
+
+SurfaceDescriptorTiles
+BasicTiledLayerBuffer::GetSurfaceDescriptorTiles()
+{
+  InfallibleTArray<TileDescriptor> tiles;
+
+  for (size_t i = 0; i < mRetainedTiles.Length(); i++) {
+    TileDescriptor tileDesc;
+    if (mRetainedTiles.SafeElementAt(i, GetPlaceholderTile()) == GetPlaceholderTile()) {
+      tileDesc = PlaceholderTileDescriptor();
+    } else {
+      tileDesc = mRetainedTiles[i].GetTileDescriptor();
+    }
+    tiles.AppendElement(tileDesc);
+  }
+  return SurfaceDescriptorTiles(mValidRegion, mPaintedRegion,
+                                tiles, mRetainedWidth, mRetainedHeight,
+                                mResolution);
+}
+
+ BasicTiledLayerBuffer
+BasicTiledLayerBuffer::OpenDescriptor(ISurfaceAllocator *aAllocator,
+                                      const SurfaceDescriptorTiles& aDescriptor)
+{
+  return BasicTiledLayerBuffer(aAllocator,
+                               aDescriptor.validRegion(),
+                               aDescriptor.paintedRegion(),
+                               aDescriptor.tiles(),
+                               aDescriptor.retainedWidth(),
+                               aDescriptor.retainedHeight(),
+                               aDescriptor.resolution());
+}
+
 void
 BasicTiledLayerBuffer::PaintThebes(const nsIntRegion& aNewValidRegion,
                                    const nsIntRegion& aPaintRegion,
@@ -126,22 +172,22 @@ BasicTiledLayerBuffer::PaintThebes(const nsIntRegion& aNewValidRegion,
   long start = PR_IntervalNow();
 #endif
 
-  // If this region is empty XMost() - 1 will give us a negative value.
+  
   NS_ASSERTION(!aPaintRegion.GetBounds().IsEmpty(), "Empty paint region\n");
 
   bool useSinglePaintBuffer = UseSinglePaintBuffer();
-  // XXX The single-tile case doesn't work at the moment, see bug 850396
-  /*
-  if (useSinglePaintBuffer) {
-    // Check if the paint only spans a single tile. If that's
-    // the case there's no point in using a single paint buffer.
-    nsIntRect paintBounds = aPaintRegion.GetBounds();
-    useSinglePaintBuffer = GetTileStart(paintBounds.x) !=
-                           GetTileStart(paintBounds.XMost() - 1) ||
-                           GetTileStart(paintBounds.y) !=
-                           GetTileStart(paintBounds.YMost() - 1);
-  }
-  */
+  
+  
+
+
+
+
+
+
+
+
+
+
 
   if (useSinglePaintBuffer) {
     const nsIntRect bounds = aPaintRegion.GetBounds();
@@ -211,7 +257,7 @@ BasicTiledLayerBuffer::ValidateTileInternal(BasicTiledLayerTile aTile,
   }
   aTile.mDeprecatedTextureClient->EnsureAllocated(gfx::IntSize(GetTileLength(), GetTileLength()), GetContentType());
   gfxASurface* writableSurface = aTile.mDeprecatedTextureClient->LockImageSurface();
-  // Bug 742100, this gfxContext really should live on the stack.
+  
   nsRefPtr<gfxContext> ctxt = new gfxContext(writableSurface);
 
   if (mSinglePaintBuffer) {
@@ -301,18 +347,18 @@ BasicTiledLayerBuffer::ComputeProgressiveUpdateRegion(const nsIntRegion& aInvali
 {
   aRegionToPaint = aInvalidRegion;
 
-  // If this is a low precision buffer, we force progressive updates. The
-  // assumption is that the contents is less important, so visual coherency
-  // is lower priority than speed.
+  
+  
+  
   bool drawingLowPrecision = IsLowPrecision();
 
-  // Find out if we have any non-stale content to update.
+  
   nsIntRegion staleRegion;
   staleRegion.And(aInvalidRegion, aOldValidRegion);
 
-  // Find out the current view transform to determine which tiles to draw
-  // first, and see if we should just abort this paint. Aborting is usually
-  // caused by there being an incoming, more relevant paint.
+  
+  
+  
   gfx::Rect viewport;
   float scaleX, scaleY;
   if (mManager->ProgressiveUpdateCallback(!staleRegion.Contains(aInvalidRegion),
@@ -323,15 +369,15 @@ BasicTiledLayerBuffer::ComputeProgressiveUpdateRegion(const nsIntRegion& aInvali
     return aIsRepeated;
   }
 
-  // Transform the screen coordinates into local layer coordinates.
+  
   nsIntRect roundedTransformedViewport =
     RoundedTransformViewportBounds(viewport, aPaintData->mScrollOffset, aPaintData->mResolution,
                                    scaleX, scaleY, aPaintData->mTransformScreenToLayer);
 
-  // Paint tiles that have stale content or that intersected with the screen
-  // at the time of issuing the draw command in a single transaction first.
-  // This is to avoid rendering glitches on animated page content, and when
-  // layers change size/shape.
+  
+  
+  
+  
   nsIntRect criticalViewportRect = roundedTransformedViewport.Intersect(aPaintData->mCompositionBounds);
   aRegionToPaint.And(aInvalidRegion, criticalViewportRect);
   aRegionToPaint.Or(aRegionToPaint, staleRegion);
@@ -340,19 +386,19 @@ BasicTiledLayerBuffer::ComputeProgressiveUpdateRegion(const nsIntRegion& aInvali
     aRegionToPaint = aInvalidRegion;
   }
 
-  // Prioritise tiles that are currently visible on the screen.
+  
   bool paintVisible = false;
   if (aRegionToPaint.Intersects(roundedTransformedViewport)) {
     aRegionToPaint.And(aRegionToPaint, roundedTransformedViewport);
     paintVisible = true;
   }
 
-  // Paint area that's visible and overlaps previously valid content to avoid
-  // visible glitches in animated elements, such as gifs.
+  
+  
   bool paintInSingleTransaction = paintVisible && (drawingStale || aPaintData->mFirstPaint);
 
-  // The following code decides what order to draw tiles in, based on the
-  // current scroll direction of the primary scrollable layer.
+  
+  
   NS_ASSERTION(!aRegionToPaint.IsEmpty(), "Unexpectedly empty paint region!");
   nsIntRect paintBounds = aRegionToPaint.GetBounds();
 
@@ -374,13 +420,13 @@ BasicTiledLayerBuffer::ComputeProgressiveUpdateRegion(const nsIntRegion& aInvali
     incY = -tileLength;
   }
 
-  // Find a tile to draw.
+  
   nsIntRect tileBounds(startX, startY, tileLength, tileLength);
   int32_t scrollDiffX = aPaintData->mScrollOffset.x - aPaintData->mLastScrollOffset.x;
   int32_t scrollDiffY = aPaintData->mScrollOffset.y - aPaintData->mLastScrollOffset.y;
-  // This loop will always terminate, as there is at least one tile area
-  // along the first/last row/column intersecting with regionToPaint, or its
-  // bounds would have been smaller.
+  
+  
+  
   while (true) {
     aRegionToPaint.And(aInvalidRegion, tileBounds);
     if (!aRegionToPaint.IsEmpty()) {
@@ -394,14 +440,14 @@ BasicTiledLayerBuffer::ComputeProgressiveUpdateRegion(const nsIntRegion& aInvali
   }
 
   if (!aRegionToPaint.Contains(aInvalidRegion)) {
-    // The region needed to paint is larger then our progressive chunk size
-    // therefore update what we want to paint and ask for a new paint transaction.
+    
+    
 
-    // If we need to draw more than one tile to maintain coherency, make
-    // sure it happens in the same transaction by requesting this work be
-    // repeated immediately.
-    // If this is unnecessary, the remaining work will be done tile-by-tile in
-    // subsequent transactions.
+    
+    
+    
+    
+    
     if (!drawingLowPrecision && paintInSingleTransaction) {
       return true;
     }
@@ -410,9 +456,9 @@ BasicTiledLayerBuffer::ComputeProgressiveUpdateRegion(const nsIntRegion& aInvali
     return false;
   }
 
-  // We're not repeating painting and we've not requested a repeat transaction,
-  // so the paint is finished. If there's still a separate low precision
-  // paint to do, it will get marked as unfinished later.
+  
+  
+  
   aPaintData->mPaintFinished = true;
   return false;
 }
@@ -428,8 +474,8 @@ BasicTiledLayerBuffer::ProgressiveUpdate(nsIntRegion& aValidRegion,
   bool repeat = false;
   bool isBufferChanged = false;
   do {
-    // Compute the region that should be updated. Repeat as many times as
-    // is required.
+    
+    
     nsIntRegion regionToPaint;
     repeat = ComputeProgressiveUpdateRegion(aInvalidRegion,
                                             aOldValidRegion,
@@ -437,45 +483,30 @@ BasicTiledLayerBuffer::ProgressiveUpdate(nsIntRegion& aValidRegion,
                                             aPaintData,
                                             repeat);
 
-    // There's no further work to be done.
+    
     if (regionToPaint.IsEmpty()) {
       break;
     }
 
     isBufferChanged = true;
 
-    // Keep track of what we're about to refresh.
+    
     aValidRegion.Or(aValidRegion, regionToPaint);
 
-    // aValidRegion may have been altered by InvalidateRegion, but we still
-    // want to display stale content until it gets progressively updated.
-    // Create a region that includes stale content.
+    
+    
+    
     nsIntRegion validOrStale;
     validOrStale.Or(aValidRegion, aOldValidRegion);
 
-    // Paint the computed region and subtract it from the invalid region.
+    
     PaintThebes(validOrStale, regionToPaint, aCallback, aCallbackData);
     aInvalidRegion.Sub(aInvalidRegion, regionToPaint);
   } while (repeat);
 
-  // Return false if nothing has been drawn, or give what has been drawn
-  // to the shadow layer to upload.
+  
+  
   return isBufferChanged;
-}
-
-BasicTiledLayerBuffer
-BasicTiledLayerBuffer::DeepCopy() const
-{
-  BasicTiledLayerBuffer result = *this;
-
-  for (size_t i = 0; i < result.mRetainedTiles.Length(); i++) {
-    if (result.mRetainedTiles[i].IsPlaceholderTile()) continue;
-
-    result.mRetainedTiles[i].mDeprecatedTextureClient =
-      new DeprecatedTextureClientTile(*result.mRetainedTiles[i].mDeprecatedTextureClient);
-  }
-
-  return result;
 }
 
 }
--- gfx/layers/client/TiledContentClient.h
+++ gfx/layers/client/TiledContentClient.h
@@ -1,7 +1,7 @@
-/* -*- Mode: C++; tab-width: 20; indent-tabs-mode: nil; c-basic-offset: 2 -*-
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+
+
+
 
 #ifndef MOZILLA_GFX_TILEDCONTENTCLIENT_H
 #define MOZILLA_GFX_TILEDCONTENTCLIENT_H
@@ -9,28 +9,35 @@
 #include "mozilla/layers/ContentClient.h"
 #include "TiledLayerBuffer.h"
 #include "gfxPlatform.h"
+#include "mozilla/layers/ISurfaceAllocator.h"
 
 namespace mozilla {
 namespace layers {
 
-/**
- * Represent a single tile in tiled buffer. The buffer keeps tiles,
- * each tile keeps a reference to a texture client. The texture client
- * is backed by a gfxReusableSurfaceWrapper that implements a
- * copy-on-write mechanism while locked. The tile should be
- * locked before being sent to the compositor and unlocked
- * as soon as it is uploaded to prevent a copy.
- * Ideal place to store per tile debug information.
- */
+class BasicTileDescriptor;
+
+
+
+
+
+
+
+
+
+
 struct BasicTiledLayerTile {
   RefPtr<DeprecatedTextureClientTile> mDeprecatedTextureClient;
 #ifdef GFX_TILEDLAYER_DEBUG_OVERLAY
   TimeStamp        mLastUpdate;
 #endif
 
-  // Placeholder
+  
   BasicTiledLayerTile()
     : mDeprecatedTextureClient(nullptr)
+  {}
+
+  BasicTiledLayerTile(DeprecatedTextureClientTile* aTextureClient)
+    : mDeprecatedTextureClient(aTextureClient)
   {}
 
   BasicTiledLayerTile(const BasicTiledLayerTile& o) {
@@ -63,15 +70,18 @@ struct BasicTiledLayerTile {
     GetSurface()->ReadLock();
   }
 
+  BasicTileDescriptor GetTileDescriptor();
+  static BasicTiledLayerTile OpenDescriptor(ISurfaceAllocator *aAllocator, const BasicTileDescriptor& aDesc);
+
   gfxReusableSurfaceWrapper* GetSurface() {
     return mDeprecatedTextureClient->GetReusableSurfaceWrapper();
   }
 };
 
-/**
- * This struct stores all the data necessary to perform a paint so that it
- * doesn't need to be recalculated on every repeated transaction.
- */
+
+
+
+
 struct BasicTiledLayerPaintData {
   CSSPoint mScrollOffset;
   CSSPoint mLastScrollOffset;
@@ -87,12 +97,12 @@ struct BasicTiledLayerPaintData {
 class ClientTiledThebesLayer;
 class ClientLayerManager;
 
-/**
- * Provide an instance of TiledLayerBuffer backed by image surfaces.
- * This buffer provides an implementation to ValidateTile using a
- * thebes callback and can support painting using a single paint buffer
- * which is much faster then painting directly into the tiles.
- */
+
+
+
+
+
+
 class BasicTiledLayerBuffer
   : public TiledLayerBuffer<BasicTiledLayerBuffer, BasicTiledLayerTile>
 {
@@ -106,6 +116,30 @@ public:
     , mManager(nullptr)
     , mLastPaintOpaque(false)
   {}
+
+  BasicTiledLayerBuffer(ISurfaceAllocator* aAllocator,
+                        const nsIntRegion& aValidRegion,
+                        const nsIntRegion& aPaintedRegion,
+                        const InfallibleTArray<TileDescriptor>& aTiles,
+                        int aRetainedWidth,
+                        int aRetainedHeight,
+                        float aResolution)
+  {
+    mValidRegion = aValidRegion;
+    mPaintedRegion = aPaintedRegion;
+    mRetainedWidth = aRetainedWidth;
+    mRetainedHeight = aRetainedHeight;
+    mResolution = aResolution;
+
+    for(size_t i = 0; i < aTiles.Length(); i++) {
+      if (aTiles[i].type() == TileDescriptor::TPlaceholderTileDescriptor) {
+        mRetainedTiles.AppendElement(GetPlaceholderTile());
+      } else {
+        const BasicTileDescriptor& basicTileDesc = aTiles[i].get_BasicTileDescriptor();
+        mRetainedTiles.AppendElement(BasicTiledLayerTile::OpenDescriptor(aAllocator, basicTileDesc));
+      }
+    }
+  }
 
   void PaintThebes(const nsIntRegion& aNewValidRegion,
                    const nsIntRegion& aPaintRegion,
@@ -131,10 +165,10 @@ public:
 
   bool HasFormatChanged() const;
 
-  /**
-   * Performs a progressive update of a given tiled buffer.
-   * See ComputeProgressiveUpdateRegion below for parameter documentation.
-   */
+  
+
+
+
   bool ProgressiveUpdate(nsIntRegion& aValidRegion,
                          nsIntRegion& aInvalidRegion,
                          const nsIntRegion& aOldValidRegion,
@@ -142,27 +176,23 @@ public:
                          LayerManager::DrawThebesLayerCallback aCallback,
                          void* aCallbackData);
 
-  /**
-   * Copy this buffer duplicating the texture hosts under the tiles
-   * XXX This should go. It is a hack because we need to keep the
-   * surface wrappers alive whilst they are locked by the compositor.
-   * Once we properly implement the texture host/client architecture
-   * for tiled layers we shouldn't need this.
-   */
-  BasicTiledLayerBuffer DeepCopy() const;
+  SurfaceDescriptorTiles GetSurfaceDescriptorTiles();
+
+  static BasicTiledLayerBuffer OpenDescriptor(ISurfaceAllocator* aAllocator,
+                                              const SurfaceDescriptorTiles& aDescriptor);
 
 protected:
   BasicTiledLayerTile ValidateTile(BasicTiledLayerTile aTile,
                                    const nsIntPoint& aTileRect,
                                    const nsIntRegion& dirtyRect);
 
-  // If this returns true, we perform the paint operation into a single large
-  // buffer and copy it out to the tiles instead of calling PaintThebes() on
-  // each tile individually. Somewhat surprisingly, this turns out to be faster
-  // on Android.
+  
+  
+  
+  
   bool UseSinglePaintBuffer() { return true; }
 
-  void ReleaseTile(BasicTiledLayerTile aTile) { /* No-op. */ }
+  void ReleaseTile(BasicTiledLayerTile aTile) {  }
 
   void SwapTiles(BasicTiledLayerTile& aTileA, BasicTiledLayerTile& aTileB) {
     std::swap(aTileA, aTileB);
@@ -179,7 +209,7 @@ private:
   gfxSize mFrameResolution;
   bool mLastPaintOpaque;
 
-  // The buffer we use when UseSinglePaintBuffer() above is true.
+  
   nsRefPtr<gfxImageSurface>     mSinglePaintBuffer;
   nsIntPoint                    mSinglePaintBufferOffset;
 
@@ -187,23 +217,23 @@ private:
                                            const nsIntPoint& aTileOrigin,
                                            const nsIntRect& aDirtyRect);
 
-  /**
-   * Calculates the region to update in a single progressive update transaction.
-   * This employs some heuristics to update the most 'sensible' region to
-   * update at this point in time, and how large an update should be performed
-   * at once to maintain visual coherency.
-   *
-   * aInvalidRegion is the current invalid region.
-   * aOldValidRegion is the valid region of mTiledBuffer at the beginning of the
-   * current transaction.
-   * aRegionToPaint will be filled with the region to update. This may be empty,
-   * which indicates that there is no more work to do.
-   * aIsRepeated should be true if this function has already been called during
-   * this transaction.
-   *
-   * Returns true if it should be called again, false otherwise. In the case
-   * that aRegionToPaint is empty, this will return aIsRepeated for convenience.
-   */
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   bool ComputeProgressiveUpdateRegion(const nsIntRegion& aInvalidRegion,
                                       const nsIntRegion& aOldValidRegion,
                                       nsIntRegion& aRegionToPaint,
@@ -213,10 +243,10 @@ private:
 
 class TiledContentClient : public CompositableClient
 {
-  // XXX: for now the layer which owns us interacts directly with our buffers.
-  // We should have a content client for each tiled buffer which manages its
-  // own valid region, resolution, etc. Then we could have a much cleaner
-  // interface and tidy up BasicTiledThebesLayer::PaintThebes (bug 862547).
+  
+  
+  
+  
   friend class ClientTiledThebesLayer;
 
 public:
