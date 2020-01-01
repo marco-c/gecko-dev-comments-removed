242a9a18a8338c96e549e4f66f1d48c24d1eb408
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 00:27:29 +0000

    Bug 261655 - Can't completely disable the new "Install Missing Plugins" infobar. r=mconnor, a=mconnor
    UltraBlame original commit: 69269dc96f7afa681c3ef67d51ea61852200e41b

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -1,11 +1,11 @@
-# -*- Mode: Java; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+# -*- Mode: javascript; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
 # ***** BEGIN LICENSE BLOCK *****
 # Version: MPL 1.1/GPL 2.0/LGPL 2.1
 #
 # The contents of this file are subject to the Mozilla Public License Version
 # 1.1 (the "License"); you may not use this file except in compliance with
 # the License. You may obtain a copy of the License at
-# http:
+# http://www.mozilla.org/MPL/
 #
 # Software distributed under the License is distributed on an "AS IS" basis,
 # WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
@@ -5052,6 +5052,11 @@ missingPluginInstaller.prototype.newMissingPlugin = function(aEvent){
                                    gMissingPluginInstaller.installSinglePlugin,
                                    false);
   }
+
+  try {
+    if (gPrefService.getBoolPref("plugins.hide_infobar_for_missing_plugin"))
+      return;
+  } catch (ex) {} 
 
   var tabbrowser = getBrowser();
   const browsers = tabbrowser.mPanelContainer.childNodes;
