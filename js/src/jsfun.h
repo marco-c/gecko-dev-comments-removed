2219701f466007462619a324498934496072594e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 15:18:21 +0000

    Bug 791850 - Lazily clone self-hosted methods installed via js_DefineFunction. r=luke
    UltraBlame original commit: 862f9cd7eb0bebf5fcc4e7a6a550bfbe0b1740db

--- js/src/jsfun.h
+++ js/src/jsfun.h
@@ -1,14 +1,14 @@
-/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 4 -*-
- *
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+
+
+
+
 
 #ifndef jsfun_h___
 #define jsfun_h___
-/*
- * JS function definitions.
- */
+
+
+
 #include "jsprvtd.h"
 #include "jspubtd.h"
 #include "jsobj.h"
@@ -23,24 +23,24 @@ namespace js { class FunctionExtended; }
 struct JSFunction : public JSObject
 {
     enum Flags {
-        INTERPRETED      = 0x0001,  /* function has a JSScript and environment. */
-        NATIVE_CTOR      = 0x0002,  /* native that can be called as a constructor */
-        EXTENDED         = 0x0004,  /* structure is FunctionExtended */
-        HEAVYWEIGHT      = 0x0008,  /* activation requires a Call object */
-        IS_FUN_PROTO     = 0x0010,  /* function is Function.prototype for some global object */
-        EXPR_CLOSURE     = 0x0020,  /* expression closure: function(x) x*x */
-        HAS_GUESSED_ATOM = 0x0040,  /* function had no explicit name, but a
-                                       name was guessed for it anyway */
-        LAMBDA           = 0x0080,  /* function comes from a FunctionExpression or Function() call
-                                       (not a FunctionDeclaration or nonstandard function-statement) */
-        SELF_HOSTED      = 0x0100,  /* function is self-hosted builtin and must not be
-                                       decompilable nor constructible. */
-        SELF_HOSTED_CTOR = 0x0200,  /* function is self-hosted builtin constructor and
-                                       must be constructible but not decompilable. */
-        HAS_REST         = 0x0400,  /* function has a rest (...) parameter */
-        HAS_DEFAULTS     = 0x0800,  /* function has at least one default parameter */
+        INTERPRETED      = 0x0001,  
+        NATIVE_CTOR      = 0x0002,  
+        EXTENDED         = 0x0004,  
+        HEAVYWEIGHT      = 0x0008,  
+        IS_FUN_PROTO     = 0x0010,  
+        EXPR_CLOSURE     = 0x0020,  
+        HAS_GUESSED_ATOM = 0x0040,  
 
-        /* Derived Flags values for convenience: */
+        LAMBDA           = 0x0080,  
+
+        SELF_HOSTED      = 0x0100,  
+
+        SELF_HOSTED_CTOR = 0x0200,  
+
+        HAS_REST         = 0x0400,  
+        HAS_DEFAULTS     = 0x0800,  
+
+        
         NATIVE_FUN = 0,
         INTERPRETED_LAMBDA = INTERPRETED | LAMBDA
     };
@@ -51,39 +51,41 @@ struct JSFunction : public JSObject
                           "shadow interface must match actual interface");
     }
 
-    uint16_t        nargs;        /* maximum number of specified arguments,
-                                     reflected as f.length/f.arity */
-    uint16_t        flags;        /* bitfield composed of the above Flags enum */
+    uint16_t        nargs;        
+
+    uint16_t        flags;        
     union U {
         class Native {
             friend struct JSFunction;
-            js::Native          native;       /* native method pointer or null */
-            const JSJitInfo     *jitinfo;     /* Information about this function to be
-                                                 used by the JIT;
-                                                 use the accessor! */
+            js::Native          native;       
+            const JSJitInfo     *jitinfo;     
+
+
         } n;
         struct Scripted {
-            JSScript    *script_; /* interpreted bytecode descriptor or null;
-                                     use the accessor! */
-            JSObject    *env_;    /* environment for new activations;
-                                     use the accessor! */
+            JSScript    *script_; 
+
+            JSObject    *env_;    
+
         } i;
         void            *nativeOrScript;
     } u;
   private:
-    js::HeapPtrAtom  atom_;       /* name for diagnostics and decompiling */
+    js::HeapPtrAtom  atom_;       
   public:
 
-    /* A function can be classified as either native (C++) or interpreted (JS): */
+    
     bool isInterpreted()            const { return flags & INTERPRETED; }
     bool isNative()                 const { return !(flags & INTERPRETED); }
 
-    /* Possible attributes of a native function: */
+    
     bool isNativeConstructor()      const { return flags & NATIVE_CTOR; }
 
-    /* Possible attributes of an interpreted function: */
+    
     bool isHeavyweight()            const { return flags & HEAVYWEIGHT; }
     bool isFunctionPrototype()      const { return flags & IS_FUN_PROTO; }
+    bool isLazy()                   const { return isInterpreted() && !hasScript(); }
+    bool hasScript()                const { return isInterpreted() && u.i.script_; }
     bool isExprClosure()            const { return flags & EXPR_CLOSURE; }
     bool hasGuessedAtom()           const { return flags & HAS_GUESSED_ATOM; }
     bool isLambda()                 const { return flags & LAMBDA; }
@@ -92,7 +94,7 @@ struct JSFunction : public JSObject
     bool hasRest()                  const { return flags & HAS_REST; }
     bool hasDefaults()              const { return flags & HAS_DEFAULTS; }
 
-    /* Compound attributes: */
+    
     bool isBuiltin() const {
         return isNative() || isSelfHostedBuiltin();
     }
@@ -104,7 +106,7 @@ struct JSFunction : public JSObject
         return isLambda() && atom_ && !hasGuessedAtom();
     }
 
-    /* Returns the strictness of this function, which must be interpreted. */
+    
     inline bool inStrictMode() const;
 
     void setArgCount(uint16_t nargs) {
@@ -152,13 +154,13 @@ struct JSFunction : public JSObject
 
     inline void setGuessedAtom(JSAtom *atom);
 
-    /* uint16_t representation bounds number of call object dynamic slots. */
+    
     enum { MAX_ARGS_AND_VARS = 2 * ((1U << 16) - 1) };
 
-    /*
-     * For an interpreted function, accessors for the initial scope object of
-     * activations (stack frames) of the function.
-     */
+    
+
+
+
     inline JSObject *environment() const;
     inline void setEnvironment(JSObject *obj);
     inline void initEnvironment(JSObject *obj);
@@ -167,7 +169,7 @@ struct JSFunction : public JSObject
     static inline size_t offsetOfAtom() { return offsetof(JSFunction, atom_); }
 
     js::Return<JSScript*> script() const {
-        JS_ASSERT(isInterpreted());
+        JS_ASSERT(hasScript());
         return JS::HandleScript::fromMarkedLocation(&u.i.script_);
     }
 
@@ -212,7 +214,7 @@ struct JSFunction : public JSObject
 
     inline void trace(JSTracer *trc);
 
-    /* Bound function accessors. */
+    
 
     inline bool initBoundFunction(JSContext *cx, js::HandleValue thisArg,
                                   const js::Value *args, unsigned argslen);
@@ -233,20 +235,20 @@ struct JSFunction : public JSObject
     }
 
   public:
-    /* Accessors for data stored in extended functions. */
+    
     inline void initializeExtended();
     inline void setExtendedSlot(size_t which, const js::Value &val);
     inline const js::Value &getExtendedSlot(size_t which) const;
 
-    /* Constructs a new type for the function if necessary. */
+    
     static bool setTypeForScriptedFunction(JSContext *cx, js::HandleFunction fun,
                                            bool singleton = false);
 
   private:
-    /*
-     * These member functions are inherited from JSObject, but should never be applied to
-     * a value statically known to be a JSFunction.
-     */
+    
+
+
+
     inline JSFunction *toFunction() MOZ_DELETE;
     inline const JSFunction *toFunction() const MOZ_DELETE;
 };
@@ -288,25 +290,25 @@ js_CloneFunctionObject(JSContext *cx, js::HandleFunction fun,
 
 extern JSFunction *
 js_DefineFunction(JSContext *cx, js::HandleObject obj, js::HandleId id, JSNative native,
-                  unsigned nargs, unsigned flags, js::Handle<js::PropertyName*> selfHostedName = JS::NullPtr(),
+                  unsigned nargs, unsigned flags,
                   js::gc::AllocKind kind = JSFunction::FinalizeKind);
 
 namespace js {
 
-/*
- * Function extended with reserved slots for use by various kinds of functions.
- * Most functions do not have these extensions, but enough are that efficient
- * storage is required (no malloc'ed reserved slots).
- */
+
+
+
+
+
 class FunctionExtended : public JSFunction
 {
     friend struct JSFunction;
 
-    /* Reserved slots available for storage by particular native functions. */
+    
     HeapValue extendedSlots[2];
 };
 
-} // namespace js
+} 
 
 inline js::FunctionExtended *
 JSFunction::toExtended()
@@ -334,22 +336,25 @@ XDRInterpretedFunction(XDRState<mode> *xdr, HandleObject enclosingScope,
 extern JSObject *
 CloneInterpretedFunction(JSContext *cx, HandleObject enclosingScope, HandleFunction fun);
 
-/*
- * Report an error that call.thisv is not compatible with the specified class,
- * assuming that the method (clasp->name).prototype.<name of callee function>
- * is what was called.
- */
+bool
+InitializeLazyFunctionScript(JSContext *cx, HandleFunction fun);
+
+
+
+
+
+
 extern void
 ReportIncompatibleMethod(JSContext *cx, CallReceiver call, Class *clasp);
 
-/*
- * Report an error that call.thisv is not an acceptable this for the callee
- * function.
- */
+
+
+
+
 extern void
 ReportIncompatible(JSContext *cx, CallReceiver call);
 
-} /* namespace js */
+} 
 
 extern JSBool
 js_fun_apply(JSContext *cx, unsigned argc, js::Value *vp);
@@ -361,4 +366,4 @@ extern JSObject*
 js_fun_bind(JSContext *cx, js::HandleObject target, js::HandleValue thisArg,
             js::Value *boundArgs, unsigned argslen);
 
-#endif /* jsfun_h___ */
+#endif 
