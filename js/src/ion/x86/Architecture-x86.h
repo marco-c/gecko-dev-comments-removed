f6bc97ba76e4b605a29fe2d63a80ef2fbc1df978
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 18:38:05 +0000

    Back out 5bf9771e1a28 (bug 860029) for bustage CLOSED TREE
    UltraBlame original commit: 6d92a2e39c778d1177a5f89270bf5a8160b169b1

--- js/src/ion/x86/Architecture-x86.h
+++ js/src/ion/x86/Architecture-x86.h
@@ -1,8 +1,9 @@
-
-
-
-
-
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ * vim: set ts=4 sw=4 et tw=99:
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #ifndef jsion_architecture_x86_h__
 #define jsion_architecture_x86_h__
@@ -14,28 +15,28 @@ namespace ion {
 static const ptrdiff_t STACK_SLOT_SIZE       = 4;
 static const uint32_t DOUBLE_STACK_ALIGNMENT   = 2;
 
-
-
-
-
+// In bytes: slots needed for potential memory->memory move spills.
+//   +8 for cycles
+//   +4 for gpr spills
+//   +8 for double spills
 static const uint32_t ION_FRAME_SLACK_SIZE    = 20;
 
-
+// Only Win64 requires shadow stack space.
 static const uint32_t ShadowStackSpace = 0;
 
-
+// An offset that is illegal for a local variable's stack allocation.
 static const int32_t INVALID_STACK_SLOT       = -1;
 
-
-
+// These offsets are specific to nunboxing, and capture offsets into the
+// components of a js::Value.
 static const int32_t NUNBOX32_TYPE_OFFSET         = 4;
 static const int32_t NUNBOX32_PAYLOAD_OFFSET      = 0;
 
+////
+// These offsets are related to bailouts.
+////
 
-
-
-
-
+// Size of each bailout table entry. On x86 this is a 5-byte relative call.
 static const uint32_t BAILOUT_TABLE_ENTRY_SIZE    = 5;
 
 class Registers {
@@ -84,22 +85,22 @@ class Registers {
 
     static const uint32_t AllocatableMask = AllMask & ~NonAllocatableMask;
 
-    
+    // Registers that can be allocated without being saved, generally.
     static const uint32_t TempMask = VolatileMask & ~NonAllocatableMask;
 
-    
+    // Registers returned from a JS -> JS call.
     static const uint32_t JSCallMask =
         (1 << JSC::X86Registers::ecx) |
         (1 << JSC::X86Registers::edx);
 
-    
+    // Registers returned from a JS -> C call.
     static const uint32_t CallMask =
         (1 << JSC::X86Registers::eax);
 
     typedef JSC::MacroAssembler::RegisterID RegisterID;
 };
 
-
+// Smallest integer type that can hold a register bitmask.
 typedef uint8_t PackedRegisterMask;
 
 class FloatRegisters {
@@ -130,8 +131,8 @@ class FloatRegisters {
     static const uint32_t AllocatableMask = AllMask & ~NonAllocatableMask;
 };
 
-} 
-} 
+} // namespace ion
+} // namespace js
 
-#endif 
+#endif // jsion_architecture_x86_h__
 
