d269823e892751eb3bb5f0f1bbefb921a7f04071
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 03:50:23 +0000

    Back out fixes for bug 560462 to fix orange.
    UltraBlame original commit: d67ddb92a98c53fd5d41515030ce2ff799e62334

--- js/src/xpconnect/src/nsDOMQS.h
+++ js/src/xpconnect/src/nsDOMQS.h
@@ -1,45 +1,45 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is XPConnect code.
+ *
+ * The Initial Developer of the Original Code is
+ * Mozilla Foundation.
+ * Portions created by the Initial Developer are Copyright (C) 2010
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
 
 #ifndef nsDOMQS_h__
 #define nsDOMQS_h__
 
 #include "nsDOMClassInfoID.h"
 
-#define DEFINE_UNWRAP_CAST(_interface, _base, _bit)                           \
+#define DEFINE_UNWRAP_CAST(_interface, _bit)                                  \
 NS_SPECIALIZE_TEMPLATE                                                        \
 inline JSBool                                                                 \
 xpc_qsUnwrapThis<_interface>(JSContext *cx,                                   \
@@ -56,7 +56,7 @@ xpc_qsUnwrapThis<_interface>(JSContext *cx,                                   \
                                                 &rv);                         \
     if(!native)                                                               \
         return xpc_qsThrow(cx, rv);                                           \
-    *ppThis = static_cast<_interface*>(static_cast<_base*>(native));          \
+    *ppThis = static_cast<_interface*>(native);                               \
     return JS_TRUE;                                                           \
 }                                                                             \
                                                                               \
@@ -72,22 +72,22 @@ xpc_qsUnwrapArg<_interface>(JSContext *cx,                                    \
     nsISupports *native = castNativeArgFromWrapper(cx, v, _bit, ppArgRef, vp, \
                                                    &rv);                      \
     if(NS_SUCCEEDED(rv))                                                      \
-        *ppArg = static_cast<_interface*>(static_cast<_base*>(native));       \
+        *ppArg = static_cast<_interface*>(native);                            \
     return rv;                                                                \
 }
 
-#define DOMCI_CASTABLE_INTERFACE(_interface, _base, _bit, _extra)             \
-  DEFINE_UNWRAP_CAST(_interface, _base, _bit)
+#define DOMCI_CASTABLE_INTERFACE(_interface, _bit, _extra)                    \
+  DEFINE_UNWRAP_CAST(_interface, _bit)
 
 DOMCI_CASTABLE_INTERFACES(unused)
 
 #undef DOMCI_CASTABLE_INTERFACE
 
-
-
-
-
-
+// Ideally we'd just add nsGenericElement to the castable interfaces, but for
+// now nsDocumentFragment inherits from nsGenericElement (even though it's not
+// an Element) so we have to special-case nsGenericElement and use
+// nsIContent::IsElement().
+// FIXME: bug 563659.
 inline JSBool
 castToElement(nsIContent *content, jsval val, nsGenericElement **ppInterface,
               jsval *pVal)
@@ -139,10 +139,4 @@ xpc_qsUnwrapArg<nsGenericElement>(JSContext *cx,
     return rv;
 }
 
-inline nsISupports*
-ToSupports(nsContentList *p)
-{
-    return static_cast<nsINodeList*>(p);
-}
-
-#endif 
+#endif /* nsDOMQS_h__ */
