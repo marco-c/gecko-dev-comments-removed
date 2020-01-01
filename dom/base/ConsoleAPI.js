c5304b44fba060b8eb8e8b5e7765fc69213a0b9e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 06:33:33 +0000

    Back out bug 631225.
    UltraBlame original commit: e8c23ad47a571906e94ab1ef5894ab8d8691dc5a

--- dom/base/ConsoleAPI.js
+++ dom/base/ConsoleAPI.js
@@ -1,40 +1,40 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is Console API code.
+ *
+ * The Initial Developer of the Original Code is Mozilla Foundation
+ * Portions created by the Initial Developer are Copyright (C) 2010
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *  David Dahl <ddahl@mozilla.com>  (Original Author)
+ *  Ryan Flint <rflint@mozilla.com>
+ *  Rob Campbell <rcampbell@mozilla.com>
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
 
 let Cu = Components.utils;
 let Ci = Components.interfaces;
@@ -50,7 +50,7 @@ ConsoleAPI.prototype = {
 
   QueryInterface: XPCOMUtils.generateQI([Ci.nsIDOMGlobalPropertyInitializer]),
 
-  
+  // nsIDOMGlobalPropertyInitializer
   init: function CA_init(aWindow) {
     let id;
     try {
@@ -62,8 +62,8 @@ ConsoleAPI.prototype = {
     }
 
     let self = this;
-    let chromeObject = {
-      
+    return {
+      // window.console API
       log: function CA_log() {
         self.notifyObservers(id, "log", arguments);
       },
@@ -76,38 +76,15 @@ ConsoleAPI.prototype = {
       error: function CA_error() {
         self.notifyObservers(id, "error", arguments);
       },
-      __exposedProps__: {
-        log: "r",
-        info: "r",
-        warn: "r",
-        error: "r"
-      }
+      // many flavors of console objects exist on the web, so calling
+      // unimplemented methods shouldn't be fatal. See bug 614350
+      __noSuchMethod__: function CA_nsm() {}
     };
-
-    
-    
-    let sandbox = Cu.Sandbox(aWindow);
-    let contentObject = Cu.evalInSandbox(
-        "(function(x) {\
-          var bind = Function.bind;\
-          var obj = {\
-            log: bind.call(x.log, x),\
-            info: bind.call(x.info, x),\
-            warn: bind.call(x.warn, x),\
-            error: bind.call(x.error, x),\
-            __mozillaConsole__: true,\
-            __noSuchMethod__: function() {}\
-          };\
-          Object.defineProperty(obj, '__mozillaConsole__', { value: true });\
-          return obj;\
-        })", sandbox)(chromeObject);
-
-      return contentObject;
   },
 
-  
-
-
+  /**
+   * Notify all observers of any console API call
+   **/
   notifyObservers: function CA_notifyObservers(aID, aLevel, aArguments) {
     if (!aID)
       return;
