662caaabf2122ca45c439ed0389a1fbbbd5fe138
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 00:30:02 +0000

    Bug 331809. Make nsSubdocumentFrame explicitly clip its contents; content can bleed out otherwise, for example if there is no scrollframe associated with the subdocument's viewport. r+sr=mats
    UltraBlame original commit: 2b587f2befde64c4f0d224b39047bbed1f8ab00a

--- layout/generic/nsFrameFrame.cpp
+++ layout/generic/nsFrameFrame.cpp
@@ -337,7 +337,16 @@ nsSubDocumentFrame::BuildDisplayList(nsDisplayListBuilder*   aBuilder,
 
   aBuilder->EnterPresShell(f, dirty);
 
-  rv = f->BuildDisplayListForStackingContext(aBuilder, dirty, aLists.Content());
+  
+  nsDisplayList childItems;
+  rv = f->BuildDisplayListForStackingContext(aBuilder, dirty, &childItems);
+  if (NS_SUCCEEDED(rv)) {
+    rv = aLists.Content()->AppendNewToTop(
+        new (aBuilder) nsDisplayClip(nsnull, &childItems,
+              nsRect(aBuilder->ToReferenceFrame(f), f->GetSize())));
+    
+    childItems.DeleteAll();
+  }
 
   aBuilder->LeavePresShell(f, dirty);
   return rv;
