12deab2cb58aee29d43932a31c7ffababe4b5168
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 09:00:36 +0000

    Bug 687610 - QuickTime can't play media from the network cache when running OOP. r=bsmedberg
    UltraBlame original commit: 630de2647c0b472a63e80313ab662c1a28a220e7

--- dom/plugins/ipc/BrowserStreamChild.h
+++ dom/plugins/ipc/BrowserStreamChild.h
@@ -1,39 +1,39 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+/* -*- Mode: C++; c-basic-offset: 2; indent-tabs-mode: nil; tab-width: 8 -*- */
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is Mozilla Plugin App.
+ *
+ * The Initial Developer of the Original Code is
+ *   Benjamin Smedberg <benjamin@smedbergs.us>
+ * Portions created by the Initial Developer are Copyright (C) 2009
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
 
 #ifndef mozilla_plugins_BrowserStreamChild_h
 #define mozilla_plugins_BrowserStreamChild_h 1
@@ -72,7 +72,7 @@ public:
   virtual bool RecvWrite(const int32_t& offset,
                          const Buffer& data,
                          const uint32_t& newsize);
-  virtual bool AnswerNPP_StreamAsFile(const nsCString& fname);
+  virtual bool RecvNPP_StreamAsFile(const nsCString& fname);
   virtual bool RecvNPP_DestroyStream(const NPReason& reason);
   virtual bool Recv__delete__();
 
@@ -96,11 +96,11 @@ public:
     EnsureDeliveryPending();
   }
 
-  
-
-
-
-
+  /**
+   * During instance destruction, artificially cancel all outstanding streams.
+   *
+   * @return false if we are already in the DELETING state.
+   */
   bool InstanceDying() {
     if (DELETING == mState)
       return false;
@@ -121,22 +121,22 @@ private:
   friend class StreamNotifyChild;
   using PBrowserStreamChild::SendNPN_DestroyStream;
 
-  
-
-
-
+  /**
+   * Post an event to ensure delivery of pending data/destroy/urlnotify events
+   * outside of the current RPC stack.
+   */
   void EnsureDeliveryPending();
 
-  
-
-
-
+  /**
+   * Deliver data, destruction, notify scheduling
+   * or cancelling the suspended timer as needed.
+   */
   void Deliver();
 
-  
-
-
-
+  /**
+   * Deliver one chunk of pending data.
+   * @return true if the plugin indicated a pause was necessary
+   */
   bool DeliverPendingData();
 
   void SetSuspendedTimer();
@@ -147,28 +147,30 @@ private:
 
   static const NPReason kStreamOpen = -1;
 
-  
-
-
-
-
-
-
+  /**
+   * The plugin's notion of whether a stream has been "closed" (no more
+   * data delivery) differs from the plugin host due to asynchronous delivery
+   * of data and NPN_DestroyStream. While the plugin-visible stream is open,
+   * mStreamStatus should be kStreamOpen (-1). mStreamStatus will be a
+   * failure code if either the parent or child indicates stream failure.
+   */
   NPReason mStreamStatus;
 
-  
-
-
-
+  /**
+   * Delivery of NPP_DestroyStream and NPP_URLNotify must be postponed until
+   * all data has been delivered.
+   */
   enum {
-    NOT_DESTROYED, 
-    DESTROY_PENDING, 
-    DESTROYED 
+    NOT_DESTROYED, // NPP_DestroyStream not yet received
+    DESTROY_PENDING, // NPP_DestroyStream received, not yet delivered
+    DESTROYED // NPP_DestroyStream delivered, NPP_URLNotify may still be pending
   } mDestroyPending;
   bool mNotifyPending;
+  bool mStreamAsFilePending;
+  nsCString mStreamAsFileName;
 
-  
-  
+  // When NPP_Destroy is called for our instance (manager), this flag is set
+  // cancels the stream and avoids sending StreamDestroyed.
   bool mInstanceDying;
 
   enum {
@@ -189,17 +191,17 @@ private:
   };
   nsTArray<PendingData> mPendingData;
 
-  
-
-
-
-
-
+  /**
+   * Asynchronous RecvWrite messages are never delivered to the plugin
+   * immediately, because that may be in the midst of an unexpected RPC
+   * stack frame. It instead posts a runnable using this tracker to cancel
+   * in case we are destroyed.
+   */
   ScopedRunnableMethodFactory<BrowserStreamChild> mDeliveryTracker;
   base::RepeatingTimer<BrowserStreamChild> mSuspendedTimer;
 };
 
-} 
-} 
+} // namespace plugins
+} // namespace mozilla
 
-#endif 
+#endif /* mozilla_plugins_BrowserStreamChild_h */
