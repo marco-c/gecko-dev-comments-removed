c4f372aa2175ed6c4f5f84c787b9689f6e39d314
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 15:41:06 +0000

    Bug 797970 - Use stdint types in IonMonkey. r=jandem
    UltraBlame original commit: 3b419a7a354c8151ba403f5e68decad1a076c2d7

--- js/src/ion/Registers.h
+++ js/src/ion/Registers.h
@@ -1,9 +1,9 @@
-/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
- * vim: set ts=4 sw=4 et tw=99:
- *
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+
+
+
+
+
 
 #ifndef jsion_cpu_registers_h__
 #define jsion_cpu_registers_h__
@@ -19,7 +19,7 @@
 #endif
 #include "FixedArityList.h"
 
-// ARM defines the RegisterID within Architecture-arm.h
+
 #if !defined(JS_CPU_ARM) && defined(JS_METHODJIT)
 #include "assembler/assembler/MacroAssembler.h"
 #endif
@@ -33,13 +33,13 @@ struct Register {
     typedef js::ion::Registers::RegisterID RegisterID;
     Code code_;
 
-    static Register FromCode(uint32 i) {
+    static Register FromCode(uint32_t i) {
         JS_ASSERT(i < Registers::Total);
         Register r = { (Registers::Code)i };
         return r;
     }
     Code code() const {
-        JS_ASSERT((uint32)code_ < Registers::Total);
+        JS_ASSERT((uint32_t)code_ < Registers::Total);
         return code_;
     }
     const char *name() const {
@@ -62,13 +62,13 @@ struct FloatRegister {
 
     Code code_;
 
-    static FloatRegister FromCode(uint32 i) {
+    static FloatRegister FromCode(uint32_t i) {
         JS_ASSERT(i < FloatRegisters::Total);
         FloatRegister r = { (FloatRegisters::Code)i };
         return r;
     }
     Code code() const {
-        JS_ASSERT((uint32)code_ < FloatRegisters::Total);
+        JS_ASSERT((uint32_t)code_ < FloatRegisters::Total);
         return code_;
     }
     const char *name() const {
@@ -85,7 +85,7 @@ struct FloatRegister {
     }
 };
 
-// Information needed to recover machine register state.
+
 class MachineState
 {
     FixedArityList<uintptr_t *, Registers::Total> regs_;
@@ -116,8 +116,8 @@ class MachineState
     }
 };
 
-} // namespace ion
-} // namespace js
+} 
+} 
 
-#endif // jsion_cpu_registers_h__
+#endif 
 
