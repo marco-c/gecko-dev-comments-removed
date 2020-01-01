cd88b0035ac4d1fed9649808f4d269dc9cb489d8
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 01:00:24 +0000

    Merging mozilla-central to mozilla-qt.
    UltraBlame original commit: 10b845068cff1bbfcc5421b0039dfdc1ab2f7733

--- gfx/src/thebes/nsThebesDeviceContext.cpp
+++ gfx/src/thebes/nsThebesDeviceContext.cpp
@@ -245,8 +245,14 @@ nsThebesDeviceContext::SetDPI()
         
         
         
-        mAppUnitsPerDevNotScaledPixel = PR_MAX(1, AppUnitsPerCSSPixel() /
-                                        PR_MAX(1, dpi / 96));
+        PRUint32 roundedDPIScaleFactor = (dpi + 48)/96;
+#ifdef MOZ_WIDGET_GTK2
+        
+        
+        roundedDPIScaleFactor = dpi/96;
+#endif
+        mAppUnitsPerDevNotScaledPixel =
+          PR_MAX(1, AppUnitsPerCSSPixel() / PR_MAX(1, roundedDPIScaleFactor));
     } else {
         
 
