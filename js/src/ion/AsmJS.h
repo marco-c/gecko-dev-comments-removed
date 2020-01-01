a68703db3d79125c7c423729ce50086284d9fb15
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 18:09:12 +0000

    Bug 851880 - Build OdinMonkey on more Unices. r=luke
    UltraBlame original commit: a3a163f9a9896974560e39638185c4af8c30f49e

--- js/src/ion/AsmJS.h
+++ js/src/ion/AsmJS.h
@@ -1,9 +1,9 @@
-/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
- * vim: set ts=4 sw=4 et tw=99:
- *
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+
+
+
+
+
 
 #if !defined(jsion_asmjs_h__)
 #define jsion_asmjs_h__
@@ -13,12 +13,11 @@
 # include <mach/mach.h>
 #endif
 
-// asm.js compilation is only available on desktop x86/x64 at the moment.
-// Don't panic, mobile support is coming soon.
+
+
 #if defined(JS_ION) && \
     !defined(ANDROID) && \
-    (defined(JS_CPU_X86) || defined(JS_CPU_X64)) &&  \
-    (defined(__linux__) || defined(XP_WIN) || defined(XP_MACOSX))
+    (defined(JS_CPU_X86) || defined(JS_CPU_X64))
 # define JS_ASMJS
 #endif
 
@@ -30,48 +29,48 @@ class AsmJSModule;
 namespace frontend { struct TokenStream; struct ParseNode; }
 namespace ion { class MIRGenerator; class LIRGraph; }
 
-// Return whether asm.js optimization is inhibitted by the platform or
-// dynamically disabled. (Exposed as JSNative for shell testing.)
+
+
 extern JSBool
 IsAsmJSCompilationAvailable(JSContext *cx, unsigned argc, Value *vp);
 
-// Called after parsing a function 'fn' which contains the "use asm" directive.
-// This function performs type-checking and code-generation. If type-checking
-// succeeds, the generated native function is assigned to |moduleFun|.
-// Otherwise, a warning will be emitted and |moduleFun| is left unchanged. The
-// function returns 'false' only if a real JS semantic error (probably OOM) is
-// pending.
+
+
+
+
+
+
 extern bool
 CompileAsmJS(JSContext *cx, frontend::TokenStream &ts, frontend::ParseNode *fn,
              const CompileOptions &options,
              ScriptSource *scriptSource, uint32_t bufStart, uint32_t bufEnd,
              MutableHandleFunction moduleFun);
 
-// Called for any "use asm" function which successfully typechecks. This
-// function performs the validation and dynamic linking of a module to its
-// given arguments. If validation succeeds, the module's return value (its
-// exports) are returned via |vp|.  Otherwise, there was a validation error and
-// execution fall back to the usual path (bytecode generation, interpretation,
-// etc). The function returns 'false' only if a real JS semantic error (OOM or
-// exception thrown when executing GetProperty on the arguments) is pending.
+
+
+
+
+
+
+
 extern JSBool
 LinkAsmJS(JSContext *cx, unsigned argc, JS::Value *vp);
 
-// Force any currently-executing asm.js code to call
-// js_HandleExecutionInterrupt.
+
+
 void
 TriggerOperationCallbackForAsmJSCode(JSRuntime *rt);
 
-// The JSRuntime maintains a stack of AsmJSModule activations. An "activation"
-// of module A is an initial call from outside A into a function inside A,
-// followed by a sequence of calls inside A, and terminated by a call that
-// leaves A. The AsmJSActivation stack serves three purposes:
-//  - record the correct cx to pass to VM calls from asm.js;
-//  - record enough information to pop all the frames of an activation if an
-//    exception is thrown;
-//  - record the information necessary for asm.js signal handlers to safely
-//    recover from (expected) out-of-bounds access, the operation callback,
-//    stack overflow, division by zero, etc.
+
+
+
+
+
+
+
+
+
+
 class AsmJSActivation
 {
     JSContext *cx_;
@@ -88,24 +87,24 @@ class AsmJSActivation
 
     const AsmJSModule &module() const { return module_; }
 
-    // Read by JIT code:
+    
     static unsigned offsetOfContext() { return offsetof(AsmJSActivation, cx_); }
     static unsigned offsetOfResumePC() { return offsetof(AsmJSActivation, resumePC_); }
 
-    // Initialized by JIT code:
+    
     static unsigned offsetOfErrorRejoinSP() { return offsetof(AsmJSActivation, errorRejoinSP_); }
 
-    // Set from SIGSEGV handler:
+    
     void setResumePC(void *pc) { resumePC_ = pc; }
 };
 
-// The asm.js spec requires that the ArrayBuffer's byteLength be a multiple of 4096.
+
 static const size_t AsmJSAllocationGranularity = 4096;
 
 #ifdef JS_CPU_X64
-// On x64, the internal ArrayBuffer data array is inflated to 4GiB (only the
-// byteLength portion of which is accessible) so that out-of-bounds accesses
-// (made using a uint32 index) are guaranteed to raise a SIGSEGV.
+
+
+
 static const size_t AsmJSBufferProtectedSize = 4 * 1024ULL * 1024ULL * 1024ULL;
 #endif
 
@@ -129,15 +128,15 @@ class AsmJSMachExceptionHandler
 };
 #endif
 
-// Struct type for passing parallel compilation data between the main thread
-// and compilation workers.
+
+
 struct AsmJSParallelTask
 {
-    LifoAlloc lifo;         // Provider of all heap memory used for compilation.
+    LifoAlloc lifo;         
 
-    uint32_t funcNum;       // Index |i| of function in |Module.function(i)|.
-    ion::MIRGenerator *mir; // Passed from main thread to worker.
-    ion::LIRGraph *lir;     // Passed from worker to main thread.
+    uint32_t funcNum;       
+    ion::MIRGenerator *mir; 
+    ion::LIRGraph *lir;     
 
     AsmJSParallelTask(size_t defaultChunkSize)
       : lifo(defaultChunkSize),
@@ -151,8 +150,8 @@ struct AsmJSParallelTask
     }
 };
 
-// Returns true if the given native is the one that is used to implement asm.js
-// module functions.
+
+
 #ifdef JS_ASMJS
 bool
 IsAsmJSModuleNative(js::Native native);
@@ -164,6 +163,6 @@ IsAsmJSModuleNative(js::Native native)
 }
 #endif
 
-} // namespace js
+} 
 
-#endif // jsion_asmjs_h__
+#endif 
