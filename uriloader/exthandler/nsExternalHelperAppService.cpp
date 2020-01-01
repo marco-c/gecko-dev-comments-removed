62a0533cb70e7f67dfe989d49f91110ee01cecc7
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 01:18:08 +0000

    Bug 455913 -  nsIHelperAppLauncher should provide info about content length. r+sr=cbiesinger.
    UltraBlame original commit: 81f3072d75be6acfc89674265d504496c15743b3

--- uriloader/exthandler/nsExternalHelperAppService.cpp
+++ uriloader/exthandler/nsExternalHelperAppService.cpp
@@ -1154,6 +1154,12 @@ NS_IMETHODIMP nsExternalAppHandler::GetTimeDownloadStarted(PRTime* aTime)
   return NS_OK;
 }
 
+NS_IMETHODIMP nsExternalAppHandler::GetContentLength(PRInt64 *aContentLength)
+{
+  *aContentLength = mContentLength;
+  return NS_OK;
+}
+
 NS_IMETHODIMP nsExternalAppHandler::CloseProgressWindow()
 {
   
@@ -1306,8 +1312,8 @@ nsresult nsExternalAppHandler::SetUpTempFile(nsIChannel * aChannel)
   dummyFile->Remove(PR_FALSE);
 #endif
 
-  
-  
+
+
   tempLeafName.Append(NS_LITERAL_CSTRING(".part"));
 
   rv = mTempFile->Append(NS_ConvertUTF8toUTF16(tempLeafName));
