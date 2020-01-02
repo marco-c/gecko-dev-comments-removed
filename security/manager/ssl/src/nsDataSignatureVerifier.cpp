00975c571593ed60331e5b09e4800ce8a2515655
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-02 23:08:13 +0000

    Bug 1043041: Use mozilla::pkix::Time instead of PRTime, r=keeler
    UltraBlame original commit: a4a8b3b58191206f53748d823cf255fba4042253

--- security/manager/ssl/src/nsDataSignatureVerifier.cpp
+++ security/manager/ssl/src/nsDataSignatureVerifier.cpp
@@ -1,6 +1,6 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+
+
 
 #include "nsDataSignatureVerifier.h"
 
@@ -42,13 +42,13 @@ nsDataSignatureVerifier::VerifyData(const nsACString & aData,
                                     const nsACString & aPublicKey,
                                     bool *_retval)
 {
-    // Allocate an arena to handle the majority of the allocations
+    
     PLArenaPool *arena;
     arena = PORT_NewArena(DER_DEFAULT_CHUNKSIZE);
     if (!arena)
         return NS_ERROR_OUT_OF_MEMORY;
 
-    // Base 64 decode the key
+    
     SECItem keyItem;
     PORT_Memset(&keyItem, 0, sizeof(SECItem));
     if (!NSSBase64_DecodeBuffer(arena, &keyItem,
@@ -58,7 +58,7 @@ nsDataSignatureVerifier::VerifyData(const nsACString & aData,
         return NS_ERROR_FAILURE;
     }
     
-    // Extract the public key from the data
+    
     CERTSubjectPublicKeyInfo *pki = SECKEY_DecodeDERSubjectPublicKeyInfo(&keyItem);
     if (!pki) {
         PORT_FreeArena(arena, false);
@@ -73,7 +73,7 @@ nsDataSignatureVerifier::VerifyData(const nsACString & aData,
         return NS_ERROR_FAILURE;
     }
     
-    // Base 64 decode the signature
+    
     SECItem signatureItem;
     PORT_Memset(&signatureItem, 0, sizeof(SECItem));
     if (!NSSBase64_DecodeBuffer(arena, &signatureItem,
@@ -84,7 +84,7 @@ nsDataSignatureVerifier::VerifyData(const nsACString & aData,
         return NS_ERROR_FAILURE;
     }
     
-    // Decode the signature and algorithm
+    
     CERTSignedData sigData;
     PORT_Memset(&sigData, 0, sizeof(CERTSignedData));
     SECStatus ss = SEC_QuickDERDecodeItem(arena, &sigData, 
@@ -96,7 +96,7 @@ nsDataSignatureVerifier::VerifyData(const nsACString & aData,
         return NS_ERROR_FAILURE;
     }
     
-    // Perform the final verification
+    
     DER_ConvertBitString(&(sigData.signature));
     ss = VFY_VerifyDataWithAlgorithmID((const unsigned char*)nsPromiseFlatCString(aData).get(),
                                        aData.Length(), publicKey,
@@ -104,7 +104,7 @@ nsDataSignatureVerifier::VerifyData(const nsACString & aData,
                                        &(sigData.signatureAlgorithm),
                                        nullptr, nullptr);
     
-    // Clean up remaining objects
+    
     SECKEY_DestroyPublicKey(publicKey);
     PORT_FreeArena(arena, false);
     
@@ -122,7 +122,7 @@ VerifyCMSDetachedSignatureIncludingCertificate(
                                 void* pinArg),
   void *verifyCertificateContext, void* pinArg)
 {
-  // XXX: missing pinArg is tolerated.
+  
   if (NS_WARN_IF(!buffer.data && buffer.len > 0) ||
       NS_WARN_IF(!detachedDigest.data && detachedDigest.len > 0) ||
       (!verifyCertificate) ||
@@ -147,21 +147,21 @@ VerifyCMSDetachedSignatureIncludingCertificate(
     return NS_ERROR_CMS_VERIFY_NO_CONTENT_INFO;
   }
 
-  // signedData is non-owning
+  
   NSSCMSSignedData* signedData =
     reinterpret_cast<NSSCMSSignedData*>(NSS_CMSContentInfo_GetContent(cinfo));
   if (!signedData) {
     return NS_ERROR_CMS_VERIFY_NO_CONTENT_INFO;
   }
 
-  // Set digest value.
+  
   if (NSS_CMSSignedData_SetDigestValue(signedData, SEC_OID_SHA1,
                                        const_cast<SECItem*>(&detachedDigest))) {
     return NS_ERROR_CMS_VERIFY_BAD_DIGEST;
   }
 
-  // Parse the certificates into CERTCertificate objects held in memory so
-  // verifyCertificate will be able to find them during path building.
+  
+  
   ScopedCERTCertList certs(CERT_NewCertList());
   if (!certs) {
     return NS_ERROR_OUT_OF_MEMORY;
@@ -172,10 +172,10 @@ VerifyCMSDetachedSignatureIncludingCertificate(
         cert(CERT_NewTempCertificate(CERT_GetDefaultCertDB(),
                                      signedData->rawCerts[i], nullptr, false,
                                      true));
-      // Skip certificates that fail to parse
+      
       if (cert) {
         if (CERT_AddCertToListTail(certs.get(), cert.get()) == SECSuccess) {
-          cert.forget(); // ownership transfered
+          cert.forget(); 
         } else {
           return NS_ERROR_OUT_OF_MEMORY;
         }
@@ -183,12 +183,12 @@ VerifyCMSDetachedSignatureIncludingCertificate(
     }
   }
 
-  // Get the end-entity certificate.
+  
   int numSigners = NSS_CMSSignedData_SignerInfoCount(signedData);
   if (NS_WARN_IF(numSigners != 1)) {
     return NS_ERROR_CMS_VERIFY_ERROR_PROCESSING;
   }
-  // signer is non-owning.
+  
   NSSCMSSignerInfo* signer = NSS_CMSSignedData_GetSignerInfo(signedData, 0);
   if (NS_WARN_IF(!signer)) {
     return NS_ERROR_CMS_VERIFY_ERROR_PROCESSING;
@@ -204,7 +204,7 @@ VerifyCMSDetachedSignatureIncludingCertificate(
     return rv;
   }
 
-  // See NSS_CMSContentInfo_GetContentTypeOID, which isn't exported from NSS.
+  
   SECOidData* contentTypeOidData =
     SECOID_FindOID(&signedData->contentInfo.contentType);
   if (!contentTypeOidData) {
@@ -216,7 +216,7 @@ VerifyCMSDetachedSignatureIncludingCertificate(
                          &contentTypeOidData->oid));
 }
 
-} // namespace mozilla
+} 
 
 namespace {
 
@@ -229,7 +229,7 @@ struct VerifyCertificateContext
 static nsresult
 VerifyCertificate(CERTCertificate* cert, void* voidContext, void* pinArg)
 {
-  // XXX: missing pinArg is tolerated
+  
   if (NS_WARN_IF(!cert) || NS_WARN_IF(!voidContext)) {
     return NS_ERROR_INVALID_ARG;
   }
@@ -269,14 +269,14 @@ VerifyCertificate(CERTCertificate* cert, void* voidContext, void* pinArg)
 
   return MapSECStatus(certVerifier->VerifyCert(cert,
                                                certificateUsageObjectSigner,
-                                               PR_Now(), pinArg,
-                                               nullptr, // hostname
-                                               0, // flags
-                                               nullptr, // stapledOCSPResponse
+                                               Now(), pinArg,
+                                               nullptr, 
+                                               0, 
+                                               nullptr, 
                                                &context->builtChain));
 }
 
-} // unnamed namespcae
+} 
 
 NS_IMETHODIMP
 nsDataSignatureVerifier::VerifySignature(const char* aRSABuf,
@@ -310,7 +310,7 @@ nsDataSignatureVerifier::VerifySignature(const char* aRSABuf,
   };
 
   VerifyCertificateContext context;
-  // XXX: pinArg is missing
+  
   rv = VerifyCMSDetachedSignatureIncludingCertificate(buffer, digest.get(),
                                                       VerifyCertificate,
                                                       &context, nullptr);
