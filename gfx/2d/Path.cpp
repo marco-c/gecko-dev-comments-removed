ca909ebb8620820a5c96b7bb473bd658e5f98e1f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-02 13:20:50 +0000

    Bug 946540: Deal with inflection points that all lie outside of (0, 1). r=jrmuizel
    UltraBlame original commit: a5f9dee7531e8b725401c538472ff6abe3b63690

--- gfx/2d/Path.cpp
+++ gfx/2d/Path.cpp
@@ -407,7 +407,8 @@ FlattenBezier(const BezierControlPoints &aControlPoints,
     return;
   }
 
-  if (count == 0) {
+  
+  if (count == 0 || ((t1 < 0 || t1 > 1.0) && ((t2 < 0 || t2 > 1.0) || count == 1)) ) {
     FlattenBezierCurveSegment(aControlPoints, aSink, aTolerance);
     return;
   }
