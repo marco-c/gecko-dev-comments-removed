bb6827e330df25cc0b4f278033c6e2dc82f75079
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-02 19:06:20 +0000

    Bug 946065 - Part 8: Move content/svg/ to dom/ and flatten subdirectories. r=jwatt
    UltraBlame original commit: 85228ee8ce8b1ed628e49baf88104469fb0ea7e6

---
+++ dom/svg/SVGGraphicsElement.cpp
@@ -0,0 +1,30 @@
+
+
+
+
+
+#include "mozilla/dom/SVGGraphicsElement.h"
+
+namespace mozilla {
+namespace dom {
+
+
+
+
+NS_IMPL_ADDREF_INHERITED(SVGGraphicsElement, SVGGraphicsElementBase)
+NS_IMPL_RELEASE_INHERITED(SVGGraphicsElement, SVGGraphicsElementBase)
+
+NS_INTERFACE_MAP_BEGIN(SVGGraphicsElement)
+  NS_INTERFACE_MAP_ENTRY(mozilla::dom::SVGTests)
+NS_INTERFACE_MAP_END_INHERITING(SVGGraphicsElementBase)
+
+
+
+
+SVGGraphicsElement::SVGGraphicsElement(already_AddRefed<nsINodeInfo>& aNodeInfo)
+  : SVGGraphicsElementBase(aNodeInfo)
+{
+}
+
+} 
+} 
