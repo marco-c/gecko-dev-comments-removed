96ab93f1e21af0470f50e240f20b98b1a787426f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-02 22:01:10 +0000

    Bug 1036789 - Convert the third quarter of MFBT to Gecko style. r=Ms2ger.
    UltraBlame original commit: 91d7659bb35145e10ff737753688e0972c1a79b5

--- mfbt/RollingMean.h
+++ mfbt/RollingMean.h
@@ -5,6 +5,7 @@
 
 
 
+
 #ifndef mozilla_RollingMean_h_
 #define mozilla_RollingMean_h_
 
@@ -28,78 +29,85 @@ namespace mozilla {
 template<typename T, typename S>
 class RollingMean
 {
-  private:
-    size_t mInsertIndex;
-    size_t mMaxValues;
-    Vector<T> mValues;
-    S mTotal;
+private:
+  size_t mInsertIndex;
+  size_t mMaxValues;
+  Vector<T> mValues;
+  S mTotal;
 
-  public:
-    static_assert(!IsFloatingPoint<T>::value,
-                  "floating-point types are unsupported due to rounding "
-                  "errors");
+public:
+  static_assert(!IsFloatingPoint<T>::value,
+                "floating-point types are unsupported due to rounding "
+                "errors");
 
-    explicit RollingMean(size_t aMaxValues)
-      : mInsertIndex(0),
-        mMaxValues(aMaxValues),
-        mTotal(0)
-    {
-      MOZ_ASSERT(aMaxValues > 0);
-    }
+  explicit RollingMean(size_t aMaxValues)
+    : mInsertIndex(0),
+      mMaxValues(aMaxValues),
+      mTotal(0)
+  {
+    MOZ_ASSERT(aMaxValues > 0);
+  }
 
-    RollingMean& operator=(RollingMean&& aOther) {
-      MOZ_ASSERT(this != &aOther, "self-assignment is forbidden");
-      this->~RollingMean();
-      new(this) RollingMean(aOther.mMaxValues);
-      mInsertIndex = aOther.mInsertIndex;
-      mTotal = aOther.mTotal;
-      mValues.swap(aOther.mValues);
-      return *this;
-    }
+  RollingMean& operator=(RollingMean&& aOther)
+  {
+    MOZ_ASSERT(this != &aOther, "self-assignment is forbidden");
+    this->~RollingMean();
+    new(this) RollingMean(aOther.mMaxValues);
+    mInsertIndex = aOther.mInsertIndex;
+    mTotal = aOther.mTotal;
+    mValues.swap(aOther.mValues);
+    return *this;
+  }
 
-    
+  
 
 
-    bool insert(T aValue) {
-      MOZ_ASSERT(mValues.length() <= mMaxValues);
+  bool insert(T aValue)
+  {
+    MOZ_ASSERT(mValues.length() <= mMaxValues);
 
-      if (mValues.length() == mMaxValues) {
-        mTotal = mTotal - mValues[mInsertIndex] + aValue;
-        mValues[mInsertIndex] = aValue;
-      } else {
-        if (!mValues.append(aValue))
-          return false;
-        mTotal = mTotal + aValue;
+    if (mValues.length() == mMaxValues) {
+      mTotal = mTotal - mValues[mInsertIndex] + aValue;
+      mValues[mInsertIndex] = aValue;
+    } else {
+      if (!mValues.append(aValue)) {
+        return false;
       }
-
-      mInsertIndex = (mInsertIndex + 1) % mMaxValues;
-      return true;
+      mTotal = mTotal + aValue;
     }
 
-    
+    mInsertIndex = (mInsertIndex + 1) % mMaxValues;
+    return true;
+  }
+
+  
 
 
-    T mean() {
-      MOZ_ASSERT(!empty());
-      return T(mTotal / mValues.length());
-    }
+  T mean()
+  {
+    MOZ_ASSERT(!empty());
+    return T(mTotal / mValues.length());
+  }
 
-    bool empty() {
-      return mValues.empty();
-    }
+  bool empty()
+  {
+    return mValues.empty();
+  }
 
-    
+  
 
 
-    void clear() {
-      mValues.clear();
-      mInsertIndex = 0;
-      mTotal = T(0);
-    }
+  void clear()
+  {
+    mValues.clear();
+    mInsertIndex = 0;
+    mTotal = T(0);
+  }
 
-    size_t maxValues() {
-      return mMaxValues;
-    }
+  size_t maxValues()
+  {
+    return mMaxValues;
+  }
 };
 
 } 
--- mfbt/WeakPtr.h
+++ mfbt/WeakPtr.h
@@ -63,6 +63,7 @@
 
 
 
+
 #ifndef mozilla_WeakPtr_h
 #define mozilla_WeakPtr_h
 
@@ -82,45 +83,47 @@ template <typename T, class WeakReference> class SupportsWeakPtrBase;
 namespace detail {
 
 
+
 template<class T>
 class WeakReference : public ::mozilla::RefCounted<WeakReference<T> >
 {
-  public:
-    explicit WeakReference(T* p) : ptr(p) {}
-    T* get() const {
-      return ptr;
-    }
+public:
+  explicit WeakReference(T* p) : mPtr(p) {}
+
+  T* get() const { return mPtr; }
 
 #ifdef MOZ_REFCOUNTED_LEAK_CHECKING
 #ifdef XP_WIN
 #define snprintf _snprintf
 #endif
-    const char* typeName() const {
-      static char nameBuffer[1024];
-      const char* innerType = ptr->typeName();
-      
-      
-      
-      MOZ_ASSERT(strlen(innerType) + sizeof("WeakReference<>") < ArrayLength(nameBuffer),
-                 "Exceedingly large type name");
-      snprintf(nameBuffer, ArrayLength(nameBuffer), "WeakReference<%s>", innerType);
-      
-      
-      return nameBuffer;
-    }
-    size_t typeSize() const {
-      return sizeof(*this);
-    }
+  const char* typeName() const
+  {
+    static char nameBuffer[1024];
+    const char* innerType = mPtr->typeName();
+    
+    
+    
+    MOZ_ASSERT(strlen(innerType) + sizeof("WeakReference<>") <
+               ArrayLength(nameBuffer),
+               "Exceedingly large type name");
+    snprintf(nameBuffer, ArrayLength(nameBuffer), "WeakReference<%s>",
+             innerType);
+    
+    
+    return nameBuffer;
+  }
+
+  size_t typeSize() const { return sizeof(*this); }
 #undef snprintf
 #endif
 
-  private:
-    friend class WeakPtrBase<T, WeakReference<T> >;
-    friend class SupportsWeakPtrBase<T, WeakReference<T> >;
-    void detach() {
-      ptr = nullptr;
-    }
-    T* ptr;
+private:
+  friend class WeakPtrBase<T, WeakReference<T> >;
+  friend class SupportsWeakPtrBase<T, WeakReference<T> >;
+
+  void detach() { mPtr = nullptr; }
+
+  T* mPtr;
 };
 
 } 
@@ -128,25 +131,29 @@ class WeakReference : public ::mozilla::RefCounted<WeakReference<T> >
 template <typename T, class WeakReference>
 class SupportsWeakPtrBase
 {
-  public:
-    WeakPtrBase<T, WeakReference> asWeakPtr() {
-      if (!weakRef)
-        weakRef = new WeakReference(static_cast<T*>(this));
-      return WeakPtrBase<T, WeakReference>(weakRef);
+public:
+  WeakPtrBase<T, WeakReference> asWeakPtr()
+  {
+    if (!weakRef) {
+      weakRef = new WeakReference(static_cast<T*>(this));
     }
+    return WeakPtrBase<T, WeakReference>(weakRef);
+  }
 
-  protected:
-    ~SupportsWeakPtrBase() {
-      static_assert(IsBaseOf<SupportsWeakPtrBase<T, WeakReference>, T>::value,
-                    "T must derive from SupportsWeakPtrBase<T, WeakReference>");
-      if (weakRef)
-        weakRef->detach();
+protected:
+  ~SupportsWeakPtrBase()
+  {
+    static_assert(IsBaseOf<SupportsWeakPtrBase<T, WeakReference>, T>::value,
+                  "T must derive from SupportsWeakPtrBase<T, WeakReference>");
+    if (weakRef) {
+      weakRef->detach();
     }
+  }
 
-  private:
-    friend class WeakPtrBase<T, WeakReference>;
+private:
+  friend class WeakPtrBase<T, WeakReference>;
 
-    RefPtr<WeakReference> weakRef;
+  RefPtr<WeakReference> weakRef;
 };
 
 template <typename T>
@@ -157,42 +164,37 @@ class SupportsWeakPtr : public SupportsWeakPtrBase<T, detail::WeakReference<T> >
 template <typename T, class WeakReference>
 class WeakPtrBase
 {
-  public:
-    WeakPtrBase(const WeakPtrBase<T, WeakReference>& o) : ref(o.ref) {}
-    
-    WeakPtrBase() : ref(new WeakReference(nullptr)) {}
+public:
+  WeakPtrBase(const WeakPtrBase<T, WeakReference>& aOther)
+    : mRef(aOther.mRef)
+  {}
 
-    operator T*() const {
-      return ref->get();
-    }
-    T& operator*() const {
-      return *ref->get();
-    }
+  
+  WeakPtrBase() : mRef(new WeakReference(nullptr)) {}
 
-    T* operator->() const {
-      return ref->get();
-    }
+  operator T*() const { return mRef->get(); }
+  T& operator*() const { return *mRef->get(); }
 
-    T* get() const {
-      return ref->get();
-    }
+  T* operator->() const { return mRef->get(); }
 
-  private:
-    friend class SupportsWeakPtrBase<T, WeakReference>;
+  T* get() const { return mRef->get(); }
 
-    explicit WeakPtrBase(const RefPtr<WeakReference> &o) : ref(o) {}
+private:
+  friend class SupportsWeakPtrBase<T, WeakReference>;
 
-    RefPtr<WeakReference> ref;
+  explicit WeakPtrBase(const RefPtr<WeakReference> &aOther) : mRef(aOther) {}
+
+  RefPtr<WeakReference> mRef;
 };
 
 template <typename T>
 class WeakPtr : public WeakPtrBase<T, detail::WeakReference<T> >
 {
-    typedef WeakPtrBase<T, detail::WeakReference<T> > Base;
-  public:
-    WeakPtr(const WeakPtr<T>& o) : Base(o) {}
-    MOZ_IMPLICIT WeakPtr(const Base& o) : Base(o) {}
-    WeakPtr() {}
+  typedef WeakPtrBase<T, detail::WeakReference<T> > Base;
+public:
+  WeakPtr(const WeakPtr<T>& aOther) : Base(aOther) {}
+  MOZ_IMPLICIT WeakPtr(const Base& aOther) : Base(aOther) {}
+  WeakPtr() {}
 };
 
 } 
