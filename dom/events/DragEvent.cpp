e3a2d0fb1d55e3ff4986599eafe1481dcc67f450
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-02 23:08:35 +0000

    Bug 1046101 part.1 Rename nsEventStructType to mozilla::EventClassID r=smaug
    UltraBlame original commit: b6e08b99faf8cd00121d4112445a7aded885c7cf

--- dom/events/DragEvent.cpp
+++ dom/events/DragEvent.cpp
@@ -116,7 +116,7 @@ DragEvent::GetDataTransfer()
   
   
   
-  if (!mEvent || mEvent->eventStructType != NS_DRAG_EVENT) {
+  if (!mEvent || mEvent->mClass != NS_DRAG_EVENT) {
     NS_WARNING("Tried to get dataTransfer from non-drag event!");
     return nullptr;
   }
