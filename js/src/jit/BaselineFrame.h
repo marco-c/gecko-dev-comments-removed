0b43b713717782bda629125bae8a5ba720a7e6f4
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-02 11:33:34 +0000

    Bug 852175 - Fix baseline stack checks on functions which may push lots of locals. r=jandem
    UltraBlame original commit: c532cabb71ecbfee8cf324d6f4dc034acd4cb2c9

--- js/src/jit/BaselineFrame.h
+++ js/src/jit/BaselineFrame.h
@@ -57,7 +57,10 @@ class BaselineFrame
         HAS_HOOK_DATA    = 1 << 7,
 
         
-        HAS_PUSHED_SPS_FRAME = 1 << 8
+        HAS_PUSHED_SPS_FRAME = 1 << 8,
+
+        
+        OVER_RECURSED    = 1 << 9
     };
 
   protected: 
@@ -303,6 +306,14 @@ class BaselineFrame
 
     void unsetPushedSPSFrame() {
         flags_ &= ~HAS_PUSHED_SPS_FRAME;
+    }
+
+    bool overRecursed() const {
+        return flags_ & OVER_RECURSED;
+    }
+
+    void setOverRecursed() {
+        flags_ |= OVER_RECURSED;
     }
 
     void trace(JSTracer *trc);
