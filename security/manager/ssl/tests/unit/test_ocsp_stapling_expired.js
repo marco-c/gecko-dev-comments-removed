1bc43e6484706bbb3a25f51d7464e1c1eb9bdbf3
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-02 19:58:17 +0000

    Bug 991815 - Part 2/2 - Tests for OCSP responses up to 1 year old. r=keeler
    UltraBlame original commit: d4fd9141e5ab755c27aae40b7876a0018dac75bb

--- security/manager/ssl/tests/unit/test_ocsp_stapling_expired.js
+++ security/manager/ssl/tests/unit/test_ocsp_stapling_expired.js
@@ -1,13 +1,13 @@
-// -*- Mode: javascript; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
-// This Source Code Form is subject to the terms of the Mozilla Public
-// License, v. 2.0. If a copy of the MPL was not distributed with this
-// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+
+
+
 "use strict";
 
-// In which we connect to a number of domains (as faked by a server running
-// locally) with OCSP stapling enabled to determine that good things happen
-// and bad things don't, specifically with respect to various expired OCSP
-// responses (stapled and otherwise).
+
+
+
+
 
 let gCurrentOCSPResponse = null;
 let gOCSPRequestCount = 0;
@@ -34,15 +34,15 @@ let args = [["good", "localhostAndExampleCom", "unused"],
              ["unknown", "localhostAndExampleCom", "unused"],
             ];
 let ocspResponses = generateOCSPResponses(args, "tlsserver");
-// Fresh response, certificate is good.
+
 let ocspResponseGood = ocspResponses[0];
-// Expired response, certificate is good.
+
 let expiredOCSPResponseGood = ocspResponses[1];
-// Fresh signature, old validity period, certificate is good.
+
 let oldValidityPeriodOCSPResponseGood = ocspResponses[2];
-// Fresh signature, certificate is revoked.
+
 let ocspResponseRevoked = ocspResponses[3];
-// Fresh signature, certificate is unknown.
+
 let ocspResponseUnknown = ocspResponses[4];
 
 function run_test() {
@@ -70,15 +70,15 @@ function add_tests_in_mode(useMozillaPKIX)
     run_next_test();
   });
 
-  // In these tests, the OCSP stapling server gives us a stapled
-  // response based on the host name ("ocsp-stapling-expired" or
-  // "ocsp-stapling-expired-fresh-ca"). We then ensure that we're
-  // properly falling back to fetching revocation information.
-  // For ocsp-stapling-expired.example.com, the OCSP stapling server
-  // staples an expired OCSP response. The certificate has not expired.
-  // For ocsp-stapling-expired-fresh-ca.example.com, the OCSP stapling
-  // server staples an OCSP response with a recent signature but with an
-  // out-of-date validity period. The certificate has not expired.
+  
+  
+  
+  
+  
+  
+  
+  
+  
   add_ocsp_test("ocsp-stapling-expired.example.com", Cr.NS_OK,
                 ocspResponseGood);
   add_ocsp_test("ocsp-stapling-expired-fresh-ca.example.com", Cr.NS_OK,
@@ -100,6 +100,20 @@ function add_tests_in_mode(useMozillaPKIX)
   add_ocsp_test("ocsp-stapling-expired.example.com",
                 getXPCOMStatusFromNSS(SEC_ERROR_OCSP_UNKNOWN_CERT),
                 ocspResponseUnknown);
+
+  if (useMozillaPKIX) {
+    
+    
+    
+    add_ocsp_test("ocsp-stapling-ancient-valid.example.com", Cr.NS_OK,
+                  ocspResponseGood);
+    add_ocsp_test("ocsp-stapling-ancient-valid.example.com",
+                  getXPCOMStatusFromNSS(SEC_ERROR_REVOKED_CERTIFICATE),
+                  ocspResponseRevoked);
+    add_ocsp_test("ocsp-stapling-ancient-valid.example.com",
+                  getXPCOMStatusFromNSS(SEC_ERROR_OCSP_UNKNOWN_CERT),
+                  ocspResponseUnknown);
+  }
 }
 
 function check_ocsp_stapling_telemetry() {
@@ -107,10 +121,14 @@ function check_ocsp_stapling_telemetry() {
                     .getService(Ci.nsITelemetry)
                     .getHistogramById("SSL_OCSP_STAPLING")
                     .snapshot();
-  do_check_eq(histogram.counts[0], 2 * 0); // histogram bucket 0 is unused
-  do_check_eq(histogram.counts[1], 2 * 0); // 0 connections with a good response
-  do_check_eq(histogram.counts[2], 2 * 0); // 0 connections with no stapled resp.
-  do_check_eq(histogram.counts[3], 2 * 9); // 9 connections with an expired response
-  do_check_eq(histogram.counts[4], 2 * 0); // 0 connections with bad responses
+  do_check_eq(histogram.counts[0], 2 * 0); 
+  do_check_eq(histogram.counts[1], 2 * 0); 
+  do_check_eq(histogram.counts[2], 2 * 0); 
+  do_check_eq(histogram.counts[3], 2 * 9 + 3); 
+                                               
+                                               
+                                               
+                                               
+  do_check_eq(histogram.counts[4], 2 * 0); 
   run_next_test();
 }
