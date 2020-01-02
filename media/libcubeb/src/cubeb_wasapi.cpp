922ee54a2f3dd4410aafb5079676ce0491d3ad89
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-02 20:49:02 +0000

    Bug 1008079 - Extract the resampling code from cubeb_wasapi.cpp so it is reusable. r=padenot
    UltraBlame original commit: 68427e971ad648fa3b8df97aae4e764f03d9cb7c

--- media/libcubeb/src/cubeb_wasapi.cpp
+++ media/libcubeb/src/cubeb_wasapi.cpp
@@ -13,13 +13,12 @@
 #include <mmdeviceapi.h>
 #include <windef.h>
 #include <audioclient.h>
-#include <math.h>
 #include <process.h>
 #include <avrt.h>
 #include "cubeb/cubeb.h"
 #include "cubeb-internal.h"
 #include "cubeb/cubeb-stdint.h"
-#include "cubeb-speex-resampler.h"
+#include "cubeb_resampler.h"
 #include <stdio.h>
 
 #if 0
@@ -53,12 +52,6 @@ hns_to_s(uint32_t hns)
   return static_cast<double>(hns) / 10000000;
 }
 
-long
-frame_count_at_rate(long frame_count, float rate)
-{
-  return static_cast<long>(ceilf(rate * frame_count) + 1);
-}
-
 void
 SafeRelease(HANDLE handle)
 {
@@ -74,9 +67,6 @@ void SafeRelease(T * ptr)
     ptr->Release();
   }
 }
-
-typedef void (*refill_function2)(cubeb_stream * stm,
-                                float * data, long frames_needed);
 
 typedef HANDLE (WINAPI *set_mm_thread_characteristics_function)(
                                       const char* TaskName, LPDWORD TaskIndex);
@@ -123,16 +113,7 @@ struct cubeb_stream
   
   uint32_t buffer_frame_count;
   
-  SpeexResamplerState * resampler;
-  
-  float * resampling_src_buffer;
-  
-
-  refill_function2 refill_function;
-  
-  uint32_t leftover_frame_count;
-  uint32_t leftover_frame_size;
-  float * leftover_frames_buffer;
+  cubeb_resampler * resampler;
   
 
   float * mix_buffer;
@@ -213,76 +194,6 @@ frames_to_bytes_before_mix(cubeb_stream * stm, size_t frames)
 }
 
 void
-refill_with_resampling(cubeb_stream * stm, float * data, long frames_needed)
-{
-  
-
-
-  float rate =
-    static_cast<float>(stm->stream_params.rate) / stm->mix_params.rate;
-
-  long before_resampling = frame_count_at_rate(frames_needed, rate);
-
-  long frame_requested = before_resampling - stm->leftover_frame_count;
-
-  size_t leftover_bytes =
-    frames_to_bytes_before_mix(stm, stm->leftover_frame_count);
-
-  
-  memcpy(stm->resampling_src_buffer, stm->leftover_frames_buffer, leftover_bytes);
-  uint8_t * buffer_start = reinterpret_cast<uint8_t *>(
-                                  stm->resampling_src_buffer) + leftover_bytes;
-
-  long got = stm->data_callback(stm, stm->user_ptr, buffer_start, frame_requested);
-
-  if (got != frame_requested) {
-    stm->draining = true;
-  }
-
-  uint32_t in_frames = before_resampling;
-  uint32_t out_frames = frames_needed;
-
-  
-
-  float * resample_dest;
-  if (should_upmix(stm) || should_downmix(stm)) {
-    resample_dest = stm->mix_buffer;
-  } else {
-    resample_dest = data;
-  }
-
-  speex_resampler_process_interleaved_float(stm->resampler,
-                                            stm->resampling_src_buffer,
-                                            &in_frames,
-                                            resample_dest,
-                                            &out_frames);
-
-  
-  stm->leftover_frame_count = before_resampling - in_frames;
-  size_t unresampled_bytes =
-    frames_to_bytes_before_mix(stm, stm->leftover_frame_count);
-
-  uint8_t * leftover_frames_start =
-    reinterpret_cast<uint8_t *>(stm->resampling_src_buffer);
-  leftover_frames_start += frames_to_bytes_before_mix(stm, in_frames);
-
-  assert(stm->leftover_frame_count <= stm->leftover_frame_size);
-  memcpy(stm->leftover_frames_buffer, leftover_frames_start, unresampled_bytes);
-
-  
-
-  assert(out_frames == frames_needed || stm->draining);
-
-  if (should_upmix(stm)) {
-    upmix(resample_dest, out_frames, data,
-          stm->stream_params.channels, stm->mix_params.channels);
-  } else if (should_downmix(stm)) {
-    downmix(resample_dest, out_frames, data,
-            stm->stream_params.channels, stm->mix_params.channels);
-  }
-}
-
-void
 refill(cubeb_stream * stm, float * data, long frames_needed)
 {
   
@@ -294,18 +205,28 @@ refill(cubeb_stream * stm, float * data, long frames_needed)
     dest = data;
   }
 
-  long got = stm->data_callback(stm, stm->user_ptr, dest, frames_needed);
-  assert(got <= frames_needed);
-  if (got != frames_needed) {
+  long out_frames = cubeb_resampler_fill(stm->resampler, dest, frames_needed);
+
+  
+  if (out_frames < 0) {
+    assert(false);
+  }
+
+  
+  if (out_frames < frames_needed) {
     LOG("draining.");
     stm->draining = true;
   }
 
+  
+
+  assert(out_frames == frames_needed || stm->draining);
+
   if (should_upmix(stm)) {
-    upmix(dest, got, data,
+    upmix(dest, out_frames, data,
           stm->stream_params.channels, stm->mix_params.channels);
   } else if (should_downmix(stm)) {
-    downmix(dest, got, data,
+    downmix(dest, out_frames, data,
             stm->stream_params.channels, stm->mix_params.channels);
   }
 }
@@ -381,7 +302,7 @@ wasapi_stream_render_loop(LPVOID stream)
       BYTE* data;
       hr = stm->render_client->GetBuffer(available, &data);
       if (SUCCEEDED(hr)) {
-        stm->refill_function(stm, reinterpret_cast<float *>(data), available);
+        refill(stm, reinterpret_cast<float *>(data), available);
 
         hr = stm->render_client->ReleaseBuffer(available, 0);
         if (FAILED(hr)) {
@@ -774,40 +695,8 @@ wasapi_stream_init(cubeb * context, cubeb_stream ** stream,
   
 
   stm->mix_params.format = CUBEB_SAMPLE_FLOAT32NE;
-
   stm->mix_params.rate = mix_format->nSamplesPerSec;
   stm->mix_params.channels = mix_format->nChannels;
-
-  float resampling_rate = static_cast<float>(stm->stream_params.rate) /
-                          stm->mix_params.rate;
-
-  if (resampling_rate != 1.0) {
-    
-
-
-
-    stm->resampler = speex_resampler_init(stm->stream_params.channels,
-                                          stm->stream_params.rate,
-                                          stm->mix_params.rate,
-                                          SPEEX_RESAMPLER_QUALITY_DESKTOP,
-                                          NULL);
-    if (!stm->resampler) {
-      LOG("Could not get a resampler");
-      CoTaskMemFree(mix_format);
-      wasapi_stream_destroy(stm);
-      return CUBEB_ERROR;
-    }
-
-    
-
-
-    stm->leftover_frame_size = static_cast<uint32_t>(ceilf(1 / resampling_rate * 2) + 1);
-    stm->leftover_frames_buffer = (float *)malloc(frames_to_bytes_before_mix(stm, stm->leftover_frame_size));
-
-    stm->refill_function = &refill_with_resampling;
-  } else {
-    stm->refill_function = &refill;
-  }
 
   hr = stm->client->Initialize(AUDCLNT_SHAREMODE_SHARED,
                                AUDCLNT_STREAMFLAGS_EVENTCALLBACK |
@@ -836,15 +725,6 @@ wasapi_stream_init(cubeb * context, cubeb_stream ** stream,
     stm->mix_buffer = (float *) malloc(frames_to_bytes_before_mix(stm, stm->buffer_frame_count));
   }
 
-  
-
-
-
-  if (stm->resampler) {
-    size_t frames_needed = static_cast<size_t>(frame_count_at_rate(stm->buffer_frame_count, resampling_rate));
-    stm->resampling_src_buffer = (float *)malloc(frames_to_bytes_before_mix(stm, frames_needed));
-  }
-
   hr = stm->client->SetEventHandle(stm->refill_event);
   if (FAILED(hr)) {
     LOG("Could set the event handle for the client %x.", hr);
@@ -871,6 +751,23 @@ wasapi_stream_init(cubeb * context, cubeb_stream ** stream,
   hr = stm->audio_clock->GetFrequency(&stm->clock_freq);
   if (FAILED(hr)) {
     LOG("failed to get audio clock frequency, %x", hr);
+    wasapi_stream_destroy(stm);
+    return CUBEB_ERROR;
+  }
+
+  
+
+
+
+  stm->resampler = cubeb_resampler_create(stm, stream_params,
+                                          stm->mix_params.rate,
+                                          data_callback,
+                                          stm->buffer_frame_count,
+                                          user_ptr,
+                                          CUBEB_RESAMPLER_QUALITY_DESKTOP);
+  if (!stm->resampler) {
+    LOG("Could not get a resampler");
+    wasapi_stream_destroy(stm);
     return CUBEB_ERROR;
   }
 
@@ -897,12 +794,8 @@ void wasapi_stream_destroy(cubeb_stream * stm)
   SafeRelease(stm->render_client);
   SafeRelease(stm->audio_clock);
 
-  if (stm->resampler) {
-    speex_resampler_destroy(stm->resampler);
-  }
+  cubeb_resampler_destroy(stm->resampler);
 
-  free(stm->leftover_frames_buffer);
-  free(stm->resampling_src_buffer);
   free(stm->mix_buffer);
   free(stm);
   CoUninitialize();
