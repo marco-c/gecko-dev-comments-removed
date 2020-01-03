6d1552af7a3d4137a0bfb68e5a45d492366a21e9
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-03 13:35:52 +0000

    Bug 1131871 - auto_com should only uninitialize when successful. r=padenot
    UltraBlame original commit: 4cea294700c1efb42f2e5a93644839b0e81c1052

--- media/libcubeb/src/cubeb_wasapi.cpp
+++ media/libcubeb/src/cubeb_wasapi.cpp
@@ -164,7 +164,7 @@ private:
 
 struct auto_com {
   auto_com()
-  : need_uninit(true) {
+  : need_uninit(false) {
     HRESULT hr = CoInitializeEx(NULL, COINIT_MULTITHREADED);
     
     
@@ -172,14 +172,15 @@ struct auto_com {
       
       
       LOG("COM already initialized in STA.");
-      need_uninit = false;
     } else if (hr == S_FALSE) {
       
       
       
       LOG("COM already initialized in MTA");
+      need_uninit = true;
     } else if (hr == S_OK) {
       LOG("COM initialized.");
+      need_uninit = true;
     }
   }
   ~auto_com() {
