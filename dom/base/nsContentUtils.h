bc5b662d523bec54216ea3426b71a9bc30c47b43
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-03 23:29:32 +0000

    Backed out changeset 4a8f45990368 (bug 1185640) for causing serviceworker w-p-t permacrashes. CLOSED TREE
    UltraBlame original commit: 26e1446feb64addb08838a55ef76e46b0c9768eb

--- dom/base/nsContentUtils.h
+++ dom/base/nsContentUtils.h
@@ -1680,10 +1680,14 @@ public:
   
 
 
+
+
   static void ASCIIToLower(nsAString& aStr);
   static void ASCIIToLower(const nsAString& aSource, nsAString& aDest);
 
   
+
+
 
 
   static void ASCIIToUpper(nsAString& aStr);
