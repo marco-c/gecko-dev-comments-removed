d5cb43138f23b2549799a0c480cc72513bf43fb0
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-03 23:34:12 +0000

    Bug 1171929 - When sharing selected text, also include the current page URL, r=margaret
    UltraBlame original commit: f61d28c21d803613a60b5ccebda0956435d2111e

--- mobile/android/base/GeckoApp.java
+++ mobile/android/base/GeckoApp.java
@@ -631,7 +631,14 @@ public abstract class GeckoApp
 
         } else if ("Share:Text".equals(event)) {
             String text = message.getString("text");
-            GeckoAppShell.openUriExternal(text, "text/plain", "", "", Intent.ACTION_SEND, "");
+            final Tab tab = Tabs.getInstance().getSelectedTab();
+            String title = "";
+            if (tab != null) {
+                title = tab.getDisplayTitle();
+                final String url = ReaderModeUtils.stripAboutReaderUrl(tab.getURL());
+                text += "\n\n" + url;
+            }
+            GeckoAppShell.openUriExternal(text, "text/plain", "", "", Intent.ACTION_SEND, title);
 
             
             Telemetry.sendUIEvent(TelemetryContract.Event.SHARE, TelemetryContract.Method.LIST);
