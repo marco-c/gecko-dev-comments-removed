9804c457c03cc172ee1f75d2f510c77ad32ecac0
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-03 23:44:07 +0000

    Bug 1194753 - Wait longer when decoding the first frame of D3D9 DXVA video before deciding it is invalid. r=cpearce
    UltraBlame original commit: 6539747394d952cac60052a788ac398564492689

--- gfx/layers/D3D9SurfaceImage.h
+++ gfx/layers/D3D9SurfaceImage.h
@@ -1,7 +1,7 @@
-
-
-
-
+/* -*- Mode: C++; tab-width: 20; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #ifndef GFX_D3DSURFACEIMAGE_H
 #define GFX_D3DSURFACEIMAGE_H
@@ -41,35 +41,38 @@ protected:
   RefPtr<IDirect3DDevice9> mDevice;
 };
 
-
-
-
-
+// Image class that wraps a IDirect3DSurface9. This class copies the image
+// passed into SetData(), so that it can be accessed from other D3D devices.
+// This class also manages the synchronization of the copy, to ensure the
+// resource is ready to use.
 class D3D9SurfaceImage : public Image {
 public:
 
   struct Data {
     Data(IDirect3DSurface9* aSurface,
          const gfx::IntRect& aRegion,
-         D3D9RecycleAllocator* aAllocator)
+         D3D9RecycleAllocator* aAllocator,
+         bool aIsFirstFrame)
       : mSurface(aSurface)
       , mRegion(aRegion)
       , mAllocator(aAllocator)
+      , mIsFirstFrame(aIsFirstFrame)
     {}
 
     RefPtr<IDirect3DSurface9> mSurface;
     gfx::IntRect mRegion;
     RefPtr<D3D9RecycleAllocator> mAllocator;
+    bool mIsFirstFrame;
   };
 
   D3D9SurfaceImage();
   virtual ~D3D9SurfaceImage();
 
-  
-  
+  // Copies the surface into a sharable texture's surface, and initializes
+  // the image.
   HRESULT SetData(const Data& aData);
 
-  
+  // Returns the description of the shared surface.
   const D3DSURFACE_DESC& GetDesc() const;
 
   gfx::IntSize GetSize() override;
@@ -82,17 +85,18 @@ public:
 
 private:
 
-  
-  
+  // Blocks the calling thread until the copy operation started in SetData()
+  // is complete, whereupon the texture is safe to use.
   void EnsureSynchronized();
 
   gfx::IntSize mSize;
   RefPtr<IDirect3DQuery9> mQuery;
   RefPtr<SharedTextureClientD3D9> mTextureClient;
   bool mValid;
+  bool mIsFirstFrame;
 };
 
-} 
-} 
+} // namepace layers
+} // namespace mozilla
 
-#endif 
+#endif // GFX_D3DSURFACEIMAGE_H
