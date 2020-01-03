c90dce4827043e4d6c14099e51e77cd140dd829b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-03 04:55:08 +0000

    Bug 1096016 - Fix post barrier insertion for MStoreUnboxedObjectOrNull, r=nmatsakis.
    UltraBlame original commit: 74628362296707a89dbcc7c02dd67022dd4609e9

--- js/src/jit/TypePolicy.h
+++ js/src/jit/TypePolicy.h
@@ -149,19 +149,6 @@ class ConvertToStringPolicy : public TypePolicy
 };
 
 
-
-template <unsigned Op>
-class ConvertToObjectOrNullPolicy : public TypePolicy
-{
-  public:
-    EMPTY_DATA_;
-    static bool staticAdjustInputs(TempAllocator &alloc, MInstruction *def);
-    bool adjustInputs(TempAllocator &alloc, MInstruction *def) {
-        return staticAdjustInputs(alloc, def);
-    }
-};
-
-
 template <unsigned Op>
 class IntPolicy : public BoxInputsPolicy
 {
@@ -395,6 +382,13 @@ class StoreTypedArrayElementStaticPolicy : public StoreTypedArrayPolicy
   public:
     EMPTY_DATA_;
     bool adjustInputs(TempAllocator &alloc, MInstruction *ins);
+};
+
+class StoreUnboxedObjectOrNullPolicy : public TypePolicy
+{
+  public:
+    EMPTY_DATA_;
+    bool adjustInputs(TempAllocator &alloc, MInstruction *def);
 };
 
 
