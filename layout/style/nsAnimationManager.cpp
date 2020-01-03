f13a4f3da1b2dbb88a00ac43bd1e5535d4021039
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-03 23:10:19 +0000

    Bug 1180125 part 3 - Extract DelayedEventDispatcher; r=dbaron
    This patch extracts a utility class for queueing up a series of EventInfo
    objects (of templated type) and then dispatching them. This covers the event
    queuing behavior in nsAnimationManager so that we can reuse it in
    nsTransitionManager.
    
    UltraBlame original commit: d9549506b9cffa8337ad44eb32e8fe3105ac1b48

--- layout/style/nsAnimationManager.cpp
+++ layout/style/nsAnimationManager.cpp
@@ -7,7 +7,6 @@
 #include "nsTransitionManager.h"
 #include "mozilla/dom/CSSAnimationBinding.h"
 
-#include "mozilla/EventDispatcher.h"
 #include "mozilla/MemoryReporting.h"
 #include "mozilla/StyleAnimationValue.h"
 #include "mozilla/dom/DocumentTimeline.h"
@@ -251,10 +250,9 @@ CSSAnimation::QueueEvents()
     StickyTimeDuration elapsedTime =
       std::min(StickyTimeDuration(mEffect->InitialAdvance()),
                computedTiming.mActiveDuration);
-    AnimationEventInfo ei(owningElement, mAnimationName, NS_ANIMATION_START,
-                          elapsedTime,
-                          PseudoTypeAsString(owningPseudoType));
-    manager->QueueEvent(ei);
+    manager->QueueEvent(
+      AnimationEventInfo(owningElement, mAnimationName, NS_ANIMATION_START,
+                         elapsedTime, PseudoTypeAsString(owningPseudoType)));
     
     message = NS_ANIMATION_END;
   } else {
@@ -274,9 +272,9 @@ CSSAnimation::QueueEvents()
     elapsedTime = computedTiming.mActiveDuration;
   }
 
-  AnimationEventInfo ei(owningElement, mAnimationName, message, elapsedTime,
-                        PseudoTypeAsString(owningPseudoType));
-  manager->QueueEvent(ei);
+  manager->QueueEvent(
+    AnimationEventInfo(owningElement, mAnimationName, message, elapsedTime,
+                       PseudoTypeAsString(owningPseudoType)));
 }
 
 CommonAnimationManager*
@@ -305,15 +303,7 @@ CSSAnimation::PseudoTypeAsString(nsCSSPseudoElements::Type aPseudoType)
 
 
 
-NS_IMPL_CYCLE_COLLECTION_CLASS(nsAnimationManager)
-NS_IMPL_CYCLE_COLLECTION_UNLINK_BEGIN(nsAnimationManager)
-  tmp->mPendingEvents.Clear();
-NS_IMPL_CYCLE_COLLECTION_UNLINK_END
-NS_IMPL_CYCLE_COLLECTION_TRAVERSE_BEGIN(nsAnimationManager)
-  for (AnimationEventInfo& info : tmp->mPendingEvents) {
-    ImplCycleCollectionTraverse(cb, info.mElement, "mPendingEvents.mElement");
-  }
-NS_IMPL_CYCLE_COLLECTION_TRAVERSE_END
+NS_IMPL_CYCLE_COLLECTION(nsAnimationManager, mEventDispatcher)
 
 NS_IMPL_CYCLE_COLLECTING_ADDREF(nsAnimationManager)
 NS_IMPL_CYCLE_COLLECTING_RELEASE(nsAnimationManager)
@@ -541,17 +531,11 @@ nsAnimationManager::CheckAnimationRule(nsStyleContext* aStyleContext,
   
   
   
-  if (!mPendingEvents.IsEmpty()) {
+  if (mEventDispatcher.HasQueuedEvents()) {
     mPresContext->Document()->SetNeedStyleFlush();
   }
 
   return GetAnimationRule(aElement, aStyleContext->GetPseudoType());
-}
-
-void
-nsAnimationManager::QueueEvent(AnimationEventInfo& aEventInfo)
-{
-  mPendingEvents.AppendElement(aEventInfo);
 }
 
 struct KeyframeData {
@@ -1021,20 +1005,4 @@ nsAnimationManager::FlushAnimations(FlushFlags aFlags)
   }
 
   MaybeStartOrStopObservingRefreshDriver();
-}
-
-void
-nsAnimationManager::DoDispatchEvents()
-{
-  EventArray events;
-  mPendingEvents.SwapElements(events);
-  
-  for (uint32_t i = 0, i_end = events.Length(); i < i_end; ++i) {
-    AnimationEventInfo &info = events[i];
-    EventDispatcher::Dispatch(info.mElement, mPresContext, &info.mEvent);
-
-    if (!mPresContext) {
-      break;
-    }
-  }
 }
