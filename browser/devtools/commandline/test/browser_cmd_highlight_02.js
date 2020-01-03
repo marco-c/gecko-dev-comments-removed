4cbd97290d9a5931869fb47ffcc14c27c2be1b56
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-03 22:52:13 +0000

    Bug 1188417 - Split gcli commands require path to match file system. r=jwalker
    UltraBlame original commit: 34e2362a1730fc91edc0be3c349d0f7e9bce24c0

--- browser/devtools/commandline/test/browser_cmd_highlight_02.js
+++ browser/devtools/commandline/test/browser_cmd_highlight_02.js
@@ -36,7 +36,7 @@ function test() {
 function getHighlighterNumber() {
   
   
-  return require("gcli/commands/highlight").highlighters.length;
+  return require("devtools/toolkit/gcli/commands/highlight").highlighters.length;
 }
 
 function* runCommand(cmd, options) {
