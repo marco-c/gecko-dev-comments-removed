5545c087efe7b6200463bcc5163ffbdaaa04a050
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-03 23:43:07 +0000

    Bug 1186273 - Part 1. Move preferences and observers into dedicated threadsafe module. r=dhylands
    UltraBlame original commit: b855bb08a22bfff7ab939c0418ae265380ab8e5e

--- dom/ipc/ContentChild.cpp
+++ dom/ipc/ContentChild.cpp
@@ -842,9 +842,6 @@ ContentChild::InitXPCOM()
         global->SetInitialProcessData(data);
     }
 
-    DebugOnly<FileUpdateDispatcher*> observer = FileUpdateDispatcher::GetSingleton();
-    NS_ASSERTION(observer, "FileUpdateDispatcher is null");
-
     
     nsRefPtr<SystemMessageHandledObserver> sysMsgObserver =
         new SystemMessageHandledObserver();
