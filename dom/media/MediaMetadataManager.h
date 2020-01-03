18291acbe9f8a3b976a1af16c0fdc95ca31d59cb
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-03 23:26:23 +0000

    Bug 1193603. Part 1 - Fix indentation and include order. r=jya.
    UltraBlame original commit: 6a1d0468f6d258da2d8ff0abd24e81dd2f3d8f17

--- dom/media/MediaMetadataManager.h
+++ dom/media/MediaMetadataManager.h
@@ -6,63 +6,68 @@
 
 #if !defined(MediaMetadataManager_h__)
 #define MediaMetadataManager_h__
-#include "VideoUtils.h"
+
 #include "mozilla/LinkedList.h"
-#include "AbstractMediaDecoder.h"
+
 #include "nsAutoPtr.h"
+#include "AbstractMediaDecoder.h"
+#include "VideoUtils.h"
 
 namespace mozilla {
 
-  
-  
-  class TimedMetadata : public LinkedListElement<TimedMetadata> {
-    public:
-      
-      int64_t mPublishTime;
-      
-      
-      nsAutoPtr<MetadataTags> mTags;
-      
-      
-      
-      nsAutoPtr<MediaInfo> mInfo;
-  };
 
+
+class TimedMetadata : public LinkedListElement<TimedMetadata> {
+public:
+  
+  int64_t mPublishTime;
   
   
-  class MediaMetadataManager
-  {
-    public:
-      ~MediaMetadataManager() {
-        TimedMetadata* element;
-        while((element = mMetadataQueue.popFirst()) != nullptr) {
-          delete element;
-        }
-      }
-      void QueueMetadata(TimedMetadata* aMetadata) {
-        mMetadataQueue.insertBack(aMetadata);
-      }
+  nsAutoPtr<MetadataTags> mTags;
+  
+  
+  
+  nsAutoPtr<MediaInfo> mInfo;
+};
 
-      void DispatchMetadataIfNeeded(AbstractMediaDecoder* aDecoder, double aCurrentTime) {
-        TimedMetadata* metadata = mMetadataQueue.getFirst();
-        while (metadata && aCurrentTime >= static_cast<double>(metadata->mPublishTime) / USECS_PER_S) {
-          
-          nsCOMPtr<nsIRunnable> removeTracksEvent =
-            new RemoveMediaTracksEventRunner(aDecoder);
-          NS_DispatchToMainThread(removeTracksEvent);
 
-          nsCOMPtr<nsIRunnable> metadataUpdatedEvent =
-            new MetadataUpdatedEventRunner(aDecoder,
-                                           metadata->mInfo,
-                                           metadata->mTags);
-          NS_DispatchToMainThread(metadataUpdatedEvent);
-          delete mMetadataQueue.popFirst();
-          metadata = mMetadataQueue.getFirst();
-        }
-      }
-    protected:
-      LinkedList<TimedMetadata> mMetadataQueue;
-  };
+
+class MediaMetadataManager
+{
+public:
+  ~MediaMetadataManager() {
+    TimedMetadata* element;
+    while((element = mMetadataQueue.popFirst()) != nullptr) {
+      delete element;
+    }
+  }
+
+  void QueueMetadata(TimedMetadata* aMetadata) {
+    mMetadataQueue.insertBack(aMetadata);
+  }
+
+  void DispatchMetadataIfNeeded(AbstractMediaDecoder* aDecoder, double aCurrentTime) {
+    TimedMetadata* metadata = mMetadataQueue.getFirst();
+    while (metadata && aCurrentTime >= static_cast<double>(metadata->mPublishTime) / USECS_PER_S) {
+      
+      nsCOMPtr<nsIRunnable> removeTracksEvent =
+        new RemoveMediaTracksEventRunner(aDecoder);
+      NS_DispatchToMainThread(removeTracksEvent);
+
+      nsCOMPtr<nsIRunnable> metadataUpdatedEvent =
+        new MetadataUpdatedEventRunner(aDecoder,
+                                       metadata->mInfo,
+                                       metadata->mTags);
+      NS_DispatchToMainThread(metadataUpdatedEvent);
+      delete mMetadataQueue.popFirst();
+      metadata = mMetadataQueue.getFirst();
+    }
+  }
+
+protected:
+  LinkedList<TimedMetadata> mMetadataQueue;
+};
+
 } 
 
 #endif
