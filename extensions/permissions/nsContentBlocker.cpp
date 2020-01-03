ffcb0bad01344922d21162f1119211d27119b7f0
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-03 23:04:34 +0000

    Bug 1191107 - Split TYPE_XMLHTTPREQUEST and TYPE_DATAREQUEST for EventSource - IGNORE IDL (r=sicking,ehsan)
    UltraBlame original commit: c5f710ed8718c44cee5853813a1f3b988317f449

--- extensions/permissions/nsContentBlocker.cpp
+++ extensions/permissions/nsContentBlocker.cpp
@@ -55,6 +55,8 @@ static const char *kTypeString[] = {
                                     "", 
                                     "", 
                                     "", 
+                                    "", 
+                                    "", 
                                     ""  
 };
 
