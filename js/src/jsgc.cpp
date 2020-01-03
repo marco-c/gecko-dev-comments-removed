7c95ea952ed5ce92420532ee18888cd3eed9fbd0
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-03 22:41:58 +0000

    Bug 1188408 - Simplify ArenasToUpdate iteration r=terrence
    UltraBlame original commit: 358049c21e45d148693d264c851445f594cc0d60

--- js/src/jsgc.cpp
+++ js/src/jsgc.cpp
@@ -2346,18 +2346,18 @@ struct ArenasToUpdate
         ALL = FOREGROUND | BACKGROUND
     };
     ArenasToUpdate(Zone* zone, KindsToUpdate kinds);
-    bool done() { return initialized && arena == nullptr; }
-    ArenaHeader* next(AutoLockHelperThreadState& lock);
+    bool done() { return kind == AllocKind::LIMIT; }
     ArenaHeader* getArenasToUpdate(AutoLockHelperThreadState& lock, unsigned max);
 
   private:
-    bool initialized;
-    KindsToUpdate kinds;
+    KindsToUpdate kinds; 
     Zone* zone;          
     AllocKind kind;      
     ArenaHeader* arena;  
 
+    AllocKind nextAllocKind(AllocKind i) { return AllocKind(uint8_t(i) + 1); }
     bool shouldProcessKind(AllocKind kind);
+    ArenaHeader* next(AutoLockHelperThreadState& lock);
 };
 
 bool ArenasToUpdate::shouldProcessKind(AllocKind kind)
@@ -2389,7 +2389,7 @@ bool ArenasToUpdate::shouldProcessKind(AllocKind kind)
 }
 
 ArenasToUpdate::ArenasToUpdate(Zone* zone, KindsToUpdate kinds)
-  : initialized(false), kinds(kinds), zone(zone)
+  : kinds(kinds), zone(zone), kind(AllocKind::FIRST), arena(nullptr)
 {
     MOZ_ASSERT(zone->isGCCompacting());
     MOZ_ASSERT(kinds && !(kinds & ~ALL));
@@ -2403,41 +2403,27 @@ ArenasToUpdate::next(AutoLockHelperThreadState& lock)
     
     
     
-    
 
-    if (initialized) {
-        MOZ_ASSERT(arena);
-        MOZ_ASSERT(shouldProcessKind(kind));
-        MOZ_ASSERT(zone);
-        arena = arena->next;
-        if (arena)
-            return arena;
-        kind = AllocKind(uint8_t(kind) + 1);
-    } else {
-        initialized = true;
-        arena = nullptr;
-        kind = AllocKind::FIRST;
-    }
-
-    for (auto i : SomeAllocKinds(kind)) {
-        if (shouldProcessKind(i)) {
-            arena = zone->arenas.getFirstArena(i);
-            if (arena) {
-                kind = i;
+    for (; kind < AllocKind::LIMIT; kind = nextAllocKind(kind)) {
+        if (shouldProcessKind(kind)) {
+            if (!arena)
+                arena = zone->arenas.getFirstArena(kind);
+            else
+                arena = arena->next;
+            if (arena)
                 return arena;
-            }
         }
     }
 
-    kind = AllocKind::LIMIT;
-    zone = nullptr;
+    MOZ_ASSERT(!arena);
+    MOZ_ASSERT(done());
     return nullptr;
 }
 
 ArenaHeader*
 ArenasToUpdate::getArenasToUpdate(AutoLockHelperThreadState& lock, unsigned count)
 {
-    if (!zone)
+    if (done())
         return nullptr;
 
     ArenaHeader* head = nullptr;
