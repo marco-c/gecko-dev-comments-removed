849103d87409773395fb069167e523c1bce647a7
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-03 22:23:30 +0000

    Bug 1187197 (part 2) - Put function return types on their own line. r=froydnj.
    UltraBlame original commit: ca5ac85253ff10b6f21ee2120cf1fb9b1c8bea4d

--- xpcom/glue/pldhash.h
+++ xpcom/glue/pldhash.h
@@ -573,7 +573,8 @@ struct PLDHashTableOps
 
 
 
-PLDHashNumber PL_DHashStringKey(PLDHashTable* aTable, const void* aKey);
+PLDHashNumber
+PL_DHashStringKey(PLDHashTable* aTable, const void* aKey);
 
 
 struct PLDHashEntryStub : public PLDHashEntryHdr
@@ -581,29 +582,34 @@ struct PLDHashEntryStub : public PLDHashEntryHdr
   const void* key;
 };
 
-PLDHashNumber PL_DHashVoidPtrKeyStub(PLDHashTable* aTable, const void* aKey);
+PLDHashNumber
+PL_DHashVoidPtrKeyStub(PLDHashTable* aTable, const void* aKey);
 
-bool PL_DHashMatchEntryStub(PLDHashTable* aTable,
-                            const PLDHashEntryHdr* aEntry,
-                            const void* aKey);
+bool
+PL_DHashMatchEntryStub(PLDHashTable* aTable,
+                       const PLDHashEntryHdr* aEntry,
+                       const void* aKey);
 
-bool PL_DHashMatchStringKey(PLDHashTable* aTable,
-                            const PLDHashEntryHdr* aEntry,
-                            const void* aKey);
+bool
+PL_DHashMatchStringKey(PLDHashTable* aTable,
+                       const PLDHashEntryHdr* aEntry,
+                       const void* aKey);
 
 void
 PL_DHashMoveEntryStub(PLDHashTable* aTable,
                       const PLDHashEntryHdr* aFrom,
                       PLDHashEntryHdr* aTo);
 
-void PL_DHashClearEntryStub(PLDHashTable* aTable, PLDHashEntryHdr* aEntry);
+void
+PL_DHashClearEntryStub(PLDHashTable* aTable, PLDHashEntryHdr* aEntry);
 
 
 
 
 
 
-const PLDHashTableOps* PL_DHashGetStubOps(void);
+const PLDHashTableOps*
+PL_DHashGetStubOps(void);
 
 
 
@@ -621,20 +627,24 @@ PL_DHashTableAdd(PLDHashTable* aTable, const void* aKey);
 void PL_DHASH_FASTCALL
 PL_DHashTableRemove(PLDHashTable* aTable, const void* aKey);
 
-void PL_DHashTableRawRemove(PLDHashTable* aTable, PLDHashEntryHdr* aEntry);
+void
+PL_DHashTableRawRemove(PLDHashTable* aTable, PLDHashEntryHdr* aEntry);
 
-size_t PL_DHashTableSizeOfExcludingThis(
+size_t
+PL_DHashTableSizeOfExcludingThis(
   const PLDHashTable* aTable,
   PLDHashSizeOfEntryExcludingThisFun aSizeOfEntryExcludingThis,
   mozilla::MallocSizeOf aMallocSizeOf, void* aArg = nullptr);
 
-size_t PL_DHashTableSizeOfIncludingThis(
+size_t
+PL_DHashTableSizeOfIncludingThis(
   const PLDHashTable* aTable,
   PLDHashSizeOfEntryExcludingThisFun aSizeOfEntryExcludingThis,
   mozilla::MallocSizeOf aMallocSizeOf, void* aArg = nullptr);
 
 #ifdef DEBUG
-void PL_DHashMarkTableImmutable(PLDHashTable* aTable);
+void
+PL_DHashMarkTableImmutable(PLDHashTable* aTable);
 #endif
 
 #endif 
