fa2958c9ead610f23d5ac236c2b90fca027174d9
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-03 23:05:11 +0000

    Bug 1190669 part 4 - Force stop transition animation when the cocoa window gets destroyed. r=smichaud
    UltraBlame original commit: 70a60348507ef8af0e6eee965cfc183b8ed0e2bf

--- widget/cocoa/nsCocoaWindow.h
+++ widget/cocoa/nsCocoaWindow.h
@@ -296,6 +296,14 @@ public:
       bool aFullScreen, nsIScreen* aTargetScreen = nullptr) override final;
     NS_IMETHOD MakeFullScreenWithNativeTransition(
       bool aFullScreen, nsIScreen* aTargetScreen = nullptr) override final;
+    NSAnimation* FullscreenTransitionAnimation() const { return mFullscreenTransitionAnimation; }
+    void ReleaseFullscreenTransitionAnimation()
+    {
+      MOZ_ASSERT(mFullscreenTransitionAnimation,
+                 "Should only be called when there is animation");
+      [mFullscreenTransitionAnimation release];
+      mFullscreenTransitionAnimation = nil;
+    }
 
     NS_IMETHOD              Resize(double aWidth, double aHeight, bool aRepaint) override;
     NS_IMETHOD              Resize(double aX, double aY, double aWidth, double aHeight, bool aRepaint) override;
@@ -414,6 +422,9 @@ protected:
   nsRefPtr<nsMenuBarX> mMenuBar;
   NSWindow*            mSheetWindowParent; 
   nsChildView*         mPopupContentView; 
+  
+  
+  NSAnimation*         mFullscreenTransitionAnimation;
   int32_t              mShadowStyle;
 
   CGFloat              mBackingScaleFactor;
