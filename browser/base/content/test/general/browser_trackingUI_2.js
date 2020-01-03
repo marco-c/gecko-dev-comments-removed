9233c419e470e8634e263b8eab7cee161529e9c9
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-03 22:17:48 +0000

    Bug 1175858 - Style updates for browser tracking protection tests;r=MattN
    UltraBlame original commit: 31be3956f347dac0643f0124f7ac7169b9cdc5ce

--- browser/base/content/test/general/browser_trackingUI_2.js
+++ browser/base/content/test/general/browser_trackingUI_2.js
@@ -4,20 +4,18 @@
 
 
 
-
-
 const {classes: Cc, interfaces: Ci, utils: Cu, results: Cr} = Components;
 const PREF = "privacy.trackingprotection.enabled";
 const PB_PREF = "privacy.trackingprotection.pbmode.enabled";
 const BENIGN_PAGE = "http://tracking.example.org/browser/browser/base/content/test/general/benignPage.html";
 const TRACKING_PAGE = "http://tracking.example.org/browser/browser/base/content/test/general/trackingPage.html";
 let TrackingProtection = null;
-let browser = null;
+let tabbrowser = null;
 
 let {UrlClassifierTestUtils} = Cu.import("resource://testing-common/UrlClassifierTestUtils.jsm", {});
 
 registerCleanupFunction(function() {
-  TrackingProtection = browser = null;
+  TrackingProtection = tabbrowser = null;
   UrlClassifierTestUtils.cleanupTestTrackers();
   Services.prefs.clearUserPref(PREF);
   Services.prefs.clearUserPref(PB_PREF);
@@ -29,52 +27,52 @@ registerCleanupFunction(function() {
 add_task(function* testNormalBrowsing() {
   yield UrlClassifierTestUtils.addTestTrackers();
 
-  browser = gBrowser;
-  let tab = browser.selectedTab = browser.addTab();
+  tabbrowser = gBrowser;
+  let tab = tabbrowser.selectedTab = tabbrowser.addTab();
 
-  TrackingProtection = browser.ownerGlobal.TrackingProtection;
-  ok (TrackingProtection, "TP is attached to the browser window");
-  is (TrackingProtection.enabled, Services.prefs.getBoolPref(PREF),
+  TrackingProtection = tabbrowser.ownerGlobal.TrackingProtection;
+  ok(TrackingProtection, "TP is attached to the browser window");
+  is(TrackingProtection.enabled, Services.prefs.getBoolPref(PREF),
     "TP.enabled is based on the original pref value");
 
   Services.prefs.setBoolPref(PREF, true);
-  ok (TrackingProtection.enabled, "TP is enabled after setting the pref");
+  ok(TrackingProtection.enabled, "TP is enabled after setting the pref");
 
   Services.prefs.setBoolPref(PREF, false);
-  ok (!TrackingProtection.enabled, "TP is disabled after setting the pref");
+  ok(!TrackingProtection.enabled, "TP is disabled after setting the pref");
 
   info("Load a test page containing tracking elements");
   yield promiseTabLoadEvent(tab, TRACKING_PAGE);
-  ok (TrackingProtection.container.hidden, "The container is hidden");
+  ok(TrackingProtection.container.hidden, "The container is hidden");
 
   info("Load a test page not containing tracking elements");
   yield promiseTabLoadEvent(tab, BENIGN_PAGE);
-  ok (TrackingProtection.container.hidden, "The container is hidden");
+  ok(TrackingProtection.container.hidden, "The container is hidden");
 });
 
 add_task(function* testPrivateBrowsing() {
   let privateWin = yield promiseOpenAndLoadWindow({private: true}, true);
-  browser = privateWin.gBrowser;
-  let tab = browser.selectedTab = browser.addTab();
+  tabbrowser = privateWin.gBrowser;
+  let tab = tabbrowser.selectedTab = tabbrowser.addTab();
 
-  TrackingProtection = browser.ownerGlobal.TrackingProtection;
-  ok (TrackingProtection, "TP is attached to the private window");
-  is (TrackingProtection.enabled, Services.prefs.getBoolPref(PB_PREF),
+  TrackingProtection = tabbrowser.ownerGlobal.TrackingProtection;
+  ok(TrackingProtection, "TP is attached to the private window");
+  is(TrackingProtection.enabled, Services.prefs.getBoolPref(PB_PREF),
     "TP.enabled is based on the pb pref value");
 
   Services.prefs.setBoolPref(PB_PREF, true);
-  ok (TrackingProtection.enabled, "TP is enabled after setting the pref");
+  ok(TrackingProtection.enabled, "TP is enabled after setting the pref");
 
   Services.prefs.setBoolPref(PB_PREF, false);
-  ok (!TrackingProtection.enabled, "TP is disabled after setting the pref");
+  ok(!TrackingProtection.enabled, "TP is disabled after setting the pref");
 
   info("Load a test page containing tracking elements");
   yield promiseTabLoadEvent(tab, TRACKING_PAGE);
-  ok (TrackingProtection.container.hidden, "The container is hidden");
+  ok(TrackingProtection.container.hidden, "The container is hidden");
 
   info("Load a test page not containing tracking elements");
   yield promiseTabLoadEvent(tab, BENIGN_PAGE);
-  ok (TrackingProtection.container.hidden, "The container is hidden");
+  ok(TrackingProtection.container.hidden, "The container is hidden");
 
   privateWin.close();
 });
