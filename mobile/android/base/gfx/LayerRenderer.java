e9639ba3176d239865df8568b238a4af74f3762f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-03 23:45:04 +0000

    Bug 1180295 - Remove the margins information from ImmutableViewportMetrics. r=rbarker
    UltraBlame original commit: 5078e84222d716b392eb06b10b67a976f7d8f169

--- mobile/android/base/gfx/LayerRenderer.java
+++ mobile/android/base/gfx/LayerRenderer.java
@@ -323,22 +323,22 @@ public class LayerRenderer implements Tabs.OnTabsChangedListener {
         return pixelBuffer;
     }
 
-    private RenderContext createScreenContext(ImmutableViewportMetrics metrics, PointF offset) {
+    private RenderContext createScreenContext(ImmutableViewportMetrics metrics) {
         RectF viewport = new RectF(0.0f, 0.0f, metrics.getWidth(), metrics.getHeight());
         RectF pageRect = metrics.getPageRect();
 
-        return createContext(viewport, pageRect, 1.0f, offset);
+        return createContext(viewport, pageRect, 1.0f);
     }
 
-    private RenderContext createPageContext(ImmutableViewportMetrics metrics, PointF offset) {
+    private RenderContext createPageContext(ImmutableViewportMetrics metrics) {
         RectF viewport = metrics.getViewport();
         RectF pageRect = metrics.getPageRect();
         float zoomFactor = metrics.zoomFactor;
 
-        return createContext(new RectF(RectUtils.round(viewport)), pageRect, zoomFactor, offset);
+        return createContext(new RectF(RectUtils.round(viewport)), pageRect, zoomFactor);
     }
 
-    private RenderContext createContext(RectF viewport, RectF pageRect, float zoomFactor, PointF offset) {
+    private RenderContext createContext(RectF viewport, RectF pageRect, float zoomFactor) {
         if (mCoordBuffer == null) {
             
             
@@ -349,7 +349,7 @@ public class LayerRenderer implements Tabs.OnTabsChangedListener {
                 throw new IllegalStateException();
             }
         }
-        return new RenderContext(viewport, pageRect, zoomFactor, offset,
+        return new RenderContext(viewport, pageRect, zoomFactor,
                                  mPositionHandle, mTextureHandle, mCoordBuffer);
     }
 
@@ -435,16 +435,14 @@ public class LayerRenderer implements Tabs.OnTabsChangedListener {
         private boolean mUpdated;
         private final Rect mPageRect;
         private final Rect mAbsolutePageRect;
-        private final PointF mRenderOffset;
 
         public Frame(ImmutableViewportMetrics metrics) {
             mFrameMetrics = metrics;
 
             
             Layer rootLayer = mView.getLayerClient().getRoot();
-            mRenderOffset = mFrameMetrics.getMarginOffset();
-            mPageContext = createPageContext(metrics, mRenderOffset);
-            mScreenContext = createScreenContext(metrics, mRenderOffset);
+            mPageContext = createPageContext(metrics);
+            mScreenContext = createScreenContext(metrics);
 
             RectF pageRect = mFrameMetrics.getPageRect();
             mAbsolutePageRect = RectUtils.round(pageRect);
@@ -582,8 +580,8 @@ public class LayerRenderer implements Tabs.OnTabsChangedListener {
             
             
             
-            final float viewLeft = context.viewport.left - context.offset.x;
-            final float viewTop = context.viewport.top - context.offset.y;
+            final float viewLeft = context.viewport.left;
+            final float viewTop = context.viewport.top;
             boolean shouldWaitToRender = false;
 
             if (Math.abs(mLastViewLeft - viewLeft) > MAX_SCROLL_SPEED_TO_REQUEST_ZOOM_RENDER ||
