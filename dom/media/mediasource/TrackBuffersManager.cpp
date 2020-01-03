891650d4d05b82778801c7cafb8fd3b796c26e2f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-03 23:12:46 +0000

    Bug 1191142: [MSE] P2. Round removed buffered intervals. r=gerald
    Calculating intersections between two interval sets with different fuzz can lead to useless tiny intervals which could cause CheckNextInsertionIndex to fail.
    
    UltraBlame original commit: 60acd4d4e716b9797e7221c9f99270108e778499

--- dom/media/mediasource/TrackBuffersManager.cpp
+++ dom/media/mediasource/TrackBuffersManager.cpp
@@ -1651,6 +1651,7 @@ TrackBuffersManager::RemoveFrames(const TimeIntervals& aIntervals,
   }
 
   
+  removedIntervals.SetFuzz(TimeUnit::FromMicroseconds(maxSampleDuration/2));
   aTrackData.mBufferedRanges -= removedIntervals;
 
   data.RemoveElementsAt(firstRemovedIndex.ref(),
