382ece9bc19fcd8b31294dab510a25ef045a63ab
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-03 23:41:23 +0000

    Bug 1194497 - Convert a few remaining PRUnichar to char16_t. r=roc
    UltraBlame original commit: 931ac2c4036fae56265485f519885a5c82fa82be

--- netwerk/streamconv/converters/nsIndexedToHTML.cpp
+++ netwerk/streamconv/converters/nsIndexedToHTML.cpp
@@ -699,7 +699,7 @@ nsIndexedToHTML::OnIndexAvailable(nsIRequest *aRequest,
 
     
     loc.Truncate(nsUnescapeCount(loc.BeginWriting()));
-    if (loc.First() == PRUnichar('.'))
+    if (loc.First() == char16_t('.'))
         pushBuffer.AppendLiteral(" class=\"hidden-object\"");
 
     pushBuffer.AppendLiteral(">\n <td sortable-data=\"");
