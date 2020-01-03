0fc469669e11311000421005731d52deb698355f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-03 23:13:07 +0000

    Bug 1191208 - Custom first-run screen for restricted profiles. r=margaret
    UltraBlame original commit: 0d99513542f3233cccb0e04c7b337241b4dba68c

--- mobile/android/base/BrowserApp.java
+++ mobile/android/base/BrowserApp.java
@@ -2635,10 +2635,6 @@ public class BrowserApp extends GeckoApp
     }
 
     private void showFirstrunPager() {
-        
-        if (RestrictedProfiles.isUserRestricted(this)) {
-            return;
-        }
         if (mFirstrunPane == null) {
             final ViewStub firstrunPagerStub = (ViewStub) findViewById(R.id.firstrun_pager_stub);
             mFirstrunPane = (FirstrunPane) firstrunPagerStub.inflate();
