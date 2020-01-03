7fa2331026905f87a198cd139b78affe0ed9f74c
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-03 21:08:00 +0000

    Bug 1179013: Uplift Add-on SDK. a=me
    https://github.com/mozilla/addon-sdk/compare/d8ba32821ef1a53963f4b8015b61f97d624018b0...96ae8d914fab9baad903cac07bf9f37da98fc0bc
    
    UltraBlame original commit: 2b779a9afddc15a0c7ffad497375d5b6fc326897

--- addon-sdk/source/test/test-ui-action-button.js
+++ addon-sdk/source/test/test-ui-action-button.js
@@ -18,6 +18,7 @@ const { getMostRecentBrowserWindow } = require('sdk/window/utils');
 const { partial } = require('sdk/lang/functional');
 const { wait } = require('./event/helpers');
 const { gc } = require('sdk/test/memory');
+const { emit, once } = require("sdk/event/core");
 
 const openBrowserWindow = partial(open, null, {features: {toolbar: true}});
 const openPrivateBrowserWindow = partial(open, null,
@@ -716,7 +717,11 @@ exports['test button tab state'] = function*(assert) {
     'tab badgeColor inherited from global');
 
   
-  yield wait();
+  yield new Promise(resolve => {
+    let target = {};
+    once(target, "ready", resolve);
+    emit(target, "ready");
+  });
 
   assert.equal(node.getAttribute('label'), state.label,
     'node label is correct');
@@ -737,7 +742,11 @@ exports['test button tab state'] = function*(assert) {
 
   
   
-  yield wait();
+  yield new Promise(resolve => {
+    let target = {};
+    once(target, "ready", resolve);
+    emit(target, "ready");
+  });
 
   state = button.state(mainTab);
 
