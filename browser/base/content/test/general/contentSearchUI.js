5da7217f682ce7dd99f5584b42c8597446e9a748
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-03 22:52:56 +0000

    Bug 1182338 - Add tests for keyboard navigation in the in-content search UI. r=adw
    UltraBlame original commit: b7144dca1caca54f0338b59cc372116a26e94115

--- browser/base/content/test/general/contentSearchUI.js
+++ browser/base/content/test/general/contentSearchUI.js
@@ -127,11 +127,27 @@ let messageHandlers = {
     ack("addInputValueToFormHistory");
   },
 
+  addDuplicateOneOff: function () {
+    let btn = gController._oneOffButtons[gController._oneOffButtons.length - 1];
+    let newBtn = btn.cloneNode(true);
+    btn.parentNode.appendChild(newBtn);
+    gController._oneOffButtons.push(newBtn);
+    ack("addDuplicateOneOff");
+  },
+
+  removeLastOneOff: function () {
+    gController._oneOffButtons.pop().remove();
+    ack("removeLastOneOff");
+  },
+
   reset: function () {
+    
+    
     
     gController.input.focus();
     content.synthesizeKey("a", { accelKey: true });
     content.synthesizeKey("VK_DELETE", {});
+    content.synthesizeKey("VK_ESCAPE", {});
     ack("reset");
   },
 };
@@ -168,6 +184,7 @@ function waitForContentSearchEvent(messageType, cb) {
 function currentState() {
   let state = {
     selectedIndex: gController.selectedIndex,
+    selectedButtonIndex: gController.selectedButtonIndex,
     numSuggestions: gController._table.hidden ? 0 : gController.numSuggestions,
     suggestionAtIndex: [],
     isFormHistorySuggestionAtIndex: [],
