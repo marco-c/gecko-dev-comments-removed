623c5ab5057dbca9aaca61121abcf559c67b2839
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-03 23:27:01 +0000

    Bug 1193245 - Use Atomic in the suspend count of the media resource. r=jwwang
    In present codebase, the |mSuspendCount| would be accessed in main thread and state machine thread, and we use the lock mechanism to avoid the race condition.
    
    However, we can use another more easier way to achieve it, that is using Atomic for the |mSuspendCount|.
    
    UltraBlame original commit: bbd394792e50d8169979d429fefb5ed52006db3b

--- dom/media/MediaResource.h
+++ dom/media/MediaResource.h
@@ -16,6 +16,7 @@
 #include "nsIInterfaceRequestor.h"
 #include "MediaCache.h"
 #include "MediaData.h"
+#include "mozilla/Atomics.h"
 #include "mozilla/Attributes.h"
 #include "mozilla/TimeStamp.h"
 #include "nsThreadUtils.h"
@@ -778,7 +779,7 @@ protected:
   
   
   nsRevocableEventPtr<nsRunnableMethod<ChannelMediaResource, void, false> > mDataReceivedEvent;
-  uint32_t           mSuspendCount;
+  Atomic<uint32_t>   mSuspendCount;
   
   
   bool               mReopenOnError;
