6e99e9c5457bd22f88ce903d31215a2f0bbb7420
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-03 23:00:13 +0000

    Bug 1182961 (part 1) - Use nsTHashtable::Iterator in CacheIndex. r=michal.
    UltraBlame original commit: bff74cecc67cb09b03974e2fa3fd85a9ba873786

--- netwerk/cache2/CacheIndex.h
+++ netwerk/cache2/CacheIndex.h
@@ -723,8 +723,6 @@ private:
 
   
   void ProcessPendingOperations();
-  static PLDHashOperator UpdateEntryInIndex(CacheIndexEntryUpdate *aEntry,
-                                            void* aClosure);
 
   
   
@@ -762,9 +760,6 @@ private:
   void     RemoveIndexFromDisk();
   
   nsresult WriteLogToDisk();
-
-  static PLDHashOperator WriteEntryToLog(CacheIndexEntry *aEntry,
-                                         void* aClosure);
 
   
   
@@ -820,7 +815,6 @@ private:
   
   
   void EnsureCorrectStats();
-  static PLDHashOperator SumIndexStats(CacheIndexEntry *aEntry, void* aClosure);
   
   void FinishRead(bool aSucceeded);
 
