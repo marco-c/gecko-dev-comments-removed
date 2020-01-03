10763af844d2482f5db2eb68cd4e20fec6edb767
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-03 14:23:31 +0000

    Bug 1088070 - If saving print settings in the content process, proxy to the parent. r=smaug.
    UltraBlame original commit: 8e04b9c921ad1df7a4aab484ad892d8aa007a6aa

--- embedding/components/printingui/ipc/nsPrintingProxy.h
+++ embedding/components/printingui/ipc/nsPrintingProxy.h
@@ -1,7 +1,7 @@
-/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+
+
+
 
 #ifndef __nsPrintingProxy_h
 #define __nsPrintingProxy_h
@@ -17,10 +17,16 @@ class nsPrintingProxy: public nsIPrintingPromptService,
 public:
     nsPrintingProxy();
 
+    static already_AddRefed<nsPrintingProxy> GetInstance();
+
     nsresult Init();
 
     NS_DECL_ISUPPORTS
     NS_DECL_NSIPRINTINGPROMPTSERVICE
+
+    nsresult SavePrintSettings(nsIPrintSettings* aPS,
+                               bool aUsePrinterNamePrefix,
+                               uint32_t aFlags);
 
     virtual PPrintProgressDialogChild*
     AllocPPrintProgressDialogChild() MOZ_OVERRIDE;
