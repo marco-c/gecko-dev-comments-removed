c9fb62d44128b7d5b909050cf48bdb3efe2df131
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-03 20:28:57 +0000

    Bug 1174009 - Remove a couple of unused AutoRooter types; r=sfink
    UltraBlame original commit: ce11a9ce760091dfd2a0acd0dd3724cace8bcd6a

--- js/src/jspubtd.h
+++ js/src/jspubtd.h
@@ -272,9 +272,7 @@ class JS_PUBLIC_API(AutoGCRooter)
         IONMASM =     -19, 
         WRAPVECTOR =  -20, 
         WRAPPER =     -21, 
-        OBJOBJHASHMAP=-22, 
         OBJU32HASHMAP=-23, 
-        OBJHASHSET =  -24, 
         JSONPARSER =  -25, 
         CUSTOM =      -26  
     };
