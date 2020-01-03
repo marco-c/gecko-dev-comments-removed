44626d34fca5f151a9e9f180690b3a92de7df1f6
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-03 23:47:55 +0000

    Bug 1084062 - Support read/unread state in reading list UI. r=mhaigh
    UltraBlame original commit: c6ebcbe6189ebca7d33243da8fb0b8a6c9440e63

--- mobile/android/base/db/BrowserContract.java
+++ mobile/android/base/db/BrowserContract.java
@@ -415,7 +415,7 @@ public class BrowserContract {
 
 
         public static final String DEFAULT_SORT_ORDER = CLIENT_LAST_MODIFIED + " DESC";
-        public static final String[] DEFAULT_PROJECTION = new String[] { _ID, URL, TITLE, EXCERPT, WORD_COUNT };
+        public static final String[] DEFAULT_PROJECTION = new String[] { _ID, URL, TITLE, EXCERPT, WORD_COUNT, IS_UNREAD };
 
         
         public static final String[] REQUIRED_FIELDS = { ReadingListItems.URL, ReadingListItems.TITLE };
