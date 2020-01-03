425df933884b94b74ec7b2b35c0ed88535b18e78
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-03 22:38:10 +0000

    Bug 1189027: Bump marionette driver to 0.11; r=ato
    UltraBlame original commit: 2a95f6b059a64bcd978f1116b376c701e644c1aa

--- testing/marionette/driver/marionette_driver/__init__.py
+++ testing/marionette/driver/marionette_driver/__init__.py
@@ -3,7 +3,7 @@
 
 
 
-__version__ = '0.10'
+__version__ = '0.11'
 
 
 from marionette_driver import ( errors, by, decorators, expected, geckoinstance,
@@ -14,4 +14,3 @@ from marionette_driver.date_time_value import DateTimeValue
 from marionette_driver.gestures import smooth_scroll, pinch
 from marionette_driver.wait import Wait
 from marionette_driver.marionette import Actions
-
