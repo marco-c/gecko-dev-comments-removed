8a76458888f9f4930f76bf01cfcbd53ad704f71a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-03 21:58:38 +0000

    Bug 1148593 - Create async stack in callback objects. r=bz, r=fitzgen
    UltraBlame original commit: fa9fe193778fd56ae95d88484814541989b16f9e

--- js/src/jsapi.h
+++ js/src/jsapi.h
@@ -4033,14 +4033,25 @@ class MOZ_STACK_CLASS JS_PUBLIC_API(AutoSetAsyncStackForNewCalls)
     JSContext* cx;
     RootedObject oldAsyncStack;
     RootedString oldAsyncCause;
+    bool oldAsyncCallIsExplicit;
 
   public:
+    enum class AsyncCallKind {
+        
+        
+        IMPLICIT,
+        
+        
+        EXPLICIT
+    };
+
     
     
     
     
     AutoSetAsyncStackForNewCalls(JSContext* cx, HandleObject stack,
-                                 HandleString asyncCause);
+                                 HandleString asyncCause,
+                                 AsyncCallKind kind = AsyncCallKind::IMPLICIT);
     ~AutoSetAsyncStackForNewCalls();
 };
 
@@ -5646,4 +5657,4 @@ extern JS_PUBLIC_API(void)
 JS_SetCurrentPerfGroupCallback(JSRuntime *rt, JSCurrentPerfGroupCallback cb);
 
 
-#endif 
+#endif
