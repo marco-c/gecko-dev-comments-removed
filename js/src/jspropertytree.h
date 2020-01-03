510f8f0396ed9f46476dc7be0d9fe6c61d1d7f56
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-03 02:20:31 +0000

    Bug 1074911 - Replace JS_ASSERT by MOZ_ASSERT. r=jorendorff
    Apply the following script
    
    sed -i '
       /JS_ASSERT(/ {
         s/JS_ASSERT(/MOZ_ASSERT(/;
         :b;
         s/ \\$/\\/;
         /;/ { p; d; };
         n;
         s/^/ /;
         b b;
      };
      s/JS_ASSERT (/MOZ_ASSERT(/;
    '
    
    Except where the JS_ASSERT macro does not end with a semi-colon, where empty
    lines are in the middle of the macro, and where the backslahes are always the
    same-length after the expression.
    
    UltraBlame original commit: ed4b995667b58b364d2a7ce9b2111fc22dc1f622

--- js/src/jspropertytree.h
+++ js/src/jspropertytree.h
@@ -1,8 +1,8 @@
-/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 4 -*-
- * vim: set ts=8 sts=4 et sw=4 tw=99:
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+
+
+
+
 
 #ifndef jspropertytree_h
 #define jspropertytree_h
@@ -43,23 +43,23 @@ class KidsPointer {
 
     bool isShape() const { return (w & TAG) == SHAPE && !isNull(); }
     Shape *toShape() const {
-        JS_ASSERT(isShape());
+        MOZ_ASSERT(isShape());
         return reinterpret_cast<Shape *>(w & ~uintptr_t(TAG));
     }
     void setShape(Shape *shape) {
-        JS_ASSERT(shape);
-        JS_ASSERT((reinterpret_cast<uintptr_t>(static_cast<Shape *>(shape)) & TAG) == 0);
+        MOZ_ASSERT(shape);
+        MOZ_ASSERT((reinterpret_cast<uintptr_t>(static_cast<Shape *>(shape)) & TAG) == 0);
         w = reinterpret_cast<uintptr_t>(static_cast<Shape *>(shape)) | SHAPE;
     }
 
     bool isHash() const { return (w & TAG) == HASH; }
     KidsHash *toHash() const {
-        JS_ASSERT(isHash());
+        MOZ_ASSERT(isHash());
         return reinterpret_cast<KidsHash *>(w & ~uintptr_t(TAG));
     }
     void setHash(KidsHash *hash) {
-        JS_ASSERT(hash);
-        JS_ASSERT((reinterpret_cast<uintptr_t>(hash) & TAG) == 0);
+        MOZ_ASSERT(hash);
+        MOZ_ASSERT((reinterpret_cast<uintptr_t>(hash) & TAG) == 0);
         w = reinterpret_cast<uintptr_t>(hash) | HASH;
     }
 
@@ -79,11 +79,11 @@ class PropertyTree
     PropertyTree();
 
   public:
-    /*
-     * Use a lower limit for objects that are accessed using SETELEM (o[x] = y).
-     * These objects are likely used as hashmaps and dictionary mode is more
-     * efficient in this case.
-     */
+    
+
+
+
+
     enum {
         MAX_HEIGHT = 512,
         MAX_HEIGHT_WITH_ELEMENTS_ACCESS = 128
@@ -101,6 +101,6 @@ class PropertyTree
     Shape *lookupChild(ThreadSafeContext *cx, Shape *parent, const StackShape &child);
 };
 
-} /* namespace js */
+} 
 
-#endif /* jspropertytree_h */
+#endif 
