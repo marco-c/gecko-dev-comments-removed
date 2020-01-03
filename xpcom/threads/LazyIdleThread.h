e0a9a8f9a1277aa9d3bda34902546a212b8d5a1f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-03 21:30:02 +0000

    Bug 1155059: Patch 1&2 - Convert Dispatch() and friends to already_AddRefed<> r=froydnj Modify Dispatch IDL and code to deal with MSVC issues with overloaded templates r=froydnj
    UltraBlame original commit: 2265e031ab97fc7005cb7ac999ef060fdc3f1b8f

--- xpcom/threads/LazyIdleThread.h
+++ xpcom/threads/LazyIdleThread.h
@@ -45,6 +45,10 @@ public:
   NS_DECL_NSITIMERCALLBACK
   NS_DECL_NSITHREADOBSERVER
   NS_DECL_NSIOBSERVER
+  
+  nsresult Dispatch(nsIRunnable* aEvent, uint32_t aFlags) {
+    return Dispatch(nsCOMPtr<nsIRunnable>(aEvent).forget(), aFlags);
+  }
 
   enum ShutdownMethod
   {
