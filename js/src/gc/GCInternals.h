eb82639d0a265e388eec9f424395c10d85a375cb
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-03 20:16:48 +0000

    Bug 1173889 - Strongly type the CallbackTracer dispatch function; r=jonco, r=mccr8
    UltraBlame original commit: d498daf4f845bb5721bedc84c27194098be720ab

--- js/src/gc/GCInternals.h
+++ js/src/gc/GCInternals.h
@@ -1,8 +1,8 @@
-/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 4 -*-
- * vim: set ts=8 sts=4 et sw=4 tw=99:
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+
+
+
+
 
 #ifndef gc_GCInternals_h
 #define gc_GCInternals_h
@@ -37,10 +37,10 @@ struct AutoFinishGC
     explicit AutoFinishGC(JSRuntime* rt);
 };
 
-/*
- * This class should be used by any code that needs to exclusive access to the
- * heap in order to trace through it...
- */
+
+
+
+
 class AutoTraceSession
 {
   public:
@@ -108,10 +108,10 @@ class AutoStopVerifyingBarriers
     }
 
     ~AutoStopVerifyingBarriers() {
-        // Nasty special case: verification runs a minor GC, which *may* nest
-        // inside of an outer minor GC. This is not allowed by the
-        // gc::Statistics phase tree. So we pause the "real" GC, if in fact one
-        // is in progress.
+        
+        
+        
+        
         gcstats::Phase outer = gc->stats.currentPhase();
         if (outer != gcstats::PHASE_NONE)
             gc->stats.endPhase(outer);
@@ -131,16 +131,22 @@ struct AutoStopVerifyingBarriers
 {
     AutoStopVerifyingBarriers(JSRuntime*, bool) {}
 };
-#endif /* JS_GC_ZEAL */
+#endif 
 
 #ifdef JSGC_HASH_TABLE_CHECKS
 void
 CheckHashTablesAfterMovingGC(JSRuntime* rt);
 #endif
 
-struct MovingTracer : JS::CallbackTracer {
+struct MovingTracer : JS::CallbackTracer
+{
     explicit MovingTracer(JSRuntime* rt) : CallbackTracer(rt, TraceWeakMapKeysValues) {}
-    void trace(void** thingp, JS::TraceKind kind) override;
+
+    void onObjectEdge(JSObject** objp) override;
+    void onChild(const JS::GCCellPtr& thing) override {
+        MOZ_ASSERT(!RelocationOverlay::isCellForwarded(thing.asCell()));
+    }
+
 #ifdef DEBUG
     TracerKind getTracerKind() const override { return TracerKind::Moving; }
 #endif
@@ -169,7 +175,7 @@ class AutoMaybeStartBackgroundAllocation
     }
 };
 
-// In debug builds, set/unset the GC sweeping flag for the current thread.
+
 struct AutoSetThreadIsSweeping
 {
 #ifdef DEBUG
@@ -194,17 +200,17 @@ struct AutoSetThreadIsSweeping
 #endif
 };
 
-// Structure for counting how many times objects in a particular group have
-// been tenured during a minor collection.
+
+
 struct TenureCount
 {
     ObjectGroup* group;
     int count;
 };
 
-// Keep rough track of how many times we tenure objects in particular groups
-// during minor collections, using a fixed size hash for efficiency at the cost
-// of potential collisions.
+
+
+
 struct TenureCountCache
 {
     TenureCount entries[16];
@@ -216,7 +222,7 @@ struct TenureCountCache
     }
 };
 
-} /* namespace gc */
-} /* namespace js */
+} 
+} 
 
-#endif /* gc_GCInternals_h */
+#endif 
