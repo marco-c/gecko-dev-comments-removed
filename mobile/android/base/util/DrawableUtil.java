22a3a846acf9e18d92d1cc18440ae570a148912f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-03 23:31:52 +0000

    Bug 1193950 - Tint menu bar icons on pre-L devices. r=mhaigh
    I wasn't using the wrapped drawable when setting the icon, which
    curiously works on L+ devices, but not pre-L.
    
    UltraBlame original commit: 87ece6852f50e9671258498a25f003a09434bcb4

--- mobile/android/base/util/DrawableUtil.java
+++ mobile/android/base/util/DrawableUtil.java
@@ -8,6 +8,7 @@ package org.mozilla.gecko.util;
 import android.content.Context;
 import android.content.res.ColorStateList;
 import android.graphics.drawable.Drawable;
+import android.support.annotation.CheckResult;
 import android.support.annotation.ColorRes;
 import android.support.annotation.DrawableRes;
 import android.support.annotation.NonNull;
@@ -16,6 +17,11 @@ import android.support.v4.graphics.drawable.DrawableCompat;
 
 public class DrawableUtil {
 
+    
+
+
+
+    @CheckResult
     public static Drawable tintDrawable(@NonNull final Context context, @DrawableRes final int drawableID,
                 @ColorRes final int colorID) {
         final Drawable icon = DrawableCompat.wrap(
@@ -24,6 +30,11 @@ public class DrawableUtil {
         return icon;
     }
 
+    
+
+
+
+    @CheckResult
     public static Drawable tintDrawableWithStateList(@NonNull final Drawable drawable,
             @NonNull final ColorStateList colorList) {
         final Drawable wrappedDrawable = DrawableCompat.wrap(drawable.mutate());
