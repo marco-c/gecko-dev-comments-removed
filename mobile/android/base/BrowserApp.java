b15e621b800bd3782be47cbeeb1523940b506a74
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-03 23:44:41 +0000

    Bug 1180295 - Hook up toolbar show/hide animations to the DynamicToolbarAnimator. r=rbarker
    UltraBlame original commit: 2ed49a9cb8594640dfd6b18fb7cde9a9e29e62b8

--- mobile/android/base/BrowserApp.java
+++ mobile/android/base/BrowserApp.java
@@ -1597,13 +1597,17 @@ public class BrowserApp extends GeckoApp
 
         
         
-        
-        
         ImmutableViewportMetrics metrics = mLayerView.getViewportMetrics();
-        final float height = metrics.viewportRectBottom - metrics.viewportRectTop;
-        if (metrics.getPageHeight() < metrics.getHeight()
-              || metrics.marginTop >= mToolbarHeight / 2
-              || (metrics.pageRectBottom == metrics.viewportRectBottom && metrics.pageRectBottom > 2*height)) {
+        float toolbarTranslation = mLayerView.getDynamicToolbarAnimator().getToolbarTranslation();
+
+        boolean shortPage = metrics.getPageHeight() < metrics.getHeight();
+        boolean toolbarMostlyVisible = toolbarTranslation < (mToolbarHeight / 2);
+        boolean atBottomOfLongPage = (metrics.pageRectBottom == metrics.viewportRectBottom)
+            && (metrics.pageRectBottom > 2 * metrics.getHeight());
+        Log.v(LOGTAG, "On pan/zoom stopped, short page: " + shortPage
+            + "; toolbarMostlyVisible: " + toolbarMostlyVisible
+            + "; atBottomOfLongPage: " + atBottomOfLongPage);
+        if (shortPage || toolbarMostlyVisible || atBottomOfLongPage) {
             mDynamicToolbar.setVisible(true, VisibilityTransition.ANIMATE);
         } else {
             mDynamicToolbar.setVisible(false, VisibilityTransition.ANIMATE);
