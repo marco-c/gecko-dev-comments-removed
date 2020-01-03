eaa3989a41a9bc37505199a9bd96c3b308172d20
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-03 07:24:09 +0000

    Bug 1114752 - Uplift Add-on SDK to Firefox a=me
    UltraBlame original commit: 2815bfd2ccb58c382e0207f451d989a973e44e57

--- addon-sdk/source/test/addons/e10s/lib/main.js
+++ addon-sdk/source/test/addons/e10s/lib/main.js
@@ -1,6 +1,6 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+
+
 'use strict';
 
 const { getMostRecentBrowserWindow, isBrowser } = require('sdk/window/utils');
@@ -11,12 +11,16 @@ const { version, platform } = require('sdk/system');
 const { when } = require('sdk/system/unload');
 const tabs = require('sdk/tabs');
 
+const SKIPPING_TESTS = {
+  "test skip": (assert) => assert.pass("nothing to test here")
+};
+
 exports.testTabIsRemote = function(assert, done) {
   const url = 'data:text/html,test-tab-is-remote';
   let tab = openTab(getMostRecentBrowserWindow(), url);
   assert.ok(tab.getAttribute('remote'), "The new tab should be remote");
 
-  // can't simply close a remote tab before it is loaded, bug 1006043
+  
   let mm = getBrowserForTab(tab).messageManager;
   mm.addMessageListener('7', function listener() {
     mm.removeMessageListener('7', listener);
@@ -26,7 +30,7 @@ exports.testTabIsRemote = function(assert, done) {
   mm.loadFrameScript('data:,sendAsyncMessage("7")', true);
 }
 
-// run e10s tests only on builds from trunk, fx-team, Nightly..
+
 if (!version.endsWith('a1')) {
   module.exports = {};
 }
@@ -48,9 +52,9 @@ function replaceWindow(remote) {
   return promise.then(focus).then(_ => close(old));
 }
 
-// bug 1054482 - e10s test addons time out on linux
+
 if (platform === 'linux') {
-  module.exports = {};
+  module.exports = SKIPPING_TESTS;
   require('sdk/test/runner').runTestsFromModule(module);
 }
 else {
