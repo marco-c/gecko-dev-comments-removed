ef4f400dfc2af0e378e7df54c12fd7535bc8d298
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-03 23:22:36 +0000

    Bug 1192304 - Common up the checks when entering GC API; r=jonco
    UltraBlame original commit: 9c9f970f1fffef726a62f9edc66a3d858f91aa6f

--- js/src/gc/GCRuntime.h
+++ js/src/gc/GCRuntime.h
@@ -887,12 +887,22 @@ class GCRuntime
 
     void requestMajorGC(JS::gcreason::Reason reason);
     SliceBudget defaultBudget(JS::gcreason::Reason reason, int64_t millis);
-    void collect(bool incremental, SliceBudget budget, JS::gcreason::Reason reason);
-    bool gcCycle(bool incremental, SliceBudget& budget, JS::gcreason::Reason reason);
-    gcstats::ZoneGCStats scanZonesBeforeGC();
     void budgetIncrementalGC(SliceBudget& budget);
     void resetIncrementalGC(const char* reason);
+
+    
+    
+    void checkCanCallAPI();
+
+    
+    
+    bool checkIfGCAllowedInCurrentState(JS::gcreason::Reason reason);
+
+    gcstats::ZoneGCStats scanZonesBeforeGC();
+    void collect(bool incremental, SliceBudget budget, JS::gcreason::Reason reason);
+    bool gcCycle(bool incremental, SliceBudget& budget, JS::gcreason::Reason reason);
     void incrementalCollectSlice(SliceBudget& budget, JS::gcreason::Reason reason);
+
     void pushZealSelectedObjects();
     void purgeRuntime();
     bool beginMarkPhase(JS::gcreason::Reason reason);
