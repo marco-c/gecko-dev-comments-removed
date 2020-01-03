9e533fd62c327d312daf08972629725b54d0b89e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-03 22:29:57 +0000

    Bug 1188233: Ensure drainComplete flag is set upon failure to create decoder. r=cpearce
    UltraBlame original commit: 9c48bac3e0d30deec06907bc582516cc170d82b1

--- dom/media/MediaFormatReader.cpp
+++ dom/media/MediaFormatReader.cpp
@@ -984,15 +984,13 @@ MediaFormatReader::DrainDecoder(TrackType aTrack)
     return;
   }
   decoder.mNeedDraining = false;
-  if (!decoder.mDecoder) {
-    return;
-  }
-  decoder.mOutputRequested = true;
-  if (decoder.mNumSamplesInput == decoder.mNumSamplesOutput) {
+  if (!decoder.mDecoder ||
+      decoder.mNumSamplesInput == decoder.mNumSamplesOutput) {
     
     NotifyDrainComplete(aTrack);
     return;
   }
+  decoder.mOutputRequested = true;
   decoder.mDecoder->Drain();
   decoder.mDraining = true;
   LOG("Requesting %s decoder to drain", TrackTypeToStr(aTrack));
