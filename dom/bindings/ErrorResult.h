8817bc336621b565a536dbebdd12982185c7b14b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 19:52:26 +0000

    Bug 933378 part 4.  Change the ErrorResult destructor to suppress the exception, after asserting that it's already suppressed.  r=bkelly
    UltraBlame original commit: ed38780a242cb738c331a9abaf63b94ae325d325

--- dom/bindings/ErrorResult.h
+++ dom/bindings/ErrorResult.h
@@ -460,13 +460,24 @@ struct JustAssertCleanupPolicy {
   static const bool suppress = false;
 };
 
+struct AssertAndSuppressCleanupPolicy {
+  static const bool assertHandled = true;
+  static const bool suppress = true;
+};
+
+struct JustSuppressCleanupPolicy {
+  static const bool assertHandled = false;
+  static const bool suppress = true;
+};
+
 } 
 
 
 
-class ErrorResult : public binding_danger::TErrorResult<binding_danger::JustAssertCleanupPolicy>
+class ErrorResult :
+    public binding_danger::TErrorResult<binding_danger::AssertAndSuppressCleanupPolicy>
 {
-  typedef binding_danger::TErrorResult<binding_danger::JustAssertCleanupPolicy> BaseErrorResult;
+  typedef binding_danger::TErrorResult<binding_danger::AssertAndSuppressCleanupPolicy> BaseErrorResult;
 
 public:
   ErrorResult()
@@ -503,17 +514,15 @@ template<typename CleanupPolicy>
 binding_danger::TErrorResult<CleanupPolicy>::operator ErrorResult&()
 {
   return *static_cast<ErrorResult*>(
-     reinterpret_cast<TErrorResult<JustAssertCleanupPolicy>*>(this));
+     reinterpret_cast<TErrorResult<AssertAndSuppressCleanupPolicy>*>(this));
 }
 
 
-class IgnoredErrorResult : public ErrorResult
+
+
+class IgnoredErrorResult :
+    public binding_danger::TErrorResult<binding_danger::JustSuppressCleanupPolicy>
 {
-public:
-  ~IgnoredErrorResult()
-  {
-    SuppressException();
-  }
 };
 
 
