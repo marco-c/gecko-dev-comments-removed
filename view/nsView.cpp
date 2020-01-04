edf54136fa16c08ea2f1c7743bd2fe2c3431b2cc
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 03:11:24 +0000

    Bug 1207161 - fix run-by-dir leak in test_bug846906.xul; r=mccr8
    UltraBlame original commit: 6d12c3f9067c913ed0f0459d7ad02655b84c18fb

--- view/nsView.cpp
+++ view/nsView.cpp
@@ -136,7 +136,12 @@ void nsView::DestroyWidget()
       nsCOMPtr<nsIRunnable> widgetDestroyer =
         new DestroyWidgetRunnable(mWindow);
 
-      NS_DispatchToMainThread(widgetDestroyer);
+      
+      
+      nsCOMPtr<nsIThread> mainThread = do_GetMainThread();
+      if (mainThread) {
+        mainThread->Dispatch(widgetDestroyer.forget(), NS_DISPATCH_NORMAL);
+      }
     }
 
     mWindow = nullptr;
