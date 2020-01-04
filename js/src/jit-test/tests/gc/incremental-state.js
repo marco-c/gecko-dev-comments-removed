4bc2971e5ffeaeb1bdc9d82807efcfc348633307
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 10:17:12 +0000

    Bug 1249367 - Make background finalization a GC phase (and clean up Zones properly); r=jonco
    UltraBlame original commit: d8da9bc56468f58be505e61e72e78bff1d2b4b61

--- js/src/jit-test/tests/gc/incremental-state.js
+++ js/src/jit-test/tests/gc/incremental-state.js
@@ -1,7 +1,4 @@
 
-
-
-
 if (!("gcstate" in this))
     quit();
 
@@ -12,10 +9,10 @@ gc();
 assertEq(gcstate(), "none");
 
 
+
 gcslice(1000000);
+while (gcstate() == "finalize") { gcslice(1); }
 assertEq(gcstate(), "none");
-
-
 
 
 
@@ -25,25 +22,37 @@ assertEq(gcstate(), "mark");
 gcslice(1000000);
 assertEq(gcstate(), "mark");
 gcslice(1000000);
+while (gcstate() == "finalize") { gcslice(1); }
 assertEq(gcstate(), "none");
+
+
+
 
 
 gczeal(8);
 gcslice(1);
 assertEq(gcstate(), "mark");
 gcslice(1);
+while (gcstate() == "finalize") { gcslice(1); }
 assertEq(gcstate(), "none");
+
+
+
 
 
 gczeal(9);
 gcslice(1);
 assertEq(gcstate(), "mark");
 gcslice(1);
+while (gcstate() == "finalize") { gcslice(1); }
 assertEq(gcstate(), "none");
+
+
 
 
 gczeal(10);
 gcslice(1000000);
 assertEq(gcstate(), "sweep");
 gcslice(1000000);
+while (gcstate() == "finalize") { gcslice(1); }
 assertEq(gcstate(), "none");
