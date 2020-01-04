c8b353c75f0c6ba1906ac57b51ca3764132755a7
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 23:39:17 +0000

    Bug 1300658 P1 Expose a main thread TaskQueue WorkerPrivate. r=baku
    UltraBlame original commit: 62775b8b0e7b7b45beec34cc1335dcd02cdcf87e

--- dom/workers/WorkerPrivate.h
+++ dom/workers/WorkerPrivate.h
@@ -59,6 +59,7 @@ struct RuntimeStats;
 } 
 
 namespace mozilla {
+class TaskQueue;
 namespace dom {
 class Function;
 class MessagePort;
@@ -917,6 +918,8 @@ class WorkerPrivate : public WorkerPrivateParent<WorkerPrivate>
   nsTObserverArray<WorkerHolder*> mHolders;
   nsTArray<nsAutoPtr<TimeoutInfo>> mTimeouts;
   uint32_t mDebuggerEventLoopLevel;
+  RefPtr<TaskQueue> mMainThreadTaskQueue;
+  nsCOMPtr<nsIEventTarget> mMainThreadEventTarget;
 
   struct SyncLoopInfo
   {
@@ -1340,6 +1343,20 @@ public:
 
   void
   MaybeDispatchLoadFailedRunnable();
+
+  
+  
+  
+  nsIEventTarget*
+  MainThreadEventTarget();
+
+  nsresult
+  DispatchToMainThread(nsIRunnable* aRunnable,
+                       uint32_t aFlags = NS_DISPATCH_NORMAL);
+
+  nsresult
+  DispatchToMainThread(already_AddRefed<nsIRunnable> aRunnable,
+                       uint32_t aFlags = NS_DISPATCH_NORMAL);
 
 private:
   WorkerPrivate(WorkerPrivate* aParent,
