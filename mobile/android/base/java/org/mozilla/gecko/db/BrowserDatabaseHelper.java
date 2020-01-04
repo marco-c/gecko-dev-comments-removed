72f1ecbc9b9a69114e7d759d105a024a181ad735
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 14:29:04 +0000

    Bug 1234315 - Introduce test for url to cache-filename r=nalexander
    We hash the page URL to produce a filename for each cached reader view page. For the purposes
    of the rl-migration we need a Java implementation, however the canonical implementation
    lives in the current JS readercache (which might be removed in future...). This test
    should help ensure that we don't lose saved items during the migration.
    
    We could extract getReaderCacheFileNameForURL(), however it seems safer to make it
    public instead - this makes it obvious that this is a migration-only method that
    shouldn't be modified/reused in future.
    
    MozReview-Commit-ID: GNg20nszMh9
    
    UltraBlame original commit: 2c81d5f8a352e3877b6a7f5c41801bcf36c6466e

--- mobile/android/base/java/org/mozilla/gecko/db/BrowserDatabaseHelper.java
+++ mobile/android/base/java/org/mozilla/gecko/db/BrowserDatabaseHelper.java
@@ -18,6 +18,7 @@ import org.mozilla.apache.commons.codec.binary.Base32;
 import org.mozilla.gecko.GeckoProfile;
 import org.mozilla.gecko.R;
 import org.mozilla.gecko.Telemetry;
+import org.mozilla.gecko.annotation.RobocopTarget;
 import org.mozilla.gecko.db.BrowserContract.Bookmarks;
 import org.mozilla.gecko.db.BrowserContract.Combined;
 import org.mozilla.gecko.db.BrowserContract.Favicons;
@@ -1167,7 +1168,9 @@ public final class BrowserDatabaseHelper extends SQLiteOpenHelper {
     
     
     
-    private static String getReaderCacheFileNameForURL(String url) {
+    
+    @RobocopTarget
+    public static String getReaderCacheFileNameForURL(String url) {
         try {
             
             
