572476be9a8e910d251651f15e0715c05d2af117
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 08:29:43 +0000

    Bug 1241373 - Make nsStyleSet::GetAuthorStyleDisabled const. r=dholbert
    UltraBlame original commit: e658250c40a6ddd5e57052a6bccc3684ba4c6d22

--- layout/style/nsStyleSet.h
+++ layout/style/nsStyleSet.h
@@ -326,7 +326,7 @@ class nsStyleSet final
                                   mozilla::CSSStyleSheet* aReferenceSheet);
 
   
-  bool GetAuthorStyleDisabled();
+  bool GetAuthorStyleDisabled() const;
   nsresult SetAuthorStyleDisabled(bool aStyleDisabled);
 
   int32_t SheetCount(mozilla::SheetType aType) const {
