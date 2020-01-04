ecf65d29db083d74b30506d957accebe3bdc6e56
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 08:31:25 +0000

    Bug 1239217 - Skip empty install manifests, apart a few exceptions. r=gps
    Install manifests are not empty in normal conditions, apart a few
    exceptions where they are only used for a "magic" `rm -rf`.
    
    However, we're going to introduce changes that will empty some of
    the install manifests and make their work happen from a different
    backend, in which case we don't want them to correspond to a `rm -rf`.
    
    UltraBlame original commit: 0d981cf65508535f7967a48fa08f93e54401ad93

--- python/mozbuild/mozbuild/backend/recursivemake.py
+++ python/mozbuild/mozbuild/backend/recursivemake.py
@@ -391,19 +391,14 @@ class RecursiveMakeBackend(CommonBackend):
         self.backend_input_files.add(mozpath.join(self.environment.topobjdir,
             'config', 'autoconf.mk'))
 
-        self._install_manifests = {
-            k: InstallManifest() for k in [
-                'dist_bin',
-                'dist_branding',
-                'dist_idl',
-                'dist_include',
-                'dist_public',
-                'dist_private',
-                'dist_sdk',
-                'dist_xpi-stage',
-                '_tests',
-                'xpidl',
-            ]}
+        self._install_manifests = defaultdict(InstallManifest)
+        
+        
+        
+        
+        self._install_manifests['dist_public']
+        self._install_manifests['dist_private']
+        self._install_manifests['dist_sdk']
 
         self._traversal = RecursiveMakeTraversal()
         self._compile_graph = defaultdict(set)
