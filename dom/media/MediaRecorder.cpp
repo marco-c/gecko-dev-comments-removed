6d4308389b3d90cf71163931841636b1ed2c02e7
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 14:17:16 +0000

    Bug 1260702: Don't crash on input sources from WebAudio in MediaRecorder r=padenot
    MozReview-Commit-ID: cseol6STUw
    
    UltraBlame original commit: 1fd5dcb62bc4be26d7de019bb978fd7970953f9b

--- dom/media/MediaRecorder.cpp
+++ dom/media/MediaRecorder.cpp
@@ -643,7 +643,7 @@ private:
     mTrackUnionStream->AddListener(mEncoder);
     
     DOMMediaStream* domStream = mRecorder->Stream();
-    if (domStream) {
+    if (domStream && domStream->GetInputStream()) {
       mInputStream = domStream->GetInputStream()->AsSourceStream();
       if (mInputStream) {
         mInputStream->AddDirectListener(mEncoder);
