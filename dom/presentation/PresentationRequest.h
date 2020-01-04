aee9b785cdca805c7f019aaabbcf7e637ec6dfe1
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 01:15:20 +0000

    Bug 1192101 - Part 1 - Support PresentationRequest / PresentationAvailability / getSession(s). Part 1 - WebIDL Bindings. r=smaug
    UltraBlame original commit: 0b07b414b0455bacb6202f9063a7d7e49649a936

---
+++ dom/presentation/PresentationRequest.h
@@ -0,0 +1,54 @@
+
+
+
+
+
+
+#ifndef mozilla_dom_PresentationRequest_h
+#define mozilla_dom_PresentationRequest_h
+
+#include "mozilla/DOMEventTargetHelper.h"
+
+namespace mozilla {
+namespace dom {
+
+class Promise;
+class PresentationAvailability;
+
+class PresentationRequest final : public DOMEventTargetHelper
+{
+public:
+  NS_DECL_ISUPPORTS_INHERITED
+  NS_DECL_CYCLE_COLLECTION_CLASS_INHERITED(PresentationRequest,
+                                           DOMEventTargetHelper)
+
+  static already_AddRefed<PresentationRequest> Constructor(const GlobalObject& aGlobal,
+                                                           const nsAString& aUrl,
+                                                           ErrorResult& aRv);
+
+  virtual JSObject* WrapObject(JSContext* aCx,
+                               JS::Handle<JSObject*> aGivenProto) override;
+
+  
+  already_AddRefed<Promise> Start(ErrorResult& aRv);
+
+  already_AddRefed<Promise> GetAvailability(ErrorResult& aRv);
+
+  IMPL_EVENT_HANDLER(sessionconnect);
+
+private:
+  PresentationRequest(nsPIDOMWindow* aWindow,
+                      const nsAString& aUrl);
+
+  ~PresentationRequest();
+
+  bool Init();
+
+  nsString mUrl;
+  nsRefPtr<PresentationAvailability> mAvailability;
+};
+
+} 
+} 
+
+#endif 
