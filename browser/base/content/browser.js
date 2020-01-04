3e7311748a03c0899fdba0d8645db1fa1a8cb760
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 10:07:36 +0000

    Bug 1228754 - correct comments from bustage fix, rs=me, DONTBUILD
    MozReview-Commit-ID: 2w311HTGaEt
    
    UltraBlame original commit: f4da8a4682a8baf7ddeb487ebcccf63d8b17649e

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -6443,6 +6443,7 @@ function checkEmptyPageOrigin(browser = gBrowser.selectedBrowser,
   }
   
   if (contentPrincipal.URI) {
+    
     if (uri.spec == "about:blank" && contentPrincipal.isNullPrincipal) {
       return true;
     }
