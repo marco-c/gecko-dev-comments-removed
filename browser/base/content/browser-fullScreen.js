543ac262919c14294cb667dc98d182cc8ee7655c
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 00:53:22 +0000

    Bug 1201374 - Add a telemetry probe to track how often F11 fullscreen mode is used (browser-fullscreen). r=ttaubert p=ally
    UltraBlame original commit: e40065dd77cebf8e6f3ed02a68ad496da313fdf4

--- browser/base/content/browser-fullScreen.js
+++ browser/base/content/browser-fullScreen.js
@@ -95,6 +95,11 @@ var FullScreen = {
       
       TabsInTitlebar.updateAppearance(true);
     }
+
+    if (enterFS) {
+      Services.telemetry.getHistogramById("FX_BROWSER_FULLSCREEN_USED")
+                        .add(1);
+    }
   },
 
   exitDomFullScreen : function() {
