e44f49f09003a9b6fd31e726d2969e33ebc4f109
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 16:14:10 +0000

    Bug 1271020: Add a null check before acting upon applicationContext r=sebastian
    Pre-rewrite code had a similar check
    
    MozReview-Commit-ID: FGqPCOabkqH
    
    UltraBlame original commit: 9ecdf2bfc28baa20f86c2225b8dc9ff41977ed2e

--- mobile/android/base/java/org/mozilla/gecko/GeckoNetworkManager.java
+++ mobile/android/base/java/org/mozilla/gecko/GeckoNetworkManager.java
@@ -263,6 +263,10 @@ public class GeckoNetworkManager extends BroadcastReceiver implements NativeEven
 
 
     private void updateNetworkStateAndConnectionType() {
+        if (applicationContext == null) {
+            Log.i(LOGTAG, "applicationContext is null while trying to update network state");
+            return;
+        }
         final ConnectivityManager connectivityManager = (ConnectivityManager) applicationContext.getSystemService(
                 Context.CONNECTIVITY_SERVICE);
         
