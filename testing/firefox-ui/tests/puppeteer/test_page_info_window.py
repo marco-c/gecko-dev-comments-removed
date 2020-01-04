401fac821ebd1dceb7be13e15f04201f94002e83
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 14:16:31 +0000

    Bug 1251600 - Fix test_page_info_window.py for handling platform names. r=whimboo
    MozReview-Commit-ID: 8PXwvIx3szI
    
    UltraBlame original commit: c0fa17b1195d11c2f94fc04c48bbbebb1667c80d

--- testing/firefox-ui/tests/puppeteer/test_page_info_window.py
+++ testing/firefox-ui/tests/puppeteer/test_page_info_window.py
@@ -69,8 +69,7 @@ class TestPageInfoWindow(FirefoxTestCase):
                            )
 
         for trigger in open_strategies:
-            if trigger == 'shortcut' and \
-                    self.marionette.session_capabilities['platform'] == 'WINDOWS_NT':
+            if trigger == 'shortcut' and self.platform == 'windows_nt':
                 
                 self.assertRaises(ValueError, self.browser.open_page_info_window,
                                   trigger=trigger)
@@ -92,7 +91,7 @@ class TestPageInfoWindow(FirefoxTestCase):
                             )
         for trigger in close_strategies:
             
-            if trigger == 'menu' and self.platform != 'Darwin':
+            if trigger == 'menu' and self.platform != 'darwin':
                 continue
 
             page_info = self.browser.open_page_info_window()
