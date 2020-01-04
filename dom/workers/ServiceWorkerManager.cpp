3f17613b58d1237204cdc7cb65b7c16fc61e3130
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 04:20:21 +0000

    Bug 1186810 (part 4) - Replace nsBaseHashtable::EnumerateRead() calls in dom/workers/ with iterators. r=nsm.
    UltraBlame original commit: c2a383729b77e2e542beed3676e8d3b761bc2e8e

--- dom/workers/ServiceWorkerManager.cpp
+++ dom/workers/ServiceWorkerManager.cpp
@@ -3665,42 +3665,6 @@ struct UnregisterIfMatchesUserData final
   void *mUserData;
 };
 
-
-PLDHashOperator
-UnregisterIfMatchesHostPerPrincipal(const nsACString& aKey,
-                                    ServiceWorkerManager::RegistrationDataPerPrincipal* aData,
-                                    void* aUserData)
-{
-  UnregisterIfMatchesUserData data(aData, aUserData);
-  for (auto iter = aData->mInfos.Iter(); !iter.Done(); iter.Next()) {
-    ServiceWorkerRegistrationInfo* reg = iter.UserData();
-
-    
-    
-    
-    ServiceWorkerRegistrationInfo* toRemove = nullptr;
-    if (data.mUserData) {
-      const nsACString& domain = *static_cast<nsACString*>(data.mUserData);
-      nsCOMPtr<nsIURI> scopeURI;
-      nsresult rv = NS_NewURI(getter_AddRefs(scopeURI), iter.Key(),
-                              nullptr, nullptr);
-      
-      if (NS_SUCCEEDED(rv) && HasRootDomain(scopeURI, domain)) {
-        toRemove = reg;
-      }
-    } else {
-      toRemove = reg;
-    }
-
-    if (toRemove) {
-      RefPtr<ServiceWorkerManager> swm = ServiceWorkerManager::GetInstance();
-      swm->ForceUnregister(data.mRegistrationData, toRemove);
-    }
-  }
-
-  return PL_DHASH_NEXT;
-}
-
 PLDHashOperator
 UnregisterIfMatchesClearOriginParams(const nsACString& aScope,
                                      ServiceWorkerRegistrationInfo* aReg,
@@ -3889,6 +3853,7 @@ ServiceWorkerManager::GetAllRegistrations(nsIArray** aResult)
 }
 
 
+
 void
 ServiceWorkerManager::ForceUnregister(RegistrationDataPerPrincipal* aRegistrationData,
                                       ServiceWorkerRegistrationInfo* aRegistration)
@@ -3927,8 +3892,20 @@ ServiceWorkerManager::Remove(const nsACString& aHost)
     return;
   }
 
-  mRegistrationInfos.EnumerateRead(UnregisterIfMatchesHostPerPrincipal,
-                                   &const_cast<nsACString&>(aHost));
+  RefPtr<ServiceWorkerManager> swm = ServiceWorkerManager::GetInstance();
+  for (auto it1 = mRegistrationInfos.Iter(); !it1.Done(); it1.Next()) {
+    ServiceWorkerManager::RegistrationDataPerPrincipal* data = it1.UserData();
+    for (auto it2 = data->mInfos.Iter(); !it2.Done(); it2.Next()) {
+      ServiceWorkerRegistrationInfo* reg = it2.UserData();
+      nsCOMPtr<nsIURI> scopeURI;
+      nsresult rv = NS_NewURI(getter_AddRefs(scopeURI), it2.Key(),
+                              nullptr, nullptr);
+      
+      if (NS_SUCCEEDED(rv) && HasRootDomain(scopeURI, aHost)) {
+        swm->ForceUnregister(data, reg);
+      }
+    }
+  }
 }
 
 void
@@ -3949,7 +3926,15 @@ void
 ServiceWorkerManager::RemoveAll()
 {
   AssertIsOnMainThread();
-  mRegistrationInfos.EnumerateRead(UnregisterIfMatchesHostPerPrincipal, nullptr);
+
+  RefPtr<ServiceWorkerManager> swm = ServiceWorkerManager::GetInstance();
+  for (auto it1 = mRegistrationInfos.Iter(); !it1.Done(); it1.Next()) {
+    ServiceWorkerManager::RegistrationDataPerPrincipal* data = it1.UserData();
+    for (auto it2 = data->mInfos.Iter(); !it2.Done(); it2.Next()) {
+      ServiceWorkerRegistrationInfo* reg = it2.UserData();
+      swm->ForceUnregister(data, reg);
+    }
+  }
 }
 
 void
