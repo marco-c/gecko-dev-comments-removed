6cf6255888dc2e743006bc9aeb4f10e39d840a38
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 05:17:58 +0000

    Bug 1225024 - Allow sloppy tolerance in ecma_6/Math/log10-approx.js. r=jorendorff
    UltraBlame original commit: 86f7c45c29716cd326643a30a9e48915e0e694cc

--- js/src/tests/ecma_6/Math/log10-approx.js
+++ js/src/tests/ecma_6/Math/log10-approx.js
@@ -2,8 +2,13 @@ assertNear(Math.log10(2), 0.3010299956639812);
 assertNear(Math.log10(7), 0.8450980400142568);
 assertNear(Math.log10(Math.E), Math.LOG10E);
 
+
+
+
+var sloppy_tolerance = 2;
+
 for (var i = -10; i < 10; i++)
-    assertNear(Math.log10(Math.pow(10, i)), i);
+    assertNear(Math.log10(Math.pow(10, i)), i, sloppy_tolerance);
 
 reportCompare(0, 0, 'ok');
 
