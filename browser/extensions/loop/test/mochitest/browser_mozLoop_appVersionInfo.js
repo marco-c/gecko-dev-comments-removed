90cb15cc50bc2bc2ca900169d4398c43ae09f9e4
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 06:45:02 +0000

    Backed out 2 changesets (bug 1227539) for Win 7 M(bc4) and likely Win XP opt M(bc6) failures. r=backout
    Backed out changeset 48d56e89c482 (bug 1227539)
    Backed out changeset 0fbd239d0c37 (bug 1227539)
    
    UltraBlame original commit: cf17f3b3be6c6e7e0f891a90ab98db5c47f176af

--- browser/extensions/loop/test/mochitest/browser_mozLoop_appVersionInfo.js
+++ browser/extensions/loop/test/mochitest/browser_mozLoop_appVersionInfo.js
@@ -2,6 +2,7 @@
 
 "use strict";
 
+const { LoopAPI } = Cu.import("chrome://loop/content/modules/MozLoopAPI.jsm", {});
 var [, gHandlers] = LoopAPI.inspect();
 
 add_task(function* test_mozLoop_appVersionInfo() {
