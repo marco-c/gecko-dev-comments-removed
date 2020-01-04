3c97b62682ae2458868d36fe5b0c8fc885300679
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 17:19:51 +0000

    Bug 1275893 - Enable logical float properties for devedition, r=jfkthame
    MozReview-Commit-ID: 4USAWZKXlUU
    
    UltraBlame original commit: a021e3facc2a0828c2a1f7bcc386c10f10570036

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -2430,7 +2430,7 @@ pref("layout.css.text-align-unsafe-value.enabled", false);
 
 
 
-#if defined(MOZ_B2G) || defined(NIGHTLY_BUILD)
+#if defined(MOZ_B2G) || !defined(RELEASE_BUILD)
 pref("layout.css.float-logical-values.enabled", true);
 #else
 pref("layout.css.float-logical-values.enabled", false);
