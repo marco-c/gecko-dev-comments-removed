a3b95b1a1f719a76b41a04c02e3c7e6a9e66e1a6
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 14:16:28 +0000

    Bug 1209273 - Part 1 - Support for color-adjust CSS property. r=dbaron
    UltraBlame original commit: 073149564592e1da4dca4a7324a23f3df35422de

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -5262,4 +5262,6 @@ pref("dom.input.fallbackUploadDir", "");
 pref("plugins.rewrite_youtube_embeds", true);
 
 
+pref("layout.css.color-adjust.enabled", true);
+
 pref("dom.mozBrowserFramesEnabled", false);
