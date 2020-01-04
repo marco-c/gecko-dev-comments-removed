5df44effad46636dbaf85503894c299fbe05fc2d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 16:35:08 +0000

    Backed out changeset 2ac62de7365d (bug 1271002) for crash in GTest: [ mozilla::image::RasterImage::UpdateImageContainer]. r=backout
    UltraBlame original commit: 2a218727e6e69350525bf4725f014d170c377821

--- image/Decoder.cpp
+++ image/Decoder.cpp
@@ -449,12 +449,6 @@ Decoder::PostFrameStop(Opacity aFrameOpacity
     mInvalidRect.UnionRect(mInvalidRect,
                            gfx::IntRect(gfx::IntPoint(0, 0), GetSize()));
   }
-
-  
-  if (mFrameCount == 1 && HasAnimation()) {
-    MOZ_ASSERT(HasProgress());
-    DecodePool::Singleton()->NotifyProgress(this);
-  }
 }
 
 void
