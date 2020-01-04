6f24d1887f5ee4436c0e87594f02f9e61e12b22e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 11:01:18 +0000

    Bug 1250934: Block GraphDriver switching if audio input is enabled (full-duplex) r=padenot Also fixes a debug that would always report 'nil' for a pointer
    MozReview-Commit-ID: 2xKD9s47d8z
    
    UltraBlame original commit: 48cb1d9b30393be6b2bc930258ed280fb16549a4

--- dom/media/MediaStreamGraph.cpp
+++ dom/media/MediaStreamGraph.cpp
@@ -134,9 +134,9 @@ MediaStreamGraphImpl::RemoveStreamGraphThread(MediaStream* aStream)
     mStreams.RemoveElement(aStream);
   }
 
-  NS_RELEASE(aStream); 
+  STREAM_LOG(LogLevel::Debug, ("Removed media stream %p from the graph", aStream));
 
-  STREAM_LOG(LogLevel::Debug, ("Removing media stream %p from the graph", aStream));
+  NS_RELEASE(aStream); 
 }
 
 void
@@ -368,6 +368,15 @@ MediaStreamGraphImpl::AudioTrackPresent(bool& aNeedsAEC)
         }
       }
     }
+  }
+
+  
+  
+  
+  if (!audioTrackPresent && mInputDeviceUsers.Count() != 0) {
+    NS_WARNING("No audio tracks, but full-duplex audio is enabled!!!!!");
+    audioTrackPresent = true;
+    shouldAEC = true;
   }
 
 #ifdef MOZ_WEBRTC
