844240d3a9ed9013ad217beea933ab458517d8dd
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 07:42:01 +0000

    Bug 1107702 - Try to avoid internal memory exhaustion problems with the Windows api GetAdaptersInfo by making a priming call to the api during startup. r=jesup
    UltraBlame original commit: 0324a62e156caa54ceb2339f8fe84c6215682fa9

--- dom/media/MediaManager.cpp
+++ dom/media/MediaManager.cpp
@@ -78,6 +78,9 @@
 #endif
 #if defined (XP_WIN)
 #include "mozilla/WindowsVersion.h"
+#include <winsock2.h>
+#include <iphlpapi.h>
+#include <tchar.h>
 #endif
 
 
@@ -1638,6 +1641,28 @@ MediaManager::GetNonE10sParent()
     mNonE10sParent = new media::Parent<media::NonE10s>(true);
   }
   return mNonE10sParent;
+}
+
+ void
+MediaManager::StartupInit()
+{
+#ifdef WIN32
+  if (IsVistaOrLater() && !IsWin8OrLater()) {
+    
+    
+    
+    unsigned long out_buf_len = sizeof(IP_ADAPTER_INFO);
+    PIP_ADAPTER_INFO pAdapterInfo = (IP_ADAPTER_INFO *) moz_xmalloc(out_buf_len);
+    if (GetAdaptersInfo(pAdapterInfo, &out_buf_len) == ERROR_BUFFER_OVERFLOW) {
+      free(pAdapterInfo);
+      pAdapterInfo = (IP_ADAPTER_INFO *) moz_xmalloc(out_buf_len);
+      GetAdaptersInfo(pAdapterInfo, &out_buf_len);
+    }
+    if (pAdapterInfo) {
+      free(pAdapterInfo);
+    }
+  }
+#endif
 }
 
 
