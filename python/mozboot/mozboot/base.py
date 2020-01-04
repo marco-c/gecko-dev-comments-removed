a2306a3d2b1dfed996c7d697b30275278a9f989f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 17:16:20 +0000

    Bug 1267781 - Bump modern Mercurial version to 3.7.3; r=smacleod
    These variables specify a version of Mercurial that is considered
    modern and won't trigger giant warnings about being out of date.
    
    We bump to 3.7.3 because 3.7.3 contains security fixes and it is
    important for as many users as possible to get these security fixes.
    
    We also update the messaging to indicate security issues with older
    releases.
    
    MozReview-Commit-ID: H4utKINrW0V
    
    UltraBlame original commit: f3c508b6292133376aacba82969a43cf29f0cc87

--- python/mozboot/mozboot/base.py
+++ python/mozboot/mozboot/base.py
@@ -75,7 +75,7 @@ We recommend the following tools for installing Python:
 
 
 
-MODERN_MERCURIAL_VERSION = LooseVersion('3.5.2')
+MODERN_MERCURIAL_VERSION = LooseVersion('3.7.3')
 
 
 MODERN_PYTHON_VERSION = LooseVersion('2.7.3')
@@ -332,6 +332,9 @@ class BaseBootstrapper(object):
         if installed:
             print('Your version of Mercurial (%s) is not modern enough.' %
                   version)
+            print('(Older versions of Mercurial have known security vulnerabilities. '
+                  'Unless you are running a patched Mercurial version, you may be '
+                  'vulnerable.')
         else:
             print('You do not have Mercurial installed')
 
