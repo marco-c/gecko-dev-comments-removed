f044a31e2b6534ab441395470f5903dfa7bcc03b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 10:27:03 +0000

    Bug 1251373 - Remove moz payment API support. r=Margaret
    MozReview-Commit-ID: IW63XQDyB16
    
    UltraBlame original commit: a83ac48ef07320775c0b705366c6b36d2f77d8b7

--- mobile/android/app/mobile.js
+++ mobile/android/app/mobile.js
@@ -827,15 +827,6 @@ pref("browser.contentHandlers.types.3.uri", "chrome://browser/locale/region.prop
 pref("browser.contentHandlers.types.3.type", "application/vnd.mozilla.maybe.feed");
 
 
-pref("dom.mozPay.enabled", true);
-
-pref("dom.payment.provider.0.name", "Firefox Marketplace");
-pref("dom.payment.provider.0.description", "marketplace.firefox.com");
-pref("dom.payment.provider.0.uri", "https://marketplace.firefox.com/mozpay/?req=");
-pref("dom.payment.provider.0.type", "mozilla/payments/pay/v1");
-pref("dom.payment.provider.0.requestMethod", "GET");
-
-
 
 pref("dom.phonenumber.substringmatching.BR", 8);
 pref("dom.phonenumber.substringmatching.CO", 10);
