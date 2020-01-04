0aaae4219dbaa87eb3443bb85ecb74b6948ca030
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 18:17:47 +0000

    Backed out changeset 36e71bcb0266 (bug 1192945)
    UltraBlame original commit: a7b96471dd304f7bf3e8df1642009ddbd31c846c

--- widget/nsBaseDragService.h
+++ widget/nsBaseDragService.h
@@ -156,8 +156,6 @@ protected:
   nsCOMPtr<nsIDOMNode> mSourceNode;
   nsCOMPtr<nsIDOMDocument> mSourceDocument;       
                                                   
-  nsContentPolicyType mContentPolicyType;         
-                                                  
   nsCOMPtr<nsIDOMDataTransfer> mDataTransfer;
 
   
