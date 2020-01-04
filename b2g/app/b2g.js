6fd9ce2eb82e4effce8f24d887bbee9e04a66aa6
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 03:54:30 +0000

    Bug 1217959 - update dom.apps.reviewer_paths to the correct value r=me
    UltraBlame original commit: 172b1a3f267fe5e2924dc1e9b2ad1c48dabd2e79

--- b2g/app/b2g.js
+++ b2g/app/b2g.js
@@ -1068,7 +1068,7 @@ pref("dom.wakelock.enabled", true);
 
 
 pref("dom.apps.customization.enabled", true);
-pref("dom.apps.reviewer_paths", "/reviewers/,/content/addon/review/");
+pref("dom.apps.reviewer_paths", "/reviewers/,/extension/reviewers/");
 
 
 pref("layout.accessiblecaret.enabled", true);
