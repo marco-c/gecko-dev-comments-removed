fcc655f1ce923b5c7a033a86fa0150629f04bf7f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 02:04:40 +0000

    Bug 1207645 - Create HeapSnapshotFileActor; r=jryans
    This commit creates the HeapSnapshotFileActor and moves the transferHeapSnapshot
    method from MemoryActor to HeapSnapshotFileActor. This is necessary because
    child processes in e10s are sandboxed and do not have access to the file system,
    and because MemoryActor is in the sandboxed child process it cannot open the
    file and send it over the RDP.
    
    This complexity is hidden at the MemoryFront layer. Users of MemoryFront will
    still simply call its saveHeapSnapshot method, and do not need to worry about
    the inner workings of how heap snapshot files are transferred over the RDP. This
    required adding a third parameter to MemoryFront's initialize method: the
    listTabs response.
    
    UltraBlame original commit: a23053342d5b7e7e525a0bb6e256babc8ac90ca5

--- devtools/client/framework/target.js
+++ devtools/client/framework/target.js
@@ -317,8 +317,26 @@ TabTarget.prototype = {
     return this._form;
   },
 
+  
+  
   get root() {
+    if (!this._root) {
+      this._root = this._getRoot();
+    }
     return this._root;
+  },
+
+  _getRoot: function () {
+    return new Promise((resolve, reject) => {
+      this.client.listTabs(response => {
+        if (response.error) {
+          reject(new Error(response.error + ": " + response.message));
+          return;
+        }
+
+        resolve(response);
+      });
+    });
   },
 
   get client() {
