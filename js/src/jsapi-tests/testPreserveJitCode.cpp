d7ea6e6f2f1179ec17540259e6f845a3c81b3148
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 02:32:19 +0000

    Bug 1208259 - Disable ARM64 Ion, fail testPreserveJitCode. r=jandem
    IonMonkey for ARM64 is not yet implemented, so don't try running it.
    
    The test case in testPreserveJitCode is explicitly counting how many
    functions get Ion-compiled. Mark the test as a known failure for ARM64
    when Ion is disabled.
    
    UltraBlame original commit: 1a06e56abe768da542290e18fba5f27f2b2da3ce

--- js/src/jsapi-tests/testPreserveJitCode.cpp
+++ js/src/jsapi-tests/testPreserveJitCode.cpp
@@ -2,6 +2,9 @@
 
 
 
+
+#include "jit/Ion.h"
+
 #include "jsapi-tests/tests.h"
 
 using namespace JS;
@@ -39,6 +42,15 @@ testPreserveJitCode(bool preserveJitCode, unsigned remainingIonScripts)
     RootedObject global(cx, createTestGlobal(preserveJitCode));
     CHECK(global);
     JSAutoCompartment ac(cx, global);
+
+#ifdef JS_CODEGEN_ARM64
+    
+    
+    
+    
+    if (!js::jit::IsIonEnabled(cx))
+        knownFail = true;
+#endif
 
     CHECK_EQUAL(countIonScripts(global), 0u);
 
