8de0abe5364f2a8a59ebcb5d01c80e00f048f938
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 19:21:27 +0000

    Bug 1241932 - Expose decoded CSS grid track properties in a Chrome API. r=heycam, r=khuey
    UltraBlame original commit: 3396152c5e11bb9c2a8af4ef1bd1c39be78d5e28

--- dom/base/Element.h
+++ dom/base/Element.h
@@ -140,6 +140,7 @@ class UndoManager;
 class DOMRect;
 class DOMRectList;
 class DestinationInsertionPointList;
+class Grid;
 
 
 #define NS_ELEMENT_IID \
@@ -829,6 +830,8 @@ public:
            nsPresContext::AppUnitsToIntCSSPixels(sf->GetScrollRange().XMost()) :
            0;
   }
+
+  void GetGridFragments(nsTArray<RefPtr<Grid>>& aResult);
 
   virtual already_AddRefed<UndoManager> GetUndoManager()
   {
