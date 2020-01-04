779152fc715ed2248204a10474c3f16a107f73e3
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 04:51:21 +0000

    Bug 1160307 - capture async stack frames on Javascript timeline markers. r=fitzgen, r=smaug r=Paolo
    UltraBlame original commit: 6f44b15e0ec68424bd67c89f5769a3151c5c28ae

--- js/src/jsapi.h
+++ js/src/jsapi.h
@@ -5500,6 +5500,25 @@ CaptureCurrentStack(JSContext* cx, MutableHandleObject stackp, unsigned maxFrame
 
 
 
+extern JS_PUBLIC_API(bool)
+CopyAsyncStack(JSContext* cx, HandleObject asyncStack,
+               HandleString asyncCause, MutableHandleObject stackp,
+               unsigned maxFrameCount);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
