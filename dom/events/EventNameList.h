fc851cfeec51f76f8844f2d9f9f5e4b43abcf60b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 00:22:04 +0000

    Bug 895274 part.23 Rename NS_LANGUAGECHANGE to eLanguageChange r=smaug
    UltraBlame original commit: 31d9af1df30fe60d55b3078776d197e311b73ff6

--- dom/events/EventNameList.h
+++ dom/events/EventNameList.h
@@ -485,7 +485,7 @@ WINDOW_EVENT(hashchange,
              EventNameType_XUL | EventNameType_HTMLBodyOrFramesetOnly,
              eBasicEventClass)
 WINDOW_EVENT(languagechange,
-             NS_LANGUAGECHANGE,
+             eLanguageChange,
              EventNameType_HTMLBodyOrFramesetOnly,
              eBasicEventClass)
 
