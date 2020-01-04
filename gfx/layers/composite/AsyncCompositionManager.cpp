d62e03e3f080f130919649f256b79f602f0123ea
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 08:04:46 +0000

    Bug 1214536 - Part 2: Replace mIterationCount in layers::Animation. r=birtles
    Replace mIterationCount with mIterations.
    
    UltraBlame original commit: e142c1d640f9fe84264d0412e5a705eb970e098f

--- gfx/layers/composite/AsyncCompositionManager.cpp
+++ gfx/layers/composite/AsyncCompositionManager.cpp
@@ -586,7 +586,7 @@ SampleAnimations(Layer* aLayer, TimeStamp aPoint)
     
     
     timing.mDelay = TimeDuration(0);
-    timing.mIterations = animation.iterationCount();
+    timing.mIterations = animation.iterations();
     timing.mDirection = static_cast<dom::PlaybackDirection>(animation.direction());
     
     
