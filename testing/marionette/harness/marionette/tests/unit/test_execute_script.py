c1b15cbfcb1e67b1f5bc5e772291f82ff81c74da
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 16:30:01 +0000

    Bug 1271758 - Ensure that execute_script does not fail on closed chrome windows. r=ato
    MozReview-Commit-ID: 7SMV36KuFCe
    
    UltraBlame original commit: ea476de583db9a5a36a32be19e4bf526a6679e95

--- testing/marionette/harness/marionette/tests/unit/test_execute_script.py
+++ testing/marionette/harness/marionette/tests/unit/test_execute_script.py
@@ -266,3 +266,14 @@ class TestExecuteChrome(TestExecuteContent):
                 var cb = arguments[arguments.length - 1];
                 setTimeout(function() { cb() }, 250);
                 """, script_timeout=100)
+
+    def test_invalid_chrome_handle(self):
+        
+        self.marionette.close_chrome_window()
+        self.assertEqual(len(self.marionette.chrome_window_handles), 1)
+
+        
+        with self.marionette.using_context('chrome'):
+            self.marionette.execute_script("""
+                return true;
+            """)
