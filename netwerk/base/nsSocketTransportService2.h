12dc70be9a7d186dd1b4e77d91294daeb1f7c930
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 12:14:09 +0000

    Bug 1257809 - "Improve telemetry for PR_Connect". r=mcmanus
    UltraBlame original commit: 2c55fae81acc0efacb8c732aa4d698169248588c

--- netwerk/base/nsSocketTransportService2.h
+++ netwerk/base/nsSocketTransportService2.h
@@ -19,6 +19,7 @@
 #include "mozilla/net/DashboardTypes.h"
 #include "mozilla/Atomics.h"
 #include "mozilla/TimeStamp.h"
+#include "nsITimer.h"
 
 class nsASocketHandler;
 struct PRPollDesc;
@@ -111,7 +112,8 @@ public:
     
     bool IsKeepaliveEnabled() { return mKeepaliveEnabledPref; }
 
-    bool IsTelemetryEnabled() { return mTelemetryEnabledPref; }
+    bool IsTelemetryEnabledAndNotSleepPhase() { return mTelemetryEnabledPref &&
+                                                       !mSleepPhase; }
     PRIntervalTime MaxTimeForPrClosePref() {return mMaxTimeForPrClosePref; }
 protected:
 
@@ -239,6 +241,11 @@ private:
     mozilla::Atomic<int32_t, mozilla::Relaxed> mMaxTimePerPollIter;
     mozilla::Atomic<bool, mozilla::Relaxed>  mTelemetryEnabledPref;
     mozilla::Atomic<PRIntervalTime, mozilla::Relaxed> mMaxTimeForPrClosePref;
+
+    
+    
+    mozilla::Atomic<bool, mozilla::Relaxed> mSleepPhase;
+    nsCOMPtr<nsITimer> mAfterWakeUpTimer;
 
     void OnKeepaliveEnabledPrefChange();
     void NotifyKeepaliveEnabledPrefChange(SocketContext *sock);
