e9ee5cdbf21bc58bcbefab6504adb3fe69ee2fb9
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 06:23:53 +0000

    Bug 1055472 - Part 10: Make the Set constructor properly subclassable. (r=Waldo)
    UltraBlame original commit: 57cf6353c9fb31f250c26a93b8a460140359e572

--- js/src/builtin/MapObject.h
+++ js/src/builtin/MapObject.h
@@ -181,7 +181,7 @@ class SetObject : public NativeObject {
 
     
     
-    static SetObject* create(JSContext *cx);
+    static SetObject* create(JSContext *cx, HandleObject proto = nullptr);
     static uint32_t size(JSContext *cx, HandleObject obj);
     static bool has(JSContext *cx, HandleObject obj, HandleValue key, bool* rval);
     static bool clear(JSContext *cx, HandleObject obj);
