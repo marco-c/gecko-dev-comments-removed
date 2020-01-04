16f2ca563784f1c98414e11e7f5179f355ce90c3
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 20:46:53 +0000

    Bug 1287827 - Part 5. Remove Loop's special permissions for about:loopconversation from MediaManager. r=jib
    UltraBlame original commit: 9d637e0a4608297045c31b13645ce4380ac72265

--- dom/media/MediaManager.cpp
+++ dom/media/MediaManager.cpp
@@ -2074,7 +2074,7 @@ MediaManager::GetUserMedia(nsPIDOMWindowInner* aWindow,
     return NS_ERROR_UNEXPECTED;
   }
   bool loop = IsLoop(docURI);
-  bool privileged = loop || IsPrivileged();
+  bool privileged = IsPrivileged();
   bool isHTTPS = false;
   docURI->SchemeIs("https", &isHTTPS);
   nsCString host;
@@ -2168,7 +2168,7 @@ MediaManager::GetUserMedia(nsPIDOMWindowInner* aWindow,
                                     false) && !IsVistaOrLater()) ||
 #endif
             (!privileged && !HostIsHttps(*docURI)) ||
-            !(loop || HostHasPermission(*docURI))) {
+            !HostHasPermission(*docURI)) {
           RefPtr<MediaStreamError> error =
               new MediaStreamError(aWindow,
                                    NS_LITERAL_STRING("NotAllowedError"));
@@ -2212,16 +2212,6 @@ MediaManager::GetUserMedia(nsPIDOMWindowInner* aWindow,
           }
         }
       }
-    }
-
-    
-    
-    
-    
-    if (loop && (videoType == MediaSourceEnum::Window ||
-                 videoType == MediaSourceEnum::Application ||
-                 videoType == MediaSourceEnum::Screen)) {
-       privileged = false;
     }
   } else if (IsOn(c.mVideo)) {
     videoType = MediaSourceEnum::Camera;
