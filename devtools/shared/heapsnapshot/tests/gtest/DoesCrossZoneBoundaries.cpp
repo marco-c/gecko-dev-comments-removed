aa01873fac7561e02abfcde3704f41dc5b62fdea
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 08:22:09 +0000

    Bug 1239666 - part 3, devtools test case changes.  r=sphink
    UltraBlame original commit: cbb0ad1e211825863b355d0b9ace98b373268bb3

--- devtools/shared/heapsnapshot/tests/gtest/DoesCrossZoneBoundaries.cpp
+++ devtools/shared/heapsnapshot/tests/gtest/DoesCrossZoneBoundaries.cpp
@@ -9,10 +9,12 @@
 
 DEF_TEST(DoesCrossZoneBoundaries, {
     
+    JS::CompartmentOptions options;
     JS::RootedObject newGlobal(cx, JS_NewGlobalObject(cx,
                                                       getGlobalClass(),
                                                       nullptr,
-                                                      JS::FireOnNewGlobalHook));
+                                                      JS::FireOnNewGlobalHook,
+                                                      options));
     ASSERT_TRUE(newGlobal);
     JS::Zone* newZone = nullptr;
     {
--- devtools/shared/heapsnapshot/tests/gtest/DoesntCrossZoneBoundaries.cpp
+++ devtools/shared/heapsnapshot/tests/gtest/DoesntCrossZoneBoundaries.cpp
@@ -9,10 +9,12 @@
 
 DEF_TEST(DoesntCrossZoneBoundaries, {
     
+    JS::CompartmentOptions options;
     JS::RootedObject newGlobal(cx, JS_NewGlobalObject(cx,
                                                       getGlobalClass(),
                                                       nullptr,
-                                                      JS::FireOnNewGlobalHook));
+                                                      JS::FireOnNewGlobalHook,
+                                                      options));
     ASSERT_TRUE(newGlobal);
     JS::Zone* newZone = nullptr;
     {
