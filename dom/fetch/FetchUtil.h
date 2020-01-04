d65177c3eb8bcf7097586b434883a1484f937d5d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 10:28:37 +0000

    Bug 1206166 - Move FetchUtil::Consume methods into separate BodyUtil class and update Fetch.cpp and ServiceWorkerEvents.cpp accordingly. r=kitcambridge
    MozReview-Commit-ID: KgEccQ2LPpC
    
    UltraBlame original commit: faaf448b9788574b99b0fd0dc554879899edcdf4

--- dom/fetch/FetchUtil.h
+++ dom/fetch/FetchUtil.h
@@ -29,46 +29,6 @@ public:
   
 
 
-
-
-  static void
-  ConsumeArrayBuffer(JSContext* aCx, JS::MutableHandle<JSObject*> aValue,
-                     uint32_t aInputLength, uint8_t* aInput, ErrorResult& aRv);
-
-  
-
-
-
-  static already_AddRefed<Blob>
-  ConsumeBlob(nsISupports* aParent, const nsString& aMimeType,
-              uint32_t aInputLength, uint8_t* aInput, ErrorResult& aRv);
-
-  
-
-
-
-  static already_AddRefed<FormData>
-  ConsumeFormData(nsIGlobalObject* aParent, const nsCString& aMimeType,
-                  const nsCString& aStr, ErrorResult& aRv);
-
-  
-
-
-
-  static nsresult
-  ConsumeText(uint32_t aInputLength, uint8_t* aInput, nsString& aText);
-
-  
-
-
-
-  static void
-  ConsumeJson(JSContext* aCx, JS::MutableHandle<JS::Value> aValue,
-              const nsString& aStr, ErrorResult& aRv);
-
-  
-
-
   static bool
   ExtractHeader(nsACString::const_iterator& aStart,
                 nsACString::const_iterator& aEnd,
