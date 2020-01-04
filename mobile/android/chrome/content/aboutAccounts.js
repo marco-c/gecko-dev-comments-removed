311ce457defc62b1a6915a03d7650c5a1c823e0a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 22:47:12 +0000

    Bug 1281220 - Remove about:accounts telemetry on Fennec. r=liuche
    MozReview-Commit-ID: GTKaauQIDKo
    
    UltraBlame original commit: e0e98ba78d86c127ccf1008c3b1021540478237b

--- mobile/android/chrome/content/aboutAccounts.js
+++ mobile/android/chrome/content/aboutAccounts.js
@@ -64,44 +64,8 @@ var loadedDeferred = null;
 
 
 
-function deferTransitionToRemoteAfterLoaded(url) {
+function deferTransitionToRemoteAfterLoaded() {
   log.d('Waiting for LOADED message.');
-
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  let recordResultTelemetry; 
-  let isCustomized = Services.prefs.prefHasUserValue("identity.fxaccounts.remote.webchannel.uri");
-  if (!isCustomized) {
-    
-    let key = Services.io.newURI(url, null, null).path.split("?")[0];
-
-    let startTime = Cu.now();
-
-    let start = Services.telemetry.getKeyedHistogramById('ABOUT_ACCOUNTS_CONTENT_SERVER_LOAD_STARTED_COUNT');
-    start.add(key);
-
-    recordResultTelemetry = function(success) {
-      let rate = Services.telemetry.getKeyedHistogramById('ABOUT_ACCOUNTS_CONTENT_SERVER_LOADED_RATE');
-      rate.add(key, success);
-
-      
-      
-      let delta = Cu.now() - startTime; 
-      let time = success ?
-            Services.telemetry.getKeyedHistogramById('ABOUT_ACCOUNTS_CONTENT_SERVER_LOADED_TIME_MS') :
-            Services.telemetry.getKeyedHistogramById('ABOUT_ACCOUNTS_CONTENT_SERVER_FAILURE_TIME_MS');
-      time.add(key, Math.round(delta));
-    };
-  }
 
   loadedDeferred = PromiseUtils.defer();
   loadedDeferred.promise.then(() => {
@@ -109,15 +73,9 @@ function deferTransitionToRemoteAfterLoaded(url) {
     document.getElementById("remote").style.opacity = 0;
     show("remote");
     document.getElementById("remote").style.opacity = 1;
-    if (!isCustomized) {
-      recordResultTelemetry(true);
-    }
   })
   .catch((e) => {
     log.w('Did not get LOADED message: ' + e.toString());
-    if (!isCustomized) {
-      recordResultTelemetry(false);
-    }
   });
 }
 
@@ -132,7 +90,7 @@ var wrapper = {
 
   init: function (url) {
     this.url = url;
-    deferTransitionToRemoteAfterLoaded(this.url);
+    deferTransitionToRemoteAfterLoaded();
 
     let iframe = document.getElementById("remote");
     this.iframe = iframe;
@@ -148,7 +106,7 @@ var wrapper = {
   },
 
   retry: function () {
-    deferTransitionToRemoteAfterLoaded(this.url);
+    deferTransitionToRemoteAfterLoaded();
 
     let webNav = this.iframe.frameLoader.docShell.QueryInterface(Ci.nsIWebNavigation);
     webNav.loadURI(this.url, Ci.nsIWebNavigation.LOAD_FLAGS_BYPASS_HISTORY, null, null, null);
