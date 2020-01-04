4c74eb7ea49047fd038ef649fb7766cb787d48d3
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 04:41:30 +0000

    Bug 1220509 - Remove unused CSSStyleSheet::PrependStyleRule. r=bzbarsky
    UltraBlame original commit: 4b086ed88cb0c00ba360de85737e6a051729b93b

--- layout/style/CSSStyleSheet.h
+++ layout/style/CSSStyleSheet.h
@@ -159,7 +159,6 @@ public:
   void InsertStyleSheetAt(CSSStyleSheet* aSheet, int32_t aIndex);
 
   
-  void PrependStyleRule(css::Rule* aRule);
   void AppendStyleRule(css::Rule* aRule);
   void ReplaceStyleRule(css::Rule* aOld, css::Rule* aNew);
 
