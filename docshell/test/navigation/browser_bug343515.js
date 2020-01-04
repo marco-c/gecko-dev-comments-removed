a093017f86b11ebbaf6294653ff6f81764dd8ed9
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 08:26:05 +0000

    Bug 1239122 - Make docshell browser-chrome tests e10s compatible. r=billm
    UltraBlame original commit: f405663b5570ba4462a7703ba3f0b4f1d9dcc143

--- docshell/test/navigation/browser_bug343515.js
+++ docshell/test/navigation/browser_bug343515.js
@@ -5,14 +5,6 @@ var testPath = "http://mochi.test:8888/browser/docshell/test/navigation/";
 var ctx = {};
 
 
-function isActive(aWindow) {
-  var docshell = aWindow.QueryInterface(Ci.nsIInterfaceRequestor)
-                        .getInterface(Ci.nsIWebNavigation)
-                        .QueryInterface(Ci.nsIDocShell);
-  return docshell.isActive;
-}
-
-
 
 
 
@@ -34,6 +26,12 @@ function oneShotListener(aElem, aType, aCallback) {
   nShotsListener(aElem, aType, aCallback, 1);
 }
 
+function waitForPageshow(aBrowser, callback) {
+  return ContentTask.spawn(aBrowser, null, function* () {
+    yield ContentTaskUtils.waitForEvent(this, "pageshow");
+  }).then(callback);
+}
+
 
 function test() {
 
@@ -49,15 +47,13 @@ function step1() {
   
   ctx.tab0 = gBrowser.selectedTab;
   ctx.tab0Browser = gBrowser.getBrowserForTab(ctx.tab0);
-  ctx.tab0Window = ctx.tab0Browser.contentWindow;
 
   
-  ok(isActive(ctx.tab0Window), "Tab 0 should be active at test start");
+  ok(ctx.tab0Browser.docShellIsActive, "Tab 0 should be active at test start");
 
   
   ctx.tab1 = gBrowser.addTab(testPath + "bug343515_pg1.html");
   ctx.tab1Browser = gBrowser.getBrowserForTab(ctx.tab1);
-  ctx.tab1Window = ctx.tab1Browser.contentWindow;
   oneShotListener(ctx.tab1Browser, "load", step2);
 }
 
@@ -66,24 +62,23 @@ function step2() {
      "Got expected tab 1 url in step 2");
 
   
-  ok(isActive(ctx.tab0Window), "Tab 0 should still be active");
-  ok(!isActive(ctx.tab1Window), "Tab 1 should not be active");
+  ok(ctx.tab0Browser.docShellIsActive, "Tab 0 should still be active");
+  ok(!ctx.tab1Browser.docShellIsActive, "Tab 1 should not be active");
 
   
-  gBrowser.selectedTab = ctx.tab1;
+  BrowserTestUtils.switchTab(gBrowser, ctx.tab1).then(() => {
+    
+    ok(!ctx.tab0Browser.docShellIsActive, "Tab 0 should be inactive");
+    ok(ctx.tab1Browser.docShellIsActive, "Tab 1 should be active");
 
-  
-  ok(!isActive(ctx.tab0Window), "Tab 0 should be inactive");
-  ok(isActive(ctx.tab1Window), "Tab 1 should be active");
+    
+    ctx.tab2 = gBrowser.addTab(testPath + "bug343515_pg2.html");
+    ctx.tab2Browser = gBrowser.getBrowserForTab(ctx.tab2);
 
-  
-  ctx.tab2 = gBrowser.addTab(testPath + "bug343515_pg2.html");
-  ctx.tab2Browser = gBrowser.getBrowserForTab(ctx.tab2);
-  ctx.tab2Window = ctx.tab2Browser.contentWindow;
-
-  
-  
-  nShotsListener(ctx.tab2Browser, "load", step3, 3);
+    
+    
+    nShotsListener(ctx.tab2Browser, "load", step3, 3);
+  });
 }
 
 function step3() {
@@ -91,115 +86,169 @@ function step3() {
      "Got expected tab 2 url in step 3");
 
   
-  ok(!isActive(ctx.tab0Window), "Tab 0 should be inactive");
-  ok(isActive(ctx.tab1Window), "Tab 1 should be active");
+  ok(!ctx.tab0Browser.docShellIsActive, "Tab 0 should be inactive");
+  ok(ctx.tab1Browser.docShellIsActive, "Tab 1 should be active");
 
   
-  ok(!isActive(ctx.tab2Window), "Tab 2 should be inactive");
-  is(ctx.tab2Window.frames.length, 2, "Tab 2 should have 2 iframes");
-  for (var i = 0; i < ctx.tab2Window.frames.length; i++)
-    info("step 3, frame " + i + " info: " + ctx.tab2Window.frames[i].location);
-  ok(!isActive(ctx.tab2Window.frames[0]), "Tab2 iframe 0 should be inactive");
-  ok(!isActive(ctx.tab2Window.frames[1]), "Tab2 iframe 1 should be inactive");
+  ok(!ctx.tab2Browser.docShellIsActive, "Tab 2 should be inactive");
+  ContentTask.spawn(ctx.tab2Browser, null, function* () {
+    is(content.frames.length, 2, "Tab 2 should have 2 iframes");
+    for (var i = 0; i < content.frames.length; i++) {
+      info("step 3, frame " + i + " info: " + content.frames[i].location);
+      let docshell = content.frames[i].QueryInterface(Ci.nsIInterfaceRequestor)
+                                      .getInterface(Ci.nsIWebNavigation)
+                                      .QueryInterface(Ci.nsIDocShell);
 
-  
-  ctx.tab2Window.location = testPath + "bug343515_pg3.html";
+      ok(!docShell.isActive, `Tab2 iframe ${i} should be inactive`);
+    }
+  }).then(() => {
+    
+    ctx.tab2Browser.loadURI(testPath + "bug343515_pg3.html");
 
-  
-  
-  nShotsListener(ctx.tab2Browser, "load", step4, 4);
+    
+    
+    nShotsListener(ctx.tab2Browser, "load", step4, 4);
+  });
 }
 
 function step4() {
+  function checkTab2Active(expected) {
+    return ContentTask.spawn(ctx.tab2Browser, expected, function* (expected) {
+      function isActive(aWindow) {
+        var docshell = aWindow.QueryInterface(Ci.nsIInterfaceRequestor)
+                              .getInterface(Ci.nsIWebNavigation)
+                              .QueryInterface(Ci.nsIDocShell);
+        return docshell.isActive;
+      }
+
+      let active = expected ? "active" : "inactive";
+      is(content.frames.length, 2, "Tab 2 should have 2 iframes");
+      for (var i = 0; i < content.frames.length; i++)
+        info("step 4, frame " + i + " info: " + content.frames[i].location);
+      is(content.frames[0].frames.length, 1, "Tab 2 iframe 0 should have 1 iframes");
+      is(isActive(content.frames[0]), expected, `Tab2 iframe 0 should be ${active}`);
+      is(isActive(content.frames[0].frames[0]), expected,
+         `Tab2 iframe 0 subiframe 0 should be ${active}`);
+      is(isActive(content.frames[1]), expected, `Tab2 iframe 1 should be ${active}`);
+    });
+  }
+
   is(testPath + "bug343515_pg3.html", ctx.tab2Browser.currentURI.spec,
      "Got expected tab 2 url in step 4");
 
   
-  ok(!isActive(ctx.tab0Window), "Tab 0 should be inactive");
-  ok(isActive(ctx.tab1Window), "Tab 1 should be active");
+  ok(!ctx.tab0Browser.docShellIsActive, "Tab 0 should be inactive");
+  ok(ctx.tab1Browser.docShellIsActive, "Tab 1 should be active");
 
   
-  ok(!isActive(ctx.tab2Window), "Tab 2 should be inactive");
-  is(ctx.tab2Window.frames.length, 2, "Tab 2 should have 2 iframes");
-  for (var i = 0; i < ctx.tab2Window.frames.length; i++)
-    info("step 4, frame " + i + " info: " + ctx.tab2Window.frames[i].location);
-  is(ctx.tab2Window.frames[0].frames.length, 1, "Tab 2 iframe 0 should have 1 iframes");
-  ok(!isActive(ctx.tab2Window.frames[0]), "Tab2 iframe 0 should be inactive");
-  ok(!isActive(ctx.tab2Window.frames[0].frames[0]), "Tab2 iframe 0 subiframe 0 should be inactive");
-  ok(!isActive(ctx.tab2Window.frames[1]), "Tab2 iframe 1 should be inactive");
+  checkTab2Active(false).then(() => {
+    
+    return BrowserTestUtils.switchTab(gBrowser, ctx.tab2);
+  }).then(() => {
+    
+    ok(!ctx.tab0Browser.docShellIsActive, "Tab 0 should be inactive");
+    ok(!ctx.tab1Browser.docShellIsActive, "Tab 1 should be inactive");
+    ok(ctx.tab2Browser.docShellIsActive, "Tab 2 should be active");
 
-  
-  gBrowser.selectedTab = ctx.tab2;
-
-  
-  ok(!isActive(ctx.tab0Window), "Tab 0 should be inactive");
-  ok(!isActive(ctx.tab1Window), "Tab 1 should be inactive");
-  ok(isActive(ctx.tab2Window), "Tab 2 should be active");
-  ok(isActive(ctx.tab2Window.frames[0]), "Tab2 iframe 0 should be active");
-  ok(isActive(ctx.tab2Window.frames[0].frames[0]), "Tab2 iframe 0 subiframe 0 should be active");
-  ok(isActive(ctx.tab2Window.frames[1]), "Tab2 iframe 1 should be active");
-
-  
-  oneShotListener(ctx.tab2Browser, "pageshow", step5);
-  ctx.tab2Browser.goBack();
-
+    return checkTab2Active(true);
+  }).then(() => {
+    
+    waitForPageshow(ctx.tab2Browser, step5);
+    ctx.tab2Browser.goBack();
+  });
 }
 
 function step5() {
   
-  ok(!isActive(ctx.tab0Window), "Tab 0 should be inactive");
-  ok(!isActive(ctx.tab1Window), "Tab 1 should be inactive");
-  ok(isActive(ctx.tab2Window), "Tab 2 should be active");
-  ok(isActive(ctx.tab2Window.frames[0]), "Tab2 iframe 0 should be active");
-  ok(isActive(ctx.tab2Window.frames[1]), "Tab2 iframe 1 should be active");
+  ok(!ctx.tab0Browser.docShellIsActive, "Tab 0 should be inactive");
+  ok(!ctx.tab1Browser.docShellIsActive, "Tab 1 should be inactive");
+  ok(ctx.tab2Browser.docShellIsActive, "Tab 2 should be active");
+  ContentTask.spawn(ctx.tab2Browser, null, function* () {
+    for (var i = 0; i < content.frames.length; i++) {
+      let docshell = content.frames[i].QueryInterface(Ci.nsIInterfaceRequestor)
+                                      .getInterface(Ci.nsIWebNavigation)
+                                      .QueryInterface(Ci.nsIDocShell);
 
-  
-  gBrowser.selectedTab = ctx.tab1;
+      ok(docShell.isActive, `Tab2 iframe ${i} should be active`);
+    }
+  }).then(() => {
+    
+    return BrowserTestUtils.switchTab(gBrowser, ctx.tab1);
+  }).then(() => {
+    
+    ctx.tab1Browser.loadURI(testPath + "bug343515_pg3.html");
 
-  
-  ctx.tab1Window.location = testPath + "bug343515_pg3.html";
-
-  
-  
-  nShotsListener(ctx.tab1Browser, "load", step6, 4);
+    
+    
+    nShotsListener(ctx.tab1Browser, "load", step6, 4);
+  });
 }
 
 function step6() {
 
   
-  ok(!isActive(ctx.tab0Window), "Tab 0 should be inactive");
-  ok(isActive(ctx.tab1Window), "Tab 1 should be active");
-  ok(isActive(ctx.tab1Window.frames[0]), "Tab1 iframe 0 should be active");
-  ok(isActive(ctx.tab1Window.frames[0].frames[0]), "Tab1 iframe 0 subiframe 0 should be active");
-  ok(isActive(ctx.tab1Window.frames[1]), "Tab1 iframe 1 should be active");
-  ok(!isActive(ctx.tab2Window), "Tab 2 should be inactive");
-  ok(!isActive(ctx.tab2Window.frames[0]), "Tab2 iframe 0 should be inactive");
-  ok(!isActive(ctx.tab2Window.frames[1]), "Tab2 iframe 1 should be inactive");
+  ok(!ctx.tab0Browser.docShellIsActive, "Tab 0 should be inactive");
+  ok(ctx.tab1Browser.docShellIsActive, "Tab 1 should be active");
+  ContentTask.spawn(ctx.tab1Browser, null, function* () {
+    function isActive(aWindow) {
+      var docshell = aWindow.QueryInterface(Ci.nsIInterfaceRequestor)
+                            .getInterface(Ci.nsIWebNavigation)
+                            .QueryInterface(Ci.nsIDocShell);
+      return docshell.isActive;
+    }
 
-  
-  oneShotListener(ctx.tab2Browser, "pageshow", step7);
-  var tab2docshell = ctx.tab2Window.QueryInterface(Ci.nsIInterfaceRequestor)
-                                   .getInterface(Ci.nsIWebNavigation);
-  tab2docshell.goForward();
+    ok(isActive(content.frames[0]), "Tab1 iframe 0 should be active");
+    ok(isActive(content.frames[0].frames[0]), "Tab1 iframe 0 subiframe 0 should be active");
+    ok(isActive(content.frames[1]), "Tab1 iframe 1 should be active");
+  }).then(() => {
+    ok(!ctx.tab2Browser.docShellIsActive, "Tab 2 should be inactive");
+    return ContentTask.spawn(ctx.tab2Browser, null, function* () {
+      for (var i = 0; i < content.frames.length; i++) {
+        let docshell = content.frames[i].QueryInterface(Ci.nsIInterfaceRequestor)
+                                        .getInterface(Ci.nsIWebNavigation)
+                                        .QueryInterface(Ci.nsIDocShell);
+
+        ok(!docShell.isActive, `Tab2 iframe ${i} should be inactive`);
+      }
+    });
+  }).then(() => {
+    
+    waitForPageshow(ctx.tab2Browser, step7);
+    ctx.tab2Browser.goForward();
+  });
 }
 
 function step7() {
+  function checkBrowser(browser, tabNum, active) {
+    return ContentTask.spawn(browser, { tabNum, active },
+                             function* ({ tabNum, active }) {
+             function isActive(aWindow) {
+               var docshell = aWindow.QueryInterface(Ci.nsIInterfaceRequestor)
+                                     .getInterface(Ci.nsIWebNavigation)
+                                     .QueryInterface(Ci.nsIDocShell);
+               return docshell.isActive;
+             }
 
-  ctx.tab2Window = ctx.tab2Browser.contentWindow;
+             let activestr = active ? "active" : "inactive";
+             is(isActive(content.frames[0]), active,
+                `Tab${tabNum} iframe 0 should be ${activestr}`);
+             is(isActive(content.frames[0].frames[0]), active,
+                `Tab${tabNum} iframe 0 subiframe 0 should be ${activestr}`);
+             is(isActive(content.frames[1]), active,
+                `Tab${tabNum} iframe 1 should be ${activestr}`);
+           });
+  }
 
   
-  ok(!isActive(ctx.tab0Window), "Tab 0 should be inactive");
-  ok(isActive(ctx.tab1Window), "Tab 1 should be active");
-  ok(isActive(ctx.tab1Window.frames[0]), "Tab1 iframe 0 should be active");
-  ok(isActive(ctx.tab1Window.frames[0].frames[0]), "Tab1 iframe 0 subiframe 0 should be active");
-  ok(isActive(ctx.tab1Window.frames[1]), "Tab1 iframe 1 should be active");
-  ok(!isActive(ctx.tab2Window), "Tab 2 should be inactive");
-  ok(!isActive(ctx.tab2Window.frames[0]), "Tab2 iframe 0 should be inactive");
-  ok(!isActive(ctx.tab2Window.frames[0].frames[0]), "Tab2 iframe 0 subiframe 0 should be inactive");
-  ok(!isActive(ctx.tab2Window.frames[1]), "Tab2 iframe 1 should be inactive");
-
-  
-  allDone();
+  ok(!ctx.tab0Browser.docShellIsActive, "Tab 0 should be inactive");
+  ok(ctx.tab1Browser.docShellIsActive, "Tab 1 should be active");
+  checkBrowser(ctx.tab1Browser, 1, true).then(() => {
+    ok(!ctx.tab2Browser.docShellIsActive, "Tab 2 should be inactive");
+    return checkBrowser(ctx.tab2Browser, 2, false);
+  }).then(() => {
+    
+    allDone();
+  });
 }
 
 function allDone() {
