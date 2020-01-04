40f3125ec0ba5d3d06c86601f0626e0cc3404638
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 06:05:28 +0000

    Bug 1228542 - Resetting AuthorStyleSheets. r=bz
    UltraBlame original commit: d9a1efa7d1ab0c74cc2f297e627221d454515fe3

--- dom/base/nsDocument.cpp
+++ dom/base/nsDocument.cpp
@@ -2311,6 +2311,11 @@ nsDocument::ResetStylesheetsToURI(nsIURI* aURI)
   RemoveStyleSheetsFromStyleSets(mAdditionalSheets[eUserSheet], SheetType::User);
   RemoveStyleSheetsFromStyleSets(mAdditionalSheets[eAuthorSheet], SheetType::Doc);
 
+  nsStyleSheetService *sheetService = nsStyleSheetService::GetInstance();
+  if (sheetService) {
+    RemoveStyleSheetsFromStyleSets(*sheetService->AuthorStyleSheets(), SheetType::Doc);
+  }
+
   
   mStyleSheets.Clear();
   mOnDemandBuiltInUASheets.Clear();
