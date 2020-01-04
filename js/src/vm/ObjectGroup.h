08fdfe999105dc849a8da0d84a9cc89e9a07276f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 20:58:38 +0000

    Bug 1281168 - Make hashing ObjectGroupCompartment::NewEntry fallible r=terrence
    UltraBlame original commit: 15a9785381a4068b90c1b5b52199ad1002919a8b

--- js/src/vm/ObjectGroup.h
+++ js/src/vm/ObjectGroup.h
@@ -542,7 +542,6 @@ class ObjectGroupCompartment
 {
     friend class ObjectGroup;
 
-    struct NewEntry;
     class NewTable;
 
     
@@ -585,6 +584,8 @@ class ObjectGroupCompartment
     AllocationSiteTable* allocationSiteTable;
 
   public:
+    struct NewEntry;
+
     ObjectGroupCompartment();
     ~ObjectGroupCompartment();
 
