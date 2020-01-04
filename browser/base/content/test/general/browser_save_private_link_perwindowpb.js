391391979907372063365e737b84d64122f676aa
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 14:35:55 +0000

    Back out changeset 105e0e4b9d80 (bug 1263031) for Windows failures in browser_save_private_link_perwindowpb.js
    UltraBlame original commit: 7116915548728dd89dd80455daee47f1adff2e0c

--- browser/base/content/test/general/browser_save_private_link_perwindowpb.js
+++ browser/base/content/test/general/browser_save_private_link_perwindowpb.js
@@ -2,111 +2,137 @@
 
 
 
-function createTemporarySaveDirectory() {
-  var saveDir = Cc["@mozilla.org/file/directory_service;1"]
-                  .getService(Ci.nsIProperties)
-                  .get("TmpD", Ci.nsIFile);
-  saveDir.append("testsavedir");
-  if (!saveDir.exists())
-    saveDir.create(Ci.nsIFile.DIRECTORY_TYPE, 0o755);
-  return saveDir;
-}
+var {LoadContextInfo} = Cu.import("resource://gre/modules/LoadContextInfo.jsm", null);
 
-function promiseNoCacheEntry() {
+function test() {
+  
+  waitForExplicitFinish();
+  let windowsToClose = [];
+  let testURI = "http://mochi.test:8888/browser/browser/base/content/test/general/bug792517.html";
+  let fileName;
+  let MockFilePicker = SpecialPowers.MockFilePicker;
   let cache = Cc["@mozilla.org/netwerk/cache-storage-service;1"]
               .getService(Ci.nsICacheStorageService);
-  cache.clear();
-  return new Promise((resolve, reject) => {
-    let entries = 0;
+
+  function checkDiskCacheFor(filename, goon) {
     Visitor.prototype = {
       onCacheStorageInfo: function(num, consumption)
       {
         info("disk storage contains " + num + " entries");
-        entries += num;
       },
       onCacheEntryInfo: function(uri)
       {
-        let urispec = uri.asciiSpec;
-        ok(false, "web content present in disk cache " + urispec);
+        var urispec = uri.asciiSpec;
+        info(urispec);
+        is(urispec.includes(filename), false, "web content present in disk cache");
       },
-      onCacheEntryVisitCompleted: function() {
-        is(entries, 0, "no content cached");
-        resolve();
+      onCacheEntryVisitCompleted: function()
+      {
+        goon();
       }
     };
     function Visitor() {}
 
-    let {LoadContextInfo} = Cu.import("resource://gre/modules/LoadContextInfo.jsm", null);
-    let storage = cache.diskCacheStorage(LoadContextInfo.default, false);
+    var storage = cache.diskCacheStorage(LoadContextInfo.default, false);
     storage.asyncVisitStorage(new Visitor(), true );
-  });
-}
+  }
 
-function promiseImageDownloaded() {
-  return new Promise((resolve, reject) => {
-    let fileName;
-    let MockFilePicker = SpecialPowers.MockFilePicker;
-    MockFilePicker.init(window);
-
-    function onTransferComplete(downloadSuccess) {
-      ok(downloadSuccess, "Image file should have been downloaded successfully ");
-
-      
-      resolve(fileName);
-    }
+  function onTransferComplete(downloadSuccess) {
+    ok(downloadSuccess, "Image file should have been downloaded successfully");
 
     
-    var destDir = createTemporarySaveDirectory();
-    var destFile = destDir.clone();
-
-    MockFilePicker.displayDirectory = destDir;
-    MockFilePicker.showCallback = function(fp) {
-      fileName = fp.defaultString;
-      destFile.append (fileName);
-      MockFilePicker.returnFiles = [destFile];
-      MockFilePicker.filterIndex = 1; 
-    };
-
-    mockTransferCallback = onTransferComplete;
-    mockTransferRegisterer.register();
-
-    registerCleanupFunction(function () {
+    executeSoon(function() {
+      checkDiskCacheFor(fileName, finish);
       mockTransferCallback = null;
-      mockTransferRegisterer.unregister();
-      MockFilePicker.cleanup();
-      destDir.remove(true);
     });
+  }
 
+  function createTemporarySaveDirectory() {
+    var saveDir = Cc["@mozilla.org/file/directory_service;1"]
+                    .getService(Ci.nsIProperties)
+                    .get("TmpD", Ci.nsIFile);
+    saveDir.append("testsavedir");
+    if (!saveDir.exists())
+      saveDir.create(Ci.nsIFile.DIRECTORY_TYPE, 0o755);
+    return saveDir;
+  }
+
+  function doTest(aIsPrivateMode, aWindow, aCallback) {
+    function contextMenuOpened(event) {
+      cache.clear();
+
+      aWindow.document.removeEventListener("popupshown", contextMenuOpened);
+
+      
+      var destDir = createTemporarySaveDirectory();
+      var destFile = destDir.clone();
+
+      MockFilePicker.displayDirectory = destDir;
+      MockFilePicker.showCallback = function(fp) {
+        fileName = fp.defaultString;
+        destFile.append (fileName);
+        MockFilePicker.returnFiles = [destFile];
+        MockFilePicker.filterIndex = 1; 
+      };
+
+      mockTransferCallback = onTransferComplete;
+      mockTransferRegisterer.register();
+
+      registerCleanupFunction(function () {
+        mockTransferRegisterer.unregister();
+        MockFilePicker.cleanup();
+        destDir.remove(true);
+      });
+
+      
+      var saveVideoCommand = aWindow.document.getElementById("context-saveimage");
+      saveVideoCommand.doCommand();
+
+      event.target.hidePopup();
+    }
+
+    aWindow.gBrowser.addEventListener("pageshow", function pageShown(event) {
+      
+      if (event.target.location == "about:blank" ||
+          event.target.location == "about:privatebrowsing") {
+        aWindow.gBrowser.selectedBrowser.loadURI(testURI);
+        return;
+      }
+      aWindow.gBrowser.removeEventListener("pageshow", pageShown);
+
+      waitForFocus(function () {
+        aWindow.document.addEventListener("popupshown", contextMenuOpened, false);
+        var img = aWindow.gBrowser.selectedBrowser.contentDocument.getElementById("img");
+        EventUtils.synthesizeMouseAtCenter(img,
+                                           { type: "contextmenu", button: 2 },
+                                           aWindow.gBrowser.contentWindow);
+      }, aWindow.gBrowser.selectedBrowser);
+    });
+  }
+
+  function testOnWindow(aOptions, aCallback) {
+    whenNewWindowLoaded(aOptions, function(aWin) {
+      windowsToClose.push(aWin);
+      
+      
+      
+      executeSoon(() => aCallback(aWin));
+    });
+  };
+
+   
+  registerCleanupFunction(function() {
+    windowsToClose.forEach(function(aWin) {
+      aWin.close();
+    });
+  });
+
+  MockFilePicker.init(window);
+  
+  testOnWindow({private: true}, function(aWin) {
+    doTest(true, aWin, finish);
   });
 }
-
-add_task(function* () {
-  let testURI = "http://mochi.test:8888/browser/browser/base/content/test/general/bug792517.html";
-  let privateWindow = yield BrowserTestUtils.openNewBrowserWindow({private: true});
-  let tab = yield BrowserTestUtils.openNewForegroundTab(privateWindow.gBrowser, testURI);
-
-  let contextMenu = privateWindow.document.getElementById("contentAreaContextMenu");
-  let popupShown = BrowserTestUtils.waitForEvent(contextMenu, "popupshown");
-  let popupHidden = BrowserTestUtils.waitForEvent(contextMenu, "popuphidden");
-  yield BrowserTestUtils.synthesizeMouseAtCenter("#img", {
-    type: "contextmenu",
-    button: 2
-  }, tab.linkedBrowser);
-  yield popupShown;
-
-  let imageDownloaded = promiseImageDownloaded();
-  
-  privateWindow.document.getElementById("context-saveimage").doCommand();
-
-  contextMenu.hidePopup();
-  yield popupHidden;
-
-  
-  yield imageDownloaded;
-  yield promiseNoCacheEntry();
-
-  yield BrowserTestUtils.closeWindow(privateWindow);
-});
 
 Cc["@mozilla.org/moz/jssubscript-loader;1"]
   .getService(Ci.mozIJSSubScriptLoader)
