0560c6fc2fc6048f40c09926fe70826550b5c75d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 21:11:27 +0000

    Bug 1288821 - Remove nsIAlertsProgressListener; r=kitcambridge r=snorp
    nsIAlertsProgressListener is a relic from XUL Fennec and we no longer
    use it in any code.
    
    UltraBlame original commit: 79374eef5dcee186392595e1de8a4eea1c69ce67

--- mobile/android/geckoview/src/main/java/org/mozilla/gecko/notifications/NotificationClient.java
+++ mobile/android/geckoview/src/main/java/org/mozilla/gecko/notifications/NotificationClient.java
@@ -22,52 +22,6 @@ public abstract class NotificationClient {
     private volatile NotificationHandler mHandler;
     private boolean mReady;
     private final LinkedList<Runnable> mTaskQueue = new LinkedList<Runnable>();
-    private final ConcurrentHashMap<Integer, UpdateRunnable> mUpdatesMap =
-            new ConcurrentHashMap<Integer, UpdateRunnable>();
-
-    
-
-
-
-
-    private class UpdateRunnable implements Runnable {
-        private long mProgress;
-        private long mProgressMax;
-        private String mAlertText;
-        final private int mNotificationID;
-
-        public UpdateRunnable(int notificationID) {
-            mNotificationID = notificationID;
-        }
-
-        public synchronized boolean updateProgress(long progress, long progressMax, String alertText) {
-            if (progress == mProgress
-                    && mProgressMax == progressMax
-                    && TextUtils.equals(mAlertText, alertText)) {
-                return false;
-            }
-
-            mProgress = progress;
-            mProgressMax = progressMax;
-            mAlertText = alertText;
-            return true;
-        }
-
-        @Override
-        public void run() {
-            long progress;
-            long progressMax;
-            String alertText;
-
-            synchronized (this) {
-                progress = mProgress;
-                progressMax = mProgressMax;
-                alertText = mAlertText;
-            }
-
-            mHandler.update(mNotificationID, progress, progressMax, alertText);
-        }
-    }
 
     
 
@@ -114,40 +68,11 @@ public abstract class NotificationClient {
 
 
 
-    public void update(final int notificationID, final long aProgress, final long aProgressMax,
-                       final String aAlertText) {
-        UpdateRunnable runnable = mUpdatesMap.get(notificationID);
-
-        if (runnable == null) {
-            runnable = new UpdateRunnable(notificationID);
-            mUpdatesMap.put(notificationID, runnable);
-        }
-
-        
-        
-        if (!runnable.updateProgress(aProgress, aProgressMax, aAlertText)) {
-            return;
-        }
-
-        synchronized (this) {
-            if (mReady) {
-                mTaskQueue.add(runnable);
-                notify();
-            }
-        }
-    }
-
-    
-
-
-
-
     public synchronized void remove(final int notificationID) {
         mTaskQueue.add(new Runnable() {
             @Override
             public void run() {
                 mHandler.remove(notificationID);
-                mUpdatesMap.remove(notificationID);
             }
         });
 
@@ -176,7 +101,6 @@ public abstract class NotificationClient {
 
     protected void unbind() {
         mReady = false;
-        mUpdatesMap.clear();
     }
 
     protected void connectHandler(NotificationHandler handler) {
