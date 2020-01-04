bc0a02889c5565013081d8f3c1dc5419e082f750
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 15:50:48 +0000

    Bug 1193762 part 1 - Remove nsEditor::IsBlockNode(nsIDOMNode*); r=ehsan
    UltraBlame original commit: 05992224611e290e9224e2f4f7a0bf96dd3b0f6e

--- editor/libeditor/nsHTMLDataTransfer.cpp
+++ editor/libeditor/nsHTMLDataTransfer.cpp
@@ -381,8 +381,8 @@ nsHTMLEditor::DoInsertHTMLWithContext(const nsAString & aInputString,
     NS_ENSURE_TRUE(parentNode, NS_ERROR_FAILURE);
 
     
-    NormalizeEOLInsertPosition(GetAsDOMNode(nodeList[0]),
-                               address_of(parentNode), &offsetOfNewNode);
+    NormalizeEOLInsertPosition(nodeList[0], address_of(parentNode),
+                               &offsetOfNewNode);
 
     
     
@@ -456,7 +456,9 @@ nsHTMLEditor::DoInsertHTMLWithContext(const nsAString & aInputString,
     nsCOMPtr<nsIDOMNode> parentBlock, lastInsertNode, insertedContextParent;
     int32_t listCount = nodeList.Length();
     int32_t j;
-    if (IsBlockNode(parentNode))
+    nsCOMPtr<nsINode> parentNodeNode = do_QueryInterface(parentNode);
+    NS_ENSURE_STATE(parentNodeNode || !parentNode);
+    if (IsBlockNode(parentNodeNode))
       parentBlock = parentNode;
     else
       parentBlock = GetBlockNodeParent(parentNode);
