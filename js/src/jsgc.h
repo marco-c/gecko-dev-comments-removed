9db44758ca50cf91c1e6a10baafeea434b1658e0
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 01:52:08 +0000

    Bug 1201057 - Move AutoEnterOOMUnsafeRegion to Utility.h with the other OOM simulation infrastructure r=terrence
    UltraBlame original commit: 1d051e09610645d5b33321293b52103641f40268

--- js/src/jsgc.h
+++ js/src/jsgc.h
@@ -1312,33 +1312,6 @@ class MOZ_RAII AutoSuppressGC
     }
 };
 
-#ifdef DEBUG
-
-class MOZ_RAII AutoEnterOOMUnsafeRegion
-{
-    bool oomEnabled_;
-    int64_t oomAfter_;
-
-  public:
-    AutoEnterOOMUnsafeRegion()
-      : oomEnabled_(OOM_maxAllocations != UINT32_MAX), oomAfter_(0)
-    {
-        if (oomEnabled_) {
-            oomAfter_ = OOM_maxAllocations - OOM_counter;
-            OOM_maxAllocations = UINT32_MAX;
-        }
-    }
-
-    ~AutoEnterOOMUnsafeRegion() {
-        MOZ_ASSERT(OOM_maxAllocations == UINT32_MAX);
-        if (oomEnabled_)
-            OOM_maxAllocations = OOM_counter + oomAfter_;
-    }
-};
-#else
-class MOZ_RAII AutoEnterOOMUnsafeRegion {};
-#endif 
-
 
 class ZoneList
 {
