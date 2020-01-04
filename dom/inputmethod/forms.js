feb2d6ba94ec37cd9d5c95757ba36ad757451a53
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 00:03:07 +0000

    Bug 1137557 - Part 3: Allow content to pass a dict representing the property of the keyboard event to send. r=masayuki, sr=smaug
    - Overloading MozInputContext#sendKey() so it could take a dict.
    - An optional trailing argument for setComposition() and endComposition() methods for these methods to take the dict.
    - New keydown() and keyup() methods that takes dict as the only argument.
    
    UltraBlame original commit: 478dd607957309378617041c9dad3ad0f8402e99

--- dom/inputmethod/forms.js
+++ dom/inputmethod/forms.js
@@ -11,6 +11,7 @@ dump("###################################### forms.js loaded\n");
 let Ci = Components.interfaces;
 let Cc = Components.classes;
 let Cu = Components.utils;
+let Cr = Components.results;
 
 Cu.import("resource://gre/modules/Services.jsm");
 Cu.import('resource://gre/modules/XPCOMUtils.jsm');
@@ -652,7 +653,7 @@ let FormAssistant = {
         break;
 
       case "keydown":
-        if (!this.focusedElement) {
+        if (!this.focusedElement || this._editing) {
           break;
         }
 
@@ -660,7 +661,7 @@ let FormAssistant = {
         break;
 
       case "keyup":
-        if (!this.focusedElement) {
+        if (!this.focusedElement || this._editing) {
           break;
         }
 
@@ -736,54 +737,99 @@ let FormAssistant = {
         }
 
         
-        let printable = !!json.charCode;
+        
+        
+        
+        let keyboardEventDict = json.keyboardEventDict;
+        let flags = 0;
 
-        let keyboardEventDict = {
+        if (keyboardEventDict) {
+          if ('flags' in keyboardEventDict) {
+            flags = keyboardEventDict.flags;
+          }
+        } else {
+          
+          let printable = !!json.charCode;
+
           
           
           
-          key: printable ?
-            String.fromCharCode(json.charCode) :
-            guessKeyNameFromKeyCode(win.KeyboardEvent, json.keyCode),
+          let key = printable ?
+              String.fromCharCode(json.charCode) :
+              guessKeyNameFromKeyCode(win.KeyboardEvent, json.keyCode);
+
           
           
-          code: "",
           
           
-          
-          keyCode: json.keyCode,
-          
-          
-          location: 0,
-          
-          repeat: json.repeat
-        };
+          let keyCode = (printable && /^[a-zA-Z0-9]$/.test(key)) ?
+              key.toUpperCase().charCodeAt(0) :
+              json.keyCode;
+
+          keyboardEventDict = {
+            key: key,
+            keyCode: keyCode,
+            
+            
+            code: "",
+            
+            
+            location: 0,
+            
+            repeat: json.repeat
+          };
+
+          flags = tip.KEY_KEEP_KEY_LOCATION_STANDARD;
+          if (!printable) {
+            flags |= tip.KEY_NON_PRINTABLE_KEY;
+          }
+          if (!keyboardEventDict.keyCode) {
+            flags |= tip.KEY_KEEP_KEYCODE_ZERO;
+          }
+        }
 
         let keyboardEvent = new win.KeyboardEvent("", keyboardEventDict);
-        let flags = tip.KEY_KEEP_KEY_LOCATION_STANDARD;
-        if (!printable) {
-          flags |= tip.KEY_NON_PRINTABLE_KEY;
-        }
-        if (!json.keyCode) {
-          flags |= tip.KEY_KEEP_KEYCODE_ZERO;
-        }
 
-        let keydownDefaultPrevented;
+        let keydownDefaultPrevented = false;
         try {
-          let consumedFlags = tip.keydown(keyboardEvent, flags);
-          keydownDefaultPrevented =
-            !!(tip.KEYDOWN_IS_CONSUMED & consumedFlags);
-          if (!json.repeat) {
-            tip.keyup(keyboardEvent, flags);
+          switch (json.method) {
+            case 'sendKey': {
+              let consumedFlags = tip.keydown(keyboardEvent, flags);
+              keydownDefaultPrevented =
+                !!(tip.KEYDOWN_IS_CONSUMED & consumedFlags);
+              if (!keyboardEventDict.repeat) {
+                tip.keyup(keyboardEvent, flags);
+              }
+              break;
+            }
+            case 'keydown': {
+              let consumedFlags = tip.keydown(keyboardEvent, flags);
+              keydownDefaultPrevented =
+                !!(tip.KEYDOWN_IS_CONSUMED & consumedFlags);
+              break;
+            }
+            case 'keyup': {
+              tip.keyup(keyboardEvent, flags);
+
+              break;
+            }
           }
-        } catch (e) {
-          dump("forms.js:" + e.toString() + "\n");
+        } catch (err) {
+          dump("forms.js:" + err.toString() + "\n");
 
           if (json.requestId) {
-            sendAsyncMessage("Forms:SendKey:Result:Error", {
-              requestId: json.requestId,
-              error: "Unable to type into destoryed input."
-            });
+            if (err instanceof Ci.nsIException &&
+                err.result == Cr.NS_ERROR_ILLEGAL_VALUE) {
+              sendAsyncMessage("Forms:SendKey:Result:Error", {
+                requestId: json.requestId,
+                error: "The values specified are illegal."
+              });
+            } else {
+              sendAsyncMessage("Forms:SendKey:Result:Error", {
+                requestId: json.requestId,
+                error: "Unable to type into destroyed input."
+              });
+            }
           }
 
           break;
@@ -915,7 +961,7 @@ let FormAssistant = {
 
       case "Forms:SetComposition": {
         CompositionManager.setComposition(target, json.text, json.cursor,
-                                          json.clauses);
+                                          json.clauses, json.keyboardEventDict);
         sendAsyncMessage("Forms:SetComposition:Result:OK", {
           requestId: json.requestId,
           selectioninfo: this.getSelectionInfo()
@@ -924,7 +970,7 @@ let FormAssistant = {
       }
 
       case "Forms:EndComposition": {
-        CompositionManager.endComposition(json.text);
+        CompositionManager.endComposition(json.text, json.keyboardEventDict);
         sendAsyncMessage("Forms:EndComposition:Result:OK", {
           requestId: json.requestId,
           selectioninfo: this.getSelectionInfo()
@@ -1444,6 +1490,7 @@ function replaceSurroundingText(element, text, offset, length) {
 let CompositionManager =  {
   _isStarted: false,
   _tip: null,
+  _KeyboardEventForWin: null,
   _clauseAttrMap: {
     'raw-input':
       Ci.nsITextInputProcessor.ATTR_RAW_CLAUSE,
@@ -1455,7 +1502,7 @@ let CompositionManager =  {
       Ci.nsITextInputProcessor.ATTR_SELECTED_CLAUSE
   },
 
-  setComposition: function cm_setComposition(element, text, cursor, clauses) {
+  setComposition: function cm_setComposition(element, text, cursor, clauses, dict) {
     
     if (!element) {
       return;
@@ -1508,13 +1555,22 @@ let CompositionManager =  {
     if (cursor >= 0) {
       tip.setCaretInPendingComposition(cursor);
     }
-    this._isStarted = tip.flushPendingComposition();
+
+    if (!dict) {
+      this._isStarted = tip.flushPendingComposition();
+    } else {
+      let keyboardEvent = new win.KeyboardEvent("", dict);
+      let flags = dict.flags;
+      this._isStarted = tip.flushPendingComposition(keyboardEvent, flags);
+    }
+
     if (this._isStarted) {
       this._tip = tip;
+      this._KeyboardEventForWin = win.KeyboardEvent;
     }
   },
 
-  endComposition: function cm_endComposition(text) {
+  endComposition: function cm_endComposition(text, dict) {
     if (!this._isStarted) {
       return;
     }
@@ -1523,9 +1579,18 @@ let CompositionManager =  {
       return;
     }
 
-    tip.commitCompositionWith(text ? text : "");
+    text = text || "";
+    if (!dict) {
+      tip.commitCompositionWith(text);
+    } else {
+      let keyboardEvent = new this._KeyboardEventForWin("", dict);
+      let flags = dict.flags;
+      tip.commitCompositionWith(text, keyboardEvent, flags);
+    }
+
     this._isStarted = false;
     this._tip = null;
+    this._KeyboardEventForWin = null;
   },
 
   
@@ -1536,5 +1601,6 @@ let CompositionManager =  {
 
     this._isStarted = false;
     this._tip = null;
+    this._KeyboardEventForWin = null;
   }
 };
