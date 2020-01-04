8ef902dd13394347da51d9f449bd6a64050b85d1
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 14:23:10 +0000

    Backed out changeset 1bad1b961817 (bug 1243722) for test failures in test_recipe_login_fields.html
    UltraBlame original commit: 8d3c7fe689607ffcbb9c81268d6d1b80e1af81a2

--- toolkit/components/passwordmgr/test/pwmgr_common.js
+++ toolkit/components/passwordmgr/test/pwmgr_common.js
@@ -282,26 +282,6 @@ function promiseFormsProcessed(expectedCount = 1) {
   });
 }
 
-function loadRecipes(recipes) {
-  return new Promise(resolve => {
-    chromeScript.addMessageListener("loadedRecipes", function loaded() {
-      chromeScript.removeMessageListener("loadedRecipes", loaded);
-      resolve(recipes);
-    });
-    chromeScript.sendAsyncMessage("loadRecipes", recipes);
-  });
-}
-
-function resetRecipes() {
-  return new Promise(resolve => {
-    chromeScript.addMessageListener("recipesReset", function reset() {
-      chromeScript.removeMessageListener("recipesReset", reset);
-      resolve();
-    });
-    chromeScript.sendAsyncMessage("resetRecipes");
-  });
-}
-
 
 
 
@@ -330,7 +310,6 @@ function runChecksAfterCommonInit(aFunction = null) {
     pwmgrCommonScript.addMessageListener("registerRunTests", () => registerRunTests());
   }
   pwmgrCommonScript.sendSyncMessage("setupParent");
-  return pwmgrCommonScript;
 }
 
 
@@ -353,13 +332,6 @@ if (this.addMessageListener) {
     var recipeParent = yield LoginManagerParent.recipeParentPromise;
     yield recipeParent.load(recipes);
     sendAsyncMessage("loadedRecipes", recipes);
-  }));
-
-  addMessageListener("resetRecipes", Task.async(function* resetRecipes() {
-    let { LoginManagerParent } = Cu.import("resource://gre/modules/LoginManagerParent.jsm", {});
-    let recipeParent = yield LoginManagerParent.recipeParentPromise;
-    yield recipeParent.reset();
-    sendAsyncMessage("recipesReset");
   }));
 
   var globalMM = Cc["@mozilla.org/globalmessagemanager;1"].getService(Ci.nsIMessageListenerManager);
