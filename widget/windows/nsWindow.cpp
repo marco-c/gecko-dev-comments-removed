24cbbf03c7699f2d84f58c948fa34ec88bf1d0a9
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 11:41:37 +0000

    Bug 1033488 - Send RTL information to child process by WM_INPUTLANGCHANGE. r=masayuki
    MozReview-Commit-ID: KB9ok2oly7y
    
    UltraBlame original commit: bd788a05e4dc1c40739a5a8e5343a0a19df9dc75

--- widget/windows/nsWindow.cpp
+++ widget/windows/nsWindow.cpp
@@ -134,6 +134,7 @@
 #include "mozilla/TextEvents.h" 
 #include "mozilla/TextEventDispatcherListener.h"
 #include "nsThemeConstants.h"
+#include "nsBidiKeyboard.h"
 
 #include "nsIGfxInfo.h"
 #include "nsUXThemeConstants.h"
@@ -5515,6 +5516,7 @@ nsWindow::ProcessMessage(UINT msg, WPARAM& wParam, LPARAM& lParam,
     case WM_INPUTLANGCHANGE:
       KeyboardLayout::GetInstance()->
         OnLayoutChange(reinterpret_cast<HKL>(lParam));
+      nsBidiKeyboard::OnLayoutChange();
       result = false; 
       break;
 
