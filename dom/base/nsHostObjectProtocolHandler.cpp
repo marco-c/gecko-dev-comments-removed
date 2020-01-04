1d213cc3edb214bcaf7f0acd24a0d34ff46284f1
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 03:21:34 +0000

    Bug 944616 - "Blob URLs don't allow query or fragment parts". r=bz
    UltraBlame original commit: 73f8f4985df4318730c919c80de59d40e546617c

--- dom/base/nsHostObjectProtocolHandler.cpp
+++ dom/base/nsHostObjectProtocolHandler.cpp
@@ -419,16 +419,26 @@ GetDataInfo(const nsACString& aUri)
   }
 
   DataInfo* res;
-  nsCString uriIgnoringRef;
-  int32_t hashPos = aUri.FindChar('#');
-  if (hashPos < 0) {
-    uriIgnoringRef = aUri;
-  }
-  else {
-    uriIgnoringRef = StringHead(aUri, hashPos);
-  }
-  gDataTable->Get(uriIgnoringRef, &res);
+
   
+  int32_t hasFragmentPos = aUri.FindChar('#');
+  int32_t hasQueryPos = aUri.FindChar('?');
+
+  int32_t pos = -1;
+  if (hasFragmentPos >= 0 && hasQueryPos >= 0) {
+    pos = std::min(hasFragmentPos, hasQueryPos);
+  } else if (hasFragmentPos >= 0) {
+    pos = hasFragmentPos;
+  } else {
+    pos = hasQueryPos;
+  }
+
+  if (pos < 0) {
+    gDataTable->Get(aUri, &res);
+  } else {
+    gDataTable->Get(StringHead(aUri, pos), &res);
+  }
+
   return res;
 }
 
