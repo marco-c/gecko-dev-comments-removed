10de17022ab9819e96d5aa0c27e8482ad3589383
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 04:03:17 +0000

    Bug 1207827 - Delete Assembler::actualOffset() and transitive closure. r=nbp
    The implemetation of Assembler::actualOffset() is now a no-op in all
    targets, and it is no longer necessary to rewrite assembler buffer
    offsets to their final form after finishing the assembler buffer.
    
    - Delete Assembler::actualOffset() in all targets.
    - Delete AsmJSModule::CodeRange::updateOffsets().
    - Delete AsmJSModule::updateCodeOffset().
    - Delete AsmJSModule::updateOffsets().
    - Delete CodeOffsetLabel::fixup().
    - Delete ICEnty::fixupReturnOffset().
    - Delete LSafepoint::fixupOffset().
    - Delete OsiIndex::fixUpOffset().
    - Delete PCMappingEntry::fixupNativeOffset().
    
    Also delete all code calling these functions.
    
    UltraBlame original commit: 97366c8fa024314ee9f130e542e1e150e96f7e6d

--- js/src/jit/JitFrames.h
+++ js/src/jit/JitFrames.h
@@ -173,7 +173,6 @@ class OsiIndex
     uint32_t snapshotOffset() const {
         return snapshotOffset_;
     }
-    void fixUpOffset(MacroAssembler& masm);
 };
 
 
--- js/src/jit/shared/CodeGenerator-shared.cpp
+++ js/src/jit/shared/CodeGenerator-shared.cpp
@@ -632,10 +632,8 @@ CodeGeneratorShared::encodeSafepoints()
     for (SafepointIndex& index : safepointIndices_) {
         LSafepoint* safepoint = index.safepoint();
 
-        if (!safepoint->encoded()) {
-            safepoint->fixupOffset(&masm);
+        if (!safepoint->encoded())
             safepoints_.encode(safepoint);
-        }
 
         index.resolve();
     }
@@ -709,14 +707,6 @@ CodeGeneratorShared::generateCompactNativeToBytecodeMap(JSContext* cx, JitCode* 
     MOZ_ASSERT(nativeToBytecodeMapSize_ == 0);
     MOZ_ASSERT(nativeToBytecodeTableOffset_ == 0);
     MOZ_ASSERT(nativeToBytecodeNumRegions_ == 0);
-
-    
-    for (unsigned i = 0; i < nativeToBytecodeList_.length(); i++) {
-        NativeToBytecode& entry = nativeToBytecodeList_[i];
-
-        
-        entry.nativeOffset = CodeOffsetLabel(masm.actualOffset(entry.nativeOffset.offset()));
-    }
 
     if (!createNativeToBytecodeScriptList(cx))
         return false;
@@ -864,11 +854,8 @@ CodeGeneratorShared::generateCompactTrackedOptimizationsMap(JSContext* cx, JitCo
         return false;
 
     
-    
     for (size_t i = 0; i < trackedOptimizations_.length(); i++) {
         NativeToTrackedOptimizations& entry = trackedOptimizations_[i];
-        entry.startOffset = CodeOffsetLabel(masm.actualOffset(entry.startOffset.offset()));
-        entry.endOffset = CodeOffsetLabel(masm.actualOffset(entry.endOffset.offset()));
         if (!unique.add(entry.optimizations))
             return false;
     }
