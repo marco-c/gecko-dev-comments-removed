baf167081df5f78d1309505f4d0c43144f436f55
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 14:22:38 +0000

    Bug 1262151 - Back out bug 1259593 for causing worse checkerboarding in some cases. r=backout
    UltraBlame original commit: bd68133cf77165a6e7fd8be2c914a47a293b3a17

--- layout/generic/nsGfxScrollFrame.cpp
+++ layout/generic/nsGfxScrollFrame.cpp
@@ -3488,19 +3488,7 @@ ScrollFrameHelper::DecideScrollableLayer(nsDisplayListBuilder* aBuilder,
 
               nsLayoutUtils::TransformRect(rootFrame, mOuter, rootCompBounds);
 
-              
-              
-              
-              if (rootCompBounds.width < displayportBase.width) {
-                nscoord diff = displayportBase.width - rootCompBounds.width;
-                displayportBase.x += diff / 2;
-                displayportBase.width -= diff;
-              }
-              if (rootCompBounds.height < displayportBase.height) {
-                nscoord diff = displayportBase.height - rootCompBounds.height;
-                displayportBase.y += diff / 2;
-                displayportBase.height -= diff;
-              }
+              displayportBase = displayportBase.Intersect(rootCompBounds);
             }
           }
         }
