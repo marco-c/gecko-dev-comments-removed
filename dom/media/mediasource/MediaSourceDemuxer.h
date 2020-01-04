e0644b9f5c9bb65840d8b2c36d4e3617f10577b6
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 12:20:47 +0000

    Bug 1259274: [MSE] P5. Use new AutoTaskQueue with MSE objects. r=gerald
    It was possible for a TrackBuffersManager to have pending tasks currently running while the MediaSourceDemuxer was shutting down the task queue. This would cause an assertion upon resolution of the promise attempting to schedule a new runnable as the task queue was now shutdown.
    
    The AutoTaskQueue only shuts down once it's no longer used.
    
    MozReview-Commit-ID: IzPh2OdGbvN
    
    UltraBlame original commit: 0996ba84787e3d49289545527b84740066f9805f

--- dom/media/mediasource/MediaSourceDemuxer.h
+++ dom/media/mediasource/MediaSourceDemuxer.h
@@ -10,7 +10,7 @@
 #include "mozilla/Atomics.h"
 #include "mozilla/Maybe.h"
 #include "mozilla/Monitor.h"
-#include "mozilla/TaskQueue.h"
+#include "AutoTaskQueue.h"
 
 #include "MediaDataDemuxer.h"
 #include "MediaDecoderReader.h"
@@ -47,7 +47,7 @@ public:
   
   void AttachSourceBuffer(TrackBuffersManager* aSourceBuffer);
   void DetachSourceBuffer(TrackBuffersManager* aSourceBuffer);
-  TaskQueue* GetTaskQueue() { return mTaskQueue; }
+  AutoTaskQueue* GetTaskQueue() { return mTaskQueue; }
 
   
   
@@ -73,7 +73,7 @@ private:
     return !GetTaskQueue() || GetTaskQueue()->IsCurrentThreadIn();
   }
 
-  RefPtr<TaskQueue> mTaskQueue;
+  RefPtr<AutoTaskQueue> mTaskQueue;
   nsTArray<RefPtr<MediaSourceTrackDemuxer>> mDemuxers;
 
   nsTArray<RefPtr<TrackBuffersManager>> mSourceBuffers;
