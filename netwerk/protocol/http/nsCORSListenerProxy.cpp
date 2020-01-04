93c1425e406c484f3eaa879b2add3a7a4c65cad3
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 01:22:36 +0000

    Bug 1203680 P8 nsCORSListenerProxy::OnStartRequest must always call outer OnStartRequest. r=ehsan
    UltraBlame original commit: e38a5ece0995e70472f6889653212373a55ab081

--- netwerk/protocol/http/nsCORSListenerProxy.cpp
+++ netwerk/protocol/http/nsCORSListenerProxy.cpp
@@ -516,8 +516,10 @@ nsCORSListenerProxy::OnStartRequest(nsIRequest* aRequest,
             do_QueryInterface(channel);
           if (httpChannelChild) {
             rv = httpChannelChild->RemoveCorsPreflightCacheEntry(uri, mRequestingPrincipal);
-            if (NS_WARN_IF(NS_FAILED(rv))) {
-              return rv;
+            if (NS_FAILED(rv)) {
+              
+              
+              NS_WARNING("Failed to remove CORS preflight cache entry!");
             }
           }
         }
