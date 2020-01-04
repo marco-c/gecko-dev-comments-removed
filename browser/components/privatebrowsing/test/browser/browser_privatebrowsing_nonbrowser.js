afdc8403c11960fd0f1e5bb4ec9645fc395d56b8
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 20:04:39 +0000

    Bug 1261842 - Fix up some private browsing tests to account for initial browsers being remote now. r=jdm
    MozReview-Commit-ID: CxhKsIXnPxj
    
    UltraBlame original commit: f6062f1139114f586e3ea9f6d946766f63dedb1d

--- browser/components/privatebrowsing/test/browser/browser_privatebrowsing_nonbrowser.js
+++ browser/components/privatebrowsing/test/browser/browser_privatebrowsing_nonbrowser.js
@@ -1,34 +1,19 @@
+"use strict";
 
 
 
 
-function test() {
-  waitForExplicitFinish();
 
-  let windowsToClose = [];
-  registerCleanupFunction(function() {
-    windowsToClose.forEach(function(win) {
-      win.close();
-    });
-  });
 
-  let win = OpenBrowserWindow({private: true});
-  win.addEventListener("load", function onLoad() {
-    win.removeEventListener("load", onLoad, false);
-    let chromeWin = win.open("chrome://browser/content/places/places.xul",
-      "_blank", "chrome,extrachrome,menubar,resizable,scrollbars,status,toolbar");
-    chromeWin.addEventListener("load", function chromeWinLoad() {
-      chromeWin.removeEventListener("load", chromeWinLoad, false);
-      win.close();
-    }, false);
-    windowsToClose.push(chromeWin);
-  }, false);
+add_task(function* () {
+  let win = yield BrowserTestUtils.openNewBrowserWindow({private: true});
+  let chromeWin = win.open("chrome://browser/content/places/places.xul", "_blank",
+    "chrome,extrachrome,menubar,resizable,scrollbars,status,toolbar");
+  yield BrowserTestUtils.waitForEvent(chromeWin, "load");
+  let obsPromise = TestUtils.topicObserved("last-pb-context-exited");
+  yield BrowserTestUtils.closeWindow(win);
+  yield obsPromise;
+  Assert.ok(true, "Got the last-pb-context-exited notification");
+  chromeWin.close();
+});
 
-  let observer = function() {
-    is(true, true, "observer fired");
-    Services.obs.removeObserver(observer, "last-pb-context-exited");
-    executeSoon(finish);
-  };
-  Services.obs.addObserver(observer, "last-pb-context-exited", false);
-  windowsToClose.push(win);
-}
