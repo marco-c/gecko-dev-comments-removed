315d301a926cbbc4e579d9e120d74d753c6deda0
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 16:25:57 +0000

    Bug 881832 - When flushing changes made by :hover style, make sure we also flush any pending changes on child documents. r=dbaron
    UltraBlame original commit: 11106afdcbe72b149ab97f4cb102fc7bd44bce72

--- layout/base/nsPresShell.cpp
+++ layout/base/nsPresShell.cpp
@@ -3630,6 +3630,16 @@ PresShell::ScheduleViewManagerFlush(PaintType aType)
   }
 }
 
+bool
+FlushLayoutRecursive(nsIDocument* aDocument,
+                     void* aData = nullptr)
+{
+  MOZ_ASSERT(!aData);
+  aDocument->EnumerateSubDocuments(FlushLayoutRecursive, nullptr);
+  aDocument->FlushPendingNotifications(Flush_Layout);
+  return true;
+}
+
 void
 PresShell::DispatchSynthMouseMove(WidgetGUIEvent* aEvent,
                                   bool aFlushOnHoverChange)
@@ -3654,7 +3664,10 @@ PresShell::DispatchSynthMouseMove(WidgetGUIEvent* aEvent,
       hoverGenerationBefore != restyleManager->AsGecko()->GetHoverGeneration()) {
     
     
-    FlushPendingNotifications(Flush_Layout);
+    
+    
+    
+    FlushLayoutRecursive(mDocument);
   }
 }
 
