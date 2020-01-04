62ad967d77a5254b0bcfb3516030768f8c98518b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 05:42:46 +0000

    Bug 1226347 - Part 2: Allow control of AEC via prefs. r=rjesup
    UltraBlame original commit: da34ad5d6957c2f267f12623cf9e2cac778a1e69

--- media/webrtc/trunk/webrtc/modules/audio_processing/echo_cancellation_impl.cc
+++ media/webrtc/trunk/webrtc/modules/audio_processing/echo_cancellation_impl.cc
@@ -336,8 +336,8 @@ int EchoCancellationImpl::Initialize() {
 }
 
 void EchoCancellationImpl::SetExtraOptions(const Config& config) {
-  extended_filter_enabled_ = true; 
-  delay_agnostic_enabled_ = true; 
+  extended_filter_enabled_ = config.Get<ExtendedFilter>().enabled;
+  delay_agnostic_enabled_ = config.Get<DelayAgnostic>().enabled;
   Configure();
 }
 
