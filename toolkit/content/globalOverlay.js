d88d06ad1f748c5ddf98ac2f8cf36653d8363c19
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 06:29:46 +0000

    Bug 1230839 - Fix mixing reference to AppConstants in globalOverlay.js r=MattN
    UltraBlame original commit: 5211eaa88dca7f1105865edcc16aa29ef6a6f4b6

--- toolkit/content/globalOverlay.js
+++ toolkit/content/globalOverlay.js
@@ -4,6 +4,8 @@
 
 function closeWindow(aClose, aPromptFunction)
 {
+  let { AppConstants } = Components.utils.import("resource://gre/modules/AppConstants.jsm");
+
   
   if (AppConstants.platform != "macosx") {
     var windowCount = 0;
