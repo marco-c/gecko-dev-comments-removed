d65aa0bbe92caef72291da6805bdaad75f0505b8
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 00:12:02 +0000

    Backed out 6 changesets (bug 1145744, bug 536509, bug 1184973, bug 1147821) for B2G test_storagePermissionsAccept.html failures.
    Backed out changeset 07f5717ac742 (bug 1184973)
    Backed out changeset d49d61e97fcb (bug 1184973)
    Backed out changeset e056e2d6b7e8 (bug 1145744)
    Backed out changeset f4806930c168 (bug 1147821)
    Backed out changeset 24c808f933e5 (bug 536509)
    Backed out changeset 03dc725619b8 (bug 1184973)
    
    CLOSED TREE
    
    UltraBlame original commit: 5482a856cc1d2fb5d2fac39bfbb975ff89f62dfc

--- dom/storage/DOMStorage.h
+++ dom/storage/DOMStorage.h
@@ -18,7 +18,6 @@
 
 class nsIPrincipal;
 class nsIDOMWindow;
-class nsPIDOMWindow;
 
 namespace mozilla {
 namespace dom {
@@ -123,7 +122,7 @@ public:
   
   
   
-  static bool CanUseStorage(nsPIDOMWindow* aWindow, DOMStorage* aStorage = nullptr);
+  static bool CanUseStorage(DOMStorage* aStorage = nullptr);
 
   bool IsPrivate() const { return mIsPrivate; }
   bool IsSessionOnly() const { return mIsSessionOnly; }
