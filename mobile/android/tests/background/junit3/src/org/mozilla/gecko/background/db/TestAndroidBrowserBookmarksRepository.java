b855dc0bd8eba847fda7cafe48f404a130de7b9b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 11:19:59 +0000

    Bug 1255234 - Fix lint error Assert and add to error list. r=sebastian
    MozReview-Commit-ID: 32Uzjjsb5MX
    
    UltraBlame original commit: 691ba3c50f118e9bbf8b0ec175b2e36912a95187

--- mobile/android/tests/background/junit3/src/org/mozilla/gecko/background/db/TestAndroidBrowserBookmarksRepository.java
+++ mobile/android/tests/background/junit3/src/org/mozilla/gecko/background/db/TestAndroidBrowserBookmarksRepository.java
@@ -332,7 +332,7 @@ public class TestAndroidBrowserBookmarksRepository extends AndroidBrowserReposit
     BookmarkRecord bmk3 = (BookmarkRecord) record3;
     record3.guid = Utils.generateGuid();
     record3.lastModified = timestampDelegate.records.get(0).lastModified + 3000;
-    assert(!record0.guid.equals(record3.guid));
+    assertFalse(record0.guid.equals(record3.guid));
 
     
     
