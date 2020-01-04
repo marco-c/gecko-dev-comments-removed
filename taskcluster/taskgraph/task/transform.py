d44d0efe14f5f00eb62c1f3e5b313d2d44ef263f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 23:37:51 +0000

    Bug 1286075: delete the legacy kind; r=mshal
    MozReview-Commit-ID: F99tUGUWj6d
    
    UltraBlame original commit: e96a7223e0d0032ac9d17c477393d5f596349317

--- taskcluster/taskgraph/task/transform.py
+++ taskcluster/taskgraph/task/transform.py
@@ -92,3 +92,9 @@ class TransformTask(base.Task):
                              + self.label)
                 return True, None
         return False, None
+
+    @classmethod
+    def from_json(cls, task_dict):
+        
+        task_dict['when'] = {}
+        return cls(task_dict['attributes']['kind'], task_dict)
