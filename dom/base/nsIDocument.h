83cbb5e4d536361f4e4d07fb41c876b6546f1617
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 10:04:00 +0000

    Bug 1220160 - part 1: add chromeonly getter for documenturi that returns the original document when on an error page, r=bz
    MozReview-Commit-ID: KaOc0WBxcIS
    
    UltraBlame original commit: 164d5bc71555fe593f7726ff208dccb730073f34

--- dom/base/nsIDocument.h
+++ dom/base/nsIDocument.h
@@ -2601,6 +2601,8 @@ public:
 
   void ObsoleteSheet(const nsAString& aSheetURI, mozilla::ErrorResult& rv);
 
+  already_AddRefed<nsIURI> GetMozDocumentURIIfNotForErrorPages();
+
   
   nsIHTMLCollection* Children();
   uint32_t ChildElementCount();
