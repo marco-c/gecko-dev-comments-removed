3c81b341af7fc98dcb910df9829a25724da46177
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 22:33:42 +0000

    Backed out changeset ec1ebef4eb86 (bug 1296420) for failures in test_aria_owns.html
    UltraBlame original commit: b7e8b15d90da87ca0491b9515ca8640f97ef132e

--- accessible/generic/DocAccessible.cpp
+++ accessible/generic/DocAccessible.cpp
@@ -1378,23 +1378,7 @@ DocAccessible::ProcessInvalidationList()
     if (!HasAccessible(content)) {
       Accessible* container = GetContainerAccessible(content);
       if (container) {
-        
-        
-        AttrRelProviderArray* list =
-          mDependentIDsHash.Get(nsDependentAtomString(content->GetID()));
-        bool shouldProcess = !!list;
-        if (shouldProcess) {
-          for (uint32_t idx = 0; idx < list->Length(); idx++) {
-            if (list->ElementAt(idx)->mRelAttr == nsGkAtoms::aria_owns) {
-              shouldProcess = false;
-              break;
-            }
-          }
-
-          if (shouldProcess) {
-            ProcessContentInserted(container, content);
-          }
-        }
+        ProcessContentInserted(container, content);
       }
     }
   }
