5fd1aa64a65dbe50a00b1a2e19e718394c780cfe
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 00:47:17 +0000

    Backed out changeset fd3a53075e54 (bug 1174574)
    UltraBlame original commit: 2b4eff0977f2ad72b25c36f159a2d5e52358efea

--- layout/generic/nsGridContainerFrame.h
+++ layout/generic/nsGridContainerFrame.h
@@ -33,9 +33,6 @@ public:
               nsHTMLReflowMetrics&     aDesiredSize,
               const nsHTMLReflowState& aReflowState,
               nsReflowStatus&          aStatus) override;
-  nscoord GetMinISize(nsRenderingContext* aRenderingContext) override;
-  nscoord GetPrefISize(nsRenderingContext* aRenderingContext) override;
-  void MarkIntrinsicISizesDirty() override;
   virtual nsIAtom* GetType() const override;
 
   void BuildDisplayList(nsDisplayListBuilder*   aBuilder,
@@ -110,11 +107,7 @@ protected:
   struct GridReflowState;
   friend nsContainerFrame* NS_NewGridContainerFrame(nsIPresShell* aPresShell,
                                                     nsStyleContext* aContext);
-  explicit nsGridContainerFrame(nsStyleContext* aContext)
-    : nsContainerFrame(aContext)
-    , mCachedMinISize(NS_INTRINSIC_WIDTH_UNKNOWN)
-    , mCachedPrefISize(NS_INTRINSIC_WIDTH_UNKNOWN)
-  {}
+  explicit nsGridContainerFrame(nsStyleContext* aContext) : nsContainerFrame(aContext) {}
 
   
 
@@ -189,11 +182,6 @@ protected:
 
     void ToPositionAndLength(const nsTArray<TrackSize>& aTrackSizes,
                              nscoord* aPos, nscoord* aLength) const;
-    
-
-
-
-    nscoord ToLength(const nsTArray<TrackSize>& aTrackSizes) const;
     
 
 
@@ -535,12 +523,6 @@ protected:
                       nsHTMLReflowMetrics& aDesiredSize,
                       nsReflowStatus&      aStatus);
 
-  
-
-
-  nscoord IntrinsicISize(nsRenderingContext* aRenderingContext,
-                         IntrinsicISizeType  aConstraint);
-
 #ifdef DEBUG
   void SanityCheckAnonymousGridItems() const;
 #endif 
@@ -585,12 +567,6 @@ private:
 
   uint32_t mExplicitGridOffsetCol;
   uint32_t mExplicitGridOffsetRow;
-
-  
-
-
-  nscoord mCachedMinISize;
-  nscoord mCachedPrefISize;
 
   
 
