92d9c65dc3b10730b89b2f56a24f3277d28ca8e7
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 15:23:37 +0000

    Remove NS_NATIVE_GRAPHIC on Windows. (bug 1266536, r=jimm)
    UltraBlame original commit: fa44404d8be401240ff14ea908037b484ccf0c7d

--- widget/windows/nsWindow.h
+++ widget/windows/nsWindow.h
@@ -473,6 +473,9 @@ private:
 protected:
 #endif 
 
+  HDC GetWindowSurface();
+  void FreeWindowSurface(HDC dc);
+
   static bool             IsAsyncResponseEvent(UINT aMsg, LRESULT& aResult);
   void                    IPCWindowProcHandler(UINT& msg, WPARAM& wParam, LPARAM& lParam);
 
