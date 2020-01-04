6b6c0a8a0db7f2d6c345deca67c1abde2e8a2268
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 14:52:09 +0000

    Bug 1224199 - Destroy SharedSurfaces before ~GLContext(). - r=jrmuizel
    UltraBlame original commit: 22f36f2a2e4c740a0c750aad18c444de94ac2658

--- gfx/gl/SharedSurfaceEGL.cpp
+++ gfx/gl/SharedSurfaceEGL.cpp
@@ -88,9 +88,12 @@ SharedSurface_EGLImage::~SharedSurface_EGLImage()
 {
     mEGL->fDestroyImage(Display(), mImage);
 
-    mGL->MakeCurrent();
-    mGL->fDeleteTextures(1, &mProdTex);
-    mProdTex = 0;
+    if (mSync) {
+        
+        
+        mEGL->fDestroySync(Display(), mSync);
+        mSync = 0;
+    }
 
     if (mConsTex) {
         MOZ_ASSERT(mGarbageBin);
@@ -98,12 +101,11 @@ SharedSurface_EGLImage::~SharedSurface_EGLImage()
         mConsTex = 0;
     }
 
-    if (mSync) {
-        
-        
-        mEGL->fDestroySync(Display(), mSync);
-        mSync = 0;
-    }
+    if (!mGL->MakeCurrent())
+        return;
+
+    mGL->fDeleteTextures(1, &mProdTex);
+    mProdTex = 0;
 }
 
 layers::TextureFlags
