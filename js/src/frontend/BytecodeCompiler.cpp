aa4166974148b20b08a2a88b0d9965dd039b69a5
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 19:13:48 +0000

    Bug 1283448 - Freeze module objects before they are passed back to the caller r=shu
    UltraBlame original commit: ff3816e93befbfd10a1965bbb18c0471bdfbb694

--- js/src/frontend/BytecodeCompiler.cpp
+++ js/src/frontend/BytecodeCompiler.cpp
@@ -801,7 +801,7 @@ frontend::CompileModule(JSContext* cx, const ReadOnlyCompileOptions& options,
 
     
     
-    if (!ModuleObject::FreezeArrayProperties(cx->asJSContext(), module))
+    if (!ModuleObject::Freeze(cx->asJSContext(), module))
         return nullptr;
 
     return module;
