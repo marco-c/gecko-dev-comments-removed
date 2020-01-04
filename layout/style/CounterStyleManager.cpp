bc9304b6c9271a01ad5d4805bfeb27de8b460a72
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 06:04:03 +0000

    Backed out changeset 427dc5894094 (bug 1197307) for bustage
    UltraBlame original commit: 301532805d409381b55b62309358b95e83f1c952

--- layout/style/CounterStyleManager.cpp
+++ layout/style/CounterStyleManager.cpp
@@ -18,6 +18,7 @@
 #include "nsTArray.h"
 #include "nsTHashtable.h"
 #include "nsUnicodeProperties.h"
+#include "prprf.h"
 
 namespace mozilla {
 
@@ -211,7 +212,8 @@ DecimalToText(CounterValue aOrdinal, nsSubstring& aResult)
 {
   
   char cbuf[std::numeric_limits<CounterValue>::digits10 + 3];
-  aResult.AppendInt(aOrdinal);
+  PR_snprintf(cbuf, sizeof(cbuf), "%ld", aOrdinal);
+  aResult.AssignASCII(cbuf);
   return true;
 }
 
