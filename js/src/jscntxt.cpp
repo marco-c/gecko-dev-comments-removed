1c9cf9cf55dba1e729bbe3462dc6026acff594b7
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 10:04:36 +0000

    Bug 1249183 - Suppress GC harder, r=terrence
    UltraBlame original commit: 1d5dd700e41e1f4e967a951e6c8d38d0d68f45b0

--- js/src/jscntxt.cpp
+++ js/src/jscntxt.cpp
@@ -295,12 +295,11 @@ js::ReportOutOfMemory(ExclusiveContext* cxArg)
 
     JSContext* cx = cxArg->asJSContext();
     cx->runtime()->hadOutOfMemory = true;
+    AutoSuppressGC suppressGC(cx);
 
     
-    if (JS::OutOfMemoryCallback oomCallback = cx->runtime()->oomCallback) {
-        AutoSuppressGC suppressGC(cx);
+    if (JS::OutOfMemoryCallback oomCallback = cx->runtime()->oomCallback)
         oomCallback(cx, cx->runtime()->oomCallbackData);
-    }
 
     if (cx->options().autoJSAPIOwnsErrorReporting() || JS_IsRunning(cx)) {
         cx->setPendingException(StringValue(cx->names().outOfMemory));
@@ -318,10 +317,8 @@ js::ReportOutOfMemory(ExclusiveContext* cxArg)
     PopulateReportBlame(cx, &report);
 
     
-    if (JSErrorReporter onError = cx->runtime()->errorReporter) {
-        AutoSuppressGC suppressGC(cx);
+    if (JSErrorReporter onError = cx->runtime()->errorReporter)
         onError(cx, msg, &report);
-    }
 
     
 
