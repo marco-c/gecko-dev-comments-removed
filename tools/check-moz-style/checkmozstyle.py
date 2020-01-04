811dffc755439920bfe7dd80e41ecee51d3ce2ef
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 02:30:51 +0000

    Bug 875605 - Add tests to check-moz-style. r=ms2ger
    UltraBlame original commit: 2c0d1469d2bffa2cd75d6a4696957335d7e6b648

--- tools/check-moz-style/checkmozstyle.py
+++ tools/check-moz-style/checkmozstyle.py
@@ -1,37 +1,38 @@
-#!/usr/bin/python
-#
-# Copyright (C) 2009 Google Inc. All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are
-# met:
-#
-#    * Redistributions of source code must retain the above copyright
-# notice, this list of conditions and the following disclaimer.
-#    * Redistributions in binary form must reproduce the above
-# copyright notice, this list of conditions and the following disclaimer
-# in the documentation and/or other materials provided with the
-# distribution.
-#    * Neither the name of Google Inc. nor the names of its
-# contributors may be used to endorse or promote products derived from
-# this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 """Script to run the linter for source code of WebKit."""
 
 import os
 import os.path
+import re
 import sys
 
 import modules.cpplint as cpplint
@@ -39,7 +40,7 @@ from modules.diff_parser import DiffParser
 from modules.scm import detect_scm_system
 
 
-# Override the usage of the lint tool.
+
 cpplint._USAGE = """
 Syntax: %(program_name)s [--verbose=#] [--git-commit=<COMMITISH>] [--output=vs7] [--filter=-x,+y,...]
 
@@ -87,8 +88,7 @@ Syntax: %(program_name)s [--verbose=#] [--git-commit=<COMMITISH>] [--output=vs7]
          --filter=
 """ % {'program_name': sys.argv[0]}
 
-
-def process_patch(patch_string, cwd, scm):
+def process_patch(patch_string, root, cwd, scm):
     """Does lint on a single patch.
 
     Args:
@@ -96,13 +96,35 @@ def process_patch(patch_string, cwd, scm):
     """
     patch = DiffParser(patch_string.splitlines())
 
+    if not len(patch.files):
+        cpplint.error("patch", 0, "patch/notempty", 3,
+                      "Patch does not appear to diff against any file.")
+        return
+
+    if not patch.status_line:
+        cpplint.error("patch", 0, "patch/nosummary", 3,
+                      "Patch does not have a summary.")
+    else:
+        proper_format = re.match(r"^Bug [0-9]+ - ", patch.status_line)
+        if not proper_format:
+            proper_format = re.match(r"^No bug - ", patch.status_line)
+            cpplint.error("patch", 0, "patch/bugnumber", 3,
+                          "Patch summary should begin with 'Bug XXXXX - ' " +
+                          "or 'No bug -'.")
+
+    if not patch.patch_description:
+        cpplint.error("patch", 0, "patch/nodescription", 3,
+                      "Patch does not have a description.")
+
     for filename, diff in patch.files.iteritems():
         file_extension = os.path.splitext(filename)[1]
 
         if file_extension in ['.cpp', '.c', '.h']:
             line_numbers = set()
+            orig_filename = filename
 
-            def error_for_patch(filename, line_number, category, confidence, message):
+            def error_for_patch(filename, line_number, category, confidence,
+                                message):
                 """Wrapper function of cpplint.error for patches.
 
                 This function outputs errors only if the line number
@@ -110,15 +132,18 @@ def process_patch(patch_string, cwd, scm):
                 """
                 if not line_numbers:
                     for line in diff.lines:
-                        # When deleted line is not set, it means that
-                        # the line is newly added.
+                        
+                        
                         if not line[0]:
                             line_numbers.add(line[1])
 
                 if line_number in line_numbers:
-                    cpplint.error(filename, line_number, category, confidence, message)
+                    cpplint.error(orig_filename, line_number,
+                                  category, confidence, message)
 
-            cpplint.process_file(os.path.join(scm.find_checkout_root(cwd), filename), error=error_for_patch)
+            cpplint.process_file(os.path.join(root, filename),
+                                 relative_name=orig_filename,
+                                 error=error_for_patch)
 
 
 def main():
@@ -126,16 +151,18 @@ def main():
 
     (args, flags) = cpplint.parse_arguments(sys.argv[1:], ["git-commit="])
     if args:
-        sys.stderr.write("ERROR: We don't support files as arguments for now.\n" + cpplint._USAGE)
+        sys.stderr.write("ERROR: We don't support files as arguments for " +
+                         "now.\n" + cpplint._USAGE)
         sys.exit(1)
 
     cwd = os.path.abspath('.')
     scm = detect_scm_system(cwd)
+    root = scm.find_checkout_root(cwd)
 
     if "--git-commit" in flags:
-        process_patch(scm.create_patch_from_local_commit(flags["--git-commit"]), cwd, scm)
+        process_patch(scm.create_patch_from_local_commit(flags["--git-commit"]), root, cwd, scm)
     else:
-        process_patch(scm.create_patch(), cwd, scm)
+        process_patch(scm.create_patch(), root, cwd, scm)
 
     sys.stderr.write('Total errors found: %d\n' % cpplint.error_count())
     sys.exit(cpplint.error_count() > 0)
