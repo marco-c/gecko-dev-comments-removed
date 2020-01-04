acbdbbf1a8d0a8202463ff01860e32849d1bf197
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 00:41:28 +0000

    bug 1196853 - convert test_cert_signatures.js to generate certificates at build time r=jcj
    Also add additional testcases that weren't in the original test (tampered
    signatures had been tested, but tampered certificates hadn't been covered).
    
    UltraBlame original commit: b82c3dd381db73184b1b6427429dcbbce65e3ab3

--- security/manager/ssl/tests/unit/test_cert_signatures.js
+++ security/manager/ssl/tests/unit/test_cert_signatures.js
@@ -11,84 +11,111 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
 do_get_profile(); 
-const certdb = Cc["@mozilla.org/security/x509certdb;1"].getService(Ci.nsIX509CertDB);
+let certdb = Cc["@mozilla.org/security/x509certdb;1"]
+               .getService(Ci.nsIX509CertDB);
 
-function load_ca(ca_name) {
-  let ca_filename = ca_name + ".der";
-  addCertFromFile(certdb, "test_cert_signatures/" + ca_filename, 'CTu,CTu,CTu');
+
+
+
+function readAndTamperWithNthByte(certificatePath, n) {
+  let pem = readFile(do_get_file(certificatePath, false));
+  let der = atob(pemToBase64(pem));
+  if (n < 0) {
+    
+    n = der.length + n;
+  }
+  let replacement = '\x22';
+  if (der.charCodeAt(n) == replacement) {
+    replacement = '\x23';
+  }
+  der = der.substring(0, n) + replacement + der.substring(n + 1);
+  return btoa(der);
 }
 
-function check_ca(ca_name) {
-  do_print("ca_name=" + ca_name);
-  let cert = certdb.findCertByNickname(null, ca_name);
 
-  let verified = {};
-  let usages = {};
-  cert.getUsagesString(true, verified, usages);
-  equal("SSL CA", usages.value, "Usages string for a CA cert should be 'SSL CA'");
+
+
+const BYTE_IN_SIGNATURE = -8;
+function addSignatureTamperedCertificate(certificatePath) {
+  let base64 = readAndTamperWithNthByte(certificatePath, BYTE_IN_SIGNATURE);
+  certdb.addCertFromBase64(base64, ",,", null);
+}
+
+function ensureSignatureVerificationFailure(certificatePath) {
+  let cert = constructCertFromFile(certificatePath);
+  checkCertErrorGeneric(certdb, cert, SEC_ERROR_BAD_SIGNATURE,
+                        certificateUsageSSLServer);
+}
+
+function tamperWithSignatureAndEnsureVerificationFailure(certificatePath) {
+  let base64 = readAndTamperWithNthByte(certificatePath, BYTE_IN_SIGNATURE);
+  let cert = certdb.constructX509FromBase64(base64);
+  checkCertErrorGeneric(certdb, cert, SEC_ERROR_BAD_SIGNATURE,
+                        certificateUsageSSLServer);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const BYTE_IN_SERIAL_NUMBER = 17;
+function addSerialNumberTamperedCertificate(certificatePath) {
+  let base64 = readAndTamperWithNthByte(certificatePath,
+                                        BYTE_IN_SERIAL_NUMBER);
+  certdb.addCertFromBase64(base64, ",,", null);
+}
+
+function tamperWithSerialNumberAndEnsureVerificationFailure(certificatePath) {
+  let base64 = readAndTamperWithNthByte(certificatePath,
+                                        BYTE_IN_SERIAL_NUMBER);
+  let cert = certdb.constructX509FromBase64(base64);
+  checkCertErrorGeneric(certdb, cert, SEC_ERROR_BAD_SIGNATURE,
+                        certificateUsageSSLServer);
 }
 
 function run_test() {
-  
-  load_ca("ca-rsa");
-  load_ca("ca-p384");
-
-  clearOCSPCache();
-  clearSessionCache();
-
-  check_ca("ca-rsa");
-  check_ca("ca-p384");
+  addCertFromFile(certdb, "test_cert_signatures/ca-rsa.pem", "CTu,,");
+  addCertFromFile(certdb, "test_cert_signatures/ca-secp384r1.pem", "CTu,,");
 
   
   
-  const int_usage = 'SSL CA';
+  
+  addSignatureTamperedCertificate("test_cert_signatures/int-rsa.pem");
+  addSignatureTamperedCertificate("test_cert_signatures/int-secp384r1.pem");
+  ensureSignatureVerificationFailure("test_cert_signatures/ee-rsa.pem");
+  ensureSignatureVerificationFailure("test_cert_signatures/ee-secp384r1.pem");
 
   
-  const ee_usage = 'Client,Server,Sign,Encrypt,Object Signer';
-
-  let cert2usage = {
-    
-    'int-rsa-valid': int_usage,
-    'rsa-valid': ee_usage,
-    'int-p384-valid': int_usage,
-    'p384-valid': ee_usage,
-
-    'rsa-valid-int-tampered-ee': "",
-    'p384-valid-int-tampered-ee': "",
-
-    'int-rsa-tampered': "",
-    'rsa-tampered-int-valid-ee': "",
-    'int-p384-tampered': "",
-    'p384-tampered-int-valid-ee': "",
-
-  };
+  
+  tamperWithSignatureAndEnsureVerificationFailure(
+    "test_cert_signatures/ee-rsa-direct.pem");
+  tamperWithSignatureAndEnsureVerificationFailure(
+    "test_cert_signatures/ee-secp384r1-direct.pem");
 
   
-  for (let cert_name in cert2usage) {
-    let cert_filename = cert_name + ".der";
-    addCertFromFile(certdb, "test_cert_signatures/" + cert_filename, ',,');
-  }
+  
+  
+  addSerialNumberTamperedCertificate("test_cert_signatures/int-rsa.pem");
+  addSerialNumberTamperedCertificate("test_cert_signatures/int-secp384r1.pem");
+  ensureSignatureVerificationFailure("test_cert_signatures/ee-rsa.pem");
+  ensureSignatureVerificationFailure("test_cert_signatures/ee-secp384r1.pem");
 
-  for (let cert_name in cert2usage) {
-    do_print("cert_name=" + cert_name);
-
-    let cert = certdb.findCertByNickname(null, cert_name);
-
-    let verified = {};
-    let usages = {};
-    cert.getUsagesString(true, verified, usages);
-    equal(cert2usage[cert_name], usages.value,
-          "Expected and actual usages string should match");
-  }
+  
+  
+  tamperWithSerialNumberAndEnsureVerificationFailure(
+    "test_cert_signatures/ee-rsa-direct.pem");
+  tamperWithSerialNumberAndEnsureVerificationFailure(
+    "test_cert_signatures/ee-secp384r1-direct.pem");
 }
