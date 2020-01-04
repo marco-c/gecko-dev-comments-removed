3cdeb1da67136fd4e8a86d10db6c1cfcf607ec34
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 03:57:52 +0000

    Bug 1217251 - In TaskThrottler, dispatch the timeout task correctly from the Java UI thread on Android. r=kats
    UltraBlame original commit: c6404f3d787c497242d7a5cc2d1ea9796205e991

--- gfx/layers/apz/src/TaskThrottler.cpp
+++ gfx/layers/apz/src/TaskThrottler.cpp
@@ -6,7 +6,7 @@
 
 #include "TaskThrottler.h"
 
-#include "base/message_loop.h"
+#include "mozilla/layers/APZThreadUtils.h"
 
 #define TASK_LOG(...)
 
@@ -49,8 +49,7 @@ TaskThrottler::PostTask(const tracked_objects::Location& aLocation,
       
       TimeDuration timeout = mMaxWait - TimeSinceLastRequest(aTimeStamp, lock);
       mTimeoutTask = NewRunnableMethod(this, &TaskThrottler::OnTimeout);
-      MessageLoop::current()->PostDelayedTask(FROM_HERE, mTimeoutTask,
-          timeout.ToMilliseconds());
+      APZThreadUtils::RunDelayedTaskOnCurrentThread(mTimeoutTask, timeout);
       return;
     }
     
