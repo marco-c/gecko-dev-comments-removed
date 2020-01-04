efc00dcc0166647f9bc8dfb61eaa9cab02b3369a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 02:15:43 +0000

    Bug 1206520: Add about:config prefs to enable throwing on asm.js validation failures; r=bz
    UltraBlame original commit: 9bae4db3f286eee88032b81b2c6835f92c3335b2

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -1118,6 +1118,7 @@ pref("javascript.options.asyncstack",       true);
 #else
 pref("javascript.options.asyncstack",       false);
 #endif
+pref("javascript.options.throw_on_asmjs_validation_failure", false);
 pref("javascript.options.ion.offthread_compilation", true);
 
 
