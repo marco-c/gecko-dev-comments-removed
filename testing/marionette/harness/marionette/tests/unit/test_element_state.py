f8fa2733e768334150ee165ba4ac735c62211675
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 17:08:38 +0000

    Bug 1272653 - Merge element state tests; r=jgriffin
    Renames test_elementState.py to test_element_state.py to patch
    Python naming conventions.  Merges test_getattr*.py tests into
    test_element_state.py to match WebDriver specification chapter structure.
    
    MozReview-Commit-ID: GkHgaUCdktp
    
    UltraBlame original commit: 685b7400fe1b4a86d22640959cda05a364ed5428

--- testing/marionette/harness/marionette/tests/unit/test_element_state.py
+++ testing/marionette/harness/marionette/tests/unit/test_element_state.py
@@ -22,3 +22,17 @@ class TestState(MarionetteTestCase):
         self.assertTrue(l.is_displayed())
         self.marionette.execute_script("arguments[0].hidden = true;", [l])
         self.assertFalse(l.is_displayed())
+
+
+class TestGetAttribute(MarionetteTestCase):
+    def test_getAttribute(self):
+        test_html = self.marionette.absolute_url("test.html")
+        self.marionette.navigate(test_html)
+        l = self.marionette.find_element(By.ID, "mozLink")
+        self.assertEqual("mozLink", l.get_attribute("id"))
+
+    def test_that_we_can_return_a_boolean_attribute_correctly(self):
+        test_html = self.marionette.absolute_url("html5/boolean_attributes.html")
+        self.marionette.navigate(test_html)
+        disabled = self.marionette.find_element(By.ID, "disabled")
+        self.assertEqual('true', disabled.get_attribute("disabled"))
