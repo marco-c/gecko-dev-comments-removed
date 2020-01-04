63778662996323575287b63a937305caf7dca253
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 04:25:42 +0000

    Back out changeset 81b802b69a74 (bug 1213154) for Linux/Mulet webgl reftest failures CLOSED TREE
    UltraBlame original commit: 8c423cd24085697f93001db72fe29a07a679c494

--- dom/html/HTMLMediaElement.cpp
+++ dom/html/HTMLMediaElement.cpp
@@ -4749,12 +4749,6 @@ HTMLMediaElement::IsPlayingThroughTheAudioChannel() const
   }
 
   
-  
-  if (mPausedForInactiveDocumentOrChannel) {
-    return false;
-  }
-
-  
   if (HasAttr(kNameSpaceID_None, nsGkAtoms::loop)) {
     return true;
   }
