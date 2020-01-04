6577511f62f4ac47cd31e04703d1d7243e6e6403
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 18:13:46 +0000

    Bug 1208328 - Add test for "addtrack" on recv side of RTCPeerConnection. r=jib
    MozReview-Commit-ID: 2KkAc5uFjAZ
    
    UltraBlame original commit: 6ce08f664f9cf5337b993cf282610681c945e97e

--- dom/media/tests/mochitest/head.js
+++ dom/media/tests/mochitest/head.js
@@ -441,8 +441,8 @@ function checkMediaStreamTrackCloneAgainstOriginal(clone, original) {
 
 
 
-function wait(time) {
-  return new Promise(r => setTimeout(r, time));
+function wait(time, message) {
+  return new Promise(r => setTimeout(() => r(message), time));
 }
 
 
@@ -482,8 +482,8 @@ var addFinallyToPromise = promise => {
 
 var listenUntil = (target, eventName, onFire) => {
   return new Promise(resolve => target.addEventListener(eventName,
-                                                        function callback() {
-    var result = onFire();
+                                                        function callback(event) {
+    var result = onFire(event);
     if (result) {
       target.removeEventListener(eventName, callback, false);
       resolve(result);
@@ -609,8 +609,7 @@ function haveEvent(target, name, cancelPromise) {
     (cancelPromise || new Promise()).then(e => Promise.reject(e)),
     new Promise(resolve => target.addEventListener(name, listener = resolve))
   ]);
-  p.then(() => target.removeEventListener(name, listener));
-  return p;
+  return p.then(event => (target.removeEventListener(name, listener), event));
 };
 
 
