235b680b1eb2ea65a5f2ea5430df1d058b55691c
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 03:37:17 +0000

    Backed out changeset 11e681d48acd (bug 1194419) for S4 Test failures
    UltraBlame original commit: 80f151ddd504ea383874c03415e44805e488d983

--- security/manager/ssl/tests/unit/pycert.py
+++ security/manager/ssl/tests/unit/pycert.py
@@ -329,6 +329,10 @@ class Certificate(object):
         hasher.update(str(self.notBefore))
         hasher.update(str(self.notAfter))
         hasher.update(self.subject)
+        
+        
+        
+        hasher.update(self.signature)
         if self.extensions:
             for extension in self.extensions:
                 hasher.update(str(extension))
