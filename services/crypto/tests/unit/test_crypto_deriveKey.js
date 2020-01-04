a194503729f2eb8f644179cdc34d30f04e326db8
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 17:06:25 +0000

    Bug 1234305 - Replace WeaveCrypto NSS implementation with Web Crypto. r=keeler
    MozReview-Commit-ID: BBOY9zSLzea
    
    UltraBlame original commit: 0e747813be13880fda46dca14981b47cae8688b3

--- services/crypto/tests/unit/test_crypto_deriveKey.js
+++ services/crypto/tests/unit/test_crypto_deriveKey.js
@@ -1,38 +1,28 @@
-var btoa;
+Components.utils.import("resource://services-crypto/WeaveCrypto.js");
 
-function test_derive(cryptoSvc) {
+function run_test() {
+  let cryptoSvc = new WeaveCrypto();
   
   let pp = "secret phrase";
   let salt = "RE5YUHpQcGl3bg==";   
-  
+
   
   let k = cryptoSvc.deriveKeyFromPassphrase(pp, salt, 16);
   do_check_eq(16, k.length);
   do_check_eq(btoa(k), "d2zG0d2cBfXnRwMUGyMwyg==");
-  
+
   
   k = cryptoSvc.deriveKeyFromPassphrase(pp, salt, 32);
   do_check_eq(32, k.length);
+  do_check_eq(btoa(k), "d2zG0d2cBfXnRwMUGyMwyroRXtnrSIeLwSDvReSfcyA=");
   let encKey = btoa(k);
-  
+
   
   let iv = cryptoSvc.generateRandomIV();
   do_check_eq(cryptoSvc.decrypt(cryptoSvc.encrypt("bacon", encKey, iv), encKey, iv), "bacon");
+
   
-  
-  k = cryptoSvc.deriveKeyFromPassphrase(pp, salt, null);
+  k = cryptoSvc.deriveKeyFromPassphrase(pp, salt);
   do_check_eq(32, k.length);
   do_check_eq(encKey, btoa(k));
-}
-
-function run_test() {
-  let cryptoSvc;
-  try {
-    let backstagePass = Components.utils.import("resource://services-crypto/WeaveCrypto.js");
-    btoa = backstagePass.btoa;
-  } catch (ex) {
-    _("Aborting test: no WeaveCrypto.js.");
-    return;
-  }
-  test_derive(new WeaveCrypto());
 }
