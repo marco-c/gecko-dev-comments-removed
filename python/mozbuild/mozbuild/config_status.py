aa7b068295e9b03a623420214edb52174d04d8a4
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 08:29:36 +0000

    Bug 1241398 - Add a dry-run mode to `mach build-backend`. r=gps
    When doing build system changes affecting backend-generated files, I
    often use `mach build-backend --diff`. But most of the time I end up
    wanting to look at the full diff again when doing further changes, which
    leads me to stash my changes away, run `mach build-backend` to get the
    initial state again, unstash and rerun `mach build-backend --diff`.
    
    This has been a time drain for long enough :)
    
    UltraBlame original commit: 012e4a8649219b7972a878ff9b9983ef80437370

--- python/mozbuild/mozbuild/config_status.py
+++ python/mozbuild/mozbuild/config_status.py
@@ -116,6 +116,8 @@ def config_status(topobjdir='.', topsrcdir='.',
                         default=default_backends,
                         help='what backend to build (default: %s).' %
                         ' '.join(default_backends))
+    parser.add_argument('--dry-run', action='store_true',
+                        help='do everything except writing files out.')
     options = parser.parse_args()
 
     
@@ -136,6 +138,10 @@ def config_status(topobjdir='.', topsrcdir='.',
     
     
     selected_backends = [get_backend_class(b)(env) for b in options.backend]
+
+    if options.dry_run:
+        for b in selected_backends:
+            b.dry_run = True
 
     reader = BuildReader(env)
     emitter = TreeMetadataEmitter(env)
