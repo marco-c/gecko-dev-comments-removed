f45214ded93ebc789b762f19bf4432b12ffe9f33
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 14:18:04 +0000

    Bug 1241096 - Add a better memory reporting system for AudioBuffers. r=erahm
    MozReview-Commit-ID: GHiauyyD3R2
    
    UltraBlame original commit: 55fa7abf986320f694750d14fe67ed87722eee39

--- dom/media/webaudio/AudioBuffer.cpp
+++ dom/media/webaudio/AudioBuffer.cpp
@@ -12,6 +12,7 @@
 #include "AudioChannelFormat.h"
 #include "mozilla/PodOperations.h"
 #include "mozilla/CheckedInt.h"
+#include "mozilla/MemoryReporting.h"
 #include "AudioNodeEngine.h"
 
 namespace mozilla {
@@ -39,6 +40,128 @@ NS_IMPL_CYCLE_COLLECTION_TRACE_END
 NS_IMPL_CYCLE_COLLECTION_ROOT_NATIVE(AudioBuffer, AddRef)
 NS_IMPL_CYCLE_COLLECTION_UNROOT_NATIVE(AudioBuffer, Release)
 
+
+
+
+
+
+
+class AudioBufferMemoryTracker : public nsIMemoryReporter
+{
+  NS_DECL_THREADSAFE_ISUPPORTS
+  NS_DECL_NSIMEMORYREPORTER
+
+private:
+  AudioBufferMemoryTracker();
+  virtual ~AudioBufferMemoryTracker();
+
+public:
+  
+  static void RegisterAudioBuffer(const AudioBuffer* aAudioBuffer);
+  static void UnregisterAudioBuffer(const AudioBuffer* aAudioBuffer);
+private:
+  static AudioBufferMemoryTracker* GetInstance();
+  
+  void RegisterAudioBufferInternal(const AudioBuffer* aAudioBuffer);
+  
+  uint32_t UnregisterAudioBufferInternal(const AudioBuffer* aAudioBuffer);
+  void Init();
+
+  
+  static StaticMutex sMutex;
+  static StaticRefPtr<AudioBufferMemoryTracker> sSingleton;
+  nsTHashtable<nsPtrHashKey<const AudioBuffer>> mBuffers;
+};
+
+StaticRefPtr<AudioBufferMemoryTracker> AudioBufferMemoryTracker::sSingleton;
+StaticMutex AudioBufferMemoryTracker::sMutex;
+
+NS_IMPL_ISUPPORTS(AudioBufferMemoryTracker, nsIMemoryReporter);
+
+AudioBufferMemoryTracker* AudioBufferMemoryTracker::GetInstance()
+{
+  sMutex.AssertCurrentThreadOwns();
+  if (!sSingleton) {
+    sSingleton = new AudioBufferMemoryTracker();
+    sSingleton->Init();
+  }
+  return sSingleton;
+}
+
+AudioBufferMemoryTracker::AudioBufferMemoryTracker()
+{
+}
+
+void
+AudioBufferMemoryTracker::Init()
+{
+  RegisterWeakMemoryReporter(this);
+}
+
+AudioBufferMemoryTracker::~AudioBufferMemoryTracker()
+{
+  UnregisterWeakMemoryReporter(this);
+}
+
+void
+AudioBufferMemoryTracker::RegisterAudioBuffer(const AudioBuffer* aAudioBuffer)
+{
+  StaticMutexAutoLock lock(sMutex);
+  AudioBufferMemoryTracker* tracker = AudioBufferMemoryTracker::GetInstance();
+  tracker->RegisterAudioBufferInternal(aAudioBuffer);
+}
+
+void
+AudioBufferMemoryTracker::UnregisterAudioBuffer(const AudioBuffer* aAudioBuffer)
+{
+  StaticMutexAutoLock lock(sMutex);
+  AudioBufferMemoryTracker* tracker = AudioBufferMemoryTracker::GetInstance();
+  uint32_t count;
+  count = tracker->UnregisterAudioBufferInternal(aAudioBuffer);
+  if (count == 0) {
+    sSingleton = nullptr;
+  }
+}
+
+void
+AudioBufferMemoryTracker::RegisterAudioBufferInternal(const AudioBuffer* aAudioBuffer)
+{
+  sMutex.AssertCurrentThreadOwns();
+  mBuffers.PutEntry(aAudioBuffer);
+}
+
+uint32_t
+AudioBufferMemoryTracker::UnregisterAudioBufferInternal(const AudioBuffer* aAudioBuffer)
+{
+  sMutex.AssertCurrentThreadOwns();
+  mBuffers.RemoveEntry(aAudioBuffer);
+  return mBuffers.Count();
+}
+
+MOZ_DEFINE_MALLOC_SIZE_OF(AudioBufferMemoryTrackerMallocSizeOf)
+
+NS_IMETHODIMP
+AudioBufferMemoryTracker::CollectReports(nsIHandleReportCallback* handleReport,
+                                         nsISupports* data, bool)
+{
+ size_t amount = 0;
+ nsresult rv;
+
+ for (auto iter = mBuffers.Iter(); !iter.Done(); iter.Next()) {
+   amount += iter.Get()->GetKey()->SizeOfIncludingThis(AudioBufferMemoryTrackerMallocSizeOf);
+ }
+
+ rv = handleReport->Callback(EmptyCString(),
+                             NS_LITERAL_CSTRING("explicit/webaudio/audiobuffer"),
+                             KIND_HEAP, UNITS_BYTES, amount,
+                             NS_LITERAL_CSTRING("Memory used by AudioBuffer"
+                                                " objects (Web Audio)"),
+                             data);
+ NS_ENSURE_SUCCESS(rv, rv);
+
+ return NS_OK;
+}
+
 AudioBuffer::AudioBuffer(AudioContext* aContext, uint32_t aNumberOfChannels,
                          uint32_t aLength, float aSampleRate,
                          already_AddRefed<ThreadSharedFloatArrayBufferList>
@@ -52,10 +175,12 @@ AudioBuffer::AudioBuffer(AudioContext* aContext, uint32_t aNumberOfChannels,
              mSharedChannels->GetChannels() == aNumberOfChannels);
   mJSChannels.SetLength(aNumberOfChannels);
   mozilla::HoldJSObjects(this);
+  AudioBufferMemoryTracker::RegisterAudioBuffer(this);
 }
 
 AudioBuffer::~AudioBuffer()
 {
+  AudioBufferMemoryTracker::UnregisterAudioBuffer(this);
   ClearJSChannels();
 }
 
