7b1521efa6268eee31b6259c2b0e21ed2c2feaf4
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 11:42:09 +0000

    Bug 1256672 - Make OOM alloc counters 64 bit. r=terrence
    UltraBlame original commit: 4b003bd6f7362b9b00e67b1dcd20bbf9477a524f

--- js/src/jit/Label.h
+++ js/src/jit/Label.h
@@ -89,7 +89,7 @@ class Label : public LabelBase
 #ifdef DEBUG
         
         JitContext* context = MaybeGetJitContext();
-        bool hadError = OOM_counter >= OOM_maxAllocations ||
+        bool hadError = js::oom::HadSimulatedOOM() ||
                         (context && context->runtime->hadOutOfMemory());
         MOZ_ASSERT_IF(!hadError, !used());
 #endif
