44a77749941d64df35d9835f3f0d584211d6b953
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 10:14:20 +0000

    Bug 1239808 - Rename test_packages.json to include package basename as prefix. r=chmanchester, r=jlund
    MozReview-Commit-ID: LxTr8Mo2P0i
    
    UltraBlame original commit: 889072501f26d2e89e3d3552e7b4fad79cbffb75

--- build/gen_test_packages_manifest.py
+++ build/gen_test_packages_manifest.py
@@ -45,9 +45,6 @@ def parse_args():
     parser.add_argument("--jsshell", required=True,
                         action="store", dest="jsshell",
                         help="Name of the jsshell zip.")
-    parser.add_argument("--use-short-names", action="store_true",
-                        help="Use short names for packages (target.$name.tests.zip "
-                             "instead of $(PACKAGE_BASENAME).$name.tests.zip)")
     for harness in PACKAGE_SPECIFIED_HARNESSES:
         parser.add_argument("--%s" % harness, required=True,
                             action="store", dest=harness,
@@ -61,6 +58,7 @@ def parse_args():
                         help="Path to the output file to be written.")
     return parser.parse_args()
 
+
 def generate_package_data(args):
     
     
@@ -69,9 +67,6 @@ def generate_package_data(args):
     
     
     tests_common = args.tests_common
-    if args.use_short_names:
-        tests_common = 'target.common.tests.zip'
-
     jsshell = args.jsshell
 
     harness_requirements = dict([(k, [tests_common]) for k in ALL_HARNESSES])
@@ -80,8 +75,6 @@ def generate_package_data(args):
         pkg_name = getattr(args, harness, None)
         if pkg_name is None:
             continue
-        if args.use_short_names:
-            pkg_name = 'target.%s.tests.zip' % harness
         harness_requirements[harness].append(pkg_name)
     return harness_requirements
 
