e33dee1cec3d868174e888ec40c108ef9019efc4
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 22:44:11 +0000

    Bug 1283334 - Part 3: Do not sparsify dense arrays when freezing - Ion. r=nbp
    UltraBlame original commit: f0571b52b9037649859179ad3141de3309934868

--- js/src/jit/IonAnalysis.cpp
+++ js/src/jit/IonAnalysis.cpp
@@ -3563,6 +3563,13 @@ jit::AddKeepAliveInstructions(MIRGraph& graph)
                     continue;
                 }
 
+                if (use->isFallibleStoreElement()) {
+                    
+                    MOZ_ASSERT_IF(!use->toFallibleStoreElement()->object()->isUnbox() && !ownerObject->isUnbox(),
+                                  use->toFallibleStoreElement()->object() == ownerObject);
+                    continue;
+                }
+
                 if (use->isInArray()) {
                     
                     MOZ_ASSERT_IF(!use->toInArray()->object()->isUnbox() && !ownerObject->isUnbox(),
