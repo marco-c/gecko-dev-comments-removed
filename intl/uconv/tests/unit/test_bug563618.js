a1b5d33a5852a0f9f55c58d163bc8e387a1d677a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 07:50:47 +0000

    Bug 1232782 - Convert JS callsites to use open2 within intl/ (r=sicking)
    UltraBlame original commit: 67894aa872d7e67563a2ae4e97a78e35fa9e9b44

--- intl/uconv/tests/unit/test_bug563618.js
+++ intl/uconv/tests/unit/test_bug563618.js
@@ -7,7 +7,7 @@
 var Ci = Components.interfaces;
 var Cu = Components.utils;
 
-Cu.import("resource://gre/modules/Services.jsm");
+Cu.import("resource://gre/modules/NetUtil.jsm");
 
 const test = [
 
@@ -27,27 +27,16 @@ const test = [
 
 	       "abcdefghijklmnopqrstuvwxyz12test03\uFFFDfoobar"]];
 
-const IOService = Components.Constructor("@mozilla.org/network/io-service;1",
-                                         "nsIIOService");
 const ConverterInputStream =
       Components.Constructor("@mozilla.org/intl/converter-input-stream;1",
                              "nsIConverterInputStream",
                              "init");
-const ios = new IOService();
 
 function testCase(testText, expectedText, bufferLength, charset)
 {
   var dataURI = "data:text/plain;charset=" + charset + "," + testText;
-
-  var channel = ios.newChannel2(dataURI,
-                                "",
-                                null,
-                                null,      
-                                Services.scriptSecurityManager.getSystemPrincipal(),
-                                null,      
-                                Ci.nsILoadInfo.SEC_NORMAL,
-                                Ci.nsIContentPolicy.TYPE_OTHER);
-  var testInputStream = channel.open();
+  var channel = NetUtil.newChannel({uri: dataURI, loadUsingSystemPrincipal: true});
+  var testInputStream = channel.open2();
   var testConverter = new ConverterInputStream(testInputStream,
                                                charset,
                                                bufferLength,
