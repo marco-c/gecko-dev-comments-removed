fc529ee2324d1bf7d1659fa5aaeffd3d23a6a87f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 19:28:45 +0000

    Bug 1283019. Only post a schedule composite if the compositor loop exists. r=dvander
    UltraBlame original commit: 0f3ebce53943e0fa6f45534f73904ec2c30b8df6

--- gfx/layers/ipc/CompositorBridgeParent.cpp
+++ gfx/layers/ipc/CompositorBridgeParent.cpp
@@ -356,7 +356,7 @@ CompositorVsyncScheduler::PostCompositeTask(TimeStamp aCompositeTimestamp)
 {
   
   MonitorAutoLock lock(mCurrentCompositeTaskMonitor);
-  if (mCurrentCompositeTask == nullptr) {
+  if (mCurrentCompositeTask == nullptr && CompositorThreadHolder::Loop()) {
     RefPtr<CancelableRunnable> task =
       NewCancelableRunnableMethod<TimeStamp>(this, &CompositorVsyncScheduler::Composite,
                                              aCompositeTimestamp);
