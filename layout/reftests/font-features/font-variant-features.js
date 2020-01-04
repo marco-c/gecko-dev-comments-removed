cee9b50400cdabc24f2501f9e6de3dd1ca01e91b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 07:44:13 +0000

    Bug 1236086 - use a PUA codepoint in the reference rendering for font feature tests. r=jfkthame
    UltraBlame original commit: da327b54c78e7478f0f8072b4202f31c02f3caa1

--- layout/reftests/font-features/font-variant-features.js
+++ layout/reftests/font-features/font-variant-features.js
@@ -194,6 +194,10 @@ var gPropertyData = [
 
 
 
+
+
+const kRefCodepoint = 0xe00c;
+
 function createFeatureTestTable(propData, whichProp, isRef, debug)
 {
   var table = document.createElement("table");
@@ -268,7 +272,8 @@ function createFeatureTestTable(propData, whichProp, isRef, debug)
       }
 
       var span = document.createElement("span");
-      span.innerHTML = (isRef ? "P" : "&#x" + cp.toString(16) + ";");
+      var cpOut = (isRef ? kRefCodepoint : cp);
+      span.innerHTML = "&#x" + cpOut.toString(16) + ";";
       span.title = f + "=" + feature;
       cell.appendChild(span);
     }
