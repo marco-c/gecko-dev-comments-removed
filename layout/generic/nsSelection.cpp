c01ad9899843844e7e75e4fa59441a7222de2a8a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 09:50:13 +0000

    Bug 1238137 - Telemetry pings for main thread keyboard-driven scroll input methods. r=ehsan
    Pings are sent in the implementations of the nsISelectionController methods
    ScrollLine, ScrollPage, ScrollCharacter, and CompleteScroll. It is assumed
    that these methods are triggered by keyboard input.
    
    A small number of false positives can occur if these methods are called
    in response to something other than keyboard input; this is considered
    acceptable.
    
    UltraBlame original commit: d8b9add70407cd81159b9dc15e27ff91d31f8ff2

--- layout/generic/nsSelection.cpp
+++ layout/generic/nsSelection.cpp
@@ -78,9 +78,12 @@ static NS_DEFINE_CID(kFrameTraversalCID, NS_FRAMETRAVERSAL_CID);
 #include "mozilla/dom/SelectionBinding.h"
 #include "mozilla/AsyncEventDispatcher.h"
 #include "nsHTMLEditor.h"
+#include "mozilla/Telemetry.h"
+#include "mozilla/layers/ScrollInputMethods.h"
 
 using namespace mozilla;
 using namespace mozilla::dom;
+using mozilla::layers::ScrollInputMethod;
 
 
 
@@ -2042,6 +2045,8 @@ nsFrameSelection::CommonPageMove(bool aForward,
     return;
 
   
+  mozilla::Telemetry::Accumulate(mozilla::Telemetry::SCROLL_INPUT_METHODS,
+      (uint32_t) ScrollInputMethod::MainThreadScrollPage);
   aScrollableFrame->ScrollBy(nsIntPoint(0, aForward ? 1 : -1),
                              nsIScrollableFrame::PAGES,
                              nsIScrollableFrame::SMOOTH);
