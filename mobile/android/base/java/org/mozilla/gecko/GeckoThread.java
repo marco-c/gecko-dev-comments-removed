50aac74e1888636588e56de8c39e370969bb0f9e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 15:13:05 +0000

    Bug 1252650 - Add GeckoThread.canUseProfile; r=snorp canUseProfile returns whether we're currently using the given profile or we can potentially start Gecko with the given profile.
    UltraBlame original commit: 3fc77a3172823ddddf2646c8bb4325f9063cb346

--- mobile/android/base/java/org/mozilla/gecko/GeckoThread.java
+++ mobile/android/base/java/org/mozilla/gecko/GeckoThread.java
@@ -130,17 +130,10 @@ public class GeckoThread extends Thread {
         return false;
     }
 
-    public static boolean initWithProfile(String profileName, File profileDir) {
-        if (profileName == null) {
-            throw new IllegalArgumentException("Null profile name");
-        }
-
-        final GeckoProfile profile = getActiveProfile();
+    private static boolean canUseProfile(final GeckoProfile profile, final String profileName,
+                                         final File profileDir) {
         if (profile == null) {
-            
-            final Context context = GeckoAppShell.getApplicationContext();
-            return init(GeckoProfile.get(context, profileName, profileDir),
-                         null,  null,  false);
+            return true;
         }
 
         
@@ -151,6 +144,29 @@ public class GeckoThread extends Thread {
             Log.e(LOGTAG, "Cannot compare profile " + profileName);
             return false;
         }
+    }
+
+    public static boolean canUseProfile(final String profileName, final File profileDir) {
+        if (profileName == null) {
+            throw new IllegalArgumentException("Null profile name");
+        }
+        return canUseProfile(getActiveProfile(), profileName, profileDir);
+    }
+
+    public static boolean initWithProfile(final String profileName, final File profileDir) {
+        if (profileName == null) {
+            throw new IllegalArgumentException("Null profile name");
+        }
+
+        final GeckoProfile profile = getActiveProfile();
+        if (profile != null) {
+            return canUseProfile(profile, profileName, profileDir);
+        }
+
+        
+        final Context context = GeckoAppShell.getApplicationContext();
+        return init(GeckoProfile.get(context, profileName, profileDir),
+                     null,  null,  false);
     }
 
     public static boolean launch() {
