f88c5942179dd4b35e8cb9ce3d893dfb785c2dfa
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 03:45:19 +0000

    Bug 1206628 - Fetch the latest search engines each time visibility of the BrowserSearch Fragment changes. r=mcomella
    UltraBlame original commit: 8ea4740134e10db3dcd57c094deedfd562d518b7

--- mobile/android/base/home/BrowserSearch.java
+++ mobile/android/base/home/BrowserSearch.java
@@ -238,6 +238,14 @@ public class BrowserSearch extends HomeFragment
     }
 
     @Override
+    public void onHiddenChanged(boolean hidden) {
+        if (!hidden) {
+            GeckoAppShell.sendEventToGecko(GeckoEvent.createBroadcastEvent("SearchEngines:GetVisible", null));
+        }
+        super.onHiddenChanged(hidden);
+    }
+
+    @Override
     public void onResume() {
         super.onResume();
 
@@ -552,6 +560,22 @@ public class BrowserSearch extends HomeFragment
         mAdapter.notifyDataSetChanged();
     }
 
+    private boolean shouldUpdateSearchEngine(ArrayList<SearchEngine> searchEngines) {
+        if (searchEngines.size() != mSearchEngines.size()) {
+            return true;
+        }
+
+        int size = searchEngines.size();
+
+        for (int i = 0; i < size; i++) {
+            if (!mSearchEngines.get(i).name.equals(searchEngines.get(i).name)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
     private void setSearchEngines(JSONObject data) {
         ThreadUtils.assertOnUiThread();
 
@@ -597,14 +621,16 @@ public class BrowserSearch extends HomeFragment
                 }
             }
 
-            mSearchEngines = Collections.unmodifiableList(searchEngines);
-            mLastLocale = Locale.getDefault();
+            
+            boolean change = shouldUpdateSearchEngine(searchEngines);
 
-            if (mAdapter != null) {
+            if (mAdapter != null && change) {
+                mSearchEngines = Collections.unmodifiableList(searchEngines);
+                mLastLocale = Locale.getDefault();
+                updateSearchEngineBar();
+
                 mAdapter.notifyDataSetChanged();
             }
-
-            updateSearchEngineBar();
 
             
             
