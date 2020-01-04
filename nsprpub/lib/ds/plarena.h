b1869beaf52da88ed11a24d4d8c6c05c9c06fb78
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 11:04:49 +0000

    Bug 1252639 (part 2) - Move contents of plarenas.h into plarena.h. r=glandium.
    Because the plarenas.h/plarena.h is strange and confusing. But the patch keeps
    plarenas.h around in a way that the API doesn't change -- any code that
    included plarena.h or plarenas.h should not need changing.
    
    UltraBlame original commit: bf22fcece06e85cd195d61e37382ffc0084e4d05

--- nsprpub/lib/ds/plarena.h
+++ nsprpub/lib/ds/plarena.h
@@ -13,7 +13,6 @@
 
 
 #include "prtypes.h"
-#include "plarenas.h"
 
 PR_BEGIN_EXTERN_C
 
@@ -46,6 +45,8 @@ struct PLArenaStats {
     PRFloat64     variance;     
 };
 #endif
+
+typedef struct PLArenaPool      PLArenaPool;
 
 struct PLArenaPool {
     PLArena     first;          
@@ -224,6 +225,74 @@ PR_IMPORT(void) __asan_unpoison_memory_region(void const volatile *addr, size_t 
         free(a); \
         (a) = 0; \
     PR_END_MACRO
+
+
+
+
+
+
+
+
+
+
+PR_EXTERN(void) PL_InitArenaPool(
+    PLArenaPool *pool, const char *name, PRUint32 size, PRUint32 align);
+
+
+
+
+
+
+PR_EXTERN(void) PL_ArenaFinish(void);
+
+
+
+
+
+
+PR_EXTERN(void) PL_FreeArenaPool(PLArenaPool *pool);
+
+
+
+
+PR_EXTERN(void) PL_FinishArenaPool(PLArenaPool *pool);
+
+
+
+
+
+PR_EXTERN(void) PL_CompactArenaPool(PLArenaPool *pool);
+
+
+
+
+
+
+
+PR_EXTERN(void *) PL_ArenaAllocate(PLArenaPool *pool, PRUint32 nb);
+
+PR_EXTERN(void *) PL_ArenaGrow(
+    PLArenaPool *pool, void *p, PRUint32 size, PRUint32 incr);
+
+PR_EXTERN(void) PL_ArenaRelease(PLArenaPool *pool, char *mark);
+
+
+
+
+PR_EXTERN(void) PL_ClearArenaPool(PLArenaPool *pool, PRInt32 pattern);
+
+
+
+
+
+typedef size_t (*PLMallocSizeFn)(const void *ptr);
+
+
+
+
+
+PR_EXTERN(size_t) PL_SizeOfArenaPoolExcludingPool(
+    const PLArenaPool *pool, PLMallocSizeFn mallocSizeOf);
 
 #ifdef PL_ARENAMETER
 
