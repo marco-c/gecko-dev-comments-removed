28bbfd97e6e6193ac049118bbbf1e54fd7a03d91
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 03:34:30 +0000

    Bug 1213545. Carry x,y offsets with inset box shadows. r=mstange
    UltraBlame original commit: 2965de9363e93a226aebb3b3f16bcaece5753984

--- gfx/thebes/gfxBlur.cpp
+++ gfx/thebes/gfxBlur.cpp
@@ -980,7 +980,8 @@ gfxAlphaBoxBlur::BlurInsetBox(gfxContext* aDestinationCtx,
                               const Color& aShadowColor,
                               bool aHasBorderRadius,
                               const RectCornerRadii& aInnerClipRadii,
-                              const Rect aSkipRect)
+                              const Rect aSkipRect,
+                              const Point aShadowOffset)
 {
   
   if ((aBlurRadius.width <= 0 && aBlurRadius.height <= 0)) {
@@ -1005,6 +1006,7 @@ gfxAlphaBoxBlur::BlurInsetBox(gfxContext* aDestinationCtx,
   srcInner.Deflate(Margin(slice));
 
   Rect dstOuter(aDestinationRect);
+  dstOuter.MoveBy(aShadowOffset);
   dstOuter.Inflate(Margin(extendDest));
   Rect dstInner = dstOuter;
   dstInner.Deflate(Margin(slice));
