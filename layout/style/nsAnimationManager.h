129163e128c32d9a52958b0254a8789b2f829283
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 06:57:37 +0000

    Bug 1219543 - Part 2: Avoid the period that mIsRunningOnCompositor is false between restyling and building display list. r=bbirtles
    Without this fix, mIsRunningOnCompositor will be unpredictable in
    MutationObserver callbacks.
    
    For example:
    
     mIsRunningOnCompositor will be false if the micro task for
     the MutationObserver is processed before building display list.
    
     mIsRunningOnCompositor will be true if there is no room to process
     the micro task before building display list.
    
    UltraBlame original commit: ea91eabfc2506100487a733fbf62ddf1d0b93b06

--- layout/style/nsAnimationManager.h
+++ layout/style/nsAnimationManager.h
@@ -22,6 +22,7 @@ namespace css {
 class Declaration;
 } 
 namespace dom {
+class KeyframeEffectReadOnly;
 class Promise;
 } 
 
@@ -373,6 +374,9 @@ private:
   static void UpdateCascadeResults(nsStyleContext* aStyleContext,
                                    mozilla::AnimationCollection*
                                      aElementAnimations);
+  static void CopyIsRunningOnCompositor(
+    mozilla::dom::KeyframeEffectReadOnly& aSourceEffect,
+    mozilla::dom::KeyframeEffectReadOnly& aDestEffect);
 };
 
 #endif 
