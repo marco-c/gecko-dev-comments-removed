058326fe16556f8fb396842ae92e0cc9c85341ea
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 16:34:29 +0000

    Bug 1271460, don't leak editor created element objects, r=ehsan
    UltraBlame original commit: 009736e264b46b6284ae49f97e6e2b420532537d

--- editor/libeditor/nsHTMLInlineTableEditor.cpp
+++ editor/libeditor/nsHTMLInlineTableEditor.cpp
@@ -109,7 +109,6 @@ nsHTMLEditor::HideInlineTableEditingUI()
 
   
   nsCOMPtr<nsIContent> bodyContent = GetRoot();
-  NS_ENSURE_TRUE(bodyContent, NS_ERROR_FAILURE);
 
   DeleteRefToAnonymousNode(mAddColumnBeforeButton, bodyContent, ps);
   mAddColumnBeforeButton = nullptr;
