aa776a97db4d163a924c162786fbd78ff3da4ed0
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 07:49:30 +0000

    Bug 1183320 - Remove FHR from android/services r=rnewman
    UltraBlame original commit: d9661fecd939146a14c5c591f395a68e2d75d211

--- mobile/android/services/src/main/java/org/mozilla/gecko/background/common/GlobalConstants.java
+++ mobile/android/services/src/main/java/org/mozilla/gecko/background/common/GlobalConstants.java
@@ -24,15 +24,6 @@ public class GlobalConstants {
   public static final int SHARED_PREFERENCES_MODE = 0;
 
   
-  
-  
-  
-  
-  public static String GECKO_PREFERENCES_CLASS = "org.mozilla.gecko.preferences.GeckoPreferences";
-  public static String GECKO_BROADCAST_HEALTHREPORT_UPLOAD_PREF_METHOD  = "broadcastHealthReportUploadPref";
-  public static String GECKO_BROADCAST_HEALTHREPORT_PRUNE_METHOD = "broadcastHealthReportPrune";
-
-  
   public static final long MILLISECONDS_PER_DAY = 24 * 60 * 60 * 1000;
   public static final long MILLISECONDS_PER_SIX_MONTHS = 180 * MILLISECONDS_PER_DAY;
 
