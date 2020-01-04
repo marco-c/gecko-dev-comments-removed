fc05e88e7249f2c1370697d1b30084d12d51040d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 10:51:23 +0000

    Bug 1253236 - Remove window argument from element.isVisible; r=automatedtester
    MozReview-Commit-ID: ITzY9RXjJCG
    
    UltraBlame original commit: 746ba7188bcfcb4f1c4906a83e0a5eb961cbee57

--- testing/marionette/element.js
+++ testing/marionette/element.js
@@ -815,7 +815,9 @@ element.inViewport = function(el, x = undefined, y = undefined) {
 
 
 
-element.isVisible = function(el, win, x = undefined, y = undefined) {
+element.isVisible = function(el, x = undefined, y = undefined) {
+  let win = el.ownerDocument.defaultView;
+
   
   let ns = atom.getElementAttribute(el, "namespaceURI", win);
   if (ns.indexOf("there.is.only.xul") < 0 &&
