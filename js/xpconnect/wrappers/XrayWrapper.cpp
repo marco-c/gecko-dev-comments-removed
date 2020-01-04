190d4800c2a542542585dd86fd677094b2982409
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 02:03:24 +0000

    Bug 1054756, part 4 - Remove BaseProxyHandler::defaultValue. r=jandem.
    UltraBlame original commit: 105433ce195b39f10f9f0b939c8786a0aff6a70f

--- js/xpconnect/wrappers/XrayWrapper.cpp
+++ js/xpconnect/wrappers/XrayWrapper.cpp
@@ -2184,19 +2184,6 @@ XrayWrapper<Base, Traits>::className(JSContext* cx, HandleObject wrapper) const
 
 template <typename Base, typename Traits>
 bool
-XrayWrapper<Base, Traits>::defaultValue(JSContext* cx, HandleObject wrapper,
-                                        JSType hint, MutableHandleValue vp) const
-{
-    
-    
-    
-    
-    
-    return OrdinaryToPrimitive(cx, wrapper, hint, vp);
-}
-
-template <typename Base, typename Traits>
-bool
 XrayWrapper<Base, Traits>::getPrototype(JSContext* cx, JS::HandleObject wrapper,
                                         JS::MutableHandleObject protop) const
 {
