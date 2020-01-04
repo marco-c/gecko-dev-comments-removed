81bf1f6881811281e766fb82d5c484a7ec0a76dd
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 09:07:45 +0000

    Bug 1244523: [mp4] P2. Have sniffer recognise ftyp qt subtype. r=cpearce
    UltraBlame original commit: b0c3e8efafc8aa13935d023762e26495fc9b99a9

--- toolkit/components/mediasniffer/nsMediaSniffer.cpp
+++ toolkit/components/mediasniffer/nsMediaSniffer.cpp
@@ -42,7 +42,8 @@ nsMediaSnifferEntry sFtypEntries[] = {
   PATTERN_ENTRY("\xFF\xFF\xFF", "avc", VIDEO_MP4), 
   PATTERN_ENTRY("\xFF\xFF\xFF", "3gp", VIDEO_3GPP), 
   PATTERN_ENTRY("\xFF\xFF\xFF\xFF", "M4A ", AUDIO_MP4),
-  PATTERN_ENTRY("\xFF\xFF\xFF\xFF", "M4P ", AUDIO_MP4)
+  PATTERN_ENTRY("\xFF\xFF\xFF\xFF", "M4P ", AUDIO_MP4),
+  PATTERN_ENTRY("\xFF\xFF\xFF\xFF", "qt  ", VIDEO_MP4),
 };
 
 static bool MatchesBrands(const uint8_t aData[4], nsACString& aSniffedType)
