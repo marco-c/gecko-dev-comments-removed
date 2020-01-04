61ca8956c22054b5c2a1e3fd6a1a4ada815758cb
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 18:30:57 +0000

    Bug 1278700 - Add missing libraries to Mac artifact builds to avoid failing in the packager when doing an artifact build on automation. r=mshal
    MozReview-Commit-ID: 7wv57TcdLUR
    
    UltraBlame original commit: b70b95b46ae5adf4df0969c6d3976e2e74b8540b

--- python/mozbuild/mozbuild/artifacts.py
+++ python/mozbuild/mozbuild/artifacts.py
@@ -290,6 +290,8 @@ class MacArtifactJob(ArtifactJob):
                 'libplugin_child_interpose.dylib',
                 
                 
+                'libmozavutil.dylib',
+                'libmozavcodec.dylib',
                 'libsoftokn3.dylib',
                 'plugin-container.app/Contents/MacOS/plugin-container',
                 'updater.app/Contents/MacOS/org.mozilla.updater',
