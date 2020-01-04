d5f5b014731628ebc9be83c91e418ff4d6a53032
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 10:01:50 +0000

    Backed out changeset dc704cb50a24 (bug 1249451) for bustage
    UltraBlame original commit: 78162b5666f91f750ae6c8af2cf16392be193e0a

--- xpcom/base/nsCycleCollector.cpp
+++ xpcom/base/nsCycleCollector.cpp
@@ -1492,26 +1492,6 @@ struct CCGraphDescriber : public LinkedListElement<CCGraphDescriber>
   Type mType;
 };
 
-class LogStringMessageAsync : public nsCancelableRunnable
-{
-public:
-  LogStringMessageAsync(const nsAString& aMsg) : mMsg(aMsg)
-  {}
-
-  NS_IMETHOD Run() override
-  {
-    nsCOMPtr<nsIConsoleService> cs =
-      do_GetService(NS_CONSOLESERVICE_CONTRACTID);
-    if (cs) {
-       cs->LogStringMessage(mMsg.get());
-    }
-    return NS_OK;
-  }
-
-private:
-  nsString mMsg;
-};
-
 class nsCycleCollectorLogSinkToFile final : public nsICycleCollectorLogSink
 {
 public:
@@ -1710,16 +1690,17 @@ private:
     aLog->mFile = logFileFinalDestination;
 
     
-    nsAutoString logPath;
-    logFileFinalDestination->GetPath(logPath);
-    nsAutoString msg = aCollectorKind +
-      NS_LITERAL_STRING(" Collector log dumped to ") + logPath;
+    nsCOMPtr<nsIConsoleService> cs =
+      do_GetService(NS_CONSOLESERVICE_CONTRACTID);
+    if (cs) {
+      
+      nsAutoString logPath;
+      logFileFinalDestination->GetPath(logPath);
 
-    
-    
-    
-    RefPtr<LogStringMessageAsync> log = new LogStringMessageAsync(msg);
-    NS_DispatchToCurrentThread(log);
+      nsString msg = aCollectorKind +
+        NS_LITERAL_STRING(" Collector log dumped to ") + logPath;
+      cs->LogStringMessage(msg.get());
+    }
     return NS_OK;
   }
 
