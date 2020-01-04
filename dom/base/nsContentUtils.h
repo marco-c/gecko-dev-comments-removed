f94bd6daa244a2a780ba40f00c6ff4ec9e259139
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 22:58:29 +0000

    Backed out changeset 53295d9cc8ef (bug 1297539) for failing own test /browser_bug1297539.js. r=backout
    UltraBlame original commit: 43a0ed761011fd5f569b7baaeaf42a040698cc4e

--- dom/base/nsContentUtils.h
+++ dom/base/nsContentUtils.h
@@ -127,7 +127,6 @@ class IPCDataTransferItem;
 class NodeInfo;
 class nsIContentChild;
 class nsIContentParent;
-class TabChild;
 class Selection;
 class TabParent;
 } 
@@ -2493,13 +2492,6 @@ public:
 
 
   static bool IsFlavorImage(const nsACString& aFlavor);
-
-  static nsresult IPCTransferableToTransferable(const mozilla::dom::IPCDataTransfer& aDataTransfer,
-                                                const bool& aIsPrivateData,
-                                                nsIPrincipal* aRequestingPrincipal,
-                                                nsITransferable* aTransferable,
-                                                mozilla::dom::nsIContentParent* aContentParent,
-                                                mozilla::dom::TabChild* aTabChild);
 
   static void TransferablesToIPCTransferables(nsISupportsArray* aTransferables,
                                               nsTArray<mozilla::dom::IPCDataTransfer>& aIPC,
