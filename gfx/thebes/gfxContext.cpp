31d3b0b11f062b2ade8c1387e60c8a501e5ba9f1
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 09:41:24 +0000

    Bug 1247380: Only copy the background if we can succesfully get a snapshot. r=jrmuizel
    MozReview-Commit-ID: uWH7MtJmSp
    
    UltraBlame original commit: b2b9363f3edccd028f45e0db2860b2cc07cad913

--- gfx/thebes/gfxContext.cpp
+++ gfx/thebes/gfxContext.cpp
@@ -854,9 +854,11 @@ gfxContext::PushGroupAndCopyBackground(gfxContentType content, Float aOpacity, S
       mDT->PushLayer(content == gfxContentType::COLOR, aOpacity, aMask, aMaskTransform, IntRect(), false);
     }
   } else {
-    if (pushOpaqueWithCopiedBG) {
+    RefPtr<SourceSurface> source;
+    
+    
+    if (pushOpaqueWithCopiedBG && (source = mDT->Snapshot())) {
       DrawTarget *oldDT = mDT;
-      RefPtr<SourceSurface> source = mDT->Snapshot();
       Point oldDeviceOffset = CurrentState().deviceOffset;
 
       PushNewDT(gfxContentType::COLOR);
