b8d193635c1cb456b9846909c6b63439df15e8c9
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 23:25:00 +0000

    Merge mozilla-central to mozilla-inbound
    UltraBlame original commit: 542b38d5eb900afb47e2f9c07cb47300bddef870

--- browser/base/content/nsContextMenu.js
+++ browser/base/content/nsContextMenu.js
@@ -603,8 +603,9 @@ nsContextMenu.prototype = {
     }
 
     const xulNS = "http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul";
-    if (aNode.namespaceURI == xulNS ||
-        aNode.nodeType == Node.DOCUMENT_NODE) {
+    if (aNode.nodeType == Node.DOCUMENT_NODE ||
+        
+        (aNode.namespaceURI == xulNS && !isXULTextLinkLabel(aNode))) {
       this.shouldDisplay = false;
       return;
     }
@@ -798,7 +799,8 @@ nsContextMenu.prototype = {
         if (!this.onLink &&
             
             
-             ((elem instanceof HTMLAnchorElement && elem.href) ||
+             (isXULTextLinkLabel(elem) ||
+              (elem instanceof HTMLAnchorElement && elem.href) ||
               (elem instanceof HTMLAreaElement && elem.href) ||
               elem instanceof HTMLLinkElement ||
               elem.getAttributeNS("http://www.w3.org/1999/xlink", "type") == "simple")) {
@@ -897,6 +899,13 @@ nsContextMenu.prototype = {
         this.showItem("spell-check-enabled", canSpell);
         this.showItem("spell-separator", canSpell);
       }
+    }
+
+    function isXULTextLinkLabel(node) {
+      return node.namespaceURI == xulNS &&
+             node.tagName == "label" &&
+             node.classList.contains('text-link') &&
+             node.href;
     }
   },
 
