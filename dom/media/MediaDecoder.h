2cf0afa25b094e7a2585fede97852cf4af3596f4
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 21:01:07 +0000

    Bug 1290780. Part 1 - Assert IsShutdown() is false in MediaDecoder::UpdateLogicalPosition(). r=kaku 1. Callbacks from the watch manager are disconnected in Shutdown(). 2. It is called from MediaOmxCommonDecoder::NotifyOffloadPlayerPositionChanged() which will not happen after Shutdown(). 3. It is called from MediaOmxCommonDecoder::ResumeStateMachine() which returns early when IsShutdown() is true.
    MozReview-Commit-ID: COmPFaQzNTq
    
    UltraBlame original commit: 9a27fb70e7057fa9fc57bf35200bd37b5cd87f5f

--- dom/media/MediaDecoder.h
+++ dom/media/MediaDecoder.h
@@ -416,6 +416,7 @@ private:
   void UpdateLogicalPosition()
   {
     MOZ_ASSERT(NS_IsMainThread());
+    MOZ_ASSERT(!IsShutdown());
     
     if (mPlayState == PLAY_STATE_PAUSED || IsSeeking()) {
       return;
