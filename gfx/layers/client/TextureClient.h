a1838c266b51134d02ca41ef72588f16ac90dc27
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 22:40:36 +0000

    Bug 1293908 - Specify the LayersBackend to be used when creating Textures r=nical
    UltraBlame original commit: f9dfb9c04270fa69231a0d249a69f9a8af741906

--- gfx/layers/client/TextureClient.h
+++ gfx/layers/client/TextureClient.h
@@ -380,6 +380,16 @@ public:
                            gfx::SurfaceFormat aFormat,
                            gfx::IntSize aSize,
                            gfx::BackendType aMoz2dBackend,
+                           LayersBackend aLayersBackend,
+                           TextureFlags aTextureFlags,
+                           TextureAllocationFlags flags = ALLOC_DEFAULT);
+
+  
+  static already_AddRefed<TextureClient>
+  CreateForRawBufferAccess(ClientIPCAllocator* aAllocator,
+                           gfx::SurfaceFormat aFormat,
+                           gfx::IntSize aSize,
+                           gfx::BackendType aMoz2dBackend,
                            TextureFlags aTextureFlags,
                            TextureAllocationFlags flags = ALLOC_DEFAULT);
 
