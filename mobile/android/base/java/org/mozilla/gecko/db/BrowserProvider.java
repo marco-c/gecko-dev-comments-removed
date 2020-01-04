05d68560e8c2947a137a2f0e1e34063471a72083
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 23:09:45 +0000

    Bug 1293710 - Activity Stream Highlights: Only select actual bookmarks (no folders and other special types). r=ahunt
    MozReview-Commit-ID: A5oV8vIIyhw
    
    UltraBlame original commit: 8bd1bb9a4fd019646ff40f596f63afde00d7d04f

--- mobile/android/base/java/org/mozilla/gecko/db/BrowserProvider.java
+++ mobile/android/base/java/org/mozilla/gecko/db/BrowserProvider.java
@@ -1172,6 +1172,7 @@ public class BrowserProvider extends SharedBrowserDatabaseProvider {
                 "AND (" + DBUtils.qualifyColumn(History.TABLE_NAME, History.VISITS) + " <= 3 " +
                   "OR " + DBUtils.qualifyColumn(History.TABLE_NAME, History.VISITS) + " IS NULL) " +
                 "AND " + DBUtils.qualifyColumn(Bookmarks.TABLE_NAME, Bookmarks.IS_DELETED)  + " = 0 " +
+                "AND " + DBUtils.qualifyColumn(Bookmarks.TABLE_NAME, Bookmarks.TYPE) + " = " + Bookmarks.TYPE_BOOKMARK + " " +
                 
                 "ORDER BY " + DBUtils.qualifyColumn(Bookmarks.TABLE_NAME, Bookmarks.DATE_CREATED) + " DESC " +
                 "LIMIT " + bookmarkLimit + ")";
