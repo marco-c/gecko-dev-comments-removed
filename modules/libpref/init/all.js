a4446be87fb21dc58ab47b80ac18a4c260518f42
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 19:06:22 +0000

    Bug 1244474 - cut off path from URLs passed to PAC scripts, r=mcmanus
    UltraBlame original commit: df6b25262c658e87715c98990d36a6b7fd8f8a0a

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -1957,6 +1957,8 @@ pref("network.cookie.lifetime.days",        90);
 
 pref("network.proxy.autoconfig_url", "");
 
+pref("network.proxy.autoconfig_url.include_path", false);
+
 
 
 pref("network.proxy.autoconfig_retry_interval_min", 5);    
