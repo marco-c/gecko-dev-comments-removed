802ae7a243e408dd7719bdef356c689a3de08fec
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 14:18:23 +0000

    Bug 1261352 - Update pdf.js to version 1.4.185. r=bdahl, r=Mossop
    UltraBlame original commit: d9c26f4f5a3be0f1023238ab1d7e3932ba165b0b

--- browser/extensions/pdfjs/content/web/viewer.js
+++ browser/extensions/pdfjs/content/web/viewer.js
@@ -3911,7 +3911,6 @@ var PDFPageView = (function PDFPageViewClosure() {
 
       
       canvas.style.width = (PRINT_OUTPUT_SCALE * 100) + '%';
-      canvas.style.height = (PRINT_OUTPUT_SCALE * 100) + '%';
 
       var cssScale = 'scale(' + (1 / PRINT_OUTPUT_SCALE) + ', ' +
                                 (1 / PRINT_OUTPUT_SCALE) + ')';
@@ -3920,8 +3919,6 @@ var PDFPageView = (function PDFPageViewClosure() {
 
       var printContainer = document.getElementById('printContainer');
       var canvasWrapper = document.createElement('div');
-      canvasWrapper.style.width = viewport.width + 'pt';
-      canvasWrapper.style.height = viewport.height + 'pt';
       canvasWrapper.appendChild(canvas);
       printContainer.appendChild(canvasWrapper);
 
@@ -7331,10 +7328,6 @@ var PDFViewerApplication = {
       
       '@supports ((size:A4) and (size:1pt 1pt)) {' +
       '@page { size: ' + pageSize.width + 'pt ' + pageSize.height + 'pt;}' +
-      
-      
-      '#printContainer {height:100%}' +
-      '#printContainer > div {width:100% !important;height:100% !important;}' +
       '}';
     body.appendChild(this.pageStyleSheet);
 
