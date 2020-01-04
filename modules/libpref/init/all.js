d9f929daf4e2e79931c449a6d74cfc9273148965
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 23:32:16 +0000

    Back out ec584a62ec26 (bug 918719) for Android failures in test_bug918719.html
    UltraBlame original commit: af1a3c41d8c139f070a553f1125b106cea3d596d

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -5081,10 +5081,6 @@ pref("dom.voicemail.defaultServiceId", 0);
 pref("dom.mapped_arraybuffer.enabled", true);
 
 
-
-pref("dom.fire_extra_xhr_loading_readystatechanges", false);
-
-
 pref("urlclassifier.malwareTable", "goog-malware-shavar,goog-unwanted-shavar,test-malware-simple,test-unwanted-simple");
 
 #ifdef MOZILLA_OFFICIAL
