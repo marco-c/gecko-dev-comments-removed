6ad9568a23a528417ba6fbc7c5f4956064b516de
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 17:57:32 +0000

    Bug 1277650 - part 3 - add MOZ_JNICALL attribute to MakeNativeMethod's argument; r=darchons
    clang complains that it's unable to instantiate this template because
    the functions being passed as arguments are MOZ_JNICALL, while the
    actual parameter to the function has no such attribute.  Adding the
    attribute makes everything line up.
    
    UltraBlame original commit: 4cd9cb917c456c8d21da9c58b953480c4df25c25

--- widget/android/jni/Natives.h
+++ widget/android/jni/Natives.h
@@ -639,7 +639,7 @@ struct NativeStub : detail::NativeStubImpl
 
 
 template<class Traits, typename Ret, typename... Args>
-constexpr JNINativeMethod MakeNativeMethod(Ret (*stub)(JNIEnv*, Args...))
+constexpr JNINativeMethod MakeNativeMethod(MOZ_JNICALL Ret (*stub)(JNIEnv*, Args...))
 {
     return {
         Traits::name,
