53d413642dced984cb111f15e1186c9d58f4316d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 10:08:28 +0000

    Bug 1091561 - fix tab bookkeeping for cut/copy/paste tests to make them work on e10s, rs=trivial
    MozReview-Commit-ID: 4b1ZMZ6q3Yh
    
    UltraBlame original commit: 68f52e2e1dc833c6ee0a159596d871b0f32b8013

--- browser/components/customizableui/test/browser_947914_button_paste.js
+++ browser/components/customizableui/test/browser_947914_button_paste.js
@@ -8,40 +8,34 @@ var initialLocation = gBrowser.currentURI.spec;
 var globalClipboard;
 
 add_task(function*() {
-  info("Check paste button existence and functionality");
+  yield BrowserTestUtils.withNewTab({gBrowser, url: "about:blank"}, function*() {
+    info("Check paste button existence and functionality");
 
-  let clipboard = Cc["@mozilla.org/widget/clipboardhelper;1"].getService(Ci.nsIClipboardHelper);
-  globalClipboard = Services.clipboard.kGlobalClipboard;
+    let clipboard = Cc["@mozilla.org/widget/clipboardhelper;1"].getService(Ci.nsIClipboardHelper);
+    globalClipboard = Services.clipboard.kGlobalClipboard;
 
-  yield PanelUI.show();
-  info("Menu panel was opened");
+    yield PanelUI.show();
+    info("Menu panel was opened");
 
-  let pasteButton = document.getElementById("paste-button");
-  ok(pasteButton, "Paste button exists in Panel Menu");
+    let pasteButton = document.getElementById("paste-button");
+    ok(pasteButton, "Paste button exists in Panel Menu");
 
-  
-  let text = "Sample text for testing";
-  clipboard.copyString(text);
+    
+    let text = "Sample text for testing";
+    clipboard.copyString(text);
 
-  
-  gURLBar.focus();
-  yield PanelUI.show();
-  info("Menu panel was opened");
+    
+    gURLBar.focus();
+    yield PanelUI.show();
+    info("Menu panel was opened");
 
-  ok(!pasteButton.hasAttribute("disabled"), "Paste button is enabled");
-  pasteButton.click();
+    ok(!pasteButton.hasAttribute("disabled"), "Paste button is enabled");
+    pasteButton.click();
 
-  is(gURLBar.value, text, "Text pasted successfully");
+    is(gURLBar.value, text, "Text pasted successfully");
+  });
 });
 
-add_task(function* asyncCleanup() {
-  
+registerCleanupFunction(function cleanup() {
   Services.clipboard.emptyClipboard(globalClipboard);
-  info("Clipboard was cleared");
-
-  
-  gBrowser.addTab(initialLocation);
-  gBrowser.removeTab(gBrowser.selectedTab);
-  info("Tabs were restored");
 });
-
