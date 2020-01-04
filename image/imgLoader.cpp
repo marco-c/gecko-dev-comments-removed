a80f719858e46e1486698648add7c175be0e4751
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 03:36:27 +0000

    Bug 1180715 (1/4) - Track image LoadTime to compare with file mtime. review=seth
    mTouchedTime is not appropriate for this as it is updated when an image
    load re-uses the same imgRequest, especially as it has one second
    granularity. A timestamp that is updated every time the backing file is
    re-read should work better.
    
    A millisecond granularity timestamp would be preferable, and would be
    achievable on most or all supported platforms. But some older
    filesystems have timestamp granularity of a second or worse, notably
    ext3 and FAT32 (and even ext4 filesytems created with inode_size < 256
    bytes, e.g. with 'mke2fs -t small' - see
    https://ext4.wiki.kernel.org/index.php/Ext4_Disk_Layout#Inode_Timestamps
    for details.)
    
    UltraBlame original commit: 38558bb6671b53b51716c9a8d897b9eb1f3668a8

--- image/imgLoader.cpp
+++ image/imgLoader.cpp
@@ -862,6 +862,7 @@ imgCacheEntry::imgCacheEntry(imgLoader* loader, imgRequest* request,
    mRequest(request),
    mDataSize(0),
    mTouchedTime(SecondsFromPRTime(PR_Now())),
+   mLoadTime(SecondsFromPRTime(PR_Now())),
    mExpiryTime(0),
    mMustValidate(false),
    
@@ -896,6 +897,11 @@ imgCacheEntry::UpdateCache(int32_t diff )
   if (!Evicted() && HasNoProxies()) {
     mLoader->CacheEntriesChanged(mRequest->IsChrome(), diff);
   }
+}
+
+void imgCacheEntry::UpdateLoadTime()
+{
+  mLoadTime = SecondsFromPRTime(PR_Now());
 }
 
 void
@@ -1710,7 +1716,7 @@ imgLoader::ValidateEntry(imgCacheEntry* aEntry,
   
   nsCOMPtr<nsIFileURL> fileUrl(do_QueryInterface(aURI));
   if (fileUrl) {
-    uint32_t lastModTime = aEntry->GetTouchedTime();
+    uint32_t lastModTime = aEntry->GetLoadTime();
 
     nsCOMPtr<nsIFile> theFile;
     rv = fileUrl->GetFile(getter_AddRefs(theFile));
