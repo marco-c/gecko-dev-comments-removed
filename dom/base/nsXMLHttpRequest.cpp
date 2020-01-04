3e5a3c540564f3aa7e84ab893e2d3a3c3bcc7ffb
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 15:13:46 +0000

    Bug 1265970.  When throwing an exception from XHR open() due to not being in an active document, make sure it's an InvalidStateError.  r=smaug
    UltraBlame original commit: 408ac64b72edd03b0b9f480d06787d03f90f07a0

--- dom/base/nsXMLHttpRequest.cpp
+++ dom/base/nsXMLHttpRequest.cpp
@@ -1553,7 +1553,9 @@ nsXMLHttpRequest::Open(const nsACString& inMethod, const nsACString& url,
     
     
     nsresult rv = CheckInnerWindowCorrectness();
-    NS_ENSURE_SUCCESS(rv, rv);
+    if (NS_WARN_IF(NS_FAILED(rv))) {
+      return NS_ERROR_DOM_INVALID_STATE_ERR;
+    }
   }
 
   nsCOMPtr<nsIURI> baseURI;
