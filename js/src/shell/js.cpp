9508bbc5b49b2dae9cd9a832759f606276f42fee
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 15:25:57 +0000

    Bug 1263868 - Properly recover from OOM in ShellAutoEntryMonitor. r=jonco
    UltraBlame original commit: 30fcbf36d0eb7f5a6b842b8a44751e46cb787007

--- js/src/shell/js.cpp
+++ js/src/shell/js.cpp
@@ -4982,7 +4982,13 @@ class ShellAutoEntryMonitor : JS::dbg::AutoEntryMonitor {
         RootedString displayId(cx, JS_GetFunctionDisplayId(function));
         if (displayId) {
             UniqueChars displayIdStr(JS_EncodeStringToUTF8(cx, displayId));
-            oom = !displayIdStr || !log.append(mozilla::Move(displayIdStr));
+            if (!displayIdStr) {
+                
+                cx->recoverFromOutOfMemory();
+                oom = true;
+                return;
+            }
+            oom = !log.append(mozilla::Move(displayIdStr));
             return;
         }
 
