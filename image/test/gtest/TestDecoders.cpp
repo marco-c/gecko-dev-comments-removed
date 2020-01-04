cd1c46c8352b13ee6c9b7b6c50ed497aa9ffe6cb
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 17:25:17 +0000

    Bug 1207830 (Part 3) - Add a test that downscaling ICOs with AND mask transparency doesn't cause disaster. r=njn
    UltraBlame original commit: 7856fc2284735fbca3b33f76eb045c9ad3e8b8c9

--- image/test/gtest/TestDecoders.cpp
+++ image/test/gtest/TestDecoders.cpp
@@ -90,6 +90,10 @@ CheckDecoderResults(const ImageTestCase& aTestCase, Decoder* aDecoder)
     return;
   }
 
+  if (aTestCase.mFlags & TEST_CASE_IGNORE_OUTPUT) {
+    return;
+  }
+
   
   EXPECT_TRUE(IsSolidColor(surface, BGRAColor::Green(),
                            aTestCase.mFlags & TEST_CASE_IS_FUZZY ? 1 : 0));
@@ -206,6 +210,10 @@ CheckDownscaleDuringDecode(const ImageTestCase& aTestCase)
     
     EXPECT_TRUE(surface != nullptr);
 
+    if (aTestCase.mFlags & TEST_CASE_IGNORE_OUTPUT) {
+      return;
+    }
+
     
     
     
@@ -290,6 +298,11 @@ TEST(ImageDecoders, ICOMultiChunk)
 TEST(ImageDecoders, ICODownscaleDuringDecode)
 {
   CheckDownscaleDuringDecode(DownscaledICOTestCase());
+}
+
+TEST(ImageDecoders, ICOWithANDMaskDownscaleDuringDecode)
+{
+  CheckDownscaleDuringDecode(DownscaledTransparentICOWithANDMaskTestCase());
 }
 
 TEST(ImageDecoders, IconSingleChunk)
