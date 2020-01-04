56a19747a5ba43c51aeb3ee19c52929f80106ab7
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 07:30:16 +0000

    Bug 1022464 - Returning to URL bar containing search terms doesn't re-enter search mode. r=mcomella
    UltraBlame original commit: 69197595f2838a89abb1e969801242797737a038

--- mobile/android/base/java/org/mozilla/gecko/BrowserApp.java
+++ mobile/android/base/java/org/mozilla/gecko/BrowserApp.java
@@ -2170,13 +2170,21 @@ public class BrowserApp extends GeckoApp
 
         final Tab tab = Tabs.getInstance().getSelectedTab();
         if (tab != null) {
-            final String userRequested = tab.getUserRequested();
+            final String userSearchTerm = tab.getUserRequested();
 
             
             
-            url = (TextUtils.isEmpty(userRequested) ? tab.getURL() : userRequested);
+            String telemetryMsg;
+            if (!TextUtils.isEmpty(userSearchTerm)) {
+                url = userSearchTerm;
+                telemetryMsg = "urlbar-userEntered";
+            } else {
+                url = tab.getURL();
+                telemetryMsg = url.isEmpty() ? "urlbar-empty" : "urlbar-url";
+            }
+
+            Telemetry.sendUIEvent(TelemetryContract.Event.SEARCH, TelemetryContract.Method.ACTIONBAR, telemetryMsg);
         }
-
         enterEditingMode(url);
     }
 
@@ -2210,7 +2218,13 @@ public class BrowserApp extends GeckoApp
 
         mBrowserToolbar.startEditing(url, animator);
 
-        showHomePagerWithAnimator(panelId, animator);
+        final boolean isUserSearchTerm = !TextUtils.isEmpty(selectedTab.getUserRequested());
+        if (isUserSearchTerm && AppConstants.NIGHTLY_BUILD) {
+            showBrowserSearchAfterAnimation(animator);
+            hideWebContentOnPropertyAnimationEnd(animator);
+        } else {
+            showHomePagerWithAnimator(panelId, animator);
+        }
 
         animator.start();
         Telemetry.startUISession(TelemetryContract.Session.AWESOMESCREEN);
@@ -2622,6 +2636,24 @@ public class BrowserApp extends GeckoApp
         refreshToolbarHeight();
     }
 
+    private void showBrowserSearchAfterAnimation(PropertyAnimator animator) {
+        if (animator == null) {
+            showBrowserSearch();
+            return;
+        }
+
+        animator.addPropertyAnimationListener(new PropertyAnimator.PropertyAnimationListener() {
+            @Override
+            public void onPropertyAnimationStart() {
+            }
+
+            @Override
+            public void onPropertyAnimationEnd() {
+                showBrowserSearch();
+            }
+        });
+    }
+
     private void showBrowserSearch() {
         if (mBrowserSearch.getUserVisibleHint()) {
             return;
@@ -2672,7 +2704,10 @@ public class BrowserApp extends GeckoApp
 
         
         
-        mHomePagerContainer.setVisibility(View.VISIBLE);
+        showHomePager(Tabs.getInstance().getSelectedTab().getMostRecentHomePanel());
+
+        
+        mHideWebContentOnAnimationEnd = false;
 
         mBrowserSearchContainer.setVisibility(View.INVISIBLE);
 
