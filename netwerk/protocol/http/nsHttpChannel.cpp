108bf34ccfa7fe72085b330aeaf54a3e4ea162cc
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 22:08:28 +0000

    Bug 1293765 - Don't release upload stream in HTTP channel until authentication loop is done. r=valentin
    UltraBlame original commit: 8cc4907776c85b5f57c1778bdd5b2a4ad8eb8055

--- netwerk/protocol/http/nsHttpChannel.cpp
+++ netwerk/protocol/http/nsHttpChannel.cpp
@@ -6346,7 +6346,14 @@ nsHttpChannel::OnStopRequest(nsIRequest *request, nsISupports *ctxt, nsresult st
     MOZ_ASSERT(NS_IsMainThread(),
                "OnStopRequest should only be called from the main thread");
 
-    mUploadStream = nullptr;
+    if (!mAuthRetryPending) {
+        
+        
+        
+        
+        LOG(("  dropping upload stream"));
+        mUploadStream = nullptr;
+    }
 
     if (NS_FAILED(status)) {
         ProcessSecurityReport(status);
