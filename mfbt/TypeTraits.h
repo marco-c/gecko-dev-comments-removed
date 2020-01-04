328b66939927784ce4200fddb8b281275edd7fa1
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 09:01:18 +0000

    Bug 1241901 part 3 - Add IsMemberPointer and IsScalar type traits. r=froydnj
    UltraBlame original commit: 30ded57e0902f7767c8bcd3975b78cf22b08437e

--- mfbt/TypeTraits.h
+++ mfbt/TypeTraits.h
@@ -355,6 +355,41 @@ struct IsArithmetic
   : IntegralConstant<bool, IsIntegral<T>::value || IsFloatingPoint<T>::value>
 {};
 
+namespace detail {
+
+template<typename T>
+struct IsMemberPointerHelper : FalseType {};
+
+template<typename T, typename U>
+struct IsMemberPointerHelper<T U::*> : TrueType {};
+
+} 
+
+
+
+
+
+
+
+
+template<typename T>
+struct IsMemberPointer
+  : detail::IsMemberPointerHelper<typename RemoveCV<T>::Type>
+{};
+
+
+
+
+
+
+
+
+template<typename T>
+struct IsScalar
+  : IntegralConstant<bool, IsArithmetic<T>::value || IsEnum<T>::value ||
+                     IsPointer<T>::value || IsMemberPointer<T>::value>
+{};
+
 
 
 
