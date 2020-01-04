4254391e46ece6a354b51dd1e49b83efc86bc3d0
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 23:13:36 +0000

    Bug 1288745 - Remove TelemetryHistogram::NewHistogram and replace with predefined histograms. r=chutten
    MozReview-Commit-ID: 6v270MnquqT
    
    UltraBlame original commit: d28195b44f260dc1cf6c787516260f33b46763fb

--- toolkit/components/telemetry/tests/unit/test_TelemetryStopwatch.js
+++ toolkit/components/telemetry/tests/unit/test_TelemetryStopwatch.js
@@ -5,8 +5,6 @@ var tmpScope = {};
 Cu.import("resource://gre/modules/TelemetryStopwatch.jsm", tmpScope);
 var TelemetryStopwatch = tmpScope.TelemetryStopwatch;
 
-
-
 const HIST_NAME = "TELEMETRY_PING";
 const HIST_NAME2 = "RANGE_CHECKSUM_ERRORS";
 const KEYED_HIST = { id: "TELEMETRY_INVALID_PING_TYPE_SUBMITTED", key: "TEST" };
