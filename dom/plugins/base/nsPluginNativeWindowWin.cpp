ad7570bbd8f8a16ca58e60d9ff4f909d6d54f998
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 00:21:59 +0000

    Bug 895274 part.18 Rename NS_PLUGIN_ACTIVATE to ePluginActivate r=smaug
    UltraBlame original commit: da3eb3291c32042466fcc13813d85b6b758413d7

--- dom/plugins/base/nsPluginNativeWindowWin.cpp
+++ dom/plugins/base/nsPluginNativeWindowWin.cpp
@@ -271,7 +271,7 @@ static LRESULT CALLBACK PluginWndProcInternal(HWND hWnd, UINT msg, WPARAM wParam
         nsCOMPtr<nsIWidget> widget;
         win->GetPluginWidget(getter_AddRefs(widget));
         if (widget) {
-          WidgetGUIEvent event(true, NS_PLUGIN_ACTIVATE, widget);
+          WidgetGUIEvent event(true, ePluginActivate, widget);
           nsEventStatus status;
           widget->DispatchEvent(&event, status);
         }
--- widget/EventMessageList.h
+++ widget/EventMessageList.h
@@ -35,7 +35,7 @@ NS_EVENT_MESSAGE(eScroll,               eWindowEventFirst + 61)
 
 
 
-NS_EVENT_MESSAGE(NS_PLUGIN_ACTIVATE,    eWindowEventFirst + 62)
+NS_EVENT_MESSAGE(ePluginActivate,       eWindowEventFirst + 62)
 NS_EVENT_MESSAGE(NS_PLUGIN_FOCUS,       eWindowEventFirst + 63)
 
 NS_EVENT_MESSAGE(NS_OFFLINE,            eWindowEventFirst + 64)
