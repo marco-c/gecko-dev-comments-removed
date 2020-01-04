c6d9baed601087eb6be4a5961b0de96931eb82cd
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 15:11:52 +0000

    Bug 1255040 - Add webidl for install/uninstall via AddonManager. r=bz
    MozReview-Commit-ID: 9eLMPbxostQ
    
    UltraBlame original commit: 71e2c45790dc01e3cc7ffd00f35bca771da21b84

--- toolkit/mozapps/extensions/amWebAPI.js
+++ toolkit/mozapps/extensions/amWebAPI.js
@@ -55,11 +55,16 @@ const APIBroker = {
 
 APIBroker.init();
 
-function Addon(properties) {
+function Addon(win, properties) {
   
   for (let key of Object.keys(properties)) {
     this[key] = properties[key];
   }
+
+  this.uninstall = function() {
+    let err = new win.Error("not yet implemented");
+    return win.Promise.reject(err);
+  };
 }
 
 
@@ -67,9 +72,9 @@ function Addon(properties) {
 
 
 function WebAPITask(generator) {
-  let task = Task.async(generator);
-
   return function(...args) {
+    let task = Task.async(generator.bind(this));
+
     let win = this.window;
 
     let wrapForContent = (obj) => {
@@ -97,8 +102,13 @@ WebAPI.prototype = {
 
   getAddonByID: WebAPITask(function*(id) {
     let addonInfo = yield APIBroker.sendRequest("getAddonByID", id);
-    return addonInfo ? new Addon(addonInfo) : null;
+    return addonInfo ? new Addon(this.window, addonInfo) : null;
   }),
+
+  createInstall() {
+    let err = new this.window.Error("not yet implemented");
+    return this.window.Promise.reject(err);
+  },
 
   classID: Components.ID("{8866d8e3-4ea5-48b7-a891-13ba0ac15235}"),
   contractID: "@mozilla.org/addon-web-api/manager;1",
