cb2fc949fbf98d0b137f6a6b389a9599519708ac
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 07:48:09 +0000

    Bug 1236802 (part 2) - Remove a dead CurrentSurface() call. r=mattwoodrow.
    UltraBlame original commit: e3629c66d4fa15703e846acef94e522c032ec7e2

--- gfx/layers/basic/BasicLayerManager.cpp
+++ gfx/layers/basic/BasicLayerManager.cpp
@@ -862,7 +862,6 @@ Transform3D(RefPtr<SourceSurface> aSource,
   aDestRect.RoundOut();
 
   
-  RefPtr<gfxASurface> dest = aDest->CurrentSurface();
   RefPtr<gfxImageSurface> destImage = new gfxImageSurface(IntSize(aDestRect.width,
                                                                     aDestRect.height),
                                                             SurfaceFormat::A8R8G8B8_UINT32);
