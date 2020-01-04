adbc89a9ca749ebdcef07cbaa7d04b5c746aab93
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 21:05:31 +0000

    Bug 1290217: Ensure input is removed after test before next one starts in browser_treeupdate_doc.js r=yzen
    MozReview-Commit-ID: DhLarH20Zr6
    
    UltraBlame original commit: f0e0cd618dde2988152a98cd4a285b0ae4afd964

--- accessible/tests/browser/browser_treeupdate_doc.js
+++ accessible/tests/browser/browser_treeupdate_doc.js
@@ -230,11 +230,20 @@ addAccessibleTask(`
   };
   testAccessibleTree(iframe, tree);
 
+  reorderEventPromise = waitForEvent(EVENT_REORDER, iframe);
   yield ContentTask.spawn(browser, {}, () => {
-    let docNode = content.document.getElementById('iframe').contentDocument;
+    let docEl =
+      content.document.getElementById('iframe').contentDocument.documentElement;
     
-    docNode.documentElement.removeChild(content.window.inputNode);
+    docEl.removeChild(docEl.firstChild);
   });
+  
+  yield reorderEventPromise;
+  tree = {
+    role: ROLE_DOCUMENT,
+    children: [ ]
+  };
+  testAccessibleTree(iframe, tree);
 
   
   reorderEventPromise = waitForEvent(EVENT_REORDER, id);
