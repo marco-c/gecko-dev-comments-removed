0d2d8af2648b3e8fb8cc47612d58f12e12e86702
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 09:30:35 +0000

    Bug 1243929 - Only show jit optimizations panel when pref is on and current selected frame has optimizations. r=vp
    UltraBlame original commit: 912f7aa4d70f46d8c1c210d3a14547289b1df05e

--- devtools/client/performance/test/browser_perf-options-show-jit-optimizations.js
+++ devtools/client/performance/test/browser_perf-options-show-jit-optimizations.js
@@ -2,35 +2,257 @@
 
 
 
+requestLongerTimeout(2);
 
+
+
+
+
+
+Services.prefs.setBoolPref(INVERT_PREF, false);
 
 function* spawnTest() {
   let { panel } = yield initPerformance(SIMPLE_URL);
-  let { EVENTS, $, DetailsView, JsCallTreeView } = panel.panelWin;
+  let { EVENTS, $, $$, window, PerformanceController } = panel.panelWin;
+  let { OverviewView, DetailsView, OptimizationsListView, JsCallTreeView, RecordingsView } = panel.panelWin;
 
+  let profilerData = { threads: [gThread] }
+
+  is(Services.prefs.getBoolPref(JIT_PREF), false, "record JIT Optimizations pref off by default");
   Services.prefs.setBoolPref(JIT_PREF, true);
+  is(Services.prefs.getBoolPref(JIT_PREF), true, "toggle on record JIT Optimizations");
+
+  
+  
+  yield startRecording(panel);
+  yield stopRecording(panel);
 
   yield startRecording(panel);
   yield stopRecording(panel);
 
-  let rendered = once(JsCallTreeView, EVENTS.JS_CALL_TREE_RENDERED);
   yield DetailsView.selectView("js-calltree");
+
+  yield injectAndRenderProfilerData();
+
+  is($("#jit-optimizations-view").classList.contains("hidden"), true,
+    "JIT Optimizations should be hidden when pref is on, but no frame selected");
+
+  
+  yield checkFrame(1);
+
+  
+  
+  yield checkFrame(2, true);
+
+  
+  yield checkFrame(3);
+
+  
+  
+  let rendered = once(JsCallTreeView, "focus");
+  mousedown(window, $$(".call-tree-item")[2]);
+  yield rendered;
+  let isHidden = $("#jit-optimizations-view").classList.contains("hidden");
+  ok(!isHidden, "opts view should be visible when selecting a frame with opts");
+
+  let select = once(PerformanceController, EVENTS.RECORDING_SELECTED);
+  rendered = once(JsCallTreeView, EVENTS.JS_CALL_TREE_RENDERED);
+  RecordingsView.selectedIndex = 0;
+  yield Promise.all([select, rendered]);
+
+  isHidden = $("#jit-optimizations-view").classList.contains("hidden");
+  ok(isHidden, "opts view is hidden when switching recordings");
+
+  rendered = once(JsCallTreeView, EVENTS.JS_CALL_TREE_RENDERED);
+  RecordingsView.selectedIndex = 1;
   yield rendered;
 
-  ok(!$("#jit-optimizations-view").classList.contains("hidden"), "JIT Optimizations shown");
+  rendered = once(JsCallTreeView, "focus");
+  mousedown(window, $$(".call-tree-item")[2]);
+  yield rendered;
+  isHidden = $("#jit-optimizations-view").classList.contains("hidden");
+  ok(!isHidden, "opts view should be visible when selecting a frame with opts");
 
   rendered = once(JsCallTreeView, EVENTS.JS_CALL_TREE_RENDERED);
   Services.prefs.setBoolPref(JIT_PREF, false);
   yield rendered;
-  ok(true, "JsCallTreeView rerendered when toggling off show-jit-optimizations.");
-  ok($("#jit-optimizations-view").classList.contains("hidden"), "JIT Optimizations hidden");
+  ok(true, "call tree rerendered when JIT pref changes");
+  isHidden = $("#jit-optimizations-view").classList.contains("hidden");
+  ok(isHidden, "opts view hidden when toggling off jit pref");
 
-  rendered = once(JsCallTreeView, EVENTS.JS_CALL_TREE_RENDERED);
-  Services.prefs.setBoolPref(JIT_PREF, true);
+  rendered = once(JsCallTreeView, "focus");
+  mousedown(window, $$(".call-tree-item")[2]);
   yield rendered;
-  ok(true, "JsCallTreeView rerendered when toggling off show-jit-optimizations.");
-  ok(!$("#jit-optimizations-view").classList.contains("hidden"), "JIT Optimizations shown");
+  isHidden = $("#jit-optimizations-view").classList.contains("hidden");
+  ok(isHidden, "opts view hidden when jit pref off and selecting a frame with opts");
 
   yield teardown(panel);
   finish();
+
+  function *injectAndRenderProfilerData() {
+    
+    info("Injecting mock profile data");
+    let recording = PerformanceController.getCurrentRecording();
+    recording._profile = profilerData;
+
+    
+    let rendered = once(JsCallTreeView, EVENTS.JS_CALL_TREE_RENDERED);
+    JsCallTreeView.render(OverviewView.getTimeInterval());
+    yield rendered;
+  }
+
+  function *checkFrame (frameIndex, hasOpts) {
+    info(`Checking frame ${frameIndex}`);
+    
+    let rendered = once(JsCallTreeView, "focus");
+    mousedown(window, $$(".call-tree-item")[frameIndex]);
+    yield rendered;
+
+    let isHidden = $("#jit-optimizations-view").classList.contains("hidden");
+    if (hasOpts) {
+      ok(!isHidden, "JIT Optimizations view is not hidden if current frame has opts.");
+    } else {
+      ok(isHidden, "JIT Optimizations view is hidden if current frame does not have opts");
+    }
+  }
 }
+
+var gUniqueStacks = new RecordingUtils.UniqueStacks();
+
+function uniqStr(s) {
+  return gUniqueStacks.getOrAddStringIndex(s);
+}
+
+
+
+
+var gThread = RecordingUtils.deflateThread({
+  samples: [{
+    time: 0,
+    frames: [
+      { location: "(root)" }
+    ]
+  }, {
+    time: 5,
+    frames: [
+      { location: "(root)" },
+      { location: "A_O1" },
+      { location: "B_O2" },
+      { location: "C (http://foo/bar/baz:56)" }
+    ]
+  }, {
+    time: 5 + 1,
+    frames: [
+      { location: "(root)" },
+      { location: "A (http://foo/bar/baz:12)" },
+      { location: "B_O2" },
+    ]
+  }, {
+    time: 5 + 1 + 2,
+    frames: [
+      { location: "(root)" },
+      { location: "A_O1" },
+      { location: "B_O3" },
+    ]
+  }, {
+    time: 5 + 1 + 2 + 7,
+    frames: [
+      { location: "(root)" },
+      { location: "A_O1" },
+      { location: "E (http://foo/bar/baz:90)" },
+      { location: "F (http://foo/bar/baz:99)" }
+    ]
+  }],
+  markers: []
+}, gUniqueStacks);
+
+
+var gRawSite1 = {
+  _testFrameInfo: { name: "A", line: "12", file: "@baz" },
+  line: 12,
+  column: 2,
+  types: [{
+    mirType: uniqStr("Object"),
+    site: uniqStr("A (http://foo/bar/bar:12)"),
+    typeset: [{
+        keyedBy: uniqStr("constructor"),
+        name: uniqStr("Foo"),
+        location: uniqStr("A (http://foo/bar/baz:12)")
+    }, {
+        keyedBy: uniqStr("primitive"),
+        location: uniqStr("self-hosted")
+    }]
+  }],
+  attempts: {
+    schema: {
+      outcome: 0,
+      strategy: 1
+    },
+    data: [
+      [uniqStr("Failure1"), uniqStr("SomeGetter1")],
+      [uniqStr("Failure2"), uniqStr("SomeGetter2")],
+      [uniqStr("Failure3"), uniqStr("SomeGetter3")]
+    ]
+  }
+};
+
+var gRawSite2 = {
+  _testFrameInfo: { name: "B", line: "10", file: "@boo" },
+  line: 40,
+  types: [{
+    mirType: uniqStr("Int32"),
+    site: uniqStr("Receiver")
+  }],
+  attempts: {
+    schema: {
+      outcome: 0,
+      strategy: 1
+    },
+    data: [
+      [uniqStr("Failure1"), uniqStr("SomeGetter1")],
+      [uniqStr("Failure2"), uniqStr("SomeGetter2")],
+      [uniqStr("Inlined"), uniqStr("SomeGetter3")]
+    ]
+  }
+};
+
+var gRawSite3 = {
+  _testFrameInfo: { name: "B", line: "10", file: "@boo" },
+  line: 34,
+  types: [{
+    mirType: uniqStr("Int32"),
+    site: uniqStr("Receiver")
+  }],
+  attempts: {
+    schema: {
+      outcome: 0,
+      strategy: 1
+    },
+    data: [
+      [uniqStr("Failure1"), uniqStr("SomeGetter1")],
+      [uniqStr("Failure2"), uniqStr("SomeGetter2")],
+      [uniqStr("Failure3"), uniqStr("SomeGetter3")]
+    ]
+  }
+};
+
+gThread.frameTable.data.forEach((frame) => {
+  const LOCATION_SLOT = gThread.frameTable.schema.location;
+  const OPTIMIZATIONS_SLOT = gThread.frameTable.schema.optimizations;
+
+  let l = gThread.stringTable[frame[LOCATION_SLOT]];
+  switch (l) {
+  case "A_O1":
+    frame[LOCATION_SLOT] = uniqStr("A (http://foo/bar/baz:12)");
+    frame[OPTIMIZATIONS_SLOT] = gRawSite1;
+    break;
+  case "B_O2":
+    frame[LOCATION_SLOT] = uniqStr("B (http://foo/bar/boo:10)");
+    frame[OPTIMIZATIONS_SLOT] = gRawSite2;
+    break;
+  case "B_O3":
+    frame[LOCATION_SLOT] = uniqStr("B (http://foo/bar/boo:10)");
+    frame[OPTIMIZATIONS_SLOT] = gRawSite3;
+    break;
+  }
+});
