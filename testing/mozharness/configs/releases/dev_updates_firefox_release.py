72533cd914d5eff2a9a44295973a298d7b6bc2d4
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 21:03:37 +0000

    Bug 1291562 - Updates builder failed in Firefox 49.0b1 r=nthomas a=release DONTBUILD
    MozReview-Commit-ID: BUwJk8tnbto
    
    UltraBlame original commit: 06b3a3e421e05e4a76a83d9ae5287dba1d905c6e

--- testing/mozharness/configs/releases/dev_updates_firefox_release.py
+++ testing/mozharness/configs/releases/dev_updates_firefox_release.py
@@ -8,6 +8,7 @@ config = {
         "dest": "tools",
         "vcs": "hg",
     },
+    "vcs_share_base": "/builds/hg-shared",
     
     "push_dest": "ssh://hg.mozilla.org/users/raliiev_mozilla.com/tools",
     
