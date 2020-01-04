f4ce53351e74eacf1894ee808430fa0a327aceeb
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 00:05:45 +0000

    Bug 1123237 - Part 2. MemoryProfiler hooks in js engine. r=terrence
    Based on patch from Ting-Yuan Huang <laszio.bugzillagmail.com>
    
    UltraBlame original commit: a80502c372f10231bbe3edc230d01ad0e961328e

--- js/src/gc/GCRuntime.h
+++ js/src/gc/GCRuntime.h
@@ -9,6 +9,7 @@
 
 #include "mozilla/Atomics.h"
 
+#include "jsfriendapi.h"
 #include "jsgc.h"
 
 #include "gc/Heap.h"
@@ -985,6 +986,8 @@ class GCRuntime
     
     GCSchedulingTunables tunables;
     GCSchedulingState schedulingState;
+
+    MemProfiler mMemProfiler;
 
   private:
     
