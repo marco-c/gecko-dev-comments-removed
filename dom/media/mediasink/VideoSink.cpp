b577759e139aeee2268e5ea50d093b7e362a948a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 22:41:14 +0000

    Bug 1298594: P4. Pop the frame when current time is past the end of the current frame. r=jwwang
    Most frames start when the previous one stop.
    
    MozReview-Commit-ID: H92Bmiki49Q
    
    UltraBlame original commit: 2cdde44ebcc788af1a875217dc3022b1812d6538

--- dom/media/mediasink/VideoSink.cpp
+++ dom/media/mediasink/VideoSink.cpp
@@ -399,7 +399,7 @@ VideoSink::UpdateRenderedVideoFrames()
   
   int64_t lastDisplayedFrameEndTime = 0;
   while (VideoQueue().GetSize() > mMinVideoQueueSize &&
-         clockTime > VideoQueue().PeekFront()->GetEndTime()) {
+         clockTime >= VideoQueue().PeekFront()->GetEndTime()) {
     RefPtr<MediaData> frame = VideoQueue().PopFront();
     if (frame->As<VideoData>()->mSentToCompositor) {
       lastDisplayedFrameEndTime = frame->GetEndTime();
