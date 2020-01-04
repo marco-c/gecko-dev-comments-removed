c3da59e7220d0979d130de057e214864138346da
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 14:16:16 +0000

    Backed out changeset dcffecae84b7 (bug 1258464)
    UltraBlame original commit: d202b268a22881704adae85f42c4473e1b82259c

--- mobile/android/base/java/org/mozilla/gecko/GeckoView.java
+++ mobile/android/base/java/org/mozilla/gecko/GeckoView.java
@@ -16,16 +16,21 @@ import org.mozilla.gecko.annotation.ReflectionTarget;
 import org.mozilla.gecko.annotation.WrapForJNI;
 import org.mozilla.gecko.gfx.GLController;
 import org.mozilla.gecko.gfx.LayerView;
+import org.mozilla.gecko.mozglue.GeckoLoader;
 import org.mozilla.gecko.mozglue.JNIObject;
+import org.mozilla.gecko.util.Clipboard;
 import org.mozilla.gecko.util.EventCallback;
 import org.mozilla.gecko.util.GeckoEventListener;
+import org.mozilla.gecko.util.HardwareUtils;
 import org.mozilla.gecko.util.NativeEventListener;
 import org.mozilla.gecko.util.NativeJSObject;
 import org.mozilla.gecko.util.ThreadUtils;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.TypedArray;
 import android.os.Binder;
 import android.os.Bundle;
 import android.os.Handler;
@@ -175,15 +180,20 @@ public class GeckoView extends LayerView
 
     public GeckoView(Context context) {
         super(context);
-        init(context);
+        init(context, null, true);
     }
 
     public GeckoView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        init(context);
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.GeckoView);
+        String url = a.getString(R.styleable.GeckoView_url);
+        boolean doInit = a.getBoolean(R.styleable.GeckoView_doinit, true);
+        a.recycle();
+        init(context, url, doInit);
     }
 
-    private void init(Context context) {
+    private void init(Context context, String url, boolean doInit) {
+
         if (GeckoAppShell.getApplicationContext() == null) {
             GeckoAppShell.setApplicationContext(context.getApplicationContext());
         }
@@ -199,6 +209,64 @@ public class GeckoView extends LayerView
         GeckoAppShell.setLayerView(this);
 
         initializeView(EventDispatcher.getInstance());
+
+        
+        
+        
+        
+        if (!doInit)
+            return;
+
+        
+        
+        boolean isGeckoActivity = false;
+        try {
+            isGeckoActivity = context instanceof GeckoActivity;
+        } catch (NoClassDefFoundError ex) {}
+
+        if (!isGeckoActivity) {
+            Clipboard.init(context);
+            HardwareUtils.init(context);
+
+            
+
+            final GeckoProfile profile = GeckoProfile.get(context);
+         }
+
+        GeckoThread.ensureInit(null, null);
+        if (url != null) {
+            GeckoAppShell.sendEventToGecko(GeckoEvent.createURILoadEvent(url));
+        }
+
+        if (context instanceof Activity) {
+            Tabs tabs = Tabs.getInstance();
+            tabs.attachToContext(context);
+        }
+
+        EventDispatcher.getInstance().registerGeckoThreadListener(mGeckoEventListener,
+            "Gecko:Ready",
+            "Accessibility:Event",
+            "Content:StateChange",
+            "Content:LoadError",
+            "Content:PageShow",
+            "DOMTitleChanged",
+            "Link:Favicon",
+            "Prompt:Show",
+            "Prompt:ShowTop");
+
+        EventDispatcher.getInstance().registerGeckoThreadListener(mNativeEventListener,
+            "Accessibility:Ready",
+            "GeckoView:Message");
+
+        if (GeckoThread.launch()) {
+            
+            GeckoProfile profile = GeckoProfile.get(context).forceCreate();
+
+        } else if (GeckoThread.isRunning()) {
+            
+            
+            connectToGecko();
+        }
     }
 
     @Override
