7af569d696b5408381dd9ed82ca27f92acdb577d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 05:52:54 +0000

    Bug 1228215 - Add a 'gmpName' parameter to GMPService::GetNodeId(), so each GMP can see a different nodeId for the same origin. r=jwwang
    UltraBlame original commit: eabac8a5315b3d319089cfbce6cf09f18ce8f02c

--- dom/media/eme/EMEUtils.h
+++ dom/media/eme/EMEUtils.h
@@ -104,6 +104,9 @@ struct ArrayData {
 ArrayData
 GetArrayBufferViewOrArrayBufferData(const dom::ArrayBufferViewOrArrayBuffer& aBufferOrView);
 
+nsString
+KeySystemToGMPName(const nsAString& aKeySystem);
+
 } 
 
 #endif 
