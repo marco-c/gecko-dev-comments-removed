264f55fac9476e34d1e3bedbaa787a8cf39a6934
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 16:24:25 +0000

    Bug 1271977 - bit shifting, bit counting. r=bbouvier
    UltraBlame original commit: ccf7dc442e1fea2b7ae1c1f28a6c75cbd54a3102

--- js/src/jit/MacroAssembler.h
+++ js/src/jit/MacroAssembler.h
@@ -795,11 +795,26 @@ class MacroAssembler : public MacroAssemblerSpecific
     inline void rshift64(Imm32 imm, Register64 dest) PER_ARCH;
 
     
+    inline void lshift32(Register shift, Register srcDest) DEFINED_ON(x86_shared);
+    inline void rshift32(Register shift, Register srcDest) DEFINED_ON(x86_shared);
+    inline void rshift32Arithmetic(Register shift, Register srcDest) DEFINED_ON(x86_shared);
+
+    
     
     inline void rotateLeft(Imm32 count, Register input, Register dest) PER_SHARED_ARCH;
     inline void rotateLeft(Register count, Register input, Register dest) PER_SHARED_ARCH;
     inline void rotateRight(Imm32 count, Register input, Register dest) PER_SHARED_ARCH;
     inline void rotateRight(Register count, Register input, Register dest) PER_SHARED_ARCH;
+
+    
+    
+
+    
+    inline void clz32(Register src, Register dest, bool knownNotZero) DEFINED_ON(x86_shared);
+    inline void ctz32(Register src, Register dest, bool knownNotZero) DEFINED_ON(x86_shared);
+
+    
+    inline void popcnt32(Register src, Register dest, Register temp) DEFINED_ON(x86_shared);
 
     
     
