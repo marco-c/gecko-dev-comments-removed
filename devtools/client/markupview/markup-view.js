c6ea956fc4a9008d601fef91b0ba7abd1d1df50a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 02:03:50 +0000

    Bug 1034110 - Listen to nativeAnonymousChildList changes from devtools;r=pbrosset
    UltraBlame original commit: 7b145ea2fbdefc0c32d0bc7375ed030326530bfd

--- devtools/client/markupview/markup-view.js
+++ devtools/client/markupview/markup-view.js
@@ -830,7 +830,7 @@ MarkupView.prototype = {
         if (type === "attributes" && container.selected) {
           requiresLayoutChange = true;
         }
-      } else if (type === "childList") {
+      } else if (type === "childList" || "nativeAnonymousChildList") {
         container.childrenDirty = true;
         
         this._updateChildren(container, {flash: true});
