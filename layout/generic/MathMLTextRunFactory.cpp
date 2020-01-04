695a048e50e26b0253ffc09e1524e8c7e3780f38
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 10:59:17 +0000

    Bug 1251995 part 2 - Add gfxTextRun::Range to replace parameter pairs like (offset, length) and (start, end). r=jfkthame
    Although this makes some places more complicated, code should generally
    be simpler and clearer. It could slightly improve performance on x64 for
    functions with more than four parameters, since it makes more data be
    passed via registers rather than the stack.
    
    MozReview-Commit-ID: D0GM2Jyrr6W
    
    UltraBlame original commit: 0d462b564421dc46a14c0d2b538413cd623bd37e

--- layout/generic/MathMLTextRunFactory.cpp
+++ layout/generic/MathMLTextRunFactory.cpp
@@ -773,12 +773,15 @@ MathMLTextRunFactory::RebuildTextRun(nsTransformedTextRun* aTextRun,
   }
   if (!child)
     return;
+
+  typedef gfxTextRun::Range Range;
+
   
   
   NS_ASSERTION(convertedString.Length() == canBreakBeforeArray.Length(),
                "Dropped characters or break-before values somewhere!");
-  child->SetPotentialLineBreaks(0, canBreakBeforeArray.Length(),
-                                canBreakBeforeArray.Elements());
+  Range range(0, uint32_t(canBreakBeforeArray.Length()));
+  child->SetPotentialLineBreaks(range, canBreakBeforeArray.Elements());
   if (transformedChild) {
     transformedChild->FinishSettingProperties(aRefDrawTarget, aMFR);
   }
@@ -796,6 +799,6 @@ MathMLTextRunFactory::RebuildTextRun(nsTransformedTextRun* aTextRun,
     
     
     aTextRun->ResetGlyphRuns();
-    aTextRun->CopyGlyphDataFrom(child, 0, child->GetLength(), 0);
+    aTextRun->CopyGlyphDataFrom(child, Range(child), 0);
   }
 }
