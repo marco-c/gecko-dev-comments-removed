35733a5224edde7bf41f2c0b04af28d4db986e31
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 09:05:33 +0000

    Bug 1242763 - Add a bunch of tooltips to the devtools' memory panel; r=jsantell
    UltraBlame original commit: f8344dfd2d252f29a3f360c9ff2afe1dd22bb81b

--- devtools/client/memory/utils.js
+++ devtools/client/memory/utils.js
@@ -8,7 +8,6 @@ Cu.import("resource://devtools/client/shared/widgets/ViewHelpers.jsm");
 const STRINGS_URI = "chrome://devtools/locale/memory.properties"
 const L10N = exports.L10N = new ViewHelpers.L10N(STRINGS_URI);
 
-const { URL } = require("sdk/url");
 const { OS } = require("resource://gre/modules/osfile.jsm");
 const { assert } = require("devtools/shared/DevToolsUtils");
 const { Preferences } = require("resource://gre/modules/Preferences.jsm");
@@ -56,14 +55,14 @@ exports.getSnapshotTitle = function (snapshot) {
 
 
 exports.getBreakdownDisplayData = function () {
-  return exports.getBreakdownNames().map(name => {
+  return exports.getBreakdownNames().map(({ name, tooltip }) => {
     
     let preset = breakdowns[name];
     let displayName = name;
     if (preset && preset.displayName) {
       displayName = preset.displayName;
     }
-    return { name, displayName };
+    return { name, tooltip, displayName };
   });
 };
 
@@ -75,7 +74,12 @@ exports.getBreakdownDisplayData = function () {
 
 exports.getBreakdownNames = function () {
   let custom = exports.getCustomBreakdowns();
-  return Object.keys(Object.assign({}, breakdowns, custom));
+  return Object.keys(Object.assign({}, breakdowns, custom))
+    .map(key => {
+      return breakdowns[key]
+        ? { name: key, tooltip: breakdowns[key].tooltip }
+        : { name: key };
+    });
 };
 
 
@@ -127,14 +131,14 @@ exports.breakdownNameToSpec = function (name) {
 
 
 exports.getDominatorTreeBreakdownDisplayData = function () {
-  return exports.getDominatorTreeBreakdownNames().map(name => {
+  return exports.getDominatorTreeBreakdownNames().map(({ name, tooltip }) => {
     
     let preset = dominatorTreeBreakdowns[name];
     let displayName = name;
     if (preset && preset.displayName) {
       displayName = preset.displayName;
     }
-    return { name, displayName };
+    return { name, tooltip, displayName };
   });
 };
 
@@ -146,7 +150,12 @@ exports.getDominatorTreeBreakdownDisplayData = function () {
 
 exports.getDominatorTreeBreakdownNames = function () {
   let custom = exports.getCustomDominatorTreeBreakdowns();
-  return Object.keys(Object.assign({}, dominatorTreeBreakdowns, custom));
+  return Object.keys(Object.assign({}, dominatorTreeBreakdowns, custom))
+    .map(key => {
+      return dominatorTreeBreakdowns[key]
+        ? { name: key, tooltip: dominatorTreeBreakdowns[key].tooltip }
+        : { name: key };
+    });
 };
 
 
@@ -163,7 +172,7 @@ exports.getCustomDominatorTreeBreakdowns = function () {
       `String stored in "${CUSTOM_BREAKDOWN_PREF}" pref cannot be parsed by \`JSON.parse()\`.`);
   }
   return customBreakdowns;
-}
+};
 
 
 
