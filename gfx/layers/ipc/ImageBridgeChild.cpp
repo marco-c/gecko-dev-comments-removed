120e156688f9de127804dbe7edf8921feade837c
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 05:37:22 +0000

    Backed out changeset 0cbe4b80d443 (bug 1215265) for mass bustage CLOSED TREE
    UltraBlame original commit: 564f5491ccdd174f9ed93ff0790e328fddd857f3

--- gfx/layers/ipc/ImageBridgeChild.cpp
+++ gfx/layers/ipc/ImageBridgeChild.cpp
@@ -262,8 +262,6 @@ static void ConnectImageBridge(ImageBridgeChild * child, ImageBridgeParent * par
 ImageBridgeChild::ImageBridgeChild()
   : mShuttingDown(false)
 {
-  MOZ_COUNT_CTOR(ImageBridgeChild);
-
   MOZ_ASSERT(NS_IsMainThread());
 
   
@@ -276,8 +274,6 @@ ImageBridgeChild::~ImageBridgeChild()
   MOZ_ASSERT(NS_IsMainThread());
 
   delete mTxn;
-
-  MOZ_COUNT_DTOR(ImageBridgeChild);
 }
 
 void
