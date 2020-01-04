9a08d39460eef657ae70eed521fa40c37e18dada
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 00:16:04 +0000

    Bug 1119692 - Part 1: Get cached jar file fd if it exists instead of always openning it. r=smaug, r=jduell
    UltraBlame original commit: 64383ee12257de9b7e82d465243a6a8b98b06c31

--- netwerk/ipc/RemoteOpenFileChild.cpp
+++ netwerk/ipc/RemoteOpenFileChild.cpp
@@ -225,6 +225,8 @@ RemoteOpenFileChild::AsyncRemoteFileOpen(int32_t aFlags,
     MOZ_CRASH("Couldn't get path from file!");
   }
 
+  mListener = aListener;
+
   if (mTabChild) {
     if (mTabChild->GetCachedFileDescriptor(path, this)) {
       
@@ -244,7 +246,6 @@ RemoteOpenFileChild::AsyncRemoteFileOpen(int32_t aFlags,
   
   AddIPDLReference();
 
-  mListener = aListener;
   mAsyncOpenCalled = true;
   return NS_OK;
 #endif
