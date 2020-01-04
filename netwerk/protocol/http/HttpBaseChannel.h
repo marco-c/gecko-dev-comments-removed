7af7e7971da102d7464635f3d0274a96e84ed9b8
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 22:21:21 +0000

    Merge m-c to fx-team. a=merge
    UltraBlame original commit: 1b93a25d0fe22bd10bae50eb705a25616f84477c

--- netwerk/protocol/http/HttpBaseChannel.h
+++ netwerk/protocol/http/HttpBaseChannel.h
@@ -233,6 +233,7 @@ public:
   NS_IMETHOD GetTopWindowURI(nsIURI **aTopWindowURI) override;
   NS_IMETHOD GetProxyURI(nsIURI **proxyURI) override;
   virtual void SetCorsPreflightParameters(const nsTArray<nsCString>& unsafeHeaders) override;
+  NS_IMETHOD GetConnectionInfoHashKey(nsACString& aConnectionInfoHashKey) override;
 
   inline void CleanRedirectCacheChainIfNecessary()
   {
@@ -316,8 +317,6 @@ public:
     
     
     void EnsureUploadStreamIsCloneableComplete(nsresult aStatus);
-
-    bool HaveListenerForTraceableChannel() { return mHaveListenerForTraceableChannel; }
 
 protected:
   nsCOMArray<nsISecurityConsoleMessage> mSecurityConsoleMessages;
@@ -422,30 +421,29 @@ protected:
   int16_t                           mPriority;
   uint8_t                           mRedirectionLimit;
 
-  uint32_t                          mApplyConversion                 : 1;
-  uint32_t                          mHaveListenerForTraceableChannel : 1;
-  uint32_t                          mCanceled                        : 1;
-  uint32_t                          mIsPending                       : 1;
-  uint32_t                          mWasOpened                       : 1;
+  uint32_t                          mApplyConversion            : 1;
+  uint32_t                          mCanceled                   : 1;
+  uint32_t                          mIsPending                  : 1;
+  uint32_t                          mWasOpened                  : 1;
   
-  uint32_t                          mRequestObserversCalled          : 1;
-  uint32_t                          mResponseHeadersModified         : 1;
-  uint32_t                          mAllowPipelining                 : 1;
-  uint32_t                          mAllowSTS                        : 1;
-  uint32_t                          mThirdPartyFlags                 : 3;
-  uint32_t                          mUploadStreamHasHeaders          : 1;
-  uint32_t                          mInheritApplicationCache         : 1;
-  uint32_t                          mChooseApplicationCache          : 1;
-  uint32_t                          mLoadedFromApplicationCache      : 1;
-  uint32_t                          mChannelIsForDownload            : 1;
-  uint32_t                          mTracingEnabled                  : 1;
+  uint32_t                          mRequestObserversCalled     : 1;
+  uint32_t                          mResponseHeadersModified    : 1;
+  uint32_t                          mAllowPipelining            : 1;
+  uint32_t                          mAllowSTS                   : 1;
+  uint32_t                          mThirdPartyFlags            : 3;
+  uint32_t                          mUploadStreamHasHeaders     : 1;
+  uint32_t                          mInheritApplicationCache    : 1;
+  uint32_t                          mChooseApplicationCache     : 1;
+  uint32_t                          mLoadedFromApplicationCache : 1;
+  uint32_t                          mChannelIsForDownload       : 1;
+  uint32_t                          mTracingEnabled             : 1;
   
-  uint32_t                          mTimingEnabled                   : 1;
-  uint32_t                          mAllowSpdy                       : 1;
-  uint32_t                          mAllowAltSvc                     : 1;
-  uint32_t                          mResponseTimeoutEnabled          : 1;
+  uint32_t                          mTimingEnabled              : 1;
+  uint32_t                          mAllowSpdy                  : 1;
+  uint32_t                          mAllowAltSvc                : 1;
+  uint32_t                          mResponseTimeoutEnabled     : 1;
   
-  uint32_t                          mAllRedirectsSameOrigin          : 1;
+  uint32_t                          mAllRedirectsSameOrigin     : 1;
 
   
   
