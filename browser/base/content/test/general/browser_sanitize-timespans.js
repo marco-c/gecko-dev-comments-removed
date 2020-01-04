0b4fef98dffb90612be36a105cded178766a336d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 07:21:33 +0000

    Bug 966135 - Increase the timeout of browser_sanitize-timespans.js
    UltraBlame original commit: 8c18d3a64acc58e483ba8175db7f9ba62892c4ae

--- browser/base/content/test/general/browser_sanitize-timespans.js
+++ browser/base/content/test/general/browser_sanitize-timespans.js
@@ -1,6 +1,8 @@
 Cu.import("resource://gre/modules/XPCOMUtils.jsm");
 Cu.import("resource://gre/modules/Services.jsm");
 
+requestLongerTimeout(2);
+
 
 var now_mSec = Date.now();
 var now_uSec = now_mSec * 1000;
