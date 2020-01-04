0939fa4281f624dc0a77e54ff62e38a2b59af5b1
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 08:20:23 +0000

    Bug 1181837 - 7 - Include inspector's head.js in font inspector to remove duplication; r=bgrins
    UltraBlame original commit: 74646bfb670f2a979933463b87f9b4dc39b49ee5

--- devtools/client/inspector/fonts/test/head.js
+++ devtools/client/inspector/fonts/test/head.js
@@ -2,79 +2,21 @@
 
 
 
-
 "use strict";
 
 
-Services.scriptloader.loadSubScript("chrome://mochitests/content/browser/devtools/client/framework/test/shared-head.js", this);
-
-const BASE_URI = "http://mochi.test:8888/browser/devtools/client/inspector/fonts/test/";
-
-
-
-
-
-
-
-var openInspector = Task.async(function*(cb) {
-  info("Opening the inspector");
-  let target = TargetFactory.forTab(gBrowser.selectedTab);
-
-  let inspector, toolbox;
-
-  
-  
-  
-  toolbox = gDevTools.getToolbox(target);
-  if (toolbox) {
-    inspector = toolbox.getPanel("inspector");
-    if (inspector) {
-      info("Toolbox and inspector already open");
-      if (cb) {
-        return cb(inspector, toolbox);
-      } else {
-        return {
-          toolbox: toolbox,
-          inspector: inspector
-        };
-      }
-    }
-  }
-
-  info("Opening the toolbox");
-  toolbox = yield gDevTools.showToolbox(target, "inspector");
-  yield waitForToolboxFrameFocus(toolbox);
-  inspector = toolbox.getPanel("inspector");
-
-  info("Waiting for the inspector to update");
-  yield inspector.once("inspector-updated");
-
-  if (cb) {
-    return cb(inspector, toolbox);
-  } else {
-    return {
-      toolbox: toolbox,
-      inspector: inspector
-    };
-  }
-});
+Services.scriptloader.loadSubScript(
+  "chrome://mochitests/content/browser/devtools/client/inspector/test/head.js",
+  this);
 
 
 
 
 
 
-
-
-
-
-
-
-var openFontInspectorForURL = Task.async(function* (url) {
-  info("Opening tab " + url);
+var openFontInspectorForURL = Task.async(function*(url) {
   yield addTab(url);
-
-  let { toolbox, inspector } = yield openInspector();
+  let {toolbox, inspector} = yield openInspectorSidebarTab("fontinspector");
 
   
 
@@ -88,74 +30,29 @@ var openFontInspectorForURL = Task.async(function* (url) {
 
 
   let onUpdated = inspector.once("fontinspector-updated");
-
   yield selectNode("body", inspector);
-  inspector.sidebar.select("fontinspector");
-
-  info("Waiting for font-inspector to update.");
   yield onUpdated;
 
-  info("Font Inspector ready.");
-
-  let { fontInspector } = inspector.sidebar.getWindowForTab("fontinspector");
   return {
-    fontInspector,
+    toolbox,
     inspector,
-    toolbox
+    view: inspector.sidebar.getWindowForTab("fontinspector").fontInspector
   };
 });
 
 
 
 
-var selectNode = Task.async(function*(selector, inspector, reason="test") {
-  info("Selecting the node for '" + selector + "'");
-  let nodeFront = yield getNodeFront(selector, inspector);
-  let updated = inspector.once("inspector-updated");
-  inspector.selection.setNodeFront(nodeFront, reason);
-  yield updated;
-});
 
 
 
 
-
-
-
-
-function getNodeFront(selector, {walker}) {
-  if (selector._form) {
-    return selector;
-  }
-  return walker.querySelector(walker.rootNode, selector);
-}
-
-
-
-
-
-
-function waitForToolboxFrameFocus(toolbox) {
-  info("Making sure that the toolbox's frame is focused");
-  let def = promise.defer();
-  let win = toolbox.frame.contentWindow;
-  waitForFocus(def.resolve, win);
-  return def.promise;
-}
-
-
-
-
-
-
-
-
-function* updatePreviewText(fontInspector, text) {
+function* updatePreviewText(view, text) {
   info(`Changing the preview text to '${text}'`);
 
-  let doc = fontInspector.chromeDoc;
+  let doc = view.chromeDoc;
   let input = doc.getElementById("preview-text-input");
-  let update = fontInspector.inspector.once("fontinspector-updated");
+  let update = view.inspector.once("fontinspector-updated");
 
   info("Focusing the input field.");
   input.focus();
