d6a063c2248852601d2f54a7797cae2cae8e0bd2
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 09:15:25 +0000

    Bug 1237848 - Updated update.py patch - r=rillian
    UltraBlame original commit: f6e4f2f869a326e461989bd4c821b7228ee45cb5

--- media/libvpx/update.py
+++ media/libvpx/update.py
@@ -602,6 +602,8 @@ def apply_patches():
     os.system("patch -p3 < clang-cl.patch")
     
     os.system("patch -p3 < cast-char-to-uint-before-shift.patch")
+    
+    os.system("patch -p3 < 1237848-check-lookahead-ctx.patch")
 
 def update_readme(commit):
     with open('README_MOZILLA') as f:
