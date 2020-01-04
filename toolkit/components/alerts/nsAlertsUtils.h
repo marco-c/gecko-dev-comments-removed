6153bfc5c06d7b773156ce973c0ed2ee86074ed3
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 03:23:26 +0000

    Back out changeset bb7e9c6a66c6 (bug 1209602) for crashing
    UltraBlame original commit: a2fd11b42de8e80158687fc665a5353e9fbf8575

--- toolkit/components/alerts/nsAlertsUtils.h
+++ toolkit/components/alerts/nsAlertsUtils.h
@@ -21,5 +21,19 @@ public:
 
   static bool
   IsActionablePrincipal(nsIPrincipal* aPrincipal);
+
+  
+
+
+
+  static void
+  GetSource(nsIPrincipal* aPrincipal, nsAString& aSource);
+
+  
+
+
+
+  static void
+  GetSourceHostPort(nsIPrincipal* aPrincipal, nsAString& aHostPort);
 };
 #endif 
