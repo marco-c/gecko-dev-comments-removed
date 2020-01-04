4b8abc812e0944d2e56409c5fe2b3d2defc54b46
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 07:59:03 +0000

    Bug 1235092 - Part 1: Optimize spread call with rest parameter. r=efaust
    UltraBlame original commit: 33600326da21be6564e6ee4e9b0394a48891d645

--- js/src/vm/Opcodes.h
+++ js/src/vm/Opcodes.h
@@ -1818,7 +1818,16 @@
 
  \
     macro(JSOP_DEBUGCHECKSELFHOSTED, 177,"debug-checkselfhosted",  NULL, 1,  1,  1,  JOF_BYTE) \
-    macro(JSOP_UNUSED178,     178,"unused178",  NULL,     1,  0,  0,  JOF_BYTE) \
+    
+
+
+
+
+
+
+
+ \
+    macro(JSOP_OPTIMIZE_SPREADCALL,178,"optimize-spreadcall", NULL, 1,  1,  2,  JOF_BYTE) \
     macro(JSOP_UNUSED179,     179,"unused179",  NULL,     1,  0,  0,  JOF_BYTE) \
     macro(JSOP_UNUSED180,     180,"unused180",  NULL,     1,  0,  0,  JOF_BYTE) \
     macro(JSOP_UNUSED181,     181,"unused181",  NULL,     1,  0,  0,  JOF_BYTE) \
