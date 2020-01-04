92075af72edd8e511b2e2ed0ec023ec98188148a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 22:21:23 +0000

    Bug 1273760 - Blocking state in Network Monitor. r=jsnajdr
    UltraBlame original commit: 7a35a32895ce10adc32d160bc0b2a69c6456b25f

--- devtools/shared/webconsole/network-monitor.js
+++ devtools/shared/webconsole/network-monitor.js
@@ -1327,8 +1327,15 @@ NetworkMonitor.prototype = {
     let timings = httpActivity.timings;
     let harTimings = {};
 
-    
-    harTimings.blocked = -1;
+    if (timings.STATUS_RESOLVING && timings.STATUS_CONNECTING_TO) {
+      harTimings.blocked = timings.STATUS_RESOLVING.first -
+                           timings.REQUEST_HEADER.first;
+    } else if (timings.STATUS_SENDING_TO) {
+      harTimings.blocked = timings.STATUS_SENDING_TO.first -
+                           timings.REQUEST_HEADER.first;
+    } else {
+      harTimings.blocked = -1;
+    }
 
     
     
@@ -1339,9 +1346,6 @@ NetworkMonitor.prototype = {
     if (timings.STATUS_CONNECTING_TO && timings.STATUS_CONNECTED_TO) {
       harTimings.connect = timings.STATUS_CONNECTED_TO.last -
                            timings.STATUS_CONNECTING_TO.first;
-    } else if (timings.STATUS_SENDING_TO) {
-      harTimings.connect = timings.STATUS_SENDING_TO.first -
-                           timings.REQUEST_HEADER.first;
     } else {
       harTimings.connect = -1;
     }
