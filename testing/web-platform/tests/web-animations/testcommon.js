632da4cfc09f205f6716f4a8675a7c43b3b00fa6
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 10:01:01 +0000

    Bug 1216842 - Part 13: Tests for effect-level easing. r=birtles
    UltraBlame original commit: 98d27c4336aa92c1196c2ed8fa75e27af5f33d4d

--- testing/web-platform/tests/web-animations/testcommon.js
+++ testing/web-platform/tests/web-animations/testcommon.js
@@ -17,42 +17,99 @@ var ANIMATION_TOP_0_5 = 100;
 var ANIMATION_TOP_1 = 200;
 
 var KEYFRAMES = [ {
-    top : ANIMATION_TOP_0 + 'px',
-    offset : 0
+  top : ANIMATION_TOP_0 + 'px',
+  offset : 0
 }, {
-    top : ANIMATION_TOP_0_5 + 'px',
-    offset : 1 / 2
+  top : ANIMATION_TOP_0_5 + 'px',
+  offset : 1 / 2
 }, {
-    top : ANIMATION_TOP_1 + 'px',
-    offset : 1
+  top : ANIMATION_TOP_1 + 'px',
+  offset : 1
 } ];
 
 
 function newAnimation(animationTarget) {
-    animationTarget.style.top = ANIMATION_TOP_DEFAULT + 'px';
-    return new Animation(animationTarget, KEYFRAMES, ANIMATION_END_TIME);
+  animationTarget.style.top = ANIMATION_TOP_DEFAULT + 'px';
+  return new Animation(animationTarget, KEYFRAMES, ANIMATION_END_TIME);
 }
 
 
 
 function createDiv(test, doc) {
-    if (!doc) {
-        doc = document;
-    }
-    var div = doc.createElement('div');
-    doc.body.appendChild(div);
-    test.add_cleanup(function() {
-        removeElement(div);
-    });
-    return div;
+  if (!doc) {
+    doc = document;
+  }
+  var div = doc.createElement('div');
+  doc.body.appendChild(div);
+  test.add_cleanup(function() {
+    removeElement(div);
+  });
+  return div;
 }
 
 
 function removeElement(element) {
-    element.parentNode.removeChild(element);
+  element.parentNode.removeChild(element);
 }
 
 
 function type(object) {
-    return Object.prototype.toString.call(object).slice(8, -1);
+  return Object.prototype.toString.call(object).slice(8, -1);
 }
+
+
+function pxToNum(str) {
+  return Number(String(str).match(/^(-?[\d.]+)px$/)[1]);
+}
+
+
+function cubicBezier(x1, y1, x2, y2) {
+  function xForT(t) {
+    var omt = 1-t;
+    return 3 * omt * omt * t * x1 + 3 * omt * t * t * x2 + t * t * t;
+  }
+
+  function yForT(t) {
+    var omt = 1-t;
+    return 3 * omt * omt * t * y1 + 3 * omt * t * t * y2 + t * t * t;
+  }
+
+  function tForX(x) {
+    
+    var mint = 0, maxt = 1;
+    for (var i = 0; i < 30; ++i) {
+      var guesst = (mint + maxt) / 2;
+      var guessx = xForT(guesst);
+      if (x < guessx) {
+        maxt = guesst;
+      } else {
+        mint = guesst;
+      }
+    }
+    return (mint + maxt) / 2;
+  }
+
+  return function bezierClosure(x) {
+    if (x == 0) {
+      return 0;
+    }
+    if (x == 1) {
+      return 1;
+    }
+    return yForT(tForX(x));
+  }
+}
+
+function stepEnd(nsteps) {
+  return function stepEndClosure(x) {
+    return Math.floor(x * nsteps) / nsteps;
+  }
+}
+
+function stepStart(nsteps) {
+  return function stepStartClosure(x) {
+    var result = Math.floor(x * nsteps + 1.0) / nsteps;
+    return (result > 1.0) ? 1.0 : result;
+  }
+}
+
