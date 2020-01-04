444bea1f6dc39a461e2b0b1b109750fb2a0a6ad2
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 21:35:41 +0000

    Bug 1294133. Null check animated geometry root. r=mattwoodrow
    UltraBlame original commit: 0bd27824832d4213a90ec0ef08de422b33589358

--- layout/base/FrameLayerBuilder.cpp
+++ layout/base/FrameLayerBuilder.cpp
@@ -2197,7 +2197,7 @@ ContainerState::GetLayerCreationHint(AnimatedGeometryRoot* aAnimatedGeometryRoot
   
   
   for (AnimatedGeometryRoot* agr = aAnimatedGeometryRoot;
-       agr != mContainerAnimatedGeometryRoot;
+       agr && agr != mContainerAnimatedGeometryRoot;
        agr = agr->mParentAGR) {
     nsIFrame* fParent = nsLayoutUtils::GetCrossDocParentFrame(*agr);
     if (!fParent) {
