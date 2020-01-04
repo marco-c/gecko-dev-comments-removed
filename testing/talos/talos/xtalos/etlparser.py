604ad83819987dc43ece1c6b1aa60bc6db73f0ea
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 03:16:14 +0000

    Bug 1205005 - Talos-e10s(tp) occasionally fails with TALOSDATA was seen 0 times, expected 1. r=aklotz
    UltraBlame original commit: 89add5fc9f9778a870be8e74ed21e1837ad7c4f3

--- testing/talos/talos/xtalos/etlparser.py
+++ testing/talos/talos/xtalos/etlparser.py
@@ -455,10 +455,11 @@ def etlparser(xperf_path, etl_filename, processID, approot=None,
             with open(error_filename, 'w') as errorFile:
                 errorFile.write('\n'.join(errors))
 
-        mud = os.environ.get('MOZ_UPLOAD_DIR', None)
-        if mud:
-            mud_filename = os.path.join(mud, etl_filename)
-            os.rename(etl_filename, mud_filename)
+
+
+
+
+
 
 
 def etlparser_from_config(config_file, **kwargs):
