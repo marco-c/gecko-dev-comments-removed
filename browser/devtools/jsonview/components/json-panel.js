37260a88a3d34ac3d2e9a05bba3080aa6883c64a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 00:04:08 +0000

    Bug 1132203 - Tests for JSON Viewer. r=jryans
    UltraBlame original commit: e059e455e5b37d713bd198c6ebbc3ffa4cfe8f75

--- browser/devtools/jsonview/components/json-panel.js
+++ browser/devtools/jsonview/components/json-panel.js
@@ -79,10 +79,10 @@ var JsonToolbar = React.createFactory(React.createClass({
   render: function() {
     return (
       Toolbar({},
-        ToolbarButton({onClick: this.onSave},
+        ToolbarButton({className: "save", onClick: this.onSave},
           Locale.$STR("jsonViewer.Save")
         ),
-        ToolbarButton({onClick: this.onCopy},
+        ToolbarButton({className: "copy", onClick: this.onCopy},
           Locale.$STR("jsonViewer.Copy")
         ),
         SearchBox({
