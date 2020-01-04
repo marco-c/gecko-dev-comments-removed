c1c37bdc76f78a6b9fe64e7c47a38750db77a0d1
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 09:46:35 +0000

    Back out 7 changesets (bug 1231793) for Windows crashes in mozilla::MediaDecoderReader::~MediaDecoderReader()
    Backed out changeset 273cc690d7cf (bug 1231793)
    Backed out changeset d79218caeefc (bug 1231793)
    Backed out changeset 75c7942267f8 (bug 1231793)
    Backed out changeset 684e62a4d9f5 (bug 1231793)
    Backed out changeset a5a15d50a8e4 (bug 1231793)
    Backed out changeset 33d6f22d9b1f (bug 1231793)
    Backed out changeset cd4fc7f2c80a (bug 1231793)
    
    UltraBlame original commit: 52d290be7c7dab9c3dedbd372e131358c3e285e2

--- dom/media/wave/WaveDecoder.h
+++ dom/media/wave/WaveDecoder.h
@@ -8,20 +8,30 @@
 
 #include "MediaDecoder.h"
 
+
+
+
+
+
+
+
+
+
+
+
 namespace mozilla {
 
-class WaveDecoder : public MediaDecoder {
+class WaveDecoder : public MediaDecoder
+{
 public:
-  
   explicit WaveDecoder(MediaDecoderOwner* aOwner) : MediaDecoder(aOwner) {}
-  MediaDecoder* Clone(MediaDecoderOwner* aOwner) override;
+  MediaDecoder* Clone(MediaDecoderOwner* aOwner) override {
+    if (!IsWaveEnabled()) {
+      return nullptr;
+    }
+    return new WaveDecoder(aOwner);
+  }
   MediaDecoderStateMachine* CreateStateMachine() override;
-
-  
-  
-  static bool IsEnabled();
-  static bool CanHandleMediaType(const nsACString& aType,
-                                 const nsAString& aCodecs);
 };
 
 } 
