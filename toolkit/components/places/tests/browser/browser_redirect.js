347ea25b95df56d08479c9bdee4a26f2e94b9bc0
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 08:21:51 +0000

    Bug 1236554 - enable browser_redirect in e10s, r=mak
    UltraBlame original commit: 530d3160f8e775af4efde14aa9985cfe31333c6d

--- toolkit/components/places/tests/browser/browser_redirect.js
+++ toolkit/components/places/tests/browser/browser_redirect.js
@@ -2,61 +2,62 @@
 
 
 
-function test() {
+add_task(function* () {
   waitForExplicitFinish();
 
   const REDIRECT_URI = NetUtil.newURI("http://mochi.test:8888/tests/toolkit/components/places/tests/browser/redirect.sjs");
   const TARGET_URI = NetUtil.newURI("http://mochi.test:8888/tests/toolkit/components/places/tests/browser/redirect-target.html");
 
-  gBrowser.selectedTab = gBrowser.addTab();
-  registerCleanupFunction(function() {
-    gBrowser.removeCurrentTab();
-  });
-  gBrowser.selectedBrowser.loadURI(REDIRECT_URI.spec);
-
   
-  let historyObserver = {
-    _redirectNotified: false,
-    onVisit: function (aURI, aVisitID, aTime, aSessionID, aReferringID,
-                      aTransitionType) {
-      info("Received onVisit: " + aURI.spec);
+  let visitedPromise = new Promise(resolve => {
+    let historyObserver = {
+      _redirectNotified: false,
+      onVisit: function (aURI, aVisitID, aTime, aSessionID, aReferringID,
+                        aTransitionType) {
+        info("Received onVisit: " + aURI.spec);
 
-      if (aURI.equals(REDIRECT_URI)) {
-        this._redirectNotified = true;
-        
-        return;
-      }
+        if (aURI.equals(REDIRECT_URI)) {
+          this._redirectNotified = true;
+          
+          return;
+        }
 
-      PlacesUtils.history.removeObserver(historyObserver);
+        PlacesUtils.history.removeObserver(historyObserver);
 
-      ok(this._redirectNotified, "The redirect should have been notified");
+        ok(this._redirectNotified, "The redirect should have been notified");
 
-      fieldForUrl(REDIRECT_URI, "frecency", function (aFrecency) {
-        ok(aFrecency != 0, "Frecency or the redirecting page should not be 0");
+        fieldForUrl(REDIRECT_URI, "frecency", function (aFrecency) {
+          ok(aFrecency != 0, "Frecency or the redirecting page should not be 0");
 
-        fieldForUrl(REDIRECT_URI, "hidden", function (aHidden) {
-          is(aHidden, 1, "The redirecting page should be hidden");
+          fieldForUrl(REDIRECT_URI, "hidden", function (aHidden) {
+            is(aHidden, 1, "The redirecting page should be hidden");
 
-          fieldForUrl(TARGET_URI, "frecency", function (aFrecency) {
-            ok(aFrecency != 0, "Frecency of the target page should not be 0");
+            fieldForUrl(TARGET_URI, "frecency", function (aFrecency) {
+              ok(aFrecency != 0, "Frecency of the target page should not be 0");
 
-            fieldForUrl(TARGET_URI, "hidden", function (aHidden) {
-              is(aHidden, 0, "The target page should not be hidden");
-
-              PlacesTestUtils.clearHistory().then(finish);
+              fieldForUrl(TARGET_URI, "hidden", function (aHidden) {
+                is(aHidden, 0, "The target page should not be hidden");
+                resolve();
+              });
             });
           });
         });
-      });
-    },
-    onBeginUpdateBatch: function () {},
-    onEndUpdateBatch: function () {},
-    onTitleChanged: function () {},
-    onDeleteURI: function () {},
-    onClearHistory: function () {},
-    onPageChanged: function () {},
-    onDeleteVisits: function () {},
-    QueryInterface: XPCOMUtils.generateQI([Ci.nsINavHistoryObserver])
-  };
-  PlacesUtils.history.addObserver(historyObserver, false);
-}
+      },
+      onBeginUpdateBatch: function () {},
+      onEndUpdateBatch: function () {},
+      onTitleChanged: function () {},
+      onDeleteURI: function () {},
+      onClearHistory: function () {},
+      onPageChanged: function () {},
+      onDeleteVisits: function () {},
+      QueryInterface: XPCOMUtils.generateQI([Ci.nsINavHistoryObserver])
+    };
+    PlacesUtils.history.addObserver(historyObserver, false);
+  });
+
+  let newTabPromise = BrowserTestUtils.openNewForegroundTab(gBrowser, REDIRECT_URI.spec);
+  yield Promise.all([visitedPromise, newTabPromise]);
+
+  yield PlacesTestUtils.clearHistory();
+  gBrowser.removeCurrentTab();
+});
