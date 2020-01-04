1869720ecf13a42df997344fb89016136a3c59e2
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 21:35:44 +0000

    Bug 1291016 - Track source-string position of possible prefix. r=heycam
    UltraBlame original commit: 32695cf856683158ecd0f70081837c3a789305c9

--- layout/generic/nsTextRunTransformations.cpp
+++ layout/generic/nsTextRunTransformations.cpp
@@ -306,6 +306,9 @@ nsCaseTransformTextRunFactory::TransformString(
   mozilla::GreekCasing::State greekState;
   mozilla::IrishCasing::State irishState;
   uint32_t irishMark = uint32_t(-1); 
+                                     
+  uint32_t irishMarkSrc; 
+                         
 
   for (uint32_t i = 0; i < length; ++i) {
     uint32_t ch = str[i];
@@ -456,6 +459,7 @@ nsCaseTransformTextRunFactory::TransformString(
         ch = mozilla::IrishCasing::UpperCase(ch, irishState, mark, action);
         if (mark) {
           irishMark = aConvertedString.Length();
+          irishMarkSrc = i;
           break;
         } else if (action) {
           nsString& str = aConvertedString; 
@@ -481,7 +485,7 @@ nsCaseTransformTextRunFactory::TransformString(
             NS_ASSERTION(str.Length() >= 2 && irishMark == str.Length() - 2,
                          "bad irishMark!");
             str.Replace(irishMark, 2, ToLowerCase(str[irishMark]));
-            aDeletedCharsArray[irishMark + 1] = true;
+            aDeletedCharsArray[irishMarkSrc + 1] = true;
             
             
             aCharsToMergeArray.SetLength(aCharsToMergeArray.Length() - 1);
