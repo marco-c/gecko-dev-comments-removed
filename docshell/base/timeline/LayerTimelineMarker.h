2c76a21b300b5d455d8389f022d9223aff2c0369
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 08:47:23 +0000

    Bug 1239864 (part 3) - Use the new rect iterators in docshell/ and dom/. r=baku.
    UltraBlame original commit: 03e0cf7f8a0e9e40d81b7ad196ef22c3cfa6a29b

--- docshell/base/timeline/LayerTimelineMarker.h
+++ docshell/base/timeline/LayerTimelineMarker.h
@@ -1,8 +1,8 @@
-/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
-/* vim: set ts=8 sts=2 et sw=2 tw=80: */
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+
+
+
+
 
 #ifndef mozilla_LayerTimelineMarker_h_
 #define mozilla_LayerTimelineMarker_h_
@@ -23,13 +23,13 @@ public:
 
   void AddLayerRectangles(dom::Sequence<dom::ProfileTimelineLayerRect>& aRectangles)
   {
-    nsIntRegionRectIterator it(mRegion);
-    while (const nsIntRect* iterRect = it.Next()) {
+    for (auto iter = mRegion.RectIter(); !iter.Done(); iter.Next()) {
+      const nsIntRect& iterRect = iter.Get();
       dom::ProfileTimelineLayerRect rect;
-      rect.mX = iterRect->X();
-      rect.mY = iterRect->Y();
-      rect.mWidth = iterRect->Width();
-      rect.mHeight = iterRect->Height();
+      rect.mX = iterRect.X();
+      rect.mY = iterRect.Y();
+      rect.mWidth = iterRect.Width();
+      rect.mHeight = iterRect.Height();
       aRectangles.AppendElement(rect, fallible);
     }
   }
@@ -38,6 +38,6 @@ private:
   nsIntRegion mRegion;
 };
 
-} // namespace mozilla
+} 
 
-#endif // mozilla_LayerTimelineMarker_h_
+#endif 
