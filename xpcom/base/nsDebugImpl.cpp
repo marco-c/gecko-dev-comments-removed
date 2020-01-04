6ee6c518a59db37d278adadaa7bea872f5319490
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 10:28:24 +0000

    Bug 1251482 - Remove remaining references to MOZILLA_XPCOMRT_API from xpcom. r=froydnj
    UltraBlame original commit: afd1b8342a53046d3bc516ac8ae586919c0f5f88

--- xpcom/base/nsDebugImpl.cpp
+++ xpcom/base/nsDebugImpl.cpp
@@ -333,9 +333,8 @@ NS_DebugBreak(uint32_t aSeverity, const char* aStr, const char* aExpr,
   if (sMultiprocessDescription) {
     PrintToBuffer("%s ", sMultiprocessDescription);
   }
-#if !defined(MOZILLA_XPCOMRT_API)
+
   PrintToBuffer("%d] ", base::GetCurrentProcId());
-#endif 
 
   PrintToBuffer("%s: ", sevString);
 
@@ -381,7 +380,7 @@ NS_DebugBreak(uint32_t aSeverity, const char* aStr, const char* aExpr,
       return;
 
     case NS_DEBUG_ABORT: {
-#if defined(MOZ_CRASHREPORTER) && !defined(MOZILLA_XPCOMRT_API)
+#if defined(MOZ_CRASHREPORTER)
       
       
       
@@ -398,7 +397,7 @@ NS_DebugBreak(uint32_t aSeverity, const char* aStr, const char* aExpr,
 #if defined(DEBUG) && defined(_WIN32)
       RealBreak();
 #endif
-#if defined(DEBUG) && !defined(MOZILLA_XPCOMRT_API)
+#if defined(DEBUG)
       nsTraceRefcnt::WalkTheStack(stderr);
 #endif
       Abort(buf.buffer);
@@ -423,15 +422,11 @@ NS_DebugBreak(uint32_t aSeverity, const char* aStr, const char* aExpr,
       return;
 
     case NS_ASSERT_STACK:
-#if !defined(MOZILLA_XPCOMRT_API)
       nsTraceRefcnt::WalkTheStack(stderr);
-#endif 
       return;
 
     case NS_ASSERT_STACK_AND_ABORT:
-#if !defined(MOZILLA_XPCOMRT_API)
       nsTraceRefcnt::WalkTheStack(stderr);
-#endif 
       
       MOZ_FALLTHROUGH;
 
@@ -603,7 +598,7 @@ NS_ErrorAccordingToNSPR()
 void
 NS_ABORT_OOM(size_t aSize)
 {
-#if defined(MOZ_CRASHREPORTER) && !defined(MOZILLA_XPCOMRT_API)
+#if defined(MOZ_CRASHREPORTER)
   CrashReporter::AnnotateOOMAllocationSize(aSize);
 #endif
   MOZ_CRASH();
