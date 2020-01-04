ad3bdba04fb9b50a0b2ac3f9d49949bfd5a900c3
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 18:56:29 +0000

    Bug 1282184 - Use lldb from Xcode install rather than system install if possible. r=ted
    This works around issues with System Integrity Protection kicking in on OSX when
    trying to run /usr/bin/lldb by attempting to using the version installed with
    XCode's command line utilities.
    
    In particular SIP prevents us from setting DYLD_LIBRARY_PATH which is needed
    to run DMD.
    
    UltraBlame original commit: 14663c45b49b8998e61c22c489357424948cfe6d

--- testing/mozbase/mozdebug/mozdebug/mozdebug.py
+++ testing/mozbase/mozdebug/mozdebug/mozdebug.py
@@ -8,6 +8,7 @@ import os
 import mozinfo
 from collections import namedtuple
 from distutils.spawn import find_executable
+from subprocess import check_output
 
 __all__ = ['get_debugger_info',
            'get_default_debugger_name',
@@ -80,6 +81,30 @@ def _windbg_installation_paths():
             yield os.path.join(windowsKitsPrefix, version,
                                'Debuggers', 'x86', 'windbg.exe')
 
+def get_debugger_path(debugger):
+    '''
+    Get the full path of the debugger.
+
+    :param debugger: The name of the debugger.
+    '''
+
+    if mozinfo.os == 'mac' and debugger == 'lldb':
+        
+        
+        
+        
+
+        
+        try:
+            path = check_output(['xcrun', '--find', 'lldb']).strip()
+            if path:
+                return path
+        except:
+            
+            pass
+
+    return find_executable(debugger)
+
 def get_debugger_info(debugger, debuggerArgs = None, debuggerInteractive = False):
     '''
     Get the information about the requested debugger.
@@ -105,7 +130,7 @@ def get_debugger_info(debugger, debuggerArgs = None, debuggerInteractive = False
             and not debugger.lower().endswith('.exe')):
             debugger += '.exe'
 
-        debuggerPath = find_executable(debugger)
+        debuggerPath = get_debugger_path(debugger)
 
     if not debuggerPath:
         
