92dd4bb6deeb7b4855f085a191de66b38ebee486
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 04:03:27 +0000

    Bug 1217726 - Fix -Wimplicit-fallthrough warnings in dom/plugins. r=jimm
    dom/plugins/base/nsNPAPIPlugin.cpp:1974:3 [-Wimplicit-fallthrough] unannotated fall-through between switch labels
    dom/plugins/base/nsNPAPIPlugin.cpp:2200:3 [-Wimplicit-fallthrough] unannotated fall-through between switch labels
    dom/plugins/ipc/PluginInstanceChild.cpp:507:5 [-Wimplicit-fallthrough] unannotated fall-through between switch labels
    
    UltraBlame original commit: 59f041348904d8466b51de6e4f77e2974fa7974f

--- dom/plugins/base/nsNPAPIPlugin.cpp
+++ dom/plugins/base/nsNPAPIPlugin.cpp
@@ -693,7 +693,6 @@ AsyncCallbackAutoLock::~AsyncCallbackAutoLock()
   }
 }
 
-
 NPP NPPStack::sCurrentNPP = nullptr;
 
 const char *
@@ -743,7 +742,6 @@ _geturl(NPP npp, const char* relativeURL, const char* target)
       (strncmp(relativeURL, "https:", 6) != 0) &&
       (strncmp(relativeURL, "ftp:", 4) != 0)) {
     nsNPAPIPluginInstance *inst = (nsNPAPIPluginInstance *) npp->ndata;
-
 
     const char *name = nullptr;
     RefPtr<nsPluginHost> host = nsPluginHost::GetInst();
@@ -1958,9 +1956,7 @@ _getvalue(NPP npp, NPNVariable variable, void *result)
         return NPERR_NO_ERROR;
       }
     }
-    else {
-      return NPERR_GENERIC_ERROR;
-    }
+    return NPERR_GENERIC_ERROR;
   }
 
 #ifndef NP_NO_QUICKDRAW
@@ -2189,14 +2185,13 @@ _getvalue(NPP npp, NPNVariable variable, void *result)
 
   
   case NPNVDOMElement:
-    
   case NPNVDOMWindow:
-    
   case NPNVserviceManager:
     
     
     *(nsISupports**)result = nullptr;
-    
+    MOZ_FALLTHROUGH;
+
   default:
     NPN_PLUGIN_LOG(PLUGIN_LOG_NORMAL, ("NPN_getvalue unhandled get value: %d\n", variable));
     return NPERR_GENERIC_ERROR;
