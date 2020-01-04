d3f09b0bbe7b2edef9ae5e2af4381bbc16db5423
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 18:15:50 +0000

    Bug 1273950 - Add pref to check captive portal without waiting for network activity r=MattN MozReview-Commit-ID: 6NG8BF4PYtp
    UltraBlame original commit: 19d9a93486ab7b7a8b9c4a5b7e78f35fea4f7504

--- toolkit/components/captivedetect/captivedetect.js
+++ toolkit/components/captivedetect/captivedetect.js
@@ -89,6 +89,8 @@ function LoginObserver(captivePortalDetector) {
                               .getService(Ci.nsIHttpActivityDistributor);
   let urlFetcher = null;
 
+  let waitForNetworkActivity = Services.appinfo.widgetToolkit == "gonk";
+
   let pageCheckingDone = function pageCheckingDone() {
     if (state === LOGIN_OBSERVER_STATE_VERIFYING) {
       urlFetcher = null;
@@ -177,10 +179,14 @@ function LoginObserver(captivePortalDetector) {
           state = LOGIN_OBSERVER_STATE_VERIFY_NEEDED;
           
         case LOGIN_OBSERVER_STATE_IDLE:
-          timer.initWithCallback(this,
-                                 captivePortalDetector._pollingTime,
-                                 timer.TYPE_ONE_SHOT);
-          break;
+          if (waitForNetworkActivity) {
+            timer.initWithCallback(this,
+                                   captivePortalDetector._pollingTime,
+                                   timer.TYPE_ONE_SHOT);
+            break;
+          }
+          
+          
         case LOGIN_OBSERVER_STATE_VERIFY_NEEDED:
           
           state = LOGIN_OBSERVER_STATE_VERIFYING;
