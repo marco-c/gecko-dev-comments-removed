b557c0d71e3f9c3715264d62f4161c182f92365e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 19:13:06 +0000

    Bug 1208373 - Introduce a new blocking mode to MediaInputPort. r=jesup
    This lets us notify about a created TrackUnionStream track (and since it was
    created, we can notify when it ends), even though it has been blocked from main
    thread.
    
    MozReview-Commit-ID: HyopzISBfbb
    
    UltraBlame original commit: 3598ea9d1b25191c983dbbbdcd5399f5b4a92e36

--- dom/media/DOMMediaStream.h
+++ dom/media/DOMMediaStream.h
@@ -34,6 +34,8 @@ class MediaStreamDirectListener;
 class MediaStreamGraph;
 class ProcessedMediaStream;
 
+enum class BlockingMode;
+
 namespace dom {
 class AudioNode;
 class HTMLCanvasElement;
@@ -304,7 +306,8 @@ public:
 
 
 
-    already_AddRefed<media::Pledge<bool, nsresult>> BlockSourceTrackId(TrackID aTrackId);
+    already_AddRefed<media::Pledge<bool, nsresult>>
+    BlockSourceTrackId(TrackID aTrackId, BlockingMode aBlockingMode);
 
   private:
     RefPtr<MediaInputPort> mInputPort;
