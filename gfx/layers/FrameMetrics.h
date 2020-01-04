a2f2e1d570ee33be6c0b326b6e3f26fe2619f599
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 15:30:03 +0000

    Bug 1267471 - Check the snap info when comparing scroll metadata for equality. rs=botond
    MozReview-Commit-ID: 5aw7vjT3YcE
    
    UltraBlame original commit: faf1c8eb92613c66051a2a564452365d6cd2e6c9

--- gfx/layers/FrameMetrics.h
+++ gfx/layers/FrameMetrics.h
@@ -803,6 +803,16 @@ struct ScrollSnapInfo {
     , mScrollSnapTypeY(NS_STYLE_SCROLL_SNAP_TYPE_NONE)
   {}
 
+  bool operator==(const ScrollSnapInfo& aOther) const
+  {
+    return mScrollSnapTypeX == aOther.mScrollSnapTypeX &&
+           mScrollSnapTypeY == aOther.mScrollSnapTypeY &&
+           mScrollSnapIntervalX == aOther.mScrollSnapIntervalX &&
+           mScrollSnapIntervalY == aOther.mScrollSnapIntervalY &&
+           mScrollSnapDestination == aOther.mScrollSnapDestination &&
+           mScrollSnapCoordinates == aOther.mScrollSnapCoordinates;
+  }
+
   
   
   uint8_t mScrollSnapTypeX;
@@ -842,8 +852,8 @@ public:
 
   bool operator==(const ScrollMetadata& aOther) const
   {
-    
     return mMetrics == aOther.mMetrics &&
+           mSnapInfo == aOther.mSnapInfo &&
            mMaskLayerIndex == aOther.mMaskLayerIndex &&
            mClipRect == aOther.mClipRect;
   }
