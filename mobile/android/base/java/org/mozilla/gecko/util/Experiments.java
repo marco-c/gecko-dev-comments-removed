afa11dbb128036f5ea78c38bc74a71db3323b317
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 10:35:02 +0000

    Bug 1004734 - Create system notification on browser update. r=liuche,mfinkle
    MozReview-Commit-ID: DsVhO2kagZB
    
    UltraBlame original commit: 2d09899861de02972eb86f542841214148e71241

--- mobile/android/base/java/org/mozilla/gecko/util/Experiments.java
+++ mobile/android/base/java/org/mozilla/gecko/util/Experiments.java
@@ -26,14 +26,18 @@ public class Experiments {
     public static final String BOOKMARKS_HISTORY_MENU = "bookmark-history-menu";
 
     
+    public static final String SEARCH_TERM = "search-term";
+
+    
+    public static final String WHATSNEW_NOTIFICATION = "whatsnew-notification";
+
+    
+    
     public static final String ONBOARDING2_A = "onboarding2-a"; 
     public static final String ONBOARDING2_B = "onboarding2-b"; 
     public static final String ONBOARDING2_C = "onboarding2-c"; 
 
     public static final String PREF_ONBOARDING_VERSION = "onboarding_version";
-
-    
-    public static final String SEARCH_TERM = "search-term";
 
     private static volatile Boolean disabled = null;
 
