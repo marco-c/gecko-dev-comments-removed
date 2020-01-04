f2572bde2a023a470ff049bbbf7159c859121d4d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 15:17:32 +0000

    Bug 1248708 - Part2.1: use mfbt/TypedEnumBits.h for DrawMode. r=jwatt
    UltraBlame original commit: 954e40c927979cc5569e2247ad632d5c0874a5f6

--- gfx/thebes/DrawMode.h
+++ gfx/thebes/DrawMode.h
@@ -6,20 +6,21 @@
 #ifndef DrawMode_h
 #define DrawMode_h
 
+#include "mozilla/TypedEnumBits.h"
+
 
 enum class DrawMode : int {
   
   
-  GLYPH_FILL = 1,
+  GLYPH_FILL = 1 << 0,
   
-  GLYPH_STROKE = 2,
-  
-  
-  GLYPH_PATH = 4,
+  GLYPH_STROKE = 1 << 1,
   
   
-  GLYPH_STROKE_UNDERNEATH = 8
+  GLYPH_PATH = 1 << 2,
+  
+  
+  GLYPH_STROKE_UNDERNEATH = 1 << 3
 };
-
+MOZ_MAKE_ENUM_CLASS_BITWISE_OPERATORS(DrawMode)
 #endif
-
