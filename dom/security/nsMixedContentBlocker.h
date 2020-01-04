86d0532a810b24dda013b9dd4c5c2586031c143d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 20:31:32 +0000

    Bug 1246540 - HSTS Priming Proof of Concept. r=ckerschb,r=mayhemer,r=jld,r=smaug,r=dkeeler,r=jmaher,p=ally
    HSTS priming changes the order of mixed-content blocking and HSTS
    upgrades, and adds a priming request to check if a mixed-content load is
    accesible over HTTPS and the server supports upgrading via the
    Strict-Transport-Security header.
    
    Every call site that uses AsyncOpen2 passes through the mixed-content
    blocker, and has a LoadInfo. If the mixed-content blocker marks the load as
    needing HSTS priming, nsHttpChannel will build and send an HSTS priming
    request on the same URI with the scheme upgraded to HTTPS. If the server
    allows the upgrade, then channel performs an internal redirect to the HTTPS URI,
    otherwise use the result of mixed-content blocker to allow or block the
    load.
    
    nsISiteSecurityService adds an optional boolean out parameter to
    determine if the HSTS state is already cached for negative assertions.
    If the host has been probed within the previous 24 hours, no HSTS
    priming check will be sent.
    
    (r=ckerschb,r=mayhemer,r=jld,r=smaug,r=dkeeler,r=jmaher,p=ally)
    
    UltraBlame original commit: 1825b8fa636aa282b0786ac11807e616e264d439

--- dom/security/nsMixedContentBlocker.h
+++ dom/security/nsMixedContentBlocker.h
@@ -28,6 +28,8 @@ enum MixedContentTypes {
 #include "nsIChannelEventSink.h"
 #include "imgRequest.h"
 
+class nsILoadInfo; 
+
 class nsMixedContentBlocker : public nsIContentPolicy,
                               public nsIChannelEventSink
 {
@@ -59,9 +61,25 @@ public:
                              nsISupports* aExtra,
                              nsIPrincipal* aRequestPrincipal,
                              int16_t* aDecision);
-  static void AccumulateMixedContentHSTS(nsIURI* aURI, bool aActive);
+  static void AccumulateMixedContentHSTS(nsIURI* aURI,
+                                         bool aActive,
+                                         bool aHasHSTSPriming);
+  
+
+
+
+
+
+
+
+  static nsresult MarkLoadInfoForPriming(nsIURI* aURI,
+                                         nsISupports* aRequestingContext,
+                                         nsILoadInfo* aLoadInfo);
+
   static bool sBlockMixedScript;
   static bool sBlockMixedDisplay;
+  static bool sUseHSTS;
+  static bool sSendHSTSPriming;
 };
 
 #endif 
