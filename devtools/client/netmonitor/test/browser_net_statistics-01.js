806cd81f4a715da678a40c01d5653b7313b09eb7
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 17:56:10 +0000

    Bug 1278346 - Switch browser_net_statistics-01,02.js to add_task. r=ochameau
    MozReview-Commit-ID: 5QDts08x0rv
    
    UltraBlame original commit: f10a5cdcf8a1b908bf2c25f15cc4c8e9df11addd

--- devtools/client/netmonitor/test/browser_net_statistics-01.js
+++ devtools/client/netmonitor/test/browser_net_statistics-01.js
@@ -1,63 +1,64 @@
 
 
 
+"use strict";
 
 
 
 
-function test() {
-  initNetMonitor(STATISTICS_URL).then(([aTab, aDebuggee, aMonitor]) => {
-    info("Starting test... ");
 
-    let panel = aMonitor.panelWin;
-    let { document, $, $all, EVENTS, NetMonitorView } = panel;
+add_task(function* () {
+  let [,, monitor] = yield initNetMonitor(STATISTICS_URL);
+  info("Starting test... ");
 
-    is(NetMonitorView.currentFrontendMode, "network-inspector-view",
-      "The initial frontend mode is correct.");
+  let panel = monitor.panelWin;
+  let { $, $all, EVENTS, NetMonitorView } = panel;
+  is(NetMonitorView.currentFrontendMode, "network-inspector-view",
+    "The initial frontend mode is correct.");
 
-    is($("#primed-cache-chart").childNodes.length, 0,
-      "There should be no primed cache chart created yet.");
-    is($("#empty-cache-chart").childNodes.length, 0,
-      "There should be no empty cache chart created yet.");
+  is($("#primed-cache-chart").childNodes.length, 0,
+    "There should be no primed cache chart created yet.");
+  is($("#empty-cache-chart").childNodes.length, 0,
+    "There should be no empty cache chart created yet.");
 
-    waitFor(panel, EVENTS.PLACEHOLDER_CHARTS_DISPLAYED).then(() => {
-      is($("#primed-cache-chart").childNodes.length, 1,
-        "There should be a placeholder primed cache chart created now.");
-      is($("#empty-cache-chart").childNodes.length, 1,
-        "There should be a placeholder empty cache chart created now.");
+  let onChartDisplayed = Promise.all([
+    waitFor(panel, EVENTS.PRIMED_CACHE_CHART_DISPLAYED),
+    waitFor(panel, EVENTS.EMPTY_CACHE_CHART_DISPLAYED)
+  ]);
+  let onPlaceholderDisplayed = waitFor(panel, EVENTS.PLACEHOLDER_CHARTS_DISPLAYED);
 
-      is($all(".pie-chart-container[placeholder=true]").length, 2,
-        "Two placeholder pie chart appear to be rendered correctly.");
-      is($all(".table-chart-container[placeholder=true]").length, 2,
-        "Two placeholder table chart appear to be rendered correctly.");
+  info("Displaying statistics view");
+  NetMonitorView.toggleFrontendMode();
+  is(NetMonitorView.currentFrontendMode, "network-statistics-view",
+    "The current frontend mode is correct.");
 
-      promise.all([
-        waitFor(panel, EVENTS.PRIMED_CACHE_CHART_DISPLAYED),
-        waitFor(panel, EVENTS.EMPTY_CACHE_CHART_DISPLAYED)
-      ]).then(() => {
-        is($("#primed-cache-chart").childNodes.length, 1,
-          "There should be a real primed cache chart created now.");
-        is($("#empty-cache-chart").childNodes.length, 1,
-          "There should be a real empty cache chart created now.");
+  info("Waiting for placeholder to display");
+  yield onPlaceholderDisplayed;
+  is($("#primed-cache-chart").childNodes.length, 1,
+    "There should be a placeholder primed cache chart created now.");
+  is($("#empty-cache-chart").childNodes.length, 1,
+    "There should be a placeholder empty cache chart created now.");
 
-        Task.spawn(function* () {
-          yield until(() => $all(".pie-chart-container:not([placeholder=true])").length == 2);
-          ok(true, "Two real pie charts appear to be rendered correctly.");
+  is($all(".pie-chart-container[placeholder=true]").length, 2,
+    "Two placeholder pie chart appear to be rendered correctly.");
+  is($all(".table-chart-container[placeholder=true]").length, 2,
+    "Two placeholder table chart appear to be rendered correctly.");
 
-          yield until(() => $all(".table-chart-container:not([placeholder=true])").length == 2);
-          ok(true, "Two real table charts appear to be rendered correctly.");
+  info("Waiting for chart to display");
+  yield onChartDisplayed;
+  is($("#primed-cache-chart").childNodes.length, 1,
+    "There should be a real primed cache chart created now.");
+  is($("#empty-cache-chart").childNodes.length, 1,
+    "There should be a real empty cache chart created now.");
 
-          teardown(aMonitor).then(finish);
-        });
-      });
-    });
+  yield until(() => $all(".pie-chart-container:not([placeholder=true])").length == 2);
+  ok(true, "Two real pie charts appear to be rendered correctly.");
 
-    NetMonitorView.toggleFrontendMode();
+  yield until(() => $all(".table-chart-container:not([placeholder=true])").length == 2);
+  ok(true, "Two real table charts appear to be rendered correctly.");
 
-    is(NetMonitorView.currentFrontendMode, "network-statistics-view",
-      "The current frontend mode is correct.");
-  });
-}
+  yield teardown(monitor);
+});
 
 function waitForTick() {
   let deferred = promise.defer();
@@ -67,6 +68,8 @@ function waitForTick() {
 
 function until(predicate) {
   return Task.spawn(function* () {
-    while (!predicate()) yield waitForTick();
+    while (!predicate()) {
+      yield waitForTick();
+    }
   });
 }
