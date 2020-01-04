f33818a11a0d9f1b51ff5744246d53a497e313bd
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 18:41:14 +0000

    Bug 1276310 part 4.  Store an XPCJSRuntime* in XPCCallContext instead of storing an XPCContext*.  r=bholley
    UltraBlame original commit: 15159f86c546fb14549729ebd73f061591815789

--- js/xpconnect/src/XPCCallContext.cpp
+++ js/xpconnect/src/XPCCallContext.cpp
@@ -26,7 +26,7 @@ XPCCallContext::XPCCallContext(JSContext* cx,
     :   mAr(cx),
         mState(INIT_FAILED),
         mXPC(nsXPConnect::XPConnect()),
-        mXPCContext(nullptr),
+        mXPCJSRuntime(nullptr),
         mJSContext(cx),
         mWrapper(nullptr),
         mTearOff(nullptr),
@@ -38,12 +38,12 @@ XPCCallContext::XPCCallContext(JSContext* cx,
     if (!mXPC)
         return;
 
-    mXPCContext = XPCContext::GetXPCContext(mJSContext);
+    mXPCJSRuntime = XPCJSRuntime::Get();
 
     
-    mPrevCallContext = XPCJSRuntime::Get()->SetCallContext(this);
+    mPrevCallContext = mXPCJSRuntime->SetCallContext(this);
 
-    mState = HAVE_CONTEXT;
+    mState = HAVE_RUNTIME;
 
     if (!obj)
         return;
@@ -124,7 +124,7 @@ void
 XPCCallContext::SetCallInfo(XPCNativeInterface* iface, XPCNativeMember* member,
                             bool isSetter)
 {
-    CHECK_STATE(HAVE_CONTEXT);
+    CHECK_STATE(HAVE_RUNTIME);
 
     
     
@@ -196,7 +196,7 @@ XPCCallContext::SystemIsBeingShutDown()
     
     
     NS_WARNING("Shutting Down XPConnect even through there is a live XPCCallContext");
-    mXPCContext = nullptr;
+    mXPCJSRuntime = nullptr;
     mState = SYSTEM_SHUTDOWN;
     if (mPrevCallContext)
         mPrevCallContext->SystemIsBeingShutDown();
@@ -204,8 +204,8 @@ XPCCallContext::SystemIsBeingShutDown()
 
 XPCCallContext::~XPCCallContext()
 {
-    if (mXPCContext) {
-        DebugOnly<XPCCallContext*> old = XPCJSRuntime::Get()->SetCallContext(mPrevCallContext);
+    if (mXPCJSRuntime) {
+        DebugOnly<XPCCallContext*> old = mXPCJSRuntime->SetCallContext(mPrevCallContext);
         MOZ_ASSERT(old == this, "bad pop from per thread data");
     }
 }
