3d2109b7dfb7b82e0c4a34f7822809affa889e51
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 16:28:34 +0000

    Bug 1267526 part.1 TSFTextStore shouldn't notify TSF of selectin change when the selection isn't actually changed r=m_kato
    MozReview-Commit-ID: 8HLz30lR2TH
    
    UltraBlame original commit: 63073a4784cf19fb3b8006134635e2ff2dcb50ce

--- widget/windows/TSFTextStore.cpp
+++ widget/windows/TSFTextStore.cpp
@@ -4757,11 +4757,18 @@ TSFTextStore::OnSelectionChangeInternal(const IMENotification& aIMENotification)
     return NS_OK;
   }
 
-  mSelection.SetSelection(
-    selectionChangeData.mOffset,
-    selectionChangeData.Length(),
-    selectionChangeData.mReversed,
-    selectionChangeData.GetWritingMode());
+  
+  
+  if (!mSelection.SetSelection(
+                    selectionChangeData.mOffset,
+                    selectionChangeData.Length(),
+                    selectionChangeData.mReversed,
+                    selectionChangeData.GetWritingMode())) {
+    MOZ_LOG(sTextStoreLog, LogLevel::Debug,
+           ("TSF: 0x%p   TSFTextStore::OnSelectionChangeInternal(), selection "
+            "isn't actually changed.", this));
+    return NS_OK;
+  }
 
   if (!selectionChangeData.mCausedBySelectionEvent) {
     
