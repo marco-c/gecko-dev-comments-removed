ffcc8ebda222ad35ed34eb92fca80fa2976be9ad
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 02:02:15 +0000

    Bug 1201869 - Part 0: Rename MNewArray::count to MNewArray::length. r=Waldo
    UltraBlame original commit: 4a1c66651bf02ac2f9c3cfc050d50f0fd06e2f28

--- js/src/jit/MIR.h
+++ js/src/jit/MIR.h
@@ -2906,7 +2906,7 @@ class MNewArray
 {
   private:
     
-    uint32_t count_;
+    uint32_t length_;
 
     
     gc::InitialHeap initialHeap_;
@@ -2916,21 +2916,21 @@ class MNewArray
 
     jsbytecode* pc_;
 
-    MNewArray(CompilerConstraintList* constraints, uint32_t count, MConstant* templateConst,
+    MNewArray(CompilerConstraintList* constraints, uint32_t length, MConstant* templateConst,
               gc::InitialHeap initialHeap, jsbytecode* pc);
 
   public:
     INSTRUCTION_HEADER(NewArray)
 
     static MNewArray* New(TempAllocator& alloc, CompilerConstraintList* constraints,
-                          uint32_t count, MConstant* templateConst,
+                          uint32_t length, MConstant* templateConst,
                           gc::InitialHeap initialHeap, jsbytecode* pc)
     {
-        return new(alloc) MNewArray(constraints, count, templateConst, initialHeap, pc);
+        return new(alloc) MNewArray(constraints, length, templateConst, initialHeap, pc);
     }
 
-    uint32_t count() const {
-        return count_;
+    uint32_t length() const {
+        return length_;
     }
 
     JSObject* templateObject() const {
