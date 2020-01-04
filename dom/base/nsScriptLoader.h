8758c24bd55c29d5a015be321b0aa3a3632d4215
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 01:52:15 +0000

    Bug 1048048 - add preload content policy types for scripts (r=baku)
    UltraBlame original commit: 450d4a13c90e95a58caa398e4fcc0a71f4800071

--- dom/base/nsScriptLoader.h
+++ dom/base/nsScriptLoader.h
@@ -354,7 +354,8 @@ public:
   static nsresult ShouldLoadScript(nsIDocument* aDocument,
                                    nsISupports* aContext,
                                    nsIURI* aURI,
-                                   const nsAString &aType);
+                                   const nsAString &aType,
+                                   bool aIsPreLoad);
 
   
 
@@ -435,7 +436,8 @@ private:
   static nsresult CheckContentPolicy(nsIDocument* aDocument,
                                      nsISupports *aContext,
                                      nsIURI *aURI,
-                                     const nsAString &aType);
+                                     const nsAString &aType,
+                                     bool aIsPreLoad);
 
   
 
