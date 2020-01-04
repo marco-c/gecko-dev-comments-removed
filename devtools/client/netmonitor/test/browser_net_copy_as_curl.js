3947916122ad9692924c33ea084bb17deee3ae5f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 21:49:45 +0000

    Bug 1269102 - Part 5: Cleanup browser_net_copy_as_curl.js, reenable for e10s r=ochameau
    MozReview-Commit-ID: IQPCafO4zGG
    
    UltraBlame original commit: 123a3bcfafc190402286b948a64be849bbd798a5

--- devtools/client/netmonitor/test/browser_net_copy_as_curl.js
+++ devtools/client/netmonitor/test/browser_net_copy_as_curl.js
@@ -1,106 +1,88 @@
 
 
 
+"use strict";
 
 
 
 
-function test() {
-  initNetMonitor(CURL_URL).then(([aTab, aDebuggee, aMonitor]) => {
-    info("Starting test... ");
 
-    const EXPECTED_POSIX_RESULT = [
-      "curl",
-      "'" + SIMPLE_SJS + "'",
-      "-H 'Host: example.com'",
-      "-H 'User-Agent: " + navigator.userAgent + "'",
-      "-H 'Accept: */*'",
-      "-H 'Accept-Language: " + navigator.language + "'",
-      "--compressed",
-      "-H 'X-Custom-Header-1: Custom value'",
-      "-H 'X-Custom-Header-2: 8.8.8.8'",
-      "-H 'X-Custom-Header-3: Mon, 3 Mar 2014 11:11:11 GMT'",
-      "-H 'Referer: " + CURL_URL + "'",
-      "-H 'Connection: keep-alive'",
-      "-H 'Pragma: no-cache'",
-      "-H 'Cache-Control: no-cache'"
-    ].join(" ");
+add_task(function* () {
+  let [tab, , monitor] = yield initNetMonitor(CURL_URL);
+  info("Starting test... ");
 
-    const EXPECTED_WIN_RESULT = [
-      "curl",
-      '"' + SIMPLE_SJS + '"',
-      '-H "Host: example.com"',
-      '-H "User-Agent: ' + navigator.userAgent + '"',
-      '-H "Accept: */*"',
-      '-H "Accept-Language: ' + navigator.language + '"',
-      "--compressed",
-      '-H "X-Custom-Header-1: Custom value"',
-      '-H "X-Custom-Header-2: 8.8.8.8"',
-      '-H "X-Custom-Header-3: Mon, 3 Mar 2014 11:11:11 GMT"',
-      '-H "Referer: ' + CURL_URL + '"',
-      '-H "Connection: keep-alive"',
-      '-H "Pragma: no-cache"',
-      '-H "Cache-Control: no-cache"'
-    ].join(" ");
+  
+  const QUOTE = Services.appinfo.OS == "WINNT" ? "\"" : "'";
 
-    const EXPECTED_RESULT = Services.appinfo.OS == "WINNT"
-      ? EXPECTED_WIN_RESULT
-      : EXPECTED_POSIX_RESULT;
+  
+  function quote(str) {
+    return QUOTE + str.replace(new RegExp(QUOTE, "g"), `\\${QUOTE}`) + QUOTE;
+  }
 
-    let { NetMonitorView } = aMonitor.panelWin;
-    let { RequestsMenu } = NetMonitorView;
+  
+  function header(h) {
+    return "-H " + quote(h);
+  }
 
-    RequestsMenu.lazyUpdate = false;
+  
+  const EXPECTED_RESULT = [
+    "curl " + quote(SIMPLE_SJS),
+    "--compressed",
+    header("Host: example.com"),
+    header("User-Agent: " + navigator.userAgent),
+    header("Accept: */*"),
+    header("Accept-Language: " + navigator.language),
+    header("X-Custom-Header-1: Custom value"),
+    header("X-Custom-Header-2: 8.8.8.8"),
+    header("X-Custom-Header-3: Mon, 3 Mar 2014 11:11:11 GMT"),
+    header("Referer: " + CURL_URL),
+    header("Connection: keep-alive"),
+    header("Pragma: no-cache"),
+    header("Cache-Control: no-cache")
+  ];
 
-    waitForNetworkEvents(aMonitor, 1).then(() => {
-      let requestItem = RequestsMenu.getItemAtIndex(0);
-      RequestsMenu.selectedItem = requestItem;
+  let { NetMonitorView } = monitor.panelWin;
+  let { RequestsMenu } = NetMonitorView;
+  RequestsMenu.lazyUpdate = false;
 
-      waitForClipboard(function validate(aResult) {
-        if (typeof aResult !== "string") {
-          return false;
-        }
-
-        
-        
-        
-        var expected = EXPECTED_RESULT.toString().match(/[-A-Za-z1-9]+ ([\"'])(?:\\\1|.)*?\1/g),
-            actual = aResult.match(/[-A-Za-z1-9]+ ([\"'])(?:\\\1|.)*?\1/g);
-
-        
-        if (!actual || expected[0] != actual[0]) {
-          return false;
-        }
-
-        
-        var expectedSet = new Set(expected);
-        var actualSet = new Set(actual);
-        if (expected.size != actual.size) {
-          return false;
-        }
-        for (let param of expectedSet) {
-          if (!actualSet.has(param)) {
-            return false;
-          }
-        }
-
-        return true;
-      }, function setup() {
-        RequestsMenu.copyAsCurl();
-      }, function onSuccess() {
-        ok(true, "Clipboard contains a cURL command for the currently selected item's url.");
-        cleanUp();
-      }, function onFailure() {
-        ok(false, "Creating a cURL command for the currently selected item was unsuccessful.");
-        cleanUp();
-      });
-
-    });
-
-    aDebuggee.performRequest(SIMPLE_SJS);
-
-    function cleanUp() {
-      teardown(aMonitor).then(finish);
-    }
+  let wait = waitForNetworkEvents(monitor, 1);
+  yield ContentTask.spawn(tab.linkedBrowser, SIMPLE_SJS, function* (url) {
+    content.wrappedJSObject.performRequest(url);
   });
-}
+  yield wait;
+
+  let requestItem = RequestsMenu.getItemAtIndex(0);
+  RequestsMenu.selectedItem = requestItem;
+
+  yield waitForClipboardPromise(function setup() {
+    RequestsMenu.copyAsCurl();
+  }, function validate(result) {
+    if (typeof result !== "string") {
+      return false;
+    }
+
+    
+    
+    
+
+    
+    
+    
+    let matchRe = /[-A-Za-z1-9]+(?: ([\"'])(?:\\\1|.)*?\1)?/g;
+
+    let actual = result.match(matchRe);
+
+    
+    if (!actual || EXPECTED_RESULT[0] != actual[0]) {
+      return false;
+    }
+
+    
+    return EXPECTED_RESULT.length === actual.length &&
+      EXPECTED_RESULT.every(param => actual.includes(param));
+  });
+
+  info("Clipboard contains a cURL command for the currently selected item's url.");
+
+  yield teardown(monitor);
+});
