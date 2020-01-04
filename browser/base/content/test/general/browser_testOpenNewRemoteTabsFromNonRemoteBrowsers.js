144b15bf78977d44a6bc9d389057ffa3f34a90f3
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 20:20:52 +0000

    Bug 1261842 - Make browser_testOpenNewRemoteTabsFromNonRemoteBrowsers.js wait for windows to be ready before sending them places. r=Gijs
    MozReview-Commit-ID: 3sunq7zrr0e
    
    UltraBlame original commit: cb6168f1e8410d7beb56757f007297094e14a713

--- browser/base/content/test/general/browser_testOpenNewRemoteTabsFromNonRemoteBrowsers.js
+++ browser/base/content/test/general/browser_testOpenNewRemoteTabsFromNonRemoteBrowsers.js
@@ -38,13 +38,13 @@ registerCleanupFunction(() => {
 
 
 add_task(function* test_new_tab() {
-  let normalWindow = yield promiseOpenAndLoadWindow({
-    remote: true
-  }, true);
-  let privateWindow = yield promiseOpenAndLoadWindow({
+  let normalWindow = yield BrowserTestUtils.openNewBrowserWindow({
+    remote: true,
+  });
+  let privateWindow = yield BrowserTestUtils.openNewBrowserWindow({
     remote: true,
     private: true,
-  }, true);
+  });
 
   for (let testWindow of [normalWindow, privateWindow]) {
     yield promiseWaitForFocus(testWindow);
@@ -80,10 +80,10 @@ add_task(function* test_new_tab() {
 
 
 add_task(function* test_new_window() {
-  let normalWindow = yield promiseOpenAndLoadWindow({
+  let normalWindow = yield BrowserTestUtils.openNewBrowserWindow({
     remote: true
   }, true);
-  let privateWindow = yield promiseOpenAndLoadWindow({
+  let privateWindow = yield BrowserTestUtils.openNewBrowserWindow({
     remote: true,
     private: true,
   }, true);
