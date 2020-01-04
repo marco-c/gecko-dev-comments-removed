c99d46269e0debd573ffd22c511f32554cc1cb1b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 04:46:08 +0000

    Bug 1220246 - fix test_geodefaults.js to avoid failing when the http server takes more than 1000ms to reply, r=Gijs.
    UltraBlame original commit: c921afa9fc3e164ab52dc4564349a92e16663317

--- toolkit/components/search/tests/xpcshell/test_geodefaults.js
+++ toolkit/components/search/tests/xpcshell/test_geodefaults.js
@@ -67,16 +67,10 @@ add_task(function* no_request_if_prefed_off() {
   Services.prefs.setBoolPref("browser.search.geoSpecificDefaults", false);
   yield asyncInit();
   checkNoRequest();
+  yield promiseAfterCache();
 
   
   do_check_eq(Services.search.currentEngine.name, getDefaultEngineName(false));
-
-  
-  
-  let commitPromise = promiseAfterCache();
-  Services.search.currentEngine = Services.search.getEngineByName(kTestEngineName);
-  Services.search.resetToOriginalDefaultEngine();
-  yield commitPromise;
 
   
   let metadata = yield promiseGlobalMetadata();
