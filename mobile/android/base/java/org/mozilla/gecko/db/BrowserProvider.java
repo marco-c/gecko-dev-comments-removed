c8fbc2e51237c5c9040b1b8c6b969f9ac37305be
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 10:30:34 +0000

    Bug 760956 - Part 4: Implement single-cursor topsite-retrieval in BrowserProvider r=rnewman
    Note that this version only returns topsites, pinned sites, and suggested sites.
    Blank tiles aren't supplied, and need to be added separately.
    
    MozReview-Commit-ID: 5Vc5pXTewHi
    
    UltraBlame original commit: 613f1eb88443821b3a7c3787d14e1f0238e4a4d0

--- mobile/android/base/java/org/mozilla/gecko/db/BrowserProvider.java
+++ mobile/android/base/java/org/mozilla/gecko/db/BrowserProvider.java
@@ -10,6 +10,8 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.mozilla.gecko.AboutPages;
+import org.mozilla.gecko.GeckoProfile;
 import org.mozilla.gecko.db.BrowserContract.Bookmarks;
 import org.mozilla.gecko.db.BrowserContract.Combined;
 import org.mozilla.gecko.db.BrowserContract.FaviconColumns;
@@ -18,6 +20,7 @@ import org.mozilla.gecko.db.BrowserContract.History;
 import org.mozilla.gecko.db.BrowserContract.Schema;
 import org.mozilla.gecko.db.BrowserContract.Tabs;
 import org.mozilla.gecko.db.BrowserContract.Thumbnails;
+import org.mozilla.gecko.db.BrowserContract.TopSites;
 import org.mozilla.gecko.db.BrowserContract.UrlAnnotations;
 import org.mozilla.gecko.db.DBUtils.UpdateOperation;
 import org.mozilla.gecko.sync.Utils;
@@ -32,6 +35,7 @@ import android.database.Cursor;
 import android.database.DatabaseUtils;
 import android.database.MatrixCursor;
 import android.database.SQLException;
+import android.database.sqlite.SQLiteCursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteQueryBuilder;
 import android.net.Uri;
@@ -101,6 +105,8 @@ public class BrowserProvider extends SharedBrowserDatabaseProvider {
     static final int THUMBNAIL_ID = 801;
 
     static final int URL_ANNOTATIONS = 900;
+
+    static final int TOPSITES = 1000;
 
     static final String DEFAULT_BOOKMARKS_SORT_ORDER = Bookmarks.TYPE
             + " ASC, " + Bookmarks.POSITION + " ASC, " + Bookmarks._ID
@@ -240,6 +246,9 @@ public class BrowserProvider extends SharedBrowserDatabaseProvider {
                 URI_MATCHER.addURI(BrowserContract.AUTHORITY, type.name, type.id);
             }
         }
+
+        
+        URI_MATCHER.addURI(BrowserContract.AUTHORITY, "topsites", TOPSITES);
     }
 
     
@@ -663,11 +672,229 @@ public class BrowserProvider extends SharedBrowserDatabaseProvider {
         return updated;
     }
 
+    private Cursor getTopSites(final Uri uri) {
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
+        final SQLiteDatabase db = getWritableDatabase(uri);
+
+        final String TABLE_TOPSITES = "topsites";
+
+        final String totalLimit = uri.getQueryParameter(BrowserContract.PARAM_LIMIT);
+        final String suggestedGridLimit = uri.getQueryParameter(BrowserContract.PARAM_SUGGESTEDSITES_LIMIT);
+
+        final String[] suggestedGridLimitArgs = new String[] {
+                suggestedGridLimit
+        };
+
+        final String[] totalLimitArgs = new String[] {
+                totalLimit
+        };
+
+        final String pinnedSitesFromClause = "FROM " + TABLE_BOOKMARKS + " WHERE " + Bookmarks.PARENT + " == ?";
+        final String[] pinnedSitesArgs = new String[] {
+                String.valueOf(Bookmarks.FIXED_PINNED_LIST_ID)
+        };
+
+        
+        
+        
+        
+        
+        
+        
+        final String freeIDSubquery =
+                " SELECT count(free_ids.position) + 1 AS rowid, numbers.position AS " + Bookmarks.POSITION +
+                " FROM (SELECT position FROM numbers WHERE position NOT IN (SELECT " + Bookmarks.POSITION + " " + pinnedSitesFromClause + ")) AS numbers" +
+                " LEFT OUTER JOIN " +
+                " (SELECT position FROM numbers WHERE position NOT IN (SELECT " + Bookmarks.POSITION + " " + pinnedSitesFromClause + ")) AS free_ids" +
+                " ON numbers.position > free_ids.position" +
+                " GROUP BY numbers.position" +
+                " ORDER BY numbers.position ASC" +
+                " LIMIT ?";
+
+        final String[] freeIDArgs = DBUtils.concatenateSelectionArgs(
+                pinnedSitesArgs,
+                pinnedSitesArgs,
+                suggestedGridLimitArgs);
+
+        
+        
+        final String ignoreForTopSitesWhereClause =
+                "(" + Combined.HISTORY_ID + " IS NOT -1)" +
+                " AND " +
+                Combined.URL + " NOT IN (SELECT " +
+                Bookmarks.URL + " FROM bookmarks WHERE " +
+                DBUtils.qualifyColumn("bookmarks", Bookmarks.PARENT) + " < ? AND " +
+                DBUtils.qualifyColumn("bookmarks", Bookmarks.IS_DELETED) + " == 0)" +
+                " AND " +
+                "(" + Combined.URL + " NOT LIKE ?)";
+
+        final String[] ignoreForTopSitesArgs = new String[] {
+                String.valueOf(Bookmarks.FIXED_ROOT_ID),
+                AboutPages.URL_FILTER
+        };
+
+
+        
+        
+        final SuggestedSites suggestedSites = GeckoProfile.get(getContext(), uri.getQueryParameter(BrowserContract.PARAM_PROFILE)).getDB().getSuggestedSites();
+
+        StringBuilder suggestedSitesBuilder = new StringBuilder();
+        
+        
+        
+        
+        final Cursor suggestedSitesCursor = suggestedSites.get(Integer.parseInt(suggestedGridLimit));
+
+        String[] suggestedSiteArgs = new String[0];
+
+        boolean firstClause = true;
+
+        final int idColumnIndex = suggestedSitesCursor.getColumnIndexOrThrow(Bookmarks._ID);
+        final int urlColumnIndex = suggestedSitesCursor.getColumnIndexOrThrow(Bookmarks.URL);
+        final int titleColumnIndex = suggestedSitesCursor.getColumnIndexOrThrow(Bookmarks.TITLE);
+
+        while (suggestedSitesCursor.moveToNext()) {
+            
+            if (!firstClause) {
+                suggestedSitesBuilder.append(" UNION ALL");
+            } else {
+                firstClause = false;
+            }
+            suggestedSitesBuilder.append(" SELECT" +
+                                         " ? AS " + Bookmarks._ID + "," +
+                                         " ? AS " + Bookmarks.URL + "," +
+                                         " ? AS " + Bookmarks.TITLE);
+
+            suggestedSiteArgs = DBUtils.appendSelectionArgs(suggestedSiteArgs,
+                                                            new String[] {
+                                                                    suggestedSitesCursor.getString(idColumnIndex),
+                                                                    suggestedSitesCursor.getString(urlColumnIndex),
+                                                                    suggestedSitesCursor.getString(titleColumnIndex)
+                                                            });
+        }
+
+        
+        
+        
+        final String suggestedLimitClause = " LIMIT MAX(0, (? - (SELECT COUNT(*) FROM " + TABLE_TOPSITES + ") - (SELECT COUNT(*) " + pinnedSitesFromClause + "))) ";
+
+        final String[] suggestedLimitArgs = DBUtils.concatenateSelectionArgs(suggestedGridLimitArgs,
+                                                                             pinnedSitesArgs);
+
+        db.beginTransaction();
+        try {
+            db.execSQL("DROP TABLE IF EXISTS " + TABLE_TOPSITES);
+
+            db.execSQL("CREATE TEMP TABLE " + TABLE_TOPSITES + " AS" +
+                       " SELECT " +
+                       Bookmarks._ID + ", " +
+                       Bookmarks.URL + ", " +
+                       Bookmarks.TITLE + ", " +
+                       Combined.HISTORY_ID + ", " +
+                       TopSites.TYPE_TOP + " AS " + TopSites.TYPE +
+                       " FROM " + Combined.VIEW_NAME +
+                       " WHERE " + ignoreForTopSitesWhereClause +
+                       " ORDER BY " + BrowserContract.getFrecencySortOrder(true, false) +
+                       " LIMIT ?",
+
+                       DBUtils.appendSelectionArgs(ignoreForTopSitesArgs,
+                                                   totalLimitArgs));
+
+            db.execSQL("INSERT INTO " + TABLE_TOPSITES +
+                       
+                       
+                       
+                       " SELECT * FROM (SELECT " +
+                       Bookmarks._ID + ", " +
+                       Bookmarks.URL + ", " +
+                       Bookmarks.TITLE + ", " +
+                       "NULL AS " + Combined.HISTORY_ID + ", " +
+                       TopSites.TYPE_SUGGESTED + " as " + TopSites.TYPE +
+                       " FROM ( " + suggestedSitesBuilder.toString() + " )" +
+                       " WHERE " +
+                       Bookmarks.URL + " NOT IN (SELECT url FROM " + TABLE_TOPSITES + ")" +
+                       " AND " +
+                       Bookmarks.URL + " NOT IN (SELECT url " + pinnedSitesFromClause + ")" +
+                       suggestedLimitClause + " )",
+
+                       DBUtils.concatenateSelectionArgs(suggestedSiteArgs,
+                                                        pinnedSitesArgs,
+                                                        suggestedLimitArgs));
+
+            final SQLiteCursor c = (SQLiteCursor) db.rawQuery(
+                        "SELECT " +
+                        Bookmarks._ID + ", " +
+                        Bookmarks.URL + ", " +
+                        Bookmarks.TITLE + ", " +
+                        Bookmarks.POSITION + ", " +
+                        Combined.HISTORY_ID + ", " +
+                        TopSites.TYPE +
+                        " FROM " + TABLE_TOPSITES +
+                        " LEFT OUTER JOIN " + 
+                        "(" + freeIDSubquery + ") AS id_results" +
+                        " ON " + DBUtils.qualifyColumn(TABLE_TOPSITES, "rowid") +
+                        " = " + DBUtils.qualifyColumn("id_results", "rowid") +
+
+                        " UNION ALL " +
+
+                        "SELECT " +
+                        Bookmarks._ID + ", " +
+                        Bookmarks.URL + ", " +
+                        Bookmarks.TITLE + ", " +
+                        Bookmarks.POSITION + ", " +
+                        "NULL AS " + Combined.HISTORY_ID + ", " +
+                        TopSites.TYPE_PINNED + " as " + TopSites.TYPE +
+                        " FROM " + TABLE_BOOKMARKS +
+                        " WHERE " + Bookmarks.PARENT + " == ? " +
+
+                        " ORDER BY " + Bookmarks.POSITION,
+
+                         DBUtils.appendSelectionArgs(freeIDArgs,
+                                                     pinnedSitesArgs));
+
+            c.setNotificationUri(getContext().getContentResolver(),
+                                 BrowserContract.AUTHORITY_URI);
+
+            
+            
+            
+            
+            
+            c.moveToFirst();
+
+            db.setTransactionSuccessful();
+            return c;
+        } finally {
+            db.endTransaction();
+        }
+    }
+
     @Override
     public Cursor query(Uri uri, String[] projection, String selection,
             String[] selectionArgs, String sortOrder) {
-        SQLiteDatabase db = getReadableDatabase(uri);
         final int match = URI_MATCHER.match(uri);
+
+        if (match == TOPSITES) {
+            return getTopSites(uri);
+        }
+
+        SQLiteDatabase db = getReadableDatabase(uri);
 
         SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
         String limit = uri.getQueryParameter(BrowserContract.PARAM_LIMIT);
