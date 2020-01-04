8b78dfc420ef8519f0dac76c0162f6b4416d7232
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 20:57:15 +0000

    Bug 1290156 - Remove the DEBUG-only lockOwner member from vixl::Simulator; r=terrence
    The checks that the `lockOwner` member was used for are redundant now that
    `js::Mutex` uses `PTHREAD_MUTEX_ERRORCHECK` in `DEBUG` builds.
    
    UltraBlame original commit: 41096329fbed09dd0b344483f896068cc8c802eb

--- js/src/jit/arm64/vixl/Simulator-vixl.h
+++ js/src/jit/arm64/vixl/Simulator-vixl.h
@@ -2668,9 +2668,6 @@ class Simulator : public DecoderVisitor {
  protected:
   
   js::Mutex lock_;
-#ifdef DEBUG
-  PRThread* lockOwner_;
-#endif
   Redirection* redirection_;
   mozilla::Vector<int64_t, 0, js::SystemAllocPolicy> spStack_;
 };
