14eedd25f838856ba8ad62eeefbdbd5ff4fe806e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 09:45:22 +0000

    Bug 1216001 part 1 - Optimize nsRange::IsNodeSelected.  r=bz
    UltraBlame original commit: 2e047065c2912e930e294eaccef64e4012794e4c

--- dom/base/nsRange.h
+++ dom/base/nsRange.h
@@ -252,6 +252,14 @@ public:
                                      bool *outNodeBefore,
                                      bool *outNodeAfter);
 
+  
+
+
+
+
+
+
+
   static bool IsNodeSelected(nsINode* aNode, uint32_t aStartOffset,
                              uint32_t aEndOffset);
 
@@ -297,6 +305,14 @@ protected:
 
 
   nsINode* GetRegisteredCommonAncestor();
+
+  
+  static bool IsNodeInSortedRanges(nsINode* aNode,
+                                   uint32_t aStartOffset,
+                                   uint32_t aEndOffset,
+                                   const nsTArray<const nsRange*>& aRanges,
+                                   size_t aRangeStart,
+                                   size_t aRangeEnd);
 
   struct MOZ_STACK_CLASS AutoInvalidateSelection
   {
