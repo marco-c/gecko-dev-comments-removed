6bb6c6f269cbc8d42010a35725b167f9a19e1a79
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 14:17:29 +0000

    Bug 1260725 - Handle and report OOM in census breakdown parsing; r=jorendorff a=kwierso
    This commit adds OOM checks that were missing to
    `GetDefaultBreakdown`. Additionally, it replaces uses of `js_new<T>` with
    `cx->new_<T>` so that OOMs are automatically reported on the context.
    
    MozReview-Commit-ID: KUqWdjSBLKn
    
    UltraBlame original commit: 7614fc1c6285d44b94388f45e7dafb79d8314d89

---
+++ js/src/jit-test/tests/debug/bug-1260725.js
@@ -0,0 +1,12 @@
+
+
+if (!('oomTest' in this))
+  throw new Error("out of memory");
+
+var dbg = new Debugger;
+dbg.onNewGlobalObject = function(global) {
+  dbg.memory.takeCensus({});
+};
+oomTest(function() {
+  newGlobal({})
+});
