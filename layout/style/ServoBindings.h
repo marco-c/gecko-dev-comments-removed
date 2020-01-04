9eafd0b3e2914b3f30f117e6ad085eac5fa21fb4
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 16:58:40 +0000

    Bug 1273838 - Part 3: Pass sheet base/referrer/principal to Servo_StylesheetFromUTF8Bytes. r=bholley
    UltraBlame original commit: 3039198867c163ef94164d98d891d1cab8f45de2

--- layout/style/ServoBindings.h
+++ layout/style/ServoBindings.h
@@ -111,8 +111,12 @@ NS_DECL_HOLDER_FFI_REFCOUNTING(nsIURI, URI)
 
 
 
-RawServoStyleSheet* Servo_StylesheetFromUTF8Bytes(const uint8_t* bytes, uint32_t length,
-                                                  mozilla::css::SheetParsingMode parsing_mode);
+RawServoStyleSheet* Servo_StylesheetFromUTF8Bytes(
+    const uint8_t* bytes, uint32_t length,
+    mozilla::css::SheetParsingMode parsing_mode,
+    ThreadSafeURIHolder* base,
+    ThreadSafeURIHolder* referrer,
+    ThreadSafePrincipalHolder* principal);
 void Servo_AddRefStyleSheet(RawServoStyleSheet* sheet);
 void Servo_ReleaseStyleSheet(RawServoStyleSheet* sheet);
 void Servo_AppendStyleSheet(RawServoStyleSheet* sheet, RawServoStyleSet* set);
