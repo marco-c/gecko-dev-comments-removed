9895e8d4de99f5dd5d045ef71e4c5e199ee60c70
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 00:03:47 +0000

    Bug 1193374 - Remove tab panel preview dimension dependency from TopSites r=mcomella
    UltraBlame original commit: 62a3a7f33f0915012ccfa3f948390ed5d53d8bd7

--- mobile/android/base/home/TopSitesThumbnailView.java
+++ mobile/android/base/home/TopSitesThumbnailView.java
@@ -8,35 +8,24 @@ package org.mozilla.gecko.home;
 import org.mozilla.gecko.R;
 import org.mozilla.gecko.ThumbnailHelper;
 import org.mozilla.gecko.util.ColorUtils;
-import org.mozilla.gecko.util.HardwareUtils;
+import org.mozilla.gecko.widget.CropImageView;
 
 import android.content.Context;
 import android.content.res.Resources;
-import android.graphics.Bitmap;
 import android.graphics.Canvas;
-import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.PorterDuff.Mode;
-import android.graphics.RectF;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
-import android.widget.ImageView;
 
 
 
 
-public class TopSitesThumbnailView extends ImageView {
+public class TopSitesThumbnailView extends CropImageView {
     private static final String LOGTAG = "GeckoTopSitesThumbnailView";
 
     
     private static final int COLOR_FILTER = 0x46FFFFFF;
-
-    
-    
-    
-    private final RectF mLayoutRect = new RectF();
-    private Matrix mLayoutCurrentMatrix = new Matrix();
-    private Matrix mLayoutNextMatrix = new Matrix();
 
     
     private final int mDefaultColor = ColorUtils.getColor(getContext(), R.color.top_site_default);
@@ -47,16 +36,11 @@ public class TopSitesThumbnailView extends ImageView {
     
     private final Paint mBorderPaint;
 
-    private boolean mResize = false;
-    private int mWidth;
-    private int mHeight;
-
     public TopSitesThumbnailView(Context context) {
         this(context, null);
 
         
         setWillNotDraw(false);
-
     }
 
     public TopSitesThumbnailView(Context context, AttributeSet attrs) {
@@ -73,69 +57,9 @@ public class TopSitesThumbnailView extends ImageView {
         mBorderPaint.setStyle(Paint.Style.STROKE);
     }
 
-    public void setImageBitmap(Bitmap bm, boolean resize) {
-        super.setImageBitmap(bm);
-        mResize = resize;
-        clearLayoutVars();
-
-        updateImageMatrix();
-    }
-
-    private void clearLayoutVars() {
-        mLayoutRect.setEmpty();
-    }
-
-    private void updateImageMatrix() {
-        if (!HardwareUtils.isTablet() || !mResize) {
-            return;
-        }
-
-        
-        if (mLayoutRect.right == mWidth && mLayoutRect.bottom == mHeight) {
-            return;
-        }
-
-        setScaleType(ScaleType.MATRIX);
-
-        mLayoutRect.set(0, 0, mWidth, mHeight);
-        mLayoutNextMatrix.setRectToRect(mLayoutRect, mLayoutRect, Matrix.ScaleToFit.CENTER);
-        setImageMatrix(mLayoutNextMatrix);
-
-        final Matrix swapReferenceMatrix = mLayoutCurrentMatrix;
-        mLayoutCurrentMatrix = mLayoutNextMatrix;
-        mLayoutNextMatrix = swapReferenceMatrix;
-    }
-
     @Override
-    public void setImageResource(int resId) {
-        super.setImageResource(resId);
-        mResize = false;
-    }
-
-    @Override
-    public void setImageDrawable(Drawable drawable) {
-        super.setImageDrawable(drawable);
-        mResize = false;
-    }
-
-    
-
-
-
-
-
-
-    @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-
-        
-        mWidth = getMeasuredWidth();
-        mHeight = (int) (mWidth * ThumbnailHelper.THUMBNAIL_ASPECT_RATIO);
-        setMeasuredDimension(mWidth, mHeight);
-
-        updateImageMatrix();
+    protected float getAspectRatio() {
+        return ThumbnailHelper.TOP_SITES_THUMBNAIL_ASPECT_RATIO;
     }
 
     
