129c44e8daefdf658e4ede5bf784097521efa3d3
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 10:26:41 +0000

    Bug 1251045 part 1.  Make it possible to dispatch a ModifyBusyCountRunnable without a JSContext.  r=khuey
    UltraBlame original commit: 08e35133f87ec9d9039dd426e4a6dede7715d13a

--- dom/workers/WorkerPrivate.cpp
+++ dom/workers/WorkerPrivate.cpp
@@ -467,6 +467,14 @@ public:
     mIncrease(aIncrease)
   { }
 
+  
+  
+  bool
+  Dispatch()
+  {
+    return WorkerControlRunnable::Dispatch(nullptr);
+  }
+
 private:
   virtual bool
   WorkerRun(JSContext* aCx, WorkerPrivate* aWorkerPrivate) override
@@ -5101,7 +5109,7 @@ WorkerPrivate::ModifyBusyCountFromWorker(JSContext* aCx, bool aIncrease)
 
   RefPtr<ModifyBusyCountRunnable> runnable =
     new ModifyBusyCountRunnable(this, aIncrease);
-  return runnable->Dispatch(aCx);
+  return runnable->Dispatch();
 }
 
 bool
