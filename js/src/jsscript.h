d91e2363b0bb1b886009c84efbc686f1d746e86b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 02:48:34 +0000

    Bug 1185106 - Part 3: Add parser support for Async functions. (r=efaust)
    UltraBlame original commit: 5174ef291f0cf523fe48a5863596543dc33d6204

--- js/src/jsscript.h
+++ js/src/jsscript.h
@@ -873,6 +873,8 @@ class ScriptSourceObject : public NativeObject
 
 enum GeneratorKind { NotGenerator, LegacyGenerator, StarGenerator };
 
+enum FunctionAsyncKind { SyncFunction, AsyncFunction };
+
 static inline unsigned
 GeneratorKindAsBits(GeneratorKind generatorKind) {
     return static_cast<unsigned>(generatorKind);
@@ -1167,6 +1169,8 @@ class JSScript : public js::gc::TenuredCell
 
     bool isDerivedClassConstructor_:1;
 
+    bool isAsync_:1;
+
     
     
   protected:
@@ -1431,6 +1435,12 @@ class JSScript : public js::gc::TenuredCell
         
         MOZ_ASSERT(!isGenerator());
         generatorKindBits_ = GeneratorKindAsBits(kind);
+    }
+
+    js::FunctionAsyncKind asyncKind() const { return isAsync_ ? js::AsyncFunction : js::SyncFunction; }
+
+    void setAsyncKind(js::FunctionAsyncKind kind) {
+        isAsync_ = kind == js::AsyncFunction;
     }
 
     void setNeedsHomeObject() {
@@ -2099,11 +2109,11 @@ class LazyScript : public gc::TenuredCell
         
         uint32_t version : 8;
 
-        uint32_t numFreeVariables : 24;
+        uint32_t numFreeVariables : 23;
         uint32_t numInnerFunctions : 20;
 
         uint32_t generatorKindBits : 2;
-
+        uint32_t isAsync : 1;
         
         
         
@@ -2215,6 +2225,10 @@ class LazyScript : public gc::TenuredCell
     }
 
     GeneratorKind generatorKind() const { return GeneratorKindFromBits(p_.generatorKindBits); }
+
+    FunctionAsyncKind asyncKind() const { return p_.isAsync ? AsyncFunction : SyncFunction; }
+
+    void setAsyncKind(FunctionAsyncKind kind) { p_.isAsync = kind == AsyncFunction; }
 
     bool isGenerator() const { return generatorKind() != NotGenerator; }
 
