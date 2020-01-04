016821dbb4760451e4026389d47baef3be0ddfd9
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 04:03:00 +0000

    Bug 1217061 - Report OOMs in IonCache::linkAndAttachStub(). r=djvj
    The method IonCache::linkAndAttachStub() can run out of memory in a few
    ways: When adding entries to the global jit code table, and when
    generating code through the macro assembler.
    
    Make sure to call ReportOutOfMemory() before returning false when that
    happens. Otherwise we won't get the right of error reported, and OOM
    simulation tests fail.
    
    The Linker already calls ReportOutOfMemory(), so we don't need to
    handle those calls.
    
    UltraBlame original commit: 08493e40a4ad44fb3d05d4c19ab8fd923a8de737

--- js/src/jit/IonCaches.cpp
+++ js/src/jit/IonCaches.cpp
@@ -350,6 +350,7 @@ IonCache::linkAndAttachStub(JSContext* cx, MacroAssembler& masm, StubAttacher& a
         JitcodeGlobalTable* globalTable = cx->runtime()->jitRuntime()->getJitcodeGlobalTable();
         if (!globalTable->addEntry(entry, cx->runtime())) {
             entry.destroy();
+            ReportOutOfMemory(cx);
             return false;
         }
 
@@ -363,11 +364,19 @@ IonCache::linkAndAttachStub(JSContext* cx, MacroAssembler& masm, StubAttacher& a
         JitcodeGlobalTable* globalTable = cx->runtime()->jitRuntime()->getJitcodeGlobalTable();
         if (!globalTable->addEntry(entry, cx->runtime())) {
             entry.destroy();
+            ReportOutOfMemory(cx);
             return false;
         }
 
         
         code->setHasBytecodeMap();
+    }
+
+    
+    
+    if (masm.oom()) {
+        ReportOutOfMemory(cx);
+        return false;
     }
 
     return true;
