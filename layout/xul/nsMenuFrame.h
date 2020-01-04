b4020e648e69c0b8a91467e521c07910df1f99a2
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 15:14:59 +0000

    Bug 1053986 - Rename nsIFrame::SetDebug to SetXULDebug.  r=dholbert
    Written purely with sed, over .h and .cpp files in layout/.
    
    MozReview-Commit-ID: KQYtO7JS2Zm
    
    UltraBlame original commit: d137bd89989d30dae2d5b9c724e4adf199100d92

--- layout/xul/nsMenuFrame.h
+++ layout/xul/nsMenuFrame.h
@@ -89,7 +89,7 @@ public:
                     nsIFrame*         aPrevInFlow) override;
 
 #ifdef DEBUG_LAYOUT
-  virtual nsresult SetDebug(nsBoxLayoutState& aState, bool aDebug) override;
+  virtual nsresult SetXULDebug(nsBoxLayoutState& aState, bool aDebug) override;
 #endif
 
   
@@ -263,7 +263,7 @@ protected:
 
 protected:
 #ifdef DEBUG_LAYOUT
-  nsresult SetDebug(nsBoxLayoutState& aState, nsIFrame* aList, bool aDebug);
+  nsresult SetXULDebug(nsBoxLayoutState& aState, nsIFrame* aList, bool aDebug);
 #endif
   nsresult Notify(nsITimer* aTimer);
 
