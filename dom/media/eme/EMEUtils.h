55f9a5952521ceb8f6807f88c137747bbba197e4
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 00:25:49 +0000

    Bug 1176145 - Expose CDM version number in keySystem string. r=edwin
    UltraBlame original commit: 0649b95bad43e703f4abe64937e751898ae17f0f

--- dom/media/eme/EMEUtils.h
+++ dom/media/eme/EMEUtils.h
@@ -54,6 +54,11 @@ bool ParseKeySystem(const nsAString& aKeySystem,
 void
 LogToBrowserConsole(const nsAString& aMsg);
 
+void
+ConstructKeySystem(const nsAString& aKeySystem,
+                   const nsAString& aCDMVersion,
+                   nsAString& aOutKeySystem);
+
 } 
 
 #endif 
