b76b79418125d20267e364520492f9f31751f9ed
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 05:27:54 +0000

    Bug 1140448 - Backout 5 changesets to backout top level shareplane.
    Changesets:
    * 13a6c5fb2f44
    * a9d3499bbec1
    * 35336ca96de2
    * 64e311403f05
    * 8b8598ed040e
    
    UltraBlame original commit: 1e789580b2065698615e61665dcfca42028f8d17

--- mobile/android/base/widget/ActivityChooserModel.java
+++ mobile/android/base/widget/ActivityChooserModel.java
@@ -773,9 +773,21 @@ public class ActivityChooserModel extends DataSetObservable {
                 
 
 
+
+
+
+
+
+
                 if (shareDialogClassName.equals(resolveInfo.activityInfo.name) &&
                         channelToRemoveLabel.equals(resolveInfo.loadLabel(packageManager))) {
-                    continue;
+                    
+                    if (!hasOtherSyncClients()) {
+                        continue;
+                    }
+
+                    resolveInfo.labelRes = R.string.overlay_share_send_other;
+                    resolveInfo.icon = R.drawable.icon_shareplane;
                 }
 
                 mActivities.add(new ActivityResolveInfo(resolveInfo));
