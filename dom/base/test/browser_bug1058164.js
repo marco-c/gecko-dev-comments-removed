00d9f9864b8f6b6611a267f7f7289eb91eb3c62a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 05:59:37 +0000

    Bug 1220752 - Fix and enable browser_bug1058164.js for e10s now that bug 918634 has landed. r=mrbkap
    This also makes the test more precise since it now checks the order of
    the pagehide and pageshow events.
    
    UltraBlame original commit: 3307a801862270294770042f6e9b19ff92ed5b24

--- dom/base/test/browser_bug1058164.js
+++ dom/base/test/browser_bug1058164.js
@@ -4,25 +4,7 @@
 
 "use strict";
 
-const kTimeout = 5000; 
-
-
-
-
-
-
-function frame_script() {
-  addEventListener("pageshow", (event) => {
-    if (event.persisted) {
-      sendAsyncMessage("test:pageshow");
-    }
-  });
-  addEventListener("pagehide", (event) => {
-    if (event.persisted) {
-      sendAsyncMessage("test:pagehide");
-    }
-  });
-}
+const PAGE = "data:text/html,<html><body>A%20regular,%20everyday,%20normal%20page.";
 
 
 
@@ -30,43 +12,43 @@ function frame_script() {
 
 
 
-function prepareForPageEvent(browser, eventType) {
-  return new Promise((resolve, reject) => {
-    let mm = browser.messageManager;
-
-    let timeoutId = setTimeout(() => {
-      ok(false, "Timed out waiting for " + eventType)
-      reject();
-    }, kTimeout);
-
-    mm.addMessageListener("test:" + eventType, function onSawEvent(message) {
-      mm.removeMessageListener("test:" + eventType, onSawEvent);
-      ok(true, "Saw " + eventType + " event in frame script.");
-      clearTimeout(timeoutId);
-      resolve();
-    });
-  });
-}
 
 
 
 
 
-function prepareForPageHideAndShow(browser) {
-  return Promise.all([prepareForPageEvent(browser, "pagehide"),
-                      prepareForPageEvent(browser, "pageshow")]);
-}
 
 
 
 
+function prepareForVisibilityEvents(browser, expectedOrder) {
+  return new Promise((resolve) => {
+    let order = [];
 
-function waitForLoad(aWindow) {
-  return new Promise((resolve, reject) => {
-    aWindow.addEventListener("load", function onLoad(aEvent) {
-      aWindow.removeEventListener("load", onLoad, true);
-      resolve();
-    }, true);
+    let checkSatisfied = () => {
+      if (order.length < expectedOrder.length) {
+        
+        return;
+      } else {
+        browser.removeEventListener("pagehide", eventListener);
+        browser.removeEventListener("pageshow", eventListener);
+
+        for (let i = 0; i < expectedOrder.length; ++i) {
+          is(order[i], expectedOrder[i], "Got expected event");
+        }
+        resolve();
+      }
+    };
+
+    let eventListener = (e) => {
+      if (e.persisted) {
+        order.push(e.type);
+        checkSatisfied();
+      }
+    };
+
+    browser.addEventListener("pagehide", eventListener);
+    browser.addEventListener("pageshow", eventListener);
   });
 }
 
@@ -77,30 +59,51 @@ function waitForLoad(aWindow) {
 
 add_task(function* test_swap_frameloader_pagevisibility_events() {
   
-  let tab = gBrowser.addTab();
+  let tab = gBrowser.addTab(PAGE);
   gBrowser.selectedTab = tab;
-  let mm = window.getGroupMessageManager("browsers");
-  mm.loadFrameScript("data:,(" + frame_script.toString() + ")();", true);
-  let browser = gBrowser.selectedBrowser;
+  let firstBrowser = tab.linkedBrowser;
+  yield BrowserTestUtils.browserLoaded(firstBrowser);
 
   
   let newWindow = gBrowser.replaceTabWithWindow(tab);
+
   
   
-  yield waitForLoad(newWindow);
-  let pageHideAndShowPromise = prepareForPageHideAndShow(newWindow.gBrowser.selectedBrowser);
+  yield BrowserTestUtils.waitForEvent(newWindow, "load");
+  let newWindowBrowser = newWindow.gBrowser.selectedBrowser;
+
   
-  yield pageHideAndShowPromise;
+  yield prepareForVisibilityEvents(newWindowBrowser, ["pagehide", "pageshow"]);
+
+  
 
   
   let newTab = gBrowser.addTab();
   gBrowser.selectedTab = newTab;
-  browser = newWindow.gBrowser.selectedBrowser;
-  pageHideAndShowPromise = prepareForPageHideAndShow(gBrowser.selectedBrowser);
-  gBrowser.swapBrowsersAndCloseOther(newTab, newWindow.gBrowser.selectedTab);
+  let emptyBrowser = newTab.linkedBrowser;
 
   
-  yield pageHideAndShowPromise;
+  
+  
+  
+  
+  
+  
+  yield new Promise((resolve) => {
+    emptyBrowser.addEventListener("pageshow", function onPageShow() {
+      emptyBrowser.removeEventListener("pageshow", onPageShow);
+      resolve();
+    });
+  });
+
+  
+  
+  let emptyBrowserPromise =
+    prepareForVisibilityEvents(emptyBrowser, ["pagehide", "pageshow"]);
+
+  gBrowser.swapBrowsersAndCloseOther(newTab, newWindow.gBrowser.selectedTab);
+
+  yield emptyBrowserPromise;
 
   gBrowser.removeTab(gBrowser.selectedTab);
 });
