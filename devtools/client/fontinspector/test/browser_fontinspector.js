338cae3622698020b5acff3dd02a9d142cc31728
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 07:21:43 +0000

    Bug 1206452 - Increase the timeout of browser_fontinspector.js
    UltraBlame original commit: 560eceda7ba08bd104379c631772a8a23345b8e3

--- devtools/client/fontinspector/test/browser_fontinspector.js
+++ devtools/client/fontinspector/test/browser_fontinspector.js
@@ -3,6 +3,8 @@
 
 "use strict";
 
+requestLongerTimeout(2);
+
 const TEST_URI = BASE_URI + "browser_fontinspector.html";
 const FONTS = [
   {name: "Ostrich Sans Medium", remote: true, url: BASE_URI + "ostrich-regular.ttf",
