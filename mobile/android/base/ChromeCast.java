9850c741f3bbce31293cf92c03987a312868bfa7
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 05:43:08 +0000

    Bug 1222907 - report media status to MediaCastingBar. r=mfinkle.
    UltraBlame original commit: 517cb8556db3215c4578bcbec09cdd0ce7abc2a3

--- mobile/android/base/ChromeCast.java
+++ mobile/android/base/ChromeCast.java
@@ -86,13 +86,23 @@ class ChromeCast implements GeckoMediaPlayer {
         @Override
         public void onStatusUpdated() {
             MediaStatus mediaStatus = remoteMediaPlayer.getMediaStatus();
-            boolean isPlaying = mediaStatus.getPlayerState() == MediaStatus.PLAYER_STATE_PLAYING;
 
-            
-            if (mediaStatus.getPlayerState() == MediaStatus.PLAYER_STATE_IDLE &&
-                mediaStatus.getIdleReason() == MediaStatus.IDLE_REASON_FINISHED) {
-
-                GeckoAppShell.sendEventToGecko(GeckoEvent.createBroadcastEvent("Casting:Stop", null));
+            switch (mediaStatus.getPlayerState()) {
+            case MediaStatus.PLAYER_STATE_PLAYING:
+                GeckoAppShell.sendEventToGecko(GeckoEvent.createBroadcastEvent("MediaPlayer:Playing", null));
+                break;
+            case MediaStatus.PLAYER_STATE_PAUSED:
+                GeckoAppShell.sendEventToGecko(GeckoEvent.createBroadcastEvent("MediaPlayer:Paused", null));
+                break;
+            case MediaStatus.PLAYER_STATE_IDLE:
+                
+                if (mediaStatus.getIdleReason() == MediaStatus.IDLE_REASON_FINISHED) {
+                    GeckoAppShell.sendEventToGecko(GeckoEvent.createBroadcastEvent("Casting:Stop", null));
+                }
+                break;
+            default:
+                
+                break;
             }
         }
 
