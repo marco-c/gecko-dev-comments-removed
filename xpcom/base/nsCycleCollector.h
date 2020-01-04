65d322e20cc238cbabf37d3d7b50455ad87c7d85
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 01:41:17 +0000

    Bug 1203840 - Trigger dirty pages purge after CC. r=njn,r=smaug,r=mccr8
    Jemalloc 4 purges dirty pages regularly during free() when the ratio of dirty
    pages compared to active pages is higher than 1 << lg_dirty_mult.  We set
    lg_dirty_mult in jemalloc_config to limit RSS usage, but it also has an impact
    on performance.
    
    So instead of enforcing a high ratio to force more pages being purged, we keep
    jemalloc's default ratio of 8, and force a regular purge of all dirty pages,
    after cycle collection.
    
    Keeping jemalloc's default ratio avoids cycle-collection-triggered purge to
    have to go through really all dirty pages when there are a lot, in which case
    the normal jemalloc purge during free() will already have kicked in. It also
    takes care of everything that doesn't run the cycle collector still having
    a level of purge, like plugins in the plugin-container.
    
    At the same time, since jemalloc_purge_freed_pages does nothing with jemalloc 4,
    repurpose the MEMORY_FREE_PURGED_PAGES_MS telemetry probe to track the time
    spent in this cycle-collector-triggered purge.
    
    UltraBlame original commit: 5559e86a2f3ba157d2bb45cc7bfb9f7a690c3327

--- xpcom/base/nsCycleCollector.h
+++ xpcom/base/nsCycleCollector.h
@@ -39,7 +39,8 @@ void nsCycleCollector_prepareForGarbageCollection();
 
 void nsCycleCollector_finishAnyCurrentCollection();
 
-void nsCycleCollector_dispatchDeferredDeletion(bool aContinuation = false);
+void nsCycleCollector_dispatchDeferredDeletion(bool aContinuation = false,
+                                               bool aPurge = false);
 bool nsCycleCollector_doDeferredDeletion();
 
 already_AddRefed<nsICycleCollectorLogSink> nsCycleCollector_createLogSink();
