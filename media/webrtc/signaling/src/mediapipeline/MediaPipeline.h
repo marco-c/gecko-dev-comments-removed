ea6f54712ef94651db4183a97aa4cd1c4190988c
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 14:24:55 +0000

    Bug 1208371 - Forward declare MediaStreamTrack in MediaPipeline.h. r=bwc
    MozReview-Commit-ID: C6PCMIEJVQD
    
    UltraBlame original commit: d599fc36575d5a02aca3b161728bce0bc8e2324b

--- media/webrtc/signaling/src/mediapipeline/MediaPipeline.h
+++ media/webrtc/signaling/src/mediapipeline/MediaPipeline.h
@@ -14,7 +14,6 @@
 #include "FakeMediaStreams.h"
 #else
 #include "DOMMediaStream.h"
-#include "MediaStreamTrack.h"
 #include "MediaStreamGraph.h"
 #include "VideoUtils.h"
 #endif
@@ -408,7 +407,7 @@ public:
   virtual void AttachToTrack(const std::string& track_id);
 
   
-  virtual bool IsVideo() const override { return !!domtrack_->AsVideoStreamTrack(); }
+  virtual bool IsVideo() const override;
 
 #if !defined(MOZILLA_EXTERNAL_LINKAGE)
   
@@ -420,15 +419,7 @@ public:
 #endif
 
   
-  virtual void DetachMedia() override {
-    ASSERT_ON_THREAD(main_thread_);
-    if (domtrack_) {
-      domtrack_->RemoveDirectListener(listener_);
-      domtrack_->RemoveListener(listener_);
-      domtrack_ = nullptr;
-    }
-    
-  }
+  virtual void DetachMedia() override;
 
   
   virtual nsresult TransportReady_s(TransportInfo &info) override;
