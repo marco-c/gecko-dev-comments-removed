460f4b1269275bce642c15f523f82adf970eba1f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 08:53:12 +0000

    Bug 1234355 - Part 2: Remove unused EME drmContentCDMNotSupported.64bit.message. r=spohl
    UltraBlame original commit: 3d625e289ebf1822b76d013ad17026456511d2cb

--- browser/base/content/browser-eme.js
+++ browser/base/content/browser-eme.js
@@ -102,9 +102,7 @@ var gEMEHandler = {
     if (notificationId == "drmContentCDMNotSupported" &&
         keySystem.startsWith("com.adobe")) {
       let os = Services.appinfo.OS.toLowerCase();
-      if (os.startsWith("win") && Services.appinfo.XPCOMABI.startsWith("x86_64")) {
-        msgId = msgPrefix + "64bit.message";
-      } else if (os.startsWith("linux") || os.startsWith("darwin")) {
+      if (os.startsWith("linux") || os.startsWith("darwin")) {
         msgId = msgPrefix + "unsupportedOS.message";
         labelParams.splice(1, 0, os.startsWith("linux") ? "Linux" : "Mac OS X");
       }
@@ -171,7 +169,6 @@ var gEMEHandler = {
     } else {
       document.getElementById(anchorId).removeAttribute("firstplay");
     }
-
 
     let mainAction = {
       label: gNavigatorBundle.getString(btnLabelId),
