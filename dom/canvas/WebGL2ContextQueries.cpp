ecefcd28e145b141ed0da6c5523b7239bd3edf4c
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 06:55:23 +0000

    Bug 1218881. Enforce queries' new availability semantics. r=jgilbert
    This adds a Runnable that we dispatch to the event loop so that we can make
    sure queries don't become available before they are supposed to.
    
    UltraBlame original commit: 9c29c0efe6914da335b79b239a0d1cd363cee988

--- dom/canvas/WebGL2ContextQueries.cpp
+++ dom/canvas/WebGL2ContextQueries.cpp
@@ -6,6 +6,8 @@
 #include "WebGL2Context.h"
 #include "GLContext.h"
 #include "WebGLQuery.h"
+#include "gfxPrefs.h"
+#include "nsThreadUtils.h"
 
 namespace mozilla {
 
@@ -255,6 +257,7 @@ WebGL2Context::EndQuery(GLenum target)
     }
 
     UpdateBoundQuery(target, nullptr);
+    NS_DispatchToCurrentThread(new WebGLQuery::AvailableRunnable(activeQuery));
 }
 
 already_AddRefed<WebGLQuery>
@@ -322,6 +325,11 @@ WebGL2Context::GetQueryParameter(JSContext*, WebGLQuery* query, GLenum pname,
 
 
         ErrorInvalidOperation("getQueryObject: `query` has never been active.");
+        return;
+    }
+
+    
+    if (!query->mCanBeAvailable && !gfxPrefs::WebGLImmediateQueries()) {
         return;
     }
 
