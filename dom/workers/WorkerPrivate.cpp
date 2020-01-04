fc4f3fa377612451ff15fcfc4ebf38d3f6d79623
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 10:41:46 +0000

    Bug 1252592.  JS warnings should not trigger error events on shared workers.  r=khuey
    UltraBlame original commit: c2fe2cdc11b19d5f29bdf4c7245e29f9fb060c21

--- dom/workers/WorkerPrivate.cpp
+++ dom/workers/WorkerPrivate.cpp
@@ -3148,6 +3148,14 @@ WorkerPrivateParent<Derived>::BroadcastErrorToSharedWorkers(
 {
   AssertIsOnMainThread();
 
+  if (JSREPORT_IS_WARNING(aFlags)) {
+    
+    
+    LogErrorToConsole(aMessage, aFilename, aLine, aLineNumber, aColumnNumber,
+                      aFlags, 0);
+    return;
+  }
+
   AutoTArray<RefPtr<SharedWorker>, 10> sharedWorkers;
   GetAllSharedWorkers(sharedWorkers);
 
