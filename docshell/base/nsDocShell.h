142c174e9340d0a395f2d1a105c3dd189a6da957
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 17:42:39 +0000

    Bug 1258033 - Part 1: Add IsTrackingProtectionOn for nsILoadContext. r=smaug
    MozReview-Commit-ID: 2eQzO6KDLyX
    
    UltraBlame original commit: e4a2e5f300998853d4f8ebd79422ccf3b250b436

--- docshell/base/nsDocShell.h
+++ docshell/base/nsDocShell.h
@@ -232,6 +232,7 @@ public:
   NS_IMETHOD GetUseRemoteTabs(bool*) override;
   NS_IMETHOD SetRemoteTabs(bool) override;
   NS_IMETHOD GetOriginAttributes(JS::MutableHandle<JS::Value>) override;
+  NS_IMETHOD IsTrackingProtectionOn(bool*) override;
 
   
   
