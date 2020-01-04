c01bc6a3d6de716d33b497e18ee3759900cef24e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 12:21:17 +0000

    Bug 1165052 - Part 4: Inline IsConstructor intrinsic. r=jandem
    UltraBlame original commit: fdbb1c215d783a85c27d71ed8bb832690962c948

--- js/src/jit/IonBuilder.h
+++ js/src/jit/IonBuilder.h
@@ -907,6 +907,7 @@ class IonBuilder
 
     
     InliningStatus inlineIsCallable(CallInfo& callInfo);
+    InliningStatus inlineIsConstructor(CallInfo& callInfo);
     InliningStatus inlineIsObject(CallInfo& callInfo);
     InliningStatus inlineToObject(CallInfo& callInfo);
     InliningStatus inlineToInteger(CallInfo& callInfo);
