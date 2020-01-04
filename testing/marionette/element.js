2dfd3a95198a84f34a85b81855602d8ab33f1e99
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 19:59:04 +0000

    Bug 1284232 - Marshal all HTML and JS collections; r=automatedtester
    This patch adds marshaling of HTMLFormControlsCollection,
    HTMLAllCollection, and HTMLOptionsCollection element collections to
    Marionette.
    
    It will allow us you to return from HTMLSelectElement.options,
    document.forms[0].elements, and document.all. This is in
    addition to the already supported document.querySelector
    (NodeList), document.getElementsBy* (HTMLCollection), and
    Array.from(ELEMENT...) collections.
    
    MozReview-Commit-ID: 71a65lZRn4S
    
    UltraBlame original commit: 7b175334ebc6fb4b898a35331d9fa04ed55ea3c7

--- testing/marionette/element.js
+++ testing/marionette/element.js
@@ -287,9 +287,10 @@ function find_(container, strategy, selector, searchFn, opts) {
         `Given ${strategy} expression "${selector}" is invalid: ${e}`);
   }
 
-  if (element.isElementCollection(res)) {
-    return res;
-  } else if (res) {
+  if (res) {
+    if (opts.all) {
+      return res;
+    }
     return [res];
   }
   return [];
@@ -577,14 +578,22 @@ function implicitlyWaitFor(func, timeout, interval = 100) {
         reject(e);
       }
 
-      
-      
-      
-      
-      
-      let col = element.isElementCollection(res);
-      if (((col && res.length > 0 ) || (!col && !!res)) ||
-          (startTime == endTime || new Date().getTime() >= endTime)) {
+      if (
+        
+        
+        (element.isCollection(res) && res.length > 0)
+
+        
+        
+        || (!element.isCollection(res) && !!res)
+
+        
+        
+        || startTime == endTime
+
+        
+        || new Date().getTime() >= endTime
+      ) {
         resolve(res);
       }
     };
@@ -605,19 +614,22 @@ function implicitlyWaitFor(func, timeout, interval = 100) {
   });
 }
 
-element.isElementCollection = function(seq) {
-  if (seq === null) {
-    return false;
+
+element.isCollection = function(seq) {
+  switch (Object.prototype.toString.call(seq)) {
+    case "[object Arguments]":
+    case "[object Array]":
+    case "[object FileList]":
+    case "[object HTMLAllCollection]":
+    case "[object HTMLCollection]":
+    case "[object HTMLFormControlsCollection]":
+    case "[object HTMLOptionsCollection]":
+    case "[object NodeList]":
+      return true;
+
+    default:
+      return false;
   }
-
-  const arrayLike = {
-    "[object Array]": 0,
-    "[object HTMLCollection]": 1,
-    "[object NodeList]": 2,
-  };
-
-  let typ = Object.prototype.toString.call(seq);
-  return typ in arrayLike;
 };
 
 element.makeWebElement = function(uuid) {
@@ -706,43 +718,40 @@ element.fromJson = function(
 
 
 element.toJson = function(obj, seenEls) {
-  switch (typeof obj) {
-    case "undefined":
-      return null;
+  let t = Object.prototype.toString.call(obj);
 
-    case "boolean":
-    case "number":
-    case "string":
-      return obj;
+  
+  if (t == "[object Undefined]" || t == "[object Null]") {
+    return null;
+  }
 
-    case "object":
-      if (obj === null) {
-        return obj;
+  
+  else if (t == "[object Boolean]" || t == "[object Number]" || t == "[object String]") {
+    return obj;
+  }
+
+  
+  else if (element.isCollection(obj)) {
+    return [...obj].map(el => element.toJson(el, seenEls));
+  }
+
+  
+  else if ("nodeType" in obj && obj.nodeType == 1) {
+    let uuid = seenEls.add(obj);
+    return {[element.Key]: uuid, [element.LegacyKey]: uuid};
+  }
+
+  
+  else {
+    let rv = {};
+    for (let prop in obj) {
+      try {
+        rv[prop] = element.toJson(obj[prop], seenEls);
+      } catch (e if (e.result == Cr.NS_ERROR_NOT_IMPLEMENTED)) {
+        logger.debug(`Skipping ${prop}: ${e.message}`);
       }
-
-      
-      else if (element.isElementCollection(obj)) {
-        return [...obj].map(el => element.toJson(el, seenEls));
-      }
-
-      
-      else if (obj.nodeType == 1) {
-        let uuid = seenEls.add(obj);
-        return {[element.Key]: uuid, [element.LegacyKey]: uuid};
-      }
-
-      
-      else {
-        let rv = {};
-        for (let prop in obj) {
-          try {
-            rv[prop] = element.toJson(obj[prop], seenEls);
-          } catch (e if (e.result == Cr.NS_ERROR_NOT_IMPLEMENTED)) {
-            logger.debug(`Skipping ${prop}: ${e.message}`);
-          }
-        }
-        return rv;
-      }
+    }
+    return rv;
   }
 };
 
