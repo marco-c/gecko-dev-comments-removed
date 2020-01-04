38471c88808a6d61b9a528c8ee666760d8871723
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 10:45:54 +0000

    Bug 1245053, NSS_3_23_RTM, only version numbers finalized, no code changes, DONTBUILD
    UltraBlame original commit: 5e135136e21ca2ea16b60d2ea6f76fabeaa44b7a

--- security/nss/lib/util/nssutil.h
+++ security/nss/lib/util/nssutil.h
@@ -19,12 +19,12 @@
 
 
 
-#define NSSUTIL_VERSION  "3.23 Beta"
+#define NSSUTIL_VERSION  "3.23"
 #define NSSUTIL_VMAJOR   3
 #define NSSUTIL_VMINOR   23
 #define NSSUTIL_VPATCH   0
 #define NSSUTIL_VBUILD   0
-#define NSSUTIL_BETA     PR_TRUE
+#define NSSUTIL_BETA     PR_FALSE
 
 SEC_BEGIN_PROTOS
 
