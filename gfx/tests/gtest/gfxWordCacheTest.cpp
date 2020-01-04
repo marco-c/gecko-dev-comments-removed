f51238d62dbfe006c701863b23815d4f8f138d23
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 01:03:40 +0000

    Bug 1203379 - Fix indentation of gfxWordCacheTest.cpp. r=jrmuizel.
    This file has an intriguing mix of 1, 2, 3 and 4 space indentation, which this
    patch fixes. The patch also fixes a small number of other style violations,
    such as long lines.
    
    UltraBlame original commit: 88f44d76266b67c9dbadaa826e8942554f60423b

--- gfx/tests/gtest/gfxWordCacheTest.cpp
+++ gfx/tests/gtest/gfxWordCacheTest.cpp
@@ -3,6 +3,7 @@
 
 
 
+
 #include "gtest/gtest.h"
 
 #include "mozilla/gfx/2D.h"
@@ -33,105 +34,108 @@ static FrameTextRunCache *gTextRuns = nullptr;
 
 class FrameTextRunCache final : public nsExpirationTracker<gfxTextRun,3> {
 public:
- enum { TIMEOUT_SECONDS = 10 };
- FrameTextRunCache()
-     : nsExpirationTracker<gfxTextRun,3>(TIMEOUT_SECONDS*1000) {}
- ~FrameTextRunCache() {
-   AgeAllGenerations();
- }
+  enum { TIMEOUT_SECONDS = 10 };
+  FrameTextRunCache()
+    : nsExpirationTracker<gfxTextRun,3>(TIMEOUT_SECONDS*1000) {}
+  ~FrameTextRunCache() {
+    AgeAllGenerations();
+  }
 
- void RemoveFromCache(gfxTextRun* aTextRun) {
-   if (aTextRun->GetExpirationState()->IsTracked()) {
-     RemoveObject(aTextRun);
-   }
- }
+  void RemoveFromCache(gfxTextRun* aTextRun) {
+    if (aTextRun->GetExpirationState()->IsTracked()) {
+      RemoveObject(aTextRun);
+    }
+  }
 
- 
- virtual void NotifyExpired(gfxTextRun* aTextRun) {
-   RemoveFromCache(aTextRun);
-   delete aTextRun;
- }
+  
+  virtual void NotifyExpired(gfxTextRun* aTextRun) {
+    RemoveFromCache(aTextRun);
+    delete aTextRun;
+  }
 };
 
 static gfxTextRun *
-MakeTextRun(const char16_t *aText, uint32_t aLength,
-           gfxFontGroup *aFontGroup, const gfxFontGroup::Parameters* aParams,
-           uint32_t aFlags)
+MakeTextRun(const char16_t *aText, uint32_t aLength, gfxFontGroup *aFontGroup,
+            const gfxFontGroup::Parameters* aParams, uint32_t aFlags)
 {
-   nsAutoPtr<gfxTextRun> textRun;
-   if (aLength == 0) {
-       abort();
-       
-   } else if (aLength == 1 && aText[0] == ' ') {
-       abort();
-       
-   } else {
-       textRun = aFontGroup->MakeTextRun(aText, aLength, aParams, aFlags);
-   }
-   if (!textRun)
-       return nullptr;
-   nsresult rv = gTextRuns->AddObject(textRun);
-   if (NS_FAILED(rv)) {
-       gTextRuns->RemoveFromCache(textRun);
-       return nullptr;
-   }
-   return textRun.forget();
+  nsAutoPtr<gfxTextRun> textRun;
+  if (aLength == 0) {
+    abort();
+    
+  } else if (aLength == 1 && aText[0] == ' ') {
+    abort();
+    
+  } else {
+    textRun = aFontGroup->MakeTextRun(aText, aLength, aParams, aFlags);
+  }
+  if (!textRun) {
+    return nullptr;
+  }
+  nsresult rv = gTextRuns->AddObject(textRun);
+  if (NS_FAILED(rv)) {
+    gTextRuns->RemoveFromCache(textRun);
+    return nullptr;
+  }
+  return textRun.forget();
 }
 
 static already_AddRefed<gfxContext>
 MakeContext ()
 {
-   const int size = 200;
+  const int size = 200;
 
-    RefPtr<DrawTarget> drawTarget = gfxPlatform::GetPlatform()->
-        CreateOffscreenContentDrawTarget(IntSize(size, size),
-                                         SurfaceFormat::B8G8R8X8);
-    nsRefPtr<gfxContext> ctx = new gfxContext(drawTarget);
+  RefPtr<DrawTarget> drawTarget = gfxPlatform::GetPlatform()->
+      CreateOffscreenContentDrawTarget(IntSize(size, size),
+                                       SurfaceFormat::B8G8R8X8);
+  nsRefPtr<gfxContext> ctx = new gfxContext(drawTarget);
 
-    return ctx.forget();
+  return ctx.forget();
 }
 
 TEST(Gfx, WordCache) {
-   gTextRuns = new FrameTextRunCache();
+  gTextRuns = new FrameTextRunCache();
 
-   nsRefPtr<gfxContext> ctx = MakeContext();
-   {
-       gfxFontStyle style (mozilla::gfx::FontStyle::NORMAL,
-                           139,
-                           10.0,
-                           0,
-                           NS_NewPermanentAtom(NS_LITERAL_STRING("en")),
-                           0.0,
-                           false, false,
-                           NS_LITERAL_STRING(""));
+  nsRefPtr<gfxContext> ctx = MakeContext();
+  {
+    gfxFontStyle style(mozilla::gfx::FontStyle::NORMAL,
+                       139,
+                       10.0,
+                       0,
+                       NS_NewPermanentAtom(NS_LITERAL_STRING("en")),
+                       0.0,
+                       false, false,
+                       NS_LITERAL_STRING(""));
 
-       nsRefPtr<gfxFontGroup> fontGroup =
-           gfxPlatform::GetPlatform()->CreateFontGroup(NS_LITERAL_STRING("Geneva, MS Sans Serif, Helvetica,serif"), &style, nullptr);
+    nsRefPtr<gfxFontGroup> fontGroup =
+      gfxPlatform::GetPlatform()->CreateFontGroup(
+        NS_LITERAL_STRING("Geneva, MS Sans Serif, Helvetica,serif"), &style,
+        nullptr);
 
-       gfxTextRunFactory::Parameters params = {
-           ctx, nullptr, nullptr, nullptr, 0, 60
-       };
+    gfxTextRunFactory::Parameters params = {
+      ctx, nullptr, nullptr, nullptr, 0, 60
+    };
 
-       uint32_t flags = gfxTextRunFactory::TEXT_IS_PERSISTENT;
+    uint32_t flags = gfxTextRunFactory::TEXT_IS_PERSISTENT;
 
-       
-       const char cString[] = "\xd8\xaa\xd9\x85";
-       nsDependentCString cStr(cString);
-       NS_ConvertUTF8toUTF16 str(cStr);
-       gfxTextRun *tr = MakeTextRun(str.get(), str.Length(), fontGroup, &params, flags);
-       tr->GetAdvanceWidth(0, str.Length(), nullptr);
+    
+    const char cString[] = "\xd8\xaa\xd9\x85";
+    nsDependentCString cStr(cString);
+    NS_ConvertUTF8toUTF16 str(cStr);
+    gfxTextRun *tr =
+      MakeTextRun(str.get(), str.Length(), fontGroup, &params, flags);
+    tr->GetAdvanceWidth(0, str.Length(), nullptr);
 
-       
-       
-       
-       const char cString2[] = "\xd8\xaa\xd9\x85\n\xd8\xaa\xd8\x85 ";
-       nsDependentCString cStr2(cString2);
-       NS_ConvertUTF8toUTF16 str2(cStr2);
-       gfxTextRun *tr2 = MakeTextRun(str2.get(), str2.Length(), fontGroup, &params, flags);
-       tr2->GetAdvanceWidth(0, str2.Length(), nullptr);
-   }
+    
+    
+    
+    const char cString2[] = "\xd8\xaa\xd9\x85\n\xd8\xaa\xd8\x85 ";
+    nsDependentCString cStr2(cString2);
+    NS_ConvertUTF8toUTF16 str2(cStr2);
+    gfxTextRun *tr2 =
+      MakeTextRun(str2.get(), str2.Length(), fontGroup, &params, flags);
+    tr2->GetAdvanceWidth(0, str2.Length(), nullptr);
+  }
 
-   delete gTextRuns;
-   gTextRuns = nullptr;
-
+  delete gTextRuns;
+  gTextRuns = nullptr;
 }
