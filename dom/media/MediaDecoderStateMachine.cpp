97f0aa07dea03d2e2a2bf2dbf6b91a4c2a5ecdb2
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 23:10:08 +0000

    Bug 1297590. Part 4 - change test `mState >= DECODER_STATE_DECODING` to `mState >= DECODER_STATE_DECODING_FIRSTFRAME`. r=kaku
    MozReview-Commit-ID: ImqqLCyp2Yc
    
    UltraBlame original commit: 2ffa21b6fb98a0759367f3fb82acfc72005474bf

--- dom/media/MediaDecoderStateMachine.cpp
+++ dom/media/MediaDecoderStateMachine.cpp
@@ -1517,7 +1517,7 @@ MediaDecoderStateMachine::Seek(SeekTarget aTarget)
   
   bool hasStartTime = mSentFirstFrameLoadedEvent || mReader->ForceZeroStartTime();
   
-  bool stateAllowed = mState >= DECODER_STATE_DECODING;
+  bool stateAllowed = mState >= DECODER_STATE_DECODING_FIRSTFRAME;
 
   if (!stateAllowed || !hasStartTime) {
     DECODER_LOG("Seek() Not Enough Data to continue at this stage, queuing seek");
