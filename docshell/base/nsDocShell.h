3930ef31a10f91f0567ef37e8b7fd187e7490de5
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 17:32:52 +0000

    Bug 968273 - On reload load from the original uri, so that all redirects are reloader.r=mayhemer
    UltraBlame original commit: 5f50f2c7e8136ee2ca4726dd356f4be5e0c9a7da

--- docshell/base/nsDocShell.h
+++ docshell/base/nsDocShell.h
@@ -352,12 +352,7 @@ protected:
   
   
   
-  
-  
-  
   nsresult DoURILoad(nsIURI* aURI,
-                     nsIURI* aOriginalURI,
-                     bool aLoadReplace,
                      nsIURI* aReferrer,
                      bool aSendReferrer,
                      uint32_t aReferrerPolicy,
