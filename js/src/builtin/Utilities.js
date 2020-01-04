193ee0d59811fd6f1511966a18a5df806076c7ae
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 07:59:34 +0000

    Bug 1226762 - Add support for getting original builtin constructors and prototypes in self-hosted code. r=efaust,f=arai
    UltraBlame original commit: 0cf4aceb220302ea48ff153531053dd62826c691

--- js/src/builtin/Utilities.js
+++ js/src/builtin/Utilities.js
@@ -150,6 +150,19 @@ function GetIterator(obj, method) {
     return iterator;
 }
 
+var _builtinCtorsCache = {__proto__: null};
+
+function GetBuiltinConstructor(builtinName) {
+    var ctor = _builtinCtorsCache[builtinName] ||
+               (_builtinCtorsCache[builtinName] = GetBuiltinConstructorImpl(builtinName));
+    assert(ctor, `No builtin with name "${builtinName}" found`);
+    return ctor;
+}
+
+function GetBuiltinPrototype(builtinName) {
+    return (_builtinCtorsCache[builtinName] || GetBuiltinConstructor(builtinName)).prototype;
+}
+
 
 function SpeciesConstructor(obj, defaultConstructor) {
     
