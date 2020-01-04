972d263f1ca53dc6ad23b150b6e27797b70bcee4
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 15:06:52 +0000

    Bug 1190172 part 11 - Clean up nsHTMLEditRules::IsEmptyBlock; r=ehsan
    UltraBlame original commit: 64d71b941c15d6daba17ec1f935a92253c3f1d21

--- editor/libeditor/nsHTMLEditRules.cpp
+++ editor/libeditor/nsHTMLEditRules.cpp
@@ -1556,7 +1556,7 @@ nsHTMLEditRules::WillInsertBreak(Selection& aSelection, bool* aCancel,
   
   
   bool isEmpty;
-  IsEmptyBlock(GetAsDOMNode(blockParent), &isEmpty);
+  IsEmptyBlock(*blockParent, &isEmpty);
   if (isEmpty) {
     nsCOMPtr<Element> br = mHTMLEditor->CreateBR(blockParent,
                                                  blockParent->Length());
@@ -4495,23 +4495,18 @@ nsHTMLEditRules::CreateStyleForInsertText(Selection& aSelection,
 
 
 nsresult
-nsHTMLEditRules::IsEmptyBlock(nsIDOMNode *aNode,
-                              bool *outIsEmptyBlock,
-                              bool aMozBRDoesntCount,
-                              bool aListItemsNotEmpty)
+nsHTMLEditRules::IsEmptyBlock(Element& aNode,
+                              bool* aOutIsEmptyBlock,
+                              MozBRCounts aMozBRCounts)
 {
-  NS_ENSURE_TRUE(aNode && outIsEmptyBlock, NS_ERROR_NULL_POINTER);
-  *outIsEmptyBlock = true;
+  MOZ_ASSERT(aOutIsEmptyBlock);
+  *aOutIsEmptyBlock = true;
 
+  NS_ENSURE_TRUE(IsBlockNode(aNode.AsDOMNode()), NS_ERROR_NULL_POINTER);
 
-  nsCOMPtr<nsIDOMNode> nodeToTest;
-  if (IsBlockNode(aNode)) nodeToTest = do_QueryInterface(aNode);
-
-
-
-  NS_ENSURE_TRUE(nodeToTest, NS_ERROR_NULL_POINTER);
-  return mHTMLEditor->IsEmptyNode(nodeToTest, outIsEmptyBlock,
-                     aMozBRDoesntCount, aListItemsNotEmpty);
+  return mHTMLEditor->IsEmptyNode(aNode.AsDOMNode(), aOutIsEmptyBlock,
+                                  aMozBRCounts == MozBRCounts::yes ? false
+                                                                   : true);
 }
 
 
@@ -6289,7 +6284,7 @@ nsHTMLEditRules::ReturnInHeader(Selection& aSelection,
 
   
   bool isEmpty;
-  res = IsEmptyBlock(aHeader.AsDOMNode(), &isEmpty, true);
+  res = IsEmptyBlock(aHeader, &isEmpty, MozBRCounts::no);
   NS_ENSURE_SUCCESS(res, res);
   if (isEmpty) {
     res = mHTMLEditor->DeleteNode(&aHeader);
@@ -6542,7 +6537,7 @@ nsHTMLEditRules::ReturnInListItem(Selection& aSelection,
   
   
   bool isEmpty;
-  nsresult res = IsEmptyBlock(aListItem.AsDOMNode(), &isEmpty, true, false);
+  nsresult res = IsEmptyBlock(aListItem, &isEmpty, MozBRCounts::no);
   NS_ENSURE_SUCCESS(res, res);
   if (isEmpty && root != list && mReturnInEmptyLIKillsList) {
     
