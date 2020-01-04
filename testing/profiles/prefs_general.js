fd31e97782ab8e7c3d01734b80850fb66a299670
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 04:27:08 +0000

    Bug 1220322 - Change the link pointed to by the one-time notification on upgrade for Push Notifications. r=markh, MattN
    UltraBlame original commit: 798c82c8a96d1099cf712434d9c9036fc48d8297

--- testing/profiles/prefs_general.js
+++ testing/profiles/prefs_general.js
@@ -117,6 +117,7 @@ user_pref("network.sntp.maxRetryCount", 1);
 
 
 user_pref("browser.push.warning.infoURL", "http://%(server)s/alerts-dummy/infoURL");
+user_pref("browser.push.warning.migrationURL", "http://%(server)s/alerts-dummy/migrationURL");
 
 
 user_pref("security.notification_enable_delay", 0);
