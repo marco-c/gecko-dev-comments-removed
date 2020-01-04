53a334f602cfe4018f3784322667711780a7d2f2
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 16:05:58 +0000

    Bug 1269959 - Update jemalloc 4 to version 4.1.1. r=glandium
    UltraBlame original commit: 69f13ba5c08cd838ee52634fd26f88564d3760c2

--- memory/jemalloc/src/test/integration/mallocx.c
+++ memory/jemalloc/src/test/integration/mallocx.c
@@ -70,22 +70,27 @@ TEST_END
 TEST_BEGIN(test_oom)
 {
 	size_t hugemax, size, alignment;
-
-	hugemax = get_huge_size(get_nhuge()-1);
+	bool oom;
+	void *ptrs[3];
+	unsigned i;
 
 	
 
 
 
-	{
-		void *p;
-
-		p = mallocx(hugemax, 0);
-		if (p != NULL) {
-			assert_ptr_null(mallocx(hugemax, 0),
-			    "Expected OOM for mallocx(size=%#zx, 0)", hugemax);
-			dallocx(p, 0);
-		}
+	hugemax = get_huge_size(get_nhuge()-1);
+	oom = false;
+	for (i = 0; i < sizeof(ptrs) / sizeof(void *); i++) {
+		ptrs[i] = mallocx(hugemax, 0);
+		if (ptrs[i] == NULL)
+			oom = true;
+	}
+	assert_true(oom,
+	    "Expected OOM during series of calls to mallocx(size=%zu, 0)",
+	    hugemax);
+	for (i = 0; i < sizeof(ptrs) / sizeof(void *); i++) {
+		if (ptrs[i] != NULL)
+			dallocx(ptrs[i], 0);
 	}
 
 #if LG_SIZEOF_PTR == 3
