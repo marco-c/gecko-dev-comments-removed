9d4efaebbd1f458ab5d516d6ec2c14fdf156ed4c
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 04:17:25 +0000

    Backed out changeset 94df1f873f5c (bug 1205913)
    UltraBlame original commit: 209dcbb6b4155ebf58265c71dc658d92cbbe42d7

--- gfx/layers/Compositor.cpp
+++ gfx/layers/Compositor.cpp
@@ -111,13 +111,7 @@ Compositor::DrawDiagnosticsInternal(DiagnosticFlags aFlags,
       color = gfx::Color(0.0f, 1.0f, 1.0f, 1.0f); 
     }
   } else if (aFlags & DiagnosticFlags::IMAGE) {
-    if (aFlags & DiagnosticFlags::NV12) {
-      color = gfx::Color(1.0f, 1.0f, 0.0f, 1.0f); 
-    } else if (aFlags & DiagnosticFlags::YCBCR) {
-      color = gfx::Color(1.0f, 0.55f, 0.0f, 1.0f); 
-    } else {
-      color = gfx::Color(1.0f, 0.0f, 0.0f, 1.0f); 
-    }
+    color = gfx::Color(1.0f, 0.0f, 0.0f, 1.0f); 
   } else if (aFlags & DiagnosticFlags::COLOR) {
     color = gfx::Color(0.0f, 0.0f, 1.0f, 1.0f); 
   } else if (aFlags & DiagnosticFlags::CONTAINER) {
