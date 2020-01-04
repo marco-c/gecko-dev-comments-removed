6e7a3cb0e58522e149282577f3cf81343f7c6504
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 19:59:53 +0000

    Bug 1255986 - [moztest] Safe-guard structured logging for exceptions from bad handlers. r=jgraham
    Any exception which gets thrown by a log handler while test results are getting generated, should
    not cause test harnesses to stop immediately. To achive that the exception details are written to
    stderr and not propagated up the stack.
    
    MozReview-Commit-ID: ChyYxApYSGx
    
    UltraBlame original commit: 0175a8da10333b8df86bf886b3490050bbabbaf9

--- testing/mozbase/mozlog/mozlog/structuredlog.py
+++ testing/mozbase/mozlog/mozlog/structuredlog.py
@@ -207,7 +207,13 @@ class StructuredLogger(object):
                     return
 
             for handler in self.handlers:
-                handler(data)
+                try:
+                    handler(data)
+                except Exception:
+                    
+                    
+                    print >> sys.__stderr__, '%s: Failure calling log handler:' % __name__
+                    print >> sys.__stderr__, traceback.format_exc()
 
     def _make_log_data(self, action, data):
         all_data = {"action": action,
