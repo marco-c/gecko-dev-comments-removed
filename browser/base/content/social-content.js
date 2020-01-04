112c12a9ffe6f3742e5694ac6c36446bf4ae23a8
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 02:46:46 +0000

    Bug 1188378 fix loading share pages when TP is turned on, r=markh
    UltraBlame original commit: f354f556b59136bc052e5b2c57d7d28e7242de37

--- browser/base/content/social-content.js
+++ browser/base/content/social-content.js
@@ -85,6 +85,19 @@ SocialErrorListener = {
     
     
     if (failure && aStatus != Components.results.NS_BINDING_ABORTED) {
+      
+      
+      
+      
+      if (docShell.hasTrackingContentBlocked) {
+        let frame = docShell.chromeEventHandler;
+        let src = frame.getAttribute("src");
+        if (aRequest && aRequest.name != src) {
+          Cu.reportError("SocialErrorListener ignoring blocked content error for " + aRequest.name);
+          return;
+        }
+      }
+
       aRequest.cancel(Components.results.NS_BINDING_ABORTED);
       this.setErrorPage();
     }
