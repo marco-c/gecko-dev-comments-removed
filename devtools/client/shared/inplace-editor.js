c3b1cb6fa99871c0de8fda8d1dce2a082f4a25a5
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 09:46:08 +0000

    Bug 1166956 - add valid unit when incrementing CSS value "0";r=tromey
    UltraBlame original commit: fb1b8c571e954c651918fa22172ec69af05469fa

--- devtools/client/shared/inplace-editor.js
+++ devtools/client/shared/inplace-editor.js
@@ -448,11 +448,19 @@ InplaceEditor.prototype = {
   _incrementCSSValue: function(value, increment, selStart, selEnd) {
     let range = this._parseCSSValue(value, selStart);
     let type = (range && range.type) || "";
-    let rawValue = (range ? value.substring(range.start, range.end) : "");
-    let incrementedValue = null, selection;
+    let rawValue = range ? value.substring(range.start, range.end) : "";
+    let preRawValue = range ? value.substr(0, range.start) : "";
+    let postRawValue = range ? value.substr(range.end) : "";
+    let info;
 
+    let incrementedValue = null, selection;
     if (type === "num") {
-      let newValue = this._incrementRawValue(rawValue, increment);
+      if (rawValue == "0") {
+        info = {};
+        info.units = this._findCompatibleUnit(preRawValue, postRawValue);
+      }
+
+      let newValue = this._incrementRawValue(rawValue, increment, info);
       if (newValue !== null) {
         incrementedValue = newValue;
         selection = [0, incrementedValue.length];
@@ -467,7 +475,6 @@ InplaceEditor.prototype = {
         selection = newValue.selection;
       }
     } else {
-      let info;
       if (type === "rgb" || type === "hsl") {
         info = {};
         let part = value.substring(range.start, selStart).split(",").length - 1;
@@ -496,14 +503,40 @@ InplaceEditor.prototype = {
       return null;
     }
 
-    let preRawValue = value.substr(0, range.start);
-    let postRawValue = value.substr(range.end);
-
     return {
       value: preRawValue + incrementedValue + postRawValue,
       start: range.start + selection[0],
       end: range.start + selection[1]
     };
+  },
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+  _findCompatibleUnit: function(beforeValue, afterValue) {
+    if (!this.property || !this.property.name) {
+      return "";
+    }
+
+    let units = ["px", "deg", "s"];
+    for (let unit of units) {
+      let value = beforeValue + "1" + unit + afterValue;
+      if (domUtils.cssPropertyIsValid(this.property.name, value)) {
+        return unit;
+      }
+    }
+    return "";
   },
 
   
@@ -641,7 +674,11 @@ InplaceEditor.prototype = {
     }
 
     let number = /\d+(\.\d+)?/.exec(rawValue);
+
     let units = rawValue.substr(number.index + number[0].length);
+    if (info && "units" in info) {
+      units = info.units;
+    }
 
     
     let newValue = Math.round((num + increment) * 1000) / 1000;
