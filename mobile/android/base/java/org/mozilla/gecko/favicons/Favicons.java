e8b59eee6512aa14f4c153325b00e8d653d499a0
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 17:37:54 +0000

    Bug 1276050 - Remove duplicate code from Favicons r=sebastian
    loadUncachedFavicon does exactly the same thing.
    
    MozReview-Commit-ID: 58Yi28JZfv4
    
    UltraBlame original commit: 9575e608cabe6f7ed7cf73475558ddbe9938bf9b

--- mobile/android/base/java/org/mozilla/gecko/favicons/Favicons.java
+++ mobile/android/base/java/org/mozilla/gecko/favicons/Favicons.java
@@ -306,15 +306,7 @@ public class Favicons {
         }
 
         
-        final LoadFaviconTask task =
-            new LoadFaviconTask(context, pageURL, targetURL, 0, callback, targetSize, true);
-        final int taskId = task.getId();
-        synchronized (loadTasks) {
-            loadTasks.put(taskId, task);
-        }
-        task.execute();
-
-        return taskId;
+        return loadUncachedFavicon(context, pageURL, targetURL, 0, targetSize, callback);
     }
 
     public static int getSizedFaviconForPageFromLocal(Context context, final String pageURL, final OnFaviconLoadedListener callback) {
