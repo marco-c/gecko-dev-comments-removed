d8e6275a70c00c41ef2818b65ef9ae75ebbca67e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 00:02:04 +0000

    Bug 1197664: Report the total number of decoded frames. CLOSED TREE r=kentuckyfriedtakahe
    UltraBlame original commit: 5b179e366ecfc4dcb3b94f5192d983fe88a31b51

--- dom/media/MediaFormatReader.h
+++ dom/media/MediaFormatReader.h
@@ -206,6 +206,7 @@ private:
       , mDrainComplete(false)
       , mNumSamplesInput(0)
       , mNumSamplesOutput(0)
+      , mNumSamplesOutputTotal(0)
       , mSizeOfQueue(0)
       , mIsHardwareAccelerated(false)
       , mLastStreamSourceID(UINT32_MAX)
@@ -263,6 +264,7 @@ private:
     nsTArray<nsRefPtr<MediaData>> mOutput;
     uint64_t mNumSamplesInput;
     uint64_t mNumSamplesOutput;
+    uint64_t mNumSamplesOutputTotal;
 
     
     
