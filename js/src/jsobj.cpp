f53278822920252945920d10c7855e81557c91ce
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 12:13:33 +0000

    Bug 1254622 - Relookup group->newScript in CreateThisForFunctionWithGroup. r=bhackett
    UltraBlame original commit: 2912de2754baafcc07201c4ecda7c975622e6df5

--- js/src/jsobj.cpp
+++ js/src/jsobj.cpp
@@ -932,8 +932,9 @@ CreateThisForFunctionWithGroup(JSContext* cx, HandleObjectGroup group,
         if (!res)
             return nullptr;
 
-        if (newKind != SingletonObject)
-            newScript->registerNewObject(res);
+        
+        if (newKind != SingletonObject && group->newScript())
+            group->newScript()->registerNewObject(res);
 
         return res;
     }
