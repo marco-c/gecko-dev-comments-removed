e76721f68b6a8a1555cfaa93f57736e1288b3488
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 18:39:58 +0000

    Bug 1281499 - Add missing include and namespace for MakeUnique<> r=mstange
    MozReview-Commit-ID: DpBZrnx47g8
    
    UltraBlame original commit: 65542739603b47543b728b90f6625ce37cab0b78

--- widget/nsTransferable.cpp
+++ widget/nsTransferable.cpp
@@ -36,6 +36,7 @@
 #include "nsIWeakReferenceUtils.h"
 #include "nsIFile.h"
 #include "nsILoadContext.h"
+#include "mozilla/UniquePtr.h"
 
 NS_IMPL_ISUPPORTS(nsTransferable, nsITransferable)
 
@@ -183,7 +184,7 @@ DataStruct::ReadCache(nsISupports** aData, uint32_t* aDataLen)
 
     uint32_t size = uint32_t(fileSize);
     
-    auto data = MakeUnique<char[]>(size);
+    auto data = mozilla::MakeUnique<char[]>(size);
     if ( !data )
       return NS_ERROR_OUT_OF_MEMORY;
       
