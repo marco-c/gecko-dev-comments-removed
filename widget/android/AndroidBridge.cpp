4d1dfe4185eaa8f0072e00be91df35d973eafe44
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 17:38:25 +0000

    Bug 1240423 - part4 : update audio playing window checking in AndroidBridge. r=snorp
    MozReview-Commit-ID: CJCnyUtSduN
    
    UltraBlame original commit: 2d337dda7dd1e2b3b490466e130239602de87c51

--- widget/android/AndroidBridge.cpp
+++ widget/android/AndroidBridge.cpp
@@ -1651,6 +1651,12 @@ nsAndroidBridge::Observe(nsISupports* aSubject, const char* aTopic,
     MOZ_ASSERT(window);
 
     nsAutoString activeStr(aData);
+    if (activeStr.EqualsLiteral("inactive-nonaudible")) {
+      
+      
+      return NS_OK;
+    }
+
     bool isPlaying = activeStr.EqualsLiteral("active");
 
     UpdateAudioPlayingWindows(window, isPlaying);
