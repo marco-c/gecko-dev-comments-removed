51e3468344de9cc153767bf3b254cf6004298c6c
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 04:21:01 +0000

    Bug 1212136 - Remove noisy warning that we don't have time to investigate properly. r=tnikkel
    UltraBlame original commit: c801709386851bf1f2fa4672a26d211035ac004e

--- layout/base/nsLayoutUtils.cpp
+++ layout/base/nsLayoutUtils.cpp
@@ -882,7 +882,8 @@ GetDisplayPortFromMarginsData(nsIContent* aContent,
   if (nsRect* baseData = static_cast<nsRect*>(aContent->GetProperty(nsGkAtoms::DisplayPortBase))) {
     base = *baseData;
   } else {
-    NS_WARNING("Attempting to get a margins-based displayport with no base data!");
+    
+    
   }
 
   nsIFrame* frame = aContent->GetPrimaryFrame();
