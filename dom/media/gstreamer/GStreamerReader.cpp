ab8b1308afa381032559d2e8d2be84481b91e5bd
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 02:39:18 +0000

    Bug 1211327 - Remove unnecessary usage of decoder monitor from MediaDecoderReader and sub-classes. r=jya.
    UltraBlame original commit: 8f33bff41ba95e829465d8664fff433ee5fb2083

--- dom/media/gstreamer/GStreamerReader.cpp
+++ dom/media/gstreamer/GStreamerReader.cpp
@@ -1200,12 +1200,6 @@ void GStreamerReader::Eos(GstAppSink* aSink)
     }
     mon.NotifyAll();
   }
-
-  {
-    ReentrantMonitorAutoEnter mon(mDecoder->GetReentrantMonitor());
-    
-    mon.NotifyAll();
-  }
 }
 
 
