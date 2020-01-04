442f008321294f864c74d5363e571cafe1d15da6
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 03:05:02 +0000

    Bug 1196047 - Move most of shared/performance to client or server. r=jsantell
    UltraBlame original commit: 4d1f1902c41061590e7786e035eecd42835c0b67

--- devtools/client/performance/test/browser_perf-legacy-front-08.js
+++ devtools/client/performance/test/browser_perf-legacy-front-08.js
@@ -7,17 +7,14 @@
 
 var WAIT_TIME = 100;
 
-const { TargetFactory } = require("devtools/client/framework/target");
-const { LegacyPerformanceFront } = require("devtools/shared/performance/legacy/front");
-const { merge } = require("sdk/util/object");
+const { LegacyPerformanceFront } = require("devtools/client/performance/legacy/front");
 
-add_task(function*() {
-  let tab = yield getTab(MAIN_DOMAIN + "doc_perf.html");
+function* spawnTest() {
+  let tab = yield getTab(SIMPLE_URL);
   let target = TargetFactory.forTab(tab);
   yield target.makeRemote();
 
   merge(target, {
-    TEST_MOCK_TIMELINE_ACTOR: true,
     TEST_PERFORMANCE_LEGACY_FRONT: true,
   });
 
@@ -25,7 +22,6 @@ add_task(function*() {
   yield front.connect();
 
   ok(front.LEGACY_FRONT, true, "Using legacy front");
-  front.on("timeline-data", () => ok(false, "There should not be any timeline-data events when mocked"));
 
   let recording = yield front.startRecording({
     withTicks: true,
@@ -34,32 +30,31 @@ add_task(function*() {
     withAllocations: true,
   });
 
-  is(recording.getConfiguration().withMarkers, false, "overrides withMarkers based off of actor support");
-  is(recording.getConfiguration().withTicks, false, "overrides withTicks based off of actor support");
+  is(recording.getConfiguration().withMarkers, true, "allows withMarkers based off of actor support");
+  is(recording.getConfiguration().withTicks, true, "allows withTicks based off of actor support");
   is(recording.getConfiguration().withMemory, false, "overrides withMemory based off of actor support");
   is(recording.getConfiguration().withAllocations, false, "overrides withAllocations based off of actor support");
 
-  yield busyWait(WAIT_TIME);
+  yield waitUntil(() => recording.getMarkers().length);
+  yield waitUntil(() => recording.getTicks().length);
 
   yield front.stopRecording(recording);
+
+  ok(recording.getMarkers().length, "we have several markers");
+  ok(recording.getTicks().length, "we have several ticks");
 
   ok(typeof recording.getDuration() === "number",
     "The front.stopRecording() allows recording to get a duration.");
   ok(recording.getDuration() >= 0, "duration is a positive number");
-  isEmptyArray(recording.getMarkers(), "markers");
-  isEmptyArray(recording.getTicks(), "ticks");
   isEmptyArray(recording.getMemory(), "memory");
   isEmptyArray(recording.getAllocations().sites, "allocations.sites");
   isEmptyArray(recording.getAllocations().timestamps, "allocations.timestamps");
   isEmptyArray(recording.getAllocations().frames, "allocations.frames");
   ok(recording.getProfile().threads[0].samples.data.length, "profile data has some samples");
-  checkSystemInfo(recording, "Host");
-  checkSystemInfo(recording, "Client");
 
   yield front.destroy();
-  yield closeDebuggerClient(target.client);
   gBrowser.removeCurrentTab();
-});
+}
 
 function isEmptyArray (array, name) {
   ok(Array.isArray(array), `${name} is an array`);
@@ -77,11 +72,4 @@ function getTab (url) {
       waitForFocus(() => resolve(tab), content, isBlank);
     });
   });
-}
-
-function checkSystemInfo (recording, type) {
-  let data = recording[`get${type}SystemInfo`]();
-  for (let field of ["appid", "apptype", "vendor", "name", "version"]) {
-    ok(data[field], `get${type}SystemInfo() has ${field} property`);
-  }
 }
