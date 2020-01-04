64b91f41d3d922a31bd2dbde3fd168c7f5682a18
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 22:52:19 +0000

    Bug 1051567 - Make sure we resend file descriptors for the first chunk of a message. r=billm
    Check if the buffers iterator was never consumed. This is a regression
    introduced when converting ipc to use BufferList in bug 1262671.
    
    MozReview-Commit-ID: LWAoVlI5CKJ
    
    UltraBlame original commit: acb978a84753654b4deca915b12504f1c0c27fd0

--- ipc/chromium/src/chrome/common/ipc_channel_posix.cc
+++ ipc/chromium/src/chrome/common/ipc_channel_posix.cc
@@ -579,7 +579,12 @@ bool Channel::ChannelImpl::ProcessOutgoingMessages() {
         int[FileDescriptorSet::MAX_DESCRIPTORS_PER_MESSAGE]));
     char buf[tmp];
 
-    if (partial_write_iter_.isNothing() &&
+    if (partial_write_iter_.isNothing()) {
+      Pickle::BufferList::IterImpl iter(msg->Buffers());
+      partial_write_iter_.emplace(iter);
+    }
+
+    if (partial_write_iter_.value().Data() == msg->Buffers().Start() &&
         !msg->file_descriptor_set()->empty()) {
       
       struct cmsghdr *cmsg;
@@ -610,11 +615,6 @@ bool Channel::ChannelImpl::ProcessOutgoingMessages() {
     struct iovec iov[kMaxIOVecSize];
     size_t iov_count = 0;
     size_t amt_to_write = 0;
-
-    if (partial_write_iter_.isNothing()) {
-      Pickle::BufferList::IterImpl iter(msg->Buffers());
-      partial_write_iter_.emplace(iter);
-    }
 
     
     Pickle::BufferList::IterImpl iter = partial_write_iter_.value();
