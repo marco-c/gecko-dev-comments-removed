e398cdec6846335001151c2245fc5eaa1e118080
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 09:01:36 +0000

    Bug 1242720 - Followup: reduce code duplication. r=jfkthame
    UltraBlame original commit: 888c1fb24f815739efe0fb7d7ae4260aaffce746

--- widget/windows/WinUtils.cpp
+++ widget/windows/WinUtils.cpp
@@ -531,13 +531,21 @@ WinUtils::SystemScaleFactor()
   
   
   
-  static int logPixelsY = 0;
-  if (!logPixelsY) {
+  static double systemScale = 0;
+  if (systemScale == 0) {
     HDC screenDC = GetDC(nullptr);
-    logPixelsY = GetDeviceCaps(screenDC, LOGPIXELSY);
+    systemScale = GetDeviceCaps(screenDC, LOGPIXELSY) / 96.0;
     ReleaseDC(nullptr, screenDC);
+
+    if (systemScale == 0) {
+      
+      
+      
+      
+      return 1.0;
+    }
   }
-  return logPixelsY / 96.0;
+  return systemScale;
 }
 
 #ifndef WM_DPICHANGED
@@ -601,19 +609,7 @@ WinUtils::LogToPhysFactor(HMONITOR aMonitor)
     return dpiY / 96.0;
   }
 
-  
-  HDC hdc = ::GetDC(nullptr);
-  double result = ::GetDeviceCaps(hdc, LOGPIXELSY) / 96.0;
-  ::ReleaseDC(nullptr, hdc);
-
-  if (result == 0) {
-    
-    
-    
-    
-    result = 1.0;
-  }
-  return result;
+  return SystemScaleFactor();
 }
 
 
