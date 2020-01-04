4f222040d9c27ff045fa68b80b93612f00edb64d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 18:28:29 +0000

    Bug 1278303 part 4 - Fix correctness issue of MCreateThis new-target operand index. r=efaust
    UltraBlame original commit: fd1f56dfff6017d2e482dce04b16672d90fb27b3

--- js/src/jit/MIR.h
+++ js/src/jit/MIR.h
@@ -4765,7 +4765,7 @@ class MCreateThis
   public:
     INSTRUCTION_HEADER(CreateThis)
     TRIVIAL_NEW_WRAPPERS
-    NAMED_OPERANDS((0, getCallee), (0, getNewTarget))
+    NAMED_OPERANDS((0, getCallee), (1, getNewTarget))
 
     
     AliasSet getAliasSet() const override {
