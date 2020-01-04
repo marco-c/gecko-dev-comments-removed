78c79f7bb573a31070c5dc1b2810497be8e342e4
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 01:36:53 +0000

    Bug 1196387 - Remove unneeded "reading time" code and layouts. r=sebastian
    UltraBlame original commit: 7be913efcdfb1471ad21c5333951262f366fdaf2

--- mobile/android/base/home/ReadingListRow.java
+++ mobile/android/base/home/ReadingListRow.java
@@ -6,37 +6,21 @@
 package org.mozilla.gecko.home;
 
 import org.mozilla.gecko.R;
-import org.mozilla.gecko.Tab;
-import org.mozilla.gecko.Tabs;
 import org.mozilla.gecko.db.BrowserContract.ReadingListItems;
-import org.mozilla.gecko.home.TwoLinePageRow;
 import org.mozilla.gecko.util.StringUtils;
 
 import android.content.Context;
-import android.content.res.Resources;
 import android.database.Cursor;
 import android.text.TextUtils;
 import android.util.AttributeSet;
-import android.view.LayoutInflater;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class ReadingListRow extends LinearLayout {
-
-    private final Resources resources;
-
-    private final TextView title;
-    private final TextView excerpt;
-    private final TextView readTime;
-    private final ImageView indicator;
-
-    
-    private static final int AVERAGE_READING_SPEED = 250;
-
-    
-    private static final float AVERAGE_WORD_LENGTH = 5.1f;
-
+    private TextView title;
+    private TextView excerpt;
+    private ImageView indicator;
 
     public ReadingListRow(Context context) {
         this(context, null);
@@ -44,14 +28,13 @@ public class ReadingListRow extends LinearLayout {
 
     public ReadingListRow(Context context, AttributeSet attrs) {
         super(context, attrs);
+    }
 
-        LayoutInflater.from(context).inflate(R.layout.reading_list_row_view, this);
-
-        resources = context.getResources();
-
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
         title = (TextView) findViewById(R.id.title);
         excerpt = (TextView) findViewById(R.id.excerpt);
-        readTime = (TextView) findViewById(R.id.read_time);
         indicator = (ImageView) findViewById(R.id.indicator);
     }
 
@@ -73,28 +56,6 @@ public class ReadingListRow extends LinearLayout {
         excerpt.setTextAppearance(getContext(), isUnread ? R.style.Widget_ReadingListRow_Title_Unread : R.style.Widget_ReadingListRow_Title_Read);
 
         indicator.setImageResource(isUnread ? R.drawable.reading_list_indicator_unread : R.drawable.reading_list_indicator_read);
-
-        
-
-
-
-
-
-
-
-
     }
 
-    
-
-
-
-
-
-    private static int getEstimatedReadTime(int length) {
-        final int minutes = (int) Math.ceil((length / AVERAGE_WORD_LENGTH) / AVERAGE_READING_SPEED);
-
-        
-        return Math.max(minutes, 1);
-    }
 }
