56d6d5c4c99e3eba926419076d7eb09f3bc2b237
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 12:05:29 +0000

    Bug 1257919 part 4.  Stop returning StackFrame instances from exceptions::CreateStack.  C++ callers of GetCurrentJSStack or exceptions::CreateStack always check for null anyway, and none of them seem to want this non-JS thing.  r=khuey
    UltraBlame original commit: 555822dca0fd7fa7f6137ad26e7bb7e543c5ea93

--- dom/base/DOMException.cpp
+++ dom/base/DOMException.cpp
@@ -218,37 +218,7 @@ Exception::Exception(const nsACString& aMessage,
     sEverMadeOneFromFactory = true;
   }
 
-  nsCOMPtr<nsIStackFrame> location;
-  if (aLocation) {
-    location = aLocation;
-  } else {
-    location = GetCurrentJSStack();
-    
-    
-    
-    
-    
-  }
-  
-  if (location) {
-    while (1) {
-      uint32_t language;
-      int32_t lineNumber;
-      if (NS_FAILED(location->GetLanguage(&language)) ||
-          language == nsIProgrammingLanguage::JAVASCRIPT ||
-          NS_FAILED(location->GetLineNumber(&lineNumber)) ||
-          lineNumber) {
-        break;
-      }
-      nsCOMPtr<nsIStackFrame> caller;
-      if (NS_FAILED(location->GetCaller(getter_AddRefs(caller))) || !caller) {
-        break;
-      }
-      location = caller;
-    }
-  }
-
-  Initialize(aMessage, aResult, aName, location, aData);
+  Initialize(aMessage, aResult, aName, aLocation, aData);
 }
 
 Exception::Exception()
@@ -454,12 +424,11 @@ Exception::Initialize(const nsACString& aMessage, nsresult aResult,
   if (aLocation) {
     mLocation = aLocation;
   } else {
-    nsresult rv;
-    nsXPConnect* xpc = nsXPConnect::XPConnect();
-    rv = xpc->GetCurrentJSStack(getter_AddRefs(mLocation));
-    if (NS_FAILED(rv)) {
-      return rv;
-    }
+    mLocation = GetCurrentJSStack();
+    
+    
+    
+    
   }
 
   mData = aData;
