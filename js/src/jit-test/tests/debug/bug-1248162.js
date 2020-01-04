121d426e6a511ab259f4b5961181349716c60d81
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 10:10:12 +0000

    Bug 1248162 - Clear pending exceptions when ignoring OOMs in Debugger::slowPathOnNewGlobalObject; r=jimb
    MozReview-Commit-ID: 5J2Vmp3FKOx
    
    UltraBlame original commit: 6839dfd7e63f5c5f7931a9bd8b2d9ccd49be6bea

---
+++ js/src/jit-test/tests/debug/bug-1248162.js
@@ -0,0 +1,14 @@
+
+
+if (typeof oomTest !== "function")
+  quit();
+
+
+for (var i = 0; i < 9; ++i) {
+  var dbg = new Debugger;
+  dbg.onNewGlobalObject = function() {};
+}
+
+oomTest(function() {
+  newGlobal();
+})
