017b8f4eeca92fce7a326d5b629fcd8c0f544193
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 20:22:09 +0000

    Bug 1288150 - Add fast breakpoint presents check for removeBreakpoints. r=jlongster
    MozReview-Commit-ID: KKDUw6st4Bs
    
    UltraBlame original commit: e5341bb0157c904feeb2589e08c2039645adf15b

--- devtools/client/sourceeditor/debugger.js
+++ devtools/client/sourceeditor/debugger.js
@@ -190,7 +190,15 @@ function removeBreakpoints(ctx) {
     meta.breakpoints = {};
   }
 
-  cm.doc.iter((line) => { removeBreakpoint(ctx, line); });
+  cm.doc.iter((line) => {
+    
+    
+    
+    if (line.wrapClass == null || !line.wrapClass.includes("breakpoint")) {
+      return;
+    }
+    removeBreakpoint(ctx, line);
+  });
 }
 
 
