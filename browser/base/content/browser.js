1e3f933a9c4a97202acbc543b6a691c6a9ecc2b3
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 00:09:52 +0000

    Bug 1170011 - Disable infinite recursion on the View Source command; r=jaws
    This disables the View Source command on view-source documents in the
    view-source-in-tab mode.
    
    UltraBlame original commit: e7402272f2570409c1dfc3feb69b0313eb4f33c8

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -4049,6 +4049,10 @@ var XULBrowserWindow = {
     delete this.isImage;
     return this.isImage = document.getElementById("isImage");
   },
+  get canViewSource () {
+    delete this.canViewSource;
+    return this.canViewSource = document.getElementById("canViewSource");
+  },
 
   init: function () {
     
@@ -4206,6 +4210,7 @@ var XULBrowserWindow = {
       if (aRequest) {
         let msg = "";
         let location;
+        let canViewSource = true;
         
         if (aRequest instanceof nsIChannel || "URI" in aRequest) {
           location = aRequest.URI;
@@ -4213,6 +4218,9 @@ var XULBrowserWindow = {
           
           if (location.scheme == "keyword" && aWebProgress.isTopLevel)
             gBrowser.userTypedValue = null;
+
+          canViewSource = !Services.prefs.getBoolPref("view_source.tab") ||
+                          location.scheme != "view-source";
 
           if (location.spec != "about:blank") {
             switch (aStatus) {
@@ -4227,10 +4235,18 @@ var XULBrowserWindow = {
         this.setDefaultStatus(msg);
 
         
-        if (browser.documentContentType && BrowserUtils.mimeTypeIsTextBased(browser.documentContentType))
+        if (browser.documentContentType && BrowserUtils.mimeTypeIsTextBased(browser.documentContentType)) {
           this.isImage.removeAttribute('disabled');
-        else
+        } else {
+          canViewSource = false;
           this.isImage.setAttribute('disabled', 'true');
+        }
+
+        if (canViewSource) {
+          this.canViewSource.removeAttribute('disabled');
+        } else {
+          this.canViewSource.setAttribute('disabled', 'true');
+        }
       }
 
       this.isBusy = false;
