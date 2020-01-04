c7ed8fc642b6998d044f8d87d111aee0ea9c5924
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 22:19:55 +0000

    Bug 1294220 - part6: use standard getter instead of loader.lazyGetter in l10n.js;r=bgrins,tromey
    MozReview-Commit-ID: 37AujDuDn0a
    
    UltraBlame original commit: 4d0ea69c57f62f807926d48e7c8bad085056554f

--- devtools/client/shared/l10n.js
+++ devtools/client/shared/l10n.js
@@ -13,12 +13,18 @@ const { sprintf } = require("devtools/client/shared/vendor/sprintf");
 
 
 function LocalizationHelper(stringBundleName) {
-  loader.lazyGetter(this, "properties", () => {
-    return parsePropertiesFile(require(`raw!${stringBundleName}`));
-  });
+  this.stringBundleName = stringBundleName;
 }
 
 LocalizationHelper.prototype = {
+  get properties() {
+    if (!this._properties) {
+      this._properties = parsePropertiesFile(require(`raw!${this.stringBundleName}`));
+    }
+
+    return this._properties;
+  },
+
   
 
 
