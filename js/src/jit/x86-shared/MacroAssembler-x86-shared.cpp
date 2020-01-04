b6a4ac5c2818e8b8d00447d086c341c2fc2279b2
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 09:49:48 +0000

    Bug 1245112 - Part 6: Move MacroAssembler::branchTest32 into generic macro assembler. r=nbp
    UltraBlame original commit: f433eabad0eecdaf0d895e68fae7f3501dd52c04

--- js/src/jit/x86-shared/MacroAssembler-x86-shared.cpp
+++ js/src/jit/x86-shared/MacroAssembler-x86-shared.cpp
@@ -112,7 +112,7 @@ MacroAssemblerX86Shared::branchNegativeZero(FloatRegister reg,
 
     
     
-    branchTest32(NonZero, scratch, Imm32(1), label);
+    asMasm().branchTest32(NonZero, scratch, Imm32(1), label);
 
     bind(&nonZero);
 #elif defined(JS_CODEGEN_X64)
