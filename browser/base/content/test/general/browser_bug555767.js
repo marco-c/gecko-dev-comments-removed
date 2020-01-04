26f612450521057e12d1b1d8f376a017ae3225b1
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 00:10:12 +0000

    Bug 1196765 - Fix and re-enable browser/base/content/test/general/browser_bug555767.js by removing unneeded checks using browser.sessionHistory removing skip-if = e10s from browser.ini. r=jimm
    UltraBlame original commit: 7fe1cc367ef17bf982da73863365ba63cc387df8

--- browser/base/content/test/general/browser_bug555767.js
+++ browser/base/content/test/general/browser_bug555767.js
@@ -1,68 +1,54 @@
-
-
-
-
-function test() {
-  waitForExplicitFinish();
-
-  let testURL = "http://example.org/browser/browser/base/content/test/general/dummy_page.html";
-  let tabSelected = false;
-
-  
-  let baseTab = gBrowser.addTab(testURL);
-  baseTab.linkedBrowser.addEventListener("load", function() {
     
-    if (baseTab.linkedBrowser.currentURI.spec == "about:blank")
-      return;
-    baseTab.linkedBrowser.removeEventListener("load", arguments.callee, true);
 
-    let testTab = gBrowser.addTab();
 
-    
-    gBrowser.selectedTab = testTab;
 
-    
-    ok(testTab.linkedBrowser.sessionHistory.count < 2,
-       "The test tab has 1 or less history entries");
-    
-    is(testTab.linkedBrowser.currentURI.spec, "about:blank",
-       "The test tab is on about:blank");
-    
-    ok(!testTab.linkedBrowser.contentDocument.body.hasChildNodes(),
-       "The test tab has no child nodes");
-    ok(!testTab.hasAttribute("busy"),
-       "The test tab doesn't have the busy attribute");
+    add_task(function* () {
+      let testURL = "http://example.org/browser/browser/base/content/test/general/dummy_page.html";
+      let tabSelected = false;
 
-    
-    gURLBar.value = "moz-action:switchtab," + JSON.stringify({url: testURL});
-    
-    gURLBar.focus();
-
-    
-    function onTabClose(aEvent) {
-      gBrowser.tabContainer.removeEventListener("TabClose", onTabClose, false);
       
-      is(aEvent.originalTarget, testTab, "Got the TabClose event for the right tab");
-      
-      ok(tabSelected, "Confirming that the tab was selected");
-      gBrowser.removeTab(baseTab);
-      finish();
-    }
-    function onTabSelect(aEvent) {
-      gBrowser.tabContainer.removeEventListener("TabSelect", onTabSelect, false);
-      
-      is(aEvent.originalTarget, baseTab, "Got the TabSelect event for the right tab");
-      
-      is(gBrowser.selectedTab, baseTab, "We've switched to the correct tab");
-      tabSelected = true;
-    }
+      let baseTab = gBrowser.addTab(testURL);
 
-    
-    gBrowser.tabContainer.addEventListener("TabClose", onTabClose, false);
-    gBrowser.tabContainer.addEventListener("TabSelect", onTabSelect, false);
+      
+      yield promiseTabLoaded(baseTab);
+      if (baseTab.linkedBrowser.currentURI.spec == "about:blank")
+        return;
+      baseTab.linkedBrowser.removeEventListener("load", arguments.callee, true);
 
-    
-    EventUtils.synthesizeKey("VK_RETURN", {});
-  }, true);
-}
+      let testTab = gBrowser.addTab();
 
+      
+      gBrowser.selectedTab = testTab;
+
+      
+      gURLBar.value = "moz-action:switchtab," + JSON.stringify({url: testURL});
+      
+      gURLBar.focus();
+
+      
+      function onTabClose(aEvent) {
+        gBrowser.tabContainer.removeEventListener("TabClose", onTabClose, false);
+        
+        is(aEvent.originalTarget, testTab, "Got the TabClose event for the right tab");
+        
+        ok(tabSelected, "Confirming that the tab was selected");
+        gBrowser.removeTab(baseTab);
+        finish();
+      }
+      function onTabSelect(aEvent) {
+        gBrowser.tabContainer.removeEventListener("TabSelect", onTabSelect, false);
+        
+        is(aEvent.originalTarget, baseTab, "Got the TabSelect event for the right tab");
+        
+        is(gBrowser.selectedTab, baseTab, "We've switched to the correct tab");
+        tabSelected = true;
+      }
+
+      
+      gBrowser.tabContainer.addEventListener("TabClose", onTabClose, false);
+      gBrowser.tabContainer.addEventListener("TabSelect", onTabSelect, false);
+
+      
+      EventUtils.synthesizeKey("VK_RETURN", {});
+    });
+ 
