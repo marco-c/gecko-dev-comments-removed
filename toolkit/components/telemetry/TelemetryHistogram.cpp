40f8a528bb1899818e606c7ec6e7dfc5269c3e35
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 22:48:49 +0000

    bug 1218576 - Pre-allocate child keyed histograms in the parent process r=gfritzsche I originally thought we'd be able to avoid the previous implementation's waste of a map full of every kind of keyed histogram. Unfortunately, other code (TelemetrySession at the very least) depends on this (and will throw if a keyed histogram isn't present, even if it is empty)
    MozReview-Commit-ID: 8MCGVa595UB
    
    UltraBlame original commit: ee46537edfbc7701ad1723c22ca5b39da713fee1

--- toolkit/components/telemetry/TelemetryHistogram.cpp
+++ toolkit/components/telemetry/TelemetryHistogram.cpp
@@ -1854,12 +1854,7 @@ internal_AccumulateChildKeyed(mozilla::Telemetry::ID aId,
   id.Append(th.id());
   id.AppendLiteral(CHILD_HISTOGRAM_SUFFIX);
   KeyedHistogram* keyed = internal_GetKeyedHistogramById(id);
-  if (!keyed) {
-    const nsDependentCString expiration(th.expiration());
-    keyed = new KeyedHistogram(id, expiration, th.histogramType, th.min, th.max,
-                               th.bucketCount, th.dataset);
-    gKeyedHistograms.Put(id, keyed);
-  }
+  MOZ_ASSERT(keyed);
   keyed->Add(aKey, aSample);
 }
 
@@ -1942,6 +1937,16 @@ void TelemetryHistogram::InitializeGlobalState(bool canRecordBase,
     const nsDependentCString expiration(h.expiration());
     gKeyedHistograms.Put(id, new KeyedHistogram(id, expiration, h.histogramType,
                                                 h.min, h.max, h.bucketCount, h.dataset));
+    if (XRE_IsParentProcess()) {
+      
+      
+      
+      nsCString childId(id);
+      childId.AppendLiteral(CHILD_HISTOGRAM_SUFFIX);
+      gKeyedHistograms.Put(childId,
+                           new KeyedHistogram(id, expiration, h.histogramType,
+                                              h.min, h.max, h.bucketCount, h.dataset));
+    }
   }
 
   
