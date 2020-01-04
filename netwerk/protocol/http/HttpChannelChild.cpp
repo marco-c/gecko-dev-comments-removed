334899ed57a980b0d9b8aebef8cb6839c3db0f7e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 06:09:25 +0000

    Backed out changeset 7a9e01e904b0 (bug 1137681) for test bustage on a CLOSED TREE
    UltraBlame original commit: 3cd5acd350fbf6aa9b4516f3620a47047dcd0862

--- netwerk/protocol/http/HttpChannelChild.cpp
+++ netwerk/protocol/http/HttpChannelChild.cpp
@@ -1740,9 +1740,6 @@ HttpChannelChild::AsyncOpen(nsIStreamListener *listener, nsISupports *aContext)
     return NS_OK;
   }
 
-  
-  HttpBaseChannel::SetDocshellUserAgentOverride();
-
   if (ShouldIntercept()) {
     mResponseCouldBeSynthesized = true;
 
