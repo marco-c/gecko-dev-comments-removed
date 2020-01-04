91e7d9e60f686d600ca1dc76592bbbf6985a4869
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 21:33:15 +0000

    Bug 1253359 - Use vendored Pytest in `python-test` and Mn harness tests r=gps
    In the `python-test` mach command and the mozharness script for
    the Marionette harness tests, use the vendored-in Pytest
    instead of installing from pip.
    
    Add the Marionette harness test requirements file to the
    file_patterns in the definition of the marionette-harness taskcluster
    job, as changes to the requirements should trigger the job to run.
    
    
    MozReview-Commit-ID: J5pln2WB4GY
    
    UltraBlame original commit: 47d2e5c4d8fb0c844dfa50ff7fa1efabea39cbab

--- python/mach_commands.py
+++ python/mach_commands.py
@@ -90,7 +90,6 @@ class MachCommands(MachCommandBase):
         
         
         
-        
         return_code = 0
         found_tests = False
         if test_objects is None:
@@ -98,9 +97,6 @@ class MachCommands(MachCommandBase):
             
             if path_only:
                 if tests:
-                    self.virtualenv_manager.install_pip_package(
-                       'pytest==2.9.1'
-                    )
                     test_objects = [{'path': p} for p in find_tests_by_path()]
                 else:
                     self.log(logging.WARN, 'python-test', {},
@@ -160,4 +156,3 @@ class MachCommands(MachCommandBase):
             return 1
 
         return 0 if return_code == 0 else 1
-
