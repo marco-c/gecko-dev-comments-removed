0d377224d5dbc7990c0d9b6e47da70b7dcfcb898
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 04:35:31 +0000

    Bug 1211260 - [css-grid] Follow-up: address a code readability nit that I missed.  r=dholbert
    UltraBlame original commit: 3143e47d7808735a7d93051a3e08c3dec9a450c0

--- layout/generic/nsGridContainerFrame.cpp
+++ layout/generic/nsGridContainerFrame.cpp
@@ -1407,7 +1407,7 @@ nsGridContainerFrame::ResolveLineRange(
     r.second = std::min(r.second, nsStyleGridLine::kMaxLine - 1);
   } else {
     
-    if (r.second < r.first) {
+    if (r.first > r.second) {
       Swap(r.first, r.second);
     } else if (r.first == r.second) {
       if (MOZ_UNLIKELY(r.first == nsStyleGridLine::kMaxLine)) {
