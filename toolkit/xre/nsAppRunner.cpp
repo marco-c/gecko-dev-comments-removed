ad2194d57066e449adc4fc4559a1b3f7357cd048
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 06:32:47 +0000

    Bug 1198459 - Update telemetry data based on the use of the forced enable pref. r=felipe
    UltraBlame original commit: b3a52644af28c686e5ef11be94e1d46585883926

--- toolkit/xre/nsAppRunner.cpp
+++ toolkit/xre/nsAppRunner.cpp
@@ -4722,6 +4722,8 @@ mozilla::BrowserTabsRemoteAutostart()
   
   if (Preferences::GetBool(kForceEnableE10sPref, false)) {
     gBrowserTabsRemoteAutostart = true;
+    prefEnabled = true;
+    status = kE10sEnabledByUser;
   }
 
   mozilla::Telemetry::Accumulate(mozilla::Telemetry::E10S_AUTOSTART, gBrowserTabsRemoteAutostart);
