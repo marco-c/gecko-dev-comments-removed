2c4415364d6c2ec00a0cdb1a95844a5936ca7a2b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 01:33:24 +0000

    Bug 1199161 - Add selectedTextContent to CaretStateChangedEvent. r=kanru, sr=smaug
    The old SelectionStateChangedEvent dispatched by SelectionChanged had
    this field, but it has not being ported to CaretStateChangedEvent. We
    should include selected text in the event so that Gaia could have a
    chance to enhance user experience.
    
    UltraBlame original commit: 0cd2bdcae8cee4153f45417e720208af34c7d0ad

--- dom/browser-element/BrowserElementParent.js
+++ dom/browser-element/BrowserElementParent.js
@@ -459,6 +459,8 @@ BrowserElementParent.prototype = {
   
   
   
+  
+  
   _handleCaretStateChanged: function(data) {
     let evt = this._createEvent('caretstatechanged', data.json,
                                  false);
