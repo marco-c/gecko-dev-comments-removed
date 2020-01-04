6b6ba0ea79ea1d44672ecb88bfda188c68471337
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 21:25:12 +0000

    Bug 1174386 - Make workers inherit the default Intl locale from the main thread, rather than using a bogus fallback value.  r=jwalden
    UltraBlame original commit: c3f06ba15fdad06f04e10291af175a15eb5870c2

--- js/src/jsapi.h
+++ js/src/jsapi.h
@@ -5065,6 +5065,12 @@ JS_SetDefaultLocale(JSContext* cx, const char* locale);
 
 
 
+extern JS_PUBLIC_API(JS::UniqueChars)
+JS_GetDefaultLocale(JSContext* cx);
+
+
+
+
 extern JS_PUBLIC_API(void)
 JS_ResetDefaultLocale(JSContext* cx);
 
