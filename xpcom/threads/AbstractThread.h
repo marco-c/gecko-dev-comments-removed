0e16b2d6b801ccd20bbab905cd86fb0551a9efc5
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 23:46:53 +0000

    Bug 1288618 - Part 6: Avoid unnecessarily allocating a TailDispatcher for XPCOMThreadWrapper. r=bholley
    UltraBlame original commit: fdee8d1538b02a69595f2c715cb9b93e9747c008

--- xpcom/threads/AbstractThread.h
+++ xpcom/threads/AbstractThread.h
@@ -67,6 +67,16 @@ public:
   virtual TaskDispatcher& TailDispatcher() = 0;
 
   
+  
+  virtual bool MightHaveTailTasks() { return true; }
+
+  
+  
+  
+  void TailDispatchTasksFor(AbstractThread* aThread);
+  bool HasTailTasksFor(AbstractThread* aThread);
+
+  
   bool SupportsTailDispatch() const { return mSupportsTailDispatch; }
 
   
