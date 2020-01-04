2d07e293e77d59f35b979430b14ae2b2cb7e8816
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 04:03:00 +0000

    bug 1217602 - remove nsIPKIParamBlock r=Cykesiopka
    nsIPKIParamBlock was unnecessary.
    
    UltraBlame original commit: 23f0dcfdcce1de9293704d6e59a679eed76eeede

--- security/manager/pki/nsNSSDialogHelper.h
+++ security/manager/pki/nsNSSDialogHelper.h
@@ -7,7 +7,8 @@
 #ifndef nsNSSDialogHelper_h
 #define nsNSSDialogHelper_h
 
-#include "nsIDOMWindow.h"
+class nsIDOMWindow;
+class nsISupports;
 
 
 
@@ -19,12 +20,8 @@ class nsNSSDialogHelper
 {
 public:
   
-  
-  static nsresult openDialog(
-                  nsIDOMWindow *window,
-                  const char *url,
-                  nsISupports *params,
-                  bool modal = true);
+  static nsresult openDialog(nsIDOMWindow* window, const char* url,
+                             nsISupports* params, bool modal = true);
 };
 
 #endif
