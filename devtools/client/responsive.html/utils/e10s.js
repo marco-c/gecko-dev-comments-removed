97dc6ac320e81739881e4ec9cd204a08f9f582ec
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 15:11:19 +0000

    Bug 1262594 - Rebuild screenshot capturing after iframe mozbrowser; r=jryans
    MozReview-Commit-ID: ASQujGzJa6s
    
    UltraBlame original commit: 849d0f0165a6b882894249ef4c465d989c002740

--- devtools/client/responsive.html/utils/e10s.js
+++ devtools/client/responsive.html/utils/e10s.js
@@ -4,20 +4,100 @@
 
 "use strict";
 
-const promise = require("promise");
+const { defer } = require("promise");
 
-module.exports = {
 
-  waitForMessage(mm, message) {
-    let deferred = promise.defer();
 
-    let onMessage = event => {
-      mm.removeMessageListener(message, onMessage);
-      deferred.resolve();
-    };
-    mm.addMessageListener(message, onMessage);
+const MESSAGE_PREFIX = "ResponsiveMode:";
+const REQUEST_DONE_SUFFIX = ":Done";
 
-    return deferred.promise;
-  },
 
-};
+
+
+
+
+
+
+
+
+
+function on(mm, message, listener) {
+  mm.addMessageListener(MESSAGE_PREFIX + message, listener);
+}
+exports.on = on;
+
+
+
+
+
+
+
+
+
+
+
+function off(mm, message, listener) {
+  mm.removeMessageListener(MESSAGE_PREFIX + message, listener);
+}
+exports.off = off;
+
+
+
+
+
+
+
+
+
+
+
+function once(mm, message) {
+  let { resolve, promise } = defer();
+
+  on(mm, message, function onMessage({data}) {
+    off(mm, message, onMessage);
+    resolve(data);
+  });
+
+  return promise;
+}
+exports.once = once;
+
+
+
+
+
+
+
+
+
+
+
+
+function emit(mm, message, data) {
+  mm.sendAsyncMessage(MESSAGE_PREFIX + message, data);
+}
+exports.emit = emit;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function request(mm, message, data) {
+  let done = once(mm, message + REQUEST_DONE_SUFFIX);
+
+  emit(mm, message, data);
+
+  return done;
+}
+exports.request = request;
