e5425db3d3a749ef7aaf746ae06ea65e36960944
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 03:43:38 +0000

    Bug 1215372 - Filter empty camera face detected events at gonk layer. r=dhylands
    UltraBlame original commit: b627b7dfb3cf498824cdfa7b49b6c77d52920490

--- dom/camera/GonkCameraControl.h
+++ dom/camera/GonkCameraControl.h
@@ -212,6 +212,8 @@ protected:
   nsCOMPtr<nsITimer>        mAutoFocusCompleteTimer;
   int32_t                   mAutoFocusCompleteExpired;
 
+  uint32_t                  mPrevFacesDetected;
+
   
   ReentrantMonitor          mReentrantMonitor;
 
