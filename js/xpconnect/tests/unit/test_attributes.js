51a0c5b3712715b33bb4172c917bba6e66eceb4a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 05:49:11 +0000

    bug 1224165 - Remove some Makefiles in js/xpconnect/tests/. r=glandium
    UltraBlame original commit: 068571096137ed86c98864eac60cc2b750b01688

--- js/xpconnect/tests/unit/test_attributes.js
+++ js/xpconnect/tests/unit/test_attributes.js
@@ -8,7 +8,7 @@ const Ci = Components.interfaces;
 function run_test() {
 
   
-  registerAppManifest(do_get_file('../components/native/xpctest.manifest'));
+  registerAppManifest(do_get_file('../components/native/chrome.manifest'));
   registerAppManifest(do_get_file('../components/js/xpctest.manifest'));
 
   
