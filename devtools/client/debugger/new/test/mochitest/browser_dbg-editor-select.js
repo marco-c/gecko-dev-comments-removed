6da08c73330dd6b8fba60977f65c35991b64eff8
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 23:48:47 +0000

    Bug 1302872 - update debugger bundle r=me UPDATE_BUNDLE
    UltraBlame original commit: 2af50b758415a1bd8c84b43731d6e5678feb19c8

--- devtools/client/debugger/new/test/mochitest/browser_dbg-editor-select.js
+++ devtools/client/debugger/new/test/mochitest/browser_dbg-editor-select.js
@@ -5,10 +5,10 @@
 
 
 
-function isBreakpointVisible(dbg) {
-  const bpLine = findElement(dbg, "breakpoint");
+function isElementVisible(dbg, elementName) {
+  const bpLine = findElement(dbg, elementName);
   const cm = findElement(dbg, "codeMirror");
-  ok(isVisibleWithin(cm, bpLine), "CodeMirror is scrolled to line");
+  return bpLine && isVisibleWithin(cm, bpLine);
 }
 
 add_task(function* () {
@@ -21,7 +21,7 @@ add_task(function* () {
   const simple2 = findSource(dbg, "simple2.js");
 
   
-  yield addBreakpoint(dbg, simple1.id, 4);
+  yield addBreakpoint(dbg, simple1, 4);
   ok(!getSelectedSource(getState()), "No selected source");
 
   
@@ -42,11 +42,11 @@ add_task(function* () {
 
   
   
-  const longSrc = findSource(dbg, "long.js");
-  yield addBreakpoint(dbg, longSrc.id, 66);
+  let longSrc = findSource(dbg, "long.js");
+  yield addBreakpoint(dbg, longSrc, 66);
 
   invokeInTab("testModel");
   yield waitForPaused(dbg);
   assertPausedLocation(dbg, longSrc, 66);
-  isBreakpointVisible(dbg);
+  ok(isElementVisible(dbg, "breakpoint"), "Breakpoint is visible");
 });
