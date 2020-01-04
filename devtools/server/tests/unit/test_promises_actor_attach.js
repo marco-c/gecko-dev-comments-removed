76b00327beca8cce95a007e72a7def6da37fb620
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 18:02:16 +0000

    Bug 1265728 - Decouple PromisesFront from PromisesActor; r=ejpbruel
    UltraBlame original commit: d6e0d9a0319f4d52cfdb15b691b50b4e1bd36079

--- devtools/server/tests/unit/test_promises_actor_attach.js
+++ devtools/server/tests/unit/test_promises_actor_attach.js
@@ -6,7 +6,7 @@
 
 
 
-const { PromisesFront } = require("devtools/server/actors/promises");
+const { PromisesFront } = require("devtools/shared/fronts/promises");
 
 add_task(function* () {
   let client = yield startTestDebuggerServer("promises-actor-test");
