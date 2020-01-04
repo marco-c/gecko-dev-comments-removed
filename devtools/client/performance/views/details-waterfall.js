bc87347e7530bf064115ac992248ff9d7b551c6e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 03:31:03 +0000

    Bug 1200485 - Repair view source URL in waterfall details. r=jsantell
    UltraBlame original commit: 712becb16698cd231ec5c1835215b96a8657941d

--- devtools/client/performance/views/details-waterfall.js
+++ devtools/client/performance/views/details-waterfall.js
@@ -130,7 +130,7 @@ var WaterfallView = Heritage.extend(DetailsSubview, {
 
 
   _onViewSource: function (_, data) {
-    gToolbox.viewSourceInDebugger(data.file, data.line);
+    gToolbox.viewSourceInDebugger(data.url, data.line);
   },
 
   
