caf248c06709099356fe7543553aba031231b79e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 14:18:07 +0000

    Bug 1254831 - Invalidate mfracs when linethickness changes. r=fredw
    UltraBlame original commit: c5403f96501802f26783f0c17500958f682a2593

--- layout/mathml/nsMathMLmfracFrame.cpp
+++ layout/mathml/nsMathMLmfracFrame.cpp
@@ -164,6 +164,20 @@ nsMathMLmfracFrame::BuildDisplayList(nsDisplayListBuilder*   aBuilder,
   }
 }
 
+
+nsresult
+nsMathMLmfracFrame::AttributeChanged(int32_t  aNameSpaceID,
+                                     nsIAtom* aAttribute,
+                                     int32_t  aModType)
+{
+  if (nsGkAtoms::linethickness_ == aAttribute) {
+    InvalidateFrame();
+  }
+  return
+    nsMathMLContainerFrame::AttributeChanged(aNameSpaceID, aAttribute,
+                                             aModType);
+}
+
  nsresult
 nsMathMLmfracFrame::MeasureForWidth(DrawTarget* aDrawTarget,
                                     nsHTMLReflowMetrics& aDesiredSize)
