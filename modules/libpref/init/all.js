47b24a5351f29c6b366e27e0a075d0927f6b5fae
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 15:10:37 +0000

    Bug 1033450 - Consolidate safebrowsing prefs in all.js. r=francois
    MozReview-Commit-ID: EA2H08dlCLl
    
    UltraBlame original commit: f71acdbd0d45ebdd0c69621dbf0f2f6ac74f9f43

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -442,7 +442,7 @@ pref("media.getusermedia.agc", 1);
 #if defined(XP_MACOSX)
 pref("media.peerconnection.capture_delay", 50);
 pref("media.getusermedia.playout_delay", 10);
-pref("media.navigator.audio.full_duplex", true);
+pref("media.navigator.audio.full_duplex", false);
 #elif defined(XP_WIN)
 pref("media.peerconnection.capture_delay", 50);
 pref("media.getusermedia.playout_delay", 40);
@@ -724,7 +724,6 @@ pref("gfx.prefer-mesa-llvmpipe", false);
 
 pref("gfx.draw-color-bars", false);
 
-pref("gfx.logging.painted-pixel-count.enabled", false);
 pref("gfx.logging.texture-usage.enabled", false);
 pref("gfx.logging.peak-texture-usage.enabled", false);
 
@@ -4957,6 +4956,7 @@ pref("dom.inter-app-communication-api.enabled", false);
 
 pref("dom.mapped_arraybuffer.enabled", false);
 
+#ifdef MOZ_SAFE_BROWSING
 
 pref("urlclassifier.malwareTable", "goog-malware-shavar,goog-unwanted-shavar,test-malware-simple,test-unwanted-simple");
 pref("urlclassifier.phishTable", "goog-phish-shavar,test-phish-simple");
@@ -4968,6 +4968,41 @@ pref("urlclassifier.disallow_completions", "test-malware-simple,test-phish-simpl
 
 pref("urlclassifier.trackingTable", "test-track-simple,mozstd-track-digest256");
 pref("urlclassifier.trackingWhitelistTable", "test-trackwhite-simple,mozstd-trackwhite-digest256");
+
+
+pref("urlclassifier.downloadBlockTable", "goog-badbinurl-shavar");
+
+
+pref("urlclassifier.gethashnoise", 4);
+
+
+pref("urlclassifier.gethash.timeout_ms", 5000);
+
+
+
+
+pref("urlclassifier.max-complete-age", 2700);
+
+pref("browser.safebrowsing.enabled", true);
+pref("browser.safebrowsing.malware.enabled", true);
+
+pref("browser.safebrowsing.downloads.remote.timeout_ms", 10000);
+pref("browser.safebrowsing.downloads.remote.url", "https://sb-ssl.google.com/safebrowsing/clientreport/download?key=%GOOGLE_API_KEY%");
+pref("browser.safebrowsing.downloads.remote.block_dangerous",            true);
+pref("browser.safebrowsing.downloads.remote.block_dangerous_host",       true);
+pref("browser.safebrowsing.downloads.remote.block_potentially_unwanted", false);
+pref("browser.safebrowsing.downloads.remote.block_uncommon",             false);
+pref("browser.safebrowsing.debug", false);
+
+pref("browser.safebrowsing.provider.google.lists", "goog-badbinurl-shavar,goog-downloadwhite-digest256,goog-phish-shavar,goog-malware-shavar,goog-unwanted-shavar");
+pref("browser.safebrowsing.provider.google.updateURL", "https://safebrowsing.google.com/safebrowsing/downloads?client=SAFEBROWSING_ID&appver=%VERSION%&pver=2.2&key=%GOOGLE_API_KEY%");
+pref("browser.safebrowsing.provider.google.gethashURL", "https://safebrowsing.google.com/safebrowsing/gethash?client=SAFEBROWSING_ID&appver=%VERSION%&pver=2.2");
+pref("browser.safebrowsing.provider.google.reportURL", "https://safebrowsing.google.com/safebrowsing/diagnostic?client=%NAME%&hl=%LOCALE%&site=");
+
+pref("browser.safebrowsing.reportPhishMistakeURL", "https://%LOCALE%.phish-error.mozilla.com/?hl=%LOCALE%&url=");
+pref("browser.safebrowsing.reportPhishURL", "https://%LOCALE%.phish-report.mozilla.com/?hl=%LOCALE%&url=");
+pref("browser.safebrowsing.reportMalwareMistakeURL", "https://%LOCALE%.malware-error.mozilla.com/?hl=%LOCALE%&url=");
+
 
 
 pref("browser.safebrowsing.forbiddenURIs.enabled", false);
@@ -4991,6 +5026,7 @@ pref("browser.safebrowsing.provider.mozilla.lists.mozfull.description", "mozfull
 
 
 pref("browser.safebrowsing.allowOverride", true);
+#endif
 
 
 pref("snav.enabled", false);
