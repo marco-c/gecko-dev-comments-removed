ab5f174c1013e90bec934cd01fb7501429c81e86
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 20:28:31 +0000

    Bug 1281179 - Create a docker image for Ubuntu 16.04 for use in tests. r=dustin
    MozReview-Commit-ID: ApHhev5J21x
    
    UltraBlame original commit: b1bc27d8f8a583ccdbbb05e1dc5829a11118d8af

--- taskcluster/taskgraph/transforms/tests/test_description.py
+++ taskcluster/taskgraph/transforms/tests/test_description.py
@@ -77,8 +77,8 @@ test_description_schema = Schema({
 
     
     Required('instance-size', default='default'): Any(
-        Any('default', 'xlarge'),
-        {'by-test-platform': {basestring: Any('default', 'xlarge')}},
+        Any('default', 'large', 'xlarge'),
+        {'by-test-platform': {basestring: Any('default', 'large', 'xlarge')}},
     ),
 
     
