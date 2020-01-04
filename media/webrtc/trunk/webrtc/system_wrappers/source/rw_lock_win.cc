7059a536424d7512d64fa54d3d601067a40b661f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 09:11:32 +0000

    Bug 1219339: switch GetStaticInstance to use IPC's Singleton<T> impl r=froyd
    UltraBlame original commit: 8ee641313088dc5fbc9eca9f870dcfe93de2843a

--- media/webrtc/trunk/webrtc/system_wrappers/source/rw_lock_win.cc
+++ media/webrtc/trunk/webrtc/system_wrappers/source/rw_lock_win.cc
@@ -69,7 +69,11 @@ bool RWLockWin::LoadModule() {
   if (!library) {
     return false;
   }
-  WEBRTC_TRACE(kTraceStateInfo, kTraceUtility, -1, "Loaded Kernel.dll");
+  
+  
+  
+  
+  
 
   initialize_srw_lock =
     (InitializeSRWLock)GetProcAddress(library, "InitializeSRWLock");
@@ -88,7 +92,7 @@ bool RWLockWin::LoadModule() {
   if (initialize_srw_lock && acquire_srw_lock_exclusive &&
       release_srw_lock_exclusive && acquire_srw_lock_shared &&
       release_srw_lock_shared) {
-    WEBRTC_TRACE(kTraceStateInfo, kTraceUtility, -1, "Loaded Native RW Lock");
+    
     native_rw_locks_supported = true;
   }
   return native_rw_locks_supported;
