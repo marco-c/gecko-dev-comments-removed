4462099c6e637fb056f6009ab91d49cd89d9f829
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 00:10:08 +0000

    Bug 1198229 - GCLI: Rename securityDesc in gclicommands.properties to reflect new content. r=ckerschb, r=flod
    UltraBlame original commit: f0aa386a944f2c2e963bab24d62cedda2e241579

--- toolkit/devtools/gcli/commands/security.js
+++ toolkit/devtools/gcli/commands/security.js
@@ -47,7 +47,7 @@ exports.items = [
   {
     
     name: "security",
-    description: l10n.lookup("securityDesc"),
+    description: l10n.lookup("securityPrivacyDesc"),
     manual: l10n.lookup("securityManual")
   },
   {
