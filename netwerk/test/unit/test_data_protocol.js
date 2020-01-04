cacd764cac1f4aba6c010e2d6a0e7c67d6bd1323
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 06:59:51 +0000

    Bug 1234369 - Convert 25 tests within netwerk/test to use AsyncOpen2 (r=mcmanus)
    UltraBlame original commit: 080ba4f54286a0b75bb5611f25b427b4af6cdb2d

--- netwerk/test/unit/test_data_protocol.js
+++ netwerk/test/unit/test_data_protocol.js
@@ -1,5 +1,5 @@
 
-Cu.import("resource://gre/modules/Services.jsm");
+Cu.import("resource://gre/modules/NetUtil.jsm");
 
 
 
@@ -40,16 +40,12 @@ function run_test() {
   for (var i = 0; i < urls.length; ++i) {
     dump("*** opening channel " + i + "\n");
     do_test_pending();
-    var chan = ios.newChannel2(urls[i][0],
-                               "",
-                               null,
-                               null,      
-                               Services.scriptSecurityManager.getSystemPrincipal(),
-                               null,      
-                               Ci.nsILoadInfo.SEC_NORMAL,
-                               Ci.nsIContentPolicy.TYPE_OTHER);
+    var chan = NetUtil.newChannel({
+      uri: urls[i][0],
+      loadUsingSystemPrincipal: true
+    });
     chan.contentType = "foo/bar"; 
-    chan.asyncOpen(new ChannelListener(on_read_complete, i), null);
+    chan.asyncOpen2(new ChannelListener(on_read_complete, i));
   }
 }
 
