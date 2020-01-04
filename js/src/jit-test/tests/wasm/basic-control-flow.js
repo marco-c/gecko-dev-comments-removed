c7a844304ff48a1298e547b0dd50b9f17b549efd
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 21:12:16 +0000

    Bug 1292126: Re-enable wasm test cases disabled; r=lth
    MozReview-Commit-ID: 6QnI9M9QSJs
    
    UltraBlame original commit: 3e859924d2f5a2b51cf11d07201a08b7cc520feb

--- js/src/jit-test/tests/wasm/basic-control-flow.js
+++ js/src/jit-test/tests/wasm/basic-control-flow.js
@@ -139,11 +139,9 @@ wasmEvalText('(module (func (if (i32.const 1) (i32.const 0) (if (i32.const 1) (i
 assertEq(wasmEvalText('(module (func (return)) (export "" 0))')(), undefined);
 assertEq(wasmEvalText('(module (func (result i32) (return (i32.const 1))) (export "" 0))')(), 1);
 assertEq(wasmEvalText('(module (func (if (return) (i32.const 0))) (export "" 0))')(), undefined);
+assertEq(wasmEvalText('(module (func (return (i32.const 1))) (export "" 0))')(), undefined);
 assertErrorMessage(() => wasmEvalText('(module (func (result f32) (return (i32.const 1))) (export "" 0))'), TypeError, mismatchError("i32", "f32"));
 assertThrowsInstanceOf(() => wasmEvalText('(module (func (result i32) (return)) (export "" 0))'), TypeError);
-
-
-
 
 
 
@@ -212,34 +210,34 @@ assertEq(wasmEvalText(`(module (func (result i32)
   (return (i32.const 2))
 ) (export "" 0))`)(), 1);
 
+var notcalled = false;
+var called = false;
+var imports = {
+    notcalled() {notcalled = true},
+    called() {called = true}
+};
+assertEq(wasmEvalText(`(module
+(import "notcalled" "")
+(import "called" "")
+(func
+  (block
+    (return (br 0))
+    (call_import 0)
+  )
+  (call_import 1)
+) (export "" 0))`, imports)(), undefined);
+assertEq(notcalled, false);
+assertEq(called, true);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+assertEq(wasmEvalText(`(module (func
+  (block
+    (i32.add
+      (i32.const 0)
+      (return (br 0))
+    )
+  )
+  (return)
+) (export "" 0))`)(), undefined);
 
 assertEq(wasmEvalText(`(module (func (result i32)
   (block
