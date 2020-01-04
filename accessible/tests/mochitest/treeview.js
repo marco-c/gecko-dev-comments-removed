1b3c5fc6dce2fa568f33fbb50699accc52539bf5
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 12:12:21 +0000

    Bug 1257128 (part 3) - Remove unused createAtom() function in a test. r=tbsaunde.
    UltraBlame original commit: 59fe9a7b94c7e4ced362db3fc46295dec48760a8

--- accessible/tests/mochitest/treeview.js
+++ accessible/tests/mochitest/treeview.js
@@ -277,12 +277,6 @@ function treeItem(aText, aOpen, aChildren)
     this.children = aChildren;
 }
 
-function createAtom(aName)
-{
-  return Components.classes["@mozilla.org/atom-service;1"]
-    .getService(Components.interfaces.nsIAtomService).getAtom(aName);
-}
-
 
 
 
