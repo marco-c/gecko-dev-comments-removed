2acf421e183cfec282344beac1350741c3aa10f3
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 03:31:06 +0000

    Bug 1174366 - Remove "Edit Site Settings" from Page menu. r=liuche
    UltraBlame original commit: b2fb2a45b78002e8ae246e2ea5342a11601a4a0a

--- mobile/android/base/BrowserApp.java
+++ mobile/android/base/BrowserApp.java
@@ -1289,15 +1289,6 @@ public class BrowserApp extends GeckoApp
             return true;
         }
 
-        if (itemId == R.id.site_settings) {
-            
-            GeckoAppShell.sendEventToGecko(GeckoEvent.createBroadcastEvent("Permissions:Get", null));
-            if (Versions.preHC) {
-                Telemetry.sendUIEvent(TelemetryContract.Event.ACTION, TelemetryContract.Method.CONTEXT_MENU, "site_settings");
-            }
-            return true;
-        }
-
         if (itemId == R.id.paste) {
             String text = Clipboard.getText();
             if (!TextUtils.isEmpty(text)) {
@@ -3174,7 +3165,6 @@ public class BrowserApp extends GeckoApp
             }
             MenuUtils.safeSetEnabled(aMenu, R.id.subscribe, false);
             MenuUtils.safeSetEnabled(aMenu, R.id.add_search_engine, false);
-            MenuUtils.safeSetEnabled(aMenu, R.id.site_settings, false);
             MenuUtils.safeSetEnabled(aMenu, R.id.add_to_launcher, false);
 
             return true;
@@ -3260,7 +3250,6 @@ public class BrowserApp extends GeckoApp
         }
         MenuUtils.safeSetEnabled(aMenu, R.id.subscribe, tab.hasFeeds());
         MenuUtils.safeSetEnabled(aMenu, R.id.add_search_engine, tab.hasOpenSearch());
-        MenuUtils.safeSetEnabled(aMenu, R.id.site_settings, !isAboutHome(tab));
         MenuUtils.safeSetEnabled(aMenu, R.id.add_to_launcher, !isAboutHome(tab));
 
         
