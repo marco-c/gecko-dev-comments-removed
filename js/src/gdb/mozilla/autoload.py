b422c00b5f46e5578727afaf095cd8852927065d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 20:02:54 +0000

    Bug 1281572 - Add gdb commands for calling iongraph with an instance of a MIRGenerator. r=sstangl
    UltraBlame original commit: d6f1c31cfcc4aaed9b5fa219b5e728dd6082fa1c

--- js/src/gdb/mozilla/autoload.py
+++ js/src/gdb/mozilla/autoload.py
@@ -10,6 +10,7 @@ import mozilla.prettyprinters
 
 import mozilla.GCCellPtr
 import mozilla.Interpreter
+import mozilla.IonGraph
 import mozilla.JSObject
 import mozilla.JSString
 import mozilla.JSSymbol
