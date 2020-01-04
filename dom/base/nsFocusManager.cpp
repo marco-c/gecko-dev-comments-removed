fa66740021303a356223ae627fecc46b7058fd01
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 02:18:25 +0000

    Bug 1208789, clear starting node before traversing backwards by document, r=smaug
    UltraBlame original commit: 8305fc7ffd1c2adc305f9047276dd2c1aed3c1c5

--- dom/base/nsFocusManager.cpp
+++ dom/base/nsFocusManager.cpp
@@ -2469,7 +2469,11 @@ nsFocusManager::DetermineElementToMoveFocus(nsPIDOMWindow* aWindow,
       nsCOMPtr<nsPIDOMWindow> focusedWindow;
       startContent = GetFocusedDescendant(aWindow, true, getter_AddRefs(focusedWindow));
     }
-    else {
+    else if (aType != MOVEFOCUS_LASTDOC) {
+      
+      
+      
+      
       startContent = aWindow->GetFocusedNode();
     }
   }
