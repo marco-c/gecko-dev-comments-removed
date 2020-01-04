fdfc8b69aab258d12e912d591384f8b9406da66d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 14:39:14 +0000

    Bug 1259473 - per comment 14, move actions involving |this| to Init() from the constructor. r=jya.
    UltraBlame original commit: 52fd577f808f21db74f8ad98c3cc82a20bae486e

--- dom/media/MediaDecoderStateMachine.h
+++ dom/media/MediaDecoderStateMachine.h
@@ -142,7 +142,7 @@ public:
                            MediaDecoderReader* aReader,
                            bool aRealTime = false);
 
-  nsresult Init();
+  nsresult Init(MediaDecoder* aDecoder);
 
   
   enum State {
