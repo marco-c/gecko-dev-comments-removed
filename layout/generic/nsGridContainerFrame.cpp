0328d431cebc058e158cc24ed164f8de3cdf11ff
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 11:52:53 +0000

    Bug 1233191 part 1 - Implement sanity checks on the flex/grid container child frame list.  Remove the anon grid item sanity checks that the frame constructor now does instead.  r=dholbert
    UltraBlame original commit: 61933d3dbe1c112564d5203a8536fbc3f0f0cfef

--- layout/generic/nsGridContainerFrame.cpp
+++ layout/generic/nsGridContainerFrame.cpp
@@ -5343,45 +5343,12 @@ nsGridContainerFrame::MergeSortedExcessOverflowContainers(nsFrameList& aList)
 }
 
 #ifdef DEBUG
-static bool
-FrameWantsToBeInAnonymousGridItem(nsIFrame* aFrame)
-{
-  
-  
-  return aFrame->IsFrameOfType(nsIFrame::eLineParticipant);
-}
-
-
-
-
-
-
-
 void
 nsGridContainerFrame::SetInitialChildList(ChildListID  aListID,
                                           nsFrameList& aChildList)
 {
   ChildListIDs supportedLists = kAbsoluteList | kFixedList | kPrincipalList;
   MOZ_ASSERT(supportedLists.Contains(aListID), "unexpected child list");
-
-  if (aListID == kPrincipalList) {
-    bool prevChildWasAnonGridItem = false;
-    for (nsFrameList::Enumerator e(aChildList); !e.AtEnd(); e.Next()) {
-      nsIFrame* child = e.get();
-      MOZ_ASSERT(!FrameWantsToBeInAnonymousGridItem(child),
-                 "frame wants to be inside an anonymous grid item, but it isn't");
-      if (child->StyleContext()->GetPseudo() ==
-            nsCSSAnonBoxes::anonymousGridItem) {
-        MOZ_ASSERT(!prevChildWasAnonGridItem, "two anon grid items in a row");
-        nsIFrame* firstWrappedChild = child->PrincipalChildList().FirstChild();
-        MOZ_ASSERT(firstWrappedChild,
-                   "anonymous grid item is empty (shouldn't happen)");
-        prevChildWasAnonGridItem = true;
-      } else {
-        prevChildWasAnonGridItem = false;
-      }
-    }
-  }
 
   return nsContainerFrame::SetInitialChildList(aListID, aChildList);
 }
