82b50dfdaef35f982e584a73552ed8c8b95f945f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 15:24:30 +0000

    Back out 3 changesets (bug 1265194) for Linux failures in test_bug625187.html and test_dom_prompts.html and Android failures in test_bug619644.html CLOSED TREE
    Backed out changeset 5a90b42789cd (bug 1265194)
    Backed out changeset b9346277145b (bug 1265194)
    Backed out changeset 755257b3f436 (bug 1265194)
    
    UltraBlame original commit: de2a25e8925636c5491901f78256c779372e2719

--- toolkit/components/prompts/test/prompt_common.js
+++ toolkit/components/prompts/test/prompt_common.js
@@ -3,74 +3,121 @@ const Cc = SpecialPowers.Cc;
 ok(Ci != null, "Access Ci");
 ok(Cc != null, "Access Cc");
 
+var didDialog;
+
 var isSelectDialog = false;
 var isTabModal = false;
-if (SpecialPowers.getBoolPref("prompts.tab_modal.enabled")) {
-    isTabModal = true;
-}
+var usePromptService = true;
 
-var isOSX = ("nsILocalFileMac" in SpecialPowers.Ci);
-var isLinux = ("@mozilla.org/gnome-gconf-service;1" in SpecialPowers.Cc);
-var isE10S = SpecialPowers.Services.appinfo.processType == 2;
-
-
-var gChromeScript = SpecialPowers.loadChromeScript(SimpleTest.getTestFileURL("chromeScript.js"));
-SimpleTest.registerCleanupFunction(() => gChromeScript.destroy());
-
-
-function handlePrompt() {
-  return new Promise(resolve => {
-    gChromeScript.addMessageListener("promptHandled", function handled(msg) {
-      gChromeScript.removeMessageListener("promptHandled", handled);
-      checkPromptState(msg.promptState, state);
-      resolve(true);
-    });
-    gChromeScript.sendAsyncMessage("handlePrompt", { action: action, isTabModal: isTabModal});
-  });
-}
-
-function checkPromptState(promptState, expectedState) {
-    
-    is(promptState.msg,         expectedState.msg,         "Checking expected message");
-    if (isOSX && !isTabModal)
-      ok(!promptState.titleHidden, "Checking title always visible on OS X");
-    else
-      is(promptState.titleHidden, expectedState.titleHidden, "Checking title visibility");
-    is(promptState.textHidden,  expectedState.textHidden,  "Checking textbox visibility");
-    is(promptState.passHidden,  expectedState.passHidden,  "Checking passbox visibility");
-    is(promptState.checkHidden, expectedState.checkHidden, "Checking checkbox visibility");
-    is(promptState.checkMsg,    expectedState.checkMsg,    "Checking checkbox label");
-    is(promptState.checked,     expectedState.checked,     "Checking checkbox checked");
-    if (!isTabModal)
-      is(promptState.iconClass, "spaced " + expectedState.iconClass, "Checking expected icon CSS class");
-    is(promptState.textValue, expectedState.textValue, "Checking textbox value");
-    is(promptState.passValue, expectedState.passValue, "Checking passbox value");
-
-    if (expectedState.butt0Label) {
-        is(promptState.butt0Label, expectedState.butt0Label, "Checking accept-button label");
-    }
-    if (expectedState.butt1Label) {
-        is(promptState.butt1Label, expectedState.butt1Label, "Checking cancel-button label");
-    }
-    if (expectedState.butt2Label) {
-        is(promptState.butt2Label, expectedState.butt2Label, "Checking extra1-button label");
-    }
+var timer; 
+function startCallbackTimer() {
+    didDialog = false;
 
     
-    if (expectedState.butt0Disabled) {
-        is(promptState.butt0Disabled, true,  "Checking accept-button is disabled");
-        is(promptState.butt1Disabled, false, "Checking cancel-button isn't disabled");
+    const dialogDelay = 10;
+
+    
+    timer = Cc["@mozilla.org/timer;1"].createInstance(Ci.nsITimer);
+    timer.init(observer, dialogDelay, Ci.nsITimer.TYPE_ONE_SHOT);
+}
+
+
+var observer = {
+    QueryInterface : SpecialPowers.wrapCallback(function (iid) {
+        const interfaces = [Ci.nsIObserver,
+                            Ci.nsISupports, Ci.nsISupportsWeakReference];
+
+        if (!interfaces.some( function(v) { return iid.equals(v) } ))
+            throw SpecialPowers.Cr.NS_ERROR_NO_INTERFACE;
+        return this;
+    }),
+
+    observe : SpecialPowers.wrapCallback(function (subject, topic, data) {
+      try {
+        if (isTabModal) {
+          var promptBox = getTabModalPromptBox(window);
+          ok(promptBox, "got tabmodal promptbox");
+          var prompts = SpecialPowers.wrap(promptBox).listPrompts();
+          if (prompts.length)
+              handleDialog(prompts[0].Dialog.ui, testNum);
+          else
+              startCallbackTimer(); 
+        } else {
+          var doc = getDialogDoc();
+          if (isSelectDialog && doc)
+              handleDialog(doc, testNum);
+          else if (doc)
+              handleDialog(doc.defaultView.Dialog.ui, testNum);
+          else
+              startCallbackTimer(); 
+        }
+      } catch (e) {
+        ok(false, "Exception thrown in the timer callback: " + e + " at " + (e.fileName || e.filename) + ":" + (e.lineNumber || e.linenumber));
+      }
+    })
+};
+
+function getTabModalPromptBox(domWin) {
+    var promptBox = null;
+
+    
+    function getChromeWindow(aWindow) {
+        var chromeWin = SpecialPowers.wrap(aWindow).QueryInterface(Ci.nsIInterfaceRequestor)
+                               .getInterface(Ci.nsIWebNavigation)
+                               .QueryInterface(Ci.nsIDocShell)
+                               .chromeEventHandler.ownerDocument.defaultView;
+        return chromeWin;
     }
 
-    is(promptState.defButton0, expectedState.defButton == "button0", "checking button0 default");
-    is(promptState.defButton1, expectedState.defButton == "button1", "checking button1 default");
-    is(promptState.defButton2, expectedState.defButton == "button2", "checking button2 default");
+    try {
+        
+        var promptWin = domWin.top;
 
-    if (isLinux && (!promptState.focused || isE10S)) {
-        todo(false, "Focus seems missing or wrong on Linux"); 
-    } else if (isOSX && expectedState.focused && expectedState.focused.startsWith("button")) {
-        is(promptState.focused, "infoBody", "buttons don't focus on OS X, but infoBody does instead");
-    } else {
-        is(promptState.focused, expectedState.focused, "Checking focused element");
+        
+        var chromeWin = getChromeWindow(promptWin);
+
+        if (chromeWin.getTabModalPromptBox)
+            promptBox = chromeWin.getTabModalPromptBox(promptWin);
+    } catch (e) {
+        
     }
+
+    
+    return SpecialPowers.unwrap(promptBox);
+}
+
+function getDialogDoc() {
+  
+  
+  var wm = Cc["@mozilla.org/appshell/window-mediator;1"].
+           getService(Ci.nsIWindowMediator);
+  
+  var enumerator = wm.getXULWindowEnumerator(null);
+
+  while (enumerator.hasMoreElements()) {
+    var win = enumerator.getNext();
+    var windowDocShell = win.QueryInterface(Ci.nsIXULWindow).docShell;
+
+    var containedDocShells = windowDocShell.getDocShellEnumerator(
+                                      Ci.nsIDocShellTreeItem.typeChrome,
+                                      Ci.nsIDocShell.ENUMERATE_FORWARDS);
+    while (containedDocShells.hasMoreElements()) {
+        
+        var childDocShell = containedDocShells.getNext();
+        
+        if (childDocShell.busyFlags != Ci.nsIDocShell.BUSY_FLAGS_NONE)
+          continue;
+        var childDoc = childDocShell.QueryInterface(Ci.nsIDocShell)
+                                    .contentViewer
+                                    .DOMDocument;
+
+        
+        if (childDoc.location.href == "chrome://global/content/commonDialog.xul")
+          return childDoc;
+        if (childDoc.location.href == "chrome://global/content/selectDialog.xul")
+          return childDoc;
+    }
+  }
+
+  return null;
 }
