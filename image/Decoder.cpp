d373d964b6dfc4f3fc08887c2c89358515c09ce6
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 19:36:00 +0000

    Bug 1285865 (Part 5) - Return a Maybe<TerminalState> from Decoder::DoDecode(). r=edwin
    UltraBlame original commit: 1c717a8dc382b5aeee20f4231488c5ea2320e890

--- image/Decoder.cpp
+++ image/Decoder.cpp
@@ -152,7 +152,12 @@ Decoder::Decode(NotNull<IResumable*> aOnResume)
       AutoRecordDecoderTelemetry telemetry(this, mIterator->Length());
 
       
-      DoDecode(mIterator->Data(), mIterator->Length());
+      Maybe<TerminalState> terminalState =
+        DoDecode(mIterator->Data(), mIterator->Length());
+
+      if (terminalState == Some(TerminalState::FAILURE)) {
+        PostDataError();
+      }
     }
   }
 
--- image/decoders/nsICODecoder.h
+++ image/decoders/nsICODecoder.h
@@ -70,7 +70,7 @@ public:
   
   size_t FirstResourceOffset() const;
 
-  void DoDecode(const char* aBuffer, size_t aLength) override;
+  Maybe<TerminalState> DoDecode(const char* aBuffer, size_t aLength) override;
   virtual void FinishInternal() override;
   virtual void FinishWithErrorInternal() override;
 
