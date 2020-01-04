1c10eadf09f630ec8698ffeed768da84a3ffb416
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 01:36:33 +0000

    Bug 1206073 - [xpcshell] Fix default value for testingModulesDir when running from mach, r=jgraham
    UltraBlame original commit: 7ea485aa45dab4bfe47598149ba452f7d9c2db78

--- testing/xpcshell/runxpcshelltests.py
+++ testing/xpcshell/runxpcshelltests.py
@@ -1093,8 +1093,7 @@ class XPCShellTests(object):
         
         
         if not testingModulesDir:
-            ourDir = os.path.dirname(__file__)
-            possible = os.path.join(ourDir, os.path.pardir, 'modules')
+            possible = os.path.join(here, os.path.pardir, 'modules')
 
             if os.path.isdir(possible):
                 testingModulesDir = possible
