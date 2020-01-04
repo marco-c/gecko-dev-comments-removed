c2bea29e58db5353bca7b069878e0ac78e217c72
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 18:15:55 +0000

    Bug 1270310 - Part 1: Make allocation in ConvertStringLineBreaks fallible. r=froydnj, r=smaug
    ConvertStringLineBreaks calls ConvertUnicharLineBreaksInSitu which uses
    fallible allocation. We should make the potential allocation in |BeginWriting|
    fallible as well and handle the failure. This also updates the callers to
    |ConvertStringLineBreaks| to handle the error properly in release builds.
    
    UltraBlame original commit: 39cf86d4874699644b4c2a97ff2b2a3e9fe0e1cc

--- xpcom/io/nsLinebreakConverter.cpp
+++ xpcom/io/nsLinebreakConverter.cpp
@@ -464,7 +464,9 @@ nsLinebreakConverter::ConvertStringLineBreaks(nsString& aIoString,
   
   
   nsString::char_iterator stringBuf;
-  aIoString.BeginWriting(stringBuf);
+  if (!aIoString.BeginWriting(stringBuf, fallible)) {
+    return NS_ERROR_OUT_OF_MEMORY;
+  }
 
   int32_t    newLen;
 
