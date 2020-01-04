8a9bc951375fc5d1d06bd0593d55af6563b7df66
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 05:05:56 +0000

    Bug 1221758 - don't run cache2 tests when cache2 isn't used. r=michal.novotny
    Was causing test failures for Thunderbird where cache2 isn't in use yet.
    
    UltraBlame original commit: 80a79987da8b7d11e63c40c1cdd7da63297d0782

--- netwerk/test/unit/test_cache2-30a-entry-pinning.js
+++ netwerk/test/unit/test_cache2-30a-entry-pinning.js
@@ -1,6 +1,10 @@
 function run_test()
 {
   do_get_profile();
+  if (!newCacheBackEndUsed()) {
+    do_check_true(true, "This test checks only cache2 specific behavior.");
+    return;
+  }
 
   
   asyncOpenCacheEntry("http://a/", "pin", Ci.nsICacheStorage.OPEN_TRUNCATE, LoadContextInfo.default,
