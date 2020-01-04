be19348ce1f612f0f0b288aecbfaacb605379286
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 07:08:42 +0000

    bug 1211849 - Remove canClear support from the Sanitizer. r=mak Modifications by Patrick <nwokopuni.coventry.ac.uk>
    UltraBlame original commit: ea9e749bb86bfb584e8591959ac57278bf43c0e4

--- browser/base/content/test/general/browser_bug409624.js
+++ browser/base/content/test/general/browser_bug409624.js
@@ -5,21 +5,24 @@
 XPCOMUtils.defineLazyModuleGetter(this, "FormHistory",
                                   "resource://gre/modules/FormHistory.jsm");
 
-function test() {
-  waitForExplicitFinish();
-
+add_task(function* test() {
   
   
-  FormHistory.update({ op: "remove" },
-                     { handleError: function (error) {
-                         do_throw("Error occurred updating form history: " + error);
-                       },
-                       handleCompletion: function (reason) { if (!reason) test2(); },
-                     });
-}
+  yield new Promise(resolve => {
+    FormHistory.update({ op: "remove" },
+                       { handleError(error) {
+                           do_throw("Error occurred updating form history: " + error);
+                         },
+                         handleCompletion(reason) {
+                           if (!reason) {
+                             resolve();
+                           } else {
+                             reject();
+                           }
+                         },
+                       });
+  });
 
-function test2()
-{
   let prefService = Cc["@mozilla.org/preferences-service;1"]
                     .getService(Components.interfaces.nsIPrefService);
 
@@ -45,29 +48,13 @@ function test2()
   prefBranch.setBoolPref("siteSettings", false);
 
   
-  s.sanitize().then(function() {
-    s.canClearItem("formdata", clearDone1, s);
-  });
-}
+  yield s.sanitize();
+  ok(!gFindBar.hasTransactions, "pre-test baseline for sanitizer");
 
-function clearDone1(aItemName, aResult, aSanitizer)
-{
-  ok(!aResult, "pre-test baseline for sanitizer");
   gFindBar.getElement("findbar-textbox").value = "m";
-  aSanitizer.canClearItem("formdata", inputEntered, aSanitizer);
-}
+  ok(gFindBar.hasTransactions, "formdata can be cleared after input");
 
-function inputEntered(aItemName, aResult, aSanitizer)
-{
-  ok(aResult, "formdata can be cleared after input");
-  aSanitizer.sanitize().then(function() {
-    aSanitizer.canClearItem("formdata", clearDone2);
-  });
-}
-
-function clearDone2(aItemName, aResult)
-{
+  yield s.sanitize();
   is(gFindBar.getElement("findbar-textbox").value, "", "findBar textbox should be empty after sanitize");
-  ok(!aResult, "canClear now false after sanitize");
-  finish();
-}
+  ok(!gFindBar.hasTransactions, "No transactions after sanitize");
+});
