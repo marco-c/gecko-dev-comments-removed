ddd0b2eea6b71b15472db3aed5082d327e9c37d1
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 21:16:08 +0000

    Bug 1287631 - fix typo in Services.appinfo use in devtools; r=fitzgen
    MozReview-Commit-ID: 1HTz3uRgMfT
    
    UltraBlame original commit: 39e38323858434bba15e4e28b18d77969b9d29f2

--- devtools/shared/heapsnapshot/tests/unit/head_heapsnapshot.js
+++ devtools/shared/heapsnapshot/tests/unit/head_heapsnapshot.js
@@ -29,8 +29,7 @@ const { LabelAndShallowSizeVisitor } = DominatorTreeNode;
 
 
 
-if (Services.appInfo &&
-    Services.appInfo.processType == Services.appInfo.PROCESS_TYPE_DEFAULT) {
+if (Services.appinfo.processType == Services.appinfo.PROCESS_TYPE_DEFAULT) {
   Services.prefs.setBoolPref("devtools.debugger.log", true);
 }
 DevToolsUtils.dumpn.wantLogging = true;
