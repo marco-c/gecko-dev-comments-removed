e8b29cedad30c44a35037e59db71efdf70dfcd7e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 06:45:37 +0000

    Bug 864780 - Changed handling of the format chunk to skip any extension. r=cpearce
    UltraBlame original commit: 27def71e2d0b2acccfdfb67b267c5f1028be038e

--- dom/media/test/manifest.js
+++ dom/media/test/manifest.js
@@ -155,6 +155,8 @@ var gPlayTests = [
   
   { name:"r16000_u8_c1_list.wav", type:"audio/x-wav", duration:4.2 },
   
+  { name:"16bit_wave_extrametadata.wav", type:"audio/x-wav", duration:1.108 },
+  
   { name:"wavedata_s24.wav", type:"audio/x-wav", duration:1.0 },
 
   
