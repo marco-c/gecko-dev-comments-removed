4a74e59c9521b069b7f56294b1f9cd7ded723db5
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 12:16:36 +0000

    Bug 1258055 - E10S helpers for mochitest-plain password manager tests not involving UI. r=dolske
    * Support changing `selfFilling` in the `setupParent` message since these tests aren't self-filling like the ones already working in e10s.
    ** Make selfFilling = false work with e10s using a registerRunTests message.
    * Register a common cleanup function to remove all logins, remove all disabled hosts, and clear the HTTP auth state in the parent.
    ** Actually check there are no existing logins in commonInit (uncomment code)
    ** Delete some removeLogin calls at the end of tests now that we removeAllLogins during cleanup
    * Consolidate loadParentTestFile with runInParent
    
    MozReview-Commit-ID: IS5pPMiM12U
    
    UltraBlame original commit: 5585f64b747d73ca4e677936cd528c3393f6df8f

--- toolkit/components/passwordmgr/test/pwmgr_common.js
+++ toolkit/components/passwordmgr/test/pwmgr_common.js
@@ -130,16 +130,12 @@ function commonInit(selfFilling) {
               getService(SpecialPowers.Ci.nsILoginManager);
   ok(pwmgr != null, "Access LoginManager");
 
-
   
   var logins = pwmgr.getAllLogins();
-  if (logins.length) {
-    
-    pwmgr.removeAllLogins();
-  }
+  is(logins.length, 0, "Not expecting logins to be present");
   var disabledHosts = pwmgr.getAllDisabledHosts();
   if (disabledHosts.length) {
-    
+    ok(false, "Warning: wasn't expecting disabled hosts to be present.");
     for (var host of disabledHosts)
       pwmgr.setLoginSavingEnabled(host, true);
   }
@@ -160,7 +156,11 @@ function commonInit(selfFilling) {
   if (selfFilling)
     return;
 
-  registerRunTests();
+  if (this.sendAsyncMessage) {
+    sendAsyncMessage("registerRunTests");
+  } else {
+    registerRunTests();
+  }
 }
 
 function registerRunTests() {
@@ -282,13 +282,19 @@ function promiseFormsProcessed(expectedCount = 1) {
   });
 }
 
-function loadParentTestFile(aRelativeFilePath) {
-  let fileURL = SimpleTest.getTestFileURL(aRelativeFilePath);
-  let testScript = SpecialPowers.loadChromeScript(fileURL);
-  SimpleTest.registerCleanupFunction(function destroyChromeScript() {
-    testScript.destroy();
+
+
+
+
+
+
+
+function runInParent(aFunctionOrURL) {
+  let chromeScript = SpecialPowers.loadChromeScript(aFunctionOrURL);
+  SimpleTest.registerCleanupFunction(() => {
+    chromeScript.destroy();
   });
-  return testScript;
+  return chromeScript;
 }
 
 
@@ -296,13 +302,14 @@ function loadParentTestFile(aRelativeFilePath) {
 
 
 
-
-function runFunctionInParent(aFunction) {
-  let chromeScript = SpecialPowers.loadChromeScript(aFunction);
-  SimpleTest.registerCleanupFunction(() => {
-    chromeScript.destroy();
-  });
-  return chromeScript;
+function runChecksAfterCommonInit(aFunction = null) {
+  SimpleTest.waitForExplicitFinish();
+  let pwmgrCommonScript = runInParent(SimpleTest.getTestFileURL("pwmgr_common.js"));
+  if (aFunction) {
+    window.addEventListener("runTests", aFunction);
+    pwmgrCommonScript.addMessageListener("registerRunTests", () => registerRunTests());
+  }
+  pwmgrCommonScript.sendSyncMessage("setupParent");
 }
 
 
@@ -315,8 +322,8 @@ if (this.addMessageListener) {
 
   Cu.import("resource://gre/modules/Task.jsm");
 
-  addMessageListener("setupParent", () => {
-    commonInit(true);
+  addMessageListener("setupParent", ({selfFilling = false} = {selfFilling: false}) => {
+    commonInit(selfFilling);
     sendAsyncMessage("doneSetup");
   });
 
@@ -334,11 +341,24 @@ if (this.addMessageListener) {
 } else {
   
   SimpleTest.registerCleanupFunction(() => {
-    let recipeParent = getRecipeParent();
-    if (!recipeParent) {
+    runInParent(function cleanupParent() {
+      const { classes: Cc, interfaces: Ci, results: Cr, utils: Cu } = Components;
+      Cu.import("resource://gre/modules/Services.jsm");
+      Cu.import("resource://gre/modules/LoginManagerParent.jsm");
+
       
-      return;
-    }
-    recipeParent.then(recipeParent => recipeParent.reset());
+      Services.logins.removeAllLogins();
+
+      let disabledHosts = Services.logins.getAllDisabledHosts();
+      disabledHosts.forEach(host => Services.logins.setLoginSavingEnabled(host, true));
+
+      let authMgr = Cc["@mozilla.org/network/http-auth-manager;1"].
+                    getService(Ci.nsIHttpAuthManager);
+      authMgr.clearAll();
+
+      if (LoginManagerParent._recipeManager) {
+        LoginManagerParent._recipeManager.reset();
+      }
+    });
   });
 }
