39b1829d491aaa8b7cbd92f2d882924f1bafd031
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 09:51:44 +0000

    Bug 1245414, part 3 - Overwrite mfbt/decimal/Decimal.* with vanilla upstream copies. r=Waldo
    UltraBlame original commit: d1ca6010b131f5401c608c63054f3123490fd0cc

--- mfbt/decimal/Decimal.h
+++ mfbt/decimal/Decimal.h
@@ -28,45 +28,28 @@
 
 
 
-
-
-
-
-
-
 #ifndef Decimal_h
 #define Decimal_h
 
-#include "mozilla/Assertions.h"
+#include "platform/PlatformExport.h"
+#include "wtf/Allocator.h"
+#include "wtf/Assertions.h"
+#include "wtf/text/WTFString.h"
 #include <stdint.h>
-#include "mozilla/Types.h"
 
-#include <string>
-
-#ifndef ASSERT
-#define DEFINED_ASSERT_FOR_DECIMAL_H 1
-#define ASSERT MOZ_ASSERT
-#endif
-
-
-
-
-#define WTF_MAKE_FAST_ALLOCATED \
-  void ignore_this_dummy_method() = delete
-
-namespace WebCore {
+namespace blink {
 
 namespace DecimalPrivate {
 class SpecialValueHandler;
-} 
+}
 
 
 
 
 
 
-class Decimal {
-    WTF_MAKE_FAST_ALLOCATED;
+class PLATFORM_EXPORT Decimal {
+    USING_FAST_MALLOC(Decimal);
 public:
     enum Sign {
         Positive,
@@ -75,6 +58,7 @@ public:
 
     
     class EncodedData {
+        DISALLOW_NEW();
         
         friend class Decimal;
         friend class DecimalPrivate::SpecialValueHandler;
@@ -93,7 +77,7 @@ public:
         bool isSpecial() const { return m_formatClass == ClassInfinity || m_formatClass == ClassNaN; }
         bool isZero() const { return m_formatClass == ClassZero; }
         Sign sign() const { return m_sign; }
-        void setSign(Sign aSign) { m_sign = aSign; }
+        void setSign(Sign sign) { m_sign = sign; }
 
     private:
         enum FormatClass {
@@ -112,29 +96,29 @@ public:
         Sign m_sign;
     };
 
-    MFBT_API explicit Decimal(int32_t = 0);
-    MFBT_API Decimal(Sign, int exponent, uint64_t coefficient);
-    MFBT_API Decimal(const Decimal&);
+    Decimal(int32_t = 0);
+    Decimal(Sign, int exponent, uint64_t coefficient);
+    Decimal(const Decimal&);
 
-    MFBT_API Decimal& operator=(const Decimal&);
-    MFBT_API Decimal& operator+=(const Decimal&);
-    MFBT_API Decimal& operator-=(const Decimal&);
-    MFBT_API Decimal& operator*=(const Decimal&);
-    MFBT_API Decimal& operator/=(const Decimal&);
+    Decimal& operator=(const Decimal&);
+    Decimal& operator+=(const Decimal&);
+    Decimal& operator-=(const Decimal&);
+    Decimal& operator*=(const Decimal&);
+    Decimal& operator/=(const Decimal&);
 
-    MFBT_API Decimal operator-() const;
+    Decimal operator-() const;
 
-    MFBT_API bool operator==(const Decimal&) const;
-    MFBT_API bool operator!=(const Decimal&) const;
-    MFBT_API bool operator<(const Decimal&) const;
-    MFBT_API bool operator<=(const Decimal&) const;
-    MFBT_API bool operator>(const Decimal&) const;
-    MFBT_API bool operator>=(const Decimal&) const;
+    bool operator==(const Decimal&) const;
+    bool operator!=(const Decimal&) const;
+    bool operator<(const Decimal&) const;
+    bool operator<=(const Decimal&) const;
+    bool operator>(const Decimal&) const;
+    bool operator>=(const Decimal&) const;
 
-    MFBT_API Decimal operator+(const Decimal&) const;
-    MFBT_API Decimal operator-(const Decimal&) const;
-    MFBT_API Decimal operator*(const Decimal&) const;
-    MFBT_API Decimal operator/(const Decimal&) const;
+    Decimal operator+(const Decimal&) const;
+    Decimal operator-(const Decimal&) const;
+    Decimal operator*(const Decimal&) const;
+    Decimal operator/(const Decimal&) const;
 
     int exponent() const
     {
@@ -150,18 +134,17 @@ public:
     bool isSpecial() const { return m_data.isSpecial(); }
     bool isZero() const { return m_data.isZero(); }
 
-    MFBT_API Decimal abs() const;
-    MFBT_API Decimal ceiling() const;
-    MFBT_API Decimal floor() const;
-    MFBT_API Decimal remainder(const Decimal&) const;
-    MFBT_API Decimal round() const;
+    Decimal abs() const;
+    Decimal ceil() const;
+    Decimal floor() const;
+    Decimal remainder(const Decimal&) const;
+    Decimal round() const;
 
-    MFBT_API double toDouble() const;
+    double toDouble() const;
     
-    MFBT_API std::string toString() const;
-    MFBT_API bool toString(char* strBuf, size_t bufLength) const;
+    String toString() const;
 
-    static MFBT_API Decimal fromDouble(double);
+    static Decimal fromDouble(double);
     
     
     
@@ -169,13 +152,13 @@ public:
     
     
     
-    static MFBT_API Decimal fromString(const std::string& aValue);
-    static MFBT_API Decimal infinity(Sign);
-    static MFBT_API Decimal nan();
-    static MFBT_API Decimal zero(Sign);
+    static Decimal fromString(const String&);
+    static Decimal infinity(Sign);
+    static Decimal nan();
+    static Decimal zero(Sign);
 
     
-    MFBT_API explicit Decimal(const EncodedData&);
+    explicit Decimal(const EncodedData&);
     const EncodedData& value() const { return m_data; }
 
 private:
@@ -185,10 +168,10 @@ private:
         int exponent;
     };
 
-    MFBT_API explicit Decimal(double);
-    MFBT_API Decimal compareTo(const Decimal&) const;
+    Decimal(double);
+    Decimal compareTo(const Decimal&) const;
 
-    static MFBT_API AlignedOperands alignOperands(const Decimal& lhs, const Decimal& rhs);
+    static AlignedOperands alignOperands(const Decimal& lhs, const Decimal& rhs);
     static inline Sign invertSign(Sign sign) { return sign == Negative ? Positive : Negative; }
 
     Sign sign() const { return m_data.sign(); }
@@ -198,16 +181,4 @@ private:
 
 } 
 
-namespace mozilla {
-  typedef WebCore::Decimal Decimal;
-} 
-
-#undef WTF_MAKE_FAST_ALLOCATED
-
-#ifdef DEFINED_ASSERT_FOR_DECIMAL_H
-#undef DEFINED_ASSERT_FOR_DECIMAL_H
-#undef ASSERT
-#endif
-
 #endif 
-
