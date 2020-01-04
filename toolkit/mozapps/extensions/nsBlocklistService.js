c4ed3d971b6117701bd01164f7f3c34064320d8d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 16:19:25 +0000

    Bug 1266235 - Rename KintoBlocklist to blocklist-clients,r=mgoodwin
    MozReview-Commit-ID: Eus7sHqN6GW
    
    UltraBlame original commit: 5f321f10da1ef1dc6f0592375fb8447a597f5e55

--- toolkit/mozapps/extensions/nsBlocklistService.js
+++ toolkit/mozapps/extensions/nsBlocklistService.js
@@ -632,10 +632,10 @@ Blocklist.prototype = {
 
     
     if (gPref.getBoolPref(PREF_KINTO_UPDATE_ENABLED)) {
-      let KintoUpdater =
+      const updater =
         Components.utils.import("resource://services-common/blocklist-updater.js",
                                 {});
-      KintoUpdater.checkVersions().catch(() => {
+      updater.checkVersions().catch(() => {
         
         
       });
