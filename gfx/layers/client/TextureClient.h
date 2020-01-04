f2213324b8b5c2277f62150ac59176130c609188
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 05:42:10 +0000

    Bug 1200595 - SharedSurface TextureData implementation. r=jgilbert
    UltraBlame original commit: fc1fbb97c8eb3d999933cf3290e43928d02f61eb

--- gfx/layers/client/TextureClient.h
+++ gfx/layers/client/TextureClient.h
@@ -535,6 +535,7 @@ public:
 
   
   virtual TextureData* GetInternalData() { return nullptr; }
+  virtual const TextureData* GetInternalData() const { return nullptr; }
 
 private:
   static void TextureClientRecycleCallback(TextureClient* aClient, void* aClosure);
@@ -704,6 +705,7 @@ public:
 
   
   virtual TextureData* GetInternalData() override { return mData; }
+  virtual const TextureData* GetInternalData() const override { return mData; }
 
   virtual void FinalizeOnIPDLThread() override;
 
