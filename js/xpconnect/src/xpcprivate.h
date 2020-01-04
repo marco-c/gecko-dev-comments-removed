a458a23db847622d939de78861505a1589734043
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 03:20:23 +0000

    Bug 1198934 - Support special-case WebExtension behavior for TypedArrays. r=billm
    UltraBlame original commit: de3d8a5f653738926bea21fc41f9166773919496

--- js/xpconnect/src/xpcprivate.h
+++ js/xpconnect/src/xpcprivate.h
@@ -3462,6 +3462,7 @@ public:
         , allowWaivers(true)
         , wantComponents(true)
         , wantExportHelpers(false)
+        , isWebExtensionContentScript(false)
         , proto(cx)
         , addonId(cx)
         , writeToGlobalPrototype(false)
@@ -3478,6 +3479,7 @@ public:
     bool allowWaivers;
     bool wantComponents;
     bool wantExportHelpers;
+    bool isWebExtensionContentScript;
     JS::RootedObject proto;
     nsCString sandboxName;
     JS::RootedString addonId;
@@ -3680,6 +3682,7 @@ public:
         , allowWaivers(true)
         , writeToGlobalPrototype(false)
         , skipWriteToGlobalPrototype(false)
+        , isWebExtensionContentScript(false)
         , universalXPConnectEnabled(false)
         , forcePermissiveCOWs(false)
         , scriptability(c)
@@ -3725,6 +3728,10 @@ public:
     
     
     bool skipWriteToGlobalPrototype;
+
+    
+    
+    bool isWebExtensionContentScript;
 
     
     
