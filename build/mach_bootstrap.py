45b309efc749b895eac158c14dc668f74fdec91b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 10:49:47 +0000

    Bug 1252601 - Handle requests errors gracefully when submitting telemetry data r=gps
    This catches the exception and prevents further attempts to post to
    telemetry in this command invocation.
    
    MozReview-Commit-ID: JDW0zUS8fjF
    
    UltraBlame original commit: 79914dafd20146221d85545cb87075164cef8c65

--- build/mach_bootstrap.py
+++ build/mach_bootstrap.py
@@ -375,8 +375,12 @@ def bootstrap(topsrcdir, mozilla_dir=None):
                 continue
             with open(path, 'r') as f:
                 data = f.read()
-                r = session.post(BUILD_TELEMETRY_SERVER, data=data,
-                                 headers={'Content-Type': 'application/json'})
+                try:
+                    r = session.post(BUILD_TELEMETRY_SERVER, data=data,
+                                     headers={'Content-Type': 'application/json'})
+                except Exception as e:
+                    print('Exception posting to telemetry server: %s' % str(e))
+                    break
                 
                 
                 if r.status_code != 200:
