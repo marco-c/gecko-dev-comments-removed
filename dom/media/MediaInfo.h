0c6a497d7a20aa5bd8542ad5545df0b0f6c66c63
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 14:30:03 +0000

    Bug 1248861: P8. Added ChannelLayout::MappingTable method. r=gerald
    MozReview-Commit-ID: 5kRa08TpGTX
    
    UltraBlame original commit: b326f17035c14bf743b80455d5cef193c82fe748

--- dom/media/MediaInfo.h
+++ dom/media/MediaInfo.h
@@ -527,6 +527,16 @@ public:
     {
       return mChannelMap;
     }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    bool MappingTable(const ChannelLayout& aOther, uint8_t* aMap = nullptr) const;
     bool IsValid() const {
       return mValid;
     }
