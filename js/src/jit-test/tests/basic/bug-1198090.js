201c5f014a795cb474bae8c2b2c77f909df6b6f0
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 00:07:34 +0000

    Bug 1198090 - Use AutoInitGCManagedObject when creating UnboxedLayout in TryConvertToUnboxedLayout() r=terrence
    UltraBlame original commit: c3988a4230d27749414ec2fbd50459d595923baf

---
+++ js/src/jit-test/tests/basic/bug-1198090.js
@@ -0,0 +1,12 @@
+
+
+if (!('oomAtAllocation' in this))
+    quit();
+
+for (let a of [
+        null, function() {}, function() {}, null, function() {}, function() {},
+        function() {}, null, null, null, null, null, null, null, null,
+        function() {}, null, null, null, function() {}
+    ]) {
+    oomAtAllocation(5);
+}
