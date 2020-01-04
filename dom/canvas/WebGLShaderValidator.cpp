c8e1ab31946977f3ec1f57737ea1a148553f6cb0
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 12:14:00 +0000

    Bug 1244611 - "Using named uniform buffer objects in the fragment shader fails". r=jgilbert
    UltraBlame original commit: c5a53b4755381106a4d1b4dda9d57c3345bd79a3

--- dom/canvas/WebGLShaderValidator.cpp
+++ dom/canvas/WebGLShaderValidator.cpp
@@ -474,13 +474,43 @@ ShaderValidator::FindUniformByMappedName(const std::string& mappedName,
         return true;
     }
 
+    const size_t dotPos = mappedName.find(".");
+
     const std::vector<sh::InterfaceBlock>& interfaces = *ShGetInterfaceBlocks(mHandle);
     for (const auto& interface : interfaces) {
+
+        std::string mappedFieldName;
+        const bool hasInstanceName = !interface.instanceName.empty();
+
+        
+        
+        
+        if (hasInstanceName) {
+
+            
+            if (std::string::npos == dotPos)
+                continue;
+
+            
+            const std::string mappedInterfaceBlockName = mappedName.substr(0, dotPos);
+            if (interface.mappedName != mappedInterfaceBlockName)
+                continue;
+
+            mappedFieldName = mappedName.substr(dotPos + 1);
+        } else {
+            mappedFieldName = mappedName;
+        }
+
         for (const auto& field : interface.fields) {
             const sh::ShaderVariable* found;
 
-            if (!field.findInfoByMappedName(mappedName, &found, out_userName))
+            if (!field.findInfoByMappedName(mappedFieldName, &found, out_userName))
                 continue;
+
+            if (hasInstanceName) {
+                
+                *out_userName = interface.name + "." + *out_userName;
+            }
 
             *out_isArray = found->isArray();
             return true;
