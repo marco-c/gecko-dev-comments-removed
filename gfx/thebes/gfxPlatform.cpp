f1571c32bdfd11be8ef7455fb89abac1b48b6a26
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 06:13:51 +0000

    Bug 990017. Prevent skia accelerated canvas backends from rendering content. r=jmuizelaar
    UltraBlame original commit: 4feee02dd1d6a7a2dab8834622f23f4a242c630c

--- gfx/thebes/gfxPlatform.cpp
+++ gfx/thebes/gfxPlatform.cpp
@@ -1116,6 +1116,16 @@ gfxPlatform::SupportsAzureContentForDrawTarget(DrawTarget* aTarget)
     return false;
   }
 
+#ifdef USE_SKIA_GPU
+ 
+ 
+ if ((aTarget->GetType() == DrawTargetType::HARDWARE_RASTER)
+     && (aTarget->GetBackendType() ==  BackendType::SKIA))
+ {
+  return false;
+ }
+#endif
+
   return SupportsAzureContentForType(aTarget->GetBackendType());
 }
 
