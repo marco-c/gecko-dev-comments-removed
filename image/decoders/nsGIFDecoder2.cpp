e62ec6cb31019d735316f87fa553d6b7a061747f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 03:58:14 +0000

    Bug 1213744 (Part 2) - Clamp the GIF frame rect to the visible rect for DDD and don't decode outside it. r=tn
    UltraBlame original commit: 61d1f8675fc39e0fd1abb9828bbd30e324eb3234

--- image/decoders/nsGIFDecoder2.cpp
+++ image/decoders/nsGIFDecoder2.cpp
@@ -207,7 +207,7 @@ nsGIFDecoder2::BeginGIF()
 }
 
 bool
-nsGIFDecoder2::CheckForTransparency(IntRect aFrameRect)
+nsGIFDecoder2::CheckForTransparency(const IntRect& aFrameRect)
 {
   
   if (mGIFStruct.is_transparent) {
@@ -229,6 +229,22 @@ nsGIFDecoder2::CheckForTransparency(IntRect aFrameRect)
   }
 
   return false;
+}
+
+IntRect
+nsGIFDecoder2::ClampToImageRect(const IntRect& aRect)
+{
+  IntRect imageRect(0, 0, mGIFStruct.screen_width, mGIFStruct.screen_height);
+  IntRect visibleFrameRect = aRect.Intersect(imageRect);
+
+  
+  
+  
+  if (visibleFrameRect.IsEmpty()) {
+    visibleFrameRect.MoveTo(0, 0);
+  }
+
+  return visibleFrameRect;
 }
 
 
@@ -275,8 +291,8 @@ nsGIFDecoder2::BeginImageFrame(uint16_t aDepth)
   }
 
   if (mDownscaler) {
-    rv = mDownscaler->BeginFrame(GetSize(), Some(frameRect), mImageData,
-                                 hasTransparency);
+    rv = mDownscaler->BeginFrame(GetSize(), Some(ClampToImageRect(frameRect)),
+                                 mImageData, hasTransparency);
   }
 
   return rv;
@@ -487,6 +503,9 @@ nsGIFDecoder2::DoLzw(const uint8_t* q)
   if (!mGIFStruct.rows_remaining) {
     return true;
   }
+  if (MOZ_UNLIKELY(mDownscaler && mDownscaler->IsFrameComplete())) {
+    return true;
+  }
 
   
   
@@ -541,6 +560,10 @@ nsGIFDecoder2::DoLzw(const uint8_t* q)
       if (code == (clear_code + 1)) {
         
         return (mGIFStruct.rows_remaining == 0);
+      }
+
+      if (MOZ_UNLIKELY(mDownscaler && mDownscaler->IsFrameComplete())) {
+        goto END;
       }
 
       if (oldcode == -1) {
