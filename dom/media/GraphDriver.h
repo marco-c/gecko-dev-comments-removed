32cfe1ba95587d5e1b3a7cd0a8305571ba5d229f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 20:40:46 +0000

    Bug 1289678 - Don't count audio stream creation failures when retrying on Telemetry. r=kinetik
    When failing to create an audio stream, we fallback to a SystemClockDriver
    marked as being a "fallback driver". When failing again to open an audio stream
    after re-trying, we can check whether we came from a fallback driver, and not
    report the failure again to telemetry.
    
    MozReview-Commit-ID: FAdQ0pCtC3m
    
    UltraBlame original commit: 58f01ab6d8560966453ee42d2f1f07c02c99b1e3

--- dom/media/GraphDriver.h
+++ dom/media/GraphDriver.h
@@ -54,6 +54,7 @@ class MediaStreamGraphImpl;
 
 class AudioCallbackDriver;
 class OfflineClockDriver;
+class SystemClockDriver;
 
 
 
@@ -162,6 +163,10 @@ public:
   }
 
   virtual OfflineClockDriver* AsOfflineClockDriver() {
+    return nullptr;
+  }
+
+  virtual SystemClockDriver* AsSystemClockDriver() {
     return nullptr;
   }
 
@@ -296,13 +301,20 @@ public:
   MediaTime GetIntervalForIteration() override;
   void WaitForNextIteration() override;
   void WakeUp() override;
-
+  void MarkAsFallback();
+  bool IsFallback();
+  SystemClockDriver* AsSystemClockDriver() {
+    return this;
+  }
 
 private:
   
   
   TimeStamp mInitialTimeStamp;
   TimeStamp mLastTimeStamp;
+  
+  
+  bool mIsFallback;
 };
 
 
@@ -525,6 +537,9 @@ private:
 
 
   bool mMicrophoneActive;
+  
+
+  bool mFromFallback;
 };
 
 class AsyncCubebTask : public Runnable
