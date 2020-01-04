d512345d41b318aac098270e6b2c6f07677436c8
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 05:57:06 +0000

    Bug 1226627 - Use fmin[f]/fmax[f] when clamping floating point values (perf).  r=roc
    UltraBlame original commit: 673461c0b772b7a63fc9eb4115ae682c3a9c32e1

--- gfx/src/nsCoord.h
+++ gfx/src/nsCoord.h
@@ -110,6 +110,8 @@ inline nscoord NSToCoordRoundWithClamp(float aValue)
   if (aValue <= nscoord_MIN) {
     return nscoord_MIN;
   }
+  
+  
 #endif
   return NSToCoordRound(aValue);
 }
@@ -259,12 +261,8 @@ inline nscoord NSToCoordFloorClamped(float aValue)
 {
 #ifndef NS_COORD_IS_FLOAT
   
-  if (aValue >= nscoord_MAX) {
-    return nscoord_MAX;
-  }
-  if (aValue <= nscoord_MIN) {
-    return nscoord_MIN;
-  }
+  aValue = fminf(aValue, nscoord_MAX);
+  aValue = fmaxf(aValue, nscoord_MIN);
 #endif
   return NSToCoordFloor(aValue);
 }
@@ -283,12 +281,8 @@ inline nscoord NSToCoordCeilClamped(double aValue)
 {
 #ifndef NS_COORD_IS_FLOAT
   
-  if (aValue >= nscoord_MAX) {
-    return nscoord_MAX;
-  }
-  if (aValue <= nscoord_MIN) {
-    return nscoord_MIN;
-  }
+  aValue = fmin(aValue, nscoord_MAX);
+  aValue = fmax(aValue, nscoord_MIN);
 #endif
   return NSToCoordCeil(aValue);
 }
@@ -315,12 +309,8 @@ inline nscoord NSToCoordTruncClamped(float aValue)
 {
 #ifndef NS_COORD_IS_FLOAT
   
-  if (aValue >= nscoord_MAX) {
-    return nscoord_MAX;
-  }
-  if (aValue <= nscoord_MIN) {
-    return nscoord_MIN;
-  }
+  aValue = fminf(aValue, nscoord_MAX);
+  aValue = fmaxf(aValue, nscoord_MIN);
 #endif
   return NSToCoordTrunc(aValue);
 }
@@ -329,12 +319,8 @@ inline nscoord NSToCoordTruncClamped(double aValue)
 {
 #ifndef NS_COORD_IS_FLOAT
   
-  if (aValue >= nscoord_MAX) {
-    return nscoord_MAX;
-  }
-  if (aValue <= nscoord_MIN) {
-    return nscoord_MIN;
-  }
+  aValue = fmin(aValue, nscoord_MAX);
+  aValue = fmax(aValue, nscoord_MIN);
 #endif
   return NSToCoordTrunc(aValue);
 }
