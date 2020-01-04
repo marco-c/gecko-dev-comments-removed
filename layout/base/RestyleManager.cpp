4e51cf8c16fff637bb89a82b80944f962c9fa073
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 14:22:52 +0000

    Bug 1140268 - Set up mCBReflowState for the 'bogus' parent reflow state used for RestyleManager::RecomputePosition. r=dbaron
    UltraBlame original commit: ffff3be72a1dd4117ac1880acad402caf517b5f2

--- layout/base/RestyleManager.cpp
+++ layout/base/RestyleManager.cpp
@@ -494,6 +494,23 @@ RestyleManager::RecomputePosition(nsIFrame* aFrame)
   parentFrame->RemoveStateBits(~nsFrameState(0));
   parentFrame->AddStateBits(savedState);
 
+  
+  
+  
+  
+  Maybe<nsHTMLReflowState> cbReflowState;
+  nsIFrame* cbFrame = parentFrame->GetContainingBlock();
+  if (cbFrame && (aFrame->GetContainingBlock() != parentFrame ||
+                  parentFrame->GetType() == nsGkAtoms::tableFrame)) {
+    LogicalSize cbSize = cbFrame->GetLogicalSize();
+    cbReflowState.emplace(cbFrame->PresContext(), cbFrame, &rc, cbSize);
+    cbReflowState->ComputedPhysicalMargin() = cbFrame->GetUsedMargin();
+    cbReflowState->ComputedPhysicalPadding() = cbFrame->GetUsedPadding();
+    cbReflowState->ComputedPhysicalBorderPadding() =
+      cbFrame->GetUsedBorderAndPadding();
+    parentReflowState.mCBReflowState = cbReflowState.ptr();
+  }
+
   NS_WARN_IF_FALSE(parentSize.ISize(parentWM) != NS_INTRINSICSIZE &&
                    parentSize.BSize(parentWM) != NS_INTRINSICSIZE,
                    "parentSize should be valid");
