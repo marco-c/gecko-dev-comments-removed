7adf4e154a99ae93eabb42be1131e427433aa86a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 18:04:39 +0000

    Backed out changeset 452617569aeb (bug 1278294) for build bustage CLOSED TREE
    UltraBlame original commit: acc2acfacebd8f54c2d0afb22972936e16ec3dbd

--- accessible/generic/DocAccessible.cpp
+++ accessible/generic/DocAccessible.cpp
@@ -1856,8 +1856,16 @@ DocAccessible::UpdateTreeOnRemoval(Accessible* aContainer, nsIContent* aChildNod
   
   Accessible* child = GetAccessible(aChildNode);
 #ifdef A11Y_LOG
-    logging::TreeInfo("process content removal", 0,
-                      "container", aContainer, "child", aChildNode);
+  if (logging::IsEnabled(logging::eTree)) {
+    logging::MsgBegin("TREE", "process content removal");
+    logging::Node("container", aContainer->GetNode());
+    logging::Node("child", aChildNode);
+    if (child)
+      logging::Address("child", child);
+    else
+      logging::MsgEntry("child accessible: null");
+
+    logging::MsgEnd();
   }
 #endif
 
