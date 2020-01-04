cd286ea89593f9ea337c8b3b3e1dbf0fbd32c1bd
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 03:01:40 +0000

    Backout Bug 1188483 - desktop_l10n.py should support --locale $locale:$revision r=jlund DONTBUILD
    UltraBlame original commit: 06d223e905d55b79bb46d3777d27623fad32b34a

--- testing/mozharness/scripts/desktop_l10n.py
+++ testing/mozharness/scripts/desktop_l10n.py
@@ -60,6 +60,7 @@ PyMakeIgnoreList = [
 
 configuration_tokens = ('branch',
                         'platform',
+                        'en_us_binary_url',
                         'update_platform',
                         'update_channel',
                         'ssh_key_dir',
