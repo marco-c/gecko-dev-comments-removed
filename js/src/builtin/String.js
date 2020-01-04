2ec2a078fc35e2a48a1c56c0fa6635cf177175dd
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 07:15:25 +0000

    Add a semicolon to avoid relying on ASI in one place in String.js.  No bug, r=trivial
    UltraBlame original commit: 271a099ca0fb3e000f14e38c42e818d04a0b59c6

--- js/src/builtin/String.js
+++ js/src/builtin/String.js
@@ -68,7 +68,7 @@ function String_substr(start, length) {
         intStart = std_Math_max(intStart + size, 0);
 
     
-    var resultLength = std_Math_min(std_Math_max(end, 0), size - intStart)
+    var resultLength = std_Math_min(std_Math_max(end, 0), size - intStart);
 
     
     if (resultLength <= 0)
