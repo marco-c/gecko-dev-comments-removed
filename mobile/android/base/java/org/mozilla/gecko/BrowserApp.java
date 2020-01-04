3c3bca63aeaee751662b1d221f68249cfc6fae0d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 16:54:50 +0000

    Bug 1266199 - Rename BrowserApp.searchEngineManager to mSearchEngineManager. r=mcomella
    UltraBlame original commit: bf8d6578893967e467eb3b37617b8c12493b7272

--- mobile/android/base/java/org/mozilla/gecko/BrowserApp.java
+++ mobile/android/base/java/org/mozilla/gecko/BrowserApp.java
@@ -317,7 +317,7 @@ public class BrowserApp extends GeckoApp
     ));
 
     @NonNull
-    private SearchEngineManager searchEngineManager; 
+    private SearchEngineManager mSearchEngineManager; 
 
     private TelemetryDispatcher mTelemetryDispatcher;
     private final SessionMeasurements mSessionMeasurements = new SessionMeasurements();
@@ -702,7 +702,7 @@ public class BrowserApp extends GeckoApp
         final Distribution distribution = Distribution.init(this);
         distribution.addOnDistributionReadyCallback(new DistributionStoreCallback(this, profile.getName()));
 
-        searchEngineManager = new SearchEngineManager(this, distribution);
+        mSearchEngineManager = new SearchEngineManager(this, distribution);
         mTelemetryDispatcher = new TelemetryDispatcher(profile.getDir().getAbsolutePath());
 
         
@@ -1092,7 +1092,7 @@ public class BrowserApp extends GeckoApp
         
         
         
-        searchEngineManager.getEngine(new UploadTelemetryCorePingCallback(BrowserApp.this));
+        mSearchEngineManager.getEngine(new UploadTelemetryCorePingCallback(BrowserApp.this));
 
         for (final BrowserAppDelegate delegate : delegates) {
             delegate.onStart(this);
@@ -1393,7 +1393,7 @@ public class BrowserApp extends GeckoApp
             mZoomedView.destroy();
         }
 
-        searchEngineManager.unregisterListeners();
+        mSearchEngineManager.unregisterListeners();
 
         EventDispatcher.getInstance().unregisterGeckoThreadListener((GeckoEventListener) this,
             "Gecko:DelayedStartup",
