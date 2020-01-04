6898e4eb444127d22d45db2bc4e5cfb636d73340
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 06:22:27 +0000

    Bug 1230397 - Write safe mode pref on the main thread r=dbaron
    UltraBlame original commit: 30de91786784c52313483251a045a9e3c2eea42e

--- widget/gonk/nsAppShell.cpp
+++ widget/gonk/nsAppShell.cpp
@@ -937,8 +937,13 @@ nsAppShell::CheckPowerKey()
     
     
     
-    Preferences::SetCString("b2g.safe_mode",
-                            (powerState == AKEY_STATE_DOWN) ? "yes" : "no");
+    nsCOMPtr<nsIRunnable> prefSetter = 
+    NS_NewRunnableFunction([powerState] () -> void {
+        Preferences::SetCString("b2g.safe_mode",
+                                (powerState == AKEY_STATE_DOWN) ? "yes" : "no");
+    });
+    NS_DispatchToMainThread(prefSetter.forget());
+
     mPowerKeyChecked = true;
 }
 
