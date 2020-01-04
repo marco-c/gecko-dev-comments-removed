7798131d4434a57eaaff411bd9be745d3a9e613d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 22:57:35 +0000

    Bug 1299661 - Use hg --template flag instead of -T. r=gps
    Versions of mercurial older than 3.0 don't support the -T shorthand for
    the --template option. While most people should be using something
    newer, it can still happen that some run an older version, and it's
    still trivial to support them by using the long option.
    
    UltraBlame original commit: c7e1ff331aff8ad0bf46a00256ae0601c2593a4b

--- python/mozboot/mozboot/bootstrap.py
+++ python/mozboot/mozboot/bootstrap.py
@@ -417,7 +417,7 @@ def current_firefox_checkout(check_output, hg=None):
         if hg and os.path.exists(hg_dir):
             
             try:
-                node = check_output([hg, 'log', '-r', '0', '-T', '{node}'], cwd=path)
+                node = check_output([hg, 'log', '-r', '0', '--template', '{node}'], cwd=path)
                 if node in HG_ROOT_REVISIONS:
                     return 'hg'
                 
