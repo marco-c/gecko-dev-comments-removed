59c3f262c930608e9f4b2c4ac6801914c2544ff6
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 00:03:56 +0000

    Bug 116953 - 4 - Move traits out of controller; r=tromey
    UltraBlame original commit: 3bcdcfa7f9bebddbf07d9b48d2cd34530e8052bd

--- browser/devtools/animationinspector/animation-controller.js
+++ browser/devtools/animationinspector/animation-controller.js
@@ -75,6 +75,38 @@ function destroy() {
 
 
 
+let getServerTraits = Task.async(function*(target) {
+  let config = [{
+    name: "hasToggleAll", actor: "animations", method: "toggleAll"
+  }, {
+    name: "hasSetCurrentTime", actor: "animationplayer", method: "setCurrentTime"
+  }, {
+    name: "hasMutationEvents", actor: "animations", method: "stopAnimationPlayerUpdates"
+  }, {
+    name: "hasSetPlaybackRate", actor: "animationplayer", method: "setPlaybackRate"
+  }, {
+    name: "hasTargetNode", actor: "domwalker", method: "getNodeFromActor"
+  }, {
+    name: "hasSetCurrentTimes", actor: "animations", method: "setCurrentTimes"
+  }];
+
+  let traits = {};
+  for (let {name, actor, method} of config) {
+    traits[name] = yield target.actorHasMethod(actor, method);
+  }
+
+  
+  traits.isNewUI = Services.prefs.getBoolPref("devtools.inspector.animationInspectorV3");
+
+  return traits;
+});
+
+
+
+
+
+
+
 
 
 
@@ -106,18 +138,7 @@ let AnimationsController = {
     this.animationsFront = new AnimationsFront(target.client, target.form);
 
     
-    this.hasToggleAll = yield target.actorHasMethod("animations", "toggleAll");
-    this.hasSetCurrentTime = yield target.actorHasMethod("animationplayer",
-                                                         "setCurrentTime");
-    this.hasMutationEvents = yield target.actorHasMethod("animations",
-                                                         "stopAnimationPlayerUpdates");
-    this.hasSetPlaybackRate = yield target.actorHasMethod("animationplayer",
-                                                          "setPlaybackRate");
-    this.hasTargetNode = yield target.actorHasMethod("domwalker",
-                                                     "getNodeFromActor");
-    this.hasSetCurrentTimes = yield target.actorHasMethod("animations",
-                                                          "setCurrentTimes");
-    this.isNewUI = Services.prefs.getBoolPref("devtools.inspector.animationInspectorV3");
+    this.traits = yield getServerTraits(target);
 
     if (this.destroyed) {
       console.warn("Could not fully initialize the AnimationsController");
@@ -212,7 +233,7 @@ let AnimationsController = {
 
 
   toggleAll: function() {
-    if (!this.hasToggleAll) {
+    if (!this.traits.hasToggleAll) {
       return promise.resolve();
     }
 
@@ -229,7 +250,7 @@ let AnimationsController = {
 
 
   setCurrentTimeAll: Task.async(function*(time, shouldPause) {
-    if (this.hasSetCurrentTimes) {
+    if (this.traits.hasSetCurrentTimes) {
       yield this.animationsFront.setCurrentTimes(this.animationPlayers, time,
                                                  shouldPause);
     } else {
@@ -256,7 +277,7 @@ let AnimationsController = {
 
     
     
-    if (!this.isListeningToMutations && this.hasMutationEvents) {
+    if (!this.isListeningToMutations && this.traits.hasMutationEvents) {
       this.animationsFront.on("mutations", this.onAnimationMutations);
       this.isListeningToMutations = true;
     }
@@ -268,13 +289,13 @@ let AnimationsController = {
     for (let {type, player} of changes) {
       if (type === "added") {
         this.animationPlayers.push(player);
-        if (!this.isNewUI) {
+        if (!this.traits.isNewUI) {
           player.startAutoRefresh();
         }
       }
 
       if (type === "removed") {
-        if (!this.isNewUI) {
+        if (!this.traits.isNewUI) {
           player.stopAutoRefresh();
         }
         yield player.release();
@@ -288,7 +309,7 @@ let AnimationsController = {
   }),
 
   startAllAutoRefresh: function() {
-    if (this.isNewUI) {
+    if (this.traits.isNewUI) {
       return;
     }
 
@@ -298,7 +319,7 @@ let AnimationsController = {
   },
 
   stopAllAutoRefresh: function() {
-    if (this.isNewUI) {
+    if (this.traits.isNewUI) {
       return;
     }
 
@@ -311,7 +332,7 @@ let AnimationsController = {
     
     
     
-    if (this.hasMutationEvents) {
+    if (this.traits.hasMutationEvents) {
       yield this.animationsFront.stopAnimationPlayerUpdates();
     }
     this.stopAllAutoRefresh();
