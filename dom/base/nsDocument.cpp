474c0bb786a5913ad31a8a02349678b636a49c05
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 02:57:34 +0000

    Bug 1187801 part 2 - Put clearing fullscreen state on element into an independent function. r=smaug
    UltraBlame original commit: e9a3138359cdef0b3145299b653ba201e03b721b

--- dom/base/nsDocument.cpp
+++ dom/base/nsDocument.cpp
@@ -11354,6 +11354,15 @@ UpdateViewportScrollbarOverrideForFullscreen(nsIDocument* aDoc)
   }
 }
 
+static void
+ClearFullscreenStateOnElement(Element* aElement)
+{
+  
+  aElement->DeleteProperty(nsGkAtoms::vr_state);
+  
+  EventStateManager::SetFullScreenState(aElement, false);
+}
+
 void
 nsDocument::CleanupFullscreenState()
 {
@@ -11366,9 +11375,7 @@ nsDocument::CleanupFullscreenState()
   
   for (nsWeakPtr& weakPtr : Reversed(mFullScreenStack)) {
     if (nsCOMPtr<Element> element = do_QueryReferent(weakPtr)) {
-      
-      element->DeleteProperty(nsGkAtoms::vr_state);
-      EventStateManager::SetFullScreenState(element, false);
+      ClearFullscreenStateOnElement(element);
     }
   }
   mFullScreenStack.Clear();
@@ -11398,13 +11405,7 @@ nsDocument::FullScreenStackPop()
     return;
   }
 
-  Element* top = FullScreenStackTop();
-
-  
-  top->DeleteProperty(nsGkAtoms::vr_state);
-
-  
-  EventStateManager::SetFullScreenState(top, false);
+  ClearFullscreenStateOnElement(FullScreenStackTop());
 
   
   
