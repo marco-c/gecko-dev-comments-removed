0f7c5c2f53e6fb3a7786daf2e02139be6ad230e6
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 20:03:41 +0000

    Bug 1280637 - Implement tsan (thread sanitizer), asan (address sanitizer), and msan (uninitialized memory) jobs, r=terrence
    MozReview-Commit-ID: JuFVqAb9C4s
    
    UltraBlame original commit: 4126bd41ce8b12ecbb4bc2ec510bba009017a035

--- js/src/devtools/automation/autospider.py
+++ js/src/devtools/automation/autospider.py
@@ -11,19 +11,21 @@ import shutil
 import subprocess
 import sys
 
-from collections import namedtuple
+from collections import Counter, namedtuple
 from os import environ as env
 from subprocess import Popen
 from threading import Timer
 
-Dirs = namedtuple('Dirs', ['scripts', 'js_src', 'source'])
+Dirs = namedtuple('Dirs', ['scripts', 'js_src', 'source', 'tooltool'])
 
 
 def directories(pathmodule, cwd, fixup=lambda s: s):
     scripts = pathmodule.join(fixup(cwd), fixup(pathmodule.dirname(__file__)))
     js_src = pathmodule.abspath(pathmodule.join(scripts, "..", ".."))
     source = pathmodule.abspath(pathmodule.join(js_src, "..", ".."))
-    return Dirs(scripts, js_src, source)
+    tooltool = pathmodule.abspath(env.get('TOOLTOOL_CHECKOUT',
+                                          pathmodule.join(source, "..")))
+    return Dirs(scripts, js_src, source, tooltool)
 
 
 
@@ -98,7 +100,6 @@ def set_vars_from_script(script, vars):
         for var in vars:
             if var in originals and len(originals[var]) > 0:
                 env[var] = "%s;%s" % (env[var], originals[var])
-                print("orig appended, %s = %s" % (var, env[var]))
 
 
 def call_alternates(binaries, command_args, *args, **kwargs):
@@ -138,12 +139,18 @@ if call_alternates(autoconfs, [], cwd=DIR.js_src) != 0:
     sys.exit(1)
 
 OBJDIR = os.path.join(DIR.source, args.objdir)
+OUTDIR = os.path.join(OBJDIR, "out")
 POBJDIR = posixpath.join(PDIR.source, args.objdir)
 AUTOMATION = env.get('AUTOMATION', False)
 MAKE = env.get('MAKE', 'make')
 MAKEFLAGS = '-j6'
 CONFIGURE_ARGS = variant['configure-args']
 UNAME_M = subprocess.check_output(['uname', '-m']).strip()
+
+
+
+env.setdefault('MOZ_UPLOAD_DIR', OBJDIR)
+ensure_dir_exists(env['MOZ_UPLOAD_DIR'], clobber=False)
 
 
 word_bits = variant.get('bits')
@@ -161,17 +168,30 @@ if word_bits is None and args.platform:
 if word_bits is None:
     word_bits = 64 if UNAME_M == 'x86_64' else 32
 
+if 'compiler' in variant:
+    compiler = variant['compiler']
+elif platform.system() == 'Darwin':
+    compiler = 'clang'
+elif platform.system() == 'Windows':
+    compiler = 'cl'
+else:
+    compiler = 'gcc'
+
+cxx = {'clang': 'clang++', 'gcc': 'g++', 'cl': 'cl'}.get(compiler)
+
+compiler_dir = env.get('GCCDIR', os.path.join(DIR.tooltool, compiler))
+if os.path.exists(os.path.join(compiler_dir, 'bin', compiler)):
+    env.setdefault('CC', os.path.join(compiler_dir, 'bin', compiler))
+    env.setdefault('CXX', os.path.join(compiler_dir, 'bin', cxx))
+    platlib = 'lib64' if word_bits == 64 else 'lib'
+    env.setdefault('LD_LIBRARY_PATH', os.path.join(compiler_dir, platlib))
+else:
+    env.setdefault('CC', compiler)
+    env.setdefault('CXX', cxx)
+
 if platform.system() == 'Darwin':
     set_vars_from_script(os.path.join(DIR.scripts, 'macbuildenv.sh'),
                          ['CC', 'CXX'])
-elif platform.system() == 'Linux':
-    if AUTOMATION:
-        GCCDIR = env.get('GCCDIR', os.path.join(DIR.source, '..', 'gcc'))
-        CONFIGURE_ARGS += ' --with-ccache'
-        env.setdefault('CC', os.path.join(GCCDIR, 'bin', 'gcc'))
-        env.setdefault('CXX', os.path.join(GCCDIR, 'bin', 'g++'))
-        platlib = 'lib64' if word_bits == 64 else 'lib'
-        env.setdefault('LD_LIBRARY_PATH', os.path.join(GCCDIR, platlib))
 elif platform.system() == 'Windows':
     MAKE = env.get('MAKE', 'mozmake')
     os.environ['SOURCE'] = DIR.source
@@ -180,25 +200,26 @@ elif platform.system() == 'Windows':
     set_vars_from_script(posixpath.join(PDIR.scripts, 'winbuildenv.sh'),
                          ['PATH', 'INCLUDE', 'LIB', 'LIBPATH', 'CC', 'CXX'])
 
-if word_bits == 64:
-    if platform.system() == 'Windows':
-        CONFIGURE_ARGS += ' --target=x86_64-pc-mingw32 --host=x86_64-pc-mingw32'
-else:
-    if platform.system() == 'Darwin':
+
+if word_bits == 32:
+    if compiler == 'clang':
         env['CC'] = '{CC} -arch i386'.format(**env)
         env['CXX'] = '{CXX} -arch i386'.format(**env)
-    elif platform.system() == 'Windows':
-        CONFIGURE_ARGS += ' --target=i686-pc-mingw32 --host=i686-pc-mingw32'
-    else:
-        env.setdefault('CC', 'gcc')
-        env.setdefault('CXX', 'g++')
+    elif compiler == 'gcc':
         env['CC'] = '{CC} -m32'.format(**env)
         env['CXX'] = '{CXX} -m32'.format(**env)
         env['AR'] = 'ar'
 
-    if platform.system() == 'Linux':
-        if AUTOMATION and UNAME_M != 'arm':
+
+if word_bits == 32:
+    if platform.system() == 'Windows':
+        CONFIGURE_ARGS += ' --target=i686-pc-mingw32 --host=i686-pc-mingw32'
+    elif platform.system() == 'Linux':
+        if UNAME_M != 'arm':
             CONFIGURE_ARGS += ' --target=i686-pc-linux --host=i686-pc-linux'
+else:
+    if platform.system() == 'Windows':
+        CONFIGURE_ARGS += ' --target=x86_64-pc-mingw32 --host=x86_64-pc-mingw32'
 
 
 ACTIVE_PROCESSES = set()
@@ -214,6 +235,7 @@ timer.daemon = True
 timer.start()
 
 ensure_dir_exists(OBJDIR, clobber=not args.dep)
+ensure_dir_exists(OUTDIR)
 
 
 def run_command(command, check=False, **kwargs):
@@ -228,6 +250,16 @@ def run_command(command, check=False, **kwargs):
     if check and status != 0:
         raise subprocess.CalledProcessError(status, command, output=stderr)
     return stdout, stderr, status
+
+
+
+for k, v in variant.get('env', {}).items():
+    env[k] = v.format(
+        DIR=DIR.scripts,
+        TOOLTOOL_CHECKOUT=DIR.tooltool,
+        MOZ_UPLOAD_DIR=env['MOZ_UPLOAD_DIR'],
+        OUTDIR=OUTDIR,
+    )
 
 CONFIGURE_ARGS += ' --enable-nspr-build'
 CONFIGURE_ARGS += ' --prefix={OBJDIR}/dist'.format(OBJDIR=POBJDIR)
@@ -246,11 +278,6 @@ def run_test_command(command, **kwargs):
     return status
 
 test_suites = set(['jstests', 'jittest', 'jsapitests', 'checks'])
-
-
-
-for k, v in variant.get('env', {}).items():
-    env[k] = v.format(DIR=DIR.scripts)
 
 
 if args.platform:
@@ -293,6 +320,36 @@ if 'jsapitests' in test_suites:
     results.append(run_test_command([jsapi_test_binary]))
 if 'jstests' in test_suites:
     results.append(run_test_command([MAKE, 'check-jstests']))
+
+if args.variant in ('tsan', 'msan'):
+    files = filter(lambda f: f.startswith("sanitize_log."), os.listdir(OUTDIR))
+    fullfiles = [os.path.join(OUTDIR, f) for f in files]
+
+    
+    sites = Counter()
+    for filename in fullfiles:
+        with open(os.path.join(OUTDIR, filename), 'rb') as fh:
+            for line in fh:
+                m = re.match(r'^SUMMARY: \w+Sanitizer: (?:data race|use-of-uninitialized-value) (.*)',
+                             line.strip())
+                if m:
+                    sites[m.group(1)] += 1
+
+    
+    summary_filename = os.path.join(env['MOZ_UPLOAD_DIR'], "%s_summary.txt" % args.variant)
+    with open(summary_filename, 'wb') as outfh:
+        for location, count in sites.most_common():
+            print >> outfh, "%d %s" % (count, location)
+    print(open(summary_filename, 'rb').read())
+
+    
+    
+    
+    
+    command = ['tar', '-C', OUTDIR, '-zcf',
+               os.path.join(env['MOZ_UPLOAD_DIR'], '%s.tar.gz' % args.variant)]
+    command += files
+    subprocess.call(command)
 
 for st in results:
     if st != 0:
