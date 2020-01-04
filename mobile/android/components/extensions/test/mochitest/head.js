bc99d5f6536a49b5d5e45b925cd3700c453bc3ca
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 16:21:24 +0000

    Bug 1267124 - Implement chrome.pageAction.show on Android. r=kmag, r=margaret
    MozReview-Commit-ID: AOwfuuCfhRx
    
    UltraBlame original commit: 7fc6b24beea495728bc5070a5b621b31e403b82d

---
+++ mobile/android/components/extensions/test/mochitest/head.js
@@ -0,0 +1,11 @@
+"use strict";
+
+
+
+const {classes: Cc, interfaces: Ci, utils: Cu} = Components;
+
+Cu.import("resource://gre/modules/PageActions.jsm");
+
+function isPageActionShown(extensionId) {
+  return PageActions.isShown(extensionId);
+}
