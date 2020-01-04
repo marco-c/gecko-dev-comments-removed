24cbfc3f70057f5861c13cf2bfd40196dd8a3b93
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 05:22:58 +0000

    Bug 1224254 - Don't try to allocate unreasonably large textures. r=Bas
    UltraBlame original commit: 29f37fd4c13e4309b97e15a5879931819c8220e9

--- gfx/thebes/gfxPrefs.h
+++ gfx/thebes/gfxPrefs.h
@@ -247,6 +247,9 @@ private:
   DECL_GFX_PREF(Live, "gfx.layerscope.port",                   LayerScopePort, int32_t, 23456);
   
   DECL_GFX_PREF(Once, "gfx.logging.crash.length",              GfxLoggingCrashLength, uint32_t, 6);
+  
+  DECL_GFX_PREF(Once, "gfx.max-alloc-size",                    MaxAllocSize, int32_t, (int32_t)500000000);
+  DECL_GFX_PREF(Once, "gfx.max-texture-size",                  MaxTextureSize, int32_t, (int32_t)32767);
   DECL_GFX_PREF(Live, "gfx.perf-warnings.enabled",             PerfWarnings, bool, false);
   DECL_GFX_PREF(Live, "gfx.SurfaceTexture.detach.enabled",     SurfaceTextureDetachEnabled, bool, true);
   DECL_GFX_PREF(Live, "gfx.testing.device-reset",              DeviceResetForTesting, int32_t, 0);
