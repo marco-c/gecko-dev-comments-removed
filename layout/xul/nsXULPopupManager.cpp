a519fa8428321effbfe97ea8263b7b888ac3a559
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 08:12:49 +0000

    Bug 950936 - Introduce ui.popup.disable_autohide pref to ease debugging popups. r=neil
    UltraBlame original commit: 7c8fa9fad7b53245ec3703b96a46e3626d3d3cdb

--- layout/xul/nsXULPopupManager.cpp
+++ layout/xul/nsXULPopupManager.cpp
@@ -109,6 +109,11 @@ void nsMenuChainItem::Detach(nsMenuChainItem** aRoot)
   }
 }
 
+bool nsXULPopupManager::sDevtoolsDisableAutoHide = false;
+
+const char* kPrefDevtoolsDisableAutoHide =
+  "ui.popup.disable_autohide";
+
 NS_IMPL_ISUPPORTS(nsXULPopupManager,
                   nsIDOMEventListener,
                   nsITimerCallback,
@@ -127,6 +132,8 @@ nsXULPopupManager::nsXULPopupManager() :
   if (obs) {
     obs->AddObserver(this, "xpcom-shutdown", false);
   }
+  Preferences::AddBoolVarCache(&sDevtoolsDisableAutoHide,
+                               kPrefDevtoolsDisableAutoHide, false);
 }
 
 nsXULPopupManager::~nsXULPopupManager() 
@@ -183,6 +190,15 @@ bool
 nsXULPopupManager::Rollup(uint32_t aCount, bool aFlush,
                           const nsIntPoint* pos, nsIContent** aLastRolledUp)
 {
+  
+  if (nsXULPopupManager::sDevtoolsDisableAutoHide) {
+    
+    if (mWidget) {
+      mWidget->CaptureRollupEvents(nullptr, false);
+    }
+    return false;
+  }
+
   bool consume = false;
 
   nsMenuChainItem* item = GetTopVisibleMenu();
