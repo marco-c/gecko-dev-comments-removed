19b1666caa8d62a185e9b3fcab326f4c0a612d41
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 15:04:38 +0000

    Bug 1264482 - Use the EnumString type for the compiler type. r=ted
    UltraBlame original commit: 177380345f2fc53853aa53e0aceca74a022a8e2f

---
+++ python/mozbuild/mozbuild/configure/constants.py
@@ -0,0 +1,15 @@
+
+
+
+
+from __future__ import absolute_import, print_function, unicode_literals
+
+from mozbuild.util import EnumString
+
+
+CompilerType = EnumString.subclass(
+    'clang',
+    'clang-cl',
+    'gcc',
+    'msvc',
+)
