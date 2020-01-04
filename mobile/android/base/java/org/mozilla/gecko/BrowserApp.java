028d087cd526119097e7fd88d71d1182bb785d17
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 07:08:51 +0000

    Merge fx-team to m-c. a=merge
    UltraBlame original commit: 4a559a618d6798eb9a8fdc559f5a7a00085e2062

--- mobile/android/base/java/org/mozilla/gecko/BrowserApp.java
+++ mobile/android/base/java/org/mozilla/gecko/BrowserApp.java
@@ -2630,6 +2630,7 @@ public class BrowserApp extends GeckoApp
         mBrowserSearchContainer.setVisibility(View.VISIBLE);
 
         
+        hideWebContent();
         mHomePagerContainer.setVisibility(View.INVISIBLE);
 
         final FragmentManager fm = getSupportFragmentManager();
