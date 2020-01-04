382f40ece58fe58faa6dd583b2f9f3592f5e7048
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 23:41:14 +0000

    Bug 1300822 - Fix tab load event wait in devtools test helpers. r=tromey
    MozReview-Commit-ID: 8vSSaDloJND
    
    UltraBlame original commit: 272f009cfe0761ee022ce1083b76bef18915e766

--- devtools/client/framework/test/browser_toolbox_custom_host.js
+++ devtools/client/framework/test/browser_toolbox_custom_host.js
@@ -3,28 +3,25 @@
 
 
 
+const TEST_URL = "data:text/html,test custom host";
+
 function test() {
   let {Toolbox} = require("devtools/client/framework/toolbox");
 
-  let toolbox, iframe, target, tab;
-
-  gBrowser.selectedTab = gBrowser.addTab();
-  target = TargetFactory.forTab(gBrowser.selectedTab);
+  let toolbox, iframe, target;
 
   window.addEventListener("message", onMessage);
 
   iframe = document.createElement("iframe");
   document.documentElement.appendChild(iframe);
 
-  gBrowser.selectedBrowser.addEventListener("load", function onLoad(evt) {
-    gBrowser.selectedBrowser.removeEventListener(evt.type, onLoad, true);
+  addTab(TEST_URL).then(function (tab) {
+    target = TargetFactory.forTab(tab);
     let options = {customIframe: iframe};
     gDevTools.showToolbox(target, null, Toolbox.HostType.CUSTOM, options)
              .then(testCustomHost, console.error)
              .then(null, console.error);
-  }, true);
-
-  content.location = "data:text/html,test custom host";
+  });
 
   function onMessage(event) {
     info("onMessage: " + event.data);
@@ -50,7 +47,7 @@ function test() {
     
     
     toolbox.destroy().then(() => {
-      toolbox = iframe = target = tab = null;
+      toolbox = iframe = target = null;
       finish();
     });
   }
