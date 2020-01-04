0daa2944cb25f4546f37648335f0e50485c5d17b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 09:23:51 +0000

    Bug 1234020: Part 2b - [webext] Return promises from the cookies APIs. r=evilpie
    UltraBlame original commit: 56a0bd3479b00a95c485a382f0c711d9c0a45308

--- toolkit/components/extensions/ext-cookies.js
+++ toolkit/components/extensions/ext-cookies.js
@@ -7,7 +7,6 @@ Cu.import("resource://gre/modules/NetUtil.jsm");
 
 var {
   EventManager,
-  runSafe,
 } = ExtensionUtils;
 
 
@@ -242,25 +241,24 @@ function* query(detailsIn, props, extension) {
 extensions.registerSchemaAPI("cookies", "cookies", (extension, context) => {
   let self = {
     cookies: {
-      get: function(details, callback) {
+      get: function(details) {
         
         for (let cookie of query(details, ["url", "name", "storeId"], extension)) {
-          runSafe(context, callback, convert(cookie));
-          return;
+          return Promise.resolve(convert(cookie));
         }
 
         
-        runSafe(context, callback, null);
+        return Promise.resolve(null);
       },
 
-      getAll: function(details, callback) {
+      getAll: function(details) {
         let allowed = ["url", "name", "domain", "path", "secure", "session", "storeId"];
         let result = Array.from(query(details, allowed, extension), convert);
 
-        runSafe(context, callback, result);
+        return Promise.resolve(result);
       },
 
-      set: function(details, callback) {
+      set: function(details) {
         let uri = NetUtil.newURI(details.url).QueryInterface(Ci.nsIURL);
 
         let path;
@@ -283,42 +281,35 @@ extensions.registerSchemaAPI("cookies", "cookies", (extension, context) => {
         
 
         let cookieAttrs = { host: details.domain, path: path, isSecure: secure };
-        if (checkSetCookiePermissions(extension, uri, cookieAttrs)) {
-          
-          
-          
-          
-          Services.cookies.add(cookieAttrs.host, path, name, value,
-                               secure, httpOnly, isSession, expiry);
+        if (!checkSetCookiePermissions(extension, uri, cookieAttrs)) {
+          return Promise.reject({message: `Permission denied to set cookie ${JSON.stringify(details)}`});
         }
 
-        if (callback) {
-          self.cookies.get(details, callback);
-        }
+        
+        
+        Services.cookies.add(cookieAttrs.host, path, name, value,
+                             secure, httpOnly, isSession, expiry);
+
+        return self.cookies.get(details);
       },
 
-      remove: function(details, callback) {
+      remove: function(details) {
         for (let cookie of query(details, ["url", "name", "storeId"], extension)) {
           Services.cookies.remove(cookie.host, cookie.name, cookie.path, false);
-          if (callback) {
-            runSafe(context, callback, {
-              url: details.url,
-              name: details.name,
-              storeId: DEFAULT_STORE,
-            });
-          }
           
-          return;
+          return Promise.resolve({
+            url: details.url,
+            name: details.name,
+            storeId: DEFAULT_STORE,
+          });
         }
 
-        if (callback) {
-          runSafe(context, callback, null);
-        }
+        return Promise.resolve(null);
       },
 
-      getAllCookieStores: function(callback) {
+      getAllCookieStores: function() {
         
-        runSafe(context, callback, [{id: DEFAULT_STORE, tabIds: []}]);
+        return Promise.resolve([{id: DEFAULT_STORE, tabIds: []}]);
       },
 
       onChanged: new EventManager(context, "cookies.onChanged", fire => {
