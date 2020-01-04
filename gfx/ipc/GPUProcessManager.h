4090765287e62013385586b3eeeec4f9b16677b1
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 22:26:44 +0000

    Backed out changeset 21cd933e4c32 (bug 1289650) for crashing in e10s browser-chrome tests. r=backout on a CLOSED TREE
    UltraBlame original commit: d4ae1e0da3e995f760eee6ae00dbc3e88968d408

--- gfx/ipc/GPUProcessManager.h
+++ gfx/ipc/GPUProcessManager.h
@@ -95,13 +95,6 @@ public:
 
   
   
-  void MapLayerTreeId(uint64_t aLayersId, base::ProcessId aOwningId);
-
-  
-  bool IsLayerTreeIdMapped(uint64_t aLayersId, base::ProcessId aRequestingId);
-
-  
-  
   
   
   uint64_t AllocateLayerTreeId();
