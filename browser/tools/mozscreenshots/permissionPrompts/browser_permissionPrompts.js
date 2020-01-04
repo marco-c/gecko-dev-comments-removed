8c11a7df971429f63ec48e6352e21bb179ac05d4
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 17:22:22 +0000

    Bug 1268619 - Add a mozscreenshots configuration for site permission notifications. r=MattN
    MozReview-Commit-ID: 8mryMwYOxxn
    
    UltraBlame original commit: 32a5b0f3bf99fd10d0849a4d17927d3179cd9ed6

---
+++ browser/tools/mozscreenshots/permissionPrompts/browser_permissionPrompts.js
@@ -0,0 +1,14 @@
+
+
+
+
+"use strict";
+
+add_task(function* capture() {
+  if (!shouldCapture()) {
+    return;
+  }
+  let sets = ["LightweightThemes", "PermissionPrompts"];
+
+  yield TestRunner.start(sets);
+});
