1570c9067d099a810acd19a6970e6bb4997e8732
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 08:51:10 +0000

    Bug 1079844 - Change various non-js/ files/tests/etc. to refer to detaching of ArrayBuffers rather than neutering.  (DOM references to "neutering" of DOM things remain as neutering.)  r=bz
    UltraBlame original commit: b9fb3bb07c79b86bf59b165efea3286422df1734

--- toolkit/components/promiseworker/tests/xpcshell/test_Promise.js
+++ toolkit/components/promiseworker/tests/xpcshell/test_Promise.js
@@ -68,12 +68,12 @@ add_task(function* test_transfer_args() {
   for (let i = 0; i < 4; ++i) {
     array[i] = i;
   }
-  Assert.equal(array.buffer.byteLength, 4, "The buffer is not neutered yet");
+  Assert.equal(array.buffer.byteLength, 4, "The buffer is not detached yet");
 
   let result = (yield worker.post("bounce", [array.buffer], [], [array.buffer]))[0];
 
   
-  Assert.equal(array.buffer.byteLength, 0, "The buffer has been neutered");
+  Assert.equal(array.buffer.byteLength, 0, "The buffer has been detached");
 
   
   Assert.equal(result.byteLength, 4, "The result has the right size");
@@ -89,13 +89,13 @@ add_task(function* test_transfer_with_meta() {
   for (let i = 0; i < 4; ++i) {
     array[i] = i;
   }
-  Assert.equal(array.buffer.byteLength, 4, "The buffer is not neutered yet");
+  Assert.equal(array.buffer.byteLength, 4, "The buffer is not detached yet");
 
   let message = new BasePromiseWorker.Meta(array, {transfers: [array.buffer]});
   let result = (yield worker.post("bounce", [message]))[0];
 
   
-  Assert.equal(array.buffer.byteLength, 0, "The buffer has been neutered");
+  Assert.equal(array.buffer.byteLength, 0, "The buffer has been detached");
 
   
   Assert.equal(result.toString(), "[object Uint8Array]", "The result appears to be a Typed Array");
