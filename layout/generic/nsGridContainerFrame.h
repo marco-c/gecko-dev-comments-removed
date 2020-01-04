afa2938fda6848b1204c755ba97ba62e721ce1e1
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 16:47:47 +0000

    Bug 1271392 part 6 - [css-grid] Remove now dead code.  r=dholbert
    UltraBlame original commit: 5e11a2a81e37ac77f646857ba8b1e1c154b96167

--- layout/generic/nsGridContainerFrame.h
+++ layout/generic/nsGridContainerFrame.h
@@ -231,15 +231,15 @@ private:
                                     nscoord              aAvailableSize);
 
   
-  void ReflowInFlowChild(nsIFrame*              aChild,
-                         const GridItemInfo*    aGridItemInfo,
-                         nsSize                 aContainerSize,
-                         mozilla::Maybe<nscoord>aStretchBSize,
-                         const Fragmentainer*   aFragmentainer,
-                         const GridReflowState& aState,
-                         const LogicalRect&     aContentArea,
-                         nsHTMLReflowMetrics&   aDesiredSize,
-                         nsReflowStatus&        aStatus);
+  void ReflowInFlowChild(nsIFrame*               aChild,
+                         const GridItemInfo*     aGridItemInfo,
+                         nsSize                  aContainerSize,
+                         mozilla::Maybe<nscoord> aStretchBSize,
+                         const Fragmentainer*    aFragmentainer,
+                         const GridReflowState&  aState,
+                         const LogicalRect&      aContentArea,
+                         nsHTMLReflowMetrics&    aDesiredSize,
+                         nsReflowStatus&         aStatus);
 
   
 
