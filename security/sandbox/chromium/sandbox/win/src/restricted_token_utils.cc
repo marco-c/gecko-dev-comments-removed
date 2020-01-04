f49f83ff7a3f63b5fad575105559ae56951174c7
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 23:13:24 +0000

    Bug 1287426 Part 8: Change the USER_NON_ADMIN token to be a restricted token with the same access. r=aklotz
    This is to work around an issue where the call to CoInitializeSecurity in MainThreadRuntime::InitializeSecurity causes the impersonation token, used to give the pre-lockdown permissions, to be replaced with one with no rights.
    This only seems to happen when the lockdown token is USER_NON_ADMIN, which is not a restricted token.
    
    MozReview-Commit-ID: 6HFuDFmWLTf
    
    UltraBlame original commit: 32f4d07e9d99b5685bba2b75366a3c01b61468a1

--- security/sandbox/chromium/sandbox/win/src/restricted_token_utils.cc
+++ security/sandbox/chromium/sandbox/win/src/restricted_token_utils.cc
@@ -53,6 +53,19 @@ DWORD CreateRestrictedToken(TokenLevel security_level,
       sid_exceptions.push_back(WinInteractiveSid);
       sid_exceptions.push_back(WinAuthenticatedUserSid);
       privilege_exceptions.push_back(SE_CHANGE_NOTIFY_NAME);
+      
+      
+      
+      
+      
+      
+      restricted_token.AddRestrictingSid(WinBuiltinUsersSid);
+      restricted_token.AddRestrictingSid(WinWorldSid);
+      restricted_token.AddRestrictingSid(WinInteractiveSid);
+      restricted_token.AddRestrictingSid(WinAuthenticatedUserSid);
+      restricted_token.AddRestrictingSid(WinRestrictedCodeSid);
+      restricted_token.AddRestrictingSidCurrentUser();
+      restricted_token.AddRestrictingSidLogonSession();
       break;
     }
     case USER_INTERACTIVE: {
