8443448a5874d75cd6c0ecf83b1676b7232ff5d0
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 20:45:28 +0000

    Bug 1287520 - Check IsPackedArray for the result array in Array.prototype.concat. r=efaust
    UltraBlame original commit: b40a60e2a4e138d4b99805b5471bcff268817f9a

--- js/src/builtin/Array.js
+++ js/src/builtin/Array.js
@@ -990,7 +990,7 @@ function ArrayConcat(arg1) {
             if (n + len > MAX_NUMERIC_INDEX)
                 ThrowTypeError(JSMSG_TOO_LONG_ARRAY);
 
-            if (IsPackedArray(E)) {
+            if (IsPackedArray(A) && IsPackedArray(E)) {
                 
                 for (k = 0; k < len; k++) {
                     
