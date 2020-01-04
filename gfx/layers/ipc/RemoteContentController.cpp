1f7c9e7ea70a46201031550949079e41192c81c6
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 14:23:34 +0000

    Bug 1255823 - Add a two-step destruction process to PAPZ. r=dvander
    MozReview-Commit-ID: 5HB1DVO4qeK
    
    UltraBlame original commit: 65e9d46daf3ebdc89f12ecfb4bcc88c0257307d3

--- gfx/layers/ipc/RemoteContentController.cpp
+++ gfx/layers/ipc/RemoteContentController.cpp
@@ -25,6 +25,8 @@
 namespace mozilla {
 namespace layers {
 
+static std::map<uint64_t, RefPtr<RemoteContentController>> sDestroyedControllers;
+
 RemoteContentController::RemoteContentController(uint64_t aLayersId,
                                                  dom::TabParent* aBrowserParent)
   : mUILoop(MessageLoop::current())
@@ -37,9 +39,6 @@ RemoteContentController::RemoteContentController(uint64_t aLayersId,
 
 RemoteContentController::~RemoteContentController()
 {
-  if (mBrowserParent) {
-    Unused << PAPZParent::Send__delete__(this);
-  }
 }
 
 void
@@ -317,17 +316,18 @@ RemoteContentController::ActorDestroy(ActorDestroyReason aWhy)
     mApzcTreeManager = nullptr;
   }
   mBrowserParent = nullptr;
-}
 
-
-
-
-
-
-static void
-DeletePAPZParent(PAPZParent* aPAPZ)
-{
-  Unused << PAPZParent::Send__delete__(aPAPZ);
+  
+  
+  
+  
+  
+  
+  
+  uint64_t key = mLayersId;
+  NS_DispatchToMainThread(NS_NewRunnableFunction([key] {
+    sDestroyedControllers.erase(key);
+  }));
 }
 
 void
@@ -336,7 +336,15 @@ RemoteContentController::Destroy()
   RefPtr<RemoteContentController> controller = this;
   NS_DispatchToMainThread(NS_NewRunnableFunction([controller] {
     if (controller->CanSend()) {
-      DeletePAPZParent(controller);
+      if (controller->SendDestroy()) {
+        
+        
+        
+        
+        uint64_t key = controller->mLayersId;
+        MOZ_ASSERT(sDestroyedControllers.find(key) == sDestroyedControllers.end());
+        sDestroyedControllers[key] = controller;
+      }
     }
   }));
 }
