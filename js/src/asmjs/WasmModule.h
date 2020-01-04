af7b8c1aa4cd8376f06a1da2b988303dd15b99d8
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 17:51:36 +0000

    Bug 1277246 - Add support for names section. r=luke
    MozReview-Commit-ID: LbGMxxDdmXx
    
    UltraBlame original commit: d02ab40ae319910659cf19114f41e7aa7ce68e4d

--- js/src/asmjs/WasmModule.h
+++ js/src/asmjs/WasmModule.h
@@ -640,7 +640,7 @@ class Module : public mozilla::LinkedListElement<Module>
     
     
 
-    const char* prettyFuncName(uint32_t funcIndex) const;
+    const char* maybePrettyFuncName(uint32_t funcIndex) const;
     const char* getFuncName(JSContext* cx, uint32_t funcIndex, UniqueChars* owner) const;
     JSAtom* getFuncAtom(JSContext* cx, uint32_t funcIndex) const;
 
