39240315b812fdb9a867cc9118ebde8bfbd19aa2
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 11:27:13 +0000

    Bug 1120715 - Part 6: Remove the dom.requestcache.enabled pref; r=bkelly
    UltraBlame original commit: 5cce63231d48e35bd68ece3419996c530237e5d7

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -5221,9 +5221,6 @@ pref("memory.report_concurrency", 10);
 pref("media.useAudioChannelAPI", false);
 
 
-pref("dom.requestcache.enabled", false);
-
-
 pref("dom.requestcontext.enabled", false);
 
 pref("dom.mozKillSwitch.enabled", false);
