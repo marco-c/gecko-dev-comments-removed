5dc9c7ed712d5774922164e2d02915a707823170
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 17:58:47 +0000

    Backed out 2 changesets (bug 1271653) for gc assertions CLOSED TREE
    Backed out changeset 7a197b8bd00a (bug 1271653)
    Backed out changeset 9c29d3067608 (bug 1271653)
    
    UltraBlame original commit: 80bf1af3cd2865208a17c3e24f38721a25e11a7f

--- js/src/jit-test/tests/debug/Object-script-lazy.js
+++ js/src/jit-test/tests/debug/Object-script-lazy.js
@@ -13,7 +13,6 @@
 var g1 = newGlobal();
 g1.eval('function f() { return "from f"; }');
 g1.eval('function g() { return "from g"; }');
-g1.eval('this.h = f.bind(g, 42, "foo");');
 
 
 var g2 = newGlobal();
@@ -21,7 +20,6 @@ var dbg = new Debugger;
 var g2w = dbg.addDebuggee(g2);
 g2.f = g1.f;
 g2.g = g1.g;
-g2.h = g1.h;
 
 
 
@@ -39,21 +37,7 @@ assertEq(gDO.unwrap().global === g2w, false);
 assertEq(gDO.unwrap().parameterNames, undefined);
 
 
-var hDO = g2w.getOwnPropertyDescriptor('h').value;
-assertEq(hDO.global, g2w);
-assertEq(hDO.unwrap().global === g2w, false);
-assertEq(hDO.unwrap().isBoundFunction, undefined);
-assertEq(hDO.unwrap().boundTargetFunction, undefined);
-assertEq(hDO.unwrap().boundThis, undefined);
-assertEq(hDO.unwrap().boundArguments, undefined);
-
-
 dbg.addDebuggee(g1);
 assertEq(fDO.unwrap().script instanceof Debugger.Script, true);
 assertEq(gDO.unwrap().parameterNames instanceof Array, true);
-assertEq(hDO.unwrap().isBoundFunction, true);
-assertEq(hDO.unwrap().boundTargetFunction, fDO.unwrap());
-assertEq(hDO.unwrap().boundThis, gDO.unwrap());
-assertEq(hDO.unwrap().boundArguments.length, 2);
-assertEq(hDO.unwrap().boundArguments[0], 42);
-assertEq(hDO.unwrap().boundArguments[1], "foo");
+
