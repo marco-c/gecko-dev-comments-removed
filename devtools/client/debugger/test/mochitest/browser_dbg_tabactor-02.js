e62e0ba3493ff27810463aed61720438628d8f78
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 19:33:04 +0000

    Bug 1285289 - add Devtools test addons as Temporary Addons. r=ochameau
    MozReview-Commit-ID: 1plXeerEAJp
    
    UltraBlame original commit: 1a3c9b20d193fb01dc8f3b536ff8e3586f9421b9

--- devtools/client/debugger/test/mochitest/browser_dbg_tabactor-02.js
+++ devtools/client/debugger/test/mochitest/browser_dbg_tabactor-02.js
@@ -7,7 +7,6 @@
 
 
 
-const CHROME_URL = "chrome://mochitests/content/browser/devtools/client/debugger/test/mochitest/";
 const ACTORS_URL = CHROME_URL + "testactors.js";
 const TAB_URL = EXAMPLE_URL + "doc_empty-tab-01.html";
 
