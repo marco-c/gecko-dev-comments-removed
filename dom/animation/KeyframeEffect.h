d24a1750d979f3ad137dc9ce50bc48d8be50deb2
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 11:47:59 +0000

    Bug 1254419 - Throw if we fail to allocate memory for a values array in getProperties(); r=bz
    MozReview-Commit-ID: BmDC51qWXcu
    
    UltraBlame original commit: deabc24fe6ed9aa10ab6af4edb704365bae9d8a1

--- dom/animation/KeyframeEffect.h
+++ dom/animation/KeyframeEffect.h
@@ -209,7 +209,8 @@ public:
   void GetFrames(JSContext*& aCx,
                  nsTArray<JSObject*>& aResult,
                  ErrorResult& aRv);
-  void GetProperties(nsTArray<AnimationPropertyDetails>& aProperties) const;
+  void GetProperties(nsTArray<AnimationPropertyDetails>& aProperties,
+                     ErrorResult& aRv) const;
 
   
   
