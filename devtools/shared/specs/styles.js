b3f2f62f63603b8936bde5dbb8e8ec7f9f85fc63
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 17:42:49 +0000

    Bug 1272724 - Move nodeSpec into its own file;r=ejpbruel
    UltraBlame original commit: fe57228e70aa503323bf177093e2cecb438a39cc

--- devtools/shared/specs/styles.js
+++ devtools/shared/specs/styles.js
@@ -10,10 +10,8 @@ const {
   generateActorSpec,
   types
 } = require("devtools/shared/protocol");
+require("devtools/shared/specs/node");
 require("devtools/shared/specs/stylesheets");
-
-
-types.addActorType("domnode");
 
 
 types.addActorType("domstylerule");
