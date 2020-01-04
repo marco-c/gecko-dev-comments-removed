81d6a2cb4f72d0ae381c4cacad2b98f2bd3442b1
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 11:04:20 +0000

    Bug 1253137 - Baldr: update encoding of memory access immediates (r=sunfish)
    MozReview-Commit-ID: L4rMuB0VGxd
    
    UltraBlame original commit: 21d121d42c75e8898733f4acd53aa25ccbb10e75

--- js/src/asmjs/AsmJS.cpp
+++ js/src/asmjs/AsmJS.cpp
@@ -51,6 +51,7 @@ using namespace js::frontend;
 using namespace js::jit;
 using namespace js::wasm;
 
+using mozilla::CeilingLog2;
 using mozilla::Compression::LZ4;
 using mozilla::HashGeneric;
 using mozilla::IsNaN;
@@ -3699,12 +3700,12 @@ static bool
 CheckAndPrepareArrayAccess(FunctionValidator& f, ParseNode* viewName, ParseNode* indexExpr,
                            bool isSimd, Scalar::Type* viewType)
 {
-    
-    if (!f.encoder().writeVarU32(0))
+    size_t flagsAt;
+    if (!f.encoder().writePatchableU8(&flagsAt))
         return false;
 
-    size_t alignAt;
-    if (!f.encoder().writePatchableVarU8(&alignAt))
+    
+    if (!f.encoder().writeVarU32(0))
         return false;
 
     size_t prepareAt;
@@ -3716,7 +3717,9 @@ CheckAndPrepareArrayAccess(FunctionValidator& f, ParseNode* viewName, ParseNode*
         return false;
 
     
-    f.encoder().patchVarU8(alignAt, TypedArrayElemSize(*viewType));
+    size_t align = TypedArrayElemSize(*viewType);
+    MOZ_ASSERT(IsPowerOfTwo(align));
+    f.encoder().patchVarU8(flagsAt, CeilingLog2(align));
 
     
     
