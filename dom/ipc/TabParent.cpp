52af7c7a9762fa91e945f97d8a65bb82ff9c42c0
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 17:06:01 +0000

    Remove TabParent use of CompositorBridgeParent. (bug 1274149 part 4, r=mattwoodrow)
    UltraBlame original commit: bca90b6efa44c2d4a33c3c47f4bd6741c91f084c

--- dom/ipc/TabParent.cpp
+++ dom/ipc/TabParent.cpp
@@ -25,12 +25,12 @@
 #include "mozilla/EventStateManager.h"
 #include "mozilla/gfx/2D.h"
 #include "mozilla/gfx/DataSurfaceHelpers.h"
+#include "mozilla/gfx/GPUProcessManager.h"
 #include "mozilla/Hal.h"
 #include "mozilla/IMEStateManager.h"
 #include "mozilla/ipc/DocumentRendererParent.h"
 #include "mozilla/jsipc/CrossProcessObjectWrappers.h"
 #include "mozilla/layers/AsyncDragMetrics.h"
-#include "mozilla/layers/CompositorBridgeParent.h"
 #include "mozilla/layers/InputAPZContext.h"
 #include "mozilla/layout/RenderFrameParent.h"
 #include "mozilla/LookAndFeel.h"
@@ -105,6 +105,7 @@ using namespace mozilla::layout;
 using namespace mozilla::services;
 using namespace mozilla::widget;
 using namespace mozilla::jsipc;
+using namespace mozilla::gfx;
 
 
 
@@ -2977,7 +2978,7 @@ TabParent::RequestNotifyLayerTreeReady()
   if (!frame || !frame->IsInitted()) {
     mNeedLayerTreeReadyNotification = true;
   } else {
-    CompositorBridgeParent::RequestNotifyLayerTreeReady(
+    GPUProcessManager::Get()->RequestNotifyLayerTreeReady(
       frame->GetLayersId(),
       mLayerUpdateObserver);
   }
@@ -2992,7 +2993,7 @@ TabParent::RequestNotifyLayerTreeCleared()
     return false;
   }
 
-  CompositorBridgeParent::RequestNotifyLayerTreeCleared(
+  GPUProcessManager::Get()->RequestNotifyLayerTreeCleared(
     frame->GetLayersId(),
     mLayerUpdateObserver);
   return true;
@@ -3036,7 +3037,7 @@ TabParent::SwapLayerTreeObservers(TabParent* aOther)
   
   
   
-  CompositorBridgeParent::SwapLayerTreeObservers(
+  GPUProcessManager::Get()->SwapLayerTreeObservers(
     rfp->GetLayersId(),
     otherRfp->GetLayersId());
 
