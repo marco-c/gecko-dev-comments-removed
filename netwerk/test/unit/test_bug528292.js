95970c2fa7cdb436ee97d096c250b5f19fdbfaef
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 07:03:52 +0000

    Backed out changeset 7459e0c97939 (bug 1234596) for test_protocolproxyservice.js failures in xpcshell CLOSED TREE
    UltraBlame original commit: 9aec7a931f193d9fdc4169566674bc10dce71481

--- netwerk/test/unit/test_bug528292.js
+++ netwerk/test/unit/test_bug528292.js
@@ -1,5 +1,5 @@
 Cu.import("resource://testing-common/httpd.js");
-Cu.import("resource://gre/modules/NetUtil.jsm");
+Cu.import("resource://gre/modules/Services.jsm");
 
 const sentCookieVal     = "foo=bar";
 const responseBody      = "response body";
@@ -64,11 +64,16 @@ function run_test()
   
   
   
-  var chan = NetUtil.newChannel({
-    uri: preRedirectURL,
-    loadUsingSystemPrincipal: true
-  }).QueryInterface(Ci.nsIHttpChannel)
-    .QueryInterface(Ci.nsIHttpChannelInternal);
+  var chan = ioService.newChannel2(preRedirectURL,
+                                   "",
+                                   null,
+                                   null,      
+                                   Services.scriptSecurityManager.getSystemPrincipal(),
+                                   null,      
+                                   Ci.nsILoadInfo.SEC_NORMAL,
+                                   Ci.nsIContentPolicy.TYPE_OTHER).
+             QueryInterface(Ci.nsIHttpChannel).
+             QueryInterface(Ci.nsIHttpChannelInternal);
   chan.forceAllowThirdPartyCookie = true;
 
   
@@ -79,7 +84,7 @@ function run_test()
     setCookieString(postRedirectURI, null, sentCookieVal, chan);
 
   
-  chan.asyncOpen2(new ChannelListener(finish_test, null));
+  chan.asyncOpen(new ChannelListener(finish_test, null), null);
   do_test_pending();
 }
 
