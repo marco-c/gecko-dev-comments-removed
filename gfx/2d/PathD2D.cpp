0af092709f74e268cb087418e2af854f90f28a42
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 05:29:22 +0000

    Bug 1131264 - Extend the workaround for drawing D2D circles. r=bas
    UltraBlame original commit: 3df173f3761d4dc2bd77ba43ffa503545456b80c

--- gfx/2d/PathD2D.cpp
+++ gfx/2d/PathD2D.cpp
@@ -230,30 +230,15 @@ PathBuilderD2D::Arc(const Point &aOrigin, Float aRadius, Float aStartAngle,
     aEndAngle = oldStart;
   }
 
-  const Float kSmallRadius = 0.007f;
-  Float midAngle = 0;
-  bool smallFullCircle = false;
-
   
   
+  bool fullCircle = false;
   if (aEndAngle - aStartAngle >= 2 * M_PI) {
-    if (aRadius > kSmallRadius) {
-      aEndAngle = Float(aStartAngle + M_PI * 1.9999);
-    }
-    else {
-      smallFullCircle = true;
-      midAngle = Float(aStartAngle + M_PI);
-      aEndAngle = Float(aStartAngle + 2 * M_PI);
-    }
+    fullCircle = true;
+    aEndAngle = Float(aStartAngle + M_PI * 1.9999);
   } else if (aStartAngle - aEndAngle >= 2 * M_PI) {
-    if (aRadius > kSmallRadius) {
-      aStartAngle = Float(aEndAngle + M_PI * 1.9999);
-    }
-    else {
-      smallFullCircle = true;
-      midAngle = Float(aEndAngle + M_PI);
-      aStartAngle = Float(aEndAngle + 2 * M_PI);
-    }
+    fullCircle = true;
+    aStartAngle = Float(aEndAngle + M_PI * 1.9999);
   }
 
   Point startPoint;
@@ -275,7 +260,12 @@ PathBuilderD2D::Arc(const Point &aOrigin, Float aRadius, Float aStartAngle,
     aAntiClockwise ? D2D1_SWEEP_DIRECTION_COUNTER_CLOCKWISE :
                      D2D1_SWEEP_DIRECTION_CLOCKWISE;
 
-  if (!smallFullCircle) {
+  
+  
+  const Float kEpsilon = 1e-5f;
+  if (!fullCircle ||
+      (std::abs(startPoint.x - endPoint.x) +
+       std::abs(startPoint.y - endPoint.y) > kEpsilon)) {
 
     if (aAntiClockwise) {
       if (aStartAngle - aEndAngle > M_PI) {
@@ -295,6 +285,9 @@ PathBuilderD2D::Arc(const Point &aOrigin, Float aRadius, Float aStartAngle,
   }
   else {
     
+    
+    Float midAngle = aEndAngle > aStartAngle ?
+      Float(aStartAngle + M_PI) : Float(aEndAngle + M_PI);
     Point midPoint;
     midPoint.x = aOrigin.x + aRadius * cosf(midAngle);
     midPoint.y = aOrigin.y + aRadius * sinf(midAngle);
@@ -305,7 +298,9 @@ PathBuilderD2D::Arc(const Point &aOrigin, Float aRadius, Float aStartAngle,
                                    direction,
                                    arcSize));
 
-    mSink->AddArc(D2D1::ArcSegment(D2DPoint(endPoint),
+    
+    
+    mSink->AddArc(D2D1::ArcSegment(D2DPoint(startPoint),
                                    D2D1::SizeF(aRadius, aRadius),
                                    0.0f,
                                    direction,
