3abd3301f6fed4bedeec42d6c889640d3794d6f4
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 00:51:18 +0000

    Bug 895274 part.99 Rename NS_MUTATION_SUBTREEMODIFIED to eLegacySubtreeModified r=smaug
    UltraBlame original commit: f6f918d677aabe854314d81d65d02d07216dddae

--- dom/events/EventListenerManager.cpp
+++ dom/events/EventListenerManager.cpp
@@ -73,7 +73,7 @@ static uint32_t
 MutationBitForEventType(EventMessage aEventType)
 {
   switch (aEventType) {
-    case NS_MUTATION_SUBTREEMODIFIED:
+    case eLegacySubtreeModified:
       return NS_EVENT_BITS_MUTATION_SUBTREEMODIFIED;
     case NS_MUTATION_NODEINSERTED:
       return NS_EVENT_BITS_MUTATION_NODEINSERTED;
@@ -314,7 +314,7 @@ EventListenerManager::AddEventListenerInternal(
       
       
       window->SetMutationListeners(
-        (aEventMessage == NS_MUTATION_SUBTREEMODIFIED) ?
+        (aEventMessage == eLegacySubtreeModified) ?
           kAllMutationBits : MutationBitForEventType(aEventMessage));
     }
   } else if (aTypeAtom == nsGkAtoms::ondeviceorientation) {
@@ -1246,7 +1246,7 @@ EventListenerManager::MutationListenerBits()
       Listener* listener = &mListeners.ElementAt(i);
       if (listener->mEventMessage >= eLegacyMutationEventFirst &&
           listener->mEventMessage <= NS_MUTATION_END) {
-        if (listener->mEventMessage == NS_MUTATION_SUBTREEMODIFIED) {
+        if (listener->mEventMessage == eLegacySubtreeModified) {
           return kAllMutationBits;
         }
         bits |= MutationBitForEventType(listener->mEventMessage);
