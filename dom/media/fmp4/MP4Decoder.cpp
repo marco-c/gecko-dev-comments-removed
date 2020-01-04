a20461ca6ec2f8725982a6e93ea1e706ab04c5a8
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 18:57:08 +0000

    Bug 1267887 - Support Opus in mp4 with the rust demuxer. r=kinetik
    Update C++ caller code for for mp4parse 0.4.0. Now feeds data through
    a read callback in mp4parse_io.
    
    Hook up the GetTrackInfo method to the rust demuxer results.
    
    Prefer rust demuxer only if there's an Opus track.
    
    Fill in audio and video track metadata. Pass audio codec_specific_config
    to the decoder.
    
    With this change sample.mp4 plays.
    
    MozReview-Commit-ID: F8xwWPZZBfZ
    
    UltraBlame original commit: 72ea61f4f82016bfbc9f9372456a24d3f2feceff

--- dom/media/fmp4/MP4Decoder.cpp
+++ dom/media/fmp4/MP4Decoder.cpp
@@ -93,7 +93,8 @@ MP4Decoder::CanHandleMediaType(const nsACString& aMIMETypeExcludingCodecs,
   
   
   const bool isMP4Audio = aMIMETypeExcludingCodecs.EqualsASCII("audio/mp4") ||
-                          aMIMETypeExcludingCodecs.EqualsASCII("audio/x-m4a");
+                          aMIMETypeExcludingCodecs.EqualsASCII("audio/x-m4a") ||
+                          aMIMETypeExcludingCodecs.EqualsASCII("audio/opus");
   const bool isMP4Video =
   
 #ifdef MOZ_GONK_MEDIACODEC
--- dom/media/platforms/agnostic/AgnosticDecoderModule.cpp
+++ dom/media/platforms/agnostic/AgnosticDecoderModule.cpp
@@ -5,6 +5,7 @@
 
 
 #include "AgnosticDecoderModule.h"
+#include "mozilla/Logging.h"
 #include "OpusDecoder.h"
 #include "VorbisDecoder.h"
 #include "VPXDecoder.h"
@@ -16,10 +17,14 @@ bool
 AgnosticDecoderModule::SupportsMimeType(const nsACString& aMimeType,
                                         DecoderDoctorDiagnostics* aDiagnostics) const
 {
-  return VPXDecoder::IsVPX(aMimeType) ||
+  bool supports =
+    VPXDecoder::IsVPX(aMimeType) ||
     OpusDataDecoder::IsOpus(aMimeType) ||
     VorbisDataDecoder::IsVorbis(aMimeType) ||
     WaveDataDecoder::IsWave(aMimeType);
+  MOZ_LOG(sPDMLog, LogLevel::Debug, ("Agnostic decoder %s requested type",
+        supports ? "supports" : "rejects"));
+  return supports;
 }
 
 already_AddRefed<MediaDataDecoder>
