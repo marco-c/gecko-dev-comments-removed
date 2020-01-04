cfde7e8e5d02ddf415ea37ddd231b2b1387be488
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 22:45:01 +0000

    Bug 1298189 - implement logging in FakeLogging.h so we get logs when running tests. r=drno
    MozReview-Commit-ID: HKY0VW4FYz2
    
    UltraBlame original commit: 2ad5a1276f73eb2402130d848875864ec9fa1780

--- media/webrtc/signaling/test/FakeLogging.h
+++ media/webrtc/signaling/test/FakeLogging.h
@@ -9,7 +9,17 @@ namespace mozilla {
 namespace detail {
 void log_print(const PRLogModuleInfo* aModule,
                       LogLevel aLevel,
-                      const char* aFmt, ...) { }
+                      const char* aFmt, ...)
+  {
+    
+    va_list ap;
+    va_start(ap, aFmt);
+    char* buff = PR_vsmprintf(aFmt, ap);
+    PR_LogPrint("%s", buff);
+    PR_smprintf_free(buff);
+    va_end(ap);
+  }
+
 }
 }
 
