1cc17e50c8aaebbcff94b514b6974792685df2b0
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 05:58:46 +0000

    Bug 1228359: Allow experiments to register chrome. r=felipe
    Simple obvious fix. Adds tests by making BootstrapMonitor (which
    test_experiments.js and others use for verifying bootstrap startup and shutdown)
    verify the list of registered chrome manifests at various points. Without the
    fix this makes test_experiment fail as expected.
    
    UltraBlame original commit: a5ad0e73b4bf19c63eb157f4fd1bbaa98eb051d8

--- toolkit/mozapps/extensions/test/xpcshell/head_addons.js
+++ toolkit/mozapps/extensions/test/xpcshell/head_addons.js
@@ -73,10 +73,35 @@ var resHandler = Services.io.getProtocolHandler("resource")
 var dataURI = NetUtil.newURI(do_get_file("data", true));
 resHandler.setSubstitution("xpcshell-data", dataURI);
 
+function isManifestRegistered(file) {
+  let manifests = Components.manager.getManifestLocations();
+  for (let i = 0; i < manifests.length; i++) {
+    let manifest = manifests.queryElementAt(i, AM_Ci.nsIURI);
+
+    
+    
+    if (manifest instanceof AM_Ci.nsIJARURI) {
+      manifest = manifest.JARFile.QueryInterface(AM_Ci.nsIFileURL).file;
+    }
+    else if (manifest instanceof AM_Ci.nsIFileURL) {
+      manifest = manifest.file.parent;
+    }
+    else {
+      continue;
+    }
+
+    if (manifest.equals(file))
+      return true;
+  }
+  return false;
+}
+
 
 
 
 this.BootstrapMonitor = {
+  inited: false,
+
   
   installed: new Map(),
   started: new Map(),
@@ -89,7 +114,15 @@ this.BootstrapMonitor = {
   installPromises: [],
 
   init() {
+    this.inited = true;
     Services.obs.addObserver(this, "bootstrapmonitor-event", false);
+  },
+
+  shutdownCheck() {
+    if (!this.inited)
+      return;
+
+    do_check_eq(this.started.size, 0);
   },
 
   clear(id) {
@@ -123,6 +156,11 @@ this.BootstrapMonitor = {
     do_check_neq(started, undefined);
     if (version != undefined)
       do_check_eq(started.data.version, version);
+
+    
+    let installPath = new FileUtils.File(started.data.installPath);
+    let isRegistered = isManifestRegistered(installPath);
+    do_check_true(isRegistered);
   },
 
   checkAddonNotStarted(id) {
@@ -143,6 +181,7 @@ this.BootstrapMonitor = {
   observe(subject, topic, data) {
     let info = JSON.parse(data);
     let id = info.data.id;
+    let installPath = new FileUtils.File(info.data.installPath);
 
     
     if (info.event == "install") {
@@ -164,17 +203,35 @@ this.BootstrapMonitor = {
 
       this.started.delete(id);
       this.stopped.set(id, info);
+
+      
+      let isRegistered = isManifestRegistered(installPath);
+      do_check_true(isRegistered);
+
+      
+      
+      
+      if (info.reason == 2 )
+        Components.manager.removeBootstrappedManifestLocation(installPath);
     }
     else {
       this.checkAddonNotStarted(id);
     }
 
     if (info.event == "uninstall") {
+      
+      let isRegistered = isManifestRegistered(installPath);
+      do_check_false(isRegistered);
+
       this.installed.delete(id);
       this.uninstalled.set(id, info)
     }
     else if (info.event == "startup") {
       this.started.set(id, info);
+
+      
+      let isRegistered = isManifestRegistered(installPath);
+      do_check_true(isRegistered);
 
       for (let resolve of this.startupPromises)
         resolve();
@@ -587,6 +644,7 @@ function promiseShutdownManager() {
   return MockAsyncShutdown.hook()
     .then(null, err => hookErr = err)
     .then( () => {
+      BootstrapMonitor.shutdownCheck();
       gInternalManager = null;
 
       
