10cbc0677963a707e1ee97243671c2b03405e375
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 17:15:46 +0000

    Bug 1275608 - [mozrunner] Remove wait_for_system_message from Emulator; r=ahal
    This code is no longer used by Marionette client or elsewhere.
    
    MozReview-Commit-ID: 4lx9CN7XIeH
    
    UltraBlame original commit: b284943099b5c9982078193ce21d263e051515ce

--- testing/mozbase/mozrunner/mozrunner/devices/emulator.py
+++ testing/mozbase/mozrunner/mozrunner/devices/emulator.py
@@ -17,7 +17,6 @@ from .base import Device
 from .emulator_battery import EmulatorBattery
 from .emulator_geo import EmulatorGeo
 from .emulator_screen import EmulatorScreen
-from ..utils import uses_marionette
 from ..errors import TimeoutException
 
 class ArchContext(object):
@@ -174,37 +173,6 @@ class Emulator(Device):
         
         self.userdata.close()
         shutil.rmtree(self.tmpdir)
-
-    
-    @uses_marionette
-    def wait_for_system_message(self, marionette):
-        marionette.set_script_timeout(45000)
-        
-        
-        
-        
-        print 'waiting for system-message-listener-ready...'
-        try:
-            marionette.execute_async_script("""
-waitFor(
-    function() { marionetteScriptFinished(true); },
-    function() { return isSystemMessageListenerReady(); }
-);
-            """)
-        except:
-            
-            
-            exc_name = sys.exc_info()[0].__name__
-            if exc_name != 'ScriptTimeoutException':
-                raise
-
-            print 'timed out'
-            
-            
-            
-            
-            pass
-        print '...done'
 
     def _get_telnet_response(self, command=None):
         output = []
