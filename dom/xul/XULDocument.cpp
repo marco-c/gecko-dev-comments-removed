b2c1c32d88fb7b76df5fa4952c9c24d1d021e5f3
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 00:56:13 +0000

    Bug 1137009 - Do not persist attributes of xul window if the window is in fullscreen. r=janv
    UltraBlame original commit: 873acda091b75ba8aee7f374cd8e6c8627d64494

--- dom/xul/XULDocument.cpp
+++ dom/xul/XULDocument.cpp
@@ -924,6 +924,23 @@ XULDocument::AttributeWillChange(nsIDocument* aDocument,
     }
 }
 
+static bool
+ShouldPersistAttribute(nsIDocument* aDocument, Element* aElement)
+{
+    if (aElement->IsXULElement(nsGkAtoms::window)) {
+        if (nsCOMPtr<nsPIDOMWindow> window = aDocument->GetWindow()) {
+            bool isFullscreen;
+            window->GetFullScreen(&isFullscreen);
+            if (isFullscreen) {
+                
+                
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
 void
 XULDocument::AttributeChanged(nsIDocument* aDocument,
                               Element* aElement, int32_t aNameSpaceID,
@@ -999,18 +1016,19 @@ XULDocument::AttributeChanged(nsIDocument* aDocument,
     bool listener, resolved;
     CheckBroadcasterHookup(aElement, &listener, &resolved);
 
-    
-    
-    
-    nsAutoString persist;
-    aElement->GetAttr(kNameSpaceID_None, nsGkAtoms::persist, persist);
-    if (!persist.IsEmpty()) {
+    if (ShouldPersistAttribute(aDocument, aElement)) {
         
-        if (persist.Find(nsDependentAtomString(aAttribute)) >= 0) {
+        
+        
+        nsString persist;
+        aElement->GetAttr(kNameSpaceID_None, nsGkAtoms::persist, persist);
+        if (!persist.IsEmpty() &&
+            
+            persist.Find(nsDependentAtomString(aAttribute)) >= 0) {
             nsContentUtils::AddScriptRunner(NS_NewRunnableMethodWithArgs
-              <nsIContent*, int32_t, nsIAtom*>
-              (this, &XULDocument::DoPersist, aElement, kNameSpaceID_None,
-               aAttribute));
+                <nsIContent*, int32_t, nsIAtom*>
+                (this, &XULDocument::DoPersist, aElement,
+                 kNameSpaceID_None, aAttribute));
         }
     }
 }
