dc4b9a038414c9b40f93e86af60297b6bbec254f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 00:20:48 +0000

    Bug 1180120 - Part 2: Move style-if-visited as well in nsStyleContext::Move (and assert that its parent is the same as the non-visited-style parent). r=dbaron
    UltraBlame original commit: 4dac8ab1e1a72abf147b1c2114f1bc15e358aee3

--- layout/style/nsStyleContext.cpp
+++ layout/style/nsStyleContext.cpp
@@ -312,7 +312,9 @@ nsStyleContext::MoveTo(nsStyleContext* aNewParent)
   
   
   MOZ_ASSERT(!IsStyleIfVisited());
+  MOZ_ASSERT(!mParent->IsStyleIfVisited());
   MOZ_ASSERT(!aNewParent->IsStyleIfVisited());
+  MOZ_ASSERT(!mStyleIfVisited || mStyleIfVisited->mParent == mParent);
 
   nsStyleContext* oldParent = mParent;
 
@@ -321,13 +323,19 @@ nsStyleContext::MoveTo(nsStyleContext* aNewParent)
   }
 
   aNewParent->AddRef();
-
   mParent->RemoveChild(this);
-
   mParent = aNewParent;
   mParent->AddChild(this);
-
   oldParent->Release();
+
+  if (mStyleIfVisited) {
+    oldParent = mStyleIfVisited->mParent;
+    aNewParent->AddRef();
+    mStyleIfVisited->mParent->RemoveChild(mStyleIfVisited);
+    mStyleIfVisited->mParent = aNewParent;
+    mStyleIfVisited->mParent->AddChild(mStyleIfVisited);
+    oldParent->Release();
+  }
 }
 
 already_AddRefed<nsStyleContext>
