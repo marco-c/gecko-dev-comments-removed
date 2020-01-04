f81a881aeddd88ad513b7b415be86bdd63e5e71f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 01:29:57 +0000

    Bug 1022369 - Correctly execute |Function("var x\n/regex/g")| without throwing a SyntaxError. r=Waldo
    UltraBlame original commit: 3a6d0761268866d9c50fa18f698c96b63e3c4590

--- js/src/frontend/Parser.cpp
+++ js/src/frontend/Parser.cpp
@@ -4215,9 +4215,27 @@ Parser<ParseHandler>::variables(YieldHandling yieldHandling,
             handler.addList(pn, pn2);
 
             bool matched;
-            if (!tokenStream.matchToken(&matched, TOK_ASSIGN))
+            
+            
+            
+            
+            
+            
+            
+            
+            if (!tokenStream.matchToken(&matched, TOK_ASSIGN, TokenStream::Operand))
                 return null();
-            if (matched) {
+            if (!matched) {
+                tokenStream.addModifierException(TokenStream::NoneIsOperand);
+
+                if (data.isConst() && location == NotInForInit) {
+                    report(ParseError, false, null(), JSMSG_BAD_CONST_DECL);
+                    return null();
+                }
+
+                if (!data.bind(name, this))
+                    return null();
+            } else {
                 if (psimple)
                     *psimple = false;
 
@@ -4266,14 +4284,6 @@ Parser<ParseHandler>::variables(YieldHandling yieldHandling,
                     if (!handler.finishInitializerAssignment(pn2, init, data.op()))
                         return null();
                 }
-            } else {
-                if (data.isConst() && location == NotInForInit) {
-                    report(ParseError, false, null(), JSMSG_BAD_CONST_DECL);
-                    return null();
-                }
-
-                if (!data.bind(name, this))
-                    return null();
             }
 
             handler.setEndPosition(pn, pn2);
