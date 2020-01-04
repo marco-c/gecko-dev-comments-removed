f15902612966d2fae9d26e430c1e446c35c245c2
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 18:14:08 +0000

    Backed out changeset 45bfebd36a99 (bug 1274221)
    UltraBlame original commit: 29257a0f6e042060f35a1ec73078480954a7ae23

--- dom/media/MediaStreamGraph.h
+++ dom/media/MediaStreamGraph.h
@@ -1439,8 +1439,6 @@ public:
   {
     return mInputs.Length();
   }
-  virtual MediaStream* GetInputStreamFor(TrackID aTrackID) { return nullptr; }
-  virtual TrackID GetInputTrackIDFor(TrackID aTrackID) { return TRACK_NONE; }
   void DestroyImpl() override;
   
 
--- dom/media/TrackUnionStream.h
+++ dom/media/TrackUnionStream.h
@@ -23,9 +23,6 @@ public:
 
   void SetTrackEnabledImpl(TrackID aTrackID, bool aEnabled) override;
 
-  MediaStream* GetInputStreamFor(TrackID aTrackID) override;
-  TrackID GetInputTrackIDFor(TrackID aTrackID) override;
-
 protected:
   
   struct TrackMapEntry {
