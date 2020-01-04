21687ca3f924744731eb59bfde968ed897f56d0a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 04:57:53 +0000

    Bug 1220308 Cache API should teardown DOM objects when Worker hits Terminating status. r=baku
    UltraBlame original commit: 10e861d62dcc3c4d333a4a323728ba097b3b4b23

--- dom/cache/Feature.cpp
+++ dom/cache/Feature.cpp
@@ -13,7 +13,7 @@ namespace mozilla {
 namespace dom {
 namespace cache {
 
-using mozilla::dom::workers::Canceling;
+using mozilla::dom::workers::Terminating;
 using mozilla::dom::workers::Status;
 using mozilla::dom::workers::WorkerPrivate;
 
@@ -73,7 +73,9 @@ Feature::Notify(JSContext* aCx, Status aStatus)
 {
   NS_ASSERT_OWNINGTHREAD(Feature);
 
-  if (aStatus < Canceling || mNotified) {
+  
+  
+  if (aStatus < Terminating || mNotified) {
     return true;
   }
 
