0435a2872abb1dfc090a656308218b589da5227e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 16:35:22 +0000

    Bug 1272778. Use IntRects for clip rects instead of Rects. r=mstange
    Previously, we took an IntRect converted to Rect and then later wanted
    ints in the backend. This avoids all of that.
    
    UltraBlame original commit: 4428e846de67dec2fa2217cd749651eb1dedb05d

--- gfx/layers/ipc/CompositorBench.h
+++ gfx/layers/ipc/CompositorBench.h
@@ -18,9 +18,9 @@ class Compositor;
 
 
 #ifdef MOZ_COMPOSITOR_BENCH
-void CompositorBench(Compositor* aCompositor, const gfx::Rect& aScreenRect);
+void CompositorBench(Compositor* aCompositor, const gfx::IntRect& aScreenRect);
 #else
-static inline void CompositorBench(Compositor* aCompositor, const gfx::Rect& aScreenRect) {}
+static inline void CompositorBench(Compositor* aCompositor, const gfx::IntRect& aScreenRect) {}
 #endif
 
 } 
