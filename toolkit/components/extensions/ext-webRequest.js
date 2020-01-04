6ca892e7f25150c634336b70c33be192f40cccd6
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 02:01:51 +0000

    Bug 1202478 - [webext] Run WebExtension callbacks asynchronously (r=gabor)
    UltraBlame original commit: 7e8c04ff6049464b74b3c2823747da516d323e3b

--- toolkit/components/extensions/ext-webRequest.js
+++ toolkit/components/extensions/ext-webRequest.js
@@ -10,7 +10,7 @@ XPCOMUtils.defineLazyModuleGetter(this, "WebRequest",
 Cu.import("resource://gre/modules/ExtensionUtils.jsm");
 var {
   SingletonEventManager,
-  runSafe,
+  runSafeSync,
 } = ExtensionUtils;
 
 
@@ -53,7 +53,7 @@ function WebRequestEventManager(context, eventName)
         }
       }
 
-      return runSafe(context, callback, data2);
+      return runSafeSync(context, callback, data2);
     };
 
     let filter2 = {};
