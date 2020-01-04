911dd7d4b0612eaab6262244858a4a5ae7f73cb8
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 11:36:02 +0000

    Bug 1230641: P1. Use UYVY (YUV422) format in decoders. r=mattwoodrow
    MozReview-Commit-ID: I6KZLi2bHk0
    
    UltraBlame original commit: f8c2b72484284f5b723a802ef1c2ba1b7a122be5

--- dom/media/platforms/apple/AppleVDADecoder.cpp
+++ dom/media/platforms/apple/AppleVDADecoder.cpp
@@ -595,14 +595,14 @@ AppleVDADecoder::CreateDecoderSpecification()
 CFDictionaryRef
 AppleVDADecoder::CreateOutputConfiguration()
 {
-  
-  SInt32 PixelFormatTypeValue = kCVPixelFormatType_420YpCbCr8BiPlanarVideoRange;
-  AutoCFRelease<CFNumberRef> PixelFormatTypeNumber =
-    CFNumberCreate(kCFAllocatorDefault,
-                   kCFNumberSInt32Type,
-                   &PixelFormatTypeValue);
-
   if (mUseSoftwareImages) {
+    
+    SInt32 PixelFormatTypeValue =
+      kCVPixelFormatType_420YpCbCr8BiPlanarVideoRange;
+    AutoCFRelease<CFNumberRef> PixelFormatTypeNumber =
+      CFNumberCreate(kCFAllocatorDefault,
+                     kCFNumberSInt32Type,
+                     &PixelFormatTypeValue);
     const void* outputKeys[] = { kCVPixelBufferPixelFormatTypeKey };
     const void* outputValues[] = { PixelFormatTypeNumber };
     static_assert(ArrayLength(outputKeys) == ArrayLength(outputValues),
@@ -617,6 +617,12 @@ AppleVDADecoder::CreateOutputConfiguration()
   }
 
 #ifndef MOZ_WIDGET_UIKIT
+  
+  SInt32 PixelFormatTypeValue = kCVPixelFormatType_422YpCbCr8;
+  AutoCFRelease<CFNumberRef> PixelFormatTypeNumber =
+    CFNumberCreate(kCFAllocatorDefault,
+                   kCFNumberSInt32Type,
+                   &PixelFormatTypeValue);
   
   const void* IOSurfaceKeys[] = { MacIOSurfaceLib::kPropIsGlobal };
   const void* IOSurfaceValues[] = { kCFBooleanTrue };
