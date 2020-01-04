0c8971fc7ea5fb5af725f8df2c5053227ebc4b32
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 10:53:54 +0000

    Bug 1236949: Split up browser_ext_tab_move_window.js. r=me
    MozReview-Commit-ID: 5WEdXucSFKa
    
    UltraBlame original commit: 6f37ab432cf08ea6d729161d205d686ffacbff0b

---
+++ browser/components/extensions/test/browser/browser_ext_tabs_move_window_pinned.js
@@ -0,0 +1,42 @@
+
+
+"use strict";
+
+add_task(function* () {
+  yield BrowserTestUtils.openNewForegroundTab(gBrowser, "http://example.net/");
+  let window1 = yield BrowserTestUtils.openNewBrowserWindow();
+  let tab1 = yield BrowserTestUtils.openNewForegroundTab(window1.gBrowser, "http://example.com/");
+  window1.gBrowser.pinTab(tab1);
+
+  let extension = ExtensionTestUtils.loadExtension({
+    manifest: {
+      "permissions": ["tabs"],
+    },
+
+    background: function() {
+      browser.tabs.query(
+        {url: "<all_urls>"},
+        tabs => {
+          let destination = tabs[0];
+          let source = tabs[1]; 
+          browser.tabs.move(source.id, {windowId: destination.windowId, index: 0});
+
+          browser.tabs.query(
+            {url: "<all_urls>"},
+            tabs => {
+              browser.test.assertEq(true, tabs[0].pinned);
+              browser.test.notifyPass("tabs.move.pin");
+            });
+        });
+    },
+  });
+
+  yield extension.startup();
+  yield extension.awaitFinish("tabs.move.pin");
+  yield extension.unload();
+
+  for (let tab of window.gBrowser.tabs) {
+    yield BrowserTestUtils.removeTab(tab);
+  }
+  yield BrowserTestUtils.closeWindow(window1);
+});
