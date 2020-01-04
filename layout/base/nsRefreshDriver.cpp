7f8cb7d1a2b53258e2f64d51d75ddf6ad6ef4c5a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 04:31:10 +0000

    Bug 1220699 - Add telemetry probe to measure vsync latency in the parent refresh driver. r=avih
    UltraBlame original commit: d2b705bd76e1ac36b725746924b4ac11d1fe5ccc

--- layout/base/nsRefreshDriver.cpp
+++ layout/base/nsRefreshDriver.cpp
@@ -361,6 +361,11 @@ private:
 
       if (XRE_IsParentProcess()) {
         MonitorAutoLock lock(mRefreshTickLock);
+        #ifndef ANDROID  
+          TimeDuration vsyncLatency = TimeStamp::Now() - aVsyncTimestamp;
+          Telemetry::Accumulate(Telemetry::FX_REFRESH_DRIVER_CHROME_FRAME_DELAY_MS,
+                                vsyncLatency.ToMilliseconds());
+        #endif
         aVsyncTimestamp = mRecentVsync;
         mProcessedVsync = true;
       }
