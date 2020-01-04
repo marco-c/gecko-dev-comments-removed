4c263fe6d4491284cd18a17248600a5bbe76f5eb
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 05:59:59 +0000

    Bug 1200595 - D3D11 TextureData implementation. r=Bas
    UltraBlame original commit: 981a6bc9b1aea682f912099fb57257640e8f9359

--- gfx/layers/LayersLogging.cpp
+++ gfx/layers/LayersLogging.cpp
@@ -13,6 +13,7 @@
 #include "nsDebug.h"                    
 #include "nsPoint.h"                    
 #include "nsRect.h"                     
+#include "base/basictypes.h"
 
 using namespace mozilla::gfx;
 
