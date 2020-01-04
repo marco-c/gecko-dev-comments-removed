27a1ed6ecffb564fdcc8efd8806b021460cc89b0
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 16:12:17 +0000

    Bug 1270968 - Add mechanism to clear GMP storage when its version changes. r=gerald
    In bug 1264497 we discovered that Netflix was broken because we'd made a change
    to not send the Adobe GMP the device bound nodeId, which it stored in GMP
    storage. When the GMP was comparing the nodeId it had stored against what it was
    passed (nothing) the comparison was failing. Users could have worked around this
    problem by clearing their GMP storage, whereupon the Adobe GMP will have stored
    "nothing" as its nodeId.
    
    When bug 1264497 was fixed, users who'd cleared their storage will see Netflix
    fail again, as their stored nodeId of "nothing" will again not match what we
    pass in. So to fix Netflix for these users, we need to clear GMP storage.
    
    This is another instance of a more general problem that we have occasionally
    encountered, namely that sometimes GMP storage becomes incompatible, and we
    need to clear it. Having a general mechanism that we can use to clear storage
    remotely will be helpful, so this patch adds one, and triggers it to fire.
    
    This mechanism is pref controlled, so that we can issue a hotfix if necessary
    to clear GMP storage.
    
    
    MozReview-Commit-ID: GzSyBj0P2JG
    
    UltraBlame original commit: d50ac88f8feef390e875b3f92f6a69544107d6c1

--- dom/media/gmp/GMPServiceParent.cpp
+++ dom/media/gmp/GMPServiceParent.cpp
@@ -146,7 +146,20 @@ GeckoMediaPluginServiceParent::Init()
 
   
   nsCOMPtr<nsIThread> thread;
-  return GetThread(getter_AddRefs(thread));
+  rv = GetThread(getter_AddRefs(thread));
+  if (NS_FAILED(rv)) {
+    return rv;
+  }
+
+  
+  int32_t version = Preferences::GetInt("media.gmp.storage.version.observed", 0);
+  int32_t expected = Preferences::GetInt("media.gmp.storage.version.expected", 0);
+  if (version != expected) {
+    Preferences::SetInt("media.gmp.storage.version.observed", expected);
+    return GMPDispatch(NewRunnableMethod(
+      this, &GeckoMediaPluginServiceParent::ClearStorage));
+  }
+  return NS_OK;
 }
 
 already_AddRefed<nsIFile>
