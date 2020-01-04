4c4974c4a7c9e21e5aa7213475b9da930bac1756
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 17:53:05 +0000

    Bug 1276368: use b=TIAS for maxBitrate encoding constraints. r=jesup
    MozReview-Commit-ID: C6kGt9pfhko
    
    UltraBlame original commit: 403728f309ce77168d47d67e69e41aad284aabbd

--- media/webrtc/signaling/src/media-conduit/VideoConduit.cpp
+++ media/webrtc/signaling/src/media-conduit/VideoConduit.cpp
@@ -1049,13 +1049,13 @@ WebrtcVideoConduit::SelectBitrates(unsigned short width,
   if (framerate >= 10) {
     out_min = out_min * (framerate/30);
     out_start = out_start * (framerate/30);
-    out_max = out_max * (framerate/30);
+    out_max = std::max((unsigned int)(out_max * (framerate/30)), cap);
   } else {
     
     
     out_min = out_min * ((10-(framerate/2))/30);
     out_start = out_start * ((10-(framerate/2))/30);
-    out_max = out_max * ((10-(framerate/2))/30);
+    out_max = std::max((unsigned int)(out_max * ((10-(framerate/2))/30)), cap);
   }
 
   if (mMinBitrate && mMinBitrate > out_min) {
