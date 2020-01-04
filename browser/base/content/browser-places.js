4c8a219eaa92092f49cc0d52640782cc283e641c
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 21:39:43 +0000

    Bug 1171746 - ensure tab specific panels close when you switch the tab r=jaws
    Adds a tabspecific attribute to the edit bookmarks panel and the Pocket subview panel to signal that these popups should close when the user navigates away from the tab. It also specifies that the localized keyboard short cut for closing a  window should close the edit bookmarks panel and the tab by adding a general function to check whether a certain <key> has been pressed. Adds tests for both closing specific tabs and checking keys.
    
    MozReview-Commit-ID: AxW5uQgDQQB
    
    UltraBlame original commit: 33fcf83eefe43e02f560fdf9c182b88c87343cc1

--- browser/base/content/browser-places.js
+++ browser/base/content/browser-places.js
@@ -128,6 +128,13 @@ var StarUI = {
             }
             this.panel.hidePopup();
             break;
+          
+          case 0:
+            let accessKey = document.getElementById("key_close");
+            if (eventMatchesKey(aEvent, accessKey)) {
+                this.panel.hidePopup();
+            }
+            break;
         }
         break;
       case "mouseout":
