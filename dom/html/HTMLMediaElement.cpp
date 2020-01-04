31bd6316db20c57dbe41d7929cf16a2c0e21f6c5
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 22:55:51 +0000

    Merge inbound to m-c. a=merge
    UltraBlame original commit: b7f7ae14590aced450bb0b0469dfb38edd2c0ace

--- dom/html/HTMLMediaElement.cpp
+++ dom/html/HTMLMediaElement.cpp
@@ -6490,11 +6490,19 @@ HTMLMediaElement::OnVisibilityChange(Visibility aOldVisibility,
         break;
     }
     case Visibility::NONVISIBLE: {
+      if (mPlayTime.IsStarted()) {
+        
+        HiddenVideoStart();
+      }
+
       mDecoder->NotifyOwnerActivityChanged(false);
       break;
     }
     case Visibility::MAY_BECOME_VISIBLE: {
       if (aOldVisibility == Visibility::NONVISIBLE) {
+        
+        HiddenVideoStop();
+
         mDecoder->NotifyOwnerActivityChanged(true);
       } else if (aOldVisibility == Visibility::IN_DISPLAYPORT) {
         
@@ -6502,6 +6510,9 @@ HTMLMediaElement::OnVisibilityChange(Visibility aOldVisibility,
       break;
     }
     case Visibility::IN_DISPLAYPORT: {
+      
+      HiddenVideoStop();
+
       mDecoder->NotifyOwnerActivityChanged(true);
       break;
     }
