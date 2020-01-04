c128545ab3b291c47b1301d0c816c48cc0edf594
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 14:29:58 +0000

    Bug 1248861: P3. Use AlignedAudioBuffer object with AudioData. r=cpearce
    MozReview-Commit-ID: 7HiF4eHlRwB
    
    UltraBlame original commit: c17c2fde0e8922acf9ab024f818eb70ae68fcc48

--- dom/media/AudioCompactor.h
+++ dom/media/AudioCompactor.h
@@ -41,7 +41,10 @@ public:
 
     while (aFrames > 0) {
       uint32_t samples = GetChunkSamples(aFrames, aChannels, maxSlop);
-      auto buffer = MakeUnique<AudioDataValue[]>(samples);
+      AlignedAudioBuffer buffer(samples);
+      if (!buffer) {
+        return false;
+      }
 
       
       uint32_t framesCopied = aCopyFunc(buffer.get(), samples);
