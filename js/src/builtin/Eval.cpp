d8fd991505d2a463511883bc6aa188bf62c9d594
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 17:05:49 +0000

    Bug 1274193 part 3 - Use GO_THROUGH_SAVED when we know there's no saved frame boundary. r=luke
    UltraBlame original commit: 722a014fc551b4e2e59ed6e114e04c0291f9f455

--- js/src/builtin/Eval.cpp
+++ js/src/builtin/Eval.cpp
@@ -426,7 +426,7 @@ bool
 js::DirectEval(JSContext* cx, HandleValue v, MutableHandleValue vp)
 {
     
-    ScriptFrameIter iter(cx, FrameIter::STOP_AT_SAVED);
+    ScriptFrameIter iter(cx, FrameIter::GO_THROUGH_SAVED);
     AbstractFramePtr caller = iter.abstractFramePtr();
 
     MOZ_ASSERT(JSOp(*iter.pc()) == JSOP_EVAL ||
