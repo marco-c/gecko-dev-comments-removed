b507a986135f08dff855521e48104eef419d7573
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 00:36:55 +0000

    Bug 1146603: Use messaging to set up the parent and child workers for tab.attach. r=jsantell
    UltraBlame original commit: 7e962aae1e4661c99e600caa3a831df773e97f7d

--- addon-sdk/source/lib/sdk/tabs/tab-firefox.js
+++ addon-sdk/source/lib/sdk/tabs/tab-firefox.js
@@ -191,10 +191,37 @@ const Tab = Class({
     if (isDestroyed(this))
       return;
 
+    let { Worker } = require('../content/worker');
+    let { connect, makeChildOptions } = require('../content/utils');
+
+    let worker = Worker(options);
+    worker.once("detach", () => {
+      worker.destroy();
+    });
+
+    let attach = frame => {
+      let childOptions = makeChildOptions(options);
+      frame.port.emit("sdk/tab/attach", childOptions);
+      connect(worker, frame, { id: childOptions.id, url: this.url });
+    };
+
     
-    
-    let { Worker } = require('./worker');
-    return Worker(options, browser(this).contentWindow);
+    let frame = frames.getFrameForBrowser(browser(this));
+    if (frame) {
+      attach(frame);
+    }
+    else {
+      let listener = (frame) => {
+        if (frame.frameElement != browser(this))
+          return;
+
+        listener.off("attach", listener);
+        attach(frame);
+      };
+      frames.on("attach", listener);
+    }
+
+    return worker;
   },
 
   destroy: function() {
