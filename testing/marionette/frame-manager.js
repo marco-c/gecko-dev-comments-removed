3e639ff7cf8722f12d35e3d60411dd7343096ed3
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 09:22:34 +0000

    Backed out 16 changesets (bug 1245153) for mochitest test-bustage on a CLOSED TREE
    Backed out changeset 30c8ec933737 (bug 1245153)
    Backed out changeset 4c2b1902d7cd (bug 1245153)
    Backed out changeset 1be5f60393a0 (bug 1245153)
    Backed out changeset 22321e6b65e9 (bug 1245153)
    Backed out changeset c1e0abbfa66e (bug 1245153)
    Backed out changeset 80ae953819c8 (bug 1245153)
    Backed out changeset 3edb67388ad6 (bug 1245153)
    Backed out changeset 55f64197f6b5 (bug 1245153)
    Backed out changeset 6bb93562a576 (bug 1245153)
    Backed out changeset 5da7628c3767 (bug 1245153)
    Backed out changeset bd41e4ab829d (bug 1245153)
    Backed out changeset ee7ee24cc65e (bug 1245153)
    Backed out changeset 4b2a5ee7199e (bug 1245153)
    Backed out changeset d75ad1397656 (bug 1245153)
    Backed out changeset 1f5e37f8e446 (bug 1245153)
    Backed out changeset aec0a0166685 (bug 1245153)
    
    UltraBlame original commit: 9f7157c44c017c82e27f3d2ab77bfb4b2bea79da

---
+++ testing/marionette/frame-manager.js
@@ -0,0 +1,230 @@
+
+
+
+
+var {classes: Cc, interfaces: Ci, utils: Cu} = Components;
+
+this.EXPORTED_SYMBOLS = ["FrameManager"];
+
+var FRAME_SCRIPT = "chrome://marionette/content/listener.js";
+
+Cu.import("resource://gre/modules/Services.jsm");
+Cu.import("resource://gre/modules/XPCOMUtils.jsm");
+
+var loader = Cc["@mozilla.org/moz/jssubscript-loader;1"]
+               .getService(Ci.mozIJSSubScriptLoader);
+
+
+var remoteFrames = [];
+
+
+
+
+
+function MarionetteRemoteFrame(windowId, frameId) {
+  this.windowId = windowId; 
+  this.frameId = frameId; 
+  this.targetFrameId = this.frameId; 
+};
+
+
+
+
+
+
+
+
+this.FrameManager = function FrameManager(server) {
+  
+  this.currentRemoteFrame = null; 
+  this.previousRemoteFrame = null; 
+  this.handledModal = false; 
+  this.server = server; 
+};
+
+FrameManager.prototype = {
+  QueryInterface: XPCOMUtils.generateQI([Ci.nsIMessageListener,
+                                         Ci.nsISupportsWeakReference]),
+
+  
+
+
+  receiveMessage: function FM_receiveMessage(message) {
+    switch (message.name) {
+      case "MarionetteFrame:getInterruptedState":
+        
+        if (this.previousRemoteFrame) {
+          let interruptedFrame = Services.wm.getOuterWindowWithId(this.previousRemoteFrame.windowId);
+          if (this.previousRemoteFrame.frameId != null) {
+            interruptedFrame = interruptedFrame.document.getElementsByTagName("iframe")[this.previousRemoteFrame.frameId]; 
+          }
+          
+          if (interruptedFrame.src == message.target.src) {
+            return {value: this.handledModal};
+          }
+        }
+        else if (this.currentRemoteFrame == null) {
+          
+          return {value: this.handledModal};
+        }
+        return {value: false};
+      case "MarionetteFrame:handleModal":
+        
+
+
+        
+        
+        
+        let isLocal = true;
+        if (this.currentRemoteFrame != null) {
+          isLocal = false;
+          this.removeMessageManagerListeners(this.currentRemoteFrame.messageManager.get());
+          
+          this.previousRemoteFrame = this.currentRemoteFrame;
+          
+          this.currentRemoteFrame = null;
+          this.server.messageManager = Cc["@mozilla.org/globalmessagemanager;1"]
+                                       .getService(Ci.nsIMessageBroadcaster);
+        }
+        this.handledModal = true;
+        this.server.sendOk(this.server.command_id);
+        return {value: isLocal};
+      case "MarionetteFrame:getCurrentFrameId":
+        if (this.currentRemoteFrame != null) {
+          return this.currentRemoteFrame.frameId;
+        }
+    }
+  },
+
+  getOopFrame: function FM_getOopFrame(winId, frameId) {
+    
+    let outerWin = Services.wm.getOuterWindowWithId(winId);
+    
+    let f = outerWin.document.getElementsByTagName("iframe")[frameId];
+    return f;
+  },
+
+  getFrameMM: function FM_getFrameMM(winId, frameId) {
+    let oopFrame = this.getOopFrame(winId, frameId);
+    let mm = oopFrame.QueryInterface(Ci.nsIFrameLoaderOwner)
+        .frameLoader.messageManager;
+    return mm;
+  },
+
+  
+
+
+
+  switchToFrame: function FM_switchToFrame(winId, frameId) {
+    let oopFrame = this.getOopFrame(winId, frameId);
+    let mm = this.getFrameMM(winId, frameId);
+
+    
+    
+    for (let i = 0; i < remoteFrames.length; i++) {
+      let frame = remoteFrames[i];
+      let frameMessageManager = frame.messageManager.get();
+      try {
+        frameMessageManager.sendAsyncMessage("aliveCheck", {});
+      } catch (e) {
+        if (e.result ==  Components.results.NS_ERROR_NOT_INITIALIZED) {
+          remoteFrames.splice(i--, 1);
+          continue;
+        }
+      }
+      if (frameMessageManager == mm) {
+        this.currentRemoteFrame = frame;
+        this.addMessageManagerListeners(mm);
+
+        mm.sendAsyncMessage("Marionette:restart");
+        return oopFrame.id;
+      }
+    }
+
+    
+    
+    
+    this.addMessageManagerListeners(mm);
+    let aFrame = new MarionetteRemoteFrame(winId, frameId);
+    aFrame.messageManager = Cu.getWeakReference(mm);
+    remoteFrames.push(aFrame);
+    this.currentRemoteFrame = aFrame;
+
+    mm.loadFrameScript(FRAME_SCRIPT, true, true);
+
+    return oopFrame.id;
+  },
+
+  
+
+
+
+  switchToModalOrigin: function FM_switchToModalOrigin() {
+    
+    if (this.previousRemoteFrame != null) {
+      this.currentRemoteFrame = this.previousRemoteFrame;
+      this.addMessageManagerListeners(this.currentRemoteFrame.messageManager.get());
+    }
+    this.handledModal = false;
+  },
+
+  
+
+
+
+
+
+
+
+
+
+
+  addMessageManagerListeners: function FM_addMessageManagerListeners(mm) {
+    mm.addWeakMessageListener("Marionette:ok", this.server);
+    mm.addWeakMessageListener("Marionette:done", this.server);
+    mm.addWeakMessageListener("Marionette:error", this.server);
+    mm.addWeakMessageListener("Marionette:emitTouchEvent", this.server);
+    mm.addWeakMessageListener("Marionette:log", this.server);
+    mm.addWeakMessageListener("Marionette:runEmulatorCmd", this.server.emulator);
+    mm.addWeakMessageListener("Marionette:runEmulatorShell", this.server.emulator);
+    mm.addWeakMessageListener("Marionette:shareData", this.server);
+    mm.addWeakMessageListener("Marionette:switchToModalOrigin", this.server);
+    mm.addWeakMessageListener("Marionette:switchedToFrame", this.server);
+    mm.addWeakMessageListener("Marionette:getVisibleCookies", this.server);
+    mm.addWeakMessageListener("Marionette:register", this.server);
+    mm.addWeakMessageListener("Marionette:listenersAttached", this.server);
+    mm.addWeakMessageListener("Marionette:getFiles", this.server);
+    mm.addWeakMessageListener("MarionetteFrame:handleModal", this);
+    mm.addWeakMessageListener("MarionetteFrame:getCurrentFrameId", this);
+    mm.addWeakMessageListener("MarionetteFrame:getInterruptedState", this);
+  },
+
+  
+
+
+
+
+
+
+
+
+
+
+
+  removeMessageManagerListeners: function FM_removeMessageManagerListeners(mm) {
+    mm.removeWeakMessageListener("Marionette:ok", this.server);
+    mm.removeWeakMessageListener("Marionette:done", this.server);
+    mm.removeWeakMessageListener("Marionette:error", this.server);
+    mm.removeWeakMessageListener("Marionette:log", this.server);
+    mm.removeWeakMessageListener("Marionette:shareData", this.server);
+    mm.removeWeakMessageListener("Marionette:runEmulatorCmd", this.server.emulator);
+    mm.removeWeakMessageListener("Marionette:runEmulatorShell", this.server.emulator);
+    mm.removeWeakMessageListener("Marionette:switchedToFrame", this.server);
+    mm.removeWeakMessageListener("Marionette:getVisibleCookies", this.server);
+    mm.removeWeakMessageListener("Marionette:listenersAttached", this.server);
+    mm.removeWeakMessageListener("Marionette:register", this.server);
+    mm.removeWeakMessageListener("Marionette:getFiles", this.server);
+    mm.removeWeakMessageListener("MarionetteFrame:handleModal", this);
+    mm.removeWeakMessageListener("MarionetteFrame:getCurrentFrameId", this);
+  }
+};
