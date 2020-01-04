81417fb653c58ca8c68e16597d8376dba3607f27
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 04:31:44 +0000

    Bug 1220544 - Fix -Wunreachable-code warnings in xpcom. r=mccr8
    xpcom/glue/PLDHashTable.cpp:471:10 [-Wunreachable-code-return] 'return' will never be executed
    xpcom/tests/TestAutoPtr.cpp:324:9 [-Wunreachable-code] code will never be executed
    xpcom/tests/TestBlockingProcess.cpp:6:11 [-Wunreachable-code-return] 'return' will never be executed
    
    UltraBlame original commit: 9d35ff5cf79698988fb3dac6628cb00c255e9337

--- xpcom/glue/PLDHashTable.cpp
+++ xpcom/glue/PLDHashTable.cpp
@@ -468,7 +468,6 @@ PLDHashTable::FindFreeEntry(PLDHashNumber aKeyHash)
   }
 
   
-  return nullptr;
 }
 
 bool
@@ -846,4 +845,3 @@ PLDHashTable::MarkImmutable()
   mChecker.SetNonWritable();
 }
 #endif
-
