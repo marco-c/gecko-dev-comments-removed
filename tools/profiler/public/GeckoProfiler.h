e9fe41ef0cb8f4a20cea23db256bf609664a9b52
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 05:53:23 +0000

    Bug 1103094 - Add accessor for nsIProfilerStartParams to nsIProfiler. r=BenWa
    UltraBlame original commit: fd8dc51c5a5c7fa98101cc2e1486177a36739968

--- tools/profiler/public/GeckoProfiler.h
+++ tools/profiler/public/GeckoProfiler.h
@@ -53,6 +53,7 @@
 #include "js/TypeDecls.h"
 #endif
 #include "mozilla/UniquePtr.h"
+#include "mozilla/Vector.h"
 
 namespace mozilla {
 class TimeStamp;
@@ -62,6 +63,10 @@ class Promise;
 } 
 
 } 
+
+#ifndef SPS_STANDALONE
+class nsIProfilerStartParams;
+#endif
 
 enum TracingMetadata {
   TRACING_DEFAULT,
@@ -182,6 +187,10 @@ static inline JSObject* profiler_get_profile_jsobject(JSContext* aCx,
 
 static inline void profiler_get_profile_jsobject_async(double aSinceTime = 0,
                                                        mozilla::dom::Promise* = 0) {}
+static inline void profiler_get_start_params(int* aEntrySize,
+                                             double* aInterval,
+                                             mozilla::Vector<const char*>* aFilters,
+                                             mozilla::Vector<const char*>* aFeatures) {}
 #endif
 
 
