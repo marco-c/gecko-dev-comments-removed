275139d55f47ff1c58c44acad27170ab1e465d6a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 08:03:35 +0000

    Bug 1239678 - fix dll inclusion pattern on Windows and the placement of nested dlls like browsercomps and clearkey, r=nalexander
    UltraBlame original commit: 8017627448ca6fcfe1c165a94d7e74c57eb032e3

--- python/mozbuild/mozbuild/artifacts.py
+++ python/mozbuild/mozbuild/artifacts.py
@@ -319,7 +319,7 @@ class WinArtifactJob(ArtifactJob):
         'firefox/dependentlibs.list',
         'firefox/platform.ini',
         'firefox/application.ini',
-        'firefox/*.dll',
+        'firefox/**/*.dll',
         'firefox/*.exe',
     }
     
@@ -341,7 +341,8 @@ class WinArtifactJob(ArtifactJob):
                 if not any(mozpath.match(f.filename, p) for p in self.package_artifact_patterns):
                     continue
 
-                basename = mozpath.basename(f.filename)
+                
+                basename = mozpath.relpath(f.filename, "firefox")
                 self.log(logging.INFO, 'artifact',
                     {'basename': basename},
                     'Adding {basename} to processed archive')
