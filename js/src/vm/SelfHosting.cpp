ad3f127af4688a2ee2f96c10328fee97246a2597
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 09:26:20 +0000

    Bug 1246131 - Part 2: Let opt builds compile again, even on a CLOSED TREE. r=bustage
    UltraBlame original commit: eac757a4e54f20dae57dbbecab0053133d1a34d2

--- js/src/vm/SelfHosting.cpp
+++ js/src/vm/SelfHosting.cpp
@@ -307,8 +307,8 @@ intrinsic_AssertionFailed(JSContext* cx, unsigned argc, Value* vp)
 static bool
 intrinsic_DumpMessage(JSContext* cx, unsigned argc, Value* vp)
 {
-#ifdef DEBUG
     CallArgs args = CallArgsFromVp(argc, vp);
+#ifdef DEBUG
     if (args.length() > 0) {
         
         JSString* str = ToString<CanGC>(cx, args[0]);
