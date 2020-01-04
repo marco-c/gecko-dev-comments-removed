05f3ac46cc812ed4779fcf1ca027dda4bce4b9e1
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 12:10:32 +0000

    Bug 1257319 - Add notifyObservers methods to GeckoAppShell; r=snorp Add notifyObservers and the synchronous syncNotifyObservers methods to GeckoAppShell to replace the BROADCAST event.
    UltraBlame original commit: f188815f4dbd0ad8c046b00073d1077c6cd72b50

--- mobile/android/base/java/org/mozilla/gecko/GeckoAppShell.java
+++ mobile/android/base/java/org/mozilla/gecko/GeckoAppShell.java
@@ -381,7 +381,25 @@ public class GeckoAppShell
     public static native void notifyGeckoOfEvent(GeckoEvent event);
 
     
-    public static native void notifyGeckoObservers(String subject, String data);
+    @WrapForJNI
+    public static native void syncNotifyObservers(String topic, String data);
+
+    @WrapForJNI(stubName = "NotifyObservers")
+    private static native void nativeNotifyObservers(String topic, String data);
+
+    public static void notifyObservers(final String topic, final String data) {
+        notifyObservers(topic, data, GeckoThread.State.RUNNING);
+    }
+
+    public static void notifyObservers(final String topic, final String data, final GeckoThread.State state) {
+        if (GeckoThread.isStateAtLeast(state)) {
+            nativeNotifyObservers(topic, data);
+        } else {
+            GeckoThread.queueNativeCallUntil(
+                    state, GeckoAppShell.class, "nativeNotifyObservers",
+                    String.class, topic, String.class, data);
+        }
+    }
 
     
 
