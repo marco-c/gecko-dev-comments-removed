f1eaa19ffa47ee5f382811664398c02a283efccc
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 23:50:38 +0000

    Backed out 2 changesets (bug 1280477) for depending on something backed out CLOSED TREE
    Backed out changeset 17fe7328e60a (bug 1280477)
    Backed out changeset d52564553d5e (bug 1280477)
    
    UltraBlame original commit: d55d2a73d11233578b47c03e4a07922f354cd9bc

--- build/clang-plugin/clang-plugin.cpp
+++ build/clang-plugin/clang-plugin.cpp
@@ -19,7 +19,6 @@
 #include "llvm/Support/FileSystem.h"
 #include "llvm/Support/Path.h"
 #include <memory>
-#include <iterator>
 
 #define CLANG_VERSION_FULL (CLANG_VERSION_MAJOR * 100 + CLANG_VERSION_MINOR)
 
@@ -224,8 +223,7 @@ bool isInIgnoredNamespaceForImplicitCtor(const Decl *Declaration) {
          Name == "MacFileUtilities" ||  
          Name == "dwarf2reader" ||      
          Name == "arm_ex_to_module" ||  
-         Name == "testing" ||           
-         Name == "Json";                
+         Name == "testing";             
 }
 
 bool isInIgnoredNamespaceForImplicitConversion(const Decl *Declaration) {
@@ -770,23 +768,11 @@ AST_MATCHER(BinaryOperator, isInSystemHeader) {
 
 
 
-AST_MATCHER(BinaryOperator, isInWhitelistForNaNExpr) {
-  const char* whitelist[] = {
-    "SkScalar.h",
-    "json_writer.cpp"
-  };
-
+AST_MATCHER(BinaryOperator, isInSkScalarDotH) {
   SourceLocation Loc = Node.getOperatorLoc();
   auto &SourceManager = Finder->getASTContext().getSourceManager();
   SmallString<1024> FileName = SourceManager.getFilename(Loc);
-
-  for (auto itr = std::begin(whitelist); itr != std::end(whitelist); itr++) {
-    if (llvm::sys::path::rbegin(FileName)->equals(*itr)) {
-      return true;
-    }
-  }
-
-  return false;
+  return llvm::sys::path::rbegin(FileName)->equals("SkScalar.h");
 }
 
 
@@ -1109,7 +1095,7 @@ DiagnosticsMatcher::DiagnosticsMatcher() {
                     declRefExpr(hasType(qualType((isFloat())))).bind("lhs"))),
                 hasRHS(has(
                     declRefExpr(hasType(qualType((isFloat())))).bind("rhs"))),
-                unless(anyOf(isInSystemHeader(), isInWhitelistForNaNExpr()))))
+                unless(anyOf(isInSystemHeader(), isInSkScalarDotH()))))
           .bind("node"),
       &NaNExpr);
 
