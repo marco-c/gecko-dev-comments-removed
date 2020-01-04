84e905b1f5aa8e083a6a5bc52721f288532434d0
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 21:18:12 +0000

    Bug 1292505 (Followup) - Add an assert that we don't create a decoder for an ICO resource during a metadata decode. r=me
    UltraBlame original commit: aef42c5d23290400e0ba59afc8c95f43fd2b1b8a

--- image/DecoderFactory.cpp
+++ image/DecoderFactory.cpp
@@ -246,6 +246,7 @@ DecoderFactory::CreateDecoderForICOResource(DecoderType aType,
   MOZ_ASSERT(decoder);
 
   
+  MOZ_ASSERT(!aICODecoder->IsMetadataDecode());
   decoder->SetMetadataDecode(aICODecoder->IsMetadataDecode());
   decoder->SetIterator(aSourceBuffer->Iterator());
   decoder->SetOutputSize(aICODecoder->OutputSize());
