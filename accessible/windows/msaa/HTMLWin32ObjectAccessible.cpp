cc9670b4dc374f50996ae3a01e4cb7b6abea8836
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 14:17:47 +0000

    Bug 1258228 - create special child accessibles in owner's constructor, part4, r=marcoz
    UltraBlame original commit: 2724b036ed2b3a3bab2987a563ccc7fc64488618

--- accessible/windows/msaa/HTMLWin32ObjectAccessible.cpp
+++ accessible/windows/msaa/HTMLWin32ObjectAccessible.cpp
@@ -20,8 +20,10 @@ HTMLWin32ObjectOwnerAccessible::
   AccessibleWrap(aContent, aDoc), mHwnd(aHwnd)
 {
   
-  if (mHwnd)
+  if (mHwnd) {
     mNativeAccessible = new HTMLWin32ObjectAccessible(mHwnd, aDoc);
+    AppendChild(mNativeAccessible);
+  }
 }
 
 
@@ -54,8 +56,6 @@ HTMLWin32ObjectOwnerAccessible::NativelyUnavailable() const
 void
 HTMLWin32ObjectOwnerAccessible::CacheChildren()
 {
-  if (mNativeAccessible)
-    AppendChild(mNativeAccessible);
 }
 
 
