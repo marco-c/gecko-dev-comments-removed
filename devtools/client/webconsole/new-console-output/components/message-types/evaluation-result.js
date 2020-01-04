99512d04db31f607431d2c2e41853463614c6994
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 22:51:42 +0000

    Bug 1296389 - Part 1: Use the same markup structure as the old console frontend r=me
    MozReview-Commit-ID: 2YVe8jtdJSe
    
    UltraBlame original commit: 17accf3384d670ab7d9dd2d0a7e84dc7ae2d009a

--- devtools/client/webconsole/new-console-output/components/message-types/evaluation-result.js
+++ devtools/client/webconsole/new-console-output/components/message-types/evaluation-result.js
@@ -42,10 +42,11 @@ function EvaluationResult(props) {
     
     
     icon,
-    dom.span(
-      {className: "message-body-wrapper message-body devtools-monospace"},
-      dom.span({},
-        GripMessageBody({grip: message.parameters})
+    dom.span({ className: "message-body-wrapper" },
+      dom.span({ className: "message-flex-body" },
+        dom.span({ className: "message-body devtools-monospace" },
+          GripMessageBody({grip: message.parameters})
+        )
       )
     )
   );
