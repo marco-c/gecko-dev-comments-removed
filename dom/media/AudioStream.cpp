cb3652f6ed001d67703ec54e60d0f529a608412c
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 23:14:03 +0000

    Bug 1286041 - Disable an assert when using winmm, on a CLOSED TREE. r=achronop
    MozReview-Commit-ID: HHWIDARNPft
    
    UltraBlame original commit: bb04b5569ab96f761384ca2b6d7206aa2daac416

--- dom/media/AudioStream.cpp
+++ dom/media/AudioStream.cpp
@@ -596,7 +596,8 @@ AudioStream::DataCallback(void* aBuffer, long aFrames)
   auto writer = AudioBufferWriter(
     reinterpret_cast<AudioDataValue*>(aBuffer), mOutChannels, aFrames);
 
-  MOZ_ASSERT(mState != INITIALIZED);
+  MOZ_ASSERT(mState != INITIALIZED ||
+             !strcmp(cubeb_get_backend_id(CubebUtils::GetCubebContext()), "winmm"));
 
   
   
