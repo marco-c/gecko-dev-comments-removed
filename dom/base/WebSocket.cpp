7e781cab3b4e5e4bc1bc60ea0ff1f645ff14d274
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 05:41:19 +0000

    Bug 1224596 part 4.  Switch WebSocket to using the new WorkerMainThreadRunnable::Dispatch signature.  r=khuey
    UltraBlame original commit: 01b94e9f414ec7d5bc30a713f5fda98477e7f00f

--- dom/base/WebSocket.cpp
+++ dom/base/WebSocket.cpp
@@ -336,7 +336,13 @@ WebSocketImpl::PrintErrorOnConsole(const char *aBundleURI,
     RefPtr<PrintErrorOnConsoleRunnable> runnable =
       new PrintErrorOnConsoleRunnable(this, aBundleURI, aError, aFormatStrings,
                                       aFormatStringsLen);
-    runnable->Dispatch(mWorkerPrivate->GetJSContext());
+    ErrorResult rv;
+    runnable->Dispatch(rv);
+    
+    
+    
+    
+    rv.SuppressException();
     return;
   }
 
@@ -617,7 +623,11 @@ WebSocketImpl::Disconnect()
   } else {
     RefPtr<DisconnectInternalRunnable> runnable =
       new DisconnectInternalRunnable(this);
-    runnable->Dispatch(mWorkerPrivate->GetJSContext());
+    ErrorResult rv;
+    runnable->Dispatch(rv);
+    
+    
+    rv.SuppressException();
   }
 
   
@@ -1259,7 +1269,7 @@ WebSocket::Constructor(const GlobalObject& aGlobal,
       new InitRunnable(webSocket->mImpl, aUrl, protocolArray,
                        nsAutoCString(file.get()), lineno, column, aRv,
                        &connectionFailed);
-    runnable->Dispatch(aGlobal.Context());
+    runnable->Dispatch(aRv);
   }
 
   if (NS_WARN_IF(aRv.Failed())) {
@@ -1339,7 +1349,7 @@ WebSocket::Constructor(const GlobalObject& aGlobal,
   } else {
     RefPtr<AsyncOpenRunnable> runnable =
       new AsyncOpenRunnable(webSocket->mImpl, aRv);
-    runnable->Dispatch(aGlobal.Context());
+    runnable->Dispatch(aRv);
   }
 
   if (NS_WARN_IF(aRv.Failed())) {
