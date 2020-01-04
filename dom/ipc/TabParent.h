7fa9a6037373df360e2252f8bdd3fd932b6e8714
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 00:29:13 +0000

    Bug 1170894 - Implement nsIFrameLoader::SwitchProcessAndLoadURI. r=smaug
    UltraBlame original commit: e4e12583c280bff2a12fc168dd2933e09d02adb3

--- dom/ipc/TabParent.h
+++ dom/ipc/TabParent.h
@@ -122,6 +122,8 @@ public:
     nsIXULBrowserWindow* GetXULBrowserWindow();
 
     void Destroy();
+    void Detach();
+    void Attach(nsFrameLoader* aFrameLoader);
 
     void RemoveWindowListeners();
     void AddWindowListeners();
@@ -515,6 +517,8 @@ private:
     bool mMarkedDestroying;
     
     bool mIsDestroyed;
+    
+    bool mIsDetached;
     
     bool mAppPackageFileDescriptorSent;
 
