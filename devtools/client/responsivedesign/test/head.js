4442fb863d664e21dc8d881dd1d54091773dcd73
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 07:35:31 +0000

    Backed out changeset b99ccc3b4e16 (bug 1235953) for devtools bustage ON A CLOSED TREE
    UltraBlame original commit: 312aec4080e5902b569fc59b47708c41325919d4

--- devtools/client/responsivedesign/test/head.js
+++ devtools/client/responsivedesign/test/head.js
@@ -3,14 +3,12 @@
 
 "use strict";
 
-let testDir = gTestPath.substr(0, gTestPath.lastIndexOf("/"));
 
-let sharedHeadURI = testDir + "../../../framework/test/shared-head.js";
-Services.scriptloader.loadSubScript(sharedHeadURI, this);
+Services.scriptloader.loadSubScript("chrome://mochitests/content/browser/devtools/client/framework/test/shared-head.js", this);
 
 
-let gcliHelpersURI = testDir + "../../../commandline/test/helpers.js";
-Services.scriptloader.loadSubScript(gcliHelpersURI, this);
+var testDir = gTestPath.substr(0, gTestPath.lastIndexOf("/"));
+Services.scriptloader.loadSubScript(testDir + "../../../commandline/test/helpers.js", this);
 
 DevToolsUtils.testing = true;
 registerCleanupFunction(() => {
@@ -25,39 +23,20 @@ registerCleanupFunction(() => {
 
 
 
-
-var openRDM = Task.async(function*(tab = gBrowser.selectedTab,
-                                   method = "menu") {
-  let manager = ResponsiveUI.ResponsiveUIManager;
-  let mgrOn = once(manager, "on");
-  if (method == "menu") {
+function openRDM(tab = gBrowser.selectedTab) {
+  return new Promise(resolve => {
+    let manager = ResponsiveUI.ResponsiveUIManager;
     document.getElementById("Tools:ResponsiveUI").doCommand();
-  } else {
-    synthesizeKeyFromKeyTag(document.getElementById("key_responsiveUI"));
-  }
-  yield mgrOn;
-
-  let rdm = manager.getResponsiveUIForTab(tab);
-  rdm.transitionsEnabled = false;
-  registerCleanupFunction(() => {
-    rdm.transitionsEnabled = true;
+    executeSoon(() => {
+      let rdm = manager.getResponsiveUIForTab(tab);
+      rdm.stack.setAttribute("notransition", "true");
+      registerCleanupFunction(function() {
+        rdm.stack.removeAttribute("notransition");
+      });
+      resolve({rdm, manager});
+    });
   });
-  return {rdm, manager};
-});
-
-
-
-
-
-var closeRDM = Task.async(function*(rdm) {
-  let mgr = ResponsiveUI.ResponsiveUIManager;
-  if (!rdm) {
-    rdm = mgr.getResponsiveUIForTab(gBrowser.selectedTab);
-  }
-  let mgrOff = mgr.once("off");
-  rdm.close();
-  yield mgrOff;
-});
+}
 
 
 
@@ -96,11 +75,6 @@ var openInspector = Task.async(function*() {
     toolbox: toolbox,
     inspector: inspector
   };
-});
-
-var closeToolbox = Task.async(function*() {
-  let target = TargetFactory.forTab(gBrowser.selectedTab);
-  yield gDevTools.closeToolbox(target);
 });
 
 
@@ -190,9 +164,46 @@ var addTab = Task.async(function* (url) {
   return tab;
 });
 
+
+
+
+
+
+
+
+
+function once(target, eventName, useCapture=false) {
+  info("Waiting for event: '" + eventName + "' on " + target + ".");
+
+  let deferred = promise.defer();
+
+  for (let [add, remove] of [
+    ["addEventListener", "removeEventListener"],
+    ["addListener", "removeListener"],
+    ["on", "off"]
+  ]) {
+    if ((add in target) && (remove in target)) {
+      target[add](eventName, function onEvent(...aArgs) {
+        info("Got event: '" + eventName + "' on " + target + ".");
+        target[remove](eventName, onEvent, useCapture);
+        deferred.resolve.apply(deferred, aArgs);
+      }, useCapture);
+      break;
+    }
+  }
+
+  return deferred.promise;
+}
+
 function wait(ms) {
   let def = promise.defer();
   setTimeout(def.resolve, ms);
+  return def.promise;
+}
+
+function nextTick() {
+  let def = promise.defer();
+  executeSoon(() => def.resolve())
   return def.promise;
 }
 
@@ -201,19 +212,19 @@ function wait(ms) {
 
 
 
-function waitForDocLoadComplete(aBrowser = gBrowser) {
+function waitForDocLoadComplete(aBrowser=gBrowser) {
   let deferred = promise.defer();
   let progressListener = {
-    onStateChange: function(webProgress, req, flags, status) {
+    onStateChange: function (webProgress, req, flags, status) {
       let docStop = Ci.nsIWebProgressListener.STATE_IS_NETWORK |
                     Ci.nsIWebProgressListener.STATE_STOP;
-      info(`Saw state ${flags.toString(16)} and status ${status.toString(16)}`);
+      info("Saw state " + flags.toString(16) + " and status " + status.toString(16));
 
       
       
       if ((flags & docStop) == docStop && status != Cr.NS_BINDING_ABORTED) {
         aBrowser.removeProgressListener(progressListener);
-        info("Browser loaded");
+        info("Browser loaded " + aBrowser.contentWindow.location);
         deferred.resolve();
       }
     },
