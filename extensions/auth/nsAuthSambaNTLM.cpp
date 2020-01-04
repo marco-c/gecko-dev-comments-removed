05c111cb4f1349e8a0c33fff53146188964aba8f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 16:44:17 +0000

    Bug 1270046 - Accept "AF" response from Samba 4.3.8, r=honzab.moz
    UltraBlame original commit: 9a33547316416679c6901b963ac327ae8087a14b

--- extensions/auth/nsAuthSambaNTLM.cpp
+++ extensions/auth/nsAuthSambaNTLM.cpp
@@ -256,7 +256,8 @@ nsAuthSambaNTLM::GetNextToken(const void *inToken,
     nsCString line;
     if (!ReadLine(mFromChildFD, line))
         return NS_ERROR_FAILURE;
-    if (!StringBeginsWith(line, NS_LITERAL_CSTRING("KK "))) {
+    if (!StringBeginsWith(line, NS_LITERAL_CSTRING("KK ")) &&
+        !StringBeginsWith(line, NS_LITERAL_CSTRING("AF "))) {
         
         return NS_ERROR_FAILURE;
     }
