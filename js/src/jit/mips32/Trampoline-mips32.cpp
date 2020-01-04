5cd7061f93ed43d3f78594265d919464739948b1
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 00:43:24 +0000

    Bug 1199080 - IonMonkey: MIPS32: Fix treating saved frame bit as part of the frame type. r=nbp
    UltraBlame original commit: 19e754769d7b743df3e4bd7a3c2ee1596c446151

--- js/src/jit/mips32/Trampoline-mips32.cpp
+++ js/src/jit/mips32/Trampoline-mips32.cpp
@@ -1146,7 +1146,7 @@ JitRuntime::generateProfilerExitFrameTailStub(JSContext* cx)
     
     
     
-    masm.ma_and(scratch2, scratch1, Imm32((1 << FRAMESIZE_SHIFT) - 1));
+    masm.ma_and(scratch2, scratch1, Imm32((1 << FRAMETYPE_BITS) - 1));
     masm.rshiftPtr(Imm32(FRAMESIZE_SHIFT), scratch1);
 
     
