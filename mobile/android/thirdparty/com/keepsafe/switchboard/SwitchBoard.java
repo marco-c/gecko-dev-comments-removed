3ac9d0f551ccd8b23c67ce6ca8ad8887637e605e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 14:24:17 +0000

    Bug 1262811 - Remove unnecessary Switchboard logging. r=sebastian
    MozReview-Commit-ID: 7y25PagX9E1
    
    UltraBlame original commit: c72c4adf49121c74257da7e91e4deb9a90363427

--- mobile/android/thirdparty/com/keepsafe/switchboard/SwitchBoard.java
+++ mobile/android/thirdparty/com/keepsafe/switchboard/SwitchBoard.java
@@ -185,11 +185,11 @@ public class SwitchBoard {
 
         try {
             final JSONObject experiment = new JSONObject(config).getJSONObject(experimentName);
-            if(DEBUG) Log.d(TAG, "experiment " + experimentName + " JSON object: " + experiment.toString());
-
             return experiment != null && experiment.getBoolean(IS_EXPERIMENT_ACTIVE);
         } catch (JSONException e) {
-            Log.e(TAG, "Error getting experiment from config", e);
+            
+            
+            
             return false;
         }
     }
@@ -263,8 +263,6 @@ public class SwitchBoard {
 
 
     @Nullable private static String readFromUrlGET(URL url) {
-        if (DEBUG) Log.d(TAG, "readFromUrl(): " + url);
-
         try {
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("GET");
@@ -273,15 +271,12 @@ public class SwitchBoard {
             InputStream is = connection.getInputStream();
             InputStreamReader inputStreamReader = new InputStreamReader(is);
             BufferedReader bufferReader = new BufferedReader(inputStreamReader, 8192);
-            String line = "";
+            String line;
             StringBuilder resultContent = new StringBuilder();
             while ((line = bufferReader.readLine()) != null) {
-                if(DEBUG) Log.d(TAG, line);
                 resultContent.append(line);
             }
             bufferReader.close();
-
-            if(DEBUG) Log.d(TAG, "readFromUrl() result: " + resultContent.toString());
 
             return resultContent.toString();
         } catch (IOException e) {
