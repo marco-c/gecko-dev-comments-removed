b8f9fce4c68b165e93d331841133d696f11d75ee
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 02:43:56 +0000

    Bug 1211339 Part 3 - Refuse to support video codecs inside audio mime types. r=jya
    UltraBlame original commit: dbe8d3254ccd054a3508d4d46c970b1ec4d03bc2

--- dom/media/fmp4/MP4Decoder.cpp
+++ dom/media/fmp4/MP4Decoder.cpp
@@ -98,10 +98,11 @@ MP4Decoder::CanHandleMediaType(const nsACString& aMIMETypeExcludingCodecs,
   
   
   
-  if (!aMIMETypeExcludingCodecs.EqualsASCII("audio/mp4") &&
-      !aMIMETypeExcludingCodecs.EqualsASCII("audio/x-m4a") &&
-      !aMIMETypeExcludingCodecs.EqualsASCII("video/mp4") &&
-      !aMIMETypeExcludingCodecs.EqualsASCII("video/x-m4v")) {
+  const bool isMP4Audio = aMIMETypeExcludingCodecs.EqualsASCII("audio/mp4") ||
+                          aMIMETypeExcludingCodecs.EqualsASCII("audio/x-m4a");
+  const bool isMP4Video = aMIMETypeExcludingCodecs.EqualsASCII("video/mp4") ||
+                          aMIMETypeExcludingCodecs.EqualsASCII("video/x-m4v");
+  if (!isMP4Audio && !isMP4Video) {
     return false;
   }
 
@@ -114,11 +115,10 @@ MP4Decoder::CanHandleMediaType(const nsACString& aMIMETypeExcludingCodecs,
   nsTArray<nsCString> codecMimes;
   if (aCodecs.IsEmpty()) {
     
-    if (aMIMETypeExcludingCodecs.EqualsLiteral("audio/mp4") ||
-        aMIMETypeExcludingCodecs.EqualsLiteral("audio/x-m4a")) {
+    if (isMP4Audio) {
       codecMimes.AppendElement(NS_LITERAL_CSTRING("audio/mp4a-latm"));
-    } else if (aMIMETypeExcludingCodecs.EqualsLiteral("video/mp4") ||
-               aMIMETypeExcludingCodecs.EqualsLiteral("video/x-m4v")) {
+    } else {
+      MOZ_ASSERT(isMP4Video);
       codecMimes.AppendElement(NS_LITERAL_CSTRING("video/avc"));
     }
   } else {
@@ -137,7 +137,9 @@ MP4Decoder::CanHandleMediaType(const nsACString& aMIMETypeExcludingCodecs,
         codecMimes.AppendElement(NS_LITERAL_CSTRING("audio/mpeg"));
         continue;
       }
-      if (IsWhitelistedH264Codec(codec)) {
+      
+      
+      if (IsWhitelistedH264Codec(codec) && isMP4Video) {
         codecMimes.AppendElement(NS_LITERAL_CSTRING("video/avc"));
         continue;
       }
