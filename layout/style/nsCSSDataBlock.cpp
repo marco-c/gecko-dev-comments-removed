5663ad20a62184a412c662cd4a16505da361b09b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 21:14:34 +0000

    Bug 1291283 - Use URLValueData::GetLocalURLFlag() to identify local-ref URI. r=dholbert
    MozReview-Commit-ID: 2tXEGMhPCkn
    
    UltraBlame original commit: 2eafd00d43790e42fb5801dfe5be4498579c25c5

--- layout/style/nsCSSDataBlock.cpp
+++ layout/style/nsCSSDataBlock.cpp
@@ -69,9 +69,24 @@ TryToStartImageLoadOnValue(const nsCSSValue& aValue, nsIDocument* aDocument,
     
     
     if (aProperty == eCSSProperty_mask_image) {
-      nsIURI* docURI = aDocument->GetDocumentURI();
+      
+      
+      
+      
+      URLValue* urlValue = aValue.GetURLStructValue();
+      if (urlValue->GetLocalURLFlag()) {
+        return;
+      }
+
+      
+      
+      
+      
+      
+      
       nsIURI* imageURI = aValue.GetURLValue();
       if (imageURI) {
+        nsIURI* docURI = aDocument->GetDocumentURI();
         bool isEqualExceptRef = false;
         nsresult  rv = imageURI->EqualsExceptRef(docURI, &isEqualExceptRef);
         if (NS_SUCCEEDED(rv) && isEqualExceptRef) {
