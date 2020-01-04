bd42e57e34b122dc0f55fb41c5f4c4c36cb1875d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 10:29:30 +0000

    Bug 1249174 (part 2) - Shrink xptiInterfaceEntry by reordering its fields. r=khuey.
    This reduces "xpti-working-set" by another 16 KiB on 64-bit.
    
    UltraBlame original commit: d8db031fc4a77c85088755f1919d9c4e11b82fed

--- xpcom/reflect/xptinfo/xptiprivate.h
+++ xpcom/reflect/xptinfo/xptiprivate.h
@@ -307,8 +307,6 @@ private:
     nsID                    mIID;
     XPTInterfaceDescriptor* mDescriptor;
 
-    uint16_t mMethodBaseIndex;
-    uint16_t mConstantBaseIndex;
     xptiTypelibGuts* mTypelib;
 
     xptiInterfaceEntry*     mParent;      
@@ -316,7 +314,12 @@ private:
     xptiInterfaceInfo* MOZ_UNSAFE_REF("The safety of this pointer is ensured "
                                       "by the semantics of xptiWorkingSet.")
                             mInfo;        
+
+    uint16_t mMethodBaseIndex;
+    uint16_t mConstantBaseIndex;
+
     xptiInfoFlags           mFlags;
+
     char                    mName[1];     
 };
 
