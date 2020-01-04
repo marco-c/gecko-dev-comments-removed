0cce3affa49d9e501c64eb5cec3ee0b2ce3aa9f6
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 08:32:51 +0000

    Bug 1241918 - remove workarounds for zero-length dash intervals in HelpersSkia.h, r=jrmuizel
    UltraBlame original commit: 90a2e74d14ca2316692bd86ec6064ecafbcd715c

--- gfx/2d/HelpersSkia.h
+++ gfx/2d/HelpersSkia.h
@@ -164,17 +164,6 @@ StrokeOptionsToPaint(SkPaint& aPaint, const StrokeOptions &aOptions)
 
     for (uint32_t i = 0; i < dashCount; i++) {
       pattern[i] = SkFloatToScalar(aOptions.mDashPattern[i % aOptions.mDashLength]);
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      if (pattern[i] == 0)
-          pattern[i] = SkScalarMulDiv(SK_ScalarNearlyZero, 33, 32);
     }
 
     SkDashPathEffect* dash = SkDashPathEffect::Create(&pattern.front(),
