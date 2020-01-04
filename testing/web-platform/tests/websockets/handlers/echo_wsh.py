5e84735da217883eda497fe23d0902c725a7aa73
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 08:34:53 +0000

    Bug 1242153 - Update web-platform-tests to revision c41adf030ec80a62ef9e1988d3c4be460f2a0012, a=testonly
    UltraBlame original commit: 549838d8eea69816f1a14ab5df248b3aa6e74a96

--- testing/web-platform/tests/websockets/handlers/echo_wsh.py
+++ testing/web-platform/tests/websockets/handlers/echo_wsh.py
@@ -1,5 +1,6 @@
 
 from mod_pywebsocket import msgutil
+from mod_pywebsocket import common
 
 _GOODBYE_MESSAGE = u'Goodbye'
 
@@ -23,3 +24,13 @@ def web_socket_transfer_data(request):
         else:
             request.ws_stream.send_message(line, binary=True)
 
+def web_socket_passive_closing_handshake(request):
+    
+    code, reason = request.ws_close_code, request.ws_close_reason
+
+    
+    
+    if code == common.STATUS_NO_STATUS_RECEIVED:
+        code = None
+
+    return code, reason
