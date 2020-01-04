bda3f7276b68d86f76d914c70ec02e59c8e9f582
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 17:04:14 +0000

    Bug 1260299 - Generate Visual Studio project files corresponding to current toolchain; r=chmanchester
    If we're running VS2013, we generate VS2013 files. If we're running VS2015,
    we generate VS2015 files. If we don't have a Visual Studio version
    defined, refuse to generate project files (hopefully this doesn't
    happen in the real world but I'm sure someone will complain if it does).
    
    MozReview-Commit-ID: 5GdsbGmWPLB
    
    UltraBlame original commit: 35199c26010a90e562177aab032dec68f985aa09

--- python/mozbuild/mozbuild/backend/visualstudio.py
+++ python/mozbuild/mozbuild/backend/visualstudio.py
@@ -74,8 +74,10 @@ class VisualStudioBackend(CommonBackend):
         
         self._out_dir = os.path.join(self.environment.topobjdir, 'msvc')
         self._projsubdir = 'projects'
-        
-        self._version = '2013'
+
+        self._version = self.environment.substs.get('MSVS_VERSION', None)
+        if not self._version:
+            raise Exception('MSVS_VERSION not defined; this should never happen')
 
         self._paths_to_sources = {}
         self._paths_to_includes = {}
