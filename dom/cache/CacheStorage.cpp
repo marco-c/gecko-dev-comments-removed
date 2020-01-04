246adfdd12661d42e8bbb35c579c16dfdd48f93a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 02:05:30 +0000

    Bug 1167100 - User nsIPrincipal.originAttribute in ContentPrincipalInfo. r=bholley
    UltraBlame original commit: d0e88c95f3c5abb537793b88c16a778c56050cae

--- dom/cache/CacheStorage.cpp
+++ dom/cache/CacheStorage.cpp
@@ -81,7 +81,7 @@ IsTrusted(const PrincipalInfo& aPrincipalInfo, bool aTestingPrefEnabled)
   
   
   if (NS_WARN_IF(aPrincipalInfo.type() != PrincipalInfo::TContentPrincipalInfo ||
-                 aPrincipalInfo.get_ContentPrincipalInfo().appId() ==
+                 aPrincipalInfo.get_ContentPrincipalInfo().attrs().mAppId ==
                  nsIScriptSecurityManager::UNKNOWN_APP_ID)) {
     return false;
   }
