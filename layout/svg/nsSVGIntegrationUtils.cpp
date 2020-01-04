b296f1956006a2562902a0adc1efdbdeebf18896
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 16:51:54 +0000

    Bug 1228280 - Part 6. Single SVG mask optimization;
    MozReview-Commit-ID: 7k1XRTpGbtH
    
    UltraBlame original commit: 91a6d836e51e6aec74442b62254f0beec61367cf

--- layout/svg/nsSVGIntegrationUtils.cpp
+++ layout/svg/nsSVGIntegrationUtils.cpp
@@ -443,6 +443,17 @@ GenerateMaskSurface(const nsSVGIntegrationUtils::PaintFramesParams& aParams,
     nsSVGIntegrationUtils::GetCSSPxToDevPxMatrix(aParams.frame);
 
   gfxContext& ctx = aParams.ctx;
+
+  
+  if ((svgMaskFrames.Length() == 1) && svgMaskFrames[0]) {
+    aOutMaskSurface =
+      svgMaskFrames[0]->GetMaskForMaskedFrame(&ctx, aParams.frame,
+                                              cssPxToDevPxMatrix, aOpacity,
+                                              &aOutMaskTransform,
+                                              svgReset->mMask.mLayers[0].mMaskMode);
+    return;
+  }
+
   ctx.Save();
   ctx.SetMatrix(gfxMatrix());
   gfxRect clipExtents = ctx.GetClipExtents();
