2763d2a6a69676d6ca4974f8ce5a57e92e539225
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 05:14:47 +0000

    Bug 1214619 - Remove nsISaveAsCharset as much as possible without breaking extensions in popular use. r=emk.
    UltraBlame original commit: 6644bf5d558c8e0810ea5e44756c3bcd1ea5ee7b

--- intl/unicharutil/nsSaveAsCharset.h
+++ intl/unicharutil/nsSaveAsCharset.h
@@ -3,71 +3,35 @@
 
 
 
-#ifndef nsSaveAsCharset_h__
-#define nsSaveAsCharset_h__
+#ifndef nsSaveAsCharset_h_
+#define nsSaveAsCharset_h_
 
 #include "nsStringFwd.h"
-#include "nsTArray.h"
 #include "nsISaveAsCharset.h"
-#include "nsCOMPtr.h"
-
-#define MASK_FALLBACK(a) (nsISaveAsCharset::mask_Fallback & (a))
-#define MASK_ENTITY(a) (nsISaveAsCharset::mask_Entity & (a))
-#define MASK_CHARSET_FALLBACK(a) (nsISaveAsCharset::mask_CharsetFallback & (a))
-#define ATTR_NO_FALLBACK(a) (nsISaveAsCharset::attr_FallbackNone == MASK_FALLBACK(a) && \
-                             nsISaveAsCharset::attr_EntityAfterCharsetConv != MASK_ENTITY(a))
-
-class nsIUnicodeEncoder;
-class nsIEntityConverter;
+#include "nsAutoPtr.h"
+#include "nsNCRFallbackEncoderWrapper.h"
+#include "nsString.h"
 
 class nsSaveAsCharset : public nsISaveAsCharset
 {
 public:
-	
-	
-	
-	
+
   nsSaveAsCharset();
 
-	
-	
-	
-	NS_DECL_ISUPPORTS
+  NS_DECL_ISUPPORTS
 
-	
-	
-	
-  NS_IMETHOD Init(const char *charset, uint32_t attr, uint32_t entityVersion) override;
+  NS_IMETHOD Init(const nsACString& aCharset, uint32_t aIgnored, uint32_t aAlsoIgnored) override;
 
-  NS_IMETHOD Convert(const char16_t *inString, char **_retval) override;
+  NS_IMETHOD Convert(const nsAString& ain, nsACString& aOut) override;
 
-  NS_IMETHODIMP GetCharset(char * *aCharset) override;
+  NS_IMETHODIMP GetCharset(nsACString& aCharset) override;
 
-protected:
+private:
 
   virtual ~nsSaveAsCharset();
 
-  NS_IMETHOD DoCharsetConversion(const char16_t *inString, char **outString);
-
-  NS_IMETHOD DoConversionFallBack(uint32_t inUCS4, char *outString, int32_t bufferLength);
-
-  
-  
-  NS_IMETHOD HandleFallBack(uint32_t character, char **outString, int32_t *bufferLength, 
-                            int32_t *currentPos, int32_t estimatedLength);
-
-  nsresult SetupUnicodeEncoder(const char* charset);
-
-  nsresult SetupCharsetList(const char *charsetList);
-
-  const char * GetNextCharset();
-
-  uint32_t mAttribute;                    
-  uint32_t mEntityVersion;                
-  nsCOMPtr<nsIUnicodeEncoder> mEncoder;   
-  nsCOMPtr<nsIEntityConverter> mEntityConverter;
-  nsTArray<nsCString> mCharsetList;
-  int32_t        mCharsetListIndex;
+  nsAutoPtr<nsNCRFallbackEncoderWrapper> mEncoder;
+  nsCString mCharset;
 };
 
 #endif
