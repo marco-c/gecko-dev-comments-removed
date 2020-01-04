9f1bc0a521459ade8cd162c3a99970cad210a364
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 06:58:15 +0000

    Bug 1211090 - Don't fail to open databases if we can't do buffered IO. r=froydnj
    UltraBlame original commit: 1df4fea9c5025aa91355c9fee1f13e17412cd3bc

--- netwerk/base/nsNetUtil.h
+++ netwerk/base/nsNetUtil.h
@@ -585,6 +585,9 @@ nsresult NS_NewBufferedOutputStream(nsIOutputStream **result,
 already_AddRefed<nsIOutputStream>
 NS_BufferOutputStream(nsIOutputStream *aOutputStream,
                       uint32_t aBufferSize);
+already_AddRefed<nsIInputStream>
+NS_BufferInputStream(nsIInputStream *aInputStream,
+                      uint32_t aBufferSize);
 
 
 nsresult NS_NewPostDataStream(nsIInputStream  **result,
