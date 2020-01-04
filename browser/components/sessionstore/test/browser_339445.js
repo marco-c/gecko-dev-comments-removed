0de08a0b00c0fb60fa5e589f5621b4534865b54c
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 16:31:14 +0000

    Bug 1251760 - Remove CPOWs from browser_bug339445.js. r=felipe
    UltraBlame original commit: 6664fb38c78954056e4a332ebe377964a0a66354

--- browser/components/sessionstore/test/browser_339445.js
+++ browser/components/sessionstore/test/browser_339445.js
@@ -2,31 +2,31 @@
 
 
 
-function test() {
+add_task(function* test() {
   
-
-  waitForExplicitFinish();
 
   let testURL = "http://mochi.test:8888/browser/" +
     "browser/components/sessionstore/test/browser_339445_sample.html";
 
   let tab = gBrowser.addTab(testURL);
-  promiseBrowserLoaded(tab.linkedBrowser).then(() => {
-    let doc = tab.linkedBrowser.contentDocument;
+  yield promiseBrowserLoaded(tab.linkedBrowser);
+
+  yield ContentTask.spawn(tab.linkedBrowser, null, function() {
+    let doc = content.document;
     is(doc.getElementById("storageTestItem").textContent, "PENDING",
        "sessionStorage value has been set");
-
-    let tab2 = gBrowser.duplicateTab(tab);
-    promiseTabRestored(tab2).then(() => {
-      let doc2 = tab2.linkedBrowser.contentDocument;
-      is(doc2.getElementById("storageTestItem").textContent, "SUCCESS",
-         "sessionStorage value has been duplicated");
-
-      
-      gBrowser.removeTab(tab2);
-      gBrowser.removeTab(tab);
-
-      finish();
-    });
   });
-}
+
+  let tab2 = gBrowser.duplicateTab(tab);
+  yield promiseTabRestored(tab2);
+
+  yield ContentTask.spawn(tab2.linkedBrowser, null, function() {
+    let doc2 = content.document;
+    is(doc2.getElementById("storageTestItem").textContent, "SUCCESS",
+       "sessionStorage value has been duplicated");
+  });
+
+  
+  yield Promise.all([ BrowserTestUtils.removeTab(tab2),
+                      BrowserTestUtils.removeTab(tab) ]);
+});
