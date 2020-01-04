7672011a2b426664e09ebfd99bc51dadb437190b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 20:06:16 +0000

    Bug 1279129 - Should schedule delazification for Debugger when relazifying scripts. r=jandem
    UltraBlame original commit: 81cb60fb8fbd8ef529f2a2c21852888b5fdab88b

---
+++ js/src/jit-test/tests/debug/Debugger-findScripts-23.js
@@ -0,0 +1,19 @@
+
+
+var dbg = new Debugger();
+
+var g = newGlobal();
+g.eval('function f(){}');
+assertEq(g.eval('isLazyFunction(f)'), true);
+
+dbg.addDebuggee(g);
+dbg.findScripts();
+assertEq(g.eval('isLazyFunction(f)'), false);
+
+dbg.removeAllDebuggees();
+relazifyFunctions();
+assertEq(g.eval('isLazyFunction(f)'), true);
+
+dbg.addDebuggee(g);
+var scripts = dbg.findScripts();
+assertEq(g.eval('isLazyFunction(f)'), false);
