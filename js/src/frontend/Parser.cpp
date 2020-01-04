25a816924831f99b8d5410665313bc0b274a5322
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 22:56:49 +0000

    Bug 1297706 - Syntax parse with statements. r=jandem
    UltraBlame original commit: 4f9433d3d361884c844a14cd52263c1653f2d83f

--- js/src/frontend/Parser.cpp
+++ js/src/frontend/Parser.cpp
@@ -5786,18 +5786,10 @@ Parser<ParseHandler>::yieldExpression(InHandling inHandling)
     MOZ_CRASH("yieldExpr");
 }
 
-template <>
-ParseNode*
-Parser<FullParseHandler>::withStatement(YieldHandling yieldHandling)
+template <typename ParseHandler>
+typename ParseHandler::Node
+Parser<ParseHandler>::withStatement(YieldHandling yieldHandling)
 {
-    
-    
-    if (handler.syntaxParser) {
-        handler.disableSyntaxParser();
-        abortedSyntaxParse = true;
-        return null();
-    }
-
     MOZ_ASSERT(tokenStream.isCurrentTokenType(TOK_WITH));
     uint32_t begin = pos().begin;
 
@@ -5829,14 +5821,6 @@ Parser<FullParseHandler>::withStatement(YieldHandling yieldHandling)
     pc->sc()->setBindingsAccessedDynamically();
 
     return handler.newWithStatement(begin, objectExpr, innerBlock);
-}
-
-template <>
-SyntaxParseHandler::Node
-Parser<SyntaxParseHandler>::withStatement(YieldHandling yieldHandling)
-{
-    JS_ALWAYS_FALSE(abortIfSyntaxParser());
-    return null();
 }
 
 template <typename ParseHandler>
