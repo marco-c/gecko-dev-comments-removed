6455fa15a25a5719c04062d737975cfdf0e1c115
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 16:17:54 +0000

    Bug 1259667 - part2: rename WidgetSimpleGestureEvent.allowedDirections to mAllowedDirections. r=masayuki
    MozReview-Commit-ID: 412YeoUoqIy
    
    UltraBlame original commit: 3a8436e77403e3fd4a0d5cbd4de310ecb56918d1

--- widget/TouchEvents.h
+++ widget/TouchEvents.h
@@ -98,7 +98,7 @@ public:
                            nsIWidget* aWidget)
     : WidgetMouseEventBase(aIsTrusted, aMessage, aWidget,
                            eSimpleGestureEventClass)
-    , allowedDirections(0)
+    , mAllowedDirections(0)
     , direction(0)
     , clickCount(0)
     , delta(0.0)
@@ -108,7 +108,7 @@ public:
   WidgetSimpleGestureEvent(const WidgetSimpleGestureEvent& aOther)
     : WidgetMouseEventBase(aOther.IsTrusted(), aOther.mMessage,
                            aOther.mWidget, eSimpleGestureEventClass)
-    , allowedDirections(aOther.allowedDirections)
+    , mAllowedDirections(aOther.mAllowedDirections)
     , direction(aOther.direction)
     , clickCount(0)
     , delta(aOther.delta)
@@ -128,7 +128,7 @@ public:
   }
 
   
-  uint32_t allowedDirections;
+  uint32_t mAllowedDirections;
   
   uint32_t direction;
   
