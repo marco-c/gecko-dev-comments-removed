785776a40a03becb1a7891566c8ae947cc863f20
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 21:11:41 +0000

    Bug 1288821 - Follow-up to fix unused variable error; r=me
    UltraBlame original commit: 5b972a509e33ad44c175354408ab6e3fce6c066c

--- toolkit/components/alerts/nsXULAlerts.cpp
+++ toolkit/components/alerts/nsXULAlerts.cpp
@@ -63,17 +63,15 @@ NS_IMPL_ISUPPORTS(nsXULAlerts, nsIAlertsService, nsIAlertsDoNotDisturb, nsIAlert
  already_AddRefed<nsXULAlerts>
 nsXULAlerts::GetInstance()
 {
-#ifdef MOZ_WIDGET_ANDROID
   
-  return nullptr;
-#else
+#ifndef MOZ_WIDGET_ANDROID
   if (!gXULAlerts) {
     gXULAlerts = new nsXULAlerts();
     ClearOnShutdown(&gXULAlerts);
   }
+#endif 
   RefPtr<nsXULAlerts> instance = gXULAlerts.get();
   return instance.forget();
-#endif 
 }
 
 NS_IMETHODIMP
