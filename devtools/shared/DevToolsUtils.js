7f1d5531bfa390c22ba57a1100e236cc4fa972ed
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 07:43:56 +0000

    Bug 1234600 - executeSoon async stacks only when DEBUG_JS_MODULES enabled. r=fitzgen
    UltraBlame original commit: 92cb3865720d4b52244774b2d08d77d3a4fdf5c2

--- devtools/shared/DevToolsUtils.js
+++ devtools/shared/DevToolsUtils.js
@@ -189,10 +189,17 @@ exports.executeSoon = function executeSoon(aFn) {
   if (isWorker) {
     setImmediate(aFn);
   } else {
-    let stack = components.stack;
-    let executor = () => {
-      Cu.callFunctionWithAsyncStack(aFn, stack, "DevToolsUtils.executeSoon");
-    };
+    let executor;
+    
+    
+    if (AppConstants.DEBUG_JS_MODULES || exports.testing) {
+      let stack = components.stack;
+      executor = () => {
+        Cu.callFunctionWithAsyncStack(aFn, stack, "DevToolsUtils.executeSoon");
+      };
+    } else {
+      executor = aFn;
+    }
     Services.tm.mainThread.dispatch({
       run: exports.makeInfallible(executor)
     }, Ci.nsIThread.DISPATCH_NORMAL);
--- devtools/shared/tests/unit/head_devtools.js
+++ devtools/shared/tests/unit/head_devtools.js
@@ -7,6 +7,11 @@ var Cr = Components.results;
 var {require} = Cu.import("resource://devtools/shared/Loader.jsm");
 const DevToolsUtils = require("devtools/shared/DevToolsUtils");
 
+DevToolsUtils.testing = true;
+do_register_cleanup(() => {
+  DevToolsUtils.testing = false;
+});
+
 
 
 
