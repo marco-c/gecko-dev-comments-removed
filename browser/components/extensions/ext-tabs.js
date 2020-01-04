e77a5fa4f477ce97fa0636686847e5bb3aa0dab7
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 03:31:02 +0000

    Bug 1214040 - WebExtension tabs.sendMessage should not raise an exception on unexistent tabId. r=billm
    UltraBlame original commit: bf9a0354bc7cb39558f137be89fa98e11d7f128b

--- browser/components/extensions/ext-tabs.js
+++ browser/components/extensions/ext-tabs.js
@@ -493,6 +493,10 @@ extensions.registerAPI((extension, context) => {
 
       sendMessage: function(tabId, message, options, responseCallback) {
         let tab = TabManager.getTab(tabId);
+        if (!tab) {
+          
+          return;
+        }
         let mm = tab.linkedBrowser.messageManager;
 
         let recipient = {extensionId: extension.id};
