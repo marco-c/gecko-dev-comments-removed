ecde59b36d0e7be426e31aa6f3a4dad757c62716
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 03:27:27 +0000

    Bug 1212027 - part 5 - add LoneManagedOrNull for simplifying a lot of upcoming code; r=jld
    A lot of existing code has variations on:
    
      if (ManagedPFooChild().Length()) {
        ...(ManagedPFooChild()[0])...
      }
      // Do something with nullptr, or some other action.
    
    It's pretty reasonable to repeat this code when the managed protocols
    are stored in an array; the code gets much less nice when managed
    protocols are stored in a hashtable.  Let's write a small utility
    function to handle those details for us.  Then when we change the
    underlying storage, we only need to update this function, rather than a
    bunch of callsites.
    
    ProtocolUtils.h is included by all the generated IPDL headers, so
    LoneManagedOrNull should be available everywhere the above pattern would
    be encountered.
    
    UltraBlame original commit: 19c5de45a704d1a8233b9326b670220fb938832c

--- ipc/glue/ProtocolUtils.h
+++ ipc/glue/ProtocolUtils.h
@@ -327,6 +327,17 @@ DuplicateHandle(HANDLE aSourceHandle,
 #endif
 
 } 
+
+template<typename Protocol>
+Protocol*
+LoneManagedOrNull(const nsTArray<Protocol*>& aManagees)
+{
+    if (aManagees.Length() == 0) {
+        return nullptr;
+    }
+    return aManagees[0];
+}
+
 } 
 
 
