86b7eae3030c6ce6eb196fa74b92ca8dcbbd9a15
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 16:09:23 +0000

    Bug 1269263 - Remove SEC_NORMAL from browser/components/feeds/test/unit/test_355473.js (r=sicking)
    UltraBlame original commit: 3aa3b8a544fb70ec7edf0df7699287a483d618f2

--- browser/components/feeds/test/unit/test_355473.js
+++ browser/components/feeds/test/unit/test_355473.js
@@ -1,5 +1,5 @@
 var Cu = Components.utils;
-Cu.import("resource://gre/modules/Services.jsm");
+Cu.import("resource://gre/modules/NetUtil.jsm");
 
 function run_test() {
   var feedFeedURI = ios.newURI("feed://example.com/feed.xml", null, null);
@@ -10,24 +10,20 @@ function run_test() {
     ios.newURI("feed:https://example.com/feed.xml", null, null);
   var httpsURI = ios.newURI("https://example.com/feed.xml", null, null);
 
-  var feedChannel = ios.newChannelFromURI2(feedFeedURI,
-                                           null,      
-                                           Services.scriptSecurityManager.getSystemPrincipal(),
-                                           null,      
-                                           Ci.nsILoadInfo.SEC_NORMAL,
-                                           Ci.nsIContentPolicy.TYPE_OTHER);
-  var httpChannel = ios.newChannelFromURI2(httpFeedURI,
-                                           null,      
-                                           Services.scriptSecurityManager.getSystemPrincipal(),
-                                           null,      
-                                           Ci.nsILoadInfo.SEC_NORMAL,
-                                           Ci.nsIContentPolicy.TYPE_OTHER);
-  var httpsChannel = ios.newChannelFromURI2(httpsFeedURI,
-                                            null,      
-                                            Services.scriptSecurityManager.getSystemPrincipal(),
-                                            null,      
-                                            Ci.nsILoadInfo.SEC_NORMAL,
-                                            Ci.nsIContentPolicy.TYPE_OTHER);
+  var feedChannel = NetUtil.newChannel({
+    uri: feedFeedURI,
+    loadUsingSystemPrincipal: true
+  });
+
+  var httpChannel = NetUtil.newChannel({
+    uri: httpFeedURI,
+    loadUsingSystemPrincipal: true
+  });
+
+  var httpsChannel = NetUtil.newChannel({
+    uri: httpsFeedURI,
+    loadUsingSystemPrincipal: true
+  });
 
   
   do_check_true(feedFeedURI.equals(feedChannel.originalURI));
