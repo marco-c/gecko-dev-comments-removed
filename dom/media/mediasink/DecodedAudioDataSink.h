3190878094e380c33221d578d0f19d8fa6855318
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 14:59:03 +0000

    Backed out changeset 4e02fc1cbd35 (bug 1264199)
    UltraBlame original commit: f9390125f8425c6a72502f58a0a173c2965086ac

--- dom/media/mediasink/DecodedAudioDataSink.h
+++ dom/media/mediasink/DecodedAudioDataSink.h
@@ -115,9 +115,6 @@ private:
   void OnAudioPopped(const RefPtr<MediaData>& aSample);
   void OnAudioPushed(const RefPtr<MediaData>& aSample);
   void NotifyAudioNeeded();
-  
-  
-  uint32_t DrainConverter(uint32_t aMaxFrames = UINT32_MAX);
   already_AddRefed<AudioData> CreateAudioFromBuffer(AlignedAudioBuffer&& aBuffer,
                                                     AudioData* aReference);
   UniquePtr<AudioConverter> mConverter;
@@ -128,7 +125,6 @@ private:
   
   
   int64_t mFramesParsed;
-  Maybe<RefPtr<AudioData>> mLastProcessedPacket;
   int64_t mLastEndTime;
   
   uint32_t mOutputRate;
