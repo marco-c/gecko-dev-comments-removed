dbbe552ba30a81e3e14cfb9cf44d749a786e16af
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 02:21:45 +0000

    Bug 1196358 - update volume setting to database when the volume changing. r=sotaro.
    UltraBlame original commit: 62392cb2aeee65ba6a41680ae1ab67df62cfccb3

--- dom/system/gonk/AudioManager.h
+++ dom/system/gonk/AudioManager.h
@@ -28,6 +28,8 @@
       {0x89, 0x10, 0xf9, 0x3c, 0x55, 0xe6, 0x62, 0xec}}
 #define NS_AUDIOMANAGER_CONTRACTID "@mozilla.org/telephony/audiomanager;1"
 
+class nsISettingsServiceLock;
+
 namespace mozilla {
 namespace hal {
 class SwitchEvent;
@@ -169,6 +171,9 @@ private:
 
   
   void InitVolumeFromDatabase();
+  void UpdateVolumeSettingToDatabase(nsISettingsServiceLock* aLock,
+                                     const char* aTopic,
+                                     uint32_t aVolIndex);
 
   
   void InitProfileVolumeSucceeded();
