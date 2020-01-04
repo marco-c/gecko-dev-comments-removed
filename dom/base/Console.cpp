32b58037450c6550dbda1bca3bf9fb586195cd4b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 08:47:40 +0000

    Bug 1239506 - Make console formatter return NaN instead of nan with %f;r=baku
    UltraBlame original commit: 24320d1e9d99d14e391db8013e88736a6bff4247

--- dom/base/Console.cpp
+++ dom/base/Console.cpp
@@ -1595,9 +1595,14 @@ Console::ProcessArguments(JSContext* aCx,
             return false;
           }
 
-          nsCString format;
-          MakeFormatString(format, integer, mantissa, 'f');
-          output.AppendPrintf(format.get(), v);
+          
+          if (std::isnan(v)) {
+            output.AppendFloat(v);
+          } else {
+            nsCString format;
+            MakeFormatString(format, integer, mantissa, 'f');
+            output.AppendPrintf(format.get(), v);
+          }
         }
         break;
 
