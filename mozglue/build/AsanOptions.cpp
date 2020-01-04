c8d8e910a34a97d80779758711eaa1e48d808d86
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 03:48:09 +0000

    Bug 1216354 - Disable LSan leak checking by default in mozglue. r=glandium
    UltraBlame original commit: 8afdbe8ce1dd61202c8f13b4999e5437caa688cf

--- mozglue/build/AsanOptions.cpp
+++ mozglue/build/AsanOptions.cpp
@@ -20,9 +20,16 @@
 
 
 
+
+
+
+
+
+
+
 extern "C" MOZ_ASAN_BLACKLIST
 const char* __asan_default_options() {
-    return "allow_user_segv_handler=1:alloc_dealloc_mismatch=0";
+    return "allow_user_segv_handler=1:alloc_dealloc_mismatch=0:detect_leaks=0";
 }
 
 #endif
