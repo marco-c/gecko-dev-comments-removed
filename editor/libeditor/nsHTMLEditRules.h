591f68bd0bc02bf8635496b3227c4473324c5933
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 15:50:24 +0000

    Bug 1191356 part 9 - Convert nsHTMLEditRules::mNewBlock to Element; r=ehsan
    UltraBlame original commit: acf185618acd3cbd431605def5677f9fa31bb0cc

--- editor/libeditor/nsHTMLEditRules.h
+++ editor/libeditor/nsHTMLEditRules.h
@@ -341,7 +341,7 @@ protected:
   bool                    mRestoreContentEditableCount;
   RefPtr<nsRange>       mUtilRange;
   uint32_t                mJoinOffset;  
-  nsCOMPtr<nsIDOMNode>    mNewBlock;
+  nsCOMPtr<Element>       mNewBlock;
   RefPtr<nsRangeStore>  mRangeItem;
   StyleCache              mCachedStyles[SIZE_STYLE_TABLE];
 };
