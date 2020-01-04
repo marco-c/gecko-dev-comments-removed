1f19573acf6ebbb96a9d484b42cfbbde94115fbc
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 22:49:23 +0000

    Bug 1293062 test fixup: name the Flash nptest plugin "Shockwave Flash" so that it passes for Flash during tests.
    MozReview-Commit-ID: 7kou5izPgmU
    
    UltraBlame original commit: fdf0f27af56b0ebcfece8ff25136048616e3b723

--- dom/plugins/test/testplugin/flashplugin/nptest_name.cpp
+++ dom/plugins/test/testplugin/flashplugin/nptest_name.cpp
@@ -2,6 +2,6 @@
 
 
 
-const char *sPluginName = "Flash Test Plug-in";
+const char *sPluginName = "Shockwave Flash";
 const char *sPluginDescription = "Flash plug-in for testing purposes.";
 const char *sMimeDescription = "application/x-shockwave-flash-test:swf:Flash test type";
