fb42e7d7d2dcafca4958b9b56fc55e9d5f6bc0b3
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 10:03:44 +0000

    Bug 1213094 - Removed services.sync.enabled preference. r=markh
    UltraBlame original commit: eb76ccb9bc1798ca98392d5a96aa1175f8adb2ec

--- services/sync/modules/service.js
+++ services/sync/modules/service.js
@@ -300,21 +300,6 @@ Sync11Service.prototype = {
   },
 
   
-  
-  get enabled() {
-    return Svc.Prefs.get("enabled");
-  },
-  set enabled(val) {
-    
-    
-    
-    if (val) {
-      throw new Error("Only disabling via this setter is supported");
-    }
-    Svc.Prefs.set("enabled", val);
-  },
-
-  
 
 
   onStartup: function onStartup() {
@@ -342,6 +327,8 @@ Sync11Service.prototype = {
 
     this._clusterManager = this.identity.createClusterManager(this);
     this.recordManager = new RecordManager(this);
+
+    this.enabled = true;
 
     this._registerEngines();
 
@@ -1239,10 +1226,6 @@ Sync11Service.prototype = {
   },
 
   sync: function sync(engineNamesToSync) {
-    if (!this.enabled) {
-      this._log.debug("Not syncing as Sync is disabled.");
-      return;
-    }
     let dateStr = new Date().toLocaleFormat(LOG_DATE_FORMAT);
     this._log.debug("User-Agent: " + SyncStorageRequest.prototype.userAgent);
     this._log.info("Starting sync at " + dateStr);
