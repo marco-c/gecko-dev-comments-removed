33ec7fbea6e6fbbddc38eadf1b3f586096e669ea
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 02:21:08 +0000

    Bug 1209209 - Create functional.py script for firefox-ui-tests to handling Firefox builds for Jenkins. r=armenzg DONTBUILD
    UltraBlame original commit: e24c8c95d6196b188a1263f2ff412630369145ac

--- testing/mozharness/scripts/firefox_ui_tests/update_release.py
+++ testing/mozharness/scripts/firefox_ui_tests/update_release.py
@@ -4,11 +4,8 @@
 
 
 
-"""firefox_ui_updates.py
 
-Author: Armen Zambrano G.
-        Henrik Skupin
-"""
+
 import copy
 import os
 import pprint
@@ -247,7 +244,6 @@ class ReleaseFirefoxUIUpdateTests(FirefoxUIUpdateTests):
 
                 retcode = self.run_test(
                     installer_path=installer_path,
-                    script_name=self.cli_script,
                     env=self.query_env(avoid_host_env=True),
                     marionette_port=marionette_port,
                 )
