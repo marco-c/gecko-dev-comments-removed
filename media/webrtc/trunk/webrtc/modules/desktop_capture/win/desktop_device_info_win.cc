82fafe10a20e8a3d8569162ceccbcdc500769531
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 06:03:57 +0000

    Bug 1202087 - Filter out non-shareable application for win8 or greater. r=jesup
    UltraBlame original commit: 95853027847262e97e52f57497206a87a3e0681b

--- media/webrtc/trunk/webrtc/modules/desktop_capture/win/desktop_device_info_win.cc
+++ media/webrtc/trunk/webrtc/modules/desktop_capture/win/desktop_device_info_win.cc
@@ -7,6 +7,7 @@
 #include "webrtc/modules/desktop_capture/win/win_shared.h"
 #include <inttypes.h>
 #include <stdio.h>
+#include <VersionHelpers.h>
 
 
 
@@ -89,6 +90,17 @@ void DesktopDeviceInfoWin::InitializeApplicationList() {
 
     
     if (dwProcessId == 0 || dwProcessId == GetCurrentProcessId()) {
+      continue;
+    }
+
+    
+    
+    const int classLength = 256;
+    WCHAR class_name[classLength] = {0};
+    GetClassName(hWnd, class_name, classLength);
+    if (IsWindows8OrGreater() &&
+        (wcscmp(class_name, L"ApplicationFrameWindow") == 0 ||
+         wcscmp(class_name, L"Windows.UI.Core.CoreWindow") == 0)) {
       continue;
     }
 
