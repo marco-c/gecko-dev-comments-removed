b3a26b99adae55b7388e3c996a48f8fbe43ca2a7
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 15:05:52 +0000

    Bug 1261578 - Part 4. Correct text overflow color; r=jfkthame
    MozReview-Commit-ID: 2zbmsHxb6II
    
    UltraBlame original commit: 4ec793ac0a38fa86bb8fb826534e0632063c9144

--- layout/generic/TextOverflow.cpp
+++ layout/generic/TextOverflow.cpp
@@ -214,8 +214,8 @@ void
 nsDisplayTextOverflowMarker::Paint(nsDisplayListBuilder* aBuilder,
                                    nsRenderingContext*   aCtx)
 {
-  nscolor foregroundColor =
-    nsLayoutUtils::GetColor(mFrame, eCSSProperty_color);
+  nsCSSProperty colorProp = mFrame->StyleContext()->GetTextFillColorProp();
+  nscolor foregroundColor = nsLayoutUtils::GetColor(mFrame, colorProp);
 
   
   nsLayoutUtils::PaintTextShadow(mFrame, aCtx, mRect, mVisibleRect,
