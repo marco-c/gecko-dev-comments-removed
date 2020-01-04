087075bf19f9bd8a9710b12c4ec7f68f5f58d48f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 07:14:46 +0000

    Bug 1229932 - check that offsets to canvas fillText() are finite. r=jmuizelaar
    UltraBlame original commit: aaa92eab9fe4c5e4ef441bcc8375591faa2a49a0

--- dom/canvas/CanvasRenderingContext2D.cpp
+++ dom/canvas/CanvasRenderingContext2D.cpp
@@ -3811,6 +3811,10 @@ CanvasRenderingContext2D::DrawOrMeasureText(const nsAString& aRawText,
     return NS_OK;
   }
 
+  if (!IsFinite(aX) || !IsFinite(aY)) {
+    return NS_OK;
+  }
+
   const ContextState &state = CurrentState();
 
   
