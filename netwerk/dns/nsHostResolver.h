9143bce23209a1a5bac223d6eeee9d99189cf3f3
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 23:10:31 +0000

    Bug 1185120 - Convert raw pointers in DNS.cpp and nsHostResolver.cpp to smart pointers. r=valentin
    UltraBlame original commit: 38cad72a77a69b3b47eff8b70d64af4486253b25

--- netwerk/dns/nsHostResolver.h
+++ netwerk/dns/nsHostResolver.h
@@ -21,6 +21,7 @@
 #include "mozilla/net/DNS.h"
 #include "mozilla/net/DashboardTypes.h"
 #include "mozilla/TimeStamp.h"
+#include "mozilla/UniquePtr.h"
 
 class nsHostResolver;
 class nsHostRecord;
@@ -73,8 +74,8 @@ public:
 
     Mutex        addr_info_lock;
     int          addr_info_gencnt; 
-    mozilla::net::AddrInfo *addr_info;
-    mozilla::net::NetAddr  *addr;
+    mozilla::UniquePtr<mozilla::net::AddrInfo> addr_info;
+    mozilla::UniquePtr<mozilla::net::NetAddr>  addr;
     bool         negative;   
 
 
