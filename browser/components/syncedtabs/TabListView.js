1e73e6b37f54be2131b79c6c6059cdb45bea1628
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 22:27:12 +0000

    Bug 1288247 - Open synced tabs in a new tab on middle click. r=markh
    MozReview-Commit-ID: LTE5NolY8V3
    
    UltraBlame original commit: 9c50e31bc8b6393cdcf328a92b1ce6fbfd5459dd

--- browser/components/syncedtabs/TabListView.js
+++ browser/components/syncedtabs/TabListView.js
@@ -185,6 +185,7 @@ TabListView.prototype = {
   
   _attachListListeners() {
     this.list.addEventListener("click", this.onClick.bind(this));
+    this.list.addEventListener("mouseup", this.onMouseUp.bind(this));
     this.list.addEventListener("keydown", this.onKeyDown.bind(this));
   },
 
@@ -263,6 +264,12 @@ TabListView.prototype = {
     }
   },
 
+  onMouseUp(event) {
+    if (event.which == 2) { 
+      this.onClick(event);
+    }
+  },
+
   onClick(event) {
     let itemNode = this._findParentItemNode(event.target);
     if (!itemNode) {
@@ -276,7 +283,18 @@ TabListView.prototype = {
       }
     }
 
-    if (event.target.classList.contains("item-twisty-container")) {
+    
+    if (itemNode.classList.contains("client")) {
+      let where = getChromeWindow(this._window).whereToOpenLink(event);
+      if (where != "current") {
+        const tabs = itemNode.querySelector(".item-tabs-list").childNodes;
+        const urls = [...tabs].map(tab => tab.dataset.url);
+        this.props.onOpenTabs(urls, where, {});
+      }
+    }
+
+    if (event.target.classList.contains("item-twisty-container")
+        && event.which != 2) {
       this.props.onToggleBranch(itemNode.dataset.id);
       return;
     }
