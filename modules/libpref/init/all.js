eb38db64700e079665f39eb284e639031ad73bbf
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 14:24:23 +0000

    Bug 1198518: [webvr] add support for OSVR. r=kip
    UltraBlame original commit: 2da4c57f45951fe964d32c6ca78a6c1dfe02f2a9

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -4810,6 +4810,8 @@ pref("dom.vr.oculus050.enabled", true);
 
 pref("dom.vr.cardboard.enabled", false);
 
+pref("dom.vr.osvr.enabled", false);
+
 pref("dom.vr.add-test-devices", 0);
 
 
@@ -4819,6 +4821,11 @@ pref("dom.vr.poseprediction.enabled", false);
 
 
 pref("gfx.vr.mirror-textures", false);
+
+pref("gfx.vr.osvr.utilLibPath", "");
+pref("gfx.vr.osvr.commonLibPath", "");
+pref("gfx.vr.osvr.clientLibPath", "");
+pref("gfx.vr.osvr.clientKitLibPath", "");
 
 
 pref("wap.UAProf.url", "");
