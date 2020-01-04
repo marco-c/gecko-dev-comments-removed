02bea3e5420a7da8a82e46473d8ccdbf8165715d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 02:21:26 +0000

    Bug 1207519 - Prevent HashTable shrink from ignoring allocation failures that may have been reported r=Waldo
    UltraBlame original commit: 2e82f6299d4a1084418f295c737be821b6074cdb

--- mfbt/AllocPolicy.h
+++ mfbt/AllocPolicy.h
@@ -60,11 +60,18 @@ namespace mozilla {
 
 
 
+
+
+
+
+
+
+
 class MallocAllocPolicy
 {
 public:
   template <typename T>
-  T* pod_malloc(size_t aNumElems)
+  T* maybe_pod_malloc(size_t aNumElems)
   {
     if (aNumElems & mozilla::tl::MulOverflowMask<sizeof(T)>::value) {
       return nullptr;
@@ -73,18 +80,36 @@ public:
   }
 
   template <typename T>
-  T* pod_calloc(size_t aNumElems)
+  T* maybe_pod_calloc(size_t aNumElems)
   {
     return static_cast<T*>(calloc(aNumElems, sizeof(T)));
   }
 
   template <typename T>
-  T* pod_realloc(T* aPtr, size_t aOldSize, size_t aNewSize)
+  T* maybe_pod_realloc(T* aPtr, size_t aOldSize, size_t aNewSize)
   {
     if (aNewSize & mozilla::tl::MulOverflowMask<sizeof(T)>::value) {
       return nullptr;
     }
     return static_cast<T*>(realloc(aPtr, aNewSize * sizeof(T)));
+  }
+
+  template <typename T>
+  T* pod_malloc(size_t aNumElems)
+  {
+    return maybe_pod_malloc<T>(aNumElems);
+  }
+
+  template <typename T>
+  T* pod_calloc(size_t aNumElems)
+  {
+    return maybe_pod_calloc<T>(aNumElems);
+  }
+
+  template <typename T>
+  T* pod_realloc(T* aPtr, size_t aOldSize, size_t aNewSize)
+  {
+    return maybe_pod_realloc<T>(aPtr, aOldSize, aNewSize);
   }
 
   void free_(void* aPtr)
