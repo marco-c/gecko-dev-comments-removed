8a3bde84c7fb16d7aa6f10d8ec35a5109b8be115
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 19:53:01 +0000

    Bug 1224664 - Assert if an ErrorResult is accessed on a thread different than the one it's created on, r=khuey
    UltraBlame original commit: 469d01eebea4e2055553289ce6542fc093460bbd

--- dom/bindings/ErrorResult.h
+++ dom/bindings/ErrorResult.h
@@ -31,6 +31,7 @@
 #include "mozilla/Assertions.h"
 #include "mozilla/Move.h"
 #include "nsTArray.h"
+#include "nsISupportsImpl.h"
 
 namespace IPC {
 class Message;
@@ -105,6 +106,7 @@ public:
     MOZ_ASSERT(!Failed());
     MOZ_ASSERT(!mMightHaveUnreportedJSException);
     MOZ_ASSERT(mUnionState == HasNothing);
+    NS_ASSERT_OWNINGTHREAD(ErrorResult);
   }
 #endif 
 
@@ -336,6 +338,12 @@ private:
 #endif 
   }
 
+  MOZ_ALWAYS_INLINE void AssertInOwningThread() const {
+#ifdef DEBUG
+    NS_ASSERT_OWNINGTHREAD(ErrorResult);
+#endif
+  }
+
   void AssignErrorCode(nsresult aRv) {
     MOZ_ASSERT(aRv != NS_ERROR_TYPE_ERR, "Use ThrowTypeError()");
     MOZ_ASSERT(aRv != NS_ERROR_RANGE_ERR, "Use ThrowRangeError()");
@@ -404,6 +412,9 @@ private:
   
   
   UnionState mUnionState;
+
+  
+  NS_DECL_OWNINGTHREAD;
 #endif
 
   
