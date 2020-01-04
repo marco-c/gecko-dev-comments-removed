91aabe0d3b0f7d9f6e1a42a1e57488f3f6758b76
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 12:03:19 +0000

    Bug 1257919 part 3.  Make the various toString methods on exceptions take an explicit JSContext.  r=khuey
    UltraBlame original commit: 6403c45366e0746c40a594b29eae7e38516bda83

--- dom/base/DOMException.h
+++ dom/base/DOMException.h
@@ -88,7 +88,7 @@ public:
 
   void GetStack(nsAString& aStack, ErrorResult& aRv) const;
 
-  void Stringify(nsString& retval);
+  void Stringify(JSContext* aCx, nsString& retval);
 
   
   Exception();
@@ -131,7 +131,7 @@ public:
   NS_DECL_NSIDOMDOMEXCEPTION
 
   
-  NS_IMETHOD ToString(nsACString& aReturn) override;
+  NS_IMETHOD ToString(JSContext* aCx, nsACString& aReturn) override;
 
   
   virtual JSObject* WrapObject(JSContext* aCx, JS::Handle<JSObject*> aGivenProto)
