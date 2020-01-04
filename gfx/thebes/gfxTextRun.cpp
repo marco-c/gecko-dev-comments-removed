7a6ce99a27ce480d8eaacf6554a06640f182829c
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 16:58:49 +0000

    Bug 1273154 - Avoid breaking a font run at U+202F (NNBSP) if possible, because Mongolian shaping depends on the text run continuing across it. r=jrmuizel
    UltraBlame original commit: b9481ab085785e98b17ea7a3f41f8059bddb5d32

--- gfx/thebes/gfxTextRun.cpp
+++ gfx/thebes/gfxTextRun.cpp
@@ -2640,7 +2640,9 @@ gfxFontGroup::FindFontForChar(uint32_t aCh, uint32_t aPrevCh, uint32_t aNextCh,
     
     
     
-    if (aPrevMatchedFont && IsClusterExtender(aCh) &&
+    const uint32_t NARROW_NO_BREAK_SPACE = 0x202f;
+    if (aPrevMatchedFont &&
+        (IsClusterExtender(aCh) || aCh == NARROW_NO_BREAK_SPACE) &&
         aPrevMatchedFont->HasCharacter(aCh)) {
         RefPtr<gfxFont> ret = aPrevMatchedFont;
         return ret.forget();
