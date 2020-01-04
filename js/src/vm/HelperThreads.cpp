46973c5ca160274204901689370bc53d38e38647
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 14:16:54 +0000

    Bug 1211723 - Share JS source text between JSRuntimes; r=jimb
    This commit adds `SharedImmutableStringsCache` which allows for de-duplication
    and sharing of immutable strings between threads and JSRuntimes.
    
    Each JSRuntime gets a SharedImmutableStringsCache member, but the accessor
    always returns the parent runtime's cache. The caches in child JSRuntime's are
    not wasting space, however, as initialization and allocation of the table
    happens lazily within SharedImmutableStringsCache.
    
    Furthermore, this commit removes `js::ScriptSource::Parent` and the
    `CompressedSourceSet`. They are unnecessary because source text is always shared
    via the parent runtime's `SharedImmutableStringsCache` now.
    
    UltraBlame original commit: 735da799e3bbb98c087339f21599571c48ce484f

--- js/src/vm/HelperThreads.cpp
+++ js/src/vm/HelperThreads.cpp
@@ -16,6 +16,7 @@
 #include "gc/GCInternals.h"
 #include "jit/IonBuilder.h"
 #include "vm/Debugger.h"
+#include "vm/SharedImmutableStringsCache.h"
 #include "vm/Time.h"
 #include "vm/TraceLogging.h"
 
@@ -1191,11 +1192,6 @@ GlobalHelperThreadState::finishParseTask(JSContext* maybecx, JSRuntime* rt, Pars
     
     Debugger::onNewScript(cx, script);
 
-    
-    
-    if (script->scriptSource()->hasCompressedSource())
-        script->scriptSource()->updateCompressedSourceSet(rt);
-
     return script;
 }
 
@@ -1621,18 +1617,25 @@ SourceCompressionTask::complete()
     }
 
     if (result == Success) {
-        ss->setCompressedSource(cx->isJSContext() ? cx->asJSContext()->runtime() : nullptr,
-                                compressed, compressedBytes, compressedHash);
+        mozilla::UniquePtr<char[], JS::FreePolicy> compressedSource(
+            reinterpret_cast<char*>(compressed));
+        compressed = nullptr;
 
-        
-        cx->updateMallocCounter(ss->computedSizeOfData());
+        auto& cache = cx->zone()->runtimeFromAnyThread()->sharedImmutableStrings();
+        auto deduped = cache.getOrCreate(mozilla::Move(compressedSource), compressedBytes);
+        if (!deduped) {
+            ReportOutOfMemory(cx);
+            result = OOM;
+            ss = nullptr;
+            return false;
+        }
+
+        ss->setCompressedSource(mozilla::Move(*deduped), ss->length());
     } else {
         js_free(compressed);
 
         if (result == OOM)
             ReportOutOfMemory(cx);
-        else if (result == Aborted && !ss->ensureOwnsSource(cx))
-            result = OOM;
     }
 
     ss = nullptr;
