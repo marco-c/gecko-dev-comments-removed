679bbc6a92ec9630c4c8f7dab88ba8a0e6bd6384
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 17:02:05 +0000

    Bug 1177943 - Part 2. Add cmd_lookUpDictionary content command. r=masayuki
    To get selected word on contnet process, I create new contnet command to look up dictionary.  Then, call nsIWidget's method to show looking up dictionary.
    
    About fetched length, see https://www.chromium.org/developers/design-documents/system-dictionary-pop-up-architecture for OSX 10.6's design
    
    MozReview-Commit-ID: Ic2r6hboEEw
    
    UltraBlame original commit: 4b63f3496a31c66eaf0ecd359cb3fc07f1cf8a6b

--- widget/EventMessageList.h
+++ widget/EventMessageList.h
@@ -312,6 +312,7 @@ NS_EVENT_MESSAGE(eContentCommandDelete)
 NS_EVENT_MESSAGE(eContentCommandUndo)
 NS_EVENT_MESSAGE(eContentCommandRedo)
 NS_EVENT_MESSAGE(eContentCommandPasteTransferable)
+NS_EVENT_MESSAGE(eContentCommandLookUpDictionary)
 
 
 
