c061d6aca112f0d20841f18b225763420016184e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 22:03:57 +0000

    Bug 1294811: Fix external tests that import head_addons.js. r=rhelmer
    MozReview-Commit-ID: EWo7Gv9oSsz
    
    UltraBlame original commit: e9313a026f954a5a0553f8226adc7fa7b6de22cf

--- browser/experiments/test/xpcshell/test_api.js
+++ browser/experiments/test/xpcshell/test_api.js
@@ -28,7 +28,7 @@ function uninstallExperimentAddons() {
   return Task.spawn(function* () {
     let addons = yield getExperimentAddons();
     for (let a of addons) {
-      yield AddonTestUtils.uninstallAddonByID(a.id);
+      yield AddonManagerTesting.uninstallAddonByID(a.id);
     }
   });
 }
@@ -412,7 +412,7 @@ add_task(function* test_addonAlreadyInstalled() {
 
   
 
-  yield AddonTestUtils.installXPIFromURL(gDataRoot + EXPERIMENT1_XPI_NAME, EXPERIMENT1_XPI_SHA1);
+  yield AddonManagerTesting.installXPIFromURL(gDataRoot + EXPERIMENT1_XPI_NAME, EXPERIMENT1_XPI_SHA1);
   addons = yield getExperimentAddons();
   Assert.equal(addons.length, 1, "1 add-on is installed.");
   list = yield experiments.getExperiments();
@@ -1407,7 +1407,7 @@ add_task(function* test_unexpectedUninstall() {
   
   
 
-  yield AddonTestUtils.uninstallAddonByID(EXPERIMENT1_ID);
+  yield AddonManagerTesting.uninstallAddonByID(EXPERIMENT1_ID);
   yield experiments._mainTask;
 
   yield experiments.notify();
@@ -1433,7 +1433,7 @@ add_task(function* testUnknownExperimentsUninstalled() {
 
   
   experiments._unregisterWithAddonManager();
-  yield AddonTestUtils.installXPIFromURL(gDataRoot + EXPERIMENT1_XPI_NAME, EXPERIMENT1_XPI_SHA1);
+  yield AddonManagerTesting.installXPIFromURL(gDataRoot + EXPERIMENT1_XPI_NAME, EXPERIMENT1_XPI_SHA1);
   experiments._registerWithAddonManager();
 
   addons = yield getExperimentAddons();
@@ -1472,7 +1472,7 @@ add_task(function* testForeignExperimentInstall() {
 
   let failed = false;
   try {
-    yield AddonTestUtils.installXPIFromURL(gDataRoot + EXPERIMENT1_XPI_NAME, EXPERIMENT1_XPI_SHA1);
+    yield AddonManagerTesting.installXPIFromURL(gDataRoot + EXPERIMENT1_XPI_NAME, EXPERIMENT1_XPI_SHA1);
   } catch (ex) {
     failed = true;
   }
@@ -1622,7 +1622,7 @@ add_task(function* test_foreignUninstallAndRestart() {
   Assert.equal(addons.length, 1, "A single experiment add-on is installed.");
   Assert.ok(addons[0].isActive, "That experiment is active.");
 
-  yield AddonTestUtils.uninstallAddonByID(EXPERIMENT1_ID);
+  yield AddonManagerTesting.uninstallAddonByID(EXPERIMENT1_ID);
   yield experiments._mainTask;
 
   addons = yield getExperimentAddons();
