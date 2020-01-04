2b168b7926f0099100eea75a219d4337cb61556a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 03:07:19 +0000

    Bug 1156062 part 1d - Clean up nsEditor::SplitNodeDeep; r=ehsan
    UltraBlame original commit: 74e5c9314b72cf8fbba214377301314c33a6b4a7

--- editor/libeditor/nsEditor.cpp
+++ editor/libeditor/nsEditor.cpp
@@ -3805,94 +3805,56 @@ nsEditor::IsPreformatted(nsIDOMNode *aNode, bool *aResult)
 
 
 
+
+
+
 int32_t
-nsEditor::SplitNodeDeep(nsIContent& aNode, nsIContent& aSplitPointParent,
+nsEditor::SplitNodeDeep(nsIContent& aNode,
+                        nsIContent& aSplitPointParent,
                         int32_t aSplitPointOffset,
                         EmptyContainers aEmptyContainers,
-                        nsIContent** outLeftNode,
-                        nsIContent** outRightNode)
+                        nsIContent** aOutLeftNode,
+                        nsIContent** aOutRightNode)
 {
-  int32_t offset;
-  nsCOMPtr<nsIDOMNode> leftNodeDOM, rightNodeDOM;
-  nsresult res = SplitNodeDeep(aNode.AsDOMNode(),
-      aSplitPointParent.AsDOMNode(), aSplitPointOffset, &offset,
-      aEmptyContainers == EmptyContainers::no, address_of(leftNodeDOM),
-      address_of(rightNodeDOM));
-  NS_ENSURE_SUCCESS(res, -1);
-  if (outLeftNode) {
-    nsCOMPtr<nsIContent> leftNode = do_QueryInterface(leftNodeDOM);
-    MOZ_ASSERT(!leftNodeDOM || leftNode);
-    leftNode.forget(outLeftNode);
-  }
-  if (outRightNode) {
-    nsCOMPtr<nsIContent> rightNode = do_QueryInterface(rightNodeDOM);
-    MOZ_ASSERT(!rightNodeDOM || rightNode);
-    rightNode.forget(outRightNode);
-  }
-  return offset;
-}
-
-nsresult
-nsEditor::SplitNodeDeep(nsIDOMNode *aNode,
-                        nsIDOMNode *aSplitPointParent,
-                        int32_t aSplitPointOffset,
-                        int32_t *outOffset,
-                        bool    aNoEmptyContainers,
-                        nsCOMPtr<nsIDOMNode> *outLeftNode,
-                        nsCOMPtr<nsIDOMNode> *outRightNode)
-{
-  nsCOMPtr<nsINode> node = do_QueryInterface(aNode);
-  NS_ENSURE_TRUE(node && aSplitPointParent && outOffset, NS_ERROR_NULL_POINTER);
+  MOZ_ASSERT(&aSplitPointParent == &aNode ||
+             nsEditorUtils::IsDescendantOf(&aSplitPointParent, &aNode));
   int32_t offset = aSplitPointOffset;
 
-  if (outLeftNode)  *outLeftNode  = nullptr;
-  if (outRightNode) *outRightNode = nullptr;
-
-  nsCOMPtr<nsINode> nodeToSplit = do_QueryInterface(aSplitPointParent);
-  while (nodeToSplit) {
-    
+  nsCOMPtr<nsIContent> leftNode, rightNode;
+  OwningNonNull<nsIContent> nodeToSplit = aSplitPointParent;
+  while (true) {
     
     
     
     
 
-    nsCOMPtr<nsIDOMCharacterData> nodeAsText = do_QueryInterface(nodeToSplit);
-    uint32_t len = nodeToSplit->Length();
-    bool bDoSplit = false;
+    bool didSplit = false;
 
-    if (!(aNoEmptyContainers || nodeAsText) || (offset && (offset != (int32_t)len)))
-    {
-      bDoSplit = true;
-      nsCOMPtr<nsIDOMNode> tempNode;
-      nsresult rv = SplitNode(nodeToSplit->AsDOMNode(), offset,
-                              getter_AddRefs(tempNode));
-      NS_ENSURE_SUCCESS(rv, rv);
+    if ((aEmptyContainers == EmptyContainers::yes &&
+         !nodeToSplit->GetAsText()) ||
+        (offset && offset != (int32_t)nodeToSplit->Length())) {
+      didSplit = true;
+      ErrorResult rv;
+      nsCOMPtr<nsIContent> newLeftNode = SplitNode(nodeToSplit, offset, rv);
+      NS_ENSURE_TRUE(!rv.Failed(), -1);
 
-      if (outRightNode) {
-        *outRightNode = nodeToSplit->AsDOMNode();
-      }
-      if (outLeftNode) {
-        *outLeftNode = tempNode;
-      }
+      rightNode = nodeToSplit;
+      leftNode = newLeftNode;
     }
 
-    nsINode* parentNode = nodeToSplit->GetParentNode();
-    NS_ENSURE_TRUE(parentNode, NS_ERROR_FAILURE);
+    NS_ENSURE_TRUE(nodeToSplit->GetParent(), -1);
+    OwningNonNull<nsIContent> parentNode = *nodeToSplit->GetParent();
 
-    if (!bDoSplit && offset) {
+    if (!didSplit && offset) {
       
       offset = parentNode->IndexOf(nodeToSplit) + 1;
-      if (outLeftNode) {
-        *outLeftNode = nodeToSplit->AsDOMNode();
-      }
+      leftNode = nodeToSplit;
     } else {
       offset = parentNode->IndexOf(nodeToSplit);
-      if (outRightNode) {
-        *outRightNode = nodeToSplit->AsDOMNode();
-      }
+      rightNode = nodeToSplit;
     }
 
-    if (nodeToSplit == node) {
+    if (nodeToSplit == &aNode) {
       
       break;
     }
@@ -3900,13 +3862,14 @@ nsEditor::SplitNodeDeep(nsIDOMNode *aNode,
     nodeToSplit = parentNode;
   }
 
-  if (!nodeToSplit) {
-    NS_NOTREACHED("null node obtained in nsEditor::SplitNodeDeep()");
-    return NS_ERROR_FAILURE;
+  if (aOutLeftNode) {
+    leftNode.forget(aOutLeftNode);
+  }
+  if (aOutRightNode) {
+    rightNode.forget(aOutRightNode);
   }
 
-  *outOffset = offset;
-  return NS_OK;
+  return offset;
 }
 
 
