29e708c04e7885172ff041051b13faee635a1cc6
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 03:26:55 +0000

    Bug 1204983 - Allow about: pages to load remote content (r=bholley,bz,florian,dragana)
    UltraBlame original commit: c7d3635f771a20df89e3c33a4e63fd1b282129a1

--- docshell/base/nsAboutRedirector.cpp
+++ docshell/base/nsAboutRedirector.cpp
@@ -9,6 +9,7 @@
 #include "nsAboutProtocolUtils.h"
 #include "mozilla/ArrayUtils.h"
 #include "nsDOMString.h"
+#include "nsIProtocolHandler.h"
 
 NS_IMPL_ISUPPORTS(nsAboutRedirector, nsIAboutModule)
 
@@ -48,7 +49,7 @@ static RedirEntry kRedirMap[] = {
   { "crashes", "chrome://global/content/crashes.xhtml", 0 },
 #endif
   {
-    "credits", "http://www.mozilla.org/credits/",
+    "credits", "https://www.mozilla.org/credits/",
     nsIAboutModule::URI_SAFE_FOR_UNTRUSTED_CONTENT
   },
   {
@@ -143,12 +144,27 @@ nsAboutRedirector::NewChannel(nsIURI* aURI,
       nsCOMPtr<nsIURI> tempURI;
       rv = NS_NewURI(getter_AddRefs(tempURI), kRedirMap[i].url);
       NS_ENSURE_SUCCESS(rv, rv);
+
+      
+      
+      
+      
+      bool isUIResource = false;
+      rv = NS_URIChainHasFlags(tempURI, nsIProtocolHandler::URI_IS_UI_RESOURCE,
+                               &isUIResource);
+      NS_ENSURE_SUCCESS(rv, rv);
+
+      nsLoadFlags loadFlags =
+        isUIResource ? static_cast<nsLoadFlags>(nsIChannel::LOAD_NORMAL)
+                     : static_cast<nsLoadFlags>(nsIChannel::LOAD_REPLACE);
+
       rv = NS_NewChannelInternal(getter_AddRefs(tempChannel),
                                  tempURI,
-                                 aLoadInfo);
-      if (NS_FAILED(rv)) {
-        return rv;
-      }
+                                 aLoadInfo,
+                                 nullptr, 
+                                 nullptr, 
+                                 loadFlags);
+      NS_ENSURE_SUCCESS(rv, rv);
 
       tempChannel->SetOriginalURI(aURI);
 
