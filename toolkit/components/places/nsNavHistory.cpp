96da5753fc039e17837f8eac11c5b9c980959fa8
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 18:08:43 +0000

    Bug 1277235 - add typed and visitCount to onVisit. r=adw
    MozReview-Commit-ID: 3XfBCiOgyAu
    
    UltraBlame original commit: e69b8135f9e224e049c140dc6fad7e8aa9b18e1f

--- toolkit/components/places/nsNavHistory.cpp
+++ toolkit/components/places/nsNavHistory.cpp
@@ -485,14 +485,16 @@ nsNavHistory::LoadPrefs()
 
 void
 nsNavHistory::NotifyOnVisit(nsIURI* aURI,
-                          int64_t aVisitID,
+                          int64_t aVisitId,
                           PRTime aTime,
-                          int64_t referringVisitID,
+                          int64_t aReferrerVisitId,
                           int32_t aTransitionType,
-                          const nsACString& aGUID,
-                          bool aHidden)
+                          const nsACString& aGuid,
+                          bool aHidden,
+                          uint32_t aVisitCount,
+                          uint32_t aTyped)
 {
-  MOZ_ASSERT(!aGUID.IsEmpty());
+  MOZ_ASSERT(!aGuid.IsEmpty());
   
   
   
@@ -505,8 +507,8 @@ nsNavHistory::NotifyOnVisit(nsIURI* aURI,
 
   NOTIFY_OBSERVERS(mCanNotify, mCacheObservers, mObservers,
                    nsINavHistoryObserver,
-                   OnVisit(aURI, aVisitID, aTime, 0,
-                           referringVisitID, aTransitionType, aGUID, aHidden));
+                   OnVisit(aURI, aVisitId, aTime, 0, aReferrerVisitId,
+                           aTransitionType, aGuid, aHidden, aVisitCount, aTyped));
 }
 
 void
