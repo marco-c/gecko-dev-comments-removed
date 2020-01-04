848e71ad860d681ee2734f228664b78019c91123
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 14:14:51 +0000

    Backed out changeset 7db6a99ec546 (bug 887016)
    UltraBlame original commit: d44ccce05064d63cd96efd511694bc1a28d94646

--- js/src/jit/IonAnalysis.cpp
+++ js/src/jit/IonAnalysis.cpp
@@ -1848,7 +1848,7 @@ jit::MakeMRegExpHoistable(MIRGraph& graph)
 
                 
                 MDefinition* use = i->consumer()->toDefinition();
-                if (use->isRegExpMatcher() || use->isRegExpTester() || use->isRegExpSearcher())
+                if (use->isRegExpMatcher() || use->isRegExpTester())
                     continue;
 
                 hoistable = false;
