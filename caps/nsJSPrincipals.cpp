9252af1030e02c934e38e7d977ca68bd43e0adaf
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 11:23:18 +0000

    Bug 1254906 - Change the annotation on JSPrincipals::dump's definition to match that of its declaration. r=bz
    The current discrepancy works because gecko and js don't actually agree
    on the meaning of JS_EXPORT_API and JS_PUBLIC_API, but moving the
    configure flags that incluences their meaning is going to make them
    agree, and that adds a fatal warning when building nsJSPrincipals.cpp
    because of the discrepancy.
    
    UltraBlame original commit: 488a4c2651474e9a7f2a821b1afcb065f731996d

--- caps/nsJSPrincipals.cpp
+++ caps/nsJSPrincipals.cpp
@@ -83,7 +83,7 @@ nsJSPrincipals::Destroy(JSPrincipals *jsprin)
 #ifdef DEBUG
 
 
-JS_EXPORT_API(void)
+JS_PUBLIC_API(void)
 JSPrincipals::dump()
 {
     if (debugToken == nsJSPrincipals::DEBUG_TOKEN) {
