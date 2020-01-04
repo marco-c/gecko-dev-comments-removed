844dc1c929644b9ea5485bb2b8ccbe6bda771a07
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 22:02:39 +0000

    Bug 1296333: Ensure better handling of nullptr and IUnknown in ipc::mscom; r=jimm
    MozReview-Commit-ID: H1PvH3PIWdK
    
    UltraBlame original commit: b6174c5855f49d9ea91c588165f4728363ff5866

--- ipc/mscom/Interceptor.cpp
+++ ipc/mscom/Interceptor.cpp
@@ -8,7 +8,6 @@
 #include "mozilla/mscom/Interceptor.h"
 #include "mozilla/mscom/InterceptorLog.h"
 
-#include "mozilla/mscom/DispatchForwarder.h"
 #include "mozilla/mscom/MainThreadInvoker.h"
 #include "mozilla/mscom/Registration.h"
 #include "mozilla/mscom/utils.h"
@@ -26,6 +25,7 @@ namespace mscom {
 Interceptor::Create(STAUniquePtr<IUnknown>& aTarget, IInterceptorSink* aSink,
                     REFIID aIid, void** aOutput)
 {
+  MOZ_ASSERT(aOutput && aTarget && aSink);
   if (!aOutput) {
     return E_INVALIDARG;
   }
@@ -142,6 +142,13 @@ Interceptor::GetInterceptorForIID(REFIID aIid, void** aOutInterceptor)
 {
   if (!aOutInterceptor) {
     return E_INVALIDARG;
+  }
+
+  if (aIid == IID_IUnknown) {
+    
+    *aOutInterceptor = static_cast<IInterceptor*>(this);
+    AddRef();
+    return S_OK;
   }
 
   RefPtr<IUnknown> unkInterceptor;
@@ -273,17 +280,6 @@ Interceptor::ThreadSafeQueryInterface(REFIID aIid, IUnknown** aOutInterface)
     *aOutInterface = static_cast<IInterceptor*>(this);
     (*aOutInterface)->AddRef();
     return S_OK;
-  }
-
-  if (aIid == IID_IDispatch) {
-    STAUniquePtr<IDispatch> disp;
-    IDispatch* rawDisp = nullptr;
-    HRESULT hr = QueryInterfaceTarget(aIid, (void**)&rawDisp);
-    if (FAILED(hr)) {
-      return hr;
-    }
-    disp.reset(rawDisp);
-    return DispatchForwarder::Create(this, disp, aOutInterface);
   }
 
   return GetInterceptorForIID(aIid, (void**)aOutInterface);
