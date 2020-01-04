ac538292b4715b4f6b69683b6a30f75335fb816d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 17:18:03 +0000

    Backed out changeset 75bb440d7525 (bug 1275637) for browser_se_first-run.js failures.
    CLOSED TREE
    
    UltraBlame original commit: 34ac60e5b29c55eba69ab7e769dbc3b2eb7d5321

--- devtools/client/shared/webgl-utils.js
+++ devtools/client/shared/webgl-utils.js
@@ -33,7 +33,7 @@ function create3DContext(canvas) {
   
   let context = null;
   try {
-    context = canvas.getContext(WEBGL_CONTEXT_NAME);
+    context = canvas.getContext(WEBGL_CONTEXT_NAME, aFlags);
   } catch (e) {
     return null;
   }
