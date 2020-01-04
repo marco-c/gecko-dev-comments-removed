d1bf16a8263aace95e0aed2e17eb2ac3e48bcbf3
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 23:26:44 +0000

    bug 1301288 - set post update values earlier in startup so Firefox post update actions can be performed. r=mhowell
    UltraBlame original commit: c518337a8da24d43364eda6ed741575c71a2655b

--- toolkit/mozapps/update/nsUpdateService.js
+++ toolkit/mozapps/update/nsUpdateService.js
@@ -1864,6 +1864,17 @@ UpdateService.prototype = {
   observe: function AUS_observe(subject, topic, data) {
     switch (topic) {
       case "post-update-processing":
+        if (readStatusFile(getUpdatesDir()) == STATE_SUCCEEDED) {
+          
+          
+          
+          let um = Cc["@mozilla.org/updates/update-manager;1"].
+                   getService(Ci.nsIUpdateManager);
+          um.activeUpdate.state = STATE_SUCCEEDED;
+          um.saveUpdates();
+          Services.prefs.setBoolPref(PREF_APP_UPDATE_POSTUPDATE, true);
+        }
+
         if (Services.appinfo.ID in APPID_TO_TOPIC) {
           
           
@@ -2076,7 +2087,6 @@ UpdateService.prototype = {
 
       
       um.activeUpdate = update;
-      Services.prefs.setBoolPref(PREF_APP_UPDATE_POSTUPDATE, true);
 
       
       cleanupActiveUpdate();
