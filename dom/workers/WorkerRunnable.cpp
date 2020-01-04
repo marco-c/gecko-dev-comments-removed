295f2b25f826d2547295fb174412a742111fd4d2
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 23:12:58 +0000

    Bug 1300655 - WorkerProxyToMainThreadRunnable::PostDispatchOnMainThread::ReleaseRunnable should implement Cancel() correctly, r=bkelly
    UltraBlame original commit: 7f00c0316a194286cc663f0bdfa1362c270de074

--- dom/workers/WorkerRunnable.cpp
+++ dom/workers/WorkerRunnable.cpp
@@ -1,8 +1,8 @@
-/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
-/* vim: set ts=8 sts=2 et sw=2 tw=80: */
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+
+
+
+
 
 #include "WorkerRunnable.h"
 
@@ -31,7 +31,7 @@ const nsIID kWorkerRunnableIID = {
   0x320cc0b5, 0xef12, 0x4084, { 0x88, 0x6e, 0xca, 0x6a, 0x81, 0xe4, 0x1d, 0x68 }
 };
 
-} // namespace
+} 
 
 #ifdef DEBUG
 WorkerRunnable::WorkerRunnable(WorkerPrivate* aWorkerPrivate,
@@ -193,7 +193,7 @@ WorkerRunnable::PostRun(JSContext* aCx, WorkerPrivate* aWorkerPrivate,
   }
 }
 
-// static
+
 WorkerRunnable*
 WorkerRunnable::FromRunnable(nsIRunnable* aRunnable)
 {
@@ -217,7 +217,7 @@ NS_INTERFACE_MAP_BEGIN(WorkerRunnable)
   NS_INTERFACE_MAP_ENTRY(nsIRunnable)
   NS_INTERFACE_MAP_ENTRY(nsICancelableRunnable)
   NS_INTERFACE_MAP_ENTRY_AMBIGUOUS(nsISupports, nsIRunnable)
-  // kWorkerRunnableIID is special in that it does not AddRef its result.
+  
   if (aIID.Equals(kWorkerRunnableIID)) {
     *aInstancePtr = this;
     return NS_OK;
@@ -250,7 +250,7 @@ WorkerRunnable::Run()
       mWorkerPrivate->AllPendingRunnablesShouldBeCanceled() &&
       !IsCanceled() && !mCallingCancelWithinRun) {
 
-    // Prevent recursion.
+    
     mCallingCancelWithinRun = true;
 
     Cancel();
@@ -274,13 +274,13 @@ WorkerRunnable::Run()
                "ScriptExecutorRunnable");
     mWorkerPrivate->AssertIsOnWorkerThread();
     MOZ_ASSERT(!JS_IsExceptionPending(mWorkerPrivate->GetJSContext()));
-    // We can't enter a useful compartment on the JSContext here; just pass it
-    // in as-is.
+    
+    
     PostRun(mWorkerPrivate->GetJSContext(), mWorkerPrivate, false);
     return NS_ERROR_FAILURE;
   }
 
-  // Track down the appropriate global, if any, to use for the AutoEntryScript.
+  
   nsCOMPtr<nsIGlobalObject> globalObject;
   bool isMainThread = !targetIsWorkerThread && !mWorkerPrivate->GetParent();
   MOZ_ASSERT(isMainThread == NS_IsMainThread());
@@ -298,11 +298,11 @@ WorkerRunnable::Run()
       globalObject = DefaultGlobalObject();
     }
 
-    // We may still not have a globalObject here: in the case of
-    // CompileScriptRunnable, we don't actually create the global object until
-    // we have the script data, which happens in a syncloop under
-    // CompileScriptRunnable::WorkerRun, so we can't assert that it got created
-    // in the PreRun call above.
+    
+    
+    
+    
+    
   } else {
     kungFuDeathGrip = mWorkerPrivate;
     if (isMainThread) {
@@ -312,11 +312,11 @@ WorkerRunnable::Run()
     }
   }
 
-  // We might run script as part of WorkerRun, so we need an AutoEntryScript.
-  // This is part of the HTML spec for workers at:
-  // http://www.whatwg.org/specs/web-apps/current-work/#run-a-worker
-  // If we don't have a globalObject we have to use an AutoJSAPI instead, but
-  // this is OK as we won't be running script in these circumstances.
+  
+  
+  
+  
+  
   Maybe<mozilla::dom::AutoJSAPI> maybeJSAPI;
   Maybe<mozilla::dom::AutoEntryScript> aes;
   JSContext* cx;
@@ -332,28 +332,28 @@ WorkerRunnable::Run()
     cx = jsapi->cx();
   }
 
-  // Note that we can't assert anything about mWorkerPrivate->GetWrapper()
-  // existing, since it may in fact have been GCed (and we may be one of the
-  // runnables cleaning up the worker as a result).
+  
+  
+  
 
-  // If we are on the parent thread and that thread is not the main thread,
-  // then we must be a dedicated worker (because there are no
-  // Shared/ServiceWorkers whose parent is itself a worker) and then we
-  // definitely have a globalObject.  If it _is_ the main thread, globalObject
-  // can be null for workers started from JSMs or other non-window contexts,
-  // sadly.
+  
+  
+  
+  
+  
+  
   MOZ_ASSERT_IF(!targetIsWorkerThread && !isMainThread,
                 mWorkerPrivate->IsDedicatedWorker() && globalObject);
 
-  // If we're on the parent thread we might be in a null compartment in the
-  // situation described above when globalObject is null.  Make sure to enter
-  // the compartment of the worker's reflector if there is one.  There might
-  // not be one if we're just starting to compile the script for this worker.
+  
+  
+  
+  
   Maybe<JSAutoCompartment> ac;
   if (!targetIsWorkerThread && mWorkerPrivate->GetWrapper()) {
-    // If we're on the parent thread and have a reflector and a globalObject,
-    // then the compartments of cx, globalObject, and the worker's reflector
-    // should all match.
+    
+    
+    
     MOZ_ASSERT_IF(globalObject,
                   js::GetObjectCompartment(mWorkerPrivate->GetWrapper()) ==
                     js::GetContextCompartment(cx));
@@ -361,9 +361,9 @@ WorkerRunnable::Run()
                   js::GetObjectCompartment(mWorkerPrivate->GetWrapper()) ==
                     js::GetObjectCompartment(globalObject->GetGlobalJSObject()));
 
-    // If we're on the parent thread and have a reflector, then our
-    // JSContext had better be either in the null compartment (and hence
-    // have no globalObject) or in the compartment of our reflector.
+    
+    
+    
     MOZ_ASSERT(!js::GetContextCompartment(cx) ||
                js::GetObjectCompartment(mWorkerPrivate->GetWrapper()) ==
                  js::GetContextCompartment(cx),
@@ -378,27 +378,27 @@ WorkerRunnable::Run()
   MOZ_ASSERT_IF(result, !jsapi->HasException());
   jsapi->ReportException();
 
-  // We can't even assert that this didn't create our global, since in the case
-  // of CompileScriptRunnable it _does_.
+  
+  
 
-  // It would be nice to avoid passing a JSContext to PostRun, but in the case
-  // of ScriptExecutorRunnable we need to know the current compartment on the
-  // JSContext (the one we set up based on the global returned from PreRun) so
-  // that we can sanely do exception reporting.  In particular, we want to make
-  // sure that we do our JS_SetPendingException while still in that compartment,
-  // because otherwise we might end up trying to create a cross-compartment
-  // wrapper when we try to move the JS exception from our runnable's
-  // ErrorResult to the JSContext, and that's not desirable in this case.
-  //
-  // We _could_ skip passing a JSContext here and then in
-  // ScriptExecutorRunnable::PostRun end up grabbing it from the WorkerPrivate
-  // and looking at its current compartment.  But that seems like slightly weird
-  // action-at-a-distance...
-  //
-  // In any case, we do NOT try to change the compartment on the JSContext at
-  // this point; in the one case in which we could do that
-  // (CompileScriptRunnable) it actually doesn't matter which compartment we're
-  // in for PostRun.
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
   PostRun(cx, mWorkerPrivate, result);
   MOZ_ASSERT(!jsapi->HasException());
 
@@ -412,8 +412,8 @@ WorkerRunnable::Cancel()
 
   MOZ_ASSERT(canceledCount, "Cancel() overflow!");
 
-  // The docs say that Cancel() should not be called more than once and that we
-  // should throw NS_ERROR_UNEXPECTED if it is.
+  
+  
   return (canceledCount == 1) ? NS_OK : NS_ERROR_UNEXPECTED;
 }
 
@@ -594,11 +594,11 @@ WorkerMainThreadRunnable::Dispatch(ErrorResult& aRv)
     aRv.ThrowUncatchableException();
   }
 
-  // Telemetry is apparently not threadsafe
-  // Telemetry::Accumulate(Telemetry::SYNC_WORKER_OPERATION, mTelemetryKey,
-  //                       static_cast<uint32_t>((TimeStamp::NowLoRes() - startTime)
-  //                                               .ToMilliseconds()));
-  Unused << startTime; // Shut the compiler up.
+  
+  
+  
+  
+  Unused << startTime; 
 }
 
 NS_IMETHODIMP
@@ -639,9 +639,9 @@ WorkerCheckAPIExposureOnMainThreadRunnable::Dispatch()
 bool
 WorkerSameThreadRunnable::PreDispatch(WorkerPrivate* aWorkerPrivate)
 {
-  // We don't call WorkerRunnable::PreDispatch, because we're using
-  // WorkerThreadModifyBusyCount for mBehavior, and WorkerRunnable will assert
-  // that PreDispatch is on the parent thread in that case.
+  
+  
+  
   aWorkerPrivate->AssertIsOnWorkerThread();
   return true;
 }
@@ -650,14 +650,14 @@ void
 WorkerSameThreadRunnable::PostDispatch(WorkerPrivate* aWorkerPrivate,
                                        bool aDispatchResult)
 {
-  // We don't call WorkerRunnable::PostDispatch, because we're using
-  // WorkerThreadModifyBusyCount for mBehavior, and WorkerRunnable will assert
-  // that PostDispatch is on the parent thread in that case.
+  
+  
+  
   aWorkerPrivate->AssertIsOnWorkerThread();
   if (aDispatchResult) {
     DebugOnly<bool> willIncrement = aWorkerPrivate->ModifyBusyCountFromWorker(true);
-    // Should never fail since if this thread is still running, so should the
-    // parent and it should be able to process a control runnable.
+    
+    
     MOZ_ASSERT(willIncrement);
   }
 }
@@ -716,12 +716,12 @@ WorkerProxyToMainThreadRunnable::PostDispatchOnMainThread()
       MOZ_ASSERT(aRunnable);
     }
 
-    // We must call RunBackOnWorkerThread() also if the runnable is cancelled.
+    
     nsresult
     Cancel() override
     {
       WorkerRun(nullptr, mWorkerPrivate);
-      return NS_OK;
+      return MainThreadWorkerControlRunnable::Cancel();
     }
 
     virtual bool
@@ -732,7 +732,7 @@ WorkerProxyToMainThreadRunnable::PostDispatchOnMainThread()
 
       mRunnable->RunBackOnWorkerThread();
 
-      // Let's release the worker thread.
+      
       mRunnable->ReleaseWorker();
       return true;
     }
@@ -758,8 +758,8 @@ WorkerProxyToMainThreadRunnable::HoldWorker()
   public:
     bool Notify(Status aStatus) override
     {
-      // We don't care about the notification. We just want to keep the
-      // mWorkerPrivate alive.
+      
+      
       return true;
     }
   };
