ae6233151fc853f9ef4f8b801fb574f045ade03e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 06:59:33 +0000

    Bug 1233884 - Remove unused CompactBufferWriters from MozBaseAssembler. r=jolesen
    UltraBlame original commit: fc153c692c77ed8a5a86eecc2c9c321c73c54c09

--- js/src/jit/arm64/vixl/MozBaseAssembler-vixl.h
+++ js/src/jit/arm64/vixl/MozBaseAssembler-vixl.h
@@ -206,11 +206,6 @@ class MozBaseAssembler : public js::jit::AssemblerShared {
  protected:
   
   ARMBuffer armbuffer_;
-
-  js::jit::CompactBufferWriter jumpRelocations_;
-  js::jit::CompactBufferWriter dataRelocations_;
-  js::jit::CompactBufferWriter relocations_;
-  js::jit::CompactBufferWriter preBarriers_;
 };
 
 
