2d155e50757a925e1fe3e2533a7cdba98ed62c7f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 19:33:28 +0000

    Bug 1241932 - Expose decoded CSS grid line properties via a Chrome API. r=mats
    UltraBlame original commit: bb4686084769fedfc0562a62c92343f591401a99

--- dom/base/Element.cpp
+++ dom/base/Element.cpp
@@ -3292,15 +3292,16 @@ Element::MozRequestPointerLock()
 void
 Element::GetGridFragments(nsTArray<RefPtr<Grid>>& aResult)
 {
-  nsIFrame* frame = GetPrimaryFrame();
-  if (frame && (frame->GetType() == nsGkAtoms::gridContainerFrame)) {
-    
-    
-    for (; frame != nullptr; frame = frame->GetNextInFlow()) {
-      aResult.AppendElement(
-        new Grid(this, static_cast<nsGridContainerFrame*>(frame))
-      );
-    }
+  nsGridContainerFrame* frame =
+    nsGridContainerFrame::GetGridFrameWithComputedInfo(GetPrimaryFrame());
+
+  
+  
+  while (frame) {
+    aResult.AppendElement(
+      new Grid(this, frame)
+    );
+    frame = static_cast<nsGridContainerFrame*>(frame->GetNextInFlow());
   }
 }
 
