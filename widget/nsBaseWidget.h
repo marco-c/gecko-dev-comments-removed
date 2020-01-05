a00eeabc333946f3d2ad0f26d00f30d97071fdd0
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 08:01:41 +0000

    Bug 1328066 - Don't broadcast the live-resize events to all browser windows unnecessarily. r=mstange
    The machinery for suppressing the displayport during live resizes
    was using the Observer service. However, in the case of multiple
    browser windows, this meant that all the open browser windows would
    have their displayport suppressed if *any* of the browser windows
    was being resized. This was mostly ok, as the displayport suppression
    would be turned off once the resize ended. However, the code to
    kick off a repaint with the unsuppressed displayport would only get
    triggered on one of the windows (whichever happened to process the
    unsuppress message last).
    
    This patch stops using the Observer service for the implementation
    machinery, and instead locates the active TabParent of the relevant
    nsWindow, and invokes the displayport suppression directly on that.
    This fixes the repainting bug and also avoids unnecessarily
    broadcasting the suppression/unsuppression notification to windows
    that don't neccessarily need it.
    
    MozReview-Commit-ID: LBHOgOW9KUp
    
    UltraBlame original commit: 37a01852d4205709e9a9bb079a41f429f360ef03

--- widget/nsBaseWidget.h
+++ widget/nsBaseWidget.h
@@ -37,6 +37,8 @@ class gfxContext;
 
 namespace mozilla {
 class CompositorVsyncDispatcher;
+class LiveResizeListener;
+
 #ifdef ACCESSIBILITY
 namespace a11y {
 class Accessible;
@@ -398,6 +400,13 @@ public:
   uint64_t CreateScrollCaptureContainer() override;
 #endif
 
+  
+  
+  
+  
+  void NotifyLiveResizeStarted();
+  void NotifyLiveResizeStopped();
+
 protected:
   
   
@@ -708,6 +717,11 @@ protected:
   };
 
   mozilla::Maybe<InitialZoomConstraints> mInitialZoomConstraints;
+
+  
+  
+  
+  nsTArray<RefPtr<mozilla::LiveResizeListener>> mLiveResizeListeners;
 
 #ifdef DEBUG
 protected:
