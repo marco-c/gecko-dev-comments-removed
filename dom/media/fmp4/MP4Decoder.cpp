90b9cfdb26eca090d38f0cd9d6b5ebe95ef94faf
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 03:04:23 +0000

    Bug 1315567: P3. Expose flac codec as being available in mp4. r=gerald
    MozReview-Commit-ID: 7C6W1KGQ1jE
    
    UltraBlame original commit: bfaa975490cfbe35626cb9a3b783844266d97769

--- dom/media/fmp4/MP4Decoder.cpp
+++ dom/media/fmp4/MP4Decoder.cpp
@@ -141,6 +141,12 @@ MP4Decoder::CanHandleMediaType(const MediaContentType& aType,
             NS_LITERAL_CSTRING("audio/opus"), aType));
         continue;
       }
+      if (codec.EqualsLiteral("flac")) {
+        trackInfos.AppendElement(
+          CreateTrackInfoWithMIMETypeAndContentTypeExtraParameters(
+            NS_LITERAL_CSTRING("audio/flac"), aType));
+        continue;
+      }
       
       
       if (IsWhitelistedH264Codec(codec) && isMP4Video) {
