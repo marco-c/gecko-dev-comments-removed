0f20976f64b99b505e20c2e9da7b87c90ce8fccf
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 02:00:02 +0000

    Bug 1307908 - Implement custom styles in new console frontend. r=linclark,tromey;
    Add the possibility to pass a style object to StringRep.
    Parse the styles strings passed as argument using a dummy node element
    to strip off forbidden properties and forbidden values.
    Add a mocha test to ensure custom styles are rendered as expected.
    
    MozReview-Commit-ID: I3LgeNRujaL
    
    UltraBlame original commit: 76bf1cbfe6ab99111a988f0302122b205ce198e1

--- devtools/client/webconsole/new-console-output/components/grip-message-body.js
+++ devtools/client/webconsole/new-console-output/components/grip-message-body.js
@@ -31,10 +31,19 @@ GripMessageBody.propTypes = {
     PropTypes.number,
     PropTypes.object,
   ]).isRequired,
+  serviceContainer: PropTypes.shape({
+    createElement: PropTypes.func.isRequired,
+  }),
+  userProvidedStyle: PropTypes.string,
 };
 
 function GripMessageBody(props) {
-  const { grip } = props;
+  const { grip, userProvidedStyle, serviceContainer } = props;
+
+  let styleObject;
+  if (userProvidedStyle && userProvidedStyle !== "") {
+    styleObject = cleanupStyle(userProvidedStyle, serviceContainer.createElement);
+  }
 
   return (
     
@@ -43,6 +52,7 @@ function GripMessageBody(props) {
         object: grip,
         useQuotes: false,
         mode: props.mode,
+        style: styleObject
       })
       : Rep({
         object: grip,
@@ -51,6 +61,42 @@ function GripMessageBody(props) {
         mode: props.mode,
       })
   );
+}
+
+function cleanupStyle(userProvidedStyle, createElement) {
+  
+  const allowedStylesRegex = new RegExp(
+    "^(?:-moz-)?(?:background|border|box|clear|color|cursor|display|float|font|line|" +
+    "margin|padding|text|transition|outline|white-space|word|writing|" +
+    "(?:min-|max-)?width|(?:min-|max-)?height)"
+  );
+
+  
+  const forbiddenValuesRegexs = [
+    
+    /\b(?:url|(?:-moz-)?element)[\s('"]+/gi,
+
+    
+    /['"(]*(?:chrome|resource|about|app|data|https?|ftp|file):+\/*/gi,
+  ];
+
+  
+  let dummy = createElement("div");
+  dummy.style = userProvidedStyle;
+
+  
+  
+  
+  return [...dummy.style]
+    .filter(name => {
+      return allowedStylesRegex.test(name)
+        && !forbiddenValuesRegexs.some(regex => regex.test(dummy.style[name]));
+    })
+    .reduce((object, name) => {
+      return Object.assign({
+        [name]: dummy.style[name]
+      }, object);
+    }, {});
 }
 
 module.exports = GripMessageBody;
