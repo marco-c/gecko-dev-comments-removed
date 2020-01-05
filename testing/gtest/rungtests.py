f7f0aa083185c4077b1844b5803f5cc612d7212a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 19:19:34 +0000

    Backed out 7 changesets (bug 1333003) for windows asan failures a=backout
    Backed out changeset 3d2b2eeda8d3 (bug 1333003)
    Backed out changeset 400d409ba4ca (bug 1333003)
    Backed out changeset 1ba027abdfc9 (bug 1333003)
    Backed out changeset 70114135bd8c (bug 1333003)
    Backed out changeset 5715b15e33c0 (bug 1333003)
    Backed out changeset 375e952bd738 (bug 1333003)
    Backed out changeset d5d4112599f2 (bug 1333003)
    
    MozReview-Commit-ID: DZUHJTdjX7V
    
    UltraBlame original commit: 4e945c008ca21f3eeaeeff0a269ebe65026db57a

--- testing/gtest/rungtests.py
+++ testing/gtest/rungtests.py
@@ -128,9 +128,7 @@ class GTests(object):
         
         if mozinfo.info["asan"]:
             
-            llvmsym = os.path.join(
-                self.xre_path,
-                "llvm-symbolizer" + mozinfo.info["bin_suffix"].encode('ascii'))
+            llvmsym = os.path.join(self.xre_path, "llvm-symbolizer")
             if os.path.isfile(llvmsym):
                 env["ASAN_SYMBOLIZER_PATH"] = llvmsym
                 log.info("gtest | ASan using symbolizer at %s", llvmsym)
--- testing/xpcshell/runxpcshelltests.py
+++ testing/xpcshell/runxpcshelltests.py
@@ -942,9 +942,7 @@ class XPCShellTests(object):
         usingTSan = "tsan" in self.mozInfo and self.mozInfo["tsan"]
         if usingASan or usingTSan:
             
-            llvmsym = os.path.join(
-                self.xrePath,
-                "llvm-symbolizer" + self.mozInfo["bin_suffix"].encode('ascii'))
+            llvmsym = os.path.join(self.xrePath, "llvm-symbolizer")
             if os.path.isfile(llvmsym):
                 if usingASan:
                     self.env["ASAN_SYMBOLIZER_PATH"] = llvmsym
