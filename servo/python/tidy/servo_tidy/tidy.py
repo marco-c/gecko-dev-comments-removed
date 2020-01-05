db687c5ca4401ed84d04f6ea09871fc247ca6f65
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 12:05:39 +0000

    servo: Merge #14351 - An in-memory RNG that shares its file descriptor (from asajeffrey:servo-rand-share-fds); r=emilio
    <!-- Please describe your changes on the following line: -->
    
    This PR implements an in-memory random number generator that only uses an OS RNG for (re)seeding. The OS RNG is shared, so there's only one file descriptor for `/dev/urandom` being used.
    
    The PR also implements a tidy check that we don't accidentally introduce an RNG. Rather annoyingly, there are a lot of transitive dependencies on `rand`, notably hash maps in `std`.
    
    This PR makes it possible to use uuids for identifiers such as pipeline and frame ids.
    
    ---
    <!-- Thank you for contributing to Servo! Please replace each `[ ]` by `[X]` when the step is complete, and replace `__` with appropriate data: -->
    - [X] `./mach build -d` does not report any errors
    - [X] `./mach test-tidy` does not report any errors
    - [X] These changes do not require tests because it's fixing a resource issue
    
    <!-- Pull requests that do not address these steps are welcome, but they will require additional verification as part of the review process. -->
    
    Source-Repo: https://github.com/servo/servo
    Source-Revision: 36ddf763f66b1b971db07649ff5c69b2e9fd5f91
    
    UltraBlame original commit: 742e507590d2fa2f747e66531bffc0136ec94862

--- servo/python/tidy/servo_tidy/tidy.py
+++ servo/python/tidy/servo_tidy/tidy.py
@@ -82,6 +82,27 @@ WEBIDL_STANDARDS = [
 ]
 
 
+
+
+BLOCKED_PACKAGES = {
+    "rand": [
+        "deque",
+        "gaol",
+        "ipc-channel",
+        "num-bigint",
+        "parking_lot_core",
+        "phf_generator",
+        "rayon",
+        "servo_rand",
+        "tempdir",
+        "tempfile",
+        "uuid",
+        "websocket",
+        "ws",
+    ],
+}
+
+
 def is_iter_empty(iterator):
     try:
         obj = iterator.next()
@@ -337,6 +358,20 @@ def check_lock(file_name, contents):
                 if version in dependency and short_source in dependency:
                     message += "\n\t\t" + name
         yield (1, message)
+
+    
+    for package in content.get("package", []):
+        package_name = package.get("name")
+        package_version = package.get("version")
+        for dependency in package.get("dependencies", []):
+            dependency = dependency.split()
+            dependency_name = dependency[0]
+            whitelist = BLOCKED_PACKAGES.get(dependency_name)
+            if whitelist is not None:
+                if package_name not in whitelist:
+                    fmt = "Package {} {} depends on blocked package {}."
+                    message = fmt.format(package_name, package_version, dependency_name)
+                    yield (1, message)
 
 
 def check_toml(file_name, lines):
