79c9803d8e6f10f819c7cb013836e14c4d3ae70b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 20:55:55 +0000

    Add a fixed-width font for the compositor's debug overlay. (bug 1352151 part 9, r=bas)
    UltraBlame original commit: c05000863d49dd34483a779725de39a47deedbe1

--- gfx/layers/composite/LayerManagerComposite.cpp
+++ gfx/layers/composite/LayerManagerComposite.cpp
@@ -638,8 +638,9 @@ LayerManagerComposite::RenderDebugOverlay(const IntRect& aBounds)
       text,
       IntPoint(2, 5),
       Matrix4x4(),
-      30,
-      650);
+      24,
+      600,
+      TextRenderer::FontType::FixedWidth);
 
     if (mUnusedApzTransformWarning) {
       
