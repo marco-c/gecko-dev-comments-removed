7b09b41acecf01d9546833d50e08b20c4f5ba255
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 04:14:39 +0000

    Bug 1319567 fix window id so it is never zero, r=kmag
    MozReview-Commit-ID: 30u1o1BOAvv
    
    UltraBlame original commit: 4010d412d8c52888861355a9de57fb933fd6d9a3

--- browser/components/extensions/ext-utils.js
+++ browser/components/extensions/ext-utils.js
@@ -924,9 +924,6 @@ extensions.on("shutdown", (type, extension) => {
 
 
 global.WindowManager = {
-  _windows: new WeakMap(),
-  _nextId: 0,
-
   
   WINDOW_ID_NONE: -1,
   WINDOW_ID_CURRENT: -2,
@@ -965,12 +962,11 @@ global.WindowManager = {
   },
 
   getId(window) {
-    if (this._windows.has(window)) {
-      return this._windows.get(window);
+    if (!window.QueryInterface) {
+      return null;
     }
-    let id = this._nextId++;
-    this._windows.set(window, id);
-    return id;
+    return window.QueryInterface(Ci.nsIInterfaceRequestor)
+                 .getInterface(Ci.nsIDOMWindowUtils).outerWindowID;
   },
 
   getWindow(id, context) {
