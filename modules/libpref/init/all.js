f4631b88432aa8418eb2f4cd6b78fd9972804300
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 19:17:37 +0000

    Merge m-c to graphics
    MozReview-Commit-ID: Ci50iHfzKbA
    
    UltraBlame original commit: 303e3c18824a61fbefe21803470cf26e7199ba49

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -186,11 +186,7 @@ pref("dom.enable_performance_observer", false);
 #endif
 
 
-#ifdef NIGHTLY_BUILD
 pref("dom.requestIdleCallback.enabled", true);
-#else
-pref("dom.requestIdleCallback.enabled", false);
-#endif
 
 
 pref("dom.gamepad.enabled", true);
@@ -1589,6 +1585,9 @@ pref("network.http.spdy.default-hpack-buffer", 65536);
 
 pref("network.http.altsvc.enabled", true);
 pref("network.http.altsvc.oe", true);
+
+
+pref("security.tls.enable_0rtt_data", true);
 
 pref("network.http.diagnostics", false);
 
@@ -5212,8 +5211,11 @@ pref("browser.safebrowsing.provider.google.reportMalwareMistakeURL", "https://%L
 pref("browser.safebrowsing.provider.google4.pver", "4");
 pref("browser.safebrowsing.provider.google4.lists", "goog-badbinurl-proto,goog-downloadwhite-proto,goog-phish-proto,googpub-phish-proto,goog-malware-proto,goog-unwanted-proto");
 pref("browser.safebrowsing.provider.google4.updateURL", "https://safebrowsing.googleapis.com/v4/threatListUpdates:fetch?$ct=application/x-protobuf&key=%GOOGLE_API_KEY%");
-
+#ifdef NIGHTLY_BUILD
+pref("browser.safebrowsing.provider.google4.gethashURL", "https://safebrowsing.googleapis.com/v4/fullHashes:find?$ct=application/x-protobuf&key=%GOOGLE_API_KEY%");
+#else
 pref("browser.safebrowsing.provider.google4.gethashURL", "");
+#endif // NIGHTLY_BUILD
 pref("browser.safebrowsing.provider.google4.reportURL", "https://safebrowsing.google.com/safebrowsing/diagnostic?client=%NAME%&hl=%LOCALE%&site=");
 pref("browser.safebrowsing.provider.google4.reportPhishMistakeURL", "https://%LOCALE%.phish-error.mozilla.com/?hl=%LOCALE%&url=");
 pref("browser.safebrowsing.provider.google4.reportMalwareMistakeURL", "https://%LOCALE%.malware-error.mozilla.com/?hl=%LOCALE%&url=");
