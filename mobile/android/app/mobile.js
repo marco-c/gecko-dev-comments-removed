67e4391d6339a6cd43e93d2e814115a8ae927204
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 02:19:47 +0000

    Bug 1306219-Part3 Add logic to determine if Widevine keysystem is supported on Fennec. r=cpearce
    MozReview-Commit-ID: 7lquhpJPYfW
    
    UltraBlame original commit: 660be9ffa179f71f28180a6f318590614d403364

--- mobile/android/app/mobile.js
+++ mobile/android/app/mobile.js
@@ -624,6 +624,8 @@ pref("media.android-remote-codec.enabled", false);
 
 pref("media.mediasource.enabled", true);
 
+pref("media.mediadrm-widevinecdm.visible", true);
+
 
 pref("image.downscale-during-decode.enabled", true);
 
