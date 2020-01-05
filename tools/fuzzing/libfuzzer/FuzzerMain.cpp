ec6120a59b9c1952ce91552f609a157f0e516802
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 03:42:32 +0000

    Bug 1302451 - Import LibFuzzer code. r=froydnj
    MozReview-Commit-ID: 3NRSquBHyr2
    
    UltraBlame original commit: 627c11c223122d1ea7335077bdb833cfb8e1ff82

---
+++ tools/fuzzing/libfuzzer/FuzzerMain.cpp
@@ -0,0 +1,21 @@
+
+
+
+
+
+
+
+
+
+
+
+#include "FuzzerDefs.h"
+
+extern "C" {
+
+int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size);
+}  
+
+int main(int argc, char **argv) {
+  return fuzzer::FuzzerDriver(&argc, &argv, LLVMFuzzerTestOneInput);
+}
