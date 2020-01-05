d33fd0b4ab66f7de1d7d15874dc27e4c24084bb0
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 20:39:04 +0000

    Bug 1313972 - backout bug 1321865 for extremely high test failure rate. r=me
    MozReview-Commit-ID: 365ppF9DbRb
    
    UltraBlame original commit: 35af33f46d9499052b4c1ccdee93907f602b6698

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -5636,7 +5636,7 @@ pref("prompts.authentication_dialog_abuse_limit", 3);
 pref("browser.storageManager.enabled", false);
 pref("browser.storageManager.pressureNotification.minIntervalMS", 1200000);
 pref("browser.storageManager.pressureNotification.usageThresholdGB", 5);
-pref("dom.IntersectionObserver.enabled", true);
+pref("dom.IntersectionObserver.enabled", false);
 
 
 pref("dom.moduleScripts.enabled", false);
