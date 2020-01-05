a2b885f1689d2e99527fb7a212e608a9fcc44548
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 08:30:44 +0000

    servo: Convert the content task's control messages and dom events to pipes
    Source-Repo: https://github.com/servo/servo
    Source-Revision: bee47744a3f3da087e1e4f0b5aa9dadb8d123a8f
    
    UltraBlame original commit: 691ab55a188ba41ff41c1488ace568a35821a1ff

--- servo/src/servo/resource/local_image_cache.rs
+++ servo/src/servo/resource/local_image_cache.rs
@@ -24,7 +24,7 @@ pub fn LocalImageCache(
 pub struct LocalImageCache {
     priv image_cache_task: ImageCacheTask,
     priv mut round_number: uint,
-    priv mut on_image_available: Option<~fn(ImageResponseMsg)>,
+    priv mut on_image_available: Option<@fn() -> ~fn(ImageResponseMsg)>,
     priv state_map: UrlMap<@ImageState>
 }
 
@@ -40,7 +40,7 @@ pub impl LocalImageCache {
     
     
     
-     fn next_round(on_image_available: ~fn(ImageResponseMsg)) {
+     fn next_round(on_image_available: @fn() -> ~fn(ImageResponseMsg)) {
         self.round_number += 1;
         self.on_image_available = Some(move on_image_available);
     }
@@ -109,7 +109,7 @@ pub impl LocalImageCache {
                 
                 let image_cache_task = self.image_cache_task.clone();
                 assert self.on_image_available.is_some();
-                let on_image_available = {copy *self.on_image_available.get_ref()};
+                let on_image_available = self.on_image_available.get()();
                 let url = copy *url;
                 do task::spawn |move url, move on_image_available| {
                     let (response_chan, response_port) = pipes::stream();
