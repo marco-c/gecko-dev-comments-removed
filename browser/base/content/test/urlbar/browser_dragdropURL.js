5656a8e37df3a7ebb26ddb23cc7d2b1a46483225
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 05:08:29 +0000

    Bug 791597 - followup, fix tests being picky, rs=me,bustage
    MozReview-Commit-ID: IiqeMXNB6nH
    
    UltraBlame original commit: b52932a0811b2fc49decad407729cb61c8431272

--- browser/base/content/test/urlbar/browser_dragdropURL.js
+++ browser/base/content/test/urlbar/browser_dragdropURL.js
@@ -5,7 +5,10 @@ const DRAG_URL = "http://www.example.com/";
 
 add_task(function* checkURLBarUpdateForDrag() {
   yield BrowserTestUtils.withNewTab(TEST_URL, function* (browser) {
-    EventUtils.synthesizeDrop(browser, gURLBar, [[{type: "text/uri-list", data: DRAG_URL}]], "copy", window);
+    
+    
+    EventUtils.synthesizeDrop(document.getElementById("downloads-button"), gURLBar,
+                              [[{type: "text/plain", data: DRAG_URL}]], "copy", window);
     is(gURLBar.value, TEST_URL, "URL bar value should not have changed");
     is(gBrowser.selectedBrowser.userTypedValue, null, "Stored URL bar value should not have changed");
   });
