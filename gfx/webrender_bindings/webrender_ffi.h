55bb25376144d98d09399377a8fbc51479179edd
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 23:24:09 +0000

    Bug 1362925 - Update webrender_bindings for cbindgen:0.1.7 r=kats
    MozReview-Commit-ID: AwlqUWWEWfz
    
    UltraBlame original commit: 0d025f2bfaaf7d189e48442d9da171a065db63ad

--- gfx/webrender_bindings/webrender_ffi.h
+++ gfx/webrender_bindings/webrender_ffi.h
@@ -23,6 +23,8 @@ bool is_glcontext_egl(void* glcontext_ptr);
 void gfx_critical_note(const char* msg);
 void* get_proc_address_from_glcontext(void* glcontext_ptr, const char* procname);
 
+} 
+
 
 
 
@@ -44,7 +46,6 @@ void* get_proc_address_from_glcontext(void* glcontext_ptr, const char* procname)
 
 #undef WR_FUNC
 #undef WR_DESTRUCTOR_SAFE_FUNC
-} 
 
 struct WrGlyphArray
 {
