e304a21265a37ac2118154ad8e4c21e125d904a5
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 23:21:07 +0000

    Bug 1356122 - Run Stylo tests in sequential mode r=aki
    UltraBlame original commit: c21bd752dc66ae708019a4156bbee69123bec4e8

--- taskcluster/taskgraph/transforms/tests.py
+++ taskcluster/taskgraph/transforms/tests.py
@@ -644,7 +644,8 @@ def parallel_stylo_tests(config, tests):
     parallel traversal in the style system."""
 
     for test in tests:
-        if not test['test-platform'].startswith('linux64-stylo/'):
+        if (not test['test-platform'].startswith('linux64-stylo/')) and \
+           (not test['test-platform'].startswith('linux64-stylo-sequential/')):
             yield test
             continue
 
@@ -655,9 +656,15 @@ def parallel_stylo_tests(config, tests):
             yield test
             continue
 
-        test['mozharness'].setdefault('extra-options', [])\
-                          .append('--parallel-stylo-traversal')
-        yield test
+        
+        if test['test-platform'].startswith('linux64-stylo-sequential/'):
+            yield test
+
+        if test['test-platform'].startswith('linux64-stylo/'):
+            
+            test['mozharness'].setdefault('extra-options', [])\
+                              .append('--parallel-stylo-traversal')
+            yield test
 
 
 @transforms.add
