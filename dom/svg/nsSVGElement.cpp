988721f07a2e654c3a410ea5cbb313a9f4bdf848
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 21:21:55 +0000

    Bug 1329088 - Pass SVG length mode to Stylo. r=emilio
    In the SVG element case, tell Stylo to use the SVG length mode.
    
    MozReview-Commit-ID: 9UR1AYRh4RE
    
    UltraBlame original commit: 426b7ff4ce19de7d31ebd57c617576915fa9ce17

--- dom/svg/nsSVGElement.cpp
+++ dom/svg/nsSVGElement.cpp
@@ -1237,8 +1237,9 @@ MappedAttrParser::ParseMappedAttrValue(nsIAtom* aMappedAttrName,
       RefPtr<URLExtraData> data = new URLExtraData(mBaseURI, mDocURI,
                                                    mElement->NodePrincipal());
       
-      changed = Servo_DeclarationBlock_SetPropertyById(mDecl->AsServo()->Raw(), propertyID,
-                                                       &value, false, data);
+      changed = Servo_DeclarationBlock_SetPropertyById(
+        mDecl->AsServo()->Raw(), propertyID, &value, false, data,
+        LengthParsingMode::SVG);
     }
 
     if (changed) {
