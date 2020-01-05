5843a6c4c619287c1a2bf0603435711310e80205
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 12:27:55 +0000

    Bug 1335983 - Migrate nsCollation::CreateCollection to use LocaleService::GetAppLocale. r=jfkthame
    MozReview-Commit-ID: 5GEnL7Kihpj
    
    UltraBlame original commit: 9fad6f3a8b036f2f42394c7acfee9060dca77054

--- intl/locale/nsCollation.h
+++ intl/locale/nsCollation.h
@@ -23,7 +23,8 @@ class nsCollationFactory final : public nsICollationFactory {
 public: 
   NS_DECL_ISUPPORTS 
 
-  NS_IMETHOD CreateCollation(nsILocale* locale, nsICollation** instancePtr) override;
+  NS_IMETHOD CreateCollation(nsICollation** instancePtr) override;
+  NS_IMETHOD CreateCollationForLocale(const nsACString& locale, nsICollation** instancePtr) override;
 
   nsCollationFactory() {}
 };
