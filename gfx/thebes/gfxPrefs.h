677c5ff0cd205ad7a1d4e369d8ab15ebfc516d78
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 16:54:11 +0000

    Merge mozilla-central to mozilla-inbound
    UltraBlame original commit: c20304c7dd0bc18f307d578969e8722900604541

--- gfx/thebes/gfxPrefs.h
+++ gfx/thebes/gfxPrefs.h
@@ -562,6 +562,8 @@ private:
 
   
   DECL_GFX_PREF(Once, "layout.scroll.root-frame-containers",   LayoutUseContainersForRootFrames, bool, true);
+  
+  DECL_GFX_PREF(Live, "layout.scrollbars.always-layerize-track", AlwaysLayerizeScrollbarTrackTestOnly, bool, false);
   DECL_GFX_PREF(Live, "layout.smaller-painted-layers",         LayoutSmallerPaintedLayers, bool, false);
 
   DECL_GFX_PREF(Once, "media.hardware-video-decoding.force-enabled",
--- layout/painting/nsDisplayList.h
+++ layout/painting/nsDisplayList.h
@@ -449,11 +449,8 @@ public:
 
 
 
-  void GetScrollbarInfo(ViewID* aOutScrollbarTarget, uint32_t* aOutScrollbarFlags)
-  {
-    *aOutScrollbarTarget = mCurrentScrollbarTarget;
-    *aOutScrollbarFlags = mCurrentScrollbarFlags;
-  }
+  ViewID GetCurrentScrollbarTarget() const { return mCurrentScrollbarTarget; }
+  uint32_t GetCurrentScrollbarFlags() const { return mCurrentScrollbarFlags; }
   
 
 
