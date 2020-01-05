14815c1ba8175aceb16e550be0295c138212b23a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 21:23:37 +0000

    Bug 1356137 - Ensure TC workspace and tooltool cache have the right permissions. r=dustin
    UltraBlame original commit: ed794b3612db26ccd7c5d44188e85cd442bea04a

--- taskcluster/taskgraph/transforms/job/toolchain.py
+++ taskcluster/taskgraph/transforms/job/toolchain.py
@@ -116,6 +116,9 @@ def docker_worker_toolchain(config, job, taskdesc):
 
     worker['command'] = [
         '/home/worker/bin/run-task',
+        
+        '--chown-recursive', '/home/worker/workspace',
+        '--chown-recursive', '/home/worker/tooltool-cache',
         '--vcs-checkout=/home/worker/workspace/build/src',
         '--',
         'bash',
