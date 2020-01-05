6c83cd958f83e5cc0d0f61a67ec1c6749e5d7208
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 18:04:33 +0000

    Bug 1335146: IonMonkey - Don't do flow-aa on blocks without entry predecessor, r=jandem
    UltraBlame original commit: 8cba3e613627bd87e9be21294a4a2c417e1c47f2

--- js/src/jit/FlowAliasAnalysis.cpp
+++ js/src/jit/FlowAliasAnalysis.cpp
@@ -463,13 +463,20 @@ FlowAliasAnalysis::analyze()
         if (!stores_->maybeFreePredecessorBlocks(*block))
             return false;
 
-        if (block->isLoopHeader())
-            loop_ = new(alloc()) LoopInfo(alloc(), loop_, *block);
-
         for (MPhiIterator def(block->phisBegin()), end(block->phisEnd()); def != end; ++def)
             def->setId(newId++);
 
         BlockStoreInfo& blockInfo = stores_->current();
+
+        
+        
+        
+        if (blockInfo.length() == 0)
+            continue;
+
+        if (block->isLoopHeader())
+            loop_ = new(alloc()) LoopInfo(alloc(), loop_, *block);
+
         for (MInstructionIterator def(block->begin()), end(block->begin(block->lastIns()));
                 def != end;
                 ++def)
@@ -587,6 +594,14 @@ FlowAliasAnalysis::processDeferredLoads(LoopInfo* info)
 
         DumpLoopInvariant(load, info->loopHeader(),  loopinvariant,
                           loopInvariantDependency);
+
+        
+        
+        
+        if (loopInvariantDependency.length() == 0) {
+            load->setDependency(store);
+            continue;
+        }
 
         if (loopinvariant) {
             if (!improveDependency(load, loopInvariantDependency, output_))
