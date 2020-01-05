798116b2425b5b3403b14e96d97e6c3feb1f32ea
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 23:01:14 +0000

    Bug 1359952 - Add ownProp flag to CanAttachDenseElementHole r=jandem
    MozReview-Commit-ID: EvpCisLDTk2
    
    UltraBlame original commit: a6ce362270952b636c8020d499de3e5bdaa89411

--- js/src/jit/CacheIR.cpp
+++ js/src/jit/CacheIR.cpp
@@ -1408,7 +1408,7 @@ GetPropIRGenerator::tryAttachDenseElement(HandleObject obj, ObjOperandId objId,
 }
 
 static bool
-CanAttachDenseElementHole(JSObject* obj)
+CanAttachDenseElementHole(JSObject* obj, bool ownProp)
 {
     
     
@@ -1421,6 +1421,10 @@ CanAttachDenseElementHole(JSObject* obj)
 
         if (ClassCanHaveExtraProperties(obj->getClass()))
             return false;
+
+        
+        if (ownProp)
+            return true;
 
         JSObject* proto = obj->staticPrototype();
         if (!proto)
@@ -1449,7 +1453,7 @@ GetPropIRGenerator::tryAttachDenseElementHole(HandleObject obj, ObjOperandId obj
     if (obj->as<NativeObject>().containsDenseElement(index))
         return false;
 
-    if (!CanAttachDenseElementHole(obj))
+    if (!CanAttachDenseElementHole(obj, false))
         return false;
 
     
@@ -2085,7 +2089,7 @@ InIRGenerator::tryAttachDenseInHole(uint32_t index, Int32OperandId indexId,
     if (obj->as<NativeObject>().containsDenseElement(index))
         return false;
 
-    if (!CanAttachDenseElementHole(obj))
+    if (!CanAttachDenseElementHole(obj, false))
         return false;
 
     
