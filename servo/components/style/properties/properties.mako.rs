a8d5c967ec797d4ee1f29a9ad1fe69c757ad09c5
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 22:46:18 +0000

    servo: Merge #16664 - style: Avoid allocating a unique nsStyleSVG and nsStyleBackground per element (from emilio:fix-eternal-todo); r=bholley
    Should also help with https://bugzilla.mozilla.org/show_bug.cgi?id=1360881, and it's a TODO that has always been there.
    
    This is on top of #16663, so only last commit needs review.
    
    Source-Repo: https://github.com/servo/servo
    Source-Revision: 38bbacda7016843c8eefb150bc058845a192e3bf
    
    UltraBlame original commit: ef8fb3d04a4361fd5247b886cbce066826741bec

--- servo/components/style/properties/properties.mako.rs
+++ servo/components/style/properties/properties.mako.rs
@@ -1,12 +1,12 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+// This file is a Mako template: http://www.makotemplates.org/
 
-
-
-
-
-
-
-
+// Please note that valid Rust syntax may be mangled by the Mako parser.
+// For example, Vec<&Foo> will be mangled as Vec&Foo>. To work around these issues, the code
+// can be escaped. In the above example, Vec<<&Foo> or Vec< &Foo> achieves the desired result of Vec<&Foo>.
 
 <%namespace name="helpers" file="/helpers.mako.rs" />
 
@@ -2044,6 +2044,15 @@ impl<'a, T: 'a> StyleStructRef<'a, T>
         }
     }
 
+    /// Get a mutable reference to the owned struct, or `None` if the struct
+    /// hasn't been mutated.
+    pub fn get_if_mutated(&mut self) -> Option<<&mut T> {
+        match *self {
+            StyleStructRef::Owned(ref mut v) => Some(v),
+            StyleStructRef::Borrowed(..) => None,
+        }
+    }
+
     /// Returns an `Arc` to the internal struct, constructing one if
     /// appropriate.
     pub fn build(self) -> Arc<T> {
@@ -2140,6 +2149,13 @@ impl<'a> StyleBuilder<'a> {
         /// Gets a mutable view of the current `${style_struct.name}` style.
         pub fn mutate_${style_struct.name_lower}(&mut self) -> &mut style_structs::${style_struct.name} {
             self.${style_struct.ident}.mutate()
+        }
+
+        /// Gets a mutable view of the current `${style_struct.name}` style,
+        /// only if it's been mutated before.
+        pub fn get_${style_struct.name_lower}_if_mutated(&mut self)
+                                                         -> Option<<&mut style_structs::${style_struct.name}> {
+            self.${style_struct.ident}.get_if_mutated()
         }
     % endfor
 
@@ -2546,11 +2562,13 @@ pub fn apply_declarations<'a, F, I>(device: &Device,
     }
 
     % if product == "gecko":
-        // FIXME(emilio): This is effectively creating a new nsStyleBackground
-        // and nsStyleSVG per element. We should only do this when necessary
-        // using the `seen` bitfield!
-        style.mutate_background().fill_arrays();
-        style.mutate_svg().fill_arrays();
+        if let Some(ref mut bg) = style.get_background_if_mutated() {
+            bg.fill_arrays();
+        }
+
+        if let Some(ref mut svg) = style.get_svg_if_mutated() {
+            svg.fill_arrays();
+        }
     % endif
 
     if is_root_element {
