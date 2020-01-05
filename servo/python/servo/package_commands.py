3a33b877d8ece61ab933f58a20721489646ea8b9
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 11:44:42 +0000

    servo: Merge #13474 - Switch default renderer to webrender, and update webrender (from glennw:wr-default); r=Ms2ger
    Source-Repo: https://github.com/servo/servo
    Source-Revision: a082501173918d6a5808881f84556cb78391b9b2
    
    UltraBlame original commit: 10f05e8aa84516a69fc5fa0ae862e0c716d7f240

--- servo/python/servo/package_commands.py
+++ servo/python/servo/package_commands.py
@@ -307,7 +307,7 @@ class PackageCommands(CommandBase):
                     delete(dir_to_package + '/build/' + f)
             print("Writing runservo.sh")
             
-            servo_args = ['-w', '-b',
+            servo_args = ['-b',
                           '--pref', 'dom.mozbrowser.enabled',
                           '--pref', 'dom.forcetouch.enabled',
                           '--pref', 'shell.builtin-key-shortcuts.enabled=false',
