c88de3a86c53fe2dc38b5cf2e4515832f7caba17
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 08:56:18 +0000

    servo: Merge #1453 - Remove a dead function (from SimonSapin:dead-code); r=larsbergstrom
    Turns out it wasn’t actually used anymore.
    
    Source-Repo: https://github.com/servo/servo
    Source-Revision: f4d7e7d3392a090852a18b2c26bc8439e858ccb7
    
    UltraBlame original commit: c2e64b043ad761a279bdc92fcaf9834786a7b772

--- servo/src/components/gfx/font_context.rs
+++ servo/src/components/gfx/font_context.rs
@@ -2,7 +2,7 @@
 
 
 
-use font::{Font, FontDescriptor, FontGroup, FontHandleMethods, FontStyle,
+use font::{Font, FontDescriptor, FontGroup, FontHandleMethods,
            SelectorPlatformIdentifier};
 use font::{SpecifiedFontStyle, UsedFontStyle};
 use font_list::FontList;
@@ -11,24 +11,12 @@ use servo_util::time::ProfilerChan;
 
 use platform::font::FontHandle;
 use platform::font_context::FontContextHandle;
-use style::computed_values::font_weight;
 
 use azure::azure_hl::BackendType;
 use std::hashmap::HashMap;
 
 use std::rc::RcMut;
 
-
-
-pub fn dummy_style() -> FontStyle {
-    return FontStyle {
-        pt_size: 20f64,
-        weight: font_weight::Weight300,
-        italic: false,
-        oblique: false,
-        families: ~"serif, sans-serif",
-    }
-}
 
 pub trait FontContextHandleMethods {
     fn create_font_from_identifier(&self, ~str, UsedFontStyle) -> Result<FontHandle, ()>;
