3eedcd2e1646a0422c1791dd89027bbf45f9721f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 19:25:03 +0000

    Bug 1315850 - Add GMPService::GetCDM. r=gerald
    Infrastructure necessary to create an instance of the CDM process.
    
    MozReview-Commit-ID: 7oQ86x6BNWj
    
    UltraBlame original commit: 997ee5288cd034211141f489b88cd15e58f5cac0

--- dom/media/gmp/GMPService.h
+++ dom/media/gmp/GMPService.h
@@ -24,6 +24,7 @@
 #include "mozilla/MozPromise.h"
 #include "GMPContentParent.h"
 #include "GMPCrashHelper.h"
+#include "ChromiumCDMParent.h"
 
 template <class> struct already_AddRefed;
 
@@ -37,12 +38,27 @@ namespace gmp {
 
 struct NodeId
 {
+  NodeId(const nsAString& aOrigin,
+         const nsAString& aTopLevelOrigin,
+         const nsAString& aGMPName)
+    : mOrigin(aOrigin)
+    , mTopLevelOrigin(aTopLevelOrigin)
+    , mGMPName(aGMPName)
+  {
+  }
   nsString mOrigin;
   nsString mTopLevelOrigin;
   nsString mGMPName;
 };
 
-typedef MozPromise<RefPtr<GMPContentParent::CloseBlocker>, nsresult,  true> GetGMPContentParentPromise;
+typedef MozPromise<RefPtr<GMPContentParent::CloseBlocker>,
+                   nsresult,
+                    true>
+  GetGMPContentParentPromise;
+typedef MozPromise<RefPtr<ChromiumCDMParent>,
+                   nsresult,
+                    true>
+  GetCDMParentPromise;
 
 class GeckoMediaPluginService : public mozIGeckoMediaPluginService
                               , public nsIObserver
@@ -53,6 +69,10 @@ public:
   virtual nsresult Init();
 
   NS_DECL_THREADSAFE_ISUPPORTS
+
+  RefPtr<GetCDMParentPromise> GetCDM(const NodeId& aNodeId,
+                                     nsTArray<nsCString> aTags,
+                                     GMPCrashHelper* aHelper);
 
   
   NS_IMETHOD GetThread(nsIThread** aThread) override;
