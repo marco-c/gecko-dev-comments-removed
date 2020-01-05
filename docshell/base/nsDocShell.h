3e36df84979cf7ffea44d5685492e2efba22b74b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 19:10:58 +0000

    Bug 1316683 - Avoid going into SpiderMonkey for retrieving origin attributes; r=baku
    Our caller is C++ code, and the implementations are all also written in C++,
    so there is no reason to go through SpiderMonkey here.  This patch also makes
    nsILoadContext builtinclass to ensure that the implementation is always native.
    
    UltraBlame original commit: ef22dba0dac560980462f670db732470d5e364c1

--- docshell/base/nsDocShell.h
+++ docshell/base/nsDocShell.h
@@ -806,6 +806,8 @@ protected:
 
   void UpdateGlobalHistoryTitle(nsIURI* aURI);
 
+  NS_IMETHOD_(void) GetOriginAttributes(mozilla::OriginAttributes& aAttrs) override;
+
   
   nsIntRect mBounds;
   nsString mName;
