29b6db7d260955c78a99d82479add48e37a7c181
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 01:34:53 +0000

    Bug 1289148 - Fixing scrollbar metrics for GTK >= 3.20 r=karlt The Gtk 3.20 scrollbars has moved towards usual box model. The scrollbar, trough,thumb and scrollbar button can now have margin, padding and border set, different for each direction (ie. left, right, bottom, top). The scrollbar metrics become ignored in Gtk 3.20 and later.
    * Draw scrollbar element [for GTK 3.20+]
    * The border for scrollbar, trough, thumb and scrollbar buttons is newly
      calculated as margin+padding+border [for GTK 3.20+].
    * The margin is subtracted for scrollbar, trough and sb buttons during paint
      function [for GTK 3.6+]
    * All scrollbar widget's borders transfered from
      nsNativeThemeGTK::GetWidgetBorder to the moz_gtk_get_widget_border.
    * Added helper function NativeThemeToGtkTheme for mapping mozilla's widget type
      to the gtk widget type.
    * Scrollbar troughs are now drawn even when there is not enough room for
      the thumb [GTK 3.20+]
    
    MozReview-Commit-ID: jd2q67gKM1
    
    UltraBlame original commit: 013a4088a2307fa31995331c4d18b47fbf75a80b

--- widget/gtk/gtkdrawing.h
+++ widget/gtk/gtkdrawing.h
@@ -510,6 +510,13 @@ moz_gtk_get_tab_thickness(WidgetNodeType aNodeType);
 
 gboolean moz_gtk_has_scrollbar_buttons(void);
 
+
+
+
+
+void moz_gtk_get_widget_min_size(WidgetNodeType aGtkWidgetType, int* width,
+                                 int* height);
+
 #if (MOZ_WIDGET_GTK == 2)
 #ifdef __cplusplus
 }
