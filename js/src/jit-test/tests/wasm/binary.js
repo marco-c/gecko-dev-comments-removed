7c98df45bc6ae1de9cc2a2937e2219fcd83688f3
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 21:25:25 +0000

    Bug 1353763 - Baldr: Fix slow test (r=bbouvier)
    MozReview-Commit-ID: 6Dp6Kn21nBp
    
    UltraBlame original commit: 20e85b5bd3429c3e7386b56fe6c6056d52219eb0

--- js/src/jit-test/tests/wasm/binary.js
+++ js/src/jit-test/tests/wasm/binary.js
@@ -355,10 +355,19 @@ assertErrorMessage(() => wasmEval(moduleWithSections([v2vSigSection, declSection
 assertErrorMessage(() => wasmEval(moduleWithSections([v2vSigSection, declSection([0]), bodySection([funcBody({locals:[], body:[EndCode,UnreachableCode]})])])), CompileError, bodyMismatch);
 
 
-var manyBlocks = [];
-for (var i = 0; i < 20000; i++)
-    manyBlocks.push(BlockCode, VoidCode, EndCode);
-wasmEval(moduleWithSections([v2vSigSection, declSection([0]), bodySection([funcBody({locals:[], body:manyBlocks})])]));
+
+
+
+
+var jco = getJitCompilerOptions();
+if (jco["ion.enable"] && jco["baseline.enable"] && jco["baseline.warmup.trigger"] > 0 && jco["ion.warmup.trigger"] > 10) {
+    var manyBlocks = [];
+    for (var i = 0; i < 20000; i++)
+        manyBlocks.push(BlockCode, VoidCode);
+    for (var i = 0; i < 20000; i++)
+        manyBlocks.push(EndCode);
+    wasmEval(moduleWithSections([v2vSigSection, declSection([0]), bodySection([funcBody({locals:[], body:manyBlocks})])]));
+}
 
 
 var tooBigNameSection = {
