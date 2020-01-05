c64fa5bed4f91ed23e4ba86807b69937654c5937
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 16:37:42 +0000

    servo: Merge #15538 - Do RestyleHint assertions at runtime so they use build-time bindgen (from heycam:mv-test); r=bholley
    Currently these assertions work off the in-tree bindings.
    
    r? bholley
    
    Source-Repo: https://github.com/servo/servo
    Source-Revision: dda5c3c3229d399672cbd7909d2b34f8985d4f3f
    
    UltraBlame original commit: 922b80ff3d1a472d0495cc57e387ad471d598983

--- servo/ports/geckolib/glue.rs
+++ servo/ports/geckolib/glue.rs
@@ -68,7 +68,7 @@ use style::properties::{ComputedValues, Importance, PropertyDeclaration};
 use style::properties::{PropertyDeclarationParseResult, PropertyDeclarationBlock, PropertyId};
 use style::properties::animated_properties::{AnimationValue, Interpolate, TransitionProperty};
 use style::properties::parse_one_declaration;
-use style::restyle_hints::RestyleHint;
+use style::restyle_hints::{self, RestyleHint};
 use style::selector_parser::PseudoElementCascadeType;
 use style::sequential;
 use style::string_cache::Atom;
@@ -98,6 +98,9 @@ pub extern "C" fn Servo_Initialize() -> () {
 
     
     thread_state::initialize(thread_state::LAYOUT);
+
+    
+    restyle_hints::assert_restyle_hints_match();
 }
 
 #[no_mangle]
