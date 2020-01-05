cb5f742c21f5cf3567b62519269e6fbeb59d4bfb
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 07:47:12 +0000

    Bug 1329362 - part 8, Convert LayerManagerComposite::RenderToPresentationSurface to use CompositorWidget instead of nsIWidget r=kats
    UltraBlame original commit: 6998e73596c6375621dcba16d046a9ca470baf55

--- gfx/layers/composite/LayerManagerComposite.cpp
+++ gfx/layers/composite/LayerManagerComposite.cpp
@@ -49,7 +49,6 @@
 #include "nsCOMPtr.h"                   
 #include "nsDebug.h"                    
 #include "nsISupportsImpl.h"            
-#include "nsIWidget.h"                  
 #include "nsPoint.h"                    
 #include "nsRect.h"                     
 #include "nsRegion.h"                   
@@ -1038,16 +1037,14 @@ private:
 void
 LayerManagerComposite::RenderToPresentationSurface()
 {
-#ifdef MOZ_WIDGET_ANDROID
-  nsIWidget* const widget = mCompositor->GetWidget()->RealWidget();
-  auto window = static_cast<ANativeWindow*>(
-      widget->GetNativeData(NS_PRESENTATION_WINDOW));
+  widget::CompositorWidget* const widget = mCompositor->GetWidget();
+  ANativeWindow* window = widget->AsAndroid()->GetPresentationANativeWindow();
 
   if (!window) {
     return;
   }
 
-  EGLSurface surface = widget->GetNativeData(NS_PRESENTATION_SURFACE);
+  EGLSurface surface = widget->AsAndroid()->GetPresentationEGLSurface();
 
   if (!surface) {
     
@@ -1056,8 +1053,7 @@ LayerManagerComposite::RenderToPresentationSurface()
       return;
     }
 
-    widget->SetNativeData(NS_PRESENTATION_SURFACE,
-                          reinterpret_cast<uintptr_t>(surface));
+    widget->AsAndroid()->SetPresentationEGLSurface(surface);
   }
 
   CompositorOGL* compositor = mCompositor->AsCompositorOGL();
@@ -1071,7 +1067,6 @@ LayerManagerComposite::RenderToPresentationSurface()
   const IntSize windowSize(ANativeWindow_getWidth(window),
                            ANativeWindow_getHeight(window));
 
-#endif
 
   if ((windowSize.width <= 0) || (windowSize.height <= 0)) {
     return;
