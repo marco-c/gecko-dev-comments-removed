c398776d7f0b37c22b684e235072e34507f445c9
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 23:56:39 +0000

    Bug 1363191 - Pop eval lexical scope before propagating its var scope's Annex B functions. (r=anba)
    UltraBlame original commit: accca3d23d7bf2df1b66186e564c79e87e67f45a

--- js/src/frontend/Parser.cpp
+++ js/src/frontend/Parser.cpp
@@ -2098,21 +2098,24 @@ Parser<FullParseHandler, char16_t>::evalBody(EvalSharedContext* evalsc)
     if (!varScope.init(pc))
         return nullptr;
 
-    
-    ParseContext::Scope lexicalScope(this);
-    if (!lexicalScope.init(pc))
-        return nullptr;
+    ParseNode* body;
+    {
+        
+        ParseContext::Scope lexicalScope(this);
+        if (!lexicalScope.init(pc))
+            return nullptr;
 
-    ParseNode* body = statementList(YieldIsName);
-    if (!body)
-        return nullptr;
+        body = statementList(YieldIsName);
+        if (!body)
+            return nullptr;
 
-    if (!checkStatementsEOF())
-        return nullptr;
+        if (!checkStatementsEOF())
+            return nullptr;
 
-    body = finishLexicalScope(lexicalScope, body);
-    if (!body)
-        return nullptr;
+        body = finishLexicalScope(lexicalScope, body);
+        if (!body)
+            return nullptr;
+    }
 
     
     
