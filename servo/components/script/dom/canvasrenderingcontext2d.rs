86468a10ed441c9c562a28f1d850dba0e62f804a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 10:48:46 +0000

    servo: Merge #9080 - Enable Pattern variant of CanvasFillOrStrokeStyle (from jdm:canvaspattern); r=Ms2ger
    This is a rebase of #8104, with a test added that exposes pre-existing problems in the canvas code for dealing with patterns.
    
    Source-Repo: https://github.com/servo/servo
    Source-Revision: 80135a70ce7f9c478a79ce9f572c42cce246e1d8
    
    UltraBlame original commit: 8830d523e1c30c5c8881395c1d87f255dce1e306

--- servo/components/script/dom/canvasrenderingcontext2d.rs
+++ servo/components/script/dom/canvasrenderingcontext2d.rs
@@ -50,10 +50,11 @@ use util::vec::byte_swap;
 
 #[must_root]
 #[derive(JSTraceable, Clone, HeapSizeOf)]
+#[allow(dead_code)]
 enum CanvasFillOrStrokeStyle {
     Color(RGBA),
     Gradient(JS<CanvasGradient>),
-    
+    Pattern(JS<CanvasPattern>),
 }
 
 
@@ -922,6 +923,9 @@ impl CanvasRenderingContext2DMethods for CanvasRenderingContext2D {
             CanvasFillOrStrokeStyle::Gradient(ref gradient) => {
                 StringOrCanvasGradientOrCanvasPattern::eCanvasGradient(Root::from_ref(&*gradient))
             },
+            CanvasFillOrStrokeStyle::Pattern(ref pattern) => {
+                StringOrCanvasGradientOrCanvasPattern::eCanvasPattern(Root::from_ref(&*pattern))
+            }
         }
     }
 
@@ -966,6 +970,9 @@ impl CanvasRenderingContext2DMethods for CanvasRenderingContext2D {
             CanvasFillOrStrokeStyle::Gradient(ref gradient) => {
                 StringOrCanvasGradientOrCanvasPattern::eCanvasGradient(Root::from_ref(&*gradient))
             },
+            CanvasFillOrStrokeStyle::Pattern(ref pattern) => {
+                StringOrCanvasGradientOrCanvasPattern::eCanvasPattern(Root::from_ref(&*pattern))
+            }
         }
     }
 
