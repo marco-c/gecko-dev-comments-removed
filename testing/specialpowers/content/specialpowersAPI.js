533fe20eecf42ee53ed1252b2bc269eb5d027e76
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 03:52:15 +0000

    Bug 1318209 - Remove mozIApplication; r=baku
    UltraBlame original commit: 32bde324c727a46b0409bc174ec33ca2d77bdc1a

--- testing/specialpowers/content/specialpowersAPI.js
+++ testing/specialpowers/content/specialpowersAPI.js
@@ -1774,16 +1774,6 @@ SpecialPowersAPI.prototype = {
       
       let uri = Services.io.newURI(arg, null, null);
       principal = secMan.createCodebasePrincipal(uri, {});
-    } else if (arg.manifestURL) {
-      
-      let appsSvc = Cc["@mozilla.org/AppsService;1"]
-                      .getService(Ci.nsIAppsService)
-      let app = appsSvc.getAppByManifestURL(arg.manifestURL);
-      if (!app) {
-        throw "No app for this manifest!";
-      }
-
-      principal = app.principal;
     } else if (arg.nodePrincipal) {
       
       
