fda630777d1514d88c43470604b92bc62a23f6c5
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 21:45:41 +0000

    Bug 1357012 - Followup: add missing ReportOutOfMemory.
    UltraBlame original commit: 2eca4618f579c77f8fd3f5303b19b999fcc3a177

--- js/src/jsscript.cpp
+++ js/src/jsscript.cpp
@@ -1858,8 +1858,10 @@ ScriptSource::tryCompressOffThread(JSContext* cx)
     
     
     auto task = MakeUnique<SourceCompressionTask>(cx->runtime(), this);
-    if (!task)
+    if (!task) {
+        ReportOutOfMemory(cx);
         return false;
+    }
     return EnqueueOffThreadCompression(cx, Move(task));
 }
 
