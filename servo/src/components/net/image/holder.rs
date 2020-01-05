2fd527e54b5bc781ef4dc21d5d98168d6b3e1f5a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 09:14:12 +0000

    servo: Merge #2707 - Remove LocalImageCacheHandle hack and associated unsafe code (from glennw:image-cache-handle)
    Source-Repo: https://github.com/servo/servo
    Source-Revision: 1d0df6e65f775f4e38d346359736626850b77d2e
    
    UltraBlame original commit: 28852f31e500ac43fbd26f50f86a9af794fa69a6

--- servo/src/components/net/image/holder.rs
+++ servo/src/components/net/image/holder.rs
@@ -8,7 +8,6 @@ use local_image_cache::LocalImageCache;
 
 use geom::size::Size2D;
 use std::mem;
-use std::ptr;
 use sync::{Arc, Mutex};
 use url::Url;
 
@@ -17,55 +16,17 @@ use url::Url;
 
 
 
-pub struct LocalImageCacheHandle {
-    data: *uint,
-}
 
-impl Drop for LocalImageCacheHandle {
-    fn drop(&mut self) {
-        unsafe {
-            let _: Box<Arc<Mutex<Box<LocalImageCache>>>> =
-                mem::transmute(mem::replace(&mut self.data, ptr::null()));
-        }
-    }
-}
-
-impl Clone for LocalImageCacheHandle {
-    fn clone(&self) -> LocalImageCacheHandle {
-        unsafe {
-            let handle = mem::transmute::<&Arc<Mutex<Box<LocalImageCache>>>,&Arc<*()>>(self.get());
-            let new_handle = (*handle).clone();
-            LocalImageCacheHandle::new(new_handle)
-        }
-    }
-}
-
-impl LocalImageCacheHandle {
-    pub unsafe fn new(cache: Arc<*()>) -> LocalImageCacheHandle {
-        LocalImageCacheHandle {
-            data: mem::transmute(box cache),
-        }
-    }
-
-    pub fn get<'a>(&'a self) -> &'a Arc<Mutex<Box<LocalImageCache>>> {
-        unsafe {
-            mem::transmute::<*uint,&'a Arc<Mutex<Box<LocalImageCache>>>>(self.data)
-        }
-    }
-}
-
-/// A struct to store image data. The image will be loaded once the first time it is requested,
-/// and an Arc will be stored.  Clones of this Arc are given out on demand.
 #[deriving(Clone)]
 pub struct ImageHolder {
     url: Url,
     image: Option<Arc<Box<Image>>>,
     cached_size: Size2D<int>,
-    local_image_cache: LocalImageCacheHandle,
+    local_image_cache: Arc<Mutex<LocalImageCache>>,
 }
 
 impl ImageHolder {
-    pub fn new(url: Url, local_image_cache: LocalImageCacheHandle) -> ImageHolder {
+    pub fn new(url: Url, local_image_cache: Arc<Mutex<LocalImageCache>>) -> ImageHolder {
         debug!("ImageHolder::new() {}", url.to_str());
         let holder = ImageHolder {
             url: url,
@@ -74,13 +35,13 @@ impl ImageHolder {
             local_image_cache: local_image_cache.clone(),
         };
 
-        // Tell the image cache we're going to be interested in this url
-        // FIXME: These two messages must be sent to prep an image for use
-        // but they are intended to be spread out in time. Ideally prefetch
-        // should be done as early as possible and decode only once we
-        // are sure that the image will be used.
+        
+        
+        
+        
+        
         {
-            let val = holder.local_image_cache.get().lock();
+            let val = holder.local_image_cache.lock();
             let mut local_image_cache = val;
             local_image_cache.prefetch(&holder.url);
             local_image_cache.decode(&holder.url);
@@ -89,16 +50,16 @@ impl ImageHolder {
         holder
     }
 
-    /// This version doesn't perform any computation, but may be stale w.r.t. newly-available image
-    /// data that determines size.
-    ///
-    /// The intent is that the impure version is used during layout when dimensions are used for
-    /// computing layout.
+    
+    
+    
+    
+    
     pub fn size(&self) -> Size2D<int> {
         self.cached_size
     }
 
-    /// Query and update the current image size.
+    
     pub fn get_size(&mut self) -> Option<Size2D<int>> {
         debug!("get_size() {}", self.url.to_str());
         self.get_image().map(|img| {
@@ -116,11 +77,11 @@ impl ImageHolder {
     pub fn get_image(&mut self) -> Option<Arc<Box<Image>>> {
         debug!("get_image() {}", self.url.to_str());
 
-        // If this is the first time we've called this function, load
-        // the image and store it for the future
+        
+        
         if self.image.is_none() {
             let port = {
-                let val = self.local_image_cache.get().lock();
+                let val = self.local_image_cache.lock();
                 let mut local_image_cache = val;
                 local_image_cache.get_image(&self.url)
             };
