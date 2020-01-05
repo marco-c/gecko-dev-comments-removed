83c41e9fca0d87ab672a195d2b105d997b2a69a5
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 12:00:13 +0000

    servo: Merge #14492 - stylo bindings update (from heycam:bindings-update-9); r=emilio
    <!-- Please describe your changes on the following line: -->
    
    For the latest m-c stylo sync, on top of #14472.
    
    r? emilio
    
    Source-Repo: https://github.com/servo/servo
    Source-Revision: c9370e04a5c09b706927068b1d514367116563a9
    
    UltraBlame original commit: 7001118ad67bd4f7fdd51bfc91aa05f4f2abb8d9

--- servo/components/style/binding_tools/regen.py
+++ servo/components/style/binding_tools/regen.py
@@ -5,12 +5,15 @@
 
 
 from __future__ import print_function
+
+import re
 import os
 import sys
 import argparse
 import platform
 import copy
 import subprocess
+import fileinput
 
 import regen_atoms
 
@@ -23,7 +26,6 @@ COMPILATION_TARGETS = {
     COMMON_BUILD_KEY: {
         "flags": [
             "--no-unstable-rust",
-            "--disable-name-namespacing",
         ],
         "clang_flags": [
             "-x", "c++", "-std=c++14",
@@ -43,6 +45,13 @@ COMPILATION_TARGETS = {
     "structs": {
         "target_dir": "../gecko_bindings",
         "flags": [
+            "--enable-cxx-namespaces",
+            
+            
+            "--raw-line", "pub use self::root::*;",
+            "--raw-line", "pub use self::root::mozilla::*;",
+            "--raw-line", "pub use self::root::mozilla::css::*;",
+            "--raw-line", "pub use self::root::mozilla::dom::*;",
             "--generate", "types,vars",
         ],
         "includes": [
@@ -69,7 +78,6 @@ COMPILATION_TARGETS = {
         "raw_lines": [
             "use atomic_refcell::AtomicRefCell;",
             "use data::ElementData;",
-            "pub use nsstring::nsStringRepr as nsString;"
         ],
         "blacklist_types": [
             "nsString",
@@ -86,15 +94,19 @@ COMPILATION_TARGETS = {
         ],
         "whitelist": [
             "RawGecko.*",
-            "mozilla_ServoElementSnapshot.*",
-            "mozilla_ConsumeStyleBehavior",
-            "mozilla_LazyComputeBehavior",
-            "mozilla_css_SheetParsingMode",
-            "mozilla_SkipRootBehavior",
+            "mozilla::ServoElementSnapshot.*",
+            "mozilla::ConsumeStyleBehavior",
+            "mozilla::LazyComputeBehavior",
+            "mozilla::css::SheetParsingMode",
+            "mozilla::SkipRootBehavior",
+            "mozilla::DisplayItemClip",  
+                                         
+                                         
             ".*ThreadSafe.*Holder",
             "AnonymousContent",
             "AudioContext",
             "CapturingContentInfo",
+            "ConsumeStyleBehavior",
             "DefaultDelete",
             "DOMIntersectionObserverEntry",
             "Element",
@@ -110,6 +122,7 @@ COMPILATION_TARGETS = {
             "GridNamedArea",
             "Image",
             "ImageURL",
+            "LazyComputeBehavior",
             "nsAttrName",
             "nsAttrValue",
             "nsBorderColors",
@@ -184,7 +197,7 @@ COMPILATION_TARGETS = {
             "ServoAttrSnapshot",
             "ServoElementSnapshot",
             "SheetParsingMode",
-            "mozilla_Side",
+            "Side",
             "StaticRefPtr",
             "StyleAnimation",
             "StyleBasicShape",
@@ -192,7 +205,8 @@ COMPILATION_TARGETS = {
             "StyleClipPath",
             "StyleClipPathGeometryBox",
             "StyleTransition",
-            "UniquePtr",
+            "mozilla::UniquePtr",
+            "mozilla::DefaultDelete",
         ],
         "bitfield_enum_types": ["nsChangeHint", "nsRestyleHint"],
         "opaque_types": [
@@ -205,12 +219,12 @@ COMPILATION_TARGETS = {
             "RefPtr_Proxy_member_function",
             "nsAutoPtr_Proxy",
             "nsAutoPtr_Proxy_member_function",
-            "mozilla_detail_PointerType",
-            "mozilla_Pair_Base",
-            "mozilla_SupportsWeakPtr",
+            "mozilla::detail::PointerType",
+            "mozilla::Pair_Base",
+            "mozilla::SupportsWeakPtr",
             "SupportsWeakPtr",
-            "mozilla_detail_WeakReference",
-            "mozilla_WeakPtr",
+            "mozilla::detail::WeakReference",
+            "mozilla::WeakPtr",
             "nsWritingIterator_reference", "nsReadingIterator_reference",
             "nsTObserverArray",  
             "nsTHashtable",  
@@ -221,19 +235,23 @@ COMPILATION_TARGETS = {
                                         
             "nsPIDOMWindow",  
                               
-            "JS_Rooted",
-            "mozilla_Maybe",
+            "JS::Rooted",
+            "mozilla::Maybe",
             "gfxSize",  
             "gfxSize_Super",  
+            "mozilla::ErrorResult",  
+        ],
+        "manual_fixups": [
+            ["root::nsString", "::nsstring::nsStringRepr"]
         ],
         "servo_mapped_generic_types": [
             {
                 "generic": True,
-                "gecko": "ServoUnsafeCell",
+                "gecko": "mozilla::ServoUnsafeCell",
                 "servo": "::std::cell::UnsafeCell"
             }, {
                 "generic": True,
-                "gecko": "ServoCell",
+                "gecko": "mozilla::ServoCell",
                 "servo": "::std::cell::Cell"
             }, {
                 "generic": False,
@@ -252,6 +270,7 @@ COMPILATION_TARGETS = {
         ],
         "flags": [
             "--generate", "functions",
+            "--disable-name-namespacing",
         ],
         "match_headers": [
             "ServoBindingList.h",
@@ -486,6 +505,16 @@ def build(objdir, target_name, debug, debugger, kind_name=None,
     
     
     
+    fixups = []
+
+    if "manual_fixups" in current_target:
+        fixups = current_target["manual_fixups"]
+
+    
+    
+    
+    
+    
     
     
     
@@ -610,9 +639,12 @@ Option<&'a mut {0}>;".format(ty))
     if "servo_mapped_generic_types" in current_target:
         for ty in current_target["servo_mapped_generic_types"]:
             flags.append("--blacklist-type")
-            flags.append("mozilla_{}".format(ty["gecko"]))
+            flags.append(ty["gecko"])
+
+            gecko_name = ty["gecko"].split("::")[-1]
             flags.append("--raw-line")
-            flags.append("pub type {0}{2} = {1}{2};".format(ty["gecko"], ty["servo"], "<T>" if ty["generic"] else ""))
+            flags.append("pub type {0}{2} = {1}{2};".format(gecko_name, ty["servo"], "<T>" if ty["generic"] else ""))
+            fixups.append(["root::{}".format(ty["gecko"]), "::gecko_bindings::structs::{}".format(gecko_name)])
 
     if "servo_owned_types" in current_target:
         for entry in current_target["servo_owned_types"]:
@@ -685,6 +717,13 @@ Option<&'a mut {0}>;".format(ty))
     except subprocess.CalledProcessError as e:
         print("FAIL\n", e.output)
         return 1
+
+    generated = fileinput.input(output_filename, inplace=True)
+    for line in generated:
+        for fixup in fixups:
+            line = re.sub("\\b{}\\b".format(fixup[0]), fixup[1], line)
+        print(line, end='')
+    generated.close()
 
     print("OK")
     print("(please test with ./mach test-stylo)")
