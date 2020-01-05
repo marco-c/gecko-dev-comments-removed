a399bbd461813bdc6dfd65aaa5ba8e95d3aee885
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 20:17:29 +0000

    Bug 1351963 (part 9) - Improve SetSampleContext(). r=jseward.
    SetSampleContext() sets the TickSample's register fields, and the two callers
    of SetSampleContext() set the TickSample's mContext.
    
    This patch changes SetSampleContext() so it sets all the fields in one place.
    It also renames SetSampleContext() as FillInSample(), because it sets more than
    just the context.
    
    UltraBlame original commit: 9dcefc9cd349f546492fc79caeaf5315d3912225

--- tools/profiler/core/platform-linux-android.cpp
+++ tools/profiler/core/platform-linux-android.cpp
@@ -77,22 +77,25 @@ Thread::GetCurrentId()
 }
 
 static void
-SetSampleContext(TickSample* sample, mcontext_t& mcontext)
+FillInSample(TickSample* aSample, ucontext_t* aContext)
 {
+  aSample->mContext = aContext;
+  mcontext_t& mcontext = aContext->uc_mcontext;
+
   
 #if defined(GP_ARCH_x86)
-  sample->mPC = reinterpret_cast<Address>(mcontext.gregs[REG_EIP]);
-  sample->mSP = reinterpret_cast<Address>(mcontext.gregs[REG_ESP]);
-  sample->mFP = reinterpret_cast<Address>(mcontext.gregs[REG_EBP]);
+  aSample->mPC = reinterpret_cast<Address>(mcontext.gregs[REG_EIP]);
+  aSample->mSP = reinterpret_cast<Address>(mcontext.gregs[REG_ESP]);
+  aSample->mFP = reinterpret_cast<Address>(mcontext.gregs[REG_EBP]);
 #elif defined(GP_ARCH_amd64)
-  sample->mPC = reinterpret_cast<Address>(mcontext.gregs[REG_RIP]);
-  sample->mSP = reinterpret_cast<Address>(mcontext.gregs[REG_RSP]);
-  sample->mFP = reinterpret_cast<Address>(mcontext.gregs[REG_RBP]);
+  aSample->mPC = reinterpret_cast<Address>(mcontext.gregs[REG_RIP]);
+  aSample->mSP = reinterpret_cast<Address>(mcontext.gregs[REG_RSP]);
+  aSample->mFP = reinterpret_cast<Address>(mcontext.gregs[REG_RBP]);
 #elif defined(GP_ARCH_arm)
-  sample->mPC = reinterpret_cast<Address>(mcontext.arm_pc);
-  sample->mSP = reinterpret_cast<Address>(mcontext.arm_sp);
-  sample->mFP = reinterpret_cast<Address>(mcontext.arm_fp);
-  sample->mLR = reinterpret_cast<Address>(mcontext.arm_lr);
+  aSample->mPC = reinterpret_cast<Address>(mcontext.arm_pc);
+  aSample->mSP = reinterpret_cast<Address>(mcontext.arm_sp);
+  aSample->mFP = reinterpret_cast<Address>(mcontext.arm_fp);
+  aSample->mLR = reinterpret_cast<Address>(mcontext.arm_lr);
 #else
 # error "bad platform"
 #endif
@@ -403,11 +406,8 @@ SamplerThread::SuspendAndSampleAndResumeThread(PS::LockRef aLock,
   
   
 
-  aSample->mContext = &sSigHandlerCoordinator->mUContext;
-
   
-  SetSampleContext(aSample,
-                   sSigHandlerCoordinator->mUContext.uc_mcontext);
+  FillInSample(aSample, &sSigHandlerCoordinator->mUContext);
 
   Tick(aLock, gPS->Buffer(aLock), aSample);
 
@@ -509,8 +509,7 @@ TickSample::PopulateContext(ucontext_t* aContext)
   MOZ_ASSERT(aContext);
 
   if (!getcontext(aContext)) {
-    mContext = aContext;
-    SetSampleContext(this, aContext->uc_mcontext);
+    FillInSample(this, aContext);
   }
 }
 
