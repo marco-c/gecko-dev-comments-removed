8c0938dc840f6f7b364ebb5cb6a9adfb2dd5fb40
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 19:21:50 +0000

    servo: Merge #16111 - Add functions for after-change style of CSS Transition (from hiikezoe:after-change-style); r=emilio
    Add functions for after-change style of CSS Transition
    
    <!-- Please describe your changes on the following line: -->
    This is a PR of https://bugzilla.mozilla.org/show_bug.cgi?id=1346663
    
    ---
    <!-- Thank you for contributing to Servo! Please replace each `[ ]` by `[X]` when the step is complete, and replace `__` with appropriate data: -->
    - [X] `./mach build -d` does not report any errors
    - [X] `./mach test-tidy` does not report any errors
    
    <!-- Either: -->
    - [X] These changes do not require tests because it's for stylo
    
    <!-- Also, please make sure that "Allow edits from maintainers" checkbox is checked, so that we can help you if you get stuck somewhere along the way.-->
    
    <!-- Pull requests that do not address these steps are welcome, but they will require additional verification as part of the review process. -->
    
    Source-Repo: https://github.com/servo/servo
    Source-Revision: 089c71b5872839c5a6c32d1988ce4d42a73860e4
    
    UltraBlame original commit: 6b9bbd396faca96f2e4549dab53b82557fe22403

--- servo/components/style/rule_tree/mod.rs
+++ servo/components/style/rule_tree/mod.rs
@@ -239,6 +239,37 @@ impl RuleTree {
         
         Some(self.insert_ordered_rules_from(current, children.into_iter().rev()))
     }
+
+    
+    pub fn remove_transition_rule_if_applicable(&self, path: &StrongRuleNode) -> StrongRuleNode {
+        
+        if path.cascade_level() != CascadeLevel::Transitions {
+            return path.clone();
+        }
+
+        path.parent().unwrap().clone()
+    }
+
+    
+    pub fn remove_animation_and_transition_rules(&self, path: &StrongRuleNode) -> StrongRuleNode {
+        
+        if !path.has_animation_or_transition_rules() {
+            return path.clone();
+        }
+
+        let iter = path.self_and_ancestors().take_while(|node| node.cascade_level() >= CascadeLevel::Animations);
+        let mut last = path;
+        let mut children = vec![];
+        for node in iter {
+            if node.cascade_level() != CascadeLevel::Animations &&
+               node.cascade_level() != CascadeLevel::Transitions {
+                children.push((node.get().source.clone().unwrap(), node.cascade_level()));
+            }
+            last = node;
+        }
+
+        self.insert_ordered_rules_from(last.parent().unwrap().clone(), children.into_iter().rev())
+    }
 }
 
 
@@ -727,6 +758,13 @@ impl StrongRuleNode {
         if self.get().free_count.load(Ordering::SeqCst) > RULE_TREE_GC_INTERVAL {
             self.gc();
         }
+    }
+
+    
+    pub fn has_animation_or_transition_rules(&self) -> bool {
+        self.self_and_ancestors()
+            .take_while(|node| node.cascade_level() >= CascadeLevel::Animations)
+            .any(|node| matches!(node.cascade_level(), CascadeLevel::Animations | CascadeLevel::Transitions))
     }
 }
 
