391fa19f7b6f638a1bfde2aefbee44c6407636a7
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 01:13:16 +0000

    Bug 1307408 - part1: fix layout issues for small widths in inspector sidebar;r=Honza
    MozReview-Commit-ID: 7bF5gsyxXfp
    
    UltraBlame original commit: a38df6f51b67ca3cd2de921dcfbb8cc8d5208973

--- devtools/client/inspector/inspector.js
+++ devtools/client/inspector/inspector.js
@@ -42,7 +42,6 @@ const TOOLBOX_L10N = new LocalizationHelper("devtools/locale/toolbox.properties"
 
 
 const INITIAL_SIDEBAR_SIZE = 350;
-const MIN_SIDEBAR_SIZE = 50;
 
 
 
@@ -457,7 +456,6 @@ Inspector.prototype = {
       className: "inspector-sidebar-splitter",
       initialWidth: INITIAL_SIDEBAR_SIZE,
       initialHeight: INITIAL_SIDEBAR_SIZE,
-      minSize: MIN_SIDEBAR_SIZE,
       splitterSize: 1,
       endPanelControl: true,
       startPanel: this.InspectorTabPanel({
