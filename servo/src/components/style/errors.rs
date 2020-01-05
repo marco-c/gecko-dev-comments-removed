903b9efd358c2cbdd39301734ba0fa388af77e5a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 09:04:06 +0000

    servo: Merge #2042 - Fix the silencing of CSS errors in the UA stylesheet (from SimonSapin:silence-ua-stylesheet); r=jdm
    r? Ms2ger
    
    Source-Repo: https://github.com/servo/servo
    Source-Revision: e3cc46a7825ef60a36a25f2ff2f18c1f02ed8b50
    
    UltraBlame original commit: 63ed45e3e47bbde7ba59b5ddb754113e010d3981

--- servo/src/components/style/errors.rs
+++ servo/src/components/style/errors.rs
@@ -22,7 +22,10 @@ impl<T, I: Iterator<Result<T, SyntaxError>>> Iterator<T> for ErrorLoggerIterator
 }
 
 
-local_data_key!(silence_errors: ())
+
+
+
+local_data_key!(silence_errors: bool)
 
 pub fn log_css_error(location: SourceLocation, message: &str) {
     
@@ -33,7 +36,7 @@ pub fn log_css_error(location: SourceLocation, message: &str) {
 
 
 pub fn with_errors_silenced<T>(f: || -> T) -> T {
-    local_data::set(silence_errors, ());
+    local_data::set(silence_errors, true);
     let result = f();
     local_data::pop(silence_errors);
     result
