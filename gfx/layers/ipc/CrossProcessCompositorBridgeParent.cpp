f1e8d0584e5e778c73657ff8bad8ffe2d477cbc2
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 12:56:15 +0000

    Merge m-c to graphics
    MozReview-Commit-ID: 7PVyxahE0p6
    
    UltraBlame original commit: c3133d426341b4e1b679af25f1489ecadd03b03f

--- gfx/layers/ipc/CrossProcessCompositorBridgeParent.cpp
+++ gfx/layers/ipc/CrossProcessCompositorBridgeParent.cpp
@@ -95,9 +95,7 @@ CrossProcessCompositorBridgeParent::AllocPLayerTransactionParent(
   }
 
   NS_WARNING("Created child without a matching parent?");
-  
-  
-  *aSuccess = true;
+  *aSuccess = false;
   LayerTransactionParent* p = new LayerTransactionParent(nullptr, this, aId);
   p->AddIPDLReference();
   return p;
