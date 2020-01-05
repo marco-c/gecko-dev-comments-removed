a11df998cc3c8ef60c20d95c130a3cbf2270fa3c
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 23:14:46 +0000

    Bug 1359085 - remove unused devtools util content-globals.js;r=ochameau
    MozReview-Commit-ID: K8Knz9B0Gav
    
    UltraBlame original commit: d69be43cf123d41a98629440c4a7b5fdd4b4fb11

--- addon-sdk/source/lib/sdk/loader/sandbox.js
+++ addon-sdk/source/lib/sdk/loader/sandbox.js
@@ -15,10 +15,6 @@ const self = require('sdk/self');
 const { getTabId } = require('../tabs/utils');
 const { getInnerId } = require('../window/utils');
 
-const { devtools } = Cu.import("resource://devtools/shared/Loader.jsm", {});
-const { require: devtoolsRequire } = devtools;
-const { addContentGlobal, removeContentGlobal } = devtoolsRequire("devtools/server/content-globals");
-
 
 
 
@@ -31,13 +27,6 @@ function sandbox(target, options) {
 
   let sandbox = Cu.Sandbox(target || systemPrincipal, options);
   Cu.setSandboxMetadata(sandbox, options.metadata);
-  let innerWindowID = options.metadata['inner-window-id']
-  if (innerWindowID) {
-    addContentGlobal({
-      global: sandbox,
-      'inner-window-id': innerWindowID
-    });
-  }
   return sandbox;
 }
 exports.sandbox = sandbox;
