1a7b9288b65e4f71ee1e9ff1b70a4441f6eb6c93
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 06:47:31 +0000

    Bug 851892 part 7.  Push the nsIDOMCSSRule implementation up to css::Rule.  r=heycam
    UltraBlame original commit: ca4b1fb9cae4d66be45e628563b4ab5ea6df31df

--- layout/style/StyleRule.h
+++ layout/style/StyleRule.h
@@ -326,11 +326,14 @@ public:
 
   NS_DECL_ISUPPORTS_INHERITED
   NS_DECL_CYCLE_COLLECTION_SCRIPT_HOLDER_CLASS_INHERITED(StyleRule, Rule)
-  NS_DECL_NSIDOMCSSRULE
   NS_DECL_NSIDOMCSSSTYLERULE
 
   
   NS_IMETHOD GetCSSStyleRule(StyleRule **aResult) override;
+
+  
+  uint16_t Type() const override;
+  void GetCssTextImpl(nsAString& aCssText) const override;
 
   
   nsCSSSelectorList* Selector() { return mSelector; }
@@ -340,6 +343,7 @@ public:
   void SetDeclaration(Declaration* aDecl);
 
   virtual int32_t GetType() const override;
+  using Rule::GetType;
 
   CSSStyleSheet* GetStyleSheet() const
   {
