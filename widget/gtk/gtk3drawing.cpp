cbd461b2a23063df7b7c687af046b2ab40c1b743
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 19:38:08 +0000

    Backed out changeset 9abd87cd2dff (bug 1343802)
    UltraBlame original commit: f387dcfd940fc00c1d47317b2d873ac0a33fb579

--- widget/gtk/gtk3drawing.cpp
+++ widget/gtk/gtk3drawing.cpp
@@ -2564,27 +2564,22 @@ GetScrollbarMetrics(GtkOrientation aOrientation)
     WidgetNodeType scrollbar = aOrientation == GTK_ORIENTATION_HORIZONTAL ?
         MOZ_GTK_SCROLLBAR_HORIZONTAL : MOZ_GTK_SCROLLBAR_VERTICAL;
 
-    gboolean backward, forward, secondary_backward, secondary_forward;
-    GtkStyleContext* style = ClaimStyleContext(scrollbar);
-    gtk_style_context_get_style(style,
-                                "has-backward-stepper", &backward,
-                                "has-forward-stepper", &forward,
-                                "has-secondary-backward-stepper",
-                                &secondary_backward,
-                                "has-secondary-forward-stepper",
-                                &secondary_forward, nullptr);
-    bool hasButtons =
-        backward || forward || secondary_backward || secondary_forward;
-
     if (gtk_get_minor_version() < 20) {
         gint slider_width, trough_border, stepper_size, min_slider_size;
+        gboolean backward, forward, secondary_backward, secondary_forward;
 
+        GtkStyleContext* style = ClaimStyleContext(scrollbar);
         gtk_style_context_get_style(style,
                                     "slider-width", &slider_width,
                                     "trough-border", &trough_border,
                                     "stepper-size", &stepper_size,
                                     "min-slider-length", &min_slider_size,
-                                    nullptr);
+                                    "has-backward-stepper", &backward,
+                                    "has-forward-stepper", &forward,
+                                    "has-secondary-backward-stepper",
+                                    &secondary_backward,
+                                    "has-secondary-forward-stepper",
+                                    &secondary_forward, nullptr);
         ReleaseStyleContext(style);
 
         metrics->size.thumb =
@@ -2594,6 +2589,8 @@ GetScrollbarMetrics(GtkOrientation aOrientation)
         
         gint breadth = slider_width + 2 * trough_border;
         
+        bool hasButtons = backward || forward ||
+            secondary_backward || secondary_forward;
         gint length = hasButtons ? 0 : min_slider_size + 2 * trough_border;
         metrics->size.scrollbar =
             SizeFromLengthAndBreadth(aOrientation, length, breadth);
@@ -2620,10 +2617,6 @@ GetScrollbarMetrics(GtkOrientation aOrientation)
     }
 
     
-    
-    metrics->border.scrollbar = GetMarginBorderPadding(style);
-    ReleaseStyleContext(style);
-
     WidgetNodeType contents, track, thumb;
     if (aOrientation == GTK_ORIENTATION_HORIZONTAL) {
         contents = MOZ_GTK_SCROLLBAR_CONTENTS_HORIZONTAL;
@@ -2637,44 +2630,20 @@ GetScrollbarMetrics(GtkOrientation aOrientation)
     
     metrics->size.thumb = GetMinMarginBox(thumb);
     
-    style = ClaimStyleContext(track);
+    GtkStyleContext* style = ClaimStyleContext(track);
     metrics->border.track = GetMarginBorderPadding(style);
     ReleaseStyleContext(style);
-    MozGtkSize trackSizeForThumb = metrics->size.thumb + metrics->border.track;
     
-    if (hasButtons) {
-        metrics->size.button = GetMinMarginBox(MOZ_GTK_SCROLLBAR_BUTTON);
-        
-        
-        
-        if (aOrientation == GTK_ORIENTATION_HORIZONTAL) {
-            gint extra = metrics->size.button.height - trackSizeForThumb.height;
-            if (extra > 0) {
-                
-                
-                metrics->border.track.top += extra / 2;
-                metrics->border.track.bottom += extra - extra / 2;
-                
-                trackSizeForThumb.height += extra;
-            }
-        } else {
-            gint extra = metrics->size.button.width - trackSizeForThumb.width;
-            if (extra > 0) {
-                
-                
-                metrics->border.track.left += extra / 2;
-                metrics->border.track.right += extra - extra / 2;
-                trackSizeForThumb.width += extra;
-            }
-        }
-    }
-
+    metrics->size.button = GetMinMarginBox(MOZ_GTK_SCROLLBAR_BUTTON);
+    
+    style = ClaimStyleContext(scrollbar);
+    metrics->border.scrollbar = GetMarginBorderPadding(style);
+    ReleaseStyleContext(style);
     style = ClaimStyleContext(contents);
     GtkBorder contentsBorder = GetMarginBorderPadding(style);
     ReleaseStyleContext(style);
-
-    metrics->size.scrollbar =
-        trackSizeForThumb + contentsBorder + metrics->border.scrollbar;
+    metrics->size.scrollbar = metrics->size.thumb + metrics->border.track +
+        contentsBorder + metrics->border.scrollbar;
 
     return metrics;
 }
