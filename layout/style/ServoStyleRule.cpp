7898689cde90deb13d9ba9bb103af90f5c07fd5d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 07:15:51 +0000

    Backed out changeset 3be6690b9f0a (bug 851892)
    UltraBlame original commit: 21fd8a323aa4c87d4d13ff96d252728565ac8b6d

--- layout/style/ServoStyleRule.cpp
+++ layout/style/ServoStyleRule.cpp
@@ -104,7 +104,6 @@ ServoStyleRule::ServoStyleRule(already_AddRefed<RawServoStyleRule> aRawRule)
   , mRawRule(aRawRule)
   , mDecls(Servo_StyleRule_GetStyle(mRawRule).Consume())
 {
-  SetIsNotDOMBinding();
 }
 
 
@@ -166,14 +165,6 @@ ServoStyleRule::SizeOfIncludingThis(MallocSizeOf aMallocSizeOf) const
 {
   
   return aMallocSizeOf(this);
-}
-
- JSObject*
-ServoStyleRule::WrapObject(JSContext* aCx,
-                           JS::Handle<JSObject*> aGivenProto)
-{
-  NS_NOTREACHED("We called SetIsNotDOMBinding() in our constructor");
-  return nullptr;
 }
 
 #ifdef DEBUG
