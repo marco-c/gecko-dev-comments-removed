fb934e69986c11573c2fc91ab3c5202e91750969
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 08:02:58 +0000

    Backed out changeset 38d9a0fa515a (bug 1322862)
    UltraBlame original commit: 1e8ed0e59c57de5dd137a07fce3cf489e6a8892e

--- testing/marionette/harness/marionette_harness/tests/unit/test_navigation.py
+++ testing/marionette/harness/marionette_harness/tests/unit/test_navigation.py
@@ -36,14 +36,7 @@ class TestNavigate(WindowManagerMixin, MarionetteTestCase):
 
     @property
     def location_href(self):
-        
-        
-        
-        
-        
-        
-        
-        return self.marionette.execute_script("return window.location.href").encode("utf-8")
+        return self.marionette.execute_script("return window.location.href")
 
     def test_set_location_through_execute_script(self):
         self.marionette.execute_script(
