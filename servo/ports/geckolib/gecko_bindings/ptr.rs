a53f01993b469f060ca8e979694b9c12317789ba
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 11:43:43 +0000

    servo: Merge #13370 - Rename GeckoArc macro so it sounds like it's useful for already-threadsafely-refcounted objects (from heycam:gecko-arc); r=Manishearth
    <!-- Please describe your changes on the following line: -->
    
    This is just some renaming.  I will want to use this macro with some objects that support thread-safe refcounting, and which thus don't use `nsMainThreadPtrHolder`.  For fun I pushed a stylo try run with this change: https://treeherder.mozilla.org/#/jobs?repo=try&revision=4b6d906b423e
    
    r? Manishearth
    
    ---
    <!-- Thank you for contributing to Servo! Please replace each `[ ]` by `[X]` when the step is complete, and replace `__` with appropriate data: -->
    - [ ] `./mach build -d` does not report any errors
    - [X] `./mach test-tidy` does not report any errors
    - [ ] These changes fix #__ (github issue number if applicable).
    
    <!-- Either: -->
    - [ ] There are tests for these changes OR
    - [X] These changes do not require tests because _____
    
    <!-- Pull requests that do not address these steps are welcome, but they will require additional verification as part of the review process. -->
    
    Source-Repo: https://github.com/servo/servo
    Source-Revision: ee3f916b652ad99e5925f574e7fa074dd3be9ad9
    
    UltraBlame original commit: 3ecf2dc28a94c93a7be2be67ce69e92738acd6ff

--- servo/ports/geckolib/gecko_bindings/ptr.rs
+++ servo/ports/geckolib/gecko_bindings/ptr.rs
@@ -9,15 +9,18 @@ use std::fmt::{self, Debug};
 
 
 
-macro_rules! define_holder_arc {
-    ($arc_type:ident, $name:ident, $holder_type:ident, $addref: ident, $release: ident) => (
+
+
+
+macro_rules! define_arc {
+    ($arc_type:ident, $name:ident, $gecko_type:ident, $addref: ident, $release: ident) => (
         #[derive(PartialEq)]
         pub struct $arc_type {
-            ptr: *mut $holder_type,
+            ptr: *mut $gecko_type,
         }
 
         impl $arc_type {
-            pub fn new(data: *mut $holder_type) -> $arc_type {
+            pub fn new(data: *mut $gecko_type) -> $arc_type {
                 debug_assert!(!data.is_null());
                 unsafe { $addref(data); }
                 $arc_type {
@@ -25,7 +28,7 @@ macro_rules! define_holder_arc {
                 }
             }
 
-            pub fn as_raw(&self) -> *mut $holder_type { self.ptr }
+            pub fn as_raw(&self) -> *mut $gecko_type { self.ptr }
         }
 
         unsafe impl Send for $arc_type {}
@@ -55,7 +58,7 @@ macro_rules! define_holder_arc {
     )
 }
 
-define_holder_arc!(GeckoArcPrincipal, Principal, ThreadSafePrincipalHolder,
-                   Gecko_AddRefPrincipalArbitraryThread, Gecko_ReleasePrincipalArbitraryThread);
-define_holder_arc!(GeckoArcURI, URI, ThreadSafeURIHolder,
-                   Gecko_AddRefURIArbitraryThread, Gecko_ReleaseURIArbitraryThread);
+define_arc!(GeckoArcPrincipal, Principal, ThreadSafePrincipalHolder,
+            Gecko_AddRefPrincipalArbitraryThread, Gecko_ReleasePrincipalArbitraryThread);
+define_arc!(GeckoArcURI, URI, ThreadSafeURIHolder,
+            Gecko_AddRefURIArbitraryThread, Gecko_ReleaseURIArbitraryThread);
