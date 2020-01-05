803294f996ca80c2835fce6932cad375db97f60b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 01:42:22 +0000

    Bug 1280883 - Avoid effects of ASI when preprocessing XBL for eslint. r=miker
    MozReview-Commit-ID: KozbJCGIpFo
    
    UltraBlame original commit: 4e5a650aca2541283ad8f8ced4466b1d9193a6a9

--- tools/lint/eslint/eslint-plugin-mozilla/lib/processors/xbl-bindings.js
+++ tools/lint/eslint/eslint-plugin-mozilla/lib/processors/xbl-bindings.js
@@ -261,10 +261,13 @@ module.exports = {
               }
 
               addSyntheticLine(indent(3) + `get ${item.attributes.name}() {`, item.textLine);
+              addSyntheticLine(indent(4) + `return (`, item.textLine);
+
               
-              
-              addSyntheticLine(indent(4) + `return`, item.textLine);
-              addNodeLines(item, 4);
+              item.textContent = item.textContent.replace(/;(?=\s*$)/, "");
+              addNodeLines(item, 5);
+
+              addSyntheticLine(indent(4) + `);`, item.textLine);
               addSyntheticLine(indent(3) + `},`, item.textEndLine);
               break;
             }
