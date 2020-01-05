f6019006f42c4b7e29adb68a48a99e6f7e53b5dc
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 11:32:28 +0000

    servo: Merge #12469 - style: Rewrite the restyle hints code to allow different kinds of element snapshots (from emilio:stylo); r=bholley
    <!-- Please describe your changes on the following line: -->
    
    ---
    <!-- Thank you for contributing to Servo! Please replace each `[ ]` by `[X]` when the step is complete, and replace `__` with appropriate data: -->
    - [x] `./mach build -d` does not report any errors
    - [x] `./mach test-tidy` does not report any errors
    
    <!-- Either: -->
    - [x] These changes do not require tests because refactoring.
    
    <!-- Pull requests that do not address these steps are welcome, but they will require additional verification as part of the review process. -->
    
    This is a rewrite for how style interfaces with its consumers in order to allow
    different representations for an element snapshot.
    
    This also changes the requirements of an element snapshot, requiring them to
    only implement MatchAttr, instead of MatchAttrGeneric. This is important for
    stylo since implementing MatchAttrGeneric is way more difficult for us given the
    atom limitations. This also allows for more performant implementations in the
    Gecko side of things.
    
    I don't want to get this merged just yet, mainly because the stylo part is not
    implemented, but I'd like early feedback from bholley and/or heycam: How do
    you see this approach? I don't think we'll have much problem to implement
    MatchAttr for our element snapshots, but... worth checking.
    
    r? heycam
    
    Source-Repo: https://github.com/servo/servo
    Source-Revision: 1e0321f7dde5f33f7d26bbd4f088622fa3660477
    
    UltraBlame original commit: 6bbba3a67511db235ec2509ba9ffe13f2ee0fcb1

--- servo/components/style/selector_impl.rs
+++ servo/components/style/selector_impl.rs
@@ -5,6 +5,7 @@
 
 
 use element_state::ElementState;
+use restyle_hints;
 use selectors::Element;
 use selectors::parser::SelectorImpl;
 use std::fmt::Debug;
@@ -13,13 +14,16 @@ use stylesheets::Stylesheet;
 pub type AttrString = <TheSelectorImpl as SelectorImpl>::AttrString;
 
 #[cfg(feature = "servo")]
-pub use servo_selector_impl::ServoSelectorImpl;
+pub use servo_selector_impl::*;
 
 #[cfg(feature = "servo")]
-pub use servo_selector_impl::{ServoSelectorImpl as TheSelectorImpl, PseudoElement, NonTSPseudoClass};
+pub use servo_selector_impl::{ServoSelectorImpl as TheSelectorImpl, ServoElementSnapshot as ElementSnapshot};
 
 #[cfg(feature = "gecko")]
-pub use gecko_selector_impl::{GeckoSelectorImpl as TheSelectorImpl, PseudoElement, NonTSPseudoClass};
+pub use gecko_selector_impl::*;
+
+#[cfg(feature = "gecko")]
+pub use gecko_selector_impl::{GeckoSelectorImpl as TheSelectorImpl};
 
 
 
@@ -69,6 +73,8 @@ impl PseudoElementCascadeType {
 }
 
 pub trait ElementExt: Element<Impl=TheSelectorImpl, AttrString=<TheSelectorImpl as SelectorImpl>::AttrString> {
+    type Snapshot: restyle_hints::ElementSnapshot<AttrString = Self::AttrString> + 'static;
+
     fn is_link(&self) -> bool;
 }
 
