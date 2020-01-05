38087e09fe0a7609dfda306bfe4140c526c0c01a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 04:12:41 +0000

    Bug 1175823 - Import test262 mapped arguments tests. r=anba
    UltraBlame original commit: 3200aaa75fc28bafdbcaa5abdd3e511831b0035b

---
+++ js/src/tests/test262/language/arguments-object/mapped/mapped-arguments-nonconfigurable-delete-1.js
@@ -0,0 +1,19 @@
+
+
+
+
+
+
+
+
+
+
+
+function argumentsAndDelete(a) {
+  Object.defineProperty(arguments, "0", {configurable: false});
+
+  assert.sameValue(delete arguments[0], false);
+  assert.sameValue(a, 1);
+  assert.sameValue(arguments[0], 1);
+}
+argumentsAndDelete(1);
