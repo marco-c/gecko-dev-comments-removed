59faa483057e3d96d9837cae9023385a7badfe6f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 08:38:37 +0000

    servo: Convert linebox scanner and other bits to use character indices instead of string byte offsets. Fix some bugs in set_char_break_before, and remove dead inline tests.
    Source-Repo: https://github.com/servo/servo
    Source-Revision: b2f8228b10867ca0ccd0ce2ee77e919bf6b6804c
    
    UltraBlame original commit: b6fce4ef97b8db16f352cf7025325e9d31475c79

--- servo/src/servo-gfx/text/text_run.rs
+++ servo/src/servo-gfx/text/text_run.rs
@@ -123,8 +123,6 @@ impl TextRun {
     }
 
     fn min_width_for_range(&self, range: &const Range) -> Au {
-        assert range.is_valid_for_string(self.text);
-
         let mut max_piece_width = Au(0);
         for self.iter_indivisible_pieces_for_range(range) |piece_range| {
             let metrics = self.font.measure_text(self, piece_range);
@@ -134,12 +132,10 @@ impl TextRun {
     }
 
     fn iter_natural_lines_for_range(&self, range: &const Range, f: fn(&const Range) -> bool) {
-        assert range.is_valid_for_string(self.text);
-
         let mut clump = Range::new(range.begin(), 0);
         let mut in_clump = false;
 
-        // clump non-linebreaks of nonzero length
+        
         for range.eachi |i| {
             match (self.glyphs.char_is_newline(i), in_clump) {
                 (false, true)  => { clump.extend_by(1); }
@@ -147,14 +143,13 @@ impl TextRun {
                 (true, false) => { /* chomp whitespace */ }
                 (true, true)  => {
                     in_clump = false;
-                    // don't include the linebreak 'glyph'
-                    // (we assume there's one GlyphEntry for a newline, and no actual glyphs)
+                    
                     if !f(&const clump) { break }
                 }
             }
         }
         
-        // flush any remaining chars as a line
+        
         if in_clump {
             clump.extend_to(range.end());
             f(&const clump);
@@ -162,86 +157,21 @@ impl TextRun {
     }
 
     fn iter_indivisible_pieces_for_range(&self, range: &const Range, f: fn(&const Range) -> bool) {
-        assert range.is_valid_for_string(self.text);
-
         let mut clump = Range::new(range.begin(), 0);
+
         loop {
-            // find next non-whitespace byte index, then clump all whitespace before it.
-            match str::find_between(self.text, clump.begin(), range.end(), |c| !char::is_whitespace(c)) {
-                Some(nonws_char_offset) => {
-                    clump.extend_to(nonws_char_offset);
-                    if !f(&const clump) { break }
-                    clump.reset(clump.end(), 0);
-                },
-                None => {
-                    // nothing left, flush last piece containing only whitespace
-                    if clump.end() < range.end() {
-                        clump.extend_to(range.end());
-                        f(&const clump);
-                        break;
-                    }
-                }
-            };
+            
+            while clump.end() < range.end() 
+                && self.glyphs.can_break_before(clump.end()) != BreakTypeNormal {
 
-            // find next whitespace byte index, then clump all non-whitespace before it.
-            match str::find_between(self.text, clump.begin(), range.end(), |c| char::is_whitespace(c)) {
-                Some(ws_char_offset) => {
-                    clump.extend_to(ws_char_offset);
-                    if !f(&const clump) { break }
-                    clump.reset(clump.end(), 0);
-                }
-                None => {
-                    // nothing left, flush last piece containing only non-whitespaces
-                    if clump.end() < range.end() {
-                        clump.extend_to(range.end());
-                        f(&const clump);
-                        break;
-                    }
-                }
+                clump.extend_by(1);
             }
+
+            
+            if !f(&const clump) || clump.end() == range.end() { break; }
+
+            
+            clump.reset(clump.end(), 1);
         }
     }
 }
- 
-// this test can't run until LayoutContext is removed as an argument
-// to min_width_for_range.
-/*
-#[test]
-fn test_calc_min_break_width() {
-
-    fn test_min_width_for_run(text: ~str, width: Au) {
-        let flib = FontCache();
-        let font = flib.get_test_font();
-        let run = TextRun(font, text);
-        run.min_width_for_range(0, text.len())
-    }
-
-    test_min_width_for_run(~"firecracker", au::from_px(84));
-    test_min_width_for_run(~"firecracker yumyum", au::from_px(84));
-    test_min_width_for_run(~"yumyum firecracker", au::from_px(84));
-    test_min_width_for_run(~"yumyum firecracker yumyum", au::from_px(84));
-}
-*/
-
-/*#[test]
-#[ignore]
-fn test_iter_indivisible_pieces() {
-    fn test_pieces(text: ~str, res: ~[~str]) {
-        let flib = FontCache();
-        let font = flib.get_test_font();
-        let run = TextRun::new(font, copy text);
-        let mut slices : ~[~str] = ~[];
-        for run.iter_indivisible_pieces_for_range(Range(0, text.len())) |subrange| {
-            slices.push(str::slice(text, subrange.begin(), subrange.length()));
-        }
-        assert slices == res;
-    }
-
-    test_pieces(~"firecracker yumyum woopwoop", ~[~"firecracker", ~" ", ~"yumyum", ~" ", ~"woopwoop"]);
-    test_pieces(~"firecracker yumyum     ", ~[~"firecracker", ~" ", ~"yumyum", ~"     "]);
-    test_pieces(~"     firecracker yumyum", ~[~"     ", ~"firecracker", ~" ", ~"yumyum"]);
-    test_pieces(~"  ", ~[~"  "]);
-    test_pieces(~"", ~[]);
-}
-
-*/
