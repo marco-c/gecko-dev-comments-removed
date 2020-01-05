66ace19795130b7806800458a79528a60a777aae
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 01:37:05 +0000

    Add GPU process type to leaklog.py. (bug 1310110, r=mccr8)
    UltraBlame original commit: bb80294893738efd57ab7f172ae56ef1705bce1e

--- testing/mozbase/mozleak/mozleak/leaklog.py
+++ testing/mozbase/mozleak/mozleak/leaklog.py
@@ -184,7 +184,7 @@ def process_leak_log(leak_log_file, leak_thresholds=None,
 
     
     
-    knownProcessTypes = ["default", "plugin", "tab", "geckomediaplugin"]
+    knownProcessTypes = ["default", "plugin", "tab", "geckomediaplugin", "gpu"]
 
     for processType in knownProcessTypes:
         log.info("TEST-INFO | leakcheck | %s process: leak threshold set at %d bytes"
