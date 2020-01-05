649de03279f8afe6ffdeb75fdca37e9b72e01920
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 08:08:48 +0000

    Bug 835981 part 2.  Switch nsIDOMXULElement::GetDatabase consumers to nsXULElement instead.  r=peterv
    UltraBlame original commit: c32788f892f37801f68fdadfd73ac38b5299b33a

--- dom/xul/XULDocument.cpp
+++ dom/xul/XULDocument.cpp
@@ -3647,10 +3647,9 @@ XULDocument::CheckTemplateBuilderHookup(nsIContent* aElement,
     
     
     
-    nsCOMPtr<nsIDOMXULElement> xulElement = do_QueryInterface(aElement);
+    RefPtr<nsXULElement> xulElement = nsXULElement::FromContent(aElement);
     if (xulElement) {
-        nsCOMPtr<nsIRDFCompositeDataSource> ds;
-        xulElement->GetDatabase(getter_AddRefs(ds));
+        nsCOMPtr<nsIRDFCompositeDataSource> ds = xulElement->GetDatabase();
         if (ds) {
             *aNeedsHookup = false;
             return NS_OK;
