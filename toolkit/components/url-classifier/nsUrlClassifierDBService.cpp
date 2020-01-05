694f75fb481a4b2797f82cd242be2ecf0b158cd4
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 21:02:23 +0000

    Bug 1311933 - P2. Add telemetry to measure if completion match type is the same for v2 and v4. r=francois
    When full match is found in both v2 and v4, the threat types returned should also be the same.
    If threat types are different, the telemetry record this by setting a bit flags which indicates
    what threat types are being returned.
    
    If threat types are the same, this telemetry will record 0.
    
    MozReview-Commit-ID: Laz77yoCg00
    
    UltraBlame original commit: aa409a18a2fe67d3303f1c72f2b98bfa34e81099

--- toolkit/components/url-classifier/nsUrlClassifierDBService.cpp
+++ toolkit/components/url-classifier/nsUrlClassifierDBService.cpp
@@ -55,6 +55,7 @@
 #include "mozilla/dom/URLClassifierChild.h"
 #include "mozilla/ipc/URIUtils.h"
 #include "nsProxyRelease.h"
+#include "SBTelemetryUtils.h"
 
 namespace mozilla {
 namespace safebrowsing {
@@ -1283,27 +1284,6 @@ nsUrlClassifierLookupCallback::ProcessComplete(CacheResult* aCacheResult)
   return NS_OK;
 }
 
-
-static uint8_t
-ConvertMatchResultToUint(const MatchResult& aResult)
-{
-  MOZ_ASSERT(!(aResult & MatchResult::eTelemetryDisabled));
-  switch (aResult) {
-  case MatchResult::eNoMatch:          return 0;
-  case MatchResult::eV2Prefix:         return 1;
-  case MatchResult::eV4Prefix:         return 2;
-  case MatchResult::eBothPrefix:       return 3;
-  case MatchResult::eAll:              return 4;
-  case MatchResult::eV2PreAndCom:      return 5;
-  case MatchResult::eV4PreAndCom:      return 6;
-  case MatchResult::eBothPreAndV2Com:  return 7;
-  case MatchResult::eBothPreAndV4Com:  return 8;
-  default:
-    MOZ_ASSERT_UNREACHABLE("Unexpected match result");
-    return 9;
-  }
-}
-
 nsresult
 nsUrlClassifierLookupCallback::HandleResults()
 {
@@ -1380,9 +1360,43 @@ nsUrlClassifierLookupCallback::HandleResults()
     }
   }
 
+  
+  if (matchResult == MatchResult::eAll && mCacheResults) {
+    MatchThreatType types = MatchThreatType::eIdentical;
+
+    
+    for (uint32_t i = 0; i < mCacheResults->Length(); i++) {
+      CacheResult* c = mCacheResults->ElementAt(i).get();
+      for (LookupResult& l : *(mResults.get())) {
+        if (l.hash.fixedLengthPrefix != c->prefix) {
+          continue;
+        }
+
+        
+        if (l.Confirmed()) {
+          types |= TableNameToThreatType(CacheResult::V2 == c->Ver(), c->table);
+        }
+        break;
+      }
+    }
+
+    auto fnIsMatchSameThreatType = [&](const MatchThreatType& aTypeMask) {
+      uint8_t val = static_cast<uint8_t>(types & aTypeMask);
+      return val == 0 || val == static_cast<uint8_t>(aTypeMask);
+    };
+    if (fnIsMatchSameThreatType(MatchThreatType::ePhishingMask) &&
+        fnIsMatchSameThreatType(MatchThreatType::eMalwareMask) &&
+        fnIsMatchSameThreatType(MatchThreatType::eUnwantedMask)) {
+      types = MatchThreatType::eIdentical;
+    }
+
+    Telemetry::Accumulate(Telemetry::URLCLASSIFIER_MATCH_THREAT_TYPE_RESULT,
+                          static_cast<uint8_t>(types));
+  }
+
   if (matchResult != MatchResult::eTelemetryDisabled) {
     Telemetry::Accumulate(Telemetry::URLCLASSIFIER_MATCH_RESULT,
-                          ConvertMatchResultToUint(matchResult));
+                          MatchResultToUint(matchResult));
   }
 
   
