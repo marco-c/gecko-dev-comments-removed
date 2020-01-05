4d5229b4c12646a36d89f5a2c71f9cebdc2160d1
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 17:28:37 +0000

    Bug 1343428 - Label runnables in dom/media/MediaDecoder.cpp. r=gerald
    MozReview-Commit-ID: Jv6iXO9ZxZH
    
    UltraBlame original commit: 30601e17a9251d4e8d51d0dbbfaf43f5da085839

--- dom/media/MediaDecoder.cpp
+++ dom/media/MediaDecoder.cpp
@@ -63,6 +63,8 @@ LazyLogModule gMediaDecoderLog("MediaDecoder");
 #define DUMP_LOG(x, ...) \
   NS_DebugBreak(NS_DEBUG_WARNING, nsPrintfCString("Decoder=%p " x, this, ##__VA_ARGS__).get(), nullptr, nullptr, -1)
 
+#define NS_DispatchToMainThread(...) CompileError_UseAbstractMainThreadInstead
+
 static const char*
 ToPlayStateStr(MediaDecoder::PlayState aState)
 {
@@ -154,6 +156,7 @@ MediaDecoder::ResourceCallback::Connect(MediaDecoder* aDecoder)
   MOZ_ASSERT(NS_IsMainThread());
   mDecoder = aDecoder;
   mTimer = do_CreateInstance("@mozilla.org/timer;1");
+  mTimer->SetTarget(mAbstractMainThread->AsEventTarget());
 }
 
 void
@@ -1815,7 +1818,7 @@ MediaDecoder::DumpDebugInfo()
 
   RefPtr<MediaDecoder> self = this;
   GetStateMachine()->RequestDebugInfo()->Then(
-    AbstractThread::MainThread(), __func__,
+    SystemGroup::AbstractMainThreadFor(TaskCategory::Other), __func__,
     [this, self, str] (const nsACString& aString) {
       DUMP_LOG("%s", str.get());
       DUMP_LOG("%s", aString.Data());
@@ -1836,7 +1839,7 @@ MediaDecoder::RequestDebugInfo()
   }
 
   return GetStateMachine()->RequestDebugInfo()->Then(
-    AbstractThread::MainThread(), __func__,
+    SystemGroup::AbstractMainThreadFor(TaskCategory::Other), __func__,
     [str] (const nsACString& aString) {
       nsCString result = str + nsCString("\n") + aString;
       return DebugInfoPromise::CreateAndResolve(result, __func__);
@@ -1872,3 +1875,4 @@ MediaMemoryTracker::~MediaMemoryTracker()
 
 
 #undef DECODER_LOG
+#undef NS_DispatchToMainThread
