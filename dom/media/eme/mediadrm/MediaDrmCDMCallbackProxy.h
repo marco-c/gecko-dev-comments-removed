1c40fc119b9bb45c793926d96013ab7bc5658e52
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 03:28:36 +0000

    Bug 1306314 - Add an ID to GMPDecryptor instances, reflect that on CDMProxy. r=gerald
    This enables us to identify GMPDecryptor instances in the child process, so that
    in a later patch when we create a GMPVideoDecoder instance, we can associate it
    with a GMPDecryptor. Then the cdm::ContentDecryptionModule8 instance that these
    two actors are adapted to can know whom it's supposed to respond to.
    
    We use the IPDL PGMPDecryptorChild actor ID as the GMPDecryptor's ID. This is unique
    per GMP process, which is sufficient.
    
    MozReview-Commit-ID: 7NKND9VjPUW
    
    UltraBlame original commit: 9e9727bdc48497378e2dfb846a7f9aca2d25ef5c

--- dom/media/eme/mediadrm/MediaDrmCDMCallbackProxy.h
+++ dom/media/eme/mediadrm/MediaDrmCDMCallbackProxy.h
@@ -17,6 +17,9 @@ class CDMProxy;
 
 class MediaDrmCDMCallbackProxy : public DecryptorProxyCallback {
 public:
+
+  void SetDecryptorId(uint32_t aId) override {}
+
   void SetSessionId(uint32_t aCreateSessionToken,
                     const nsCString& aSessionId) override;
 
