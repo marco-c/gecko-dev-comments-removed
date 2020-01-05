02e68cd0e8764836fb99be57235baa885368d156
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 17:19:40 +0000

    Backed out changeset 2873be3659a3 (bug 1335873)
    UltraBlame original commit: 90ac7081a45fbccba3cb35e0b49f098b50d0f11c

--- build/mach_bootstrap.py
+++ build/mach_bootstrap.py
@@ -408,8 +408,7 @@ class ImportHook(object):
         
         
         if not os.path.exists(module.__file__[:-1]):
-            if os.path.exists(module.__file__):
-                os.remove(module.__file__)
+            os.remove(module.__file__)
             del sys.modules[module.__name__]
             module = self(name, globals, locals, fromlist, level)
 
