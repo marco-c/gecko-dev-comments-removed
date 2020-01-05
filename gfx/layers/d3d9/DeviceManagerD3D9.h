ef4927906d0bfa292b239b54e2280cd74757b12c
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 05:35:20 +0000

    Bug 1322465 part 5 - Use explicit/MOZ_IMPLICIT for the unary constructors in gfx/. r=Ehsan
    MozReview-Commit-ID: 5sdclirTQCV
    
    UltraBlame original commit: 65136c629a8b01a9d56e29262ba5272f221bbebe

--- gfx/layers/d3d9/DeviceManagerD3D9.h
+++ gfx/layers/d3d9/DeviceManagerD3D9.h
@@ -107,11 +107,11 @@ public:
 private:
   friend class DeviceManagerD3D9;
 
-  SwapChainD3D9(DeviceManagerD3D9 *aDeviceManager);
+  explicit SwapChainD3D9(DeviceManagerD3D9 *aDeviceManager);
 
   
   ~SwapChainD3D9();
-  
+
   bool Init(HWND hWnd);
 
   
