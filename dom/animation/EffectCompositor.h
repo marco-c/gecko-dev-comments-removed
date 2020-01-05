ffbe5c9a315e37c4cbec8d161860ab6e7e7419cf
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 16:24:35 +0000

    Bug 1331704 - Part 3: Drop all of unused stuff. r=birtles a=abillings
    MozReview-Commit-ID: BWAC0iZLw6t
    
    UltraBlame original commit: 0077d103e8629294d9ffbb7cf4a7cfc6fd4fba5b

--- dom/animation/EffectCompositor.h
+++ dom/animation/EffectCompositor.h
@@ -233,25 +233,6 @@ public:
     nsCSSPropertyID aProperty,
     const AnimationPerformanceWarning& aWarning);
 
-  
-  
-  
-  
-  static StyleAnimationValue GetBaseStyle(nsCSSPropertyID aProperty,
-                                          nsStyleContext* aStyleContext,
-                                          dom::Element& aElement,
-                                          CSSPseudoElementType aPseudoType);
-
-  
-  
-  
-  static StyleAnimationValue GetBaseStyle(nsCSSPropertyID aProperty,
-                                          const nsIFrame* aFrame);
-
-  
-  static void ClearBaseStyles(dom::Element& aElement,
-                              CSSPseudoElementType aPseudoType);
-
 private:
   ~EffectCompositor() = default;
 
