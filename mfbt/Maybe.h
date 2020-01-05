61c5f8307912443ef8ed3fde8f9ed24a71cc6f77
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 05:49:00 +0000

    Backed out changeset 79754b9b89b7 (bug 1325351)
    UltraBlame original commit: d9c389a8bcbf43f32df70eddda5d2d564cd90e12

--- mfbt/Maybe.h
+++ mfbt/Maybe.h
@@ -438,12 +438,11 @@ public:
 
 
 
-template<typename T,
-         typename U = typename std::remove_cv<
-           typename std::remove_reference<T>::type>::type>
-Maybe<U>
+template<typename T>
+Maybe<typename RemoveCV<typename RemoveReference<T>::Type>::Type>
 Some(T&& aValue)
 {
+  typedef typename RemoveCV<typename RemoveReference<T>::Type>::Type U;
   Maybe<U> value;
   value.emplace(Forward<T>(aValue));
   return value;
