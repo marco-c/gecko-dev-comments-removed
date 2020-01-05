044795f54153637144ffd3f9e1ea0deed67f39d1
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 11:52:07 +0000

    servo: Merge #13886 - Adds parsing test for linear-gradient (from AVGP:13837-linear-gradient-tests); r=canaltinova
    <!-- Please describe your changes on the following line: -->
    Tests if `linear-gradient` is correctly parsed without `<side-or-corner>` as well as with `to left` and `to right`.
    
    ---
    <!-- Thank you for contributing to Servo! Please replace each `[ ]` by `[X]` when the step is complete, and replace `__` with appropriate data: -->
    - [X] `./mach build -d` does not report any errors
    - [X] `./mach test-tidy` does not report any errors
    - [X] These changes fix #13837 (github issue number if applicable).
    
    <!-- Either: -->
    - [X] There are tests for these changes OR
    - [ ] These changes do not require tests because _____
    
    <!-- Pull requests that do not address these steps are welcome, but they will require additional verification as part of the review process. -->
    
    Source-Repo: https://github.com/servo/servo
    Source-Revision: f7b0ef343a796a3bd67e05cee1bbd7117191d9b8
    
    UltraBlame original commit: 80e58da3350c188381eb1e9ea3739d5613c89e98

--- servo/tests/unit/style/parsing/image.rs
+++ servo/tests/unit/style/parsing/image.rs
@@ -10,6 +10,36 @@ use style::values::specified::image::*;
 use url::Url;
 
 #[test]
+fn test_linear_gradient() {
+    
+    assert_roundtrip_with_context!(Image::parse, "linear-gradient(to left, red, green)",
+                                                 "linear-gradient(4.712389rad, red, green)");
+
+    
+    assert_roundtrip_with_context!(Image::parse, "linear-gradient(to right, red, green)",
+                                                 "linear-gradient(1.5707964rad, red, green)");
+
+    
+    assert_roundtrip_with_context!(Image::parse, "linear-gradient(to right top, red, green)");
+
+    
+    assert_roundtrip_with_context!(Image::parse, "linear-gradient(45deg, red, green)",
+                                                 "linear-gradient(0.7853982rad, red, green)");
+
+   
+   assert_roundtrip_with_context!(Image::parse, "linear-gradient(red, yellow, green)",
+                                                "linear-gradient(3.1415927rad, red, yellow, green)");
+
+   
+   assert_roundtrip_with_context!(Image::parse, "linear-gradient(red, green, yellow 50%)",
+                                                "linear-gradient(3.1415927rad, red, green, yellow 50%)");
+
+    
+    assert_roundtrip_with_context!(Image::parse, "linear-gradient(red, green)",
+                                                 "linear-gradient(3.1415927rad, red, green)");
+}
+
+#[test]
 fn test_radial_gradient() {
     
     assert_roundtrip_with_context!(Image::parse, "radial-gradient(circle closest-side at 20px 30px, red, green)");
