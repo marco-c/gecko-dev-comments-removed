34e89785d3b6426e81bdea2cb7281b719bae7de6
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 00:40:46 +0000

    Bug 1290337 - Part 5: Replace Value parameter to const Value& in JIT. r=nbp
    UltraBlame original commit: 9f0b9ebc2f1fa06820db74823ebe9179f8e627cf

--- js/src/jit/JitFrameIterator.h
+++ js/src/jit/JitFrameIterator.h
@@ -378,7 +378,7 @@ struct MaybeReadFallback
         return UndefinedValue();
     }
 
-    NoGCValue noGCPlaceholder(Value v) const {
+    NoGCValue noGCPlaceholder(const Value& v) const {
         if (v.isMagic(JS_OPTIMIZED_OUT))
             return NoGC_MagicOptimizedOut;
         return NoGC_UndefinedValue;
@@ -445,7 +445,7 @@ class SnapshotIterator
 
     Value allocationValue(const RValueAllocation& a, ReadMethod rm = RM_Normal);
     MOZ_MUST_USE bool allocationReadable(const RValueAllocation& a, ReadMethod rm = RM_Normal);
-    void writeAllocationValuePayload(const RValueAllocation& a, Value v);
+    void writeAllocationValuePayload(const RValueAllocation& a, const Value& v);
     void warnUnreadableAllocation();
 
   private:
@@ -477,7 +477,7 @@ class SnapshotIterator
 
     
     
-    void storeInstructionResult(Value v);
+    void storeInstructionResult(const Value& v);
 
   public:
     
@@ -651,7 +651,7 @@ class InlineFrameIterator
 
   private:
     void findNextFrame();
-    JSObject* computeEnvironmentChain(Value envChainValue, MaybeReadFallback& fallback,
+    JSObject* computeEnvironmentChain(const Value& envChainValue, MaybeReadFallback& fallback,
                                       bool* hasInitialEnv = nullptr) const;
 
   public:
