a8139e24bfd3a7f8f9465dbd645e371b9ecef4b2
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 21:59:12 +0000

    Backed out changeset a5a77d4a3cf3 (bug 1302470)
    UltraBlame original commit: 23656266698b0e59000c3b0fbe3b906a632ca3bd

--- toolkit/components/typeaheadfind/nsTypeAheadFind.cpp
+++ toolkit/components/typeaheadfind/nsTypeAheadFind.cpp
@@ -1210,22 +1210,22 @@ nsTypeAheadFind::IsRangeVisible(nsIPresShell *aPresShell,
     return false;
 
   nsIFrame *frame = content->GetPrimaryFrame();
-  if (!frame) {
+  if (!frame)    
     return false;  
-  }
 
-  if (!frame->StyleVisibility()->IsVisible()) {
+  if (!frame->StyleVisibility()->IsVisible())
     return false;
-  }
 
   
   
   if (aUsesIndependentSelection) {
-    *aUsesIndependentSelection =
+    *aUsesIndependentSelection = 
       (frame->GetStateBits() & NS_FRAME_INDEPENDENT_SELECTION);
   }
 
   
+  if (!aMustBeInViewPort)   
+    return true; 
 
   
   int32_t startRangeOffset, startFrameOffset, endFrameOffset;
@@ -1259,18 +1259,8 @@ nsTypeAheadFind::IsRangeVisible(nsIPresShell *aPresShell,
                                     minDistance);
 
     if (rectVisibility != nsRectVisibility_kAboveViewport) {
-      
-      
-      return IsRangeRendered(aPresShell, aPresContext, aRange);
+      return true;
     }
-  }
-
-  
-
-  if (!aMustBeInViewPort) {
-    
-    
-    return true;
   }
 
   
@@ -1288,9 +1278,8 @@ nsTypeAheadFind::IsRangeVisible(nsIPresShell *aPresShell,
                             false  
                             );
 
-  if (!frameTraversal) {
+  if (!frameTraversal)
     return false;
-  }
 
   while (rectVisibility == nsRectVisibility_kAboveViewport) {
     frameTraversal->Next();
