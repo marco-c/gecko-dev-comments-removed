53da9f090e12985ff2d3d762649543129f1255a8
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 10:39:18 +0000

    servo: Merge #8216 - M1502: Improve HTTP monitoring devtool support (from akumar21NCSU:master); r=jdm
    Source-Repo: https://github.com/servo/servo
    Source-Revision: 45f07ec320b77ef8fa6511c9dc12598bc5d0f5e0
    
    UltraBlame original commit: 3eaa7246d5dfe1b592531e03a51546e2be16c0ff

--- servo/components/script/document_loader.rs
+++ servo/components/script/document_loader.rs
@@ -78,6 +78,7 @@ impl DocumentLoader {
         pending.load_async(listener)
     }
 
+
     
     pub fn finish_load(&mut self, load: LoadType) {
         let idx = self.blocking_loads.iter().position(|unfinished| *unfinished == load);
