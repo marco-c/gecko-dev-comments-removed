33b4193236d209852c83a9e9b00b76192b0d8adf
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 19:59:25 +0000

    Bug 1348854 Don't show permissions prompt for legacy addons installed from search r=rhelmer
    MozReview-Commit-ID: 16eDuGvFM2Y
    
    UltraBlame original commit: 43c03ad7e9c15ecea7a549a4f49a604de81f48d9

--- toolkit/mozapps/extensions/test/browser/browser_searching.js
+++ toolkit/mozapps/extensions/test/browser/browser_searching.js
@@ -552,8 +552,6 @@ add_test(function() {
     installBtn = get_install_button(remoteItem);
     is(installBtn.hidden, false, "Install button should be showing before install");
     remoteItem.mAddon.install.addListener(listener);
-    
-    promiseNotification();
     EventUtils.synthesizeMouseAtCenter(installBtn, { }, gManagerWindow);
   });
 });
