55e01c3810b61bed44d265f7ba25dc0d4a9585ca
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 04:42:14 +0000

    Backed out changeset 5b70ccd12b42 (bug 1305832) for webgl failures
    MozReview-Commit-ID: F42Iwt1Rblt
    
    UltraBlame original commit: 0a3b96cee2eaeeaabd71ce228c25d9d555e2e04a

--- dom/canvas/WebGLContext.cpp
+++ dom/canvas/WebGLContext.cpp
@@ -2256,8 +2256,6 @@ ZeroTexImageWithClear(WebGLContext* webgl, GLContext* gl, TexImageTarget target,
 
     {
         gl::GLContext::LocalErrorScope errorScope(*gl);
-        MOZ_ASSERT(target != LOCAL_GL_TEXTURE_2D_ARRAY &&
-                   target != LOCAL_GL_TEXTURE_3D);
         gl->fFramebufferTexture2D(LOCAL_GL_FRAMEBUFFER, attachPoint, target.get(), tex,
                                   level);
         if (errorScope.GetError()) {
@@ -2365,12 +2363,18 @@ ZeroTextureData(WebGLContext* webgl, const char* funcName, GLuint tex,
     const auto driverUnpackInfo = usage->idealUnpack;
     MOZ_RELEASE_ASSERT(driverUnpackInfo, "GFX: ideal unpack info not set.");
 
-    if (!webgl->IsWebGL2() && usage->format->d) {
+    if (usage->IsRenderable() && depth == 1 &&
+        !xOffset && !yOffset && !zOffset)
+    {
         
-        const bool success = ZeroTexImageWithClear(webgl, gl, target, tex, level, usage,
-                                                   width, height);
-        MOZ_ASSERT(success);
-        return success;
+        
+        do {
+            if (ZeroTexImageWithClear(webgl, gl, target, tex, level, usage, width,
+                                      height))
+            {
+                return true;
+            }
+        } while (false);
     }
 
     const webgl::PackingInfo packing = driverUnpackInfo->ToPacking();
