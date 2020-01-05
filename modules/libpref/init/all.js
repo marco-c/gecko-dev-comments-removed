854328589ffffd0e05af4b46b0a4d2dc7d8b7f50
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 03:32:25 +0000

    Backed out changeset a8be4ebc85cf (bug 1313595) for permaorange unexpected assertion in test_referrerdirective.html, a=backout
    MozReview-Commit-ID: GxBqDrHHg7z
    
    UltraBlame original commit: 5e76768327660437bf3486554ad318e4b70276e1

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -5545,9 +5545,6 @@ pref("security.mixed_content.use_hsts", true);
 pref ("security.mixed_content.hsts_priming_cache_timeout", 10080);
 
 
-pref ("security.mixed_content.hsts_priming_request_timeout", 3000);
-
-
 #ifdef NIGHTLY_BUILD
 pref("dom.storageManager.enabled", true);
 #else
