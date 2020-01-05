1176c9f687a4040654953cbaec2588e5155ae6b4
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 08:51:51 +0000

    servo: Merge #1138 - Implement HTMLUnknownElement::new (from Ms2ger:reflect-HTMLUnknownElement); r=jdm
    jdm: I think we should move nodes more in line with the rest of the DOM. How do you feel about something like this?
    
    Source-Repo: https://github.com/servo/servo
    Source-Revision: 2c3d5ec79f62744be530d6bc9ea7210a1116c828
    
    UltraBlame original commit: 102ecb61afed18008c9d0a03075bb20d9ac60b23

--- servo/src/components/script/dom/htmlunknownelement.rs
+++ servo/src/components/script/dom/htmlunknownelement.rs
@@ -2,8 +2,25 @@
 
 
 
+use dom::bindings::codegen::HTMLUnknownElementBinding;
+use dom::document::AbstractDocument;
+use dom::element::HTMLUnknownElementTypeId;
 use dom::htmlelement::HTMLElement;
+use dom::node::{AbstractNode, Node, ScriptView};
 
 pub struct HTMLUnknownElement {
     htmlelement: HTMLElement
+}
+
+impl HTMLUnknownElement {
+    pub fn new_inherited(localName: ~str, document: AbstractDocument) -> HTMLUnknownElement {
+        HTMLUnknownElement {
+            htmlelement: HTMLElement::new(HTMLUnknownElementTypeId, localName, document)
+        }
+    }
+
+    pub fn new(localName: ~str, document: AbstractDocument) -> AbstractNode<ScriptView> {
+        let element = HTMLUnknownElement::new_inherited(localName, document);
+        Node::reflect_node(@mut element, document, HTMLUnknownElementBinding::Wrap)
+    }
 }
