5c7dab829164793b6f78206b8e556574b9d9177f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 03:02:50 +0000

    Bug 1299615 - Part 7: Adjust expected test result after update to ICU 58. r=Waldo
    UltraBlame original commit: a3cc99e3ff2427b7e24c7e62aa7d9dc83e01b7e5

--- js/src/tests/Intl/NumberFormat/format.js
+++ js/src/tests/Intl/NumberFormat/format.js
@@ -31,9 +31,9 @@ assertEq(format.format(123456789.123456789), "￥123,456,789");
 
 
 format = new Intl.NumberFormat("ar-jo", {style: "currency", currency: "JOD"});
-assertEq(format.format(0), "د.أ.‏ ٠٫٠٠٠");
-assertEq(format.format(-1), "‏-د.أ.‏ ١٫٠٠٠");
-assertEq(format.format(123456789.123456789), "د.أ.‏ ١٢٣٬٤٥٦٬٧٨٩٫١٢٣");
+assertEq(format.format(0), "٠٫٠٠٠ د.أ.‏");
+assertEq(format.format(-1), "؜-١٫٠٠٠ د.أ.‏");
+assertEq(format.format(123456789.123456789), "١٢٣٬٤٥٦٬٧٨٩٫١٢٣ د.أ.‏");
 
 
 format = new Intl.NumberFormat("th-th-u-nu-thai",
