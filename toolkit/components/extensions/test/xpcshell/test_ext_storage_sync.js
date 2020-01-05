f6a74f557cb7cd36102c3ef4288502ef31c660d4
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 03:28:29 +0000

    Bug 1253740 - Hash extension ID to obfuscate installed add-ons, r=bsilverberg,kmag
    MozReview-Commit-ID: ASBrDxIq2lF
    
    UltraBlame original commit: d4b969307f46d9514fffad88c8c8e922b67dda2a

--- toolkit/components/extensions/test/xpcshell/test_ext_storage_sync.js
+++ toolkit/components/extensions/test/xpcshell/test_ext_storage_sync.js
@@ -328,8 +328,6 @@ function assertKeyRingKey(keyRing, extensionId, expectedKey, message) {
 
 
 const defaultExtensionId = "{13bdde76-4dc7-11e6-9bdc-54ee758d6342}";
-
-const defaultCollectionId = defaultExtensionId;
 const defaultExtension = {id: defaultExtensionId};
 
 const BORING_KB = "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef";
@@ -343,6 +341,7 @@ const loggedInUser = {
     },
   },
 };
+const defaultCollectionId = extensionIdToCollectionId(loggedInUser, defaultExtensionId);
 
 function uuid() {
   const uuidgen = Cc["@mozilla.org/uuid-generator;1"].getService(Ci.nsIUUIDGenerator);
