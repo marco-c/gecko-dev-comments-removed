5fa90721b86e8c8e8804e327b8c9bc52c67a6ba4
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 22:49:54 +0000

    Bug 1357880 - Add a telemetry probe for non-passive keyboard event listeners r=smaug, data-review=bsmedberg
    This commit adds a telemetry probe to track the percentage of pages that ever
    have a non-passive 'keydown' or 'keypress' event that could preventDefault()
    APZ key scrolling of the root of a page.
    
    A flag is added to each EventListenerManager to track whether it ever had
    a qualifying event listener, and then in nsGlobalWindow::FreeInnerObjects()
    the event targets that could preventDefault() a scroll are checked for this
    flag. This check is done at nsGlobalWindow::FreeInnerObjects() so that the
    DOM is still alive.
    
    MozReview-Commit-ID: EkK3vxehZA5
    
    UltraBlame original commit: 7129e53f3a9cc8f0a1feb3f31d70f969f62a1587

--- dom/events/EventListenerManager.h
+++ dom/events/EventListenerManager.h
@@ -163,6 +163,7 @@ protected:
   uint16_t mMayHaveTouchEventListener : 1;
   uint16_t mMayHaveMouseEnterLeaveEventListener : 1;
   uint16_t mMayHavePointerEnterLeaveEventListener : 1;
+  uint16_t mMayHaveAPZAwareKeyEventListener : 1;
   uint16_t mMayHaveKeyEventListener : 1;
   uint16_t mMayHaveInputOrCompositionEventListener : 1;
   uint16_t mClearingListeners : 1;
@@ -441,6 +442,8 @@ public:
 
   bool MayHaveMouseEnterLeaveEventListener() { return mMayHaveMouseEnterLeaveEventListener; }
   bool MayHavePointerEnterLeaveEventListener() { return mMayHavePointerEnterLeaveEventListener; }
+
+  bool MayHaveAPZAwareKeyEventListener() const { return mMayHaveAPZAwareKeyEventListener; }
 
   
 
