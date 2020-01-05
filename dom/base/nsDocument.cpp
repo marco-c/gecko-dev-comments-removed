7e3afbfdd4bd5a1f09458f8fdff84be0c80f8479
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 04:39:33 +0000

    Bug 1044586 - fix propagating document events to the window when the inner window has changed, r=smaug
    MozReview-Commit-ID: G6yPBwbITvG
    
    UltraBlame original commit: 512d5944a602d037a7c3455190a41f39aab37c9a

--- dom/base/nsDocument.cpp
+++ dom/base/nsDocument.cpp
@@ -7620,9 +7620,12 @@ nsDocument::GetEventTargetParent(EventChainPreVisitor& aVisitor)
 
   
   if (aVisitor.mEvent->mMessage != eLoad) {
-    nsGlobalWindow* window = nsGlobalWindow::Cast(GetWindow());
-    aVisitor.mParentTarget =
-      window ? window->GetTargetForEventTargetChain() : nullptr;
+    nsPIDOMWindowInner* innerWindow = GetInnerWindow();
+    if (innerWindow && innerWindow->IsCurrentInnerWindow()) {
+      nsGlobalWindow* window = nsGlobalWindow::Cast(GetWindow());
+      aVisitor.mParentTarget =
+        window ? window->GetTargetForEventTargetChain() : nullptr;
+    }
   }
   return NS_OK;
 }
