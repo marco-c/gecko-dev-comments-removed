28a3d5208ab61f70b3a6dd6ac07f8a0fb4defa18
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 00:54:32 +0000

    Revert "Bug 1306708 - Fix rebasing bustage on a CLOSED TREE"
    This reverts commit cfc7bb2a6a9710dd9963185f264c70950fd5595c.
    
    UltraBlame original commit: 9b9690621e02ec4a590d5d19a933348134030ea6

--- ipc/glue/MessageChannel.cpp
+++ ipc/glue/MessageChannel.cpp
@@ -776,7 +776,8 @@ MessageChannel::Send(Message* aMsg)
     }
 
     MOZ_RELEASE_ASSERT(!aMsg->is_sync());
-    MOZ_RELEASE_ASSERT(aMsg->nested_level() != IPC::Message::NESTED_INSIDE_SYNC);
+    
+    MOZ_RELEASE_ASSERT(aMsg->priority() != IPC::Message::PRIORITY_HIGH);
 
     CxxStackFrame frame(*this, OUT_MESSAGE, aMsg);
 
