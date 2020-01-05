aa61f3fbb047efdf6c740af5ddc52cfba4c4fb74
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 02:16:57 +0000

    Bug 1243846 - Handle cross-doc,non-implicit root observations + test fixes. r=mstange
    UltraBlame original commit: abaf54d32b12058820d8a5be414866962e4495ca

--- dom/base/DOMIntersectionObserver.cpp
+++ dom/base/DOMIntersectionObserver.cpp
@@ -307,8 +307,25 @@ DOMIntersectionObserver::Update(nsIDocument* aDocument, DOMHighResTimeStamp time
     nsRect targetRect;
     Maybe<nsRect> intersectionRect;
 
-    if (rootFrame && targetFrame &&
-       (!mRoot || nsLayoutUtils::IsAncestorFrameCrossDoc(rootFrame, targetFrame))) {
+    if (rootFrame && targetFrame) {
+      
+      
+      if (mRoot) {
+        
+        
+        
+        if (target->GetComposedDoc() != root->GetComposedDoc()) {
+          continue;
+        }
+        
+        
+        
+        
+        if (!nsLayoutUtils::IsAncestorFrameCrossDoc(rootFrame, targetFrame)) {
+          continue;
+        }
+      }
+
       targetRect = nsLayoutUtils::GetAllInFlowRectsUnion(
         targetFrame,
         nsLayoutUtils::GetContainingBlockForClientRect(targetFrame),
