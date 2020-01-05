0c1c877d9d74b52f5b15eaf9f95db14241d9b37a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 10:12:52 +0000

    servo: Merge #6461 - Use a dedicated type for the opaque type parameter to WorkQueue in layout (from Ms2ger:workqueuedata); r=pcwalton
    Currently, we use UnsafeFlow and UnsafeLayoutNode, both of which are aliases
    for (usize, usize) and thus interconvertible. This change should make it
    clearer that the WorkQueue is not limited to one particular type.
    
    Source-Repo: https://github.com/servo/servo
    Source-Revision: 07a1e187f03484c5f326e48c4a5fed81c134d215
    
    UltraBlame original commit: db1c1b5f8db67651dea141eb2aef4e36bc01e4f0

--- servo/components/layout/layout_task.rs
+++ servo/components/layout/layout_task.rs
@@ -19,7 +19,7 @@ use fragment::{Fragment, FragmentBorderBoxIterator};
 use incremental::{LayoutDamageComputation, REFLOW, REFLOW_ENTIRE_DOCUMENT, REPAINT};
 use layout_debug;
 use opaque_node::OpaqueNodeMethods;
-use parallel::{self, UnsafeFlow};
+use parallel::{self, WorkQueueData};
 use sequential;
 use wrapper::LayoutNode;
 
@@ -109,7 +109,7 @@ pub struct LayoutTaskData {
     pub stylist: Box<Stylist>,
 
     
-    pub parallel_traversal: Option<WorkQueue<SharedLayoutContextWrapper, UnsafeFlow>>,
+    pub parallel_traversal: Option<WorkQueue<SharedLayoutContextWrapper, WorkQueueData>>,
 
     
     pub dirty: Rect<Au>,
