36fcb2182974cc16cc92d58665f15a3a25e18d0a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 06:14:55 +0000

    Bug 1323100 - Stop double-registering the Media_Encoder thread with the profiler. r=froydnj
    MozReview-Commit-ID: 8TQMVhbw13s
    
    UltraBlame original commit: 7947e6f167f2cb3a4c77b65a96a67b6f1fffc343

--- dom/media/MediaRecorder.cpp
+++ dom/media/MediaRecorder.cpp
@@ -283,8 +283,6 @@ class MediaRecorder::Session: public nsIObserver,
       } else {
         
         mSession->Extract(true);
-        if (mSession->mIsRegisterProfiler)
-           profiler_unregister_thread();
         if (NS_FAILED(NS_DispatchToMainThread(
                         new DestroyRunnable(mSession)))) {
           MOZ_ASSERT(false, "NS_DispatchToMainThread DestroyRunnable failed");
@@ -419,7 +417,6 @@ public:
     , mTimeSlice(aTimeSlice)
     , mStopIssued(false)
     , mIsStartEventFired(false)
-    , mIsRegisterProfiler(false)
     , mNeedSessionEndTask(true)
     , mSelectedVideoTrackID(TRACK_NONE)
   {
@@ -606,12 +603,6 @@ private:
   {
     MOZ_ASSERT(NS_GetCurrentThread() == mReadThread);
     LOG(LogLevel::Debug, ("Session.Extract %p", this));
-
-    if (!mIsRegisterProfiler) {
-      char aLocal;
-      profiler_register_thread("Media_Encoder", &aLocal);
-      mIsRegisterProfiler = true;
-    }
 
     PROFILER_LABEL("MediaRecorder", "Session Extract",
       js::ProfileEntry::Category::OTHER);
@@ -922,8 +913,6 @@ private:
   bool mStopIssued;
   
   bool mIsStartEventFired;
-  
-  bool mIsRegisterProfiler;
   
   
   
