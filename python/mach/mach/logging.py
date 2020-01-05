457cf29abb979520eb907ff153a0dd57fe606ec2
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 05:23:34 +0000

    Bug 1318044 - Issue sgr0 after printing every line; r=froydnj
    The sgr0 terminal capabity defines the byte sequence needed to reset
    terminal text to its default state. For reasons documented inline in
    this commit, we now print this sequence after every line printed by
    mach's terminal logger.
    
    MozReview-Commit-ID: 3RukP0QXtqy
    
    UltraBlame original commit: 3b0dd252edd7efc2da801ff8f141473fbc90193a

--- python/mach/mach/logging.py
+++ python/mach/mach/logging.py
@@ -93,6 +93,7 @@ class StructuredTerminalFormatter(StructuredHumanFormatter):
 
     def set_terminal(self, terminal):
         self.terminal = terminal
+        self._sgr0 = blessings.tigetstr('sgr0') if terminal and blessings else ''
 
     def format(self, record):
         f = record.msg.format(**record.params)
@@ -102,7 +103,13 @@ class StructuredTerminalFormatter(StructuredHumanFormatter):
 
         t = self.terminal.blue(format_seconds(self._time(record)))
 
-        return '%s %s' % (t, self._colorize(f))
+        
+        
+        
+        
+        
+        
+        return '%s %s%s' % (t, self._colorize(f), self._sgr0)
 
     def _colorize(self, s):
         if not self.terminal:
