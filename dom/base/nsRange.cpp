48f4c04cd5dc4a5695e82c751df2bd934b350fd5
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 06:20:29 +0000

    Bug 1327902 - Set the range boundary point outside (before) the node if it HasIndependentSelection().  r=smaug
    UltraBlame original commit: 944f680d77075b4858fe478622e1e934dbb57d17

--- dom/base/nsRange.cpp
+++ dom/base/nsRange.cpp
@@ -3300,7 +3300,18 @@ nsRange::ExcludeNonSelectableNodes(nsTArray<RefPtr<nsRange>>* aOutRanges)
           }
 
           
-          rv = CreateRange(node, 0, endParent, endOffset,
+          nsINode* startParent = node;
+          int32_t startOffset = 0;
+          
+          
+          if (content && content->HasIndependentSelection()) {
+            nsINode* parent = startParent->GetParent();
+            if (parent) {
+              startOffset = parent->IndexOf(startParent);
+              startParent = parent;
+            }
+          }
+          rv = CreateRange(startParent, startOffset, endParent, endOffset,
                            getter_AddRefs(newRange));
           if (NS_FAILED(rv) || newRange->Collapsed()) {
             newRange = nullptr;
