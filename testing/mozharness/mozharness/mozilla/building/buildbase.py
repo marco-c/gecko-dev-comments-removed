5d2fb5e8aed02a933f9af27cb79227a6d4fc0533
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 22:05:48 +0000

    Bug 1337861 - [Fennec-Relpro] Enforce the presence of $MOZ_BUILD_DATE r=jlund
    MozReview-Commit-ID: DzEeeYQjwLW
    
    UltraBlame original commit: 2f45a2547cb49cf6e1579c4079bb44ec7426af2e

--- testing/mozharness/mozharness/mozilla/building/buildbase.py
+++ testing/mozharness/mozharness/mozilla/building/buildbase.py
@@ -795,16 +795,27 @@ or run without that action (ie: --no-{action})"
 
         buildid = None
         if c.get("is_automation"):
-            if self.buildbot_config['properties'].get('buildid'):
-                self.info("Determining buildid from buildbot properties")
-                buildid = self.buildbot_config['properties']['buildid'].encode(
-                    'ascii', 'replace'
-                )
+            if self.buildbot_config.get('properties'):
+                
+                if self.buildbot_config.get('properties').get('buildid'):
+                    
+                    
+                    self.info("Determining buildid from buildbot properties")
+                    buildid = self.buildbot_config['properties']['buildid'].encode(
+                        'ascii', 'replace'
+                    )
             else:
                 
                 
                 
-                buildid = os.environ.get('MOZ_BUILD_DATE')
+                
+                try:
+                    buildid = os.environ['MOZ_BUILD_DATE']
+                except KeyError:
+                    self.fatal(
+                        "MOZ_BUILD_DATE must be provided as an environment var on Taskcluster"
+                    )
+
 
         if not buildid:
             self.info("Creating buildid through current time")
