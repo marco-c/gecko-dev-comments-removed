9df37e516651b39581a0afe268179137fd893aa9
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 19:54:54 +0000

    servo: Merge #16159 - Centralize note_dirty_descendants implementation, and fully propagate dirty_descendants in resolve_style (from bholley:note_dirty_descendants); r=heycam
    The current code can leave the tree in an inconsistent state, with the dirty
    descendants bit not fully propagated.
    
    Reviewed in https://bugzilla.mozilla.org/show_bug.cgi?id=1350441
    
    Source-Repo: https://github.com/servo/servo
    Source-Revision: 8adf1919732a764617ce7c8c26e772a9a7663b98
    
    UltraBlame original commit: 6fa1eb4be652ab3aa72cc6bc25c36b156d1182b4

--- servo/components/style/traversal.rs
+++ servo/components/style/traversal.rs
@@ -9,7 +9,7 @@
 use atomic_refcell::{AtomicRefCell, AtomicRefMut};
 use context::{SharedStyleContext, StyleContext, ThreadLocalStyleContext};
 use data::{ElementData, ElementStyles, StoredRestyleHint};
-use dom::{NodeInfo, TElement, TNode};
+use dom::{DirtyDescendants, NodeInfo, TElement, TNode};
 use matching::{MatchMethods, MatchResults};
 use restyle_hints::{RESTYLE_DESCENDANTS, RESTYLE_SELF};
 use selector_parser::RestyleDamage;
@@ -387,7 +387,7 @@ fn resolve_style_internal<E, F>(context: &mut StyleContext<E>,
         
         
         
-        unsafe { element.set_dirty_descendants() };
+        unsafe { element.note_descendants::<DirtyDescendants>() };
     }
 
     
