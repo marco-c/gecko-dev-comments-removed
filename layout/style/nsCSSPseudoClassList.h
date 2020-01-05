8eb925166bd4b43c48ba25079cc8a5f57525251f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 23:20:44 +0000

    Bug 1361244 - Add an internal -moz-autofill-preview pseudo class for styling preview fields. r=heycam
    MozReview-Commit-ID: 6siKEgBPTKt
    
    UltraBlame original commit: 75d9703cd37581c4a986bc7d80499340b7aca794

--- layout/style/nsCSSPseudoClassList.h
+++ layout/style/nsCSSPseudoClassList.h
@@ -211,6 +211,9 @@ CSS_STATE_PSEUDO_CLASS(mozMathIncrementScriptLevel,
 CSS_STATE_PSEUDO_CLASS(mozAutofill, ":-moz-autofill",
                        CSS_PSEUDO_CLASS_ENABLED_IN_UA_SHEETS_AND_CHROME, "",
                        NS_EVENT_STATE_AUTOFILL)
+CSS_STATE_PSEUDO_CLASS(mozAutofillPreview, ":-moz-autofill-preview",
+                       CSS_PSEUDO_CLASS_ENABLED_IN_UA_SHEETS_AND_CHROME, "",
+                       NS_EVENT_STATE_AUTOFILL_PREVIEW)
 
 
 
