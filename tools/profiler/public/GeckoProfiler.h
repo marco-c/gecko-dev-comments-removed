eca79b87b38b2c92dba13d1b516ae86681e3b1f4
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 22:06:42 +0000

    Bug 1358074 (part 2) - Unexport and rename ProfilerState. r=mstange.
    gPS is declared in GeckoProfiler.h so that it can be tested as non-null in a
    couple of functions.
    
    - These checks are of little value, so this patch removes them.
    
    - That lets us remove the ProfilerState and gPS declarations from
      GeckoProfiler.h.
    
    - And, because ProfilerState is now only used within platform.cpp, that lets us
      rename it as PS, which is how we currently refer to it (via a typedef) within
      platform.cpp anyway.
    
    UltraBlame original commit: 9baa76979a717579ad43a9814d0fed9c73717b5a

--- tools/profiler/public/GeckoProfiler.h
+++ tools/profiler/public/GeckoProfiler.h
@@ -329,14 +329,6 @@ class ProfilerMarkerPayload;
 
 extern MOZ_THREAD_LOCAL(PseudoStack*) tlsPseudoStack;
 
-class ProfilerState;
-
-
-
-
-
-extern ProfilerState* gPS;
-
 #ifndef SAMPLE_FUNCTION_NAME
 # if defined(__GNUC__) || defined(_MSC_VER)
 #  define SAMPLE_FUNCTION_NAME __FUNCTION__
@@ -355,8 +347,6 @@ profiler_call_enter(const char* aInfo,
 {
   
 
-  MOZ_RELEASE_ASSERT(gPS);
-
   PseudoStack* stack = tlsPseudoStack.get();
   if (!stack) {
     return stack;
@@ -372,8 +362,6 @@ static inline void
 profiler_call_exit(void* aHandle)
 {
   
-
-  MOZ_RELEASE_ASSERT(gPS);
 
   if (!aHandle) {
     return;
@@ -497,8 +485,6 @@ inline PseudoStack*
 profiler_get_pseudo_stack(void)
 {
   
-
-  MOZ_RELEASE_ASSERT(gPS);
 
   return tlsPseudoStack.get();
 }
