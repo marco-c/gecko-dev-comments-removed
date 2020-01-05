0f058db4d1f32a6bca47c237843bfca6d2423589
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 22:55:23 +0000

    Bug 833143 - Don't GC in nsXREDirProvider::DoShutdown. r=bsmedberg, r=billm
    UltraBlame original commit: 5e01d4124eaefeb0702f31689a9223f14351368e

--- toolkit/xre/nsXREDirProvider.cpp
+++ toolkit/xre/nsXREDirProvider.cpp
@@ -1239,15 +1239,6 @@ nsXREDirProvider::DoShutdown()
       static const char16_t kShutdownPersist[] = u"shutdown-persist";
       obsSvc->NotifyObservers(nullptr, "profile-change-net-teardown", kShutdownPersist);
       obsSvc->NotifyObservers(nullptr, "profile-change-teardown", kShutdownPersist);
-
-      
-      
-
-      if (JSContext* cx = dom::danger::GetJSContext()) {
-        JS_GC(cx);
-      }
-
-      
       obsSvc->NotifyObservers(nullptr, "profile-before-change", kShutdownPersist);
       obsSvc->NotifyObservers(nullptr, "profile-before-change-qm", kShutdownPersist);
       obsSvc->NotifyObservers(nullptr, "profile-before-change-telemetry", kShutdownPersist);
