abd6d64bac69d0d0c1f34aaabdf96f9391385ef0
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 04:45:48 +0000

    Bug 1321471. Part 1 - Use of MozPromise::ThenPromise() in place of CompletionPromise(). r=jya.
    MozReview-Commit-ID: INbGpzwK4f9
    
    UltraBlame original commit: 31c6008a6773d09b0f84d6db482d25b88d2c1639

--- dom/media/MediaDecoderStateMachine.cpp
+++ dom/media/MediaDecoderStateMachine.cpp
@@ -1656,11 +1656,10 @@ ShutdownState::Enter()
   
   master->mWatchManager.Shutdown();
 
-  return Reader()->Shutdown()
-    ->Then(OwnerThread(), __func__, master,
-           &MediaDecoderStateMachine::FinishShutdown,
-           &MediaDecoderStateMachine::FinishShutdown)
-    ->CompletionPromise();
+  return Reader()->Shutdown()->ThenPromise(
+    OwnerThread(), __func__, master,
+    &MediaDecoderStateMachine::FinishShutdown,
+    &MediaDecoderStateMachine::FinishShutdown);
 }
 
 #define INIT_WATCHABLE(name, val) \
