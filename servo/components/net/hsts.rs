c6c57e59f080a63f852509532527a83e5b0865c6
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 10:43:30 +0000

    servo: Merge #8653 - Remove dependency on regex_macros (from servo:no-regex-macros); r=Ms2ger
    This reduces the amount of code using unstable features that we depend on.
    The hand-written IP address parser is probably just as fast.
    
    Source-Repo: https://github.com/servo/servo
    Source-Revision: 6f35b867c9c3bb7a345e2ac34e5970b93a1d3ea1
    
    UltraBlame original commit: 2fc3a5e034ed4e8b20a0bfb0fc2cb0250fc5b645

--- servo/components/net/hsts.rs
+++ servo/components/net/hsts.rs
@@ -2,8 +2,9 @@
 
 
 
-use net_traits::{IPV4_REGEX, IPV6_REGEX, IncludeSubdomains};
+use net_traits::IncludeSubdomains;
 use rustc_serialize::json::{decode};
+use std::net::{Ipv4Addr, Ipv6Addr};
 use std::str::{from_utf8};
 use time;
 use url::Url;
@@ -19,7 +20,7 @@ pub struct HSTSEntry {
 
 impl HSTSEntry {
     pub fn new(host: String, subdomains: IncludeSubdomains, max_age: Option<u64>) -> Option<HSTSEntry> {
-        if IPV4_REGEX.is_match(&host) || IPV6_REGEX.is_match(&host) {
+        if host.parse::<Ipv4Addr>().is_ok() || host.parse::<Ipv6Addr>().is_ok() {
             None
         } else {
             Some(HSTSEntry {
