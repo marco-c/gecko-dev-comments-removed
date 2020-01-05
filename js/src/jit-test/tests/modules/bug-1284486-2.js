c4e9a57f3a1ef7598ea3e9cc7ac9ff35c62732a5
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 19:53:50 +0000

    Bug 1351345 - Add test that a module loader does not attempt to re-instantiate a previously instantiated module r=shu
    UltraBlame original commit: 8b964027442044f5fe933e2fc92eb6704e706798

--- js/src/jit-test/tests/modules/bug-1284486-2.js
+++ js/src/jit-test/tests/modules/bug-1284486-2.js
@@ -11,16 +11,14 @@
 
 
 
-
 let moduleRepo = {};
 setModuleResolveHook(function(module, specifier) {
     return moduleRepo[specifier];
 });
+let c;
 try {
     let b = moduleRepo['b'] = parseModule("export var b = 3; export var c = 4;");
-    let c = moduleRepo['c'] = parseModule("export * from 'a'; export * from 'b';");
+    c = moduleRepo['c'] = parseModule("export * from 'a'; export * from 'b';");
     c.declarationInstantiation();
 } catch (exc) {}
-let a = moduleRepo['a'] = parseModule("export var a = 1; export var b = 2;");
-let d = moduleRepo['d'] = parseModule("import { a } from 'c'; a;");
-d.declarationInstantiation();
+c.declarationInstantiation();
