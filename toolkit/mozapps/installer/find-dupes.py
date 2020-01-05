328776a4b78e159a77d67beaee373c867d79a1ad
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 03:33:19 +0000

    Bug 1317313 - Unbust todays Android Aurora Nightly (post merge-day) by adding more to whitelist, and fixing regex. r=mshal,Tomcat
    MozReview-Commit-ID: BaSzx54qSZ8
    
    UltraBlame original commit: c765cdbfce2895e7974563481fc213887f3a38fb

--- toolkit/mozapps/installer/find-dupes.py
+++ toolkit/mozapps/installer/find-dupes.py
@@ -38,11 +38,13 @@ def normalize_l10n_path(p):
     >>> normalize_l10n_path('chrome/fr/locale/fr/browser/aboutHome.dtd')
     'chrome/en-US/locale/en-US/browser/aboutHome.dtd'
     '''
-    p = re.sub(r'chrome/(\S+)/locale/\1',
-               'chrome/en-US/locale/en-US',
+    
+    
+    p = re.sub(r'chrome/(\S+)/locale/\1/',
+               'chrome/en-US/locale/en-US/',
                p)
-    p = re.sub(r'chrome/(\S+)/locale',
-               'chrome/en-US/locale',
+    p = re.sub(r'chrome/(\S+)/locale/',
+               'chrome/en-US/locale/',
                p)
     return p
 
