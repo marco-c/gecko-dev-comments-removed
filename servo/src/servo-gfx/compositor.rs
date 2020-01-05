3a71643dbeb1f6a0e2c12a11d5ccf68c78a534d7
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 08:40:04 +0000

    servo: Language changes.
    Source-Repo: https://github.com/servo/servo
    Source-Revision: e85b3798f226b4910a065fc2f6f116d45bc18084
    
    UltraBlame original commit: acbc15ed6ff8715f416a4416a95b1860abb999de

--- servo/src/servo-gfx/compositor.rs
+++ servo/src/servo-gfx/compositor.rs
@@ -22,7 +22,7 @@ pub struct LayerBufferSet {
 
 
 pub trait Compositor {
-    fn begin_drawing(next_dt: comm::Chan<LayerBufferSet>);
-    fn draw(next_dt: comm::Chan<LayerBufferSet>, +draw_me: LayerBufferSet);
+    fn begin_drawing(&self, next_dt: comm::Chan<LayerBufferSet>);
+    fn draw(&self, next_dt: comm::Chan<LayerBufferSet>, +draw_me: LayerBufferSet);
 }
 
