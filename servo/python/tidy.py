6e3ad7cb021e7d6f3e0b3331c1b7b1552a8e8a08
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 11:06:03 +0000

    servo: Merge #10433 - Implement WebGLRenderingContext.getActiveUniform() (from saurvs:pr3); r=emilio
    Fixes https://github.com/servo/servo/issues/10397
    
    Source-Repo: https://github.com/servo/servo
    Source-Revision: 4cc241c858ce8ee7894495d082a3bfc6233d80e8
    
    UltraBlame original commit: e0982ea8e93e42207bd4713d299a27f7dc70caaa

--- servo/python/tidy.py
+++ servo/python/tidy.py
@@ -220,7 +220,7 @@ def check_lock(file_name, contents):
         raise StopIteration
 
     
-    exceptions = ["bitflags", "xml-rs", "gl_generator"]
+    exceptions = ["bitflags", "xml-rs", "gl_generator", "byteorder"]
 
     import toml
     content = toml.loads(contents)
