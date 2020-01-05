c7fd0d1b7533ed129763d639d4d2520c5396289d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 21:26:36 +0000

    Bug 1355161 - provide a scriptable equivalent of NS_DispatchToMainThread, r=froydnj.
    UltraBlame original commit: 781aa8ce66f68af1dec97d3f94ef7a6330a42cc7

--- xpcom/threads/nsThreadManager.cpp
+++ xpcom/threads/nsThreadManager.cpp
@@ -348,3 +348,17 @@ nsThreadManager::GetHighestNumberOfThreads()
   OffTheBooksMutexAutoLock lock(mLock);
   return mHighestNumberOfThreads;
 }
+
+NS_IMETHODIMP
+nsThreadManager::DispatchToMainThread(nsIRunnable *aEvent)
+{
+  
+  MOZ_ASSERT(NS_IsMainThread());
+
+  
+  if (NS_WARN_IF(!mMainThread)) {
+    return NS_ERROR_NOT_INITIALIZED;
+  }
+
+  return mMainThread->DispatchFromScript(aEvent, 0);
+}
