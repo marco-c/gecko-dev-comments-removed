1e059e2fe4b831eae43406270d3036cbce88a673
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 04:59:54 +0000

    Bug 1319318: When resolving a video/audio frame's shrinkwrapped BSize from controls, check whether controls are orthogonal when reading its size. r=mats
    MozReview-Commit-ID: 9kH7rLF66zd
    
    UltraBlame original commit: 70a0740cbd8129b84aabb426a50c3cf3240f8266

--- layout/generic/nsVideoFrame.cpp
+++ layout/generic/nsVideoFrame.cpp
@@ -377,7 +377,9 @@ nsVideoFrame::Reflow(nsPresContext* aPresContext,
                   borderPadding.left, borderPadding.top, 0, aStatus);
 
       if (child->GetContent() == mVideoControls && isBSizeShrinkWrapping) {
-        contentBoxBSize = kidDesiredSize.BSize(myWM);
+        
+        contentBoxBSize = myWM.IsOrthogonalTo(wm) ?
+          kidDesiredSize.ISize(wm) : kidDesiredSize.BSize(wm);
       }
 
       FinishReflowChild(child, aPresContext,
