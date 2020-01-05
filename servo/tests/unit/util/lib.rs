89de138be1a8c050fee6e66b9e03347590a83132
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 11:16:27 +0000

    servo: Merge #11159 - Fix warnings in unit tests (from mbrubeck:test-warnings); r=emilio
    Source-Repo: https://github.com/servo/servo
    Source-Revision: 1e8ddba3e84617d2e0e01d2e7fbf8548abed518f
    
    UltraBlame original commit: ee070d7b4ee97f55441598bb9757a95c369cf12e

--- servo/tests/unit/util/lib.rs
+++ servo/tests/unit/util/lib.rs
@@ -2,18 +2,12 @@
 
 
 
-#![cfg_attr(test, feature(plugin, custom_derive))]
-#![cfg_attr(test, plugin(plugins))]
-#![feature(alloc)]
+#![cfg(test)]
 
-extern crate alloc;
-extern crate app_units;
-extern crate euclid;
-extern crate libc;
 extern crate util;
 
-#[cfg(test)] mod cache;
-#[cfg(test)] mod opts;
-#[cfg(test)] mod str;
-#[cfg(test)] mod thread;
-#[cfg(test)] mod prefs;
+mod cache;
+mod opts;
+mod prefs;
+mod str;
+mod thread;
