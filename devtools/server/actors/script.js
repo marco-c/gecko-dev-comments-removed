397528b8ff5239306820c1a84b5073f82acd0cc1
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 07:56:07 +0000

    Bug 1333840 - Protect from frame.this access for wasm debugger frame. r=ochameau
    MozReview-Commit-ID: eEhoHiXQTr
    
    UltraBlame original commit: 592325b7b0d1e9320c692f43f7ecedf5ad478c38

--- devtools/server/actors/script.js
+++ devtools/server/actors/script.js
@@ -2190,10 +2190,15 @@ function hackDebugger(Debugger) {
 
 
   Debugger.Script.prototype.toString = function () {
+    if (this.type == "wasm") {
+      return "[wasm]";
+    }
+
     let output = "";
     if (this.url) {
       output += this.url;
     }
+
     if (typeof this.staticLevel != "undefined") {
       output += ":L" + this.staticLevel;
     }
