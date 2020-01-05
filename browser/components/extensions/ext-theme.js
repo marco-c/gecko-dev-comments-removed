83f2d646682867ef86f973f469e270006a9e9232
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 07:28:06 +0000

    Bug 1330340 - Implement support for lightweight theme properties (headerURL, accentcolor, textcolor) for Theming API. r=mikedeboer
    MozReview-Commit-ID: GS1Gh4HVcOt
    
    UltraBlame original commit: 90f3467f8b8c00d67ec880b2e1517512c084cd21

--- browser/components/extensions/ext-theme.js
+++ browser/components/extensions/ext-theme.js
@@ -1,19 +1,62 @@
 "use strict";
 
+Cu.import("resource://gre/modules/Services.jsm");
+
 XPCOMUtils.defineLazyModuleGetter(this, "Preferences",
                                   "resource://gre/modules/Preferences.jsm");
 
 
 extensions.on("manifest_theme", (type, directive, extension, manifest) => {
   let enabled = Preferences.get("extensions.webextensions.themes.enabled");
-  extension.emit("test-message", "themes-enabled", enabled);
 
-  if (enabled) {
-    
+  if (!enabled || !manifest || !manifest.theme) {
+    return;
+  }
+  
+  let lwtStyles = {footerURL: ""};
+  if (manifest.theme.colors) {
+    let colors = manifest.theme.colors;
+    for (let color of Object.getOwnPropertyNames(colors)) {
+      let val = colors[color];
+      
+      if (val === null) {
+        continue;
+      }
+
+      if (color == "accentcolor") {
+        lwtStyles.accentcolor = val;
+        continue;
+      }
+      if (color == "textcolor") {
+        lwtStyles.textcolor = val;
+      }
+    }
+  }
+
+  if (manifest.theme.images) {
+    let images = manifest.theme.images;
+    for (let image of Object.getOwnPropertyNames(images)) {
+      let val = images[image];
+      if (val === null) {
+        continue;
+      }
+
+      if (image == "headerURL") {
+        lwtStyles.headerURL = val;
+      }
+    }
+  }
+
+  if (lwtStyles.headerURL &&
+      lwtStyles.accentcolor &&
+      lwtStyles.textcolor) {
+    Services.obs.notifyObservers(null,
+      "lightweight-theme-styling-update",
+      JSON.stringify(lwtStyles));
   }
 });
 
 extensions.on("shutdown", (type, extension) => {
-  
+  Services.obs.notifyObservers(null, "lightweight-theme-styling-update", null);
 });
 
