4af7637801e350bb1df2d0642d7c0d1fbb76f19b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 22:43:12 +0000

    merge mozilla-inbound to mozilla-central. r=merge a=merge
    MozReview-Commit-ID: ELENTh08Be7
    
    UltraBlame original commit: 308cdb913d717f8edc932f5bac93854e3e54b84d

--- dom/ipc/ContentParent.cpp
+++ dom/ipc/ContentParent.cpp
@@ -2227,12 +2227,7 @@ ContentParent::InitInternal(ProcessPriority aInitialPriority,
     SerializeURI(nullptr, xpcomInit.userContentSheetURL());
   }
 
-  
   gfxPlatform::GetPlatform()->BuildContentDeviceData(&xpcomInit.contentDeviceData());
-  
-  xpcomInit.gfxNonDefaultVarUpdates() = gfxVars::FetchNonDefaultVars();
-  
-  gfxVars::AddReceiver(this);
 
   nsCOMPtr<nsIGfxInfo> gfxInfo = services::GetGfxInfo();
   if (gfxInfo) {
