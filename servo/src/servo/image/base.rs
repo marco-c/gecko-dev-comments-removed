c4bda1f6587c1e074371b06337f591f7c245ba65
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 08:22:43 +0000

    servo: Decode images in the ImageCache
    Source-Repo: https://github.com/servo/servo
    Source-Revision: 872a82b9f0cf628676960cd87a3b9e72f688b4f2
    
    UltraBlame original commit: def85bd777ce4217fbc526558c1de7b835bbb86d

--- servo/src/servo/image/base.rs
+++ servo/src/servo/image/base.rs
@@ -2,10 +2,15 @@ export Image;
 
 export load;
 export load_from_memory;
+export test_image_bin;
 
 import stb_image::image::{image, load, load_from_memory};
 
-
-
+// FIXME: Images must not be copied every frame. Instead we should atomically
+// reference count them.
 
 type Image = image;
+
+fn test_image_bin() -> ~[u8] {
+    #include_bin("test.jpeg")
+}
