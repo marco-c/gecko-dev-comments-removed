573a3c5286a72ed77920a960ce5283d5d2b7cccf
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 01:15:18 +0000

    Bug 1059454 - Enable commented out constructor test in string-iterator-surfaces.js. r=till
    UltraBlame original commit: bf1119f4a7c20fb1513a7c18e17d59b8bc2a056b

--- js/src/jit-test/tests/for-of/string-iterator-surfaces.js
+++ js/src/jit-test/tests/for-of/string-iterator-surfaces.js
@@ -31,14 +31,22 @@ function assertBuiltinFunction(o, name, arity) {
 
     assertEq(typeof fn, "function");
     assertEq(Object.getPrototypeOf(fn), Function.prototype);
-    
-    
+    assertEq(isConstructor(fn), false);
 
-    arraysEqual(Object.getOwnPropertyNames(fn).sort(), ["length", "name", "arguments", "caller"].sort());
+    assertEq(arraysEqual(Object.getOwnPropertyNames(fn).sort(), ["length", "name"].sort()), true);
 
-    
     assertDataDescriptor(Object.getOwnPropertyDescriptor(fn, "length"), {
         value: arity,
+        writable: false,
+        enumerable: false,
+        configurable: true
+    });
+
+    var functionName = typeof name === "symbol"
+                       ? String(name).replace(/^Symbol\((.+)\)$/, "[$1]")
+                       : name;
+    assertDataDescriptor(Object.getOwnPropertyDescriptor(fn, "name"), {
+        value: functionName,
         writable: false,
         enumerable: false,
         configurable: true
@@ -59,7 +67,7 @@ assertEq(Object.getPrototypeOf(iterProto),
          Object.getPrototypeOf(Object.getPrototypeOf([][Symbol.iterator]())));
 
 
-arraysEqual(Object.getOwnPropertyNames(iterProto).sort(), ["next"]);
+assertEq(arraysEqual(Object.getOwnPropertyNames(iterProto).sort(), ["next"]), true);
 
 
 assertBuiltinFunction(iterProto, "next", 0);
