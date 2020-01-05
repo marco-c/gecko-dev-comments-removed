6c5a765e020087e556d82f5b55afa25c8f5fa892
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 17:25:55 +0000

    Bug 1333447 - ARM assembler: fast paths for putting simple instructions. r=nbp
    UltraBlame original commit: bcd5db7ac471cad3370ab332e3425c3cbac9bf15

--- js/src/jit/shared/IonAssemblerBuffer.h
+++ js/src/jit/shared/IonAssemblerBuffer.h
@@ -135,6 +135,15 @@ class BufferSlice
             memcpy(&instructions[length()], source, numBytes);
         bytelength_ += numBytes;
     }
+
+    MOZ_ALWAYS_INLINE
+    void putU32Aligned(uint32_t value) {
+        MOZ_ASSERT(bytelength_ + 4 <= SliceSize);
+        MOZ_ASSERT((bytelength_ & 3) == 0);
+        MOZ_ASSERT((uintptr_t(&instructions[0]) & 3) == 0);
+        *reinterpret_cast<uint32_t*>(&instructions[bytelength_]) = value;
+        bytelength_ += 4;
+    }
 };
 
 template<int SliceSize, class Inst>
@@ -232,6 +241,16 @@ class AssemblerBuffer
 
     BufferOffset putInt(uint32_t value) {
         return putBytes(sizeof(value), &value);
+    }
+
+    MOZ_ALWAYS_INLINE
+    BufferOffset putU32Aligned(uint32_t value) {
+        if (!ensureSpace(sizeof(value)))
+            return BufferOffset();
+
+        BufferOffset ret = nextOffset();
+        tail->putU32Aligned(value);
+        return ret;
     }
 
     
@@ -357,7 +376,8 @@ class AssemblerBuffer
     
     Inst* getInst(BufferOffset off) {
         const int offset = off.getOffset();
-        MOZ_RELEASE_ASSERT(off.assigned() && offset >= 0 && (unsigned)offset < size());
+        
+        MOZ_ASSERT(off.assigned() && offset >= 0 && unsigned(offset) < size());
 
         
         if (offset >= int(bufferSize))
