87fc9c12874647bd9934408d58555c021ceefb19
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 18:40:03 +0000

    Bug 1346286 - Remove CPOWs from browser_history_persist.js r=Felipe
    MozReview-Commit-ID: 22fQbSgMzQM
    
    UltraBlame original commit: e457c43d07bf8b91586f1b539a4acd41e95b44f3

--- browser/components/sessionstore/test/browser_history_persist.js
+++ browser/components/sessionstore/test/browser_history_persist.js
@@ -1,6 +1,7 @@
 
 
 
+
 "use strict";
 
 
@@ -17,27 +18,34 @@ add_task(function* check_history_not_persisted() {
   yield TabStateFlusher.flush(browser);
   let state = JSON.parse(ss.getTabState(tab));
   ok(!state.entries[0].persist, "Should have collected the persistence state");
-  gBrowser.removeTab(tab);
+  yield promiseRemoveTab(tab);
   browser = null;
 
   
   tab = gBrowser.addTab("about:blank");
   browser = tab.linkedBrowser;
   yield promiseTabState(tab, state);
-  let sessionHistory = browser.sessionHistory;
 
-  is(sessionHistory.count, 1, "Should be a single history entry");
-  is(sessionHistory.getEntryAtIndex(0, false).URI.spec, "about:blank", "Should be the right URL");
+  yield ContentTask.spawn(browser, null, function() {
+    let sessionHistory = docShell.QueryInterface(Ci.nsIInterfaceRequestor)
+                                 .getInterface(Ci.nsISHistory);
+
+    is(sessionHistory.count, 1, "Should be a single history entry");
+    is(sessionHistory.getEntryAtIndex(0, false).URI.spec, "about:blank", "Should be the right URL");
+  });
 
   
   browser.loadURI("about:robots");
   yield promiseBrowserLoaded(browser);
-  sessionHistory = browser.sessionHistory;
-  is(sessionHistory.count, 1, "Should be a single history entry");
-  is(sessionHistory.getEntryAtIndex(0, false).URI.spec, "about:robots", "Should be the right URL");
+  yield ContentTask.spawn(browser, null, function() {
+    let sessionHistory = docShell.QueryInterface(Ci.nsIInterfaceRequestor)
+                                 .getInterface(Ci.nsISHistory);
+    is(sessionHistory.count, 1, "Should be a single history entry");
+    is(sessionHistory.getEntryAtIndex(0, false).URI.spec, "about:robots", "Should be the right URL");
+  });
 
   
-  gBrowser.removeTab(tab);
+  yield promiseRemoveTab(tab);
 });
 
 
@@ -54,26 +62,32 @@ add_task(function* check_history_default_persisted() {
   yield TabStateFlusher.flush(browser);
   let state = JSON.parse(ss.getTabState(tab));
   delete state.entries[0].persist;
-  gBrowser.removeTab(tab);
+  yield promiseRemoveTab(tab);
   browser = null;
 
   
   tab = gBrowser.addTab("about:blank");
   browser = tab.linkedBrowser;
   yield promiseTabState(tab, state);
-  let sessionHistory = browser.sessionHistory;
+  yield ContentTask.spawn(browser, null, function() {
+    let sessionHistory = docShell.QueryInterface(Ci.nsIInterfaceRequestor)
+                                 .getInterface(Ci.nsISHistory);
 
-  is(sessionHistory.count, 1, "Should be a single history entry");
-  is(sessionHistory.getEntryAtIndex(0, false).URI.spec, "about:blank", "Should be the right URL");
+    is(sessionHistory.count, 1, "Should be a single history entry");
+    is(sessionHistory.getEntryAtIndex(0, false).URI.spec, "about:blank", "Should be the right URL");
+  });
 
   
   browser.loadURI("about:robots");
   yield promiseBrowserLoaded(browser);
-  sessionHistory = browser.sessionHistory;
-  is(sessionHistory.count, 2, "Should be two history entries");
-  is(sessionHistory.getEntryAtIndex(0, false).URI.spec, "about:blank", "Should be the right URL");
-  is(sessionHistory.getEntryAtIndex(1, false).URI.spec, "about:robots", "Should be the right URL");
+  yield ContentTask.spawn(browser, null, function() {
+    let sessionHistory = docShell.QueryInterface(Ci.nsIInterfaceRequestor)
+                                 .getInterface(Ci.nsISHistory);
+    is(sessionHistory.count, 2, "Should be two history entries");
+    is(sessionHistory.getEntryAtIndex(0, false).URI.spec, "about:blank", "Should be the right URL");
+    is(sessionHistory.getEntryAtIndex(1, false).URI.spec, "about:robots", "Should be the right URL");
+  });
 
   
-  gBrowser.removeTab(tab);
+  yield promiseRemoveTab(tab);
 });
