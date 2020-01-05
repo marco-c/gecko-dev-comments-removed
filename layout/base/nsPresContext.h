4100e960a1009a5d56da55b1c3e7304c6bb411a0
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 17:38:51 +0000

    Bug 1340904 - Implement telemetry scroll tracking in C++ r=smaug
    UltraBlame original commit: 11b27d2c87e71f1ed379c99c3f9ff5ca72d7e042

--- layout/base/nsPresContext.h
+++ layout/base/nsPresContext.h
@@ -888,6 +888,29 @@ public:
     return mFramesReflowed;
   }
 
+  
+
+
+
+  void SetTelemetryScrollY(nscoord aScrollY)
+  {
+    nscoord delta = abs(aScrollY - mTelemetryScrollLastY);
+    mTelemetryScrollLastY = aScrollY;
+
+    mTelemetryScrollTotalY += delta;
+    if (aScrollY > mTelemetryScrollMaxY) {
+      mTelemetryScrollMaxY = aScrollY;
+    }
+  }
+  nscoord TelemetryScrollMaxY() const
+  {
+    return mTelemetryScrollMaxY;
+  }
+  nscoord TelemetryScrollTotalY() const
+  {
+    return mTelemetryScrollTotalY;
+  }
+
   static nscoord GetBorderWidthForKeyword(unsigned int aBorderWidthKeyword)
   {
     
@@ -1358,6 +1381,10 @@ protected:
 
   
   mozilla::TimeStamp    mLastStyleUpdateForAllAnimations;
+
+  nscoord mTelemetryScrollLastY;
+  nscoord mTelemetryScrollMaxY;
+  nscoord mTelemetryScrollTotalY;
 
   unsigned              mHasPendingInterrupt : 1;
   unsigned              mPendingInterruptFromTest : 1;
