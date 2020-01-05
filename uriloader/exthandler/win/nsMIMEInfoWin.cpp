da950e4a932977b74363de5253d63ca60d52141e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 17:03:30 +0000

    Bug 1340803 - Make nsMIMEInfoWin::GetPossibleLocalHandlers check fileExt is non-empty before checking its first char. r=jimm
    UltraBlame original commit: f4eae24795b9ae702254e576a1f2c5d8d72c7a60

--- uriloader/exthandler/win/nsMIMEInfoWin.cpp
+++ uriloader/exthandler/win/nsMIMEInfoWin.cpp
@@ -612,8 +612,9 @@ nsMIMEInfoWin::GetPossibleLocalHandlers(nsIArray **_retval)
   }
 
   nsAutoString fileExtToUse;
-  if (fileExt.First() != '.')
+  if (!fileExt.IsEmpty() && fileExt.First() != '.') {
     fileExtToUse = char16_t('.');
+  }
   fileExtToUse.Append(NS_ConvertUTF8toUTF16(fileExt));
 
   
