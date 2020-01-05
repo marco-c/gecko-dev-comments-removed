ca00427b6664c3bbe56e7bf3e51c578d9205bcb0
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 10:15:00 +0000

    servo: Merge #6603 - Remove url field from Page (from wjh:remove-url-from-page-6589); r=jdm
    Resolves issue [#6589](https://github.com/servo/servo/issues/6589).
    
    Source-Repo: https://github.com/servo/servo
    Source-Revision: 3d4122e7c1e9e412d88f69b853af5265a4c9f0fe
    
    UltraBlame original commit: 42944cfafb9bd942a405a3d1405a97299dfbf764

--- servo/components/script/page.rs
+++ servo/components/script/page.rs
@@ -11,7 +11,6 @@ use dom::window::Window;
 use msg::constellation_msg::PipelineId;
 use std::cell::Cell;
 use std::rc::Rc;
-use url::Url;
 
 
 #[derive(JSTraceable)]
@@ -21,11 +20,6 @@ pub struct Page {
 
     
     frame: DOMRefCell<Option<Frame>>,
-
-    
-    
-    
-    url: Url,
 
     
     needs_reflow: Cell<bool>,
@@ -61,11 +55,10 @@ impl IterablePage for Rc<Page> {
 }
 
 impl Page {
-    pub fn new(id: PipelineId, url: Url) -> Page {
+    pub fn new(id: PipelineId) -> Page {
         Page {
             id: id,
             frame: DOMRefCell::new(None),
-            url: url,
             needs_reflow: Cell::new(true),
             children: DOMRefCell::new(vec!()),
         }
