8f45cc793132d4e890d78bf354f8e5d918a207ca
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 16:24:28 +0000

    Bug 1226398 - Part 1: Remove remaining legacy generator from dom/indexedDB/test/. r=jaws
    UltraBlame original commit: f248ec7b14ab68461d112afa97794e0a28f21939

--- dom/indexedDB/test/unit/test_globalObjects_ipc.js
+++ dom/indexedDB/test/unit/test_globalObjects_ipc.js
@@ -5,7 +5,7 @@
 
 var testGenerator = testSteps();
 
-function testSteps()
+function* testSteps()
 {
   
   run_test_in_child("./GlobalObjectsChild.js", function() {
--- dom/indexedDB/test/unit/test_oldDirectories.js
+++ dom/indexedDB/test/unit/test_oldDirectories.js
@@ -5,7 +5,7 @@
 
 var testGenerator = testSteps();
 
-function testSteps()
+function* testSteps()
 {
   
   const url = "http://www.mozilla.org";
