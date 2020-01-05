487f3557018f50d9eea886b1f25f4341d8c1240e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 21:02:26 +0000

    servo: Merge #16363 - stylo: Use prefixed values in ExtremumLength (from Manishearth:stylo-prefixed); r=upsuper
    r=xidorn https://bugzilla.mozilla.org/show_bug.cgi?id=1355674
    
    Source-Repo: https://github.com/servo/servo
    Source-Revision: 3c5a21ebf39b31cb77cb1a2e162e4abdac325e74
    
    UltraBlame original commit: 314cb20d1da4da98c0cb83b4881f2a7e6591b2ab

--- servo/components/style/values/mod.rs
+++ servo/components/style/values/mod.rs
@@ -192,7 +192,7 @@ impl<A: ToComputedValue, B: ToComputedValue> ToComputedValue for Either<A, B> {
 
 
 define_css_keyword_enum!(ExtremumLength:
-                         "max-content" => MaxContent,
-                         "min-content" => MinContent,
-                         "fit-content" => FitContent,
-                         "fill-available" => FillAvailable);
+                         "-moz-max-content" => MaxContent,
+                         "-moz-min-content" => MinContent,
+                         "-moz-fit-content" => FitContent,
+                         "-moz-available" => FillAvailable);
