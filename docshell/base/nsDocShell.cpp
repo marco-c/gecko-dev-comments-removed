52839098050c91c35c557ceff6c031257608eb5b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 03:30:02 +0000

    Bug 1316889 - Remove dead triggeringPrincipal fallback in docshell. r=bz
    UltraBlame original commit: af938e1a6b64ed5f062028badaceec25eb9b0ee6

--- docshell/base/nsDocShell.cpp
+++ docshell/base/nsDocShell.cpp
@@ -9876,18 +9876,10 @@ nsDocShell::InternalLoad(nsIURI* aURI,
 #endif
     }
 
-    
-    nsCOMPtr<nsIPrincipal> requestingPrincipal = aTriggeringPrincipal;
-    if (!requestingPrincipal && aReferrer) {
-      rv =
-        CreatePrincipalFromReferrer(aReferrer, getter_AddRefs(requestingPrincipal));
-      NS_ENSURE_SUCCESS(rv, rv);
-    }
-
     int16_t shouldLoad = nsIContentPolicy::ACCEPT;
     rv = NS_CheckContentLoadPolicy(contentType,
                                    aURI,
-                                   requestingPrincipal,
+                                   aTriggeringPrincipal,
                                    requestingContext,
                                    EmptyCString(),  
                                    nullptr,  
