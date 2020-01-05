54adaf1d8f470bb583232e1d6284a23d7ee5406b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 07:31:02 +0000

    Bug 1331804 - New naming scheme for runnables (r=ehsan)
    MozReview-Commit-ID: FOfrUXwGYws
    
    UltraBlame original commit: ea9be6b31ed04d0f9df8caede694a1975494a950

--- parser/html/nsHtml5TreeOpExecutor.cpp
+++ parser/html/nsHtml5TreeOpExecutor.cpp
@@ -265,7 +265,7 @@ nsHtml5TreeOpExecutor::ContinueInterruptedParsingAsync()
 {
   if (!mDocument || !mDocument->IsInBackgroundWindow()) {
     nsCOMPtr<nsIRunnable> flusher = new nsHtml5ExecutorReflusher(this);  
-    if (NS_FAILED(mDocument->Dispatch("ContinueInterruptedParsingAsync",
+    if (NS_FAILED(mDocument->Dispatch("nsHtml5ExecutorReflusher",
                                       dom::TaskCategory::Other,
                                       flusher.forget()))) {
       NS_WARNING("failed to dispatch executor flush event");
@@ -647,10 +647,6 @@ nsHtml5TreeOpExecutor::RunScript(nsIContent* aScriptElement)
 
   NS_ASSERTION(aScriptElement, "No script to run");
   nsCOMPtr<nsIScriptElement> sele = do_QueryInterface(aScriptElement);
-  if (!sele) {
-    MOZ_ASSERT(nsNameSpaceManager::GetInstance()->mSVGDisabled, "Node didn't QI to script, but SVG wasn't disabled.");
-    return;
-  }
   
   if (!mParser) {
     NS_ASSERTION(sele->IsMalformed(), "Script wasn't marked as malformed.");
