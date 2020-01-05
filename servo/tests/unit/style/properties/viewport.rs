643217d66e310fa9e37d30f7e66b1ae8e125f415
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 18:42:21 +0000

    servo: Merge #15997 - Rearrange PropertyDeclaration to avoid embedding DeclaredValue (from bholley:rearrange_propdecl); r=mbrubeck
    From https://bugzilla.mozilla.org/show_bug.cgi?id=1347719
    
    This effectively combines the discriminants of the two enums and reduces the
    size of PropertyDeclaration by one word.
    
    Source-Repo: https://github.com/servo/servo
    Source-Revision: 5d61afd11876fc82e3d3c52ddd8f7aa3e2c3de47
    
    UltraBlame original commit: 0efb2fdb3ee884ac6a3241a09fd3f1536edee41e

--- servo/tests/unit/style/properties/viewport.rs
+++ servo/tests/unit/style/properties/viewport.rs
@@ -3,7 +3,7 @@
 
 
 use app_units::Au;
-use style::properties::{DeclaredValue, PropertyDeclaration};
+use style::properties::PropertyDeclaration;
 use style::properties::longhands::border_top_width;
 use style::values::HasViewportPercentage;
 use style::values::specified::{Length, NoCalcLength, ViewportPercentageLength};
@@ -12,16 +12,16 @@ use style::values::specified::{Length, NoCalcLength, ViewportPercentageLength};
 fn has_viewport_percentage_for_specified_value() {
     
     let pvw = PropertyDeclaration::BorderTopWidth(
-        DeclaredValue::Value(border_top_width::SpecifiedValue::from_length(
+        border_top_width::SpecifiedValue::from_length(
             Length::NoCalc(NoCalcLength::ViewportPercentage(ViewportPercentageLength::Vw(100.)))
-        ))
+        )
     );
     assert!(pvw.has_viewport_percentage());
 
     let pabs = PropertyDeclaration::BorderTopWidth(
-        DeclaredValue::Value(border_top_width::SpecifiedValue::from_length(
+        border_top_width::SpecifiedValue::from_length(
             Length::NoCalc(NoCalcLength::Absolute(Au(100)))
-        ))
+        )
     );
     assert!(!pabs.has_viewport_percentage());
 }
