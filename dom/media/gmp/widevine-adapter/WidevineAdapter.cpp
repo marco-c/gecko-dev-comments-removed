0d99165821e753fe8d12f2d95a292458b9c2e746
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 16:45:31 +0000

    Bug 1341135 - Rename CDM log macro from Log to CDM_LOG. r=gerald
    This prevents the Log macro from colliding with the Log function on
    IPC ParamTraits definitions.
    
    MozReview-Commit-ID: Hd2v6ilbmGc
    
    UltraBlame original commit: 9f04fef720ecdedfc9ecc0aa27f1a3e07b6c4054

--- dom/media/gmp/widevine-adapter/WidevineAdapter.cpp
+++ dom/media/gmp/widevine-adapter/WidevineAdapter.cpp
@@ -1,7 +1,7 @@
-/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+
+
+
 
 #include "WidevineAdapter.h"
 #include "content_decryption_module.h"
@@ -23,7 +23,7 @@ GMPErr GMPGetCurrentTime(GMPTimestamp* aOutTime) {
   return sPlatform->getcurrenttime(aOutTime);
 }
 
-// Call on main thread only.
+
 GMPErr GMPSetTimerOnMainThread(GMPTask* aTask, int64_t aTimeoutMS) {
   return sPlatform->settimer(aTask, aTimeoutMS);
 }
@@ -44,7 +44,7 @@ WidevineAdapter::SetAdaptee(PRLibrary* aLib)
 
 void* GetCdmHost(int aHostInterfaceVersion, void* aUserData)
 {
-  Log("GetCdmHostFunc(%d, %p)", aHostInterfaceVersion, aUserData);
+  CDM_LOG("GetCdmHostFunc(%d, %p)", aHostInterfaceVersion, aUserData);
   WidevineDecryptor* decryptor = reinterpret_cast<WidevineDecryptor*>(aUserData);
   MOZ_ASSERT(decryptor);
   return static_cast<cdm::Host_8*>(decryptor);
@@ -67,7 +67,7 @@ WidevineAdapter::GMPInit(const GMPPlatformAPI* aPlatformAPI)
     return GMPGenericErr;
   }
 
-  Log(STRINGIFY(INITIALIZE_CDM_MODULE)"()");
+  CDM_LOG(STRINGIFY(INITIALIZE_CDM_MODULE)"()");
   init();
 
   return GMPNoErr;
@@ -79,19 +79,19 @@ WidevineAdapter::GMPGetAPI(const char* aAPIName,
                            void** aPluginAPI,
                            uint32_t aDecryptorId)
 {
-  Log("WidevineAdapter::GMPGetAPI(%s, 0x%p, 0x%p, %u) this=0x%p",
-      aAPIName, aHostAPI, aPluginAPI, aDecryptorId, this);
+  CDM_LOG("WidevineAdapter::GMPGetAPI(%s, 0x%p, 0x%p, %u) this=0x%p",
+          aAPIName, aHostAPI, aPluginAPI, aDecryptorId, this);
   if (!strcmp(aAPIName, GMP_API_DECRYPTOR)) {
     if (WidevineDecryptor::GetInstance(aDecryptorId)) {
-      // We only support one CDM instance per PGMPDecryptor. Fail!
-      Log("WidevineAdapter::GMPGetAPI() Tried to create more than once CDM per IPDL actor! FAIL!");
+      
+      CDM_LOG("WidevineAdapter::GMPGetAPI() Tried to create more than once CDM per IPDL actor! FAIL!");
       return GMPQuotaExceededErr;
     }
     auto create = reinterpret_cast<decltype(::CreateCdmInstance)*>(
       PR_FindFunctionSymbol(mLib, "CreateCdmInstance"));
     if (!create) {
-      Log("WidevineAdapter::GMPGetAPI(%s, 0x%p, 0x%p, %u) this=0x%p FAILED to find CreateCdmInstance",
-        aAPIName, aHostAPI, aPluginAPI, aDecryptorId, this);
+      CDM_LOG("WidevineAdapter::GMPGetAPI(%s, 0x%p, 0x%p, %u) this=0x%p FAILED to find CreateCdmInstance",
+              aAPIName, aHostAPI, aPluginAPI, aDecryptorId, this);
       return GMPGenericErr;
     }
 
@@ -104,11 +104,11 @@ WidevineAdapter::GMPGetAPI(const char* aAPIName,
              &GetCdmHost,
              decryptor));
     if (!cdm) {
-      Log("WidevineAdapter::GMPGetAPI(%s, 0x%p, 0x%p, %u) this=0x%p FAILED to create cdm",
-          aAPIName, aHostAPI, aPluginAPI, aDecryptorId, this);
+      CDM_LOG("WidevineAdapter::GMPGetAPI(%s, 0x%p, 0x%p, %u) this=0x%p FAILED to create cdm",
+              aAPIName, aHostAPI, aPluginAPI, aDecryptorId, this);
       return GMPGenericErr;
     }
-    Log("cdm: 0x%p", cdm);
+    CDM_LOG("cdm: 0x%p", cdm);
     RefPtr<CDMWrapper> wrapper(new CDMWrapper(cdm, decryptor));
     decryptor->SetCDM(wrapper, aDecryptorId);
     *aPluginAPI = decryptor;
@@ -116,12 +116,12 @@ WidevineAdapter::GMPGetAPI(const char* aAPIName,
   } else if (!strcmp(aAPIName, GMP_API_VIDEO_DECODER)) {
     RefPtr<CDMWrapper> wrapper = WidevineDecryptor::GetInstance(aDecryptorId);
 
-    // There is a possible race condition, where the decryptor will be destroyed
-    // before we are able to create the video decoder, so we create a dummy
-    // decoder to avoid crashing.
+    
+    
+    
     if (!wrapper) {
-      Log("WidevineAdapter::GMPGetAPI(%s, 0x%p, 0x%p, %u) this=0x%p No cdm for video decoder. Using a DummyDecoder",
-          aAPIName, aHostAPI, aPluginAPI, aDecryptorId, this);
+      CDM_LOG("WidevineAdapter::GMPGetAPI(%s, 0x%p, 0x%p, %u) this=0x%p No cdm for video decoder. Using a DummyDecoder",
+              aAPIName, aHostAPI, aPluginAPI, aDecryptorId, this);
 
       *aPluginAPI = new WidevineDummyDecoder();
     } else {
@@ -135,17 +135,17 @@ WidevineAdapter::GMPGetAPI(const char* aAPIName,
 void
 WidevineAdapter::GMPShutdown()
 {
-  Log("WidevineAdapter::GMPShutdown()");
+  CDM_LOG("WidevineAdapter::GMPShutdown()");
 
   decltype(::DeinitializeCdmModule)* deinit;
   deinit = (decltype(deinit))(PR_FindFunctionSymbol(mLib, "DeinitializeCdmModule"));
   if (deinit) {
-    Log("DeinitializeCdmModule()");
+    CDM_LOG("DeinitializeCdmModule()");
     deinit();
   }
 }
 
-/* static */
+
 bool
 WidevineAdapter::Supports(int32_t aModuleVersion,
                           int32_t aInterfaceVersion,
@@ -156,4 +156,4 @@ WidevineAdapter::Supports(int32_t aModuleVersion,
          aHostVersion == cdm::Host_8::kVersion;
 }
 
-} // namespace mozilla
+} 
