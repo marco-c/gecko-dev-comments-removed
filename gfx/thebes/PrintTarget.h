0a6f97879c0ffc4b8d013cc7cae84ec78a0914e0
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 03:43:18 +0000

    Bug 1317295 - Record PrintTarget::GetReferenceDrawTarget DrawTargets. r=edwin
    UltraBlame original commit: 32387613c3056728ab0ed2da07cf4d29265baf06

--- gfx/thebes/PrintTarget.h
+++ gfx/thebes/PrintTarget.h
@@ -131,7 +131,7 @@ public:
 
 
 
-  virtual already_AddRefed<DrawTarget> GetReferenceDrawTarget();
+  virtual already_AddRefed<DrawTarget> GetReferenceDrawTarget(DrawEventRecorder* aRecorder);
 
 protected:
 
