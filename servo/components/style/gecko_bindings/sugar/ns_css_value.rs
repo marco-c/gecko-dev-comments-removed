b2132586b19eb5a1554358e8626e02827ee22b78
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 19:19:22 +0000

    servo: Merge #16100 - stylo: Use a threadsafe array for counters (from bholley:threadsafe_array_for_counters); r=upsuper
    See https://bugzilla.mozilla.org/show_bug.cgi?id=1348606
    
    Source-Repo: https://github.com/servo/servo
    Source-Revision: 6a9fe88929c6fbfb6584efff1a11eb493dce68cc
    
    UltraBlame original commit: 3fe21d1c94af155db6a36bd8d84b5d497b30fe3b

--- servo/components/style/gecko_bindings/sugar/ns_css_value.rs
+++ servo/components/style/gecko_bindings/sugar/ns_css_value.rs
@@ -6,7 +6,8 @@
 
 use app_units::Au;
 use gecko_bindings::bindings;
-use gecko_bindings::structs::{nsCSSValue, nsCSSUnit, nsCSSValue_Array, nscolor};
+use gecko_bindings::structs::{nsCSSValue, nsCSSUnit};
+use gecko_bindings::structs::{nsCSSValue_Array, nsCSSValue_ThreadSafeArray, nscolor};
 use std::mem;
 use std::ops::{Index, IndexMut};
 use std::slice;
@@ -126,8 +127,10 @@ impl Drop for nsCSSValue {
     }
 }
 
-impl nsCSSValue_Array {
-    
+macro_rules! decl_cssarray_sugar {
+($name:ident) => {
+impl $name {
+    /// Return the length of this `nsCSSValue::Array`
     #[inline]
     pub fn len(&self) -> usize {
         self.mCount
@@ -138,20 +141,20 @@ impl nsCSSValue_Array {
         self.mArray.as_ptr()
     }
 
-    
+    /// Get the array as a slice of nsCSSValues.
     #[inline]
     pub fn as_slice(&self) -> &[nsCSSValue] {
         unsafe { slice::from_raw_parts(self.buffer(), self.len()) }
     }
 
-    
+    /// Get the array as a mutable slice of nsCSSValues.
     #[inline]
     pub fn as_mut_slice(&mut self) -> &mut [nsCSSValue] {
         unsafe { slice::from_raw_parts_mut(self.buffer() as *mut _, self.len()) }
     }
 }
 
-impl Index<usize> for nsCSSValue_Array {
+impl Index<usize> for $name {
     type Output = nsCSSValue;
     #[inline]
     fn index(&self, i: usize) -> &nsCSSValue {
@@ -159,10 +162,14 @@ impl Index<usize> for nsCSSValue_Array {
     }
 }
 
-impl IndexMut<usize> for nsCSSValue_Array {
+impl IndexMut<usize> for $name {
     #[inline]
     fn index_mut(&mut self, i: usize) -> &mut nsCSSValue {
         &mut self.as_mut_slice()[i]
     }
 }
+}
+}
 
+decl_cssarray_sugar!(nsCSSValue_Array);
+decl_cssarray_sugar!(nsCSSValue_ThreadSafeArray);
