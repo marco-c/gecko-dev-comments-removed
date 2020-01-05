371f6a44fc1111a7cb90e074c8e36cddd5386426
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 17:26:57 +0000

    Bug 1340195 - Remove e10s block for LastPass. r=mconley
    MozReview-Commit-ID: 3EiKLNsUGYR
    
    UltraBlame original commit: 4ff72c2c865f1ea79d404faa0a52ac8b5dd90a57

--- browser/extensions/e10srollout/bootstrap.js
+++ browser/extensions/e10srollout/bootstrap.js
@@ -73,9 +73,7 @@ function defineCohort() {
 
     Preferences.set(PREF_E10S_ADDON_BLOCKLIST,
                     
-                    "{dc572301-7619-498c-a57d-39143191b318};" +
-                    
-                    "support@lastpass.com;");
+                    "{dc572301-7619-498c-a57d-39143191b318};");
   } else {
     Preferences.reset(PREF_E10S_ADDON_POLICY);
   }
