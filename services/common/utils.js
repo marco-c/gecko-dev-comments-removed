e1325c930f4c2be6c362ecb0e0623493deea01a4
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 16:18:25 +0000

    Bug 1338668 - Ignore tag query folder IDs when comparing client and server bookmark records. r=tcsc
    MozReview-Commit-ID: 2Mz9fTsTou5
    
    UltraBlame original commit: 2cb7e2608b1d0697364694ca8faf99692727c2ec

--- services/common/utils.js
+++ services/common/utils.js
@@ -74,6 +74,23 @@ this.CommonUtils = {
 
 
 
+  arrayEqual(a, b) {
+    if (a.length !== b.length) {
+      return false;
+    }
+    for (let i = 0; i < a.length; i++) {
+      if (a[i] !== b[i]) {
+        return false;
+      }
+    }
+    return true;
+  },
+
+  
+
+
+
+
 
 
 
