97721087854fbf3bb848724daa5c958e74bc1d10
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 01:09:11 +0000

    Bug 1308121-Handle unrecognized content type parameter when trying to get supported capability. r=cpearce
    MozReview-Commit-ID: GqSjn6BjhTi
    
    UltraBlame original commit: 3f78ba15eb60f44ad44f0a61046fc4e8216b5785

--- dom/media/eme/MediaKeySystemAccess.cpp
+++ dom/media/eme/MediaKeySystemAccess.cpp
@@ -641,6 +641,37 @@ AllCodecsOfType(const nsTArray<GMPCodecString>& aCodecs, const CodecType aCodecT
   return true;
 }
 
+static bool
+IsParameterUnrecognized(const nsAString& aContentType)
+{
+  nsAutoString contentType(aContentType);
+  contentType.StripWhitespace();
+
+  nsTArray<nsString> params;
+  nsAString::const_iterator start, end, semicolon, equalSign;
+  contentType.BeginReading(start);
+  contentType.EndReading(end);
+  semicolon = start;
+  
+  while (semicolon != end) {
+    if (FindCharInReadable(';', semicolon, end)) {
+      equalSign = ++semicolon;
+      if (FindCharInReadable('=', equalSign, end)) {
+        params.AppendElement(Substring(semicolon, equalSign));
+        semicolon = equalSign;
+      }
+    }
+  }
+
+  for (auto param : params) {
+    if (!param.LowerCaseEqualsLiteral("codecs") &&
+        !param.LowerCaseEqualsLiteral("profiles")) {
+      return true;
+    }
+  }
+  return false;
+}
+
 
 static Sequence<MediaKeySystemMediaCapability>
 GetSupportedCapabilities(const CodecType aCodecType,
@@ -749,6 +780,10 @@ GetSupportedCapabilities(const CodecType aCodecType,
     
     
     
+    if (IsParameterUnrecognized(contentType)) {
+      continue;
+    }
+
     
     
     
