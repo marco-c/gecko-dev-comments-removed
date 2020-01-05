5a4b3a18bf40694221fec4762f1fcffada35edc3
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 04:40:31 +0000

    Bug 1321387 part 1: Wrap ReflowOutput impl in "namespace mozilla {}". r=TYLin
    The class definition (in the header file) is already inside the namespace. But
    the method implementations are not (until now), and they've only been compiling
    successfully thanks to a "using namespace mozilla" that this file was picking
    up from another .cpp file, via unified builds.
    
    MozReview-Commit-ID: ImRBpAVA0en
    
    UltraBlame original commit: 5dc43b5e3673c0ffac1099fbd14a2b31f9263e10

--- layout/generic/ReflowOutput.cpp
+++ layout/generic/ReflowOutput.cpp
@@ -1,9 +1,9 @@
-/* vim: set shiftwidth=2 tabstop=8 autoindent cindent expandtab: */
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-/* struct containing the output from nsIFrame::Reflow */
+
+
+
+
+
 
 #include "mozilla/ReflowOutput.h"
 #include "mozilla/ReflowInput.h"
@@ -11,8 +11,8 @@
 void
 nsOverflowAreas::UnionWith(const nsOverflowAreas& aOther)
 {
-  // FIXME: We should probably change scrollable overflow to use
-  // UnionRectIncludeEmpty (but leave visual overflow using UnionRect).
+  
+  
   NS_FOR_FRAME_OVERFLOW_TYPES(otype) {
     mRects[otype].UnionRect(mRects[otype], aOther.mRects[otype]);
   }
@@ -21,8 +21,8 @@ nsOverflowAreas::UnionWith(const nsOverflowAreas& aOther)
 void
 nsOverflowAreas::UnionAllWith(const nsRect& aRect)
 {
-  // FIXME: We should probably change scrollable overflow to use
-  // UnionRectIncludeEmpty (but leave visual overflow using UnionRect).
+  
+  
   NS_FOR_FRAME_OVERFLOW_TYPES(otype) {
     mRects[otype].UnionRect(mRects[otype], aRect);
   }
@@ -35,6 +35,8 @@ nsOverflowAreas::SetAllTo(const nsRect& aRect)
     mRects[otype] = aRect;
   }
 }
+
+namespace mozilla {
 
 ReflowOutput::ReflowOutput(const ReflowInput& aState,
                                          uint32_t aFlags)
@@ -57,11 +59,13 @@ ReflowOutput::SetOverflowAreasToDesiredBounds()
 void
 ReflowOutput::UnionOverflowAreasWithDesiredBounds()
 {
-  // FIXME: We should probably change scrollable overflow to use
-  // UnionRectIncludeEmpty (but leave visual overflow using UnionRect).
+  
+  
   nsRect rect(0, 0, Width(), Height());
   NS_FOR_FRAME_OVERFLOW_TYPES(otype) {
     nsRect& o = mOverflowAreas.Overflow(otype);
     o.UnionRect(o, rect);
   }
 }
+
+} 
