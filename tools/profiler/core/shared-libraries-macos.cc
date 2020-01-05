6bcb8522756e02cd744be59a9a22366df01f6fa1
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 18:43:20 +0000

    Bug 1329111 - Record shared library architecture on Mac. r=glandium,njn
    This is interesting information on Mac because some system libraries come in
    two 64 bit versions: a regular one, and one that contains Haswell-specific
    instructions. The former 'architecture' is called x86_64 and the latter is
    called x86_64h (h for Haswell).
    
    We set arch to the empty string on non-Mac platforms. It's not all that
    interesting on those platforms because there will be only one architecture
    in the binary at the given path, unlike on Mac where you can have fat
    binaries with multiple architectures.
    
    MozReview-Commit-ID: Dgnslv0D3Ug
    
    UltraBlame original commit: cdff9dc4a8ba63be253c3360357071ed0d308545

--- tools/profiler/core/shared-libraries-macos.cc
+++ tools/profiler/core/shared-libraries-macos.cc
@@ -4,6 +4,7 @@
 
 
 #include <AvailabilityMacros.h>
+#include <mach-o/arch.h>
 #include <mach-o/loader.h>
 #include <mach-o/dyld_images.h>
 #include <mach/task_info.h>
@@ -81,9 +82,13 @@ void addSharedLibrary(const platform_mach_header* header, char *path, SharedLibr
     nameStr.Cut(0, pos + 1);
   }
 
+  const NXArchInfo* archInfo =
+    NXGetArchInfoFromCpuType(header->cputype, header->cpusubtype);
+
   info.AddSharedLibrary(SharedLibrary(start, start + size, 0, uuid.str(),
                                       nameStr, pathStr, nameStr, pathStr,
-                                      ""));
+                                      "",
+                                      archInfo ? archInfo->name : ""));
 }
 
 
