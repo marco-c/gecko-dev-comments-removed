d6ea0d04e7bb68e4173b11f2bb8b948392ccca85
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 06:29:13 +0000

    Bug 1329726 - part 2 - ensure {,HOST_}RUST_PROGRAMS are hooked into root-deps.mk; r=chmanchester
    If we don't do this, a directory with only
    RUST_PROGRAMS (resp. HOST_RUST_PROGRAMS) won't get traversed properly.
    
    UltraBlame original commit: 479b8c84babf0c5d5cd69dc75144eeffb7ab547c

--- python/mozbuild/mozbuild/backend/recursivemake.py
+++ python/mozbuild/mozbuild/backend/recursivemake.py
@@ -539,13 +539,16 @@ class RecursiveMakeBackend(CommonBackend):
             backend_file.write('JAR_MANIFEST := %s\n' % obj.path.full_path)
 
         elif isinstance(obj, RustProgram):
-            
-            
-            
             self._process_rust_program(obj, backend_file)
+            
+            build_target = self._build_target_for_obj(obj)
+            self._compile_graph[build_target]
 
         elif isinstance(obj, HostRustProgram):
             self._process_host_rust_program(obj, backend_file)
+            
+            build_target = self._build_target_for_obj(obj)
+            self._compile_graph[build_target]
 
         elif isinstance(obj, Program):
             self._process_program(obj.program, backend_file)
