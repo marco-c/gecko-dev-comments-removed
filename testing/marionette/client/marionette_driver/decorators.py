ecc6d447090bfd8f5b6eca7b931cd748b26d78cf
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 03:09:57 +0000

    Bug 1299216 - Enhance handling of crashes in Marionette. r=automatedtester
    There are currently various issues in Marionette which cause extra and unnecessary logging output
    in case of chrome and content crashes. Especially the latter ones are not easy to discover. This
    patch makes the following improvements:
    
    * Correctly mark process crashes as IOError test failure. Until now we have only used the "Process
      has been closed" message, which does not correctly cover it, especially not if the process shutdown without
      a crash.
    
    * Allow changing the socket_timeout for the socket client. This was mainly necessary to allow unit tests
      to run faster.
    
    * Collect the number of crashes so it's known even later if the process has been crashed during the
      current session. It also fixes the case when check_for_crash() gets called twice, and for the
      second time False is returned which will trigger an invalid code path.
    
    * Reduce code duplication when destroying a session.
    
    * Adding a unit test to verify the correct behavior for chrome and content crashes.
    
    MozReview-Commit-ID: KdUQuJqFRli
    
    UltraBlame original commit: 32b908ad1b02660ff9bce79cbae9b8b4e5515a94

--- testing/marionette/client/marionette_driver/decorators.py
+++ testing/marionette/client/marionette_driver/decorators.py
@@ -33,31 +33,34 @@ def do_process_check(func, always=False):
 
         def check_for_crash():
             try:
-                m.check_for_crash()
-            except:
+                return m.check_for_crash()
+            except Exception:
                 
                 traceback.print_exc()
+
+                return False
 
         try:
             return func(*args, **kwargs)
         except (MarionetteException, IOError) as e:
             exc, val, tb = sys.exc_info()
+            crashed = False
 
             
             
             
             if not isinstance(e, MarionetteException) or type(e) is MarionetteException:
                 if not always:
-                    check_for_crash()
+                    crashed = check_for_crash()
 
             
-            if type(e) in (socket.error, socket.timeout):
-                m.force_shutdown()
+            if type(e) in (socket.error, socket.timeout) or crashed:
+                m.handle_socket_failure(crashed)
 
             raise exc, val, tb
         finally:
             if always:
-                check_for_crash(m)
+                check_for_crash()
     return _
 
 
