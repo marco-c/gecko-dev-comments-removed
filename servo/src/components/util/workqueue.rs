20300c7070727229b8e073ddb42dbf8909b51d48
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 09:03:25 +0000

    servo: Merge #1948 - Set a name for WorkQueue tasks (fixes #1830) (from mbrubeck:task-name); r=jdm
    This is my first Servo patch.  Please don't hesitate to offer basic Rust tips.
    
    Note: This assigns the same name to all of the worker tasks for a given WorkQueue.  Would it be useful to generate a unique name for each tasks (LayoutWorker1, LayoutWorker2...)?
    
    Source-Repo: https://github.com/servo/servo
    Source-Revision: ee88817461ea80f6e883626100d4ccf22f9796a5
    
    UltraBlame original commit: 606426755d29e227e43bd0130ed861712521f79c

--- servo/src/components/util/workqueue.rs
+++ servo/src/components/util/workqueue.rs
@@ -7,7 +7,7 @@
 
 
 
-use native;
+use task;
 use std::cast;
 use std::comm;
 use std::mem;
@@ -200,7 +200,7 @@ pub struct WorkQueue<QUD,WUD> {
 impl<QUD:Send,WUD:Send> WorkQueue<QUD,WUD> {
     /// Creates a new work queue and spawns all the threads associated with
     /// it.
-    pub fn new(thread_count: uint, user_data: QUD) -> WorkQueue<QUD,WUD> {
+    pub fn new(task_name: &str, thread_count: uint, user_data: QUD) -> WorkQueue<QUD,WUD> {
         // Set up data structures.
         let (supervisor_port, supervisor_chan) = Chan::new();
         let (mut infos, mut threads) = (~[], ~[]);
@@ -235,7 +235,7 @@ impl<QUD:Send,WUD:Send> WorkQueue<QUD,WUD> {
 
         // Spawn threads.
         for thread in threads.move_iter() {
-            native::task::spawn(proc() {
+            task::spawn_named(task_name.to_owned(), proc() {
                 let mut thread = thread;
                 thread.start()
             })
