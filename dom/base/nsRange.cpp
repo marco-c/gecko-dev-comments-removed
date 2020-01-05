1e4aead990e067aa161330ac73f4382cdf364876
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 18:21:47 +0000

    Backed out 2 changesets (bug 1343695) for osx chrome mochitest assertions a=backout
    Backed out changeset e5db40a036fe (bug 1343695)
    Backed out changeset 687ffd715113 (bug 1343695)
    
    MozReview-Commit-ID: 2CN1gipg3ia
    
    UltraBlame original commit: 41c24766c4ce0e2f98ae6a0a3b2a7d294e6a4544

--- dom/base/nsRange.cpp
+++ dom/base/nsRange.cpp
@@ -2906,6 +2906,13 @@ static nsresult GetPartialTextRect(nsLayoutUtils::RectCallback* aCallback,
 {
   nsTextFrame* textFrame = GetTextFrameForContent(aContent, aFlushLayout);
   if (textFrame) {
+    
+    nsAutoString textContent;
+    if (aTextList) {
+      mozilla::ErrorResult err; 
+      aContent->GetTextContent(textContent, err);
+    }
+
     nsIFrame* relativeTo = nsLayoutUtils::GetContainingBlockForClientRect(textFrame);
     for (nsTextFrame* f = textFrame; f; f = static_cast<nsTextFrame*>(f->GetNextContinuation())) {
       int32_t fstart = f->GetContentOffset(), fend = f->GetContentEnd();
@@ -2936,13 +2943,11 @@ static nsresult GetPartialTextRect(nsLayoutUtils::RectCallback* aCallback,
 
       
       if (aTextList) {
-        nsIFrame::RenderedText renderedText = f->GetRenderedText(
-          textContentStart,
-          textContentEnd,
-          nsIFrame::TextOffsetType::OFFSETS_IN_CONTENT_TEXT,
-          nsIFrame::TrailingWhitespace::DONT_TRIM_TRAILING_WHITESPACE);
-
-        aTextList->AppendElement(renderedText.mString, fallible);
+        const nsAString& textSubstring =
+          Substring(textContent,
+                    textContentStart,
+                    (textContentEnd - textContentStart));
+        aTextList->AppendElement(textSubstring, fallible);
       }
     }
   }
