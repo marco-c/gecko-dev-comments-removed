b93e99bc58d715796a37ec64b80a14ffb82e1c26
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 23:30:10 +0000

    bug 1358151: temporary workaround for rust race condition r=ted
    MozReview-Commit-ID: KiPArBKMSu1
    
    UltraBlame original commit: 3bc93cae8482f672720ca636e3dee5a65aabdcb3

--- toolkit/library/rust/shared/lib.rs
+++ toolkit/library/rust/shared/lib.rs
@@ -20,6 +20,16 @@ use std::os::raw::c_char;
 use std::panic;
 
 
+
+
+thread_local!(static UNUSED_THREAD_LOCAL: () = ());
+#[no_mangle]
+pub extern "C" fn rust_init_please_remove_this_after_updating_rust_1_19() {
+    UNUSED_THREAD_LOCAL.with(|_| ());
+}
+
+
+
 #[no_mangle]
 pub extern "C" fn intentional_panic(message: *const c_char) {
     panic!("{}", unsafe { CStr::from_ptr(message) }.to_string_lossy());
