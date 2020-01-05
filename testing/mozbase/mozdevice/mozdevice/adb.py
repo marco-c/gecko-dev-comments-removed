374390a84222a62a1161675a97473deb6b986c07
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 02:06:21 +0000

    Bug 1306703 - Autophone - mozdevice - minimal Android 7 support in adb*.py, r=droeh
    UltraBlame original commit: 4eba1ac43a15e170e67f19e9a7633cddf41c60c1

--- testing/mozbase/mozdevice/mozdevice/adb.py
+++ testing/mozbase/mozdevice/mozdevice/adb.py
@@ -602,14 +602,14 @@ class ADBDevice(ADBCommand):
         
         
         
-        if self.shell_bool("/system/bin/ls /", timeout=timeout):
+        if self.shell_bool("/system/bin/ls /data/local/tmp", timeout=timeout):
             self._ls = "/system/bin/ls"
-        elif self.shell_bool("/system/xbin/ls /", timeout=timeout):
+        elif self.shell_bool("/system/xbin/ls /data/local/tmp", timeout=timeout):
             self._ls = "/system/xbin/ls"
         else:
             raise ADBError("ADBDevice.__init__: ls not found")
         try:
-            self.shell_output("%s -1A /" % self._ls, timeout=timeout)
+            self.shell_output("%s -1A /data/local/tmp" % self._ls, timeout=timeout)
             self._ls += " -1A"
         except ADBError:
             self._ls += " -a"
