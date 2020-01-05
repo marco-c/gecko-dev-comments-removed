fb5a58382ee39e0c0f908480b68a57435f91b680
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 12:28:11 +0000

    Bug 1252960 - Rename UrlMetadata* table to UrlImageData* r=ahunt
    MozReview-Commit-ID: 2TNcctxAbRc
    
    UltraBlame original commit: af979611f4d15604b3469bd3b377570dd9f08446

--- mobile/android/base/java/org/mozilla/gecko/db/BrowserProvider.java
+++ mobile/android/base/java/org/mozilla/gecko/db/BrowserProvider.java
@@ -165,7 +165,7 @@ public class BrowserProvider extends SharedBrowserDatabaseProvider {
     static {
         sTables = new Table[] {
             
-            new URLMetadataTable()
+            new URLImageDataTable()
         };
         
         HashMap<String, String> map;
@@ -370,8 +370,8 @@ public class BrowserProvider extends SharedBrowserDatabaseProvider {
 
     
     
-    private URLMetadataTable getURLMetadataTable() {
-        return (URLMetadataTable) sTables[0];
+    private URLImageDataTable getURLImageDataTable() {
+        return (URLImageDataTable) sTables[0];
     }
 
     private static boolean hasFaviconsInProjection(String[] projection) {
@@ -2204,7 +2204,7 @@ public class BrowserProvider extends SharedBrowserDatabaseProvider {
 
         return deleteFavicons(uri, faviconSelection, null) +
                deleteThumbnails(uri, thumbnailSelection, null) +
-               getURLMetadataTable().deleteUnused(getWritableDatabase(uri));
+               getURLImageDataTable().deleteUnused(getWritableDatabase(uri));
     }
 
     @Override
