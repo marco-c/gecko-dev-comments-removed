cb35d7d272c49b55065139bb42137c9bd07ac34f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 23:47:09 +0000

    Bug 1363465 Adjust VersionsHelpers.h to versionhelpers.h for MinGW r=bas
    MozReview-Commit-ID: 7h04gOYwAJu
    
    UltraBlame original commit: 8941c8f4764340b52cd65984665a1b466c16f6ff

--- gfx/layers/d3d11/CompositorD3D11.cpp
+++ gfx/layers/d3d11/CompositorD3D11.cpp
@@ -34,7 +34,11 @@
 #include "D3D11ShareHandleImage.h"
 #include "DeviceAttachmentsD3D11.h"
 
+#ifdef __MINGW32__
+#include <versionhelpers.h> 
+#else
 #include <VersionHelpers.h> 
+#endif
 #include <winsdkver.h>
 
 namespace mozilla {
