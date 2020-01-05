3ff6102e6f6c7bcbaccc91c79caaae0864390d7c
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 00:59:38 +0000

    Bug 1308208 - remove redundant null check from GridLines::SetLineInfo. r=baku
    MozReview-Commit-ID: 55lIk7Klh4I
    
    UltraBlame original commit: 6d63144fb5dc98ed4556f0af0514a1a7c39c90cd

--- dom/grid/GridLines.cpp
+++ dom/grid/GridLines.cpp
@@ -68,6 +68,7 @@ GridLines::SetLineInfo(const ComputedGridTrackInfo* aTrackInfo,
                        const nsTArray<RefPtr<GridArea>>& aAreas,
                        bool aIsRow)
 {
+  MOZ_ASSERT(aLineInfo);
   mLines.Clear();
 
   if (!aTrackInfo) {
@@ -96,9 +97,7 @@ GridLines::SetLineInfo(const ComputedGridTrackInfo* aTrackInfo,
                          lastTrackEdge;
 
       nsTArray<nsString> lineNames;
-      if (aLineInfo) {
-        lineNames = aLineInfo->mNames.SafeElementAt(i, nsTArray<nsString>());
-      }
+      lineNames = aLineInfo->mNames.SafeElementAt(i, nsTArray<nsString>());
 
       
       for (auto area : aAreas) {
