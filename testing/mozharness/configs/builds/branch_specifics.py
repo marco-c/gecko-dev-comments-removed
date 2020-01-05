bd52b5114f078746b613329c6db32255af53d3c1
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 12:45:08 +0000

    Bug 1313389 - ci for quantum graphics r=bustage
    UltraBlame original commit: 299ee41d08aae7512994658acdb093584cb3966f

--- testing/mozharness/configs/builds/branch_specifics.py
+++ testing/mozharness/configs/builds/branch_specifics.py
@@ -52,7 +52,7 @@ config = {
                 'force_clobber': True,
             },
             'macosx64': {
-                'src_mozconfig': 'browser/config/mozconfigs/macosx64/release',
+                'src_mozconfig': 'browser/config/mozconfigs/macosx-universal/release',
                 'force_clobber': True,
             },
             'win32': {
@@ -75,10 +75,16 @@ config = {
             'linux64-asan': {
                 'update_channel': 'default',
             },
+            'linux64-cc': {
+                'update_channel': 'default',
+            },
             'linux64-st-an-debug': {
                 'update_channel': 'default',
             },
             'linux64-st-an': {
+                'update_channel': 'default',
+            },
+            'linux64-tsan': {
                 'update_channel': 'default',
             },
             'linux64-add-on-devel': {
@@ -126,7 +132,7 @@ config = {
                 'force_clobber': True,
             },
             'macosx64': {
-                'src_mozconfig': 'browser/config/mozconfigs/macosx64/beta',
+                'src_mozconfig': 'browser/config/mozconfigs/macosx-universal/beta',
                 'force_clobber': True,
             },
             'win32': {
@@ -149,10 +155,16 @@ config = {
             'linux64-asan': {
                 'update_channel': 'default',
             },
+            'linux64-cc': {
+                'update_channel': 'default',
+            },
             'linux64-st-an-debug': {
                 'update_channel': 'default',
             },
             'linux64-st-an': {
+                'update_channel': 'default',
+            },
+            'linux64-tsan': {
                 'update_channel': 'default',
             },
             'linux64-add-on-devel': {
@@ -224,10 +236,16 @@ config = {
             'linux64-asan': {
                 'update_channel': 'default',
             },
+            'linux64-cc': {
+                'update_channel': 'default',
+            },
             'linux64-st-an-debug': {
                 'update_channel': 'default',
             },
             'linux64-st-an': {
+                'update_channel': 'default',
+            },
+            'linux64-tsan': {
                 'update_channel': 'default',
             },
             'macosx64-debug': {
@@ -289,8 +307,67 @@ config = {
         "graph_server_branch_name": "UX",
         'stage_server': 'upload.ffxbld.productdelivery.prod.mozaws.net',
     },
+    
+    
     'date': {
-        'update_channel': 'nightly-date',
+        'update_channel': 'beta-dev',
+        'enable_release_promotion': 1,
+        'platform_overrides': {
+            'linux': {
+                'src_mozconfig': 'browser/config/mozconfigs/linux32/beta',
+            },
+            'linux-debug': {
+                'update_channel': 'default',
+            },
+            'linux64': {
+                'src_mozconfig': 'browser/config/mozconfigs/linux64/beta',
+            },
+            'linux64-debug': {
+                'update_channel': 'default',
+            },
+            'linux64-asan-debug': {
+                'update_channel': 'default',
+            },
+            'linux64-asan': {
+                'update_channel': 'default',
+            },
+            'linux64-cc': {
+                'update_channel': 'default',
+            },
+            'linux64-st-an-debug': {
+                'update_channel': 'default',
+            },
+            'linux64-st-an': {
+                'update_channel': 'default',
+            },
+            'linux64-tsan': {
+                'update_channel': 'default',
+            },
+            'macosx64': {
+                'src_mozconfig': 'browser/config/mozconfigs/macosx-universal/beta',
+            },
+            'macosx64-debug': {
+                'update_channel': 'default',
+            },
+            'macosx64-st-an': {
+                'update_channel': 'default',
+            },
+            'macosx64-st-an-debug': {
+                'update_channel': 'default',
+            },
+            'win32': {
+                'src_mozconfig': 'browser/config/mozconfigs/win32/beta',
+            },
+            'win32-debug': {
+                'update_channel': 'default',
+            },
+            'win64': {
+                'src_mozconfig': 'browser/config/mozconfigs/win64/beta',
+            },
+            'win64-debug': {
+                'update_channel': 'default',
+            },
+        },
         'stage_server': 'upload.ffxbld.productdelivery.prod.mozaws.net',
     },
     'cypress': {
@@ -343,10 +420,16 @@ config = {
             'linux64-asan': {
                 'update_channel': 'default',
             },
+            'linux64-cc': {
+                'update_channel': 'default',
+            },
             'linux64-st-an-debug': {
                 'update_channel': 'default',
             },
             'linux64-st-an': {
+                'update_channel': 'default',
+            },
+            'linux64-tsan': {
                 'update_channel': 'default',
             },
             'macosx64': {
