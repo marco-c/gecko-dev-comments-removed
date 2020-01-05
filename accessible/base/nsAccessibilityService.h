f78956ef89b1e67127595430f6e33b87d5031edb
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 05:04:48 +0000

    Bug 1313693 - implement iterable states, part1 - add 64bit version of states stringifying, r=davidb
    UltraBlame original commit: 2b63a4c846a3d727e63120ec269bada9e45164a9

--- accessible/base/nsAccessibilityService.h
+++ accessible/base/nsAccessibilityService.h
@@ -24,6 +24,11 @@ class nsPluginFrame;
 class nsITreeView;
 
 namespace mozilla {
+
+namespace dom {
+  class DOMStringList;
+}
+
 namespace a11y {
 
 class ApplicationAccessible;
@@ -108,6 +113,8 @@ public:
   
 
 
+  already_AddRefed<mozilla::dom::DOMStringList>
+    GetStringStates(uint64_t aStates) const;
   void GetStringStates(uint32_t aState, uint32_t aExtraState,
                        nsISupports **aStringStates);
 
