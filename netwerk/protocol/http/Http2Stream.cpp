6ca05f7af7f3d6699adfcc4729c7419622ed43df
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 20:42:58 +0000

    Bug 1352146 - Don't allow status phrases in http/2. r=mcmanus
    MozReview-Commit-ID: Cf30tUivhnB
    
    UltraBlame original commit: c8199958741afef261c03b81e5b4e89cb7918f38

--- netwerk/protocol/http/Http2Stream.cpp
+++ netwerk/protocol/http/Http2Stream.cpp
@@ -1022,6 +1022,19 @@ Http2Stream::ConvertResponseHeaders(Http2Decompressor *decompressor,
 
   nsresult errcode;
   httpResponseCode = statusString.ToInteger(&errcode);
+
+  
+  
+  
+  nsAutoCString parsedStatusString;
+  parsedStatusString.AppendInt(httpResponseCode);
+  if (!parsedStatusString.Equals(statusString)) {
+    LOG3(("Http2Stream::ConvertResposeHeaders %p status %s is not just a code",
+          this, statusString.BeginReading()));
+    
+    return NS_ERROR_ILLEGAL_VALUE;
+  }
+
   LOG3(("Http2Stream::ConvertResponseHeaders %p response code %d\n", this, httpResponseCode));
   if (mIsTunnel) {
     LOG3(("Http2Stream %p Tunnel Response code %d", this, httpResponseCode));
