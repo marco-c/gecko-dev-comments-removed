48b18ac6a906d85676343461bc0e6fb429fca66f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 18:13:49 +0000

    Bug 1342635 - Part 1: Add a telemetry probe for IPDL IPC deserialization time, r=billm
    MozReview-Commit-ID: 8XWFWoRECrC
    
    UltraBlame original commit: c41ed10a2c0528f82d52258e4c14f07fc407dd5f

--- ipc/chromium/src/base/pickle.h
+++ ipc/chromium/src/base/pickle.h
@@ -16,6 +16,7 @@
 #include "mozilla/Attributes.h"
 #include "mozilla/BufferList.h"
 #include "mozilla/mozalloc.h"
+#include "mozilla/TimeStamp.h"
 
 #ifdef FUZZING
 #include "base/singleton.h"
@@ -36,6 +37,7 @@ private:
   friend class Pickle;
 
   mozilla::BufferList<InfallibleAllocPolicy>::IterImpl iter_;
+  mozilla::TimeStamp start_;
 
   template<typename T>
   void CopyInto(T* dest);
@@ -127,7 +129,10 @@ class Pickle {
   }
 #endif
 
-  void EndRead(PickleIterator& iter) const;
+  
+  
+  
+  void EndRead(PickleIterator& iter, uint32_t ipcMessageType = 0) const;
 
   
   
