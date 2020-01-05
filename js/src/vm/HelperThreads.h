6900fc0ae9a22517c79086a7281e125e59b47c1a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 21:47:59 +0000

    Bug 1357012 - Use fallible append for compression tasks and use UniquePtrs. (r=jonco)
    UltraBlame original commit: bcf9341329ab5ade13acd349acc96cdafe5e883e

--- js/src/vm/HelperThreads.h
+++ js/src/vm/HelperThreads.h
@@ -72,7 +72,7 @@ class GlobalHelperThreadState
 
     typedef Vector<jit::IonBuilder*, 0, SystemAllocPolicy> IonBuilderVector;
     typedef Vector<ParseTask*, 0, SystemAllocPolicy> ParseTaskVector;
-    typedef Vector<SourceCompressionTask*, 0, SystemAllocPolicy> SourceCompressionTaskVector;
+    typedef Vector<UniquePtr<SourceCompressionTask>, 0, SystemAllocPolicy> SourceCompressionTaskVector;
     typedef Vector<GCHelperState*, 0, SystemAllocPolicy> GCHelperStateVector;
     typedef Vector<GCParallelTask*, 0, SystemAllocPolicy> GCParallelTaskVector;
     typedef Vector<PromiseTask*, 0, SystemAllocPolicy> PromiseTaskVector;
@@ -165,7 +165,10 @@ class GlobalHelperThreadState
     template <typename T>
     void remove(T& vector, size_t* index)
     {
-        vector[(*index)--] = vector.back();
+        
+        if (*index != vector.length() - 1)
+            vector[*index] = mozilla::Move(vector.back());
+        (*index)--;
         vector.popBack();
     }
 
@@ -550,7 +553,7 @@ struct AutoEnqueuePendingParseTasksAfterGC {
 
 
 bool
-EnqueueOffThreadCompression(JSContext* cx, SourceCompressionTask* task);
+EnqueueOffThreadCompression(JSContext* cx, UniquePtr<SourceCompressionTask> task);
 
 
 
@@ -622,10 +625,6 @@ struct ParseTask
 
     
     ScriptSourceObject* sourceObject;
-
-    
-    
-    SourceCompressionTask* sourceCompressionTask;
 
     
     
@@ -704,7 +703,6 @@ class SourceCompressionTask
     JSRuntime* runtime_;
 
     
-    static const uint64_t MajorGCNumberWaitingForFixup = UINT64_MAX;
     uint64_t majorGCNumber_;
 
     
@@ -718,30 +716,18 @@ class SourceCompressionTask
 
   public:
     
-    
-    
     SourceCompressionTask(JSRuntime* rt, ScriptSource* source)
       : runtime_(rt),
-        majorGCNumber_(CurrentThreadCanAccessRuntime(rt)
-                       ? rt->gc.majorGCCount()
-                       : MajorGCNumberWaitingForFixup),
+        majorGCNumber_(rt->gc.majorGCCount()),
         sourceHolder_(source)
     { }
 
     bool runtimeMatches(JSRuntime* runtime) const {
         return runtime == runtime_;
     }
-
-    void fixupMajorGCNumber(JSRuntime* runtime) {
-        MOZ_ASSERT(majorGCNumber_ == MajorGCNumberWaitingForFixup);
-        majorGCNumber_ = runtime->gc.majorGCCount();
-    }
-
     bool shouldStart() const {
         
         
-        if (majorGCNumber_ == MajorGCNumberWaitingForFixup)
-            return false;
         return runtime_->gc.majorGCCount() > majorGCNumber_ + 1;
     }
 
