a0929e1223aa17148bbfd3ad2012c7597d3a6532
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 19:47:39 +0000

    Merge inbound to m-c a=merge
    UltraBlame original commit: 272ce6c2572164f5f6a9fba2a980ba9ccf50770c

--- widget/windows/nsWindow.cpp
+++ widget/windows/nsWindow.cpp
@@ -3036,6 +3036,7 @@ nsWindow::SetCursor(imgIContainer* aCursor,
 
 
 
+
 #ifdef MOZ_XUL
 nsTransparencyMode nsWindow::GetTransparencyMode()
 {
@@ -3044,7 +3045,19 @@ nsTransparencyMode nsWindow::GetTransparencyMode()
 
 void nsWindow::SetTransparencyMode(nsTransparencyMode aMode)
 {
-  GetTopLevelWindow(true)->SetWindowTranslucencyInner(aMode);
+  nsWindow* window = GetTopLevelWindow(true);
+  MOZ_ASSERT(window);
+
+  if (!window || window->DestroyCalled()) {
+      return;
+  }
+
+  if (nsWindowType::eWindowType_toplevel == window->mWindowType) {
+      NS_WARNING("Cannot set transparency mode on top-level windows.");
+      return;
+  }
+
+  window->SetWindowTranslucencyInner(aMode);
 }
 
 void nsWindow::UpdateOpaqueRegion(const LayoutDeviceIntRegion& aOpaqueRegion)
