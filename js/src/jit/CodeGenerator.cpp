6dfb3ac549d099b5ce260d2a8610805ceacafeff
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 06:51:47 +0000

    Bug 1321521 - RegisterSets: Add a register type to getAny and add the equivalent hasAny function. r=lth
    UltraBlame original commit: 54d34c02426840cf68b051ccaea81facc8a6afc3

--- js/src/jit/CodeGenerator.cpp
+++ js/src/jit/CodeGenerator.cpp
@@ -4686,7 +4686,7 @@ CodeGenerator::generateArgumentsChecks(bool bailout)
     MResumePoint* rp = mir.entryResumePoint();
 
     
-    Register temp = GeneralRegisterSet(EntryTempMask).getAny();
+    Register temp = AllocatableGeneralRegisterSet(EntryTempMask).getAny();
 
     const CompileInfo& info = gen->info();
 
