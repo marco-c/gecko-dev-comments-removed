d257b6c80e24267450c972d5e70fd36f9dbb1fbb
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 10:54:05 +0000

    servo: Merge #9473 - Bump clipboard to remove libc 0.1.z dependency (from nox:adieu-libc-1); r=KiChjang
    Source-Repo: https://github.com/servo/servo
    Source-Revision: ec06c256d70937e91acb7309d7518b76962ea3a4
    
    UltraBlame original commit: b8b0a050d1a38fb06ff1781fb056d0f210ef990a

--- servo/python/tidy.py
+++ servo/python/tidy.py
@@ -168,7 +168,7 @@ def check_lock(file_name, contents):
         raise StopIteration
 
     
-    exceptions = ["libc", "cocoa"]
+    exceptions = ["cocoa"]
 
     import toml
     content = toml.loads(contents)
