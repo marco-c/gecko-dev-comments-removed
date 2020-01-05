85d484712d350af2fee0abbc2f63820ca0b37f3b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 07:04:09 +0000

    Bug 1331999: Fix race in setting initial popup background color. r=aswan
    MozReview-Commit-ID: KgRTxRIFbVd
    
    UltraBlame original commit: 3544f347daf2af7a48e9f8b4a8ff75181d948041

--- browser/components/extensions/ext-utils.js
+++ browser/components/extensions/ext-utils.js
@@ -343,6 +343,7 @@ class BasePopup {
 
     this.panel.style.setProperty("--arrowpanel-background", panelBackground);
     this.panel.style.setProperty("--panel-arrow-image-vertical", panelArrow);
+    this.background = background;
   }
 }
 
@@ -471,6 +472,8 @@ class ViewPopup extends BasePopup {
       
       
       let popupRect = this.panel.getBoundingClientRect();
+
+      this.setBackground(this.background);
 
       let win = this.window;
       let popupBottom = win.mozInnerScreenY + popupRect.bottom;
