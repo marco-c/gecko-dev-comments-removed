12ab2510d0d01704a0b32759945a6027051a71d3
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 19:50:31 +0000

    Bug 1351522 - Label runnables in dom/media/ogg/OggDemuxer.cpp; r=jwwang
    MozReview-Commit-ID: 8dIioCvfiJk
    
    UltraBlame original commit: d30e6dbd1748afe6e7355eed927f24899e4b9ce9

--- dom/media/ogg/OggDemuxer.cpp
+++ dom/media/ogg/OggDemuxer.cpp
@@ -137,8 +137,9 @@ OggDemuxer::~OggDemuxer()
                ptr, __func__, isChained));
       Telemetry::Accumulate(Telemetry::HistogramID::MEDIA_OGG_LOADED_IS_CHAINED, isChained);
     });
-    
-    AbstractThread::MainThread()->Dispatch(task.forget());
+    SystemGroup::Dispatch("~OggDemuxer::report_telemetry",
+                          TaskCategory::Other,
+                          task.forget());
   }
 }
 
