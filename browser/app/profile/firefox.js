ae993e421d26baf5c1eb275e4b76ea4f9e04f968
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 00:15:10 +0000

    Backed out changeset 1e8a7c6dcea1 (bug 1284588) for leaks in browser-chrome tests on OS X 10.10 debug. r=backout
    UltraBlame original commit: b1ed834646428b724a2ff0427f01769a26a93beb

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -980,15 +980,7 @@ pref("security.sandbox.windows.log.stackTraceDepth", 0);
 
 
 
-
-
-
-
-#if defined(NIGHTLY_BUILD)
-pref("security.sandbox.content.level", 2);
-#else
 pref("security.sandbox.content.level", 1);
-#endif
 #endif
 
 #if defined(XP_LINUX) && defined(MOZ_SANDBOX) && defined(MOZ_CONTENT_SANDBOX)
