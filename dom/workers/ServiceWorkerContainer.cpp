ea92b652bad5e19c206abcf13468886f158831c0
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 20:40:09 +0000

    Backed out changeset 321a37f440e8 (bug 1353638) for linting failure in testing/web-platform/meta/MANIFEST.json. r=backout
    UltraBlame original commit: 801bf613b8de5c1393543509364e8e5eb2376edf

--- dom/workers/ServiceWorkerContainer.cpp
+++ dom/workers/ServiceWorkerContainer.cpp
@@ -207,9 +207,9 @@ ServiceWorkerContainer::Register(const nsAString& aScriptURL,
     }
   }
 
-  
-  
-  nsLoadFlags loadFlags = nsIRequest::LOAD_NORMAL;
+  bool useCache = aOptions.mUseCache.WasPassed() && aOptions.mUseCache.Value();
+  nsLoadFlags loadFlags = useCache ? nsIRequest::LOAD_NORMAL
+                                   : nsIRequest::VALIDATE_ALWAYS;
 
   
   
