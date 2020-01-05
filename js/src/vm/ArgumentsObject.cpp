2941e08921c24bfcc32687cea7731e18991e0383
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 16:36:37 +0000

    Bug 1335862 - Implement mayResolve for arguments objects. r=jandem
    UltraBlame original commit: b9ee8724079936ea2adab7d017ff7a3aab61c151

--- js/src/vm/ArgumentsObject.cpp
+++ js/src/vm/ArgumentsObject.cpp
@@ -430,6 +430,21 @@ ArgumentsObject::obj_delProperty(JSContext* cx, HandleObject obj, HandleId id,
     return result.succeed();
 }
 
+ bool
+ArgumentsObject::obj_mayResolve(const JSAtomState& names, jsid id, JSObject*)
+{
+    
+    if (!JSID_IS_ATOM(id))
+        return true;
+
+    JSAtom* atom = JSID_TO_ATOM(id);
+    uint32_t index;
+    if (atom->isIndex(&index))
+        return true;
+
+    return atom == names.length || atom == names.callee;
+}
+
 static bool
 MappedArgGetter(JSContext* cx, HandleObject obj, HandleId id, MutableHandleValue vp)
 {
@@ -854,7 +869,7 @@ const ClassOps MappedArgumentsObject::classOps_ = {
     nullptr,                 
     MappedArgumentsObject::obj_enumerate,
     MappedArgumentsObject::obj_resolve,
-    nullptr,                 
+    ArgumentsObject::obj_mayResolve,
     ArgumentsObject::finalize,
     nullptr,                 
     nullptr,                 
@@ -891,7 +906,7 @@ const ClassOps UnmappedArgumentsObject::classOps_ = {
     nullptr,                 
     UnmappedArgumentsObject::obj_enumerate,
     UnmappedArgumentsObject::obj_resolve,
-    nullptr,                 
+    ArgumentsObject::obj_mayResolve,
     ArgumentsObject::finalize,
     nullptr,                 
     nullptr,                 
