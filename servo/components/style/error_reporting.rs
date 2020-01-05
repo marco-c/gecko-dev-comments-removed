415f7106604084edd2ddd61b32c4b4c7f353789d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 10:51:12 +0000

    servo: Merge #9261 - Remove the msg dependency from style (from bholley:geckolib_nomsg); r=Manishearth
    This is part of slimming down the dependencies of geckolib.
    
    Source-Repo: https://github.com/servo/servo
    Source-Revision: 3680e82cab766a412a969a21c0cdff0c7f97d7bc
    
    UltraBlame original commit: c1e5af72c75bae3242a69806f4deb266f2bc9b8a

---
+++ servo/components/style/error_reporting.rs
@@ -0,0 +1,25 @@
+
+
+
+
+use cssparser::{Parser, SourcePosition};
+use log;
+
+pub trait ParseErrorReporter {
+    fn report_error(&self, input: &mut Parser, position: SourcePosition, message: &str);
+    fn clone(&self) -> Box<ParseErrorReporter + Send + Sync>;
+}
+
+pub struct StdoutErrorReporter;
+impl ParseErrorReporter for StdoutErrorReporter {
+    fn report_error(&self, input: &mut Parser, position: SourcePosition, message: &str) {
+         if log_enabled!(log::LogLevel::Info) {
+             let location = input.source_location(position);
+             info!("{}:{} {}", location.line, location.column, message)
+         }
+    }
+
+    fn clone(&self) -> Box<ParseErrorReporter + Send + Sync> {
+        box StdoutErrorReporter
+    }
+}
