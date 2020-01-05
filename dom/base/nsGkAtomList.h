825e61d9d70b913b5bba08a5415c2c9610bff567
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 22:19:06 +0000

    Bug 1358061 - remove moz-audiochannel codes for media element and web audio. r=baku
    MozReview-Commit-ID: KPuhxCVezOZ
    
    UltraBlame original commit: b19e0113456d4dd3c204cb7585ae179b0c6c9605

--- dom/base/nsGkAtomList.h
+++ dom/base/nsGkAtomList.h
@@ -627,7 +627,6 @@ GK_ATOM(mouseout, "mouseout")
 GK_ATOM(mouseover, "mouseover")
 GK_ATOM(mousethrough, "mousethrough")
 GK_ATOM(mouseup, "mouseup")
-GK_ATOM(mozaudiochannel, "mozaudiochannel")
 GK_ATOM(mozfullscreenchange, "mozfullscreenchange")
 GK_ATOM(mozfullscreenerror, "mozfullscreenerror")
 GK_ATOM(mozpointerlockchange, "mozpointerlockchange")
@@ -1958,10 +1957,6 @@ GK_ATOM(onuserproximity, "onuserproximity")
 
 
 GK_ATOM(ondevicelight, "ondevicelight")
-
-
-GK_ATOM(onmozinterruptbegin, "onmozinterruptbegin")
-GK_ATOM(onmozinterruptend, "onmozinterruptend")
 
 
 GK_ATOM(ondevicechange, "ondevicechange")
