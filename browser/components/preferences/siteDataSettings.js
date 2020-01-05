0de2d15d0128d65453b63c278d34089acdeda959
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 23:51:27 +0000

    Part 1: Bug 1348733 - List sites using quota storage or appcache in Settings of Site Data, r=Gijs
    This patch does:
    - remove codes about counting and removing http cache per site
    - group and list sites based on host across scheme, port and origin attributes
    - list sites using quota usage or appcache
    - sort sites descending by usage by default
    
    MozReview-Commit-ID: 29zZTzOsC7c
    
    UltraBlame original commit: c50e480ea03538a65a292b34ec89e63317689faa

--- browser/components/preferences/siteDataSettings.js
+++ browser/components/preferences/siteDataSettings.js
@@ -39,7 +39,7 @@ let gSiteDataSettings = {
     this._prefStrBundle = document.getElementById("bundlePreferences");
     SiteDataManager.getSites().then(sites => {
       this._sites = sites;
-      let sortCol = document.getElementById("hostCol");
+      let sortCol = document.querySelector("treecol[data-isCurrentSortCol=true]");
       this._sortSites(this._sites, sortCol);
       this._buildSitesList(this._sites);
       Services.obs.notifyObservers(null, "sitedata-settings-init");
@@ -92,14 +92,17 @@ let gSiteDataSettings = {
     switch (col.id) {
       case "hostCol":
         sortFunc = (a, b) => {
-          let aHost = a.uri.host.toLowerCase();
-          let bHost = b.uri.host.toLowerCase();
+          let aHost = a.host.toLowerCase();
+          let bHost = b.host.toLowerCase();
           return aHost.localeCompare(bHost);
         }
         break;
 
       case "statusCol":
-        sortFunc = (a, b) => a.status - b.status;
+        sortFunc = (a, b) => {
+          return a.persisted && !b.persisted ? 1 :
+                 !a.persisted && b.persisted ? -1 : 0;
+        };
         break;
 
       case "usageCol":
@@ -133,22 +136,21 @@ let gSiteDataSettings = {
     }
 
     let keyword = this._searchBox.value.toLowerCase().trim();
-    for (let data of sites) {
-      let host = data.uri.host;
+    for (let site of sites) {
+      let host = site.host;
       if (keyword && !host.includes(keyword)) {
         continue;
       }
 
-      if (data.userAction === "remove") {
+      if (site.userAction === "remove") {
         continue;
       }
 
-      let size = DownloadUtils.convertByteUnits(data.usage);
+      let size = DownloadUtils.convertByteUnits(site.usage);
       let item = document.createElement("richlistitem");
-      item.setAttribute("data-origin", data.uri.spec);
       item.setAttribute("host", host);
       item.setAttribute("usage", this._prefStrBundle.getFormattedString("siteUsage", size));
-      if (data.status === Ci.nsIPermissionManager.ALLOW_ACTION ) {
+      if (site.persisted) {
         item.setAttribute("status", this._prefStrBundle.getString("persistent"));
       }
       this._list.appendChild(item);
@@ -159,12 +161,10 @@ let gSiteDataSettings = {
   _removeSiteItems(items) {
     for (let i = items.length - 1; i >= 0; --i) {
       let item = items[i];
-      let origin = item.getAttribute("data-origin");
-      for (let site of this._sites) {
-        if (site.uri.spec === origin) {
-          site.userAction = "remove";
-          break;
-        }
+      let host = item.getAttribute("host");
+      let siteForHost = this._sites.find(site => site.host == host);
+      if (siteForHost) {
+        siteForHost.userAction = "remove";
       }
       item.remove();
     }
@@ -178,7 +178,7 @@ let gSiteDataSettings = {
     let removals = [];
     this._sites = this._sites.filter(site => {
       if (site.userAction === "remove") {
-        removals.push(site.uri);
+        removals.push(site.host);
         return false;
       }
       return true;
@@ -207,21 +207,21 @@ let gSiteDataSettings = {
         
         let hostsTable = new Map();
         
-        for (let uri of removals) {
-          let baseDomain = Services.eTLD.getBaseDomain(uri);
+        for (let host of removals) {
+          let baseDomain = Services.eTLD.getBaseDomainFromHost(host);
           let hosts = hostsTable.get(baseDomain);
           if (!hosts) {
             hosts = [];
             hostsTable.set(baseDomain, hosts);
           }
-          hosts.push(uri.host);
+          hosts.push(host);
         }
         
         for (let site of this._sites) {
-          let baseDomain = Services.eTLD.getBaseDomain(site.uri);
+          let baseDomain = Services.eTLD.getBaseDomainFromHost(site.host);
           let hosts = hostsTable.get(baseDomain);
           if (hosts) {
-            hosts.push(site.uri.host);
+            hosts.push(site.host);
           }
         }
 
@@ -233,7 +233,14 @@ let gSiteDataSettings = {
         window.openDialog("chrome://browser/content/preferences/siteDataRemoveSelected.xul", "", features, args);
         allowed = args.allowed;
         if (allowed) {
-          SiteDataManager.remove(removals);
+          try {
+            SiteDataManager.remove(removals);
+          } catch (e) {
+            
+            
+            
+            Cu.reportError(e);
+          }
         }
       }
     }
