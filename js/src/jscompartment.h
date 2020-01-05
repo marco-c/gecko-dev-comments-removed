a0fcdb275037ec72d74febb426755366a446d313
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 06:18:53 +0000

    Backed out changesets 29bc945061a0 and 1c11ff13ba6c (bug 1324773) for causing frequent GC crashes in CI.
    UltraBlame original commit: 55441ae91e84fdf2d511a2847d9ef95d04ad7f48

--- js/src/jscompartment.h
+++ js/src/jscompartment.h
@@ -447,10 +447,9 @@ struct JSCompartment
     
     
     
-    using VarNameSet = JS::GCHashSet<JSAtom*,
-                                     js::DefaultHasher<JSAtom*>,
-                                     js::SystemAllocPolicy>;
-    JS::WeakCache<VarNameSet> varNames_;
+    JS::GCHashSet<JSAtom*,
+                  js::DefaultHasher<JSAtom*>,
+                  js::SystemAllocPolicy> varNames_;
 
   public:
     
@@ -684,7 +683,7 @@ struct JSCompartment
     MOZ_MUST_USE bool addToVarNames(JSContext* cx, JS::Handle<JSAtom*> name);
 
     void removeFromVarNames(JS::Handle<JSAtom*> name) {
-        varNames_.get().remove(name);
+        varNames_.remove(name);
     }
 
     
