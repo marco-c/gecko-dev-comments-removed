979156929725bcbd5bb6b8a9066fe10fbcfc7fd1
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 22:41:15 +0000

    Bug 1355559: Acquire the stack walk workaround lock in LdrResolveDelayLoadedAPI. r=mstange,aklotz
    UltraBlame original commit: 9f64942d7eb133c63234c4976f51295a667a3cc9

--- toolkit/xre/test/win/TestDllInterceptor.cpp
+++ toolkit/xre/test/win/TestDllInterceptor.cpp
@@ -243,6 +243,18 @@ bool TestLdrUnloadDll(void* aFunc)
   return patchedLdrUnloadDll(0) != 0;
 }
 
+bool TestLdrResolveDelayLoadedAPI(void* aFunc)
+{
+  
+  typedef PVOID (WINAPI *LdrResolveDelayLoadedAPIType)(PVOID, PVOID, PVOID,
+                                                       PVOID, PVOID, ULONG);
+  auto patchedLdrResolveDelayLoadedAPI =
+    reinterpret_cast<LdrResolveDelayLoadedAPIType>(aFunc);
+  
+  
+  return patchedLdrResolveDelayLoadedAPI(0, 0, 0, 0, 0, 99) == 0;
+}
+
 bool TestSetUnhandledExceptionFilter(void* aFunc)
 {
   auto patchedSetUnhandledExceptionFilter =
@@ -471,6 +483,7 @@ int main()
 #ifdef _M_X64
       TestHook(TestGetKeyState, "user32.dll", "GetKeyState") &&    
       TestHook(TestLdrUnloadDll, "ntdll.dll", "LdrUnloadDll") &&
+      TestHook(TestLdrResolveDelayLoadedAPI, "ntdll.dll", "LdrResolveDelayLoadedAPI") &&
 #endif
       MaybeTestHook(ShouldTestTipTsf(), TestProcessCaretEvents, "tiptsf.dll", "ProcessCaretEvents") &&
 #ifdef _M_IX86
