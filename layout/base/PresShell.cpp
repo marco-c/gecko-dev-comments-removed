d551548a959e27c2ed404c24ed6704ad2ac6b2ad
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 19:48:46 +0000

    Bug 1351518 - Stop bailing out before NS_DOCUMENT_STATE_WINDOW_INACTIVE check for stylo. r=heycam
    MozReview-Commit-ID: 2XQS3XJcvj4
    
    UltraBlame original commit: 9db9f41e4b94d359d661fa763a529bf3b16017fc

--- layout/base/PresShell.cpp
+++ layout/base/PresShell.cpp
@@ -4302,19 +4302,17 @@ PresShell::DocumentStatesChanged(nsIDocument* aDocument,
   NS_PRECONDITION(!mIsDocumentGone, "Unexpected DocumentStatesChanged");
   NS_PRECONDITION(aDocument == mDocument, "Unexpected aDocument");
 
-  nsStyleSet* styleSet = mStyleSet->GetAsGecko();
-  if (!styleSet) {
+  nsStyleSet* geckoSet = mStyleSet->GetAsGecko();
+  if (!geckoSet) {
     
     
     
     NS_WARNING("stylo: ServoStyleSets cannot respond to document state "
                "changes yet (only matters for chrome documents). See bug 1290285.");
-    return;
-  }
 
-  if (mDidInitialize &&
-      styleSet->HasDocumentStateDependentStyle(mDocument->GetRootElement(),
-                                               aStateMask)) {
+  } else if (mDidInitialize &&
+             geckoSet->HasDocumentStateDependentStyle(mDocument->GetRootElement(),
+                                                      aStateMask)) {
     mPresContext->RestyleManager()->PostRestyleEvent(mDocument->GetRootElement(),
                                                      eRestyle_Subtree,
                                                      nsChangeHint(0));
