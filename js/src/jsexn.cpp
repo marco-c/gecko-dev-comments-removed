8fb021b6f5f9f3c0e858faca1ae2a7d7bdda338d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 21:00:07 +0000

    Backed out changeset c30eae661333 (bug 1352449) for failing non-unified spidermonkey build. r=backout
    UltraBlame original commit: 8efcd39c326ad1137c4e39303fa115d26bd62e4b

--- js/src/jsexn.cpp
+++ js/src/jsexn.cpp
@@ -209,13 +209,8 @@ ErrorObject::classes[JSEXN_ERROR_LIMIT] = {
 size_t
 ExtraMallocSize(JSErrorReport* report)
 {
-    if (report->linebuf()) {
-        
-
-
-
-        return (report->linebufLength() + 1) * sizeof(char16_t) + 1;
-    }
+    if (report->linebuf())
+        return (report->linebufLength() + 1) * sizeof(char16_t);
 
     return 0;
 }
@@ -230,13 +225,6 @@ bool
 CopyExtraData(JSContext* cx, uint8_t** cursor, JSErrorReport* copy, JSErrorReport* report)
 {
     if (report->linebuf()) {
-        
-
-
-
-        if (size_t(*cursor) % 2)
-            (*cursor)++;
-
         size_t linebufSize = (report->linebufLength() + 1) * sizeof(char16_t);
         const char16_t* linebufCopy = (const char16_t*)(*cursor);
         js_memcpy(*cursor, report->linebuf(), linebufSize);
