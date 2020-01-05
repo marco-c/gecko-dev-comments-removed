598586afa8648ecc77964386232e921a65b8b587
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 02:43:43 +0000

    Bug 1313943 - Legacy event initializers should all do nothing during dispatch; r=bkelly
    DOM's initEvent says that it must do nothing during dispatch.  UI Events
    says they behave the same as initEvent, so the legacy initializers
    defined there also shouldn't do anything during dispatch.  Previously we
    only did this for initEvent itself, but other legacy initializers would
    function normally during dispatch.
    
    Edge passes the wpt test that's updated in this commit.  Chrome passes
    all but initCustomEvent.  It would be nice to expand the test to have
    coverage for all our legacy initializers, but the likelihood of
    real-world compat issues from this behavior is slim enough that I didn't
    think it was worth the effort.
    
    MozReview-Commit-ID: IYLOuwlPGSj
    
    UltraBlame original commit: 90bbef07e8be0a604b5a4d06e384e7942ee25823

--- dom/smil/TimeEvent.cpp
+++ dom/smil/TimeEvent.cpp
@@ -56,6 +56,8 @@ void
 TimeEvent::InitTimeEvent(const nsAString& aType, nsGlobalWindow* aView,
                          int32_t aDetail)
 {
+  NS_ENSURE_TRUE_VOID(!mEvent->mFlags.mIsBeingDispatched);
+
   Event::InitEvent(aType, false , false );
   mDetail = aDetail;
   mView = aView ? aView->GetOuterWindow() : nullptr;
