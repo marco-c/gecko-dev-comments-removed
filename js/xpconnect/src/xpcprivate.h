f18e6984bb26f95ebe76ee4220de14d24264e282
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 22:20:57 +0000

    Bug 1334278 - have FormatStackDump return UniqueChars; r=froydnj
    Change FormatStackDump to return UniqueChars and fix up the users.  This
    removes a bit more manual memory management.
    
    MozReview-Commit-ID: 60GBgeS4rzg
    
    UltraBlame original commit: ae4e80fe5f3484941b5c54b9aeddff60ba64d02d

--- js/xpconnect/src/xpcprivate.h
+++ js/xpconnect/src/xpcprivate.h
@@ -2443,8 +2443,7 @@ xpc_DumpJSStack(bool showArgs, bool showLocals, bool showThisProps);
 
 
 
-
-extern char*
+extern JS::UniqueChars
 xpc_PrintJSStack(JSContext* cx, bool showArgs, bool showLocals,
                  bool showThisProps);
 
