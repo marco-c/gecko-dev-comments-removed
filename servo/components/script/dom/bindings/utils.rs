331b06c197412c7b9066d1722e14982e52088f48
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 11:20:40 +0000

    servo: Merge #11308 - Implement [Func] (from nox:guarded); r=jdm
    First part of #11292, this just includes support of `[Func]`.
    
    Source-Repo: https://github.com/servo/servo
    Source-Revision: 073c5e3b6b8950c01c9e5691925d24787fd06d53
    
    UltraBlame original commit: d617487b1df5e64426932a60052241015f05126d

--- servo/components/script/dom/bindings/utils.rs
+++ servo/components/script/dom/bindings/utils.rs
@@ -39,7 +39,6 @@ use std::ffi::CString;
 use std::os::raw::c_void;
 use std::ptr;
 use std::slice;
-use util::prefs;
 
 
 pub struct WindowProxyHandler(pub *const libc::c_void);
@@ -550,31 +549,3 @@ unsafe extern "C" fn instance_class_has_proto_at_depth(clasp: *const js::jsapi::
 pub const DOM_CALLBACKS: DOMCallbacks = DOMCallbacks {
     instanceClassMatchesProto: Some(instance_class_has_proto_at_depth),
 };
-
-
-pub struct Prefable<T: 'static> {
-    
-    pub pref: Option<&'static str>,
-    
-    pub specs: &'static [T],
-    
-    
-    pub terminator: bool,
-}
-
-impl<T> Prefable<T> {
-    
-    
-    pub fn specs(&self) -> &'static [T] {
-        if let Some(pref) = self.pref {
-            if !prefs::get_pref(pref).as_boolean().unwrap_or(false) {
-                return if self.terminator {
-                    &self.specs[self.specs.len() - 1..]
-                } else {
-                    &[]
-                };
-            }
-        }
-        self.specs
-    }
-}
