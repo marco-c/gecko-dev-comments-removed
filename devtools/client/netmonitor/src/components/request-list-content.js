5c1856859a9f9fc47a6344d1f296271600b10f72
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 20:36:22 +0000

    Bug 1352699 - Fix gStore instance issue in NetMonitorController r=Honza
    MozReview-Commit-ID: CWAnmdEtJwc
    
    UltraBlame original commit: a822f74d410e7253929f8761a221f2b7687f29ff

--- devtools/client/netmonitor/src/components/request-list-content.js
+++ devtools/client/netmonitor/src/components/request-list-content.js
@@ -39,7 +39,7 @@ const RequestListContent = createClass({
     dispatch: PropTypes.func.isRequired,
     displayedRequests: PropTypes.object.isRequired,
     firstRequestStartedMillis: PropTypes.number.isRequired,
-    fromCache: PropTypes.bool.isRequired,
+    fromCache: PropTypes.bool,
     onCauseBadgeClick: PropTypes.func.isRequired,
     onItemMouseDown: PropTypes.func.isRequired,
     onSecurityIconClick: PropTypes.func.isRequired,
@@ -266,6 +266,14 @@ module.exports = connect(
   }),
   (dispatch) => ({
     dispatch,
+    
+
+
+    onCauseBadgeClick: (cause) => {
+      if (cause.stacktrace && cause.stacktrace.length > 0) {
+        dispatch(Actions.selectDetailsPanelTab("stack-trace"));
+      }
+    },
     onItemMouseDown: (id) => dispatch(Actions.selectRequest(id)),
     
 
@@ -274,14 +282,6 @@ module.exports = connect(
     onSecurityIconClick: (securityState) => {
       if (securityState && securityState !== "insecure") {
         dispatch(Actions.selectDetailsPanelTab("security"));
-      }
-    },
-    
-
-
-    onCauseBadgeClick: (cause) => {
-      if (cause.stacktrace && cause.stacktrace.length > 0) {
-        dispatch(Actions.selectDetailsPanelTab("stack-trace"));
       }
     },
     onSelectDelta: (delta) => dispatch(Actions.selectDelta(delta)),
