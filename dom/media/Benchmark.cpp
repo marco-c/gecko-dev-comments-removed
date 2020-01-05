2652c673e258341d8c5b4453ddeaa902410376ce
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 07:35:51 +0000

    Backed out changeset c429f0a0040e (bug 1316282) for timing out Windows PGO builds a=backout CLOSED TREE
    MozReview-Commit-ID: K88JI9UiHqU
    
    UltraBlame original commit: 5c5b0537712323cf1d9610a6fe1b127c65902182

--- dom/media/Benchmark.cpp
+++ dom/media/Benchmark.cpp
@@ -15,6 +15,10 @@
 #include "mozilla/Telemetry.h"
 #include "mozilla/dom/ContentChild.h"
 
+#ifndef MOZ_WIDGET_ANDROID
+#include "WebMSample.h"
+#endif
+
 namespace mozilla {
 
 
@@ -40,11 +44,9 @@ VP9Benchmark::IsVP9DecodeFast()
   if (!sHasRunTest && (!hasPref || hadRecentUpdate != sBenchmarkVersionID)) {
     sHasRunTest = true;
 
-#include "WebMSample.h"
-
     RefPtr<WebMDemuxer> demuxer =
       new WebMDemuxer(
-        new BufferMediaResource(webMSample, sizeof(webMSample), nullptr,
+        new BufferMediaResource(sWebMSample, sizeof(sWebMSample), nullptr,
                                 MediaContainerType(MEDIAMIMETYPE("video/webm"))));
     RefPtr<Benchmark> estimiser =
       new Benchmark(demuxer,
