607d520865087ffed2bae2d02668ae5d31a3b44a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 07:20:48 +0000

    Bug 1317632 - If the bookmark to add is a reader mode page, defer db action till it cache completely and save the same url in bookmarks and uriannotations table to avoid URL chagne between two http requests. r=sebastian
    MozReview-Commit-ID: 8ZwteTOKe1G
    
    UltraBlame original commit: e7c232eb0efa4076d3402a919671edeff7e9de28

--- mobile/android/base/java/org/mozilla/gecko/Tab.java
+++ mobile/android/base/java/org/mozilla/gecko/Tab.java
@@ -531,16 +531,18 @@ public class Tab {
 
         final String pageUrl = ReaderModeUtils.stripAboutReaderUrl(getURL());
 
-        ThreadUtils.postToBackgroundThread(new Runnable() {
-            @Override
-            public void run() {
-                mDB.addBookmark(getContentResolver(), mTitle, pageUrl);
-                Tabs.getInstance().notifyListeners(Tab.this, Tabs.TabEvents.BOOKMARK_ADDED);
-            }
-        });
-
         if (AboutPages.isAboutReader(url)) {
             ReadingListHelper.cacheReaderItem(pageUrl, mId, mAppContext);
+            
+        } else {
+            ThreadUtils.postToBackgroundThread(new Runnable() {
+                @Override
+                public void run() {
+                    mDB.addBookmark(getContentResolver(), mTitle, pageUrl);
+                    Tabs.getInstance().notifyListeners(Tab.this, Tabs.TabEvents.BOOKMARK_ADDED);
+                }
+            });
+
         }
     }
 
