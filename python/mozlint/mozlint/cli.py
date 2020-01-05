ddd66488de639f1f7eaeeffa4b911fa8ae17e07b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 01:39:53 +0000

    Bug 1309963 - Make sure return codes are passed from .lint files up to the cli, r=jgraham
    If a linter returns a status code instead of a list of results, mozlint will throw away
    that status code and move on to the next linter. We should make sure that status code
    bubbles all the way up to the cli.
    
    MozReview-Commit-ID: 9fXpmtlUUT1
    
    UltraBlame original commit: 01cef2481413ced6cbe2ae54f5a48400dbd4920c

--- python/mozlint/mozlint/cli.py
+++ python/mozlint/mozlint/cli.py
@@ -106,7 +106,7 @@ def run(paths, linters, fmt, rev, workdir, **lintargs):
     
     
     print(formatter(results).encode('utf-8', 'replace'))
-    return 1 if results else 0
+    return lint.return_code
 
 
 if __name__ == '__main__':
