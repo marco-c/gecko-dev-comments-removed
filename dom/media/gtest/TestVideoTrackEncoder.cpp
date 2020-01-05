1045abbe20674dd14b1eb6afa41d85c3dcecc667
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 07:09:10 +0000

    Bug 1330918 - Set timestamp in VP8TrackEncoder GTest. r=jesup
    MozReview-Commit-ID: 4LeR1iUZBgs
    
    UltraBlame original commit: a3538d0e7314f9dae25af9353b88dc06aa110fe6

--- dom/media/gtest/TestVideoTrackEncoder.cpp
+++ dom/media/gtest/TestVideoTrackEncoder.cpp
@@ -265,13 +265,16 @@ TEST(VP8VideoTrackEncoder, FrameEncode)
   
   
   VideoSegment segment;
+  TimeStamp now = TimeStamp::Now();
   for (nsTArray<RefPtr<Image>>::size_type i = 0; i < images.Length(); i++)
   {
     RefPtr<Image> image = images[i];
     segment.AppendFrame(image.forget(),
                         mozilla::StreamTime(90000),
                         generator.GetSize(),
-                        PRINCIPAL_HANDLE_NONE);
+                        PRINCIPAL_HANDLE_NONE,
+                        false,
+                        now + TimeDuration::FromSeconds(i));
   }
 
   
