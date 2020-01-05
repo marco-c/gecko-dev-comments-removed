b0537c153a18eefcedb38fe8f40a0bc85e2d141c
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 02:02:12 +0000

    Bug 1292273 - Skip CompositorD3D11::EndFrame when device-removed happened and add some logs for tracking the behavior. r=dvander
    UltraBlame original commit: 34fd4c7639ec76c97674be80df48fd75c0a2c8ca

--- gfx/layers/d3d11/CompositorD3D11.cpp
+++ gfx/layers/d3d11/CompositorD3D11.cpp
@@ -979,9 +979,16 @@ CompositorD3D11::BeginFrame(const nsIntRegion& aInvalidRegion,
   
   
   NS_ASSERTION(mHwnd, "Couldn't find an HWND when initialising?");
-  if (::IsIconic(mHwnd) || mDevice->GetDeviceRemovedReason() != S_OK) {
+  if (::IsIconic(mHwnd)) {
     
     
+    ReadUnlockTextures();
+    *aRenderBoundsOut = IntRect();
+    return;
+  }
+
+  if (mDevice->GetDeviceRemovedReason() != S_OK) {
+    gfxCriticalNote << "GFX: D3D11 skip BeginFrame with device-removed.";
     ReadUnlockTextures();
     *aRenderBoundsOut = IntRect();
     return;
@@ -1076,6 +1083,13 @@ CompositorD3D11::EndFrame()
 {
   if (!mDefaultRT) {
     Compositor::EndFrame();
+    return;
+  }
+
+  if (mDevice->GetDeviceRemovedReason() != S_OK) {
+    gfxCriticalNote << "GFX: D3D11 skip EndFrame with device-removed.";
+    Compositor::EndFrame();
+    mCurrentRT = nullptr;
     return;
   }
 
