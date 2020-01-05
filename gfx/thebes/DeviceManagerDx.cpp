4a846d4c0de1b3703e5a13f8c10da96199954177
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 00:37:44 +0000

    Backed out changeset e1300d4c0a52 (bug 1292923) for windows reftest failures a=backout
    UltraBlame original commit: 36dbc4e296bf3adaaefe0807b2161673463ac676

--- gfx/thebes/DeviceManagerDx.cpp
+++ gfx/thebes/DeviceManagerDx.cpp
@@ -671,18 +671,6 @@ DeviceManagerDx::TextureSharingWorks()
 }
 
 bool
-DeviceManagerDx::CanInitializeKeyedMutexTextures()
-{
-  MutexAutoLock lock(mDeviceLock);
-  if (!mDeviceStatus) {
-    return false;
-  }
-  
-  
-  return mDeviceStatus->adapter().VendorId != 0x8086;
-}
-
-bool
 DeviceManagerDx::IsWARP()
 {
   MutexAutoLock lock(mDeviceLock);
