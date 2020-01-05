850b105963676dfdea21728bffe20cbaae796052
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 16:50:22 +0000

    Bug 1284897 - Hook GetSaveFileNameW/GetOpenFileNameW to record and grant a sandboxed process permission to access user-chosen files. r=jimm
    UltraBlame original commit: 8804a3f8fb1589b244b14528cca8d3ec5a331c68

--- dom/plugins/ipc/PluginProcessParent.cpp
+++ dom/plugins/ipc/PluginProcessParent.cpp
@@ -98,13 +98,6 @@ AddSandboxAllowedFiles(int32_t aSandboxLevel,
     }
 
     
-    if (aSandboxLevel > 2) {
-        AddSandboxAllowedFile(aAllowedFilesRead, dirSvc, NS_WIN_HOME_DIR);
-        AddSandboxAllowedFile(aAllowedFilesRead, dirSvc, NS_WIN_HOME_DIR,
-                              NS_LITERAL_STRING("\\*"));
-    }
-
-    
     
     
     AddSandboxAllowedFile(aAllowedFilesReadWrite, dirSvc, NS_WIN_APPDATA_DIR,
