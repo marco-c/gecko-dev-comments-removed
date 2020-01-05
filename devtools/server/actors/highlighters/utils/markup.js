b318a1205ec20c52f8560f2c8d17e886a7af61cf
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 04:39:16 +0000

    Bug 1306937 - use XHTML NS by default when creating highlighter nodes;r=pbro
    MozReview-Commit-ID: 6OI5AzfWlD6
    
    UltraBlame original commit: db231dc73bf45a837ea21f04a8906646cfac5c17

--- devtools/server/actors/highlighters/utils/markup.js
+++ devtools/server/actors/highlighters/utils/markup.js
@@ -34,6 +34,7 @@ exports.getCSSStyleRules = (...args) =>
   lazyContainer.DOMUtils.getCSSStyleRules(...args);
 
 const SVG_NS = "http://www.w3.org/2000/svg";
+const XHTML_NS = "http://www.w3.org/1999/xhtml";
 const XUL_NS = "http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul";
 const STYLESHEET_URI = "resource://devtools/server/actors/" +
                        "highlighters.css";
@@ -188,16 +189,11 @@ exports.createSVGNode = createSVGNode;
 
 
 
-
 function createNode(win, options) {
   let type = options.nodeType || "div";
+  let namespace = options.namespace || XHTML_NS;
 
-  let node;
-  if (options.namespace) {
-    node = win.document.createElementNS(options.namespace, type);
-  } else {
-    node = win.document.createElement(type);
-  }
+  let node = win.document.createElementNS(namespace, type);
 
   for (let name in options.attributes || {}) {
     let value = options.attributes[name];
