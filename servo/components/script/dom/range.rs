bbd0de508cb3588d41613a09b7c9c573128453f7
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 10:32:34 +0000

    servo: Merge #7882 - Refactor Error enum usage to consistently be qualified (from anthgur:consistent-enum-use); r=Ms2ger
    Closes #7869
    
    Source-Repo: https://github.com/servo/servo
    Source-Revision: f5cd90805ec67fb52f73ec4cef9cef881a67522d
    
    UltraBlame original commit: 3e2a2646d46ef1ead10cee8396d290289cebb42f

--- servo/components/script/dom/range.rs
+++ servo/components/script/dom/range.rs
@@ -11,7 +11,6 @@ use dom::bindings::codegen::Bindings::RangeBinding::{self, RangeConstants};
 use dom::bindings::codegen::Bindings::TextBinding::TextMethods;
 use dom::bindings::codegen::Bindings::WindowBinding::WindowMethods;
 use dom::bindings::codegen::InheritTypes::{CharacterDataCast, NodeCast, TextCast, TextDerived};
-use dom::bindings::error::Error::HierarchyRequest;
 use dom::bindings::error::{Error, ErrorResult, Fallible};
 use dom::bindings::global::GlobalRef;
 use dom::bindings::js::{JS, Root, RootedReference};
@@ -117,7 +116,7 @@ impl Range {
 
         // Step 12.
         if contained_children.iter().any(|n| n.is_doctype()) {
-            return Err(HierarchyRequest);
+            return Err(Error::HierarchyRequest);
         }
 
         Ok((first_contained_child, last_contained_child, contained_children))
@@ -597,7 +596,7 @@ impl RangeMethods for Range {
         match start_node.type_id() {
             
             NodeTypeId::CharacterData(CharacterDataTypeId::Text) => (),
-            NodeTypeId::CharacterData(_) => return Err(HierarchyRequest),
+            NodeTypeId::CharacterData(_) => return Err(Error::HierarchyRequest),
             _ => ()
         }
 
@@ -608,7 +607,7 @@ impl RangeMethods for Range {
                 let parent = match start_node.GetParentNode() {
                     Some(parent) => parent,
                     
-                    None => return Err(HierarchyRequest)
+                    None => return Err(Error::HierarchyRequest)
                 };
                 
                 (Some(Root::from_ref(start_node.r())), parent)
