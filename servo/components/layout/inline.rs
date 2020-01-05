34f4d716e537b59712ec374a1b8e62c4e1767dca
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 10:19:21 +0000

    servo: Merge #6903 - Fix percentage height calculation, absolute containing block height calculations (from glennw:fix-height-again); r=pcwalton
    It's not possible to correctly determine during the css cascade whether the container height
    is explicitly specified. Additionally, the spec https://drafts.csswg.org/css2/visudet.html#the-height-property says this should affect the *used* height, rather than the computed height.
    
    This significantly improves the layout in #6643.
    
    Source-Repo: https://github.com/servo/servo
    Source-Revision: 028707f5cd3263fd1476669207f67d5b9d5d4806
    
    UltraBlame original commit: 3ab736713d34e5b5cceb37a32a45db6fce625069

--- servo/components/layout/inline.rs
+++ servo/components/layout/inline.rs
@@ -1346,7 +1346,7 @@ impl Flow for InlineFlow {
 
         
         let containing_block_block_size =
-            self.base.block_container_explicit_block_size.unwrap_or(Au(0));
+            self.base.block_container_explicit_block_size;
         for fragment in self.fragments.fragments.iter_mut() {
             fragment.update_late_computed_replaced_inline_size_if_necessary();
             fragment.assign_replaced_block_size_if_necessary(
