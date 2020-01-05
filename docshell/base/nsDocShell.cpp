e0d9e19de16bcc3adea48c100eb8bb354efac57d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 07:44:54 +0000

    Bug 1307736 - Ensure History loads pass valid triggeringPrincipal. r=bz
    UltraBlame original commit: f6379ae84275e13b70789c0fc9da853c50751aca

--- docshell/base/nsDocShell.cpp
+++ docshell/base/nsDocShell.cpp
@@ -12588,11 +12588,10 @@ nsDocShell::LoadHistoryEntry(nsISHEntry* aEntry, uint32_t aLoadType)
   }
 
   
-  
-  
-  
+  MOZ_ASSERT(triggeringPrincipal,
+             "need a valid triggeringPrincipal to load from history");
   if (!triggeringPrincipal) {
-    triggeringPrincipal = nsContentUtils::GetSystemPrincipal();
+    return NS_ERROR_FAILURE;
   }
 
   
