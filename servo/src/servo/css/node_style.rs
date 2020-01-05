1f341577d2cdc0f6eb865a4e31a3aa278c512644
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 08:35:27 +0000

    servo: Make the interface to calculated styles a little nicer
    Source-Repo: https://github.com/servo/servo
    Source-Revision: 37be37cbb102184d3eb1c60d1f230cb09b610bae
    
    UltraBlame original commit: d26663535d49b2f1f9eb754af785473eadef53f9

---
+++ servo/src/servo/css/node_style.rs
@@ -0,0 +1,103 @@
+
+
+
+
+
+
+
+use dom::node::Node;
+use newcss::color::{Color, rgba};
+use newcss::units::{Length, Px};
+use newcss::values::{CSSValue, Specified, Inherit};
+use newcss::values::{CSSMargin, CSSMarginLength};
+use newcss::values::{CSSBorderWidth, CSSBorderWidthLength};
+use newcss::computed::ComputedStyle;
+
+
+trait StyledNode {
+    fn style(&self) -> NodeStyle/&self;
+}
+
+impl Node: StyledNode {
+    fn style(&self) -> NodeStyle/&self {
+        NodeStyle::new(self)
+    }
+}
+
+
+pub struct NodeStyle {
+    priv node: &Node
+}
+
+impl NodeStyle {
+
+    static fn new(node: &r/Node) -> NodeStyle/&r {
+        NodeStyle {
+            node: node
+        }
+    }
+
+    fn background_color(&self) -> Color {
+        resolve(self, rgba(0, 0, 0, 0.0), |cs| cs.background_color() )
+    }
+
+    fn margin_top(&self) -> CSSMargin {
+        resolve(self, CSSMarginLength(Px(0.0)), |cs| cs.margin_top() )
+    }
+
+    fn margin_right(&self) -> CSSMargin {
+        resolve(self, CSSMarginLength(Px(0.0)), |cs| cs.margin_right() )
+    }
+
+    fn margin_bottom(&self) -> CSSMargin {
+        resolve(self, CSSMarginLength(Px(0.0)), |cs| cs.margin_bottom() )
+    }
+
+    fn margin_left(&self) -> CSSMargin {
+        resolve(self, CSSMarginLength(Px(0.0)), |cs| cs.margin_left() )
+    }
+
+    fn border_top_width(&self) -> CSSBorderWidth {
+        resolve(self, CSSBorderWidthLength(Px(0.0)), |cs| cs.border_top_width() )
+    }
+
+    fn border_right_width(&self) -> CSSBorderWidth {
+        resolve(self, CSSBorderWidthLength(Px(0.0)), |cs| cs.border_right_width() )
+    }
+
+    fn border_bottom_width(&self) -> CSSBorderWidth {
+        resolve(self, CSSBorderWidthLength(Px(0.0)), |cs| cs.border_bottom_width() )
+    }
+
+    fn border_left_width(&self) -> CSSBorderWidth {
+        resolve(self, CSSBorderWidthLength(Px(0.0)), |cs| cs.border_left_width() )
+    }
+
+    fn border_top_color(&self) -> Color {
+        resolve(self, rgba(255, 255, 255, 0.0), |cs| cs.border_top_color() )
+    }
+
+    fn border_right_color(&self) -> Color {
+        resolve(self, rgba(255, 255, 255, 0.0), |cs| cs.border_right_color() )
+    }
+
+    fn border_bottom_color(&self) -> Color {
+        resolve(self, rgba(255, 255, 255, 0.0), |cs| cs.border_bottom_color() )
+    }
+
+    fn border_left_color(&self) -> Color {
+        resolve(self, rgba(255, 255, 255, 0.0), |cs| cs.border_left_color() )
+    }
+
+}
+
+fn resolve<T>(node_style: &NodeStyle, default: T, get: &fn(cs: ComputedStyle) -> CSSValue<T>) -> T {
+    let node = node_style.node;
+    let select_res = node.get_css_select_results();
+    let computed = select_res.computed_style();
+    let value = get(computed);
+    match move value {
+        Inherit =>  move default,
+        Specified(move value) => move value,
+    }
+}
