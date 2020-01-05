1e912ebba773609712766e5fe555ae3e298002b3
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 17:37:22 +0000

    Bug 1340322 - Part 9: Templatize functions that will be used with servo's computed values. r=birtles
    MozReview-Commit-ID: 8d5wHBhF4y9
    
    UltraBlame original commit: 7bacb513bfc1ead958300239eb00d347496f2118

--- layout/style/nsAnimationManager.cpp
+++ layout/style/nsAnimationManager.cpp
@@ -473,13 +473,14 @@ private:
 static Maybe<ComputedTimingFunction>
 ConvertTimingFunction(const nsTimingFunction& aTimingFunction);
 
+template<class BuilderType>
 static void
 UpdateOldAnimationPropertiesWithNew(
     CSSAnimation& aOld,
     TimingParams& aNewTiming,
     nsTArray<Keyframe>&& aNewKeyframes,
     bool aNewIsStylePaused,
-    CSSAnimationBuilder& aBuilder)
+    BuilderType& aBuilder)
 {
   bool animationChanged = false;
 
@@ -526,11 +527,12 @@ UpdateOldAnimationPropertiesWithNew(
 
 
 
+template<class BuilderType>
 static already_AddRefed<CSSAnimation>
 BuildAnimation(nsPresContext* aPresContext,
                const NonOwningAnimationTarget& aTarget,
                const StyleAnimation& aSrc,
-               CSSAnimationBuilder& aBuilder,
+               BuilderType& aBuilder,
                nsAnimationManager::CSSAnimationCollection* aCollection)
 {
   MOZ_ASSERT(aPresContext);
@@ -1030,12 +1032,13 @@ CSSAnimationBuilder::GetComputedValue(nsPresContext* aPresContext,
   return result;
 }
 
+template<class BuilderType>
 static nsAnimationManager::OwningCSSAnimationPtrArray
 BuildAnimations(nsPresContext* aPresContext,
                 const NonOwningAnimationTarget& aTarget,
                 const nsStyleAutoArray<StyleAnimation>& aStyleAnimations,
                 uint32_t aStyleAnimationNameCount,
-                CSSAnimationBuilder& aBuilder,
+                BuilderType& aBuilder,
                 nsAnimationManager::CSSAnimationCollection* aCollection)
 {
   nsAnimationManager::OwningCSSAnimationPtrArray result;
@@ -1089,11 +1092,12 @@ nsAnimationManager::UpdateAnimations(nsStyleContext* aStyleContext,
   DoUpdateAnimations(target, *disp, builder);
 }
 
+template<class BuilderType>
 void
 nsAnimationManager::DoUpdateAnimations(
   const NonOwningAnimationTarget& aTarget,
   const nsStyleDisplay& aStyleDisplay,
-  CSSAnimationBuilder& aBuilder)
+  BuilderType& aBuilder)
 {
   
   
