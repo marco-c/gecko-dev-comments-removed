b8fd02a5797ce32c4c5d30abe623c743f990e48b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 00:51:24 +0000

    Bug 1307251 - make KTableEntry's constructors constexpr; r=dholbert
    This change removes (rather large) static constructors on some platforms.
    
    UltraBlame original commit: 5a1a95ccf071e5d9648d9402243afcfe8dd2e6cd

--- layout/style/nsCSSProps.h
+++ layout/style/nsCSSProps.h
@@ -340,7 +340,7 @@ public:
     
     
 
-    KTableEntry(nsCSSKeyword aKeyword, int16_t aValue)
+    constexpr KTableEntry(nsCSSKeyword aKeyword, int16_t aValue)
       : mKeyword(aKeyword)
       , mValue(aValue)
     {
@@ -348,7 +348,7 @@ public:
 
     template<typename T,
              typename = typename std::enable_if<std::is_enum<T>::value>::type>
-    KTableEntry(nsCSSKeyword aKeyword, T aValue)
+    constexpr KTableEntry(nsCSSKeyword aKeyword, T aValue)
       : mKeyword(aKeyword)
       , mValue(static_cast<int16_t>(aValue))
     {
