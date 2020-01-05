26cb8c2e753344f94f9e7f96278d77bb3ec04d43
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 17:44:58 +0000

    Bug 1314361 - Part 7: Strip addonId from existing DOMStorage origin attributes. r=mayhemer
    UltraBlame original commit: 99ca8cce005e8ace45d3030713960d9e658b0aa8

--- dom/storage/StorageDBUpdater.cpp
+++ dom/storage/StorageDBUpdater.cpp
@@ -1,8 +1,8 @@
-/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
-/* vim: set ts=8 sts=2 et sw=2 tw=80: */
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+
+
+
+
 
 #include "StorageManager.h"
 
@@ -15,8 +15,8 @@
 #include "mozilla/Services.h"
 #include "mozilla/Tokenizer.h"
 
-// Current version of the database schema
-#define CURRENT_SCHEMA_VERSION 1
+
+#define CURRENT_SCHEMA_VERSION 2
 
 namespace mozilla {
 namespace dom {
@@ -57,7 +57,7 @@ nsReverseStringSQLFunction::OnFunctionCall(
   return NS_OK;
 }
 
-// "scope" to "origin attributes suffix" and "origin key" convertor
+
 
 class ExtractOriginData : protected mozilla::Tokenizer
 {
@@ -68,24 +68,24 @@ public:
   {
     using mozilla::OriginAttributes;
 
-    // Parse optional appId:isInIsolatedMozBrowserElement: string, in case
-    // we don't find it, the scope is our new origin key and suffix
-    // is empty.
+    
+    
+    
     suffix.Truncate();
     origin.Assign(scope);
 
-    // Bail out if it isn't appId.
+    
     uint32_t appId;
     if (!ReadInteger(&appId)) {
       return;
     }
 
-    // Should be followed by a colon.
+    
     if (!CheckChar(':')) {
       return;
     }
 
-    // Bail out if it isn't 'isolatedBrowserFlag'.
+    
     nsDependentCSubstring isolatedBrowserFlag;
     if (!ReadWord(isolatedBrowserFlag)) {
       return;
@@ -97,34 +97,34 @@ public:
       return;
     }
 
-    // Should be followed by a colon.
+    
     if (!CheckChar(':')) {
       return;
     }
 
-    // OK, we have found appId and inIsolatedMozBrowser flag, create the suffix
-    // from it and take the rest as the origin key.
+    
+    
 
-    // If the profile went through schema 1 -> schema 0 -> schema 1 switching
-    // we may have stored the full attributes origin suffix when there were
-    // more than just appId and inIsolatedMozBrowser set on storage principal's
-    // OriginAttributes.
-    //
-    // To preserve full uniqueness we store this suffix to the scope key.
-    // Schema 0 code will just ignore it while keeping the scoping unique.
-    //
-    // The whole scope string is in one of the following forms (when we are
-    // here):
-    //
-    // "1001:f:^appId=1001&inBrowser=false&addonId=101:gro.allizom.rxd.:https:443"
-    // "1001:f:gro.allizom.rxd.:https:443"
-    //         |
-    //         +- the parser cursor position.
-    //
-    // If there is '^', the full origin attributes suffix follows.  We search
-    // for ':' since it is the delimiter used in the scope string and is never
-    // contained in the origin attributes suffix.  Remaining string after
-    // the comma is the reversed-domain+schema+port tuple.
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     Record();
     if (CheckChar('^')) {
       Token t;
@@ -139,7 +139,7 @@ public:
       attrs.CreateSuffix(suffix);
     }
 
-    // Consume the rest of the input as "origin".
+    
     origin.Assign(Substring(mCursor, mEnd));
   }
 };
@@ -197,7 +197,48 @@ GetOriginParticular::OnFunctionCall(
   return NS_OK;
 }
 
-} // namespace
+class StripOriginAddonId final : public mozIStorageFunction
+{
+public:
+  explicit StripOriginAddonId() {}
+
+private:
+  ~StripOriginAddonId() {}
+
+  NS_DECL_ISUPPORTS
+  NS_DECL_MOZISTORAGEFUNCTION
+};
+
+NS_IMPL_ISUPPORTS(StripOriginAddonId, mozIStorageFunction)
+
+NS_IMETHODIMP
+StripOriginAddonId::OnFunctionCall(
+    mozIStorageValueArray* aFunctionArguments, nsIVariant** aResult)
+{
+  nsresult rv;
+
+  nsAutoCString suffix;
+  rv = aFunctionArguments->GetUTF8String(0, suffix);
+  NS_ENSURE_SUCCESS(rv, rv);
+
+  
+  
+  OriginAttributes oa;
+  bool ok = oa.PopulateFromSuffix(suffix);
+  NS_ENSURE_TRUE(ok, NS_ERROR_FAILURE);
+
+  nsAutoCString newSuffix;
+  oa.CreateSuffix(newSuffix);
+
+  nsCOMPtr<nsIWritableVariant> outVar = new nsVariant();
+  rv = outVar->SetAsAUTF8String(newSuffix);
+  NS_ENSURE_SUCCESS(rv, rv);
+
+  outVar.forget(aResult);
+  return NS_OK;
+}
+
+} 
 
 namespace StorageDBUpdater {
 
@@ -209,7 +250,7 @@ nsresult CreateSchema1Tables(mozIStorageConnection *aWorkerConnection)
           "CREATE TABLE IF NOT EXISTS webappsstore2 ("
           "originAttributes TEXT, "
           "originKey TEXT, "
-          "scope TEXT, " // Only for schema0 downgrade compatibility
+          "scope TEXT, " 
           "key TEXT, "
           "value TEXT)"));
   NS_ENSURE_SUCCESS(rv, rv);
@@ -234,7 +275,7 @@ nsresult Update(mozIStorageConnection *aWorkerConnection)
   rv = aWorkerConnection->GetSchemaVersion(&schemaVer);
   NS_ENSURE_SUCCESS(rv, rv);
 
-  // downgrade (v0) -> upgrade (v1+) specific code
+  
   if (schemaVer >= 1) {
     bool schema0IndexExists;
     rv = aWorkerConnection->IndexExists(NS_LITERAL_CSTRING("scope_key_index"),
@@ -242,12 +283,12 @@ nsresult Update(mozIStorageConnection *aWorkerConnection)
     NS_ENSURE_SUCCESS(rv, rv);
 
     if (schema0IndexExists) {
-      // If this index exists, the database (already updated to schema >1)
-      // has been run again on schema 0 code.  That recreated that index
-      // and might store some new rows while updating only the 'scope' column.
-      // For such added rows we must fill the new 'origin*' columns correctly
-      // otherwise there would be a data loss.  The safest way to do it is to
-      // simply run the whole update to schema 1 again.
+      
+      
+      
+      
+      
+      
       schemaVer = 0;
     }
   }
@@ -268,9 +309,9 @@ nsresult Update(mozIStorageConnection *aWorkerConnection)
 
     if (!webappsstore2Exists && !webappsstoreExists &&
         !moz_webappsstoreExists) {
-      // The database is empty, this is the first start.  Just create the schema
-      // table and break to the next version to update to, i.e. bypass update
-      // from the old version.
+      
+      
+      
 
       rv = CreateSchema1Tables(aWorkerConnection);
       NS_ENSURE_SUCCESS(rv, rv);
@@ -283,7 +324,7 @@ nsresult Update(mozIStorageConnection *aWorkerConnection)
 
     doVacuum = true;
 
-    // Ensure Gecko 1.9.1 storage table
+    
     rv = aWorkerConnection->ExecuteSimpleSQL(NS_LITERAL_CSTRING(
            "CREATE TABLE IF NOT EXISTS webappsstore2 ("
            "scope TEXT, "
@@ -305,10 +346,10 @@ nsresult Update(mozIStorageConnection *aWorkerConnection)
                                            1, function1);
     NS_ENSURE_SUCCESS(rv, rv);
 
-    // Check if there is storage of Gecko 1.9.0 and if so, upgrade that storage
-    // to actual webappsstore2 table and drop the obsolete table. First process
-    // this newer table upgrade to priority potential duplicates from older
-    // storage table.
+    
+    
+    
+    
     if (webappsstoreExists) {
       rv = aWorkerConnection->ExecuteSimpleSQL(
         NS_LITERAL_CSTRING("INSERT OR IGNORE INTO "
@@ -322,9 +363,9 @@ nsresult Update(mozIStorageConnection *aWorkerConnection)
       NS_ENSURE_SUCCESS(rv, rv);
     }
 
-    // Check if there is storage of Gecko 1.8 and if so, upgrade that storage
-    // to actual webappsstore2 table and drop the obsolete table. Potential
-    // duplicates will be ignored.
+    
+    
+    
     if (moz_webappsstoreExists) {
       rv = aWorkerConnection->ExecuteSimpleSQL(
         NS_LITERAL_CSTRING("INSERT OR IGNORE INTO "
@@ -340,10 +381,10 @@ nsresult Update(mozIStorageConnection *aWorkerConnection)
 
     aWorkerConnection->RemoveFunction(NS_LITERAL_CSTRING("REVERSESTRING"));
 
-    // Update the scoping to match the new implememntation: split to oa suffix
-    // and origin key First rename the old table, we want to remove some columns
-    // no longer needed, but even before that drop all indexes from it (CREATE
-    // IF NOT EXISTS for index on the new table would falsely find the index!)
+    
+    
+    
+    
     rv = aWorkerConnection->ExecuteSimpleSQL(NS_LITERAL_CSTRING(
           "DROP INDEX IF EXISTS webappsstore2.origin_key_index"));
     NS_ENSURE_SUCCESS(rv, rv);
@@ -368,7 +409,7 @@ nsresult Update(mozIStorageConnection *aWorkerConnection)
                                            1, originKeyFunc);
     NS_ENSURE_SUCCESS(rv, rv);
 
-    // Here we ensure this schema tables when we are updating.
+    
     rv = CreateSchema1Tables(aWorkerConnection);
     NS_ENSURE_SUCCESS(rv, rv);
 
@@ -391,26 +432,45 @@ nsresult Update(mozIStorageConnection *aWorkerConnection)
 
     MOZ_FALLTHROUGH;
   }
+  case 1: {
+    nsCOMPtr<mozIStorageFunction> oaStripAddonId(
+      new StripOriginAddonId());
+    rv = aWorkerConnection->CreateFunction(NS_LITERAL_CSTRING("STRIP_ADDON_ID"), 1, oaStripAddonId);
+    NS_ENSURE_SUCCESS(rv, rv);
+
+    rv = aWorkerConnection->ExecuteSimpleSQL(NS_LITERAL_CSTRING(
+          "UPDATE webappsstore2 "
+          "SET originAttributes = STRIP_ADDON_ID(originAttributes) "
+          "WHERE originAttributes LIKE '^%'"));
+    NS_ENSURE_SUCCESS(rv, rv);
+
+    aWorkerConnection->RemoveFunction(NS_LITERAL_CSTRING("STRIP_ADDON_ID"));
+
+    rv = aWorkerConnection->SetSchemaVersion(2);
+    NS_ENSURE_SUCCESS(rv, rv);
+
+    MOZ_FALLTHROUGH;
+  }
   case CURRENT_SCHEMA_VERSION:
-    // Ensure the tables and indexes are up.  This is mostly a no-op
-    // in common scenarios.
+    
+    
     rv = CreateSchema1Tables(aWorkerConnection);
     NS_ENSURE_SUCCESS(rv, rv);
 
-    // Nothing more to do here, this is the current schema version
+    
     break;
 
   default:
     MOZ_ASSERT(false);
     break;
-  } // switch
+  } 
 
   rv = transaction.Commit();
   NS_ENSURE_SUCCESS(rv, rv);
 
   if (doVacuum) {
-    // In some cases this can make the disk file of the database significantly
-    // smaller.  VACUUM cannot be executed inside a transaction.
+    
+    
     rv = aWorkerConnection->ExecuteSimpleSQL(
       NS_LITERAL_CSTRING("VACUUM"));
     NS_ENSURE_SUCCESS(rv, rv);
@@ -419,6 +479,6 @@ nsresult Update(mozIStorageConnection *aWorkerConnection)
   return NS_OK;
 }
 
-} // namespace StorageDBUpdater
-} // namespace dom
-} // namespace mozilla
+} 
+} 
+} 
