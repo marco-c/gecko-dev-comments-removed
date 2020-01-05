ebc2dac2f83d8e4d34ab4fdfd20c5c9d462bfabe
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 12:33:02 +0000

    servo: Merge #15381 - Use SIMD in `gfx` when possible (from servo:simd); r=larsbergstrom
    Source-Repo: https://github.com/servo/servo
    Source-Revision: 725b805c45eedf96cd3b0b0ea10275470acf83bf
    
    UltraBlame original commit: 48d7c2837e6596e17ebc64585708ca9822290e7b

--- servo/components/gfx/lib.rs
+++ servo/components/gfx/lib.rs
@@ -3,6 +3,7 @@
 
 
 
+#![feature(cfg_target_feature)]
 #![cfg_attr(any(target_os = "linux", target_os = "android", target_os = "windows"), feature(heap_api))]
 
 #![feature(alloc)]
@@ -62,7 +63,7 @@ extern crate serde_derive;
 extern crate servo_geometry;
 extern crate servo_url;
 #[macro_use] extern crate servo_atoms;
-#[cfg(any(target_arch = "x86_64", target_arch = "aarch64"))]
+#[cfg(any(target_feature = "sse2", target_feature = "neon"))]
 extern crate simd;
 extern crate smallvec;
 extern crate style;
--- servo/components/gfx/text/glyph.rs
+++ servo/components/gfx/text/glyph.rs
@@ -5,7 +5,7 @@
 use app_units::Au;
 use euclid::point::Point2D;
 use range::{self, EachIndex, Range, RangeIndex};
-#[cfg(any(target_arch = "x86_64", target_arch = "aarch64"))]
+#[cfg(any(target_feature = "sse2", target_feature = "neon"))]
 use simd::u32x4;
 use std::{fmt, mem, u16};
 use std::cmp::{Ordering, PartialOrd};
@@ -74,7 +74,7 @@ pub type GlyphId = u32;
 
 
 const FLAG_CHAR_IS_SPACE: u32       = 0x40000000;
-#[cfg(any(target_arch = "x86_64", target_arch = "aarch64"))]
+#[cfg(any(target_feature = "sse2", target_feature = "neon"))]
 const FLAG_CHAR_IS_SPACE_SHIFT: u32 = 30;
 const FLAG_IS_SIMPLE_GLYPH: u32     = 0x80000000;
 
@@ -591,7 +591,7 @@ impl<'a> GlyphStore {
     }
 
     #[inline]
-    #[cfg(any(target_arch = "x86_64", target_arch = "aarch64"))]
+    #[cfg(any(target_feature = "sse2", target_feature = "neon"))]
     fn advance_for_byte_range_simple_glyphs(&self, range: &Range<ByteIndex>, extra_word_spacing: Au) -> Au {
         let advance_mask = u32x4::splat(GLYPH_ADVANCE_MASK);
         let space_flag_mask = u32x4::splat(FLAG_CHAR_IS_SPACE);
@@ -634,14 +634,14 @@ impl<'a> GlyphStore {
 
     
     #[inline]
-    #[cfg(not(any(target_arch = "x86_64", target_arch = "aarch64")))]
+    #[cfg(not(any(target_feature = "sse2", target_feature = "neon")))]
     fn advance_for_byte_range_simple_glyphs(&self, range: &Range<ByteIndex>, extra_word_spacing: Au) -> Au {
         self.advance_for_byte_range_slow_path(range, extra_word_spacing)
     }
 
     
     #[inline]
-    #[cfg(any(target_arch = "x86_64", target_arch = "aarch64"))]
+    #[cfg(any(target_feature = "sse2", target_feature = "neon"))]
     #[allow(unsafe_code)]
     fn transmute_entry_buffer_to_u32_buffer(&self) -> &[u32] {
         unsafe { mem::transmute(self.entry_buffer.as_slice()) }
