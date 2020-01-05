d81427ddfd7d34b21e74c68ade6e8551f6a9fd7a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 22:14:44 +0000

    Bug 1356243 - Enable Screenshots by default. r=Mossop
    MozReview-Commit-ID: 6qSVA9IcwIi
    
    UltraBlame original commit: ad9d525e6db72327cd534740981fdf3b7a11a3ae

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -64,9 +64,6 @@ pref("extensions.hotfix.certs.2.sha1Fingerprint", "39:E7:2B:7A:5B:CF:37:78:F9:5D
 pref("extensions.systemAddon.update.url", "https://aus5.mozilla.org/update/3/SystemAddons/%VERSION%/%BUILD_ID%/%BUILD_TARGET%/%LOCALE%/%CHANNEL%/%OS_VERSION%/%DISTRIBUTION%/%DISTRIBUTION_VERSION%/update.xml");
 
 
-pref("extensions.screenshots.system-disabled", true);
-
-
 
 pref("extensions.autoDisableScopes", 15);
 
