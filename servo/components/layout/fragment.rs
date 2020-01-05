75222979e65c75b33cc2ff21737b96e2da148c00
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 09:24:53 +0000

    servo: Merge #3296 - Support CSS2 background-position keyword and percent values. r=SimonSapin (from mbrubeck:background-position)
    Source-Repo: https://github.com/servo/servo
    Source-Revision: 8e2ac7a5c54326f47cbb23394142beee0c17cff2
    
    UltraBlame original commit: 4ff4c54ac4284c279e26128d0cbd375e3ab12a55

--- servo/components/layout/fragment.rs
+++ servo/components/layout/fragment.rs
@@ -728,12 +728,15 @@ impl Fragment {
         };
         debug!("(building display list) building background image");
 
+        let image_width = Au::from_px(image.width as int);
+        let image_height = Au::from_px(image.height as int);
+
         
         let mut bounds = *absolute_bounds;
         let horizontal_position = model::specified(background.background_position.horizontal,
-                                                   bounds.size.width);
+                                                   bounds.size.width - image_width);
         let vertical_position = model::specified(background.background_position.vertical,
-                                                 bounds.size.height);
+                                                 bounds.size.height - image_height);
 
         
         
@@ -742,9 +745,6 @@ impl Fragment {
             base: BaseDisplayItem::new(bounds, self.node, level),
             children: DisplayList::new(),
         });
-
-        let image_width = Au::from_px(image.width as int);
-        let image_height = Au::from_px(image.height as int);
 
         match background.background_attachment {
             background_attachment::scroll => {
@@ -759,25 +759,21 @@ impl Fragment {
                     }
                     background_repeat::repeat_x => {
                         bounds.size.height = Au::min(bounds.size.height - vertical_position, image_height);
-                        if horizontal_position > Au(0) {
-                            bounds.origin.x = bounds.origin.x - image_width;
+                        if horizontal_position != Au(0) {
                             bounds.size.width = bounds.size.width + image_width;
                         }
                     }
                     background_repeat::repeat_y => {
                         bounds.size.width = Au::min(bounds.size.width - horizontal_position, image_width);
-                        if vertical_position > Au(0) {
-                            bounds.origin.y = bounds.origin.y - image_height;
+                        if vertical_position != Au(0) {
                             bounds.size.height = bounds.size.height + image_height;
                         }
                     }
                     background_repeat::repeat => {
-                        if horizontal_position > Au(0) {
-                            bounds.origin.x = bounds.origin.x - image_width;
+                        if horizontal_position != Au(0) {
                             bounds.size.width = bounds.size.width + image_width;
                         }
-                        if vertical_position > Au(0) {
-                            bounds.origin.y = bounds.origin.y - image_height;
+                        if vertical_position != Au(0) {
                             bounds.size.height = bounds.size.height + image_height;
                         }
                     }
