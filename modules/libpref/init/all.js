13f6a53baca94f8f14797c14551aa517ffab4bb2
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 02:24:37 +0000

    Bug 1305135 - Disable async rendering on Nightly. Required a modification due to changes in build defines. r=bsmedberg
    MozReview-Commit-ID: GqImLPdsa3m
    
    UltraBlame original commit: 1da68ee13f25373b5a8f310fe3d598f4f36dcf72

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -2824,8 +2824,12 @@ pref("dom.ipc.plugins.unloadTimeoutSecs", 30);
 
 pref("dom.ipc.plugins.asyncInit.enabled", false);
 
+#ifdef RELEASE_OR_BETA
+pref("dom.ipc.plugins.asyncdrawing.enabled", false);
+#else
 
 pref("dom.ipc.plugins.asyncdrawing.enabled", true);
+#endif
 
 pref("dom.ipc.processCount", 1);
 
