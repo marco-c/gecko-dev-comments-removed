57167c7cb58779ce349662d499d8c81481f574ee
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 05:53:48 +0000

    Bug 1324320 follow-up: Fix a mistake that breaks the build
    UltraBlame original commit: 70a3d7e03d550eab7b5d9478c7192c0ae7b535e5

--- build/clang-plugin/BaseCheck.h
+++ build/clang-plugin/BaseCheck.h
@@ -2,15 +2,15 @@
 
 
 
-#ifndef MozCheck_h__
-#define MozCheck_h__
+#ifndef BaseCheck_h__
+#define BaseCheck_h__
 
 class MozContext {};
 typedef MozContext ContextType;
 
-class MozCheck : public MatchFinder::MatchCallback {
+class BaseCheck : public MatchFinder::MatchCallback {
 public:
-  MozCheck(StringRef CheckName, ContextType* Context) {}
+  BaseCheck(StringRef CheckName, ContextType* Context) {}
   virtual void registerMatchers(MatchFinder *Finder) {}
   virtual void registerPPCallbacks(CompilerInstance& CI) {}
   virtual void check(const MatchFinder::MatchResult &Result) {}
@@ -18,7 +18,6 @@ public:
                          DiagnosticIDs::Level Level = DiagnosticIDs::Warning) {
     DiagnosticsEngine &Diag = Context->getDiagnostics();
     unsigned ID = Diag.getDiagnosticIDs()->getCustomDiagID(Level, Description);
-    
     return Diag.Report(Loc, ID);
   }
 
