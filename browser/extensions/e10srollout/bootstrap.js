20044c0a89b8e95201ddde4d62f2f39e548541ba
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 18:56:42 +0000

    Bug 1344345 - Revert e10s addons rule to only activate on mpc=true and webextensions. r=mconley
    MozReview-Commit-ID: 738BO2QqvzC
    
    UltraBlame original commit: d572929cc1829e28403125aa05ec5930cf914fd3

--- browser/extensions/e10srollout/bootstrap.js
+++ browser/extensions/e10srollout/bootstrap.js
@@ -19,7 +19,7 @@ const TEST_THRESHOLD = {
 
 const ADDON_ROLLOUT_POLICY = {
   "beta"    : "51alladdons", 
-  "release" : "51set1",
+  "release" : "50allmpc",
   "esr"     : "esrA", 
 };
 
@@ -73,7 +73,10 @@ function defineCohort() {
 
     Preferences.set(PREF_E10S_ADDON_BLOCKLIST,
                     
-                    "{dc572301-7619-498c-a57d-39143191b318};");
+                    "{dc572301-7619-498c-a57d-39143191b318};"
+                    
+                    + "firefox@mega.co.nz"
+                    );
   } else {
     Preferences.reset(PREF_E10S_ADDON_POLICY);
   }
