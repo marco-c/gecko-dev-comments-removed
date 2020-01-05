412ad72c0b61e1cb4d15b5d0a9d3ebc58b220431
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 06:45:31 +0000

    Bug 851892 part 15.  Convert CSSFontFaceRule to WebIDL.  r=peterv,heycam
    UltraBlame original commit: eda4ef1e028891d7a188badc964fdf968541a15f

--- layout/style/nsCSSRules.h
+++ layout/style/nsCSSRules.h
@@ -249,14 +249,12 @@ public:
   nsCSSFontFaceRule(uint32_t aLineNumber, uint32_t aColumnNumber)
     : mozilla::css::Rule(aLineNumber, aColumnNumber)
   {
-    SetIsNotDOMBinding();
   }
 
   nsCSSFontFaceRule(const nsCSSFontFaceRule& aCopy)
     
     : mozilla::css::Rule(aCopy), mDecl(aCopy.mDecl)
   {
-    SetIsNotDOMBinding();
   }
 
   NS_DECL_ISUPPORTS_INHERITED
@@ -279,6 +277,7 @@ public:
   
   uint16_t Type() const override;
   void GetCssTextImpl(nsAString& aCssText) const override;
+  nsICSSDeclaration* Style();
 
   virtual size_t SizeOfIncludingThis(mozilla::MallocSizeOf aMallocSizeOf) const override;
 
