bcc356b2a3c0303b3aeb3bcfd4302fc132402726
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 06:28:35 +0000

    Merge mozilla-central to mozilla-inbound
    UltraBlame original commit: 48517fcee7701be4f684128d09d4a9d3dcd80a4f

--- js/src/vm/Scope.h
+++ js/src/vm/Scope.h
@@ -71,7 +71,10 @@ enum class ScopeKind : uint8_t
     NonSyntactic,
 
     
-    Module
+    Module,
+
+    
+    WasmFunction
 };
 
 static inline bool
@@ -896,6 +899,57 @@ class ModuleScope : public Scope
 
 
 
+class WasmFunctionScope : public Scope
+{
+    friend class BindingIter;
+    friend class Scope;
+    static const ScopeKind classScopeKind_ = ScopeKind::WasmFunction;
+
+  public:
+    struct Data
+    {
+        uint32_t length;
+        uint32_t nextFrameSlot;
+        uint32_t funcIndex;
+
+        
+        GCPtr<WasmInstanceObject*> instance;
+
+        BindingName names[1];
+
+        void trace(JSTracer* trc);
+    };
+
+    static WasmFunctionScope* create(JSContext* cx, WasmInstanceObject* instance, uint32_t funcIndex);
+
+    static size_t sizeOfData(uint32_t length) {
+        return sizeof(Data) + (length ? length - 1 : 0) * sizeof(BindingName);
+    }
+
+  private:
+    Data& data() {
+        return *reinterpret_cast<Data*>(data_);
+    }
+
+    const Data& data() const {
+        return *reinterpret_cast<Data*>(data_);
+    }
+
+  public:
+    WasmInstanceObject* instance() const {
+        return data().instance;
+    }
+
+    uint32_t funcIndex() const {
+        return data().funcIndex;
+    }
+
+    static Shape* getEmptyEnvironmentShape(ExclusiveContext* cx);
+};
+
+
+
+
 
 
 
@@ -1002,6 +1056,7 @@ class BindingIter
     void init(GlobalScope::Data& data);
     void init(EvalScope::Data& data, bool strict);
     void init(ModuleScope::Data& data);
+    void init(WasmFunctionScope::Data& data);
 
     bool hasFormalParameterExprs() const {
         return flags_ & HasFormalParameterExprs;
@@ -1071,6 +1126,10 @@ class BindingIter
     }
 
     explicit BindingIter(ModuleScope::Data& data) {
+        init(data);
+    }
+
+    explicit BindingIter(WasmFunctionScope::Data& data) {
         init(data);
     }
 
@@ -1378,6 +1437,7 @@ DEFINE_SCOPE_DATA_GCPOLICY(js::VarScope::Data);
 DEFINE_SCOPE_DATA_GCPOLICY(js::GlobalScope::Data);
 DEFINE_SCOPE_DATA_GCPOLICY(js::EvalScope::Data);
 DEFINE_SCOPE_DATA_GCPOLICY(js::ModuleScope::Data);
+DEFINE_SCOPE_DATA_GCPOLICY(js::WasmFunctionScope::Data);
 
 #undef DEFINE_SCOPE_DATA_GCPOLICY
 
