5af657dcfed7e55a46f4743222d98a9653d40750
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 21:38:21 +0000

    Bug 1304656 - Puppeteers restart method has to pass-through arguments. r=maja_zf
    Puppeteer enforces to use an in_app restart, unless the clean argument is
    set to True. But whatever case is present, all passed in arguments have
    to be forwarded to Marionette.
    
    MozReview-Commit-ID: ADPRvuXhyXh
    
    UltraBlame original commit: a73bf50396ab6e6756c06bac380ef2bb36edbd41

--- testing/marionette/puppeteer/firefox/firefox_puppeteer/mixins.py
+++ testing/marionette/puppeteer/firefox/firefox_puppeteer/mixins.py
@@ -60,15 +60,16 @@ class PuppeteerMixin(object):
             self.browser.tabbar.close_all_tabs([self.browser.tabbar.tabs[0]])
             self.browser.tabbar.tabs[0].switch_to()
 
-    def restart(self, **kwargs):
+    def restart(self, *args, **kwargs):
         """Restart Firefox and re-initialize data.
 
         :param flags: Specific restart flags for Firefox
         """
-        if kwargs.get('clean'):
-            self.marionette.restart(clean=True)
-        else:
-            self.marionette.restart(in_app=True)
+        
+        if not kwargs.get('clean'):
+            kwargs.update({"in_app": True})
+
+        self.marionette.restart(*args, **kwargs)
 
         
         self.browser = self.puppeteer.windows.switch_to(lambda win: type(win) is BrowserWindow)
