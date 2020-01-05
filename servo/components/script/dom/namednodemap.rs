2dcdc6e8bf75b0e5d777d97424d6ffea10a2f1c0
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 10:02:08 +0000

    servo: Merge #5594 - Refactoring, added conversion to lowercase for get_attribute_by_name method. (fixes #5561) (from mskrzypkows:get_attribute_by_name); r=Ms2ger
    Source-Repo: https://github.com/servo/servo
    Source-Revision: a3dafe49999d24e12b2f6b5cc74b468e58b7384a
    
    UltraBlame original commit: 78cab5ebc213cf95a8c6fc1067c5d342e3388f53

--- servo/components/script/dom/namednodemap.rs
+++ servo/components/script/dom/namednodemap.rs
@@ -60,8 +60,7 @@ impl<'a> NamedNodeMapMethods for JSRef<'a, NamedNodeMap> {
         let owner = self.owner.root();
         
         let owner = owner.r();
-        let name = owner.parsed_name(name);
-        owner.get_attribute_by_name(&Atom::from_slice(&name))
+        owner.get_attribute_by_name(name)
     }
 
     
