6d30031480419fd2fe44a4b4361c1e3a1b2e4275
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 09:04:49 +0000

    servo: Merge #2080 - Clear tiles before dropping CompositorLayer (from mbrubeck:2069-forget-tiles); r=larsbergstrom
    This fixes a pixmap leak and task failure (#2069).
    
    Source-Repo: https://github.com/servo/servo
    Source-Revision: 5d12dc805b2714ad2e3094ed84efe41281ec24d5
    
    UltraBlame original commit: 91a7302890d0933e8c56a084a56600dd81b9d329

--- servo/src/components/main/compositing/compositor.rs
+++ servo/src/components/main/compositing/compositor.rs
@@ -394,6 +394,11 @@ impl IOCompositor {
 
             ContainerLayer::add_child_start(self.root_layer.clone(),
                                             ContainerLayerKind(new_layer.root_layer.clone()));
+
+            
+            for layer in self.compositor_layer.mut_iter() {
+                layer.clear_all_tiles();
+            }
             self.compositor_layer = Some(new_layer);
         }
 
