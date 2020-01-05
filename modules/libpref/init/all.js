2661d8c7523cf75e1878ebd228895d45a969ca58
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 03:21:13 +0000

    merge mozilla-inbound to mozilla-central a=merge
    UltraBlame original commit: d38d06f85ef59c5dbb5d4a1a8d895957a78714de

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -381,7 +381,6 @@ pref("media.raw.enabled", true);
 pref("media.ogg.enabled", true);
 pref("media.opus.enabled", true);
 pref("media.wave.enabled", true);
-pref("media.wave.decoder.enabled", true);
 pref("media.webm.enabled", true);
 
 #ifdef MOZ_APPLEMEDIA
@@ -578,7 +577,6 @@ pref("media.mediasource.webm.enabled", true);
 pref("media.mediasource.webm.audio.enabled", true);
 
 
-pref("media.format-reader.ogg", true);
 pref("media.flac.enabled", true);
 pref("media.ogg.flac.enabled", true);
 
@@ -1186,6 +1184,9 @@ pref("dom.forms.color", true);
 
 
 pref("dom.forms.datetime", false);
+
+
+pref("dom.forms.datetime.timepicker", false);
 
 
 pref("dom.forms.autocomplete.experimental", false);
@@ -4613,6 +4614,7 @@ pref("layers.offmainthreadcomposition.log-animations", false);
 pref("layers.bufferrotation.enabled", true);
 
 pref("layers.componentalpha.enabled", true);
+pref("layers.draw-mask-debug", false);
 
 
 pref("gfx.content.use-native-pushlayer", false);
