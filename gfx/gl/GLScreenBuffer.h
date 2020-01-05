396f03fd9121fdcc55c732e8f8565c7471934ef4
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 05:48:01 +0000

    Bug 1325483 - Query DRAW_BUFFER0 from GLScreenBuffer. - r=daoshengmu
    MozReview-Commit-ID: KjhbQM9B8EZ
    
    UltraBlame original commit: 88a11be7bd194e1920c5e8f45ccfeef7ce69b6d2

--- gfx/gl/GLScreenBuffer.h
+++ gfx/gl/GLScreenBuffer.h
@@ -241,9 +241,8 @@ public:
     void SetReadBuffer(GLenum userMode);
     void SetDrawBuffer(GLenum userMode);
 
-    GLenum GetReadBufferMode() const {
-        return mUserReadBufferMode;
-    }
+    GLenum GetReadBufferMode() const { return mUserReadBufferMode; }
+    GLenum GetDrawBufferMode() const { return mUserDrawBufferMode; }
 
     
 
