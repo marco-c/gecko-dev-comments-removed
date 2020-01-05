83428d79f4000de6ef57353a515437839e29ef78
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 05:01:02 +0000

    Bug 1322070: P6. Construct AudioInfo object from Vorbis CodecState. r=gerald
    MozReview-Commit-ID: GitrIqFbSVd
    
    UltraBlame original commit: b03503ee52873796df74096716ce98dd96de9044

--- dom/media/ogg/OggCodecState.h
+++ dom/media/ogg/OggCodecState.h
@@ -68,6 +68,11 @@ public:
   ogg_packet* PopFront() { return static_cast<ogg_packet*>(nsDeque::PopFront()); }
   ogg_packet* PeekFront() { return static_cast<ogg_packet*>(nsDeque::PeekFront()); }
   ogg_packet* Pop() { return static_cast<ogg_packet*>(nsDeque::Pop()); }
+  ogg_packet* operator[](size_t aIndex) const
+  {
+    return static_cast<ogg_packet*>(nsDeque::ObjectAt(aIndex));
+  }
+  size_t Length() const { return nsDeque::GetSize(); }
   void PushFront(ogg_packet* aPacket) { nsDeque::PushFront(aPacket); }
   void Erase() { nsDeque::Erase(); }
 };
@@ -280,19 +285,21 @@ public:
   nsresult Reset() override;
   bool IsHeader(ogg_packet* aPacket) override;
   nsresult PageIn(ogg_page* aPage) override;
+  const TrackInfo* GetInfo() const override { return &mInfo; }
 
   
   MetadataTags* GetTags() override;
 
-  
-  static int64_t Time(vorbis_info* aInfo, int64_t aGranulePos);
-
-  vorbis_info mInfo;
+private:
+  AudioInfo mInfo;
+  vorbis_info mVorbisInfo;
   vorbis_comment mComment;
   vorbis_dsp_state mDsp;
   vorbis_block mBlock;
+  OggPacketQueue mHeaders;
 
-private:
+  
+  static int64_t Time(vorbis_info* aInfo, int64_t aGranulePos);
 
   
   
