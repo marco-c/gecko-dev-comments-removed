01e0d052c5db7bf5a29f7fd858e4afea303dec29
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 20:15:41 +0000

    Bug 1291320 - Make refresh command synchronous. r=ato,automatedtester
    Update the refresh command to make it synchronous, and let it return
    once the target page has been loaded. This can be accomplished by using
    the loadListener object in listener.js.
    
    MozReview-Commit-ID: Lc8QoGFeQrY
    
    UltraBlame original commit: c6eb25a8dc1cfe765b9e2ec11ce7fa1504383783

--- testing/marionette/driver.js
+++ testing/marionette/driver.js
@@ -1139,11 +1139,13 @@ GeckoDriver.prototype.goForward = function* (cmd, resp) {
 };
 
 
-GeckoDriver.prototype.refresh = function*(cmd, resp) {
+
+
+GeckoDriver.prototype.refresh = function* (cmd, resp) {
   assert.content(this.context);
   assert.window(this.getCurrentWindow());
 
-  yield this.listener.refresh();
+  yield this.listener.refresh({pageTimeout: this.timeouts.pageLoad});
 };
 
 
