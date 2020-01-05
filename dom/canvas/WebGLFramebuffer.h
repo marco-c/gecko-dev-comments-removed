d0c485978b26c6aa715d662326d53e66a7fc77d0
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 05:19:02 +0000

    Bug 1324157 - Check dst colorDrawBuffers against src in BlitFramebuffer. - r=lenzak800
    MozReview-Commit-ID: 1vzUTG4RD5
    
    UltraBlame original commit: 78aafdccfc4413551a35df0881978cc280fe93b1

--- dom/canvas/WebGLFramebuffer.h
+++ dom/canvas/WebGLFramebuffer.h
@@ -173,6 +173,7 @@ protected:
     struct ResolvedData {
         
         bool hasSampleBuffers;
+        std::vector<const WebGLFBAttachPoint*> colorDrawBuffers;
         const WebGLFBAttachPoint* depthBuffer;
         const WebGLFBAttachPoint* stencilBuffer;
 
