6bb6da916dd2fd8bad4a96d032acb81e61e623f7
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 11:57:12 +0000

    servo: Merge #14268 - Regenerate bindings (from Manishearth:regen); r=bholley
    Don't merge yet
    
    Source-Repo: https://github.com/servo/servo
    Source-Revision: 95bc7ece477d201605c77a5e73c211bdd7723ee9
    
    UltraBlame original commit: ad973d89dae31942d6e0a48d899b057537fd4551

--- servo/tests/unit/stylo/sanity_checks.rs
+++ servo/tests/unit/stylo/sanity_checks.rs
@@ -1,10 +1,10 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-
-
-
-
-
-
+//! Different static asserts that ensure the build does what it's expected to.
+//!
+//! TODO: maybe cfg(test) this?
 
 #![allow(unused_imports)]
 
@@ -19,32 +19,32 @@ macro_rules! check_enum_value {
     }
 }
 
-
-
+// NB: It's a shame we can't do this statically with bitflags, but no
+// const-fn and no other way to access the numerical value :-(
 macro_rules! check_enum_value_non_static {
     ($a:expr, $b:expr) => {
-        assert_eq!($a as usize, $b as usize);
+        assert_eq!($a.0 as usize, $b as usize);
     }
 }
 
 #[test]
 fn assert_restyle_hints_match() {
-    use style::restyle_hints::*; 
-    use style::gecko_bindings::structs::nsRestyleHint;
+    use style::restyle_hints::*; // For flags
+    use style::gecko_bindings::structs;
 
-    check_enum_value_non_static!(nsRestyleHint::eRestyle_Self, RESTYLE_SELF.bits());
-    
-    
-    
-    
-    
-    
-    check_enum_value_non_static!(nsRestyleHint::eRestyle_SomeDescendants, RESTYLE_DESCENDANTS.bits());
-    check_enum_value_non_static!(nsRestyleHint::eRestyle_LaterSiblings, RESTYLE_LATER_SIBLINGS.bits());
+    check_enum_value_non_static!(structs::nsRestyleHint_eRestyle_Self, RESTYLE_SELF.bits());
+    // XXX This for Servo actually means something like an hypothetical
+    // Restyle_AllDescendants (but without running selector matching on the
+    // element). ServoRestyleManager interprets it like that, but in practice we
+    // should align the behavior with Gecko adding a new restyle hint, maybe?
+    //
+    // See https://bugzilla.mozilla.org/show_bug.cgi?id=1291786
+    check_enum_value_non_static!(structs::nsRestyleHint_eRestyle_SomeDescendants, RESTYLE_DESCENDANTS.bits());
+    check_enum_value_non_static!(structs::nsRestyleHint_eRestyle_LaterSiblings, RESTYLE_LATER_SIBLINGS.bits());
 }
 
-
-
+// Note that we can't call each_pseudo_element, parse_pseudo_element, or
+// similar, because we'd need the foreign atom symbols to link.
 #[test]
 fn assert_basic_pseudo_elements() {
     let mut saw_before = false;
