9dbfba4468a1dcede5368959c7e5a926999b8927
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 11:07:06 +0000

    servo: Merge #10516 - Various cleanup (from Ms2ger:cleanup); r=nox
    Source-Repo: https://github.com/servo/servo
    Source-Revision: 5adf36231e739f57dc7d1d85b37fb2a47b8f5d74
    
    UltraBlame original commit: 989283b5ecb69d298781213c71854f995391d7dd

--- servo/components/script/dom/xmldocument.rs
+++ servo/components/script/dom/xmldocument.rs
@@ -4,7 +4,6 @@
 
 use document_loader::DocumentLoader;
 use dom::bindings::codegen::Bindings::DocumentBinding::DocumentMethods;
-use dom::bindings::codegen::Bindings::WindowBinding::WindowMethods;
 use dom::bindings::codegen::Bindings::XMLDocumentBinding::{self, XMLDocumentMethods};
 use dom::bindings::global::GlobalRef;
 use dom::bindings::inheritance::Castable;
@@ -77,16 +76,16 @@ impl XMLDocument {
 impl XMLDocumentMethods for XMLDocument {
     
     fn GetLocation(&self) -> Option<Root<Location>> {
-        self.document.GetLocation()
+        self.upcast::<Document>().GetLocation()
     }
 
     
     fn SupportedPropertyNames(&self) -> Vec<DOMString> {
-        self.document.SupportedPropertyNames()
+        self.upcast::<Document>().SupportedPropertyNames()
     }
 
     
     fn NamedGetter(&self, _cx: *mut JSContext, name: DOMString, found: &mut bool) -> *mut JSObject {
-        self.document.NamedGetter(_cx, name, found)
+        self.upcast::<Document>().NamedGetter(_cx, name, found)
     }
 }
