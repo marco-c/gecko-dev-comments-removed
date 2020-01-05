1d5d07abc132c778ddd9cae414075b3467d23803
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 17:18:41 +0000

    Bug 1335645 - Part 5 - Fix warning when test.relativeURI is undefined r=junior
    MozReview-Commit-ID: HosOLS1Rzpi
    
    UltraBlame original commit: c43904b6fb097226fae5c2a3e34d0b5814b371ef

--- netwerk/test/unit/test_URIs.js
+++ netwerk/test/unit/test_URIs.js
@@ -1,4 +1,10 @@
 
+
+
+
+
+"use strict";
+
 Components.utils.import("resource://gre/modules/NetUtil.jsm");
 
 var gIoService = Components.classes["@mozilla.org/network/io-service;1"]
@@ -300,7 +306,8 @@ function do_check_property(aTest, aURI, aPropertyName, aTestFunctor) {
 function do_test_uri_basic(aTest) {
   var URI;
 
-  do_info("Basic tests for " + aTest.spec + " relative URI: " + aTest.relativeURI);
+  do_info("Basic tests for " + aTest.spec +
+          " relative URI: " + (aTest.relativeURI === undefined ? "(none)" : aTest.relativeURI) );
 
   try {
     URI = NetUtil.newURI(aTest.spec);
