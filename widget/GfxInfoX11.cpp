aff35d4f5ec451055be79c6025a6f54518b00483
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 17:22:36 +0000

    Backed out changeset 4187cda95a1d (bug 1341959)
    UltraBlame original commit: f76695801b2a0bd36eb4263e2e797d3062889afe

--- widget/GfxInfoX11.cpp
+++ widget/GfxInfoX11.cpp
@@ -313,7 +313,6 @@ GfxInfo::GetFeatureStatusImpl(int32_t aFeature,
     
     if (aFeature == nsIGfxInfo::FEATURE_OPENGL_LAYERS ||
         aFeature == nsIGfxInfo::FEATURE_WEBGL_OPENGL ||
-        aFeature == nsIGfxInfo::FEATURE_WEBGL2 ||
         aFeature == nsIGfxInfo::FEATURE_WEBGL_MSAA) {
 
       
