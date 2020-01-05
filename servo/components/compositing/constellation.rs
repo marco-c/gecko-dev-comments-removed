caba3872f920488f52f03feb5ee86caf930ebb82
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 10:27:45 +0000

    servo: Merge #7545 - Update prefs API to return an Option<bool> (from jgraham:get_pref_option); r=jdm
    This allows for situations where there is no reasonable default
    to apply for the pref value e.g. when we are just listing values
    
    Source-Repo: https://github.com/servo/servo
    Source-Revision: 9d4217990060b3d2c93a4b39fe391985f01a5979
    
    UltraBlame original commit: 89cfbc72c06c945be69984a68f07ad69af33e6ca

--- servo/components/compositing/constellation.rs
+++ servo/components/compositing/constellation.rs
@@ -891,7 +891,7 @@ impl<LTF: LayoutTaskFactory, STF: ScriptTaskFactory> Constellation<LTF, STF> {
                                    containing_pipeline_id: PipelineId,
                                    subpage_id: SubpageId,
                                    event: MozBrowserEvent) {
-        assert!(prefs::get_pref("dom.mozbrowser.enabled", false));
+        assert!(prefs::get_pref("dom.mozbrowser.enabled").unwrap_or(false));
 
         
         
@@ -1373,7 +1373,7 @@ impl<LTF: LayoutTaskFactory, STF: ScriptTaskFactory> Constellation<LTF, STF> {
 
     
     fn trigger_mozbrowserlocationchange(&self, pipeline_id: PipelineId) {
-        if prefs::get_pref("dom.mozbrowser.enabled", false) {
+        if prefs::get_pref("dom.mozbrowser.enabled").unwrap_or(false) {
             
             let event_info = {
                 let pipeline = self.pipeline(pipeline_id);
