e66dd099d5786ad0a478a8a9057069d352c23694
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 06:44:22 +0000

    Backed out changeset 0ee273b613db (bug 1324093)
    UltraBlame original commit: b5ecc8950d720df408cfa60636c9e947ab7b9f0c

--- mfbt/Assertions.h
+++ mfbt/Assertions.h
@@ -23,6 +23,7 @@
 #include "nsTraceRefcnt.h"
 #endif
 
+#if defined(MOZ_HAS_MOZGLUE) || defined(MOZILLA_INTERNAL_API)
 
 
 
@@ -33,7 +34,6 @@ MOZ_BEGIN_EXTERN_C
 extern MFBT_DATA const char* gMozCrashReason;
 MOZ_END_EXTERN_C
 
-#if !defined(DEBUG) && (defined(MOZ_HAS_MOZGLUE) || defined(MOZILLA_INTERNAL_API))
 static inline void
 AnnotateMozCrashReason(const char* reason)
 {
