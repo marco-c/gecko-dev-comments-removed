6d72ed1b15ef8b535ee6fcc93bb6743329c082be
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 07:05:32 +0000

    Bug 1328142 - enable switching between simulcast receive streams r=jesup
    UltraBlame original commit: 4ea7585240cfbecfa047fb578ba04478f2f19e1f

--- media/webrtc/signaling/src/mediapipeline/MediaPipeline.h
+++ media/webrtc/signaling/src/mediapipeline/MediaPipeline.h
@@ -124,7 +124,7 @@ class MediaPipeline : public sigslot::has_slots<> {
   
   
   void SelectSsrc_m(size_t ssrc_index);
-  void SelectSsrc_s(size_t ssrc_index);
+  void SelectSsrc_s(uint32_t ssrc);
 
   virtual Direction direction() const { return direction_; }
   virtual const std::string& trackid() const { return track_id_; }
