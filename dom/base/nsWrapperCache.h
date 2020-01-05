4be3635e87134edac1131c1c3e831b676cdc08cb
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 06:47:25 +0000

    Bug 851892 part 4.  Make css::Rule wrappercached.  r=heycam,peterv
    Note that this increases the size of css::Rule by three words, unfortunately.
    
    UltraBlame original commit: d6aa4c6192df0321f7d26da609cd5d28c4df9259

--- dom/base/nsWrapperCache.h
+++ dom/base/nsWrapperCache.h
@@ -19,10 +19,25 @@ namespace dom {
 class TabChildGlobal;
 class ProcessGlobal;
 } 
+namespace css {
+class ImportRule;
+class NameSpaceRule;
+class StyleRule;
+class MediaRule;
+class DocumentRule;
+} 
+class ServoStyleRule;
+class CSSSupportsRule;
 } 
 class SandboxPrivate;
 class nsInProcessTabChildGlobal;
 class nsWindowRoot;
+class nsCSSFontFaceRule;
+class nsCSSFontFeatureValuesRule;
+class nsCSSKeyframeRule;
+class nsCSSKeyframesRule;
+class nsCSSPageRule;
+class nsCSSCounterStyleRule;
 
 #define NS_WRAPPERCACHE_IID \
 { 0x6f3179a1, 0x36f7, 0x4a5c, \
@@ -272,11 +287,27 @@ protected:
   }
 
 private:
+  
+  
+  
   friend class mozilla::dom::TabChildGlobal;
   friend class mozilla::dom::ProcessGlobal;
   friend class SandboxPrivate;
   friend class nsInProcessTabChildGlobal;
   friend class nsWindowRoot;
+  friend class mozilla::css::ImportRule;
+  friend class mozilla::css::NameSpaceRule;
+  friend class mozilla::css::StyleRule;
+  friend class mozilla::css::MediaRule;
+  friend class mozilla::css::DocumentRule;
+  friend class mozilla::ServoStyleRule;
+  friend class mozilla::CSSSupportsRule;
+  friend class nsCSSFontFaceRule;
+  friend class nsCSSFontFeatureValuesRule;
+  friend class nsCSSKeyframeRule;
+  friend class nsCSSKeyframesRule;
+  friend class nsCSSPageRule;
+  friend class nsCSSCounterStyleRule;
   void SetIsNotDOMBinding()
   {
     MOZ_ASSERT(!mWrapper && !(GetWrapperFlags() & ~WRAPPER_IS_NOT_DOM_BINDING),
