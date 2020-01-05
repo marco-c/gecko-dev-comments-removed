673272b49c61742d7e6c471ca643bcbf4908490b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 09:02:07 +0000

    servo: Merge #1838 - Updates for HTMLCollection (from brunoabinader:htmlcollection-live); r=jdm
    Source-Repo: https://github.com/servo/servo
    Source-Revision: 2238d81b530d40c164df59569fb4d11914ad5c99
    
    UltraBlame original commit: 8eb833430171a698af4e72ea37c5be3b93ed113d

--- servo/src/components/script/dom/htmldatalistelement.rs
+++ servo/src/components/script/dom/htmldatalistelement.rs
@@ -42,6 +42,7 @@ impl HTMLDataListElement {
 
 impl HTMLDataListElement {
     pub fn Options(&self) -> JS<HTMLCollection> {
+        
         let doc = self.htmlelement.element.node.owner_doc();
         let doc = doc.get();
         HTMLCollection::new(&doc.window, ~[])
--- servo/src/components/script/dom/htmlmapelement.rs
+++ servo/src/components/script/dom/htmlmapelement.rs
@@ -51,6 +51,7 @@ impl HTMLMapElement {
     }
 
     pub fn Areas(&self) -> JS<HTMLCollection> {
+        
         let doc = self.htmlelement.element.node.owner_doc();
         let doc = doc.get();
         HTMLCollection::new(&doc.window, ~[])
