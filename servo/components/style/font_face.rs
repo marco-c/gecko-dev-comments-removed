b9ebc3a92fdf337e2227a01856a75a4e993cdc44
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 16:37:47 +0000

    servo: Merge #15552 - Update cssparser to 0.9 (from servo:cssparserup); r=nox
    <!-- Please describe your changes on the following line: -->
    
    ---
    <!-- Thank you for contributing to Servo! Please replace each `[ ]` by `[X]` when the step is complete, and replace `__` with appropriate data: -->
    - [x] `./mach build -d` does not report any errors
    - [x] `./mach test-tidy` does not report any errors
    - [ ] These changes fix #__ (github issue number if applicable).
    
    <!-- Either: -->
    - [ ] There are tests for these changes OR
    - [ ] These changes do not require tests because _____
    
    <!-- Pull requests that do not address these steps are welcome, but they will require additional verification as part of the review process. -->
    
    Source-Repo: https://github.com/servo/servo
    Source-Revision: 216a89f7766dd366c4afbeae42cf6e1fb4f67349
    
    UltraBlame original commit: 9f38984114fa95888bbbb510e15a31d86940db34

--- servo/components/style/font_face.rs
+++ servo/components/style/font_face.rs
@@ -12,6 +12,7 @@
 use computed_values::{font_style, font_weight, font_stretch};
 use computed_values::font_family::FamilyName;
 use cssparser::{AtRuleParser, DeclarationListParser, DeclarationParser, Parser};
+#[cfg(feature = "gecko")] use cssparser::UnicodeRange;
 use parser::{ParserContext, log_css_error, Parse};
 use std::fmt;
 use std::iter;
@@ -238,7 +239,7 @@ macro_rules! font_face_descriptors {
                 dest.write_str("@font-face {\n")?;
                 $(
                     dest.write_str(concat!("  ", $m_name, ": "))?;
-                    self.$m_ident.to_css(dest)?;
+                    ToCss::to_css(&self.$m_ident, dest)?;
                     dest.write_str(";\n")?;
                 )*
                 $(
@@ -247,7 +248,7 @@ macro_rules! font_face_descriptors {
                     
                     if self.$o_ident != $o_initial {
                         dest.write_str(concat!("  ", $o_name, ": "))?;
-                        self.$o_ident.to_css(dest)?;
+                        ToCss::to_css(&self.$o_ident, dest)?;
                         dest.write_str(";\n")?;
                     }
                 )*
@@ -298,8 +299,8 @@ font_face_descriptors! {
         "font-stretch" stretch: font_stretch::T = font_stretch::T::normal,
 
         /// The ranges of code points outside of which this font face should not be used.
-        "unicode-range" unicode_range: Vec<unicode_range::Range> = vec![
-            unicode_range::Range { start: 0, end: unicode_range::MAX }
+        "unicode-range" unicode_range: Vec<UnicodeRange> = vec![
+            UnicodeRange { start: 0, end: 0x10FFFF }
         ],
     ]
 }
@@ -315,52 +316,4 @@ font_face_descriptors! {
     ]
     optional descriptors = [
     ]
-}
-
-
-#[cfg(feature = "gecko")]
-pub mod unicode_range {
-    use cssparser::{Parser, Token};
-    use parser::{ParserContext, Parse};
-    use std::fmt;
-    use style_traits::{ToCss, OneOrMoreCommaSeparated};
-
-    
-    pub const MAX: u32 = ::std::char::MAX as u32;
-
-    
-    #[derive(Debug, PartialEq, Eq)]
-    pub struct Range {
-        
-        pub start: u32,
-
-        
-        pub end: u32,
-    }
-
-    impl OneOrMoreCommaSeparated for Range {}
-
-    impl Parse for Range {
-        fn parse(_context: &ParserContext, input: &mut Parser) -> Result<Self, ()> {
-            
-            
-            
-            match input.next() {
-                Ok(Token::UnicodeRange(start, end)) => {
-                    if end <= MAX && start <= end {
-                        Ok(Range { start: start, end: end })
-                    } else {
-                        Err(())
-                    }
-                }
-                _ => Err(())
-            }
-        }
-    }
-
-    impl ToCss for Range {
-        fn to_css<W>(&self, dest: &mut W) -> fmt::Result where W: fmt::Write {
-            Token::UnicodeRange(self.start, self.end).to_css(dest)
-        }
-    }
 }
