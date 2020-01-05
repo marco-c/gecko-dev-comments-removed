eda70c1609d858c3582c6b0ca9f501ecd438c761
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 12:49:59 +0000

    Merge m-c to graphics
    MozReview-Commit-ID: 6ocpYm7MBCV
    
    UltraBlame original commit: f778def55c45214a28cc397c63e866cd151b26b0

--- widget/PuppetWidget.cpp
+++ widget/PuppetWidget.cpp
@@ -205,6 +205,15 @@ PuppetWidget::Show(bool aState)
   }
 
   if (!wasVisible && mVisible) {
+    
+    
+    
+    
+    
+    
+    
+    
+    mPreviouslyAttachedWidgetListener = nullptr;
     Resize(mBounds.width, mBounds.height, false);
     Invalidate(mBounds);
   }
