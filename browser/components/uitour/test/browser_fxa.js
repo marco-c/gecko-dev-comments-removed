981d482552694372b508314b392885b33f2efe3a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 07:38:40 +0000

    Bug 1296767 part 10 - Remove FxA Migration code. r=markh
    MozReview-Commit-ID: DamrbblcMmZ
    
    UltraBlame original commit: 26c065f79c549002dd228d11b0dc0cdaf9c27ff1

--- browser/components/uitour/test/browser_fxa.js
+++ browser/components/uitour/test/browser_fxa.js
@@ -18,7 +18,7 @@ function test() {
 
 registerCleanupFunction(function*() {
   yield signOut();
-  gFxAccounts.updateAppMenuItem();
+  gFxAccounts.updateUI();
 });
 
 var tests = [
@@ -35,7 +35,7 @@ var tests = [
     yield setSignedInUser();
     let userData = yield fxAccounts.getSignedInUser();
     isnot(userData, null, "Logged in now");
-    gFxAccounts.updateAppMenuItem(); 
+    gFxAccounts.updateUI(); 
     yield showMenuPromise("appMenu");
     yield showHighlightPromise("accountStatus");
     let highlight = document.getElementById("UITourHighlightContainer");
