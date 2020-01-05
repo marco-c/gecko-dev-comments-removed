52555b9039af7c0e953a7d579e796860d395345c
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 04:08:30 +0000

    bug 1286934 - Switch to using sccache2. r=gps
    This patch does a few things:
    1) Change all the in-tree tooltool manifests to contain sccache2 instead of the existing Python sccache
    2) Change mozconfig.cache to point at sccache.
    3) Lightly tweak the --with-cccache configure option to support sccache, and detect whether we're using ccache or sccache and set an option appropriately.
    4) Add a MOZ_SCCACHE_VERBOSE_STATS option, and add a target in the top-level Makefile to make sccache spit out its stats at the end of the build. This is useful to see the cache hits/errors until we get something better.
    5) Add MOZ_USING_SCCACHE to the build telemetry. Not that I think it will be immediately useful, but for future use.
    
    MozReview-Commit-ID: 9lrdLwNj5Bm
    
    UltraBlame original commit: 683e59dc30940172227d80afa8b4e0d9a2cb1bb3

--- python/mozbuild/mozbuild/mach_commands.py
+++ python/mozbuild/mozbuild/mach_commands.py
@@ -509,7 +509,7 @@ class Build(MachCommandBase):
             
             telemetry_data['substs'] = {}
             try:
-                for key in ['MOZ_ARTIFACT_BUILDS', 'MOZ_USING_CCACHE']:
+                for key in ['MOZ_ARTIFACT_BUILDS', 'MOZ_USING_CCACHE', 'MOZ_USING_SCCACHE']:
                     value = self.substs.get(key, False)
                     telemetry_data['substs'][key] = value
             except BuildEnvironmentNotFoundException:
