274803dab578767a89b4ac9938b4785b64968daf
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 09:34:45 +0000

    servo: Merge #3947 - Embedding string map (from zmike:embedding-string_map); r=jdm
    implements a string map which is 100% identical to CEF
    
    r+ larsbergstrom  jdm
    
    Source-Repo: https://github.com/servo/servo
    Source-Revision: 99fc4ab634738136daa993443042a4cbf68c510c
    
    UltraBlame original commit: ff4de38a410826d95205f339415963e48391377d

--- servo/ports/cef/eutil.rs
+++ servo/ports/cef/eutil.rs
@@ -2,6 +2,18 @@
 
 
 
+use libc::c_int;
+use std::slice;
+use std::str;
+
 pub fn fptr_is_null(fptr: *const u8) -> bool {
     fptr.is_null()
+}
+
+pub fn slice_to_str(s: *const u8, l: uint, f: |&str| -> c_int) -> c_int {
+    unsafe {
+        slice::raw::buf_as_slice(s, l, |result| {
+             str::from_utf8(result).map(|s| f(s)).unwrap_or(0)
+        })
+    }
 }
