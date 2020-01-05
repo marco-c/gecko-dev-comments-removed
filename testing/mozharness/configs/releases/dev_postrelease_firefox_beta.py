4c46c7cc93d697cd2b8a0148aa0f506580af48c8
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 21:57:54 +0000

    Bug 1355018 - Make post-release steps use the unified repo r=rail
    MozReview-Commit-ID: GtfoY8RVE2n
    
    UltraBlame original commit: 2d53e3a676bb1617ac701b5470a91ec831bb921b

--- testing/mozharness/configs/releases/dev_postrelease_firefox_beta.py
+++ testing/mozharness/configs/releases/dev_postrelease_firefox_beta.py
@@ -8,6 +8,7 @@ config = {
         "branch": "default",
         "dest": "date",
         "vcs": "hg",
+        "clone_upstream_url": "https://hg.mozilla.org/mozilla-unified",
     },
     
     "push_dest": "ssh://hg.mozilla.org/projects/date",
