8fa8af6c7ade1a03e49231b2f5ae5b9bf57958cf
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 02:28:50 +0000

    Bug 1313555 - Update pdf.js to version 1.6.274. r=bdahl
    UltraBlame original commit: 4e6fc3d814dbf407d3153977b67c743fd8b420ba

--- browser/extensions/pdfjs/content/build/pdf.worker.js
+++ browser/extensions/pdfjs/content/build/pdf.worker.js
@@ -24,8 +24,8 @@
 }(this, function (exports) {
   
   'use strict';
-  var pdfjsVersion = '1.6.263';
-  var pdfjsBuild = '7e392c0';
+  var pdfjsVersion = '1.6.274';
+  var pdfjsBuild = '1c3fb17';
   var pdfjsFilePath = typeof document !== 'undefined' && document.currentScript ? document.currentScript.src : null;
   var pdfjsLibs = {};
   (function pdfjsWrapper() {
