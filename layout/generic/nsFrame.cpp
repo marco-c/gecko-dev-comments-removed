3880bf457e547975357cb3512c9c2aa4ecf637cd
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 03:01:51 +0000

    Bug 1314206 part 2 - [css-grid] Don't check for unconstrained inline size since it never occurs (other than by accident).  r=dholbert
    UltraBlame original commit: 5eed1210b69a03c364741fa7b35107703e1114c2

--- layout/generic/nsFrame.cpp
+++ layout/generic/nsFrame.cpp
@@ -4762,19 +4762,16 @@ nsFrame::ComputeSize(nsRenderingContext* aRenderingContext,
     if (inlineStyleCoord->IsCoordPercentCalcUnit()) {
       minISize = std::min(minISize, result.ISize(aWM));
     } else if (aFlags & eIClampMarginBoxMinSize) {
-      auto cbSize = aCBSize.ISize(aWM);
-      if (cbSize != NS_UNCONSTRAINEDSIZE) {
-        
-        
-        
-        
-        
-        auto maxMinISize = std::max(nscoord(0), cbSize -
-                                                aPadding.ISize(aWM) -
-                                                aBorder.ISize(aWM) -
-                                                aMargin.ISize(aWM));
-        minISize = std::min(minISize, maxMinISize);
-      }
+      
+      
+      
+      
+      
+      auto maxMinISize = std::max(nscoord(0), aCBSize.ISize(aWM) -
+                                              aPadding.ISize(aWM) -
+                                              aBorder.ISize(aWM) -
+                                              aMargin.ISize(aWM));
+      minISize = std::min(minISize, maxMinISize);
     }
   } else {
     
