c22e7a2ce9526e9e3bc8118b4b1a1354d31b00f7
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 04:47:37 +0000

    Bug 1321875: Fix relative lifetimes of lpAttributeList and handlesToInherit in LaunchApp. r=aklotz
    CreateThreadAttributeList warns:
    
    // Note that the pointer to the HANDLE array ends up embedded in the result of
    // this function and must stay alive until FreeThreadAttributeList is called,
    // hence it is passed in so the owner is the caller of this function.
    
    but the caller was passing a |handlesToInherit| that was declared inside a block scope that ends before we're finished using lpAttributeList.
    
    This happened to work on MSVC but leads to badness under clang-cl.
    
    + Bonus fix for a sometimes-uninitialized warning in CreateThreadAttributeList.
    
    MozReview-Commit-ID: 6uu3ICjfj5k
    
    UltraBlame original commit: 72cd631fdd7957fccec0beea95f6b89df27fe65a

--- ipc/chromium/src/base/process_util_win.cc
+++ ipc/chromium/src/base/process_util_win.cc
@@ -239,7 +239,7 @@ LPPROC_THREAD_ATTRIBUTE_LIST CreateThreadAttributeList(HANDLE *handlesToInherit,
     return NULL;
 
   SIZE_T threadAttrSize;
-  LPPROC_THREAD_ATTRIBUTE_LIST lpAttributeList;
+  LPPROC_THREAD_ATTRIBUTE_LIST lpAttributeList = NULL;
 
   if (!(*InitializeProcThreadAttributeListPtr)(NULL, 1, 0, &threadAttrSize) &&
       GetLastError() != ERROR_INSUFFICIENT_BUFFER)
@@ -296,13 +296,16 @@ bool LaunchApp(const std::wstring& cmdline,
   startup_info.dwFlags = STARTF_USESHOWWINDOW;
   startup_info.wShowWindow = start_hidden ? SW_HIDE : SW_SHOW;
 
+  
+  
   LPPROC_THREAD_ATTRIBUTE_LIST lpAttributeList = NULL;
+  HANDLE handlesToInherit[2];
+  int handleCount = 0;
+
   
   if (mozilla::IsVistaOrLater()) {
     
     
-    HANDLE handlesToInherit[2];
-    int handleCount = 0;
     HANDLE stdOut = ::GetStdHandle(STD_OUTPUT_HANDLE);
     HANDLE stdErr = ::GetStdHandle(STD_ERROR_HANDLE);
 
