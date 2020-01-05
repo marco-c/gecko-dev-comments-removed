41a27bd51cc11b1e9575b58c3428bab4d8610e79
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 12:19:16 +0000

    Bug 1336124 - Remove unused B2G code in session teardown; r=maja_zf
    MozReview-Commit-ID: CHmdB7BvBT8
    
    UltraBlame original commit: c93a410adde07a4393778b1cbec7257bed7ef39e

--- testing/marionette/driver.js
+++ testing/marionette/driver.js
@@ -2198,15 +2198,8 @@ GeckoDriver.prototype.closeChromeWindow = function (cmd, resp) {
 
 GeckoDriver.prototype.sessionTearDown = function (cmd, resp) {
   if (this.curBrowser !== null) {
-    if (this.appName == "B2G") {
-      globalMessageManager.broadcastAsyncMessage(
-          "Marionette:sleepSession" + this.curBrowser.mainContentId, {});
-      this.curBrowser.knownFrames.splice(
-          this.curBrowser.knownFrames.indexOf(this.curBrowser.mainContentId), 1);
-    } else {
-      
-      Preferences.set(CONTENT_LISTENER_PREF, false);
-    }
+    
+    Preferences.set(CONTENT_LISTENER_PREF, false);
 
     
     for (let win in this.browsers) {
@@ -2220,7 +2213,7 @@ GeckoDriver.prototype.sessionTearDown = function (cmd, resp) {
     let winEn = Services.wm.getEnumerator(null);
     while (winEn.hasMoreElements()) {
       let win = winEn.getNext();
-      if (win.messageManager){
+      if (win.messageManager) {
         win.messageManager.removeDelayedFrameScript(FRAME_SCRIPT);
       } else {
         logger.error(
