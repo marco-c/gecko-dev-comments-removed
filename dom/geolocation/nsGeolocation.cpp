f1457751c670e78a019c5debee8d1ebe28978e22
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 17:55:54 +0000

    Backed out changeset 8b1c50cdb50c (bug 1072859) for failing devtools/client/responsive.html/test/browser/browser_permission_doorhanger.js. r=backout a=merge
    MozReview-Commit-ID: LaknhYl9fEw
    
    UltraBlame original commit: 193989276571a5bd19bc669c6ca42828abe7b4f1

--- dom/geolocation/nsGeolocation.cpp
+++ dom/geolocation/nsGeolocation.cpp
@@ -1172,7 +1172,9 @@ Geolocation::IsAlreadyCleared(nsGeolocationRequest* aRequest)
 bool
 Geolocation::ShouldBlockInsecureRequests() const
 {
-  if (Preferences::GetBool(PREF_GEO_SECURITY_ALLOWINSECURE, false)) {
+  
+  
+  if (Preferences::GetBool(PREF_GEO_SECURITY_ALLOWINSECURE, true)) {
     return false;
   }
 
@@ -1186,7 +1188,7 @@ Geolocation::ShouldBlockInsecureRequests() const
     return false;
   }
 
-  if (!nsGlobalWindow::Cast(win)->IsSecureContextIfOpenerIgnored()) {
+  if (!nsGlobalWindow::Cast(win)->IsSecureContext()) {
     nsContentUtils::ReportToConsole(nsIScriptError::errorFlag,
                                     NS_LITERAL_CSTRING("DOM"), doc,
                                     nsContentUtils::eDOM_PROPERTIES,
