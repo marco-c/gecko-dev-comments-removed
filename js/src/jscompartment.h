89f2688da7a4c2bd9c333a86d805b45e301a46d4
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 06:11:15 +0000

    Bug 1312001 - Scramble hash codes securely, to avoid leaking bits of object and symbol addresses.
    MozReview-Commit-ID: yR1cIjrlPP
    
    UltraBlame original commit: c484c1e7eeb61f4abd6d9e2352eacd52b1a47cbf

--- js/src/jscompartment.h
+++ js/src/jscompartment.h
@@ -701,6 +701,12 @@ struct JSCompartment
     
     void ensureRandomNumberGenerator();
 
+  private:
+    mozilla::non_crypto::XorShift128PlusRNG randomKeyGenerator_;
+
+  public:
+    mozilla::HashCodeScrambler randomHashCodeScrambler();
+
     static size_t offsetOfRegExps() {
         return offsetof(JSCompartment, regExps);
     }
