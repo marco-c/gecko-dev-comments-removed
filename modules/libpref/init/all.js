975de0fadbf2e8cf09abf89d4c5fba5d043fadfd
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 20:29:53 +0000

    Bug 1321865 - Enable IntersectionObserver and tests. r=smaug, r=jet
    UltraBlame original commit: 718fb66559f71d1838b3bc6b187e050d44e3f566

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -5633,7 +5633,7 @@ pref("prompts.authentication_dialog_abuse_limit", 3);
 pref("browser.storageManager.enabled", false);
 pref("browser.storageManager.pressureNotification.minIntervalMS", 1200000);
 pref("browser.storageManager.pressureNotification.usageThresholdGB", 5);
-pref("dom.IntersectionObserver.enabled", false);
+pref("dom.IntersectionObserver.enabled", true);
 
 
 pref("dom.moduleScripts.enabled", false);
