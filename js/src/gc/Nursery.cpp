fc55adfc337c21566d4a488efcf4380d3dc9466b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 21:15:30 +0000

    Bug 1341026 - Always initialise nursery data structures even if size is under one chunk r=jandem
    UltraBlame original commit: d61868b3d1333cb0da1a96ba645c92a2bd1a0231

--- js/src/gc/Nursery.cpp
+++ js/src/gc/Nursery.cpp
@@ -134,19 +134,19 @@ js::Nursery::Nursery(JSRuntime* rt)
 bool
 js::Nursery::init(uint32_t maxNurseryBytes, AutoLockGC& lock)
 {
-    
-    maxNurseryChunks_ = maxNurseryBytes >> ChunkShift;
-
-    
-    if (maxNurseryChunks_ == 0)
-        return true;
-
     if (!mallocedBuffers.init())
         return false;
 
     freeMallocedBuffersTask = js_new<FreeMallocedBuffersTask>(runtime()->defaultFreeOp());
     if (!freeMallocedBuffersTask || !freeMallocedBuffersTask->init())
         return false;
+
+    
+    maxNurseryChunks_ = maxNurseryBytes >> ChunkShift;
+
+    
+    if (maxNurseryChunks_ == 0)
+        return true;
 
     AutoMaybeStartBackgroundAllocation maybeBgAlloc;
     updateNumChunksLocked(1, maybeBgAlloc, lock);
