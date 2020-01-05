95d01e81f8ce1c266245dc33ddb8dd9fee881af5
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 16:34:09 +0000

    Bug 1338172 part E - make all the PPluginWidget stuff Windows-only. Rip out the GTK-specific native widget support from widget/gtk/nsWindow and elsewhere, r=jimm
    MozReview-Commit-ID: J6E8sYcyX4U
    
    UltraBlame original commit: 9ee6c6f4e59c7967108a727a80e46d686b983a0b

--- dom/ipc/TabChild.h
+++ dom/ipc/TabChild.h
@@ -62,10 +62,6 @@ namespace widget {
 struct AutoCacheNativeKeyCommands;
 } 
 
-namespace plugins {
-class PluginWidgetChild;
-} 
-
 namespace dom {
 
 class TabChild;
@@ -609,7 +605,9 @@ public:
 
   bool DeallocPPluginWidgetChild(PPluginWidgetChild* aActor) override;
 
+#ifdef XP_WIN
   nsresult CreatePluginWidget(nsIWidget* aParent, nsIWidget** aOut);
+#endif
 
   LayoutDeviceIntPoint GetClientOffset() const { return mClientOffset; }
   LayoutDeviceIntPoint GetChromeDisplacement() const { return mChromeDisp; };
