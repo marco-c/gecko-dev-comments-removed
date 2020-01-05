54c09f7b0561bab07b915ddc9649f3f99ec06017
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 16:37:23 +0000

    Bug 1338534 - [mozlog] Indicate unexpected pass with the correct status. r=ahal
    Unexpected passes can cause failures in pytest when the expected failures are set to strict. When we're logging these we want to indicate that the correct outcome.
    
    MozReview-Commit-ID: 8VoR6CTrynY
    
    UltraBlame original commit: b854735013bbbd22e800e4a6c4031475be7ff110

--- testing/mozbase/mozlog/mozlog/pytest_mozlog/plugin.py
+++ testing/mozbase/mozlog/mozlog/pytest_mozlog/plugin.py
@@ -82,6 +82,10 @@ class MozLog(object):
             if isinstance(report.longrepr, basestring):
                 
                 message = stack = report.longrepr
+                if report.longrepr.startswith('[XPASS(strict)]'):
+                    
+                    
+                    expected, status = ('FAIL', 'PASS')
             else:
                 try:
                     
