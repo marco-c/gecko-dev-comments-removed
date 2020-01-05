47e1c89712c5e53a5f970e6df31a13e5577f7a37
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 03:21:28 +0000

    Bug 1315798 - Remove FMRADIO support from HAL. r=mds
    MozReview-Commit-ID: E6KGgMd8Cfg
    
    UltraBlame original commit: 6f0ab55c4b46692e1888872a299ed764823fbf6e

--- dom/system/gonk/AudioManager.cpp
+++ dom/system/gonk/AudioManager.cpp
@@ -889,35 +889,6 @@ AudioManager::GetForceForUse(int32_t aUsage, int32_t* aForce) {
 }
 
 NS_IMETHODIMP
-AudioManager::GetFmRadioAudioEnabled(bool *aFmRadioAudioEnabled)
-{
-  *aFmRadioAudioEnabled = IsFmOutConnected();
-  return NS_OK;
-}
-
-NS_IMETHODIMP
-AudioManager::SetFmRadioAudioEnabled(bool aFmRadioAudioEnabled)
-{
-  UpdateDeviceConnectionState(aFmRadioAudioEnabled,
-                              AUDIO_DEVICE_OUT_FM,
-                              NS_LITERAL_CSTRING(""));
-  
-  
-#if ANDROID_VERSION < 19
-  
-  if (aFmRadioAudioEnabled) {
-    uint32_t volIndex = mStreamStates[AUDIO_STREAM_MUSIC]->GetVolumeIndex();
-    nsresult rv = mStreamStates[AUDIO_STREAM_FM]->
-      SetVolumeIndex(volIndex, AUDIO_DEVICE_OUT_FM);
-    if (NS_WARN_IF(NS_FAILED(rv))) {
-      return rv;
-    }
-  }
-#endif
-  return NS_OK;
-}
-
-NS_IMETHODIMP
 AudioManager::SetAudioChannelVolume(uint32_t aChannel, uint32_t aIndex)
 {
   if (aChannel >= NUMBER_OF_AUDIO_CHANNELS) {
