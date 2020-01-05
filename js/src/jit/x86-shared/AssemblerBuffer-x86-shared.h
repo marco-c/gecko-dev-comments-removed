83653425e63907f6041e45ab0007f9060d119703
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 06:02:03 +0000

    Bug 1326302 - Part 3: Guard against reentrancy in PageProtectingVector. r=jandem
    UltraBlame original commit: 3d875cf8e44d3b63c6cd8632b1570b77734e0e1b

--- js/src/jit/x86-shared/AssemblerBuffer-x86-shared.h
+++ js/src/jit/x86-shared/AssemblerBuffer-x86-shared.h
@@ -182,6 +182,7 @@ namespace jit {
 #ifndef RELEASE_OR_BETA
         PageProtectingVector<unsigned char, 256, SystemAllocPolicy,
                               false,  false,
+                              true,
                               32 * 1024> m_buffer;
 #else
         mozilla::Vector<unsigned char, 256, SystemAllocPolicy> m_buffer;
