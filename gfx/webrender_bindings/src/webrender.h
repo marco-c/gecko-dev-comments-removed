a276a2ea6cf35f2f0000376f6f08e4484ac5d74c
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 12:47:25 +0000

    Bug 1318761 - Properly create nested stacking contexts in bindings.rs. r=kats?
    UltraBlame original commit: e55d50d1193279fc0e8c76194424f6efe99f8b74

--- gfx/webrender_bindings/src/webrender.h
+++ gfx/webrender_bindings/src/webrender.h
@@ -92,7 +92,7 @@ WR_FUNC;
 
 
 WR_INLINE void
-wr_pop_dl_builder(wrwindowstate* wrWindow, wrstate *wrState, WRRect bounds,
+wr_pop_dl_builder(wrstate *wrState, WRRect bounds,
                   WRRect overflow, const float* matrix, uint64_t scrollId)
 WR_FUNC;
 
