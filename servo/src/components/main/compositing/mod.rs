ff1369a6704b016a52cbc920b151ee1bc2b70fbe
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 08:45:07 +0000

    servo: Merge #638 - constellation blocks on compositor before sending permission to paint to (from tikue:master); r=metajack
    ... renderers
    
    Source-Repo: https://github.com/servo/servo
    Source-Revision: 0acb313c55bd57faa632115a0e3d92edb3bccc71
    
    UltraBlame original commit: aeead81e2a0520a16e9000b40aca4940b5c82627

--- servo/src/components/main/compositing/mod.rs
+++ servo/src/components/main/compositing/mod.rs
@@ -141,7 +141,7 @@ pub enum Msg {
     
     ChangeRenderState(RenderState),
     
-    SetIds(SendableFrameTree),
+    SetIds(SendableFrameTree, Chan<()>),
 }
 
 
@@ -345,8 +345,9 @@ impl CompositorTask {
                     ChangeReadyState(ready_state) => window.set_ready_state(ready_state),
                     ChangeRenderState(render_state) => window.set_render_state(render_state),
 
-                    SetIds(frame_tree) => {
+                    SetIds(frame_tree, response_chan) => {
                         pipeline = Some(frame_tree.pipeline);
+                        response_chan.send(());
                     }
 
                     GetSize(chan) => {
