0865198b7ff50a8d8cc70f2f32d77ba369f499b9
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 22:07:26 +0000

    Bug 1355585 - Streamline the format of "handlers.json", align the implementation, and reorganize tests. r=mak
    This patch significantly improves the test coverage for both the JSON and RDF back-ends. There is a clearer separation between tests using predefined data and tests for the injection of default handlers. The predefined data includes more significant property combinations, and the JSON is now formatted. Helper functions are renamed for clarity.
    
    Functions like "exists" that have different paths for MIME types and protocols are now tested with both, while behaviors that have a single path are now only tested with MIME types for efficiency.
    
    The file format is redesigned to be more compact, and all the data is normalized when saving instead of when loading. Duplicates are now handled correctly when saving.
    
    MozReview-Commit-ID: JI4I1M0N3lq
    
    UltraBlame original commit: bcf5a28b1873a470c8626aeabae23ff69a191146

--- uriloader/exthandler/tests/unit/test_handlerService_rdf.js
+++ uriloader/exthandler/tests/unit/test_handlerService_rdf.js
@@ -5,33 +5,44 @@
 
 
 
-
-
 XPCOMUtils.defineLazyServiceGetter(this, "gHandlerService",
                                    "@mozilla.org/uriloader/handler-service;1",
                                    "nsIHandlerService");
 
-var scriptFile = do_get_file("common_test_handlerService.js");
-Services.scriptloader.loadSubScript(NetUtil.newURI(scriptFile).spec);
 
-var prepareImportDB = Task.async(function* () {
-  yield reloadData();
+
+
+
+let unloadHandlerStore = Task.async(function* () {
+  
+  
+  
+  gHandlerService;
+
+  let promise = TestUtils.topicObserved("handlersvc-rdf-replace-complete");
+  Services.obs.notifyObservers(null, "handlersvc-rdf-replace");
+  yield promise;
+});
+
+
+
+
+let deleteHandlerStore = Task.async(function* () {
+  yield unloadHandlerStore();
+
+  yield OS.File.remove(rdfFile.path, { ignoreAbsent: true });
+});
+
+
+
+
+let copyTestDataToHandlerStore = Task.async(function* () {
+  yield unloadHandlerStore();
 
   let fileName = AppConstants.platform == "android" ? "mimeTypes-android.rdf"
                                                     : "mimeTypes.rdf";
   yield OS.File.copy(do_get_file(fileName).path, rdfFile.path);
 });
 
-var removeImportDB = Task.async(function* () {
-  yield reloadData();
-
-  yield OS.File.remove(rdfFile.path, { ignoreAbsent: true });
-});
-
-var reloadData = Task.async(function* () {
-  
-  let svc = gHandlerService;
-  let promise = TestUtils.topicObserved("handlersvc-rdf-replace-complete");
-  Services.obs.notifyObservers(null, "handlersvc-rdf-replace");
-  yield promise;
-});
+var scriptFile = do_get_file("common_test_handlerService.js");
+Services.scriptloader.loadSubScript(NetUtil.newURI(scriptFile).spec);
