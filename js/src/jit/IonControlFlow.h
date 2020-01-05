003d3e1cb2d5a0102bf9fbceeef46d3b52b71ac4
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 06:42:35 +0000

    Bug 1323854 - IonMonkey: Part2: Keep the same semantics as bytecode on CFGCompare, r=nbp
    UltraBlame original commit: 4dbe893355f7096535a833eeb314267f4258933a

--- js/src/jit/IonControlFlow.h
+++ js/src/jit/IonControlFlow.h
@@ -317,9 +317,18 @@ class CFGTableSwitch : public CFGControlInstruction
 
 
 
+
+
+
+
 class CFGCompare : public CFGAryControlInstruction<2>
 {
-    CFGCompare(CFGBlock* succ1, CFGBlock* succ2)
+    const size_t truePopAmount_;
+    const size_t falsePopAmount_;
+
+    CFGCompare(CFGBlock* succ1, size_t truePopAmount, CFGBlock* succ2, size_t falsePopAmount)
+      : truePopAmount_(truePopAmount),
+        falsePopAmount_(falsePopAmount)
     {
         replaceSuccessor(0, succ1);
         replaceSuccessor(1, succ2);
@@ -327,13 +336,42 @@ class CFGCompare : public CFGAryControlInstruction<2>
 
   public:
     CFG_CONTROL_HEADER(Compare);
-    TRIVIAL_CFG_NEW_WRAPPERS
+
+    static CFGCompare* NewFalseBranchIsDefault(TempAllocator& alloc, CFGBlock* case_,
+                                               CFGBlock* default_)
+    {
+        
+        
+        return new(alloc) CFGCompare(case_, 2, default_, 2);
+    }
+
+    static CFGCompare* NewFalseBranchIsNextCompare(TempAllocator& alloc, CFGBlock* case_,
+                                                   CFGBlock* nextCompare)
+    {
+        
+        
+        
+        return new(alloc) CFGCompare(case_, 2, nextCompare, 1);
+    }
+
+    static CFGCompare* CopyWithNewTargets(TempAllocator& alloc, CFGCompare* old,
+                                          CFGBlock* succ1, CFGBlock* succ2)
+    {
+        return new(alloc) CFGCompare(succ1, old->truePopAmount(), succ2, old->falsePopAmount());
+    }
+
 
     CFGBlock* trueBranch() const {
         return getSuccessor(0);
     }
     CFGBlock* falseBranch() const {
         return getSuccessor(1);
+    }
+    size_t truePopAmount() const {
+        return truePopAmount_;
+    }
+    size_t falsePopAmount() const {
+        return falsePopAmount_;
     }
 };
 
