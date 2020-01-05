3b9906b7ce07ac84f9fe5ca1f589b51268fc6a23
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 21:11:28 +0000

    Bug 1354731 - Quietly fail mknod of character devices in content sandbox. r=gcp
    MozReview-Commit-ID: 2aehI5V7tQp
    
    UltraBlame original commit: 9135513229b9f24fdb120d16ead7b019ad3fbe5b

--- security/sandbox/linux/SandboxFilter.cpp
+++ security/sandbox/linux/SandboxFilter.cpp
@@ -655,8 +655,15 @@ public:
     CASES_FOR_fchown:
     case __NR_fchmod:
     case __NR_flock:
-#endif
       return Allow();
+
+      
+    case __NR_mknod: {
+      Arg<mode_t> mode(1);
+      return If((mode & S_IFMT) == S_IFCHR, Error(EPERM))
+        .Else(InvalidSyscall());
+    }
+#endif
 
     case __NR_readlinkat:
 #ifdef DESKTOP
