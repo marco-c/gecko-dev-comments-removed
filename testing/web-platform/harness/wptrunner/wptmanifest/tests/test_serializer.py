d84305aea0aa4a5e9dc532bc5a5f4fe0c82ef410
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 04:01:12 +0000

    Bug 1318666 - Update to latest wptrunner, a=testonly
    MozReview-Commit-ID: 9HnoUTtAf4l
    
    UltraBlame original commit: 1ee751ec56eba735291b9ec0e3d451db2cca41e1

--- testing/web-platform/harness/wptrunner/wptmanifest/tests/test_serializer.py
+++ testing/web-platform/harness/wptrunner/wptmanifest/tests/test_serializer.py
@@ -2,9 +2,12 @@
 
 
 
+import sys
 import unittest
 
 from cStringIO import StringIO
+
+import pytest
 
 from .. import parser, serializer
 
@@ -196,6 +199,7 @@ class TokenizerTest(unittest.TestCase):
                      r"""key: "#"
 """)
 
+    @pytest.mark.xfail(sys.maxunicode == 0xFFFF, reason="narrow unicode")
     def test_escape_9(self):
         self.compare(r"""key: \U10FFFFabc""",
                      u"""key: \U0010FFFFabc
