6c9c3d3e80357854248bd2e825cf7f0a9eb5cb6f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 23:33:17 +0000

    Backed out changeset d0669259020b (bug 1362937) for causing merge conflicts when merging to m-c
    UltraBlame original commit: 9b55ab643feb8e20273adea6be4cdb2ba0223008

--- browser/extensions/formautofill/test/unit/test_profileStorage.js
+++ browser/extensions/formautofill/test/unit/test_profileStorage.js
@@ -143,11 +143,6 @@ add_task(function* test_getByFilter() {
   filter = {info: {fieldName: "street-address"}, searchString: ".*"};
   profiles = profileStorage.getByFilter(filter);
   do_check_eq(profiles.length, 0);
-
-  
-  filter = {info: {fieldName: "tel"}, searchString: "1"};
-  profiles = profileStorage.getByFilter(filter);
-  do_check_eq(profiles.length, 0);
 });
 
 add_task(function* test_add() {
