00c3f88fd5e42ff80b092f473166196dc031c19c
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 02:22:02 +0000

    Bug 1210795 - Part 2: Changes Infinity expression. r=pbro
    MozReview-Commit-ID: Bnoy5k388B8
    
    UltraBlame original commit: e3f96551347dddaf90cb22c3e886e3de04adeaa5

--- devtools/client/animationinspector/components/animation-time-block.js
+++ devtools/client/animationinspector/components/animation-time-block.js
@@ -24,11 +24,10 @@ const DURATION_RESOLUTION = 4;
 
 const MIN_PROGRESS_THRESHOLD = 0.1;
 
+
+const MAX_INFINITE_ANIMATIONS_ITERATIONS = 10;
+
 const SVG_NS = "http://www.w3.org/2000/svg";
-
-const FADE_MASK_ID = "animationinspector-fade-mask";
-
-const FADE_GRADIENT_ID = "animationinspector-fade-gradient";
 
 
 
@@ -76,13 +75,36 @@ AnimationTimeBlock.prototype = {
     let backgroundIterations = TimeScale.getIterationsBackgroundData(animation);
 
     
-    const totalDuration = TimeScale.getDuration() * state.playbackRate;
+    const summaryEl = createNode({
+      parent: this.containerEl,
+      namespace: "http://www.w3.org/2000/svg",
+      nodeType: "svg",
+      attributes: {
+        "class": "summary",
+        "preserveAspectRatio": "none",
+        "style": `left: ${ x - (state.delay > 0 ? delayW : 0) }%`
+      }
+    });
+
+    
+    const totalDisplayedDuration = state.playbackRate * TimeScale.getDuration();
+
+    
+    const strokeHeightForViewBox = 0.5 / this.containerEl.clientHeight;
+
+    
+    summaryEl.setAttribute("viewBox",
+                           `${ state.delay < 0 ? state.delay : 0 }
+                            -${ 1 + strokeHeightForViewBox }
+                            ${ totalDisplayedDuration }
+                            ${ 1 + strokeHeightForViewBox * 2 }`);
 
     
     const segmentHelperFn = getSegmentHelper(state, this.win);
 
     
-    const minSegmentDuration = totalDuration / this.containerEl.clientWidth;
+    const minSegmentDuration =
+      totalDisplayedDuration / this.containerEl.clientWidth;
     
     let minProgressThreshold = MIN_PROGRESS_THRESHOLD;
     
@@ -93,69 +115,80 @@ AnimationTimeBlock.prototype = {
     }
 
     
-    const strokeHeightForViewBox = 0.5 / this.containerEl.clientHeight;
-
-    
-    const summaryEl = createNode({
-      parent: this.containerEl,
-      namespace: SVG_NS,
-      nodeType: "svg",
-      attributes: {
-        "class": "summary",
-        "viewBox": `${ state.delay < 0 ? state.delay : 0 }
-                    -${ 1 + strokeHeightForViewBox }
-                    ${ totalDuration }
-                    ${ 1 + strokeHeightForViewBox * 2 }`,
-        "preserveAspectRatio": "none",
-        "style": `left: ${ x - (state.delay > 0 ? delayW : 0) }%;`,
-      }
-    });
-
-    
-    const iterationCount = state.iterationCount ? state.iterationCount : 1;
-
-    
-    if (state.fill === "both" || state.fill === "forwards") {
-      renderForwardsFill(summaryEl, state, iterationCount,
-                         totalDuration, segmentHelperFn);
-    }
+    let mainIterationStartTime = 0;
+    let iterationStart = state.iterationStart;
+    let iterationCount = state.iterationCount ? state.iterationCount : Infinity;
 
     
     if (state.delay > 0) {
       renderDelay(summaryEl, state, segmentHelperFn);
+      mainIterationStartTime = state.delay;
+    } else {
+      const negativeDelayCount = -state.delay / state.duration;
+      
+      iterationStart += negativeDelayCount;
+      
+      if (iterationCount !== Infinity) {
+        iterationCount -= negativeDelayCount;
+      }
     }
 
+    
+    
     
     
     const firstSectionCount =
-      state.iterationStart % 1 === 0
-      ? 0 : Math.min(iterationCount, 1) - state.iterationStart % 1;
+      iterationStart % 1 === 0
+      ? 0 : Math.min(iterationCount, 1) - iterationStart % 1;
     if (firstSectionCount) {
-      renderFirstIteration(summaryEl, state, firstSectionCount,
-                           minSegmentDuration, minProgressThreshold,
-                           segmentHelperFn);
-    }
-
-    
-    const middleSectionCount =
-      Math.floor(state.iterationCount - firstSectionCount);
-    renderMiddleIterations(summaryEl, state, firstSectionCount,
-                           middleSectionCount, minSegmentDuration,
+      renderFirstIteration(summaryEl, state, mainIterationStartTime,
+                           firstSectionCount, minSegmentDuration,
                            minProgressThreshold, segmentHelperFn);
-
-    
-    const lastSectionCount =
-      iterationCount - middleSectionCount - firstSectionCount;
-    if (lastSectionCount) {
-      renderLastIteration(summaryEl, state, firstSectionCount,
-                          middleSectionCount, lastSectionCount,
-                          minSegmentDuration, minProgressThreshold,
-                          segmentHelperFn);
     }
 
-    
-    if (state.endDelay > 0) {
-      renderEndDelay(summaryEl, state, iterationCount, segmentHelperFn);
+    if (iterationCount === Infinity) {
+      
+      
+      renderInfinity(summaryEl, state, mainIterationStartTime,
+                     firstSectionCount, totalDisplayedDuration,
+                     minSegmentDuration, minProgressThreshold, segmentHelperFn);
+    } else {
+      
+
+      
+      if (state.fill === "both" || state.fill === "forwards") {
+        renderForwardsFill(summaryEl, state, mainIterationStartTime,
+                           iterationCount, totalDisplayedDuration,
+                           segmentHelperFn);
+      }
+
+      
+      
+      
+      const middleSectionCount =
+        Math.floor(iterationCount - firstSectionCount);
+      renderMiddleIterations(summaryEl, state, mainIterationStartTime,
+                             firstSectionCount, middleSectionCount,
+                             minSegmentDuration, minProgressThreshold,
+                             segmentHelperFn);
+
+      
+      
+      
+      const lastSectionCount =
+        iterationCount - middleSectionCount - firstSectionCount;
+      if (lastSectionCount) {
+        renderLastIteration(summaryEl, state, mainIterationStartTime,
+                            firstSectionCount, middleSectionCount,
+                            lastSectionCount, minSegmentDuration,
+                            minProgressThreshold, segmentHelperFn);
+      }
+
+      
+      if (state.endDelay > 0) {
+        renderEndDelay(summaryEl, state,
+                       mainIterationStartTime, iterationCount, segmentHelperFn);
+      }
     }
 
     
@@ -325,10 +358,11 @@ function renderDelay(parentEl, state, getSegment) {
 
 
 
-function renderFirstIteration(parentEl, state, firstSectionCount,
-                              minSegmentDuration, minProgressThreshold,
-                              getSegment) {
-  const startTime = state.delay;
+
+function renderFirstIteration(parentEl, state, mainIterationStartTime,
+                              firstSectionCount, minSegmentDuration,
+                              minProgressThreshold, getSegment) {
+  const startTime = mainIterationStartTime;
   const endTime = startTime + firstSectionCount * state.duration;
   const segments =
     createPathSegments(startTime, endTime, minSegmentDuration,
@@ -346,10 +380,12 @@ function renderFirstIteration(parentEl, state, firstSectionCount,
 
 
 
-function renderMiddleIterations(parentEl, state, firstSectionCount,
-                                middleSectionCount, minSegmentDuration,
-                                minProgressThreshold, getSegment) {
-  const offset = state.delay + firstSectionCount * state.duration;
+
+function renderMiddleIterations(parentEl, state, mainIterationStartTime,
+                                firstSectionCount, middleSectionCount,
+                                minSegmentDuration, minProgressThreshold,
+                                getSegment) {
+  const offset = mainIterationStartTime + firstSectionCount * state.duration;
   for (let i = 0; i < middleSectionCount; i++) {
     
     const startTime = offset + i * state.duration;
@@ -372,12 +408,13 @@ function renderMiddleIterations(parentEl, state, firstSectionCount,
 
 
 
-function renderLastIteration(parentEl, state, firstSectionCount,
-                             middleSectionCount, lastSectionCount,
-                             minSegmentDuration, minProgressThreshold,
-                             getSegment) {
-  const startTime = state.delay + firstSectionCount * state.duration
-                    + middleSectionCount * state.duration;
+
+function renderLastIteration(parentEl, state, mainIterationStartTime,
+                             firstSectionCount, middleSectionCount,
+                             lastSectionCount, minSegmentDuration,
+                             minProgressThreshold, getSegment) {
+  const startTime = mainIterationStartTime +
+                      (firstSectionCount + middleSectionCount) * state.duration;
   const endTime = startTime + lastSectionCount * state.duration;
   const segments =
     createPathSegments(startTime, endTime, minSegmentDuration,
@@ -392,8 +429,65 @@ function renderLastIteration(parentEl, state, firstSectionCount,
 
 
 
-function renderEndDelay(parentEl, state, iterationCount, getSegment) {
-  const startTime = state.delay + iterationCount * state.duration;
+
+
+
+
+function renderInfinity(parentEl, state, mainIterationStartTime,
+                        firstSectionCount, totalDuration, minSegmentDuration,
+                        minProgressThreshold, getSegment) {
+  
+  
+  let uncappedInfinityIterationCount =
+    (totalDuration - firstSectionCount * state.duration) / state.duration;
+  
+  
+  uncappedInfinityIterationCount =
+    parseFloat(uncappedInfinityIterationCount.toPrecision(6));
+  const infinityIterationCount =
+    Math.min(MAX_INFINITE_ANIMATIONS_ITERATIONS,
+             Math.ceil(uncappedInfinityIterationCount));
+
+  
+  const firstStartTime =
+    mainIterationStartTime + firstSectionCount * state.duration;
+  const firstEndTime = firstStartTime + state.duration;
+  const firstSegments =
+    createPathSegments(firstStartTime, firstEndTime, minSegmentDuration,
+                       minProgressThreshold, getSegment);
+  appendPathElement(parentEl, firstSegments, "iteration-path infinity");
+
+  
+  const isAlternate = state.direction.match(/alternate/);
+  for (let i = 1; i < infinityIterationCount; i++) {
+    const startTime = firstStartTime + i * state.duration;
+    let segments;
+    if (isAlternate && i % 2) {
+      
+      segments = firstSegments.map(segment => {
+        return { x: firstEndTime - segment.x + startTime, y: segment.y };
+      });
+    } else {
+      
+      segments = firstSegments.map(segment => {
+        return { x: segment.x - firstStartTime + startTime, y: segment.y };
+      });
+    }
+    appendPathElement(parentEl, segments, "iteration-path infinity copied");
+  }
+}
+
+
+
+
+
+
+
+
+
+function renderEndDelay(parentEl, state,
+                        mainIterationStartTime, iterationCount, getSegment) {
+  const startTime = mainIterationStartTime + iterationCount * state.duration;
   const startSegment = getSegment(startTime);
   const endSegment = { x: startTime + state.endDelay, y: startSegment.y };
   appendPathElement(parentEl, [startSegment, endSegment], "enddelay-path");
@@ -407,15 +501,14 @@ function renderEndDelay(parentEl, state, iterationCount, getSegment) {
 
 
 
-function renderForwardsFill(parentEl, state, iterationCount,
-                            totalDuration, getSegment) {
-  const startTime = state.delay + iterationCount * state.duration +
-                    (state.endDelay > 0 ? state.endDelay : 0);
+
+function renderForwardsFill(parentEl, state, mainIterationStartTime,
+                            iterationCount, totalDuration, getSegment) {
+  const startTime = mainIterationStartTime + iterationCount * state.duration +
+                      (state.endDelay > 0 ? state.endDelay : 0);
   const startSegment = getSegment(startTime);
   const endSegment = { x: totalDuration, y: startSegment.y };
-  const pathEl = appendPathElement(parentEl, [startSegment, endSegment],
-                                   "fill-forwards-path");
-  appendFadeEffect(parentEl, pathEl);
+  appendPathElement(parentEl, [startSegment, endSegment], "fill-forwards-path");
 }
 
 
@@ -428,7 +521,7 @@ function getSegmentHelper(state, win) {
   
   
   const timing = Object.assign({}, state, {
-    iterations: state.iterationCount ? state.iterationCount : 1
+    iterations: state.iterationCount ? state.iterationCount : Infinity
   });
   
   const dummyAnimation =
@@ -526,68 +619,4 @@ function appendPathElement(parentEl, pathSegments, cls) {
       "transform": "scale(1, -1)"
     }
   });
-}
-
-
-
-
-
-
-function appendFadeEffect(parentEl, el) {
-  
-  
-  if (!parentEl.ownerDocument.body.querySelector(`#${ FADE_MASK_ID }`)) {
-    const svgEl = parentEl.closest(".summary");
-    const defsEl = createNode({
-      parent: svgEl,
-      namespace: SVG_NS,
-      nodeType: "defs"
-    });
-    const gradientEl = createNode({
-      parent: defsEl,
-      namespace: SVG_NS,
-      nodeType: "linearGradient",
-      attributes: {
-        "id": FADE_GRADIENT_ID
-      }
-    });
-    createNode({
-      parent: gradientEl,
-      namespace: SVG_NS,
-      nodeType: "stop",
-      attributes: {
-        "offset": 0
-      }
-    });
-    createNode({
-      parent: gradientEl,
-      namespace: SVG_NS,
-      nodeType: "stop",
-      attributes: {
-        "offset": 1
-      }
-    });
-
-    const maskEl = createNode({
-      parent: defsEl,
-      namespace: SVG_NS,
-      nodeType: "mask",
-      attributes: {
-        "id": FADE_MASK_ID,
-        "maskContentUnits": "objectBoundingBox",
-      }
-    });
-    createNode({
-      parent: maskEl,
-      namespace: SVG_NS,
-      nodeType: "rect",
-      attributes: {
-        "y": `${ 1 + svgEl.viewBox.animVal.y }`,
-        "width": "1",
-        "height": `${ svgEl.viewBox.animVal.height }`,
-        "fill": `url(#${ FADE_GRADIENT_ID })`
-      }
-    });
-  }
-  el.setAttribute("mask", `url(#${ FADE_MASK_ID })`);
 }
