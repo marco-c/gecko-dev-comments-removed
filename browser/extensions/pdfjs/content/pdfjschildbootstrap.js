05594f8162e0d68a6286010e5682d222b7006fdc
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 20:34:16 +0000

    Backed out changeset 5de79bf87514 (bug 1354081)
    UltraBlame original commit: ae417c8b8217005456e2f381bb2089cc032b400b

--- browser/extensions/pdfjs/content/pdfjschildbootstrap.js
+++ browser/extensions/pdfjs/content/pdfjschildbootstrap.js
@@ -21,7 +21,14 @@
 
 
 
+Components.utils.import("resource://gre/modules/Services.jsm");
+Components.utils.import("resource://pdf.js/PdfJs.jsm");
 Components.utils.import("resource://pdf.js/PdfjsContentUtils.jsm");
 
 
 PdfjsContentUtils.init();
+
+if (Services.appinfo.processType === Services.appinfo.PROCESS_TYPE_CONTENT) {
+  
+  PdfJs.updateRegistration();
+}
