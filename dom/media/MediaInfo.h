897f58d234f8e9fd4e7fa990a0b2e521eaf8ded4
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 04:33:03 +0000

    Bug 1320829 - updated WebM demuxer to surface alpha information. r=jya
    MozReview-Commit-ID: 1jBmc5KUEme
    
    UltraBlame original commit: c599df1589cc18d70b70b93d1bebb81dbe008ca3

--- dom/media/MediaInfo.h
+++ dom/media/MediaInfo.h
@@ -215,6 +215,7 @@ public:
     , mExtraData(aOther.mExtraData)
     , mRotation(aOther.mRotation)
     , mImageRect(aOther.mImageRect)
+    , mAlphaPresent(aOther.mAlphaPresent)
   {
   }
 
@@ -236,6 +237,16 @@ public:
   UniquePtr<TrackInfo> Clone() const override
   {
     return MakeUnique<VideoInfo>(*this);
+  }
+
+  void SetAlpha(bool aAlphaPresent)
+  {
+    mAlphaPresent = aAlphaPresent;
+  }
+
+  bool HasAlpha() const
+  {
+    return mAlphaPresent;
   }
 
   nsIntRect ImageRect() const
@@ -309,6 +320,9 @@ private:
   
   
   nsIntRect mImageRect;
+
+  
+  bool mAlphaPresent = false;
 };
 
 class AudioInfo : public TrackInfo {
