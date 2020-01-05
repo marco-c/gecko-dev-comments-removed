ee7f9a64c22ea6d4678cdf6622be15a5153575fb
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 18:53:26 +0000

    Bug 1346235 - part 1: translate native error to Java exception. r=jchen
    MozReview-Commit-ID: BcpfJLSzTvk
    
    UltraBlame original commit: fa8084f4f200ebb0b1649287c583e60cfa9198f3

--- mozglue/android/SharedMemNatives.cpp
+++ mozglue/android/SharedMemNatives.cpp
@@ -3,7 +3,9 @@
 
 
 
+#include <errno.h>
 #include <jni.h>
+#include <stdio.h>
 #include <string.h>
 #include <sys/mman.h>
 
@@ -52,6 +54,12 @@ jlong JNICALL
 Java_org_mozilla_gecko_mozglue_SharedMemory_map(JNIEnv *env, jobject jobj, jint fd, jint length)
 {
   void* address = mmap(NULL, length, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+  if (address == MAP_FAILED) {
+    char msg[128];
+    snprintf(msg, sizeof(msg), "mmap failed. errno=%d", errno);
+    env->ThrowNew(env->FindClass("java/lang/NullPointerException"), msg);
+    return 0;
+  }
   return jlong(address);
 }
 
