f25172aa6630d29d62e19b9893b4c42172dd4af7
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 05:58:32 +0000

    Bug 1323100 - Assign names to all remaining threads that are created through NS_NewThread and create them using NS_NewNamedThread instead. r=froydnj
    MozReview-Commit-ID: 7W1dt2BBKJZ
    
    UltraBlame original commit: 699f7f888ea36afec5d6bcc76da086812a424183

--- xpcom/threads/TimerThread.cpp
+++ xpcom/threads/TimerThread.cpp
@@ -323,7 +323,8 @@ TimerThread::Init()
 
   if (mInitInProgress.exchange(true) == false) {
     
-    nsresult rv = NS_NewThread(getter_AddRefs(mThread), this);
+    nsresult rv =
+      NS_NewNamedThread("Timer Thread", getter_AddRefs(mThread), this);
     if (NS_FAILED(rv)) {
       mThread = nullptr;
     } else {
