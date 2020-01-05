9ba6a59a86b43cd813cbe3510066809cda742e2d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 02:58:05 +0000

    Backed out changeset 3983dd7e87bb (bug 1309516)
    UltraBlame original commit: d284ffa5e67381e5a256f9a90d99c0d4ab116913

--- dom/media/MediaFormatReader.cpp
+++ dom/media/MediaFormatReader.cpp
@@ -1952,11 +1952,25 @@ MediaFormatReader::SetSeekTarget(const SeekTarget& aTarget)
   
   if (!ForceZeroStartTime()) {
     target.SetTime(aTarget.GetTime() - TimeUnit::FromMicroseconds(StartTime())
-                   + mInfo.mStartTime);
+                   + DemuxStartTime());
   }
 
   mOriginalSeekTarget = target;
   mFallbackSeekTime = mPendingSeekTime = Some(target.GetTime());
+}
+
+TimeUnit
+MediaFormatReader::DemuxStartTime()
+{
+  MOZ_ASSERT(OnTaskQueue());
+  MOZ_ASSERT(!ForceZeroStartTime());
+  MOZ_ASSERT(HasAudio() || HasVideo());
+
+  const TimeUnit startTime =
+    std::min(mAudio.mFirstDemuxedSampleTime.refOr(TimeUnit::FromInfinity()),
+             mVideo.mFirstDemuxedSampleTime.refOr(TimeUnit::FromInfinity()));
+
+  return startTime.IsInfinite() ? TimeUnit::FromMicroseconds(0) : startTime;
 }
 
 void
--- dom/media/MediaFormatReader.h
+++ dom/media/MediaFormatReader.h
@@ -540,6 +540,7 @@ private:
 
   
   void SetSeekTarget(const SeekTarget& aTarget);
+  media::TimeUnit DemuxStartTime();
   bool IsSeeking() const { return mPendingSeekTime.isSome(); }
   bool IsVideoSeeking() const
   {
