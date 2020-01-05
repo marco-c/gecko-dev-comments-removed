a655712f1329fbfa844c0dfe8c7073c3182cff99
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 10:40:27 +0000

    servo: Merge #8175 - Fixed panic in ActiveTimers.set_timeout_or_interval (from benschulz:set-timeout-panic); r=jdm
    `ActiveTimers.set_timeout_or_interval` asserts that the pipeline is not currently frozen. Apparently that is too strict. When pending network requests complete after a pipeline is frozen, scripts may be executed and a timer scheduled.
    
    With these changes scheduling a timer while the pipeline is frozen behaves as if the timer was scheduled at the time the pipeline was frozen.
    
    To reproduce the panic
     1. `./mach run -r http://google.com`,
     2. immediately click on any link and
     3. wait for the panic.
    
    Source-Repo: https://github.com/servo/servo
    Source-Revision: f74754f582fa8126eacb1fa423cd9db2325d8621
    
    UltraBlame original commit: 463b35a88e96570c2060191939ab219d8c189c1b

--- servo/components/script/timers.rs
+++ servo/components/script/timers.rs
@@ -139,8 +139,6 @@ impl ActiveTimers {
                                is_interval: IsInterval,
                                source: TimerSource)
                                -> i32 {
-        assert!(self.suspended_since.get().is_none());
-
         
         let TimerHandle(new_handle) = self.next_timer_handle.get();
         self.next_timer_handle.set(TimerHandle(new_handle + 1));
@@ -283,7 +281,10 @@ impl ActiveTimers {
     }
 
     fn schedule_timer_call(&self) {
-        assert!(self.suspended_since.get().is_none());
+        if self.suspended_since.get().is_some() {
+            
+            return;
+        }
 
         let timers = self.timers.borrow();
 
@@ -318,7 +319,12 @@ impl ActiveTimers {
     }
 
     fn base_time(&self) -> MsDuration {
-        precise_time_ms() - self.suspension_offset.get()
+        let offset = self.suspension_offset.get();
+
+        match self.suspended_since.get() {
+            Some(time) => time - offset,
+            None => precise_time_ms() - offset,
+        }
     }
 
     
