e3c0e2566a7baac1cbba88f70fe478142fb0cd2d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 21:02:59 +0000

    Bug 1333968 - Label the DidComposite message (r=dvander)
    MozReview-Commit-ID: 7cW7apxUJu0
    
    UltraBlame original commit: 47dfef12051fb99603dcaedd659a03c4a3207fe5

--- gfx/layers/ipc/CompositorBridgeChild.h
+++ gfx/layers/ipc/CompositorBridgeChild.h
@@ -265,6 +265,9 @@ private:
                                                  const uint64_t& aEpoch,
                                                  const bool& aActive) override;
 
+  already_AddRefed<nsIEventTarget>
+  GetSpecificMessageEventTarget(const Message& aMsg) override;
+
   
   class SharedFrameMetricsData {
   public:
