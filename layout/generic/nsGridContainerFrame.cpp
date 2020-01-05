8226c5b7d4ceac69077947d805d495b7852f2bc4
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 06:48:01 +0000

    Bug 1330380 part 1 - Rename a couple of variables.  r=dholbert
    UltraBlame original commit: a6f5417cf3f249688ee74c9a95bd35c58a086ef9

--- layout/generic/nsGridContainerFrame.cpp
+++ layout/generic/nsGridContainerFrame.cpp
@@ -3748,8 +3748,8 @@ ContentContribution(const GridItemInfo&    aGridItem,
   if (size == NS_INTRINSIC_WIDTH_UNKNOWN) {
     
     
-    nscoord cbISize = INFINITE_ISIZE_COORD;
-    nscoord cbBSize = NS_UNCONSTRAINEDSIZE;
+    nscoord availISize = INFINITE_ISIZE_COORD;
+    nscoord availBSize = NS_UNCONSTRAINEDSIZE;
     auto childWM = child->GetWritingMode();
     const bool isOrthogonal = childWM.IsOrthogonalTo(aCBWM);
     
@@ -3758,12 +3758,12 @@ ContentContribution(const GridItemInfo&    aGridItem,
     if (aState.mCols.mCanResolveLineRangeSize) {
       nscoord sz = aState.mCols.ResolveSize(aGridItem.mArea.mCols);
       if (isOrthogonal) {
-        cbBSize = sz;
+        availBSize = sz;
         if (aGridItem.mState[aAxis] & ItemState::eClampMarginBoxMinSize) {
           bMinSizeClamp = sz;
         }
       } else {
-        cbISize = sz;
+        availISize = sz;
         if (aGridItem.mState[aAxis] & ItemState::eClampMarginBoxMinSize) {
           iMinSizeClamp = sz;
         }
@@ -3774,7 +3774,7 @@ ContentContribution(const GridItemInfo&    aGridItem,
     } else {
       iMinSizeClamp = aMinSizeClamp;
     }
-    LogicalSize availableSize(childWM, cbISize, cbBSize);
+    LogicalSize availableSize(childWM, availISize, availBSize);
     size = ::MeasuringReflow(child, aState.mReflowInput, aRC, availableSize,
                              iMinSizeClamp, bMinSizeClamp);
     nsIFrame::IntrinsicISizeOffsetData offsets = child->IntrinsicBSizeOffsets();
