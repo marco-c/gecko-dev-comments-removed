cbf3333815d533d8d790f5064e4817b32ebedaa4
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 01:23:34 +0000

    Bug 1274633 - remove official branding 'learn more' page, r=MattN
    MozReview-Commit-ID: 6OtpPeaw0zj
    
    UltraBlame original commit: 6658f810356298603974bfc46c375f099239c0b5

--- browser/branding/official/pref/firefox-branding.js
+++ browser/branding/official/pref/firefox-branding.js
@@ -4,7 +4,7 @@
 
 pref("startup.homepage_override_url", "");
 pref("startup.homepage_welcome_url", "https://www.mozilla.org/%LOCALE%/firefox/%VERSION%/firstrun/");
-pref("startup.homepage_welcome_url.additional", "https://www.mozilla.org/%LOCALE%/firefox/%VERSION%/firstrun/learnmore/");
+pref("startup.homepage_welcome_url.additional", "");
 
 pref("app.update.interval", 43200); 
 
