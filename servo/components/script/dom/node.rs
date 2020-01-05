df2342642f195ff96d74ae7f5cd524f6a61f450a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 10:41:08 +0000

    servo: Merge #8476 - Remove unnecessary 'node changed' notification (from frewsxcv:unnecessary-document-notification); r=nox
    This same notification already happens in `CharacterData::SetData`
    
    Source-Repo: https://github.com/servo/servo
    Source-Revision: 1b20bc90ee8321aaa95e1906321753549ad166e6
    
    UltraBlame original commit: 9b04adb489f8b830651814b84e23047364cbf336

--- servo/components/script/dom/node.rs
+++ servo/components/script/dom/node.rs
@@ -1935,10 +1935,6 @@ impl NodeMethods for Node {
             NodeTypeId::CharacterData(..) => {
                 let characterdata = self.downcast::<CharacterData>().unwrap();
                 characterdata.SetData(value);
-
-                
-                let document = self.owner_doc();
-                document.content_changed(self, NodeDamage::OtherNodeDamage);
             }
             NodeTypeId::DocumentType |
             NodeTypeId::Document => {}
