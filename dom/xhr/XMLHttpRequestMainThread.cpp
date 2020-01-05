607f41cebcf2c6dfcc92de9e74330287a7f953ff
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 00:26:32 +0000

    Backed out changeset 490c671f8047 (bug 1303121) for failures in test_CrossSiteXHR.html
    UltraBlame original commit: 51836860e20bbd0b146ea11810750d7f986cddcf

--- dom/xhr/XMLHttpRequestMainThread.cpp
+++ dom/xhr/XMLHttpRequestMainThread.cpp
@@ -1038,8 +1038,10 @@ XMLHttpRequestMainThread::CloseRequestWithError(const ProgressEventType aType)
     if (!mFlagSyncLooping) {
       if (mUpload && !mUploadComplete) {
         mUploadComplete = true;
+        DispatchProgressEvent(mUpload, ProgressEventType::progress, 0, 0);
         DispatchProgressEvent(mUpload, aType, 0, 0);
       }
+      DispatchProgressEvent(this, ProgressEventType::progress, 0, 0);
       DispatchProgressEvent(this, aType, 0, 0);
     }
   }
@@ -2185,6 +2187,7 @@ XMLHttpRequestMainThread::ChangeStateToDone()
   
   
   if (!mFlagSynchronous && mUpload && !mUploadComplete) {
+    DispatchProgressEvent(mUpload, ProgressEventType::progress, 0, 0);
     DispatchProgressEvent(mUpload, ProgressEventType::error, 0, 0);
   }
 
