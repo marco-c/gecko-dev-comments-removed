b03245bad894dbbea21aab7ee8f363556adc4c1e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 19:53:05 +0000

    Bug 1331092 - Part 10: Support JSOP_TOASYNCGEN, JSOP_TOASYNCITER, and JSOP_AWAIT in decompiler. r=shu
    UltraBlame original commit: 6564ccfdc7044009fd5d056865491d2e778fd93b

--- js/src/jsopcode.cpp
+++ js/src/jsopcode.cpp
@@ -2027,7 +2027,11 @@ ExpressionDecompiler::decompilePC(jsbytecode* pc, uint8_t defIndex)
           case JSOP_LAMBDA:
           case JSOP_LAMBDA_ARROW:
           case JSOP_TOASYNC:
+          case JSOP_TOASYNCGEN:
             return write("FUN");
+
+          case JSOP_TOASYNCITER:
+            return write("ASYNCITER");
 
           case JSOP_MOREITER:
             
@@ -2071,6 +2075,7 @@ ExpressionDecompiler::decompilePC(jsbytecode* pc, uint8_t defIndex)
           case JSOP_UNINITIALIZED:
             return write("UNINITIALIZED");
 
+          case JSOP_AWAIT:
           case JSOP_YIELD:
             
             
