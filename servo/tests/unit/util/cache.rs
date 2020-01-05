146306d4db5a94432c1f1b9a116ac11e09f32492
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 11:16:44 +0000

    servo: Merge #11180 - Minor cleanup and optimizations in glyph/style caching (from mbrubeck:text-cleanup); r=pcwalton
    Gets rid of some unnecessary String and Arc clones during text shaping and style matching.
    
    r? pcwalton
    
    Source-Repo: https://github.com/servo/servo
    Source-Revision: e2990766dc1c7461b55c96f0ce7116d35d4fd3c6
    
    UltraBlame original commit: 327d24febd2e8995659dba7550b1ef41d41f7781

--- servo/tests/unit/util/cache.rs
+++ servo/tests/unit/util/cache.rs
@@ -13,7 +13,7 @@ fn test_hashcache() {
     assert!(cache.find(&1).is_some());
     assert!(cache.find(&2).is_none());
 
-    cache.find_or_create(&2, |_v| { Cell::new("two") });
+    cache.find_or_create(2, || { Cell::new("two") });
     assert!(cache.find(&1).is_some());
     assert!(cache.find(&2).is_some());
 }
@@ -44,7 +44,7 @@ fn test_lru_cache() {
     assert!(cache.find(&4).is_some());  
 
     
-    cache.find_or_create(&1, |_| { Cell::new("one") }); 
+    cache.find_or_create(1, || { Cell::new("one") }); 
 
     assert!(cache.find(&1).is_some()); 
     assert!(cache.find(&2).is_none()); 
