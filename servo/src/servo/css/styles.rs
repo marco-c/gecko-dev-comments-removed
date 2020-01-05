17d1905cd4ec1cc6e26ccf478a53bc334a45dd31
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 08:28:53 +0000

    servo: Eliminate more warnings
    Source-Repo: https://github.com/servo/servo
    Source-Revision: e836269b8df58bfda69a3112a9f8cecab8eaa8dc
    
    UltraBlame original commit: e53e2b60f3a16fd8dfdc966c2fc00653c4aac73f

--- servo/src/servo/css/styles.rs
+++ servo/src/servo/css/styles.rs
@@ -119,7 +119,7 @@ impl Node : StyleMethods {
             fail ~"get_style() called on a node without a style!";
         }
         
-        return copy *self.aux(|x| copy x).style;
+        return copy *self.aux(|x| copy *x).style;
     }
 
     /**
