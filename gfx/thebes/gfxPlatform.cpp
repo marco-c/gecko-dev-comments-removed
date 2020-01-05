a3a33a1647ee8596f664926b1145b7e97d72e04b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 12:51:59 +0000

    Bug 1328602 - Start implementing the render thread. r=gfx?
    UltraBlame original commit: 178137a72f3df3e897c2abba94b48b4ee2568d6a

--- gfx/thebes/gfxPlatform.cpp
+++ gfx/thebes/gfxPlatform.cpp
@@ -7,6 +7,7 @@
 #include "mozilla/layers/CompositorThread.h"
 #include "mozilla/layers/ImageBridgeChild.h"
 #include "mozilla/layers/ISurfaceAllocator.h"     
+#include "mozilla/layers/RenderThread.h"
 #include "mozilla/gfx/gfxVars.h"
 #include "mozilla/gfx/GPUProcessManager.h"
 #include "mozilla/gfx/GraphicsMessages.h"
@@ -939,6 +940,9 @@ gfxPlatform::InitLayersIPC()
 
     if (XRE_IsParentProcess())
     {
+        if (gfxPrefs::WebRenderEnabled()) {
+            layers::RenderThread::Start();
+        }
         layers::CompositorThreadHolder::Start();
     }
 }
@@ -965,6 +969,7 @@ gfxPlatform::ShutdownLayersIPC()
 
         
         layers::CompositorThreadHolder::Shutdown();
+        layers::RenderThread::ShutDown();
     } else {
       
       
