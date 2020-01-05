8dc05524efb01f46ac4a66e490286b1817786c1c
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 03:01:03 +0000

    Bug 1313099 - Put nodejs in tooltool (mozharness changes) r=jlund
    MozReview-Commit-ID: 7ijnlF5D9B2
    
    UltraBlame original commit: 231272fa95c5c156601c2a121f050b3ad62d74a3

--- testing/mozharness/configs/remove_executables.py
+++ testing/mozharness/configs/remove_executables.py
@@ -2,5 +2,7 @@ config = {
     
     "download_minidump_stackwalk": False,
     "minidump_stackwalk_path": "/usr/local/bin/linux64-minidump_stackwalk",
+    "download_nodejs": False,
+    "nodejs_path": "/usr/local/bin/node",
     "exes": {}
 }
