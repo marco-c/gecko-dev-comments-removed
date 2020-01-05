947048f3df5634128663183c12cf5d275ece9601
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 03:45:43 +0000

    Bug 1316729 - Pass command-line args to gtest. r=ted
    MozReview-Commit-ID: 8Xs52E9NnCq
    
    UltraBlame original commit: 8df58dfdea13c9c79975abb4e0c6514b1b4da5f6

--- toolkit/xre/nsAppRunner.cpp
+++ toolkit/xre/nsAppRunner.cpp
@@ -277,7 +277,7 @@ extern "C" MOZ_EXPORT void XRE_LibFuzzerSetMain(int argc, char** argv, LibFuzzer
 #endif
 
 namespace mozilla {
-int (*RunGTest)() = 0;
+int (*RunGTest)(int*, char**) = 0;
 } 
 
 using namespace mozilla;
@@ -3740,7 +3740,7 @@ XREMain::XRE_mainStartup(bool* aExitFlag)
     
     if (mozilla::RunGTest) {
       gIsGtest = true;
-      result = mozilla::RunGTest();
+      result = mozilla::RunGTest(&gArgc, gArgv);
       gIsGtest = false;
     } else {
       result = 1;
