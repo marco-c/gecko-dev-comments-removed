951280107100ae236ccf235b90c86c1115aaa254
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 03:28:49 +0000

    Backed out changeset e2a5702d96b0 (bug 1306314)
    UltraBlame original commit: 6adbbb16e04af32c3a358774013638aa8cee2ccd

--- dom/media/gmp/GMPLoader.h
+++ dom/media/gmp/GMPLoader.h
@@ -42,10 +42,7 @@ public:
 
   
   virtual GMPErr GMPInit(const GMPPlatformAPI* aPlatformAPI) = 0;
-  virtual GMPErr GMPGetAPI(const char* aAPIName,
-                           void* aHostAPI,
-                           void** aPluginAPI,
-                           uint32_t aDecryptorId) = 0;
+  virtual GMPErr GMPGetAPI(const char* aAPIName, void* aHostAPI, void** aPluginAPI) = 0;
   virtual void GMPShutdown() = 0;
   virtual void GMPSetNodeId(const char* aNodeId, uint32_t aLength) = 0;
 };
@@ -86,10 +83,7 @@ public:
                     GMPAdapter* aAdapter = nullptr) = 0;
 
   
-  virtual GMPErr GetAPI(const char* aAPIName,
-                        void* aHostAPI,
-                        void** aPluginAPI,
-                        uint32_t aDecryptorId) = 0;
+  virtual GMPErr GetAPI(const char* aAPIName, void* aHostAPI, void** aPluginAPI) = 0;
 
   
   
