88e8c0027392964430afd5dedc1b064b5730833f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 03:32:31 +0000

    Bug 1313556 - update rust parser for pssh parsing. r=rillian
    MozReview-Commit-ID: KH8K7mizUHl
    
    UltraBlame original commit: 88e9a1a777f4a06b845f7ba8dbff472b44611d49

--- media/libstagefright/binding/include/mp4parse.h
+++ media/libstagefright/binding/include/mp4parse.h
@@ -48,16 +48,20 @@ typedef struct mp4parse_track_info {
 	int64_t media_time;
 } mp4parse_track_info;
 
-typedef struct mp4parse_codec_specific_config {
+typedef struct mp4parse_byte_data {
 	uint32_t length;
 	uint8_t const* data;
-} mp4parse_codec_specific_config;
+} mp4parse_byte_data;
+
+typedef struct mp4parse_pssh_info {
+	mp4parse_byte_data data;
+} mp4parse_pssh_info;
 
 typedef struct mp4parse_track_audio_info {
 	uint16_t channels;
 	uint16_t bit_depth;
 	uint32_t sample_rate;
-	mp4parse_codec_specific_config codec_specific_config;
+	mp4parse_byte_data codec_specific_config;
 } mp4parse_track_audio_info;
 
 typedef struct mp4parse_track_video_info {
@@ -99,9 +103,19 @@ mp4parse_error mp4parse_get_track_audio_info(mp4parse_parser* parser, uint32_t t
 
 mp4parse_error mp4parse_get_track_video_info(mp4parse_parser* parser, uint32_t track_index, mp4parse_track_video_info* info);
 
+
 mp4parse_error mp4parse_get_fragment_info(mp4parse_parser* parser, mp4parse_fragment_info* info);
 
+
 mp4parse_error mp4parse_is_fragmented(mp4parse_parser* parser, uint32_t track_id, uint8_t* fragmented);
+
+
+
+
+
+
+
+mp4parse_error mp4parse_get_pssh_info(mp4parse_parser* parser, mp4parse_pssh_info* info);
 
 
 
