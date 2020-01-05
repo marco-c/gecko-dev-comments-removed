34571421c366f343e1cacb1da06ab26cfc779893
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 17:46:20 +0000

    Backed out changeset a293a617ebed (bug 1321026) on suspicion of causing xpcshell bustage. r=backout on a CLOSED TREE
    UltraBlame original commit: a33c70b339b9c1e015165170ef38d1768a553881

--- testing/xpcshell/runxpcshelltests.py
+++ testing/xpcshell/runxpcshelltests.py
@@ -1092,8 +1092,7 @@ class XPCShellTests(object):
                  testClass=XPCShellTestThread, failureManifest=None,
                  log=None, stream=None, jsDebugger=False, jsDebuggerPort=0,
                  test_tags=None, dump_tests=None, utility_path=None,
-                 rerun_failures=False, threadCount=NUM_THREADS,
-                 failure_manifest=None, jscovdir=None, **otherOptions):
+                 rerun_failures=False, failure_manifest=None, jscovdir=None, **otherOptions):
         """Run xpcshell tests.
 
         |xpcshell|, is the xpcshell executable to use to run the tests.
@@ -1194,7 +1193,6 @@ class XPCShellTests(object):
         self.pluginsPath = pluginsPath
         self.sequential = sequential
         self.failure_manifest = failure_manifest
-        self.threadCount = threadCount
         self.jscovdir = jscovdir
 
         self.testCount = 0
@@ -1349,7 +1347,7 @@ class XPCShellTests(object):
         if self.sequential:
             self.log.info("Running tests sequentially.")
         else:
-            self.log.info("Using at most %d threads." % self.threadCount)
+            self.log.info("Using at most %d threads." % NUM_THREADS)
 
         
         
@@ -1370,7 +1368,7 @@ class XPCShellTests(object):
                 break
 
             
-            while keep_going and tests_queue and (len(running_tests) < self.threadCount):
+            while keep_going and tests_queue and (len(running_tests) < NUM_THREADS):
                 test = tests_queue.popleft()
                 running_tests.add(test)
                 test.start()
