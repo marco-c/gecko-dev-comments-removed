bd01951d0ebb0c483726897eb9c0934bc2bc2aa4
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 20:03:39 +0000

    Bug 1351732 - Part 1: Add an ArenaAllocator strdup extension. r=froydnj
    This adds an extension to ArenaAllocator that provides strdup-like
    functionality for various string types.
    
    MozReview-Commit-ID: 87SHTs6flHY
    
    UltraBlame original commit: b7e322f24106a81673135d9e1dd839609de35688

--- xpcom/tests/gtest/TestArenaAllocator.cpp
+++ xpcom/tests/gtest/TestArenaAllocator.cpp
@@ -5,6 +5,7 @@
 
 
 #include "mozilla/ArenaAllocator.h"
+#include "mozilla/ArenaAllocatorExtensions.h"
 #include "nsIMemoryReporter.h" 
 
 #include "gtest/gtest.h"
@@ -276,4 +277,20 @@ TEST(ArenaAllocator, Clear)
   prev_sz = sz;
   sz = a.SizeOfExcludingThis(TestSizeOf);
   EXPECT_GT(sz, prev_sz);
+}
+
+TEST(ArenaAllocator, Extensions)
+{
+  ArenaAllocator<4096, 8> a;
+  const char* const kTestStr = "This is a test string.";
+  char* dup = mozilla::ArenaStrdup(kTestStr, a);
+  EXPECT_STREQ(dup, kTestStr);
+
+  NS_NAMED_LITERAL_STRING(wideStr, "A wide string.");
+  nsLiteralString::char_type* wide = mozilla::ArenaStrdup(wideStr, a);
+  EXPECT_TRUE(wideStr.Equals(wide));
+
+  NS_NAMED_LITERAL_CSTRING(cStr, "A c-string.");
+  nsLiteralCString::char_type* cstr = mozilla::ArenaStrdup(cStr, a);
+  EXPECT_TRUE(cStr.Equals(cstr));
 }
