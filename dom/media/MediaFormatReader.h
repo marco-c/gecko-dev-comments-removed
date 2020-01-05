e500b5a5eef6813057fed5a21b16a8e54ca9cd06
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 03:11:27 +0000

    Backed out changeset 9ee070ee6dad (bug 1315144) to get at the patch below it CLOSED TREE
    MozReview-Commit-ID: 3d5vSfNXqO4
    
    UltraBlame original commit: 6af65d5a444459c2b0854d19f15be78e4d66dc9d

--- dom/media/MediaFormatReader.h
+++ dom/media/MediaFormatReader.h
@@ -328,21 +328,9 @@ private:
     Maybe<MediaResult> mError;
     bool HasFatalError() const
     {
-      if (!mError.isSome()) {
-        return false;
-      }
-      if (mError.ref() == NS_ERROR_DOM_MEDIA_DECODE_ERR) {
-        
-        
-        return mNumOfConsecutiveError > mMaxConsecutiveError;
-      } else if (mError.ref() == NS_ERROR_DOM_MEDIA_NEED_NEW_DECODER) {
-        
-        
-        return false;
-      } else {
-        
-        return true;
-      }
+      return mError.isSome() &&
+             (mError.ref() != NS_ERROR_DOM_MEDIA_DECODE_ERR ||
+              mNumOfConsecutiveError > mMaxConsecutiveError);
     }
 
     
