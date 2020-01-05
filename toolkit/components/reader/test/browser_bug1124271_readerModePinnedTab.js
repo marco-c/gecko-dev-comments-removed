f3167991a763407b8196cfd80502fc18dd5f23df
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 02:56:08 +0000

    Bug 1189491 - Take anchors into account in the reader view, r=gijs
    MozReview-Commit-ID: 1udixhxnVao
    
    UltraBlame original commit: 4da7d22ebcd2fdd3047487d7c2e483093324263c

--- toolkit/components/reader/test/browser_bug1124271_readerModePinnedTab.js
+++ toolkit/components/reader/test/browser_bug1124271_readerModePinnedTab.js
@@ -6,7 +6,7 @@
 
 const PREF = "reader.parse-on-load.enabled";
 
-const TEST_PATH = "http://example.com/browser/browser/base/content/test/general/";
+const TEST_PATH = getRootDirectory(gTestPath).replace("chrome://mochitests/content", "http://example.com");
 
 var readerButton = document.getElementById("reader-mode-button");
 
@@ -37,9 +37,9 @@ add_task(function* () {
   
   is(gBrowser.tabs.length, initialTabsCount, "No additional tabs were opened.");
 
+  let pageShownPromise = BrowserTestUtils.waitForContentEvent(tab.linkedBrowser, "pageshow");
   readerButton.click();
-  yield BrowserTestUtils.waitForContentEvent(tab.linkedBrowser, "pageshow");
-
+  yield pageShownPromise;
   
   is(gBrowser.tabs.length, initialTabsCount, "No additional tabs were opened.");
 
