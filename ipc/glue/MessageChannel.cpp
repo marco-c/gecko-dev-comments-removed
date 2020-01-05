d70eba561d50dd9e3ec5400b705d9dca9b078535
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 20:17:18 +0000

    Bug 1349699 - Assert that the Chromium channel is closed when MessageLoop is destroyed (r=dvander)
    MozReview-Commit-ID: I7HyjVanlxJ
    
    UltraBlame original commit: 5cc766c0864e83d4caaf4eb712d466a5c2d38809

--- ipc/glue/MessageChannel.cpp
+++ ipc/glue/MessageChannel.cpp
@@ -654,6 +654,16 @@ MessageChannel::Clear()
     
     
 
+#if !defined(ANDROID)
+    if (!Unsound_IsClosed()) {
+#if defined(MOZ_CRASHREPORTER)
+        CrashReporter::AnnotateCrashReport(NS_LITERAL_CSTRING("ProtocolName"),
+                                           nsDependentCString(mName));
+#endif
+        MOZ_CRASH("MessageChannel destroyed without being closed");
+    }
+#endif
+
     if (gParentProcessBlocker == this) {
         gParentProcessBlocker = nullptr;
     }
