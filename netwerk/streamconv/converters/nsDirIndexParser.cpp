8a314c789763b7c7c895d7fc368473957feb584d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 19:40:08 +0000

    Bug 1344467 - clear the format type for unknown types r=valentin To make sure the last (unknown) entry is always -1 ---  netwerk/streamconv/converters/nsDirIndexParser.cpp | 4 +---  1 file changed, 1 insertion(+), 3 deletions(-)
    UltraBlame original commit: 5ddd3bdbb8d4d864d07aefad7f08a8c73170bc7d

--- netwerk/streamconv/converters/nsDirIndexParser.cpp
+++ netwerk/streamconv/converters/nsDirIndexParser.cpp
@@ -153,9 +153,6 @@ nsDirIndexParser::ParseFormat(const char* aFormatStr) {
   
   if (mFormat == nullptr)
     return NS_ERROR_OUT_OF_MEMORY;
-  mFormat[num] = -1;
-  mFormat[0] = -1; 
-  
   int formatNum=0;
   do {
     while (*aFormatStr && nsCRT::IsAsciiSpace(char16_t(*aFormatStr)))
@@ -179,6 +176,7 @@ nsDirIndexParser::ParseFormat(const char* aFormatStr) {
     if (name.LowerCaseEqualsLiteral("description"))
       mHasDescription = true;
     
+    mFormat[formatNum] = -1;
     for (Field* i = gFieldTable; i->mName; ++i) {
       if (name.EqualsIgnoreCase(i->mName)) {
         mFormat[formatNum] = i->mType;
