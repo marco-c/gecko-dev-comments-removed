10fe989aef484c8c6a4772d1edb3f4941650105e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 19:22:14 +0000

    Bug 1333482 part 1 - [css-ui] Introduce the 'appearance: auto | none' property.  r=dholbert
    MozReview-Commit-ID: 3itCDTKuYQ9
    
    UltraBlame original commit: 51cff1ed410ba17ae8b072128ffa1781652eb0e5

--- layout/style/nsStyleStruct.h
+++ layout/style/nsStyleStruct.h
@@ -27,6 +27,7 @@
 #include "nsStyleAutoArray.h"
 #include "nsStyleCoord.h"
 #include "nsStyleConsts.h"
+#include "nsThemeConstants.h"
 #include "nsChangeHint.h"
 #include "nsPresContext.h"
 #include "nsCOMPtr.h"
@@ -2660,7 +2661,20 @@ struct MOZ_NEEDS_MEMMOVABLE_MEMBERS nsStyleDisplay
                                            
                                            
   uint8_t mContain;             
+
+  friend class nsComputedDOMStyle;
+  friend class nsRuleNode;
+  uint8_t mMozAppearance;       
   uint8_t mAppearance;          
+public:
+  MOZ_MUST_USE uint8_t UsedAppearance() const {
+    if (mAppearance == NS_THEME_NONE) {
+      return NS_THEME_NONE;
+    }
+    MOZ_ASSERT(mAppearance == NS_THEME_AUTO);
+    return mMozAppearance; 
+  }
+
   uint8_t mPosition;            
 
   
