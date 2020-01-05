e63b8726b9ade95c7c5129cd8bbbf991f175ba2a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 18:31:23 +0000

    Bug 1345262 (part 3) - Split PseudoStack::sampleContext() in two. r=mstange.
    It does almost entirely different things depending on whether we are setting or
    clearing the context.
    
    UltraBlame original commit: 7414c13a82a1b43316a5d169eb2c33f4f9b4253d

--- tools/profiler/public/PseudoStack.h
+++ tools/profiler/public/PseudoStack.h
@@ -290,27 +290,30 @@ public:
                     mozilla::sig_safe_t(mozilla::ArrayLength(mStack)));
   }
 
-  void sampleContext(JSContext* context)
+  void setJSContext(JSContext* aContext)
   {
-    if (mContext && !context) {
-      
-      
-      flushSamplerOnJSShutdown();
-    }
+    MOZ_ASSERT(aContext && !mContext);
 
-    mContext = context;
+    mContext = aContext;
 
-    if (!context) {
-      return;
-    }
-
-    js::SetContextProfilingStack(context,
+    js::SetContextProfilingStack(aContext,
                                  (js::ProfileEntry*) mStack,
                                  (uint32_t*) &mStackPointer,
                                  (uint32_t) mozilla::ArrayLength(mStack));
     if (mStartJSSampling) {
       enableJSSampling();
     }
+  }
+
+  void clearJSContext()
+  {
+    if (mContext) {
+      
+      
+      flushSamplerOnJSShutdown();
+    }
+
+    mContext = nullptr;
   }
 
   void enableJSSampling()
