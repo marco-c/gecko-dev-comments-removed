8829bddeccb42045e046e430a076e7653c82b3cc
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 12:11:43 +0000

    servo: Merge #15300 - Remove unused part of the return value of parse_blob_url() (from servo:blob-fragment); r=Wafflespeanut
    Source-Repo: https://github.com/servo/servo
    Source-Revision: 81712560cafbb6a5ed12c5306522a6ea0cbe09f7
    
    UltraBlame original commit: da159b43518982c0910b4408573f03dbcf361096

--- servo/components/net_traits/blob_url_store.rs
+++ servo/components/net_traits/blob_url_store.rs
@@ -35,15 +35,14 @@ pub struct BlobBuf {
 
 
 
-pub fn parse_blob_url(url: &ServoUrl) -> Result<(Uuid, FileOrigin, Option<String>), ()> {
+pub fn parse_blob_url(url: &ServoUrl) -> Result<(Uuid, FileOrigin), ()> {
     let url_inner = try!(Url::parse(url.path()).map_err(|_| ()));
-    let fragment = url_inner.fragment().map(|s| s.to_string());
     let id = {
         let mut segs = try!(url_inner.path_segments().ok_or(()));
         let id = try!(segs.nth(0).ok_or(()));
         try!(Uuid::from_str(id).map_err(|_| ()))
     };
-    Ok((id, get_blob_origin(&ServoUrl::from_url(url_inner)), fragment))
+    Ok((id, get_blob_origin(&ServoUrl::from_url(url_inner))))
 }
 
 
