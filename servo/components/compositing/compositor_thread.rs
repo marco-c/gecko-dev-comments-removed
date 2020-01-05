066771652600d1bc291a1883fe09bceff3ec77b6
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 11:22:24 +0000

    servo: Merge #11569 - compositing: Remove CompositorThread struct (from talklittle:remove_struct_10261); r=metajack
    <!-- Please describe your changes on the following line: -->
    Remove `CompositorThread` struct, since it wraps only `IOCompositor`. Use `IOCompositor` directly.
    
    ---
    <!-- Thank you for contributing to Servo! Please replace each `[ ]` by `[X]` when the step is complete, and replace `__` with appropriate data: -->
    - [X] `./mach build -d` does not report any errors
    - [X] `./mach test-tidy` does not report any errors
    - [X] These changes fix #10261 (github issue number if applicable).
    
    <!-- Either: -->
    - [ ] There are tests for these changes OR
    - [X] These changes do not require tests because refactoring
    
    <!-- Pull requests that do not address these steps are welcome, but they will require additional verification as part of the review process. -->
    
    Source-Repo: https://github.com/servo/servo
    Source-Revision: 6c5f5d35f56f75e5a235836c042a55b67da553d6
    
    UltraBlame original commit: 7fd1d00cb1ebe8d7764893799550fffd2d65540d

--- servo/components/compositing/compositor_thread.rs
+++ servo/components/compositing/compositor_thread.rs
@@ -5,7 +5,7 @@
 
 
 use SendableFrameTree;
-use compositor::{CompositingReason, IOCompositor};
+use compositor::CompositingReason;
 use euclid::point::Point2D;
 use euclid::size::Size2D;
 use gfx_traits::{Epoch, FrameTreeId, LayerId, LayerProperties, PaintListener};
@@ -17,14 +17,12 @@ use profile_traits::mem;
 use profile_traits::time;
 use script_traits::{AnimationState, ConstellationMsg, EventResult};
 use std::fmt::{Debug, Error, Formatter};
-use std::rc::Rc;
 use std::sync::mpsc::{Receiver, Sender, channel};
 use style_traits::cursor::Cursor;
 use style_traits::viewport::ViewportConstraints;
 use url::Url;
 use webrender;
 use webrender_traits;
-use windowing::WindowMethods;
 
 
 
@@ -228,17 +226,6 @@ impl Debug for Msg {
             Msg::PipelineExited(..) => write!(f, "PipelineExited"),
             Msg::GetScrollOffset(..) => write!(f, "GetScrollOffset"),
         }
-    }
-}
-
-pub struct CompositorThread;
-
-impl CompositorThread {
-    pub fn create<Window>(window: Rc<Window>,
-                          state: InitialCompositorState)
-                          -> IOCompositor<Window>
-                          where Window: WindowMethods + 'static {
-        IOCompositor::create(window, state)
     }
 }
 
