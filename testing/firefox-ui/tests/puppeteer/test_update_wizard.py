c02516f1371ff4d11f1d8e8886ade7827f2e3f22
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 05:54:10 +0000

    Backed out changeset 25505bf601f6 (bug 1326236) for test_update_wizard.py failures a=backout
    UltraBlame original commit: 2cf1ac92e30a466fb065ec4ee582b8dba68a10ed

--- testing/firefox-ui/tests/puppeteer/test_update_wizard.py
+++ testing/firefox-ui/tests/puppeteer/test_update_wizard.py
@@ -60,7 +60,3 @@ class TestUpdateWizard(PuppeteerMixin, MarionetteTestCase):
         
         self.assertEqual(self.wizard.downloading.progress.get_property('localName'),
                          'progressmeter')
-
-        
-        self.assertIsInstance(self.wizard.selected_index, int)
-        self.assertEqual(self.wizard.selected_panel, self.wizard.checking)
