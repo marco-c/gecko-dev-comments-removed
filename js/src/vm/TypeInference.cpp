d889a60eb197f1492068aa1b4984b3f8de822e3a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 22:00:34 +0000

    Bug 1319746: Add a memory reporter for CFGGraph on BaselineScript, r=njn
    UltraBlame original commit: cef3dd72d4440bcf056f4be73bfeeb8b2ddd3c2f

--- js/src/vm/TypeInference.cpp
+++ js/src/vm/TypeInference.cpp
@@ -4464,14 +4464,18 @@ void
 Zone::addSizeOfIncludingThis(mozilla::MallocSizeOf mallocSizeOf,
                              size_t* typePool,
                              size_t* baselineStubsOptimized,
+                             size_t* cachedCFG,
                              size_t* uniqueIdMap,
                              size_t* shapeTables,
                              size_t* atomsMarkBitmaps)
 {
     *typePool += types.typeLifoAlloc().sizeOfExcludingThis(mallocSizeOf);
     if (jitZone()) {
+        
+        
         *baselineStubsOptimized +=
             jitZone()->optimizedStubSpace()->sizeOfExcludingThis(mallocSizeOf);
+        *cachedCFG += jitZone()->cfgSpace()->sizeOfExcludingThis(mallocSizeOf);
     }
     *uniqueIdMap += uniqueIds().sizeOfExcludingThis(mallocSizeOf);
     *shapeTables += baseShapes().sizeOfExcludingThis(mallocSizeOf)
