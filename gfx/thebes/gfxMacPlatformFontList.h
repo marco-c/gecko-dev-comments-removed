afb071513bf4dcd06babcb9df3989cb2176dab51
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 07:04:58 +0000

    Bug 1330423 - Remove redundant 'virtual' keyword from methods in the Mac font backend. r=jrmuizel
    UltraBlame original commit: 4a772ea363ab231f7812753f0b77cf712423e124

--- gfx/thebes/gfxMacPlatformFontList.h
+++ gfx/thebes/gfxMacPlatformFontList.h
@@ -41,14 +41,14 @@ public:
         ::CGFontRelease(mFontRef);
     }
 
-    virtual CGFontRef GetFontRef();
+    CGFontRef GetFontRef();
 
     
     
-    virtual hb_blob_t *GetFontTable(uint32_t aTag) override;
+    hb_blob_t *GetFontTable(uint32_t aTag) override;
 
-    virtual void AddSizeOfIncludingThis(mozilla::MallocSizeOf aMallocSizeOf,
-                                        FontListSizes* aSizes) const override;
+    void AddSizeOfIncludingThis(mozilla::MallocSizeOf aMallocSizeOf,
+                                FontListSizes* aSizes) const override;
 
     nsresult ReadCMAP(FontInfoData *aFontInfoData = nullptr) override;
 
@@ -57,9 +57,10 @@ public:
     bool IsCFF();
 
 protected:
-    virtual gfxFont* CreateFontInstance(const gfxFontStyle *aFontStyle, bool aNeedsBold) override;
+    gfxFont* CreateFontInstance(const gfxFontStyle *aFontStyle,
+                                bool aNeedsBold) override;
 
-    virtual bool HasFontTable(uint32_t aTableTag) override;
+    bool HasFontTable(uint32_t aTableTag) override;
 
     static void DestroyBlobFunc(void* aUserData);
 
@@ -120,7 +121,7 @@ public:
         InfallibleTArray<mozilla::dom::FontFamilyListEntry>* aList);
 
 protected:
-    virtual gfxFontFamily*
+    gfxFontFamily*
     GetDefaultFontForPlatform(const gfxFontStyle* aStyle) override;
 
 private:
@@ -130,7 +131,7 @@ private:
     virtual ~gfxMacPlatformFontList();
 
     
-    virtual nsresult InitFontListForPlatform() override;
+    nsresult InitFontListForPlatform() override;
 
     
     void InitSingleFaceList();
@@ -141,11 +142,12 @@ private:
     
     gfxFontFamily* FindSystemFontFamily(const nsAString& aFamily);
 
-    static void RegisteredFontsChangedNotificationCallback(CFNotificationCenterRef center,
-                                                           void *observer,
-                                                           CFStringRef name,
-                                                           const void *object,
-                                                           CFDictionaryRef userInfo);
+    static void
+    RegisteredFontsChangedNotificationCallback(CFNotificationCenterRef center,
+                                               void *observer,
+                                               CFStringRef name,
+                                               const void *object,
+                                               CFDictionaryRef userInfo);
 
     
     gfxFontEntry* GlobalFontFallback(const uint32_t aCh,
