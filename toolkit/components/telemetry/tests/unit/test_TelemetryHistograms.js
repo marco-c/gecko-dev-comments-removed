66b14b0493104ee41bd60a2ec1cdd3451578852e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 16:55:07 +0000

    Backed out changeset 3bb2c974b5dc due to Browser Console spamming (bug 1334469)
    UltraBlame original commit: 4134c04c80285c24471ad44f5197af40c8c9eeec

--- toolkit/components/telemetry/tests/unit/test_TelemetryHistograms.js
+++ toolkit/components/telemetry/tests/unit/test_TelemetryHistograms.js
@@ -487,15 +487,6 @@ add_task(function* test_keyed_histogram() {
     threw = true;
   }
   Assert.ok(threw, "getKeyedHistogramById should have thrown");
-
-  
-  const KEYED_ID = "TELEMETRY_TEST_KEYED_COUNT";
-  let h = Telemetry.getKeyedHistogramById(KEYED_ID);
-
-  
-  h.add("");
-  Assert.ok(!("" in h.snapshot()),
-            "Keyed histogram should not allow empty keys.");
 });
 
 add_task(function* test_keyed_boolean_histogram() {
