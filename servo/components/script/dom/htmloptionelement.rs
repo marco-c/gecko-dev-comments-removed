d2ab562453421f51fb37fbe5673d87d783c6cbb6
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 10:31:26 +0000

    servo: Merge #7745 - Implement <option> 'defaultSelected' and 'selected' attributes (from frewsxcv:html-option-element-selected); r=nox
    Continued from #7743
    
    Source-Repo: https://github.com/servo/servo
    Source-Revision: 8547d132f9a6f5499c437572ea5648c6baf2f621
    
    UltraBlame original commit: 97c9787adbfaa6cf72e79dd12412517a2329e299

--- servo/components/script/dom/htmloptionelement.rs
+++ servo/components/script/dom/htmloptionelement.rs
@@ -17,11 +17,18 @@ use dom::eventtarget::{EventTarget, EventTargetTypeId};
 use dom::htmlelement::{HTMLElement, HTMLElementTypeId};
 use dom::node::{Node, NodeTypeId};
 use dom::virtualmethods::VirtualMethods;
+use std::cell::Cell;
 use util::str::{DOMString, split_html_space_chars};
 
 #[dom_struct]
 pub struct HTMLOptionElement {
-    htmlelement: HTMLElement
+    htmlelement: HTMLElement,
+
+    
+    selectedness: Cell<bool>,
+
+    
+    dirtiness: Cell<bool>,
 }
 
 impl HTMLOptionElementDerived for EventTarget {
@@ -38,7 +45,9 @@ impl HTMLOptionElement {
                      document: &Document) -> HTMLOptionElement {
         HTMLOptionElement {
             htmlelement:
-                HTMLElement::new_inherited(HTMLElementTypeId::HTMLOptionElement, localName, prefix, document)
+                HTMLElement::new_inherited(HTMLElementTypeId::HTMLOptionElement, localName, prefix, document),
+            selectedness: Cell::new(false),
+            dirtiness: Cell::new(false),
         }
     }
 
@@ -122,6 +131,23 @@ impl HTMLOptionElementMethods for HTMLOptionElement {
     // https://html.spec.whatwg.org/multipage/#attr-option-label
     make_setter!(SetLabel, "label");
 
+    // https://html.spec.whatwg.org/multipage/#dom-option-defaultselected
+    make_bool_getter!(DefaultSelected, "selected");
+
+    // https://html.spec.whatwg.org/multipage/#dom-option-defaultselected
+    make_bool_setter!(SetDefaultSelected, "selected");
+
+    
+    fn Selected(&self) -> bool {
+        self.selectedness.get()
+    }
+
+    
+    fn SetSelected(&self, selected: bool) {
+        self.dirtiness.set(true);
+        self.selectedness.set(selected);
+        
+    }
 }
 
 impl VirtualMethods for HTMLOptionElement {
@@ -145,6 +171,22 @@ impl VirtualMethods for HTMLOptionElement {
                         node.set_enabled_state(true);
                         node.check_parent_disabled_state_for_option();
                     }
+                }
+            },
+            &atom!(selected) => {
+                match mutation {
+                    AttributeMutation::Set(_) => {
+                        
+                        if !self.dirtiness.get() {
+                            self.selectedness.set(true);
+                        }
+                    },
+                    AttributeMutation::Removed => {
+                        
+                        if !self.dirtiness.get() {
+                            self.selectedness.set(false);
+                        }
+                    },
                 }
             },
             _ => {},
