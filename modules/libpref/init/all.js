f4fbd75b838f26b76313fb626a44ad5d16f6d999
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 16:45:30 +0000

    Merge m-i to m-c, a=merge
    UltraBlame original commit: d84beb192e57e26846c82d3df3599381f4663792

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -1257,7 +1257,7 @@ pref("privacy.trackingprotection.lower_network_priority",  false);
 
 pref("dom.event.contextmenu.enabled",       true);
 pref("dom.event.clipboardevents.enabled",   true);
-#if defined(XP_WIN) && !defined(RELEASE_OR_BETA) || defined(MOZ_WIDGET_GTK) && !defined(RELEASE_OR_BETA) || defined(XP_MACOSX) && !defined(RELEASE_OR_BETA)
+#if defined(XP_WIN) && !defined(RELEASE_OR_BETA) || defined(MOZ_WIDGET_GTK) && !defined(RELEASE_OR_BETA) || defined(XP_MACOSX) && !defined(RELEASE_OR_BETA) || defined(MOZ_WIDGET_ANDROID) && !defined(RELEASE_OR_BETA)
 pref("dom.event.highrestimestamp.enabled",  true);
 #else
 pref("dom.event.highrestimestamp.enabled",  false);
@@ -1487,6 +1487,7 @@ pref("network.http.accept.default", "text/html,application/xhtml+xml,application
 
 
 pref("network.http.sendRefererHeader",      2);
+
 
 
 pref("network.http.referer.userControlPolicy", 3);
