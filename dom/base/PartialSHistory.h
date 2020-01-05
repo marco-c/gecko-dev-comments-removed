809e3a4c2cea06eab7c3e48c493aac9817aa3401
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 05:22:56 +0000

    Bug 1315105 - Part 1: Add support for prerendering PartialSHistories to GroupedSHistory, r=smaug
    MozReview-Commit-ID: A5bwSy8NkH3
    
    UltraBlame original commit: ab6c012704b9552a14d44d5ab1ec95e81ba3f7d2

--- dom/base/PartialSHistory.h
+++ dom/base/PartialSHistory.h
@@ -57,7 +57,13 @@ private:
   uint32_t mGlobalIndexOffset;
 
   
+  int32_t mActive;
+
+  
   nsCOMPtr<nsIFrameLoader> mOwnerFrameLoader;
+
+  
+  nsCOMPtr<nsIGroupedSHistory> mGroupedSHistory;
 };
 
 } 
