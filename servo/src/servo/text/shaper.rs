d8267f7a511338afc2e23f01945ab52462ab0471
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 08:19:23 +0000

    servo: Print text to screen. Aw, yeah.
    Source-Repo: https://github.com/servo/servo
    Source-Revision: 8d2eb11582714f52c857e142b57430834634bf0c
    
    UltraBlame original commit: dbc8fc15e4d88643509be0b665500f12bf09e21a

--- servo/src/servo/text/shaper.rs
+++ servo/src/servo/text/shaper.rs
@@ -156,5 +156,5 @@ fn should_get_glyph_h_advance() {
     let font = font::create_test_font();
     let glyphs = shape_text(&font, "firecracker");
     
-    assert glyphs.all { |glyph| glyph.pos.advance.x == px_to_au(10) };
+    assert glyphs.all { |glyph| glyph.pos.advance.x == px_to_au(20) };
 }
