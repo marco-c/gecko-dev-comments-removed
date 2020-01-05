768345fb8d6214525c32bb03bfab5c0cd275e50c
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 23:59:48 +0000

    Bug 1019450 - Cleanup test_promiseBookmarksTree. r=mak
    MozReview-Commit-ID: C7Zp44toHkX
    
    UltraBlame original commit: 318c40d7d0d8b526ff6636071718cdc51116891b

--- toolkit/components/places/tests/head_common.js
+++ toolkit/components/places/tests/head_common.js
@@ -929,3 +929,28 @@ async function compareFavicons(icon1, icon2, msg) {
   Assert.ok(data2.length > 0, "Should fetch icon data");
   Assert.deepEqual(data1, data2, msg);
 }
+
+
+
+
+
+
+
+
+
+
+function mapItemIdToInternalRootName(aItemId) {
+  switch (aItemId) {
+    case PlacesUtils.placesRootId:
+      return "placesRoot";
+    case PlacesUtils.bookmarksMenuFolderId:
+      return "bookmarksMenuFolder";
+    case PlacesUtils.toolbarFolderId:
+      return "toolbarFolder";
+    case PlacesUtils.unfiledBookmarksFolderId:
+      return "unfiledBookmarksFolder";
+    case PlacesUtils.mobileFolderId:
+      return "mobileFolder";
+  }
+  return null;
+}
