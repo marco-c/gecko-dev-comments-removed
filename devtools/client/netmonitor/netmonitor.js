d910e00c946373b99491bb23e8245c8c2f7cfeb7
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 17:22:18 +0000

    Bug 1340464 - PART 1: wrap init process into bootstrap function;r=Honza
    MozReview-Commit-ID: 53z2XDkEiVv
    
    UltraBlame original commit: 44fc24be2928d4975a054db66281689a1e47ee83

--- devtools/client/netmonitor/netmonitor.js
+++ devtools/client/netmonitor/netmonitor.js
@@ -2,29 +2,21 @@
 
 
 
+
+
 "use strict";
 
 const { BrowserLoader } = Components.utils.import("resource://devtools/client/shared/browser-loader.js", {});
 
-function Netmonitor(toolbox) {
-  const require = window.windowRequire = BrowserLoader({
-    baseURI: "resource://devtools/client/netmonitor/",
-    window,
-    commonLibRequire: toolbox.browserRequire,
-  }).require;
+var Netmonitor = {
+  bootstrap: ({ tabTarget, toolbox }) => {
+    const require = window.windowRequire = BrowserLoader({
+      baseURI: "resource://devtools/client/netmonitor/",
+      window,
+      commonLibRequire: toolbox.browserRequire,
+    }).require;
 
-  
-  const EventEmitter = require("devtools/shared/event-emitter");
-  EventEmitter.decorate(window);
-
-  window.NetMonitorController = require("./netmonitor-controller").NetMonitorController;
-  window.NetMonitorController._toolbox = toolbox;
-  window.NetMonitorController._target = toolbox.target;
-}
-
-Netmonitor.prototype = {
-  init() {
-    const require = window.windowRequire;
+    const EventEmitter = require("devtools/shared/event-emitter");
     const { createFactory } = require("devtools/client/shared/vendor/react");
     const { render } = require("devtools/client/shared/vendor/react-dom");
     const Provider = createFactory(require("devtools/client/shared/vendor/react-redux").Provider);
@@ -32,21 +24,28 @@ Netmonitor.prototype = {
     
     const NetworkMonitor = createFactory(require("./components/network-monitor"));
 
-    this.networkMonitor = document.querySelector(".root");
+    
+    EventEmitter.decorate(window);
+
+    window.NetMonitorController = require("./netmonitor-controller").NetMonitorController;
+    window.NetMonitorController._toolbox = toolbox;
+    window.NetMonitorController._target = tabTarget;
+
+    this.root = document.querySelector(".root");
 
     render(Provider(
       { store: window.gStore },
-      NetworkMonitor({ toolbox: window.NetMonitorController._toolbox }),
-    ), this.networkMonitor);
+      NetworkMonitor(),
+    ), this.root);
 
     return window.NetMonitorController.startupNetMonitor();
   },
 
-  destroy() {
+  destroy: () => {
     const require = window.windowRequire;
     const { unmountComponentAtNode } = require("devtools/client/shared/vendor/react-dom");
 
-    unmountComponentAtNode(this.networkMonitor);
+    unmountComponentAtNode(this.root);
 
     return window.NetMonitorController.shutdownNetMonitor();
   }
