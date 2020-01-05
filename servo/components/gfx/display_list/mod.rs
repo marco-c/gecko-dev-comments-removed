618a25a01caf96ee7d4d5579e79aa4db9f01c16b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 09:54:19 +0000

    servo: Merge #5270 - Fix hit testing with stacking contexts (apparent since transforms landed) (from glennw:hit-test-stacking-context); r=pcwalton
    Source-Repo: https://github.com/servo/servo
    Source-Revision: fef439726aea26ef631f414c4852fa4cb49fff15
    
    UltraBlame original commit: 67044b11bb5a465a10a5af4f6e12606a05e45fa8

--- servo/components/gfx/display_list/mod.rs
+++ servo/components/gfx/display_list/mod.rs
@@ -469,6 +469,9 @@ impl StackingContext {
             }
         }
 
+        
+        point = point - self.bounds.origin;
+
         debug_assert!(!topmost_only || result.is_empty());
         let frac_point = self.transform.transform_point(&Point2D(point.x.to_frac32_px(),
                                                                  point.y.to_frac32_px()));
