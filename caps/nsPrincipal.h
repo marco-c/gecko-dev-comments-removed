4eb8093de31f37baf0ff8a034b3f2e245bcb8246
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 17:46:50 +0000

    Bug 1340710 - Part 1: Remove nsPrincipal::SetURI() since it's unused; r=bholley
    UltraBlame original commit: 2c7b8a9a8989e98cb08aa1cf8317c5f2daab2bc5

--- caps/nsPrincipal.h
+++ caps/nsPrincipal.h
@@ -36,7 +36,6 @@ public:
                 const mozilla::OriginAttributes& aOriginAttributes);
 
   virtual nsresult GetScriptLocation(nsACString& aStr) override;
-  void SetURI(nsIURI* aURI);
 
   
 
