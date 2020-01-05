24459a2362d35a6bb2b7859a8ac30ac76becf65c
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 01:54:11 +0000

    Bug 1299929 - Part 2: Add extra info for VRController; r=kip
    MozReview-Commit-ID: 5y8X5ZPnsHb
    
    UltraBlame original commit: 031f5a617e7a6c88179bea035ed18dcf230c1ac4

--- gfx/vr/gfxVROpenVR.h
+++ gfx/vr/gfxVROpenVR.h
@@ -92,9 +92,14 @@ class VRControllerOpenVR : public VRControllerHost
 {
 public:
   explicit VRControllerOpenVR();
+  void SetTrackedIndex(uint32_t aTrackedIndex);
+  uint32_t GetTrackedIndex();
 
 protected:
   virtual ~VRControllerOpenVR();
+
+  
+  uint32_t mTrackedIndex;
 };
 
 } 
