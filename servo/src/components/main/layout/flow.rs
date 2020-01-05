6d50263402866a7bf34b61429049d4cd86521843
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 09:01:17 +0000

    servo: Merge #1681 - Implement `position: absolute` for non-replaced elements (from pradeep90:absolute-position); r=pcwalton
    + Re-implement fixed positioning using the absolute positioning code.
    + Add reftests for absolute positioning and fixed positioning.
    + Refactor assign_widths in BlockFlow to isolate the calculation of
    widths and margins.
    + Pass down details of the Containing Block for absolute and fixed flows
    during layout. Use it to calculate the static position of absolute flows.
    + Defer calculation of absolute flow dimensions till we build the
    display list.
    
    This implements https://github.com/mozilla/servo/issues/1537 and https://github.com/mozilla/servo/issues/787
    
    Source-Repo: https://github.com/servo/servo
    Source-Revision: ada9224d0ef5b403562ebaaeb2e5f66729ffc589
    
    UltraBlame original commit: e0ea27b18ffa174cf01fa1716652bc5f42f4fa3e

--- servo/src/components/main/layout/flow.rs
+++ servo/src/components/main/layout/flow.rs
@@ -26,9 +26,10 @@
 
 
 use css::node_style::StyledNode;
-use layout::block::BlockFlow;
+use layout::block::{BlockFlow};
 use layout::box_::Box;
 use layout::context::LayoutContext;
+use layout::construct::OptVector;
 use layout::display_list_builder::{DisplayListBuilder, ExtraDisplayListData};
 use layout::floats::Floats;
 use layout::incremental::RestyleDamage;
@@ -45,12 +46,15 @@ use geom::rect::Rect;
 use gfx::display_list::{ClipDisplayItemClass, DisplayListCollection, DisplayList};
 use layout::display_list_builder::ToGfxColor;
 use gfx::color::Color;
+use servo_util::smallvec::{SmallVec, SmallVec0};
 use servo_util::geometry::Au;
 use std::cast;
 use std::cell::RefCell;
 use std::sync::atomics::Relaxed;
+use std::vec::VecMutIterator;
+use std::iter::Zip;
 use style::ComputedValues;
-use style::computed_values::text_align;
+use style::computed_values::{text_align, position};
 
 
 
@@ -115,6 +119,63 @@ pub trait Flow {
 
     
     fn mark_as_root(&mut self) {}
+
+    
+    
+    
+    
+
+    
+    
+    
+    fn is_store_overflow_delayed(&mut self) -> bool {
+        false
+    }
+
+    fn is_root(&self) -> bool {
+        false
+    }
+
+    fn is_float(&self) -> bool {
+        false
+    }
+
+    
+    fn positioning(&self) -> position::T {
+        position::static_
+    }
+
+    
+    fn is_fixed(&self) -> bool {
+        self.positioning() == position::fixed
+    }
+
+    fn is_positioned(&self) -> bool {
+        self.is_relatively_positioned() || self.is_absolutely_positioned()
+    }
+
+    fn is_relatively_positioned(&self) -> bool {
+        self.positioning() == position::relative
+    }
+
+    fn is_absolutely_positioned(&self) -> bool {
+        self.positioning() == position::absolute || self.is_fixed()
+    }
+
+    
+    fn is_root_of_absolute_flow_tree(&self) -> bool {
+        false
+    }
+
+    
+    fn generated_cb_size(&self) -> Size2D<Au> {
+        fail!("generated_cb_size not yet implemented")
+    }
+
+    
+    fn generated_cb_position(&self) -> Point2D<Au> {
+        fail!("this is not the CB-generating flow you're looking for")
+    }
 
     
     fn debug_str(&self) -> ~str {
@@ -208,6 +269,7 @@ pub trait MutableFlowUtils {
                           self,
                           builder: &DisplayListBuilder,
                           container_block_size: &Size2D<Au>,
+                          absolute_cb_abs_position: Point2D<Au>,
                           dirty: &Rect<Au>,
                           index: uint,
                           mut list: &RefCell<DisplayListCollection<E>>)
@@ -229,6 +291,16 @@ pub trait MutableOwnedFlowUtils {
     /// All flows must be finished at some point, or they will not have their intrinsic widths
     /// properly computed. (This is not, however, a memory safety problem.)
     fn finish(&mut self, context: &mut LayoutContext);
+
+    /// Set absolute descendants for this flow.
+    ///
+    /// Set this flow as the Containing Block for all the absolute descendants.
+    fn set_abs_descendants(&mut self, abs_descendants: AbsDescendants);
+
+    /// Set fixed descendants for this flow.
+    ///
+    /// Set yourself as the Containing Block for all the fixed descendants.
+    fn set_fixed_descendants(&mut self, fixed_descendants: AbsDescendants);
 
     /// Destroys the flow.
     fn destroy(&mut self);
@@ -483,6 +555,61 @@ impl FlowFlags {
     }
 }
 
+/// The Descendants of a flow.
+///
+/// Also, details about their position wrt this flow.
+/// FIXME: This should use @pcwalton's reference counting scheme (Coming Soon).
+pub struct Descendants {
+    /// Links to every Descendant.
+    descendant_links: SmallVec0<Rawlink>,
+    /// Static y offsets of all descendants from the start of this flow box.
+    static_y_offsets: SmallVec0<Au>,
+}
+
+impl Descendants {
+    pub fn new() -> Descendants {
+        Descendants {
+            descendant_links: SmallVec0::new(),
+            static_y_offsets: SmallVec0::new(),
+        }
+    }
+
+    pub fn len(&self) -> uint {
+        self.descendant_links.len()
+    }
+
+    pub fn push(&mut self, given_descendant: Rawlink) {
+        self.descendant_links.push(given_descendant);
+    }
+
+    /// Push the given descendants on to the existing descendants.
+    ///
+    /// Ignore any static y offsets, because they are None before layout.
+    pub fn push_descendants(&mut self, mut given_descendants: Descendants) {
+        for elem in given_descendants.descendant_links.move_iter() {
+            self.descendant_links.push(elem);
+        }
+    }
+
+    /// Return an iterator over the descendant flows.
+    pub fn iter<'a>(&'a mut self) -> DescendantIter<'a> {
+        self.descendant_links.mut_slice_from(0).mut_iter()
+    }
+
+    /// Return an iterator over (descendant, static y offset).
+    pub fn iter_with_offset<'a>(&'a mut self) -> DescendantOffsetIter<'a> {
+        self.descendant_links.mut_slice_from(0).mut_iter().zip(
+            self.static_y_offsets.mut_slice_from(0).mut_iter())
+    }
+}
+
+pub type AbsDescendants = Descendants;
+pub type FixedDescendants = Descendants;
+
+type DescendantIter<'a> = VecMutIterator<'a, Rawlink>;
+
+type DescendantOffsetIter<'a> = Zip<VecMutIterator<'a, Rawlink>, VecMutIterator<'a, Au>>;
+
 /// Data common to all flows.
 pub struct BaseFlow {
     restyle_damage: RestyleDamage,
@@ -498,8 +625,9 @@ pub struct BaseFlow {
     min_width: Au,
     pref_width: Au,
 
-    /// The position of the upper left corner of the border box of this flow, relative to the
-    /// containing block.
+    /// The upper left corner of the box representing this flow, relative to
+    /// the box representing its parent flow.
+    /// For absolute flows, this represents the position wrt to its Containing Block.
     position: Rect<Au>,
 
     /// The amount of overflow of this flow, relative to the containing block. Must include all the
@@ -514,11 +642,31 @@ pub struct BaseFlow {
     /// The floats next to this flow.
     floats: Floats,
 
-    /// The number of floated descendants of this flow (including this flow, if it's floated).
+    /// For normal flows, this is the number of floated descendants that are
+    /// not contained within any other floated descendant of this flow. For
+    /// floats, it is 1.
+    /// It is used to allocate float data if necessary and to
+    /// decide whether to do an in-order traversal for assign_height.
     num_floats: uint,
 
     /// The position of this flow in page coordinates, computed during display list construction.
     abs_position: Point2D<Au>,
+
+    /// Details about descendants with position 'absolute' for which we are
+    /// the CB. This is in tree order. This includes any direct children.
+    abs_descendants: AbsDescendants,
+    /// Details about descendants with position 'fixed'.
+    /// TODO: Optimize this, because this will be set only for the root.
+    fixed_descendants: FixedDescendants,
+
+    /// Offset wrt the nearest positioned ancestor - aka the Containing Block
+    /// for any absolutely positioned elements.
+    absolute_static_x_offset: Au,
+    /// Offset wrt the Initial Containing Block.
+    fixed_static_x_offset: Au,
+
+    /// Reference to the Containing Block, if this flow is absolutely positioned.
+    absolute_cb: Rawlink,
 
     /// Whether this flow has been destroyed.
     ///
@@ -576,6 +724,11 @@ impl BaseFlow {
             floats: Floats::new(),
             num_floats: 0,
             abs_position: Point2D(Au::new(0), Au::new(0)),
+            abs_descendants: Descendants::new(),
+            fixed_descendants: Descendants::new(),
+            absolute_static_x_offset: Au::new(0),
+            fixed_static_x_offset: Au::new(0),
+            absolute_cb: Rawlink::none(),
 
             destroyed: false,
 
@@ -707,15 +860,56 @@ impl<'a> MutableFlowUtils for &'a mut Flow {
         f(mut_base(self).children.back_mut())
     }
 
+    /// Calculate and set overflow for current flow.
+    ///
+    /// CSS Section 11.1
+    /// This is the union of rectangles of the flows for which we define the
+    /// Containing Block.
+    ///
+    /// Assumption: This is called in a bottom-up traversal, so kids' overflows have
+    /// already been set.
+    /// Assumption: Absolute descendants have had their overflow calculated.
     fn store_overflow(self, _: &mut LayoutContext) {
         let my_position = mut_base(self).position;
         let mut overflow = my_position;
-        for kid in mut_base(self).child_iter() {
-            let mut kid_overflow = base(kid).overflow;
-            kid_overflow = kid_overflow.translate(&my_position.origin);
-            overflow = overflow.union(&kid_overflow)
+
+        if self.is_block_container() {
+            for kid in child_iter(self) {
+                if kid.is_store_overflow_delayed() {
+                    // Absolute flows will be handled by their CB. If we are
+                    // their CB, they will show up in `abs_descendants`.
+                    continue;
+                }
+                let mut kid_overflow = base(kid).overflow;
+                kid_overflow = kid_overflow.translate(&my_position.origin);
+                overflow = overflow.union(&kid_overflow)
+            }
+
+            for descendant_link in mut_base(self).abs_descendants.iter() {
+                match descendant_link.resolve() {
+                    Some(flow) => {
+                        let mut kid_overflow = base(flow).overflow;
+                        kid_overflow = kid_overflow.translate(&my_position.origin);
+                        overflow = overflow.union(&kid_overflow)
+                    }
+                    None => fail!("empty Rawlink to a descendant")
+                }
+            }
+
+            if self.is_root() {
+                for fixed_descendant_link in mut_base(self).fixed_descendants.iter() {
+                    match fixed_descendant_link.resolve() {
+                        Some(flow) => {
+                            let mut kid_overflow = base(flow).overflow;
+                            kid_overflow = kid_overflow.translate(&my_position.origin);
+                            overflow = overflow.union(&kid_overflow)
+                        }
+                        None => fail!("empty Rawlink to a descendant")
+                    }
+                }
+            }
         }
-        mut_base(self).overflow = overflow
+        mut_base(self).overflow = overflow;
     }
 
     /// Push display items for current flow and its children onto `list`.
@@ -723,17 +917,29 @@ impl<'a> MutableFlowUtils for &'a mut Flow {
     /// For InlineFlow, add display items for all its boxes onto list`.
     /// For BlockFlow, add a ClipDisplayItemClass for itself and its children,
     /// plus any other display items like border.
+    ///
+    /// `container_block_size`: Size of the Containing Block for the current
+    /// flow. This is used for relative positioning (which resolves percentage
+    /// values for 'top', etc. after all Containing Block heights have been computed.)
+    /// `absolute_cb_abs_position`: Absolute position of the Containing Block
+    /// for the flow if it is absolutely positioned.
     fn build_display_lists<E:ExtraDisplayListData>(
                           self,
                           builder: &DisplayListBuilder,
                           container_block_size: &Size2D<Au>,
+                          absolute_cb_abs_position: Point2D<Au>,
                           dirty: &Rect<Au>,
                           mut index: uint,
                           lists: &RefCell<DisplayListCollection<E>>)
                           -> bool {
         debug!("Flow: building display list");
         index = match self.class() {
-            BlockFlowClass => self.as_block().build_display_list_block(builder, container_block_size, dirty, index, lists),
+            BlockFlowClass => self.as_block().build_display_list_block(builder,
+                                                                       container_block_size,
+                                                                       absolute_cb_abs_position,
+                                                                       dirty,
+                                                                       index,
+                                                                       lists),
             InlineFlowClass => self.as_inline().build_display_list_inline(builder, container_block_size, dirty, index, lists),
         };
 
@@ -742,24 +948,68 @@ impl<'a> MutableFlowUtils for &'a mut Flow {
         }
 
         if self.is_block_container() {
+            let block = self.as_block();
             let mut child_lists = DisplayListCollection::new();
             child_lists.add_list(DisplayList::new());
             let child_lists = RefCell::new(child_lists);
-            let container_block_size = match self.class() {
-                BlockFlowClass => {
-                    if self.as_block().box_.is_some() {
-                        self.as_block().box_.get_ref().border_box.get().size
-                    } else {
-                        base(self).position.size
-                    }
-                },
-                _ => {
-                    base(self).position.size
-                }
-            };
+            let container_block_size;
+            let abs_cb_position;
+            // TODO(pradeep): Move this into a generated CB function and stuff in Flow.
+            match block.box_ {
+                Some(ref box_) => {
+                    // FIXME: This should be the size of the content box (which is the
+                    // Containing Block formed by a BlockFlow), not the border box.
+                    container_block_size = box_.border_box.get().size;
 
-            for kid in child_iter(self) {
-                kid.build_display_lists(builder, &container_block_size, dirty, 0u, &child_lists);
+                    abs_cb_position = if block.is_positioned() {
+                        block.base.abs_position + block.generated_cb_position()
+                    } else {
+                        absolute_cb_abs_position
+                    };
+                }
+                None => fail!("Flow: block container should have a box_")
+            }
+
+            for kid in block.base.child_iter() {
+                if kid.is_absolutely_positioned() {
+                    // All absolute flows will be handled by their CB.
+                    continue;
+                }
+                kid.build_display_lists(builder, &container_block_size,
+                                        abs_cb_position,
+                                        dirty, 0u, &child_lists);
+            }
+
+            // TODO: Maybe we should handle position 'absolute' and 'fixed'
+            // descendants before normal descendants just in case there is a
+            // problem when display-list building is parallel and both the
+            // original parent and this flow access the same absolute flow.
+            // Note that this can only be done once we have paint order
+            // working cos currently the later boxes paint over the absolute
+            // and fixed boxes :|
+            for abs_descendant_link in block.base.abs_descendants.iter() {
+                match abs_descendant_link.resolve() {
+                    Some(flow) => {
+                        // TODO(pradeep): Send in your abs_position directly.
+                        flow.build_display_lists(builder, &container_block_size,
+                                                 abs_cb_position,
+                                                 dirty, 0u, &child_lists);
+                    }
+                    None => fail!("empty Rawlink to a descendant")
+                }
+            }
+
+            if block.is_root() {
+                for fixed_descendant_link in block.base.fixed_descendants.iter() {
+                    match fixed_descendant_link.resolve() {
+                        Some(flow) => {
+                            flow.build_display_lists(builder, &container_block_size,
+                                                     abs_cb_position,
+                                                     dirty, 0u, &child_lists);
+                        }
+                        None => fail!("empty Rawlink to a descendant")
+                    }
+                }
             }
 
             let mut child_lists = Some(child_lists.unwrap());
@@ -787,7 +1037,7 @@ impl<'a> MutableFlowUtils for &'a mut Flow {
         true
     }
 
-    
+    /// Destroys the flow.
     fn destroy(self) {
         for kid in child_iter(self) {
             kid.destroy()
@@ -798,7 +1048,7 @@ impl<'a> MutableFlowUtils for &'a mut Flow {
 }
 
 impl MutableOwnedFlowUtils for ~Flow {
-    
+    /// Adds a new flow as a child of this flow. Fails if this flow is marked as a leaf.
     fn add_new_child(&mut self, mut new_child: ~Flow) {
         {
             let kid_base = mut_base(new_child);
@@ -810,15 +1060,60 @@ impl MutableOwnedFlowUtils for ~Flow {
         let _ = base.parallel.children_count.fetch_add(1, Relaxed);
     }
 
-    
-    
-    
-    
-    
-    
+    /// Finishes a flow. Once a flow is finished, no more child flows or boxes may be added to it.
+    /// This will normally run the bubble-widths (minimum and preferred -- i.e. intrinsic -- width)
+    /// calculation, unless the global `bubble_widths_separately` flag is on.
+    ///
+    /// All flows must be finished at some point, or they will not have their intrinsic widths
+    /// properly computed. (This is not, however, a memory safety problem.)
     fn finish(&mut self, context: &mut LayoutContext) {
         if !context.opts.bubble_widths_separately {
             self.bubble_widths(context)
+        }
+    }
+
+    /// Set absolute descendants for this flow.
+    ///
+    /// Set yourself as the Containing Block for all the absolute descendants.
+    ///
+    /// Assumption: This is called in a bottom-up traversal, so that nothing
+    /// else is accessing the descendant flows.
+    fn set_abs_descendants(&mut self, abs_descendants: AbsDescendants) {
+        let self_link = Rawlink::some(*self);
+        let block = self.as_block();
+        block.base.abs_descendants = abs_descendants;
+
+        for descendant_link in block.base.abs_descendants.iter() {
+            match descendant_link.resolve() {
+                Some(flow) => {
+                    let base = mut_base(flow);
+                    base.absolute_cb = self_link.clone();
+                }
+                None => fail!("empty Rawlink to a descendant")
+            }
+        }
+    }
+
+    /// Set fixed descendants for this flow.
+    ///
+    /// Set yourself as the Containing Block for all the fixed descendants.
+    ///
+    /// Assumption: This is called in a bottom-up traversal, so that nothing
+    /// else is accessing the descendant flows.
+    /// Assumption: This is the root flow.
+    fn set_fixed_descendants(&mut self, fixed_descendants: FixedDescendants) {
+        let self_link = Rawlink::some(*self);
+        let block = self.as_block();
+        block.base.fixed_descendants = fixed_descendants;
+
+        for descendant_link in block.base.fixed_descendants.iter() {
+            match descendant_link.resolve() {
+                Some(flow) => {
+                    let base = mut_base(flow);
+                    base.absolute_cb = self_link.clone();
+                }
+                None => fail!("empty Rawlink to a descendant")
+            }
         }
     }
 
@@ -828,4 +1123,3 @@ impl MutableOwnedFlowUtils for ~Flow {
         self_borrowed.destroy();
     }
 }
-
