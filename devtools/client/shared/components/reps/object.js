02e9a461954a7ded7149ffd4780545642c298266
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 12:35:15 +0000

    Bug 1335050 - Allow customization of object rep's title generation. r=Honza
    MozReview-Commit-ID: G3SLHv8Kvyb
    
    UltraBlame original commit: fb66941fca4727647218e98faef001e57d632338

--- devtools/client/shared/components/reps/object.js
+++ devtools/client/shared/components/reps/object.js
@@ -26,20 +26,21 @@ define(function (require, exports, module) {
     displayName: "Obj",
 
     propTypes: {
-      object: React.PropTypes.object,
+      object: React.PropTypes.object.isRequired,
       
       mode: React.PropTypes.oneOf(Object.keys(MODE).map(key => MODE[key])),
       objectLink: React.PropTypes.func,
+      title: React.PropTypes.string,
     },
 
     getTitle: function (object) {
-      let className = object && object.class ? object.class : "Object";
+      let title = this.props.title || object.class || "Object";
       if (this.props.objectLink) {
         return this.props.objectLink({
           object: object
-        }, className);
+        }, title);
       }
-      return className;
+      return title;
     },
 
     safePropIterator: function (object, max) {
