306b6ec0d7c5bb18898ec547de471a84804e0e54
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 07:46:08 +0000

    Bug 1073952: inherit CSP into iframe sandbox srcdoc r=ckerschb,dveditz MozReview-Commit-ID: KTzCLoDfYnd
    UltraBlame original commit: 1e631015acc8513043c0a3ad1c4a31b50679f7b5

--- caps/nsNullPrincipal.cpp
+++ caps/nsNullPrincipal.cpp
@@ -107,6 +107,20 @@ nsNullPrincipal::GetHashValue(uint32_t *aResult)
 }
 
 NS_IMETHODIMP
+nsNullPrincipal::SetCsp(nsIContentSecurityPolicy* aCsp) {
+  
+  
+
+  MOZ_ASSERT(!mCSP, "do not destroy an existing CSP");
+  if (mCSP) {
+    return NS_ERROR_ALREADY_INITIALIZED;
+  }
+
+  mCSP = aCsp;
+  return NS_OK;
+}
+
+NS_IMETHODIMP
 nsNullPrincipal::GetURI(nsIURI** aURI)
 {
   return NS_EnsureSafeToReturn(mURI, aURI);
@@ -187,4 +201,3 @@ nsNullPrincipal::Write(nsIObjectOutputStream* aStream)
 
   return NS_OK;
 }
-
