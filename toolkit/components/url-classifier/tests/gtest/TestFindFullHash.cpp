3d58037c51df5fb0f9f0d4b1c05b4597400a449c
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 23:03:36 +0000

    Give a local anonymous struct a name, in order to work around a toolchain compiler deficiency of the browser rooting-hazard-analysis setup.  No bug, r=sfink over IRC
    UltraBlame original commit: 8ac493e856332d48eb215a0fa80a75bf6cd0582e

--- toolkit/components/url-classifier/tests/gtest/TestFindFullHash.cpp
+++ toolkit/components/url-classifier/tests/gtest/TestFindFullHash.cpp
@@ -123,7 +123,7 @@ void PopulateDuration(Duration& aDest, const MyDuration& aSrc)
 
 static MyDuration EXPECTED_MIN_WAIT_DURATION = { 12, 10 };
 static MyDuration EXPECTED_NEG_CACHE_DURATION = { 120, 9 };
-static const struct {
+static const struct ExpectedMatch {
   nsCString mCompleteHash;
   ThreatType mThreatType;
   MyDuration mPerHashCacheDuration;
