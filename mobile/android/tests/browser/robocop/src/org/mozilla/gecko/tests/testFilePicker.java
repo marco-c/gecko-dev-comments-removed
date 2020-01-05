22e97a01d0660159bde7bdcd1a9cb340d5ebde35
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 06:32:20 +0000

    Bug 1326317 - Convert FilePicker events to bundle events; r=sebastian
    Convert the "FilePicker:Show" event used in FilePicker and
    testFilePicker to use GeckoBundle/BundleEventListener. UI event is used
    because the listener performs Intent operations, which is best for the
    UI thread. "FilePicker:Result" notification is removed in favor of
    using a callback.
    
    UltraBlame original commit: 66b64c1105db7b1a07273c9d31c07cfd6652231c

--- mobile/android/tests/browser/robocop/src/org/mozilla/gecko/tests/testFilePicker.java
+++ mobile/android/tests/browser/robocop/src/org/mozilla/gecko/tests/testFilePicker.java
@@ -1,38 +1,30 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this file,
- * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+
+
 
 package org.mozilla.gecko.tests;
 
 import static org.mozilla.gecko.tests.helpers.AssertionHelper.fFail;
 
 import org.mozilla.gecko.EventDispatcher;
-import org.mozilla.gecko.GeckoApp;
-import org.mozilla.gecko.GeckoAppShell;
-import org.mozilla.gecko.util.GeckoEventListener;
+import org.mozilla.gecko.util.BundleEventListener;
+import org.mozilla.gecko.util.EventCallback;
+import org.mozilla.gecko.util.GeckoBundle;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
-public class testFilePicker extends JavascriptTest implements GeckoEventListener {
+public class testFilePicker extends JavascriptTest implements BundleEventListener {
     private static final String TEST_FILENAME = "/mnt/sdcard/my-favorite-martian.png";
 
     public testFilePicker() {
         super("testFilePicker.js");
     }
 
-    @Override
-    public void handleMessage(String event, final JSONObject message) {
-        // We handle the FilePicker message here so we can send back hard coded file information. We
-        // don't want to try to emulate "picking" a file using the Android intent chooser.
-        if (event.equals("FilePicker:Show")) {
-            try {
-                message.put("file", TEST_FILENAME);
-            } catch (JSONException ex) {
-                fFail("Can't add filename to message " + TEST_FILENAME);
-            }
-
-            mActions.sendGeckoEvent("FilePicker:Result", message.toString());
+    @Override 
+    public void handleMessage(final String event, final GeckoBundle message,
+                              final EventCallback callback) {
+        
+        
+        if ("FilePicker:Show".equals(event)) {
+            callback.sendSuccess(TEST_FILENAME);
         }
     }
 
@@ -40,13 +32,13 @@ public class testFilePicker extends JavascriptTest implements GeckoEventListener
     public void setUp() throws Exception {
         super.setUp();
 
-        GeckoApp.getEventDispatcher().registerGeckoThreadListener(this, "FilePicker:Show");
+        EventDispatcher.getInstance().registerUiThreadListener(this, "FilePicker:Show");
     }
 
     @Override
     public void tearDown() throws Exception {
         super.tearDown();
 
-        GeckoApp.getEventDispatcher().unregisterGeckoThreadListener(this, "FilePicker:Show");
+        EventDispatcher.getInstance().unregisterUiThreadListener(this, "FilePicker:Show");
     }
 }
