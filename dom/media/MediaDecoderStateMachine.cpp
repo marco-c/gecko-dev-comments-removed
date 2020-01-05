3ec41703221f11f668c356114c4c37aa0ae34816
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 02:57:36 +0000

    Bug 1309516 part 9 - make sure that MDSM::mDuration is always assigned once we have meatadata; r=jwwang
    MozReview-Commit-ID: KF7J1XGo2MD
    
    UltraBlame original commit: 9d1487ccc2e34cecc2ee040b81d5ea164b57e964

--- dom/media/MediaDecoderStateMachine.cpp
+++ dom/media/MediaDecoderStateMachine.cpp
@@ -1302,6 +1302,11 @@ DecodeMetadataState::OnMetadataRead(MetadataHolder* aMetadata)
     mMaster->RecomputeDuration();
   }
 
+  
+  if (mMaster->mDuration.Ref().isNothing()) {
+    mMaster->mDuration = Some(TimeUnit::FromInfinity());
+  }
+
   if (mMaster->HasVideo()) {
     SLOG("Video decode isAsync=%d HWAccel=%d videoQueueSize=%d",
          Reader()->IsAsync(),
@@ -2937,11 +2942,6 @@ MediaDecoderStateMachine::FinishDecodeFirstFrame()
   DECODER_LOG("FinishDecodeFirstFrame");
 
   mMediaSink->Redraw(Info().mVideo);
-
-  
-  if (mDuration.Ref().isNothing()) {
-    mDuration = Some(TimeUnit::FromInfinity());
-  }
 
   DECODER_LOG("Media duration %lld, "
               "transportSeekable=%d, mediaSeekable=%d",
