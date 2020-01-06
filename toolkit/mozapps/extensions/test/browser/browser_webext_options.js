9714cf1b17654ae6686cd625de1e85c1afe27ce6
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 05:24:52 +0000

    Bug 1357486: Part 0b - Fix inline options browser tests with OOP extensions. r=aswan
    MozReview-Commit-ID: Bc4hNFmVOlw
    
    UltraBlame original commit: 4e252b1de0d02d24067ac817d4d4de6351e4e039

--- toolkit/mozapps/extensions/test/browser/browser_webext_options.js
+++ toolkit/mozapps/extensions/test/browser/browser_webext_options.js
@@ -2,6 +2,8 @@
 
 
 
+Components.utils.import("resource://testing-common/ContentTask.jsm", {});
+
 
 
 
@@ -31,9 +33,11 @@ async function runTest(installer) {
   var browser = mgrWindow.document.querySelector("#detail-grid > rows > .inline-options-browser");
   var rows = browser.parentNode;
 
+  let url = await ContentTask.spawn(browser, {}, () => content.location.href);
+
   ok(browser, "Grid should have a browser child");
   is(browser.localName, "browser", "Grid should have a browser child");
-  is(browser.currentURI.spec, element.mAddon.optionsURL, "Browser has the expected options URL loaded")
+  is(url, element.mAddon.optionsURL, "Browser has the expected options URL loaded")
 
   is(browser.clientWidth, rows.clientWidth,
      "Browser should be the same width as its parent node");
