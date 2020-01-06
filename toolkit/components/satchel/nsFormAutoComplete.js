2b77d8ceee0685b90ca0008cdf69c4c185233cf2
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 02:46:11 +0000

    Bug 1371554 - Change .indexOf(...) == 0 to the more expressive .startsWith(...). r=mattn.
    MozReview-Commit-ID: Jr3ALp6v2rM
    
    UltraBlame original commit: 3b978477fa6994be455c98c9404405aff3cfa37a

--- toolkit/components/satchel/nsFormAutoComplete.js
+++ toolkit/components/satchel/nsFormAutoComplete.js
@@ -515,13 +515,19 @@ FormAutoComplete.prototype = {
     let boundaryCalc = 0;
     
     for (let token of searchTokens) {
-      boundaryCalc += (entry.textLowerCase.indexOf(token) == 0);
-      boundaryCalc += (entry.textLowerCase.includes(" " + token));
+      if (entry.textLowerCase.startsWith(token)) {
+        boundaryCalc++;
+      }
+      if (entry.textLowerCase.includes(" " + token)) {
+        boundaryCalc++;
+      }
     }
     boundaryCalc = boundaryCalc * this._boundaryWeight;
     
     
-    boundaryCalc += this._prefixWeight * (entry.textLowerCase.indexOf(aSearchString) == 0);
+    if (entry.textLowerCase.startsWith(aSearchString)) {
+      boundaryCalc += this._prefixWeight;
+    }
     entry.totalScore = Math.round(entry.frecency * Math.max(1, boundaryCalc));
   }
 
