4aae00fb3659130c429e27c10cd05c5b3e286b8b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 06:46:44 +0000

    Bug 1381577 - Part D; Add support for not passing --branch and --skip-buildbot-actions to mozharness tasks on windows workers. r=dustin Land date changes to support windows nightlies onto central
    MozReview-Commit-ID: BWngsoOaNzg
    
    UltraBlame original commit: fdb3b53456b2bd2f4e6e8ba60e40c940df41c7b9

--- taskcluster/taskgraph/transforms/job/mozharness.py
+++ taskcluster/taskgraph/transforms/job/mozharness.py
@@ -85,7 +85,12 @@ mozharness_run_schema = Schema({
     Required('requires-signed-builds', default=False): bool,
 
     
+    
     Required('use-simple-package', default=True): bool,
+
+    
+    
+    Required('use-magic-mh-args', default=True): bool,
 })
 
 
@@ -99,6 +104,9 @@ def mozharness_on_docker_worker_setup(config, job, taskdesc):
     if not run['use-simple-package']:
         raise NotImplementedError("Simple packaging cannot be disabled via"
                                   "'use-simple-package' on docker-workers")
+    if not run['use-magic-mh-args']:
+        raise NotImplementedError("Cannot disabled mh magic arg passing via"
+                                  "'use-magic-mh-args' on docker-workers")
 
     
     taskdesc['worker']['docker-image'] = {"in-tree": "desktop-build"}
@@ -229,8 +237,10 @@ def mozharness_on_generic_worker(config, job, taskdesc):
     mh_command.append('\\'.join([r'.\build\src\testing', run['script'].replace('/', '\\')]))
     for cfg in run['config']:
         mh_command.append('--config ' + cfg.replace('/', '\\'))
-    mh_command.append('--branch ' + config.params['project'])
-    mh_command.append(r'--skip-buildbot-actions --work-dir %cd:Z:=z:%\build')
+    if run['use-magic-mh-args']:
+        mh_command.append('--branch ' + config.params['project'])
+        mh_command.append(r'--skip-buildbot-actions')
+    mh_command.append(r'--work-dir %cd:Z:=z:%\build')
     for action in run.get('actions', []):
         mh_command.append('--' + action)
 
