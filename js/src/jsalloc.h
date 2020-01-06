ce4215542ade3f7775174e3a9e1427357331f4a5
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 16:32:52 +0000

    Backed out 2 changesets (bug 1404837, bug 1404787) for build bustage a=backout
    Backed out changeset ec4e1ade8cf2 (bug 1404837)
    Backed out changeset 5e41366119a2 (bug 1404787)
    
    MozReview-Commit-ID: 8fHMqSdDxzh
    
    UltraBlame original commit: a862d066b0aaba4044fc25de0be863127080cdb3

--- js/src/jsalloc.h
+++ js/src/jsalloc.h
@@ -51,6 +51,8 @@ class SystemAllocPolicy
     }
 };
 
+void ReportOutOfMemory(JSContext* cx);
+
 
 
 
@@ -129,7 +131,7 @@ class TempAllocPolicy
 
     bool checkSimulatedOOM() const {
         if (js::oom::ShouldFailWithOOM()) {
-            JS_ReportOutOfMemory(cx_);
+            ReportOutOfMemory(cx_);
             return false;
         }
 
