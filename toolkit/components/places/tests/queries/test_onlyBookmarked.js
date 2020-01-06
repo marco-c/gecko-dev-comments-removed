5f1d1033d8f46a8dbc254efd30845caf7d0950e4
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 04:45:11 +0000

    Bug 1360872 - Return empty strings for `null` bookmark titles. r=mak
    MozReview-Commit-ID: Dd2sEfYvnBt
    
    UltraBlame original commit: c0fd6c0590be4a936df313660ed5f875494d9148

--- toolkit/components/places/tests/queries/test_onlyBookmarked.js
+++ toolkit/components/places/tests/queries/test_onlyBookmarked.js
@@ -20,6 +20,7 @@ var testData = [
   
   { isBookmark: true,
     uri: "http://bookmarked.com/",
+    title: "",
     parentGuid: PlacesUtils.bookmarks.toolbarGuid,
     index: PlacesUtils.bookmarks.DEFAULT_INDEX,
     isInQuery: true },
@@ -27,6 +28,7 @@ var testData = [
   
   { isBookmark: true,
     uri: "http://bookmarked-elsewhere.com/",
+    title: "",
     parentGuid: PlacesUtils.bookmarks.menuGuid,
     index: PlacesUtils.bookmarks.DEFAULT_INDEX,
     isInQuery: false },
@@ -34,6 +36,7 @@ var testData = [
   
   { isVisit: true,
     uri: "http://notbookmarked.com/",
+    title: "",
     isInQuery: false }
 ];
 
@@ -68,6 +71,7 @@ add_task(async function test_onlyBookmarked() {
     
     { isBookmark: true,
       uri: "http://bookmarked2.com/",
+      title: "",
       parentGuid: PlacesUtils.bookmarks.toolbarGuid,
       index: PlacesUtils.bookmarks.DEFAULT_INDEX,
       isInQuery: true },
@@ -75,6 +79,7 @@ add_task(async function test_onlyBookmarked() {
     
     { isBookmark: true,
       uri: "http://bookmarked-elsewhere2.com/",
+      title: "",
       parentGuid: PlacesUtils.bookmarks.menuGuid,
       index: PlacesUtils.bookmarks.DEFAULT_INDEX,
       isInQuery: false }
