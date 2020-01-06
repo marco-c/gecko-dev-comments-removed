00916fc61e584b5a81be192ecc1a0ea7b990f36f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 18:47:52 +0000

    servo: Merge #18971 - Use env::var_os to read paths from the environment (from mbrubeck:var); r=emilio
    This avoids unnecessary UTF-8 validation on OsStrings that we just pass
    back to the OS.
    
    ---
    - [x] `./mach build -d` does not report any errors
    - [x] `./mach test-tidy` does not report any errors
    - [x] These changes do not require tests because they are code cleanup only
    
    Source-Repo: https://github.com/servo/servo
    Source-Revision: 5ab0ac162019aede12a8150118328566467fddf2
    
    UltraBlame original commit: 9e9e162ba1d80306371d51a682abcd0c12272700

--- servo/ports/servo/build.rs
+++ servo/ports/servo/build.rs
@@ -17,7 +17,7 @@ fn main() {
 
 fn android_main() {
     
-    let ndk_path = env::var("ANDROID_NDK").ok().expect("Please set the ANDROID_NDK environment variable");
+    let ndk_path = env::var_os("ANDROID_NDK").expect("Please set the ANDROID_NDK environment variable");
     let ndk_path = Path::new(&ndk_path);
 
     
@@ -62,7 +62,7 @@ fn android_main() {
     println!("toolchain path is: {}", toolchain_path.to_str().unwrap());
 
     
-    let out_dir = env::var("OUT_DIR").ok().expect("Cargo should have set the OUT_DIR environment variable");
+    let out_dir = env::var("OUT_DIR").expect("Cargo should have set the OUT_DIR environment variable");
     let directory = Path::new(&out_dir);
 
     
