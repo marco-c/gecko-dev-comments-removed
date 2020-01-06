15f1537879881ac621016d0dcb187e153b54f300
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 04:08:40 +0000

    Bug 1374494 - P2: Update cubeb-pulse-rs to commit djg/cubeb-pulse-rs/6451581. r=kinetik
    MozReview-Commit-ID: IscK7OpIZbB
    
    UltraBlame original commit: 5ff54dc9d0ad1bc6edc3a09704c6be8b611bc801

--- media/libcubeb/cubeb-pulse-rs/src/backend/mod.rs
+++ media/libcubeb/cubeb-pulse-rs/src/backend/mod.rs
@@ -7,8 +7,16 @@ mod context;
 mod cork_state;
 mod stream;
 
+use std::os::raw::c_char;
+use std::ffi::CStr;
+
 pub type Result<T> = ::std::result::Result<T, i32>;
 
 pub use self::context::Context;
 pub use self::stream::Device;
 pub use self::stream::Stream;
+
+
+fn try_cstr_from<'str>(s: *const c_char) -> Option<&'str CStr> {
+    if s.is_null() { None } else { Some(unsafe { CStr::from_ptr(s) }) }
+}
