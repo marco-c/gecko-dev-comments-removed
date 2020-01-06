9c93b11e4f1caea49403261df7f3997eff44650e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 11:55:15 +0000

    Bug 1302470 Part 2: Branch IsRangeVisible to delegate to IsRangeRendered when range is in viewport. r=masayuki
    MozReview-Commit-ID: LZyvt08I9vz
    
    UltraBlame original commit: 6b948c53394446f77cf68340671b0ba828057850

--- toolkit/components/typeaheadfind/nsTypeAheadFind.cpp
+++ toolkit/components/typeaheadfind/nsTypeAheadFind.cpp
@@ -1218,11 +1218,13 @@ nsTypeAheadFind::IsRangeVisible(nsIPresShell *aPresShell,
     return false;
 
   nsIFrame *frame = content->GetPrimaryFrame();
-  if (!frame)
+  if (!frame) {
     return false;  
+  }
 
-  if (!frame->StyleVisibility()->IsVisible())
+  if (!frame->StyleVisibility()->IsVisible()) {
     return false;
+  }
 
   
   
@@ -1232,8 +1234,6 @@ nsTypeAheadFind::IsRangeVisible(nsIPresShell *aPresShell,
   }
 
   
-  if (!aMustBeInViewPort)
-    return true; 
 
   
   int32_t startFrameOffset, endFrameOffset;
@@ -1269,8 +1269,18 @@ nsTypeAheadFind::IsRangeVisible(nsIPresShell *aPresShell,
                                     minDistance);
 
     if (rectVisibility != nsRectVisibility_kAboveViewport) {
-      return true;
+      
+      
+      return IsRangeRendered(aPresShell, aPresContext, aRange);
     }
+  }
+
+  
+
+  if (!aMustBeInViewPort) {
+    
+    
+    return true;
   }
 
   
@@ -1288,8 +1298,9 @@ nsTypeAheadFind::IsRangeVisible(nsIPresShell *aPresShell,
                             false  
                             );
 
-  if (!frameTraversal)
+  if (!frameTraversal) {
     return false;
+  }
 
   while (rectVisibility == nsRectVisibility_kAboveViewport) {
     frameTraversal->Next();
