501ac26cb628123a0f6cc097203044b5cf4e97af
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 13:45:14 +0000

    Bug 1399393 Refactor create_tasks to avoid bottlenecks r=dustin
    MozReview-Commit-ID: cJW5X3HSCx
    
    UltraBlame original commit: 45fe1c0506456d86919732e4223c1d0b2772e7f3

--- taskcluster/taskgraph/create.py
+++ taskcluster/taskgraph/create.py
@@ -47,52 +47,61 @@ def create_tasks(taskgraph, label_to_taskid, params, decision_task_id=None):
     task_group_id = decision_task_id or slugid()
     scheduler_id = 'gecko-level-{}'.format(params['level'])
 
+    
+    
+    for task_id in taskgraph.graph.nodes:
+        task_def = taskgraph.tasks[task_id].task
+
+        
+        
+        
+        
+        
+        if decision_task_id:
+            if not any(t in taskgraph.tasks for t in task_def.get('dependencies', [])):
+                task_def.setdefault('dependencies', []).append(decision_task_id)
+
+        task_def['taskGroupId'] = task_group_id
+        task_def['schedulerId'] = scheduler_id
+
     with futures.ThreadPoolExecutor(CONCURRENCY) as e:
         fs = {}
 
         
         
         
-        
-        
-        
-        
-        
-        
-        for task_id in taskgraph.graph.visit_postorder():
-            task_def = taskgraph.tasks[task_id].task
-            attributes = taskgraph.tasks[task_id].attributes
+        tasklist = set(taskgraph.graph.visit_postorder())
+        alltasks = tasklist.copy()
 
-            
-            
-            
-            
-            
-            if decision_task_id:
-                if not any(t in taskgraph.tasks for t in task_def.get('dependencies', [])):
-                    task_def.setdefault('dependencies', []).append(decision_task_id)
-
-            task_def['taskGroupId'] = task_group_id
-            task_def['schedulerId'] = scheduler_id
-
-            
-            deps_fs = [fs[dep] for dep in task_def.get('dependencies', [])
-                       if dep in fs]
-            for f in futures.as_completed(deps_fs):
-                f.result()
-
-            fs[task_id] = e.submit(create_task, session, task_id,
-                                   taskid_to_label[task_id], task_def)
-
-            
-            for i in range(1, attributes.get('task_duplicates', 1)):
+        def schedule_tasks(f=None):
+            to_remove = set()
+            for task_id in tasklist:
+                task_def = taskgraph.tasks[task_id].task
                 
-                fs[task_id] = e.submit(create_task, session, slugid(),
-                                       taskid_to_label[task_id], task_def)
+                
+                
+                
+                deps = set(task_def.get('dependencies', [])) & alltasks
+                if any((d not in fs or not fs[d].done()) for d in deps):
+                    continue
 
-        
-        for f in futures.as_completed(fs.values()):
-            f.result()
+                fs[task_id] = e.submit(create_task, session, task_id,
+                                       taskid_to_label[task_id], task_def)
+                to_remove.add(task_id)
+
+                
+                attributes = taskgraph.tasks[task_id].attributes
+                for i in range(1, attributes.get('task_duplicates', 1)):
+                    
+                    fs[task_id] = e.submit(create_task, session, slugid(),
+                                           taskid_to_label[task_id], task_def)
+            tasklist.difference_update(to_remove)
+
+        schedule_tasks()
+        while tasklist:
+            for f in futures.as_completed(fs.values()):
+                f.result()
+            schedule_tasks()
 
 
 def create_task(session, task_id, label, task_def):
