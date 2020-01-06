5b0c0dc3ce0c771093af4b68d5990f89ab8bc2d2
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 23:57:15 +0000

    Don't update content client if all we do is buffer operations. (bug 1419539, r=bas)
    PaintOffMainThread uses didUpdate to track whether we queued work on the paint
    thread and also if we painted new content into the buffer and need to update
    the content client. These are independent and should have different flags. We
    may flip the buffers incorrectly if we don't do this.
    
    This is also important because ContentClient can return a BufferState even if
    there are no operations and no backing buffer, causing a crash in Updated().
    
    MozReview-Commit-ID: C6OW3jDJ3B4
    
    UltraBlame original commit: 97fd4316ee229a7feaed11d593b68e5a443e5f90

--- gfx/layers/client/ClientPaintedLayer.cpp
+++ gfx/layers/client/ClientPaintedLayer.cpp
@@ -175,6 +175,28 @@ ClientPaintedLayer::PaintThebes(nsTArray<ReadbackProcessor::Update>* aReadbackUp
   }
 }
 
+class MOZ_RAII AutoQueuedAsyncPaint
+{
+public:
+  explicit AutoQueuedAsyncPaint(ClientLayerManager* aLayerManager)
+    : mLayerManager(aLayerManager)
+    , mQueuedAsyncPaints(false)
+  { }
+
+  void Queue() { mQueuedAsyncPaints = true; }
+
+  ~AutoQueuedAsyncPaint()
+  {
+    if (mQueuedAsyncPaints) {
+      mLayerManager->SetQueuedAsyncPaints();
+    }
+  }
+
+private:
+  ClientLayerManager* mLayerManager;
+  bool mQueuedAsyncPaints;
+};
+
 
 
 
@@ -201,23 +223,21 @@ ClientPaintedLayer::PaintThebes(nsTArray<ReadbackProcessor::Update>* aReadbackUp
 void
 ClientPaintedLayer::PaintOffMainThread()
 {
-  uint32_t flags = GetPaintFlags();
+  AutoQueuedAsyncPaint asyncPaints(ClientManager());
 
+  uint32_t flags = GetPaintFlags();
   PaintState state = mContentClient->BeginPaint(this, flags | ContentClient::PAINT_ASYNC);
-  bool didUpdate = false;
 
   if (state.mBufferState) {
     PaintThread::Get()->PrepareBuffer(state.mBufferState);
-    didUpdate = true;
+    asyncPaints.Queue();
   }
 
   if (!UpdatePaintRegion(state)) {
-    if (didUpdate) {
-      ClientManager()->SetQueuedAsyncPaints();
-    }
     return;
   }
 
+  bool didUpdate = false;
   RotatedBuffer::DrawIterator iter;
 
   
@@ -259,6 +279,7 @@ ClientPaintedLayer::PaintOffMainThread()
 
     mContentClient->ReturnDrawTarget(target);
 
+    asyncPaints.Queue();
     didUpdate = true;
   }
 
@@ -267,9 +288,7 @@ ClientPaintedLayer::PaintOffMainThread()
 
   if (didUpdate) {
     UpdateContentClient(state);
-    ClientManager()->SetQueuedAsyncPaints();
   }
-  return;
 }
 
 void
