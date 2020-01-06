0d5cef0f8a27adaa1eb94438de4b457b19bf64c0
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 18:45:35 +0000

    Bug 1399453 - Temporarily annotate crash reports with compositor thread owners. r=dvander
    From the crashes associated with bug 1389021, we know that some
    compositor thread IPDL owners are not being cleaned up properly. We do
    not know which protocols are causing the problem, so we temporarily will
    annotate the logs with the ownership status. This should be limited to
    under a dozen instances of the protocols.
    
    This change will be backed out after a few builds are produced with it
    and we see the first crash reports with the relevant data.
    
    UltraBlame original commit: dc9d9c01080a3a9d36af97706a0ec612663c48b0

--- gfx/layers/ipc/ImageBridgeParent.cpp
+++ gfx/layers/ipc/ImageBridgeParent.cpp
@@ -66,9 +66,12 @@ ImageBridgeParent::ImageBridgeParent(MessageLoop* aLoop,
   : mMessageLoop(aLoop)
   , mSetChildThreadPriority(false)
   , mClosed(false)
-  , mCompositorThreadHolder(CompositorThreadHolder::GetSingleton())
 {
   MOZ_ASSERT(NS_IsMainThread());
+
+  mCompositorThreadHolder =
+    new CompositorThreadHolderDebug(IsSameProcess() ? "ImageBridgeSame"
+                                                    : "ImageBridge");
 
   
   
