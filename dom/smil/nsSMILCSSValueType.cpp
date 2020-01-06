18e749ca4301e69af53aa08b357e2c033436d89d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 02:15:29 +0000

    Bug 1355349 - Reformat a ternary operator to match coding style; r=hiro
    MozReview-Commit-ID: KUkqXMmzgR8
    
    UltraBlame original commit: c0ef218a7f9eed89290381a22975e55355ea3474

--- dom/smil/nsSMILCSSValueType.cpp
+++ dom/smil/nsSMILCSSValueType.cpp
@@ -260,8 +260,9 @@ nsSMILCSSValueType::Add(nsSMILValue& aDest, const nsSMILValue& aValueToAdd,
   MOZ_ASSERT(destWrapper || valueToAddWrapper,
              "need at least one fully-initialized value");
 
-  nsCSSPropertyID property = (valueToAddWrapper ? valueToAddWrapper->mPropID :
-                            destWrapper->mPropID);
+  nsCSSPropertyID property = valueToAddWrapper
+                             ? valueToAddWrapper->mPropID
+                             : destWrapper->mPropID;
   
   
   if (property == eCSSProperty_font_size_adjust ||
