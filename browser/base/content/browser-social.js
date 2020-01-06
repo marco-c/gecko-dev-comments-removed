2c30a951dbf57a2aabb83fffaddebd1ce15681b8
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 01:52:08 +0000

    Backed out changeset a311d7c6ce20 (bug 1363975)
    UltraBlame original commit: de5a1808d7fd7b9d946a519e2068ee6bdc0da0dd

--- browser/base/content/browser-social.js
+++ browser/base/content/browser-social.js
@@ -13,9 +13,6 @@ var SocialUI,
 (function() {
 "use strict";
 
-XPCOMUtils.defineLazyModuleGetter(this, "Utils",
-  "resource://gre/modules/sessionstore/Utils.jsm");
-
 XPCOMUtils.defineLazyGetter(this, "OpenGraphBuilder", function() {
   let tmp = {};
   Cu.import("resource:///modules/Social.jsm", tmp);
@@ -206,11 +203,7 @@ SocialActivationListener = {
         if (provider.postActivationURL) {
           
           
-          let triggeringPrincipal = Utils.deserializePrincipal(aMessage.data.triggeringPrincipal);
-          gBrowser.loadOneTab(provider.postActivationURL, {
-            inBackground: SocialShare.panel.state == "open",
-            triggeringPrincipal,
-          });
+          gBrowser.loadOneTab(provider.postActivationURL, {inBackground: SocialShare.panel.state == "open"});
         }
       });
     }, options);
