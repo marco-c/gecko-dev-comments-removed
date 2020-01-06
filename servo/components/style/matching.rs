93f4e3165b2993f3c465421c392c0165b5cdd566
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 14:58:50 +0000

    servo: Merge #18624 - style: Early return and skip duplicate assertion in matching.rs (from emilio:easier-is-better); r=nox
    Source-Repo: https://github.com/servo/servo
    Source-Revision: 30e007ba1a537c7721bb824f4ddf4d915804b087
    
    UltraBlame original commit: 778b195af350a9228226e3c3d525a2aad68eaf72

--- servo/components/style/matching.rs
+++ servo/components/style/matching.rs
@@ -186,8 +186,9 @@ trait PrivateMatchMethods: TElement {
         use context::DISPLAY_CHANGED_FROM_NONE_FOR_SMIL;
         use properties::longhands::display::computed_value as display;
 
-        debug_assert!(restyle_hints.intersects(RESTYLE_SMIL),
-                      "Should have restyle hint for SMIL");
+        if !restyle_hints.intersects(RESTYLE_SMIL) {
+            return;
+        }
 
         let display_changed_from_none = old_values.map_or(false, |old| {
             let old_display_style = old.get_box().clone_display();
@@ -203,10 +204,10 @@ trait PrivateMatchMethods: TElement {
             
             
             
-            debug_assert!(restyle_hints.intersects(RESTYLE_SMIL),
-                          "Display animation should only happen for SMIL");
-            let task = ::context::SequentialTask::process_post_animation(*self,
-                                                                         DISPLAY_CHANGED_FROM_NONE_FOR_SMIL);
+            let task = ::context::SequentialTask::process_post_animation(
+                *self,
+                DISPLAY_CHANGED_FROM_NONE_FOR_SMIL,
+            );
             context.thread_local.tasks.push(task);
         }
     }
@@ -222,12 +223,12 @@ trait PrivateMatchMethods: TElement {
         use context::UpdateAnimationsTasks;
 
         if context.shared.traversal_flags.for_animation_only() {
-            if restyle_hint.intersects(RESTYLE_SMIL) {
-                self.handle_display_change_for_smil_if_needed(context,
-                                                              old_values.as_ref().map(|v| &**v),
-                                                              new_values,
-                                                              restyle_hint);
-            }
+            self.handle_display_change_for_smil_if_needed(
+                context,
+                old_values.as_ref().map(|v| &**v),
+                new_values,
+                restyle_hint,
+            );
             return;
         }
 
