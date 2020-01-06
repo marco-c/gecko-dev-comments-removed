74a5044b5102e7db742ff6d46529489a7442b4bb
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 09:02:07 +0000

    Bug 1382968 - Flag Browser Console target as being chrome and related to a TabActor. r=jryans
    The Browser Console interacts with a ChromeActor instance, which as any TabActor inherited actor,
    expects to be "attached" by calling its `attach` request. isTabActor set to true ensures that.
    While chrome set to true allows client codebase to enable additional behavior for chrome debugging.
    
    MozReview-Commit-ID: 1MVLBKnluhg
    
    UltraBlame original commit: d0b89a730143d4590aba70d7890342ba3883bab7

--- devtools/client/webconsole/hudservice.js
+++ devtools/client/webconsole/hudservice.js
@@ -191,8 +191,7 @@ HUD_SERVICE.prototype =
         .then(() => client.getProcess())
         .then(aResponse => {
           
-          
-          return { form: aResponse.form, client: client, chrome: false };
+          return { form: aResponse.form, client, chrome: true, isTabActor: true };
         });
     }
 
