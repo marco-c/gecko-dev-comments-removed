2a43a7674023d50d9b2ba753ddc935d2c9cc2677
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 08:48:03 +0000

    Bug 1386680 - irregexp: reduce size of initial evaluation stack.  r=bhackett.
    UltraBlame original commit: 42fdaa0b3066bca39a9466383e259976b365b068

--- js/src/irregexp/RegExpStack.h
+++ js/src/irregexp/RegExpStack.h
@@ -90,7 +90,7 @@ class RegExpStack
     static const uintptr_t kMemoryTop = static_cast<uintptr_t>(-1);
 
     
-    static const size_t kMinimumStackSize = 1 * 1024;
+    static const size_t kMinimumStackSize = 256;
 
     
     static const size_t kMaximumStackSize = 64 * 1024 * 1024;
