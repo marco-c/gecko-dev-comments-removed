260e47df0e9e7d708edd063a7019d3dc01471eb6
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 05:59:28 +0000

    Bug 1381074: Have downloadable blocklist recognize advanced layers. r=dvander
    MozReview-Commit-ID: GlzDUQoAH6P
    
    UltraBlame original commit: b08d78d02da1e1c78cb6935f4e570798cb088ddd

--- widget/GfxInfoBase.cpp
+++ widget/GfxInfoBase.cpp
@@ -353,6 +353,8 @@ BlacklistFeatureToGfxFeature(const nsAString& aFeature)
       return nsIGfxInfo::FEATURE_CANVAS2D_ACCELERATION;
   else if (aFeature.EqualsLiteral("WEBGL2"))
     return nsIGfxInfo::FEATURE_WEBGL2;
+  else if (aFeature.EqualsLiteral("ADVANCED_LAYERS"))
+    return nsIGfxInfo::FEATURE_ADVANCED_LAYERS;
 
   
   
@@ -983,6 +985,7 @@ GfxInfoBase::EvaluateDownloadedBlacklist(nsTArray<GfxDriverInfo>& aDriverInfo)
     nsIGfxInfo::FEATURE_WEBRTC_HW_ACCELERATION,
     nsIGfxInfo::FEATURE_CANVAS2D_ACCELERATION,
     nsIGfxInfo::FEATURE_WEBGL2,
+    nsIGfxInfo::FEATURE_ADVANCED_LAYERS,
     0
   };
 
