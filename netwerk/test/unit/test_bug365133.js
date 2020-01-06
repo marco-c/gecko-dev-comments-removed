8becdff0a210473a10e2b3b8b41021d92d227917
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 21:19:47 +0000

    Backed out changeset cfe4b1e6be9b (bug 1416343) for ESlint failing in modules/test/browser/browser_SitePermissions_tab_urls.js:38:14 r=backout on a CLOSED TREE
    UltraBlame original commit: 561d658b088d1ea00d226891603b91acadee4d91

--- netwerk/test/unit/test_bug365133.js
+++ netwerk/test/unit/test_bug365133.js
@@ -1,5 +1,4 @@
 const URL = "ftp://localhost/bug365133/";
-Cu.import("resource://gre/modules/NetUtil.jsm");
 
 const tests = [
   [ 
@@ -76,7 +75,11 @@ function storeData() {
                createInstance(Ci.nsIStringInputStream);
   stream.data = tests[0][0];
 
-  var url = NetUtil.newURI(URL);
+  var url = {
+    password: "",
+    asciiSpec: URL,
+    QueryInterface: XPCOMUtils.generateQI([Ci.nsIURI])
+  };
 
   var channel = {
     URI: url,
