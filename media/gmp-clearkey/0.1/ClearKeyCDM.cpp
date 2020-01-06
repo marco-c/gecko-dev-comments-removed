3da46b4bdf0a1d74c3b7d19b89fb557bf760064d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 23:53:31 +0000

    Bug 1417297 - Part1 - Convert gmp-clearkey to use Chromium ContentDecryptionModule_9 interface. r=cpearce
    1. Make ClearKeyCDM inherits cdm::ContentDecryptionModule_9
    2. Pass cdm::Host_9 instance instead of cdm::Host8
    3. Modify the manifest to 1.4.9
    
    MozReview-Commit-ID: JbeBm5YNZ22
    
    UltraBlame original commit: 88b8a5b2f6689fb37183400b8bbf5915406deaef

--- media/gmp-clearkey/0.1/ClearKeyCDM.cpp
+++ media/gmp-clearkey/0.1/ClearKeyCDM.cpp
@@ -4,7 +4,7 @@
 
 using namespace cdm;
 
-ClearKeyCDM::ClearKeyCDM(Host_8* aHost)
+ClearKeyCDM::ClearKeyCDM(Host_9* aHost)
 {
   mHost = aHost;
   mSessionManager = new ClearKeySessionManager(mHost);
@@ -18,6 +18,15 @@ ClearKeyCDM::Initialize(bool aAllowDistinctiveIdentifier,
                         aAllowPersistentState);
 }
 
+void
+ClearKeyCDM::GetStatusForPolicy(uint32_t aPromiseId,
+                                const Policy& aPolicy)
+{
+  
+  
+  
+  assert(false);
+}
 void
 ClearKeyCDM::SetServerCertificate(uint32_t aPromiseId,
                                   const uint8_t* aServerCertificateData,
@@ -178,6 +187,15 @@ void
 ClearKeyCDM::OnQueryOutputProtectionStatus(QueryResult aResult,
                                            uint32_t aLinkMask,
                                            uint32_t aOutputProtectionMask)
+{
+  
+  assert(false);
+}
+
+void
+ClearKeyCDM::OnStorageId(uint32_t aVersion,
+                         const uint8_t* aStorageId,
+                         uint32_t aStorageIdSize)
 {
   
   assert(false);
