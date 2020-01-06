8eecc0594f68e05f74308fce5812e46cd8c3c186
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 00:05:08 +0000

    Backed out changeset 3c716b3b4541 (bug 1361900)
    UltraBlame original commit: 0c55713c1af4d1f1bbbfbc6f4c909726c6ad78c2

--- js/xpconnect/loader/mozJSComponentLoader.cpp
+++ js/xpconnect/loader/mozJSComponentLoader.cpp
@@ -683,26 +683,28 @@ mozJSComponentLoader::ObjectForLocation(ComponentLoaderInfo& aInfo,
     rv = PathifyURI(aInfo.URI(), cachePath);
     NS_ENSURE_SUCCESS(rv, rv);
 
-    if (!mReuseLoaderGlobal) {
-        script = ScriptPreloader::GetSingleton().GetCachedScript(cx, cachePath);
-        if (!script && cache) {
-            ReadCachedScript(cache, cachePath, cx, mSystemPrincipal, &script);
+    if (cache) {
+        if (!mReuseLoaderGlobal) {
+            script = ScriptPreloader::GetSingleton().GetCachedScript(cx, cachePath);
+            if (!script) {
+                rv = ReadCachedScript(cache, cachePath, cx, mSystemPrincipal, &script);
+            }
+        } else {
+            rv = ReadCachedFunction(cache, cachePath, cx, mSystemPrincipal,
+                                    function.address());
         }
-    } else if (cache) {
-        ReadCachedFunction(cache, cachePath, cx, mSystemPrincipal,
-                           function.address());
-    }
 
-    if (script || function) {
-        LOG(("Successfully loaded %s from startupcache\n", nativePath.get()));
-    } else if (cache) {
-        
-        
-        
-        writeToCache = true;
-        
-        
-        JS_ClearPendingException(cx);
+        if (NS_SUCCEEDED(rv)) {
+            LOG(("Successfully loaded %s from startupcache\n", nativePath.get()));
+        } else {
+            
+            
+            
+            writeToCache = true;
+            
+            
+            JS_ClearPendingException(cx);
+        }
     }
 
     if (!script && !function) {
