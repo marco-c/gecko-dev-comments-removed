a942753e08dbb32ea92a474e30e02c8660ccde1d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 01:32:09 +0000

    merge mozilla-inbound to mozilla-central. r=merge a=merge
    MozReview-Commit-ID: CEadD9RweN4
    
    UltraBlame original commit: ebad93e117700d8e2d65573b824beb18a8cc2030

--- browser/components/preferences/in-content/findInPage.js
+++ browser/components/preferences/in-content/findInPage.js
@@ -239,8 +239,10 @@ var gSearchResultsPane = {
         noResultsEl.hidden = false;
 
         let strings = this.strings;
-        document.getElementById("sorry-message").textContent =
-          strings.getFormattedString("searchResults.sorryMessage2", [query]);
+
+        document.getElementById("sorry-message").textContent = AppConstants.platform == "win" ?
+          strings.getFormattedString("searchResults.sorryMessageWin", [query]) :
+          strings.getFormattedString("searchResults.sorryMessageUnix", [query]);
         let brandName = document.getElementById("bundleBrand").getString("brandShortName");
         document.getElementById("need-help").innerHTML =
           strings.getFormattedString("searchResults.needHelp", [brandName]);
@@ -270,7 +272,7 @@ var gSearchResultsPane = {
 
   searchWithinNode(nodeObject, searchPhrase) {
     let matchesFound = false;
-    if (nodeObject.childElementCount == 0) {
+    if (nodeObject.childElementCount == 0 || nodeObject.tagName == "menulist") {
       let simpleTextNodes = this.textNodeDescendants(nodeObject);
 
       for (let node of simpleTextNodes) {
