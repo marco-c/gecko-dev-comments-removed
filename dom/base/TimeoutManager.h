ac66e44859df60b540e09f6ab0f0fb6936aaa479
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 03:22:45 +0000

    Bug 1371787 P4 Pass Timeout to DOMMinTimeoutValue(). r=ehsan
    UltraBlame original commit: 66c516526c98aebb7850110615869d72abee78b8

--- dom/base/TimeoutManager.h
+++ dom/base/TimeoutManager.h
@@ -55,7 +55,7 @@ public:
   void ClearAllTimeouts();
   uint32_t GetTimeoutId(mozilla::dom::Timeout::Reason aReason);
 
-  int32_t DOMMinTimeoutValue(bool aIsTracking) const;
+  int32_t DOMMinTimeoutValue(Timeout* aTimeout) const;
 
   
   
