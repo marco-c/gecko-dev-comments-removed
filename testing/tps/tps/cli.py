46e89c3ba7ee82c0a33d8c904498cb64e4704764
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 05:55:43 +0000

    Bug 1380402 - Set MOZ_DEVELOPER_REPO_DIR in the environment when running TPS on mac. r=markh
    MozReview-Commit-ID: 2jtresePxiu
    
    UltraBlame original commit: 77f4c7873ce3b3a8110af42b1a14ff9fcea9f903

--- testing/tps/tps/cli.py
+++ testing/tps/tps/cli.py
@@ -106,6 +106,10 @@ def main():
             if m:
                 extensionDir = '%s:/%s' % (m.group(0)[1:2], extensionDir[3:])
                 extensionDir = extensionDir.replace('/', '\\')
+    if sys.platform == 'darwin':
+        
+        sourceRoot = os.path.join(extensionDir, '..', '..', '..', '..')
+        os.environ['MOZ_DEVELOPER_REPO_DIR'] = os.path.abspath(sourceRoot)
 
     TPS = TPSTestRunner(extensionDir,
                         binary=options.binary,
