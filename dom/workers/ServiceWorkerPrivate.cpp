764a0d500067e603f043bc665a196b973ab85d67
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 01:39:04 +0000

    Bug 1368106 - Use the new MOZ_DIAGNOSTIC_ASSERT_ENABLED instead of several defines r=glandium
    MozReview-Commit-ID: KOM35SQnoRh
    
    UltraBlame original commit: f26f1c5652fca64191701d09fc49adf76e0c49a3

--- dom/workers/ServiceWorkerPrivate.cpp
+++ dom/workers/ServiceWorkerPrivate.cpp
@@ -1805,7 +1805,7 @@ ServiceWorkerPrivate::SpawnWorkerIfNeeded(WakeUpReason aWhy,
   info.mOriginAttributes = mInfo->GetOriginAttributes();
 
   
-#if defined(DEBUG) || !defined(RELEASE_OR_BETA)
+#ifdef MOZ_DIAGNOSTIC_ASSERT_ENABLED
   nsCOMPtr<nsIContentSecurityPolicy> csp;
   Unused << info.mPrincipal->GetCsp(getter_AddRefs(csp));
   MOZ_DIAGNOSTIC_ASSERT(!csp);
