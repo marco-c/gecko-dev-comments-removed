51d0ace7fbf8792eba54e9cce226912c62b3bdaa
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 20:44:15 +0000

    Bug 1415445 - part 3: nsIEditActionListener's WillCreateElement() and DidCreateElement() should take child node at insertion point or new node itself rather than the container node and offset in it r=m_kato
    nsIEditActionListener::WillCreateElement() and
    nsIEditActionListener::DidCreateElement() are implemented only by m-c.
    
    So, we can remove a set of container node and offset in it from their argument.
    Instead, WillCreateElement() should take a node which will be next sibling of
    the new node.
    
    Note that only implementation of them is, HTMLEditRules::DidCreateElement().
    So, we can get rid of them and can call HTMLEditRules::DidCreateElement()
    directly from EditorBase::CreateNode().  However, such change should be done
    in another bug which checks all nsIEditActionListener method implementations.
    
    MozReview-Commit-ID: 4LQEs2WwrVC
    
    UltraBlame original commit: 57ed7f0422124ea09e45dd8df7c14d4a0bc689f6

--- editor/libeditor/EditorBase.cpp
+++ editor/libeditor/EditorBase.cpp
@@ -1430,12 +1430,13 @@ EditorBase::CreateNode(nsAtom* aTag,
     pointToInsert.Set(aParent, aParent->Length());
   } else if (aChildAtPosition) {
     pointToInsert.Set(aChildAtPosition);
-    
-    
-    Unused << pointToInsert.Offset();
   } else {
     pointToInsert.Set(aParent, aPosition);
   }
+  
+  
+  
+  int32_t offset = static_cast<int32_t>(pointToInsert.Offset());
 
   AutoRules beginRulesSniffing(this, EditAction::createNode, nsIEditor::eNext);
 
@@ -1443,8 +1444,7 @@ EditorBase::CreateNode(nsAtom* aTag,
     AutoActionListenerArray listeners(mActionListeners);
     for (auto& listener : listeners) {
       listener->WillCreateNode(nsDependentAtomString(aTag),
-                               GetAsDOMNode(pointToInsert.Container()),
-                               pointToInsert.Offset());
+                               GetAsDOMNode(pointToInsert.GetChildAtOffset()));
     }
   }
 
@@ -1458,15 +1458,13 @@ EditorBase::CreateNode(nsAtom* aTag,
     MOZ_ASSERT(ret);
   }
 
-  mRangeUpdater.SelAdjCreateNode(pointToInsert.Container(),
-                                 pointToInsert.Offset());
+  mRangeUpdater.SelAdjCreateNode(pointToInsert.Container(), offset);
 
   {
     AutoActionListenerArray listeners(mActionListeners);
     for (auto& listener : listeners) {
-      listener->DidCreateNode(nsDependentAtomString(aTag), GetAsDOMNode(ret),
-                              GetAsDOMNode(pointToInsert.Container()),
-                              pointToInsert.Offset(), rv);
+      listener->DidCreateNode(nsDependentAtomString(aTag),
+                              GetAsDOMNode(ret), rv);
     }
   }
 
