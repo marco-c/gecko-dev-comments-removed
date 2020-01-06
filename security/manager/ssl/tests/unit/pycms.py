9f39b2122cc2533f557dcafce716ad0de350a89b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 21:37:14 +0000

    bug 1413336 - (3/7) fix pycert.py and pykey.py with respect to pyasn1/pyasn1-modules updates r=Cykesiopka
    MozReview-Commit-ID: CsxOF7LdEHB
    
    UltraBlame original commit: 62a772c934611f76b351627c8aa9fa4116b1edaa

--- security/manager/ssl/tests/unit/pycms.py
+++ security/manager/ssl/tests/unit/pycms.py
@@ -113,7 +113,9 @@ class CMS(object):
             raise pykey.UnknownHashAlgorithmError(pykeyHash)
         algorithmIdentifier = rfc2459.AlgorithmIdentifier()
         algorithmIdentifier['algorithm'] = univ.ObjectIdentifier(oidString)
-        algorithmIdentifier['parameters'] = univ.Null()
+        
+        nullEncapsulated = encoder.encode(univ.Null())
+        algorithmIdentifier['parameters'] = univ.Any(nullEncapsulated)
         return algorithmIdentifier
 
     def buildSignerInfo(self, certificate, pykeyHash, digestValue):
