775b9b34e493a8b5b5d260f2618bd15db0507943
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 05:11:22 +0000

    Bug 1378481 - Assign 'roundingError' in the default branch too, to avoid a maybe-uninitialized compiler warning.  r=dholbert
    MozReview-Commit-ID: CQk1PBaflct
    
    UltraBlame original commit: 8648cd5683afab2ed09f592c3d408df83f6172f9

--- layout/generic/nsGridContainerFrame.cpp
+++ layout/generic/nsGridContainerFrame.cpp
@@ -4749,6 +4749,7 @@ nsGridContainerFrame::Tracks::AlignJustifyContent(
     default:
       MOZ_ASSERT_UNREACHABLE("unknown align-/justify-content value");
       between = 0; 
+      roundingError = 0; 
   }
   between += mGridGap;
   for (TrackSize& sz : mSizes) {
