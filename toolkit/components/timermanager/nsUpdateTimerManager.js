dff4ade942b79c891f0fee69fd3384798d6c5fb1
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 04:05:06 +0000

    Backed out changeset 5856f1c45a92 (bug 1375077) for Windows opt permaorange in test_worker_basic.html
    MozReview-Commit-ID: C0kXjRHfDEU
    
    UltraBlame original commit: 7fb0f878a23b54cb069c28836f347f9baf077c06

--- toolkit/components/timermanager/nsUpdateTimerManager.js
+++ toolkit/components/timermanager/nsUpdateTimerManager.js
@@ -57,7 +57,7 @@ function LOG(string) {
 
 
 function TimerManager() {
-  Services.obs.addObserver(this, "profile-before-change");
+  Services.obs.addObserver(this, "xpcom-shutdown");
 }
 TimerManager.prototype = {
   
@@ -109,8 +109,8 @@ TimerManager.prototype = {
         this._canEnsureTimer = true;
         this._ensureTimer(firstInterval);
         break;
-      case "profile-before-change":
-        Services.obs.removeObserver(this, "profile-before-change");
+      case "xpcom-shutdown":
+        Services.obs.removeObserver(this, "xpcom-shutdown");
 
         
         this._cancelTimer();
