07f43eb3d3588f2fb66d757a8a49d1f91f26f204
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 23:59:47 +0000

    Bug 1298370 - Add SameSite attribute to Cookie Inspector r=pbro
    MozReview-Commit-ID: C0n35vEaAdJ
    
    UltraBlame original commit: ff9b8f883b0c7b725613818862c68cc7e87e1cac

--- devtools/server/actors/storage.js
+++ devtools/server/actors/storage.js
@@ -22,6 +22,14 @@ loader.lazyRequireGetter(this, "naturalSortCaseInsensitive",
 
 
 
+const COOKIE_SAMESITE = {
+  LAX: "Lax",
+  STRICT: "Strict",
+  UNSET: "Unset"
+};
+
+
+
 
 
 const SEPARATOR_GUID = "{9d414cc5-8319-0a04-0586-c0a6ae01670a}";
@@ -540,8 +548,20 @@ StorageActors.createActor({
       value: new LongStringActor(this.conn, cookie.value || ""),
       isDomain: cookie.isDomain,
       isSecure: cookie.isSecure,
-      isHttpOnly: cookie.isHttpOnly
+      isHttpOnly: cookie.isHttpOnly,
+      sameSite: this.getSameSiteStringFromCookie(cookie)
     };
+  },
+
+  getSameSiteStringFromCookie(cookie) {
+    switch (cookie.sameSite) {
+      case cookie.SAMESITE_LAX:
+        return COOKIE_SAMESITE.LAX;
+      case cookie.SAMESITE_STRICT:
+        return COOKIE_SAMESITE.STRICT;
+    }
+    
+    return COOKIE_SAMESITE.UNSET;
   },
 
   populateStoresForHost(host) {
@@ -653,7 +673,8 @@ StorageActors.createActor({
       { name: "value", editable: true, hidden: false },
       { name: "isDomain", editable: false, hidden: true },
       { name: "isSecure", editable: true, hidden: true },
-      { name: "isHttpOnly", editable: true, hidden: false }
+      { name: "isHttpOnly", editable: true, hidden: false },
+      { name: "sameSite", editable: false, hidden: false }
     ];
   }),
 
