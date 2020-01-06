bd0fe119f3241f80690648b3f89e15c0f2cee45c
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 01:44:36 +0000

    Bug 1368121 - Remove telemetry for a11y update time. r=eeejay
    UltraBlame original commit: 0fbbdadea65d7320ed0df17c0d8a4093eb988f0b

--- accessible/base/NotificationController.cpp
+++ accessible/base/NotificationController.cpp
@@ -591,7 +591,6 @@ void
 NotificationController::WillRefresh(mozilla::TimeStamp aTime)
 {
   PROFILER_LABEL_FUNC(js::ProfileEntry::Category::OTHER);
-  Telemetry::AutoTimer<Telemetry::A11Y_UPDATE_TIME> updateTimer;
 
   
   
