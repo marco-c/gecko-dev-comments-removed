8450ad36f4282864e9afe1c8154c26c9c6af7af1
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 05:46:26 +0000

    Bug 1380244 - trigger skip-to-next-key-frame quicker; r=jya
    This patch is going to loose the criteria of skip-to-next-key-frame.
    
    The original rules are here:
    http://searchfox.org/mozilla-central/rev/31311070d9860b24fe4a7a36976c14b328c16208/dom/media/MediaFormatReader.cpp#1559
    
    Skip-to-next-key-frame is triggered if the playback position is LARGER than the next key frame time.
    But, from the video-track point of view, when the skip-to-next-key-frame is triggered, it skips to the next-next key frame.
    
    Here is an example, say, we are playing a media file with its playback position at time _a_,
    and its video decoding is falling behind at time _v_.
    The next key frame is at time _k1_ and next-next key frame is at time _k2_.
    
                  a
    ----|---------|---------|-------------|----------------> time
        v                   k1            k2
    
    When the playback position _a_ passes _k1_ (_a_ > _k1_), the skip-to-next-key-frame is triggered,
    and the demuxer jumps to _k2_ directly.
    
    
    
    The idea here is to give a chance when (_a_ == _k1_), let demuxer jump to _k1_ and see if the video decoding could catch up.
    
    MozReview-Commit-ID: 6aRSYDOI1ds
    
    UltraBlame original commit: be2c265f67c476d1f02244b444fd0362e660c0a2

--- dom/media/MediaFormatReader.cpp
+++ dom/media/MediaFormatReader.cpp
@@ -1556,7 +1556,7 @@ MediaFormatReader::ShouldSkip(TimeUnit aTimeThreshold)
     
     return false;
   }
-  return (nextKeyframe < aTimeThreshold
+  return (nextKeyframe <= aTimeThreshold
           || (mVideo.mTimeThreshold
               && mVideo.mTimeThreshold.ref().EndTime() < aTimeThreshold))
          && nextKeyframe.ToMicroseconds() >= 0
