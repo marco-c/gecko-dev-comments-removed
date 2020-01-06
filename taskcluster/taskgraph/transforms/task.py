210b9701bef985fbc004f69dafd7cbedad65d23b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 12:45:08 +0000

    Backed out changeset 046d705929f7 (bug 1383880)
    UltraBlame original commit: d65d89adb9acc213ead0b778721d3d5c8a89e062

--- taskcluster/taskgraph/transforms/task.py
+++ taskcluster/taskgraph/transforms/task.py
@@ -17,7 +17,6 @@ import time
 from copy import deepcopy
 
 from mozbuild.util import memoize
-from mozbuild import schedules
 from taskgraph.util.attributes import TRUNK_PROJECTS
 from taskgraph.util.hash import hash_path
 from taskgraph.util.treeherder import split_symbol
@@ -160,10 +159,6 @@ task_description_schema = Schema({
         {'seta': None},
         
         {'skip-unless-changed': [basestring]},
-        
-        {'skip-unless-schedules': list(schedules.ALL_COMPONENTS)},
-        
-        {'skip-unless-schedules-or-seta': list(schedules.ALL_COMPONENTS)},
     ),
 
     
