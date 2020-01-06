df0bd03a7e3b2a2f0b1c6809189a77c1314fef75
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 20:04:32 +0000

    Bug 366192 - Fixed http/https regex checks. r=mak
    MozReview-Commit-ID: FL0AWkM9Ggm
    
    UltraBlame original commit: bbbd75cd12a945396d21f2000f5a9d4bc09602bb

--- toolkit/mozapps/handling/content/dialog.js
+++ toolkit/mozapps/handling/content/dialog.js
@@ -121,7 +121,7 @@ var dialog = {
         elm.setAttribute("image", "moz-icon://" + uri.spec + "?size=32");
       } else if (app instanceof Ci.nsIWebHandlerApp) {
         let uri = ios.newURI(app.uriTemplate);
-        if (/^https?/.test(uri.scheme)) {
+        if (/^https?$/.test(uri.scheme)) {
           
           
           
