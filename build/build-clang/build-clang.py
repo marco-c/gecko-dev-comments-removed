cdb7bc1852d70f03e2898c5cc52161ff2b223d95
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 16:39:59 +0000

    Bug 1405602 - Add clang-apply-replacements to the clang-tidy build generated by toolchains. r=ehsan
    MozReview-Commit-ID: 1BT2ukzLoC3
    
    UltraBlame original commit: cca8c45c7a34e6d0363e998d4f6c73302016231f

--- build/build-clang/build-clang.py
+++ build/build-clang/build-clang.py
@@ -269,6 +269,7 @@ def get_tool(config, key):
 
 
 
+
 def prune_final_dir_for_clang_tidy(final_dir):
     
     dirs = ("bin", "include", "lib", "libexec", "msbuild-bin", "share", "tools")
@@ -279,7 +280,9 @@ def prune_final_dir_for_clang_tidy(final_dir):
             raise Exception("Expected %s to be a directory" %f)
 
     
-    re_clang_tidy = re.compile(r"^clang-tidy(\.exe)?$", re.I)
+    
+    re_clang_tidy = re.compile(
+        r"^clang-(tidy|apply-replacements)(\.exe)?$", re.I)
     for f in glob.glob("%s/bin/*" % final_dir):
         if re_clang_tidy.search(os.path.basename(f)) is None:
             delete(f)
