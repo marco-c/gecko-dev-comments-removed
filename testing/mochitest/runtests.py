5081ea6a2a0ec3fa676a951fb57cbfd77e37bd9f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 10:05:42 +0000

    Bug 1390884 - Do not use chaos mode for test verification; r=jmaher
    Chaos mode for test verification doesn't quite work -- it is a bit flaky still.
    I intend to come back to it at a later date.
    
    UltraBlame original commit: 3539f73f0f0484a8982ae642e71006d5fbc033b8

--- testing/mochitest/runtests.py
+++ testing/mochitest/runtests.py
@@ -1809,8 +1809,8 @@ toolbar#nav-bar {
         except AttributeError:
             pass
         try:
-            if options.objPath:
-                sandbox_whitelist_paths.append(options.objPath)
+            if options.topsrcdir:
+                sandbox_whitelist_paths.append(options.topsrcdir)
         except AttributeError:
             pass
         if (platform.system() == "Linux" or
@@ -2354,39 +2354,12 @@ toolbar#nav-bar {
                     break
             return result
 
-        def step3():
-            stepOptions = copy.deepcopy(options)
-            stepOptions.repeat = VERIFY_REPEAT
-            stepOptions.keep_open = False
-            stepOptions.environment.append("MOZ_CHAOSMODE=")
-            result = self.runTests(stepOptions)
-            self.message_logger.finish()
-            return result
-
-        def step4():
-            stepOptions = copy.deepcopy(options)
-            stepOptions.repeat = 0
-            stepOptions.keep_open = False
-            stepOptions.environment.append("MOZ_CHAOSMODE=")
-            for i in xrange(VERIFY_REPEAT_SINGLE_BROWSER):
-                result = self.runTests(stepOptions)
-                self.message_logger.finish()
-                if result != 0:
-                    break
-            return result
-
         steps = [
             ("1. Run each test %d times in one browser." % VERIFY_REPEAT,
              step1),
             ("2. Run each test %d times in a new browser each time." %
              VERIFY_REPEAT_SINGLE_BROWSER,
              step2),
-            ("3. Run each test %d times in one browser, in chaos mode." %
-             VERIFY_REPEAT,
-             step3),
-            ("4. Run each test %d times in a new browser each time, "
-             "in chaos mode." % VERIFY_REPEAT_SINGLE_BROWSER,
-             step4),
         ]
 
         stepResults = {}
