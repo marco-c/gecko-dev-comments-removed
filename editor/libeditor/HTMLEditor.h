b9f909c5caf0375f32d4ad15d546dbbb93536a21
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 21:38:11 +0000

    Bug 1417344 - Clean up HTMLEditor::InsertBR. r=masayuki on a CLOSED TREE
    UltraBlame original commit: 40bf7c21792a5bf4ee68e5448d08617368c7774c

--- editor/libeditor/HTMLEditor.h
+++ editor/libeditor/HTMLEditor.h
@@ -477,7 +477,7 @@ protected:
                nsCOMPtr<nsIDOMNode>* outBRNode,
                nsIEditor::EDirection aSelect = nsIEditor::eNone) override;
 
-  nsresult InsertBR(nsCOMPtr<nsIDOMNode>* outBRNode);
+  nsresult InsertBR();
 
   
 
