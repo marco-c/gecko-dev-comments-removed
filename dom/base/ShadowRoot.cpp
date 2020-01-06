7238903c9b4b92350722707c13245a77c41c68e0
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 12:43:21 +0000

    Bug 1396568 - Make DocumentFragment::mHost a strong reference (so that ShadowRoot.host doesn't become null), r=mrbkap
    UltraBlame original commit: cdbce427f71c185250b9eba30d8a9f9957d1001a

--- dom/base/ShadowRoot.cpp
+++ dom/base/ShadowRoot.cpp
@@ -59,14 +59,14 @@ NS_INTERFACE_MAP_END_INHERITING(DocumentFragment)
 NS_IMPL_ADDREF_INHERITED(ShadowRoot, DocumentFragment)
 NS_IMPL_RELEASE_INHERITED(ShadowRoot, DocumentFragment)
 
-ShadowRoot::ShadowRoot(nsIContent* aContent,
+ShadowRoot::ShadowRoot(Element* aElement,
                        already_AddRefed<mozilla::dom::NodeInfo>&& aNodeInfo,
                        nsXBLPrototypeBinding* aProtoBinding)
-  : DocumentFragment(aNodeInfo), mPoolHost(aContent),
+  : DocumentFragment(aNodeInfo), mPoolHost(aElement),
     mProtoBinding(aProtoBinding), mShadowElement(nullptr),
     mInsertionPointChanged(false), mIsComposedDocParticipant(false)
 {
-  SetHost(aContent);
+  SetHost(aElement);
 
   
   
@@ -75,7 +75,7 @@ ShadowRoot::ShadowRoot(nsIContent* aContent,
 
   SetFlags(NODE_IS_IN_SHADOW_TREE);
 
-  ExtendedDOMSlots()->mBindingParent = aContent;
+  ExtendedDOMSlots()->mBindingParent = aElement;
   ExtendedDOMSlots()->mContainingShadow = this;
 
   
