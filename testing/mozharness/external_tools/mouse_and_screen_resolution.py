76036ec62f62ffa0bd82ee8ae283392f5387f491
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 14:52:10 +0000

    Bug 1401501 - ensure windows10 resolution is 1280x1024. r=ahal
    UltraBlame original commit: 889010e4d6cfba305828f90b7954ae5a090862e5

--- testing/mozharness/external_tools/mouse_and_screen_resolution.py
+++ testing/mozharness/external_tools/mouse_and_screen_resolution.py
@@ -45,11 +45,7 @@ def wfetch(url, retries=5):
 
 def main():
 
-    if not (platform.version().startswith('6.1.760') and not 'PROGRAMFILES(X86)' in os.environ):
-        
-        print "INFO: This script was written to be used with Windows 7 32-bit machines."
-        return 0
-
+    
     parser = OptionParser()
     parser.add_option(
         "--configuration-url", dest="configuration_url", type="string",
