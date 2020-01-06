b25885e4aaf4049d4da9a13f2a663bd021979cd2
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 09:22:21 +0000

    bug 1388656 size to actual used length in nsTArray::SetCapacity() optimization r=padenot
    MozReview-Commit-ID: BGFliZTx4QL
    
    UltraBlame original commit: a88ed8010517d597fd4fc94a124152575ca19f46

--- dom/media/webaudio/blink/Reverb.h
+++ dom/media/webaudio/blink/Reverb.h
@@ -50,8 +50,7 @@ public:
     
     Reverb(mozilla::ThreadSharedFloatArrayBufferList* impulseResponseBuffer,
            size_t impulseResponseBufferLength, size_t maxFFTSize,
-           size_t numberOfChannels, bool useBackgroundThreads, bool normalize,
-           float sampleRate);
+           bool useBackgroundThreads, bool normalize, float sampleRate);
 
     void process(const mozilla::AudioBlock* sourceBus,
                  mozilla::AudioBlock* destinationBus);
@@ -63,7 +62,7 @@ public:
 private:
     void initialize(const nsTArray<const float*>& impulseResponseBuffer,
                     size_t impulseResponseBufferLength, size_t maxFFTSize,
-                    size_t numberOfChannels, bool useBackgroundThreads);
+                    bool useBackgroundThreads);
 
     size_t m_impulseResponseLength;
 
