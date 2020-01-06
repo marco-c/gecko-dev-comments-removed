1af5d42b1038e99be4c9b07a2862ac6e79c100e5
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 00:20:16 +0000

    Bug 1363219 - Try harder to pre-render offscreen portions of scrollbar thumbs. r=mstange
    In bug 1359868 we started to do this, but we bounded the pre-render region for
    the entire scrollbar by the widget bounds, which is not helpful for tall
    scrollframes with short thumbs.
    
    This time, we are bounding the pre-render region of the thumb only, so a small
    thumb will always be completely painted.
    
    MozReview-Commit-ID: 5LuP5Lfahdm
    
    UltraBlame original commit: f74ec590a5343f603c2e070afa25743dd15df43d

--- layout/generic/nsGfxScrollFrame.cpp
+++ layout/generic/nsGfxScrollFrame.cpp
@@ -3094,32 +3094,12 @@ ScrollFrameHelper::AppendScrollPartsTo(nsDisplayListBuilder*   aBuilder,
     }
 
     
-    bool thumbGetsLayer = (scrollTargetId != FrameMetrics::NULL_SCROLL_ID);
-    bool isRcdRsf = mIsRoot && mOuter->PresContext()->IsRootContentDocument();
-    nsRect dirty = aDirtyRect;
-    if (isRcdRsf || thumbGetsLayer) {
-      nsRect overflow = scrollParts[i]->GetVisualOverflowRectRelativeToParent();
-      if (isRcdRsf) {
-        
-        
-        
-        dirty = overflow;
-      } else {
-        
-        
-        
-        
-        
-        nsSize refSize = aBuilder->RootReferenceFrame()->GetSize();
-        gfxSize scale = nsLayoutUtils::GetTransformToAncestorScale(mOuter);
-        if (scale.width != 0 && scale.height != 0) {
-          refSize.width /= scale.width;
-          refSize.height /= scale.height;
-        }
-        dirty = nsLayoutUtils::ComputePartialPrerenderArea(dirty, overflow, refSize);
-      }
-    }
-
+    
+    
+    
+    nsRect dirty = mIsRoot && mOuter->PresContext()->IsRootContentDocument()
+                   ? scrollParts[i]->GetVisualOverflowRectRelativeToParent()
+                   : aDirtyRect;
     nsDisplayListBuilder::AutoBuildingDisplayList
       buildingForChild(aBuilder, scrollParts[i],
                        dirty + mOuter->GetOffsetTo(scrollParts[i]), true);
