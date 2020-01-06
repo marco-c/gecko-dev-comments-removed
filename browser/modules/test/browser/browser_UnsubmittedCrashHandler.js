41ebdf611e7b0b623e257d7e330b5c2e8d7f3570
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 13:57:33 +0000

    Bug 1399796 - UnsubmittedCrashHandler should never check for unsubmitted crash reports if disabled or suppressed. r=Felipe
    Bug 1355492 moved the logic for scanning for unsubmitted crash reports out of the initialization
    of UnsubmittedCrashHandler, and the initialization is what decided whether or not it was
    appropriate to scan in the first place. This was done so that scanning could be deferred
    until idle after first paint.
    
    This patch makes it so that the scanning logic first ensures that the UnsubmittedCrashHandler
    is actually enabled and not suppressed (which is calculated earlier).
    
    I've also taken the liberty of adding a regression test.
    
    MozReview-Commit-ID: 3Aihom5Q17R
    
    UltraBlame original commit: 1256cadba71bc9e0677e4f92174bc76ad8952eb5

--- browser/modules/test/browser/browser_UnsubmittedCrashHandler.js
+++ browser/modules/test/browser/browser_UnsubmittedCrashHandler.js
@@ -206,6 +206,18 @@ add_task(async function setup() {
   
   
   UnsubmittedCrashHandler.uninit();
+
+  
+  
+  
+  await createPendingCrashReports(1);
+
+  notification =
+    await UnsubmittedCrashHandler.checkForUnsubmittedCrashReports();
+  Assert.ok(!notification, "There should not be a notification");
+
+  clearPendingCrashReports();
+
   await SpecialPowers.pushPrefEnv({
     set: [
       ["browser.crashReports.unsubmittedCheck.enabled", true],
