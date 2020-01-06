90a721e9f4bedfd5bfcbbe9302bae11ae5a3884f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 21:59:44 +0000

    servo: Merge #19316 - Fix Stylo tests to pass on both Stable and Nightly Rust (from servo:stylo-size-of); r=emilio
    This is on top of https://github.com/servo/servo/pull/19285.
    
    Rust Nightly has new enum memory layout optimizations: https://github.com/rust-lang/rust/pull/45225
    
    Source-Repo: https://github.com/servo/servo
    Source-Revision: 17e97b9320fdb7cdb33bbc5f4d0fde0653bbf2e4
    
    UltraBlame original commit: 6de571030d5d998dcadbd3dac602fa006395165c

--- servo/tests/unit/stylo/size_of.rs
+++ servo/tests/unit/stylo/size_of.rs
@@ -57,5 +57,7 @@ size_of_test!(test_size_of_specified_image, specified::image::Image, 40);
 
 
 
-size_of_test!(test_size_of_computed_image_layer, computed::image::ImageLayer, 48);
-size_of_test!(test_size_of_specified_image_layer, specified::image::ImageLayer, 48);
+size_of_test!(test_size_of_computed_image_layer, computed::image::ImageLayer,
+              if cfg!(rustc_has_pr45225) { 40 } else { 48 });
+size_of_test!(test_size_of_specified_image_layer, specified::image::ImageLayer,
+              if cfg!(rustc_has_pr45225) { 40 } else { 48 });
