92fa4c60191225ea457bfb9db232115d816125a7
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 10:50:36 +0000

    Bug 1390203: Only get highlight images on wifi. r=sebastian
    MozReview-Commit-ID: FtSDZ6Yu5Ql
    
    UltraBlame original commit: b1779a4ff84a20a9dff5895f46847a04d6f52099

--- mobile/android/base/java/org/mozilla/gecko/activitystream/homepanel/stream/StreamOverridablePageIconLayout.java
+++ mobile/android/base/java/org/mozilla/gecko/activitystream/homepanel/stream/StreamOverridablePageIconLayout.java
@@ -19,6 +19,7 @@ import org.mozilla.gecko.R;
 import org.mozilla.gecko.icons.IconCallback;
 import org.mozilla.gecko.icons.IconResponse;
 import org.mozilla.gecko.icons.Icons;
+import org.mozilla.gecko.util.NetworkUtils;
 import org.mozilla.gecko.widget.FaviconView;
 
 import java.util.concurrent.Future;
@@ -58,7 +59,10 @@ public class StreamOverridablePageIconLayout extends FrameLayout implements Icon
     public void updateIcon(@NonNull final String pageURL, @Nullable final String overrideImageURL) {
         cancelPendingRequests();
 
-        if (!TextUtils.isEmpty(overrideImageURL)) {
+        
+        
+        if (NetworkUtils.isWifi(getContext()) &&
+                !TextUtils.isEmpty(overrideImageURL)) {
             setUIMode(UIMode.NONFAVICON_IMAGE);
 
             
