feb7b0b8e263238f76f0a9e9a52487741e4eb60b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 09:27:23 +0000

    Bug 437381 - Remove nsIJSXMLHttpRequest and fix the comments in nsIXMLHttpRequest.h. r=smaug
    MozReview-Commit-ID: CYkV01S9QEZ
    
    UltraBlame original commit: 7d262cd9d65aeca062d1d5ae92bf90906edcd4ea

--- dom/xhr/XMLHttpRequestMainThread.h
+++ dom/xhr/XMLHttpRequestMainThread.h
@@ -158,7 +158,6 @@ public:
 
 class XMLHttpRequestMainThread final : public XMLHttpRequest,
                                        public nsIXMLHttpRequest,
-                                       public nsIJSXMLHttpRequest,
                                        public nsIStreamListener,
                                        public nsIChannelEventSink,
                                        public nsIProgressEventSink,
