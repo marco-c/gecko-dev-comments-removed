342cb929d03f8c185e3a1de40f8976d84ee22eff
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 17:03:08 +0000

    Bug 1402015 - Submit re-compressed MAR metadata to Balrog r=sfraser a=release DONTBUILD
    MozReview-Commit-ID: DuJxPBErAhg
    
    UltraBlame original commit: 70b3fb79bc92e26af03c2cf16445975b30346781

--- testing/mozharness/configs/releases/dev_updates_firefox_beta.py
+++ testing/mozharness/configs/releases/dev_updates_firefox_beta.py
@@ -12,7 +12,7 @@ config = {
     
     "push_dest": "ssh://hg.mozilla.org/users/raliiev_mozilla.com/tools",
     
-    "shipped-locales-url": "https://hg.mozilla.org/projects/date/raw-file/{revision}/browser/locales/shipped-locales",
+    "shipped-locales-url": "https://hg.mozilla.org/projects/jamun/raw-file/{revision}/browser/locales/shipped-locales",
     "ignore_no_changes": True,
     "ssh_user": "ffxbld",
     "ssh_key": "~/.ssh/ffxbld_rsa",
@@ -20,19 +20,24 @@ config = {
     "archive_prefix": "https://ftp.stage.mozaws.net/pub",
     "previous_archive_prefix": "https://archive.mozilla.org/pub",
     "download_domain": "download.mozilla.org",
-    "balrog_url": "http://ec2-54-241-39-23.us-west-1.compute.amazonaws.com",
+    "balrog_url": "http://54.90.211.22:9090",
     "balrog_username": "balrog-stage-ffxbld",
     "update_channels": {
         "beta-dev": {
             "version_regex": r"^(\d+\.\d+(b\d+)?)$",
             "requires_mirrors": True,
             
-            "patcher_config": "mozDate-branch-patcher2.cfg",
+            "patcher_config": "mozJamun-branch-patcher2.cfg",
             "update_verify_channel": "beta-dev-localtest",
             "mar_channel_ids": [],
             "channel_names": ["beta-dev", "beta-dev-localtest", "beta-dev-cdntest"],
             "rules_to_update": ["firefox-beta-dev-cdntest", "firefox-beta-dev-localtest"],
             "publish_rules": ["firefox-beta"],
+            "bz2_blob_suffix": "-bz2",
+            "bz2_rules_to_update": ["firefox-beta-cdntest-bz2", "firefox-beta-localtest-bz2"],
+            "bz2_publish_rules": [652],
+            "complete_mar_filename_pattern": '%s-%s.bz2.complete.mar',
+            "complete_mar_bouncer_product_pattern": '%s-%s-complete-bz2',
         }
     },
     "balrog_use_dummy_suffix": False,
