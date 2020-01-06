c45d9c446dfa86fb20053a4067b38cc7b7b16e3e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 09:51:43 +0000

    Bug 1390691 - Fix up Gecko_CalcStyleDifference for Linux 32-bit ABI. r=manishearth
    Bindgen bitfield enums don't work as return values with the Linux 32-bit ABI at
    the moment because they wrap the value in a struct.
    
    This causes the Rust side to believe the callee expects space for the struct
    return value, while C++ believes it's just an integer value.
    
    MozReview-Commit-ID: FRBqlZuMiAR
    
    UltraBlame original commit: fa23649e846848d4dfd864589071684e75deb1ed

--- layout/style/ServoBindings.h
+++ layout/style/ServoBindings.h
@@ -395,10 +395,13 @@ void Gecko_NoteAnimationOnlyDirtyElement(RawGeckoElementBorrowed element);
 nsStyleContext* Gecko_GetStyleContext(RawGeckoElementBorrowed element,
                                       nsIAtom* aPseudoTagOrNull);
 mozilla::CSSPseudoElementType Gecko_GetImplementedPseudo(RawGeckoElementBorrowed element);
-nsChangeHint Gecko_CalcStyleDifference(ServoStyleContextBorrowed old_style,
-                                       ServoStyleContextBorrowed new_style,
-                                       uint64_t old_style_bits,
-                                       bool* any_style_changed);
+
+
+
+uint32_t Gecko_CalcStyleDifference(ServoStyleContextBorrowed old_style,
+                                   ServoStyleContextBorrowed new_style,
+                                   uint64_t old_style_bits,
+                                   bool* any_style_changed);
 nsChangeHint Gecko_HintsHandledForDescendants(nsChangeHint aHint);
 
 
