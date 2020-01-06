be26f70e836a710993be5e28f8d77361bdd049a1
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 04:33:32 +0000

    Bug 1364896 - Part 4 - Add a popupshowing listener to the entire document instead of individual elements. r=nhnt11
    MozReview-Commit-ID: Fypw3TSziyk
    
    UltraBlame original commit: 3b533f122a59f912bd009ff56bffa84a5b690968

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -8229,54 +8229,12 @@ function restoreLastSession() {
 
 
 var ContextMenuTouchModeObserver = {
-  get _searchBarContextMenu() {
-    let searchbar = document.getElementById("searchbar");
-    let textBox = document.getAnonymousElementByAttribute(searchbar,
-                                        "anonid", "searchbar-textbox");
-    let inputBox = document.getAnonymousElementByAttribute(textBox,
-                                        "anonid", "textbox-input-box");
-    let menu = document.getAnonymousElementByAttribute(inputBox,
-                                        "anonid", "input-box-contextmenu");
-    return menu;
-  },
-
-  get _urlBarContextMenu() {
-    let urlbar = document.getElementById("urlbar");
-    let textBox = document.getAnonymousElementByAttribute(urlbar,
-                                        "anonid", "textbox-input-box");
-    let menu = document.getAnonymousElementByAttribute(textBox,
-                                        "anonid", "input-box-contextmenu");
-    return menu;
-  },
-
-  _addListener(el) {
-    el.addEventListener("popupshowing", this);
-  },
-
-  _removeListener(el) {
-    el.removeEventListener("popupshowing", this);
-  },
-
   init() {
-    
-
-    
-    
-    this._addListener(document.getElementById("mainPopupSet"));
-
-    
-    this._addListener(document.getElementById("back-button"));
-    this._addListener(document.getElementById("forward-button"));
-
-    
-    this._addListener(this._searchBarContextMenu);
-
-    
-    this._addListener(this._urlBarContextMenu);
+    window.addEventListener("popupshowing", this, true);
   },
 
   handleEvent(event) {
-    let target = event.target;
+    let target = event.originalTarget;
     if (target.localName != "menupopup") {
       return;
     }
@@ -8289,11 +8247,7 @@ var ContextMenuTouchModeObserver = {
   },
 
   uninit() {
-    this._removeListener(document.getElementById("mainPopupSet"));
-    this._removeListener(document.getElementById("back-button"));
-    this._removeListener(document.getElementById("forward-button"));
-    this._removeListener(this._searchBarContextMenu);
-    this._removeListener(this._urlBarContextMenu);
+    window.removeEventListener("popupshowing", this, true);
   },
 };
 
