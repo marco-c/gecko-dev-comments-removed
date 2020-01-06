9f80b6f688224327f663ebfae3264382239e642d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 21:57:30 +0000

    Backed out changeset edf2b70d2f01 (bug 1408182)for failing in  devtools/client/netmonitor/test/browser_net_copy_params.js r=backout on a CLOSED TREE
    UltraBlame original commit: 3da20c5290dfdef39b1a6c2f08e6f71d8bda450b

--- devtools/client/netmonitor/.eslintrc.js
+++ devtools/client/netmonitor/.eslintrc.js
@@ -20,10 +20,4 @@ module.exports = {
     
     "mozilla/reject-some-requires": ["error", "^(chrome|chrome:.*|resource:.*|devtools/server/.*|.*\\.jsm|devtools/shared/platform/(chome|content)/.*)$"],
   },
-
-  "parserOptions": {
-    "ecmaFeatures": {
-      experimentalObjectRestSpread: true,
-    },
-  },
 };
