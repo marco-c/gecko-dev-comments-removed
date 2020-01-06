2168b18d68662d1b0b779efc7ebc96928d1b0ece
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 12:43:12 +0000

    Bug 1395900 part 2 - Teach analyzeNewLoopTypes about for-in iterator value slot. r=tcampbell
    UltraBlame original commit: fdc73e6b6a213edf515fd5a28db8ed0fa892766e

--- js/src/jit/IonBuilder.cpp
+++ js/src/jit/IonBuilder.cpp
@@ -570,6 +570,18 @@ IonBuilder::analyzeNewLoopTypes(const CFGBlock* loopEntryBlock)
             return abort(AbortReason::Alloc);
     }
 
+    if (loopEntry->isForIn()) {
+        
+        
+        
+
+        MPhi* phi = entry->getSlot(entry->stackDepth() - 1)->toPhi();
+        MOZ_ASSERT(phi->getOperand(0)->type() == MIRType::Undefined);
+
+        if (!phi->addBackedgeType(alloc(), MIRType::Value, nullptr))
+            return abort(AbortReason::Alloc);
+    }
+
     
     jsbytecode* start = loopEntryBlock->stopPc();
     start += GetBytecodeLength(start);
