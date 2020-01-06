3c042812639aac898c9effb9ca3ff3d8dfc32cb2
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 09:10:37 +0000

    Bug 1388939, part 4 - Give clear names to the nsFrameManager methods for updating the registered undisplayed style contexts. r=dholbert
    MozReview-Commit-ID: LZuleDSt6LR
    
    UltraBlame original commit: 8a86aaef19f75c1d808be5ce0f20d2198e09055d

--- layout/base/nsFrameManager.h
+++ layout/base/nsFrameManager.h
@@ -100,8 +100,8 @@ public:
   
 
 
-  void ChangeUndisplayedContent(nsIContent* aContent,
-                                nsStyleContext* aStyleContext)
+  void ChangeRegisteredDisplayNoneStyleFor(nsIContent* aContent,
+                                           nsStyleContext* aStyleContext)
   {
     ChangeStyleContextInMap(mDisplayNoneMap, aContent, aStyleContext);
   }
@@ -109,8 +109,8 @@ public:
   
 
 
-  void ChangeDisplayContents(nsIContent* aContent,
-                             nsStyleContext* aStyleContext)
+  void ChangeRegisteredDisplayContentsStyleFor(nsIContent* aContent,
+                                               nsStyleContext* aStyleContext)
   {
     ChangeStyleContextInMap(mDisplayContentsMap, aContent, aStyleContext);
   }
