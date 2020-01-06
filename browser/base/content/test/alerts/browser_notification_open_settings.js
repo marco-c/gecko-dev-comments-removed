6bf07fe8e5d97b02524a199878afaa98a07559df
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 07:19:16 +0000

    Bug 1383709 - Don't wait for the sync-pane-loaded notification in old prefs since it won't be fired when only viewing the Content pane. r=mconley
    Waiting for this notification is only necessary in the 'new prefs', as detailed by the comment that I added in this patch.
    
    MozReview-Commit-ID: LpFwHrqNc0g
    
    UltraBlame original commit: 35b75b53f9d362a04950c3914cca4d344e7c4e07

--- browser/base/content/test/alerts/browser_notification_open_settings.js
+++ browser/base/content/test/alerts/browser_notification_open_settings.js
@@ -1,8 +1,10 @@
 "use strict";
 
 var notificationURL = "http://example.org/browser/browser/base/content/test/alerts/file_dom_notifications.html";
-var expectedURL = Services.prefs.getBoolPref("browser.preferences.useOldOrganization") ? "about:preferences#content"
-                                                                                       : "about:preferences#privacy";
+var useOldPrefs = Services.prefs.getBoolPref("browser.preferences.useOldOrganization");
+var expectedURL = useOldPrefs ? "about:preferences#content"
+                              : "about:preferences#privacy";
+
 
 add_task(async function test_settingsOpen_observer() {
   info("Opening a dummy tab so openPreferences=>switchToTabHavingURI doesn't use the blank tab.");
@@ -11,7 +13,11 @@ add_task(async function test_settingsOpen_observer() {
     url: "about:robots"
   }, async function dummyTabTask(aBrowser) {
     
-    let syncPaneLoadedPromise = TestUtils.topicObserved("sync-pane-loaded", () => true);
+    
+    
+    
+    
+    let syncPaneLoadedPromise = useOldPrefs || TestUtils.topicObserved("sync-pane-loaded", () => true);
 
     let tabPromise = BrowserTestUtils.waitForNewTab(gBrowser, expectedURL);
     info("simulate a notifications-open-settings notification");
@@ -36,7 +42,11 @@ add_task(async function test_settingsOpen_button() {
       url: notificationURL
     }, async function tabTask(aBrowser) {
       
-      let syncPaneLoadedPromise = TestUtils.topicObserved("sync-pane-loaded", () => true);
+      
+      
+      
+      
+      let syncPaneLoadedPromise = useOldPrefs || TestUtils.topicObserved("sync-pane-loaded", () => true);
 
       info("Waiting for notification");
       await openNotification(aBrowser, "showNotification2");
