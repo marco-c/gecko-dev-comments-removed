6cd3e6358d43455ae11d240f58b7368d489633fb
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 20:21:10 +0000

    Bug 1414596 - P2. Bump VP9 benchmark version ID. r=gerald
    This will force a re-run of the VP9 benchmark.
    
    MozReview-Commit-ID: 533NpQWrHDS
    
    UltraBlame original commit: 9164ab300d556757b6cd7de3257b068939bee7de

--- dom/media/Benchmark.cpp
+++ dom/media/Benchmark.cpp
@@ -32,7 +32,7 @@ namespace mozilla {
 
 
 
-const uint32_t VP9Benchmark::sBenchmarkVersionID = 2;
+const uint32_t VP9Benchmark::sBenchmarkVersionID = 3;
 
 const char* VP9Benchmark::sBenchmarkFpsPref = "media.benchmark.vp9.fps";
 const char* VP9Benchmark::sBenchmarkFpsVersionCheck = "media.benchmark.vp9.versioncheck";
