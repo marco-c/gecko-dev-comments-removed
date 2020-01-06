fbe586286fa64391711ee3e3a6dc1488567dcf66
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 04:20:47 +0000

    Bug 1374278 - Fix a race condition between GPUChild and CompositorManagerChild when the GPU process crashes. r=dvander
    UltraBlame original commit: b59e819dfaa55698c4c8dfbd26f516e512f15dbd

--- gfx/layers/ipc/CompositorManagerChild.cpp
+++ gfx/layers/ipc/CompositorManagerChild.cpp
@@ -19,19 +19,21 @@ namespace layers {
 StaticRefPtr<CompositorManagerChild> CompositorManagerChild::sInstance;
 
  bool
-CompositorManagerChild::IsInitialized()
+CompositorManagerChild::IsInitialized(base::ProcessId aGPUPid)
 {
   MOZ_ASSERT(NS_IsMainThread());
-  return sInstance && sInstance->CanSend();
+  
+  
+  
+  return sInstance && sInstance->CanSend() && sInstance->OtherPid() == aGPUPid;
 }
 
  bool
 CompositorManagerChild::InitSameProcess(uint32_t aNamespace)
 {
   MOZ_ASSERT(NS_IsMainThread());
-  if (NS_WARN_IF(sInstance &&
-                 sInstance->OtherPid() == base::GetCurrentProcId())) {
-    MOZ_ASSERT_UNREACHABLE("Already initialized");
+  if (NS_WARN_IF(IsInitialized(base::GetCurrentProcId()))) {
+    MOZ_ASSERT_UNREACHABLE("Already initialized same process");
     return false;
   }
 
@@ -47,8 +49,12 @@ CompositorManagerChild::Init(Endpoint<PCompositorManagerChild>&& aEndpoint,
 {
   MOZ_ASSERT(NS_IsMainThread());
   if (sInstance) {
+    
+    
+    
+    
     MOZ_ASSERT(sInstance->mNamespace != aNamespace);
-    MOZ_RELEASE_ASSERT(!sInstance->CanSend());
+    MOZ_RELEASE_ASSERT(sInstance->OtherPid() != aEndpoint.OtherPid());
   }
 
   sInstance = new CompositorManagerChild(Move(aEndpoint), aNamespace);
