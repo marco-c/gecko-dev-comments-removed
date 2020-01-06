72fefd556911d8a84f00f8c8ec3e65bc17821284
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 20:23:15 +0000

    Bug 1414767 - Moving SlicedInputStream under mozilla namespace, r=froydnj
    UltraBlame original commit: 4f5a502acbe831a1d17d5af06efd63c0b3081a8e

--- xpcom/io/SlicedInputStream.h
+++ xpcom/io/SlicedInputStream.h
@@ -14,6 +14,8 @@
 #include "nsIIPCSerializableInputStream.h"
 #include "nsISeekableStream.h"
 
+namespace mozilla {
+
 
 
 class SlicedInputStream final : public nsIAsyncInputStream
@@ -77,5 +79,7 @@ private:
   uint32_t mAsyncWaitFlags;
   uint32_t mAsyncWaitRequestedCount;
 };
+
+} 
 
 #endif 
