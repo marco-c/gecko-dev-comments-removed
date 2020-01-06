a59b97ef7c29f0b838290d0931acfa8567c94ce8
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 03:28:17 +0000

    Bug 1370461 - Add support for stylo metadata in wpt, r=maja_zf
    MozReview-Commit-ID: FdrZtQpb0KB
    
    UltraBlame original commit: a86a509f0d851cc89d0b1e1c1748b6a0566f414c

--- testing/web-platform/tests/tools/wptrunner/wptrunner/wpttest.py
+++ testing/web-platform/tests/tools/wptrunner/wptrunner/wpttest.py
@@ -74,6 +74,8 @@ class RunInfo(dict):
         elif "debug" not in self:
             
             self["debug"] = False
+        if product == "firefox" and "stylo" not in self:
+            self["stylo"] = False
         if extras is not None:
             self.update(extras)
 
