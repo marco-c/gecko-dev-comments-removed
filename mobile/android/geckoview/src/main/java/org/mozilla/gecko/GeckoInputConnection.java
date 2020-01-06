537b3c401b5c505bd57e1f874527dbbba2c9974d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 14:29:38 +0000

    Bug 1400878 - Don't show keyboard on Android unless the user caused it r=jchen
    MozReview-Commit-ID: KURh08X4Q4U
    
    UltraBlame original commit: 7e16f505b25f498935afa0dbc893cb9eef2d1207

--- mobile/android/geckoview/src/main/java/org/mozilla/gecko/GeckoInputConnection.java
+++ mobile/android/geckoview/src/main/java/org/mozilla/gecko/GeckoInputConnection.java
@@ -67,6 +67,7 @@ class GeckoInputConnection
     private String mIMEModeHint = "";
     private String mIMEActionHint = "";
     private boolean mInPrivateBrowsing;
+    private boolean mIsUserAction;
     private boolean mFocused;
 
     private String mCurrentInputMethod = "";
@@ -646,7 +647,10 @@ class GeckoInputConnection
         outAttrs.initialSelStart = Selection.getSelectionStart(editable);
         outAttrs.initialSelEnd = Selection.getSelectionEnd(editable);
 
-        showSoftInput();
+        if (mIsUserAction) {
+            showSoftInput();
+        }
+
         return this;
     }
 
@@ -969,7 +973,7 @@ class GeckoInputConnection
 
     @Override
     public void notifyIMEContext(int state, String typeHint, String modeHint, String actionHint,
-                                 boolean inPrivateBrowsing) {
+                                 boolean inPrivateBrowsing, boolean isUserAction) {
         
         
         
@@ -997,6 +1001,7 @@ class GeckoInputConnection
         mIMEModeHint = (modeHint == null) ? "" : modeHint;
         mIMEActionHint = (actionHint == null) ? "" : actionHint;
         mInPrivateBrowsing = inPrivateBrowsing;
+        mIsUserAction = isUserAction;
 
         
         mUpdateRequest = null;
