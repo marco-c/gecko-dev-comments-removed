4e8a94fd158594cf8e1228dccaec9b686690320b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 17:59:17 +0000

    Scissor the visible region for intermediate surfaces that require redraw. (bug 1408566 part 2, r=mattwoodrow)
    UltraBlame original commit: ba80e255a6064721ce46edeacf180ed47cea3191

--- gfx/layers/mlgpu/RenderViewMLGPU.cpp
+++ gfx/layers/mlgpu/RenderViewMLGPU.cpp
@@ -131,6 +131,13 @@ void
 RenderViewMLGPU::RenderAfterBackdropCopy()
 {
   MOZ_ASSERT(mContainer && mContainer->NeedsSurfaceCopy());
+
+  
+  
+  
+  mInvalidBounds = mContainer->GetShadowVisibleRegion().GetBounds().ToUnknownRect() -
+                   GetTargetOffset();
+
   ExecuteRendering();
 }
 
