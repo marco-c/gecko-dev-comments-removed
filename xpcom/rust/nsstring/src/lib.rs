82294924cd1647385ef4a56342cb3e980cc249dd
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 18:28:14 +0000

    Bug 1403213 - Move nsstring into servo/support/gecko/nsstring, r=froydnj
    MozReview-Commit-ID: FmKtJxSCdYc
    
    UltraBlame original commit: 74c58b336d30628e14b8ed735ae2d6dbe6cf6bbd

--- xpcom/rust/nsstring/src/lib.rs
+++ xpcom/rust/nsstring/src/lib.rs
@@ -108,22 +108,26 @@
 
 
 
+
+
+
+
 #![allow(non_camel_case_types)]
 #![deny(warnings)]
 
 #[macro_use]
 extern crate bitflags;
 
-use std::ops::{Deref, DerefMut};
-use std::marker::PhantomData;
 use std::borrow;
-use std::slice;
-use std::mem;
-use std::fmt;
 use std::cmp;
+use std::fmt;
+use std::marker::PhantomData;
+use std::mem;
+use std::ops::{Deref, DerefMut};
+use std::os::raw::c_void;
+use std::slice;
 use std::str;
 use std::u32;
-use std::os::raw::c_void;
 
 
 
@@ -134,7 +138,7 @@ mod data_flags {
         // While this has the same layout as u16, it cannot be passed
         // over FFI safely as a u16.
         #[repr(C)]
-        pub flags DataFlags : u16 {
+        pub flags DataFlags: u16 {
             const TERMINATED = 1 << 0, // IsTerminated returns true
             const VOIDED = 1 << 1, // IsVoid returns true
             const SHARED = 1 << 2, // mData points to a heap-allocated, shared buffer
@@ -150,15 +154,15 @@ mod class_flags {
         // While this has the same layout as u16, it cannot be passed
         // over FFI safely as a u16.
         #[repr(C)]
-        pub flags ClassFlags : u16 {
+        pub flags ClassFlags: u16 {
             const INLINE = 1 << 0, // |this|'s buffer is inline
             const NULL_TERMINATED = 1 << 1, // |this| requires its buffer is null-terminated
         }
     }
 }
 
-use data_flags::DataFlags;
 use class_flags::ClassFlags;
+use data_flags::DataFlags;
 
 
 
@@ -1004,12 +1008,12 @@ impl cmp::PartialEq<str> for nsAString {
     }
 }
 
-#[cfg(not(debug_assertions))]
+#[cfg(not(feature = "gecko_debug"))]
 #[allow(non_snake_case)]
 unsafe fn Gecko_IncrementStringAdoptCount(_: *mut c_void) {}
 
 extern "C" {
-    #[cfg(debug_assertions)]
+    #[cfg(feature = "gecko_debug")]
     fn Gecko_IncrementStringAdoptCount(data: *mut c_void);
 
     
@@ -1057,17 +1061,10 @@ pub mod test_helpers {
     
     
 
-    use super::{
-        nsCString,
-        nsString,
-        nsCStr,
-        nsStr,
-        nsCStringRepr,
-        nsStringRepr,
-        data_flags,
-        class_flags,
-    };
     use std::mem;
+    use super::{class_flags, data_flags};
+    use super::{nsCStr, nsCString, nsCStringRepr};
+    use super::{nsStr, nsString, nsStringRepr};
 
     
     
