3c0027b1089e3b31fae0a6f31a16ef73d6fa4a59
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 12:10:32 +0000

    Bug 1391003 - Use the new photon line selection and hover background colors in devtool. r=bgrins
    UltraBlame original commit: 95089cbed4161a64d1016b1202be07067c6194ba

--- devtools/client/shared/theme.js
+++ devtools/client/shared/theme.js
@@ -16,7 +16,8 @@ const variableFileContents = require("raw!devtools/client/themes/variables.css")
 const THEME_SELECTOR_STRINGS = {
   light: ":root.theme-light {",
   dark: ":root.theme-dark {",
-  firebug: ":root.theme-firebug {"
+  firebug: ":root.theme-firebug {",
+  root: ":root {",
 };
 const THEME_PREF = "devtools.theme";
 
@@ -61,6 +62,14 @@ const getColor = exports.getColor = (type, theme) => {
   let themeName = theme || getTheme();
   let themeFile = getThemeFile(themeName);
   let match = themeFile.match(new RegExp("--theme-" + type + ": (.*);"));
+  let variableMatch = match ? match[1].match(/var\((.*)\)/) : null;
+
+  
+  
+  if (variableMatch) {
+    themeFile = getThemeFile("root");
+    match = themeFile.match(new RegExp(`${variableMatch[1]}: (.*);`));
+  }
 
   
   return match ? match[1] : null;
