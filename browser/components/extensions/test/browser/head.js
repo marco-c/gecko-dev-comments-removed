0d1e03ce5ccc18f8471a66d66715232cb66f3224
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 19:43:29 +0000

    Backed out 2 changesets (bug 1345433) for eslint bustage
    Backed out changeset 7353f6f68571 (bug 1345433)
    Backed out changeset 6ee966b99e7e (bug 1345433)
    
    MozReview-Commit-ID: 47mqBLmmBpv
    
    UltraBlame original commit: 083251780a8607fe8ae377fc979ca7e83c29b898

--- browser/components/extensions/test/browser/head.js
+++ browser/components/extensions/test/browser/head.js
@@ -36,9 +36,6 @@ const {AppConstants} = Cu.import("resource://gre/modules/AppConstants.jsm", {});
 const {CustomizableUI} = Cu.import("resource:///modules/CustomizableUI.jsm", {});
 const {Preferences} = Cu.import("resource://gre/modules/Preferences.jsm", {});
 
-const {Utils} = Cu.import("resource://gre/modules/sessionstore/Utils.jsm", {});
-const triggeringPrincipal_base64 = Utils.SERIALIZED_SYSTEMPRINCIPAL;
-
 
 
 
