b1d6fa386780d4c69c221f85af553625f734b3e3
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 15:40:06 +0000

    Backed out changeset 0b56649e9680 (bug 1403695) for mass failing tests due to non-local connection. r=backout on a CLOSED TREE
    UltraBlame original commit: a1903b0f0ead5b2952dd4a1b6a41b20a65a07f44

--- testing/profiles/prefs_general.js
+++ testing/profiles/prefs_general.js
@@ -327,10 +327,6 @@ user_pref("browser.search.geoSpecificDefaults", false);
 
 user_pref("extensions.shield-recipe-client.api_url", "");
 
-
-user_pref("browser.ping-centre.staging.endpoint", "");
-user_pref("browser.ping-centre.production.endpoint", "");
-
 user_pref("media.eme.enabled", true);
 
 
