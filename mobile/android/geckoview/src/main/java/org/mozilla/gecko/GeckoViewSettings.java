7d0fae01c8a6493d22ccccb4f7555b6e58c7f17c
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 09:47:36 +0000

    Bug 1369815 - Add display mode to GeckoViewSettings r=esawin
    UltraBlame original commit: d25a5921d961a0f5dc392a785f5d46704ca82bfa

--- mobile/android/geckoview/src/main/java/org/mozilla/gecko/GeckoViewSettings.java
+++ mobile/android/geckoview/src/main/java/org/mozilla/gecko/GeckoViewSettings.java
@@ -22,6 +22,24 @@ public final class GeckoViewSettings {
         }
     }
 
+    public enum DisplayMode {
+        
+        BROWSER(0),
+        MINIMAL_UI(1),
+        STANDALONE(2),
+        FULLSCREEN(3);
+
+        private final int mMode;
+
+        DisplayMode(int mode) {
+            mMode = mode;
+        }
+
+        public int value() {
+            return mMode;
+        }
+    }
+
     
 
 
@@ -32,6 +50,13 @@ public final class GeckoViewSettings {
 
     public static final Key<Boolean> USE_PRIVATE_MODE =
         new Key<Boolean>("usePrivateMode");
+
+    
+
+
+    public static final Key<Boolean> USE_DISPLAY_MODE =
+        new Key<Boolean>("useDisplayMode");
+
     
 
 
@@ -75,6 +100,23 @@ public final class GeckoViewSettings {
     public boolean getBoolean(final Key<Boolean> key) {
         synchronized (mBundle) {
             return mBundle.getBoolean(key.text);
+        }
+    }
+
+    public void setInt(final Key<Boolean> key, int value) {
+        synchronized (mBundle) {
+            final Object old = mBundle.get(key.text);
+            if (old != null && old.equals(value)) {
+                return;
+            }
+            mBundle.putInt(key.text, value);
+        }
+        dispatchUpdate();
+    }
+
+    public int getInt(final Key<Boolean> key) {
+        synchronized (mBundle) {
+            return mBundle.getInt(key.text);
         }
     }
 
