56304e09ddc6613b77fab038b7a45e55b138e5dc
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 12:04:47 +0000

    servo: Merge #18272 - Correct "is sorted" check in profile statistics (from mateon1:fix/profile-sorted-assert); r=emilio
    <!-- Please describe your changes on the following line: -->
    Correct the debug_assert check for whether or not statistics collected by the --profile flag are sorted.
    I'm not sure how I could add a test for this change, and whether that is necessary.
    
    I also wonder if it makes sense to replace the sort_by calls (currently using explicit comparisons) in this file with something like
    ```
    data.sort_by(|a, b| a.partial_cmp(b).expect("no NaN in collected statistics"))
    ```
    
    ---
    <!-- Thank you for contributing to Servo! Please replace each `[ ]` by `[X]` when the step is complete, and replace `__` with appropriate data: -->
    - [X] `./mach build -d` does not report any errors
    - [X] `./mach test-tidy` does not report any errors
    - [X] These changes fix #18270 (github issue number if applicable).
    
    <!-- Either: -->
    - [ ] There are tests for these changes OR
    - [ ] These changes do not require tests because _____
    
    <!-- Also, please make sure that "Allow edits from maintainers" checkbox is checked, so that we can help you if you get stuck somewhere along the way.-->
    
    <!-- Pull requests that do not address these steps are welcome, but they will require additional verification as part of the review process. -->
    
    Source-Repo: https://github.com/servo/servo
    Source-Revision: 2550b47decbc9876d96a459e399658ca54998bda
    
    UltraBlame original commit: 51cb54b738296e7f21f97861049e2a7314ab53f4

--- servo/components/profile/time.rs
+++ servo/components/profile/time.rs
@@ -328,7 +328,7 @@ impl Profiler {
     
     pub fn get_statistics(data: &[f64]) -> (f64, f64, f64, f64) {
         data.iter().fold(-f64::INFINITY, |a, &b| {
-            debug_assert!(a < b, "Data must be sorted");
+            debug_assert!(a <= b, "Data must be sorted");
             b
         });
 
