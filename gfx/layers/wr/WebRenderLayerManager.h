4186c1913bd3f8373c2bfd9a847e06d7baa4a091
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 20:23:33 +0000

    Bug 1411472 - Add support of PersistentBufferProviderShared r=nical
    UltraBlame original commit: 89513063a10631897ae55d14828e965c8ebe7aab

--- gfx/layers/wr/WebRenderLayerManager.h
+++ gfx/layers/wr/WebRenderLayerManager.h
@@ -30,6 +30,9 @@ namespace mozilla {
 
 struct ActiveScrolledRoot;
 
+namespace dom {
+class TabGroup;
+}
 
 namespace layers {
 
@@ -119,6 +122,9 @@ public:
   virtual void SetIsFirstPaint() override { mIsFirstPaint = true; }
   virtual void SetFocusTarget(const FocusTarget& aFocusTarget) override;
 
+  virtual already_AddRefed<PersistentBufferProvider>
+  CreatePersistentBufferProvider(const gfx::IntSize& aSize, gfx::SurfaceFormat aFormat) override;
+
   bool AsyncPanZoomEnabled() const override;
 
   
@@ -156,6 +162,8 @@ public:
 
   void WrUpdated();
   void WindowOverlayChanged() { mWindowOverlayChanged = true; }
+
+  dom::TabGroup* GetTabGroup();
 
 private:
   
