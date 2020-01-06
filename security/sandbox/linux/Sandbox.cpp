6d2cad909626c5cff530b1a82d601ef818373434
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 06:24:04 +0000

    Bug 1376910 - Unshare the SysV IPC namespace in content processes. r=gcp
    MozReview-Commit-ID: 1Uajj68rEuC
    
    UltraBlame original commit: 17e2e2aa8f56546d6749d41266af06b7390df7db

--- security/sandbox/linux/Sandbox.cpp
+++ security/sandbox/linux/Sandbox.cpp
@@ -565,6 +565,7 @@ SandboxEarlyInit(GeckoProcessType aType)
   case GeckoProcessType_Default:
     MOZ_ASSERT(false, "SandboxEarlyInit in parent process");
     return;
+
 #ifdef MOZ_GMP_SANDBOX
   case GeckoProcessType_GMPlugin:
     if (!info.Test(SandboxInfo::kEnabledForMedia)) {
@@ -576,8 +577,18 @@ SandboxEarlyInit(GeckoProcessType aType)
     canChroot = info.Test(SandboxInfo::kHasSeccompBPF);
     break;
 #endif
-    
-    
+
+#ifdef MOZ_CONTENT_SANDBOX
+  case GeckoProcessType_Content:
+    if (!info.Test(SandboxInfo::kEnabledForContent)) {
+      break;
+    }
+#ifndef MOZ_ALSA
+    canUnshareIPC = true;
+#endif
+    break;
+#endif
+
   default:
     
     break;
