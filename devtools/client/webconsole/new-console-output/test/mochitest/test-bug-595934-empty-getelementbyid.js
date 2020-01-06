a80642805186e364afa4ae904aa7bdba3181aa3e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 16:45:22 +0000

    Backed out changeset 8149514f0473 (bug 1406042) for failing devtools/client/webconsole/new-console-output/test/mochitest/browser_webconsole_property_provider.js. r=backout
    UltraBlame original commit: e27e466cec06d42e12328206bc5026bd68d5f7fa

--- devtools/client/webconsole/new-console-output/test/mochitest/test-bug-595934-empty-getelementbyid.js
+++
@@ -1,8 +0,0 @@
-
-
-
-
-
-window.addEventListener("load", function () {
-  document.getElementById("");
-});
--- devtools/client/webconsole/new-console-output/test/mochitest/test-bug-601177-log-levels.js
+++
@@ -1,8 +0,0 @@
-
-
-
-
-
-foobarBug601177strictError = "strict error";
-
-window.foobarBug601177exception();
