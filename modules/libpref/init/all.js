dfe5c50c462d20cd67f1401ebd371c7bf074c1f5
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 09:32:54 +0000

    Bug 1351148 Part12: Disable input event queue until related bugs are fixed. r=smaug.
    MozReview-Commit-ID: 74cycVYPe20
    
    UltraBlame original commit: 54d481f0dbf1311df2dafc1101240c0ede76ada7

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -3122,7 +3122,7 @@ pref("idle_queue.long_period", 50);
 
 
 #ifdef NIGHTLY_BUILD
-pref("prioritized_input_events.enabled", true);
+pref("prioritized_input_events.enabled", false);
 #else
 pref("prioritized_input_events.enabled", false);
 #endif
