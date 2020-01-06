afffc699ea6dfa2f40715816adbd1df04975bf20
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 02:01:23 +0000

    Bug 1369140 - Part 1: Revert to only flushing styles when checking whether an element is focusable; r=bzbarsky
    Bug 612018 made us flush layout for caret painting which isn't triggered
    from anything that can call this code any mode, therefore we can revert
    the change from that bug.
    
    UltraBlame original commit: d44994c678733c604990ecaca9dadbc9693c6625

--- dom/base/nsFocusManager.cpp
+++ dom/base/nsFocusManager.cpp
@@ -1569,7 +1569,7 @@ nsFocusManager::CheckIfFocusable(nsIContent* aContent, uint32_t aFlags)
 
   
   mEventHandlingNeedsFlush = false;
-  doc->FlushPendingNotifications(FlushType::Layout);
+  doc->FlushPendingNotifications(FlushType::Frames);
 
   nsIPresShell *shell = doc->GetShell();
   if (!shell)
