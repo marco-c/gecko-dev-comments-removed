9698bc3b370d0431190284341f39869d6d613b07
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 11:32:56 +0000

    Bug 1389752 - Throw TypeError if [[OwnPropertyKeys]] of scripted proxies contains duplicates. r=till, r=qdot
    UltraBlame original commit: 9affea4e745fbb4a16aa7ef01da540426e1403ac

--- js/src/jit-test/tests/proxy/testDirectProxyKeys4.js
+++ js/src/jit-test/tests/proxy/testDirectProxyKeys4.js
@@ -1,5 +1,7 @@
 load(libdir + "asserts.js");
 
+
+
 var handler = { ownKeys: () => [ 'foo', 'foo' ] };
 for (let p of [new Proxy({}, handler), Proxy.revocable({}, handler).proxy])
-    assertDeepEq(Object.keys(p), []); 
+    assertThrowsInstanceOf(() => Object.keys(p), TypeError);
