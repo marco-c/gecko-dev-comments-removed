06a3e9c849adf520da2ba531a6a16de90444a782
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 05:11:37 +0000

    Bug 1367694 - Set payment request's shippingOption and shippingType attributes when creating it. r=baku
    Per spec, given details and options parameters of PaymentRequest constructor,
    1) if details.shippingOptions exists and IDs of all shipping options are unique,
       request's shippingOption will be set to the last selected option's ID.
       Otherwise, set request's shippingOption to null.
    2) if options.requestShipping is ture, set request's shippingType to
       options.shippingType. Otherwise, set request's shippingType to null.
    
    UltraBlame original commit: 251b82b42ba23763f559122b72ce20341ed43096

--- dom/payments/PaymentRequestManager.cpp
+++ dom/payments/PaymentRequestManager.cpp
@@ -103,7 +103,8 @@ nsresult
 ConvertDetailsBase(const PaymentDetailsBase& aDetails,
                    nsTArray<IPCPaymentItem>& aDisplayItems,
                    nsTArray<IPCPaymentShippingOption>& aShippingOptions,
-                   nsTArray<IPCPaymentDetailsModifier>& aModifiers)
+                   nsTArray<IPCPaymentDetailsModifier>& aModifiers,
+                   bool aResetShippingOptions)
 {
   if (aDetails.mDisplayItems.WasPassed()) {
     for (const PaymentItem& item : aDetails.mDisplayItems.Value()) {
@@ -112,7 +113,7 @@ ConvertDetailsBase(const PaymentDetailsBase& aDetails,
       aDisplayItems.AppendElement(displayItem);
     }
   }
-  if (aDetails.mShippingOptions.WasPassed()) {
+  if (aDetails.mShippingOptions.WasPassed() && !aResetShippingOptions) {
     for (const PaymentShippingOption& option : aDetails.mShippingOptions.Value()) {
       IPCPaymentShippingOption shippingOption;
       ConvertShippingOption(option, shippingOption);
@@ -134,13 +135,14 @@ ConvertDetailsBase(const PaymentDetailsBase& aDetails,
 
 nsresult
 ConvertDetailsInit(const PaymentDetailsInit& aDetails,
-                   IPCPaymentDetails& aIPCDetails)
+                   IPCPaymentDetails& aIPCDetails,
+                   bool aResetShippingOptions)
 {
   
   nsTArray<IPCPaymentItem> displayItems;
   nsTArray<IPCPaymentShippingOption> shippingOptions;
   nsTArray<IPCPaymentDetailsModifier> modifiers;
-  nsresult rv = ConvertDetailsBase(aDetails, displayItems, shippingOptions, modifiers);
+  nsresult rv = ConvertDetailsBase(aDetails, displayItems, shippingOptions, modifiers, aResetShippingOptions);
   if (NS_WARN_IF(NS_FAILED(rv))) {
     return rv;
   }
@@ -175,7 +177,10 @@ ConvertDetailsUpdate(const PaymentDetailsUpdate& aDetails,
   nsTArray<IPCPaymentItem> displayItems;
   nsTArray<IPCPaymentShippingOption> shippingOptions;
   nsTArray<IPCPaymentDetailsModifier> modifiers;
-  nsresult rv = ConvertDetailsBase(aDetails, displayItems, shippingOptions, modifiers);
+  
+  
+  nsresult rv = ConvertDetailsBase(
+    aDetails, displayItems, shippingOptions, modifiers, false);
   if (NS_WARN_IF(NS_FAILED(rv))) {
     return rv;
   }
@@ -352,6 +357,37 @@ PaymentRequestManager::GetPaymentRequestById(const nsAString& aRequestId)
   return nullptr;
 }
 
+void
+GetSelectedShippingOption(const PaymentDetailsInit& aDetails,
+                          nsAString& aOption,
+                          bool* aResetOptions)
+{
+  SetDOMStringToNull(aOption);
+  if (!aDetails.mShippingOptions.WasPassed()) {
+    return;
+  }
+
+  nsTArray<nsString> seenIDs;
+  const Sequence<PaymentShippingOption>& shippingOptions =
+    aDetails.mShippingOptions.Value();
+  for (const PaymentShippingOption& shippingOption : shippingOptions) {
+    
+    
+    
+    if (seenIDs.Contains(shippingOption.mId)) {
+      SetDOMStringToNull(aOption);
+      *aResetOptions = true;
+      return;
+    }
+    seenIDs.AppendElement(shippingOption.mId);
+
+    
+    if (shippingOption.mSelected) {
+      aOption = shippingOption.mId;
+    }
+  }
+}
+
 nsresult
 PaymentRequestManager::CreatePayment(nsPIDOMWindowInner* aWindow,
                                      const Sequence<PaymentMethodData>& aMethodData,
@@ -362,26 +398,7 @@ PaymentRequestManager::CreatePayment(nsPIDOMWindowInner* aWindow,
   MOZ_ASSERT(NS_IsMainThread());
   NS_ENSURE_ARG_POINTER(aRequest);
   *aRequest = nullptr;
-
   nsresult rv;
-  nsTArray<IPCPaymentMethodData> methodData;
-  for (const PaymentMethodData& data : aMethodData) {
-    IPCPaymentMethodData ipcMethodData;
-    rv = ConvertMethodData(data, ipcMethodData);
-    if (NS_WARN_IF(NS_FAILED(rv))) {
-      return rv;
-    }
-    methodData.AppendElement(ipcMethodData);
-  }
-
-  IPCPaymentDetails details;
-  rv = ConvertDetailsInit(aDetails, details);
-  if (NS_WARN_IF(NS_FAILED(rv))) {
-    return rv;
-  }
-
-  IPCPaymentOptions options;
-  ConvertOptions(aOptions, options);
 
   RefPtr<PaymentRequest> request = PaymentRequest::CreatePaymentRequest(aWindow, rv);
   if (NS_WARN_IF(NS_FAILED(rv))) {
@@ -400,8 +417,48 @@ PaymentRequestManager::CreatePayment(nsPIDOMWindowInner* aWindow,
   }
   request->SetId(requestId);
 
+  
+
+
+
+
+
+
+  nsAutoString shippingOption;
+  bool resetShippingOptions = false;
+  GetSelectedShippingOption(aDetails, shippingOption, &resetShippingOptions);
+  request->SetShippingOption(shippingOption);
+
+  
+
+
+  if (aOptions.mRequestShipping) {
+    request->SetShippingType(
+        Nullable<PaymentShippingType>(aOptions.mShippingType));
+  }
+
   nsAutoString internalId;
   request->GetInternalId(internalId);
+
+  nsTArray<IPCPaymentMethodData> methodData;
+  for (const PaymentMethodData& data : aMethodData) {
+    IPCPaymentMethodData ipcMethodData;
+    rv = ConvertMethodData(data, ipcMethodData);
+    if (NS_WARN_IF(NS_FAILED(rv))) {
+      return rv;
+    }
+    methodData.AppendElement(ipcMethodData);
+  }
+
+  IPCPaymentDetails details;
+  rv = ConvertDetailsInit(aDetails, details, resetShippingOptions);
+  if (NS_WARN_IF(NS_FAILED(rv))) {
+    return rv;
+  }
+
+  IPCPaymentOptions options;
+  ConvertOptions(aOptions, options);
+
   IPCPaymentCreateActionRequest action(internalId,
                                        methodData,
                                        details,
@@ -488,6 +545,12 @@ PaymentRequestManager::UpdatePayment(const nsAString& aRequestId,
   if (!request) {
     return NS_ERROR_UNEXPECTED;
   }
+
+  
+  
+  
+  
+  
 
   IPCPaymentDetails details;
   nsresult rv = ConvertDetailsUpdate(aDetails, details);
