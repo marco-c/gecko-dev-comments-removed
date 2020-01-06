2107d87a1fbfe55ce7bb552733819d12483f30ce
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 18:28:01 +0000

    Backed out changeset fb90c2538141 (bug 1407331) for build bustage CLOSED TREE
    MozReview-Commit-ID: 2Z6OSmyov6P
    
    UltraBlame original commit: a1d57c259c8e9e0b5156dbd8f8cb42c294e94059

--- testing/mozharness/configs/single_locale/dev-mozilla-beta.py
+++ testing/mozharness/configs/single_locale/dev-mozilla-beta.py
@@ -1,7 +1,7 @@
 config = {
     "branch": "jamun",
     "nightly_build": True,
-    "update_channel": "beta",
+    "update_channel": "beta-dev",
 
     
     "hg_l10n_base": "https://hg.mozilla.org/l10n-central",
