d18acacba44d2ff13afd01f0a6b121eb37593ed5
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 02:01:57 +0000

    Bug 1363505 - Add tab squeeze reflow test. r=florian
    MozReview-Commit-ID: Jd7uVrNaMbh
    
    UltraBlame original commit: 5e037ab2ee52aa9d1e26a8143f7967fa2dac0e25

--- browser/base/content/test/performance/browser_tabopen_squeeze_reflows.js
+++ browser/base/content/test/performance/browser_tabopen_squeeze_reflows.js
@@ -1,6 +1,3 @@
-
-
-
 "use strict";
 
 
@@ -13,12 +10,13 @@
 
 
 const EXPECTED_REFLOWS = [
-  
-  
   [
     "select@chrome://global/content/bindings/textbox.xml",
+    "focusAndSelectUrlBar@chrome://browser/content/browser.js",
+    "_adjustFocusAfterTabSwitch@chrome://browser/content/tabbrowser.xml",
   ],
 ];
+
 
 
 
@@ -31,19 +29,24 @@ add_task(async function() {
   
   
   
+  const TAB_COUNT_FOR_SQUEEZE = computeMaxTabCount() - 1;
+
+  await createTabs(TAB_COUNT_FOR_SQUEEZE);
+
+  
+  
+  
+  
   
   let origTab = gBrowser.selectedTab;
 
-  
   await withReflowObserver(async function() {
     let switchDone = BrowserTestUtils.waitForEvent(window, "TabSwitchDone");
     BrowserOpenTab();
     await BrowserTestUtils.waitForEvent(gBrowser.selectedTab, "transitionend",
-        false, e => e.propertyName === "max-width");
+      false, e => e.propertyName === "max-width");
     await switchDone;
   }, EXPECTED_REFLOWS, window, origTab);
 
-  let switchDone = BrowserTestUtils.waitForEvent(window, "TabSwitchDone");
-  await BrowserTestUtils.removeTab(gBrowser.selectedTab);
-  await switchDone;
+  await removeAllButFirstTab();
 });
