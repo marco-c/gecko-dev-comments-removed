611a298bc6cce0e7d209b0c3124448a3e2875fc9
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 21:39:44 +0000

    Bug 1417442 - P3. Don't enable nv12 surfaces if we failed to parse the version string. r=mattwoodrow
    MozReview-Commit-ID: 1MKtCOO1gcS
    
    UltraBlame original commit: eeb2f534cf100cb58fb65a3fc33f4fb9512c4da8

--- dom/media/platforms/wmf/DXVA2Manager.cpp
+++ dom/media/platforms/wmf/DXVA2Manager.cpp
@@ -1265,7 +1265,7 @@ DXVA2Manager::IsNV12Supported(uint32_t aVendorID,
     
     
     uint64_t driverVersion;
-    if (widget::ParseDriverVersion(aDriverVersionString, &driverVersion) &&
+    if (!widget::ParseDriverVersion(aDriverVersionString, &driverVersion) ||
         driverVersion < widget::V(21, 19, 411, 0)) {
       return false;
     }
