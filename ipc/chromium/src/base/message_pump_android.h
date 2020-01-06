a1ba3f170fe104a99fb137479d9befb18c6391dd
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 12:45:41 +0000

    Bug 1396869 - eliminate -Wunused-private-field warnings in Android's message pump; r=jld
    Android's MessagePumpForUI includes some useless code in its
    MessagePumpAndroid class, which is completely unused.  Let's go ahead
    and delete that.
    
    UltraBlame original commit: 366b9e432a3067e28bffba14fe9e538c62a584a9

--- ipc/chromium/src/base/message_pump_android.h
+++ ipc/chromium/src/base/message_pump_android.h
@@ -12,18 +12,6 @@
 
 namespace base {
 
-class MessagePumpForUI;
-
-class MessagePumpAndroid {
-
- public:
-  MessagePumpAndroid(MessagePumpForUI &pump);
-  ~MessagePumpAndroid();
-
- private:
-  base::MessagePumpForUI &pump;
-};
-
 
 
 class MessagePumpForUI : public MessagePump {
@@ -65,9 +53,6 @@ class MessagePumpForUI : public MessagePump {
   TimeTicks delayed_work_time_;
 
   bool work_scheduled;
-
-  
-  MessagePumpAndroid pump;
 
   DISALLOW_COPY_AND_ASSIGN(MessagePumpForUI);
 };
