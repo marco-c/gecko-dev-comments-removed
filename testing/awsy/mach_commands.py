4e441bd0fae35058e4b27907664aa63e2ff921fc
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 16:36:03 +0000

    Bug 1395540 - Part 4: Fix the DMD stacks. r=njn
    This runs the DMD files through the DMD stack fixer so that we end up with
    usable stacks instead of just offsets into libxul.
    
    UltraBlame original commit: 1d4a667909cc526489cccd8860fd2b1ca2d55581

--- testing/awsy/mach_commands.py
+++ testing/awsy/mach_commands.py
@@ -171,6 +171,10 @@ class MachCommands(MachCommandBase):
             for k, v in env_vars[arch].iteritems():
                 os.environ[k] = v
 
+            
+            if bin_dir not in sys.path:
+                sys.path.append(bin_dir)
+
         for k, v in kwargs.iteritems():
             setattr(args, k, v)
 
