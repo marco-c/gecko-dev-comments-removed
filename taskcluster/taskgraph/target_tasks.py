7fc529e5c609bcd9c5a247ac1265b192736f18ad
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 19:40:38 +0000

    bug 1412690 - allow for relpro target_tasks_method. r=rail
    This patch creates a new `filter_beta_release_tasks` function. By
    pulling this logic out of `mozilla_beta_tasks`, we can reuse it in the
    release promotion `target_tasks_method`s.
    
    MozReview-Commit-ID: Kwk3jgtooCS
    
    UltraBlame original commit: 4adbc92314b51d11888f57b38f13b187315d93ca

--- taskcluster/taskgraph/target_tasks.py
+++ taskcluster/taskgraph/target_tasks.py
@@ -42,6 +42,49 @@ def filter_upload_symbols(task, parameters):
         parameters.get('project') in ('mozilla-beta', 'mozilla-release')
 
 
+def filter_beta_release_tasks(task, parameters, ignore_kinds=None, allow_l10n=False):
+    if not standard_filter(task, parameters):
+        return False
+    if ignore_kinds is None:
+        ignore_kinds = [
+            'balrog',
+            'beetmover', 'beetmover-checksums', 'beetmover-l10n',
+            'beetmover-repackage', 'beetmover-repackage-signing',
+            'checksums-signing',
+            'nightly-l10n', 'nightly-l10n-signing',
+            'push-apk', 'push-apk-breakpoint',
+            'repackage-l10n',
+        ]
+    platform = task.attributes.get('build_platform')
+    if platform in (
+            
+            'linux-pgo', 'linux64-pgo',
+            'win32-pgo', 'win64-pgo',
+            'android-api-16-nightly', 'android-x86-nightly'
+            ):
+        return False
+
+    if platform in (
+            'linux', 'linux64',
+            'macosx64',
+            'win32', 'win64',
+            ):
+        if task.attributes['build_type'] == 'opt' and \
+           task.attributes.get('unittest_suite') != 'talos':
+            return False
+
+    
+    if task.kind in ignore_kinds:
+        return False
+
+    
+    
+    if not allow_l10n and task.attributes.get('locale', '') != '':
+        return False
+
+    return True
+
+
 def standard_filter(task, parameters):
     return all(
         filter_func(task, parameters) for filter_func in
@@ -240,58 +283,55 @@ def target_tasks_nightly_linux(full_task_graph, parameters):
 @_target_task('mozilla_beta_tasks')
 def target_tasks_mozilla_beta(full_task_graph, parameters):
     """Select the set of tasks required for a promotable beta or release build
-    of linux, plus android CI. The candidates build process involves a pipeline
+    of desktop, plus android CI. The candidates build process involves a pipeline
     of builds and signing, but does not include beetmover or balrog jobs."""
 
-    def filter(task):
-        if not standard_filter(task, parameters):
-            return False
-        platform = task.attributes.get('build_platform')
-        if platform in (
-                
-                'linux-pgo', 'linux64-pgo',
-                'win32-pgo', 'win64-pgo',
-                'android-api-16-nightly', 'android-x86-nightly'
-                ):
-            return False
-
-        if platform in (
-                'linux', 'linux64',
-                'macosx64',
-                'win32', 'win64',
-                ):
-            if task.attributes['build_type'] == 'opt' and \
-               task.attributes.get('unittest_suite') != 'talos':
-                return False
-
-        
-        if task.kind in [
-            'balrog',
-            'beetmover', 'beetmover-checksums', 'beetmover-l10n',
-            'beetmover-repackage', 'beetmover-repackage-signing',
-            'checksums-signing',
-            'nightly-l10n', 'nightly-l10n-signing',
-            'push-apk', 'push-apk-breakpoint',
-            'repackage-l10n',
-        ]:
-            return False
-
-        
-        
-        if task.attributes.get('locale', '') != '':
-            return False
-
-        return True
-
-    return [l for l, t in full_task_graph.tasks.iteritems() if filter(t)]
+    return [l for l, t in full_task_graph.tasks.iteritems() if
+            filter_beta_release_tasks(t, parameters)]
 
 
 @_target_task('mozilla_release_tasks')
 def target_tasks_mozilla_release(full_task_graph, parameters):
     """Select the set of tasks required for a promotable beta or release build
-    of linux, plus android CI. The candidates build process involves a pipeline
+    of desktop, plus android CI. The candidates build process involves a pipeline
     of builds and signing, but does not include beetmover or balrog jobs."""
-    return target_tasks_mozilla_beta(full_task_graph, parameters)
+
+    return [l for l, t in full_task_graph.tasks.iteritems() if
+            filter_beta_release_tasks(t, parameters)]
+
+
+@_target_task('mozilla_beta_desktop_promotion')
+def target_tasks_mozilla_beta_desktop_promotion(full_task_graph, parameters):
+    """Select the superset of tasks required to promote a beta or release build
+    of desktop. This should include all non-android mozilla_beta tasks, plus
+    l10n, beetmover, balrog, etc."""
+
+    beta_tasks = [l for l, t in full_task_graph.tasks.iteritems() if
+                  filter_beta_release_tasks(t, parameters,
+                                            ignore_kinds=[],
+                                            allow_l10n=True)]
+    allow_kinds = [
+        'build', 'build-signing', 'repackage', 'repackage-signing',
+        'nightly-l10n', 'nightly-l10n-signing', 'repackage-l10n',
+    ]
+
+    def filter(task):
+        platform = task.attributes.get('build_platform')
+
+        
+        if platform and 'android' in platform:
+            return False
+
+        if task.kind not in allow_kinds:
+            return False
+
+        
+        
+        
+        if task.label in beta_tasks:
+            return True
+
+    return [l for l, t in full_task_graph.tasks.iteritems() if filter(t)]
 
 
 @_target_task('candidates_fennec')
