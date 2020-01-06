1080ef2e9e3da965d77f102054130d2e7cf07cb8
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 20:04:13 +0000

    Bug 1407561 - Lazy loading of tooltip text when user hovers the status column. r=gasolin
    MozReview-Commit-ID: BXP8B10Q5LT
    
    UltraBlame original commit: 2c77266e1b40c7a67901de4c3ccca2554422da4a

--- devtools/client/netmonitor/test/browser_net_filter-flags.js
+++ devtools/client/netmonitor/test/browser_net_filter-flags.js
@@ -158,190 +158,198 @@ add_task(function* () {
 
   
   setFreetextFilter("is:running");
-  testContents([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+  yield testContents([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
 
   
   setFreetextFilter("is:from-cache");
-  testContents([0, 0, 0, 0, 0, 0, 0, 0, 0, 1]);
+  yield testContents([0, 0, 0, 0, 0, 0, 0, 0, 0, 1]);
 
   setFreetextFilter("is:cached");
-  testContents([0, 0, 0, 0, 0, 0, 0, 0, 0, 1]);
+  yield testContents([0, 0, 0, 0, 0, 0, 0, 0, 0, 1]);
 
   
   setFreetextFilter("-is:from-cache");
-  testContents([1, 1, 1, 1, 1, 1, 1, 1, 1, 0]);
+  yield testContents([1, 1, 1, 1, 1, 1, 1, 1, 1, 0]);
 
   setFreetextFilter("-is:cached");
-  testContents([1, 1, 1, 1, 1, 1, 1, 1, 1, 0]);
+  yield testContents([1, 1, 1, 1, 1, 1, 1, 1, 1, 0]);
 
   
   setFreetextFilter("status-code:200");
-  testContents([1, 1, 1, 1, 1, 1, 1, 1, 1, 0]);
+  yield testContents([1, 1, 1, 1, 1, 1, 1, 1, 1, 0]);
 
   
   setFreetextFilter("-status-code:200");
-  testContents([0, 0, 0, 0, 0, 0, 0, 0, 0, 1]);
+  yield testContents([0, 0, 0, 0, 0, 0, 0, 0, 0, 1]);
 
   
   setFreetextFilter("mime-type:HtmL");
-  testContents([1, 1, 0, 0, 0, 0, 0, 0, 0, 0]);
+  yield testContents([1, 1, 0, 0, 0, 0, 0, 0, 0, 0]);
 
   
   setFreetextFilter("-mime-type:HtmL");
-  testContents([0, 0, 1, 1, 1, 1, 1, 1, 1, 1]);
+  yield testContents([0, 0, 1, 1, 1, 1, 1, 1, 1, 1]);
 
   
   setFreetextFilter("method:get");
-  testContents([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]);
+  yield testContents([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]);
 
   
   setFreetextFilter("method:post");
-  testContents([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+  yield testContents([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
 
   
   setFreetextFilter("scheme:http");
-  testContents([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]);
+  yield testContents([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]);
 
   setFreetextFilter("scheme:https");
-  testContents([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+  yield testContents([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
 
   
   setFreetextFilter("regexp:content.*?Sam");
-  testContents([1, 1, 0, 0, 0, 0, 0, 0, 0, 0]);
+  yield testContents([1, 1, 0, 0, 0, 0, 0, 0, 0, 0]);
 
   
   setFreetextFilter("set-cookie-name:name2");
-  testContents([0, 1, 0, 0, 0, 0, 0, 0, 0, 0]);
+  yield testContents([0, 1, 0, 0, 0, 0, 0, 0, 0, 0]);
 
   setFreetextFilter("set-cookie-name:not-existing");
-  testContents([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+  yield testContents([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
 
   
   setFreetextFilter("set-cookie-value:value2");
-  testContents([0, 1, 0, 0, 0, 0, 0, 0, 0, 0]);
+  yield testContents([0, 1, 0, 0, 0, 0, 0, 0, 0, 0]);
 
   setFreetextFilter("set-cookie-value:not-existing");
-  testContents([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+  yield testContents([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
 
   
   setFreetextFilter("set-cookie-domain:.example.com");
-  testContents([0, 1, 0, 0, 0, 0, 0, 0, 0, 0]);
+  yield testContents([0, 1, 0, 0, 0, 0, 0, 0, 0, 0]);
 
   setFreetextFilter("set-cookie-domain:.foo.example.com");
-  testContents([0, 1, 0, 0, 0, 0, 0, 0, 0, 0]);
+  yield testContents([0, 1, 0, 0, 0, 0, 0, 0, 0, 0]);
 
   setFreetextFilter("set-cookie-domain:.not-existing.example.com");
-  testContents([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+  yield testContents([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
 
   
   setFreetextFilter("size:-1");
-  testContents([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+  yield testContents([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
 
   setFreetextFilter("size:0");
-  testContents([0, 0, 0, 0, 1, 1, 1, 1, 0, 1]);
+  yield testContents([0, 0, 0, 0, 1, 1, 1, 1, 0, 1]);
 
   setFreetextFilter("size:34");
-  testContents([0, 0, 1, 1, 0, 0, 0, 0, 0, 0]);
+  yield testContents([0, 0, 1, 1, 0, 0, 0, 0, 0, 0]);
 
   
   setFreetextFilter("size:9.659k");
-  testContents([0, 0, 0, 0, 0, 0, 0, 0, 1, 0]);
+  yield testContents([0, 0, 0, 0, 0, 0, 0, 0, 1, 0]);
 
   
   setFreetextFilter("size:10989");
-  testContents([0, 0, 0, 0, 0, 0, 0, 0, 1, 0]);
+  yield testContents([0, 0, 0, 0, 0, 0, 0, 0, 1, 0]);
 
   
   setFreetextFilter("size:11.804k");
-  testContents([0, 0, 0, 0, 0, 0, 0, 0, 1, 0]);
+  yield testContents([0, 0, 0, 0, 0, 0, 0, 0, 1, 0]);
 
   
   setFreetextFilter("transferred:200");
-  testContents([0, 0, 0, 0, 1, 1, 1, 1, 0, 0]);
+  yield testContents([0, 0, 0, 0, 1, 1, 1, 1, 0, 0]);
 
   setFreetextFilter("transferred:234");
-  testContents([1, 0, 1, 0, 0, 0, 0, 0, 0, 1]);
+  yield testContents([1, 0, 1, 0, 0, 0, 0, 0, 0, 1]);
 
   setFreetextFilter("transferred:248");
-  testContents([0, 0, 1, 1, 0, 0, 0, 0, 0, 1]);
+  yield testContents([0, 0, 1, 1, 0, 0, 0, 0, 0, 1]);
 
   
   setFreetextFilter("larger-than:-1");
-  testContents([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]);
+  yield testContents([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]);
 
   setFreetextFilter("larger-than:0");
-  testContents([1, 1, 1, 1, 0, 0, 0, 0, 1, 0]);
+  yield testContents([1, 1, 1, 1, 0, 0, 0, 0, 1, 0]);
 
   setFreetextFilter("larger-than:33");
-  testContents([0, 0, 1, 1, 0, 0, 0, 0, 1, 0]);
+  yield testContents([0, 0, 1, 1, 0, 0, 0, 0, 1, 0]);
 
   setFreetextFilter("larger-than:34");
-  testContents([0, 0, 0, 0, 0, 0, 0, 0, 1, 0]);
+  yield testContents([0, 0, 0, 0, 0, 0, 0, 0, 1, 0]);
 
   setFreetextFilter("larger-than:10.73k");
-  testContents([0, 0, 0, 0, 0, 0, 0, 0, 1, 0]);
+  yield testContents([0, 0, 0, 0, 0, 0, 0, 0, 1, 0]);
 
   setFreetextFilter("larger-than:10.732k");
-  testContents([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+  yield testContents([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
 
   
   setFreetextFilter("transferred-larger-than:-1");
-  testContents([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]);
+  yield testContents([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]);
 
   setFreetextFilter("transferred-larger-than:214");
-  testContents([1, 1, 1, 1, 0, 0, 0, 0, 1, 1]);
+  yield testContents([1, 1, 1, 1, 0, 0, 0, 0, 1, 1]);
 
   setFreetextFilter("transferred-larger-than:247");
-  testContents([0, 1, 1, 1, 0, 0, 0, 0, 1, 0]);
+  yield testContents([0, 1, 1, 1, 0, 0, 0, 0, 1, 0]);
 
   setFreetextFilter("transferred-larger-than:248");
-  testContents([0, 1, 0, 1, 0, 0, 0, 0, 1, 0]);
+  yield testContents([0, 1, 0, 1, 0, 0, 0, 0, 1, 0]);
 
   setFreetextFilter("transferred-larger-than:10.73k");
-  testContents([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+  yield testContents([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
 
   
   setFreetextFilter("cause:xhr");
-  testContents([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]);
+  yield testContents([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]);
 
   setFreetextFilter("cause:script");
-  testContents([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+  yield testContents([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
 
   
   setFreetextFilter("has-response-header:Content-Type");
-  testContents([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]);
+  yield testContents([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]);
 
   setFreetextFilter("has-response-header:Last-Modified");
-  testContents([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+  yield testContents([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
 
   
   setFreetextFilter("remote-ip:127.0.0.1");
-  testContents([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]);
+  yield testContents([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]);
 
   setFreetextFilter("remote-ip:192.168.1.2");
-  testContents([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+  yield testContents([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
 
   
   setFreetextFilter("domain:example.com");
-  testContents([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]);
+  yield testContents([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]);
 
   setFreetextFilter("domain:wrongexample.com");
-  testContents([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+  yield testContents([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
 
   
   setFreetextFilter("protocol:http/1");
-  testContents([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]);
+  yield testContents([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]);
 
   setFreetextFilter("protocol:http/2");
-  testContents([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+  yield testContents([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
 
   
   setFreetextFilter("-mime-type:HtmL status-code:200");
-  testContents([0, 0, 1, 1, 1, 1, 1, 1, 1, 0]);
+  yield testContents([0, 0, 1, 1, 1, 1, 1, 1, 1, 0]);
 
   yield teardown(monitor);
 
-  function testContents(visibility) {
+  function* testContents(visibility) {
+    let requestItems = document.querySelectorAll(".request-list-item");
+    for (let requestItem of requestItems) {
+      requestItem.scrollIntoView();
+      let requestsListStatus = requestItem.querySelector(".requests-list-status");
+      EventUtils.sendMouseEvent({ type: "mouseover" }, requestsListStatus);
+      yield waitUntil(() => requestsListStatus.title);
+    }
+
     const items = getSortedRequests(store.getState());
     const visibleItems = getDisplayedRequests(store.getState());
 
