5d2573517c3693a7b8385c1eb68be166bb72629c
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 01:41:03 +0000

    Bug 1367870 - Add stringification helper for WrRect. r=jrmuizel
    MozReview-Commit-ID: FMw6tPI2LOJ
    
    UltraBlame original commit: 25c281fca778ac22d64857128e426a43fa6543e3

--- gfx/layers/LayersLogging.h
+++ gfx/layers/LayersLogging.h
@@ -18,6 +18,8 @@
 #include "nsRegion.h"                   
 #include "nscore.h"                     
 
+struct WrRect;
+
 namespace mozilla {
 namespace gfx {
 template <class units, class F> struct RectTyped;
@@ -86,6 +88,10 @@ AppendToString(std::stringstream& aStream, const mozilla::gfx::IntRectTyped<T>& 
     r.x, r.y, r.width, r.height).get();
   aStream << sfx;
 }
+
+void
+AppendToString(std::stringstream& aStream, const WrRect& r,
+               const char* pfx="", const char* sfx="");
 
 void
 AppendToString(std::stringstream& aStream, const nsRegion& r,
