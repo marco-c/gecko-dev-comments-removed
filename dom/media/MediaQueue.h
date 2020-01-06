fbdc8263d656ade6dd98b80bb8086af4c99c994a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 13:27:41 +0000

    Bug 1398999 - nsDequeFunctor::operator() doesn't need to return anything - r=froydnj
    Its return value is never used, and most implementations return nullptr anyway.
    
    MozReview-Commit-ID: 8rxC053mmE8
    
    UltraBlame original commit: 1a96a71425be91b3121acf64e08188307a89b965

--- dom/media/MediaQueue.h
+++ dom/media/MediaQueue.h
@@ -18,9 +18,9 @@ namespace mozilla {
 
 template <class T>
 class MediaQueueDeallocator : public nsDequeFunctor {
-  virtual void* operator() (void* aObject) {
+  virtual void operator()(void* aObject)
+  {
     RefPtr<T> releaseMe = dont_AddRef(static_cast<T*>(aObject));
-    return nullptr;
   }
 };
 
