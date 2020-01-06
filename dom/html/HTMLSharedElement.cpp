dad6ba90c94f684bf8f3a21c1703cedd8261e4d1
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 16:38:47 +0000

    Bug 1405169 - Fix/Simplify Refcount and CC macros for HTML Element classes; r=bz
    MozReview-Commit-ID: CV77bM0Pq1e
    
    UltraBlame original commit: 7d914237e51c27365601978eaf9a7a846495239c

--- dom/html/HTMLSharedElement.cpp
+++ dom/html/HTMLSharedElement.cpp
@@ -31,8 +31,8 @@ HTMLSharedElement::~HTMLSharedElement()
 {
 }
 
-NS_IMPL_ADDREF_INHERITED(HTMLSharedElement, Element)
-NS_IMPL_RELEASE_INHERITED(HTMLSharedElement, Element)
+NS_IMPL_ADDREF_INHERITED(HTMLSharedElement, nsGenericHTMLElement)
+NS_IMPL_RELEASE_INHERITED(HTMLSharedElement, nsGenericHTMLElement)
 
 
 NS_INTERFACE_MAP_BEGIN(HTMLSharedElement)
