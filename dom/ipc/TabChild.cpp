8f65d1828b6e568038a02c83d69192b05e0dc7a9
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 04:00:52 +0000

    Backed out 4 changesets (bug 1343728) for failures in test_group_mouseevents.html a=backout
    Backed out changeset 2029cb6bce86 (bug 1343728)
    Backed out changeset 0169e09f6030 (bug 1343728)
    Backed out changeset 831e95b29f66 (bug 1343728)
    Backed out changeset d5630ef01b17 (bug 1343728)
    
    MozReview-Commit-ID: 2RRBTmp5FGj
    
    UltraBlame original commit: 14f84d3b3233a87af51f5ad8302ee24cc2371a01

--- dom/ipc/TabChild.cpp
+++ dom/ipc/TabChild.cpp
@@ -407,10 +407,6 @@ TabChild::TabChild(nsIContentChild* aManager,
 #if defined(ACCESSIBILITY)
   , mTopLevelDocAccessibleChild(nullptr)
 #endif
-  , mPendingDocShellIsActive(false)
-  , mPendingDocShellPreserveLayers(false)
-  , mPendingDocShellReceivedMessage(false)
-  , mPendingDocShellBlockers(0)
 {
   nsWeakPtr weakPtrThis(do_GetWeakReference(static_cast<nsITabChild*>(this)));  
   mSetAllowedTouchBehaviorCallback = [weakPtrThis](uint64_t aInputBlockId,
@@ -2367,26 +2363,20 @@ TabChild::RecvDestroy()
   return IPC_OK();
 }
 
-void
-TabChild::AddPendingDocShellBlocker()
+mozilla::ipc::IPCResult
+TabChild::RecvSetDocShellIsActive(const bool& aIsActive,
+                                  const bool& aPreserveLayers,
+                                  const uint64_t& aLayerObserverEpoch)
 {
-  mPendingDocShellBlockers++;
-}
-
-void
-TabChild::RemovePendingDocShellBlocker()
-{
-  mPendingDocShellBlockers--;
-  if (!mPendingDocShellBlockers && mPendingDocShellReceivedMessage) {
-    mPendingDocShellReceivedMessage = false;
-    InternalSetDocShellIsActive(mPendingDocShellIsActive,
-                                mPendingDocShellPreserveLayers);
+  
+  
+  
+  
+  if (mLayerObserverEpoch >= aLayerObserverEpoch) {
+    return IPC_OK();
   }
-}
+  mLayerObserverEpoch = aLayerObserverEpoch;
 
-void
-TabChild::InternalSetDocShellIsActive(bool aIsActive, bool aPreserveLayers)
-{
   auto clearForcePaint = MakeScopeExit([&] {
     
     
@@ -2412,7 +2402,7 @@ TabChild::InternalSetDocShellIsActive(bool aIsActive, bool aPreserveLayers)
     
     
     
-    mPuppetWidget->GetLayerManager()->SetLayerObserverEpoch(mLayerObserverEpoch);
+    mPuppetWidget->GetLayerManager()->SetLayerObserverEpoch(aLayerObserverEpoch);
   }
 
   
@@ -2426,8 +2416,8 @@ TabChild::InternalSetDocShellIsActive(bool aIsActive, bool aPreserveLayers)
       
       
       if (IPCOpen()) {
-        Unused << SendForcePaintNoOp(mLayerObserverEpoch);
-        return;
+        Unused << SendForcePaintNoOp(aLayerObserverEpoch);
+        return IPC_OK();
       }
     }
 
@@ -2468,33 +2458,7 @@ TabChild::InternalSetDocShellIsActive(bool aIsActive, bool aPreserveLayers)
   } else if (!aPreserveLayers) {
     MakeHidden();
   }
-}
 
-mozilla::ipc::IPCResult
-TabChild::RecvSetDocShellIsActive(const bool& aIsActive,
-                                  const bool& aPreserveLayers,
-                                  const uint64_t& aLayerObserverEpoch)
-{
-  
-  
-  
-  
-  if (mLayerObserverEpoch >= aLayerObserverEpoch) {
-    return IPC_OK();
-  }
-  mLayerObserverEpoch = aLayerObserverEpoch;
-
-  
-  
-  
-  if (mPendingDocShellBlockers > 0) {
-    mPendingDocShellReceivedMessage = true;
-    mPendingDocShellIsActive = aIsActive;
-    mPendingDocShellPreserveLayers = aPreserveLayers;
-    return IPC_OK();
-  }
-
-  InternalSetDocShellIsActive(aIsActive, aPreserveLayers);
   return IPC_OK();
 }
 
