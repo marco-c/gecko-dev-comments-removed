0ea9a201c156b42a5dbd31aa88edeb5c46522e57
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 15:25:29 +0000

    Bug 1403444 - Replace the rb_foreach_* macros with a range iterator. r=njn
    UltraBlame original commit: f0aef1a9d5ac970744229fca6c9bf2f121c493d0

--- memory/build/rb.h
+++ memory/build/rb.h
@@ -620,98 +620,116 @@ private:
     }
     return node;
   }
+
+  
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+public:
+  class Iterator
+  {
+    T* mSentinel;
+    T* mPath[3 * ((SIZEOF_PTR << 3) - (SIZEOF_PTR_2POW + 1))];
+    unsigned mDepth;
+
+  public:
+    explicit Iterator(RedBlackTree<T, Trait>* aTree)
+      : mSentinel(&aTree->rbt_nil)
+      , mDepth(0)
+    {
+      
+      if (aTree->rbt_root != mSentinel) {
+        T* node;
+        mPath[mDepth++] = aTree->rbt_root;
+        while ((node = Trait::GetTreeNode(mPath[mDepth - 1]).Left()) !=
+               mSentinel) {
+          mPath[mDepth++] = node;
+        }
+      }
+    }
+
+    class Item
+    {
+      Iterator* mIterator;
+      T* mItem;
+
+    public:
+      Item(Iterator* aIterator, T* aItem)
+        : mIterator(aIterator)
+        , mItem(aItem)
+      { }
+
+      bool operator!=(const Item& aOther) const
+      {
+        return (mIterator != aOther.mIterator) || (mItem != aOther.mItem);
+      }
+
+      T* operator*() const { return mItem; }
+
+      const Item& operator++()
+      {
+        mItem = mIterator->Next();
+        return *this;
+      }
+    };
+
+    Item begin()
+    {
+      return Item(this, mDepth > 0 ? mPath[mDepth - 1] : nullptr);
+    }
+
+    Item end()
+    {
+      return Item(this, nullptr);
+    }
+
+    T* Next()
+    {
+      T* node;
+      if ((node = Trait::GetTreeNode(mPath[mDepth - 1]).Right()) !=
+          mSentinel) {
+        
+        mPath[mDepth++] = node;
+        while ((node = Trait::GetTreeNode(mPath[mDepth - 1]).Left()) !=
+               mSentinel) {
+          mPath[mDepth++] = node;
+        }
+      } else {
+        
+
+        for (mDepth--; mDepth > 0; mDepth--) {
+          if (Trait::GetTreeNode(mPath[mDepth - 1]).Left() == mPath[mDepth]) {
+            break;
+          }
+        }
+      }
+      return mDepth > 0 ? mPath[mDepth - 1] : nullptr;
+    }
+  };
+
+  Iterator iter() { return Iterator(this); }
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#define rbp_f_height (3 * ((SIZEOF_PTR << 3) - (SIZEOF_PTR_2POW + 1)))
-
-#define rb_foreach_begin(a_type, a_field, a_tree, a_var)                       \
-  {                                                                            \
-    {                                                                          \
-      /* Initialize the path to contain the left spine.             */         \
-      a_type* rbp_f_path[rbp_f_height];                                        \
-      a_type* rbp_f_node;                                                      \
-      unsigned rbp_f_depth = 0;                                                \
-      if ((a_tree)->rbt_root != &(a_tree)->rbt_nil) {                          \
-        rbp_f_path[rbp_f_depth] = (a_tree)->rbt_root;                          \
-        rbp_f_depth++;                                                         \
-        while ((rbp_f_node = a_field(rbp_f_path[rbp_f_depth - 1]).Left()) !=   \
-               &(a_tree)->rbt_nil) {                                           \
-          rbp_f_path[rbp_f_depth] = rbp_f_node;                                \
-          rbp_f_depth++;                                                       \
-        }                                                                      \
-      }                                                                        \
-      /* While the path is non-empty, iterate.                      */         \
-      while (rbp_f_depth > 0) {                                                \
-        (a_var) = rbp_f_path[rbp_f_depth - 1];
-
-#define rb_foreach_end(a_type, a_field, a_tree, a_var)
-                 \
-  if ((rbp_f_node = a_field(rbp_f_path[rbp_f_depth - 1]).Right()) !=           \
-      &(a_tree)->rbt_nil) {                                                    \
-    /* The successor is the left-most node in the right   */                   \
-    /* subtree.                                           */                   \
-    rbp_f_path[rbp_f_depth] = rbp_f_node;                                      \
-    rbp_f_depth++;                                                             \
-    while ((rbp_f_node = a_field(rbp_f_path[rbp_f_depth - 1]).Left()) !=       \
-           &(a_tree)->rbt_nil) {                                               \
-      rbp_f_path[rbp_f_depth] = rbp_f_node;                                    \
-      rbp_f_depth++;                                                           \
-    }                                                                          \
-  } else {                                                                     \
-    /* The successor is above the current node.  Unwind   */                   \
-    /* until a left-leaning edge is removed from the      */                   \
-    /* path, or the path is empty.                        */                   \
-    for (rbp_f_depth--; rbp_f_depth > 0; rbp_f_depth--) {                      \
-      if (a_field(rbp_f_path[rbp_f_depth - 1]).Left() ==                       \
-          rbp_f_path[rbp_f_depth]) {                                           \
-        break;                                                                 \
-      }                                                                        \
-    }                                                                          \
-  }                                                                            \
-  }                                                                            \
-  }                                                                            \
-  }
 
 #endif 
