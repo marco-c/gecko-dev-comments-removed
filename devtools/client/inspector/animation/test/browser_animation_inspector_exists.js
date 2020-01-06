465bf95e9af9560f6feae2c7c64f928c272990ba
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 19:34:59 +0000

    Bug 1400115 - Part 3: Add test. r=gl,pbro
    MozReview-Commit-ID: LIpkVybR8GA
    
    UltraBlame original commit: ec9072a356c07533f2b6bc02bbd85ebc625d8373

---
+++ devtools/client/inspector/animation/test/browser_animation_inspector_exists.js
@@ -0,0 +1,12 @@
+
+
+
+"use strict";
+
+
+
+add_task(async function () {
+  const { animationInspector, panel } = await openAnimationInspector();
+  ok(animationInspector, "AnimationInspector should exist");
+  ok(panel, "Main animation-inspector panel should exist");
+});
