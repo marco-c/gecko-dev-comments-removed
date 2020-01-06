11251ae3e119b0f76a49ee145a48e4756fd44804
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 23:51:02 +0000

    Bug 1381459 - Allow closing of browserless tabs. r=whimboo
    Closing a tab should always be allowed, even if the current content browser doesn't exist.
    This can be the case when a tab gets moved to a different process.
    
    MozReview-Commit-ID: LaU9MgEyCOx
    
    UltraBlame original commit: 705facc285e5717428374c81ac35ed7746e3861d

--- testing/marionette/driver.js
+++ testing/marionette/driver.js
@@ -241,6 +241,12 @@ Object.defineProperty(GeckoDriver.prototype, "windows", {
   },
 });
 
+Object.defineProperty(GeckoDriver.prototype, "windowType", {
+  get() {
+    return this.curBrowser.window.document.documentElement.getAttribute("windowtype");
+  },
+});
+
 Object.defineProperty(GeckoDriver.prototype, "windowHandles", {
   get() {
     let hs = [];
@@ -1165,9 +1171,9 @@ GeckoDriver.prototype.getTitle = function() {
 
 
 GeckoDriver.prototype.getWindowType = function(cmd, resp) {
-  let win = assert.window(this.getCurrentWindow());
+  assert.window(this.getCurrentWindow());
 
-  resp.body.value = win.document.documentElement.getAttribute("windowtype");
+  resp.body.value = this.windowType;
 };
 
 
@@ -2800,7 +2806,7 @@ GeckoDriver.prototype.deleteCookie = function(cmd) {
 
 
 GeckoDriver.prototype.close = async function() {
-  assert.contentBrowser(this.curBrowser);
+  assert.window(this.getCurrentWindow(Context.Content));
   this._assertAndDismissModal();
 
   let nwins = 0;
