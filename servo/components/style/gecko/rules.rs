457707f1c28d0bca43cbeb3deb176d5be6bf6322
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 02:41:45 +0000

    servo: Merge #17236 - Define Gecko CounterStyle and FontFaceRule clone functions (from bradwerth:cloneRulesBetter); r=heycam
    <!-- Please describe your changes on the following line: -->
    Defines and calls Gecko CounterStyle and FontFaceRule clone functions, protected by cfg directives to only apply in gecko builds.
    
    ---
    <!-- Thank you for contributing to Servo! Please replace each `[ ]` by `[X]` when the step is complete, and replace `__` with appropriate data: -->
    - [X] `./mach build -d` does not report any errors
    - [X] `./mach test-tidy` does not report any errors
    - [ ] These changes fix #__ (github issue number if applicable).
    
    <!-- Either: -->
    - [X] There are tests for these changes OR
    - [ ] These changes do not require tests because _____
    
    <!-- Also, please make sure that "Allow edits from maintainers" checkbox is checked, so that we can help you if you get stuck somewhere along the way.-->
    
    <!-- Pull requests that do not address these steps are welcome, but they will require additional verification as part of the review process. -->
    
    Source-Repo: https://github.com/servo/servo
    Source-Revision: 0dc3fbfce3263cb8aabbed8c5f9b856a12e59460
    
    UltraBlame original commit: 78b1221a307d768c75f75fdb4bc9386ce93a9210

--- servo/components/style/gecko/rules.rs
+++ servo/components/style/gecko/rules.rs
@@ -150,6 +150,18 @@ impl ToNsCssValue for FontDisplay {
     }
 }
 
+impl FontFaceRule {
+    
+    
+    pub fn deep_clone_from_gecko(&self) -> FontFaceRule {
+        let result = unsafe {
+            UniqueRefPtr::from_addrefed(
+                bindings::Gecko_CSSFontFaceRule_Clone(self.get()))
+        };
+        result.get()
+    }
+}
+
 impl From<FontFaceRuleData> for FontFaceRule {
     fn from(data: FontFaceRuleData) -> FontFaceRule {
         let mut result = unsafe {
@@ -175,6 +187,18 @@ impl ToCssWithGuard for FontFaceRule {
 
 
 pub type CounterStyleRule = RefPtr<nsCSSCounterStyleRule>;
+
+impl CounterStyleRule {
+    
+    
+    pub fn deep_clone_from_gecko(&self) -> CounterStyleRule {
+        let result = unsafe {
+            UniqueRefPtr::from_addrefed(
+                bindings::Gecko_CSSCounterStyle_Clone(self.get()))
+        };
+        result.get()
+    }
+}
 
 impl From<counter_style::CounterStyleRuleData> for CounterStyleRule {
     fn from(data: counter_style::CounterStyleRuleData) -> CounterStyleRule {
