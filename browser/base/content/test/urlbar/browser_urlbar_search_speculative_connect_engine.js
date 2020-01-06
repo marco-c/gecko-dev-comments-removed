4e55ca6166d2a8009aec3759906291392a866a5b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 08:20:22 +0000

    Bug 1355451 - Speculative connect to websites on mousedown on awesomebar item. r=mak
    We preconnect to an explicit URL or the url in moz-action:remotetab on the popup.
    
    MozReview-Commit-ID: 2U6nhwM6RUk
    
    UltraBlame original commit: 742dfaf7631d739391a7aaee22e1568050b53555

--- browser/base/content/test/urlbar/browser_urlbar_search_speculative_connect_engine.js
+++ browser/base/content/test/urlbar/browser_urlbar_search_speculative_connect_engine.js
@@ -7,7 +7,6 @@
 
 
 
-let {HttpServer} = Cu.import("resource://testing-common/httpd.js", {});
 let gHttpServer = null;
 let gScheme = "http";
 let gHost = "localhost"; 
@@ -15,17 +14,7 @@ let gPort = 20709;
 const TEST_ENGINE_BASENAME = "searchSuggestionEngine2.xml";
 
 add_task(async function setup() {
-  if (!gHttpServer) {
-    gHttpServer = new HttpServer();
-    try {
-      gHttpServer.start(gPort);
-      gPort = gHttpServer.identity.primaryPort;
-      gHttpServer.identity.setPrimary(gScheme, gHost, gPort);
-    } catch (ex) {
-      info("We can't launch our http server successfully.")
-    }
-  }
-  is(gHttpServer.identity.has(gScheme, gHost, gPort), true, "make sure we have this domain listed");
+  gHttpServer = runHttpServer(gScheme, gHost, gPort);
 
   await SpecialPowers.pushPrefEnv({
     set: [["browser.urlbar.autoFill", true],
