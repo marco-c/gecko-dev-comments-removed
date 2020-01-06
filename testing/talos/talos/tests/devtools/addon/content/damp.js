fdfd63eb20b94390a1e27d2090ad42c94d551654
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 18:44:54 +0000

    Bug 1171482 - add DAMP test for inspector mutations;r=ochameau
    MozReview-Commit-ID: 7ItEe4O4jyB
    
    UltraBlame original commit: 502b8b343daf93b437019b4e72b304f18271937e

--- testing/talos/talos/tests/devtools/addon/content/damp.js
+++ testing/talos/talos/tests/devtools/addon/content/damp.js
@@ -317,6 +317,63 @@ Damp.prototype = {
     yield this.testTeardown();
   }),
 
+  
+
+
+
+  _inspectorMutationsTest: Task.async(function* () {
+    let tab = yield this.testSetup(SIMPLE_URL);
+    let messageManager = tab.linkedBrowser.messageManager;
+    let {toolbox} = yield this.openToolbox("inspector");
+    let inspector = toolbox.getPanel("inspector");
+
+    
+    const LIMIT = 100;
+    const DELAY = 5;
+
+    messageManager.loadFrameScript("data:,(" + encodeURIComponent(
+      `function () {
+        const LIMIT = ${LIMIT};
+        addMessageListener("start-mutations-test", function () {
+          let addElement = function(index) {
+            if (index == LIMIT) {
+              // LIMIT was reached, stop adding elements.
+              return;
+            }
+            let div = content.document.createElement("div");
+            content.document.body.appendChild(div);
+            content.setTimeout(() => addElement(index + 1), ${DELAY});
+          };
+          addElement(0);
+        });
+      }`
+    ) + ")()", false);
+
+    let start = performance.now();
+
+    yield new Promise(resolve => {
+      let childListMutationsCounter = 0;
+      inspector.on("markupmutation", (evt, mutations) => {
+        let childListMutations = mutations.filter(m => m.type === "childList");
+        childListMutationsCounter += childListMutations.length;
+        if (childListMutationsCounter === LIMIT) {
+          
+          resolve();
+        }
+      });
+
+      messageManager.sendAsyncMessage("start-mutations-test");
+    });
+
+    this._results.push({
+      name: "inspector.mutations",
+      value: performance.now() - start
+    });
+
+    yield this.closeToolbox(null);
+    yield this.testTeardown();
+  }),
+
   takeCensus(label) {
     let start = performance.now();
 
@@ -674,6 +731,7 @@ Damp.prototype = {
     tests["console.bulklog"] = this._consoleBulkLoggingTest;
     tests["console.streamlog"] = this._consoleStreamLoggingTest;
     tests["console.objectexpand"] = this._consoleObjectExpansionTest;
+    tests["inspector.mutations"] = this._inspectorMutationsTest;
 
     
     let filter = Services.prefs.getCharPref("talos.subtests", "");
