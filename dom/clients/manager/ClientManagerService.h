2c58ac12e2095700b3023fc34fd3d9806874fd05
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 20:42:02 +0000

    Bug 1415779 P3 Add the ClientSource class and hook it into ClientManager/ClientManagerService. r=baku
    UltraBlame original commit: e1724fab9cbd23fd844d7f0658078592d11cff8c

--- dom/clients/manager/ClientManagerService.h
+++ dom/clients/manager/ClientManagerService.h
@@ -6,21 +6,39 @@
 #ifndef _mozilla_dom_ClientManagerService_h
 #define _mozilla_dom_ClientManagerService_h
 
+#include "nsDataHashtable.h"
+
 namespace mozilla {
 
 namespace dom {
+
+class ClientSourceParent;
 
 
 
 
 class ClientManagerService final
 {
+  
+  
+  nsDataHashtable<nsIDHashKey, ClientSourceParent*> mSourceTable;
+
   ClientManagerService();
   ~ClientManagerService();
 
 public:
   static already_AddRefed<ClientManagerService>
   GetOrCreateInstance();
+
+  void
+  AddSource(ClientSourceParent* aSource);
+
+  void
+  RemoveSource(ClientSourceParent* aSource);
+
+  ClientSourceParent*
+  FindSource(const nsID& aID,
+             const mozilla::ipc::PrincipalInfo& aPrincipalInfo);
 
   NS_INLINE_DECL_REFCOUNTING(mozilla::dom::ClientManagerService)
 };
