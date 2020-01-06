3a8eb6b2ac119b18f7e5daec86bbd99abf5ee3b1
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 06:33:21 +0000

    Bug 1366694 Part 4: Change Windows content process sandbox level to 3 on Nightly. r=jimm
    UltraBlame original commit: 60ef4d9f30239938dc0769cab69c54f3ccbb7d3f

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1058,7 +1058,7 @@ pref("dom.ipc.plugins.sandbox-level.flash", 0);
 
 
 #if defined(NIGHTLY_BUILD)
-pref("security.sandbox.content.level", 2);
+pref("security.sandbox.content.level", 3);
 #else
 pref("security.sandbox.content.level", 1);
 #endif
