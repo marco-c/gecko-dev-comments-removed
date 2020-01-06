65f415c8b28b9d69cc76a68faede8fe9bb52e2ef
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 03:34:20 +0000

    Bug 1372470 - Fix form submission intermittent error. r=lchang
    MozReview-Commit-ID: KQIian9kRBE
    
    UltraBlame original commit: 50d32ce7f58c3aa846e1175c189a50cd1bd11463

--- browser/extensions/formautofill/test/browser/browser_first_time_use_doorhanger.js
+++ browser/extensions/formautofill/test/browser/browser_first_time_use_doorhanger.js
@@ -27,9 +27,8 @@ add_task(async function test_first_time_save() {
         form.querySelector("#tel").value = "1-345-345-3456";
 
         
-        setTimeout(() => {
-          form.querySelector("input[type=submit]").click();
-        }, 500);
+        await new Promise(resolve => setTimeout(resolve, 500));
+        form.querySelector("input[type=submit]").click();
       });
 
       await promiseShown;
@@ -65,12 +64,11 @@ add_task(async function test_non_first_time_save() {
         form.querySelector("#tel").value = "1-650-903-0800";
 
         
-        setTimeout(() => {
-          form.querySelector("input[type=submit]").click();
-        }, 500);
+        await new Promise(resolve => setTimeout(resolve, 500));
+        form.querySelector("input[type=submit]").click();
       });
 
-      await new Promise(resolve => setTimeout(resolve, 1000));
+      await sleep(1000);
       is(PopupNotifications.panel.state, "closed", "Doorhanger is hidden");
     }
   );
