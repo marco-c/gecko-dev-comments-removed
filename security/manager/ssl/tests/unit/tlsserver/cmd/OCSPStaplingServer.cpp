98c26bbb80f9e2cd9d8a4b624255e0823bd3033f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 09:56:28 +0000

    Bug 1390131 - Modify GenerateOCSPResponse.cpp to allow thisUpdate to be modified r=keeler
    MozReview-Commit-ID: EL9E4UtZg17
    
    UltraBlame original commit: 2e2bbc07c946e2fee216297e33871ebffe9a3393

--- security/manager/ssl/tests/unit/tlsserver/cmd/OCSPStaplingServer.cpp
+++ security/manager/ssl/tests/unit/tlsserver/cmd/OCSPStaplingServer.cpp
@@ -102,7 +102,7 @@ DoSNISocketConfig(PRFileDesc *aFd, const SECItem *aSrvNameArr,
 
   
   SECItemArray *response = GetOCSPResponseForType(host->mORT, cert, arena,
-                                                  host->mAdditionalCertName);
+                                                  host->mAdditionalCertName, 0);
   if (!response) {
     return SSL_SNI_SEND_ALERT;
   }
