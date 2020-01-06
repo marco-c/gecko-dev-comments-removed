b3a8c8d5d31e1ce9652fcc943781597a3afd0aa8
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 08:17:12 +0000

    Merge m-c to inbound a=merge
    MozReview-Commit-ID: 1p4eNdfSz9p
    
    UltraBlame original commit: c46a847dac3b0b15c88e200fbf0a41941a5b9d80

--- dom/base/nsObjectLoadingContent.cpp
+++ dom/base/nsObjectLoadingContent.cpp
@@ -1391,7 +1391,7 @@ nsObjectLoadingContent::MaybeRewriteYoutubeEmbed(nsIURI* aURI, nsIURI* aBaseURI,
   
   
   nsAutoCString path;
-  aURI->GetPath(path);
+  aURI->GetPathQueryRef(path);
   if (!StringBeginsWith(path, NS_LITERAL_CSTRING("/v/"))) {
     return;
   }
