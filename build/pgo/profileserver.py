669ad573d1cee82a14b923790e68f0ce7abd84f4
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 11:04:28 +0000

    Bug 1393657 - Clean up imports in profileserver.py; r=glandium
    Remove unused imports. Put stdlib imports first. And sort.
    
    MozReview-Commit-ID: 9CAlCe2vA9z
    
    UltraBlame original commit: 758077726592e53df0e7d0f7b0a6e9fbaca9dca5

--- build/pgo/profileserver.py
+++ build/pgo/profileserver.py
@@ -4,20 +4,17 @@
 
 
 
+import json
+import os
+import shutil
+import tempfile
+
+from buildconfig import substs
+from mozbuild.base import MozbuildObject
+from mozhttpd import MozHttpd
 from mozprofile import FirefoxProfile, Profile, Preferences
 from mozprofile.permissions import ServerLocations
 from mozrunner import FirefoxRunner, CLI
-from mozhttpd import MozHttpd
-import json
-import socket
-import threading
-import os
-import sys
-import shutil
-import tempfile
-from datetime import datetime
-from mozbuild.base import MozbuildObject
-from buildconfig import substs
 
 PORT = 8888
 
