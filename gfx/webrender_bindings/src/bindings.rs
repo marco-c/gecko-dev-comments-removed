5794ea84aaee142f7a94e06d05cfeb03ff2d627b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 02:00:05 +0000

    Bug 1367734 - Tone down webrender's memory usage to avoid OOMing during media mochitests. r=kvark
    MozReview-Commit-ID: 9RIR37ZOSFR
    
    UltraBlame original commit: 082c1ba38bd310fcabe90a31155298899120b7c2

--- gfx/webrender_bindings/src/bindings.rs
+++ gfx/webrender_bindings/src/bindings.rs
@@ -881,6 +881,7 @@ pub extern "C" fn wr_window_new(window_id: WrWindowId,
         enable_profiler: enable_profiler,
         recorder: recorder,
         blob_image_renderer: Some(Box::new(Moz2dImageRenderer::new())),
+        cache_expiry_frames: 60, 
         ..Default::default()
     };
 
