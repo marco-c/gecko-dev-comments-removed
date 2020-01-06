86095bff3886b4b6e2fb3b5867210774eba27d7f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 10:20:24 +0000

    Bug 1389497 - Update bindings for API change in WR cset 63ec62c. r=kvark
    MozReview-Commit-ID: 7vCZWSfb9Hh
    
    UltraBlame original commit: f75cec1930f286ce8472e3705e9448c51e7e6b3e

--- gfx/webrender_bindings/src/bindings.rs
+++ gfx/webrender_bindings/src/bindings.rs
@@ -589,7 +589,6 @@ pub extern "C" fn wr_window_new(window_id: WrWindowId,
         recorder: recorder,
         blob_image_renderer: Some(Box::new(Moz2dImageRenderer::new(workers.clone()))),
         workers: Some(workers.clone()),
-        cache_expiry_frames: 60, 
         enable_render_on_scroll: false,
         ..Default::default()
     };
