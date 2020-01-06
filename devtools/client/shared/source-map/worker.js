3a775e6f8b7b0f9a526092e3191d6c814a0b9e5e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 11:54:27 +0000

    Bug 1395613 - import bundles from devtools-source-map 0.12.0; r=bgrins
    MozReview-Commit-ID: n5XTA3YN3G
    
    UltraBlame original commit: eff91b4e33156ebcb56b52eb22a87cf034cd8619

--- devtools/client/shared/source-map/worker.js
+++ devtools/client/shared/source-map/worker.js
@@ -564,12 +564,17 @@ return  (function(modules) {
 	    try {
 	      const response = publicInterface[method].apply(undefined, args);
 	      if (response instanceof Promise) {
-	        response.then(val => self.postMessage({ id, response: val }), err => self.postMessage({ id, error: err }));
+	        response.then(val => self.postMessage({ id, response: val }),
+	        
+	        
+	        err => self.postMessage({ id, error: err.toString() }));
 	      } else {
 	        self.postMessage({ id, response });
 	      }
 	    } catch (error) {
-	      self.postMessage({ id, error });
+	      
+	      
+	      self.postMessage({ id, error: error.toString() });
 	    }
 	  };
 	}
