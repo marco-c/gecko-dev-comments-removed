4418c4f8acf5f51ca843464f51f7c04e1cbd39e7
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 03:51:12 +0000

    Bug 1374953 - Allow audio/mpegurl as HLS media type. r=jya
    MozReview-Commit-ID: 6J4A4G21D0N
    
    UltraBlame original commit: 1de0000cf1bcd531c4b7bfc0d9629093518eb668

--- dom/media/DecoderTraits.cpp
+++ dom/media/DecoderTraits.cpp
@@ -67,12 +67,14 @@ IsAndroidMediaType(const MediaContainerType& aType)
  bool
 DecoderTraits::IsHttpLiveStreamingType(const MediaContainerType& aType)
 {
+  const auto& mimeType = aType.Type();
   return 
          
-         aType.Type() == MEDIAMIMETYPE("application/vnd.apple.mpegurl")
+         mimeType == MEDIAMIMETYPE("application/vnd.apple.mpegurl")
          
-         || aType.Type() == MEDIAMIMETYPE("application/x-mpegurl")
-         || aType.Type() == MEDIAMIMETYPE("audio/x-mpegurl");
+         || mimeType == MEDIAMIMETYPE("application/x-mpegurl")
+         || mimeType == MEDIAMIMETYPE("audio/mpegurl")
+         || mimeType == MEDIAMIMETYPE("audio/x-mpegurl");
 }
 
  bool
