e01c71ff9f711185872bbc843b1b5f1f699d5051
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 00:21:15 +0000

    Bug 1354504 - Add test cases for SearchBox and AutocompletePopup react components. r=ntim, jdescottes.
    [SearchBox]
    - SearchBox setup
    - initial empty value
    - initially notFocussed
    - setting value to inputbox
    - checking for focus on the input
    - onBlur
    - ClearButton hidden initialized
    - ClearButton shown on some value
    KeyDown emptyAutoComplete return undefined
    - ArrowDown
    - ArrowUp
    - PageDown
    - PageUp
    - Home
    - End
    - Tab
    	- and hide box
    - Backspace
    	- reactivates popup
    - Enter
    	- and hide box
    - Escape
    
    [AutocompletePopup]
    - initialy DOM invisibility
    - focus on input and visibility
    - List compare
    - Initial selectedIndex = -1
    - Blur hiding of autocomplete
    - filtering of Values
    - jumpToTop (by key events)
    - jumpToBottom (by key events)
    - jumpBy (by key events)
    - select (by key events)
    - Selection class
    - MouseDown
    
    MozReview-Commit-ID: 9PjGztW0PIy
    
    UltraBlame original commit: f7b1a464331846b8e849367329913e883b4d3861

--- devtools/client/shared/components/test/mochitest/head.js
+++ devtools/client/shared/components/test/mochitest/head.js
@@ -211,3 +211,25 @@ function shallowRenderComponent(component, props) {
   renderer.render(el, {});
   return renderer.getRenderOutput();
 }
+
+
+
+
+
+
+
+
+
+async function createComponentTest(factory, props) {
+  const container = document.createElement("div");
+  document.body.appendChild(container);
+
+  const component = ReactDOM.render(factory(props), container);
+  await forceRender(component);
+
+  return {
+    container,
+    component,
+    $: (s) => container.querySelector(s),
+  };
+}
