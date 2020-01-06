7f87464df4ba7f7a3ccd645d7a6ea9765c847096
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 18:43:02 +0000

    Bug 1356531 - Remove moz_places_faviconindex and old Places migrations. r=Paolo
    MozReview-Commit-ID: CUCuW4iMSrk
    
    UltraBlame original commit: 89bfafd51a2c53bc3e0395c34256ba5159618b1d

--- toolkit/components/places/Database.h
+++ toolkit/components/places/Database.h
@@ -19,7 +19,7 @@
 
 
 
-#define DATABASE_SCHEMA_VERSION 40
+#define DATABASE_SCHEMA_VERSION 41
 
 
 #define TOPIC_PLACES_INIT_COMPLETE "places-init-complete"
@@ -280,21 +280,6 @@ protected:
   
 
 
-  nsresult MigrateV13Up();
-  nsresult MigrateV15Up();
-  nsresult MigrateV17Up();
-  nsresult MigrateV18Up();
-  nsresult MigrateV19Up();
-  nsresult MigrateV20Up();
-  nsresult MigrateV21Up();
-  nsresult MigrateV22Up();
-  nsresult MigrateV23Up();
-  nsresult MigrateV24Up();
-  nsresult MigrateV25Up();
-  nsresult MigrateV26Up();
-  nsresult MigrateV27Up();
-  nsresult MigrateV28Up();
-  nsresult MigrateV30Up();
   nsresult MigrateV31Up();
   nsresult MigrateV32Up();
   nsresult MigrateV33Up();
@@ -305,6 +290,7 @@ protected:
   nsresult MigrateV38Up();
   nsresult MigrateV39Up();
   nsresult MigrateV40Up();
+  nsresult MigrateV41Up();
 
   nsresult UpdateBookmarkRootTitles();
 
