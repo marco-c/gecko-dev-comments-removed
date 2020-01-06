784bc1f9d66208e574b4cec08a5fff8088596b48
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 13:11:14 +0000

    Bug 1398659. P11 - replace mCacheStream.GetLength() with GetLength(). r=gerald
    MozReview-Commit-ID: BGgObe2XG5C
    
    UltraBlame original commit: fadb12f8f604e32db23b5e49beb1b55160a66c1a

--- dom/media/MediaResource.cpp
+++ dom/media/MediaResource.cpp
@@ -748,7 +748,7 @@ void ChannelMediaResource::Resume()
       mReopenOnError = true;
       element->DownloadResumed();
     } else {
-      int64_t totalLength = mCacheStream.GetLength();
+      int64_t totalLength = GetLength();
       
       
       
