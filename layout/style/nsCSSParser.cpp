46f9110138657a72ad4af6811bc2450cf9dd550a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 02:37:33 +0000

    Bug 1345709 - Lowercase color keyword in specified value. r=heycam
    MozReview-Commit-ID: d5cbTvSpQt
    
    UltraBlame original commit: e1880a72e41e5718754a6321e87b5923348908c0

--- layout/style/nsCSSParser.cpp
+++ layout/style/nsCSSParser.cpp
@@ -6761,6 +6761,9 @@ CSSParserImpl::ParseColor(nsCSSValue& aValue)
 
     case eCSSToken_Ident: {
       if (NS_ColorNameToRGB(tk->mIdent, &rgba)) {
+        
+        
+        nsContentUtils::ASCIIToLower(tk->mIdent);
         aValue.SetStringValue(tk->mIdent, eCSSUnit_Ident);
         return CSSParseResult::Ok;
       }
