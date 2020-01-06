7e44bcbf4be50fdeaf5def3b39509208f4860b4a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 13:41:35 +0000

    merge mozilla-central to autoland. r=merge a=merge
    UltraBlame original commit: 9d99ac9ae088bcee2b7f89f2aff10ff0bea73f56

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -5831,12 +5831,11 @@ pref("security.data_uri.unique_opaque_origin", true);
 pref("security.data_uri.block_toplevel_data_uri_navigations", false);
 
 
-#if defined(NIGHTLY_BUILD) && !defined(MOZ_WIDGET_ANDROID)
+#if !defined(MOZ_WIDGET_ANDROID)
 pref("dom.storageManager.enabled", true);
 #else
 pref("dom.storageManager.enabled", false);
 #endif
-
 pref("dom.storageManager.prompt.testing", false);
 pref("dom.storageManager.prompt.testing.allow", false);
 
