3350207576143b8b6a5dd0be7cc1db9afd50ce6a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 00:42:50 +0000

    Backed out changeset 99ed53cfebd6 (bug 1298803) for causing Windows Marionette hangs.
    UltraBlame original commit: 66fd29976718caafa5cad88ebec62b9c06b13500

--- testing/marionette/puppeteer/firefox/firefox_puppeteer/ui/windows.py
+++ testing/marionette/puppeteer/firefox/firefox_puppeteer/ui/windows.py
@@ -409,7 +409,8 @@ class BaseWindow(BaseLib):
             if kwargs[modifier] is True:
                 keys.append(keymap[modifier])
 
-        keys.append(command_key)
+        
+        keys.append(command_key.lower())
 
         self.switch_to()
 
