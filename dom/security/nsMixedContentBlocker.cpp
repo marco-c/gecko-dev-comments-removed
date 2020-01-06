3d2c80cde2a2d750790739185ee221272c18ee54
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 21:34:20 +0000

    Bug 1398229 - Save-link-as feature should use the loading principal - part 3 - implementation of nsIContentPolicy.TYPE_SAVE_AS_DOWNLOAD, r=ckerschb, r=tanvi
    UltraBlame original commit: 6b4cebf12e3fd1d1603f03b84a6cfdeb200204fc

--- dom/security/nsMixedContentBlocker.cpp
+++ dom/security/nsMixedContentBlocker.cpp
@@ -578,6 +578,13 @@ nsMixedContentBlocker::ShouldLoad(bool aHadInsecureImageRedirect,
 
     
     
+    
+    case TYPE_SAVEAS_DOWNLOAD:
+      *aDecision = ACCEPT;
+      return NS_OK;
+
+    
+    
     case TYPE_IMAGE:
     case TYPE_MEDIA:
     case TYPE_OBJECT_SUBREQUEST:
