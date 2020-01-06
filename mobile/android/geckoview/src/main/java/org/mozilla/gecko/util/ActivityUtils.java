489ce22df2487ca08c50148dd622f0ccf8c4fb34
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 10:34:47 +0000

    Bug 1391160 - Remember SYSTEM_UI_FLAG_LIGHT_STATUS_BAR when show/hide fullscreen. r=nechen,sebastian,walkingice
    Keep the value of SYSTEM_UI_FLAG_LIGHT_STATUS_BAR in window system ui status when show/hide fullscreen.
    
    MozReview-Commit-ID: CnOsCFKuN65
    
    UltraBlame original commit: 21cb52d98f1ce76926802fe853776eec551556f8

--- mobile/android/geckoview/src/main/java/org/mozilla/gecko/util/ActivityUtils.java
+++ mobile/android/geckoview/src/main/java/org/mozilla/gecko/util/ActivityUtils.java
@@ -14,6 +14,8 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
+import org.mozilla.gecko.AppConstants;
+
 public class ActivityUtils {
     private ActivityUtils() {
     }
@@ -37,6 +39,13 @@ public class ActivityUtils {
                 }
             } else {
                 newVis = View.SYSTEM_UI_FLAG_VISIBLE;
+            }
+
+            if (AppConstants.Versions.feature23Plus) {
+                
+                
+                final int oldVis = window.getDecorView().getSystemUiVisibility();
+                newVis |= (oldVis & View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
             }
 
             window.getDecorView().setSystemUiVisibility(newVis);
