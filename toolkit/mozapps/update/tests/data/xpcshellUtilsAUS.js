cbbb33e7e39984d58c58aa8568159295512446f5
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 18:06:06 +0000

    Bug 1348087 Part 3 - Test changes for new app update downloader implementation. r=rstrong
    MozReview-Commit-ID: 34zJprqSfWF
    
    UltraBlame original commit: ae2fcdddead1e6381ef148226ddb380cbcd32592

--- toolkit/mozapps/update/tests/data/xpcshellUtilsAUS.js
+++ toolkit/mozapps/update/tests/data/xpcshellUtilsAUS.js
@@ -148,6 +148,7 @@ var gUpdates;
 var gStatusCode;
 var gStatusText;
 var gStatusResult;
+var gSlowDownloadContinue = false;
 
 var gProcess;
 var gAppTimer;
@@ -3682,7 +3683,12 @@ const downloadListener = {
 
 
 
-function start_httpserver() {
+
+
+
+
+
+function start_httpserver(options) {
   let dir = getTestDirFile();
   debugDump("http server directory path: " + dir.path);
 
@@ -3695,6 +3701,11 @@ function start_httpserver() {
   gTestserver = new HttpServer();
   gTestserver.registerDirectory("/", dir);
   gTestserver.registerPathHandler("/" + gHTTPHandlerPath, pathHandler);
+  if (options && options.slowDownload) {
+    gTestserver.registerPathHandler("/" + FILE_SIMPLE_MAR, marPathHandler);
+  } else if (options && options.errorDownload) {
+    gTestserver.registerPathHandler("/" + FILE_SIMPLE_MAR, marErrorPathHandler);
+  }
   gTestserver.start(-1);
   let testserverPort = gTestserver.identity.primaryPort;
   gURLData = URL_HOST + ":" + testserverPort + "/";
@@ -3713,6 +3724,34 @@ function pathHandler(aMetadata, aResponse) {
   aResponse.setHeader("Content-Type", "text/xml", false);
   aResponse.setStatusLine(aMetadata.httpVersion, gResponseStatusCode, "OK");
   aResponse.bodyOutputStream.write(gResponseBody, gResponseBody.length);
+}
+
+
+
+
+
+
+
+
+
+
+function marPathHandler(aRequest, aResponse) {
+  aResponse.processAsync();
+  aResponse.setHeader("Content-Type", "binary/octet-stream");
+  aResponse.setHeader("Content-Length", SIZE_SIMPLE_MAR);
+
+  (function marPathHandlerTimeout() {
+    if (gSlowDownloadContinue) {
+      let content = readFileBytes(getTestDirFile(FILE_SIMPLE_MAR));
+      aResponse.bodyOutputStream.write(content, SIZE_SIMPLE_MAR);
+      aResponse.finish();
+    } else {
+      do_timeout(100, marPathHandlerTimeout);
+    }
+  })();
+}
+function marErrorPathHandler(aRequest, aResponse) {
+  aResponse.setStatusLine(aMetadata.httpVersion, 500, "Internal server error");
 }
 
 
@@ -4062,132 +4101,6 @@ function runUpdateUsingApp(aExpectedStatus) {
 
   debugDump("finish - launching application to apply update");
 }
-
-
-
-
-
-
-function initMockIncrementalDownload() {
-  const INC_CONTRACT_ID = "@mozilla.org/network/incremental-download;1";
-  let incrementalDownloadCID =
-    MockRegistrar.register(INC_CONTRACT_ID, IncrementalDownload);
-  do_register_cleanup(() => {
-    MockRegistrar.unregister(incrementalDownloadCID);
-  });
-}
-
-function IncrementalDownload() {
-  this.wrappedJSObject = this;
-}
-
-IncrementalDownload.prototype = {
-  
-  init(uri, file, chunkSize, intervalInSeconds) {
-    this._destination = file;
-    this._URI = uri;
-    this._finalURI = uri;
-  },
-
-  start(observer, ctxt) {
-    let tm = Cc["@mozilla.org/thread-manager;1"].
-             getService(Ci.nsIThreadManager);
-    
-    
-    tm.dispatchToMainThread(() => {
-      this._observer = observer.QueryInterface(Ci.nsIRequestObserver);
-      this._ctxt = ctxt;
-      this._observer.onStartRequest(this, this._ctxt);
-      let mar = getTestDirFile(FILE_SIMPLE_MAR);
-      mar.copyTo(this._destination.parent, this._destination.leafName);
-      let status = Cr.NS_OK;
-      switch (gIncrementalDownloadErrorType++) {
-        case 0:
-          status = Cr.NS_ERROR_NET_RESET;
-          break;
-        case 1:
-          status = Cr.NS_ERROR_CONNECTION_REFUSED;
-          break;
-        case 2:
-          status = Cr.NS_ERROR_NET_RESET;
-          break;
-        case 3:
-          status = Cr.NS_OK;
-          break;
-        case 4:
-          status = Cr.NS_ERROR_OFFLINE;
-          
-          
-          let tm2 = Cc["@mozilla.org/thread-manager;1"].
-                    getService(Ci.nsIThreadManager);
-          tm2.dispatchToMainThread(function() {
-            Services.obs.notifyObservers(gAUS,
-                                         "network:offline-status-changed",
-                                         "online");
-          });
-          break;
-      }
-      this._observer.onStopRequest(this, this._ctxt, status);
-    });
-  },
-
-  get URI() {
-    return this._URI;
-  },
-
-  get currentSize() {
-    throw Cr.NS_ERROR_NOT_IMPLEMENTED;
-  },
-
-  get destination() {
-    return this._destination;
-  },
-
-  get finalURI() {
-    return this._finalURI;
-  },
-
-  get totalSize() {
-    throw Cr.NS_ERROR_NOT_IMPLEMENTED;
-  },
-
-  
-  cancel(aStatus) {
-    throw Cr.NS_ERROR_NOT_IMPLEMENTED;
-  },
-  suspend() {
-    throw Cr.NS_ERROR_NOT_IMPLEMENTED;
-  },
-  isPending() {
-    throw Cr.NS_ERROR_NOT_IMPLEMENTED;
-  },
-  _loadFlags: 0,
-  get loadFlags() {
-    return this._loadFlags;
-  },
-  set loadFlags(val) {
-    this._loadFlags = val;
-  },
-
-  _loadGroup: null,
-  get loadGroup() {
-    return this._loadGroup;
-  },
-  set loadGroup(val) {
-    this._loadGroup = val;
-  },
-
-  _name: "",
-  get name() {
-    return this._name;
-  },
-
-  _status: 0,
-  get status() {
-    return this._status;
-  },
-  QueryInterface: XPCOMUtils.generateQI([Ci.nsIIncrementalDownload])
-};
 
 
 
