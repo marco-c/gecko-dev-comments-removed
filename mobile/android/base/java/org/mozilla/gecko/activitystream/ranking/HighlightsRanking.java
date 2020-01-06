0d9f2b79079e2634ebf4733e0e3d009d7af30af5
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 07:48:03 +0000

    Backed out changeset 371d34adf87a (bug 1369604) for Android bustage at testActivityStreamContextMenu.java:297: method fromCursor in class Highlight cannot be applied to given types. r=backout
    UltraBlame original commit: 45f201fe8a9a04031281696a9a25b8d96d636355

--- mobile/android/base/java/org/mozilla/gecko/activitystream/ranking/HighlightsRanking.java
+++ mobile/android/base/java/org/mozilla/gecko/activitystream/ranking/HighlightsRanking.java
@@ -7,8 +7,8 @@ package org.mozilla.gecko.activitystream.ranking;
 
 import android.database.Cursor;
 import android.support.annotation.VisibleForTesting;
-
 import android.util.Log;
+
 import org.mozilla.gecko.activitystream.homepanel.model.Highlight;
 
 import java.util.Arrays;
@@ -96,14 +96,12 @@ public class HighlightsRanking {
 
 
 
-    @VisibleForTesting static List<HighlightCandidate> extractFeatures(final Cursor cursor) {
-        
-        final HighlightCandidateCursorIndices cursorIndices = new HighlightCandidateCursorIndices(cursor);
+    @VisibleForTesting static List<HighlightCandidate> extractFeatures(Cursor cursor) {
         return looselyMapCursor(cursor, new Func1<Cursor, HighlightCandidate>() {
             @Override
             public HighlightCandidate call(Cursor cursor) {
                 try {
-                    return HighlightCandidate.fromCursor(cursor, cursorIndices);
+                    return HighlightCandidate.fromCursor(cursor);
                 } catch (HighlightCandidate.InvalidHighlightCandidateException e) {
                     Log.w(LOG_TAG, "Skipping invalid highlight item", e);
                     return null;
