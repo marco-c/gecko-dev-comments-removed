eb003e810fe400937eae2a772ec6b30f05c28f48
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 09:13:20 +0000

    Backed out changeset e5936d7c91ba (bug 1370682) for bustage: missing return statement at WebRenderTypes.h:361. r=backout on a CLOSED TREE
    UltraBlame original commit: 7376d8576c0214a2f1fd74a5b8a9a9badfc3b463

--- layout/tables/nsTableFrame.cpp
+++ layout/tables/nsTableFrame.cpp
@@ -7449,7 +7449,7 @@ BCBlockDirSeg::CreateWebRenderCommands(BCPaintBorderIterator& aIter,
   }
   wrSide[eSideLeft] = wr::ToBorderSide(ToDeviceColor(param->mBorderColor), param->mBorderStyle);
 
-  wr::BorderRadius borderRadii = wr::EmptyBorderRadius();
+  wr::BorderRadius borderRadii = wr::ToBorderRadius( {0, 0}, {0, 0}, {0, 0}, {0, 0} );
 
   
   
@@ -7707,7 +7707,7 @@ BCInlineDirSeg::CreateWebRenderCommands(BCPaintBorderIterator& aIter,
   }
   wrSide[eSideTop] = wr::ToBorderSide(ToDeviceColor(param->mBorderColor), param->mBorderStyle);
 
-  wr::BorderRadius borderRadii = wr::EmptyBorderRadius();
+  wr::BorderRadius borderRadii = wr::ToBorderRadius( {0, 0}, {0, 0}, {0, 0}, {0, 0} );
 
   
   
