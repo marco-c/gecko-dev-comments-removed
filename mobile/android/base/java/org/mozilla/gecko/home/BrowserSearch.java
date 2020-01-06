c872dd30b70c34f24a91397c965f14f5a91011f6
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 09:26:18 +0000

    Bug 1375351 - Part 3: Remove SkinConfig. r=nechen,walkingice
    SkinConfig is used to detect current skin is Australis or Photon at runtime.
    Since we don't support Australis anymore in Nightly, this class should be removed.
    
    MozReview-Commit-ID: 8yjTFYyQcgt
    
    UltraBlame original commit: 303c7feb73731612834c429d532520314d13de02

--- mobile/android/base/java/org/mozilla/gecko/home/BrowserSearch.java
+++ mobile/android/base/java/org/mozilla/gecko/home/BrowserSearch.java
@@ -33,7 +33,6 @@ import org.mozilla.gecko.db.BrowserContract.URLColumns;
 import org.mozilla.gecko.home.HomePager.OnUrlOpenListener;
 import org.mozilla.gecko.home.SearchLoader.SearchCursorLoader;
 import org.mozilla.gecko.preferences.GeckoPreferences;
-import org.mozilla.gecko.skin.SkinConfig;
 import org.mozilla.gecko.toolbar.AutocompleteHandler;
 import org.mozilla.gecko.util.BundleEventListener;
 import org.mozilla.gecko.util.EventCallback;
@@ -1197,10 +1196,9 @@ public class BrowserSearch extends HomeFragment
                 final Cursor c = getCursor(position);
                 final TwoLinePageRow row = (TwoLinePageRow) view;
 
-                if (SkinConfig.isPhoton()) {
-                    
-                    row.setTitleFormatter(mTwoLinePageRowTitleFormatter);
-                }
+                
+                row.setTitleFormatter(mTwoLinePageRowTitleFormatter);
+
                 row.updateFromCursor(c);
                 row.setPrivateMode(isPrivate);
             }
