29433c2b30291944dca2a055a88c34324da9f79d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 00:16:06 +0000

    Bug 1355890 - Add remote-active system notification r=automatedtester,Gijs
    Adds a new system notification, "remote-active", which fires
    when the Marionette remote protocol becomes active.
    
    MozReview-Commit-ID: 3Parr82Ch6I
    
    UltraBlame original commit: 89c597001a47f4920097ea9eba773cdc1e0a00b8

--- testing/marionette/server.js
+++ testing/marionette/server.js
@@ -43,6 +43,8 @@ const PREF_CONTENT_LISTENER = "marionette.contentListener";
 const PREF_PORT = "marionette.port";
 const PREF_RECOMMENDED = "marionette.prefs.recommended";
 
+const NOTIFY_RUNNING = "remote-active";
+
 
 
 
@@ -319,6 +321,8 @@ server.TCPListener = class {
       return;
     }
 
+    Services.obs.notifyObservers(this, NOTIFY_RUNNING, true);
+
     if (Preferences.get(PREF_RECOMMENDED)) {
       
       for (let [k, v] of RECOMMENDED_PREFS) {
@@ -356,8 +360,10 @@ server.TCPListener = class {
       logger.debug(`Resetting recommended pref ${k}`);
       Preferences.reset(k);
     }
-
     this.alteredPrefs.clear();
+
+    Services.obs.notifyObservers(this, NOTIFY_RUNNING, false);
+
     this.alive = false;
   }
 
