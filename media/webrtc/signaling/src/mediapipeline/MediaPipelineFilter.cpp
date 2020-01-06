21089fe63c40e5fbe775a6f0990fe7b1050fcd5e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 16:37:34 +0000

    Bug 1402334 - Build mediapipeline using unified build; r=drno
    This replaces MOZ_MTLOG with CSFLog, which is already set up to handle having
    a shared LazyLogModule used from difference source files.
    
    MozReview-Commit-ID: KNUKL92aWcw
    
    UltraBlame original commit: 978b0400ac3b1bdbb074c3a759144caec58ba90c

--- media/webrtc/signaling/src/mediapipeline/MediaPipelineFilter.cpp
+++ media/webrtc/signaling/src/mediapipeline/MediaPipelineFilter.cpp
@@ -10,10 +10,14 @@
 #include "MediaPipelineFilter.h"
 
 #include "webrtc/common_types.h"
-#include "logging.h"
 
+#include "CSFLog.h"
 
-MOZ_MTLOG_MODULE("mediapipeline")
+static const char* mpfLogTag = "MediaPipelineFilter";
+#ifdef LOGTAG
+#undef LOGTAG
+#endif
+#define LOGTAG mpfLogTag
 
 namespace mozilla {
 
@@ -41,8 +45,10 @@ bool MediaPipelineFilter::Filter(const webrtc::RTPHeader& header,
     return true;
   }
   if (!header.extension.rtpStreamId.empty()) {
-    MOZ_MTLOG(ML_DEBUG, "MediaPipelineFilter ignoring seq# " << header.sequenceNumber <<
-              " ssrc: " << header.ssrc << " RID: " << header.extension.rtpStreamId.data());
+    CSFLogDebug(LOGTAG,
+                "MediaPipelineFilter ignoring seq# %u ssrc: %u RID: %s",
+                header.sequenceNumber, header.ssrc,
+                header.extension.rtpStreamId.data());
   }
 
   if (remote_ssrc_set_.count(header.ssrc)) {
