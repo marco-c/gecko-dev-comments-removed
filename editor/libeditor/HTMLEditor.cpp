3df0426ee116b369e14feea6977f2a16c2aef188
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 16:40:46 +0000

    Bug 1405751 - Part 1: Remove the HTMLEditor::GetNextHTMLSibling() overload accepting a parent and an offset; r=masayuki
    UltraBlame original commit: fa55d49d1b7b81cd4d1610b0050bd27020a0bb98

--- editor/libeditor/HTMLEditor.cpp
+++ editor/libeditor/HTMLEditor.cpp
@@ -3854,25 +3854,6 @@ HTMLEditor::GetNextHTMLSibling(nsIDOMNode* inNode,
   return NS_OK;
 }
 
-
-
-
-
-
-nsIContent*
-HTMLEditor::GetNextHTMLSibling(nsINode* aParent,
-                               int32_t aOffset)
-{
-  MOZ_ASSERT(aParent);
-
-  nsIContent* node = aParent->GetChildAt(aOffset + 1);
-  if (!node || IsEditable(node)) {
-    return node;
-  }
-
-  return GetNextHTMLSibling(node);
-}
-
 nsresult
 HTMLEditor::GetNextHTMLSibling(nsIDOMNode* inParent,
                                int32_t inOffset,
@@ -3884,7 +3865,10 @@ HTMLEditor::GetNextHTMLSibling(nsIDOMNode* inParent,
   nsCOMPtr<nsINode> parent = do_QueryInterface(inParent);
   NS_ENSURE_TRUE(parent, NS_ERROR_NULL_POINTER);
 
-  *outNode = do_QueryInterface(GetNextHTMLSibling(parent, inOffset));
+  nsCOMPtr<nsINode> child = parent->GetChildAt(inOffset);
+  NS_ENSURE_TRUE(child, NS_ERROR_NULL_POINTER);
+
+  *outNode = do_QueryInterface(GetPriorHTMLSibling(child));
   return NS_OK;
 }
 
