4de20aaee0e994140067241cf289ca45c628c551
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 21:35:19 +0000

    Bug 1410528 - Add a --dll flag to the JS shell for loading shared libs, r=ted,jonco
    UltraBlame original commit: ec0702d55b012ccb507686d5a644e7c568524f0e

--- js/src/jsapi.h
+++ js/src/jsapi.h
@@ -7169,6 +7169,13 @@ typedef bool
 extern JS_PUBLIC_API(bool)
 SetGetPerformanceGroupsCallback(JSContext*, GetGroupsCallback, void*);
 
+
+
+
+
+extern JS_PUBLIC_API(void)
+NoteIntentionalCrash();
+
 } 
 
 namespace js {
