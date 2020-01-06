6a3a08d014f8d72c1ac259ae0b7ba52615f0c4f7
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 08:16:10 +0000

    Bug 1386478 - don't cap the return value of GetEndTime(). r=jya
    A chained ogg file contains multiple streams and timestamps will not
    monolithically increase. The current code will cap AudioEndTime()
    to the end time of the 1st stream and currentTime won't be updated
    correctly when playback position reaches the 2nd stream or later ones.
    
    http://searchfox.org/mozilla-central/rev/bbc1c59e460a27b20929b56489e2e55438de81fa/dom/media/MediaDecoderStateMachine.cpp#3636-3637
    
    MozReview-Commit-ID: DbIspYmEjVv
    
    UltraBlame original commit: d607b1cb6d583af829b9bac5484c6d53961cc66b

--- dom/media/mediasink/AudioSink.cpp
+++ dom/media/mediasink/AudioSink.cpp
@@ -233,9 +233,7 @@ AudioSink::GetEndTime() const
     NS_WARNING("Int overflow calculating audio end time");
     return TimeUnit::Zero();
   }
-  
-  
-  return std::min(mLastEndTime, played);
+  return played;
 }
 
 UniquePtr<AudioStream::Chunk>
