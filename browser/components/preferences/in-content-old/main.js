3a65cde2ca2a17b4fc42c9abd565e81d68fe5633
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 01:25:08 +0000

    Bug 1367350 - Add Learn More link for the "Use recommended performance settings" checkbox. r=jaws
    MozReview-Commit-ID: ALEuQlTphKa
    
    UltraBlame original commit: 17638409ceb0a680ed3f17c4f01581923f6aecfb

--- browser/components/preferences/in-content-old/main.js
+++ browser/components/preferences/in-content-old/main.js
@@ -54,6 +54,10 @@ var gMainPane = {
     });
     this.updatePerformanceSettingsBox();
 
+    let performanceSettingsLink = document.getElementById("performanceSettingsLearnMore");
+    let performanceSettingsUrl = Services.urlFormatter.formatURLPref("app.support.baseURL") + "performance";
+    performanceSettingsLink.setAttribute("href", performanceSettingsUrl);
+
     
     this._updateUseCurrentButton();
     window.addEventListener("focus", this._updateUseCurrentButton.bind(this));
