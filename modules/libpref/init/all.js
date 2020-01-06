405cededb8415ea05304e99ae10f65a5894621fe
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 07:38:40 +0000

    Merge m-c to inbound, a=merge
    MozReview-Commit-ID: 1JlPbFMKbJZ
    
    UltraBlame original commit: c8e2129968bfbf2afb293e72852c4b08cadcbda4

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -202,6 +202,7 @@ pref("dom.gamepad.non_standard_events.enabled", false);
 pref("dom.gamepad.non_standard_events.enabled", true);
 #endif
 pref("dom.gamepad.extensions.enabled", true);
+pref("dom.gamepad.haptic_feedback.enabled", true);
 
 
 
@@ -444,7 +445,7 @@ pref("media.decoder-doctor.wmf-disabled-is-failure", false);
 pref("media.decoder-doctor.new-issue-endpoint", "https://webcompat.com/issues/new");
 
 
-#ifdef RELEASE
+#ifdef RELEASE_OR_BETA
 pref("media.suspend-bkgnd-video.enabled", false);
 #else
 pref("media.suspend-bkgnd-video.enabled", true);
