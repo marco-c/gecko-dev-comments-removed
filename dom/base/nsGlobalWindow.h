713bc6b761975306b1d872003d6e394d30ea1d0a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 21:07:21 +0000

    Bug 1414935.  Hide window.print entirely when it's disabled by pref.  r=mystor
    MozReview-Commit-ID: ArDw85eQYeh
    
    UltraBlame original commit: b9aa0fc4f179dbe92419b3b153df170bd1eb697d

--- dom/base/nsGlobalWindow.h
+++ dom/base/nsGlobalWindow.h
@@ -456,6 +456,8 @@ public:
 
   static bool IsRequestIdleCallbackEnabled(JSContext* aCx, JSObject* );
 
+  static bool IsWindowPrintEnabled(JSContext* , JSObject* );
+
   bool DoResolve(JSContext* aCx, JS::Handle<JSObject*> aObj,
                  JS::Handle<jsid> aId,
                  JS::MutableHandle<JS::PropertyDescriptor> aDesc);
