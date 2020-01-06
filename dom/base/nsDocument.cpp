6714b04b771006ae4ba162c7e0c07255a7d9c117
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 11:32:52 +0000

    Bug 1392892 - Avoid creating a live nsContentList in nsDocument::GetTitleElement(); r=smaug
    UltraBlame original commit: 5419f3e63cae6be9c4c0c426e6ba27c75247d5a9

--- dom/base/nsDocument.cpp
+++ dom/base/nsDocument.cpp
@@ -7160,8 +7160,12 @@ nsDocument::GetTitleElement()
 
   
   
-  RefPtr<nsContentList> list =
-    NS_GetContentList(this, kNameSpaceID_XHTML, NS_LITERAL_STRING("title"));
+  
+  
+  RefPtr<nsContentList> list = new nsContentList(this, kNameSpaceID_XHTML,
+                                                 nsGkAtoms::title, nsGkAtoms::title,
+                                                  true,
+                                                  false);
 
   nsIContent* first = list->Item(0, false);
 
