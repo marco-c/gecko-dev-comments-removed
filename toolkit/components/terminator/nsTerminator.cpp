e995b29e2eb41e06d0ffd57f41dd0a1b578564c7
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 19:37:02 +0000

    Bug 1401400 - Part 3. Process all threads when crashing for hang. r=gsvelto
    MozReview-Commit-ID: JH4PGYJreF7
    
    UltraBlame original commit: 687bbdd7afaf107b2a40cd1d6e671c74fbe828be

--- toolkit/components/terminator/nsTerminator.cpp
+++ toolkit/components/terminator/nsTerminator.cpp
@@ -157,6 +157,9 @@ RunWatchdog(void* arg)
     }
 
     
+#if defined(MOZ_CRASHREPORTER)
+    CrashReporter::SetMinidumpAnalysisAllThreads();
+#endif
     MOZ_CRASH("Shutdown too long, probably frozen, causing a crash.");
   }
 }
