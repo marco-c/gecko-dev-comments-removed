ce0ae0e8196c2f5df411157e243d3179f2285306
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 20:03:15 +0000

    Simplify copying the front buffer to the back buffer (bug 1399692 part 5, r=bas)
    To sync the back buffer with the front buffer, we set the back buffer rect and
    rotation to the front buffer's, and then copy over the pixels that different.
    
    We used to do the updating of the rect and rotation before BeginPaint, but that
    isn't necessary and we can move it to be with the copying of pixels.
    
    MozReview-Commit-ID: HzBKvMZkn1
    
    UltraBlame original commit: 3d0da65640964e5a0565b5e7b7646cf719ce1449

--- gfx/layers/client/ContentClient.cpp
+++ gfx/layers/client/ContentClient.cpp
@@ -163,7 +163,6 @@ ContentClient::BeginPaint(PaintedLayer* aLayer,
 
     if (mBuffer->Lock(lockMode)) {
       
-      
       FinalizeFrame(result.mRegionToDraw);
 
       if (!mBuffer->AdjustTo(dest.mBufferRect,
@@ -834,26 +833,8 @@ ContentClientDoubleBuffered::BeginPaint(PaintedLayer* aLayer,
                                         uint32_t aFlags)
 {
   mIsNewBuffer = false;
-
   if (!mFrontBuffer || !mBuffer) {
     mFrontAndBackBufferDiffer = false;
-  }
-
-  if (mFrontAndBackBufferDiffer) {
-    if (mFrontBuffer->DidSelfCopy()) {
-      
-      
-      
-      
-      gfx::IntRect backBufferRect = mBuffer->BufferRect();
-      backBufferRect.MoveTo(mFrontBuffer->BufferRect().TopLeft());
-
-      mBuffer->SetBufferRect(backBufferRect);
-      mBuffer->SetBufferRotation(IntPoint(0,0));
-    } else {
-      mBuffer->SetBufferRect(mFrontBuffer->BufferRect());
-      mBuffer->SetBufferRotation(mFrontBuffer->BufferRotation());
-    }
   }
 
   return ContentClient::BeginPaint(aLayer, aFlags);
@@ -873,11 +854,13 @@ void
 ContentClientDoubleBuffered::FinalizeFrame(const nsIntRegion& aRegionToDraw)
 {
   if (!mFrontAndBackBufferDiffer) {
-    MOZ_ASSERT(!mFrontBuffer->DidSelfCopy(), "If we have to copy the world, then our buffers are different, right?");
+    MOZ_ASSERT(!mFrontBuffer || !mFrontBuffer->DidSelfCopy(),
+               "If the front buffer did a self copy then our front and back buffer must be different.");
     return;
   }
-  MOZ_ASSERT(mFrontBuffer);
-  if (!mFrontBuffer) {
+
+  MOZ_ASSERT(mFrontBuffer && mBuffer);
+  if (!mFrontBuffer || !mBuffer) {
     return;
   }
 
@@ -890,8 +873,20 @@ ContentClientDoubleBuffered::FinalizeFrame(const nsIntRegion& aRegionToDraw)
 
   mFrontAndBackBufferDiffer = false;
 
+  
+  
+  gfx::IntRect backBufferRect = mBuffer->BufferRect();
+  backBufferRect.MoveTo(mFrontBuffer->BufferRect().TopLeft());
+  mBuffer->SetBufferRect(backBufferRect);
+  mBuffer->SetBufferRotation(mBuffer->BufferRotation());
+
+  
   nsIntRegion updateRegion = mFrontUpdatedRegion;
   if (mFrontBuffer->DidSelfCopy()) {
+    
+    
+    mBuffer->SetBufferRotation(IntPoint(0,0));
+
     mFrontBuffer->ClearDidSelfCopy();
     updateRegion = mBuffer->BufferRect();
   }
@@ -900,10 +895,6 @@ ContentClientDoubleBuffered::FinalizeFrame(const nsIntRegion& aRegionToDraw)
   
   updateRegion.Sub(updateRegion, aRegionToDraw);
   if (updateRegion.IsEmpty()) {
-    return;
-  }
-
-  if (!mBuffer) {
     return;
   }
 
