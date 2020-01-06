db1cf5f19b8188532df1c56afc419178156f3f7d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 00:50:57 +0000

    Bug 1359203 Part 3 Add extensions.legacy.enabled preference
    MozReview-Commit-ID: 3CaWhlcOgql
    
    UltraBlame original commit: 4e40b5b294428fc51707a47a471cdb5d90424fdd

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -4824,6 +4824,7 @@ pref("xpinstall.signatures.required", false);
 pref("extensions.alwaysUnpack", false);
 pref("extensions.minCompatiblePlatformVersion", "2.0");
 pref("extensions.webExtensionsMinPlatformVersion", "42.0a1");
+pref("extensions.legacy.enabled", true);
 pref("extensions.allow-non-mpc-extensions", true);
 
 
@@ -5730,25 +5731,13 @@ pref("fuzzing.enabled", false);
 
 
 
-pref("layers.advanced.background-color", 2);
-pref("layers.advanced.background-image", 2);
 pref("layers.advanced.border-layers", 2);
 pref("layers.advanced.boxshadow-inset-layers", 2);
 pref("layers.advanced.boxshadow-outer-layers", 2);
-pref("layers.advanced.bullet-layers", 2);
-pref("layers.advanced.button-foreground-layers", 2);
-pref("layers.advanced.canvas-background-color", 2);
 pref("layers.advanced.caret-layers", 2);
-pref("layers.advanced.columnRule-layers", 2);
 pref("layers.advanced.displaybuttonborder-layers", 2);
-pref("layers.advanced.image-layers", 2);
 pref("layers.advanced.outline-layers", 2);
-pref("layers.advanced.solid-color", 2);
-pref("layers.advanced.table", 2);
-pref("layers.advanced.text-layers", 2);
-
-
-pref("gfx.webrendest.enabled", false);
+pref("layers.advanced.solid-color-layers", 2);
 
 
 pref("dom.xhr.lowercase_header.enabled", true);
