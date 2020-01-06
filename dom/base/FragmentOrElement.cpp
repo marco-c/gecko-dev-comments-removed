5b4a568da58ffc1300be1a641ca0011874775fa5
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 08:08:01 +0000

    Backed out changeset 5dd6f04aea59 (bug 1383780) for failing crashtests on stylo. r=backout on a CLOSED TREE
    UltraBlame original commit: 9ba3d16a975bdfa01508a969ba5a4f1841cb6463

--- dom/base/FragmentOrElement.cpp
+++ dom/base/FragmentOrElement.cpp
@@ -383,9 +383,7 @@ nsIContent::GetBaseURI(bool aTryUseXHRDocBaseURI) const
     MOZ_ASSERT(bindingParent);
     SVGUseElement* useElement = static_cast<SVGUseElement*>(bindingParent);
     
-    if (URLExtraData* data = useElement->GetContentURLData()) {
-      return do_AddRef(data->BaseURI());
-    }
+    return do_AddRef(useElement->GetContentURLData()->BaseURI());
   }
 
   nsIDocument* doc = OwnerDoc();
@@ -455,9 +453,7 @@ nsIContent::GetBaseURIWithoutXMLBase() const
     nsIContent* bindingParent = GetBindingParent();
     MOZ_ASSERT(bindingParent);
     SVGUseElement* useElement = static_cast<SVGUseElement*>(bindingParent);
-    if (URLExtraData* data = useElement->GetContentURLData()) {
-      return data->BaseURI();
-    }
+    return useElement->GetContentURLData()->BaseURI();
   }
   
   
@@ -491,9 +487,7 @@ nsIContent::GetURLDataForStyleAttr() const
     nsIContent* bindingParent = GetBindingParent();
     MOZ_ASSERT(bindingParent);
     SVGUseElement* useElement = static_cast<SVGUseElement*>(bindingParent);
-    if (URLExtraData* data = useElement->GetContentURLData()) {
-      return data;
-    }
+    return useElement->GetContentURLData();
   }
   
   
