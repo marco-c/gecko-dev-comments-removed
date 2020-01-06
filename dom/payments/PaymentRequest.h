d69b9de4cf036e9c26a39c90ea0a0a86e2e96146
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 03:15:48 +0000

    Bug 1345365 - PaymentRequest API canMakePayment(), abort() and show() implementation. r=baku
    UltraBlame original commit: bba1dee067699e2e2dd3f36e8997cb596a56f0a5

--- dom/payments/PaymentRequest.h
+++ dom/payments/PaymentRequest.h
@@ -17,6 +17,7 @@ namespace mozilla {
 namespace dom {
 
 class EventHandlerNonNull;
+class PaymentAddress;
 class PaymentResponse;
 
 class PaymentRequest final : public DOMEventTargetHelper
@@ -33,6 +34,9 @@ public:
   CreatePaymentRequest(nsPIDOMWindowInner* aWindow, nsresult& aRv);
 
   static bool PrefEnabled(JSContext* aCx, JSObject* aObj);
+
+  static bool IsValidMethodData(const Sequence<PaymentMethodData>& aMethodData,
+                                nsAString& aErrorMsg);
 
   static bool
   IsValidNumber(const nsAString& aItem,
@@ -57,9 +61,22 @@ public:
               const PaymentOptions& aOptions,
               ErrorResult& aRv);
 
-  already_AddRefed<Promise> Show(ErrorResult& aRv);
-  already_AddRefed<Promise> Abort(ErrorResult& aRv);
   already_AddRefed<Promise> CanMakePayment(ErrorResult& aRv);
+  void RespondCanMakePayment(bool aResult);
+
+  already_AddRefed<Promise> Show(ErrorResult& aRv);
+  void RespondShowPayment(bool aAccept,
+                          const nsAString& aMethodName,
+                          const nsAString& aDetails,
+                          const nsAString& aPayerName,
+                          const nsAString& aPayerEmail,
+                          const nsAString& aPayerPhone,
+                          nsresult aRv = NS_ERROR_DOM_ABORT_ERR);
+  void RejectShowPayment(nsresult aRejectReason);
+  void RespondComplete();
+
+  already_AddRefed<Promise> Abort(ErrorResult& aRv);
+  void RespondAbortPayment(bool aResult);
 
   void GetId(nsAString& aRetVal) const;
   void GetInternalId(nsAString& aRetVal);
@@ -67,8 +84,10 @@ public:
 
   bool Equals(const nsAString& aInternalId) const;
 
+  bool ReadyForUpdate();
   void SetUpdating(bool aUpdating);
 
+  already_AddRefed<PaymentAddress> GetShippingAddress() const;
   void GetShippingOption(nsAString& aRetVal) const;
 
   Nullable<PaymentShippingType> GetShippingType() const;
@@ -87,6 +106,16 @@ protected:
   
   
   nsString mId;
+  
+  RefPtr<Promise> mResultPromise;
+  
+  RefPtr<Promise> mAcceptPromise;
+  
+  RefPtr<Promise> mAbortPromise;
+  
+  RefPtr<PaymentResponse> mResponse;
+  
+  RefPtr<PaymentAddress> mShippingAddress;
   
   nsString mShippingOption;
 
--- dom/payments/PaymentRequestService.h
+++ dom/payments/PaymentRequestService.h
@@ -36,7 +36,21 @@ public:
 private:
   ~PaymentRequestService() = default;
 
+  nsresult
+  SetActionCallback(const nsAString& aRequestId,
+                    nsIPaymentActionCallback* aCallback);
+  nsresult
+  RemoveActionCallback(const nsAString& aRequestId);
+
+  
+  nsresult
+  CallTestingUIAction(const nsAString& aRequestId, uint32_t aActionType);
+
   FallibleTArray<nsCOMPtr<nsIPaymentRequest>> mRequestQueue;
+
+  nsInterfaceHashtable<nsStringHashKey, nsIPaymentActionCallback> mCallbackHashtable;
+
+  nsCOMPtr<nsIPaymentUIService> mTestingUIService;
 };
 
 } 
