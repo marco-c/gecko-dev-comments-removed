015a508a2e146695d3bdfd13480587c6ccb8a503
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 07:22:09 +0000

    Bug 1384459 - Only enable WebVR device enumeration for 64-bit builds r=jgilbert We wish to enable WebVR only for 64-bit builds before it rides the trains with 55 in release.
    This will serve a few purposes:
    - Reduction of test requirements by reduction of configuration matrices.
    - Ensuring that the optimal 64-bit builds are used for WebVR sites,
      which are often likely to hit 32-bit address space fragmentation
      limitations resulting in OOMs.
    - Act as a rudimentary soft-launch in 55.  56 is expected to bring a
      larger set of users to 64-bit builds.
    
    MozReview-Commit-ID: 207ABcd31dP
    
    UltraBlame original commit: 813927e0ce55bb64118d89266928883bdaf47a4a

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -5237,7 +5237,12 @@ pref("dom.vr.controller_trigger_threshold", "0.1");
 
 pref("dom.vr.navigation.timeout", 5000);
 
+#if defined(HAVE_64BIT_BUILD)
+
 pref("dom.vr.oculus.enabled", true);
+#else
+pref("dom.vr.oculus.enabled", false);
+#endif
 
 
 
@@ -5257,7 +5262,8 @@ pref("dom.vr.oculus.quit.timeout", 30000);
 
 pref("dom.vr.osvr.enabled", false);
 
-#ifdef XP_WIN
+#if defined(XP_WIN) && defined(HAVE_64BIT_BUILD)
+
 pref("dom.vr.openvr.enabled", true);
 #else
 
