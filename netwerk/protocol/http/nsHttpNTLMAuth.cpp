1832b6b66ad1b08ab3d9411937f36b1e7e8e66df
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 11:13:50 +0000

    Bug 1392641 Check return value for functions declared with warn_unused_result r=bagder,nwgh
    MozReview-Commit-ID: BJj3Lvtntd6
    
    UltraBlame original commit: 56ae3de7d69c13dcef92308a4695b5db7307dade

--- netwerk/protocol/http/nsHttpNTLMAuth.cpp
+++ netwerk/protocol/http/nsHttpNTLMAuth.cpp
@@ -351,7 +351,9 @@ nsHttpNTLMAuth::GenerateCredentials(nsIHttpAuthenticableChannel *authChannel,
 
             uint32_t length;
             uint8_t* certArray;
-            cert->GetRawDER(&length, &certArray);
+            rv = cert->GetRawDER(&length, &certArray);
+            if (NS_FAILED(rv))
+                return rv;
 
             
             
