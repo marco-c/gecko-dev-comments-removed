ba7e6fe6a7de585d1287d2ff01c8421d7b7a4160
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 23:47:47 +0000

    Bug 1403840 - add cose rust lib with a test, r=keeler,ttaubert
    Summary:
    This adds the COSE rust library from https://github.com/franziskuskiefer/cose-rust with its C API from https://github.com/franziskuskiefer/cose-c-api to gecko with a basic test.
    The COSE library will be used for verifying add-on signatures in future.
    
    Reviewers: keeler, ttaubert
    
    Reviewed By: keeler
    
    Bug #: 1403840
    
    Differential Revision: https://phabricator.services.mozilla.com/D232
    
    UltraBlame original commit: cfcbb8333389ccf2ff91176f1aecf50199be018b

---
+++ third_party/rust/cose/build.rs
@@ -0,0 +1,11 @@
+use std::env;
+
+fn main() {
+    
+    
+    #[cfg(test)]
+    let lib_dir = env::var("NSS_LIB_DIR");
+    if let Ok(lib_dir) = env::var("NSS_LIB_DIR") {
+        println!("cargo:rustc-link-search={}", lib_dir);
+    }
+}
