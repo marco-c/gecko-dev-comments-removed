e67b36e75c97742cd56376bd17e76e7c59588073
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 17:59:15 +0000

    Fix a coordinate space bug when composing some intermediate surfaces. (bug 1408566 part 1, r=mattwoodrow)
    UltraBlame original commit: 740c4ed36aa1e30402fc5c1c5ae66177c563dc28

--- gfx/layers/mlgpu/RenderPassMLGPU.cpp
+++ gfx/layers/mlgpu/RenderPassMLGPU.cpp
@@ -1020,25 +1020,28 @@ RenderViewPass::RenderWithBackdropCopy()
   MOZ_ASSERT(transform.Is2D(&transform2d) &&
              !gfx::ThebesMatrix(transform2d).HasNonIntegerTranslation());
 
+  IntPoint translation = IntPoint::Truncate(transform._41, transform._42);
+
+  RenderViewMLGPU* childView = mAssignedLayer->GetRenderView();
+
   IntRect visible = mAssignedLayer->GetShadowVisibleRegion().GetBounds().ToUnknownRect();
-  visible += IntPoint::Truncate(transform._41, transform._42);
-  visible -= mParentView->GetTargetOffset();
+  IntRect sourceRect = visible + translation - mParentView->GetTargetOffset();
+  IntPoint destPoint = visible.TopLeft() - childView->GetTargetOffset();
 
   RefPtr<MLGTexture> dest = mAssignedLayer->GetRenderTarget()->GetTexture();
   RefPtr<MLGTexture> source = mParentView->GetRenderTarget()->GetTexture();
 
   
+  sourceRect = sourceRect.Intersect(IntRect(IntPoint(0, 0), source->GetSize()));
+
   
-  visible = visible.Intersect(IntRect(IntPoint(0, 0), source->GetSize()));
-  visible = visible.Intersect(IntRect(visible.TopLeft(), dest->GetSize()));
+  IntRect destRect(destPoint, sourceRect.Size());
+  destRect = destRect.Intersect(IntRect(IntPoint(0, 0), dest->GetSize()));
+  sourceRect = sourceRect.Intersect(IntRect(sourceRect.TopLeft(), destRect.Size()));
 
-  mDevice->CopyTexture(dest, IntPoint(0, 0), source, visible);
-
-  RenderViewMLGPU* childView = mAssignedLayer->GetRenderView();
+  mDevice->CopyTexture(dest, destPoint, source, sourceRect);
   childView->RenderAfterBackdropCopy();
-
   mParentView->RestoreDeviceState();
-
   TexturedRenderPass::ExecuteRendering();
 }
 
