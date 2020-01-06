b01f78bdcf0eb54839a9db1defaa01e7b660ea30
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 19:25:35 +0000

    Bug 1411786 - Use mozilla::Atomic for the recycled size count. r=njn
    UltraBlame original commit: 98893371b4c0388525b93f1ef6e391385158397f

--- memory/build/mozjemalloc.cpp
+++ memory/build/mozjemalloc.cpp
@@ -109,10 +109,11 @@
 
 #include "mozmemory_wrap.h"
 #include "mozjemalloc.h"
-#include "mozilla/Sprintf.h"
-#include "mozilla/Likely.h"
+#include "mozilla/Atomics.h"
 #include "mozilla/DoublyLinkedList.h"
 #include "mozilla/GuardObjects.h"
+#include "mozilla/Likely.h"
+#include "mozilla/Sprintf.h"
 #include "mozilla/UniquePtr.h"
 
 
@@ -483,7 +484,7 @@ static size_t arena_maxclass;
 static const size_t gRecycleLimit = CHUNK_RECYCLE_LIMIT * CHUNKSIZE_DEFAULT;
 
 
-static size_t gRecycledSize;
+static mozilla::Atomic<size_t, mozilla::ReleaseAcquire> gRecycledSize;
 
 
 
@@ -1200,24 +1201,6 @@ FORK_HOOK void _malloc_postfork_child(void);
 
 
 
-
-static inline size_t
-load_acquire_z(size_t *p)
-{
-	volatile size_t result = *p;
-#  ifdef XP_WIN
-	
-
-
-
-
-	volatile long dummy = 0;
-	InterlockedExchange(&dummy, 1);
-#  else
-	__sync_synchronize();
-#  endif
-	return result;
-}
 
 static void
 _malloc_message(const char *p)
@@ -2178,7 +2161,7 @@ chunk_dealloc(void* aChunk, size_t aSize, ChunkType aType)
   gChunkRTree.Unset(aChunk);
 
   if (CAN_RECYCLE(aSize)) {
-    size_t recycled_so_far = load_acquire_z(&gRecycledSize);
+    size_t recycled_so_far = gRecycledSize;
     
     if (recycled_so_far < gRecycleLimit) {
       size_t recycle_remaining = gRecycleLimit - recycled_so_far;
