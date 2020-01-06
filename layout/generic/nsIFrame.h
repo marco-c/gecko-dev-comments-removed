1b1e2b5a60ca386a7375a2887a7dcc47ba6e1971
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 01:54:39 +0000

    Bug 1368369 - pt 1 - Use nsTArray instead of AutoTArray in FrameProperties, and embed the FrameProperties object directly in nsIFrame. r=mats
    UltraBlame original commit: 4abbee1979c215ffbf126534d45bf1ca76c0b29b

--- layout/generic/nsIFrame.h
+++ layout/generic/nsIFrame.h
@@ -3390,22 +3390,13 @@ public:
   GetProperty(FrameProperties::Descriptor<T> aProperty,
               bool* aFoundResult = nullptr) const
   {
-    if (mProperties) {
-      return mProperties->Get(aProperty, aFoundResult);
-    }
-    if (aFoundResult) {
-      *aFoundResult = false;
-    }
-    return FrameProperties::ReinterpretHelper<T>::FromPointer(nullptr);
+    return mProperties.Get(aProperty, aFoundResult);
   }
 
   template<typename T>
   bool HasProperty(FrameProperties::Descriptor<T> aProperty) const
   {
-    if (mProperties) {
-      return mProperties->Has(aProperty);
-    }
-    return false;
+    return mProperties.Has(aProperty);
   }
 
   
@@ -3413,10 +3404,7 @@ public:
   void SetProperty(FrameProperties::Descriptor<T> aProperty,
                    FrameProperties::PropertyType<T> aValue)
   {
-    if (!mProperties) {
-      mProperties = mozilla::MakeUnique<FrameProperties>();
-    }
-    mProperties->Set(aProperty, aValue, this);
+    mProperties.Set(aProperty, aValue, this);
   }
 
   
@@ -3425,10 +3413,7 @@ public:
   void AddProperty(FrameProperties::Descriptor<T> aProperty,
                    FrameProperties::PropertyType<T> aValue)
   {
-    if (!mProperties) {
-      mProperties = mozilla::MakeUnique<FrameProperties>();
-    }
-    mProperties->Add(aProperty, aValue);
+    mProperties.Add(aProperty, aValue);
   }
 
   template<typename T>
@@ -3436,28 +3421,18 @@ public:
   RemoveProperty(FrameProperties::Descriptor<T> aProperty,
                  bool* aFoundResult = nullptr)
   {
-    if (mProperties) {
-      return mProperties->Remove(aProperty, aFoundResult);
-    }
-    if (aFoundResult) {
-      *aFoundResult = false;
-    }
-    return FrameProperties::ReinterpretHelper<T>::FromPointer(nullptr);
+    return mProperties.Remove(aProperty, aFoundResult);
   }
 
   template<typename T>
   void DeleteProperty(FrameProperties::Descriptor<T> aProperty)
   {
-    if (mProperties) {
-      mProperties->Delete(aProperty, this);
-    }
+    mProperties.Delete(aProperty, this);
   }
 
   void DeleteAllProperties()
   {
-    if (mProperties) {
-      mProperties->DeleteAll(this);
-    }
+    mProperties.DeleteAll(this);
   }
 
   
@@ -3966,8 +3941,7 @@ protected:
   
 
 
-
-  mozilla::UniquePtr<FrameProperties> mProperties;
+  FrameProperties  mProperties;
 
   
   
