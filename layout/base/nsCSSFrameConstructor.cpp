c27372be4e29de4026ed2d601ba92d4e7fd27470
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 18:42:08 +0000

    Bug 1410226: Properly compute the insertion point for a display: contents child in an XBL binding. r=mats
    MozReview-Commit-ID: DzuGDHZEc1A
    
    UltraBlame original commit: cc74db25e470a24480dba59c56e74049c3aa774a

--- layout/base/nsCSSFrameConstructor.cpp
+++ layout/base/nsCSSFrameConstructor.cpp
@@ -7111,27 +7111,23 @@ nsCSSFrameConstructor::GetInsertionPrevSibling(InsertionPoint* aInsertion,
 nsContainerFrame*
 nsCSSFrameConstructor::GetContentInsertionFrameFor(nsIContent* aContent)
 {
-  
-  nsIFrame* frame = aContent->GetPrimaryFrame();
+  nsIFrame* frame;
+  while (!(frame = aContent->GetPrimaryFrame())) {
+    if (!GetDisplayContentsStyleFor(aContent)) {
+      return nullptr;
+    }
 
-  if (!frame) {
-    if (GetDisplayContentsStyleFor(aContent)) {
-      nsIContent* parent = aContent->GetParent();
-      if (parent && parent == aContent->GetContainingShadow()) {
-        parent = parent->GetBindingParent();
-      }
-      frame = parent ? GetContentInsertionFrameFor(parent) : nullptr;
-    }
-    if (!frame) {
+    aContent = aContent->GetFlattenedTreeParent();
+    if (!aContent) {
       return nullptr;
     }
-  } else {
-    
-    
-    
-    if (frame->GetContent() != aContent) {
-      return nullptr;
-    }
+  }
+
+  
+  
+  
+  if (frame->GetContent() != aContent) {
+    return nullptr;
   }
 
   nsContainerFrame* insertionFrame = frame->GetContentInsertionFrame();
