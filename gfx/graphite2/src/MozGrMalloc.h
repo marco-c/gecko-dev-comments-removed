18e4d44fed8714ff7f3801ddc44f8e7ed1ef9e6d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 19:14:25 +0000

    Bug 1407601 Revert to the previous behavior in MozGrMalloc.h for Linux, and add extern C to correct Solaris build break r=baku
    MozReview-Commit-ID: GP6N6pcqQp
    
    UltraBlame original commit: 9f9a79521c243d3d71ae385cfcec016de46f134e

--- gfx/graphite2/src/MozGrMalloc.h
+++ gfx/graphite2/src/MozGrMalloc.h
@@ -12,19 +12,33 @@
 
 #include "mozilla/mozalloc.h"
 
-inline void* malloc(size_t size)
+#if defined(XP_LINUX)
+
+#define malloc moz_xmalloc
+#define calloc moz_xcalloc
+#define realloc moz_xrealloc
+
+#else
+
+
+
+
+
+extern "C" inline void* malloc(size_t size)
 {
     return moz_xmalloc(size);
 }
 
-inline void* calloc(size_t nmemb, size_t size)
+extern "C" inline void* calloc(size_t nmemb, size_t size)
 {
     return moz_xcalloc(nmemb, size);
 }
 
-inline void* realloc(void *ptr, size_t size)
+extern "C" inline void* realloc(void *ptr, size_t size)
 {
     return moz_xrealloc(ptr, size);
 }
+
+#endif 
 
 #endif 
