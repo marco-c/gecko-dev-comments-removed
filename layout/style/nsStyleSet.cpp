ebc8ca63fddd6d19b80f368d13c759623d611102
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 04:51:54 +0000

    Bug 1377196 Part 2 - Add AppendAllXBLStyleSheets to ServoStyleSet. r=heycam
    MozReview-Commit-ID: HSQnBk8djil
    
    UltraBlame original commit: f065e3a455a6b7bbd69b02706a5edc84d7241e30

--- layout/style/nsStyleSet.cpp
+++ layout/style/nsStyleSet.cpp
@@ -745,19 +745,10 @@ nsStyleSet::AddDocStyleSheet(CSSStyleSheet* aSheet, nsIDocument* aDocument)
 }
 
 void
-nsStyleSet::AppendAllXBLStyleSheets(nsTArray<mozilla::CSSStyleSheet*>& aArray) const
+nsStyleSet::AppendAllXBLStyleSheets(nsTArray<StyleSheet*>& aArray) const
 {
   if (mBindingManager) {
-    
-    
-    
-    AutoTArray<StyleSheet*, 32> sheets;
-    mBindingManager->AppendAllSheets(sheets);
-    for (StyleSheet* handle : sheets) {
-      MOZ_ASSERT(handle->IsGecko(), "stylo: AppendAllSheets shouldn't give us "
-                                    "ServoStyleSheets yet");
-      aArray.AppendElement(handle->AsGecko());
-    }
+    mBindingManager->AppendAllSheets(aArray);
   }
 }
 
