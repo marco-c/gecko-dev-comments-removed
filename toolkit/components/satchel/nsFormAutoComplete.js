a65aa6e891eaa45a61e98dc396bb5181c9392365
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 15:18:43 +0000

    Backed out changeset 553d26ab8ee8 (bug 1395427)
    UltraBlame original commit: 583edc8f8c18deba37cf0bb1d9416d76ba0839de

--- toolkit/components/satchel/nsFormAutoComplete.js
+++ toolkit/components/satchel/nsFormAutoComplete.js
@@ -118,15 +118,10 @@ FormHistoryClient.prototype = {
 
 
 
-
-
-
-
-  remove(value, guid) {
+  remove(value) {
     this.mm.sendAsyncMessage("FormHistory:RemoveEntry", {
       inputName: this.inputName,
       value,
-      guid
     });
   },
 
@@ -626,7 +621,7 @@ FormAutoCompleteResult.prototype = {
     let [removedEntry] = this.entries.splice(index, 1);
 
     if (removeFromDB) {
-      this.client.remove(removedEntry.text, removedEntry.guid);
+      this.client.remove(removedEntry.text);
     }
   },
 };
