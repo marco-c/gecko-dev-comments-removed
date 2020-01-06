70ffc1ed6118cdb37f3a016898e5a8cb66b03885
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 09:47:12 +0000

    Bug 1376895 - Make preloaded browser use pre-existing content process. r=mconley We want to avoid to have several cached content processes, one for each preloaded browser (one per window) and one for the preallocated process. For that we force the preloaded browser to choose an existing process and during the first navigation in that tab, that leaves about:newtab, we re-run the process selecting algorithm
    UltraBlame original commit: adf5ed713e0df1a523b8b7634dd7b943a1daa5b2

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -1088,6 +1088,14 @@ function _loadURIWithFlags(browser, uri, params) {
   }
 
   let mustChangeProcess = requiredRemoteType != currentRemoteType;
+  let newFrameloader = false;
+  if (browser.getAttribute("isPreloadBrowser") == "true" && uri != "about:newtab") {
+    
+    
+    mustChangeProcess = true;
+    newFrameloader = true;
+    browser.removeAttribute("isPreloadBrowser");
+  }
 
   
   if (!requiredRemoteType) {
@@ -1122,7 +1130,8 @@ function _loadURIWithFlags(browser, uri, params) {
         referrer: referrer ? referrer.spec : null,
         referrerPolicy,
         remoteType: requiredRemoteType,
-        postData
+        postData,
+        newFrameloader,
       }
 
       if (params.userContextId) {
@@ -1167,6 +1176,11 @@ function LoadInOtherProcess(browser, loadOptions, historyIndex = -1) {
 
 
 function RedirectLoad({ target: browser, data }) {
+  if (browser.getAttribute("isPreloadBrowser") == "true") {
+    browser.removeAttribute("isPreloadBrowser");
+    data.loadOptions.newFrameloader = true;
+  }
+
   if (data.loadOptions.reloadInFreshProcess) {
     
     
