55409e88be7005a5733a2d27b5370bd306e13abb
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 02:46:53 +0000

    bug 1370176, generic configuration for l10n, r=flod,gps
    The current way to configure compare-locales has a lot of
    assumptions that make our l10n system really stubborn.
    
    The generic configuration is independent of python, and uses
    toml files for configuration. They're still modular, but
    there's only one file format.
    
    See http://moz-l10n-config.readthedocs.io/en/latest/fileformat.html
    for the specification.
    
    Also fixes a few nits in filter.py, where we compared the
    entity key as bool, which is false if we pass in ''.
    Explicitly compare as "entity is None" to be precise about
    when we're checking files.
    
    MozReview-Commit-ID: 5TmfobaImF4
    
    UltraBlame original commit: 91ddd067412484aec8d482eabe27eb048bea7ef8

--- mobile/android/locales/filter.py
+++ mobile/android/locales/filter.py
@@ -53,7 +53,7 @@ def test(mod, path, entity = None):
     
     return "error"
   if mod == "mobile/android":
-    if not entity:
+    if entity is None:
       if (re.match(r"mobile-l10n.js", path) or
           re.match(r"defines.inc", path)):
         return "ignore"
