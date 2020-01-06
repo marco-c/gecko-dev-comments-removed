0b187d678b4e4d4b979cce4a4e9f7e2f1306697e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 18:26:13 +0000

    Bug 1376973 - Part 1: Making the favicon of the allTabs menu obeys originAttributes and rename attribute 'iconLoadingPrincipal' to 'iconloadingprincipal'. r=arthuredelstein,dao
    MozReview-Commit-ID: AqjIr7xkXs9
    
    UltraBlame original commit: 0f393e8b236da0db7063d5b933f74cef20d84824

--- browser/components/sessionstore/test/browser_attributes.js
+++ browser/components/sessionstore/test/browser_attributes.js
@@ -32,7 +32,7 @@ add_task(async function test() {
 
   
   ok(tab.hasAttribute("image"), "tab.image exists");
-  ok(tab.hasAttribute("iconLoadingPrincipal"), "tab.iconLoadingPrincipal exists");
+  ok(tab.hasAttribute("iconloadingprincipal"), "tab.iconloadingprincipal exists");
 
   tab.toggleMuteAudio();
   
@@ -45,11 +45,11 @@ add_task(async function test() {
   
   ss.persistTabAttribute("image");
   ss.persistTabAttribute("muted");
-  ss.persistTabAttribute("iconLoadingPrincipal");
+  ss.persistTabAttribute("iconloadingprincipal");
   ss.persistTabAttribute("activemedia-blocked");
   let {attributes} = JSON.parse(ss.getTabState(tab));
   ok(!("image" in attributes), "'image' attribute not saved");
-  ok(!("iconLoadingPrincipal" in attributes), "'iconLoadingPrincipal' attribute not saved");
+  ok(!("iconloadingprincipal" in attributes), "'iconloadingprincipal' attribute not saved");
   ok(!("muted" in attributes), "'muted' attribute not saved");
   ok(!("custom" in attributes), "'custom' attribute not saved");
   ok(!("activemedia-blocked" in attributes), "'activemedia-blocked' attribute not saved");
