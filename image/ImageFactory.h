774fe1d314473555c373076ee5d2bcd299ceb153
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 07:08:15 +0000

    Bug 1377158 - (Part 4) Add a new parameter to pass style backend type to create image. r=heycam
    MozReview-Commit-ID: 74gWTbUyYOZ
    
    UltraBlame original commit: b05327c3c55f15d52e7b3adf474e944ce600ef03

--- image/ImageFactory.h
+++ image/ImageFactory.h
@@ -7,6 +7,7 @@
 #ifndef mozilla_image_ImageFactory_h
 #define mozilla_image_ImageFactory_h
 
+#include "mozilla/StyleBackendType.h"
 #include "nsCOMPtr.h"
 #include "nsProxyRelease.h"
 
@@ -45,7 +46,8 @@ public:
                                              const nsCString& aMimeType,
                                              ImageURL* aURI,
                                              bool aIsMultiPart,
-                                             uint32_t aInnerWindowId);
+                                             uint32_t aInnerWindowId,
+                                             StyleBackendType aStyleBackendType = StyleBackendType::None);
   
 
 
@@ -83,7 +85,8 @@ private:
                     const nsCString& aMimeType,
                     ImageURL* aURI,
                     uint32_t aImageFlags,
-                    uint32_t aInnerWindowId);
+                    uint32_t aInnerWindowId,
+                    StyleBackendType aStyleBackendType);
 
   
   virtual ~ImageFactory() = 0;
