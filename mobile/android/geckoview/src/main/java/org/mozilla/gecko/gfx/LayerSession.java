76f1d888bb2e437961e165522d0f7be742db596e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 21:55:32 +0000

    Bug 1416310 - 3. Merge GeckoLayerClient into other classes; r=rbarker
    Move the GeckoLayerClient JNI callbacks to LayerSession.Compositor. Move
    the rest of the viewport code to LayerView. Finally, move the input
    event synthesis code to NativePanZoomController.
    
    MozReview-Commit-ID: 1FEAM43KcwL
    
    UltraBlame original commit: c4136f1d28d4c47fa67a6e2cd835a906105dcfa7

--- mobile/android/geckoview/src/main/java/org/mozilla/gecko/gfx/LayerSession.java
+++ mobile/android/geckoview/src/main/java/org/mozilla/gecko/gfx/LayerSession.java
@@ -31,6 +31,8 @@ public class LayerSession {
     protected class Compositor extends JNIObject {
         public LayerView layerView;
 
+        private volatile boolean mContentDocumentIsDisplayed;
+
         public boolean isReady() {
             return LayerSession.this.isCompositorReady();
         }
@@ -55,8 +57,7 @@ public class LayerSession {
         @Override protected native void disposeNative();
 
         @WrapForJNI(calledFrom = "any", dispatchTo = "gecko")
-        public native void attachToJava(GeckoLayerClient layerClient,
-                                        NativePanZoomController npzc);
+        public native void attachToJava(NativePanZoomController npzc);
 
         @WrapForJNI(calledFrom = "ui", dispatchTo = "gecko")
         public native void onBoundsChanged(int left, int top, int width, int height);
@@ -127,6 +128,28 @@ public class LayerSession {
         @WrapForJNI(calledFrom = "ui", dispatchTo = "current")
         public native void sendToolbarPixelsToCompositor(final int width, final int height,
                                                          final int[] pixels);
+
+        @WrapForJNI(calledFrom = "gecko")
+        private void contentDocumentChanged() {
+            mContentDocumentIsDisplayed = false;
+        }
+
+        @WrapForJNI(calledFrom = "gecko")
+        private boolean isContentDocumentDisplayed() {
+            return mContentDocumentIsDisplayed;
+        }
+
+        
+        
+        
+        
+        @WrapForJNI(calledFrom = "ui")
+        public void updateRootFrameMetrics(float scrollX, float scrollY, float zoom) {
+            mContentDocumentIsDisplayed = true;
+            if (layerView != null) {
+                layerView.onMetricsChanged(scrollX, scrollY, zoom);
+            }
+        }
     }
 
     protected final Compositor mCompositor = new Compositor();
