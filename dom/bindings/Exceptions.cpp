e8c4b07c6580ac97c5537014fcd95cc58873cc5c
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 08:34:15 +0000

    Bug 1149830 - Remove nsIStackFrame::language. r=bz,mccr8
    MozReview-Commit-ID: LxTiLFzNJ9Y
    
    UltraBlame original commit: 331cdaa5807b822903b3234a202a6613d0adf635

--- dom/bindings/Exceptions.cpp
+++ dom/bindings/Exceptions.cpp
@@ -14,7 +14,6 @@
 #include "mozilla/dom/BindingUtils.h"
 #include "mozilla/dom/DOMException.h"
 #include "mozilla/dom/ScriptSettings.h"
-#include "nsIProgrammingLanguage.h"
 #include "nsPIDOMWindow.h"
 #include "nsServiceManagerUtils.h"
 #include "nsThreadUtils.h"
@@ -317,18 +316,6 @@ NS_INTERFACE_MAP_BEGIN_CYCLE_COLLECTION(JSStackFrame)
   NS_INTERFACE_MAP_ENTRY(nsIStackFrame)
   NS_INTERFACE_MAP_ENTRY(nsISupports)
 NS_INTERFACE_MAP_END
-
-NS_IMETHODIMP JSStackFrame::GetLanguage(uint32_t* aLanguage)
-{
-  *aLanguage = nsIProgrammingLanguage::JAVASCRIPT;
-  return NS_OK;
-}
-
-NS_IMETHODIMP JSStackFrame::GetLanguageName(nsACString& aLanguageName)
-{
-  aLanguageName.AssignLiteral("JavaScript");
-  return NS_OK;
-}
 
 
 
