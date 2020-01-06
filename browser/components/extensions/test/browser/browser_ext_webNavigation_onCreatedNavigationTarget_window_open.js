14676ea9180fe58e9a826a18eda392aeed86d4f6
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 18:26:36 +0000

    Bug 1403349 - Prevent WebNavigation jsm to leak the sourceTab browser on unpaired CreatedNavigationTarget messages. r=aswan
    This applies the following changes:
    
    - store a weak reference to the browser element in the WebNavigation.jsm Manager's map
      of pending CreatedNavigationTarget messages
    
    - when a CreatedNavigationTarget message is received from a sourceTab
      for a created window that is not currently tracked in the map
      (e.g. it has been immediately closed), the message received from the sourceTab
      is not saved in the map of the pending CreatedNavigationTarget (and a message
      is logged in the console to make easier to investigate issues related to discarded
      CreatedNavigationTarget events).
    
    - adds an additional assertion to the related test case to ensure that no CreatedNavigationTarget
      message is still pending in the WebNavigation/jsm's Manager.
    
    MozReview-Commit-ID: FijQ8IqiY8L
    
    UltraBlame original commit: 6692df0c568732b5f38bc594e7e020b3ea2e0f41

--- browser/components/extensions/test/browser/browser_ext_webNavigation_onCreatedNavigationTarget_window_open.js
+++ browser/components/extensions/test/browser/browser_ext_webNavigation_onCreatedNavigationTarget_window_open.js
@@ -58,6 +58,16 @@ async function runTestCase({extension, openNavTarget, expectedWebNavProps}) {
   is(completedNavMsg.url, url, "Got the expected webNavigation.onCompleted url property");
 }
 
+
+
+
+
+function assertNoPendingCreatedNavigationTargetData() {
+  const {Manager} = Cu.import("resource://gre/modules/WebNavigation.jsm", {});
+  Assert.equal(Manager.createdNavigationTargetByOuterWindowId.size, 0,
+               "There should be no pending createdNavigationTarget messages in WebNavigation");
+}
+
 add_task(async function test_window_open() {
   const tab1 = await BrowserTestUtils.openNewForegroundTab(gBrowser, SOURCE_PAGE);
 
@@ -107,6 +117,8 @@ add_task(async function test_window_open() {
       url: `${OPENED_PAGE}#new-win-from-window-open`,
     },
   });
+
+  assertNoPendingCreatedNavigationTargetData();
 
   await BrowserTestUtils.removeTab(tab1);
 
@@ -162,6 +174,8 @@ add_task(async function test_window_open_from_subframe() {
       url: `${OPENED_PAGE}#new-win-from-window-open-subframe`,
     },
   });
+
+  assertNoPendingCreatedNavigationTargetData();
 
   await BrowserTestUtils.removeTab(tab1);
 
@@ -226,6 +240,8 @@ add_task(async function test_window_open_close_from_browserAction_popup() {
       url: `${OPENED_PAGE}#new-tab-from-window-open`,
     },
   });
+
+  assertNoPendingCreatedNavigationTargetData();
 
   await BrowserTestUtils.removeTab(tab1);
 
