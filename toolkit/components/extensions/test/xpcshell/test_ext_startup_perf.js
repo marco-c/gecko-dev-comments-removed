0738f37f2784ced972305a867150b7ec2667aa99
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 11:57:37 +0000

    Bug 1393621: Part 3 - Add test for framework JSMs loaded at startup. r=zombie
    MozReview-Commit-ID: DvVO9bzwyXf
    
    UltraBlame original commit: fab7bacb59284801d9c47f6ef5d0529f5ae20154

--- toolkit/components/extensions/test/xpcshell/test_ext_startup_perf.js
+++ toolkit/components/extensions/test/xpcshell/test_ext_startup_perf.js
@@ -6,9 +6,26 @@ const STARTUP_APIS = [
   "backgroundPage",
 ];
 
+const STARTUP_MODULES = [
+  "resource://gre/modules/Extension.jsm",
+  "resource://gre/modules/ExtensionCommon.jsm",
+  "resource://gre/modules/ExtensionParent.jsm",
+  
+  
+  
+  "resource://gre/modules/ExtensionPermissions.jsm",
+  "resource://gre/modules/ExtensionUtils.jsm",
+];
+
+if (!Services.prefs.getBoolPref("extensions.webextensions.remote")) {
+  STARTUP_MODULES.push(
+    "resource://gre/modules/ExtensionChild.jsm",
+    "resource://gre/modules/ExtensionPageChild.jsm");
+}
 
 
-add_task(async function test_loaded_api_scripts() {
+
+add_task(async function test_loaded_scripts() {
   await ExtensionTestUtils.startAddonManager();
 
   let extension = ExtensionTestUtils.loadExtension({
@@ -27,6 +44,14 @@ add_task(async function test_loaded_api_scripts() {
 
   deepEqual(loadedAPIs.sort(), STARTUP_APIS,
             "No extra APIs should be loaded at startup for a simple extension");
+
+
+  const loader = Cc["@mozilla.org/moz/jsloader;1"].getService(Ci.xpcIJSModuleLoader);
+  let loadedModules = loader.loadedModules()
+                            .filter(url => url.startsWith("resource://gre/modules/Extension"));
+
+  deepEqual(loadedModules.sort(), STARTUP_MODULES.sort(),
+            "No extra extension modules should be loaded at startup for a simple extension");
 
   await extension.unload();
 });
