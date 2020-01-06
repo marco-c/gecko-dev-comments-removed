3381768596cce6baa4c798a478ea7d1a57579d52
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 09:08:15 +0000

    Bug 1388715 - browser_contextmenu_touch.js needs apz.test.fails_with_native_injection=true to pass on windows 10 in taskcluster. r=johann
    MozReview-Commit-ID: 83FqO7FFS0m
    
    UltraBlame original commit: 6395741da4657cbe8c65bb77a29a25caa63d3711

--- browser/base/content/test/contextMenu/browser_contextmenu_touch.js
+++ browser/base/content/test/contextMenu/browser_contextmenu_touch.js
@@ -28,6 +28,16 @@ async function openAndCheckContextMenu(contextMenu, target) {
 }
 
 
+add_task(async function setup() {
+  let isWindows = AppConstants.isPlatformAndVersionAtLeast("win", "10.0");
+  await SpecialPowers.pushPrefEnv({
+    set: [
+      ["apz.test.fails_with_native_injection", isWindows]
+    ]
+  });
+});
+
+
 add_task(async function test_contentarea_contextmenu_touch() {
   await BrowserTestUtils.withNewTab("about:blank", async function(browser) {
     let contextMenu = document.getElementById("contentAreaContextMenu");
