19b9f5a696b0e297ab9e5f9f10da45190e0f7290
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 07:35:07 +0000

    Bug 1365900 - Create ServoFontFeatureValuesRule and bind servo data r=xidorn
    Add ServoFontFeatureValuesRule for CSSOM representation of
    font-feature-values rule and wire up with bindings.
    
    MozReview-Commit-ID: B4Lpj43bvKX
    
    UltraBlame original commit: 63810d7a059ce4bd462db414343f4fe04f7ac4f7

--- layout/style/ServoCSSRuleList.cpp
+++ layout/style/ServoCSSRuleList.cpp
@@ -12,6 +12,7 @@
 #include "mozilla/ServoBindings.h"
 #include "mozilla/ServoDocumentRule.h"
 #include "mozilla/ServoImportRule.h"
+#include "mozilla/ServoFontFeatureValuesRule.h"
 #include "mozilla/ServoKeyframesRule.h"
 #include "mozilla/ServoMediaRule.h"
 #include "mozilla/ServoNamespaceRule.h"
@@ -108,6 +109,7 @@ ServoCSSRuleList::GetRule(uint32_t aIndex)
       CASE_RULE(SUPPORTS, Supports)
       CASE_RULE(DOCUMENT, Document)
       CASE_RULE(IMPORT, Import)
+      CASE_RULE(FONT_FEATURE_VALUES, FontFeatureValues)
 #undef CASE_RULE
       
       
