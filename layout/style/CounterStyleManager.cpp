b2f0ac2f5affcac243b3c0d4f5d1937664f164af
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 11:41:27 +0000

    Bug 1393189 part 2 - Have CounterStyle::GetStyleName return nsIAtom and make it const. r=dholbert
    MozReview-Commit-ID: KmnQSK7ZvND
    
    UltraBlame original commit: 1200482baf04a5d9bea563be0c2ce4e7a4e47a50

--- layout/style/CounterStyleManager.cpp
+++ layout/style/CounterStyleManager.cpp
@@ -581,7 +581,7 @@ public:
   {
   }
 
-  virtual void GetStyleName(nsAString& aResult) override;
+  virtual nsIAtom* GetStyleName() const override;
   virtual void GetPrefix(nsAString& aResult) override;
   virtual void GetSuffix(nsAString& aResult) override;
   virtual void GetSpokenCounterText(CounterValue aOrdinal,
@@ -618,14 +618,10 @@ private:
   nsIAtom** const mName;
 };
 
- void
-BuiltinCounterStyle::GetStyleName(nsAString& aResult)
+ nsIAtom*
+BuiltinCounterStyle::GetStyleName() const
 {
-  MOZ_ASSERT(mStyle != NS_STYLE_LIST_STYLE_CUSTOM);
-  const nsCString& str =
-    nsCSSProps::ValueToKeyword(mStyle, nsCSSProps::kListStyleKTable);
-  MOZ_ASSERT(!str.IsEmpty());
-  aResult.Assign(NS_ConvertUTF8toUTF16(str));
+  return *mName;
 }
 
  void
@@ -1066,7 +1062,7 @@ public:
   nsCSSCounterStyleRule* GetRule() const { return mRule; }
   uint32_t GetRuleGeneration() const { return mRuleGeneration; }
 
-  virtual void GetStyleName(nsAString& aResult) override;
+  virtual nsIAtom* GetStyleName() const override;
   virtual void GetPrefix(nsAString& aResult) override;
   virtual void GetSuffix(nsAString& aResult) override;
   virtual void GetSpokenCounterText(CounterValue aOrdinal,
@@ -1223,11 +1219,10 @@ CustomCounterStyle::ResetDependentData()
   }
 }
 
- void
-CustomCounterStyle::GetStyleName(nsAString& aResult)
+ nsIAtom*
+CustomCounterStyle::GetStyleName() const
 {
-  nsDependentAtomString name(mName);
-  aResult.Assign(name);
+  return mName;
 }
 
  void
@@ -1747,10 +1742,10 @@ AnonymousCounterStyle::AnonymousCounterStyle(uint8_t aSystem,
 {
 }
 
- void
-AnonymousCounterStyle::GetStyleName(nsAString& aResult)
+ nsIAtom*
+AnonymousCounterStyle::GetStyleName() const
 {
-  aResult.Truncate();
+  return nullptr;
 }
 
  void
