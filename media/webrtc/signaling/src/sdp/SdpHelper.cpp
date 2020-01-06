4e3c5d290bdb46aa1f81318d0b3e5860dd5055f5
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 10:53:34 +0000

    Bug 1392745: keep MID when disabling m-sections r=mjf
    MozReview-Commit-ID: 3CDA7lF70kr
    
    UltraBlame original commit: 5ce80881b78cf5a5355147e8eb3d6b8377939443

--- media/webrtc/signaling/src/sdp/SdpHelper.cpp
+++ media/webrtc/signaling/src/sdp/SdpHelper.cpp
@@ -138,9 +138,11 @@ SdpHelper::MsectionIsDisabled(const SdpMediaSection& msection) const
 void
 SdpHelper::DisableMsection(Sdp* sdp, SdpMediaSection* msection)
 {
+  std::string mid;
+
   
   if (msection->GetAttributeList().HasAttribute(SdpAttribute::kMidAttribute)) {
-    std::string mid = msection->GetAttributeList().GetMid();
+    mid = msection->GetAttributeList().GetMid();
     if (sdp->GetAttributeList().HasAttribute(SdpAttribute::kGroupAttribute)) {
       UniquePtr<SdpGroupAttributeList> newGroupAttr(new SdpGroupAttributeList(
             sdp->GetAttributeList().GetGroup()));
@@ -156,6 +158,12 @@ SdpHelper::DisableMsection(Sdp* sdp, SdpMediaSection* msection)
     new SdpDirectionAttribute(SdpDirectionAttribute::kInactive);
   msection->GetAttributeList().SetAttribute(direction);
   msection->SetPort(0);
+
+  
+  if (!mid.empty()) {
+    msection->GetAttributeList().SetAttribute(new SdpStringAttribute(
+          SdpAttribute::kMidAttribute, mid));
+  }
 
   msection->ClearCodecs();
 
