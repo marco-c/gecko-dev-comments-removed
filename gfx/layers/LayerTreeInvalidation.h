a22b469c3284056c611bae747716a034802af9b0
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 08:31:47 +0000

    Change the subdoc invalidation callback to handle overflow. (bug 1345891 part 5, r=mattwoodrow)
    UltraBlame original commit: f886c403b88a78035ae0e2f7fe71c830e40bcb3f

--- gfx/layers/LayerTreeInvalidation.h
+++ gfx/layers/LayerTreeInvalidation.h
@@ -24,7 +24,7 @@ class ContainerLayer;
 
 
 typedef void (*NotifySubDocInvalidationFunc)(ContainerLayer* aLayer,
-                                             const nsIntRegion& aRegion);
+                                             const nsIntRegion* aRegion);
 
 
 
