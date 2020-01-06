523634956ba546242eab14fceab6fa81ad895022
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 14:03:10 +0000

    Bug 1399540 - Add some IDN testcases with mixed punycode and non-punycode labels. r=valentin
    UltraBlame original commit: 8e697bb0803149993dadd2b3742b15c4ab6c83f7

--- netwerk/test/unit/test_idn_urls.js
+++ netwerk/test/unit/test_idn_urls.js
@@ -286,9 +286,16 @@ const testcases = [
     
     ["เครื่องทําน้ําทําน้ําแข็ง",
                  "xn--22cdjb2fanb9fyepcbbb9dwh4a3igze4fdcd",
-                                                     false, true, true]
-];
+                                                     false, true, true],
 
+    
+    ["䕮䕵䕶䕱.ascii", "xn--google.ascii",                       false, true,  true],
+    ["ascii.䕮䕵䕶䕱", "ascii.xn--google",                       false, true,  true],
+    ["中国123.䕮䕵䕶䕱", "xn--123-u68dy61b.xn--google",           false, true,  true],
+    ["䕮䕵䕶䕱.中国123", "xn--google.xn--123-u68dy61b",           false, true,  true],
+    ["xn--accountlogin.䕮䕵䕶䕱", "xn--accountlogin.xn--google", false, true,  true],
+    ["䕮䕵䕶䕱.xn--accountlogin", "xn--google.xn--accountlogin", false, true,  true],
+];
 
 const profiles = ["ASCII", "high", "moderate"];
 
@@ -311,13 +318,13 @@ function run_test() {
             var expectedUnicode = test[2 + i];
             var isASCII = {};
 
-	    var result;
-	    try {
-		result = idnService.convertToDisplayIDN(URL, isASCII);
-	    } catch(e) {
-		result = ".com";
-	    }
-            if (punycodeURL.substr(0, 4) == "xn--") {
+            var result;
+            try {
+                result = idnService.convertToDisplayIDN(URL, isASCII);
+            } catch(e) {
+                result = ".com";
+            }
+            if (punycodeURL.substr(0, 4) == "xn--" || punycodeURL.indexOf(".xn--") > 0) {
                 
                 
                 do_check_eq(escape(result),
