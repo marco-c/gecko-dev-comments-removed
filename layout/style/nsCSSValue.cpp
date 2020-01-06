9b25b2e0a29f511074cdc6abe9055c7e069e6c2e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 13:53:28 +0000

    Bug 1399758 - Measure ImageValue objects. r=heycam.
    We have about 11,500 of these when loading gmail in a Stylo-enabled build, from
    SpecifiedUrls; the objects themselves account for about 1.3 MiB of memory, and the
    strings within them about 2.9 MiB.
    
    We also have a very small number of them on the Gecko side.
    
    MozReview-Commit-ID: AduCIaDIzGG
    
    UltraBlame original commit: 1536fa69bad4d6ee7f1f4c265fab57504f6263d9

--- layout/style/nsCSSValue.cpp
+++ layout/style/nsCSSValue.cpp
@@ -2150,8 +2150,7 @@ nsCSSValue::SizeOfExcludingThis(mozilla::MallocSizeOf aMallocSizeOf) const
 
     
     case eCSSUnit_Image:
-      
-      
+      n += mValue.mImage->SizeOfIncludingThis(aMallocSizeOf);
       break;
 
     
@@ -3074,6 +3073,15 @@ css::ImageValue::~ImageValue()
 
     iter.Remove();
   }
+}
+
+size_t
+css::ImageValue::SizeOfIncludingThis(MallocSizeOf aMallocSizeOf) const
+{
+  size_t n = aMallocSizeOf(this);
+  n += css::URLValueData::SizeOfExcludingThis(aMallocSizeOf);
+  n += mRequests.ShallowSizeOfExcludingThis(aMallocSizeOf);
+  return n;
 }
 
 size_t
--- layout/style/nsCSSValue.h
+++ layout/style/nsCSSValue.h
@@ -219,7 +219,7 @@ struct ImageValue final : public URLValueData
 
   void Initialize(nsIDocument* aDocument);
 
-  
+  size_t SizeOfIncludingThis(mozilla::MallocSizeOf aMallocSizeOf) const;
 
 protected:
   ~ImageValue();
