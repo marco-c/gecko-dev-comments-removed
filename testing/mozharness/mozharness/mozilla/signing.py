389943ee4ffb73edd5383a374b1c238a2232e207
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 03:09:00 +0000

    Bug 1372482 - linux 55.0 repack betas are broken in comparing locales because due to missing python executable.r=aki a=testing
    MozReview-Commit-ID: KGSmRRFxbiu
    
    UltraBlame original commit: dda52d6a97060aaf16e1fc9e1bc24036ff7db3d4

--- testing/mozharness/mozharness/mozilla/signing.py
+++ testing/mozharness/mozharness/mozilla/signing.py
@@ -46,8 +46,13 @@ class SigningMixin(BaseSigningMixin):
         token = os.path.join(dirs['base_work_dir'], 'token')
         nonce = os.path.join(dirs['base_work_dir'], 'nonce')
         host_cert = os.path.join(signing_dir, 'host.cert')
+        python = sys.executable
+        
+        
+        if 'mock_target' in self.config:
+            python = 'python2.7'
         cmd = [
-            sys.executable,
+            python,
             os.path.join(signing_dir, 'signtool.py'),
             '--cachedir', cache_dir,
             '-t', token,
