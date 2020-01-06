874574ecbc7b2c94fb972868f3826a686d517cb8
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 02:37:10 +0000

    Bug 1370732 - Define Cc and Ci in sanitize.js. r=Gijs
    These are referred to, but never explicitly defined. Under some
    circumstances with shared JSM globals, this will cause an error.
    
    MozReview-Commit-ID: 5DP2NSRoNML
    
    UltraBlame original commit: 4ae5d273a14975c2b5ddc609eb1abdf2bed5ff99

--- browser/base/content/sanitize.js
+++ browser/base/content/sanitize.js
@@ -28,6 +28,8 @@ XPCOMUtils.defineLazyModuleGetter(this, "Preferences",
 XPCOMUtils.defineLazyModuleGetter(this, "setTimeout",
                                   "resource://gre/modules/Timer.jsm");
 
+var {Cc: classes, Ci: interfaces} = Components;
+
 
 
 
