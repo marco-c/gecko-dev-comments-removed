158adea1d9c760415f8f9ca3a5b892f7a938685d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 04:50:02 +0000

    Bug 1375446 - Check the return code of Sprinter::put in Disassemble1. r=nbp
    UltraBlame original commit: 650c65dc3e87374d2fef22bc1edb5124567a69a2

---
+++ js/src/jit-test/tests/auto-regress/bug1375446.js
@@ -0,0 +1,13 @@
+
+
+if (!('oomTest' in this))
+    quit();
+
+loadFile(`
+    disassemble(function() {
+	return assertDeepEq(x.concat(obj), [1, 2, 3, "hey"]);
+    })
+`);
+function loadFile(lfVarx) {
+    oomTest(new Function(lfVarx));
+}
