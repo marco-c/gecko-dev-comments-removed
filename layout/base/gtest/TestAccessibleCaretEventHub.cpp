1274f843f965d3960ca8f9719a6f8c8a8fc58ec3
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 21:30:46 +0000

    Bug 1417796 - Remove 'virtual' qualifiers for 'override' methods. r=mtseng
    Fix warning: 'virtual' is redundant since the function is already declared
    'override'
    
    MozReview-Commit-ID: Kps9ZZoFniI
    
    UltraBlame original commit: d5306abe90a0cd61d03a3edb45c9d5039d82c5f8

--- layout/base/gtest/TestAccessibleCaretEventHub.cpp
+++ layout/base/gtest/TestAccessibleCaretEventHub.cpp
@@ -71,15 +71,15 @@ public:
     mInitialized = true;
   }
 
-  virtual nsPoint GetTouchEventPosition(WidgetTouchEvent* aEvent,
-                                        int32_t aIdentifier) const override
+  nsPoint GetTouchEventPosition(WidgetTouchEvent* aEvent,
+                                int32_t aIdentifier) const override
   {
     
     LayoutDeviceIntPoint touchIntPoint = aEvent->mTouches[0]->mRefPoint;
     return nsPoint(touchIntPoint.x, touchIntPoint.y);
   }
 
-  virtual nsPoint GetMouseEventPosition(WidgetMouseEvent* aEvent) const override
+  nsPoint GetMouseEventPosition(WidgetMouseEvent* aEvent) const override
   {
     
     LayoutDeviceIntPoint mouseIntPoint = aEvent->AsGUIEvent()->mRefPoint;
