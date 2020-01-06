195c3f4aae679e28bfeea0a91d27207c32779fc0
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 20:53:36 +0000

    Bug 1409148 - Add test to ensure that the plugin overlay is displayed in the correct style. r=johannh
    MozReview-Commit-ID: Hn9HtnJvbyC
    
    UltraBlame original commit: 1c1fd21f52805c7699c22acc528eb797083c6dfb

--- browser/base/content/test/plugins/browser_CTP_shouldShowOverlay.js
+++ browser/base/content/test/plugins/browser_CTP_shouldShowOverlay.js
@@ -9,6 +9,12 @@
 
 
 
+
+
+
+
+
+
 var rootDir = getRootDirectory(gTestPath);
 const gTestRoot = rootDir.replace("chrome://mochitests/content/", "http://127.0.0.1:8888/");
 
@@ -49,7 +55,7 @@ add_task(async function() {
       Assert.ok(overlay, `overlay exists in ${testcase.id}`);
 
       let expectedVisibility = (testcase.getAttribute("shouldshow") == "true");
-      Assert.ok(overlay.classList.contains("visible") == expectedVisibility,
+      Assert.ok((overlay.getAttribute("sizing") != "blank") == expectedVisibility,
                 `The expected visibility is correct in ${testcase.id}`);
     }
   })
