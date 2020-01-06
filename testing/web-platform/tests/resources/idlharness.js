486fbacbc998c5706fb314fe2dc725be999cd1b0
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 14:53:43 +0000

    Bug 1402002 - Update web-platform-tests to revision b88862e6e574c2116f8efea0e982f6776a3c4b22, a=testonly
    MozReview-Commit-ID: FlhL7aMvsiY
    
    UltraBlame original commit: 3a93e822c501a73ca645c0361332560056fa04de

--- testing/web-platform/tests/resources/idlharness.js
+++ testing/web-platform/tests/resources/idlharness.js
@@ -332,7 +332,7 @@ IdlArray.prototype.is_json_type = function(type)
 
 
     var idlType = type.idlType;
-    
+
     if (type.generic == "Promise") { return false; }
 
     
@@ -392,8 +392,11 @@ IdlArray.prototype.is_json_type = function(type)
        default:
            var thing = this.members[idlType];
            if (!thing) { throw new Error("Type " + idlType + " not found"); }
-
            if (thing instanceof IdlEnum) { return true; }
+
+           if (thing instanceof IdlTypedef) {
+               return this.is_json_type(thing.idlType);
+           }
 
            
            if (thing instanceof IdlDictionary) {
@@ -407,7 +410,7 @@ IdlArray.prototype.is_json_type = function(type)
                }
                return Array.from(map.values()).every(this.is_json_type, this);
            }
-           
+
            
            
            if (thing instanceof IdlInterface) {
@@ -1788,10 +1791,6 @@ IdlInterface.prototype.do_member_operation_asserts = function(memberHolderObject
         })),
         "property has wrong .length");
 
-    if (member.is_to_json_regular_operation()) {
-        this.test_to_json_operation(memberHolderObject, member);
-    }
-
     
     var args = member.arguments.map(function(arg) {
         return create_suitable_object(arg.idlType);
@@ -1854,7 +1853,7 @@ IdlInterface.prototype.test_to_json_operation = function(memberHolderObject, mem
         test(function() {
             var json = memberHolderObject.toJSON();
             map.forEach(function(type, k) {
-                assert_true(k in json, "property " + k + " should be present in the output of " + this.name + ".prototype.toJSON()");
+                assert_true(k in json, "property " + JSON.stringify(k) + " should be present in the output of " + this.name + ".prototype.toJSON()");
                 var descriptor = Object.getOwnPropertyDescriptor(json, k);
                 assert_true(descriptor.writable, "property " + k + " should be writable");
                 assert_true(descriptor.configurable, "property " + k + " should be configurable");
@@ -1863,7 +1862,7 @@ IdlInterface.prototype.test_to_json_operation = function(memberHolderObject, mem
                 delete json[k];
             }, this);
             for (var k in json) {
-                assert_unreached("property " + k + " should not be present in the output of " + this.name + ".prototype.toJSON()");
+                assert_unreached("property " + JSON.stringify(k) + " should not be present in the output of " + this.name + ".prototype.toJSON()");
             }
         }.bind(this), "Test default toJSON operation of " + this.name);
     } else {
@@ -2065,6 +2064,7 @@ IdlInterface.prototype.test_object = function(desc)
         : "object";
 
     this.test_primary_interface_of(desc, obj, exception, expected_typeof);
+
     var current_interface = this;
     while (current_interface)
     {
@@ -2280,6 +2280,10 @@ IdlInterface.prototype.test_interface_of = function(desc, obj, exception, expect
                     cb();
                 }
             }.bind(this));
+        }
+
+        if (member.is_to_json_regular_operation()) {
+            this.test_to_json_operation(obj, member);
         }
     }
 };
