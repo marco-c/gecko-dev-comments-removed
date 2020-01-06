756c9d3ee08a4d6074b6f36151c1fc44c01918e5
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 14:01:59 +0000

    servo: Merge #18532 - Undo #18497 in the MatchAndCascade case (from bholley:more_cache_insertion); r=emilio
    It's easy to construct examples where not inserting in those cases causes performance
    to get worse (for example, any long list of siblings that match the same selectors
    while having some non-effectual differences in LocalName/Class/Id/etc). And the LRU
    nature of the cache already does the right thing of pruning non-useful entries.
    
    Fixing this causes several hundred more sharing hits on wikipedia.
    
    Source-Repo: https://github.com/servo/servo
    Source-Revision: da64abc4298549362debc96e5843bd36e6d23b7f
    
    UltraBlame original commit: b1e13cff22b9fa9ed414cf60f44b850e95603e1f

--- servo/components/style/traversal.rs
+++ servo/components/style/traversal.rs
@@ -723,12 +723,29 @@ where
                 resolver.cascade_styles_with_default_parents(cascade_inputs)
             };
 
-            context.thread_local.sharing_cache.insert_if_possible(
-                &element,
-                &new_styles.primary,
-                None,
-                traversal_data.current_dom_depth,
-            );
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            if !new_styles.primary.0.reused_via_rule_node {
+                context.thread_local.sharing_cache.insert_if_possible(
+                    &element,
+                    &new_styles.primary,
+                    None,
+                    traversal_data.current_dom_depth,
+                );
+            }
 
             new_styles
         }
