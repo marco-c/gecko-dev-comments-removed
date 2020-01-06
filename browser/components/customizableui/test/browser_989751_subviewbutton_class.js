e951cd068f1fcde68098d4d8a3e297788ec3e480
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 08:20:04 +0000

    Bug 1387023 - initialize devtools before asserting menu items in browser_989751_subviewbutton_class.js;r=Gijs
    MozReview-Commit-ID: 9piucVkS0Y9
    
    UltraBlame original commit: f66e161dd72f83f0bc87fbfe00415379deee5352

--- browser/components/customizableui/test/browser_989751_subviewbutton_class.js
+++ browser/components/customizableui/test/browser_989751_subviewbutton_class.js
@@ -15,6 +15,11 @@ function insertClassNameToMenuChildren(parentMenu) {
 
 function checkSubviewButtonClass(menuId, buttonId, subviewId) {
   return async function() {
+    
+    
+    Cu.import("resource://devtools/shared/Loader.jsm", {})
+        .require("devtools/client/framework/devtools-browser");
+
     info("Checking for items without the subviewbutton class in " + buttonId + " widget");
     let menu = document.getElementById(menuId);
     insertClassNameToMenuChildren(menu);
