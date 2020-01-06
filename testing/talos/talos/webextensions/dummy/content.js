f9d9f4e417d7bb7651d3b071900d6d265789c8ba
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 13:36:05 +0000

    Bug 1398974: Part 6 - Add content script messaging to webext talos add-on. r=mixedpuppy,jmaher
    MozReview-Commit-ID: Edn7BnZoHvK
    
    UltraBlame original commit: 59069fc4ad5150fbb002628a618e4fdb009f7b40

--- testing/talos/talos/webextensions/dummy/content.js
+++ testing/talos/talos/webextensions/dummy/content.js
@@ -1,3 +1,12 @@
 "use strict";
 
 
+
+browser.runtime.sendMessage({
+  msg: "Hello from content script",
+  url: location.href,
+});
+
+browser.runtime.onMessage.addListener(msg => {
+  return Promise.resolve({code: "10-4", msg});
+});
