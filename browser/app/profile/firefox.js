102c15e5016a8532fc02267b5fca89e02e2e0c03
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 02:32:26 +0000

    Bug 1368872 - Enable FormAutofill Heuristics by default.; r=MattN
    MozReview-Commit-ID: DwMQfov15Eh
    
    UltraBlame original commit: 60afe5db03953126ca3f6a40e7a3aab81e425556

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1663,7 +1663,7 @@ pref("extensions.formautofill.experimental", true);
 pref("extensions.formautofill.experimental", false);
 #endif
 pref("extensions.formautofill.addresses.enabled", true);
-pref("extensions.formautofill.heuristics.enabled", false);
+pref("extensions.formautofill.heuristics.enabled", true);
 pref("extensions.formautofill.loglevel", "Warn");
 
 
