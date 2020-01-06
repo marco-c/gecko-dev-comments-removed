0a0115a958bfea3aee924e298fdb3072aa2890bb
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 08:21:43 +0000

    Bug 1383296 Part 1: Remove all uses of the nsCSSValue::Serialization enum, now that it no longer has multiple values. r=heycam
    MozReview-Commit-ID: n6px5hNHtB
    
    UltraBlame original commit: acf2dcc2c76a1b95f3fb36532a0ae245d02a31b5

--- layout/style/Declaration.h
+++ layout/style/Declaration.h
@@ -310,15 +310,12 @@ private:
   bool operator==(const Declaration& aCopy) const = delete;
 
   void GetPropertyValueInternal(nsCSSPropertyID aProperty, nsAString& aValue,
-                                nsCSSValue::Serialization aValueSerialization,
                                 bool* aIsTokenStream = nullptr) const;
   bool GetPropertyIsImportantByID(nsCSSPropertyID aProperty) const;
 
   static void AppendImportanceToString(bool aIsImportant, nsAString& aString);
   
-  bool AppendValueToString(nsCSSPropertyID aProperty, nsAString& aResult) const;
   bool AppendValueToString(nsCSSPropertyID aProperty, nsAString& aResult,
-                           nsCSSValue::Serialization aValueSerialization,
                            bool* aIsTokenStream = nullptr) const;
   
   void AppendPropertyAndValueToString(nsCSSPropertyID aProperty,
@@ -332,12 +329,10 @@ private:
 
   void GetImageLayerValue(nsCSSCompressedDataBlock *data,
                           nsAString& aValue,
-                          nsCSSValue::Serialization aSerialization,
                           const nsCSSPropertyID aTable[]) const;
 
   void GetImageLayerPositionValue(nsCSSCompressedDataBlock *data,
                                   nsAString& aValue,
-                                  nsCSSValue::Serialization aSerialization,
                                   const nsCSSPropertyID aTable[]) const;
 
 public:
