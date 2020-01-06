f41f18707bb44a85e4ab5e320c54fd0648e904fc
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 15:26:25 +0000

    Bug 1403615: Also follow the NODE_DESCENDANTS_NEED_FRAMES bit in ClearRestyleStateFromSubtree. r=bholley
    We don't follow this bit intentionally because we know that even if it's set,
    when none of the other two bits are set there are no other restyle / change
    hints down the tree.
    
    We rely on the frame constructor to clean the mess up, though, and it doesn't
    really do a good work about it.
    
    In particular, the case we're hitting on the test-case is:
    
    <body descendant-need-frames change=reconstruct style="display: table-column-group">
      <div descendant-need-frames>
        <div descendant-need-frames>
          <span needs-frame></span>
        </div>
      </div>
    </body>
    
    When we see we need to reconstruct the body, we call
    ClearRestyleStateFromSubtree, but that doesn't do much now, since we don't
    follow the descendant-need-frames bits.
    
    Then, when we reconstruct the content, we arrive at[1] when constructing the
    first child <div>. The <div> flags have been cleared, but not the children's!
    
    Then a text-node is inserted in a <div>, breaking all sorts of invariants.
    
    This is the easiest fix. Other fixes include clearing the flags on
    SetAsUndisplayedContent. But that implies not clearing them in
    ShouldCreateItemsForChild, and doing that somewhere more sensible.
    
    I suspect it's not too hard, but that's a slightly more risky change, will do it
    if you prefer it.
    
    [1]: http://searchfox.org/mozilla-central/rev/3dbb47302e114219c53e99ebaf50c5cb727358ab/layout/base/nsCSSFrameConstructor.cpp#6092
    
    MozReview-Commit-ID: 7026wkQLQkz
    
    UltraBlame original commit: 8d4f4b54141ac107d52688bf2b4392e860ab554d

--- layout/base/ServoRestyleManager.cpp
+++ layout/base/ServoRestyleManager.cpp
@@ -415,8 +415,7 @@ ServoRestyleManager::ClearServoDataFromSubtree(Element* aElement)
  void
 ServoRestyleManager::ClearRestyleStateFromSubtree(Element* aElement)
 {
-  if (aElement->HasDirtyDescendantsForServo() ||
-      aElement->HasAnimationOnlyDirtyDescendantsForServo()) {
+  if (aElement->HasAnyOfFlags(Element::kAllServoDescendantBits)) {
     StyleChildrenIterator it(aElement);
     for (nsIContent* n = it.GetNextChild(); n; n = it.GetNextChild()) {
       if (n->IsElement()) {
