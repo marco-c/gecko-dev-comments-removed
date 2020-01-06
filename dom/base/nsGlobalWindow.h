7a47f313659b0d305e1d8a8156b0b83d32166126
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 16:36:04 +0000

    Bug 1405353 - stop using nsAutoPtr in nsGlobalWindow; r=mystor
    UniquePtr is better.
    
    UltraBlame original commit: 4bb8b4c980373d19f99ecdeedca70e4c09d1156d

--- dom/base/nsGlobalWindow.h
+++ dom/base/nsGlobalWindow.h
@@ -1982,7 +1982,7 @@ protected:
   nsCOMPtr<nsIDOMOfflineResourceList> mApplicationCache;
 
   using XBLPrototypeHandlerTable = nsJSThingHashtable<nsPtrHashKey<nsXBLPrototypeHandler>, JSObject*>;
-  nsAutoPtr<XBLPrototypeHandlerTable> mCachedXBLPrototypeHandlers;
+  mozilla::UniquePtr<XBLPrototypeHandlerTable> mCachedXBLPrototypeHandlers;
 
   
   
@@ -2015,7 +2015,7 @@ protected:
   nsTArray<uint32_t> mEnabledSensors;
 
 #if defined(MOZ_WIDGET_ANDROID)
-  nsAutoPtr<mozilla::dom::WindowOrientationObserver> mOrientationChangeObserver;
+  mozilla::UniquePtr<mozilla::dom::WindowOrientationObserver> mOrientationChangeObserver;
 #endif
 
 #ifdef MOZ_WEBSPEECH
