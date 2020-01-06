36ee36d04e3b0917efd17f1c51f2cf0c58be09cc
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 19:43:01 +0000

    Bug 1410652 - Let WebDriver:SwitchToFrame take a web element. r=whimboo
    The "element" field on the WebDriver:SwitchToFrame command request's
    body takes a string web element reference UUID as input.  This patch
    changes it so that it can also take a web element JSON Object.
    
    The old behaviour can be removed with Firefox 60.
    
    MozReview-Commit-ID: JcTD3MRxjOe
    
    UltraBlame original commit: 89c77901b957b1149d013951317c957c1c58bb4c

--- testing/marionette/driver.js
+++ testing/marionette/driver.js
@@ -1730,7 +1730,16 @@ GeckoDriver.prototype.switchToFrame = async function(cmd) {
   assert.window(this.getCurrentWindow());
   assert.noUserPrompt(this.dialog);
 
-  let {id, element, focus} = cmd.parameters;
+  let {id, focus} = cmd.parameters;
+
+  
+  
+  let byFrame;
+  if (typeof cmd.parameters.element == "string") {
+    byFrame = WebElement.fromUUID(cmd.parameters.element, Context.Chrome);
+  } else if (cmd.parameters.element) {
+    byFrame = WebElement.fromJSON(cmd.parameters.element);
+  }
 
   const otherErrorsExpr = /about:.+(error)|(blocked)\?/;
   const checkTimer = Cc["@mozilla.org/timer;1"].createInstance(Ci.nsITimer);
@@ -1758,7 +1767,7 @@ GeckoDriver.prototype.switchToFrame = async function(cmd) {
     let foundFrame = null;
 
     
-    if (typeof id == "undefined" && typeof element == "undefined") {
+    if (typeof id == "undefined" && !byFrame) {
       this.curFrame = null;
       if (focus) {
         this.mainFrame.focus();
@@ -1769,9 +1778,8 @@ GeckoDriver.prototype.switchToFrame = async function(cmd) {
     }
 
     
-    if (typeof element != "undefined") {
-      let webEl = WebElement.fromUUID(element, Context.Chrome);
-      let wantedFrame = this.curBrowser.seenEls.get(webEl);
+    if (byFrame) {
+      let wantedFrame = this.curBrowser.seenEls.get(byFrame);
 
       
       if (wantedFrame.tagName == "xul:browser" ||
@@ -1874,7 +1882,7 @@ GeckoDriver.prototype.switchToFrame = async function(cmd) {
     }
 
   } else if (this.context == Context.Content) {
-    if (!id && !element &&
+    if (!id && !byFrame &&
         this.curBrowser.frameManager.currentRemoteFrame !== null) {
       
       
