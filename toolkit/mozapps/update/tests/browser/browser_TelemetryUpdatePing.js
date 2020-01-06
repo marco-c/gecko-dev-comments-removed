f20fa78978ae97999f3d723bf77e646b070da107
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 18:09:35 +0000

    Bug 1405614 - Add 'displayVersion' to the update ping with reason 'ready'. r=chutten,mhowell
    This is needed in addition to "version" as, for some channel, it will report
    the release suffix.
    
    MozReview-Commit-ID: BT2Vxm3Q2nK
    
    UltraBlame original commit: dab164aaabfd1cb7651784d8825dfa776ae07a42

--- toolkit/mozapps/update/tests/browser/browser_TelemetryUpdatePing.js
+++ toolkit/mozapps/update/tests/browser/browser_TelemetryUpdatePing.js
@@ -58,7 +58,7 @@ add_task(async function testUpdatePingReady() {
 
   
   
-  for (let f of ["targetVersion", "targetChannel"]) {
+  for (let f of ["targetVersion", "targetChannel", "targetDisplayVersion"]) {
     ok(f in updatePing.payload,
        `${f} must be available in the update ping payload.`);
     ok(typeof(updatePing.payload[f]) == "string",
