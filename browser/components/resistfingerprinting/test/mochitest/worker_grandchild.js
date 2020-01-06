c13b396186e6aaeef45d25f15c2b01aec9472db9
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 02:45:58 +0000

    Bug 1217238 - Regression tests for reducing precision of time exposed by Javascript. r=mystor
    This patch is adapted from Tor bug 1517.
    
    Test that the following javascript codes will return timeStamps that are rounded
    to 100ms.
    
    performance.now()
    new Date().getTime()
    new Event("").timeStamp
    new File([], "").lastModified
    new File([], "").lastModifiedDate.getTime()
    audioContext.currentTime * 1000
    canvasStream.currentTime * 1000
    video.currentTime * 1000
    audio.currentTime * 1000
    
    The first five codes are also tested in simple workers and nested workers,
    created before and after the pref is on, to ensure that the pref has correctly
    propagated.
    
    MozReview-Commit-ID: CuoxmGRrBnm
    
    UltraBlame original commit: e967714a341a26b64ce118edb3172faa35fbd4b3

---
+++ browser/components/resistfingerprinting/test/mochitest/worker_grandchild.js
@@ -0,0 +1,10 @@
+self.addEventListener("message", function(event) {
+  let timeStampCodes = event.data;
+
+  let timeStamps = [];
+  for (let timeStampCode of timeStampCodes) {
+    timeStamps.push(eval(timeStampCode));
+  }
+  
+  postMessage(timeStamps);
+});
