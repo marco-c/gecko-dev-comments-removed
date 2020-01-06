1fad9eaa76da7a9a5ea82930da81e5ea6e41de3d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 00:01:07 +0000

    Bug 1349750 - Move the scroll thumb length from AsyncDragMetrics to ScrollThumbData. r=kats
    MozReview-Commit-ID: Atp7Kgn6adM
    
    UltraBlame original commit: 1c1eb8a23e87b05fbc7443757203ae6c8cc33849

--- gfx/layers/LayerAttributes.h
+++ gfx/layers/LayerAttributes.h
@@ -20,19 +20,28 @@ namespace layers {
 
 struct ScrollThumbData {
   ScrollThumbData()
-    : mDirection(ScrollDirection::NONE), mThumbRatio(0.0f) {}
-  ScrollThumbData(ScrollDirection aDirection, float aThumbRatio)
-    : mDirection(aDirection), mThumbRatio(aThumbRatio) {}
+    : mDirection(ScrollDirection::NONE)
+    , mThumbRatio(0.0f)
+  {}
+  ScrollThumbData(ScrollDirection aDirection,
+                  float aThumbRatio,
+                  CSSCoord aThumbLength)
+    : mDirection(aDirection)
+    , mThumbRatio(aThumbRatio)
+    , mThumbLength(aThumbLength)
+  {}
 
   ScrollDirection mDirection;
   
   
   
   float mThumbRatio;
+  CSSCoord mThumbLength;
 
   bool operator==(const ScrollThumbData& aOther) const {
     return mDirection == aOther.mDirection &&
-           mThumbRatio == aOther.mThumbRatio;
+           mThumbRatio == aOther.mThumbRatio &&
+           mThumbLength == aOther.mThumbLength;
   }
   bool operator!=(const ScrollThumbData& aOther) const {
     return !(*this == aOther);
