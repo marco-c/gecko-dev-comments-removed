cb1c0da44646bb1b513858bfe8aeff1561a14604
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 02:41:55 +0000

    Bug 1371093 - Remove now redundant tooltool manifest definitions from mozharness. r=mshal
    Bug 1356952 defined tooltool manifest at the taskcluster job definition.
    This makes many definitions from mozharness now redundant.
    
    While this would ideally have been done in bug 1356952, some of those
    definitions are likely to still be used by some buildbot job or some
    obscure taskcluster job seldomly triggered. Preparing for the
    possibility that this might burn some jobs, making this removal
    backout-able independently allows other work to happen on top of bug
    1356952 without the risk of everything having to be backed out because
    of some random bustage.
    
    UltraBlame original commit: 0c8ab344a2491cb9b293ed04ea8c7fa3c1d955e6

--- testing/mozharness/configs/builds/releng_sub_android_configs/64_api_15_gradle.py
+++ testing/mozharness/configs/builds/releng_sub_android_configs/64_api_15_gradle.py
@@ -3,7 +3,6 @@ config = {
     'stage_platform': 'android-api-15-gradle',
     'build_type': 'api-15-gradle',
     'src_mozconfig': 'mobile/android/config/mozconfigs/android-api-15-gradle/nightly',
-    'tooltool_manifest_src': 'mobile/android/config/tooltool-manifests/android/releng.manifest',
     'multi_locale_config_platform': 'android',
     
     
