ed693e2d39067b325787764d23304e44207c662b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 08:44:28 +0000

    Bug 1387987 - Use listheader instead of richlistitem as list header in the permission dialog. r=prathiksha
    MozReview-Commit-ID: DJayquaZ01V
    
    UltraBlame original commit: 66576627d41ec952ca17b25bddf1fc2856120e22

--- browser/components/preferences/in-content-new/tests/browser_permissions_dialog.js
+++ browser/components/preferences/in-content-new/tests/browser_permissions_dialog.js
@@ -44,14 +44,14 @@ add_task(async function addPermission() {
   let richlistbox = doc.getElementById("permissionsBox");
 
   
-  Assert.equal(richlistbox.itemCount - 1, 0,
+  Assert.equal(richlistbox.itemCount, 0,
                "Number of permission items is 0 initially");
 
   
   SitePermissions.set(URI, "desktop-notification", SitePermissions.ALLOW);
 
   
-  Assert.equal(richlistbox.itemCount - 1, 1);
+  Assert.equal(richlistbox.itemCount, 1);
   checkPermissionItem(URL, "Allow");
 
   SitePermissions.remove(URL, "desktop-notification");
@@ -74,12 +74,12 @@ add_task(async function observePermissionDelete() {
 
   SitePermissions.set(URI, "desktop-notification", SitePermissions.ALLOW);
 
-  Assert.equal(richlistbox.itemCount - 1, 1,
+  Assert.equal(richlistbox.itemCount, 1,
                "The box contains one permission item initially");
 
   SitePermissions.remove(URI, "desktop-notification");
 
-  Assert.equal(richlistbox.itemCount - 1, 0);
+  Assert.equal(richlistbox.itemCount, 0);
 });
 
 add_task(async function onPermissionChange() {
@@ -109,10 +109,10 @@ add_task(async function onPermissionDelete() {
 
   SitePermissions.set(URI, "desktop-notification", SitePermissions.ALLOW);
 
-  richlistbox.selectItem(richlistbox.getItemAtIndex(1));
+  richlistbox.selectItem(richlistbox.getItemAtIndex(0));
   doc.getElementById("removePermission").click();
 
-  await waitForCondition(() => richlistbox.itemCount - 1 == 0);
+  await waitForCondition(() => richlistbox.itemCount == 0);
 
   Assert.equal(SitePermissions.get(URI, "desktop-notification").state,
                SitePermissions.ALLOW,
@@ -135,7 +135,7 @@ add_task(async function onAllPermissionsDelete() {
   SitePermissions.set(u, "desktop-notification", SitePermissions.ALLOW);
 
   doc.getElementById("removeAllPermissions").click();
-  await waitForCondition(() => richlistbox.itemCount - 1 == 0);
+  await waitForCondition(() => richlistbox.itemCount == 0);
 
   Assert.equal(SitePermissions.get(URI, "desktop-notification").state,
      SitePermissions.ALLOW);
@@ -161,10 +161,10 @@ add_task(async function onPermissionChangeAndDelete() {
   doc.getElementsByAttribute("value", SitePermissions.BLOCK)[0].click();
 
   
-  richlistbox.selectItem(richlistbox.getItemAtIndex(1));
+  richlistbox.selectItem(richlistbox.getItemAtIndex(0));
   doc.getElementById("removePermission").click();
 
-  await waitForCondition(() => richlistbox.itemCount - 1 == 0);
+  await waitForCondition(() => richlistbox.itemCount == 0);
 
   doc.getElementById("btnApplyChanges").click();
 
@@ -198,10 +198,10 @@ add_task(async function onPermissionDeleteCancel() {
   SitePermissions.set(URI, "desktop-notification", SitePermissions.ALLOW);
 
   
-  richlistbox.selectItem(richlistbox.getItemAtIndex(1));
+  richlistbox.selectItem(richlistbox.getItemAtIndex(0));
   doc.getElementById("removePermission").click();
 
-  await waitForCondition(() => richlistbox.itemCount - 1 == 0);
+  await waitForCondition(() => richlistbox.itemCount == 0);
 
   doc.getElementById("cancel").click();
 
