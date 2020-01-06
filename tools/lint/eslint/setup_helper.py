eb4bad3419076d45590271440f25de0fe5d3918e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 06:06:22 +0000

    Bug 1380956 - Don't manually install our local eslint plugins as npm will do that for us. r=ahal
    This works since we added the modules as local references in package.json.
    
    MozReview-Commit-ID: 5e6EkcThJTX
    
    UltraBlame original commit: 74d387e1e050855dd1a8629b7e586b8b437a7608

--- tools/lint/eslint/setup_helper.py
+++ tools/lint/eslint/setup_helper.py
@@ -63,8 +63,6 @@ def eslint_setup():
     orig_cwd = os.getcwd()
     sys.path.append(os.path.dirname(__file__))
 
-    module_path = get_eslint_module_path()
-
     
     
     os.chdir(get_project_root())
@@ -80,22 +78,6 @@ def eslint_setup():
     cmd.extend(extra_parameters)
     print("Installing eslint for mach using \"%s\"..." % (" ".join(cmd)))
     if not call_process("eslint", cmd):
-        return 1
-
-    
-    cmd = [npm_path, "install",
-           os.path.join(module_path, "eslint-plugin-mozilla")]
-    cmd.extend(extra_parameters)
-    print("Installing eslint-plugin-mozilla using \"%s\"..." % (" ".join(cmd)))
-    if not call_process("eslint-plugin-mozilla", cmd):
-        return 1
-
-    
-    cmd = [npm_path, "install",
-           os.path.join(module_path, "eslint-plugin-spidermonkey-js")]
-    cmd.extend(extra_parameters)
-    print("Installing eslint-plugin-spidermonkey-js using \"%s\"..." % (" ".join(cmd)))
-    if not call_process("eslint-plugin-spidermonkey-js", cmd):
         return 1
 
     eslint_path = os.path.join(get_project_root(), "node_modules", ".bin", "eslint")
@@ -128,16 +110,18 @@ def expected_eslint_modules():
         expected_modules = json.load(f)["dependencies"]
 
     
+    
     mozilla_json_path = os.path.join(get_eslint_module_path(),
                                      "eslint-plugin-mozilla", "package.json")
     with open(mozilla_json_path, "r") as f:
-        expected_modules["eslint-plugin-mozilla"] = json.load(f)
+        expected_modules.update(json.load(f)["dependencies"])
 
+    
     
     mozilla_json_path = os.path.join(get_eslint_module_path(),
                                      "eslint-plugin-spidermonkey-js", "package.json")
     with open(mozilla_json_path, "r") as f:
-        expected_modules["eslint-plugin-spidermonkey-js"] = json.load(f)
+        expected_modules.update(json.load(f)["dependencies"])
 
     return expected_modules
 
@@ -187,24 +171,15 @@ def eslint_module_needs_setup():
 
         data = json.load(open(path))
 
+        if version_range.startswith("file:"):
+            
+            
+            continue
+
         if not version_in_range(data["version"], version_range):
             print("%s v%s should be v%s." % (name, data["version"], version_range))
             has_issues = True
             continue
-
-        if name == "eslint-plugin-mozilla" or name == "eslint-plugin-spidermonkey-js":
-            
-            if (cmp(data["dependencies"], expected_data["dependencies"]) != 0 or
-                cmp(data["devDependencies"], expected_data["devDependencies"]) != 0):
-                print("Dependencies of %s differ." % (name))
-                has_issues = True
-                continue
-
-            
-            
-            if check_eslint_files(node_modules_path, name):
-                print("%s has out of-date files." % (name))
-                has_issues = True
 
     return has_issues
 
