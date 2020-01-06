43adc38a65a53293797e1f59c8f0c37c1cdacf14
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 04:05:21 +0000

    Bug 1374204 - set the preference for android hardware encoder to true in nightly. r=jib
    MozReview-Commit-ID: L0ibqWACQLD
    
    UltraBlame original commit: 41bf073edcae732f02b6f24147e5308e84414685

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -547,8 +547,13 @@ pref("media.peerconnection.capture_delay", 100);
 pref("media.getusermedia.playout_delay", 100);
 pref("media.navigator.audio.full_duplex", true);
 
+#ifdef NIGHTLY_BUILD
+pref("media.navigator.hardware.vp8_encode.acceleration_enabled", true);
+pref("media.navigator.hardware.vp8_encode.acceleration_remote_enabled", true);
+#else
 pref("media.navigator.hardware.vp8_encode.acceleration_enabled", false);
 pref("media.navigator.hardware.vp8_encode.acceleration_remote_enabled", false);
+#endif
 pref("media.navigator.hardware.vp8_decode.acceleration_enabled", false);
 #elif defined(XP_LINUX)
 pref("media.peerconnection.capture_delay", 70);
