2d6cdc8d40b069a1771b40958bdf19437763d070
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 03:10:06 +0000

    Bug 1312182 - Consolidate the uses of MOZ_TELEMETRY_REPORTING in the code. r=flod,gfritzsche,jaws
    The MOZ_TELEMETRY_REPORTING define does not control whether or not Telemetry is enabled
    but rather if it will send the gathered data to Mozilla servers. We still want to
    display the about:preferences options and let developers know about this behaviour.
    Please note that this patch is not changing any behaviour: it's only making it explicit
    by showing the options as disabled rather than hiding them.
    
    MozReview-Commit-ID: 7A0y0E0hm0Q
    
    UltraBlame original commit: 7f1e5af6138bbcbd7c3ef73d37e50b91c52e18e1

--- browser/components/preferences/in-content/advanced.js
+++ browser/components/preferences/in-content/advanced.js
@@ -46,9 +46,7 @@ var gAdvancedPane = {
       this.initSubmitCrashes();
     }
     this.initTelemetry();
-    if (AppConstants.MOZ_TELEMETRY_REPORTING) {
-      this.initSubmitHealthReport();
-    }
+    this.initSubmitHealthReport();
     this.updateOnScreenKeyboardVisibility();
     this.updateCacheSizeInputField();
     this.updateActualCacheSize();
@@ -78,10 +76,8 @@ var gAdvancedPane = {
                      gAdvancedPane.updateHardwareAcceleration);
     setEventListener("advancedPrefs", "select",
                      gAdvancedPane.tabSelectionChanged);
-    if (AppConstants.MOZ_TELEMETRY_REPORTING) {
-      setEventListener("submitHealthReportBox", "command",
-                       gAdvancedPane.updateSubmitHealthReport);
-    }
+    setEventListener("submitHealthReportBox", "command",
+                     gAdvancedPane.updateSubmitHealthReport);
 
     setEventListener("connectionSettings", "command",
                      gAdvancedPane.showConnections);
@@ -293,8 +289,10 @@ var gAdvancedPane = {
 
 
   initTelemetry() {
-    if (AppConstants.MOZ_TELEMETRY_REPORTING) {
-      this._setupLearnMoreLink("toolkit.telemetry.infoURL", "telemetryLearnMore");
+    this._setupLearnMoreLink("toolkit.telemetry.infoURL", "telemetryLearnMore");
+    
+    if (!AppConstants.MOZ_TELEMETRY_REPORTING) {
+      document.getElementById("submitTelemetryBox").setAttribute("disabled", "true");
     }
   },
 
@@ -303,46 +301,45 @@ var gAdvancedPane = {
 
 
   setTelemetrySectionEnabled(aEnabled) {
-    if (AppConstants.MOZ_TELEMETRY_REPORTING) {
+    
+    let disabled = !aEnabled;
+    document.getElementById("submitTelemetryBox").disabled = disabled;
+    if (disabled) {
       
-      let disabled = !aEnabled;
-      document.getElementById("submitTelemetryBox").disabled = disabled;
-      if (disabled) {
-        
-        Services.prefs.setBoolPref("toolkit.telemetry.enabled", false);
-      }
-      document.getElementById("telemetryDataDesc").disabled = disabled;
+      Services.prefs.setBoolPref("toolkit.telemetry.enabled", false);
     }
+    document.getElementById("telemetryDataDesc").disabled = disabled;
   },
 
   
 
 
   initSubmitHealthReport() {
-    if (AppConstants.MOZ_TELEMETRY_REPORTING) {
-      this._setupLearnMoreLink("datareporting.healthreport.infoURL", "FHRLearnMore");
+    this._setupLearnMoreLink("datareporting.healthreport.infoURL", "FHRLearnMore");
 
-      let checkbox = document.getElementById("submitHealthReportBox");
+    let checkbox = document.getElementById("submitHealthReportBox");
 
-      if (Services.prefs.prefIsLocked(PREF_UPLOAD_ENABLED)) {
-        checkbox.setAttribute("disabled", "true");
-        return;
-      }
-
-      checkbox.checked = Services.prefs.getBoolPref(PREF_UPLOAD_ENABLED);
-      this.setTelemetrySectionEnabled(checkbox.checked);
+    
+    
+    
+    if (Services.prefs.prefIsLocked(PREF_UPLOAD_ENABLED) ||
+        !AppConstants.MOZ_TELEMETRY_REPORTING) {
+      checkbox.setAttribute("disabled", "true");
+      return;
     }
+
+    checkbox.checked = Services.prefs.getBoolPref(PREF_UPLOAD_ENABLED) &&
+                       AppConstants.MOZ_TELEMETRY_REPORTING;
+    this.setTelemetrySectionEnabled(checkbox.checked);
   },
 
   
 
 
   updateSubmitHealthReport() {
-    if (AppConstants.MOZ_TELEMETRY_REPORTING) {
-      let checkbox = document.getElementById("submitHealthReportBox");
-      Services.prefs.setBoolPref(PREF_UPLOAD_ENABLED, checkbox.checked);
-      this.setTelemetrySectionEnabled(checkbox.checked);
-    }
+    let checkbox = document.getElementById("submitHealthReportBox");
+    Services.prefs.setBoolPref(PREF_UPLOAD_ENABLED, checkbox.checked);
+    this.setTelemetrySectionEnabled(checkbox.checked);
   },
 
   updateOnScreenKeyboardVisibility() {
