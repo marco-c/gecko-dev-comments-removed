23bd6d674e2750ee92abe950eb73930815740cf8
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 23:50:30 +0000

    Bug 1416153 - Part 0: Fix tests that uses BrowserTestUtils.waitForNewTab to perform the registration of the next event handler instantly inside the new tab event handler. r=Gijs
    UltraBlame original commit: 4e261d018c0371b60877450b6d3d925eb6b54ac3

--- browser/base/content/test/tabs/browser_opened_file_tab_navigated_to_web.js
+++ browser/base/content/test/tabs/browser_opened_file_tab_navigated_to_web.js
@@ -22,7 +22,7 @@ add_task(async function() {
   });
 
   
-  let promiseTabOpened = BrowserTestUtils.waitForNewTab(gBrowser, openedUriString);
+  let promiseTabOpened = BrowserTestUtils.waitForNewTab(gBrowser, openedUriString, true);
   await ContentTask.spawn(tab.linkedBrowser, openedUriString, uri => {
     content.open(uri, "_blank");
   });
@@ -33,7 +33,6 @@ add_task(async function() {
   });
 
   let openedBrowser = openedTab.linkedBrowser;
-  await BrowserTestUtils.browserLoaded(openedBrowser);
 
   
   openedBrowser.loadURI("http://example.org/");
