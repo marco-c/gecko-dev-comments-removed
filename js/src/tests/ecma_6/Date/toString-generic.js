40c27a56d2093e9837bdc681abcb1e39fb574555
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 06:21:07 +0000

    Backed out changeset 07dd7b5ed0e8 (bug 1381433)
    UltraBlame original commit: 1e4e0f3f80b80249fb841a00d3dc8226dadac09d

--- js/src/tests/ecma_6/Date/toString-generic.js
+++ js/src/tests/ecma_6/Date/toString-generic.js
@@ -1,12 +1,10 @@
 var BUGNUMBER = 861219;
 var summary = 'Date.prototype.toString is a generic function';
 
-
-
 print(BUGNUMBER + ": " + summary);
 
 for (var thisValue of [{}, [], /foo/, Date.prototype, new Proxy(new Date(), {})])
-  assertThrowsInstanceOf(() => Date.prototype.toString.call(thisValue), TypeError);
+  assertEq(Date.prototype.toString.call(thisValue), "Invalid Date");
 
 for (var prim of [null, undefined, 0, 1.2, true, false, "foo", Symbol.iterator])
   assertThrowsInstanceOf(() => Date.prototype.toString.call(prim), TypeError);
