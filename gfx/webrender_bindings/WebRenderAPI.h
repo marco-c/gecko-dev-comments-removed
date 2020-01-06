c092489ab20837f025cc6da5b0a23473861364d6
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 21:33:21 +0000

    Bug 1389149 - Add WR bindings for setting hit-test info and doing hit-tests. r=mstange
    This exposes some functions on wr::DisplayListBuilder to set hit-test
    info and to do hit-tests.
    
    The mechanism to set hit-test info is made a little more generic than is
    actually used in this patchset, because doing it this way allows for
    more optimization possibilities.
    Specifically, the API allows setting some hit-testing state which is then
    applied to all display items that are created until that state is
    updated or cleared. An alternative would be to specify the hit-testing
    state explicitly when creating particular display items; however doing
    that would force the call sites that create the display items to compute
    or obtain the hit-testing state, which would make the code less
    encapsulated and harder to refactor/optimize.
    
    MozReview-Commit-ID: EJoCFv83iu8
    
    UltraBlame original commit: 45332b138026fc08b3a7923c4bc91a8313ec6a3e

--- gfx/webrender_bindings/WebRenderAPI.h
+++ gfx/webrender_bindings/WebRenderAPI.h
@@ -12,6 +12,7 @@
 #include <unordered_set>
 
 #include "mozilla/AlreadyAddRefed.h"
+#include "mozilla/gfx/CompositorHitTestInfo.h"
 #include "mozilla/layers/SyncObject.h"
 #include "mozilla/Range.h"
 #include "mozilla/webrender/webrender_ffi.h"
@@ -143,6 +144,10 @@ public:
   void UpdateScrollPosition(const wr::WrPipelineId& aPipelineId,
                             const layers::FrameMetrics::ViewID& aScrollId,
                             const wr::LayoutPoint& aScrollPosition);
+  bool HitTest(const wr::WorldPoint& aPoint,
+               wr::WrPipelineId& aOutPipelineId,
+               layers::FrameMetrics::ViewID& aOutScrollId,
+               gfx::CompositorHitTestInfo& aOutHitInfo);
 
   void GenerateFrame();
   void GenerateFrame(const nsTArray<wr::WrOpacityProperty>& aOpacityArray,
@@ -420,6 +425,13 @@ public:
   layers::FrameMetrics::ViewID TopmostScrollId();
   
   bool TopmostIsClip();
+
+  
+  
+  void SetHitTestInfo(const layers::FrameMetrics::ViewID& aScrollId,
+                      gfx::CompositorHitTestInfo aHitInfo);
+  
+  void ClearHitTestInfo();
 
   
   wr::WrState* Raw() { return mWrState; }
