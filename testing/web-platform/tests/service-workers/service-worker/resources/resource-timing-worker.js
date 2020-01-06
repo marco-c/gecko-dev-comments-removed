68628d75245fa9ec96a1e6d02e0e9b095dd7dc97
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 16:41:54 +0000

    Bug 1191943 P3 Update resource-timing.https.html and performance-timeline.https.html WPT tests and expectations. r=asuth
    UltraBlame original commit: 3d492063a52cefae09ef88d4eaa769772b98107f

--- testing/web-platform/tests/service-workers/service-worker/resources/resource-timing-worker.js
+++ testing/web-platform/tests/service-workers/service-worker/resources/resource-timing-worker.js
@@ -1,5 +1,9 @@
 self.addEventListener('fetch', function(event) {
     if (event.request.url.indexOf('dummy.js') != -1) {
-      event.respondWith(new Response());
+      event.respondWith(new Promise(resolve => {
+        
+        
+        setTimeout(_ => resolve(new Response()), 100);
+      }));
     }
   });
