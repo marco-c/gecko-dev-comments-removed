cec4c3d241b86b7d3e3305c2dd50824d0a2be0ff
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 11:37:58 +0000

    Bug 1390951 - Use the same style backend when cloning documents. r=xidorn
    MozReview-Commit-ID: F6HMAsSsNDa
    
    UltraBlame original commit: 3fe2012dd403392317fdbaec3f4f30672c10839d

--- dom/base/nsDocument.cpp
+++ dom/base/nsDocument.cpp
@@ -9607,6 +9607,7 @@ nsDocument::CloneDocHelper(nsDocument* clone, bool aPreallocateChildren) const
   clone->mContentLanguage = mContentLanguage;
   clone->SetContentTypeInternal(GetContentTypeInternal());
   clone->mSecurityInfo = mSecurityInfo;
+  clone->mStyleBackendType = mStyleBackendType;
 
   
   clone->mType = mType;
