87b5362618ade778ae8ea485ad725745dfe48baf
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 04:27:44 +0000

    Bug 1367696 - PART I determine the tour type;r=mossop,timdream
    MozReview-Commit-ID: KUXPftqZdl1
    
    UltraBlame original commit: 4cd7f4883692a394484deeb2ca4e1d59eafbc0cc

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1706,6 +1706,8 @@ pref("browser.suppress_first_window_animation", true);
 
 
 pref("browser.onboarding.enabled", true);
+
+pref("browser.onboarding.tourset-version", 1);
 pref("browser.onboarding.hidden", false);
 
 
