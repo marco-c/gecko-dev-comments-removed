8f69c305ecbf01520c6b10c874d6cbf09085e10d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 05:46:57 +0000

    Bug 1376964 - Part 10: Allow style worker threads to pick fonts out of the user font cache. r=jfkthame
    MozReview-Commit-ID: FX0T5ltA2u4
    
    UltraBlame original commit: bb098650d199a2a565f7dab6678b2b3b69905822

--- gfx/thebes/gfxUserFontSet.cpp
+++ gfx/thebes/gfxUserFontSet.cpp
@@ -474,6 +474,12 @@ gfxUserFontEntry::ContinueLoad()
     }
 }
 
+static bool
+IgnorePrincipal(gfxFontSrcURI* aURI)
+{
+    return aURI->InheritsSecurityContext();
+}
+
 void
 gfxUserFontEntry::DoLoadNextSrc(bool aForceAsync)
 {
@@ -535,9 +541,16 @@ gfxUserFontEntry::DoLoadNextSrc(bool aForceAsync)
                     currSrc.mFormatFlags)) {
 
                 if (ServoStyleSet* set = ServoStyleSet::Current()) {
-                    set->AppendTask(PostTraversalTask::LoadFontEntry(this));
-                    SetLoadState(STATUS_LOAD_PENDING);
-                    return;
+                    
+                    
+                    
+                    
+                    
+                    if (currSrc.mUseOriginPrincipal && IgnorePrincipal(currSrc.mURI)) {
+                        set->AppendTask(PostTraversalTask::LoadFontEntry(this));
+                        SetLoadState(STATUS_LOAD_PENDING);
+                        return;
+                    }
                 }
 
                 gfxFontSrcPrincipal* principal = nullptr;
@@ -565,6 +578,14 @@ gfxUserFontEntry::DoLoadNextSrc(bool aForceAsync)
                             }
                             return;
                         }
+                    }
+
+                    if (ServoStyleSet* set = ServoStyleSet::Current()) {
+                        
+                        
+                        set->AppendTask(PostTraversalTask::LoadFontEntry(this));
+                        SetLoadState(STATUS_LOAD_PENDING);
+                        return;
                     }
 
                     
@@ -1134,12 +1155,6 @@ gfxUserFontSet::UserFontCache::Flusher::Observe(nsISupports* aSubject,
     }
 
     return NS_OK;
-}
-
-static bool
-IgnorePrincipal(gfxFontSrcURI* aURI)
-{
-    return aURI->InheritsSecurityContext();
 }
 
 bool
