1eda91c889f97310fc6d4d9cc1b8ec637889b87a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 06:30:41 +0000

    servo: Merge #17798 - Derive some SVGPaint impls (from servo:derive-all-the-things); r=emilio
    Source-Repo: https://github.com/servo/servo
    Source-Revision: fa34af609fd916f4e4df86ba73e9a5843177a29f
    
    UltraBlame original commit: 8739cb41ff9d0cf2d83fa64b1ca37b7a9eae6f82

--- servo/components/style/values/generics/mod.rs
+++ servo/components/style/values/generics/mod.rs
@@ -255,7 +255,7 @@ impl ToCss for FontSettingTagFloat {
 
 
 #[cfg_attr(feature = "servo", derive(HeapSizeOf))]
-#[derive(Clone, Debug, PartialEq, ToAnimatedValue)]
+#[derive(Clone, Debug, PartialEq, ToAnimatedValue, ToComputedValue, ToCss)]
 pub struct SVGPaint<ColorType> {
     
     pub kind: SVGPaintKind<ColorType>,
@@ -269,7 +269,7 @@ pub struct SVGPaint<ColorType> {
 
 
 #[cfg_attr(feature = "servo", derive(HeapSizeOf))]
-#[derive(Clone, Debug, PartialEq, ToAnimatedValue, ToCss)]
+#[derive(Clone, Debug, PartialEq, ToAnimatedValue, ToComputedValue, ToCss)]
 pub enum SVGPaintKind<ColorType> {
     
     None,
@@ -281,35 +281,6 @@ pub enum SVGPaintKind<ColorType> {
     ContextFill,
     
     ContextStroke,
-}
-
-impl<ColorType> SVGPaintKind<ColorType> {
-    
-    pub fn convert<F, OtherColor>(&self, f: F) -> SVGPaintKind<OtherColor>
-        where F: Fn(&ColorType) -> OtherColor {
-            match *self {
-                SVGPaintKind::None => SVGPaintKind::None,
-                SVGPaintKind::ContextStroke => SVGPaintKind::ContextStroke,
-                SVGPaintKind::ContextFill => SVGPaintKind::ContextFill,
-                SVGPaintKind::Color(ref color) => {
-                    SVGPaintKind::Color(f(color))
-                }
-                SVGPaintKind::PaintServer(ref server) => {
-                    SVGPaintKind::PaintServer(server.clone())
-                }
-            }
-    }
-}
-
-impl<ColorType> SVGPaint<ColorType> {
-    
-    pub fn convert<F, OtherColor>(&self, f: F) -> SVGPaint<OtherColor>
-        where F: Fn(&ColorType) -> OtherColor {
-        SVGPaint {
-            kind: self.kind.convert(&f),
-            fallback: self.fallback.as_ref().map(|color| f(color))
-        }
-    }
 }
 
 impl<ColorType> SVGPaintKind<ColorType> {
@@ -365,15 +336,3 @@ impl<ColorType: Parse> Parse for SVGPaint<ColorType> {
         }
     }
 }
-
-impl<ColorType: ToCss> ToCss for SVGPaint<ColorType> {
-    fn to_css<W>(&self, dest: &mut W) -> fmt::Result where W: fmt::Write {
-        self.kind.to_css(dest)?;
-        if let Some(ref fallback) = self.fallback {
-            fallback.to_css(dest)?;
-        }
-        Ok(())
-    }
-}
-
-
