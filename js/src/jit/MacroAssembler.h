89e9cc410e1e376a3519a46d0558c27fb8a76884
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 23:46:01 +0000

    Bug 1419497 part 2 - Optimize pre-barriers in jit code by handling more cases without calling into C++. r=jonco
    UltraBlame original commit: 092d8e20762a7ae160b62da4ae935d8df5fb0223

--- js/src/jit/MacroAssembler.h
+++ js/src/jit/MacroAssembler.h
@@ -1503,6 +1503,9 @@ class MacroAssembler : public MacroAssemblerSpecific
     
     void wasmEmitStackCheck(Register sp, Register scratch, Label* onOverflow);
 
+    void emitPreBarrierFastPath(JSRuntime* rt, MIRType type, Register temp1, Register temp2,
+                                Register temp3, Label* noBarrier);
+
   public:
     
     
