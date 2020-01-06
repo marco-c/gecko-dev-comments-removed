1413722305311118787037cf6b109b6c86ec987d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 10:55:33 +0000

    Bug 1390402, add a faster variant of TextEditor::GetDocumentIsEmpty(), r=masayuki
    UltraBlame original commit: 73e410c3bfecc3dd020837408c4f0963a4e06768

--- editor/libeditor/EditorBase.h
+++ editor/libeditor/EditorBase.h
@@ -877,7 +877,10 @@ public:
   virtual bool AreNodesSameType(nsIContent* aNode1, nsIContent* aNode2);
 
   static bool IsTextNode(nsIDOMNode* aNode);
-  static bool IsTextNode(nsINode* aNode);
+  static bool IsTextNode(nsINode* aNode)
+  {
+    return aNode->NodeType() == nsIDOMNode::TEXT_NODE;
+  }
 
   static nsCOMPtr<nsIDOMNode> GetChildAt(nsIDOMNode* aParent, int32_t aOffset);
   static nsIContent* GetNodeAtRangeOffsetPoint(nsINode* aParentOrNode,
@@ -969,7 +972,16 @@ public:
   
 
 
-  Element* GetRoot();
+  Element* GetRoot()
+  {
+    if (!mRootElement) {
+      
+      nsCOMPtr<nsIDOMElement> root;
+      GetRootElement(getter_AddRefs(root));
+    }
+
+    return mRootElement;
+  }
 
   
 
