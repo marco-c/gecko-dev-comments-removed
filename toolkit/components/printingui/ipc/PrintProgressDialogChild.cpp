b646c41b0a3fbd4b56ad86dca97148133021277b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 21:38:39 +0000

    Bug 1409971 - Part 1. Add cancelling print job interface to PPrintProgressDialog. r=mconley
    This patch will add empty ipc interface in order to notify cancelling print job.
    
    MozReview-Commit-ID: KqBN5A4ARSd
    
    UltraBlame original commit: 983d715192fefc0fc34fa894ba2d03cd3d17c584

--- toolkit/components/printingui/ipc/PrintProgressDialogChild.cpp
+++ toolkit/components/printingui/ipc/PrintProgressDialogChild.cpp
@@ -43,6 +43,12 @@ PrintProgressDialogChild::RecvDialogOpened()
   return IPC_OK();
 }
 
+mozilla::ipc::IPCResult
+PrintProgressDialogChild::RecvCancelledCurrentJob()
+{
+  return IPC_OK();
+}
+
 
 
 NS_IMETHODIMP
