7e13490ec90246114bdeb2fc29c7af8bae0a5d7b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 10:16:02 +0000

    Bug 1390386 - Add GLContext::mImplicitMakeCurrent. - r=jrmuizel
    MozReview-Commit-ID: 5iyGHNzoCpd
    
    UltraBlame original commit: 9d16670edeb5a831913ae92469c5754a768b9b07

--- dom/canvas/WebGLContext.cpp
+++ dom/canvas/WebGLContext.cpp
@@ -1085,7 +1085,7 @@ WebGLContext::SetDimensions(int32_t signedWidth, int32_t signedHeight)
     
     
 
-    MakeContextCurrent();
+    gl->mImplicitMakeCurrent = true;
 
     gl->fViewport(0, 0, mWidth, mHeight);
     mViewportX = mViewportY = 0;
@@ -1929,12 +1929,6 @@ WebGLContext::ForceRestoreContext()
 
     
     EnqueueUpdateContextLossStatus();
-}
-
-void
-WebGLContext::MakeContextCurrent() const
-{
-    gl->MakeCurrent();
 }
 
 already_AddRefed<mozilla::gfx::SourceSurface>
