0b09738b1bb4715e5f602f0da194dcb78b09a9da
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 05:15:11 +0000

    Bug 1370630 - Untrusted submit event shouldn't trigger form submission. r=smaug.
    MozReview-Commit-ID: 6jl7Xvh1Y1Y
    
    UltraBlame original commit: 367b6f947f8721c2dd1d3dae7212c83e20bc1760

--- dom/html/HTMLFormElement.cpp
+++ dom/html/HTMLFormElement.cpp
@@ -474,7 +474,10 @@ nsresult
 HTMLFormElement::GetEventTargetParent(EventChainPreVisitor& aVisitor)
 {
   aVisitor.mWantsWillHandleEvent = true;
-  if (aVisitor.mEvent->mOriginalTarget == static_cast<nsIContent*>(this)) {
+  
+  
+  if (aVisitor.mEvent->mOriginalTarget == static_cast<nsIContent*>(this) &&
+      aVisitor.mEvent->IsTrusted()) {
     uint32_t msg = aVisitor.mEvent->mMessage;
     if (msg == eFormSubmit) {
       if (mGeneratingSubmit) {
@@ -516,7 +519,10 @@ HTMLFormElement::WillHandleEvent(EventChainPostVisitor& aVisitor)
 nsresult
 HTMLFormElement::PostHandleEvent(EventChainPostVisitor& aVisitor)
 {
-  if (aVisitor.mEvent->mOriginalTarget == static_cast<nsIContent*>(this)) {
+  
+  
+  if (aVisitor.mEvent->mOriginalTarget == static_cast<nsIContent*>(this) &&
+      aVisitor.mEvent->IsTrusted()) {
     EventMessage msg = aVisitor.mEvent->mMessage;
     if (msg == eFormSubmit) {
       
