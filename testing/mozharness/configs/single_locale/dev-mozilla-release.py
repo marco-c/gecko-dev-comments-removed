c7cbe09f6cd6bfd1cbb5c39bf48a6563c4c07907
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 18:28:09 +0000

    Bug 1407331 - Staging releases: Remove *-dev update channel r=bhearsum
    MozReview-Commit-ID: 2hevL6EQjmq
    
    UltraBlame original commit: bd87a65f05b012430972ab4f6fed0dac155cf73f

--- testing/mozharness/configs/single_locale/dev-mozilla-release.py
+++ testing/mozharness/configs/single_locale/dev-mozilla-release.py
@@ -1,7 +1,7 @@
 config = {
     "branch": "jamun",
     "nightly_build": True,
-    "update_channel": "release-dev",
+    "update_channel": "release",
 
     
     "hg_l10n_base": "https://hg.mozilla.org/l10n-central",
