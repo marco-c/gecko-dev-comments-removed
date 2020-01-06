07f5b9e4bc9faf4f37aceb8ae17db3507d4d8dbb
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 04:04:45 +0000

    Bug 1371513: fix hg debuginstall r=Callek
    MozReview-Commit-ID: EAxMJQSSWBM
    
    UltraBlame original commit: 1c8abc6019d9faffe2e533869a311062e123e7f6

--- testing/mozharness/mozharness/base/vcs/mercurial.py
+++ testing/mozharness/mozharness/base/vcs/mercurial.py
@@ -370,7 +370,7 @@ class MercurialVCS(ScriptMixin, LogMixin, TransferMixin):
 
         
         self.run_command(self.hg + ['--version'])
-        self.run_command(self.hg + ['debuginstall'])
+        self.run_command(self.hg + ['debuginstall', '--config=ui.username=worker'])
 
         args = self.hg + [
             '--config', 'extensions.robustcheckout=%s' % self.robustcheckout_path,
