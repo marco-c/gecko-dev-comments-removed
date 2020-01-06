0d51ee5be2b0c0ef669e6e7c01afa44da51876ab
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 19:35:00 +0000

    Bug 1404801 - Part 2: Implement DOM for animation list. r=gl
    MozReview-Commit-ID: 8iYTuFlkcSr
    
    UltraBlame original commit: d0e460f14cae081458466becb14e38d9a7728bd3

---
+++ devtools/client/inspector/animation/components/AnimationList.js
@@ -0,0 +1,29 @@
+
+
+
+
+"use strict";
+
+const { createFactory, DOM: dom, PropTypes, PureComponent } =
+  require("devtools/client/shared/vendor/react");
+
+const AnimationItem = createFactory(require("./AnimationItem"));
+
+class AnimationList extends PureComponent {
+  static get propTypes() {
+    return {
+      animations: PropTypes.arrayOf(PropTypes.object).isRequired,
+    };
+  }
+
+  render() {
+    return dom.ul(
+      {
+        className: "animation-list"
+      },
+      this.props.animations.map(animation => AnimationItem({ animation }))
+    );
+  }
+}
+
+module.exports = AnimationList;
