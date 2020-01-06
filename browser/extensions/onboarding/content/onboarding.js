a32024d20fa3f69a49a1db1be5c3c335c5e35e30
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 02:37:30 +0000

    Bug 1369282 - Update the close button style in the onboarding overlay to fit the spec;r=Fischer,mossop,rexboy
    MozReview-Commit-ID: L1j6ECdtgHM
    
    UltraBlame original commit: 5decd51af7a980c2b96c95bb9729610dabeb9d9d

--- browser/extensions/onboarding/content/onboarding.js
+++ browser/extensions/onboarding/content/onboarding.js
@@ -42,7 +42,7 @@ class Onboarding {
   handleEvent(evt) {
     switch (evt.target.id) {
       case "onboarding-overlay-icon":
-      case "onboarding-tour-close-btn":
+      case "onboarding-overlay-close-btn":
       
       
       
@@ -69,7 +69,7 @@ class Onboarding {
     
     div.innerHTML = `
       <div id="onboarding-overlay-dialog">
-        <button id="onboarding-tour-close-btn">X</button>
+        <span id="onboarding-overlay-close-btn"></span>
         <header>Getting started?</header>
         <nav>
           <ul></ul>
