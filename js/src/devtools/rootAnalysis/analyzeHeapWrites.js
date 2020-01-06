985312ae16ce3b67a139e455f91db2a9e6622b5e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 01:32:56 +0000

    Bug 1363596 part 2 - Add support for symbols() function. r=heycam
    MozReview-Commit-ID: IuFnhkdWG3C
    
    UltraBlame original commit: bfb3d68ed8c4ed67900236ddba07782be1343313

--- js/src/devtools/rootAnalysis/analyzeHeapWrites.js
+++ js/src/devtools/rootAnalysis/analyzeHeapWrites.js
@@ -147,6 +147,7 @@ function treatAsSafeArgument(entry, varName, csuName)
         
         ["Gecko_CopyFontFamilyFrom", "dst", null],
         ["Gecko_SetCounterStyleToName", "aPtr", null],
+        ["Gecko_SetCounterStyleToSymbols", "aPtr", null],
         ["Gecko_CopyCounterStyle", "aDst", null],
         ["Gecko_SetMozBinding", "aDisplay", null],
         [/ClassOrClassList/, /aClass/, null],
