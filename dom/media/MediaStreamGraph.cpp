40e3fbc06b746de5384795ac98f158be9c130fb3
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 21:21:26 +0000

    Bug 1415556 - P7. Remove unecessary locking. r=padenot.
    mForceShutdownTicket and mShutdownTimer are only ever accessed on the main thread. We don't need the use of the monitor to reset them.
    
    MozReview-Commit-ID: 1DL8bLmzEH5
    
    UltraBlame original commit: 329102ab6f3cce1ac6cd1dbfc16a740460a728e9

--- dom/media/MediaStreamGraph.cpp
+++ dom/media/MediaStreamGraph.cpp
@@ -1530,7 +1530,6 @@ MediaStreamGraphImpl::ForceShutDown(media::ShutdownTicket* aShutdownTicket)
   MOZ_ASSERT(NS_IsMainThread(), "Must be called on main thread");
   LOG(LogLevel::Debug, ("MediaStreamGraph %p ForceShutdown", this));
 
-  MonitorAutoLock lock(mMonitor);
   if (aShutdownTicket) {
     MOZ_ASSERT(!mForceShutdownTicket);
     
@@ -1541,8 +1540,9 @@ MediaStreamGraphImpl::ForceShutDown(media::ShutdownTicket* aShutdownTicket)
                             MediaStreamGraph::AUDIO_CALLBACK_DRIVER_SHUTDOWN_TIMEOUT,
                             nsITimer::TYPE_ONE_SHOT);
   }
-  mForceShutDown = true;
   mForceShutdownTicket = aShutdownTicket;
+  MonitorAutoLock lock(mMonitor);
+  mForceShutDown = true;
   if (LifecycleStateRef() == LIFECYCLE_THREAD_NOT_STARTED) {
     
     
@@ -1558,7 +1558,6 @@ NS_IMETHODIMP
 MediaStreamGraphImpl::Notify(nsITimer* aTimer)
 {
   MOZ_ASSERT(NS_IsMainThread());
-  MonitorAutoLock lock(mMonitor);
   NS_ASSERTION(!mForceShutdownTicket, "MediaStreamGraph took too long to shut down!");
   
   
