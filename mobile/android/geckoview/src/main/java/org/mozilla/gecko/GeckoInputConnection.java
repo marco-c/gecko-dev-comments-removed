7daccd371f9c40b97fcbaa0743bd58efd503c2ea
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 10:39:53 +0000

    Bug 1377749 - Follow-up to add null check for editable; r=me
    Follow-up to add a null check for the editable instance. r=me for
    trivial patch.
    
    MozReview-Commit-ID: CAtB2z2mRbC
    
    UltraBlame original commit: 59d0f9b5458b1bc734f0cade024fcea51b0025f6

--- mobile/android/geckoview/src/main/java/org/mozilla/gecko/GeckoInputConnection.java
+++ mobile/android/geckoview/src/main/java/org/mozilla/gecko/GeckoInputConnection.java
@@ -944,6 +944,9 @@ class GeckoInputConnection
                 
                 
                 final Editable editable = getEditable();
+                if (editable == null) {
+                    break;
+                }
                 final Object[] spans = editable.getSpans(0, editable.length(), Object.class);
                 for (final Object span : spans) {
                     if ((editable.getSpanFlags(span) & Spanned.SPAN_COMPOSING) != 0) {
