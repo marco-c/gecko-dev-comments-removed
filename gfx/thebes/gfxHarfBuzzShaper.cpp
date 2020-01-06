e21847f99588f5f08a65eb83b143b46ba30d2dcc
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 17:19:02 +0000

    Bug 1360128 - Pass values from font-variation-settings through to the harfbuzz font object, so that shaping can take variations into account. r=jrmuizel
    UltraBlame original commit: e09ffeb018f88003cbedfb219064897af6689a9c

--- gfx/thebes/gfxHarfBuzzShaper.cpp
+++ gfx/thebes/gfxHarfBuzzShaper.cpp
@@ -1283,6 +1283,15 @@ gfxHarfBuzzShaper::Initialize()
     uint32_t scale = FloatToFixed(mFont->GetAdjustedSize()); 
     hb_font_set_scale(mHBFont, scale, scale);
 
+    const auto& vars = mFont->GetStyle()->variationSettings;
+    size_t len = vars.Length();
+    if (len > 0) {
+        
+        
+        auto hbVars = reinterpret_cast<const hb_variation_t*>(vars.Elements());
+        hb_font_set_variations(mHBFont, hbVars, len);
+    }
+
     return true;
 }
 
