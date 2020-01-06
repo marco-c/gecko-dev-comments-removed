0e52c85cd06a7292554e3fe14c44dd0fc670fd40
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 07:20:24 +0000

    Bug 1384629: omit schema={} for run-missing-tests; r=bstack
    MozReview-Commit-ID: EtBqyvOzFs2
    
    UltraBlame original commit: f6ecdab2e8ee8be316d0e8fb34ce6c3e1a2680be

--- taskcluster/actions/run_missing_tests.py
+++ taskcluster/actions/run_missing_tests.py
@@ -30,7 +30,6 @@ logger = logging.getLogger(__name__)
     """,
     order=100,  
     context=[],  
-    schema={},
 )
 def run_missing_tests(parameters, input, task_group_id, task_id, task):
     decision_task_id = find_decision_task(parameters)
