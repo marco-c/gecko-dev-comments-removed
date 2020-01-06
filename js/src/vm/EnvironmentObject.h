9c938a8493f6a6ed48c0156819001611fde0ef87
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 08:34:39 +0000

    Bug 1387115 - Expose WebAssembly instance memory and globals via debugger scope. r=luke
    MozReview-Commit-ID: 7EnxNmaiNOF
    
    UltraBlame original commit: a301b2aa12671fdd37bc02805a03f8e9a1693843

--- js/src/vm/EnvironmentObject.h
+++ js/src/vm/EnvironmentObject.h
@@ -427,6 +427,28 @@ typedef Rooted<ModuleEnvironmentObject*> RootedModuleEnvironmentObject;
 typedef Handle<ModuleEnvironmentObject*> HandleModuleEnvironmentObject;
 typedef MutableHandle<ModuleEnvironmentObject*> MutableHandleModuleEnvironmentObject;
 
+class WasmInstanceEnvironmentObject : public EnvironmentObject
+{
+    
+    
+    
+    
+    static const uint32_t SCOPE_SLOT = 1;
+
+  public:
+    static const Class class_;
+
+    static const uint32_t RESERVED_SLOTS = 2;
+
+    static WasmInstanceEnvironmentObject* createHollowForDebug(JSContext* cx,
+                                                               Handle<WasmInstanceScope*> scope);
+    WasmInstanceScope& scope() const {
+        Value v = getReservedSlot(SCOPE_SLOT);
+        MOZ_ASSERT(v.isPrivateGCThing());
+        return *static_cast<WasmInstanceScope*>(v.toGCThing());
+    }
+};
+
 class WasmFunctionCallObject : public EnvironmentObject
 {
     
@@ -440,7 +462,7 @@ class WasmFunctionCallObject : public EnvironmentObject
 
     static const uint32_t RESERVED_SLOTS = 2;
 
-    static WasmFunctionCallObject* createHollowForDebug(JSContext* cx,
+    static WasmFunctionCallObject* createHollowForDebug(JSContext* cx, HandleObject enclosing,
                                                         Handle<WasmFunctionScope*> scope);
     WasmFunctionScope& scope() const {
         Value v = getReservedSlot(SCOPE_SLOT);
@@ -1015,6 +1037,7 @@ JSObject::is<js::EnvironmentObject>() const
     return is<js::CallObject>() ||
            is<js::VarEnvironmentObject>() ||
            is<js::ModuleEnvironmentObject>() ||
+           is<js::WasmInstanceEnvironmentObject>() ||
            is<js::WasmFunctionCallObject>() ||
            is<js::LexicalEnvironmentObject>() ||
            is<js::WithEnvironmentObject>() ||
