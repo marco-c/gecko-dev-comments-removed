935bacc87b031465c079c9f1a0a341cf858b69eb
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 11:13:43 +0000

    Bug 1390249: Add PingCentre to browser/modules. r=ursula
    MozReview-Commit-ID: GtwWh0iAqlu
    
    UltraBlame original commit: 944ab211fc311de2d2fe54e9540d86c02ec7faa0

--- browser/base/content/test/static/browser_all_files_referenced.js
+++ browser/base/content/test/static/browser_all_files_referenced.js
@@ -88,6 +88,9 @@ var whitelist = [
   {file: "resource://app/modules/NewTabWebChannel.jsm"},
 
   
+  {file: "resource://app/modules/PingCentre.jsm"},
+
+  
   {file: "resource://gre/res/fonts/mathfontSTIXGeneral.properties"},
   {file: "resource://gre/res/fonts/mathfontUnicode.properties"},
 
