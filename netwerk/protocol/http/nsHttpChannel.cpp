c18879467fb219872e89dbba76679b194dd47923
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 11:39:35 +0000

    Bug 1395121 - NS_NOTREACHED("mListener is null") in nsHttpChannel::OnStartRequest, r=valentin
    UltraBlame original commit: 02b9376bea86bd86e86d1747509e6660af545928

--- netwerk/protocol/http/nsHttpChannel.cpp
+++ netwerk/protocol/http/nsHttpChannel.cpp
@@ -9223,9 +9223,16 @@ nsresult
 nsHttpChannel::TriggerNetwork(int32_t aTimeout)
 {
     MOZ_ASSERT(NS_IsMainThread(), "Must be called on the main thread");
-    
-    
+
     LOG(("nsHttpChannel::TriggerNetwork [this=%p]\n", this));
+
+    if (mCanceled) {
+        LOG(("  channel was canceled.\n"));
+        return mStatus;
+    }
+
+    
+    
     if (mNetworkTriggered) {
         LOG(("  network already triggered. Returning.\n"));
         return NS_OK;
