63a605fcb976ffef94296fd9dbef9f09d5bd60e1
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 17:18:59 +0000

    Bug 1407505 - Relax assertion in ExecutableAllocator destructor if the embedding leaked r=jandem
    UltraBlame original commit: 3d373c760a7f85a926815957b06c0156ee1b4d2e

--- js/src/jit/ExecutableAllocator.cpp
+++ js/src/jit/ExecutableAllocator.cpp
@@ -128,7 +128,8 @@ ExecutableAllocator::~ExecutableAllocator()
         m_smallPools[i]->release(true);
 
     
-    MOZ_ASSERT_IF(m_pools.initialized(), m_pools.empty());
+    MOZ_ASSERT_IF(m_pools.initialized() && rt_->gc.shutdownCollectedEverything(),
+                  m_pools.empty());
 }
 
 ExecutablePool*
