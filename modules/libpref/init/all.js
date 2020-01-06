b5db623f6ac90155a94bbd6062836e110a33970d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 04:59:43 +0000

    Backed out changeset 684b6b32fab2 (bug 1375743) for driver issues on Intel HD Graphics.
    UltraBlame original commit: 47fc833e0893c5777641a72782ee05ac16a18031

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -5741,11 +5741,7 @@ pref("fuzzing.enabled", false);
 #endif
 
 #if defined(XP_WIN)
-#if defined(NIGHTLY_BUILD)
-pref("layers.mlgpu.dev-enabled", true);
-#else
 pref("layers.mlgpu.dev-enabled", false);
-#endif
 
 
 
