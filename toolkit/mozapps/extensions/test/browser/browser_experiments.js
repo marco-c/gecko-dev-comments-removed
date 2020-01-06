3a72d4ce7c59436fe838fe7d05180e518862d781
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 07:24:38 +0000

    Bug 1383741 - Ensure browser_experiments.js works with both new and old Preferences UI. r=mconley
    MozReview-Commit-ID: Bj2r0xLKxAj
    
    UltraBlame original commit: f29c950fa2262e1bbae4072f3056a4662648727a

--- toolkit/mozapps/extensions/test/browser/browser_experiments.js
+++ toolkit/mozapps/extensions/test/browser/browser_experiments.js
@@ -162,28 +162,39 @@ add_task(async function testExperimentLearnMore() {
 });
 
 add_task(async function testOpenPreferences() {
+  var useOldPrefs = Services.prefs.getBoolPref("browser.preferences.useOldOrganization");
+
   await gCategoryUtilities.openType("experiment");
   let btn = gManagerWindow.document.getElementById("experiments-change-telemetry");
 
   is_element_visible(btn, "Change telemetry button visible in in-content UI.");
 
   let deferred = Promise.defer();
-  Services.obs.addObserver(function observer(prefWin, topic, data) {
-    Services.obs.removeObserver(observer, "privacy-pane-loaded");
-    info("Privacy preference pane opened.");
-    executeSoon(function() {
-      
-      
-      
-      let el = prefWin.document.getElementById("dataCollectionGroup");
-      is_element_visible(el);
 
-      prefWin.close();
-      info("Closed preferences pane.");
+  function ensureElementIsVisible(preferencesPane, visibleElement) {
+    Services.obs.addObserver(function observer(prefWin, topic, data) {
+      Services.obs.removeObserver(observer, preferencesPane + "-pane-loaded");
+      info(preferencesPane + " preference pane opened.");
+      executeSoon(function() {
+        
+        
+        
+        let el = prefWin.document.getElementById(visibleElement);
+        is_element_visible(el);
 
-      deferred.resolve();
-    });
-  }, "privacy-pane-loaded");
+        prefWin.close();
+        info("Closed preferences pane.");
+
+        deferred.resolve();
+      });
+    }, preferencesPane + "-pane-loaded");
+  }
+
+  if (useOldPrefs) {
+    ensureElementIsVisible("advanced", "header-advanced");
+  } else {
+    ensureElementIsVisible("privacy", "dataCollectionGroup");
+  }
 
   info("Loading preferences pane.");
   
