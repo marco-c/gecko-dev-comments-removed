e806157f93145673ca88bc3011b382b943366f1e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 14:36:23 +0000

    Backed out changeset 4dab43248f15 (bug 1372823) for crashing GTest on Windows 10 x64 debug. r=backout
    MozReview-Commit-ID: 1u7TFtv6Mdv
    
    UltraBlame original commit: b14c75b83d0226333b1240466ea9f07cfb206ff3

--- mozglue/build/WindowsDllBlocklist.cpp
+++ mozglue/build/WindowsDllBlocklist.cpp
@@ -303,8 +303,10 @@ printf_stderr(const char *fmt, ...)
 }
 
 
+#ifdef _M_IX86
 typedef MOZ_NORETURN_PTR void (__fastcall* BaseThreadInitThunk_func)(BOOL aIsInitialThread, void* aStartAddress, void* aThreadParam);
 static BaseThreadInitThunk_func stub_BaseThreadInitThunk = nullptr;
+#endif
 
 typedef NTSTATUS (NTAPI *LdrLoadDll_func) (PWCHAR filePath, PULONG flags, PUNICODE_STRING moduleFileName, PHANDLE handle);
 static LdrLoadDll_func stub_LdrLoadDll;
@@ -786,6 +788,7 @@ continue_loading:
   return stub_LdrLoadDll(filePath, flags, moduleFileName, handle);
 }
 
+#ifdef _M_IX86
 static bool
 ShouldBlockThread(void* aStartAddress)
 {
@@ -820,6 +823,8 @@ patched_BaseThreadInitThunk(BOOL aIsInitialThread, void* aStartAddress,
 
   stub_BaseThreadInitThunk(aIsInitialThread, aStartAddress, aThreadParam);
 }
+
+#endif 
 
 
 static WindowsDllInterceptor NtDllIntercept;
@@ -870,6 +875,8 @@ DllBlocklist_Initialize(uint32_t aInitFlags)
   }
 #endif
 
+#ifdef _M_IX86 
+
   
   
   if (!GetModuleHandleW(L"WRusr.dll")) {
@@ -881,6 +888,7 @@ DllBlocklist_Initialize(uint32_t aInitFlags)
 #endif
     }
   }
+#endif 
 }
 
 MFBT_API void
