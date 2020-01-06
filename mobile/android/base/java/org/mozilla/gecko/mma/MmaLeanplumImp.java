c6c3edcfb1f1175cd450c077eec51fb2aa57565a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 01:34:04 +0000

    Bug 1361664 - Part2. Create MVP Triggers and Deeplinks for Mobile Marketing Automatin. r=maliu
    This patch do 6 things. They are related so I put them in the same patch.
    1. Extract MmaEvent Name
    2. If MMA is diabled, don't send event.
    3. Add check before sending 'Set Default Borwser' deep link
    4. Add user attribute for delay messaging focus install status.
    5. If the user pref off at runtime, we ask Leanplum to stop and prevent our app sending event to Leanplum.
    6. Fix proguard. Only keep necceary files.
    
    MozReview-Commit-ID: APEmr1JXBLH
    
    UltraBlame original commit: 49714b590d48afd529e830aba3bdb9fbe1dbfe4f

--- mobile/android/base/java/org/mozilla/gecko/mma/MmaLeanplumImp.java
+++ mobile/android/base/java/org/mozilla/gecko/mma/MmaLeanplumImp.java
@@ -7,17 +7,14 @@
 package org.mozilla.gecko.mma;
 
 import android.app.Activity;
-import android.app.Application;
 import android.content.Context;
+import android.content.pm.PackageManager;
 
 import com.leanplum.Leanplum;
 import com.leanplum.LeanplumActivityHelper;
-import com.leanplum.annotations.Parser;
 
-import org.mozilla.gecko.ActivityHandlerHelper;
 import org.mozilla.gecko.AppConstants;
 import org.mozilla.gecko.MmaConstants;
-import org.mozilla.gecko.util.ContextUtils;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -25,7 +22,10 @@ import java.util.Map;
 
 public class MmaLeanplumImp implements MmaInterface {
     @Override
-    public void init(Activity activity) {
+    public void init(final Activity activity) {
+        if (activity == null) {
+            return;
+        }
         Leanplum.setApplicationContext(activity.getApplicationContext());
 
         LeanplumActivityHelper.enableLifecycleCallbacks(activity.getApplication());
@@ -36,18 +36,29 @@ public class MmaLeanplumImp implements MmaInterface {
             Leanplum.setAppIdForDevelopmentMode(MmaConstants.MOZ_LEANPLUM_SDK_CLIENTID, MmaConstants.MOZ_LEANPLUM_SDK_KEY);
         }
 
-
         Map<String, Object> attributes = new HashMap<>();
-        boolean installedFocus = ContextUtils.isPackageInstalled(activity, "org.mozilla.focus");
-        boolean installedKlar = ContextUtils.isPackageInstalled(activity, "org.mozilla.klar");
+        boolean installedFocus = isPackageInstalled(activity, "org.mozilla.focus");
+        boolean installedKlar = isPackageInstalled(activity, "org.mozilla.klar");
         if (installedFocus || installedKlar) {
             attributes.put("focus", "installed");
         }
         Leanplum.start(activity, attributes);
-        Leanplum.track("Launch");
+
         
         
-        LeanplumActivityHelper.onResume(activity);
+        
+        
+        
+        
+        
+        
+        
+        activity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                LeanplumActivityHelper.onResume(activity);
+            }
+        });
     }
 
     @Override
@@ -70,5 +81,15 @@ public class MmaLeanplumImp implements MmaInterface {
     @Override
     public void stop() {
         Leanplum.stop();
+    }
+
+    private static boolean isPackageInstalled(final Context context, String packageName) {
+        try {
+            PackageManager pm = context.getPackageManager();
+            pm.getPackageInfo(packageName, 0);
+            return true;
+        } catch (PackageManager.NameNotFoundException e) {
+            return false;
+        }
     }
 }
