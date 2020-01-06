1f21059a34d76f19c05af50f9ebed5cb55340b7b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 10:46:24 +0000

    Bug 1391703 - Introduce common JSM for security/manager/tools/ scripts. r=keeler
    PSM has various xpcshell scripts under the security/manager/tools/ folder. At
    the moment, these scripts:
      1. Duplicate code.
      2. Aren't testable.
    
    This patch introduces a common, unit tested JSM that these scripts can use.
    
    MozReview-Commit-ID: 5NKRUeJgG8f
    
    UltraBlame original commit: 121e4d470c111f07d1dd45959bd4777de529f473

--- security/manager/tools/.eslintrc.js
+++ security/manager/tools/.eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
   "globals": {
     
     
+    "__LOCATION__": false,
     "arguments": false
   }
 };
--- security/manager/tools/dumpGoogleRoots.js
+++ security/manager/tools/dumpGoogleRoots.js
@@ -13,25 +13,13 @@
 
 
 
-var Cc = Components.classes;
-var Ci = Components.interfaces;
+const { classes: Cc, interfaces: Ci, utils: Cu, results: Cr } = Components;
+
+const { PSMToolUtils } =
+  Cu.import(`file:///${__LOCATION__.parent.path}/PSMToolUtils.jsm`, {});
 
 function downloadRoots() {
-  let req = Cc["@mozilla.org/xmlextras/xmlhttprequest;1"]
-              .createInstance(Ci.nsIXMLHttpRequest);
-  req.open("GET", "https://pki.google.com/roots.pem", false);
-  try {
-    req.send();
-  } catch (e) {
-    throw new Error("ERROR: problem downloading Google Root PEMs: " + e);
-  }
-
-  if (req.status != 200) {
-    throw new Error("ERROR: problem downloading Google Root PEMs. Status: " +
-                    req.status);
-  }
-
-  let pem = req.responseText;
+  let pem = PSMToolUtils.downloadFile("https://pki.google.com/roots.pem", false);
   let roots = [];
   let currentPEM = "";
   let readingRoot = false;
