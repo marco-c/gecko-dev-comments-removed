fedb07e91ed3f200223d414224dd18b255c5b05f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 11:44:22 +0000

    Backed out changeset ce0752c07ff6 (bug 1393046) under suspicion of causing asan failures in devtools tests a=backout CLOSED TREE
    MozReview-Commit-ID: CKtxKuqiqVb
    
    UltraBlame original commit: 0a3ef99797097a11bd12bd5f7852326f2458b8d9

--- tools/profiler/core/platform.cpp
+++ tools/profiler/core/platform.cpp
@@ -2640,46 +2640,6 @@ profiler_get_buffer_info_helper(uint32_t* aCurrentPosition,
 }
 
 static void
-PollJSSamplingForCurrentThread()
-{
-  MOZ_RELEASE_ASSERT(CorePS::Exists());
-
-  PSAutoLock lock(gPSMutex);
-
-  ThreadInfo* info = TLSInfo::Info(lock);
-  if (!info) {
-    return;
-  }
-
-  info->PollJSSampling();
-}
-
-
-
-
-
-
-
-
-
-
-
-
-static void
-TriggerPollJSSamplingOnMainThread()
-{
-  nsCOMPtr<nsIThread> mainThread;
-  nsresult rv = NS_GetMainThread(getter_AddRefs(mainThread));
-  if (NS_SUCCEEDED(rv) && mainThread) {
-    nsCOMPtr<nsIRunnable> task =
-      NS_NewRunnableFunction("TriggerPollJSSamplingOnMainThread", []() {
-        PollJSSamplingForCurrentThread();
-      });
-    SystemGroup::Dispatch(TaskCategory::Other, task.forget());
-  }
-}
-
-static void
 locked_profiler_start(PSLockRef aLock, int aEntries, double aInterval,
                       uint32_t aFeatures,
                       const char** aFilters, uint32_t aFilterCount)
@@ -2729,11 +2689,6 @@ locked_profiler_start(PSLockRef aLock, int aEntries, double aInterval,
           
           
           info->PollJSSampling();
-        } else if (info->IsMainThread()) {
-          
-          
-          
-          TriggerPollJSSamplingOnMainThread();
         }
       }
     }
@@ -3121,7 +3076,17 @@ void
 profiler_js_interrupt_callback()
 {
   
-  PollJSSamplingForCurrentThread();
+
+  MOZ_RELEASE_ASSERT(CorePS::Exists());
+
+  PSAutoLock lock(gPSMutex);
+
+  ThreadInfo* info = TLSInfo::Info(lock);
+  if (!info) {
+    return;
+  }
+
+  info->PollJSSampling();
 }
 
 double
