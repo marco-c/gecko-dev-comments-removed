5e1bd6b0f8a0752a453f80420aa884e2b168d841
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 14:35:50 +0000

    Revert asynchronous window resizes on Windows. (bug 1380462, r=bas)
    UltraBlame original commit: 757ae5e740f21d0c703233173422efd4fce7b501

--- gfx/thebes/gfxPrefs.h
+++ gfx/thebes/gfxPrefs.h
@@ -610,7 +610,7 @@ private:
   DECL_GFX_PREF(Live, "layers.shared-buffer-provider.enabled", PersistentBufferProviderSharedEnabled, bool, false);
   DECL_GFX_PREF(Live, "layers.single-tile.enabled",            LayersSingleTileEnabled, bool, true);
   DECL_GFX_PREF(Once, "layers.stereo-video.enabled",           StereoVideoEnabled, bool, false);
-  DECL_GFX_PREF(Live, "layers.force-synchronous-resize",       LayersForceSynchronousResize, bool, false);
+  DECL_GFX_PREF(Live, "layers.force-synchronous-resize",       LayersForceSynchronousResize, bool, true);
 
   
   
