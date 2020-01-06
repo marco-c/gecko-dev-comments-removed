1baec57c5544bb1f54d96c21ad7fd150dbe89be2
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 21:06:17 +0000

    Bug 1416540 - Convert AnimationValue::GetStyleValue to return a float-based Size. r=mattwoodrow
    This follows from the previous patch; these values feed into UpdateMinMaxScale
    as well, which explicitly wants to use floats, so there's no point in creating
    doubles. The source of this information is also a float-based matrix.
    
    MozReview-Commit-ID: LPk4Xm9AaJJ
    
    UltraBlame original commit: 2466c8a4fb24bdcfec613c7d4e6fb5b5d5d6fea4

--- layout/style/StyleAnimationValue.h
+++ layout/style/StyleAnimationValue.h
@@ -9,8 +9,8 @@
 #ifndef mozilla_StyleAnimationValue_h_
 #define mozilla_StyleAnimationValue_h_
 
-#include "gfxPoint.h"
 #include "mozilla/gfx/MatrixFwd.h"
+#include "mozilla/gfx/Point.h"
 #include "mozilla/ServoBindingTypes.h"
 #include "mozilla/UniquePtr.h"
 #include "nsStringFwd.h"
@@ -424,7 +424,7 @@ public:
   }
 
   
-  gfxSize GetScaleValue(const nsIFrame* aForFrame) const;
+  mozilla::gfx::Size GetScaleValue(const nsIFrame* aForFrame) const;
 
   const css::ComplexColorData& GetComplexColorData() const {
     MOZ_ASSERT(mUnit == eUnit_ComplexColor, "unit mismatch");
@@ -612,7 +612,7 @@ struct AnimationValue
 
   
   
-  gfxSize GetScaleValue(const nsIFrame* aFrame) const;
+  mozilla::gfx::Size GetScaleValue(const nsIFrame* aFrame) const;
 
   
   void SerializeSpecifiedValue(nsCSSPropertyID aProperty,
