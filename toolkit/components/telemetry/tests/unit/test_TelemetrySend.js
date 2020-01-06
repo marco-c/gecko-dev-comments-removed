7500d74ac22fca313724d92c7155518d1913e2b3
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 07:59:48 +0000

    Bug 1357517 - Disable pref observer TelemetrySend test on platforms where the crash reporter component is not available (like asan). r=me
    UltraBlame original commit: 45821738caeb0294b7fec3a00e51413124955110

--- toolkit/components/telemetry/tests/unit/test_TelemetrySend.js
+++ toolkit/components/telemetry/tests/unit/test_TelemetrySend.js
@@ -523,6 +523,12 @@ add_task(async function test_measurePingsSize() {
 });
 
 add_task(async function test_pref_observer() {
+  
+  let registrar = Components.manager.QueryInterface(Ci.nsIComponentRegistrar);
+  if (!registrar.isContractIDRegistered("@mozilla.org/toolkit/crash-reporter;1")) {
+    return;
+  }
+
   await TelemetrySend.setup(true);
 
   let origTelemetryEnabled = Services.prefs.getBoolPref(TelemetryUtils.Preferences.TelemetryEnabled);
