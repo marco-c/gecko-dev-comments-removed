3f87d736b22e8fc388ff1971bba8bb9865e10519
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 19:37:11 +0000

    Bug 1410794 (attempt 2) - Change some |string| occurrences in nsIPrefBranch.idl to |ACString|. r=erahm.
    This makes the code nicer. In particular, it removes many getter_Copies()
    calls. The patch also converts a lot of nsCStrings to nsAutoCString, which will
    avoid heap allocation in the common case.
    
    The patch also renames PREF_CopyCharPref() as PREF_GetCStringPref(), because
    it's actually getting a string, not a char, and that matches the existing
    GetCString() and GetDefaultCString() methods. Correspondingly, it also renames
    PREF_SetCharPref() as PREF_SetCStringPref().
    
    The |aPrefName| arguments in nsIPrefBranch.idl remain as |string| because they
    almost always involve passing in C string literals, and passing "foo" is much
    nicer than passing NS_LITERAL_CSTRING("foo").
    
    It's worth noting that early versions of this patch used |AUTF8String| instead
    of |ACString|. But it turns out that libpref stores prefs internally as Latin1.
    And |ACString| is compatible with Latin1 but |AUTF8String| isn't, because
    non-ASCII Latin1 strings are not valid UTF-8!
    
    MozReview-Commit-ID: D3f7a1Vl1oE
    
    UltraBlame original commit: 5c906f68ee713d8dcf039b5617901035dfd69471

--- netwerk/base/nsIOService.cpp
+++ netwerk/base/nsIOService.cpp
@@ -1360,10 +1360,10 @@ nsIOService::PrefsChanged(nsIPrefBranch *prefs, const char *pref)
 void
 nsIOService::ParsePortList(nsIPrefBranch *prefBranch, const char *pref, bool remove)
 {
-    nsCString portList;
+    nsAutoCString portList;
 
     
-    prefBranch->GetCharPref(pref, getter_Copies(portList));
+    prefBranch->GetCharPref(pref, portList);
     if (!portList.IsVoid()) {
         nsTArray<nsCString> portListArray;
         ParseString(portList, ',', portListArray);
