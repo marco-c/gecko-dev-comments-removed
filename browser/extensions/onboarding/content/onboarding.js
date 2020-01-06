28fc966478aff2e357014b7c4b7b7b09fbb356ec
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 08:31:52 +0000

    Bug 1377283 - making Close onboarding dialog button accessible. r=gasolin, mossop
    MozReview-Commit-ID: 47rUBS3PoNY
    
    UltraBlame original commit: a4e1205b2d4bd6feeca915e0d35556741d51a25f

--- browser/extensions/onboarding/content/onboarding.js
+++ browser/extensions/onboarding/content/onboarding.js
@@ -800,7 +800,6 @@ class Onboarding {
     
     div.innerHTML = `
       <div id="onboarding-overlay-dialog">
-        <button id="onboarding-overlay-close-btn" class="onboarding-close-btn"></button>
         <header id="onboarding-header"></header>
         <nav>
           <ul id="onboarding-tour-list"></ul>
@@ -808,6 +807,7 @@ class Onboarding {
         <footer id="onboarding-footer">
           <input type="checkbox" id="onboarding-tour-hidden-checkbox" /><label for="onboarding-tour-hidden-checkbox"></label>
         </footer>
+        <button id="onboarding-overlay-close-btn" class="onboarding-close-btn"></button>
       </div>
     `;
 
