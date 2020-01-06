8d94e7ea5428f07082cf4ad8f80f626e8e84bfd5
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 23:46:29 +0000

    Backed out 4 changesets (bug 1417251) for reftest failures, e.g. in box-decoration-break-with-outset-box-shadow-1.html. r=backout on a CLOSED TREE
    Backed out changeset cdd6eb876d3d (bug 1417251)
    Backed out changeset c2b174094be3 (bug 1417251)
    Backed out changeset 9a85c4bd47e4 (bug 1417251)
    Backed out changeset a9f5ffd87cd5 (bug 1417251)
    
    UltraBlame original commit: 814d5927ffb8a5398809dcafd1e218a7a325ba5e

--- docshell/base/nsDocShell.cpp
+++ docshell/base/nsDocShell.cpp
@@ -10493,9 +10493,6 @@ nsDocShell::InternalLoad(nsIURI* aURI,
     (aFlags & INTERNAL_LOAD_FLAGS_ALLOW_THIRD_PARTY_FIXUP) != 0;
   mURIResultedInDocument = false;  
 
-  
-  
-  
   if (aLoadType == LOAD_NORMAL ||
       aLoadType == LOAD_STOP_CONTENT ||
       LOAD_TYPE_HAS_FLAGS(aLoadType, LOAD_FLAGS_REPLACE_HISTORY) ||
