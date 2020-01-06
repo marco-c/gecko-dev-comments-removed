d17bd7dcd22fdfd66857faef0f3f12dcdf522f84
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 01:29:52 +0000

    Backed out changesets 7662d766f166 and fc2c82d8ba22 (bug 1366005) for browser_all_files_referenced.js failures on a CLOSED TREE.
    UltraBlame original commit: 259e315b5790a1a321640dcb1b4d945ab73b75a8

--- browser/components/uitour/UITour-lib.js
+++ browser/components/uitour/UITour-lib.js
@@ -197,6 +197,48 @@ if (typeof Mozilla == "undefined") {
 
 
 
+
+
+
+  Mozilla.UITour.showHeartbeat = function(message, thankyouMessage, flowId, engagementURL,
+					  learnMoreLabel, learnMoreURL, options) {
+    var args = {
+      message,
+      thankyouMessage,
+      flowId,
+      engagementURL,
+      learnMoreLabel,
+      learnMoreURL,
+    };
+
+    if (options) {
+      for (var option in options) {
+	if (!options.hasOwnProperty(option)) {
+	  continue;
+	}
+	args[option] = options[option];
+      }
+    }
+
+    _sendEvent("showHeartbeat", args);
+  };
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   
 
 
