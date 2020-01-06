80014a20ea50b1aefd8dec4ec78b69c70b7fd889
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 12:29:22 +0000

    Bug 1390660, part 2 - Define and use a new macro for CC isupports. r=peterv
    The main purpose of defining this is to make conversion of places that
    use the non-CC variant easier. There are many more places that could
    be converted to use these new macros, if somebody felt motivated.
    
    MozReview-Commit-ID: HspjcN76fjg
    
    UltraBlame original commit: 7fcbb4f25c5cad3754aeda9cd48188719d27a1e2

--- xpcom/base/nsCycleCollectionParticipant.h
+++ xpcom/base/nsCycleCollectionParticipant.h
@@ -958,6 +958,22 @@ static NS_CYCLE_COLLECTION_INNERCLASS NS_CYCLE_COLLECTION_INNERNAME;
 
 
 
+
+#define NS_IMPL_QUERY_INTERFACE_CYCLE_COLLECTION_INHERITED(aClass, aSuper, ...) \
+  NS_INTERFACE_TABLE_HEAD_CYCLE_COLLECTION_INHERITED(aClass)                    \
+  NS_INTERFACE_TABLE_INHERITED(aClass, __VA_ARGS__)                             \
+  NS_INTERFACE_TABLE_TAIL_INHERITING(aSuper)
+
+#define NS_IMPL_ISUPPORTS_CYCLE_COLLECTION_INHERITED(aClass, aSuper, ...)         \
+  NS_IMPL_QUERY_INTERFACE_CYCLE_COLLECTION_INHERITED(aClass, aSuper, __VA_ARGS__) \
+  NS_IMPL_ADDREF_INHERITED(aClass, aSuper)                                        \
+  NS_IMPL_RELEASE_INHERITED(aClass, aSuper)
+
+
+
+
+
+
 inline bool TopThreeWordsEquals(const nsID& aID,
                                 const nsID& aOther1,
                                 const nsID& aOther2)
