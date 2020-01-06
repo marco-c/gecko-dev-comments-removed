b0e7db0cdaad81881a5e6d2d7caeaf7595322f4d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 10:12:43 +0000

    Bug 1375911 - Avoid creating a stylo thread pool in e10s parent processes. r=xidorn
    MozReview-Commit-ID: LW92yNDKZf4
    
    UltraBlame original commit: 74df08122c3de5d27d58a4fe4404417055aee5f6

--- layout/style/ServoBindings.h
+++ layout/style/ServoBindings.h
@@ -651,6 +651,9 @@ void Gecko_AddPropertyToSet(nsCSSPropertyIDSetBorrowedMut, nsCSSPropertyID);
 int32_t Gecko_RegisterNamespace(nsIAtom* ns);
 
 
+bool Gecko_ShouldCreateStyleThreadPool();
+
+
 #define STYLE_STRUCT(name, checkdata_cb)                                       \
   void Gecko_Construct_Default_nsStyle##name(                                  \
     nsStyle##name* ptr,                                                        \
