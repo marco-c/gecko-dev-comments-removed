82774a786a3a59611e299006286efecdeb7f2a73
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 02:55:44 +0000

    Bug 1343743 - Part3-1: Use system group's event target for TCPSocketChild's constructor, r=bwc
    Pass an event target argument in constructor, so we can call gNeckoChild->SetEventTargetForActor before sending constructor message to parent.
    
    UltraBlame original commit: df1028511939cadf8fc0670d5da4de05e69f3cc8

--- media/mtransport/nr_socket_prsock.cpp
+++ media/mtransport/nr_socket_prsock.cpp
@@ -110,6 +110,7 @@
 #include "mozilla/SyncRunnable.h"
 #include "nsTArray.h"
 #include "mozilla/dom/TCPSocketBinding.h"
+#include "mozilla/SystemGroup.h"
 #include "nsITCPSocketCallback.h"
 #include "nsIPrefService.h"
 #include "nsIPrefBranch.h"
@@ -1969,9 +1970,16 @@ void NrTcpSocketIpc::connect_i(const nsACString &remote_addr,
                                uint16_t local_port,
                                const nsACString &tls_host) {
   ASSERT_ON_THREAD(io_thread_);
+  
+  
+  MOZ_ASSERT(NS_IsMainThread());
+
   mirror_state_ = NR_CONNECTING;
 
-  dom::TCPSocketChild* child = new dom::TCPSocketChild(NS_ConvertUTF8toUTF16(remote_addr), remote_port);
+  dom::TCPSocketChild* child =
+    new dom::TCPSocketChild(NS_ConvertUTF8toUTF16(remote_addr),
+                            remote_port,
+                            SystemGroup::EventTargetFor(TaskCategory::Other));
   socket_child_ = child;
 
   
