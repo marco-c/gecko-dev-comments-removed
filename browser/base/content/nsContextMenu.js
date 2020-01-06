8fad6c50b548862c42452ff5cb7ff1c5a2a12694
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 04:22:33 +0000

    Bug 1364896 - Part 2 - Increase margin of all context menus that are triggered through touch. r=dao
    MozReview-Commit-ID: ISc3ZyS1njY
    
    UltraBlame original commit: 134d9ee1e545d7da27888a4c0a3b8eae689bbbba

--- browser/base/content/nsContextMenu.js
+++ browser/base/content/nsContextMenu.js
@@ -63,13 +63,13 @@ function openContextMenu(aMessage) {
   let event = gContextMenuContentData.event;
 
   
-  if (event.mozInputSource == MouseEvent.MOZ_SOURCE_TOUCH) {
-    popup.setAttribute("touchmode", "true");
-  } else {
-    popup.removeAttribute("touchmode");
-  }
+  
+  
+  var newEvent = document.createEvent("MouseEvent");
+  newEvent.initNSMouseEvent("contextmenu", true, true, null, 0, event.screenX, event.screenY,
+                            0, 0, false, false, false, false, 0, null, 0, event.mozInputSource);
 
-  popup.openPopupAtScreen(event.screenX, event.screenY, true);
+  popup.openPopupAtScreen(newEvent.screenX, newEvent.screenY, true, newEvent);
 }
 
 function nsContextMenu(aXulMenu, aIsShift) {
