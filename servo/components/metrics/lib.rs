5fe5f73e3ad6020990e116da0b413d270694217f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 21:14:22 +0000

    servo: Merge #19218 - added navigation start for interactive metrics (from avadacatavra:nav-start); r=jdm
    <!-- Please describe your changes on the following line: -->
     follow up from  #18670, fixing #19099
    
    ---
    <!-- Thank you for contributing to Servo! Please replace each `[ ]` by `[X]` when the step is complete, and replace `__` with appropriate data: -->
    - [X] `./mach build -d` does not report any errors
    - [X] `./mach test-tidy` does not report any errors
    - [X] These changes fix #19099 (github issue number if applicable).
    
    <!-- Either: -->
    - [ ] There are tests for these changes OR
    - [ ] These changes do not require tests because _____
    
    <!-- Also, please make sure that "Allow edits from maintainers" checkbox is checked, so that we can help you if you get stuck somewhere along the way.-->
    
    <!-- Pull requests that do not address these steps are welcome, but they will require additional verification as part of the review process. -->
    
    Source-Repo: https://github.com/servo/servo
    Source-Revision: 7d13e5b83c346d06dc68f22857a17f30ea797402
    
    UltraBlame original commit: a3be64eeb28c9284dff61b1a467e9c6eae4546a7

--- servo/components/metrics/lib.rs
+++ servo/components/metrics/lib.rs
@@ -41,6 +41,7 @@ pub trait ProgressiveWebMetric {
 }
 
 
+
 pub const MAX_TASK_NS: u64 = 50000000;
 
 const INTERACTIVE_WINDOW_SECONDS_IN_NS: u64 = 10000000000;
@@ -84,6 +85,7 @@ fn set_metric<U: ProgressiveWebMetric>(
 
     
     if opts::get().print_pwm {
+        println!("Navigation start: {}", pwm.get_navigation_start().unwrap());
         println!("{:?} {:?}", metric_type, time);
     }
 
@@ -215,6 +217,10 @@ impl InteractiveMetrics {
 
     pub fn get_tti(&self) -> Option<u64> {
         self.time_to_interactive.get()
+    }
+
+    pub fn needs_tti(&self) -> bool {
+        self.get_tti().is_none()
     }
 }
 
