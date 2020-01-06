7f4e2d52cc8c3d70992e66581e6146d1bcc0fa55
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 07:00:37 +0000

    Bug 1328727: use json-e for cron decision tasks too; r=aki,jonasfj
    Changes to cron decision tasks:
     - drops some unnecessary routes
     - drops tags.createdForUser: nobody..
     - more use of environment variables within the command line
    
    MozReview-Commit-ID: 9zoqFvwrBRs
    
    UltraBlame original commit: 2badbccc0c6e34cbdf542379a3c31db34dbb1cae

--- taskcluster/taskgraph/cron/decision.py
+++ taskcluster/taskgraph/cron/decision.py
@@ -7,11 +7,13 @@
 
 from __future__ import absolute_import, print_function, unicode_literals
 
+import jsone
 import pipes
 import yaml
-import re
 import os
 import slugid
+
+from taskgraph.util.time import current_json_time
 
 
 def run_decision_task(job, params):
@@ -27,74 +29,55 @@ def run_decision_task(job, params):
 
 
 def make_decision_task(params, symbol, arguments=[], head_rev=None):
-    """Generate a basic decision task, based on the root
-    .taskcluster.yml"""
+    """Generate a basic decision task, based on the root .taskcluster.yml"""
     with open('.taskcluster.yml') as f:
-        taskcluster_yml = f.read()
+        taskcluster_yml = yaml.load(f)
 
     if not head_rev:
         head_rev = params['head_rev']
 
+    slugids = {}
+
+    def as_slugid(name):
+        
+        name = name[0]
+        if name not in slugids:
+            slugids[name] = slugid.nice()
+        return slugids[name]
+
     
     
-    comment = '"no push -- cron task \'{job_name}\'"'.format(**params),
-    replacements = {
-        '\'{{{?now}}}?\'': "{'relative-datestamp': '0 seconds'}",
-        '{{{?owner}}}?': 'nobody@mozilla.org',
-        '{{#shellquote}}{{{comment}}}{{/shellquote}}': comment,
-        '{{{?source}}}?': params['head_repository'],
-        '{{{?url}}}?': params['head_repository'],
-        '{{{?project}}}?': params['project'],
-        '{{{?level}}}?': params['level'],
-        '{{{?revision}}}?': head_rev,
-        '\'{{#from_now}}([^{]*){{/from_now}}\'': "{'relative-datestamp': '\\1'}",
-        '{{{?pushdate}}}?': '0',
-        
-        '{{{?pushlog_id}}}?': '-1',
-        
-        
+    
+    context = {
+        'tasks_for': 'cron',
+        'repository': {
+            'url': params['head_repository'],
+            'project': params['project'],
+            'level': params['level'],
+        },
+        'push': {
+            'revision': params['head_rev'],
+            
+            'pushlog_id': -1,
+            'pushdate': 0,
+            'owner': 'nobody',
+            'comment': '',
+        },
+        'cron': {
+            'task_id': os.environ.get('TASK_ID', '<cron task id>'),
+            'job_name': params['job_name'],
+            'job_symbol': symbol,
+            
+            'quoted_args': ' '.join(pipes.quote(a) for a in arguments),
+        },
+        'now': current_json_time(),
+        'as_slugid': as_slugid,
     }
-    for pattern, replacement in replacements.iteritems():
-        taskcluster_yml = re.sub(pattern, replacement, taskcluster_yml)
 
-    task = yaml.load(taskcluster_yml)['tasks'][0]['task']
+    rendered = jsone.render(taskcluster_yml, context)
+    if len(rendered['tasks']) != 1:
+        raise Exception("Expected .taskcluster.yml to only produce one cron task")
+    task = rendered['tasks'][0]
 
-    
-    task['metadata']['name'] = 'Decision task for cron job ' + params['job_name']
-    cron_task_id = os.environ.get('TASK_ID', '<cron task id>')
-    descr_md = 'Created by a [cron task](https://tools.taskcluster.net/task-inspector/#{}/)'
-    task['metadata']['description'] = descr_md.format(cron_task_id)
-
-    
-    for i, route in enumerate(task['routes']):
-        if route.startswith('index'):
-            task['routes'][i] = route + '-' + params['job_name']
-
-    th = task['extra']['treeherder']
-    th['groupSymbol'] = 'cron'
-    th['symbol'] = symbol
-
-    
-    match = re.match(r'https://(hg.mozilla.org)/(.*?)/?$', params['head_repository'])
-    if not match:
-        raise Exception('Unrecognized head_repository')
-    repo_scope = 'assume:repo:{}/{}:cron:{}'.format(
-        match.group(1), match.group(2), params['job_name'])
-    task.setdefault('scopes', []).append(repo_scope)
-
-    
-    shellcmd = task['payload']['command']
-    shellcmd[-1] = shellcmd[-1].rstrip('\n')  
-    for arg in arguments:
-        shellcmd[-1] += ' ' + pipes.quote(arg)
-
-    task_id = slugid.nice()
-
-    
-    
-    task['taskGroupId'] = task_id
-
-    
-    task['schedulerId'] = 'gecko-level-{}'.format(params['level'])
-
+    task_id = task.pop('taskId')
     return (task_id, task)
