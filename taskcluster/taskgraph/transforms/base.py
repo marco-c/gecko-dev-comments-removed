2b8265bdb751900c951c6b02f29fb29a3e247253
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 06:43:48 +0000

    Bug 1374940 - Allow transforms to access info about tasks from kind dependencies of the current kind. r=dustin
    UltraBlame original commit: eac3bc21f3708cf404a1115c57addedcd0104bb1

--- taskcluster/taskgraph/transforms/base.py
+++ taskcluster/taskgraph/transforms/base.py
@@ -9,7 +9,8 @@ class TransformConfig(object):
     """A container for configuration affecting transforms.  The `config`
     argument to transforms is an instance of this class, possibly with
     additional kind-specific attributes beyond those set here."""
-    def __init__(self, kind, path, config, params):
+    def __init__(self, kind, path, config, params,
+                 kind_dependencies_tasks=None):
         
         self.kind = kind
 
@@ -21,6 +22,10 @@ class TransformConfig(object):
 
         
         self.params = params
+
+        
+        
+        self.kind_dependencies_tasks = kind_dependencies_tasks
 
 
 class TransformSequence(object):
