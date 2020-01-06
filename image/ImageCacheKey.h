fcb2aa752134161e79146b0a0373413aef08745a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 08:59:23 +0000

    Bug 1377158 - (Part 2) Add the info of StyloEnabled() to hash function to make reftests of styloVsGecko get the correct caches. r=heycam
    MozReview-Commit-ID: 7cyXheHQ7Ot
    
    UltraBlame original commit: c8d944dfc0a5ec4b1b490f00c832a19e5532b5ca

--- image/ImageCacheKey.h
+++ image/ImageCacheKey.h
@@ -58,7 +58,8 @@ private:
   static PLDHashNumber ComputeHash(ImageURL* aURI,
                                    const Maybe<uint64_t>& aBlobSerial,
                                    const OriginAttributes& aAttrs,
-                                   void* aControlledDocument);
+                                   void* aControlledDocument,
+                                   bool aIsStyloEnabled);
   static void* GetControlledDocumentToken(nsIDocument* aDocument);
 
   RefPtr<ImageURL> mURI;
@@ -67,6 +68,10 @@ private:
   void* mControlledDocument;
   PLDHashNumber mHash;
   bool mIsChrome;
+  
+  
+  
+  bool mIsStyloEnabled;
 };
 
 } 
