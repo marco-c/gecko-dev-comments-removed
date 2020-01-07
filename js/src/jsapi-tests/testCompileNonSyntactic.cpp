f7e92ef7d5c510327d0f822a5e04ebe2e31eac12
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 03:28:00 +0000

    Bug 1393270: Follow-up: Fix build bustage from rebase. r=bustage
    MozReview-Commit-ID: Ix7itPBAdno
    
    UltraBlame original commit: 15aab71cf2e6baaaf9bc12110f8e17b65c9b7077

--- js/src/jsapi-tests/testCompileNonSyntactic.cpp
+++ js/src/jsapi-tests/testCompileNonSyntactic.cpp
@@ -2,22 +2,26 @@
 
 
 
-#include "gc/GCInternals.h"
 #include "jsapi-tests/tests.h"
+
+#include "gc/GCInternals.h"
 #include "vm/Monitor.h"
 #include "vm/MutexIDs.h"
 
 
+using namespace JS;
+using js::AutoLockMonitor;
+
 struct OffThreadTask {
     OffThreadTask()
-      : monitor(mutexid::TestMutex),
+      : monitor(js::mutexid::TestMutex),
         token(nullptr)
     {}
 
     void* waitUntilDone(JSContext* cx)
     {
         if (OffThreadParsingMustWaitForGC(cx->runtime()))
-            gc::FinishGC(cx);
+            js::gc::FinishGC(cx);
 
         AutoLockMonitor alm(monitor);
         while (!token) {
