539b02a6bab3d456cf5f48e941ceae5fa19a625e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 21:16:10 +0000

    Backed out changeset ad8f78a9b78c (bug 1423017) for causing multiple failures at build/src/dom/base/nsDocument.cpp on a CLOSED TREE
    UltraBlame original commit: e9a0eeeed39a9d7d9fd17a28dceb5a36f6e66dfa

--- dom/base/nsIDocument.h
+++ dom/base/nsIDocument.h
@@ -1203,17 +1203,6 @@ public:
   
 
 
-
-
-
-
-
-  void UpdateViewportOverflowType(nscoord aScrolledWidth,
-                                  nscoord aScrollportWidth);
-
-  
-
-
   virtual bool WillIgnoreCharsetOverride() {
     return true;
   }
@@ -4288,37 +4277,13 @@ protected:
   
   uint32_t mUpdateNestLevel;
 
-  enum ViewportType : uint8_t {
+  enum ViewportType {
     DisplayWidthHeight,
     Specified,
     Unknown
   };
 
   ViewportType mViewportType;
-
-  
-  
-  
-  
-  enum class ViewportOverflowType : uint8_t {
-    
-    
-    
-    NoOverflow,
-
-    
-    
-
-    
-    Desktop,
-    
-    
-    
-    ButNotMinScaleSize,
-    
-    MinScaleSize,
-  };
-  ViewportOverflowType mViewportOverflowType;
 
   PLDHashTable* mSubDocuments;
 
