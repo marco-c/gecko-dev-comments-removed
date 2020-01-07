0fda9c59acf9ed5d9f25eee63b383d065067b389
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 23:55:27 +0000

    Bug 1472973 part 4 - Use JS::GetNonCCWObjectGlobal in PrefableDisablers::isEnabled. r=bz
    UltraBlame original commit: aaaa74019db7f6d20fbdc88eac13043ba155441b

--- dom/bindings/DOMJSClass.h
+++ dom/bindings/DOMJSClass.h
@@ -121,7 +121,7 @@ struct PrefableDisablers {
     
     
     if (nonExposedGlobals &&
-        IsNonExposedGlobal(cx, js::GetGlobalForObjectCrossCompartment(obj),
+        IsNonExposedGlobal(cx, JS::GetNonCCWObjectGlobal(obj),
                            nonExposedGlobals)) {
       return false;
     }
@@ -131,8 +131,7 @@ struct PrefableDisablers {
     if (secureContext && !IsSecureContextOrObjectIsFromSecureContext(cx, obj)) {
       return false;
     }
-    if (enabledFunc &&
-        !enabledFunc(cx, js::GetGlobalForObjectCrossCompartment(obj))) {
+    if (enabledFunc && !enabledFunc(cx, JS::GetNonCCWObjectGlobal(obj))) {
       return false;
     }
     return true;
