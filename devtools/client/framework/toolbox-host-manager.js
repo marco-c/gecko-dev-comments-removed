123d874f59a2b8458f100587d4a2121c38253f04
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 22:15:20 +0000

    Bug 1469602 - Clean up custom host messages. r=ochameau
    Slightly simplify custom host messaging by removing the redundant stringify /
    parse.  This makes it simpler to ignore messages you might overhear.
    
    MozReview-Commit-ID: JZHAzZAVeHZ
    
    UltraBlame original commit: c3ad68679ef115f04af7386562280ce03fad3f2b

--- devtools/client/framework/toolbox-host-manager.js
+++ devtools/client/framework/toolbox-host-manager.js
@@ -104,20 +104,21 @@ ToolboxHostManager.prototype = {
     if (!event.data) {
       return;
     }
+    const msg = event.data;
     
     
-    if (event.data.frameId != this.frameId) {
+    if (msg.frameId != this.frameId) {
       return;
     }
-    switch (event.data.name) {
+    switch (msg.name) {
       case "switch-host":
-        this.switchHost(event.data.hostType);
+        this.switchHost(msg.hostType);
         break;
       case "raise-host":
         this.host.raise();
         break;
       case "set-host-title":
-        this.host.setTitle(event.data.title);
+        this.host.setTitle(msg.title);
         break;
     }
   },
