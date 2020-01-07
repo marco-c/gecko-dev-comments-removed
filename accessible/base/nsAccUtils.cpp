bc2ed428594cdd8ecb7e9d9783d2607c5477d479
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 17:23:43 +0000

    Bug 1455511: Always prune the accessibility tree for sliders. r=surkov
    It doesn't make sense for a slider to have descendants and this confuses NVDA.
    
    MozReview-Commit-ID: 2A3gqTSu38g
    
    UltraBlame original commit: 3f80d2aaef435081f05551eae1a5c9b42302d56b

--- accessible/base/nsAccUtils.cpp
+++ accessible/base/nsAccUtils.cpp
@@ -436,21 +436,27 @@ nsAccUtils::MustPrune(Accessible* aAccessible)
 {
   roles::Role role = aAccessible->Role();
 
-  
-  
   return
-    (role == roles::MENUITEM ||
-     role == roles::COMBOBOX_OPTION ||
-     role == roles::OPTION ||
-     role == roles::ENTRY ||
-     role == roles::FLAT_EQUATION ||
-     role == roles::PASSWORD_TEXT ||
-     role == roles::PUSHBUTTON ||
-     role == roles::TOGGLE_BUTTON ||
-     role == roles::GRAPHIC ||
-     role == roles::SLIDER ||
-     role == roles::PROGRESSBAR ||
-     role == roles::SEPARATOR) &&
-    aAccessible->ContentChildCount() == 1 &&
-    aAccessible->ContentChildAt(0)->IsTextLeaf();
+    
+    
+    role == roles::SLIDER ||
+    
+    
+    (
+     (
+      role == roles::MENUITEM ||
+      role == roles::COMBOBOX_OPTION ||
+      role == roles::OPTION ||
+      role == roles::ENTRY ||
+      role == roles::FLAT_EQUATION ||
+      role == roles::PASSWORD_TEXT ||
+      role == roles::PUSHBUTTON ||
+      role == roles::TOGGLE_BUTTON ||
+      role == roles::GRAPHIC ||
+      role == roles::PROGRESSBAR ||
+      role == roles::SEPARATOR
+     ) &&
+     aAccessible->ContentChildCount() == 1 &&
+     aAccessible->ContentChildAt(0)->IsTextLeaf()
+    );
 }
