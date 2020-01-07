9adebcd4929159b74d3352e7e63461cf45842a79
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 21:46:40 +0000

    No Bug, mozilla-central repo-update HSTS HPKP blocklist remote-settings. r=mtabara
    Differential Revision: https://phabricator.services.mozilla.com/D1615
    
    UltraBlame original commit: 4056174b9eeda13d5e56981f0097805ccae94888

--- security/manager/ssl/StaticHPKPins.h
+++ security/manager/ssl/StaticHPKPins.h
@@ -758,6 +758,7 @@ static const TransportSecurityPreload kPublicKeyPinningPreloadList[] = {
   { "encrypted.google.com", true, false, false, -1, &kPinset_google_root_pems },
   { "es.search.yahoo.com", false, true, false, -1, &kPinset_yahoo },
   { "espanol.search.yahoo.com", false, true, false, -1, &kPinset_yahoo },
+  { "example.test", true, true, false, -1, &kPinset_test },
   { "exclude-subdomains.pinning.example.com", false, false, false, 0, &kPinset_mozilla_test },
   { "facebook.com", false, false, false, -1, &kPinset_facebook },
   { "fi.google.com", true, false, false, -1, &kPinset_google_root_pems },
@@ -1164,4 +1165,4 @@ static const TransportSecurityPreload kPublicKeyPinningPreloadList[] = {
 
 static const int32_t kUnknownId = -1;
 
-static const PRTime kPreloadPKPinsExpirationTime = INT64_C(1536837856621000);
+static const PRTime kPreloadPKPinsExpirationTime = INT64_C(1537278828249000);
