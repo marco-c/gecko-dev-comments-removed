db4316cd5fe29f6011327a2f71e7ca03c3813ee9
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 19:22:40 +0000

    Bug 1461178 - Clear properties list when marking an object group as having unknown properties. r=jonco
    UltraBlame original commit: 513231568d6ee537bafe7576681f36c5dc7b6cf7

--- js/src/vm/TypeInference.cpp
+++ js/src/vm/TypeInference.cpp
@@ -2981,6 +2981,8 @@ ObjectGroup::markUnknown(const AutoSweepObjectGroup& sweep, JSContext* cx)
         }
     }
 
+    clearProperties(sweep);
+
     if (ObjectGroup* unboxedGroup = maybeOriginalUnboxedGroup())
         MarkObjectGroupUnknownProperties(cx, unboxedGroup);
     if (maybeUnboxedLayout(sweep) && maybeUnboxedLayout(sweep)->nativeGroup())
@@ -4359,6 +4361,11 @@ ConstraintTypeSet::sweep(const AutoSweepBase& sweep, Zone* zone,
 inline void
 ObjectGroup::clearProperties(const AutoSweepObjectGroup& sweep)
 {
+    
+    
+    if (zone()->needsIncrementalBarrier())
+        traceChildren(zone()->barrierTracer());
+
     setBasePropertyCount(sweep, 0);
     propertySet = nullptr;
 }
