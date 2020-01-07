be13ac5c9adb4a44ffbb62e85b8509957e11749f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 03:17:13 +0000

    Backed out changeset 9687e3d987be (bug 1415507) for bc failures in browser/components/extensions/test/browser/browser_ext_tabs_saveAsPDF.js on a CLOSED TREE
    UltraBlame original commit: f7f3280ea84a5933e3a8484d73ee600f04396e9b

--- browser/components/extensions/ext-tabs.js
+++ browser/components/extensions/ext-tabs.js
@@ -921,13 +921,10 @@ this.tabs = class extends ExtensionAPI {
             picker.open(function(retval) {
               if (retval == 0 || retval == 2) {
                 
-
-                
-                
                 try {
                   let fstream = Cc["@mozilla.org/network/file-output-stream;1"].createInstance(Ci.nsIFileOutputStream);
-                  fstream.init(picker.file, 0x2A, 0o666, 0); 
-                  fstream.close();
+                  fstream.init(picker.file, 0x2A, 0x1B6, 0); 
+                  fstream.close(); 
                 } catch (e) {
                   resolve(retval == 0 ? "not_saved" : "not_replaced");
                   return;
@@ -935,10 +932,6 @@ this.tabs = class extends ExtensionAPI {
 
                 let psService = Cc["@mozilla.org/gfx/printsettings-service;1"].getService(Ci.nsIPrintSettingsService);
                 let printSettings = psService.newPrintSettings;
-
-                printSettings.printerName = "";
-                printSettings.isInitializedFromPrinter = true;
-                printSettings.isInitializedFromPrefs = true;
 
                 printSettings.printToFile = true;
                 printSettings.toFileName = picker.file.path;
@@ -949,15 +942,6 @@ this.tabs = class extends ExtensionAPI {
                 printSettings.printFrameType = Ci.nsIPrintSettings.kFramesAsIs;
                 printSettings.outputFormat = Ci.nsIPrintSettings.kOutputFormatPDF;
 
-                if (pageSettings.paperSizeUnit !== null) {
-                  printSettings.paperSizeUnit = pageSettings.paperSizeUnit;
-                }
-                if (pageSettings.paperWidth !== null) {
-                  printSettings.paperWidth = pageSettings.paperWidth;
-                }
-                if (pageSettings.paperHeight !== null) {
-                  printSettings.paperHeight = pageSettings.paperHeight;
-                }
                 if (pageSettings.orientation !== null) {
                   printSettings.orientation = pageSettings.orientation;
                 }
@@ -973,29 +957,14 @@ this.tabs = class extends ExtensionAPI {
                 if (pageSettings.showBackgroundImages !== null) {
                   printSettings.printBGImages = pageSettings.showBackgroundImages;
                 }
-                if (pageSettings.edgeLeft !== null) {
-                  printSettings.edgeLeft = pageSettings.edgeLeft;
+                if (pageSettings.paperSizeUnit !== null) {
+                  printSettings.paperSizeUnit = pageSettings.paperSizeUnit;
                 }
-                if (pageSettings.edgeRight !== null) {
-                  printSettings.edgeRight = pageSettings.edgeRight;
+                if (pageSettings.paperWidth !== null) {
+                  printSettings.paperWidth = pageSettings.paperWidth;
                 }
-                if (pageSettings.edgeTop !== null) {
-                  printSettings.edgeTop = pageSettings.edgeTop;
-                }
-                if (pageSettings.edgeBottom !== null) {
-                  printSettings.edgeBottom = pageSettings.edgeBottom;
-                }
-                if (pageSettings.marginLeft !== null) {
-                  printSettings.marginLeft = pageSettings.marginLeft;
-                }
-                if (pageSettings.marginRight !== null) {
-                  printSettings.marginRight = pageSettings.marginRight;
-                }
-                if (pageSettings.marginTop !== null) {
-                  printSettings.marginTop = pageSettings.marginTop;
-                }
-                if (pageSettings.marginBottom !== null) {
-                  printSettings.marginBottom = pageSettings.marginBottom;
+                if (pageSettings.paperHeight !== null) {
+                  printSettings.paperHeight = pageSettings.paperHeight;
                 }
                 if (pageSettings.headerLeft !== null) {
                   printSettings.headerStrLeft = pageSettings.headerLeft;
@@ -1015,25 +984,22 @@ this.tabs = class extends ExtensionAPI {
                 if (pageSettings.footerRight !== null) {
                   printSettings.footerStrRight = pageSettings.footerRight;
                 }
+                if (pageSettings.marginLeft !== null) {
+                  printSettings.marginLeft = pageSettings.marginLeft;
+                }
+                if (pageSettings.marginRight !== null) {
+                  printSettings.marginRight = pageSettings.marginRight;
+                }
+                if (pageSettings.marginTop !== null) {
+                  printSettings.marginTop = pageSettings.marginTop;
+                }
+                if (pageSettings.marginBottom !== null) {
+                  printSettings.marginBottom = pageSettings.marginBottom;
+                }
 
-                let printProgressListener = {
-                  onLocationChange(webProgress, request, location, flags) { },
-                  onProgressChange(webProgress, request, curSelfProgress, maxSelfProgress, curTotalProgress, maxTotalProgress) { },
-                  onSecurityChange(webProgress, request, state) { },
-                  onStateChange(webProgress, request, flags, status) {
-                    if ((flags & Ci.nsIWebProgressListener.STATE_STOP) && (flags & Ci.nsIWebProgressListener.STATE_IS_DOCUMENT)) {
-                      resolve(retval == 0 ? "saved" : "replaced");
-                    }
-                  },
-                  onStatusChange: function(webProgress, request, status, message) {
-                    if (status != 0) {
-                      resolve(retval == 0 ? "not_saved" : "not_replaced");
-                    }
-                  },
-                  QueryInterface: XPCOMUtils.generateQI([Ci.nsIWebProgressListener]),
-                };
+                activeTab.linkedBrowser.print(activeTab.linkedBrowser.outerWindowID, printSettings, null);
 
-                activeTab.linkedBrowser.print(activeTab.linkedBrowser.outerWindowID, printSettings, printProgressListener);
+                resolve(retval == 0 ? "saved" : "replaced");
               } else {
                 
                 resolve("canceled");
