2454d9ffeffb324287e725906c0fc2bc00913fbe
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 11:04:17 +0000

    Bug 1449255: Part 11a - Rename test_bug514327_1 to test_blocklist_plugin_severities.js. r=aswan
    MozReview-Commit-ID: Aor9QhJiBzE
    
    UltraBlame original commit: 9ef52e1e1a7c16944c7ca8b17639fbd73a1c1db5

--- toolkit/mozapps/extensions/test/xpcshell/test_blocklist_plugin_severities.js
+++ toolkit/mozapps/extensions/test/xpcshell/test_blocklist_plugin_severities.js
@@ -2,8 +2,6 @@
 
 
 
-const nsIBLS = Ci.nsIBlocklistService;
-
 var PLUGINS = [{
   
   name: "test_bug514327_1",
@@ -40,17 +38,17 @@ function run_test() {
 
   copyBlocklistToProfile(do_get_file("data/test_bug514327_1.xml"));
 
-  var blocklist = Cc["@mozilla.org/extensions/blocklist;1"].getService(nsIBLS);
+  var {blocklist} = Services;
 
   
-  Assert.ok(blocklist.getPluginBlocklistState(PLUGINS[0], "1", "1.9") == nsIBLS.STATE_BLOCKED);
+  Assert.ok(blocklist.getPluginBlocklistState(PLUGINS[0], "1", "1.9") == blocklist.STATE_BLOCKED);
 
   
-  Assert.ok(blocklist.getPluginBlocklistState(PLUGINS[1], "1", "1.9") == nsIBLS.STATE_OUTDATED);
+  Assert.ok(blocklist.getPluginBlocklistState(PLUGINS[1], "1", "1.9") == blocklist.STATE_OUTDATED);
 
   
-  Assert.ok(blocklist.getPluginBlocklistState(PLUGINS[2], "1", "1.9") == nsIBLS.STATE_OUTDATED);
+  Assert.ok(blocklist.getPluginBlocklistState(PLUGINS[2], "1", "1.9") == blocklist.STATE_OUTDATED);
 
   
-  Assert.ok(blocklist.getPluginBlocklistState(PLUGINS[3], "1", "1.9") == nsIBLS.STATE_NOT_BLOCKED);
+  Assert.ok(blocklist.getPluginBlocklistState(PLUGINS[3], "1", "1.9") == blocklist.STATE_NOT_BLOCKED);
 }
