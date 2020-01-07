651c3880c56d42e65c16f55c0e75484e0a9b3af4
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 02:10:58 +0000

    Bug 1419771 - Introduce DOMPrefs, a thread-safe access to preferences for DOM - part 9 - ServiceWorker testing enabled, r=asuth
    UltraBlame original commit: 785e52d7a0fe18043aaa701b2b14429183388015

--- dom/workers/WorkerPrefs.h
+++ dom/workers/WorkerPrefs.h
@@ -21,7 +21,6 @@
 
 
 
-WORKER_SIMPLE_PREF("dom.serviceWorkers.testing.enabled", ServiceWorkersTestingEnabled, SERVICEWORKERS_TESTING_ENABLED)
 WORKER_SIMPLE_PREF("dom.storageManager.enabled", StorageManagerEnabled, STORAGEMANAGER_ENABLED)
 WORKER_SIMPLE_PREF("dom.promise_rejection_events.enabled", PromiseRejectionEventsEnabled, PROMISE_REJECTION_EVENTS_ENABLED)
 WORKER_SIMPLE_PREF("dom.push.enabled", PushEnabled, PUSH_ENABLED)
