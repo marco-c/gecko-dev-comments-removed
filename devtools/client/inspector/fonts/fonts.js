5fda53266e630d334e99547ec3efd7fd0d57f5ae
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 23:51:40 +0000

    Bug 1465424 - Added 3 histograms to test font editor usage assumptions; r=rcaliman
    MozReview-Commit-ID: 5RWjUqRhtyH
    
    UltraBlame original commit: 0340ab4b2be0a13c7b4a16264a9a0609ff6ad981

--- devtools/client/inspector/fonts/fonts.js
+++ devtools/client/inspector/fonts/fonts.js
@@ -54,6 +54,10 @@ const REGISTERED_AXES_TO_FONT_PROPERTIES = {
 };
 const REGISTERED_AXES = Object.keys(REGISTERED_AXES_TO_FONT_PROPERTIES);
 
+const HISTOGRAM_N_FONT_AXES = "DEVTOOLS_FONTEDITOR_N_FONT_AXES";
+const HISTOGRAM_N_FONTS_RENDERED = "DEVTOOLS_FONTEDITOR_N_FONTS_RENDERED";
+const HISTOGRAM_FONT_TYPE_DISPLAYED = "DEVTOOLS_FONTEDITOR_FONT_TYPE_DISPLAYED";
+
 class FontInspector {
   constructor(inspector, window) {
     this.cssProperties = getCssProperties(inspector.toolbox);
@@ -467,6 +471,32 @@ class FontInspector {
   
 
 
+  logTelemetryProbesOnNewNode() {
+    const { fontData, fontEditor } = this.store.getState();
+    const { telemetry } = this.inspector;
+
+    
+    const nbOfFontsRendered = fontData.fonts.length;
+    telemetry.getHistogramById(HISTOGRAM_N_FONTS_RENDERED).add(nbOfFontsRendered);
+
+    
+    
+    const editedFont = fontEditor.fonts[0];
+    if (!editedFont) {
+      return;
+    }
+
+    const nbOfAxes = editedFont.variationAxes ? editedFont.variationAxes.length : 0;
+    telemetry.getHistogramById(HISTOGRAM_FONT_TYPE_DISPLAYED).add(
+      !nbOfAxes ? "non-variable" : "variable");
+    if (nbOfAxes) {
+      telemetry.getHistogramById(HISTOGRAM_N_FONT_AXES).add(nbOfAxes);
+    }
+  }
+
+  
+
+
 
 
 
@@ -542,8 +572,9 @@ class FontInspector {
   onNewNode() {
     this.ruleView.off("property-value-updated", this.onRulePropertyUpdated);
     if (this.isPanelVisible()) {
-      this.update();
-      this.refreshFontEditor();
+      Promise.all([this.update(), this.refreshFontEditor()]).then(() => {
+        this.logTelemetryProbesOnNewNode();
+      }).catch(e => console.error(e));
     }
   }
 
