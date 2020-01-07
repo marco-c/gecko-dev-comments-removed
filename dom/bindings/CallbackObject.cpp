6427063b46a80f26aed7d6285dcf30120a93b60e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 23:55:23 +0000

    Bug 1472973 part 2 - Use JS::GetNonCCWObjectGlobal in some functions where we unwrapped the object. r=bz
    UltraBlame original commit: 7049feb994eeb47283e34efb660743ef56c9b3b9

--- dom/bindings/CallbackObject.cpp
+++ dom/bindings/CallbackObject.cpp
@@ -196,7 +196,7 @@ CallbackObject::CallSetup::CallSetup(CallbackObject* aCallback,
       globalObject = win;
     } else {
       
-      JSObject* global = js::GetGlobalForObjectCrossCompartment(realCallback);
+      JSObject* global = JS::GetNonCCWObjectGlobal(realCallback);
       globalObject = xpc::NativeGlobal(global);
       MOZ_ASSERT(globalObject);
     }
