80f94f8475f1d91dea0ba6f61478ee1228a151c5
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 17:16:21 +0000

    Bug 1456616 - Remove suppressFocusScroll from nsXULCommandDispatcher;r=bz
    This attribute doesn't do anything.
    
    MozReview-Commit-ID: FnshlyyAms5
    
    UltraBlame original commit: c3ce5469b3ae507082792fdd206f43af41b62c88

--- dom/xul/nsXULCommandDispatcher.cpp
+++ dom/xul/nsXULCommandDispatcher.cpp
@@ -453,19 +453,6 @@ nsXULCommandDispatcher::GetControllerForCommand(const char *aCommand, nsIControl
 }
 
 NS_IMETHODIMP
-nsXULCommandDispatcher::GetSuppressFocusScroll(bool* aSuppressFocusScroll)
-{
-  *aSuppressFocusScroll = false;
-  return NS_OK;
-}
-
-NS_IMETHODIMP
-nsXULCommandDispatcher::SetSuppressFocusScroll(bool aSuppressFocusScroll)
-{
-  return NS_OK;
-}
-
-NS_IMETHODIMP
 nsXULCommandDispatcher::Lock()
 {
   
