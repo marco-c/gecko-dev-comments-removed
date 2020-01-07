4b5ea1a5b602ae60a604fadb30a5503a3ce99c53
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 21:16:47 +0000

    Bug 1429687 - Fix import issues r=rwood
    MozReview-Commit-ID: Lgi1AbiY29J
    
    UltraBlame original commit: fcbec6eece3f7ebbf006b5daeef939c2b4df3a1e

--- testing/talos/talos/xtalos/__init__.py
+++ testing/talos/talos/xtalos/__init__.py
@@ -1,6 +1,5 @@
 
 from __future__ import absolute_import
 
-from start_xperf import start  
-from start_xperf import start_from_config  
-import etlparser  
+from .start_xperf import start, start_from_config  
+from . import etlparser  
