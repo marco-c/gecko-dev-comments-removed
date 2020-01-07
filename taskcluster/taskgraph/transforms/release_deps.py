5f81b5d8c3ca0faa0eb52483b3225c129c3fa255
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 01:04:45 +0000

    bug 1423081 - add dynamic dep_tasks based on shipping-phase. r=callek
    MozReview-Commit-ID: 23HK0N523Q7
    
    UltraBlame original commit: ccf665c924ece476364e1bb8d3911b897f089e8f

--- taskcluster/taskgraph/transforms/release_deps.py
+++ taskcluster/taskgraph/transforms/release_deps.py
@@ -9,6 +9,8 @@ from __future__ import absolute_import, print_function, unicode_literals
 
 from taskgraph.transforms.base import TransformSequence
 
+PHASES = ['build', 'promote', 'push', 'ship']
+
 transforms = TransformSequence()
 
 
@@ -36,6 +38,7 @@ def add_dependencies(config, jobs):
         dependencies = {}
         
         product = _get_product(job)
+        phase = job.get('shipping-phase')
         if product is None:
             continue
 
@@ -53,7 +56,12 @@ def add_dependencies(config, jobs):
                 if 'old-id' in dep_task.label:
                     continue
             
-            if _get_product(dep_task.task) == product:
+            dep_phase = dep_task.attributes.get('shipping_phase')
+            if dep_phase and PHASES.index(dep_phase) > PHASES.index(phase):
+                continue
+            
+            if _get_product(dep_task.task) == product or \
+                    dep_task.attributes.get('shipping_product') == product:
                 dependencies[dep_task.label] = dep_task.label
 
         job.setdefault('dependencies', {}).update(dependencies)
