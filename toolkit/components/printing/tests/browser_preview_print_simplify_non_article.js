51dfc4deea5cb0d3465a344575743f022ebd2fd3
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 08:56:08 +0000

    Bug 1445837 - Fix race condition in toolkit/components/printing/tests/browser_preview_print_simplify_non_article.js. r=mconley
    This patch address a race condition issue for toolkit/components/printing/tests/
    browser_preview_print_simplify_non_article.js test added in bug 1440638.
    
    MozReview-Commit-ID: 4X17u2CP4ea
    
    UltraBlame original commit: d2c963c5022507cb99c74c4bcba9d2c596b4339e

--- toolkit/components/printing/tests/browser_preview_print_simplify_non_article.js
+++ toolkit/components/printing/tests/browser_preview_print_simplify_non_article.js
@@ -65,7 +65,8 @@ add_task(async function switch_print_preview_browsers() {
 
   
   await ContentTask.spawn(simplifiedPPBrowser, null, async function() {
-    is(content.document.title, "Failed to load article from page", "Should have recovery content.");
+    await ContentTaskUtils.waitForCondition(() => content.document.title === "Failed to load article from page",
+      "Simplified document title should be updated with recovery title.");
   });
 
   
