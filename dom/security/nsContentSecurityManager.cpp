ffbd140b6ad1c8dd8745135f47bf9fc19a051456
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 11:18:02 +0000

    Bug 1404744 - Check for FTP subresource after applying CSP. r=ckerschb
    UltraBlame original commit: b2c0699b9bd405300986bc26f256e8bb9c0c53a7

--- dom/security/nsContentSecurityManager.cpp
+++ dom/security/nsContentSecurityManager.cpp
@@ -159,8 +159,8 @@ nsContentSecurityManager::AllowInsecureRedirectToDataURI(nsIChannel* aNewChannel
   return false;
 }
 
- bool
-nsContentSecurityManager::AllowFTPSubresourceLoad(nsIChannel* aChannel)
+ nsresult
+nsContentSecurityManager::CheckFTPSubresourceLoad(nsIChannel* aChannel)
 {
   
   
@@ -168,23 +168,24 @@ nsContentSecurityManager::AllowFTPSubresourceLoad(nsIChannel* aChannel)
 
   nsCOMPtr<nsILoadInfo> loadInfo = aChannel->GetLoadInfo();
   if (!loadInfo) {
-    return true;
+    return NS_OK;
   }
 
   nsContentPolicyType type = loadInfo->GetExternalContentPolicyType();
   if (type == nsIContentPolicy::TYPE_DOCUMENT) {
-    return true;
+    return NS_OK;
   }
 
   nsCOMPtr<nsIURI> uri;
   nsresult rv = NS_GetFinalChannelURI(aChannel, getter_AddRefs(uri));
-  if (NS_FAILED(rv) || !uri) {
-    return true;
+  NS_ENSURE_SUCCESS(rv, rv);
+  if (!uri) {
+    return NS_OK;
   }
 
   bool isFtpURI = (NS_SUCCEEDED(uri->SchemeIs("ftp", &isFtpURI)) && isFtpURI);
   if (!isFtpURI) {
-    return true;
+    return NS_OK;
   }
 
   nsCOMPtr<nsIDocument> doc;
@@ -204,7 +205,7 @@ nsContentSecurityManager::AllowFTPSubresourceLoad(nsIChannel* aChannel)
                                   "BlockSubresourceFTP",
                                   params, ArrayLength(params));
 
-  return false;
+  return NS_ERROR_CONTENT_BLOCKED;
 }
 
 static nsresult
@@ -655,6 +656,10 @@ nsContentSecurityManager::doContentSecurityCheck(nsIChannel* aChannel,
   NS_ENSURE_SUCCESS(rv, rv);
 
   
+  rv = CheckFTPSubresourceLoad(aChannel);
+  NS_ENSURE_SUCCESS(rv, rv);
+
+  
   loadInfo->SetInitialSecurityCheckDone(true);
 
   
@@ -671,6 +676,9 @@ nsContentSecurityManager::AsyncOnChannelRedirect(nsIChannel* aOldChannel,
   
   if (loadInfo && loadInfo->GetEnforceSecurity()) {
     nsresult rv = CheckChannel(aNewChannel);
+    if (NS_SUCCEEDED(rv)) {
+      rv = CheckFTPSubresourceLoad(aNewChannel);
+    }
     if (NS_FAILED(rv)) {
       aOldChannel->Cancel(rv);
       return rv;
@@ -804,10 +812,6 @@ nsContentSecurityManager::CheckChannel(nsIChannel* aChannel)
     NS_ENSURE_SUCCESS(rv, rv);
     
     
-  }
-
-  if (!AllowFTPSubresourceLoad(aChannel)) {
-    return NS_ERROR_CONTENT_BLOCKED;
   }
 
   return NS_OK;
