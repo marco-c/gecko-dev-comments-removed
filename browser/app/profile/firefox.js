fe010d7582f2bb6c081acabcfd77b51b9ac8d377
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 07:07:48 +0000

    Bug 1441824: Let level 5 (Alternate Desktop) for the Windows content sandbox ride the trains. r=jimm
    UltraBlame original commit: 05d753add33269235fda49fc9d486b7d10cde971

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1047,11 +1047,7 @@ pref("dom.ipc.plugins.sandbox-level.flash", 0);
 
 
 
-#if defined(NIGHTLY_BUILD)
 pref("security.sandbox.content.level", 5);
-#else
-pref("security.sandbox.content.level", 4);
-#endif
 
 
 
