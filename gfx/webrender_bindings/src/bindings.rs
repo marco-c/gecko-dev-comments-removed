209292477bc8b866f36aacabb0eed16fef515440
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 01:03:26 +0000

    Backed out changeset fd2a2e17aef3 (bug 1415034) for build bustage at layout/painting/nsDisplayList.cpp:9992. r=backout
    UltraBlame original commit: bcae439efd9ce769f1412b14d2c4ce3205117eb9

--- gfx/webrender_bindings/src/bindings.rs
+++ gfx/webrender_bindings/src/bindings.rs
@@ -359,16 +359,13 @@ pub enum WrFilterOpType {
   Opacity = 6,
   Saturate = 7,
   Sepia = 8,
-  DropShadow = 9,
 }
 
 #[repr(C)]
 #[derive(Copy, Clone)]
 pub struct WrFilterOp {
     filter_type: WrFilterOpType,
-    argument: c_float, 
-    offset: LayoutVector2D, 
-    color: ColorF, 
+    argument: c_float,
 }
 
 /// cbindgen:derive-eq=false
@@ -1296,9 +1293,6 @@ pub extern "C" fn wr_dp_push_stacking_context(state: &mut WrState,
             WrFilterOpType::Opacity => FilterOp::Opacity(PropertyBinding::Value(c_filter.argument), c_filter.argument),
             WrFilterOpType::Saturate => FilterOp::Saturate(c_filter.argument),
             WrFilterOpType::Sepia => FilterOp::Sepia(c_filter.argument),
-            WrFilterOpType::DropShadow => FilterOp::DropShadow(c_filter.offset,
-                                                               c_filter.argument,
-                                                               c_filter.color),
         }
     }).collect();
 
