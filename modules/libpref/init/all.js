25667fb18570eb73fca4c59f4e3aeb2783ddceee
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 17:54:06 +0000

    Bug 1459293 - Enable new Android fling physics on the Nightly channel. r=kats
    MozReview-Commit-ID: EHwhzClIICz
    
    UltraBlame original commit: 49805b8273d6cfdd3168d948ad276b372015d91d

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -632,7 +632,11 @@ pref("layers.geometry.d3d11.enabled", true);
 pref("apz.allow_checkerboarding", true);
 pref("apz.allow_immediate_handoff", true);
 pref("apz.allow_zooming", false);
+#ifdef NIGHTLY_BUILD
+pref("apz.android.chrome_fling_physics.enabled", true);
+#else
 pref("apz.android.chrome_fling_physics.enabled", false);
+#endif
 pref("apz.android.chrome_fling_physics.friction", "0.015");
 pref("apz.android.chrome_fling_physics.inflexion", "0.35");
 pref("apz.android.chrome_fling_physics.stop_threshold", "0.1");
@@ -2624,6 +2628,7 @@ pref("mousewheel.acceleration.factor", 10);
 
 pref("mousewheel.system_scroll_override_on_root_content.vertical.factor", 200);
 pref("mousewheel.system_scroll_override_on_root_content.horizontal.factor", 200);
+
 
 
 
