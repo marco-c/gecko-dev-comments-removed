bcbeb60b70dab1491bc6e8e6f9af5258f7ff4c8f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 00:03:46 +0000

    Bug 1419771 - Introduce DOMPreferences, a thread-safe access to preferences for DOM - part 7 - Notification Request-Interaction enabled, r=asuth
    UltraBlame original commit: cbc192478bf518fb6d9cb4fb4d93a5bb3bb67ba3

--- dom/workers/WorkerPrefs.h
+++ dom/workers/WorkerPrefs.h
@@ -21,7 +21,6 @@
 
 
 
-WORKER_SIMPLE_PREF("dom.webnotifications.requireinteraction.enabled", DOMWorkerNotificationRIEnabled, DOM_WORKERNOTIFICATIONRI)
 WORKER_SIMPLE_PREF("dom.serviceWorkers.enabled", ServiceWorkersEnabled, SERVICEWORKERS_ENABLED)
 WORKER_SIMPLE_PREF("dom.serviceWorkers.testing.enabled", ServiceWorkersTestingEnabled, SERVICEWORKERS_TESTING_ENABLED)
 WORKER_SIMPLE_PREF("dom.serviceWorkers.openWindow.enabled", OpenWindowEnabled, OPEN_WINDOW_ENABLED)
