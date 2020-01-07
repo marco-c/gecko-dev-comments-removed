d617bc212685d60df928271a6c8aa8661589ab4a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 10:10:57 +0000

    Bug 1445465 - Part 3: Update tests. r=Waldo
    UltraBlame original commit: 3214fb35ccd684ada1d5cd230563c80b7f795fe7

--- js/src/tests/non262/Intl/NumberFormat/StringBuffer.js
+++ js/src/tests/non262/Intl/NumberFormat/StringBuffer.js
@@ -10,8 +10,28 @@
 
 var format = new Intl.NumberFormat("it-IT", {minimumFractionDigits: 1});
 
-assertEq(format.format(1123123123123123123123.1), "1.123.123.123.123.120.000.000,0");
-assertEq(format.format(12123123123123123123123.1), "12.123.123.123.123.100.000.000,0");
-assertEq(format.format(123123123123123123123123.1), "123.123.123.123.123.000.000.000,0");
+assertEq(format.format(1123123123123123123123.1), "1.123.123.123.123.123.100.000,0");
+assertEq(format.format(12123123123123123123123.1), "12.123.123.123.123.122.000.000,0");
+assertEq(format.format(123123123123123123123123.1), "123.123.123.123.123.120.000.000,0");
+
+
+function formatToFixed(x) {
+    var mfd = format.resolvedOptions().maximumFractionDigits;
+    var s = x.toFixed(mfd);
+
+    
+    var m = s.match(/^(\d)\.(\d+)e\+(\d+)$/);
+    assertEq(m !== null, true);
+    s = m[1] + m[2].padEnd(m[3], "0");
+
+    
+    m = s.match(/\d{1,3}(?=(?:\d{3})*$)/g);
+    assertEq(m !== null, true);
+    return m.join(".") + ",0";
+}
+
+assertEq(formatToFixed(1123123123123123123123.1), "1.123.123.123.123.123.100.000,0");
+assertEq(formatToFixed(12123123123123123123123.1), "12.123.123.123.123.122.000.000,0");
+assertEq(formatToFixed(123123123123123123123123.1), "123.123.123.123.123.120.000.000,0");
 
 reportCompare(0, 0, "ok");
