38f58629c61ad3e8559d6b00e39ba8ef6b42000e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 05:20:42 +0000

    Bug 1436663 - EditorBase::JoinNodeDeep() should set result after actually joining the nodes r=m_kato
    EditorBase::JoinNodeDeep() returns a DOM point which was start of right node.
    Currently, this is set before actually joining the nodes.  Therefore, we see
    warnings since right node which becomes joined node may have less children
    than left node.
    
    This patch also makes the NS_WARNING_ASSERTION to NS_ASSERTION since no tests
    hit this.  So, we can use it to detect regressions.
    
    MozReview-Commit-ID: 3RmRP588AkF
    
    UltraBlame original commit: 216fac39a9f44b708a4242a47a8ee8296eed3a34

--- editor/libeditor/EditorBase.cpp
+++ editor/libeditor/EditorBase.cpp
@@ -4360,13 +4360,13 @@ EditorBase::JoinNodeDeep(nsIContent& aLeftNode,
          AreNodesSameType(leftNodeToJoin, rightNodeToJoin)) {
     uint32_t length = leftNodeToJoin->Length();
 
-    ret.Set(rightNodeToJoin, length);
-
     
     nsresult rv = JoinNodes(*leftNodeToJoin, *rightNodeToJoin);
     if (NS_WARN_IF(NS_FAILED(rv))) {
       return EditorDOMPoint();
     }
+
+    ret.Set(rightNodeToJoin, length);
 
     if (parentNode->GetAsText()) {
       
