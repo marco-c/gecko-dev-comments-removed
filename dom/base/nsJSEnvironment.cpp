5c9031a227c11d8c2a5709ea17f6f95aed4fb450
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 21:45:19 +0000

    Bug 1467920, ensure only non-negative values are reported to telemetry, r=mccr8
    UltraBlame original commit: 9948ffb9dc033658ac0e5820887bea449bd34837

--- dom/base/nsJSEnvironment.cpp
+++ dom/base/nsJSEnvironment.cpp
@@ -1281,7 +1281,9 @@ FireForgetSkippable(uint32_t aSuspected, bool aRemoveChildless,
     if (!aDeadline.IsNull()) {
       if (aDeadline < now) {
         
-        idleDuration = aDeadline - startTimeStamp;
+        if (aDeadline > startTimeStamp) {
+          idleDuration = aDeadline - startTimeStamp;
+        }
       } else {
         idleDuration = duration;
       }
