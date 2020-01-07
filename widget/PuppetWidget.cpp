5f45360cef904e8f1c6f77be682522be0cf840e5
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 11:58:24 +0000

    Bug 1453016 - user safer enum serialization for gfx::SurfaceFormat in IPC; r=kats
    MozReview-Commit-ID: Fhyg8e5lyW6
    
    UltraBlame original commit: 3961427969b9aa31624e2bd36593fe92bf699e96

--- widget/PuppetWidget.cpp
+++ widget/PuppetWidget.cpp
@@ -93,6 +93,9 @@ PuppetWidget::PuppetWidget(TabChild* aTabChild)
   , mDefaultScale(-1)
   , mCursorHotspotX(0)
   , mCursorHotspotY(0)
+  , mEnabled{ false }
+  , mVisible{ false }
+  , mNeedIMEStateInit{ false }
   , mIgnoreCompositionEvents(false)
 {
   
@@ -1034,7 +1037,7 @@ PuppetWidget::SetCursor(imgIContainer* aCursor,
   nsDependentCString cursorData(surfaceData.get(), length);
   mozilla::gfx::IntSize size = dataSurface->GetSize();
   if (!mTabChild->SendSetCustomCursor(cursorData, size.width, size.height, stride,
-                                      static_cast<uint8_t>(dataSurface->GetFormat()),
+                                      dataSurface->GetFormat(),
                                       aHotspotX, aHotspotY, mUpdateCursor)) {
     return NS_ERROR_FAILURE;
   }
