f6c84624932b27197ae9f2a07946cf64d33e5ed0
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 18:46:48 +0000

    Bug 1239708: Improve awesomebar autofill. Part 3: Front-end changes. r=mak
    MozReview-Commit-ID: 8AR11dVfIVn
    
    UltraBlame original commit: 8b944decbb138ec1342f90f8ae1c92304b4f3832

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -296,7 +296,6 @@ pref("browser.urlbar.doubleClickSelectsAll", false);
 
 
 pref("browser.urlbar.autoFill", true);
-pref("browser.urlbar.autoFill.typed", true);
 pref("browser.urlbar.speculativeConnect.enabled", true);
 
 
