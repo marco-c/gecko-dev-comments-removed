e465b3ea185a45d57fe2881ab4ab51a1956fb02d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 11:04:58 +0000

    Bug 1449255: Part 25 - Fold test_bug596607 into test_registry.js. r=aswan
    MozReview-Commit-ID: KK6zpHmKlWS
    
    UltraBlame original commit: 74241511b6b244bdfc44508c0bcc4365c4406181

--- toolkit/mozapps/extensions/test/xpcshell/test_registry.js
+++ toolkit/mozapps/extensions/test/xpcshell/test_registry.js
@@ -34,97 +34,97 @@ var addon2 = {
   }]
 };
 
+const IDS = ["addon1@tests.mozilla.org",
+             "addon2@tests.mozilla.org",
+             "addon3@tests.mozilla.org"];
+
+
 const addon1Dir = writeInstallRDFForExtension(addon1, gProfD, "addon1");
 const addon2Dir = writeInstallRDFForExtension(addon2, gProfD, "addon2");
+const addon3Dir = gProfD.clone();
+addon3Dir.append("addon3@tests.mozilla.org");
 
 let registry;
 
-function run_test() {
-  
-  if (!("nsIWindowsRegKey" in Ci))
-    return;
-
+add_task(async function setup() {
   registry = new MockRegistry();
   registerCleanupFunction(() => {
     registry.shutdown();
   });
-
-  do_test_pending();
-
-  run_test_1();
-}
+});
 
 
-function run_test_1() {
+add_task(async function test_1() {
   registry.setValue(Ci.nsIWindowsRegKey.ROOT_KEY_LOCAL_MACHINE,
                    "SOFTWARE\\Mozilla\\XPCShell\\Extensions",
                     "addon1@tests.mozilla.org", addon1Dir.path);
   registry.setValue(Ci.nsIWindowsRegKey.ROOT_KEY_CURRENT_USER,
                     "SOFTWARE\\Mozilla\\XPCShell\\Extensions",
                     "addon2@tests.mozilla.org", addon2Dir.path);
+  registry.setValue(Ci.nsIWindowsRegKey.ROOT_KEY_CURRENT_USER,
+                    "SOFTWARE\\Mozilla\\XPCShell\\Extensions",
+                    "addon3@tests.mozilla.org", addon3Dir.path);
 
-  startupManager();
+  await promiseStartupManager();
 
-  AddonManager.getAddonsByIDs(["addon1@tests.mozilla.org",
-                               "addon2@tests.mozilla.org"], function([a1, a2]) {
-    Assert.notEqual(a1, null);
-    Assert.ok(a1.isActive);
-    Assert.ok(!hasFlag(a1.permissions, AddonManager.PERM_CAN_UNINSTALL));
-    Assert.equal(a1.scope, AddonManager.SCOPE_SYSTEM);
+  let [a1, a2, a3] = await AddonManager.getAddonsByIDs(IDS);
+  notEqual(a1, null);
+  ok(a1.isActive);
+  ok(!hasFlag(a1.permissions, AddonManager.PERM_CAN_UNINSTALL));
+  equal(a1.scope, AddonManager.SCOPE_SYSTEM);
 
-    Assert.notEqual(a2, null);
-    Assert.ok(a2.isActive);
-    Assert.ok(!hasFlag(a2.permissions, AddonManager.PERM_CAN_UNINSTALL));
-    Assert.equal(a2.scope, AddonManager.SCOPE_USER);
+  notEqual(a2, null);
+  ok(a2.isActive);
+  ok(!hasFlag(a2.permissions, AddonManager.PERM_CAN_UNINSTALL));
+  equal(a2.scope, AddonManager.SCOPE_USER);
 
-    executeSoon(run_test_2);
-  });
-}
+  equal(a3, null);
+});
 
 
-function run_test_2() {
+add_task(async function test_2() {
   registry.setValue(Ci.nsIWindowsRegKey.ROOT_KEY_LOCAL_MACHINE,
                     "SOFTWARE\\Mozilla\\XPCShell\\Extensions",
                     "addon1@tests.mozilla.org", null);
   registry.setValue(Ci.nsIWindowsRegKey.ROOT_KEY_CURRENT_USER,
                     "SOFTWARE\\Mozilla\\XPCShell\\Extensions",
                     "addon2@tests.mozilla.org", null);
+  registry.setValue(Ci.nsIWindowsRegKey.ROOT_KEY_CURRENT_USER,
+                    "SOFTWARE\\Mozilla\\XPCShell\\Extensions",
+                    "addon3@tests.mozilla.org", null);
 
-  restartManager();
+  await promiseRestartManager();
 
-  AddonManager.getAddonsByIDs(["addon1@tests.mozilla.org",
-                               "addon2@tests.mozilla.org"], function([a1, a2]) {
-    Assert.equal(a1, null);
-    Assert.equal(a2, null);
-
-    executeSoon(run_test_3);
-  });
-}
+  let [a1, a2, a3] = await AddonManager.getAddonsByIDs(IDS);
+  equal(a1, null);
+  equal(a2, null);
+  equal(a3, null);
+});
 
 
-function run_test_3() {
+add_task(async function test_3() {
   registry.setValue(Ci.nsIWindowsRegKey.ROOT_KEY_LOCAL_MACHINE,
                     "SOFTWARE\\Mozilla\\XPCShell\\Extensions",
                     "addon1@tests.mozilla.org", addon2Dir.path);
   registry.setValue(Ci.nsIWindowsRegKey.ROOT_KEY_CURRENT_USER,
                     "SOFTWARE\\Mozilla\\XPCShell\\Extensions",
                     "addon2@tests.mozilla.org", addon1Dir.path);
+  registry.setValue(Ci.nsIWindowsRegKey.ROOT_KEY_CURRENT_USER,
+                    "SOFTWARE\\Mozilla\\XPCShell\\Extensions",
+                    "addon3@tests.mozilla.org", addon3Dir.path);
 
-  restartManager();
+  await promiseRestartManager();
 
-  AddonManager.getAddonsByIDs(["addon1@tests.mozilla.org",
-                               "addon2@tests.mozilla.org"], function([a1, a2]) {
-    Assert.equal(a1, null);
-    Assert.equal(a2, null);
-
-    executeSoon(run_test_4);
-  });
-}
+  let [a1, a2, a3] = await AddonManager.getAddonsByIDs(IDS);
+  equal(a1, null);
+  equal(a2, null);
+  equal(a3, null);
+});
 
 
-function run_test_4() {
+add_task(async function test_4() {
   
-  restartManager();
+  await promiseRestartManager();
 
   registry.setValue(Ci.nsIWindowsRegKey.ROOT_KEY_LOCAL_MACHINE,
                     "SOFTWARE\\Mozilla\\XPCShell\\Extensions",
@@ -132,13 +132,16 @@ function run_test_4() {
   registry.setValue(Ci.nsIWindowsRegKey.ROOT_KEY_CURRENT_USER,
                     "SOFTWARE\\Mozilla\\XPCShell\\Extensions",
                     "addon2@tests.mozilla.org", null);
+  registry.setValue(Ci.nsIWindowsRegKey.ROOT_KEY_CURRENT_USER,
+                    "SOFTWARE\\Mozilla\\XPCShell\\Extensions",
+                    "addon3@tests.mozilla.org", null);
 
-  restartManager();
+  await promiseRestartManager();
 
   registry.setValue(Ci.nsIWindowsRegKey.ROOT_KEY_LOCAL_MACHINE,
                     "SOFTWARE\\Mozilla\\XPCShell\\Extensions",
                     "addon1@tests.mozilla.org", addon1Dir.path);
-  restartManager();
+  await promiseRestartManager();
 
   registry.setValue(Ci.nsIWindowsRegKey.ROOT_KEY_LOCAL_MACHINE,
                     "SOFTWARE\\Mozilla\\XPCShell\\Extensions",
@@ -148,13 +151,10 @@ function run_test_4() {
                     "addon2@tests.mozilla.org", addon1Dir.path);
   writeInstallRDFForExtension(addon2, gProfD, "addon1");
 
-  restartManager();
+  await promiseRestartManager();
 
-  AddonManager.getAddonsByIDs(["addon1@tests.mozilla.org",
-                               "addon2@tests.mozilla.org"], function([a1, a2]) {
-    Assert.equal(a1, null);
-    Assert.notEqual(a2, null);
-
-    executeSoon(do_test_finished);
-  });
-}
+  let [a1, a2, a3] = await AddonManager.getAddonsByIDs(IDS);
+  equal(a1, null);
+  notEqual(a2, null);
+  equal(a3, null);
+});
