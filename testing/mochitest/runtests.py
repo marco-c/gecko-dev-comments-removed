8bdb58c11ee393ae0a5561b157f2ffa386fa3f29
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 03:03:47 +0000

    Bug 1406350 - part1: Update runtest.py to new loopback setup. r=pehrsons
    MozReview-Commit-ID: GLB78LWVTHP
    
    UltraBlame original commit: 629c42087f29a94f1acd88564c1c3712adeb33c7

--- testing/mochitest/runtests.py
+++ testing/mochitest/runtests.py
@@ -750,21 +750,25 @@ def findTestMediaDevices(log):
     info['video'] = name
 
     
-    def sine_source_loaded():
+    def null_sink_loaded():
         o = subprocess.check_output(
             ['/usr/bin/pactl', 'list', 'short', 'modules'])
-        return filter(lambda x: 'module-sine-source' in x, o.splitlines())
+        return filter(lambda x: 'module-null-sink' in x, o.splitlines())
 
-    if not sine_source_loaded():
+    if not null_sink_loaded():
         
         subprocess.check_call(['/usr/bin/pactl', 'load-module',
-                               'module-sine-source'])
-    if not sine_source_loaded():
-        log.error('Couldn\'t load module-sine-source')
+                               'module-null-sink'])
+
+    if not null_sink_loaded():
+        log.error('Couldn\'t load module-null-sink')
         return None
 
     
-    info['audio'] = 'Sine source at 440 Hz'
+    subprocess.check_call(['/usr/bin/pacmd', 'set-default-sink', 'null'])
+
+    
+    info['audio'] = 'Monitor of Null Output'
     return info
 
 
