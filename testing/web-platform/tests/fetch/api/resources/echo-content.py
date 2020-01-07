ab4d141326972a3729860aba5181f400d332ddda
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 20:55:30 +0000

    Bug 1351193 - Part 2: Prevent content sniffing of the response in original echo-content.py, r=nika
    The changes made to echo-content.py are the same as the ones made in Bug
    1435781.
    
    MozReview-Commit-ID: GEPJxuptBii
    
    UltraBlame original commit: 74219e6cc8b0baab1c2eb7c7127007e3b7bfcd33

--- testing/web-platform/tests/fetch/api/resources/echo-content.py
+++ testing/web-platform/tests/fetch/api/resources/echo-content.py
@@ -2,8 +2,9 @@ def main(request, response):
 
     headers = [("X-Request-Method", request.method),
                ("X-Request-Content-Length", request.headers.get("Content-Length", "NO")),
-               ("X-Request-Content-Type", request.headers.get("Content-Type", "NO"))]
-
+               ("X-Request-Content-Type", request.headers.get("Content-Type", "NO")),
+               
+               ("Content-Type", "text/plain")]
     content = request.body
 
     return headers, content
