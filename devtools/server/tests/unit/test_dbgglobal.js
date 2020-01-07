03388df7232586f2b8442c3102e18c4b793478d8
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 09:29:31 +0000

    Bug 1447944 - Remove devtool's check_except helper function in favour of Assert.throws. r=jdescottes.
    MozReview-Commit-ID: 8W0oZd2P505
    
    UltraBlame original commit: a064dac7fd392774535cadc6651d8d3c250ed120

--- devtools/server/tests/unit/test_dbgglobal.js
+++ devtools/server/tests/unit/test_dbgglobal.js
@@ -6,19 +6,27 @@
 function run_test() {
   
   
-  check_except(function() {
-    DebuggerServer.createListener();
-  });
-  check_except(DebuggerServer.closeAllListeners);
-  check_except(DebuggerServer.connectPipe);
+  Assert.throws(() => DebuggerServer.createListener(),
+    /DebuggerServer has not been initialized/,
+    "createListener should throw before it has been initialized");
+  Assert.throws(DebuggerServer.closeAllListeners,
+    /this is undefined/,
+    "closeAllListeners should throw before it has been initialized");
+  Assert.throws(DebuggerServer.connectPipe,
+    /this is undefined/,
+    "connectPipe should throw before it has been initialized");
 
   
   DebuggerServer.init();
 
   
   
-  check_except(DebuggerServer.closeAllListeners);
-  check_except(DebuggerServer.connectPipe);
+  Assert.throws(DebuggerServer.closeAllListeners,
+    /this is undefined/,
+    "closeAllListeners should throw if createRootActor hasn't been added");
+  Assert.throws(DebuggerServer.connectPipe,
+    /this is undefined/,
+    "closeAllListeners should throw if createRootActor hasn't been added");
 
   DebuggerServer.registerModule("xpcshell-test/testactors");
 
