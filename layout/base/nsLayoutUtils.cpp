9195a4c1d442bed4bb19e4f1f2f535e9df18abf2
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 05:22:41 +0000

    Backed out changeset 9eb8652389f2 (bug 1427635) for clipboard failures at dom/browser-element/mochitest/test_browserElement_oop_CopyPaste.html
    UltraBlame original commit: b3c010d9f5d7ec4e64246e425d271270ae78ce40

--- layout/base/nsLayoutUtils.cpp
+++ layout/base/nsLayoutUtils.cpp
@@ -1945,11 +1945,8 @@ nsLayoutUtils::DoCompareTreePosition(nsIFrame* aFrame1,
   }
 
   AutoTArray<nsIFrame*,20> frame1Ancestors;
-  
-  
-  
-  if (!FillAncestors(aFrame1, aCommonAncestor, &frame1Ancestors) &&
-      aCommonAncestor) {
+  if (aCommonAncestor &&
+      !FillAncestors(aFrame1, aCommonAncestor, &frame1Ancestors)) {
     
     
     return DoCompareTreePosition(aFrame1, aFrame2,
