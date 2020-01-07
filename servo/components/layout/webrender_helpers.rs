7b390688e0fd574160c8bbe17521818636e6d6ce
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 00:31:26 +0000

    servo: Merge #19414 - Update WR (AA on transformed box-shadows, resource leak fixes) (from glennw:update-wr-leaks); r=wafflespeanut
    Source-Repo: https://github.com/servo/servo
    Source-Revision: e64647188879b8269b62c1d5c8aa05732ab9d96d
    
    UltraBlame original commit: 83037c76a0781d85b17637ea5db98b55833a1907

--- servo/components/layout/webrender_helpers.rs
+++ servo/components/layout/webrender_helpers.rs
@@ -264,7 +264,6 @@ impl WebRenderDisplayItemConverter for DisplayItem {
             
             is_backface_visible: true,
             tag,
-            edge_aa_segment_mask: webrender_api::EdgeAaSegmentMask::empty(),
         }
     }
 
