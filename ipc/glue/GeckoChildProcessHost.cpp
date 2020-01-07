5bf5c77bf23fba54b29c45954e26a2b2281d9ed1
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 23:26:05 +0000

    Bug 1469691 - Fix intermittent launch failure caused by registering a Mach port too late. r=erahm
    UltraBlame original commit: 583f3742e6c918ab1824e6e351adc3317e9af020

--- ipc/glue/GeckoChildProcessHost.cpp
+++ ipc/glue/GeckoChildProcessHost.cpp
@@ -817,6 +817,12 @@ GeckoChildProcessHost::PerformAsyncLaunchInternal(std::vector<std::string>& aExt
 
   childArgv.push_back(childProcessType);
 
+# ifdef MOZ_WIDGET_COCOA
+  
+  
+  ReceivePort parent_recv_port(mach_connection_name.c_str());
+# endif 
+
 # if defined(MOZ_WIDGET_ANDROID)
   LaunchAndroidService(childProcessType, childArgv,
                        mLaunchOptions->fds_to_remap, &process);
@@ -834,7 +840,6 @@ GeckoChildProcessHost::PerformAsyncLaunchInternal(std::vector<std::string>& aExt
   const int kTimeoutMs = 10000;
 
   MachReceiveMessage child_message;
-  ReceivePort parent_recv_port(mach_connection_name.c_str());
   kern_return_t err = parent_recv_port.WaitForMessage(&child_message, kTimeoutMs);
   if (err != KERN_SUCCESS) {
     std::string errString = StringPrintf("0x%x %s", err, mach_error_string(err));
