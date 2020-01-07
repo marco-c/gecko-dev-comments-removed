6c0c71e3b48c54e147d442a00feec02bf84dc914
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 01:28:00 +0000

    servo: Merge #19613 - style: Workaround a likely valgrind false-positive (from emilio:slotted-list); r=jdm
    Source-Repo: https://github.com/servo/servo
    Source-Revision: fd6847fd764c423b8952d59179f5c08ebec185c5
    
    UltraBlame original commit: 3ce865c24dd7348a8ca710656c1e76918542897e

--- servo/components/style/selector_map.rs
+++ servo/components/style/selector_map.rs
@@ -433,7 +433,13 @@ fn specific_bucket_for<'a>(
         
         
         
-        Component::Slotted(ref selector) => find_bucket(selector.iter()),
+        
+        
+        
+        
+        
+        
+        
         _ => Bucket::Universal
     }
 }
