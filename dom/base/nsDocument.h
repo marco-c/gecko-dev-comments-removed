26b33fdbddcb18790e3e2ab03eace26cb3f026e3
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 08:10:30 +0000

    Bug 1444580: Devirtualize GetCurrentContentSink. r=smaug
    Another easy one.
    
    MozReview-Commit-ID: DJnTRYTiHwH
    
    UltraBlame original commit: 4b7ec8478e33a6779437ad6de366e9439c761fcd

--- dom/base/nsDocument.h
+++ dom/base/nsDocument.h
@@ -586,8 +586,6 @@ public:
   virtual void MaybePreconnect(nsIURI* uri,
                                mozilla::CORSMode aCORSMode) override;
 
-  virtual nsISupports* GetCurrentContentSink() override;
-
   
   void AsyncBlockOnload();
 
