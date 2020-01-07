8aa0c7fa22472726b14c493279743c329dd0c0a4
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 19:35:00 +0000

    Bug 1462341 - Properly report OOM inside BytecodeEmitter::emitSwitch. r=anba
    UltraBlame original commit: 69dbf5691b01d1ddd889ab1f511027ef9d1d1d65

---
+++ js/src/jit-test/tests/auto-regress/bug1462341.js
@@ -0,0 +1,15 @@
+
+
+if (!('oomTest' in this))
+  quit();
+
+loadFile(`
+  switch (0) {
+    case (-1):
+  }
+`);
+function loadFile(lfVarx) {
+  oomTest(function() {
+      let m = parseModule(lfVarx);
+  });
+}
