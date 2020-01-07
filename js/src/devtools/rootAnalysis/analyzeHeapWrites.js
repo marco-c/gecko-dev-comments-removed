5705f5e46e9c8b8d2051334f7cb39a479f21a4c6
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 03:23:51 +0000

    Bug 1429723: Remove static analysis special-case that is no longer relevant. r=sfink
    MozReview-Commit-ID: bqCHE0eOx
    
    UltraBlame original commit: a3642d94dc8b045eda9fa9836f67ba19eab3b1f8

--- js/src/devtools/rootAnalysis/analyzeHeapWrites.js
+++ js/src/devtools/rootAnalysis/analyzeHeapWrites.js
@@ -239,7 +239,6 @@ function treatAsSafeArgument(entry, varName, csuName)
         ["Gecko_CopyAlternateValuesFrom", "aDest", null],
         ["Gecko_CounterStyle_GetName", "aResult", null],
         ["Gecko_CounterStyle_GetSingleString", "aResult", null],
-        ["Gecko_EnsureMozBorderColors", "aBorder", null],
         ["Gecko_nsTArray_FontFamilyName_AppendNamed", "aNames", null],
         ["Gecko_nsTArray_FontFamilyName_AppendGeneric", "aNames", null],
     ];
@@ -1349,18 +1348,7 @@ function isSafeMemberPointer(containerType, memberType)
     if (memberType.Kind != 'Pointer')
         return false;
 
-    const {Type: {Kind: pointeeKind, Name: pointeeTypeName}} = memberType;
-
     
-    
-    
-    if (containerType == 'nsStyleBorder' &&
-        pointeeKind == 'CSU' &&
-        pointeeTypeName == 'nsBorderColors')
-    {
-        return true;
-    }
-
     return false;
 }
 
