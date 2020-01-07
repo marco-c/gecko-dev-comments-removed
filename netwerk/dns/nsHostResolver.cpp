55c12d4bed9d93837df08cae60d8d8b8259f13d8
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 23:45:47 +0000

    Bug 1471280 - Add new pref for how much longer resolver threads should remain idle r=bagder
    The new pref is "network.dns.resolver-thread-extra-idle-time-seconds"
    The default is 60 seconds. This means that threads will stay idle for an extra 60 seconds, after which they are shutdown.
    Setting the pref to 0 would preserve the behaviour before the threads were swiched to use nsThreadPool - meaning that they would be shutdown immediately after ThreadFunc completes.
    Setting the pref to -1 would keep the threads alive forever.
    
    MozReview-Commit-ID: CoUB5gan4MR
    
    UltraBlame original commit: d7788f2fd8a2d23d5cdd179115b2d7258d6d4f02

--- netwerk/dns/nsHostResolver.cpp
+++ netwerk/dns/nsHostResolver.cpp
@@ -509,6 +509,7 @@ nsHostRecord::RemoveOrRefresh()
 
 static const char kPrefGetTtl[] = "network.dns.get-ttl";
 static const char kPrefNativeIsLocalhost[] = "network.dns.native-is-localhost";
+static const char kPrefThreadIdleTime[] = "network.dns.resolver-thread-extra-idle-time-seconds";
 static bool sGetTtlEnabled = false;
 mozilla::Atomic<bool, mozilla::Relaxed> gNativeIsLocalhost;
 
@@ -594,9 +595,23 @@ nsHostResolver::Init()
     }
 #endif
 
+    
+    
+    int32_t poolTimeoutSecs = Preferences::GetInt(kPrefThreadIdleTime, 60);
+    uint32_t poolTimeoutMs;
+    if (poolTimeoutSecs < 0) {
+        
+        poolTimeoutMs = UINT32_MAX;
+    } else {
+        
+        poolTimeoutMs = mozilla::clamped<uint32_t>(poolTimeoutSecs * 1000,
+                                                   0, 3600 * 1000);
+    }
+
     nsCOMPtr<nsIThreadPool> threadPool = do_CreateInstance(NS_THREADPOOL_CONTRACTID);
     MOZ_ALWAYS_SUCCEEDS(threadPool->SetThreadLimit(MAX_RESOLVER_THREADS));
     MOZ_ALWAYS_SUCCEEDS(threadPool->SetIdleThreadLimit(MAX_RESOLVER_THREADS));
+    MOZ_ALWAYS_SUCCEEDS(threadPool->SetIdleThreadTimeout(poolTimeoutMs));
     MOZ_ALWAYS_SUCCEEDS(threadPool->SetThreadStackSize(nsIThreadManager::kThreadPoolStackSize));
     MOZ_ALWAYS_SUCCEEDS(threadPool->SetName(NS_LITERAL_CSTRING("DNS Resolver")));
     mResolverThreads = threadPool.forget();
