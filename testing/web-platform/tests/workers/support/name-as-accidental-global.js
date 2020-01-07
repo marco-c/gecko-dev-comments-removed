5951f47bc1569aa1a931bd1d1ea26ac20fb2559a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 06:12:32 +0000

    Bug 1439198 - Fix workers/name-property.html WPT test, r=qdot
    UltraBlame original commit: e4a67f7ddcde6cd99348e9104bd7ed07074da44a

--- testing/web-platform/tests/workers/support/name-as-accidental-global.js
+++ testing/web-platform/tests/workers/support/name-as-accidental-global.js
@@ -1,8 +1,9 @@
 "use strict";
+importScripts("/resources/testharness.js");
 
 var name = "something else";
 
 
-test(() => { }, "Declaring name as an accidental global must not cause a harness error");
+test(() => { }, `Declaring name as an accidental global must not cause a harness error for ${self.constructor.name}`);
 
 done();
