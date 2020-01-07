3ac73da8749f09eb91c996dc9319fa191f4e31f7
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 07:00:58 +0000

    Bug 1440177 - Part 1: Don't call GetLayerState from BuildContainerLayerFor as it recurses into child display items to find the answer. r=jnicol
    MozReview-Commit-ID: 3A4OBC5L6Ps
    
    UltraBlame original commit: ce353b632a3108600e45cd1c21786ea9fb21be7c

--- layout/painting/FrameLayerBuilder.cpp
+++ layout/painting/FrameLayerBuilder.cpp
@@ -5487,7 +5487,6 @@ ChooseScaleAndSetTransform(FrameLayerBuilder* aLayerBuilder,
                            const Matrix4x4* aTransform,
                            const ContainerLayerParameters& aIncomingScale,
                            ContainerLayer* aLayer,
-                           LayerState aState,
                            ContainerLayerParameters& aOutgoingScale)
 {
   nsIntPoint offset;
@@ -5506,7 +5505,7 @@ ChooseScaleAndSetTransform(FrameLayerBuilder* aLayerBuilder,
   }
   Matrix transform2d;
   if (aContainerFrame &&
-      (aState == LAYER_INACTIVE || aState == LAYER_SVG_EFFECTS) &&
+      aLayerBuilder->GetContainingPaintedLayerData() &&
       (!aTransform || (aTransform->Is2D(&transform2d) &&
                        !transform2d.HasNonTranslation()))) {
     
@@ -5687,13 +5686,7 @@ FrameLayerBuilder::BuildContainerLayerFor(nsDisplayListBuilder* aBuilder,
       return nullptr;
   }
 
-  LayerState layerState = aContainerItem ? aContainerItem->GetLayerState(aBuilder, aManager, aParameters) : LAYER_ACTIVE;
-  if (layerState == LAYER_INACTIVE &&
-      nsDisplayItem::ForceActiveLayers()) {
-    layerState = LAYER_ACTIVE;
-  }
-
-  if (aContainerItem && layerState == LAYER_ACTIVE_EMPTY) {
+  if (aContainerItem && aContainerItem->GetType() == DisplayItemType::TYPE_SCROLL_INFO_LAYER) {
     
     
     
@@ -5721,7 +5714,7 @@ FrameLayerBuilder::BuildContainerLayerFor(nsDisplayListBuilder* aBuilder,
                                   aContainerItem,
                                   bounds.Intersect(childrenVisible),
                                   aTransform, aParameters,
-                                  containerLayer, layerState, scaleParameters)) {
+                                  containerLayer, scaleParameters)) {
     return nullptr;
   }
 
