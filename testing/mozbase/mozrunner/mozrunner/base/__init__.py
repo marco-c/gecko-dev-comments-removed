c3c30e332b97a05cc0a426d40bd736f7e7a18671
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 12:37:30 +0000

    Bug 1445944 - [mozrunner] Create a base BlinkRuntimeRunner and add a ChromeRunner to the runners list r=rwood
    This allows consumers to bootstrap Chrome with mozrunner. For now the profile implementation
    is just an empty class but this will be expanded in a future commit.
    
    MozReview-Commit-ID: 1Z14FudH0JJ
    
    UltraBlame original commit: df5d226d1df3204b7fb4b975e7075a3a3e7a9775

--- testing/mozbase/mozrunner/mozrunner/base/__init__.py
+++ testing/mozbase/mozrunner/mozrunner/base/__init__.py
@@ -1,7 +1,6 @@
+
 from __future__ import absolute_import
 
 from .runner import BaseRunner
 from .device import DeviceRunner, FennecRunner
-from .browser import GeckoRuntimeRunner
-
-__all__ = ['BaseRunner', 'DeviceRunner', 'FennecRunner', 'GeckoRuntimeRunner']
+from .browser import GeckoRuntimeRunner, BlinkRuntimeRunner
