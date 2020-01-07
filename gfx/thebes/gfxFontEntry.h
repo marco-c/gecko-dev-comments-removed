3224f92ff622d7db9fc9b280e5e3a767f6b40a49
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 03:55:49 +0000

    Bug 1323743 - patch 1 - Add an API to gfxFontEntry to allow querying the available variation axes in a font face. (No actual implementation yet.) r=dholbert
    UltraBlame original commit: 625b879e0a93343591140c2ba4cf3a2afc614c1c

--- gfx/thebes/gfxFontEntry.h
+++ gfx/thebes/gfxFontEntry.h
@@ -11,6 +11,7 @@
 #include "gfxFontConstants.h"
 #include "gfxFontFeatures.h"
 #include "gfxFontUtils.h"
+#include "gfxFontVariations.h"
 #include "nsTArray.h"
 #include "nsTHashtable.h"
 #include "mozilla/HashFunctions.h"
@@ -343,6 +344,16 @@ public:
     };
 
     bool SupportsScriptInGSUB(const hb_tag_t* aScriptTags);
+
+    
+    
+    virtual bool HasVariations()
+    {
+        return false;
+    }
+    virtual void GetVariationAxes(nsTArray<gfxFontVariationAxis>& aVariationAxes)
+    {
+    }
 
     nsString         mName;
     nsString         mFamilyName;
--- gfx/thebes/gfxMacPlatformFontList.h
+++ gfx/thebes/gfxMacPlatformFontList.h
@@ -61,7 +61,7 @@ public:
 
     bool RequiresAATLayout() const { return mRequiresAAT; }
 
-    bool HasVariations();
+    bool HasVariations() override;
     bool IsCFF();
 
     
