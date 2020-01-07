3ff2951e43a71378b4823f1ecc9e9bc8303b21fd
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 20:55:01 +0000

    Bug 1466222 - Get rid of unused 'appName' config value and config entries for signature_verification_script. r=catlee
    for "Cleanup l10n mozharness config files."
    
    Differential Revision: https://phabricator.services.mozilla.com/D1530
    
    UltraBlame original commit: 2a373427f70806eaaa0db7c07870757116f65b45

--- testing/mozharness/configs/single_locale/try_android-api-16.py
+++ testing/mozharness/configs/single_locale/try_android-api-16.py
@@ -32,7 +32,6 @@ config = {
         "MOZ_UPDATE_CHANNEL": "try", 
     },
     "upload_branch": "%s-android-api-16" % BRANCH,
-    "signature_verification_script": "tools/release/signing/verify-android-signature.sh",
     "platform": "android", 
 
     
