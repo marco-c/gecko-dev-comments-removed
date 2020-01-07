83adbe88a0872363a030ba141a56f0bd84d62986
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 05:12:22 +0000

    Bug 1432716 - Group the "Show search suggestions ahead of browsing history in address bar results" checkbox with other search suggestion checkboxes. ui=shorlander r=adw
    MozReview-Commit-ID: 6Xt8MixhV3x
    
    UltraBlame original commit: 98b4549200aafb12ff8a29af5620fe3bba11a0d1

--- browser/components/preferences/in-content/tests/browser_searchShowSuggestionsFirst.js
+++ browser/components/preferences/in-content/tests/browser_searchShowSuggestionsFirst.js
@@ -7,6 +7,8 @@ const CHECKBOX_ID = "showSearchSuggestionsFirstCheckbox";
 
 
 add_task(async function openWithSearchSuggestionsShownFirst() {
+  await SpecialPowers.pushPrefEnv({set: [["browser.urlbar.suggest.searches", true]]});
+
   
   
   Assert.equal(Services.prefs.getCharPref(PREF_NAME, ""), "",
