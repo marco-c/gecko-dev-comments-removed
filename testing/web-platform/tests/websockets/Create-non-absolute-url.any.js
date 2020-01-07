a7e65ce62692b64760852359a5090a9af6e43903
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 17:34:41 +0000

    Bug 1456463 [wpt PR 10598] - Convert WebSocket tests to .any.js format, a=testonly
    Automatic update from web-platform-testsConvert WebSocket tests to .any.js format (#10598)
    
    Take 2 (see #5148 for take 1), fixes #2557.
    --
    
    wpt-commits: 28b62c1c17ac678ea2c21bf09fafe346731c0fca
    wpt-pr: 10598
    
    UltraBlame original commit: 1e379aba0d93b62931a9cd2426634229853573a5

---
+++ testing/web-platform/tests/websockets/Create-non-absolute-url.any.js
@@ -0,0 +1,6 @@
+
+
+        test(function () {
+            var wsocket;
+            assert_throws("SYNTAX_ERR", function () { wsocket = CreateWebSocketNonAbsolute() });
+        }, "W3C WebSocket API - Create WebSocket - Pass a non absolute URL - SYNTAX_ERR is thrown")
