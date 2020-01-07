b342b4ec28ea64a6d4f2f9ae4f34cf6aef374157
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 19:27:07 +0000

    Bug 1457083 - part 4: Replace mHTMLEditor in HTMLEditRules with HTMLEditorRef() r=m_kato
    Although, this patch removes first check of mHTMLEditor in each method.  If
    this causes some security issues, we should add now.  However, automated
    tests don't indicate it.
    
    Anyway, it should be fixed by bug 1454900 in same cycle.
    
    MozReview-Commit-ID: 3LAtOQHyR5J
    
    UltraBlame original commit: c469a8b14bf4d221375b51255e801039597d7e39

--- editor/libeditor/TextEditRulesBidi.cpp
+++ editor/libeditor/TextEditRulesBidi.cpp
@@ -51,17 +51,17 @@ TextEditRules::CheckBidiLevelForDeletion(
     return NS_ERROR_FAILURE;
   }
 
-  nsBidiLevel levelBefore;
-  nsBidiLevel levelAfter;
-  RefPtr<nsFrameSelection> frameSelection = aSelection->GetFrameSelection();
-  NS_ENSURE_TRUE(frameSelection, NS_ERROR_NULL_POINTER);
+  RefPtr<nsFrameSelection> frameSelection = SelectionRef().GetFrameSelection();
+  if (NS_WARN_IF(!frameSelection)) {
+    return NS_ERROR_FAILURE;
+  }
 
   nsPrevNextBidiLevels levels = frameSelection->
     GetPrevNextBidiLevels(aSelectionPoint.GetContainerAsContent(),
                           aSelectionPoint.Offset(), true);
 
-  levelBefore = levels.mLevelBefore;
-  levelAfter = levels.mLevelAfter;
+  nsBidiLevel levelBefore = levels.mLevelBefore;
+  nsBidiLevel levelAfter = levels.mLevelAfter;
 
   nsBidiLevel currentCaretLevel = frameSelection->GetCaretBidiLevel();
 
@@ -87,6 +87,8 @@ TextEditRules::CheckBidiLevelForDeletion(
 void
 TextEditRules::UndefineCaretBidiLevel(Selection* aSelection)
 {
+  MOZ_ASSERT(IsEditorDataAvailable());
+
   
 
 
@@ -95,7 +97,7 @@ TextEditRules::UndefineCaretBidiLevel(Selection* aSelection)
 
 
 
-  RefPtr<nsFrameSelection> frameSelection = aSelection->GetFrameSelection();
+  RefPtr<nsFrameSelection> frameSelection = SelectionRef().GetFrameSelection();
   if (frameSelection) {
     frameSelection->UndefineCaretBidiLevel();
   }
