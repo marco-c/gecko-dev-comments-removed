c8099d42f0c0a6cd4e1e77fad0ba080ad115c799
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 07:07:45 +0000

    Bug 1423628: Let not processing native events ride the trains on Windows. r=jimm
    UltraBlame original commit: 818464a2fb0e51c418d5ccbdb9868bb921b5ff7f

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -223,11 +223,7 @@ pref("dom.keyboardevent.dispatch_during_composition", false);
 
 
 
-#ifdef EARLY_BETA_OR_EARLIER
-pref("dom.keyboardevent.keypress.dispatch_non_printable_keys_only_system_group_in_content", true);
-#else
 pref("dom.keyboardevent.keypress.dispatch_non_printable_keys_only_system_group_in_content", false);
-#endif
 
 
 pref("dom.webmidi.enabled", false);
@@ -3394,7 +3390,7 @@ pref("dom.ipc.processCount.file", 1);
 pref("dom.ipc.processCount.extension", 1);
 
 
-#if defined(XP_WIN) && defined(NIGHTLY_BUILD)
+#if defined(XP_WIN)
 pref("dom.ipc.useNativeEventProcessing.content", false);
 #else
 pref("dom.ipc.useNativeEventProcessing.content", true);
@@ -5071,15 +5067,6 @@ pref("geo.wifi.xhr.timeout", 60000);
 
 
 pref("device.sensors.enabled", true);
-pref("device.sensors.orientation.enabled", true);
-pref("device.sensors.motion.enabled", true);
-#ifdef EARLY_BETA_OR_EARLIER
-pref("device.sensors.proximity.enabled", false);
-pref("device.sensors.ambientLight.enabled", false);
-#else
-pref("device.sensors.proximity.enabled", true);
-pref("device.sensors.ambientLight.enabled", true);
-#endif
 
 
 pref("device.storage.enabled", false);
