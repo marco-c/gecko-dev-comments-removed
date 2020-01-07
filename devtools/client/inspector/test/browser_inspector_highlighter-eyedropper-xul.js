8efbdcbbfcaad8ceac74924a2b25d6e168aa48f7
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 22:44:28 +0000

    Bug 1448126 - Part 1 - Remove tests for the "scale" binding. r=bgrins
    MozReview-Commit-ID: 9LxA3ERl89r
    
    UltraBlame original commit: 14cc2c33cf998501d7114f9b6c0f7ef5569e4c92

--- devtools/client/inspector/test/browser_inspector_highlighter-eyedropper-xul.js
+++ devtools/client/inspector/test/browser_inspector_highlighter-eyedropper-xul.js
@@ -18,7 +18,7 @@ add_task(async function() {
   ok(isDisabled(button), "The button is hidden in the toolbar");
 
   info("Check the color picker");
-  await selectNode("#scale", inspector);
+  await selectNode("#tree", inspector);
 
   
   let ruleView = inspector.getPanel("ruleview").view;
