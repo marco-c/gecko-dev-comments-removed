faaa9ee985ad881fd0dcf2b679fc3af6fe84faa7
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 01:19:58 +0000

    Merge mozilla-central to autoland r=merge
    UltraBlame original commit: deb775eb35bb3e157e89646ede5416b5f00cf8ee

--- dom/base/nsDocument.cpp
+++ dom/base/nsDocument.cpp
@@ -2345,15 +2345,17 @@ nsDocument::ResetToURI(nsIURI *aURI, nsILoadGroup *aLoadGroup,
     
     
 
-    
-    
-    nsCOMPtr<nsIRequestContextService> rcsvc =
-      do_GetService("@mozilla.org/network/request-context-service;1");
-    if (rcsvc) {
-      nsCOMPtr<nsIRequestContext> rc;
-      rcsvc->GetRequestContextFromLoadGroup(aLoadGroup, getter_AddRefs(rc));
-      if (rc) {
-        rc->BeginLoad();
+    if (IsContentDocument()) {
+      
+      
+      nsCOMPtr<nsIRequestContextService> rcsvc =
+        do_GetService("@mozilla.org/network/request-context-service;1");
+      if (rcsvc) {
+        nsCOMPtr<nsIRequestContext> rc;
+        rcsvc->GetRequestContextFromLoadGroup(aLoadGroup, getter_AddRefs(rc));
+        if (rc) {
+          rc->BeginLoad();
+        }
       }
     }
   }
