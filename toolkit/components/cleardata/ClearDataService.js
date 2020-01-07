0fdbecec105f76e79a691d6481335bb7bddc04f1
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 22:18:05 +0000

    Bug 1268889 - Implement Clear-Site-Data header - part 1, r=mayhemer
    UltraBlame original commit: dd3b7047ea6f00c7488329b50842623f1bbdd3fb

--- toolkit/components/cleardata/ClearDataService.js
+++ toolkit/components/cleardata/ClearDataService.js
@@ -703,7 +703,7 @@ ClearDataService.prototype = Object.freeze({
     }
 
     return this._deleteInternal(aFlags, aCallback, aCleaner => {
-      if ("deleteByPrincipal" in aCleaner && aCleaner.deleteByPrincipal) {
+      if (aCleaner.deleteByPrincipal) {
         return aCleaner.deleteByPrincipal(aPrincipal);
       }
       
