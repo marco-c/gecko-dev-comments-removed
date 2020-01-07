22adc5385c17c2be113247683a37d443637c227c
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 09:37:49 +0000

    servo: Merge #20419 - Update mozjs (from emilio:mozjs-up); r=jdm
    Peeks up https://github.com/servo/rust-mozjs/pull/408.
    
    Source-Repo: https://github.com/servo/servo
    Source-Revision: b8279b376bb4b6118c5a65fa6aeaf5a08c6e953b
    
    UltraBlame original commit: 870708dc30ed7c76e8b7b0516ac51c9994d94389

--- servo/components/script/dom/dommatrix.rs
+++ servo/components/script/dom/dommatrix.rs
@@ -61,8 +61,8 @@ impl DOMMatrix {
     
     pub fn FromFloat32Array(
         global: &GlobalScope,
-        mut array: CustomAutoRooterGuard<Float32Array>)
-        -> Fallible<DomRoot<DOMMatrix>> {
+        array: CustomAutoRooterGuard<Float32Array>,
+    ) -> Fallible<DomRoot<DOMMatrix>> {
         let vec: Vec<f64> = array.to_vec().iter().map(|&x| x as f64).collect();
         DOMMatrix::Constructor_(global, vec)
     }
@@ -70,11 +70,11 @@ impl DOMMatrix {
     
     pub fn FromFloat64Array(
         global: &GlobalScope,
-        mut array: CustomAutoRooterGuard<Float64Array>)
-        -> Fallible<DomRoot<DOMMatrix>> {
-            let vec: Vec<f64> = array.to_vec();
-            DOMMatrix::Constructor_(global, vec)
-        }
+        array: CustomAutoRooterGuard<Float64Array>,
+    ) -> Fallible<DomRoot<DOMMatrix>> {
+        let vec: Vec<f64> = array.to_vec();
+        DOMMatrix::Constructor_(global, vec)
+    }
 }
 
 impl DOMMatrixMethods for DOMMatrix {
