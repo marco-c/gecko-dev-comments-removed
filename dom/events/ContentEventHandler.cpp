fdd631de5fe374df8c0403a1246a3665aa0ffd6c
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 02:11:16 +0000

    Bug 1419803 - Use GetComposedDoc instead of GetUncomposedDoc in ContentEventHandler. r=smaug
    UltraBlame original commit: 2ba9bb352b5d0d72644bf8498e332a812e2cff1b

--- dom/events/ContentEventHandler.cpp
+++ dom/events/ContentEventHandler.cpp
@@ -309,11 +309,11 @@ ContentEventHandler::InitRootContent(Selection* aNormalSelection)
   }
 
   
-  if (NS_WARN_IF(startNode->GetUncomposedDoc() != mPresShell->GetDocument())) {
+  if (NS_WARN_IF(startNode->GetComposedDoc() != mPresShell->GetDocument())) {
     return NS_ERROR_FAILURE;
   }
 
-  NS_ASSERTION(startNode->GetUncomposedDoc() == endNode->GetUncomposedDoc(),
+  NS_ASSERTION(startNode->GetComposedDoc() == endNode->GetComposedDoc(),
                "firstNormalSelectionRange crosses the document boundary");
 
   mRootContent = startNode->GetSelectionRootContent(mPresShell);
