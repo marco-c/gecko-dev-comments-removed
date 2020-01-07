00bf9770b6d2ff1d805e270e2250eb7661696a67
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 06:45:45 +0000

    Bug 1441056 - Get D3D11 device from EGLDisplay in RenderCompositorANGLE::Initialize() r=nical
    UltraBlame original commit: eb8cb1ab5f49e86485451b9b14d07318829f576c

--- gfx/webrender_bindings/RenderCompositorANGLE.cpp
+++ gfx/webrender_bindings/RenderCompositorANGLE.cpp
@@ -45,10 +45,37 @@ RenderCompositorANGLE::~RenderCompositorANGLE()
   MOZ_ASSERT(!mEGLSurface);
 }
 
+ID3D11Device*
+RenderCompositorANGLE::GetDeviceOfEGLDisplay()
+{
+  const auto& egl = &gl::sEGLLibrary;
+
+  
+  EGLDeviceEXT eglDevice = nullptr;
+  egl->fQueryDisplayAttribEXT(egl->Display(), LOCAL_EGL_DEVICE_EXT, (EGLAttrib*)&eglDevice);
+  MOZ_ASSERT(eglDevice);
+  ID3D11Device* device = nullptr;
+  egl->fQueryDeviceAttribEXT(eglDevice, LOCAL_EGL_D3D11_DEVICE_ANGLE, (EGLAttrib*)&device);
+  if (!device) {
+    gfxCriticalNote << "Failed to get D3D11Device from EGLDisplay";
+    return false;
+  }
+  return device;
+}
+
 bool
 RenderCompositorANGLE::Initialize()
 {
-  mDevice = gfx::DeviceManagerDx::Get()->GetCompositorDevice();
+  const auto& egl = &gl::sEGLLibrary;
+
+  nsCString discardFailureId;
+  if (!egl->EnsureInitialized( true, &discardFailureId)) {
+    gfxCriticalNote << "Failed to load EGL library: " << discardFailureId.get();
+    return false;
+  }
+
+  mDevice = GetDeviceOfEGLDisplay();
+
   if (!mDevice) {
     gfxCriticalNote << "[D3D11] failed to get compositor device.";
     return false;
@@ -142,7 +169,6 @@ RenderCompositorANGLE::Initialize()
 
   
   
-  nsCString discardFailureId;
   mGL = gl::GLContextProviderEGL::CreateHeadless(flags, &discardFailureId);
   if (!mGL || !mGL->IsANGLE()) {
     gfxCriticalNote << "Failed ANGLE GL context creation for WebRender: " << gfx::hexa(mGL.get());
