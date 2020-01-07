c0798796abfca3b084452107b182ee17721e2dea
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 22:34:46 +0000

    Bug 1463608 - Add test helpers for payment address and cards. r=jaws
    * addSampleAddressesAndBasicCard now accepts arrays of addresses and card, but with no args defaults to its original behavior
    * addAddressRecord and addCardRecord helpers
    * New helpers for each step in the sequence of clicking through to add a new address
    
    MozReview-Commit-ID: Dto5MSekJTV
    
    UltraBlame original commit: a7d3beb6afb8002b8b689e140c9b4c1e98976153

--- browser/components/payments/res/containers/basic-card-form.js
+++ browser/components/payments/res/containers/basic-card-form.js
@@ -45,6 +45,7 @@ export default class BasicCardForm extends PaymentStateSubscriberMixin(HTMLEleme
     this.saveButton.addEventListener("click", this);
 
     this.persistCheckbox = new LabelledCheckbox();
+    this.persistCheckbox.className = "persist-checkbox";
 
     
     let url = "formautofill/editCreditCard.xhtml";
