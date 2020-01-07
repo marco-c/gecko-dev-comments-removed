9bf05e9fefc88f5dd720177d90c1458531151cfa
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 13:26:55 +0000

    Bug 1436812 P6 Move the NS_CheckContentLoadPolicy() from ServiceWorkerManager to ServiceWorkerContainer. r=baku
    UltraBlame original commit: 2f27625fd5117e193980882ef5869be79cd39d30

--- dom/serviceworkers/ServiceWorkerContainer.cpp
+++ dom/serviceworkers/ServiceWorkerContainer.cpp
@@ -6,6 +6,7 @@
 
 #include "ServiceWorkerContainer.h"
 
+#include "nsContentPolicyUtils.h"
 #include "nsContentSecurityManager.h"
 #include "nsContentUtils.h"
 #include "nsIDocument.h"
@@ -19,6 +20,7 @@
 #include "nsCycleCollectionParticipant.h"
 #include "nsServiceManagerUtils.h"
 
+#include "mozilla/LoadInfo.h"
 #include "mozilla/dom/DOMPrefs.h"
 #include "mozilla/dom/Navigator.h"
 #include "mozilla/dom/Promise.h"
@@ -297,6 +299,36 @@ ServiceWorkerContainer::Register(const nsAString& aScriptURL,
     aRv.Throw(NS_ERROR_DOM_SECURITY_ERR);
     return nullptr;
   }
+
+  
+  
+  
+  
+  
+  
+  nsCOMPtr<nsILoadInfo> secCheckLoadInfo =
+    new LoadInfo(doc->NodePrincipal(), 
+                 doc->NodePrincipal(), 
+                 doc,                  
+                 nsILoadInfo::SEC_ONLY_FOR_EXPLICIT_CONTENTSEC_CHECK,
+                 nsIContentPolicy::TYPE_INTERNAL_SERVICE_WORKER);
+
+  
+  int16_t decision = nsIContentPolicy::ACCEPT;
+  rv = NS_CheckContentLoadPolicy(scriptURI,
+                                 secCheckLoadInfo,
+                                 NS_LITERAL_CSTRING("application/javascript"),
+                                 &decision);
+  if (NS_FAILED(rv)) {
+    aRv.Throw(NS_ERROR_DOM_SECURITY_ERR);
+    return nullptr;
+
+  }
+  if (NS_WARN_IF(decision != nsIContentPolicy::ACCEPT)) {
+    aRv.Throw(NS_ERROR_CONTENT_BLOCKED);
+    return nullptr;
+  }
+
 
   
   
