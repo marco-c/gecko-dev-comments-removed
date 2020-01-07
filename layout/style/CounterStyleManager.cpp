3811530f099fcbed8ad78f3c95faea439caf8c00
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 09:37:57 +0000

    Bug 1448436 - Remove an unneeded workaround for MSVC <2017 from CounterStyleManager.cpp. r=xidorn
    UltraBlame original commit: e81fdb3b711feb859cb780f6d20a291a34c43eaa

--- layout/style/CounterStyleManager.cpp
+++ layout/style/CounterStyleManager.cpp
@@ -956,29 +956,19 @@ BuiltinCounterStyle::GetInitialCounterText(CounterValue aOrdinal,
   }
 }
 
-
-
-
-#if !defined(_MSC_VER) || _MSC_VER >= 1910
-static constexpr BuiltinCounterStyle gBuiltinStyleTable[] =
-#else
-static const BuiltinCounterStyle gBuiltinStyleTable[] =
-#endif
-{
+static constexpr BuiltinCounterStyle gBuiltinStyleTable[] = {
 #define BUILTIN_COUNTER_STYLE(value_, atom_) \
   { NS_STYLE_LIST_STYLE_ ## value_, &nsGkAtoms::atom_ },
 #include "BuiltinCounterStyleList.h"
 #undef BUILTIN_COUNTER_STYLE
 };
 
-#if !defined(_MSC_VER) || _MSC_VER >= 1910
 #define BUILTIN_COUNTER_STYLE(value_, atom_) \
   static_assert(gBuiltinStyleTable[NS_STYLE_LIST_STYLE_ ## value_].GetStyle() \
                 == NS_STYLE_LIST_STYLE_ ## value_, "Builtin counter style " \
                 #atom_ " has unmatched index and value.");
 #include "BuiltinCounterStyleList.h"
 #undef BUILTIN_COUNTER_STYLE
-#endif
 
 class DependentBuiltinCounterStyle final : public BuiltinCounterStyle
 {
