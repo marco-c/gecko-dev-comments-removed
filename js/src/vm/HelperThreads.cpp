4e287b820a2d65fa430d2256cae24f135e257f95
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 17:52:07 +0000

    Bug 1458456 part 3a - Notify main thread after finishing wasm tasks to fix a race with waitForAllThreads. r=lth
    UltraBlame original commit: f3ffc928dddd32983a2f623b3a254606d4dc6406

--- js/src/vm/HelperThreads.cpp
+++ js/src/vm/HelperThreads.cpp
@@ -1828,8 +1828,11 @@ HelperThread::handleWasmWorkload(AutoLockHelperThreadState& locked, wasm::Compil
         wasm::ExecuteCompileTaskFromHelperThread(task);
     }
 
-    
     currentTask.reset();
+
+    
+    
+    HelperThreadState().notifyAll(GlobalHelperThreadState::CONSUMER, locked);
 }
 
 void
@@ -1846,14 +1849,14 @@ HelperThread::handleWasmTier2GeneratorWorkload(AutoLockHelperThreadState& locked
         task->execute();
     }
 
+    currentTask.reset();
+    js_delete(task);
+
     
     
     
     HelperThreadState().incWasmTier2GeneratorsFinished(locked);
     HelperThreadState().notifyAll(GlobalHelperThreadState::CONSUMER, locked);
-
-    js_delete(task);
-    currentTask.reset();
 }
 
 void
@@ -1871,8 +1874,11 @@ HelperThread::handlePromiseHelperTaskWorkload(AutoLockHelperThreadState& locked)
         task->dispatchResolveAndDestroy();
     }
 
-    
     currentTask.reset();
+
+    
+    
+    HelperThreadState().notifyAll(GlobalHelperThreadState::CONSUMER, locked);
 }
 
 void
