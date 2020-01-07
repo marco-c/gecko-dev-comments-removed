88720107f04d1101b60ee7f952a15d2d408bc299
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 21:06:14 +0000

    Bug 1464096 - 3. Remove SessionTextInput.isInputActive; r=esawin
    The SessionTextInput.isInputActive API is prone to races. Its
    functionality can be replicated through the
    SessionTextInput.Delegate.restartInput callback, so it's okay to remove
    it.
    
    MozReview-Commit-ID: GFYjbH8cQv0
    
    UltraBlame original commit: 20c2e0dcc01ab9ab2ff54578d4dbaa4eab82e7c9

--- mobile/android/geckoview/src/main/java/org/mozilla/geckoview/GeckoInputConnection.java
+++ mobile/android/geckoview/src/main/java/org/mozilla/geckoview/GeckoInputConnection.java
@@ -590,7 +590,7 @@ import java.lang.reflect.Proxy;
     @Override
     public boolean sendKeyEvent(@NonNull KeyEvent event) {
         event = translateKey(event.getKeyCode(), event);
-        mEditableClient.sendKeyEvent(getView(), isInputActive(), event.getAction(), event);
+        mEditableClient.sendKeyEvent(getView(), event.getAction(), event);
         return false; 
     }
 
@@ -635,12 +635,6 @@ import java.lang.reflect.Proxy;
                 }
                 break;
         }
-    }
-
-    @Override 
-    public synchronized boolean isInputActive() {
-        
-        return mIMEState != IME_STATE_DISABLED;
     }
 
     @Override 
