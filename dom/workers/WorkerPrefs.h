acf5d4f6ac21d3fc5feefe2a2198c6ca8ae11ec4
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 01:04:10 +0000

    Bug 1419771 - Introduce DOMPreferences, a thread-safe access to preferences for DOM - part 10 - StorageManager enabled, r=asuth
    UltraBlame original commit: 5e44aeda4196494f6ac6114faa38d8091a136329

--- dom/workers/WorkerPrefs.h
+++ dom/workers/WorkerPrefs.h
@@ -21,7 +21,6 @@
 
 
 
-WORKER_SIMPLE_PREF("dom.storageManager.enabled", StorageManagerEnabled, STORAGEMANAGER_ENABLED)
 WORKER_SIMPLE_PREF("dom.promise_rejection_events.enabled", PromiseRejectionEventsEnabled, PROMISE_REJECTION_EVENTS_ENABLED)
 WORKER_SIMPLE_PREF("dom.push.enabled", PushEnabled, PUSH_ENABLED)
 WORKER_SIMPLE_PREF("dom.streams.enabled", StreamsEnabled, STREAMS_ENABLED)
