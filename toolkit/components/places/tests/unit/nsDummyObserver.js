7d64212fb969d2721c19ceac92211c9f9f15b695
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 02:24:38 +0000

    Bug 1421703 - Fix tests to use onVisits r=mak
    Fairly straightforward. Fixes the tests that were broken due to
    the changes introduced in part 1.
    
    MozReview-Commit-ID: GbZ9ZpmG9nE
    
    UltraBlame original commit: f228865f492ca24b82720fc40fdb00b8b0e8c10c

--- toolkit/components/places/tests/unit/nsDummyObserver.js
+++ toolkit/components/places/tests/unit/nsDummyObserver.js
@@ -17,7 +17,7 @@ DummyObserver.prototype = {
   
   onBeginUpdateBatch() {},
   onEndUpdateBatch() {},
-  onVisit(aURI, aVisitID, aTime, aSessionID, aReferringID, aTransitionType) {
+  onVisits(aVisits) {
     Services.obs.notifyObservers(null, "dummy-observer-visited");
   },
   onTitleChanged() {},
