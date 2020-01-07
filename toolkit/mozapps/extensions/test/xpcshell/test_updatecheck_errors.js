20e9a77440dd729ac6969f260e542255e10b07e2
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 11:04:50 +0000

    Bug 1449255: Part 22 - Rename test_bug570173 to test_updatecheck_errors.js and modernize. r=aswan
    MozReview-Commit-ID: 2sN8JzFdEfy
    
    UltraBlame original commit: 33564c71bfdafd8b5181e63f1150de49e706eee5

---
+++ toolkit/mozapps/extensions/test/xpcshell/test_updatecheck_errors.js
@@ -0,0 +1,52 @@
+
+
+
+
+
+
+
+Services.prefs.setBoolPref("extensions.checkUpdateSecurity", false);
+
+var testserver;
+const profileDir = gProfD.clone();
+profileDir.append("extensions");
+
+add_task(async function setup() {
+  createAppInfo("xpcshell@tests.mozilla.org", "XPCShell", "1", "1.9.2");
+
+  
+  testserver = AddonTestUtils.createHttpServer({hosts: ["example.com"]});
+  testserver.registerDirectory("/data/", do_get_file("data"));
+  testserver.registerDirectory("/addons/", do_get_file("addons"));
+
+  await promiseStartupManager();
+});
+
+
+add_task(async function() {
+  let {addon} = await promiseInstallXPI({
+    id: "addon1@tests.mozilla.org",
+    version: "1.0",
+    updateURL: `http://example.com/data/test_missing.json`,
+    targetApplications: [{
+      id: "xpcshell@tests.mozilla.org",
+      minVersion: "1",
+      maxVersion: "1"
+    }],
+    name: "Test Addon 1",
+    bootstrap: "true",
+  });
+
+  equal(addon.version, "1.0");
+
+  
+  let update = await promiseFindAddonUpdates(addon, AddonManager.UPDATE_WHEN_USER_REQUESTED)
+    .catch(e => e);
+
+  ok(!update.compatibilityUpdate, "not expecting a compatibility update");
+  ok(!update.updateAvailable, "not expecting a compatibility update");
+
+  equal(update.error, AddonManager.UPDATE_STATUS_DOWNLOAD_ERROR);
+
+  addon.uninstall();
+});
