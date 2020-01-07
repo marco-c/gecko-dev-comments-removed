7c90d9d9314206b17ee68e03969a257e710ee269
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 17:17:53 +0000

    Bug 1456274 - Remove local progress listener in DevTools target. r=ochameau
    DevTools targets have used a local progress listener for a long time to track
    tab navigation.  However, this is redundant with server side code that does the
    same thing.
    
    Removing this from the target reduces differences between local and remote
    debugging.  It also simplifies one piece of the target, which is a massively
    twisted module.
    
    MozReview-Commit-ID: E7lm4GUFZQO
    
    UltraBlame original commit: 7cf34fe2036ec2ac76111b54e409aa98a39232e6

--- devtools/client/framework/target.js
+++ devtools/client/framework/target.js
@@ -145,8 +145,6 @@ function TabTarget(tab) {
 exports.TabTarget = TabTarget;
 
 TabTarget.prototype = {
-  _webProgressListener: null,
-
   
 
 
@@ -496,8 +494,6 @@ TabTarget.prototype = {
 
 
   _setupListeners: function() {
-    this._webProgressListener = new TabWebProgressListener(this);
-    this.tab.linkedBrowser.addProgressListener(this._webProgressListener);
     this.tab.addEventListener("TabClose", this);
     this.tab.parentNode.addEventListener("TabSelect", this);
     this.tab.ownerDocument.defaultView.addEventListener("unload", this);
@@ -508,10 +504,6 @@ TabTarget.prototype = {
 
 
   _teardownListeners: function() {
-    if (this._webProgressListener) {
-      this._webProgressListener.destroy();
-    }
-
     this._tab.ownerDocument.defaultView.removeEventListener("unload", this);
     this._tab.removeEventListener("TabClose", this);
     this._tab.parentNode.removeEventListener("TabSelect", this);
@@ -758,81 +750,6 @@ TabTarget.prototype = {
       this.client.request(packet);
     }
   },
-};
-
-
-
-
-
-
-
-function TabWebProgressListener(target) {
-  this.target = target;
-}
-
-TabWebProgressListener.prototype = {
-  target: null,
-
-  QueryInterface: XPCOMUtils.generateQI([Ci.nsIWebProgressListener,
-                                         Ci.nsISupportsWeakReference]),
-
-  onStateChange: function(progress, request, flag) {
-    let isStart = flag & Ci.nsIWebProgressListener.STATE_START;
-    let isDocument = flag & Ci.nsIWebProgressListener.STATE_IS_DOCUMENT;
-    let isNetwork = flag & Ci.nsIWebProgressListener.STATE_IS_NETWORK;
-    let isRequest = flag & Ci.nsIWebProgressListener.STATE_IS_REQUEST;
-
-    
-    if (!isStart || !isDocument || !isRequest || !isNetwork) {
-      return;
-    }
-
-    
-    if (progress.isTopLevel) {
-      
-      
-      if (this.target._client) {
-        this.target._navRequest = request;
-      } else {
-        this.target.emit("will-navigate", request);
-      }
-    }
-  },
-
-  onProgressChange: function() {},
-  onSecurityChange: function() {},
-  onStatusChange: function() {},
-
-  onLocationChange: function(webProgress, request, URI, flags) {
-    if (this.target &&
-        !(flags & Ci.nsIWebProgressListener.LOCATION_CHANGE_SAME_DOCUMENT)) {
-      let window = webProgress.DOMWindow;
-      
-      
-      if (this.target._client) {
-        this.target._navWindow = window;
-      } else {
-        this.target.emit("navigate", window);
-      }
-    }
-  },
-
-  
-
-
-  destroy: function() {
-    if (this.target.tab) {
-      try {
-        this.target.tab.linkedBrowser.removeProgressListener(this);
-      } catch (ex) {
-        
-      }
-    }
-    this.target._webProgressListener = null;
-    this.target._navRequest = null;
-    this.target._navWindow = null;
-    this.target = null;
-  }
 };
 
 function WorkerTarget(workerClient) {
