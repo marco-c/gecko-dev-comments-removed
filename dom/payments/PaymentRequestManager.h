f6356fe46d947551aa5609cc1326a62b5ec720b8
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 22:06:51 +0000

    Bug 1442453 - Pass objects around instead of string IDs in the child. r=baku
    The existing code only passed strings around to identify PaymentRequest
    objects. That meant that we were constantly having to do hashtable lookups to
    find the corresponding object. This patch just uses the single map that IPDL
    maintains for us and passes objects around, obviating the need for more
    hashtable lookups.
    
    MozReview-Commit-ID: 6BBonrc6q4x
    
    UltraBlame original commit: df705bf3c62f0134268adc689cc23e47c1deef10

--- dom/payments/PaymentRequestManager.h
+++ dom/payments/PaymentRequestManager.h
@@ -32,9 +32,6 @@ public:
 
   static already_AddRefed<PaymentRequestManager> GetSingleton();
 
-  already_AddRefed<PaymentRequest>
-  GetPaymentRequestById(const nsAString& aRequestId);
-
   
 
 
@@ -55,19 +52,17 @@ public:
   nsresult CompletePayment(PaymentRequest* aRequest,
                            const PaymentComplete& aComplete);
   nsresult UpdatePayment(JSContext* aCx,
-                         const nsAString& aRequestId,
+                         PaymentRequest* aRequest,
                          const PaymentDetailsUpdate& aDetails,
                          bool aRequestShipping,
                          bool aDeferredShow);
 
-  nsresult RespondPayment(const IPCPaymentActionResponse& aResponse);
-  nsresult ChangeShippingAddress(const nsAString& aRequestId,
+  nsresult RespondPayment(PaymentRequest* aRequest,
+                          const IPCPaymentActionResponse& aResponse);
+  nsresult ChangeShippingAddress(PaymentRequest* aRequest,
                                  const IPCPaymentAddress& aAddress);
-  nsresult ChangeShippingOption(const nsAString& aRequestId,
+  nsresult ChangeShippingOption(PaymentRequest* aRequest,
                                 const nsAString& aOption);
-
-  nsresult
-  ReleasePaymentChild(const nsAString& aId);
 
 private:
   PaymentRequestManager() = default;
@@ -84,8 +79,6 @@ private:
 
   void NotifyRequestDone(PaymentRequest* aRequest);
 
-  
-  nsDataHashtable<nsStringHashKey, PaymentRequest*> mPaymentChildHash;
   
   nsDataHashtable<nsRefPtrHashKey<PaymentRequest>, uint32_t> mActivePayments;
   RefPtr<PaymentRequest> mShowingRequest;
