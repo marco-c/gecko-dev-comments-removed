199f84fba9de80d9a416e31dfe1344cb3de90206
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 21:46:26 +0000

    Bug 1462470 - Part 2 - Alert about trackers when Tracking Protection is off in the identity popup. r=Paolo
    This commit changes the tracking protection UI to always be present in the
    identity popup (control center) UI. Following the UI spec in
    https://mozilla.invisionapp.com/share/RSIY1B8GMC2#/screens/297824891,
    we are now warning about trackers even when TP is disabled and alter the
    button that shows up in this case to open the TP settings in about:preferences.
    
    MozReview-Commit-ID: 6g8rzWVRaua
    
    UltraBlame original commit: a242cf779e3936a0b5cb084e2ba6227033d01968

--- browser/base/content/browser-trackingprotection.js
+++ browser/base/content/browser-trackingprotection.js
@@ -15,6 +15,13 @@ var TrackingProtection = {
   activeTooltipText: null,
   disabledTooltipText: null,
 
+  get _baseURIForChannelClassifier() {
+    
+    
+    
+    return Services.io.newURI("https://" + gBrowser.selectedBrowser.currentURI.hostPort);
+  },
+
   init() {
     let $ = selector => document.querySelector(selector);
     this.container = $("#tracking-protection-container");
@@ -67,8 +74,8 @@ var TrackingProtection = {
     }
   },
 
-  openPreferences() {
-    openPreferences("privacy-trackingprotection", { origin: "appMenu-trackingprotection" });
+  openPreferences(origin) {
+    openPreferences("privacy-trackingprotection", { origin });
   },
 
   updateEnabled() {
@@ -76,7 +83,8 @@ var TrackingProtection = {
       Services.prefs.getBoolPref(this.PREF_ENABLED_GLOBALLY);
     this.enabledInPrivateWindows =
       Services.prefs.getBoolPref(this.PREF_ENABLED_IN_PRIVATE_WINDOWS);
-    this.container.hidden = !this.enabled;
+
+    this.content.setAttribute("enabled", this.enabled);
 
     if (PrivateBrowsingUtils.isWindowPrivate(window)) {
       this.broadcaster.setAttribute("enabled", this.enabledInPrivateWindows);
@@ -120,10 +128,6 @@ var TrackingProtection = {
   },
 
   onSecurityChange(state, isSimulated) {
-    if (!this.enabled) {
-      return;
-    }
-
     
     
     if (isSimulated) {
@@ -135,10 +139,11 @@ var TrackingProtection = {
     let isBlocking = state & Ci.nsIWebProgressListener.STATE_BLOCKED_TRACKING_CONTENT;
     let isAllowing = state & Ci.nsIWebProgressListener.STATE_LOADED_TRACKING_CONTENT;
 
-    if (isBlocking) {
+    if (isBlocking && this.enabled) {
       this.icon.setAttribute("tooltiptext", this.activeTooltipText);
       this.icon.setAttribute("state", "blocked-tracking-content");
       this.content.setAttribute("state", "blocked-tracking-content");
+      this.content.removeAttribute("hasException");
 
       
       if (this.enabledGlobally) {
@@ -152,15 +157,39 @@ var TrackingProtection = {
 
       this.shieldHistogramAdd(2);
     } else if (isAllowing) {
-      this.icon.setAttribute("tooltiptext", this.disabledTooltipText);
-      this.icon.setAttribute("state", "loaded-tracking-content");
-      this.content.setAttribute("state", "loaded-tracking-content");
+      
+      let hasException = false;
+      if (PrivateBrowsingUtils.isBrowserPrivate(gBrowser.selectedBrowser)) {
+        hasException = PrivateBrowsingUtils.existsInTrackingAllowlist(this._baseURIForChannelClassifier);
+      } else {
+        hasException = Services.perms.testExactPermission(this._baseURIForChannelClassifier,
+          "trackingprotection") == Services.perms.ALLOW_ACTION;
+      }
 
-      this.shieldHistogramAdd(1);
+      if (hasException) {
+        this.content.setAttribute("hasException", "true");
+      } else {
+        this.content.removeAttribute("hasException");
+      }
+
+      
+      if (this.enabled) {
+        this.icon.setAttribute("tooltiptext", this.disabledTooltipText);
+        this.icon.setAttribute("state", "loaded-tracking-content");
+        this.shieldHistogramAdd(1);
+      } else {
+        this.icon.removeAttribute("tooltiptext");
+        this.icon.removeAttribute("state");
+        this.shieldHistogramAdd(0);
+      }
+
+      
+      this.content.setAttribute("state", "loaded-tracking-content");
     } else {
       this.icon.removeAttribute("tooltiptext");
       this.icon.removeAttribute("state");
       this.content.removeAttribute("state");
+      this.content.removeAttribute("hasException");
 
       
       this.shieldHistogramAdd(0);
@@ -171,19 +200,15 @@ var TrackingProtection = {
   },
 
   disableForCurrentPage() {
-    
-    
-    
-    let normalizedUrl = Services.io.newURI(
-      "https://" + gBrowser.selectedBrowser.currentURI.hostPort);
+    let baseURI = this._baseURIForChannelClassifier;
 
     
     
     
     if (PrivateBrowsingUtils.isBrowserPrivate(gBrowser.selectedBrowser)) {
-      PrivateBrowsingUtils.addToTrackingAllowlist(normalizedUrl);
+      PrivateBrowsingUtils.addToTrackingAllowlist(baseURI);
     } else {
-      Services.perms.add(normalizedUrl,
+      Services.perms.add(baseURI,
         "trackingprotection", Services.perms.ALLOW_ACTION);
     }
 
@@ -200,13 +225,20 @@ var TrackingProtection = {
     
     
     
-    let normalizedUrl = Services.io.newURI(
-      "https://" + gBrowser.selectedBrowser.currentURI.hostPort);
+    if (!this.enabled) {
+      this.openPreferences("identityPopup-TP-action");
+      return;
+    }
+
+    
+    
+    
+    let baseURI = this._baseURIForChannelClassifier;
 
     if (PrivateBrowsingUtils.isBrowserPrivate(gBrowser.selectedBrowser)) {
-      PrivateBrowsingUtils.removeFromTrackingAllowlist(normalizedUrl);
+      PrivateBrowsingUtils.removeFromTrackingAllowlist(baseURI);
     } else {
-      Services.perms.remove(normalizedUrl, "trackingprotection");
+      Services.perms.remove(baseURI, "trackingprotection");
     }
 
     
