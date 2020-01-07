5365d44c2d92269bd37e634764d05c24f7a50410
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 23:18:18 +0000

    Bug 1472145 - Part 2. Add telemetry to track how frequently WebP images are used. r=tnikkel data-r=chutten
    This patch adds three telemetry scalars to track how WebP is used. All
    of these scalars are updated when we do the MIME type confirmation for
    an imgRequest when the first data comes in. We know at this point we
    decided to load the given content, so there should be minimal false
    positives for data the browser loaded but never displayed.
    
    The first two scalars are merely whether or not WebP was observed. One
    is for probes, which are tiny WebP images suggested by the Google WebP
    FAQ to probe for different aspects of WebP support (lossy, animated,
    etc). We want to count this separately as actual WebP content that the
    website wishes us to display. Probes will give a measure of how many
    users visit websites that probe for WebP support, and content will give
    a measure of how many websites don't care and just give us WebP images
    regardless.
    
    The third scalar is intended to give a relative measure of how many WebP
    images we are being served relative to all other image types. We expect
    the ratio to be small, but it would be good to confirm this from the
    data.
    
    UltraBlame original commit: 8d1f4fe78843a2948e244414ee8bdf46660bea39

--- image/imgRequest.cpp
+++ image/imgRequest.cpp
@@ -39,6 +39,7 @@
 #include "imgIRequest.h"
 
 #include "mozilla/IntegerPrintfMacros.h"
+#include "mozilla/Telemetry.h"
 
 using namespace mozilla;
 using namespace mozilla::image;
@@ -940,6 +941,7 @@ imgRequest::OnStopRequest(nsIRequest* aRequest,
 struct mimetype_closure
 {
   nsACString* newType;
+  uint32_t segmentSize;
 };
 
 
@@ -986,11 +988,49 @@ PrepareForNewPart(nsIRequest* aRequest, nsIInputStream* aInStr, uint32_t aCount,
   if (aInStr) {
     mimetype_closure closure;
     closure.newType = &result.mContentType;
+    closure.segmentSize = 0;
 
     
     
     uint32_t out;
     aInStr->ReadSegments(sniff_mimetype_callback, &closure, aCount, &out);
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    bool webp = result.mContentType.EqualsLiteral(IMAGE_WEBP);
+    bool webpProbe = false;
+    if (webp) {
+      
+      
+      const uint32_t kMaxProbeSize = 100;
+      if (closure.segmentSize < kMaxProbeSize &&
+          NS_FAILED(aURI->SchemeIs("data", &webpProbe))) {
+        webpProbe = false;
+      }
+
+      if (webpProbe) {
+        Telemetry::ScalarSet(Telemetry::ScalarID::IMAGES_WEBP_PROBE_OBSERVED,
+                             true);
+      } else {
+        Telemetry::ScalarSet(Telemetry::ScalarID::IMAGES_WEBP_CONTENT_OBSERVED,
+                             true);
+      }
+    }
+
+    if (!webpProbe) {
+      Telemetry::ScalarAdd(Telemetry::ScalarID::IMAGES_WEBP_CONTENT_FREQUENCY,
+                           webp ? NS_LITERAL_STRING("webp") :
+                                  NS_LITERAL_STRING("other"), 1);
+    }
   }
 
   nsCOMPtr<nsIChannel> chan(do_QueryInterface(aRequest));
@@ -1241,6 +1281,7 @@ sniff_mimetype_callback(nsIInputStream* in,
 
   NS_ASSERTION(closure, "closure is null!");
 
+  closure->segmentSize = count;
   if (count > 0) {
     imgLoader::GetMimeTypeFromContent(fromRawSegment, count, *closure->newType);
   }
