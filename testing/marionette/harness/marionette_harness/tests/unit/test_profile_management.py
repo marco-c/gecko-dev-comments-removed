2074694657e262ae817c0a6ddfb4ba72185eae92
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 03:51:37 +0000

    Bug 1429759 - Allow Marionette tests to change the user profile. r=automatedtester,maja_zf
    If Marionette handles the instance of the application including the
    user profile, the tests should be able to change the user profile.
    
    This will enable tests to cover specific bugs in the application as
    caused by the profile name and path.
    
    To prevent dataloss changing the profile should only be allowed
    when the application is not running.
    
    MozReview-Commit-ID: JWQGV4JWP61
    
    UltraBlame original commit: 949b1558f9b9bd4b8975ca5f1ff1ebb75688bcec

--- testing/marionette/harness/marionette_harness/tests/unit/test_profile_management.py
+++ testing/marionette/harness/marionette_harness/tests/unit/test_profile_management.py
@@ -4,33 +4,232 @@
 
 from __future__ import absolute_import
 
+import os
+import shutil
+import tempfile
+
+import mozprofile
+
+from marionette_driver import errors
 from marionette_harness import MarionetteTestCase
 
 
-class TestProfileManagement(MarionetteTestCase):
+class BaseProfileManagement(MarionetteTestCase):
 
     def setUp(self):
-        MarionetteTestCase.setUp(self)
-        self.marionette.enforce_gecko_prefs(
-            {"marionette.test.bool": True,
-             "marionette.test.string": "testing",
-             "marionette.test.int": 3
-             })
-        self.marionette.set_context("chrome")
+        super(BaseProfileManagement, self).setUp()
 
-    def test_preferences_are_set(self):
-        self.assertTrue(self.marionette.get_pref("marionette.test.bool"))
-        self.assertEqual(self.marionette.get_pref("marionette.test.string"), "testing")
-        self.assertEqual(self.marionette.get_pref("marionette.test.int"), 3)
+        self.orig_profile_path = self.profile_path
 
-    def test_change_preference(self):
-        self.assertTrue(self.marionette.get_pref("marionette.test.bool"))
+    def tearDown(self):
+        shutil.rmtree(self.orig_profile_path, ignore_errors=True)
 
-        self.marionette.enforce_gecko_prefs({"marionette.test.bool": False})
+        self.marionette.profile = None
 
-        self.assertFalse(self.marionette.get_pref("marionette.test.bool"))
+        super(BaseProfileManagement, self).tearDown()
 
-    def test_clean_profile(self):
+    @property
+    def profile(self):
+        return self.marionette.instance.profile
+
+    @property
+    def profile_path(self):
+        return self.marionette.instance.profile.profile
+
+
+class WorkspaceProfileManagement(BaseProfileManagement):
+
+    def setUp(self):
+        super(WorkspaceProfileManagement, self).setUp()
+
+        
+        
+        self.workspace = tempfile.mkdtemp()
+        self.marionette.instance.workspace = self.workspace
+
+    def tearDown(self):
+        self.marionette.instance.workspace = None
+
+        shutil.rmtree(self.workspace, ignore_errors=True)
+
+        super(WorkspaceProfileManagement, self).tearDown()
+
+
+class ExternalProfileMixin(object):
+
+    def setUp(self):
+        super(ExternalProfileMixin, self).setUp()
+
+        
+        tmp_dir = tempfile.mkdtemp(suffix="external")
+        shutil.rmtree(tmp_dir, ignore_errors=True)
+
+        self.external_profile = mozprofile.Profile(profile=tmp_dir)
+        
+        self.external_profile.create_new = False
+
+    def tearDown(self):
+        shutil.rmtree(self.external_profile.profile, ignore_errors=True)
+
+        super(ExternalProfileMixin, self).tearDown()
+
+
+class TestQuitRestartWithoutWorkspace(BaseProfileManagement):
+
+    def test_quit_keeps_same_profile(self):
+        self.marionette.quit()
+        self.marionette.start_session()
+
+        self.assertEqual(self.profile_path, self.orig_profile_path)
+        self.assertTrue(os.path.exists(self.orig_profile_path))
+
+    def test_quit_clean_creates_new_profile(self):
+        self.marionette.quit(clean=True)
+        self.marionette.start_session()
+
+        self.assertNotEqual(self.profile_path, self.orig_profile_path)
+        self.assertFalse(os.path.exists(self.orig_profile_path))
+
+    def test_restart_keeps_same_profile(self):
+        self.marionette.restart()
+
+        self.assertEqual(self.profile_path, self.orig_profile_path)
+        self.assertTrue(os.path.exists(self.orig_profile_path))
+
+    def test_restart_clean_creates_new_profile(self):
         self.marionette.restart(clean=True)
 
-        self.assertEqual(self.marionette.get_pref("marionette.test.bool"), None)
+        self.assertNotEqual(self.profile_path, self.orig_profile_path)
+        self.assertFalse(os.path.exists(self.orig_profile_path))
+
+
+class TestQuitRestartWithWorkspace(WorkspaceProfileManagement):
+
+    def test_quit_keeps_same_profile(self):
+        self.marionette.quit()
+        self.marionette.start_session()
+
+        self.assertEqual(self.profile_path, self.orig_profile_path)
+        self.assertNotIn(self.workspace, self.profile_path)
+        self.assertTrue(os.path.exists(self.orig_profile_path))
+
+    def test_quit_clean_creates_new_profile(self):
+        self.marionette.quit(clean=True)
+        self.marionette.start_session()
+
+        self.assertNotEqual(self.profile_path, self.orig_profile_path)
+        self.assertIn(self.workspace, self.profile_path)
+        self.assertFalse(os.path.exists(self.orig_profile_path))
+
+    def test_restart_keeps_same_profile(self):
+        self.marionette.restart()
+
+        self.assertEqual(self.profile_path, self.orig_profile_path)
+        self.assertNotIn(self.workspace, self.profile_path)
+        self.assertTrue(os.path.exists(self.orig_profile_path))
+
+    def test_restart_clean_creates_new_profile(self):
+        self.marionette.restart(clean=True)
+
+        self.assertNotEqual(self.profile_path, self.orig_profile_path)
+        self.assertIn(self.workspace, self.profile_path)
+        self.assertFalse(os.path.exists(self.orig_profile_path))
+
+
+class TestSwitchProfileFailures(BaseProfileManagement):
+
+    def test_raise_for_switching_profile_while_instance_is_running(self):
+        with self.assertRaisesRegexp(errors.MarionetteException, "instance is not running"):
+            self.marionette.instance.switch_profile()
+
+
+class TestSwitchProfileWithoutWorkspace(ExternalProfileMixin, BaseProfileManagement):
+
+    def setUp(self):
+        super(TestSwitchProfileWithoutWorkspace, self).setUp()
+
+        self.marionette.quit()
+
+    def test_do_not_call_cleanup_of_profile_for_path_only(self):
+        
+        
+        
+        self.marionette.instance._profile = self.external_profile.profile
+        self.marionette.instance.switch_profile()
+
+    def test_new_random_profile_name(self):
+        self.marionette.instance.switch_profile()
+        self.marionette.start_session()
+
+        self.assertNotEqual(self.profile_path, self.orig_profile_path)
+        self.assertFalse(os.path.exists(self.orig_profile_path))
+
+    def test_new_named_profile(self):
+        self.marionette.instance.switch_profile("foobar")
+        self.marionette.start_session()
+
+        self.assertNotEqual(self.profile_path, self.orig_profile_path)
+        self.assertIn("foobar", self.profile_path)
+        self.assertFalse(os.path.exists(self.orig_profile_path))
+
+    def test_clone_existing_profile(self):
+        self.marionette.instance.switch_profile(clone_from=self.external_profile)
+        self.marionette.start_session()
+
+        self.assertIn(os.path.basename(self.external_profile.profile), self.profile_path)
+        self.assertTrue(os.path.exists(self.external_profile.profile))
+
+    def test_replace_with_current_profile(self):
+        self.marionette.instance.profile = self.profile
+        self.marionette.start_session()
+
+        self.assertEqual(self.profile_path, self.orig_profile_path)
+        self.assertTrue(os.path.exists(self.orig_profile_path))
+
+    def test_replace_with_external_profile(self):
+        self.marionette.instance.profile = self.external_profile
+        self.marionette.start_session()
+
+        self.assertEqual(self.profile_path, self.external_profile.profile)
+        self.assertFalse(os.path.exists(self.orig_profile_path))
+
+        
+        self.marionette.quit()
+        self.marionette.instance.profile = None
+
+        self.assertNotEqual(self.profile_path, self.external_profile.profile)
+        self.assertTrue(os.path.exists(self.external_profile.profile))
+
+
+class TestSwitchProfileWithWorkspace(ExternalProfileMixin, WorkspaceProfileManagement):
+
+    def setUp(self):
+        super(TestSwitchProfileWithWorkspace, self).setUp()
+
+        self.marionette.quit()
+
+    def test_new_random_profile_name(self):
+        self.marionette.instance.switch_profile()
+        self.marionette.start_session()
+
+        self.assertNotEqual(self.profile_path, self.orig_profile_path)
+        self.assertIn(self.workspace, self.profile_path)
+        self.assertFalse(os.path.exists(self.orig_profile_path))
+
+    def test_new_named_profile(self):
+        self.marionette.instance.switch_profile("foobar")
+        self.marionette.start_session()
+
+        self.assertNotEqual(self.profile_path, self.orig_profile_path)
+        self.assertIn("foobar", self.profile_path)
+        self.assertIn(self.workspace, self.profile_path)
+        self.assertFalse(os.path.exists(self.orig_profile_path))
+
+    def test_clone_existing_profile(self):
+        self.marionette.instance.switch_profile(clone_from=self.external_profile)
+        self.marionette.start_session()
+
+        self.assertNotEqual(self.profile_path, self.orig_profile_path)
+        self.assertIn(self.workspace, self.profile_path)
+        self.assertIn(os.path.basename(self.external_profile.profile), self.profile_path)
+        self.assertTrue(os.path.exists(self.external_profile.profile))
