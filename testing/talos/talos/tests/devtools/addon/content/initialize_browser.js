697730600bbbd490e192ddb55eca6aa6dffcc5a6
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 09:30:28 +0000

    Bug 1447354 - Remove talos XUL overlays. r=jmaher
    Move the scripts and bootstrapping code that was in the overlay into a file
    loaded by bootstrap file.
    
    MozReview-Commit-ID: EIqQnU7rCbq
    
    UltraBlame original commit: 00a9d9c4d542027962ec1c0ed0acc50ebc0f8570

---
+++ testing/talos/talos/tests/devtools/addon/content/initialize_browser.js
@@ -0,0 +1,43 @@
+function initializeBrowser(win) {
+  Services.scriptloader.loadSubScript("chrome://talos-powers-content/content/TalosParentProfiler.js", win);
+  Services.scriptloader.loadSubScript("chrome://damp/content/damp.js", win);
+
+  const PREFIX = "damp@mozilla.org:";
+
+  
+  var proxiedServices = {
+    runTest(config, callback) {
+      (new win.Damp()).startTest(callback, config);
+    },
+
+    toClipboard(text) {
+      const gClipboardHelper = Cc["@mozilla.org/widget/clipboardhelper;1"]
+                                   .getService(Ci.nsIClipboardHelper);
+      gClipboardHelper.copyString(text);
+    }
+  };
+
+  var groupMM = win.getGroupMessageManager("browsers");
+  groupMM.loadFrameScript("chrome://damp/content/framescript.js", true);
+
+  
+  groupMM.addMessageListener(PREFIX + "chrome-exec-message", function listener(m) {
+    function sendResult(result) {
+      groupMM.broadcastAsyncMessage(PREFIX + "chrome-exec-reply", {
+        id: m.data.id,
+        result
+      });
+    }
+
+    var command = m.data.command;
+    if (!proxiedServices.hasOwnProperty(command.name))
+      throw new Error("DAMP: service doesn't exist: '" + command.name + "'");
+
+    var service = proxiedServices[command.name];
+    if (command.name == "runTest") 
+      service(command.data, sendResult);
+    else
+      sendResult(service(command.data));
+
+  });
+}
