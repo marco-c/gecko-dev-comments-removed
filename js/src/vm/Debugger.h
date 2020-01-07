c52431edb6e8c1be3705c36454dfab121eaef229
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 09:05:43 +0000

    Bug 1443583 - Part 2.6: Make DebuggerFrame_freeScriptFrameIterData a method of DebuggerFrame. r=jimb.
    Most of the work is to smooth out the types.
    
    UltraBlame original commit: 260f636f90793a27c01164ec9009916bf0c9362f

--- js/src/vm/Debugger.h
+++ js/src/vm/Debugger.h
@@ -1332,6 +1332,7 @@ class DebuggerFrame : public NativeObject
     static NativeObject* initClass(JSContext* cx, HandleObject dbgCtor, HandleObject objProto);
     static DebuggerFrame* create(JSContext* cx, HandleObject proto, const FrameIter& iter,
                                  HandleNativeObject debugger);
+    void freeFrameIterData(FreeOp* fop);
 
     static MOZ_MUST_USE bool getArguments(JSContext* cx, HandleDebuggerFrame frame,
                                           MutableHandleDebuggerArguments result);
