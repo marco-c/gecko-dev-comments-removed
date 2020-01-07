db44bd788c79905d662ffcb1a0e11ee2933f80b9
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 00:09:34 +0000

    Bug 1394564 - Delete current outgoing message when PrimeNewOutgoingMessage fails. r=mcmanus
    UltraBlame original commit: d0f561d1bf62a9a286273961debdeedda9526d42

--- netwerk/protocol/websocket/WebSocketChannel.cpp
+++ netwerk/protocol/websocket/WebSocketChannel.cpp
@@ -2051,6 +2051,10 @@ WebSocketChannel::PrimeNewOutgoingMessage()
   if (!mCurrentOut)
     return;
 
+  auto cleanupAfterFailure = MakeScopeExit([&] {
+    DeleteCurrentOutGoingMessage();
+  });
+
   WsMsgType msgType = mCurrentOut->GetMsgType();
 
   LOG(("WebSocketChannel::PrimeNewOutgoingMessage "
@@ -2070,6 +2074,7 @@ WebSocketChannel::PrimeNewOutgoingMessage()
     if (mClientClosed) {
       DeleteCurrentOutGoingMessage();
       PrimeNewOutgoingMessage();
+      cleanupAfterFailure.release();
       return;
     }
 
@@ -2258,6 +2263,8 @@ WebSocketChannel::PrimeNewOutgoingMessage()
   
   
   
+
+  cleanupAfterFailure.release();
 }
 
 void
