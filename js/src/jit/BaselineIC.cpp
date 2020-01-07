f8875f5369c2105dfdc35aceb7b1967ba33812df
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 09:56:33 +0000

    Bug 1448136 - Ensure Debug OSR transition is respected in InstanceOf Fallback stub. r=jandem
    UltraBlame original commit: af66807a5c4b8366dc98d3aae7d55124003a1039

--- js/src/jit/BaselineIC.cpp
+++ js/src/jit/BaselineIC.cpp
@@ -4146,9 +4146,12 @@ TryAttachInstanceOfStub(JSContext* cx, BaselineFrame* frame, ICInstanceOf_Fallba
 }
 
 static bool
-DoInstanceOfFallback(JSContext* cx, BaselineFrame* frame, ICInstanceOf_Fallback* stub,
+DoInstanceOfFallback(JSContext* cx, BaselineFrame* frame, ICInstanceOf_Fallback* stub_,
                      HandleValue lhs, HandleValue rhs, MutableHandleValue res)
 {
+    
+    DebugModeOSRVolatileStub<ICInstanceOf_Fallback*> stub(ICStubEngine::Baseline, frame, stub_);
+
     FallbackICSpew(cx, stub, "InstanceOf");
 
     if (!rhs.isObject()) {
@@ -4162,6 +4165,10 @@ DoInstanceOfFallback(JSContext* cx, BaselineFrame* frame, ICInstanceOf_Fallback*
         return false;
 
     res.setBoolean(cond);
+
+    
+    if (stub.invalid())
+        return true;
 
     if (!obj->is<JSFunction>()) {
         stub->noteUnoptimizableAccess();
