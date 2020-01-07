2269d1185fd461fc0e7fa01866dc473e5b0990da
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 06:55:35 +0000

    Bug 1440309: Call String_repeat with int32 values to avoid repeated bailouts. r=jandem
    UltraBlame original commit: 7b649daec5fb72014a2e22327411ed34dd7c77f7

--- js/src/builtin/String.js
+++ js/src/builtin/String.js
@@ -68,8 +68,7 @@ function String_generic_match(thisValue, regexp) {
 
 
 
-function String_pad(maxLength, fillString, padEnd = false) {
-
+function String_pad(maxLength, fillString, padEnd) {
     
     RequireObjectCoercible(this);
     let str = ToString(this);
@@ -90,14 +89,20 @@ function String_pad(maxLength, fillString, padEnd = false) {
         return str;
 
     
+    
+    if (intMaxLength > MAX_STRING_LENGTH)
+        ThrowRangeError(JSMSG_RESULTING_STRING_TOO_LARGE);
+
+    
     let fillLen = intMaxLength - strLen;
 
     
+    
+    
     let truncatedStringFiller = callFunction(String_repeat, filler,
-                                             fillLen / filler.length);
+                                             (fillLen / filler.length) | 0);
 
-    truncatedStringFiller += callFunction(String_substr, filler, 0,
-                                          fillLen % filler.length);
+    truncatedStringFiller += Substring(filler, 0, fillLen % filler.length);
 
     
     if (padEnd === true)
@@ -503,11 +508,14 @@ function String_repeat(count) {
     if (n < 0)
         ThrowRangeError(JSMSG_NEGATIVE_REPETITION_COUNT);
 
-    if (!(n * S.length < (1 << 28)))
+    
+    if (!(n * S.length <= MAX_STRING_LENGTH))
         ThrowRangeError(JSMSG_RESULTING_STRING_TOO_LARGE);
 
     
-    n = n & ((1 << 28) - 1);
+    assert((MAX_STRING_LENGTH & (MAX_STRING_LENGTH + 1)) === 0,
+           "MAX_STRING_LENGTH can be used as a bitmask");
+    n = n & MAX_STRING_LENGTH;
 
     
     var T = "";
