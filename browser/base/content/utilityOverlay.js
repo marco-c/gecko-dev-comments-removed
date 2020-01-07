f116a485bdbab6f8fe4532fd3c59f9359ad50a5e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 08:42:32 +0000

    Bug 1440573 - Policy: Disable safe mode. r=jimm,felipe
    This policy disables the safe-mode UI entry points. In addition, only on Windows when using GPO, it also disables entering Safe Mode by holding down the Shift Key
    
    UltraBlame original commit: 83dffebb15369683ef0426c43d68c593f5d2b618

--- browser/base/content/utilityOverlay.js
+++ browser/base/content/utilityOverlay.js
@@ -832,6 +832,9 @@ function buildHelpMenu() {
   document.getElementById("feedbackPage")
           .disabled = !Services.policies.isAllowed("feedbackCommands");
 
+  document.getElementById("helpSafeMode")
+          .disabled = !Services.policies.isAllowed("safeMode");
+
   
   if (typeof gSafeBrowsing != "undefined") {
     gSafeBrowsing.setReportPhishingMenu();
