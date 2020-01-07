bf204cdbda20233f2af4a1c934a6b7eb99941986
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 00:04:37 +0000

    Bug 1417827: Convert internal char* to nsCString in DNS.h r=valentin
    MozReview-Commit-ID: Js1mXiKaKnt
    
    UltraBlame original commit: 34cfc821e335cafbb98e18d400c2abbcbc32c19f

--- netwerk/dns/DNS.h
+++ netwerk/dns/DNS.h
@@ -8,6 +8,7 @@
 #define DNS_h_
 
 #include "nscore.h"
+#include "nsString.h"
 #include "prio.h"
 #include "prnetdb.h"
 #include "plstr.h"
@@ -134,27 +135,26 @@ public:
 class AddrInfo {
 public:
   
-  
-  AddrInfo(const char *host, const PRAddrInfo *prAddrInfo, bool disableIPv4,
-           bool filterNameCollision, const char *cname);
+  AddrInfo(const nsACString& host, const PRAddrInfo *prAddrInfo,
+           bool disableIPv4, bool filterNameCollision,
+           const nsACString& cname);
 
   
-  AddrInfo(const char *host, const char *cname);
+  
+  AddrInfo(const nsACString& host, const nsACString& cname);
+
   ~AddrInfo();
 
   void AddAddress(NetAddrElement *address);
 
   size_t SizeOfIncludingThis(mozilla::MallocSizeOf mallocSizeOf) const;
 
-  char *mHostName;
-  char *mCanonicalName;
+  nsCString mHostName;
+  nsCString mCanonicalName;
   uint32_t ttl;
   static const uint32_t NO_TTL_DATA = (uint32_t) -1;
 
   LinkedList<NetAddrElement> mAddresses;
-
-private:
-  void Init(const char *host, const char *cname);
 };
 
 
