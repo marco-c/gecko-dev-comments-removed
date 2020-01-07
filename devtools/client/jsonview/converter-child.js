dcd44731bb586bef6c1c4dc5f32083a53461845f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 18:34:34 +0000

    Backed out changeset 093b3a6f6089 (bug 1459640) for failing devtools' devtools/client/jsonview/test/browser_jsonview_content_type.js, at least on OS X debug
    UltraBlame original commit: c34056cde92e21b6d72c4b0b6deb640e68701039

--- devtools/client/jsonview/converter-child.js
+++ devtools/client/jsonview/converter-child.js
@@ -22,8 +22,6 @@ const BinaryInput = CC("@mozilla.org/binaryinputstream;1",
 const BufferStream = CC("@mozilla.org/io/arraybuffer-input-stream;1",
                        "nsIArrayBufferInputStream", "setData");
 
-const kCSP = "default-src 'none' ; script-src resource:; ";
-
 
 loader.lazyGetter(this, "jsonViewStrings", () => {
   return Services.strings.createBundle(
@@ -86,8 +84,8 @@ Converter.prototype = {
     
     try {
       request.QueryInterface(Ci.nsIHttpChannel);
-      request.setResponseHeader("Content-Security-Policy", kCSP, false);
-      request.setResponseHeader("Content-Security-Policy-Report-Only", "", false);
+      request.setResponseHeader("Content-Security-Policy",
+        "default-src 'none' ; script-src resource:; ", false);
     } catch (ex) {
       
     }
@@ -250,10 +248,6 @@ function initialHTML(doc) {
       "dir": Services.locale.isAppLocaleRTL ? "rtl" : "ltr"
     }, [
       element("head", {}, [
-        element("meta", {
-          "http-equiv": "Content-Security-Policy",
-          content: kCSP,
-        }),
         element("link", {
           rel: "stylesheet",
           type: "text/css",
