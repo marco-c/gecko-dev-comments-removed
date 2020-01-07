2d4b91e831b71afd98b0e8b30de3269074003a06
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 05:20:02 +0000

    Bug 1437184. Inline FillAzure() r=bas
    There's only one caller so let's just eliminate inline FillAzure into
    Fill.
    
    MozReview-Commit-ID: JYpAQMkhEcS
    
    UltraBlame original commit: ccb72c7fa363a2153832845ff5dce5dd81302d55

--- gfx/thebes/gfxContext.h
+++ gfx/thebes/gfxContext.h
@@ -505,7 +505,6 @@ private:
   void EnsurePath();
   
   void EnsurePathBuilder();
-  void FillAzure(const Pattern& aPattern, mozilla::gfx::Float aOpacity);
   CompositionOp GetOp();
   void ChangeTransform(const mozilla::gfx::Matrix &aNewMatrix, bool aUpdatePatternTransform = true);
   Rect GetAzureDeviceSpaceClipBounds() const;
