3bf42e98c65a4c4ca897f6ac2c3ed6e2f4ad4182
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 19:18:21 +0000

    Bug 1462727 - Make WindowsMapRemoteView.cpp work with April 2018 Update SDK. r=aklotz
    MozReview-Commit-ID: DtzFeCmbGqO
    
    UltraBlame original commit: 4c764e503ceff397a4c2de2712ec89087ea4de6e

--- mozglue/misc/WindowsMapRemoteView.cpp
+++ mozglue/misc/WindowsMapRemoteView.cpp
@@ -15,12 +15,12 @@
 
 
 
-PVOID WINAPI
+WINBASEAPI PVOID WINAPI
 MapViewOfFileNuma2(HANDLE aFileMapping, HANDLE aProcess, ULONG64 aOffset,
                    PVOID aBaseAddress, SIZE_T aViewSize, ULONG aAllocationType,
                    ULONG aPageProtection, ULONG aPreferredNode);
 
-BOOL WINAPI
+WINBASEAPI BOOL WINAPI
 UnmapViewOfFile2(HANDLE aProcess, PVOID aBaseAddress, ULONG aUnmapFlags);
 
 #endif 
