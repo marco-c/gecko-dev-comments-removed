8fa9c3e25c6d2c259cab589687203c702e83c845
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 21:55:26 +0000

    Bug 1468846: Move MediaList to its own module. r=xidorn
    And move the parsing from a free function to MediaList::parse.
    
    MozReview-Commit-ID: 75ES6I2EEOE
    
    UltraBlame original commit: 9103f4c99136177a1fe56db949bea7b0b6cf09d8

--- servo/components/style/media_queries/mod.rs
+++ servo/components/style/media_queries/mod.rs
@@ -7,39 +7,21 @@
 
 
 use Atom;
-use context::QuirksMode;
-use cssparser::{Delimiter, Parser};
-use cssparser::{ParserInput, Token};
-use error_reporting::{ContextualParseError, ParseErrorReporter};
-use parser::{ParserContext, ParserErrorContext};
+use cssparser::Parser;
+use parser::ParserContext;
 use selectors::parser::SelectorParseErrorKind;
 use std::fmt::{self, Write};
 use str::string_as_ascii_lowercase;
 use style_traits::{CssWriter, ParseError, StyleParseErrorKind, ToCss};
 use values::CustomIdent;
 
+mod media_list;
+
+pub use self::media_list::MediaList;
 #[cfg(feature = "servo")]
 pub use servo::media_queries::{Device, Expression};
 #[cfg(feature = "gecko")]
 pub use gecko::media_queries::{Device, Expression};
-
-
-#[css(comma)]
-#[derive(Clone, Debug, MallocSizeOf, ToCss)]
-pub struct MediaList {
-    
-    #[css(iterable)]
-    pub media_queries: Vec<MediaQuery>,
-}
-
-impl MediaList {
-    
-    pub fn empty() -> Self {
-        MediaList {
-            media_queries: vec![],
-        }
-    }
-}
 
 
 #[derive(Clone, Copy, Debug, Eq, MallocSizeOf, PartialEq, ToCss)]
@@ -231,119 +213,5 @@ impl MediaQuery {
             }
             expressions.push(Expression::parse(context, input)?)
         }
-    }
-}
-
-
-
-
-
-
-
-pub fn parse_media_query_list<R>(
-    context: &ParserContext,
-    input: &mut Parser,
-    error_reporter: &R,
-) -> MediaList
-where
-    R: ParseErrorReporter,
-{
-    if input.is_exhausted() {
-        return MediaList::empty();
-    }
-
-    let mut media_queries = vec![];
-    loop {
-        let start_position = input.position();
-        match input.parse_until_before(Delimiter::Comma, |i| MediaQuery::parse(context, i)) {
-            Ok(mq) => {
-                media_queries.push(mq);
-            },
-            Err(err) => {
-                media_queries.push(MediaQuery::never_matching());
-                let location = err.location;
-                let error =
-                    ContextualParseError::InvalidMediaRule(input.slice_from(start_position), err);
-                let error_context = ParserErrorContext { error_reporter };
-                context.log_css_error(&error_context, location, error);
-            },
-        }
-
-        match input.next() {
-            Ok(&Token::Comma) => {},
-            Ok(_) => unreachable!(),
-            Err(_) => break,
-        }
-    }
-
-    MediaList {
-        media_queries: media_queries,
-    }
-}
-
-impl MediaList {
-    
-    pub fn evaluate(&self, device: &Device, quirks_mode: QuirksMode) -> bool {
-        
-        
-        self.media_queries.is_empty() || self.media_queries.iter().any(|mq| {
-            let media_match = mq.media_type.matches(device.media_type());
-
-            
-            let query_match = media_match &&
-                mq.expressions
-                    .iter()
-                    .all(|expression| expression.matches(&device, quirks_mode));
-
-            
-            match mq.qualifier {
-                Some(Qualifier::Not) => !query_match,
-                _ => query_match,
-            }
-        })
-    }
-
-    
-    pub fn is_empty(&self) -> bool {
-        self.media_queries.is_empty()
-    }
-
-    
-    
-    
-    
-    pub fn append_medium(&mut self, context: &ParserContext, new_medium: &str) -> bool {
-        let mut input = ParserInput::new(new_medium);
-        let mut parser = Parser::new(&mut input);
-        let new_query = match MediaQuery::parse(&context, &mut parser) {
-            Ok(query) => query,
-            Err(_) => {
-                return false;
-            },
-        };
-        
-        
-        
-        self.media_queries.retain(|query| query != &new_query);
-        self.media_queries.push(new_query);
-        true
-    }
-
-    
-    
-    
-    
-    pub fn delete_medium(&mut self, context: &ParserContext, old_medium: &str) -> bool {
-        let mut input = ParserInput::new(old_medium);
-        let mut parser = Parser::new(&mut input);
-        let old_query = match MediaQuery::parse(context, &mut parser) {
-            Ok(query) => query,
-            Err(_) => {
-                return false;
-            },
-        };
-        let old_len = self.media_queries.len();
-        self.media_queries.retain(|query| query != &old_query);
-        old_len != self.media_queries.len()
     }
 }
