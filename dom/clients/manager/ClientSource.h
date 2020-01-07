094b9c52b58ed0572f46d02cd2ba0b226b0e748d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 22:30:01 +0000

    Bug 1469941 Note DOMContentLoaded on the ClientSource and trigger service worker update in appropriate process. r=mrbkap
    UltraBlame original commit: 3f78c17a1ceaaaa4b7e1fa04c3375f96e5acc9c8

--- dom/clients/manager/ClientSource.h
+++ dom/clients/manager/ClientSource.h
@@ -150,6 +150,11 @@ public:
   const Maybe<ServiceWorkerDescriptor>&
   GetController() const;
 
+  
+  
+  void
+  NoteDOMContentLoaded();
+
   RefPtr<ClientOpPromise>
   Focus(const ClientFocusArgs& aArgs);
 
