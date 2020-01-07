3c54e507d63444a4256e713d1b48b42ec5ee1fef
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 17:52:52 +0000

    Bug 1458456 part 6 - Some TSan automation changes. r=sfink
    UltraBlame original commit: e186ada847ff99cbe00e0e7f19201d309beeba6f

--- js/src/devtools/automation/autospider.py
+++ js/src/devtools/automation/autospider.py
@@ -501,7 +501,7 @@ if variant.get('ignore-test-failures'):
     logging.warning("Ignoring test results %s" % (results,))
     results = [0]
 
-if args.variant in ('tsan', 'msan'):
+if args.variant == 'msan':
     files = filter(lambda f: f.startswith("sanitize_log."), os.listdir(OUTDIR))
     fullfiles = [os.path.join(OUTDIR, f) for f in files]
 
@@ -532,43 +532,6 @@ if args.variant in ('tsan', 'msan'):
         print("Found %d errors out of %d allowed" % (len(sites), max_allowed))
         if len(sites) > max_allowed:
             results.append(1)
-
-    if 'expect-errors' in variant:
-        
-        
-        
-        
-        
-        
-        
-
-        for site in sites:
-            
-            m = re.search(r'/([^/]+):\d+ in (.+)', site)
-            if m:
-                error = tuple(m.groups())
-            else:
-                
-                error = (site, '(unknown)')
-            errors[error] += 1
-
-        remaining = Counter(errors)
-        for expect in variant['expect-errors']:
-            
-            expect = tuple(expect)
-            if remaining[expect] == 0:
-                print("Did not see known error in %s function %s" % expect)
-            else:
-                remaining[expect] -= 1
-
-        status = 0
-        for filename, function in (e for e, c in remaining.items() if c > 0):
-            if AUTOMATION:
-                print("TinderboxPrint: tsan error<br/>%s function %s" % (filename, function))
-                status = 1
-            else:
-                print("*** tsan error in %s function %s" % (filename, function))
-        results.append(status)
 
     
     
