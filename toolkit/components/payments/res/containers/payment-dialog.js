6be9e81d136686852a5f069207a5ce75d0396802
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 06:34:38 +0000

    Bug 1440843 - Don't dispatch shipping*change events if requestShipping is false. r=jaws
    MozReview-Commit-ID: Cu74O0Reu4L
    
    UltraBlame original commit: 0ec115a548858e4ce55d271dc900387a934476d9

--- toolkit/components/payments/res/containers/payment-dialog.js
+++ toolkit/components/payments/res/containers/payment-dialog.js
@@ -166,12 +166,16 @@ class PaymentDialog extends PaymentStateSubscriberMixin(HTMLElement) {
   stateChangeCallback(state) {
     super.stateChangeCallback(state);
 
-    if (state.selectedShippingAddress != this._cachedState.selectedShippingAddress) {
-      this.changeShippingAddress(state.selectedShippingAddress);
-    }
+    
+    
+    if (state.request.paymentOptions.requestShipping) {
+      if (state.selectedShippingAddress != this._cachedState.selectedShippingAddress) {
+        this.changeShippingAddress(state.selectedShippingAddress);
+      }
 
-    if (state.selectedShippingOption != this._cachedState.selectedShippingOption) {
-      this.changeShippingOption(state.selectedShippingOption);
+      if (state.selectedShippingOption != this._cachedState.selectedShippingOption) {
+        this.changeShippingOption(state.selectedShippingOption);
+      }
     }
 
     this._cachedState.selectedShippingAddress = state.selectedShippingAddress;
