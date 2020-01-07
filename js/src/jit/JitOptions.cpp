64d602940ce41cae64cd3ce5d3fa96fa22657654
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 05:15:30 +0000

    Bug 1434230 part 4 - Enable Spectre string mitigations by default. r=luke
    UltraBlame original commit: 9848ae936a3d35820e1e9a6b69eeb856aa53aa78

--- js/src/jit/JitOptions.cpp
+++ js/src/jit/JitOptions.cpp
@@ -233,7 +233,7 @@ DefaultJitOptions::DefaultJitOptions()
     }
 
     SET_DEFAULT(spectreIndexMasking, true);
-    SET_DEFAULT(spectreStringMitigations, false);
+    SET_DEFAULT(spectreStringMitigations, true);
 
     
     SET_DEFAULT(disableUnboxedObjects, false);
