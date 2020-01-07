835123999b81f7a322b17ab27f1c6288bbd6a795
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 11:44:50 +0000

    Bug 1453090. Don't PopClip on a different DT. r=jrmuizel
    Make sure we PushClip on the same DT as we PopClip
    
    UltraBlame original commit: 73e6728b22c5bc2a79c650907c4e30fcd84f55a0

--- gfx/webrender_bindings/Moz2DImageRenderer.cpp
+++ gfx/webrender_bindings/Moz2DImageRenderer.cpp
@@ -241,11 +241,6 @@ static bool Moz2DRenderCallback(const Range<const uint8_t> aBlob,
     return false;
   }
 
-  if (aDirtyRect) {
-    Rect dirty(aDirtyRect->origin.x, aDirtyRect->origin.y, aDirtyRect->size.width, aDirtyRect->size.height);
-    dt->PushClipRect(dirty);
-  }
-
   if (aTileOffset) {
     
     
@@ -256,6 +251,11 @@ static bool Moz2DRenderCallback(const Range<const uint8_t> aBlob,
     tileset.mTiles = &tile;
     tileset.mTileCount = 1;
     dt = gfx::Factory::CreateTiledDrawTarget(tileset);
+  }
+
+  if (aDirtyRect) {
+    Rect dirty(aDirtyRect->origin.x, aDirtyRect->origin.y, aDirtyRect->size.width, aDirtyRect->size.height);
+    dt->PushClipRect(dirty);
   }
 
   struct Reader {
