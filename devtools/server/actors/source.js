ad55bf35b44d3061db5cf406881a669976b2fbdd
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 05:52:50 +0000

    Backed out changeset a8aa7e2af130 (bug 1436151) for xpcshell failures on /test_setBreakpoint-at-the-beginning-of-a-line.js. a=backout
    UltraBlame original commit: 941681a0ddc64f46dda9c2e69ea36a78bc307dee

--- devtools/server/actors/source.js
+++ devtools/server/actors/source.js
@@ -907,48 +907,25 @@ let SourceActor = ActorClassWithSpec(sourceSpec, {
           entryPoints.push({ script, offsets });
         }
       }
-
-      if (entryPoints.length > 0) {
-        setBreakpointAtEntryPoints(actor, entryPoints);
-        return true;
-      }
-
-      return false;
-    }
-
-    
-    
-    for (let script of scripts) {
-      let columnToOffsetMap = script.getAllColumnOffsets()
-                                    .filter(({ lineNumber }) => {
-                                      return lineNumber === generatedLine;
-                                    });
-      for (let { columnNumber: column, offset } of columnToOffsetMap) {
-        if (column >= generatedColumn && column <= generatedLastColumn) {
-          entryPoints.push({ script, offsets: [offset] });
-        }
-      }
-    }
-
-    
-    
-    if (entryPoints.length === 0) {
+    } else {
+      
+      
       for (let script of scripts) {
-        let columnToOffsetMap = script
-          .getAllColumnOffsets()
-          .filter(({ lineNumber }) => {
-            return lineNumber === generatedLine;
-          });
-
-        if (columnToOffsetMap.length > 0) {
-          let { columnNumber: column, offset } = columnToOffsetMap[0];
-          if (generatedColumn < column) {
+        let columnToOffsetMap = script.getAllColumnOffsets()
+                                      .filter(({ lineNumber }) => {
+                                        return lineNumber === generatedLine;
+                                      });
+        for (let { columnNumber: column, offset } of columnToOffsetMap) {
+          if (column >= generatedColumn && column <= generatedLastColumn) {
             entryPoints.push({ script, offsets: [offset] });
           }
         }
       }
     }
 
+    if (entryPoints.length === 0) {
+      return false;
+    }
     setBreakpointAtEntryPoints(actor, entryPoints);
     return true;
   }
