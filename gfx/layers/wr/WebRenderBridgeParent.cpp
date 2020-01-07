76e030301f6e3e0ffcedb9b121963550057f4544
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 04:45:54 +0000

    Bug 1435282 - Document that function arguments are always null. r=sotaro
    MozReview-Commit-ID: HOu4fshHSuX
    
    UltraBlame original commit: 30b7617cb90398134ca05d2909829927b16695f6

--- gfx/layers/wr/WebRenderBridgeParent.cpp
+++ gfx/layers/wr/WebRenderBridgeParent.cpp
@@ -1199,6 +1199,11 @@ WebRenderBridgeParent::SampleAnimations(nsTArray<wr::WrOpacityProperty>& aOpacit
 void
 WebRenderBridgeParent::CompositeToTarget(gfx::DrawTarget* aTarget, const gfx::IntRect* aRect)
 {
+  
+  
+  MOZ_ASSERT(aTarget == nullptr);
+  MOZ_ASSERT(aRect == nullptr);
+
   AUTO_PROFILER_TRACING("Paint", "CompositeToTraget");
   if (mPaused) {
     return;
