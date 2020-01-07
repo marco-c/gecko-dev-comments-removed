bcdc7008b3620f210e992c1e3fa073167397bed8
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 22:19:52 +0000

    Bug 1453795 - LibJAR - Initialize member fields in classes/ structures. r=aklotz
    UltraBlame original commit: a79434a2511b80f92554c15317e2d6271849ace9

--- modules/libjar/zipwriter/nsDeflateConverter.h
+++ modules/libjar/zipwriter/nsDeflateConverter.h
@@ -26,12 +26,18 @@ public:
     NS_DECL_NSISTREAMCONVERTER
 
     nsDeflateConverter()
+        : mWrapMode(WRAP_NONE)
+        , mOffset(0)
+        , mZstream()
     {
         
         mLevel = 6;
     }
 
     explicit nsDeflateConverter(int32_t level)
+        : mWrapMode(WRAP_NONE)
+        , mOffset(0)
+        , mZstream()
     {
         mLevel = level;
     }
