13dc31c3a37aa946ff9c3871060fdfa7315b17b0
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 11:20:26 +0000

    Bug 1441358 - ensure langpacks across platforms are identical r=Callek
    UltraBlame original commit: 9589c8e1acbdebf3e57206aedef7287a9130e43e

--- browser/base/content/test/static/browser_all_files_referenced.js
+++ browser/base/content/test/static/browser_all_files_referenced.js
@@ -54,7 +54,7 @@ var whitelist = [
   
   {file: "chrome://global/locale/fallbackMenubar.properties",
    platforms: ["linux", "win"]},
-  {file: "chrome://global/locale/printPageSetup.dtd", platforms: ["macosx"]},
+  {file: "chrome://global/locale/printPageSetup.dtd", platforms: ["linux", "macosx"]},
   {file: "chrome://global/locale/printPreviewProgress.dtd",
    platforms: ["macosx"]},
   {file: "chrome://global/locale/printProgress.dtd", platforms: ["macosx"]},
