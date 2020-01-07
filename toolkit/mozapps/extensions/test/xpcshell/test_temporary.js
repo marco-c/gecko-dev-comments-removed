7538e29138cd16b8d923adf0490164859bb2a78c
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 20:09:54 +0000

    Bug 1464707 - Skip test_temporary.js::test_zip_extension in Thunderbird. r=kmag
    UltraBlame original commit: 7b6e1437845e328b7ce1a75aaeaf7479faca8c98

--- toolkit/mozapps/extensions/test/xpcshell/test_temporary.js
+++ toolkit/mozapps/extensions/test/xpcshell/test_temporary.js
@@ -789,7 +789,8 @@ add_task(async function() {
 });
 
 
-add_task(async function test_zip_extension() {
+add_task({ skip_if: () => AppConstants.MOZ_APP_NAME == "thunderbird" },
+         async function test_zip_extension() {
   let xpi = createTempWebExtensionFile({
     background() {
       
