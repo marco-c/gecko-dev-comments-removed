6764de0e48a5d7ccae00a982f67edf3583437d40
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 10:08:52 +0000

    Bug 1053898 - Update inspector to prevent context-menu on slotted elements;r=bgrins
    MozReview-Commit-ID: loWAiRBT18
    
    UltraBlame original commit: 5d2c3fe3111237716313367a8c147295d24922c1

--- devtools/client/inspector/inspector.js
+++ devtools/client/inspector/inspector.js
@@ -1371,6 +1371,11 @@ Inspector.prototype = {
   },
 
   _openMenu: function({ target, screenX = 0, screenY = 0 } = { }) {
+    if (this.selection.isSlotted()) {
+      
+      return null;
+    }
+
     let markupContainer = this.markup.getContainer(this.selection.nodeFront);
 
     this.contextMenuTarget = target;
