e6089c17e125705ac12c2eefcb2c9f8f7d07d20d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 06:58:26 +0000

    Bug 1174003 part 4: [css-flexbox] Remove IsMainAxisHorizontal() check from DoFlexLayout(). r=mats
    This patch doesn't affect behavior.  It just replaces some (correct) physical-axis-dependent code with equivalent logical-axis-dependent code.
    
    MozReview-Commit-ID: 27QJU2cFWh
    
    UltraBlame original commit: 201aadc96164accd39ee4ffcb6e5108ad17eccbc

--- layout/generic/nsFlexContainerFrame.cpp
+++ layout/generic/nsFlexContainerFrame.cpp
@@ -4520,10 +4520,10 @@ nsFlexContainerFrame::DoFlexLayout(nsPresContext*           aPresContext,
                                      item->Frame(), availSize);
         if (!sizeOverride) {
           
-          if (aAxisTracker.IsMainAxisHorizontal()) {
-            childReflowInput.SetComputedWidth(item->GetMainSize());
+          if (item->IsInlineAxisMainAxis()) {
+            childReflowInput.SetComputedISize(item->GetMainSize());
           } else {
-            childReflowInput.SetComputedHeight(item->GetMainSize());
+            childReflowInput.SetComputedBSize(item->GetMainSize());
           }
         }
 
