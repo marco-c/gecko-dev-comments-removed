6a82f962af73af5de4064af73b361ad7967bc135
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 03:51:40 +0000

    Bug 1432773 - listen for unload in capture phase to reset click count; r=whimboo
    The bubbling phase is too late so resetClick didn't always get called.
    
    MozReview-Commit-ID: JIseY5o3oAX
    
    UltraBlame original commit: c0b051e8b50065d52e59a6b1b47844ae464f60d4

--- testing/marionette/listener.js
+++ testing/marionette/listener.js
@@ -53,7 +53,7 @@ let curContainer = {frame: content, shadowRoot: null};
 
 addEventListener("click", event.DoubleClickTracker.setClick);
 addEventListener("dblclick", event.DoubleClickTracker.resetClick);
-addEventListener("unload", event.DoubleClickTracker.resetClick);
+addEventListener("unload", event.DoubleClickTracker.resetClick, true);
 
 const seenEls = new element.Store();
 const SUPPORTED_STRATEGIES = new Set([
