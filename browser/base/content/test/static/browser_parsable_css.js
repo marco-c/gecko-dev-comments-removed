544e21165198a8712772147da82451693f8a57ed
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 18:32:42 +0000

    Bug 1460870 - Whitelist fullscreen pseudo-class in pdfjs when unprefixed fullscreen is disabled. r=Gijs
    MozReview-Commit-ID: L5EpFOHc5v4
    
    UltraBlame original commit: 87df7799c295045d0222beab4ddd45889d71cfab

--- browser/base/content/test/static/browser_parsable_css.js
+++ browser/base/content/test/static/browser_parsable_css.js
@@ -72,6 +72,11 @@ if (!Services.prefs.getBoolPref("full-screen-api.unprefix.enabled")) {
     sourceName: /(?:res|gre-resources)\/(ua|html)\.css$/i,
     errorMessage: /Unknown pseudo-class .*\bfullscreen\b/i,
     isFromDevTools: false
+  }, {
+    
+    sourceName: /web\/viewer\.css$/i,
+    errorMessage: /Unknown pseudo-class .*\bfullscreen\b/i,
+    isFromDevTools: false
   });
 }
 
