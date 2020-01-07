1fa497aa4c368d4c2471bceb8d6324f62b070a50
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 03:27:52 +0000

    Bug 1431127 - Fix TypeError: panel._selectors.getPause. r=jdescottes
    UltraBlame original commit: 9e42d6d64671fa229408bc7c5b9308fd21460d45

--- devtools/client/debugger/panel.js
+++ devtools/client/debugger/panel.js
@@ -131,10 +131,16 @@ DebuggerPanel.prototype = {
 
   
 
+  isPaused() {
+    let framesController = this.panelWin.DebuggerController.StackFrames;
+    let thread = framesController.activeThread;
+    return thread && thread.paused;
+  },
+
   getFrames() {
     let framesController = this.panelWin.DebuggerController.StackFrames;
     let thread = framesController.activeThread;
-    if (thread && thread.paused) {
+    if (this.isPaused()) {
       return {
         frames: thread.cachedFrames,
         selected: framesController.currentFrameDepth,
