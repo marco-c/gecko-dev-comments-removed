9f35e73e7b3ff2211b0bdfb93182093496c9c777
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 05:13:23 +0000

    Bug 1319203: Slightly optimize the jit->wasm stub; r=jandem
    MozReview-Commit-ID: Hu5TLtz8iQk
    
    UltraBlame original commit: fd805f7ee6006d811b3c08a2fd869c21d9fff663

--- js/src/wasm/WasmStubs.cpp
+++ js/src/wasm/WasmStubs.cpp
@@ -483,7 +483,8 @@ GenerateJitEntry(MacroAssembler& masm, size_t funcExportIndex, const FuncExport&
     Label oolCall;
     for (size_t i = 0; i < fe.sig().args().length(); i++) {
         unsigned jitArgOffset = frameSize + JitFrameLayout::offsetOfActualArg(i);
-        masm.loadValue(Address(sp, jitArgOffset), scratchV);
+        Address jitArgAddr(sp, jitArgOffset);
+        masm.loadValue(jitArgAddr, scratchV);
 
         Label next;
         switch (fe.sig().args()[i]) {
@@ -506,8 +507,8 @@ GenerateJitEntry(MacroAssembler& masm, size_t funcExportIndex, const FuncExport&
             masm.branchTestUndefined(Assembler::Equal, tag, &nullOrUndefined);
             masm.branchTestNull(Assembler::NotEqual, tag, &notNullOrUndefined);
             masm.bind(&nullOrUndefined);
-            masm.mov(ImmWord(0), scratchG);
-            masm.jump(&storeBack);
+            masm.storeValue(Int32Value(0), jitArgAddr);
+            masm.jump(&next);
             masm.bind(&notNullOrUndefined);
 
             
@@ -517,8 +518,7 @@ GenerateJitEntry(MacroAssembler& masm, size_t funcExportIndex, const FuncExport&
             
 
             masm.bind(&storeBack);
-            masm.boxNonDouble(JSVAL_TYPE_INT32, scratchG, scratchV);
-            masm.storeValue(scratchV, Address(sp, jitArgOffset));
+            masm.storeValue(JSVAL_TYPE_INT32, scratchG, jitArgAddr);
             break;
           }
           case ValType::F32:
@@ -541,15 +541,15 @@ GenerateJitEntry(MacroAssembler& masm, size_t funcExportIndex, const FuncExport&
             
             Label notUndefined;
             masm.branchTestUndefined(Assembler::NotEqual, tag, &notUndefined);
-            masm.loadConstantDouble(JS::GenericNaN(), scratchF);
-            masm.jump(&storeBack);
+            masm.storeValue(DoubleValue(JS::GenericNaN()), jitArgAddr);
+            masm.jump(&next);
             masm.bind(&notUndefined);
 
             
             Label notNull;
             masm.branchTestNull(Assembler::NotEqual, tag, &notNull);
-            masm.loadConstantDouble(0.0, scratchF);
-            masm.jump(&storeBack);
+            masm.storeValue(DoubleValue(0.), jitArgAddr);
+            masm.jump(&next);
             masm.bind(&notNull);
 
             
@@ -559,8 +559,7 @@ GenerateJitEntry(MacroAssembler& masm, size_t funcExportIndex, const FuncExport&
             
 
             masm.bind(&storeBack);
-            masm.boxDouble(scratchF, scratchV, scratchF);
-            masm.storeValue(scratchV, Address(sp, jitArgOffset));
+            masm.boxDouble(scratchF, jitArgAddr);
             break;
           }
           default: {
