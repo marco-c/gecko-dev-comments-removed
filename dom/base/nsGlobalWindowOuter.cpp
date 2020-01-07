055c7ccdf1a0c303c27d870f023a5cbd54c3dcd6
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 12:38:05 +0000

    Bug 1453517 - Consolidate popup permission checks to nsContentUtils; r=bz
    Permissions checks for popups were happening in nsIPopupWindowManager,
    but that really only required a pref and perm check. The XPCOM
    machinery around this isn't really needed, and the check was only
    called from two places in content, so this fits in nsContentUtils.
    
    MozReview-Commit-ID: 9x60x1BKWcr
    
    UltraBlame original commit: 77694987ba61cf176fc403badc29431158f3f831

--- dom/base/nsGlobalWindowOuter.cpp
+++ dom/base/nsGlobalWindowOuter.cpp
@@ -1725,7 +1725,7 @@ nsGlobalWindowOuter::SetNewDocument(nsIDocument* aDocument,
 
   mContext->WillInitializeContext();
 
-  RefPtr<nsGlobalWindowInner> currentInner = GetCurrentInnerWindowInternal();
+  nsGlobalWindowInner *currentInner = GetCurrentInnerWindowInternal();
 
   if (currentInner && currentInner->mNavigator) {
     currentInner->mNavigator->OnNavigation();
@@ -1738,9 +1738,6 @@ nsGlobalWindowOuter::SetNewDocument(nsIDocument* aDocument,
 
   nsCOMPtr<WindowStateHolder> wsh = do_QueryInterface(aState);
   NS_ASSERTION(!aState || wsh, "What kind of weird state are you giving me here?");
-
-  bool handleDocumentOpen = false;
-  bool doomCurrentInner = false;
 
   JS::Rooted<JSObject*> newInnerGlobal(cx);
   if (reUseInnerWindow) {
@@ -1825,13 +1822,32 @@ nsGlobalWindowOuter::SetNewDocument(nsIDocument* aDocument,
 
     if (currentInner && currentInner->GetWrapperPreserveColor()) {
       if (oldDoc == aDocument) {
-        handleDocumentOpen = true;
+        
+        
+        
+        currentInner->AsInner()->CreatePerformanceObjectIfNeeded();
+        if (currentInner->mPerformance) {
+          newInnerWindow->mPerformance =
+            Performance::CreateForMainThread(newInnerWindow->AsInner(),
+                                             aDocument->NodePrincipal(),
+                                             currentInner->mPerformance->GetDOMTiming(),
+                                             currentInner->mPerformance->GetChannel());
+        }
+
+        
+        
+        
+        
+        currentInner->ForEachEventTargetObject(
+          [&] (DOMEventTargetHelper* aDETH, bool* aDoneOut) {
+            aDETH->BindToOwner(newInnerWindow->AsGlobal());
+          });
       }
 
       
       
       if (!currentInner->IsFrozen()) {
-        doomCurrentInner = true;
+        currentInner->FreeInnerObjects();
       }
     }
 
@@ -1981,19 +1997,6 @@ nsGlobalWindowOuter::SetNewDocument(nsIDocument* aDocument,
     
     newInnerWindow->mChromeEventHandler = mChromeEventHandler;
   }
-
-  
-  
-  if (handleDocumentOpen) {
-    newInnerWindow->MigrateStateForDocumentOpen(currentInner);
-  }
-
-  
-  
-  if (doomCurrentInner) {
-    currentInner->FreeInnerObjects();
-  }
-  currentInner = nullptr;
 
   
   
@@ -5347,25 +5350,6 @@ nsGlobalWindowOuter::GetTopWindowRoot()
   return window.forget();
 }
 
-static
-bool IsPopupBlocked(nsIDocument* aDoc)
-{
-  nsCOMPtr<nsIPopupWindowManager> pm =
-    do_GetService(NS_POPUPWINDOWMANAGER_CONTRACTID);
-
-  if (!pm) {
-    return false;
-  }
-
-  if (!aDoc) {
-    return true;
-  }
-
-  uint32_t permission = nsIPopupWindowManager::ALLOW_POPUP;
-  pm->TestPermission(aDoc->NodePrincipal(), &permission);
-  return permission == nsIPopupWindowManager::DENY_POPUP;
-}
-
 void
 nsGlobalWindowOuter::FirePopupBlockedEvent(nsIDocument* aDoc,
                                            nsIURI* aPopupURI,
@@ -5413,8 +5397,10 @@ nsGlobalWindowOuter::CanSetProperty(const char *aPrefName)
 bool
 nsGlobalWindowOuter::PopupWhitelisted()
 {
-  if (!IsPopupBlocked(mDoc))
+  if (mDoc && nsContentUtils::CanShowPopup(mDoc->NodePrincipal()))
+  {
     return true;
+  }
 
   nsCOMPtr<nsPIDOMWindowOuter> parent = GetParent();
   if (parent == this)
