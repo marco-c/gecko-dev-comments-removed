c611a3bccdd5168918ef26439e204d5bf31037da
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 00:04:30 +0000

    Bug 1414322 - Refactor keyboard and visibility tests. r=ato
    Both 'test_text.py' and 'test_typing.py' contained nearly duplicated
    tests for send_keys(), which have been removed from test_text.py now.
    
    The HTML testcase file 'javaScript.html' was used in various tests
    whereby it's initial purpose was to test keyboard events. As such
    unit tests have been updated to use inline HTML, or another more
    generic HTML testcase file. Also parts of the the file related to
    visibility checks have been moved to 'visibility.html'.
    
    MozReview-Commit-ID: CEWPPGdAffW
    
    UltraBlame original commit: c6d4860fcd0a97c99cedf4757048eb39e1903a4d

--- testing/marionette/harness/marionette_harness/tests/unit/test_rendered_element.py
+++ testing/marionette/harness/marionette_harness/tests/unit/test_rendered_element.py
@@ -1,34 +1,28 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+import urllib
 
 from marionette_driver.by import By
-
 from marionette_harness import MarionetteTestCase
+
+
+def inline(doc):
+    return "data:text/html;charset=utf-8,{}".format(urllib.quote(doc))
 
 
 class RenderedElementTests(MarionetteTestCase):
 
-    def testWeCanGetComputedStyleValueOnElement(self):
-        test_url = self.marionette.absolute_url('javascriptPage.html')
-        self.marionette.navigate(test_url)
-        element = self.marionette.find_element(By.ID, "green-parent")
-        backgroundColour = element.value_of_css_property("background-color")
+    def test_get_computed_style_value_from_element(self):
+        self.marionette.navigate(inline("""
+            <div style="color: green;" id="parent">
+              <p id="green">This should be green</p>
+              <p id="red" style="color: red;">But this is red</p>
+            </div>
+            """))
 
-        self.assertEqual("rgb(0, 128, 0)", backgroundColour)
+        parent = self.marionette.find_element(By.ID, "parent")
+        self.assertEqual("rgb(0, 128, 0)", parent.value_of_css_property("color"))
 
-        element = self.marionette.find_element(By.ID, "red-item")
-        backgroundColour = element.value_of_css_property("background-color")
+        green = self.marionette.find_element(By.ID, "green")
+        self.assertEqual("rgb(0, 128, 0)", green.value_of_css_property("color"))
 
-        self.assertEqual("rgb(255, 0, 0)", backgroundColour)
+        red = self.marionette.find_element(By.ID, "red")
+        self.assertEqual("rgb(255, 0, 0)", red.value_of_css_property("color"))
