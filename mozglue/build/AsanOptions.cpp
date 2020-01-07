1b8ccd05cdaeb1df9720cb52c33c6ee4523d6497
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 00:20:03 +0000

    Bug 1419371 - Set max_malloc_fill_size/max_free_fill_size for ASan. r=glandium
    MozReview-Commit-ID: KPng6w8JsHI
    
    UltraBlame original commit: d69d41fa15316ee32d4222f3b4ed2ea2e1dc44dd

--- mozglue/build/AsanOptions.cpp
+++ mozglue/build/AsanOptions.cpp
@@ -40,9 +40,27 @@
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 extern "C" MOZ_ASAN_BLACKLIST
 const char* __asan_default_options() {
     return "allow_user_segv_handler=1:alloc_dealloc_mismatch=0:detect_leaks=0"
+           ":max_free_fill_size=268435456:max_malloc_fill_size=268435456"
+           ":malloc_fill_byte=228:free_fill_byte=229"
 #ifdef MOZ_ASAN_REPORTER
            ":log_path=/tmp/ff_asan_log"
 #endif
