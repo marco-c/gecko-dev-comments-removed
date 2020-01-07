c70333fbcd983715f868ed9ccdb0d2209e73e6df
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 02:43:56 +0000

    Bug 1423857 - MIPS: Add big endian support. r=jandem
    This fixes JIT for MIPS big endian. These are changes in platform
    independent code.
    
    UltraBlame original commit: 3d7ad9a9c2fee4a21d716882580e77ff5b06ea89

--- js/src/wasm/WasmJS.cpp
+++ js/src/wasm/WasmJS.cpp
@@ -68,6 +68,10 @@ wasm::HasCompilerSupport(JSContext* cx)
     if (!wasm::HaveSignalHandlers())
         return false;
 
+#if !MOZ_LITTLE_ENDIAN
+    return false;
+#endif
+
 #ifdef ENABLE_WASM_THREAD_OPS
     
     if (!jit::AtomicOperations::isLockfree8())
