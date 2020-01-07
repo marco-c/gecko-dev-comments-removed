263bfd8d7607ceb16ddfe664d475bed03320c68f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 07:43:20 +0000

    Bug 1443268 - Remove test_places_guid_downgrade.js from sync r=kitcambridge
    MozReview-Commit-ID: 2P8EnTfugmz
    
    UltraBlame original commit: e6069d8a3dafd6bbb06b321562adcd5aa55f9c4e

--- services/common/tests/unit/head_global.js
+++ services/common/tests/unit/head_global.js
@@ -3,7 +3,8 @@
 
 var Cm = Components.manager;
 
-var gSyncProfile = do_get_profile();
+
+do_get_profile();
 ChromeUtils.import("resource://gre/modules/XPCOMUtils.jsm");
 
 ChromeUtils.import("resource://testing-common/AppInfo.jsm", this);
