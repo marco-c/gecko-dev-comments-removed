0c2ff8c7eee488dfd0ce42860ec52bb887e3c6ef
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 01:06:59 +0000

    Bug 1423836 - Make credit card month/year select flexible, and remove unnecessary height by setting resizable=no. r=lchang
    MozReview-Commit-ID: 7IiuIYT7fWK
    
    UltraBlame original commit: 02f8e71f9990dfb5d243ce194442ff53b850a1fe

--- browser/extensions/formautofill/content/manageDialog.js
+++ browser/extensions/formautofill/content/manageDialog.js
@@ -350,7 +350,7 @@ class ManageCreditCards extends ManageRecords {
     
     
     if (!creditCard || !this._hasMasterPassword || await MasterPassword.ensureLoggedIn(true)) {
-      this.prefWin.gSubDialog.open(EDIT_CREDIT_CARD_URL, null, creditCard);
+      this.prefWin.gSubDialog.open(EDIT_CREDIT_CARD_URL, "resizable=no", creditCard);
     }
   }
 
