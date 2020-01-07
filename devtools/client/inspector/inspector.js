1f1ca0eca5ad99dcc1010b61622c0aeac20823cd
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 17:52:01 +0000

    Bug 1458770 - Use the toolbox's Telemetry instance in the Inspector. r=miker
    UltraBlame original commit: 5bc4513eef701a4874bffc984868a15f485a24b4

--- devtools/client/inspector/inspector.js
+++ devtools/client/inspector/inspector.js
@@ -13,7 +13,6 @@ const promise = require("promise");
 const EventEmitter = require("devtools/shared/event-emitter");
 const {executeSoon} = require("devtools/shared/DevToolsUtils");
 const {Toolbox} = require("devtools/client/framework/toolbox");
-const Telemetry = require("devtools/client/shared/telemetry");
 const HighlightersOverlay = require("devtools/client/inspector/shared/highlighters-overlay");
 const ReflowTracker = require("devtools/client/inspector/shared/reflow-tracker");
 const Store = require("devtools/client/inspector/store");
@@ -104,6 +103,7 @@ function Inspector(toolbox) {
   this.panelDoc = window.document;
   this.panelWin = window;
   this.panelWin.inspector = this;
+  this.telemetry = toolbox.telemetry;
 
   this.store = Store();
 
@@ -114,7 +114,6 @@ function Inspector(toolbox) {
   this.highlighters = new HighlightersOverlay(this);
   this.reflowTracker = new ReflowTracker(this._target);
   this.styleChangeTracker = new InspectorStyleChangeTracker(this);
-  this.telemetry = new Telemetry();
 
   
   
@@ -1340,6 +1339,8 @@ Inspector.prototype = {
     this.styleChangeTracker.destroy();
     this.search.destroy();
 
+    this.telemetry = null;
+    this._target = null;
     this._toolbox = null;
     this.breadcrumbs = null;
     this.highlighters = null;
@@ -1354,7 +1355,6 @@ Inspector.prototype = {
     this.show3PaneTooltip = null;
     this.sidebar = null;
     this.store = null;
-    this.target = null;
     this.threePaneTooltip = null;
 
     this._panelDestroyer = promise.all([
@@ -2145,7 +2145,7 @@ Inspector.prototype = {
       return;
     }
 
-    this.telemetry.logScalar("devtools.copy.unique.css.selector.opened", 1);
+    this.telemetry.toolOpened("copyuniquecssselector");
     this.selection.nodeFront.getUniqueSelector().then(selector => {
       clipboardHelper.copyString(selector);
     }).catch(console.error);
@@ -2159,7 +2159,7 @@ Inspector.prototype = {
       return;
     }
 
-    this.telemetry.logScalar("devtools.copy.full.css.selector.opened", 1);
+    this.telemetry.toolOpened("copyfullcssselector");
     this.selection.nodeFront.getCssPath().then(path => {
       clipboardHelper.copyString(path);
     }).catch(console.error);
@@ -2173,7 +2173,7 @@ Inspector.prototype = {
       return;
     }
 
-    this.telemetry.logScalar("devtools.copy.xpath.opened", 1);
+    this.telemetry.toolOpened("copyxpath");
     this.selection.nodeFront.getXPath().then(path => {
       clipboardHelper.copyString(path);
     }).catch(console.error);
