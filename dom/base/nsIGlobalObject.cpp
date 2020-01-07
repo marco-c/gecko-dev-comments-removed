a4cc3eb7b509281627a8b272b8ef647cd10eb28f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 02:36:30 +0000

    Bug 1429486 Expose GetClientInfo() and GetController() on nsIGlobalObject. r=asuth
    UltraBlame original commit: fdccad4541928bdcc0e953b3d7f22441e99dedc0

--- dom/base/nsIGlobalObject.cpp
+++ dom/base/nsIGlobalObject.cpp
@@ -9,6 +9,10 @@
 #include "nsThreadUtils.h"
 #include "nsHostObjectProtocolHandler.h"
 
+using mozilla::Maybe;
+using mozilla::dom::ClientInfo;
+using mozilla::dom::ServiceWorkerDescriptor;
+
 nsIGlobalObject::~nsIGlobalObject()
 {
   UnlinkHostObjectURIs();
@@ -111,4 +115,20 @@ nsIGlobalObject::TraverseHostObjectURIs(nsCycleCollectionTraversalCallback &aCb)
   for (uint32_t index = 0; index < mHostObjectURIs.Length(); ++index) {
     nsHostObjectProtocolHandler::Traverse(mHostObjectURIs[index], aCb);
   }
+}
+
+Maybe<ClientInfo>
+nsIGlobalObject::GetClientInfo() const
+{
+  
+  
+  return Maybe<ClientInfo>();
+}
+
+Maybe<ServiceWorkerDescriptor>
+nsIGlobalObject::GetController() const
+{
+  
+  
+  return Maybe<ServiceWorkerDescriptor>();
 }
