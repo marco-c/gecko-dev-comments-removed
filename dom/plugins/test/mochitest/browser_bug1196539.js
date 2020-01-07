9e796e6904da0498f7672bb92030383358fa182a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 18:29:39 +0000

    Bug 1176019 - Fix browser_bug1196539.js painting check r=mconley
    MozReview-Commit-ID: HgzcSIdIh1h
    
    UltraBlame original commit: 9c6b0eb062ca41adb298dd5a5170fe0649c8e8d7

--- dom/plugins/test/mochitest/browser_bug1196539.js
+++ dom/plugins/test/mochitest/browser_bug1196539.js
@@ -14,6 +14,10 @@ const kMaxPaints = 10;
 add_task(async function() {
   let result, tabSwitchedPromise;
 
+  
+  
+  await SpecialPowers.pushPrefEnv({set: [["browser.tabs.remote.tabCacheSize", 0]]});
+
   setTestPluginEnabledState(Ci.nsIPluginTag.STATE_ENABLED, "Test Plug-in");
 
   let testTab = gBrowser.selectedTab;
