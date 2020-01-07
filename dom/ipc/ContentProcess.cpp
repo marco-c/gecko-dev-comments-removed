b58e8977be78ff66094019f4bc313b93a8122456
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 09:41:14 +0000

    Bug 1447246 - Refactor ContentProcess::Init(). r=jimm
    The current code is a bit of a mess. This patch does the following.
    
    - Changes the processing from backwards to forwards. This avoids the need for
      all the `found` booleans, because if a flag is present multiple times, the
      last one will naturally override.
    
    - Tightens up the checking. It now doesn't use assertions, but instead returns
      false if any of the options are missing arguments, or have malformed
      arguments, or any of the mandatory flags are missing. (It assumes that
      -appdir and -profile are optional.)
    
    - Renames the loop variable `idx` as `i`.
    
    - Changes `!strcmp(...)` to `strcmp(...) == 0`, because I find that clearer.
    
    - Avoids a redundant nsCString when handling -appdir.
    
    The patch also tweaks GeckoChildProcessHost::mGroupId, which was buggy. It
    holds the appModelUserId argument, which XRE_InitChildProcess() always expects
    is present in the command. But it's only set to a non-empty value in
    InitWindowsGroupID(), which is only called for plugin processes. So in lots of
    cases the appModelUserId argument was missing, and a different argument would
    be interpreted as the appModelUserId argument (seemingly without noticeable ill
    effect).
    
    The patch changes things to mGroupId defaults to "-", which means it's always
    present in the command.
    
    Note: all this explains why the old code for ContentProcess::Init() started
    processing from argument aArgc, instead of aArgc-1 as you might expect -- it
    had to read one extra arg in order to see the argument following -appdir,
    because XRE_InitChildProcess() was decrementing aArgc for the appModelUserId
    argument even when that argument wasn't present. The new code for
    ContentProcess::Init() doesn't have to read past aArgc-1 because the mGroupId
    fix ensures the appModelUserId argument is always present.
    
    MozReview-Commit-ID: 8a8k6ABYMgo
    
    UltraBlame original commit: 7948eefa9274b05fe48692eb69990700f264857b

--- dom/ipc/ContentProcess.cpp
+++ dom/ipc/ContentProcess.cpp
@@ -94,146 +94,132 @@ SetPrefsFd(int aFd)
 bool
 ContentProcess::Init(int aArgc, char* aArgv[])
 {
-  
-  bool foundAppdir = false;
-  bool foundChildID = false;
-  bool foundIsForBrowser = false;
-#ifdef XP_WIN
-  bool foundPrefsHandle = false;
-#endif
-  bool foundPrefsLen = false;
-  bool foundSchedulerPrefs = false;
-
-  uint64_t childID;
-  bool isForBrowser;
-
+  Maybe<uint64_t> childID;
+  Maybe<bool> isForBrowser;
+  Maybe<base::SharedMemoryHandle> prefsHandle;
+  Maybe<size_t> prefsLen;
+  Maybe<const char*> schedulerPrefs;
 #if defined(XP_MACOSX) && defined(MOZ_CONTENT_SANDBOX)
-  
-  bool foundProfile = false;
   nsCOMPtr<nsIFile> profileDir;
 #endif
 
-  char* schedulerPrefs = nullptr;
-  base::SharedMemoryHandle prefsHandle = base::SharedMemory::NULLHandle();
-  size_t prefsLen = 0;
-  for (int idx = aArgc; idx > 0; idx--) {
-    if (!aArgv[idx]) {
+  for (int i = 1; i < aArgc; i++) {
+    if (!aArgv[i]) {
       continue;
     }
 
-    if (!strcmp(aArgv[idx], "-appdir")) {
-      MOZ_ASSERT(!foundAppdir);
-      if (foundAppdir) {
-        continue;
+    if (strcmp(aArgv[i], "-appdir") == 0) {
+      if (++i == aArgc) {
+        return false;
       }
-      nsCString appDir;
-      appDir.Assign(nsDependentCString(aArgv[idx+1]));
+      nsDependentCString appDir(aArgv[i]);
       mXREEmbed.SetAppDir(appDir);
-      foundAppdir = true;
-    } else if (!strcmp(aArgv[idx], "-childID")) {
-      MOZ_ASSERT(!foundChildID);
-      if (foundChildID) {
-        continue;
+
+    } else if (strcmp(aArgv[i], "-childID") == 0) {
+      if (++i == aArgc) {
+        return false;
       }
-      if (idx + 1 < aArgc) {
-        childID = strtoull(aArgv[idx + 1], nullptr, 10);
-        foundChildID = true;
+      char* str = aArgv[i];
+      childID = Some(strtoull(str, &str, 10));
+      if (str[0] != '\0') {
+        return false;
       }
-    } else if (!strcmp(aArgv[idx], "-isForBrowser") || !strcmp(aArgv[idx], "-notForBrowser")) {
-      MOZ_ASSERT(!foundIsForBrowser);
-      if (foundIsForBrowser) {
-        continue;
-      }
-      isForBrowser = strcmp(aArgv[idx], "-notForBrowser");
-      foundIsForBrowser = true;
+
+    } else if (strcmp(aArgv[i], "-isForBrowser") == 0) {
+      isForBrowser = Some(true);
+
+    } else if (strcmp(aArgv[i], "-notForBrowser") == 0) {
+      isForBrowser = Some(false);
+
 #ifdef XP_WIN
-    } else if (!strcmp(aArgv[idx], "-prefsHandle")) {
-      char* str = aArgv[idx + 1];
-      MOZ_ASSERT(str[0] != '\0');
+    } else if (strcmp(aArgv[i], "-prefsHandle") == 0) {
+      if (++i == aArgc) {
+        return false;
+      }
       
       
-      prefsHandle = reinterpret_cast<HANDLE>(strtoull(str, &str, 10));
-      MOZ_ASSERT(str[0] == '\0');
-      foundPrefsHandle = true;
+      
+      char* str = aArgv[i];
+      prefsHandle = Some(reinterpret_cast<HANDLE>(strtoull(str, &str, 10)));
+      if (str[0] != '\0') {
+        return false;
+      }
 #endif
-    } else if (!strcmp(aArgv[idx], "-prefsLen")) {
-      char* str = aArgv[idx + 1];
-      MOZ_ASSERT(str[0] != '\0');
+
+    } else if (strcmp(aArgv[i], "-prefsLen") == 0) {
+      if (++i == aArgc) {
+        return false;
+      }
       
       
-      prefsLen = strtoull(str, &str, 10);
-      MOZ_ASSERT(str[0] == '\0');
-      foundPrefsLen = true;
-    } else if (!strcmp(aArgv[idx], "-schedulerPrefs")) {
-      schedulerPrefs = aArgv[idx + 1];
-      foundSchedulerPrefs = true;
-    } else if (!strcmp(aArgv[idx], "-safeMode")) {
+      
+      char* str = aArgv[i];
+      prefsLen = Some(strtoull(str, &str, 10));
+      if (str[0] != '\0') {
+        return false;
+      }
+
+    } else if (strcmp(aArgv[i], "-schedulerPrefs") == 0) {
+      if (++i == aArgc) {
+        return false;
+      }
+      schedulerPrefs = Some(aArgv[i]);
+
+    } else if (strcmp(aArgv[i], "-safeMode") == 0) {
       gSafeMode = true;
-    }
 
 #if defined(XP_MACOSX) && defined(MOZ_CONTENT_SANDBOX)
-    else if (!strcmp(aArgv[idx], "-profile")) {
-      MOZ_ASSERT(!foundProfile);
-      if (foundProfile) {
-        continue;
+    } else if (strcmp(aArgv[i], "-profile") == 0) {
+      if (++i == aArgc) {
+        return false;
       }
       bool flag;
-      nsresult rv = XRE_GetFileFromPath(aArgv[idx+1], getter_AddRefs(profileDir));
-      if (NS_FAILED(rv) ||
-          NS_FAILED(profileDir->Exists(&flag)) || !flag) {
+      nsresult rv = XRE_GetFileFromPath(aArgv[i], getter_AddRefs(profileDir));
+      if (NS_FAILED(rv) || NS_FAILED(profileDir->Exists(&flag)) || !flag) {
         NS_WARNING("Invalid profile directory passed to content process.");
         profileDir = nullptr;
       }
-      foundProfile = true;
-    }
-#endif 
-
-    bool allFound = foundAppdir
-                 && foundChildID
-                 && foundIsForBrowser
-                 && foundPrefsLen
-                 && foundSchedulerPrefs
-#ifdef XP_WIN
-                 && foundPrefsHandle
 #endif
-#if defined(XP_MACOSX) && defined(MOZ_CONTENT_SANDBOX)
-                 && foundProfile
-#endif
-                 && true;
-
-    if (allFound) {
-      break;
     }
   }
 
 #ifdef ANDROID
   
   MOZ_RELEASE_ASSERT(gPrefsFd != -1);
-  prefsHandle = base::FileDescriptor(gPrefsFd,  true);
+  prefsHandle = Some(base::FileDescriptor(gPrefsFd,  true));
 #elif XP_UNIX
-  prefsHandle = base::FileDescriptor(kPrefsFileDescriptor,
-                                      true);
+  prefsHandle = Some(base::FileDescriptor(kPrefsFileDescriptor,
+                                           true));
 #endif
 
   
+  if (childID.isNothing() ||
+      isForBrowser.isNothing() ||
+      prefsHandle.isNothing() ||
+      prefsLen.isNothing() ||
+      schedulerPrefs.isNothing()) {
+    return false;
+  }
+
+  
   base::SharedMemory shm;
-  if (!shm.SetHandle(prefsHandle,  true)) {
+  if (!shm.SetHandle(*prefsHandle,  true)) {
     NS_ERROR("failed to open shared memory in the child");
     return false;
   }
-  if (!shm.Map(prefsLen)) {
+  if (!shm.Map(*prefsLen)) {
     NS_ERROR("failed to map shared memory in the child");
     return false;
   }
   Preferences::DeserializePreferences(static_cast<char*>(shm.memory()),
-                                      prefsLen);
+                                      *prefsLen);
 
-  Scheduler::SetPrefs(schedulerPrefs);
+  Scheduler::SetPrefs(*schedulerPrefs);
   mContent.Init(IOThreadChild::message_loop(),
                 ParentPid(),
                 IOThreadChild::channel(),
-                childID,
-                isForBrowser);
+                *childID,
+                *isForBrowser);
   mXREEmbed.Start();
 #if (defined(XP_MACOSX)) && defined(MOZ_CONTENT_SANDBOX)
   mContent.SetProfileDir(profileDir);
