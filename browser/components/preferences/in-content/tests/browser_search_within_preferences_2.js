41a50afbc5bc10cd1c31e0bb92c59859e9b446e2
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 09:35:55 +0000

    Bug 1447667 - Use a different element in preferences search test. r=jaws
    MozReview-Commit-ID: 94HVZ9zLhH5
    
    UltraBlame original commit: 6d78d6d31e09e4aba6bb49fecbe064c38067b38b

--- browser/components/preferences/in-content/tests/browser_search_within_preferences_2.js
+++ browser/components/preferences/in-content/tests/browser_search_within_preferences_2.js
@@ -83,13 +83,13 @@ add_task(async function() {
   
   {
     let searchInput = gBrowser.contentDocument.getElementById("searchInput");
-    let bcsElem = gBrowser.contentDocument.getElementById("browserContainersSettings");
+    let suhElem = gBrowser.contentDocument.getElementById("showUpdateHistory");
 
     is(searchInput, gBrowser.contentDocument.activeElement.closest("#searchInput"),
       "Search input should be focused when visiting preferences");
 
-    ok(!bcsElem.getAttribute("search-l10n-ids").includes(l10nId),
-      "browserContainersSettings element should not contain the l10n id here.");
+    ok(!suhElem.getAttribute("search-l10n-ids").includes(l10nId),
+      "showUpdateHistory element should not contain the l10n id here.");
 
     let query = "Language";
     let searchCompletedPromise = BrowserTestUtils.waitForEvent(
@@ -97,7 +97,7 @@ add_task(async function() {
     EventUtils.sendString(query);
     await searchCompletedPromise;
 
-    is_element_hidden(bcsElem, "browserContainersSettings should not be in search results");
+    is_element_hidden(suhElem, "showUpdateHistory should not be in search results");
   }
 
   await BrowserTestUtils.removeTab(gBrowser.selectedTab);
@@ -112,8 +112,8 @@ add_task(async function() {
     is(searchInput, gBrowser.contentDocument.activeElement.closest("#searchInput"),
       "Search input should be focused when visiting preferences");
 
-    let bcsElem = gBrowser.contentDocument.getElementById("browserContainersSettings");
-    bcsElem.setAttribute("search-l10n-ids", l10nId);
+    let suhElem = gBrowser.contentDocument.getElementById("showUpdateHistory");
+    suhElem.setAttribute("search-l10n-ids", l10nId);
 
     let query = "Language";
     let searchCompletedPromise = BrowserTestUtils.waitForEvent(
@@ -121,7 +121,7 @@ add_task(async function() {
     EventUtils.sendString(query);
     await searchCompletedPromise;
 
-    is_element_visible(bcsElem, "browserContainersSettings should be in search results");
+    is_element_visible(suhElem, "showUpdateHistory should be in search results");
   }
 
   await BrowserTestUtils.removeTab(gBrowser.selectedTab);
