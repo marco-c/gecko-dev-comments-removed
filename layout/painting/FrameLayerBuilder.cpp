110af9a647404ad71bbc4935f70926dc84cb428a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 11:05:33 +0000

    Backed out changeset c9db3f332a19 (bug 1440753) for content sometimes not being rendered (bug 1451597). a=backout
    UltraBlame original commit: 1b258f938525fda65ef80ffa0408bc665d5d8948

--- layout/painting/FrameLayerBuilder.cpp
+++ layout/painting/FrameLayerBuilder.cpp
@@ -5934,7 +5934,7 @@ FrameLayerBuilder::RecomputeVisibilityForItems(nsTArray<AssignedDisplayItem>& aI
       newVisible.Sub(visible, removed);
       
       if (newVisible.GetNumRects() <= 15) {
-        visible = Move(newVisible);
+        visible = newVisible;
       }
     }
   }
