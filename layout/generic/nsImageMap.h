086428290725461e7fac298ec5684266a7026cd4
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 01:08:53 +0000

    Bug 1424952: Modernize a bit more nsImageMap, and make removals a bit more incremental. r=dholbert
    Summary: MozReview-Commit-ID: 4iXJtLKEwY7
    
    Reviewers: dholbert
    
    Reviewed By: dholbert
    
    Bug #: 1424952
    
    Differential Revision: https://phabricator.services.mozilla.com/D336
    
    MozReview-Commit-ID: 3FANAhS06Z5
    
    UltraBlame original commit: 3838a6073eb7ffcf9a5cb053b80964ed548e895e

--- layout/generic/nsImageMap.h
+++ layout/generic/nsImageMap.h
@@ -82,6 +82,8 @@ public:
   nsresult GetBoundsForAreaContent(nsIContent *aContent,
                                    nsRect& aBounds);
 
+  using AreaList = AutoTArray<mozilla::UniquePtr<Area>, 8>;
+
 protected:
   virtual ~nsImageMap();
 
@@ -92,12 +94,15 @@ protected:
   void SearchForAreas(nsIContent* aParent);
 
   void AddArea(mozilla::dom::HTMLAreaElement* aArea);
+  void AreaRemoved(mozilla::dom::HTMLAreaElement* aArea);
 
   void MaybeUpdateAreas(nsIContent *aContent);
 
   nsImageFrame* mImageFrame;  
   nsCOMPtr<nsIContent> mMap;
-  AutoTArray<Area*, 8> mAreas; 
+
+  
+  AreaList mAreas;
 
   
   
