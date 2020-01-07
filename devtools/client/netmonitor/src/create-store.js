fd12f5e5f289f2a966ac3c3a09ba91be4a1fea34
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 07:01:43 +0000

    Bug 1419350 - Stop doing React updates while netmonitor is in background; r=ochameau
    MozReview-Commit-ID: FvKFDiM8xNB
    
    UltraBlame original commit: a5574f24919beb4431287fbe92762a0a154e5052

--- devtools/client/netmonitor/src/create-store.js
+++ devtools/client/netmonitor/src/create-store.js
@@ -8,18 +8,18 @@ const Services = require("Services");
 const { applyMiddleware, createStore } = require("devtools/client/shared/vendor/redux");
 
 
-const batching = require("../middleware/batching");
-const prefs = require("../middleware/prefs");
-const thunk = require("../middleware/thunk");
-const recording = require("../middleware/recording");
+const batching = require("./middleware/batching");
+const prefs = require("./middleware/prefs");
+const thunk = require("./middleware/thunk");
+const recording = require("./middleware/recording");
 
 
-const rootReducer = require("../reducers/index");
-const { FilterTypes, Filters } = require("../reducers/filters");
-const { Requests } = require("../reducers/requests");
-const { Sort } = require("../reducers/sort");
-const { TimingMarkers } = require("../reducers/timing-markers");
-const { UI, Columns } = require("../reducers/ui");
+const rootReducer = require("./reducers/index");
+const { FilterTypes, Filters } = require("./reducers/filters");
+const { Requests } = require("./reducers/requests");
+const { Sort } = require("./reducers/sort");
+const { TimingMarkers } = require("./reducers/timing-markers");
+const { UI, Columns } = require("./reducers/ui");
 
 
 
