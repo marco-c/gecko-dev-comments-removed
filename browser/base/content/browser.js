5893113f7d60121c620bb9e363b39b58a028747d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 00:06:08 +0000

    Bug 1418020: No need to hide form validation popoup onLocationChange. r=Felipe
    We already hide the popup when element blurs, which is fired when navigating to
    another page.
    
    MozReview-Commit-ID: 5GvlX1CiOQL
    
    UltraBlame original commit: 067b1e9bee3dcaa2b56d90daec19f62f8803de96

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -4585,9 +4585,6 @@ var XULBrowserWindow = {
   onLocationChange(aWebProgress, aRequest, aLocationURI, aFlags) {
     var location = aLocationURI ? aLocationURI.spec : "";
 
-    
-    FormValidationHandler.hidePopup();
-
     let pageTooltip = document.getElementById("aHTMLTooltip");
     let tooltipNode = pageTooltip.triggerNode;
     if (tooltipNode) {
