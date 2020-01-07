2c73daa93767dfed197b47cfd7af5d44d71c2edd
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 13:04:25 +0000

    Bug 1455226 - Load memory/initializer.js via browserRequire instead of a script tag;r=ochameau
    "async" methods defined in scripts loaded via a <script> tag seem
    to indirectly use the document's Promise, which is not usable after
    the DOM of the document has been destroyed.
    
    This is an issue if we invoke this code during the destroy of the
    toolbox.
    
    MozReview-Commit-ID: C8juQqJlVDN
    
    UltraBlame original commit: ff1831623026f2f3b7e208c63810fee500b7e565

--- devtools/client/memory/initializer.js
+++ devtools/client/memory/initializer.js
@@ -6,57 +6,53 @@
 
 "use strict";
 
-const BrowserLoaderModule = {};
-ChromeUtils.import("resource://devtools/client/shared/browser-loader.js", BrowserLoaderModule);
-const { require } = BrowserLoaderModule.BrowserLoader({
-  baseURI: "resource://devtools/client/memory/",
-  window
-});
-const { Task } = require("devtools/shared/task");
 const { createFactory, createElement } = require("devtools/client/shared/vendor/react");
 const ReactDOM = require("devtools/client/shared/vendor/react-dom");
 const { Provider } = require("devtools/client/shared/vendor/react-redux");
 const App = createFactory(require("devtools/client/memory/app"));
 const Store = require("devtools/client/memory/store");
 const { assert } = require("devtools/shared/DevToolsUtils");
-const Promise = require("Promise");
 
 
+let root, store, unsubscribe;
 
+const initialize = async function() {
+  
+  let { gFront, gToolbox, gHeapAnalysesClient } = window;
 
+  root = document.querySelector("#app");
+  store = Store();
+  const app = createElement(App, {
+    toolbox: gToolbox,
+    front: gFront,
+    heapWorker: gHeapAnalysesClient
+  });
+  const provider = createElement(Provider, { store }, app);
+  ReactDOM.render(provider, root);
+  unsubscribe = store.subscribe(onStateChange);
 
-var gToolbox, gFront, gHeapAnalysesClient;
+  
+  window.gStore = store;
+};
 
-
-
-
-var gStore, gRoot, gApp, gProvider, unsubscribe, isHighlighted;
-
-var initialize = Task.async(function* () {
-  gRoot = document.querySelector("#app");
-  gStore = Store();
-  gApp = createElement(App,
-    { toolbox: gToolbox, front: gFront, heapWorker: gHeapAnalysesClient });
-  gProvider = createElement(Provider, { store: gStore }, gApp);
-  ReactDOM.render(gProvider, gRoot);
-  unsubscribe = gStore.subscribe(onStateChange);
-});
-
-var destroy = Task.async(function* () {
-  const ok = ReactDOM.unmountComponentAtNode(gRoot);
+const destroy = async function() {
+  const ok = ReactDOM.unmountComponentAtNode(root);
   assert(ok, "Should successfully unmount the memory tool's top level React component");
 
   unsubscribe();
+};
 
-  gStore = gRoot = gApp = gProvider = unsubscribe = isHighlighted = null;
-});
+
+let isHighlighted;
 
 
 
 
 
 function onStateChange() {
-  let isRecording = gStore.getState().allocations.recording;
+  let { gToolbox } = window;
+
+  let isRecording = store.getState().allocations.recording;
   if (isRecording === isHighlighted) {
     return;
   }
@@ -69,3 +65,5 @@ function onStateChange() {
 
   isHighlighted = isRecording;
 }
+
+module.exports = { initialize, destroy };
