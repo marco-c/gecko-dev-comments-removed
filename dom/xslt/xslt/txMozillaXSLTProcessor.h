35ddaac02dbe2b8aa9dfe5f57a2f76c5a599bbbb
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 04:48:17 +0000

    Bug 1435430 part 5.  Remove nsIXSLTProcessor::SetParameter.  r=mystor
    MozReview-Commit-ID: 47Pc4Puub4T
    
    UltraBlame original commit: 578a73b5b1781141090be88fcfefdec90c26685f

--- dom/xslt/xslt/txMozillaXSLTProcessor.h
+++ dom/xslt/xslt/txMozillaXSLTProcessor.h
@@ -160,6 +160,11 @@ private:
     void notifyError();
     nsresult ensureStylesheet();
 
+    
+    nsresult SetParameter(const nsAString& aNamespaceURI,
+                          const nsAString& aLocalName,
+                          nsIVariant* aValue);
+
     nsCOMPtr<nsISupports> mOwner;
 
     RefPtr<txStylesheet> mStylesheet;
