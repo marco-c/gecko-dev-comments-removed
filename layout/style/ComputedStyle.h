763945da44a1bca077a8967633cd06eab7ee900a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 11:27:13 +0000

    Merge inbound to mozilla-central. a=merge
    UltraBlame original commit: 15678b283f0f62b7a27afba6e572ef8961d46c69

--- layout/style/ComputedStyle.h
+++ layout/style/ComputedStyle.h
@@ -114,7 +114,9 @@ public:
                 CSSPseudoElementType aPseudoType,
                 ServoComputedDataForgotten aComputedValues);
 
-  nsPresContext* PresContext() const { return mPresContext; }
+  
+  
+  nsPresContext* PresContextForFrame() const { return mPresContext; }
   const ServoComputedData* ComputedData() const { return &mSource; }
 
   
@@ -382,12 +384,6 @@ public:
 
   inline void StartBackgroundImageLoads();
 
-  static bool IsReset(const nsStyleStructID aSID) {
-    MOZ_ASSERT(0 <= aSID && aSID < nsStyleStructID_Length,
-               "must be an inherited or reset SID");
-    return nsStyleStructID_Reset_Start <= aSID;
-  }
-  static bool IsInherited(const nsStyleStructID aSID) { return !IsReset(aSID); }
   static uint32_t GetBitForSID(const nsStyleStructID aSID) { return 1 << aSID; }
 
 #ifdef DEBUG
