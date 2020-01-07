c0b6330fa1d86242f9ccf47fb9349e2c19efadba
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 13:16:05 +0000

    Bug 1452897 [wpt PR 10362] - [css-typed-om] Add support for font properties., a=testonly
    Automatic update from web-platform-tests[css-typed-om] Add support for font properties.
    
    Had to modify the test harness to allow numbers in keywords. This
    coincidentally fixed an error in one of the tests (transform-style).
    
    Bug: 820299
    Change-Id: Ia7e2854f38a5b960e6537d2c4ba309cb71c1e963
    Reviewed-on: https://chromium-review.googlesource.com/999225
    Reviewed-by: nainar <nainarchromium.org>
    Commit-Queue: Darren Shen <shendchromium.org>
    Cr-Commit-Position: refs/heads/master{#549473}
    
    --
    
    wpt-commits: e62fc73167b0c090d687e7c4c632745c5e98860a
    wpt-pr: 10362
    wpt-commits: e62fc73167b0c090d687e7c4c632745c5e98860a
    wpt-pr: 10362
    
    UltraBlame original commit: 83121c3424a5d778c9bf976db0273101c2463ed9

--- testing/web-platform/tests/css/css-typed-om/the-stylepropertymap/properties/resources/testsuite.js
+++ testing/web-platform/tests/css/css-typed-om/the-stylepropertymap/properties/resources/testsuite.js
@@ -389,7 +389,7 @@ function runPropertyTests(propertyName, testCases) {
 
     
     
-    const syntaxExamples = testCase.syntax.toLowerCase().match(/^[a-z\-]+$/) ?
+    const syntaxExamples = testCase.syntax.toLowerCase().match(/^[a-z0-9\-]+$/) ?
       createKeywordExample(testCase.syntax) :
       gTestSyntaxExamples[testCase.syntax];
 
