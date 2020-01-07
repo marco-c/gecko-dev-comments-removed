a7f0b4c8dd02b88a7a1549a526ab7118c376b4a3
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 11:26:31 +0000

    Bug 1452080: Remove ComputedStyle::PresContext usage in animation code. r=hiro
    MozReview-Commit-ID: HwooTF9PGnq
    
    UltraBlame original commit: bffebe8aa2549848be300e0c96b29477425721d3

--- dom/animation/KeyframeEffectReadOnly.h
+++ dom/animation/KeyframeEffectReadOnly.h
@@ -166,7 +166,7 @@ public:
   void SetKeyframes(JSContext* aContext, JS::Handle<JSObject*> aKeyframes,
                     ErrorResult& aRv);
   void SetKeyframes(nsTArray<Keyframe>&& aKeyframes,
-                    const ComputedStyle* aComputedValues);
+                    const ComputedStyle* aStyle);
 
   
   
@@ -244,8 +244,7 @@ public:
 
   
   
-  template<typename StyleType>
-  void CalculateCumulativeChangeHint(StyleType* aComputedStyle);
+  void CalculateCumulativeChangeHint(const ComputedStyle* aStyle);
 
   
   
@@ -379,13 +378,6 @@ protected:
 private:
   nsChangeHint mCumulativeChangeHint;
 
-  template<typename StyleType>
-  void DoSetKeyframes(nsTArray<Keyframe>&& aKeyframes, StyleType* aStyle);
-
-  template<typename StyleType>
-  void DoUpdateProperties(StyleType* aStyle);
-
-
   void ComposeStyleRule(RawServoAnimationValueMap& aAnimationValues,
                         const AnimationProperty& aProperty,
                         const AnimationPropertySegment& aSegment,
@@ -395,6 +387,7 @@ private:
   already_AddRefed<ComputedStyle> CreateComputedStyleForAnimationValue(
     nsCSSPropertyID aProperty,
     const AnimationValue& aValue,
+    nsPresContext* aPresContext,
     const ComputedStyle* aBaseComputedStyle);
 
   nsIFrame* GetAnimationFrame() const;
