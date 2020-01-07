208a579b80f6793b91053049b9d8f451ad55b52e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 21:59:07 +0000

    Bug 1466490 - Actually record non-main scalars on the main process after deserialization/recording. r=Dexter
    MozReview-Commit-ID: HNGq5kVVc3k
    
    UltraBlame original commit: e3891caf027f304b6b9c2a6040edc7053daba8ea

--- toolkit/components/telemetry/TelemetryScalar.cpp
+++ toolkit/components/telemetry/TelemetryScalar.cpp
@@ -1122,9 +1122,11 @@ internal_CanRecordForScalarID(const StaticMutexAutoLock& lock,
 
 
 
+
+
 ScalarResult
 internal_CanRecordScalar(const StaticMutexAutoLock& lock, const ScalarKey& aId,
-                         bool aKeyed)
+                         bool aKeyed, bool aForce = false)
 {
   
   if (internal_IsKeyedScalar(lock, aId) != aKeyed) {
@@ -1138,7 +1140,7 @@ internal_CanRecordScalar(const StaticMutexAutoLock& lock, const ScalarKey& aId,
   }
 
   
-  if (!internal_CanRecordProcess(lock, aId)) {
+  if (!aForce && !internal_CanRecordProcess(lock, aId)) {
     return ScalarResult::CannotRecordInProcess;
   }
 
@@ -1427,7 +1429,7 @@ internal_UpdateScalar(const StaticMutexAutoLock& lock, const nsACString& aName,
            ScalarResult::NotInitialized : ScalarResult::UnknownScalar;
   }
 
-  ScalarResult sr = internal_CanRecordScalar(lock, uniqueId, false);
+  ScalarResult sr = internal_CanRecordScalar(lock, uniqueId, false, aForce);
   if (sr != ScalarResult::Ok) {
     if (sr == ScalarResult::CannotRecordDataset) {
       return ScalarResult::Ok;
@@ -1605,7 +1607,7 @@ internal_UpdateKeyedScalar(const StaticMutexAutoLock& lock,
            ScalarResult::NotInitialized : ScalarResult::UnknownScalar;
   }
 
-  ScalarResult sr = internal_CanRecordScalar(lock, uniqueId, true);
+  ScalarResult sr = internal_CanRecordScalar(lock, uniqueId, true, aForce);
   if (sr != ScalarResult::Ok) {
     if (sr == ScalarResult::CannotRecordDataset) {
       return ScalarResult::Ok;
