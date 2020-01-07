a5213e6ff6960ca5a8a1553e7fe84ee0ca90ef68
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 04:09:14 +0000

    Bug 1432977 part 1.  Remove nsIDOMHTMLElement's draggable attribute.  r=mccr8
    MozReview-Commit-ID: Gj8thsaNGCF
    
    UltraBlame original commit: e0759dfa715ed7bbeca71b3ee37574c59b0e9f29

--- dom/events/EventStateManager.cpp
+++ dom/events/EventStateManager.cpp
@@ -28,6 +28,7 @@
 
 #include "nsCOMPtr.h"
 #include "nsFocusManager.h"
+#include "nsGenericHTMLElement.h"
 #include "nsIContent.h"
 #include "nsIContentInlines.h"
 #include "nsIDocument.h"
@@ -1991,16 +1992,13 @@ EventStateManager::DetermineDragTargetAndDefaultData(nsPIDOMWindowOuter* aWindow
   
   if (!*aSelection) {
     while (dragContent) {
-      nsCOMPtr<nsIDOMHTMLElement> htmlElement = do_QueryInterface(dragContent);
-      if (htmlElement) {
-        bool draggable = false;
-        htmlElement->GetDraggable(&draggable);
-        if (draggable)
+      if (auto htmlElement = nsGenericHTMLElement::FromContent(dragContent)) {
+        if (htmlElement->Draggable()) {
           break;
+        }
       }
       else {
-        nsCOMPtr<nsIDOMXULElement> xulElement = do_QueryInterface(dragContent);
-        if (xulElement) {
+        if (dragContent->IsXULElement()) {
           
           
           
