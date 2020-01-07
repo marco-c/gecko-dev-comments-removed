d6554024e689730a5914084c3219ca0a6ec516ae
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 05:19:11 +0000

    Bug 1431960 - Forcibly construct the frame for browser to construct browser binding in preferences subdialog. r=bz
    UltraBlame original commit: deea96928bddafcc4ca2924368b70cd1cbdc6c3c

--- browser/components/preferences/in-content/subdialogs.js
+++ browser/components/preferences/in-content/subdialogs.js
@@ -72,6 +72,16 @@ SubDialog.prototype = {
   async open(aURL, aFeatures = null, aParams = null, aClosingCallback = null) {
     
     await this._frameCreated;
+
+    if (!this._frame.contentWindow) {
+      
+      
+      
+      
+      
+      this._frame.getBoundingClientRect();
+    }
+
     
     if (this._openedURL || this._isClosing) {
       if (!this._isClosing) {
@@ -525,6 +535,12 @@ var gSubDialog = {
       return;
     }
 
+    if (this._dialogs.length == 0) {
+      
+      
+      this._dialogStack.hidden = false;
+    }
+
     this._preloadDialog.open(aURL, aFeatures, aParams, aClosingCallback);
     this._dialogs.push(this._preloadDialog);
     this._preloadDialog = new SubDialog({template: this._dialogTemplate,
@@ -532,7 +548,6 @@ var gSubDialog = {
                                          id: this._nextDialogID++});
 
     if (this._dialogs.length == 1) {
-      this._dialogStack.hidden = false;
       this._ensureStackEventListeners();
     }
   },
