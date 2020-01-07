e2a8faa974e3033d2589fd2d2036706650ca9e5b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 05:11:35 +0000

    Bug 1429932 - Part 1: Remove mFireAfterPaintEvents and use mTransactions instead. r=tnikkel
    This fixes a bug where EnsureEventualDidPaintEvent needs to be called separately for each transaction id, but we skip it since mFireAfterPaintEvents is still true from the previous paint.
    We now track the equivalent state by checking for the presence of mTransactions[aTransactionId], and correctly schedule an eventual didpaint for each id.
    
    MozReview-Commit-ID: JnRTycGEyom
    
    UltraBlame original commit: 39ffcc432d0c04641acf3be018f1919a9d620893

--- layout/base/nsPresContext.h
+++ layout/base/nsPresContext.h
@@ -1291,6 +1291,12 @@ protected:
                                          const char* aName,
                                          uint32_t aDelay);
 
+  struct TransactionInvalidations {
+    uint64_t mTransactionId;
+    nsTArray<nsRect> mInvalidations;
+  };
+  TransactionInvalidations* GetInvalidations(uint64_t aTransactionId);
+
   
   
   
@@ -1359,10 +1365,6 @@ protected:
 
   mozilla::UniquePtr<nsBidi> mBidiEngine;
 
-  struct TransactionInvalidations {
-    uint64_t mTransactionId;
-    nsTArray<nsRect> mInvalidations;
-  };
   AutoTArray<TransactionInvalidations, 4> mTransactions;
 
   
@@ -1488,8 +1490,6 @@ protected:
   unsigned              mSuppressResizeReflow : 1;
 
   unsigned              mIsVisual : 1;
-
-  unsigned              mFireAfterPaintEvents : 1;
 
   unsigned              mIsChrome : 1;
   unsigned              mIsChromeOriginImage : 1;
