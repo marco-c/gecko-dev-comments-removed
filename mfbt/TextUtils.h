860a244b3f2ad7e8baa8326e37e0149e5f9908c2
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 12:05:23 +0000

    Bug 1453456 - Add/use more ASCII alphanumeric testing/conversion functions from MFBT in SpiderMonkey code in various places.  r=froydnj
    UltraBlame original commit: fde6a66eb3170c0211ba97dcb3b43b4de45e23d8

--- mfbt/TextUtils.h
+++ mfbt/TextUtils.h
@@ -44,11 +44,80 @@ public:
 
 template<typename Char>
 constexpr bool
-IsAsciiAlpha(Char aChar)
+IsAsciiLowercaseAlpha(Char aChar)
 {
   using UnsignedChar = typename detail::MakeUnsignedChar<Char>::Type;
   auto uc = static_cast<UnsignedChar>(aChar);
-  return ('a' <= uc && uc <= 'z') || ('A' <= uc && uc <= 'Z');
+  return 'a' <= uc && uc <= 'z';
+}
+
+
+
+
+
+
+
+template<typename Char>
+constexpr bool
+IsAsciiUppercaseAlpha(Char aChar)
+{
+  using UnsignedChar = typename detail::MakeUnsignedChar<Char>::Type;
+  auto uc = static_cast<UnsignedChar>(aChar);
+  return 'A' <= uc && uc <= 'Z';
+}
+
+
+
+
+
+
+
+template<typename Char>
+constexpr bool
+IsAsciiAlpha(Char aChar)
+{
+  return IsAsciiLowercaseAlpha(aChar) || IsAsciiUppercaseAlpha(aChar);
+}
+
+
+
+
+
+
+
+template<typename Char>
+constexpr bool
+IsAsciiAlphanumeric(Char aChar)
+{
+  using UnsignedChar = typename detail::MakeUnsignedChar<Char>::Type;
+  auto uc = static_cast<UnsignedChar>(aChar);
+  return ('0' <= uc && uc <= '9') || IsAsciiAlpha(uc);
+}
+
+
+
+
+
+template<typename Char>
+constexpr uint8_t
+AsciiAlphanumericToNumber(Char aChar)
+{
+  using UnsignedChar = typename detail::MakeUnsignedChar<Char>::Type;
+  auto uc = static_cast<UnsignedChar>(aChar);
+
+  if ('0' <= uc && uc <= '9') {
+    return aChar - '0';
+  }
+
+  if ('A' <= uc && uc <= 'Z') {
+    return aChar - 'A' + 10;
+  }
+
+  
+  
+  
+  
+  return (aChar - 'a' + 10) / ('a' <= uc && uc <= 'z');
 }
 
 } 
