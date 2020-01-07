5048f7bde9540dccdd4d370ae222c6da5c2b2f6a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 00:26:52 +0000

    Backed out 3 changesets (bug 1331696) for failing browser chrome on  toolkit/components/narrate/test/browser_narrate.js r=backout on a CLOSED TREE
    Backed out changeset 4f5d0c5f191b (bug 1331696)
    Backed out changeset ce87e2d2f1db (bug 1331696)
    Backed out changeset e2c5f5afb6fc (bug 1331696)
    
    UltraBlame original commit: efde0448d7bb7e5a7bef4ac6a9c734f87f2981a3

--- dom/media/webspeech/synth/speechd/SpeechDispatcherService.cpp
+++ dom/media/webspeech/synth/speechd/SpeechDispatcherService.cpp
@@ -513,7 +513,8 @@ SpeechDispatcherService::Speak(const nsAString& aText, const nsAString& aUri,
   
   spd_set_voice_pitch(mSpeechdClient, static_cast<int>((aPitch - 1) * 100));
 
-  nsresult rv = aTask->Setup(callback);
+  
+  nsresult rv = aTask->Setup(callback, 0, 0, 0);
 
   if (NS_FAILED(rv)) {
     return rv;
@@ -545,6 +546,13 @@ SpeechDispatcherService::Speak(const nsAString& aText, const nsAString& aUri,
       SPD_EVENT_END));
   }
 
+  return NS_OK;
+}
+
+NS_IMETHODIMP
+SpeechDispatcherService::GetServiceType(SpeechServiceType* aServiceType)
+{
+  *aServiceType = nsISpeechService::SERVICETYPE_INDIRECT_AUDIO;
   return NS_OK;
 }
 
