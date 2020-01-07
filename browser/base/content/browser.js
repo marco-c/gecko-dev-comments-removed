77bf57168508e32e770fabd7d719bf98055003d6
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 05:43:43 +0000

    Bug 1403648 - set urlbar focus and about:home favicon in the DOMContentLoaded handler to reduce window open flicker, r=johannh.
    UltraBlame original commit: c03c60285f5db6a16c05c61165e78783fbe9f83b

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -1253,6 +1253,18 @@ var gBrowserInit = {
     if (AppConstants.CAN_DRAW_IN_TITLEBAR) {
       gDragSpaceObserver.init();
     }
+
+    
+    
+    this._callWithURIToLoad(uriToLoad => {
+      if (uriToLoad == "about:home") {
+        gBrowser.setIcon(gBrowser.selectedTab, "chrome://branding/content/icon32.png");
+      } else if (uriToLoad == "about:privatebrowsing") {
+        gBrowser.setIcon(gBrowser.selectedTab, "chrome://browser/skin/privatebrowsing/favicon.svg");
+      }
+    });
+
+    this._setInitialFocus();
   },
 
   onLoad() {
@@ -1351,18 +1363,6 @@ var gBrowserInit = {
         Cu.reportError(e);
       }
     }
-
-    this._setInitialFocus();
-
-    
-    
-    this._callWithURIToLoad(uriToLoad => {
-      if (uriToLoad == "about:home") {
-        gBrowser.setIcon(gBrowser.selectedTab, "chrome://branding/content/icon32.png");
-      } else if (uriToLoad == "about:privatebrowsing") {
-        gBrowser.setIcon(gBrowser.selectedTab, "chrome://browser/skin/privatebrowsing/favicon.svg");
-      }
-    });
 
     
     this._boundDelayedStartup = this._delayedStartup.bind(this);
