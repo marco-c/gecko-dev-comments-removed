da5f67e1d94095f78d1560616cb1d120ef8ba0e8
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 06:25:35 +0000

    Bug 1440407 P1 Add nsPIDOMWindowInner::AsGlobal() to cheaply cast to nsIGlobalObject*. r=mystor
    UltraBlame original commit: 65cd02963e834f7608bd8a0eaea6d1c19127cac9

--- dom/base/nsGlobalWindowInner.cpp
+++ dom/base/nsGlobalWindowInner.cpp
@@ -7937,6 +7937,18 @@ nsPIDOMWindowInner::GetDocGroup() const
   return nullptr;
 }
 
+nsIGlobalObject*
+nsPIDOMWindowInner::AsGlobal()
+{
+  return nsGlobalWindowInner::Cast(this);
+}
+
+const nsIGlobalObject*
+nsPIDOMWindowInner::AsGlobal() const
+{
+  return nsGlobalWindowInner::Cast(this);
+}
+
 
 namespace mozilla {
 namespace dom {
