a5a7635b0a1022eca41f1c96222e8db3f5f9a01e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 06:45:33 +0000

    Bug 1432682 - Part 0: Factor out some test code into a new lib file, jit-test/lib/stepping.js. r=jimb.
    This also fixes a bug in that code: it skipped the first stopping point in the
    function (before the first instruction).
    
    UltraBlame original commit: 77878d66cb1f61c508131b0e5b29aaa3883a3a02

--- js/src/jit-test/tests/debug/Frame-onStep-19.js
+++ js/src/jit-test/tests/debug/Frame-onStep-19.js
@@ -6,40 +6,17 @@
 
 
 
-function runTest(script, expected) {
-    let g = newGlobal();
-    g.eval(script);
+load(libdir + "stepping.js");
 
-    let dbg = new Debugger(g);
-    let log = [];
-    dbg.onEnterFrame = frame => {
-        let previousLine = undefined;
-        frame.onStep = function() {
-            let line = this.script.getOffsetLocation(this.offset).lineNumber;
-            if (line != previousLine) {
-                log.push(line);
-                previousLine = line;
-            }
-        };
-
-        
-        dbg.onEnterFrame = undefined;
-    };
-
-    g.f();
-
-    assertEq(log.join(","), expected.join(","));
-}
-
-runTest(
+testStepping(
     `\
-      var f = (function() {      // line 1
+      (function() {              // line 1
         let x = 1;               // line 2
         funcb("funcb");          // line 3
         function funcb(msg) {    // line 4
           console.log(msg)
         }
-      });                        // line 7
+      })                         // line 7
     `,
     [2, 3, 7]);
 
@@ -47,7 +24,7 @@ runTest(
 
 
 
-runTest(
+testStepping(
     `\
       function f() {    //  1
         var x = 0;      //  2
@@ -59,5 +36,6 @@ runTest(
         class Car {}    //  8
         return x;       //  9
       }                 // 10
+      f
     `,
     [2, 3, 8, 9, 10]);
