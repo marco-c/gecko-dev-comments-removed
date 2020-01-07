b6117204d7bfabf915f56d4fae7296c862efca84
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 07:18:39 +0000

    Bug 1442506: Adjust assertion to allow empty appends. r=xidorn
    Deletion at the end of a text-node ends up translated to an empty append. It's
    harmless though.
    
    Reviewers: xidorn
    
    Bug #: 1442506
    
    Differential Revision: https://phabricator.services.mozilla.com/D667
    
    MozReview-Commit-ID: DqheOYVWx8o
    
    UltraBlame original commit: 06a5b8c510f048c2d7c82136116bf676a70f07c9

--- layout/base/RestyleManager.cpp
+++ layout/base/RestyleManager.cpp
@@ -207,7 +207,7 @@ WhitespaceOnlyChangedOnAppend(const CharT* aBuffer,
                               size_t aOldLength,
                               size_t aNewLength)
 {
-  MOZ_ASSERT(aOldLength < aNewLength);
+  MOZ_ASSERT(aOldLength <= aNewLength);
   if (!WhitespaceOnly(aBuffer, aOldLength)) {
     
     return false;
