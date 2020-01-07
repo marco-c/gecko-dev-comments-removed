863441f86eaedc1ea38618618fd4d822d0eed18f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 06:00:02 +0000

    Bug 1438511 - Copy top window's uidensity attribute in sidebarUtils.js instead of forcing gUIDensity.update(). r=florian
    MozReview-Commit-ID: Ljwn5iyV37V
    
    UltraBlame original commit: 3ae4195fba9785117f76bdbe2cf3775cbb806a46

--- browser/components/places/content/sidebarUtils.js
+++ browser/components/places/content/sidebarUtils.js
@@ -3,7 +3,11 @@
 
 
 ChromeUtils.import("resource://gre/modules/AppConstants.jsm");
-window.top.gUIDensity.update();
+
+let uidensity = window.top.document.documentElement.getAttribute("uidensity");
+if (uidensity) {
+  document.documentElement.setAttribute("uidensity", uidensity);
+}
 
 var SidebarUtils = {
   handleTreeClick: function SU_handleTreeClick(aTree, aEvent, aGutterSelect) {
