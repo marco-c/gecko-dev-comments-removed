0feb93ff09ab7f60a589fef8d0276409f4256d67
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 00:01:31 +0000

    Bug 1418433 - move the implementation of SetStylistStyleSheetsDirty to .cpp file. r=heycam
    This is a pre-patch for the real fix of Bug 1418433.
    
    In the real fix, we'll add a IncrementUndisplayedRestyleGeneration() call into
    SetStylistStyleSheetsDirty(). However, the IncrementUndisplayedRestyleGeneration()
    call needs get through some deep structures in nsPresContext, RestyleManager,...
    etc., and doing so means we need to move bunches of related include files, forward
    declarations, from .cpp file to .h file, which doesn't make sense.
    
    Instead, we move the implementation parts of SetStylistStyleSheetsDirty() to .cpp
    file (since it is now a bit more complicated than it was), so we can use the existing
    include files in the .cpp file to add IncrementUndisplayedRestyleGeneration() call
    (this is in a following patch).
    
    MozReview-Commit-ID: 3Vp9qyCf8NA
    
    UltraBlame original commit: 1790eb0797c8c7e9a395f55067a7ab9c19cda14b

--- layout/style/ServoStyleSet.h
+++ layout/style/ServoStyleSet.h
@@ -554,15 +554,9 @@ private:
   
 
 
-  void SetStylistStyleSheetsDirty()
-  {
-    mStylistState |= StylistState::StyleSheetsDirty;
-  }
+  void SetStylistStyleSheetsDirty();
 
-  void SetStylistXBLStyleSheetsDirty()
-  {
-    mStylistState |= StylistState::XBLStyleSheetsDirty;
-  }
+  void SetStylistXBLStyleSheetsDirty();
 
   bool StylistNeedsUpdate() const
   {
