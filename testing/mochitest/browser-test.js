547992ae48a42f6932f9643c09212361dfdcbeef
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 08:54:06 +0000

    Bug 1442465 - Part 2: Use BrowserTestUtils.{waitForTabClosing,waitForSessionStoreUpdate} instead of BrowserTestUtils.tabRemoved. r=dao
    UltraBlame original commit: aa1497ed75e50222faaed7e65b8c41818217681e

--- testing/mochitest/browser-test.js
+++ testing/mochitest/browser-test.js
@@ -536,11 +536,15 @@ Tester.prototype = {
     if (this.currentTest && window.gBrowser && gBrowser.tabs.length > 1) {
       while (gBrowser.tabs.length > 1) {
         let lastTab = gBrowser.tabContainer.lastChild;
-        this.currentTest.addResult(new testResult({
-          name: baseMsg.replace("{elt}", "tab") + ": " +
-                lastTab.linkedBrowser.currentURI.spec,
-          allowFailure: this.currentTest.allowFailure,
-        }));
+        if (!lastTab.closing) {
+          
+          
+          this.currentTest.addResult(new testResult({
+            name: baseMsg.replace("{elt}", "tab") + ": " +
+              lastTab.linkedBrowser.currentURI.spec,
+            allowFailure: this.currentTest.allowFailure,
+          }));
+        }
         gBrowser.removeTab(lastTab);
       }
     }
