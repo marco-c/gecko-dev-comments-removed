d84f74ff6d271f7b5060c2452da95f8ee41aacae
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 05:15:54 +0000

    Bug 1436508 part 11.  Remove the use of the nsIDOMKeyEvent::DOM_VK* constants in C++.  r=masayuki
    MozReview-Commit-ID: Honw0NrVMuV
    
    UltraBlame original commit: 5ae4bf25badf967f4b5bec4c5d093b3837bb1570

--- dom/events/VirtualKeyCodeList.h
+++ dom/events/VirtualKeyCodeList.h
@@ -20,11 +20,11 @@
 
 
 #define DEFINE_VK_INTERNAL(aKeyName) \
-  NS_DEFINE_VK(VK##aKeyName, nsIDOMKeyEvent::DOM_VK##aKeyName)
+  NS_DEFINE_VK(VK##aKeyName, mozilla::dom::KeyboardEventBinding::DOM_VK##aKeyName)
 
 
 #define DEFINE_VK_INTERNAL2(aKeyName, aKeyCodeName) \
-  NS_DEFINE_VK(VK##aKeyName, nsIDOMKeyEvent::DOM_VK##aKeyCodeName)
+  NS_DEFINE_VK(VK##aKeyName, mozilla::dom::KeyboardEventBinding::DOM_VK##aKeyCodeName)
 
 DEFINE_VK_INTERNAL(_CANCEL)
 DEFINE_VK_INTERNAL(_HELP)
