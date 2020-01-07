e6f6588aa66629f55422ece302224f2bd98e0b8c
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 19:06:57 +0000

    Bug 1450960 - split AddonConsole into its own file; r=ochameau
    MozReview-Commit-ID: GaC5tvSwb9
    
    UltraBlame original commit: 979280d2d6110d0f1f48e323364587ece03c59d3

--- devtools/server/actors/addon.js
+++ devtools/server/actors/addon.js
@@ -9,13 +9,12 @@ var Services = require("Services");
 var { ActorPool } = require("devtools/server/actors/common");
 var { TabSources } = require("./utils/TabSources");
 var makeDebugger = require("./utils/make-debugger");
-var { ConsoleAPIListener } = require("devtools/server/actors/webconsole/listeners");
 var DevToolsUtils = require("devtools/shared/DevToolsUtils");
-var { assert, update } = DevToolsUtils;
+var { assert } = DevToolsUtils;
 
 loader.lazyRequireGetter(this, "AddonThreadActor", "devtools/server/actors/thread", true);
 loader.lazyRequireGetter(this, "unwrapDebuggerObjectGlobal", "devtools/server/actors/thread", true);
-loader.lazyRequireGetter(this, "WebConsoleActor", "devtools/server/actors/webconsole", true);
+loader.lazyRequireGetter(this, "AddonConsoleActor", "devtools/server/actors/addon-console", true);
 
 loader.lazyImporter(this, "AddonManager", "resource://gre/modules/AddonManager.jsm");
 
@@ -252,84 +251,3 @@ BrowserAddonActor.prototype.requestTypes = {
   "reload": BrowserAddonActor.prototype.onReload
 };
 
-
-
-
-
-
-
-
-
-
-
-
-
-function AddonConsoleActor(addon, connection, parentActor) {
-  this.addon = addon;
-  WebConsoleActor.call(this, connection, parentActor);
-}
-
-AddonConsoleActor.prototype = Object.create(WebConsoleActor.prototype);
-
-update(AddonConsoleActor.prototype, {
-  constructor: AddonConsoleActor,
-
-  actorPrefix: "addonConsole",
-
-  
-
-
-  addon: null,
-
-  
-
-
-  get window() {
-    return this.parentActor.global;
-  },
-
-  
-
-
-  destroy() {
-    WebConsoleActor.prototype.destroy.call(this);
-    this.addon = null;
-  },
-
-  
-
-
-
-
-
-
-
-  onStartListeners: function ACAOnStartListeners(request) {
-    let startedListeners = [];
-
-    while (request.listeners.length > 0) {
-      let listener = request.listeners.shift();
-      switch (listener) {
-        case "ConsoleAPI":
-          if (!this.consoleAPIListener) {
-            this.consoleAPIListener =
-              new ConsoleAPIListener(null, this, { addonId: this.addon.id });
-            this.consoleAPIListener.init();
-          }
-          startedListeners.push(listener);
-          break;
-      }
-    }
-    return {
-      startedListeners: startedListeners,
-      nativeConsoleAPI: true,
-      traits: this.traits,
-    };
-  },
-});
-
-AddonConsoleActor.prototype.requestTypes = Object.create(
-  WebConsoleActor.prototype.requestTypes
-);
-AddonConsoleActor.prototype.requestTypes.startListeners =
-  AddonConsoleActor.prototype.onStartListeners;
