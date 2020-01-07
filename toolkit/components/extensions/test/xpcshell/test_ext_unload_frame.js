44b553e08a41aa8cb328f6b62e6981207ef52635
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 08:18:42 +0000

    Bug 1444758: Part 14 - Migrate test_ext_unload_frame to xpcshell. r=aswan
    MozReview-Commit-ID: 4lwqDQ8rI1K
    
    UltraBlame original commit: 546f73d3fc888160057bf544b072de3888bdb707

--- toolkit/components/extensions/test/xpcshell/test_ext_unload_frame.js
+++ toolkit/components/extensions/test/xpcshell/test_ext_unload_frame.js
@@ -1,42 +1,36 @@
-<!DOCTYPE HTML>
-<html>
-<head>
-  <title>WebExtensions test</title>
-  <script type="text/javascript" src="/tests/SimpleTest/SimpleTest.js"></script>
-  <script type="text/javascript" src="/tests/SimpleTest/SpawnTask.js"></script>
-  <script type="text/javascript" src="/tests/SimpleTest/ExtensionTestUtils.js"></script>
-  <script type="text/javascript" src="head.js"></script>
-  <link rel="stylesheet" type="text/css" href="/tests/SimpleTest/test.css">
-</head>
-<body>
-
-<script>
 "use strict";
 
-/* globals delayedNotifyPass */ // Available in the background page of the test extensions.
+const server = createHttpServer({hosts: ["example.com"]});
+server.registerDirectory("/data/", do_get_file("data"));
 
-// Background and content script for testSendMessage_*
-function sendMessage_background() {
+server.registerPathHandler("/dummy", (request, response) => {
+  response.setStatusLine(request.httpVersion, 200, "OK");
+  response.setHeader("Content-Type", "text/html", false);
+  response.write("<!DOCTYPE html><html></html>");
+});
+
+
+function sendMessage_background(delayedNotifyPass) {
   browser.runtime.onMessage.addListener((msg, sender, sendResponse) => {
     browser.test.assertEq("from frame", msg, "Expected message from frame");
-    sendResponse("msg from back"); // Should not throw or anything like that.
+    sendResponse("msg from back"); 
     delayedNotifyPass("Received sendMessage from closing frame");
   });
 }
 function sendMessage_contentScript(testType) {
   browser.runtime.sendMessage("from frame", reply => {
-    // The frame has been removed, so we should not get this callback!
+    
     browser.test.fail(`Unexpected reply: ${reply}`);
   });
   if (testType == "frame") {
     frameElement.remove();
   } else {
-    window.close();
+    browser.test.sendMessage("close-window");
   }
 }
 
-// Background and content script for testConnect_*
-function connect_background() {
+
+function connect_background(delayedNotifyPass) {
   browser.runtime.onConnect.addListener(port => {
     browser.test.assertEq("port from frame", port.name);
 
@@ -65,33 +59,33 @@ function connect_contentScript(testType) {
 
   let port = browser.runtime.connect({name: "port from frame"});
   port.onMessage.addListener(msg => {
-    // The background page sends a reply as soon as we call runtime.connect().
-    // It is possible that the reply reaches this frame before the
-    // window.close() request has been processed.
+    
+    
+    
     if (!isUnloading) {
       browser.test.log(`Ignorting unexpected reply ("${msg}") because the page is not being unloaded.`);
       return;
     }
 
-    // The frame has been removed, so we should not get a reply.
+    
     browser.test.fail(`Unexpected reply: ${msg}`);
   });
   port.postMessage("from frame");
 
-  // Removing the frame or window should disconnect the port.
+  
   if (testType == "frame") {
     frameElement.remove();
   } else {
-    window.close();
+    browser.test.sendMessage("close-window");
   }
 }
 
-// `testType` is "window" or "frame".
+
 function createTestExtension(testType, backgroundScript, contentScript) {
-  // Make a roundtrip between the background page and the test runner (which is
-  // in the same process as the content script) to make sure that we record a
-  // failure in case the content script's sendMessage or onMessage handlers are
-  // called even after the frame or window was removed.
+  
+  
+  
+  
   function delayedNotifyPass(msg) {
     browser.test.onMessage.addListener((type, echoMsg) => {
       if (type == "pong") {
@@ -103,12 +97,12 @@ function createTestExtension(testType, backgroundScript, contentScript) {
     browser.test.sendMessage("ping", msg);
   }
   let extension = ExtensionTestUtils.loadExtension({
-    background: `${delayedNotifyPass};(${backgroundScript})();`,
+    background: `(${backgroundScript})(${delayedNotifyPass});`,
     manifest: {
       content_scripts: [{
         js: ["contentscript.js"],
         all_frames: testType == "frame",
-        matches: ["http://mochi.test/*/file_sample.html"],
+        matches: ["http://example.com/data/file_sample.html"],
       }],
     },
     files: {
@@ -125,11 +119,18 @@ add_task(async function testSendMessage_and_remove_frame() {
   let extension = createTestExtension("frame", sendMessage_background, sendMessage_contentScript);
   await extension.startup();
 
-  let frame = document.createElement("iframe");
-  frame.src = "file_sample.html";
-  document.body.appendChild(frame);
+  let contentPage = await ExtensionTestUtils.loadContentPage(
+    "http://example.com/dummy");
+
+  await contentPage.spawn(null, () => {
+    let {document} = this.content;
+    let frame = document.createElement("iframe");
+    frame.src = "/data/file_sample.html";
+    document.body.appendChild(frame);
+  });
 
   await extension.awaitFinish("Received sendMessage from closing frame");
+  await contentPage.close();
   await extension.unload();
 });
 
@@ -137,24 +138,34 @@ add_task(async function testConnect_and_remove_frame() {
   let extension = createTestExtension("frame", connect_background, connect_contentScript);
   await extension.startup();
 
-  let frame = document.createElement("iframe");
-  frame.src = "file_sample.html";
-  document.body.appendChild(frame);
+  let contentPage = await ExtensionTestUtils.loadContentPage(
+    "http://example.com/dummy");
+
+  await contentPage.spawn(null, () => {
+    let {document} = this.content;
+    let frame = document.createElement("iframe");
+    frame.src = "/data/file_sample.html";
+    document.body.appendChild(frame);
+  });
 
   await extension.awaitFinish("Received onDisconnect from closing frame");
+  await contentPage.close();
   await extension.unload();
 });
 
 add_task(async function testSendMessage_and_remove_window() {
   if (AppConstants.MOZ_BUILD_APP !== "browser") {
-    // We can't rely on this timing on Android.
+    
     return;
   }
 
   let extension = createTestExtension("window", sendMessage_background, sendMessage_contentScript);
   await extension.startup();
 
-  window.open("file_sample.html");
+  let contentPage = await ExtensionTestUtils.loadContentPage(
+    "http://example.com/data/file_sample.html");
+  await extension.awaitMessage("close-window");
+  await contentPage.close();
 
   await extension.awaitFinish("Received sendMessage from closing frame");
   await extension.unload();
@@ -162,19 +173,18 @@ add_task(async function testSendMessage_and_remove_window() {
 
 add_task(async function testConnect_and_remove_window() {
   if (AppConstants.MOZ_BUILD_APP !== "browser") {
-    // We can't rely on this timing on Android.
+    
     return;
   }
 
   let extension = createTestExtension("window", connect_background, connect_contentScript);
   await extension.startup();
 
-  window.open("file_sample.html");
+  let contentPage = await ExtensionTestUtils.loadContentPage(
+    "http://example.com/data/file_sample.html");
+  await extension.awaitMessage("close-window");
+  await contentPage.close();
 
   await extension.awaitFinish("Received onDisconnect from closing frame");
   await extension.unload();
 });
-
-</script>
-</body>
-</html>
