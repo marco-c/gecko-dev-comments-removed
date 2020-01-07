bc44d16213a150e61d90c66fb7838b91d4f2f9fa
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 07:11:54 +0000

    Bug 1429904 - Make FrameKey members const. r=njn
    MozReview-Commit-ID: KTYmCJM9tOH
    
    UltraBlame original commit: b465484b85ab8a071a51d7d4a7129d0437964ac3

--- tools/profiler/core/ProfileBufferEntry.h
+++ tools/profiler/core/ProfileBufferEntry.h
@@ -172,11 +172,11 @@ public:
   struct FrameKey {
     
     
-    nsCString mLocation;
-    mozilla::Maybe<unsigned> mLine;
-    mozilla::Maybe<unsigned> mCategory;
-    mozilla::Maybe<JITAddress> mJITAddress;
-    mozilla::Maybe<uint32_t> mJITDepth;
+    const nsCString mLocation;
+    const mozilla::Maybe<unsigned> mLine;
+    const mozilla::Maybe<unsigned> mCategory;
+    const mozilla::Maybe<JITAddress> mJITAddress;
+    const mozilla::Maybe<uint32_t> mJITDepth;
 
     explicit FrameKey(const char* aLocation)
      : mLocation(aLocation)
