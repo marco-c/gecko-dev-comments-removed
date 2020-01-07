cbdd189baf8c49a914672234cb8ae784da063d2e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 04:22:45 +0000

    Bug 633062 p3 - Remove event loop spinning from the EngineManager. r=markh
    MozReview-Commit-ID: 7tdKT8Wo3t4
    
    UltraBlame original commit: bb1030bdab48652fb0ce1965543106e1eb381b8a

--- services/sync/tests/unit/test_syncscheduler.js
+++ services/sync/tests/unit/test_syncscheduler.js
@@ -76,7 +76,7 @@ add_task(async function setup() {
   
   
   clientsEngine._removeRemoteClient = async (id) => {};
-  Service.engineManager.clear();
+  await Service.engineManager.clear();
 
   validate_all_future_pings();
 
