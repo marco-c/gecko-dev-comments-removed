537e1722dd1a2b21f8231f0e75c9cc40e1e2a50c
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 18:43:45 +0000

    Bug 1460315 - Run Mn suite on both android-4.3-arm7 debug and opt; r=gbrown
    MozReview-Commit-ID: Bq4gJJahwYQ
    
    UltraBlame original commit: c58979622f4e662bf6101e1650adf69b8831e3c8

--- testing/marionette/client/marionette_driver/geckoinstance.py
+++ testing/marionette/client/marionette_driver/geckoinstance.py
@@ -96,8 +96,8 @@ class GeckoInstance(object):
         "javascript.options.showInConsole": True,
 
         
-        
         "marionette.enabled": True,
+        
         "marionette.defaultPrefs.enabled": True,
 
         
