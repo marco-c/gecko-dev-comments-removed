a20c967598a26780e79baa7f106c7dd6e7b1fff6
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 17:39:06 +0000

    Bug 1456516 [wpt PR 10606] - Reindent websocket *.any.js files, a=testonly
    Automatic update from web-platform-testsEditorial: Reindent websocket *.any.js files
    
    Command used:
    find . -name "*.any.js" | xargs -I {} js-beautify -r -s 2 -n {}
    
    --
    
    wpt-commits: 78d32dd2ed106a59a34664ade2274426601365de
    wpt-pr: 10606
    
    UltraBlame original commit: ffbddf5559c43743e4d0ad578db8199091169826

--- testing/web-platform/tests/websockets/Create-non-absolute-url.any.js
+++ testing/web-platform/tests/websockets/Create-non-absolute-url.any.js
@@ -1,6 +1,8 @@
 
 
-        test(function () {
-            var wsocket;
-            assert_throws("SYNTAX_ERR", function () { wsocket = CreateWebSocketNonAbsolute() });
-        }, "W3C WebSocket API - Create WebSocket - Pass a non absolute URL - SYNTAX_ERR is thrown")
+test(function() {
+  var wsocket;
+  assert_throws("SYNTAX_ERR", function() {
+    wsocket = CreateWebSocketNonAbsolute()
+  });
+}, "W3C WebSocket API - Create WebSocket - Pass a non absolute URL - SYNTAX_ERR is thrown")
