f25fa2ade012833d846cebb196c2f9b0066126f5
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 18:20:36 +0000

    Bug 739219 - Remove nsIBrowserHistory. r=standard8
    MozReview-Commit-ID: G1n44eSFWr1
    
    UltraBlame original commit: 6c4bad08fa9a8946b103f814c8cd08d22a7e8364

--- toolkit/components/places/nsNavHistory.h
+++ toolkit/components/places/nsNavHistory.h
@@ -8,9 +8,9 @@
 
 #include "nsINavHistoryService.h"
 #include "nsPIPlacesDatabase.h"
-#include "nsIBrowserHistory.h"
 #include "nsINavBookmarksService.h"
 #include "nsIFaviconService.h"
+#include "nsIGlobalHistory2.h"
 
 #include "nsIObserverService.h"
 #include "nsICollation.h"
@@ -78,7 +78,6 @@ class nsIAutoCompleteController;
 class nsNavHistory final : public nsSupportsWeakReference
                          , public nsINavHistoryService
                          , public nsIObserver
-                         , public nsIBrowserHistory
                          , public nsPIPlacesDatabase
                          , public mozIStorageVacuumParticipant
 {
@@ -89,7 +88,6 @@ public:
 
   NS_DECL_THREADSAFE_ISUPPORTS
   NS_DECL_NSINAVHISTORYSERVICE
-  NS_DECL_NSIBROWSERHISTORY
   NS_DECL_NSIOBSERVER
   NS_DECL_NSPIPLACESDATABASE
   NS_DECL_MOZISTORAGEVACUUMPARTICIPANT
@@ -352,11 +350,6 @@ public:
 
   bool hasEmbedVisit(nsIURI* aURI);
 
-  
-
-
-  void clearEmbedVisits();
-
   int32_t GetFrecencyAgedWeight(int32_t aAgeInDays) const
   {
     if (aAgeInDays <= mFirstBucketCutoffInDays) {
@@ -512,9 +505,6 @@ protected:
 
   nsresult DecayFrecency();
 
-  nsresult RemovePagesInternal(const nsCString& aPlaceIdsQueryString);
-  nsresult CleanupPlacesOnVisitsDelete(const nsCString& aPlaceIdsQueryString);
-
   
 
 
@@ -552,8 +542,6 @@ protected:
   nsresult ResultsAsList(mozIStorageStatement* statement,
                          nsNavHistoryQueryOptions* aOptions,
                          nsCOMArray<nsNavHistoryResultNode>* aResults);
-
-  void TitleForDomain(const nsCString& domain, nsACString& aTitle);
 
   
   nsMaybeWeakPtrArray<nsINavHistoryObserver> mObservers;
