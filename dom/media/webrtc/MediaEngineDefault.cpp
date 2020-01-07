0898c4478463007f6131f5e99cac723f08d96fdd
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 00:24:02 +0000

    Bug 1397793 - Move MediaEngineDefault to use the MSG rate instead of something hard-coded. r=pehrsons
    This is "just" for testing, but is cleaner, and skips some resampling, and is in
    line with the other patches, to converge with always using MSG rate when we can.
    
    MozReview-Commit-ID: CBQHEDQWJE3
    
    UltraBlame original commit: bf8977e0f440c0280da32a7052214834dc6701ca

--- dom/media/webrtc/MediaEngineDefault.cpp
+++ dom/media/webrtc/MediaEngineDefault.cpp
@@ -26,7 +26,6 @@
 #include "YuvStamper.h"
 #endif
 
-#define AUDIO_RATE mozilla::MediaEngine::DEFAULT_SAMPLE_RATE
 #define DEFAULT_AUDIO_TIMER_MS 10
 namespace mozilla {
 
@@ -332,6 +331,7 @@ NS_IMPL_ISUPPORTS0(MediaEngineDefaultAudioSource)
 MediaEngineDefaultAudioSource::MediaEngineDefaultAudioSource()
   : MediaEngineAudioSource(kReleased)
   , mLastNotify(0)
+  , mFreq(1000)
 {}
 
 MediaEngineDefaultAudioSource::~MediaEngineDefaultAudioSource()
@@ -382,10 +382,8 @@ MediaEngineDefaultAudioSource::Allocate(const dom::MediaTrackConstraints &aConst
     return NS_ERROR_FAILURE;
   }
 
+  mFreq = aPrefs.mFreq ? aPrefs.mFreq : 1000;
   mState = kAllocated;
-  
-  mSineGenerator = new SineWaveGenerator(AUDIO_RATE,
-                                         static_cast<uint32_t>(aPrefs.mFreq ? aPrefs.mFreq : 1000));
   *aOutHandle = nullptr;
   return NS_OK;
 }
@@ -409,9 +407,15 @@ MediaEngineDefaultAudioSource::Start(SourceMediaStream* aStream, TrackID aID,
     return NS_ERROR_FAILURE;
   }
 
+
+  if (!mSineGenerator) {
+    
+    mSineGenerator = new SineWaveGenerator(aStream->GraphRate(), mFreq);
+  }
+
   
   AudioSegment* segment = new AudioSegment();
-  aStream->AddAudioTrack(aID, AUDIO_RATE, 0, segment, SourceMediaStream::ADDTRACK_QUEUED);
+  aStream->AddAudioTrack(aID, mRate, 0, segment, SourceMediaStream::ADDTRACK_QUEUED);
 
   
   mTrackID = aID;
@@ -467,7 +471,7 @@ MediaEngineDefaultAudioSource::NotifyPull(MediaStreamGraph* aGraph,
   MOZ_ASSERT(aID == mTrackID);
   AudioSegment segment;
   
-  TrackTicks desired = aSource->TimeToTicksRoundUp(AUDIO_RATE, aDesiredTime);
+  TrackTicks desired = aSource->TimeToTicksRoundUp(mRate, aDesiredTime);
   TrackTicks delta = desired - mLastNotify;
   mLastNotify += delta;
   AppendToSegment(segment, delta, aPrincipalHandle);
