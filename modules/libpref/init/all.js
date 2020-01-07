50c03cf610080cd7202e84667593ae46539ea7b2
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 08:00:41 +0000

    Bug 1444074 - Enable full-duplex by default for all platforms. r=padenot
    MozReview-Commit-ID: LUvReTPd1TA
    
    UltraBlame original commit: d5df49dd7e753496674c50e23704d387c4d39a16

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -557,25 +557,18 @@ pref("media.getusermedia.agc", 3);
 
 
 
+pref("media.navigator.audio.full_duplex", true);
 #if defined(XP_MACOSX)
 pref("media.peerconnection.capture_delay", 50);
-pref("media.navigator.audio.full_duplex", true);
 #elif defined(XP_WIN)
 pref("media.peerconnection.capture_delay", 50);
-pref("media.navigator.audio.full_duplex", true);
 #elif defined(ANDROID)
 pref("media.peerconnection.capture_delay", 100);
-pref("media.navigator.audio.full_duplex", true);
 pref("media.navigator.hardware.vp8_encode.acceleration_enabled", true);
 pref("media.navigator.hardware.vp8_encode.acceleration_remote_enabled", true);
 pref("media.navigator.hardware.vp8_decode.acceleration_enabled", false);
-#elif defined(XP_LINUX) || defined(MOZ_SNDIO)
-pref("media.peerconnection.capture_delay", 70);
-pref("media.navigator.audio.full_duplex", true);
 #else
-
-pref("media.peerconnection.capture_delay", 50);
-pref("media.navigator.audio.full_duplex", false);
+pref("media.peerconnection.capture_delay", 70);
 #endif
 
 
