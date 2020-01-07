cd38baa3d99800c67c8a3eee43eb76f67e5f1bc9
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 07:30:34 +0000

    Bug 1439777 p2 - Remove weave:ui:* related code. r=markh,tcsc
    MozReview-Commit-ID: D1H36YeiJCS
    
    UltraBlame original commit: 6e83f07e8f2f14c9eb2b0d3838acb4ad105338de

--- services/sync/tests/unit/test_fxa_node_reassignment.js
+++ services/sync/tests/unit/test_fxa_node_reassignment.js
@@ -25,13 +25,6 @@ add_task(async function setup() {
 
   
   Status.__authManager = Service.identity = new BrowserIDManager();
-
-  
-  function onUIError() {
-    do_throw("Errors should not be presented in the UI.");
-  }
-  Svc.Obs.add("weave:ui:login:error", onUIError);
-  Svc.Obs.add("weave:ui:sync:error", onUIError);
 });
 
 
