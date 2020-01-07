6abae7ce1d80fd9b5516f9f3977c2e644b7bfa9a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 18:36:41 +0000

    Bug 1460244 - For tables with CSS display properties, correctly expose table interfaces to a11y clients. r=surkov
    MozReview-Commit-ID: EFyn3MCth88
    
    UltraBlame original commit: 02854660533cbced52a6b597016a18a976ebc490

--- accessible/base/MarkupMap.h
+++ accessible/base/MarkupMap.h
@@ -328,7 +328,7 @@ MARKUPMAP(
   table,
   [](Element* aElement, Accessible* aContext) -> Accessible* {
      if (aElement->GetPrimaryFrame()->AccessibleType() != eHTMLTableType) {
-       return new ARIAGridAccessible(aElement, aContext->Document());
+       return new ARIAGridAccessibleWrap(aElement, aContext->Document());
      }
      return nullptr;
   },
@@ -350,7 +350,7 @@ MARKUPMAP(
        
        
        if (!aContext->IsHTMLTableRow()) {
-         return new ARIAGridCellAccessible(aElement, aContext->Document());
+         return new ARIAGridCellAccessibleWrap(aElement, aContext->Document());
        }
        if (aElement->HasAttr(kNameSpaceID_None, nsGkAtoms::scope)) {
          return new HTMLTableHeaderCellAccessibleWrap(aElement, aContext->Document());
@@ -366,7 +366,7 @@ MARKUPMAP(
   [](Element* aElement, Accessible* aContext) -> Accessible* {
      if (aContext->IsTableRow() && aContext->GetContent() == aElement->GetParent()) {
        if (!aContext->IsHTMLTableRow()) {
-         return new ARIAGridCellAccessible(aElement, aContext->Document());
+         return new ARIAGridCellAccessibleWrap(aElement, aContext->Document());
        }
        return new HTMLTableHeaderCellAccessibleWrap(aElement, aContext->Document());
      }
