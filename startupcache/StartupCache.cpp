de76ed36c0dfa4718c8a9c01cd25eacad3ffd547
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 06:33:41 +0000

    Bug 1439047 - Part 2: Cleanup StartupCache::PutBuffer hashtable usage. r=froydnj
    This switches over to using `LookupForAdd` which allows us to avoid a second
    lookup when adding the entry. Addtionally `nsDependentCString` is used to
    avoid copying the id string when looking up the entry.
    
    UltraBlame original commit: 7dc8f7e2eee49af5f6d328f357f069b899f16ae8

--- startupcache/StartupCache.cpp
+++ startupcache/StartupCache.cpp
@@ -310,11 +310,11 @@ StartupCache::PutBuffer(const char* id, UniquePtr<char[]>&& inbuf, uint32_t len)
     return NS_ERROR_NOT_AVAILABLE;
   }
 
-  nsCString idStr(id);
+  nsDependentCString idStr(id);
   
-  CacheEntry* entry;
+  auto entry = mTable.LookupForAdd(idStr);
 
-  if (mTable.Get(idStr)) {
+  if (entry) {
     NS_WARNING("Existing entry in StartupCache.");
     
     return NS_OK;
@@ -327,8 +327,9 @@ StartupCache::PutBuffer(const char* id, UniquePtr<char[]>&& inbuf, uint32_t len)
   }
 #endif
 
-  entry = new CacheEntry(Move(inbuf), len);
-  mTable.Put(idStr, entry);
+  entry.OrInsert([&inbuf, &len]() {
+      return new CacheEntry(Move(inbuf), len);
+  });
   mPendingWrites.AppendElement(idStr);
   return ResetStartupWriteTimer();
 }
