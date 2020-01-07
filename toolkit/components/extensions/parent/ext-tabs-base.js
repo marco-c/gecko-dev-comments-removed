344cd7503d16b4ded4b4086cbc4832e8685ae253
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 10:50:37 +0000

    Bug 1450572 - Optimize windows.get(windowId) r=mixedpuppy
    MozReview-Commit-ID: EHfSbaNIX75
    
    UltraBlame original commit: 57d58de5d58daa70b93b53117e7300820637d7ef

--- toolkit/components/extensions/parent/ext-tabs-base.js
+++ toolkit/components/extensions/parent/ext-tabs-base.js
@@ -1414,10 +1414,12 @@ class WindowTrackerBase extends EventEmitter {
       return this.getCurrentWindow(context);
     }
 
-    for (let window of this.browserWindows(true)) {
-      if (this.getId(window) === id) {
-        return window;
-      }
+    let window = Services.wm.getOuterWindowWithId(id);
+    if (window && !window.closed && (window.document.readyState !== "complete"
+        || this.isBrowserWindow(window))) {
+      
+      
+      return window;
     }
 
     if (strict) {
