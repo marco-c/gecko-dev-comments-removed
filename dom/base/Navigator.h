6108deb6bfbc698197fd5b853f54620880e60f5f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 04:06:50 +0000

    Bug 1432271 - Remove dom/time; r=baku
    dom/time contained the TimeService and TimeManager classes, used for
    setting time via Gecko on FirefoxOS. Since FirefoxOS is no longer in
    the code base, the directory can be removed.
    
    MozReview-Commit-ID: 8PEk3e6HA67
    
    UltraBlame original commit: a117f844cf6584759e36f235c37aaeb5fd65b1bb

--- dom/base/Navigator.h
+++ dom/base/Navigator.h
@@ -75,10 +75,6 @@ class VRDisplay;
 class VRServiceTest;
 class StorageManager;
 
-namespace time {
-class TimeManager;
-} 
-
 class Navigator final : public nsISupports
                       , public nsWrapperCache
 {
@@ -182,9 +178,6 @@ public:
   bool IsWebVRContentPresenting() const;
   void RequestVRPresentation(VRDisplay& aDisplay);
   nsINetworkProperties* GetNetworkProperties();
-#ifdef MOZ_TIME_MANAGER
-  time::TimeManager* GetMozTime(ErrorResult& aRv);
-#endif 
 
   Presentation* GetPresentation(ErrorResult& aRv);
 
@@ -271,7 +264,6 @@ private:
   RefPtr<network::Connection> mConnection;
   RefPtr<CredentialsContainer> mCredentials;
   RefPtr<MediaDevices> mMediaDevices;
-  RefPtr<time::TimeManager> mTimeManager;
   RefPtr<ServiceWorkerContainer> mServiceWorkerContainer;
   nsCOMPtr<nsPIDOMWindowInner> mWindow;
   RefPtr<Presentation> mPresentation;
