c875fd9acf32ef40c6f6171339265693d074c387
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 03:53:11 +0000

    Backed out 4 changesets (bug 1381645) for failing in dom/tests/mochitest/general/test_interfaces.html on a CLOSED TREE
    Backed out changeset 7b2482a5eb04 (bug 1381645)
    Backed out changeset ef3252b0ba21 (bug 1381645)
    Backed out changeset 7c022a5e28f2 (bug 1381645)
    Backed out changeset 2f71c8989cc0 (bug 1381645)
    
    UltraBlame original commit: 4bd6d0d97a7c63c3f3eae27f2baf4a914d0cb50d

--- dom/tests/mochitest/general/test_interfaces.js
+++ dom/tests/mochitest/general/test_interfaces.js
@@ -1181,21 +1181,21 @@ var interfaceNamesInGlobalScope =
 
     {name: "VideoStreamTrack", insecureContext: true},
 
-    {name: "VRDisplay", releaseNonWindows: false},
+    {name: "VRDisplay", insecureContext: true, releaseNonWindows: false},
 
-    {name: "VRDisplayCapabilities", releaseNonWindows: false},
+    {name: "VRDisplayCapabilities", insecureContext: true, releaseNonWindows: false},
 
-    {name: "VRDisplayEvent", releaseNonWindows: false},
+    {name: "VRDisplayEvent", insecureContext: true, releaseNonWindows: false},
 
-    {name: "VREyeParameters", releaseNonWindows: false},
+    {name: "VREyeParameters", insecureContext: true, releaseNonWindows: false},
 
-    {name: "VRFieldOfView", releaseNonWindows: false},
+    {name: "VRFieldOfView", insecureContext: true, releaseNonWindows: false},
 
-    {name: "VRFrameData", releaseNonWindows: false},
+    {name: "VRFrameData", insecureContext: true, releaseNonWindows: false},
 
-    {name: "VRPose", releaseNonWindows: false},
+    {name: "VRPose", insecureContext: true, releaseNonWindows: false},
 
-    {name: "VRStageParameters", releaseNonWindows: false},
+    {name: "VRStageParameters", insecureContext: true, releaseNonWindows: false},
 
     {name: "VTTCue", insecureContext: true},
 
