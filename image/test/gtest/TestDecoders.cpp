1a77d6bfa5cb09732cba43d7838fb4322be4edf7
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 20:17:51 +0000

    Bug 1462355 - Part 8. Avoid allocating on the heap in DrawableFrameRef. r=tnikkel
    We can easily use Maybe<DataSourceSurface::ScopedMap> instead of
    allocated the map on the heap. This does require some minor changes to
    ScopedMap to properly support moves, but should be much more efficient.
    
    UltraBlame original commit: 8d412f560489b690870257077e15f1b1ad3d7b75

--- image/test/gtest/TestDecoders.cpp
+++ image/test/gtest/TestDecoders.cpp
@@ -467,8 +467,8 @@ TEST_F(ImageDecoders, AnimatedGIFWithFRAME_FIRST)
     EXPECT_TRUE(NS_SUCCEEDED(result.Surface().Seek(0)));
     EXPECT_TRUE(bool(result.Surface()));
 
-    EXPECT_TRUE(NS_SUCCEEDED(result.Surface().Seek(1)));
-    EXPECT_TRUE(bool(result.Surface()));
+    RawAccessFrameRef partialFrame = result.Surface().RawAccessRef(1);
+    EXPECT_TRUE(bool(partialFrame));
   }
 
   
@@ -553,8 +553,8 @@ TEST_F(ImageDecoders, AnimatedGIFWithFRAME_CURRENT)
     EXPECT_TRUE(NS_SUCCEEDED(result.Surface().Seek(0)));
     EXPECT_TRUE(bool(result.Surface()));
 
-    EXPECT_TRUE(NS_SUCCEEDED(result.Surface().Seek(1)));
-    EXPECT_TRUE(bool(result.Surface()));
+    RawAccessFrameRef partialFrame = result.Surface().RawAccessRef(1);
+    EXPECT_TRUE(bool(partialFrame));
   }
 
   
@@ -596,8 +596,8 @@ TEST_F(ImageDecoders, AnimatedGIFWithFRAME_CURRENT)
     EXPECT_TRUE(NS_SUCCEEDED(result.Surface().Seek(0)));
     EXPECT_TRUE(bool(result.Surface()));
 
-    EXPECT_TRUE(NS_SUCCEEDED(result.Surface().Seek(1)));
-    EXPECT_TRUE(bool(result.Surface()));
+    RawAccessFrameRef partialFrame = result.Surface().RawAccessRef(1);
+    EXPECT_TRUE(bool(partialFrame));
   }
 }
 
@@ -665,8 +665,8 @@ TEST_F(ImageDecoders, AnimatedGIFWithExtraImageSubBlocks)
   EXPECT_TRUE(NS_SUCCEEDED(result.Surface().Seek(0)));
   EXPECT_TRUE(bool(result.Surface()));
 
-  EXPECT_TRUE(NS_SUCCEEDED(result.Surface().Seek(1)));
-  EXPECT_TRUE(bool(result.Surface()));
+  RawAccessFrameRef partialFrame = result.Surface().RawAccessRef(1);
+  EXPECT_TRUE(bool(partialFrame));
 }
 
 TEST_F(ImageDecoders, TruncatedSmallGIFSingleChunk)
