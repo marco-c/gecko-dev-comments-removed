3fa36c139dec97d85f4944226c2bb97f7765e90f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 19:21:18 +0000

    Bug 1447338 - Add thread "responsiveness" as a configurable feature to the Gecko Profiler; r=mstange
    MozReview-Commit-ID: KTJRvQzUwsf
    
    UltraBlame original commit: 1a16d4d1f6206ad99137299ab4e35d3e350d8227

--- devtools/client/performance-new/store/reducers.js
+++ devtools/client/performance-new/store/reducers.js
@@ -88,7 +88,7 @@ function entries(state = 10000000, action) {
 
 
 
-function features(state = ["js", "stackwalk"], action) {
+function features(state = ["js", "stackwalk", "responsiveness"], action) {
   switch (action.type) {
     case "CHANGE_FEATURES":
       return action.features;
