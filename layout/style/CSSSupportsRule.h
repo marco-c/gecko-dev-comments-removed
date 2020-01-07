539df6a253a0170de710b22dfabd4851c2d1b9d4
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 02:31:00 +0000

    Bug 1427512 - Part 2: Remove nsIDOMCSSSupportsRule. r=xidorn
    MozReview-Commit-ID: JQ721P3IMZN
    
    UltraBlame original commit: 967ab08fb8b296cb102418588f03a400dc443be3

--- layout/style/CSSSupportsRule.h
+++ layout/style/CSSSupportsRule.h
@@ -8,13 +8,13 @@
 #define mozilla_dom_CSSSupportsRule_h
 
 #include "mozilla/css/GroupRule.h"
-#include "nsIDOMCSSSupportsRule.h"
+#include "nsIDOMCSSConditionRule.h"
 
 namespace mozilla {
 namespace dom {
 
 class CSSSupportsRule : public css::ConditionRule
-                      , public nsIDOMCSSSupportsRule
+                      , public nsIDOMCSSConditionRule
 {
 protected:
   using ConditionRule::ConditionRule;
@@ -31,9 +31,6 @@ public:
 
   
   NS_IMETHOD SetConditionText(const nsAString& aConditionText) override = 0;
-
-  
-  NS_DECL_NSIDOMCSSSUPPORTSRULE
 
   
   uint16_t Type() const override { return nsIDOMCSSRule::SUPPORTS_RULE; }
