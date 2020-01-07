eb9c6667d6bb0d6e5adfff39ea06b69e7b456b21
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 12:34:49 +0000

    Bug 1454384 - [1.0] Enforce non-null extras and arguments in GeckoRuntimeSettings. r=snorp
    UltraBlame original commit: 3eede08e88ded9c1b1c9d32070cdf6e3d88e3569

--- mobile/android/geckoview/src/main/java/org/mozilla/geckoview/GeckoRuntimeSettings.java
+++ mobile/android/geckoview/src/main/java/org/mozilla/geckoview/GeckoRuntimeSettings.java
@@ -52,6 +52,9 @@ public final class GeckoRuntimeSettings implements Parcelable {
 
 
         public @NonNull Builder arguments(final @NonNull String[] args) {
+            if (args == null) {
+                throw new IllegalArgumentException("Arguments must not  be null");
+            }
             mSettings.mArgs = args;
             return this;
         }
@@ -62,6 +65,9 @@ public final class GeckoRuntimeSettings implements Parcelable {
 
 
         public @NonNull Builder extras(final @NonNull Bundle extras) {
+            if (extras == null) {
+                throw new IllegalArgumentException("Extras must not  be null");
+            }
             mSettings.mExtras = extras;
             return this;
         }
