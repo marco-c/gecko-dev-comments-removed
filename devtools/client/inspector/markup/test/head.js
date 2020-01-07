8f42ec44c1684ba83b3489bacf4b5d6c9cd60e0b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 22:48:40 +0000

    Bug 1460848 - Always scroll to container element when clicking on reveal;r=bgrins
    MozReview-Commit-ID: 6KY8EBYX0uD
    
    UltraBlame original commit: ed0176b3adac9dedc614993e464abf8d42f5534f

--- devtools/client/inspector/markup/test/head.js
+++ devtools/client/inspector/markup/test/head.js
@@ -715,3 +715,19 @@ function waitForNMutations(inspector, type, count) {
   });
 }
 
+
+
+
+
+async function clickOnRevealLink(inspector, container) {
+  const onSelection = inspector.selection.once("new-node-front");
+  const revealLink = container.elt.querySelector(".reveal-link");
+  const tagline = revealLink.closest(".tag-line");
+  const win = inspector.markup.doc.defaultView;
+
+  
+  EventUtils.synthesizeMouseAtCenter(tagline, {type: "mouseover"}, win);
+  EventUtils.synthesizeMouseAtCenter(revealLink, {}, win);
+
+  await onSelection;
+}
