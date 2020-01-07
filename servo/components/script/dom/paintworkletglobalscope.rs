216ae6fe4a162ab78da5e5fca74c0e1f3b42f8d6
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 01:58:50 +0000

    servo: Merge #19256 - Paint worklets: Implement timeout for worklet painter threads (from yati-sagade:master); r=asajeffrey
    When a paint worklet thread takes too long, we would like to move on,
    since we have a ~16ms budget for rendering at 60fps. At the moment, there
    is no provision in the paintworklet spec to signal such timeouts to the
    developer. ajeffrey opened an [issue][1] for this, but it got punted to
    v2 of the spec. Hence we are silently timing out unresponsive paint
    scripts.
    
    The timeout value is chosen to be 10ms by default, and can be overridden
    by setting the SERVO_PAINT_WORKLET_TIMEOUT_MS environment variable.
    
    In the absence of such a timeout, the reftest in this commit would fail
    by timing out the testrunner itself, since the paint script never
    returns. From my discussions with ajeffrey, this should do until we spec
    out a way to signal timeouts to the script developer.
    
    This fixes #17370.
    
    [1]: https://github.com/w3c/css-houdini-drafts/issues/507
    
    <!-- Please describe your changes on the following line: -->
    
    ---
    <!-- Thank you for contributing to Servo! Please replace each `[ ]` by `[X]` when the step is complete, and replace `__` with appropriate data: -->
    - [x] `./mach build -d` does not report any errors
    - [x] `./mach test-tidy` does not report any errors
    - [x] These changes fix #__ (github issue number if applicable).
    
    <!-- Either: -->
    - [x] There are tests for these changes OR
    - [ ] These changes do not require tests because _____
    
    <!-- Also, please make sure that "Allow edits from maintainers" checkbox is checked, so that we can help you if you get stuck somewhere along the way.-->
    
    <!-- Pull requests that do not address these steps are welcome, but they will require additional verification as part of the review process. -->
    
    Source-Repo: https://github.com/servo/servo
    Source-Revision: 54f8cc37c5a7874607128e3518fd0982f7065565
    
    UltraBlame original commit: 917173512a567ac19442db3b18afa983c93d725f

--- servo/components/script/dom/paintworkletglobalscope.rs
+++ servo/components/script/dom/paintworkletglobalscope.rs
@@ -45,9 +45,10 @@ use js::rust::Runtime;
 use msg::constellation_msg::PipelineId;
 use net_traits::image::base::PixelFormat;
 use net_traits::image_cache::ImageCache;
-use script_traits::DrawAPaintImageResult;
+use script_traits::{DrawAPaintImageResult, PaintWorkletError};
 use script_traits::Painter;
 use servo_atoms::Atom;
+use servo_config::prefs::PREFS;
 use servo_url::ServoUrl;
 use std::cell::Cell;
 use std::collections::HashMap;
@@ -58,6 +59,8 @@ use std::sync::Arc;
 use std::sync::Mutex;
 use std::sync::mpsc;
 use std::sync::mpsc::Sender;
+use std::thread;
+use std::time::Duration;
 use style_traits::CSSPixel;
 use style_traits::DevicePixel;
 use style_traits::SpeculativePainter;
@@ -343,7 +346,7 @@ impl PaintWorkletGlobalScope {
                                   device_pixel_ratio: TypedScale<f32, CSSPixel, DevicePixel>,
                                   properties: Vec<(Atom, String)>,
                                   arguments: Vec<String>)
-                                  -> DrawAPaintImageResult {
+                                  -> Result<DrawAPaintImageResult, PaintWorkletError> {
                 let name = self.name.clone();
                 let (sender, receiver) = mpsc::channel();
                 let task = PaintWorkletTask::DrawAPaintImage(name,
@@ -354,13 +357,74 @@ impl PaintWorkletGlobalScope {
                                                              sender);
                 self.executor.lock().expect("Locking a painter.")
                     .schedule_a_worklet_task(WorkletTask::Paint(task));
-                receiver.recv().expect("Worklet thread died?")
+
+                let timeout = PREFS.get("dom.worklet.timeout_ms")
+                                   .as_u64()
+                                   .unwrap_or(10u64);
+
+                let timeout_duration = Duration::from_millis(timeout);
+                receiver.recv_timeout(timeout_duration)
+                        .map_err(|e| PaintWorkletError::from(e))
             }
         }
         Box::new(WorkletPainter {
             name: name,
             executor: Mutex::new(self.worklet_global.executor()),
         })
+    }
+}
+
+
+pub enum PaintWorkletTask {
+    DrawAPaintImage(Atom,
+                    TypedSize2D<f32, CSSPixel>,
+                    TypedScale<f32, CSSPixel, DevicePixel>,
+                    Vec<(Atom, String)>,
+                    Vec<String>,
+                    Sender<DrawAPaintImageResult>),
+    SpeculativelyDrawAPaintImage(Atom,
+                                 Vec<(Atom, String)>,
+                                 Vec<String>),
+}
+
+
+
+
+
+#[derive(JSTraceable, MallocSizeOf)]
+#[must_root]
+struct PaintDefinition {
+    class_constructor: Heap<JSVal>,
+    paint_function: Heap<JSVal>,
+    constructor_valid_flag: Cell<bool>,
+    context_alpha_flag: bool,
+    
+    input_arguments_len: usize,
+    
+    
+    
+    context: Dom<PaintRenderingContext2D>,
+}
+
+impl PaintDefinition {
+    fn new(class_constructor: HandleValue,
+           paint_function: HandleValue,
+           alpha: bool,
+           input_arguments_len: usize,
+           context: &PaintRenderingContext2D)
+           -> Box<PaintDefinition>
+    {
+        let result = Box::new(PaintDefinition {
+            class_constructor: Heap::default(),
+            paint_function: Heap::default(),
+            constructor_valid_flag: Cell::new(true),
+            context_alpha_flag: alpha,
+            input_arguments_len: input_arguments_len,
+            context: Dom::from_ref(context),
+        });
+        result.class_constructor.set(class_constructor.get());
+        result.paint_function.set(paint_function.get());
+        result
     }
 }
 
@@ -445,58 +509,14 @@ impl PaintWorkletGlobalScopeMethods for PaintWorkletGlobalScope {
 
         Ok(())
     }
-}
 
-
-pub enum PaintWorkletTask {
-    DrawAPaintImage(Atom,
-                    TypedSize2D<f32, CSSPixel>,
-                    TypedScale<f32, CSSPixel, DevicePixel>,
-                    Vec<(Atom, String)>,
-                    Vec<String>,
-                    Sender<DrawAPaintImageResult>),
-    SpeculativelyDrawAPaintImage(Atom,
-                                 Vec<(Atom, String)>,
-                                 Vec<String>),
-}
-
-
-
-
-
-#[derive(JSTraceable, MallocSizeOf)]
-#[must_root]
-struct PaintDefinition {
-    class_constructor: Heap<JSVal>,
-    paint_function: Heap<JSVal>,
-    constructor_valid_flag: Cell<bool>,
-    context_alpha_flag: bool,
-    
-    input_arguments_len: usize,
     
     
     
-    context: Dom<PaintRenderingContext2D>,
-}
-
-impl PaintDefinition {
-    fn new(class_constructor: HandleValue,
-           paint_function: HandleValue,
-           alpha: bool,
-           input_arguments_len: usize,
-           context: &PaintRenderingContext2D)
-           -> Box<PaintDefinition>
-    {
-        let result = Box::new(PaintDefinition {
-            class_constructor: Heap::default(),
-            paint_function: Heap::default(),
-            constructor_valid_flag: Cell::new(true),
-            context_alpha_flag: alpha,
-            input_arguments_len: input_arguments_len,
-            context: Dom::from_ref(context),
-        });
-        result.class_constructor.set(class_constructor.get());
-        result.paint_function.set(paint_function.get());
-        result
+    
+    
+    
+    fn Sleep(&self, ms: u64) {
+        thread::sleep(Duration::from_millis(ms));
     }
 }
