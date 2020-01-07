6be2955236cdbcca9eef6c41de6635f81674dcc4
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 00:04:51 +0000

    Backed out changeset 994147e6fd70 (bug 1405562) for build bustages on obj-firefox/dist/include/mozilla/layers/KnowsCompositor.h:100:75 r=backout on a CLOSED TREE
    UltraBlame original commit: dd549b14646ce686ad4df1a6a1f9ddb164d4eed8

--- dom/media/MediaData.cpp
+++ dom/media/MediaData.cpp
@@ -323,7 +323,8 @@ VideoData::CreateAndCopyData(const VideoInfo& aInfo,
   
   
   if (IsWin8OrLater() && !XRE_IsParentProcess() &&
-      aAllocator && aAllocator->SupportsD3D11()) {
+      aAllocator && aAllocator->GetCompositorBackendType()
+                    == layers::LayersBackend::LAYERS_D3D11) {
     RefPtr<layers::D3D11YCbCrImage> d3d11Image = new layers::D3D11YCbCrImage();
     PlanarYCbCrData data = ConstructPlanarYCbCrData(aInfo, aBuffer, aPicture);
     if (d3d11Image->SetData(layers::ImageBridgeChild::GetSingleton()
