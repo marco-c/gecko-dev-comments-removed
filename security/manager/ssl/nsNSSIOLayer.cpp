b93eb08d6f2d6858d05f7a5a4f68914d530f3db1
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 00:05:50 +0000

    Bug 1422156 - Update tlsFlags to change 0x40 to be compat mode rather than 7e02 handshake. r=keeler
    Reviewers: keeler
    
    Reviewed By: keeler
    
    Subscribers: mcmanus
    
    Bug #: 1422156
    
    Differential Revision: https://phabricator.services.mozilla.com/D306
    
    UltraBlame original commit: 989959ce9988e3ac9308c28ac10b5dc87f3b1036

--- security/manager/ssl/nsNSSIOLayer.cpp
+++ security/manager/ssl/nsNSSIOLayer.cpp
@@ -94,7 +94,7 @@ static uint32_t getTLSProviderFlagFallbackLimit(uint32_t flags)
   return (flags & 0x38) >> 3;
 }
 
-static bool getTLSProviderFlagAltHandshake(uint32_t flags)
+static bool getTLSProviderFlagCompatMode(uint32_t flags)
 {
   return (flags & 0x40);
 }
@@ -2602,12 +2602,12 @@ nsSSLIOLayerSetOptions(PRFileDesc* fd, bool forSTARTTLS,
   }
 
   
-  if (getTLSProviderFlagAltHandshake(infoObject->GetProviderTlsFlags())) {
+  if (getTLSProviderFlagCompatMode(infoObject->GetProviderTlsFlags())) {
     MOZ_LOG(gPIPNSSLog, LogLevel::Debug,
-            ("[%p] nsSSLIOLayerSetOptions: Use AltHandshake\n", fd));
-    if (SECSuccess != SSL_UseAltServerHelloType(fd, PR_TRUE)) {
+            ("[%p] nsSSLIOLayerSetOptions: Use Compatible Handshake\n", fd));
+    if (SECSuccess != SSL_OptionSet(fd, SSL_ENABLE_TLS13_COMPAT_MODE, PR_TRUE)) {
           MOZ_LOG(gPIPNSSLog, LogLevel::Error,
-                  ("[%p] nsSSLIOLayerSetOptions: Use AltHandshake failed\n", fd));
+                  ("[%p] nsSSLIOLayerSetOptions: Setting compat mode failed\n", fd));
           
     }
   }
