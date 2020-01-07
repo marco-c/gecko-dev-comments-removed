30bd4269b42548d7936cf5417854e3e3b656f3da
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 19:46:17 +0000

    bug 1463425 - Fix flake8 in browser/ r=ahal,gps
    MozReview-Commit-ID: 11Kil84u0TP
    
    UltraBlame original commit: 02159d1ec622088538ee4a5d1d9af5043e8c787f

--- browser/locales/filter.py
+++ browser/locales/filter.py
@@ -2,40 +2,41 @@
 
 
 
-def test(mod, path, entity = None):
-  import re
-  
-  if mod not in ("netwerk", "dom", "toolkit", "security/manager",
-                 "devtools/client", "devtools/shared", "devtools/startup",
-                 "browser",
-                 "browser/extensions/formautofill",
-                 "browser/extensions/onboarding",
-                 "browser/extensions/webcompat-reporter",
-                 "extensions/spellcheck",
-                 "other-licenses/branding/firefox",
-                 "browser/branding/official",
-                 "services/sync"):
-    return "ignore"
-  if mod not in ("browser", "extensions/spellcheck"):
-    
-    return "error"
-  if entity is None:
-    
-    if mod == "extensions/spellcheck":
-      return "ignore"
-    return "error"
-  if mod == "extensions/spellcheck":
-    
-    return "error"
-  if path == "defines.inc":
-    return "ignore" if entity == "MOZ_LANGPACK_CONTRIBUTORS" else "error"
 
-  if mod == "browser" and path == "chrome/browser-region/region.properties":
+def test(mod, path, entity=None):
+    import re
     
-    return ("ignore"
-            if (re.match(r"browser\.search\.order\.[1-9]", entity) or
-                re.match(r"browser\.contentHandlers\.types\.[0-5]", entity) or
-                re.match(r"gecko\.handlerService\.schemes\.", entity) or
-                re.match(r"gecko\.handlerService\.defaultHandlersVersion", entity))
-            else "error")
-  return "error"
+    if mod not in ("netwerk", "dom", "toolkit", "security/manager",
+                   "devtools/client", "devtools/shared", "devtools/startup",
+                   "browser",
+                   "browser/extensions/formautofill",
+                   "browser/extensions/onboarding",
+                   "browser/extensions/webcompat-reporter",
+                   "extensions/spellcheck",
+                   "other-licenses/branding/firefox",
+                   "browser/branding/official",
+                   "services/sync"):
+        return "ignore"
+    if mod not in ("browser", "extensions/spellcheck"):
+        
+        return "error"
+    if entity is None:
+        
+        if mod == "extensions/spellcheck":
+            return "ignore"
+        return "error"
+    if mod == "extensions/spellcheck":
+        
+        return "error"
+    if path == "defines.inc":
+        return "ignore" if entity == "MOZ_LANGPACK_CONTRIBUTORS" else "error"
+
+    if mod == "browser" and path == "chrome/browser-region/region.properties":
+        
+        return ("ignore"
+                if (re.match(r"browser\.search\.order\.[1-9]", entity) or
+                    re.match(r"browser\.contentHandlers\.types\.[0-5]", entity) or
+                    re.match(r"gecko\.handlerService\.schemes\.", entity) or
+                    re.match(r"gecko\.handlerService\.defaultHandlersVersion", entity))
+                else "error")
+    return "error"
