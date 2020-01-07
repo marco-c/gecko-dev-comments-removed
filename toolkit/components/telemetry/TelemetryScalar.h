1a19245fc32e600a5e290f4b5074bc76c5940b22
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 20:26:18 +0000

    Backed out 3 changesets (bug 1454606) for assert failure on /build/src/obj-firefox/dist/include/js/Value.h
    Backed out changeset bac711e0ab42 (bug 1454606)
    Backed out changeset 1df93e12f6ec (bug 1454606)
    Backed out changeset b126c9d96aaf (bug 1454606)
    
    UltraBlame original commit: 032953979332b61c8fe2507aea9cae2fda417371

--- toolkit/components/telemetry/TelemetryScalar.h
+++ toolkit/components/telemetry/TelemetryScalar.h
@@ -96,11 +96,6 @@ nsresult SerializeScalars(mozilla::JSONWriter &aWriter);
 nsresult SerializeKeyedScalars(mozilla::JSONWriter &aWriter);
 nsresult DeserializePersistedScalars(JSContext* aCx, JS::HandleValue aData);
 nsresult DeserializePersistedKeyedScalars(JSContext* aCx, JS::HandleValue aData);
-
-
-void DeserializationStarted();
-
-void ApplyPendingOperations();
 } 
 
 #endif 
