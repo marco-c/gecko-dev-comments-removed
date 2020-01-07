80f631f866bf5a0dbce8d889a2d9ed724f964cde
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 21:12:32 +0000

    Bug 1465892 - Re-enable assertions for WR hit-testing codepath. r=botond
    MozReview-Commit-ID: 489Gyhgf1C5
    
    UltraBlame original commit: 8a711fdf59d088cbc50719646934712714dad436

--- gfx/layers/apz/src/APZCTreeManager.cpp
+++ gfx/layers/apz/src/APZCTreeManager.cpp
@@ -2519,12 +2519,9 @@ APZCTreeManager::GetAPZCAtPointWR(const ScreenPoint& aHitTestPoint,
   result = GetTargetAPZC(layersId, scrollId);
   if (!result) {
     
-    
-    
-    
-    
+    MOZ_ASSERT(scrollId == FrameMetrics::NULL_SCROLL_ID);
     result = FindRootApzcForLayersId(layersId);
-    
+    MOZ_ASSERT(result);
   }
 
   bool isScrollbar = bool(hitInfo & gfx::CompositorHitTestInfo::eScrollbar);
