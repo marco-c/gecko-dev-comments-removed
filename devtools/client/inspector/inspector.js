35058a7d29fc4f117efa4657108b833108f9aa07
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 11:13:02 +0000

    Bug 1448096 - Throttle the inspector window resize handler with requestIdleCallback. r=bgrins
    UltraBlame original commit: db6ccdb848165514252bf3d7958d9679cc0fc062

--- devtools/client/inspector/inspector.js
+++ devtools/client/inspector/inspector.js
@@ -526,8 +526,11 @@ Inspector.prototype = {
 
 
   onPanelWindowResize: function() {
-    this.splitBox.setState({
-      vert: this.useLandscapeMode(),
+    this.panelWin.cancelIdleCallback(this._resizeTimerId);
+    this._resizeTimerId = this.panelWin.requestIdleCallback(() => {
+      this.splitBox.setState({
+        vert: this.useLandscapeMode(),
+      });
     });
   },
 
