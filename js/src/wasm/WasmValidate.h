0f99eaded3ac5a90f0103c65bc97b1e53606d7fe
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 23:53:43 +0000

    Bug 1472639; r=jseward
    UltraBlame original commit: 7ad1f8237199ca9d233d3c6a3a48c2a9c975723d

--- js/src/wasm/WasmValidate.h
+++ js/src/wasm/WasmValidate.h
@@ -765,6 +765,9 @@ ValidateFunctionBody(const ModuleEnvironment& env, uint32_t funcIndex, uint32_t 
 MOZ_MUST_USE bool
 DecodeModuleTail(Decoder& d, ModuleEnvironment* env);
 
+void
+ConvertMemoryPagesToBytes(Limits* memory);
+
 
 
 
