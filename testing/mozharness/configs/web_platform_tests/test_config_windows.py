ad6fe75b176e9b89258017e303bd11a37aa56512
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 13:02:57 +0000

    Bug 1452179: [mozharness] Don't special case the path to mozinstall on windows; r=whimboo
    New versions of pip don't install entrypoint scripts with a `-script.py` on
    windows. Instead, just call the bare script name everywhere. On unix-like
    systems, this uses the shebang; on Windows, this executes the `.exe` wrapper.
    
    Differential Revision: https://phabricator.services.mozilla.com/D967
    
    UltraBlame original commit: 8bfb706430413ef5a3db44ec6f8337730b522a56

--- testing/mozharness/configs/web_platform_tests/test_config_windows.py
+++ testing/mozharness/configs/web_platform_tests/test_config_windows.py
@@ -4,7 +4,6 @@
 
 
 
-import os
 import sys
 
 config = {
@@ -21,8 +20,6 @@ config = {
     "exes": {
         'python': sys.executable,
         'hg': 'c:/mozilla-build/hg/hg',
-        'mozinstall': ['%s/build/venv/scripts/python' % os.getcwd(),
-                       '%s/build/venv/scripts/mozinstall-script.py' % os.getcwd()],
     },
 
     "default_actions": [
