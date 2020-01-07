128500e4da880a7881dbcf71bc615b8102500123
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 09:01:52 +0000

    Bug 1446348 - Poison TI data more eagerly. r=tcampbell
    UltraBlame original commit: 99a7578ee8e4a7b968b7a63061026f0c40f78cbb

--- js/src/vm/TypeInference.cpp
+++ js/src/vm/TypeInference.cpp
@@ -4155,6 +4155,8 @@ ConstraintTypeSet::trace(Zone* zone, JSTracer* trc)
         }
         MOZ_RELEASE_ASSERT(oldObjectCount == oldObjectsFound);
         setBaseObjectCount(objectCount);
+        
+        JS_POISON(oldArray - 1, JS_SWEPT_TI_PATTERN, (oldCapacity + 1) * sizeof(oldArray[0]));
     } else if (objectCount == 1) {
         ObjectKey* key = (ObjectKey*) objectSet;
         TraceObjectKey(trc, &key);
@@ -4191,6 +4193,8 @@ ConstraintTypeSet::sweep(Zone* zone, AutoClearTypeInferenceStateOnOOM& oom)
     if (objectCount >= 2) {
         unsigned oldCapacity = TypeHashSet::Capacity(objectCount);
         ObjectKey** oldArray = objectSet;
+
+        MOZ_RELEASE_ASSERT(uintptr_t(oldArray[-1]) == oldCapacity);
 
         clearObjects();
         objectCount = 0;
@@ -4229,6 +4233,8 @@ ConstraintTypeSet::sweep(Zone* zone, AutoClearTypeInferenceStateOnOOM& oom)
             }
         }
         setBaseObjectCount(objectCount);
+        
+        JS_POISON(oldArray - 1, JS_SWEPT_TI_PATTERN, (oldCapacity + 1) * sizeof(oldArray[0]));
     } else if (objectCount == 1) {
         ObjectKey* key = (ObjectKey*) objectSet;
         if (!IsObjectKeyAboutToBeFinalized(&key)) {
@@ -4261,7 +4267,9 @@ ConstraintTypeSet::sweep(Zone* zone, AutoClearTypeInferenceStateOnOOM& oom)
                 oom.setOOM();
             }
         }
-        constraint = constraint->next();
+        TypeConstraint* next = constraint->next();
+        JS_POISON(constraint, JS_SWEPT_TI_PATTERN, sizeof(TypeConstraint));
+        constraint = next;
     }
 }
 
@@ -4360,13 +4368,15 @@ ObjectGroup::sweep(AutoClearTypeInferenceStateOnOOM* oom)
 
 
 
+                    JS_POISON(prop, JS_SWEPT_TI_PATTERN, sizeof(Property));
                     continue;
                 }
 
                 Property* newProp = typeLifoAlloc.new_<Property>(*prop);
+                JS_POISON(prop, JS_SWEPT_TI_PATTERN, sizeof(Property));
                 if (newProp) {
                     Property** pentry = TypeHashSet::Insert<jsid, Property, Property>
-                                            (typeLifoAlloc, propertySet, propertyCount, prop->id);
+                                      (typeLifoAlloc, propertySet, propertyCount, newProp->id);
                     if (pentry) {
                         *pentry = newProp;
                         newProp->types.sweep(zone(), *oom);
@@ -4387,9 +4397,11 @@ ObjectGroup::sweep(AutoClearTypeInferenceStateOnOOM* oom)
         prop->types.checkMagic();
         if (singleton() && !prop->types.constraintList() && !zone()->isPreservingCode()) {
             
+            JS_POISON(prop, JS_SWEPT_TI_PATTERN, sizeof(Property));
             clearProperties();
         } else {
             Property* newProp = typeLifoAlloc.new_<Property>(*prop);
+            JS_POISON(prop, JS_SWEPT_TI_PATTERN, sizeof(Property));
             if (newProp) {
                 propertySet = (Property**) newProp;
                 newProp->types.sweep(zone(), *oom);
