5b98dee71f63a83bd0cec455b3d0a0dbe18cd26e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 17:56:16 +0000

    Bug 1433718 - Make unit tests pass with the 3 pane inspector on in nightly. r=pbro
    UltraBlame original commit: b7aeba47bb1fdac1946cc2afcf19accaf4ff4c91

--- devtools/client/inspector/animation/test/browser_animation_logic_avoid-updating-during-hiding.js
+++ devtools/client/inspector/animation/test/browser_animation_logic_avoid-updating-during-hiding.js
@@ -10,6 +10,8 @@
 
 
 add_task(async function() {
+  info("Switch to 2 pane inspector to see if the animation only refreshes when visible");
+  await pushPref("devtools.inspector.three-pane-enabled", false);
   await addTab(URL_ROOT + "doc_custom_playback_rate.html");
   const { animationInspector, inspector, panel } = await openAnimationInspector();
 
