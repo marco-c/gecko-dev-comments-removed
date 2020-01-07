4b0dba260eef832326b4f0a5265b3ba3bd486368
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 12:51:42 +0000

    Backed out 3 changesets (bug 1444976) for failing android at dom/media/tests/mochitest/test_getUserMedia_mediaStreamTrackClone.html a=backout
    Backed out changeset 79b7ee8b01ee (bug 1444976)
    Backed out changeset 35ca96cabe3c (bug 1444976)
    Backed out changeset 2512c9c24244 (bug 1444976)
    
    UltraBlame original commit: 0e45c13b34e815cb42a9f08bb44142d1a81e186e

--- dom/media/GraphDriver.cpp
+++ dom/media/GraphDriver.cpp
@@ -3,14 +3,12 @@
 
 
 
-
 #include <MediaStreamGraphImpl.h>
 #include "mozilla/dom/AudioContext.h"
 #include "mozilla/SharedThreadPool.h"
 #include "mozilla/ClearOnShutdown.h"
 #include "mozilla/Unused.h"
 #include "CubebUtils.h"
-#include "Tracing.h"
 
 #ifdef MOZ_WEBRTC
 #include "webrtc/MediaEngineWebRTC.h"
@@ -883,9 +881,6 @@ long
 AudioCallbackDriver::DataCallback(const AudioDataValue* aInputBuffer,
                                   AudioDataValue* aOutputBuffer, long aFrames)
 {
-   TRACE_AUDIO_CALLBACK_BUDGET(aFrames, mSampleRate);
-   TRACE_AUDIO_CALLBACK();
-
   
   if (!mAddedMixer) {
     mGraphImpl->mMixer.AddCallback(this);
