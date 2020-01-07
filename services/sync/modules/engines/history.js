07b929c6b5cdfcad7f8fa443609df37ac4eea9b6
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 01:20:53 +0000

    Bug 1423395 - Clamp and truncate microseconds from incoming history visit dates when deduping r=markh
    MozReview-Commit-ID: H1lljWcSZLZ
    
    UltraBlame original commit: 9723654fda3997e90b1c1b2eed5df324c9457c6b

--- services/sync/modules/engines/history.js
+++ services/sync/modules/engines/history.js
@@ -314,7 +314,11 @@ HistoryStore.prototype = {
 
     let i, k;
     for (i = 0; i < curVisitsAsArray.length; i++) {
-      curVisits.add(curVisitsAsArray[i].date + "," + curVisitsAsArray[i].type);
+      
+      let {date, type} = curVisitsAsArray[i];
+      let dateObj = PlacesUtils.toDate(date);
+      let millis = PlacesSyncUtils.history.clampVisitDate(dateObj).getTime();
+      curVisits.add(`${millis},${type}`);
     }
 
     
@@ -340,8 +344,7 @@ HistoryStore.prototype = {
       let originalVisitDate = PlacesUtils.toDate(Math.round(visit.date));
       visit.date = PlacesSyncUtils.history.clampVisitDate(originalVisitDate);
 
-      let visitDateAsPRTime = PlacesUtils.toPRTime(visit.date);
-      let visitKey = visitDateAsPRTime + "," + visit.type;
+      let visitKey = `${visit.date.getTime()},${visit.type}`;
       if (curVisits.has(visitKey)) {
         
         
