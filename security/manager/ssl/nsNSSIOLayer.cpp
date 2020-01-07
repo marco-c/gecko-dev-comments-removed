93e0f6c0c7cd02d6747af310deec67e94e45de90
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 18:01:02 +0000

    bug 1255120 - add telemetry for client certificate use r=jcj
    MozReview-Commit-ID: Et8Fm78Ujxn
    
    UltraBlame original commit: 69c1887daefdf3c93b4d6d9227b389896f9cfd14

--- security/manager/ssl/nsNSSIOLayer.cpp
+++ security/manager/ssl/nsNSSIOLayer.cpp
@@ -1985,6 +1985,8 @@ nsConvertCANamesToStrings(const UniquePLArenaPool& arena, char** caNameStrings,
         }
 
         if (headerlen + contentlen != dername->len) {
+            Telemetry::ScalarAdd(Telemetry::ScalarID::SECURITY_CLIENT_CERT,
+                                 NS_LITERAL_STRING("compat"), 1);
             
             
             
@@ -2145,6 +2147,9 @@ nsNSS_SSLGetClientAuthData(void* arg, PRFileDesc* socket,
     return SECFailure;
   }
 
+  Telemetry::ScalarAdd(Telemetry::ScalarID::SECURITY_CLIENT_CERT,
+                       NS_LITERAL_STRING("requested"), 1);
+
   RefPtr<nsNSSSocketInfo> info(
     BitwiseCast<nsNSSSocketInfo*, PRFilePrivate*>(socket->higher->secret));
 
@@ -2190,6 +2195,8 @@ nsNSS_SSLGetClientAuthData(void* arg, PRFileDesc* socket,
   } else if (*runnable->mPRetCert || *runnable->mPRetKey) {
     
     info->SetSentClientCert();
+    Telemetry::ScalarAdd(Telemetry::ScalarID::SECURITY_CLIENT_CERT,
+                         NS_LITERAL_STRING("sent"), 1);
   }
 
   return runnable->mRV;
