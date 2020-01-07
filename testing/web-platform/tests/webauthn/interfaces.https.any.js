1dbb42762f705cd8a7fe78738bfbb86ef65a899e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 12:29:09 +0000

    Bug 1452296 [wpt PR 10285] - Check inherited interfaces exist, a=testonly
    Automatic update from web-platform-testsCheck inherited interfaces exist (#10285)
    
    Also fix affected tests.
    
    wpt-commits: 4d1c1925e6c863b9365d7b8196f7b538378ffdb5
    wpt-pr: 10285
    wpt-commits: 4d1c1925e6c863b9365d7b8196f7b538378ffdb5
    wpt-pr: 10285
    
    UltraBlame original commit: 12541a4f7b34e260399935dbcaf65d4aa2abad64

--- testing/web-platform/tests/webauthn/interfaces.https.any.js
+++ testing/web-platform/tests/webauthn/interfaces.https.any.js
@@ -20,6 +20,7 @@ promise_test(async () => {
   idlArray.add_untested_idls('interface CredentialCreationOptions {};');
   idlArray.add_untested_idls('interface CredentialRequestOptions {};');
   idlArray.add_untested_idls("interface Navigator { };");
+  idlArray.add_untested_idls("interface Credential { };");
   
   idlArray.add_untested_idls("partial interface Navigator { readonly attribute WebAuthentication authentication; };");
   idlArray.add_objects({
