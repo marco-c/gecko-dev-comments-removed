7504dca64e199feb87dc350147572c5ab1a89553
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 01:04:13 +0000

    Bug 1419771 - Introduce DOMPreferences, a thread-safe access to preferences for DOM - part 15 - Offscreen Canvas enabled, r=asuth
    UltraBlame original commit: 1397a6bbb4460fa7b1d4da88cabdcd128dbd1ec9

--- dom/base/DOMPreferences.h
+++ dom/base/DOMPreferences.h
@@ -71,6 +71,10 @@ public:
   
   static bool RequestContextEnabled();
   static bool RequestContextEnabled(JSContext* aCx, JSObject* aObj);
+
+  
+  static bool OffscreenCanvasEnabled();
+  static bool OffscreenCanvasEnabled(JSContext* aCx, JSObject* aObj);
 };
 
 } 
