4cf94061cc2a9984df00f2e29a08fb5875271816
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 06:08:57 +0000

    Bug 1438956 - Remove nsIDOMTimeRanges. r=bz
    MozReview-Commit-ID: DAgQ0OkSxkq
    
    UltraBlame original commit: 86e9709e94b370c1527b6c0c0f4f266fd598ae90

--- dom/html/HTMLMediaElement.cpp
+++ dom/html/HTMLMediaElement.cpp
@@ -2699,35 +2699,27 @@ HTMLMediaElement::SetCurrentTime(double aCurrentTime, ErrorResult& aRv)
 
 
 
-
-static nsresult
+static bool
 IsInRanges(TimeRanges& aRanges,
            double aValue,
-           bool& aIsInRanges,
            int32_t& aIntervalIndex)
 {
-  aIsInRanges = false;
-  uint32_t length;
-  nsresult rv = aRanges.GetLength(&length);
-  NS_ENSURE_SUCCESS(rv, rv);
+  uint32_t length = aRanges.Length();
+
   for (uint32_t i = 0; i < length; i++) {
-    double start, end;
-    rv = aRanges.Start(i, &start);
-    NS_ENSURE_SUCCESS(rv, rv);
+    double start = aRanges.Start(i, IgnoreErrors());
     if (start > aValue) {
       aIntervalIndex = i - 1;
-      return NS_OK;
+      return false;
     }
-    rv = aRanges.End(i, &end);
-    NS_ENSURE_SUCCESS(rv, rv);
+    double end = aRanges.End(i, IgnoreErrors());
     if (aValue <= end) {
       aIntervalIndex = i;
-      aIsInRanges = true;
-      return NS_OK;
+      return true;
     }
   }
   aIntervalIndex = length - 1;
-  return NS_OK;
+  return false;
 }
 
 already_AddRefed<Promise>
@@ -2791,9 +2783,8 @@ HTMLMediaElement::Seek(double aTime,
   }
   RefPtr<TimeRanges> seekable =
     new TimeRanges(ToSupports(OwnerDoc()), seekableIntervals);
-  uint32_t length = 0;
-  seekable->GetLength(&length);
-  if (!length) {
+  uint32_t length = seekable->Length();
+  if (length == 0) {
     promise->MaybeReject(NS_ERROR_DOM_INVALID_STATE_ERR);
     return promise.forget();
   }
@@ -2804,25 +2795,14 @@ HTMLMediaElement::Seek(double aTime,
   
   
   int32_t range = 0;
-  bool isInRange = false;
-  if (NS_FAILED(IsInRanges(*seekable, aTime, isInRange, range))) {
-    aRv.Throw(NS_ERROR_DOM_INVALID_STATE_ERR); 
-    return promise.forget();
-  }
+  bool isInRange = IsInRanges(*seekable, aTime, range);
   if (!isInRange) {
     if (range != -1) {
       
       
       if (uint32_t(range + 1) < length) {
-        double leftBound, rightBound;
-        if (NS_FAILED(seekable->End(range, &leftBound))) {
-          aRv.Throw(NS_ERROR_DOM_INVALID_STATE_ERR);
-          return promise.forget();
-        }
-        if (NS_FAILED(seekable->Start(range + 1, &rightBound))) {
-          aRv.Throw(NS_ERROR_DOM_INVALID_STATE_ERR);
-          return promise.forget();
-        }
+        double leftBound = seekable->End(range, IgnoreErrors());
+        double rightBound = seekable->Start(range + 1, IgnoreErrors());
         double distanceLeft = Abs(leftBound - aTime);
         double distanceRight = Abs(rightBound - aTime);
         if (distanceLeft == distanceRight) {
@@ -2834,15 +2814,12 @@ HTMLMediaElement::Seek(double aTime,
       } else {
         
         
-        if (NS_FAILED(seekable->End(length - 1, &aTime))) {
-          aRv.Throw(NS_ERROR_DOM_INVALID_STATE_ERR);
-          return promise.forget();
-        }
+        aTime = seekable->GetEndTime();
       }
     } else {
       
       
-      seekable->Start(0, &aTime);
+      aTime = seekable->GetStartTime();
     }
   }
 
@@ -2903,13 +2880,11 @@ HTMLMediaElement::Played()
 
   uint32_t timeRangeCount = 0;
   if (mPlayed) {
-    mPlayed->GetLength(&timeRangeCount);
+    timeRangeCount = mPlayed->Length();
   }
   for (uint32_t i = 0; i < timeRangeCount; i++) {
-    double begin;
-    double end;
-    mPlayed->Start(i, &begin);
-    mPlayed->End(i, &end);
+    double begin = mPlayed->Start(i, IgnoreErrors());
+    double end = mPlayed->End(i, IgnoreErrors());
     ranges->Add(begin, end);
   }
 
