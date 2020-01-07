68e299b348516eaa5a45b7d664a4f25c6914ccee
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 22:38:27 +0000

    Bug 1349799 - Implement WebGLPowerPreference and gl::CreateContextFlags::HIGH_POWER. - r=kvark
    Based on patches by :daoshengmu.
    
    MozReview-Commit-ID: FSbJV8DLyJ4
    
    UltraBlame original commit: ee86c06ef4cf80b7ff7c7491fff7312930480cf9

--- dom/canvas/WebGLContext.h
+++ dom/canvas/WebGLContext.h
@@ -30,10 +30,6 @@
 #include "ScopedGLHelpers.h"
 #include "TexUnpackBlob.h"
 
-#ifdef XP_MACOSX
-#include "ForceDiscreteGPUHelperCGL.h"
-#endif
-
 
 #include "CacheMap.h"
 #include "WebGLContextLossHandler.h"
@@ -117,30 +113,17 @@ void AssertUintParamCorrect(gl::GLContext* gl, GLenum pname, GLuint shadow);
 
 struct WebGLContextOptions
 {
-    
+    bool alpha = true;
+    bool depth = true;
+    bool stencil = false;
+    bool premultipliedAlpha = true;
+    bool antialias = true;
+    bool preserveDrawingBuffer = false;
+    bool failIfMajorPerformanceCaveat = false;
+    dom::WebGLPowerPreference powerPreference = dom::WebGLPowerPreference::Default;
+
     WebGLContextOptions();
-
-    bool operator==(const WebGLContextOptions& other) const {
-        return
-            alpha == other.alpha &&
-            depth == other.depth &&
-            stencil == other.stencil &&
-            premultipliedAlpha == other.premultipliedAlpha &&
-            antialias == other.antialias &&
-            preserveDrawingBuffer == other.preserveDrawingBuffer;
-    }
-
-    bool operator!=(const WebGLContextOptions& other) const {
-        return !operator==(other);
-    }
-
-    bool alpha;
-    bool depth;
-    bool stencil;
-    bool premultipliedAlpha;
-    bool antialias;
-    bool preserveDrawingBuffer;
-    bool failIfMajorPerformanceCaveat;
+    bool operator==(const WebGLContextOptions&) const;
 };
 
 
@@ -2041,16 +2024,6 @@ public:
     template <typename WebGLObjectType>
     JSObject* WebGLObjectAsJSObject(JSContext* cx, const WebGLObjectType*,
                                     ErrorResult& rv) const;
-
-#ifdef XP_MACOSX
-    
-    
-    
-    
-    
-    ForceDiscreteGPUHelperCGL mForceDiscreteGPUHelper;
-#endif
-
 public:
     
     void GenerateWarning(const char* fmt, ...) const MOZ_FORMAT_PRINTF(2, 3);
