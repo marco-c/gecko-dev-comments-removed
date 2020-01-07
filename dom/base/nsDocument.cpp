43bd3abec0a31b7c19d1c7e7eefa9dc3a923f380
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 17:21:25 +0000

    Merge mozilla-central to autoland. a=merge CLOSED TREE
    UltraBlame original commit: f551d1f94952bd23073a75244fba738e6e3080c9

--- dom/base/nsDocument.cpp
+++ dom/base/nsDocument.cpp
@@ -15,6 +15,7 @@
 #include "mozilla/ArrayUtils.h"
 #include "mozilla/AutoRestore.h"
 #include "mozilla/BinarySearch.h"
+#include "mozilla/CSSEnabledState.h"
 #include "mozilla/DebugOnly.h"
 #include "mozilla/EffectSet.h"
 #include "mozilla/EnumSet.h"
@@ -3524,11 +3525,10 @@ nsIDocument::GetActiveElement()
   }
 
   
-  RefPtr<nsHTMLDocument> htmlDoc = AsHTMLDocument();
-  if (htmlDoc) {
+  if (IsHTMLOrXHTML()) {
     
     
-    return htmlDoc->GetBody();
+    return AsHTMLDocument()->GetBody();
   }
 
   
@@ -5744,7 +5744,7 @@ GetPseudoElementType(const nsString& aString, ErrorResult& aRv)
   }
   RefPtr<nsAtom> pseudo = NS_Atomize(Substring(aString, 1));
   return nsCSSPseudoElements::GetPseudoType(pseudo,
-      nsCSSProps::EnabledState::eInUASheets);
+      CSSEnabledState::eInUASheets);
 }
 
 already_AddRefed<Element>
