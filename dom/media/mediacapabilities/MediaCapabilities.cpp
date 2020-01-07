b1c0a89ac629d61d71d4f3f20c403fb1900d2031
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 23:33:05 +0000

    Bug 1409664 - P18. Properly pass KnowsCompositor object to decoder. r=bryce, r=bz
    Summary:
    To properly determine if a decoder is hardware accelerated, we must pass information about the compositor to the decoder.
    
    Depends on D1631
    
    Tags: #secure-revision
    
    Bug #: 1409664
    
    Differential Revision: https://phabricator.services.mozilla.com/D1632
    
    UltraBlame original commit: 25ff69daabcab6c10cfd9275cccbda3790ed5a3e

--- dom/media/mediacapabilities/MediaCapabilities.cpp
+++ dom/media/mediacapabilities/MediaCapabilities.cpp
@@ -6,6 +6,7 @@
 
 #include "MediaCapabilities.h"
 #include "DecoderTraits.h"
+#include "Layers.h"
 #include "MediaInfo.h"
 #include "MediaRecorder.h"
 #include "PDMFactory.h"
@@ -18,6 +19,8 @@
 #include "mozilla/dom/Promise.h"
 #include "mozilla/dom/WorkerPrivate.h"
 #include "mozilla/dom/WorkerRef.h"
+#include "mozilla/layers/KnowsCompositor.h"
+#include "nsContentUtils.h"
 
 namespace mozilla {
 namespace dom {
@@ -160,16 +163,18 @@ MediaCapabilities::DecodingInfo(
     
     
 
+    RefPtr<layers::KnowsCompositor> compositor = GetCompositor();
     double frameRate = videoContainer->ExtendedType().GetFramerate().ref();
     promises.AppendElement(InvokeAsync(
       taskQueue,
       __func__,
-      [taskQueue, frameRate, config = std::move(config)]() mutable
+      [taskQueue, frameRate, compositor, config = std::move(config)]() mutable
       -> RefPtr<CapabilitiesPromise> {
         
         
         CreateDecoderParams params{ *config,
                                     taskQueue,
+                                    compositor,
                                     CreateDecoderParams::VideoFrameRate(
                                       frameRate),
                                     TrackInfo::kVideoTrack };
@@ -388,6 +393,30 @@ bool
 MediaCapabilities::CheckTypeForEncoder(const nsAString& aType)
 {
   return MediaRecorder::IsTypeSupported(aType);
+}
+
+already_AddRefed<layers::KnowsCompositor>
+MediaCapabilities::GetCompositor()
+{
+  nsCOMPtr<nsPIDOMWindowInner> window = do_QueryInterface(GetParentObject());
+  if (NS_WARN_IF(!window)) {
+    return nullptr;
+  }
+
+  nsCOMPtr<nsIDocument> doc = window->GetExtantDoc();
+  if (NS_WARN_IF(!doc)) {
+    return nullptr;
+  }
+  RefPtr<layers::LayerManager> layerManager =
+    nsContentUtils::LayerManagerForDocument(doc);
+  if (NS_WARN_IF(!layerManager)) {
+    return nullptr;
+  }
+  RefPtr<layers::KnowsCompositor> knows = layerManager->AsKnowsCompositor();
+  if (NS_WARN_IF(!knows)) {
+    return nullptr;
+  }
+  return knows->GetForMedia().forget();
 }
 
 bool
