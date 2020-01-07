581fdc72f3c6c53e2d5b11ab9d6ec53b811668d3
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 10:25:13 +0000

    Bug 1437619 [wpt PR 9484] - Correct integration with Sauce Labs service, a=testonly
    Automatic update from web-platform-testsCorrect integration with Sauce Labs service (#9484)
    
    A recently merged patch centralized configuration values which defined
    the domain names available during test execution [1]. That patch was
    based on a mistaken assumption regarding the location of that value and
    the stage of the test execution at which it would be available.
    (Specifically: the value is not present in the environment configuration
    object when the "environment extras" are initialized.) This interfered
    with the initialization of the "environment" for the Sauce Labs service,
    making it impossible to run the tests there.
    
    Defer the retrieval of the environment configuration object to the time
    that object's context manager is activated. Update the call sites to
    provide the environment configuration at this moment. Also update the
    signature of the context manager for the `FontInstaller` "environment
    extra" to accept this second parameter.
    
    [1] https://github.com/w3c/web-platform-tests/pull/8614
    
    wpt-commits: e56563d2b1e8bd225bf4c8b4682cbd12beeae1a7
    wpt-pr: 9484
    wpt-commits: e56563d2b1e8bd225bf4c8b4682cbd12beeae1a7
    wpt-pr: 9484
    
    UltraBlame original commit: b05b78e57595a3f62f44169b76961fd54f7355f8

--- testing/web-platform/tests/tools/wptrunner/wptrunner/browsers/sauce.py
+++ testing/web-platform/tests/tools/wptrunner/wptrunner/browsers/sauce.py
@@ -127,15 +127,29 @@ def get_tar(url, dest):
 class SauceConnect():
 
     def __init__(self, **kwargs):
-        self.config = kwargs["config"]
         self.sauce_user = kwargs["sauce_user"]
         self.sauce_key = kwargs["sauce_key"]
         self.sauce_tunnel_id = kwargs["sauce_tunnel_id"]
         self.sauce_connect_binary = kwargs.get("sauce_connect_binary")
         self.sc_process = None
         self.temp_dir = None
+        self.env_config = None
 
-    def __enter__(self, options):
+    def __call__(self, env_options, env_config):
+        self.env_config = env_config
+
+        return self
+
+    def __enter__(self):
+        
+        
+        
+        
+        
+        
+        
+        assert self.env_config is not None, 'The instance has been invoked.'
+
         if not self.sauce_connect_binary:
             self.temp_dir = tempfile.mkdtemp()
             get_tar("https://saucelabs.com/downloads/sc-4.4.9-linux.tar.gz", self.temp_dir)
@@ -153,7 +167,7 @@ class SauceConnect():
             "--metrics-address=0.0.0.0:9876",
             "--readyfile=./sauce_is_ready",
             "--tunnel-domains",
-            ",".join(self.config['domains'].values())
+            ",".join(self.env_config['domains'].values())
         ])
 
         
@@ -180,6 +194,7 @@ class SauceConnect():
             raise SauceException("Unable to start Sauce Connect Proxy. Process exited with code %s", self.sc_process.returncode)
 
     def __exit__(self, exc_type, exc_val, exc_tb):
+        self.env_config = None
         self.sc_process.terminate()
         if self.temp_dir and os.path.exists(self.temp_dir):
             try:
