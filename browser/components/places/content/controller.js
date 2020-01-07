306d74870444fe94c3537148754eefdcd8597637
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 06:05:53 +0000

    Bug 1437583 - Split plural forms for Delete/Add bookmarks in separate strings r=standard8
    Since the number of tabs is not actually displayed, having two separate strings from 1 vs many is preferable.
    
    MozReview-Commit-ID: y1dgvsCxmC
    
    UltraBlame original commit: a13152330b64654647491499664f7022eb6886c3

--- browser/components/places/content/controller.js
+++ browser/components/places/content/controller.js
@@ -621,14 +621,15 @@ PlacesController.prototype = {
     }
 
     
-    var deleteHistoryItem = document.getElementById("placesContext_delete_history");
-    deleteHistoryItem.label = PlacesUIUtils.getPluralString("cmd.deletePages.label",
-                                                            metadata.length);
-    deleteHistoryItem.accessKey = PlacesUIUtils.getString("cmd.deletePages.accesskey");
-    var createBookmarkItem = document.getElementById("placesContext_createBookmark");
-    createBookmarkItem.label = PlacesUIUtils.getPluralString("cmd.bookmarkPages.label",
-                                                             metadata.length);
-    createBookmarkItem.accessKey = PlacesUIUtils.getString("cmd.bookmarkPages.accesskey");
+    let stringId = metadata.length === 1 ? "SinglePage" : "MultiplePages";
+
+    let deleteHistoryItem = document.getElementById("placesContext_delete_history");
+    deleteHistoryItem.label = PlacesUIUtils.getString(`cmd.delete${stringId}.label`);
+    deleteHistoryItem.accessKey = PlacesUIUtils.getString(`cmd.delete${stringId}.accesskey`);
+
+    let createBookmarkItem = document.getElementById("placesContext_createBookmark");
+    createBookmarkItem.label = PlacesUIUtils.getString(`cmd.bookmark${stringId}.label`);
+    createBookmarkItem.accessKey = PlacesUIUtils.getString(`cmd.bookmark${stringId}.accesskey`);
 
     return usableItemCount > 0;
   },
