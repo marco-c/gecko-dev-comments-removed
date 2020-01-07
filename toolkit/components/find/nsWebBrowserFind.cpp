1ec6aaaa6274a949f4c6077562a8f36b0c9dd494
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 18:44:56 +0000

    Bug 1460940 - Remove nsIDOMDocument uses in toolkit/. r=bz
    MozReview-Commit-ID: LJhw1bKsUkn
    
    UltraBlame original commit: 34afaf053d6d3008482c031b5d13a3aa8eec2c52

--- toolkit/components/find/nsWebBrowserFind.cpp
+++ toolkit/components/find/nsWebBrowserFind.cpp
@@ -20,7 +20,6 @@
 #include "nsIPresShell.h"
 #include "nsPresContext.h"
 #include "nsIDocument.h"
-#include "nsIDOMDocument.h"
 #include "nsISelectionController.h"
 #include "nsIFrame.h"
 #include "nsITextControlFrame.h"
@@ -424,23 +423,21 @@ nsWebBrowserFind::SetSelectionAndScroll(nsPIDOMWindowOuter* aWindow,
 
 
 nsresult
-nsWebBrowserFind::GetRootNode(nsIDOMDocument* aDomDoc, nsIDOMNode** aNode)
+nsWebBrowserFind::GetRootNode(nsIDocument* aDoc, nsIDOMNode** aNode)
 {
+  NS_ENSURE_ARG_POINTER(aDoc);
   NS_ENSURE_ARG_POINTER(aNode);
   *aNode = 0;
 
-  nsCOMPtr<nsIDocument> doc = do_QueryInterface(aDomDoc);
-  NS_ENSURE_ARG_POINTER(doc);
-
-  if (doc->IsHTMLOrXHTML()) {
-    Element* body = doc->GetBody();
+  if (aDoc->IsHTMLOrXHTML()) {
+    Element* body = aDoc->GetBody();
     NS_ENSURE_ARG_POINTER(body);
     NS_ADDREF(*aNode = body->AsDOMNode());
     return NS_OK;
   }
 
   
-  Element* root = doc->GetDocumentElement();
+  Element* root = aDoc->GetDocumentElement();
   NS_ENSURE_ARG_POINTER(root);
   NS_ADDREF(*aNode = root->AsDOMNode());
   return NS_OK;
@@ -450,7 +447,7 @@ nsresult
 nsWebBrowserFind::SetRangeAroundDocument(nsRange* aSearchRange,
                                          nsRange* aStartPt,
                                          nsRange* aEndPt,
-                                         nsIDOMDocument* aDoc)
+                                         nsIDocument* aDoc)
 {
   nsCOMPtr<nsIDOMNode> bodyNode;
   nsresult rv = GetRootNode(aDoc, getter_AddRefs(bodyNode));
@@ -484,7 +481,7 @@ nsWebBrowserFind::SetRangeAroundDocument(nsRange* aSearchRange,
 nsresult
 nsWebBrowserFind::GetSearchLimits(nsRange* aSearchRange,
                                   nsRange* aStartPt, nsRange* aEndPt,
-                                  nsIDOMDocument* aDoc, Selection* aSel,
+                                  nsIDocument* aDoc, Selection* aSel,
                                   bool aWrap)
 {
   NS_ENSURE_ARG_POINTER(aSel);
@@ -727,17 +724,14 @@ nsWebBrowserFind::SearchInFrame(nsPIDOMWindowOuter* aWindow, bool aWrapping,
 
   nsCOMPtr<nsIDOMRange> foundRange;
 
-  nsCOMPtr<nsIDOMDocument> domDoc = do_QueryInterface(theDoc);
-  MOZ_ASSERT(domDoc);
-
   
   if (!aWrapping)
-    rv = GetSearchLimits(searchRange, startPt, endPt, domDoc, sel, false);
+    rv = GetSearchLimits(searchRange, startPt, endPt, theDoc, sel, false);
 
   
   
   else
-    rv = GetSearchLimits(searchRange, startPt, endPt, domDoc, sel, true);
+    rv = GetSearchLimits(searchRange, startPt, endPt, theDoc, sel, true);
 
   NS_ENSURE_SUCCESS(rv, rv);
 
