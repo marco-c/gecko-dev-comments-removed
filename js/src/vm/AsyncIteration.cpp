d77bbd8f02ebc70db4ae16723d41c682e1ff9864
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 12:06:36 +0000

    Bug 1447442 - Part 14: Remove unused parameters from GlobalObject::createBlankPrototypeInheriting and js::InitClass. r=jorendorff
    UltraBlame original commit: bedd95bcb7987f1f3484ad5e8d0d6b09b9a316b1

--- js/src/vm/AsyncIteration.cpp
+++ js/src/vm/AsyncIteration.cpp
@@ -528,7 +528,7 @@ GlobalObject::initAsyncGenerators(JSContext* cx, Handle<GlobalObject*> global)
 
     
     RootedObject asyncFromSyncIterProto(
-        cx, GlobalObject::createBlankPrototypeInheriting(cx, global, &PlainObject::class_,
+        cx, GlobalObject::createBlankPrototypeInheriting(cx, &PlainObject::class_,
                                                          asyncIterProto));
     if (!asyncFromSyncIterProto)
         return false;
@@ -541,7 +541,7 @@ GlobalObject::initAsyncGenerators(JSContext* cx, Handle<GlobalObject*> global)
 
     
     RootedObject asyncGenProto(
-        cx, GlobalObject::createBlankPrototypeInheriting(cx, global, &PlainObject::class_,
+        cx, GlobalObject::createBlankPrototypeInheriting(cx, &PlainObject::class_,
                                                          asyncIterProto));
     if (!asyncGenProto)
         return false;
