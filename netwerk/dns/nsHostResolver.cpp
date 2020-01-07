2b6a1a0a24cc3b1d99cb4d31ca5bf163b6da345f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 00:05:20 +0000

    Bug 1422173 - Use lock when changing nsHostRecord.addr_info r=mayhemer
    MozReview-Commit-ID: A6Qk6G3Y1SY
    
    UltraBlame original commit: 253690f0ab21b7f963cf2aaef144279d4318795d

--- netwerk/dns/nsHostResolver.cpp
+++ netwerk/dns/nsHostResolver.cpp
@@ -866,6 +866,10 @@ nsHostResolver::ResolveHost(const char             *host,
                              LOG_HOST(host, netInterface),
                              (af == PR_AF_INET) ? "AF_INET" : "AF_INET6"));
 
+                        
+                        
+                        MutexAutoLock lock(he->rec->addr_info_lock);
+
                         he->rec->addr_info = nullptr;
                         if (unspecHe->rec->negative) {
                             he->rec->negative = unspecHe->rec->negative;
