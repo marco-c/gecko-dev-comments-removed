2c55181401c3197d895b436c502adaa9f9978fc7
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 18:15:35 +0000

    Backed out 2 changesets (bug 1322845) for failing reftest background-repeat-large-area.html == background-repeat-large-area-ref.html on a CLOSED TREE
    Backed out changeset 4fd46cd822bf (bug 1322845)
    Backed out changeset abd41b3e63e7 (bug 1322845)
    
    UltraBlame original commit: 7b85106020e91b909b1106632423252a977383e6

--- taskcluster/taskgraph/transforms/tests.py
+++ taskcluster/taskgraph/transforms/tests.py
@@ -542,18 +542,8 @@ def set_treeherder_machine_platform(config, tests):
         'android-api-16-gradle/opt': 'android-api-16-gradle/opt',
     }
     for test in tests:
-        
-        
-        
-        
-        
-        
-        
-        if '-qr' in test['test-platform']:
-            test['treeherder-machine-platform'] = test['test-platform']
-        else:
-            test['treeherder-machine-platform'] = translation.get(
-                test['build-platform'], test['test-platform'])
+        test['treeherder-machine-platform'] = translation.get(
+            test['build-platform'], test['test-platform'])
         yield test
 
 
