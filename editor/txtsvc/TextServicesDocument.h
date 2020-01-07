b0a265936f07a660817db978a41251bc0d246d53
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 03:28:42 +0000

    Bug 1430982 - part 2: Rename nsTextServicesDocument to mozilla::TextServicesDocument and expose its header r=m_kato
    For making nsTextServicesDocument accessible from anywhere directly, we need to
    expose its header.  Then, it should be in mozilla namespace.
    
    This patch renames nsTextServicesDocument to mozilla::TextServicesDocument
    and expose the header file as "mozilla/TextServicesDocument.h".
    
    MozReview-Commit-ID: 9PmP73PXSJu
    
    UltraBlame original commit: 1e9838519ff198f4bd48ee4f44082226b4880561

---
+++ editor/txtsvc/TextServicesDocument.h
@@ -0,0 +1,186 @@
+
+
+
+
+
+#ifndef mozilla_TextServicesDocument_h
+#define mozilla_TextServicesDocument_h
+
+#include "nsCOMPtr.h"
+#include "nsCycleCollectionParticipant.h"
+#include "nsIEditActionListener.h"
+#include "nsISupportsImpl.h"
+#include "nsITextServicesDocument.h"
+#include "nsStringFwd.h"
+#include "nsTArray.h"
+#include "nscore.h"
+
+class nsIContent;
+class nsIContentIterator;
+class nsIDOMCharacterData;
+class nsIDOMDocument;
+class nsIDOMNode;
+class nsIEditor;
+class nsINode;
+class nsISelection;
+class nsISelectionController;
+class nsITextServicesFilter;
+
+
+#define NS_TEXTSERVICESDOCUMENT_CID \
+  { 0x019718e3, 0xcdb5, 0x11d2, \
+    { 0x8d, 0x3c, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0 } }
+
+namespace mozilla {
+
+class OffsetEntry;
+class TextEditor;
+
+
+
+
+class TextServicesDocument final : public nsITextServicesDocument
+                                 , public nsIEditActionListener
+{
+private:
+  enum class IteratorStatus : uint8_t
+  {
+    
+    eDone = 0,
+    
+    eValid,
+    
+    ePrev,
+    
+    eNext,
+  };
+
+  nsCOMPtr<nsIDOMDocument> mDOMDocument;
+  nsCOMPtr<nsISelectionController> mSelCon;
+  RefPtr<TextEditor> mTextEditor;
+  nsCOMPtr<nsIContentIterator> mIterator;
+  nsCOMPtr<nsIContent> mPrevTextBlock;
+  nsCOMPtr<nsIContent> mNextTextBlock;
+  nsTArray<OffsetEntry*> mOffsetTable;
+  RefPtr<nsRange> mExtent;
+  nsCOMPtr<nsITextServicesFilter> mTxtSvcFilter;
+
+  int32_t mSelStartIndex;
+  int32_t mSelStartOffset;
+  int32_t mSelEndIndex;
+  int32_t mSelEndOffset;
+
+  IteratorStatus mIteratorStatus;
+
+protected:
+  virtual ~TextServicesDocument();
+
+public:
+  TextServicesDocument();
+
+  NS_DECL_CYCLE_COLLECTING_ISUPPORTS
+  NS_DECL_CYCLE_COLLECTION_CLASS_AMBIGUOUS(TextServicesDocument,
+                                           nsITextServicesDocument)
+
+  
+  NS_IMETHOD InitWithEditor(nsIEditor* aEditor) override;
+  NS_IMETHOD GetDocument(nsIDOMDocument** aDoc) override;
+  NS_IMETHOD SetExtent(nsRange* aRange) override;
+  NS_IMETHOD ExpandRangeToWordBoundaries(nsRange* aRange) override;
+  NS_IMETHOD SetFilter(nsITextServicesFilter* aFilter) override;
+  NS_IMETHOD GetCurrentTextBlock(nsString* aStr) override;
+  NS_IMETHOD FirstBlock() override;
+  NS_IMETHOD LastSelectedBlock(TSDBlockSelectionStatus* aSelStatus,
+                               int32_t* aSelOffset,
+                               int32_t* aSelLength) override;
+  NS_IMETHOD PrevBlock() override;
+  NS_IMETHOD NextBlock() override;
+  NS_IMETHOD IsDone(bool* aIsDone) override;
+  NS_IMETHOD SetSelection(int32_t aOffset, int32_t aLength) override;
+  NS_IMETHOD ScrollSelectionIntoView() override;
+  NS_IMETHOD DeleteSelection() override;
+  NS_IMETHOD InsertText(const nsString* aText) override;
+
+  
+  NS_DECL_NSIEDITACTIONLISTENER
+
+  static nsresult GetRangeEndPoints(nsRange* aRange,
+                                    nsINode** aStartContainer,
+                                    int32_t* aStartOffset,
+                                    nsINode** aEndContainer,
+                                    int32_t* aEndOffset);
+
+private:
+  nsresult CreateContentIterator(nsRange* aRange,
+                                 nsIContentIterator** aIterator);
+
+  already_AddRefed<nsINode> GetDocumentContentRootNode();
+  already_AddRefed<nsRange> CreateDocumentContentRange();
+  already_AddRefed<nsRange> CreateDocumentContentRootToNodeOffsetRange(
+                              nsINode* aParent,
+                              uint32_t aOffset,
+                              bool aToStart);
+  nsresult CreateDocumentContentIterator(nsIContentIterator** aIterator);
+
+  nsresult AdjustContentIterator();
+
+  static nsresult FirstTextNode(nsIContentIterator* aIterator,
+                                IteratorStatus* aIteratorStatus);
+  static nsresult LastTextNode(nsIContentIterator* aIterator,
+                               IteratorStatus* aIteratorStatus);
+
+  static nsresult FirstTextNodeInCurrentBlock(nsIContentIterator* aIterator);
+  static nsresult FirstTextNodeInPrevBlock(nsIContentIterator* aIterator);
+  static nsresult FirstTextNodeInNextBlock(nsIContentIterator* aIterator);
+
+  nsresult GetFirstTextNodeInPrevBlock(nsIContent** aContent);
+  nsresult GetFirstTextNodeInNextBlock(nsIContent** aContent);
+
+  static bool IsBlockNode(nsIContent* aContent);
+  static bool IsTextNode(nsIContent* aContent);
+  static bool IsTextNode(nsIDOMNode* aNode);
+
+  static bool DidSkip(nsIContentIterator* aFilteredIter);
+  static void ClearDidSkip(nsIContentIterator* aFilteredIter);
+
+  static bool HasSameBlockNodeParent(nsIContent* aContent1,
+                                     nsIContent* aContent2);
+
+  nsresult SetSelectionInternal(int32_t aOffset, int32_t aLength,
+                                bool aDoUpdate);
+  nsresult GetSelection(TSDBlockSelectionStatus* aSelStatus,
+                        int32_t* aSelOffset, int32_t* aSelLength);
+  nsresult GetCollapsedSelection(TSDBlockSelectionStatus* aSelStatus,
+                                 int32_t* aSelOffset, int32_t* aSelLength);
+  nsresult GetUncollapsedSelection(TSDBlockSelectionStatus* aSelStatus,
+                                   int32_t* aSelOffset, int32_t* aSelLength);
+
+  bool SelectionIsCollapsed();
+  bool SelectionIsValid();
+
+  static nsresult CreateOffsetTable(nsTArray<OffsetEntry*>* aOffsetTable,
+                                    nsIContentIterator* aIterator,
+                                    IteratorStatus* aIteratorStatus,
+                                    nsRange* aIterRange, nsString* aStr);
+  static nsresult ClearOffsetTable(nsTArray<OffsetEntry*>* aOffsetTable);
+
+  static nsresult NodeHasOffsetEntry(nsTArray<OffsetEntry*>* aOffsetTable,
+                                     nsINode* aNode,
+                                     bool* aHasEntry,
+                                     int32_t* aEntryIndex);
+
+  nsresult RemoveInvalidOffsetEntries();
+  nsresult SplitOffsetEntry(int32_t aTableIndex, int32_t aOffsetIntoEntry);
+
+  static nsresult FindWordBounds(nsTArray<OffsetEntry*>* aOffsetTable,
+                                 nsString* aBlockStr,
+                                 nsINode* aNode, int32_t aNodeOffset,
+                                 nsINode** aWordStartNode,
+                                 int32_t* aWordStartOffset,
+                                 nsINode** aWordEndNode,
+                                 int32_t* aWordEndOffset);
+};
+
+} 
+
+#endif 
