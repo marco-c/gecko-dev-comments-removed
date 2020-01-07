1225892726793d273ce05c79aa0e040aef9d91f6
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 12:02:50 +0000

    Bug 1445272: Implement text-to-binary support for anyref; r=luke
    UltraBlame original commit: 670d462e97cbd93bf7856ee6793ecd530394c928

--- js/src/wasm/WasmAST.h
+++ js/src/wasm/WasmAST.h
@@ -209,6 +209,7 @@ enum class AstExprKind
     Load,
     Nop,
     Pop,
+    RefNull,
     Return,
     SetGlobal,
     SetLocal,
@@ -1162,6 +1163,19 @@ class AstExtraConversionOperator final : public AstExpr
     AstExpr* operand() const { return operand_; }
 };
 #endif
+
+class AstRefNull final : public AstExpr
+{
+    ValType refType_;
+  public:
+    static const AstExprKind Kind = AstExprKind::RefNull;
+    explicit AstRefNull(ValType refType)
+      : AstExpr(Kind, ExprType::Limit), refType_(refType)
+    {}
+    ValType refType() const {
+        return refType_;
+    }
+};
 
 
 
