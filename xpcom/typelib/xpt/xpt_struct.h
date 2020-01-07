d8635ce783e56776871f3a5356cd5561ebf589a1
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 10:45:26 +0000

    Bug 1438688, part 6 - Compile XPT information to C++ at build time. r=glandium,njn
    This patch handles the actual generation of the static data structures
    used to represent XPT information. XPT files are generated in the same
    way as they are now, but they are used only as an intermediate
    representation to speed up incremental compilation rather than
    something used by Firefox itself. Instead of linking XPTs into a
    single big XPT file at packaging time, they are linked into a single
    big C++ file at build time, that defines the various static consts in
    XPTHeader.
    
    In xpt.py, every data structure that can get written to disk gets an
    additional code_gen() method that returns a representation of that
    data structure as C++ source code. CodeGenData aggregates this
    information together, handling deduplication and the final source code
    generation.
    
    The ctors are needed for XPTConstValue to statically initialize the
    different union cases without resorting to designated initializers,
    which are part of C99, not C++. Designated initializers appear to be
    supported in C++ code by Clang and GCC, but not MSVC. The ctors must
    be constexpr to ensure they are actually statically initialized so
    they can be shared between Firefox processes.
    
    I also removed an unnecessary "union" in XPTConstDescriptor.
    
    Together, these patches reduce the amount of memory reported by
    xpti-working-set from about 860,000 bytes to about 200,000 bytes. The
    remaining memory is used for xptiInterface and xptiTypelibGuts (which
    are thin wrappers around the XPT interfaces and header) and hash
    tables to speed up looking up interfaces by name or IID. That could
    potentially be eliminated from dynamic allocations in follow up
    work. These patches did not affect memory reporting because XPT arenas
    are still used by the remaining XPTI data structures.
    
    MozReview-Commit-ID: Jvi9ByCPa6H
    
    UltraBlame original commit: 8786eabb61a41e1b83b69bf8f1f30d135184c404

--- xpcom/typelib/xpt/xpt_struct.h
+++ xpcom/typelib/xpt/xpt_struct.h
@@ -178,6 +178,15 @@ union XPTConstValue {
   uint16_t ui16;
   int32_t i32;
   uint32_t ui32;
+
+  
+  
+  
+  
+  explicit constexpr XPTConstValue(int16_t aInt) : i16(aInt) {}
+  explicit constexpr XPTConstValue(uint16_t aInt) : ui16(aInt) {}
+  explicit constexpr XPTConstValue(int32_t aInt) : i32(aInt) {}
+  explicit constexpr XPTConstValue(uint32_t aInt) : ui32(aInt) {}
 };
 
 struct XPTConstDescriptor {
@@ -187,7 +196,7 @@ struct XPTConstDescriptor {
 
   uint32_t mName; 
   XPTTypeDescriptor mType;
-  union XPTConstValue mValue;
+  XPTConstValue mValue;
 };
 
 
