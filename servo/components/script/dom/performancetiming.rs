7ea83cc5f0c3dc3152aebf6866d11da59abe01ac
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 01:08:38 +0000

    servo: Merge #19569 - Add a topLevelDomComplete metric (from asajeffrey:script-perf-measure-topLevelDomComplete); r=jdm
    <!-- Please describe your changes on the following line: -->
    
    Measure DOM completion without iframes. This is in support of measuring when the main document is loaded, ignoring secondary content, e.g. ads.
    
    ---
    <!-- Thank you for contributing to Servo! Please replace each `[ ]` by `[X]` when the step is complete, and replace `__` with appropriate data: -->
    - [X] `./mach build -d` does not report any errors
    - [X] `./mach test-tidy` does not report any errors
    - [X] These changes fix #19561
    - [X] There are tests for these changes
    
    <!-- Also, please make sure that "Allow edits from maintainers" checkbox is checked, so that we can help you if you get stuck somewhere along the way.-->
    
    <!-- Pull requests that do not address these steps are welcome, but they will require additional verification as part of the review process. -->
    
    Source-Repo: https://github.com/servo/servo
    Source-Revision: d832e1b8f843612b4cb124d8c4eb9dce94e92d50
    
    UltraBlame original commit: 95fbb28d7e72d97e9cfd9ef009a461d6853a340e

--- servo/components/script/dom/performancetiming.rs
+++ servo/components/script/dom/performancetiming.rs
@@ -86,6 +86,12 @@ impl PerformanceTimingMethods for PerformanceTiming {
     fn LoadEventEnd(&self) -> u64 {
         self.document.get_load_event_end()
     }
+
+    
+    
+    fn TopLevelDomComplete(&self) -> u64 {
+        self.document.get_top_level_dom_complete()
+    }
 }
 
 
