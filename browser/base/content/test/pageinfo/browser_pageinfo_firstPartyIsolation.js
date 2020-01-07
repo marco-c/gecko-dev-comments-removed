08f0388b205774d82e8b9b988c373982d8e92870
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 02:10:41 +0000

    Bug 1403365 - fixed intermittent in browser_pageinfo_firstPartyIsolation.js r=baku
    When we try to load https://example.com/browser/browser/base/content/test/pageinfo/image.html,
    sometimes BrowserTestUtils will return early even the page doesn't load
    yet, for example, the browser.currentURI is still about:blank.
    And loading about:blank will cause error in pageInfo.js, hence caused
    the timeout.
    
    So I add more parameters to browserLoaded to make sure browwserLoaded is
    returned with the correct url.
    
    UltraBlame original commit: cb4fd975b8bb668aa5b877386b8cf02f62561107

--- browser/base/content/test/pageinfo/browser_pageinfo_firstPartyIsolation.js
+++ browser/base/content/test/pageinfo/browser_pageinfo_firstPartyIsolation.js
@@ -71,16 +71,15 @@ async function test() {
     Services.prefs.clearUserPref("privacy.firstparty.isolate");
   });
 
+  let url = "https://example.com/browser/browser/base/content/test/pageinfo/image.html";
   gBrowser.selectedTab = BrowserTestUtils.addTab(gBrowser);
-  gBrowser.selectedBrowser.loadURI("https://example.com/browser/browser/base/content/test/pageinfo/image.html");
-  await BrowserTestUtils.browserLoaded(gBrowser.selectedBrowser);
-
-  let spec = gBrowser.selectedBrowser.currentURI.spec;
+  gBrowser.selectedBrowser.loadURI(url);
+  await BrowserTestUtils.browserLoaded(gBrowser.selectedBrowser, false, url);
 
   
   
   
-  let pageInfo = BrowserPageInfo(spec, "mediaTab", {});
+  let pageInfo = BrowserPageInfo(url, "mediaTab", {});
   info("waitForEvent pageInfo");
   await waitForEvent(pageInfo, "load");
 
