a3ca5b0dbb27909caefa6b7e2a7ab53d8772bc5a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 18:45:54 +0000

    Bug 1461373 - Set BRNameMatchingPolicy to "Enforce" for Nightly r=keeler
    Summary:
    Change the security.pki.name_matching_mode pref to 3 for Enforce on Nightly.
    
    BR_9_2_1_SUBJECT_ALT_NAMES show that ~99.98% of encountered certificates have
    an acceptable SAN, so our compatibility risk is about 0.02%.
    
    BR_9_2_2_SUBJECT_COMMON_NAME also shows, 99.89% of certificate common names are
    present in a subject alternative name extension, giving a worst-case of 0.11%
    risk, though BR_9_2_1_SUBJECT_ALT_NAMES is more what we're affecting here.
    
    Test Plan: none
    
    Reviewers: keeler
    
    Tags: #secure-revision
    
    Bug #: 1461373
    
    Differential Revision: https://phabricator.services.mozilla.com/D1277
    
    UltraBlame original commit: 3b78d613f29dc16ed620ef9cb71eb9d87c11bb95

--- security/manager/ssl/security-prefs.js
+++ security/manager/ssl/security-prefs.js
@@ -92,7 +92,7 @@ pref("security.signed_app_signatures.policy", 2);
 #ifdef RELEASE_OR_BETA
 pref("security.pki.name_matching_mode", 1);
 #else
-pref("security.pki.name_matching_mode", 2);
+pref("security.pki.name_matching_mode", 3);
 #endif
 
 
