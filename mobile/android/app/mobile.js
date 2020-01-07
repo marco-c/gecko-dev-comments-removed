1678424f51d2bf33afd0b91a41db7830790e066e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 01:09:23 +0000

    Bug 1422263 - turn on fullscreen video orientation lock for all channels. r=jwwang
    MozReview-Commit-ID: EGCx0AdH4Yy
    
    UltraBlame original commit: b12a23323c29d955ae2d421a20413815f8b9c5c8

--- mobile/android/app/mobile.js
+++ mobile/android/app/mobile.js
@@ -904,6 +904,4 @@ pref("javascript.options.native_regexp", false);
 
 pref("media.navigator.permission.device", true);
 
-#ifdef NIGHTLY_BUILD
 pref("media.videocontrols.lock-video-orientation", true);
-#endif
