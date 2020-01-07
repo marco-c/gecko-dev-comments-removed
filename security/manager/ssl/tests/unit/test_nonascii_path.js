6aae4cecb8851977697bfc8f20d761b75f95e70d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 04:11:54 +0000

    Bug 1433903 - Skip parts of test_nonascii_path.js for Thunderbird. r=emk
    UltraBlame original commit: b76a1f5e05dc5978f8905631c3aa3720caa73fff

--- security/manager/ssl/tests/unit/test_nonascii_path.js
+++ security/manager/ssl/tests/unit/test_nonascii_path.js
@@ -19,7 +19,9 @@ env.set("XPCSHELL_TEST_PROFILE_DIR", file.path);
 
 file = do_get_profile(); 
 Assert.ok(/[^\x20-\x7f]/.test(file.path), "the profile path should contain a non-ASCII character");
-if (mozinfo.os == "win") {
+
+
+if (mozinfo.os == "win" && AppConstants.MOZ_APP_NAME != "thunderbird") {
   file.QueryInterface(Components.interfaces.nsILocalFileWin);
   Assert.ok(/[^\x20-\x7f]/.test(file.canonicalPath), "the profile short path should contain a non-ASCII character");
 }
