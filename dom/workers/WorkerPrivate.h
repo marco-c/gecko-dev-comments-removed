9d8c8d08cfe5cd34d8026ee18e1dd9ab199e7658
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 05:06:43 +0000

    Bug 1435263 - Get rid of WorkerPrivateParent template - part 13 - Start/Close/Kill/Terminate, r=bkelly
    UltraBlame original commit: 20b7fc423f7d7cded7bf9e51a79767d9600d82db

--- dom/workers/WorkerPrivate.h
+++ dom/workers/WorkerPrivate.h
@@ -143,15 +143,6 @@ private:
     return static_cast<Derived*>(const_cast<WorkerPrivateParent*>(this));
   }
 
-  bool
-  NotifyPrivate(WorkerStatus aStatus);
-
-  bool
-  TerminatePrivate()
-  {
-    return NotifyPrivate(Terminating);
-  }
-
 public:
   NS_INLINE_DECL_REFCOUNTING(WorkerPrivateParent)
 
@@ -166,39 +157,6 @@ public:
 
   already_AddRefed<WorkerRunnable>
   MaybeWrapAsWorkerRunnable(already_AddRefed<nsIRunnable> aRunnable);
-
-  
-  bool
-  Start();
-
-  
-  bool
-  Notify(WorkerStatus aStatus)
-  {
-    return NotifyPrivate(aStatus);
-  }
-
-  bool
-  Cancel()
-  {
-    return Notify(Canceling);
-  }
-
-  bool
-  Kill()
-  {
-    return Notify(Killing);
-  }
-
-  bool
-  Terminate()
-  {
-    AssertIsOnParentThread();
-    return TerminatePrivate();
-  }
-
-  bool
-  Close();
 
   bool
   ProxyReleaseMainThreadObjects();
@@ -470,6 +428,39 @@ public:
     mParentEventTargetRef = nullptr;
     mSelfRef = nullptr;
   }
+
+  
+  bool
+  Start();
+
+  
+  bool
+  Notify(WorkerStatus aStatus)
+  {
+    return NotifyPrivate(aStatus);
+  }
+
+  bool
+  Cancel()
+  {
+    return Notify(Canceling);
+  }
+
+  bool
+  Kill()
+  {
+    return Notify(Killing);
+  }
+
+  bool
+  Terminate()
+  {
+    AssertIsOnParentThread();
+    return TerminatePrivate();
+  }
+
+  bool
+  Close();
 
   
   
@@ -1401,6 +1392,15 @@ private:
   nsresult
   DispatchPrivate(already_AddRefed<WorkerRunnable> aRunnable,
                   nsIEventTarget* aSyncLoopTarget);
+
+  bool
+  NotifyPrivate(WorkerStatus aStatus);
+
+  bool
+  TerminatePrivate()
+  {
+    return NotifyPrivate(Terminating);
+  }
 
   bool
   MayContinueRunning()
