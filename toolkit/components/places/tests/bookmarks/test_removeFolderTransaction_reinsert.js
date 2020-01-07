c6fbc17d12b777792c0bb5e1f8025a4d84eda3e9
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 06:28:54 +0000

    Bug 656936 - Remove Bookmarks::GetRemoveFolderTransaction. r=standard8
    MozReview-Commit-ID: DLyjzC7ODyB
    
    UltraBlame original commit: 96d20e4d0a56c1ff6770862eeeeeb5130a41bd89

--- toolkit/components/places/tests/bookmarks/test_removeFolderTransaction_reinsert.js
+++ toolkit/components/places/tests/bookmarks/test_removeFolderTransaction_reinsert.js
@@ -3,25 +3,24 @@
 
 
 
+
+
 add_task(async function test_removeFolderTransaction_reinsert() {
   let folder = await PlacesUtils.bookmarks.insert({
     type: PlacesUtils.bookmarks.TYPE_FOLDER,
     parentGuid: PlacesUtils.bookmarks.menuGuid,
     title: "Test folder",
   });
-  let folderId = await PlacesUtils.promiseItemId(folder.guid);
   let fx = await PlacesUtils.bookmarks.insert({
     parentGuid: folder.guid,
     title: "Get Firefox!",
     url: "http://getfirefox.com",
   });
-  let fxId = await PlacesUtils.promiseItemId(fx.guid);
   let tb = await PlacesUtils.bookmarks.insert({
     parentGuid: folder.guid,
     title: "Get Thunderbird!",
     url: "http://getthunderbird.com",
   });
-  let tbId = await PlacesUtils.promiseItemId(tb.guid);
 
   let notifications = [];
   function checkNotifications(expected, message) {
@@ -30,6 +29,7 @@ add_task(async function test_removeFolderTransaction_reinsert() {
   }
 
   let observer = {
+    __proto__: NavBookmarkObserver.prototype,
     onItemAdded(itemId, parentId, index, type, uri, title, dateAdded, guid,
                 parentGuid) {
       notifications.push(["onItemAdded", itemId, parentId, guid, parentGuid]);
@@ -43,10 +43,14 @@ add_task(async function test_removeFolderTransaction_reinsert() {
     PlacesUtils.bookmarks.removeObserver(observer);
   });
 
-  let transaction = PlacesUtils.bookmarks.getRemoveFolderTransaction(folderId);
-  deepEqual(notifications, [], "We haven't executed the transaction yet");
+  let transaction = PlacesTransactions.Remove({guid: folder.guid});
 
-  transaction.doTransaction();
+  let folderId = await PlacesUtils.promiseItemId(folder.guid);
+  let fxId = await PlacesUtils.promiseItemId(fx.guid);
+  let tbId = await PlacesUtils.promiseItemId(tb.guid);
+
+  await transaction.transact();
+
   checkNotifications([
     ["onItemRemoved", tbId, folderId, tb.guid, folder.guid],
     ["onItemRemoved", fxId, folderId, fx.guid, folder.guid],
@@ -54,17 +58,25 @@ add_task(async function test_removeFolderTransaction_reinsert() {
       PlacesUtils.bookmarks.menuGuid],
   ], "Executing transaction should remove folder and its descendants");
 
-  transaction.undoTransaction();
-  
-  let newFolderGuid = await PlacesUtils.promiseItemGuid(folderId);
-  checkNotifications([
-    ["onItemAdded", folderId, PlacesUtils.bookmarksMenuFolderId, newFolderGuid,
-      PlacesUtils.bookmarks.menuGuid],
-  ], "Undo should reinsert folder with same ID and different GUID");
+  await PlacesTransactions.undo();
 
-  transaction.redoTransaction();
+  folderId = await PlacesUtils.promiseItemId(folder.guid);
+  fxId = await PlacesUtils.promiseItemId(fx.guid);
+  tbId = await PlacesUtils.promiseItemId(tb.guid);
+
   checkNotifications([
-    ["onItemRemoved", folderId, PlacesUtils.bookmarksMenuFolderId,
-      newFolderGuid, PlacesUtils.bookmarks.menuGuid],
-  ], "Redo should forward new GUID to observer");
+    ["onItemAdded", folderId, PlacesUtils.bookmarksMenuFolderId, folder.guid,
+      PlacesUtils.bookmarks.menuGuid],
+    ["onItemAdded", fxId, folderId, fx.guid, folder.guid],
+    ["onItemAdded", tbId, folderId, tb.guid, folder.guid],
+  ], "Undo should reinsert folder with different id but same GUID");
+
+  await PlacesTransactions.redo();
+
+  checkNotifications([
+    ["onItemRemoved", tbId, folderId, tb.guid, folder.guid],
+    ["onItemRemoved", fxId, folderId, fx.guid, folder.guid],
+    ["onItemRemoved", folderId, PlacesUtils.bookmarksMenuFolderId, folder.guid,
+      PlacesUtils.bookmarks.menuGuid],
+  ], "Redo should pass the GUID to observer");
 });
