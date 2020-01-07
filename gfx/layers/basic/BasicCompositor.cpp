df9b554a15936e9024bf11637079e1531ebf7b47
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 11:31:21 +0000

    Bug 1449641 - Remove extraneous "if (mRenderTarget->mDrawTarget != mDrawTarget)". r=sotaro
    The same condition is checked just before this if, so it's always true.
    
    MozReview-Commit-ID: 9Vscnkz7AoY
    
    UltraBlame original commit: ac980a443965c8ab42fbe370f2300b8f09c43c8e

--- gfx/layers/basic/BasicCompositor.cpp
+++ gfx/layers/basic/BasicCompositor.cpp
@@ -1038,12 +1038,7 @@ BasicCompositor::TryToEndRemoteDrawing(bool aForceToEnd)
   if (mRenderTarget->mDrawTarget != mDrawTarget) {
     
     
-    RefPtr<SourceSurface> source;
-    if (mRenderTarget->mDrawTarget != mDrawTarget) {
-      source = mWidget->EndBackBufferDrawing();
-    } else {
-      source = mRenderTarget->mDrawTarget->Snapshot();
-    }
+    RefPtr<SourceSurface> source = mWidget->EndBackBufferDrawing();
     RefPtr<DrawTarget> dest(mTarget ? mTarget : mDrawTarget);
 
     nsIntPoint offset = mTarget ? mTargetBounds.TopLeft() : nsIntPoint();
