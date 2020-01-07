5e3a405c0057f6110f815351dc03164b1d66f2a7
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 03:30:22 +0000

    Bug 1431738 - We will disable TFO on late beta and release. r=jduell
    UltraBlame original commit: bf09b4d00654009d098cbb749a756789dde59dfb

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -4825,11 +4825,14 @@ pref("network.tcp.keepalive.retry_interval", 1);
 pref("network.tcp.keepalive.probe_count", 4);
 #endif
 
-#if defined(XP_WIN) || defined(XP_MACOSX)
+#if !defined(EARLY_BETA_OR_EARLIER)
+pref("network.tcp.tcp_fastopen_enable", false);
+#elif  defined(XP_WIN) || defined(XP_MACOSX)
 pref("network.tcp.tcp_fastopen_enable", true);
 #else
 pref("network.tcp.tcp_fastopen_enable", false);
 #endif
+
 pref("network.tcp.tcp_fastopen_consecutive_failure_limit", 5);
 
 
