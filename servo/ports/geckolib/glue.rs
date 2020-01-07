7d1f4152c4ac6ba559c516b98bf58d07687188fd
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 06:11:32 +0000

    servo: Merge #20081 - style: More serialization tweaks (from emilio:more-longhand-stuff); r=nox
    This still doesn't fix everything. In particular, we need to check whether the
    subproperty will be enabled in Longhands and LonghandsToSerialize too.
    
    I haven't decided yet on what's the best way to do that.
    
    Source-Repo: https://github.com/servo/servo
    Source-Revision: 2c060eb81a8eab0fdcbf13231bf7703ea96bc657
    
    UltraBlame original commit: 01328de0be21dfeeef394b2b0f048fc8d6994e6c

--- servo/ports/geckolib/glue.rs
+++ servo/ports/geckolib/glue.rs
@@ -3797,24 +3797,23 @@ struct PropertyAndIndex {
 }
 
 struct PrioritizedPropertyIter<'a> {
-    properties: &'a nsTArray<PropertyValuePair>,
+    properties: &'a [PropertyValuePair],
     sorted_property_indices: Vec<PropertyAndIndex>,
     curr: usize,
 }
 
 impl<'a> PrioritizedPropertyIter<'a> {
-    pub fn new(properties: &'a nsTArray<PropertyValuePair>) -> PrioritizedPropertyIter {
+    fn new(properties: &'a [PropertyValuePair]) -> PrioritizedPropertyIter {
         
         
-        let all = PropertyId::Shorthand(ShorthandId::All);
-
+        
         let mut sorted_property_indices: Vec<PropertyAndIndex> =
             properties.iter().enumerate().map(|(index, pair)| {
-                PropertyAndIndex {
-                    property: PropertyId::from_nscsspropertyid(pair.mProperty)
-                              .unwrap_or(all.clone()),
-                    index,
-                }
+                let property =
+                    PropertyId::from_nscsspropertyid(pair.mProperty)
+                        .unwrap_or(PropertyId::Shorthand(ShorthandId::All));
+
+                PropertyAndIndex { property, index }
             }).collect();
         sorted_property_indices.sort_by(|a, b| compare_property_priority(&a.property, &b.property));
 
