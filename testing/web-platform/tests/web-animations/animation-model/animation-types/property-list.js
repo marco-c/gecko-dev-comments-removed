63e6f61032b243c69950daf6dbcad029e70a4670
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 21:41:01 +0000

    Bug 1465815 [wpt PR 11276] - [web-animations] Fix timeout/0 tests in unsupporting browsers, a=testonly
    Automatic update from web-platform-tests[web-animations] Fix timeout/0 tests in unsupporting browsers
    
    Part of #11269.
    
    --
    
    wpt-commits: ee6c9d315b05a2afe40a13cbf26d3126d52c34a0
    wpt-pr: 11276
    
    UltraBlame original commit: d313a947c2056a9bf148032377b02f8b31641f87

--- testing/web-platform/tests/web-animations/animation-model/animation-types/property-list.js
+++ testing/web-platform/tests/web-animations/animation-model/animation-types/property-list.js
@@ -1556,6 +1556,7 @@ function createTestElement(t, setup) {
 
 function isSupported(property) {
   const testKeyframe = new TestKeyframe(propertyToIDL(property));
+  assert_not_equals(window.KeyframeEffect, undefined, 'window.KeyframeEffect');
   try {
     
     
