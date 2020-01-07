90ea9bd401c04a6c2bfd3960dc5cdcb40364c8bb
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 18:48:10 +0000

    Bug 1453591 - Add gtest coverage for the persistence logic. r=chutten,froydnj,janerik
    This changes the build system to add a new define when on
    Android or when tests are enabled, MOZ_TELEMETRY_GECKOVIEW.
    
    MozReview-Commit-ID: 5n2A8G2ZzRK
    
    UltraBlame original commit: 2fa8aedae0ffbed270af75dd62489bc33f589b0f

--- toolkit/components/telemetry/tests/gtest/TelemetryFixture.h
+++ toolkit/components/telemetry/tests/gtest/TelemetryFixture.h
@@ -5,11 +5,10 @@
 #ifndef TelemetryFixture_h_
 #define TelemetryFixture_h_
 
+#include "gtest/gtest.h"
+#include "nsITelemetry.h"
 #include "mozilla/CycleCollectedJSContext.h"
 #include "mozilla/dom/ScriptSettings.h"
-#include "mozilla/dom/SimpleGlobalObject.h"
-
-using namespace mozilla;
 
 class TelemetryTestFixture: public ::testing::Test {
 protected:
@@ -20,19 +19,6 @@ protected:
 
   nsCOMPtr<nsITelemetry> mTelemetry;
 };
-
-void
-TelemetryTestFixture::SetUp()
-{
-  mTelemetry = do_GetService("@mozilla.org/base/telemetry;1");
-
-  mCleanGlobal =
-    dom::SimpleGlobalObject::Create(dom::SimpleGlobalObject::GlobalType::BindingDetail);
-
-  
-  ASSERT_NE(mCleanGlobal, nullptr) << "SimpleGlobalObject must return a valid global object.";
-}
-
 
 
 
@@ -46,20 +32,8 @@ public:
   JSContext* GetJSContext() const;
 
 protected:
-  dom::AutoJSAPI mJsAPI;
+  mozilla::dom::AutoJSAPI mJsAPI;
   JSContext* mCx;
 };
-
-AutoJSContextWithGlobal::AutoJSContextWithGlobal(JSObject* aGlobalObject)
-  : mCx(nullptr)
-{
-  
-  MOZ_ALWAYS_TRUE(mJsAPI.Init(aGlobalObject));
-}
-
-JSContext* AutoJSContextWithGlobal::GetJSContext() const
-{
-  return mJsAPI.cx();
-}
 
 #endif 
