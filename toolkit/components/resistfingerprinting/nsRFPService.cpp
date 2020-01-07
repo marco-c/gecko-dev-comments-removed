fd40b1d5da3a5dfe8bbfe5bba3f43d2cdde0411b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 17:19:24 +0000

    Bug 1418162 Use a build constant to determine update channel, and update ESR equation for 60 r=froydnj,Gijs
    Previously we used Services.appinfo.defaultUpdateChannel to determine if we were on ESR
    and if so, we would assert to make sure the ESR equation hadn't changed. But that method
    is a footgun.  We could use UpdateUtils.getUpdateChannel, but for our purposes here, the
    compile-time constant will work just as well.
    
    If it's set correctly, for Mozilla, we will perform our assert check. If it's not set to
    esr (e.g. for Tor), they'll skip the assert. They probably want to control their user agent
    themselves anyway.
    
    MozReview-Commit-ID: DLnWSEpCVJ6
    
    UltraBlame original commit: ac0b202721dcfa1e57be7e004565996d47348cfa

--- toolkit/components/resistfingerprinting/nsRFPService.cpp
+++ toolkit/components/resistfingerprinting/nsRFPService.cpp
@@ -693,23 +693,16 @@ nsRFPService::GetSpoofedUserAgent(nsACString &userAgent)
   
   
   
-  nsCOMPtr<nsIXULRuntime> runtime =
-    do_GetService("@mozilla.org/xre/runtime;1", &rv);
-  NS_ENSURE_SUCCESS(rv, rv);
-
-  nsAutoCString updateChannel;
-  rv = runtime->GetDefaultUpdateChannel(updateChannel);
-  NS_ENSURE_SUCCESS(rv, rv);
-
-  
-  
-  
-  if (updateChannel.EqualsLiteral("esr")) {
-    MOZ_ASSERT(((firefoxVersion % 7) == 3),
+  if (!strcmp(NS_STRINGIFY(MOZ_UPDATE_CHANNEL), "esr")) {
+    MOZ_ASSERT(((firefoxVersion % 7) == 4),
       "Please udpate ESR version formula in nsRFPService.cpp");
   }
 
-  uint32_t spoofedVersion = firefoxVersion - ((firefoxVersion - 3) % 7);
+  
+  
+  
+  
+  uint32_t spoofedVersion = firefoxVersion - ((firefoxVersion - 4) % 7);
   userAgent.Assign(nsPrintfCString(
     "Mozilla/5.0 (%s; rv:%d.0) Gecko/%s Firefox/%d.0",
     SPOOFED_UA_OS, spoofedVersion, LEGACY_BUILD_ID, spoofedVersion));
