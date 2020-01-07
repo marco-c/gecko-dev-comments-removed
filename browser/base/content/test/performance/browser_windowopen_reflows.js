44809668fbcb14108525a144760d97f90c1be9e8
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 01:23:55 +0000

    Bug 1394914 - toolbars no longer need to wait for xul overlays to load before initializing CustomizableUI icons (avoid sidebar icon flickering), r=Gijs.
    UltraBlame original commit: 7c96cab6821a26ab01bb9067e4ccd3791b2643dc

--- browser/base/content/test/performance/browser_windowopen_reflows.js
+++ browser/base/content/test/performance/browser_windowopen_reflows.js
@@ -12,7 +12,32 @@
 
 
 
-const EXPECTED_REFLOWS = [];
+const EXPECTED_REFLOWS = [
+  {
+    stack: [
+      "select@chrome://global/content/bindings/textbox.xml",
+      "focusAndSelectUrlBar@chrome://browser/content/browser.js",
+    ],
+  },
+];
+
+if (Services.appinfo.OS == "Linux") {
+  if (gMultiProcessBrowser) {
+    EXPECTED_REFLOWS.push({
+      stack: [
+        "handleEvent@chrome://browser/content/tabbrowser.xml",
+      ],
+    });
+  } else {
+    EXPECTED_REFLOWS.push({
+      stack: [
+        "handleEvent@chrome://browser/content/tabbrowser.xml",
+        "inferFromText@chrome://browser/content/browser.js",
+        "handleEvent@chrome://browser/content/browser.js",
+      ],
+    });
+  }
+}
 
 if (Services.appinfo.OS == "WINNT") {
   EXPECTED_REFLOWS.push(
@@ -32,18 +57,21 @@ if (Services.appinfo.OS == "WINNT" || Services.appinfo.OS == "Darwin") {
   EXPECTED_REFLOWS.push(
     {
       stack: [
-        "select@chrome://global/content/bindings/textbox.xml",
-        "focusAndSelectUrlBar@chrome://browser/content/browser.js",
-      ],
-    },
-    {
-      stack: [
         "rect@chrome://browser/content/browser-tabsintitlebar.js",
         "_update@chrome://browser/content/browser-tabsintitlebar.js",
         "init@chrome://browser/content/browser-tabsintitlebar.js",
         "handleEvent@chrome://browser/content/tabbrowser.xml",
       ],
       times: 4, 
+    },
+    {
+      stack: [
+        "onOverflow@resource:///modules/CustomizableUI.jsm",
+        "init@resource:///modules/CustomizableUI.jsm",
+        "observe@resource:///modules/CustomizableUI.jsm",
+        "_delayedStartup@chrome://browser/content/browser.js",
+      ],
+      times: 2, 
     },
   );
 }
