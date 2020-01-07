35995e88cceb7e88676977a70343fac985cfe20f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 21:46:30 +0000

    Bug 1458375 - Add a new browser-idle-startup-tasks-finished notification. r=Felipe
    This notification fires when a browser window finishes running its idle tasks. Note
    that it might never run if every window goes away before the idle tasks can run.
    
    MozReview-Commit-ID: IEH98S29jhV
    
    UltraBlame original commit: 6c43f8368b073a72d20cae6231e56a5b2da09f28

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -1161,6 +1161,7 @@ var delayedStartupPromise = new Promise(resolve => {
 
 var gBrowserInit = {
   delayedStartupFinished: false,
+  idleTasksFinished: false,
 
   _tabToAdopt: undefined,
 
@@ -1777,6 +1778,14 @@ var gBrowserInit = {
         Cu.reportError(ex);
       }
     }, {timeout: 10000});
+
+    
+    
+    
+    scheduleIdleTask(() => {
+      this.idleTasksFinished = true;
+      Services.obs.notifyObservers(window, "browser-idle-startup-tasks-finished");
+    });
   },
 
   
