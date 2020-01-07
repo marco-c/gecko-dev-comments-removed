3fae04d7dc02fdfb0ce8915f83761531a7091307
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 08:15:18 +0000

    Bug 1444139: Re-enable support for webkit-prefixed pixel-density media queries (e.g. -webkit-min-device-pixel-ratio) r=xidorn
    MozReview-Commit-ID: EqwsynLihb1
    
    UltraBlame original commit: 3187e7424f01bffbcb4264483b310ea68685c40f

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -3074,7 +3074,7 @@ pref("layout.css.prefixes.webkit", true);
 
 
 
-pref("layout.css.prefixes.device-pixel-ratio-webkit", false);
+pref("layout.css.prefixes.device-pixel-ratio-webkit", true);
 
 
 
