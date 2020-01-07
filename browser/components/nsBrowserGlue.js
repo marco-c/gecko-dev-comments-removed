56d9b76026a5a54db8c22aabd8f51f0d5061560e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 02:07:58 +0000

    Backed out 3 changesets (bug 1417473) for failing browser_HybridContentTelemetry.js. r=backout on a CLOSED TREE
    Backed out changeset 27db7bf978be (bug 1417473)
    Backed out changeset a365a5a59ad1 (bug 1417473)
    Backed out changeset 443747e39abe (bug 1417473)
    
    UltraBlame original commit: e89a072ca221a8f44a38a65ebc6298191f5aeede

--- browser/components/nsBrowserGlue.js
+++ browser/components/nsBrowserGlue.js
@@ -39,7 +39,6 @@ XPCOMUtils.defineLazyModuleGetters(this, {
   FileUtils: "resource://gre/modules/FileUtils.jsm",
   FileSource: "resource://gre/modules/L10nRegistry.jsm",
   FormValidationHandler: "resource:///modules/FormValidationHandler.jsm",
-  HybridContentTelemetry: "resource://gre/modules/HybridContentTelemetry.jsm",
   Integration: "resource://gre/modules/Integration.jsm",
   L10nRegistry: "resource://gre/modules/L10nRegistry.jsm",
   LanguagePrompt: "resource://gre/modules/LanguagePrompt.jsm",
@@ -3072,11 +3071,4 @@ this.NSGetFactory = XPCOMUtils.generateNSGetFactory(components);
 var globalMM = Cc["@mozilla.org/globalmessagemanager;1"].getService(Ci.nsIMessageListenerManager);
 globalMM.addMessageListener("UITour:onPageEvent", function(aMessage) {
   UITour.onPageEvent(aMessage, aMessage.data);
-});
-
-
-
-
-globalMM.addMessageListener("HybridContentTelemetry:onTelemetryMessage", aMessage => {
-  HybridContentTelemetry.onTelemetryMessage(aMessage, aMessage.data);
 });
