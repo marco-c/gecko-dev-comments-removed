ca47a10933336d9db56536f5dd1a7fc006ab611b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 10:17:41 +0000

    Backed out changeset dcd8ba6a9028 (bug 1429394) for build bustages. CLOSED TREE
    UltraBlame original commit: 469b253318c17722f4e543cca7580887fb0087eb

--- browser/base/content/test/static/browser_all_files_referenced.js
+++ browser/base/content/test/static/browser_all_files_referenced.js
@@ -51,6 +51,9 @@ var whitelist = [
   {file: "chrome://pippki/content/load_device.xul"},
 
   
+  {file: "chrome://global/locale/brand.dtd"},
+
+  
   
   {file: "chrome://global/locale/fallbackMenubar.properties",
    platforms: ["linux", "win"]},
