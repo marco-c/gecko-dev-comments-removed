21b8cb508ab0e4f9c16663fe6a09663910e968e5
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 12:00:55 +0000

    Bug 1448044 - kinto-storage-adapter.js chokes on large updates r=glasserc
    MozReview-Commit-ID: ncHwROrfHV
    
    UltraBlame original commit: ef75b7ebedc350f8f213d7e1298f1bef35f8bf33

--- services/common/kinto-storage-adapter.js
+++ services/common/kinto-storage-adapter.js
@@ -266,20 +266,32 @@ class FirefoxAdapter extends Kinto.adapters.BaseAdapter {
 
     return conn.executeTransaction(async function doExecuteTransaction() {
       
-      const parameters = [
-        collection,
-        ...options.preload,
-      ];
-      const placeholders = options.preload.map(_ => "?");
-      const stmt = statements.listRecordsById + "(" + placeholders.join(",") + ");";
-      const rows = await conn.execute(stmt, parameters);
 
-      const preloaded = rows.reduce((acc, row) => {
-        const record = JSON.parse(row.getResultByName("record"));
-        acc[row.getResultByName("record_id")] = record;
-        return acc;
-      }, {});
+      
+      
+      const limit = 100;
+      let preloaded = {};
+      let preload;
+      let more = options.preload;
 
+      while (more.length > 0) {
+        preload = more.slice(0, limit);
+        more = more.slice(limit, more.length);
+
+        const parameters = [
+          collection,
+          ...preload,
+        ];
+        const placeholders = preload.map(_ => "?");
+        const stmt = statements.listRecordsById + "(" + placeholders.join(",") + ");";
+        const rows = await conn.execute(stmt, parameters);
+
+        rows.reduce((acc, row) => {
+          const record = JSON.parse(row.getResultByName("record"));
+          acc[row.getResultByName("record_id")] = record;
+          return acc;
+        }, preloaded);
+      }
       const proxy = transactionProxy(collection, preloaded);
       result = callback(proxy);
 
