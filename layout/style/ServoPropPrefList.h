140399d4e076725c929aa933816e4d4b3492a5c1
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 09:50:11 +0000

    Bug 1449039 part 12 - Keep only necessary params for CSS_PROP definitions. r=emilio
    MozReview-Commit-ID: 6LG9WjXemno
    
    UltraBlame original commit: c6e9dfb1a65264018061295b78b8860f28ae6442

--- layout/style/ServoPropPrefList.h
+++ layout/style/ServoPropPrefList.h
@@ -1,16 +1,15 @@
-/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
-/* vim: set ts=8 sts=2 et sw=2 tw=80: */
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+
+
+
+
 
 #ifndef mozilla_ServoPropPrefList_h
 #define mozilla_ServoPropPrefList_h
 
 namespace mozilla {
 
-#define CSS_PROP(name_, id_, method_, flags_, pref_, parsevariant_, kwtable_, \
-                 stylestruct_, stylestructoffset_, animtype_)                 \
+#define CSS_PROP(name_, id_, method_, flags_, pref_, ...) \
     const bool SERVO_PREF_ENABLED_##id_ = !(sizeof(pref_) == 1);
 #define CSS_PROP_SHORTHAND(name_, id_, method_, flags_, pref_)  \
     const bool SERVO_PREF_ENABLED_##id_ = !(sizeof(pref_) == 1);
@@ -29,4 +28,4 @@ namespace mozilla {
 
 }
 
-#endif // mozilla_ServoPropPrefList_h
+#endif 
