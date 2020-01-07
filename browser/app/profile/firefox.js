bebf951ccd030d42ec370698101ac0a0aa77c8d3
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 09:30:21 +0000

    Backed out 3 changesets (bug 1435142) for browser-chrome failures on browser_preloadedBrowser_zoom.js
    Backed out changeset 7334da12de35 (bug 1435142)
    Backed out changeset cd802f66b7aa (bug 1435142)
    Backed out changeset 1505855fc858 (bug 1435142)
    
    UltraBlame original commit: 8b7876354e566bfba91874af329bd167b5e78067

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -463,7 +463,6 @@ pref("browser.link.open_newwindow.disabled_in_fullscreen", false);
 #endif
 
 
-pref("browser.tabs.closeTabByDblclick", false);
 pref("browser.tabs.closeWindowWithLastTab", true);
 pref("browser.tabs.insertRelatedAfterCurrent", true);
 pref("browser.tabs.warnOnClose", true);
