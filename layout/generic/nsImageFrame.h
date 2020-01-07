67a7985d9507083f23b01543bc445b37721b56ad
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 23:18:26 +0000

    Bug 215083: Fix CreateContinuingFrame to account for non-nsImageLoadingContent image frames. r=heycam
    MozReview-Commit-ID: CLx3siPe4Cx
    
    UltraBlame original commit: cd4d140d2826391e40660efce32438d01b181a1e

--- layout/generic/nsImageFrame.h
+++ layout/generic/nsImageFrame.h
@@ -198,6 +198,9 @@ public:
       !HasAnyStateBits(NS_FRAME_GENERATED_CONTENT);
   }
 
+  
+  nsImageFrame* CreateContinuingFrame(nsIPresShell*, ComputedStyle*) const;
+
 private:
   friend nsIFrame* NS_NewImageFrame(nsIPresShell*, ComputedStyle*);
   friend nsIFrame* NS_NewImageFrameForContentProperty(nsIPresShell*, ComputedStyle*);
