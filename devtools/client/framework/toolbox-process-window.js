d9e561cdec57848ed46db32b502e25abc6fa08e2
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 17:41:33 +0000

    Bug 1457278 - Stop putting scratchpad in the default tool set in the Browser Toolbox;r=pbro
    We currently overflow the toolbar menu in a clean profile in the Browser Toolbox.
    Scratchpad can be accessed from the parent process by opening the Scratchpad window
    and targeting chrome, so this patch removes it from the default set.
    
    MozReview-Commit-ID: 6esMvDCt6Um
    
    UltraBlame original commit: 2ae98aca0b7eaa9e026b036d228736c5e3a33ffc

--- devtools/client/framework/toolbox-process-window.js
+++ devtools/client/framework/toolbox-process-window.js
@@ -98,7 +98,6 @@ function setPrefDefaults() {
   Services.prefs.setBoolPref("devtools.inspector.showAllAnonymousContent", true);
   Services.prefs.setBoolPref("browser.dom.window.dump.enabled", true);
   Services.prefs.setBoolPref("devtools.command-button-noautohide.enabled", true);
-  Services.prefs.setBoolPref("devtools.scratchpad.enabled", true);
   
   Services.prefs.setBoolPref("devtools.debugger.source-maps-enabled", false);
   Services.prefs.setBoolPref("devtools.debugger.new-debugger-frontend", true);
