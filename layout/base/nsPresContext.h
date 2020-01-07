2b3637f24b0becfb3666936e84bacc5a820ccde0
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 05:22:47 +0000

    merge mozilla-central to autoland. CLOSED TREE
    UltraBlame original commit: efd7d596ff1bc5b5a9c2c4ac7d5488a97a626eeb

--- layout/base/nsPresContext.h
+++ layout/base/nsPresContext.h
@@ -1288,6 +1288,12 @@ protected:
                                          const char* aName,
                                          uint32_t aDelay);
 
+  struct TransactionInvalidations {
+    uint64_t mTransactionId;
+    nsTArray<nsRect> mInvalidations;
+  };
+  TransactionInvalidations* GetInvalidations(uint64_t aTransactionId);
+
   
   
   
@@ -1356,10 +1362,6 @@ protected:
 
   mozilla::UniquePtr<nsBidi> mBidiEngine;
 
-  struct TransactionInvalidations {
-    uint64_t mTransactionId;
-    nsTArray<nsRect> mInvalidations;
-  };
   AutoTArray<TransactionInvalidations, 4> mTransactions;
 
   
@@ -1485,8 +1487,6 @@ protected:
   unsigned              mSuppressResizeReflow : 1;
 
   unsigned              mIsVisual : 1;
-
-  unsigned              mFireAfterPaintEvents : 1;
 
   unsigned              mIsChrome : 1;
   unsigned              mIsChromeOriginImage : 1;
