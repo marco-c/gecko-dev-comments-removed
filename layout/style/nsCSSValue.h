3e3ea916f30817c268664add70fbbf7c799c5aff
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 09:31:40 +0000

    Bug 1446108: followup: Make a destructor public to workaround bug 1448384 and reopen the CLOSED TREE. r=me
    MozReview-Commit-ID: BRnjUONXMud
    
    UltraBlame original commit: 08814c30af6d31a8c61899ef117ddcfa5614e984

--- layout/style/nsCSSValue.h
+++ layout/style/nsCSSValue.h
@@ -1206,11 +1206,10 @@ struct nsCSSValueSharedList final {
   {
   }
 
-private:
+  
   
   ~nsCSSValueSharedList();
 
-public:
   NS_INLINE_DECL_THREADSAFE_REFCOUNTING(nsCSSValueSharedList)
 
   void AppendToString(nsCSSPropertyID aProperty, nsAString& aResult) const;
