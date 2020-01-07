d14eab3bde437c8035b3f88803b4a88f817440aa
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 00:05:17 +0000

    Bug 1421582 - Fix dom/base/test/browser_bug902350.js to wait for events in proper order. r=Gijs
    UltraBlame original commit: d7fe31eaf4f38dec638d3ce322dde91763e7fb0f

--- dom/base/test/browser_bug902350.js
+++ dom/base/test/browser_bug902350.js
@@ -2,67 +2,43 @@
 
 
 
+add_task(async function mixed_content_block_for_target_top_test() {
+  const PREF_ACTIVE = "security.mixed_content.block_active_content";
+  const httpsTestRoot = getRootDirectory(gTestPath)
+    .replace("chrome://mochitests/content", "https://example.com");
 
-const PREF_ACTIVE = "security.mixed_content.block_active_content";
-const gHttpTestRoot = "https://example.com/browser/dom/base/test/";
-var origBlockActive;
-var gTestBrowser = null;
+  await SpecialPowers.pushPrefEnv({ set: [[ PREF_ACTIVE, true ]] });
 
-registerCleanupFunction(function() {
+  let newTab = await BrowserTestUtils.openNewForegroundTab({ gBrowser,
+                                                             waitForLoad: true });
+  let testBrowser = newTab.linkedBrowser;
+
+  var url = httpsTestRoot + "file_bug902350.html";
+  var frameUrl = httpsTestRoot + "file_bug902350_frame.html";
+  let loadPromise = BrowserTestUtils.browserLoaded(testBrowser, false, url);
+  let frameLoadPromise = BrowserTestUtils.browserLoaded(testBrowser, true,
+                                                        frameUrl);
+  testBrowser.loadURI(url);
+  await loadPromise;
+  await frameLoadPromise;
+
   
-  Services.prefs.setBoolPref(PREF_ACTIVE, origBlockActive);
-});
-
-function MixedTestsCompleted() {
-  gBrowser.removeCurrentTab();
-  window.focus();
-  finish();
-}
-
-function test() {
-  waitForExplicitFinish();
-
-  origBlockActive = Services.prefs.getBoolPref(PREF_ACTIVE);
-
-  Services.prefs.setBoolPref(PREF_ACTIVE, true);
-
-  var newTab = BrowserTestUtils.addTab(gBrowser);
-  gBrowser.selectedTab = newTab;
-  gTestBrowser = gBrowser.selectedBrowser;
-
-  BrowserTestUtils.browserLoaded(gTestBrowser).then(() => {
-    
-    var url = gHttpTestRoot + "file_bug902350.html";
-    BrowserTestUtils.browserLoaded(gTestBrowser, true ).then(MixedTest1A);
-    gTestBrowser.loadURI(url);
-  });
-}
-
-
-function MixedTest1A() {
-  BrowserTestUtils.browserLoaded(gTestBrowser, true ).then(MixedTest1B);
-}
-
-
-function MixedTest1B() {
-  BrowserTestUtils.browserLoaded(gTestBrowser).then(MixedTest1C);
-
-  ContentTask.spawn(gTestBrowser, null, function() {
+  let insecureUrl = "http://example.com/";
+  let insecureLoadPromise = BrowserTestUtils.browserLoaded(testBrowser, false,
+                                                           insecureUrl);
+  ContentTask.spawn(testBrowser, null, function() {
     var frame = content.document.getElementById("testing_frame");
     var topTarget = frame.contentWindow.document.getElementById("topTarget");
     topTarget.click();
   });
 
   
-  let {gIdentityHandler} = gTestBrowser.ownerGlobal;
+  await insecureLoadPromise;
+
+  
+  let {gIdentityHandler} = testBrowser.ownerGlobal;
   ok (!gIdentityHandler._identityBox.classList.contains("mixedActiveBlocked"),
       "Mixed Content Doorhanger did not appear when trying to navigate top");
-}
 
-function MixedTest1C() {
-  ContentTask.spawn(gTestBrowser, null, function() {
-    Assert.equal(content.location.href, "http://example.com/",
-      "Navigating to insecure domain through target='_top' failed.")
-  }).then(MixedTestsCompleted);
-}
-
+  await BrowserTestUtils.removeTab(newTab);
+});
