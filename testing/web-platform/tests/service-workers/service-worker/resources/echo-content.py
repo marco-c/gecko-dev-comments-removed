4b516065ef4aa00017eadbd7897dcf0d3b9d7aa4
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 04:48:47 +0000

    Bug 1435781 Make fetch-event.https.html POST tests set a content-type when reflecting the upload body back in the response. r=bz
    UltraBlame original commit: 370e267e160568862f1fd9ec246ab5bb840f586e

--- testing/web-platform/tests/service-workers/service-worker/resources/echo-content.py
+++ testing/web-platform/tests/service-workers/service-worker/resources/echo-content.py
@@ -4,7 +4,10 @@ def main(request, response):
 
     headers = [("X-Request-Method", request.method),
                ("X-Request-Content-Length", request.headers.get("Content-Length", "NO")),
-               ("X-Request-Content-Type", request.headers.get("Content-Type", "NO"))]
+               ("X-Request-Content-Type", request.headers.get("Content-Type", "NO")),
+
+               
+               ("Content-Type", "text/plain")]
 
     content = request.body
 
