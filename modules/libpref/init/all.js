dca7e0f5f4c3a41122aa58702c1ea159180d37a9
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 01:08:31 +0000

    Bug 1471704 - Enable tiling on Linux. r=nical
    MozReview-Commit-ID: JzqdK5abvSC
    
    UltraBlame original commit: 0bd6762c91fbfec5ae54592ac32e2c30e7d2c270

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -3069,6 +3069,9 @@ pref("layout.idle_period.time_limit", 1);
 pref("layout.css.webkit-appearance.enabled", false);
 
 
+
+
+
 #ifdef RELEASE_OR_BETA
 pref("dom.animations-api.core.enabled", false);
 #else
@@ -4824,7 +4827,7 @@ pref("layers.max-active", -1);
 
 pref("layers.offmainthreadcomposition.frame-rate", -1);
 
-#if defined(XP_MACOSX) || defined (OS_OPENBSD)
+#if defined(XP_MACOSX) || defined(MOZ_WIDGET_GTK)
 pref("layers.enable-tiles", true);
 #else
 pref("layers.enable-tiles", false);
