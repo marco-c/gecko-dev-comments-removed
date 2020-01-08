9102722f0e242464fcf9339d03c1d8d77d4000c9
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 16:49:30 +0000

    Bug 1504448 - Fix buffering problems in WebPDecoder. r=tnikkel
    There were two unrelated buffering problems in nsWebPDecoder. The first
    was with the decoder contract. We are expected to loop until the
    iterator is unable to provide more data, and wait for the SourceBuffer
    to reschedule us, where as nsWebPDecoder::DoDecode only did one pass.
    Thus when something yielded wanting more data, we would just wait
    forever.
    
    The second was the integration with the libwebp API. We are expected to
    retry when we receive SUSPENDED from the decoder, as it decided to yield
    pixels instead of continuing to decode as many as possible.
    
    The tests did not cover the first problem because multi chunk decoder
    tests do not use SourceBuffer scheduling. This is an oversight. They now
    will write a chunk of data, let the SourceBuffer reschedule the decoder,
    and repeat until all of the data has been written.
    
    The tests did not cover the second problem because all of the reference
    WebP images are too small. This patch adds a new test with a large WebP
    image (converted from a Mozilla all hands photo of lanyards). This
    should actually trigger the SUSPEND behaviour of libwebp.
    
    Differential Revision: https://phabricator.services.mozilla.com/D10817
    
    UltraBlame original commit: 1262664295ff6c471a0515b64a57b56d3c57b6ee

--- image/test/gtest/TestDecoders.cpp
+++ image/test/gtest/TestDecoders.cpp
@@ -195,7 +195,7 @@ CheckDecoderDelayedChunk(const ImageTestCase& aTestCase)
 }
 
 static void
-CheckDecoderMultiChunk(const ImageTestCase& aTestCase)
+CheckDecoderMultiChunk(const ImageTestCase& aTestCase, uint64_t aChunkSize = 1)
 {
   nsCOMPtr<nsIInputStream> inputStream = LoadFile(aTestCase.mPath);
   ASSERT_TRUE(inputStream != nullptr);
@@ -216,22 +216,31 @@ CheckDecoderMultiChunk(const ImageTestCase& aTestCase)
                                            DefaultSurfaceFlags());
   ASSERT_TRUE(decoder != nullptr);
   RefPtr<IDecodingTask> task =
-    new AnonymousDecodingTask(WrapNotNull(decoder),  false);
+    new AnonymousDecodingTask(WrapNotNull(decoder),  true);
 
-  for (uint64_t read = 0; read < length ; ++read) {
+  
+  
+  task->Run();
+
+  while (length > 0) {
+    uint64_t read = length > aChunkSize ? aChunkSize : length;
+    length -= read;
+
     uint64_t available = 0;
     rv = inputStream->Available(&available);
-    ASSERT_TRUE(available > 0);
+    ASSERT_TRUE(available >= read);
     ASSERT_TRUE(NS_SUCCEEDED(rv));
 
-    rv = sourceBuffer->AppendFromInputStream(inputStream, 1);
+    
+    rv = sourceBuffer->AppendFromInputStream(inputStream, read);
     ASSERT_TRUE(NS_SUCCEEDED(rv));
 
-    task->Run();
+    
+    SpinPendingEvents();
   }
 
   sourceBuffer->Complete(NS_OK);
-  task->Run();
+  SpinPendingEvents();
 
   CheckDecoderResults(aTestCase, decoder);
 }
@@ -775,6 +784,11 @@ TEST_F(ImageDecoders, WebPMultiChunk)
   CheckDecoderMultiChunk(GreenWebPTestCase());
 }
 
+TEST_F(ImageDecoders, WebPLargeMultiChunk)
+{
+  CheckDecoderMultiChunk(LargeWebPTestCase(),  64);
+}
+
 TEST_F(ImageDecoders, WebPDownscaleDuringDecode)
 {
   CheckDownscaleDuringDecode(DownscaledWebPTestCase());
@@ -976,7 +990,7 @@ TEST_F(ImageDecoders, LargeICOWithBMPSingleChunk)
 
 TEST_F(ImageDecoders, LargeICOWithBMPMultiChunk)
 {
-  CheckDecoderMultiChunk(LargeICOWithBMPTestCase());
+  CheckDecoderMultiChunk(LargeICOWithBMPTestCase(),  64);
 }
 
 TEST_F(ImageDecoders, LargeICOWithPNGSingleChunk)
