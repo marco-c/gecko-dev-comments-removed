7a72657ef50a4135d17ba708b90023d2377bfb07
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 01:39:13 +0000

    Bug 1467555 - Make Android single-locale nightlies tier-2. r=Callek
    UltraBlame original commit: 025c35ad797ffc349272d1587afd237abbff95d2

--- taskcluster/taskgraph/transforms/signing.py
+++ taskcluster/taskgraph/transforms/signing.py
@@ -124,10 +124,10 @@ def make_task_description(config, jobs):
 
             
             
-            if '-ccov' in build_platform or '-msvc' in build_platform:
-                treeherder.setdefault('tier', 2)
-            else:
-                treeherder.setdefault('tier', 1)
+            treeherder.setdefault(
+                'tier',
+                dep_job.task.get('extra', {}).get('treeherder', {}).get('tier', 1)
+            )
             treeherder.setdefault('kind', 'build')
 
         label = job['label']
