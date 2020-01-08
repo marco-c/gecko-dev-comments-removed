8362b93fd3cd35d00793ae97dfa5c75f8bf39013
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 07:41:19 +0000

    Bug 1482950 - Fix eslint errors in the tracking annotations test. r=dimi!
    Depends on D3721
    
    Differential Revision: https://phabricator.services.mozilla.com/D3722
    
    UltraBlame original commit: bd35c530c6e7073220f7cd32c1b2e37987f5df43

--- netwerk/test/unit/test_trackingProtection_annotateChannels.js
+++ netwerk/test/unit/test_trackingProtection_annotateChannels.js
@@ -19,7 +19,7 @@ ChromeUtils.import("resource://testing-common/httpd.js");
 
 
 
-const runtime = Cc["@mozilla.org/xre/app-info;1"].getService(Ci.nsIXULRuntime);
+const runtime = Services.appinfo;
 if (runtime.processType == runtime.PROCESS_TYPE_DEFAULT) {
   do_get_profile();
 }
@@ -46,10 +46,8 @@ listener.prototype = {
     request.cancel(Cr.NS_ERROR_ABORT);
     this._nextTest();
   },
-  onDataAvailable: function(request, context, stream, offset, count) {
-  },
-  onStopRequest: function(request, context, status) {
-  }
+  onDataAvailable: (request, context, stream, offset, count) => {},
+  onStopRequest: (request, context, status) => {},
 };
 
 var httpServer;
@@ -110,7 +108,7 @@ function makeChannel(path, loadingPrincipal, topWindowURI) {
   if (loadingPrincipal) {
     chan = NetUtil.newChannel({
       uri: path,
-      loadingPrincipal: loadingPrincipal,
+      loadingPrincipal,
       securityFlags: Ci.nsILoadInfo.SEC_ALLOW_CROSS_ORIGIN_DATA_IS_NULL,
       contentPolicyType: Ci.nsIContentPolicy.TYPE_OTHER,
     });
@@ -129,7 +127,7 @@ function makeChannel(path, loadingPrincipal, topWindowURI) {
   return chan;
 }
 
-var tests =[
+var tests = [
   
   setup_test,
 
@@ -308,8 +306,7 @@ var tests =[
   }
 ];
 
-function runTests()
-{
+function runTests() {
   if (!tests.length) {
     do_test_finished();
     return;
