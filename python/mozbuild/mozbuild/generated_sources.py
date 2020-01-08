2f6143364bf6ae6226f066bd39ea4bcd6a40ea2e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 17:49:23 +0000

    bug 1507047 - fix packaging of generated Rust sources. r=froydnj
    Differential Revision: https://phabricator.services.mozilla.com/D11874
    
    UltraBlame original commit: c63f0e55048749f967369fec6d9a67b61169c52a

--- python/mozbuild/mozbuild/generated_sources.py
+++ python/mozbuild/mozbuild/generated_sources.py
@@ -49,8 +49,7 @@ def get_generated_sources():
     
     
     rust_build_kind = 'debug' if buildconfig.substs.get('MOZ_DEBUG_RUST') else 'release'
-    base = mozpath.join('toolkit/library',
-                        buildconfig.substs['RUST_TARGET'],
+    base = mozpath.join(buildconfig.substs['RUST_TARGET'],
                         rust_build_kind,
                         'build')
     finder = FileFinder(mozpath.join(buildconfig.topobjdir, base))
