ad6418404d7b197a9a2629bcb48b256f341b3e4a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 17:11:57 +0000

    Bug 1376873 - Fix GetRTCPSenderReport; r=ng
    Differential Revision: https://phabricator.services.mozilla.com/D7431
    
    UltraBlame original commit: b55b0368d9f21849fa465fa9b3f028285c9ea6ae

--- media/webrtc/trunk/webrtc/call/video_receive_stream.h
+++ media/webrtc/trunk/webrtc/call/video_receive_stream.h
@@ -99,6 +99,9 @@ class VideoReceiveStream {
     RtcpPacketTypeCounter rtcp_packet_type_counts;
     RtcpStatistics rtcp_stats;
 
+    uint32_t rtcp_sender_packets_sent;
+    uint32_t rtcp_sender_octets_sent;
+
     
     
     rtc::Optional<webrtc::TimingFrameInfo> timing_frame_info;
