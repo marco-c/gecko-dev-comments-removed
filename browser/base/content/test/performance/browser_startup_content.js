df9e49ef8492f95bf1426ef5f095b582379765da
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 03:43:32 +0000

    Bug 1479235: Stop whitelisting SpecialPowers helper modules for content process startup. r=felipe
    MozReview-Commit-ID: 3y3qTVsB3ly
    
    UltraBlame original commit: 56dc94316408a37796340168f457b0cb42e96a00

--- browser/base/content/test/performance/browser_startup_content.js
+++ browser/base/content/test/performance/browser_startup_content.js
@@ -23,18 +23,12 @@ const whitelist = {
     "extension-process-script.js",
   ]),
   modules: new Set([
-    
     "chrome://mochikit/content/ShutdownLeaksCollector.jsm",
-    "resource://specialpowers/MockColorPicker.jsm",
-    "resource://specialpowers/MockFilePicker.jsm",
-    "resource://specialpowers/MockPermissionPrompt.jsm",
 
     
     "resource://gre/modules/AppConstants.jsm",
     "resource://gre/modules/AsyncShutdown.jsm",
     "resource://gre/modules/DeferredTask.jsm",
-    "resource://gre/modules/FileUtils.jsm",
-    "resource://gre/modules/NetUtil.jsm",
     "resource://gre/modules/PromiseUtils.jsm",
     "resource://gre/modules/Services.jsm", 
     "resource://gre/modules/Timer.jsm",
@@ -71,9 +65,6 @@ const whitelist = {
     
     "resource://gre/modules/ExtensionUtils.jsm",
     "resource://gre/modules/MessageChannel.jsm",
-
-    
-    "resource://gre/modules/ServiceWorkerCleanUp.jsm",
   ]),
 };
 
