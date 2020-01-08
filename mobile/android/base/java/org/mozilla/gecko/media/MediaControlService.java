6976f9da827bc775c4dea24c1029525574404f22
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 02:32:12 +0000

    Bug 1477041 - Modified MediaControlService and notification flow. r=jchen
    Created a new parcelable which is sent to MediaControlService where
    the notification is created based on the data of this object instead of
    referencing a static notification. Toggling the service is now done through
    startService with a specific action for shutting down instead of calling
    stopService due to concurrency issues where the service can be stopped before
    having a chance to call startForeground.
    
    MozReview-Commit-ID: 6qNPintkVy
    
    UltraBlame original commit: 06cdeb2fd5224952178ff7a804b9151a1b5f2af9

--- mobile/android/base/java/org/mozilla/gecko/media/MediaControlService.java
+++ mobile/android/base/java/org/mozilla/gecko/media/MediaControlService.java
@@ -1,22 +1,57 @@
+
+
+
+
 package org.mozilla.gecko.media;
 
+import android.annotation.SuppressLint;
+import android.app.Notification;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 import android.util.Log;
 
+import org.mozilla.gecko.AppConstants;
+import org.mozilla.gecko.GeckoApplication;
 import org.mozilla.gecko.R;
 
 public class MediaControlService extends Service {
     private static final String LOGTAG = "MediaControlService";
 
+    private Notification currentNotification;
+
+    @SuppressLint("NewApi")
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        
+        
+        if (AppConstants.Versions.preO) {
+            currentNotification = new Notification.Builder(this).build();
+        } else {
+            currentNotification = new Notification.Builder(this, GeckoApplication.getDefaultNotificationChannel().getId()).build();
+        }
+    }
+
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.d(LOGTAG, "onStartCommand");
-        startForeground(R.id.mediaControlNotification, GeckoMediaControlAgent.getInstance().getCurrentNotification());
 
-        if (intent != null && intent.getAction() != null) {
-            GeckoMediaControlAgent.getInstance().handleAction(intent.getAction());
+        if (intent.hasExtra(GeckoMediaControlAgent.EXTRA_NOTIFICATION_DATA)) {
+            currentNotification = GeckoMediaControlAgent.getInstance().createNotification(
+                    (MediaNotification) intent.getParcelableExtra(GeckoMediaControlAgent.EXTRA_NOTIFICATION_DATA));
+        }
+
+        startForeground(R.id.mediaControlNotification, currentNotification);
+
+        if (intent.getAction() != null) {
+            final String action = intent.getAction();
+            if (action.equals(GeckoMediaControlAgent.ACTION_SHUTDOWN)) {
+                stopForeground(true);
+                stopSelfResult(startId);
+            } else {
+                GeckoMediaControlAgent.getInstance().handleAction(action);
+            }
         }
 
         return START_NOT_STICKY;
