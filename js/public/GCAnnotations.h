aa45ae93f5b257b9b5347de4ed2adfced87ea14f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 02:51:14 +0000

    Bug 1347999.  Annotate xpidl methods/attributes that can be implemented in script as JS_HAZ_CAN_RUN_SCRIPT. r=froydnj
    UltraBlame original commit: 3895fde69249ee14f4f3d55aafe090bcfd0d6cb0

--- js/public/GCAnnotations.h
+++ js/public/GCAnnotations.h
@@ -42,6 +42,10 @@
 
 # define JS_HAZ_GC_SUPPRESSED __attribute__((tag("Suppress GC")))
 
+
+
+# define JS_HAZ_CAN_RUN_SCRIPT __attribute__((tag("Can run script")))
+
 #else
 
 # define JS_HAZ_GC_THING
@@ -51,6 +55,7 @@
 # define JS_HAZ_NON_GC_POINTER
 # define JS_HAZ_GC_CALL
 # define JS_HAZ_GC_SUPPRESSED
+# define JS_HAZ_CAN_RUN_SCRIPT
 
 #endif
 
