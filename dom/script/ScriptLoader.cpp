04ad80b744eb35701f9d07cf3d53677d696f47fe
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 01:08:09 +0000

    Bug 1475228 - Allocate script loader source buffers on the JS heap and pass ownership when compiling r=baku
    UltraBlame original commit: e91802969fb712eae9d2a5975406082bdbb64b95

--- dom/script/ScriptLoader.cpp
+++ dom/script/ScriptLoader.cpp
@@ -1899,14 +1899,13 @@ SourceBufferHolder
 ScriptLoader::GetScriptSource(JSContext* aCx, ScriptLoadRequest* aRequest)
 {
   
+  
 
   
   if (aRequest->mIsInline) {
     nsAutoString inlineData;
     aRequest->mElement->GetScriptText(inlineData);
 
-    
-    
     size_t nbytes = inlineData.Length() * sizeof(char16_t);
     JS::UniqueTwoByteChars chars(static_cast<char16_t*>(JS_malloc(aCx, nbytes)));
     MOZ_RELEASE_ASSERT(chars);
@@ -1914,9 +1913,10 @@ ScriptLoader::GetScriptSource(JSContext* aCx, ScriptLoadRequest* aRequest)
     return SourceBufferHolder(std::move(chars), inlineData.Length());
   }
 
-  return SourceBufferHolder(aRequest->ScriptText().begin(),
-                            aRequest->ScriptText().length(),
-                            SourceBufferHolder::NoOwnership);
+  size_t length = aRequest->ScriptText().length();
+  return SourceBufferHolder(aRequest->ScriptText().extractOrCopyRawBuffer(),
+                            length,
+                            SourceBufferHolder::GiveOwnership);
 }
 
 nsresult
