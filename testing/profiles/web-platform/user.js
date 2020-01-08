f98b739afc6e06b22462daf225aa075b45e070c4
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 11:46:22 +0000

    Bug 1493549 - Enable media.navigator.streams.fake in WPT r=jgraham
    Reland of https://phabricator.services.mozilla.com/D6579 with updated expectations
    
    Differential Revision: https://phabricator.services.mozilla.com/D6676
    
    UltraBlame original commit: 5d1c5acc269af59f7fe09144aa50015f08cb2fb2

--- testing/profiles/web-platform/user.js
+++ testing/profiles/web-platform/user.js
@@ -11,6 +11,8 @@ user_pref("extensions.update.notifyUser", false);
 
 user_pref("focusmanager.testmode", true);
 
+user_pref("media.navigator.streams.fake", true);
+
 user_pref("network.preload", true);
 
 user_pref("network.proxy.type", 0);
