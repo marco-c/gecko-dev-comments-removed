50b8d8cad8372b3a56a5dbad268ec05c9eb3bd4f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 12:58:16 +0000

    Bug 1451169 - Reduce indirection for static atom pointers in nsCSSFrameConstructor.h. r=xidorn
    UltraBlame original commit: 972d5469e2b9d8c945e37446d684ae0b8656dbb5

--- layout/base/nsCSSFrameConstructor.h
+++ layout/base/nsCSSFrameConstructor.h
@@ -742,7 +742,7 @@ private:
     FrameFullConstructor mFullConstructor;
     
     
-    nsICSSAnonBoxPseudo * const * const mAnonBoxPseudo;
+    nsICSSAnonBoxPseudo* const mAnonBoxPseudo;
   };
 
   
@@ -752,7 +752,7 @@ private:
   struct FrameConstructionDataByTag {
     
     
-    const nsStaticAtom * const * const mTag;
+    const nsStaticAtom* const* mTag;
     const FrameConstructionData mData;
   };
 
@@ -783,7 +783,7 @@ private:
 
   struct PseudoParentData {
     const FrameConstructionData mFCData;
-    nsICSSAnonBoxPseudo * const * const mPseudoType;
+    nsICSSAnonBoxPseudo * const* mPseudoType;
   };
   
 
