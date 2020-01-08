4385f7d181484557faa9f1bebbc02bceb70d707f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 04:10:36 +0000

    Backed out changeset 3f41992ab77e (bug 1446309) because wrong bug number. r=backout
    UltraBlame original commit: b44b01a42d8c47688ad9af21fd9463c8e19f791b

--- gfx/layers/ipc/SharedSurfacesChild.cpp
+++ gfx/layers/ipc/SharedSurfacesChild.cpp
@@ -30,39 +30,20 @@ public:
 
   ImageKeyData(ImageKeyData&& aOther)
     : mManager(std::move(aOther.mManager))
-    , mDirtyRect(std::move(aOther.mDirtyRect))
     , mImageKey(aOther.mImageKey)
   { }
 
   ImageKeyData& operator=(ImageKeyData&& aOther)
   {
     mManager = std::move(aOther.mManager);
-    mDirtyRect = std::move(aOther.mDirtyRect);
     mImageKey = aOther.mImageKey;
     return *this;
-  }
-
-  void MergeDirtyRect(const Maybe<IntRect>& aDirtyRect)
-  {
-    if (mDirtyRect) {
-      if (aDirtyRect) {
-        mDirtyRect->UnionRect(mDirtyRect.ref(), aDirtyRect.ref());
-      }
-    } else {
-      mDirtyRect = aDirtyRect;
-    }
-  }
-
-  Maybe<IntRect> TakeDirtyRect()
-  {
-    return std::move(mDirtyRect);
   }
 
   ImageKeyData(const ImageKeyData&) = delete;
   ImageKeyData& operator=(const ImageKeyData&) = delete;
 
   RefPtr<WebRenderLayerManager> mManager;
-  Maybe<IntRect> mDirtyRect;
   wr::ImageKey mImageKey;
 };
 
@@ -162,23 +143,14 @@ public:
         bool ownsKey = wrBridge->GetNamespace() == entry.mImageKey.mNamespace;
         if (!ownsKey) {
           entry.mImageKey = wrBridge->GetNextImageKey();
-          entry.TakeDirtyRect();
           aResources.AddExternalImage(mId, entry.mImageKey);
-        } else {
-          entry.MergeDirtyRect(aDirtyRect);
-          Maybe<IntRect> dirtyRect = entry.TakeDirtyRect();
-          if (dirtyRect) {
-            aResources.UpdateExternalImage(mId, entry.mImageKey,
-                                           ViewAs<ImagePixel>(dirtyRect.ref()));
-          }
+        } else if (aDirtyRect) {
+          aResources.UpdateExternalImage(mId, entry.mImageKey,
+                                         ViewAs<ImagePixel>(aDirtyRect.ref()));
         }
 
         key = entry.mImageKey;
         found = true;
-      } else {
-        
-        
-        entry.MergeDirtyRect(aDirtyRect);
       }
     }
 
