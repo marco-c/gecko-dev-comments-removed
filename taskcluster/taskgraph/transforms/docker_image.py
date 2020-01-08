3d99884e0550c12869cd4376324696cd913e007d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 17:45:49 +0000

    Bug 1486071 - Retry docker-image and packages tasks that fail during apt-get. r=dustin
    When apt-get fails, it has a distinctive error code (100). Most of the
    time, when apt-get fails, it's because of some network error, or
    possibly some problem unpacking archives. When that happens, retrying
    the task usually "fixes" the issue.
    
    One of the (currently) most common causes of problems is
    snapshot.debian.org not being available to some of the EC2 instances.
    
    It would be possible to only set things up so that we only retry when we
    detect such setup (checking the public IP of the instance is not in the
    known list of problematic IPs), but that would require possibly wrapping
    apt-get, or something along those line, which is not entirely trivial to
    do for the packages tasks, because they don't rely on docker images.
    
    However, since there aren't many apt-get failures other than these,
    and since there have been, historically, some intermittent apt-get
    failures of a different nature that were solved by re-running the tasks,
    it seems fair to just retry wheneven apt-get fails.
    
    One downside of the approach is that if for some reason a change to a
    Dockerfile ends up mentioning a package that doesn't exist, that too
    will result in multiple retries ; which might be inconvenient, but
    that's not something that's going to happen often.
    
    Differential Revision: https://phabricator.services.mozilla.com/D11420
    
    UltraBlame original commit: 7c0c8f3df6984e8be4ce8fbd9cd5b8dfd792ea90

--- taskcluster/taskgraph/transforms/docker_image.py
+++ taskcluster/taskgraph/transforms/docker_image.py
@@ -179,6 +179,8 @@ def fill_template(config, tasks):
                 'docker-in-docker': True,
                 'taskcluster-proxy': True,
                 'max-run-time': 7200,
+                
+                'retry-exit-status': [100],
             },
         }
         
