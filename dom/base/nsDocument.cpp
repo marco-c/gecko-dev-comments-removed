31558c8a662b046a6f40634539715f345703f37c
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 22:18:10 +0000

    Bug 1516366 - Move CloneDocHelper to nsIDocument. r=smaug
    Differential Revision: https://phabricator.services.mozilla.com/D15363
    
    UltraBlame original commit: 87aa3be318fabc7256a2f6441d5dc2583b51826e

--- dom/base/nsDocument.cpp
+++ dom/base/nsDocument.cpp
@@ -8073,7 +8073,7 @@ void nsIDocument::RefreshLinkHrefs() {
   }
 }
 
-nsresult nsDocument::CloneDocHelper(nsDocument* clone) const {
+nsresult nsIDocument::CloneDocHelper(nsIDocument* clone) const {
   clone->mIsStaticDocument = mCreatingStaticClone;
 
   
@@ -8110,7 +8110,7 @@ nsresult nsDocument::CloneDocHelper(nsDocument* clone) const {
   
   
   
-  clone->nsDocument::SetDocumentURI(nsIDocument::GetDocumentURI());
+  clone->SetDocumentURI(nsIDocument::GetDocumentURI());
   clone->SetChromeXHRDocURI(mChromeXHRDocURI);
   clone->SetPrincipal(NodePrincipal());
   clone->mDocumentBaseURI = mDocumentBaseURI;
@@ -10964,8 +10964,8 @@ void nsIDocument::DocAddSizeOfExcludingThis(nsWindowSizes& aWindowSizes) const {
     mNodeInfoManager->AddSizeOfIncludingThis(aWindowSizes);
   }
 
-  aWindowSizes.mDOMMediaQueryLists +=
-      mDOMMediaQueryLists.sizeOfExcludingThis(aWindowSizes.mState.mMallocSizeOf);
+  aWindowSizes.mDOMMediaQueryLists += mDOMMediaQueryLists.sizeOfExcludingThis(
+      aWindowSizes.mState.mMallocSizeOf);
 
   for (const MediaQueryList* mql : mDOMMediaQueryLists) {
     aWindowSizes.mDOMMediaQueryLists +=
