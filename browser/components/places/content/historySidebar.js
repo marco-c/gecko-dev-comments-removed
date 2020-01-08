d479e075ec1881b8d1ac5daf8cb31f98acf8a83f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 16:26:13 +0000

    Bug 1482091: Part 2 - Remove TelemetryStopwatch.jsm in favor of native implementation. r=chutten
    Differential Revision: https://phabricator.services.mozilla.com/D9888
    
    UltraBlame original commit: afd0a13bcfff4c92ad2497ec687f466484481667

--- browser/components/places/content/historySidebar.js
+++ browser/components/places/content/historySidebar.js
@@ -20,8 +20,6 @@ XPCOMUtils.defineLazyScriptGetter(this, ["PlacesInsertionPoint", "PlacesControll
                                   "chrome://browser/content/places/controller.js");
 
 
-ChromeUtils.import("resource://gre/modules/TelemetryStopwatch.jsm");
-
 var gHistoryTree;
 var gSearchBox;
 var gHistoryGrouping = "";
