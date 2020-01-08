9d310306725c9b4479f0b76559e26dd6516fa341
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 16:19:33 +0000

    Bug 1500165 - Include gear on each page of about:addons r=jaws
    Differential Revision: https://phabricator.services.mozilla.com/D9155
    
    UltraBlame original commit: 56f06760024b68d78c44d082ba0e06b1506ad68b

--- toolkit/mozapps/extensions/content/extensions.js
+++ toolkit/mozapps/extensions/content/extensions.js
@@ -866,6 +866,16 @@ var gViewController = {
     this.currentViewObj.node.setAttribute("loading", "true");
     this.currentViewObj.node.focus();
 
+    let headingName = document.getElementById("heading-name");
+    try {
+      headingName.textContent = gStrings.ext.GetStringFromName(`listHeading.${view.param}`);
+      setSearchLabel(view.param);
+    } catch (e) {
+      
+      headingName.textContent = "";
+    }
+
+
     if (aViewId == aPreviousView)
       this.currentViewObj.refresh(view.param, ++this.currentViewRequest, aState);
     else
@@ -2398,14 +2408,6 @@ var gListView = {
     this._type = aType;
     this.node.setAttribute("type", aType);
     this.showEmptyNotice(false);
-
-    try {
-      document.getElementById("list-view-heading-name")
-        .textContent = gStrings.ext.GetStringFromName(`listHeading.${aType}`);
-      setSearchLabel(aType);
-    } catch (e) {
-      
-    }
 
     this._listBox.textContent = "";
 
