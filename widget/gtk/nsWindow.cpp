1c139bfcf17ad4f33e9acf846f89212169322ebc
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 20:31:34 +0000

    Bug 1485616 - [Wayland] Wait with drawing until mozcontainer init is finished, r=jhorak
    Differential Revision: https://phabricator.services.mozilla.com/D14070
    
    UltraBlame original commit: ad2c33f952543b89dc9ecc3441f37b15dff6cc76

--- widget/gtk/nsWindow.cpp
+++ widget/gtk/nsWindow.cpp
@@ -1869,6 +1869,9 @@ gboolean nsWindow::OnExposeEvent(cairo_t *cr) {
 
   
   if (!mGdkWindow || mIsFullyObscured || !mHasMappedToplevel) return FALSE;
+#ifdef MOZ_WAYLAND
+  if (mContainer && !mContainer->ready_to_draw) return FALSE;
+#endif
 
   nsIWidgetListener *listener = GetListener();
   if (!listener) return FALSE;
