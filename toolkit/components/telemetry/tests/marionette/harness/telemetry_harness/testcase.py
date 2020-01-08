96082dce9dac9d95fe7313e33be0b67d1fdd882b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 18:56:55 +0000

    Bug 1502849 - Add new telemetry client test for SEARCH_COUNTS across sessions; r=Dexter,gfritzsche
    Depends on D12288
    
    Differential Revision: https://phabricator.services.mozilla.com/D12289
    
    UltraBlame original commit: a92340f218538b107ed3a8b9ec19a980b9a7c160

--- toolkit/components/telemetry/tests/marionette/harness/telemetry_harness/testcase.py
+++ toolkit/components/telemetry/tests/marionette/harness/telemetry_harness/testcase.py
@@ -7,8 +7,6 @@ import simplejson as json
 import time
 import zlib
 
-from multiprocessing import Process
-
 from firefox_puppeteer import PuppeteerMixin
 from marionette_driver.addons import Addons
 from marionette_driver.errors import MarionetteException
@@ -123,22 +121,21 @@ class TelemetryTestCase(PuppeteerMixin, MarionetteTestCase):
         self.restart(clean=False, in_app=True)
 
     def install_addon(self):
-        trigger = Process(target=self._install_addon)
-        trigger.start()
+        """Install a minimal addon."""
 
-    def _install_addon(self):
-        
-        
-        
-        
-        
+        resources_dir = os.path.join(
+            os.path.dirname(os.path.dirname(__file__)), "resources"
+        )
+
+        addon_path = os.path.abspath(os.path.join(resources_dir, "helloworld"))
+
         try:
-            
-            addon_path = os.path.join('resources_dir', 'easyscreenshot.xpi')
             addons = Addons(self.marionette)
-            addons.install(addon_path)
+            addons.install(addon_path, temp=True)
         except MarionetteException as e:
-            self.fail('{} - Error installing addon: {} - '.format(e.cause, e.message))
+            self.fail(
+                "{} - Error installing addon: {} - ".format(e.cause, e.message)
+            )
 
     @property
     def client_id(self):
