6ade6a7dbabda1dc3cf0863d62b841a5016f53a8
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 20:49:15 +0000

    Bug 1513157 - Don't install minidump_stackwalk in the Ubuntu test image. r=gps,ted
    Differential Revision: https://phabricator.services.mozilla.com/D14133
    
    UltraBlame original commit: 84433828d732d0806310ec43e49ebc1cead61402

--- testing/mozharness/configs/remove_executables.py
+++ testing/mozharness/configs/remove_executables.py
@@ -1,7 +1,5 @@
 config = {
     
-    "download_minidump_stackwalk": False,
-    "minidump_stackwalk_path": "/usr/local/bin/linux64-minidump_stackwalk",
     "download_nodejs": False,
     "nodejs_path": "/usr/local/bin/node",
     "exes": {}
