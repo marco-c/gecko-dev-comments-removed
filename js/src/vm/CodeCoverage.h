ff6061a4e12ae7dfe970ea96c93046a44da3fe2f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 16:36:37 +0000

    Bug 1504073 - Don't create lcov output file as soon as the runtime is created, but only when we actually have to write lcov data out. r=nbp
    UltraBlame original commit: 8d667ad8e5aad0da4007060712fbdb056613f7b8

--- js/src/vm/CodeCoverage.h
+++ js/src/vm/CodeCoverage.h
@@ -131,7 +131,13 @@ class LCovRuntime
     void init();
 
     
-    bool isEnabled() const { return out_.isInitialized(); }
+    bool isEnabled() const {
+      static bool isEnabled_ = [](){
+        const char* outDir = getenv("JS_CODE_COVERAGE_OUTPUT_DIR");
+        return outDir && *outDir != 0;
+      };
+      return isEnabled_;
+    }
 
     
     
