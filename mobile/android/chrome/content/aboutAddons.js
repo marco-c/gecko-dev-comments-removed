19e08df55f37575bb98e55ac5990091034e341d9
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 17:26:47 +0000

    Bug 1429488 - Part 8: Fix mobile about:addons code for enabling/disabling themes. r=aswan
    about:addons on Android has some logic to ensure that only one theme can be
    active at the same time.
    
    At some point however, themes must have learned to take care of themselves in
    that regard, which means that this code
    a) has become unnecessary, and
    b) with Webextension themes it can actually cause a harmful race condition:
    
    Explicitly disabling the previously active theme implicitly enables the default
    theme, so what happens is that depending on the size of the Webextension theme
    to be enabled, the new theme can be enabled before the previous disable() call
    completes. This leads to the new theme then immediately being disabled again as
    the previous disable() call finally gets around to enabling the default theme.
    The smaller the Webextension theme to be enabled and the faster it loads, the
    more likely this is to happen.
    
    We still need to manually fix up the disabled state of themes in the UI, though,
    so that it shows the correct state without a reload. Therefore, we take the
    opportunity to fix the problem that up until now, disabling a theme wouldn't
    mark the default theme as enabled in the UI, unless you manually reloaded the
    page afterwards.
    
    Differential Revision: https://phabricator.services.mozilla.com/D10732
    
    UltraBlame original commit: 71df0988d340789d2a6fb26edb36936947b543c8

--- mobile/android/chrome/content/aboutAddons.js
+++ mobile/android/chrome/content/aboutAddons.js
@@ -521,20 +521,33 @@ var Addons = {
       return addon.enable();
     }
 
-    let opType;
-    if (addon.type == "theme") {
+    function updateOtherThemeStateInUI(item) {
       if (aValue) {
         
-        let list = document.getElementById("addons-list");
-        let item = list.firstElementChild;
-        while (item) {
-          if (item.addon && (item.addon.type == "theme") && (item.addon.isActive)) {
-            item.addon.disable();
-            item.setAttribute("isDisabled", true);
-            break;
-          }
-          item = item.nextSibling;
+        if (item.addon.isActive) {
+          item.setAttribute("isDisabled", true);
+          return true;
         }
+      
+      } else if (item.addon.id == "default-theme@mozilla.org") {
+        item.removeAttribute("isDisabled");
+        return true;
+      }
+      return false;
+    }
+
+    let opType;
+    if (addon.type == "theme") {
+      
+      
+      let list = document.getElementById("addons-list");
+      let item = list.firstElementChild;
+      while (item) {
+        if (item.addon && (item.addon.type == "theme") &&
+            updateOtherThemeStateInUI(item)) {
+          break;
+        }
+        item = item.nextSibling;
       }
       setDisabled(addon, !aValue);
     } else if (addon.type == "locale") {
