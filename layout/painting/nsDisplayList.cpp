9ab9398c3ef6998baef0b27b0624a6da11edad5e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 11:21:25 +0000

    Backed out 4 changesets (bug 1418816) for reftest perma failure bugs/289480.html a=backout
    Backed out changeset d722f5a4a8af (bug 1418816)
    Backed out changeset 75d9a625be06 (bug 1418816)
    Backed out changeset 807f0f510c25 (bug 1418816)
    Backed out changeset 3c4cec04f28d (bug 1418816)
    
    UltraBlame original commit: 4273b6a762adb462db19936963348aae848a0618

--- layout/painting/nsDisplayList.cpp
+++ layout/painting/nsDisplayList.cpp
@@ -9397,8 +9397,6 @@ nsDisplayPerspective::CreateWebRenderCommands(
   Point3D roundedOrigin(NS_round(newOrigin.x), NS_round(newOrigin.y), 0);
 
   gfx::Matrix4x4 transformForSC = gfx::Matrix4x4::Translation(roundedOrigin);
-  
-  nsIFrame* perspectiveFrame = mFrame->GetContainingBlock(nsIFrame::SKIP_SCROLLED_FRAME);
 
   nsTArray<mozilla::wr::WrFilterOp> filters;
   StackingContextHelper sc(aSc,
@@ -9412,7 +9410,7 @@ nsDisplayPerspective::CreateWebRenderCommands(
                            &perspectiveMatrix,
                            gfx::CompositionOp::OP_OVER,
                            !BackfaceIsHidden(),
-                           perspectiveFrame->Extend3DContext());
+                           true);
 
   return mList.CreateWebRenderCommands(
     aBuilder, aResources, sc, aManager, aDisplayListBuilder);
