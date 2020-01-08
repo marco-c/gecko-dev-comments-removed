35c55f1107b7890bff28ce96cbb98548e612b4c8
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 15:15:08 +0000

    Bug 1500862 - part 1: Make HTMLEditor::SetInlineProperty() remove exclusive style automatically r=m_kato
    StyleUpdatingCommand::ToggleState() removes exclusive style when it sets
    superscript style or subscript style.  However, it accesses protected
    members of EditorBase via AutoTransactionBatch even though it's outside
    of editor classes.
    
    This patch moves the removing exclusive style code from the method to
    HTMLEditor::SetInlineProperty() and rename it "AsAction".
    
    Differential Revision: https://phabricator.services.mozilla.com/D9477
    
    UltraBlame original commit: 7bda40ae7f004f43c3afbfb66f7a3b5a2ea2eaa9

--- editor/libeditor/HTMLEditor.h
+++ editor/libeditor/HTMLEditor.h
@@ -336,16 +336,15 @@ public:
 
   nsresult AddZIndex(int32_t aChange);
 
-  nsresult SetInlineProperty(nsAtom& aProperty,
-                             nsAtom* aAttribute,
-                             const nsAString& aValue)
-  {
-    nsresult rv = SetInlinePropertyInternal(aProperty, aAttribute, aValue);
-    if (NS_WARN_IF(NS_FAILED(rv))) {
-      return rv;
-    }
-    return NS_OK;
-  }
+  
+
+
+
+
+
+  nsresult SetInlinePropertyAsAction(nsAtom& aProperty,
+                                     nsAtom* aAttribute,
+                                     const nsAString& aValue);
 
   nsresult GetInlineProperty(nsAtom* aProperty,
                              nsAtom* aAttribute,
