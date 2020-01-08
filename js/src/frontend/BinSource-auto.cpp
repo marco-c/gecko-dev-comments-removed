c6c003c1e9b077ff519b7edae99d5a06e079dc5d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 12:39:47 +0000

    Bug 1494930 - Part 2: Correctly generate JSOP_FUNCALL and JSOP_FUNAPPLY. (r=arai)
    UltraBlame original commit: 9fc8180f36219eeb95f900c35da744f0aee4a96d

--- js/src/frontend/BinSource-auto.cpp
+++ js/src/frontend/BinSource-auto.cpp
@@ -3860,6 +3860,18 @@ BinASTParser<Tok>::parseInterfaceCallExpression(const size_t start, const BinKin
     BINJS_MOZ_TRY_DECL(arguments, parseArguments());
 
     auto op = JSOP_CALL;
+
+    
+    if (PropertyName* prop = factory_.maybeDottedProperty(callee)) {
+        if (prop == cx_->names().apply) {
+            op = JSOP_FUNAPPLY;
+            if (parseContext_->isFunctionBox())
+                parseContext_->functionBox()->usesApply = true;
+        } else if (prop == cx_->names().call) {
+            op = JSOP_FUNCALL;
+        }
+    }
+
     
     if (factory_.isEvalName(callee, cx_)) {
         if (!parseContext_->varScope().lookupDeclaredNameForAdd(callee->name())
