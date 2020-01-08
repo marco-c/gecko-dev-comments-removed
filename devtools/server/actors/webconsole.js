75d28190a1888a64328368a7e06b6ec0faf2d7ab
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 03:16:22 +0000

    Bug 1444132 - Fix sendHTTPRequest with the new setup. r=jryans
    MozReview-Commit-ID: GCZWd47ICLH
    
    UltraBlame original commit: bb4fb7bb9ab254277325941640c0c2256ea003b2

--- devtools/server/actors/webconsole.js
+++ devtools/server/actors/webconsole.js
@@ -1762,8 +1762,8 @@ WebConsoleActor.prototype =
 
 
 
-  sendHTTPRequest(message) {
-    const { url, method, headers, body } = message.request;
+  async sendHTTPRequest({ request }) {
+    const { url, method, headers, body } = request;
 
     
     
@@ -1799,15 +1799,30 @@ WebConsoleActor.prototype =
 
     NetUtil.asyncFetch(channel, () => {});
 
-    const actor = this.getNetworkEventActor(channel.channelId);
-
     
-    this._netEvents.set(channel.channelId, actor);
-
-    return {
-      from: this.actorID,
-      eventActor: actor.form()
-    };
+    const { channelId } = channel;
+    if (this.networkMonitorActor) {
+      const actor = this.networkMonitorActor.getNetworkEventActor(channelId);
+      return {
+        eventActor: actor.form()
+      };
+    } else if (this.networkMonitorActorId) {
+      
+      
+      const messageManager = this.parentActor.messageManager;
+      return new Promise(resolve => {
+        const onMessage = ({ data }) => {
+          messageManager.removeMessageListener("debug:get-network-event-actor",
+            onMessage);
+          resolve({
+            eventActor: data
+          });
+        };
+        messageManager.addMessageListener("debug:get-network-event-actor", onMessage);
+        messageManager.sendAsyncMessage("debug:get-network-event-actor", { channelId });
+      });
+    }
+    return null;
   },
 
   
