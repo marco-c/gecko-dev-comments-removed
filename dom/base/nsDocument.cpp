5b9190b7ec39bdb21ce38bce54745d97fa25023b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 11:40:26 +0000

    Merge mozilla-central to mozilla-inbound.  a=merge CLOSED TREE
    UltraBlame original commit: f125ee6d1cef0ac302c3b4570b10b251a4638c54

--- dom/base/nsDocument.cpp
+++ dom/base/nsDocument.cpp
@@ -7299,6 +7299,12 @@ nsIDocument::GetViewportInfo(const ScreenIntSize& aDisplaySize)
     mValidWidth = (!widthStr.IsEmpty() && NS_SUCCEEDED(widthErrorCode) && mViewportSize.width > 0);
     mValidHeight = (!heightStr.IsEmpty() && NS_SUCCEEDED(heightErrorCode) && mViewportSize.height > 0);
 
+    
+    
+    if ((!mValidWidth && !widthStr.IsEmpty()) && !mValidHeight) {
+      mAutoSize = true;
+    }
+
     mAllowZoom = true;
     nsAutoString userScalable;
     GetHeaderData(nsGkAtoms::viewport_user_scalable, userScalable);
