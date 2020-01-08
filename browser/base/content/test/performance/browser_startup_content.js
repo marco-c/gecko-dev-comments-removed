f4320762a22a2cec7ba7672d38c1e2cac2c79f49
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 14:31:56 +0000

    Bug 1497707, part 1 - Get rid of xpcIJSModuleLoader r=kmag
    This interface is only used for a few testing functions. Just move
    them to Cu.
    
    Differential Revision: https://phabricator.services.mozilla.com/D8168
    
    UltraBlame original commit: 80bf9ddf5bedaaded17e4d0f3a2860ed73892c10

--- browser/base/content/test/performance/browser_startup_content.js
+++ browser/base/content/test/performance/browser_startup_content.js
@@ -110,17 +110,16 @@ add_task(async function() {
     Cm.QueryInterface(Ci.nsIServiceManager);
     ChromeUtils.import("resource://gre/modules/AppConstants.jsm");
     let collectStacks = AppConstants.NIGHTLY_BUILD || AppConstants.DEBUG;
-    let loader = Cc["@mozilla.org/moz/jsloader;1"].getService(Ci.xpcIJSModuleLoader);
     let components = {};
-    for (let component of loader.loadedComponents()) {
+    for (let component of Cu.loadedComponents()) {
       
 
       components[component.replace(/.*\//, "")] =
-        collectStacks ? loader.getComponentLoadStack(component) : "";
+        collectStacks ? Cu.getComponentLoadStack(component) : "";
     }
     let modules = {};
-    for (let module of loader.loadedModules()) {
-      modules[module] = collectStacks ? loader.getModuleImportStack(module) : "";
+    for (let module of Cu.loadedModules()) {
+      modules[module] = collectStacks ? Cu.getModuleImportStack(module) : "";
     }
     let services = {};
     for (let contractID of Object.keys(Cc)) {
