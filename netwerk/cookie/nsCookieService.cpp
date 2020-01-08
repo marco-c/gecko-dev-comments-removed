e5aefe4dc911d1fc4335168c78aef325fb9d7308
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 16:19:21 +0000

    Bug 1502240 - Ensure that Content Blocking allow list is applied to all cookie policies r=baku
    Unfortunately we can't test BEHAVIOR_REJECT using the AntiTracking framework,
    because the AntiTracking callbacks are incompatible with it.  (The tracking
    callbacks expect to be able to unblock themselves, but under BEHAVIOR_REJECT,
    that can't happen.)
    
    Differential Revision: https://phabricator.services.mozilla.com/D10664
    
    UltraBlame original commit: bec76d96700206177d42ca13e789519bc7eb313a

--- netwerk/cookie/nsCookieService.cpp
+++ netwerk/cookie/nsCookieService.cpp
@@ -4284,7 +4284,10 @@ nsCookieService::CheckPrefs(nsICookiePermission    *aPermissionService,
   }
 
   
-  if (aCookieBehavior == nsICookieService::BEHAVIOR_REJECT) {
+  
+  
+  if (aCookieBehavior == nsICookieService::BEHAVIOR_REJECT &&
+      !aFirstPartyStorageAccessGranted) {
     COOKIE_LOGFAILURE(aCookieHeader ? SET_COOKIE : GET_COOKIE, aHostURI, aCookieHeader, "cookies are disabled");
     *aRejectedReason = nsIWebProgressListener::STATE_COOKIES_BLOCKED_ALL;
     return STATUS_REJECTED;
@@ -4292,8 +4295,6 @@ nsCookieService::CheckPrefs(nsICookiePermission    *aPermissionService,
 
   
   if (aIsForeign) {
-    
-    
     if (aCookieBehavior == nsICookieService::BEHAVIOR_REJECT_FOREIGN &&
         !aFirstPartyStorageAccessGranted) {
       COOKIE_LOGFAILURE(aCookieHeader ? SET_COOKIE : GET_COOKIE, aHostURI, aCookieHeader, "context is third party");
@@ -4301,19 +4302,12 @@ nsCookieService::CheckPrefs(nsICookiePermission    *aPermissionService,
       return STATUS_REJECTED;
     }
 
-    if (aCookieBehavior == nsICookieService::BEHAVIOR_LIMIT_FOREIGN) {
-      if (aNumOfCookies == 0) {
-        COOKIE_LOGFAILURE(aCookieHeader ? SET_COOKIE : GET_COOKIE, aHostURI, aCookieHeader, "context is third party");
-        *aRejectedReason = nsIWebProgressListener::STATE_COOKIES_BLOCKED_FOREIGN;
-        return STATUS_REJECTED;
-      }
+    if (aCookieBehavior == nsICookieService::BEHAVIOR_LIMIT_FOREIGN &&
+        !aFirstPartyStorageAccessGranted && aNumOfCookies == 0) {
+      COOKIE_LOGFAILURE(aCookieHeader ? SET_COOKIE : GET_COOKIE, aHostURI, aCookieHeader, "context is third party");
+      *aRejectedReason = nsIWebProgressListener::STATE_COOKIES_BLOCKED_FOREIGN;
+      return STATUS_REJECTED;
     }
-
-    MOZ_ASSERT(aCookieBehavior == nsICookieService::BEHAVIOR_ACCEPT ||
-               aCookieBehavior == nsICookieService::BEHAVIOR_LIMIT_FOREIGN ||
-               
-               aCookieBehavior == nsICookieService::BEHAVIOR_REJECT_FOREIGN ||
-               aCookieBehavior == nsICookieService::BEHAVIOR_REJECT_TRACKER);
 
     if (aThirdPartySession)
       return STATUS_ACCEPT_SESSION;
