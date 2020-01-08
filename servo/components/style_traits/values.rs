05507146d6172630b5cdb75a3a06cab63ee0107a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 23:28:42 +0000

    Bug 1519729 - Document public macros. r=emilio
    Undocumented public macros emit warnings in nightly-2019-01-11,
    and we #![deny] that warning.
    
    Cherry-picks a commit from https://github.com/servo/servo/pull/22674
    
    UltraBlame original commit: f63ebd7e9e281f22e71c268151337178de2c246a

--- servo/components/style_traits/values.rs
+++ servo/components/style_traits/values.rs
@@ -432,7 +432,7 @@ impl_to_css_for_predefined_type!(::cssparser::RGBA);
 impl_to_css_for_predefined_type!(::cssparser::Color);
 impl_to_css_for_predefined_type!(::cssparser::UnicodeRange);
 
-#[macro_export]
+
 macro_rules! define_css_keyword_enum {
     (pub enum $name:ident { $($variant:ident = $css:expr,)+ }) => {
         #[allow(missing_docs)]
