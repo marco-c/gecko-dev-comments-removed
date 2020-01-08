c3fc4ed0bbcb28ef404410d88cc00aac53ca37e6
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 11:35:15 +0000

    Bug 1490277 - use correct protocol for reading memory/table descriptors.  r=jseward
    The spec requires us not to use HasProperty because, Proxies.  Easy
    enough to use GetProperty; the spec is detailed about how to handle
    undefined values.
    
    UltraBlame original commit: df613c034e275ae90840d14b25492f14a87e73d6

--- js/src/jit-test/tests/wasm/regress/proxy-get-trap-table.js
+++ js/src/jit-test/tests/wasm/regress/proxy-get-trap-table.js
@@ -1,11 +1,14 @@
+
+
+
 assertErrorMessage(() => {
     var desc = {
         element: "anyfunc",
         initial: 1
     };
     var proxy = new Proxy({}, {
-        has: true
+        get: true
     });
     Object.setPrototypeOf(desc, proxy);
     let table = new WebAssembly.Table(desc);
-}, TypeError, /proxy handler's has trap/);
+}, TypeError, /proxy handler's get trap/);
