602f8347f7f3fd2802275480f6176760ad647fb1
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 16:25:37 +0000

    Bug 1376873 - Updates to dom/media/, dom/media/systemservices and dom/media/webrtc; r=pehrsons
    Differential Revision: https://phabricator.services.mozilla.com/D7438
    
    UltraBlame original commit: 19d9a30b300c4653088bffa1598c26f6f16c427d

--- dom/media/webrtc/MediaEnginePrefs.h
+++ dom/media/webrtc/MediaEnginePrefs.h
@@ -1,17 +1,17 @@
-
-
-
-
-
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=8 sts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #ifndef MediaEnginePrefs_h
 #define MediaEnginePrefs_h
 
 namespace mozilla {
 
-
-
-
+/**
+ * Video source and friends.
+ */
 class MediaEnginePrefs {
 public:
   static const int DEFAULT_VIDEO_FPS = 30;
@@ -26,6 +26,7 @@ public:
     , mFPS(0)
     , mFreq(0)
     , mAecOn(false)
+    , mUseAecMobile(false)
     , mAgcOn(false)
     , mNoiseOn(false)
     , mAec(0)
@@ -41,8 +42,9 @@ public:
   int32_t mWidth;
   int32_t mHeight;
   int32_t mFPS;
-  int32_t mFreq; 
+  int32_t mFreq; // for test tones (fake:true)
   bool mAecOn;
+  bool mUseAecMobile;
   bool mAgcOn;
   bool mNoiseOn;
   int32_t mAec;
@@ -59,7 +61,7 @@ public:
     return memcmp(this, &aRhs, sizeof(MediaEnginePrefs)) == 0;
   };
 
-  
+  // mWidth and/or mHeight may be zero (=adaptive default), so use functions.
 
   int32_t GetWidth(bool aHD = false) const {
     return mWidth? mWidth : (mHeight?
@@ -75,8 +77,8 @@ public:
 
 private:
   static int32_t GetDefWidth(bool aHD = false) {
-    
-    
+    // It'd be nice if we could use the ternary operator here, but we can't
+    // because of bug 1002729.
     if (aHD) {
       return DEFAULT_169_VIDEO_WIDTH;
     }
@@ -85,8 +87,8 @@ private:
   }
 
   static int32_t GetDefHeight(bool aHD = false) {
-    
-    
+    // It'd be nice if we could use the ternary operator here, but we can't
+    // because of bug 1002729.
     if (aHD) {
       return DEFAULT_169_VIDEO_HEIGHT;
     }
@@ -95,6 +97,6 @@ private:
   }
 };
 
-} 
+} // namespace mozilla
 
-#endif 
+#endif // MediaEnginePrefs_h
