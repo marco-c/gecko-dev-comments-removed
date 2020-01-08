8ab45041bfa1e95c3c41bff09a72ee506de141fa
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 03:31:45 +0000

    Bug 1462879 - PerformanceNavigationTiming must be notified correctly - part 1 - notify, r=smaug, r=valentin
    UltraBlame original commit: d36901e8058edb6f8fb3188f46c3b4fe8d61e722

--- layout/base/nsDocumentViewer.cpp
+++ layout/base/nsDocumentViewer.cpp
@@ -1170,6 +1170,11 @@ nsDocumentViewer::LoadComplete(nsresult aStatus)
       if (timing) {
         timing->NotifyLoadEventEnd();
       }
+
+      nsPIDOMWindowInner* innerWindow = window->GetCurrentInnerWindow();
+      if (innerWindow) {
+        innerWindow->QueuePerformanceNavigationTiming();
+      }
     }
   } else {
     
