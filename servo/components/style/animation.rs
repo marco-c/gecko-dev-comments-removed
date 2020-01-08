bb81bd61063c0f601f4332d12125cc2c960741cc
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 14:45:20 +0000

    Bug 1500260 - Remove unused expired boolean in Animation::Transition. r=emilio
    The last caller who used was #14418, which did fix a problem but introduced
    multiple. In particular, now transitions don't get expired ever, until they
    finish running of course.
    
    That is not ok, given you can have something that the user can trigger to change
    the style (hi, :hover, for example), and right now that triggers new
    transitions, getting this into a really funny state.
    
    I should give fixing this a shot, but it's non-trivial at all.
    
    This cherry-picks part of servo/servo#20757.
    
    UltraBlame original commit: 6d0c84abdfa1d1b5e64e78a1826762e110c7ea57

--- servo/components/style/animation.rs
+++ servo/components/style/animation.rs
@@ -214,9 +214,7 @@ pub enum Animation {
     
     
     
-    
-    
-    Transition(OpaqueNode, f64, AnimationFrame, bool),
+    Transition(OpaqueNode, f64, AnimationFrame),
     
     
     
@@ -232,19 +230,9 @@ pub enum Animation {
 impl Animation {
     
     #[inline]
-    pub fn mark_as_expired(&mut self) {
-        debug_assert!(!self.is_expired());
-        match *self {
-            Animation::Transition(_, _, _, ref mut expired) => *expired = true,
-            Animation::Keyframes(_, _, _, ref mut state) => state.expired = true,
-        }
-    }
-
-    
-    #[inline]
     pub fn is_expired(&self) -> bool {
         match *self {
-            Animation::Transition(_, _, _, expired) => expired,
+            Animation::Transition(..) => false,
             Animation::Keyframes(_, _, _, ref state) => state.expired,
         }
     }
@@ -253,7 +241,7 @@ impl Animation {
     #[inline]
     pub fn node(&self) -> &OpaqueNode {
         match *self {
-            Animation::Transition(ref node, _, _, _) => node,
+            Animation::Transition(ref node, _, _) => node,
             Animation::Keyframes(ref node, _, _, _) => node,
         }
     }
@@ -465,7 +453,6 @@ pub fn start_transitions_if_applicable(
                         duration: box_style.transition_duration_mod(i).seconds() as f64,
                         property_animation: property_animation,
                     },
-                     false,
                 )).unwrap();
 
             had_animations = true;
@@ -656,7 +643,7 @@ pub fn update_style_for_animation<E>(
     debug_assert!(!animation.is_expired());
 
     match *animation {
-        Animation::Transition(_, start_time, ref frame, _) => {
+        Animation::Transition(_, start_time, ref frame) => {
             debug!("update_style_for_animation: transition found");
             let now = context.timer.seconds();
             let mut new_style = (*style).clone();
@@ -868,7 +855,7 @@ pub fn complete_expired_transitions(
         if let Some(ref animations) = animations_to_expire {
             for animation in *animations {
                 
-                if let Animation::Transition(_, _, ref frame, _) = *animation {
+                if let Animation::Transition(_, _, ref frame) = *animation {
                     frame.property_animation.update(Arc::make_mut(style), 1.0);
                 }
             }
