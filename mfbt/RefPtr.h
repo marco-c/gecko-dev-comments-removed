fb0ceee361abd77ce54000a8f3ee4793285f746f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 10:00:14 +0000

    Bug 1489317: Part 1 - Add forward delcaration for mscom::AgileReference to RefPtr; r=froydnj
    I'd like to add a constructor and operator= to RefPtr for mscom::AgileReference.
    This patch is simply the forward declarations to allow for that.
    
    Differential Revision: https://phabricator.services.mozilla.com/D5317
    
    UltraBlame original commit: 0f8554d82b32a541f2bf7663887c15b9df1bcd76

--- mfbt/RefPtr.h
+++ mfbt/RefPtr.h
@@ -22,6 +22,11 @@ class nsISupports;
 namespace mozilla {
 template<class T> class OwningNonNull;
 template<class T> class StaticRefPtr;
+#if defined(XP_WIN)
+namespace mscom {
+class AgileReference;
+} 
+#endif 
 
 
 
@@ -152,6 +157,9 @@ public:
 
   MOZ_IMPLICIT RefPtr(const nsQueryReferent& aHelper);
   MOZ_IMPLICIT RefPtr(const nsCOMPtr_helper& aHelper);
+#if defined(XP_WIN)
+  MOZ_IMPLICIT RefPtr(const mozilla::mscom::AgileReference& aAgileRef);
+#endif 
 
   
   template<class U>
@@ -215,6 +223,9 @@ public:
 
   RefPtr<T>& operator=(const nsQueryReferent& aQueryReferent);
   RefPtr<T>& operator=(const nsCOMPtr_helper& aHelper);
+#if defined(XP_WIN)
+  RefPtr<T>& operator=(const mozilla::mscom::AgileReference& aAgileRef);
+#endif 
 
   RefPtr<T>&
   operator=(RefPtr<T> && aRefPtr)
