8f5c33d5a0fae8252097ef477f3a4bc316392b33
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 11:12:05 +0000

    Bug 1396642 - support smaller viewBox coordinates at the expense of larger ones r=dholbert
    UltraBlame original commit: dc6c04a63309d3461c0ca9dc23ae186a91deb8c6

--- layout/svg/nsSVGOuterSVGFrame.cpp
+++ layout/svg/nsSVGOuterSVGFrame.cpp
@@ -240,6 +240,9 @@ nsSVGOuterSVGFrame::GetIntrinsicRatio()
   
   
   
+  
+  
+  
 
   SVGSVGElement *content = static_cast<SVGSVGElement*>(GetContent());
   const nsSVGLength2& width =
@@ -248,8 +251,9 @@ nsSVGOuterSVGFrame::GetIntrinsicRatio()
     content->mLengthAttributes[SVGSVGElement::ATTR_HEIGHT];
 
   if (!width.IsPercentage() && !height.IsPercentage()) {
-    nsSize ratio(NSToCoordRoundWithClamp(width.GetAnimValue(content)),
-                 NSToCoordRoundWithClamp(height.GetAnimValue(content)));
+    nsSize ratio(
+      nsPresContext::CSSPixelsToAppUnits(width.GetAnimValue(content)),
+      nsPresContext::CSSPixelsToAppUnits(height.GetAnimValue(content)));
     if (ratio.width < 0) {
       ratio.width = 0;
     }
@@ -279,8 +283,8 @@ nsSVGOuterSVGFrame::GetIntrinsicRatio()
     if (viewBoxHeight < 0.0f) {
       viewBoxHeight = 0.0f;
     }
-    return nsSize(NSToCoordRoundWithClamp(viewBoxWidth),
-                  NSToCoordRoundWithClamp(viewBoxHeight));
+    return nsSize(nsPresContext::CSSPixelsToAppUnits(viewBoxWidth),
+                  nsPresContext::CSSPixelsToAppUnits(viewBoxHeight));
   }
 
   return nsSVGDisplayContainerFrame::GetIntrinsicRatio();
