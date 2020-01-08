a1a1a106acafe75ce938c5f68fb0bb12658e4586
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 10:58:03 +0000

    Bug 1314057 - Remove new-debugger-frontend flag from framework devtool when not defaulted to true r=davidwalsh
    UltraBlame original commit: 2ba943d7458dba6a080fe8e09654d03bcfb8d566

--- devtools/client/framework/test/browser_browser_toolbox_debugger.js
+++ devtools/client/framework/test/browser_browser_toolbox_debugger.js
@@ -19,6 +19,7 @@ const { fetch } = require("devtools/shared/DevToolsUtils");
 
 const debuggerHeadURL = CHROME_URL_ROOT + "../../debugger/new/test/mochitest/head.js";
 const helpersURL = CHROME_URL_ROOT + "../../debugger/new/test/mochitest/helpers.js";
+const helpersContextURL = CHROME_URL_ROOT + "../../debugger/new/test/mochitest/helpers/context.js";
 const testScriptURL = CHROME_URL_ROOT + "test_browser_toolbox_debugger.js";
 
 add_task(async function runTest() {
@@ -126,12 +127,13 @@ add_task(async function runTest() {
 
   
   
-  debuggerHead = debuggerHead.replace(/Services.scriptloader.loadSubScript[^\)]*\);/g, "");
+  const { content: debuggerHelpers } = await fetch(helpersURL);
+  const { content: debuggerContextHelpers } = await fetch(helpersContextURL);
+  debuggerHead = debuggerHead + debuggerContextHelpers + debuggerHelpers;
 
   
   
-  const { content: debuggerHelpers } = await fetch(helpersURL);
-  debuggerHead = debuggerHead + debuggerHelpers;
+  debuggerHead = debuggerHead.replace(/Services.scriptloader.loadSubScript[^\)]*\);/g, "");
 
   
   
