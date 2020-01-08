92053f9e12df9afa7faafcbd88b0997dbb7ae343
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 12:43:00 +0000

    Bug 1492482 - Remove CPOW usage from browser_urlbarHashChangeProxyState.js r=Gijs
    Depends on D6952
    
    Differential Revision: https://phabricator.services.mozilla.com/D6953
    
    UltraBlame original commit: 159f859490af5faf83fd8d967f22af51e9b4f88f

--- browser/base/content/test/urlbar/browser_urlbarHashChangeProxyState.js
+++ browser/base/content/test/urlbar/browser_urlbarHashChangeProxyState.js
@@ -89,10 +89,9 @@ add_task(async function() {
 
 
 add_task(async function() {
-  let tab = await BrowserTestUtils.openNewForegroundTab(gBrowser, "about:newtab", false);
   
-  
-  await BrowserTestUtils.waitForCondition(() => !tab.linkedBrowser.contentDocumentAsCPOW.hidden);
+  gBrowser.removePreloadedBrowser();
+  let tab = await BrowserTestUtils.openNewForegroundTab(gBrowser, "about:newtab");
   let url = "https://example.org/browser/browser/base/content/test/urlbar/dummy_page.html#foo";
   gURLBar.value = url;
   gURLBar.select();
