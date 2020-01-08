34d09a69c02e89c29e09237c1650636e449b3198
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 18:48:00 +0000

    Bug 1508898 - Prepare the Linux sandbox's socketcall/ipc-call dispatch table for reformatting. r=gcp
    The tables in SandboxFilterUtil.cpp should remain vertically aligned,
    but clang-format would disagree.  This patch excludes that region from
    reformatting, and applies the other changes that clang-format would make
    there.
    
    Differential Revision: https://phabricator.services.mozilla.com/D12499
    
    UltraBlame original commit: 023c546c01cd2d3c4d382ab5e9245a48f138e934

--- security/sandbox/linux/SandboxFilterUtil.cpp
+++ security/sandbox/linux/SandboxFilterUtil.cpp
@@ -63,9 +63,10 @@ SandboxPolicyBase::EvaluateSyscall(int aSysno) const {
     }
 #endif 
 #endif 
-#define DISPATCH_SOCKETCALL(sysnum, socketnum)                       \
-    case sysnum:                                                     \
-      return EvaluateSocketCall(socketnum, true).valueOr(InvalidSyscall())
+
+#define DISPATCH_SOCKETCALL(sysnum, socketnum) \
+  case sysnum:                                 \
+    return EvaluateSocketCall(socketnum, true).valueOr(InvalidSyscall())
 #ifdef __NR_socket
       DISPATCH_SOCKETCALL(__NR_socket,      SYS_SOCKET);
       DISPATCH_SOCKETCALL(__NR_bind,        SYS_BIND);
@@ -80,7 +81,7 @@ SandboxPolicyBase::EvaluateSyscall(int aSysno) const {
 #ifdef __NR_send
       DISPATCH_SOCKETCALL(__NR_send,        SYS_SEND);
       DISPATCH_SOCKETCALL(__NR_recv,        SYS_RECV);
-#endif 
+#endif  
       DISPATCH_SOCKETCALL(__NR_sendto,      SYS_SENDTO);
       DISPATCH_SOCKETCALL(__NR_recvfrom,    SYS_RECVFROM);
       DISPATCH_SOCKETCALL(__NR_shutdown,    SYS_SHUTDOWN);
@@ -91,13 +92,13 @@ SandboxPolicyBase::EvaluateSyscall(int aSysno) const {
       DISPATCH_SOCKETCALL(__NR_accept4,     SYS_ACCEPT4);
       DISPATCH_SOCKETCALL(__NR_recvmmsg,    SYS_RECVMMSG);
       DISPATCH_SOCKETCALL(__NR_sendmmsg,    SYS_SENDMMSG);
-#endif 
+#endif  
 #undef DISPATCH_SOCKETCALL
 #ifndef __NR_socketcall
 #ifndef ANDROID
-#define DISPATCH_SYSVCALL(sysnum, ipcnum)         \
-    case sysnum:                                  \
-      return EvaluateIpcCall(ipcnum).valueOr(InvalidSyscall())
+#define DISPATCH_SYSVCALL(sysnum, ipcnum) \
+  case sysnum:                            \
+    return EvaluateIpcCall(ipcnum).valueOr(InvalidSyscall())
       DISPATCH_SYSVCALL(__NR_semop,       SEMOP);
       DISPATCH_SYSVCALL(__NR_semget,      SEMGET);
       DISPATCH_SYSVCALL(__NR_semctl,      SEMCTL);
@@ -111,8 +112,9 @@ SandboxPolicyBase::EvaluateSyscall(int aSysno) const {
       DISPATCH_SYSVCALL(__NR_shmget,      SHMGET);
       DISPATCH_SYSVCALL(__NR_shmctl,      SHMCTL);
 #undef DISPATCH_SYSVCALL
-#endif 
-#endif 
+#endif  
+#endif  
+
   default:
     return InvalidSyscall();
   }
