c8e748fc42348a90f2f56fd6f7cb406e1c9c568f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 21:24:36 +0000

    Bug 1514744 - Update webrender to commit 3a4ce4b66a7bc9bd10773744d20530c609a61e90 (WR PR #3424). r=kats
    https://github.com/servo/webrender/pull/3424
    
    Differential Revision: https://phabricator.services.mozilla.com/D14743
    
    UltraBlame original commit: cb5149cb7718033d55bbb5a8acda1d26967d1243

--- gfx/wr/webrender/src/picture.rs
+++ gfx/wr/webrender/src/picture.rs
@@ -732,13 +732,23 @@ impl TileCache {
 
         let prim_data = &resources.as_common_data(&prim_instance);
 
-        let prim_rect = LayoutRect::new(
-            prim_instance.prim_origin,
-            prim_data.prim_size,
-        );
-        let clip_rect = prim_data
-            .prim_relative_clip_rect
-            .translate(&prim_instance.prim_origin.to_vector());
+        let (prim_rect, clip_rect) = match prim_instance.kind {
+            PrimitiveInstanceKind::Picture { pic_index, .. } => {
+                let pic = &pictures[pic_index.0];
+                (pic.local_rect, LayoutRect::max_rect())
+            }
+            _ => {
+                let prim_rect = LayoutRect::new(
+                    prim_instance.prim_origin,
+                    prim_data.prim_size,
+                );
+                let clip_rect = prim_data
+                    .prim_relative_clip_rect
+                    .translate(&prim_instance.prim_origin.to_vector());
+
+                (prim_rect, clip_rect)
+            }
+        };
 
         
         
@@ -850,7 +860,7 @@ impl TileCache {
             
             
             
-            if clip_chain_node.spatial_node_index > surface_spatial_node_index {
+            if clip_chain_node.spatial_node_index >= surface_spatial_node_index {
                 clip_chain_spatial_nodes.push(clip_chain_node.spatial_node_index);
                 clip_chain_uids.push(clip_chain_node.handle.uid());
             }
