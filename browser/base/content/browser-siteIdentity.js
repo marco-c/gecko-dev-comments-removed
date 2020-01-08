81770b59b77ec419a50e05cf2262f1e9d7c1bf51
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 23:45:55 +0000

    Backed out changeset 1e7c60e83b86 (bug 1497827) for perma failing at testing/firefox-ui/tests/functional/security/test_ev_certificate.py. CLOSED TREE
    UltraBlame original commit: a56825e2ea2c5b1a2ffb15c813d5d699475f98dc

--- browser/base/content/browser-siteIdentity.js
+++ browser/base/content/browser-siteIdentity.js
@@ -760,7 +760,7 @@ var gIdentityHandler = {
     
     if (this._isEV) {
       let iData = this.getIdentityData();
-      owner = iData.subjectOrg;
+      host = owner = iData.subjectOrg;
       verifier = this._identityIconLabels.tooltipText;
 
       
