778e0e44fc4eec2156c8dd7361e981c64e932807
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 14:44:04 +0000

    Bug 1435871 - Open prefs from Payment Request in a tab. r=jaws
    Differential Revision: https://phabricator.services.mozilla.com/D8497
    
    UltraBlame original commit: 0f7592b662e27238b8ebc442fa755e5e891cdef9

--- browser/components/payments/res/containers/payment-dialog.js
+++ browser/components/payments/res/containers/payment-dialog.js
@@ -385,7 +385,7 @@ export default class PaymentDialog extends PaymentStateSubscriberMixin(HTMLEleme
       let link = manageTextEl.querySelector("a");
       
       
-      link.href = "about:preferences#privacy-address-autofill";
+      link.href = "about:preferences#privacy-form-autofill";
     }
 
     this._renderPayButton(state);
