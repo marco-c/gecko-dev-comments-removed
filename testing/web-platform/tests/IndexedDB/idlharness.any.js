7219eca180a3cb0bc484d8e46ec46a3dae8e79ca
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 02:06:15 +0000

    Bug 1475440 [wpt PR 11958] - Update the IndexedDB tests to cover all workers, a=testonly
    Automatic update from web-platform-testsUpdate the IndexedDB tests to cover all workers (#11958)
    
    --
    
    wpt-commits: ceba83a56edd838cca0aacd35549f979292a5995
    wpt-pr: 11958
    
    UltraBlame original commit: ca5a8041e097bb764fc287b2dc8ab1b36ffca256

---
+++ testing/web-platform/tests/IndexedDB/idlharness.any.js
@@ -0,0 +1,25 @@
+
+
+
+
+idl_test(
+  ['IndexedDB'],
+  ['html', 'dom'],
+  idl_array => {
+    idl_array.add_objects({
+      IDBCursor: [],
+      IDBCursorWithValue: [],
+      IDBDatabase: [],
+      IDBFactory: [self.indexedDB],
+      IDBIndex: [],
+      IDBKeyRange: [IDBKeyRange.only(0)],
+      IDBObjectStore: [],
+      IDBOpenDBRequest: [],
+      IDBRequest: [],
+      IDBTransaction: [],
+      IDBVersionChangeEvent: ['new IDBVersionChangeEvent("type")'],
+      DOMStringList: [],
+    });
+  },
+  'IndexedDB interfaces'
+);
