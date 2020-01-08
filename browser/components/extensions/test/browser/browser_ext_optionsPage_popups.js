f3040db251dcfd6c4b52782a1bf2d30bccaa65f1
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 23:02:21 +0000

    Bug 1468460 - Fix and test extension menus and menus.overrideContext on options_ui pages. r=mixedpuppy,robwu
    Depends on D9920
    
    Differential Revision: https://phabricator.services.mozilla.com/D15225
    
    UltraBlame original commit: dd5395e679a627a326a3a8b305c3f5f44329ddfa

--- browser/components/extensions/test/browser/browser_ext_optionsPage_popups.js
+++ browser/components/extensions/test/browser/browser_ext_optionsPage_popups.js
@@ -2,20 +2,57 @@
 
 "use strict";
 
+async function openContextMenuInOptionsPage(optionsBrowser) {
+  let contentAreaContextMenu = document.getElementById("contentAreaContextMenu");
+  let popupShownPromise = BrowserTestUtils.waitForEvent(contentAreaContextMenu, "popupshown");
+
+  await BrowserTestUtils.waitForCondition(async () => {
+    BrowserTestUtils.synthesizeMouseAtCenter("a", {type: "contextmenu"}, optionsBrowser);
+
+    
+    
+    
+    
+    return Promise.race([
+      popupShownPromise.then(() => true),
+      delay(500).then(() => false),
+    ]);
+  }, "Waiting the context menu to be shown");
+
+  return contentAreaContextMenu;
+}
+
+function contextMenuClosed(contextMenu) {
+  
+  
+  return BrowserTestUtils.waitForCondition(async () => {
+    await closeContextMenu(contextMenu);
+    return contextMenu.state === "closed";
+  }, "Wait context menu popup to be closed");
+}
+
 add_task(async function test_tab_options_popups() {
-  function backgroundScript() {
+  async function backgroundScript() {
+    browser.menus.onShown.addListener(info => {
+      browser.test.sendMessage("extension-menus-onShown", info);
+    });
+
+    await browser.menus.create({id: "sidebaronly", title: "sidebaronly", viewTypes: ["sidebar"]});
+    await browser.menus.create({id: "tabonly", title: "tabonly", viewTypes: ["tab"]});
+    await browser.menus.create({id: "anypage", title: "anypage"});
+
     browser.runtime.openOptionsPage();
   }
 
   function optionsScript() {
-    browser.test.sendMessage("options-page:loaded");
+    browser.test.sendMessage("options-page:loaded", document.documentURI);
   }
 
   let extension = ExtensionTestUtils.loadExtension({
     useAddonManager: "temporary",
 
     manifest: {
-      "permissions": ["tabs"],
+      "permissions": ["tabs", "menus"],
       "options_ui": {
         "page": "options.html",
       },
@@ -41,25 +78,11 @@ add_task(async function test_tab_options_popups() {
 
   await extension.startup();
 
-  
-  await extension.awaitMessage("options-page:loaded");
+  const pageUrl = await extension.awaitMessage("options-page:loaded");
 
   const optionsBrowser = gBrowser.selectedBrowser.contentDocument.getElementById("addon-options");
-  let contentAreaContextMenu = document.getElementById("contentAreaContextMenu");
-  let popupShownPromise = BrowserTestUtils.waitForEvent(contentAreaContextMenu, "popupshown");
 
-  await BrowserTestUtils.waitForCondition(async () => {
-    BrowserTestUtils.synthesizeMouseAtCenter("a", {type: "contextmenu"}, optionsBrowser);
-
-    
-    
-    
-    
-    return Promise.race([
-      popupShownPromise.then(() => true),
-      delay(500).then(() => false),
-    ]);
-  }, "Waiting the context menu to be shown");
+  const contentAreaContextMenu = await openContextMenuInOptionsPage(optionsBrowser);
 
   let contextMenuItemIds = [
     "context-openlinkintab",
@@ -75,14 +98,97 @@ add_task(async function test_tab_options_popups() {
     ok(!item.disabled, `${itemID} should not be disabled`);
   }
 
-  
-  
-  await BrowserTestUtils.waitForCondition(async () => {
-    let popupHiddenPromise = BrowserTestUtils.waitForEvent(contentAreaContextMenu, "popuphidden");
-    contentAreaContextMenu.hidePopup();
-    await popupHiddenPromise;
-    return contentAreaContextMenu.state === "closed";
-  }, "Wait context menu popup to be closed");
+  const menuDetails = await extension.awaitMessage("extension-menus-onShown");
+
+  isnot(menuDetails.targetElementId, undefined, "Got a targetElementId in the menu details");
+  delete menuDetails.targetElementId;
+
+
+  Assert.deepEqual(menuDetails, {
+    menuIds: ["anypage"],
+    contexts: ["link", "all"],
+    viewType: undefined,
+    frameId: 0,
+    editable: false,
+    linkText: "options page link",
+    linkUrl: "http://mochi.test:8888/",
+    pageUrl,
+  }, "Got the expected menu details from menus.onShown");
+
+  await contextMenuClosed(contentAreaContextMenu);
+
+  BrowserTestUtils.removeTab(aboutAddonsTab);
+
+  await extension.unload();
+});
+
+add_task(async function overrideContext_in_options_page() {
+  function optionsScript() {
+    document.addEventListener("contextmenu", () => {
+      browser.menus.overrideContext({});
+      browser.test.sendMessage("contextmenu-overridden");
+    }, {once: true});
+    browser.test.sendMessage("options-page:loaded");
+  }
+
+  let extension = ExtensionTestUtils.loadExtension({
+    useAddonManager: "temporary",
+    manifest: {
+      "permissions": ["tabs", "menus", "menus.overrideContext"],
+      "options_ui": {
+        "page": "options.html",
+      },
+    },
+    files: {
+      "options.html": `<!DOCTYPE html>
+        <html>
+          <head>
+            <meta charset="utf-8">
+            <script src="options.js" type="text/javascript"></script>
+          </head>
+          <body style="height: 100px;">
+            <h1>Extensions Options</h1>
+            <a href="http://mochi.test:8888/">options page link</a>
+          </body>
+        </html>`,
+      "options.js": optionsScript,
+    },
+    async background() {
+      
+      await new Promise(resolve => {
+        browser.menus.create({id: "bg_1_1", title: "bg_1_1"});
+        browser.menus.create({id: "bg_1_2", title: "bg_1_2"});
+        
+        browser.menus.create(
+          {id: "bg_1_3", title: "bg_1_3", targetUrlPatterns: ["*://nomatch/*"]},
+          
+          
+          resolve);
+      });
+      browser.runtime.openOptionsPage();
+    },
+  });
+
+  const aboutAddonsTab = await BrowserTestUtils.openNewForegroundTab(gBrowser, "about:addons");
+
+  await extension.startup();
+  await extension.awaitMessage("options-page:loaded");
+
+  const optionsBrowser = gBrowser.selectedBrowser.contentDocument.getElementById("addon-options");
+  const contentAreaContextMenu = await openContextMenuInOptionsPage(optionsBrowser);
+
+  await extension.awaitMessage("contextmenu-overridden");
+
+  const allVisibleMenuItems = Array.from(contentAreaContextMenu.children).filter(elem => {
+    return !elem.hidden;
+  }).map(elem => elem.id);
+
+  Assert.deepEqual(allVisibleMenuItems, [
+    `${makeWidgetId(extension.id)}-menuitem-_bg_1_1`,
+    `${makeWidgetId(extension.id)}-menuitem-_bg_1_2`,
+  ], "Expected only extension menu items");
+
+  await contextMenuClosed(contentAreaContextMenu);
 
   BrowserTestUtils.removeTab(aboutAddonsTab);
 
