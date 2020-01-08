a6c12a2f3cff3d448a6202643d2db49ff3348519
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 07:35:45 +0000

    Bug 1483487 - Fix static analysis bustage and add missing comments on a CLOSED TREE r=me
    UltraBlame original commit: d2dd172726371ade8959495deb5c1cf60dc7fa29

--- dom/bindings/DOMJSProxyHandler.cpp
+++ dom/bindings/DOMJSProxyHandler.cpp
@@ -70,13 +70,16 @@ struct SetDOMProxyInformation
 SetDOMProxyInformation gSetDOMProxyInformation;
 
 static inline void
-CheckExpandoObject(JSObject* proxy, JS::Value expando)
+CheckExpandoObject(JSObject* proxy, const JS::Value& expando)
 {
 #ifdef DEBUG
   JSObject* obj = &expando.toObject();
   MOZ_ASSERT(!js::gc::EdgeNeedsSweepUnbarriered(&obj));
   MOZ_ASSERT(js::GetObjectCompartment(proxy) == js::GetObjectCompartment(obj));
 
+  
+  
+  
   nsISupports* native = UnwrapDOMObject<nsISupports>(proxy);
   nsWrapperCache* cache;
   CallQueryInterface(native, &cache);
