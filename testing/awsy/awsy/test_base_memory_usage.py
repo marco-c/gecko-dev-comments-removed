c57ac3b8f3728df715b058648b957d51ae4ef5ec
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 00:03:06 +0000

    Bug 1474208: Use median of all process values for AWSY base content. r=erahm
    MozReview-Commit-ID: DBiJjeTlyZE
    
    UltraBlame original commit: 8504c0085847c091aec4268699c6eeb23356c58c

--- testing/awsy/awsy/test_base_memory_usage.py
+++ testing/awsy/awsy/test_base_memory_usage.py
@@ -17,7 +17,7 @@ CHECKPOINTS = [
         'name': "After tabs open [+30s, forced GC]",
         'path': "memory-report-TabsOpenForceGC-4.json.gz",
         'name_filter': 'Web Content', 
-        'count': 1 
+        'median': True, 
     },
 ]
 
