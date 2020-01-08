4f340cabe2d35e27fe372c5ef4dc7dd33fd87064
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 21:21:28 +0000

    Bug 1500864 - Cull items within opacity:0 containers when merging with retained display lists. r=mstange
    Differential Revision: https://phabricator.services.mozilla.com/D14304
    
    UltraBlame original commit: 0f2d638c5c8fc345174855e1977f0735b7310d32

--- layout/painting/RetainedDisplayListBuilder.h
+++ layout/painting/RetainedDisplayListBuilder.h
@@ -95,7 +95,8 @@ RetainedDisplayListData* GetOrSetRetainedDisplayListData(nsIFrame* aRootFrame);
 struct RetainedDisplayListBuilder {
   RetainedDisplayListBuilder(nsIFrame* aReferenceFrame,
                              nsDisplayListBuilderMode aMode, bool aBuildCaret)
-      : mBuilder(aReferenceFrame, aMode, aBuildCaret, true) {}
+      : mBuilder(aReferenceFrame, aMode, aBuildCaret, true),
+        mCurrentSubtreeIsForEventsAndPluginsOnly(false) {}
   ~RetainedDisplayListBuilder() { mList.DeleteAll(&mBuilder); }
 
   nsDisplayListBuilder* Builder() { return &mBuilder; }
@@ -122,11 +123,20 @@ struct RetainedDisplayListBuilder {
                              AnimatedGeometryRoot* aAGR,
                              uint32_t aCallerKey = 0,
                              uint32_t aNestingDepth = 0);
+
+  
+
+
+
+
+
+
+
   bool MergeDisplayLists(
       nsDisplayList* aNewList, RetainedDisplayList* aOldList,
       RetainedDisplayList* aOutList,
       mozilla::Maybe<const mozilla::ActiveScrolledRoot*>& aOutContainerASR,
-      uint32_t aOuterKey = 0);
+      nsDisplayItem* aOuterItem = nullptr);
 
   bool ComputeRebuildRegion(nsTArray<nsIFrame*>& aModifiedFrames,
                             nsRect* aOutDirty,
@@ -145,6 +155,10 @@ struct RetainedDisplayListBuilder {
   nsDisplayListBuilder mBuilder;
   RetainedDisplayList mList;
   WeakFrame mPreviousCaret;
+
+  
+  
+  bool mCurrentSubtreeIsForEventsAndPluginsOnly;
 };
 
 #endif  
