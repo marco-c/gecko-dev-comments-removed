f7e9e763f388570c78f1c84617b124624da5a27f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 17:55:05 +0000

    Bug 1505173 [wpt PR 13955] - [Background Fetch] Reject call to updateUI if event is inactive., a=testonly
    Automatic update from web-platform-tests[Background Fetch] Reject call to updateUI if event is inactive.
    
    Spec:
    https://wicg.github.io/background-fetch/#background-fetch-update-ui-event-update-ui
    
    Bug: 901909
    Change-Id: Ia319cd03e41e65c5a6a1f5e61cde6f4ae7705a32
    Reviewed-on: https://chromium-review.googlesource.com/c/1320330
    Commit-Queue: Rayan Kanso <rayankanschromium.org>
    Reviewed-by: Peter Beverloo <peterchromium.org>
    Reviewed-by: Matt Falkenhagen <falkenchromium.org>
    Reviewed-by: Hiroki Nakagawa <nhirokichromium.org>
    Cr-Commit-Position: refs/heads/master{#606118}
    
    --
    
    wpt-commits: 8762072fac59630a7dd6e024dc7ceab71e86ffcc
    wpt-pr: 13955
    
    UltraBlame original commit: dc170bbb9a031992abb0b720a9be9d9034a8b5b7

--- testing/web-platform/tests/background-fetch/service_workers/sw-update-ui.js
+++ testing/web-platform/tests/background-fetch/service_workers/sw-update-ui.js
@@ -1,3 +1,4 @@
+importScripts('/resources/testharness.js');
 importScripts('sw-helpers.js');
 
 async function updateUI(event) {
@@ -13,10 +14,20 @@ async function updateUI(event) {
 
   return Promise.all(updateParams.map(param => event.updateUI(param)))
            .then(() => 'update success')
-           .catch(e => e.message);
+           .catch(e => e.name);
 }
 
 self.addEventListener('backgroundfetchsuccess', event => {
+  if (event.registration.id === 'update-inactive') {
+    
+    
+    
+    new Promise(r => step_timeout(r, 0))
+        .then(() => event.updateUI({ title: 'New title' }))
+        .catch(e => sendMessageToDocument({ update: e.name }));
+    return;
+  }
+
   event.waitUntil(updateUI(event)
-      .then(update => sendMessageToDocument({ type: event.type, update })))
+      .then(update => sendMessageToDocument({ update })));
 });
