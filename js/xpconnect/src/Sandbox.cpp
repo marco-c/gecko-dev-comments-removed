9ee1751feacd7c6cd6e5c3a32992a0860ac09adf
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 22:33:19 +0000

    Bug 1517434 part 3.  Get rid of pointer compares to nsXPConnect::SystemPrincipal.  r=mccr8
    Differential Revision: https://phabricator.services.mozilla.com/D15674
    
    UltraBlame original commit: 1853f95c94b101e7e24a180f6eacaf2a628e5863

--- js/xpconnect/src/Sandbox.cpp
+++ js/xpconnect/src/Sandbox.cpp
@@ -64,6 +64,7 @@
 #include "mozilla/dom/XMLHttpRequest.h"
 #include "mozilla/dom/XMLSerializerBinding.h"
 #include "mozilla/dom/FormDataBinding.h"
+#include "mozilla/BasePrincipal.h"
 #include "mozilla/DeferredFinalize.h"
 #include "mozilla/NullPrincipal.h"
 
@@ -1045,7 +1046,7 @@ nsresult xpc::CreateSandboxObject(JSContext* cx, MutableHandleValue vp,
   
   
 
-  bool isSystemPrincipal = principal == nsXPConnect::SystemPrincipal();
+  bool isSystemPrincipal = principal->IsSystemPrincipal();
   if (isSystemPrincipal) {
     creationOptions.setClampAndJitterTime(false);
   }
@@ -1171,7 +1172,7 @@ nsresult xpc::CreateSandboxObject(JSContext* cx, MutableHandleValue vp,
       }
     }
 
-    bool allowComponents = principal == nsXPConnect::SystemPrincipal();
+    bool allowComponents = principal->IsSystemPrincipal();
     if (options.wantComponents && allowComponents &&
         !ObjectScope(sandbox)->AttachComponentsObject(cx))
       return NS_ERROR_XPC_UNEXPECTED;
@@ -1348,7 +1349,6 @@ static bool GetExpandedPrincipal(JSContext* cx, HandleObject arrayObj,
       return false;
     }
 
-    nsresult rv;
     nsCOMPtr<nsIPrincipal> principal;
     if (allowed.isObject()) {
       
@@ -1381,10 +1381,7 @@ static bool GetExpandedPrincipal(JSContext* cx, HandleObject arrayObj,
       }
 
       
-      bool isSystem;
-      rv = nsXPConnect::SecurityManager()->IsSystemPrincipal(principal,
-                                                             &isSystem);
-      NS_ENSURE_SUCCESS(rv, false);
+      bool isSystem = principal->IsSystemPrincipal();
       if (isSystem) {
         JS_ReportErrorASCII(
             cx, "System principal is not allowed in an expanded principal");
