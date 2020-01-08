21ab7328ef836ceb7bd882669eb2d7ed5d83fa80
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 16:51:21 +0000

    Bug 1501722: Check for initial environment in GetGeneratorForFrame r=jorendorff
    Differential Revision: https://phabricator.services.mozilla.com/D10598
    
    UltraBlame original commit: fd8edda85657acccb13ed11cf5289fdbafd4ab8b

---
+++ js/src/jit-test/tests/generators/bug1501722.js
@@ -0,0 +1,9 @@
+
+
+(function () {
+    g = newGlobal();
+    g.parent = this;
+    g.eval("(function() { var dbg = Debugger(parent); dbg.onEnterFrame = function() {} } )")
+    ``;
+    oomTest(async function() {}, {expectExceptionOnFailure: false});
+})()
