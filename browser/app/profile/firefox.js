b55852a1c3a9d14716db69b628056dd825a6dbab
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 08:56:05 +0000

    Bug 1480144: Let Windows Restart Manager ride the trains r=jmathies
    The plan is to study the "restart manager" in 62 with a pref flip,
    and if that goes well enable it on release in 63. If it is to be
    enabled for the whole population in 63 it needs to ride the trains,
    so we'll let it be enabled by default as 63 goes to beta. There will
    be time to uplift a backout so it won't reach the release population,
    if the 62 study indicates problems.
    
    Differential Revision: https://phabricator.services.mozilla.com/D2620
    
    UltraBlame original commit: fafc1900d5dfeb7e94d7a39982d0603fd3c8f4fb

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1262,11 +1262,7 @@ pref("toolkit.startup.max_resumed_crashes", 3);
 
 
 #if defined(XP_WIN)
-#if defined(NIGHTLY_BUILD)
 pref("toolkit.winRegisterApplicationRestart", true);
-#else
-pref("toolkit.winRegisterApplicationRestart", false);
-#endif
 #endif
 
 
