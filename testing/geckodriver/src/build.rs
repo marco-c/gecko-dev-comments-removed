d98ad8c08763e3f2a505f3a0964847f9d23f60fe
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 07:47:42 +0000

    Bug 1396821 - [geckodriver] Switch geckodriver crate from rustc_serialize to serde. r=ato
    MozReview-Commit-ID: B8H3bZQKkJ7
    
    UltraBlame original commit: 5c0ddd45f926a15d7d90f574f706fcc7f33d1e3b

--- testing/geckodriver/src/build.rs
+++ testing/geckodriver/src/build.rs
@@ -1,6 +1,5 @@
+use serde_json::Value;
 use std::fmt;
-
-use rustc_serialize::json::{Json};
 
 include!(concat!(env!("OUT_DIR"), "/build-info.rs"));
 
@@ -34,8 +33,8 @@ impl fmt::Display for BuildInfo {
 
 
 
-impl Into<Json> for BuildInfo {
-    fn into(self) -> Json {
-        Json::String(BuildInfo::version().to_string())
+impl Into<Value> for BuildInfo {
+    fn into(self) -> Value {
+        Value::String(BuildInfo::version().to_string())
     }
 }
