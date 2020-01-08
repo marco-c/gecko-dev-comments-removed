24eba9348dcd16c24e9a255b4ff2d438dc2ab3f8
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 16:35:41 +0000

    Bug 1504080 - Add Nursery::stats() r=jonco
    Add a new function for a commonly accesses structure.
    
    UltraBlame original commit: 881a67c2941279c07463c782d5aa65e49e96d281

--- js/src/gc/Nursery.h
+++ js/src/gc/Nursery.h
@@ -531,6 +531,7 @@ class Nursery
     uintptr_t position() const { return position_; }
 
     JSRuntime* runtime() const { return runtime_; }
+    gcstats::Statistics& stats() const;
 
     
     void* allocate(size_t size);
