29aa404c371171258f96211502da907306bc3419
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 02:31:41 +0000

    Bug 1476605 - Fix Error rep failure when stacktrace is a longString; r=sole.
    The actual fix for this issue will come in a future
    reps bundle when https://github.com/devtools-html/debugger.html/pull/6705
    gets merged.
    But, I do want this change to be uplifted to release, and it should be
    easier to only apply this simple change than the whole next release
    which would contain unrelated code changes.
    Also, this patch introduces a test to make sure we don't ever regress
    this.
    
    MozReview-Commit-ID: 3QPrx3TLln0
    
    UltraBlame original commit: 98a2048f6d50a1a3f1ee7f76011d78622bb3e9b7

--- devtools/client/shared/components/reps/reps.js
+++ devtools/client/shared/components/reps/reps.js
@@ -1978,7 +1978,7 @@ function getStacktraceElements(props, preview) {
     
     
     const locationParts = location.match(/^(.*):(\d+):(\d+)$/);
-    if (props.onViewSourceInDebugger && location && !IGNORED_SOURCE_URLS.includes(locationParts[1]) && locationParts) {
+    if (props.onViewSourceInDebugger && location && locationParts && !IGNORED_SOURCE_URLS.includes(locationParts[1])) {
       const [, url, line, column] = locationParts;
       onLocationClick = e => {
         
