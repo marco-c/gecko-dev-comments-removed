9292ec01cf99d89239645ef4ed8687c205e82938
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 16:32:37 +0000

    Bug 1491672 - Migrate About Dialog to use Fluent for localization r=flod,zbraniecki,jaws
    Differential Revision: https://phabricator.services.mozilla.com/D7206
    
    UltraBlame original commit: 66b802c51088636ca0d90116a141a18b54cd3cb2

--- browser/base/content/aboutDialog.js
+++ browser/base/content/aboutDialog.js
@@ -10,7 +10,7 @@
 ChromeUtils.import("resource://gre/modules/Services.jsm");
 ChromeUtils.import("resource://gre/modules/AppConstants.jsm");
 
-function init(aEvent) {
+async function init(aEvent) {
   if (aEvent.target != document)
     return;
 
@@ -50,11 +50,10 @@ function init(aEvent) {
   }
 
   
-  let bundle = Services.strings.createBundle("chrome://browser/locale/browser.properties");
   let archResource = Services.appinfo.is64Bit
-                     ? "aboutDialog.architecture.sixtyFourBit"
-                     : "aboutDialog.architecture.thirtyTwoBit";
-  let arch = bundle.GetStringFromName(archResource);
+                     ? "aboutDialog-architecture-sixtyFourBit"
+                     : "aboutDialog-architecture-thirtyTwoBit";
+  let [arch] = await document.l10n.formatValues([{id: archResource}]);
   versionField.textContent += ` (${arch})`;
 
   
