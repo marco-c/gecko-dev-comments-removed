4c6a5eb215e783bd8a846979474e7d8d3ccef51d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 13:12:02 +0000

    Bug 1494222: fix qcms_fuzzer coverage by enabling all path combinations. r=agaynor
    UltraBlame original commit: 25183982ae1bae54b2ffeacbcf178bea3ec9323d

--- gfx/qcms/fuzztest/qcms_fuzzer.cpp
+++ gfx/qcms/fuzztest/qcms_fuzzer.cpp
@@ -39,7 +39,7 @@ transform(qcms_profile* src_profile, qcms_profile* dst_profile, size_t size)
   qcms_intent intent = qcms_profile_get_rendering_intent(src_profile);
   
   
-  if (size % 0x10) {
+  if (size % 15) {
     qcms_profile_precache_output_transform(dst_profile);
   }
 
