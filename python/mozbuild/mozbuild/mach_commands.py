4d86919965faa0edd174c06fa88f9344d098cb15
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 21:19:38 +0000

    Bug 1514341 - mach clang-format - fix path on windows. r=sylvestre
    Differential Revision: https://phabricator.services.mozilla.com/D14605
    
    UltraBlame original commit: 0b5954be5426443fb44de34e43da0a12e46252d7

--- python/mozbuild/mozbuild/mach_commands.py
+++ python/mozbuild/mozbuild/mach_commands.py
@@ -9,6 +9,7 @@ import hashlib
 import itertools
 import json
 import logging
+import ntpath
 import operator
 import os
 import re
@@ -2856,7 +2857,7 @@ class StaticAnalysis(MachCommandBase):
                 
                 
                 original_path = l[0]
-                local_path = original_path.replace(self.topsrcdir, ".")
+                local_path = ntpath.basename(original_path)
                 target_file = os.path.join(tmpdir, local_path)
                 faketmpdir = os.path.dirname(target_file)
                 if not os.path.isdir(faketmpdir):
