b1475d4162c21de5345f4c42a4b0d3433dc2fa3f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 01:21:03 +0000

    Bug 1473180 part 4 - Remove DeclarationPushMode::Update and related code. r=emilio
    MozReview-Commit-ID: 1fI5YRa54lQ
    
    UltraBlame original commit: 75345fe6e02d9f90fc3266a432205528d85eeccf

--- servo/components/style/properties/declaration_block.rs
+++ servo/components/style/properties/declaration_block.rs
@@ -74,10 +74,6 @@ pub enum DeclarationPushMode {
     Parsing,
     
     
-    
-    Update,
-    
-    
     Append,
 }
 
@@ -470,34 +466,6 @@ impl PropertyDeclarationBlock {
 
     
     
-    pub fn will_change_in_update_mode(
-        &self,
-        source_declarations: &SourcePropertyDeclaration,
-        importance: Importance,
-    ) -> bool {
-        
-        
-        
-        let needs_update = |decl: &_| {
-            if self.is_definitely_new(decl) {
-                return true;
-            }
-            self.declarations.iter().enumerate()
-                .find(|&(_, ref slot)| slot.id() == decl.id())
-                .map_or(true, |(i, slot)| {
-                    let important = self.declarations_importance[i];
-                    *slot != *decl || important != importance.important()
-                })
-        };
-        source_declarations.declarations.iter().any(&needs_update) ||
-            source_declarations.all_shorthand.declarations().any(|decl| needs_update(&decl))
-    }
-
-    
-    
-    
-    
-    
     
     
     pub fn extend(
@@ -586,15 +554,6 @@ impl PropertyDeclarationBlock {
                             }
                         }
                     }
-                }
-                if matches!(mode, DeclarationPushMode::Update) {
-                    let important = self.declarations_importance[i];
-                    if *slot == declaration && important == importance.important() {
-                        return false;
-                    }
-                    *slot = declaration;
-                    self.declarations_importance.set(i, importance.important());
-                    return true;
                 }
 
                 index_to_remove = Some(i);
