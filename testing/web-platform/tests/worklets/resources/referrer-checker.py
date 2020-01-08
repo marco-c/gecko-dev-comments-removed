142fe98401b30166ca7d3fa0adfaa473c92d680f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 07:16:45 +0000

    Bug 1481369 [wpt PR 12330] - Fix worklet referrer tests to match spec, a=testonly
    Automatic update from web-platform-testsFix worklet referrer tests to match spec
    
    --
    
    wpt-commits: b315ee9209a6039e96412520163764b3ae360605
    wpt-pr: 12330
    
    UltraBlame original commit: 23d8c44ece32cb90b1ffdf2900a132ebbd10cc6a

--- testing/web-platform/tests/worklets/resources/referrer-checker.py
+++ testing/web-platform/tests/worklets/resources/referrer-checker.py
@@ -1,15 +1,24 @@
 
 
 def main(request, response):
-    referrer = request.headers.get("referer", "")
+    
+    
+    
+    
+    
+    referrer = request.headers.get("referer", "").split("?")[0]
+    referrer_policy = request.GET.first("referrer_policy")
     expected_referrer = request.GET.first("expected_referrer", "")
-
     response_headers = [("Content-Type", "text/javascript"),
                         ("Access-Control-Allow-Origin", "*")]
 
-    
-    
-    if (expected_referrer != "" and
-        referrer.startswith(expected_referrer + "?")):
-        return (200, response_headers, "")
+    if referrer_policy == "no-referrer" or referrer_policy == "origin":
+        if referrer == expected_referrer:
+            return (200, response_headers, "")
+        return (404, response_headers)
+
+    if referrer_policy == "same-origin":
+        if referrer == expected_referrer:
+            return (200, response_headers, "")
+        return (404, response_headers)
     return (404, response_headers)
