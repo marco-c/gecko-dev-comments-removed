c466360f63ddbca1f919e6198df1c1399233ea20
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 16:34:01 +0000

    Bug 1482689 - Don't import payment-dialog.js from other elements. r=sfoster
    Importing payment-dialog.js ends up importing every other element because it imports the elements needed to render the full dialog.
    We don't want all of our elements to depend on every other payment one so we shouldn't import payment-dialog.js anywhere in non-test code.
    
    Differential Revision: https://phabricator.services.mozilla.com/D10576
    
    UltraBlame original commit: aca271fb517576426f43faa02c7b2f3eb42468e5

--- browser/components/payments/res/containers/address-form.js
+++ browser/components/payments/res/containers/address-form.js
@@ -4,7 +4,6 @@
 
 
 import LabelledCheckbox from "../components/labelled-checkbox.js";
-import PaymentDialog from "./payment-dialog.js";
 import PaymentRequestPage from "../components/payment-request-page.js";
 import PaymentStateSubscriberMixin from "../mixins/PaymentStateSubscriberMixin.js";
 import paymentRequest from "../paymentRequest.js";
@@ -199,7 +198,7 @@ export default class AddressForm extends PaymentStateSubscriberMixin(PaymentRequ
       
       let errorText = (editing && merchantFieldErrors && merchantFieldErrors[errorName]) || "";
       field.setCustomValidity(errorText);
-      let span = PaymentDialog.maybeCreateFieldErrorElement(container);
+      let span = paymentRequest.maybeCreateFieldErrorElement(container);
       span.textContent = errorText;
     }
 
@@ -281,7 +280,7 @@ export default class AddressForm extends PaymentStateSubscriberMixin(PaymentRequ
   onInvalidField(event) {
     let field = event.target;
     let container = field.closest(`#${field.id}-container`);
-    let errorTextSpan = PaymentDialog.maybeCreateFieldErrorElement(container);
+    let errorTextSpan = paymentRequest.maybeCreateFieldErrorElement(container);
     errorTextSpan.textContent = field.validationMessage;
   }
 
