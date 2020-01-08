4dea3567780446b816fdbc87745f9b330d533b3a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 16:44:18 +0000

    Bug 1458308 - Remove app.update.auto and app.update.enabled from prefs.rs and marionette.js r=rstrong,ato
    `app.update.auto` should actually never have been needed here. `app.update.disabledForTesting`, and before that, `app.update.enabled` will prevent updates altogether. Now that the `app.update.auto` pref is not the correct mechanism for disabling automatic update, this pref should be removed from these files.
    
    `app.update.enabled` can also be removed from prefs.rs at this time as per the comment in the file indicating that it can be removed when Firefox 62 stabilizes.
    
    Depends on D10315
    
    Differential Revision: https://phabricator.services.mozilla.com/D10780
    
    UltraBlame original commit: 663e1f142dd49d3bc7912a4e7661bdf7d02cda89

--- testing/marionette/components/marionette.js
+++ testing/marionette/components/marionette.js
@@ -60,12 +60,6 @@ const RECOMMENDED_PREFS = new Map([
   
   
   
-  ["app.update.auto", false],
-
-  
-  
-  
-  
   ["app.update.disabledForTesting", true],
   ["security.turn_off_all_security_so_that_viruses_can_take_over_this_computer", true],
 
