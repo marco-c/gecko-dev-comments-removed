a4db5345ca3723377cd4de6c88f370bcca389917
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 18:20:23 +0000

    Bug 1506687 Part 1: Make FlexItemValues also provide the item's frame rect. r=dholbert
    Differential Revision: https://phabricator.services.mozilla.com/D12182
    
    UltraBlame original commit: 04f110532f4974387e48a3272fe219188299b205

--- dom/flex/FlexItemValues.cpp
+++ dom/flex/FlexItemValues.cpp
@@ -12,7 +12,8 @@
 namespace mozilla {
 namespace dom {
 
-NS_IMPL_CYCLE_COLLECTION_WRAPPERCACHE(FlexItemValues, mParent)
+NS_IMPL_CYCLE_COLLECTION_WRAPPERCACHE(FlexItemValues, mParent, mNode,
+                                      mFrameRect)
 NS_IMPL_CYCLE_COLLECTING_ADDREF(FlexItemValues)
 NS_IMPL_CYCLE_COLLECTING_RELEASE(FlexItemValues)
 NS_INTERFACE_MAP_BEGIN_CYCLE_COLLECTION(FlexItemValues)
@@ -50,6 +51,14 @@ FlexItemValues::FlexItemValues(FlexLineValues* aParent,
   mNode = aItem->mNode;
 
   
+  
+  mFrameRect = new DOMRectReadOnly(mParent,
+    nsPresContext::AppUnitsToDoubleCSSPixels(aItem->mFrameRect.X()),
+    nsPresContext::AppUnitsToDoubleCSSPixels(aItem->mFrameRect.Y()),
+    nsPresContext::AppUnitsToDoubleCSSPixels(aItem->mFrameRect.Width()),
+    nsPresContext::AppUnitsToDoubleCSSPixels(aItem->mFrameRect.Height()));
+
+  
   mMainBaseSize = nsPresContext::AppUnitsToDoubleCSSPixels(
     aItem->mMainBaseSize);
   mMainDeltaSize = nsPresContext::AppUnitsToDoubleCSSPixels(
@@ -74,6 +83,12 @@ nsINode*
 FlexItemValues::GetNode() const
 {
   return mNode;
+}
+
+DOMRectReadOnly*
+FlexItemValues::FrameRect() const
+{
+  return mFrameRect;
 }
 
 double
