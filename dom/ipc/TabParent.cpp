c9cd13b748e017ba69bbd9e94812c6c101912cca
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 16:17:19 +0000

    Bug 1503979 - Remove nsITabParent.useAsyncPanZoom because it is unused r=baku
    Differential Revision: https://phabricator.services.mozilla.com/D10596
    
    UltraBlame original commit: 100637523a3dce41e38b4f2889231c805449e511

--- dom/ipc/TabParent.cpp
+++ dom/ipc/TabParent.cpp
@@ -2825,13 +2825,6 @@ TabParent::GetLoadContext()
   return loadContext.forget();
 }
 
-NS_IMETHODIMP
-TabParent::GetUseAsyncPanZoom(bool* useAsyncPanZoom)
-{
-  *useAsyncPanZoom = AsyncPanZoomEnabled();
-  return NS_OK;
-}
-
 
 NS_IMETHODIMP
 TabParent::SetDocShellIsActive(bool isActive)
