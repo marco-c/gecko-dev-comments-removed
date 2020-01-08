99f8738e4627ccd0a0956093f984fb103e8e7993
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 23:17:56 +0000

    Bug 1511891 part 1 - Add BindVarOperation and use it for JSOP_BINDVAR in interpreter and JITs. r=tcampbell
    This also adds a GetVariablesObject helper so we don't have to duplicate the
    logic there.
    
    Differential Revision: https://phabricator.services.mozilla.com/D13698
    
    UltraBlame original commit: 582477c043dd5367a4acb02ca0b5032ad6d9997d

--- js/src/vm/EnvironmentObject-inl.h
+++ js/src/vm/EnvironmentObject-inl.h
@@ -24,6 +24,16 @@ inline LexicalEnvironmentObject& NearestEnclosingExtensibleLexicalEnvironment(
   return env->as<LexicalEnvironmentObject>();
 }
 
+
+
+inline JSObject& GetVariablesObject(JSObject* envChain) {
+  while (!envChain->isQualifiedVarObj()) {
+    envChain = envChain->enclosingEnvironment();
+  }
+  MOZ_ASSERT(envChain);
+  return *envChain;
+}
+
 inline void EnvironmentObject::setAliasedBinding(JSContext* cx, uint32_t slot,
                                                  PropertyName* name,
                                                  const Value& v) {
