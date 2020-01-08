9ae7f2225c29c2cb8cb5550424847e5d97afdaa9
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 09:33:43 +0000

    Backed out changeset 0adb1474cc23 (bug 1469148) for failing bc at browser/components/extensions/test/browser/test-oop-extensions/browser_ext_menus_capture_secondary_click.js on a CLOSED TREE
    UltraBlame original commit: 1eef890debe96a14d09676c6208164160e94c96a

--- browser/components/extensions/parent/ext-menus.js
+++ browser/components/extensions/parent/ext-menus.js
@@ -274,9 +274,7 @@ var gMenuBuilder = {
       element.setAttribute("disabled", "true");
     }
 
-    let button;
-
-    element.addEventListener("command", event => {
+    element.addEventListener("command", event => { 
       if (event.target !== event.currentTarget) {
         return;
       }
@@ -306,8 +304,6 @@ var gMenuBuilder = {
         info.modifiers.push("MacCtrl");
       }
 
-      info.button = button;
-
       
       
       let actionFor = {
@@ -321,20 +317,6 @@ var gMenuBuilder = {
       }
 
       item.extension.emit("webext-menu-menuitem-click", info, contextData.tab);
-    }, {once: true});
-
-    element.addEventListener("click", event => { 
-      if (event.target !== event.currentTarget) {
-        return;
-      }
-
-      button = event.button;
-      if (event.button) {
-        element.doCommand();
-        if (event.button !== 0 && event.button !== 2) {
-          contextData.menu.hidePopup();
-        }
-      }
     });
 
     
