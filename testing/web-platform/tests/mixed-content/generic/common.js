deca713227e9f81f01308ce088544f0babf4fb2e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 10:38:32 +0000

    Bug 1489390 [wpt PR 12892] - Add more WPT tests for mixed-content check in workers/worklets (1/2), a=testonly
    Automatic update from web-platform-testsAdd more WPT tests for mixed-content check in workers/worklets (1/2)
    
    This CL adds test code for more worker/worklet-related cases
    to mixed-content/ WPT tests:
    - module-worker-top-level (outsideSettings of https: module script)
    - module-data-worker-import (outsideSettings of data: worker)
    - classic-data-worker-fetch (insideSettings of data: worker)
    - worklet-*-top-level (outsideSettings of https: worklets)
    - worklet-*-data-import (outsideSettings of data: worklets)
    
    Actual generated tests is added in a separate CL
    https://chromium-review.googlesource.com/1212746
    for easier code review.
    
    These tests are for
    https://chromium-review.googlesource.com/1208390.
    
    Bug: 880986, 880023, 880015
    Change-Id: I07eb96cffec889103bf437813180127644466af8
    Reviewed-on: https://chromium-review.googlesource.com/1212744
    Commit-Queue: Hiroshige Hayashizaki <hiroshigechromium.org>
    Reviewed-by: Mike West <mkwstchromium.org>
    Reviewed-by: Hiroki Nakagawa <nhirokichromium.org>
    Reviewed-by: Andy Paicu <andypaicuchromium.org>
    Cr-Commit-Position: refs/heads/master{#590239}
    
    --
    
    wpt-commits: 46535e59509a8d070884a3869ceeed43a1145aa1
    wpt-pr: 12892
    
    UltraBlame original commit: 1ce6504ba4472e38720093d961a7a7bbdaf97df1

--- testing/web-platform/tests/mixed-content/generic/common.js
+++ testing/web-platform/tests/mixed-content/generic/common.js
@@ -184,6 +184,16 @@ function requestViaFetch(url) {
   return fetch(url);
 }
 
+function dedicatedWorkerUrlThatFetches(url) {
+  return `data:text/javascript,
+    fetch('${url}')
+      .then(() => postMessage(''),
+            () => postMessage(''));`;
+}
+
+function workerUrlThatImports(url) {
+  return `data:text/javascript,import '${url}';`;
+}
 
 
 
@@ -191,10 +201,12 @@ function requestViaFetch(url) {
 
 
 
-function requestViaWorker(url) {
+
+
+function requestViaDedicatedWorker(url, options) {
   var worker;
   try {
-    worker = new Worker(url);
+    worker = new Worker(url, options);
   } catch (e) {
     return Promise.reject(e);
   }
@@ -202,6 +214,29 @@ function requestViaWorker(url) {
   worker.postMessage('');
 
   return worker.eventPromise;
+}
+
+
+function get_worklet(type) {
+  if (type == 'animation')
+    return CSS.animationWorklet;
+  if (type == 'layout')
+    return CSS.layoutWorklet;
+  if (type == 'paint')
+    return CSS.paintWorklet;
+  if (type == 'audio')
+    return new OfflineAudioContext(2,44100*40,44100).audioWorklet;
+
+  assert_unreached('unknown worklet type is passed.');
+  return undefined;
+}
+
+function requestViaWorklet(type, url) {
+  try {
+    return get_worklet(type).addModule(url);
+  } catch (e) {
+    return Promise.reject(e);
+  }
 }
 
 
