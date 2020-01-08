9ae03d8c7c9c554e913b18d90dd2ca2a6708e026
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 13:44:10 +0000

    Bug 1494809 - Support WebGL exts for BPTC and RGTC. - r=kvark,qdot
    UltraBlame original commit: ae8e6587acbcc40cbb42e882f15294f4425e301d

--- dom/canvas/WebGLTextureUpload.cpp
+++ dom/canvas/WebGLTextureUpload.cpp
@@ -1018,62 +1018,38 @@ ValidateTargetForFormat(WebGLContext* webgl, TexImageTarget target,
     
     
     
-
-    switch (format->effectiveFormat) {
-    
-    
-    case webgl::EffectiveFormat::DEPTH_COMPONENT16:
-    case webgl::EffectiveFormat::DEPTH_COMPONENT24:
-    case webgl::EffectiveFormat::DEPTH_COMPONENT32F:
-    case webgl::EffectiveFormat::DEPTH24_STENCIL8:
-    case webgl::EffectiveFormat::DEPTH32F_STENCIL8:
-    
-    case webgl::EffectiveFormat::COMPRESSED_R11_EAC:
-    case webgl::EffectiveFormat::COMPRESSED_SIGNED_R11_EAC:
-    case webgl::EffectiveFormat::COMPRESSED_RG11_EAC:
-    case webgl::EffectiveFormat::COMPRESSED_SIGNED_RG11_EAC:
-    case webgl::EffectiveFormat::COMPRESSED_RGB8_ETC2:
-    case webgl::EffectiveFormat::COMPRESSED_SRGB8_ETC2:
-    case webgl::EffectiveFormat::COMPRESSED_RGB8_PUNCHTHROUGH_ALPHA1_ETC2:
-    case webgl::EffectiveFormat::COMPRESSED_SRGB8_PUNCHTHROUGH_ALPHA1_ETC2:
-    case webgl::EffectiveFormat::COMPRESSED_RGBA8_ETC2_EAC:
-    case webgl::EffectiveFormat::COMPRESSED_SRGB8_ALPHA8_ETC2_EAC:
-    
-    case webgl::EffectiveFormat::COMPRESSED_RGB_S3TC_DXT1_EXT:
-    case webgl::EffectiveFormat::COMPRESSED_RGBA_S3TC_DXT1_EXT:
-    case webgl::EffectiveFormat::COMPRESSED_RGBA_S3TC_DXT3_EXT:
-    case webgl::EffectiveFormat::COMPRESSED_RGBA_S3TC_DXT5_EXT:
-        if (target == LOCAL_GL_TEXTURE_3D) {
-            webgl->ErrorInvalidOperation("Format %s cannot be used with TEXTURE_3D.",
-                                         format->name);
+    const bool ok = [&]() {
+        if (bool(format->d) & (target == LOCAL_GL_TEXTURE_3D))
             return false;
-        }
-        break;
 
-    
-    
-    case webgl::EffectiveFormat::ATC_RGB_AMD:
-    case webgl::EffectiveFormat::ATC_RGBA_EXPLICIT_ALPHA_AMD:
-    case webgl::EffectiveFormat::ATC_RGBA_INTERPOLATED_ALPHA_AMD:
-    
-    case webgl::EffectiveFormat::COMPRESSED_RGB_PVRTC_4BPPV1:
-    case webgl::EffectiveFormat::COMPRESSED_RGBA_PVRTC_4BPPV1:
-    case webgl::EffectiveFormat::COMPRESSED_RGB_PVRTC_2BPPV1:
-    case webgl::EffectiveFormat::COMPRESSED_RGBA_PVRTC_2BPPV1:
-    
-    case webgl::EffectiveFormat::ETC1_RGB8_OES:
-        if (target == LOCAL_GL_TEXTURE_3D ||
-            target == LOCAL_GL_TEXTURE_2D_ARRAY)
-        {
-            webgl->ErrorInvalidOperation("Format %s cannot be used with TEXTURE_3D or"
-                                         " TEXTURE_2D_ARRAY.",
-                                         format->name);
-            return false;
-        }
-        break;
+        if (format->compression) {
+            switch (format->compression->family) {
+            case webgl::CompressionFamily::ES3:
+            case webgl::CompressionFamily::S3TC:
+                if (target == LOCAL_GL_TEXTURE_3D)
+                    return false;
+                break;
 
-    default:
-        break;
+            case webgl::CompressionFamily::ATC:
+            case webgl::CompressionFamily::ETC1:
+            case webgl::CompressionFamily::PVRTC:
+            case webgl::CompressionFamily::RGTC:
+                if (target == LOCAL_GL_TEXTURE_3D ||
+                    target == LOCAL_GL_TEXTURE_2D_ARRAY)
+                {
+                    return false;
+                }
+                break;
+            default:
+                break;
+            }
+        }
+        return true;
+    }();
+    if (!ok) {
+        webgl->ErrorInvalidOperation("Format %s cannot be used with target %s.",
+                                     format->name, GetEnumName(target.get()));
+        return false;
     }
 
     return true;
@@ -1598,6 +1574,8 @@ WebGLTexture::CompressedTexSubImage(TexImageTarget target,
     
     case webgl::CompressionFamily::ES3:  
     case webgl::CompressionFamily::S3TC: 
+    case webgl::CompressionFamily::BPTC:
+    case webgl::CompressionFamily::RGTC:
         if (!IsSubImageBlockAligned(dstFormat->compression, imageInfo, xOffset, yOffset,
                                     blob->mWidth, blob->mHeight))
         {
