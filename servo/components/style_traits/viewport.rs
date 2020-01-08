93b4d4407304aae1aacd8d6dc95bd56fa63de527
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 10:46:12 +0000

    Bug 1491577 - Format component style_traits. r=emilio
    This cherry-picks servo/servo#21620.
    
    UltraBlame original commit: d88b4c39dfce50e116436441080dcc9b4acf593e

--- servo/components/style_traits/viewport.rs
+++ servo/components/style_traits/viewport.rs
@@ -28,7 +28,10 @@ define_css_keyword_enum! {
 
 
 #[derive(Clone, Debug, PartialEq)]
-#[cfg_attr(feature = "servo", derive(Deserialize, Serialize, MallocSizeOf))]
+#[cfg_attr(
+    feature = "servo",
+    derive(Deserialize, Serialize, MallocSizeOf)
+)]
 pub struct ViewportConstraints {
     
     
@@ -43,7 +46,7 @@ pub struct ViewportConstraints {
     
     pub user_zoom: UserZoom,
     
-    pub orientation: Orientation
+    pub orientation: Orientation,
 }
 
 impl ToCss for ViewportConstraints {
@@ -93,7 +96,8 @@ pub enum Zoom {
 
 impl ToCss for Zoom {
     fn to_css<W>(&self, dest: &mut CssWriter<W>) -> fmt::Result
-        where W: fmt::Write,
+    where
+        W: fmt::Write,
     {
         match *self {
             Zoom::Number(number) => number.to_css(dest),
@@ -101,7 +105,7 @@ impl ToCss for Zoom {
             Zoom::Percentage(percentage) => {
                 (percentage * 100.).to_css(dest)?;
                 dest.write_char('%')
-            }
+            },
         }
     }
 }
@@ -121,16 +125,16 @@ impl Zoom {
             
             
             
-            Token::Percentage { unit_value, .. } if NonNegative.is_ok(ParsingMode::DEFAULT, unit_value) => {
+            Token::Percentage { unit_value, .. }
+                if NonNegative.is_ok(ParsingMode::DEFAULT, unit_value) =>
+            {
                 Ok(Zoom::Percentage(unit_value))
-            }
+            },
             Token::Number { value, .. } if NonNegative.is_ok(ParsingMode::DEFAULT, value) => {
                 Ok(Zoom::Number(value))
-            }
-            Token::Ident(ref value) if value.eq_ignore_ascii_case("auto") => {
-                Ok(Zoom::Auto)
-            }
-            ref t => Err(location.new_unexpected_token_error(t.clone()))
+            },
+            Token::Ident(ref value) if value.eq_ignore_ascii_case("auto") => Ok(Zoom::Auto),
+            ref t => Err(location.new_unexpected_token_error(t.clone())),
         }
     }
 
@@ -141,7 +145,7 @@ impl Zoom {
         match *self {
             Zoom::Number(number) => Some(number as f32),
             Zoom::Percentage(percentage) => Some(percentage as f32),
-            Zoom::Auto => None
+            Zoom::Auto => None,
         }
     }
 }
