42601683c79b780d2ec24088a4a5cdec1541ad94
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 16:42:11 +0000

    Bug 1498942 - Lengthen timeouts for detecting hung content processes during shutdown r=Ehsan
    Differential Revision: https://phabricator.services.mozilla.com/D10621
    
    UltraBlame original commit: 11edba6d53bb8b4a7eee1b46398b932900d456f8

--- modules/libpref/init/StaticPrefList.h
+++ modules/libpref/init/StaticPrefList.h
@@ -181,6 +181,22 @@ VARCACHE_PREF(
 
 
 
+
+#if !defined(DEBUG) && !defined(MOZ_ASAN) && !defined(MOZ_VALGRIND) && \
+    !defined(MOZ_TSAN)
+# define PREF_VALUE 10
+#else
+# define PREF_VALUE 0
+#endif
+VARCACHE_PREF(
+  "dom.ipc.tabs.shutdownTimeoutSecs",
+   dom_ipc_tabs_shutdownTimeoutSecs,
+  RelaxedAtomicUint32, PREF_VALUE
+)
+#undef PREF_VALUE
+
+
+
 VARCACHE_PREF(
   "dom.keyboardevent.keypress.set_keycode_and_charcode_to_same_value",
    dom_keyboardevent_keypress_set_keycode_and_charcode_to_same_value,
