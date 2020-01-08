73fbba7450bc5def41b3f596060c88844c9f3200
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 01:43:26 +0000

    Bug 1475943: Don't trace a wasm global object if it's being created; r=jseward
    UltraBlame original commit: 5b31b8bf2d680fba2f46cfa99576f1027bed328f

--- js/src/wasm/WasmJS.cpp
+++ js/src/wasm/WasmJS.cpp
@@ -2155,6 +2155,12 @@ const Class WasmGlobalObject::class_ =
 WasmGlobalObject::trace(JSTracer* trc, JSObject* obj)
 {
     WasmGlobalObject* global = reinterpret_cast<WasmGlobalObject*>(obj);
+    if (global->isNewborn()) {
+        
+        
+        
+        return;
+    }
     switch (global->type().code()) {
       case ValType::AnyRef:
         if (global->cell()->ptr)
@@ -2229,6 +2235,8 @@ WasmGlobalObject::create(JSContext* cx, HandleVal hval, bool isMutable)
     obj->initReservedSlot(TYPE_SLOT, Int32Value(int32_t(val.type().bitsUnsafe())));
     obj->initReservedSlot(MUTABLE_SLOT, JS::BooleanValue(isMutable));
     obj->initReservedSlot(CELL_SLOT, PrivateValue(cell));
+
+    MOZ_ASSERT(!obj->isNewborn());
 
     return obj;
 }
