5e4e5519640c234dd6239dde15adeed581feb31f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 16:06:53 +0000

    Bug 1467712 - Fail if SimpleTest ok() is called with more than 2 arguments;r=Standard8
    Depends on D10417
    
    Differential Revision: https://phabricator.services.mozilla.com/D10418
    
    UltraBlame original commit: 026eb1f6dc6e9780574b16c7c642c3179330a1d9

--- testing/mochitest/browser-test.js
+++ testing/mochitest/browser-test.js
@@ -17,7 +17,7 @@ ChromeUtils.defineModuleGetter(this, "ContentSearch",
   "resource:///modules/ContentSearch.jsm");
 
 const SIMPLETEST_OVERRIDES =
-  ["ok", "is", "isnot", "todo", "todo_is", "todo_isnot", "info", "expectAssertions", "requestCompleteLog"];
+  ["ok", "record", "is", "isnot", "todo", "todo_is", "todo_isnot", "info", "expectAssertions", "requestCompleteLog"];
 
 setTimeout(testInit, 0);
 
@@ -88,7 +88,8 @@ function testInit() {
       
       var webNav = content.window.docShell
                           .QueryInterface(Ci.nsIWebNavigation);
-      webNav.loadURI(url, null, null, null, null);
+      var systemPrincipal = Services.scriptSecurityManager.getSystemPrincipal();
+      webNav.loadURI(url, null, null, null, null, systemPrincipal);
     };
 
     var listener = 'data:,function doLoad(e) { var data=e.detail&&e.detail.data;removeEventListener("contentEvent", function (e) { doLoad(e); }, false, true);sendAsyncMessage("chromeEvent", {"data":data}); };addEventListener("contentEvent", function (e) { doLoad(e); }, false, true);';
@@ -1288,7 +1289,15 @@ function testScope(aTester, aTest, expected) {
   aTest.allowFailure = expected == "fail";
 
   var self = this;
-  this.ok = function test_ok(condition, name, ex, stack) {
+  this.ok = function test_ok(condition, name) {
+    if (arguments.length > 2) {
+      const ex = "Too many arguments passed to ok(condition, name)`.";
+      self.record(false, name, ex);
+    } else {
+      self.record(condition, name);
+    }
+  };
+  this.record = function test_record(condition, name, ex, stack) {
     aTest.addResult(new testResult({
       name, pass: condition, ex,
       stack: stack || Components.stack.caller,
@@ -1296,11 +1305,11 @@ function testScope(aTester, aTest, expected) {
     }));
   };
   this.is = function test_is(a, b, name) {
-    self.ok(a == b, name, "Got " + a + ", expected " + b, false,
+    self.record(a == b, name, "Got " + a + ", expected " + b, false,
             Components.stack.caller);
   };
   this.isnot = function test_isnot(a, b, name) {
-    self.ok(a != b, name, "Didn't expect " + a + ", but got it", false,
+    self.record(a != b, name, "Didn't expect " + a + ", but got it", false,
             Components.stack.caller);
   };
   this.todo = function test_todo(condition, name, ex, stack) {
