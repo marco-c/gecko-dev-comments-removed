92e901a04a8424f1f9cfb2495ab1bec8427c1311
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 00:11:20 +0000

    Bug 1466979 - Separate JS::ubi::EdgeVectorTracer and JS::ubi::SimpleEdgeRange into UbiNodeUtils.h r=jimb
    Separated SimpleEdgeRange into the new UbiNodeUtils.h, updated build files, changed SimpleEdgeRange::init to SimpleEdgeRange::addTracerEdges, created bool addEdge to add new edges to the vector.
    
    UltraBlame original commit: 383714d5acad1c17adb25b8145f0cf1c25f1261c

--- js/src/vm/UbiNode.cpp
+++ js/src/vm/UbiNode.cpp
@@ -19,6 +19,7 @@
 #include "js/Debug.h"
 #include "js/TracingAPI.h"
 #include "js/TypeDecls.h"
+#include "js/UbiNodeUtils.h"
 #include "js/Utility.h"
 #include "js/Vector.h"
 #include "util/Text.h"
@@ -224,7 +225,6 @@ Node::exposeToJS() const
 
 
 
-
 class EdgeVectorTracer : public JS::CallbackTracer {
     
     EdgeVector* vec;
@@ -285,31 +285,6 @@ class EdgeVectorTracer : public JS::CallbackTracer {
     { }
 };
 
-
-
-
-class SimpleEdgeRange : public EdgeRange {
-    EdgeVector edges;
-    size_t i;
-
-    void settle() {
-        front_ = i < edges.length() ? &edges[i] : nullptr;
-    }
-
-  public:
-    explicit SimpleEdgeRange() : edges(), i(0) { }
-
-    bool init(JSRuntime* rt, void* thing, JS::TraceKind kind, bool wantNames = true) {
-        EdgeVectorTracer tracer(rt, &edges, wantNames);
-        js::TraceChildren(&tracer, thing, kind);
-        settle();
-        return tracer.okay;
-    }
-
-    void popFront() override { i++; settle(); }
-};
-
-
 template<typename Referent>
 JS::Zone*
 TracerConcrete<Referent>::zone() const
@@ -337,7 +312,7 @@ TracerConcrete<Referent>::edges(JSContext* cx, bool wantNames) const {
     if (!range)
         return nullptr;
 
-    if (!range->init(cx->runtime(), ptr, JS::MapTypeToTraceKind<Referent>::kind, wantNames))
+    if (!range->addTracerEdges(cx->runtime(), ptr, JS::MapTypeToTraceKind<Referent>::kind, wantNames))
         return nullptr;
 
     
@@ -560,6 +535,14 @@ UniquePtr<EdgeRange>
 Concrete<RootList>::edges(JSContext* cx, bool wantNames) const {
     MOZ_ASSERT_IF(wantNames, get().wantNames);
     return js::MakeUnique<PreComputedEdgeRange>(get().edges);
+}
+
+bool
+SimpleEdgeRange::addTracerEdges(JSRuntime* rt, void* thing, JS::TraceKind kind, bool wantNames) {
+    EdgeVectorTracer tracer(rt, &edges, wantNames);
+    js::TraceChildren(&tracer, thing, kind);
+    settle();
+    return tracer.okay;
 }
 
 } 
