83e9737ba8d8fc3cd5b1795586399742b7b3cbbf
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 12:06:44 +0000

    Bug 1494426 part 2: Use ReflowInput::ComputedSize() convenience method in a few more places. r=TYLin
    This patch isn't expected to change behavior; it's just some simplification.
    
    Depends on D6976
    
    Differential Revision: https://phabricator.services.mozilla.com/D6978
    
    UltraBlame original commit: d5e6ec65686c6fd1f2b25b3549db479cd34033f9

--- layout/generic/nsBlockFrame.cpp
+++ layout/generic/nsBlockFrame.cpp
@@ -1118,10 +1118,7 @@ CalculateContainingBlockSizeForAbsolutes(WritingMode aWM,
       
       
       WritingMode lastWM = aLastRI->GetWritingMode();
-      LogicalSize lastRISize =
-        LogicalSize(lastWM,
-                    aLastRI->ComputedISize(),
-                    aLastRI->ComputedBSize()).ConvertTo(aWM, lastWM);
+      LogicalSize lastRISize = aLastRI->ComputedSize().ConvertTo(aWM, lastWM);
       LogicalMargin lastRIPadding =
         aLastRI->ComputedLogicalPadding().ConvertTo(aWM, lastWM);
       LogicalMargin logicalScrollbars(aWM, scrollbars);
