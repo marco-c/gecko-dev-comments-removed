70f174c59fba583b1f42cc47a0c698ce6b0d1fa5
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 13:54:11 +0000

    Bug 1431441 - Part 7 - Relax WebReplay fcntl rules to avoid sqlite crash r=froydnj
    Patch provided by Brian Hackett <bhackettmozilla.com>.
    
    Only allow a limited set of commands to be used when events are not
    passed through and we are recording/replaying the outputs.
    
    Depends on D8474
    
    Differential Revision: https://phabricator.services.mozilla.com/D8475
    
    UltraBlame original commit: 1dfdc7ba998d4c03a5e93e6afff087086413f94e

--- toolkit/recordreplay/ProcessRedirectDarwin.cpp
+++ toolkit/recordreplay/ProcessRedirectDarwin.cpp
@@ -717,6 +717,13 @@ static PreambleResult
 Preamble_fcntl(CallArguments* aArguments)
 {
   
+  
+  
+  
+  if (AreThreadEventsPassedThrough()) {
+    return PreambleResult::Redirect;
+  }
+
   auto& cmd = aArguments->Arg<1, size_t>();
   switch (cmd) {
   case F_GETFL:
