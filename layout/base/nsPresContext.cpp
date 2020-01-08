0968b22beb14c7c051561586025f9c75f46ce098
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 23:05:59 +0000

    Bug 1518965: Remove nsPresContext::mLastResizeEventVisibleArea and accessor functions that are never used. r=botond
    Differential Revision: https://phabricator.services.mozilla.com/D16126
    
    UltraBlame original commit: 606870290cd6ece614fe54d67f273fe2a803ecdd

--- layout/base/nsPresContext.cpp
+++ layout/base/nsPresContext.cpp
@@ -176,11 +176,6 @@ nsPresContext::nsPresContext(dom::Document* aDocument, nsPresContextType aType)
       mCurAppUnitsPerDevPixel(0),
       mAutoQualityMinFontSizePixelsPref(0),
       mLangService(nsLanguageAtomService::GetService()),
-      
-      
-      mLastResizeEventVisibleArea(nsRect(nscoord_MIN, nscoord_MIN,
-                                         NS_UNCONSTRAINEDSIZE,
-                                         NS_UNCONSTRAINEDSIZE)),
       mPageSize(-1, -1),
       mPageScale(0.0),
       mPPScale(1.0f),
