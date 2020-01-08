0f44d6b810fdf2b3e4c4a02f5c6ec13a8ed6cbef
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 23:58:07 +0000

    Bug 1520805 - Add the socket process to known processes for leak logging. r=kershaw
    Differential Revision: https://phabricator.services.mozilla.com/D16842
    
    UltraBlame original commit: 69d656df4a8e626ea184db1573861b56ccd17671

--- testing/mozbase/mozleak/mozleak/leaklog.py
+++ testing/mozbase/mozleak/mozleak/leaklog.py
@@ -164,7 +164,7 @@ def process_leak_log(leak_log_file, leak_thresholds=None,
 
     
     
-    knownProcessTypes = ["default", "plugin", "tab", "gmplugin", "gpu", "rdd", "vr"]
+    knownProcessTypes = ["default", "plugin", "tab", "gmplugin", "gpu", "rdd", "vr", "socket"]
 
     for processType in knownProcessTypes:
         log.info("TEST-INFO | leakcheck | %s process: leak threshold set at %d bytes"
