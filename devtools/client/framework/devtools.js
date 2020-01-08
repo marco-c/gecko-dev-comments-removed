79cdae74ea2c273b64652c999db3ce5d0cdbdcb7
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 11:49:22 +0000

    Bug 1485676 - Connect to local server from TargetFactory.forTab and make TabTarget always remote-like. r=yulia
    Tags: #secure-revision
    
    Bug #: 1485676
    
    Differential Revision: https://phabricator.services.mozilla.com/D4078
    
    MozReview-Commit-ID: JAwiySsBZBu
    
    UltraBlame original commit: 644fb45085db3e8f157f7b0fe8916fe8fc5ad045

--- devtools/client/framework/devtools.js
+++ devtools/client/framework/devtools.js
@@ -691,7 +691,7 @@ DevTools.prototype = {
 
 
   async inspectNode(tab, nodeSelectors, startTime) {
-    const target = TargetFactory.forTab(tab);
+    const target = await TargetFactory.forTab(tab);
 
     const toolbox = await gDevTools.showToolbox(target, "inspector", null, null,
                                                 startTime, "inspect_dom");
@@ -731,7 +731,7 @@ DevTools.prototype = {
 
 
   async inspectA11Y(tab, nodeSelectors, startTime) {
-    const target = TargetFactory.forTab(tab);
+    const target = await TargetFactory.forTab(tab);
 
     const toolbox = await gDevTools.showToolbox(
       target, "accessibility", null, null, startTime);
