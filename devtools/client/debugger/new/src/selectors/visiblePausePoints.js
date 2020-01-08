8fcb10ff559d5314697ea9c5ba7250fb9b8074c6
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 21:59:19 +0000

    Bug 1515736 - [release 111] [breakpoints] convert pause points to a list (#7504). r=lsmyth
    UltraBlame original commit: 1bc7f0ea547fa2a2c792295311ff75f37f494378

--- devtools/client/debugger/new/src/selectors/visiblePausePoints.js
+++ devtools/client/debugger/new/src/selectors/visiblePausePoints.js
@@ -1,10 +1,9 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at <http://mozilla.org/MPL/2.0/>. */
+
+
+
 
 import { getSelectedSource } from "../reducers/sources";
 import { getPausePoints } from "../reducers/ast";
-import { convertToList } from "../utils/pause/pausePoints";
 
 export function getVisiblePausePoints(state) {
   const source = getSelectedSource(state);
@@ -12,6 +11,5 @@ export function getVisiblePausePoints(state) {
     return null;
   }
 
-  const pausePoints = getPausePoints(state, source.id);
-  return convertToList(pausePoints);
+  return getPausePoints(state, source.id);
 }
