b0391e7cd8e9af9feb4f0cdad74f2bb18de3a14e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 07:28:32 +0000

    Bug 1482454 - enabled accessibility panel by default. r=gl
    MozReview-Commit-ID: K18YgGgmxOV
    
    UltraBlame original commit: f06260e3db1c62e7e24845fb4d92a6d7c6093427

--- devtools/client/inspector/inspector.js
+++ devtools/client/inspector/inspector.js
@@ -1697,7 +1697,13 @@ Inspector.prototype = {
       click: () => this.showAccessibilityProperties(),
       disabled: true
     });
-    this._updateA11YMenuItem(showA11YPropsItem);
+    
+    
+    const startup = this._toolbox.getToolStartup("accessibility");
+    if (startup && startup.accessibility.enabled) {
+      this._updateA11YMenuItem(showA11YPropsItem);
+    }
+
     menu.append(showA11YPropsItem);
   },
 
