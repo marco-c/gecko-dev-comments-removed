67be1aa7a4a47051283927e3546a19bb5eae64e7
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 01:17:23 +0000

    Backed out 2 changesets (bug 1474722)For freqvently failing test verify on gfx/tests/reftest/1474722.html CLOSED TREE
    Backed out changeset 53d0bbb455cb (bug 1474722)
    Backed out changeset 315d75c42ef0 (bug 1474722)
    
    UltraBlame original commit: ee47b3edea9199a1ef1450d62ca77ad68ae16d7b

--- gfx/thebes/gfxBlur.cpp
+++ gfx/thebes/gfxBlur.cpp
@@ -594,12 +594,6 @@ GetBlur(gfxContext* aDestinationCtx,
   if (useDestRect) {
     minSize = aRectSize;
   }
-
-  int32_t maxTextureSize = gfxPlatform::MaxTextureSize();
-  if (minSize.width > maxTextureSize || minSize.height > maxTextureSize) {
-    return nullptr;
-  }
-
   aOutMinSize = minSize;
 
   DrawTarget* destDT = aDestinationCtx->GetDrawTarget();
@@ -961,7 +955,13 @@ gfxAlphaBoxBlur::BlurRectangle(gfxContext* aDestinationCtx,
                                const gfxRect& aDirtyRect,
                                const gfxRect& aSkipRect)
 {
-  if (!RectIsInt32Safe(ToRect(aRect))) {
+  const double maxSize = (double)gfxPlatform::MaxTextureSize();
+  const double maxPos = (double)std::numeric_limits<std::int16_t>::max();
+  if (aRect.width > maxSize || aRect.height > maxSize ||
+      std::abs(aRect.x) > maxPos || std::abs(aRect.y) > maxPos) {
+    
+    
+    
     return;
   }
 
@@ -1015,6 +1015,11 @@ gfxAlphaBoxBlur::BlurRectangle(gfxContext* aDestinationCtx,
     }
   }
 
+  
+  
+  
+  
+  
   
   
   
