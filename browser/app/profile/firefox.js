ffb9504b3a61a9acee4673e3086ce09848103246
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 12:06:33 +0000

    Merge mozilla-central to autoland. a=merge CLOSED TREE
    UltraBlame original commit: 23f8684d08739df708dc7af14ed3e8ce1349fbc5

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1498,11 +1498,6 @@ pref("browser.ping-centre.production.endpoint", "https://tiles.services.mozilla.
 
 pref("media.gmp-provider.enabled", true);
 
-
-#ifdef NIGHTLY_BUILD
-pref("network.cookie.cookieBehavior", 4 );
-#endif
-
 pref("browser.contentblocking.allowlist.storage.enabled", true);
 
 pref("browser.contentblocking.global-toggle.enabled", true);
