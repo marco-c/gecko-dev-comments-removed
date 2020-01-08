29f28e123ff78018c5378eeb31d4cc694aef0da4
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 22:34:15 +0000

    Bug 1517572 - [release 116] [workers] polish the ui (#7622). r=dwalsh
    UltraBlame original commit: 3a0d4e552830a0a4344ed1c3b6249972eebc6c65

---
+++ devtools/client/debugger/new/src/components/SecondaryPanes/Worker.js
@@ -0,0 +1,66 @@
+
+
+
+
+
+
+import React, { Component } from "react";
+import { connect } from "../../utils/connect";
+import classnames from "classnames";
+
+import actions from "../../actions";
+import { getCurrentThread, getThreadIsPaused } from "../../selectors";
+import { getDisplayName, isWorker } from "../../utils/workers";
+import AccessibleImage from "../shared/AccessibleImage";
+
+import type { Thread } from "../../types";
+
+type Props = {
+  selectThread: typeof actions.selectThread,
+  isPaused: boolean,
+  thread: Thread,
+  currentThread: string
+};
+
+export class Worker extends Component<Props> {
+  render() {
+    const { currentThread, isPaused, thread } = this.props;
+
+    const label = isWorker(thread)
+      ? getDisplayName(thread)
+      : L10N.getStr("mainThread");
+
+    return (
+      <div
+        className={classnames(
+          "worker",
+          thread.actor == currentThread && "selected"
+        )}
+        key={thread.actor}
+        onClick={() => this.props.selectThread(thread.actor)}
+      >
+        <div clasName="icon">
+          <AccessibleImage className={isWorker ? "worker" : "file"} />
+        </div>
+        <div className="label">{label}</div>
+        {isPaused ? (
+          <div className="pause-badge">
+            <AccessibleImage className="pause" />
+          </div>
+        ) : null}
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state, props: Props) => ({
+  currentThread: getCurrentThread(state),
+  isPaused: getThreadIsPaused(state, props.thread.actor)
+});
+
+export default connect(
+  mapStateToProps,
+  {
+    selectThread: actions.selectThread
+  }
+)(Worker);
