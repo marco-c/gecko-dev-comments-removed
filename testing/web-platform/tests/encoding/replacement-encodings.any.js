b3f3ead3f0c9904a7d9b74fbfc46728cdf421b5d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 02:02:01 +0000

    Bug 1475129 [wpt PR 11930] - Reformat Encoding tests, a=testonly
    Automatic update from web-platform-testsMoving some encoding files from .html to .any.js
    
    --
    Generalized test
    
    --
    converted some encoding tests to use any.js
    
    --
    
    wpt-commits: e44b8b3dae4f0f0c874ef0b2a89290d0dd32dcdd, 12b30f56e3dddb896d343a6b6331cd25d3740e1d, dfa045c04225bbd6c1d7b6c1512f446948c12deb
    wpt-pr: 11930
    
    UltraBlame original commit: 833dfc818644474fb6522a33f8ff6b43ba4724d9

---
+++ testing/web-platform/tests/encoding/replacement-encodings.any.js
@@ -0,0 +1,25 @@
+
+
+
+
+const replacement_labels = [];
+encodings_table.forEach(section => {
+  section.encodings
+         .filter(encoding => encoding.name === 'replacement')
+         .forEach(encoding => {
+           encoding.labels.forEach(label => { replacement_labels.push(label); })
+         });
+});
+
+replacement_labels.forEach(label => {
+  decode_test(
+    label,
+    '%41%42%43%61%62%63%31%32%33%A0',
+    'U+FFFD',
+    `${label} - non-empty input decodes to one replacement character.`);
+
+  decode_test(
+    label,
+    '',
+    '', `${label} - empty input decodes to empty output.`);
+});
