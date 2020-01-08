2e1b24434a4dad55a810f32184db6c3473100ed3
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 15:35:31 +0000

    Bug 1501869 - Part 1: Refine the mechanism for VR tasks. r=kip
    Differential Revision: https://phabricator.services.mozilla.com/D9879
    
    UltraBlame original commit: cdb87cbfe882997bdcbd39d5380738a42aa07a27

--- gfx/vr/VRManager.cpp
+++ gfx/vr/VRManager.cpp
@@ -166,7 +166,6 @@ VRManager::Shutdown()
 void
 VRManager::Init()
 {
-  StartTasks();
   mInitialized = true;
 }
 
@@ -447,6 +446,7 @@ VRManager::NotifyVRVsync(const uint32_t& aDisplayID)
 void
 VRManager::EnumerateVRDisplays()
 {
+  StartTasks();
   
 
 
