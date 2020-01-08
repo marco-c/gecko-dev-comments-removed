bb48aec50fb09f4c54b568cb5def08e38814c943
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 16:13:48 +0000

    Bug 1459603 - Remove the checkbox from the persistent storage notification. r=johannh
    Remove the checkbox from the persistent storage notification.
    
    Differential Revision: https://phabricator.services.mozilla.com/D8891
    
    UltraBlame original commit: d52479cf19f9780653ba0cf0be873bfe36bb8cfd

--- browser/modules/test/browser/browser_PermissionUI_prompts.js
+++ browser/modules/test/browser/browser_PermissionUI_prompts.js
@@ -68,8 +68,9 @@ async function testPrompt(Prompt) {
     popupNotification.checkbox.checked = false;
 
     let isNotificationPrompt = Prompt == PermissionUI.DesktopNotificationPermissionPrompt;
+    let isPersistentStoragePrompt = Prompt == PermissionUI.PersistentStoragePermissionPrompt;
 
-    let expectedSecondaryActionsCount = isNotificationPrompt ? 2 : 1;
+    let expectedSecondaryActionsCount = isNotificationPrompt || isPersistentStoragePrompt ? 2 : 1;
     Assert.equal(notification.secondaryActions.length, expectedSecondaryActionsCount,
                  "There should only be " + expectedSecondaryActionsCount + " secondary action(s)");
     await clickSecondaryAction();
@@ -97,7 +98,7 @@ async function testPrompt(Prompt) {
     
     popupNotification = getPopupNotificationNode();
     let secondaryActionToClickIndex = 0;
-    if (isNotificationPrompt) {
+    if (isNotificationPrompt || isPersistentStoragePrompt) {
       secondaryActionToClickIndex = 1;
     } else {
       popupNotification.checkbox.checked = true;
