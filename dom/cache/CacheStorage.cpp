22338a77457431e65e8f6ee0cee37a6212a7ab2b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 09:58:25 +0000

    Bug 1489844 - Port DOMPrefs to StaticPrefs - part 3 - dom.caches.testing.enabled, r=ehsan
    UltraBlame original commit: b94e1eb738f193957f056de06333b38c810fcd55

--- dom/cache/CacheStorage.cpp
+++ dom/cache/CacheStorage.cpp
@@ -26,6 +26,7 @@
 #include "mozilla/ipc/BackgroundUtils.h"
 #include "mozilla/ipc/PBackgroundChild.h"
 #include "mozilla/ipc/PBackgroundSharedTypes.h"
+#include "mozilla/StaticPrefs.h"
 #include "nsContentUtils.h"
 #include "nsIDocument.h"
 #include "nsIGlobalObject.h"
@@ -207,7 +208,7 @@ CacheStorage::CreateOnWorker(Namespace aNamespace, nsIGlobalObject* aGlobal,
   
   
   
-  bool testingEnabled = DOMPrefs::DOMCachesTestingEnabled() ||
+  bool testingEnabled = StaticPrefs::dom_caches_testing_enabled() ||
                         DOMPrefs::ServiceWorkersTestingEnabled() ||
                         aWorkerPrivate->ServiceWorkersTestingInWindow() ||
                         aWorkerPrivate->IsServiceWorker();
