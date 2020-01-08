e7b19c90b0709b6c1442123ee50af590bd817869
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 16:35:10 +0000

    Bug 1499507 - Don't collect line numbers for profiling stack frames. r=njn
    They were not displayed in the UI, and the instructions to initialize the line
    field of a stack frame increased code size unnecessarily.
    
    This change reduces the binary size on Linux x64 by around 100KB.
    
    Here's a diff of the impact on the code generated for Attr_Binding::get_specified
    in the Mac build:
    
     -20,17 +20,16 
        movq       0x8(%rbx), %rax
        movq       %r12, %rcx
        shlq       $0x5, %rcx
        leaq       aGetAttrspecifi, %rdx                       ; "get Attr.specified"
        movq       %rdx, (%rax,%rcx)
        movq       $0x0, 0x8(%rax,%rcx)
        leaq       -40(%rbp), %rdx
        movq       %rdx, 0x10(%rax,%rcx)
    -   movl       $0x106, 0x18(%rax,%rcx)
        movl       $0x1c, 0x1c(%rax,%rcx)
        leal       0x1(%r12), %eax
        movl       %eax, 0x10(%rbx)
    
        movq       %r15, %rdi
        call       __ZNK7mozilla3dom4Attr9SpecifiedEv          ; mozilla::dom::Attr::Specified() const
        movzxl     %al, %eax
        movabsq    $0xfff9000000000000, %rcx
    
    Depends on D9193
    
    Differential Revision: https://phabricator.services.mozilla.com/D9195
    
    UltraBlame original commit: cd066bdf78f52f98533d9866ddfd357d47f16f7f

--- dom/script/ScriptSettings.cpp
+++ dom/script/ScriptSettings.cpp
@@ -645,7 +645,7 @@ AutoEntryScript::AutoEntryScript(nsIGlobalObject* aGlobalObject,
   
   , mCallerOverride(cx())
 #ifdef MOZ_GECKO_PROFILER
-  , mAutoProfilerLabel("AutoEntryScript", aReason, __LINE__,
+  , mAutoProfilerLabel("AutoEntryScript", aReason,
                        js::ProfilingStackFrame::Category::JS)
 #endif
 {
