12329eb78cf594a8d09a33c516e069fe2aaae8e6
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 13:37:10 +0000

    Bug 1496649 Use modern MozAfterPaint goodness in tresize r=mconley
    Some new features were added to MozAfterPaint events in bugs 1264409 and
    1264798 to allow tests to make more accurate measurements.  Updating
    tresize to use these features is not directly related to the main goal
    of this bug, but lets do it anyway while we're touching tresize...
    
    UltraBlame original commit: 8d9338c622944658d6737ff7831d1c14aa789a43

--- testing/talos/talos/tests/tresize/addon/content/tresize.js
+++ testing/talos/talos/tests/tresize/addon/content/tresize.js
@@ -6,69 +6,62 @@
 
 
 
-var dataSet = [];
-var windowSize = 425;
-var resizeIncrement = 2;
-var count = 0;
-var max = 300;
+async function runTest(callback, locationSearch) {
+  const INCREMENT = 2;
+  const MAX = 300;
 
-var dumpDataSet = false;
-var doneCallback = null;
+  
+  
+  
+  function measurePaintTime(action, marker) {
+    return new Promise(resolve => {
+      let startTime;
+      let lastTransaction = window.windowUtils.lastTransactionId;
+      function painted(event) {
+        if (event.transactionId <= lastTransaction) {
+          return;
+        }
 
-function painted() {
-  window.removeEventListener("MozAfterPaint", painted, true);
-  var paintTime = window.performance.now();
-  Profiler.pause("resize " + count);
-  dataSet[count].end = paintTime;
-  setTimeout(resizeCompleted, 20);
-}
-
-function resizeTest() {
-  try {
-    windowSize += resizeIncrement;
-    window.addEventListener("MozAfterPaint", painted, true);
-    Profiler.resume("resize " + count);
-    dataSet[count] = {"start": window.performance.now()};
-    window.resizeTo(windowSize, windowSize);
-  } catch (ex) { finish({error: ex.message}); }
-}
-
-function testCompleted() {
-  try {
-    Profiler.finishTest();
-    var total = 0;
-    var diffs = [];
-    for (var idx = 0; idx < count; idx++) {
-      var diff = dataSet[idx].end - dataSet[idx].start;
-      total += diff;
-      diffs.push(diff);
-    }
-    var average = (total / count);
-    finish({diffs, average});
-  } catch (ex) { finish({error: ex.message}); }
-}
-
-function resizeCompleted() {
-  count++;
-  if (count >= max) {
-    testCompleted();
-  } else {
-    resizeTest();
+        if (marker) {
+          Profiler.pause(marker);
+        }
+        window.removeEventListener("MozAfterPaint", painted, true);
+        let time = event.paintTimeStamp - startTime;
+        resolve(time);
+      }
+      window.addEventListener("MozAfterPaint", painted, true);
+      if (marker) {
+        Profiler.resume(marker);
+      }
+      startTime = window.performance.now();
+      action();
+    });
   }
-}
 
-function runTest(callback, locationSearch) {
-  doneCallback = callback;
-  window.moveTo(10, 10);
-  window.resizeTo(windowSize, windowSize);
+  
+  let windowSize = 425;
+  await measurePaintTime(() => {
+    window.moveTo(10, 10);
+    window.resizeTo(windowSize, windowSize);
+  });
+
   Profiler.initFromURLQueryParams(locationSearch);
   Profiler.beginTest("tresize");
-  resizeTest();
-}
 
-function finish(msgs) {
-  if (doneCallback) {
-    doneCallback(msgs);
+  let times = [];
+  for (let i = 0; i < MAX; i++) {
+    const marker = `resize ${i}`;
+    windowSize += INCREMENT;
+
+    let time = await measurePaintTime(() => {
+      window.resizeTo(windowSize, windowSize);
+    }, marker);
+    times.push(time);
+
+    await new Promise(resolve => setTimeout(resolve, 20));
   }
-}
 
+  let total = times.reduce((a, b) => a + b);
+  let average = total / times.length;
+  callback({average});
+}
