39528396327492cc8c980352205dbd722deb4b31
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 17:47:29 +0000

    bug 1497111 - Remove unnecessarily null'ed out values of PaymentRequest.webidl. r=baku
    UltraBlame original commit: c4d4368566b4135bf0ae0c435926ebe525de6d47

--- dom/payments/PaymentRequest.cpp
+++ dom/payments/PaymentRequest.cpp
@@ -471,12 +471,14 @@ PaymentRequest::IsValidDetailsUpdate(const PaymentDetailsUpdate& aDetails,
 {
   nsAutoString message;
   
-  nsresult rv = IsValidCurrencyAmount(NS_LITERAL_STRING("details.total"),
-                                      aDetails.mTotal.mAmount,
-                                      true, 
-                                      message);
-  if (NS_FAILED(rv)) {
-    return rv;
+  if (aDetails.mTotal.WasPassed()) {
+    nsresult rv = IsValidCurrencyAmount(NS_LITERAL_STRING("details.total"),
+                                        aDetails.mTotal.Value().mAmount,
+                                        true, 
+                                        message);
+    if (NS_FAILED(rv)) {
+      return rv;
+    }
   }
   return IsValidDetailsBase(aDetails, aRequestShipping, message);
 }
@@ -531,12 +533,14 @@ PaymentRequest::IsValidDetailsBase(const PaymentDetailsBase& aDetails,
       if (NS_FAILED(rv)) {
         return rv;
       }
-      rv = IsValidCurrencyAmount(NS_LITERAL_STRING("details.modifiers.total"),
-                                 modifier.mTotal.mAmount,
-                                 true, 
-                                 aErrorMsg);
-      if (NS_FAILED(rv)) {
-        return rv;
+      if (modifier.mTotal.WasPassed()) {
+        rv = IsValidCurrencyAmount(NS_LITERAL_STRING("details.modifiers.total"),
+                                   modifier.mTotal.Value().mAmount,
+                                   true, 
+                                   aErrorMsg);
+        if (NS_FAILED(rv)) {
+          return rv;
+        }
       }
       if (modifier.mAdditionalDisplayItems.WasPassed()) {
         const Sequence<PaymentItem>& displayItems = modifier.mAdditionalDisplayItems.Value();
