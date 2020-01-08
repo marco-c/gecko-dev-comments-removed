bdbbd2e4a19032453599d40a13eda05d053be6f7
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 07:29:26 +0000

    Backed out changeset 32e2d0ddc56a (bug 1477100) for build bustages on editDialog.css. CLOSED TREE
    UltraBlame original commit: cd628c962e757d258c5fca92e9586e88851caf7c

--- browser/components/payments/res/containers/basic-card-form.js
+++ browser/components/payments/res/containers/basic-card-form.js
@@ -188,9 +188,6 @@ export default class BasicCardForm extends PaymentStateSubscriberMixin(PaymentRe
         billingAddressSelect.value = Object.keys(addresses)[0];
       }
     }
-    
-    
-    this.formHandler.updatePopulatedState(billingAddressSelect);
 
     this.updateRequiredState();
     this.updateSaveButtonState();
