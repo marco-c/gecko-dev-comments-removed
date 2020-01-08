d943b437e41a83e86d3a88099f999d78b0c951f0
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 20:59:07 +0000

    Bug 1499655 - Move the flushLayout code into forceLayerTreeToCompositor. r=botond
    These two bits of code conceptually belong together, since the
    flushLayout code is intended to work around a case where
    forceLayerTreeToCompositor wasn't doing anything. So it makes sense to
    just put them into the same function.
    
    Differential Revision: https://phabricator.services.mozilla.com/D14205
    
    UltraBlame original commit: 14a53c3f5f7f189842b9d4b614579ed09d71af4a

--- gfx/layers/apz/test/mochitest/apz_test_utils.js
+++ gfx/layers/apz/test/mochitest/apz_test_utils.js
@@ -394,6 +394,8 @@ async function waitUntilApzStable() {
 
 
 async function forceLayerTreeToCompositor() {
+  
+  document.body.style.left = "1px";
   var utils = SpecialPowers.getDOMWindowUtils(window);
   if (!utils.isMozAfterPaintPending) {
     dump("Forcing a paint since none was pending already...\n");
