e66d7a25e616933a41f322ad9e55f0aefe360045
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 15:39:55 +0000

    Bug 1500897 - geckoview: only spin beetmover tasks on GECKOVIEW_XX_RELBRANCH r=dustin,tomprince
    geckoview: only spin beetmover tasks on GECKOVIEW_XX_RELBRANCH
    
    Differential Revision: https://phabricator.services.mozilla.com/D9551
    
    UltraBlame original commit: 90ccf3e2726d525f10872e97f94c5bbc3395d065

--- taskcluster/taskgraph/transforms/task.py
+++ taskcluster/taskgraph/transforms/task.py
@@ -158,6 +158,9 @@ task_description_schema = Schema({
     Optional('run-on-projects'): optionally_keyed_by('build-platform', [basestring]),
 
     
+    Optional('run-on-hg-branches'): optionally_keyed_by('project', [basestring]),
+
+    
     
     Required('shipping-phase'): Any(
         None,
