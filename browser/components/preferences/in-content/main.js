2ce4f86fb305a77c464106089fdedc8a14c56dc8
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 12:50:19 +0000

    Bug 1469696 - Part 5: Test listing and installing AMO langpacks r=aswan,jaws
    Differential Revision: https://phabricator.services.mozilla.com/D6315
    
    UltraBlame original commit: 54abaf608a606310adfebfa816051aa55bf6475a

--- browser/components/preferences/in-content/main.js
+++ browser/components/preferences/in-content/main.js
@@ -809,6 +809,7 @@ var gMainPane = {
 
     
     let menuitem = document.createXULElement("menuitem");
+    menuitem.id = "defaultBrowserLanguageSearch";
     menuitem.setAttribute(
       "label", await document.l10n.formatValue("browser-languages-search"));
     menuitem.addEventListener("command", () => {
