519d9dba77fd046098af9e8cc4b48d64bcabc410
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 09:18:27 +0000

    Bug 1440879 - Ensure all the stl_wrappers end up in the generated-files tarball. r=froydnj
    UltraBlame original commit: 93892cfed0155d11a86c199785bde65b2f59801a

--- python/mozbuild/mozbuild/frontend/data.py
+++ python/mozbuild/mozbuild/frontend/data.py
@@ -1168,9 +1168,8 @@ class GeneratedFile(ContextDerived):
             '.inc',
             '.py',
             '.rs',
-            'new', 
         )
-        self.required_for_compile = [f for f in self.outputs if f.endswith(suffixes)]
+        self.required_for_compile = [f for f in self.outputs if f.endswith(suffixes) or 'stl_wrappers/' in f]
 
 
 class ChromeManifestEntry(ContextDerived):
