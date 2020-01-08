2a5fc2e03701f4486a854020b89fcef3abd751d0
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 11:12:51 +0000

    Bug 1491395. Replace BorderWidths with LayoutSideOffsets. on a CLOSED TREE
    UltraBlame original commit: 32f29c236ddee791f097e2427d330739dfbec070

--- layout/tables/nsTableFrame.cpp
+++ layout/tables/nsTableFrame.cpp
@@ -7554,7 +7554,7 @@ BCBlockDirSeg::CreateWebRenderCommands(BCPaintBorderIterator& aIter,
 
   
   
-  wr::BorderWidths borderWidths = wr::ToBorderWidths(roundedRect.size.width,
+  wr::LayoutSideOffsets borderWidths = wr::ToBorderWidths(roundedRect.size.width,
                                                      roundedRect.size.width,
                                                      roundedRect.size.width,
                                                      roundedRect.size.width);
@@ -7841,7 +7841,7 @@ BCInlineDirSeg::CreateWebRenderCommands(BCPaintBorderIterator& aIter,
 
   
   
-  wr::BorderWidths borderWidths = wr::ToBorderWidths(roundedRect.size.height,
+  wr::LayoutSideOffsets borderWidths = wr::ToBorderWidths(roundedRect.size.height,
                                                      roundedRect.size.height,
                                                      roundedRect.size.height,
                                                      roundedRect.size.height);
@@ -8086,7 +8086,7 @@ nsTableFrame::CreateWebRenderCommandsForBCBorders(wr::DisplayListBuilder& aBuild
 
   LayoutDeviceRect allBorderRect;
   wr::BorderSide wrSide[4];
-  wr::BorderWidths wrWidths;
+  wr::LayoutSideOffsets wrWidths;
   wr::BorderRadius borderRadii = wr::EmptyBorderRadius();
   bool backfaceIsVisible = false;
   NS_FOR_CSS_SIDES(side) {
