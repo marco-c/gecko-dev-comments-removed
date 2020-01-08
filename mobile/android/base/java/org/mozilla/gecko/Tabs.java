90798d43c993721f7ab0783c62e0b5635079fb9b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 12:51:47 +0000

    Backed out changeset a42437c2e1fe (bug 1478735) for failures in mobile/android/tests/browser/chrome/test_session_parentid.html
    UltraBlame original commit: bde685714bea04e95e8fc69e4da2cd51039d3bcd

--- mobile/android/base/java/org/mozilla/gecko/Tabs.java
+++ mobile/android/base/java/org/mozilla/gecko/Tabs.java
@@ -67,7 +67,6 @@ public class Tabs implements BundleEventListener {
     
     
     private volatile Tab mSelectedTab;
-    private volatile int mPreviouslySelectedTabId = INVALID_TAB_ID;
 
     
     private final HashMap<Integer, Tab> mTabs = new HashMap<Integer, Tab>();
@@ -338,7 +337,6 @@ public class Tabs implements BundleEventListener {
         notifyListeners(tab, TabEvents.SELECTED);
 
         if (oldTab != null) {
-            mPreviouslySelectedTabId = oldTab.getId();
             notifyListeners(oldTab, TabEvents.UNSELECTED);
         }
 
@@ -494,12 +492,15 @@ public class Tabs implements BundleEventListener {
             }
         }
 
-        final Tab parentTab = getTab(tab.getParentId());
-        if (tab.getParentId() == mPreviouslySelectedTabId && tab.getParentId() != INVALID_TAB_ID && parentTab != null) {
-            return parentTab;
-        } else {
-            return nextTab;
+        Tab parent = getTab(tab.getParentId());
+        if (parent != null) {
+            
+            if (nextTab != null && nextTab.getParentId() == tab.getParentId())
+                return nextTab;
+            else
+                return parent;
         }
+        return nextTab;
     }
 
     public Iterable<Tab> getTabsInOrder() {
