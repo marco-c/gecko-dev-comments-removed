1840978e46815775fb86504917f4cd95eae0ce81
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 14:05:28 +0000

    Backed out 2 changesets (bug 1498379, bug 1498381) for permafailing at /test/tabs/browser_multiselect_tabs_drag_to_bookmarks_toolbar.js a=backout
    Backed out changeset 0178681fab81 (bug 1498379)
    Backed out changeset 962c7631d98c (bug 1498381)
    
    UltraBlame original commit: 4b02380c0bbb5151f1a1f4606c29f2a1cbb70225

--- browser/base/content/test/general/browser_homeDrop.js
+++ browser/base/content/test/general/browser_homeDrop.js
@@ -19,8 +19,6 @@ add_task(async function() {
     let setHomepageDialogPromise = BrowserTestUtils.domWindowOpened();
 
     EventUtils.synthesizeDrop(dragSrcElement, homeButton, dragData, "copy", window);
-    
-    EventUtils.synthesizeMouseAtCenter(homeButton, { type: "mouseup" }, window);
 
     let setHomepageDialog = await setHomepageDialogPromise;
     ok(true, "dialog appeared in response to home button drop");
@@ -70,8 +68,6 @@ add_task(async function() {
         
         expectUncaughtException();
         EventUtils.synthesizeDrop(dragSrcElement, homeButton, [[{type: "text/plain", data: "javascript:8888"}]], "copy", window);
-        
-        EventUtils.synthesizeMouseAtCenter(homeButton, { type: "mouseup" }, window);
       });
     });
   }
