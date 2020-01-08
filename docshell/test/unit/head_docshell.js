ff57b1ba415061a528d9522e436eef1901903169
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 16:42:19 +0000

    Bug 1496082: Enable ESLint for docshell/test/navigation and docshell/test/unit (manual fixes) r=bzbarsky
    Implemented the manual linting fixes for docshell/test/navigation, docshell/test/unit and docshell/test/unit_ipc
    Depends on D9430
    
    Differential Revision: https://phabricator.services.mozilla.com/D10080
    
    UltraBlame original commit: 776b54474da922d36446aa7cb6e93354767d0913

--- docshell/test/unit/head_docshell.js
+++ docshell/test/unit/head_docshell.js
@@ -1,7 +1,7 @@
 
 
 
+ChromeUtils.import("resource://gre/modules/Services.jsm");
 
-var dirSvc = Cc["@mozilla.org/file/directory_service;1"].
-             getService(Ci.nsIProperties);
+var dirSvc = Services.dirSvc;
 var profileDir = do_get_profile();
