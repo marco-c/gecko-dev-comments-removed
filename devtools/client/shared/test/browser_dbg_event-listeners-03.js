245000f8c4296818e65defc44fb69c79d34841e0
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 15:29:49 +0000

    Bug 1500986 - Migrate old event-listener and worker debugger tests  r=jdescottes
    UltraBlame original commit: ca3eb80797aa25c80a673fb397e7df1a25c9a775

---
+++ devtools/client/shared/test/browser_dbg_event-listeners-03.js
@@ -0,0 +1,92 @@
+
+
+
+
+
+"use strict";
+
+
+
+
+
+
+
+
+Services.scriptloader.loadSubScript(
+  "chrome://mochitests/content/browser/devtools/client/shared/test/helper_workers.js",
+  this);
+
+var { DebuggerServer } = require("devtools/server/main");
+var { DebuggerClient } = require("devtools/shared/client/debugger-client");
+
+const TAB_URL = TEST_URI_ROOT + "doc_native-event-handler.html";
+
+var gClient;
+var gTab;
+
+function test() {
+  DebuggerServer.init();
+  DebuggerServer.registerAllActors();
+
+  const transport = DebuggerServer.connectPipe();
+  gClient = new DebuggerClient(transport);
+  gClient.connect().then(([aType, aTraits]) => {
+    is(aType, "browser",
+      "Root actor should identify itself as a browser.");
+
+    addTab(TAB_URL)
+      .then((tab) => {
+        gTab = tab;
+        return attachThreadActorForUrl(gClient, TAB_URL);
+      })
+      .then(pauseDebuggee)
+      .then(testEventListeners)
+      .then(() => gClient.close())
+      .then(finish)
+      .catch(error => {
+        ok(false, "Got an error: " + error.message + "\n" + error.stack);
+      });
+  });
+}
+
+function pauseDebuggee(threadClient) {
+  const deferred = getDeferredPromise().defer();
+
+  gClient.addOneTimeListener("paused", (event, packet) => {
+    is(packet.type, "paused",
+      "We should now be paused.");
+    is(packet.why.type, "debuggerStatement",
+      "The debugger statement was hit.");
+
+    deferred.resolve(threadClient);
+  });
+
+  generateMouseClickInTab(gTab, "content.document.querySelector('button')");
+
+  return deferred.promise;
+}
+
+function testEventListeners(threadClient) {
+  const deferred = getDeferredPromise().defer();
+
+  threadClient.eventListeners(packet => {
+    if (packet.error) {
+      const msg = "Error getting event listeners: " + packet.message;
+      ok(false, msg);
+      deferred.reject(msg);
+      return;
+    }
+
+    
+    
+    
+    is(packet.listeners.length, 2, "Found all event listeners.");
+    threadClient.resume(deferred.resolve);
+  });
+
+  return deferred.promise;
+}
+
+registerCleanupFunction(function() {
+  gClient = null;
+});
