f2649e5610cc0e16d9caf0e647a8cbdd1d8bb3af
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 09:59:06 +0000

    Bug 1489844 - Port DOMPrefs to StaticPrefs - part 19 - privacy.resistFingerprinting.autoDeclineNoUserInputCanvasPrompts, r=ehsan
    UltraBlame original commit: c236c5795ee6bc62c31edd3b2ba29dbcc1badf19

--- dom/base/DOMPrefsInternal.h
+++ dom/base/DOMPrefsInternal.h
@@ -19,7 +19,6 @@
 
 
 
-DOM_PREF(EnableAutoDeclineCanvasPrompts, "privacy.resistFingerprinting.autoDeclineNoUserInputCanvasPrompts")
 DOM_PREF(DevToolsEnabled, "devtools.enabled")
 DOM_PREF(PerformanceObserverEnabled, "dom.enable_performance_observer")
 DOM_PREF(IndexedDBStorageOptionsEnabled, "dom.indexedDB.storageOption.enabled")
