2829d368caeaf6c3e52ab6cf65246d81831f2a88
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 07:19:51 +0000

    Bug 1482377 [wpt PR 12405] - Convert compat+console+fullscreen+storage+url+xhr to idl_test, a=testonly
    Automatic update from web-platform-testsConvert compat+console+fullscreen+storage+url+xhr to idl_test (#12405)
    
    Also add more objects in some of these tests.
    
    XHR will now be tested against workers as well, and local tested
    revealed a test will be failing due to an old TODO in Blink:
    https://cs.chromium.org/chromium/src/third_party/blink/renderer/core/xmlhttprequest/xml_http_request.idl?l=79&rcl=9c356a9697625b3c25856529908af0c3253dfa2c
    
    A failing test for that is good :)
    --
    
    wpt-commits: 9786a4b1317c03b89ea3bf2e997f2e2b6a3690ae
    wpt-pr: 12405
    
    UltraBlame original commit: fc2710055c3a592c52ccf5e94e008765175afbf9

--- testing/web-platform/tests/console/idlharness.any.js
+++ testing/web-platform/tests/console/idlharness.any.js
@@ -3,12 +3,7 @@
 
 
 
-promise_test(async () => {
-  const srcs = ['console'];
-  const [idl] = await Promise.all(
-      srcs.map(i => fetch(`/interfaces/${i}.idl`).then(r => r.text())));
-
-  const idl_array = new IdlArray();
-  idl_array.add_idls(idl);
-  idl_array.test();
-}, 'console interfaces');
+idl_test(
+  ['console'],
+  [] 
+);
