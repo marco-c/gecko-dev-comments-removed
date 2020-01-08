5ef42e2e9cd23e0fb6e94bf12a36fe6a16f68ea4
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 16:04:14 +0000

    Backed out 2 changesets (bug 1487022) for causing spidermonkey bustages in /builds/worker/workspace/build/src/js/src/jit-test/tests/wasm/memory.js CLOSED TREE
    Backed out changeset 860712299944 (bug 1487022)
    Backed out changeset 1cf53a430311 (bug 1487022)
    
    UltraBlame original commit: 405215e36bd0b694b453c17ca9616adb0b81bcc7

--- js/src/jit/BaselineInspector.cpp
+++ js/src/jit/BaselineInspector.cpp
@@ -760,25 +760,17 @@ BaselineInspector::hasSeenNonStringIterMore(jsbytecode* pc)
     return stub->toIteratorMore_Fallback()->hasNonStringResult();
 }
 
-
-
-
 bool
-BaselineInspector::hasSeenDoubleResult(jsbytecode* pc, bool defaultIfEmpty)
+BaselineInspector::hasSeenDoubleResult(jsbytecode* pc)
 {
     if (!hasBaselineScript()) {
         return false;
     }
 
     const ICEntry& entry = icEntryFromPC(pc);
-    ICFallbackStub* stub = entry.fallbackStub();
+    ICStub* stub = entry.fallbackStub();
 
     MOZ_ASSERT(stub->isUnaryArith_Fallback() || stub->isBinaryArith_Fallback());
-
-    
-    if (stub->state().numOptimizedStubs() == 0 && !entry.fallbackStub()->state().hasFailures()) {
-        return defaultIfEmpty;
-    }
 
     if (stub->isUnaryArith_Fallback()) {
         return stub->toUnaryArith_Fallback()->sawDoubleResult();
