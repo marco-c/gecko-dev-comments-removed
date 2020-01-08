d4498849214889ea8d45ca0d8365655f466b3528
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 02:53:01 +0000

    Bug 971462 - Hide event type from constructor of WidgetCommandEvent r=smaug
    The constructor of WidgetCommandEvent takes 2 nsAtom pointers.  One is for
    specifying event type, the other is for specifying the command.  The
    difference of these arguments are pretty unclear for other developers and
    the former argument is always nsGkAtoms::onAppCommand unless nullptr in
    C++ code.  So, we can hide the former argument.
    
    Then, we should create another constructor for creating empty command event
    from constructor of dom::CommandEvent.
    
    Differential Revision: https://phabricator.services.mozilla.com/D2506
    
    UltraBlame original commit: 86f8589018f6fa44d6a8cb92e8a1d24eb9c4082c

--- widget/MiscEvents.h
+++ widget/MiscEvents.h
@@ -11,6 +11,7 @@
 #include "mozilla/BasicEvents.h"
 #include "nsCOMPtr.h"
 #include "nsAtom.h"
+#include "nsGkAtoms.h"
 #include "nsITransferable.h"
 
 namespace mozilla {
@@ -106,6 +107,7 @@ class WidgetCommandEvent : public WidgetGUIEvent
 public:
   virtual WidgetCommandEvent* AsCommandEvent() override { return this; }
 
+protected:
   WidgetCommandEvent(bool aIsTrusted, nsAtom* aEventType,
                      nsAtom* aCommand, nsIWidget* aWidget)
     : WidgetGUIEvent(aIsTrusted, eUnidentifiedEvent, aWidget,
@@ -113,6 +115,25 @@ public:
     , mCommand(aCommand)
   {
     mSpecifiedEventType = aEventType;
+  }
+
+public:
+  
+
+
+
+  WidgetCommandEvent(bool aIsTrusted, nsAtom* aCommand, nsIWidget* aWidget)
+    : WidgetCommandEvent(aIsTrusted, nsGkAtoms::onAppCommand,
+                         aCommand, aWidget)
+  {
+  }
+
+  
+
+
+  WidgetCommandEvent()
+    : WidgetCommandEvent(false, nullptr, nullptr, nullptr)
+  {
   }
 
   virtual WidgetEvent* Duplicate() const override
