45a75fecc06bc38f6ee677160fc57091f725b952
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 13:29:47 +0000

    Bug 1497598.  Stop using GetCurrentJSContext in loadcontext code. r=mccr8
    Differential Revision: https://phabricator.services.mozilla.com/D8122
    
    UltraBlame original commit: 99cc4ced26e020d6a31335647738c361f86e4bfe

--- docshell/base/nsDocShell.cpp
+++ docshell/base/nsDocShell.cpp
@@ -13704,12 +13704,10 @@ nsDocShell::GetIsTopLevelContentDocShell(bool* aIsTopLevelContentDocShell)
 
 
 NS_IMETHODIMP
-nsDocShell::GetScriptableOriginAttributes(JS::MutableHandle<JS::Value> aVal)
+nsDocShell::GetScriptableOriginAttributes(JSContext* aCx,
+                                          JS::MutableHandle<JS::Value> aVal)
 {
-  JSContext* cx = nsContentUtils::GetCurrentJSContext();
-  MOZ_ASSERT(cx);
-
-  return GetOriginAttributes(cx, aVal);
+  return GetOriginAttributes(aCx, aVal);
 }
 
 
