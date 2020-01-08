47d527e3a0af2619b2e5deaad2441e823587b245
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 07:24:02 +0000

    Bug 1483014 [wpt PR 12436] - Rewrite WebVTT IDL tests to use idl_test, a=testonly
    Automatic update from web-platform-testsRewrite WebVTT IDL tests to use idl_test (#12436)
    
    The results are unchanged on Chrome and Firefox. Chrome fails 4/55
    tests and Firefox passes them all.
    --
    
    wpt-commits: e90a5159a53432d57a15fd9b6ec605e411ee776d
    wpt-pr: 12436
    
    UltraBlame original commit: 439e0e17dc881a2e97b47ab7a3411730201bbda0

---
+++ testing/web-platform/tests/webvtt/api/idlharness.window.js
@@ -0,0 +1,15 @@
+
+
+
+'use strict';
+
+idl_test(
+  ['webvtt'],
+  ['html', 'dom'],
+  idl_array => {
+    idl_array.add_objects({
+      VTTCue: ['new VTTCue(0, 0, "")'],
+      VTTRegion: ['new VTTRegion()'],
+    });
+  }
+);
