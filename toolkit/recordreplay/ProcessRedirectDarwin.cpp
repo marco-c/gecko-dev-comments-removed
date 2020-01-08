7d71f910eae9ec41d2b99ee37478695ee6f86e3d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 16:59:23 +0000

    Bug 1505139 - Tolerate getenv races in JPEG library initialization, r=lsmyth.
    UltraBlame original commit: 218da4526e22db6d81ab827c7f5ec9e44a8fc92f

--- toolkit/recordreplay/ProcessRedirectDarwin.cpp
+++ toolkit/recordreplay/ProcessRedirectDarwin.cpp
@@ -169,7 +169,7 @@ namespace recordreplay {
   MACRO(fseek, RR_SaveRvalHadErrorNegative)                      \
   MACRO(ftell, RR_SaveRvalHadErrorNegative)                      \
   MACRO(fwrite, RR_ScalarRval)                                   \
-  MACRO(getenv, RR_CStringRval, nullptr, nullptr, Preamble_Veto<0>) \
+  MACRO(getenv, RR_CStringRval, Preamble_getenv, nullptr, Preamble_Veto<0>) \
   MACRO(localtime_r, RR_SaveRvalHadErrorZero<RR_Compose<         \
                        RR_WriteBufferFixedSize<1, sizeof(struct tm)>, \
                        RR_RvalIsArgument<1>>>)                   \
@@ -1357,6 +1357,23 @@ RR_fread(Stream& aEvents, CallArguments* aArguments, ErrorType* aError)
   aEvents.CheckInput(capacity);
   MOZ_RELEASE_ASSERT(rval <= capacity);
   aEvents.RecordOrReplayBytes(buf, rval * elemSize);
+}
+
+static PreambleResult
+Preamble_getenv(CallArguments* aArguments)
+{
+  
+  
+  auto env = aArguments->Arg<0, const char*>();
+
+  
+  
+  if (strncmp(env, "JSIMD_", 6) == 0) {
+    aArguments->Rval<char*>() = nullptr;
+    return PreambleResult::Veto;
+  }
+
+  return PreambleResult::Redirect;
 }
 
 static struct tm gGlobalTM;
