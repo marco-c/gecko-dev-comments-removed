340799672191e9d4401909ca7529e232a694bafb
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 14:45:51 +0000

    Bug 1499895 - [2.1] Update onLoadRequest tests. r=jchen
    UltraBlame original commit: b9614de52765a2da6de9eb067c2431c9517b95cd

--- mobile/android/geckoview/src/androidTest/java/org/mozilla/geckoview/test/TestRunnerActivity.java
+++ mobile/android/geckoview/src/androidTest/java/org/mozilla/geckoview/test/TestRunnerActivity.java
@@ -51,8 +51,8 @@ public class TestRunnerActivity extends Activity {
         }
 
         @Override
-        public GeckoResult<AllowOrDeny> onLoadRequest(GeckoSession session, String uri,
-                                                      int target, int flags) {
+        public GeckoResult<AllowOrDeny> onLoadRequest(GeckoSession session,
+                                                  LoadRequest request) {
             
             return GeckoResult.fromValue(AllowOrDeny.ALLOW);
         }
