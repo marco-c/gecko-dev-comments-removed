e5354669bf0f296439f313428d12ebb1aa6f6d09
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 16:47:24 +0000

    Bug 1497448: Use try/catch for _handle function in ServerSocketConnection. r=ochameau,jdescottes
    At first, I wanted it is better to call DebuggerServer.destroy() when
    `devtools.debugger.remote-enabled` will set to false. Likewise, call
    DebuggerServer.init() in case of true. But, because DebuggerServer.init() is
    called by various context such as RemoteDebugger, restoring is difficult after
    destroying. In this patch, simply, we make avoiding to call
    DebuggerServer.closeAllListener() from inside of ServerSocketConnection.
    
    Depends on D10864
    
    Differential Revision: https://phabricator.services.mozilla.com/D10865
    
    UltraBlame original commit: 2f968d0631f513abd33a76f564a9c2b32377216c

--- devtools/shared/security/socket.js
+++ devtools/shared/security/socket.js
@@ -638,15 +638,17 @@ ServerSocketConnection.prototype = {
 
 
 
-  _handle() {
+  async _handle() {
     dumpn("Debugging connection starting authentication on " + this.address);
-    const self = this;
-    (async function() {
-      self._listenForTLSHandshake();
-      await self._createTransport();
-      await self._awaitTLSHandshake();
-      await self._authenticate();
-    })().then(() => this.allow()).catch(e => this.deny(e));
+    try {
+      this._listenForTLSHandshake();
+      await this._createTransport();
+      await this._awaitTLSHandshake();
+      await this._authenticate();
+      this.allow();
+    } catch (e) {
+      this.deny(e);
+    }
   },
 
   
