75bd1c415412f264898c49ec18fec2b0b4ea8cac
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 23:21:34 +0000

    Bug 1513057 - P1: Start the new socket process basics (prefs, full xpcom init, logging, no sandboxing) r=mayhemer,dragana
    Differential Revision: https://phabricator.services.mozilla.com/D14148
    
    UltraBlame original commit: c083f11eb58d510309b4e85578609dc8c2c7c902

--- netwerk/base/nsIOService.h
+++ netwerk/base/nsIOService.h
@@ -48,6 +48,7 @@ namespace mozilla {
 namespace net {
 class NeckoChild;
 class nsAsyncRedirectVerifyHelper;
+class SocketProcessHost;
 
 class nsIOService final : public nsIIOService,
                           public nsIObserver,
@@ -113,6 +114,11 @@ class nsIOService final : public nsIIOService,
   
   nsresult RecheckCaptivePortal();
 
+  void OnProcessLaunchComplete(SocketProcessHost* aHost, bool aSucceeded);
+  void OnProcessUnexpectedShutdown(SocketProcessHost* aHost);
+  bool SocketProcessReady();
+  void NotifySocketProcessPrefsChanged(const char* aName);
+
  private:
   
   
@@ -161,6 +167,9 @@ class nsIOService final : public nsIIOService,
   nsresult SpeculativeConnectInternal(nsIURI* aURI, nsIPrincipal* aPrincipal,
                                       nsIInterfaceRequestor* aCallbacks,
                                       bool aAnonymous);
+
+  nsresult LaunchSocketProcess();
+  void DestroySocketProcess();
 
  private:
   bool mOffline;
@@ -213,6 +222,9 @@ class nsIOService final : public nsIIOService,
 
   
   mozilla::Atomic<PRIntervalTime> mNetTearingDownStarted;
+
+  SocketProcessHost* mSocketProcess;
+  nsTArray<const char*> mQueuedPrefNames;
 
  public:
   
--- xpcom/build/nsXULAppAPI.h
+++ xpcom/build/nsXULAppAPI.h
@@ -444,6 +444,8 @@ XRE_API(bool, XRE_IsE10sParentProcess, ())
 #include "mozilla/GeckoProcessTypes.h"
 #undef GECKO_PROCESS_TYPE
 
+XRE_API(bool, XRE_IsSocketProcess, ())
+
 
 
 
