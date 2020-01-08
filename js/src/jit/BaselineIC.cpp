919200507e23d247bfeadb8180d9f11d830b4f3c
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 01:59:28 +0000

    Bug 1438727: [Part 2] Implement a subset of JSOP_SUB in CacheIR r=jandem
    UltraBlame original commit: cc58162b8a3a7ba5c153c01bb211afd24b4323f9

--- js/src/jit/BaselineIC.cpp
+++ js/src/jit/BaselineIC.cpp
@@ -4952,6 +4952,7 @@ DoCacheIRBinaryArithFallback(JSContext* cx, BaselineFrame* frame, ICBinaryArith_
     
     switch(op) {
       case JSOP_ADD:
+      case JSOP_SUB:
         break;
       default:
         return false; 
