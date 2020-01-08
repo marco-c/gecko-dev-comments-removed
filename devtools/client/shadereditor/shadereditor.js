ab9333cf9d5d71310a2ee09f85d4f47200da0da4
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 11:43:01 +0000

    Bug 1366534-convert uses of 'defer' to 'new Promise' in client/shadereditor;r=nchevobbe
    convert uses of "defer" to "new Promise" in client/shadereditor
    
    Differential Revision: https://phabricator.services.mozilla.com/D6220
    
    UltraBlame original commit: def8d4c0a39f707cab7d90d60a2281aca3e28b50

--- devtools/client/shadereditor/shadereditor.js
+++ devtools/client/shadereditor/shadereditor.js
@@ -6,7 +6,6 @@
 const {XPCOMUtils} = require("resource://gre/modules/XPCOMUtils.jsm");
 const {SideMenuWidget} = require("devtools/client/shared/widgets/SideMenuWidget.jsm");
 const promise = require("promise");
-const defer = require("devtools/shared/defer");
 const {Task} = require("devtools/shared/task");
 const EventEmitter = require("devtools/shared/event-emitter");
 const { HTMLTooltip } = require("devtools/client/shared/widgets/tooltip/HTMLTooltip");
@@ -415,22 +414,21 @@ class ShadersEditorsView {
       return this._editorPromises.get(type);
     }
 
-    const deferred = defer();
-    this._editorPromises.set(type, deferred.promise);
+    const promise = new Promise(resolve =>{
+      
+      
+      const parent = $("#" + type + "-editor");
+      const editor = new Editor(DEFAULT_EDITOR_CONFIG);
+      editor.config.mode = Editor.modes[type];
 
-    
-    
-    const parent = $("#" + type + "-editor");
-    const editor = new Editor(DEFAULT_EDITOR_CONFIG);
-    editor.config.mode = Editor.modes[type];
-
-    if (this._destroyed) {
-      deferred.resolve(editor);
-    } else {
-      editor.appendTo(parent).then(() => deferred.resolve(editor));
-    }
-
-    return deferred.promise;
+      if (this._destroyed) {
+        resolve(editor);
+      } else {
+        editor.appendTo(parent).then(() => resolve(editor));
+      }
+    });
+    this._editorPromises.set(type, promise);
+    return promise;
   }
 
   
