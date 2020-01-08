d99a4245d11190b39223324fc54668412d5b2303
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 03:50:11 +0000

    Backed out changeset 37ac6d023498 (bug 1481655) for bustage at build/src/gfx/layers/wr/WebRenderCommandBuilder.cpp on a CLOSED TREE
    UltraBlame original commit: 74910b7a444fbbdb6399a15e19593a3c30790eec

--- gfx/layers/wr/WebRenderCommandBuilder.cpp
+++ gfx/layers/wr/WebRenderCommandBuilder.cpp
@@ -748,9 +748,7 @@ struct DIGroup
         GP("doing children in EndGroup\n");
         aGrouper->PaintContainerItem(this, item, bounds, children, aContext, aRecorder);
       } else {
-        
-        
-        if (dirty && item->Type() != DisplayItemType::TYPE_COMPOSITOR_HITTEST_INFO) {
+        if (dirty) {
           
           
           
