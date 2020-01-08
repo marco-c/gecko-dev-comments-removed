51257137ddc635b62c05a5559d3ff30f84b0a73d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 09:27:22 +0000

    Bug 1483451 [wpt PR 12488] - Add WPT serialization tests for wasm modules, a=testonly
    Automatic update from web-platform-testsAdd WPT serialization tests for wasm modules
    
    These WPT tests are based on similar tests in
    wpt/html/infrastructure/safe-passing-of-structured-data/shared-array-buffers/
    
    This also fixes some test failures for the SharedArrayBuffer tests.
    
    Bug: chromium:798572
    Change-Id: I50b0edbebe4e97f5358513d248657d2831f48201
    Reviewed-on: https://chromium-review.googlesource.com/1175490
    Reviewed-by: Marijn Kruisselbrink <mekchromium.org>
    Commit-Queue: Ben Smith <binjichromium.org>
    Cr-Commit-Position: refs/heads/master{#586480}
    
    --
    
    wpt-commits: 3dae97c21757a4fdf03c43ccf5e68e4297d1b6ec
    wpt-pr: 12488
    
    UltraBlame original commit: 1df5c3c0f0bb1705cff1ae8b04284df670568c9a

---
+++ testing/web-platform/tests/wasm/serialization/resources/incrementer-worker-with-channel.js
@@ -0,0 +1,8 @@
+"use strict";
+importScripts("/resources/testharness.js");
+importScripts("./test-incrementer.js");
+
+self.onmessage = ({ data }) => {
+  
+  setupDestinationIncrementer(data, data);
+};
