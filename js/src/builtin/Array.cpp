85291e4369be72330abec9fcf6edebd7f540ebdc
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 03:56:43 +0000

    Bug 1481032: Non-extensible arrays are already shrunk to capacity. r=jandem
    UltraBlame original commit: b20289e446893be662123b91104e35646408a578

--- js/src/builtin/Array.cpp
+++ js/src/builtin/Array.cpp
@@ -791,8 +791,12 @@ js::ArraySetLength(JSContext* cx, Handle<ArrayObject*> arr, HandleId id,
             MOZ_ASSERT(oldCapacity >= oldInitializedLength);
             if (oldInitializedLength > newLen)
                 arr->setDenseInitializedLengthMaybeNonExtensible(cx, newLen);
-            if (oldCapacity > newLen)
-                arr->shrinkElements(cx, newLen);
+            if (oldCapacity > newLen) {
+                if (arr->isExtensible())
+                    arr->shrinkElements(cx, newLen);
+                else
+                    MOZ_ASSERT(arr->getDenseInitializedLength() == arr->getDenseCapacity());
+            }
 
             
             
