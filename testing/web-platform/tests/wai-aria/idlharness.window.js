10da5d44d87d375168b12a62c3a9e283be57775e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 09:54:09 +0000

    Bug 1488352 [wpt PR 12822] - Add IDL tests for WAI-ARIA, a=testonly
    Automatic update from web-platform-testsAdd IDL tests for WAI-ARIA (#12822)
    
    --
    
    wpt-commits: 4caae418cffa4f803f2fe283a4d3fd7cfaae7461
    wpt-pr: 12822
    
    UltraBlame original commit: bb34e79a1660b0640411760ae62e203dbc4ad6a7

---
+++ testing/web-platform/tests/wai-aria/idlharness.window.js
@@ -0,0 +1,15 @@
+
+
+
+'use strict';
+
+idl_test(
+  ['wai-aria'],
+  ['dom'],
+  idl_array => {
+    idl_array.add_objects({
+      Element: ['element'],
+    });
+    self.element = document.createElementNS(null, "test");
+  }
+);
