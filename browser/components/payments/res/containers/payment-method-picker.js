e62fdbd6bd519aa86abbacd628aef39760f7c005
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 01:37:35 +0000

    Merge mozilla-inbound to mozilla-central. a=merge
    UltraBlame original commit: 6eec814dea789707d04f42bbb01097d44532e2a9

--- browser/components/payments/res/containers/payment-method-picker.js
+++ browser/components/payments/res/containers/payment-method-picker.js
@@ -20,6 +20,7 @@ export default class PaymentMethodPicker extends RichPicker {
     this.securityCodeInput.autocomplete = "off";
     this.securityCodeInput.placeholder = "CVV"; 
     this.securityCodeInput.size = 3;
+    this.securityCodeInput.classList.add("security-code");
     this.securityCodeInput.addEventListener("change", this);
   }
 
