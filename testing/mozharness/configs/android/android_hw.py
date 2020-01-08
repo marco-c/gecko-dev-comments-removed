c19cd95334d5fd447e50ac2e4b1a1eb1bfbe005a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 15:43:06 +0000

    Bug 1499246 - specify tooltool_cache for android-hw, r=jmaher.
    UltraBlame original commit: 6b83a5ae6c26a27b54b2bd2fd2a9fc0f608c0fd6

--- testing/mozharness/configs/android/android_hw.py
+++ testing/mozharness/configs/android/android_hw.py
@@ -20,12 +20,12 @@ config = {
         'install',
         'run-tests',
     ],
+    "tooltool_cache": "/builds/tooltool_cache",
     
     "download_tooltool": True,
     "download_minidump_stackwalk": True,
     "minidump_stackwalk_path": "linux64-minidump_stackwalk",
     "tooltool_servers": ['https://tooltool.mozilla-releng.net/'],
-    
     "minidump_tooltool_manifest_path": "config/tooltool-manifests/linux64/releng.manifest",
     "xpcshell_extra": "--remoteTestRoot=/data/local/tests",
 }
