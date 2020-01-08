4f9bd54d8f0207d86a8856c8dfb2b4260ea47f66
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 03:50:39 +0000

    Bug 1479316: Pass through link related messages when in responsive design mode. r=jryans
    Prior to bug 1453751 responsive design mode relied on the parent process seeing
    that no favicon had been loaded and so defaulting to the root favicon for a
    site. This would have meant that on some occasions responsive design mode didn't
    show the correct favicon for pages that specified their own. This change passes
    all the link related messages to the parent so favicons, feeds and searches
    should work correctly.
    
    Differential Revision: https://phabricator.services.mozilla.com/D2957
    
    UltraBlame original commit: e367443dd417aed7d28eb6eed8288c6361a50d6f

--- devtools/client/responsive.html/browser/tunnel.js
+++ devtools/client/responsive.html/browser/tunnel.js
@@ -381,6 +381,10 @@ MessageManagerTunnel.prototype = {
   INNER_TO_OUTER_MESSAGES: [
     
     "Browser:LoadURI",
+    "Link:SetIcon",
+    "Link:SetFailedIcon",
+    "Link:AddFeed",
+    "Link:AddSearch",
     
     "Content:LoadURIResult",
     "Content:LocationChange",
