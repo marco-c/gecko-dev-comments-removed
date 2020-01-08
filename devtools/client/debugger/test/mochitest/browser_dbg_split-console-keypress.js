ab625b0c8d982bf53e5ab7d6ad1a62451fae4f0e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 02:34:30 +0000

    Bug 1470922 - Fix tests failing with the new Jsterm; r=bgrins.
    MozReview-Commit-ID: EaOSFmWa1tZ
    
    UltraBlame original commit: d01677a667bc35ccd45af746d035d3daa97571ba

--- devtools/client/debugger/test/mochitest/browser_dbg_split-console-keypress.js
+++ devtools/client/debugger/test/mochitest/browser_dbg_split-console-keypress.js
@@ -64,7 +64,8 @@ function test() {
     
     let consoleLostFocus = false;
     jsterm.focus();
-    jsterm.inputNode.addEventListener("blur", () => {consoleLostFocus = true;});
+    const node = jsterm.inputNode || jsterm.node;
+    node.addEventListener("blur", () => {consoleLostFocus = true;});
 
     is(gThreadClient.paused, true,
       "Should be paused at debugger statement.");
