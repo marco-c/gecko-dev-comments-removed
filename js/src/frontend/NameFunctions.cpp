9ee3efc67862813fad96aa32a9aeb664a0559c2f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 09:26:08 +0000

    Bug 1485066 - Part 6: Reduce char-string-char roundtrips when quoting strings. r=Waldo
    UltraBlame original commit: b08b0cfc1dbe100feece9c2085cb73d8577735ed

--- js/src/frontend/NameFunctions.cpp
+++ js/src/frontend/NameFunctions.cpp
@@ -50,8 +50,11 @@ class NameResolver
             return buf->append('.') && buf->append(name);
 
         
-        JSString* source = QuoteString(cx, name, '"');
-        return source && buf->append('[') && buf->append(source) && buf->append(']');
+        UniqueChars source = QuoteString(cx, name, '"');
+        return source &&
+               buf->append('[') &&
+               buf->append(source.get(), strlen(source.get())) &&
+               buf->append(']');
     }
 
     
