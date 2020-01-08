03ae938362632853b9a1a4c73fa8e9106ac5c32c
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 01:02:31 +0000

    Bug 1420514 - Remove app.update.enabled from prefs files r=rstrong
    MozReview-Commit-ID: 5pv8VFNYqKu
    
    UltraBlame original commit: 3214e87404db29888017c427c7ea5c69a852a8c0

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -124,13 +124,6 @@ pref("app.update.log", false);
 pref("app.update.backgroundMaxErrors", 10);
 
 
-#ifdef MOZ_UPDATER
-pref("app.update.enabled", true);
-#else
-pref("app.update.enabled", false);
-#endif
-
-
 pref("app.update.doorhanger", true);
 
 
@@ -144,7 +137,6 @@ pref("app.update.download.promptMaxAttempts", 2);
 
 
 pref("app.update.elevation.promptMaxAttempts", 2);
-
 
 
 
