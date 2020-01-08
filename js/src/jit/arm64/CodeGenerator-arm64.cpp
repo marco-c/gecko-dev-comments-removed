a666fd7b99816ba17ade146c36023ef331b83e5f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 15:02:50 +0000

    Bug 1498047 - Fix generateInvalidator(). r=nbp
    UltraBlame original commit: f4d8966d0af45c469399f46b93afaf635c09c9b0

--- js/src/jit/arm64/CodeGenerator-arm64.cpp
+++ js/src/jit/arm64/CodeGenerator-arm64.cpp
@@ -1041,6 +1041,9 @@ CodeGeneratorARM64::generateInvalidateEpilogue()
     masm.bind(&invalidate_);
 
     
+    masm.push(lr);
+
+    
     invalidateEpilogueData_ = masm.pushWithPatch(ImmWord(uintptr_t(-1)));
 
     TrampolinePtr thunk = gen->jitRuntime()->getInvalidationThunk();
