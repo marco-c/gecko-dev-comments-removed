9fbaa1062389f0f7090bbb6f9d77a456c3f8b4d6
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 20:49:36 +0000

    Bug 1504947 - Change fake-column computations to work from (LineToken, uint32_t offset) inputs.  r=tcampbell
    UltraBlame original commit: 48910f558ce6baf1d1bbf9da4f01a8dcf6618327

--- js/src/frontend/TokenStream.h
+++ js/src/frontend/TokenStream.h
@@ -843,11 +843,21 @@ class TokenStreamAnyChars : public TokenStreamShared {
 
     class LineToken {
       uint32_t index;
+#ifdef DEBUG
+      uint32_t offset_;  
+#endif
 
       friend class SourceCoords;
 
      public:
-      explicit LineToken(uint32_t index, uint32_t offset) : index(index) {}
+      explicit LineToken(uint32_t index, uint32_t offset)
+          : index(index)
+#ifdef DEBUG
+            ,
+            offset_(offset)
+#endif
+      {
+      }
 
       bool isFirstLine() const { return index == 0; }
 
@@ -871,9 +881,24 @@ class TokenStreamAnyChars : public TokenStreamShared {
       return lineNumberFromIndex(lineToken.index);
     }
 
-    uint32_t columnIndex(uint32_t offset) const;
-    void lineNumAndColumnIndex(uint32_t offset, uint32_t* lineNum,
-                               uint32_t* column) const;
+    
+
+
+
+
+
+
+
+
+    uint32_t columnIndex(LineToken lineToken, uint32_t offset) const {
+      MOZ_ASSERT(lineToken.offset_ == offset, "use a consistent token");
+
+      uint32_t lineStartOffset = lineStartOffsets_[lineToken.index];
+      MOZ_RELEASE_ASSERT(offset >= lineStartOffset);
+
+      uint32_t relative = offset - lineStartOffset;
+      return (lineToken.isFirstLine() ? initialColumn_ : 0) + relative;
+    }
   };
 
   SourceCoords srcCoords;
@@ -888,6 +913,18 @@ class TokenStreamAnyChars : public TokenStreamShared {
 
   uint32_t lineNumber(LineToken lineToken) const {
     return srcCoords.lineNumber(lineToken);
+  }
+
+  uint32_t columnIndex(LineToken lineToken, uint32_t offset) const {
+    return srcCoords.columnIndex(lineToken, offset);
+  }
+
+  
+  void lineAndColumnAt(uint32_t offset, uint32_t* lineNum,
+                       uint32_t* column) const {
+    LineToken token = srcCoords.lineToken(offset);
+    *lineNum = srcCoords.lineNumber(token);
+    *column = srcCoords.columnIndex(token, offset);
   }
 
   
@@ -943,8 +980,6 @@ class TokenStreamAnyChars : public TokenStreamShared {
   void computeErrorMetadataNoOffset(ErrorMetadata* err);
 
   
-
-  void lineAndColumnAt(size_t offset, uint32_t* line, uint32_t* column) const;
 
   
   
@@ -1929,12 +1964,7 @@ class GeneralTokenStreamChars : public SpecializedTokenStreamCharsBase<Unit> {
 
   void computeLineAndColumn(uint32_t offset, uint32_t* line,
                             uint32_t* column) const {
-    const TokenStreamAnyChars& anyChars = anyCharsAccess();
-
-    auto lineToken = anyChars.lineToken(offset);
-    *line = anyChars.lineNumber(lineToken);
-
-    anyCharsAccess().srcCoords.lineNumAndColumnIndex(offset, line, column);
+    anyCharsAccess().lineAndColumnAt(offset, line, column);
   }
 
   void newSimpleToken(TokenKind kind, TokenStart start,
@@ -2469,7 +2499,9 @@ class MOZ_STACK_CLASS TokenStreamSpecific
   }
 
   uint32_t columnAt(size_t offset) const final {
-    return anyCharsAccess().srcCoords.columnIndex(offset);
+    const TokenStreamAnyChars& anyChars = anyCharsAccess();
+    auto lineToken = anyChars.lineToken(offset);
+    return anyChars.columnIndex(lineToken, offset);
   }
 
   bool hasTokenizationStarted() const final;
