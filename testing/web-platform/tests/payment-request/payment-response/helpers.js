5739f1f6a71aa9c81e75c12af05a9b29b42f15f7
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 08:39:28 +0000

    Bug 1473347 [wpt PR 11772] - tests for onpayerdetailchange, a=testonly
    Automatic update from web-platform-teststests for onpayerdetailchange (#11772)
    
    --
    
    wpt-commits: 906cac1dbf6e4fdcaa94bda8cf73c6344dce964d
    wpt-pr: 11772
    
    UltraBlame original commit: 020a18a0acf68a51368257504f031acad0392a6f

--- testing/web-platform/tests/payment-request/payment-response/helpers.js
+++ testing/web-platform/tests/payment-request/payment-response/helpers.js
@@ -35,7 +35,7 @@ test(() => {
 
 
 async function getPaymentResponse(options, id) {
-  const { response } = getPaymentRequestResponse(options, id);
+  const { response } = await getPaymentRequestResponse(options, id);
   return response;
 }
 
