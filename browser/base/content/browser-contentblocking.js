c82558fb7c0075b9ce39501a54d264110b7b7cde
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 08:00:14 +0000

    Merge mozilla-central to inbound.  a=merge CLOSED TREE
    UltraBlame original commit: ab8276b5924e4ade5daec800cefc702f72cef34c

--- browser/base/content/browser-contentblocking.js
+++ browser/base/content/browser-contentblocking.js
@@ -134,10 +134,16 @@ var ContentBlocking = {
   PREF_ANIMATIONS_ENABLED: "toolkit.cosmeticAnimations.enabled",
   PREF_REPORT_BREAKAGE_ENABLED: "browser.contentblocking.reportBreakage.enabled",
   PREF_REPORT_BREAKAGE_URL: "browser.contentblocking.reportBreakage.url",
+  PREF_INTRO_COUNT_CB: "browser.contentblocking.introCount",
+  PREF_INTRO_COUNT_TP: "privacy.trackingprotection.introCount",
   content: null,
   icon: null,
   activeTooltipText: null,
   disabledTooltipText: null,
+
+  get prefIntroCount() {
+    return this.contentBlockingUIEnabled ? this.PREF_INTRO_COUNT_CB : this.PREF_INTRO_COUNT_TP;
+  },
 
   get appMenuLabel() {
     delete this.appMenuLabel;
@@ -429,14 +435,11 @@ var ContentBlocking = {
     } else if (active && webProgress.isTopLevel) {
       this.iconBox.setAttribute("animate", "true");
 
-      
-      if (TrackingProtection.enabledGlobally) {
-        let introCount = Services.prefs.getIntPref("privacy.trackingprotection.introCount");
-        if (introCount < this.MAX_INTROS) {
-          Services.prefs.setIntPref("privacy.trackingprotection.introCount", ++introCount);
-          Services.prefs.savePrefFile(null);
-          this.showIntroPanel();
-        }
+      let introCount = Services.prefs.getIntPref(this.prefIntroCount);
+      if (introCount < this.MAX_INTROS) {
+        Services.prefs.setIntPref(this.prefIntroCount, ++introCount);
+        Services.prefs.savePrefFile(null);
+        this.showIntroPanel();
       }
     }
 
@@ -493,11 +496,8 @@ var ContentBlocking = {
   },
 
   dontShowIntroPanelAgain() {
-    
-    
-    if (TrackingProtection.enabledGlobally) {
-      Services.prefs.setIntPref("privacy.trackingprotection.introCount",
-                                this.MAX_INTROS);
+    if (!PrivateBrowsingUtils.isBrowserPrivate(gBrowser.selectedBrowser)) {
+      Services.prefs.setIntPref(this.prefIntroCount, this.MAX_INTROS);
       Services.prefs.savePrefFile(null);
     }
   },
@@ -506,13 +506,39 @@ var ContentBlocking = {
     let brandBundle = document.getElementById("bundle_brand");
     let brandShortName = brandBundle.getString("brandShortName");
 
+    let introTitle;
+    let introDescription;
+    
+    
+    let variation;
+
+    if (this.contentBlockingUIEnabled) {
+      introTitle = gNavigatorBundle.getFormattedString("contentBlocking.intro.title",
+                                                       [brandShortName]);
+      
+      
+      if (TrackingProtection.enabledGlobally) {
+        introDescription = gNavigatorBundle.getString("contentBlocking.intro.v2.description");
+        variation = 2;
+      } else {
+        introDescription = gNavigatorBundle.getFormattedString("contentBlocking.intro.v1.description",
+                                                               [brandShortName]);
+        variation = 1;
+      }
+    } else {
+      introTitle = gNavigatorBundle.getString("trackingProtection.intro.title");
+      introDescription = gNavigatorBundle.getFormattedString("trackingProtection.intro.description2",
+                                                             [brandShortName]);
+      variation = 0;
+    }
+
     let openStep2 = () => {
       
       
       this.dontShowIntroPanelAgain();
 
       let nextURL = Services.urlFormatter.formatURLPref("privacy.trackingprotection.introURL") +
-                    "?step=2&newtab=true";
+                    `?step=2&newtab=true&variation=${variation}`;
       switchToTabHavingURI(nextURL, true, {
         
         
@@ -536,11 +562,7 @@ var ContentBlocking = {
 
     let panelTarget = await UITour.getTarget(window, "trackingProtection");
     UITour.initForBrowser(gBrowser.selectedBrowser, window);
-    UITour.showInfo(window, panelTarget,
-                    gNavigatorBundle.getString("trackingProtection.intro.title"),
-                    gNavigatorBundle.getFormattedString("trackingProtection.intro.description2",
-                                                        [brandShortName]),
-                    undefined, buttons,
+    UITour.showInfo(window, panelTarget, introTitle, introDescription, undefined, buttons,
                     { closeButtonCallback: () => this.dontShowIntroPanelAgain() });
   },
 };
