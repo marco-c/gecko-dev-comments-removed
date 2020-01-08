f09d0f5b6697752e3beef09445b42fa85462fb82
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 03:59:06 +0000

    Bug 1480695 - Add telemetry to record how frequently we encounter a cross-origin <iframe> within a filter. r=mstange,Nika
    UltraBlame original commit: a18c257cf35d1b10cd713579b83357084812ba72

--- layout/painting/nsDisplayList.h
+++ layout/painting/nsDisplayList.h
@@ -835,6 +835,12 @@ public:
 
   void SetInTransform(bool aInTransform) { mInTransform = aInTransform; }
 
+  
+
+
+
+  bool IsInFilter() const { return mInFilter; }
+
   bool IsInPageSequence() const { return mInPageSequence; }
   void SetInPageSequence(bool aInPage) { mInPageSequence = aInPage; }
 
@@ -1172,21 +1178,26 @@ public:
   
 
 
-  class AutoFilterASRSetter {
+
+  class AutoEnterFilter {
   public:
-    AutoFilterASRSetter(nsDisplayListBuilder* aBuilder, bool aUsingFilter)
-      : mBuilder(aBuilder), mOldValue(aBuilder->mFilterASR)
+    AutoEnterFilter(nsDisplayListBuilder* aBuilder, bool aUsingFilter)
+      : mBuilder(aBuilder)
+      , mOldValue(aBuilder->mFilterASR)
+      , mOldInFilter(aBuilder->mInFilter)
     {
       if (!aBuilder->mFilterASR && aUsingFilter) {
         aBuilder->mFilterASR = aBuilder->CurrentActiveScrolledRoot();
+        aBuilder->mInFilter = true;
       }
     }
-    ~AutoFilterASRSetter() {
+    ~AutoEnterFilter() {
       mBuilder->mFilterASR = mOldValue;
     }
   private:
     nsDisplayListBuilder* mBuilder;
     const ActiveScrolledRoot* mOldValue;
+    bool mOldInFilter;
   };
 
   
@@ -1948,6 +1959,7 @@ private:
   
   
   bool                           mInTransform;
+  bool                           mInFilter;
   bool                           mInPageSequence;
   bool                           mIsInChromePresContext;
   bool                           mSyncDecodeImages;
