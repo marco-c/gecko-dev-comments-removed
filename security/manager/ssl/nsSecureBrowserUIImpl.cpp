d0ca8d51989594330476153a36049095d1f6c456
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 17:22:48 +0000

    Backed out changeset 365e0d2414bf (bug 1504728) for browser-chrome failures. CLOSED TREE
    UltraBlame original commit: 447536cc6c984ae06ae44a25194d5612fad6628b

--- security/manager/ssl/nsSecureBrowserUIImpl.cpp
+++ security/manager/ssl/nsSecureBrowserUIImpl.cpp
@@ -205,10 +205,6 @@ nsSecureBrowserUIImpl::CheckForBlockedContent()
   if (docShell->GetHasAllCookiesBeenBlocked()) {
     mState |= STATE_COOKIES_BLOCKED_ALL;
   }
-
-  if (docShell->GetHasCookiesLoaded()) {
-    mState |= STATE_COOKIES_LOADED;
-  }
 }
 
 
