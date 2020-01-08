e19192a48ab4234bc370af622c6e0831f7bd4071
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 07:49:17 +0000

    Convert mozhttpd's test suite to pytest (Bug 1479420) r=davehunt
    Currently mozhttpd's test suite uses Python's unittest. Several tests don't
    shut down started MozHttpd server instances, resulting in blocking test runs.
    This patch resolves that problem by using pytest fixtures.
    
    Differential Revision: https://phabricator.services.mozilla.com/D3764
    
    UltraBlame original commit: f168924d45d2346aaaa19b34360692282bba9e9f

--- testing/mozbase/mozhttpd/tests/basic.py
+++ testing/mozbase/mozhttpd/tests/basic.py
@@ -5,47 +5,49 @@ from __future__ import absolute_import
 import mozhttpd
 import mozfile
 import os
-import tempfile
-import unittest
+
+import pytest
 
 import mozunit
 
 
-class TestBasic(unittest.TestCase):
-    """ Test basic Mozhttpd capabilites """
-
-    def test_basic(self):
-        """ Test mozhttpd can serve files """
-
-        tempdir = tempfile.mkdtemp()
-
-        
-        sizes = {'small': [128], 'large': [16384]}
-
-        for k in sizes.keys():
-            
-            sizes[k].append(os.urandom(sizes[k][0]))
-
-            
-            fpath = os.path.join(tempdir, k)
-            sizes[k].append(fpath)
-
-            
-            with open(fpath, 'wb') as f:
-                f.write(sizes[k][1])
-
-        server = mozhttpd.MozHttpd(docroot=tempdir)
-        server.start()
-        server_url = server.get_url()
-
-        
-        for k in sizes.keys():
-            retrieved_content = mozfile.load(server_url + k).read()
-            self.assertEqual(retrieved_content, sizes[k][1])
-
-        
-        mozfile.rmtree(tempdir)
+@pytest.fixture(name="files")
+def fixture_files():
+    """Return a list of tuples with name and binary_string."""
+    return [("small", os.urandom(128)), ("large", os.urandom(16384))]
 
 
-if __name__ == '__main__':
+@pytest.fixture(name="docroot")
+def fixture_docroot(tmpdir, files):
+    """Yield a str path to docroot."""
+    docroot = tmpdir.mkdir("docroot")
+
+    for name, binary_string in files:
+        filename = docroot.join(name)
+        filename.write_binary(binary_string)
+
+    yield str(docroot)
+
+    docroot.remove()
+
+
+@pytest.fixture(name="httpd_url")
+def fixture_httpd_url(docroot):
+    """Yield the URL to a started MozHttpd server."""
+    httpd = mozhttpd.MozHttpd(docroot=docroot)
+    httpd.start()
+    yield httpd.get_url()
+    httpd.stop()
+
+
+def test_basic(httpd_url, files):
+    """Test that mozhttpd can serve files."""
+
+    
+    for name, binary_string in files:
+        retrieved_content = mozfile.load(httpd_url + name).read()
+        assert retrieved_content == binary_string
+
+
+if __name__ == "__main__":
     mozunit.main()
