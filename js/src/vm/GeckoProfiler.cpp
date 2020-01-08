cabcdca6c607dfba06ec7d70f90945770946b571
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 07:29:31 +0000

    bug 785922: Emit column numbers for JS frames and functions in the gecko profiler r=sfink,mstange
    Add support for column numbers when profiling JS stack frames and functions.  This will help debug minified scripts when inspecting performance profiles.  The column information will be emitted as a new column property that is part of the frameTable in the profile output, and will also be appended in the descriptive profiler string.
    
    Differential Revision: https://phabricator.services.mozilla.com/D3059
    
    UltraBlame original commit: ef6a3b493405a675b56bdfed1f9480600b58ed74

--- js/src/vm/GeckoProfiler.cpp
+++ js/src/vm/GeckoProfiler.cpp
@@ -275,12 +275,17 @@ GeckoProfilerRuntime::allocProfileString(JSScript* script, JSFunction* maybeFun)
     size_t lenFilename = strlen(filename);
 
     
-    uint64_t lineno = script->lineno();
+    uint32_t lineno = script->lineno();
     size_t lenLineno = 1;
-    for (uint64_t i = lineno; i /= 10; lenLineno++);
+    for (uint32_t i = lineno; i /= 10; lenLineno++);
 
     
-    size_t len = lenFilename + lenLineno + 1; 
+    uint32_t column = script->column();
+    size_t lenColumn = 1;
+    for (uint32_t i = column; i /= 10; lenColumn++);
+
+    
+    size_t len = lenFilename + 1 + lenLineno + 1 + lenColumn; 
     if (atom) {
         len += JS::GetDeflatedUTF8StringLength(atom) + 3; 
     }
@@ -297,9 +302,11 @@ GeckoProfilerRuntime::allocProfileString(JSScript* script, JSFunction* maybeFun)
         if (!atomStr)
             return nullptr;
 
-        ret = snprintf(cstr.get(), len + 1, "%s (%s:%" PRIu64 ")", atomStr.get(), filename, lineno);
+        ret = snprintf(cstr.get(), len + 1, "%s (%s:%" PRIu32 ":%" PRIu32 ")",
+                atomStr.get(), filename, lineno, column);
     } else {
-        ret = snprintf(cstr.get(), len + 1, "%s:%" PRIu64, filename, lineno);
+        ret = snprintf(cstr.get(), len + 1, "%s:%" PRIu32 ":%" PRIu32,
+                filename, lineno, column);
     }
 
     MOZ_ASSERT(ret == len, "Computed length should match actual length!");
