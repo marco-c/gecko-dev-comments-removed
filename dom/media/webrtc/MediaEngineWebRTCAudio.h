c117f582cd89084c19940a0d172e8dfd0e93649c
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 14:27:17 +0000

    Bug 1497254 - clang-format MediaEngineWebRTCAudio.{cpp,h} and MediaEnginePrefs.h. r=pehrsons
    Differential Revision: https://phabricator.services.mozilla.com/D8959
    
    UltraBlame original commit: b4d7770b8661338c878166beca941eeb830f6041

--- dom/media/webrtc/MediaEngineWebRTCAudio.h
+++ dom/media/webrtc/MediaEngineWebRTCAudio.h
@@ -35,15 +35,12 @@ public:
                                     bool aDelayAgnostic,
                                     bool aExtendedFilter);
 
-  bool RequiresSharing() const override
-  {
-    return false;
-  }
+  bool RequiresSharing() const override { return false; }
 
   nsString GetName() const override;
   nsCString GetUUID() const override;
 
-  nsresult Allocate(const dom::MediaTrackConstraints &aConstraints,
+  nsresult Allocate(const dom::MediaTrackConstraints& aConstraints,
                     const MediaEnginePrefs& aPrefs,
                     const nsString& aDeviceId,
                     const ipc::PrincipalInfo& aPrincipalInfo,
@@ -133,7 +130,8 @@ private:
   
   
   
-  const nsMainThreadPtrHandle<media::Refcountable<dom::MediaTrackSettings>> mSettings;
+  const nsMainThreadPtrHandle<media::Refcountable<dom::MediaTrackSettings>>
+    mSettings;
 
   
   MediaEngineSourceState mState;
@@ -265,17 +263,14 @@ private:
   bool mEnded;
 };
 
-
 class MediaEngineWebRTCAudioCaptureSource : public MediaEngineSource
 {
 public:
-  explicit MediaEngineWebRTCAudioCaptureSource(const char* aUuid)
-  {
-  }
+  explicit MediaEngineWebRTCAudioCaptureSource(const char* aUuid) {}
   nsString GetName() const override;
   nsCString GetUUID() const override;
-  nsresult Allocate(const dom::MediaTrackConstraints &aConstraints,
-                    const MediaEnginePrefs &aPrefs,
+  nsresult Allocate(const dom::MediaTrackConstraints& aConstraints,
+                    const MediaEnginePrefs& aPrefs,
                     const nsString& aDeviceId,
                     const ipc::PrincipalInfo& aPrincipalInfo,
                     AllocationHandle** aOutHandle,
