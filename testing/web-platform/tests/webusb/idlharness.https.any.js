a5858f6e3d607afe615eb0ad25c80b66a372df0b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 01:34:35 +0000

    Bug 1474677 [wpt PR 11890] - Update the webusb IDL file, a=testonly
    Automatic update from web-platform-testsUpdate the webusb IDL file (#11890)
    
    --
    
    wpt-commits: db9d66853f0efd055c1f2100d0d9d468049dd9a1
    wpt-pr: 11890
    
    UltraBlame original commit: 4b108cb6c31c86c50b9155e1ce3cd642d8f59beb

--- testing/web-platform/tests/webusb/idlharness.https.any.js
+++ testing/web-platform/tests/webusb/idlharness.https.any.js
@@ -14,52 +14,56 @@ var usbEndpoint;
 var usbConnectionEvent;
 
 usb_test(async () => {
-  const idl = await fetch('/interfaces/webusb.idl').then(r => r.text());
-  const html = await fetch('/interfaces/html.idl').then(r => r.text());
-  const dom = await fetch('/interfaces/dom.idl').then(r => r.text());
-
-  let idl_array = new IdlArray();
-  idl_array.add_idls(idl);
-  idl_array.add_dependency_idls(html);
-  idl_array.add_dependency_idls(dom);
-
   
-  idl_array.add_untested_idls('dictionary PermissionDescriptor {};');
-  idl_array.add_untested_idls('interface PermissionStatus {};');
+  let device;
+  try {
+    device = (await getFakeDevice()).device;
+  } catch (e) {}
 
-  let {device} = await getFakeDevice();
+  return idl_test(
+    ['webusb'],
+    ['html', 'dom'],
+    idl_array => {
+      
+      idl_array.add_untested_idls('dictionary PermissionDescriptor {};');
+      idl_array.add_untested_idls('interface PermissionStatus {};');
 
-  usbDevice = device;
-  usbConfiguration = usbDevice.configurations[0];
-  usbInterface = usbConfiguration.interfaces[0];
-  usbAlternateInterface = usbInterface.alternates[0];
-  usbEndpoint = usbAlternateInterface.endpoints[0];
-  usbConnectionEvent =
-      new USBConnectionEvent('connect', { device: usbDevice })
+      try {
+        usbDevice = device;
+        usbConfiguration = usbDevice.configurations[0];
+        usbInterface = usbConfiguration.interfaces[0];
+        usbAlternateInterface = usbInterface.alternates[0];
+        usbEndpoint = usbAlternateInterface.endpoints[0];
+        usbConnectionEvent =
+            new USBConnectionEvent('connect', { device: usbDevice })
+      } catch (e) {}
 
-  if (self.GLOBAL.isWindow()) {
-    idl_array.add_objects({ Navigator: ['navigator'] });
-  } else if (self.GLOBAL.isWorker()) {
-    idl_array.add_objects({ WorkerNavigator: ['navigator'] });
-  }
+      if (self.GLOBAL.isWindow()) {
+        idl_array.add_objects({ Navigator: ['navigator'] });
+      } else if (self.GLOBAL.isWorker()) {
+        idl_array.add_objects({ WorkerNavigator: ['navigator'] });
+      }
 
-  idl_array.add_objects({
-    USB: ['navigator.usb'],
-    USBAlternateInterface: ['usbAlternateInterface'],
-    USBConfiguration: ['usbConfiguration'],
-    USBConnectionEvent: ['usbConnectionEvent'],
-    USBDevice: ['usbDevice'],
-    USBEndpoint: ['usbEndpoint'],
-    USBInterface: ['usbInterface'],
-    USBInTransferResult: ['new USBInTransferResult("ok")'],
-    USBOutTransferResult: ['new USBOutTransferResult("ok")'],
-    USBIsochronousInTransferResult: ['new USBIsochronousInTransferResult([])'],
-    USBIsochronousOutTransferResult: ['new USBIsochronousOutTransferResult([])'],
-    USBIsochronousInTransferPacket: ['new USBIsochronousInTransferPacket("ok")'],
-    USBIsochronousOutTransferPacket: ['new USBIsochronousOutTransferPacket("ok")'],
-  });
-
-  idl_array.test();
-}, 'WebUSB IDL test');
+      idl_array.add_objects({
+        Navigator: ['navigator'],
+        USB: ['navigator.usb'],
+        USBAlternateInterface: ['usbAlternateInterface'],
+        USBConfiguration: ['usbConfiguration'],
+        USBConnectionEvent: ['usbConnectionEvent'],
+        USBDevice: ['usbDevice'],
+        USBEndpoint: ['usbEndpoint'],
+        USBInterface: ['usbInterface'],
+        USBInTransferResult: ['new USBInTransferResult("ok")'],
+        USBOutTransferResult: ['new USBOutTransferResult("ok")'],
+        USBIsochronousInTransferResult: ['new USBIsochronousInTransferResult([])'],
+        USBIsochronousOutTransferResult: ['new USBIsochronousOutTransferResult([])'],
+        USBIsochronousInTransferPacket: ['new USBIsochronousInTransferPacket("ok")'],
+        USBIsochronousOutTransferPacket: ['new USBIsochronousOutTransferPacket("ok")'],
+      });
+      idl_array.prevent_multiple_testing('Navigator');
+    },
+    'WebUSB IDL test'
+  )
+});
 
 done();
