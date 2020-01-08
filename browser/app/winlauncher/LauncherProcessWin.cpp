e02174d730e6ab49df38bf9d02db5d0e0c407464
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 08:33:20 +0000

    Bug 1486901: Part 1 - Allow RunAsLauncherProcess to fall back to environment and command line if the parent process check does not return true; r=mhowell!
    Differential Revision: https://phabricator.services.mozilla.com/D4496
    
    UltraBlame original commit: 6f6c28eb6041a8d18caf5cead0562c271449b75f

--- browser/app/winlauncher/LauncherProcessWin.cpp
+++ browser/app/winlauncher/LauncherProcessWin.cpp
@@ -152,12 +152,19 @@ RunAsLauncherProcess(int& argc, wchar_t** argv)
     return true;
   }
 
-  return !isChildOfFirefox.value();
-#else
+  if (!isChildOfFirefox.value()) {
+    return true;
+  }
+#endif 
+
+  if (mozilla::EnvHasValue("MOZ_LAUNCHER_PROCESS")) {
+    mozilla::SaveToEnv("MOZ_LAUNCHER_PROCESS=");
+    return true;
+  }
+
   return CheckArg(argc, argv, L"launcher",
                   static_cast<const wchar_t**>(nullptr),
-                  CheckArgFlag::CheckOSInt | CheckArgFlag::RemoveArg) == ARG_FOUND;
-#endif 
+                  CheckArgFlag::RemoveArg) == ARG_FOUND;
 }
 
 int
