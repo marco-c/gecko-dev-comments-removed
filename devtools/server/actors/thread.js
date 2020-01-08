a82188492cba05a8b50ac370a40ed00dcc2e3fd2
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 08:23:55 +0000

    Bug 956376 - Part 3: Use Debugger.findSources in ThreadActor#_discoverSources. r=ochameau
    UltraBlame original commit: 37f94ae472d60f1004844c09146c9eb3b3522055

--- devtools/server/actors/thread.js
+++ devtools/server/actors/thread.js
@@ -1114,19 +1114,9 @@ const ThreadActor = ActorClassWithSpec(threadSpec, {
 
 
   _discoverSources: function() {
-    
-    const sourcesToScripts = new Map();
-    const scripts = this.dbg.findScripts();
-
-    for (let i = 0, len = scripts.length; i < len; i++) {
-      const s = scripts[i];
-      if (s.source) {
-        sourcesToScripts.set(s.source, s);
-      }
-    }
-
-    return Promise.all([...sourcesToScripts.values()].map(script => {
-      return this.sources.createSourceActors(script.source);
+    const sources = this.dbg.findSources();
+    return Promise.all(sources.map(source => {
+      return this.sources.createSourceActors(source);
     }));
   },
 
