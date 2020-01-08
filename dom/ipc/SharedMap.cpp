981898a32978476a85b85f6520b072bfe9a273a4
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 00:52:17 +0000

    Bug 1463587: Part 3 - Add bindings for SharedMap, and expose it via process message managers. r=erahm,baku,bz
    This is the first basic implementation of a shared-memory key-value store for
    JS message managers. It has one read-write endpoint in the parent process, and
    separate read-only endpoints for each child-process message manager.
    
    Changes to the parent endpoint are broadcast to the children as snapshots.
    Each snapshot triggers a "change" event with a list of changed keys.
    
    It currently has the following limitations:
    
    - It only supports basic structured clone data. There's no support for blobs,
      input streams, message ports... Blob support will be added in a follow-up
      patch.
    
    - Changes are currently only broadcast to child endpoints when flush() is
      explicitly called in the parent, or when new child processes are launched.
      In a follow-up, this will be changed to automatically flush after changes
      when the event loop is idle.
    
    - All set operations clone their inputs synchronously, which means that
      there's no trivial way for callers to batch multiple changes to a single key
      without some additional effort. It might be useful to add a
      delayed-serialization option to the .set() call in a follow-up, for callers
      who are sure they know what they're doing.
    
    MozReview-Commit-ID: IM8a3UgejXU
    
    UltraBlame original commit: b771ae1bc124cd969cf53916e790b8b2fc4d0da9

--- dom/ipc/SharedMap.cpp
+++ dom/ipc/SharedMap.cpp
@@ -1,16 +1,18 @@
-/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
-/* vim: set ts=8 sts=2 et sw=2 tw=80: */
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+
+
+
+
 
 #include "SharedMap.h"
+#include "SharedMapChangeEvent.h"
 
 #include "MemMapSnapshot.h"
 #include "ScriptPreloader-inl.h"
 
 #include "mozilla/dom/ContentParent.h"
 #include "mozilla/dom/ProcessGlobal.h"
+#include "mozilla/dom/ScriptSettings.h"
 
 using namespace mozilla::loader;
 
@@ -21,8 +23,8 @@ using namespace ipc;
 namespace dom {
 namespace ipc {
 
-// Align to size of uintptr_t here, to be safe. It's probably not strictly
-// necessary, though.
+
+
 constexpr size_t kStructuredCloneAlign = sizeof(uintptr_t);
 
 
@@ -36,10 +38,12 @@ AlignTo(size_t* aOffset, size_t aAlign)
 
 
 SharedMap::SharedMap()
+  : DOMEventTargetHelper()
 {}
 
 SharedMap::SharedMap(nsIGlobalObject* aGlobal, const FileDescriptor& aMapFile,
                      size_t aMapSize)
+  : DOMEventTargetHelper(aGlobal)
 {
   mMapFile.reset(new FileDescriptor(aMapFile));
   mMapSize = aMapSize;
@@ -79,17 +83,17 @@ SharedMap::Entry::Read(JSContext* aCx,
                        ErrorResult& aRv)
 {
   if (mData.is<StructuredCloneData>()) {
-    // We have a temporary buffer for a key that was changed after the last
-    // snapshot. Just decode it directly.
+    
+    
     auto& holder = mData.as<StructuredCloneData>();
     holder.Read(aCx, aRetVal, aRv);
     return;
   }
 
-  // We have a pointer to a shared memory region containing our structured
-  // clone data. Create a temporary buffer to decode that data, and then
-  // discard it so that we don't keep a separate process-local copy around any
-  // longer than necessary.
+  
+  
+  
+  
   StructuredCloneData holder;
   if (!holder.CopyExternalData(Data(), Size())) {
     aRv.Throw(NS_ERROR_OUT_OF_MEMORY);
@@ -121,6 +125,71 @@ SharedMap::Update(const FileDescriptor& aMapFile, size_t aMapSize,
   }
   mMapSize = aMapSize;
   mEntries.Clear();
+  mEntryArray.reset();
+
+
+  AutoEntryScript aes(GetParentObject(), "SharedMap change event");
+  JSContext* cx = aes.cx();
+
+  RootedDictionary<MozSharedMapChangeEventInit> init(cx);
+  if (!init.mChangedKeys.SetCapacity(aChangedKeys.Length(), fallible)) {
+    NS_WARNING("Failed to dispatch SharedMap change event");
+    return;
+  }
+  for (auto& key : aChangedKeys) {
+    Unused << init.mChangedKeys.AppendElement(NS_ConvertUTF8toUTF16(key),
+                                              fallible);
+  }
+
+  RefPtr<SharedMapChangeEvent> event =
+    SharedMapChangeEvent::Constructor(this, NS_LITERAL_STRING("change"), init);
+  event->SetTrusted(true);
+
+  DispatchEvent(*event);
+}
+
+
+const nsTArray<SharedMap::Entry*>&
+SharedMap::EntryArray() const
+{
+  if (mEntryArray.isNothing()) {
+    MaybeRebuild();
+
+    mEntryArray.emplace(mEntries.Count());
+    auto& array = mEntryArray.ref();
+    for (auto& entry : IterHash(mEntries)) {
+      array.AppendElement(entry);
+    }
+  }
+
+  return mEntryArray.ref();
+}
+
+const nsString
+SharedMap::GetKeyAtIndex(uint32_t aIndex) const
+{
+  return NS_ConvertUTF8toUTF16(EntryArray()[aIndex]->Name());
+}
+
+JS::Value
+SharedMap::GetValueAtIndex(uint32_t aIndex) const
+{
+  JSObject* wrapper = GetWrapper();
+  MOZ_ASSERT(wrapper,
+             "Should never see GetValueAtIndex on a SharedMap without a live "
+             "wrapper");
+  if (!wrapper) {
+    return JS::NullValue();
+  }
+
+  AutoJSContext cx;
+
+  JSAutoRealm ar(cx, wrapper);
+
+  JS::RootedValue val(cx);
+  EntryArray()[aIndex]->Read(cx, &val, IgnoreErrors());
+
+  return val;
 }
 
 void
@@ -156,20 +225,20 @@ SharedMap::MaybeRebuild()
     return Ok();
   }
 
-  // This function maps a shared memory region created by Serialize() and reads
-  // its header block to build a new mEntries hashtable of its contents.
-  //
-  // The entries created by this function contain a pointer to this SharedMap
-  // instance, and the offsets and sizes of their structured clone data within
-  // its shared memory region. When needed, that structured clone data is
-  // retrieved directly as indexes into the SharedMap's shared memory region.
+  
+  
+  
+  
+  
+  
+  
 
   MOZ_TRY(mMap.initWithHandle(*mMapFile, mMapSize));
   mMapFile.reset();
 
-  // We should be able to pass this range as an initializer list or an immediate
-  // param, but gcc currently chokes on that if optimization is enabled, and
-  // initializes everything to 0.
+  
+  
+  
   Range<uint8_t> range(&mMap.get<uint8_t>()[0], mMap.size());
   InputBuffer buffer(range);
 
@@ -180,13 +249,13 @@ SharedMap::MaybeRebuild()
     auto entry = MakeUnique<Entry>(*this);
     entry->Code(buffer);
 
-    // This buffer was created at runtime, during this session, so any errors
-    // indicate memory corruption, and are fatal.
+    
+    
     MOZ_RELEASE_ASSERT(!buffer.error());
 
-    // Note: Order of evaluation of function arguments is not guaranteed, so we
-    // can't use entry.release() in place of entry.get() without entry->Name()
-    // sometimes resulting in a null dereference.
+    
+    
+    
     mEntries.Put(entry->Name(), entry.get());
     Unused << entry.release();
   }
@@ -204,8 +273,8 @@ WritableSharedMap::WritableSharedMap()
   : SharedMap()
 {
   mWritable = true;
-  // Serialize the initial empty contents of the map immediately so that we
-  // always have a file descriptor to send to callers of CloneMapFile().
+  
+  
   Unused << Serialize();
   MOZ_RELEASE_ASSERT(mMap.initialized());
 }
@@ -223,29 +292,29 @@ WritableSharedMap::GetReadOnly()
 Result<Ok, nsresult>
 WritableSharedMap::Serialize()
 {
-  // Serializes a new snapshot of the map, initializes a new read-only shared
-  // memory region with its contents, and updates all entries to point to that
-  // new snapshot.
-  //
-  // The layout of the snapshot is as follows:
-  //
-  // - A header containing a uint32 count field containing the number of
-  //   entries in the map, followed by that number of serialized entry headers,
-  //   as produced by Entry::Code.
-  //
-  // - A data block containing structured clone data for each of the entries'
-  //   values. This data is referenced by absolute byte offsets from the start
-  //   of the shared memory region, encoded in each of the entry header values.
-  //   Each entry's data is aligned to kStructuredCloneAlign, and therefore may
-  //   have alignment padding before it.
-  //
-  // This serialization format is decoded by the MaybeRebuild() method of
-  // read-only SharedMap() instances, and used to populate their mEntries
-  // hashtables.
-  //
-  // Writable instances never read the header blocks, but instead directly
-  // update their Entry instances to point to the appropriate offsets in the
-  // shared memory region created by this function.
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
 
   uint32_t count = mEntries.Count();
 
@@ -279,12 +348,12 @@ WritableSharedMap::Serialize()
     offset += entry->Size();
   }
 
-  // FIXME: We should create a separate OutputBuffer class which can encode to
-  // a static memory region rather than dynamically allocating and then
-  // copying.
+  
+  
+  
   memcpy(ptr.get(), header.Get(), header.cursor());
 
-  // We've already updated offsets at this point. We need this to succeed.
+  
   mMap.reset();
   MOZ_RELEASE_ASSERT(mem.Finalize(mMap).isOk());
 
@@ -362,10 +431,39 @@ WritableSharedMap::KeyChanged(const nsACString& aName)
   if (!mChangedKeys.ContainsSorted(aName)) {
     mChangedKeys.InsertElementSorted(aName);
   }
+  mEntryArray.reset();
 }
 
-NS_IMPL_ISUPPORTS0(SharedMap)
 
-} // ipc
-} // dom
-} // mozilla
+JSObject*
+SharedMap::WrapObject(JSContext* aCx, JS::HandleObject aGivenProto)
+{
+  return MozSharedMap_Binding::Wrap(aCx, this, aGivenProto);
+}
+
+JSObject*
+WritableSharedMap::WrapObject(JSContext* aCx, JS::HandleObject aGivenProto)
+{
+  return MozWritableSharedMap_Binding::Wrap(aCx, this, aGivenProto);
+}
+
+ already_AddRefed<SharedMapChangeEvent>
+SharedMapChangeEvent::Constructor(EventTarget* aEventTarget,
+                                  const nsAString& aType,
+                                  const MozSharedMapChangeEventInit& aInit)
+{
+  RefPtr<SharedMapChangeEvent> event = new SharedMapChangeEvent(aEventTarget);
+
+  bool trusted = event->Init(aEventTarget);
+  event->InitEvent(aType, aInit.mBubbles, aInit.mCancelable);
+  event->SetTrusted(trusted);
+  event->SetComposed(aInit.mComposed);
+
+  event->mChangedKeys = aInit.mChangedKeys;
+
+  return event.forget();
+}
+
+} 
+} 
+} 
