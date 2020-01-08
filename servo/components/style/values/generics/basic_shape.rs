74b234a2be259b69ca8cde16709b01561c2a890e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 08:14:17 +0000

    Bug 1485766 - Drop the manually implementation of ToCSS for BasicShape::Polygon. r=emilio
    The implementation of ToCSS for Polygon has some rule, and we could use skip_if
    to handle and serialization of fill-rule. However, we should derive ToCSS for
    the pair of LengthOrPercentages, so define a new type for it.
    
    Differential Revision: https://phabricator.services.mozilla.com/D4153
    
    UltraBlame original commit: f7ad979ef6da000082e124258474910b85d58262

--- servo/components/style/values/generics/basic_shape.rs
+++ servo/components/style/values/generics/basic_shape.rs
@@ -113,15 +113,22 @@ pub enum ShapeRadius<LengthOrPercentage> {
 
 
 
-#[css(function)]
+#[css(comma, function)]
 #[derive(Clone, Debug, MallocSizeOf, PartialEq, SpecifiedValueInfo,
-         ToComputedValue)]
+         ToComputedValue, ToCss)]
 pub struct Polygon<LengthOrPercentage> {
     
+    #[css(skip_if = "fill_is_default")]
     pub fill: FillRule,
     
-    pub coordinates: Vec<(LengthOrPercentage, LengthOrPercentage)>,
+    #[css(iterable)]
+    pub coordinates: Vec<PolygonCoord<LengthOrPercentage>>,
 }
+
+
+#[derive(Clone, Debug, MallocSizeOf, PartialEq, SpecifiedValueInfo,
+         ToComputedValue, ToCss)]
+pub struct PolygonCoord<LengthOrPercentage>(pub LengthOrPercentage, pub LengthOrPercentage);
 
 
 
@@ -203,7 +210,7 @@ where
             .iter()
             .zip(other.coordinates.iter())
             .map(|(this, other)| {
-                Ok((
+                Ok(PolygonCoord(
                     this.0.animate(&other.0, procedure)?,
                     this.1.animate(&other.1, procedure)?,
                 ))
@@ -239,34 +246,14 @@ where
     }
 }
 
-impl<L: ToCss> ToCss for Polygon<L> {
-    fn to_css<W>(&self, dest: &mut CssWriter<W>) -> fmt::Result
-    where
-        W: Write,
-    {
-        dest.write_str("polygon(")?;
-        if self.fill != FillRule::default() {
-            self.fill.to_css(dest)?;
-            dest.write_str(", ")?;
-        }
-
-        for (i, coord) in self.coordinates.iter().enumerate() {
-            if i > 0 {
-                dest.write_str(", ")?;
-            }
-
-            coord.0.to_css(dest)?;
-            dest.write_str(" ")?;
-            coord.1.to_css(dest)?;
-        }
-
-        dest.write_str(")")
-    }
-}
-
 impl Default for FillRule {
     #[inline]
     fn default() -> Self {
         FillRule::Nonzero
     }
+}
+
+#[inline]
+fn fill_is_default(fill: &FillRule) -> bool {
+    *fill == FillRule::default()
 }
