0baabb90eb6ab41500b39e9d1e30eec667557953
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 18:37:46 +0000

    Bug 1506697 [wpt PR 14024] - [wptserve] Eliminate race condition, a=testonly
    Automatic update from web-platform-tests[wptserve] Eliminate race condition (#14024)
    
    This race condition was expressed during testing sessions where the
    first test to use the Stash feature issued did so with multiple requests
    made in parallel.
    
    --
    
    wpt-commits: cbb25e2c99696956ed2a36e7bcdbdee1dca71705
    wpt-pr: 14024
    
    UltraBlame original commit: 378d0c633eaee8cac785d3f0aa0e24f397efa6e2

--- testing/web-platform/tests/tools/wptserve/wptserve/stash.py
+++ testing/web-platform/tests/tools/wptserve/wptserve/stash.py
@@ -104,6 +104,7 @@ class Stash(object):
 
     _proxy = None
     lock = None
+    _initializing = threading.Lock()
 
     def __init__(self, default_path, address=None, authkey=None):
         self.default_path = default_path
@@ -115,7 +116,16 @@ class Stash(object):
             Stash._proxy = {}
             Stash.lock = threading.Lock()
 
-        if Stash._proxy is None:
+        
+        
+        
+        
+        
+        
+        with Stash._initializing:
+            if Stash.lock:
+                return
+
             manager = ClientDictManager(address, authkey)
             manager.connect()
             Stash._proxy = manager.get_dict()
