5f660366c18833e629d5663b3bd974555b130919
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 10:59:27 +0000

    Bug 1468384 - Add Debugger.Script.isModule. r=jimb
    Differential Revision: https://phabricator.services.mozilla.com/D5380
    
    UltraBlame original commit: 309721897c1c45f166a2cd8f0332ea354e118db0

---
+++ js/src/jit-test/tests/debug/Script-isModule-01.js
@@ -0,0 +1,13 @@
+
+
+const g = newGlobal();
+const dbg = Debugger(g);
+let count = 0;
+dbg.onNewScript = function (script) {
+    count += 1;
+    assertEq(script.isModule, true);
+};
+const m = g.parseModule("");
+g.instantiateModule(m);
+g.evaluateModule(m);
+assertEq(count, 1);
