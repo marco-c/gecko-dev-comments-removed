e9dbad996e03da0be877ad157607199099f2b224
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 09:41:41 +0000

    Bug 1488691 - Prevent registering listeners if PIP failed; r=jchen
    Also, if entering PIP fails will make sure that upon restarting the activity
    it's orientation will match device's.
    
    Differential Revision: https://phabricator.services.mozilla.com/D5035
    
    UltraBlame original commit: 4d5f4308428513f8c18cd05d87c96c75e19f9f30

--- mobile/android/base/java/org/mozilla/gecko/media/PictureInPictureController.java
+++ mobile/android/base/java/org/mozilla/gecko/media/PictureInPictureController.java
@@ -43,9 +43,9 @@ public class PictureInPictureController implements BundleEventListener {
 
 
     public void tryEnteringPictureInPictureMode() throws IllegalStateException {
-        if (shouldTryPipMode()) {
+        if (shouldTryPipMode() &&
+                pipActivity.enterPictureInPictureMode(getPipParams(isMediaPlaying()))) {
             EventDispatcher.getInstance().registerUiThreadListener(this, "MediaControlService:MediaPlayingStatus");
-            pipActivity.enterPictureInPictureMode(getPipParams(isMediaPlaying()));
             isInPipMode = true;
         }
     }
