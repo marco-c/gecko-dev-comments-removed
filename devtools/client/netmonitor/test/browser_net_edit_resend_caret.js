8505bef3cab71ff7f4e715f176b0b413a29666e6
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 11:19:04 +0000

    Bug 1485305 - devtools/ Ensure loadURI always passes a triggeringPrincipal() r=bgrins
    Differential Revision: https://phabricator.services.mozilla.com/D4555
    
    UltraBlame original commit: e1bac78c8df579c6b6cc77e5c52b12c5f2e06a77

--- devtools/client/netmonitor/test/browser_net_edit_resend_caret.js
+++ devtools/client/netmonitor/test/browser_net_edit_resend_caret.js
@@ -21,7 +21,7 @@ add_task(async function() {
 
   
   const waitForEvents = waitForNetworkEvents(monitor, 1);
-  tab.linkedBrowser.loadURI(SIMPLE_URL);
+  BrowserTestUtils.loadURI(tab.linkedBrowser, SIMPLE_URL);
   await waitForEvents;
 
   
