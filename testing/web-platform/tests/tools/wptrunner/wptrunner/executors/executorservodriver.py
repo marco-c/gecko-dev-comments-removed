b54c8f0c8c6d6f6cb44cfc9397b3d320337476da
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 02:45:55 +0000

    Bug 1477569 [wpt PR 12132] - run_in_headless_android_emulator: add port forwarding for webdriver, a=testonly
    Automatic update from web-platform-testsMerge pull request #12132 from web-platform-tests/sync_1f6603d10061b26e941569a4caab260e8c357932
    
    Merge pull request #12132 from sync_1f6603d10061b26e941569a4caab260e8c357932
    --
    
    wpt-commits: 6750443769885220d34a7f74d03a257dc8a8723b
    wpt-pr: 12132
    
    UltraBlame original commit: 1bba3533d50b618fdfd83d6713d36b80dc2dc026

--- testing/web-platform/tests/tools/wptrunner/wptrunner/executors/executorservodriver.py
+++ testing/web-platform/tests/tools/wptrunner/wptrunner/executors/executorservodriver.py
@@ -58,7 +58,7 @@ class ServoWebDriverProtocol(Protocol):
     def connect(self):
         """Connect to browser via WebDriver."""
         
-        wait_for_service((self.host, self.port), timeout=120)
+        wait_for_service((self.host, self.port), timeout=300)
 
         self.session = webdriver.Session(self.host, self.port, extension=ServoCommandExtensions)
         self.session.start()
