2798955d4c6853e6793d892e613869984c5f6f88
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 03:50:42 +0000

    Bug 1468224 - remove dialogs from nsIX509CertDB PKCS12File methods. r=keeler
    - enhance nsIX509CertDB.importPKCS12File to accept a password and return error code.
    - enhance nsIX509CertDB.exportPKCS12File to accept a password and return error code.
    - remove password and error prompts being invoked in C++ layer to Javascript layer.
    - update unit tests
    - add unit test for importing certs with empty string password and no passwords.
    - remove unused code
    
    MozReview-Commit-ID: 23ypAzBarOp
    
    UltraBlame original commit: 6d894ce7832254b7807ca2dffca08ba43bedb069

--- security/manager/ssl/nsPKCS12Blob.h
+++ security/manager/ssl/nsPKCS12Blob.h
@@ -24,54 +24,23 @@ public:
   ~nsPKCS12Blob() {}
 
   
-  nsresult ImportFromFile(nsIFile* file);
+  nsresult ImportFromFile(nsIFile* file, const nsAString& password,
+                          uint32_t& error);
 
   
-  nsresult ExportToFile(nsIFile* file, nsIX509Cert** certs, int numCerts);
+  nsresult ExportToFile(nsIFile* file, nsIX509Cert** certs, int numCerts,
+                        const nsAString& password, uint32_t& error);
 
 private:
   nsCOMPtr<nsIInterfaceRequestor> mUIContext;
 
   
-  nsresult getPKCS12FilePassword(uint32_t& passwordBufferLength,
-                                 mozilla::UniquePtr<uint8_t[]>& passwordBuffer);
-  nsresult newPKCS12FilePassword(uint32_t& passwordBufferLength,
-                                 mozilla::UniquePtr<uint8_t[]>& passwordBuffer);
   nsresult inputToDecoder(mozilla::UniqueSEC_PKCS12DecoderContext& dcx,
                           nsIFile* file,
                           PRErrorCode& nssError);
-  mozilla::UniquePtr<uint8_t[]> stringToBigEndianBytes(const nsString& uni,
+  mozilla::UniquePtr<uint8_t[]> stringToBigEndianBytes(const nsAString& uni,
                                                        uint32_t& bytesLength);
-  void handleError(int myerr, PRErrorCode prerr);
-
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  enum class RetryReason
-  {
-    DoNotRetry,
-    BadPassword,
-    AutoRetryEmptyPassword,
-  };
-  enum class ImportMode
-  {
-    StandardPrompt,
-    TryZeroLengthSecitem
-  };
-
-  void handleImportError(PRErrorCode nssError, RetryReason& retryReason,
-                         uint32_t passwordLengthInBytes);
-
-  nsresult ImportFromFileHelper(nsIFile* file,
-                                ImportMode aImportMode,
-                                RetryReason& aWantRetry);
+  uint32_t handlePRErrorCode(PRErrorCode prerr);
 
   static SECItem* nicknameCollision(SECItem* oldNick, PRBool* cancel,
                                     void* wincx);
