02e04bbc7f3b0e438b901f430ac95961590ffe4f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 13:01:01 +0000

    Bug 1494615 - Move ADBAddon module to devtools/shared/adb;r=ladybenko
    Depends on D7402. Moving the module to a shared older. Following
    patches will cleanup the implementation to remove WebIDE couplings.
    
    Differential Revision: https://phabricator.services.mozilla.com/D7403
    
    UltraBlame original commit: 3d5b641b9783187b6dba552bb246ec6d28d0dbbd

--- devtools/client/aboutdebugging-new/src/modules/usb-runtimes.js
+++ devtools/client/aboutdebugging-new/src/modules/usb-runtimes.js
@@ -5,7 +5,7 @@
 "use strict";
 
 const { ADBScanner } = require("devtools/shared/adb/adb-scanner");
-const { getADBAddon } = require("devtools/client/webide/modules/addons");
+const { getADBAddon } = require("devtools/shared/adb/adb-addon");
 
 
 
