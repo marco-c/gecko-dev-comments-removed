80390676e2ad11f52daaac0c092fd47667033110
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 22:35:33 +0000

    Bug 1513876 - Implement register a toplevel window global actor. r=nika
    Implement register a toplevel window global actor.
    
    Differential Revision: https://phabricator.services.mozilla.com/D15101
    
    UltraBlame original commit: 3280e1de13ecb18fd4f02be55f17e8e374f40412

--- dom/base/ChromeUtils.cpp
+++ dom/base/ChromeUtils.cpp
@@ -19,6 +19,7 @@
 #include "mozilla/TimeStamp.h"
 #include "mozilla/dom/BrowsingContext.h"
 #include "mozilla/dom/IdleDeadline.h"
+#include "mozilla/dom/JSWindowActorService.h"
 #include "mozilla/dom/ReportingHeader.h"
 #include "mozilla/dom/UnionTypes.h"
 #include "mozilla/dom/WindowBinding.h"  
@@ -768,6 +769,15 @@ constexpr auto kSkipSelfHosted = JS::SavedFrameSelfHosted::Exclude;
 
   return ReportingHeader::HasReportingHeaderForOrigin(
       NS_ConvertUTF16toUTF8(aOrigin));
+}
+
+ void ChromeUtils::RegisterWindowActor(
+    const GlobalObject& aGlobal, const nsAString& aName,
+    const WindowActorOptions& aOptions, ErrorResult& aRv) {
+  MOZ_ASSERT(XRE_IsParentProcess());
+
+  RefPtr<JSWindowActorService> service = JSWindowActorService::GetSingleton();
+  service->RegisterWindowActor(aName, aOptions, aRv);
 }
 
 }  
