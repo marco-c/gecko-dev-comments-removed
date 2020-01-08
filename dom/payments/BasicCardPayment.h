c499c1590402530e1e44c6c613122c40214902c2
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 14:57:37 +0000

    Backed out changeset fa6b7a70f2db (bug 1497215) fro xpcshell failures in browser/components/payments/test/unit/test_response_creation.js CLOSED TREE
    UltraBlame original commit: dedd03c893c8cfa5a721b0a04cf5b26cee98db4b

--- dom/payments/BasicCardPayment.h
+++ dom/payments/BasicCardPayment.h
@@ -27,6 +27,22 @@ public:
   bool IsValidBasicCardErrors(JSContext* aCx, JSObject* aData);
   bool IsValidExpiryMonth(const nsAString& aExpiryMonth);
   bool IsValidExpiryYear(const nsAString& aExpiryYear);
+
+
+
+
+
+  nsresult EncodeBasicCardData(const nsAString& aCardholderName,
+                               const nsAString& aCardNumber,
+                               const nsAString& aExpiryMonth,
+                               const nsAString& aExpiryYear,
+                               const nsAString& aCardSecurityCode,
+                               nsIPaymentAddress* aBillingAddress,
+                               nsAString& aResult);
+
+  nsresult DecodeBasicCardData(const nsAString& aData,
+                               nsPIDOMWindowInner* aWindow,
+                               BasicCardResponse& aResponse);
 private:
   BasicCardService() = default;
   ~BasicCardService() = default;
