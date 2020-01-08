b221b2ea6729df6ede984384c8be3a1b8021517b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 21:08:38 +0000

    Bug 1514104 - Remove vpx_mem_set_functions support. r=jya
    vpx_mem_set_functions support is only enabled when
    MOZ_VPX_NO_MEM_REPORTING is not set. It is currently set unconditionally
    when building with the in-tree libvpx. When building with system libvpx,
    it is set when the vpx_mem_set_functions can't be found in the system
    libvpx library.
    
    Upstream removed the vpx_mem_set_functions function in version 1.5, and
    we require at least that version, meaning, in practice,
    MOZ_VPX_NO_MEM_REPORTING is now always set.
    
    We might as well remove the define and the code that's conditional to
    not being defined.
    
    Differential Revision: https://phabricator.services.mozilla.com/D14517
    
    UltraBlame original commit: 8160b9f376eefdfba360fb8d1a851c6dcf4bcd93

--- xpcom/build/XPCOMInit.cpp
+++ xpcom/build/XPCOMInit.cpp
@@ -132,15 +132,6 @@ extern nsresult nsStringInputStreamConstructor(nsISupports*, REFNSIID, void**);
 #include "mozilla/ipc/GeckoChildProcessHost.h"
 
 #include "ogg/ogg.h"
-#if defined(MOZ_VPX) && !defined(MOZ_VPX_NO_MEM_REPORTING)
-#if defined(HAVE_STDINT_H)
-
-
-
-#undef HAVE_STDINT_H
-#endif
-#include "vpx_mem/vpx_mem.h"
-#endif
 
 #include "GeckoProfiler.h"
 
@@ -393,32 +384,6 @@ NS_IMPL_ISUPPORTS(OggReporter, nsIMemoryReporter)
 CountingAllocatorBase<OggReporter>::AmountType
     CountingAllocatorBase<OggReporter>::sAmount(0);
 
-#ifdef MOZ_VPX
-class VPXReporter final : public nsIMemoryReporter,
-                          public CountingAllocatorBase<VPXReporter> {
- public:
-  NS_DECL_ISUPPORTS
-
- private:
-  NS_IMETHOD
-  CollectReports(nsIHandleReportCallback* aHandleReport, nsISupports* aData,
-                 bool aAnonymize) override {
-    MOZ_COLLECT_REPORT("explicit/media/libvpx", KIND_HEAP, UNITS_BYTES,
-                       MemoryAllocated(),
-                       "Memory allocated through libvpx for WebM media files.");
-
-    return NS_OK;
-  }
-
-  ~VPXReporter() {}
-};
-
-NS_IMPL_ISUPPORTS(VPXReporter, nsIMemoryReporter)
-
- template <>
-Atomic<size_t> CountingAllocatorBase<VPXReporter>::sAmount(0);
-#endif 
-
 #ifdef ENABLE_BIGINT
 class GMPReporter final : public nsIMemoryReporter,
                           public CountingAllocatorBase<GMPReporter> {
@@ -645,14 +610,6 @@ NS_InitXPCOM2(nsIServiceManager** aResult, nsIFile* aBinDirectory,
       OggReporter::CountingMalloc, OggReporter::CountingCalloc,
       OggReporter::CountingRealloc, OggReporter::CountingFree);
 
-#if defined(MOZ_VPX) && !defined(MOZ_VPX_NO_MEM_REPORTING)
-  
-  vpx_mem_set_functions(VPXReporter::CountingMalloc,
-                        VPXReporter::CountingCalloc,
-                        VPXReporter::CountingRealloc, VPXReporter::CountingFree,
-                        memcpy, memset, memmove);
-#endif
-
 #ifdef ENABLE_BIGINT
   
   mozilla::SetGMPMemoryFunctions();
@@ -697,9 +654,6 @@ NS_InitXPCOM2(nsIServiceManager** aResult, nsIFile* aBinDirectory,
   
   RegisterStrongMemoryReporter(new ICUReporter());
   RegisterStrongMemoryReporter(new OggReporter());
-#ifdef MOZ_VPX
-  RegisterStrongMemoryReporter(new VPXReporter());
-#endif
 
   mozilla::Telemetry::Init();
 
