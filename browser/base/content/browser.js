2676e91abd5a74bfe5b862da01913518fcd5303d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 08:48:32 +0000

    Bug 1486824 - change BrowserTestUtils to just call OpenBrowserWindow so it does the same thing as opening a window normally, r=mconley,nhnt11
    Differential Revision: https://phabricator.services.mozilla.com/D4466
    
    UltraBlame original commit: 4e195f7b2d6b8c12aba04ef69401e9af65daa05a

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -4281,6 +4281,19 @@ function toOpenWindowByType(inType, uri, features) {
     window.open(uri, "_blank", "chrome,extrachrome,menubar,resizable,scrollbars,status,toolbar");
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
 function OpenBrowserWindow(options) {
   var telemetryObj = {};
   TelemetryStopwatch.start("FX_NEW_WINDOW_MS", telemetryObj);
