6bfc406642dd7aff04c7a67ec2fa593109e91092
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 02:42:43 +0000

    Bug 1477336 [wpt PR 12103] - Simplify the magnetometer IDL test, a=testonly
    Automatic update from web-platform-testsSimplify the magnetometer IDL test (#12103)
    
    --
    
    wpt-commits: 66cd026a6d24acd9780b14b25a21563bdd4dbb71
    wpt-pr: 12103
    
    UltraBlame original commit: 31740ce97b03e433a8b524026cd25d502ffaa998

---
+++ testing/web-platform/tests/magnetometer/idlharness.https.window.js
@@ -0,0 +1,18 @@
+
+
+
+
+
+'use strict';
+
+idl_test(
+  ['magnetometer'],
+  ['generic-sensor', 'dom'],
+  idl_array => {
+    idl_array.add_objects({
+      Magnetometer: ['new Magnetometer();'],
+      UncalibratedMagnetometer: ['new UncalibratedMagnetometer();']
+    });
+  },
+  'Test IDL implementation of Magnetometer Sensor'
+);
