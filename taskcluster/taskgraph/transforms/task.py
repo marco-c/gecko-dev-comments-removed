0b7c61d5a94261f85289126f37448ad5ff72cb5d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 01:51:46 +0000

    Bug 1461777 - Taskcluster Sa target should also run on Windows. r=gps
    MozReview-Commit-ID: 7o7iPneK34e
    
    UltraBlame original commit: 23b90e4be604f0a25ac13fdcfa904ae4dc84fef9

--- taskcluster/taskgraph/transforms/task.py
+++ taskcluster/taskgraph/transforms/task.py
@@ -298,6 +298,9 @@ task_description_schema = Schema({
         
         
         Optional('purge-caches-exit-status'): [int],
+
+        
+        Optional('skip-artifacts'): bool,
     }, {
         Required('implementation'): 'generic-worker',
         Required('os'): Any('windows', 'macosx'),
@@ -376,6 +379,9 @@ task_description_schema = Schema({
         
         Required('chain-of-trust'): bool,
         Optional('taskcluster-proxy'): bool,
+
+        
+        Optional('skip-artifacts'): bool,
     }, {
         Required('implementation'): 'native-engine',
         Required('os'): Any('macosx', 'linux'),
@@ -409,6 +415,8 @@ task_description_schema = Schema({
             
             Required('name'): basestring,
         }],
+        
+        Optional('skip-artifacts'): bool,
     }, {
         Required('implementation'): 'script-engine-autophone',
         Required('os'): Any('macosx', 'linux'),
@@ -939,7 +947,7 @@ def build_generic_worker_payload(config, task, task_def):
 
     artifacts = []
 
-    for artifact in worker['artifacts']:
+    for artifact in worker.get('artifacts', []):
         a = {
             'path': artifact['path'],
             'type': artifact['type'],
