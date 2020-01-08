3ba34d167466c0de71fb91ef4c5be7116264a23f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 04:02:35 +0000

    Bug 1481793 part 2 - Do a CheckedUnwrap in JS_StructuredClone. r=luke
    UltraBlame original commit: 697419a81da4f0b4e3895ab86527df6c17897c11

--- js/src/vm/StructuredClone.cpp
+++ js/src/vm/StructuredClone.cpp
@@ -2890,12 +2890,16 @@ JS_StructuredClone(JSContext* cx, HandleValue value, MutableHandleValue vp,
 
     JSAutoStructuredCloneBuffer buf(JS::StructuredCloneScope::SameProcessSameThread, callbacks, closure);
     {
-        
-        
-        
         if (value.isObject()) {
-            AutoRealm ar(cx, &value.toObject());
-            if (!buf.write(cx, value, callbacks, closure))
+            RootedObject obj(cx, &value.toObject());
+            obj = CheckedUnwrap(obj);
+            if (!obj) {
+                ReportAccessDenied(cx);
+                return false;
+            }
+            AutoRealm ar(cx, obj);
+            RootedValue unwrappedVal(cx, ObjectValue(*obj));
+            if (!buf.write(cx, unwrappedVal, callbacks, closure))
                 return false;
         } else {
             if (!buf.write(cx, value, callbacks, closure))
