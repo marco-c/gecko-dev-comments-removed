d769c30788eb4771b21b4c0f195f39a0418ff0e3
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 00:08:44 +0000

    Bug 1459209 P3 Scaffold RemoteServiceWorkerContainerImpl to connect the binding ServiceWorkerContainer to the PServiceWorkerContainerChild actor. r=mrbkap
    UltraBlame original commit: 246c9fdaf98bfaf564660f19e9253bb9129d790c

--- dom/serviceworkers/ServiceWorkerContainerChild.h
+++ dom/serviceworkers/ServiceWorkerContainerChild.h
@@ -8,19 +8,38 @@
 #define mozilla_dom_serviceworkercontainerchild_h__
 
 #include "mozilla/dom/PServiceWorkerContainerChild.h"
+#include "mozilla/dom/WorkerHolderToken.h"
 
 namespace mozilla {
 namespace dom {
 
 class ServiceWorkerContainerChild final : public PServiceWorkerContainerChild
+                                        , public WorkerHolderToken::Listener
 {
+  RefPtr<WorkerHolderToken> mWorkerHolderToken;
+  RemoteServiceWorkerContainerImpl* mOwner;
+  bool mTeardownStarted;
+
   
   void
   ActorDestroy(ActorDestroyReason aReason) override;
 
+  
+  void
+  WorkerShuttingDown() override;
+
 public:
-  ServiceWorkerContainerChild() = default;
+  explicit ServiceWorkerContainerChild(WorkerHolderToken* aWorkerHolderToken);
   ~ServiceWorkerContainerChild() = default;
+
+  void
+  SetOwner(RemoteServiceWorkerContainerImpl* aOwner);
+
+  void
+  RevokeOwner(RemoteServiceWorkerContainerImpl* aOwner);
+
+  void
+  MaybeStartTeardown();
 };
 
 } 
