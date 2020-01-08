d11827229c101c403651bc4fe0e8dfa49dd07f01
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 14:17:08 +0000

    Bug 1494549 - Wait until ADB started in USB status sidebar test;r=daisuke
    Depends on D8335
    
    Differential Revision: https://phabricator.services.mozilla.com/D8513
    
    UltraBlame original commit: e9abf60db23b75c1807bc6afa5b3fa9d8d483af6

--- devtools/client/aboutdebugging-new/test/browser/browser_aboutdebugging_sidebar_usb_status.js
+++ devtools/client/aboutdebugging-new/test/browser/browser_aboutdebugging_sidebar_usb_status.js
@@ -4,6 +4,7 @@
 "use strict";
 
 const { adbAddon } = require("devtools/shared/adb/adb-addon");
+const { ADB } = require("devtools/shared/adb/adb");
 
 
 
@@ -32,6 +33,13 @@ add_task(async function() {
   info("Install the adb extension and wait for the message to udpate");
   adbAddon.install();
   await waitUntil(() => usbStatusElement.textContent.includes("USB devices enabled"));
+
+  
+  
+  
+  
+  info("Wait until ADB has started.");
+  await waitUntil(() => ADB.ready);
 
   info("Uninstall the adb extension and wait for the message to udpate");
   adbAddon.uninstall();
