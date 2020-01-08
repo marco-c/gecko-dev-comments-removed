3c78a1e540a2d9decd0c942731555663057cfe5e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 16:16:16 +0000

    Bug 1428558 - Part 2. Improve plumbing to sending resource updates to WebRender. r=nical
    Animated images will require scheduling a composite of the frame in
    addition to updating the ImageKey/external image ID bindings. It would
    be good if this could be done as part of the same IPDL message.
    Additionally a page may have many animated images that we update the
    frame for at the same time, so these updates should be batched together.
    In the event that we needed to regenerate the display list, or produce
    an empty transaction, ideally we would just throw these resource updates
    in with the rest of the changes. This patch allows us to do all of that
    without unnecessarily burdening the caller with tracking extra state.
    
    Differential Revision: https://phabricator.services.mozilla.com/D7499
    
    UltraBlame original commit: 1a3433fb1c2975e86cf2c3c7821ff10999f5dbab

--- gfx/layers/wr/WebRenderLayerManager.cpp
+++ gfx/layers/wr/WebRenderLayerManager.cpp
@@ -14,7 +14,6 @@
 #include "mozilla/dom/TabGroup.h"
 #include "mozilla/gfx/DrawEventRecorder.h"
 #include "mozilla/layers/CompositorBridgeChild.h"
-#include "mozilla/layers/IpcResourceUpdateQueue.h"
 #include "mozilla/layers/StackingContextHelper.h"
 #include "mozilla/layers/TextureClient.h"
 #include "mozilla/layers/WebRenderBridgeChild.h"
@@ -248,7 +247,8 @@ WebRenderLayerManager::EndEmptyTransaction(EndTransactionFlags aFlags)
   }
 
   WrBridge()->EndEmptyTransaction(mFocusTarget, mPendingScrollUpdates,
-      mPaintSequenceNumber, mLatestTransactionId, refreshStart, mTransactionStart);
+      mAsyncResourceUpdates, mPaintSequenceNumber, mLatestTransactionId,
+      refreshStart, mTransactionStart);
   ClearPendingScrollInfoUpdate();
 
   mTransactionStart = TimeStamp();
@@ -342,6 +342,19 @@ WebRenderLayerManager::EndTransactionWithoutLayer(nsDisplayList* aDisplayList,
 
   mLatestTransactionId = mTransactionIdAllocator->GetTransactionId( true);
   TimeStamp refreshStart = mTransactionIdAllocator->GetTransactionStart();
+
+  if (mAsyncResourceUpdates) {
+    if (resourceUpdates.IsEmpty()) {
+      resourceUpdates = std::move(mAsyncResourceUpdates.ref());
+    } else {
+      
+      
+      
+      WrBridge()->UpdateResources(mAsyncResourceUpdates.ref(),
+                                   false);
+    }
+    mAsyncResourceUpdates.reset();
+  }
 
   for (const auto& key : mImageKeysToDelete) {
     resourceUpdates.DeleteImage(key);
@@ -704,6 +717,40 @@ WebRenderLayerManager::CreatePersistentBufferProvider(const gfx::IntSize& aSize,
     }
   }
   return LayerManager::CreatePersistentBufferProvider(aSize, aFormat);
+}
+
+wr::IpcResourceUpdateQueue&
+WebRenderLayerManager::AsyncResourceUpdates()
+{
+  MOZ_ASSERT(NS_IsMainThread());
+
+  if (!mAsyncResourceUpdates) {
+    mAsyncResourceUpdates.emplace(WrBridge());
+
+    RefPtr<Runnable> task = NewRunnableMethod(
+      "WebRenderLayerManager::FlushAsyncResourceUpdates",
+      this, &WebRenderLayerManager::FlushAsyncResourceUpdates);
+    NS_DispatchToMainThread(task.forget());
+  }
+
+  return mAsyncResourceUpdates.ref();
+}
+
+void
+WebRenderLayerManager::FlushAsyncResourceUpdates()
+{
+  MOZ_ASSERT(NS_IsMainThread());
+
+  if (!mAsyncResourceUpdates) {
+    return;
+  }
+
+  if (!IsDestroyed() && WrBridge()) {
+    WrBridge()->UpdateResources(mAsyncResourceUpdates.ref(),
+                                 true);
+  }
+
+  mAsyncResourceUpdates.reset();
 }
 
 } 
