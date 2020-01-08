238ff5fbd3461ab4d157e81928e52852c99cf865
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 08:53:19 +0000

    Bug 1487452 - Set devtools.debugger.features.map-await-expression to true only on Nightly; r=bgrins.
    Differential Revision: https://phabricator.services.mozilla.com/D4691
    
    UltraBlame original commit: a35cbdf27bd1f79b1d756866d5a28b8e29f4c4f8

--- devtools/client/preferences/devtools-client.js
+++ devtools/client/preferences/devtools-client.js
@@ -341,7 +341,7 @@ pref("devtools.aboutdebugging.network-locations", "[]");
 #endif
 
 
-#if defined(NIGHTLY_BUILD) || defined(MOZ_DEV_EDITION)
+#if defined(NIGHTLY_BUILD)
 pref("devtools.debugger.features.map-await-expression", true);
 #else
 pref("devtools.debugger.features.map-await-expression", false);
