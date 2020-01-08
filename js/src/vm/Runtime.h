a64f9756b0f66e6d7a4ada4a8bc010f933f49b3f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 22:52:58 +0000

    Bug 1517823 - Part 2: Pass arena to MallocProvider client. r=sfink
    UltraBlame original commit: 752c683e631d73518b21da4b0924ac80ce1f6d5f

--- js/src/vm/Runtime.h
+++ js/src/vm/Runtime.h
@@ -834,13 +834,14 @@ struct JSRuntime : public js::MallocProvider<JSRuntime> {
 
 
 
-  JS_FRIEND_API void* onOutOfMemory(js::AllocFunction allocator, size_t nbytes,
+  JS_FRIEND_API void* onOutOfMemory(js::AllocFunction allocator,
+                                    arena_id_t arena, size_t nbytes,
                                     void* reallocPtr = nullptr,
                                     JSContext* maybecx = nullptr);
 
   
   JS_FRIEND_API void* onOutOfMemoryCanGC(js::AllocFunction allocator,
-                                         size_t nbytes,
+                                         arena_id_t arena, size_t nbytes,
                                          void* reallocPtr = nullptr);
 
   static const unsigned LARGE_ALLOCATION = 25 * 1024 * 1024;
