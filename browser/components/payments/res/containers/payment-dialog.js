778b9338f6de5c4c0d6d068d8f238355c2d5a235
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 21:37:42 +0000

    Bug 1446158 - Create a handleEvent custom element mixin to forward events from handleEvent to on* methods. r=MattN
    UltraBlame original commit: 7ca43b63f0e9fe96859e0cb0cedec494953d37b1

--- browser/components/payments/res/containers/payment-dialog.js
+++ browser/components/payments/res/containers/payment-dialog.js
@@ -2,6 +2,7 @@
 
 
 
+import HandleEventMixin from "../mixins/HandleEventMixin.js";
 import PaymentStateSubscriberMixin from "../mixins/PaymentStateSubscriberMixin.js";
 import paymentRequest from "../paymentRequest.js";
 
@@ -26,7 +27,7 @@ import "./shipping-option-picker.js";
 
 
 
-export default class PaymentDialog extends PaymentStateSubscriberMixin(HTMLElement) {
+export default class PaymentDialog extends HandleEventMixin(PaymentStateSubscriberMixin(HTMLElement)) {
   constructor() {
     super();
     this._template = document.getElementById("payment-dialog-template");
@@ -77,22 +78,20 @@ export default class PaymentDialog extends PaymentStateSubscriberMixin(HTMLEleme
     super.disconnectedCallback();
   }
 
-  handleEvent(event) {
-    if (event.type == "click") {
-      switch (event.currentTarget) {
-        case this._viewAllButton:
-          let orderDetailsShowing = !this.requestStore.getState().orderDetailsShowing;
-          this.requestStore.setState({ orderDetailsShowing });
-          break;
-        case this._payButton:
-          this.pay();
-          break;
-        case this._manageText:
-          if (event.target instanceof HTMLAnchorElement) {
-            this.openPreferences(event);
-          }
-          break;
-      }
+  onClick(event) {
+    switch (event.currentTarget) {
+      case this._viewAllButton:
+        let orderDetailsShowing = !this.requestStore.getState().orderDetailsShowing;
+        this.requestStore.setState({ orderDetailsShowing });
+        break;
+      case this._payButton:
+        this.pay();
+        break;
+      case this._manageText:
+        if (event.target instanceof HTMLAnchorElement) {
+          this.openPreferences(event);
+        }
+        break;
     }
   }
 
