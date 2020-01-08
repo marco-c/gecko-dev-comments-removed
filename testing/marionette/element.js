914d5147404c4ac9a457a351fe44d58518ee5004
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 02:32:02 +0000

    Bug 1400233 - Drop ContentWebElement.LegacyIdentifier key from Marionette. r=ato,whimboo
    Remove the legacy key that Marionette uses to identify web elements.
    
    UltraBlame original commit: 036eaf559f72ab3054b2fa1699feb748cc94431d

--- testing/marionette/element.js
+++ testing/marionette/element.js
@@ -1443,7 +1443,6 @@ class WebElement {
     for (let key of keys) {
       switch (key) {
         case ContentWebElement.Identifier:
-        case ContentWebElement.LegacyIdentifier:
           return ContentWebElement.fromJSON(json);
 
         case ContentWebWindow.Identifier:
@@ -1510,14 +1509,12 @@ class WebElement {
 
 
 
-
   static isReference(obj) {
     if (Object.prototype.toString.call(obj) != "[object Object]") {
       return false;
     }
 
     if ((ContentWebElement.Identifier in obj) ||
-        (ContentWebElement.LegacyIdentifier in obj) ||
         (ContentWebWindow.Identifier in obj) ||
         (ContentWebFrame.Identifier in obj) ||
         (ChromeWebElement.Identifier in obj)) {
@@ -1545,26 +1542,22 @@ this.WebElement = WebElement;
 
 class ContentWebElement extends WebElement {
   toJSON() {
-    return {
-      [ContentWebElement.Identifier]: this.uuid,
-      [ContentWebElement.LegacyIdentifier]: this.uuid,
-    };
+    return {[ContentWebElement.Identifier]: this.uuid};
   }
 
   static fromJSON(json) {
-    const {Identifier, LegacyIdentifier} = ContentWebElement;
+    const {Identifier} = ContentWebElement;
 
-    if (!(Identifier in json) && !(LegacyIdentifier in json)) {
+    if (!(Identifier in json)) {
       throw new InvalidArgumentError(
           pprint`Expected web element reference, got: ${json}`);
     }
 
-    let uuid = json[Identifier] || json[LegacyIdentifier];
+    let uuid = json[Identifier];
     return new ContentWebElement(uuid);
   }
 }
 ContentWebElement.Identifier = "element-6066-11e4-a52e-4f735466cecf";
-ContentWebElement.LegacyIdentifier = "ELEMENT";
 this.ContentWebElement = ContentWebElement;
 
 
@@ -1574,10 +1567,7 @@ this.ContentWebElement = ContentWebElement;
 
 class ContentWebWindow extends WebElement {
   toJSON() {
-    return {
-      [ContentWebWindow.Identifier]: this.uuid,
-      [ContentWebElement.LegacyIdentifier]: this.uuid,
-    };
+    return {[ContentWebWindow.Identifier]: this.uuid};
   }
 
   static fromJSON(json) {
@@ -1599,10 +1589,7 @@ this.ContentWebWindow = ContentWebWindow;
 
 class ContentWebFrame extends WebElement {
   toJSON() {
-    return {
-      [ContentWebFrame.Identifier]: this.uuid,
-      [ContentWebElement.LegacyIdentifier]: this.uuid,
-    };
+    return {[ContentWebFrame.Identifier]: this.uuid};
   }
 
   static fromJSON(json) {
@@ -1623,10 +1610,7 @@ this.ContentWebFrame = ContentWebFrame;
 
 class ChromeWebElement extends WebElement {
   toJSON() {
-    return {
-      [ChromeWebElement.Identifier]: this.uuid,
-      [ContentWebElement.LegacyIdentifier]: this.uuid,
-    };
+    return {[ChromeWebElement.Identifier]: this.uuid};
   }
 
   static fromJSON(json) {
