9ee117fa7d92e8dd8ac01f2ff0a7a87930f9ae3a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 23:31:28 +0000

    Backed out changeset 5f9ccc7b6039 (bug 1515810) for failing in browser_ext_contextMenus.js
    UltraBlame original commit: 6573f476d87a23ad9a25413587df39e1ef0fb483

--- browser/components/extensions/test/browser/head.js
+++ browser/components/extensions/test/browser/head.js
@@ -30,7 +30,6 @@
 
 
 
-
 const {PromiseTestUtils} = ChromeUtils.import("resource://testing-common/PromiseTestUtils.jsm", {});
 PromiseTestUtils.whitelistRejectionsGlobally(/Message manager disconnected/);
 PromiseTestUtils.whitelistRejectionsGlobally(/No matching message handler/);
@@ -286,16 +285,6 @@ function closeBrowserAction(extension, win = window) {
   CustomizableUI.hidePanelForNode(node);
 
   return Promise.resolve();
-}
-
-async function toggleBookmarksToolbar(visible = true) {
-  let bookmarksToolbar = document.getElementById("PersonalToolbar");
-  let transitionPromise =
-    BrowserTestUtils.waitForEvent(bookmarksToolbar, "transitionend",
-                                  e => e.propertyName == "max-height");
-
-  setToolbarVisibility(bookmarksToolbar, visible);
-  await transitionPromise;
 }
 
 async function openContextMenuInPopup(extension, selector = "body") {
