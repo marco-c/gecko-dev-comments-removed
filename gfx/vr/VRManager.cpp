9f789edb9429872f491113aae4942faf8c876120
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 23:35:15 +0000

    Bug 1516554 - Part 4: Fix Win 7 try fails when VR process enable but no GPU process. r=kip
    Differential Revision: https://phabricator.services.mozilla.com/D16258
    
    UltraBlame original commit: bff1af3da43332085f4c862c9d8718c17c193e8d

--- gfx/vr/VRManager.cpp
+++ gfx/vr/VRManager.cpp
@@ -78,7 +78,7 @@ VRManager::VRManager()
 #if !defined(MOZ_WIDGET_ANDROID)
   
   
-  if (!gfxPrefs::VRProcessEnabled()) {
+  if (!gfxPrefs::VRProcessEnabled() || !XRE_IsGPUProcess()) {
     VRServiceManager::Get().CreateService();
   }
   if (VRServiceManager::Get().IsServiceValid()) {
@@ -127,6 +127,7 @@ void VRManager::Destroy() {
     VRServiceManager::Get().Shutdown();
   }
 #endif
+  Shutdown();
   mInitialized = false;
 }
 
@@ -140,7 +141,9 @@ void VRManager::Shutdown() {
   if (VRServiceManager::Get().IsServiceValid()) {
     VRServiceManager::Get().Stop();
   }
-  if (gfxPrefs::VRProcessEnabled() && mVRServiceStarted) {
+  
+  
+  if (XRE_IsGPUProcess() && gfxPrefs::VRProcessEnabled() && mVRServiceStarted) {
     RefPtr<Runnable> task = NS_NewRunnableFunction(
         "VRServiceManager::ShutdownVRProcess",
         []() -> void { VRServiceManager::Get().ShutdownVRProcess(); });
@@ -435,13 +438,15 @@ void VRManager::EnumerateVRDisplays() {
 
 
 #if !defined(MOZ_WIDGET_ANDROID)
-  if (gfxPrefs::VRProcessEnabled() && !mVRServiceStarted) {
-    VRServiceManager::Get().CreateVRProcess();
-    mVRServiceStarted = true;
-  } else if (!gfxPrefs::VRProcessEnabled()) {
-    if (VRServiceManager::Get().IsServiceValid()) {
-      VRServiceManager::Get().Start();
+  if (!mVRServiceStarted) {
+    if (XRE_IsGPUProcess() && gfxPrefs::VRProcessEnabled()) {
+      VRServiceManager::Get().CreateVRProcess();
       mVRServiceStarted = true;
+    } else {
+      if (VRServiceManager::Get().IsServiceValid()) {
+        VRServiceManager::Get().Start();
+        mVRServiceStarted = true;
+      }
     }
   }
 #endif
