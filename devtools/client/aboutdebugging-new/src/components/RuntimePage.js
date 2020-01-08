d5bbaad4298ad4c8ff903550933c9d02d72b7eb3
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 03:15:37 +0000

    Bug 1477596: Show runtime information. r=jdescottes
    MozReview-Commit-ID: 5j7HTlGW9x9
    
    UltraBlame original commit: 6592552f94716fefbf7340777d2d4a80eba3c958

---
+++ devtools/client/aboutdebugging-new/src/components/RuntimePage.js
@@ -0,0 +1,29 @@
+
+
+
+
+"use strict";
+
+const { createFactory, PureComponent } = require("devtools/client/shared/vendor/react");
+const dom = require("devtools/client/shared/vendor/react-dom-factories");
+
+const RuntimeInfo = createFactory(require("./RuntimeInfo"));
+
+const Services = require("Services");
+
+class RuntimePage extends PureComponent {
+  render() {
+    return dom.article(
+      {
+        className: "runtime-page",
+      },
+      RuntimeInfo({
+        icon: "chrome://branding/content/icon64.png",
+        name: Services.appinfo.name,
+        version: Services.appinfo.version,
+      }),
+    );
+  }
+}
+
+module.exports = RuntimePage;
