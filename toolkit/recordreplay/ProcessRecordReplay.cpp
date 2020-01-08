c5f72d057f80000a9a80e049a0351810c85da4db
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 01:42:24 +0000

    Bug 1465294 Part 14 - Use a single thread in stylo when recording/replaying, r=xidorn.
    UltraBlame original commit: a0ee9e803ff24c59245322da5c2f692d803300e1

--- toolkit/recordreplay/ProcessRecordReplay.cpp
+++ toolkit/recordreplay/ProcessRecordReplay.cpp
@@ -152,6 +152,9 @@ RecordReplayInterface_Initialize(int aArgc, char* aArgv[])
   InitializeCountdownThread();
   SetupDirtyMemoryHandler();
 
+  
+  putenv((char*) "STYLO_THREADS=1");
+
   thread->SetPassThrough(false);
 
   Lock::InitializeLocks();
