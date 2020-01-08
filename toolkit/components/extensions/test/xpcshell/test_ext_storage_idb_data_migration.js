1aebd2dd6d974ba0dd48febc1f362e949afc0dd5
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 02:09:24 +0000

    Bug 1476929 - Fix test_ext_storage_idb_data_migration.js failure when running on beta. r=mixedpuppy
    This test file has to call AddonTestUtils.createAppInfo before importing ExtensionStorage.jsm:
    ExtensionStorage.jsm internally imports Services.jsm and so it would cache a Services.appinfo
    object with an undefined version property, which makes the AddonManager.jsm to raise an exception
    when the test file runs on beta (because it tries to access the undefined version property when
    !MOZ_COMPATIBILITY_NIGHTLY).
    
    MozReview-Commit-ID: Kok2llzLJ66
    
    UltraBlame original commit: bf63176221f0c8dff2f35f7ee2716c0ef4e6afc6

--- toolkit/components/extensions/test/xpcshell/test_ext_storage_idb_data_migration.js
+++ toolkit/components/extensions/test/xpcshell/test_ext_storage_idb_data_migration.js
@@ -5,6 +5,9 @@
 
 
 
+AddonTestUtils.init(this);
+AddonTestUtils.createAppInfo("xpcshell@tests.mozilla.org", "XPCShell", "1", "42");
+
 ChromeUtils.import("resource://gre/modules/XPCOMUtils.jsm");
 ChromeUtils.import("resource://gre/modules/ExtensionStorage.jsm");
 ChromeUtils.import("resource://gre/modules/TelemetryController.jsm");
@@ -19,14 +22,9 @@ XPCOMUtils.defineLazyModuleGetters(this, {
 });
 
 const {
-  createAppInfo,
   promiseShutdownManager,
   promiseStartupManager,
 } = AddonTestUtils;
-
-AddonTestUtils.init(this);
-
-createAppInfo("xpcshell@tests.mozilla.org", "XPCShell", "1", "42");
 
 const {
   IDB_MIGRATED_PREF_BRANCH,
