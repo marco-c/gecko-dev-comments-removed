c19b625bdcbea780d36b00aa08514221480bc6ff
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 13:34:08 +0000

    Bug 1488792: Reject calls to update() while service worker in 'installing' state r=asuth
    The service workers spec mandates that calling ServiceWorkerRegistration.update()
    on a registration whose newest worker is in the 'installing' state fail immediately.
    This commit implements this requirement and tests it.
    
    Differential Revision: https://phabricator.services.mozilla.com/D5241
    
    UltraBlame original commit: 9a70e53741767b0c2e149e16f5a33594bebc89ba

--- dom/serviceworkers/ServiceWorkerManager.cpp
+++ dom/serviceworkers/ServiceWorkerManager.cpp
@@ -2434,6 +2434,20 @@ ServiceWorkerManager::Update(nsIPrincipal* aPrincipal,
                                                nsCString(aScope));
 }
 
+namespace {
+
+void
+RejectUpdateWithInvalidStateError(ServiceWorkerUpdateFinishCallback& aCallback)
+{
+  ErrorResult error(NS_ERROR_DOM_INVALID_STATE_ERR);
+  aCallback.UpdateFailed(error);
+
+  
+  error.SuppressException();
+}
+
+}
+
 void
 ServiceWorkerManager::UpdateInternal(nsIPrincipal* aPrincipal,
                                      const nsACString& aScope,
@@ -2459,12 +2473,12 @@ ServiceWorkerManager::UpdateInternal(nsIPrincipal* aPrincipal,
   
   RefPtr<ServiceWorkerInfo> newest = registration->Newest();
   if (!newest) {
-    ErrorResult error(NS_ERROR_DOM_INVALID_STATE_ERR);
-    aCallback->UpdateFailed(error);
+    RejectUpdateWithInvalidStateError(*aCallback);
+    return;
+  }
 
-    
-    error.SuppressException();
-
+  if (newest->State() == ServiceWorkerState::Installing) {
+    RejectUpdateWithInvalidStateError(*aCallback);
     return;
   }
 
