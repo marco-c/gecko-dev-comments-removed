77647cc98ca9fa6780c8993e1d0bc91523bcbcc3
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 11:11:28 +0000

    Bug 1479033 - Introduce Android accessibility directory. r=Jamie
    UltraBlame original commit: 1b683d101ee9eae43d7fee0bf2c0b6065c50bb16

---
+++ accessible/android/ApplicationAccessibleWrap.h
@@ -0,0 +1,20 @@
+
+
+
+
+
+
+
+#ifndef mozilla_a11y_ApplicationAccessibleWrap_h__
+#define mozilla_a11y_ApplicationAccessibleWrap_h__
+
+#include "ApplicationAccessible.h"
+
+namespace mozilla {
+namespace a11y {
+
+typedef ApplicationAccessible ApplicationAccessibleWrap;
+} 
+} 
+
+#endif
