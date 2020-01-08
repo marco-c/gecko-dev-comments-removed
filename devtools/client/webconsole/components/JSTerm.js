2f97f6b55fdb52e77fec233636d4e3d28bbdf5e3
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 23:10:16 +0000

    Bug 1515501 - Close autocomplete popup on Cmd/Ctrl + ArrowRight; r=Honza.
    This was causing some issue with the resulting text being non-sense.
    A test case is added to ensure it works as expected.
    
    Differential Revision: https://phabricator.services.mozilla.com/D15936
    
    UltraBlame original commit: d3aef0e30186f54f428fb6f0299a64ecd11039dd

--- devtools/client/webconsole/components/JSTerm.js
+++ devtools/client/webconsole/components/JSTerm.js
@@ -177,6 +177,18 @@ class JSTerm extends Component {
           return "CodeMirror.Pass";
         };
 
+        const onArrowRight = () => {
+          
+          
+          if (this.getAutoCompletionText()) {
+            this.acceptProposedCompletion();
+            return null;
+          }
+
+          this.clearCompletion();
+          return "CodeMirror.Pass";
+        };
+
         this.editor = new Editor({
           autofocus: true,
           enableCodeFolding: false,
@@ -239,17 +251,9 @@ class JSTerm extends Component {
             "Ctrl-Left": onArrowLeft,
             "Cmd-Left": onArrowLeft,
 
-            "Right": () => {
-              
-              
-              if (this.getAutoCompletionText()) {
-                this.acceptProposedCompletion();
-                return null;
-              }
-
-              this.clearCompletion();
-              return "CodeMirror.Pass";
-            },
+            "Right": onArrowRight,
+            "Ctrl-Right": onArrowRight,
+            "Cmd-Right": onArrowRight,
 
             "Ctrl-N": () => {
               
@@ -843,6 +847,15 @@ class JSTerm extends Component {
         (this.autocompletePopup.isOpen || this.getAutoCompletionText())
       ) {
         this.clearCompletion();
+      }
+
+      
+      if (event.keyCode === KeyCodes.DOM_VK_RIGHT) {
+        if (this.getAutoCompletionText()) {
+          this.acceptProposedCompletion();
+        }
+        this.clearCompletion();
+        event.preventDefault();
       }
 
       return;
