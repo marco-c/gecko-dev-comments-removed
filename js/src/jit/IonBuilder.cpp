2076275747b34311b2a392b294cb50c5520400a7
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 11:47:14 +0000

    Backed out 2 changesets (bug 1488786) for raptor gdocs failures. CLOSED TREE
    Backed out changeset 6c5b95f8154e (bug 1488786)
    Backed out changeset e0f7882e7316 (bug 1488786)
    
    UltraBlame original commit: 8bf660646ca6f18a269af9550c23d34a87140579

--- js/src/jit/IonBuilder.cpp
+++ js/src/jit/IonBuilder.cpp
@@ -8998,17 +8998,6 @@ IonBuilder::getElemAddCache(MDefinition* obj, MDefinition* index)
         } else {
             barrier = BarrierKind::TypeSet;
         }
-
-        
-        
-        if (barrier != BarrierKind::TypeSet) {
-            BarrierKind protoBarrier;
-            MOZ_TRY_VAR(protoBarrier, PropertyReadOnPrototypeNeedsTypeBarrier(this, obj, nullptr, types));
-            if (protoBarrier != BarrierKind::NoBarrier) {
-                MOZ_ASSERT(barrier <= protoBarrier);
-                barrier = protoBarrier;
-            }
-        }
     } else {
         
         
