185995ea27a8ce16655d93c14bfedd36c2a1c986
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 19:49:45 +0000

    Bug 1511095 - Remove support for the 30fps pref, and replace the loading.svg with the 30fps variant. r=mconley
    Differential Revision: https://phabricator.services.mozilla.com/D13462
    
    UltraBlame original commit: 0ceb5c6cc6b076e9b79f9d5f70ae901f8fd9644e

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -431,7 +431,6 @@ pref("browser.link.open_newwindow.disabled_in_fullscreen", false);
 
 
 pref("browser.tabs.multiselect", true);
-pref("browser.tabs.30FpsThrobber", false);
 pref("browser.tabs.closeTabByDblclick", false);
 pref("browser.tabs.closeWindowWithLastTab", true);
 
