b53bf63c50fca6e1d51f66b5b3738adf5e89b8d3
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 10:03:44 +0000

    Bug 1451503 Convert reftest to a webextension r=kmag
    Differential Revision: https://phabricator.services.mozilla.com/D5233
    
    UltraBlame original commit: 1858dfdbd0e36d382e03354bd728f81be58a3fc3

---
+++ layout/tools/reftest/api.js
@@ -0,0 +1,114 @@
+
+
+
+
+const Cm = Components.manager;
+
+ChromeUtils.import("resource://gre/modules/Services.jsm");
+ChromeUtils.import("resource://gre/modules/XPCOMUtils.jsm");
+
+XPCOMUtils.defineLazyServiceGetter(this, "resProto",
+                                   "@mozilla.org/network/protocol;1?name=resource",
+                                   "nsISubstitutingProtocolHandler");
+
+XPCOMUtils.defineLazyServiceGetter(this, "aomStartup",
+                                   "@mozilla.org/addons/addon-manager-startup;1",
+                                   "amIAddonManagerStartup");
+
+function processTerminated() {
+  return new Promise(resolve => {
+    Services.obs.addObserver(function observe(subject, topic) {
+      if (topic == "ipc:content-shutdown") {
+        Services.obs.removeObserver(observe, topic);
+        resolve();
+      }
+    }, "ipc:content-shutdown");
+  });
+}
+
+function startAndroid(win) {
+  
+  win.setTimeout(function() {OnRefTestLoad(win);}, 0);
+}
+
+var WindowListener = {
+  onOpenWindow: function(xulWin) {
+    Services.wm.removeListener(WindowListener);
+
+    let win = xulWin.docShell.domWindow;
+    win.addEventListener("load", function listener() {
+      
+      for (win of Services.wm.getEnumerator("navigator:browser")) {
+        break;
+      }
+
+      win.addEventListener("pageshow", function() {
+        startAndroid(win);
+      }, {once: true});
+    }, {once: true});
+  }
+};
+
+this.reftest = class extends ExtensionAPI {
+  onStartup() {
+    let uri = Services.io.newURI("chrome/reftest/res/", null, this.extension.rootURI);
+    resProto.setSubstitutionWithFlags("reftest", uri, resProto.ALLOW_CONTENT_ACCESS);
+
+    const manifestURI = Services.io.newURI("manifest.json", null, this.extension.rootURI);
+    this.chromeHandle = aomStartup.registerChrome(manifestURI, [
+      ["content", "reftest", "chrome/reftest/content/", "contentaccessible=yes"],
+    ]);
+
+    
+    
+    
+    
+    
+    
+    let win = Services.wm.getMostRecentWindow("navigator:browser");
+
+    if (Services.appinfo.OS == "Android") {
+      ChromeUtils.import("resource://reftest/reftest.jsm");
+      if (win) {
+        startAndroid(win);
+      } else {
+        Services.wm.addListener(WindowListener);
+      }
+      return;
+    }
+
+    Services.io.manageOfflineStatus = false;
+    Services.io.offline = false;
+
+    let dummy = Services.ww.openWindow(null, "about:blank", "dummy",
+                                       "chrome,dialog=no,left=800,height=200,width=200,all",null);
+    dummy.onload = async function() {
+      
+      win.close();
+
+      ChromeUtils.import("resource://reftest/PerTestCoverageUtils.jsm");
+      if (PerTestCoverageUtils.enabled) {
+        
+        
+        await processTerminated();
+      }
+
+      dummy.focus();
+      Services.ww.openWindow(null, "chrome://reftest/content/reftest.xul",
+                             "_blank", "chrome,dialog=no,all", {});
+    };
+  }
+
+  onShutdown() {
+    resProto.setSubstitution("reftest", null);
+
+    this.chromeHandle.destruct();
+    this.chromeHandle = null;
+
+    if (Services.appinfo.OS == "Android") {
+      Services.wm.removeListener(WindowListener);
+      OnRefTestUnload();
+      Cu.unload("resource://reftest/reftest.jsm");
+    }
+  }
+};
