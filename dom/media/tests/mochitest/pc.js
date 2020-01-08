515714e64a7229acd4f542fb5f7fcbec75197856
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 15:04:22 +0000

    Bug 1225729 - Restore RTCP timestamp tests on Android. r=jib
    Android no longer seems to generate the mysterious 2085978496000 timestamps, so we can run these tests on Android again.
    
    Differential Revision: https://phabricator.services.mozilla.com/D9356
    
    UltraBlame original commit: 69d881a7866706d50389e433904d5946106e42d1

--- dom/media/tests/mochitest/pc.js
+++ dom/media/tests/mochitest/pc.js
@@ -1793,24 +1793,19 @@ PeerConnectionWrapper.prototype = {
       if (!twoMachines) {
         
         
-        if (res.timestamp != 2085978496000) {
-          if (false) {
-            ok(res.timestamp >= minimum,
-               "Valid " + (res.isRemote? "rtcp" : "rtp") + " timestamp " +
-                   res.timestamp + " >= " + minimum + " (" +
-                   (res.timestamp - minimum) + " ms)");
-          } else {
-            info("FIXME bug 1495446: uncomment the timestamp test case " +
-                 "above after RTCP epoch bug 1495446 is fixed.");
-          }
-          ok(res.timestamp <= nowish,
+        if (false) {
+          ok(res.timestamp >= minimum,
              "Valid " + (res.isRemote? "rtcp" : "rtp") + " timestamp " +
-                 res.timestamp + " <= " + nowish + " (" +
-                 (res.timestamp - nowish) + " ms)");
+                 res.timestamp + " >= " + minimum + " (" +
+                 (res.timestamp - minimum) + " ms)");
         } else {
-          info("Bug 1225729: Uninitialized timestamp (" + res.timestamp +
-                "), should be >=" + minimum + " and <= " + nowish);
+          info("FIXME bug 1495446: uncomment the timestamp test case " +
+               "above after RTCP epoch bug 1495446 is fixed.");
         }
+        ok(res.timestamp <= nowish,
+           "Valid " + (res.isRemote? "rtcp" : "rtp") + " timestamp " +
+               res.timestamp + " <= " + nowish + " (" +
+               (res.timestamp - nowish) + " ms)");
       }
       if (res.isRemote) {
         continue;
