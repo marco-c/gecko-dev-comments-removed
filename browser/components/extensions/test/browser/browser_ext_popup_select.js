c97afb1dcc1f4bce252ae995bf9592bfbf07695d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 07:23:02 +0000

    Bug 1480292 - Fix browser_ext_popup_select intermittent failures. r=mixedpuppy
    From what I saw by reproducing it locally (e.g. I've also been able to trigger it locally pretty often on linux64 by using "./mach mochitest --verify"), the select popup may sometimes be still using part of its previous position while switching between the test case for the browserAction and the test case for the pageAction.
    
    On linux64 this test was also (from time to time) sending the mouse event when the select element wasn't yet ready to be clicked in the extension popup.
    
    This patch aims to make the behaviors of this test more stable by applying the following changes:
    - disable the cosmeticAnimations while running these tests
    - explicitly wait the select element in the popup (using ContentTask.spawn and ContentTaskUtils.waitForCondition) before sending it a mouse event
    - explicilty close the select popup (and wait the select popup to be hidden) before proceeding to the next test case
    
    The following push to try seems to confirm that these changes are helping to make the test more stable:
    - https://treeherder.mozilla.org/#/jobs?repo=try&revision=086c526724ba409068d679036dd3ef13788535b6&selectedJob=194152144
    
    Differential Revision: https://phabricator.services.mozilla.com/D3438
    
    UltraBlame original commit: f35fa6e7fb463ce848beb3df1550a12089527974

--- browser/components/extensions/test/browser/browser_ext_popup_select.js
+++ browser/components/extensions/test/browser/browser_ext_popup_select.js
@@ -2,6 +2,10 @@
 
 "use strict";
 
+add_task(async function testSetup() {
+  Services.prefs.setBoolPref("toolkit.cosmeticAnimations.enabled", false);
+});
+
 add_task(async function testPopupSelectPopup() {
   let extension = ExtensionTestUtils.loadExtension({
     background() {
@@ -46,6 +50,13 @@ add_task(async function testPopupSelectPopup() {
   async function testPanel(browser) {
     let popupPromise = promisePopupShown(selectPopup);
 
+    
+    
+    await ContentTask.spawn(browser, null, async () => {
+      await ContentTaskUtils.waitForCondition(() => {
+        return content.document && content.document.querySelector("#select");
+      });
+    });
     BrowserTestUtils.synthesizeMouseAtCenter("#select", {}, browser);
 
     await popupPromise;
@@ -65,6 +76,11 @@ add_task(async function testPopupSelectPopup() {
 
     is(Math.floor(boxObject.screenY + elemRect.bottom), popupRect.top,
        "Select popup has the correct y origin");
+
+    
+    const onPopupHidden = BrowserTestUtils.waitForEvent(selectPopup, "popuphidden");
+    selectPopup.hidePopup();
+    await onPopupHidden;
   }
 
   {
@@ -86,4 +102,8 @@ add_task(async function testPopupSelectPopup() {
   }
 
   await extension.unload();
+});
+
+add_task(async function testTeardown() {
+  Services.prefs.clearUserPref("toolkit.cosmeticAnimations.enabled");
 });
