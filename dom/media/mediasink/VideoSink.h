1d1aaaff6dcc08c966985de7a6f49da6cbbc95d6
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 08:22:50 +0000

    Backed out changeset 8c8ff2c7bfa1 (bug 1485536) for crashes at VideoSink. CLOSED TREE
    UltraBlame original commit: b0a28bbe750bdac8ac5b3eaa65787fcb74fb6f9c

--- dom/media/mediasink/VideoSink.h
+++ dom/media/mediasink/VideoSink.h
@@ -132,8 +132,7 @@ private:
   
   TimeUnit mVideoFrameEndTime;
 
-  uint32_t mOldCompositorDroppedCount;
-  uint32_t mPendingDroppedCount;
+  uint32_t mOldDroppedCount;
 
   
   MediaEventListener mPushListener;
