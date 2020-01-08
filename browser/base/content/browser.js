5ac8251f94e74a1b516d76cd32696479e57acfaa
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 08:51:12 +0000

    Bug 1485746 - Cursor gets reset to start of address bar on window switch. r=adw
    This restores the previous behavior where we set the selection only when setting a new different value
    
    Differential Revision: https://phabricator.services.mozilla.com/D4528
    
    UltraBlame original commit: 621979b26f697513f06f4c5f2db1d5ee580c4363

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -2686,9 +2686,15 @@ function URLBarSetURI(aURI) {
     valid = true;
   }
 
+  let isDifferentValidValue = valid && value != gURLBar.value;
   gURLBar.value = value;
   gURLBar.valueIsTyped = !valid;
   gURLBar.removeAttribute("usertyping");
+  if (isDifferentValidValue) {
+    
+    
+    gURLBar.selectionStart = gURLBar.selectionEnd = 0;
+  }
 
   SetPageProxyState(valid ? "valid" : "invalid");
 }
