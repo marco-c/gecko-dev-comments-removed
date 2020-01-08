28d9949d1dfad072162d74bccdb3b78f9593cc77
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 07:34:43 +0000

    Bug 1480027 - Part 3: Ignore animations that have negative infinity delay or end delay. r=gl
    Differential Revision: https://phabricator.services.mozilla.com/D3380
    
    UltraBlame original commit: a2ad987a8861b30f011c4406a7b69494a0af4497

--- devtools/client/inspector/animation/animation.js
+++ devtools/client/inspector/animation/animation.js
@@ -676,8 +676,8 @@ class AnimationInspector {
     
     
     
-    animations = animations.filter(anim => anim.state.delay !== Infinity &&
-                                           anim.state.endDelay !== Infinity);
+    animations = animations.filter(anim => Math.abs(anim.state.delay) !== Infinity &&
+                                           Math.abs(anim.state.endDelay) !== Infinity);
 
     this.inspector.store.dispatch(updateAnimations(animations));
 
