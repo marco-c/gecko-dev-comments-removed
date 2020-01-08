cf56a7c89d1a00e233e39ac6abda51d5f1cdaea7
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 14:48:12 +0000

    Bug 1497898 - Update gecko wpt manifest update to use caches, r=ato
    Depends on D8231
    
    Differential Revision: https://phabricator.services.mozilla.com/D8232
    
    UltraBlame original commit: 57877c614829ff051ed0e2b66f895292e904b207

--- testing/web-platform/mach_commands_base.py
+++ testing/web-platform/mach_commands_base.py
@@ -2,7 +2,10 @@
 
 
 
+import os
 import sys
+
+from mozboot.util import get_state_dir
 
 
 def create_parser_wpt():
@@ -44,4 +47,5 @@ class WebPlatformTestsRunner(object):
         return manifestupdate.run(logger=logger,
                                   src_root=self.setup.topsrcdir,
                                   obj_root=self.setup.topobjdir,
+                                  cache_root=os.path.join(get_state_dir()[0], "cache", "wpt"),
                                   **kwargs)
