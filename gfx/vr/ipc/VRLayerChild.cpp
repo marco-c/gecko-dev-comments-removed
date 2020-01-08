6f80c7746caddd642de2282425bddf3d5ce8d224
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 22:45:40 +0000

    Backed out changeset 87fae5f294b8 (bug 1474034) for VRDisplayHost.cpp build bustages CLOSED TREE
    UltraBlame original commit: 600572209eb7a9c656c4f3bc19aba10422d525f7

--- gfx/vr/ipc/VRLayerChild.cpp
+++ gfx/vr/ipc/VRLayerChild.cpp
@@ -67,7 +67,7 @@ void VRLayerChild::SubmitFrame(const VRDisplayInfo& aDisplayInfo) {
 
 
 
-  if (!mThisFrameTexture || aDisplayInfo.mDisplayState.lastSubmittedFrameId ==
+  if (!mThisFrameTexture || aDisplayInfo.mDisplayState.mLastSubmittedFrameId ==
                                 mLastSubmittedFrameId) {
     mThisFrameTexture = mCanvasElement->GetVRFrame();
   }
