455cd3da14402413862b678344594461ebbc7770
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 18:20:08 +0000

    Backed out changeset 26933ef9fa13 (bug 1506323) for landing the wrong patch.
    UltraBlame original commit: 22e8321b69f180a1d74975fd8f162326a49d0efe

--- js/src/vm/JSContext.h
+++ js/src/vm/JSContext.h
@@ -1071,6 +1071,16 @@ ExpandErrorArgumentsVA(JSContext* cx, JSErrorCallback callback,
 extern void
 ReportUsageErrorASCII(JSContext* cx, HandleObject callee, const char* msg);
 
+
+
+
+
+
+
+extern bool
+PrintError(JSContext* cx, FILE* file, JS::ConstUTF8CharsZ toStringResult,
+           JSErrorReport* report, bool reportWarnings);
+
 extern void
 ReportIsNotDefined(JSContext* cx, HandlePropertyName name);
 
