bd23a5386c00f85f44df8a787226ba762fc2f13d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 07:28:21 +0000

    Bug 1483546 [wpt PR 12500] - Avoid invoking the Notification constructor in service workers, a=testonly
    Automatic update from web-platform-testsAvoid invoking the Notification constructor in service workers (#12500)
    
    The idlharness.https.any.serviceworker.html test had a lot of spurious
    failures because of this:
    https://wpt.fyi/results/notifications/idlharness.https.any.serviceworker.html?sha=86e157b387
    https://wpt.fyi/results/notifications/idlharness.https.any.serviceworker.html?sha=3a498429bf
    
    With these changes, there are fewer subtests, but no failures in
    Chrome and 9 in Firefox, which appear to be legitimate failures.
    --
    
    wpt-commits: ae58b0b432139b369ba7961452ff94fde2282d0a
    wpt-pr: 12500
    
    UltraBlame original commit: 35dd99779ab6446f629d53bc363e90ebfff4d79c

--- testing/web-platform/tests/notifications/idlharness.https.any.js
+++ testing/web-platform/tests/notifications/idlharness.https.any.js
@@ -10,18 +10,22 @@ idl_test(
   ['notifications'],
   ['service-workers', 'html', 'dom'],
   idl_array => {
-    idl_array.add_objects({
-      Notification: ['notification'],
-    });
     if (self.ServiceWorkerGlobalScope) {
       idl_array.add_objects({
-        NotificationEvent: ['notificationEvent'],
         ServiceWorkerGlobalScope: ['self'],
       });
-    }
-    self.notification = new Notification("Running idlharness.");
-    if (self.ServiceWorkerGlobalScope) {
-      self.notificationEvent = new NotificationEvent("type", { notification: notification });
+      
+      
+      
+    } else {
+      
+      
+      
+      
+      idl_array.add_objects({
+        Notification: ['notification'],
+      });
+      self.notification = new Notification('title');
     }
   }
 );
