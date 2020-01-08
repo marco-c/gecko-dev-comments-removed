cd827fba3f1f9dd5ec412dc79a8311d7a1beb44f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 14:37:24 +0000

    Bug 1499236 - remove customizableui menubar binding, r=Felipe
    Differential Revision: https://phabricator.services.mozilla.com/D8901
    
    UltraBlame original commit: 5c84e7821c57ada673b4cf554a000961f57efdf8

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -107,7 +107,7 @@ XPCOMUtils.defineLazyScriptGetter(this, ["LightWeightThemeWebInstaller",
                                   "chrome://browser/content/browser-addons.js");
 XPCOMUtils.defineLazyScriptGetter(this, "ctrlTab",
                                   "chrome://browser/content/browser-ctrlTab.js");
-XPCOMUtils.defineLazyScriptGetter(this, "CustomizationHandler",
+XPCOMUtils.defineLazyScriptGetter(this, ["CustomizationHandler", "AutoHideMenubar"],
                                   "chrome://browser/content/browser-customization.js");
 XPCOMUtils.defineLazyScriptGetter(this, ["PointerLock", "FullScreen"],
                                   "chrome://browser/content/browser-fullScreenAndPointerLock.js");
@@ -1242,6 +1242,9 @@ var gBrowserInit = {
       }
     }
 
+    
+    
+    AutoHideMenubar.init();
     
     window.TabBarVisibility.update();
     TabsInTitlebar.init();
