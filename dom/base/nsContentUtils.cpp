e7ce4ce31ef5d5ab7d074c032affb9397b5dcf06
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 14:11:26 +0000

    Bug 734063 - Remove the multicol element from nsContentUtils::IsHTMLBlock r=hsivonen
    UltraBlame original commit: c4a64c83582060ffc3c30501a47cfa59ecc8fab3

--- dom/base/nsContentUtils.cpp
+++ dom/base/nsContentUtils.cpp
@@ -1755,7 +1755,6 @@ nsContentUtils::IsHTMLBlock(nsIContent* aContent)
                                        nsGkAtoms::li,
                                        nsGkAtoms::listing,
                                        nsGkAtoms::menu,
-                                       nsGkAtoms::multicol, 
                                        nsGkAtoms::nav,
                                        nsGkAtoms::ol,
                                        nsGkAtoms::p,
