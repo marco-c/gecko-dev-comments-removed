742f15cee68c678ec7b69079ec7002c1cee93bf7
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 10:53:35 +0000

    Bug 1491736 - Remove an unnecessary #include from "jit/IonCode.h".  r=jandem
    UltraBlame original commit: d9300e88eb74b637f44f98dfa6ce3b30b17eb642

--- js/src/vm/TypeInference.h
+++ js/src/vm/TypeInference.h
@@ -1188,28 +1188,6 @@ inline bool isInlinableCall(jsbytecode* pc);
 bool
 ClassCanHaveExtraProperties(const Class* clasp);
 
-
-
-class IonCompilationId
-{
-    
-    
-    uint32_t idLo_;
-    uint32_t idHi_;
-
-  public:
-    explicit IonCompilationId(uint64_t id)
-      : idLo_(id & UINT32_MAX),
-        idHi_(id >> 32)
-    {}
-    bool operator==(const IonCompilationId& other) const {
-        return idLo_ == other.idLo_ && idHi_ == other.idHi_;
-    }
-    bool operator!=(const IonCompilationId& other) const {
-        return !operator==(other);
-    }
-};
-
 class RecompileInfo
 {
     JSScript* script_;
