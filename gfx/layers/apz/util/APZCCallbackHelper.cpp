503503ac6bc9cf76a07546baf8e4eb1a25920fc8
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 12:58:00 +0000

    Bug 1470504 - Use IGNORE_ROOT_SCROLL_FRAME in PrepareForSetTargetAPZCNotification() whenever zooming is enabled. r=kats
    Differential Revision: https://phabricator.services.mozilla.com/D7644
    
    UltraBlame original commit: 85dc90d04f3154cba25ac699314b96fdb4879eaf

--- gfx/layers/apz/util/APZCCallbackHelper.cpp
+++ gfx/layers/apz/util/APZCCallbackHelper.cpp
@@ -676,12 +676,13 @@ PrepareForSetTargetAPZCNotification(nsIWidget* aWidget,
   nsPoint point =
     nsLayoutUtils::GetEventCoordinatesRelativeTo(aWidget, aRefPoint, aRootFrame);
   uint32_t flags = 0;
-#ifdef MOZ_WIDGET_ANDROID
-  
-  
-  
-  flags = nsLayoutUtils::IGNORE_ROOT_SCROLL_FRAME;
-#endif
+  if (gfxPrefs::APZAllowZooming()) {
+    
+    
+    
+    
+    flags = nsLayoutUtils::IGNORE_ROOT_SCROLL_FRAME;
+  }
   nsIFrame* target =
     nsLayoutUtils::GetFrameForPoint(aRootFrame, point, flags);
   nsIScrollableFrame* scrollAncestor = target
