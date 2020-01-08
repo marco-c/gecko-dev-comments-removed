2dfb04004b75918348874e32330f1d792ce72397
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 10:34:58 +0000

    bug 1490333: marionette: move whenIdle to sync module; r=whimboo
    This patch moves the private whenIdle function to sync so it can
    be shared across JSMs.
    
    It also changes its semantics somewhat, so that instead of taking
    a callback function (suitable for DOM event callbacks) it returns
    a promise that is resolved when the main thread becomes idle and
    the window has completed an animation frame tick.
    
    UltraBlame original commit: 567ccbeb5a4f0a5aebde95ea462e74033419182a

--- testing/marionette/sync.js
+++ testing/marionette/sync.js
@@ -16,6 +16,7 @@ const {Log} = ChromeUtils.import("chrome://marionette/content/log.js", {});
 XPCOMUtils.defineLazyGetter(this, "logger", Log.get);
 
 this.EXPORTED_SYMBOLS = [
+  "IdlePromise",
   "MessageManagerDestroyedPromise",
   "PollPromise",
   "Sleep",
@@ -263,5 +264,22 @@ function MessageManagerDestroyedPromise(messageManager) {
     }
 
     Services.obs.addObserver(observe, "message-manager-disconnect");
+  });
+}
+
+
+
+
+
+
+
+
+
+
+function IdlePromise(window) {
+  return new Promise(resolve => {
+    Services.tm.idleDispatchToMainThread(() => {
+      window.requestAnimationFrame(resolve);
+    });
   });
 }
