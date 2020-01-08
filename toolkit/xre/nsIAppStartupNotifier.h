a3e0bece31fcb5319c3b4a05d545a307afc7a7ce
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 11:00:21 +0000

    Bug 1491741 - Part 1: Remove the XPCOM registration for nsAppStartupNotifier; r=mccr8
    Differential Revision: https://phabricator.services.mozilla.com/D5994
    
    UltraBlame original commit: f60256376b971f045d37953ddf77311ca1e6119f

--- toolkit/xre/nsIAppStartupNotifier.h
+++ toolkit/xre/nsIAppStartupNotifier.h
@@ -42,8 +42,6 @@
 
 
 
-#define NS_APPSTARTUPNOTIFIER_CONTRACTID "@mozilla.org/embedcomp/appstartup-notifier;1"
-
 #define APPSTARTUP_CATEGORY "app-startup"
 #define APPSTARTUP_TOPIC    "app-startup"
 
