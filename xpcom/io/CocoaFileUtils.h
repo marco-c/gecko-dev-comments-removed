83d2003ff5b0ebb6f97b3aefcb768a5ad8a05920
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 07:48:25 +0000

    Bug 1355346 - Call nsCocoaFileUtils::AddOriginMetadataToFile off of the main-thread when completing downloads. r=mstange
    Tags: #secure-revision
    
    Bug #: 1355346
    
    Differential Revision: https://phabricator.services.mozilla.com/D3674
    
    UltraBlame original commit: 566a5b21073a2899b9ee539236ac057d3b81b382

--- xpcom/io/CocoaFileUtils.h
+++ xpcom/io/CocoaFileUtils.h
@@ -22,16 +22,21 @@ nsresult GetFileCreatorCode(CFURLRef aUrl, OSType* aCreatorCode);
 nsresult SetFileCreatorCode(CFURLRef aUrl, OSType aCreatorCode);
 nsresult GetFileTypeCode(CFURLRef aUrl, OSType* aTypeCode);
 nsresult SetFileTypeCode(CFURLRef aUrl, OSType aTypeCode);
+
+
 void     AddOriginMetadataToFile(const CFStringRef filePath,
                                  const CFURLRef sourceURL,
                                  const CFURLRef referrerURL);
+
 void     AddQuarantineMetadataToFile(const CFStringRef filePath,
                                      const CFURLRef sourceURL,
                                      const CFURLRef referrerURL,
                                      const bool isFromWeb,
                                      const bool createProps=false);
+
 void CopyQuarantineReferrerUrl(const CFStringRef aFilePath,
                                nsAString& aReferrer);
+
 CFURLRef GetTemporaryFolderCFURLRef();
 
 } 
