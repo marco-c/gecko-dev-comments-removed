d89bb008322096995b17fce655966a89f6605411
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 23:36:05 +0000

    Bug 1482389, remove tree box object special case in Codegen.py, r=bz
    UltraBlame original commit: a4b83cd280350877dd748848d41e4671f3970a3b

--- dom/bindings/Codegen.py
+++ dom/bindings/Codegen.py
@@ -7404,11 +7404,8 @@ class CGCallGenerator(CGThing):
         if not static:
             call = CGWrapper(call, pre="%s->" % object)
         call = CGList([call, CGWrapper(args, pre="(", post=")")])
-        if ((returnType is None or returnType.isVoid() or
-             resultOutParam is not None) and
-            
-            
-            descriptor.name != "TreeBoxObject"):
+        if (returnType is None or returnType.isVoid() or
+            resultOutParam is not None):
             assert resultConversion is None
             call = CGList([
                 CGWrapper(
