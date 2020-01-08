c5274a314b95459b48f7faed9832a9f570f1a7a6
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 08:14:38 +0000

    Bug 1483030 - Implement basic ARM64 codegen. r=djvj
    UltraBlame original commit: d81eb0bdb46afb881262bf2567d9604fe57bd762

--- js/src/jit/arm64/Lowering-arm64.cpp
+++ js/src/jit/arm64/Lowering-arm64.cpp
@@ -82,7 +82,10 @@ LIRGenerator::visitUnbox(MUnbox* unbox)
         
         lir = new(alloc()) LUnbox(useRegisterAtStart(box));
     } else {
-        lir = new(alloc()) LUnbox(useAtStart(box));
+        
+        
+        
+        lir = new(alloc()) LUnbox(useRegisterAtStart(box));
     }
 
     if (unbox->fallible())
@@ -178,21 +181,23 @@ LIRGeneratorARM64::lowerForBitAndAndBranch(LBitAndAndBranch* baab, MInstruction*
 void
 LIRGeneratorARM64::defineUntypedPhi(MPhi* phi, size_t lirIndex)
 {
-    MOZ_CRASH("defineUntypedPhi");
+    defineTypedPhi(phi, lirIndex);
 }
 
 void
 LIRGeneratorARM64::lowerUntypedPhiInput(MPhi* phi, uint32_t inputPosition,
                                         LBlock* block, size_t lirIndex)
 {
-    MOZ_CRASH("lowerUntypedPhiInput");
+    lowerTypedPhiInput(phi, inputPosition, block, lirIndex);
 }
 
 void
 LIRGeneratorARM64::lowerForShift(LInstructionHelper<1, 2, 0>* ins,
                                  MDefinition* mir, MDefinition* lhs, MDefinition* rhs)
 {
-    MOZ_CRASH("lowerForShift");
+    ins->setOperand(0, useRegister(lhs));
+    ins->setOperand(1, useRegisterOrConstant(rhs));
+    define(ins, mir);
 }
 
 void
