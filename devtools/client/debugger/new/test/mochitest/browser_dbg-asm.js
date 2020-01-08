edfdee3d42d4947e7030e718f22546fabdf91e13
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 13:04:32 +0000

    Revert "Backed out changeset 8566206da1bf (bug 1496401) for devtools failures at test/mochitest/browser_jsterm_await.js"
    This reverts commit c639cc0b0a613949bd3e9c275c35e6adf5a8c38f.
    
    UltraBlame original commit: c161940934133ef7899b83cf273b17e6ac6ff572

--- devtools/client/debugger/new/test/mochitest/browser_dbg-asm.js
+++ devtools/client/debugger/new/test/mochitest/browser_dbg-asm.js
@@ -9,6 +9,7 @@ add_task(async function() {
 
   await waitForSources(dbg, "doc-asm.html", "asm.js");
 
+  
   is(findAllElements(dbg, "sourceNodes").length, 4);
 
   await selectSource(dbg, "asm.js");
