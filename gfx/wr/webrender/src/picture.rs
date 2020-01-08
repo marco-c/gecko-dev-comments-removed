7f70745cdf5ef94aa118832295bb8a0fedead696
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 23:39:41 +0000

    Bug 1505934 - WR: Don't establish a raster root in pictures with sufficiently large local bounds (take 2) r=jrmuizel
    re-open of D16335, which got backed out due to Wrench test failing.
    The test was failing because of different AA on a plane-splitting case, which isn't guaranteed anyway.
    This revision updates the test.
    
    Differential Revision: https://phabricator.services.mozilla.com/D16560
    
    UltraBlame original commit: 97ae7728e2432a8cbd9c682b6eebf689ae69bdcc

--- gfx/wr/webrender/src/picture.rs
+++ gfx/wr/webrender/src/picture.rs
@@ -86,6 +86,17 @@ pub const TILE_SIZE_WIDTH: i32 = 1024;
 pub const TILE_SIZE_HEIGHT: i32 = 256;
 const FRAMES_BEFORE_CACHING: usize = 2;
 
+
+
+
+
+
+const MAX_CACHE_SIZE: f32 = 2048.0;
+
+
+const MAX_SURFACE_SIZE: f32 = 4096.0;
+
+
 #[derive(Debug)]
 pub struct GlobalTransformInfo {
     
@@ -1278,6 +1289,11 @@ impl SurfaceInfo {
         }
     }
 
+    pub fn fits_surface_size_limits(&self) -> bool {
+        self.map_local_to_surface.bounds.size.width <= MAX_SURFACE_SIZE &&
+        self.map_local_to_surface.bounds.size.height <= MAX_SURFACE_SIZE
+    }
+
     
     
     pub fn take_render_tasks(&mut self) -> Vec<RenderTaskId> {
@@ -1821,6 +1837,7 @@ impl PicturePrimitive {
         };
 
         let state = PictureState {
+            
             is_cacheable: true,
             map_local_to_pic,
             map_pic_to_world,
@@ -2022,35 +2039,9 @@ impl PicturePrimitive {
 
             
             
-            let xf = frame_context.clip_scroll_tree.get_relative_transform(
-                parent_raster_spatial_node_index,
-                surface_spatial_node_index,
-            ).expect("BUG: unable to get relative transform");
-
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            let establishes_raster_root = xf.has_perspective_component();
-
-            
-            
             
             
             let raster_space = RasterSpace::Screen;
-
-            let raster_spatial_node_index = if establishes_raster_root {
-                surface_spatial_node_index
-            } else {
-                parent_raster_spatial_node_index
-            };
 
             let inflation_factor = match composite_mode {
                 PictureCompositeMode::Filter(FilterOp::Blur(blur_radius)) => {
@@ -2063,32 +2054,58 @@ impl PicturePrimitive {
                 }
             };
 
-            let surface_index = state.push_surface(
+            let mut surface = {
+                
+                
+                let xf = frame_context.clip_scroll_tree.get_relative_transform(
+                    parent_raster_spatial_node_index,
+                    surface_spatial_node_index,
+                ).expect("BUG: unable to get relative transform");
+
+                let establishes_raster_root = xf.has_perspective_component();
+
                 SurfaceInfo::new(
                     surface_spatial_node_index,
-                    raster_spatial_node_index,
+                    if establishes_raster_root {
+                        surface_spatial_node_index
+                    } else {
+                        parent_raster_spatial_node_index
+                    },
                     inflation_factor,
                     frame_context.screen_world_rect,
                     &frame_context.clip_scroll_tree,
                 )
-            );
+            };
 
-            self.raster_config = Some(RasterConfig {
-                composite_mode,
-                surface_index,
-                establishes_raster_root,
-            });
+            if surface_spatial_node_index != parent_raster_spatial_node_index &&
+                !surface.fits_surface_size_limits()
+            {
+                
+                surface = SurfaceInfo::new(
+                    surface_spatial_node_index,
+                    parent_raster_spatial_node_index,
+                    inflation_factor,
+                    frame_context.screen_world_rect,
+                    &frame_context.clip_scroll_tree,
+                );
+            };
 
             
             
             if let Some(ref mut surface_desc) = self.surface_desc {
                 surface_desc.update(
                     surface_spatial_node_index,
-                    raster_spatial_node_index,
+                    surface.raster_spatial_node_index,
                     frame_context.clip_scroll_tree,
                     raster_space,
                 );
             }
+
+            self.raster_config = Some(RasterConfig {
+                composite_mode,
+                establishes_raster_root: surface_spatial_node_index == surface.raster_spatial_node_index,
+                surface_index: state.push_surface(surface),
+            });
         }
 
         Some(mem::replace(&mut self.prim_list.pictures, SmallVec::new()))
@@ -2154,7 +2171,7 @@ impl PicturePrimitive {
                 let map_local_to_containing_block: SpaceMapper<LayoutPixel, LayoutPixel> = SpaceMapper::new_with_target(
                     containing_block_index,
                     cluster.spatial_node_index,
-                    LayoutRect::zero(),     
+                    LayoutRect::zero(), 
                     &frame_context.clip_scroll_tree,
                 );
 
@@ -2341,10 +2358,6 @@ impl PicturePrimitive {
                 
                 
 
-                
-                
-                
-                const MAX_CACHE_SIZE: f32 = 2048.0;
                 let too_big_to_cache = unclipped.size.width > MAX_CACHE_SIZE ||
                                        unclipped.size.height > MAX_CACHE_SIZE;
 
