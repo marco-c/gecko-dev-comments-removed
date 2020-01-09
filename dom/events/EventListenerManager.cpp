0396d844fa4375ac84c7b81218424e1757f63f5f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 09:38:34 +0000

    Bug 1524130 - Remove deprecation of attr modified event. r=smaug
    It was added in bug 1461696 probably for tracking whether bug 1460295
    is a wide-spread issue or so. This deprecation was already part of
    deprecated mutation event, so it's never output to console. It was
    added purely for getting telemetry.
    
    Given that related issues are now fixed, we no longer needs this.
    
    Differential Revision: https://phabricator.services.mozilla.com/D18935
    
    UltraBlame original commit: 904d06bacb8f5a5593165fd10bfca302a4059db4

--- dom/events/EventListenerManager.cpp
+++ dom/events/EventListenerManager.cpp
@@ -287,9 +287,6 @@ void EventListenerManager::AddEventListenerInternal(
       nsCOMPtr<Document> doc = window->GetExtantDoc();
       if (doc) {
         doc->WarnOnceAbout(Document::eMutationEvent);
-        if (aEventMessage == eLegacyAttrModified) {
-          doc->WarnOnceAbout(Document::eDOMAttrModifiedEvent);
-        }
       }
       
       
