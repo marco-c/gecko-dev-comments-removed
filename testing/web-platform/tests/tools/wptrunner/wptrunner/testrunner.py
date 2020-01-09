8ed5c783cf92c02b01a1460b7e971a59cb56527c
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 10:03:56 +0000

    Bug 1511764 - Bail out of startup if we are unable to connect to wpt, r=whimboo
    The previous setup was broken because subclasses could run code after the cinnection failed assuming
    that the connection had succeeded. Unwinding the stack and sending the failure messages at the
    top level avoid this pitfall.
    
    Depends on D19487
    
    Differential Revision: https://phabricator.services.mozilla.com/D19488
    
    UltraBlame original commit: 4cf8e31cb26140e621218bf5f6c43afad73a4c3d

--- testing/web-platform/tests/tools/wptrunner/wptrunner/testrunner.py
+++ testing/web-platform/tests/tools/wptrunner/wptrunner/testrunner.py
@@ -70,7 +70,13 @@ class TestRunner(object):
 
     def setup(self):
         self.logger.debug("Executor setup")
-        self.executor.setup(self)
+        try:
+            self.executor.setup(self)
+        except Exception:
+            
+            self.send_message("init_failed")
+        else:
+            self.send_message("init_succeeded")
         self.logger.debug("Executor setup done")
 
     def teardown(self):
