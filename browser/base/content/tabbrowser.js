e752d4252c1ad219550d1b16506f68a603967c9a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 13:33:53 +0000

    Backed out changeset 51ffa59f1488 (bug 1521923) for WR failures on outline-013.html . CLOSED TREE
    UltraBlame original commit: 6b8e4909d303024cef8f404fc1d2ae4f3ecce055

--- browser/base/content/tabbrowser.js
+++ browser/base/content/tabbrowser.js
@@ -3705,6 +3705,13 @@ window._gBrowser = {
 
     
     
+    
+    let activeTab = gBrowser.selectedTab;
+    let inactiveTabs = tabs.filter(t => t != activeTab);
+    let activeTabNewIndex = tabs.indexOf(activeTab);
+
+    
+    
     if (this.animationsEnabled) {
       for (let tab of tabs) {
         tab.style.maxWidth = ""; 
@@ -3712,28 +3719,33 @@ window._gBrowser = {
       }
     }
 
-    
-    
-    
-    
-    
-    
-    let selectedTabIndex = Math.max(0, tabs.indexOf(gBrowser.selectedTab));
-    let selectedTab = tabs[selectedTabIndex];
-    let win = this.replaceTabWithWindow(selectedTab, aOptions);
-    win.addEventListener("before-initial-tab-adopted", () => {
-      for (let i = 0; i < tabs.length; ++i) {
-        if (i != selectedTabIndex) {
-          win.gBrowser.adoptTab(tabs[i], i);
-        }
+    let win;
+    let firstInactiveTab = inactiveTabs[0];
+
+    let adoptRemainingTabs = () => {
+      for (let i = 1; i < inactiveTabs.length; i++) {
+        win.gBrowser.adoptTab(inactiveTabs[i], i);
       }
+
+      if (activeTabNewIndex > -1) {
+        win.gBrowser.adoptTab(activeTab, activeTabNewIndex, true );
+      }
+
       
       let winVisibleTabs = win.gBrowser.visibleTabs;
       let winTabLength = winVisibleTabs.length;
       win.gBrowser.addRangeToMultiSelectedTabs(winVisibleTabs[0],
                                                winVisibleTabs[winTabLength - 1]);
-      win.gBrowser.lockClearMultiSelectionOnce();
-    }, {once: true});
+    };
+
+    
+    if (firstInactiveTab.hasAttribute("pending")) {
+      firstInactiveTab.addEventListener("TabClose", adoptRemainingTabs, {once: true});
+    } else {
+      firstInactiveTab.linkedBrowser.addEventListener("EndSwapDocShells", adoptRemainingTabs, {once: true});
+    }
+
+    win = this.replaceTabWithWindow(firstInactiveTab, aOptions);
     return win;
   },
 
