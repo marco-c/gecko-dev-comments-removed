aa234b86419a7116ff63d2f9307b1d6384f8ad80
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 20:29:26 +0000

    Bug 1533562 - Implement titlebar gradient drawing with a new TitlebarGradientView. r=spohl
    Differential Revision: https://phabricator.services.mozilla.com/D22646
    
    UltraBlame original commit: 382967c743c8bdbcb04f5bf369fee5a6f818ea6e

--- widget/cocoa/nsCocoaWindow.h
+++ widget/cocoa/nsCocoaWindow.h
@@ -162,10 +162,17 @@ typedef struct _nsCocoaWindowList {
 - (void)sendToplevelDeactivateEvents;
 @end
 
-@class ToolbarWindow;
+@interface TitlebarGradientView : NSView
+@end
 
 
 @interface ToolbarWindow : BaseWindow {
+  
+  
+  
+  
+  TitlebarGradientView* mTitlebarGradientView;  
+
   CGFloat mUnifiedToolbarHeight;
   CGFloat mSheetAttachmentPosition;
   NSRect mWindowButtonsRect;
@@ -183,6 +190,7 @@ typedef struct _nsCocoaWindowList {
 - (void)placeFullScreenButton:(NSRect)aRect;
 - (NSPoint)windowButtonsPositionWithDefaultPosition:(NSPoint)aDefaultPosition;
 - (NSPoint)fullScreenButtonPositionWithDefaultPosition:(NSPoint)aDefaultPosition;
+- (void)windowMainStateChanged;
 @end
 
 class nsCocoaWindow final : public nsBaseWidget, public nsPIWidgetCocoa {
