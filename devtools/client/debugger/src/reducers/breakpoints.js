00d646328b3c06c2812ab95fcc516523c1b759ca
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 19:12:09 +0000

    Bug 1541629 - Part 3: Adopt new SourceActor types/actions throughout codebase. r=jlast
    Differential Revision: https://phabricator.services.mozilla.com/D27958
    
    UltraBlame original commit: 9fe6de0bfd86fa9683cac4d76c5df5730cf25769

--- devtools/client/debugger/src/reducers/breakpoints.js
+++ devtools/client/debugger/src/reducers/breakpoints.js
@@ -14,7 +14,6 @@ import { isEqual } from "lodash";
 
 import { makeBreakpointId, findPosition } from "../utils/breakpoint";
 import { findEmptyLines } from "../utils/empty-lines";
-import { isInlineScript } from "../utils/source";
 
 
 import { getBreakpointsList as getBreakpointsListSelector } from "../selectors/breakpoints";
@@ -107,18 +106,20 @@ function update(
       };
     }
 
-    case "ADD_SOURCES": {
-      const { sources } = action;
+    case "INSERT_SOURCE_ACTORS": {
+      const { items } = action;
 
-      const scriptSources = sources.filter(source => isInlineScript(source));
+      const scriptActors = items.filter(
+        item => item.introductionType === "scriptElement"
+      );
 
-      if (scriptSources.length > 0) {
+      if (scriptActors.length > 0) {
         const { ...breakpointPositions } = state.breakpointPositions;
 
         
         
-        for (const source of scriptSources) {
-          delete breakpointPositions[source.id];
+        for (const { source } of scriptActors) {
+          delete breakpointPositions[source];
         }
 
         state = { ...state, breakpointPositions };
