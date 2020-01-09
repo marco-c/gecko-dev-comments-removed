ae39de5cd47632f10de9717f48ea9d772e9b597f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 00:02:40 +0000

    Backed out changeset 92c56dcf69b8 (bug 1512949) for mochitest failures setup | Login to delete is defined: login0 - false == true - got false, expected true (operator ==) CLOSED TREE
    UltraBlame original commit: 800b3eb8a6954c2c874d71be356a337494e3babc

--- devtools/server/tests/unit/test_objectgrips-21.js
+++ devtools/server/tests/unit/test_objectgrips-21.js
@@ -4,12 +4,6 @@
 
 "use strict";
 
-Services.prefs.setBoolPref("security.allow_eval_with_system_principal", true);
-
-registerCleanupFunction(() => {
-  Services.prefs.clearUserPref("security.allow_eval_with_system_principal");
-});
-
 
 
 
