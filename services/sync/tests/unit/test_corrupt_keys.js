1e5785abbd08278fa046a2b1e421417529f22ff4
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 22:33:23 +0000

    Bug 1550473 - Remove ajv-4.1.1.js from eval()-whitelist. r=ckerschb
    Allow eval() by flipping pref in all affected test files.
    
    Differential Revision: https://phabricator.services.mozilla.com/D31771
    
    UltraBlame original commit: 4f2475241df7d23e70c60b427b935e55b1e50df2

--- services/sync/tests/unit/test_corrupt_keys.js
+++ services/sync/tests/unit/test_corrupt_keys.js
@@ -6,6 +6,12 @@ const {HistoryEngine} = ChromeUtils.import("resource://services-sync/engines/his
 const {CryptoWrapper, WBORecord} = ChromeUtils.import("resource://services-sync/record.js");
 const {Service} = ChromeUtils.import("resource://services-sync/service.js");
 
+
+Services.prefs.setBoolPref("security.allow_eval_with_system_principal", true);
+registerCleanupFunction(() => {
+  Services.prefs.clearUserPref("security.allow_eval_with_system_principal");
+});
+
 add_task(async function test_locally_changed_keys() {
   enableValidationPrefs();
 
