261a58499a9b39ecb40c0b2fd0232a6fc8acffd4
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 23:13:06 +0000

    Backed out 4 changesets (bug 1553482) for build bustages in BinASTTokenReaderBase.h CLOSED TREE
    Backed out changeset 49278023aa7d (bug 1553482)
    Backed out changeset 57a5c8689ed3 (bug 1553482)
    Backed out changeset c50879a48ff8 (bug 1553482)
    Backed out changeset 5570e3e6c5e5 (bug 1553482)
    
    UltraBlame original commit: cbb2af10089cfcdd849726f14b84ca5754f9b9fb

--- js/src/frontend/BinASTParserPerTokenizer.cpp
+++ js/src/frontend/BinASTParserPerTokenizer.cpp
@@ -133,7 +133,7 @@ JS::Result<ParseNode*> BinASTParserPerTokenizer<Tok>::parseAux(
   MOZ_TRY(tokenizer_->readHeader());
 
   ParseNode* result(nullptr);
-  const Context topContext((RootContext()));
+  const Context topContext(Context::topLevel());
   MOZ_TRY_VAR(result, asFinalParser()->parseProgram(topContext));
 
   mozilla::Maybe<GlobalScope::Data*> bindings =
@@ -196,7 +196,7 @@ JS::Result<FunctionNode*> BinASTParserPerTokenizer<Tok>::parseLazyFunction(
 
   
   
-  const Context context((RootContext()));
+  const Context context(Context::topLevel());
   MOZ_TRY(
       (asFinalParser()->*parseFunc)(func->nargs(), &params, &tmpBody, context));
 
