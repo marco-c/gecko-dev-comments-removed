6bb079d728f20a3b224bb69545b25fb8e3e8f8df
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 12:13:08 +0000

    Bug 1511560 - Move syscalls for adjusting memory mapping properties into SandboxPolicyCommon. r=gcp
    madvise is used by our malloc (and probably others), and mprotect is
    used with shared memory, including when created by another process, so
    the common policy should include those rules.
    
    Depends on D14521
    
    Differential Revision: https://phabricator.services.mozilla.com/D14522
    
    UltraBlame original commit: bab79f85596242146787d6d2a5ad56596cc1343e

--- security/sandbox/linux/SandboxFilter.cpp
+++ security/sandbox/linux/SandboxFilter.cpp
@@ -448,6 +448,23 @@ class SandboxPolicyCommon : public SandboxPolicyBase {
         return Allow();
 
         
+      case __NR_mprotect:
+        return Allow();
+
+        
+      case __NR_madvise: {
+        Arg<int> advice(2);
+        return If(advice == MADV_DONTNEED, Allow())
+            .ElseIf(advice == MADV_FREE, Allow())
+            .ElseIf(advice == MADV_HUGEPAGE, Allow())
+            .ElseIf(advice == MADV_NOHUGEPAGE, Allow())
+#ifdef MOZ_ASAN
+            .ElseIf(advice == MADV_DONTDUMP, Allow())
+#endif
+            .Else(InvalidSyscall());
+      }
+
+      
 #if defined(ANDROID) || defined(MOZ_ASAN)
       case __NR_sigaltstack:
 #endif
@@ -1052,8 +1069,9 @@ class ContentSandboxPolicy : public SandboxPolicyCommon {
             .Default(SandboxPolicyCommon::EvaluateSyscall(sysno));
       }
 
-      case __NR_mprotect:
       case __NR_brk:
+        
+        
       case __NR_madvise:
         
         
@@ -1335,20 +1353,6 @@ class GMPSandboxPolicy : public SandboxPolicyCommon {
       case __NR_openat:
         return Trap(OpenTrap, mFiles);
 
-        
-      case __NR_mprotect:
-        return Allow();
-      case __NR_madvise: {
-        Arg<int> advice(2);
-        return If(advice == MADV_DONTNEED, Allow())
-            .ElseIf(advice == MADV_FREE, Allow())
-            .ElseIf(advice == MADV_HUGEPAGE, Allow())
-            .ElseIf(advice == MADV_NOHUGEPAGE, Allow())
-#  ifdef MOZ_ASAN
-            .ElseIf(advice == MADV_DONTDUMP, Allow())
-#  endif
-            .Else(InvalidSyscall());
-      }
       case __NR_brk:
       CASES_FOR_geteuid:
         return Allow();
