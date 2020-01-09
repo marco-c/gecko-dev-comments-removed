33055e3683f0dc744294849519d30d474ef05a59
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 20:16:32 +0000

    Bug 1541339 - WebSocket channel should use the correct CookieSettings in workers, r=Ehsan
    Differential Revision: https://phabricator.services.mozilla.com/D28949
    
    UltraBlame original commit: daeba572395db4aa51076a85d1b908405152d50a

--- netwerk/test/unit/test_websocket_offline.js
+++ netwerk/test/unit/test_websocket_offline.js
@@ -36,6 +36,7 @@ function run_test() {
     chan.initLoadInfo(null, 
                       Services.scriptSecurityManager.getSystemPrincipal(),
                       null, 
+                      null, 
                       Ci.nsILoadInfo.SEC_ALLOW_CROSS_ORIGIN_DATA_IS_NULL,
                       Ci.nsIContentPolicy.TYPE_WEBSOCKET);
 
