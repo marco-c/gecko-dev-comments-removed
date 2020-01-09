e9c8cf5872a0f2dfba403d661c36972de439bd2f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 17:41:14 +0000

    Bug 1541352 - Debounce autocompletion action in JSTerm. r=ochameau.
    Debouncing the autocomplete action allow us to not do too many
    requests as the user is typing, which should make typing in the
    console faster, especially on slow machine or on debug builds.
    
    The tests shouldn't be impacted as we listen for a specific event
    in them.
    
    Differential Revision: https://phabricator.services.mozilla.com/D25604
    
    UltraBlame original commit: 2e8a7fee489774e1a4f3b1feb5801c232396f6be

--- devtools/client/webconsole/components/JSTerm.js
+++ devtools/client/webconsole/components/JSTerm.js
@@ -6,6 +6,7 @@
 
 const {Utils: WebConsoleUtils} = require("devtools/client/webconsole/utils");
 const Services = require("Services");
+const { debounce } = require("devtools/shared/debounce");
 
 loader.lazyServiceGetter(this, "clipboardHelper",
                          "@mozilla.org/widget/clipboardhelper;1",
@@ -77,6 +78,7 @@ class JSTerm extends Component {
       updateHistoryPosition: PropTypes.func.isRequired,
       
       autocompleteUpdate: PropTypes.func.isRequired,
+      autocompleteClear: PropTypes.func.isRequired,
       
       autocompleteData: PropTypes.object.isRequired,
       
@@ -100,6 +102,11 @@ class JSTerm extends Component {
     this._blurEventHandler = this._blurEventHandler.bind(this);
     this.onContextMenu = this.onContextMenu.bind(this);
     this.imperativeUpdate = this.imperativeUpdate.bind(this);
+
+    
+    
+    
+    this.autocompleteUpdate = debounce(this.props.autocompleteUpdate, 75, this);
 
     
 
@@ -808,7 +815,7 @@ class JSTerm extends Component {
     if (this.lastInputValue !== value) {
       this.resizeInput();
       if (this.props.autocomplete) {
-        this.props.autocompleteUpdate();
+        this.autocompleteUpdate();
       }
       this.lastInputValue = value;
     }
@@ -1347,7 +1354,7 @@ class JSTerm extends Component {
       }
     }
 
-    this.clearCompletion();
+    this.props.autocompleteClear();
 
     if (completionText) {
       this.insertStringAtCursor(completionText, numberOfCharsToReplaceCharsBeforeCursor);
@@ -1630,7 +1637,7 @@ function mapDispatchToProps(dispatch) {
     updateHistoryPosition: (direction, expression) =>
       dispatch(historyActions.updateHistoryPosition(direction, expression)),
     autocompleteUpdate: force => dispatch(autocompleteActions.autocompleteUpdate(force)),
-    autocompleteBailOut: () => dispatch(autocompleteActions.autocompleteBailOut()),
+    autocompleteClear: () => dispatch(autocompleteActions.autocompleteClear()),
   };
 }
 
