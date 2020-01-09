12bdd63bdbeb931684a6a8d9af54cc1f29df1bfd
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 22:49:34 +0000

    Bug 1553058 - Remove unused gPageSize case. r=njn
    MALLOC_STATIC_PAGESIZE is only set on some platforms. Specifically, it's
    not set on ia64 and sparc. Which means the case MALLOC_STATIC_PAGESIZE
    && (sparc || ia64) never happens, and gPageSize is never 8 KiB.
    
    Differential Revision: https://phabricator.services.mozilla.com/D31965
    
    UltraBlame original commit: 1243ae6eed2811af4bf41233d6eec744ea7b557f

--- memory/build/mozjemalloc.cpp
+++ memory/build/mozjemalloc.cpp
@@ -420,10 +420,7 @@ static const size_t kChunkSizeMask = kChunkSize - 1;
 
 
 
-#  if (defined(SOLARIS) || defined(__FreeBSD__)) && \
-      (defined(__sparc) || defined(__sparcv9) || defined(__ia64))
-static const size_t gPageSize = 8_KiB;
-#  elif defined(__powerpc64__)
+#  if defined(__powerpc64__)
 static const size_t gPageSize = 64_KiB;
 #  else
 static const size_t gPageSize = 4_KiB;
