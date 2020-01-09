0c0a30a5df4c0cd69f322e20cd38fa9f409190d7
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 13:41:18 +0000

    Bug 1532397 - Fix multiple highlight issues in debugger r=jlast
    Replaces previous commit which was backed out for flow issue
    
    Differential Revision: https://phabricator.services.mozilla.com/D22142
    
    UltraBlame original commit: 6fa85e4e05636f782b5bcc935e0e87dcd60af1ba

--- devtools/client/debugger/new/src/components/Editor/Preview/index.js
+++ devtools/client/debugger/new/src/components/Editor/Preview/index.js
@@ -50,10 +50,13 @@ function inPopup(e) {
 
 function getElementFromPos(pos: DOMRect) {
   
-  return document.elementFromPoint(
-    pos.x + pos.width / 2,
-    pos.y + pos.height / 2
-  );
+  
+  const elementsAtPoint = [
+    
+    ...document.elementsFromPoint(pos.x + pos.width / 2, pos.y + pos.height / 2)
+  ];
+
+  return elementsAtPoint.find(el => el.className.startsWith("cm-"));
 }
 
 class Preview extends PureComponent<Props, State> {
