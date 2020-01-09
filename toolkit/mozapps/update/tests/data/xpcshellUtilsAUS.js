e72a73c6e74aa1f838c1231b53b153e25ef66000
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 01:23:38 +0000

    Bug 1279108 - Remove code that disables asan leak detection in app update xpcshell tests. r=mhowell
    The leaks were fixed in Bug 1168010
    
    Differential Revision: https://phabricator.services.mozilla.com/D18155
    
    UltraBlame original commit: fde25393009b2b798a211a00f2e9af6f8b48cacd

--- toolkit/mozapps/update/tests/data/xpcshellUtilsAUS.js
+++ toolkit/mozapps/update/tests/data/xpcshellUtilsAUS.js
@@ -180,7 +180,6 @@ var gEnvXPCOMDebugBreak;
 var gEnvXPCOMMemLeakLog;
 var gEnvDyldLibraryPath;
 var gEnvLdLibraryPath;
-var gASanOptions;
 
 
 
@@ -4264,14 +4263,6 @@ function setEnvironment() {
 
   gShouldResetEnv = true;
 
-  
-  if (gEnv.exists("ASAN_OPTIONS")) {
-    gASanOptions = gEnv.get("ASAN_OPTIONS");
-    gEnv.set("ASAN_OPTIONS", gASanOptions + ":detect_leaks=0");
-  } else {
-    gEnv.set("ASAN_OPTIONS", "detect_leaks=0");
-  }
-
   if (IS_WIN && !gEnv.exists("XRE_NO_WINDOWS_CRASH_DIALOG")) {
     gAddedEnvXRENoWindowsCrashDialog = true;
     debugDump("setting the XRE_NO_WINDOWS_CRASH_DIALOG environment " +
@@ -4352,9 +4343,6 @@ function resetEnvironment() {
   }
 
   gShouldResetEnv = false;
-
-  
-  gEnv.set("ASAN_OPTIONS", gASanOptions ? gASanOptions : "");
 
   if (gEnvXPCOMMemLeakLog) {
     debugDump("setting the XPCOM_MEM_LEAK_LOG environment variable back to " +
