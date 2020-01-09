7df954a99e7e0bc3a7ddffb91846ed2dc4dc72b4
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 21:51:34 +0000

    Bug 1524539 - Finish porting browser_autocomplete_a11y_label.js to QuantumBar. r=Jamie
    Differential Revision: https://phabricator.services.mozilla.com/D29811
    
    UltraBlame original commit: 37b9048629cfd3197b289f9c5d396116b6a0eb1f

--- browser/components/urlbar/tests/browser/browser_autocomplete_a11y_label.js
+++ browser/components/urlbar/tests/browser/browser_autocomplete_a11y_label.js
@@ -9,6 +9,43 @@ const SUGGEST_ALL_PREF = "browser.search.suggest.enabled";
 const SUGGEST_URLBAR_PREF = "browser.urlbar.suggest.searches";
 const TEST_ENGINE_BASENAME = "searchSuggestionEngine.xml";
 
+async function getResultText(element) {
+  await initAccessibilityService();
+  await BrowserTestUtils.waitForCondition(() => accService.getAccessibleFor(element));
+  let accessible = accService.getAccessibleFor(element);
+  return accessible.name;
+}
+
+let accService;
+async function initAccessibilityService() {
+  if (accService) {
+    return;
+  }
+  accService = Cc["@mozilla.org/accessibilityService;1"].getService(
+    Ci.nsIAccessibilityService);
+  if (Services.appinfo.accessibilityEnabled) {
+    return;
+  }
+
+  async function promiseInitOrShutdown(init = true) {
+    await new Promise(resolve => {
+      let observe = (subject, topic, data) => {
+        Services.obs.removeObserver(observe, "a11y-init-or-shutdown");
+        
+        if (data === (init ? "1" : "0")) {
+          resolve();
+        }
+      };
+      Services.obs.addObserver(observe, "a11y-init-or-shutdown");
+    });
+  }
+  await promiseInitOrShutdown(true);
+  registerCleanupFunction(async () => {
+    accService = null;
+    await promiseInitOrShutdown(false);
+  });
+}
+
 add_task(async function switchToTab() {
   let tab = await BrowserTestUtils.openNewForegroundTab(gBrowser, "about:about");
 
@@ -17,11 +54,15 @@ add_task(async function switchToTab() {
   Assert.equal(result.type, UrlbarUtils.RESULT_TYPE.TAB_SWITCH,
     "Should have a switch tab result");
 
-  
-  
-  
   let element = await UrlbarTestUtils.waitForAutocompleteResultAt(window, 1);
-  is(element.label, "about:about about:about Tab", "Result a11y label should be: <title> <url> Tab");
+  is(await getResultText(element),
+     UrlbarPrefs.get("quantumbar") ?
+       
+       "about : about— Switch to Tab" :
+       "about:about about:about Tab",
+     UrlbarPrefs.get("quantumbar") ?
+       "Result a11y label should be: <title>— Switch to Tab" :
+       "Result a11y label should be: <title> <url> Tab");
 
   await UrlbarTestUtils.promisePopupClose(window);
   gBrowser.removeTab(tab);
@@ -52,7 +93,8 @@ add_task(async function searchSuggestions() {
   let expectedSearches = [
     "foo",
     "foofoo",
-    "foobar",
+    
+    UrlbarPrefs.get("quantumbar") ? "foo bar " : "foobar",
   ];
   for (let i = 0; i < length; i++) {
     let result = await UrlbarTestUtils.getDetailsOfResultAt(window, i);
@@ -60,13 +102,22 @@ add_task(async function searchSuggestions() {
       Assert.greaterOrEqual(expectedSearches.length, 0,
         "Should still have expected searches remaining");
       let suggestion = expectedSearches.shift();
-      
-      
-      
       let element = await UrlbarTestUtils.waitForAutocompleteResultAt(window, i);
-      Assert.equal(element.label,
-        suggestion + " browser_searchSuggestionEngine searchSuggestionEngine.xml Search",
-        "Result label should be: <search term> <engine name> Search");
+      let selected = element.hasAttribute("selected");
+      if (!selected) {
+        
+        element.toggleAttribute("selected", true);
+      }
+      Assert.equal(await getResultText(element),
+        UrlbarPrefs.get("quantumbar") ?
+          suggestion + "— Search with browser_searchSuggestionEngine searchSuggestionEngine.xml" :
+          suggestion + " browser_searchSuggestionEngine searchSuggestionEngine.xml Search",
+        UrlbarPrefs.get("quantumbar") ?
+          "Result label should be: <search term>— Search with <engine name>" :
+          "Result label should be: <search term> <engine name> Search");
+      if (!selected) {
+        element.toggleAttribute("selected", false);
+      }
     }
   }
   Assert.ok(expectedSearches.length == 0);
