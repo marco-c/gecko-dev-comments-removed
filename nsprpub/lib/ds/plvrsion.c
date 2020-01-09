297574d7a5c3caf5f349ff324a98016c3edf004a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 09:47:15 +0000

    Backed out 2 changesets (bug 1526010, bug 1520646) mass mochitest failures UPGRADE_NSPR_RELEASE CLOSED TREE
    Backed out changeset 5b4cd1f37414 (bug 1520646)
    Backed out changeset c7041aff5ad7 (bug 1526010)
    
    UltraBlame original commit: 42763a2bd75901be5380580a9817c06a4c9a8a2a

--- nsprpub/lib/ds/plvrsion.c
+++ nsprpub/lib/ds/plvrsion.c
@@ -54,7 +54,7 @@ PRVersionDescription VERSION_DESC_NAME =
       _PRODUCTION,        
      "Portable runtime",  
      "N/A",               
-     "This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.",
+      "Copyright (c) 1998 Netscape Communications Corporation. All Rights Reserved",
       "http://www.mozilla.org/MPL/",
      ""
 };
