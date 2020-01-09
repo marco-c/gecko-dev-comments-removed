a40ff5e35e3be27dd415f55b936942fda835a73a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 19:48:33 +0000

    Bug 1526086: Fix possible shutdown deadlock when writing preloader cache. r=erahm
    There are currently some odd circumstances where we deadlock waiting for the
    background save thread to finish while it is blocked on sync dispatch to the
    main thread during shutdown.
    
    There were existing workarounds to prevent this, which tried to synchronously
    complete the main thread work required by the background thread at the start
    of shutdown, and some fallback anti-deadlock assertions to catch any remaining
    corner cases, but apparently Fennec has corner cases of its own that we didn't
    anticipate.
    
    This patch takes the more straightforward route of using an async shutdown
    blocker, which allows the async shutdown service to safely spin the event loop
    until the save completes, rather than an independent monitor loop, which does
    not.
    
    It also fixes a potential data race where the save thread could clear its
    mSaveThread member before NS_NewNamedThread returned, running afoul of
    nsCOMPtr sanity checks.
    
    Differential Revision: https://phabricator.services.mozilla.com/D28127
    
    UltraBlame original commit: ad8ea6ea53e9ec9df9ff49db568fa174c74daf36

--- js/xpconnect/loader/ScriptPreloader.h
+++ js/xpconnect/loader/ScriptPreloader.h
@@ -18,6 +18,7 @@
 #include "mozilla/Result.h"
 #include "mozilla/loader/AutoMemMap.h"
 #include "nsClassHashtable.h"
+#include "nsIAsyncShutdown.h"
 #include "nsIFile.h"
 #include "nsIMemoryReporter.h"
 #include "nsIObserver.h"
@@ -58,7 +59,8 @@ using namespace mozilla::loader;
 
 class ScriptPreloader : public nsIObserver,
                         public nsIMemoryReporter,
-                        public nsIRunnable {
+                        public nsIRunnable,
+                        public nsIAsyncShutdownBlocker {
   MOZ_DEFINE_MALLOC_SIZE_OF(MallocSizeOf)
 
   friend class mozilla::loader::ScriptCacheChild;
@@ -68,6 +70,7 @@ class ScriptPreloader : public nsIObserver,
   NS_DECL_NSIOBSERVER
   NS_DECL_NSIMEMORYREPORTER
   NS_DECL_NSIRUNNABLE
+  NS_DECL_NSIASYNCSHUTDOWNBLOCKER
 
   static ScriptPreloader& GetSingleton();
   static ScriptPreloader& GetChildSingleton();
@@ -374,7 +377,6 @@ class ScriptPreloader : public nsIObserver,
 
   ScriptPreloader();
 
-  void ForceWriteCacheFile();
   void Cleanup();
 
   void FinishPendingParses(MonitorAutoLock& aMal);
@@ -386,11 +388,15 @@ class ScriptPreloader : public nsIObserver,
   
   Result<Ok, nsresult> WriteCache();
 
+  void StartCacheWrite();
+
   
   
   void PrepareCacheWrite();
 
   void PrepareCacheWriteInternal();
+
+  void CacheWriteComplete();
 
   void FinishContentStartup();
 
@@ -413,6 +419,8 @@ class ScriptPreloader : public nsIObserver,
   static void OffThreadDecodeCallback(JS::OffThreadToken* token, void* context);
   void MaybeFinishOffThreadDecode();
   void DoFinishOffThreadDecode();
+
+  already_AddRefed<nsIAsyncShutdownClient> GetShutdownBarrier();
 
   size_t ShallowHeapSizeOfIncludingThis(mozilla::MallocSizeOf mallocSizeOf) {
     return (mallocSizeOf(this) +
@@ -442,7 +450,6 @@ class ScriptPreloader : public nsIObserver,
   bool mSaveComplete = false;
   bool mDataPrepared = false;
   bool mCacheInvalidated = false;
-  bool mBlockedOnSyncDispatch = false;
 
   
   
@@ -474,10 +481,7 @@ class ScriptPreloader : public nsIObserver,
   nsCString mContentStartupFinishedTopic;
 
   nsCOMPtr<nsIFile> mProfD;
-  
-  
-  
-  RefPtr<nsIThread> mSaveThread;
+  nsCOMPtr<nsIThread> mSaveThread;
   nsCOMPtr<nsITimer> mSaveTimer;
 
   
