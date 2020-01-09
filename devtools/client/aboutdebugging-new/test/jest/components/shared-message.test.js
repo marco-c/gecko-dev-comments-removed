bc9464fef6927b1c8a1c4b124edc30d807a6c060
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 20:17:57 +0000

    Bug 1546370 - Revert Node + Jest version bump r=jdescottes,ladybenko
    Revert Node + Jest version bump as try is not supporting node >=10
    
    Differential Revision: https://phabricator.services.mozilla.com/D28484
    
    UltraBlame original commit: d29767eb72476134b304e13e3076d0120709f69c

--- devtools/client/aboutdebugging-new/test/jest/components/shared-message.test.js
+++ devtools/client/aboutdebugging-new/test/jest/components/shared-message.test.js
@@ -7,51 +7,67 @@
 
 
 
-const renderer = require("react-test-renderer");
-const React = require("devtools/client/shared/vendor/react");
-const dom = require("devtools/client/shared/vendor/react-dom-factories");
 
-const { MESSAGE_LEVEL } = require("devtools/client/aboutdebugging-new/src/constants");
 
-const Message = React.createFactory(require("devtools/client/aboutdebugging-new/src/components/shared/Message"));
 
-describe("Message component", () => {
-  it("renders the expected snapshot for INFO level", () => {
-    const message = renderer.create(Message({
-      children: dom.div({}, "Message content"),
-      className: "some-classname-1",
-      level: MESSAGE_LEVEL.INFO,
-    }));
-    expect(message.toJSON()).toMatchSnapshot();
-  });
 
-  it("renders the expected snapshot for WARNING level", () => {
-    const message = renderer.create(Message({
-      children: dom.div({}, "Message content"),
-      className: "some-classname-2",
-      level: MESSAGE_LEVEL.WARNING,
-    }));
-    expect(message.toJSON()).toMatchSnapshot();
-  });
 
-  it("renders the expected snapshot for ERROR level", () => {
-    const message = renderer.create(Message({
-      children: dom.div({}, "Message content"),
-      className: "some-classname-3",
-      level: MESSAGE_LEVEL.ERROR,
-    }));
-    expect(message.toJSON()).toMatchSnapshot();
+
+
+
+describe("Dummy test", () => {
+  it("runs", () => {
+    expect(true).toBe(true);
   });
 });
 
-describe("Message component renders with closing button", () => {
-  it("renders the expected snapshot for Message with closing button", () => {
-    const message = renderer.create(Message({
-      children: dom.div({}, "Message content"),
-      className: "some-classname-1",
-      level: MESSAGE_LEVEL.INFO,
-      isCloseable: true,
-    }));
-    expect(message.toJSON()).toMatchSnapshot();
-  });
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
