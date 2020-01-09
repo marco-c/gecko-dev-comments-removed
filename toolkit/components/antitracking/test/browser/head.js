555be4b3b2ae83f2ad1c0f46cdf728443d629f56
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 17:34:15 +0000

    Bug 1538974 - Ensure that we accept uppercase characters for url-classifier annotation skip URLs; r=johannh
    Differential Revision: https://phabricator.services.mozilla.com/D26019
    
    UltraBlame original commit: 6bd4f28460973ba24d321015663a97b5777355ec

--- toolkit/components/antitracking/test/browser/head.js
+++ toolkit/components/antitracking/test/browser/head.js
@@ -284,6 +284,21 @@ this.AntiTracking = {
 
     if (extraPrefs && Array.isArray(extraPrefs) && extraPrefs.length) {
       await SpecialPowers.pushPrefEnv({"set": extraPrefs });
+
+      for (let item of extraPrefs) {
+        
+        
+        
+        if (item[0] == "urlclassifier.trackingAnnotationSkipURLs") {
+          info("Waiting for the skip list service to initialize...");
+          let classifier = Cc["@mozilla.org/url-classifier/dbservice;1"]
+                             .getService(Ci.nsIURIClassifier);
+          let feature = classifier.getFeatureByName("tracking-annotation");
+          await TestUtils.waitForCondition(() => feature.skipHostList == item[1].toLowerCase(),
+                                           "Skip list service initialized");
+          break;
+        }
+      }
     }
 
     await UrlClassifierTestUtils.addTestTrackers();
