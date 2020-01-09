9dc6ec32629fd25b3a79e1d99eb3a8fc14f9798c
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 15:25:23 +0000

    Bug 1536753 - browser_test_focus_urlbar.js: make waitForSearchFinish work with the quantumbar enabled. r=Standard8
    Differential Revision: https://phabricator.services.mozilla.com/D24144
    
    UltraBlame original commit: 1a7a7301a3316e991c8d136bd8d4abac32679b44

--- accessible/tests/browser/events/browser_test_focus_urlbar.js
+++ accessible/tests/browser/events/browser_test_focus_urlbar.js
@@ -23,6 +23,12 @@ function isEventForAutocompleteItem(event) {
 
 
 function waitForSearchFinish() {
+  if (UrlbarPrefs.get("quantumbar")) {
+    return Promise.all([
+      gURLBar.lastQueryContextPromise,
+      BrowserTestUtils.waitForCondition(() => gURLBar.view.isOpen)
+    ]);
+  }
   return BrowserTestUtils.waitForCondition(() =>
     (gURLBar.popupOpen && gURLBar.controller.searchStatus >=
       Ci.nsIAutoCompleteController.STATUS_COMPLETE_NO_MATCH),
