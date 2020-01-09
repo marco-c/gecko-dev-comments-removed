9718b0193ad21cb7da1ba3a6d23413d22d3f8ff8
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 21:37:11 +0000

    Bug 1550561 - Support edit context menu on HTML inputs in chrome documents. r=bgrins
    Differential Revision: https://phabricator.services.mozilla.com/D30552
    
    UltraBlame original commit: 3315610792f4b94ab15a102fa8119efec42f9a2e

--- toolkit/content/editMenuOverlay.js
+++ toolkit/content/editMenuOverlay.js
@@ -64,7 +64,7 @@ window.addEventListener("contextmenu", (e) => {
   
   let needsContextMenu = e.target.ownerDocument == document &&
                          !e.defaultPrevented &&
-                         e.target.localName == "textarea" &&
+                         ["textarea", "input"].includes(e.target.localName) &&
                          e.target.namespaceURI == "http://www.w3.org/1999/xhtml";
 
   if (!needsContextMenu) {
