909c243f2452466e65599f223f00a2081f5c673a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 10:34:25 +0000

    Bug 1520175 - [marionette] Use "preflight_run_cmd_suites" in mozharness config. r=jmaher,gbrown
    When running Marionette tests in Taskcluster a default window size
    per platform has to be set. Further no screen saver, nor notifications
    should be active which could interfere with the focus of the
    application. Further there is no need to display the taskbar too.
    
    Differential Revision: https://phabricator.services.mozilla.com/D17156
    
    UltraBlame original commit: 2e6a0be2fb273b3dd41026bd09ef18f590b64874

--- testing/mozharness/configs/marionette/mac_taskcluster_config.py
+++ testing/mozharness/configs/marionette/mac_taskcluster_config.py
@@ -1,4 +1,33 @@
 
+
+
+DISABLE_SCREEN_SAVER = False
+ADJUST_MOUSE_AND_SCREEN = False
+
+
 config = {
     "tooltool_cache": "/builds/tooltool_cache",
+    "run_cmd_checks_enabled": True,
+    "preflight_run_cmd_suites": [
+        
+        {
+            "name": "disable_screen_saver",
+            "cmd": ["xset", "s", "off", "s", "reset"],
+            "architectures": ["32bit", "64bit"],
+            "halt_on_failure": False,
+            "enabled": DISABLE_SCREEN_SAVER
+        },
+        {
+            "name": "run mouse & screen adjustment script",
+            "cmd": [
+                
+                
+                "python", "../scripts/external_tools/mouse_and_screen_resolution.py",
+                "--configuration-file",
+                "../scripts/external_tools/machine-configuration.json"],
+            "architectures": ["32bit"],
+            "halt_on_failure": True,
+            "enabled": ADJUST_MOUSE_AND_SCREEN
+        },
+    ],
 }
--- testing/mozharness/configs/marionette/prod_config.py
+++ testing/mozharness/configs/marionette/prod_config.py
@@ -1,6 +1,11 @@
 
 HG_SHARE_BASE_DIR = "/builds/hg-shared"
 
+
+DISABLE_SCREEN_SAVER = True
+ADJUST_MOUSE_AND_SCREEN = False
+
+
 config = {
     
     "marionette_address": "localhost:2828",
@@ -32,5 +37,28 @@ config = {
             "testsdir": "marionette"
         }
     },
+    "run_cmd_checks_enabled": True,
+    "preflight_run_cmd_suites": [
+        
+        {
+            "name": "disable_screen_saver",
+            "cmd": ["xset", "s", "off", "s", "reset"],
+            "halt_on_failure": False,
+            "architectures": ["32bit", "64bit"],
+            "enabled": DISABLE_SCREEN_SAVER
+        },
+        {
+            "name": "run mouse & screen adjustment script",
+            "cmd": [
+                
+                
+                "python", "../scripts/external_tools/mouse_and_screen_resolution.py",
+                "--configuration-file",
+                "../scripts/external_tools/machine-configuration.json"],
+            "architectures": ["32bit"],
+            "halt_on_failure": True,
+            "enabled": ADJUST_MOUSE_AND_SCREEN
+        },
+    ],
     "structured_output": True,
 }
