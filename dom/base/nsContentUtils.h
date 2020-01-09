802ee3e9a19a47fe92078d19932b58aaddf1563c
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 18:30:12 +0000

    Bug 1543321 - Implement nsContentUtils::GetThreadSafeUTFOrigin, r=valentin
    Differential Revision: https://phabricator.services.mozilla.com/D26862
    
    UltraBlame original commit: 1cd48cfaec567b2c023a58b133aa313c1f0fa920

--- dom/base/nsContentUtils.h
+++ dom/base/nsContentUtils.h
@@ -2136,10 +2136,15 @@ class nsContentUtils {
 
 
 
+
+
+
   static nsresult GetASCIIOrigin(nsIPrincipal* aPrincipal, nsACString& aOrigin);
   static nsresult GetASCIIOrigin(nsIURI* aURI, nsACString& aOrigin);
+  static nsresult GetThreadSafeASCIIOrigin(nsIURI* aURI, nsACString& aOrigin);
   static nsresult GetUTFOrigin(nsIPrincipal* aPrincipal, nsAString& aOrigin);
   static nsresult GetUTFOrigin(nsIURI* aURI, nsAString& aOrigin);
+  static nsresult GetThreadSafeUTFOrigin(nsIURI* aURI, nsAString& aOrigin);
 
   
 
