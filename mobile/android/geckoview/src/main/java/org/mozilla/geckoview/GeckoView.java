8277e5e1a8403f117d86f84951900dc827062b16
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 12:08:59 +0000

    Backed out changeset b63cae045a63 (bug 1462018) for geckoview failures
    UltraBlame original commit: 9c42526c4442304d733d16f1e10365a91a702f7f

--- mobile/android/geckoview/src/main/java/org/mozilla/geckoview/GeckoView.java
+++ mobile/android/geckoview/src/main/java/org/mozilla/geckoview/GeckoView.java
@@ -17,7 +17,6 @@ import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Context;
 import android.content.res.Configuration;
-import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Rect;
@@ -166,22 +165,6 @@ public class GeckoView extends FrameLayout {
 
         public boolean shouldPinOnScreen() {
             return mDisplay != null ? mDisplay.shouldPinOnScreen() : false;
-        }
-
-        
-
-
-
-
-
-
-        @UiThread
-        @NonNull GeckoResult<Bitmap> capturePixels() {
-            if (mDisplay == null) {
-                return GeckoResult.fromException(new IllegalStateException("Display must be created before pixels can be captured"));
-            }
-
-            return mDisplay.capturePixels();
         }
     }
 
@@ -725,19 +708,5 @@ public class GeckoView extends FrameLayout {
             }
         }
         mSession.getTextInput().autofill(strValues);
-    }
-
-    
-
-
-
-
-
-
-
-
-    @UiThread
-    public @NonNull GeckoResult<Bitmap> capturePixels() {
-        return mDisplay.capturePixels();
     }
 }
