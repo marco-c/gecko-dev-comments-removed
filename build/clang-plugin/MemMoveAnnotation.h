abff27742a011e7c9548f0d338ad5aab73e74315
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 14:50:00 +0000

    Bug 1535651.  Whitelist std::__pair_base as memmovable for static analysis purposes.  r=emilio
    In at least some libstdc++ versions, std::pair (which we want to be
    memmovable) inherits from __pair_base.  We have a similar setup
    already for __atomic_base.
    
    Differential Revision: https://phabricator.services.mozilla.com/D23698
    
    UltraBlame original commit: 4b6ece1be5c2d4a095c42cf76bdc42120f0da872

--- build/clang-plugin/MemMoveAnnotation.h
+++ build/clang-plugin/MemMoveAnnotation.h
@@ -27,6 +27,7 @@ protected:
       if (Name == "pair" || Name == "atomic" ||
           
           Name == "__atomic_base" || Name == "atomic_bool" ||
+          Name == "__pair_base" ||
           
           Name == "_Atomic_impl" || Name == "_Atomic_base" ||
           Name == "_Atomic_bool" || Name == "_Atomic_char" ||
