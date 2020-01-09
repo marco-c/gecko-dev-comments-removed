fe0a33c764915f91ea16857e8014a2cf9aa616fc
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 01:39:54 +0000

    Bug 1523882 - Rework snapping logic in clip mask generate to fix uneven box shadows. r=kvark,nical
    Differential Revision: https://phabricator.services.mozilla.com/D18060
    
    UltraBlame original commit: 43d250c96e71928eb14dbd6dde03595959b36f89

--- gfx/wr/webrender/src/gpu_types.rs
+++ gfx/wr/webrender/src/gpu_types.rs
@@ -6,7 +6,7 @@ use api::{
     DeviceHomogeneousVector, DevicePoint, DeviceSize, DeviceRect,
     LayoutRect, LayoutToWorldTransform, LayoutTransform,
     PremultipliedColorF, LayoutToPictureTransform, PictureToLayoutTransform, PicturePixel,
-    WorldPixel, WorldToLayoutTransform, LayoutPoint,
+    WorldPixel, WorldToLayoutTransform, LayoutPoint, DeviceVector2D
 };
 use clip_scroll_tree::{ClipScrollTree, ROOT_SPATIAL_NODE_INDEX, SpatialNodeIndex};
 use gpu_cache::{GpuCacheAddress, GpuDataRequest};
@@ -143,6 +143,7 @@ pub struct ClipMaskInstance {
     pub local_pos: LayoutPoint,
     pub tile_rect: LayoutRect,
     pub sub_rect: DeviceRect,
+    pub snap_offsets: SnapOffsets,
 }
 
 
@@ -571,6 +572,28 @@ pub enum UvRectKind {
         bottom_left: DeviceHomogeneousVector,
         bottom_right: DeviceHomogeneousVector,
     },
+}
+
+
+
+#[cfg_attr(feature = "capture", derive(Serialize))]
+#[cfg_attr(feature = "replay", derive(Deserialize))]
+#[derive(Copy, Clone, Debug)]
+#[repr(C)]
+pub struct SnapOffsets {
+    
+    pub top_left: DeviceVector2D,
+    
+    pub bottom_right: DeviceVector2D,
+}
+
+impl SnapOffsets {
+    pub fn empty() -> Self {
+        SnapOffsets {
+            top_left: DeviceVector2D::zero(),
+            bottom_right: DeviceVector2D::zero(),
+        }
+    }
 }
 
 #[derive(Debug, Copy, Clone)]
