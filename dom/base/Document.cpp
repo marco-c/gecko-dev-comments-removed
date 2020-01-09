86ebee7c43e34a422048e3286f7361324c2a2c42
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 22:55:31 +0000

    Backed out changeset edba72c0c8c6 (bug 1551320) for failures on test_autoselect_project.html. CLOSED TREE
    UltraBlame original commit: b01123b50f102787c91291c0b25db80981a3db54

--- dom/base/Document.cpp
+++ dom/base/Document.cpp
@@ -5523,17 +5523,6 @@ already_AddRefed<Element> Document::CreateElement(
     return nullptr;
   }
 
-  
-  if (IsXULDocument()) {
-#if DEBUG
-    xpc_DumpJSStack(true, true, false);
-#endif
-    MOZ_DIAGNOSTIC_ASSERT(false,
-                          "CreateElement() not allowed in XUL document.");
-    rv.Throw(NS_ERROR_FAILURE);
-    return nullptr;
-  }
-
   bool needsLowercase = IsHTMLDocument() && !IsLowercaseASCII(aTagName);
   nsAutoString lcTagName;
   if (needsLowercase) {
