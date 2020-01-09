f43f7bd6e0959c19ea5364b33afe2d38b42b9a34
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 13:34:19 +0000

    Bug 1523969 part 3 - Move method definition inline comments to new line in 'chrome/'. r=ehsan
    Differential Revision: https://phabricator.services.mozilla.com/D21103
    
    UltraBlame original commit: 02e1b59e669da81a15505dacd708e6fc7f567d5b

--- chrome/nsChromeRegistryChrome.cpp
+++ chrome/nsChromeRegistryChrome.cpp
@@ -353,7 +353,8 @@ void nsChromeRegistryChrome::SendRegisteredChrome(
   }
 }
 
- void nsChromeRegistryChrome::ChromePackageFromPackageEntry(
+
+void nsChromeRegistryChrome::ChromePackageFromPackageEntry(
     const nsACString& aPackageName, PackageEntry* aPackage,
     ChromePackage* aChromePackage, const nsCString& aSelectedSkin) {
   nsAutoCString appLocale;
