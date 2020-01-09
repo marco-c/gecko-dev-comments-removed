b569ec630a0fa9e53ef34358d09fbf00d01a9057
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 00:24:16 +0000

    Bug 1521334, make parser to flush its tree operation queue sooner, r=hsivonen
    UltraBlame original commit: d9ae827234990ad8ce27a2337df9dbc9dc578b05

--- modules/libpref/init/StaticPrefList.h
+++ modules/libpref/init/StaticPrefList.h
@@ -561,7 +561,7 @@ VARCACHE_PREF(
 VARCACHE_PREF(
   "html5.flushtimer.initialdelay",
    html5_flushtimer_initialdelay,
-  RelaxedAtomicInt32, 120
+  RelaxedAtomicInt32, 16
 )
 
 
@@ -569,7 +569,7 @@ VARCACHE_PREF(
 VARCACHE_PREF(
   "html5.flushtimer.subsequentdelay",
    html5_flushtimer_subsequentdelay,
-  RelaxedAtomicInt32, 120
+  RelaxedAtomicInt32, 16
 )
 
 
