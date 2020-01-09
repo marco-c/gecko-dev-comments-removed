59932b97e0b3bd68e7dc6d4a94e0b67f7520f52b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 21:49:56 +0000

    Backed out changeset c5798de806e2 (bug 1521280) for crashing when searching about:config for upcoming beta (bug 1551013)
    UltraBlame original commit: fa3cfee27619ddc9bcbcf70555bda4eb1e815146

--- toolkit/content/editMenuOverlay.js
+++ toolkit/content/editMenuOverlay.js
@@ -60,13 +60,12 @@ window.addEventListener("DOMContentLoaded", () => {
 
 
 window.addEventListener("contextmenu", (e) => {
-  const HTML_NS = "http://www.w3.org/1999/xhtml";
   
   
-  let needsContextMenu = e.target.ownerDocument == document && !e.defaultPrevented && (
-    (e.target.localName == "textarea" && e.target.namespaceURI == HTML_NS)
-    || e.target.closest("textbox[is='search-textbox']")
-  );
+  let needsContextMenu = e.target.ownerDocument == document &&
+                         !e.defaultPrevented &&
+                         e.target.localName == "textarea" &&
+                         e.target.namespaceURI == "http://www.w3.org/1999/xhtml";
 
   if (!needsContextMenu) {
     return;
