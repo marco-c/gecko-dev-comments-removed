e7c40f31922f8edb1a89915b7472b546268807bb
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 13:53:18 +0000

    Bug 1213453 - Session-unique group id. r=pehrsons
    Spec in [1] mentions: "The browsing session-unique group identifier for the source of the MediaStreamTrack."
    
    [1] https://www.w3.org/TR/mediacapture-streams/#constrainable-properties
    
    Differential Revision: https://phabricator.services.mozilla.com/D20371
    
    UltraBlame original commit: 1cac58480c6a9a8acc8a11219c8c230e8ff921bb

--- dom/media/MediaManager.h
+++ dom/media/MediaManager.h
@@ -276,7 +276,8 @@ class MediaManager final : public nsIMediaManagerService,
 
  public:  
   static void AnonymizeDevices(MediaDeviceSet& aDevices,
-                               const nsACString& aOriginKey);
+                               const nsACString& aOriginKey,
+                               const uint64_t aWindowId);
   static already_AddRefed<nsIWritableVariant> ToJSArray(
       MediaDeviceSet& aDevices);
 
