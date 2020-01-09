ab0016a5f00516e53534a41efc742a9e4530708d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 20:07:35 +0000

    Bug 1545808 - Enable dom.vr.openvr.action_input in Release. r=kip
    Differential Revision: https://phabricator.services.mozilla.com/D28254
    
    UltraBlame original commit: 00db2a31309b4e74a5d9659db745c5a92899dfcd

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -5509,11 +5509,7 @@ pref("dom.vr.openvr.enabled", true);
 
 pref("dom.vr.openvr.enabled", false);
 #endif
-#ifdef RELEASE_OR_BETA
-pref("dom.vr.openvr.action_input", false);
-#else
 pref("dom.vr.openvr.action_input", true);
-#endif
 
 
 
