81de01651dbf58509b85c1b22942f8f68a48d704
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 18:36:46 +0000

    Bug 1536411 - StoragePrincipal - part 2 - Worker and StoragePrincipal, r=Ehsan
    Differential Revision: https://phabricator.services.mozilla.com/D24026
    
    UltraBlame original commit: 382b8c528545e55f11deb83bc7bff63958f2b45e

--- dom/workers/ScriptLoader.cpp
+++ dom/workers/ScriptLoader.cpp
@@ -1188,7 +1188,7 @@ class ScriptLoaderRunnable final : public nsIRunnable, public nsINamed {
       
       
       
-      rv = mWorkerPrivate->SetPrincipalFromChannel(channel);
+      rv = mWorkerPrivate->SetPrincipalsFromChannel(channel);
       NS_ENSURE_SUCCESS(rv, rv);
 
       nsCOMPtr<nsIContentSecurityPolicy> csp = mWorkerPrivate->GetCSP();
@@ -1296,8 +1296,9 @@ class ScriptLoaderRunnable final : public nsIRunnable, public nsINamed {
       
       
       
-      rv = mWorkerPrivate->SetPrincipalOnMainThread(responsePrincipal,
-                                                    loadGroup);
+      
+      rv = mWorkerPrivate->SetPrincipalsOnMainThread(
+          responsePrincipal, responsePrincipal, loadGroup);
       MOZ_DIAGNOSTIC_ASSERT(NS_SUCCEEDED(rv));
 
       rv = mWorkerPrivate->SetCSPFromHeaderValues(aCSPHeaderValue,
@@ -1841,7 +1842,7 @@ class ChannelGetterRunnable final : public WorkerMainThreadRunnable {
         getter_AddRefs(channel));
     NS_ENSURE_SUCCESS(mResult, true);
 
-    mResult = mLoadInfo.SetPrincipalFromChannel(channel);
+    mResult = mLoadInfo.SetPrincipalsFromChannel(channel);
     NS_ENSURE_SUCCESS(mResult, true);
 
     mLoadInfo.mChannel = channel.forget();
--- dom/workers/sharedworkers/SharedWorker.cpp
+++ dom/workers/sharedworkers/SharedWorker.cpp
@@ -176,6 +176,26 @@ already_AddRefed<SharedWorker> SharedWorker::Constructor(
     return nullptr;
   }
 
+  PrincipalInfo storagePrincipalInfo;
+  if (loadInfo.mPrincipal->Equals(loadInfo.mStoragePrincipal)) {
+    storagePrincipalInfo = principalInfo;
+  } else {
+    aRv = PrincipalToPrincipalInfo(loadInfo.mStoragePrincipal,
+                                   &storagePrincipalInfo);
+    if (NS_WARN_IF(aRv.Failed())) {
+      return nullptr;
+    }
+  }
+
+  nsTArray<ContentSecurityPolicy> storagePrincipalCSP;
+  nsTArray<ContentSecurityPolicy> storagePrincipalPreloadCSP;
+  aRv = PopulateContentSecurityPolicyArray(loadInfo.mStoragePrincipal,
+                                           storagePrincipalCSP,
+                                           storagePrincipalPreloadCSP);
+  if (NS_WARN_IF(aRv.Failed())) {
+    return nullptr;
+  }
+
   
   
   nsCOMPtr<nsIGlobalObject> global = do_QueryInterface(window);
@@ -207,7 +227,8 @@ already_AddRefed<SharedWorker> SharedWorker::Constructor(
   RemoteWorkerData remoteWorkerData(
       nsString(aScriptURL), baseURL, resolvedScriptURL, name,
       loadingPrincipalInfo, loadingPrincipalCSP, loadingPrincipalPreloadCSP,
-      principalInfo, principalCSP, principalPreloadCSP, loadInfo.mDomain,
+      principalInfo, principalCSP, principalPreloadCSP, storagePrincipalInfo,
+      storagePrincipalCSP, storagePrincipalPreloadCSP, loadInfo.mDomain,
       isSecureContext, ipcClientInfo, storageAllowed, true );
 
   PSharedWorkerChild* pActor = actorChild->SendPSharedWorkerConstructor(
