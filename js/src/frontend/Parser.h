feb59216fcc927e63a0961385bb8ce7a7c04b7c2
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 11:56:10 +0000

    Bug 1530389 - Enclose BinAST code in Parser.h with JS_BUILD_BINAST. r=Yoric
    Differential Revision: https://phabricator.services.mozilla.com/D21091
    
    UltraBlame original commit: 7e5a341f5662da8ab99d7ab4ac8b895016ba71ed

--- js/src/frontend/Parser.h
+++ js/src/frontend/Parser.h
@@ -269,8 +269,11 @@ class MOZ_STACK_CLASS ParserSharedBase : private JS::AutoGCRooter {
   
   friend void js::frontend::TraceParser(JSTracer* trc,
                                         JS::AutoGCRooter* parser);
+
+#if defined(JS_BUILD_BINAST)
   friend void js::frontend::TraceBinParser(JSTracer* trc,
                                            JS::AutoGCRooter* parser);
+#endif  
 
  private:
   
