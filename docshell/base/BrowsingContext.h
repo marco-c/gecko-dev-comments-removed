ce93069346d1c5f039a54170e2e5d51d206c1e1b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 14:33:47 +0000

    Bug 1532661 - Part 3: Add WillSetX and DidSetX callbacks for BrowsingContext synced fields, r=farre
    Depends on D22191
    
    Differential Revision: https://phabricator.services.mozilla.com/D22192
    
    UltraBlame original commit: 58f77fa35eeab8de03c52f316d9f0553685a5f8d

--- docshell/base/BrowsingContext.h
+++ docshell/base/BrowsingContext.h
@@ -62,8 +62,13 @@ class BrowsingContextBase {
   }
   ~BrowsingContextBase() = default;
 
-#define BC_FIELD(name, type) type m##name;
-#include "mozilla/dom/BCFieldList.h"
+#define MOZ_BC_FIELD(name, type)                                    \
+  type m##name;                                                     \
+                                                                    \
+  /* shadow to validate fields. aSource is setter process or null*/ \
+  void WillSet##name(type const& aValue, ContentParent* aSource) {} \
+  void DidSet##name(ContentParent* aSource) {}
+#include "mozilla/dom/BrowsingContextFieldList.h"
 };
 
 
@@ -340,6 +345,14 @@ class BrowsingContext : public nsWrapperCache,
           uintptr_t(this) - offsetof(BrowsingContext, mLocation));
     }
   };
+
+  
+  void WillSetOpener(const RefPtr<BrowsingContext>& aValue,
+                     ContentParent* aSource) {
+    if (aValue) {
+      MOZ_RELEASE_ASSERT(aValue->Group() == Group());
+    }
+  }
 
   
   const Type mType;
