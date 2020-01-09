23fdacb189702543e7c5d353f88af6360e35c0df
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 18:35:40 +0000

    Bug 1540581 - P11. Handle colorspace correctly with D3D11 images. r=mattwoodrow,nical
    Differential Revision: https://phabricator.services.mozilla.com/D26466
    
    UltraBlame original commit: bfef6e9e74d53eaa3ef44f27498f31c2b5aee34f

--- gfx/layers/D3D9SurfaceImage.cpp
+++ gfx/layers/D3D9SurfaceImage.cpp
@@ -80,8 +80,12 @@ already_AddRefed<IDirect3DSurface9> DXGID3D9TextureData::GetD3D9Surface()
 }
 
 bool DXGID3D9TextureData::Serialize(SurfaceDescriptor& aOutDescriptor) {
-  aOutDescriptor =
-      SurfaceDescriptorD3D10((WindowsHandle)(mHandle), mFormat, GetSize());
+  
+  bool isYUV = mFormat == SurfaceFormat::NV12 ||
+               mFormat == SurfaceFormat::P010 || mFormat == SurfaceFormat::P016;
+  aOutDescriptor = SurfaceDescriptorD3D10(
+      (WindowsHandle)(mHandle), mFormat, GetSize(),
+      isYUV ? gfx::YUVColorSpace::BT601 : gfx::YUVColorSpace::UNKNOWN);
   return true;
 }
 
