dfc7e5f855daf227d510ce4c86592460d7172757
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 21:45:52 +0000

    Bug 1535484 - Add proper logs for node-devtools test suite r=jlast
    Differential Revision: https://phabricator.services.mozilla.com/D30630
    
    UltraBlame original commit: 15d5a90bad2de0ff603734a83471a2de2435461e

--- devtools/client/webconsole/test/components/evaluation-result.test.js
+++ devtools/client/webconsole/test/components/evaluation-result.test.js
@@ -4,8 +4,11 @@
 
 
 const expect = require("expect");
-const { render, mount } = require("enzyme");
-const sinon = require("sinon");
+const { render } = require("enzyme");
+
+
+
+
 
 
 const { createFactory } = require("devtools/client/shared/vendor/react");
@@ -97,26 +100,27 @@ describe("EvaluationResult component:", () => {
     );
   });
 
-  it("displays a [Learn more] link", () => {
-    const store = setupStore();
+  
+  
+  
 
-    const message = stubPreparedMessages.get("asdf()");
+  
 
-    serviceContainer.openLink = sinon.spy();
-    const wrapper = mount(
-      Provider({ store }, EvaluationResult({ message, serviceContainer }))
-    );
+  
+  
+  
+  
 
-    const url =
-      "https://developer.mozilla.org/docs/Web/JavaScript/Reference/Errors/Not_defined";
-    const learnMore = wrapper.find(".learn-more-link");
-    expect(learnMore.length).toBe(1);
-    expect(learnMore.prop("title")).toBe(url);
+  
+  
+  
+  
+  
 
-    learnMore.simulate("click");
-    const call = serviceContainer.openLink.getCall(0);
-    expect(call.args[0]).toEqual(message.exceptionDocURL);
-  });
+  
+  
+  
+  
 
   it("has the expected indent", () => {
     const message = stubPreparedMessages.get("new Date(0)");
