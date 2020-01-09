85d2a1aa6d4eef8d8bf9af501428651f2fc47f7e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 17:32:59 +0000

    Bug 1538281 - make tree view row keyboard navigation consistent with other shared components. r=nchevobbe
    Differential Revision: https://phabricator.services.mozilla.com/D24538
    
    UltraBlame original commit: bd190be1c55896e1ca204fb2dbd400feaf20926c

--- devtools/client/shared/focus.js
+++ devtools/client/shared/focus.js
@@ -5,15 +5,18 @@
 "use strict";
 
 
-
-
-
-exports.focusableSelector = [
-  "a[href]:not([tabindex='-1'])",
-  "button:not([disabled]):not([tabindex='-1'])",
-  "iframe:not([tabindex='-1'])",
-  "input:not([disabled]):not([tabindex='-1'])",
-  "select:not([disabled]):not([tabindex='-1'])",
-  "textarea:not([disabled]):not([tabindex='-1'])",
-  "[tabindex]:not([tabindex='-1'])",
-].join(", ");
+define(function(require, exports, module) {
+  
+  
+  
+  
+  module.exports.focusableSelector = [
+    "a[href]:not([tabindex='-1'])",
+    "button:not([disabled]):not([tabindex='-1'])",
+    "iframe:not([tabindex='-1'])",
+    "input:not([disabled]):not([tabindex='-1'])",
+    "select:not([disabled]):not([tabindex='-1'])",
+    "textarea:not([disabled]):not([tabindex='-1'])",
+    "[tabindex]:not([tabindex='-1'])",
+  ].join(", ");
+});
