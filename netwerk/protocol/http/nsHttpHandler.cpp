65c62212f44b497ff5ff848d712f0e251e856b6d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 20:21:40 +0000

    Bug 1535697 - Part 5: Pass the top window origin to nsHttpConnectionInfo objects when constructing them; r=michal
    Differential Revision: https://phabricator.services.mozilla.com/D28375
    
    UltraBlame original commit: 969945148b3d02410e597f8b3edce3960d3f20d7

--- netwerk/protocol/http/nsHttpHandler.cpp
+++ netwerk/protocol/http/nsHttpHandler.cpp
@@ -2496,9 +2496,13 @@ nsresult nsHttpHandler::SpeculativeConnectInternal(
   nsAutoCString username;
   aURI->GetUsername(username);
 
-  RefPtr<nsHttpConnectionInfo> ci =
-      new nsHttpConnectionInfo(host, port, EmptyCString(), username, nullptr,
-                               originAttributes, usingSSL);
+  
+  
+  
+  
+  RefPtr<nsHttpConnectionInfo> ci = new nsHttpConnectionInfo(
+      host, port, EmptyCString(), username, EmptyCString(), nullptr,
+      originAttributes, usingSSL);
   ci->SetAnonymous(anonymous);
 
   return SpeculativeConnect(ci, aCallbacks);
