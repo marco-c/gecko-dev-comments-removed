a43734eec4408e2d1f7efb5f65845d6cc0a61082
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 16:29:39 +0000

    Bug 1523734 - Migrate about:blocked to Fluent r=flod,Gijs
    Differential Revision: https://phabricator.services.mozilla.com/D23611
    
    UltraBlame original commit: d0c23055931239ae141171f8912bbefdfee06fbf

--- browser/base/content/blockedSite.js
+++ browser/base/content/blockedSite.js
@@ -83,57 +83,39 @@ function initPage() {
       return;
   }
 
-  var el;
-
-  if (error !== "malware") {
-    el = document.getElementById("errorTitleText_malware");
-    el.remove();
-    el = document.getElementById("errorShortDescText_malware");
-    el.remove();
-    el = document.getElementById("errorLongDesc_malware");
-    el.remove();
-  }
-
-  if (error !== "phishing") {
-    el = document.getElementById("errorTitleText_phishing");
-    el.remove();
-    el = document.getElementById("errorShortDescText_phishing");
-    el.remove();
-    el = document.getElementById("errorLongDesc_phishing");
-    el.remove();
-  }
-
-  if (error !== "unwanted") {
-    el = document.getElementById("errorTitleText_unwanted");
-    el.remove();
-    el = document.getElementById("errorShortDescText_unwanted");
-    el.remove();
-    el = document.getElementById("errorLongDesc_unwanted");
-    el.remove();
-  }
-
-  if (error !== "harmful") {
-    el = document.getElementById("errorTitleText_harmful");
-    el.remove();
-    el = document.getElementById("errorShortDescText_harmful");
-    el.remove();
-    el = document.getElementById("errorLongDesc_harmful");
-    el.remove();
-  }
+  
+  
+  let titleText = document.getElementById("errorTitleText");
+  document.l10n.setAttributes(titleText, "safeb-blocked-" + error + "-page-title");
+  let shortDesc = document.getElementById("errorShortDescText");
+  document.l10n.setAttributes(shortDesc, "safeb-blocked-" + error + "-page-short-desc");
 
   
-  if (getOverride()) {
-    document.getElementById(error + "_error_desc_no_override").remove();
+  let innerDesc = document.getElementById("errorInnerDescription");
+  let innerDescL10nID = "safeb-blocked-" + error + "-page-error-desc-";
+  if (!getOverride()) {
+    innerDescL10nID += "no-override";
+    document.getElementById("ignore_warning_link").remove();
   } else {
-    document.getElementById(error + "_error_desc_override").remove();
+    innerDescL10nID += "override";
+  }
+  if (error == "unwanted" || error == "harmful") {
+    document.getElementById("report_detection").remove();
   }
 
-  
-  let sitenameElem = document.getElementById(error + "_sitename");
-  sitenameElem.setAttribute("class", "sitename");
-  sitenameElem.textContent = getHostString();
+  document.l10n.setAttributes(innerDesc, innerDescL10nID, {sitename: getHostString()});
 
-  document.title = document.getElementById("errorTitleText_" + error).textContent;
+  
+  let learnMore = document.getElementById("learn_more");
+  document.l10n.setAttributes(learnMore, "safeb-blocked-" + error + "-page-learn-more");
+
+  
+  let errorSitename = document.getElementById("error_desc_sitename");
+  errorSitename.setAttribute("class", "sitename");
+
+  let titleEl = document.createElement("title");
+  document.l10n.setAttributes(titleEl, "safeb-blocked-" + error + "-page-title");
+  document.head.appendChild(titleEl);
 
   
   var event = new CustomEvent("AboutBlockedLoaded",
