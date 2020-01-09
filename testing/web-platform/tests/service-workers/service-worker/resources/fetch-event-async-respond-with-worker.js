6eb05d7c5c1f5b61f24a2d8d3c145b9f5b6bf53b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 19:38:35 +0000

    Bug 1536619 [wpt PR 15852] - service worker: Improve WPT tests for async respondWith/waitUntil., a=testonly
    Automatic update from web-platform-tests
    service worker: Improve WPT tests for async respondWith/waitUntil. (#15852)
    
    See discussion at [1] and [2].
    
    This makes the following changes.
    
    1.
    Adds a test for:
    
    self.addEventListener('fetch', e => {
      Promise.resolve().then(() => {
        e.respondWith(new Response('hi'));
      });
    });
    
    This should not throw because respondWith() is called while the event
    dispatch flag is still set.
    
    The microtask checkpoint is in "Cleanup After Running Scripts" here:
    https://html.spec.whatwg.org/multipage/webappapis.html#clean-up-after-running-script
    
    This is called from step 16.2 here:
    https://heycam.github.io/webidl/#call-a-user-objects-operation
    
    Which in turn is called from the DOM spec's "Inner Invoke" to call event
    targets:
    https://dom.spec.whatwg.org/#concept-event-listener-inner-invoke
    
    2.
    Changes the expectation for:
    
    addEventListener('message', event => {
      Promise.resolve().then(event.waitUntil(p));
    });
    
    From throws to not throws, for the same reasoning as above.
    
    3.
    Changes the expectation for:
    
    addEventListener('message', event => {
      waitPromise = Promise.resolve();
      event.waitUntil(waitPromise);
      waitPromise.then(() => {
        Promise.resolve().then(() => {event.waitUntil();});
      });
    });
    
    From throws to not throws. This is subtle. Because all the promises
    are just Promise.resolve(), the event dispatch flag is still set
    by the time the second waitUntil() is called.
    
    4.
    To test what 3. originally intended, a new test is
    added which makes waitPromise a promise that does not immediately
    resolve.
    
    5.
    Changes the expectation for:
    
    addEventListener(‘fetch’, event => {
      response = Promise.resolve(new Response('RESP'));
      event.respondWith(response);
      response.then(() => {
        Promise.resolve().then(() => {event.waitUntil();});
      })
    });
    
    Again this is because the promises used resolve immediately,
    so the event dispatch flag is still set.
    
    Similarly, a new test is added to cover the original intent.
    
    These WPT changes appear to match the behavior of Safari and Edge while
    diverging from Chrome and (partially) Firefox.
    
    [1] https://github.com/w3c/ServiceWorker/issues/1213
    [2] https://github.com/w3c/ServiceWorker/issues/1394
    
    Bug: 942414
    Change-Id: I9a4a56d71d3919ed614ff78df2bdc6cc0251dadd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1524393
    Commit-Queue: Matt Falkenhagen <falkenchromium.org>
    Reviewed-by: Ben Kelly <wanderviewchromium.org>
    Cr-Commit-Position: refs/heads/master{#641514}
    --
    
    wpt-commits: cecb3eba4dae3d795876f7b4be71bd49afa03356
    wpt-pr: 15852
    
    UltraBlame original commit: a941738d800eb075978e20b17f3a72cb680a8398

--- testing/web-platform/tests/service-workers/service-worker/resources/fetch-event-async-respond-with-worker.js
+++ testing/web-platform/tests/service-workers/service-worker/resources/fetch-event-async-respond-with-worker.js
@@ -1,19 +1,56 @@
-var result;
 
-self.addEventListener('message', function(event) {
-    event.data.port.postMessage(result);
+
+
+
+
+let reportResult;
+let resultPromise;
+
+
+
+self.addEventListener('message', (event) => {
+  resultPromise = new Promise((resolve) => {
+    reportResult = resolve;
   });
+
+  
+  
+  event.waitUntil(resultPromise.then(result => {
+    event.source.postMessage(result);
+  }));
+});
+
+
+function tryRespondWith(event) {
+  try {
+    event.respondWith(new Response());
+    reportResult({didThrow: false});
+  } catch (error) {
+    reportResult({didThrow: true, error: error.name});
+  }
+}
+
+function respondWithInTask(event) {
+  setTimeout(() => {
+    tryRespondWith(event);
+  }, 0);
+}
+
+function respondWithInMicrotask(event) {
+  Promise.resolve().then(() => {
+    tryRespondWith(event);
+  });
+}
 
 self.addEventListener('fetch', function(event) {
-    setTimeout(function() {
-        try {
-          event.respondWith(new Response());
-          result = 'FAIL: did not throw';
-        } catch (error) {
-          if (error.name == 'InvalidStateError')
-            result = 'PASS';
-          else
-            result = 'FAIL: Unexpected exception: ' + error;
-        }
-      }, 0);
-  });
+  const path = new URL(event.request.url).pathname;
+  const test = path.substring(path.lastIndexOf('/') + 1);
+
+  
+  
+  if (test == 'respondWith-in-task') {
+    respondWithInTask(event);
+  } else if (test == 'respondWith-in-microtask') {
+    respondWithInMicrotask(event);
+  }
+});
