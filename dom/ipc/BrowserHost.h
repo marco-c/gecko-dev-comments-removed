8301f20a93639cf831e93c6ff8fe8b981230ad0c
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 22:56:43 +0000

    Bug 1525720, part 15 - Apply appropriate nsIRemoteTab methods to all BrowserParents. r=nika
    This commit finally updates some nsIRemoteTab methods to apply
    to the whole tree of BrowserParent's.
    
    Differential Revision: https://phabricator.services.mozilla.com/D31447
    
    UltraBlame original commit: 99f971a02d87941ee49a391de0e0626c170c0821

--- dom/ipc/BrowserHost.h
+++ dom/ipc/BrowserHost.h
@@ -62,6 +62,16 @@ class BrowserHost : public RemoteBrowser, public nsIRemoteTab {
   }
   a11y::DocAccessibleParent* GetTopLevelDocAccessible() const;
 
+  
+  
+  template <typename Callback>
+  void VisitAll(Callback aCallback) {
+    if (!mRoot) {
+      return;
+    }
+    mRoot->VisitAll(aCallback);
+  }
+
   void LoadURL(nsIURI* aURI) override;
   void ResumeLoad(uint64_t aPendingSwitchId) override;
   void DestroyStart() override;
