8993a686599298642b4ebe4e3918442fbd391e26
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 23:14:35 +0000

    Bug 1552979 - Make JS::CompileModule return the module directly, rather than by outparam duplicative of a boolean return value.  r=arai
    UltraBlame original commit: d22bd25523e5a8a49030ae35e9a5aba797c466a3

--- js/public/Modules.h
+++ js/public/Modules.h
@@ -87,10 +87,9 @@ extern JS_PUBLIC_API bool FinishDynamicModuleImport(
 
 
 
-extern JS_PUBLIC_API bool CompileModule(JSContext* cx,
-                                        const ReadOnlyCompileOptions& options,
-                                        SourceText<char16_t>& srcBuf,
-                                        MutableHandle<JSObject*> moduleRecord);
+extern JS_PUBLIC_API JSObject* CompileModule(
+    JSContext* cx, const ReadOnlyCompileOptions& options,
+    SourceText<char16_t>& srcBuf);
 
 
 
