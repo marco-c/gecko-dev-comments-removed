0f8223165d98536a6ae7630f93f34bcf820b0c38
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 13:38:48 +0000

    Backed out changeset cc4b2dbdfd83 (bug 1530208) for failing dom/base/test/unit/test_isequalnode.js on a CLOSED TREE
    UltraBlame original commit: ed6397d7e51a7e9084281d2a8b33b0c5e8eeedb7

--- dom/base/nsTextFragment.h
+++ dom/base/nsTextFragment.h
@@ -236,12 +236,6 @@ class nsTextFragment final {
 
   size_t SizeOfExcludingThis(mozilla::MallocSizeOf aMallocSizeOf) const;
 
-  
-
-
-
-  MOZ_MUST_USE bool TextEquals(const nsTextFragment& aOther) const;
-
  private:
   void ReleaseText();
 
