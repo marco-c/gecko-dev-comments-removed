07b4dbae29980b3e6745aa90f5abb2eb36b03e20
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 17:34:02 +0000

    Bug 1541616 - Part 1: Fix unimplemented.js. r=khyperia
    Differential Revision: https://phabricator.services.mozilla.com/D26032
    
    UltraBlame original commit: b0d590221ac3484fefb93ef2df05efa7cbfd8ccc

--- js/src/tests/non262/fields/unimplemented.js
+++ js/src/tests/non262/fields/unimplemented.js
@@ -1,45 +1,57 @@
 
 
 
-let source = `class C {
+let fieldsEnabled = false;
+try {
+  Function("class C { x; }");
+  fieldsEnabled = true;
+} catch (exc) {
+  assertEq(exc instanceof SyntaxError, true);
+}
+
+
+
+if (!fieldsEnabled) {
+  let source = `class C {
     x
-}`;
-assertThrowsInstanceOf(() => Function(source), SyntaxError);
+  }`;
+  assertThrowsInstanceOf(() => Function(source), SyntaxError);
 
-source = `class C {
+  source = `class C {
     x = 0;
-}`;
-assertThrowsInstanceOf(() => Function(source), SyntaxError);
+  }`;
+  assertThrowsInstanceOf(() => Function(source), SyntaxError);
 
-source = `class C {
+  source = `class C {
     0 = 0;
-}`;
-assertThrowsInstanceOf(() => Function(source), SyntaxError);
+  }`;
+  assertThrowsInstanceOf(() => Function(source), SyntaxError);
 
-source = `class C {
+  source = `class C {
     [0] = 0;
-}`;
-assertThrowsInstanceOf(() => Function(source), SyntaxError);
+  }`;
+  assertThrowsInstanceOf(() => Function(source), SyntaxError);
 
-source = `class C {
+  source = `class C {
     "hi" = 0;
-}`;
-assertThrowsInstanceOf(() => Function(source), SyntaxError);
+  }`;
+  assertThrowsInstanceOf(() => Function(source), SyntaxError);
 
-source = `class C {
+  source = `class C {
     "hi" = 0;
-}`;
-assertThrowsInstanceOf(() => Function(source), SyntaxError);
+  }`;
+  assertThrowsInstanceOf(() => Function(source), SyntaxError);
 
-source = `class C {
+  source = `class C {
     d = function(){};
-}`;
-assertThrowsInstanceOf(() => Function(source), SyntaxError);
+  }`;
+  assertThrowsInstanceOf(() => Function(source), SyntaxError);
 
-source = `class C {
+  source = `class C {
     d = class D { x = 0; };
-}`;
-assertThrowsInstanceOf(() => Function(source), SyntaxError);
+  }`;
+  assertThrowsInstanceOf(() => Function(source), SyntaxError);
+}
 
 if (typeof reportCompare === "function")
   reportCompare(true, true);
