44fa731b10afdcc90d1fda75b9fa44ce55693ca5
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 10:04:50 +0000

    Bug 1526840 - Adapt ToBoolean Ion type inference for false BigInt values r=jandem
    Differential Revision: https://phabricator.services.mozilla.com/D19477
    
    UltraBlame original commit: 0ca63f8a449c5cafdbc3ca181061275e1d1c9b5d

--- js/src/jit/IonBuilder.cpp
+++ js/src/jit/IonBuilder.cpp
@@ -2805,7 +2805,13 @@ AbortReasonOr<Ok> IonBuilder::improveTypesAtTypeOfCompare(MCompare* ins,
     filter.addType(TypeSet::StringType(), alloc_->lifoAlloc());
   } else if (constant->toString() == TypeName(JSTYPE_SYMBOL, names)) {
     filter.addType(TypeSet::SymbolType(), alloc_->lifoAlloc());
-  } else if (constant->toString() == TypeName(JSTYPE_OBJECT, names)) {
+  }
+#ifdef ENABLE_BIGINT
+  else if (constant->toString() == TypeName(JSTYPE_BIGINT, names)) {
+    filter.addType(TypeSet::BigIntType(), alloc_->lifoAlloc());
+  }
+#endif
+  else if (constant->toString() == TypeName(JSTYPE_OBJECT, names)) {
     filter.addType(TypeSet::NullType(), alloc_->lifoAlloc());
     if (trueBranch) {
       filter.addType(TypeSet::AnyObjectType(), alloc_->lifoAlloc());
@@ -3049,6 +3055,10 @@ AbortReasonOr<Ok> IonBuilder::improveTypesAtTest(MDefinition* ins,
                  alloc_->lifoAlloc());  
     base.addType(TypeSet::StringType(),
                  alloc_->lifoAlloc());  
+#ifdef ENABLE_BIGINT
+    base.addType(TypeSet::BigIntType(),
+                 alloc_->lifoAlloc());  
+#endif
 
     
     
