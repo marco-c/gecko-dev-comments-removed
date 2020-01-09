efda0256cb452963d38d7bdc8fa3abc46a66b791
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 23:04:22 +0000

    Bug 1341155 - Show full path in File column in developer tools. r=Honza
    New column, defaulted to hidden, to show full URL for resource in netMonitor panel.
    
    Differential Revision: https://phabricator.services.mozilla.com/D31348
    
    UltraBlame original commit: c1d2feb784e02c9ed085d1faed370e85edcb3032

---
+++ devtools/client/netmonitor/src/components/SecurityState.js
@@ -0,0 +1,75 @@
+
+
+
+
+"use strict";
+
+const { Component } = require("devtools/client/shared/vendor/react");
+const { div } = require("devtools/client/shared/vendor/react-dom-factories");
+const PropTypes = require("devtools/client/shared/vendor/react-prop-types");
+const { L10N } = require("../utils/l10n");
+const { propertiesEqual } = require("../utils/request-utils");
+
+const UPDATED_DOMAIN_PROPS = [
+  "remoteAddress",
+  "securityState",
+  "urlDetails",
+];
+
+class SecurityState extends Component {
+  static get propTypes() {
+    return {
+      item: PropTypes.object.isRequired,
+      onSecurityIconMouseDown: PropTypes.func.isRequired,
+    };
+  }
+
+  shouldComponentUpdate(nextProps) {
+    return !propertiesEqual(UPDATED_DOMAIN_PROPS, this.props.item, nextProps.item);
+  }
+
+  render() {
+    const {
+      item,
+      onSecurityIconMouseDown,
+    } = this.props;
+
+    const {
+      securityState,
+      urlDetails: {
+        isLocal,
+      },
+    } = item;
+    const iconClassList = ["requests-security-state-icon"];
+
+    let iconTitle;
+    let realSecurityState = securityState;
+
+    
+    
+    if (isLocal) {
+      realSecurityState = "secure";
+    }
+
+    if (realSecurityState) {
+      iconClassList.push(`security-state-${realSecurityState}`);
+      iconTitle = L10N.getStr(`netmonitor.security.state.${realSecurityState}`);
+    }
+
+    return (
+      div({},
+        div({
+          className: iconClassList.join(" "),
+          onMouseDown: onSecurityIconMouseDown,
+          title: iconTitle,
+        }),
+        item.isThirdPartyTrackingResource && div({
+          className: "tracking-resource",
+          title: L10N.getStr("netmonitor.trackingResource.tooltip"),
+        }),
+      )
+    );
+  }
+}
+
+module.exports = SecurityState;
