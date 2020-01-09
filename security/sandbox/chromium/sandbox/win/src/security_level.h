5c52fe76353cb8caf871264e606cde5f596d59b7
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 22:33:55 +0000

    Bug 1523526 - Don't allow CFG on old releases of Windows for arm64 r=bobowen,aklotz
    There's a bug in ole32.dll on arm64 versions of Windows prior to 1809, that crashes our content processes if we enable CFG. We've reported the issue, but even if it gets fixed, we can't assume users will have the update.
    
    This patch uses process mitigation policy flags to disable CFG on arm64 before 1809. Based on testing, we only need to do this in the sandbox for child processes, and it's not strictly necessary for the launcher stub to set the flag on the main process. But I've included that anyway as a guard against some yet-undiscovered scenario that might hit the issue and make the browser unusable.
    
    The effects of this patch won't be visible until we actually enable CFG in a subsequent landing.
    
    Differential Revision: https://phabricator.services.mozilla.com/D29474
    
    UltraBlame original commit: e8ac4b512f9d03dae670be39b399ea8189e1ca05

--- security/sandbox/chromium/sandbox/win/src/security_level.h
+++ security/sandbox/chromium/sandbox/win/src/security_level.h
@@ -255,6 +255,15 @@ const MitigationFlags MITIGATION_IMAGE_LOAD_NO_LOW_LABEL = 0x00080000;
 
 const MitigationFlags MITIGATION_IMAGE_LOAD_PREFER_SYS32 = 0x00100000;
 
+
+
+
+
+
+
+
+const MitigationFlags MITIGATION_CONTROL_FLOW_GUARD_DISABLE = 0x80000000;
+
 }  
 
 #endif  
