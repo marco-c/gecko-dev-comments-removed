1dcaf06376161d8687244cd448ce6133db6e1377
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 10:37:02 +0000

    Bug 1524688: Part 1b - Support static JavaScript components in browser_all_files_referenced. r=mccr8
    UltraBlame original commit: df8ab98551c15e956da71cb96dbede239a9a40eb

--- browser/base/content/test/static/browser_all_files_referenced.js
+++ browser/base/content/test/static/browser_all_files_referenced.js
@@ -643,6 +643,10 @@ add_task(async function checkAllTheFiles() {
   
   await throttledMapPromises(manifestURIs, parseManifest);
 
+  for (let jsm of Components.manager.getComponentJSMs()) {
+    gReferencesFromCode.set(jsm, null);
+  }
+
   
   
   
