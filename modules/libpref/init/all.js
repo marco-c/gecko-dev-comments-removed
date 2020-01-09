585e15b624569e10026263a3ce0fcba541fc4696
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 01:53:24 +0000

    Merge mozilla-central to autoland. a=merge on a CLOSED TREE
    UltraBlame original commit: 14d45a5d898d8f40dfd7bff01d17dc0a59192ff9

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -23,7 +23,9 @@ pref("general.useragent.site_specific_overrides", true);
 
 pref("general.config.obscure_value", 13); 
 
+#ifndef MOZ_BUILD_APP_IS_BROWSER
 pref("general.warnOnAboutConfig", true);
+#endif
 
 
 pref("browser.bookmarks.max_backups",       5);
