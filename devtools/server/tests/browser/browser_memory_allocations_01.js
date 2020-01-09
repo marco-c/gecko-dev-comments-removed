f69124c43deb8adbd2130bb1e069f9d33dec30f2
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 00:29:51 +0000

    Bug 1520782 - Convert memory tests using chrome documents and using server references directly to browser mochitests. r=julienw
    This test was using chrome mochitest which forces the test page to be running in chrome and in parent process.
    This doesn't reflect typical setup where the page runs unprivileged in content process.
    Also, with the current bug, the pages running in system principal will be debugged with a special setup.
    Actors will be run with modules loaded in a distinct loader in order to be executed
    in a distinct compartment, distinct from the shared system principal compartment.
    That a prerequisite for the Debugger API. It has to run in a distinct compartment than its debuggee.
    
    Depends on D16824
    
    Differential Revision: https://phabricator.services.mozilla.com/D16825
    
    UltraBlame original commit: 552b8ad866be42f5e1ca2376026c6539d2bd28d3

---
+++ devtools/server/tests/browser/browser_memory_allocations_01.js
@@ -0,0 +1,95 @@
+
+
+
+"use strict";
+
+add_task(async function() {
+  const target = await addTabTarget("data:text/html;charset=utf-8,test-doc");
+  const memory = await target.getFront("memory");
+
+  await memory.attach();
+
+  await memory.startRecordingAllocations();
+  ok(true, "Can start recording allocations");
+
+  
+  const [line1, line2, line3] =
+    await ContentTask.spawn(gBrowser.selectedBrowser, null, function() {
+      
+      return content.eval("(" + function() {
+        let alloc1, alloc2, alloc3;
+
+        
+        (function outer() {
+          (function middle() {
+            (function inner() {
+              alloc1 = {}; alloc1.line = Error().lineNumber;
+              alloc2 = []; alloc2.line = Error().lineNumber;
+              
+              alloc3 = new function() {}; alloc3.line = Error().lineNumber;
+            }());
+          }());
+        }());
+        
+
+        return [ alloc1.line, alloc2.line, alloc3.line ];
+      } + ")()");
+    });
+
+  const response = await memory.getAllocations();
+
+  await memory.stopRecordingAllocations();
+  ok(true, "Can stop recording allocations");
+
+  
+  
+
+  function isTestAllocation(alloc) {
+    const frame = response.frames[alloc];
+    return frame
+      && frame.functionDisplayName === "inner"
+      && (frame.line === line1
+          || frame.line === line2
+          || frame.line === line3);
+  }
+
+  const testAllocations = response.allocations.filter(isTestAllocation);
+  ok(testAllocations.length >= 3,
+     "Should find our 3 test allocations (plus some allocations for the error "
+     + "objects used to get line numbers)");
+
+  
+  
+  
+  
+
+  const expectedLines = new Set([line1, line2, line3]);
+  is(expectedLines.size, 3, "We are expecting 3 allocations");
+
+  for (const alloc of testAllocations) {
+    const innerFrame = response.frames[alloc];
+    ok(innerFrame, "Should get the inner frame");
+    is(innerFrame.functionDisplayName, "inner");
+    expectedLines.delete(innerFrame.line);
+
+    const middleFrame = response.frames[innerFrame.parent];
+    ok(middleFrame, "Should get the middle frame");
+    is(middleFrame.functionDisplayName, "middle");
+
+    const outerFrame = response.frames[middleFrame.parent];
+    ok(outerFrame, "Should get the outer frame");
+    is(outerFrame.functionDisplayName, "outer");
+
+    
+    
+    
+    
+  }
+
+  is(expectedLines.size, 0,
+     "Should have found all the expected lines");
+
+  await memory.detach();
+
+  await target.destroy();
+});
