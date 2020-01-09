370b38d11c11d09bae81a977b56ab99e6a52a1cc
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 16:05:16 +0000

    Bug 1537542 - Hide addon preferences if the addon cannot access the current window. r=aswan
    Differential Revision: https://phabricator.services.mozilla.com/D24534
    
    UltraBlame original commit: 6e7b485a2f314633b7f059095207ad3655d83759

--- toolkit/mozapps/extensions/content/extensions.js
+++ toolkit/mozapps/extensions/content/extensions.js
@@ -1580,6 +1580,17 @@ var gViewController = {
   onEvent() {},
 };
 
+async function isAddonAllowedInCurrentWindow(aAddon) {
+  if (allowPrivateBrowsingByDefault ||
+      aAddon.type !== "extension" ||
+      !PrivateBrowsingUtils.isContentWindowPrivate(window)) {
+    return true;
+  }
+
+  const perms = await ExtensionPermissions.get(aAddon.id);
+  return perms.permissions.includes("internal:privateBrowsingAllowed");
+}
+
 function hasInlineOptions(aAddon) {
   return aAddon.optionsType == AddonManager.OPTIONS_TYPE_INLINE_BROWSER ||
          aAddon.type == "plugin";
@@ -2991,6 +3002,7 @@ var gDetailView = {
     
     let privateBrowsingRow = document.getElementById("detail-privateBrowsing-row");
     let privateBrowsingFooterRow = document.getElementById("detail-privateBrowsing-row-footer");
+
     if (allowPrivateBrowsingByDefault || aAddon.type != "extension" ||
         !(aAddon.permissions & AddonManager.PERM_CAN_CHANGE_PRIVATEBROWSING_ACCESS)) {
       this._privateBrowsing.hidden = true;
@@ -3005,8 +3017,12 @@ var gDetailView = {
       this._privateBrowsing.value = perms.permissions.includes("internal:privateBrowsingAllowed") ? "1" : "0";
     }
 
-    document.getElementById("detail-prefs-btn").hidden = !aIsRemote &&
-      !gViewController.commands.cmd_showItemPreferences.isEnabled(aAddon);
+    
+    
+    let hidePreferences = (!aIsRemote &&
+      !gViewController.commands.cmd_showItemPreferences.isEnabled(aAddon)) ||
+      !await isAddonAllowedInCurrentWindow(aAddon);
+    document.getElementById("detail-prefs-btn").hidden = hidePreferences;
 
     var gridRows = document.querySelectorAll("#detail-grid rows row");
     let first = true;
@@ -3285,6 +3301,12 @@ var gDetailView = {
       whenViewLoaded(async () => {
         const addon = this._addon;
         await addon.startupPromise;
+
+        
+        
+        if (!await isAddonAllowedInCurrentWindow(addon)) {
+          return;
+        }
 
         const browserContainer = await this.createOptionsBrowser(rows);
 
