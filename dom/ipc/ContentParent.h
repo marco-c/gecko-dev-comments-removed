ab54543fcfa4f5dd79fa901a4184a1b91dda592e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 20:39:14 +0000

    Bug 1493225, part 2 - Cancel content JS when navigating through history to prevent hangs r=smaug
    In this part, we pass along the navigation type (and index for when using the
    dropdown on the back/forward buttons). This allows us to check if there's a
    top-level load "between" the start and end pages.
    
    The patch might look a bit strange, since we're passing the navigation
    operation to two places from RemoteWebNavigation.js (the normal message passing
    that existed before this patch and the HangMonitor channel in this patch). This
    is primarily to make it easier to stop passing the navigation info along the
    HangMonitor channel once session history is uplifted into the parent process.
    At that point, the check for whether there's a top-level load could happen in
    TabParent (I think).
    
    Differential Revision: https://phabricator.services.mozilla.com/D23090
    
    UltraBlame original commit: a7fa3041bff04a25fa39fb6f714071314f3cc69b

--- dom/ipc/ContentParent.h
+++ dom/ipc/ContentParent.h
@@ -32,6 +32,7 @@
 #include "nsHashKeys.h"
 #include "nsIInterfaceRequestor.h"
 #include "nsIObserver.h"
+#include "nsIRemoteTab.h"
 #include "nsIThreadInternal.h"
 #include "nsIDOMGeoPositionCallback.h"
 #include "nsIDOMGeoPositionErrorCallback.h"
@@ -109,6 +110,7 @@ class MemoryReport;
 class TabContext;
 class GetFilesHelper;
 class MemoryReportRequestHost;
+struct CancelContentJSOptions;
 
 #define NS_CONTENTPARENT_IID                         \
   {                                                  \
@@ -591,7 +593,10 @@ class ContentParent final : public PContentParent,
                                    bool aForceRepaint,
                                    const layers::LayersObserverEpoch& aEpoch);
 
-  void CancelContentJSExecutionIfRunning(BrowserParent* aBrowserParent);
+  void CancelContentJSExecutionIfRunning(
+      BrowserParent* aBrowserParent,
+      nsIRemoteTab::NavigationType aNavigationType,
+      const CancelContentJSOptions& aCancelContentJSOptions);
 
   
   
