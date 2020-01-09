535dfbc0ae4769db2d780d94b6bf2f085df3ef43
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 13:47:42 +0000

    Bug 1526891 - Part 11: Initiate asynchronous deletion of unreferenced files in IDB databases directly on the PBackground thread instead of going through the main thread; r=asuth
    Differential Revision: https://phabricator.services.mozilla.com/D20920
    
    UltraBlame original commit: c8434f83fb5964672d4d2e221f11013a49055f0e

--- dom/quota/Client.h
+++ dom/quota/Client.h
@@ -172,11 +172,6 @@ class Client {
 
   virtual void ShutdownWorkThreads() = 0;
 
-  
-  virtual void DidInitialize(QuotaManager* aQuotaManager) {}
-
-  virtual void WillShutdown() {}
-
  protected:
   virtual ~Client() {}
 };
