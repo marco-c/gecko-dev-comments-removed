adf6778c9c8f3a3d1bb3fa12f2d6c86a65165574
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 10:03:12 +0000

    Backed out changeset 1f7d1d6bf61d (bug 1488679) for causing build bustages on xre/nsAppRunner.cpp CLOSED TREE
    UltraBlame original commit: bdbab05f9667a968745c1400027d50580eb91b1e

--- toolkit/xre/nsAppRunner.cpp
+++ toolkit/xre/nsAppRunner.cpp
@@ -1531,12 +1531,7 @@ static inline void DumpVersion() {
   if (gAppData->vendor) {
     printf("%s ", (const char*)gAppData->vendor);
   }
-  printf("%s ", (const char*)gAppData->name);
-
-  
-  
-  printf("%s", MOZ_APP_VERSION_DISPLAY);
-
+  printf("%s %s", (const char*)gAppData->name, (const char*)gAppData->version);
   if (gAppData->copyright) {
     printf(", %s", (const char*)gAppData->copyright);
   }
