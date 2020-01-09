791ec8da18ee6ff349bd258cef11d7e52bfc0a12
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 21:36:19 +0000

    Bug 1529612 - Part 4: Work around issue where receiver would get message before sender saw its bufferedAmount drop. r=mjf
    Differential Revision: https://phabricator.services.mozilla.com/D30389
    
    UltraBlame original commit: 074678d618bbbd75055a45de59c0d810bd80f8b0

--- netwerk/sctp/datachannel/DataChannel.cpp
+++ netwerk/sctp/datachannel/DataChannel.cpp
@@ -299,7 +299,8 @@ DataChannelConnection::DataChannelConnection(DataConnectionListener* listener,
       mMaxMessageSizeSet(false),
       mMaxMessageSize(0),
       mAllocateEven(false),
-      mTransportHandler(aHandler) {
+      mTransportHandler(aHandler),
+      mDeferSend(false) {
   mCurrentStream = 0;
   mState = CLOSED;
   mSocket = nullptr;
@@ -844,11 +845,16 @@ void DataChannelConnection::SctpDtlsInput(const std::string& aTransportId,
   usrsctp_conninput(static_cast<void*>(this), packet.data(), packet.len(), 0);
 }
 
-void DataChannelConnection::SendPacket(nsAutoPtr<MediaPacket> packet) {
-  
-  if (!mTransportId.empty() && mTransportHandler) {
-    mTransportHandler->SendPacket(mTransportId, std::move(*packet));
-  }
+void DataChannelConnection::SendPacket(std::unique_ptr<MediaPacket>&& packet) {
+  mSTS->Dispatch(NS_NewRunnableFunction(
+      "DataChannelConnection::SendPacket",
+      [this, self = RefPtr<DataChannelConnection>(this),
+       packet = std::move(packet)]() mutable {
+        
+        if (!mTransportId.empty() && mTransportHandler) {
+          mTransportHandler->SendPacket(mTransportId, std::move(*packet));
+        }
+      }));
 }
 
 
@@ -873,17 +879,16 @@ int DataChannelConnection::SctpDtlsOutput(void* addr, void* buffer,
   
   
   
-  nsAutoPtr<MediaPacket> packet(new MediaPacket);
+  std::unique_ptr<MediaPacket> packet(new MediaPacket);
   packet->SetType(MediaPacket::SCTP);
   packet->Copy(static_cast<const uint8_t*>(buffer), length);
 
-  
-  
-  
-  
-  peer->mSTS->Dispatch(WrapRunnable(RefPtr<DataChannelConnection>(peer),
-                                    &DataChannelConnection::SendPacket, packet),
-                       NS_DISPATCH_NORMAL);
+  if (NS_IsMainThread() && peer->mDeferSend) {
+    peer->mDeferredSend.emplace_back(std::move(packet));
+    return 0;
+  }
+
+  peer->SendPacket(std::move(packet));
   return 0;  
 }
 #endif
@@ -1272,10 +1277,18 @@ bool DataChannelConnection::SendDeferredMessages() {
     
     
     size_t written = 0;
+    mDeferSend = true;
     blocked = SendBufferedMessages(channel->mBufferedData, &written);
+    mDeferSend = false;
     if (written) {
       channel->DecrementBufferedAmount(written);
     }
+
+    for (auto&& packet : mDeferredSend) {
+      MOZ_ASSERT(written);
+      SendPacket(std::move(packet));
+    }
+    mDeferredSend.clear();
 
     
     
@@ -2577,6 +2590,7 @@ int DataChannelConnection::SendMsgInternal(OutgoingMsg& msg, size_t* aWritten) {
     ssize_t written = usrsctp_sendv(
         mSocket, msg.GetData(), length, nullptr, 0, (void*)&msg.GetInfo(),
         (socklen_t)sizeof(struct sctp_sendv_spa), SCTP_SENDV_SPA, 0);
+
     if (written < 0) {
       error = errno;
       goto out;
@@ -2726,11 +2740,19 @@ int DataChannelConnection::SendDataMsgInternalOrBuffer(DataChannel& channel,
   MutexAutoLock lock(mLock);
   bool buffered;
   size_t written = 0;
+  mDeferSend = true;
   int error =
       SendMsgInternalOrBuffer(channel.mBufferedData, msg, buffered, &written);
+  mDeferSend = false;
   if (written) {
     channel.DecrementBufferedAmount(written);
   }
+
+  for (auto&& packet : mDeferredSend) {
+    MOZ_ASSERT(written);
+    SendPacket(std::move(packet));
+  }
+  mDeferredSend.clear();
 
   
   if (!error && buffered && !mPendingType) {
