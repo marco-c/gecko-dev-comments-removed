51889645217c81c3b11db58a06cc438e5a4b3644
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 10:03:19 +0000

    Backed out changeset 91bfef9d7343 (bug 1527374) for causing merge conflict. a=backout
    UltraBlame original commit: 875a93046d849ffe16d8a1c71d335ed92a69c61d

--- toolkit/xre/nsAppRunner.cpp
+++ toolkit/xre/nsAppRunner.cpp
@@ -4245,9 +4245,7 @@ int XREMain::XRE_mainStartup(bool* aExitFlag) {
       flagFile, &cachesOK, &isDowngrade, lastVersion);
 
 #ifdef MOZ_BLOCK_PROFILE_DOWNGRADE
-  
-  
-  if (!CheckArg("allow-downgrade") && isDowngrade) {
+  if (isDowngrade && !CheckArg("allow-downgrade")) {
     rv = CheckDowngrade(mProfD, mProfLD, mProfileName, mNativeApp, mProfileSvc,
                         lastVersion);
     if (rv == NS_ERROR_LAUNCHED_CHILD_PROCESS || rv == NS_ERROR_ABORT) {
