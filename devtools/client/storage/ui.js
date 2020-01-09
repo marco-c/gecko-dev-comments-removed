21d260acee4601b78f9b3509ebbe60c5b8f991ae
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 18:58:28 +0000

    Bug 1544438 - Prevent backspace from deleting Storage entry when editing; r=miker
    Differential Revision: https://phabricator.services.mozilla.com/D27880
    
    UltraBlame original commit: a7af13fee7b22d8a56d75bb72fc1191792dab23d

--- devtools/client/storage/ui.js
+++ devtools/client/storage/ui.js
@@ -1131,16 +1131,21 @@ class StorageUI {
 
 
   handleKeypress(event) {
-    if (event.keyCode == KeyCodes.DOM_VK_ESCAPE && !this.sidebar.hidden) {
-      
-      this.hideSidebar();
-      this.sidebarToggledOpen = false;
-      event.stopPropagation();
-      event.preventDefault();
-    } else if (event.keyCode == KeyCodes.DOM_VK_BACK_SPACE && this.table.selectedRow) {
-      this.onRemoveItem();
-      event.stopPropagation();
-      event.preventDefault();
+    if (event.keyCode == KeyCodes.DOM_VK_ESCAPE) {
+      if (!this.sidebar.hidden) {
+        this.hideSidebar();
+        this.sidebarToggledOpen = false;
+        
+        event.stopPropagation();
+        event.preventDefault();
+      }
+    } else if (event.keyCode == KeyCodes.DOM_VK_BACK_SPACE ||
+      event.keyCode == KeyCodes.DOM_VK_DELETE) {
+      if (this.table.selectedRow && event.target.nodeName !== "textbox") {
+        this.onRemoveItem();
+        event.stopPropagation();
+        event.preventDefault();
+      }
     }
   }
 
