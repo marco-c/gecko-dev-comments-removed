ac18dab23b4187c3d7d1fffdcde1dd51258010c8
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 18:49:29 +0000

    Bug 1544374 - Make FilterOp not copy. r=gw
    Differential Revision: https://phabricator.services.mozilla.com/D27480
    
    UltraBlame original commit: f0c87ec1d694d43288d8ec0a01e290e753ccfeb4

--- gfx/wr/webrender_api/src/display_item.rs
+++ gfx/wr/webrender_api/src/display_item.rs
@@ -626,7 +626,7 @@ pub enum MixBlendMode {
 }
 
 #[repr(C)]
-#[derive(Clone, Copy, Debug, PartialEq, Deserialize, Serialize)]
+#[derive(Clone, Debug, PartialEq, Deserialize, Serialize)]
 pub enum FilterOp {
     
     
@@ -650,7 +650,7 @@ pub enum FilterOp {
 impl FilterOp {
     
     
-    pub fn sanitize(self) -> FilterOp {
+    pub fn sanitize(&self) -> FilterOp {
         match self {
             FilterOp::Blur(radius) => {
                 let radius = radius.min(MAX_BLUR_RADIUS);
@@ -658,9 +658,9 @@ impl FilterOp {
             }
             FilterOp::DropShadow(offset, radius, color) => {
                 let radius = radius.min(MAX_BLUR_RADIUS);
-                FilterOp::DropShadow(offset, radius, color)
+                FilterOp::DropShadow(*offset, radius, *color)
             }
-            filter => filter,
+            filter => filter.clone(),
         }
     }
 }
