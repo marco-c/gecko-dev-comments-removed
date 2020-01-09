ed5c46cc7d0e9a71764253bde984a3b5a3c17bb4
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 01:37:30 +0000

    Bug 1516665 - Part 3: Additional logging. r=JanH
    Added additional logging. In case the agent is not attached but we are not on a
    release or beta build, allow createNotification to be called with a null context
    so that we may be able to gather more information from the reports.
    
    Depends on D18115
    
    Differential Revision: https://phabricator.services.mozilla.com/D18116
    
    UltraBlame original commit: 45c0ce350507fbe5a5a7885b55c2e8bce39fc00e

--- mobile/android/base/java/org/mozilla/gecko/media/MediaControlService.java
+++ mobile/android/base/java/org/mozilla/gecko/media/MediaControlService.java
@@ -39,7 +39,10 @@ public class MediaControlService extends Service {
         Log.d(LOGTAG, "onStartCommand");
 
         if (intent.hasExtra(GeckoMediaControlAgent.EXTRA_NOTIFICATION_DATA)) {
-            if (GeckoMediaControlAgent.getInstance().isAttachedToContext()) {
+            if (GeckoMediaControlAgent.getInstance().isAttachedToContext() ||
+                    
+
+                    !AppConstants.RELEASE_OR_BETA) {
                     currentNotification = GeckoMediaControlAgent.getInstance().createNotification(
                             (MediaNotification) intent.getParcelableExtra(GeckoMediaControlAgent.EXTRA_NOTIFICATION_DATA));
             } else {
