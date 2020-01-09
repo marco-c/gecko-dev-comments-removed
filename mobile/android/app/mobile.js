198866a65dfe145f8a9dc00dbcc7269c0141d1af
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 22:53:09 +0000

    Bug 1553166 - Android browser.cache.disk.max_entry_size is too restrictive, r=valentin
    We no longer use small cache on mobile, so such small max_entry_size is inadequate.
    
    Differential Revision: https://phabricator.services.mozilla.com/D32104
    
    UltraBlame original commit: 4045603461d296f1271d0e49e554131c6ce3a88e

--- mobile/android/app/mobile.js
+++ mobile/android/app/mobile.js
@@ -69,8 +69,6 @@ pref("ui.caretBlinkCount", 10);
 
 
 pref("browser.cache.disk.enable", true);
-pref("browser.cache.disk.capacity", 20480); 
-pref("browser.cache.disk.max_entry_size", 4096); 
 pref("browser.cache.disk.smart_size.enabled", true);
 
 pref("browser.cache.memory.enable", true);
