c2dfbab3ac05df16ff0067b366edc0261aa55f9f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 16:50:26 +0000

    Backed out changeset 59e4bc8fa1ba (bug 1479760) for reftest failures on rtl-selection-with-decoration.html. CLOSED TREE
    UltraBlame original commit: dce6a59456288808e68e71ec339ee2b40a04dc37

--- layout/generic/nsTextFrame.cpp
+++ layout/generic/nsTextFrame.cpp
@@ -4064,16 +4064,17 @@ bool nsTextPaintStyle::InitSelectionColorsAndShadow() {
 
   mInitSelectionColorsAndShadow = true;
 
-  
-  if (RefPtr<ComputedStyle> style =
-          mFrame->ComputeSelectionStyle(selectionStatus)) {
-    mSelectionBGColor =
-        style->GetVisitedDependentColor(&nsStyleBackground::mBackgroundColor);
-    mSelectionTextColor =
-        style->GetVisitedDependentColor(&nsStyleText::mWebkitTextFillColor);
-    mHasSelectionShadow = true;
-    mSelectionShadow = style->StyleText()->mTextShadow;
-    return true;
+  if (selectionStatus == nsISelectionController::SELECTION_ON) {
+    
+    if (RefPtr<ComputedStyle> style = mFrame->ComputeSelectionStyle()) {
+      mSelectionBGColor =
+          style->GetVisitedDependentColor(&nsStyleBackground::mBackgroundColor);
+      mSelectionTextColor =
+          style->GetVisitedDependentColor(&nsStyleText::mWebkitTextFillColor);
+      mHasSelectionShadow = true;
+      mSelectionShadow = style->StyleText()->mTextShadow;
+      return true;
+    }
   }
 
   nscolor selectionBGColor =
