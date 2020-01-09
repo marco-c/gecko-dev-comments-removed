4712b9698bc6c34eb19c81bc8371792e28849794
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 20:17:47 +0000

    Backed out 6 changesets (bug 1533562) for breaking reftests and failures in letter-spacing-005.xht
    Backed out changeset 1976a614f8ce (bug 1533562)
    Backed out changeset 02adcc70efbe (bug 1533562)
    Backed out changeset 00ffda400dc5 (bug 1533562)
    Backed out changeset 710d3c0129de (bug 1533562)
    Backed out changeset 64720021f45c (bug 1533562)
    Backed out changeset 68559438c818 (bug 1533562)
    
    UltraBlame original commit: d2f707cb83c9ac62fceb976c69d7ce69ca69de06

--- widget/cocoa/nsChildView.h
+++ widget/cocoa/nsChildView.h
@@ -601,6 +601,7 @@ class nsChildView final : public nsBaseWidget {
   
   bool mShowsResizeIndicator;
   LayoutDeviceIntRect mResizeIndicatorRect;
+  bool mHasRoundedBottomCorners;
   int mDevPixelCornerRadius;
   bool mIsCoveringTitlebar;
   bool mIsFullscreen;
