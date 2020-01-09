cc424b7b5df7cca5d22a89d24ba01204ef6a860b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 01:03:08 +0000

    Bug 1522724 - Don't clobber visualScrollOffsetUpdate flag if the visual scroll update is higher priority. r=kats,botond
    We need to do both of updates in the case where both updates in the same
    transaction.
    
    Differential Revision: https://phabricator.services.mozilla.com/D17582
    
    UltraBlame original commit: d660ac034e63124372cae5835fcd982363700f5c

--- gfx/layers/apz/src/AsyncPanZoomController.cpp
+++ gfx/layers/apz/src/AsyncPanZoomController.cpp
@@ -4285,7 +4285,9 @@ void AsyncPanZoomController::NotifyLayersUpdated(
   
   bool visualScrollOffsetUpdated =
       aLayerMetrics.GetVisualScrollUpdateType() != FrameMetrics::eNone;
-  if (aLayerMetrics.GetScrollUpdateType() == FrameMetrics::eMainThread ||
+  if ((aLayerMetrics.GetScrollUpdateType() == FrameMetrics::eMainThread &&
+       aLayerMetrics.GetVisualScrollUpdateType() !=
+           FrameMetrics::eMainThread) ||
       smoothScrollRequested) {
     visualScrollOffsetUpdated = false;
   }
