3d8cad5d4ac1ad4fb55d4cef5e7e3991b6c30022
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 00:20:32 +0000

    Backed out changeset a69c1a1dc6fe (bug 1519074) for build bustages on a CLOSED TREE
    UltraBlame original commit: db283d60bcf1b16b3812cdd57a88e1eada225f81

--- dom/security/nsCSPContext.h
+++ dom/security/nsCSPContext.h
@@ -37,9 +37,6 @@ namespace mozilla {
 namespace dom {
 class Element;
 }
-namespace ipc {
-class ContentSecurityPolicy;
-}
 }  
 
 class nsCSPContext : public nsIContentSecurityPolicy {
@@ -57,9 +54,6 @@ class nsCSPContext : public nsIContentSecurityPolicy {
   nsresult InitFromOther(nsCSPContext* otherContext,
                          mozilla::dom::Document* aDoc,
                          nsIPrincipal* aPrincipal);
-
-  void SetIPCPolicies(
-      const nsTArray<mozilla::ipc::ContentSecurityPolicy>& policies);
 
   
 
@@ -139,8 +133,6 @@ class nsCSPContext : public nsIContentSecurityPolicy {
   }
 
  private:
-  void EnsureIPCPoliciesRead();
-
   bool permitsInternal(CSPDirective aDir,
                        mozilla::dom::Element* aTriggeringElement,
                        nsICSPEventListener* aCSPEventListener,
@@ -161,12 +153,6 @@ class nsCSPContext : public nsIContentSecurityPolicy {
 
   nsString mReferrer;
   uint64_t mInnerWindowID;  
-  
-  
-  
-  
-  
-  nsTArray<mozilla::ipc::ContentSecurityPolicy> mIPCPolicies;
   nsTArray<nsCSPPolicy*> mPolicies;
   nsCOMPtr<nsIURI> mSelfURI;
   nsCOMPtr<nsILoadGroup> mCallingChannelLoadGroup;
