b69233f0edb4c72da0154840a9bb650ab824cd67
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 09:50:17 +0000

    Bug 1523952 - Make keyword searches work with QuantumBar. r=adw
    Differential Revision: https://phabricator.services.mozilla.com/D18964
    
    UltraBlame original commit: f57b6157b03530a10e1836066b1be9214ea2929e

--- browser/components/urlbar/tests/browser/browser_keyword_select_and_type.js
+++ browser/components/urlbar/tests/browser/browser_keyword_select_and_type.js
@@ -1,9 +1,12 @@
 
 
 
-function is_selected(index) {
-  is(gURLBar.popup.richlistbox.selectedIndex, index, `Item ${index + 1} should be selected`);
-}
+"use strict";
+
+
+
+
+
 
 add_task(async function() {
   let bookmarks = [];
@@ -32,24 +35,37 @@ add_task(async function() {
   await waitForAutocompleteResultAt(1);
 
   
-  is_selected(0);
+  Assert.equal(UrlbarTestUtils.getSelectedIndex(window), 0,
+    "Should have the first item selected");
+
   
   EventUtils.synthesizeKey("KEY_ArrowDown");
-  is_selected(1);
+  Assert.equal(UrlbarTestUtils.getSelectedIndex(window), 1,
+    "Should have the second item selected");
+
   
   EventUtils.synthesizeKey("KEY_ArrowUp");
-  is_selected(0);
+  Assert.equal(UrlbarTestUtils.getSelectedIndex(window), 0,
+    "Should have the first item selected");
 
   EventUtils.sendString("b");
   await promiseSearchComplete();
 
-  is(gURLBar.textValue, "keyword ab", "urlbar should have expected input");
-  let result = await waitForAutocompleteResultAt(0);
-  isnot(result, null, "Should have first item");
-  let uri = NetUtil.newURI(result.getAttribute("url"));
-  is(uri.spec, PlacesUtils.mozActionURI("keyword", {url: "http://example.com/?q=ab", input: "keyword ab"}), "Expect correct url");
+  Assert.equal(gURLBar.textValue, "keyword ab", "urlbar should have expected input");
 
-  EventUtils.synthesizeKey("KEY_Escape");
-  await promisePopupHidden(gURLBar.popup);
+  let result = await UrlbarTestUtils.getDetailsOfResultAt(window, 0);
+
+  Assert.equal(result.type, UrlbarUtils.RESULT_TYPE.KEYWORD,
+    "Should have a result of type keyword");
+  if (UrlbarPrefs.get("quantumbar")) {
+    Assert.equal(result.url, "http://example.com/?q=ab",
+      "Should have the correct url");
+  } else {
+    Assert.equal(result.url,
+      PlacesUtils.mozActionURI("keyword", {url: "http://example.com/?q=ab", input: "keyword ab"}),
+      "Should have the correct url");
+  }
+
+  await UrlbarTestUtils.promisePopupClose(window);
   gBrowser.removeTab(tab);
 });
