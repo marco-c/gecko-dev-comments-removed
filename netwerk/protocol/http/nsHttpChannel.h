9f6360a4b2b097d896197ddb8f82b5384d5e49f7
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 13:59:24 +0000

    Bug 1525036 - Respect CrossOriginPolicy in nsHttpChannel r=nika
    Differential Revision: https://phabricator.services.mozilla.com/D21415
    
    UltraBlame original commit: fa0363d33dbd074afe05a3278617d368e4d13aa1

--- netwerk/protocol/http/nsHttpChannel.h
+++ netwerk/protocol/http/nsHttpChannel.h
@@ -475,6 +475,10 @@ class nsHttpChannel final : public HttpBaseChannel,
 
   void ProcessSecurityReport(nsresult status);
 
+  nsresult GetResponseCrossOriginPolicy(
+      nsILoadInfo::CrossOriginPolicy *aResponseCrossOriginPolicy);
+  nsresult ProcessCrossOriginHeader();
+
   
 
 
