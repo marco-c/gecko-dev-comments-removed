e74389ff1a32e2c6a8fe766de3f859ebe142f50c
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 22:32:50 +0000

    Bug 1551501 - Fix BinASTTokenReaderContext::readVarU32 to match encoder. r=Yoric
    Differential Revision: https://phabricator.services.mozilla.com/D31054
    
    UltraBlame original commit: 62f8232e95795381a6b8a14b25121f73d9a1877a

--- js/src/frontend/BinASTTokenReaderContext.cpp
+++ js/src/frontend/BinASTTokenReaderContext.cpp
@@ -168,6 +168,7 @@ JS::Result<Ok> BinASTTokenReaderContext::AutoList::done() {
 
 
 
+
 MOZ_MUST_USE JS::Result<uint32_t> BinASTTokenReaderContext::readVarU32() {
   uint32_t result = 0;
   uint32_t shift = 0;
@@ -176,7 +177,7 @@ MOZ_MUST_USE JS::Result<uint32_t> BinASTTokenReaderContext::readVarU32() {
     uint32_t byte;
     MOZ_TRY_VAR(byte, readByte());
 
-    const uint32_t newResult = result | (byte >> 1) << shift;
+    const uint32_t newResult = result | (byte & 0x7f) << shift;
     if (newResult < result) {
       return raiseError("Overflow during readVarU32");
     }
@@ -184,7 +185,7 @@ MOZ_MUST_USE JS::Result<uint32_t> BinASTTokenReaderContext::readVarU32() {
     result = newResult;
     shift += 7;
 
-    if ((byte & 1) == 0) {
+    if ((byte & 0x80) == 0) {
       return result;
     }
 
