4106cd84e9eefd38baf897d20c82a5766c1a8d10
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 17:18:35 +0000

    Bug 1434726 - Avoid SIGBUS on SPARC systems in HashIIDPtrKey r=froydnj
    Differential Revision: https://phabricator.services.mozilla.com/D25429
    
    UltraBlame original commit: 3b847ff23be24d5ea114872a9cde5ce582a9f64a

--- js/xpconnect/src/XPCMaps.cpp
+++ js/xpconnect/src/XPCMaps.cpp
@@ -21,7 +21,9 @@ using namespace mozilla;
 
 
 static PLDHashNumber HashIIDPtrKey(const void* key) {
-  return HashGeneric(*((uintptr_t*)key));
+  uintptr_t v;
+  memcpy(&v, key, sizeof(v));
+  return HashGeneric(v);
 }
 
 static bool MatchIIDPtrKey(const PLDHashEntryHdr* entry, const void* key) {
