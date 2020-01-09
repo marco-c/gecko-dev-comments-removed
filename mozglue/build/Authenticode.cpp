63d676433a5b79ae42aa7e73acdb04b525db1f61
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 21:20:21 +0000

    Bug 1545355: Uppercase hash tags before calling WinVerifyTrust on catalog files; r=mhowell
    On Windows 7, WinVerifyTrust fails unless the tag is uppercased. This patch
    also adds a missing call to CryptCATAdminReleaseCatalogContext, the need for
    which was poorly documented on MSDN.
    
    Differential Revision: https://phabricator.services.mozilla.com/D30146
    
    UltraBlame original commit: bfc71363efc998d502e494acdb4c43401a3da008

--- mozglue/build/Authenticode.cpp
+++ mozglue/build/Authenticode.cpp
@@ -31,6 +31,7 @@
 #include "mozilla/ArrayUtils.h"
 #include "mozilla/Assertions.h"
 #include "mozilla/DynamicallyLinkedFunctionPtr.h"
+#include "mozilla/ScopeExit.h"
 #include "mozilla/TypeTraits.h"
 #include "mozilla/UniquePtr.h"
 #include "mozilla/WindowsVersion.h"
@@ -41,6 +42,8 @@
 #include <wincrypt.h>
 #include <wintrust.h>
 #include <mscat.h>
+
+#include <string.h>
 
 namespace {
 
@@ -307,11 +310,26 @@ bool SignedBinary::VerifySignature(const wchar_t* aFilePath) {
     return false;
   }
 
+  static const mozilla::DynamicallyLinkedFunctionPtr<decltype(
+      &::CryptCATAdminReleaseCatalogContext)>
+    pCryptCATAdminReleaseCatalogContext(L"wintrust.dll",
+                                        "CryptCATAdminReleaseCatalogContext");
+  if (!pCryptCATAdminReleaseCatalogContext) {
+    return false;
+  }
+
   HCATINFO catInfoHdl = pCryptCATAdminEnumCatalogFromHash(
       rawCatAdmin, hashBuf.get(), hashLen, 0, nullptr);
   if (!catInfoHdl) {
     return false;
   }
+
+  
+  
+  auto cleanCatInfoHdl = mozilla::MakeScopeExit(
+    [rawCatAdmin, catInfoHdl]() -> void {
+    pCryptCATAdminReleaseCatalogContext(rawCatAdmin, catInfoHdl, 0);
+  });
 
   
 
@@ -336,6 +354,13 @@ bool SignedBinary::VerifySignature(const wchar_t* aFilePath) {
   if (!::CryptBinaryToStringW(hashBuf.get(), hashLen,
                               CRYPT_STRING_HEXRAW | CRYPT_STRING_NOCRLF,
                               strHashBuf.get(), &strHashBufLen)) {
+    return false;
+  }
+
+  
+  
+  
+  if (_wcsupr_s(strHashBuf.get(), strHashBufLen + 1)) {
     return false;
   }
 
