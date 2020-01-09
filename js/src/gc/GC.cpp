2c28535b93fa27cadb47b365edf5c3a13c90692e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 17:20:11 +0000

    Bug 1540670 - Forbid a nursery size of 0 r=jonco
    The nursery uses capacity_ == 0 to determine if it is disabled.  This patch
    avoids setting the capacity to zero by requring the minimum size to be at
    least ArenaSize (usually 1 page).
    
    Differential Revision: https://phabricator.services.mozilla.com/D25717
    
    UltraBlame original commit: 02b9a8e35a2a5daa1fa6bdbe1121e4a2694cf031

--- js/src/gc/GC.cpp
+++ js/src/gc/GC.cpp
@@ -1403,7 +1403,8 @@ bool GCSchedulingTunables::setParameter(JSGCParamKey key, uint32_t value,
       gcMaxBytes_ = value;
       break;
     case JSGC_MIN_NURSERY_BYTES:
-      if (value > gcMaxNurseryBytes_ && gcMaxNurseryBytes_ != 0) {
+      if ((value > gcMaxNurseryBytes_ && gcMaxNurseryBytes_ != 0) ||
+          value < ArenaSize) {
         
         
         return false;
