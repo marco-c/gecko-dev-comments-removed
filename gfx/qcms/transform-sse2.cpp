d3600f19adbce63b486fc48c206bce17118ecf9a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 23:32:14 +0000

    Bug 1551084 - Part 3. Use templates to reduce code duplication in QCMS. r=miko
    No functional change. This allows us to support both alpha and non-alpha
    variants with the same implementation, in addition to laying the ground
    work for QCMS to support BGRA as an input and output.
    
    Differential Revision: https://phabricator.services.mozilla.com/D30820
    
    UltraBlame original commit: 34a34996c41274de709eb8ed8f94b26af84109e5

--- gfx/qcms/transform-sse2.cpp
+++ gfx/qcms/transform-sse2.cpp
@@ -10,13 +10,14 @@ static const ALIGN float floatScaleX4[4] =
 static const ALIGN float clampMaxValueX4[4] =
     { CLAMPMAXVAL, CLAMPMAXVAL, CLAMPMAXVAL, CLAMPMAXVAL};
 
-void qcms_transform_data_rgb_out_lut_sse2(qcms_transform *transform,
-                                          unsigned char *src,
-                                          unsigned char *dest,
-                                          size_t length)
+template <size_t kRIndex, size_t kGIndex, size_t kBIndex, size_t kAIndex = NO_A_INDEX>
+static void qcms_transform_data_template_lut_sse2(const qcms_transform *transform,
+                                                  const unsigned char *src,
+                                                  unsigned char *dest,
+                                                  size_t length)
 {
     unsigned int i;
-    float (*mat)[4] = transform->matrix;
+    const float (*mat)[4] = transform->matrix;
     char input_back[32];
     
 
@@ -46,117 +47,7 @@ void qcms_transform_data_rgb_out_lut_sse2(qcms_transform *transform,
     const __m128 max   = _mm_load_ps(clampMaxValueX4);
     const __m128 min   = _mm_setzero_ps();
     const __m128 scale = _mm_load_ps(floatScaleX4);
-
-    
-    __m128 vec_r, vec_g, vec_b, result;
-
-    
-    if (!length)
-        return;
-
-    
-    length--;
-
-    
-    vec_r = _mm_load_ss(&igtbl_r[src[0]]);
-    vec_g = _mm_load_ss(&igtbl_g[src[1]]);
-    vec_b = _mm_load_ss(&igtbl_b[src[2]]);
-    src += 3;
-
-    
-
-    for (i=0; i<length; i++)
-    {
-        
-        vec_r = _mm_shuffle_ps(vec_r, vec_r, 0);
-        vec_g = _mm_shuffle_ps(vec_g, vec_g, 0);
-        vec_b = _mm_shuffle_ps(vec_b, vec_b, 0);
-
-        
-        vec_r = _mm_mul_ps(vec_r, mat0);
-        vec_g = _mm_mul_ps(vec_g, mat1);
-        vec_b = _mm_mul_ps(vec_b, mat2);
-
-        
-        vec_r  = _mm_add_ps(vec_r, _mm_add_ps(vec_g, vec_b));
-        vec_r  = _mm_max_ps(min, vec_r);
-        vec_r  = _mm_min_ps(max, vec_r);
-        result = _mm_mul_ps(vec_r, scale);
-
-        
-        _mm_store_si128((__m128i*)output, _mm_cvtps_epi32(result));
-
-        
-        vec_r = _mm_load_ss(&igtbl_r[src[0]]);
-        vec_g = _mm_load_ss(&igtbl_g[src[1]]);
-        vec_b = _mm_load_ss(&igtbl_b[src[2]]);
-        src += 3;
-
-        
-        dest[OUTPUT_R_INDEX] = otdata_r[output[0]];
-        dest[OUTPUT_G_INDEX] = otdata_g[output[1]];
-        dest[OUTPUT_B_INDEX] = otdata_b[output[2]];
-        dest += RGB_OUTPUT_COMPONENTS;
-    }
-
-    
-
-    vec_r = _mm_shuffle_ps(vec_r, vec_r, 0);
-    vec_g = _mm_shuffle_ps(vec_g, vec_g, 0);
-    vec_b = _mm_shuffle_ps(vec_b, vec_b, 0);
-
-    vec_r = _mm_mul_ps(vec_r, mat0);
-    vec_g = _mm_mul_ps(vec_g, mat1);
-    vec_b = _mm_mul_ps(vec_b, mat2);
-
-    vec_r  = _mm_add_ps(vec_r, _mm_add_ps(vec_g, vec_b));
-    vec_r  = _mm_max_ps(min, vec_r);
-    vec_r  = _mm_min_ps(max, vec_r);
-    result = _mm_mul_ps(vec_r, scale);
-
-    _mm_store_si128((__m128i*)output, _mm_cvtps_epi32(result));
-
-    dest[OUTPUT_R_INDEX] = otdata_r[output[0]];
-    dest[OUTPUT_G_INDEX] = otdata_g[output[1]];
-    dest[OUTPUT_B_INDEX] = otdata_b[output[2]];
-}
-
-void qcms_transform_data_rgba_out_lut_sse2(qcms_transform *transform,
-                                           unsigned char *src,
-                                           unsigned char *dest,
-                                           size_t length)
-{
-    unsigned int i;
-    float (*mat)[4] = transform->matrix;
-    char input_back[32];
-    
-
-
-
-    float const * input = (float*)(((uintptr_t)&input_back[16]) & ~0xf);
-    
-
-    uint32_t const * output = (uint32_t*)input;
-
-    
-    const float *igtbl_r = transform->input_gamma_table_r;
-    const float *igtbl_g = transform->input_gamma_table_g;
-    const float *igtbl_b = transform->input_gamma_table_b;
-
-    
-    const uint8_t *otdata_r = &transform->output_table_r->data[0];
-    const uint8_t *otdata_g = &transform->output_table_g->data[0];
-    const uint8_t *otdata_b = &transform->output_table_b->data[0];
-
-    
-    const __m128 mat0  = _mm_load_ps(mat[0]);
-    const __m128 mat1  = _mm_load_ps(mat[1]);
-    const __m128 mat2  = _mm_load_ps(mat[2]);
-
-    
-    const __m128 max   = _mm_load_ps(clampMaxValueX4);
-    const __m128 min   = _mm_setzero_ps();
-    const __m128 scale = _mm_load_ps(floatScaleX4);
+    const unsigned int components = A_INDEX_COMPONENTS(kAIndex);
 
     
     __m128 vec_r, vec_g, vec_b, result;
@@ -170,11 +61,13 @@ void qcms_transform_data_rgba_out_lut_sse2(qcms_transform *transform,
     length--;
 
     
-    vec_r = _mm_load_ss(&igtbl_r[src[0]]);
-    vec_g = _mm_load_ss(&igtbl_g[src[1]]);
-    vec_b = _mm_load_ss(&igtbl_b[src[2]]);
-    alpha = src[3];
-    src += 4;
+    vec_r = _mm_load_ss(&igtbl_r[src[kRIndex]]);
+    vec_g = _mm_load_ss(&igtbl_g[src[kGIndex]]);
+    vec_b = _mm_load_ss(&igtbl_b[src[kBIndex]]);
+    if (kAIndex != NO_A_INDEX) {
+        alpha = src[kAIndex];
+    }
+    src += components;
 
     
 
@@ -191,8 +84,10 @@ void qcms_transform_data_rgba_out_lut_sse2(qcms_transform *transform,
         vec_b = _mm_mul_ps(vec_b, mat2);
 
         
-        dest[OUTPUT_A_INDEX] = alpha;
-        alpha   = src[3];
+        if (kAIndex != NO_A_INDEX) {
+            dest[kAIndex] = alpha;
+            alpha = src[kAIndex];
+        }
 
         
         vec_r  = _mm_add_ps(vec_r, _mm_add_ps(vec_g, vec_b));
@@ -204,16 +99,16 @@ void qcms_transform_data_rgba_out_lut_sse2(qcms_transform *transform,
         _mm_store_si128((__m128i*)output, _mm_cvtps_epi32(result));
 
         
-        vec_r = _mm_load_ss(&igtbl_r[src[0]]);
-        vec_g = _mm_load_ss(&igtbl_g[src[1]]);
-        vec_b = _mm_load_ss(&igtbl_b[src[2]]);
-        src += 4;
+        vec_r = _mm_load_ss(&igtbl_r[src[kRIndex]]);
+        vec_g = _mm_load_ss(&igtbl_g[src[kGIndex]]);
+        vec_b = _mm_load_ss(&igtbl_b[src[kBIndex]]);
+        src += components;
 
         
-        dest[OUTPUT_R_INDEX] = otdata_r[output[0]];
-        dest[OUTPUT_G_INDEX] = otdata_g[output[1]];
-        dest[OUTPUT_B_INDEX] = otdata_b[output[2]];
-        dest += RGBA_OUTPUT_COMPONENTS;
+        dest[kRIndex] = otdata_r[output[0]];
+        dest[kGIndex] = otdata_g[output[1]];
+        dest[kBIndex] = otdata_b[output[2]];
+        dest += components;
     }
 
     
@@ -226,7 +121,9 @@ void qcms_transform_data_rgba_out_lut_sse2(qcms_transform *transform,
     vec_g = _mm_mul_ps(vec_g, mat1);
     vec_b = _mm_mul_ps(vec_b, mat2);
 
-    dest[OUTPUT_A_INDEX] = alpha;
+    if (kAIndex != NO_A_INDEX) {
+        dest[kAIndex] = alpha;
+    }
 
     vec_r  = _mm_add_ps(vec_r, _mm_add_ps(vec_g, vec_b));
     vec_r  = _mm_max_ps(min, vec_r);
@@ -235,9 +132,23 @@ void qcms_transform_data_rgba_out_lut_sse2(qcms_transform *transform,
 
     _mm_store_si128((__m128i*)output, _mm_cvtps_epi32(result));
 
-    dest[OUTPUT_R_INDEX] = otdata_r[output[0]];
-    dest[OUTPUT_G_INDEX] = otdata_g[output[1]];
-    dest[OUTPUT_B_INDEX] = otdata_b[output[2]];
+    dest[kRIndex] = otdata_r[output[0]];
+    dest[kGIndex] = otdata_g[output[1]];
+    dest[kBIndex] = otdata_b[output[2]];
 }
 
+void qcms_transform_data_rgb_out_lut_sse2(const qcms_transform *transform,
+                                          const unsigned char *src,
+                                          unsigned char *dest,
+                                          size_t length)
+{
+  qcms_transform_data_template_lut_sse2<RGBA_R_INDEX, RGBA_G_INDEX, RGBA_B_INDEX>(transform, src, dest, length);
+}
 
+void qcms_transform_data_rgba_out_lut_sse2(const qcms_transform *transform,
+                                           const unsigned char *src,
+                                           unsigned char *dest,
+                                           size_t length)
+{
+  qcms_transform_data_template_lut_sse2<RGBA_R_INDEX, RGBA_G_INDEX, RGBA_B_INDEX, RGBA_A_INDEX>(transform, src, dest, length);
+}
