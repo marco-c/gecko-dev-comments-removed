cd6ed563c2fbdcefc8ef53e9e6de5d9b3b902403
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 18:42:03 +0000

    Bug 1542785 - Expose a `top` method on BrowsingContext to Chrome JS, r=farre
    Differential Revision: https://phabricator.services.mozilla.com/D26554
    
    UltraBlame original commit: 04fa858d12cf64a881d3f73eb19bb1110b45b358

--- docshell/base/BrowsingContext.h
+++ docshell/base/BrowsingContext.h
@@ -156,6 +156,8 @@ class BrowsingContext : public nsWrapperCache,
 
   BrowsingContext* GetParent() const { return mParent; }
 
+  BrowsingContext* Top();
+
   already_AddRefed<BrowsingContext> GetOpener() const { return Get(mOpenerId); }
   void SetOpener(BrowsingContext* aOpener) {
     SetOpenerId(aOpener ? aOpener->Id() : 0);
@@ -383,8 +385,6 @@ class BrowsingContext : public nsWrapperCache,
   
   
   void ClearWindowProxy() { mWindowProxy = nullptr; }
-
-  BrowsingContext* TopLevelBrowsingContext();
 
   friend class Location;
   friend class RemoteLocationProxy;
