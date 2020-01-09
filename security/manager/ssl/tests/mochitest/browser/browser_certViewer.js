2a9b90239a9ef4c4a48168320d08601ca0b1c827
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 15:27:22 +0000

    Bug 1429796 Cleanup storage in CertBlocklist to allow easy addition of new types of pair (e.g. whitelist entries) r=keeler
    Differential Revision: https://phabricator.services.mozilla.com/D17668
    
    UltraBlame original commit: 99079ab7e52e38f0698b184c3a9209f0badbde80

--- security/manager/ssl/tests/mochitest/browser/browser_certViewer.js
+++ security/manager/ssl/tests/mochitest/browser/browser_certViewer.js
@@ -100,11 +100,12 @@ add_task(async function testRevoked() {
   
   
   
-  let certBlocklist = Cc["@mozilla.org/security/certblocklist;1"]
-                        .getService(Ci.nsICertBlocklist);
-  certBlocklist.revokeCertBySubjectAndPubKey(
+  let certBlocklist = Cc["@mozilla.org/security/certstorage;1"]
+                        .getService(Ci.nsICertStorage);
+  certBlocklist.setRevocationBySubjectAndPubKey(
     "MBIxEDAOBgNVBAMMB3Jldm9rZWQ=", 
-    "VCIlmPM9NkgFQtrs4Oa5TeFcDu6MWRTKSNdePEhOgD8="); 
+    "VCIlmPM9NkgFQtrs4Oa5TeFcDu6MWRTKSNdePEhOgD8=", 
+    Ci.nsICertStorage.STATE_ENFORCE); 
   let cert = await readCertificate("revoked.pem", ",,");
   let win = await displayCertificate(cert);
   
