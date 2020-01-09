bf6cd864e07528dee1625c8d7f287be49cccf501
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 01:33:56 +0000

    Bug 1523942 - Remove unused addonID/addonPath from SourceActor. r=jlast
    Differential Revision: https://phabricator.services.mozilla.com/D18244
    
    UltraBlame original commit: bfdfbd4fa2cea68683bb2ad792aeefa8499475af

--- devtools/server/actors/source.js
+++ devtools/server/actors/source.js
@@ -6,8 +6,7 @@
 
 "use strict";
 
-const { Cc, Ci } = require("chrome");
-const Services = require("Services");
+const { Ci } = require("chrome");
 const { BreakpointActor, setBreakpointAtEntryPoints } = require("devtools/server/actors/breakpoint");
 const { GeneratedLocation } = require("devtools/server/actors/common");
 const { createValueGrip } = require("devtools/server/actors/object/utils");
@@ -18,7 +17,6 @@ const { joinURI } = require("devtools/shared/path");
 const { sourceSpec } = require("devtools/shared/specs/source");
 const { findClosestScriptBySource } = require("devtools/server/actors/utils/closest-scripts");
 
-loader.lazyRequireGetter(this, "mapURIToAddonID", "devtools/server/actors/utils/map-uri-to-addon-id");
 loader.lazyRequireGetter(this, "arrayBufferGrip", "devtools/server/actors/array-buffer", true);
 
 function isEvalSource(source) {
@@ -82,39 +80,6 @@ exports.getSourceURL = getSourceURL;
 
 
 
-function resolveURIToLocalPath(uri) {
-  let resolved;
-  switch (uri.scheme) {
-    case "jar":
-    case "file":
-      return uri;
-
-    case "chrome":
-      resolved = Cc["@mozilla.org/chrome/chrome-registry;1"]
-                  .getService(Ci.nsIChromeRegistry).convertChromeURL(uri);
-      return resolveURIToLocalPath(resolved);
-
-    case "resource":
-      resolved = Cc["@mozilla.org/network/protocol;1?name=resource"]
-                  .getService(Ci.nsIResProtocolHandler).resolveURI(uri);
-      uri = Services.io.newURI(resolved);
-      return resolveURIToLocalPath(uri);
-
-    default:
-      return null;
-  }
-}
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -150,8 +115,6 @@ const SourceActor = ActorClassWithSpec(sourceSpec, {
     this.onSource = this.onSource.bind(this);
     this._getSourceText = this._getSourceText.bind(this);
 
-    this._mapSourceToAddon();
-
     this._init = null;
   },
 
@@ -180,12 +143,6 @@ const SourceActor = ActorClassWithSpec(sourceSpec, {
     }
     return this._originalUrl;
   },
-  get addonID() {
-    return this._addonID;
-  },
-  get addonPath() {
-    return this._addonPath;
-  },
 
   get isCacheEnabled() {
     if (this.threadActor._parent._getCacheDisabled) {
@@ -206,8 +163,6 @@ const SourceActor = ActorClassWithSpec(sourceSpec, {
     return {
       actor: this.actorID,
       url: this.url ? this.url.split(" -> ").pop() : null,
-      addonID: this._addonID,
-      addonPath: this._addonPath,
       isBlackBoxed: this.threadActor.sources.isBlackBoxed(this.url),
       sourceMapURL: source ? source.sourceMapURL : null,
       introductionUrl: introductionUrl ? introductionUrl.split(" -> ").pop() : null,
@@ -238,55 +193,6 @@ const SourceActor = ActorClassWithSpec(sourceSpec, {
       query.url = this.url;
     }
     return this.dbg.findScripts(query);
-  },
-
-  _mapSourceToAddon: function() {
-    let nsuri;
-    try {
-      nsuri = Services.io.newURI(this.url.split(" -> ").pop());
-    } catch (e) {
-      
-      return;
-    }
-
-    const localURI = resolveURIToLocalPath(nsuri);
-    if (!localURI) {
-      return;
-    }
-
-    const id = mapURIToAddonID(localURI);
-    if (!id) {
-      return;
-    }
-    this._addonID = id;
-
-    if (localURI instanceof Ci.nsIJARURI) {
-      
-      this._addonPath = localURI.JAREntry;
-    } else if (localURI instanceof Ci.nsIFileURL) {
-      
-      
-      const target = localURI.file;
-      let path = target.leafName;
-
-      
-      
-      let root = target.parent;
-      let file = root.parent;
-      while (file && mapURIToAddonID(Services.io.newFileURI(file))) {
-        path = root.leafName + "/" + path;
-        root = file;
-        file = file.parent;
-      }
-
-      if (!file) {
-        const error = new Error("Could not find the root of the add-on for " + this.url);
-        DevToolsUtils.reportException("SourceActor.prototype._mapSourceToAddon", error);
-        return;
-      }
-
-      this._addonPath = path;
-    }
   },
 
   _reportLoadSourceError: function(error) {
