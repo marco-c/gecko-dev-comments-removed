aad4a903a18ec421783755bb7221ddbd6f0c0e4b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 17:40:18 +0000

    Bug 1541513 part 5.  Stop using AutoJSContext in XPCWrappedNativeProto.  r=mccr8
    Differential Revision: https://phabricator.services.mozilla.com/D26005
    
    UltraBlame original commit: 92f5dfde163e587388a8d4f1eda4a0f91b7784de

--- js/xpconnect/src/XPCWrappedNative.cpp
+++ js/xpconnect/src/XPCWrappedNative.cpp
@@ -196,7 +196,7 @@ nsresult XPCWrappedNative::WrapNewGlobal(xpcObjectHelper& nativeHelper,
 
   
   XPCWrappedNativeProto* proto = XPCWrappedNativeProto::GetNewOrUsed(
-      scope, nativeHelper.GetClassInfo(), scrProto);
+      cx, scope, nativeHelper.GetClassInfo(), scrProto);
   if (!proto) {
     return NS_ERROR_FAILURE;
   }
@@ -403,7 +403,7 @@ nsresult XPCWrappedNative::GetNewOrUsed(xpcObjectHelper& helper,
   
 
   if (info && !isClassInfoSingleton) {
-    proto = XPCWrappedNativeProto::GetNewOrUsed(Scope, info, scrProto);
+    proto = XPCWrappedNativeProto::GetNewOrUsed(cx, Scope, info, scrProto);
     if (!proto) {
       return NS_ERROR_FAILURE;
     }
