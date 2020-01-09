2993b5d2719d22b2ee296cc818e99828e45dae90
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 22:39:48 +0000

    Bug 1552977 - Add JS::EvaluateUtf8PathDontInflate that doesn't inflate UTF-8 file contents to UTF-16 before evaluating them.  r=arai
    Differential Revision: https://phabricator.services.mozilla.com/D31881
    
    UltraBlame original commit: 839bc53c18ca8f3d92e576f9a7cd1dbd582d2d07

--- js/public/CompilationAndEvaluation.h
+++ js/public/CompilationAndEvaluation.h
@@ -169,6 +169,25 @@ extern JS_PUBLIC_API bool EvaluateUtf8Path(
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+extern JS_PUBLIC_API bool EvaluateUtf8PathDontInflate(
+    JSContext* cx, const ReadOnlyCompileOptions& options, const char* filename,
+    MutableHandle<Value> rval);
+
+
+
+
+
 extern JS_PUBLIC_API JSScript* Compile(JSContext* cx,
                                        const ReadOnlyCompileOptions& options,
                                        SourceText<char16_t>& srcBuf);
