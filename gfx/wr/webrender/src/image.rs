ed566397d30eb5353455e3abe7d2075641381595
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 11:26:03 +0000

    Bug 1528753 - Compute the layout space tiling origin. r=kvark
    The tiling origin is computed withing image::tiles instead of being provided to the function.
    In addition, the image rect in device space is exposed as function parameter.
    In a followup, callers will have to determine the correct image rect using the blob image's visible area.
    
    Differential Revision: https://phabricator.services.mozilla.com/D20175
    
    UltraBlame original commit: d88b5f155edc46d64511c2da4dcb40912ef25a72

--- gfx/wr/webrender/src/image.rs
+++ gfx/wr/webrender/src/image.rs
@@ -241,7 +241,7 @@ impl Iterator for TileIterator {
 pub fn tiles(
     prim_rect: &LayoutRect,
     visible_rect: &LayoutRect,
-    device_image_size: &DeviceIntSize,
+    image_rect: &DeviceIntRect,
     device_tile_size: i32,
 ) -> TileIterator {
     
@@ -294,28 +294,18 @@ pub fn tiles(
         }
     };
 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    let layout_tiling_origin = prim_rect.origin;
-    let device_image_range_x = 0..device_image_size.width;
-    let device_image_range_y = 0..device_image_size.height;
+    let device_image_range_x = image_rect.x_range();
+    let device_image_range_y = image_rect.y_range();
 
     
     
-    let x_device_tile_size = i32::min(device_tile_size, device_image_size.width);
-    let y_device_tile_size = i32::min(device_tile_size, device_image_size.height);
+    let x_device_tile_size = i32::min(device_tile_size, image_rect.size.width);
+    let y_device_tile_size = i32::min(device_tile_size, image_rect.size.height);
 
     
     let layout_tile_size = LayoutSize::new(
-        x_device_tile_size as f32 / device_image_size.width as f32 * prim_rect.size.width,
-        y_device_tile_size as f32 / device_image_size.height as f32 * prim_rect.size.height,
+        x_device_tile_size as f32 / image_rect.size.width as f32 * prim_rect.size.width,
+        y_device_tile_size as f32 / image_rect.size.height as f32 * prim_rect.size.height,
     );
 
     
@@ -323,18 +313,18 @@ pub fn tiles(
 
     let x_extent = tiles_1d(
         layout_tile_size.width,
-        visible_rect.min_x()..visible_rect.max_x(),
+        visible_rect.x_range(),
+        prim_rect.min_x(),
         device_image_range_x,
         x_device_tile_size,
-        layout_tiling_origin.x,
     );
 
     let y_extent = tiles_1d(
         layout_tile_size.height,
-        visible_rect.min_y()..visible_rect.max_y(),
+        visible_rect.y_range(),
+        prim_rect.min_y(),
         device_image_range_y,
         y_device_tile_size,
-        layout_tiling_origin.y,
     );
 
     TileIterator {
@@ -356,9 +346,9 @@ pub fn tiles(
 fn tiles_1d(
     layout_tile_size: f32,
     layout_visible_range: Range<f32>,
+    layout_prim_start: f32,
     device_image_range: Range<i32>,
     device_tile_size: i32,
-    layout_tiling_origin: f32,
 ) -> TileIteratorExtent {
     
     debug_assert!(layout_tile_size > 0.0);
@@ -375,11 +365,15 @@ fn tiles_1d(
     let image_tiles = tile_range_1d(&device_image_range, device_tile_size);
 
     
+    let layout_offset = device_image_range.start as f32 * layout_tile_size / device_tile_size as f32;
+    
+    let layout_tiling_origin = layout_prim_start - layout_offset;
+
+    
     let visible_tiles_start = f32::floor((layout_visible_range.start - layout_tiling_origin) / layout_tile_size) as i32;
     let visible_tiles_end = f32::ceil((layout_visible_range.end - layout_tiling_origin) / layout_tile_size) as i32;
 
     
-
     let tiles_start = i32::max(image_tiles.start, visible_tiles_start);
     let tiles_end = i32::min(image_tiles.end, visible_tiles_end);
 
@@ -505,8 +499,8 @@ pub fn compute_tile_size(
 ) -> DeviceIntSize {
     let regular_tile_size = regular_tile_size as i32;
     size2(
-        compute_tile_size_1d(image_rect.min_x()..image_rect.max_x(), regular_tile_size, tile.x as i32),
-        compute_tile_size_1d(image_rect.min_y()..image_rect.max_y(), regular_tile_size, tile.y as i32),
+        compute_tile_size_1d(image_rect.x_range(), regular_tile_size, tile.x as i32),
+        compute_tile_size_1d(image_rect.y_range(), regular_tile_size, tile.y as i32),
     )
 }
 
@@ -560,8 +554,8 @@ pub fn for_each_tile_in_range(
     range: &TileRange,
     mut callback: impl FnMut(TileOffset),
 ) {
-    for y in range.min_y()..range.max_y() {
-        for x in range.min_x()..range.max_x() {
+    for y in range.y_range() {
+        for x in range.x_range() {
             callback(point2(x, y));
         }
     }
@@ -571,14 +565,14 @@ pub fn for_each_tile_in_range(
 mod tests {
     use super::*;
     use std::collections::HashSet;
-    use api::{LayoutRect, DeviceIntSize};
-    use euclid::{rect, size2};
+    use api::LayoutRect;
+    use euclid::rect;
 
     
     fn checked_for_each_tile(
         prim_rect: &LayoutRect,
         visible_rect: &LayoutRect,
-        device_image_size: &DeviceIntSize,
+        device_image_rect: &DeviceIntRect,
         device_tile_size: i32,
         callback: &mut FnMut(&LayoutRect, TileOffset, EdgeAaSegmentMask),
     ) {
@@ -587,7 +581,7 @@ mod tests {
         for tile in tiles(
             prim_rect,
             visible_rect,
-            device_image_size,
+            device_image_rect,
             device_tile_size,
         ) {
             
@@ -606,7 +600,7 @@ mod tests {
         let mut count = 0;
         checked_for_each_tile(&rect(0., 0., 1000., 1000.),
             &rect(75., 75., 400., 400.),
-            &size2(400, 400),
+            &rect(0, 0, 400, 400),
             36,
             &mut |_tile_rect, _tile_offset, _tile_flags| {
                 count += 1;
@@ -620,7 +614,7 @@ mod tests {
         let mut count = 0;
         checked_for_each_tile(&rect(0., 0., 74., 74.),
               &rect(75., 75., 400., 400.),
-              &size2(400, 400),
+              &rect(0, 0, 400, 400),
               36,
               &mut |_tile_rect, _tile_offset, _tile_flags| {
                 count += 1;
@@ -632,38 +626,38 @@ mod tests {
     #[test]
     fn test_tiles_1d() {
         
-        let result = tiles_1d(64.0, -10000.0..10000.0, 0..64, 64, 0.0);
+        let result = tiles_1d(64.0, -10000.0..10000.0, 0.0, 0..64, 64);
         assert_eq!(result.tile_range.start, 0);
         assert_eq!(result.tile_range.end, 1);
         assert_eq!(result.first_tile_layout_size, 64.0);
         assert_eq!(result.last_tile_layout_size, 64.0);
 
         
-        let result = tiles_1d(64.0, -10000.0..10000.0, -64..0, 64, 0.0);
+        let result = tiles_1d(64.0, -10000.0..10000.0, -64.0, -64..0, 64);
         assert_eq!(result.tile_range.start, -1);
         assert_eq!(result.tile_range.end, 0);
         assert_eq!(result.first_tile_layout_size, 64.0);
         assert_eq!(result.last_tile_layout_size, 64.0);
 
         
-        let result = tiles_1d(64.0, -10000.0..10000.0, -64..64, 64, 0.0);
+        let result = tiles_1d(64.0, -10000.0..10000.0, -64.0, -64..64, 64);
         assert_eq!(result.tile_range.start, -1);
         assert_eq!(result.tile_range.end, 1);
         assert_eq!(result.first_tile_layout_size, 64.0);
         assert_eq!(result.last_tile_layout_size, 64.0);
 
         
-        let result = tiles_1d(64.0, -100.0..10.0, 64..310, 64, 0.0);
+        let result = tiles_1d(64.0, -100.0..10.0, 64.0, 64..310, 64);
         assert_eq!(result.tile_range.start, result.tile_range.end);
 
         
         
-        let result = tiles_1d(64.0, 10.0..10000.0, -64..64, 64, 0.0);
+        let result = tiles_1d(64.0, 10.0..10000.0, -64.0, -64..64, 64);
         assert_eq!(result.tile_range.start, 0);
         assert_eq!(result.tile_range.end, 1);
         assert_eq!(result.first_tile_layout_size, 64.0);
         assert_eq!(result.last_tile_layout_size, 64.0);
-        let result = tiles_1d(64.0, -10000.0..-10.0, -64..64, 64, 0.0);
+        let result = tiles_1d(64.0, -10000.0..-10.0, -64.0, -64..64, 64);
         assert_eq!(result.tile_range.start, -1);
         assert_eq!(result.tile_range.end, 0);
         assert_eq!(result.first_tile_layout_size, 64.0);
@@ -671,21 +665,21 @@ mod tests {
 
         
         
-        let result = tiles_1d(128.0, -10000.0..10000.0, -64..32, 64, 0.0);
+        let result = tiles_1d(128.0, -10000.0..10000.0, -64.0, -64..32, 64);
         assert_eq!(result.tile_range.start, -1);
         assert_eq!(result.tile_range.end, 1);
         assert_eq!(result.first_tile_layout_size, 128.0);
         assert_eq!(result.last_tile_layout_size, 64.0);
 
         
-        let result = tiles_1d(10.0, 0.0..20.0, 0..64, 64, 0.0);
+        let result = tiles_1d(10.0, 0.0..20.0, 0.0, 0..64, 64);
         assert_eq!(result.tile_range.start, 0);
         assert_eq!(result.tile_range.end, 1);
         assert_eq!(result.first_tile_layout_size, 10.0);
         assert_eq!(result.last_tile_layout_size, 10.0);
 
         
-        let result = tiles_1d(10.0, -20.0..0.0, 0..64, 64, -20.0);
+        let result = tiles_1d(10.0, -20.0..0.0, -20.0, 0..64, 64);
         assert_eq!(result.tile_range.start, 0);
         assert_eq!(result.tile_range.end, 1);
         assert_eq!(result.first_tile_layout_size, 10.0);
@@ -724,7 +718,7 @@ mod tests {
 
 
         
-        let result = tiles_1d(64.0, -10000.0..10000.0, 300..310, 64, 0.0);
+        let result = tiles_1d(64.0, -10000.0..10000.0, 0.0, 300..310, 64);
         assert_eq!(result.tile_range.start, 4);
         assert_eq!(result.tile_range.end, 5);
         assert_eq!(result.first_tile_layout_size, 10.0);
