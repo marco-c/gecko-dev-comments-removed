7ce4612676b48c078310eba4f3ccd40c1799cfbc
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 14:36:11 +0000

    Bug 1534847 - Part 1: Refactor loadSourceText to separate caching from logic. r=jlast
    Summary:
    
    Splitting up this logic makes us less likely to introduce code that would break
    the caching behavior. If you look closely at these changes, you'll notice that
    there actually one one early return in this code that would cause us to
    exit without clearing the 'requests' cache meaning we could get stuck in
    an infinite loading state.
    
    Reviewers: jlast
    
    Reviewed By: jlast
    
    Bug #: 1534847
    
    Differential Revision: https://phabricator.services.mozilla.com/D23451
    
    UltraBlame original commit: 3befa22661a8cbab7b30f337fb3b42fbcb30fcdd

--- devtools/client/debugger/new/src/actions/sources/loadSourceText.js
+++ devtools/client/debugger/new/src/actions/sources/loadSourceText.js
@@ -12,7 +12,6 @@ import * as parser from "../../workers/parser";
 import { isLoaded, isOriginal } from "../../utils/source";
 import { Telemetry } from "devtools-modules";
 
-import defer from "../../utils/defer";
 import type { ThunkArgs } from "../types";
 
 import type { Source } from "../../types";
@@ -32,6 +31,7 @@ async function loadSource(state, source: Source, { sourceMaps, client }) {
     throw new Error("No source actor for loadSource");
   }
 
+  telemetry.start(loadSourceHistogram, source);
   const response = await client.sourceContents(source.actors[0]);
   telemetry.finish(loadSourceHistogram, source);
 
@@ -42,56 +42,62 @@ async function loadSource(state, source: Source, { sourceMaps, client }) {
   };
 }
 
+async function loadSourceTextPromise(
+  source: Source,
+  { dispatch, getState, client, sourceMaps }: ThunkArgs
+): Promise<?Source> {
+  if (isLoaded(source)) {
+    return source;
+  }
+
+  await dispatch({
+    type: "LOAD_SOURCE_TEXT",
+    sourceId: source.id,
+    [PROMISE]: loadSource(getState(), source, { sourceMaps, client })
+  });
+
+  const newSource = getSource(getState(), source.id);
+  if (!newSource) {
+    return;
+  }
+
+  if (!newSource.isWasm && isLoaded(newSource)) {
+    parser.setSource(newSource);
+    await dispatch(setBreakpointPositions(newSource.id));
+  }
+
+  return newSource;
+}
 
 
 
 
-export function loadSourceText(source: ?Source) {
-  return async ({ dispatch, getState, client, sourceMaps }: ThunkArgs) => {
-    if (!source) {
+
+export function loadSourceText(inputSource: ?Source) {
+  return async (thunkArgs: ThunkArgs) => {
+    if (!inputSource) {
       return;
     }
+    
+    
+    const source = inputSource;
 
     const id = source.id;
-    
-    if (requests.has(id)) {
-      return requests.get(id);
+    let promise = requests.get(id);
+    if (!promise) {
+      promise = (async () => {
+        try {
+          return await loadSourceTextPromise(source, thunkArgs);
+        } catch (e) {
+          
+          
+        } finally {
+          requests.delete(id);
+        }
+      })();
+      requests.set(id, promise);
     }
 
-    if (isLoaded(source)) {
-      return Promise.resolve();
-    }
-
-    const deferred = defer();
-    requests.set(id, deferred.promise);
-
-    telemetry.start(loadSourceHistogram, source);
-    try {
-      await dispatch({
-        type: "LOAD_SOURCE_TEXT",
-        sourceId: source.id,
-        [PROMISE]: loadSource(getState(), source, { sourceMaps, client })
-      });
-    } catch (e) {
-      deferred.resolve();
-      requests.delete(id);
-      return;
-    }
-
-    const newSource = getSource(getState(), source.id);
-    if (!newSource) {
-      return;
-    }
-
-    if (!newSource.isWasm && isLoaded(newSource)) {
-      parser.setSource(newSource);
-      await dispatch(setBreakpointPositions(newSource.id));
-    }
-
-    
-    deferred.resolve();
-    requests.delete(id);
-
-    return source;
+    return promise;
   };
 }
