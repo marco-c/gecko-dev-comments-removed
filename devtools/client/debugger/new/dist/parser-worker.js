61429f302ca5e1cb435dcba1983be15290e8857d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 02:27:16 +0000

    Bug 1525492 - [release 123] Fix mapBindings exception when declaration with no initial value is provided (#7830) (#7865). r=dwalsh
    UltraBlame original commit: eb8480682540d72d201115ea0919340d1619ba75

--- devtools/client/debugger/new/dist/parser-worker.js
+++ devtools/client/debugger/new/dist/parser-worker.js
@@ -23674,7 +23674,7 @@ function getAssignmentTarget(node, bindings) {
 
 
 function globalizeDeclaration(node, bindings) {
-  return node.declarations.map(declaration => t.expressionStatement(t.assignmentExpression("=", getAssignmentTarget(declaration.id, bindings), declaration.init)));
+  return node.declarations.map(declaration => t.expressionStatement(t.assignmentExpression("=", getAssignmentTarget(declaration.id, bindings), declaration.init || t.unaryExpression("void", t.numericLiteral(0)))));
 }
 
 
