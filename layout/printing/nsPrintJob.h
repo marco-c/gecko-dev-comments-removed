5478d75a721dec47faba9f47fd7aab04707f49e6
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 22:17:08 +0000

    Bug 1551950. Rename nsPrintJob::SetDocAndURLIntoProgress to SetURLAndTitleOnProgress. r=bobowen
    Differential Revision: https://phabricator.services.mozilla.com/D31278
    
    UltraBlame original commit: 89b3cde264df3bb09a06ad3d35419315f9c87120

--- layout/printing/nsPrintJob.h
+++ layout/printing/nsPrintJob.h
@@ -135,8 +135,9 @@ class nsPrintJob final : public nsIObserver,
   
   
   nsresult FinishPrintPreview();
-  void SetDocAndURLIntoProgress(const mozilla::UniquePtr<nsPrintObject>& aPO,
-                                nsIPrintProgressParams* aParams);
+  void SetURLAndTitleOnProgressParams(
+      const mozilla::UniquePtr<nsPrintObject>& aPO,
+      nsIPrintProgressParams* aParams);
   void EllipseLongString(nsAString& aStr, const uint32_t aLen, bool aDoFront);
   nsresult CheckForPrinters(nsIPrintSettings* aPrintSettings);
   void CleanupDocTitleArray(char16_t**& aArray, int32_t& aCount);
