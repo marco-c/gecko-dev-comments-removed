90c7e839b8aa4d5f561cf82ae0a715b920b7b8ad
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 14:22:10 +0000

    Bug 1530364 - Ensure compartment always has a live global when creating a new global in it. r=bzbarsky
    Differential Revision: https://phabricator.services.mozilla.com/D23287
    
    UltraBlame original commit: 7f3111601872c9d6e2a49a52cc57f7c7a25210e7

--- js/src/vm/GlobalObject.cpp
+++ js/src/vm/GlobalObject.cpp
@@ -570,6 +570,17 @@ GlobalObject* GlobalObject::new_(JSContext* cx, const Class* clasp,
   MOZ_ASSERT(!cx->isExceptionPending());
   MOZ_ASSERT_IF(cx->zone(), !cx->zone()->isAtomsZone());
 
+  
+  
+  
+  Rooted<GlobalObject*> existingGlobal(cx);
+  const JS::RealmCreationOptions& creationOptions = options.creationOptions();
+  if (creationOptions.compartmentSpecifier() ==
+      JS::CompartmentSpecifier::ExistingCompartment) {
+    Compartment* comp = creationOptions.compartment();
+    existingGlobal = &comp->firstGlobal();
+  }
+
   Realm* realm = NewRealm(cx, principals, options);
   if (!realm) {
     return nullptr;
