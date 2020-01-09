2318a8dbbffe134e6a2f06853d3a193e34dad12c
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 11:45:49 +0000

    Bug 1528636 - Enable staging updates on Linux entirely and not just for EARLY_BETA_OR_EARLIER for Firefox 67. r=mhowell
    Differential Revision: https://phabricator.services.mozilla.com/D20975
    
    UltraBlame original commit: 50e17e84f24d4fae03f4b499d7c76fd229d224ea

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -145,13 +145,7 @@ pref("app.update.silent", false);
 
 
 
-#if defined(XP_WIN) || defined(XP_MACOSX)
 pref("app.update.staging.enabled", true);
-#elif defined(EARLY_BETA_OR_EARLIER)
-pref("app.update.staging.enabled", true);
-#else
-pref("app.update.staging.enabled", false);
-#endif
 
 
 pref("app.update.url", "https://aus5.mozilla.org/update/6/%PRODUCT%/%VERSION%/%BUILD_ID%/%BUILD_TARGET%/%LOCALE%/%CHANNEL%/%OS_VERSION%/%SYSTEM_CAPABILITIES%/%DISTRIBUTION%/%DISTRIBUTION_VERSION%/update.xml");
