a18742c16269c527d53a44093ca3644046142677
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 23:02:05 +0000

    Bug 1551873 - Fix JSWindowActor's intermittent TV failures; r=nika
    Differential Revision: https://phabricator.services.mozilla.com/D31788
    
    UltraBlame original commit: 199eaff06ecd465e8cae54496369d147edb48f3e

--- dom/ipc/tests/JSWindowActor/head.js
+++ dom/ipc/tests/JSWindowActor/head.js
@@ -64,12 +64,13 @@ function declTest(name, cfg) {
 
     
     info("browser ready");
-    await Promise.resolve(test(browser, win));
-
-    
-    ChromeUtils.unregisterWindowActor("Test");
-    await BrowserTestUtils.closeWindow(win);
-
-    info("Exiting test: " + name);
+    try {
+      await Promise.resolve(test(browser, win));
+    } finally {
+      
+      ChromeUtils.unregisterWindowActor("Test");
+      await BrowserTestUtils.closeWindow(win);
+      info("Exiting test: " + name);
+    }
   });
 }
