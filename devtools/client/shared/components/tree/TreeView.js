caba5f83c71592e33e1ec346323a2db2ef86c1b2
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 00:22:51 +0000

    Backed out changeset 50cc5c5bd5ef (bug 1520107) for mochitest failures at test_accessible_row_context_menu.html. CLOSED TREE
    UltraBlame original commit: 1aee4f577b05824f71ca36db2ab640e76d90cc60

--- devtools/client/shared/components/tree/TreeView.js
+++ devtools/client/shared/components/tree/TreeView.js
@@ -118,8 +118,6 @@ define(function(require, exports, module) {
         
         onClickRow: PropTypes.func,
         
-        onContextMenuTree: PropTypes.func,
-        
         header: PropTypes.bool,
         
         expandableStrings: PropTypes.bool,
@@ -514,8 +512,8 @@ define(function(require, exports, module) {
       const classNames = ["treeTable"];
       this.rows = [];
 
-      const { className, onContextMenuTree } = this.props;
       
+      const className = this.props.className;
       if (className) {
         classNames.push(...className.split(" "));
       }
@@ -543,7 +541,6 @@ define(function(require, exports, module) {
           },
           tabIndex: 0,
           onKeyDown: this.onKeyDown,
-          onContextMenu: onContextMenuTree && onContextMenuTree.bind(this),
           "aria-label": this.props.label || "",
           "aria-activedescendant": this.state.selected,
           cellPadding: 0,
@@ -551,6 +548,7 @@ define(function(require, exports, module) {
           TreeHeader(props),
           dom.tbody({
             role: "presentation",
+            tabIndex: -1,
           }, rows)
         )
       );
