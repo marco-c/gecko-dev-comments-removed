0ac74386e0b922c0b19251eb8c6fdd019c769370
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 01:37:36 +0000

    Bug 1355588 - Implement slide-up/down tab opening/closing animation. r=dao
    This patch implements a new, preffed-off tab opening/closing animation where the tab slides up and down in place. In addition, it adjusts some tests and code that were relying on the max-width transitionend event happening. The intent of the patch is to land it preffed-off and iterate on the design in the tree
    
    Differential Revision: https://phabricator.services.mozilla.com/D15955
    
    UltraBlame original commit: 2827b5f1530b53e4fdf5f97b1c32728a15482578

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -483,6 +483,9 @@ pref("browser.tabs.showAudioPlayingIcon", true);
 pref("browser.tabs.delayHidingAudioPlayingIconMS", 3000);
 
 
+pref("browser.tabs.newanimations", false);
+
+
 #if defined(NIGHTLY_BUILD) && !defined(MOZ_ASAN)
 pref("browser.tabs.remote.separatePrivilegedContentProcess", true);
 #endif
