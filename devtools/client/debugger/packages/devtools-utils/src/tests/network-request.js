582c44c916ce342012057ac27aaebf6de7f4b6c8
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 22:36:46 +0000

    Bug 1552123 - debugger/devtools node test runners fail to detect jest snapshot failures. r=jdescottes
    Differential Revision: https://phabricator.services.mozilla.com/D31624
    
    UltraBlame original commit: e62a90a57da1f9e2e546376fe317c1850ca2f553

--- devtools/client/debugger/packages/devtools-utils/src/tests/network-request.js
+++ devtools/client/debugger/packages/devtools-utils/src/tests/network-request.js
@@ -47,11 +47,10 @@ describe("network request", () => {
   it("timed out fetch", async () => {
     global.fetch.mockImplementation(async () => {});
 
-    try {
-      await networkRequest("foo");
-    } catch (e) {
+    
+    networkRequest("foo").catch(e => {
       expect(e.message).toEqual("Connect timeout error");
-    }
+    });
 
     jest.runAllTimers();
   });
