774deba6f5cc3172986d0af4cc2f1b5fe49b4512
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 18:33:12 +0000

    Bug 1538731 - Add test for panel reloading with about:devtools-toolbox;r=daisuke
    Depends on D26899
    This test would timeout with the fix in the previous patch
    
    Differential Revision: https://phabricator.services.mozilla.com/D26900
    
    UltraBlame original commit: ff05581f5fe0125940697e9e3332bd84481dad98

--- devtools/client/aboutdebugging-new/test/browser/browser_aboutdebugging_devtoolstoolbox_reload.js
+++ devtools/client/aboutdebugging-new/test/browser/browser_aboutdebugging_devtoolstoolbox_reload.js
@@ -4,7 +4,22 @@
 "use strict";
 
 
+requestLongerTimeout(5);
+
+
 Services.scriptloader.loadSubScript(CHROME_URL_ROOT + "helper-collapsibilities.js", this);
+
+const TOOLS = [
+  "inspector",
+  "webconsole",
+  "jsdebugger",
+  "styleeditor",
+  "performance",
+  "memory",
+  "netmonitor",
+  "storage",
+  "accessibility",
+];
 
 
 
@@ -13,14 +28,20 @@ add_task(async function() {
   info("Force all debug target panes to be expanded");
   prepareCollapsibilitiesTest();
 
+  for (const toolId of TOOLS) {
+    await testReloadAboutDevToolsToolbox(toolId);
+  }
+});
+
+async function testReloadAboutDevToolsToolbox(toolId) {
   const { document, tab, window } = await openAboutDebugging();
   await selectThisFirefoxPage(document, window.AboutDebugging.store);
   const { devtoolsBrowser, devtoolsTab, devtoolsWindow } =
     await openAboutDevtoolsToolbox(document, tab, window);
 
-  info("Select webconsole tool");
+  info(`Select tool: ${toolId}`);
   const toolbox = getToolbox(devtoolsWindow);
-  await toolbox.selectTool("webconsole");
+  await toolbox.selectTool(toolId);
 
   info("Reload about:devtools-toolbox page");
   devtoolsBrowser.reload();
@@ -33,4 +54,4 @@ add_task(async function() {
 
   await closeAboutDevtoolsToolbox(document, devtoolsTab, window);
   await removeTab(tab);
-});
+}
