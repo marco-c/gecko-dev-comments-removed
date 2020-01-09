28fd04fdae9143b1254fa3270fc09e5e1c312afd
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 22:06:37 +0000

    Bug 1549762 - Don't build mozglue linker tests if building with icecream. r=glandium
    As icecream doesn't deal well with .incbin.
    
    Differential Revision: https://phabricator.services.mozilla.com/D30951
    
    UltraBlame original commit: 57ad9770bc01ffdbc52f841d8e437820a8a222f1

--- python/mozbuild/mozbuild/telemetry.py
+++ python/mozbuild/mozbuild/telemetry.py
@@ -218,10 +218,7 @@ def get_build_opts(substs):
         compiler = substs.get('CC_TYPE', None)
         if compiler:
             opts['compiler'] = str(compiler)
-        
-        
-        prefix = os.path.basename(substs.get('CCACHE_PREFIX', ''))
-        if substs.get('CXX_IS_ICECREAM', None) or prefix == 'icecc':
+        if substs.get('CXX_IS_ICECREAM', None):
             opts['icecream'] = True
         return opts
     except BuildEnvironmentNotFoundException:
