e1117b4e5294b7f96c18d9c29328dcbb61b4b516
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 14:07:08 +0000

    Bug 1527922 - Remove DevTools button in browser_toolbarKeyNav.js to prevent branch-specific focus in toolbar r=Gijs
    Differential Revision: https://phabricator.services.mozilla.com/D22876
    
    UltraBlame original commit: f21b07ac7fb7600419842f42992e119a57956e8d

--- browser/base/content/test/keyboard/browser_toolbarKeyNav.js
+++ browser/base/content/test/keyboard/browser_toolbarKeyNav.js
@@ -11,6 +11,10 @@
 
 const PERMISSIONS_PAGE = "https://example.com/browser/browser/base/content/test/permissions/permissions.html";
 
+
+
+CustomizableUI.removeWidgetFromArea("developer-button");
+
 async function expectFocusAfterKey(aKey, aFocus, aAncestorOk = false) {
   let res = aKey.match(/^(Shift\+)?(?:(.)|(.+))$/);
   let shift = Boolean(res[1]);
@@ -229,4 +233,8 @@ add_task(async function testArrowsOverflowButton() {
     document.getElementById("nav-bar-overflow-button").clientWidth;
     await expectFocusAfterKey("ArrowLeft", "sidebar-button");
   });
+});
+
+registerCleanupFunction(async function resetToolbar() {
+  await CustomizableUI.reset();
 });
