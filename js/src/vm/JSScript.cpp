321a5d68f7d1bb3018ed0e2ea6550bd6c4cdc9ec
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 14:15:07 +0000

    Bug 1533755 - Use RefPtr for SharedScriptData pointers. r=jandem
    Leave manual refcounting in the ScriptDataTable for now since it
    requires a bit of care to make the automatic types do the right thing
    when sweeping.
    
    Differential Revision: https://phabricator.services.mozilla.com/D22718
    
    UltraBlame original commit: 65c88218fbb26505573c73e2ff25fdf1a04d1a7d

--- js/src/vm/JSScript.cpp
+++ js/src/vm/JSScript.cpp
@@ -2908,39 +2908,16 @@ SharedScriptData* js::SharedScriptData::new_(JSContext* cx, uint32_t codeLength,
 
 inline js::ScriptBytecodeHasher::Lookup::Lookup(SharedScriptData* data)
     : scriptData(data),
-      hash(mozilla::HashBytes(scriptData->data(), scriptData->dataLength())) {
-  scriptData->incRefCount();
-}
-
-inline js::ScriptBytecodeHasher::Lookup::~Lookup() {
-  scriptData->decRefCount();
-}
+      hash(mozilla::HashBytes(scriptData->data(), scriptData->dataLength())) {}
 
 bool JSScript::createSharedScriptData(JSContext* cx, uint32_t codeLength,
                                       uint32_t noteLength, uint32_t natoms) {
-  MOZ_ASSERT(!scriptData());
-  SharedScriptData* ssd =
-      SharedScriptData::new_(cx, codeLength, noteLength, natoms);
-  if (!ssd) {
-    return false;
-  }
-
-  setScriptData(ssd);
-  return true;
-}
-
-void JSScript::freeScriptData() {
-  if (scriptData_) {
-    scriptData_->decRefCount();
-    scriptData_ = nullptr;
-  }
-}
-
-void JSScript::setScriptData(js::SharedScriptData* data) {
   MOZ_ASSERT(!scriptData_);
-  scriptData_ = data;
-  scriptData_->incRefCount();
+  scriptData_ = SharedScriptData::new_(cx, codeLength, noteLength, natoms);
+  return !!scriptData_;
 }
+
+void JSScript::freeScriptData() { scriptData_ = nullptr; }
 
 
 
@@ -2962,20 +2939,20 @@ bool JSScript::shareScriptData(JSContext* cx) {
   ScriptDataTable::AddPtr p = cx->scriptDataTable(lock).lookupForAdd(lookup);
   if (p) {
     MOZ_ASSERT(ssd != *p);
-    freeScriptData();
-    setScriptData(*p);
+    scriptData_ = *p;
   } else {
     if (!cx->scriptDataTable(lock).add(p, ssd)) {
-      freeScriptData();
       ReportOutOfMemory(cx);
       return false;
     }
 
     
-    scriptData()->incRefCount();
+    ssd->AddRef();
   }
 
+  
   MOZ_ASSERT(scriptData()->refCount() >= 2);
+
   return true;
 }
 
@@ -2989,7 +2966,7 @@ void js::SweepScriptData(JSRuntime* rt) {
   for (ScriptDataTable::Enum e(table); !e.empty(); e.popFront()) {
     SharedScriptData* scriptData = e.front();
     if (scriptData->refCount() == 1) {
-      scriptData->decRefCount();
+      scriptData->Release();
       e.removeFront();
     }
   }
@@ -3740,9 +3717,7 @@ void JSScript::finalize(FreeOp* fop) {
     fop->free_(data_);
   }
 
-  if (scriptData_) {
-    scriptData_->decRefCount();
-  }
+  freeScriptData();
 
   
   
@@ -4243,7 +4218,7 @@ JSScript* js::detail::CopyScript(JSContext* cx, HandleScript src,
   if (cx->zone() != src->zoneFromAnyThread()) {
     src->scriptData()->markForCrossZone(cx);
   }
-  dst->setScriptData(src->scriptData());
+  dst->scriptData_ = src->scriptData();
 
   return dst;
 }
