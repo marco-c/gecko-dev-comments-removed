ebb46f71bbbbf730a6a6abaa8eea877444c0b7a5
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 13:35:07 +0000

    Bug 1462018 - Add API for taking screenshot of visible page. r=snorp,geckoview-reviewers
    Invert screen pixels in `nsWindow`.
    
    Update `RequestScreenPixels` in `nsWindow` to accept a `GeckoResult` as an argument and save as a `GlobalRef`.
    Update `RecvScreenPixels` in `nsWindow` in order to invert the image before sending over JNI rather than requiring callers to invert the image themselves.
    Complete the `GeckoResult` if there is one in `RecvScreenPixels` instead of making a callback to the compositor.
    
    Remove `RecvScreenPixels` from `GeckoSession` and `GeckoSession.Compositor`.
    Move `RecvScreenPixels` and `getPixels` to `GeckoDisplay`
    Rename `getPixels` to `capturePixels`
    
    Return Bitmap from `RecvScreenPixels`.
    
    Return `GeckoResult` from `capturePixels`.
    
    Add doc comments to new methods and classes.
    Update FennecNativeDriver to use `GeckoView` and `GeckoResult`.
    
    Update API docs and Changelog
    
    Add tests
    
    Differential Revision: https://phabricator.services.mozilla.com/D21397
    
    UltraBlame original commit: 6c1f943e591c547b4c5c5ca642a449728e03b5ac

--- mobile/android/geckoview/src/main/java/org/mozilla/geckoview/GeckoView.java
+++ mobile/android/geckoview/src/main/java/org/mozilla/geckoview/GeckoView.java
@@ -17,6 +17,7 @@ import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Context;
 import android.content.res.Configuration;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Rect;
@@ -165,6 +166,22 @@ public class GeckoView extends FrameLayout {
 
         public boolean shouldPinOnScreen() {
             return mDisplay != null ? mDisplay.shouldPinOnScreen() : false;
+        }
+
+        
+
+
+
+
+
+
+        @UiThread
+        @NonNull GeckoResult<Bitmap> capturePixels() {
+            if (mDisplay == null) {
+                return GeckoResult.fromException(new IllegalStateException("Display must be created before pixels can be captured"));
+            }
+
+            return mDisplay.capturePixels();
         }
     }
 
@@ -708,5 +725,19 @@ public class GeckoView extends FrameLayout {
             }
         }
         mSession.getTextInput().autofill(strValues);
+    }
+
+    
+
+
+
+
+
+
+
+
+    @UiThread
+    public @NonNull GeckoResult<Bitmap> capturePixels() {
+        return mDisplay.capturePixels();
     }
 }
