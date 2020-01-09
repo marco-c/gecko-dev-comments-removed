1bd2f2a99ff306be69b62972b68e6d7f8e6b3287
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 15:39:31 +0000

    Backed out changeset e3c771005557 (bug 1527316) for timeout build bustages on a CLOSED TREE
    UltraBlame original commit: 6d60e6058ebfb78c8b3e2d64ef90b6e303b6d422

--- testing/xpcshell/runxpcshelltests.py
+++ testing/xpcshell/runxpcshelltests.py
@@ -1098,8 +1098,6 @@ class XPCShellTests(object):
         
         
         nodeBin = os.getenv('MOZ_NODE_PATH', None)
-        if not nodeBin and build:
-            nodeBin = build.substs.get('NODEJS')
         if not nodeBin:
             self.log.warning('MOZ_NODE_PATH environment variable not set. '
                              'Tests requiring http/2 will fail.')
