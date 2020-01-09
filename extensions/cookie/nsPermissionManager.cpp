ee498c53712e453dbcc149876a9445725bd59f63
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 00:27:19 +0000

    Bug 1520361 - Add Telemetry to know how people add site to the exception list. r=smaug,cpearce,janerik
    Add Telemetry to know the number of times user adds site to allow list, remove site from allow list, add site to block list and remove site from block list.
    
    Differential Revision: https://phabricator.services.mozilla.com/D16642
    
    UltraBlame original commit: 89875cd4d9422dd0f7a169184ac2633a6669a196

--- extensions/cookie/nsPermissionManager.cpp
+++ extensions/cookie/nsPermissionManager.cpp
@@ -732,6 +732,60 @@ static bool IsPersistentExpire(uint32_t aExpire) {
          aExpire != nsIPermissionManager::EXPIRE_POLICY;
 }
 
+static void UpdateAutoplayTelemetry(const nsCString& aType,
+                                    uint32_t aOldPermission,
+                                    uint32_t aNewPermission,
+                                    uint32_t aExpireType) {
+  if (!aType.EqualsLiteral("autoplay-media")) {
+    return;
+  }
+
+  if (aExpireType != nsIPermissionManager::EXPIRE_NEVER) {
+    return;
+  }
+
+  
+  if (aOldPermission == nsIPermissionManager::UNKNOWN_ACTION) {
+    if (aNewPermission == nsIPermissionManager::ALLOW_ACTION) {
+      AccumulateCategorical(
+          mozilla::Telemetry::LABELS_AUTOPLAY_SITES_SETTING_CHANGE::AddAllow);
+    } else if (aNewPermission == nsIPermissionManager::DENY_ACTION) {
+      AccumulateCategorical(
+          mozilla::Telemetry::LABELS_AUTOPLAY_SITES_SETTING_CHANGE::AddBlock);
+    }
+    return;
+  }
+
+  
+  if (aNewPermission == nsIPermissionManager::UNKNOWN_ACTION) {
+    if (aOldPermission == nsIPermissionManager::ALLOW_ACTION) {
+      AccumulateCategorical(
+          mozilla::Telemetry::LABELS_AUTOPLAY_SITES_SETTING_CHANGE::
+              RemoveAllow);
+    } else if (aOldPermission == nsIPermissionManager::DENY_ACTION) {
+      AccumulateCategorical(
+          mozilla::Telemetry::LABELS_AUTOPLAY_SITES_SETTING_CHANGE::
+              RemoveBlock);
+    }
+    return;
+  }
+
+  
+  if (aNewPermission == nsIPermissionManager::ALLOW_ACTION &&
+      aOldPermission == nsIPermissionManager::DENY_ACTION) {
+    AccumulateCategorical(
+        mozilla::Telemetry::LABELS_AUTOPLAY_SITES_SETTING_CHANGE::AddAllow);
+    AccumulateCategorical(
+        mozilla::Telemetry::LABELS_AUTOPLAY_SITES_SETTING_CHANGE::RemoveBlock);
+  } else if (aNewPermission == nsIPermissionManager::DENY_ACTION &&
+             aOldPermission == nsIPermissionManager::ALLOW_ACTION) {
+    AccumulateCategorical(
+        mozilla::Telemetry::LABELS_AUTOPLAY_SITES_SETTING_CHANGE::AddBlock);
+    AccumulateCategorical(
+        mozilla::Telemetry::LABELS_AUTOPLAY_SITES_SETTING_CHANGE::RemoveAllow);
+  }
+}
+
 }  
 
 
@@ -1817,6 +1871,10 @@ nsresult nsPermissionManager::AddInternal(
     }
 
     case eOperationAdding: {
+      UpdateAutoplayTelemetry(aType,
+                              nsIPermissionManager::UNKNOWN_ACTION,
+                              aPermission,
+                              aExpireType);
       if (aDBOperation == eWriteToDB) {
         
         id = ++mLargestID;
@@ -1860,6 +1918,10 @@ nsresult nsPermissionManager::AddInternal(
         break;
       }
 
+      UpdateAutoplayTelemetry(aType,
+                              oldPermissionEntry.mPermission,
+                              nsIPermissionManager::UNKNOWN_ACTION,
+                              aExpireType);
       entry->GetPermissions().RemoveElementAt(index);
 
       
@@ -1898,6 +1960,11 @@ nsresult nsPermissionManager::AddInternal(
         NS_WARNING("Attempting to modify EXPIRE_POLICY permission");
         break;
       }
+
+      UpdateAutoplayTelemetry(aType,
+                              entry->GetPermissions()[index].mPermission,
+                              aPermission,
+                              aExpireType);
 
       
       
