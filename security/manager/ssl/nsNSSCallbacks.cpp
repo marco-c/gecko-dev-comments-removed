b4e8d29fae8949911984f6d8236d8a270ff103df
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 20:31:28 +0000

    Bug 1547701 - Use LOAD_BYPASS_URL_ClASSIFIER flag for OCSP request. r=keeler
    If OCSP request is blocked, we can't get the certificate revocation
    informatoin.
    Add nsIChannel::LOAD_BYPASS_URL_ClASSIFIER to enfore URL classifier
    bypasses OCSP request.
    
    Differential Revision: https://phabricator.services.mozilla.com/D29230
    
    UltraBlame original commit: aa674b410265213ab67b295f854ef353613a83e3

--- security/manager/ssl/nsNSSCallbacks.cpp
+++ security/manager/ssl/nsNSSCallbacks.cpp
@@ -262,7 +262,8 @@ OCSPRequest::Run() {
   }
 
   channel->SetLoadFlags(nsIRequest::LOAD_ANONYMOUS |
-                        nsIChannel::LOAD_BYPASS_SERVICE_WORKER);
+                        nsIChannel::LOAD_BYPASS_SERVICE_WORKER |
+                        nsIChannel::LOAD_BYPASS_URL_CLASSIFIER);
 
   
   
