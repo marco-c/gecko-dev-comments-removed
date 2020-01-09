ee04f28cd6b0ed5c05a865cd979ed72168c4ac8d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 16:37:43 +0000

    Bug 1537977 - Add a ManagedEndpoint IPDL type for manually constructing actors, r=mccr8
    Differential Revision: https://phabricator.services.mozilla.com/D24425
    
    UltraBlame original commit: 8cde0ca01548efbce3d08620f0831acd6af26938

--- ipc/glue/ProtocolUtils.h
+++ ipc/glue/ProtocolUtils.h
@@ -871,6 +871,61 @@ nsresult CreateEndpoints(const PrivateIPDLInterface& aPrivate,
   return NS_OK;
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+template <class PFooSide>
+class ManagedEndpoint {
+ public:
+  ManagedEndpoint() : mId(0) {}
+
+  ManagedEndpoint(const PrivateIPDLInterface&, int32_t aId) : mId(aId) {}
+
+  ManagedEndpoint(ManagedEndpoint&& aOther) : mId(aOther.mId) {
+    aOther.mId = 0;
+  }
+
+  ManagedEndpoint& operator=(ManagedEndpoint&& aOther) {
+    mId = aOther.mId;
+    aOther.mId = 0;
+    return *this;
+  }
+
+  bool IsValid() const { return mId != 0; }
+
+  Maybe<int32_t> ActorId() const { return IsValid() ? Some(mId) : Nothing(); }
+
+ private:
+  friend struct IPC::ParamTraits<ManagedEndpoint<PFooSide>>;
+
+  ManagedEndpoint(const ManagedEndpoint&) = delete;
+  ManagedEndpoint& operator=(const ManagedEndpoint&) = delete;
+
+  
+  int32_t mId;
+
+  
+  
+};
+
 void TableToArray(const nsTHashtable<nsPtrHashKey<void>>& aTable,
                   nsTArray<void*>& aArray);
 
@@ -993,6 +1048,29 @@ struct ParamTraits<mozilla::ipc::Endpoint<PFooSide>> {
 
   static void Log(const paramType& aParam, std::wstring* aLog) {
     aLog->append(StringPrintf(L"Endpoint"));
+  }
+};
+
+template <class PFooSide>
+struct ParamTraits<mozilla::ipc::ManagedEndpoint<PFooSide>> {
+  typedef mozilla::ipc::ManagedEndpoint<PFooSide> paramType;
+
+  static void Write(Message* aMsg, const paramType& aParam) {
+    IPC::WriteParam(aMsg, aParam.mId);
+  }
+
+  static bool Read(const Message* aMsg, PickleIterator* aIter,
+                   paramType* aResult) {
+    MOZ_RELEASE_ASSERT(aResult->mId == 0);
+
+    if (!IPC::ReadParam(aMsg, aIter, &aResult->mId)) {
+      return false;
+    }
+    return true;
+  }
+
+  static void Log(const paramType& aParam, std::wstring* aLog) {
+    aLog->append(StringPrintf(L"ManagedEndpoint"));
   }
 };
 
