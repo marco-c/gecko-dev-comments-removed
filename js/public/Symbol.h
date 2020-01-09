e47e5a53ee87e00f78777fadeed7a7418c3d5f88
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 21:50:30 +0000

    Bug 1549340 - Part 2: Use union instead of reinterpret_cast to initialize const char* with symbol for JSPropertySpec.name. r=jandem
    Differential Revision: https://phabricator.services.mozilla.com/D30493
    
    UltraBlame original commit: 8a86e2d47ce87821583188fd222e6e402bad2a28

--- js/public/Symbol.h
+++ js/public/Symbol.h
@@ -103,9 +103,8 @@ extern JS_PUBLIC_API Symbol* GetWellKnownSymbol(JSContext* cx,
 
 
 
-inline bool PropertySpecNameIsSymbol(const char* name) {
-  uintptr_t u = reinterpret_cast<uintptr_t>(name);
-  return u != 0 && u - 1 < WellKnownSymbolLimit;
+inline bool PropertySpecNameIsSymbol(uintptr_t name) {
+  return name != 0 && name - 1 < WellKnownSymbolLimit;
 }
 
 }  
