dc21597b743d4d6a497a62d9f0c35dad7a3320f6
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 01:22:05 +0000

    Bug 1521826 - Part 1: Sort commands by a shortcut being set r=Gijs
    Commands that have a shortcut will be listed first in the extension shortcut UI.
    
    Differential Revision: https://phabricator.services.mozilla.com/D17877
    
    UltraBlame original commit: 2a2cbeecf1ca9cc4f56712b4346043acdac109a0

--- toolkit/mozapps/extensions/content/shortcuts.js
+++ toolkit/mozapps/extensions/content/shortcuts.js
@@ -273,6 +273,14 @@ async function renderAddons(addons) {
 
     if (extension.shortcuts) {
       let commands = await extension.shortcuts.allCommands();
+      commands.sort((a, b) => {
+        
+        if (!a.shortcut == !b.shortcut)
+          return 0;
+        if (a.shortcut)
+          return -1;
+        return 1;
+      });
 
       for (let command of commands) {
         let row = document.importNode(templates.row.content, true);
