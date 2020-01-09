ca3bee8dd01666d850aea98762ba4cec841d389d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 21:54:57 +0000

    Bug 1534451 - Send Mobile Activation Telemetry ping. r=JanH
    Differential Revision: https://phabricator.services.mozilla.com/D29668
    
    UltraBlame original commit: 91bc05242c6f02a5d30c71557693129303e7067e

--- mobile/android/base/java/org/mozilla/gecko/telemetry/TelemetryUploadService.java
+++ mobile/android/base/java/org/mozilla/gecko/telemetry/TelemetryUploadService.java
@@ -19,6 +19,7 @@ import org.mozilla.gecko.sync.ExtendedJSONObject;
 import org.mozilla.gecko.sync.net.BaseResource;
 import org.mozilla.gecko.sync.net.BaseResourceDelegate;
 import org.mozilla.gecko.sync.net.Resource;
+import org.mozilla.gecko.telemetry.pingbuilders.TelemetryActivationPingBuilder;
 import org.mozilla.gecko.telemetry.stores.TelemetryPingStore;
 import org.mozilla.gecko.util.DateUtil;
 import org.mozilla.gecko.util.NetworkUtils;
@@ -125,6 +126,8 @@ public class TelemetryUploadService extends JobIntentService {
             if (delegate.hadConnectionError()) {
                 break;
             }
+
+            checkPingsPersistence(context, ping.getDocID());
         }
 
         final boolean wereAllUploadsSuccessful = !delegate.hadConnectionError();
@@ -134,6 +137,23 @@ public class TelemetryUploadService extends JobIntentService {
         }
         store.onUploadAttemptComplete(successfulUploadIDs);
         return wereAllUploadsSuccessful;
+    }
+
+    
+
+
+
+
+    private static void checkPingsPersistence(Context context, String successfulUploadID) {
+        final String activationID = TelemetryActivationPingBuilder.getActivationPingId(context);
+
+        if(activationID == null) {
+            return;
+        }
+
+        if(activationID.equals(successfulUploadID)) {
+            TelemetryActivationPingBuilder.setActivationPingSent(context, true);
+        }
     }
 
     private static void uploadPayload(final String url, final ExtendedJSONObject payload, final ResultDelegate delegate) {
