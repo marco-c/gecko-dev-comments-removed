d16ed7a66579f3d36026a8b0d665b3c04f7ee433
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 17:10:45 +0000

    Bug 1520339 - Part 2: handling memory reporting for VR process. r=kip,erahm
    Differential Revision: https://phabricator.services.mozilla.com/D25321
    
    UltraBlame original commit: 31c1b09faec49bde81d6cb798f930e95b70f6141

--- gfx/vr/ipc/VRProcessManager.h
+++ gfx/vr/ipc/VRProcessManager.h
@@ -9,6 +9,7 @@
 #include "VRProcessParent.h"
 
 namespace mozilla {
+class MemoryReportingProcess;
 namespace gfx {
 
 class VRManagerChild;
@@ -37,6 +38,9 @@ class VRProcessManager final : public VRProcessParent::Listener {
                         mozilla::ipc::Endpoint<PVRGPUChild>* aOutVRBridge);
 
   VRChild* GetVRChild();
+  
+  
+  RefPtr<MemoryReportingProcess> GetProcessMemoryReporter();
 
   virtual void OnProcessLaunchComplete(VRProcessParent* aParent) override;
   virtual void OnProcessUnexpectedShutdown(VRProcessParent* aParent) override;
