34f62adfaeb3473cfdded5c912f83d63b644dcc9
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 17:40:31 +0000

    Bug 1537622 - add rudimentary support for linting XUL files, r=Standard8
    Differential Revision: https://phabricator.services.mozilla.com/D24298
    
    UltraBlame original commit: 3bf1176d151b6d5d2b6a84b41b3181af9da3c50b

--- tools/lint/eslint/eslint-plugin-mozilla/lib/processors/xbl-bindings.js
+++ tools/lint/eslint/eslint-plugin-mozilla/lib/processors/xbl-bindings.js
@@ -11,99 +11,7 @@
 
 const NS_XBL = "http://www.mozilla.org/xbl";
 
-let sax = require("sax");
-
-
-let errorRegex = /(.*)\nLine: (\d+)\nColumn: (\d+)\nChar: (.*)/;
-function parseError(err) {
-  let matches = err.message.match(errorRegex);
-  if (!matches) {
-    return null;
-  }
-
-  return {
-    fatal: true,
-    message: matches[1],
-    line: parseInt(matches[2]) + 1,
-    column: parseInt(matches[3]),
-  };
-}
-
-let entityRegex = /&[\w][\w-\.]*;/g;
-
-
-function XMLParser(parser) {
-  this.parser = parser;
-  parser.onopentag = this.onOpenTag.bind(this);
-  parser.onclosetag = this.onCloseTag.bind(this);
-  parser.ontext = this.onText.bind(this);
-  parser.onopencdata = this.onOpenCDATA.bind(this);
-  parser.oncdata = this.onCDATA.bind(this);
-  parser.oncomment = this.onComment.bind(this);
-
-  this.document = {
-    local: "#document",
-    uri: null,
-    children: [],
-    comments: [],
-  };
-  this._currentNode = this.document;
-}
-
-XMLParser.prototype = {
-  parser: null,
-
-  onOpenTag(tag) {
-    let node = {
-      parentNode: this._currentNode,
-      local: tag.local,
-      namespace: tag.uri,
-      attributes: {},
-      children: [],
-      comments: [],
-      textContent: "",
-      textLine: this.parser.line,
-      textColumn: this.parser.column,
-      textEndLine: this.parser.line,
-    };
-
-    for (let attr of Object.keys(tag.attributes)) {
-      if (tag.attributes[attr].uri == "") {
-        node.attributes[attr] = tag.attributes[attr].value;
-      }
-    }
-
-    this._currentNode.children.push(node);
-    this._currentNode = node;
-  },
-
-  onCloseTag(tagname) {
-    this._currentNode.textEndLine = this.parser.line;
-    this._currentNode = this._currentNode.parentNode;
-  },
-
-  addText(text) {
-    this._currentNode.textContent += text;
-  },
-
-  onText(text) {
-    
-    this.addText(text.replace(entityRegex, "null"));
-  },
-
-  onOpenCDATA() {
-    
-    this.addText(" ".repeat("<![CDATA[".length));
-  },
-
-  onCDATA(text) {
-    this.addText(text);
-  },
-
-  onComment(text) {
-    this._currentNode.comments.push(text);
-  },
-};
+let XMLParser = require("./processor-helpers").XMLParser;
 
 
 
@@ -204,20 +112,10 @@ module.exports = {
     scriptLines = [];
     lineMap = [];
 
-    
-    
-    
-    let parser = sax.parser(false, {
-      lowercase: true,
-      xmlns: true,
-    });
-
-    parser.onerror = function(err) {
-      xmlParseError = parseError(err);
-    };
-
-    let xp = new XMLParser(parser);
-    parser.write(text);
+    let xp = new XMLParser(text);
+    if (xp.lastError) {
+      xmlParseError = xp.lastError;
+    }
 
     
     let document = xp.document;
