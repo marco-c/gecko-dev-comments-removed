4bce2efdadc1acd6885e9ce1331abb68b361085c
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 20:54:13 +0000

    Backed out 3 changesets (bug 1525640) for frequent wpt failures CLOSED TREE
    Backed out changeset d9a454afe9bf (bug 1525640)
    Backed out changeset 19aa991328cb (bug 1525640)
    Backed out changeset 4294abed890f (bug 1525640)
    
    UltraBlame original commit: 3be80123e3af0bf2aece853e560891b3163e5d7a

--- netwerk/protocol/http/nsHttpConnectionInfo.h
+++ netwerk/protocol/http/nsHttpConnectionInfo.h
@@ -134,11 +134,8 @@ class nsHttpConnectionInfo final : public ARefBase {
   uint32_t GetTlsFlags() const { return mTlsFlags; }
 
   
-  
-  void SetIsTrrServiceChannel(bool aIsTRRChannel) {
-    mIsTrrServiceChannel = aIsTRRChannel;
-  }
-  bool GetIsTrrServiceChannel() const { return mIsTrrServiceChannel; }
+  void SetTrrUsed(bool aUsed) { mTrrUsed = aUsed; }
+  bool GetTrrUsed() const { return mTrrUsed; }
 
   
   
@@ -225,7 +222,7 @@ class nsHttpConnectionInfo final : public ARefBase {
 
   uint32_t mTlsFlags;
   uint16_t mIsolated : 1;
-  uint16_t mIsTrrServiceChannel : 1;
+  uint16_t mTrrUsed : 1;
   uint16_t mTrrDisabled : 1;
   uint16_t mIPv4Disabled : 1;
   uint16_t mIPv6Disabled : 1;
