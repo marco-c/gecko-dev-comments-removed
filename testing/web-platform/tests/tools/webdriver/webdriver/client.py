9bca6e54c3d772c71b663625ecf49d5bec67df81
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 09:40:56 +0000

    Bug 1517736 [wpt PR 14715] - Prevent webdriver client Session.close() exception, a=testonly
    Automatic update from web-platform-tests
    webdriver: prevent Session.close() exception (#14715)
    
    This change adds handling for the case when the call to
    self.send_session_command("DELETE", "window") in Session.close() in
    tools/webdriver/webdriver/client.py returns None.
    
    Otherwise without this change, the code ends up checking len() on the
    return value of self.send_session_command("DELETE", "window"), which can
    result in a "TypeError: object of type 'NoneType' has no len()" exception.
    
    Fixes https://github.com/web-platform-tests/wpt/issues/14714
    --
    
    wpt-commits: e90f4c8bdba3cb1183c187d269b0a3dbbd5ee8e1
    wpt-pr: 14715
    
    UltraBlame original commit: f551b250bc43d9542baf1a2689cb29a4ed9265d9

--- testing/web-platform/tests/tools/webdriver/webdriver/client.py
+++ testing/web-platform/tests/tools/webdriver/webdriver/client.py
@@ -566,7 +566,7 @@ class Session(object):
     @command
     def close(self):
         handles = self.send_session_command("DELETE", "window")
-        if len(handles) == 0:
+        if handles is not None and len(handles) == 0:
             
             self.session_id = None
 
