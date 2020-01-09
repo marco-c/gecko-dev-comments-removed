00bbfc28ab4a0eb38016ec2027d5233c81039684
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 23:52:17 +0000

    Bug 1512432 - convert richlistitem binding to Custom Element, r=bgrins
    Differential Revision: https://phabricator.services.mozilla.com/D32474
    
    UltraBlame original commit: e09471d136599b68f40f6bb9ec7fcc419732948e

--- toolkit/content/widgets/richlistbox.js
+++ toolkit/content/widgets/richlistbox.js
@@ -981,24 +981,13 @@ MozElements.MozRichlistitem = class MozRichlistitem extends MozElements.BaseText
   get current() {
     return this.getAttribute("current") == "true";
   }
-  disconnectedCallback() {
-    var control = this.control;
-    if (!control)
-      return;
-    
-    
-    
-    if (this.selected) {
-      var suppressSelect = control._suppressOnSelect;
-      control._suppressOnSelect = true;
-      control.removeItemFromSelection(this);
-      control._suppressOnSelect = suppressSelect;
-    }
-    if (this.current)
-      control.currentItem = null;
-  }
 };
 
 MozXULElement.implementCustomInterface(
   MozElements.MozRichlistitem, [Ci.nsIDOMXULSelectControlItemElement]
 );
+
+
+if (document.documentURI != "about:addons") {
+  customElements.define("richlistitem", MozElements.MozRichlistitem);
+}
