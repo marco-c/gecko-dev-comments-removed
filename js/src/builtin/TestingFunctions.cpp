21dc8613f66c5169a0264cef2386a1df14d20409
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 01:37:51 +0000

    Bug 1518377 - Fix the way isIon accesses current script r=nbp
    Existing code didn't handle IonIC frames, but the
    FrameScriptIter::script function does the right thing for us here.
    
    Differential Revision: https://phabricator.services.mozilla.com/D18151
    
    UltraBlame original commit: 56023d83552d08ef93b28f20fa3ea72e7eeb08f1

--- js/src/builtin/TestingFunctions.cpp
+++ js/src/builtin/TestingFunctions.cpp
@@ -2693,22 +2693,25 @@ static bool testingFunc_inIon(JSContext* cx, unsigned argc, Value* vp) {
     return true;
   }
 
+  JSScript* script = cx->currentScript();
+
+  
   ScriptFrameIter iter(cx);
   if (!iter.done() && iter.isIon()) {
     
-    jit::JSJitFrameIter jitIter(cx->activation()->asJit());
-    ++jitIter;
-    jitIter.script()->resetWarmUpResetCounter();
-  } else {
-    
-    JSScript* script = cx->currentScript();
-    if (script && script->getWarmUpResetCount() >= 20) {
-      return ReturnStringCopy(
-          cx, args, "Compilation is being repeatedly prevented. Giving up.");
-    }
+    script->resetWarmUpResetCounter();
+
+    args.rval().setBoolean(true);
+    return true;
   }
 
-  args.rval().setBoolean(!iter.done() && iter.isIon());
+  
+  if (script && script->getWarmUpResetCount() >= 20) {
+    return ReturnStringCopy(
+        cx, args, "Compilation is being repeatedly prevented. Giving up.");
+  }
+
+  args.rval().setBoolean(false);
   return true;
 }
 
