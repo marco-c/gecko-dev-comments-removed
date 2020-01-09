bcb3956339228dc6b83cc4c9f912ee6df4c23182
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 19:25:20 +0000

    Bug 1536121 - cleanup display list code a little bit to prep for refactor. r=gw
    * make all enums repr(u8) (compiler bug blocking this long fixed)
    * add display list stats feature
    * remove cache markers (abandoned design)
    * don't always push empty SetFilters before PushStackingContext
    * remove dead pub methods
    
    Differential Revision: https://phabricator.services.mozilla.com/D25845
    
    UltraBlame original commit: 43003251156190b170a66b2c213d94d7d02cbf10

--- gfx/wr/webrender/src/glyph_rasterizer/no_pathfinder.rs
+++ gfx/wr/webrender/src/glyph_rasterizer/no_pathfinder.rs
@@ -58,7 +58,7 @@ impl GlyphRasterizer {
         
         for key in glyph_keys {
             match glyph_key_cache.entry(key.clone()) {
-                Entry::Occupied(mut entry) => {
+                Entry::Occupied(entry) => {
                     let value = entry.into_mut();
                     match *value {
                         GlyphCacheEntry::Cached(ref glyph) => {
