48559042b31162f9ae842334e246af131cd64a10
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 01:27:05 +0000

    Bug 1524005 - Inspect Element throwing on Anonymous nodes and content missing from Markup view r=rcaliman
    Differential Revision: https://phabricator.services.mozilla.com/D18106
    
    UltraBlame original commit: 22c5f8ae131e3c3a2328b2108aa7042fb55138af

--- devtools/server/actors/inspector/event-collector.js
+++ devtools/server/actors/inspector/event-collector.js
@@ -10,6 +10,7 @@
 const { Cu } = require("chrome");
 const Services = require("Services");
 const makeDebugger = require("devtools/server/actors/utils/make-debugger");
+const { isAnonymous } = require("devtools/shared/layout/utils");
 
 
 const JQUERY_LIVE_REGEX = /return typeof \w+.*.event\.triggered[\s\S]*\.event\.(dispatch|handle).*arguments/;
@@ -346,7 +347,9 @@ class JQueryEventCollector extends MainEventCollector {
     const jQuery = this.getJQuery(node);
     const handlers = [];
 
-    if (!jQuery) {
+    
+    
+    if (!jQuery || isAnonymous(node)) {
       if (checkOnly) {
         return false;
       }
