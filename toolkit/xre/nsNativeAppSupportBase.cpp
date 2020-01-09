205c2f920a24be2037bf0ecd29d16c61dca6a7ba
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 13:45:32 +0000

    Bug 513742: Remove support for DDE from toolkit. r=jimm
    Differential Revision: https://phabricator.services.mozilla.com/D19075
    
    UltraBlame original commit: 61ed2a0798487c50c5faf70241aa637d01da6b10

--- toolkit/xre/nsNativeAppSupportBase.cpp
+++ toolkit/xre/nsNativeAppSupportBase.cpp
@@ -21,13 +21,6 @@ nsNativeAppSupportBase::Start(bool *result) {
 NS_IMETHODIMP
 nsNativeAppSupportBase::Enable() { return NS_OK; }
 
-
-NS_IMETHODIMP
-nsNativeAppSupportBase::Stop(bool *result) {
-  *result = true;
-  return NS_OK;
-}
-
 NS_IMETHODIMP
 nsNativeAppSupportBase::Quit() { return NS_OK; }
 
