210fd090e8d08d842acffa98e77efd84efbf16d4
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 18:43:06 +0000

    Bug 1543449 - use a minPasswordLength rather than skipEmptyFields property when collecting password fields. r=jaws
    Differential Revision: https://phabricator.services.mozilla.com/D27202
    
    UltraBlame original commit: 127dba490969122ab2c7523bc98db6f739d95f0a

--- toolkit/components/passwordmgr/test/unit/test_getPasswordFields.js
+++ toolkit/components/passwordmgr/test/unit/test_getPasswordFields.js
@@ -12,20 +12,20 @@ const TESTCASES = [
     description: "Empty document",
     document: ``,
     returnedFieldIDsByFormLike: [],
-    skipEmptyFields: undefined,
+    minPasswordLength: undefined,
   },
   {
     description: "Non-password input with no <form> present",
     document: `<input>`,
     
     returnedFieldIDsByFormLike: [[]],
-    skipEmptyFields: undefined,
+    minPasswordLength: undefined,
   },
   {
     description: "1 password field outside of a <form>",
     document: `<input id="pw1" type=password>`,
     returnedFieldIDsByFormLike: [["pw1"]],
-    skipEmptyFields: undefined,
+    minPasswordLength: undefined,
   },
   {
     description: "4 empty password fields outside of a <form>",
@@ -34,78 +34,98 @@ const TESTCASES = [
       <input id="pw3" type=password>
       <input id="pw4" type=password>`,
     returnedFieldIDsByFormLike: [[]],
-    skipEmptyFields: undefined,
+    minPasswordLength: undefined,
   },
   {
-    description: "4 password fields outside of a <form> (1 empty, 3 full) with skipEmpty",
+    description: "4 password fields outside of a <form> (1 empty, 3 full) with minPasswordLength=2",
     document: `<input id="pw1" type=password>
       <input id="pw2" type=password value="pass2">
       <input id="pw3" type=password value="pass3">
       <input id="pw4" type=password value="pass4">`,
     returnedFieldIDsByFormLike: [["pw2", "pw3", "pw4"]],
-    skipEmptyFields: true,
+    minPasswordLength: 2,
   },
   {
     description: "Form with 1 password field",
     document: `<form><input id="pw1" type=password></form>`,
     returnedFieldIDsByFormLike: [["pw1"]],
-    skipEmptyFields: undefined,
+    minPasswordLength: undefined,
   },
   {
     description: "Form with 2 password fields",
     document: `<form><input id="pw1" type=password><input id='pw2' type=password></form>`,
     returnedFieldIDsByFormLike: [["pw1", "pw2"]],
-    skipEmptyFields: undefined,
+    minPasswordLength: undefined,
   },
   {
     description: "1 password field in a form, 1 outside",
     document: `<form><input id="pw1" type=password></form><input id="pw2" type=password>`,
     returnedFieldIDsByFormLike: [["pw1"], ["pw2"]],
-    skipEmptyFields: undefined,
+    minPasswordLength: undefined,
   },
   {
     description: "2 password fields outside of a <form> with 1 linked via @form",
     document: `<input id="pw1" type=password><input id="pw2" type=password form='form1'>
       <form id="form1"></form>`,
     returnedFieldIDsByFormLike: [["pw1"], ["pw2"]],
-    skipEmptyFields: undefined,
+    minPasswordLength: undefined,
   },
   {
-    description: "2 password fields outside of a <form> with 1 linked via @form + skipEmpty",
+    description: "2 password fields outside of a <form> with 1 linked via @form + minPasswordLength",
     document: `<input id="pw1" type=password><input id="pw2" type=password form="form1">
       <form id="form1"></form>`,
     returnedFieldIDsByFormLike: [[], []],
-    skipEmptyFields: true,
+    minPasswordLength: 2,
   },
   {
-    description: "skipEmptyFields should also skip white-space only fields",
+    description: "minPasswordLength should also skip white-space only fields",
     
     document: `<input id="pw-space" type=password value=" ">
                <input id="pw-tab" type=password value="	">
                <input id="pw-newline" type=password form="form1" value="
-">
+                                                                        ">
       <form id="form1"></form>`,
     
     returnedFieldIDsByFormLike: [[], []],
-    skipEmptyFields: true,
+    minPasswordLength: 2,
   },
   {
-    description: "2 password fields outside of a <form> with 1 linked via @form + skipEmpty with 1 empty",
+    description: "minPasswordLength should skip too-short field values",
+    document: `<form>
+               <input id="pw-empty" type=password>
+               <input id="pw-tooshort" type=password value="p">
+               <input id="pw" type=password value="pz">
+               </form>`,
+    returnedFieldIDsByFormLike: [["pw"]],
+    minPasswordLength: 2,
+  },
+  {
+    description: "minPasswordLength should allow matching-length field values",
+    document: `<form>
+               <input id="pw-empty" type=password>
+               <input id="pw-matchlen" type=password value="pz">
+               <input id="pw" type=password value="pazz">
+               </form>`,
+    returnedFieldIDsByFormLike: [["pw-matchlen", "pw"]],
+    minPasswordLength: 2,
+  },
+  {
+    description: "2 password fields outside of a <form> with 1 linked via @form + minPasswordLength with 1 empty",
     document: `<input id="pw1" type=password value=" pass1 "><input id="pw2" type=password form="form1">
       <form id="form1"></form>`,
     returnedFieldIDsByFormLike: [["pw1"], []],
-    skipEmptyFields: true,
+    minPasswordLength: 2,
     fieldOverrideRecipe: {
       
       hosts: ["localhost:8080"],
     },
   },
   {
-    description: "3 password fields outside of a <form> with 1 linked via @form + skipEmpty",
+    description: "3 password fields outside of a <form> with 1 linked via @form + minPasswordLength",
     document: `<input id="pw1" type=password value="pass1"><input id="pw2" type=password form="form1" value="pass2"><input id="pw3" type=password value="pass3">
       <form id="form1"><input id="pw4" type=password></form>`,
     returnedFieldIDsByFormLike: [["pw3"], ["pw2"]],
-    skipEmptyFields: true,
+    minPasswordLength: 2,
     fieldOverrideRecipe: {
       hosts: ["localhost:8080"],
       notPasswordSelector: "#pw1",
@@ -118,7 +138,7 @@ const TESTCASES = [
     description: "1 password field outside of a <form> which gets removed/disconnected",
     document: `<input id="pw1" type=password>`,
     returnedFieldIDsByFormLike: [[]],
-    skipEmptyFields: undefined,
+    minPasswordLength: undefined,
   },
 ];
 
@@ -158,7 +178,7 @@ for (let tc of TESTCASES) {
         formLikeIndex++;
         let pwFields = LoginManagerContent._getPasswordFields(formLikeFromInput, {
           fieldOverrideRecipe: testcase.fieldOverrideRecipe,
-          skipEmptyFields: testcase.skipEmptyFields,
+          minPasswordLength: testcase.minPasswordLength,
         });
 
         if (ChromeUtils.getClassName(formLikeFromInput.rootElement) === "HTMLFormElement") {
@@ -184,6 +204,50 @@ for (let tc of TESTCASES) {
                              "Check password field " + i + " ID");
         }
       }
+    });
+  })();
+}
+
+const EMOJI_TESTCASES = [
+  {
+    description: "Single characters composed of 2 code units should ideally fail minPasswordLength of 2",
+    document: `<form>
+               <input id="pw" type=password value="💩">
+               </form>`,
+    returnedFieldIDsByFormLike: [["pw"]],
+    minPasswordLength: 2,
+  },
+  {
+    description: "Single characters composed of multiple code units should ideally fail minPasswordLength of 2",
+    document: `<form>
+               <input id="pw" type=password value="👪">
+               </form>`,
+    minPasswordLength: 2,
+  },
+];
+
+
+
+for (let tc of EMOJI_TESTCASES) {
+  info("Sanity checking the testcase: " + tc.description);
+
+  (function() {
+    let testcase = tc;
+    add_task(async function() {
+      info("Starting testcase: " + testcase.description);
+      let document = MockDocument.createTestDocument("http://localhost:8080/test/",
+                                                     testcase.document);
+      let input = document.querySelector("input[type='password']");
+      Assert.ok(input, "Found the password field");
+      let formLike = LoginFormFactory.createFromField(input);
+      let pwFields = LoginManagerContent._getPasswordFields(formLike, {
+        minPasswordLength: testcase.minPasswordLength,
+      });
+      info("Got password fields: " + pwFields.length);
+      todo_check_eq(
+        pwFields.length, 0,
+        "Check a single-character (emoji) password is excluded from the password fields collection"
+      );
     });
   })();
 }
