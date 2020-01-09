65f724ce9ff334c90a5356492df1ca1e263aaa61
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 23:13:24 +0000

    Bug 1553593 - Allow the searchplugins stat call to happen later with WebRender. r=florian
    Differential Revision: https://phabricator.services.mozilla.com/D32471
    
    UltraBlame original commit: 00fe5cbd527fcf3d60ee5c5b98e18291835d2727

--- browser/base/content/test/performance/browser_startup_mainthreadio.js
+++ browser/base/content/test/performance/browser_startup_mainthreadio.js
@@ -530,6 +530,7 @@ const startupPhases = {
     { 
       path: "XREAppDist:searchplugins",
       condition: WIN,
+      ignoreIfUnused: true, 
       stat: 1,
     },
     {
@@ -642,6 +643,12 @@ const startupPhases = {
       path: "XCurProcD:omni.ja",
       condition: WIN,
       stat: 7,
+    },
+    { 
+      path: "XREAppDist:searchplugins",
+      condition: WIN,
+      ignoreIfUnused: true, 
+      stat: 1,
     },
   ],
 };
