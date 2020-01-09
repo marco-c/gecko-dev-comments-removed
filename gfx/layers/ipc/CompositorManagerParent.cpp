9f02d7af2cbacdd1016ac8502d45c56a9e9c4d77
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 18:48:55 +0000

    Bug 1543560 - Move NotifyWebRenderError() to CompositorManagerParent r=nical
    NotifyWebRenderError is sent via CompositorBridgeParent/CompositorBridgeChild. It is not good, since they are re-created for each nsBaseWidget::CreateCompositorSession() call. Then there could be a case that the NotifyWebRenderError is not notified to GPUProcessManager and a crash was caused by NotifyWebRenderError message during ipc close.  CompositorManagerParent/CompositorManagerChild are stable for sending NotifyWebRenderError.
    
    Differential Revision: https://phabricator.services.mozilla.com/D27030
    
    UltraBlame original commit: 719725292e4cafffb19ff16688879875d4053b0e

--- gfx/layers/ipc/CompositorManagerParent.cpp
+++ gfx/layers/ipc/CompositorManagerParent.cpp
@@ -11,6 +11,7 @@
 #include "mozilla/layers/ContentCompositorBridgeParent.h"
 #include "mozilla/layers/CompositorThread.h"
 #include "mozilla/layers/SharedSurfacesParent.h"
+#include "mozilla/Unused.h"
 #include "nsAutoPtr.h"
 #include "VsyncSource.h"
 
@@ -128,6 +129,9 @@ void CompositorManagerParent::Bind(
 }
 
 void CompositorManagerParent::BindComplete() {
+  MOZ_ASSERT(CompositorThreadHolder::IsInCompositorThread() ||
+             NS_IsMainThread());
+
   
   
   AddRef();
@@ -320,6 +324,17 @@ mozilla::ipc::IPCResult CompositorManagerParent::RecvReportMemory(
       });
 
   return IPC_OK();
+}
+
+
+void CompositorManagerParent::NotifyWebRenderError(wr::WebRenderError aError) {
+  MOZ_ASSERT(CompositorThreadHolder::IsInCompositorThread());
+
+  StaticMutexAutoLock lock(sMutex);
+  if (NS_WARN_IF(!sInstance)) {
+    return;
+  }
+  Unused << sInstance->SendNotifyWebRenderError(aError);
 }
 
 }  
