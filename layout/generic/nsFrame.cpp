ee87d2cc3284c1204d010a6d6be39e5826cb16a7
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 11:40:02 +0000

    Bug 1529831 - Don't create wrap lists for positioned frames that are leaves. r=miko
    Differential Revision: https://phabricator.services.mozilla.com/D20773
    
    UltraBlame original commit: 65294fe0c614644692640a01497539533aa13b63

--- layout/generic/nsFrame.cpp
+++ layout/generic/nsFrame.cpp
@@ -3404,6 +3404,15 @@ static nsDisplayItem* WrapInWrapList(nsDisplayListBuilder* aBuilder,
 
   
   
+  
+  
+  if (!item->GetAbove() && item->Frame() == aFrame && aFrame->PrincipalChildList().IsEmpty()) {
+    aList->RemoveBottom();
+    return item;
+  }
+
+  
+  
   return MakeDisplayItem<nsDisplayWrapList>(aBuilder, aFrame, aList,
                                             aContainerASR, true);
 }
