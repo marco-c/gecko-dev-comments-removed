ad599b529d51c05cb24547514a265836ea319940
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 13:35:19 +0000

    Backed out changeset 6c1f943e591c (bug 1462018) for robocop failures FennecNativeDriver.java on a CLOSED TREE.
    UltraBlame original commit: 6d5c965e0c8e63c2a2ca0cbbc33a08c463b1a5e3

--- mobile/android/geckoview/src/main/java/org/mozilla/geckoview/GeckoDisplay.java
+++ mobile/android/geckoview/src/main/java/org/mozilla/geckoview/GeckoDisplay.java
@@ -6,7 +6,6 @@
 
 package org.mozilla.geckoview;
 
-import android.graphics.Bitmap;
 import android.support.annotation.NonNull;
 import android.support.annotation.UiThread;
 import android.view.Surface;
@@ -118,32 +117,5 @@ public class GeckoDisplay {
     public boolean shouldPinOnScreen() {
         ThreadUtils.assertOnUiThread();
         return session.getDisplay() == this && session.shouldPinOnScreen();
-    }
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    @UiThread
-    public @NonNull GeckoResult<Bitmap> capturePixels() {
-        ThreadUtils.assertOnUiThread();
-        if (!session.isCompositorReady()) {
-            return GeckoResult.fromException(
-                    new IllegalStateException("Compositor must be ready before pixels can be captured"));
-        }
-        GeckoResult<Bitmap> result = new GeckoResult<>();
-        session.mCompositor.requestScreenPixels(result);
-        return result;
     }
 }
