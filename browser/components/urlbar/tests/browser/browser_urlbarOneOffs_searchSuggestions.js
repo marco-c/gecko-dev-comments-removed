3d76ac666c146ab314ecef9981d32d83a8808a70
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 13:54:24 +0000

    Bug 1533060 - Add a test for selecting a search suggestion after selecting a one-off button. r=adw
    Depends on D22353,D22289
    
    Differential Revision: https://phabricator.services.mozilla.com/D22529
    
    UltraBlame original commit: 8d02942b98f56c2ed93a153c644a8d4a0930d791

--- browser/components/urlbar/tests/browser/browser_urlbarOneOffs_searchSuggestions.js
+++ browser/components/urlbar/tests/browser/browser_urlbarOneOffs_searchSuggestions.js
@@ -1,4 +1,20 @@
+
+
+
+"use strict";
+
+
+
+
+
 const TEST_ENGINE_BASENAME = "searchSuggestionEngine.xml";
+const TEST_ENGINE2_BASENAME = "searchSuggestionEngine2.xml";
+
+const serverInfo = {
+  scheme: "http",
+  host: "localhost",
+  port: 20709, 
+};
 
 add_task(async function init() {
   await PlacesUtils.history.clear();
@@ -8,7 +24,10 @@ add_task(async function init() {
   });
   let engine = await SearchTestUtils.promiseNewSearchEngine(
     getRootDirectory(gTestPath) + TEST_ENGINE_BASENAME);
+  let engine2 = await SearchTestUtils.promiseNewSearchEngine(
+    getRootDirectory(gTestPath) + TEST_ENGINE2_BASENAME);
   let oldDefaultEngine = await Services.search.getDefault();
+  await Services.search.moveEngine(engine2, 0);
   await Services.search.moveEngine(engine, 0);
   await Services.search.setDefault(engine);
   registerCleanupFunction(async function() {
@@ -20,69 +39,118 @@ add_task(async function init() {
   });
 });
 
+async function withSecondSuggestion(testFn) {
+  await BrowserTestUtils.withNewTab(gBrowser, async () => {
+    let typedValue = "foo";
+    await promiseAutocompleteResultPopup(typedValue, window, true);
+    await promiseSuggestionsPresent();
+    assertState(0, -1, typedValue);
 
+    
+    EventUtils.synthesizeKey("KEY_ArrowDown");
+    assertState(1, -1, "foofoo");
 
-add_task(async function oneOffReturnAfterSuggestion() {
-  let tab = await BrowserTestUtils.openNewForegroundTab(gBrowser);
+    
+    EventUtils.synthesizeKey("KEY_ArrowDown");
+    assertState(2, -1, "foobar");
 
-  let typedValue = "foo";
-  await promiseAutocompleteResultPopup(typedValue, window, true);
-  await promiseSuggestionsPresent();
-  assertState(0, -1, typedValue);
-
-  
-  EventUtils.synthesizeKey("KEY_ArrowDown");
-  assertState(1, -1, "foofoo");
-
-  
-  EventUtils.synthesizeKey("KEY_ArrowDown");
-  assertState(2, -1, "foobar");
-
-  
-  EventUtils.synthesizeKey("KEY_ArrowDown", {altKey: true});
-  assertState(2, 0, "foobar");
-
-  let resultsPromise =
-    BrowserTestUtils.browserLoaded(gBrowser.selectedBrowser, false,
-                                   `http://mochi.test:8888/?terms=foobar`);
-  EventUtils.synthesizeKey("KEY_Enter");
-  await resultsPromise;
-
+    await withHttpServer(serverInfo, () => {
+      return testFn();
+    });
+  });
   await PlacesUtils.history.clear();
-  BrowserTestUtils.removeTab(tab);
+}
+
+
+
+add_task(async function test_returnAfterSuggestion() {
+  await withSecondSuggestion(async () => {
+    
+    EventUtils.synthesizeKey("KEY_ArrowDown", {altKey: true});
+    assertState(2, 0, "foobar");
+
+    let resultsPromise =
+      BrowserTestUtils.browserLoaded(gBrowser.selectedBrowser, false,
+                                     `http://mochi.test:8888/?terms=foobar`);
+    EventUtils.synthesizeKey("KEY_Enter");
+    await resultsPromise;
+  });
 });
 
 
-add_task(async function oneOffClickAfterSuggestion() {
-  let tab = await BrowserTestUtils.openNewForegroundTab(gBrowser);
 
-  let typedValue = "foo";
-  await promiseAutocompleteResultPopup(typedValue, window, true);
-  await promiseSuggestionsPresent();
-  assertState(0, -1, typedValue);
+add_task(async function test_returnAfterSuggestion_nonDefault() {
+  await withSecondSuggestion(async () => {
+    
+    EventUtils.synthesizeKey("KEY_ArrowDown", {altKey: true});
+    EventUtils.synthesizeKey("KEY_ArrowDown", {altKey: true});
+    assertState(2, 1, "foobar");
 
-  
-  EventUtils.synthesizeKey("KEY_ArrowDown");
-  assertState(1, -1, "foofoo");
+    let resultsPromise =
+      BrowserTestUtils.browserLoaded(gBrowser.selectedBrowser, false,
+                                     `http://localhost:20709/?terms=foobar`);
+    EventUtils.synthesizeKey("KEY_Enter");
+    await resultsPromise;
+  });
+});
 
-  
-  EventUtils.synthesizeKey("KEY_ArrowDown");
-  assertState(2, -1, "foobar");
 
-  let oneOffs = UrlbarTestUtils.getOneOffSearchButtons(window).getSelectableButtons(true);
-  let resultsPromise =
-    BrowserTestUtils.browserLoaded(gBrowser.selectedBrowser, false,
-                                   `http://mochi.test:8888/?terms=foobar`);
-  EventUtils.synthesizeMouseAtCenter(oneOffs[0], {});
-  await resultsPromise;
+add_task(async function test_clickAfterSuggestion() {
+  await withSecondSuggestion(async () => {
+    let oneOffs = UrlbarTestUtils.getOneOffSearchButtons(window).getSelectableButtons(true);
+    let resultsPromise =
+      BrowserTestUtils.browserLoaded(gBrowser.selectedBrowser, false,
+                                     `http://mochi.test:8888/?terms=foobar`);
+    EventUtils.synthesizeMouseAtCenter(oneOffs[0], {});
+    await resultsPromise;
+  });
+});
 
-  await PlacesUtils.history.clear();
-  BrowserTestUtils.removeTab(tab);
+
+add_task(async function test_clickAfterSuggestion_nonDefault() {
+  await withSecondSuggestion(async () => {
+    let oneOffs = UrlbarTestUtils.getOneOffSearchButtons(window).getSelectableButtons(true);
+    let resultsPromise =
+      BrowserTestUtils.browserLoaded(gBrowser.selectedBrowser, false,
+                                     `http://localhost:20709/?terms=foobar`);
+    EventUtils.synthesizeMouseAtCenter(oneOffs[1], {});
+    await resultsPromise;
+  });
+});
+
+
+add_task(async function test_selectOneOffThenSuggestion() {
+  if (!UrlbarPrefs.get("quantumbar")) {
+    
+    return;
+  }
+  await BrowserTestUtils.withNewTab(gBrowser, async () => {
+    let typedValue = "foo";
+    await promiseAutocompleteResultPopup(typedValue, window, true);
+    await promiseSuggestionsPresent();
+    assertState(0, -1, typedValue);
+
+    
+    EventUtils.synthesizeKey("KEY_ArrowDown", {altKey: true});
+    EventUtils.synthesizeKey("KEY_ArrowDown", {altKey: true});
+    assertState(0, 1, "foo");
+
+    
+    await withHttpServer(serverInfo, async () => {
+      let result = await UrlbarTestUtils.getDetailsOfResultAt(window, 2);
+
+      let resultsPromise =
+        BrowserTestUtils.browserLoaded(gBrowser.selectedBrowser, false,
+                                       `http://localhost:20709/?terms=foobar`);
+      EventUtils.synthesizeMouseAtCenter(result.element.row, {});
+      await resultsPromise;
+    });
+  });
 });
 
 add_task(async function overridden_engine_not_reused() {
   info("An overridden search suggestion item should not be reused by a search with another engine");
-  let tab = await BrowserTestUtils.openNewForegroundTab(gBrowser);
+  await BrowserTestUtils.withNewTab(gBrowser, async () => {
     let typedValue = "foo";
     await promiseAutocompleteResultPopup(typedValue, window, true);
     await promiseSuggestionsPresent();
@@ -103,7 +171,7 @@ add_task(async function overridden_engine_not_reused() {
     assertState(0, -1, "foo");
     result = await UrlbarTestUtils.getDetailsOfResultAt(window, 1);
     Assert.notEqual(result.displayed.action, label, "The label should have been updated");
-    BrowserTestUtils.removeTab(tab);
+  });
 });
 
 function assertState(result, oneOff, textValue = undefined) {
