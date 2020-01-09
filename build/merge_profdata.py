9fdeaff6e3615ec5a1980f5782d5b230672f4e3f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 23:09:54 +0000

    Bug 1553972 - make --with-pgo-profile-path take a directory; r=nalexander
    e10s profiling or IR-based PGO instrumentation will both produce
    multiple `.profraw` files that need to be handled in some way.  Since
    clang's `-fprofile-generate` option takes a directory, it seems fitting
    to make `--with-pgo-profile-path` mirror that by taking a directory, and
    letting `merge_profdata.py` deal with whatever files it might find in
    said directory.
    
    Differential Revision: https://phabricator.services.mozilla.com/D32389
    
    UltraBlame original commit: 99dfff71a4ca4e7cec211404f9efde80b3b7a693

--- build/merge_profdata.py
+++ build/merge_profdata.py
@@ -2,10 +2,17 @@
 
 
 
+import glob
 import subprocess
+import sys
 import buildconfig
 
 
-def main(_, profile_file):
+def main(_, profile_dir):
+    profraw_files = glob.glob(profile_dir + '/*.profraw')
+    if not profraw_files:
+        print('Could not find any profraw files in ' + profile_dir)
+        sys.exit(1)
+
     subprocess.check_call([buildconfig.substs['LLVM_PROFDATA'], 'merge',
-                           '-o', 'merged.profdata', profile_file])
+                           '-o', 'merged.profdata'] + profraw_files)
