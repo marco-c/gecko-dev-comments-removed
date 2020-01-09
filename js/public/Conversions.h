b6d5c1918f6a36f9a3cbf4a1c2fbbc7649a53aa0
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 17:15:19 +0000

    Bug 1540357 - Fix implicit int truncation dom/canvas's js/* includes. r=sfink
    Depends on D25495
    
    Differential Revision: https://phabricator.services.mozilla.com/D25496
    
    UltraBlame original commit: 89552599db42520b12be078b9db8f848cce87a36

--- js/public/Conversions.h
+++ js/public/Conversions.h
@@ -348,7 +348,7 @@ inline UnsignedInteger ToUnsignedInteger(double d) {
   
   
   if (exponent < ResultWidth) {
-    UnsignedInteger implicitOne = UnsignedInteger(1) << exponent;
+    const auto implicitOne = static_cast<UnsignedInteger>(UnsignedInteger{1} << exponent);
     result &= implicitOne - 1;  
     result += implicitOne;      
   }
