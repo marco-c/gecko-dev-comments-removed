10cea7b95b1d2f1dada05d12af9301f2dbc787f2
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 17:34:13 +0000

    Bug 1493448: Print out the failing HRESULT when assertion fires in mscom::AgileReference::Clear; r=Jamie
    Differential Revision: https://phabricator.services.mozilla.com/D26001
    
    UltraBlame original commit: 8e2a228491260fecd35cdd2d84b7f5c7ecaed5ec

--- ipc/mscom/AgileReference.cpp
+++ ipc/mscom/AgileReference.cpp
@@ -12,6 +12,11 @@
 #include "mozilla/Move.h"
 #include "mozilla/mscom/Utils.h"
 
+#if defined(MOZILLA_INTERNAL_API)
+#include "nsDebug.h"
+#include "nsPrintfCString.h"
+#endif  
+
 #if NTDDI_VERSION < NTDDI_WINBLUE
 
 
@@ -100,7 +105,13 @@ void AgileReference::Clear() {
   }
 
   DebugOnly<HRESULT> hr = git->RevokeInterfaceFromGlobal(mGitCookie);
+#if defined(MOZILLA_INTERNAL_API)
+  NS_WARNING_ASSERTION(SUCCEEDED(hr), nsPrintfCString(
+      "IGlobalInterfaceTable::RevokeInterfaceFromGlobal failed with HRESULT "
+      "0x%08lX", ((HRESULT)hr)).get());
+#else
   MOZ_ASSERT(SUCCEEDED(hr));
+#endif  
   mGitCookie = 0;
 }
 
