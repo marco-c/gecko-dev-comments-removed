6e1a2d7d897a2e55a94f8239b1ac2b979d5a9db1
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 00:42:41 +0000

    Bug 1464711 - Fix typo in ExtensionContent script.requiresCleanup getter. r=zombie
    This patch fixes the typo in the requiresCleanup getter and adds an additional step
    in the automated tests to verify that the scripts created by browser.tabs.removeCSS
    are not being added to the content scripts that requires cleanup.
    
    Differential Revision: https://phabricator.services.mozilla.com/D17345
    
    UltraBlame original commit: ca8f8514f60dddc015b4a0644520cc1263479ae9

--- browser/components/extensions/test/browser/browser_ext_tabs_removeCSS.js
+++ browser/components/extensions/test/browser/browser_ext_tabs_removeCSS.js
@@ -111,6 +111,27 @@ add_task(async function testExecuteScript() {
 
   await extension.awaitFinish("removeCSS");
 
+  
+  
+  await ContentTask.spawn(tab.linkedBrowser, extension.id, async (extId) => {
+    const {
+      DocumentManager,
+    } = ChromeUtils.import("resource://gre/modules/ExtensionContent.jsm", {});
+
+    let contentScriptContext = Array.from(
+      DocumentManager.getContexts(content.window).values()
+    ).find(context => context.extension.id === extId);
+
+    for (let script of contentScriptContext.scripts) {
+      if (script.matcher.removeCSS && script.requiresCleanup) {
+        throw new Error("tabs.removeCSS scripts should not require cleanup");
+      }
+    }
+  }).catch(err => {
+    
+    ok(false, err);
+  });
+
   await extension.unload();
 
   BrowserTestUtils.removeTab(tab);
