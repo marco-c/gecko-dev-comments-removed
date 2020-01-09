1bc6e51701dcdd19a5292b2f674414ea7d8d99b5
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 00:00:57 +0000

    Bug 1517641 - [1.0] Add experimental ad-blocking category to the Tracking Protection API.
    Differential Revision: https://phabricator.services.mozilla.com/D16700
    
    UltraBlame original commit: c92a0281831e1c23e7b2fdb8fae5b0dc88cee7f5

--- mobile/android/geckoview/src/main/java/org/mozilla/geckoview/GeckoSession.java
+++ mobile/android/geckoview/src/main/java/org/mozilla/geckoview/GeckoSession.java
@@ -3816,7 +3816,7 @@ public class GeckoSession implements Parcelable {
         @IntDef(flag = true,
                 value = { CATEGORY_NONE, CATEGORY_AD, CATEGORY_ANALYTIC,
                           CATEGORY_SOCIAL, CATEGORY_CONTENT, CATEGORY_ALL,
-                          CATEGORY_TEST })
+                          CATEGORY_TEST, CATEGORY_AD_EXT })
          @interface Category {}
 
         static final int CATEGORY_NONE = 0;
@@ -3844,6 +3844,10 @@ public class GeckoSession implements Parcelable {
 
 
         static final int CATEGORY_ALL = (1 << 5) - 1;
+        
+
+
+        static final int CATEGORY_AD_EXT = 1 << 6;
 
         
 
