80e7e3ca2d69872c73858d61df73cc4cfde83632
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 10:09:06 +0000

    Backed out changeset 91bfef9d7343 (bug 1527374) for causing merge conflicts CLOSED TREE
    UltraBlame original commit: ea8190021659c7b40fe5e91865ea0f6a0a0bbd1d

--- toolkit/xre/nsAppRunner.cpp
+++ toolkit/xre/nsAppRunner.cpp
@@ -1943,6 +1943,22 @@ static ReturnAbortOnError ProfileLockedDialog(nsIFile* aProfileDir,
   }
 }
 
+static nsresult ProfileLockedDialog(nsIToolkitProfile* aProfile,
+                                    nsIProfileUnlocker* aUnlocker,
+                                    nsINativeAppSupport* aNative,
+                                    nsIProfileLock** aResult) {
+  nsCOMPtr<nsIFile> profileDir;
+  nsresult rv = aProfile->GetRootDir(getter_AddRefs(profileDir));
+  if (NS_FAILED(rv)) return rv;
+
+  nsCOMPtr<nsIFile> profileLocalDir;
+  rv = aProfile->GetLocalDir(getter_AddRefs(profileLocalDir));
+  if (NS_FAILED(rv)) return rv;
+
+  return ProfileLockedDialog(profileDir, profileLocalDir, aUnlocker, aNative,
+                             aResult);
+}
+
 static const char kProfileManagerURL[] =
     "chrome://mozapps/content/profile/profileSelection.xul";
 
@@ -2048,37 +2064,6 @@ static bool gDoMigration = false;
 static bool gDoProfileReset = false;
 static nsCOMPtr<nsIToolkitProfile> gResetOldProfile;
 
-static nsresult LockProfile(nsINativeAppSupport* aNative, nsIFile* aRootDir,
-                            nsIFile* aLocalDir, nsIToolkitProfile* aProfile,
-                            nsIProfileLock** aResult) {
-  
-  
-  
-  
-
-  static const int kLockRetrySeconds = 5;
-  static const int kLockRetrySleepMS = 100;
-
-  nsresult rv;
-  nsCOMPtr<nsIProfileUnlocker> unlocker;
-  const TimeStamp start = TimeStamp::Now();
-  do {
-    if (aProfile) {
-      rv = aProfile->Lock(getter_AddRefs(unlocker), aResult);
-    } else {
-      rv = NS_LockProfilePath(aRootDir, aLocalDir, getter_AddRefs(unlocker),
-                              aResult);
-    }
-    if (NS_SUCCEEDED(rv)) {
-      StartupTimeline::Record(StartupTimeline::AFTER_PROFILE_LOCKED);
-      return NS_OK;
-    }
-    PR_Sleep(kLockRetrySleepMS);
-  } while (TimeStamp::Now() - start <
-           TimeDuration::FromSeconds(kLockRetrySeconds));
-
-  return ProfileLockedDialog(aRootDir, aLocalDir, unlocker, aNative, aResult);
-}
 
 
 
@@ -2087,15 +2072,27 @@ static nsresult LockProfile(nsINativeAppSupport* aNative, nsIFile* aRootDir,
 
 
 
-
-
-static nsresult SelectProfile(nsToolkitProfileService* aProfileSvc,
-                              nsINativeAppSupport* aNative, nsIFile** aRootDir,
-                              nsIFile** aLocalDir,
-                              nsIToolkitProfile** aProfile) {
+static nsresult SelectProfile(nsIProfileLock** aResult,
+                              nsIToolkitProfileService* aProfileSvc,
+                              nsINativeAppSupport* aNative, bool* aStartOffline,
+                              nsACString* aProfileName) {
   StartupTimeline::Record(StartupTimeline::SELECT_PROFILE);
 
   nsresult rv;
+  ArgResult ar;
+  *aResult = nullptr;
+  *aStartOffline = false;
+
+  ar = CheckArg("offline", nullptr,
+                CheckArgFlag::CheckOSInt | CheckArgFlag::RemoveArg);
+  if (ar == ARG_BAD) {
+    PR_fprintf(PR_STDERR,
+               "Error: argument --offline is invalid when argument --osint is "
+               "specified\n");
+    return NS_ERROR_FAILURE;
+  }
+
+  if (ar || EnvHasValue("XRE_START_OFFLINE")) *aStartOffline = true;
 
   if (EnvHasValue("MOZ_RESET_PROFILE_RESTART")) {
     gDoProfileReset = true;
@@ -2112,8 +2109,8 @@ static nsresult SelectProfile(nsToolkitProfileService* aProfileSvc,
 
   
   
-  ArgResult ar = CheckArg("reset-profile", nullptr,
-                          CheckArgFlag::CheckOSInt | CheckArgFlag::RemoveArg);
+  ar = CheckArg("reset-profile", nullptr,
+                CheckArgFlag::CheckOSInt | CheckArgFlag::RemoveArg);
   if (ar == ARG_BAD) {
     PR_fprintf(PR_STDERR,
                "Error: argument --reset-profile is invalid when argument "
@@ -2136,10 +2133,16 @@ static nsresult SelectProfile(nsToolkitProfileService* aProfileSvc,
     gDoMigration = true;
   }
 
+  nsCOMPtr<nsIFile> rootDir;
+  nsCOMPtr<nsIFile> localDir;
+  nsCOMPtr<nsIToolkitProfile> profile;
   
+  nsToolkitProfileService* service =
+      static_cast<nsToolkitProfileService*>(aProfileSvc);
   bool didCreate = false;
-  rv = aProfileSvc->SelectStartupProfile(&gArgc, gArgv, gDoProfileReset,
-      aRootDir, aLocalDir, aProfile, &didCreate);
+  rv = service->SelectStartupProfile(
+      &gArgc, gArgv, gDoProfileReset, getter_AddRefs(rootDir),
+      getter_AddRefs(localDir), getter_AddRefs(profile), &didCreate);
 
   if (rv == NS_ERROR_SHOW_PROFILE_MANAGER) {
     return ShowProfileManager(aProfileSvc, aNative);
@@ -2154,20 +2157,86 @@ static nsresult SelectProfile(nsToolkitProfileService* aProfileSvc,
     gDoMigration = false;
   }
 
-  if (gDoProfileReset && !*aProfile) {
-    NS_WARNING("Profile reset is only supported for named profiles.");
+  if (gDoProfileReset) {
+    if (!profile) {
+      NS_WARNING("Profile reset is only supported for named profiles.");
+      return NS_ERROR_ABORT;
+    }
+
+    {
+      
+      
+      nsIProfileLock* tempProfileLock;
+      nsCOMPtr<nsIProfileUnlocker> unlocker;
+      rv = profile->Lock(getter_AddRefs(unlocker), &tempProfileLock);
+      if (NS_FAILED(rv)) {
+        return ProfileLockedDialog(profile, unlocker, aNative,
+                                   &tempProfileLock);
+      }
+    }
+
+    
+    gResetOldProfile = profile;
+    rv = service->CreateResetProfile(getter_AddRefs(profile));
+    if (NS_SUCCEEDED(rv)) {
+      rv = profile->GetRootDir(getter_AddRefs(rootDir));
+      NS_ENSURE_SUCCESS(rv, rv);
+      SaveFileToEnv("XRE_PROFILE_PATH", rootDir);
+
+      rv = profile->GetLocalDir(getter_AddRefs(localDir));
+      NS_ENSURE_SUCCESS(rv, rv);
+      SaveFileToEnv("XRE_PROFILE_LOCAL_PATH", localDir);
+
+      rv = profile->GetName(*aProfileName);
+      if (NS_FAILED(rv)) {
+        aProfileName->Truncate(0);
+      }
+    } else {
+      NS_WARNING("Profile reset failed.");
+      return NS_ERROR_ABORT;
+    }
+  }
+
+  
+  
+  
+  if (!rootDir) {
     return NS_ERROR_ABORT;
   }
 
   
   
   
-  if (!*aRootDir) {
-    NS_WARNING("Failed to select or create profile.");
-    return NS_ERROR_ABORT;
-  }
+  
 
-  return NS_OK;
+  static const int kLockRetrySeconds = 5;
+  static const int kLockRetrySleepMS = 100;
+
+  nsCOMPtr<nsIProfileUnlocker> unlocker;
+  const TimeStamp start = TimeStamp::Now();
+  do {
+    if (profile) {
+      rv = profile->Lock(getter_AddRefs(unlocker), aResult);
+    } else {
+      rv = NS_LockProfilePath(rootDir, localDir, getter_AddRefs(unlocker),
+                              aResult);
+    }
+    if (NS_SUCCEEDED(rv)) {
+      StartupTimeline::Record(StartupTimeline::AFTER_PROFILE_LOCKED);
+      
+      if (aProfileName && profile) {
+        rv = profile->GetName(*aProfileName);
+        if (NS_FAILED(rv)) {
+          aProfileName->Truncate(0);
+        }
+      }
+      return NS_OK;
+    }
+    PR_Sleep(kLockRetrySleepMS);
+  } while (TimeStamp::Now() - start <
+           TimeDuration::FromSeconds(kLockRetrySeconds));
+
+  return ProfileLockedDialog(rootDir, localDir, unlocker, aNative, aResult);
 }
 
 #ifdef MOZ_BLOCK_PROFILE_DOWNGRADE
@@ -2350,6 +2419,8 @@ static const char kProfileDowngradeURL[] =
     "chrome://mozapps/content/profile/profileDowngrade.xul";
 
 static ReturnAbortOnError CheckDowngrade(nsIFile* aProfileDir,
+                                         nsIFile* aProfileLocalDir,
+                                         nsACString& aProfileName,
                                          nsINativeAppSupport* aNative,
                                          nsIToolkitProfileService* aProfileSvc,
                                          const nsCString& aLastVersion) {
@@ -2926,7 +2997,7 @@ class XREMain {
   Result<bool, nsresult> CheckLastStartupWasCrash();
 
   nsCOMPtr<nsINativeAppSupport> mNativeApp;
-  RefPtr<nsToolkitProfileService> mProfileSvc;
+  nsCOMPtr<nsIToolkitProfileService> mProfileSvc;
   nsCOMPtr<nsIFile> mProfD;
   nsCOMPtr<nsIFile> mProfLD;
   nsCOMPtr<nsIProfileLock> mProfileLock;
@@ -3473,19 +3544,6 @@ int XREMain::XRE_mainInit(bool* aExitFlag) {
   }
   if (ar == ARG_FOUND) {
     SaveToEnv("MOZ_NEW_INSTANCE=1");
-  }
-
-  ar = CheckArg("offline", nullptr,
-                CheckArgFlag::CheckOSInt | CheckArgFlag::RemoveArg);
-  if (ar == ARG_BAD) {
-    PR_fprintf(PR_STDERR,
-               "Error: argument --offline is invalid when argument --osint is "
-               "specified\n");
-    return 1;
-  }
-
-  if (ar || EnvHasValue("XRE_START_OFFLINE")) {
-    mStartOffline = true;
   }
 
   
@@ -4133,9 +4191,8 @@ int XREMain::XRE_mainStartup(bool* aExitFlag) {
     return 1;
   }
 
-  nsCOMPtr<nsIToolkitProfile> profile;
-  rv = SelectProfile(mProfileSvc, mNativeApp, getter_AddRefs(mProfD),
-                     getter_AddRefs(mProfLD), getter_AddRefs(profile));
+  rv = SelectProfile(getter_AddRefs(mProfileLock), mProfileSvc, mNativeApp,
+                     &mStartOffline, &mProfileName);
   if (rv == NS_ERROR_LAUNCHED_CHILD_PROCESS || rv == NS_ERROR_ABORT) {
     *aExitFlag = true;
     return 0;
@@ -4146,57 +4203,13 @@ int XREMain::XRE_mainStartup(bool* aExitFlag) {
     ProfileMissingDialog(mNativeApp);
     return 1;
   }
-
-  
-  
-  rv = LockProfile(mNativeApp, mProfD, mProfLD, profile,
-      getter_AddRefs(mProfileLock));
-  if (rv == NS_ERROR_LAUNCHED_CHILD_PROCESS || rv == NS_ERROR_ABORT) {
-    *aExitFlag = true;
-    return 0;
-  } else if (NS_FAILED(rv)) {
-    return 1;
-  }
-
-  if (gDoProfileReset) {
-    
-    mProfileLock->Unlock();
-
-    
-    gResetOldProfile = profile;
-    rv = mProfileSvc->CreateResetProfile(getter_AddRefs(profile));
-    if (NS_SUCCEEDED(rv)) {
-      rv = profile->GetRootDir(getter_AddRefs(mProfD));
-      NS_ENSURE_SUCCESS(rv, 1);
-      SaveFileToEnv("XRE_PROFILE_PATH", mProfD);
-
-      rv = profile->GetLocalDir(getter_AddRefs(mProfLD));
-      NS_ENSURE_SUCCESS(rv, 1);
-      SaveFileToEnv("XRE_PROFILE_LOCAL_PATH", mProfLD);
-
-      
-      rv = LockProfile(mNativeApp, mProfD, mProfLD, profile,
-                       getter_AddRefs(mProfileLock));
-      if (rv == NS_ERROR_LAUNCHED_CHILD_PROCESS || rv == NS_ERROR_ABORT) {
-        *aExitFlag = true;
-        return 0;
-      } else if (NS_FAILED(rv)) {
-        return 1;
-      }
-    } else {
-      NS_WARNING("Profile reset failed.");
-      return 1;
-    }
-  }
-
-  if (profile) {
-    rv = profile->GetName(mProfileName);
-    if (NS_FAILED(rv)) {
-      mProfileName.Truncate(0);
-    }
-  }
-
   gProfileLock = mProfileLock;
+
+  rv = mProfileLock->GetDirectory(getter_AddRefs(mProfD));
+  NS_ENSURE_SUCCESS(rv, 1);
+
+  rv = mProfileLock->GetLocalDirectory(getter_AddRefs(mProfLD));
+  NS_ENSURE_SUCCESS(rv, 1);
 
   nsAutoCString version;
   BuildVersion(version);
@@ -4233,7 +4246,8 @@ int XREMain::XRE_mainStartup(bool* aExitFlag) {
 
 #ifdef MOZ_BLOCK_PROFILE_DOWNGRADE
   if (isDowngrade && !CheckArg("allow-downgrade")) {
-    rv = CheckDowngrade(mProfD, mNativeApp, mProfileSvc, lastVersion);
+    rv = CheckDowngrade(mProfD, mProfLD, mProfileName, mNativeApp, mProfileSvc,
+                        lastVersion);
     if (rv == NS_ERROR_LAUNCHED_CHILD_PROCESS || rv == NS_ERROR_ABORT) {
       *aExitFlag = true;
       return 0;
@@ -4486,7 +4500,8 @@ nsresult XREMain::XRE_mainRun() {
     }
 
     if (gDoProfileReset) {
-      nsresult backupCreated = ProfileResetCleanup(mProfileSvc,
+      nsresult backupCreated = ProfileResetCleanup(
+          static_cast<nsToolkitProfileService*>(mProfileSvc.get()),
           gResetOldProfile);
       if (NS_FAILED(backupCreated))
         NS_WARNING("Could not cleanup the profile that was reset");
@@ -4684,7 +4699,7 @@ nsresult XREMain::XRE_mainRun() {
   AddSandboxAnnotations();
 #endif 
 
-  mProfileSvc->CompleteStartup();
+  static_cast<nsToolkitProfileService*>(mProfileSvc.get())->CompleteStartup();
 
   {
     rv = appStartup->Run();
