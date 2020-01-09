b04bd9811359ba366546468b87657a2a3ef494f0
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 02:21:38 +0000

    Bug 1525238 - Use actorID for tracked rules and stylesheets. r=pbro
    Replaces custom generated hashes with the actorIDs which are stable
    during the editing session enough to use as unique identifiers.
    
    For future restore / persistence, we still have the metadata about each
    rule and stylesheet to attempt to identify them again.
    
    Differential Revision: https://phabricator.services.mozilla.com/D18660
    
    UltraBlame original commit: 89ba5f983e613a1aea267c72b21915115782dbd9

--- devtools/server/actors/styles.js
+++ devtools/server/actors/styles.js
@@ -1085,10 +1085,12 @@ var StyleRuleActor = protocol.ActorClassWithSpec(styleRuleSpec, {
 
   get metadata() {
     const data = {};
+    data.id = this.actorID;
     
     
     data.ancestors = this.ancestorRules.map(rule => {
       return {
+        id: rule.actorID,
         
         
         type: rule.rawRule.type,
@@ -1117,6 +1119,7 @@ var StyleRuleActor = protocol.ActorClassWithSpec(styleRuleSpec, {
         
         
         href: this.rawNode.baseURI,
+        id: this.pageStyle.walker.getNode(this.rawNode).actorID,
         
         index: data.selector,
         
@@ -1131,6 +1134,7 @@ var StyleRuleActor = protocol.ActorClassWithSpec(styleRuleSpec, {
         
         type: this.sheetActor.href ? "stylesheet" : "inline",
         href: this.sheetActor.href || this.sheetActor.window.location.toString(),
+        id: this.sheetActor.actorID,
         index: this.sheetActor.styleSheetIndex,
         
         isFramed: this.sheetActor.ownerWindow !== this.sheetActor.window,
