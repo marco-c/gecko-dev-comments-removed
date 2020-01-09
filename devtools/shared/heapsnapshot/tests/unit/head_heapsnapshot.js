f59094934a29e398df6f3ff5cb8593963901d839
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 00:40:10 +0000

    Bug 1512949 - Remove eval from tests and flip assertion flag r=ckerschb
    Differential Revision: https://phabricator.services.mozilla.com/D14054
    
    UltraBlame original commit: 7a3cc3042d048fb467e303fe362e94e9a8160632

--- devtools/shared/heapsnapshot/tests/unit/head_heapsnapshot.js
+++ devtools/shared/heapsnapshot/tests/unit/head_heapsnapshot.js
@@ -31,8 +31,10 @@ const { LabelAndShallowSizeVisitor } = DominatorTreeNode;
 
 if (Services.appinfo.processType == Services.appinfo.PROCESS_TYPE_DEFAULT) {
   Services.prefs.setBoolPref("devtools.debugger.log", true);
+  Services.prefs.setBoolPref("security.allow_eval_with_system_principal", true);
   registerCleanupFunction(() => {
     Services.prefs.clearUserPref("devtools.debugger.log");
+    Services.prefs.clearUserPref("security.allow_eval_with_system_principal");
   });
 }
 
