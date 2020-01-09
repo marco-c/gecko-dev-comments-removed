e9ffa379daf4026af3e29352c518a7d788424565
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 10:05:21 +0000

    Bug 1526775 - Removed unused ui.bookmark.mobilefolder.enabled pref. r=snorp
    It's only being referenced in these two lines in mobile.js, but nowhere else
    anymore.
    
    Differential Revision: https://phabricator.services.mozilla.com/D19309
    
    UltraBlame original commit: 67b183ba1627661cfa49db52e899c5d61b2c3c84

--- mobile/android/app/mobile.js
+++ mobile/android/app/mobile.js
@@ -381,14 +381,6 @@ pref("browser.ui.zoom.force-user-scalable", false);
 pref("view_source.wrap_long_lines", true);
 
 
-
-#ifdef NIGHTLY_BUILD
-pref("ui.bookmark.mobilefolder.enabled", true);
-#else
-pref("ui.bookmark.mobilefolder.enabled", false);
-#endif
-
-
 pref("ui.touch.radius.enabled", false);
 pref("ui.touch.radius.leftmm", 3);
 pref("ui.touch.radius.topmm", 5);
