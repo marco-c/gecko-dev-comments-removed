709f601ab60e173c03402956250cc30e740d71a2
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 18:06:55 +0000

    Backed out changeset 47d793fbd8cc (bug 1533385) at nalexander's request.
    UltraBlame original commit: 9bc397e8199db4df590f7307b8ba6afb32c9ce96

--- mobile/android/geckoview/src/main/java/org/mozilla/geckoview/GeckoRuntimeSettings.java
+++ mobile/android/geckoview/src/main/java/org/mozilla/geckoview/GeckoRuntimeSettings.java
@@ -84,19 +84,6 @@ public final class GeckoRuntimeSettings extends RuntimeSettings {
 
 
 
-
-        public @NonNull Builder configFilePath(final @Nullable String configFilePath) {
-            getSettings().mConfigFilePath = configFilePath;
-            return this;
-        }
-
-        
-
-
-
-
-
-
         public @NonNull Builder javaScriptEnabled(final boolean flag) {
             getSettings().mJavaScript.set(flag);
             return this;
@@ -343,7 +330,6 @@ public final class GeckoRuntimeSettings extends RuntimeSettings {
      boolean mUseContentProcess;
      String[] mArgs;
      Bundle mExtras;
-     String mConfigFilePath;
 
      ContentBlocking.Settings mContentBlocking;
 
@@ -426,7 +412,6 @@ public final class GeckoRuntimeSettings extends RuntimeSettings {
         mScreenHeightOverride = settings.mScreenHeightOverride;
         mCrashHandler = settings.mCrashHandler;
         mRequestedLocales = settings.mRequestedLocales;
-        mConfigFilePath = settings.mConfigFilePath;
     }
 
      void commit() {
@@ -459,18 +444,6 @@ public final class GeckoRuntimeSettings extends RuntimeSettings {
 
     public @NonNull Bundle getExtras() {
         return mExtras;
-    }
-
-    
-
-
-
-
-
-
-
-    public @Nullable String getConfigFilePath() {
-        return mConfigFilePath;
     }
 
     
@@ -849,7 +822,6 @@ public final class GeckoRuntimeSettings extends RuntimeSettings {
         out.writeInt(mScreenHeightOverride);
         out.writeString(mCrashHandler != null ? mCrashHandler.getName() : null);
         out.writeStringArray(mRequestedLocales);
-        out.writeString(mConfigFilePath);
     }
 
     
@@ -879,7 +851,6 @@ public final class GeckoRuntimeSettings extends RuntimeSettings {
         }
 
         mRequestedLocales = source.createStringArray();
-        mConfigFilePath = source.readString();
     }
 
     public static final Parcelable.Creator<GeckoRuntimeSettings> CREATOR
