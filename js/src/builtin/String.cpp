ec6ccfc441a50978293ea4b7551dc74c7efba6e8
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 13:54:44 +0000

    Bug 1530937 part 11 - Convert even more callVMs in CodeGenerator.cpp. r=tcampbell
    Differential Revision: https://phabricator.services.mozilla.com/D22677
    
    UltraBlame original commit: b49300f6cc1fc2411be9031f765f240099ec715f

--- js/src/builtin/String.cpp
+++ js/src/builtin/String.cpp
@@ -3277,9 +3277,9 @@ static ArrayObject* SplitSingleCharHelper(JSContext* cx, HandleLinearString str,
 }
 
 
-ArrayObject* js::str_split_string(JSContext* cx, HandleObjectGroup group,
-                                  HandleString str, HandleString sep,
-                                  uint32_t limit) {
+ArrayObject* js::StringSplitString(JSContext* cx, HandleObjectGroup group,
+                                   HandleString str, HandleString sep,
+                                   uint32_t limit) {
   MOZ_ASSERT(limit > 0, "Only called for strictly positive limit.");
 
   RootedLinearString linearStr(cx, str->ensureLinear(cx));
