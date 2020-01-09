249129eec2711bd5e094f782e8b2c793f9c127c7
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 13:33:03 +0000

    Bug 1531293 - Enable javascript.options.bigint on Nightly r=jandem
    Differential Revision: https://phabricator.services.mozilla.com/D21528
    
    UltraBlame original commit: c2fdfa3db1b2ba072f6a9577b1c62cde68c573cf

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -1600,7 +1600,11 @@ pref("javascript.options.spectre.jit_to_C++_calls", true);
 pref("javascript.options.streams", true);
 
 
+#ifdef NIGHTLY_BUILD
+pref("javascript.options.bigint", true);
+#else
 pref("javascript.options.bigint", false);
+#endif
 
 
 #ifdef NIGHTLY_BUILD
