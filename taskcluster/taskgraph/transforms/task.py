bb94c5c12414322b164378a411bc097c11240a43
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 15:43:34 +0000

    Bug 1352113 - Shippable Builds - Add index builder, but don't do any index's yet. r=aki
    Differential Revision: https://phabricator.services.mozilla.com/D22224
    
    UltraBlame original commit: e7eeb55c71ba25d062ef8c7eaac85f81f437f28d

--- taskcluster/taskgraph/transforms/task.py
+++ taskcluster/taskgraph/transforms/task.py
@@ -127,7 +127,7 @@ task_description_schema = Schema({
 
         
         'type': Any('generic', 'nightly', 'l10n', 'nightly-with-multi-l10n',
-                    'release', 'nightly-l10n'),
+                    'release', 'nightly-l10n', 'shippable'),
 
         
         
@@ -1456,6 +1456,12 @@ def add_nightly_index_routes(config, task):
     
     task = add_l10n_index_routes(config, task, force_locale="en-US")
 
+    return task
+
+
+@index_builder('shippable')
+def add_shippable_index_routes(config, task):
+    
     return task
 
 
