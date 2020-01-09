f35ed6ac4de1e68d7cbc2742b40c97d901d26710
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 21:45:01 +0000

    Bug 1535592 - Don't resizeToContent until all content is modified. r=jaws
    Differential Revision: https://phabricator.services.mozilla.com/D30721
    
    UltraBlame original commit: 3665b343720dbbdf145769089959b4cecaf81c2d

--- browser/base/content/aboutDialog.js
+++ browser/base/content/aboutDialog.js
@@ -61,7 +61,6 @@ async function init(aEvent) {
   document.l10n.setAttributes(versionField, versionId, versionAttributes);
 
   await document.l10n.translateElements([versionField]);
-  window.sizeToContent();
 
   
   let relNotesLink = document.getElementById("releasenotes");
@@ -87,9 +86,10 @@ async function init(aEvent) {
   if (AppConstants.MOZ_APP_VERSION_DISPLAY.endsWith("esr")) {
     document.getElementById("release").hidden = false;
   }
+
+  window.sizeToContent();
+
   if (AppConstants.platform == "macosx") {
-    
-    window.sizeToContent();
     window.moveTo((screen.availWidth / 2) - (window.outerWidth / 2), screen.availHeight / 5);
   }
 }
