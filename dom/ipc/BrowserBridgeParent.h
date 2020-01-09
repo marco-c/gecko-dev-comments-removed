3d01a20fce59d67a3199a8ba1c0b0f32620a467d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 16:30:06 +0000

    Bug 1525427 - Part 4: Correctly tear down TabParent within BrowserBridgeParent, r=farre
    Differential Revision: https://phabricator.services.mozilla.com/D25184
    
    UltraBlame original commit: 0227a59eba8e9334a21e445c2db65d02b3130852

--- dom/ipc/BrowserBridgeParent.h
+++ dom/ipc/BrowserBridgeParent.h
@@ -35,6 +35,9 @@ class BrowserBridgeParent : public PBrowserBridgeParent {
     return static_cast<TabParent*>(PBrowserBridgeParent::Manager());
   }
 
+  
+  void Destroy();
+
  protected:
   friend class PBrowserBridgeParent;
 
