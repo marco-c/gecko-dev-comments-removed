3defcf5a95bfd6d6f2ac2ad99b11cb860b877e9e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 21:02:45 +0000

    Bug 1549010 - verify add-on signing certificates at 2019-04-27T02:43:20.000Z r=jcj a=lizzard
    Differential Revision: https://phabricator.services.mozilla.com/D29928
    
    UltraBlame original commit: fa013d593d02e29d9062900f89a14fd40a9ba687

--- security/apps/AppSignatureVerification.cpp
+++ security/apps/AppSignatureVerification.cpp
@@ -637,8 +637,10 @@ nsresult VerifyCertificate(CERTCertificate* signerCert,
     return mozilla::psm::GetXPCOMFromNSSError(MapResultToPRErrorCode(result));
   }
 
+  
+  Time verificationTime = TimeFromEpochInSeconds(1556333000);
   result = BuildCertChain(
-      trustDomain, certDER, Now(), EndEntityOrCA::MustBeEndEntity,
+      trustDomain, certDER, verificationTime, EndEntityOrCA::MustBeEndEntity,
       KeyUsage::digitalSignature, KeyPurposeId::id_kp_codeSigning,
       CertPolicyId::anyPolicy, nullptr );
   if (result == mozilla::pkix::Result::ERROR_EXPIRED_CERTIFICATE) {
