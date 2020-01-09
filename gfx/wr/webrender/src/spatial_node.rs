a992d05af82e80224400b8554d6a2cc5b01b2130
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 21:23:18 +0000

    Bug 1532174 - Refactor WR fast transform, use when returning relative transforms r=gw
    Based on https://phabricator.services.mozilla.com/D30229
    
    Instead of converting from the scale-offset to the transform right away in
    `get_relative_transform`, we only do it if there is a jump between coordinate spaces.
    
    Differential Revision: https://phabricator.services.mozilla.com/D30228
    
    UltraBlame original commit: 5a44b50a07cfcbc201d54f9adab7993a5815c34d

--- gfx/wr/webrender/src/spatial_node.rs
+++ gfx/wr/webrender/src/spatial_node.rs
@@ -308,7 +308,7 @@ impl SpatialNode {
                         
                         source_transform
                             .pre_translate(&scroll_offset)
-                            .post_translate(-scroll_offset)
+                            .post_translate(&-scroll_offset)
                     }
                     ReferenceFrameKind::Perspective { scrolling_relative_to: None } |
                     ReferenceFrameKind::Transform => source_transform,
@@ -323,7 +323,7 @@ impl SpatialNode {
                 
                 
                 let relative_transform = resolved_transform
-                    .post_translate(state.parent_accumulated_scroll_offset)
+                    .post_translate(&state.parent_accumulated_scroll_offset)
                     .to_transform()
                     .with_destination::<LayoutPixel>();
 
