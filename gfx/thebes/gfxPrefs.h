4fd89720ba0a6ed47f872c1e2867aa103c2858b8
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 23:24:25 +0000

    Bug 1550422 - P17. Convert gfxPrefs::GfxLoggingLevel to StaticPrefs. r=jrmuizel
    Fly-by fix, we make LoggingPrefs::sGfxLogLevel as it is written on the main thread but read on different threads.
    
    Differential Revision: https://phabricator.services.mozilla.com/D31460
    
    UltraBlame original commit: d5ec03906d351f448dd491d080e96bddb776676f

--- gfx/thebes/gfxPrefs.h
+++ gfx/thebes/gfxPrefs.h
@@ -289,8 +289,6 @@ class gfxPrefs final {
   
   
 
-  
-  DECL_GFX_PREF(Live, "gfx.logging.level",                     GfxLoggingLevel, int32_t, mozilla::gfx::LOG_DEFAULT);
   DECL_GFX_PREF(Once, "layers.windowrecording.path",           LayersWindowRecordingPath, std::string, std::string());
 
   DECL_GFX_PREF(Live, "layout.frame_rate",                     LayoutFrameRate, int32_t, -1);
