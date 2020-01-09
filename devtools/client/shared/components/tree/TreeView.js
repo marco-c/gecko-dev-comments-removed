8e29859e486f8cdb2ec2f102859ee8b2df96cc9a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 00:21:00 +0000

    Backed out changeset 611bd5949e83 (bug 1520107) for dt failures, on a CLOSED TREE.
    UltraBlame original commit: 1c7ed5d9e1fe429f8a753b40156744b73ef12cda

--- devtools/client/shared/components/tree/TreeView.js
+++ devtools/client/shared/components/tree/TreeView.js
@@ -118,8 +118,6 @@ define(function(require, exports, module) {
         
         onClickRow: PropTypes.func,
         
-        onContextMenuTree: PropTypes.func,
-        
         header: PropTypes.bool,
         
         expandableStrings: PropTypes.bool,
@@ -313,7 +311,7 @@ define(function(require, exports, module) {
       }
 
       
-      this.refs.tree.focus();
+      this.tree.focus();
       event.preventDefault();
     }
 
@@ -514,8 +512,8 @@ define(function(require, exports, module) {
       const classNames = ["treeTable"];
       this.rows = [];
 
-      const { className, onContextMenuTree } = this.props;
       
+      const className = this.props.className;
       if (className) {
         classNames.push(...className.split(" "));
       }
@@ -538,10 +536,11 @@ define(function(require, exports, module) {
         dom.table({
           className: classNames.join(" "),
           role: "tree",
-          ref: "tree",
+          ref: tree => {
+            this.tree = tree;
+          },
           tabIndex: 0,
           onKeyDown: this.onKeyDown,
-          onContextMenu: onContextMenuTree && onContextMenuTree.bind(this),
           "aria-label": this.props.label || "",
           "aria-activedescendant": this.state.selected,
           cellPadding: 0,
@@ -549,6 +548,7 @@ define(function(require, exports, module) {
           TreeHeader(props),
           dom.tbody({
             role: "presentation",
+            tabIndex: -1,
           }, rows)
         )
       );
