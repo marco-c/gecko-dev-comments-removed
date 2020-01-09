83a813518eee21cbceb5ff2f73045a01274bb0e3
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 20:04:27 +0000

    Bug 1505126: Part 2 - Render the ADB status into a new component, with an icon r=jdescottes,daisuke,Ola
    Differential Revision: https://phabricator.services.mozilla.com/D28664
    
    UltraBlame original commit: cfca5a733e48717d4396c884d2116340b30b46f0

--- devtools/client/aboutdebugging-new/test/browser/browser_aboutdebugging_message_close.js
+++ devtools/client/aboutdebugging-new/test/browser/browser_aboutdebugging_message_close.js
@@ -4,23 +4,41 @@
 "use strict";
 
 
-
+Services.scriptloader.loadSubScript(CHROME_URL_ROOT + "helper-addons.js", this);
 
 
 add_task(async function() {
-  const { document, tab } = await openAboutDebugging();
+  const EXTENSION_NAME = "Temporary web extension";
+  const EXTENSION_ID = "test-devtools@mozilla.org";
 
-  const usbStatusElement = document.querySelector(".js-sidebar-usb-status");
-  ok(usbStatusElement, "Sidebar shows the USB status element");
-  ok(usbStatusElement.textContent.includes("USB disabled"),
-    "USB status element has the expected content");
+  const { document, tab, window } = await openAboutDebugging();
+  await selectThisFirefoxPage(document, window.AboutDebugging.store);
 
-  const button = document.querySelector(".js-sidebar-item .qa-message-button-close");
+  await installTemporaryExtensionFromXPI({
+    id: EXTENSION_ID,
+    name: EXTENSION_NAME,
+    extraProperties: {
+      
+      "wrongProperty": {},
+    },
+  }, document);
+
+  info("Wait until a debug target item appears");
+  await waitUntil(() => findDebugTargetByText(EXTENSION_NAME, document));
+  const target = findDebugTargetByText(EXTENSION_NAME, document);
+
+  const warningMessage = target.querySelector(".js-message");
+  ok(!!warningMessage, "A warning message is displayed for the installed addon");
+
+  const button = warningMessage.querySelector(".qa-message-button-close");
+  ok(!!button, "The warning message has a close button");
+
+  info("Closing the message and waiting for it to disappear");
   button.click();
+  await waitUntil(() => {
+    return target.querySelector(".js-message") === null;
+  });
 
-  
-  ok(document.querySelector(".js-sidebar-usb-status") === null,
-    "USB status element is no longer displayed");
-
+  await removeTemporaryExtension(EXTENSION_NAME, document);
   await removeTab(tab);
 });
