49ce777bf2d01f27de2606492bd1f0a90966885e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 11:09:14 +0000

    Bug 1526257 - (part 2) Remove reundant IsAboutToBeFinalised calls r=arai
    The new cellIter() now filters out cells that are about to be finalised.
    
    Differential Revision: https://phabricator.services.mozilla.com/D19891
    
    UltraBlame original commit: a9070349961bfb3f7e491e1d31dde04c49145acb

--- js/src/vm/Realm.cpp
+++ js/src/vm/Realm.cpp
@@ -691,11 +691,7 @@ static bool AddLazyFunctionsForRealm(JSContext* cx,
   for (auto i = cx->zone()->cellIter<JSObject>(kind); !i.done(); i.next()) {
     JSFunction* fun = &i->as<JSFunction>();
 
-    
-    
-    
-    if (gc::IsAboutToBeFinalizedUnbarriered(&fun) ||
-        fun->realm() != cx->realm()) {
+    if (fun->realm() != cx->realm()) {
       continue;
     }
 
@@ -858,11 +854,8 @@ void Realm::clearScriptNames() { scriptNameMap.reset(); }
 
 void Realm::clearBreakpointsIn(FreeOp* fop, js::Debugger* dbg,
                                HandleObject handler) {
-  for (auto iter = zone()->cellIter<JSScript>(); !iter.done(); iter.next()) {
-    JSScript* script = iter;
-    if (gc::IsAboutToBeFinalizedUnbarriered(&script)) {
-      continue;
-    }
+  for (auto script = zone()->cellIter<JSScript>(); !script.done();
+       script.next()) {
     if (script->realm() == this && script->hasAnyBreakpointsOrStepMode()) {
       script->clearBreakpointsIn(fop, dbg, handler);
     }
