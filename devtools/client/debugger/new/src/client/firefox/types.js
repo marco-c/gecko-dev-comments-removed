9cb894b786e57c5980fc8d3a386cceb763dbbabe
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 09:39:39 +0000

    Bug 1525757 - [release 124] sync from master. r=dwalsh
    UltraBlame original commit: c6d8f7c2e22da3530ad47556e052749eaa39886d

--- devtools/client/debugger/new/src/client/firefox/types.js
+++ devtools/client/debugger/new/src/client/firefox/types.js
@@ -402,7 +402,7 @@ export type BreakpointClient = {
     line: number,
     column: ?number
   },
-  setOptions: (BreakpointOptions) => Promise<BreakpointClient>,
+  setOptions: BreakpointOptions => Promise<BreakpointClient>,
   
   source: SourceClient,
   options: BreakpointOptions
