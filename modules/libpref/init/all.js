aa8eb0d0b5f64d9ce92c77ac79076b8feedcdcd1
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 13:47:27 +0000

    Bug 1517546 - Enable dyanmic module import by default r=smaug
    UltraBlame original commit: 6ce854f480d62475419081de730a80166d4946f4

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -1602,11 +1602,7 @@ pref("javascript.options.streams", true);
 pref("javascript.options.bigint", false);
 
 
-#ifdef NIGHTLY_BUILD
 pref("javascript.options.dynamicImport", true);
-#else
-pref("javascript.options.dynamicImport", false);
-#endif
 
 
 pref("advanced.mailftp",                    false);
