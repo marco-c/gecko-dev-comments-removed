4158c6d6c4ac47a6f6ded54b62ddd08474dbb5ae
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 15:17:24 +0000

    Bug 1430359 - Disable HTTP Cache does not work after use of performance analysis. r=Honza
    Added a condition in the `openStatistics` action to check if Disabled Cache is checked, if yes, do a reload with cache disabled
    
    Differential Revision: https://phabricator.services.mozilla.com/D23856
    
    UltraBlame original commit: 3ad61ef0d62de8bfb569b48fb461386aea41bc33

--- devtools/client/netmonitor/src/actions/ui.js
+++ devtools/client/netmonitor/src/actions/ui.js
@@ -3,7 +3,7 @@
 
 
 "use strict";
-
+const Services = require("Services");
 const {
   ACTIVITY_TYPE,
   OPEN_NETWORK_DETAILS,
@@ -19,6 +19,8 @@ const {
 } = require("../constants");
 
 const { getDisplayedRequests } = require("../selectors/index");
+
+const DEVTOOLS_DISABLE_CACHE_PREF = "devtools.cache.disabled";
 
 
 
@@ -87,6 +89,13 @@ function disableBrowserCache(disabled) {
 function openStatistics(connector, open) {
   if (open) {
     connector.triggerActivity(ACTIVITY_TYPE.RELOAD.WITH_CACHE_ENABLED);
+  } else if (Services.prefs.getBoolPref(DEVTOOLS_DISABLE_CACHE_PREF)) {
+    
+    
+    
+    
+    
+    connector.triggerActivity(ACTIVITY_TYPE.DISABLE_CACHE);
   }
   return {
     type: OPEN_STATISTICS,
