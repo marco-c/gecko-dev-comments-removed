189e93e237efd4bdd643d61b321fab691e7c546b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 23:44:38 +0000

    Bug 1554203 - [mozlog] Move formatter tests to one location and refactor r=jgraham
    As the number of tests have grown considerably, for clarity, this patch
    moves the formatter tests in test_structured.py to test_formatters.py.
    
    I am striking an error on one of the tests since the move.
    `test_base64_other`, which tests that ability to read different characters
    in the HTMLformatter. It appears that whilst moving the tests, `test_end`
    now returns a unicode string, which fails the assertion. Am looking into
    Python 2 v 3 discrepancies, but any guidance would be most welcome. Error
    message will be attached below.
    
    Differential Revision: https://phabricator.services.mozilla.com/D32732
    
    UltraBlame original commit: 43d9e99a44c1cbe180433fc7eb7495af05cbe7c5

--- testing/mozbase/mozlog/mozlog/formatters/html/html.py
+++ testing/mozbase/mozlog/mozlog/formatters/html/html.py
@@ -9,6 +9,7 @@ import base64
 import cgi
 from datetime import datetime
 import os
+import json
 
 from .. import base
 
@@ -158,15 +159,28 @@ class HTMLFormatter(base.BaseFormatter):
                     else:
                         href = content
                 else:
+                    if not isinstance(content, (six.text_type, six.binary_type)):
+                        
+                        content = json.dumps(content)
                     
                     
-                    
-                    utf8_encoded_bytes = six.text_type(content).encode('utf-8',
-                                                                       'xmlcharrefreplace')
-                    b64_encoded_bytes = base64.b64encode(utf8_encoded_bytes)
-                    b64_encoded_str = b64_encoded_bytes.decode()
-                    href = "data:text/html;charset=utf-8;base64,{0}".format(b64_encoded_str)
+                    if isinstance(content, six.text_type):
+                        is_known_utf8 = True
+                        content_bytes = six.text_type(content).encode('utf-8',
+                                                                      'xmlcharrefreplace')
+                    else:
+                        is_known_utf8 = False
+                        content_bytes = content
 
+                    meta = ["text/html"]
+                    if is_known_utf8:
+                        meta.append("charset=utf-8")
+
+                    
+                    
+                    b64_bytes = base64.b64encode(content_bytes)
+                    b64_text = b64_bytes.decode()
+                    href = "data:%s;base64,%s" % (";".join(meta), b64_text)
                 links_html.append(html.a(
                     name.title(),
                     class_=name,
