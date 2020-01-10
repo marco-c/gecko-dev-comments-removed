c299e8072d19c20e1d1239fee241ff9f8f00ccec
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 14:59:57 +0000

    Bug 1577167 - Enable WS to be on by default in Nightly and Beta. r=Honza
    Enable WS to be on by default in Nightly and Beta
    
    Differential Revision: https://phabricator.services.mozilla.com/D43924
    
    UltraBlame original commit: dac2c9556da51433909640df23f531603da1fafa

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -2152,7 +2152,7 @@ pref("devtools.netmonitor.har.pageLoadedTimeout", 1500);
 pref("devtools.netmonitor.har.enableAutoExportToFile", false);
 
 
-#if defined(NIGHTLY_BUILD)
+#if defined(NIGHTLY_BUILD) || defined(MOZ_DEV_EDITION)
   pref("devtools.netmonitor.features.webSockets", true);
 #else
   pref("devtools.netmonitor.features.webSockets", false);
