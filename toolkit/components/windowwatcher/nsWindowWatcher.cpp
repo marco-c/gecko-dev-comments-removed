002abbd104a2687ba2c4444e5f75ed190a723fd5
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 17:57:02 +0000

    Bug 1553804: Part 1 - Don't allow opening content windows with chrome openers. r=nika
    Differential Revision: https://phabricator.services.mozilla.com/D45326
    
    UltraBlame original commit: 1ed250faeb2e8a83167f2d8358a6dc4a396ead78

--- toolkit/components/windowwatcher/nsWindowWatcher.cpp
+++ toolkit/components/windowwatcher/nsWindowWatcher.cpp
@@ -728,8 +728,24 @@ nsresult nsWindowWatcher::OpenWindowInternal(
   
   dom::AutoJSAPI jsapiChromeGuard;
 
+  nsCOMPtr<nsIDOMChromeWindow> chromeWin = do_QueryInterface(aParent);
+
   bool windowTypeIsChrome =
       chromeFlags & nsIWebBrowserChrome::CHROME_OPENAS_CHROME;
+
+  if (!aForceNoOpener) {
+    if (chromeWin && !windowTypeIsChrome) {
+      NS_WARNING(
+          "Content windows may never have chrome windows as their openers.");
+      return NS_ERROR_INVALID_ARG;
+    }
+    if (aParent && !chromeWin && windowTypeIsChrome) {
+      NS_WARNING(
+          "Chrome windows may never have content windows as their openers.");
+      return NS_ERROR_INVALID_ARG;
+    }
+  }
+
   if (isCallerChrome && !hasChromeParent && !windowTypeIsChrome) {
     
     
@@ -776,7 +792,6 @@ nsresult nsWindowWatcher::OpenWindowInternal(
     
     
     
-    nsCOMPtr<nsIDOMChromeWindow> chromeWin = do_QueryInterface(aParent);
     if (!aDialog && !chromeWin &&
         !(chromeFlags & (nsIWebBrowserChrome::CHROME_MODAL |
                          nsIWebBrowserChrome::CHROME_OPENAS_DIALOG |
