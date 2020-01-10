fe2f715bcc42c230cc859b558e0784994c85aa3c
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 21:56:12 +0000

    Bug 1590384 [wpt PR 19826] - wake lock: Expand IDL harness tests, a=testonly
    Automatic update from web-platform-tests
    wake lock: Expand IDL harness tests (#19826)
    
    As mentioned in #19738, the IDL tests need more content, especially now that
    interfaces/wake-lock.idl has been updated to match the latest version of the
    spec.
    
    * Add Navigator/WorkerNavigator to the objects we test.
    * Add WakeLockSentinel to the objects we test by actually requesting and
      then releasing a lock.
    --
    
    wpt-commits: 977a8e18be0adad48ced728de64e0b4497ca723b
    wpt-pr: 19826
    
    UltraBlame original commit: 0705d26f987f799bda1acbda809b260d0908d42f

--- testing/web-platform/tests/wake-lock/idlharness.https.any.js
+++ testing/web-platform/tests/wake-lock/idlharness.https.any.js
@@ -8,9 +8,20 @@
 idl_test(
   ['wake-lock'],
   ['dom', 'html', 'permissions'],
-  idl_array => {
+  async idl_array => {
+    if (self.GLOBAL.isWorker()) {
+      idl_array.add_objects({ WorkerNavigator: ['navigator'] });
+    } else {
+      idl_array.add_objects({ Navigator: ['navigator'] });
+    }
     idl_array.add_objects({
       WakeLock: ['navigator.wakeLock'],
+      WakeLockSentinel: ['sentinel'],
     });
+
+    
+    
+    self.sentinel = await navigator.wakeLock.request('screen');
+    self.sentinel.release();
   }
 );
