2dc004e6b5b96a63b8b7acb5323dab94865e3c38
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 14:22:58 +0000

    Bug 1574592 - Add CompositorWidget::GetNativeLayerRoot. r=mattwoodrow
    This will give the layer manager direct access to the native layers.
    
    Differential Revision: https://phabricator.services.mozilla.com/D42400
    
    UltraBlame original commit: de0c56e1ffad7de4e606b77a1f7156772f857569

--- widget/CompositorWidget.h
+++ widget/CompositorWidget.h
@@ -11,6 +11,7 @@
 #include "mozilla/gfx/2D.h"
 #include "mozilla/layers/CompositorOptions.h"
 #include "mozilla/layers/LayersTypes.h"
+#include "mozilla/layers/NativeLayer.h"
 
 class nsIWidget;
 class nsBaseWidget;
@@ -122,6 +123,10 @@ class CompositorWidget {
 
 
   virtual void DoCompositorCleanup() {}
+
+  virtual RefPtr<layers::NativeLayerRoot> GetNativeLayerRoot() {
+    return nullptr;
+  }
 
   
 
