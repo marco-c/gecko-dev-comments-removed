20f15cd7d8c08e79e44a243d83993d2a6fa0564f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 14:46:42 +0000

    Bug 1575352 - Remove dynamic linking of urlmon for MinGW. r=aklotz
    4eca0f08c43b73dc1dd908fad58bdfd7f6973119 introduced dynamic linking of urlmon
    for MinGW as a workaround.  Now that MinGW was updated, we can remove that
    workaround.
    
    Differential Revision: https://phabricator.services.mozilla.com/D43281
    
    UltraBlame original commit: 137971f64c25356ca3a3a6e37fa879e301a4528f

--- widget/windows/UrlmonHeaderOnlyUtils.h
+++ widget/windows/UrlmonHeaderOnlyUtils.h
@@ -39,21 +39,6 @@ inline LauncherResult<_bstr_t> UrlmonValidateUri(const wchar_t* aUri) {
     return LAUNCHER_ERROR_FROM_RESULT(pidlResult);
   }
 
-#ifndef __MINGW32__
-  const auto createUri = CreateUri;
-#else
-  HMODULE urlmonDll = GetModuleHandleW(L"urlmon");
-  if (!urlmonDll) {
-    return LAUNCHER_ERROR_FROM_LAST();
-  }
-
-  const auto createUri = reinterpret_cast<decltype(CreateUri)*>(
-      GetProcAddress(urlmonDll, "CreateUri"));
-  if (!createUri) {
-    return LAUNCHER_ERROR_FROM_LAST();
-  }
-#endif
-
   
   
   
@@ -63,7 +48,7 @@ inline LauncherResult<_bstr_t> UrlmonValidateUri(const wchar_t* aUri) {
       Uri_CREATE_CRACK_UNKNOWN_SCHEMES | Uri_CREATE_PRE_PROCESS_HTML_URI |
       Uri_CREATE_IE_SETTINGS;
   RefPtr<IUri> uri;
-  HRESULT hr = createUri(aUri, flags, 0, getter_AddRefs(uri));
+  HRESULT hr = CreateUri(aUri, flags, 0, getter_AddRefs(uri));
   if (FAILED(hr)) {
     return LAUNCHER_ERROR_FROM_HRESULT(hr);
   }
