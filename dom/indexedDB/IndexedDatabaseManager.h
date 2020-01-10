9429f5861329c6e7b681bf7242dc1f95ed3e135f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 22:25:04 +0000

    Bug 1168606 - Introduce preference for maximum number of extra records to preload, defaulting to 0/off for now. r=ttung,asuth
    Differential Revision: https://phabricator.services.mozilla.com/D43466
    
    UltraBlame original commit: 9d8b02674d45531c6be74da899131e905b319ea3

--- dom/indexedDB/IndexedDatabaseManager.h
+++ dom/indexedDB/IndexedDatabaseManager.h
@@ -97,6 +97,10 @@ class IndexedDatabaseManager final {
 
   static bool PreprocessingEnabled();
 
+  
+  
+  static int32_t MaxPreloadExtraRecords();
+
   void ClearBackgroundActor();
 
   already_AddRefed<FileManager> GetFileManager(PersistenceType aPersistenceType,
