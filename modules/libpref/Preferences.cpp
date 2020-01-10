494dd3fafffd23a1d13804522345f00e439c933a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 01:31:32 +0000

    Backed out changeset c0931707a491 (bug 1556131) for assertion failures in StaticPrefList.h CLOSED TREE
    UltraBlame original commit: de4cfe3bb323d74d73a57dfa7671c423bb0ce3dd

--- modules/libpref/Preferences.cpp
+++ modules/libpref/Preferences.cpp
@@ -87,7 +87,6 @@
 #include "PLDHashTable.h"
 #include "plstr.h"
 #include "prlink.h"
-#include "xpcpublic.h"
 
 #ifdef DEBUG
 #  include <map>
@@ -1692,8 +1691,7 @@ static void NotifyCallbacks(const char* aPrefName, const PrefWrapper* aPref) {
   }
 
 #ifdef DEBUG
-  if (XRE_IsParentProcess() &&
-      (StaticPrefs::preferences_check_once_policy() || xpc::IsInAutomation())) {
+  if (XRE_IsParentProcess() && StaticPrefs::preferences_check_once_policy()) {
     
     
     
