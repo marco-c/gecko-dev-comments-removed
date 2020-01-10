2fc2e962178d2591b170d34474f78f6377dbb058
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 23:06:53 +0000

    Bug 1595368: [firefox-ci] Use consitent logic to determine tooltool url; r=sfraser a=task-fix
    Differential Revision: https://phabricator.services.mozilla.com/D52498
    
    Depends on D52497
    
    UltraBlame original commit: e15867a663717a5866759bdb1162431f91bcf1eb

--- testing/mozharness/configs/builds/build_pool_specifics.py
+++ testing/mozharness/configs/builds/build_pool_specifics.py
@@ -3,9 +3,6 @@
 
 config = {
     "taskcluster": {
-        
-        "tooltool_servers": ['http://taskcluster/tooltool.mozilla-releng.net/'],
-        "tooltool_url": 'http://taskcluster/tooltool.mozilla-releng.net/',
         'upload_env': {
             'UPLOAD_PATH': '/builds/worker/artifacts',
         },
