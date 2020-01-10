e52db5c3d45b6cfadee61b582cd9d6b071995ac7
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 14:37:44 +0000

    Bug 1558958 [wpt PR 17301] - bluetooth: Implement getAvailability(), a=testonly
    Automatic update from web-platform-tests
    bluetooth: Implement getAvailability()
    
    This change implements the getAvailability() method for
    navigator.bluetooth as defined in the specification.
    
    Bug: 707640
    Change-Id: I9e9b3e7f8ea7f259e975f71cb6d9570e5f04b479
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1651516
    Reviewed-by: Chris Harrelson <chrishtrchromium.org>
    Reviewed-by: Giovanni Ortuño Urquidi <ortunochromium.org>
    Reviewed-by: Kinuko Yasuda <kinukochromium.org>
    Commit-Queue: Ovidio de Jesús Ruiz-Henríquez <odejesushchromium.org>
    Auto-Submit: Ovidio de Jesús Ruiz-Henríquez <odejesushchromium.org>
    Cr-Commit-Position: refs/heads/master{#688987}
    
    --
    
    wpt-commits: 4bea59463f988fe991f485b05c4fbb0fc2f00d7d
    wpt-pr: 17301
    
    UltraBlame original commit: d1a30366320a2327da5d87ea9cc03901553c6f4d

--- testing/web-platform/tests/resources/chromium/web-bluetooth-test.js
+++ testing/web-platform/tests/resources/chromium/web-bluetooth-test.js
@@ -82,6 +82,7 @@ class FakeBluetooth {
     this.fake_bluetooth_ptr_ = new bluetooth.mojom.FakeBluetoothPtr();
     Mojo.bindInterface(bluetooth.mojom.FakeBluetooth.name,
         mojo.makeRequest(this.fake_bluetooth_ptr_).handle, 'process');
+    this.fake_central_ = null;
   }
 
   
@@ -105,12 +106,16 @@ class FakeBluetooth {
   
   
   async simulateCentral({state}) {
+    if (this.fake_central_)
+      throw 'simulateCentral() should only be called once';
+
     await this.setLESupported(true);
 
     let {fakeCentral: fake_central_ptr} =
       await this.fake_bluetooth_ptr_.simulateCentral(
         toMojoCentralState(state));
-    return new FakeCentral(fake_central_ptr);
+    this.fake_central_ = new FakeCentral(fake_central_ptr);
+    return this.fake_central_;
   }
 
   
@@ -200,6 +205,16 @@ class FakeCentral {
         new bluetooth.mojom.ScanResult(scanResult));
 
     return this.fetchOrCreatePeripheral_(scanResult.deviceAddress);
+  }
+
+  
+  
+  
+  
+  
+  
+  async setState(state) {
+    await this.fake_central_ptr_.setState(toMojoCentralState(state));
   }
 
   
