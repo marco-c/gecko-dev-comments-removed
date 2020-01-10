5f98f05e9118cd7b5fd212aa77950ec3c200af56
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 17:39:37 +0000

    Bug 1569183: Create an ipdl struct called WebrtcProxyConfig, to cut down on the arg count on lots of functions. r=mjf,mayhemer
    Differential Revision: https://phabricator.services.mozilla.com/D45562
    
    UltraBlame original commit: 8196045d38b50e04313725215c36ba1618c50486

--- media/mtransport/nr_socket_proxy_config.h
+++ media/mtransport/nr_socket_proxy_config.h
@@ -8,21 +8,15 @@
 #define nr_socket_proxy_config__
 
 #include <memory>
-#include "nsString.h"
-
-class nsIPrincipal;
 
 namespace mozilla {
 namespace net {
-class LoadInfoArgs;
+class WebrtcProxyConfig;
 }
 
 class NrSocketProxyConfig {
  public:
-  typedef enum { kDisableProxy, kEnableProxy, kForceProxy } ProxyPolicy;
-
-  NrSocketProxyConfig(uint64_t aTabId, const nsCString& aAlpn,
-                      const net::LoadInfoArgs& aArgs, ProxyPolicy aProxyPolicy);
+  explicit NrSocketProxyConfig(const net::WebrtcProxyConfig& aProxyConfig);
 
   
   
@@ -31,10 +25,8 @@ class NrSocketProxyConfig {
 
   ~NrSocketProxyConfig();
 
-  uint64_t GetTabId() const;
-  const nsCString& GetAlpn() const;
-  const net::LoadInfoArgs& GetLoadInfoArgs() const;
-  ProxyPolicy GetProxyPolicy() const;
+  const net::WebrtcProxyConfig& GetConfig() const;
+  bool GetForceProxy() const;
 
  private:
   
