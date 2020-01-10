325e21186102fcacc5dbc369d0a33d445868a250
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 01:14:41 +0000

    Bug 1551490 - Fix failures when browser_update_checkForUpdate run with HTML about:addons enabled. r=aswan
    Depends on D31778
    
    Differential Revision: https://phabricator.services.mozilla.com/D31779
    
    UltraBlame original commit: d316cf637ff946a374708b8c6fbaf977c84a9b83

--- browser/base/content/test/webextensions/head.js
+++ browser/base/content/test/webextensions/head.js
@@ -419,13 +419,25 @@ async function interactiveUpdateTest(autoUpdate, checkFn) {
     if (manualUpdatePromise) {
       await manualUpdatePromise;
 
-      let list = win.document.getElementById("addon-list");
-
-      
-      list.clientHeight;
-
-      let item = list.itemChildren.find(_item => _item.value == ID);
-      EventUtils.synthesizeMouseAtCenter(item._updateBtn, {}, win);
+      if (win.useHtmlViews) {
+        
+        const availableUpdates = win.document.getElementById("updates-manualUpdatesFound-btn");
+        availableUpdates.click();
+        let doc = win.getHtmlBrowser().contentDocument;
+        let card = await BrowserTestUtils.waitForCondition(() => {
+         return doc.querySelector(`addon-card[addon-id="${ID}"]`);
+        }, `Wait addon card for "${ID}"`);
+        let updateBtn = card.querySelector('panel-item[action="install-update"]');
+        ok(updateBtn, `Found update button for "${ID}"`);
+        updateBtn.click();
+      } else {
+        
+        let list = win.document.getElementById("addon-list");
+        
+        list.clientHeight;
+        let item = list.itemChildren.find(_item => _item.value == ID);
+        EventUtils.synthesizeMouseAtCenter(item._updateBtn, {}, win);
+      }
     }
 
     return {promise};
