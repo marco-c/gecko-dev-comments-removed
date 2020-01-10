d31f8b95fd84a5ea770ac25b3b132d15a17f9513
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 14:12:46 +0000

    Bug 1575924 - Final clang-tidy checks for Resist Fingerprinting r=timhuang
    Deleting a null pointer has no effect. Else after return.
    
    Depends on D43108
    
    Differential Revision: https://phabricator.services.mozilla.com/D43109
    
    UltraBlame original commit: a7a5fdf0f2cb673f06d71a801cdaba9209943069

--- toolkit/components/resistfingerprinting/nsRFPService.cpp
+++ toolkit/components/resistfingerprinting/nsRFPService.cpp
@@ -345,9 +345,9 @@ nsresult nsRFPService::RandomMidpoint(long long aClampedTimeUSec,
     
     if (aSecretSeed != nullptr) {
       StaticMutexAutoLock lock(sLock);
-      if (sSecretMidpointSeed != nullptr) {
-        delete[] sSecretMidpointSeed;
-      }
+
+      delete[] sSecretMidpointSeed;
+
       sSecretMidpointSeed = new uint8_t[kSeedSize];
       memcpy(sSecretMidpointSeed, aSecretSeed, kSeedSize);
     }
@@ -474,12 +474,11 @@ double nsRFPService::ReduceTimePrecisionImpl(double aTime, TimeScale aTimeScale,
   if (!IsTimerPrecisionReductionEnabled(aType)) {
     if (!StaticPrefs::privacy_reduceTimerPrecision_unconditional()) {
       return aTime;
-    } else {
-      unconditionalClamping = true;
-      aResolutionUSec = RFP_TIMER_UNCONDITIONAL_VALUE;  
-      aContextMixin = 0;  
-                          
     }
+    unconditionalClamping = true;
+    aResolutionUSec = RFP_TIMER_UNCONDITIONAL_VALUE;  
+    aContextMixin = 0;  
+                        
   }
 
   if (aResolutionUSec <= 0) {
