a2fc4795af7fb57bfc93ba5c19905b6b135f1abe
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 00:00:44 +0000

    Bug 1196094 - disable _exit in child processes when generating profile information, r=froydnj
    Differential Revision: https://phabricator.services.mozilla.com/D33269
    
    UltraBlame original commit: 4b78f4fb473c487bb403987db4cbe011426ccb42

--- xpcom/base/nscore.h
+++ xpcom/base/nscore.h
@@ -167,8 +167,14 @@
 
 
 
+
+
+
+
+
 #if defined(NS_BUILD_REFCNT_LOGGING) || defined(MOZ_VALGRIND) || \
-    defined(MOZ_ASAN) || defined(MOZ_CODE_COVERAGE)
+    defined(MOZ_ASAN) || defined(MOZ_CODE_COVERAGE) ||           \
+    defined(MOZ_PROFILE_GENERATE)
 #  define NS_FREE_PERMANENT_DATA
 #endif
 
