609d24a0ba9f78eba2dfc07e1355e194dd72c0c6
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 15:08:29 +0000

    Bug 1575240 - inspect(x) should show x in the appropriate panel. r=nchevobbe
    Differential Revision: https://phabricator.services.mozilla.com/D42747
    
    UltraBlame original commit: 74fce607d8a711ca1c947a5c1fa351637ba04d5d

--- devtools/client/framework/toolbox.js
+++ devtools/client/framework/toolbox.js
@@ -3389,23 +3389,21 @@ Toolbox.prototype = {
       objectActor.preview &&
       objectActor.preview.nodeType === domNodeConstants.ELEMENT_NODE
     ) {
+      return this.viewElementInInspector(objectActor, inspectFromAnnotation);
+    }
+
+    if (objectActor.class == "Function") {
+      const { url, line } = objectActor.location;
+      return this.viewSourceInDebugger(url, line);
+    }
+
+    if (objectActor.type !== "null" && objectActor.type !== "undefined") {
       
-      await this.loadTool("inspector");
-      const inspector = this.getPanel("inspector");
-      const nodeFound = await inspector.inspectNodeActor(
-        objectActor.actor,
-        inspectFromAnnotation
-      );
-      if (nodeFound) {
-        await this.selectTool("inspector");
+      
+      if (this.currentToolId != "webconsole") {
+        await this.openSplitConsole();
       }
-    } else if (
-      objectActor.type !== "null" &&
-      objectActor.type !== "undefined"
-    ) {
-      
-      
-      await this.openSplitConsole();
+
       const panel = this.getPanel("webconsole");
       panel.hud.ui.inspectObjectActor(objectActor);
     }
@@ -3793,6 +3791,19 @@ Toolbox.prototype = {
       sourceLine,
       sourceColumn
     );
+  },
+
+  viewElementInInspector: async function(objectActor, inspectFromAnnotation) {
+    
+    await this.loadTool("inspector");
+    const inspector = this.getPanel("inspector");
+    const nodeFound = await inspector.inspectNodeActor(
+      objectActor.actor,
+      inspectFromAnnotation
+    );
+    if (nodeFound) {
+      await this.selectTool("inspector");
+    }
   },
 
   
