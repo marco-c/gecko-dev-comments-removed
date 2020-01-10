2a825c0a8a63ec2a3e8fc91b0b686ab367337ad5
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 13:47:38 +0000

    Bug 1210157 - Add Python 3 support to mozboot.base and mozboot.bootstrap r=firefox-build-system-reviewers,mshal
    Add support for both Python 3 and Python 2.7 to the mozboot.base and
    mozboot.bootstrap modules.  Remove legacy Python 2.6 code or mark it for later
    removal.
    
    Depends on D39359
    
    Differential Revision: https://phabricator.services.mozilla.com/D39360
    
    UltraBlame original commit: 5d26b3d7d4a0005c695d95001ed32494669b1888

--- python/mozboot/mozboot/bootstrap.py
+++ python/mozboot/mozboot/bootstrap.py
@@ -8,12 +8,17 @@ import platform
 import sys
 import os
 import subprocess
-try:
+
+
+
+
+if sys.version_info < (3,):
     from ConfigParser import (
         Error as ConfigParserError,
         RawConfigParser,
     )
-except ImportError:
+    input = raw_input
+else:
     from configparser import (
         Error as ConfigParserError,
         RawConfigParser,
@@ -56,6 +61,7 @@ APPLICATIONS_LIST = [
     ('GeckoView/Firefox for Android Artifact Mode', 'mobile_android_artifact_mode'),
     ('GeckoView/Firefox for Android', 'mobile_android'),
 ]
+
 
 
 
@@ -217,7 +223,7 @@ def update_or_create_build_telemetry_config(path):
     if not config.has_section('build'):
         config.add_section('build')
     config.set('build', 'telemetry', 'true')
-    with open(path, 'wb') as f:
+    with open(path, 'w') as f:
         config.write(f)
     return True
 
@@ -297,7 +303,7 @@ class Bootstrapper(object):
         print(CLONE_VCS.format(repo_name, vcs))
 
         while True:
-            dest = raw_input(CLONE_VCS_PROMPT.format(vcs))
+            dest = input(CLONE_VCS_PROMPT.format(vcs))
             dest = dest.strip()
             if not dest:
                 return ''
@@ -642,7 +648,8 @@ def current_firefox_checkout(check_output, env, hg=None):
             try:
                 node = check_output([hg, 'log', '-r', '0', '--template', '{node}'],
                                     cwd=path,
-                                    env=env)
+                                    env=env,
+                                    universal_newlines=True)
                 if node in HG_ROOT_REVISIONS:
                     return ('hg', path)
                 
