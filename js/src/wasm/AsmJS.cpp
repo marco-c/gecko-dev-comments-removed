a2ebe6db02e73c2edc45ca6b7aaba2a625b1cbf5
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 11:08:11 +0000

    Bug 1570343: Guard the use of wasm ref types opcodes with dynamic checks; r=luke
    Differential Revision: https://phabricator.services.mozilla.com/D40057
    
    UltraBlame original commit: 73ae74f44aa6dc86d92d61c88351928a6defb244

--- js/src/wasm/AsmJS.cpp
+++ js/src/wasm/AsmJS.cpp
@@ -1355,9 +1355,9 @@ class MOZ_STACK_CLASS JS_HAZ_ROOTED ModuleValidatorShared {
         funcImportMap_(cx),
         arrayViews_(cx),
         compilerEnv_(CompileMode::Once, Tier::Optimized, OptimizedBackend::Ion,
-                     DebugEnabled::False,  false),
-        env_( false, &compilerEnv_, Shareable::False,
-             ModuleKind::AsmJS) {
+                     DebugEnabled::False,  false,
+                      false),
+        env_(&compilerEnv_, Shareable::False, ModuleKind::AsmJS) {
     compilerEnv_.computeParameters( false);
     env_.minMemoryLength = RoundUpToNextValidAsmJSHeapLength(0);
   }
