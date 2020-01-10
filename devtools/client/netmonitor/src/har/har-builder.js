8bddcd3379857abe144fdc1ca57676f8c4ec143d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 01:54:37 +0000

    Backed out changeset b4f0ee392b97 (bug 1174100) for failing at browser_net_har_import.js on a CLOSED TREE.
    UltraBlame original commit: bef3fa175d99de7fef6e1c34e94d6eba50719f35

--- devtools/client/netmonitor/src/har/har-builder.js
+++ devtools/client/netmonitor/src/har/har-builder.js
@@ -112,7 +112,7 @@ HarBuilder.prototype = {
 
     entry.request = await this.buildRequest(file);
     entry.response = await this.buildResponse(file);
-    entry.cache = await this.buildCache(file);
+    entry.cache = this.buildCache(file);
     entry.timings = eventTimings ? eventTimings.timings : {};
 
     
@@ -403,19 +403,18 @@ HarBuilder.prototype = {
     return content;
   },
 
-  buildCache: async function(file) {
+  buildCache: function(file) {
     const cache = {};
 
-    
-    if (file.status !== "304") {
+    if (!file.fromCache) {
       return cache;
     }
 
-    if (file.responseCacheAvailable && this._options.requestData) {
-      const responseCache = await this._options.requestData(file.id, "responseCache");
-      if (responseCache.cache) {
-        cache.afterRequest = this.buildCacheEntry(responseCache.cache);
-      }
+    
+    
+
+    if (file.cacheEntry) {
+      cache.afterRequest = this.buildCacheEntry(file.cacheEntry);
     } else {
       cache.afterRequest = null;
     }
@@ -426,12 +425,10 @@ HarBuilder.prototype = {
   buildCacheEntry: function(cacheEntry) {
     const cache = {};
 
-    if (typeof cacheEntry !== "undefined") {
-      cache.expires = cacheEntry.expires;
-      cache.lastAccess = cacheEntry.lastFetched;
-      cache.eTag = "";
-      cache.hitCount = cacheEntry.fetchCount;
-    }
+    cache.expires = findValue(cacheEntry, "Expires");
+    cache.lastAccess = findValue(cacheEntry, "Last Fetched");
+    cache.eTag = "";
+    cache.hitCount = findValue(cacheEntry, "Fetch Count");
 
     return cache;
   },
