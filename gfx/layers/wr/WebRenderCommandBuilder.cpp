d9d048b5a1174e5c503e4c890e2b518fc66f4fc9
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 11:01:33 +0000

    Bug 1570399. Revert the adjustment of the visible rect from bug 1565904. r=nical
    This change was unintentional and may be responsible for the performance regression seen in
    bug 1570256.
    
    Differential Revision: https://phabricator.services.mozilla.com/D40085
    
    UltraBlame original commit: 2e775f77d3ab9b31dd70c22946c733473c55411d

--- gfx/layers/wr/WebRenderCommandBuilder.cpp
+++ gfx/layers/wr/WebRenderCommandBuilder.cpp
@@ -1526,6 +1526,10 @@ void WebRenderCommandBuilder::DoGroupingForDisplayList(
                              untransformedPaintRect, scale.width, scale.height,
                              group.mAppUnitsPerDevPixel, residualOffset))
                          .Intersect(group.mLayerBounds);
+  
+  
+  
+  group.mPaintRect = group.mPaintRect - group.mLayerBounds.TopLeft();
 
   g.mTransform = Matrix::Scaling(scale.width, scale.height)
                      .PostTranslate(residualOffset.x, residualOffset.y);
