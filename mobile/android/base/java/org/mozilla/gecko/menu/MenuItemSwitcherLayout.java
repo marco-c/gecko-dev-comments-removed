57e9f1056819542f98579ea1d58e66991cfbef93
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 04:42:01 +0000

    Bug 1529557 - Mutate share drawable to prevent against it being reused from cache; r=VladBaicu
    Summary:
    The share drawable is used in multiple places in code.
    Android would cache it and subsequently serve a now dirty version of it with
    transformations potentially added.
    By mutating it into a new drawable we ensure we'll always have it clean.
    
    Reviewers: VladBaicu
    
    Reviewed By: VladBaicu
    
    Subscribers: emilio
    
    Bug #: 1529557
    
    Differential Revision: https://phabricator.services.mozilla.com/D37734
    
    UltraBlame original commit: 395584e3f86a3e2d7b6eb623e1c3daffae200705

--- mobile/android/base/java/org/mozilla/gecko/menu/MenuItemSwitcherLayout.java
+++ mobile/android/base/java/org/mozilla/gecko/menu/MenuItemSwitcherLayout.java
@@ -9,9 +9,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.mozilla.gecko.R;
+import org.mozilla.gecko.util.BitmapUtils;
 
 import android.annotation.TargetApi;
 import android.content.Context;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
@@ -118,6 +120,9 @@ public class MenuItemSwitcherLayout extends LinearLayout
     }
 
     public void setIcon(Drawable icon) {
+        
+        
+        icon = new BitmapDrawable(BitmapUtils.getBitmapFromDrawable(icon));
         mMenuItem.setIcon(icon);
         mMenuButton.setIcon(icon);
     }
