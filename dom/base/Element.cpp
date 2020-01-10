cd788422bf2d5ab8a51b749564ab71109b5515c9
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 18:49:19 +0000

    Bug 1514926 - Followup: remove now unused arrowscrollbox check from MayNeedToLoadXBLBinding. r=emilio
    Differential Revision: https://phabricator.services.mozilla.com/D48023
    
    UltraBlame original commit: 6e8f0afa223c278413b9f5464200d71451650461

--- dom/base/Element.cpp
+++ dom/base/Element.cpp
@@ -518,8 +518,7 @@ void Element::ClearStyleStateLocks() {
 }
 
 static bool MayNeedToLoadXBLBinding(const Element& aElement) {
-  if (!aElement.IsAnyOfXULElements(nsGkAtoms::panel, nsGkAtoms::textbox,
-                                   nsGkAtoms::arrowscrollbox)) {
+  if (!aElement.IsAnyOfXULElements(nsGkAtoms::panel, nsGkAtoms::textbox)) {
     
     
     return false;
