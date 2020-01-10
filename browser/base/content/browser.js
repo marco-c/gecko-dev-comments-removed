ae9362c0892c025583e83275c79b163b83237434
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 18:32:37 +0000

    Bug 1584076 - Hide the 'Firefox Services' label when there are no services. r=vbudhram
    Differential Revision: https://phabricator.services.mozilla.com/D47720
    
    UltraBlame original commit: d726e857c5507202bbf0033ef2b9162068016c81

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -695,8 +695,10 @@ function updateFxaToolbarMenu(enable, isInitialUpdate = false) {
     document.getElementById(
       "PanelUI-fxa-menu-monitor-button"
     ).hidden = !gFxaMonitorLoginUrl;
-    document.getElementById("fxa-menu-service-separator").hidden =
-      !gFxaSendLoginUrl && !gFxaMonitorLoginUrl;
+    
+    let hideSvcs = !gFxaSendLoginUrl && !gFxaMonitorLoginUrl;
+    document.getElementById("fxa-menu-service-separator").hidden = hideSvcs;
+    document.getElementById("fxa-menu-service-label").hidden = hideSvcs;
 
     document.getElementById(
       "fxa-menu-device-name-label"
