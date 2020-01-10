7e5b1072695ec044305ad9cb80a48f84c9a9a8f6
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 16:22:43 +0000

    Bug 1579053 - use relative nodeFront's target in pageStyles; r=gl
    Differential Revision: https://phabricator.services.mozilla.com/D44821
    
    UltraBlame original commit: 1b465f93bbfb4301cbb4aa1bde14b3c47d24056d

--- devtools/client/inspector/boxmodel/box-model.js
+++ devtools/client/inspector/boxmodel/box-model.js
@@ -172,13 +172,16 @@ BoxModel.prototype = {
         return null;
       }
 
-      const node = this.inspector.selection.nodeFront;
+      const nodeFront = this.inspector.selection.nodeFront;
+      const inspectorFront = nodeFront.inspectorFront;
+      
+      const pageStyle = await inspectorFront.getPageStyle();
 
-      let layout = await this.inspector.pageStyle.getLayout(node, {
+      let layout = await pageStyle.getLayout(nodeFront, {
         autoMargins: true,
       });
 
-      const styleEntries = await this.inspector.pageStyle.getApplied(node, {
+      const styleEntries = await pageStyle.getApplied(nodeFront, {
         
         skipPseudo: true,
       });
@@ -186,22 +189,22 @@ BoxModel.prototype = {
 
       
       
-      const isPositionEditable = await this.inspector.pageStyle.isPositionEditable(
-        node
-      );
+      const isPositionEditable = await pageStyle.isPositionEditable(nodeFront);
 
       layout = Object.assign({}, layout, {
         isPositionEditable,
       });
 
-      const actorCanGetOffSetParent = await this.inspector.currentTarget.actorHasMethod(
+      const actorCanGetOffSetParent = await nodeFront.targetFront.actorHasMethod(
         "domwalker",
         "getOffsetParent"
       );
 
       if (actorCanGetOffSetParent) {
         
-        const offsetParent = await this.inspector.walker.getOffsetParent(node);
+        const offsetParent = await inspectorFront.walker.getOffsetParent(
+          nodeFront
+        );
         this.store.dispatch(updateOffsetParent(offsetParent));
       }
 
