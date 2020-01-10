8cc8fd44705b706f63aa5c3fd17d533ea30b402f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 16:52:32 +0000

    Merge mozilla-central to mozilla-inbound
    UltraBlame original commit: 0b0c20ea1aa0062bcadeaffb68923368f0f18347

--- dom/security/nsCSPParser.cpp
+++ dom/security/nsCSPParser.cpp
@@ -467,6 +467,22 @@ nsCSPBaseSrc* nsCSPParser::keywordSource() {
     }
     return new nsCSPKeywordSrc(CSP_UTF16KeywordToEnum(mCurToken));
   }
+
+  if (CSP_IsKeyword(mCurToken, CSP_UNSAFE_ALLOW_REDIRECTS)) {
+    if (!CSP_IsDirective(mCurDir[0],
+                         nsIContentSecurityPolicy::NAVIGATE_TO_DIRECTIVE)) {
+      
+      AutoTArray<nsString, 2> params = {
+          NS_LITERAL_STRING("unsafe-allow-redirects"),
+          NS_LITERAL_STRING("navigate-to")};
+      logWarningErrorToConsole(nsIScriptError::warningFlag,
+                               "IgnoringSourceWithinDirective", params);
+      return nullptr;
+    }
+
+    return new nsCSPKeywordSrc(CSP_UTF16KeywordToEnum(mCurToken));
+  }
+
   return nullptr;
 }
 
@@ -855,6 +871,19 @@ nsCSPDirective* nsCSPParser::directiveName() {
     AutoTArray<nsString, 1> params = {mCurToken};
     logWarningErrorToConsole(nsIScriptError::warningFlag,
                              "notSupportingDirective", params);
+    return nullptr;
+  }
+
+  
+  
+  
+  
+  if (CSP_IsDirective(mCurToken,
+                      nsIContentSecurityPolicy::NAVIGATE_TO_DIRECTIVE) &&
+      !StaticPrefs::security_csp_enableNavigateTo()) {
+    AutoTArray<nsString, 1> params = {mCurToken};
+    logWarningErrorToConsole(nsIScriptError::warningFlag,
+                             "couldNotProcessUnknownDirective", params);
     return nullptr;
   }
 
