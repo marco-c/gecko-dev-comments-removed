7ef48e5502a3e0a07244b024805a7fdf200792ee
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 22:00:36 +0000

    Bug 1592840 - Fix panic in wrench when replaying the display list. r=ktaeleman,kvark
    Differential Revision: https://phabricator.services.mozilla.com/D51206
    
    UltraBlame original commit: 045f91f9a76f60826a5d0ddcb978a50f722321d2

--- gfx/wr/wrench/src/yaml_frame_reader.rs
+++ gfx/wr/wrench/src/yaml_frame_reader.rs
@@ -441,7 +441,11 @@ impl YamlFrameReader {
         let root_pipeline_id = wrench.root_pipeline_id;
         self.build_pipeline(wrench, root_pipeline_id, &yaml["root"]);
 
-        wrench.renderer.set_external_image_handler(self.external_image_handler.take().unwrap());
+        
+        
+        if let Some(external_image_handler) = self.external_image_handler.take() {
+            wrench.renderer.set_external_image_handler(external_image_handler);
+        }
     }
 
     fn build_pipeline(
