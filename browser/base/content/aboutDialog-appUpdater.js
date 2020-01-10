46fb7cec2307b5e91e023e33d5b919274acc4454
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 01:13:30 +0000

    Bug 1557963.  Stop using [array] in nsIUpdateService.  r=rstrong
    Differential Revision: https://phabricator.services.mozilla.com/D34278
    
    UltraBlame original commit: a3031bdbf14ad7515fd6abca317b10e71ecfcbff

--- browser/base/content/aboutDialog-appUpdater.js
+++ browser/base/content/aboutDialog-appUpdater.js
@@ -264,10 +264,10 @@ appUpdater.prototype =
     
 
 
-    onCheckComplete(aRequest, aUpdates, aUpdateCount) {
+    onCheckComplete(aRequest, aUpdates) {
       gAppUpdater.isChecking = false;
       gAppUpdater.update = gAppUpdater.aus.
-                           selectUpdate(aUpdates, aUpdates.length);
+                           selectUpdate(aUpdates);
       if (!gAppUpdater.update) {
         gAppUpdater.selectPanel("noUpdatesFound");
         return;
