6b997ad51e5e71c59beeb10177a6dfea637a8066
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 02:00:41 +0000

    Bug 1395509 - Track malloc memory used by RegExpStatics objects r=pbone
    I wanted to make VectorMatchPairs use ZoneAllocPolicy but this is also used in a bunch of places where it's not attached to a GC thing, so I left this as a todo.
    
    Differential Revision: https://phabricator.services.mozilla.com/D34556
    
    UltraBlame original commit: 37f4b005af23352ad4c10b5319682c47f80398e0

--- js/src/vm/RegExpStatics.cpp
+++ js/src/vm/RegExpStatics.cpp
@@ -24,7 +24,7 @@ static void resc_finalize(FreeOp* fop, JSObject* obj) {
   MOZ_ASSERT(fop->onMainThread());
   RegExpStatics* res =
       static_cast<RegExpStatics*>(obj->as<RegExpStaticsObject>().getPrivate());
-  fop->delete_(res);
+  fop->delete_(obj, res, MemoryUse::RegExpStatics);
 }
 
 static void resc_trace(JSTracer* trc, JSObject* obj) {
@@ -60,7 +60,9 @@ RegExpStaticsObject* RegExpStatics::create(JSContext* cx) {
   if (!res) {
     return nullptr;
   }
-  obj->setPrivate(static_cast<void*>(res));
+  
+  
+  InitObjectPrivate(obj, res, MemoryUse::RegExpStatics);
   return obj;
 }
 
