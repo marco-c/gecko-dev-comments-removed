67bef6041c9f3b64533931089003ef0e9a124f07
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 22:28:15 +0000

    Bug 1594025 - move 'this site is not deceptive' string into markup to allow help hamburger menu to copy it, r=flod,fluent-reviewers
    The help view copies strings from the main menubar. When we moved the original DTD string
    to ftl, there were performance implications for using it in browser.xhtml, so it was
    only added once needed. The help view copies attributes from the items in the main menubar's
    help menu, and so didn't copy the label for this item, resulting in the broken
    behaviour.
    
    To fix this, it's enough to have the string in the markup. As we've moved the other strings
    into menubar.ftl, I'm taking the opportunity to move this string there, too, next to its
    sibling string to report deceptive sites.
    
    Differential Revision: https://phabricator.services.mozilla.com/D51850
    
    UltraBlame original commit: 489afa3eb121ff1ab70f405880acd757502d1e38

--- browser/base/content/browser-safebrowsing.js
+++ browser/base/content/browser-safebrowsing.js
@@ -25,10 +25,6 @@ var gSafeBrowsing = {
       "menu_HelpPopup_reportPhishingErrortoolmenu"
     );
     reportErrorMenu.hidden = !isPhishingPage;
-    if (isPhishingPage && !reportErrorMenu.hasAttribute("data-l10n-id")) {
-      MozXULElement.insertFTLIfNeeded("browser/safebrowsing/blockedSite.ftl");
-      document.l10n.setAttributes(reportErrorMenu, "safeb-palm-notdeceptive");
-    }
 
     
     
