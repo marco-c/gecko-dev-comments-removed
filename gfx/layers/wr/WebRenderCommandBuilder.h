e88bf8c5cf4cdbc1666042b81834920b66c0473f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 22:34:08 +0000

    Backed out 5 changesets (bug 1554499) for frequent crashes, at least on OS X (bug 1594381). a=backout
    Backed out changeset 3a49bec95338 (bug 1554499)
    Backed out changeset c802ab8cc730 (bug 1554499)
    Backed out changeset f60fee484460 (bug 1554499)
    Backed out changeset a49d1c9e8b14 (bug 1554499)
    Backed out changeset 133cddb65f59 (bug 1554499)
    
    UltraBlame original commit: d9daf28a5559712a8db772aa8388f444a93f4063

--- gfx/layers/wr/WebRenderCommandBuilder.h
+++ gfx/layers/wr/WebRenderCommandBuilder.h
@@ -192,17 +192,8 @@ class WebRenderCommandBuilder final {
       frame->AddProperty(WebRenderUserDataProperty::Key(), userDataTable);
     }
 
-    
-    
-    
-    const uint32_t key =
-        T::Type() == WebRenderUserData::UserDataType::eAnimation
-            ? static_cast<uint32_t>(aItem->GetType())
-            : aItem->GetPerFrameKey();
-
-    RefPtr<WebRenderUserData>& data =
-        userDataTable->GetOrInsert(WebRenderUserDataKey(key, T::Type()));
-
+    RefPtr<WebRenderUserData>& data = userDataTable->GetOrInsert(
+        WebRenderUserDataKey(aItem->GetPerFrameKey(), T::Type()));
     if (!data) {
       data = new T(GetRenderRootStateManager(aRenderRoot), aItem);
       mWebRenderUserDatas.PutEntry(data);
