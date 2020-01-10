edeb383c71cef2128e3f343aed4487ba7fc34aee
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 00:02:17 +0000

    Backed out changeset 764fab821251 (bug 1478037) for crashtest failures on 439184-1.html. CLOSED TREE
    UltraBlame original commit: 35e9a755a4bbd2a3db074702453e82f1930336b7

--- dom/jsurl/nsJSProtocolHandler.cpp
+++ dom/jsurl/nsJSProtocolHandler.cpp
@@ -39,6 +39,7 @@
 #include "nsThreadUtils.h"
 #include "nsIScriptChannel.h"
 #include "mozilla/dom/Document.h"
+#include "nsILoadInfo.h"
 #include "nsIObjectInputStream.h"
 #include "nsIObjectOutputStream.h"
 #include "nsITextToSubURI.h"
@@ -48,9 +49,9 @@
 #include "mozilla/CycleCollectedJSContext.h"
 #include "mozilla/dom/ScriptSettings.h"
 #include "mozilla/dom/PopupBlocker.h"
+#include "nsILoadInfo.h"
 #include "nsContentSecurityManager.h"
 
-#include "mozilla/LoadInfo.h"
 #include "mozilla/Maybe.h"
 #include "mozilla/ipc/URIUtils.h"
 
@@ -160,9 +161,7 @@ nsresult nsJSThunk::EvaluateScript(
 
   
   
-  
-  nsCOMPtr<nsIContentSecurityPolicy> csp =
-      static_cast<mozilla::net::LoadInfo*>(loadInfo.get())->GetCSPToInherit();
+  nsCOMPtr<nsIContentSecurityPolicy> csp = loadInfo->GetCsp();
   if (csp) {
     bool allowsInlineScript = true;
     rv = csp->GetAllowsInline(nsIContentPolicy::TYPE_SCRIPT,
@@ -176,26 +175,33 @@ nsresult nsJSThunk::EvaluateScript(
                               &allowsInlineScript);
 
     
-    if (NS_FAILED(rv) || !allowsInlineScript) {
+    if (!allowsInlineScript) {
       return NS_ERROR_DOM_RETVAL_UNDEFINED;
     }
   }
 
   
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+  csp = nullptr;
+  mozilla::dom::Document* prevDoc = aOriginalInnerWindow->GetExtantDoc();
+  if (prevDoc) {
+    csp = prevDoc->GetCsp();
+  }
+  if (csp) {
+    bool allowsInlineScript = true;
+    rv = csp->GetAllowsInline(nsIContentPolicy::TYPE_SCRIPT,
+                              EmptyString(),  
+                              true,           
+                              nullptr,        
+                              nullptr,        
+                              EmptyString(),  
+                              0,              
+                              0,              
+                              &allowsInlineScript);
+    
+    if (!allowsInlineScript) {
+      return NS_ERROR_DOM_RETVAL_UNDEFINED;
+    }
+  }
 
   
   nsIScriptGlobalObject* global = GetGlobalObject(aChannel);
@@ -204,23 +210,22 @@ nsresult nsJSThunk::EvaluateScript(
   }
 
   
+  
+  mozilla::dom::Document* doc = aOriginalInnerWindow->GetExtantDoc();
+  if (doc && doc->HasScriptsBlockedBySandbox()) {
+    return NS_ERROR_DOM_RETVAL_UNDEFINED;
+  }
+
+  
+  AutoPopupStatePusher popupStatePusher(aPopupState);
+
+  
   nsCOMPtr<nsPIDOMWindowOuter> win = do_QueryInterface(global);
   nsPIDOMWindowInner* innerWin = win->GetCurrentInnerWindow();
 
   if (innerWin != aOriginalInnerWindow) {
     return NS_ERROR_UNEXPECTED;
   }
-
-  mozilla::dom::Document* targetDoc = innerWin->GetExtantDoc();
-
-  
-  
-  if (targetDoc && targetDoc->HasScriptsBlockedBySandbox()) {
-    return NS_ERROR_DOM_RETVAL_UNDEFINED;
-  }
-
-  
-  AutoPopupStatePusher popupStatePusher(aPopupState);
 
   nsCOMPtr<nsIScriptGlobalObject> innerGlobal = do_QueryInterface(innerWin);
 
