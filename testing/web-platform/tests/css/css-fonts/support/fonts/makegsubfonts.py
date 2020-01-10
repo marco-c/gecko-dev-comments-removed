e8cce946e51bffe831eeebbd90856595394b0f0a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 15:12:06 +0000

    Bug 1559975 - convert testing/web-platform to python3 compatible syntax r=jgraham
    Differential Revision: https://phabricator.services.mozilla.com/D37101
    
    UltraBlame original commit: 514f732deb3081429743d8c58902c7f94921f728

--- testing/web-platform/tests/css/css-fonts/support/fonts/makegsubfonts.py
+++ testing/web-platform/tests/css/css-fonts/support/fonts/makegsubfonts.py
@@ -125,7 +125,7 @@ def makeLookup1():
     		if table.format == 4:
     			table.cmap[cp] = glyphName
     		else:
-    			raise NotImplementedError, "Unsupported cmap table format: %d" % table.format
+    			raise NotImplementedError("Unsupported cmap table format: %d" % table.format)
     	cp += 1
 
     	
@@ -146,7 +146,7 @@ def makeLookup1():
     		if table.format == 4:
     			table.cmap[cp] = glyphName
     		else:
-    			raise NotImplementedError, "Unsupported cmap table format: %d" % table.format
+    			raise NotImplementedError("Unsupported cmap table format: %d" % table.format)
 
         
     	cp += 3
@@ -327,7 +327,7 @@ def makeLookup3():
     		if table.format == 4:
     			table.cmap[cp] = glyphName
     		else:
-    			raise NotImplementedError, "Unsupported cmap table format: %d" % table.format
+    			raise NotImplementedError("Unsupported cmap table format: %d" % table.format)
     	cp += 1
 
     	
@@ -348,7 +348,7 @@ def makeLookup3():
     			if table.format == 4:
     				table.cmap[cp] = glyphName
     			else:
-    				raise NotImplementedError, "Unsupported cmap table format: %d" % table.format
+    				raise NotImplementedError("Unsupported cmap table format: %d" % table.format)
     		cp += 1
 
     
