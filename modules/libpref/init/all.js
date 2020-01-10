62c1f80188117d87ac4a28384e2a0f865377ab14
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 00:54:56 +0000

    Bug 1543812 - Add ability to block all autoplay. r=johannh,alwu,flod,fluent-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D30135
    
    UltraBlame original commit: 75a6f48ca07813830b88470606a2145b2f7610e7

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -546,9 +546,6 @@ pref("media.autoplay.default", 0);
 pref("media.autoplay.block-webaudio", false);
 
 
-pref("media.autoplay.allow-muted", true);
-
-
 pref("media.autoplay.allow-extension-background-pages", true);
 
 
