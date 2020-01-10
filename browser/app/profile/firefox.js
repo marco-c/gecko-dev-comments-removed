e0fdf6c8ee08ce853cf76467772137d69779de4e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 17:35:07 +0000

    Bug 1581088 - Enable search on Beta/DevEdition. r=Honza
    Differential Revision: https://phabricator.services.mozilla.com/D46044
    
    UltraBlame original commit: 0d431c4676b2dd1396abe74edcce146f772fdc22

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -2098,7 +2098,7 @@ pref("devtools.serviceWorkers.testing.enabled", false);
 pref("devtools.netmonitor.enabled", true);
 
 
-#if defined(NIGHTLY_BUILD)
+#if defined(NIGHTLY_BUILD) || defined(MOZ_DEV_EDITION)
   pref("devtools.netmonitor.features.search", true);
 #else
   pref("devtools.netmonitor.features.search", false);
