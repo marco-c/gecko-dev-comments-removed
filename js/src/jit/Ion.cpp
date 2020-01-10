131d700e11691639ea2a4d62259b3d0c1516f755
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 19:10:44 +0000

    Backed out changeset a69d0ed7e27c (bug 1573809) for causing multiple failures CLOSED TREE
    UltraBlame original commit: ffe0ac689fc7b0a562cb8946124d2bcd83a792dd

--- js/src/jit/Ion.cpp
+++ js/src/jit/Ion.cpp
@@ -580,15 +580,15 @@ void JitRuntime::SweepJitcodeGlobalTable(JSRuntime* rt) {
   }
 }
 
-void JitRealm::traceWeak(JSTracer* trc, JS::Realm* realm) {
+void JitRealm::sweep(JS::Realm* realm) {
   
   MOZ_ASSERT(!HasOffThreadIonCompile(realm));
 
   stubCodes_->sweep();
 
   for (WeakHeapPtrJitCode& stub : stubs_) {
-    if (stub) {
-      TraceWeakEdge(trc, &stub, "JitRealm::stubs_");
+    if (stub && IsAboutToBeFinalized(&stub)) {
+      stub.set(nullptr);
     }
   }
 }
