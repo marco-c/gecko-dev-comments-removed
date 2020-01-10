757ccdb92ec910f53fbfe33d879719e2ddab287e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 16:51:12 +0000

    Bug 1580645 - Ensure that node removal only removes mutation BPs in subtree. r=davidwalsh
    Differential Revision: https://phabricator.services.mozilla.com/D44296
    
    UltraBlame original commit: 1f0a09043c26c8d315273e22f08b9a3bdd5762a0

--- devtools/server/actors/inspector/walker.js
+++ devtools/server/actors/inspector/walker.js
@@ -2147,10 +2147,25 @@ var WalkerActor = protocol.ActorClassWithSpec(walkerSpec, {
       return;
     }
 
+    
+    
+    const nextWalkerSibling = this._getNextTraversalSibling(targetNode);
+
     const walker = this.getDocumentWalker(targetNode);
     do {
       this._updateMutationBreakpointState("detach", walker.currentNode, null);
-    } while (walker.nextNode());
+    } while (
+      walker.nextNode() &&
+      !(nextWalkerSibling || walker.currentNode !== nextWalkerSibling)
+    );
+  },
+
+  _getNextTraversalSibling(targetNode) {
+    let current = targetNode;
+    while (current && !current.nextSibling) {
+      current = current.parentNode;
+    }
+    return current ? current.nextSibling : null;
   },
 
   
