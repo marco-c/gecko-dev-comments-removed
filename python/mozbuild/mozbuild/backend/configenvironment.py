2a8d16e0f3c29e913bc6de7b1aa2fd4fa6f17ffa
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 14:00:11 +0000

    Bug 844509 - Don't create a separate unicode version of the build config. r=nalexander
    Now that the configuration comes in without bytes strings, there is no
    need to convert it anymore.
    
    Differential Revision: https://phabricator.services.mozilla.com/D42631
    
    UltraBlame original commit: a07ecf63a8975f077d11a847db9426d828b6071e

--- python/mozbuild/mozbuild/backend/configenvironment.py
+++ python/mozbuild/mozbuild/backend/configenvironment.py
@@ -184,31 +184,6 @@ class ConfigEnvironment(object):
                 external = mozpath.join(self.topsrcdir, external)
             self.external_source_dir = mozpath.normpath(external)
 
-        
-        
-        
-        
-        
-        self.substs_unicode = {}
-
-        def decode(v):
-            if not isinstance(v, six.text_type):
-                try:
-                    return v.decode('utf-8')
-                except UnicodeDecodeError:
-                    return v.decode('utf-8', 'replace')
-
-        for k, v in self.substs.items():
-            if not isinstance(v, six.string_types):
-                if isinstance(v, Iterable):
-                    type(v)(decode(i) for i in v)
-            elif not isinstance(v, six.text_type):
-                v = decode(v)
-
-            self.substs_unicode[k] = v
-
-        self.substs_unicode = ReadOnlyDict(self.substs_unicode)
-
     @property
     def is_artifact_build(self):
         return self.substs.get('MOZ_ARTIFACT_BUILDS', False)
