9d1ac3b681c99a0334242ed92f91a026da2ed3b3
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 01:23:15 +0000

    Bug 1546094 Remove alternate-server-replay-4.0.4.py package filtering r=AlexandruIonescu
    Differential Revision: https://phabricator.services.mozilla.com/D34326
    
    UltraBlame original commit: a91367c0b7a96abcea2e620044cbf47933175607

--- testing/raptor/raptor/playback/alternate-server-replay-4.0.4.py
+++ testing/raptor/raptor/playback/alternate-server-replay-4.0.4.py
@@ -99,14 +99,12 @@ class AlternateServerPlayback:
         """
         self.flowmap = {}
         for i in flows:
-            
-            
-            if i.response and i.response.data.content:
+            if i.response:
                 l = self.flowmap.setdefault(self._hash(i), [])
                 l.append(i)
             else:
                 ctx.log.info(
-                    "Request %s has no response data content. Removing from request list"
+                    "Request %s has no response. Removing from request list"
                     % i.request.url
                 )
         ctx.master.addons.trigger("update", [])
