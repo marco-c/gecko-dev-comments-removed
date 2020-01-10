4bd1905ec71d6a79f923ffc050cc6c07f3f99f5e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 03:48:32 +0000

    Bug 1562886 - Avoid noinline on GCC with skcms to work around compiler error. r=me
    UltraBlame original commit: 4f7ddb86b9897738a34ab05c81864fcb154411c5

--- gfx/skia/skia/third_party/skcms/src/Transform_inl.h
+++ gfx/skia/skia/third_party/skcms/src/Transform_inl.h
@@ -559,7 +559,8 @@ SI void sample_clut_16(const skcms_A2B* a2b, I32 ix, F* r, F* g, F* b) {
 
 
 
-#if 1 && defined(__GNUC__) && !defined(__clang__) && defined(__mips64)
+
+#if 1 && defined(__GNUC__) && !defined(__clang__) && (defined(__mips64) || defined(__s390x__) || defined(__i586__) || defined(__i486__) || defined(__i386__))
     #define MAYBE_NOINLINE __attribute__((noinline))
 #else
     #define MAYBE_NOINLINE
