918f27ffade0abb42029d27b33eb2d7c0188cfa3
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 23:44:32 +0000

    Bug 1596465 - Remove GetBindingParent() usage from nsINode::Contains. r=smaug
    And make ShadowRoot.contains() and checking nodes from different shadow trees
    faster.
    
    Differential Revision: https://phabricator.services.mozilla.com/D53037
    
    UltraBlame original commit: f5b772f54a1874b92274e8eab74f929db6346747

--- dom/base/nsINode.cpp
+++ dom/base/nsINode.cpp
@@ -2555,29 +2555,47 @@ bool nsINode::Contains(const nsINode* aOther) const {
   if (aOther == this) {
     return true;
   }
+
   if (!aOther || OwnerDoc() != aOther->OwnerDoc() ||
       IsInUncomposedDoc() != aOther->IsInUncomposedDoc() ||
-      !aOther->IsContent() || !GetFirstChild()) {
+      !aOther->IsContent() || !HasChildren()) {
     return false;
   }
 
-  const nsIContent* other = static_cast<const nsIContent*>(aOther);
-  if (this == OwnerDoc()) {
+  if (IsDocument()) {
     
     
     
-    return !other->IsInAnonymousSubtree();
+    return !aOther->IsInAnonymousSubtree();
   }
 
   if (!IsElement() && !IsDocumentFragment()) {
     return false;
   }
 
-  if (AsContent()->GetBindingParent() != other->GetBindingParent()) {
+  if (IsInShadowTree() != aOther->IsInShadowTree() ||
+      IsInNativeAnonymousSubtree() != aOther->IsInNativeAnonymousSubtree()) {
     return false;
   }
 
-  return other->IsInclusiveDescendantOf(this);
+  if (IsInNativeAnonymousSubtree()) {
+    if (GetClosestNativeAnonymousSubtreeRoot() !=
+        aOther->GetClosestNativeAnonymousSubtreeRoot()) {
+      return false;
+    }
+  }
+
+  if (IsInShadowTree()) {
+    ShadowRoot* otherRoot = aOther->GetContainingShadow();
+    if (IsShadowRoot()) {
+      return otherRoot == this;
+    }
+    if (otherRoot != GetContainingShadow()) {
+      return false;
+    }
+  }
+
+  return aOther->IsInclusiveDescendantOf(this);
 }
 
 uint32_t nsINode::Length() const {
