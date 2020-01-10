4c0b7b625ef5621993700867c0ea430f087fd1f0
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 22:25:09 +0000

    Bug 1168606 - Support preloading also for index cursors. r=ttung,asuth
    Depends on D43252
    
    Differential Revision: https://phabricator.services.mozilla.com/D43461
    
    UltraBlame original commit: f1be81af7d6b1dd98040b90ff4e094da950f01f6

--- dom/indexedDB/ActorsParent.cpp
+++ dom/indexedDB/ActorsParent.cpp
@@ -7613,7 +7613,9 @@ class Cursor final : public PBackgroundIDBCursorParent {
   nsCString mContinueQuery;
   nsCString mContinueToQuery;
   nsCString mContinuePrimaryKeyQuery;
-  const nsCString mLocale;
+  const nsCString
+      mLocale;  
+                
 
   
   
@@ -7676,8 +7678,9 @@ class Cursor final : public PBackgroundIDBCursorParent {
 
   mozilla::ipc::IPCResult RecvDeleteMe() override;
 
-  mozilla::ipc::IPCResult RecvContinue(const CursorRequestParams& aParams,
-                                       const Key& key) override;
+  mozilla::ipc::IPCResult RecvContinue(
+      const CursorRequestParams& aParams, const Key& aCurrentKey,
+      const Key& aCurrentObjectStoreKey) override;
 
   bool IsLocaleAware() const { return !mLocale.IsEmpty(); }
 
@@ -15225,7 +15228,8 @@ void Cursor::SendResponseInternal(
     if (!files.IsEmpty()) {
       MOZ_ASSERT(aResponse.type() ==
                      CursorResponse::TArrayOfObjectStoreCursorResponse ||
-                 aResponse.type() == CursorResponse::TIndexCursorResponse);
+                 aResponse.type() ==
+                     CursorResponse::TArrayOfIndexCursorResponse);
       MOZ_ASSERT(mDatabase);
       MOZ_ASSERT(mBackgroundParent);
 
@@ -15247,10 +15251,12 @@ void Cursor::SendResponseInternal(
           break;
         }
 
-        case CursorResponse::TIndexCursorResponse:
-          MOZ_ASSERT(i == 0);
-          serializedInfo = &aResponse.get_IndexCursorResponse().cloneInfo();
+        case CursorResponse::TArrayOfIndexCursorResponse: {
+          auto& responses = aResponse.get_ArrayOfIndexCursorResponse();
+          MOZ_ASSERT(i < responses.Length());
+          serializedInfo = &responses[i].cloneInfo();
           break;
+        }
 
         default:
           MOZ_CRASH("Should never get here!");
@@ -15304,8 +15310,9 @@ mozilla::ipc::IPCResult Cursor::RecvDeleteMe() {
   return IPC_OK();
 }
 
-mozilla::ipc::IPCResult Cursor::RecvContinue(const CursorRequestParams& aParams,
-                                             const Key& aCurrentKey) {
+mozilla::ipc::IPCResult Cursor::RecvContinue(
+    const CursorRequestParams& aParams, const Key& aCurrentKey,
+    const Key& aCurrentObjectStoreKey) {
   AssertIsOnBackgroundThread();
   MOZ_ASSERT(aParams.type() != CursorRequestParams::T__None);
   MOZ_ASSERT(!mActorDestroyed);
@@ -15329,6 +15336,7 @@ mozilla::ipc::IPCResult Cursor::RecvContinue(const CursorRequestParams& aParams,
   
   
   
+  
   if (!aCurrentKey.IsUnset()) {
     if (IsLocaleAware()) {
       const nsresult rv =
@@ -15339,6 +15347,13 @@ mozilla::ipc::IPCResult Cursor::RecvContinue(const CursorRequestParams& aParams,
       }
     }
     mPosition = aCurrentKey;
+  }
+
+  if (!aCurrentObjectStoreKey.IsUnset()) {
+    MOZ_ASSERT(mType == OpenCursorParams::TIndexOpenCursorParams ||
+               mType == OpenCursorParams::TIndexOpenKeyCursorParams);
+
+    mObjectStorePosition = aCurrentObjectStoreKey;
   }
 
   if (!trustParams && !VerifyRequestParams(aParams)) {
@@ -25788,10 +25803,15 @@ nsresult Cursor::CursorOpBase::PopulateResponseFromStatement(
         return NS_ERROR_NOT_IMPLEMENTED;
       }
 
-      MOZ_ASSERT(aInitializeResponse);
-      mResponse = IndexCursorResponse();
+      if (aInitializeResponse) {
+        mResponse = nsTArray<IndexCursorResponse>();
+      } else {
+        MOZ_ASSERT(mResponse.type() ==
+                   CursorResponse::TArrayOfIndexCursorResponse);
+      }
 
-      auto& response = mResponse.get_IndexCursorResponse();
+      auto& responses = mResponse.get_ArrayOfIndexCursorResponse();
+      auto& response = *responses.AppendElement();
       response.cloneInfo().data().data = std::move(cloneInfo.mData);
       response.key() = mCursor->mPosition;
       response.sortKey() = mCursor->mLocaleAwarePosition;
@@ -25831,7 +25851,8 @@ nsresult Cursor::CursorOpBase::PopulateExtraResponses(
     const nsCString& aOperation) {
   AssertIsOnConnectionThread();
 
-  if (mCursor->mType != OpenCursorParams::TObjectStoreOpenCursorParams) {
+  if (mCursor->mType != OpenCursorParams::TObjectStoreOpenCursorParams &&
+      mCursor->mType != OpenCursorParams::TIndexOpenCursorParams) {
     IDB_WARNING(
         "PRELOAD: Not yet implemented. Extra results were queried, but are "
         "discarded for now.");
@@ -26255,7 +26276,8 @@ nsresult Cursor::OpenOp::DoIndexDatabaseWork(DatabaseConnection* aConnection) {
   
   
   const nsCString firstQuery = queryStart + keyRangeClause + directionClause +
-                               kOpenLimit + NS_LITERAL_CSTRING("1");
+                               kOpenLimit +
+                               ToAutoCString(1 + mCursor->mMaxExtraCount);
 
   DatabaseConnection::CachedStatement stmt;
   nsresult rv = aConnection->GetCachedStatement(firstQuery, &stmt);
@@ -26301,7 +26323,12 @@ nsresult Cursor::OpenOp::DoIndexDatabaseWork(DatabaseConnection* aConnection) {
                                  NS_LITERAL_CSTRING("index_table."),
                                  std::move(queryStart));
 
-  return NS_OK;
+  
+  
+  
+
+  return PopulateExtraResponses(stmt, mCursor->mMaxExtraCount,
+                                NS_LITERAL_CSTRING("OpenOp"));
 }
 
 nsresult Cursor::OpenOp::DoIndexKeyDatabaseWork(
--- dom/indexedDB/IDBCursor.h
+++ dom/indexedDB/IDBCursor.h
@@ -33,6 +33,9 @@ namespace indexedDB {
 class BackgroundCursorChild;
 }
 
+
+
+
 class IDBCursor final : public nsISupports, public nsWrapperCache {
  public:
   typedef indexedDB::Key Key;
@@ -48,7 +51,6 @@ class IDBCursor final : public nsISupports, public nsWrapperCache {
     DIRECTION_INVALID
   };
 
- private:
   enum Type {
     Type_ObjectStore,
     Type_ObjectStoreKey,
@@ -56,6 +58,7 @@ class IDBCursor final : public nsISupports, public nsWrapperCache {
     Type_IndexKey,
   };
 
+ private:
   indexedDB::BackgroundCursorChild* mBackgroundActor;
 
   
@@ -74,8 +77,8 @@ class IDBCursor final : public nsISupports, public nsWrapperCache {
   JS::Heap<JS::Value> mCachedValue;
 
   Key mKey;
-  Key mSortKey;
-  Key mPrimaryKey;
+  Key mSortKey;     
+  Key mPrimaryKey;  
   StructuredCloneReadInfo mCloneInfo;
 
   const Type mType;
@@ -120,6 +123,8 @@ class IDBCursor final : public nsISupports, public nsWrapperCache {
 
   IDBCursorDirection GetDirection() const;
 
+  Type GetType() const;
+
   bool IsContinueCalled() const { return mContinueCalled; }
 
   void GetKey(JSContext* aCx, JS::MutableHandle<JS::Value> aResult,
@@ -163,6 +168,9 @@ class IDBCursor final : public nsISupports, public nsWrapperCache {
 
   void InvalidateCachedResponses();
 
+  
+  bool IsLocaleAware() const;
+
   NS_DECL_CYCLE_COLLECTING_ISUPPORTS
   NS_DECL_CYCLE_COLLECTION_SCRIPT_HOLDER_CLASS(IDBCursor)
 
@@ -175,9 +183,6 @@ class IDBCursor final : public nsISupports, public nsWrapperCache {
             Key aKey);
 
   ~IDBCursor();
-
-  
-  bool IsLocaleAware() const;
 
   void DropJSObjects();
 
