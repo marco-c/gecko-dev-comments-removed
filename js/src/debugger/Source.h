3cf64d366551a84befb6c89bd6e7c171f7f4e042
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 13:59:56 +0000

    Bug 1572280 Part 1 - Add Debugger.Source.reparse(), r=jimb.
    Differential Revision: https://phabricator.services.mozilla.com/D41126
    
    UltraBlame original commit: bc14494262b1b5c8720d0c9bb015ee95d0dbc291

--- js/src/debugger/Source.h
+++ js/src/debugger/Source.h
@@ -1,18 +1,18 @@
-/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*-
- * vim: set ts=8 sts=2 et sw=2 tw=80:
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+
+
+
+
 
 #ifndef dbg_Source_h
 #define dbg_Source_h
 
 #include "jsapi.h"
 
-#include "NamespaceImports.h"   // for Value, HandleObject, CallArgs
-#include "debugger/Debugger.h"  // for DebuggerSourceReferent
-#include "gc/Rooting.h"         // for HandleNativeObject
-#include "vm/NativeObject.h"    // for NativeObject
+#include "NamespaceImports.h"   
+#include "debugger/Debugger.h"  
+#include "gc/Rooting.h"         
+#include "vm/NativeObject.h"    
 
 namespace js {
 class GlobalObject;
@@ -49,7 +49,7 @@ class DebuggerSource : public NativeObject {
   static DebuggerSource* checkThis(JSContext* cx, const CallArgs& args,
                                    const char* fnname, const char* refname);
 
-  // JS methods
+  
   static bool construct(JSContext* cx, unsigned argc, Value* vp);
   static bool getText(JSContext* cx, unsigned argc, Value* vp);
   static bool getBinary(JSContext* cx, unsigned argc, Value* vp);
@@ -64,6 +64,7 @@ class DebuggerSource : public NativeObject {
   static bool getIntroductionType(JSContext* cx, unsigned argc, Value* vp);
   static bool setSourceMapURL(JSContext* cx, unsigned argc, Value* vp);
   static bool getSourceMapURL(JSContext* cx, unsigned argc, Value* vp);
+  static bool reparse(JSContext* cx, unsigned argc, Value* vp);
 
  private:
   static const JSClassOps classOps_;
@@ -72,6 +73,6 @@ class DebuggerSource : public NativeObject {
   static const JSFunctionSpec methods_[];
 };
 
-} /* namespace js */
+} 
 
-#endif /* dbg_Source_h */
+#endif 
