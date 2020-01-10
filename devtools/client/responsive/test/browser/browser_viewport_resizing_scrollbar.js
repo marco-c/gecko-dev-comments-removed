29b5428939e1517960974880b8948cafcfc39269
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 21:04:12 +0000

    Bug 1561227 Part 8: Update expectations for existing tests. r=botond
    The change to await snapshotWindow is something that should have been
    done in Bug 1573254.
    
    Differential Revision: https://phabricator.services.mozilla.com/D47510
    
    UltraBlame original commit: 8117cafb3a205311137ee2a294765fed2a4ed967

--- devtools/client/responsive/test/browser/browser_viewport_resizing_scrollbar.js
+++ devtools/client/responsive/test/browser/browser_viewport_resizing_scrollbar.js
@@ -87,7 +87,7 @@ addRDMTask(TEST_URL, async function({ ui, manager }) {
 
     
     await setViewportSize(ui, manager, 300, 600);
-    const initialSnapshot = snapshotWindow(browser.contentWindow);
+    const initialSnapshot = await snapshotWindow(browser.contentWindow);
 
     
     await setViewportSize(ui, manager, 600, 300);
@@ -99,7 +99,7 @@ addRDMTask(TEST_URL, async function({ ui, manager }) {
 
     
     await setViewportSize(ui, manager, 300, 600);
-    const finalSnapshot = snapshotWindow(browser.contentWindow);
+    const finalSnapshot = await snapshotWindow(browser.contentWindow);
 
     const result = compareSnapshots(initialSnapshot, finalSnapshot, true);
     is(result[2], result[1], "Window snapshots should match.");
