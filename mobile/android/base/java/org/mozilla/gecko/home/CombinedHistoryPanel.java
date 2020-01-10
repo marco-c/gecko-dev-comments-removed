c7f551fb0a0db18f41d86fdf987a17908d0bb792
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 02:37:55 +0000

    Bug 1551614 - Call BrowserApp's onCreate() before finish(); r=VladBaicu
    Fix for the following scenario:
    - "Don't keep activities" checked
    - Change system language
    
    Upon detecting a locale change in BrowserApp's onCreate() we would call
    finish() and then return before calling super.onCreate().
    
    This patch introdues the call to super.onCreate() before returning and ensures
    that the app will die cleanly so that upon restarting Gecko can reinit add-ons
    that touch the UI.
    
    Differential Revision: https://phabricator.services.mozilla.com/D35402
    
    UltraBlame original commit: 819d3061b8fb3cbc3143ac68e8f521b7e32f362c

--- mobile/android/base/java/org/mozilla/gecko/home/CombinedHistoryPanel.java
+++ mobile/android/base/java/org/mozilla/gecko/home/CombinedHistoryPanel.java
@@ -14,6 +14,7 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.annotation.UiThread;
+import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
 import android.support.v4.widget.SwipeRefreshLayout;
@@ -111,6 +112,15 @@ public class CombinedHistoryPanel extends HomeFragment implements RemoteClientsD
     @Override
     public void onCreate(Bundle savedInstance) {
         super.onCreate(savedInstance);
+
+        
+        
+        
+        
+        final FragmentActivity parent = getActivity();
+        if (parent != null && parent.isFinishing()) {
+            return;
+        }
 
         int cachedRecentTabsCount = 0;
         if (mPanelStateChangeListener != null ) {
