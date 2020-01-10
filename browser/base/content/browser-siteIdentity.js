ea6a14dd7e660581e823f534c22b1bd40f9bee3c
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 15:32:29 +0000

    Bug 1508080 - Don't set directionality on identity-icon-labels on extension pages r=dao
    Differential Revision: https://phabricator.services.mozilla.com/D42000
    
    UltraBlame original commit: 74c70e9a5ec215f3b974d88909edc4ec7332eed1

--- browser/base/content/browser-siteIdentity.js
+++ browser/base/content/browser-siteIdentity.js
@@ -770,6 +770,7 @@ var gIdentityHandler = {
         "identity.extension.label",
         [extensionName]
       );
+      icon_labels_dir = "";
     } else if (this._uriHasHost && this._isSecureConnection) {
       
       this._identityBox.className = "verifiedDomain";
