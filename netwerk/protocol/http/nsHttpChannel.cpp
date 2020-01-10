37c4b80dde2dba39beed503b9c8f9e6a8c8a1ced
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 01:04:21 +0000

    Bug 1554680 - Use WindowGlobalParent::IsInitialDocument instead of WindowGlobalParent::DocumentPrincipal to figure out if the page is the initial about:blank r=nika
    For a popup, the principal will be that of the opener, so we can't use that to
    determine if the window is about:blank.
    
    Differential Revision: https://phabricator.services.mozilla.com/D33249
    
    UltraBlame original commit: 247251d5a94c65d1a3764461e67849e453da99ac

--- netwerk/protocol/http/nsHttpChannel.cpp
+++ netwerk/protocol/http/nsHttpChannel.cpp
@@ -7450,19 +7450,30 @@ nsHttpChannel::HasCrossOriginOpenerPolicyMismatch(bool* aMismatch) {
     LOG(("doc origin:%s - res origin: %s\n", docOrigin.get(), resOrigin.get()));
   }
 
-  if (!compareResult) {
-    
-    
-    
-    
-    
-    if (!documentOrigin->GetIsNullPrincipal() ||
-        !(documentPolicy &
-          nsILoadInfo::OPENER_POLICY_UNSAFE_ALLOW_OUTGOING_FLAG) ||
-        !(resultPolicy == nsILoadInfo::OPENER_POLICY_NULL)) {
-      *aMismatch = true;
-      return NS_OK;
-    }
+  if (compareResult) {
+    return NS_OK;
+  }
+
+  
+  
+  
+  
+  
+
+  if (!(documentPolicy &
+        nsILoadInfo::OPENER_POLICY_UNSAFE_ALLOW_OUTGOING_FLAG)) {
+    *aMismatch = true;
+    return NS_OK;
+  }
+
+  if (resultPolicy != nsILoadInfo::OPENER_POLICY_NULL) {
+    *aMismatch = true;
+    return NS_OK;
+  }
+
+  if (!ctx->Canonical()->GetCurrentWindowGlobal()->IsInitialDocument()) {
+    *aMismatch = true;
+    return NS_OK;
   }
 
   return NS_OK;
