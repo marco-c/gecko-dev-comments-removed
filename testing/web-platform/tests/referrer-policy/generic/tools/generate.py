02adb46b5fedecb82f48dd37f684dd4214bd31af
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 11:33:58 +0000

    Bug 1569085 [wpt PR 18101] - [WPT/referrer-policy] Move delivery_type control out of test_expansion, a=testonly
    Automatic update from web-platform-tests
    [WPT/referrer-policy] Move delivery_type control out of test_expansion
    
    A subsequent CL [1] will introduce `delivery_type_schema`
    that controls relationship between source contexts,
    subresource types, and delivery types.
    
    As a preparation, this CL removes the restriction on `delivery_type`
    specified in `elements-not-supporting-attr-referrer`,
    `elements-not-supporting-rel-noreferrer`, and
    `test_expansion` entries, that will be replaced by
    `delivery_type_schema` in [1].
    
    Generated files will be updated after some more changes, not now.
    Also, changes needed in JavaScripts to handle new generated values
    are not included in this CL and will land later.
    
    Just after this CL, the generator produces more files than before
    due to lack of `delivery_type_schema`, but this will be fixed
    by [1].
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/1586548
    
    Bug: 906850
    Change-Id: I3d6ecfc6fc377864884f367ba554c2a7fafac671
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1717535
    Commit-Queue: Hiroshige Hayashizaki <hiroshigechromium.org>
    Reviewed-by: Kenichi Ishibashi <bashichromium.org>
    Reviewed-by: Hiroki Nakagawa <nhirokichromium.org>
    Cr-Commit-Position: refs/heads/master{#681098}
    
    --
    
    wpt-commits: cf7b59d32e9dc4e00749fd6e5de13dc16872709c
    wpt-pr: 18101
    
    UltraBlame original commit: 3222d30c4fcc7d77e21a0606b352297f2ca88b0f

--- testing/web-platform/tests/referrer-policy/generic/tools/generate.py
+++ testing/web-platform/tests/referrer-policy/generic/tools/generate.py
@@ -49,7 +49,7 @@ the target request is %(origin)s.'''
         meta = ''
         headers = []
         if delivery_value != None:
-            if delivery_type == 'meta-referrer':
+            if delivery_type == 'meta':
                 meta = \
                     '<meta name="referrer" content="%s">' % delivery_value
             elif delivery_type == 'http-rp':
@@ -58,10 +58,10 @@ the target request is %(origin)s.'''
                 headers.append('Referrer-Policy: ' + '%s' % delivery_value)
                 
                 headers.append('Access-Control-Allow-Origin: *')
-            elif delivery_type == 'attr-referrer':
+            elif delivery_type == 'attr':
                 
                 pass
-            elif delivery_type == 'rel-noreferrer':
+            elif delivery_type == 'rel-noref':
                 
                 pass
             else:
