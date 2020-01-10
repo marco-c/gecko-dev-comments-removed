14d3671a9cc738d24cd069dc8eb9ff709571924d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 14:58:21 +0000

    Backed out changeset 3ab29c05ea2f (bug 1521893) build bustage in /builds/worker/workspace/build/src/widget/cocoa/nsTouchBarInputIcon.h. CLOSED TREE
    UltraBlame original commit: 3abcd517522920b1cdd9efc21ff475d7b013e179

--- widget/cocoa/nsIconLoaderService.h
+++ widget/cocoa/nsIconLoaderService.h
@@ -17,7 +17,7 @@
 #include "nsIContentPolicy.h"
 
 class nsIURI;
-class nsINode;
+class nsIContent;
 class nsIPrincipal;
 class imgRequestProxy;
 
@@ -25,7 +25,7 @@ class nsIconLoaderService : public imgINotificationObserver {
  public:
   
   
-  nsIconLoaderService(nsINode* aContent, nsIntRect* aImageRegionRect,
+  nsIconLoaderService(nsIContent* aContent, nsIntRect* aImageRegionRect,
                       RefPtr<nsIconLoaderObserver> aObserver, uint32_t aIconHeight,
                       uint32_t aIconWidth, CGFloat aScaleFactor = 0.0f);
 
@@ -37,11 +37,11 @@ class nsIconLoaderService : public imgINotificationObserver {
   
   nsresult LoadIcon(nsIURI* aIconURI);
 
-  NSImage* GetNativeIconImage();
-
+  
+  
+  
+  
   void Destroy();
-
-  void ReleaseJSObjects() { mContent = nil; }
 
  protected:
   virtual ~nsIconLoaderService();
@@ -49,7 +49,7 @@ class nsIconLoaderService : public imgINotificationObserver {
  private:
   nsresult OnFrameComplete(imgIRequest* aRequest);
 
-  nsCOMPtr<nsINode> mContent;
+  nsCOMPtr<nsIContent> mContent;
   nsContentPolicyType mContentType;
   RefPtr<imgRequestProxy> mIconRequest;
   nsIntRect* mImageRegionRect;
@@ -60,4 +60,5 @@ class nsIconLoaderService : public imgINotificationObserver {
   CGFloat mScaleFactor;
   RefPtr<nsIconLoaderObserver> mCompletionHandler;
 };
+
 #endif  
