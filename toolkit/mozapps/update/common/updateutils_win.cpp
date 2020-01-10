d090e51e5b9e1af9351d5c2a91ed6aac73f12c65
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 18:56:07 +0000

    Backed out changeset 27df5399e032 (bug 1510494) for causing xpcshell test failures in toolkit/mozapps/update/tests/unit_service_updater/marStageSuccessCompleteSvc.js CLOSED TREE
    UltraBlame original commit: 6b9cf21a84bcde09d3dc46760784fc95b55818b8

--- toolkit/mozapps/update/common/updateutils_win.cpp
+++ toolkit/mozapps/update/common/updateutils_win.cpp
@@ -95,65 +95,30 @@ BOOL PathAppendSafe(LPWSTR base, LPCWSTR extra) {
 
 
 
-BOOL GetUUIDString(LPWSTR outBuf) {
-  UUID uuid;
-  RPC_WSTR uuidString = nullptr;
-
-  
-  
-  
-  if (UuidCreate(&uuid) != RPC_S_OK) {
-    return FALSE;
-  }
-  if (UuidToStringW(&uuid, &uuidString) != RPC_S_OK) {
-    return FALSE;
-  }
-  if (!uuidString) {
-    return FALSE;
-  }
-
-  if (wcslen(reinterpret_cast<LPCWSTR>(uuidString)) > MAX_PATH) {
-    return FALSE;
-  }
-  wcsncpy(outBuf, reinterpret_cast<LPCWSTR>(uuidString), MAX_PATH + 1);
-  RpcStringFreeW(&uuidString);
-
-  return TRUE;
-}
-
-
-
-
-
-
-
-
 
 
 BOOL GetUUIDTempFilePath(LPCWSTR basePath, LPCWSTR prefix, LPWSTR tmpPath) {
   WCHAR filename[MAX_PATH + 1] = {L"\0"};
   if (prefix) {
-    if (wcslen(prefix) > MAX_PATH) {
-      return FALSE;
-    }
-    wcsncpy(filename, prefix, MAX_PATH + 1);
+    wcsncpy(filename, prefix, MAX_PATH);
   }
 
-  WCHAR tmpFileNameString[MAX_PATH + 1] = {L"\0"};
-  if (!GetUUIDString(tmpFileNameString)) {
+  UUID tmpFileNameUuid;
+  RPC_WSTR tmpFileNameString = nullptr;
+  if (UuidCreate(&tmpFileNameUuid) != RPC_S_OK) {
+    return FALSE;
+  }
+  if (UuidToStringW(&tmpFileNameUuid, &tmpFileNameString) != RPC_S_OK) {
+    return FALSE;
+  }
+  if (!tmpFileNameString) {
     return FALSE;
   }
 
-  size_t tmpFileNameStringLen = wcslen(tmpFileNameString);
-  if (wcslen(filename) + tmpFileNameStringLen > MAX_PATH) {
-    return FALSE;
-  }
-  wcsncat(filename, tmpFileNameString, tmpFileNameStringLen);
+  wcsncat(filename, (LPCWSTR)tmpFileNameString, MAX_PATH);
+  RpcStringFreeW(&tmpFileNameString);
 
-  if (wcslen(basePath) > MAX_PATH) {
-    return FALSE;
-  }
-  wcsncpy(tmpPath, basePath, MAX_PATH + 1);
+  wcsncpy(tmpPath, basePath, MAX_PATH);
   if (!PathAppendSafe(tmpPath, filename)) {
     return FALSE;
   }
--- toolkit/mozapps/update/common/updateutils_win.h
+++ toolkit/mozapps/update/common/updateutils_win.h
@@ -36,12 +36,7 @@ DIR* opendir(const WCHAR* path);
 int closedir(DIR* dir);
 dirent* readdir(DIR* dir);
 
-
-
-#define UUID_LEN 37
-
 BOOL PathAppendSafe(LPWSTR base, LPCWSTR extra);
-BOOL GetUUIDString(LPWSTR outBuf);
 BOOL GetUUIDTempFilePath(LPCWSTR basePath, LPCWSTR prefix, LPWSTR tmpPath);
 
 #endif  
