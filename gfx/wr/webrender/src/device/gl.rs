7b0c93b97218f9a30a4b3d11da8d542576a48d95
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 22:04:02 +0000

    Bug 1591945 - Ensure strings passed to glShaderSource are null-terminated on android emulator. r=gw
    The emulator's implementation of glShaderSource can crash if the source string
    are not null-terminated, even though we correctly pass the lengths of the
    strings. Work around this by adding a null terminator when running on the
    emulator.
    
    Depends on D51293
    
    Differential Revision: https://phabricator.services.mozilla.com/D51294
    
    UltraBlame original commit: 17366ca83b0c964f0b413ddd76229a768660d9e5

--- gfx/wr/webrender/src/device/gl.rs
+++ gfx/wr/webrender/src/device/gl.rs
@@ -1037,6 +1037,10 @@ pub struct Device {
     optimal_pbo_stride: NonZeroUsize,
 
     
+    
+    requires_null_terminated_shader_source: bool,
+
+    
     extensions: Vec<String>,
 
     
@@ -1415,6 +1419,11 @@ impl Device {
         let supports_texture_swizzle = allow_texture_swizzling &&
             (gl.get_type() == gl::GlType::Gles || supports_extension(&extensions, "GL_ARB_texture_storage"));
 
+
+        
+        
+        let requires_null_terminated_shader_source = is_emulator;
+
         
         
         
@@ -1470,6 +1479,7 @@ impl Device {
             frame_id: GpuFrameId(0),
             extensions,
             texture_storage_usage,
+            requires_null_terminated_shader_source,
             optimal_pbo_stride,
             dump_shader_source,
             surface_is_y_flipped,
@@ -1560,10 +1570,19 @@ impl Device {
         name: &str,
         shader_type: gl::GLenum,
         source: &String,
+        requires_null_terminated_shader_source: bool,
     ) -> Result<gl::GLuint, ShaderError> {
         debug!("compile {}", name);
         let id = gl.create_shader(shader_type);
-        gl.shader_source(id, &[source.as_bytes()]);
+        if requires_null_terminated_shader_source {
+            
+            
+            use std::ffi::CString;
+            let terminated_source = CString::new(source.as_bytes()).unwrap();
+            gl.shader_source(id, &[terminated_source.as_bytes_with_nul()]);
+        } else {
+            gl.shader_source(id, &[source.as_bytes()]);
+        }
         gl.compile_shader(id);
         let log = gl.get_shader_info_log(id);
         let mut status = [0];
@@ -1870,7 +1889,7 @@ impl Device {
         if build_program {
             
             let vs_source = info.compute_source(self, SHADER_KIND_VERTEX);
-            let vs_id = match Device::compile_shader(&*self.gl, &info.base_filename, gl::VERTEX_SHADER, &vs_source) {
+            let vs_id = match Device::compile_shader(&*self.gl, &info.base_filename, gl::VERTEX_SHADER, &vs_source, self.requires_null_terminated_shader_source) {
                     Ok(vs_id) => vs_id,
                     Err(err) => return Err(err),
                 };
@@ -1878,7 +1897,7 @@ impl Device {
             
             let fs_source = info.compute_source(self, SHADER_KIND_FRAGMENT);
             let fs_id =
-                match Device::compile_shader(&*self.gl, &info.base_filename, gl::FRAGMENT_SHADER, &fs_source) {
+                match Device::compile_shader(&*self.gl, &info.base_filename, gl::FRAGMENT_SHADER, &fs_source, self.requires_null_terminated_shader_source) {
                     Ok(fs_id) => fs_id,
                     Err(err) => {
                         self.gl.delete_shader(vs_id);
