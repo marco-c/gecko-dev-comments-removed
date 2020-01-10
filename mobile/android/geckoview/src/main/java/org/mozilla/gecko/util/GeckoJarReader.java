73d82ec3e6bdca57cd2b6dd4cd165e2133c6ebfb
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 15:15:37 +0000

    Bug 1548776 - URL-decode all file names in GeckoJarReader. r=snorp
    Assuming the URL string the JAR reader receives has been completely URL-encoded,
    we need to decode not just the first nested jarUrl, as already happens through
    getZipFile(), but all subsequent path components, too.
    
    At least for jar:-URLs received from Gecko, the above assumption certainly seems
    to be true.
    
    Differential Revision: https://phabricator.services.mozilla.com/D44033
    
    UltraBlame original commit: cd343dac2978e474019c52761afc9e7b9adb76d7

--- mobile/android/geckoview/src/main/java/org/mozilla/gecko/util/GeckoJarReader.java
+++ mobile/android/geckoview/src/main/java/org/mozilla/gecko/util/GeckoJarReader.java
@@ -23,8 +23,10 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URLDecoder;
 import java.util.Stack;
 
 
@@ -194,6 +196,11 @@ public final class GeckoJarReader {
         
         while (!jarUrls.empty()) {
             String fileName = jarUrls.pop();
+            try {
+                fileName = URLDecoder.decode(fileName, "UTF-8");
+            } catch (UnsupportedEncodingException e) {
+                
+            }
 
             if (inputStream != null) {
                 
