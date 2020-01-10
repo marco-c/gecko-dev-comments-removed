6a1bdeddce438a8cb2d58a4b9df35b368ce484ee
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 00:56:41 +0000

    Bug 1552714 - Extract DOMLocalization out of DocumentL10n. r=smaug
    Differential Revision: https://phabricator.services.mozilla.com/D32397
    
    UltraBlame original commit: bffc5993b912cbab7d012775abacad52fa5d5cdc

--- dom/l10n/Mutations.h
+++ dom/l10n/Mutations.h
@@ -4,7 +4,7 @@
 #include "nsRefreshDriver.h"
 #include "nsStubMutationObserver.h"
 #include "nsTHashtable.h"
-#include "mozilla/dom/DocumentL10n.h"
+#include "mozilla/dom/l10n/DOMLocalization.h"
 
 namespace mozilla {
 namespace dom {
@@ -24,7 +24,7 @@ class Mutations final : public nsStubMutationObserver,
   NS_DECL_NSIMUTATIONOBSERVER_CONTENTINSERTED
   NS_DECL_NSIMUTATIONOBSERVER_ATTRIBUTECHANGED
 
-  explicit Mutations(DocumentL10n* aDocumentL10n);
+  explicit Mutations(DOMLocalization* aDOMLocalization);
 
   
 
@@ -57,7 +57,7 @@ class Mutations final : public nsStubMutationObserver,
   bool mObserving = false;
   bool mRefreshObserver = false;
   RefPtr<nsRefreshDriver> mRefreshDriver;
-  DocumentL10n* mDocumentL10n;
+  DOMLocalization* mDOMLocalization;
 
   
   nsTHashtable<nsRefPtrHashKey<Element>> mPendingElementsHash;
@@ -73,8 +73,8 @@ class Mutations final : public nsStubMutationObserver,
  private:
   ~Mutations() {
     StopRefreshObserver();
-    MOZ_ASSERT(!mDocumentL10n,
-               "DocumentL10n<-->Mutations cycle should be broken.");
+    MOZ_ASSERT(!mDOMLocalization,
+               "DOMLocalization<-->Mutations cycle should be broken.");
   }
 };
 
