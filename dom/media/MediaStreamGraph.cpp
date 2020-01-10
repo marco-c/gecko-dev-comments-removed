e4bc0677e6bfc64c45568f6eba4e52145f15060f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 18:36:45 +0000

    Bug 1454998 - Remove AudioNodeStream::SampleRate(). r=karlt
    AudioNodeStream is a subclass of MediaStream, which now exposes a
    public const mSampleRate member.
    
    Differential Revision: https://phabricator.services.mozilla.com/D47688
    
    UltraBlame original commit: 8ff03f2f4ca2da0761fb285f8c403b51765a19cf

--- dom/media/MediaStreamGraph.cpp
+++ dom/media/MediaStreamGraph.cpp
@@ -1205,7 +1205,7 @@ void MediaStreamGraphImpl::Process() {
           for (uint32_t j = i + 1; j < mStreams.Length(); ++j) {
             AudioNodeStream* nextStream = mStreams[j]->AsAudioNodeStream();
             if (nextStream) {
-              MOZ_ASSERT(n->SampleRate() == nextStream->SampleRate(),
+              MOZ_ASSERT(n->mSampleRate == nextStream->mSampleRate,
                          "All AudioNodeStreams in the graph must have the same "
                          "sampling rate");
             }
@@ -1213,7 +1213,7 @@ void MediaStreamGraphImpl::Process() {
 #endif
           
           
-          ProduceDataForStreamsBlockByBlock(i, n->SampleRate());
+          ProduceDataForStreamsBlockByBlock(i, n->mSampleRate);
           doneAllProducing = true;
         } else {
           ps->ProcessInput(mProcessedTime, mStateComputedTime,
