111cf735e06f7d4c4dff1d23ad9ed6d83a6f15e7
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 17:20:36 +0000

    Bug 1547063 - Cairo support for SharedFTFace. r=jfkthame
    Differential Revision: https://phabricator.services.mozilla.com/D44492
    
    UltraBlame original commit: 0b008480f41c1da642106e5e0de5b5abc0e406fc

--- gfx/cairo/cairo/src/cairo.h
+++ gfx/cairo/cairo/src/cairo.h
@@ -1215,6 +1215,30 @@ typedef enum _cairo_hint_metrics {
 
 
 
+typedef enum _cairo_lcd_filter {
+    CAIRO_LCD_FILTER_DEFAULT,
+    CAIRO_LCD_FILTER_NONE,
+    CAIRO_LCD_FILTER_INTRA_PIXEL,
+    CAIRO_LCD_FILTER_FIR3,
+    CAIRO_LCD_FILTER_FIR5
+} cairo_lcd_filter_t;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -1266,6 +1290,10 @@ cairo_font_options_set_hint_metrics (cairo_font_options_t *options,
 				     cairo_hint_metrics_t  hint_metrics);
 cairo_public cairo_hint_metrics_t
 cairo_font_options_get_hint_metrics (const cairo_font_options_t *options);
+
+cairo_public void
+cairo_font_options_set_lcd_filter (cairo_font_options_t   *options,
+                    cairo_lcd_filter_t  lcd_filter);
 
 
 
