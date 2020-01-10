9f78fa6d0dbf10641a315312e688f1175a6a06c7
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 22:28:12 +0000

    Bug 1593966 - Add minimal support for the DW_FORM_GNU_ref_alt and DW_FORM_GNU_strp_alt operand forms in dump_syms r=froydnj
    Differential Revision: https://phabricator.services.mozilla.com/D51801
    
    UltraBlame original commit: ab19234907a0e553676b1c1bd45d37c8627f85e9

--- toolkit/crashreporter/google-breakpad/src/common/dwarf/dwarf2enums.h
+++ toolkit/crashreporter/google-breakpad/src/common/dwarf/dwarf2enums.h
@@ -152,7 +152,12 @@ enum DwarfForm {
   DW_FORM_ref_sig8 = 0x20,
   
   DW_FORM_GNU_addr_index = 0x1f01,
-  DW_FORM_GNU_str_index = 0x1f02
+  DW_FORM_GNU_str_index = 0x1f02,
+
+  
+  
+  DW_FORM_GNU_ref_alt = 0x1f20,
+  DW_FORM_GNU_strp_alt = 0x1f21,
 };
 
 
