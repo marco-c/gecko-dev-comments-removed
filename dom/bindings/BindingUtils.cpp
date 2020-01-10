95c023849100af1e7745cce9130c20eaff42a789
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 17:18:02 +0000

    Bug 1581414 - Don't report deprecation warnings after diverging from the recording, r=mccr8.
    Differential Revision: https://phabricator.services.mozilla.com/D45960
    
    UltraBlame original commit: 83428e27816e47c926cbe9810131169a7b231f05

--- dom/bindings/BindingUtils.cpp
+++ dom/bindings/BindingUtils.cpp
@@ -4009,6 +4009,13 @@ void DeprecationWarning(JSContext* aCx, JSObject* aObject,
 void DeprecationWarning(const GlobalObject& aGlobal,
                         Document::DeprecatedOperations aOperation) {
   if (NS_IsMainThread()) {
+    
+    
+    
+    if (recordreplay::HasDivergedFromRecording()) {
+      return;
+    }
+
     nsCOMPtr<nsPIDOMWindowInner> window =
         do_QueryInterface(aGlobal.GetAsSupports());
     if (window && window->GetExtantDoc()) {
