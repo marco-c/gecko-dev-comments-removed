f4804338c778a2a783919243f0cbc539dfaa831d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 21:25:16 +0000

    Bug 1587694 [wpt PR 19613] - Update service_worker_unregister_and_register(), a=testonly
    Automatic update from web-platform-tests
    Update service_worker_unregister_and_register()
    
    Allows callsites of this helper function to pass RegistrationOptions.
    It will be useful for testing module service workers.
    
    Bug: 824647
    Change-Id: Idc9a5c012144da46dfc4d692ce26df9f604a6ea1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1851624
    Commit-Queue: Kenichi Ishibashi <bashichromium.org>
    Reviewed-by: Hiroki Nakagawa <nhirokichromium.org>
    Cr-Commit-Position: refs/heads/master{#704963}
    
    --
    Stop using default parameters in service_worker_unregister_and_register()
    
    The previous CL (crrev.com/c/1851624) seemed to make some tests flaky.
    I'm not sure the exact reason. This CL attempts to fix flakiness by not
    using default parameters.
    
    Bug: N/A
    Change-Id: Ia6fae23881704e4eb41e33ebe3e22e89872d7f87
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1866093
    Commit-Queue: Kenichi Ishibashi <bashichromium.org>
    Reviewed-by: Hiroki Nakagawa <nhirokichromium.org>
    Cr-Commit-Position: refs/heads/master{#707713}
    
    --
    
    wpt-commits: 9cb445cb5b453bc5aecb0c349ab062627bcefe10, 9c6848c2e4d7284ba326fe7898c6616c9b29854d
    wpt-pr: 19613
    
    UltraBlame original commit: 6947e48eb86bb9a8202777eae087b02694f34345

--- testing/web-platform/tests/service-workers/service-worker/resources/test-helpers.sub.js
+++ testing/web-platform/tests/service-workers/service-worker/resources/test-helpers.sub.js
@@ -1,10 +1,19 @@
 
 
-function service_worker_unregister_and_register(test, url, scope) {
+
+
+
+
+
+
+function service_worker_unregister_and_register(test, url, scope, options) {
   if (!scope || scope.length == 0)
     return Promise.reject(new Error('tests must define a scope'));
 
-  var options = { scope: scope };
+  if (options && options.scope)
+    return Promise.reject(new Error('scope must not be passed in options'));
+
+  options = Object.assign({ scope: scope }, options);
   return service_worker_unregister(test, scope)
     .then(function() {
         return navigator.serviceWorker.register(url, options);
