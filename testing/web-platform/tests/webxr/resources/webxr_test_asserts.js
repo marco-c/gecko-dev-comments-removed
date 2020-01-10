0c8c1ab27d4f0d09636cef74949c79dce6960f16
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 19:12:48 +0000

    Bug 1585117 [wpt PR 19409] - Test origin-offset for `viewer` space, a=testonly
    Automatic update from web-platform-tests
    Test origin-offset for `viewer` space
    
    Bug: 1009129
    
    Change-Id: Iea0ee4259c894db49a76f8f4e1ca9b53f95f69ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1829296
    Commit-Queue: Piotr Bialecki <bialpiochromium.org>
    Reviewed-by: Klaus Weidner <klauswchromium.org>
    Reviewed-by: Alexander Cooper <alcooperchromium.org>
    Cr-Commit-Position: refs/heads/master{#701468}
    
    --
    
    wpt-commits: 187e0e0d82dd6c2440ae6673b5ed2e130dc61a70
    wpt-pr: 19409
    
    UltraBlame original commit: a173e48499c53609cea5d3b7d6b0bd8a5f09a297

--- testing/web-platform/tests/webxr/resources/webxr_test_asserts.js
+++ testing/web-platform/tests/webxr/resources/webxr_test_asserts.js
@@ -33,6 +33,31 @@ let assert_point_approx_equals = function(p1, p2, epsilon = FLOAT_EPSILON, prefi
 
 
 
+let assert_point_significantly_not_equals = function(p1, p2, epsilon = FLOAT_EPSILON, prefix = "") {
+
+  assert_not_equals(p1, null, prefix + "p1 must be non-null");
+  assert_not_equals(p2, null, prefix + "p2 must be non-null");
+
+  let mismatched_component = null;
+  for (const v of ['x', 'y', 'z', 'w']) {
+    if (Math.abs(p1[v] - p2[v]) > epsilon) {
+      mismatched_component = v;
+      break;
+    }
+  }
+
+  if (mismatched_component === null) {
+    let error_message = prefix + ' Point comparison failed.\n';
+    error_message += ` p1: {x: ${p1.x}, y: ${p1.y}, z: ${p1.z}, w: ${p1.w}}\n`;
+    error_message += ` p2: {x: ${p2.x}, y: ${p2.y}, z: ${p2.z}, w: ${p2.w}}\n`;
+    error_message += ` Difference in components did not exceeded the given epsilon.\n`;
+    assert_unreached(error_message);
+  }
+};
+
+
+
+
 let assert_matrix_approx_equals = function(m1, m2, epsilon = FLOAT_EPSILON, prefix = "") {
   if (m1 == null && m2 == null) {
     return;
