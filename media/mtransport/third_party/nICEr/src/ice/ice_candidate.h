7defa04ddad1a1e498221099dc49b1b3ebe8f1e0
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 18:30:08 +0000

    Bug 1567201 - Obfuscate server reflex candidates if necessary; r=ng
    According to https://tools.ietf.org/html/draft-ietf-rtcweb-mdns-ice-candidates-03#section-3.1.2.1
    we need to hide the rel-addr and rel-port attributes for server reflex
    candidates in order to avoid leaking local addresses.
    
    Differential Revision: https://phabricator.services.mozilla.com/D46982
    
    UltraBlame original commit: ddc6931cddeccc448423cf4c5dc20d787a8e2daa

--- media/mtransport/third_party/nICEr/src/ice/ice_candidate.h
+++ media/mtransport/third_party/nICEr/src/ice/ice_candidate.h
@@ -65,6 +65,7 @@ struct nr_ice_candidate_ {
   nr_transport_addr addr;             
 
   nr_transport_addr base;             
+  int obfuscate_addrs;                
   char *mdns_addr;                    
   char *foundation;                   
   UINT4 priority;                     
@@ -112,7 +113,7 @@ void nr_ice_candidate_compute_codeword(nr_ice_candidate *cand);
 int nr_ice_candidate_process_stun(nr_ice_candidate *cand, UCHAR *msg, int len, nr_transport_addr *faddr);
 int nr_ice_candidate_destroy(nr_ice_candidate **candp);
 void nr_ice_candidate_stop_gathering(nr_ice_candidate *cand);
-int nr_ice_format_candidate_attribute(nr_ice_candidate *cand, char *attr, int maxlen);
+int nr_ice_format_candidate_attribute(nr_ice_candidate *cand, char *attr, int maxlen, int obfuscate_srflx_addr);
 int nr_ice_peer_candidate_from_attribute(nr_ice_ctx *ctx,char *attr,nr_ice_media_stream *stream,nr_ice_candidate **candp);
 int nr_ice_peer_peer_rflx_candidate_create(nr_ice_ctx *ctx,char *label, nr_ice_component *comp,nr_transport_addr *addr, nr_ice_candidate **candp);
 int nr_ice_candidate_compute_priority(nr_ice_candidate *cand);
