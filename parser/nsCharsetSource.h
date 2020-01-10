515b81dd6ea8791ce4856085ba49b5987a30d948
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 00:19:52 +0000

    Bug 1556746 - Remove kCharsetFromHintPrevDoc that have been accidentally restored by resolving merge conflict. r=hsivonen
    Differential Revision: https://phabricator.services.mozilla.com/D33642
    
    UltraBlame original commit: 55fd70d72e0873ecc75240956253288d3df4345b

--- parser/nsCharsetSource.h
+++ parser/nsCharsetSource.h
@@ -15,7 +15,6 @@ enum {
   kCharsetFromCache,
   kCharsetFromParentFrame,
   kCharsetFromAutoDetection,
-  kCharsetFromHintPrevDoc,
   kCharsetFromMetaPrescan,  
   kCharsetFromMetaTag,      
   kCharsetFromIrreversibleAutoDetection,
