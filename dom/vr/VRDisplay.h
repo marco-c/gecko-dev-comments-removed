e4474fbd4d0e1b44d32eb2b03aea2e0dace109fe
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 21:34:56 +0000

    Bug 1565268 - Part 2: Removing mDisplayName/Id from VRDisplay. r=kip
    We can remove mDisplayName/Id and just use VRDisplayInfo to get these two values.
    
    Differential Revision: https://phabricator.services.mozilla.com/D50870
    
    UltraBlame original commit: b70aa06f74d54f7aea1aec723975a414f7072ed7

--- dom/vr/VRDisplay.h
+++ dom/vr/VRDisplay.h
@@ -275,10 +275,8 @@ class VRDisplay final : public DOMEventTargetHelper, public nsIObserver {
   VRDisplayCapabilities* Capabilities();
   VRStageParameters* GetStageParameters();
 
-  uint32_t DisplayId() const { return mDisplayId; }
-  void GetDisplayName(nsAString& aDisplayName) const {
-    aDisplayName = mDisplayName;
-  }
+  uint32_t DisplayId() const;
+  void GetDisplayName(nsAString& aDisplayName) const;
   
   
   
@@ -338,9 +336,6 @@ class VRDisplay final : public DOMEventTargetHelper, public nsIObserver {
   void UpdateFrameInfo();
 
   RefPtr<gfx::VRDisplayClient> mClient;
-
-  uint32_t mDisplayId;
-  nsString mDisplayName;
 
   RefPtr<VRDisplayCapabilities> mCapabilities;
   RefPtr<VRStageParameters> mStageParameters;
