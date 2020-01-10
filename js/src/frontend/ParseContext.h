0d45a77019df2c0f56e579c8f686aff5c78a0d76
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 16:43:44 +0000

    Bug 1577857 - Hoist FunctionTree into its own header r=tcampbell
    Differential Revision: https://phabricator.services.mozilla.com/D45041
    
    UltraBlame original commit: a67d5dbc29ccb7422fba3ab27fecbe1f17618edd

--- js/src/frontend/ParseContext.h
+++ js/src/frontend/ParseContext.h
@@ -11,6 +11,7 @@
 
 #include "frontend/BytecodeCompiler.h"
 #include "frontend/ErrorReporter.h"
+#include "frontend/FunctionTree.h"
 #include "frontend/NameCollections.h"
 #include "frontend/SharedContext.h"
 
@@ -177,19 +178,6 @@ class UsedNameTracker {
 
 class FunctionTree;
 class FunctionTreeHolder;
-
-
-
-class MOZ_RAII AutoPushTree {
-  FunctionTreeHolder& holder_;
-  FunctionTree* oldParent_ = nullptr;
-
- public:
-  explicit AutoPushTree(FunctionTreeHolder& holder);
-  ~AutoPushTree();
-
-  bool init(JSContext* cx, FunctionBox* box);
-};
 
 
 
