5eca8536e95c151658f96813d70edde4ac5f56b8
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 11:00:46 +0000

    Bug 1569996 - Enable setting the default referrer policy for third-party tracking resources to strict-origin-when-cross-origin when Enhanced Tracking Protection is turned on; r=baku
    Differential Revision: https://phabricator.services.mozilla.com/D39885
    
    UltraBlame original commit: b0124f06562982dce60b820d95aad23afd5cec90

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -1490,12 +1490,8 @@ pref("network.http.referer.defaultPolicy", 3);
 
 
 
-#ifdef NIGHTLY_BUILD
 
 pref("network.http.referer.defaultPolicy.trackers", 2);
-#else
-pref("network.http.referer.defaultPolicy.trackers", 3);
-#endif
 
 
 
