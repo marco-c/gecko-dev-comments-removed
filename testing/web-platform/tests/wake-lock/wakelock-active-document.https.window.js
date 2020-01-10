2f2a7278583b710662effddcbcf386ab40ea3ca8
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 21:18:28 +0000

    Bug 1587531 [wpt PR 19600] - wake lock: Implement new proposed API., a=testonly
    Automatic update from web-platform-tests
    wake lock: Implement new proposed API.
    
    This is another big update to the implementation, which now follows the
    API proposed in https://github.com/w3c/wake-lock/issues/226.
    
    From a Blink/Chromium internals perspective, things have not changed much,
    and we still perform the same permission checks and connect to the wake lock
    service via WakeLockStateRecord. The biggest change is that WakeLockController
    was merged into WakeLock itself.
    
    From an end-user perspective, though, the API now looks like a mix of the
    old, navigator-based API and the one we implemented a few months ago:
    
    * The main idea is that WakeLock.request() no longer returns a promise that
      does not resolve while the lock is held, as that is not very intuitive.
      Instead, it returns a new WakeLockSentinel object.
    * WakeLockSentinel is a new interface that can release() a lock and receive
      "release" events.
    * We do go back to a Navigator-based API, but we also support
      WorkerNavigator.
    * There is no WakeLockRequest interface or state tracking in the WakeLock
      interface.
    * WakeLock.requestPermission() was removed.
    * Consequently, we now have a WakeLockEvent interface.
    
    The changes to the spec still need to land, but there is some rough
    consensus on what the API should look like, and we do not want to miss the
    M79 branch cut.
    
    Bug: 257511, 1010162
    Change-Id: If1baed914ccd9eb7103a1bb71a40529976777ec2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1849683
    Reviewed-by: Kentaro Hara <harakenchromium.org>
    Reviewed-by: Bo <boliuchromium.org>
    Reviewed-by: Reilly Grant <reillygchromium.org>
    Commit-Queue: Raphael Kubo da Costa <raphael.kubo.da.costaintel.com>
    Cr-Commit-Position: refs/heads/master{#705632}
    
    --
    
    wpt-commits: 1a69ff8bc7a40d2e37ce0ce822e980a94bad9333
    wpt-pr: 19600
    
    UltraBlame original commit: 48db767a35dd4e1bd86fd3e92b1025d1672c7f8e

--- testing/web-platform/tests/wake-lock/wakelock-active-document.https.window.js
+++ testing/web-platform/tests/wake-lock/wakelock-active-document.https.window.js
@@ -3,8 +3,8 @@ function getWakeLockObject(iframe, url) {
     iframe.addEventListener(
       "load",
       () => {
-        const { WakeLock } = iframe.contentWindow;
-        resolve(WakeLock);
+        const { wakeLock } = iframe.contentWindow.navigator;
+        resolve(wakeLock);
       },
       { once: true }
     );
@@ -35,7 +35,7 @@ promise_test(async t => {
   );
   
   iframe.remove();
-}, "WakeLock.request() aborts if the document is not active.");
+}, "navigator.wakeLock.request() aborts if the document is not active.");
 
 promise_test(async t => {
   
@@ -78,4 +78,4 @@ promise_test(async t => {
   );
   
   outerIframe.remove();
-}, "WakeLock.request() aborts if the document is active, but not fully active.");
+}, "navigator.wakeLock.request() aborts if the document is active, but not fully active.");
