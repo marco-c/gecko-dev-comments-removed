34b04cf1c398a1df5fe9642fc3bd38ef42d27750
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 15:37:04 +0000

    Backed out changeset 109f0deb14a4 (bug 1545038) for build bustages. CLOSED TREE
    UltraBlame original commit: 80898589e1b159b81cba7a76d3ac23dce8bd5f1e

--- js/src/tests/lib/tests.py
+++ js/src/tests/lib/tests.py
@@ -169,8 +169,6 @@ class RefTestCase(object):
         
         self.jitflags = []
         
-        self.ignoredflags = []
-        
         
         self.test_reflect_stringify = None
         
@@ -231,9 +229,6 @@ class RefTestCase(object):
             cmd += ["--module", self.abs_path()]
         else:
             cmd += ["-f", self.abs_path()]
-        for flag in self.ignoredflags:
-            if flag in cmd:
-                cmd.remove(flag)
         return cmd
 
     def __str__(self):
