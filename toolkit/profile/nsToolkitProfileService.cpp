eaa68777a11f4987312ab651d7fd2ddde2f924e9
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 02:44:42 +0000

    Bug 1555319: Normalize the case of the installation path to always get a consistent install hash. r=froydnj
    The XRE_EXECUTABLE_FILE directory entry gives us the actual path that the binary
    was launched with. On systems where the filesystem is case insensitive this
    can be in any case, which ends up being a different install hash. This patch
    ensures that we get the correct case for the install path before generating the
    hash.
    
    We have the problem of users who are already affected by this issue. This patch
    also leaves the old hash available, if no default profile is found for the
    correct hash then we also check for a profile for the old hash, if so we use it
    for this hash going forwards. Testing this is kind of a pain, we have to add a
    way to override the old hash that we will check against. I'm not totally happy with
    how it is done here but not sure there is anything better.
    
    This also adds a test that calling xpcshell with differing cases returns the
    same install hash.
    
    Differential Revision: https://phabricator.services.mozilla.com/D34774
    
    UltraBlame original commit: 7aef19c3fd3dd72dc7779e67a582fca6d2595643

--- toolkit/profile/nsToolkitProfileService.cpp
+++ toolkit/profile/nsToolkitProfileService.cpp
@@ -771,9 +771,22 @@ nsresult nsToolkitProfileService::Init() {
     
     rv = mProfileDB.GetString(mInstallSection.get(), "Default",
                               installProfilePath);
+
     
     
-    mIsFirstRun = NS_FAILED(rv);
+    if (NS_FAILED(rv)) {
+      mIsFirstRun = true;
+
+      
+      
+      
+      rv = gDirServiceProvider->GetLegacyInstallHash(installHash);
+      NS_ENSURE_SUCCESS(rv, rv);
+      CopyUTF16toUTF8(installHash, mLegacyInstallSection);
+      mLegacyInstallSection.Insert(INSTALL_PREFIX, 0);
+    } else {
+      mIsFirstRun = false;
+    }
   }
 
   nsToolkitProfile* currentProfile = nullptr;
@@ -1098,8 +1111,9 @@ nsresult nsToolkitProfileService::CreateDefaultProfile(
 NS_IMETHODIMP
 nsToolkitProfileService::SelectStartupProfile(
     const nsTArray<nsCString>& aArgv, bool aIsResetting,
-    const nsACString& aUpdateChannel, nsIFile** aRootDir, nsIFile** aLocalDir,
-    nsIToolkitProfile** aProfile, bool* aDidCreate) {
+    const nsACString& aUpdateChannel, const nsACString& aLegacyInstallHash,
+    nsIFile** aRootDir, nsIFile** aLocalDir, nsIToolkitProfile** aProfile,
+    bool* aDidCreate) {
   int argc = aArgv.Length();
   
   
@@ -1115,6 +1129,10 @@ nsToolkitProfileService::SelectStartupProfile(
   argv[argc] = nullptr;
 
   mUpdateChannel = aUpdateChannel;
+  if (!aLegacyInstallHash.IsEmpty()) {
+    mLegacyInstallSection.Assign(aLegacyInstallHash);
+    mLegacyInstallSection.Insert(INSTALL_PREFIX, 0);
+  }
 
   bool wasDefault;
   nsresult rv =
@@ -1361,6 +1379,49 @@ nsresult nsToolkitProfileService::SelectStartupProfile(
   }
   if (ar == ARG_FOUND) {
     return NS_ERROR_SHOW_PROFILE_MANAGER;
+  }
+
+  if (mIsFirstRun && mUseDedicatedProfile &&
+      !mInstallSection.Equals(mLegacyInstallSection)) {
+    
+    
+    
+    
+    nsCString defaultDescriptor;
+    rv = mProfileDB.GetString(mLegacyInstallSection.get(), "Default",
+                              defaultDescriptor);
+
+    if (NS_SUCCEEDED(rv)) {
+      
+      bool isRelative;
+      nsCString descriptor;
+
+      for (RefPtr<nsToolkitProfile> profile : mProfiles) {
+        GetProfileDescriptor(profile, descriptor, &isRelative);
+
+        if (descriptor.Equals(defaultDescriptor)) {
+          
+          
+          
+          nsTArray<UniquePtr<KeyValue>> strings =
+              GetSectionStrings(&mProfileDB, mLegacyInstallSection.get());
+          for (const auto& kv : strings) {
+            mProfileDB.SetString(mInstallSection.get(), kv->key.get(),
+                                 kv->value.get());
+          }
+
+          
+          
+          
+          Flush();
+
+          
+          mDedicatedProfile = profile;
+          mIsFirstRun = false;
+          break;
+        }
+      }
+    }
   }
 
   
