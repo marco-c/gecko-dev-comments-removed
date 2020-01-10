8d261d6100a1929cd3069dfcf3fe36f7795114b5
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 03:23:51 +0000

    Bug 1562315 - Respect again=false indicated by http2 session when calling through TLSFilterTransaction, r=dragana
    Differential Revision: https://phabricator.services.mozilla.com/D36384
    
    UltraBlame original commit: 3eea5e6ac99ea5b13dd7435ddab7ac6af5d3ac72

--- netwerk/protocol/http/TunnelUtils.h
+++ netwerk/protocol/http/TunnelUtils.h
@@ -1,8 +1,8 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
 
-
-
-
-
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #ifndef mozilla_net_TLSFilterTransaction_h
 #define mozilla_net_TLSFilterTransaction_h
@@ -19,75 +19,75 @@
 #include "mozilla/TimeStamp.h"
 #include "prio.h"
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// a TLSFilterTransaction wraps another nsAHttpTransaction but
+// applies a encode/decode filter of TLS onto the ReadSegments
+// and WriteSegments data. It is not used for basic https://
+// but it is used for supplemental TLS tunnels - such as those
+// needed by CONNECT tunnels in HTTP/2 or even CONNECT tunnels when
+// the underlying proxy connection is already running TLS
+//
+// HTTP/2 CONNECT tunnels cannot use pushed IO layers because of
+// the multiplexing involved on the base stream. i.e. the base stream
+// once it is decrypted may have parts that are encrypted with a
+// variety of keys, or none at all
+
+/* ************************************************************************
+The input path of http over a spdy CONNECT tunnel once it is established as a
+stream
+
+note the "real http transaction" can be either a http/1 transaction or another
+spdy session inside the tunnel.
+
+  nsHttpConnection::OnInputStreamReady (real socket)
+  nsHttpConnection::OnSocketReadable()
+  SpdySession::WriteSegment()
+  SpdyStream::WriteSegment (tunnel stream)
+  SpdyConnectTransaction::WriteSegment
+  SpdyStream::OnWriteSegment(tunnel stream)
+  SpdySession::OnWriteSegment()
+  SpdySession::NetworkRead()
+  nsHttpConnection::OnWriteSegment (real socket)
+  realSocketIn->Read() return data from network
+
+now pop the stack back up to SpdyConnectTransaction::WriteSegment, the data
+that has been read is stored mInputData
+
+  SpdyConnectTransaction.mTunneledConn::OnInputStreamReady(mTunnelStreamIn)
+  SpdyConnectTransaction.mTunneledConn::OnSocketReadable()
+  TLSFilterTransaction::WriteSegment()
+  nsHttpTransaction::WriteSegment(real http transaction)
+  TLSFilterTransaction::OnWriteSegment() removes tls on way back up stack
+  SpdyConnectTransaction.mTunneledConn::OnWriteSegment()
+  // gets data from mInputData
+  SpdyConnectTransaction.mTunneledConn.mTunnelStreamIn->Read()
+
+The output path works similarly:
+  nsHttpConnection::OnOutputStreamReady (real socket)
+  nsHttpConnection::OnSocketWritable()
+  SpdySession::ReadSegments (locates tunnel)
+  SpdyStream::ReadSegments (tunnel stream)
+  SpdyConnectTransaction::ReadSegments()
+  SpdyConnectTransaction.mTunneledConn::OnOutputStreamReady (tunnel connection)
+  SpdyConnectTransaction.mTunneledConn::OnSocketWritable (tunnel connection)
+  TLSFilterTransaction::ReadSegment()
+  nsHttpTransaction::ReadSegment (real http transaction generates plaintext on
+                                  way down)
+  TLSFilterTransaction::OnReadSegment (BUF and LEN gets encrypted here on way
+                                       down)
+  SpdyConnectTransaction.mTunneledConn::OnReadSegment (BUF and LEN)
+                                                      (tunnel connection)
+  SpdyConnectTransaction.mTunneledConn.mTunnelStreamOut->Write(BUF, LEN) ..
+                                                     get stored in mOutputData
+
+Now pop the stack back up to SpdyConnectTransaction::ReadSegment(), where it has
+the encrypted text available in mOutputData
+
+  SpdyStream->OnReadSegment(BUF,LEN) from mOutputData. Tunnel stream
+  SpdySession->OnReadSegment() // encrypted data gets put in a data frame
+  nsHttpConnection->OnReadSegment()
+  realSocketOut->write() writes data to network
+
+**************************************************************************/
 
 struct PRSocketOptionData;
 
@@ -139,11 +139,15 @@ class TLSFilterTransaction final : public nsAHttpTransaction,
                    nsIAsyncInputStream** outSocketIn,
                    nsIAsyncOutputStream** outSocketOut);
 
-  
+  // nsAHttpTransaction overloads
   bool IsNullTransaction() override;
   NullHttpTransaction* QueryNullTransaction() override;
   nsHttpTransaction* QueryHttpTransaction() override;
   SpdyConnectTransaction* QuerySpdyConnectTransaction() override;
+  MOZ_MUST_USE nsresult WriteSegmentsAgain(nsAHttpSegmentWriter* writer,
+                                           uint32_t count,
+                                           uint32_t* countWritten,
+                                           bool* again) override;
 
  private:
   MOZ_MUST_USE nsresult StartTimerCallback();
@@ -165,12 +169,12 @@ class TLSFilterTransaction final : public nsAHttpTransaction,
 
  private:
   RefPtr<nsAHttpTransaction> mTransaction;
-  nsWeakPtr mWeakTrans;  
+  nsWeakPtr mWeakTrans;  // SpdyConnectTransaction *
   nsCOMPtr<nsISupports> mSecInfo;
   nsCOMPtr<nsITimer> mTimer;
   RefPtr<NudgeTunnelCallback> mNudgeCallback;
 
-  
+  // buffered network output, after encryption
   UniquePtr<char[]> mEncryptedText;
   uint32_t mEncryptedTextUsed;
   uint32_t mEncryptedTextSize;
@@ -202,9 +206,9 @@ class SpdyConnectTransaction final : public NullHttpTransaction {
     return this;
   }
 
-  
-  
-  
+  // A transaction is forced into plaintext when it is intended to be used as a
+  // CONNECT tunnel but the setup fails. The plaintext only carries the CONNECT
+  // error.
   void ForcePlainText();
   void MapStreamToHttpConnection(nsISocketTransport* aTransport,
                                  nsHttpConnectionInfo* aConnInfo,
@@ -218,9 +222,9 @@ class SpdyConnectTransaction final : public NullHttpTransaction {
   nsHttpRequestHead* RequestHead() final;
   void Close(nsresult reason) final;
 
-  
-  
-  
+  // ConnectedReadyForInput() tests whether the spdy connect transaction is
+  // attached to an nsHttpConnection that can properly deal with flow control,
+  // etc..
   bool ConnectedReadyForInput();
 
   bool IsWebsocket() { return mIsWebsocket; }
@@ -253,17 +257,17 @@ class SpdyConnectTransaction final : public NullHttpTransaction {
   TimeStamp mTimestampSyn;
   RefPtr<nsHttpConnectionInfo> mConnInfo;
 
-  
-  
-  
-  
+  // mTunneledConn, mTunnelTransport, mTunnelStreamIn, mTunnelStreamOut
+  // are the connectors to the "real" http connection. They are created
+  // together when the tunnel setup is complete and a static reference is held
+  // for the lifetime of the tunnel.
   RefPtr<nsHttpConnection> mTunneledConn;
   RefPtr<SocketTransportShim> mTunnelTransport;
   RefPtr<InputStreamShim> mTunnelStreamIn;
   RefPtr<OutputStreamShim> mTunnelStreamOut;
   RefPtr<nsHttpTransaction> mDrivingTransaction;
 
-  
+  // This is all for websocket support
   bool mIsWebsocket;
   bool mConnRefTaken;
   nsCOMPtr<nsIAsyncOutputStream> mInputShimPipe;
@@ -277,7 +281,7 @@ class SpdyConnectTransaction final : public NullHttpTransaction {
   bool mCreateShimErrorCalled;
 };
 
-}  
-}  
+}  // namespace net
+}  // namespace mozilla
 
-#endif  
+#endif  // mozilla_net_TLSFilterTransaction_h
