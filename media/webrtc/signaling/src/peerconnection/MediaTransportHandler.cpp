62d5a23e77fee9638aeff128d69c654ce23726a1
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 11:21:18 +0000

    Bug 1452713 - Update webRTCIPHandlingPolicy to match Chrome r=bwc,mixedpuppy
    Previously, the network.webRTCIPHandlingPolicy "disable_non_proxied_udp" only
    enabled the use of WebRTC if a proxy was configured and the WebRTC service
    supported TURN TCP.
    
    This aims to match Chrome's behavior by forcing the use of a proxy if one is
    configured, otherwise falling back to mode 3 (no host candidates and default
    route only).
    
    Also, remove some dead code left over from the old way of routing TURN
    communications through an HTTP proxy.
    
    Differential Revision: https://phabricator.services.mozilla.com/D37892
    
    UltraBlame original commit: 57b8410f0239f2756915d3ac62f152f3e70c213d

--- media/webrtc/signaling/src/peerconnection/MediaTransportHandler.cpp
+++ media/webrtc/signaling/src/peerconnection/MediaTransportHandler.cpp
@@ -162,6 +162,7 @@ class MediaTransportHandlerSTS : public MediaTransportHandler,
   RefPtr<NrIceCtx> mIceCtx;
   RefPtr<NrIceResolver> mDNSResolver;
   std::map<std::string, Transport> mTransports;
+  bool mProxyOnlyIfBehindProxy = false;
   bool mProxyOnly = false;
 
   
@@ -425,6 +426,8 @@ nsresult MediaTransportHandlerSTS::CreateIceCtx(
                                               __func__);
         }
 
+        mProxyOnlyIfBehindProxy = Preferences::GetBool(
+            "media.peerconnection.ice.proxy_only_if_behind_proxy", false);
         mProxyOnly =
             Preferences::GetBool("media.peerconnection.ice.proxy_only", false);
 
@@ -658,6 +661,10 @@ void MediaTransportHandlerSTS::StartIceGathering(
   mInitPromise->Then(
       mStsThread, __func__,
       [=, self = RefPtr<MediaTransportHandlerSTS>(this)]() {
+        if (mIceCtx->GetProxyConfig() && mProxyOnlyIfBehindProxy) {
+          mProxyOnly = true;
+        }
+
         
         
         
