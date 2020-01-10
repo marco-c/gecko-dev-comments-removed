ebf00ff40493bad9f51a8c8c8ac9c5c00d7807ff
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 18:46:00 +0000

    Bug 1584375. Remove unused origin from recording. r=nical
    This was added as part of an intermediate step to blob
    recoordination. It's not used anymore.
    
    Differential Revision: https://phabricator.services.mozilla.com/D47354
    
    UltraBlame original commit: dfa9c9bab74190db7873249a8c79608441135a1d

--- gfx/webrender_bindings/src/moz2d_renderer.rs
+++ gfx/webrender_bindings/src/moz2d_renderer.rs
@@ -172,7 +172,6 @@ struct BlobReader<'a> {
     reader: BufReader<'a>,
     
     begin: usize,
-    origin: IntPoint,
 }
 
 #[derive(PartialEq, Debug, Eq, Clone, Copy)]
@@ -203,9 +202,8 @@ impl<'a> BlobReader<'a> {
         let index_offset_pos = buf.len()-(mem::size_of::<usize>() + mem::size_of::<IntPoint>());
         assert!(index_offset_pos < buf.len());
         let index_offset = unsafe { convert_from_bytes::<usize>(&buf[index_offset_pos..]) };
-        let origin = unsafe { convert_from_bytes(&buf[(index_offset_pos + mem::size_of::<usize>())..]) };
 
-        BlobReader { reader: BufReader::new(&buf[index_offset..index_offset_pos]), begin: 0, origin }
+        BlobReader { reader: BufReader::new(&buf[index_offset..index_offset_pos]), begin: 0}
     }
 
     
@@ -251,13 +249,12 @@ impl BlobWriter {
     }
 
     
-    fn finish(mut self, origin: IntPoint) -> Vec<u8> {
+    fn finish(mut self) -> Vec<u8> {
         
         
         let index_begin = self.data.len();
         self.data.extend_from_slice(&self.index);
         self.data.extend_from_slice(convert_to_bytes(&index_begin));
-        self.data.extend_from_slice(convert_to_bytes(&origin));
         self.data
     }
 }
@@ -461,7 +458,7 @@ fn merge_blob_images(old_buf: &[u8], new_buf: &[u8], dirty_rect: Box2d, old_visi
 
     
 
-    let result = result.finish(new_reader.origin);
+    let result = result.finish();
     dump_index(&result);
     result
 }
