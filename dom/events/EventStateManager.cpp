59e395eb8c176bbb7b421030d062629776c78958
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 14:40:39 +0000

    Bug 1572939 - part1 : allow user inputs on editable content to activate document. r=masayuki
    As Chrome and Safari didn't block autoplay when haiving user input on editable content, it causes a compatible issue on Firefox because we only allow user inputs happening on non-editable content to activate document.
    
    It seems that we don't really need to restrict that user inputs, which can activate document, should only occur on non-editable content.
    Even if they occur on non-editable content, it might still have a chance to annoy user, it's totally depending on websites' design.
    
    Differential Revision: https://phabricator.services.mozilla.com/D43532
    
    UltraBlame original commit: 2ba2516d4a2c9d353a724b8c433701bf1b9df329

--- dom/events/EventStateManager.cpp
+++ dom/events/EventStateManager.cpp
@@ -800,15 +800,6 @@ nsresult EventStateManager::PreHandleEvent(nsPresContext* aPresContext,
   return NS_OK;
 }
 
-static bool IsTextInput(nsIContent* aContent) {
-  MOZ_ASSERT(aContent);
-  if (!aContent->IsElement()) {
-    return false;
-  }
-  TextEditor* textEditor = aContent->AsElement()->GetTextEditorInternal();
-  return textEditor && !textEditor->IsReadonly();
-}
-
 void EventStateManager::NotifyTargetUserActivation(WidgetEvent* aEvent,
                                                    nsIContent* aTargetContent) {
   if (!aEvent->IsTrusted()) {
@@ -827,13 +818,6 @@ void EventStateManager::NotifyTargetUserActivation(WidgetEvent* aEvent,
 
   Document* doc = node->OwnerDoc();
   if (!doc || doc->HasBeenUserGestureActivated()) {
-    return;
-  }
-
-  
-  
-  
-  if (aTargetContent->IsEditable() || IsTextInput(aTargetContent)) {
     return;
   }
 
