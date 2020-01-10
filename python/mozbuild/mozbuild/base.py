7ba6aa4703d8528cfab219f7da76a01cbb8381c8
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 02:38:15 +0000

    Bug 1560527 - Enable make backend verbose mode automatically rather than relying on mach setting it. r=froydnj
    This makes running without mach more consistent. e.g. running
    `make -C $objdir/toolkit/library/rust target` makes the cargo log
    verbose, and adding `-s` makes it less verbose.
    
    Differential Revision: https://phabricator.services.mozilla.com/D35521
    
    UltraBlame original commit: 2901d32eddaa6accd04b30725c7307338e94f48f

--- python/mozbuild/mozbuild/base.py
+++ python/mozbuild/mozbuild/base.py
@@ -713,8 +713,6 @@ class MozbuildObject(ProcessExecutionMixin):
 
         if silent:
             args.append('-s')
-        else:
-            args.append('BUILD_VERBOSE_LOG=1')
 
         
         
