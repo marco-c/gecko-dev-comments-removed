7f378f8dea1458a79874cb2d9ef4a4e9886f1812
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 09:59:19 +0000

    Bug 1564657 [wpt PR 17549] - Fix bad placeholder string in Preload + SRI tests, a=testonly
    Automatic update from web-platform-tests
    Fix bad placeholder string in Preload + SRI tests
    
    R=yhiranochromium.org
    
    Bug: 677022
    Change-Id: Iac6114a810417c115e92aaa7cfb0c2b978d563f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1679139
    Reviewed-by: Yutaka Hirano <yhiranochromium.org>
    Commit-Queue: Dominic Farolino <domchromium.org>
    Cr-Commit-Position: refs/heads/master{#672834}
    
    --
    
    wpt-commits: d9d459e7578c23b68865a02b8465c390d9025448
    wpt-pr: 17549
    
    UltraBlame original commit: f3e9a3e90e95b5b57b1e8d5d6ea9b0aa2479a220

--- testing/web-platform/tests/resources/sriharness.js
+++ testing/web-platform/tests/resources/sriharness.js
@@ -76,7 +76,9 @@ const SRIPreloadTest = (preload_sri_success, subresource_sri_success, name,
 
   
   test.step_func(() => {
-    assert_true(link.relList.supports('preload'), "Clever message here.");
+    assert_true(link.relList.supports('preload'),
+      "This test is automatically failing because the browser does not" +
+      "support `preload` links.");
   })();
 
   
