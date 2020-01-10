01c3885329f98b7504ae6c5c28f0e86e52064a84
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 20:45:19 +0000

    Bug 1587907 - Move test content inside the BrowserTestUtils.withNewTab function callback r=gregtatum
    In Bug 1587907 we got some contentPid not found errors. It seemed like we were
    failing intermittently at getting content pid. After moving test content inside
    the BrowserTestUtils.withNewTab callback, we won't get the same error anymore.
    Also from my testing, it looks like it makes the test execution faster. We were
    getting some intermittents before because of the timeouts, these will be fixed
    if my testing is correct.
    
    Differential Revision: https://phabricator.services.mozilla.com/D49951
    
    UltraBlame original commit: d739b932e308a4f462e7d4b7599b74657b25e6df

--- tools/profiler/tests/browser/browser_test_profile_multi_frame_page_info.js
+++ tools/profiler/tests/browser/browser_test_profile_multi_frame_page_info.js
@@ -7,57 +7,73 @@ add_task(async function test_profile_multi_frame_page_info() {
     return;
   }
   Assert.ok(!Services.profiler.IsActive());
-  
+  info("Clear the previous pages just in case we still some open tabs.");
   await Services.profiler.ClearAllPages();
+
+  info(
+    "Start the profiler to test the page information with multi frame page."
+  );
   startProfiler();
 
+  info("Open a tab with multi_frame.html in it.");
   
   const url = BASE_URL + "multi_frame.html";
-  let contentPid;
   await BrowserTestUtils.withNewTab(url, async function(contentBrowser) {
-    contentPid = await ContentTask.spawn(contentBrowser, null, () => {
+    const contentPid = await ContentTask.spawn(contentBrowser, null, () => {
       return Services.appinfo.processID;
     });
-  });
 
-  const profile = await Services.profiler.getProfileDataAsync();
-  Services.profiler.StopProfiler();
+    info("Capture the profile data.");
+    const profile = await Services.profiler.getProfileDataAsync();
+    Services.profiler.StopProfiler();
 
-  let foundPage = 0;
-  
-  let contentProcess = profile.processes.find(
-    p => p.threads[0].pid == contentPid
-  );
-  let parentPage;
-  for (const page of contentProcess.pages) {
+    let foundPage = 0;
     
-    if (page.url == url) {
-      Assert.equal(page.url, url);
-      Assert.equal(typeof page.browsingContextID, "number");
-      Assert.equal(typeof page.innerWindowID, "number");
+    let contentProcess = profile.processes.find(
+      p => p.threads[0].pid == contentPid
+    );
+
+    if (!contentProcess) {
+      throw new Error(
+        `Could not find the content process with given pid: ${contentPid}`
+      );
+    }
+
+    info(
+      "Check if the captured pages are the ones with correct values we created."
+    );
+
+    let parentPage;
+    for (const page of contentProcess.pages) {
       
-      Assert.equal(page.embedderInnerWindowID, 0);
-      parentPage = page;
-      foundPage++;
-      break;
+      if (page.url == url) {
+        Assert.equal(page.url, url);
+        Assert.equal(typeof page.browsingContextID, "number");
+        Assert.equal(typeof page.innerWindowID, "number");
+        
+        Assert.equal(page.embedderInnerWindowID, 0);
+        parentPage = page;
+        foundPage++;
+        break;
+      }
     }
-  }
 
-  Assert.notEqual(typeof parentPage, "undefined");
+    Assert.notEqual(typeof parentPage, "undefined");
 
-  for (const page of contentProcess.pages) {
-    
-    if (page.url == BASE_URL + "single_frame.html") {
-      Assert.equal(page.url, BASE_URL + "single_frame.html");
-      Assert.equal(typeof page.browsingContextID, "number");
-      Assert.equal(typeof page.innerWindowID, "number");
-      Assert.equal(typeof page.embedderInnerWindowID, "number");
-      Assert.notEqual(typeof parentPage, "undefined");
-      Assert.equal(page.embedderInnerWindowID, parentPage.innerWindowID);
-      foundPage++;
-      break;
+    for (const page of contentProcess.pages) {
+      
+      if (page.url == BASE_URL + "single_frame.html") {
+        Assert.equal(page.url, BASE_URL + "single_frame.html");
+        Assert.equal(typeof page.browsingContextID, "number");
+        Assert.equal(typeof page.innerWindowID, "number");
+        Assert.equal(typeof page.embedderInnerWindowID, "number");
+        Assert.notEqual(typeof parentPage, "undefined");
+        Assert.equal(page.embedderInnerWindowID, parentPage.innerWindowID);
+        foundPage++;
+        break;
+      }
     }
-  }
 
-  Assert.equal(foundPage, 2);
+    Assert.equal(foundPage, 2);
+  });
 });
