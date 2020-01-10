7c835e1e5dc7f28d1cd5d69aa792cdd02962648e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 21:12:49 +0000

    Bug 1447807 - Error: browser.ownerGlobal is null r=mixedpuppy
    Error: browser.ownerGlobal is null
    
    Differential Revision: https://phabricator.services.mozilla.com/D50578
    
    UltraBlame original commit: 652978346e607e3b90b6a9419e3c98f0682a4ad7

--- browser/components/extensions/parent/ext-browser.js
+++ browser/components/extensions/parent/ext-browser.js
@@ -699,13 +699,20 @@ class TabTracker extends TabTrackerBase {
   }
 
   getBrowserData(browser) {
-    let { gBrowser } = browser.ownerGlobal;
+    let window = browser.ownerGlobal;
+    if (!window) {
+      return {
+        tabId: -1,
+        windowId: -1,
+      };
+    }
+    let { gBrowser } = window;
     
     if (!gBrowser || !gBrowser.getTabForBrowser) {
-      if (browser.ownerGlobal.top.document.documentURI === "about:addons") {
+      if (window.top.document.documentURI === "about:addons") {
         
         
-        browser = browser.ownerGlobal.docShell.chromeEventHandler;
+        browser = window.docShell.chromeEventHandler;
 
         ({ gBrowser } = browser.ownerGlobal);
       } else {
