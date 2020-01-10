6b67f4613acf6b18c0f1a0e5be981f894e45a9a3
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 00:12:18 +0000

    Backed out 5 changesets (bug 1533877) for causing wpt permafailure in /html/cross-origin/anonymous.tentative.html CLOSED TREE
    Backed out changeset 1804d3178cd4 (bug 1533877)
    Backed out changeset b5a3e938f421 (bug 1533877)
    Backed out changeset 08efb10cceb2 (bug 1533877)
    Backed out changeset c4b8d755873b (bug 1533877)
    Backed out changeset e53056e11429 (bug 1533877)
    
    UltraBlame original commit: c15e88a7c4877694bdc3b11bff90895fc17ae65c

--- docshell/base/BrowsingContextFieldList.h
+++ docshell/base/BrowsingContextFieldList.h
@@ -17,7 +17,6 @@
 MOZ_BC_FIELD_RACY(Name, nsString)
 MOZ_BC_FIELD_RACY(Closed, bool)
 MOZ_BC_FIELD(CrossOriginPolicy, nsILoadInfo::CrossOriginPolicy)
-MOZ_BC_FIELD(InheritedCrossOriginPolicy, nsILoadInfo::CrossOriginPolicy)
 MOZ_BC_FIELD(OpenerPolicy, nsILoadInfo::CrossOriginOpenerPolicy)
 
 
