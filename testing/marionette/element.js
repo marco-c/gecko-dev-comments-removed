fae7b5b03387248e6994cab33871d073b671c295
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 21:40:03 +0000

    Bug 1587627 - [marionette] Remove `Anon` and `AnonAttribute` strategies from "WebDriver:FindElement" and "WebDriver:FindElements" command. r=webdriver-reviewers,maja_zf
    Differential Revision: https://phabricator.services.mozilla.com/D50804
    
    UltraBlame original commit: f77279b36116704bc63db97b0c2906295688a908

--- testing/marionette/element.js
+++ testing/marionette/element.js
@@ -82,8 +82,6 @@ element.Strategy = {
   PartialLinkText: "partial link text",
   TagName: "tag name",
   XPath: "xpath",
-  Anon: "anon",
-  AnonAttribute: "anon attribute",
 };
 
 
@@ -329,17 +327,7 @@ element.find = function(container, strategy, selector, opts = {}) {
       
       
       if (!opts.all && (!foundEls || foundEls.length == 0)) {
-        let msg;
-        switch (strategy) {
-          case element.Strategy.AnonAttribute:
-            msg =
-              "Unable to locate anonymous element: " + JSON.stringify(selector);
-            break;
-
-          default:
-            msg = "Unable to locate element: " + selector;
-        }
-
+        let msg = `Unable to locate element: ${selector}`;
         reject(new NoSuchElementError(msg));
       }
 
@@ -361,19 +349,7 @@ function find_(
   let rootNode = container.shadowRoot || container.frame.document;
 
   if (!startNode) {
-    switch (strategy) {
-      
-      
-      case element.Strategy.Anon:
-      case element.Strategy.AnonAttribute:
-        if (rootNode.nodeType == rootNode.DOCUMENT_NODE) {
-          startNode = rootNode.documentElement;
-        }
-        break;
-
-      default:
-        startNode = rootNode;
-    }
+    startNode = rootNode;
   }
 
   let res;
@@ -495,24 +471,6 @@ element.findByPartialLinkText = function(startNode, linkText) {
 
 
 
-element.findAnonymousNodes = function*(document, node) {
-  let anons = document.getAnonymousNodes(node) || [];
-  for (let node of anons) {
-    yield node;
-  }
-};
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 function* filterLinks(startNode, predicate) {
@@ -592,17 +550,6 @@ function findElement(strategy, selector, document, startNode = undefined) {
       } catch (e) {
         throw new InvalidSelectorError(`${e.message}: "${selector}"`);
       }
-
-    case element.Strategy.Anon:
-      return element.findAnonymousNodes(document, startNode).next().value;
-
-    case element.Strategy.AnonAttribute:
-      let attr = Object.keys(selector)[0];
-      return document.getAnonymousElementByAttribute(
-        startNode,
-        attr,
-        selector[attr]
-      );
   }
 
   throw new InvalidSelectorError(`No such strategy: ${strategy}`);
@@ -663,21 +610,6 @@ function findElements(strategy, selector, document, startNode = undefined) {
 
     case element.Strategy.Selector:
       return startNode.querySelectorAll(selector);
-
-    case element.Strategy.Anon:
-      return [...element.findAnonymousNodes(document, startNode)];
-
-    case element.Strategy.AnonAttribute:
-      let attr = Object.keys(selector)[0];
-      let el = document.getAnonymousElementByAttribute(
-        startNode,
-        attr,
-        selector[attr]
-      );
-      if (el) {
-        return [el];
-      }
-      return [];
 
     default:
       throw new InvalidSelectorError(`No such strategy: ${strategy}`);
