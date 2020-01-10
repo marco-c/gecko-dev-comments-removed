d6a909f3cf2a6f63fbecca34589cfb945902b54c
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 18:06:28 +0000

    Bug 1568497 [wpt PR 18505] - [Gecko Bug 1568497] Maintain update-intermittent for tests with conditions during a full update, a=testonly
    Automatic update from web-platform-tests
    Maintain update-intermittent for tests with conditions during a full update
    
    When `update_intermittent` is True and `remove_intermittent` is False,
    ensure `full_update` does not remove any existing intermittent statuses
    from tests with conditions.
    
    Differential Revision: https://phabricator.services.mozilla.com/D39614
    
    bugzilla-url: https://bugzilla.mozilla.org/show_bug.cgi?id=1568497
    gecko-commit: c267aa48b4dfebc648170f866168f4b699607a4a
    gecko-integration-branch: autoland
    gecko-reviewers: jgraham
    
    --
    Fixup lint errors
    
    --
    Fix use of intern in py3
    
    --
    Disable tests on py3
    
    --
    
    wpt-commits: 3b94f8748f9f03d9a4c63c2a8067f7f52535306f, 01ce984988b5decb335ac4c820e6d482777901d6, 77de8589cdd2f4cfaeeb54db9a40be118249b40e, 4534f63392ecac1a5593e7a5433568a4849d8ddf
    wpt-pr: 18505
    
    UltraBlame original commit: f98fe564945f59cf61858e8fa594093de5668bab

--- testing/web-platform/tests/tools/wptrunner/wptrunner/manifestupdate.py
+++ testing/web-platform/tests/tools/wptrunner/wptrunner/manifestupdate.py
@@ -515,9 +515,9 @@ class PropertyUpdate(object):
                 update_properties |= set(dependent_props)
             for condition in current_conditions:
                 if ((not condition.variables.issubset(update_properties) and
-                    not run_info_by_condition[condition])):
-                        conditions.append((condition.condition_node,
-                                            self.from_ini_value(condition.value)))
+                     not run_info_by_condition[condition])):
+                    conditions.append((condition.condition_node,
+                                       self.from_ini_value(condition.value)))
 
             new_conditions, errors = self._update_conditions_full(property_tree,
                                                                   prev_default=prev_default)
@@ -678,8 +678,9 @@ class ExpectedUpdate(PropertyUpdate):
         
         
         
-        if (not self.update_intermittent or self.remove_intermittent
-            or not result.known_intermittent):
+        if (not self.update_intermittent or
+            self.remove_intermittent or
+            not result.known_intermittent):
             return result.status
         return result.status + result.known_intermittent
 
