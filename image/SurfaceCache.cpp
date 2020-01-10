d07f927c84811d2383d5bb36eebeea601ca6bdac
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 16:41:01 +0000

    Backed out changeset 3519c255f86e (bug 1284651) for gtest failures CLOSED TREE
    UltraBlame original commit: 093599edfef84f61c8211b67ded2b037068d9020

--- image/SurfaceCache.cpp
+++ image/SurfaceCache.cpp
@@ -1391,11 +1391,6 @@ void SurfaceCache::Initialize() {
   uint64_t surfaceCacheMaxSizeKB =
       StaticPrefs::image_mem_surfacecache_max_size_kb_AtStartup();
 
-  if (sizeof(uintptr_t) <= 4) {
-    
-    surfaceCacheMaxSizeKB = 1024 * 1024;
-  }
-
   
   
   
