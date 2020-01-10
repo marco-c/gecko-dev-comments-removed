583670c81b3fe211d6352bcdabd21c8ae4292e3f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 12:48:48 +0000

    Bug 1572590 - Indent conditionally-defined prefs in geckoview-prefs.js. r=KrisWright
    Differential Revision: https://phabricator.services.mozilla.com/D41317
    
    UltraBlame original commit: 2b5c252ee8743ea9fada88dae37385187ad014eb

--- mobile/android/app/geckoview-prefs.js
+++ mobile/android/app/geckoview-prefs.js
@@ -2,6 +2,10 @@
 
 
 
+
+
+
+
 #filter substitution
 
 #include mobile.js
@@ -21,9 +25,9 @@ pref("toolkit.telemetry.eventping.enabled", false);
 pref("geckoview.console.enabled", false);
 
 #ifdef RELEASE_OR_BETA
-pref("geckoview.logging", "Warn");
+  pref("geckoview.logging", "Warn");
 #else
-pref("geckoview.logging", "Debug");
+  pref("geckoview.logging", "Debug");
 #endif
 
 
