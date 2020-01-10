567f177bf201956bce84120a3452507ef18d3ac1
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 10:18:36 +0000

    Bug 1568226 - Hide Lockwise data in Monitor card if a master password is set. r=ewright
    Differential Revision: https://phabricator.services.mozilla.com/D39228
    
    UltraBlame original commit: 03a739869466e18121e6f8ed6b00332a10026d31

--- browser/components/protections/content/monitor-card.js
+++ browser/components/protections/content/monitor-card.js
@@ -77,14 +77,24 @@ export default class MonitorClass {
     const exposedPasswords = this.doc.querySelector(
       "span[data-type='exposed-passwords']"
     );
-    const exposedLockwisePasswords = this.doc.querySelector(
-      "span[data-type='breached-lockwise-passwords']"
-    );
 
     storedEmail.textContent = monitorData.monitoredEmails;
     knownBreaches.textContent = monitorData.numBreaches;
     exposedPasswords.textContent = monitorData.passwords;
-    exposedLockwisePasswords.textContent =
-      monitorData.potentiallyBreachedLogins;
+
+    
+    if (monitorData.potentiallyBreachedLogins > 0) {
+      const lockwiseSection = this.doc.querySelector(
+        ".monitor-breached-passwords"
+      );
+      const exposedLockwisePasswords = this.doc.querySelector(
+        "span[data-type='breached-lockwise-passwords']"
+      );
+
+      exposedLockwisePasswords.textContent =
+        monitorData.potentiallyBreachedLogins;
+
+      lockwiseSection.classList.remove("hidden");
+    }
   }
 }
