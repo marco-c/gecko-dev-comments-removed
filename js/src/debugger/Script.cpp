18e6637dddaaf64435d9190da8eb5556e773a8fa
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 22:00:31 +0000

    Bug 1591342: When setting breakpoints, require usable cross-compartment wrappers. r=jonco
    When the `Debugger` API sets a breakpoint in a JSScript or wasm::Instance, the
    BreakpointSite and Breakpoint objects belong to the code's compartment
    (logically, at least - they're C++ objects and don't actually have any
    compartment). Since a `Debugger` and its debuggees must be in separate
    compartments, the Breakpoint's references to its owning `Debugger` and its
    handler object must go through cross-compartment wrappers.
    
    If we have nuked the `Debugger`'s compartment, it's not clear how we're still
    trying to set breakpoints in its debuggees, but we should at least throw an
    error, to capture a JavaScript stack when it occurs.
    
    Differential Revision: https://phabricator.services.mozilla.com/D51210
    
    UltraBlame original commit: f42214158987b271acc419ddd399018623da6b6a

--- js/src/debugger/Script.cpp
+++ js/src/debugger/Script.cpp
@@ -2043,6 +2043,22 @@ struct DebuggerScript::SetBreakpointMatcher {
   RootedObject handler_;
   RootedObject debuggerObject_;
 
+  bool wrapCrossCompartmentEdges() {
+    if (!cx_->compartment()->wrap(cx_, &handler_) ||
+        !cx_->compartment()->wrap(cx_, &debuggerObject_)) {
+      return false;
+    }
+
+    
+    
+    if (IsDeadProxyObject(handler_) || IsDeadProxyObject(debuggerObject_)) {
+      ReportAccessDenied(cx_);
+      return false;
+    }
+
+    return true;
+  }
+
  public:
   explicit SetBreakpointMatcher(JSContext* cx, Debugger* dbg, size_t offset,
                                 HandleObject handler)
@@ -2076,8 +2092,7 @@ struct DebuggerScript::SetBreakpointMatcher {
     
     
     AutoRealm ar(cx_, script);
-    if (!cx_->compartment()->wrap(cx_, &handler_) ||
-        !cx_->compartment()->wrap(cx_, &debuggerObject_)) {
+    if (!wrapCrossCompartmentEdges()) {
       return false;
     }
 
@@ -2115,8 +2130,7 @@ struct DebuggerScript::SetBreakpointMatcher {
     
     
     AutoRealm ar(cx_, wasmInstance);
-    if (!cx_->compartment()->wrap(cx_, &handler_) ||
-        !cx_->compartment()->wrap(cx_, &debuggerObject_)) {
+    if (!wrapCrossCompartmentEdges()) {
       return false;
     }
 
