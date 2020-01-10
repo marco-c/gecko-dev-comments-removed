b8b2b648737c2ff937c926d88c5cd4b063114ce6
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 00:52:31 +0000

    Bug 1556345 - Make slice length during anmiation telemetry use the same definition of whether we are animating as everywhere else r=sfink
    I'm not sure how this used to work but I'm pretty sure it doesn't any more. This changes us to use a single defintion of whether we're animating everywhere.
    
    Differential Revision: https://phabricator.services.mozilla.com/D33663
    
    UltraBlame original commit: 2e6f341318683e4c40f6a6a832340d767129ebc7

--- js/src/gc/Statistics.cpp
+++ js/src/gc/Statistics.cpp
@@ -147,7 +147,7 @@ using PhaseTable = EnumeratedArray<Phase, Phase::LIMIT, PhaseInfo>;
 using PhaseKindTable =
     EnumeratedArray<PhaseKind, PhaseKind::LIMIT, PhaseKindInfo>;
 
-#include "gc/StatsPhasesGenerated.inc"
+#include "gc/StatsPhasesGenerated.cpp"
 
 static double t(TimeDuration duration) { return duration.ToMilliseconds(); }
 
@@ -1111,7 +1111,7 @@ void Statistics::endSlice() {
     if (slice.budget.isTimeBudget()) {
       int64_t budget_ms = slice.budget.timeBudget.budget;
       runtime->addTelemetry(JS_TELEMETRY_GC_BUDGET_MS, budget_ms);
-      if (budget_ms == runtime->gc.defaultSliceBudget()) {
+      if (IsCurrentlyAnimating(runtime->lastAnimationTime, slice.end)) {
         runtime->addTelemetry(JS_TELEMETRY_GC_ANIMATION_MS, t(sliceTime));
       }
 
