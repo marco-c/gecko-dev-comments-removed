62540344c1b1efe23bdc76c29d6f15b1bbe367c5
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 03:26:28 +0000

    Backed out 2 changesets (bug 1552607) for causing checkstyle failures. CLOSED TREE
    Backed out changeset 53a39e402451 (bug 1552607)
    Backed out changeset 6385b8616d54 (bug 1552607)
    
    UltraBlame original commit: 3de1b396b7177de25726848b8f62872eeb6072c8

--- dom/media/platforms/android/RemoteDataDecoder.cpp
+++ dom/media/platforms/android/RemoteDataDecoder.cpp
@@ -437,7 +437,7 @@ class RemoteAudioDecoder : public RemoteDataDecoder {
     return mFirstDemuxedSampleTime->ToMicroseconds() > aTime;
   }
 
-  bool ShouldDiscardSample(int64_t aSession) const {
+  bool ShouldDiscardSample() const {
     AssertOnTaskQueue();
     
     
@@ -445,8 +445,7 @@ class RemoteAudioDecoder : public RemoteDataDecoder {
     
     
     
-    return GetState() == State::SHUTDOWN || !mFirstDemuxedSampleTime ||
-           mSession != aSession;
+    return GetState() == State::SHUTDOWN || !mFirstDemuxedSampleTime;
   }
 
   
@@ -467,7 +466,7 @@ class RemoteAudioDecoder : public RemoteDataDecoder {
 
     AssertOnTaskQueue();
 
-    if (ShouldDiscardSample(aSample->Session()) || !aBuffer->IsValid()) {
+    if (ShouldDiscardSample() || !aBuffer->IsValid()) {
       aSample->Dispose();
       return;
     }
@@ -591,7 +590,6 @@ RemoteDataDecoder::RemoteDataDecoder(MediaData::Type aType,
       mFormat(aFormat),
       mDrmStubId(aDrmStubId),
       mTaskQueue(aTaskQueue),
-      mSession(0),
       mNumPendingInputs(0) {}
 
 RefPtr<MediaDataDecoder::FlushPromise> RemoteDataDecoder::Flush() {
@@ -608,7 +606,6 @@ RefPtr<MediaDataDecoder::FlushPromise> RemoteDataDecoder::ProcessFlush() {
   mDecodePromise.RejectIfExists(NS_ERROR_DOM_MEDIA_CANCELED, __func__);
   mDrainPromise.RejectIfExists(NS_ERROR_DOM_MEDIA_CANCELED, __func__);
   SetState(State::DRAINED);
-  mSession++;
   mJavaDecoder->Flush();
   return FlushPromise::CreateAndResolve(true, __func__);
 }
@@ -636,7 +633,7 @@ RefPtr<MediaDataDecoder::DecodePromise> RemoteDataDecoder::Drain() {
     SetState(State::DRAINING);
     self->mInputBufferInfo->Set(0, 0, -1,
                                 MediaCodec::BUFFER_FLAG_END_OF_STREAM);
-    mJavaDecoder->Input(nullptr, self->mInputBufferInfo, nullptr, mSession);
+    mJavaDecoder->Input(nullptr, self->mInputBufferInfo, nullptr);
     return p;
   });
 }
@@ -743,7 +740,7 @@ RefPtr<MediaDataDecoder::DecodePromise> RemoteDataDecoder::ProcessDecode(
   SetState(State::DRAINABLE);
   mInputBufferInfo->Set(0, aSample->Size(), aSample->mTime.ToMicroseconds(), 0);
   return mJavaDecoder->Input(bytes, mInputBufferInfo,
-                             GetCryptoInfoFromSample(aSample), mSession)
+                             GetCryptoInfoFromSample(aSample))
              ? mDecodePromise.Ensure(__func__)
              : DecodePromise::CreateAndReject(
                    MediaResult(NS_ERROR_OUT_OF_MEMORY, __func__), __func__);
@@ -831,7 +828,6 @@ void RemoteDataDecoder::DrainComplete() {
   }
   SetState(State::DRAINED);
   ReturnDecodedData();
-  mSession++;
   
   mJavaDecoder->Flush();
 }
