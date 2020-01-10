40c4bdb6d85c02b54f8cc67f82deffe9fd535f51
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 16:43:04 +0000

    Bug 1575479 - Add support for STL iterators and range-based for to nsBaseHashtable. r=froydnj
    Differential Revision: https://phabricator.services.mozilla.com/D44982
    
    UltraBlame original commit: dcbc7c69fb6450f79cc7f3743fe88a57b2e826e6

--- xpcom/ds/PLDHashTable.h
+++ xpcom/ds/PLDHashTable.h
@@ -571,6 +571,8 @@ class PLDHashTable {
   class Iterator {
    public:
     explicit Iterator(PLDHashTable* aTable);
+    struct EndIteratorTag {};
+    Iterator(PLDHashTable* aTable, EndIteratorTag aTag);
     Iterator(Iterator&& aOther);
     ~Iterator();
 
@@ -591,6 +593,13 @@ class PLDHashTable {
     
     void Remove();
 
+    bool operator==(const Iterator& aOther) const {
+      MOZ_ASSERT(mTable == aOther.mTable);
+      return mNexts == aOther.mNexts;
+    }
+
+    Iterator Clone() const { return {*this}; }
+
    protected:
     PLDHashTable* mTable;  
 
@@ -607,7 +616,7 @@ class PLDHashTable {
     void MoveToNextLiveEntry();
 
     Iterator() = delete;
-    Iterator(const Iterator&) = delete;
+    Iterator(const Iterator&);
     Iterator& operator=(const Iterator&) = delete;
     Iterator& operator=(const Iterator&&) = delete;
   };
