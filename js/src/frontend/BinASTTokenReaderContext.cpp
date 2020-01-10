0a92c6c4a4207b5aaabe3a199cc5d29cf7ac97e5
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 15:41:49 +0000

    Bug 1572013 - Use infallibleEmplaceBack in HuffmanDictionary;r=arai
    Differential Revision: https://phabricator.services.mozilla.com/D44394
    
    UltraBlame original commit: 0c55fd936d607f88edc7fbaa0ea3eb71a27b6f44

--- js/src/frontend/BinASTTokenReaderContext.cpp
+++ js/src/frontend/BinASTTokenReaderContext.cpp
@@ -2189,15 +2189,13 @@ HuffmanDictionary::HuffmanDictionary(JSContext* cx)
     : fields(cx), listLengths(cx) {
   
   for (size_t i = 0; i < BINAST_INTERFACE_AND_FIELD_LIMIT; ++i) {
-    HuffmanTable fieldDefaultValue(HuffmanTableUnreachable{});
-    fields.infallibleAppend(std::move(fieldDefaultValue));
+    fields.infallibleEmplaceBack(HuffmanTableUnreachable{});
   }
 
   
   
   for (size_t i = 0; i < BINAST_NUMBER_OF_LIST_TYPES; ++i) {
-    HuffmanTableListLength listLengthDefaultValue(HuffmanTableUnreachable{});
-    listLengths.infallibleAppend(std::move(listLengthDefaultValue));
+    listLengths.infallibleEmplaceBack(HuffmanTableUnreachable{});
   }
 }
 
