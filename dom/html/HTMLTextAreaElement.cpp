13528005a170f88fbf2097f4023290cdef07c43b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 18:12:08 +0000

    Bug 1581523: part 2) Remove outdated comment in `HTMLTextAreaElement`. r=jorgk
    Differential Revision: https://phabricator.services.mozilla.com/D46127
    
    UltraBlame original commit: 27dad17d200b533b6616d23d7f61c79a0c60b3d5

--- dom/html/HTMLTextAreaElement.cpp
+++ dom/html/HTMLTextAreaElement.cpp
@@ -1046,7 +1046,6 @@ HTMLTextAreaElement::GetCols() { return Cols(); }
 
 NS_IMETHODIMP_(int32_t)
 HTMLTextAreaElement::GetWrapCols() {
-  
   nsHTMLTextWrap wrapProp;
   nsITextControlElement::GetWrapPropertyEnum(this, wrapProp);
   if (wrapProp == nsITextControlElement::eHTMLTextWrap_Off) {
