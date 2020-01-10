37f55f8aad4ab4d8b22d54e061790e9f0307a797
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 10:50:23 +0000

    Bug 1569440 - [mozrunner] Log when minidumps directory does not exist on device; r=gbrown
    The missing directory seems to happens ~20 or so times per test run. I hope this
    can help with investigations.
    
    Differential Revision: https://phabricator.services.mozilla.com/D39613
    
    UltraBlame original commit: f681cbc7d8a9dee1c01971d9b3bf16afa03cb776

--- testing/mozbase/mozrunner/mozrunner/devices/base.py
+++ testing/mozbase/mozrunner/mozrunner/devices/base.py
@@ -59,12 +59,18 @@ class Device(object):
         """
         remote_dump_dir = posixpath.join(self.app_ctx.remote_profile, 'minidumps')
         local_dump_dir = tempfile.mkdtemp()
+        if not self.device.is_dir(remote_dump_dir):
+            
+            
+            print("WARNING: No crash directory {} found on remote device".format(remote_dump_dir))
         try:
             self.device.pull(remote_dump_dir, local_dump_dir)
         except ADBError as e:
             
             if 'does not exist' not in str(e):
                 raise
+            else:
+                print("WARNING: {}".format(e))
         if os.listdir(local_dump_dir):
             self.device.rm(remote_dump_dir, recursive=True)
         return local_dump_dir
