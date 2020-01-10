13033cd51a3641334422a0b462efe3169016bf99
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 14:51:08 +0000

    Bug 1554976 - Copy actual address to CandidateInfo in MediaTransportHandler; r=mjf
    Differential Revision: https://phabricator.services.mozilla.com/D38494
    
    UltraBlame original commit: 24728144c26381ad70306b1c897a6f8c57319beb

--- media/webrtc/signaling/src/common/CandidateInfo.h
+++ media/webrtc/signaling/src/common/CandidateInfo.h
@@ -13,6 +13,8 @@ namespace mozilla {
 
 struct CandidateInfo {
   std::string mCandidate;
+  std::string mMDNSAddress;
+  std::string mActualAddress;
   std::string mUfrag;
   std::string mDefaultHostRtp;
   uint16_t mDefaultPortRtp = 0;
