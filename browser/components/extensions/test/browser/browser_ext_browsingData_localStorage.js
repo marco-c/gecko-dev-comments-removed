bf92c35ae53fcdd78de2f0070158d09b1770be37
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 23:56:26 +0000

    Bug 1596542 - Skip test_browserData_should_not_remove_extension_data if LSNG is disabled. r=mixedpuppy
    When LSNG is disabled, the browsingData API does clear the localStorage data by notify a "extension:purge-localStorage"
    message over the observer service, when the browserData API call doesn't specify a list of specific origins to clear,
    the code that handles "extension:purge-localStorage" will clear all the localStorage data, including the one stored by extensions:
    
    - https://searchfox.org/mozilla-central/rev/e7c61f4a68b974d5fecd216dc7407b631a24eb8f/browser/components/extensions/parent/ext-browsingData.js#172
    - https://searchfox.org/mozilla-central/rev/e7c61f4a68b974d5fecd216dc7407b631a24eb8f/dom/storage/StorageObserver.cpp#292-295,323,326
    
    Differential Revision: https://phabricator.services.mozilla.com/D53071
    
    UltraBlame original commit: d79432359063ff9e76eb2beb606d77713444cf88

--- browser/components/extensions/test/browser/browser_ext_browsingData_localStorage.js
+++ browser/components/extensions/test/browser/browser_ext_browsingData_localStorage.js
@@ -134,6 +134,14 @@ add_task(async function test_browserData_on_aboutnewtab_and_file_data() {
 });
 
 add_task(async function test_browserData_should_not_remove_extension_data() {
+  if (!Services.prefs.getBoolPref("dom.storage.next_gen")) {
+    
+    
+    
+    info("Test skipped because LSNG is currently disabled");
+    return;
+  }
+
   let extension = ExtensionTestUtils.loadExtension({
     async background() {
       window.localStorage.setItem("key", "value");
