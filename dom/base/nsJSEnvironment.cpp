cea1701ca581494d731bfa2485938a07212c4077
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 18:46:15 +0000

    Bug 1585697 - Move CycleCollectorStats definition to the top of the file r=mccr8
    This needs to be defined before FireForgetSkippable for the subsequnt patches.
    
    Differential Revision: https://phabricator.services.mozilla.com/D47918
    
    UltraBlame original commit: c3c52501f57581970abb7d228249a3b25d14f0fd

--- dom/base/nsJSEnvironment.cpp
+++ dom/base/nsJSEnvironment.cpp
@@ -97,8 +97,6 @@
 using namespace mozilla;
 using namespace mozilla::dom;
 
-const size_t gStackSize = 8192;
-
 
 #ifdef CompareString
 #  undef CompareString
@@ -201,6 +199,58 @@ static bool sShuttingDown;
 static bool sIsCompactingOnUserInactive = false;
 
 static TimeDuration sGCUnnotifiedTotalTime;
+
+struct CycleCollectorStats {
+  constexpr CycleCollectorStats() = default;
+  void Init();
+  void Clear();
+  void PrepareForCycleCollectionSlice(TimeStamp aDeadline = TimeStamp());
+  void FinishCycleCollectionSlice();
+  void RunForgetSkippable();
+
+  
+  TimeStamp mBeginSliceTime;
+
+  
+  TimeStamp mEndSliceTime;
+
+  
+  TimeStamp mBeginTime;
+
+  
+  TimeDuration mMaxGCDuration;
+
+  
+  bool mRanSyncForgetSkippable = false;
+
+  
+  uint32_t mSuspected = 0;
+
+  
+  
+  TimeDuration mMaxSkippableDuration;
+
+  
+  TimeDuration mMaxSliceTime;
+
+  
+  TimeDuration mMaxSliceTimeSinceClear;
+
+  
+  TimeDuration mTotalSliceTime;
+
+  
+  bool mAnyLockedOut = false;
+
+  
+  FILE* mFile = nullptr;
+
+  
+  
+  TimeStamp mIdleDeadline;
+};
+
+CycleCollectorStats gCCStats;
 
 static const char* ProcessNameForCollectorLog() {
   return XRE_GetProcessType() == GeckoProcessType_Default ? "default"
@@ -1245,118 +1295,67 @@ static TimeDuration TimeUntilNow(TimeStamp start) {
   return TimeBetween(start, TimeStamp::Now());
 }
 
-struct CycleCollectorStats {
-  constexpr CycleCollectorStats() = default;
+void CycleCollectorStats::Init() {
+  Clear();
 
-  void Init() {
-    Clear();
-
-    char* env = getenv("MOZ_CCTIMER");
-    if (!env) {
-      return;
+  char* env = getenv("MOZ_CCTIMER");
+  if (!env) {
+    return;
+  }
+  if (strcmp(env, "none") == 0) {
+    mFile = nullptr;
+  } else if (strcmp(env, "stdout") == 0) {
+    mFile = stdout;
+  } else if (strcmp(env, "stderr") == 0) {
+    mFile = stderr;
+  } else {
+    mFile = fopen(env, "a");
+    if (!mFile) {
+      MOZ_CRASH("Failed to open MOZ_CCTIMER log file.");
     }
-    if (strcmp(env, "none") == 0) {
-      mFile = nullptr;
-    } else if (strcmp(env, "stdout") == 0) {
-      mFile = stdout;
-    } else if (strcmp(env, "stderr") == 0) {
-      mFile = stderr;
-    } else {
-      mFile = fopen(env, "a");
-      if (!mFile) {
-        MOZ_CRASH("Failed to open MOZ_CCTIMER log file.");
+  }
+}
+
+void CycleCollectorStats::Clear() {
+  if (mFile && mFile != stdout && mFile != stderr) {
+    fclose(mFile);
+  }
+  *this = CycleCollectorStats();
+}
+
+void CycleCollectorStats::FinishCycleCollectionSlice() {
+  if (mBeginSliceTime.IsNull()) {
+    
+    
+    return;
+  }
+
+  mEndSliceTime = TimeStamp::Now();
+  TimeDuration duration = mEndSliceTime - mBeginSliceTime;
+
+  if (duration.ToSeconds()) {
+    TimeDuration idleDuration;
+    if (!mIdleDeadline.IsNull()) {
+      if (mIdleDeadline < mEndSliceTime) {
+        
+        idleDuration = mIdleDeadline - mBeginSliceTime;
+      } else {
+        idleDuration = duration;
       }
     }
+
+    uint32_t percent =
+        uint32_t(idleDuration.ToSeconds() / duration.ToSeconds() * 100);
+    Telemetry::Accumulate(Telemetry::CYCLE_COLLECTOR_SLICE_DURING_IDLE,
+                          percent);
   }
 
-  void Clear() {
-    if (mFile && mFile != stdout && mFile != stderr) {
-      fclose(mFile);
-    }
-    *this = CycleCollectorStats();
-  }
-
-  void PrepareForCycleCollectionSlice(TimeStamp aDeadline = TimeStamp());
-
-  void FinishCycleCollectionSlice() {
-    if (mBeginSliceTime.IsNull()) {
-      
-      
-      return;
-    }
-
-    mEndSliceTime = TimeStamp::Now();
-    TimeDuration duration = mEndSliceTime - mBeginSliceTime;
-
-    if (duration.ToSeconds()) {
-      TimeDuration idleDuration;
-      if (!mIdleDeadline.IsNull()) {
-        if (mIdleDeadline < mEndSliceTime) {
-          
-          idleDuration = mIdleDeadline - mBeginSliceTime;
-        } else {
-          idleDuration = duration;
-        }
-      }
-
-      uint32_t percent =
-          uint32_t(idleDuration.ToSeconds() / duration.ToSeconds() * 100);
-      Telemetry::Accumulate(Telemetry::CYCLE_COLLECTOR_SLICE_DURING_IDLE,
-                            percent);
-    }
-
-    TimeDuration sliceTime = TimeBetween(mBeginSliceTime, mEndSliceTime);
-    mMaxSliceTime = std::max(mMaxSliceTime, sliceTime);
-    mMaxSliceTimeSinceClear = std::max(mMaxSliceTimeSinceClear, sliceTime);
-    mTotalSliceTime += sliceTime;
-    mBeginSliceTime = TimeStamp();
-  }
-
-  void RunForgetSkippable();
-
-  
-  TimeStamp mBeginSliceTime;
-
-  
-  TimeStamp mEndSliceTime;
-
-  
-  TimeStamp mBeginTime;
-
-  
-  TimeDuration mMaxGCDuration;
-
-  
-  bool mRanSyncForgetSkippable = false;
-
-  
-  uint32_t mSuspected = 0;
-
-  
-  
-  TimeDuration mMaxSkippableDuration;
-
-  
-  TimeDuration mMaxSliceTime;
-
-  
-  TimeDuration mMaxSliceTimeSinceClear;
-
-  
-  TimeDuration mTotalSliceTime;
-
-  
-  bool mAnyLockedOut = false;
-
-  
-  FILE* mFile = nullptr;
-
-  
-  
-  TimeStamp mIdleDeadline;
-};
-
-CycleCollectorStats gCCStats;
+  TimeDuration sliceTime = TimeBetween(mBeginSliceTime, mEndSliceTime);
+  mMaxSliceTime = std::max(mMaxSliceTime, sliceTime);
+  mMaxSliceTimeSinceClear = std::max(mMaxSliceTimeSinceClear, sliceTime);
+  mTotalSliceTime += sliceTime;
+  mBeginSliceTime = TimeStamp();
+}
 
 void CycleCollectorStats::PrepareForCycleCollectionSlice(TimeStamp aDeadline) {
   mBeginSliceTime = TimeStamp::Now();
