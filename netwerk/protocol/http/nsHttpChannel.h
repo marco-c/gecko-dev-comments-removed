581caa6b8fd55ced0f38e2af63ea0fd10b273f81
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 20:43:49 +0000

    Bug 1579992 - Load pages into new webCOOP+COEP process type r=nika
    * This patch makes pages with the `OPENER_POLICY_SAME_ORIGIN_EMBEDDER_POLICY_REQUIRE_CORP` policy load into a special `webCOOP+COEP={pageOrigin}` remote type.
    * Adds `E10SUtils.WEB_REMOTE_COOP_COEP_TYPE_PREFIX="webCOOP+COEP="`
    * When a COOP process switch occurs and the target page doesn't have this policy, we pass a `preferredRemoteType="web"` into `E10SUtils.getRemoteTypeForPrincipal` ensuring that we correctly get a different `remoteType`
    * E10SUtils.getRemoteTypeForPrincipal is changed such that `if preferredRemoteType.startsWith(WEB_REMOTE_COOP_COEP_TYPE_PREFIX)` we don't override it with `webIsolated={pageOrigin}`.
    * `coop_header.sjs` is changed to also allow setting `Cross-Origin-Embedder-Policy` headers
    * `browser_httpCrossOriginOpenerPolicy.js` is changed to test that pages are correctly opened in the correct remoteType process.
    
    Differential Revision: https://phabricator.services.mozilla.com/D48715
    
    UltraBlame original commit: 15a9ec5f9b344d37cc7c1b6144e0d22adc365272

--- netwerk/protocol/http/nsHttpChannel.h
+++ netwerk/protocol/http/nsHttpChannel.h
@@ -648,6 +648,10 @@ class nsHttpChannel final : public HttpBaseChannel,
   static const uint32_t WAIT_FOR_CACHE_ENTRY = 1;
   static const uint32_t WAIT_FOR_OFFLINE_CACHE_ENTRY = 2;
 
+  
+  
+  Maybe<nsILoadInfo::CrossOriginOpenerPolicy> mComputedCrossOriginOpenerPolicy;
+
   bool mCacheOpenWithPriority;
   uint32_t mCacheQueueSizeWhenOpen;
 
