0fb4486aa86ef75f6ece64b5ab22c2d0f9f6eb15
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 16:43:58 +0000

    Bug 1575974 - Restrict pingsender to an allowlist of destination hosts r=chutten,gsvelto
    Sometimes curl doesn't have the url parsing function we would like to use.
    In order to not fail open (and have tests work on all platforms) we roll
    our own url parsing function. (Ew.)
    
    Differential Revision: https://phabricator.services.mozilla.com/D44046
    
    UltraBlame original commit: 521655c73cd1f418b8732c47b9821661ccfb303b

--- toolkit/components/telemetry/pingsender/pingsender.cpp
+++ toolkit/components/telemetry/pingsender/pingsender.cpp
@@ -4,6 +4,7 @@
 
 
 #include <cstdlib>
+#include <cstring>
 #include <ctime>
 #include <fstream>
 #include <iomanip>
@@ -24,6 +25,25 @@ const char* kContentEncodingHeader = "Content-Encoding: gzip";
 
 
 const uint32_t kConnectionTimeoutMs = 30 * 1000;
+
+
+bool IsValidDestination(std::string aHost) {
+  static const std::string kValidDestinations[] = {
+    "localhost",
+    "incoming.telemetry.mozilla.org",
+  };
+  for (auto destination : kValidDestinations) {
+    if (aHost == destination) {
+      return true;
+    }
+  }
+  return false;
+}
+
+bool IsValidDestination(char* aHost) {
+  return IsValidDestination(std::string(aHost));
+}
+
 
 
 
