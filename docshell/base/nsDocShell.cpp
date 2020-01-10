9872b256d3aeabf46953dabec8b213432b5b3ae5
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 02:51:58 +0000

    Bug 1500829 - Remove nsIDocShell::shouldSaveLayoutState. r=nika
    It's unused.
    
    Differential Revision: https://phabricator.services.mozilla.com/D34949
    
    UltraBlame original commit: 08d6d034d45b7b0bbf2e3a6a8e429eb0d3c5e5bc

--- docshell/base/nsDocShell.cpp
+++ docshell/base/nsDocShell.cpp
@@ -11803,18 +11803,6 @@ nsresult nsDocShell::LoadHistoryEntry(nsISHEntry* aEntry, uint32_t aLoadType) {
   return rv;
 }
 
-NS_IMETHODIMP
-nsDocShell::GetShouldSaveLayoutState(bool* aShould) {
-  *aShould = false;
-  if (mOSHE) {
-    
-    
-    *aShould = mOSHE->GetSaveLayoutStateFlag();
-  }
-
-  return NS_OK;
-}
-
 nsresult nsDocShell::PersistLayoutHistoryState() {
   nsresult rv = NS_OK;
 
