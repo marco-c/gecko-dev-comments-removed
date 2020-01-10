d98d675354fd479e772d6fc13526009c6f88fd7a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 00:53:23 +0000

    Bug 1556555 - Generate TabId for PBrowserBridge in content process and retain reference to it. r=nika
    This lets us get the TabId for the PBrowser created via PBrowserBridge in the embedding process.
    
    Differential Revision: https://phabricator.services.mozilla.com/D33565
    
    UltraBlame original commit: 65ad80c87934af3e0b4b8fba5b73a34c0a46690f

--- dom/ipc/RemoteBrowser.h
+++ dom/ipc/RemoteBrowser.h
@@ -47,6 +47,7 @@ class RemoteBrowser : public nsISupports {
   
   virtual BrowserBridgeHost* AsBrowserBridgeHost() = 0;
 
+  virtual TabId GetTabId() const = 0;
   virtual LayersId GetLayersId() const = 0;
   virtual BrowsingContext* GetBrowsingContext() const = 0;
   virtual nsILoadContext* GetLoadContext() const = 0;
