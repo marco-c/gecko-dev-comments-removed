70515e6493d4219cf78743747a8f3c47373e76e0
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 13:47:06 +0000

    Bug 1564168 - [jsdbg2] Debugger.prototype.enabled should be removed.
    Differential Revision: https://phabricator.services.mozilla.com/D39414
    
    UltraBlame original commit: e40f0edf3e405abc486973305c18c23473c3df0b

--- js/src/debugger/Debugger.h
+++ js/src/debugger/Debugger.h
@@ -509,8 +509,6 @@ class Debugger : private mozilla::LinkedListElement<Debugger> {
     return observedGCs.put(majorGCNumber);
   }
 
-  bool isEnabled() const { return enabled; }
-
   static SavedFrame* getObjectAllocationSite(JSObject& obj);
 
   struct AllocationsLogEntry {
@@ -556,7 +554,6 @@ class Debugger : private mozilla::LinkedListElement<Debugger> {
       debuggees; 
   JS::ZoneSet debuggeeZones; 
   js::GCPtrObject uncaughtExceptionHook; 
-  bool enabled;
   bool allowUnobservedAsmJS;
 
   
@@ -654,6 +651,7 @@ class Debugger : private mozilla::LinkedListElement<Debugger> {
   FrameMap frames;
 
   
+
 
 
 
@@ -845,8 +843,6 @@ class Debugger : private mozilla::LinkedListElement<Debugger> {
   static MOZ_MUST_USE bool setHookImpl(JSContext* cx, CallArgs& args,
                                        Debugger& dbg, Hook which);
 
-  static bool getEnabled(JSContext* cx, unsigned argc, Value* vp);
-  static bool setEnabled(JSContext* cx, unsigned argc, Value* vp);
   static bool getOnDebuggerStatement(JSContext* cx, unsigned argc, Value* vp);
   static bool setOnDebuggerStatement(JSContext* cx, unsigned argc, Value* vp);
   static bool getOnExceptionUnwind(JSContext* cx, unsigned argc, Value* vp);
@@ -1422,16 +1418,12 @@ js::GCPtrNativeObject& Debugger::toJSObjectRef() {
   return object;
 }
 
-bool Debugger::observesEnterFrame() const {
-  return enabled && getHook(OnEnterFrame);
-}
+bool Debugger::observesEnterFrame() const { return getHook(OnEnterFrame); }
 
-bool Debugger::observesNewScript() const {
-  return enabled && getHook(OnNewScript);
-}
+bool Debugger::observesNewScript() const { return getHook(OnNewScript); }
 
 bool Debugger::observesNewGlobalObject() const {
-  return enabled && getHook(OnNewGlobalObject);
+  return getHook(OnNewGlobalObject);
 }
 
 bool Debugger::observesGlobal(GlobalObject* global) const {
