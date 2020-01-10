c24bd0b7d47663a1feed18fba4c668ebece9485b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 19:10:02 +0000

    Bug 1586278 - Append ".0" to stringified default values of floating-point prefs to distinguish them from integers in about:config. r=njn
    Differential Revision: https://phabricator.services.mozilla.com/D48197
    
    UltraBlame original commit: e7d483d8420ee716eb167dc691a4e892532a94b6

--- modules/libpref/Preferences.cpp
+++ modules/libpref/Preferences.cpp
@@ -5235,8 +5235,13 @@ static void InitPref_float(const char* aName, float aDefaultValue) {
   MOZ_ASSERT(XRE_IsParentProcess());
   PrefValue value;
   
+  
+  
   nsAutoCString defaultValue;
   defaultValue.AppendFloat(aDefaultValue);
+  if (!defaultValue.Contains('.') && !defaultValue.Contains('e')) {
+    defaultValue.AppendLiteral(".0");
+  }
   value.mStringVal = defaultValue.get();
   pref_SetPref(aName, PrefType::String, PrefValueKind::Default, value,
                 false,
