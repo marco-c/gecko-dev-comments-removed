2093b8b3879c6b22a6a0cb7ffeec3be04b46c90a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 17:27:46 +0000

    Bug 1579311 - Adjust the order of the buttons in the confirmation dialog based on OS and focus the confirmation button by default. r=MattN
    Differential Revision: https://phabricator.services.mozilla.com/D45274
    
    UltraBlame original commit: 0aaeca7f56b7ca7e06db9b93c544f3d9f3b6f990

--- browser/components/aboutlogins/content/components/confirmation-dialog.js
+++ browser/components/aboutlogins/content/components/confirmation-dialog.js
@@ -17,17 +17,26 @@ export default class ConfirmationDialog extends HTMLElement {
     document.l10n.connectRoot(shadowRoot);
     shadowRoot.appendChild(template.content.cloneNode(true));
 
+    this._buttons = this.shadowRoot.querySelector(".buttons");
     this._cancelButton = this.shadowRoot.querySelector(".cancel-button");
     this._confirmButton = this.shadowRoot.querySelector(".confirm-button");
     this._dismissButton = this.shadowRoot.querySelector(".dismiss-button");
     this._message = this.shadowRoot.querySelector(".message");
     this._overlay = this.shadowRoot.querySelector(".overlay");
     this._title = this.shadowRoot.querySelector(".title");
+
+    this._buttons.classList.toggle("macosx", navigator.platform == "MacIntel");
   }
 
   handleEvent(event) {
     switch (event.type) {
       case "keydown":
+        if (event.repeat) {
+          
+          
+          event.preventDefault();
+          return;
+        }
         if (event.key === "Escape" && !event.defaultPrevented) {
           this.onCancel();
         }
@@ -92,7 +101,8 @@ export default class ConfirmationDialog extends HTMLElement {
 
     
     
-    this._cancelButton.focus();
+    
+    this._confirmButton.focus();
 
     this._promise = new Promise((resolve, reject) => {
       this._resolve = resolve;
