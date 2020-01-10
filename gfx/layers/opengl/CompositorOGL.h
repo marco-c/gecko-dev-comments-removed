776e354d70d7071b84e23988a83edb43fd74ed8e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 15:24:28 +0000

    Bug 1578099 - Use NativeLayer::NextSurfaceAsFramebuffer in CompositorOGL and remove #ifdefs. r=mattwoodrow
    CompositingRenderTargetOGL objects are no longer reused between frames. They are
    recreated each frame around a reused framebuffer. This lets us remove the
    SetOrigin method again, because we specify the right render target origin on
    creation.
    
    Depends on D44328
    
    Differential Revision: https://phabricator.services.mozilla.com/D44329
    
    UltraBlame original commit: d1d69713257666afec28fbb6b8a58080decf13f6

--- gfx/layers/opengl/CompositorOGL.h
+++ gfx/layers/opengl/CompositorOGL.h
@@ -37,14 +37,6 @@
 #include "nsXULAppAPI.h"      
 #include "nscore.h"           
 
-#ifdef XP_MACOSX
-
-
-
-struct DummyIOSurface;
-typedef DummyIOSurface* IOSurfacePtr;
-#endif
-
 class nsIWidget;
 
 namespace mozilla {
@@ -283,11 +275,6 @@ class CompositorOGL final : public Compositor {
   
   void RegisterTextureSource(TextureSource* aTextureSource);
   void UnregisterTextureSource(TextureSource* aTextureSource);
-
-#ifdef XP_MACOSX
-  void RegisterIOSurface(IOSurfacePtr aSurface);
-  void UnregisterIOSurface(IOSurfacePtr aSurface);
-#endif
 
  private:
   template <typename Geometry>
@@ -537,18 +524,6 @@ class CompositorOGL final : public Compositor {
   gfx::IntSize mViewportSize;
 
   ShaderProgramOGL* mCurrentProgram;
-
-#ifdef XP_MACOSX
-  struct IOSurfaceRefHasher {
-    std::size_t operator()(const IOSurfacePtr& aSurface) const {
-      return HashGeneric(reinterpret_cast<uintptr_t>(aSurface));
-    }
-  };
-
-  std::unordered_map<IOSurfacePtr, RefPtr<CompositingRenderTargetOGL>,
-                     IOSurfaceRefHasher>
-      mRegisteredIOSurfaceRenderTargets;
-#endif
 };
 
 }  
