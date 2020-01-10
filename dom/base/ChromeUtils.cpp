ebf2baa70fae415afded8ff8787dbfee03809783
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 18:25:53 +0000

    Bug 1579835, part 3 - Fix ChromeUtils::RequestProcInfo() with Fission. r=Ehsan
    Differential Revision: https://phabricator.services.mozilla.com/D45376
    
    UltraBlame original commit: c0fb66a739a3ea04ecb0b0e68a7953c6f5f5aa9d

--- dom/base/ChromeUtils.cpp
+++ dom/base/ChromeUtils.cpp
@@ -763,7 +763,7 @@ already_AddRefed<Promise> ChromeUtils::RequestProcInfo(GlobalObject& aGlobal,
                       
                       nsAutoString processType;
                       processType.Assign(contentParent->GetRemoteType());
-                      if (processType.EqualsLiteral(DEFAULT_REMOTE_TYPE)) {
+                      if (IsWebRemoteType(processType)) {
                         type = mozilla::ProcType::Web;
                       } else if (processType.EqualsLiteral(FILE_REMOTE_TYPE)) {
                         type = mozilla::ProcType::File;
