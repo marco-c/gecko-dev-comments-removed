552fc20e748f7694db847a3d819c941bd0785423
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 09:50:14 +0000

    Bug 1561955 - Run ./mach eslint on protections JS files to fix lint errors. r=eslint-fix CLOSED TREE
    UltraBlame original commit: fe65e1f85f7453f4368a3100a67c6dab52fb2662

--- browser/components/protections/content/monitor-card.js
+++ browser/components/protections/content/monitor-card.js
@@ -37,9 +37,7 @@ export default class MonitorClass {
       this.buildContent(loginData, monitorData);
 
       
-      const monitorCard = this.doc.querySelector(
-        ".card.monitor-card.hidden"
-      );
+      const monitorCard = this.doc.querySelector(".card.monitor-card.hidden");
       monitorCard.classList.remove("hidden");
     });
   }
