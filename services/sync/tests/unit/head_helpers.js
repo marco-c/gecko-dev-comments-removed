c838dd2bb9c90e021acfdf56aecaa99b62992fd4
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 16:41:01 +0000

    Bug 1575921 - store the FxA device ID in an FxA-specific pref branch. r=eoger
    Differential Revision: https://phabricator.services.mozilla.com/D43897
    
    UltraBlame original commit: 93d2d4bbe26300bc2caa3f712561333ac40ddf41

--- services/sync/tests/unit/head_helpers.js
+++ services/sync/tests/unit/head_helpers.js
@@ -509,15 +509,6 @@ function promiseOneObserver(topic, callback) {
   });
 }
 
-
-
-
-
-Utils._orig_getDefaultDeviceName = Utils.getDefaultDeviceName;
-Utils.getDefaultDeviceName = function() {
-  return "Test device name";
-};
-
 async function registerRotaryEngine() {
   let { RotaryEngine } = ChromeUtils.import(
     "resource://testing-common/services/sync/rotaryengine.js"
