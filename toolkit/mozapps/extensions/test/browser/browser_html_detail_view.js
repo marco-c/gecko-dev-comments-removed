75e40f4ab9d21a5dd3e84ab0911eec4f009bc9df
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 09:45:54 +0000

    Bug 1557175 - Hide inline options when add-on is disabled r=rpl
    Differential Revision: https://phabricator.services.mozilla.com/D35068
    
    UltraBlame original commit: 00ab4104c30487f9cf191a5aadfd085276c292a7

--- toolkit/mozapps/extensions/test/browser/browser_html_detail_view.js
+++ toolkit/mozapps/extensions/test/browser/browser_html_detail_view.js
@@ -767,14 +767,31 @@ add_task(async function testPrivateBrowsingExtension() {
   ok(!(await hasPrivateAllowed(id)), "PB is not allowed");
 
   let pbRow = card.querySelector(".addon-detail-row-private-browsing");
+  let name = card.querySelector(".addon-name");
 
   
   let [allow, disallow] = pbRow.querySelectorAll("input");
   let updated = BrowserTestUtils.waitForEvent(card, "update");
+
+  
+  let addonDisabled = AddonTestUtils.promiseAddonEvent("onDisabled");
   allow.click();
+  await addonDisabled;
+  is(
+    doc.l10n.getAttributes(name).id,
+    null,
+    "The disabled message is not shown for the add-on"
+  );
+
+  
   await updated;
   ok(!badge.hidden, "The PB badge is now shown");
   ok(await hasPrivateAllowed(id), "PB is allowed");
+  is(
+    doc.l10n.getAttributes(name).id,
+    null,
+    "The disabled message is not shown for the add-on"
+  );
 
   
   updated = BrowserTestUtils.waitForEvent(card, "update");
