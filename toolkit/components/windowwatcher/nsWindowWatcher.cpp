72dfe4ff62eabb72ecb8471a2793d878b7cceab2
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 11:49:14 +0000

    Bug 1571272: Add missing null check. r=bzbarsky
    Differential Revision: https://phabricator.services.mozilla.com/D40672
    
    UltraBlame original commit: 20c6a0493dd9fa5bd7932715fdaa7dfe1401561c

--- toolkit/components/windowwatcher/nsWindowWatcher.cpp
+++ toolkit/components/windowwatcher/nsWindowWatcher.cpp
@@ -296,8 +296,10 @@ nsWindowWatcher::OpenWindow(mozIDOMWindowProxy* aParent, const char* aUrl,
                               false,
                               false,
                               nullptr, getter_AddRefs(bc)));
-  nsCOMPtr<mozIDOMWindowProxy> win(bc->GetDOMWindow());
-  win.forget(aResult);
+  if (bc) {
+    nsCOMPtr<mozIDOMWindowProxy> win(bc->GetDOMWindow());
+    win.forget(aResult);
+  }
   return NS_OK;
 }
 
