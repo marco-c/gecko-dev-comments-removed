4dfa169125e69d25c2f4d2513b1501249a7604a9
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 20:07:31 +0000

    Backed out changeset 445aeafc7bd3 (bug 1588835) for causing perma fails on browser_bug633691.js. CLOSED TREE
    UltraBlame original commit: c7e591ee61c5368f31a48d6317d5521f16594db4

---
+++ browser/base/content/test/general/browser_bug633691.js
@@ -0,0 +1,45 @@
+
+
+
+
+add_task(async function test() {
+  const URL = "data:text/html,<iframe width='700' height='700'></iframe>";
+  await BrowserTestUtils.withNewTab({ gBrowser, url: URL }, async function(
+    browser
+  ) {
+    await ContentTask.spawn(
+      browser,
+      {
+        is_element_hidden_: is_element_hidden.toSource(),
+        is_hidden_: is_hidden.toSource(),
+      },
+      async function({ is_element_hidden_, is_hidden_ }) {
+        let loadError = ContentTaskUtils.waitForEvent(
+          this,
+          "AboutNetErrorLoad",
+          false,
+          null,
+          true
+        );
+        let iframe = content.document.querySelector("iframe");
+        iframe.src = "https://expired.example.com/";
+
+        await loadError;
+
+        
+        let is_hidden = eval(`(() => ${is_hidden_})()`);
+        let is_element_hidden = eval(`(() => ${is_element_hidden_})()`);
+        
+        let doc = content.document.getElementsByTagName("iframe")[0]
+          .contentDocument;
+        let aP = doc.getElementById("badCertAdvancedPanel");
+        ok(aP, "Advanced content should exist");
+        void is_hidden; 
+        is_element_hidden(
+          aP,
+          "Advanced content should not be visible by default"
+        );
+      }
+    );
+  });
+});
