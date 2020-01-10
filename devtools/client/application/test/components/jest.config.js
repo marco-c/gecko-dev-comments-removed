bbbf698bbe7aaced56edcc2dec31638dfa3fd619
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 09:49:55 +0000

    Backed out changeset 266931d06efb (bug 1558756) for causing devtools failures CLOSED TREE
    UltraBlame original commit: 4382f734fc377bbe7433fa6d58e58cd732ae4c82

--- devtools/client/application/test/components/jest.config.js
+++ devtools/client/application/test/components/jest.config.js
@@ -14,7 +14,6 @@ module.exports = {
     "^devtools/shared/plural-form": `${__dirname}/fixtures/plural-form`,
     "^chrome": `${__dirname}/fixtures/Chrome`,
     "^Services": `${__dirname}/fixtures/Services`,
-    "^devtools/client/shared/unicode-url": `${__dirname}/fixtures/unicode-url`,
     
     "^devtools\\/(.*)": `${__dirname}/../../../../$1`,
   },
