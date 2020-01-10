a38215a287b09da7f41bf9470e7ebcc442e4e030
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 18:22:34 +0000

    Backed out changeset e10a3c249943 (bug 1575054) for crashtest failures at reftest/tests/layout/generic/crashtests/812893.html on a CLOSED TREE
    UltraBlame original commit: 2bc477bd65f2a0169572f46528296568cadd0793

--- layout/generic/nsColumnSetFrame.cpp
+++ layout/generic/nsColumnSetFrame.cpp
@@ -323,14 +323,13 @@ nsColumnSetFrame::ReflowConfig nsColumnSetFrame::ChooseColumnStrategy(
   int32_t numColumns = colStyle->mColumnCount;
 
   
-  bool isBalancing =
+  const bool isBalancing =
       colStyle->mColumnFill == StyleColumnFill::Balance && !aForceAuto;
   if (isBalancing) {
     const uint32_t kMaxNestedColumnBalancingDepth = 2;
     const uint32_t balancingDepth =
         ColumnBalancingDepth(aReflowInput, kMaxNestedColumnBalancingDepth);
     if (balancingDepth == kMaxNestedColumnBalancingDepth) {
-      isBalancing = false;
       numColumns = 1;
     }
   }
