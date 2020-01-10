945269bc7b9c8ab431b3121f80c5af8504df222d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 00:15:04 +0000

    Bug 1518730 - Disable TRR for connectivity checks r=dragana
    Depends on D33473
    
    Differential Revision: https://phabricator.services.mozilla.com/D33475
    
    UltraBlame original commit: 61ee2e790ebd66f3710d57e7388a3a8b73a2496d

--- netwerk/base/NetworkConnectivityService.cpp
+++ netwerk/base/NetworkConnectivityService.cpp
@@ -129,15 +129,17 @@ NetworkConnectivityService::RecheckDNS() {
   nsAutoCString host;
   Preferences::GetCString("network.connectivity-service.DNSv4.domain", host);
 
-  rv = dns->AsyncResolveNative(host, nsIDNSService::RESOLVE_DISABLE_IPV6, this,
-                               NS_GetCurrentThread(), attrs,
-                               getter_AddRefs(mDNSv4Request));
+  rv = dns->AsyncResolveNative(
+      host,
+      nsIDNSService::RESOLVE_DISABLE_IPV6 | nsIDNSService::RESOLVE_DISABLE_TRR,
+      this, NS_GetCurrentThread(), attrs, getter_AddRefs(mDNSv4Request));
   NS_ENSURE_SUCCESS(rv, rv);
 
   Preferences::GetCString("network.connectivity-service.DNSv6.domain", host);
-  rv = dns->AsyncResolveNative(host, nsIDNSService::RESOLVE_DISABLE_IPV4, this,
-                               NS_GetCurrentThread(), attrs,
-                               getter_AddRefs(mDNSv6Request));
+  rv = dns->AsyncResolveNative(
+      host,
+      nsIDNSService::RESOLVE_DISABLE_IPV4 | nsIDNSService::RESOLVE_DISABLE_TRR,
+      this, NS_GetCurrentThread(), attrs, getter_AddRefs(mDNSv6Request));
   return rv;
 }
 
@@ -195,9 +197,10 @@ static inline already_AddRefed<nsIChannel> SetupIPCheckChannel(bool ipv4) {
       nullptr,  
       nullptr,  
       nullptr,
-      nsIRequest::LOAD_BYPASS_CACHE |    
-          nsIRequest::INHIBIT_CACHING |  
-          nsIRequest::LOAD_ANONYMOUS);   
+      nsIRequest::LOAD_BYPASS_CACHE |     
+          nsIRequest::INHIBIT_CACHING |   
+          nsIRequest::LOAD_DISABLE_TRR |  
+          nsIRequest::LOAD_ANONYMOUS);    
 
   NS_ENSURE_SUCCESS(rv, nullptr);
 
