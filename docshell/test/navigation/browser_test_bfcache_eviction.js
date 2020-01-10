ef50013f8f134303a85beeaffcc166dce3c7c405
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 22:35:47 +0000

    Bug 1591899 - Make various docshell tests work with session history in the parent turned off. r=smaug
    Differential Revision: https://phabricator.services.mozilla.com/D50791
    
    UltraBlame original commit: fcbaebf2501b8acd8ddcb54c71a511bd52fb0615

--- docshell/test/navigation/browser_test_bfcache_eviction.js
+++ docshell/test/navigation/browser_test_bfcache_eviction.js
@@ -14,30 +14,56 @@ add_task(async function() {
   await BrowserTestUtils.withNewTab({ gBrowser, url: testPage }, async function(
     browser
   ) {
-    
     let testDone = {};
-    testDone.promise = new Promise(resolve => {
-      testDone.resolve = resolve;
-    });
-
-    let legacySHistory = browser.browsingContext.sessionHistory;
-    
-    let historyListener = {
-      OnContentViewerEvicted() {
-        ok(
-          true,
-          "History listener got called after a content viewer was evicted"
-        );
-        legacySHistory.removeSHistoryListener(historyListener);
-        
-        testDone.resolve();
-      },
-      QueryInterface: ChromeUtils.generateQI([
-        Ci.nsISHistoryListener,
-        Ci.nsISupportsWeakReference,
-      ]),
-    };
-    legacySHistory.addSHistoryListener(historyListener);
+    if (!SpecialPowers.getBoolPref("fission.sessionHistoryInParent")) {
+      
+      testDone.promise = ContentTask.spawn(browser, null, async function() {
+        return new Promise(resolve => {
+          let webNavigation = content.docShell.QueryInterface(Ci.nsIWebNavigation);
+          let { legacySHistory } = webNavigation.sessionHistory;
+          
+          let historyListener = {
+            OnContentViewerEvicted() {
+              ok(
+                true,
+                "History listener got called after a content viewer was evicted"
+              );
+              legacySHistory.removeSHistoryListener(historyListener);
+              
+              resolve();
+            },
+            QueryInterface: ChromeUtils.generateQI([
+              Ci.nsISHistoryListener,
+              Ci.nsISupportsWeakReference,
+            ]),
+          };
+          legacySHistory.addSHistoryListener(historyListener);
+        });
+      });
+    } else {
+      
+      testDone.promise = new Promise(resolve => {
+        testDone.resolve = resolve;
+      });
+      let legacySHistory = browser.browsingContext.sessionHistory;
+      
+      let historyListener = {
+        OnContentViewerEvicted() {
+          ok(
+            true,
+            "History listener got called after a content viewer was evicted"
+          );
+          legacySHistory.removeSHistoryListener(historyListener);
+          
+          testDone.resolve();
+        },
+        QueryInterface: ChromeUtils.generateQI([
+          Ci.nsISHistoryListener,
+          Ci.nsISupportsWeakReference,
+        ]),
+      };
+      legacySHistory.addSHistoryListener(historyListener);
+    }
 
     
     testPage = `data:text/html,<html id='html1'><body id='body1'>I am a second tab!</body></html>`;
