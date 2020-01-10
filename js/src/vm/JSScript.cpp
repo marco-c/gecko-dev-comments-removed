d5c70d663153e31d87ad6dd47ffa3d895e83aa45
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 19:32:00 +0000

    Backed out changeset cc3b04156828 (bug 1562146) for SM build bustage at bug1562146.js on a CLOSED TREE.
    UltraBlame original commit: 8dfd8ae933128a7d9c8028c9294e8e0076c1d0a5

--- js/src/vm/JSScript.cpp
+++ js/src/vm/JSScript.cpp
@@ -2214,9 +2214,6 @@ JSFlatString* ScriptSource::substring(JSContext* cx, size_t start,
   MOZ_ASSERT(start <= stop);
 
   size_t len = stop - start;
-  if (!len) {
-    return cx->emptyString();
-  }
   UncompressedSourceCache::AutoHoldEntry holder;
 
   
@@ -2244,9 +2241,6 @@ JSFlatString* ScriptSource::substringDontDeflate(JSContext* cx, size_t start,
   MOZ_ASSERT(start <= stop);
 
   size_t len = stop - start;
-  if (!len) {
-    return cx->emptyString();
-  }
   UncompressedSourceCache::AutoHoldEntry holder;
 
   
