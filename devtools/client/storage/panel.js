aa8934e2f0d156d39a524c11d876aaf5b72addbc
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 12:29:12 +0000

    Bug 1569676 - Make storage panel's destroy synchronous. r=jdescottes
    Differential Revision: https://phabricator.services.mozilla.com/D39713
    
    UltraBlame original commit: 41f4ddd45b453560c7625c295a44595ab989b70a

--- devtools/client/storage/panel.js
+++ devtools/client/storage/panel.js
@@ -52,22 +52,22 @@ class StoragePanel {
 
 
   destroy() {
-    if (!this._destroyed) {
-      this.UI.destroy();
-      this.UI = null;
-
-      
-      this._front.destroy();
-      this._front = null;
-      this._destroyed = true;
-
-      this._target.off("close", this.destroy);
-      this._target = null;
-      this._toolbox = null;
-      this._panelWin = null;
+    if (this._destroyed) {
+      return;
     }
+    this._destroyed = true;
 
-    return Promise.resolve(null);
+    this.UI.destroy();
+    this.UI = null;
+
+    
+    this._front.destroy();
+    this._front = null;
+
+    this._target.off("close", this.destroy);
+    this._target = null;
+    this._toolbox = null;
+    this._panelWin = null;
   }
 }
 
