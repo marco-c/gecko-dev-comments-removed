87dc7eeaec301e0d051c7d8735c5930e0b94f819
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 16:08:20 +0000

    Bug 1574852 - part 67-5: Rewrite `HTMLEditRules::HandleDeleteNonCollapsedSelection()` with `EditorDOMPoint` r=m_kato
    Before splitting the method, we should make it use `EditorDOMPoint` to
    treat selection edges.
    
    Differential Revision: https://phabricator.services.mozilla.com/D44455
    
    UltraBlame original commit: 18c10d385d35d5992942786081b79036648bc58e

--- editor/libeditor/SelectionState.h
+++ editor/libeditor/SelectionState.h
@@ -209,6 +209,19 @@ class MOZ_STACK_CLASS AutoTrackDOMPoint final {
   ~AutoTrackDOMPoint() {
     mRangeUpdater.DropRangeItem(mRangeItem);
     if (mPoint) {
+      
+      
+      
+      if (NS_WARN_IF(!mRangeItem->mStartContainer) ||
+          NS_WARN_IF(mRangeItem->mStartOffset < 0)) {
+        mPoint->Clear();
+        return;
+      }
+      if (NS_WARN_IF(mRangeItem->mStartContainer->Length() <
+                     static_cast<uint32_t>(mRangeItem->mStartOffset))) {
+        mPoint->SetToEndOf(mRangeItem->mStartContainer);
+        return;
+      }
       mPoint->Set(mRangeItem->mStartContainer, mRangeItem->mStartOffset);
       return;
     }
