25f54b3e1ba2e1839970e43070138bc86e5b27cf
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 10:14:32 +0000

    Bug 1559962 - Use simpler expression for determining copy direction as per bulk memory operations spec change. r=jseward
    * https://github.com/WebAssembly/bulk-memory-operations/issues/96
    
    Differential Revision: https://phabricator.services.mozilla.com/D37799
    
    UltraBlame original commit: 1731c6c8abb5cefa505166365b016a73814c4d58

--- js/src/wasm/WasmInstance.cpp
+++ js/src/wasm/WasmInstance.cpp
@@ -457,8 +457,7 @@ static int32_t PerformWait(Instance* instance, uint32_t byteOffset, T value,
   uint64_t highestDstOffset = uint64_t(dstByteOffset) + uint64_t(len - 1);
   uint64_t highestSrcOffset = uint64_t(srcByteOffset) + uint64_t(len - 1);
 
-  bool copyDown =
-      srcByteOffset < dstByteOffset && dstByteOffset < highestSrcOffset;
+  bool copyDown = srcByteOffset < dstByteOffset;
 
   if (highestDstOffset >= memLen || highestSrcOffset >= memLen) {
     
@@ -675,7 +674,7 @@ static int32_t PerformWait(Instance* instance, uint32_t byteOffset, T value,
   uint64_t highestDstOffset = uint64_t(dstOffset) + (len - 1);
   uint64_t highestSrcOffset = uint64_t(srcOffset) + (len - 1);
 
-  bool copyDown = srcOffset < dstOffset && dstOffset < highestSrcOffset;
+  bool copyDown = srcOffset < dstOffset;
 
   if (highestDstOffset >= dstTableLen || highestSrcOffset >= srcTableLen) {
     
