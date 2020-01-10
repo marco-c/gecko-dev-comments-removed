62935cbdc3233b46a5c052e09478f800266bbede
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 04:08:21 +0000

    Bug 1561729 - Add unit tests for android power measurement parsing. r=perftest-reviewers,rwood
    This patch adds three unit tests (in one file) to the set of the raptor tests. The tests include (i) an Android 8 power parsing test, (ii) an android 7 power parsing test, and (iii) a test for android OS baseline parsing (tested using android 8 data).
    
    Differential Revision: https://phabricator.services.mozilla.com/D36926
    
    UltraBlame original commit: 62787e383696c4a2445180c4ddd75133007d7561

--- testing/raptor/raptor/power.py
+++ testing/raptor/raptor/power.py
@@ -17,7 +17,7 @@ def init_android_power_test(raptor):
     upload_dir = os.getenv("MOZ_UPLOAD_DIR")
     if not upload_dir:
         LOG.critical(
-            "% power test ignored; MOZ_UPLOAD_DIR unset" % raptor.config["app"]
+            "%s power test ignored; MOZ_UPLOAD_DIR unset" % raptor.config["app"]
         )
         return
     
@@ -91,7 +91,7 @@ def finish_android_power_test(raptor, test_name, os_baseline=False):
     upload_dir = os.getenv("MOZ_UPLOAD_DIR")
     if not upload_dir:
         LOG.critical(
-            "% power test ignored because MOZ_UPLOAD_DIR was not set" % test_name
+            "%s power test ignored because MOZ_UPLOAD_DIR was not set" % test_name
         )
         return
     
