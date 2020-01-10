eb40d8f18d1207b5757dc3cfb930f24ccba9d94b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 23:07:55 +0000

    Bug 1497007 - Minor improvements. r=ttung,asuth
    Differential Revision: https://phabricator.services.mozilla.com/D46947
    
    UltraBlame original commit: 44af22f9c5f4f1b3a868c20c28616012f17f6f07

--- dom/indexedDB/IDBTransaction.cpp
+++ dom/indexedDB/IDBTransaction.cpp
@@ -583,8 +583,7 @@ void IDBTransaction::AbortInternal(const nsresult aAbortCode,
   RefPtr<DOMException> error = aError;
 
   const bool isVersionChange = mMode == VERSION_CHANGE;
-  const bool isInvalidated = mDatabase->IsInvalidated();
-  bool needToSendAbort = mReadyState == INITIAL;
+  const bool needToSendAbort = mReadyState == INITIAL;
 
   mAbortCode = aAbortCode;
   mReadyState = DONE;
@@ -594,7 +593,7 @@ void IDBTransaction::AbortInternal(const nsresult aAbortCode,
     
     
     
-    if (!isInvalidated) {
+    if (!mDatabase->IsInvalidated()) {
       mDatabase->RevertToPreviousState();
     }
 
@@ -608,6 +607,8 @@ void IDBTransaction::AbortInternal(const nsresult aAbortCode,
         mDatabase->Spec()->objectStores();
 
     if (specArray.IsEmpty()) {
+      
+      
       mObjectStores.Clear();
       mDeletedObjectStores.Clear();
     } else {
@@ -847,7 +848,7 @@ DOMException* IDBTransaction::GetError() const {
 already_AddRefed<DOMStringList> IDBTransaction::ObjectStoreNames() const {
   AssertIsOnOwningThread();
 
-  if (mMode == IDBTransaction::VERSION_CHANGE) {
+  if (mMode == VERSION_CHANGE) {
     return mDatabase->ObjectStoreNames();
   }
 
