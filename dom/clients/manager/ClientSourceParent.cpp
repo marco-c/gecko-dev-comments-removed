ee89e11f1ff8ace752e281cdc7a3e5f5fa95e6ab
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 20:42:03 +0000

    Backed out changeset 87c82b01a388 (bug 1583859) for causing bustages in /builds/worker/workspace/build/src/obj-firefox/dist/include/mozilla/RefPtr.h CLOSED TREE
    UltraBlame original commit: 2cd1797819f4395a460678e437b649ac75b5f557

--- dom/clients/manager/ClientSourceParent.cpp
+++ dom/clients/manager/ClientSourceParent.cpp
@@ -241,7 +241,7 @@ bool ClientSourceParent::IsFrozen() const { return mFrozen; }
 
 bool ClientSourceParent::ExecutionReady() const { return mExecutionReady; }
 
-RefPtr<GenericNonExclusivePromise> ClientSourceParent::ExecutionReadyPromise() {
+RefPtr<GenericPromise> ClientSourceParent::ExecutionReadyPromise() {
   
   
   MOZ_ASSERT(!mExecutionReady);
