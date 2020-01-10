55440ea5e28279892034e4f7aafc93b378464211
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 18:26:51 +0000

    Bug 1536747 - [Linux] PictureInPicture window - open window manager menu on right mouse click, r=jhorak
    This patch depends on Bug 1584492.
    
    Differential Revision: https://phabricator.services.mozilla.com/D47424
    
    UltraBlame original commit: ca257801d86f5d4e9c1768722c7131216c07d38f

--- widget/gtk/nsWindow.cpp
+++ widget/gtk/nsWindow.cpp
@@ -2981,6 +2981,17 @@ void nsWindow::OnButtonReleaseEvent(GdkEventButton* aEvent) {
   if (StaticPrefs::ui_context_menus_after_mouseup()) {
     DispatchContextMenuEventFromMouseEvent(domButton, aEvent);
   }
+
+  
+  
+  if (mIsPIPWindow && aEvent->button == 3) {
+    static auto sGdkWindowShowWindowMenu =
+        (gboolean(*)(GdkWindow * window, GdkEvent*))
+            dlsym(RTLD_DEFAULT, "gdk_window_show_window_menu");
+    if (sGdkWindowShowWindowMenu) {
+      sGdkWindowShowWindowMenu(mGdkWindow, (GdkEvent*)aEvent);
+    }
+  }
 }
 
 void nsWindow::OnContainerFocusInEvent(GdkEventFocus* aEvent) {
