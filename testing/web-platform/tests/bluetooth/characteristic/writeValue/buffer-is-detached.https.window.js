7ebca79ae4f13f9dc830bcd1111ecfd0288e99ed
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 10:04:52 +0000

    Bug 1564800 [wpt PR 17578] - bluetooth: Standalone Tests for characteristic/, a=testonly
    Automatic update from web-platform-tests
    bluetooth: Standalone Tests for characteristic/
    
    This change refactors the Web Bluetooth WPT tests in the characteristic/
    directory to be standalone window tests, as well as use the async/await
    pattern. This makes the tests easier to format and easier to read.
    
    The generated tests are not updated in this patch because they will
    require updating the Python script that generates them.
    
    Bug: 509038
    Change-Id: I74d2158bbff3ef3b30d5286129cc3fc855e9a9da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1682840
    Reviewed-by: Reilly Grant <reillygchromium.org>
    Commit-Queue: Ovidio de Jesús Ruiz-Henríquez <odejesushchromium.org>
    Cr-Commit-Position: refs/heads/master{#675371}
    
    --
    
    wpt-commits: 175bb1c4268b76f5023708a11c9b48c694e7eae7
    wpt-pr: 17578
    
    UltraBlame original commit: 769fd7986e62db9789c93d1f4cee495db05e208e

---
+++ testing/web-platform/tests/bluetooth/characteristic/writeValue/buffer-is-detached.https.window.js
@@ -0,0 +1,25 @@
+
+
+
+
+
+'use strict';
+const test_desc = 'writeValue() fails when passed a detached buffer';
+
+function detachBuffer(buffer) {
+  window.postMessage('', '*', [buffer]);
+}
+
+bluetooth_test(async (t) => {
+  const {characteristic} = await getMeasurementIntervalCharacteristic();
+
+  const typed_array = Uint8Array.of(1, 2);
+  detachBuffer(typed_array.buffer);
+  await promise_rejects(
+      t, 'InvalidStateError', characteristic.writeValue(typed_array));
+
+  const array_buffer = Uint8Array.of(3, 4).buffer;
+  detachBuffer(array_buffer);
+  await promise_rejects(
+      t, 'InvalidStateError', characteristic.writeValue(array_buffer));
+}, test_desc);
