e37afe0db2d338fc721ab8547ab342e470315ed2
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 20:07:41 +0000

    Bug 1587690: Implement simple issue list UI. r=rcaliman,ladybenko
    Depends on D48948
    
    Differential Revision: https://phabricator.services.mozilla.com/D48949
    
    UltraBlame original commit: 98266b5e7e1ac8027db7eea41d773862e5184ff8

---
+++ devtools/client/inspector/compatibility/components/IssueList.js
@@ -0,0 +1,32 @@
+
+
+
+
+"use strict";
+
+const {
+  createFactory,
+  PureComponent,
+} = require("devtools/client/shared/vendor/react");
+const dom = require("devtools/client/shared/vendor/react-dom-factories");
+const PropTypes = require("devtools/client/shared/vendor/react-prop-types");
+
+const Types = require("../types");
+
+const IssueItem = createFactory(require("./IssueItem"));
+
+class IssueList extends PureComponent {
+  static get propTypes() {
+    return {
+      issues: PropTypes.arrayOf(PropTypes.shape(Types.issue)).isRequired,
+    };
+  }
+
+  render() {
+    const { issues } = this.props;
+
+    return dom.ul({}, issues.map(issue => IssueItem({ ...issue })));
+  }
+}
+
+module.exports = IssueList;
