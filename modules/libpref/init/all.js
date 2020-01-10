955a952412af5a39f0b0f31b62216f2257d393ee
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 15:35:18 +0000

    Bug 1575983 - Remove duplicate general.* prefs, part 2 r=njn
    Removes the last 12 duplicate general.* prefs. Replaces default values in StaticPrefList.yaml with their all.js counterparts where needed. Imports comments from all.js.
    
    Differential Revision: https://phabricator.services.mozilla.com/D44263
    
    UltraBlame original commit: 5360c56427523cf521e5984f11276edace647838

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -2635,23 +2635,11 @@ pref("mousewheel.autodir.honourroot", false);
 
 
 
-pref("general.smoothScroll.mouseWheel.durationMinMS", 200);
-pref("general.smoothScroll.mouseWheel.durationMaxMS", 400);
-pref("general.smoothScroll.pixels.durationMinMS", 150);
-pref("general.smoothScroll.pixels.durationMaxMS", 150);
-pref("general.smoothScroll.lines.durationMinMS", 150);
-pref("general.smoothScroll.lines.durationMaxMS", 150);
-pref("general.smoothScroll.pages.durationMinMS", 150);
-pref("general.smoothScroll.pages.durationMaxMS", 150);
 pref("general.smoothScroll.scrollbars.durationMinMS", 150);
 pref("general.smoothScroll.scrollbars.durationMaxMS", 150);
-pref("general.smoothScroll.other.durationMinMS", 150);
-pref("general.smoothScroll.other.durationMaxMS", 150);
 
-pref("general.smoothScroll.mouseWheel", true);
 pref("general.smoothScroll.pixels", true);
 pref("general.smoothScroll.lines", true);
-pref("general.smoothScroll.pages", true);
 pref("general.smoothScroll.scrollbars", true);
 pref("general.smoothScroll.other", true);
 
