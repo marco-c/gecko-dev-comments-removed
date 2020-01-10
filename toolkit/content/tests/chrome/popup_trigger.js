6576c575bfadffad1af0c0bd9d678df5afd857a9
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 17:29:34 +0000

    Bug 1555497 - Remove menupopup binding, r=bgrins
    Differential Revision: https://phabricator.services.mozilla.com/D33282
    
    UltraBlame original commit: 61808b1ff0a848cbff9ac393fbf4a0304abd2b43

--- toolkit/content/tests/chrome/popup_trigger.js
+++ toolkit/content/tests/chrome/popup_trigger.js
@@ -60,7 +60,7 @@ var popupTests = [
       gExpectedTriggerNode = gIsMenu ? "notset" : gTrigger;
       synthesizeMouse(gTrigger, 4, 4, {});
     },
-    result(testname) {
+    async result(testname) {
       gExpectedTriggerNode = null;
       
       is(
@@ -89,6 +89,13 @@ var popupTests = [
           testname + " opener.document.popupNode"
         );
       }
+
+      
+      
+      
+      await new Promise(r =>
+        requestAnimationFrame(() => requestAnimationFrame(r))
+      );
 
       
       var popuprect = gMenuPopup.getBoundingClientRect();
