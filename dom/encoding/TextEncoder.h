0c0f3a3f2a070059593029c006a6a136bfd40671
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 17:36:02 +0000

    Bug 1561573 - Avoid linearization and inflation to UTF-16 of the string input to TextEncoder. r=jandem,bzbarsky
    Differential Revision: https://phabricator.services.mozilla.com/D44121
    
    UltraBlame original commit: c7d88625df86c1ee5932292c955e9d912cadebe9

--- dom/encoding/TextEncoder.h
+++ dom/encoding/TextEncoder.h
@@ -40,7 +40,7 @@ class TextEncoder final : public NonRefcountedDOMObject {
 
 
 
-  void GetEncoding(nsAString& aEncoding);
+  void GetEncoding(nsACString& aEncoding);
 
   
 
@@ -52,11 +52,12 @@ class TextEncoder final : public NonRefcountedDOMObject {
 
 
   void Encode(JSContext* aCx, JS::Handle<JSObject*> aObj,
-              const nsAString& aString, JS::MutableHandle<JSObject*> aRetval,
-              ErrorResult& aRv);
+              JS::Handle<JSString*> aString,
+              JS::MutableHandle<JSObject*> aRetval, ErrorResult& aRv);
 
-  void EncodeInto(const nsAString& aSrc, const Uint8Array& aDst,
-                  TextEncoderEncodeIntoResult& aResult);
+  void EncodeInto(JSContext* aCx, JS::Handle<JSString*> aSrc,
+                  const Uint8Array& aDst, TextEncoderEncodeIntoResult& aResult,
+                  OOMReporter& aError);
 };
 
 }  
