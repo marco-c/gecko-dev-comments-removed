22e6a97400683e54591bc268e8e843d8bb4bd017
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 18:02:11 +0000

    Bug 1542830: Part 6 - Rewrite the untrusted modules processor in toolkit/xre; r=mhowell
    * Significant cleanup to `ModuleEvaluator`
    * `UntrustedModuleData` holds all of the accumulated untrusted module info for
      a single process.
    * `ProcessedModuleLoadEvent` holds information about an individual untrusted
      module load in a Gecko-friendly, sanitized, format.
    * Since multiple `ProcessModuleLoadEvent` objects may reference the same
      module, we store module metadata in a shared `ModuleInfo` structure.
    * The `UntrustedModulesProcessor` receives the events from `mozglue` and
      processes them on a background thread:
    ** It does not start background processing until the main thread has gone idle.
       The idea here is that we do not want to add any more background work until
       we are reasonably confident that Gecko is no longer starting up or doing
       other intense activity.
    ** Background processing runs at a background priority level, *except* when
       results are requested by telemetry itself.
    ** Telemetry requests the data via `UntrustedModulesProcessor::GetProcessedData`
       which runs at normal priority and returns a promise to the caller.
    
    Differential Revision: https://phabricator.services.mozilla.com/D43160
    
    UltraBlame original commit: 0a9169ab2623c6c91a876a5128bf2895892548ec

--- toolkit/xre/WinDllServices.h
+++ toolkit/xre/WinDllServices.h
@@ -7,67 +7,29 @@
 #ifndef mozilla_WinDllServices_h
 #define mozilla_WinDllServices_h
 
-#include "mozilla/CombinedStacks.h"
 #include "mozilla/glue/WindowsDllServices.h"
-#include "mozilla/Maybe.h"
-#include "mozilla/ModuleEvaluator_windows.h"
-#include "mozilla/mozalloc.h"
-#include "mozilla/UniquePtr.h"
-#include "mozilla/Vector.h"
+#include "mozilla/RefPtr.h"
+#include "mozilla/UntrustedModulesProcessor.h"
 
 namespace mozilla {
 
-
-
-class UntrustedModuleLoadTelemetryData {
- public:
-  UntrustedModuleLoadTelemetryData() = default;
-  
-  UntrustedModuleLoadTelemetryData(UntrustedModuleLoadTelemetryData&&) =
-      default;
-  UntrustedModuleLoadTelemetryData(
-      const UntrustedModuleLoadTelemetryData& aOther) = delete;
-
-  Vector<ModuleLoadEvent, 0, InfallibleAllocPolicy> mEvents;
-  Telemetry::CombinedStacks mStacks;
-  int mErrorModules = 0;
-  Maybe<double> mXULLoadDurationMS;
-};
-
-class UntrustedModulesManager;
-
-class DllServices : public mozilla::glue::DllServices {
+class DllServices : public glue::DllServices {
  public:
   static DllServices* Get();
 
   static const char* kTopicDllLoadedMainThread;
   static const char* kTopicDllLoadedNonMainThread;
 
-  
-
-
-
-
-
-
-
-
-
-
-  bool GetUntrustedModuleTelemetryData(UntrustedModuleLoadTelemetryData& aOut);
+  RefPtr<UntrustedModulesPromise> GetUntrustedModulesData();
 
  private:
   DllServices();
   ~DllServices() = default;
 
-  void NotifyDllLoad(const bool aIsMainThread,
-                     const nsString& aDllName) override;
+  void NotifyDllLoad(glue::EnhancedModuleLoadInfo&& aModLoadInfo) override;
+  void NotifyModuleLoadBacklog(ModuleLoadInfoVec&& aEvents) override;
 
-  void NotifyUntrustedModuleLoads(
-      const Vector<glue::ModuleLoadEvent, 0, InfallibleAllocPolicy>& aEvents)
-      override;
-
-  UniquePtr<UntrustedModulesManager> mUntrustedModulesManager;
+  RefPtr<UntrustedModulesProcessor> mUntrustedModulesProcessor;
 };
 
 }  
