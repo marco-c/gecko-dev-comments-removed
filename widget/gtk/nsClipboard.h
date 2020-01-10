5405fd5a9e39b686c538f091a4c4071c2bfada37
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 17:52:53 +0000

    Bug 1581638 - Provide logging to Linux/Gtk clipboard code, r=jhorak
    Differential Revision: https://phabricator.services.mozilla.com/D46266
    
    UltraBlame original commit: 15ce070b81cc1c62712b5aa0377644841e23b99a

--- widget/gtk/nsClipboard.h
+++ widget/gtk/nsClipboard.h
@@ -13,6 +13,17 @@
 #include "nsIBinaryOutputStream.h"
 #include <gtk/gtk.h>
 
+#undef LOG
+#ifdef MOZ_LOGGING
+#  include "mozilla/Logging.h"
+#  include "nsTArray.h"
+#  include "Units.h"
+extern mozilla::LazyLogModule gClipboardLog;
+#  define LOGCLIP(args) MOZ_LOG(gClipboardLog, mozilla::LogLevel::Debug, args)
+#else
+#  define LOGCLIP(args)
+#endif 
+
 class nsRetrievalContext {
  public:
   
@@ -76,5 +87,6 @@ class nsClipboard : public nsIClipboard, public nsIObserver {
 extern const int kClipboardTimeout;
 
 GdkAtom GetSelectionAtom(int32_t aWhichClipboard);
+int GetGeckoClipboardType(GtkClipboard* aGtkClipboard);
 
 #endif 
