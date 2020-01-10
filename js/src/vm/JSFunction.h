f078b0c986f58a9bda75ad45f2f698dc9d0c24b7
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 05:23:36 +0000

    Bug 1566607 - Add JSFunction::hasLazyScript. r=jandem
    This helper distinguishes normal lazy functions from lazy self-hosted
    funtions which have a nullptr LazyScript. A function may return true for
    hasLazyScript but still return a null lazy-script if it has been
    partially initialized and is still being accessed such as by GC.
    
    Differential Revision: https://phabricator.services.mozilla.com/D38252
    
    UltraBlame original commit: 3f2f72c37e4ee29bbfabb71a9cff72f05c8578e1

--- js/src/vm/JSFunction.h
+++ js/src/vm/JSFunction.h
@@ -299,10 +299,10 @@ class JSFunction : public js::NativeObject {
   
   
   
-  
-  
-  
   bool hasScript() const { return flags() & INTERPRETED; }
+  bool hasLazyScript() const {
+    return isInterpretedLazy() && !isSelfHostedOrIntrinsic();
+  }
 
   
   bool isArrow() const { return kind() == Arrow; }
@@ -594,7 +594,7 @@ class JSFunction : public js::NativeObject {
     if (hasScript()) {
       return nonLazyScript()->functionNonDelazifying();
     }
-    if (isInterpretedLazy() && !isSelfHostedBuiltin()) {
+    if (hasLazyScript()) {
       return lazyScript()->functionNonDelazifying();
     }
     return nullptr;
@@ -620,12 +620,11 @@ class JSFunction : public js::NativeObject {
                         uint16_t* length);
 
   js::LazyScript* lazyScript() const {
-    MOZ_ASSERT(isInterpretedLazy() && u.scripted.s.lazy_);
+    MOZ_ASSERT(hasLazyScript() && u.scripted.s.lazy_);
     return u.scripted.s.lazy_;
   }
-
-  js::LazyScript* lazyScriptOrNull() const {
-    MOZ_ASSERT(isInterpretedLazy());
+  js::LazyScript* maybeLazyScript() const {
+    MOZ_ASSERT(hasLazyScript());
     return u.scripted.s.lazy_;
   }
 
@@ -636,8 +635,8 @@ class JSFunction : public js::NativeObject {
     if (hasScript()) {
       return nonLazyScript()->generatorKind();
     }
-    if (js::LazyScript* lazy = lazyScriptOrNull()) {
-      return lazy->generatorKind();
+    if (hasLazyScript()) {
+      return lazyScript()->generatorKind();
     }
     MOZ_ASSERT(isSelfHostedBuiltin());
     return js::GeneratorKind::NotGenerator;
@@ -654,8 +653,8 @@ class JSFunction : public js::NativeObject {
     if (hasScript()) {
       return nonLazyScript()->asyncKind();
     }
-    if (js::LazyScript* lazy = lazyScriptOrNull()) {
-      return lazy->asyncKind();
+    if (hasLazyScript()) {
+      return lazyScript()->asyncKind();
     }
     MOZ_ASSERT(isSelfHostedBuiltin());
     return js::FunctionAsyncKind::SyncFunction;
@@ -677,9 +676,9 @@ class JSFunction : public js::NativeObject {
 
   void setUnlazifiedScript(JSScript* script) {
     MOZ_ASSERT(isInterpretedLazy());
-    if (lazyScriptOrNull()) {
+    if (hasLazyScript()) {
       
-      js::LazyScript::writeBarrierPre(lazyScriptOrNull());
+      js::LazyScript::writeBarrierPre(lazyScript());
       if (!lazyScript()->maybeScript()) {
         lazyScript()->initScript(script);
       }
