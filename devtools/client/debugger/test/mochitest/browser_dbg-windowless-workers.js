24a01edf67394c172ebb225d94d95a5daddcfaab
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 14:29:16 +0000

    Bug 1574502 - Sidebar panels should be collapsed by default. r=davidwalsh
    Differential Revision: https://phabricator.services.mozilla.com/D42317
    
    UltraBlame original commit: 41933c6deb8e10bb844e57f8d50f8874eb67a581

--- devtools/client/debugger/test/mochitest/browser_dbg-windowless-workers.js
+++ devtools/client/debugger/test/mochitest/browser_dbg-windowless-workers.js
@@ -37,6 +37,9 @@ function getValue(dbg, index) {
 
 
 add_task(async function() {
+  await pushPref("devtools.debugger.features.windowless-workers", true);
+  await pushPref("devtools.debugger.workers-visible", true);
+
   const dbg = await initDebugger("doc-windowless-workers.html");
   const mainThread = dbg.toolbox.threadFront.actor;
 
