091841c8ce86c8c0d4ed754546f730beab630b7a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 19:11:27 +0000

    Bug 1574415 - Part 11: Add regression test for StoreTypedObjectScalarProperty clobbered register bug. r=jandem
    Regression test for the fixed TypedObject bug mentioned in part 9.
    
    Differential Revision: https://phabricator.services.mozilla.com/D47759
    
    UltraBlame original commit: 1bd9ae4caa0ba3aa9b08743cbd899c0ce052567d

--- js/src/jit/BaselineIC.cpp
+++ js/src/jit/BaselineIC.cpp
@@ -2252,6 +2252,9 @@ bool FallbackICCodeCompiler::emit_SetElem() {
 void StoreToTypedObject(JSContext* cx, MacroAssembler& masm, Scalar::Type type,
                         const ValueOperand& value, const Address& dest,
                         Register scratch, Label* failure) {
+  
+  
+
   Label done;
 
   if (type == Scalar::Float32 || type == Scalar::Float64) {
