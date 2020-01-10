dffad9115ab58845f22e61162b0298c27b25c92e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 11:24:30 +0000

    Bug 1545356 - Hovering over 'flex' and 'grid' buttons, in the inspector view, indicates that they are clickable r=gl
    I made changes to the `LOCALIZATION NOTE` in the `inspector.properties` file and updated `DISPLAY_TYPES` in the `element-editor.js` file for `flex`, `grid`, `inline-flex` and `inline-grid`.
    
    Differential Revision: https://phabricator.services.mozilla.com/D40537
    
    UltraBlame original commit: 06b3b957184a4a44f6845d969e44c6ea8ea5faef

--- devtools/client/inspector/markup/views/element-editor.js
+++ devtools/client/inspector/markup/views/element-editor.js
@@ -72,13 +72,13 @@ const HTML_VOID_ELEMENTS = [
 
 
 const DISPLAY_TYPES = {
-  flex: INSPECTOR_L10N.getStr("markupView.display.flex.tooltiptext"),
+  flex: INSPECTOR_L10N.getStr("markupView.display.flex.tooltiptext2"),
   "inline-flex": INSPECTOR_L10N.getStr(
-    "markupView.display.inlineFlex.tooltiptext"
+    "markupView.display.inlineFlex.tooltiptext2"
   ),
-  grid: INSPECTOR_L10N.getStr("markupView.display.grid.tooltiptext"),
+  grid: INSPECTOR_L10N.getStr("markupView.display.grid.tooltiptext2"),
   "inline-grid": INSPECTOR_L10N.getStr(
-    "markupView.display.inlineGrid.tooltiptext"
+    "markupView.display.inlineGrid.tooltiptext2"
   ),
   subgrid: INSPECTOR_L10N.getStr("markupView.display.subgrid.tooltiptiptext"),
   "flow-root": INSPECTOR_L10N.getStr("markupView.display.flowRoot.tooltiptext"),
