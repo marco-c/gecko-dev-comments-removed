3b16b0836878acd09b2caa729d07bd88a613cbc0
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 18:15:54 +0000

    Bug 1551581 - New InactiveCSS rule to show warning when display:* used on a floated element; r=miker,pbro,fluent-reviewers,flod
    Differential Revision: https://phabricator.services.mozilla.com/D45309
    
    UltraBlame original commit: 7be1052a93279bde9838f35104232895bd8a9432

--- devtools/server/actors/utils/inactive-property-helper.js
+++ devtools/server/actors/utils/inactive-property-helper.js
@@ -172,6 +172,29 @@ class InactivePropertyHelper {
         msgId: "inactive-css-property-because-of-display",
         numFixProps: 1,
       },
+      {
+        invalidProperties: ["display"],
+        when: () =>
+          this.isFloated &&
+          this.checkResolvedStyle("display", [
+            "inline",
+            "inline-block",
+            "inline-table",
+            "inline-flex",
+            "inline-grid",
+            "table-cell",
+            "table-row",
+            "table-row-group",
+            "table-header-group",
+            "table-footer-group",
+            "table-column",
+            "table-column-group",
+            "table-caption",
+          ]),
+        fixId: "inactive-css-not-display-block-on-floated-fix",
+        msgId: "inactive-css-not-display-block-on-floated",
+        numFixProps: 2,
+      },
     ];
   }
 
@@ -334,22 +357,7 @@ class InactivePropertyHelper {
 
 
 
-  checkStyle(propName, values) {
-    return this.checkStyleForNode(this.node, propName, values);
-  }
-
-  
-
-
-
-
-
-
-
-
-
-
-  checkStyleForNode(node, propName, values) {
+  checkComputedStyle(propName, values) {
     if (!this.style) {
       return false;
     }
@@ -359,8 +367,26 @@ class InactivePropertyHelper {
   
 
 
+
+
+
+
+
+
+  checkResolvedStyle(propName, values) {
+    if (!(this.cssRule && this.cssRule.style)) {
+      return false;
+    }
+    const { style } = this.cssRule;
+
+    return values.some(value => style[propName] === value);
+  }
+
+  
+
+
   isInlineLevel() {
-    return this.checkStyle("display", [
+    return this.checkComputedStyle("display", [
       "inline",
       "inline-block",
       "inline-table",
@@ -379,7 +405,7 @@ class InactivePropertyHelper {
 
 
   get flexContainer() {
-    return this.checkStyle("display", ["flex", "inline-flex"]);
+    return this.checkComputedStyle("display", ["flex", "inline-flex"]);
   }
 
   
@@ -394,7 +420,7 @@ class InactivePropertyHelper {
 
 
   get gridContainer() {
-    return this.checkStyle("display", ["grid", "inline-grid"]);
+    return this.checkComputedStyle("display", ["grid", "inline-grid"]);
   }
 
   
@@ -409,8 +435,8 @@ class InactivePropertyHelper {
 
 
   get multiColContainer() {
-    const autoColumnWidth = this.checkStyle("column-width", ["auto"]);
-    const autoColumnCount = this.checkStyle("column-count", ["auto"]);
+    const autoColumnWidth = this.checkComputedStyle("column-width", ["auto"]);
+    const autoColumnCount = this.checkComputedStyle("column-count", ["auto"]);
 
     return !autoColumnWidth || !autoColumnCount;
   }
@@ -473,6 +499,13 @@ class InactivePropertyHelper {
 
   get nonReplaced() {
     return !this.replaced;
+  }
+
+  
+
+
+  get isFloated() {
+    return this.style && this.style.cssFloat !== "none";
   }
 
   
