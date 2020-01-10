859ef42dac0c94be6251c2f7b86fafb7ab549eec
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 10:22:46 +0000

    Bug 1543359 - P3. Add kCVPixelFormatType_420YpCbCr8BiPlanarFullRange support to MacIOSurface. r=mattwoodrow
    This will allow to differentiate full range vs limited range YUV.
    
    Differential Revision: https://phabricator.services.mozilla.com/D27211
    
    UltraBlame original commit: 90fff717198bcd58d786ee8849aa7e548260b446

--- gfx/gl/GLBlitHelper.cpp
+++ gfx/gl/GLBlitHelper.cpp
@@ -1008,7 +1008,7 @@ bool GLBlitHelper::BlitImage(layers::MacIOSurfaceImage* const srcImage,
       return false;
   }
 
-  if (pixelFormat == '2vuy') {
+  if (pixelFormat == kCVPixelFormatType_422YpCbCr8) {
     fragBody = kFragBody_CrYCb;
     
     
