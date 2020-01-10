911e10f94b0750c4dd8971f01e723b8bdc066e7e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 20:15:59 +0000

    Bug 1578752 - Pass event listener column to debugger for more accurate pretty print. r=davidwalsh
    Now the event listener tooltip url in the inspector will have an additional column part for generated file, which will be parsed as location.column to debugger, and the debugger pretty print can correctly create and use source map. It's the same bug as [[ https://bugzilla.mozilla.org/show_bug.cgi?id=1045237 | Bug 1045237]], [[ https://bugzilla.mozilla.org/show_bug.cgi?id=1134798 | Bug 1134798 ]], and [[ https://bugzilla.mozilla.org/show_bug.cgi?id=1175911 | Bug 1175911 ]]
    
    Differential Revision: https://phabricator.services.mozilla.com/D48812
    
    UltraBlame original commit: 0762a641fec70adb98249fdcf11b879d9506f118

--- devtools/client/shared/widgets/tooltip/EventTooltipHelper.js
+++ devtools/client/shared/widgets/tooltip/EventTooltipHelper.js
@@ -127,12 +127,13 @@ EventTooltip.prototype = {
           sourceMapService.subscribe(
             location.url,
             location.line,
-            null,
+            location.column,
             callback
           );
           this._subscriptions.push({
             url: location.url,
             line: location.line,
+            column: location.column,
             callback,
           });
         }
@@ -285,7 +286,6 @@ EventTooltip.prototype = {
     const content = header.nextElementSibling;
 
     const { sourceActor, uri } = this._eventEditors.get(content);
-
     const location = this._parseLocation(uri);
     if (location) {
       
@@ -309,15 +309,22 @@ EventTooltip.prototype = {
     if (uri && uri !== "?") {
       uri = uri.replace(/"/g, "");
 
-      const matches = uri.match(/(.*):(\d+$)/);
+      let matches = uri.match(/(.*):(\d+):(\d+$)/);
 
       if (matches) {
         return {
           url: matches[1],
           line: parseInt(matches[2], 10),
+          column: parseInt(matches[3], 10),
+        };
+      } else if ((matches = uri.match(/(.*):(\d+$)/))) {
+        return {
+          url: matches[1],
+          line: parseInt(matches[2], 10),
+          column: null,
         };
       }
-      return { url: uri, line: 1 };
+      return { url: uri, line: 1, column: null };
     }
     return null;
   },
@@ -357,7 +364,7 @@ EventTooltip.prototype = {
       sourceMapService.unsubscribe(
         subscription.url,
         subscription.line,
-        null,
+        subscription.column,
         subscription.callback
       );
     }
