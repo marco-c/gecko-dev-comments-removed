deef8e40c077b82988b4aa6451df1280c33bb463
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 14:29:08 +0000

    Bug 1575780 - We need a long request's timeout for trronly mode. r=mayhemer
    Differential Revision: https://phabricator.services.mozilla.com/D43410
    
    UltraBlame original commit: a902d015e909abb13de811988b58490111786cdb

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -5007,8 +5007,6 @@ pref("network.trr.bootstrapAddress", "");
 
 pref("network.trr.blacklist-duration", 60);
 
-pref("network.trr.request-timeout", 1500);
-
 pref("network.trr.early-AAAA", false);
 
 pref("network.trr.skip-AAAA-when-not-supported", true);
