e67c17f7dd3041f3cab8cb5d5514971110735b1a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 12:45:23 +0000

    Bug 1569881 - Short term workaround for Intel GPU r=jrmuizel
    When Windows version is mort than 1903 and GPU is intel GPU, it could cause flickering with DXGI_SWAP_EFFECT_FLIP_SEQUENTIAL. See Bug 1556634. As a short term workaround, use DXGI_ALPHA_MODE_PREMULTIPLIED instead of DXGI_ALPHA_MODE_IGNORE at SwapChain.
    
    Differential Revision: https://phabricator.services.mozilla.com/D41713
    
    UltraBlame original commit: 5d16fc1cebc7f5bd239eb3ee181687d0036f8901

--- gfx/thebes/gfxPlatform.cpp
+++ gfx/thebes/gfxPlatform.cpp
@@ -3115,6 +3115,22 @@ void gfxPlatform::InitWebRenderConfig() {
       gfxVars::SetUseWebRenderTripleBufferingWin(true);
     }
   }
+
+  
+  
+  
+  
+  
+  if (IsWin10May2019UpdateOrLater() && UseWebRender() &&
+      gfxVars::UseWebRenderDCompWin()) {
+    nsAutoString adapterVendorID;
+    nsCOMPtr<nsIGfxInfo> gfxInfo = services::GetGfxInfo();
+    gfxInfo->GetAdapterVendorID(adapterVendorID);
+    if (adapterVendorID == u"0x8086") {  
+      gfxVars::SetWorkaroundWebRenderIntelBug1556634(true);
+      gfxCriticalNote << "Use Premul SwapChain for Intel GPU";
+    }
+  }
 #endif
 
   
