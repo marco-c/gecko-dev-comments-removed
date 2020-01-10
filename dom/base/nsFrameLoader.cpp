c8ac6d23df517afe50ee5b95c992612bff4b56fc
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 04:38:43 +0000

    bug 1562889 - Convert content-sessionStore to C++: add browser epochs support r=peterv
    Differential Revision: https://phabricator.services.mozilla.com/D36606
    
    UltraBlame original commit: f33f444893963bf4bcf0385a2f43052f19170a7e

--- dom/base/nsFrameLoader.cpp
+++ dom/base/nsFrameLoader.cpp
@@ -3171,6 +3171,18 @@ bool nsFrameLoader::RequestTabStateFlush(uint32_t aFlushId, bool aIsFinal) {
   return false;
 }
 
+void nsFrameLoader::RequestEpochUpdate(uint32_t aEpoch) {
+  if (mSessionStoreListener) {
+    mSessionStoreListener->SetEpoch(aEpoch);
+    return;
+  }
+
+  
+  if (auto* browserParent = GetBrowserParent()) {
+    Unused << browserParent->SendUpdateEpoch(aEpoch);
+  }
+}
+
 void nsFrameLoader::Print(uint64_t aOuterWindowID,
                           nsIPrintSettings* aPrintSettings,
                           nsIWebProgressListener* aProgressListener,
