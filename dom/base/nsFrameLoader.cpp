d7ae6730039f2690429219a4e0f0c1ad7f38f145
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 00:06:32 +0000

    Bug 1519546, part 4 - Wire up basic EffectsInfo updates in DOM IPC. r=farre
    This commit adds RemoteBrowser::UpdateEffects for updating a remote browser's
    EffectsInfo over IPC.
    
    A following commit will actually use the EffectsInfo for
    enabling/disabling rendering for a remote browser, and another
    commit will actually use these IPDL methods.
    
    Differential Revision: https://phabricator.services.mozilla.com/D31472
    
    UltraBlame original commit: d31b7d33efc711fb8115663f4cfc5bc98fd58d73

--- dom/base/nsFrameLoader.cpp
+++ dom/base/nsFrameLoader.cpp
@@ -2628,16 +2628,11 @@ bool nsFrameLoader::TryRemoteBrowserInternal() {
       bool equals;
       if (!((parentWebNav = do_GetInterface(parentDocShell)) &&
             NS_SUCCEEDED(
+                NS_NewURI(getter_AddRefs(aboutAddons), "about:addons")) &&
+            NS_SUCCEEDED(
                 parentWebNav->GetCurrentURI(getter_AddRefs(parentURI))) &&
-            ((NS_SUCCEEDED(
-                  NS_NewURI(getter_AddRefs(aboutAddons), "about:addons")) &&
-              NS_SUCCEEDED(parentURI->EqualsExceptRef(aboutAddons, &equals)) &&
-              equals) ||
-             (NS_SUCCEEDED(NS_NewURI(
-                  getter_AddRefs(aboutAddons),
-                  "chrome://mozapps/content/extensions/aboutaddons.html")) &&
-              NS_SUCCEEDED(parentURI->EqualsExceptRef(aboutAddons, &equals)) &&
-              equals)))) {
+            NS_SUCCEEDED(parentURI->EqualsExceptRef(aboutAddons, &equals)) &&
+            equals)) {
         return false;
       }
     }
@@ -2782,6 +2777,10 @@ bool nsFrameLoader::IsRemoteFrame() {
     return true;
   }
   return false;
+}
+
+RemoteBrowser* nsFrameLoader::GetRemoteBrowser() const {
+  return mRemoteBrowser;
 }
 
 BrowserParent* nsFrameLoader::GetBrowserParent() const {
