7458c641b8492c70a5fc7807d17d99e3a3e1a71b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 05:14:37 +0000

    Bug 1564138 - Fix DevTools PropTypes errors when updating eslint. r=Ola.
    Differential Revision: https://phabricator.services.mozilla.com/D38093
    
    UltraBlame original commit: 238a8eb35207e07a52db336b8440776f987fca8f

--- devtools/client/application/src/components/Worker.js
+++ devtools/client/application/src/components/Worker.js
@@ -58,6 +58,8 @@ class Worker extends Component {
         active: PropTypes.bool,
         name: PropTypes.string.isRequired,
         scope: PropTypes.string.isRequired,
+        lastUpdateTime: PropTypes.number.isRequired,
+        url: PropTypes.string.isRequired,
         
         registrationFront: PropTypes.object,
         workerTargetFront: PropTypes.object,
