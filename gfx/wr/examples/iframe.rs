ea02feb4489edbe45ff691ec34d3647c417f8c69
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 18:14:06 +0000

    Backed out changeset da5d5eb153f2 (bug 1582273) for causing tidy bustages. CLOSED TREE
    UltraBlame original commit: fc1980f07d3042f2d2940423a0cb46ad23967482

--- gfx/wr/examples/iframe.rs
+++ gfx/wr/examples/iframe.rs
@@ -41,7 +41,7 @@ impl Example for App {
         sub_builder.push_simple_stacking_context(
             sub_bounds.origin,
             space_and_clip.spatial_id,
-            PrimitiveFlags::IS_BACKFACE_VISIBLE,
+            true,
         );
 
         
@@ -73,7 +73,7 @@ impl Example for App {
         builder.push_simple_stacking_context(
             sub_bounds.origin,
             space_and_clip.spatial_id,
-            PrimitiveFlags::IS_BACKFACE_VISIBLE,
+            true,
         );
 
         
