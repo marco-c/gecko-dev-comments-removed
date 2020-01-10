ff1630f812904d7ecaf557ecc2983b303a87c3ae
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 19:30:27 +0000

    Bug 1585373 - Re-enable PoisonIOInterposer for the mingw build r=dmajor
    This was disabled in Bug 1441558 because it broke the mingw-gcc build.
    But it doesn't break the mingw-clang build, and we don't support mingw-gcc
    anymore, so we can re-enable it.
    
    Differential Revision: https://phabricator.services.mozilla.com/D48645
    
    UltraBlame original commit: eb3960641cacd569bef099693acd1adadca02743

--- xpcom/build/PoisonIOInterposer.h
+++ xpcom/build/PoisonIOInterposer.h
@@ -37,7 +37,7 @@ void MozillaUnRegisterDebugFILE(FILE* aFile);
 
 MOZ_END_EXTERN_C
 
-#if defined(XP_MACOSX) || (defined(XP_WIN) && !defined(__MINGW32__))
+#if defined(XP_MACOSX) || defined(XP_WIN)
 
 #  ifdef __cplusplus
 namespace mozilla {
