2de85187bc4d4c21ea2bfe67faf9aeb56d145a88
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 16:57:19 +0000

    Bug 1580908 - Part 1: Update uuid to 0.7 to dedupe it. r=froydnj
    This also removes a dependency on rand 0.4 getting it closer to being
    able to remove that.
    
    Differential Revision: https://phabricator.services.mozilla.com/D45710
    
    UltraBlame original commit: 641f75b6c5d8d475ea7645aca80cb7237764cead

--- xpcom/rust/gkrust_utils/src/lib.rs
+++ xpcom/rust/gkrust_utils/src/lib.rs
@@ -7,7 +7,6 @@ use std::fmt::Write;
 
 #[no_mangle]
 pub extern "C" fn GkRustUtils_GenerateUUID(res: &mut nsACString) {
-    
-    let uuid = Uuid::new_v4().hyphenated().to_string();
-    write!(res, "{{{}}}", uuid).expect("Unexpected uuid generated");
+    let uuid = Uuid::new_v4();
+    write!(res, "{{{}}}", uuid.to_hyphenated_ref()).expect("Unexpected uuid generated");
 }
