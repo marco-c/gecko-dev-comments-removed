fce47a144756044fee9c496e581e93cb4ffe74fc
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 17:15:35 +0000

    Bug 1532933 - Enable ESLint for dom/xhr (automatic changes). r=Standard8,baku
    # ignore-this-changeset
    
    Differential Revision: https://phabricator.services.mozilla.com/D45680
    
    UltraBlame original commit: 2422a9d771bddef28d977b0013ab344935aa831f

--- dom/xhr/tests/terminateSyncXHR_worker.js
+++ dom/xhr/tests/terminateSyncXHR_worker.js
@@ -5,15 +5,16 @@
 
 onmessage = function(event) {
   throw "No messages should reach me!";
-}
+};
 
 var xhr = new XMLHttpRequest();
 xhr.open("GET", "worker_testXHR.txt", false);
-xhr.addEventListener("loadstart", function ()
-{
+xhr.addEventListener("loadstart", function() {
   
   postMessage("TERMINATE");
   
-  while(1) { true; }
+  while (1) {
+    true;
+  }
 });
 xhr.send(null);
