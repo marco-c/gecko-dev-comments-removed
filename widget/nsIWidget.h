e94b79b928c8103458f84d207011454c7ab33d14
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 22:24:40 +0000

    Bug 1554761 - Notify with a 'mac_app_activate' topic when the App has been activated, capturing Command-Tabbing behavior. OSX only. r=spohl
    Differential Revision: https://phabricator.services.mozilla.com/D39936
    
    UltraBlame original commit: 48b38cc561d44cb04b9bc3a9d1d499a1fb526152

--- widget/nsIWidget.h
+++ widget/nsIWidget.h
@@ -261,6 +261,12 @@ enum nsTopLevelWidgetZPlacement {
 
 #define NS_WIDGET_RESUME_PROCESS_OBSERVER_TOPIC "resume_process_notification"
 
+
+
+
+
+#define NS_WIDGET_MAC_APP_ACTIVATE_OBSERVER_TOPIC "mac_app_activate"
+
 namespace mozilla {
 namespace widget {
 
