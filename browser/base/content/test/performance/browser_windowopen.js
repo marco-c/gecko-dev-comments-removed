03499ffa97bc463cf040ac606c7a2eb440642e1a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 14:49:11 +0000

    Bug 1576936 - fix browser_windowopen for devtools button having moved, r=jaws
    Differential Revision: https://phabricator.services.mozilla.com/D43625
    
    UltraBlame original commit: 06a3816a2a49d95c16f13c6cb64ba0f66a95c398

--- browser/base/content/test/performance/browser_windowopen.js
+++ browser/base/content/test/performance/browser_windowopen.js
@@ -83,12 +83,7 @@ add_task(async function() {
             inRange(r.h, 13, 14) &&
             inRange(r.w, 14, 16) && 
             inRange(r.y1, 40, 80) && 
-            
-            
-            
-            AppConstants.MOZ_DEV_EDITION
-              ? inRange(r.x1, 100, 120)
-              : inRange(r.x1, 65, 100),
+            inRange(r.x1, 65, 100), 
         },
         {
           name: "bug 1555842 - the urlbar shouldn't flicker",
