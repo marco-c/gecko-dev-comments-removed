c2e4461be3c4f44baa4b399fbd8f70291569eae6
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 03:49:36 +0000

    Bug 1563450: rename `Selection::AddItemInternal` to `Selection::MaybeAddRangeAndTruncateOverlaps`. r=smaug
    Differential Revision: https://phabricator.services.mozilla.com/D36883
    
    UltraBlame original commit: 9342b4491a2db3dd11354fc1016e66486f923046

--- dom/base/Selection.h
+++ dom/base/Selection.h
@@ -161,8 +161,8 @@ class Selection final : public nsSupportsWeakReference,
                           ScrollAxis aVertical = ScrollAxis(),
                           ScrollAxis aHorizontal = ScrollAxis(),
                           int32_t aFlags = 0);
-  nsresult SubtractRange(RangeData* aRange, nsRange* aSubtract,
-                         nsTArray<RangeData>* aOutput);
+  static nsresult SubtractRange(RangeData* aRange, nsRange* aSubtract,
+                                nsTArray<RangeData>* aOutput);
 
  private:
   
@@ -747,7 +747,8 @@ class Selection final : public nsSupportsWeakReference,
   
 
 
-  nsresult AddItemInternal(nsRange* aRange, int32_t* aOutIndex);
+  nsresult MaybeAddRangeAndTruncateOverlaps(nsRange* aRange,
+                                            int32_t* aOutIndex);
 
   Document* GetDocument() const;
   nsPIDOMWindowOuter* GetWindow() const;
