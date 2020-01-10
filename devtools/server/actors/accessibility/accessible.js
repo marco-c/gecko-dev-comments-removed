1d0b1b0643126310aed5294806f98a9e9176645d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 00:58:44 +0000

    Bug 1552067 - adding text label audit type serverside support. r=nchevobbe
    Differential Revision: https://phabricator.services.mozilla.com/D33185
    
    UltraBlame original commit: e54cf94c44709cef253430a05447031e300f7856

--- devtools/server/actors/accessibility/accessible.js
+++ devtools/server/actors/accessibility/accessible.js
@@ -9,7 +9,8 @@ const { Actor, ActorClassWithSpec } = require("devtools/shared/protocol");
 const { accessibleSpec } = require("devtools/shared/specs/accessibility");
 const { accessibility: { AUDIT_TYPE } } = require("devtools/shared/constants");
 
-loader.lazyRequireGetter(this, "getContrastRatioFor", "devtools/server/actors/accessibility/contrast", true);
+loader.lazyRequireGetter(this, "getContrastRatioFor", "devtools/server/actors/accessibility/audit/contrast", true);
+loader.lazyRequireGetter(this, "auditTextLabel", "devtools/server/actors/accessibility/audit/text-label", true);
 loader.lazyRequireGetter(this, "isDefunct", "devtools/server/actors/utils/accessibility", true);
 loader.lazyRequireGetter(this, "findCssSelector", "devtools/shared/inspector/css-logic", true);
 loader.lazyRequireGetter(this, "events", "devtools/shared/event-emitter");
@@ -428,10 +429,24 @@ const AccessibleActor = ActorClassWithSpec(accessibleSpec, {
     return contrastRatio;
   },
 
+  
+
+
+
+
+
+
+
+
+
   _getAuditByType(type) {
     switch (type) {
       case AUDIT_TYPE.CONTRAST:
         return this._getContrastRatio();
+      case AUDIT_TYPE.TEXT_LABEL:
+        
+        
+        return auditTextLabel(this.rawAccessible);
       default:
         return null;
     }
