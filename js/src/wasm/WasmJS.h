9944e1e821e01f701b8947ba0add78203ca356f6
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 02:44:01 +0000

    Bug 1395509 - Track malloc memory used by wasm objects, part 1 r=luke
    This adds tracking of malloc memory to WasmInstanceObject, WasmGlobalObject, WasmMemoryObject and ResolveResponseClosure (the straightforward cases).
    
    Differential Revision: https://phabricator.services.mozilla.com/D35485
    
    UltraBlame original commit: b396c73d8978b5c1b381cc56aa02762b8c8dca8a

--- js/src/wasm/WasmJS.h
+++ js/src/wasm/WasmJS.h
@@ -20,6 +20,7 @@
 #define wasm_js_h
 
 #include "gc/Policy.h"
+#include "gc/ZoneAllocator.h"
 #include "vm/NativeObject.h"
 #include "wasm/WasmTypes.h"
 
@@ -225,7 +226,7 @@ class WasmInstanceObject : public NativeObject {
   
   
   using ExportMap = GCHashMap<uint32_t, HeapPtr<JSFunction*>,
-                              DefaultHasher<uint32_t>, SystemAllocPolicy>;
+                              DefaultHasher<uint32_t>, ZoneAllocPolicy>;
   ExportMap& exports() const;
 
   
@@ -233,7 +234,7 @@ class WasmInstanceObject : public NativeObject {
   
   using ScopeMap =
       JS::WeakCache<GCHashMap<uint32_t, WeakHeapPtr<WasmFunctionScope*>,
-                              DefaultHasher<uint32_t>, SystemAllocPolicy>>;
+                              DefaultHasher<uint32_t>, ZoneAllocPolicy>>;
   ScopeMap& scopes() const;
 
  public:
@@ -275,7 +276,8 @@ class WasmInstanceObject : public NativeObject {
   static WasmFunctionScope* getFunctionScope(
       JSContext* cx, HandleWasmInstanceObject instanceObj, uint32_t funcIndex);
 
-  WasmGlobalObjectVector& indirectGlobals() const;
+  using GlobalObjectVector = GCVector<WasmGlobalObject*, 0, ZoneAllocPolicy>;
+  GlobalObjectVector& indirectGlobals() const;
 };
 
 
@@ -295,7 +297,7 @@ class WasmMemoryObject : public NativeObject {
   using InstanceSet =
       JS::WeakCache<GCHashSet<WeakHeapPtrWasmInstanceObject,
                               MovableCellHasher<WeakHeapPtrWasmInstanceObject>,
-                              SystemAllocPolicy>>;
+                              ZoneAllocPolicy>>;
   bool hasObservers() const;
   InstanceSet& observers() const;
   InstanceSet* getOrCreateObservers(JSContext* cx);
