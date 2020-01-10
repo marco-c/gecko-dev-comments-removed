5a90c2b2c432a3886b48f5051825b0c6529999b7
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 18:14:00 +0000

    Bug 1583291 - Implement the left-shift operator in a manner that doesn't invoke C++ undefined behavior when the shift result would overflow int32_t.  r=froydnj
    Differential Revision: https://phabricator.services.mozilla.com/D46858
    
    UltraBlame original commit: 3350ec10023764430fa41b6efc3ce4333d15370c

--- js/src/vm/Interpreter-inl.h
+++ js/src/vm/Interpreter-inl.h
@@ -10,6 +10,7 @@
 #include "vm/Interpreter.h"
 
 #include "mozilla/Maybe.h"
+#include "mozilla/WrappingOperations.h"
 
 #include "jsnum.h"
 
@@ -916,7 +917,12 @@ static MOZ_ALWAYS_INLINE bool BitLsh(JSContext* cx, MutableHandleValue lhs,
     return BigInt::lsh(cx, lhs, rhs, out);
   }
 
-  out.setInt32(lhs.toInt32() << (rhs.toInt32() & 31));
+  
+  
+  
+  uint32_t left = static_cast<uint32_t>(lhs.toInt32());
+  uint8_t right = rhs.toInt32() & 31;
+  out.setInt32(mozilla::WrapToSigned(left << right));
   return true;
 }
 
