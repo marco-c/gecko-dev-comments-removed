f65bd82d844c5f71b8a1c27fc082e2e8ef731272
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 18:13:25 +0000

    Bug 1578075 - Increase stack size of paint threads on macOS Catalina to 1MB. r=jrmuizel
    Differential Revision: https://phabricator.services.mozilla.com/D46834
    
    UltraBlame original commit: 1d2de2fa28edd3bb9a19c3610468d30ee65bcbae

--- gfx/layers/PaintThread.cpp
+++ gfx/layers/PaintThread.cpp
@@ -79,7 +79,7 @@ static uint32_t GetPaintThreadStackSize() {
 #else
   
   if (nsCocoaFeatures::OnCatalinaOrLater()) {
-    static const uint32_t kCatalinaPaintThreadStackSize = 512 * 1024;
+    static const uint32_t kCatalinaPaintThreadStackSize = 1024 * 1024;
     static_assert(kCatalinaPaintThreadStackSize >= nsIThreadManager::DEFAULT_STACK_SIZE,
                   "update default stack size of paint "
                   "workers");
