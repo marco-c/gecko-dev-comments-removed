4d10a3379155f1e037713acea753f5c99194314e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 12:10:09 +0000

    Bug 1543068 - P2 Take COEP into account in CORP check r=nika
    Implement https://mikewest.github.io/corpp/#corp-check
    
    Differential Revision: https://phabricator.services.mozilla.com/D38789
    
    UltraBlame original commit: 0833c215f847cabfd9c2762e3eda522985aaae53

--- netwerk/protocol/http/nsHttpChannel.cpp
+++ netwerk/protocol/http/nsHttpChannel.cpp
@@ -7552,6 +7552,7 @@ nsresult nsHttpChannel::ProcessCrossOriginResourcePolicyHeader() {
     return NS_OK;
   }
 
+  
   uint32_t corsMode;
   MOZ_ALWAYS_SUCCEEDS(GetCorsMode(&corsMode));
   if (corsMode != nsIHttpChannelInternal::CORS_MODE_NO_CORS) {
@@ -7574,6 +7575,20 @@ nsresult nsHttpChannel::ProcessCrossOriginResourcePolicyHeader() {
   Unused << mResponseHead->GetHeader(nsHttp::Cross_Origin_Resource_Policy,
                                      content);
 
+  
+  
+  if (StaticPrefs::browser_tabs_remote_useCrossOriginEmbedderPolicy()) {
+    RefPtr<mozilla::dom::BrowsingContext> ctx;
+    mLoadInfo->GetBrowsingContext(getter_AddRefs(ctx));
+
+    
+    
+    if ((content.IsEmpty() && ctx &&
+        ctx->GetEmbedderPolicy() == nsILoadInfo::EMBEDDER_POLICY_REQUIRE_CORP)) {
+      content = NS_LITERAL_CSTRING("same-origin");
+    }
+  }
+
   if (content.IsEmpty()) {
     return NS_OK;
   }
@@ -7582,6 +7597,7 @@ nsresult nsHttpChannel::ProcessCrossOriginResourcePolicyHeader() {
   nsContentUtils::GetSecurityManager()->GetChannelResultPrincipal(
       this, getter_AddRefs(channelOrigin));
 
+  
   
   if (content.EqualsLiteral("same-origin")) {
     if (!channelOrigin->Equals(mLoadInfo->LoadingPrincipal())) {
