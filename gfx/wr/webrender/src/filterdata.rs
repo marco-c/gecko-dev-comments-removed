954a3d31404a2da54c1875d499ba18e524421c14
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 04:23:22 +0000

    Bug 1555483 - Part 3: Add SVG render task and shader r=gw
    The SVG task supports the pre-existing SVG filters along with Blend.
    
    Differential Revision: https://phabricator.services.mozilla.com/D34091
    
    UltraBlame original commit: 5618213e2196109b3455de9003507d5cdcc79c7b

--- gfx/wr/webrender/src/filterdata.rs
+++ gfx/wr/webrender/src/filterdata.rs
@@ -122,6 +122,23 @@ impl From<SFilterDataKey> for SFilterDataTemplate {
     }
 }
 
+impl SFilterData {
+    pub fn is_identity(&self) -> bool {
+        self.r_func == SFilterDataComponent::Identity
+            && self.g_func == SFilterDataComponent::Identity
+            && self.b_func == SFilterDataComponent::Identity
+            && self.a_func == SFilterDataComponent::Identity
+    }
+
+    pub fn update(&self, mut request: GpuDataRequest) {
+        push_component_transfer_data(&self.r_func, &mut request);
+        push_component_transfer_data(&self.g_func, &mut request);
+        push_component_transfer_data(&self.b_func, &mut request);
+        push_component_transfer_data(&self.a_func, &mut request);
+        assert!(!self.is_identity());
+    }
+}
+
 impl SFilterDataTemplate {
     
     
@@ -131,15 +148,8 @@ impl SFilterDataTemplate {
         &mut self,
         frame_state: &mut FrameBuildingState,
     ) {
-        if let Some(mut request) = frame_state.gpu_cache.request(&mut self.gpu_cache_handle) {
-            push_component_transfer_data(&self.data.r_func, &mut request);
-            push_component_transfer_data(&self.data.g_func, &mut request);
-            push_component_transfer_data(&self.data.b_func, &mut request);
-            push_component_transfer_data(&self.data.a_func, &mut request);
-            assert!(self.data.r_func != SFilterDataComponent::Identity
-                 || self.data.g_func != SFilterDataComponent::Identity
-                 || self.data.b_func != SFilterDataComponent::Identity
-                 || self.data.a_func != SFilterDataComponent::Identity);
+        if let Some(request) = frame_state.gpu_cache.request(&mut self.gpu_cache_handle) {
+            self.data.update(request);
         }
     }
 }
