fdf0ad3569a876ec6f68ce8c118e51fc54c73e5e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 22:47:33 +0000

    Backed out changeset b2026c2b563f (bug 1594529) for Browser-chrome failures in workspace/build/src/docshell/base/nsDocShell. CLOSED TREE
    UltraBlame original commit: 1138923577823fbae40675932f07cd1a79730db1

--- docshell/base/nsDocShell.cpp
+++ docshell/base/nsDocShell.cpp
@@ -10127,12 +10127,6 @@ nsresult nsDocShell::DoURILoad(nsDocShellLoadState* aLoadState,
     securityFlags |= nsILoadInfo::SEC_SANDBOXED;
   }
 
-  
-  MOZ_ASSERT_IF(contentPolicyType == nsIContentPolicy::TYPE_DOCUMENT,
-                !mBrowsingContext->GetParent());
-  
-  MOZ_ASSERT_IF(contentPolicyType == nsIContentPolicy::TYPE_SUBDOCUMENT,
-                mBrowsingContext->GetParent());
   RefPtr<LoadInfo> loadInfo =
       (contentPolicyType == nsIContentPolicy::TYPE_DOCUMENT)
           ? new LoadInfo(loadingWindow, aLoadState->TriggeringPrincipal(),
