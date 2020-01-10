c8705e44575bc8e755ae7a6ee5b8e90456564514
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 05:15:13 +0000

    Bug 1562881 - Part 4: Showing the degraded UI for Http pages by default. r=johannh
    Differential Revision: https://phabricator.services.mozilla.com/D37790
    
    UltraBlame original commit: 1228703fef68f40d31e79c4eece5304426262c37

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1359,13 +1359,9 @@ pref("security.insecure_password.ui.enabled", true);
 pref("security.insecure_field_warning.contextual.enabled", true);
 
 
-pref("security.insecure_connection_icon.enabled", false);
+pref("security.insecure_connection_icon.enabled", true);
 
-#if defined(NIGHTLY_BUILD)
 pref("security.insecure_connection_icon.pbmode.enabled", true);
-#else
-pref("security.insecure_connection_icon.pbmode.enabled", false);
-#endif
 
 
 pref("security.insecure_connection_text.enabled", false);
