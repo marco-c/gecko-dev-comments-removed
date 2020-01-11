66063ae7faa237d5fc783b9075c35df0e5c1ac00
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-11 02:25:24 +0000

    Bug 1595800 - Part 4: Update RDM to use setRDMPaneOrientation from BrowsingContext r=bradwerth
    Depends on D55002
    
    Differential Revision: https://phabricator.services.mozilla.com/D55017
    
    UltraBlame original commit: ef1e1295c57862200685a3dcfd3c93fa8180cd1f

--- devtools/client/responsive/ui.js
+++ devtools/client/responsive/ui.js
@@ -304,6 +304,9 @@ class ResponsiveUI {
     
     if (!isTabContentDestroying) {
       await this.inited;
+
+      
+      await this.updateScreenOrientation("landscape-primary", 0);
     }
 
     if (this.isBrowserUIEnabled) {
