bd87b65b2d6abaf50c3d12abaa7db50934c578c2
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-11 04:26:04 +0000

    Bug 1604001 - Add the missing process type of the fork server for tests: fix flake8 linting issue. a=lint-fix CLOSED TREE
    UltraBlame original commit: 8e1b11b00157269f1f781753efc241e005efeaf1

--- testing/mozbase/mozleak/mozleak/leaklog.py
+++ testing/mozbase/mozleak/mozleak/leaklog.py
@@ -164,7 +164,17 @@ def process_leak_log(leak_log_file, leak_thresholds=None,
 
     
     
-    knownProcessTypes = ["default", "forkserver", "plugin", "tab", "gmplugin", "gpu", "rdd", "vr", "socket"]
+    knownProcessTypes = [
+                         "default",
+                         "forkserver",
+                         "gmplugin",
+                         "gpu",
+                         "plugin",
+                         "rdd",
+                         "socket",
+                         "tab",
+                         "vr",
+                        ]
 
     for processType in knownProcessTypes:
         log.info("TEST-INFO | leakcheck | %s process: leak threshold set at %d bytes"
