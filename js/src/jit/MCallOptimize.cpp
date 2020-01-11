37fe0b6875b24ecb5dc7218fb6bcccd290ae26a7
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-11 03:11:36 +0000

    Bug 1602720 part 3 - Rename initializeArrayElement to initArrayElementFastPath. r=tcampbell
    Depends on D56531
    
    Differential Revision: https://phabricator.services.mozilla.com/D56532
    
    UltraBlame original commit: 54f4fc55ea0d3fd98eaaf45f26357591d57fea7b

--- js/src/jit/MCallOptimize.cpp
+++ js/src/jit/MCallOptimize.cpp
@@ -817,8 +817,8 @@ IonBuilder::InliningResult IonBuilder::inlineArray(CallInfo& callInfo,
       MConstant* id = MConstant::New(alloc(), Int32Value(i));
       current->add(id);
 
-      MOZ_TRY(initializeArrayElement(array, id, value,
-                                      false));
+      MOZ_TRY(initArrayElementFastPath(array, id, value,
+                                        false));
     }
 
     MInstruction* setLength = setInitializedLength(array, initLength);
