60d506463a5729163145884d098bf35957321dae
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-11 03:05:05 +0000

    Bug 1598629 - Part 1: Debug workers within the new threads panel in the debugger r=bhackett,jdescottes
    Differential Revision: https://phabricator.services.mozilla.com/D54746
    
    UltraBlame original commit: a23e6cc88a9c2b27f83f1f28555b97fa6d4af90e

--- devtools/client/application/test/browser/head.js
+++ devtools/client/application/test/browser/head.js
@@ -26,6 +26,9 @@ async function enableServiceWorkerDebugging() {
   await pushPref("dom.ipc.processCount", 1);
 
   
+  await pushPref("devtools.debugger.features.windowless-service-workers", true);
+
+  
   Services.ppmm.releaseCachedProcesses();
 }
 
