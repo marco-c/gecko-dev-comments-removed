46ec7b51fa7adacc35e1c6e4a5002b2d70e6d143
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-11 03:48:28 +0000

    Bug 1594931 - Stop compiling NSS' DBM legacy database r=kjacobs,keeler,mhowell,MattN
    This change modifies all tests that use key3/cert8 to use the new files. It
    removes test_sdr_upgraded_with_password, as without the upgrade part that is now
    the same test as test_sdr_preexisting_with_password.
    
    Differential Revision: https://phabricator.services.mozilla.com/D55708
    
    UltraBlame original commit: 5c137b3793a799caec1ee15ad5aa3d0da1706123

--- toolkit/components/passwordmgr/test/unit/head.js
+++ toolkit/components/passwordmgr/test/unit/head.js
@@ -80,8 +80,7 @@ add_task(async function test_common_initialize() {
   
   
   
-  const isAndroid = AppConstants.platform == "android";
-  const keyDBName = isAndroid ? "key4.db" : "key3.db";
+  const keyDBName = "key4.db";
   await OS.File.copy(
     do_get_file(`data/${keyDBName}`).path,
     OS.Path.join(OS.Constants.Path.profileDir, keyDBName)
