7fe3c74885f0897c1b9741e09b80f34a54841c9a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-11 05:21:06 +0000

    Bug 1598998 - Fix CanvasContextType::ImageBitmap handling r=nical
    Differential Revision: https://phabricator.services.mozilla.com/D56348
    
    UltraBlame original commit: 67142a370d4ad734db1cc0acfb986b4b8966ce8c

--- gfx/layers/AsyncCanvasRenderer.h
+++ gfx/layers/AsyncCanvasRenderer.h
@@ -8,6 +8,7 @@
 #define MOZILLA_LAYERS_ASYNCCANVASRENDERER_H_
 
 #include "LayersTypes.h"
+#include "mozilla/dom/CanvasRenderingContextHelper.h"
 #include "mozilla/gfx/Point.h"  
 #include "mozilla/Mutex.h"
 #include "nsCOMPtr.h"  
@@ -34,6 +35,7 @@ namespace layers {
 
 class CanvasClient;
 class TextureClient;
+class ImageContainer;
 
 
 
@@ -102,6 +104,11 @@ class AsyncCanvasRenderer final {
 
   already_AddRefed<nsISerialEventTarget> GetActiveEventTarget();
 
+  ImageContainer* GetImageContainer();
+
+  dom::CanvasContextType GetContextType();
+  void SetContextType(dom::CanvasContextType aContextType);
+
   
   
   dom::HTMLCanvasElement* mHTMLCanvasElement;
@@ -144,6 +151,12 @@ class AsyncCanvasRenderer final {
 
   
   nsCOMPtr<nsISerialEventTarget> mActiveEventTarget;
+
+  
+  RefPtr<ImageContainer> mImageContainer;
+
+  
+  dom::CanvasContextType mContextType;
 };
 
 }  
