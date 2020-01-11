46df75cf4106cbea9932ad37855e5a7946c1f49e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-11 04:23:33 +0000

    Bug 1600267: part 3) Call `ComparePoints` instead of `ComparePoints_Deprecated` in `Abstractrange::SetStartAndEndInternal`. r=smaug
    Differential Revision: https://phabricator.services.mozilla.com/D55297
    
    UltraBlame original commit: 4459ef04ad8e613882e34aca76bd80b3fdab4659

--- dom/base/AbstractRange.cpp
+++ dom/base/AbstractRange.cpp
@@ -7,6 +7,8 @@
 #include "mozilla/dom/AbstractRange.h"
 #include "mozilla/dom/AbstractRangeBinding.h"
 
+#include "mozilla/Assertions.h"
+#include "mozilla/Attributes.h"
 #include "mozilla/RangeUtils.h"
 #include "mozilla/dom/StaticRange.h"
 #include "nsContentUtils.h"
@@ -132,10 +134,17 @@ nsresult AbstractRange::SetStartAndEndInternal(
     return NS_OK;
   }
 
+  const Maybe<int32_t> pointOrder =
+      nsContentUtils::ComparePoints(aStartBoundary, aEndBoundary);
+  if (!pointOrder) {
+    
+    MOZ_ASSERT_UNREACHABLE();
+    return NS_ERROR_INVALID_ARG;
+  }
+
   
   
-  if (nsContentUtils::ComparePoints_Deprecated(aStartBoundary, aEndBoundary) ==
-      1) {
+  if (*pointOrder == 1) {
     aRange->DoSetRange(aEndBoundary, aEndBoundary, newEndRoot);
     return NS_OK;
   }
