c1c59ee09ad408ad04c1b0ac267a7f11a24db66b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-11 05:25:26 +0000

    Bug 1530372 - Part 1: Change BigInt to an in-line trace kind. r=sfink
    BigInts need to use `0x01` as their TraceKind value, but that value is
    currently already used for Scripts. Therefore move `Script` to an
    out-of-line trace kind to free up `0x01` for BigInts.
    
    Differential Revision: https://phabricator.services.mozilla.com/D56198
    
    UltraBlame original commit: 3b2680319bf7071264d8d2e4494c45f75036a2d0

--- js/public/TraceKind.h
+++ js/public/TraceKind.h
@@ -40,11 +40,9 @@ enum class TraceKind {
   
   
   Object = 0x00,
+  BigInt = 0x01,
   String = 0x02,
   Symbol = 0x03,
-
-  
-  Script = 0x01,
 
   
   Shape = 0x04,
@@ -59,10 +57,10 @@ enum class TraceKind {
   
   BaseShape = 0x0F,
   JitCode = 0x1F,
-  LazyScript = 0x2F,
-  Scope = 0x3F,
-  RegExpShared = 0x4F,
-  BigInt = 0x5F
+  Script = 0x2F,
+  LazyScript = 0x3F,
+  Scope = 0x4F,
+  RegExpShared = 0x5F
 };
 const static uintptr_t OutOfLineTraceKindMask = 0x07;
 
