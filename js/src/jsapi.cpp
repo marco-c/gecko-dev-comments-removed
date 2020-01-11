461d226350b73bea5561801ae99dabdf32caee65
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-11 01:04:45 +0000

    Bug 1590907 - Determine 64-bit in public JS header without configure macro. r=froydnj,sfink
    If only relying on JS_64BIT to determine whether the system is 64-bits,
    then the result will be incorrect when the header is installed as a
    public header for use by embedders, and since JS_BITS_PER_WORD affects
    the layout of structs in header files, things will go badly wrong.
    
    This uses two other ways of determining pointer width, hopefully
    cross-platform enough. __SIZEOF_POINTER__ is a GCC-ism and probably
    works in Clang as well. UINTPTR_MAX is hopefully sufficiently
    cross-platform as a last resort.
    
    Differential Revision: https://phabricator.services.mozilla.com/D52458
    
    UltraBlame original commit: 1fcd2dd277663e7dfbde5fc4c7cc3ff7c1ba5a3b

--- js/src/jsapi.cpp
+++ js/src/jsapi.cpp
@@ -124,6 +124,14 @@ using JS::SourceText;
 #  define JS_ADDRESSOF_VA_LIST(ap) (&(ap))
 #endif
 
+
+
+#ifdef JS_64BIT
+static_assert(JS_BITS_PER_WORD == 64, "values must be in sync");
+#else
+static_assert(JS_BITS_PER_WORD == 32, "values must be in sync");
+#endif
+
 JS_PUBLIC_API void JS::CallArgs::reportMoreArgsNeeded(JSContext* cx,
                                                       const char* fnname,
                                                       unsigned required,
