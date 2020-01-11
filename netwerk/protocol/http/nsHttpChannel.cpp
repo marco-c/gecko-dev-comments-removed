aca5ccfee0a6f9aae6723245cd42897cbe6476ef
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-11 02:05:52 +0000

    Bug 1596409 - P2: Seperate nsHttpConnectionMgr and AltSvcCache r=dragana
    Differential Revision: https://phabricator.services.mozilla.com/D54161
    
    UltraBlame original commit: 16635b64a0a2be2ec0d3f04c7392002cb987a2c4

--- netwerk/protocol/http/nsHttpChannel.cpp
+++ netwerk/protocol/http/nsHttpChannel.cpp
@@ -6733,7 +6733,7 @@ nsresult nsHttpChannel::BeginConnect() {
   if (mLoadFlags & LOAD_FRESH_CONNECTION) {
     
     if (mLoadFlags & LOAD_INITIAL_DOCUMENT_URI) {
-      gHttpHandler->ConnMgr()->ClearAltServiceMappings();
+      gHttpHandler->AltServiceCache()->ClearAltServiceMappings();
       rv = gHttpHandler->ConnMgr()->DoShiftReloadConnectionCleanup(
           mConnectionInfo);
       if (NS_FAILED(rv)) {
--- netwerk/protocol/http/nsHttpTransaction.cpp
+++ netwerk/protocol/http/nsHttpTransaction.cpp
@@ -1708,7 +1708,7 @@ nsresult nsHttpTransaction::HandleContentStart() {
         break;
       case 421:
         LOG(("Misdirected Request.\n"));
-        gHttpHandler->ConnMgr()->ClearHostMapping(mConnInfo);
+        gHttpHandler->AltServiceCache()->ClearHostMapping(mConnInfo);
 
         
         if (!mRestartCount) {
