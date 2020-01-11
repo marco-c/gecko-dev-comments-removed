73a10f2b6184e1e09ae8ffbc627e5e2efbee0b70
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-11 01:01:25 +0000

    Bug 1598516 - Expose a RemoteWindowContext on ParentChannelListener. r=mayhemer,kmag
    Without DocumentChannel, nsExtProtocolChannel::OpenURL calls into nsExternalHelperAppService::LoadURI in the content process.
    We then manually forward this to the parent process over PContent, create a RemoteWindowContext around the browser parent, and then call LoadURI again.
    
    With DocumemntChannel, the nsExtProtocolChannel already lives in the parent, so we just need to provide a RemoteWindowContext directly (that the code accesses via GetInterface on the callbacks).
    
    Differential Revision: https://phabricator.services.mozilla.com/D54247
    
    UltraBlame original commit: 6ba74ff735ba034ec37f9187e8c9063322f48111

--- netwerk/ipc/DocumentLoadListener.cpp
+++ netwerk/ipc/DocumentLoadListener.cpp
@@ -626,6 +626,19 @@ DocumentLoadListener::OnStartRequest(nsIRequest* aRequest) {
     return NS_OK;
   }
 
+  
+  
+  
+  
+  
+  
+  nsresult status = NS_OK;
+  aRequest->GetStatus(&status);
+  if (status == NS_ERROR_NO_CONTENT) {
+    mDocumentChannelBridge->DisconnectChildListeners(NS_ERROR_NO_CONTENT);
+    return NS_OK;
+  }
+
   mChannel->Suspend();
   mSuspendedChannel = true;
 
