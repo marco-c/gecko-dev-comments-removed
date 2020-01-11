e89ec5f7055cb8d44386a482d27f03be6b2b5a16
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-11 00:13:44 +0000

    Bug 1597911 - Enable MOZ_RELEASE_ASSERT in DCLayerTree::CreateSurface() r=gw
    Differential Revision: https://phabricator.services.mozilla.com/D53929
    
    UltraBlame original commit: a13e231354216fee09a60eec3c816b2ddd32d4b0

--- gfx/webrender_bindings/DCLayerTree.cpp
+++ gfx/webrender_bindings/DCLayerTree.cpp
@@ -228,8 +228,7 @@ void DCLayerTree::Unbind() {
 void DCLayerTree::CreateSurface(wr::NativeSurfaceId aId,
                                 wr::DeviceIntSize aSize, bool aIsOpaque) {
   auto it = mDCLayers.find(wr::AsUint64(aId));
-  
-  
+  MOZ_RELEASE_ASSERT(it == mDCLayers.end());
   MOZ_ASSERT(it == mDCLayers.end());
   if (it != mDCLayers.end()) {
     
