9ee352463be1f696c430ecbde81bb94c456d6267
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-11 01:00:44 +0000

    Bug 1598787 - Rename XRE_ChildProcessTypeToString to XRE_GeckoProcessTypeToString. r=froydnj
    This function works on all GeckoProcessTypes, not just those for child
    processes.
    
    Differential Revision: https://phabricator.services.mozilla.com/D54375
    
    UltraBlame original commit: d8525c50715e43e83fa584728313beea7e5a159d

--- gfx/ipc/GPUChild.cpp
+++ gfx/ipc/GPUChild.cpp
@@ -233,7 +233,7 @@ void GPUChild::ActorDestroy(ActorDestroyReason aWhy) {
 
     Telemetry::Accumulate(
         Telemetry::SUBPROCESS_ABNORMAL_ABORT,
-        nsDependentCString(XRE_ChildProcessTypeToString(GeckoProcessType_GPU)),
+        nsDependentCString(XRE_GeckoProcessTypeToString(GeckoProcessType_GPU)),
         1);
 
     
