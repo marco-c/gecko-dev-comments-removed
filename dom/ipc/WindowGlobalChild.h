dcfa0c9251101bbbfd52c2a76395967457fddc1a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-11 02:48:53 +0000

    Bug 1588839 - Part 3: Preserve cross-process async stacks in sendQuery/sendAsyncMessage calls. r=bzbarsky
    This makes it much simpler to track down the source of message handler errors
    by linking the cross-process caller chains which sent the problematic message.
    
    Differential Revision: https://phabricator.services.mozilla.com/D50883
    
    UltraBlame original commit: aa4e0207b45fbab40ab0d310b67630c6aced6859

--- dom/ipc/WindowGlobalChild.h
+++ dom/ipc/WindowGlobalChild.h
@@ -85,7 +85,8 @@ class WindowGlobalChild final : public WindowGlobalActor,
   already_AddRefed<BrowserChild> GetBrowserChild();
 
   void ReceiveRawMessage(const JSWindowActorMessageMeta& aMeta,
-                         ipc::StructuredCloneData&& aData);
+                         ipc::StructuredCloneData&& aData,
+                         ipc::StructuredCloneData&& aStack);
 
   
   already_AddRefed<JSWindowActorChild> GetActor(const nsAString& aName,
@@ -112,7 +113,8 @@ class WindowGlobalChild final : public WindowGlobalActor,
 
   
   mozilla::ipc::IPCResult RecvRawMessage(const JSWindowActorMessageMeta& aMeta,
-                                         const ClonedMessageData& aData);
+                                         const ClonedMessageData& aData,
+                                         const ClonedMessageData& aStack);
 
   mozilla::ipc::IPCResult RecvLoadURIInChild(nsDocShellLoadState* aLoadState,
                                              bool aSetNavigating);
