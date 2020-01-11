37c0ac90df77fb9416215f59daaee665e8b77d5c
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-11 03:01:58 +0000

    Backed out changeset 2968e233f172 (bug 1600929) for hazzard bustages on Device.cpp. CLOSED TREE
    UltraBlame original commit: 30a9d0d9104a44838e490e6f6485256fa288dddd

--- dom/webgpu/Buffer.h
+++ dom/webgpu/Buffer.h
@@ -6,15 +6,11 @@
 #ifndef GPU_BUFFER_H_
 #define GPU_BUFFER_H_
 
-#include "js/RootingAPI.h"
 #include "mozilla/dom/Nullable.h"
-#include "mozilla/webgpu/WebGPUTypes.h"
+#include "mozilla/dom/TypedArray.h"
 #include "ObjectModel.h"
 
 namespace mozilla {
-namespace ipc {
-class Shmem;
-}  
 namespace webgpu {
 
 class Device;
@@ -24,30 +20,11 @@ class Buffer final : public ObjectBase, public ChildOf<Device> {
   GPU_DECL_CYCLE_COLLECTION(Buffer)
   GPU_DECL_JS_WRAP(Buffer)
 
-  struct Mapping final {
-    UniquePtr<ipc::Shmem> mShmem;
-    JS::Heap<JSObject*> mArrayBuffer;
-
-    Mapping(ipc::Shmem&& aShmem, JSObject* aArrayBuffer);
-  };
-
-  Buffer(Device* const aParent, RawId aId, BufferAddress aSize);
-  void InitMapping(ipc::Shmem&& aShmem, JSObject* aArrayBuffer);
-
  private:
+  explicit Buffer(Device* parent);
   virtual ~Buffer();
 
-  const RawId mId;
-  
-  
-  
-  const BufferAddress mSize;
-  nsString mLabel;
-  Maybe<Mapping> mMapping;
-
  public:
-  already_AddRefed<dom::Promise> MapReadAsync(ErrorResult& aRv);
-  void Unmap(JSContext* aCx, ErrorResult& aRv);
 };
 
 }  
