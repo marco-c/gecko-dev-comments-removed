6503d42f1f5eea32dccc24eedb5f28057ce9195f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-11 00:48:26 +0000

    Bug 1570549 - P8 - Restore SDP comparison telemetry;r=bwc
    This moves the telemetry and preferences out into their own files and restores the parse result telemetery.
    
    Differential Revision: https://phabricator.services.mozilla.com/D52749
    
    UltraBlame original commit: 0574444bda4879ee8a9c76389b7f496485e248f9

--- media/webrtc/signaling/src/sdp/HybridSdpParser.cpp
+++ media/webrtc/signaling/src/sdp/HybridSdpParser.cpp
@@ -5,124 +5,21 @@
 
 #include "signaling/src/sdp/HybridSdpParser.h"
 #include "signaling/src/sdp/SdpLog.h"
+#include "signaling/src/sdp/SdpPref.h"
+#include "signaling/src/sdp/SdpTelemetry.h"
 #include "signaling/src/sdp/SipccSdpParser.h"
 #include "signaling/src/sdp/RsdparsaSdpParser.h"
 #include "signaling/src/sdp/ParsingResultComparer.h"
 
 #include "mozilla/Logging.h"
 #include "mozilla/Preferences.h"
+#include "mozilla/Telemetry.h"
 
 #include <unordered_map>
 
 namespace mozilla {
 
 using mozilla::LogLevel;
-
-
-class SdpPref {
- private:
-  static const std::string PRIMARY_PREF;
-  static const std::string ALTERNATE_PREF;
-  static const std::string DEFAULT;
-
-  
-  enum class Parsers {
-    Sipcc,
-    WebRtcSdp,
-  };
-
-  
-  enum class AlternateParseModes {
-    Parallel,  
-               
-    Failover,  
-    Never,     
-  };
-
-  
-  
-  template <class T>
-  static auto Pref(const std::string& aPrefName,
-                   const std::unordered_map<std::string, T>& aMap) -> T {
-    MOZ_ASSERT(aMap.find(DEFAULT) != aMap.end());
-
-    nsCString value;
-    if (NS_FAILED(Preferences::GetCString(aPrefName.c_str(), value))) {
-      return aMap.at(DEFAULT);
-    }
-    const auto found = aMap.find(value.get());
-    if (found != aMap.end()) {
-      return found->second;
-    }
-    return aMap.at(DEFAULT);
-  }
-
-  
-  static auto Parser() -> Parsers {
-    static const auto values = std::unordered_map<std::string, Parsers>{
-        {"legacy", Parsers::Sipcc},
-        {"webrtc-sdp", Parsers::WebRtcSdp},
-        {DEFAULT, Parsers::Sipcc},
-    };
-    return Pref(PRIMARY_PREF, values);
-  }
-
-  
-  static auto AlternateParseMode() -> AlternateParseModes {
-    static const auto values =
-        std::unordered_map<std::string, AlternateParseModes>{
-            {"parallel", AlternateParseModes::Parallel},
-            {"failover", AlternateParseModes::Failover},
-            {"never", AlternateParseModes::Never},
-            {DEFAULT, AlternateParseModes::Parallel},
-        };
-    return Pref(ALTERNATE_PREF, values);
-  }
-
- public:
-  
-  
-  
-
-  
-  static auto Primary() -> UniquePtr<SdpParser> {
-    switch (Parser()) {
-      case Parsers::Sipcc:
-        return UniquePtr<SdpParser>(new SipccSdpParser());
-      case Parsers::WebRtcSdp:
-        return UniquePtr<SdpParser>(new RsdparsaSdpParser());
-    }
-  }
-
-  static auto Secondary() -> Maybe<UniquePtr<SdpParser>> {
-    if (AlternateParseMode() != AlternateParseModes::Parallel) {
-      return Nothing();
-    }
-    switch (Parser()) {  
-      case Parsers::Sipcc:
-        return Some(UniquePtr<SdpParser>(new RsdparsaSdpParser()));
-      case Parsers::WebRtcSdp:
-        return Some(UniquePtr<SdpParser>(new SipccSdpParser()));
-    }
-  }
-
-  static auto Failover() -> Maybe<UniquePtr<SdpParser>> {
-    if (AlternateParseMode() != AlternateParseModes::Failover) {
-      return Nothing();
-    }
-    switch (Parser()) {
-      case Parsers::Sipcc:
-        return Some(UniquePtr<SdpParser>(new RsdparsaSdpParser()));
-      case Parsers::WebRtcSdp:
-        return Some(UniquePtr<SdpParser>(new SipccSdpParser()));
-    }
-  }
-};
-
-const std::string SdpPref::PRIMARY_PREF = "media.peerconnection.sdp.parser";
-const std::string SdpPref::ALTERNATE_PREF =
-    "media.peerconnection.sdp.alternate_parse_mode";
-const std::string SdpPref::DEFAULT = "default";
 
 HybridSdpParser::HybridSdpParser()
     : mPrimary(SdpPref::Primary()),
@@ -145,22 +42,33 @@ HybridSdpParser::HybridSdpParser()
 auto HybridSdpParser::Parse(const std::string& aText)
     -> UniquePtr<SdpParser::Results> {
   using Results = UniquePtr<SdpParser::Results>;
+  using Role = SdpTelemetry::Roles;
+  using Mode = SdpPref::AlternateParseModes;
+
+  Mode mode = Mode::Never;
   auto results = mPrimary->Parse(aText);
+
   
   
-  auto compare = [&results, &aText](Results&& aResB) -> Results {
-    ParsingResultComparer::Compare(results, aResB, aText);
+  auto compare = [&](Results&& aResB) -> Results {
+    SdpTelemetry::RecordParse(aResB, mode, Role::Secondary);
+    ParsingResultComparer::Compare(results, aResB, aText, mode);
     return std::move(results->Ok() ? results : aResB);
   };
   
-  mSecondary.apply(
-      [&](auto& sec) { results = compare(std::move(sec->Parse(aText))); });
+  mSecondary.apply([&](auto& sec) {
+    mode = Mode::Parallel;
+    results = compare(std::move(sec->Parse(aText)));
+  });
   
   mFailover.apply([&](auto& failover) {  
+    mode = Mode::Failover;
     if (!results->Ok()) {
       results = compare(std::move(failover->Parse(aText)));
     }
   });
+
+  SdpTelemetry::RecordParse(results, mode, Role::Primary);
   return results;
 }
 
