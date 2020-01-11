6f2ee6b73126269fbd665af3fa6933730cca7569
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-11 05:10:30 +0000

    Bug 1603856 Part 1 - Specify crashing task explicitly when requesting minidumps, r=gsvelto.
    Differential Revision: https://phabricator.services.mozilla.com/D57200
    
    UltraBlame original commit: 46ec9d57ca2012e8a78c94d7d3191f451cea12df

--- toolkit/crashreporter/breakpad-client/mac/crash_generation/crash_generation_client.cc
+++ toolkit/crashreporter/breakpad-client/mac/crash_generation/crash_generation_client.cc
@@ -40,13 +40,14 @@ bool CrashGenerationClient::RequestDumpForException(
     int exception_type,
     int exception_code,
     int exception_subcode,
-    mach_port_t crashing_thread) {
+    mach_port_t crashing_thread,
+    mach_port_t crashing_task) {
   
   
   ReceivePort acknowledge_port;
 
   MachSendMessage message(kDumpRequestMessage);
-  message.AddDescriptor(mach_task_self());            
+  message.AddDescriptor(crashing_task);               
   message.AddDescriptor(crashing_thread);             
   message.AddDescriptor(MACH_PORT_NULL);              
   message.AddDescriptor(acknowledge_port.GetPort());  
