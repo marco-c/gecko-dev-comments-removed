13070d574216286653c79e7b03418948b7ee1605
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-11 01:57:00 +0000

    Bug 1599263 - make the test_intermediate_preloads.js test collect telemetry data for all products during testing. r=jcj
    Differential Revision: https://phabricator.services.mozilla.com/D55246
    
    UltraBlame original commit: af6b936aa160b9a173069b91794d86f2d8397a8f

--- security/manager/ssl/tests/unit/test_intermediate_preloads.js
+++ security/manager/ssl/tests/unit/test_intermediate_preloads.js
@@ -217,6 +217,13 @@ add_task(
     skip_if: () => !AppConstants.MOZ_NEW_CERT_STORAGE,
   },
   async function test_preload_invalid_hash() {
+    
+    
+    Services.prefs.setBoolPref(
+      "toolkit.telemetry.testing.overrideProductsCheck",
+      true
+    );
+
     Services.prefs.setBoolPref(INTERMEDIATES_ENABLED_PREF, true);
     const invalidHash =
       "6e340b9cffb37a989ca544e6bb780a2c78901d3fb33738768511a30617afa01d";
