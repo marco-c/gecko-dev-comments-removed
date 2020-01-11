49f3f3128defacf952f92f07ed63a3600e96ad8a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-11 05:26:31 +0000

    Bug 1606827 - A corner is not rounded when the radius is zero in either dimension (not necessarily both) r=nical
    Differential Revision: https://phabricator.services.mozilla.com/D58615
    
    UltraBlame original commit: 5105e62fbbe6cb144a7a7a756128da326d724004

--- gfx/wr/webrender_api/src/display_item.rs
+++ gfx/wr/webrender_api/src/display_item.rs
@@ -1302,12 +1302,14 @@ impl BorderRadius {
         }
     }
 
+    
+    
     pub fn is_zero(&self) -> bool {
-        if let Some(radius) = self.is_uniform() {
-            radius == 0.0
-        } else {
-            false
-        }
+        let corner_is_zero = |corner: &LayoutSize| corner.width == 0.0 || corner.height == 0.0;
+        corner_is_zero(&self.top_left) &&
+        corner_is_zero(&self.top_right) &&
+        corner_is_zero(&self.bottom_right) &&
+        corner_is_zero(&self.bottom_left)
     }
 }
 
