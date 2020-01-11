9a0f7df27f770c27b3df1188f33b32016be68dde
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-11 00:03:59 +0000

    Backed out changeset a3a7133456b4 (bug 1589198) for reftest at layout/reftests/svg/filters/filter-nested-filtering-02.svg. On a CLOSED TREE
    UltraBlame original commit: 981d7988a2797e13e84ab57859e4e277ea46962b

--- gfx/wr/webrender/src/picture.rs
+++ gfx/wr/webrender/src/picture.rs
@@ -4282,8 +4282,6 @@ impl PicturePrimitive {
                 
                 
                 
-                
-                
                 let snap_surface_to_raster = SpaceSnapper::new_with_target(
                     surface.raster_spatial_node_index,
                     self.spatial_node_index,
