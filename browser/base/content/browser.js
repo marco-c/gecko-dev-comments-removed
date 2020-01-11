6c46a8efee98eb5b44f3797db1e5aa001020e27e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-11 03:11:59 +0000

    Bug 1601450 - Further restrict the retained results behavior. r=dao
    If the user types a full url, confirms it, and then focuses the urlbar, we should
    not
    
    When focusing the address bar after typing and confirming a page, we end up reopening
    the view because the search string is the same. This causes unexpected noise and
    selection problems (the url is not selected anymore).
    This patch changes the behavior so we don't reopen on valid pageproxystate, that
    seems to make sense considering the feature scope of restoring an abandoned search.
    
    Additionally, make the keyboard shortcut not trying to reopen the view if it's
    already open, to avoid messing up with the selection.
    
    Finally, restore the appropriate allowAutofill status from the last context.
    This may still cause us to not autofill something that was previously autofilled,
    if 2 tabs contain the same exact search, but that's an edge case, so acceptable.
    
    Differential Revision: https://phabricator.services.mozilla.com/D56635
    
    UltraBlame original commit: 0a45507aa14adf61f7453fb4445126389893fef0

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -2824,11 +2824,11 @@ function focusAndSelectUrlBar() {
 function openLocation(event) {
   if (window.location.href == AppConstants.BROWSER_CHROME_URL) {
     focusAndSelectUrlBar();
-    if (
-      !gURLBar.view.maybeReopen() &&
-      gURLBar.openViewOnFocusForCurrentTab &&
-      !gURLBar.view.isOpen
-    ) {
+    
+    if (gURLBar.view.isOpen) {
+      return;
+    }
+    if (!gURLBar.view.maybeReopen() && gURLBar.openViewOnFocusForCurrentTab) {
       gURLBar.startQuery({ event });
     }
     return;
