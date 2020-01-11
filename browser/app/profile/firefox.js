4bb9eb98e5a5ddcd1192b006ea7eb7d94afb9cba
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-11 04:07:44 +0000

    Backed out changeset 58075bcbdcef (bug 1588050) for dt failures on browser_aboutdebugging_process_main.js . CLOSED TREE
    UltraBlame original commit: 660a95371114ac87ed1f171513cfaa3ca4aab9c8

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1908,11 +1908,8 @@ pref("devtools.toolbox.tabsOrder", "");
 
 
 
-#if defined(NIGHTLY_BUILD)
-pref("devtools.browsertoolbox.fission", true);
-#else
+
 pref("devtools.browsertoolbox.fission", false);
-#endif
 
 
 
