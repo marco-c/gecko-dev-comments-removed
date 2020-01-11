2ea12b2d005af814e18d672e260bb06909b5f32a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-11 05:40:22 +0000

    Bug 1606794 - [marionette] Fixed WebDriver:TakeScreenshot to capture frame's content. r=marionette-reviewers,ato
    Differential Revision: https://phabricator.services.mozilla.com/D58622
    
    UltraBlame original commit: 6b5606ee256044c583ddfc076c684a59848ff385

--- testing/marionette/driver.js
+++ testing/marionette/driver.js
@@ -387,6 +387,33 @@ GeckoDriver.prototype.sendAsync = function(name, data, commandID) {
 
 
 
+GeckoDriver.prototype.getBrowsingContext = async function(topContext = false) {
+  let browsingContext = null;
+
+  switch (this.context) {
+    case Context.Chrome:
+      browsingContext = this.getCurrentWindow().docShell.browsingContext;
+      break;
+
+    case Context.Content:
+      const id = await this.listener.getBrowsingContextId(topContext);
+      browsingContext = BrowsingContext.get(id);
+      break;
+  }
+
+  return browsingContext;
+};
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -3003,13 +3030,9 @@ GeckoDriver.prototype.takeScreenshot = async function(cmd) {
   
   full = webEl ? false : full;
 
-  let browsingContext;
   let rect;
-
   switch (this.context) {
     case Context.Chrome:
-      browsingContext = win.docShell.browsingContext;
-
       if (id) {
         let el = this.curBrowser.seenEls.get(webEl, win);
         rect = el.getBoundingClientRect();
@@ -3028,10 +3051,13 @@ GeckoDriver.prototype.takeScreenshot = async function(cmd) {
       break;
 
     case Context.Content:
-      browsingContext = this.curBrowser.contentBrowser.browsingContext;
       rect = await this.listener.getScreenshotRect({ el: webEl, full, scroll });
       break;
   }
+
+  
+  
+  const browsingContext = await this.getBrowsingContext(!webEl);
 
   let canvas = await capture.canvas(
     win,
