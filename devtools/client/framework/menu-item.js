0bd4eee29ee5bf6c039e675ea497bb1b6d78ac33
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-11 02:15:33 +0000

    Bug 1306198: Show browser and os info on the device menu item. r=pbro,mtigley
    Depends on D55251
    
    Differential Revision: https://phabricator.services.mozilla.com/D55252
    
    UltraBlame original commit: c446bdb23bd1a76c5ce384100ff805110ce165e5

--- devtools/client/framework/menu-item.js
+++ devtools/client/framework/menu-item.js
@@ -56,6 +56,7 @@ function MenuItem({
   hover = () => {},
   id = null,
   label = "",
+  image = null,
   submenu = null,
   type = "normal",
   visible = true,
@@ -69,6 +70,7 @@ function MenuItem({
   this.hover = hover;
   this.id = id;
   this.label = label;
+  this.image = image;
   this.submenu = submenu;
   this.type = type;
   this.visible = visible;
