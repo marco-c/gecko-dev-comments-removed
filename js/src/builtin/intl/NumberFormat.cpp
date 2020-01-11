e587646fc15cebd4e8e82b939279eca27aaa1899
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-11 03:30:02 +0000

    Bug 1603371 - Use _GLIBCXX_RELEASE instead of __GLIBCXX__ r=froydnj
    Differential Revision: https://phabricator.services.mozilla.com/D56956
    
    UltraBlame original commit: 2ba741910e8f1c78669842b0be590dbb151b1922

--- js/src/builtin/intl/NumberFormat.cpp
+++ js/src/builtin/intl/NumberFormat.cpp
@@ -402,7 +402,7 @@ static const MeasureUnit& FindSimpleMeasureUnit(const char* subtype) {
 
 static constexpr size_t MaxUnitLength() {
   
-#if __GLIBCXX__ > 20181026
+#if _GLIBCXX_RELEASE >= 7
   size_t length = 0;
   for (const auto& unit : simpleMeasureUnits) {
     length = std::max(length, std::char_traits<char>::length(unit.subtype));
