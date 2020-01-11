b9579154cf86c695ba6a907152e6fea67ca2f2c5
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-11 04:05:32 +0000

    Bug 1577596 - Expose autoplay permission requests in GeckoView r=geckoview-reviewers,rbarker,agi
    Differential Revision: https://phabricator.services.mozilla.com/D57134
    
    UltraBlame original commit: 1a9be6d049a78ad37c99298dfce43fba6dd45687

--- mobile/android/geckoview/src/main/java/org/mozilla/geckoview/GeckoSession.java
+++ mobile/android/geckoview/src/main/java/org/mozilla/geckoview/GeckoSession.java
@@ -774,7 +774,6 @@ public class GeckoSession implements Parcelable {
                                       final String event,
                                       final GeckoBundle message,
                                       final EventCallback callback) {
-
                 if (delegate == null) {
                     callback.sendSuccess( false);
                     return;
@@ -799,6 +798,10 @@ public class GeckoSession implements Parcelable {
                         
                         callback.sendError("Unsupported");
                         return;
+                    } else if ("autoplay-media-inaudible".equals(typeString)) {
+                        type = PermissionDelegate.PERMISSION_AUTOPLAY_INAUDIBLE;
+                    } else if ("autoplay-media-audible".equals(typeString)) {
+                        type = PermissionDelegate.PERMISSION_AUTOPLAY_AUDIBLE;
                     } else {
                         throw new IllegalArgumentException("Unknown permission request: " + typeString);
                     }
@@ -4970,6 +4973,16 @@ public class GeckoSession implements Parcelable {
         
 
 
+        int PERMISSION_AUTOPLAY_INAUDIBLE = 3;
+
+        
+
+
+        int PERMISSION_AUTOPLAY_AUDIBLE = 4;
+
+        
+
+
         interface Callback {
             
 
@@ -5217,7 +5230,10 @@ public class GeckoSession implements Parcelable {
     @Retention(RetentionPolicy.SOURCE)
     @IntDef({PermissionDelegate.PERMISSION_GEOLOCATION,
             PermissionDelegate.PERMISSION_DESKTOP_NOTIFICATION,
-            PermissionDelegate.PERMISSION_XR})
+            PermissionDelegate.PERMISSION_PERSISTENT_STORAGE,
+            PermissionDelegate.PERMISSION_XR,
+            PermissionDelegate.PERMISSION_AUTOPLAY_INAUDIBLE,
+            PermissionDelegate.PERMISSION_AUTOPLAY_AUDIBLE})
      @interface Permission {}
 
     
