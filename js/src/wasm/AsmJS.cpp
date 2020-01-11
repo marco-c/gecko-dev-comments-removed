a12bacc6e188cef233b030a844702da5dee5d33d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-11 02:40:31 +0000

    Backed out 2 changesets (bug 1598377) for build bustages at WasmCompile.cpp. CLOSED TREE
    Backed out changeset 774ad330f4f6 (bug 1598377)
    Backed out changeset f0865dabd55a (bug 1598377)
    
    UltraBlame original commit: 7c75c3077b22479e61a6e414b54fef6bb932f092

--- js/src/wasm/AsmJS.cpp
+++ js/src/wasm/AsmJS.cpp
@@ -1353,7 +1353,7 @@ class MOZ_STACK_CLASS JS_HAZ_ROOTED ModuleValidatorShared {
         compilerEnv_(CompileMode::Once, Tier::Optimized, OptimizedBackend::Ion,
                      DebugEnabled::False,  false,
                       false,  false,
-                      false,  false),
+                      false),
         env_(&compilerEnv_, Shareable::False, ModuleKind::AsmJS) {
     compilerEnv_.computeParameters( false);
     env_.minMemoryLength = RoundUpToNextValidAsmJSHeapLength(0);
