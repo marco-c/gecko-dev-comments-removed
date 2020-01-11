fa94fb57e300beab3cb08817a44c055d8999aebb
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-11 00:56:17 +0000

    Bug 1598656 - Ensure RoUninitialize is not called until after the COMPtr<>s have been released. r=emk
    Differential Revision: https://phabricator.services.mozilla.com/D54505
    
    UltraBlame original commit: b9e66763153cb3b1154718df8cc4e56ee6a29772

--- intl/locale/windows/OSPreferences_win.cpp
+++ intl/locale/windows/OSPreferences_win.cpp
@@ -1,8 +1,8 @@
-/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
- *
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+
+
+
+
 
 #include "OSPreferences.h"
 #include "mozilla/intl/LocaleService.h"
@@ -11,7 +11,7 @@
 
 #include <windows.h>
 
-#ifndef __MINGW32__  // WinRT headers not yet supported by MinGW
+#ifndef __MINGW32__  
 #  include <roapi.h>
 #  include <wrl.h>
 #  include <Windows.System.UserProfile.h>
@@ -33,14 +33,14 @@ bool OSPreferences::ReadSystemLocales(nsTArray<nsCString>& aLocaleList) {
 
 #ifndef __MINGW32__
   if (IsWin8OrLater()) {
-    // Try to get language list from GlobalizationPreferences; if this fails,
-    // we'll fall back to GetUserPreferredUILanguages.
-    // Per MSDN, these APIs are not available prior to Win8.
+    
+    
+    
 
-    // RoInitialize may fail with "cannot change thread mode after it is set",
-    // if the runtime was already initialized on this thread.
-    // This appears to be harmless, and we can proceed to attempt the following
-    // runtime calls.
+    
+    
+    
+    
     HRESULT inited = RoInitialize(RO_INIT_MULTITHREADED);
     if (SUCCEEDED(inited) || inited == RPC_E_CHANGED_MODE) {
       ComPtr<IGlobalizationPreferencesStatics> globalizationPrefs;
@@ -65,23 +65,22 @@ bool OSPreferences::ReadSystemLocales(nsTArray<nsCString>& aLocaleList) {
           }
         }
       }
-
-      // Only close the runtime if we successfully initialized it above,
-      // otherwise we assume it was already in use and should be left as is.
-      if (SUCCEEDED(inited)) {
-        RoUninitialize();
-      }
+    }
+    
+    
+    if (SUCCEEDED(inited)) {
+      RoUninitialize();
     }
   }
 #endif
 
-  // Per MSDN, GetUserPreferredUILanguages is available from Vista onwards,
-  // so we can use it unconditionally (although it may not work well!)
+  
+  
   if (aLocaleList.IsEmpty()) {
-    // Note that according to the questioner at
-    // https://stackoverflow.com/questions/52849233/getuserpreferreduilanguages-never-returns-more-than-two-languages,
-    // this may not always return the full list of languages we'd expect.
-    // We should always get at least the first-preference lang, though.
+    
+    
+    
+    
     ULONG numLanguages = 0;
     DWORD cchLanguagesBuffer = 0;
     if (!GetUserPreferredUILanguages(MUI_LANGUAGE_NAME, &numLanguages, nullptr,
@@ -171,23 +170,23 @@ static LCTYPE ToTimeLCType(OSPreferences::DateTimeFormatStyle aFormatStyle) {
   }
 }
 
-/**
- * Windows API includes regional preferences from the user only
- * if we pass empty locale string or if the locale string matches
- * the current locale.
- *
- * Since Windows API only allows us to retrieve two options - short/long
- * we map it to our four options as:
- *
- *   short  -> short
- *   medium -> short
- *   long   -> long
- *   full   -> long
- *
- * In order to produce a single date/time format, we use CLDR pattern
- * for combined date/time string, since Windows API does not provide an
- * option for this.
- */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 bool OSPreferences::ReadDateTimePattern(DateTimeFormatStyle aDateStyle,
                                         DateTimeFormatStyle aTimeStyle,
                                         const nsACString& aLocale,
@@ -200,11 +199,11 @@ bool OSPreferences::ReadDateTimePattern(DateTimeFormatStyle aDateStyle,
   bool isTime = aTimeStyle != DateTimeFormatStyle::None &&
                 aTimeStyle != DateTimeFormatStyle::Invalid;
 
-  // If both date and time are wanted, we'll initially read them into a
-  // local string, and then insert them into the overall date+time pattern;
-  // but if only one is needed we'll work directly with the return value.
-  // Set 'str' to point to the string we will use to retrieve patterns
-  // from Windows.
+  
+  
+  
+  
+  
   nsAutoString tmpStr;
   nsAString* str;
   if (isDate && isTime) {
@@ -229,19 +228,19 @@ bool OSPreferences::ReadDateTimePattern(DateTimeFormatStyle aDateStyle,
       return false;
     }
 
-    // We're doing it to ensure the terminator will fit when Windows writes the
-    // data to its output buffer. See bug 1358159 for details.
+    
+    
     str->SetLength(len);
     GetLocaleInfoEx(reinterpret_cast<const wchar_t*>(localeName.BeginReading()),
                     lcType, (WCHAR*)str->BeginWriting(), len);
-    str->SetLength(len - 1);  // -1 because len counts the null terminator
+    str->SetLength(len - 1);  
 
-    // Windows uses "ddd" and "dddd" for abbreviated and full day names
-    // respectively,
-    //   https://msdn.microsoft.com/en-us/library/windows/desktop/dd317787(v=vs.85).aspx
-    // but in a CLDR/ICU-style pattern these should be "EEE" and "EEEE".
-    //   http://userguide.icu-project.org/formatparse/datetime
-    // So we fix that up here.
+    
+    
+    
+    
+    
+    
     nsAString::const_iterator start, pos, end;
     start = str->BeginReading(pos);
     str->EndReading(end);
@@ -254,20 +253,20 @@ bool OSPreferences::ReadDateTimePattern(DateTimeFormatStyle aDateStyle,
       }
     }
 
-    // Also, Windows uses lowercase "g" or "gg" for era, but ICU wants uppercase
-    // "G" (it would interpret "g" as "modified Julian day"!). So fix that.
+    
+    
     int32_t index = str->FindChar('g');
     if (index >= 0) {
       str->Replace(index, 1, 'G');
-      // If it was a double "gg", just drop the second one.
+      
       index++;
       if (str->CharAt(index) == 'g') {
         str->Cut(index, 1);
       }
     }
 
-    // If time was also requested, we need to substitute the date pattern from
-    // Windows into the date+time format that we have in aRetVal.
+    
+    
     if (isTime) {
       nsAString::const_iterator start, pos, end;
       start = aRetVal.BeginReading(pos);
@@ -287,18 +286,18 @@ bool OSPreferences::ReadDateTimePattern(DateTimeFormatStyle aDateStyle,
       return false;
     }
 
-    // We're doing it to ensure the terminator will fit when Windows writes the
-    // data to its output buffer. See bug 1358159 for details.
+    
+    
     str->SetLength(len);
     GetLocaleInfoEx(reinterpret_cast<const wchar_t*>(localeName.BeginReading()),
                     lcType, (WCHAR*)str->BeginWriting(), len);
     str->SetLength(len - 1);
 
-    // Windows uses "t" or "tt" for a "time marker" (am/pm indicator),
-    //   https://msdn.microsoft.com/en-us/library/windows/desktop/dd318148(v=vs.85).aspx
-    // but in a CLDR/ICU-style pattern that should be "a".
-    //   http://userguide.icu-project.org/formatparse/datetime
-    // So we fix that up here.
+    
+    
+    
+    
+    
     int32_t index = str->FindChar('t');
     if (index >= 0) {
       str->Replace(index, 1, 'a');
