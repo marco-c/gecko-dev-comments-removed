f3a3e3637339f76deb1562e38370d3827fd8cf2e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-11 03:07:32 +0000

    Bug 1594390 - Keep session history listener alive in docshell/test/navigation/browser_test_bfcache_eviction.js, r=smaug
    Differential Revision: https://phabricator.services.mozilla.com/D56600
    
    UltraBlame original commit: 1875f86df1ae974eef40719356a19fd7f9823cd2

--- docshell/test/navigation/browser_test_bfcache_eviction.js
+++ docshell/test/navigation/browser_test_bfcache_eviction.js
@@ -10,7 +10,7 @@ add_task(async function() {
 
   
   var testPage =
-    "data:text/html,<html id='html1'><body id='body1'></body></html>";
+    "data:text/html,<html id='html1'><body id='body1'>First tab ever opened</body></html>";
   await BrowserTestUtils.withNewTab({ gBrowser, url: testPage }, async function(
     browser
   ) {
@@ -31,6 +31,7 @@ add_task(async function() {
                 "History listener got called after a content viewer was evicted"
               );
               legacySHistory.removeSHistoryListener(historyListener);
+              delete content._testListener;
               
               resolve();
             },
@@ -40,6 +41,8 @@ add_task(async function() {
             ]),
           };
           legacySHistory.addSHistoryListener(historyListener);
+          
+          content._testListener = historyListener;
         });
       });
     } else {
@@ -56,6 +59,7 @@ add_task(async function() {
             "History listener got called after a content viewer was evicted"
           );
           legacySHistory.removeSHistoryListener(historyListener);
+          delete content._testListener;
           
           testDone.resolve();
         },
@@ -65,6 +69,8 @@ add_task(async function() {
         ]),
       };
       legacySHistory.addSHistoryListener(historyListener);
+      
+      content._testListener = historyListener;
     }
 
     
