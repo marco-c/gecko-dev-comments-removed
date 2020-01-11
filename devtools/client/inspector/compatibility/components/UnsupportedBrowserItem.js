b5e8d4d8d23bd6f1a3c061b189994030baae966d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-11 01:22:25 +0000

    Bug 1590982: Introduce UnsupportedBrowserItem component. r=rcaliman,ladybenko
    Differential Revision: https://phabricator.services.mozilla.com/D52971
    
    UltraBlame original commit: befe2819a36704bd005d465fbefabdc6dcb136d3

---
+++ devtools/client/inspector/compatibility/components/UnsupportedBrowserItem.js
@@ -0,0 +1,39 @@
+
+
+
+
+"use strict";
+
+const { PureComponent } = require("devtools/client/shared/vendor/react");
+const dom = require("devtools/client/shared/vendor/react-dom-factories");
+const PropTypes = require("devtools/client/shared/vendor/react-prop-types");
+
+const Types = require("../types");
+
+class UnsupportedBrowserItem extends PureComponent {
+  static get propTypes() {
+    return {
+      id: Types.browser.id,
+      name: Types.browser.name,
+      versions: PropTypes.arrayOf(
+        PropTypes.shape({
+          alias: Types.browser.status,
+          version: Types.browser.version,
+        })
+      ),
+    };
+  }
+
+  render() {
+    const { name, versions } = this.props;
+
+    const versionsTitle = versions
+      .map(({ alias, version }) => version + (alias ? `(${alias})` : ""))
+      .join(", ");
+    const title = `${name} ${versionsTitle}`;
+
+    return dom.li({ title }, name);
+  }
+}
+
+module.exports = UnsupportedBrowserItem;
