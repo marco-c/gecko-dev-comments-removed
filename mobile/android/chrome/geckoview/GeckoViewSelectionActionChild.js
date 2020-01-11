c1775b8a3e12b33e9f29b54c89402a3f557194bf
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-11 00:57:41 +0000

    Bug 1597707 - Do not dispatch SELECT_ALL selection action if the input is empty or all the text is already selected. r=esawin
    Do not dispatch SELECT_ALL selection action if the input is empty or all the text is already selected.
    
    Differential Revision: https://phabricator.services.mozilla.com/D53828
    
    UltraBlame original commit: d453bc8a18d2fb3c36fe1efa564fb4508e6f69fe

--- mobile/android/chrome/geckoview/GeckoViewSelectionActionChild.js
+++ mobile/android/chrome/geckoview/GeckoViewSelectionActionChild.js
@@ -70,7 +70,27 @@ class GeckoViewSelectionActionChild extends GeckoViewChildModule {
       },
       {
         id: "org.mozilla.geckoview.SELECT_ALL",
-        predicate: e => e.reason !== "longpressonemptycontent",
+        predicate: e => {
+          if (e.reason === "longpressonemptycontent") {
+            return false;
+          }
+          if (e.selectionEditable && e.target && e.target.activeElement) {
+            const element = e.target.activeElement;
+            let value = "";
+            if (element.value) {
+              value = element.value;
+            } else if (
+              element.isContentEditable ||
+              e.target.designMode === "on"
+            ) {
+              value = element.innerText;
+            }
+            
+            
+            return value !== "" && value !== e.selectedTextContent;
+          }
+          return true;
+        },
         perform: e => docShell.doCommand("cmd_selectAll"),
       },
     ];
