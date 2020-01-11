61fc8d1e934acc774788ea8ccf54b097de8f60a3
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-11 05:12:06 +0000

    Bug 1600595: Part 1 - Consolidate ShmemAllocator and IShmemAllocator into one class r=nical
    IShmemAllocator was introduced in bug 1272018 as a planned replacement for ShmemAllocator.  They are essentially the same interface.  This patch removes ShmemAllocator.
    
    Differential Revision: https://phabricator.services.mozilla.com/D56224
    
    UltraBlame original commit: 40a8f46b2286944e57ca408db51492cbba0c61d1

--- gfx/layers/ipc/VideoBridgeParent.h
+++ gfx/layers/ipc/VideoBridgeParent.h
@@ -18,7 +18,7 @@ class CompositorThreadHolder;
 
 class VideoBridgeParent final : public PVideoBridgeParent,
                                 public HostIPCAllocator,
-                                public ShmemAllocator {
+                                public mozilla::ipc::IShmemAllocator {
  public:
   ~VideoBridgeParent();
 
@@ -46,7 +46,7 @@ class VideoBridgeParent final : public PVideoBridgeParent,
       const nsTArray<AsyncParentMessageData>& aMessage) override;
 
   
-  ShmemAllocator* AsShmemAllocator() override { return this; }
+  IShmemAllocator* AsShmemAllocator() override { return this; }
   bool IsSameProcess() const override;
   bool IPCOpen() const override { return !mClosed; }
 
@@ -57,7 +57,7 @@ class VideoBridgeParent final : public PVideoBridgeParent,
   bool AllocUnsafeShmem(size_t aSize, ipc::SharedMemory::SharedMemoryType aType,
                         ipc::Shmem* aShmem) override;
 
-  void DeallocShmem(ipc::Shmem& aShmem) override;
+  bool DeallocShmem(ipc::Shmem& aShmem) override;
 
  private:
   explicit VideoBridgeParent(VideoBridgeSource aSource);
