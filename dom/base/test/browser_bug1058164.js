1435fcb6243e6be610a1f73f4e6d11e1422368b2
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-11 02:45:52 +0000

    Bug 1596918: Part 4c - Fix callers which depend on document lifecycle changes. r=mccr8
    ContentTask tasks have a different lifetime than SpecialPowers tasks, with the
    former being tied to the lifetime of a message manager and the latter tied to
    the lifetime of a window global. That means that existing ContentTask callers
    which expect to be able to register load listeners before the creation of a
    window global, or which expect to persist after a page has navigated, won't
    work as SpecialPowers tasks.
    
    Since those sorts of tasks are not really resilient in the face of Fission,
    they should really be written to work differently, but this patch mostly just
    reverts them to using ContentTask for the time being.
    
    Differential Revision: https://phabricator.services.mozilla.com/D53744
    
    UltraBlame original commit: be770d112dd8a1a98f5072f81dc9aa1fd3ffafe5

--- dom/base/test/browser_bug1058164.js
+++ dom/base/test/browser_bug1058164.js
@@ -203,7 +203,7 @@ add_task(async function test_swap_frameloader_pagevisibility_events() {
 
   
   
-  await SpecialPowers.spawn(emptyBrowser, [], async () => {
+  await ContentTask.spawn(emptyBrowser, null, async () => {
     if (content.document.visibilityState === "hidden") {
       info("waiting for hidden emptyBrowser to pageshow");
       await ContentTaskUtils.waitForEvent(content, "pageshow", {});
