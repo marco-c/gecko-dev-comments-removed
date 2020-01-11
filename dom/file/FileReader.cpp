61268bac72ca5b839c8b1a8bff17b170b3ddc35e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-11 01:18:46 +0000

    Bug 1599843 - FileReader::OnLoadEnd is infallible. r=baku
    Depends on D54991
    
    Differential Revision: https://phabricator.services.mozilla.com/D54992
    
    UltraBlame original commit: 676aefeaec6fa2eb0b068a16cc3a41abef9aa865

--- dom/file/FileReader.cpp
+++ dom/file/FileReader.cpp
@@ -612,7 +612,8 @@ FileReader::OnInputStreamReady(nsIAsyncInputStream* aStream) {
     if (rv == NS_BASE_STREAM_CLOSED) {
       rv = NS_OK;
     }
-    return OnLoadEnd(rv);
+    OnLoadEnd(rv);
+    return NS_OK;
   }
 
   mTransferred += count;
@@ -637,7 +638,7 @@ FileReader::GetName(nsACString& aName) {
   return NS_OK;
 }
 
-nsresult FileReader::OnLoadEnd(nsresult aStatus) {
+void FileReader::OnLoadEnd(nsresult aStatus) {
   
   ClearProgressEventTimer();
 
@@ -647,20 +648,20 @@ nsresult FileReader::OnLoadEnd(nsresult aStatus) {
   
   if (NS_FAILED(aStatus)) {
     FreeDataAndDispatchError(aStatus);
-    return NS_OK;
+    return;
   }
 
   
   
   if (mDataLen != mTotal) {
     FreeDataAndDispatchError(NS_ERROR_FAILURE);
-    return NS_OK;
+    return;
   }
 
   
   if (mDataFormat == FILE_AS_ARRAYBUFFER) {
     OnLoadEndArrayBuffer();
-    return NS_OK;
+    return;
   }
 
   nsresult rv = NS_OK;
@@ -681,11 +682,10 @@ nsresult FileReader::OnLoadEnd(nsresult aStatus) {
 
   if (NS_WARN_IF(NS_FAILED(rv))) {
     FreeDataAndDispatchError(rv);
-    return NS_OK;
+    return;
   }
 
   FreeDataAndDispatchSuccess();
-  return NS_OK;
 }
 
 void FileReader::Abort() {
