cb0b8f4f0c42cfaee407cf1d8fb50b5e6706ba13
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-11 01:00:32 +0000

    Backed out changeset 8f52344661fe (bug 1598787) for build bustages at build/src/tools/fuzzing/faulty/Faulty.cpp on a CLOSED TREE
    UltraBlame original commit: a9de318848b60f57ada623808db6f969c69ad908

--- gfx/ipc/GPUChild.cpp
+++ gfx/ipc/GPUChild.cpp
@@ -233,7 +233,7 @@ void GPUChild::ActorDestroy(ActorDestroyReason aWhy) {
 
     Telemetry::Accumulate(
         Telemetry::SUBPROCESS_ABNORMAL_ABORT,
-        nsDependentCString(XRE_GeckoProcessTypeToString(GeckoProcessType_GPU)),
+        nsDependentCString(XRE_ChildProcessTypeToString(GeckoProcessType_GPU)),
         1);
 
     
