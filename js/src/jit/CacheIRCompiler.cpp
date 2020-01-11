8b663907fc9464715701e21ea3abcf64f2247248
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-11 04:49:15 +0000

    Bug 1605591 - Always check sharedData before reading funLength. r=jandem
    In the bound function case, we move the length initialization before the name
    is set. This may result in the length being set before the guards are all
    finished, but that doesn't affect the slow-path.
    
    Depends on D58083
    
    Differential Revision: https://phabricator.services.mozilla.com/D58084
    
    UltraBlame original commit: 9ec61acfd6fc554cae7359e099eb06da847c6ab2

--- js/src/jit/CacheIRCompiler.cpp
+++ js/src/jit/CacheIRCompiler.cpp
@@ -2970,6 +2970,7 @@ bool CacheIRCompiler::emitLoadFunctionLengthResult() {
   
   masm.loadPtr(Address(obj, JSFunction::offsetOfScript()), scratch);
   masm.loadPtr(Address(scratch, JSScript::offsetOfSharedData()), scratch);
+  masm.branchTestPtr(Assembler::Zero, scratch, scratch, failure->label());
   masm.loadPtr(Address(scratch, RuntimeScriptData::offsetOfISD()), scratch);
   masm.load16ZeroExtend(
       Address(scratch, ImmutableScriptData::offsetOfFunLength()), scratch);
