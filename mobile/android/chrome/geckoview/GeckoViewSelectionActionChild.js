b2c85a3cc0d0cd5a8a4660b4a58935cb721e38b8
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-11 00:23:59 +0000

    Backed out changeset 4fbd18ff4990 (bug 1597707) for causing geckoview failures in SelectionActionDelegateTest.request
    UltraBlame original commit: 60309bd4fc363dd294246a0172706a2e1b38bc8d

--- mobile/android/chrome/geckoview/GeckoViewSelectionActionChild.js
+++ mobile/android/chrome/geckoview/GeckoViewSelectionActionChild.js
@@ -70,18 +70,7 @@ class GeckoViewSelectionActionChild extends GeckoViewChildModule {
       },
       {
         id: "org.mozilla.geckoview.SELECT_ALL",
-        predicate: e => {
-          if (e.reason === "longpressonemptycontent") {
-            return false;
-          }
-          if (e.target && e.target.activeElement) {
-            const value = e.target.activeElement.value;
-            
-            
-            return value !== "" && value !== e.selectedTextContent;
-          }
-          return true;
-        },
+        predicate: e => e.reason !== "longpressonemptycontent",
         perform: e => docShell.doCommand("cmd_selectAll"),
       },
     ];
