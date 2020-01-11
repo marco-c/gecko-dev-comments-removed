1be1df0b357b64972001f332522d7b789c860154
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-11 02:18:58 +0000

    Bug 1596224 [wpt PR 20242] - Add --ignore-glob flag to wpt lint., a=testonly
    Automatic update from web-platform-tests
    Add --ignore-glob flag to wpt lint. (#20242)
    
    This allows specifying file patterns to ignore via the commandline.
    
    This feature is used inside Chromium CI to skip linting some
    auto-generated files that aren't part of WPT. The motivation to make
    this change upstream is because we sometimes get conflicts between the
    local Chromium copy and the update upstream copy.
    --
    
    wpt-commits: cc31ccce0496f075d7db5b08b16b2d2398b20a46
    wpt-pr: 20242
    
    UltraBlame original commit: 9be2cdaaff23f332c087655c9d043988be6db17f

--- testing/web-platform/tests/tools/lint/tests/test_lint.py
+++ testing/web-platform/tests/tools/lint/tests/test_lint.py
@@ -398,6 +398,29 @@ def test_check_css_globally_unique_ignored_dir(caplog):
     assert caplog.text == ""
 
 
+def test_ignore_glob(caplog):
+    
+    
+    
+    
+    with _mock_lint("check_path") as mocked_check_path:
+        with _mock_lint("check_file_contents") as mocked_check_file_contents:
+            rv = lint(_dummy_repo, ["ref/absolute.html", "ref/existent_relative.html"], "normal", "*solu*")
+            assert rv == 0
+            
+            assert mocked_check_path.call_count == 1
+            assert mocked_check_file_contents.call_count == 1
+            assert caplog.text == ""
+    
+    with _mock_lint("check_path") as mocked_check_path:
+        with _mock_lint("check_file_contents") as mocked_check_file_contents:
+            rv = lint(_dummy_repo, ["ref/absolute.html", "ref/existent_relative.html"], "normal")
+            assert rv == 1
+            assert mocked_check_path.call_count == 2
+            assert mocked_check_file_contents.call_count == 2
+            assert "ABSOLUTE-URL-REF" in caplog.text
+
+
 def test_all_filesystem_paths():
     with mock.patch(
             'tools.lint.lint.walk',
@@ -443,7 +466,8 @@ def test_main_with_args():
                 m.assert_called_once_with(repo_root,
                                           [os.path.relpath(os.path.join(os.getcwd(), x), repo_root)
                                            for x in ['a', 'b', 'c']],
-                                          "normal")
+                                          "normal",
+                                          str())
     finally:
         sys.argv = orig_argv
 
@@ -455,7 +479,7 @@ def test_main_no_args():
         with _mock_lint('lint', return_value=True) as m:
             with _mock_lint('changed_files', return_value=['foo', 'bar']):
                 lint_mod.main(**vars(create_parser().parse_args()))
-                m.assert_called_once_with(repo_root, ['foo', 'bar'], "normal")
+                m.assert_called_once_with(repo_root, ['foo', 'bar'], "normal", str())
     finally:
         sys.argv = orig_argv
 
@@ -467,6 +491,6 @@ def test_main_all():
         with _mock_lint('lint', return_value=True) as m:
             with _mock_lint('all_filesystem_paths', return_value=['foo', 'bar']):
                 lint_mod.main(**vars(create_parser().parse_args()))
-                m.assert_called_once_with(repo_root, ['foo', 'bar'], "normal")
+                m.assert_called_once_with(repo_root, ['foo', 'bar'], "normal", str())
     finally:
         sys.argv = orig_argv
