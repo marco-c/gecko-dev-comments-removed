6872b7cd1432821be20b6b67b28c8f9255092564
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-11 04:05:06 +0000

    Bug 1604625 - Add nightly config value to enable GL error checking. r=kvark
    Differential Revision: https://phabricator.services.mozilla.com/D57549
    
    UltraBlame original commit: 35af0b925215124a619bc054aeeb8d01e99b4e63

--- gfx/webrender_bindings/WebRenderAPI.cpp
+++ gfx/webrender_bindings/WebRenderAPI.cpp
@@ -70,6 +70,15 @@ class NewRenderer : public RendererEvent {
     *mUseDComp = compositor->UseDComp();
     *mUseTripleBuffering = compositor->UseTripleBuffering();
 
+    
+    
+    
+    bool panic_on_gl_error = false;
+#ifdef NIGHTLY_BUILD
+    panic_on_gl_error =
+        StaticPrefs::gfx_webrender_panic_on_gl_error_AtStartup();
+#endif
+
     bool allow_texture_swizzling = gfx::gfxVars::UseGLSwizzle();
     bool isMainWindow = true;  
     bool supportLowPriorityTransactions = isMainWindow;
@@ -104,7 +113,8 @@ class NewRenderer : public RendererEvent {
             compositor->GetMaxUpdateRects(),
             compositor->GetMaxPartialPresentRects(), mDocHandle, &wrRenderer,
             mMaxTextureSize,
-            StaticPrefs::gfx_webrender_enable_gpu_markers_AtStartup())) {
+            StaticPrefs::gfx_webrender_enable_gpu_markers_AtStartup(),
+            panic_on_gl_error)) {
       
       return;
     }
