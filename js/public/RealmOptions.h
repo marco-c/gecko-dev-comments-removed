a3e3f7d0c88344338e4dce254550993c471ce74c
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-11 05:50:24 +0000

    Bug 1607930 - Enable deferred allocation mode by default r=tcampbell
    Differential Revision: https://phabricator.services.mozilla.com/D59366
    
    UltraBlame original commit: 24ac3bde10e02b9b38aba10385f8566caae50152

--- js/public/RealmOptions.h
+++ js/public/RealmOptions.h
@@ -312,7 +312,7 @@ class JS_PUBLIC_API RealmBehaviors {
   
   bool singletonsAsTemplates_ = true;
   bool isNonLive_ = false;
-  bool deferredParserAlloc_ = false;
+  bool deferredParserAlloc_ = true;
 };
 
 
