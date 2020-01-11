db9ee2b008f4bdca11cfa375c0c89ef45eff269e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-11 02:56:02 +0000

    Bug 1598582 Part 2: Add event to record OptimizeSourceSurface. r=jrmuizel
    This also optimizes the surface returned by CreateSourceSurfaceFromData to
    match what would happen in the non-recorded case. This means that all surfaces
    of type RECORDING are optimized, i.e. they represent a surface type that
    matches the DrawTarget type in the Translator.
    
    Differential Revision: https://phabricator.services.mozilla.com/D55292
    
    UltraBlame original commit: 908c5b3e65b491cfcd71acedc853f989191a3e22

--- gfx/2d/RecordedEvent.h
+++ gfx/2d/RecordedEvent.h
@@ -27,7 +27,7 @@ const uint32_t kMagicInt = 0xc001feed;
 const uint16_t kMajorRevision = 10;
 
 
-const uint16_t kMinorRevision = 0;
+const uint16_t kMinorRevision = 1;
 
 struct ReferencePtr {
   ReferencePtr() : mLongPtr(0) {}
@@ -319,6 +319,7 @@ class RecordedEvent {
     EXTERNALSURFACECREATION,
     FLUSH,
     DETACHALLSNAPSHOTS,
+    OPTIMIZESOURCESURFACE,
     LAST,
   };
 
