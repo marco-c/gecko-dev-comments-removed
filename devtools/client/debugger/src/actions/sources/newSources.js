e4cf0c1dd14caf4415a43231c72c0ee4aa421d23
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-11 02:40:20 +0000

    Bug 1599882 - Memoize tabs selectors. r=bhackett
    Differential Revision: https://phabricator.services.mozilla.com/D55015
    
    UltraBlame original commit: 7cb21c0caf6315278909f44c28eb0cf1cfab3531

--- devtools/client/debugger/src/actions/sources/newSources.js
+++ devtools/client/debugger/src/actions/sources/newSources.js
@@ -207,6 +207,7 @@ function checkPendingBreakpoints(cx: Context, sourceId: string) {
 
     
     await dispatch(loadSourceText({ cx, source }));
+
     await dispatch(setBreakableLines(cx, source.id));
 
     await Promise.all(
