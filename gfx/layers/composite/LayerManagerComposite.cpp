6a89938fcc2ceef6d1637034467310705139fb65
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-11 00:37:37 +0000

    Bug 1597585 - Make LayerManagerComposite use mGL to pass the GL context to PreRender, just like WebRender. r=mattwoodrow
    Differential Revision: https://phabricator.services.mozilla.com/D53772
    
    UltraBlame original commit: a01a7f2f2c18355f8fc2ceab401ac324c182c364

--- gfx/layers/composite/LayerManagerComposite.cpp
+++ gfx/layers/composite/LayerManagerComposite.cpp
@@ -41,6 +41,7 @@
 #include "mozilla/gfx/Rect.h"           
 #include "mozilla/gfx/Types.h"          
 #include "mozilla/layers/Compositor.h"  
+#include "mozilla/layers/CompositorOGL.h"
 #include "mozilla/layers/CompositorTypes.h"
 #include "mozilla/layers/Effects.h"              
 #include "mozilla/layers/LayerMetricsWrapper.h"  
@@ -62,7 +63,6 @@
 #  include <android/native_window.h>
 #  include "mozilla/jni/Utils.h"
 #  include "mozilla/widget/AndroidCompositorWidget.h"
-#  include "opengl/CompositorOGL.h"
 #  include "GLConsts.h"
 #  include "GLContextEGL.h"
 #  include "GLContextProvider.h"
@@ -1103,7 +1103,9 @@ bool LayerManagerComposite::Render(const nsIntRegion& aInvalidRegion,
 
   mozilla::widget::WidgetRenderingContext widgetContext;
 #if defined(XP_MACOSX)
-  widgetContext.mLayerManager = this;
+  if (CompositorOGL* compositorOGL = mCompositor->AsCompositorOGL()) {
+    widgetContext.mGL = compositorOGL->gl();
+  }
 #endif
 
   {
