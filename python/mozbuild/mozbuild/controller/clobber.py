6ac36d2c735a4487a55e343d7ec653436b60fc92
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-11 05:03:08 +0000

    Bug 1606692 - Scripts in python/mozbuild/mozbuild/controller support Python 3 r=firefox-build-system-reviewers,mshal
    Differential Revision: https://phabricator.services.mozilla.com/D58546
    
    UltraBlame original commit: 878d1084a83a39e8ee92e075a5c63bbf81f5cc91

--- python/mozbuild/mozbuild/controller/clobber.py
+++ python/mozbuild/mozbuild/controller/clobber.py
@@ -2,7 +2,7 @@
 
 
 
-from __future__ import absolute_import, print_function
+from __future__ import absolute_import, print_function, unicode_literals
 
 r'''This module contains code for managing clobbering of the tree.'''
 
