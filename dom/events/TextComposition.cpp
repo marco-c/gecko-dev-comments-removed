0fe816136bfb14875477a89a1f607bd501189d2d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-11 05:23:33 +0000

    Bug 1600267: part 11) Call `ComparePoints` instead of `ComparePoints_Deprecated` in `TextComposition`. r=masayuki
    Differential Revision: https://phabricator.services.mozilla.com/D57967
    
    UltraBlame original commit: f6d4c4f8fca34318f7bf78a627a5f75e53dea5cc

--- dom/events/TextComposition.cpp
+++ dom/events/TextComposition.cpp
@@ -715,9 +715,9 @@ RawRangeBoundary TextComposition::GetStartRef() const {
         continue;
       }
       
-      bool disconnected = false;
-      if (nsContentUtils::ComparePoints_Deprecated(
-              range->StartRef(), firstRange->StartRef(), &disconnected) == -1) {
+      
+      if (*nsContentUtils::ComparePoints(range->StartRef(),
+                                         firstRange->StartRef()) == -1) {
         firstRange = range;
       }
     }
@@ -772,9 +772,9 @@ RawRangeBoundary TextComposition::GetEndRef() const {
         continue;
       }
       
-      bool disconnected = false;
-      if (nsContentUtils::ComparePoints_Deprecated(
-              lastRange->EndRef(), range->EndRef(), &disconnected) == -1) {
+      
+      if (*nsContentUtils::ComparePoints(lastRange->EndRef(),
+                                         range->EndRef()) == -1) {
         lastRange = range;
       }
     }
