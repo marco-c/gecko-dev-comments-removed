9c06e7084c2cc672c15571341de30daa24e7d908
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-11 02:28:45 +0000

    Bug 1589700 - Record the number of unique site origins across all tabs r=smaug,agi,Dexter
    Differential Revision: https://phabricator.services.mozilla.com/D54027
    
    UltraBlame original commit: 52af8765cb2153b4218308039cf3387faf41900a

--- docshell/base/CanonicalBrowsingContext.h
+++ docshell/base/CanonicalBrowsingContext.h
@@ -81,6 +81,12 @@ class CanonicalBrowsingContext final : public BrowsingContext {
 
   
   
+  static uint32_t CountSiteOrigins(
+      GlobalObject& aGlobal,
+      const Sequence<mozilla::OwningNonNull<BrowsingContext>>& aRoots);
+
+  
+  
   void UpdateMediaAction(MediaControlActions aAction);
 
   
