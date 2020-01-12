ce4ee182fd2f62745287fa6fa5008f4d6f5526d6
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-12 14:52:54 +0000

    Bug 1585008 - Part 1: Use CSS grid to place browser resizers in RDM. r=bgrins,bradwerth
    Place resizers for RDM browser using CSS grid. This revision takes advantage of using grid-template-columns/rows to specify where browser stack elements are placed in the grid.
    
    Differential Revision: https://phabricator.services.mozilla.com/D59189
    
    UltraBlame original commit: 5062702263870d73052474f4c8e3814086bb9b10

--- devtools/client/responsive/ui.js
+++ devtools/client/responsive/ui.js
@@ -250,6 +250,14 @@ class ResponsiveUI {
     rdmFrame.src = "chrome://devtools/content/responsive/toolbar.xhtml";
     rdmFrame.classList.add("rdm-toolbar");
 
+    
+    const resizeHandle = doc.createElement("div");
+    resizeHandle.classList.add("viewport-resize-handle");
+    const resizeHandleX = doc.createElement("div");
+    resizeHandleX.classList.add("viewport-horizontal-resize-handle");
+    const resizeHandleY = doc.createElement("div");
+    resizeHandleY.classList.add("viewport-vertical-resize-handle");
+
     this.browserContainerEl = gBrowser.getBrowserContainer(
       gBrowser.getBrowserForTab(this.tab)
     );
@@ -261,6 +269,9 @@ class ResponsiveUI {
 
     
     this.browserStackEl.prepend(rdmFrame);
+    this.browserStackEl.append(resizeHandle);
+    this.browserStackEl.append(resizeHandleX);
+    this.browserStackEl.append(resizeHandleY);
 
     
     message.wait(rdmFrame.contentWindow, "script-init").then(async () => {
@@ -278,6 +289,9 @@ class ResponsiveUI {
     });
 
     this.rdmFrame = rdmFrame;
+    this.resizeHandle = resizeHandle;
+    this.resizeHandleX = resizeHandleX;
+    this.resizeHandleY = resizeHandleY;
   }
 
   
@@ -331,6 +345,11 @@ class ResponsiveUI {
       this.rdmFrame.contentWindow.removeEventListener("message", this);
       this.rdmFrame.remove();
 
+      
+      this.resizeHandle.remove();
+      this.resizeHandleX.remove();
+      this.resizeHandleY.remove();
+
       this.browserContainerEl.classList.remove("responsive-mode");
       this.browserStackEl.style.removeProperty("--rdm-width");
       this.browserStackEl.style.removeProperty("--rdm-height");
@@ -365,6 +384,9 @@ class ResponsiveUI {
     this.tab = null;
     this.inited = null;
     this.rdmFrame = null;
+    this.resizeHandle = null;
+    this.resizeHandleX = null;
+    this.resizeHandleY = null;
     this.toolWindow = null;
     this.swap = null;
 
