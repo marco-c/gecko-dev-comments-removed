dddd97788d2787ab24d64eb1c9e15e7210e67e6d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-16 00:17:09 +0000

    Bug 1430182 - Remove racy code from test_file_upload.html r=dom-workers-and-storage-reviewers,sg
    This test contains a few race conditions that could cause it to
    hang and time out. This commit removes them, hopefully solving the
    intermittent failures.
    
    Differential Revision: https://phabricator.services.mozilla.com/D59918
    
    UltraBlame original commit: c35bb210b8ae793c844bd94c1848d246bf601293

--- dom/serviceworkers/test/utils.js
+++ dom/serviceworkers/test/utils.js
@@ -1,15 +1,18 @@
 function waitForState(worker, state, context) {
   return new Promise(resolve => {
-    if (worker.state === state) {
-      resolve(context);
-      return;
-    }
-    worker.addEventListener("statechange", function onStateChange() {
+    function onStateChange() {
       if (worker.state === state) {
         worker.removeEventListener("statechange", onStateChange);
         resolve(context);
       }
-    });
+    }
+
+    
+    
+    worker.addEventListener("statechange", onStateChange);
+
+    
+    onStateChange();
   });
 }
 
