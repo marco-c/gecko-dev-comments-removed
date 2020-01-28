e8798fb05bf4092f93489465788d4368588f846e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-28 16:43:26 +0000

    Bug 1607854 - Set field initializers as part of FunctionScriptEmitter::initScript r=djvj
    Depends on D60712
    
    Differential Revision: https://phabricator.services.mozilla.com/D60713
    
    UltraBlame original commit: 04f0206977325927b8beac1ca9e0bd495b0d99e6

--- js/src/frontend/FunctionEmitter.h
+++ js/src/frontend/FunctionEmitter.h
@@ -259,7 +259,7 @@ class MOZ_STACK_CLASS FunctionScriptEmitter {
   
   
   
-  MOZ_MUST_USE bool initScript();
+  MOZ_MUST_USE bool initScript(const FieldInitializers& fieldInitializers);
 
  private:
   MOZ_MUST_USE bool emitExtraBodyVarScope();
