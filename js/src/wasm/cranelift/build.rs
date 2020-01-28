331bbb56788ea6123889959fa8c1747130fd7a56
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-28 16:51:26 +0000

    Bug 1574865 - Baldr: Add support for Cranelift stackmaps. r=jseward
    This commit adds support for receiving stackmaps from Cranelift
    and converting to wasm::Stackmaps. [1] will change the stackmap
    representation in Cranelift to be the same as in Spidermonkey.
    
    The stack overflow/interrupt trap handler stackmap is implemented by
    sharing code with Ion.
    
    [1] https://github.com/bytecodealliance/cranelift/pull/1317
    
    Differential Revision: https://phabricator.services.mozilla.com/D58886
    
    UltraBlame original commit: f89941b23fced74c8bd88d5ba389e39c3277104a

--- js/src/wasm/cranelift/build.rs
+++ js/src/wasm/cranelift/build.rs
@@ -37,6 +37,7 @@ fn main() {
         .whitelist_function("global_.*")
         .whitelist_function("table_.*")
         .whitelist_function("funcType_.*")
+        .whitelist_function("stackmaps_.*")
         .whitelist_type("Cranelift.*")
         
         .rustified_enum("BD_.*|Trap|TypeCode|FuncTypeIdDescKind")
