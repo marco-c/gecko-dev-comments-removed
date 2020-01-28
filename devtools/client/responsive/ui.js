219a6a3e917e3701dcc7dc49e9440adb15fccf88
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-28 16:17:58 +0000

    Backed out 3 changesets (bug 1603370) for permafailing bug 1600635 on a CLOSED TREE
    Backed out changeset 0124af9f8ded (bug 1603370)
    Backed out changeset 1d819921c8a3 (bug 1603370)
    Backed out changeset b567b33ed95a (bug 1603370)
    
    UltraBlame original commit: 4b6a77b24e823bbf1225b551fac8a11d5e45dc1b

--- devtools/client/responsive/ui.js
+++ devtools/client/responsive/ui.js
@@ -11,7 +11,6 @@ const {
   getOrientation,
 } = require("devtools/client/responsive/utils/orientation");
 const Constants = require("devtools/client/responsive/constants");
-const { TargetList } = require("devtools/shared/resources/target-list");
 
 loader.lazyRequireGetter(
   this,
@@ -109,8 +108,6 @@ class ResponsiveUI {
     this.onResizeStart = this.onResizeStart.bind(this);
     this.onResizeStop = this.onResizeStop.bind(this);
 
-    this.onTargetAvailable = this.onTargetAvailable.bind(this);
-
     
     this.inited = this.init();
 
@@ -131,10 +128,6 @@ class ResponsiveUI {
     }
 
     return this._isBrowserUIEnabled;
-  }
-
-  get currentTarget() {
-    return this.targetList.targetFront;
   }
 
   
@@ -224,7 +217,11 @@ class ResponsiveUI {
     await this.connectToServer();
 
     
-    await this.restoreUIState();
+    await this.restoreState();
+
+    if (this.isBrowserUIEnabled) {
+      await this.responsiveFront.setDocumentInRDMPane(true);
+    }
 
     if (!this.isBrowserUIEnabled) {
       
@@ -323,11 +320,6 @@ class ResponsiveUI {
       return false;
     }
     this.destroying = true;
-
-    this.targetList.unwatchTargets(
-      [this.targetList.TYPES.FRAME],
-      this.onTargetAvailable
-    );
 
     
     
@@ -436,13 +428,8 @@ class ResponsiveUI {
     DebuggerServer.registerAllActors();
     this.client = new DebuggerClient(DebuggerServer.connectPipe());
     await this.client.connect();
-
     const targetFront = await this.client.mainRoot.getTab();
-    this.targetList = new TargetList(this.client.mainRoot, targetFront);
-    await this.targetList.watchTargets(
-      [this.targetList.TYPES.FRAME],
-      this.onTargetAvailable
-    );
+    this.responsiveFront = await targetFront.getFront("responsive");
   }
 
   
@@ -485,8 +472,6 @@ class ResponsiveUI {
         }
         break;
       case "BeforeTabRemotenessChange":
-        this.onRemotenessChange(event);
-        break;
       case "TabClose":
       case "unload":
         this.manager.closeIfNeeded(browserWindow, tab, {
@@ -734,7 +719,8 @@ class ResponsiveUI {
   }
 
   async onScreenshot() {
-    const captureScreenshotSupported = await this.currentTarget.actorHasMethod(
+    const targetFront = await this.client.mainRoot.getTab();
+    const captureScreenshotSupported = await targetFront.actorHasMethod(
       "responsive",
       "captureScreenshot"
     );
@@ -758,17 +744,10 @@ class ResponsiveUI {
     );
   }
 
-  async hasDeviceState() {
-    const deviceState = await asyncStorage.getItem(
-      "devtools.responsive.deviceState"
-    );
-    return !!deviceState;
-  }
-
   
 
 
-  async restoreUIState() {
+  async restoreState() {
     
     if (this.isBrowserUIEnabled) {
       const leftAlignmentEnabled = Services.prefs.getBoolPref(
@@ -779,38 +758,10 @@ class ResponsiveUI {
       this.updateUIAlignment(leftAlignmentEnabled);
     }
 
-    const hasDeviceState = await this.hasDeviceState();
-    if (hasDeviceState) {
-      
-      
-      return;
-    }
-
-    const height = Services.prefs.getIntPref(
-      "devtools.responsive.viewport.height",
-      0
+    const deviceState = await asyncStorage.getItem(
+      "devtools.responsive.deviceState"
     );
-    const width = Services.prefs.getIntPref(
-      "devtools.responsive.viewport.width",
-      0
-    );
-    this.updateViewportSize(width, height);
-  }
-
-  
-
-
-  async restoreActorState() {
-    if (this.isBrowserUIEnabled) {
-      
-      
-      
-      
-      await this.responsiveFront.setDocumentInRDMPane(true);
-    }
-
-    const hasDeviceState = await this.hasDeviceState();
-    if (hasDeviceState) {
+    if (deviceState) {
       
       
       return;
@@ -837,15 +788,16 @@ class ResponsiveUI {
       0
     );
 
+    let reloadNeeded = false;
     const { type, angle } = this.getInitialViewportOrientation({
       width,
       height,
     });
 
+    this.updateViewportSize(width, height);
     await this.updateDPPX(pixelRatio);
     await this.updateScreenOrientation(type, angle);
 
-    let reloadNeeded = false;
     if (touchSimulationEnabled) {
       reloadNeeded |=
         (await this.updateTouchSimulation(touchSimulationEnabled)) &&
@@ -961,7 +913,8 @@ class ResponsiveUI {
 
 
   async updateScreenOrientation(type, angle, isViewportRotated = false) {
-    const simulateOrientationChangeSupported = await this.currentTarget.actorHasMethod(
+    const targetFront = await this.client.mainRoot.getTab();
+    const simulateOrientationChangeSupported = await targetFront.actorHasMethod(
       "responsive",
       "simulateScreenOrientationChange"
     );
@@ -1077,30 +1030,6 @@ class ResponsiveUI {
     }
 
     return this.browserWindow;
-  }
-
-  async onTargetAvailable({ targetFront }) {
-    this.responsiveFront = await targetFront.getFront("responsive");
-    await this.restoreActorState();
-  }
-
-  async onRemotenessChange(event) {
-    const isTargetSwitchingEnabled = Services.prefs.getBoolPref(
-      "devtools.target-switching.enabled",
-      false
-    );
-
-    
-    
-    if (isTargetSwitchingEnabled && this.isBrowserUIEnabled) {
-      const newTarget = await this.client.mainRoot.getTab();
-      await this.targetList.switchToTarget(newTarget);
-    } else {
-      const { browserWindow, tab } = this;
-      this.manager.closeIfNeeded(browserWindow, tab, {
-        reason: event.type,
-      });
-    }
   }
 }
 
