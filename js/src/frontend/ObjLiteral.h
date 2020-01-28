8fe898d9db66c3ee22fc7e761ec9b6eec0a1a96a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-28 16:31:22 +0000

    Bug 1610192: Remove unneeded array-index in ObjLiteral array mode. r=mgaudet
    The ObjLiteral format allows for names (as atom indices) or numeric
    indices to be attached to each property in the literal. The numeric case
    is limited to 23 bits by the packed format in memory.
    
    The object-literal case ({}-literal with some numeric indices) already
    checks for in-range values before deciding to use the ObjLiteral
    infrastructure, but the array case did not check this.
    
    Fortunately, the indices in the array case were only used as a sanity
    check for debug-mode assertions, and never for any actual indexing
    operation, so no out-of-bounds behavior could result, even before this
    fix. However, the sanity-check assert will fire at
    object-construction-from-ObjLiteral time in cases where the ObjLiteral
    mode should not have been used, because no range check was done at
    ObjLiteral-construction time.
    
    This change removes the use of indices for array-mode ObjLiterals
    entirely.
    
    Differential Revision: https://phabricator.services.mozilla.com/D60474
    
    UltraBlame original commit: eeb6fd68c0223a72d8714734a34d3e6da69995e1

--- js/src/frontend/ObjLiteral.h
+++ js/src/frontend/ObjLiteral.h
@@ -195,15 +195,29 @@ inline bool ObjLiteralOpcodeHasAtomArg(ObjLiteralOpcode op) {
 
 struct ObjLiteralReaderBase;
 
+
+
+
+
 struct ObjLiteralKey {
  private:
   uint32_t value_;
-  bool isArrayIndex_;
+
+  enum ObjLiteralKeyType {
+    None,
+    AtomIndex,
+    ArrayIndex,
+  };
+
+  ObjLiteralKeyType type_;
+
+  ObjLiteralKey(uint32_t value, ObjLiteralKeyType ty)
+      : value_(value), type_(ty) {}
 
  public:
-  ObjLiteralKey() : value_(0), isArrayIndex_(true) {}
+  ObjLiteralKey() : ObjLiteralKey(0, None) {}
   ObjLiteralKey(uint32_t value, bool isArrayIndex)
-      : value_(value), isArrayIndex_(isArrayIndex) {}
+      : ObjLiteralKey(value, isArrayIndex ? ArrayIndex : AtomIndex) {}
   ObjLiteralKey(const ObjLiteralKey& other) = default;
 
   static ObjLiteralKey fromPropName(uint32_t atomIndex) {
@@ -212,9 +226,11 @@ struct ObjLiteralKey {
   static ObjLiteralKey fromArrayIndex(uint32_t index) {
     return ObjLiteralKey(index, true);
   }
+  static ObjLiteralKey none() { return ObjLiteralKey(); }
 
-  bool isAtomIndex() const { return !isArrayIndex_; }
-  bool isArrayIndex() const { return isArrayIndex_; }
+  bool isNone() const { return type_ == None; }
+  bool isAtomIndex() const { return type_ == AtomIndex; }
+  bool isArrayIndex() const { return type_ == ArrayIndex; }
 
   uint32_t getAtomIndex() const {
     MOZ_ASSERT(isAtomIndex());
@@ -300,12 +316,23 @@ struct ObjLiteralWriter : private ObjLiteralWriterBase {
 
   void beginObject(ObjLiteralFlags flags) { flags_ = flags; }
   void setPropName(uint32_t propName) {
+    
+    MOZ_ASSERT(!flags_.contains(ObjLiteralFlag::Array));
     MOZ_ASSERT(propName <= ATOM_INDEX_MASK);
     nextKey_ = ObjLiteralKey::fromPropName(propName);
   }
   void setPropIndex(uint32_t propIndex) {
+    
+    MOZ_ASSERT(!flags_.contains(ObjLiteralFlag::Array));
     MOZ_ASSERT(propIndex <= ATOM_INDEX_MASK);
     nextKey_ = ObjLiteralKey::fromArrayIndex(propIndex);
+  }
+  void beginDenseArrayElements() {
+    
+    MOZ_ASSERT(flags_.contains(ObjLiteralFlag::Array));
+    
+    
+    nextKey_ = ObjLiteralKey::none();
   }
 
   MOZ_MUST_USE bool propWithConstNumericValue(const JS::Value& value) {
