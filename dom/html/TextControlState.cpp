03ba519a6876a05689a6256c2d3eae7ec2b7c8e8
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-28 16:46:47 +0000

    Bug 1606043 - Make `TextControlState::SetValueWithoutTextEditor()` notify the text control element of value changed before dispatching `eEditorInput` event r=smaug
    I made `nsContentUtils::DispatchInputEvent()` update validity of event target
    before dispatching `eEditorInput` event in bug 1584963.  However, this is not
    enough because it updates validity only when the type of `<input>` element is
    `mail`, but it's required for other types, for example, `<input required>`.
    
    This patch reverts the hack added in bug 1584963, instead, makes
    `TextControlState::SetValueWithoutTextEditor()` notifies `TextControlElement`
    of value change before dispatching `eEditorInput` event because it may have made
    damage to the performance if `nsContentUtils::DispatchInputEvent()` updated
    **all** validity state of the event target.
    
    Differential Revision: https://phabricator.services.mozilla.com/D60828
    
    UltraBlame original commit: 2977fbf09b4125cb5ebd87541ef40bad3f6bf390

--- dom/html/TextControlState.cpp
+++ dom/html/TextControlState.cpp
@@ -3036,6 +3036,12 @@ bool TextControlState::SetValueWithoutTextEditor(
     
     if (aHandlingSetValue.GetSetValueFlags() & eSetValue_BySetUserInput) {
       MOZ_ASSERT(aHandlingSetValue.GetTextControlElement());
+
+      
+      
+      aHandlingSetValue.GetTextControlElement()->OnValueChanged(
+          true, ValueChangeKind::UserInteraction);
+
       MOZ_ASSERT(!aHandlingSetValue.GetSettingValue().IsVoid());
       DebugOnly<nsresult> rvIgnored = nsContentUtils::DispatchInputEvent(
           MOZ_KnownLive(aHandlingSetValue.GetTextControlElement()),
