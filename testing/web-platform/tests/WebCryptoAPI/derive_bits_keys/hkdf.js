f480d9f6bc3666a28964a6744ed558cafc49b74e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-28 16:37:48 +0000

    Bug 1608707 [wpt PR 21141] - WebCryptoAPI: Fix HKDF null length test, a=testonly
    Automatic update from web-platform-tests
    WebCryptoAPI: Fix HKDF null length test (#21141)
    
    The Derive Bits operation should throw an OperationError if the length
    is zero (or null, according to WebIDL). This is also indicated by the
    comment above the test case.
    
    Refs: https://github.com/nodejs/webcrypto/issues/36
    --
    
    wpt-commits: 6c9ab2cb222b30606a53052efe6938ec73266aa1
    wpt-pr: 21141
    
    UltraBlame original commit: cf211553f81f4753eb861e0eb3fb0b3d9dd0303c

--- testing/web-platform/tests/WebCryptoAPI/derive_bits_keys/hkdf.js
+++ testing/web-platform/tests/WebCryptoAPI/derive_bits_keys/hkdf.js
@@ -1,33 +1,33 @@
 
 function define_tests() {
-    
+    // May want to test prefixed implementations.
     var subtle = self.crypto.subtle;
 
-    
-    
+    // hkdf2_vectors sets up test data with the correct derivations for each
+    // test case.
     var testData = getTestData();
     var derivedKeys = testData.derivedKeys;
     var salts = testData.salts;
     var derivations = testData.derivations;
     var infos = testData.infos;
 
-    
+    // What kinds of keys can be created with deriveKey? The following:
     var derivedKeyTypes = testData.derivedKeyTypes;
 
     return setUpBaseKeys(derivedKeys)
     .then(function(allKeys) {
-        
-        
-        
-        
+        // We get several kinds of base keys. Normal ones that can be used for
+        // derivation operations, ones that lack the deriveBits usage, ones
+        // that lack the deriveKeys usage, and one key that is for the wrong
+        // algorithm (not HKDF in this case).
         var baseKeys = allKeys.baseKeys;
         var noBits = allKeys.noBits;
         var noKey = allKeys.noKey;
         var wrongKey = allKeys.wrongKey;
 
-        
-        
-        
+        // Test each combination of derivedKey size, salt size, hash function,
+        // and number of iterations. The derivations object is structured in
+        // that way, so navigate it to run tests and compare with correct results.
         Object.keys(derivations).forEach(function(derivedKeySize) {
             Object.keys(derivations[derivedKeySize]).forEach(function(saltSize) {
                 Object.keys(derivations[derivedKeySize][saltSize]).forEach(function(hashName) {
@@ -35,7 +35,7 @@ function define_tests() {
                         var testName = derivedKeySize + " derivedKey, " + saltSize + " salt, " + hashName + ", with " + infoSize + " info";
                         var algorithm = {name: "HKDF", salt: salts[saltSize], info: infos[infoSize], hash: hashName};
 
-                        
+                        // Check for correct deriveBits result
                         subsetTest(promise_test, function(test) {
                             return subtle.deriveBits(algorithm, baseKeys[derivedKeySize], 256)
                             .then(function(derivation) {
@@ -45,7 +45,7 @@ function define_tests() {
                             });
                         }, testName);
 
-                        
+                        // 0 length (OperationError)
                         subsetTest(promise_test, function(test) {
                             return subtle.deriveBits(algorithm, baseKeys[derivedKeySize], 0)
                             .then(function(derivation) {
@@ -55,8 +55,8 @@ function define_tests() {
                             });
                         }, testName + " with 0 length");
 
-                        
-                        
+                        // Check for correct deriveKey results for every kind of
+                        // key that can be created by the deriveKeys operation.
                         derivedKeyTypes.forEach(function(derivedKeyType) {
                             var testName = "Derived key of type ";
                             Object.keys(derivedKeyType.algorithm).forEach(function(prop) {
@@ -64,11 +64,11 @@ function define_tests() {
                             });
                             testName += " using " + derivedKeySize + " derivedKey, " + saltSize + " salt, " + hashName + ", with " + infoSize + " info";
 
-                            
+                            // Test the particular key derivation.
                             subsetTest(promise_test, function(test) {
                                 return subtle.deriveKey(algorithm, baseKeys[derivedKeySize], derivedKeyType.algorithm, true, derivedKeyType.usages)
                                 .then(function(key) {
-                                    
+                                    // Need to export the key to see that the correct bits were set.
                                     return subtle.exportKey("raw", key)
                                     .then(function(buffer) {
                                         assert_true(equalBuffers(buffer, derivations[derivedKeySize][saltSize][hashName][infoSize].slice(0, derivedKeyType.algorithm.length/8)), "Exported key matches correct value");
@@ -81,9 +81,9 @@ function define_tests() {
                                 });
                             }, testName);
 
-                            
+                            // Test various error conditions for deriveKey:
 
-                            
+                            // - illegal name for hash algorithm (NotSupportedError)
                             var badHash = hashName.substring(0, 3) + hashName.substring(4);
                             subsetTest(promise_test, function(test) {
                                 var badAlgorithm = {name: "HKDF", salt: salts[saltSize], hash: badHash, info: algorithm.info};
@@ -95,7 +95,7 @@ function define_tests() {
                                 });
                             }, testName + " with bad hash name " + badHash);
 
-                            
+                            // - baseKey usages missing "deriveKey" (InvalidAccessError)
                             subsetTest(promise_test, function(test) {
                                 return subtle.deriveKey(algorithm, noKey[derivedKeySize], derivedKeyType.algorithm, true, derivedKeyType.usages)
                                 .then(function(key) {
@@ -105,7 +105,7 @@ function define_tests() {
                                 });
                             }, testName + " with missing deriveKey usage");
 
-                            
+                            // - baseKey algorithm does not match HKDF (InvalidAccessError)
                             subsetTest(promise_test, function(test) {
                                 return subtle.deriveKey(algorithm, wrongKey, derivedKeyType.algorithm, true, derivedKeyType.usages)
                                 .then(function(key) {
@@ -117,9 +117,9 @@ function define_tests() {
 
                         });
 
-                        
+                        // Test various error conditions for deriveBits below:
 
-                        
+                        // missing salt (TypeError)
                         subsetTest(promise_test, function(test) {
                             return subtle.deriveBits({name: "HKDF", info: infos[infoSize], hash: hashName}, baseKeys[derivedKeySize], 0)
                             .then(function(derivation) {
@@ -129,7 +129,7 @@ function define_tests() {
                             });
                         }, testName + " with missing salt");
 
-                        
+                        // missing info (TypeError)
                         subsetTest(promise_test, function(test) {
                             return subtle.deriveBits({name: "HKDF", salt: salts[saltSize], hash: hashName}, baseKeys[derivedKeySize], 0)
                             .then(function(derivation) {
@@ -139,17 +139,17 @@ function define_tests() {
                             });
                         }, testName + " with missing info");
 
-                        
+                        // length null (OperationError)
                         subsetTest(promise_test, function(test) {
                             return subtle.deriveBits(algorithm, baseKeys[derivedKeySize], null)
                             .then(function(derivation) {
-                                assert_unreached("null length should have thrown an TypeError");
+                                assert_unreached("null length should have thrown an OperationError");
                             }, function(err) {
-                                assert_equals(err.name, "TypeError", "deriveBits with null length correctly threw OperationError: " + err.message);
+                                assert_equals(err.name, "OperationError", "deriveBits with null length correctly threw OperationError: " + err.message);
                             });
                         }, testName + " with null length");
 
-                        
+                        // length not multiple of 8 (OperationError)
                         subsetTest(promise_test, function(test) {
                             return subtle.deriveBits(algorithm, baseKeys[derivedKeySize], 44)
                             .then(function(derivation) {
@@ -159,7 +159,7 @@ function define_tests() {
                             });
                         }, testName + " with non-multiple of 8 length");
 
-                        
+                        // - illegal name for hash algorithm (NotSupportedError)
                         var badHash = hashName.substring(0, 3) + hashName.substring(4);
                         subsetTest(promise_test, function(test) {
                             var badAlgorithm = {name: "HKDF", salt: salts[saltSize], hash: badHash, info: algorithm.info};
@@ -171,7 +171,7 @@ function define_tests() {
                             });
                         }, testName + " with bad hash name " + badHash);
 
-                        
+                        // - baseKey usages missing "deriveBits" (InvalidAccessError)
                         subsetTest(promise_test, function(test) {
                             return subtle.deriveBits(algorithm, noBits[derivedKeySize], 256)
                             .then(function(derivation) {
@@ -181,7 +181,7 @@ function define_tests() {
                             });
                         }, testName + " with missing deriveBits usage");
 
-                        
+                        // - baseKey algorithm does not match HKDF (InvalidAccessError)
                         subsetTest(promise_test, function(test) {
                             return subtle.deriveBits(algorithm, wrongKey, 256)
                             .then(function(derivation) {
@@ -193,7 +193,7 @@ function define_tests() {
                     });
                 });
 
-                
+                // - legal algorithm name but not digest one (e.g., PBKDF2) (NotSupportedError)
                 var nonDigestHash = "PBKDF2";
                 Object.keys(infos).forEach(function(infoSize) {
                     var testName = derivedKeySize + " derivedKey, " + saltSize + " salt, " + nonDigestHash + ", with " + infoSize + " info";
@@ -234,9 +234,9 @@ function define_tests() {
         });
     });
 
-    
-    
-    
+    // Deriving bits and keys requires starting with a base key, which is created
+    // by importing a derivedKey. setUpBaseKeys returns a promise that yields the
+    // necessary base keys.
     function setUpBaseKeys(derivedKeys) {
         var promises = [];
 
