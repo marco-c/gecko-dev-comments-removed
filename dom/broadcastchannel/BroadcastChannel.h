0afa40fe5cc440c7de56b5a1d55bacfee26850fa
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-28 16:48:20 +0000

    Bug 1610020 - BroadcastChannel + wasm - part 4 - BroadcastChannel connected to RefMessageBodyService, r=smaug
    Differential Revision: https://phabricator.services.mozilla.com/D60320
    
    UltraBlame original commit: 45ff3b0339875142e7eaa1039dc97cab9f94f9a0

--- dom/broadcastchannel/BroadcastChannel.h
+++ dom/broadcastchannel/BroadcastChannel.h
@@ -25,6 +25,7 @@ namespace dom {
 
 class BroadcastChannelChild;
 class BroadcastChannelMessage;
+class RefMessageBodyService;
 class WorkerRef;
 
 class BroadcastChannel final : public DOMEventTargetHelper {
@@ -57,7 +58,8 @@ class BroadcastChannel final : public DOMEventTargetHelper {
   void Shutdown();
 
  private:
-  BroadcastChannel(nsIGlobalObject* aGlobal, const nsAString& aChannel);
+  BroadcastChannel(nsIGlobalObject* aGlobal, const nsAString& aChannel,
+                   const nsID& aPortUUID);
 
   ~BroadcastChannel();
 
@@ -67,11 +69,17 @@ class BroadcastChannel final : public DOMEventTargetHelper {
 
   RefPtr<BroadcastChannelChild> mActor;
 
+  RefPtr<RefMessageBodyService> mRefMessageBodyService;
+
   RefPtr<WorkerRef> mWorkerRef;
 
   nsString mChannel;
 
   enum { StateActive, StateClosed } mState;
+
+  
+  
+  nsID mPortUUID;
 };
 
 }  
