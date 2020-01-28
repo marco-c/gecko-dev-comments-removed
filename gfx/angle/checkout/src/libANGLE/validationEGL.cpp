2ac75f6577c757c995b3a57118e702d31527ce85
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-28 16:36:26 +0000

    Bug 1608235 - [ANGLE] Validate `context` before use. r=sotaro
    Differential Revision: https://phabricator.services.mozilla.com/D60790
    
    UltraBlame original commit: b09849127a4f53a858ae0bfcba1343e5bd41458f

--- gfx/angle/checkout/src/libANGLE/validationEGL.cpp
+++ gfx/angle/checkout/src/libANGLE/validationEGL.cpp
@@ -2628,6 +2628,8 @@ Error ValidateStreamConsumerGLTextureExternalAttribsNV(const Display *display,
         return EglBadAccess() << "Stream consumer extension not active";
     }
 
+    ANGLE_TRY(ValidateContext(display, context));
+
     
     
     
@@ -2640,13 +2642,6 @@ Error ValidateStreamConsumerGLTextureExternalAttribsNV(const Display *display,
     {
         return EglBadStream() << "Invalid stream";
     }
-
-    if (!context)
-    {
-        return EglBadAccess() << "No GL context current to calling thread.";
-    }
-
-    ANGLE_TRY(ValidateContext(display, context));
 
     if (stream->getState() != EGL_STREAM_STATE_CREATED_KHR)
     {
