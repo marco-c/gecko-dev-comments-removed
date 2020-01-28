1352adfa07c2301dbb261b910fbd9333871de637
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-28 16:50:14 +0000

    Bug 1611238 - Enforce eval restrictions in the Parent Process/System Context r=ckerschb
    Differential Revision: https://phabricator.services.mozilla.com/D60880
    
    UltraBlame original commit: 25d7b70112bf032689b8241f3910d2a53b285db1

--- dom/security/nsContentSecurityUtils.cpp
+++ dom/security/nsContentSecurityUtils.cpp
@@ -467,15 +467,7 @@ bool nsContentSecurityUtils::IsEvalAllowed(JSContext* cx,
       fileName.get(), NS_ConvertUTF16toUTF8(aScript).get());
 #endif
 
-#if defined(RELEASE_OR_BETA) && !defined(EARLY_BETA_OR_EARLIER)
-  
-  
-  
-  
-  return false;
-#else
   return true;
-#endif
 }
 
 
