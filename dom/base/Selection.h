d5d2631ee636631bd0ece038860a816ad29ba5d5
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-28 16:26:40 +0000

    Bug 1609662: part 10) Update documentation of `Selection::MaybeAddTableCellRange`. r=smaug
    Differential Revision: https://phabricator.services.mozilla.com/D60134
    
    UltraBlame original commit: 117454131940c66d03dfd389d571954af9ecc0bb

--- dom/base/Selection.h
+++ dom/base/Selection.h
@@ -730,6 +730,11 @@ class Selection final : public nsSupportsWeakReference,
   nsresult GetTableCellLocationFromRange(nsRange* aRange,
                                          TableSelection* aSelectionType,
                                          int32_t* aRow, int32_t* aCol);
+
+  
+
+
+
   nsresult MaybeAddTableCellRange(nsRange* aRange, bool* aDidAddRange,
                                   int32_t* aOutIndex);
 
