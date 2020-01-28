86ca7c567e673de642d327818ee0a0934e32a17a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-28 16:50:23 +0000

    Bug 1609990 - AgentCluster comparison in MessagePort and BroadcastChannel - part 2 - MessageDataType, r=smaug
    Differential Revision: https://phabricator.services.mozilla.com/D60428
    
    UltraBlame original commit: c95aac908332b288ce44940afe73da6afc6dff20

--- dom/broadcastchannel/BroadcastChannelService.cpp
+++ dom/broadcastchannel/BroadcastChannelService.cpp
@@ -96,8 +96,9 @@ void BroadcastChannelService::PostMessage(BroadcastChannelParent* aParent,
 
   
   nsTArray<RefPtr<BlobImpl>> blobImpls;
-  if (aData.type() == MessageData::TClonedMessageData) {
-    const nsTArray<IPCBlob>& blobs = aData.get_ClonedMessageData().blobs();
+  if (aData.data().type() == MessageDataType::TClonedMessageData) {
+    const nsTArray<IPCBlob>& blobs =
+        aData.data().get_ClonedMessageData().blobs();
     if (!blobs.IsEmpty()) {
       blobImpls.SetCapacity(blobs.Length());
 
@@ -127,10 +128,11 @@ void BroadcastChannelService::PostMessage(BroadcastChannelParent* aParent,
 
     
     MessageData newData(aData);
-    MOZ_ASSERT(newData.type() == aData.type());
+    MOZ_ASSERT(newData.data().type() == aData.data().type());
 
     if (!blobImpls.IsEmpty()) {
-      nsTArray<IPCBlob>& newBlobImpls = newData.get_ClonedMessageData().blobs();
+      nsTArray<IPCBlob>& newBlobImpls =
+          newData.data().get_ClonedMessageData().blobs();
       MOZ_ASSERT(blobImpls.Length() == newBlobImpls.Length());
 
       
@@ -147,9 +149,9 @@ void BroadcastChannelService::PostMessage(BroadcastChannelParent* aParent,
   }
 
   
-  if (aData.type() == MessageData::TRefMessageData) {
+  if (aData.data().type() == MessageDataType::TRefMessageData) {
     Unused << aParent->SendRefMessageDelivered(
-        aData.get_RefMessageData().uuid(), selectedActorsOnSamePid);
+        aData.data().get_RefMessageData().uuid(), selectedActorsOnSamePid);
   }
 }
 
