2f5ed83254052f2be70324494972436e943da79b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-28 16:37:03 +0000

    Bug 1609402 - Improve flakey test that relied on being able to allocate 1MB of GC things in under 5 seconds r=sfink
    Tests that involve timing are often problematic.  Here we're checking that hitting the heap limit inside the five second time limit we set for skipping last ditch GCs doesn't trigger a GC, but allocating enough to hit this limit can take longer than five seconds on a busy system.
    
    The patch makes the max heap size much smaller and so reduces the possibility that that this can happen.
    
    Differential Revision: https://phabricator.services.mozilla.com/D60655
    
    UltraBlame original commit: 86c17304b4ad481b4e2ad4def4b44b4f759ef563

--- js/src/jit-test/tests/gc/bug-1505622.js
+++ js/src/jit-test/tests/gc/bug-1505622.js
@@ -22,7 +22,9 @@ function allocUntilFail() {
 gczeal(0);
 
 
-gcparam("maxBytes", 1024 * 1024);
+gc();
+let currentSize = gcparam("gcBytes");
+gcparam("maxBytes", currentSize + 16 * 1024);
 
 
 gcparam("minLastDitchGCPeriod", 5);
