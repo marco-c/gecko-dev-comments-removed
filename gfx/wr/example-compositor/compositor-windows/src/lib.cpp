55a60a305367a4da24e682ce46d90d0081e969e4
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-28 16:35:33 +0000

    Bug 1610949 - Only use WS_EX_NOREDIRECTIONBITMAP when using DirectComposition. r=gw
    Differential Revision: https://phabricator.services.mozilla.com/D60744
    
    UltraBlame original commit: 49b97ea7f14de57a67c434a6aae5217b21f498a2

--- gfx/wr/example-compositor/compositor-windows/src/lib.cpp
+++ gfx/wr/example-compositor/compositor-windows/src/lib.cpp
@@ -1,6 +1,6 @@
-
-
-
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #define UNICODE
 
@@ -38,12 +38,12 @@ enum SyncMode {
     Query = 4,
 };
 
-
+// The OS compositor representation of a picture cache tile.
 struct Tile {
 #ifndef USE_VIRTUAL_SURFACES
-    
+    // Represents the underlying DirectComposition surface texture that gets drawn into.
     IDCompositionSurface *pSurface;
-    
+    // Represents the node in the visual tree that defines the properties of this tile (clip, position etc).
     IDCompositionVisual2 *pVisual;
 #endif
 };
@@ -84,14 +84,14 @@ struct CachedFrameBuffer {
 };
 
 struct Window {
-    
+    // Win32 window details
     HWND hWnd;
     HINSTANCE hInstance;
     bool enable_compositor;
     RECT client_rect;
     SyncMode sync_mode;
 
-    
+    // Main interfaces to D3D11 and DirectComposition
     ID3D11Device *pD3D11Device;
     IDCompositionDesktopDevice *pDCompDevice;
     IDCompositionTarget *pDCompTarget;
@@ -99,30 +99,30 @@ struct Window {
     ID3D11Query *pQueries[NUM_QUERIES];
     int current_query;
 
-    
+    // ANGLE interfaces that wrap the D3D device
     EGLDeviceEXT EGLDevice;
     EGLDisplay EGLDisplay;
     EGLContext EGLContext;
     EGLConfig config;
-    
+    // Framebuffer surface for debug mode when we are not using DC
     EGLSurface fb_surface;
 
-    
+    // The currently bound surface, valid during bind() and unbind()
     IDCompositionSurface *pCurrentSurface;
     EGLImage mEGLImage;
     GLuint mColorRBO;
 
-    
-    
+    // The root of the DC visual tree. Nothing is drawn on this, but
+    // all child tiles are parented to here.
     IDCompositionVisual2 *pRoot;
     IDCompositionVisualDebug *pVisualDebug;
     std::vector<CachedFrameBuffer> mFrameBuffers;
 
-    
+    // Maintain list of layer state between frames to avoid visual tree rebuild.
     std::vector<uint64_t> mCurrentLayers;
     std::vector<uint64_t> mPrevLayers;
 
-    
+    // Maps WR surface IDs to each OS surface
     std::unordered_map<uint64_t, Surface> surfaces;
 };
 
@@ -131,7 +131,7 @@ static const wchar_t *CLASS_NAME = L"WR DirectComposite";
 static GLuint GetOrCreateFbo(Window *window, int aWidth, int aHeight) {
     GLuint fboId = 0;
 
-    
+    // Check if we have a cached FBO with matching dimensions
     for (auto it = window->mFrameBuffers.begin(); it != window->mFrameBuffers.end(); ++it) {
         if (it->width == aWidth && it->height == aHeight) {
             fboId = it->fboId;
@@ -139,23 +139,23 @@ static GLuint GetOrCreateFbo(Window *window, int aWidth, int aHeight) {
         }
     }
 
-    
+    // If not, create a new FBO with attached depth buffer
     if (fboId == 0) {
-        
+        // Create the depth buffer
         GLuint depthRboId;
         glGenRenderbuffers(1, &depthRboId);
         glBindRenderbuffer(GL_RENDERBUFFER, depthRboId);
         glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT24,
                               aWidth, aHeight);
 
-        
+        // Create the framebuffer and attach the depth buffer to it
         glGenFramebuffers(1, &fboId);
         glBindFramebuffer(GL_DRAW_FRAMEBUFFER, fboId);
         glFramebufferRenderbuffer(GL_DRAW_FRAMEBUFFER,
                                   GL_DEPTH_ATTACHMENT,
                                   GL_RENDERBUFFER, depthRboId);
 
-        
+        // Store this in the cache for future calls.
         CachedFrameBuffer frame_buffer_info;
         frame_buffer_info.width = aWidth;
         frame_buffer_info.height = aHeight;
@@ -184,7 +184,7 @@ static LRESULT CALLBACK WndProc(
 
 extern "C" {
     Window *com_dc_create_window(int width, int height, bool enable_compositor, SyncMode sync_mode) {
-        
+        // Create a simple Win32 window
         Window *window = new Window;
         window->hInstance = GetModuleHandle(NULL);
         window->enable_compositor = enable_compositor;
@@ -218,14 +218,17 @@ extern "C" {
         UINT window_height = static_cast<UINT>(ceil(float(window_rect.bottom - window_rect.top) * dpiY / 96.f));
 
         LPCWSTR name;
+        DWORD style;
         if (enable_compositor) {
             name = L"example-compositor (DirectComposition)";
+            style = WS_EX_NOREDIRECTIONBITMAP;
         } else {
             name = L"example-compositor (Simple)";
+            style = 0;
         }
 
         window->hWnd = CreateWindowEx(
-            WS_EX_NOREDIRECTIONBITMAP,
+            style,
             CLASS_NAME,
             name,
             WS_OVERLAPPEDWINDOW,
@@ -243,7 +246,7 @@ extern "C" {
         UpdateWindow(window->hWnd);
         GetClientRect(window->hWnd, &window->client_rect);
 
-        
+        // Create a D3D11 device
         D3D_FEATURE_LEVEL featureLevelSupported;
         HRESULT hr = D3D11CreateDevice(
             nullptr,
@@ -271,7 +274,7 @@ extern "C" {
         hr = window->pD3D11Device->QueryInterface(&window->pDXGIDevice);
         assert(SUCCEEDED(hr));
 
-        
+        // Create a DirectComposition device
         hr = DCompositionCreateDevice2(
             window->pDXGIDevice,
             __uuidof(IDCompositionDesktopDevice),
@@ -279,7 +282,7 @@ extern "C" {
         );
         assert(SUCCEEDED(hr));
 
-        
+        // Create a DirectComposition target for a Win32 window handle
         hr = window->pDCompDevice->CreateTargetForHwnd(
             window->hWnd,
             TRUE,
@@ -287,7 +290,7 @@ extern "C" {
         );
         assert(SUCCEEDED(hr));
 
-        
+        // Create an ANGLE EGL device that wraps D3D11
         window->EGLDevice = eglCreateDeviceANGLE(
             EGL_D3D11_DEVICE_ANGLE,
             window->pD3D11Device,
@@ -350,7 +353,7 @@ extern "C" {
             EGL_NONE
         };
 
-        
+        // Create an EGL context that can be used for drawing
         window->EGLContext = eglCreateContext(
             window->EGLDisplay,
             window->config,
@@ -358,7 +361,7 @@ extern "C" {
             ctx_attribs
         );
 
-        
+        // Create the root of the DirectComposition visual tree
         hr = window->pDCompDevice->CreateVisual(&window->pRoot);
         assert(SUCCEEDED(hr));
         hr = window->pDCompTarget->SetRoot(window->pRoot);
@@ -370,8 +373,8 @@ extern "C" {
         );
         assert(SUCCEEDED(hr));
 
-        
-        
+        // Uncomment this to see redraw regions during composite
+        // window->pVisualDebug->EnableRedrawRegions();
 
         EGLBoolean ok = eglMakeCurrent(
             window->EGLDisplay,
@@ -422,7 +425,7 @@ extern "C" {
     }
 
     bool com_dc_tick(Window *) {
-        
+        // Check and dispatch the windows event loop
         MSG msg;
         while (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
             if (msg.message == WM_QUIT) {
@@ -437,7 +440,7 @@ extern "C" {
     }
 
     void com_dc_swap_buffers(Window *window) {
-        
+        // If not using DC mode, then do a normal EGL swap buffers.
         if (window->fb_surface != EGL_NO_SURFACE) {
             switch (window->sync_mode) {
                 case SyncMode::None:
@@ -447,7 +450,7 @@ extern "C" {
                     eglSwapInterval(window->EGLDisplay, 1);
                     break;
                 default:
-                    assert(false);  
+                    assert(false);  // unexpected vsync mode for simple compositor.
                     break;
             }
 
@@ -463,16 +466,16 @@ extern "C" {
                     DwmFlush();
                     break;
                 case SyncMode::Query:
-                    
+                    // todo!!!!
                     break;
                 default:
-                    assert(false);  
+                    assert(false);  // unexpected vsync mode for native compositor
                     break;
             }
         }
     }
 
-    
+    // Create a new DC surface
     void com_dc_create_surface(
         Window *window,
         uint64_t id,
@@ -487,7 +490,7 @@ extern "C" {
         surface.tile_height = tile_height;
         surface.is_opaque = is_opaque;
 
-        
+        // Create the visual node in the DC tree that stores properties
         HRESULT hr = window->pDCompDevice->CreateVisual(&surface.pVisual);
         assert(SUCCEEDED(hr));
 
@@ -503,7 +506,7 @@ extern "C" {
         );
         assert(SUCCEEDED(hr));
 
-        
+        // Bind the surface memory to this visual
         hr = surface.pVisual->SetContent(surface.pVirtualSurface);
         assert(SUCCEEDED(hr));
 #endif
@@ -526,7 +529,7 @@ extern "C" {
         Tile tile;
 
 #ifndef USE_VIRTUAL_SURFACES
-        
+        // Create the video memory surface.
         DXGI_ALPHA_MODE alpha_mode = surface.is_opaque ? DXGI_ALPHA_MODE_IGNORE : DXGI_ALPHA_MODE_PREMULTIPLIED;
         HRESULT hr = window->pDCompDevice->CreateSurface(
             surface.tile_width,
@@ -537,15 +540,15 @@ extern "C" {
         );
         assert(SUCCEEDED(hr));
 
-        
+        // Create the visual node in the DC tree that stores properties
         hr = window->pDCompDevice->CreateVisual(&tile.pVisual);
         assert(SUCCEEDED(hr));
 
-        
+        // Bind the surface memory to this visual
         hr = tile.pVisual->SetContent(tile.pSurface);
         assert(SUCCEEDED(hr));
 
-        
+        // Place the visual in local-space of this surface
         float offset_x = (float) (x * surface.tile_width);
         float offset_y = (float) (y * surface.tile_height);
         tile.pVisual->SetOffsetX(offset_x);
@@ -596,7 +599,7 @@ extern "C" {
 #ifdef USE_VIRTUAL_SURFACES
         surface.pVirtualSurface->Release();
 #else
-        
+        // Release the video memory and visual in the tree
         for (auto tile_it=surface.tiles.begin() ; tile_it != surface.tiles.end() ; ++tile_it) {
             tile_it->second.pSurface->Release();
             tile_it->second.pVisual->Release();
@@ -607,7 +610,7 @@ extern "C" {
         window->surfaces.erase(id);
     }
 
-    
+    // Bind a DC surface to allow issuing GL commands to it
     GLuint com_dc_bind_surface(
         Window *window,
         uint64_t surface_id,
@@ -627,9 +630,9 @@ extern "C" {
         assert(surface.tiles.count(key) == 1);
         Tile &tile = surface.tiles[key];
 
-        
-        
-        
+        // Inform DC that we want to draw on this surface. DC uses texture
+        // atlases when the tiles are small. It returns an offset where the
+        // client code must draw into this surface when this happens.
         RECT update_rect;
         update_rect.left = dirty_x0;
         update_rect.top = dirty_y0;
@@ -640,7 +643,7 @@ extern "C" {
         ID3D11Texture2D *pTexture;
         HRESULT hr;
 
-        
+        // Store the current surface for unbinding later
 #ifdef USE_VIRTUAL_SURFACES
         LONG tile_offset_x = VIRTUAL_OFFSET + tile_x * surface.tile_width;
         LONG tile_offset_y = VIRTUAL_OFFSET + tile_y * surface.tile_height;
@@ -667,8 +670,8 @@ extern "C" {
         window->pCurrentSurface = tile.pSurface;
 #endif
 
-        
-        
+        // DC includes the origin of the dirty / update rect in the draw offset,
+        // undo that here since WR expects it to be an absolute offset.
         assert(SUCCEEDED(hr));
         offset.x -= dirty_x0;
         offset.y -= dirty_y0;
@@ -676,7 +679,7 @@ extern "C" {
         *x_offset = offset.x;
         *y_offset = offset.y;
 
-        
+        // Construct an EGLImage wrapper around the D3D texture for ANGLE.
         const EGLAttrib attribs[] = { EGL_NONE };
         window->mEGLImage = eglCreateImage(
             window->EGLDisplay,
@@ -686,34 +689,34 @@ extern "C" {
             attribs
         );
 
-        
+        // Get the current FBO and RBO id, so we can restore them later
         GLint currentFboId, currentRboId;
         glGetIntegerv(GL_DRAW_FRAMEBUFFER_BINDING, &currentFboId);
         glGetIntegerv(GL_RENDERBUFFER_BINDING, &currentRboId);
 
-        
+        // Create a render buffer object that is backed by the EGL image.
         glGenRenderbuffers(1, &window->mColorRBO);
         glBindRenderbuffer(GL_RENDERBUFFER, window->mColorRBO);
         glEGLImageTargetRenderbufferStorageOES(GL_RENDERBUFFER, window->mEGLImage);
 
-        
+        // Get or create an FBO for the specified dimensions
         GLuint fboId = GetOrCreateFbo(window, desc.Width, desc.Height);
 
-        
+        // Attach the new renderbuffer to the FBO
         glBindFramebuffer(GL_DRAW_FRAMEBUFFER, fboId);
         glFramebufferRenderbuffer(GL_DRAW_FRAMEBUFFER,
                                   GL_COLOR_ATTACHMENT0,
                                   GL_RENDERBUFFER,
                                   window->mColorRBO);
 
-        
+        // Restore previous FBO and RBO bindings
         glBindFramebuffer(GL_DRAW_FRAMEBUFFER, currentFboId);
         glBindRenderbuffer(GL_RENDERBUFFER, currentRboId);
 
         return fboId;
     }
 
-    
+    // Unbind a currently bound DC surface
     void com_dc_unbind_surface(Window *window) {
         HRESULT hr = window->pCurrentSurface->EndDraw();
         assert(SUCCEEDED(hr));
@@ -728,7 +731,7 @@ extern "C" {
     void com_dc_begin_transaction(Window *) {
     }
 
-    
+    // Add a DC surface to the visual tree. Called per-frame to build the composition.
     void com_dc_add_surface(
         Window *window,
         uint64_t id,
@@ -742,8 +745,8 @@ extern "C" {
         Surface surface = window->surfaces[id];
         window->mCurrentLayers.push_back(id);
 
-        
-        
+        // Place the visual - this changes frame to frame based on scroll position
+        // of the slice.
         float offset_x = (float) (x + window->client_rect.left);
         float offset_y = (float) (y + window->client_rect.top);
 #ifdef USE_VIRTUAL_SURFACES
@@ -753,8 +756,8 @@ extern "C" {
         surface.pVisual->SetOffsetX(offset_x);
         surface.pVisual->SetOffsetY(offset_y);
 
-        
-        
+        // Set the clip rect - converting from world space to the pre-offset space
+        // that DC requires for rectangle clips.
         D2D_RECT_F clip_rect;
         clip_rect.left = clip_x - offset_x;
         clip_rect.top = clip_y - offset_y;
@@ -763,7 +766,7 @@ extern "C" {
         surface.pVisual->SetClip(clip_rect);
     }
 
-    
+    // Finish the composition transaction, telling DC to composite
     void com_dc_end_transaction(Window *window) {
         bool same = window->mPrevLayers == window->mCurrentLayers;
 
@@ -774,8 +777,8 @@ extern "C" {
             for (auto it = window->mCurrentLayers.begin(); it != window->mCurrentLayers.end(); ++it) {
                 Surface &surface = window->surfaces[*it];
 
-                
-                
+                // Add this visual as the last element in the visual tree (z-order is implicit,
+                // based on the order tiles are added).
                 hr = window->pRoot->AddVisual(
                     surface.pVisual,
                     FALSE,
@@ -792,7 +795,7 @@ extern "C" {
         assert(SUCCEEDED(hr));
     }
 
-    
+    // Get a pointer to an EGL symbol
     void *com_dc_get_proc_address(const char *name) {
         return eglGetProcAddress(name);
     }
