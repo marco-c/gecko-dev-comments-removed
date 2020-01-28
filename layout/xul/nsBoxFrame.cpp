57b2df4738a922cef8591a8cd98afa76deaee68b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-28 16:39:56 +0000

    Bug 1607575 - Remove support for the XUL ordinal attribute r=bgrins,bz
    This removes nsTreeColumns::RestoreNaturalOrder, which requires the ordinal attribute to function. It only has one consumer: toolkit/content/widgets/tree.js. The call is removed from that consumer in this patch. This will break column ordering in MozTree's, which is fixed by a later changeset in this stack.
    
    Differential Revision: https://phabricator.services.mozilla.com/D59761
    
    UltraBlame original commit: 5a23d2af7031dfc3a63d07f231cd0873b8438752

--- layout/xul/nsBoxFrame.cpp
+++ layout/xul/nsBoxFrame.cpp
@@ -887,20 +887,6 @@ nsresult nsBoxFrame::AttributeChanged(int32_t aNameSpaceID, nsAtom* aAttribute,
 
     PresShell()->FrameNeedsReflow(this, IntrinsicDirty::StyleChange,
                                   NS_FRAME_IS_DIRTY);
-  } else if (aAttribute == nsGkAtoms::ordinal) {
-    nsIFrame* parent = GetParentXULBox(this);
-    
-    
-    
-    
-    
-    if (parent && !(GetStateBits() & NS_FRAME_OUT_OF_FLOW) &&
-        StyleDisplay()->mDisplay != mozilla::StyleDisplay::MozPopup) {
-      parent->XULRelayoutChildAtOrdinal(this);
-      
-      PresShell()->FrameNeedsReflow(parent, IntrinsicDirty::StyleChange,
-                                    NS_FRAME_IS_DIRTY);
-    }
   }
   
   
@@ -1050,7 +1036,8 @@ static bool IsBoxOrdinalLEQ(nsIFrame* aFrame1, nsIFrame* aFrame2) {
   
   nsIFrame* aRealFrame1 = nsPlaceholderFrame::GetRealFrameFor(aFrame1);
   nsIFrame* aRealFrame2 = nsPlaceholderFrame::GetRealFrameFor(aFrame2);
-  return aRealFrame1->GetXULOrdinal() <= aRealFrame2->GetXULOrdinal();
+  return aRealFrame1->StyleXUL()->mBoxOrdinal <=
+         aRealFrame2->StyleXUL()->mBoxOrdinal;
 }
 
 void nsBoxFrame::CheckBoxOrder() {
@@ -1076,13 +1063,13 @@ nsresult nsBoxFrame::LayoutChildAt(nsBoxLayoutState& aState, nsIFrame* aBox,
 }
 
 nsresult nsBoxFrame::XULRelayoutChildAtOrdinal(nsIFrame* aChild) {
-  int32_t ord = aChild->GetXULOrdinal();
+  int32_t ord = aChild->StyleXUL()->mBoxOrdinal;
 
   nsIFrame* child = mFrames.FirstChild();
   nsIFrame* newPrevSib = nullptr;
 
   while (child) {
-    if (ord < child->GetXULOrdinal()) {
+    if (ord < child->StyleXUL()->mBoxOrdinal) {
       break;
     }
 
