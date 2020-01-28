1f2cfe6190141a79157502ac88c67c512f4f3c4f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-28 16:54:02 +0000

    Bug 1611173 - Remove support for non-nsISupports XPIDL interfaces. r=nika
    It used to be that some XPIDL interfaces were allowed to be non-nsISupports,
    and were only used to hold a bunch of constants. It appears that we've now
    had enough de-COM work happen that there are no longer any, so we can remove
    support for this.
    
    Differential Revision: https://phabricator.services.mozilla.com/D61008
    
    UltraBlame original commit: 27bd6308aff76b6700fd088ff52fa2b9d4d02b48

--- xpcom/idl-parser/xpidl/rust_macros.py
+++ xpcom/idl-parser/xpidl/rust_macros.py
@@ -51,12 +51,7 @@ def write_interface(iface, fd):
     if iface.namemap is None:
         raise Exception("Interface was not resolved.")
 
-    
-    
-    if iface.base is None and iface.name != "nsISupports":
-        assert len([m for m in iface.members
-                    if type(m) == xpidl.Attribute or type(m) == xpidl.Method]) == 0
-        return
+    assert iface.base or (iface.name == "nsISupports")
 
     base = 'Some("%s")' % iface.base if iface.base is not None else 'None'
     try:
