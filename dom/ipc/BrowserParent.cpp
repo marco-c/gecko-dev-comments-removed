9cd3aa6936e0cd59df5140f064d9c86be259c960
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-28 16:35:56 +0000

    Bug 1607364 - CrashReporting API r=baku
    Implement Crash Report for Reporting API.
    
    Differential Revision: https://phabricator.services.mozilla.com/D58053
    
    UltraBlame original commit: 41a9a81b0127253c1d6af892c98d4583de9a21a1

--- dom/ipc/BrowserParent.cpp
+++ dom/ipc/BrowserParent.cpp
@@ -100,6 +100,7 @@
 #include "gfxUtils.h"
 #include "nsILoginManagerAuthPrompter.h"
 #include "nsPIWindowRoot.h"
+#include "nsReadableUtils.h"
 #include "nsIAuthPrompt2.h"
 #include "gfxDrawable.h"
 #include "ImageOps.h"
@@ -114,6 +115,7 @@
 #include "mozilla/dom/CanonicalBrowsingContext.h"
 #include "MMPrinter.h"
 #include "SessionStoreFunctions.h"
+#include "mozilla/dom/CrashReport.h"
 
 #ifdef XP_WIN
 #  include "mozilla/plugins/PluginWidgetParent.h"
@@ -704,6 +706,32 @@ void BrowserParent::ActorDestroy(ActorDestroyReason why) {
   
   
   BrowserParent::PopFocus(this);
+
+  if (why == AbnormalShutdown) {
+    
+    if (StaticPrefs::dom_reporting_crash_enabled()) {
+      nsCOMPtr<nsIPrincipal> principal = GetContentPrincipal();
+
+      if (principal) {
+        nsAutoCString crash_reason;
+        CrashReporter::GetAnnotation(OtherPid(),
+                                     CrashReporter::Annotation::MozCrashReason,
+                                     crash_reason);
+        
+        
+        bool is_oom = false;
+        if (crash_reason == "OOM" || crash_reason == "OOM!" ||
+            StringBeginsWith(crash_reason,
+                             NS_LITERAL_CSTRING("[unhandlable oom]")) ||
+            StringBeginsWith(crash_reason,
+                             NS_LITERAL_CSTRING("Unhandlable OOM"))) {
+          is_oom = true;
+        }
+
+        CrashReport::Deliver(principal, is_oom);
+      }
+    }
+  }
 
   
   
