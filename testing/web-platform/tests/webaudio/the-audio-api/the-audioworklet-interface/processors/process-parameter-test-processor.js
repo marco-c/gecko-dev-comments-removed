bbdefe9875e3aa4d90882332b0110f796d6cf2b2
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-28 17:05:54 +0000

    Bug 1606148 test AudioWorkletNode with 0 inputs and >1 outputs r=padenot
    Differential Revision: https://phabricator.services.mozilla.com/D59191
    
    UltraBlame original commit: 9bca5d9745aa75ce3acdb294d695c288b7b2a9c0

---
+++ testing/web-platform/tests/webaudio/the-audio-api/the-audioworklet-interface/processors/process-parameter-test-processor.js
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+
+class ProcessParameterTestProcessor extends AudioWorkletProcessor {
+  process(inputs, outputs) {
+    this.port.postMessage({
+      inputs: inputs,
+      outputs: outputs
+    });
+    return false;
+  }
+}
+
+registerProcessor('process-parameter-test', ProcessParameterTestProcessor);
