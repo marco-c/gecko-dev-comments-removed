e42cfa853fa6d2bd33f1b02c7d562e84500b9e32
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-28 16:35:51 +0000

    Bug 1608592 - Remove MINIDUMP_SAVEPATH environment variable from android tasks; r=bc
    MINIDUMP_SAVEPATH does nothing and can be safely removed. The correct variable,
    MINIDUMP_SAVE_PATH is already set in the Android scripts, such as
    https://searchfox.org/mozilla-central/rev/9b4b41b95cbcda63f565bdc24411e15248f91d83/testing/mozharness/scripts/android_emulator_unittest.py#412
    so no other changes are required.
    
    Differential Revision: https://phabricator.services.mozilla.com/D59923
    
    UltraBlame original commit: 231c45e27a37de9f1c2e2bcec5c8c2405e2201d7

--- testing/mozharness/configs/android/wrench.py
+++ testing/mozharness/configs/android/wrench.py
@@ -24,6 +24,5 @@ config = {
     "env": {
         "DISPLAY": ":0.0",
         "PATH": "%(PATH)s:%(abs_work_dir)s/android-sdk-linux/emulator:%(abs_work_dir)s/android-sdk-linux/tools:%(abs_work_dir)s/android-sdk-linux/platform-tools",
-        "MINIDUMP_SAVEPATH": "%(abs_work_dir)s/minidumps",
     },
 }
