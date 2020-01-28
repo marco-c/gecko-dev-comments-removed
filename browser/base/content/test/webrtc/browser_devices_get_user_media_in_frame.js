55862348ec365f4467b907bbd9013c86a7ce9670
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-28 16:59:46 +0000

    Bug 1611454 - browser_devices_get_user_media_in_frame.js: enable dom.security.featurePolicy.enabled and permissions.delegation.enabled. r=mjf
    These prefs got disabled for Late Beta and Release in bug 1611454 but the test
    uses the feature (access to media devices from frame).
    
    Differential Revision: https://phabricator.services.mozilla.com/D61093
    
    UltraBlame original commit: df59b74d33d74c9e4fda377616b20c40da832a71

--- browser/base/content/test/webrtc/browser_devices_get_user_media_in_frame.js
+++ browser/base/content/test/webrtc/browser_devices_get_user_media_in_frame.js
@@ -2,6 +2,13 @@
 
 
 
+SpecialPowers.pushPrefEnv({
+  set: [
+    ["dom.security.featurePolicy.enabled", true],
+    ["permissions.delegation.enabled", true],
+  ],
+});
+
 let gShouldObserveSubframes;
 
 var gTests = [
