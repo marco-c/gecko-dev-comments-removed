fb2fddd4c2a0d29dd47fa430ae9482f5b2da40ad
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-02-12 01:15:31 +0000

    Backed out 3 changesets (bug 1608022) for causing browser_findbar.js to perma fail CLOSED TREE
    Backed out changeset b7097a9f6a16 (bug 1608022)
    Backed out changeset 853e5ec59184 (bug 1608022)
    Backed out changeset d538041cc083 (bug 1608022)
    
    UltraBlame original commit: 1ca6137e356bee0ccebb3bfa614a9bf953cc6ce0

--- browser/base/content/browser-places.js
+++ browser/base/content/browser-places.js
@@ -1396,6 +1396,29 @@ var BookmarkingUI = {
       : this.STATUS_UNSTARRED;
   },
 
+  get _starredTooltip() {
+    delete this._starredTooltip;
+    return (this._starredTooltip = this._getFormattedTooltip(
+      "starButtonOn.tooltip2"
+    ));
+  },
+
+  get _unstarredTooltip() {
+    delete this._unstarredTooltip;
+    return (this._unstarredTooltip = this._getFormattedTooltip(
+      "starButtonOff.tooltip2"
+    ));
+  },
+
+  _getFormattedTooltip(strId) {
+    let args = [];
+    let shortcut = document.getElementById(this.BOOKMARK_BUTTON_SHORTCUT);
+    if (shortcut) {
+      args.push(ShortcutUtils.prettifyShortcut(shortcut));
+    }
+    return gNavigatorBundle.getFormattedString(strId, args);
+  },
+
   onPopupShowing: function BUI_onPopupShowing(event) {
     
     if (event.target != event.currentTarget) {
@@ -1669,22 +1692,16 @@ var BookmarkingUI = {
       }
     }
 
-    if (!this.star) {
-      
-      
-      return;
-    }
-
     
-    let shortcut = document.getElementById(this.BOOKMARK_BUTTON_SHORTCUT);
-    let l10nArgs = {
-      shortcut: ShortcutUtils.prettifyShortcut(shortcut),
-    };
-    document.l10n.setAttributes(
+    for (let element of [
       this.star,
-      starred ? "urlbar-star-edit-bookmark" : "urlbar-star-add-bookmark",
-      l10nArgs
-    );
+      document.getElementById("context-bookmarkpage"),
+    ]) {
+      element.setAttribute(
+        "tooltiptext",
+        starred ? this._starredTooltip : this._unstarredTooltip
+      );
+    }
 
     Services.obs.notifyObservers(
       null,
@@ -1700,84 +1717,40 @@ var BookmarkingUI = {
   updateBookmarkPageMenuItem: function BUI_updateBookmarkPageMenuItem(
     forceReset
   ) {
-    let menuItem = document.getElementById("menu_bookmarkThisPage");
-    
-    if (!menuItem) {
+    if (!this.stringbundleset) {
       
       return;
     }
-
-    
-    if (!document.location.href.includes("browser.xhtml")) {
-      
-      document.l10n.setAttributes(menuItem, "menu-bookmark-this-page");
-      return;
-    }
-
     let isStarred = !forceReset && this._itemGuids.size > 0;
-
-    
-    
-    let menuItemL10nId = isStarred
-      ? "menu-bookmark-edit"
-      : "menu-bookmark-this-page";
-
-    
-    document.l10n.setAttributes(menuItem, menuItemL10nId);
+    let label = this.stringbundleset.getAttribute(
+      isStarred ? "string-editthisbookmark" : "string-bookmarkthispage"
+    );
 
     let panelMenuToolbarButton = document.getElementById(
       "panelMenuBookmarkThisPage"
     );
-
-    
     if (!panelMenuToolbarButton) {
+      
+      
+      document
+        .getElementById("menu_bookmarkThisPage")
+        .setAttribute("label", label);
       return;
     }
 
-    
-    document.l10n.setAttributes(panelMenuToolbarButton, menuItemL10nId);
-
-    
-    let contextItem = document.getElementById("context-bookmarkpage");
-    let shortcutElem = document.getElementById(this.BOOKMARK_BUTTON_SHORTCUT);
-
-    if (shortcutElem) {
-      let shortcut = ShortcutUtils.prettifyShortcut(shortcutElem);
-      let contextItemL10nId = isStarred
-        ? "main-context-menu-bookmark-change-with-shortcut"
-        : "main-context-menu-bookmark-add-with-shortcut";
-      let l10nArgs = { shortcut };
-      document.l10n.setAttributes(contextItem, contextItemL10nId, l10nArgs);
-    } else {
-      let contextItemL10nId = isStarred
-        ? "main-context-menu-bookmark-change"
-        : "main-context-menu-bookmark-add";
-      document.l10n.setAttributes(contextItem, contextItemL10nId);
+    for (let element of [
+      document.getElementById("menu_bookmarkThisPage"),
+      document.getElementById("context-bookmarkpage"),
+      panelMenuToolbarButton,
+    ]) {
+      element.setAttribute("label", label);
     }
 
     
-    
-    
-    
-    this._latestMenuItemL10nId = menuItemL10nId;
-    document.l10n.formatMessages([{ id: menuItemL10nId }]).then(l10n => {
-      
-      
-      
-      if (this._latestMenuItemL10nId != menuItemL10nId) {
-        return;
-      }
-
-      
-      let label = l10n[0].attributes[0].value;
-
-      
-      PageActions.actionForID(PageActions.ACTION_ID_BOOKMARK).setTitle(
-        label,
-        window
-      );
-    });
-
+    PageActions.actionForID(PageActions.ACTION_ID_BOOKMARK).setTitle(
+      label,
+      window
+    );
     this._updateStar();
   },
 
