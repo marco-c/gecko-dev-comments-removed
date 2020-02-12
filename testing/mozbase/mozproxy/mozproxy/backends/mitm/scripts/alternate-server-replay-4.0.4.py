0f81fab7518d3681231eadc3d13286d4089a42da
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-02-12 01:19:23 +0000

    Bug 1612790 - Dump domain stats r=Bebe
    This patch allows us to dump per-domain hit counts to add more insights in the
    proxy usage.
    
    Differential Revision: https://phabricator.services.mozilla.com/D61432
    
    UltraBlame original commit: 67e4d5a030424968e01d66ecd526b1be71d92a5a

--- testing/mozbase/mozproxy/mozproxy/backends/mitm/scripts/alternate-server-replay-4.0.4.py
+++ testing/mozbase/mozproxy/mozproxy/backends/mitm/scripts/alternate-server-replay-4.0.4.py
@@ -1,15 +1,16 @@
+# This file was copied from  mitmproxy/mitmproxy/addons/serverplayback.py release tag 4.0.4
+# and modified by Florin Strugariu
 
-
-
-
-
-
+# Altered features:
+# * returns 404 rather than dropping the whole HTTP/2 connection on the floor
+# * remove the replay packages that don't have any content in their response package
 from __future__ import absolute_import, print_function
 
 import os
 import json
 import hashlib
 import urllib
+from collections import defaultdict
 
 import typing
 from urllib import parse
@@ -18,7 +19,7 @@ from mitmproxy import ctx, http
 from mitmproxy import exceptions
 from mitmproxy import io
 
-
+# PATCHING AREA  - ALLOWS HTTP/2 WITH NO CERT SNIFFING
 from mitmproxy.proxy.protocol import tls
 from mitmproxy.proxy.protocol.http2 import Http2Layer, SafeH2Connection
 
@@ -42,7 +43,7 @@ tls.TlsLayer.alpn_for_client_connection = _alpn
 
 def _server_conn(self):
     if not self.server_conn.connected() and self.server_conn not in self.connections:
-        
+        # we can't use ctx.log in this layer
         print("Ignored CONNECT call on upstream server")
         return
     if self.server_conn.connected():
@@ -66,7 +67,7 @@ Http2Layer._initiate_server_conn = _server_conn
 
 def _remote_settings_changed(self, event, other_conn):
     if other_conn not in self.connections:
-        
+        # we can't use ctx.log in this layer
         print("Ignored remote settings upstream")
         return True
     new_settings = dict(
@@ -77,7 +78,7 @@ def _remote_settings_changed(self, event, other_conn):
 
 
 Http2Layer._handle_remote_settings_changed = _remote_settings_changed
-
+# END OF PATCHING
 
 
 class AlternateServerPlayback:
@@ -85,6 +86,7 @@ class AlternateServerPlayback:
         ctx.master.addons.remove(ctx.master.addons.get("serverplayback"))
         self.flowmap = {}
         self.configured = False
+        self.netlocs = defaultdict(int)
 
     def load(self, loader):
         loader.add_option(
@@ -92,6 +94,10 @@ class AlternateServerPlayback:
             typing.Sequence[str],
             [],
             "Replay server responses from a saved file.",
+        )
+        loader.add_option(
+            "upload_dir", str, "",
+            "Upload directory",
         )
 
     def load_flows(self, flows):
@@ -142,8 +148,8 @@ class AlternateServerPlayback:
         """
         r = flow.request
 
-        
-        
+        # unquote url
+        # See Bug 1509835
         _, _, path, _, query, _ = urllib.parse.urlparse(parse.unquote(r.url))
         queriesArray = urllib.parse.parse_qsl(query, keep_blank_values=True)
 
@@ -171,19 +177,30 @@ class AlternateServerPlayback:
             self.configured = True
             self.load_files(ctx.options.server_replay_files)
 
+    def done(self):
+        if not ctx.options.upload_dir:
+            return
+        netlocs = sorted(self.netlocs.items(), key=lambda item: -item[1])
+        path = os.path.join(ctx.options.upload_dir, "mitm_netlocs.log")
+        with open(path, "w") as f:
+            for url, count in netlocs:
+                f.write("%s: %d\n" % (url, count))
+
     def request(self, f):
         if self.flowmap:
+            parsed_url = urllib.parse.urlparse(parse.unquote(f.request.url))
+            self.netlocs[parsed_url.netloc] += 1
             rflow = self.next_flow(f)
             if rflow:
                 response = rflow.response.copy()
                 response.is_replay = True
-                
-                
+                # Refresh server replay responses by adjusting date, expires and
+                # last-modified headers, as well as adjusting cookie expiration.
                 response.refresh()
 
                 f.response = response
             else:
-                
+                # returns 404 rather than dropping the whole HTTP/2 connection
                 ctx.log.warn(
                     "server_playback: killed non-replay request {}".format(
                         f.request.url
