dc8e0ddadf236405628262a3f42f57d432e2b673
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-02-12 01:20:24 +0000

    Bug 1567927 - Report karma code coverage failures with existing istanbul thresholds r=dmose
    Parse coverage errors and report like other karma errors.
    
    Differential Revision: https://phabricator.services.mozilla.com/D60902
    
    UltraBlame original commit: 0dbea7129a30a1da9eb7d0b655b3f0adf97e4eb4

--- browser/components/newtab/bin/try-runner.js
+++ browser/components/newtab/bin/try-runner.js
@@ -50,6 +50,7 @@ function logStart(name) {
 function karma() {
   logStart("karma");
 
+  const errors = [];
   const { out } = execOut("npm", [
     "run",
     "testmc:unit",
@@ -63,6 +64,7 @@ function karma() {
     return false;
   }
 
+  
   let jsonContent;
   try {
     
@@ -72,27 +74,28 @@ function karma() {
     console.error("exception reading karma-run-results.json: ", ex);
     return false;
   }
-
   const results = JSON.parse(jsonContent);
-
-  const failed = results.summary.failed === 0;
-
-  let errors = [];
   
   for (let testArray in results.result) {
     let failedTests = Array.from(results.result[testArray]).filter(
       test => !test.success && !test.skipped
     );
 
-    let errs = failedTests.map(test => {
-      return `${test.suite.join(":")} ${test.description}: ${test.log[0]}`;
-    });
+    errors.push(
+      ...failedTests.map(
+        test => `${test.suite.join(":")} ${test.description}: ${test.log[0]}`
+      )
+    );
+  }
 
-    errors = errors.concat(errs);
+  
+  const coverage = out.match(/ERROR.+coverage-istanbul.+/g);
+  if (coverage) {
+    errors.push(...coverage.map(line => line.match(/Coverage.+/)[0]));
   }
 
   logErrors("karma", errors);
-  return failed;
+  return errors.length === 0;
 }
 
 function sasslint() {
