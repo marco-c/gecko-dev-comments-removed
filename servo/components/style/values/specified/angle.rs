69d7b27fdced7f9b9fe8671abc042d95d4521fbb
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-02-12 01:30:40 +0000

    Bug 1614160 - Add AngleOrPercentage to style system. r=emilio
    Differential Revision: https://phabricator.services.mozilla.com/D62158
    
    UltraBlame original commit: 8e9fe5726cf6b1c2c2ee102dce2c9934cd1ff3bf

--- servo/components/style/values/specified/angle.rs
+++ servo/components/style/values/specified/angle.rs
@@ -163,7 +163,8 @@ impl Angle {
 
 
 
-enum AllowUnitlessZeroAngle {
+#[allow(missing_docs)]
+pub enum AllowUnitlessZeroAngle {
     Yes,
     No,
 }
@@ -203,7 +204,7 @@ impl Angle {
         Self::parse_internal(context, input, AllowUnitlessZeroAngle::Yes)
     }
 
-    fn parse_internal<'i, 't>(
+    pub(super) fn parse_internal<'i, 't>(
         context: &ParserContext,
         input: &mut Parser<'i, 't>,
         allow_unitless_zero: AllowUnitlessZeroAngle,
