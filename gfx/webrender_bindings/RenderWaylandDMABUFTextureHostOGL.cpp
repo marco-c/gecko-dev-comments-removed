1b4bbc07a6a9984cab48e417cd2426a0281f0486
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-02-12 01:23:18 +0000

    Bug 1613052 [Wayland] Use texture handle from WaylandDMABUFSurface directly at WebRender, r=sotaro
    Depends on D61546
    
    Differential Revision: https://phabricator.services.mozilla.com/D61547
    
    UltraBlame original commit: a4c9db030d03e0f0b7e98a2f29a276747b48df6b

--- gfx/webrender_bindings/RenderWaylandDMABUFTextureHostOGL.cpp
+++ gfx/webrender_bindings/RenderWaylandDMABUFTextureHostOGL.cpp
@@ -1,8 +1,8 @@
-/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
-/* vim: set ts=8 sts=2 et sw=2 tw=80: */
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+
+
+
+
 
 #include "RenderWaylandDMABUFTextureHostOGL.h"
 
@@ -14,7 +14,7 @@ namespace mozilla::wr {
 
 RenderWaylandDMABUFTextureHostOGL::RenderWaylandDMABUFTextureHostOGL(
     WaylandDMABufSurface* aSurface)
-    : mSurface(aSurface), mTextureHandle(0) {
+    : mSurface(aSurface) {
   MOZ_COUNT_CTOR_INHERITED(RenderWaylandDMABUFTextureHostOGL,
                            RenderTextureHostOGL);
 }
@@ -27,15 +27,11 @@ RenderWaylandDMABUFTextureHostOGL::~RenderWaylandDMABUFTextureHostOGL() {
 
 GLuint RenderWaylandDMABUFTextureHostOGL::GetGLHandle(
     uint8_t aChannelIndex) const {
-  MOZ_ASSERT(mSurface);
-  return mTextureHandle;
+  return mSurface->GetGLTexture();
 }
 
 gfx::IntSize RenderWaylandDMABUFTextureHostOGL::GetSize(
     uint8_t aChannelIndex) const {
-  if (!mSurface) {
-    return gfx::IntSize();
-  }
   return gfx::IntSize(mSurface->GetWidth(), mSurface->GetHeight());
 }
 
@@ -45,8 +41,8 @@ wr::WrExternalImage RenderWaylandDMABUFTextureHostOGL::Lock(
 
   if (mGL.get() != aGL) {
     if (mGL) {
-      // This should not happen. EGLImage is created only in
-      // parent process.
+      
+      
       MOZ_ASSERT_UNREACHABLE("Unexpected GL context");
       return InvalidToWrExternalImage();
     }
@@ -57,37 +53,31 @@ wr::WrExternalImage RenderWaylandDMABUFTextureHostOGL::Lock(
     return InvalidToWrExternalImage();
   }
 
-  if (!mTextureHandle) {
+  bool bindTexture = IsFilterUpdateNecessary(aRendering);
+
+  if (!mSurface->GetGLTexture()) {
     if (!mSurface->CreateEGLImage(mGL)) {
       return InvalidToWrExternalImage();
     }
-
-    mGL->fGenTextures(1, &mTextureHandle);
-    // Cache rendering filter.
-    mCachedRendering = aRendering;
-    ActivateBindAndTexParameteri(mGL, LOCAL_GL_TEXTURE0, LOCAL_GL_TEXTURE_2D,
-                                 mTextureHandle, aRendering);
-    mGL->fEGLImageTargetTexture2D(LOCAL_GL_TEXTURE_2D, mSurface->GetEGLImage());
-  } else if (IsFilterUpdateNecessary(aRendering)) {
-    // Cache new rendering filter.
-    mCachedRendering = aRendering;
-    ActivateBindAndTexParameteri(mGL, LOCAL_GL_TEXTURE0, LOCAL_GL_TEXTURE_2D,
-                                 mTextureHandle, aRendering);
+    bindTexture = true;
   }
 
-  return NativeTextureToWrExternalImage(
-      mTextureHandle, 0, 0, mSurface->GetWidth(), mSurface->GetHeight());
+  if (bindTexture) {
+    
+    mCachedRendering = aRendering;
+    ActivateBindAndTexParameteri(mGL, LOCAL_GL_TEXTURE0, LOCAL_GL_TEXTURE_2D,
+                                 mSurface->GetGLTexture(), aRendering);
+  }
+
+  return NativeTextureToWrExternalImage(mSurface->GetGLTexture(), 0, 0,
+                                        mSurface->GetWidth(),
+                                        mSurface->GetHeight());
 }
 
 void RenderWaylandDMABUFTextureHostOGL::Unlock() {}
 
 void RenderWaylandDMABUFTextureHostOGL::DeleteTextureHandle() {
-  if (mTextureHandle) {
-    // XXX recycle gl texture, since SharedSurface_EGLImage and
-    // RenderEGLImageTextureHost is not recycled.
-    mGL->fDeleteTextures(1, &mTextureHandle);
-    mTextureHandle = 0;
-  }
+  mSurface->ReleaseEGLImage();
 }
 
-}  // namespace mozilla::wr
+}  
