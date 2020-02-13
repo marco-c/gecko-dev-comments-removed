f3ef63d54ae0e14580de6d9e40e41edb1ca6d7ce
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-02-13 18:46:01 +0000

    Bug 1613692 - Disable ecosystem telemetry on all channels. r=chutten
    Differential Revision: https://phabricator.services.mozilla.com/D62565
    
    UltraBlame original commit: 833b9196e2889f088b7c27733ae244c43e1c66ac

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1523,11 +1523,7 @@ pref("toolkit.telemetry.updatePing.enabled", true);
 
 pref("toolkit.telemetry.bhrPing.enabled", true);
 
-#ifdef NIGHTLY_BUILD
-  pref("toolkit.telemetry.ecosystemtelemetry.enabled", true);
-#else
-  pref("toolkit.telemetry.ecosystemtelemetry.enabled", false);
-#endif
+pref("toolkit.telemetry.ecosystemtelemetry.enabled", false);
 
 
 pref("browser.ping-centre.telemetry", true);
