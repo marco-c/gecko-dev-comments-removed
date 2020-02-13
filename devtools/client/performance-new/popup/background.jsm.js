4c847c08b5e436c58b1c6b07cfc208a3189c2b67
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-02-13 18:47:01 +0000

    Bug 1614712 - Add a requestId to the profiler's WebChannel messages; r=julienw
    Differential Revision: https://phabricator.services.mozilla.com/D62459
    
    UltraBlame original commit: 33b7e55476b41a5c39b59f8f23980deca50ffa37

--- devtools/client/performance-new/popup/background.jsm.js
+++ devtools/client/performance-new/popup/background.jsm.js
@@ -440,6 +440,7 @@ function handleWebChannelMessage(channel, id, message, target) {
     return;
   }
   const messageFromFrontend =  (message);
+  const { requestId } = messageFromFrontend;
   switch (messageFromFrontend.type) {
     case "STATUS_QUERY": {
       
@@ -449,6 +450,7 @@ function handleWebChannelMessage(channel, id, message, target) {
         {
           type: "STATUS_RESPONSE",
           menuButtonIsEnabled: ProfilerMenuButton.isEnabled(),
+          requestId,
         },
         target
       );
@@ -472,6 +474,7 @@ function handleWebChannelMessage(channel, id, message, target) {
       channel.send(
         {
           type: "ENABLE_MENU_BUTTON_DONE",
+          requestId,
         },
         target
       );
