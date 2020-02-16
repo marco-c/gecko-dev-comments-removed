a3d50265741db227d21ae43b3b81439dbfcb9409
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-02-16 00:15:04 +0000

    Bug 1591408 [wpt PR 19886] - Use structured clone battery of tests for more, a=testonly
    Automatic update from web-platform-tests
    Use structured clone battery of tests for more
    
    And fix a bug in how it did Blobs.
    --
    
    wpt-commits: 69ca5c0184c496ec70e7be4e003295c29d728885
    wpt-pr: 19886
    
    UltraBlame original commit: 5417ad80b65300ff11ad2c0765351a149c7e8ebf

--- testing/web-platform/tests/html/webappapis/structured-clone/structured-clone-battery-of-tests-harness.js
+++ testing/web-platform/tests/html/webappapis/structured-clone/structured-clone-battery-of-tests-harness.js
@@ -17,12 +17,17 @@ function runStructuredCloneBatteryOfTests(runner) {
     setup() {},
     preTest() {},
     postTest() {},
-    teardown() {}
+    teardown() {},
+    hasDocument: true
   };
   runner = Object.assign({}, defaultRunner, runner);
 
   let setupPromise = runner.setup();
   const allTests = structuredCloneBatteryOfTests.map(test => {
+
+    if (!runner.hasDocument && test.requiresDocument) {
+      return;
+    }
 
     return new Promise(resolve => {
       promise_test(async _ => {
