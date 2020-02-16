4171dabba2c6a7bd4098d7101362ebfb066dc6b6
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-02-16 00:12:51 +0000

    Bug 1592535 - Add a column displaying the last frame in the stack trace info if it exists r=Harald,Honza
    Differential Revision: https://phabricator.services.mozilla.com/D61423
    
    UltraBlame original commit: efefdcf32ad530477ad92a761173f801da684e57

--- devtools/client/netmonitor/src/components/request-list/RequestListItem.js
+++ devtools/client/netmonitor/src/components/request-list/RequestListItem.js
@@ -39,6 +39,11 @@ const {
 
 
 
+loader.lazyGetter(this, "RequestListColumnInitiator", function() {
+  return createFactory(
+    require("devtools/client/netmonitor/src/components/request-list/RequestListColumnInitiator")
+  );
+});
 loader.lazyGetter(this, "RequestListColumnCause", function() {
   return createFactory(
     require("devtools/client/netmonitor/src/components/request-list/RequestListColumnCause")
@@ -190,6 +195,11 @@ const COLUMN_COMPONENTS = [
     column: "cause",
     ColumnComponent: RequestListColumnCause,
     props: ["onCauseBadgeMouseDown"],
+  },
+  {
+    column: "initiator",
+    ColumnComponent: RequestListColumnInitiator,
+    props: ["onInitiatorBadgeMouseDown"],
   },
   { column: "type", ColumnComponent: RequestListColumnType },
   {
