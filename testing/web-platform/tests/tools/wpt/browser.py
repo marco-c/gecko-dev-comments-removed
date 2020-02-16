1416928635d1b25a2d1f3c07be6a753fb73360a0
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-02-16 00:20:29 +0000

    Bug 1613902 [wpt PR 21656] - Convert returned binary data from call() to string, a=testonly
    Automatic update from web-platform-tests
    Convert returned binary data from call() to string
    
    This changes is based on the same cause as
    commit 6ea4b6f95f85e8a425e827ddddc5f2caa06755c3
    
    subprocess.check_output() returns binary data as output. In python2,
    binary is basically an alias of str so it can be directly used as a
    normal string. However in python3 the binary data is different to a
    string. Inserting binary data into a string in python3 generates
    strings like '/some/path/with/b'binary'/data/inserted' which are not
    correct file paths. We must decode() the returned data in order to use
    it as a string.
    
    This change also explicitly sets encoding="utf-8". In Python2,
    default decoding is ASCII while in Python 3 it is "utf-8". Leaving
    encoding parameters as defaults will break non-ASCII output on Python 2.
    
    --
    
    wpt-commits: 9ce6f138bb56ac6941cd58366808f16f9c4a53cd
    wpt-pr: 21656
    
    UltraBlame original commit: aadcecb663b4c3d4a18f187a7ee7280c2bf0bff5

--- testing/web-platform/tests/tools/wpt/browser.py
+++ testing/web-platform/tests/tools/wpt/browser.py
@@ -1087,11 +1087,11 @@ class Safari(Browser):
         except subprocess.CalledProcessError:
             self.logger.warning("Failed to call %s --version" % webdriver_binary)
             return None
-        m = re.match(br"Included with Safari (.*)", version_string)
+        m = re.match(r"Included with Safari (.*)", version_string)
         if not m:
             self.logger.warning("Failed to extract version from: %s" % version_string)
             return None
-        return m.group(1).decode()
+        return m.group(1)
 
 
 class Servo(Browser):
@@ -1234,7 +1234,7 @@ class WebKitGTKMiniBrowser(WebKit):
         gcc = find_executable("gcc")
         if gcc:
             try:
-                triplet = call(gcc, "-dumpmachine").decode().strip()
+                triplet = call(gcc, "-dumpmachine").strip()
             except subprocess.CalledProcessError:
                 pass
         
@@ -1253,7 +1253,7 @@ class WebKitGTKMiniBrowser(WebKit):
         if binary is None:
             return None
         try:  
-            output = call(binary, "--version").decode().strip()
+            output = call(binary, "--version").strip()
         except subprocess.CalledProcessError:
             return None
         
