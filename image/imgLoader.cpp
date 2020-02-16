6407711a583397795ef0c1c8323d7b65b6b1da6c
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-02-16 00:13:11 +0000

    Bug 1615539 - Fix explicit image memory reporting broken on Windows. r=jrmuizel
    Differential Revision: https://phabricator.services.mozilla.com/D62899
    
    UltraBlame original commit: 10d8c314b3942eb7b9beb4dd5fd8797dc209a708

--- image/imgLoader.cpp
+++ image/imgLoader.cpp
@@ -492,7 +492,9 @@ class imgMemoryReporter final : public nsIMemoryReporter {
                 "Decoded image data which isn't stored on the heap.",
                 aCounter.DecodedNonHeap());
 
-    ReportValue(aHandleReport, aData, KIND_OTHER, aPathPrefix,
+    
+    
+    ReportValue(aHandleReport, aData, KIND_NONHEAP, aPathPrefix,
                 "decoded-unknown",
                 "Decoded image data which is unknown to be on the heap or not.",
                 aCounter.DecodedUnknown());
