f33971b5452b9e464b034f7e2b2ec227042fc75d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-02-16 00:13:06 +0000

    Bug 1506376, remove useless private: from VsyncEvent struct r=mattwoodrow
    Depends on D61083
    
    Differential Revision: https://phabricator.services.mozilla.com/D62032
    
    UltraBlame original commit: 621ec02db41c7cf303996b971f523e33e8a20978

--- gfx/thebes/VsyncSource.h
+++ gfx/thebes/VsyncSource.h
@@ -20,12 +20,7 @@ class CompositorVsyncDispatcher;
 class VsyncIdType {};
 typedef layers::BaseTransactionId<VsyncIdType> VsyncId;
 
-namespace layout {
-class PVsyncChild;
-}
-
 namespace gfx {
-class PVsyncBridgeParent;
 
 
 
@@ -96,24 +91,13 @@ class VsyncSource {
 
 }  
 
-namespace recordreplay {
-namespace child {
-void NotifyVsyncObserver();
-}
-}  
-
 struct VsyncEvent {
   VsyncId mId;
   TimeStamp mTime;
 
- private:
   VsyncEvent(const VsyncId& aId, const TimeStamp& aTime)
       : mId(aId), mTime(aTime) {}
   VsyncEvent() {}
-  friend class gfx::VsyncSource::Display;
-  friend class gfx::PVsyncBridgeParent;
-  friend class layout::PVsyncChild;
-  friend void recordreplay::child::NotifyVsyncObserver();
 };
 
 }  
