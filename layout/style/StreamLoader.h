3966c2ac233ac7f00d9887bf8f7131126a3b8871
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-02-16 00:12:42 +0000

    Bug 1605895 - Add some assertions to catch misuse of the stream loader / load datas. r=heycam
    Differential Revision: https://phabricator.services.mozilla.com/D62671
    
    UltraBlame original commit: 60549faf473472dd052e719d360d3dbc69698b71

--- layout/style/StreamLoader.h
+++ layout/style/StreamLoader.h
@@ -1,8 +1,8 @@
-
-
-
-
-
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=8 sts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #ifndef mozilla_css_StreamLoader_h
 #define mozilla_css_StreamLoader_h
@@ -10,6 +10,7 @@
 #include "nsIStreamListener.h"
 #include "nsString.h"
 #include "mozilla/css/SheetLoadData.h"
+#include "mozilla/Assertions.h"
 
 class nsIInputStream;
 
@@ -27,9 +28,9 @@ class StreamLoader : public nsIStreamListener {
  private:
   virtual ~StreamLoader();
 
-  
-
-
+  /**
+   * callback method used for ReadSegments
+   */
   static nsresult WriteSegmentFun(nsIInputStream*, void*, const char*, uint32_t,
                                   uint32_t, uint32_t*);
 
@@ -39,14 +40,18 @@ class StreamLoader : public nsIStreamListener {
   nsresult mStatus;
   Maybe<const Encoding*> mEncodingFromBOM;
 
-  
-  
-  
+  // We store the initial three bytes of the stream into mBOMBytes, and then
+  // use that buffer to detect a BOM. We then shift any non-BOM bytes into
+  // mBytes, and store all subsequent data in that buffer.
   nsCString mBytes;
   nsAutoCStringN<3> mBOMBytes;
+
+#ifdef MOZ_DIAGNOSTIC_ASSERT_ENABLED
+  bool mOnStopRequestCalled = false;
+#endif
 };
 
-}  
-}  
+}  // namespace css
+}  // namespace mozilla
 
-#endif  
+#endif  // mozilla_css_StreamLoader_h
