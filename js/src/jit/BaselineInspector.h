1531d8fc44ca79999469586ec994c5f6a3237f15
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-02-16 00:12:24 +0000

    Bug 1608994 part 3 - Remove class hook template object. r=iain
    Ion no longer uses the template object after part 1.
    
    Differential Revision: https://phabricator.services.mozilla.com/D62740
    
    UltraBlame original commit: 70af5c75f31c8e3d3d5efa96677cdbf994c8bc37

--- js/src/jit/BaselineInspector.h
+++ js/src/jit/BaselineInspector.h
@@ -1,8 +1,8 @@
-
-
-
-
-
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ * vim: set ts=8 sts=2 et sw=2 tw=80:
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #ifndef jit_BaselineInspector_h
 #define jit_BaselineInspector_h
@@ -95,10 +95,9 @@ class BaselineInspector {
 
   JSObject* getTemplateObject(jsbytecode* pc);
   JSObject* getTemplateObjectForNative(jsbytecode* pc, Native native);
-  JSObject* getTemplateObjectForClassHook(jsbytecode* pc, const JSClass* clasp);
 
-  
-  
+  // Sometimes the group a template object will have is known, even if the
+  // object itself isn't.
   ObjectGroup* getTemplateObjectGroup(jsbytecode* pc);
 
   JSFunction* getSingleCallee(jsbytecode* pc);
@@ -106,10 +105,10 @@ class BaselineInspector {
   LexicalEnvironmentObject* templateNamedLambdaObject();
   CallObject* templateCallObject();
 
-  
-  
-  
-  
+  // If |innerized| is true, we're doing a GETPROP on a WindowProxy and
+  // IonBuilder unwrapped/innerized it to do the lookup on the Window (the
+  // global object) instead. In this case we should only look for Baseline
+  // stubs that performed the same optimization.
   MOZ_MUST_USE bool commonGetPropFunction(
       jsbytecode* pc, jsid id, bool innerized, JSObject** holder,
       Shape** holderShape, JSFunction** commonGetter, Shape** globalShape,
@@ -128,7 +127,7 @@ class BaselineInspector {
                                    uint32_t* slot, JSObject** prototypeObject);
 };
 
-}  
-}  
+}  // namespace jit
+}  // namespace js
 
-#endif 
+#endif /* jit_BaselineInspector_h */
