bcdac692009a4f585730748e9bfe01dd1de5d069
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-02-18 16:50:46 +0000

    Bug 1614868 - Ignore combining diacritic characters in history search. r=jfkthame,mak
    IsCombiningDiacritic(-1) returns false, so there is no need to specially
    handle -1 in GetLowerUTF8Codepoint_inline.
    
    It is no longer necessary for GetNaked to check whether a character is a
    combining character because all callers now skip combining diacritics
    and GetNaked already makes sure that decomposition removes a diacritic
    and not something else.
    
    Differential Revision: https://phabricator.services.mozilla.com/D62533
    
    UltraBlame original commit: 19462e426dc3ee6ee7eae32a2558ce54a2c7e082

--- intl/unicharutil/util/nsUnicodeProperties.cpp
+++ intl/unicharutil/util/nsUnicodeProperties.cpp
@@ -315,6 +315,9 @@ uint32_t GetNaked(uint32_t aCh) {
   static const UNormalizer2* normalizer;
   static HashMap<uint32_t, uint32_t> nakedCharCache;
 
+  NS_ASSERTION(!IsCombiningDiacritic(aCh),
+               "This character needs to be skipped");
+
   HashMap<uint32_t, uint32_t>::Ptr entry = nakedCharCache.lookup(aCh);
   if (entry.found()) {
     return entry->value();
@@ -338,13 +341,6 @@ uint32_t GetNaked(uint32_t aCh) {
   if (decompositionLen < 1) {
     
     return aCh;
-  }
-
-  if (u_getIntPropertyValue(aCh, UCHAR_GENERAL_CATEGORY) & U_GC_M_MASK) {
-    
-    
-    baseChar = aCh;
-    goto cache;
   }
 
   if (NS_IS_HIGH_SURROGATE(decomposition[0])) {
