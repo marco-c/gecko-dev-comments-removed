3874d2055b891df16387c3b02de9fb9736d953a8
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-02-19 13:38:55 +0000

    Backed out changeset fa5ee85e20a5 (bug 1603673) for causing lint failure in GeckoRuntimeSettings.java CLOSED TREE
    UltraBlame original commit: eae597c03f1f0b4da80dc5e282543ffe8c1e6c63

--- mobile/android/geckoview/src/main/java/org/mozilla/geckoview/GeckoRuntimeSettings.java
+++ mobile/android/geckoview/src/main/java/org/mozilla/geckoview/GeckoRuntimeSettings.java
@@ -166,22 +166,6 @@ public final class GeckoRuntimeSettings extends RuntimeSettings {
 
 
 
-
-        public @NonNull Builder webManifest(final boolean enabled) {
-            getSettings().mWebManifest.set(enabled);
-            return this;
-        }
-
-        
-
-
-
-
-
-
-
-
-
         public @NonNull Builder consoleOutput(final boolean enabled) {
             getSettings().mConsoleOutput.set(enabled);
             return this;
@@ -458,8 +442,6 @@ public final class GeckoRuntimeSettings extends RuntimeSettings {
         return mContentBlocking;
     }
 
-     final Pref<Boolean> mWebManifest = new Pref<Boolean>(
-        "dom.manifest.enabled", true);
      final Pref<Boolean> mJavaScript = new Pref<Boolean>(
         "javascript.enabled", true);
      final Pref<Boolean> mRemoteDebugging = new Pref<Boolean>(
@@ -801,29 +783,6 @@ public final class GeckoRuntimeSettings extends RuntimeSettings {
         }
         
         return out.toString();
-    }
-
-    
-
-
-
-
-
-
-
-    public @NonNull GeckoRuntimeSettings setWebManifestEnabled(final boolean enabled) {
-        mWebManifest.commit(enabled);
-        return this;
-    }
-
-    
-
-
-
-
-
-    public boolean getWebManifestEnabled() {
-        return mWebManifest.get();
     }
 
     
