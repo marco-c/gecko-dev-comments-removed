cb80ab430b3427bc88972a5091d98dfb41d38f16
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-02-23 11:38:25 +0000

    Bug 1613549 - Reduce the timeout in browser_search_tips.js because the original delay has been removed. r=adw
    Differential Revision: https://phabricator.services.mozilla.com/D63211
    
    UltraBlame original commit: ba373603f390cf83509d0c809bea5cac93a639e8

--- browser/components/urlbar/tests/browser/browser_search_tips.js
+++ browser/components/urlbar/tests/browser/browser_search_tips.js
@@ -19,7 +19,6 @@ XPCOMUtils.defineLazyModuleGetters(this, {
 });
 
 
-const SHOW_TIP_DELAY_MS = 200;
 const MAX_SHOWN_COUNT = 4;
 const LAST_UPDATE_THRESHOLD_MS = 24 * 60 * 60 * 1000;
 const TIPS = {
@@ -669,7 +668,7 @@ async function checkTip(win, expectedTip, closeView = true) {
   if (!expectedTip) {
     
     
-    await new Promise(resolve => setTimeout(resolve, 3 * SHOW_TIP_DELAY_MS));
+    await new Promise(resolve => setTimeout(resolve, 100));
     Assert.ok(!win.gURLBar.view.isOpen);
     return;
   }
