b866e38d8ac2b4a6317bcff66049c4329614ef7a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-02-23 11:41:26 +0000

    Bug 1611886 - Add VRManagerParent::Shutdown() r=nical
    Differential Revision: https://phabricator.services.mozilla.com/D61144
    
    UltraBlame original commit: 4349043df363f2cc00d9ff73714f681269635386

--- gfx/vr/VRManager.cpp
+++ gfx/vr/VRManager.cpp
@@ -82,6 +82,13 @@ VRManager* VRManager::Get() {
   return sVRManagerSingleton;
 }
 
+
+VRManager* VRManager::MaybeGet() {
+  MOZ_ASSERT(XRE_IsParentProcess() || XRE_IsGPUProcess());
+
+  return sVRManagerSingleton;
+}
+
 Atomic<uint32_t> VRManager::sDisplayBase(0);
 
 
@@ -1117,6 +1124,22 @@ void VRManager::Shutdown() {
   
   
   
+}
+
+void VRManager::ShutdownVRManagerParents() {
+  
+  VRManagerParentSet vrManagerParents;
+  for (auto iter = mVRManagerParents.Iter(); !iter.Done(); iter.Next()) {
+    vrManagerParents.PutEntry(iter.Get()->GetKey());
+  }
+
+  for (auto iter = vrManagerParents.Iter(); !iter.Done(); iter.Next()) {
+    iter.Get()->GetKey()->Close();
+    iter.Remove();
+  }
+
+  MOZ_DIAGNOSTIC_ASSERT(mVRManagerParents.IsEmpty(),
+                        "Closing should have cleared all entries.");
 }
 
 void VRManager::CheckWatchDog() {
