9484c9a34a73aa8e8d66449440b9102bb6562289
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-02-23 11:46:02 +0000

    Backed out changeset 78353ef17d0d (bug 1616052) for causing xpc crashes mozilla::storage::Service::Observe.
    CLOSED TREE
    
    UltraBlame original commit: 7a16634abb489984a0ef3adff4fad18c2a58b4a9

--- services/settings/remote-settings.js
+++ services/settings/remote-settings.js
@@ -17,15 +17,31 @@ const { XPCOMUtils } = ChromeUtils.import(
   "resource://gre/modules/XPCOMUtils.jsm"
 );
 
-XPCOMUtils.defineLazyModuleGetters(this, {
-  UptakeTelemetry: "resource://services-common/uptake-telemetry.js",
-  pushBroadcastService: "resource://gre/modules/PushBroadcastService.jsm",
-  RemoteSettingsClient: "resource://services-settings/RemoteSettingsClient.jsm",
-  Utils: "resource://services-settings/Utils.jsm",
-  FilterExpressions:
-    "resource://gre/modules/components-utils/FilterExpressions.jsm",
-  RemoteSettingsWorker: "resource://services-settings/RemoteSettingsWorker.jsm",
-});
+ChromeUtils.defineModuleGetter(
+  this,
+  "UptakeTelemetry",
+  "resource://services-common/uptake-telemetry.js"
+);
+ChromeUtils.defineModuleGetter(
+  this,
+  "pushBroadcastService",
+  "resource://gre/modules/PushBroadcastService.jsm"
+);
+ChromeUtils.defineModuleGetter(
+  this,
+  "RemoteSettingsClient",
+  "resource://services-settings/RemoteSettingsClient.jsm"
+);
+ChromeUtils.defineModuleGetter(
+  this,
+  "Utils",
+  "resource://services-settings/Utils.jsm"
+);
+ChromeUtils.defineModuleGetter(
+  this,
+  "FilterExpressions",
+  "resource://gre/modules/components-utils/FilterExpressions.jsm"
+);
 
 XPCOMUtils.defineLazyGlobalGetters(this, ["fetch"]);
 
@@ -157,19 +173,10 @@ function remoteSettingsFunction() {
 
 
 
-
   remoteSettings.pollChanges = async ({
     expectedTimestamp,
     trigger = "manual",
-    full = false,
   } = {}) => {
-    
-    if (full) {
-      gPrefs.clearUserPref(PREF_SETTINGS_SERVER_BACKOFF);
-      gPrefs.clearUserPref(PREF_SETTINGS_LAST_UPDATE);
-      gPrefs.clearUserPref(PREF_SETTINGS_LAST_ETAG);
-    }
-
     let pollTelemetryArgs = {
       source: TELEMETRY_SOURCE_POLL,
       trigger,
@@ -383,7 +390,8 @@ function remoteSettingsFunction() {
         if (!client) {
           return null;
         }
-        const localTimestamp = await client.getLastModified();
+        const kintoCol = await client.openCollection();
+        const localTimestamp = await kintoCol.db.getLastModified();
         const lastCheck = Services.prefs.getIntPref(
           client.lastCheckTimePref,
           0
@@ -409,25 +417,6 @@ function remoteSettingsFunction() {
       defaultSigner: DEFAULT_SIGNER,
       collections: collections.filter(c => !!c),
     };
-  };
-
-  
-
-
-  remoteSettings.clearAll = async () => {
-    const { collections } = await remoteSettings.inspect();
-    await Promise.all(
-      collections.map(async ({ collection }) => {
-        const client = RemoteSettings(collection);
-        
-        await client.attachments.deleteAll();
-        
-        const kintoCollection = await client.openCollection();
-        await kintoCollection.clear();
-        
-        Services.prefs.clearUserPref(client.lastCheckTimePref);
-      })
-    );
   };
 
   
