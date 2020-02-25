eb9fae1d945e628e426237107e80514d15285a8d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-02-25 16:30:20 +0000

    Backed out changeset df563f103355 (bug 1616847) for causing devtools failure at devtools/client/shared/test/browser_html_tooltip_offset.js
    CLOSED TREE
    
    UltraBlame original commit: dd2fa5793be60ddd521575715867674c63116042

--- devtools/client/shared/widgets/tooltip/HTMLTooltip.js
+++ devtools/client/shared/widgets/tooltip/HTMLTooltip.js
@@ -481,24 +481,14 @@ HTMLTooltip.prototype = {
 
 
 
-
   async show(anchor, options) {
     const { left, top } = this._updateContainerBounds(anchor, options);
-    const isTooltipVisible = this.isVisible();
 
     if (this.useXulWrapper) {
-      if (!isTooltipVisible) {
-        await this._showXulWrapperAt(left, top);
-      } else {
-        this._moveXulWrapperTo(left, top);
-      }
+      await this._showXulWrapperAt(left, top);
     } else {
       this.container.style.left = left + "px";
       this.container.style.top = top + "px";
-    }
-
-    if (isTooltipVisible) {
-      return;
     }
 
     this.container.classList.add("tooltip-visible");
@@ -506,19 +496,14 @@ HTMLTooltip.prototype = {
     
     this._focusedElement = this.doc.activeElement;
 
-    if (this.doc.defaultView) {
-      if (this.attachEventsTimer) {
-        this.doc.defaultView.clearTimeout(this.attachEventsTimer);
-      }
-
-      this.attachEventsTimer = this.doc.defaultView.setTimeout(() => {
-        
-        this.topWindow = this._getTopWindow();
-        this.topWindow.addEventListener("click", this._onClick, true);
-        this.topWindow.addEventListener("mouseup", this._onMouseup, true);
-        this.emit("shown");
-      }, 0);
-    }
+    this.doc.defaultView.clearTimeout(this.attachEventsTimer);
+    this.attachEventsTimer = this.doc.defaultView.setTimeout(() => {
+      
+      this.topWindow = this._getTopWindow();
+      this.topWindow.addEventListener("click", this._onClick, true);
+      this.topWindow.addEventListener("mouseup", this._onMouseup, true);
+      this.emit("shown");
+    }, 0);
   },
 
   startTogglingOnHover(baseNode, targetNodeCb, options) {
@@ -527,6 +512,27 @@ HTMLTooltip.prototype = {
 
   stopTogglingOnHover() {
     this.toggle.stop();
+  },
+
+  
+
+
+
+
+
+  updateContainerBounds(anchor, options) {
+    if (!this.isVisible()) {
+      return;
+    }
+
+    const { left, top } = this._updateContainerBounds(anchor, options);
+
+    if (this.useXulWrapper) {
+      this._moveXulWrapperTo(left, top);
+    } else {
+      this.container.style.left = left + "px";
+      this.container.style.top = top + "px";
+    }
   },
 
   _updateContainerBounds(anchor, { position, x = 0, y = 0 } = {}) {
