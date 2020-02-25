1e70b57ae7196895b37373c2dd0fa982751d8ffd
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-02-25 16:27:25 +0000

    Bug 1616165 [wpt PR 21845] - pywebsocket: Add and update to pywebsocket3, a=testonly
    Automatic update from web-platform-tests
    Update to use pywebsocket3 instead of pywebsocket
    
    This commit updates localpaths.py to point to pywebsocket3. This also
    updates serve.py to remove using deprecated functions.
    
    --
    Remove pywebsocket from //tools
    
    This commit removes pywebsocket from //tools, since WPT now uses
    //tools/thir_party/pywebsocket3.
    
    --
    Update websocket handler to pywebsocket3
    
    This commit updates websocket handler to use new function provided in
    pywebsocket3.
    
    --
    
    wpt-commits: 7e52ecb9b61b73425093d39dbadceb9c6e10b754, da56e6cf601033599b04d36fbd0e1d092032b241, 4a72ff602700e848da0d69fff3f133ee3e16087e
    wpt-pr: 21845
    
    UltraBlame original commit: 388c82448cb61f31b24af77a47a16a4ba34e6870

---
+++ testing/web-platform/tests/tools/third_party/pywebsocket3/test/testdata/handlers/abort_by_user_wsh.py
@@ -0,0 +1,41 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+from mod_pywebsocket import handshake
+
+
+def web_socket_do_extra_handshake(request):
+    raise handshake.AbortedByUserException("abort for test")
+
+
+def web_socket_transfer_data(request):
+    raise handshake.AbortedByUserException("abort for test")
+
+
+
---
+++ testing/web-platform/tests/tools/third_party/pywebsocket3/test/testdata/handlers/sub/non_callable_wsh.py
@@ -0,0 +1,35 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""Non-callable handlers.
+"""
+
+web_socket_do_extra_handshake = True
+web_socket_transfer_data = 1
+
+
