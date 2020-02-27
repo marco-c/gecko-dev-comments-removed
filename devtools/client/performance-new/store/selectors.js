41c61741d73fa8d094c2b82eb858d95b11f74c83
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-02-27 18:19:51 +0000

    Bug 1597378 - Change location of the presets; r=julienw
    The presets in recording-utils.js couldn't be shared via the ChromeUtils.import
    mechanism. When they were in recording-utils.js they could only be loaded in
    via the DevTools require() loader. This commit changes it so that they are
    stored in a jsm, and are also injected to each UI interface where the
    ChromeUtils.import function is available (only in the initializers).
    
    Differential Revision: https://phabricator.services.mozilla.com/D62912
    
    UltraBlame original commit: 9b760d8fc1dc8160a8d2c6bd06cd8eeba529926b

--- devtools/client/performance-new/store/selectors.js
+++ devtools/client/performance-new/store/selectors.js
@@ -3,7 +3,7 @@
 
 
 "use strict";
-const { presets } = require("devtools/shared/performance-new/recording-utils");
+
 
 
 
@@ -51,6 +51,9 @@ const getThreadsString = state => getThreads(state).join(",");
 const getObjdirs = state => state.objdirs;
 
 
+const getPresets = state => getInitializedValues(state).presets;
+
+
 const getPresetName = state => state.presetName;
 
 
@@ -60,6 +63,7 @@ const getPresetName = state => state.presetName;
 
 
 const getRecordingSettings = state => {
+  const presets = getPresets(state);
   const presetName = getPresetName(state);
   const preset = presets[presetName];
   if (preset) {
@@ -132,6 +136,7 @@ module.exports = {
   getThreads,
   getThreadsString,
   getObjdirs,
+  getPresets,
   getPresetName,
   getRecordingSettings,
   getInitializedValues,
