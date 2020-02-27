28df69d3e6f2bf906078851b1cdf5c620c643eab
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-02-27 18:25:27 +0000

    Bug 1607366 - Deactivate browser_startup_flicker when we have Fission without WebRender;r=florian
    Differential Revision: https://phabricator.services.mozilla.com/D64412
    
    UltraBlame original commit: 7bdea74790de1ab4734e1efaa373c7ffed9a01b0

--- browser/base/content/test/performance/browser_startup_flicker.js
+++ browser/base/content/test/performance/browser_startup_flicker.js
@@ -9,6 +9,19 @@
 
 
 add_task(async function() {
+  const isWebRenderEnabled = Services.prefs.getBoolPref("gfx.webrender.all");
+  const isFissionEnabled = Services.prefs.getBoolPref("fission.autostart");
+  if (isFissionEnabled && !isWebRenderEnabled) {
+    
+    
+    
+    ok(
+      true,
+      "Detected Fission without WebRender. Flicker expected, deactivating flicker test"
+    );
+    return;
+  }
+
   let startupRecorder = Cc["@mozilla.org/test/startuprecorder;1"].getService()
     .wrappedJSObject;
   await startupRecorder.done;
