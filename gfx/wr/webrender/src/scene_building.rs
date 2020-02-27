afe11e7a07f15ebd5b8373b36f86a509f8fcd63a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-02-27 18:27:26 +0000

    Backed out 6 changesets (bug 1510030) for webrender bustages CLOSED TREE
    Backed out changeset 28a2fba71977 (bug 1510030)
    Backed out changeset 1700b3a416cd (bug 1510030)
    Backed out changeset 087518046b0c (bug 1510030)
    Backed out changeset 6b3af91a7ce6 (bug 1510030)
    Backed out changeset 737f4bc3afd3 (bug 1510030)
    Backed out changeset 921116ca67a5 (bug 1510030)
    
    UltraBlame original commit: 7fc17499b72099c0fa5d27f76c619229c01f2516

--- gfx/wr/webrender/src/scene_building.rs
+++ gfx/wr/webrender/src/scene_building.rs
@@ -1214,7 +1214,7 @@ impl<'a> SceneBuilder<'a> {
                 self.add_solid_rectangle(
                     clip_and_scroll,
                     &layout,
-                    PropertyBinding::Value(ColorF::TRANSPARENT),
+                    ColorF::TRANSPARENT,
                 );
             }
             DisplayItem::ClearRectangle(ref info) => {
@@ -2749,19 +2749,13 @@ impl<'a> SceneBuilder<'a> {
         &mut self,
         clip_and_scroll: ScrollNodeAndClipChain,
         info: &LayoutPrimitiveInfo,
-        color: PropertyBinding<ColorF>,
+        color: ColorF,
     ) {
-        match color {
-            PropertyBinding::Value(value) => {
-                if value.a == 0.0 {
-                    
-                    
-                    
-                    self.add_primitive_to_hit_testing_list(info, clip_and_scroll);
-                    return;
-                }
-            },
-            PropertyBinding::Binding(..) => {},
+        if color.a == 0.0 {
+            
+            
+            self.add_primitive_to_hit_testing_list(info, clip_and_scroll);
+            return;
         }
 
         self.add_primitive(
