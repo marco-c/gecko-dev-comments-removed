53130a9bce617113a133e9b396da9abfa2a0730b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-02-27 18:24:03 +0000

    Bug 1615710 - Remove the Debugger::lazyScripts weakmap. r=jandem
    This can be combined with the Debugger::scripts map since keys will not
    conflict. This is now all possible since we use js::BaseScript as the root
    type.
    
    Depends on D64123
    
    Differential Revision: https://phabricator.services.mozilla.com/D64124
    
    UltraBlame original commit: 30b40b53ff10d3560ef3abb40948eac8ed7d2164

--- js/src/debugger/Debugger.h
+++ js/src/debugger/Debugger.h
@@ -749,7 +749,6 @@ class Debugger : private mozilla::LinkedListElement<Debugger> {
   
   using ScriptWeakMap = DebuggerWeakMap<BaseScript, DebuggerScript>;
   ScriptWeakMap scripts;
-  ScriptWeakMap lazyScripts;
 
   using BaseScriptVector = JS::GCVector<BaseScript*>;
 
