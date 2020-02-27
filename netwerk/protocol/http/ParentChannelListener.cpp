9dbe6bc498cc03dfc97138d8a4cb4c8fbdfc9694
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-02-27 18:23:05 +0000

    Bug 1616716 - Implement nsIAuthPromptProvider on ParentChannelListener. r=mayhemer
    Differential Revision: https://phabricator.services.mozilla.com/D63425
    
    UltraBlame original commit: 35186917e79acc9c7171c82bb123c6ade4f2bad2

--- netwerk/protocol/http/ParentChannelListener.cpp
+++ netwerk/protocol/http/ParentChannelListener.cpp
@@ -21,6 +21,9 @@
 #include "nsQueryObject.h"
 #include "nsIAuthPrompt.h"
 #include "nsIAuthPrompt2.h"
+#include "nsIPromptFactory.h"
+#include "Element.h"
+#include "nsILoginManagerAuthPrompter.h"
 
 using mozilla::Unused;
 using mozilla::dom::ServiceWorkerInterceptController;
@@ -61,6 +64,7 @@ NS_INTERFACE_MAP_BEGIN(ParentChannelListener)
   NS_INTERFACE_MAP_ENTRY(nsIRequestObserver)
   NS_INTERFACE_MAP_ENTRY(nsIMultiPartChannelListener)
   NS_INTERFACE_MAP_ENTRY(nsINetworkInterceptController)
+  NS_INTERFACE_MAP_ENTRY_CONDITIONAL(nsIAuthPromptProvider, mBrowsingContext)
   NS_INTERFACE_MAP_ENTRY_AMBIGUOUS(nsISupports, nsIInterfaceRequestor)
   NS_INTERFACE_MAP_ENTRY_CONCRETE(ParentChannelListener)
 NS_INTERFACE_MAP_END
@@ -150,12 +154,6 @@ ParentChannelListener::GetInterface(const nsIID& aIID, void** result) {
     return QueryInterface(aIID, result);
   }
 
-  if (aIID.Equals(NS_GET_IID(nsIAuthPromptProvider))) {
-    if (mBrowserParent) {
-      return mBrowserParent->QueryInterface(aIID, result);
-    }
-  }
-
   if (mBrowserParent && aIID.Equals(NS_GET_IID(nsIPrompt))) {
     nsCOMPtr<dom::Element> frameElement = mBrowserParent->GetOwnerElement();
     if (frameElement) {
@@ -183,11 +181,7 @@ ParentChannelListener::GetInterface(const nsIID& aIID, void** result) {
 
   if (mBrowserParent && (aIID.Equals(NS_GET_IID(nsIAuthPrompt)) ||
                          aIID.Equals(NS_GET_IID(nsIAuthPrompt2)))) {
-    nsCOMPtr<nsIAuthPromptProvider> provider(do_QueryObject(mBrowserParent));
-    if (provider) {
-      return provider->GetAuthPrompt(nsIAuthPromptProvider::PROMPT_NORMAL, aIID,
-                                     result);
-    }
+    return GetAuthPrompt(nsIAuthPromptProvider::PROMPT_NORMAL, aIID, result);
   }
 
   if (aIID.Equals(NS_GET_IID(nsIRemoteWindowContext)) && mBrowserParent) {
@@ -381,6 +375,41 @@ void ParentChannelListener::ClearInterceptedChannel(
   
   
   mInterceptCanceled = true;
+}
+
+
+
+
+
+NS_IMETHODIMP
+ParentChannelListener::GetAuthPrompt(uint32_t aPromptReason, const nsIID& iid,
+                                     void** aResult) {
+  if (!mBrowserParent) {
+    return NS_ERROR_NOT_AVAILABLE;
+  }
+  
+  nsresult rv;
+  nsCOMPtr<nsIPromptFactory> wwatch =
+      do_GetService(NS_WINDOWWATCHER_CONTRACTID, &rv);
+  NS_ENSURE_SUCCESS(rv, rv);
+
+  nsCOMPtr<nsPIDOMWindowOuter> window;
+  RefPtr<dom::Element> frame = mBrowserParent->GetOwnerElement();
+  if (frame) window = frame->OwnerDoc()->GetWindow();
+
+  
+  
+  nsCOMPtr<nsISupports> prompt;
+  rv = wwatch->GetPrompt(window, iid, getter_AddRefs(prompt));
+  NS_ENSURE_SUCCESS(rv, rv);
+
+  nsCOMPtr<nsILoginManagerAuthPrompter> prompter = do_QueryInterface(prompt);
+  if (prompter) {
+    prompter->SetBrowser(frame);
+  }
+
+  *aResult = prompt.forget().take();
+  return NS_OK;
 }
 
 }  
