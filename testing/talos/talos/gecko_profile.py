dc9895337759a1e495dd86e59c8e1955564bf337
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-02-28 22:11:44 +0000

    Backed out changeset 7dee76ec48c4 (bug 1615727) for causing Talos failures.
    CLOSED TREE
    
    UltraBlame original commit: 5490c9f430b8ac94093156d0490b95f31507e3cf

--- testing/talos/talos/gecko_profile.py
+++ testing/talos/talos/gecko_profile.py
@@ -82,12 +82,6 @@ class GeckoProfile(object):
         
         
         env.update({
-            
-            
-            
-            
-            'MOZ_BASE_PROFILER_STARTUP': '1',
-
             'MOZ_PROFILER_STARTUP': '1',
             'MOZ_PROFILER_STARTUP_INTERVAL': str(self.option('interval')),
             'MOZ_PROFILER_STARTUP_ENTRIES': str(self.option('entries')),
