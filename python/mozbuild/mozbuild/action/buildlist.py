4bd2bf468a8b3cca457c33a5fac14d27c70bfd87
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-02-28 22:12:20 +0000

    Backed out 6 changesets (bug 1617303, bug 1617313, bug 1616885, bug 1616920, bug 1616989, bug 1617014) for causing gecko decision task bustages in talos/unittests/python.ini
    CLOSED TREE
    
    Backed out changeset 409619387576 (bug 1617313)
    Backed out changeset 538f415043e2 (bug 1617303)
    Backed out changeset 544cb78b8946 (bug 1617014)
    Backed out changeset 0f0bf0824a95 (bug 1616989)
    Backed out changeset ec9f0b18c0a7 (bug 1616920)
    Backed out changeset a32c3300a011 (bug 1616885)
    
    UltraBlame original commit: 5b71cf4df24cef4fc49ffd0d3b6ae17721f567a8

--- python/mozbuild/mozbuild/action/buildlist.py
+++ python/mozbuild/mozbuild/action/buildlist.py
@@ -1,15 +1,14 @@
-
-
-
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 '''A generic script to add entries to a file
 if the entry does not already exist.
 
 Usage: buildlist.py <filename> <entry> [<entry> ...]
 '''
-from __future__ import absolute_import, print_function, unicode_literals
+from __future__ import absolute_import, print_function
 
-import io
 import sys
 import os
 
@@ -24,10 +23,10 @@ def addEntriesToListFile(listFile, entries):
     add each entry in |entries| to the file, unless it is already
     present."""
     ensureParentDir(listFile)
-    lock = lock_file(listFile + '.lck')
+    lock = lock_file(listFile + ".lck")
     try:
         if os.path.exists(listFile):
-            f = io.open(listFile)
+            f = open(listFile)
             existing = set(x.strip() for x in f.readlines())
             f.close()
         else:
@@ -35,10 +34,10 @@ def addEntriesToListFile(listFile, entries):
         for e in entries:
             if e not in existing:
                 existing.add(e)
-        with io.open(listFile, 'w', newline='\n') as f:
-            f.write('\n'.join(sorted(existing)) + '\n')
+        with open(listFile, 'wb') as f:
+            f.write("\n".join(sorted(existing))+"\n")
     finally:
-        del lock  
+        del lock  # Explicitly release the lock_file to free it
 
 
 def main(args):
