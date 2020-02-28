5832bf1dde79df8b822bd23d39ddae599a34e8a2
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-02-28 22:13:30 +0000

    Bug 1618165 - Provide BaseAutoLock and BaseAutoUnlock deduction guides for Mutex references. r=froydnj
    Differential Revision: https://phabricator.services.mozilla.com/D64359
    
    UltraBlame original commit: b6ce0a07d782451a3fc8e58f59b3e480b4bc2cdd

--- ipc/mscom/WeakRef.cpp
+++ ipc/mscom/WeakRef.cpp
@@ -74,8 +74,8 @@ void SharedRef::Clear() {
 
 }  
 
-typedef BaseAutoLock<detail::SharedRef&> SharedRefAutoLock;
-typedef BaseAutoUnlock<detail::SharedRef&> SharedRefAutoUnlock;
+typedef detail::BaseAutoLock<detail::SharedRef&> SharedRefAutoLock;
+typedef detail::BaseAutoUnlock<detail::SharedRef&> SharedRefAutoUnlock;
 
 WeakReferenceSupport::WeakReferenceSupport(Flags aFlags)
     : mRefCnt(0), mFlags(aFlags) {
