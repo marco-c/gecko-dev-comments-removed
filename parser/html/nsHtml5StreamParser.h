c0619a4131a4cc05cfba7a936035d4ce20d2db99
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-02-29 15:53:07 +0000

    Bug 1618643 - Remove nsAutoPtr usage from parser. r=hsivonen
    Differential Revision: https://phabricator.services.mozilla.com/D64251
    
    UltraBlame original commit: e65a9431a729e41b7a09d91e2ba583962be88a85

--- parser/html/nsHtml5StreamParser.h
+++ parser/html/nsHtml5StreamParser.h
@@ -6,12 +6,12 @@
 #ifndef nsHtml5StreamParser_h
 #define nsHtml5StreamParser_h
 
-#include "nsAutoPtr.h"
 #include "nsCOMPtr.h"
 #include "nsHtml5MetaScanner.h"
 #include "mozilla/Encoding.h"
 #include "mozilla/EncodingDetector.h"
 #include "mozilla/JapaneseDetector.h"
+#include "mozilla/UniquePtr.h"
 #include "nsHtml5TreeOpExecutor.h"
 #include "nsHtml5OwningUTF16Buffer.h"
 #include "nsIInputStream.h"
@@ -429,7 +429,7 @@ class nsHtml5StreamParser final : public nsISupports {
   
 
 
-  nsAutoPtr<nsHtml5MetaScanner> mMetaScanner;
+  mozilla::UniquePtr<nsHtml5MetaScanner> mMetaScanner;
 
   
   
@@ -484,12 +484,12 @@ class nsHtml5StreamParser final : public nsISupports {
   
 
 
-  nsAutoPtr<nsHtml5TreeBuilder> mTreeBuilder;
+  mozilla::UniquePtr<nsHtml5TreeBuilder> mTreeBuilder;
 
   
 
 
-  nsAutoPtr<nsHtml5Tokenizer> mTokenizer;
+  mozilla::UniquePtr<nsHtml5Tokenizer> mTokenizer;
 
   
 
@@ -533,7 +533,7 @@ class nsHtml5StreamParser final : public nsISupports {
 
 
 
-  nsTArray<nsAutoPtr<nsHtml5Speculation>> mSpeculations;
+  nsTArray<mozilla::UniquePtr<nsHtml5Speculation>> mSpeculations;
   mozilla::Mutex mSpeculationMutex;
 
   
