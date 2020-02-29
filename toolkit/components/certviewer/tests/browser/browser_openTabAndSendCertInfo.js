1a8e64d6a4921ef4acd4953081cf49b2b35c9231
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-02-29 15:53:53 +0000

    Bug 1617983 - Adjust browser_openTabAndSendCertInfo test for running about:certificate in the privileged about content process. r=NeilDeakin
    The test was originally using BrowserTestUtils.waitForNewWindow with an argument for
    the expected initial tab URL which was a function rather than a string. This helper
    function never expects a function, but transparently passed it along to browserLoaded,
    which _can_ handle a function, so everything worked.
    
    With the privileged about content process enabled, the waitForNewWindow code fell
    down a codepath that doesn't handle the function parameter at all, and causes the
    test to wait for a XULFrameLoaderCreated event that will never fire.
    
    This patch adjusts the test to no longer pass the function to waitForNewWindow, since
    it never supported having a function passed to it. Instead, we do the check for the
    initial tab URL after the window has been opened.
    
    Differential Revision: https://phabricator.services.mozilla.com/D64603
    
    UltraBlame original commit: 623af81c196a283c8b1f1322bb2967b9b65e6251

--- toolkit/components/certviewer/tests/browser/browser_openTabAndSendCertInfo.js
+++ toolkit/components/certviewer/tests/browser/browser_openTabAndSendCertInfo.js
@@ -69,12 +69,27 @@ add_task(async function openFromPopUp() {
 
   let [win] = await openCertDownloadDialog(cert);
   let viewCertButton = win.document.getElementById("viewC-button");
-  let newWinOpened = BrowserTestUtils.waitForNewWindow({
-    url: spec => spec.startsWith("about:certificate"),
-  });
+  let newWinOpened = BrowserTestUtils.waitForNewWindow();
   viewCertButton.click();
+
   let topWin = await newWinOpened;
-  let spec = topWin.gBrowser.selectedBrowser.currentURI.spec;
+  let browser = topWin.gBrowser.selectedBrowser;
+
+  
+  
+  
+  
+  
+  
+  
+  
+  if (!browser.currentURI.spec.startsWith("about:certificate")) {
+    await BrowserTestUtils.browserLoaded(browser, false, spec =>
+      spec.startsWith("about:certificate")
+    );
+  }
+
+  let spec = browser.currentURI.spec;
   checkSpec(spec);
 
   await BrowserTestUtils.closeWindow(topWin); 
