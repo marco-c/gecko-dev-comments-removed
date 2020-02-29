6f84c97fd4c6d979733a65b4fa30645e36071753
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-02-29 15:52:13 +0000

    Bug 1618764 - Ensure test page is not in the cache; r=Standard8
    Differential Revision: https://phabricator.services.mozilla.com/D64716
    
    UltraBlame original commit: 4512768c541da0bea1e69ccd35158108e5b48dda

--- browser/components/search/test/browser/browser_searchTelemetry.js
+++ browser/components/search/test/browser/browser_searchTelemetry.js
@@ -152,7 +152,7 @@ add_task(async function test_simple_search_page_visit_telemetry() {
     {
       gBrowser,
       
-      url: getSERPUrl(getPageUrl(false, true)),
+      url: getSERPUrl(getPageUrl()) + `&random=${Math.random()}`,
     },
     async () => {
       let scalars = {};
