03eebe9e029417694fb598a9499ad924cff52695
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-03-03 21:01:24 +0000

    Bug 1617369 - Reformat bitsdownload/ using rustfmt r=valentin
    Depends on D64870
    
    Differential Revision: https://phabricator.services.mozilla.com/D64871
    
    UltraBlame original commit: 1eadd6558fe35b5243bc1cf9002a3e4cdb946f1f

--- toolkit/components/bitsdownload/src/bits_interface/request.rs
+++ toolkit/components/bitsdownload/src/bits_interface/request.rs
@@ -453,7 +453,8 @@ impl BitsRequest {
                 Pretask,
             ));
         }
-        self.cancel_action.set(CancelAction::InProgress(maybe_status));
+        self.cancel_action
+            .set(CancelAction::InProgress(maybe_status));
 
         let task: Box<CancelTask> = Box::new(CancelTask::new(
             RefPtr::new(self),
@@ -489,7 +490,8 @@ impl BitsRequest {
             
             if let Some(status) = maybe_status {
                 self.download_status_nsresult.set(status);
-                self.download_status_error_type.set(Some(BitsStateCancelled));
+                self.download_status_error_type
+                    .set(Some(BitsStateCancelled));
             }
         }
 
