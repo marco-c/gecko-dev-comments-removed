89b50988de75d49fc8777a1dda0cbcaf9830c48d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-03-03 21:03:19 +0000

    Bug 1618643 - Remove nsAutoPtr usage from parser. r=hsivonen
    Differential Revision: https://phabricator.services.mozilla.com/D64251
    
    UltraBlame original commit: 7ac3680310d3c9fdd358e728a07d267d9f52e04c

--- parser/html/nsHtml5TokenizerHSupplement.h
+++ parser/html/nsHtml5TokenizerHSupplement.h
@@ -14,7 +14,7 @@ inline nsHtml5HtmlAttributes* GetAttributes() { return attributes; }
 
 bool EnsureBufferSpace(int32_t aLength);
 
-nsAutoPtr<nsHtml5Highlighter> mViewSource;
+mozilla::UniquePtr<nsHtml5Highlighter> mViewSource;
 
 
 
