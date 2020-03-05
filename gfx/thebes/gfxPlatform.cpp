b8a08ef53ec356c27dcb641bb4ede15e135e363d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-03-05 10:08:10 +0000

    Bug 1616623 - Switching gfx telemetry to user lifetimes and removing last_seen r=chutten
    Differential Revision: https://phabricator.services.mozilla.com/D63361
    
    UltraBlame original commit: 23373df819c831dcffc8ae519b4afe652d28ded8

--- gfx/thebes/gfxPlatform.cpp
+++ gfx/thebes/gfxPlatform.cpp
@@ -1174,10 +1174,6 @@ void gfxPlatform::ReportTelemetry() {
   gfxInfo->GetAdapterDeviceID(adapterDeviceId);
   Telemetry::ScalarSet(Telemetry::ScalarID::GFX_ADAPTER_DEVICE_ID,
                        adapterDeviceId);
-  
-  
-  Telemetry::ScalarSet(Telemetry::ScalarID::GFX_ADAPTER_DEVICE_ID_LAST_SEEN,
-                       adapterDeviceId);
 
   nsString adapterSubsystemId;
   gfxInfo->GetAdapterSubsysID(adapterSubsystemId);
@@ -3530,12 +3526,6 @@ void gfxPlatform::NotifyCompositorCreated(LayersBackend aBackend) {
   if (XRE_IsParentProcess()) {
     Telemetry::ScalarSet(
         Telemetry::ScalarID::GFX_COMPOSITOR,
-        NS_ConvertUTF8toUTF16(GetLayersBackendName(mCompositorBackend)));
-
-    
-    
-    Telemetry::ScalarSet(
-        Telemetry::ScalarID::GFX_COMPOSITOR_LAST_SEEN,
         NS_ConvertUTF8toUTF16(GetLayersBackendName(mCompositorBackend)));
   }
 
