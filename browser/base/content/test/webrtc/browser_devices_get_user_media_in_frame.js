82e17695b94b69dfb43cc9f62d8b5bb433b44847
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-03-05 10:11:59 +0000

    Bug 1618447, gShouldObserveSubframes should only be set for the out of process frames part of the test, r=kmag
    Differential Revision: https://phabricator.services.mozilla.com/D64864
    
    UltraBlame original commit: ec7eaa5ccb3403a80b9131c70362ea6e24e489c7

--- browser/base/content/test/webrtc/browser_devices_get_user_media_in_frame.js
+++ browser/base/content/test/webrtc/browser_devices_get_user_media_in_frame.js
@@ -9,9 +9,7 @@ SpecialPowers.pushPrefEnv({
   ],
 });
 
-
-
-const gShouldObserveSubframes = SpecialPowers.useRemoteSubframes;
+let gShouldObserveSubframes = false;
 
 var gTests = [
   {
@@ -582,6 +580,10 @@ add_task(async function test_inprocess() {
 });
 
 add_task(async function test_outofprocess() {
+  
+  
+  gShouldObserveSubframes = SpecialPowers.useRemoteSubframes;
+
   let observeSubFrameIds = gShouldObserveSubframes ? ["frame1", "frame2"] : [];
   await runTests(gTests, {
     relativeURI: "get_user_media_in_oop_frame.html",
