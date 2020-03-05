b81ddc743fe7be5e782b11c46ba1b46ec4302633
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-03-05 10:13:48 +0000

    Bug 1617086 - Keep WebGPU pass dependencies alive while recording r=jgilbert
    Since we switched to "software" pass recording,
    `wgpu` no longer keeps references to the objects used when recording a pass.
    This means Gecko needs to keep them alive up until the encoding is done.
    
    Differential Revision: https://phabricator.services.mozilla.com/D65319
    
    UltraBlame original commit: 6b25a4fb78431d4b92407fa29f2393f4e0094dd7

--- dom/webgpu/ComputePassEncoder.h
+++ dom/webgpu/ComputePassEncoder.h
@@ -14,6 +14,7 @@
 namespace mozilla {
 namespace webgpu {
 
+class BindGroup;
 class Buffer;
 class CommandEncoder;
 class ComputePipeline;
@@ -32,6 +33,9 @@ class ComputePassEncoder final : public ObjectBase,
   void Cleanup() {}
 
   ffi::WGPURawPass mRaw;
+  
+  std::vector<RefPtr<const BindGroup>> mUsedBindGroups;
+  std::vector<RefPtr<const ComputePipeline>> mUsedPipelines;
 
  public:
   void SetBindGroup(uint32_t aSlot, const BindGroup& aBindGroup,
