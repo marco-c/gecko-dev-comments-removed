db62bfead7a0901a5005f03c1d1733ee381dc388
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-03-05 10:09:18 +0000

    Bug 1613835 - Don't try to merge synced bookmarks if a transaction is already open. r=markh
    Occasionally, we might try to apply synced bookmarks when a transaction
    is already in progress. Consider something like this:
    
    1. The user clicks the star button, which adds a bookmark to the
       default folder. Under the hood, this runs a transaction to
       completion—`BEGIN`, some `INSERT`s and `UPDATE`s, then `COMMIT`.
    2. The `item-added` observer notification kicks off a sync.
    3. The user, with the star UI still open, picks a new folder for the
       bookmark. This moves the bookmark under the hood.
    4. To move the bookmark, we run `BEGIN` on the Places connection's
       async thread. Remember, `Sqlite.jsm` runs async statements one at a
       time.
    5. Concurrently, the merge runnable is scheduled on the async thread.
       It's not aware of the `Sqlite.jsm` transaction queue, and doesn't
       know that a transaction for the move is already open.
    6. The merger tries to open its own transaction with `BEGIN`, fails
       noisly, and returns a "cannot start a transaction within a
       transaction" error back to the main thread.
    7. The move transaction started in (4) runs to completion, updating
       the new bookmark's parent and committing the changes.
    
    This is a case of bad timing—retrying the sync once the user finishes
    making changes will work—but reports errors in telemetry and logs.
    This commit downgrades those to warnings.
    
    Depends on D63732
    
    Differential Revision: https://phabricator.services.mozilla.com/D63734
    
    UltraBlame original commit: 36989870cc805a534eead080a260663c5dd7ff8b

--- toolkit/components/places/bookmark_sync/src/merger.rs
+++ toolkit/components/places/bookmark_sync/src/merger.rs
@@ -184,11 +184,20 @@ impl MergeTask {
             result: AtomicRefCell::new(Err(error::Error::DidNotRun)),
         })
     }
-}
 
-impl Task for MergeTask {
-    fn run(&self) {
+    fn merge(&self) -> error::Result<store::ApplyStatus> {
         let mut db = self.db.clone();
+        if db.transaction_in_progress()? {
+            
+            
+            
+            
+            
+            
+            
+            
+            return Err(error::Error::StorageBusy);
+        }
         let log = Logger::new(self.max_log_level, self.logger.clone());
         let driver = Driver::new(log, self.progress.clone());
         let mut store = store::Store::new(
@@ -199,10 +208,16 @@ impl Task for MergeTask {
             self.remote_time_millis,
             &self.weak_uploads,
         );
-        *self.result.borrow_mut() = store
-            .validate()
-            .and_then(|_| store.prepare())
-            .and_then(|_| store.merge_with_driver(&driver, &*self.controller));
+        store.validate()?;
+        store.prepare()?;
+        let status = store.merge_with_driver(&driver, &*self.controller)?;
+        Ok(status)
+    }
+}
+
+impl Task for MergeTask {
+    fn run(&self) {
+        *self.result.borrow_mut() = self.merge();
     }
 
     fn done(&self) -> Result<(), nsresult> {
