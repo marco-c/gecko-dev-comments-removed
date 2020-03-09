79b1927631df4f215e4c7b33e137cdac9d14f286
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-03-09 23:47:02 +0000

    Bug 1620177 - Fix differences in dependentlibs.list between Windows native and cross builds. r=rstewart
    Differential Revision: https://phabricator.services.mozilla.com/D65659
    
    UltraBlame original commit: 64f9399ad9e1780d059319b18e927b0debced944

--- toolkit/library/build/dependentlibs.py
+++ toolkit/library/build/dependentlibs.py
@@ -20,18 +20,26 @@ from mozpack.executables import (
 from buildconfig import substs
 
 def dependentlibs_win32_objdump(lib):
-    proc = subprocess.Popen([substs['LLVM_OBJDUMP'], '--private-headers', lib], stdout = subprocess.PIPE)
+    proc = subprocess.Popen([substs['LLVM_OBJDUMP'], '--private-headers', lib], stdout = subprocess.PIPE,
+                            universal_newlines=True)
     deps = []
     for line in proc.stdout:
         match = re.match('\s+DLL Name: (\S+)', line)
         if match:
-            deps.append(match.group(1))
+            
+            
+            
+            
+            
+            
+            deps.append(match.group(1).lower())
     proc.wait()
     return deps
 
 def dependentlibs_readelf(lib):
     '''Returns the list of dependencies declared in the given ELF .so'''
-    proc = subprocess.Popen([substs.get('TOOLCHAIN_PREFIX', '') + 'readelf', '-d', lib], stdout = subprocess.PIPE)
+    proc = subprocess.Popen([substs.get('TOOLCHAIN_PREFIX', '') + 'readelf', '-d', lib], stdout = subprocess.PIPE,
+                            universal_newlines=True)
     deps = []
     for line in proc.stdout:
         
@@ -53,7 +61,8 @@ def dependentlibs_readelf(lib):
 
 def dependentlibs_mac_objdump(lib):
     '''Returns the list of dependencies declared in the given MACH-O dylib'''
-    proc = subprocess.Popen([substs['LLVM_OBJDUMP'], '--private-headers', lib], stdout = subprocess.PIPE)
+    proc = subprocess.Popen([substs['LLVM_OBJDUMP'], '--private-headers', lib], stdout = subprocess.PIPE,
+                            universal_newlines=True)
     deps = []
     cmd = None
     for line in proc.stdout:
@@ -113,7 +122,7 @@ def gen_list(output, lib):
     output.write('\n'.join(deps.keys()) + '\n')
 
     with open(output.name + ".gtest", 'w') as gtest_out:
-        libs = deps.keys()
+        libs = list(deps.keys())
         libs[-1] = 'gtest/' + libs[-1]
         gtest_out.write('\n'.join(libs) + '\n')
 
