cce022ca54a98ec805ee81d86711cb5bb825726f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-03-09 23:54:20 +0000

    Bug 1615112 - part2 : use default title and favicon to fill missing properties if needed. r=MeFisto94
    We don't want to show empty information on the virtual control interface when media session's title and artwork are empty or doesn't be set properly. Therefore, in this situation, we should use default title and favicon icon instead.
    
    Differential Revision: https://phabricator.services.mozilla.com/D65656
    
    UltraBlame original commit: e26028b01ac474410f95141a96ee414d5fab5ee9

--- dom/media/mediasession/MediaSessionController.cpp
+++ dom/media/mediasession/MediaSessionController.cpp
@@ -209,9 +209,26 @@ MediaMetadataBase MediaSessionController::GetCurrentMediaMetadata() const {
   if (mActiveMediaSessionContextId) {
     Maybe<MediaMetadataBase> metadata =
         mMetadataMap.Get(*mActiveMediaSessionContextId);
-    return metadata ? *metadata : CreateDefaultMetadata();
+    if (!metadata) {
+      return CreateDefaultMetadata();
+    }
+    FillMissingTitleAndArtworkIfNeeded(*metadata);
+    return *metadata;
   }
   return CreateDefaultMetadata();
+}
+
+void MediaSessionController::FillMissingTitleAndArtworkIfNeeded(
+    MediaMetadataBase& aMetadata) const {
+  
+  
+  
+  if (aMetadata.mTitle.IsEmpty()) {
+    aMetadata.mTitle = GetDefaultTitle();
+  }
+  if (aMetadata.mArtwork.IsEmpty()) {
+    aMetadata.mArtwork.AppendElement()->mSrc = GetDefaultFaviconURL();
+  }
 }
 
 }  
