00bcecbf417bf5725b89e88ee8d3a0988afe4e5c
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-03-09 23:47:43 +0000

    Bug 1620482 - Don't treat non-user-triggered autofill into login forms as an edit. r=MattN
    * Dont flag a form as user-modified when the input is from autofilling a login
    * Avoid sending PasswordEditedOrGenerated messages to the parent from non-user-modified forms.
    
    Differential Revision: https://phabricator.services.mozilla.com/D65668
    
    UltraBlame original commit: aaedc7cf346b9bef8644d0f51e525305688019b6

--- toolkit/components/passwordmgr/test/browser/browser_doorhanger_password_edits.js
+++ toolkit/components/passwordmgr/test/browser/browser_doorhanger_password_edits.js
@@ -64,6 +64,10 @@ add_task(async function test_edit_password() {
 
   for (let testCase of testCases) {
     info("Test case: " + JSON.stringify(testCase));
+    
+    await LoginTestUtils.clearData();
+    await cleanupDoorhanger();
+    await cleanupPasswordNotifications();
 
     
     if (testCase.usernameInPageExists) {
@@ -88,6 +92,8 @@ add_task(async function test_edit_password() {
       );
     }
 
+    let formFilledPromise = listenForTestNotification("FormProcessed");
+
     await BrowserTestUtils.withNewTab(
       {
         gBrowser,
@@ -96,25 +102,35 @@ add_task(async function test_edit_password() {
           "passwordmgr/test/browser/form_basic.html",
       },
       async function(browser) {
+        await formFilledPromise;
+
+        
+        await initForm(browser, {
+          "#form-basic-username": testCase.usernameInPage,
+          "#form-basic-password": "",
+        });
+
+        info("Editing the form");
+        await changeContentFormValues(browser, {
+          "#form-basic-password": testCase.passwordInPage,
+        });
+
         
         
+        info("Submitting the form");
+        let formSubmittedPromise = listenForTestNotification("FormSubmit");
         let promiseShown = BrowserTestUtils.waitForEvent(
           PopupNotifications.panel,
           "popupshown",
           event => event.target == PopupNotifications.panel
         );
-        let formSubmittedPromise = listenForTestNotification("FormSubmit");
-        await changeContentFormValues(browser, {
-          "#form-basic-username": testCase.usernameInPage,
-          "#form-basic-password": testCase.passwordInPage,
-        });
-        await TestUtils.waitForTick();
-        await SpecialPowers.spawn(browser, [], async function() {
+        await SpecialPowers.spawn(browser, [], function() {
           content.document.getElementById("form-basic").submit();
         });
         await formSubmittedPromise;
 
         let notif = await waitForDoorhanger(browser, "any");
+        ok(!notif.dismissed, "Doorhanger is not dismissed");
         await promiseShown;
 
         
@@ -146,8 +162,9 @@ add_task(async function test_edit_password() {
           "popuphidden"
         );
         clickDoorhangerButton(notif, CHANGE_BUTTON);
-        let [result] = await promiseLogin;
         await promiseHidden;
+        info("Waiting for storage changed");
+        let [result] = await promiseLogin;
 
         
         let login = expectModifyLogin
@@ -174,12 +191,17 @@ add_task(async function test_edit_password() {
           expectedLogin.timeCreated = meta.timePasswordChanged;
         }
         verifyLogins([expectedLogin]);
-
-        await cleanupDoorhanger();
       }
     );
-
-    
-    Services.logins.removeAllLogins();
   }
 });
+
+async function initForm(browser, formDefaults = {}) {
+  await ContentTask.spawn(browser, formDefaults, async function(
+    selectorValues
+  ) {
+    for (let [sel, value] of Object.entries(selectorValues)) {
+      content.document.querySelector(sel).value = value;
+    }
+  });
+}
