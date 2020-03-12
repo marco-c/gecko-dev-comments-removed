8c2d5a62b0029074a2b987c53fd96f89da2a2034
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-03-12 13:03:40 +0000

    Bug 1620999 - Part 8: Assign FunctionTree's box in the constructor. r=mgaudet
    That way we don't need the extra `setFunctionBox()` method and with part 6 it's
    now easier to spot that `FunctionTree::funbox_` doesn't change after construction.
    
    Differential Revision: https://phabricator.services.mozilla.com/D66055
    
    UltraBlame original commit: 84c74c8221024fbd312b08f495e6ecb2fb407ac5

--- js/src/frontend/FunctionTree.h
+++ js/src/frontend/FunctionTree.h
@@ -1,8 +1,8 @@
-
-
-
-
-
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ * vim: set ts=8 sts=2 et sw=2 tw=80:
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #ifndef frontend_FunctionTree_h
 #define frontend_FunctionTree_h
@@ -18,29 +18,29 @@ namespace frontend {
 class FunctionBox;
 class ParserBase;
 
-
-
+// A tree of function nodes pointing to a FunctionBox and all its
+// nested inner functions.
 class FunctionTree {
   FunctionBox* funbox_;
 
   Vector<FunctionTree> children_;
 
  public:
-  explicit FunctionTree(JSContext* cx) : funbox_(nullptr), children_(cx) {}
+  FunctionTree(JSContext* cx, FunctionBox* funbox)
+      : funbox_(funbox), children_(cx) {}
 
-  
-  
-  
-  
-  FunctionTree* add(JSContext* cx) {
-    if (!children_.emplaceBack(cx)) {
+  // Note: If we're using vector type, the pointer returned here
+  // is only valid if the tree is only added to in DFS order
+  //
+  // Open to suggestions about how to do that better.
+  FunctionTree* add(JSContext* cx, FunctionBox* funbox) {
+    if (!children_.emplaceBack(cx, funbox)) {
       return nullptr;
     }
     return &children_.back();
   }
 
   FunctionBox* funbox() { return funbox_; }
-  void setFunctionBox(FunctionBox* node) { funbox_ = node; }
 
   using FunctionTreeVisitorFunction = bool (*)(ParserBase*, FunctionTree*);
   bool visitRecursively(JSContext* cx, ParserBase* parser,
@@ -64,11 +64,11 @@ class FunctionTree {
   static void dump(JSContext* cx, FunctionTree& node, int indent);
 };
 
-
-
-
-
-
+// Owner of a function tree
+//
+// Note: Function trees point to function boxes, which only have the lifetime of
+//       the BytecodeCompiler, so exercise caution when holding onto a
+//       holder.
 class FunctionTreeHolder {
  private:
   FunctionTree treeRoot_;
@@ -76,15 +76,15 @@ class FunctionTreeHolder {
 
  public:
   explicit FunctionTreeHolder(JSContext* cx)
-      : treeRoot_(cx), currentParent_(&treeRoot_) {}
+      : treeRoot_(cx, nullptr), currentParent_(&treeRoot_) {}
 
   FunctionTree* getFunctionTree() { return &treeRoot_; }
   FunctionTree* getCurrentParent() { return currentParent_; }
   void setCurrentParent(FunctionTree* parent) { currentParent_ = parent; }
 };
 
-
-
+// A class used to maintain our function tree as ParseContexts are
+// pushed and popped.
 class MOZ_RAII AutoPushTree {
   FunctionTreeHolder& holder_;
   FunctionTree* oldParent_ = nullptr;
@@ -96,7 +96,7 @@ class MOZ_RAII AutoPushTree {
   bool init(JSContext* cx, FunctionBox* box);
 };
 
-}  
-}  
+}  // namespace frontend
+}  // namespace js
 
 #endif
