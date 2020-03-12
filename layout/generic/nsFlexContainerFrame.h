da958ce96e28aea7cbf2a266b6d798a06112b962
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-03-12 12:57:01 +0000

    Bug 1620490 Part 5 - Make CreateOrClearFlexContainerInfo() return the container info pointer, and use the pointer in DoFlexLayout. r=dholbert
    Also, move UpdateFlexLineAndItemInfo() from the end of DoFlexLayout() to
    the end of Reflow() because I'm going to extract the computation of
    final size of flex container into a helper method in a later patch, and
    call it Reflow() just after DoFlexLayout().
    
    Differential Revision: https://phabricator.services.mozilla.com/D65783
    
    UltraBlame original commit: fdfb108df9e8aa5f68a68e46343f4493b0b486e6

--- layout/generic/nsFlexContainerFrame.h
+++ layout/generic/nsFlexContainerFrame.h
@@ -279,13 +279,19 @@ class nsFlexContainerFrame final : public nsContainerFrame {
       nscoord& aContentBoxCrossSize, nscoord& aFlexContainerAscent,
       nscoord aAvailableBSizeForContent, mozilla::LinkedList<FlexLine>& aLines,
       nsTArray<StrutInfo>& aStruts, const FlexboxAxisTracker& aAxisTracker,
-      nscoord aMainGapSize, nscoord aCrossGapSize, bool aHasLineClampEllipsis);
+      nscoord aMainGapSize, nscoord aCrossGapSize, bool aHasLineClampEllipsis,
+      ComputedFlexContainerInfo* const aContainerInfo);
 
   
 
 
 
-  void CreateOrClearFlexContainerInfo();
+
+
+
+
+
+  ComputedFlexContainerInfo* CreateOrClearFlexContainerInfo();
 
   
 
