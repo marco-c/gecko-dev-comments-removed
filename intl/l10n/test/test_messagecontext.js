73e1bed908b96dfe48567375a60f83335557f531
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-03-12 13:00:27 +0000

    Bug 1560038 - Switch uses of FluentBundle to use fluent-rs. r=jfkthame
    Differential Revision: https://phabricator.services.mozilla.com/D57403
    
    UltraBlame original commit: 350df98095f8ff32bd688a59803ddb2d733483b9

--- intl/l10n/test/test_messagecontext.js
+++ intl/l10n/test/test_messagecontext.js
@@ -2,9 +2,6 @@
 
 
 function run_test() {
-  const { FluentBundle, FluentResource } =
-    ChromeUtils.import("resource://gre/modules/Fluent.jsm");
-
   test_methods_presence(FluentBundle);
   test_methods_calling(FluentBundle, FluentResource);
 
