1eb52bea69b67b5f4ecd8e28fa3ef1a82a08d963
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-03-12 13:09:37 +0000

    Bug 1621476 - Disable WebAuthn on GeckoView r=jcj
    We'll enable it once it's actually supported.
    
    Differential Revision: https://phabricator.services.mozilla.com/D66401
    
    UltraBlame original commit: fac88f2cec9c34349e1f6742a41bfd0002bf3ae0

--- mobile/android/app/geckoview-prefs.js
+++ mobile/android/app/geckoview-prefs.js
@@ -72,6 +72,7 @@ pref("ui.android.mouse_as_touch", 2);
 
 
 pref("security.webauth.webauthn_enable_android_fido2", false);
+pref("security.webauth.webauthn", false);
 
 
 pref("media.geckoview.autoplay.request", true);
