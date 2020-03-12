e37fa6e3a2b0f6218f3de58a8a895832ce575256
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-03-12 12:58:49 +0000

    Bug 1621106 - Bump minor version for mozbase modules depending on mozlog 6.0 and/or mozcrash 2.0; r=wlach
    Minor version bumps in preparation for new pypi releases of modules now requiring mozlog 6.0 and/or mozcrash 2.0.
    
    Differential Revision: https://phabricator.services.mozilla.com/D66116
    
    UltraBlame original commit: 9e9f8b9f4af5b3df1da57b9614c6dac458d47dfe

--- testing/mozbase/manifestparser/setup.py
+++ testing/mozbase/manifestparser/setup.py
@@ -1,13 +1,13 @@
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this file,
-# You can obtain one at http://mozilla.org/MPL/2.0/.
+
+
+
 
 from __future__ import absolute_import
 
 from setuptools import setup
 
 PACKAGE_NAME = "manifestparser"
-PACKAGE_VERSION = '2.0.0'
+PACKAGE_VERSION = '2.1.0'
 
 DEPS = [
     'mozlog >= 6.0',
@@ -21,7 +21,7 @@ setup(name=PACKAGE_NAME,
                    'Programming Language :: Python :: 3',
                    'Programming Language :: Python :: 3.5',
                    ],
-                  # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+                  
       keywords='mozilla manifests',
       author='Mozilla Automation and Testing Team',
       author_email='tools@lists.mozilla.org',
