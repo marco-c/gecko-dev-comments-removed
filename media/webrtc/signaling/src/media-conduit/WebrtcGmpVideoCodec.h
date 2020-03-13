ac7a060903862f4d04d9fcdcd0e0b0c441e31d2b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-03-13 21:33:35 +0000

    Bug 1544181 - Check for large frames in GMPDecodeData; r=ng
    Differential Revision: https://phabricator.services.mozilla.com/D64341
    
    UltraBlame original commit: 2dfe1500d02eebf29c7f02d042246e7fc843ead9

--- media/webrtc/signaling/src/media-conduit/WebrtcGmpVideoCodec.h
+++ media/webrtc/signaling/src/media-conduit/WebrtcGmpVideoCodec.h
@@ -126,6 +126,8 @@ class GMPDecodeData {
         mRenderTimeMs(aRenderTimeMs) {
     
     
+    MOZ_RELEASE_ASSERT(aInputImage._length <
+                       (std::numeric_limits<size_t>::max() >> 1));
     mImage._length = aInputImage._length;
     mImage._size =
         aInputImage._length +
