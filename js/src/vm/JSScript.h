112a06440830615d65eab417996e8bfa278bd0b4
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-03-16 12:00:19 +0000

    Bug 1619803 - Add BaseScript::swapData for updating data_ field r=sfink
    Add helper method to manage updating BaseScript::data_ that correctly
    notifies the GC MemoryUse tracking. This simplifies things later when we will
    need to update data_ during delazification / relazification.
    
    Care must be taken to barrier the PrivateScriptData when disconnecting it
    from the object graph. The location it is swapped to may already have been
    traced so barriers are required at the source.
    
    Differential Revision: https://phabricator.services.mozilla.com/D66140
    
    UltraBlame original commit: 424df8f11c16da585fb070968ca966b7ba28d1fa

--- js/src/vm/JSScript.h
+++ js/src/vm/JSScript.h
@@ -2348,6 +2348,9 @@ setterLevel:                                                                  \
 
   bool hasPrivateScriptData() const { return data_ != nullptr; }
 
+  
+  void swapData(UniquePtr<PrivateScriptData>& other);
+
   mozilla::Span<const JS::GCCellPtr> gcthings() const {
     return data_ ? data_->gcthings() : mozilla::Span<JS::GCCellPtr>();
   }
