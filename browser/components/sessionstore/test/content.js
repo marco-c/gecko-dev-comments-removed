44ae15beef00c4aa201af07129127e05992ddd56
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-03-16 12:02:32 +0000

    Bug 1467437.  Align with other browsers and the spec for whether hashchange and popstate events bubble. r=smaug
    I think I caught all the places in our tree that add event listeners for these
    events on a non-Window (and hence depend on bubbling behavior), but I'm a
    little worried about what happens if webextensions do bareword
    addEventListener() for these events...
    
    Differential Revision: https://phabricator.services.mozilla.com/D66798
    
    UltraBlame original commit: c4c3a54fcffa79aca314246e3ea22ad000e34748

--- browser/components/sessionstore/test/content.js
+++ browser/components/sessionstore/test/content.js
@@ -48,9 +48,13 @@ if (sessionHistory) {
 
 
 
-addEventListener("hashchange", function() {
-  sendAsyncMessage("ss-test:hashchange");
-});
+addEventListener(
+  "hashchange",
+  function() {
+    sendAsyncMessage("ss-test:hashchange");
+  },
+  true
+);
 
 addMessageListener("ss-test:getStyleSheets", function(msg) {
   let sheets = content.document.styleSheets;
