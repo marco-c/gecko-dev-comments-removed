de4a2da79a8c4ca004efbf61ec3b33fbcd15655f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-03-16 12:04:33 +0000

    Backed out changeset c0235b7bddcc (bug 1621415) for Mochitest perma failures on test_printpreview.xhtml. CLOSED TREE
    UltraBlame original commit: db34bff4aa792a7fb5254201015e9ce85e22ab3d

--- dom/base/DocumentOrShadowRoot.h
+++ dom/base/DocumentOrShadowRoot.h
@@ -246,12 +246,6 @@ class DocumentOrShadowRoot {
   void ClearAdoptedStyleSheets();
 
   
-
-
-
-  void CloneAdoptedSheetsFrom(DocumentOrShadowRoot&);
-
-  
   already_AddRefed<StyleSheet> RemoveSheet(StyleSheet& aSheet);
   void InsertSheetAt(size_t aIndex, StyleSheet& aSheet);
 
