a5051314defc653da5747274717692e5dc8b66af
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-03-17 14:10:07 +0000

    Bug 1617170 - Make AsyncDeleteFile a member function of FileManager. r=janv,dom-workers-and-storage-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D65114
    
    UltraBlame original commit: 6fac755f3fd4893918944c27960427352c5f10cb

--- dom/indexedDB/ActorsParent.h
+++ dom/indexedDB/ActorsParent.h
@@ -1,8 +1,8 @@
-
-
-
-
-
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=8 sts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #ifndef mozilla_dom_indexeddb_actorsparent_h__
 #define mozilla_dom_indexeddb_actorsparent_h__
@@ -26,7 +26,7 @@ namespace quota {
 
 class Client;
 
-}  
+}  // namespace quota
 
 namespace indexedDB {
 
@@ -71,10 +71,8 @@ class PermissionRequestHelper final : public PermissionRequestBase {
   void OnPromptComplete(PermissionValue aPermissionValue) override;
 };
 
-nsresult AsyncDeleteFile(FileManager* aFileManager, int64_t aFileId);
+}  // namespace indexedDB
+}  // namespace dom
+}  // namespace mozilla
 
-}  
-}  
-}  
-
-#endif  
+#endif  // mozilla_dom_indexeddb_actorsparent_h__
