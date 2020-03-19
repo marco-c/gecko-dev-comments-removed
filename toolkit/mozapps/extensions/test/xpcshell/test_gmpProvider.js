4225391dde1ccfdfee712869abc27c155548e4d0
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-03-19 14:04:15 +0000

    Bug 1522930, delay GMP initialization until after first point to improve perceived performance. r=mconley
    Differential Revision: https://phabricator.services.mozilla.com/D67165
    
    UltraBlame original commit: 2c6d8b14baf788c7d03bcce48c9e76b14fface84

--- toolkit/mozapps/extensions/test/xpcshell/test_gmpProvider.js
+++ toolkit/mozapps/extensions/test/xpcshell/test_gmpProvider.js
@@ -58,6 +58,11 @@ MockGMPInstallManager.prototype = {
 
 add_task(async function setup() {
   createAppInfo("xpcshell@tests.mozilla.org", "XPCShell", "1", "1.9.2");
+
+  
+  
+  Services.obs.notifyObservers(null, "ipc:first-content-process-created");
+
   await promiseStartupManager();
 
   gPrefs.setBoolPref(GMPScope.GMPPrefs.KEY_LOGGING_DUMP, true);
