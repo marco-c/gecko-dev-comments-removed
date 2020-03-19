254fd11ef30f342cca1cf9bb226fe29e4c4398e8
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-03-19 14:01:44 +0000

    Bug 1617111, reduce the time forced to be used outside animation frame callbacks, r=farre
    Differential Revision: https://phabricator.services.mozilla.com/D67123
    
    UltraBlame original commit: 2bb5b1e1caf647c617838c455527a4b7fa25431b

--- layout/base/nsRefreshDriver.cpp
+++ layout/base/nsRefreshDriver.cpp
@@ -741,7 +741,7 @@ class VsyncRefreshDriverTimer : public RefreshDriverTimer {
       
       if (mVsyncRefreshDriverTimer) {
         timeForOutsideTick = TimeDuration::FromMilliseconds(
-            mVsyncRefreshDriverTimer->GetTimerRate().ToMilliseconds() / 10.0f);
+            mVsyncRefreshDriverTimer->GetTimerRate().ToMilliseconds() / 100.0f);
         RefPtr<VsyncRefreshDriverTimer> timer = mVsyncRefreshDriverTimer;
         timer->RunRefreshDrivers(aId, aVsyncTimestamp);
         
