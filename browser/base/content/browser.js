5218d7eca62404c29bb3125e23d0666f81878fdc
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-03-19 14:04:06 +0000

    Bug 1618696 - Prevent overlapping show/hide of the ConfirmationHint. r=MattN
    * Track and clear a timerID for the ConfirmationHint to avoid callbacks from one show() call interfering with a subsequent call.
    * Tighten up waiting for and verifying the confirmation hint in browser_doorhanger_generated_password.js
    * Pass in the correct browser when retrieving the anchorNode for the confirmation hint.
    
    Differential Revision: https://phabricator.services.mozilla.com/D67398
    
    UltraBlame original commit: 336f37c4efb6167e11b984f4c86929e413015776

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -9081,6 +9081,8 @@ TabModalPromptBox.prototype = {
 };
 
 var ConfirmationHint = {
+  _timerID: null,
+
   
 
 
@@ -9103,6 +9105,8 @@ var ConfirmationHint = {
 
 
   show(anchor, messageId, options = {}) {
+    this._reset();
+
     this._message.textContent = gBrowserBundle.GetStringFromName(
       `confirmationHint.${messageId}.label`
     );
@@ -9130,8 +9134,7 @@ var ConfirmationHint = {
       "popupshown",
       () => {
         this._animationBox.setAttribute("animate", "true");
-
-        setTimeout(() => {
+        this._timerID = setTimeout(() => {
           this._panel.hidePopup(true);
         }, DURATION + 120);
       },
@@ -9141,8 +9144,8 @@ var ConfirmationHint = {
     this._panel.addEventListener(
       "popuphidden",
       () => {
-        this._panel.removeAttribute("hidearrow");
-        this._animationBox.removeAttribute("animate");
+        
+        this._reset();
       },
       { once: true }
     );
@@ -9152,6 +9155,15 @@ var ConfirmationHint = {
       position: "bottomcenter topleft",
       triggerEvent: options.event,
     });
+  },
+
+  _reset() {
+    if (this._timerID) {
+      clearTimeout(this._timerID);
+      this._timerID = null;
+    }
+    this._panel.removeAttribute("hidearrow");
+    this._animationBox.removeAttribute("animate");
   },
 
   get _panel() {
