69a8d0716386adb1268ae6a2cc250a380d57c79e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-03-19 14:02:20 +0000

    Bug 1622757 - Start fewer parallel tasks if not enough work to satisfy maximum number of threads r=sfink
    Previously we could start more parallel tasks than there were work items.  This changes things around so we take a work item when we create a ParallelWorker, so that we can stop creating them if we run of of work.
    
    I change the foreground cell update code to not use ParallelWorker at all for work that happens entirely on the main thread and doesn't need synchronisation.
    
    Differential Revision: https://phabricator.services.mozilla.com/D66962
    
    UltraBlame original commit: ff16837de2697b5392c533b8940797f3cd59a160

--- js/src/gc/ParallelWork.h
+++ js/src/gc/ParallelWork.h
@@ -37,24 +37,34 @@ class ParallelWorker : public GCParallelTask {
 
   ParallelWorker(GCRuntime* gc, WorkFunc func, WorkItemIterator& work,
                  const SliceBudget& budget, AutoLockHelperThreadState& lock)
-      : GCParallelTask(gc), func_(func), work_(work), budget_(budget) {}
+      : GCParallelTask(gc),
+        func_(func),
+        work_(work),
+        budget_(budget),
+        item_(work.get()) {
+    
+    
+    work.next();
+  }
 
   void run() {
     
     AutoDisableProxyCheck noProxyCheck;
 
-    AutoLockHelperThreadState lock;
-    while (!work().done()) {
-      WorkItem item = work().get();
-      work().next();
-
-      AutoUnlockHelperThreadState unlock(lock);
-      size_t steps = func_(gc, item);
-
+    for (;;) {
+      size_t steps = func_(gc, item_);
       budget_.step(steps);
       if (budget_.isOverBudget()) {
         break;
       }
+
+      AutoLockHelperThreadState lock;
+      if (work().done()) {
+        break;
+      }
+
+      item_ = work().get();
+      work().next();
     }
   }
 
@@ -69,6 +79,9 @@ class ParallelWorker : public GCParallelTask {
 
   
   SliceBudget budget_;
+
+  
+  WorkItem item_;
 };
 
 static constexpr size_t MaxParallelWorkers = 8;
