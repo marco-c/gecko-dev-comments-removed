ef6dadaaf24e3150b64527455f4ee516433d1d1f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-03-19 14:04:09 +0000

    Bug 1622111 - Convert dom.use_components_shim to static pref. r=njn
    Converts dom.use_components_shim to a static pref and removes its `all.js` entry.
    
    Differential Revision: https://phabricator.services.mozilla.com/D67184
    
    UltraBlame original commit: caa735c8bb9154e686da69f986236ed188904e1a

--- dom/base/nsGlobalWindowInner.cpp
+++ dom/base/nsGlobalWindowInner.cpp
@@ -2813,14 +2813,7 @@ bool nsGlobalWindowInner::DoResolve(
   
   
   
-  static bool watchingComponentsPref = false;
-  static bool useComponentsShim = false;
-  if (!watchingComponentsPref) {
-    watchingComponentsPref = true;
-    Preferences::AddBoolVarCache(&useComponentsShim, "dom.use_components_shim",
-                                 true);
-  }
-  if (useComponentsShim &&
+  if (StaticPrefs::dom_use_components_shim() &&
       aId == XPCJSRuntime::Get()->GetStringID(XPCJSContext::IDX_COMPONENTS)) {
     return ResolveComponentsShim(aCx, aObj, aDesc);
   }
