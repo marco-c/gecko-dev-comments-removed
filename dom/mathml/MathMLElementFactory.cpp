3e81537d565ced4d011b048586eb2d17e672be77
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-03-19 13:59:37 +0000

    Bug 1377999 - Make MathML nodes to adapt the DOMArena changes r=smaug
    Differential Revision: https://phabricator.services.mozilla.com/D57702
    
    UltraBlame original commit: d377d84d850094fedd97cb9691f5a7bee55e1d50

--- dom/mathml/MathMLElementFactory.cpp
+++ dom/mathml/MathMLElementFactory.cpp
@@ -13,6 +13,8 @@ using namespace mozilla::dom;
 
 nsresult NS_NewMathMLElement(
     Element** aResult, already_AddRefed<mozilla::dom::NodeInfo>&& aNodeInfo) {
-  NS_ADDREF(*aResult = new MathMLElement(aNodeInfo));
+  RefPtr<mozilla::dom::NodeInfo> nodeInfo(aNodeInfo);
+  auto* nim = nodeInfo->NodeInfoManager();
+  NS_ADDREF(*aResult = new (nim) MathMLElement(nodeInfo.forget()));
   return NS_OK;
 }
