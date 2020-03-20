45facfd7b09fbe9d79cb87ba1a15c1a002be0f76
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-03-20 12:03:19 +0000

    Bug 1612534 - Switch to explicit (export (func)). r=lth
    This commit uses a regex to automatically change exports of indices
    to specify that they are exporting functions.
    
    Differential Revision: https://phabricator.services.mozilla.com/D67250
    
    UltraBlame original commit: a783089c764d1145650af4409cee7262802b6f82

--- js/src/jit-test/tests/wasm/fence.js
+++ js/src/jit-test/tests/wasm/fence.js
@@ -4,7 +4,7 @@
 wasmFullPass(
 `(module
 	(func atomic.fence)
-	(export "run" 0)
+	(export "run" (func 0))
 )`);
 
 
@@ -12,7 +12,7 @@ wasmFullPass(
 `(module
 	(memory 1)
 	(func atomic.fence)
-	(export "run" 0)
+	(export "run" (func 0))
 )`);
 
 
@@ -20,5 +20,5 @@ wasmFullPass(
 `(module
 	(memory 1 1 shared)
 	(func atomic.fence)
-	(export "run" 0)
+	(export "run" (func 0))
 )`);
