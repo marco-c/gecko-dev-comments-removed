b764baf5d4c8f8c4cc4c7ad1801c7a912c83de0a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-03-23 22:31:47 +0000

    Bug 1622360 - Remove the split_render_roots pref and immediate codepaths that depend on it. r=jrmuizel
    Differential Revision: https://phabricator.services.mozilla.com/D67613
    
    UltraBlame original commit: 57d53e839d56ac51cb0ccf858ee6f6b5406cbfa8

--- gfx/layers/wr/WebRenderBridgeChild.cpp
+++ gfx/layers/wr/WebRenderBridgeChild.cpp
@@ -32,6 +32,7 @@ WebRenderBridgeChild::WebRenderBridgeChild(const wr::PipelineId& aPipelineId)
       mManager(nullptr),
       mIPCOpen(false),
       mDestroyed(false),
+      mSentDisplayList(false),
       mFontKeysDeleted(),
       mFontInstanceKeysDeleted() {}
 
@@ -73,9 +74,7 @@ void WebRenderBridgeChild::DoDestroy() {
 
 void WebRenderBridgeChild::AddWebRenderParentCommand(
     const WebRenderParentCommand& aCmd, wr::RenderRoot aRenderRoot) {
-  MOZ_ASSERT(aRenderRoot == wr::RenderRoot::Default ||
-             (XRE_IsParentProcess() &&
-              StaticPrefs::gfx_webrender_split_render_roots_AtStartup()));
+  MOZ_ASSERT(aRenderRoot == wr::RenderRoot::Default);
   mParentCommands[aRenderRoot].AppendElement(aCmd);
 }
 
@@ -118,9 +117,7 @@ void WebRenderBridgeChild::EndTransaction(
   TimeStamp fwdTime = TimeStamp::Now();
 
   for (auto& renderRoot : aRenderRoots) {
-    MOZ_ASSERT(renderRoot.mRenderRoot == wr::RenderRoot::Default ||
-               (XRE_IsParentProcess() &&
-                StaticPrefs::gfx_webrender_split_render_roots_AtStartup()));
+    MOZ_ASSERT(renderRoot.mRenderRoot == wr::RenderRoot::Default);
     renderRoot.mCommands = std::move(mParentCommands[renderRoot.mRenderRoot]);
     renderRoot.mIdNamespace = mIdNamespace;
   }
@@ -130,6 +127,7 @@ void WebRenderBridgeChild::EndTransaction(
     mManager->TakeCompositionPayloads(payloads);
   }
 
+  mSentDisplayList = true;
   this->SendSetDisplayList(
       std::move(aRenderRoots), mDestroyedActors, GetFwdTransactionId(),
       aTransactionId, aContainsSVGGroup, aVsyncId, aVsyncStartTime,
@@ -156,9 +154,7 @@ void WebRenderBridgeChild::EndEmptyTransaction(
   TimeStamp fwdTime = TimeStamp::Now();
 
   for (auto& update : aRenderRootUpdates) {
-    MOZ_ASSERT(update.mRenderRoot == wr::RenderRoot::Default ||
-               (XRE_IsParentProcess() &&
-                StaticPrefs::gfx_webrender_split_render_roots_AtStartup()));
+    MOZ_ASSERT(update.mRenderRoot == wr::RenderRoot::Default);
     update.mCommands = std::move(mParentCommands[update.mRenderRoot]);
   }
 
@@ -185,8 +181,7 @@ void WebRenderBridgeChild::ProcessWebRenderParentCommands() {
 
   for (auto renderRoot : wr::kRenderRoots) {
     if (!mParentCommands[renderRoot].IsEmpty()) {
-      MOZ_ASSERT(renderRoot == wr::RenderRoot::Default ||
-                 StaticPrefs::gfx_webrender_split_render_roots_AtStartup());
+      MOZ_ASSERT(renderRoot == wr::RenderRoot::Default);
       this->SendParentCommands(mParentCommands[renderRoot], renderRoot);
       mParentCommands[renderRoot].Clear();
     }
@@ -548,6 +543,7 @@ mozilla::ipc::IPCResult WebRenderBridgeChild::RecvWrReleasedImages(
 }
 
 void WebRenderBridgeChild::BeginClearCachedResources() {
+  mSentDisplayList = false;
   mIsInClearCachedResources = true;
   
   
