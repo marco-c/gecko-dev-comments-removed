df0b911152103d2f03a84896cde81838c2af2610
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-03-23 22:33:58 +0000

    Bug 1614933 - Ensure that glibc's lazy initializers run before we enable the content process sandbox on Linux; r=jld
    Differential Revision: https://phabricator.services.mozilla.com/D63471
    
    UltraBlame original commit: 578c8ba9598fa427cf9e6b6dd0762bb537bba0d5

--- security/sandbox/linux/Sandbox.cpp
+++ security/sandbox/linux/Sandbox.cpp
@@ -27,6 +27,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/mman.h>
 #include <sys/prctl.h>
 #include <sys/ptrace.h>
 #include <sys/syscall.h>
@@ -500,6 +501,14 @@ void SandboxEarlyInit() {
   }
 }
 
+static void RunGlibcLazyInitializers() {
+  
+  int fd = shm_open("/dummy", O_RDONLY, 0);
+  if (fd > 0) {
+    close(fd);  
+  }
+}
+
 static void SandboxLateInit() {
 #ifdef NIGHTLY_BUILD
   gSandboxCrashOnError = true;
@@ -516,6 +525,8 @@ static void SandboxLateInit() {
       gSandboxCrashOnError = envVar[0] != '0';
     }
   }
+
+  RunGlibcLazyInitializers();
 }
 
 
