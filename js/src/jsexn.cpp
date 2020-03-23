757497d09b38480ea778b15250b1bc168ee49d34
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-03-23 22:32:06 +0000

    Bug 1622562 - Remove JSREPORT_IS_WARNING from js/src. r=tcampbell
    Differential Revision: https://phabricator.services.mozilla.com/D67028
    
    UltraBlame original commit: e76f213e7f04ad207540fa5ce6086e3d979a96dc

--- js/src/jsexn.cpp
+++ js/src/jsexn.cpp
@@ -277,8 +277,7 @@ JS_FRIEND_API JSLinearString* js::GetErrorTypeName(JSContext* cx,
 
 void js::ErrorToException(JSContext* cx, JSErrorReport* reportp,
                           JSErrorCallback callback, void* userRef) {
-  MOZ_ASSERT(reportp);
-  MOZ_ASSERT(!JSREPORT_IS_WARNING(reportp->flags));
+  MOZ_ASSERT(!reportp->isWarning());
 
   
   
