22c94cea4758920b598f8fe7b8c5e2df58beea34
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-03-25 17:40:21 +0000

    Bug 1477936 - fixing document node highlighting in accessibility panel. r=mtigley
    Differential Revision: https://phabricator.services.mozilla.com/D66616
    
    UltraBlame original commit: f3aefb524279042061cd69ddb8af40379ee80cab

--- devtools/server/actors/highlighters/accessible.js
+++ devtools/server/actors/highlighters/accessible.js
@@ -13,7 +13,10 @@ const {
   createSVGNode,
   isNodeValid,
 } = require("devtools/server/actors/highlighters/utils/markup");
-const { TEXT_NODE } = require("devtools/shared/dom-node-constants");
+const {
+  TEXT_NODE,
+  DOCUMENT_NODE,
+} = require("devtools/shared/dom-node-constants");
 const { setIgnoreLayoutChanges } = require("devtools/shared/layout/utils");
 
 loader.lazyRequireGetter(
@@ -154,11 +157,12 @@ class AccessibleHighlighter extends AutoRefreshHighlighter {
     this.highlighterEnv.off("will-navigate", this.onWillNavigate);
     this.pageListenerTarget.removeEventListener("pagehide", this.onPageHide);
     this.pageListenerTarget = null;
+
+    AutoRefreshHighlighter.prototype.destroy.call(this);
+
     this.accessibleInfobar.destroy();
     this.accessibleInfobar = null;
-
     this.markup.destroy();
-    AutoRefreshHighlighter.prototype.destroy.call(this);
   }
 
   
@@ -181,7 +185,11 @@ class AccessibleHighlighter extends AutoRefreshHighlighter {
 
 
   _isNodeValid(node) {
-    return super._isNodeValid(node) || isNodeValid(node, TEXT_NODE);
+    return (
+      super._isNodeValid(node) ||
+      isNodeValid(node, TEXT_NODE) ||
+      isNodeValid(node, DOCUMENT_NODE)
+    );
   }
 
   
