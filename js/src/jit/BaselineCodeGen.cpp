5785eaf27596349cffc126f0a6130a89c5971ab9
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-03-31 15:36:34 +0000

    Bug 1622530 - Part 3: Specialize JSOp::BuiltinProto to JSOp::FunctionProto to decouple bytecode from JSProtoKey. r=jorendorff,jandem
    Differential Revision: https://phabricator.services.mozilla.com/D66904
    
    UltraBlame original commit: a5ba53c1a73d99d3e2e06b013115fa218dc41b8a

--- js/src/jit/BaselineCodeGen.cpp
+++ js/src/jit/BaselineCodeGen.cpp
@@ -6402,24 +6402,22 @@ bool BaselineCodeGen<Handler>::emit_InitHomeObject() {
 }
 
 template <>
-bool BaselineCompilerCodeGen::emit_BuiltinProto() {
+bool BaselineCompilerCodeGen::emit_FunctionProto() {
   
-  JSObject* builtin = BuiltinProtoOperation(cx, handler.pc());
-  if (!builtin) {
+  JSObject* funProto = FunctionProtoOperation(cx);
+  if (!funProto) {
     return false;
   }
-  frame.push(ObjectValue(*builtin));
+  frame.push(ObjectValue(*funProto));
   return true;
 }
 
 template <>
-bool BaselineInterpreterCodeGen::emit_BuiltinProto() {
+bool BaselineInterpreterCodeGen::emit_FunctionProto() {
   prepareVMCall();
 
-  pushBytecodePCArg();
-
-  using Fn = JSObject* (*)(JSContext*, jsbytecode*);
-  if (!callVM<Fn, BuiltinProtoOperation>()) {
+  using Fn = JSObject* (*)(JSContext*);
+  if (!callVM<Fn, FunctionProtoOperation>()) {
     return false;
   }
 
