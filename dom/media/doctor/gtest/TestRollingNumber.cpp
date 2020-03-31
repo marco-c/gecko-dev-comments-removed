69d8006847db64d9fa9953ee04b103aa10dc43c3
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-03-31 15:36:24 +0000

    Bug 1625138 - Part 38: Replace mozilla::IsSame with std::is_same in dom/. r=froydnj
    Differential Revision: https://phabricator.services.mozilla.com/D68557
    
    UltraBlame original commit: 85b79a5f1f0b5d2f3b133bc71632a57d67721cab

--- dom/media/doctor/gtest/TestRollingNumber.cpp
+++ dom/media/doctor/gtest/TestRollingNumber.cpp
@@ -11,13 +11,14 @@
 
 #include <cstdint>
 #include <gtest/gtest.h>
+#include <type_traits>
 
 using RN8 = mozilla::RollingNumber<uint8_t>;
 
 TEST(RollingNumber, Value)
 {
   
-  static_assert(mozilla::IsSame<RN8::ValueType, uint8_t>::value, "");
+  static_assert(std::is_same_v<RN8::ValueType, uint8_t>, "");
 
   
   const RN8 n;
