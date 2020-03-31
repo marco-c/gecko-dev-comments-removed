2aac0fdb9edbceb4a0ba6fb36b12d0079754ef07
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-03-31 15:40:55 +0000

    Backed out 2 changesets (bug 1595596) for build bustage in build/src/mozglue/misc/MmapFaultHandler.cpp. CLOSED TREE
    Backed out changeset 34c018c96749 (bug 1595596)
    Backed out changeset 1e21eefd5fce (bug 1595596)
    
    UltraBlame original commit: 9a71a1f4ed3343ff46ddf7d2e65e0d283bc741f2

--- modules/libjar/nsZipArchive.cpp
+++ modules/libjar/nsZipArchive.cpp
@@ -16,7 +16,7 @@
 #  include "brotli/decode.h"  
 #endif
 #include "nsISupportsUtils.h"
-#include "mozilla/MmapFaultHandler.h"
+#include "MmapFaultHandler.h"
 #include "prio.h"
 #include "plstr.h"
 #include "mozilla/Attributes.h"
