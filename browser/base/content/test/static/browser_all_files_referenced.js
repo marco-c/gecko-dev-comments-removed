92902e7a108f055c347311b53676423d27cbc53a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-03-31 15:39:40 +0000

    Bug 1348533 - Remove unused buttons.png/downloadButtons.png from chrome://mozapps/skin/downloads/. r=ntim
    Differential Revision: https://phabricator.services.mozilla.com/D68887
    
    UltraBlame original commit: bcfa49b5212b8309807609ad6af8a602bff3d905

--- browser/base/content/test/static/browser_all_files_referenced.js
+++ browser/base/content/test/static/browser_all_files_referenced.js
@@ -186,15 +186,6 @@ var whitelist = [
   { file: "chrome://marionette/content/test_dialog.properties" },
   { file: "chrome://marionette/content/test_dialog.xhtml" },
   
-  {
-    file: "chrome://mozapps/skin/downloads/buttons.png",
-    platforms: ["macosx"],
-  },
-  {
-    file: "chrome://mozapps/skin/downloads/downloadButtons.png",
-    platforms: ["linux", "win"],
-  },
-  
   { file: "chrome://pippki/content/resetpassword.xhtml" },
   
   { file: "resource://gre/modules/Manifest.jsm" },
