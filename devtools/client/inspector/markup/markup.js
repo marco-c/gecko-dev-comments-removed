89106077f3555db5840720c8adc8ecefac58b912
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-03-31 15:33:16 +0000

    Bug 1625209 - Remove keyboard shortcuts when destroying the DevTools MarkupView r=rcaliman
    Differential Revision: https://phabricator.services.mozilla.com/D68410
    
    UltraBlame original commit: 0248631210d2178fafa88fefa1e6ae1ab7ce07fe

--- devtools/client/inspector/markup/markup.js
+++ devtools/client/inspector/markup/markup.js
@@ -1135,6 +1135,10 @@ MarkupView.prototype = {
       window: this.win,
     });
 
+    
+    
+    this._shortcuts = shortcuts;
+
     this._onShortcut = this._onShortcut.bind(this);
 
     
@@ -2299,6 +2303,11 @@ MarkupView.prototype = {
     if (this._undo) {
       this._undo.destroy();
       this._undo = null;
+    }
+
+    if (this._shortcuts) {
+      this._shortcuts.destroy();
+      this._shortcuts = null;
     }
 
     this.popup.destroy();
