e64ec3cece9b371d1ebb69116ea4d0de4b85f6ed
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-03-31 15:29:17 +0000

    Bug 1616679: Map accessible actions to Mac actions based on action name. r=eeejay
    Differential Revision: https://phabricator.services.mozilla.com/D67200
    
    UltraBlame original commit: 570f25f87e0a1d49e413b2ab92710ffd58c5fdbb

--- accessible/mac/mozHTMLAccessible.mm
+++ accessible/mac/mozHTMLAccessible.mm
@@ -1,9 +1,9 @@
-/* -*- Mode: Objective-C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
-/* vim:expandtab:shiftwidth=2:tabstop=2:
- */
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+
+
+
+
+
 
 #import "mozHTMLAccessible.h"
 
@@ -18,8 +18,8 @@
   nsAutoString title;
   if (AccessibleWrap* accWrap = [self getGeckoAccessible]) {
     mozilla::ErrorResult rv;
-    // XXX use the flattening API when there are available
-    // see bug 768298
+    
+    
     accWrap->GetContent()->GetTextContent(title, rv);
   } else if (ProxyAccessible* proxy = [self getProxyAccessible]) {
     proxy->Title(title);
@@ -48,7 +48,7 @@
 @implementation mozLinkAccessible
 
 - (NSArray*)accessibilityAttributeNames {
-  // if we're expired, we don't support any attributes.
+  
   if (![self getGeckoAccessible] && ![self getProxyAccessible]) return [NSArray array];
 
   static NSMutableArray* attributes = nil;
@@ -68,31 +68,12 @@
 }
 
 - (NSArray*)accessibilityActionNames {
-  // if we're expired, we don't support any actions.
+  
   if (![self getGeckoAccessible] && ![self getProxyAccessible]) return [NSArray array];
 
-  // Always advertise press action first.
+  
   return [@[ NSAccessibilityPressAction ]
       arrayByAddingObjectsFromArray:[super accessibilityActionNames]];
-}
-
-- (void)accessibilityPerformAction:(NSString*)action {
-  AccessibleWrap* accWrap = [self getGeckoAccessible];
-  ProxyAccessible* proxy = [self getProxyAccessible];
-  if (!accWrap && !proxy) {
-    return;
-  }
-
-  if ([action isEqualToString:NSAccessibilityPressAction]) {
-    if (accWrap) {
-      accWrap->DoAction(0);
-    } else if (proxy) {
-      proxy->DoAction(0);
-    }
-    return;
-  }
-
-  [super accessibilityPerformAction:action];
 }
 
 - (NSString*)customDescription {
