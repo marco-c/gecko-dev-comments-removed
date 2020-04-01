24b32c708b9a0fb3360049c5c46c190e94651a48
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-04-01 13:26:11 +0000

    Bug 1597436 - Fix uses of nsIDocShellTreeItem in nsGlobalWindowOuter::SetNewDocument. r=emilio
    Differential Revision: https://phabricator.services.mozilla.com/D67359
    
    UltraBlame original commit: 83c7100427c4e8ca08aebc9a04a9a26751516a53

--- dom/base/nsGlobalWindowOuter.cpp
+++ dom/base/nsGlobalWindowOuter.cpp
@@ -2460,9 +2460,20 @@ nsresult nsGlobalWindowOuter::SetNewDocument(Document* aDocument,
     
     
     
-    if (!mDocShell ||
-        mDocShell->ItemType() != nsIDocShellTreeItem::typeChrome ||
-        mDoc->NodePrincipal()->IsSystemPrincipal()) {
+    const bool isContentAboutBlankInChromeDocshell = [&] {
+      if (!mDocShell) {
+        return false;
+      }
+
+      RefPtr<BrowsingContext> bc = mDocShell->GetBrowsingContext();
+      if (!bc || bc->GetType() != BrowsingContext::Type::Chrome) {
+        return false;
+      }
+
+      return !mDoc->NodePrincipal()->IsSystemPrincipal();
+    }();
+
+    if (!isContentAboutBlankInChromeDocshell) {
       newInnerWindow->mHasNotifiedGlobalCreated = true;
       nsContentUtils::AddScriptRunner(NewRunnableMethod(
           "nsGlobalWindowOuter::DispatchDOMWindowCreated", this,
