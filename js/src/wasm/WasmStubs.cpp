aadf06bda3fba494e39af60cf0a6bbc6be549594
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-04-01 13:26:19 +0000

    Backed out changeset 5c02a3e6d56e (bug 1622828) for build bustages on WasmInstance.cpp. CLOSED TREE
    UltraBlame original commit: dc1c34d3f21b9a0ee12ac53c7db1a75804c21feb

--- js/src/wasm/WasmStubs.cpp
+++ js/src/wasm/WasmStubs.cpp
@@ -1933,7 +1933,7 @@ static bool GenerateImportInterpExit(MacroAssembler& masm, const FuncImport& fi,
   i++;
 
   
-  unsigned argc = abiArgCount;
+  unsigned argc = fi.funcType().args().length();
   if (i->kind() == ABIArg::GPR) {
     masm.mov(ImmWord(argc), i->gpr());
   } else {
