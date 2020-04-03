6cba6e02639cf2ce6e3a2f3df5d54325f4e9e789
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-04-03 15:25:47 +0000

    Bug 1624902 - [taskgraph] Implement substrategies in the bugbug scheduler for dealing with platforms, r=marco
    The bugbug scheduler currently chooses which manifests are important, and we
    then run *every* task that contains those manifests. This is likely overkill
    and we can reduce the number of configurations we run these manifests on.
    
    Differential Revision: https://phabricator.services.mozilla.com/D68466
    
    UltraBlame original commit: 6d15fe012042ccf2aea65e0ef28a2a16150be73d

--- tools/tryselect/selectors/auto.py
+++ tools/tryselect/selectors/auto.py
@@ -28,7 +28,7 @@ def run(message='{msg}', push=True, closed_tree=False, try_config=None):
     
     
     try_config.setdefault('optimize-strategies',
-                          'taskgraph.optimize:experimental.bugbug_push_schedules')
+                          'taskgraph.optimize:experimental.bugbug.all')
 
     task_config = {
         'version': 2,
