a090a45c86fbba01551372e78a6e3b512d9379ea
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-04-03 15:25:52 +0000

    Bug 1622360 - Unwind super sketchy changes to glyph atlas code. r=jrmuizel
    Depends on D68863
    
    Differential Revision: https://phabricator.services.mozilla.com/D68864
    
    UltraBlame original commit: c472796b3ed16d7c12cdf23b29259bd9a084b822

--- gfx/thebes/gfxFontMissingGlyphs.cpp
+++ gfx/thebes/gfxFontMissingGlyphs.cpp
@@ -164,12 +164,7 @@ class WRUserData : public layers::LayerUserData,
   static UserDataKey sWRUserDataKey;
 };
 
-
-
-
-static const int CONTENT_RECT_GLYPH_ATLAS = 8;
-
-static RefPtr<SourceSurface> gWRGlyphAtlas[16];
+static RefPtr<SourceSurface> gWRGlyphAtlas[8];
 static LinkedList<WRUserData> gWRUsers;
 UserDataKey WRUserData::sWRUserDataKey;
 
@@ -224,14 +219,12 @@ static void PurgeWRGlyphAtlas() {
   
   for (WRUserData* user : gWRUsers) {
     auto* manager = user->mManager;
-    for (size_t i = 0; i < 16; i++) {
+    for (size_t i = 0; i < 8; i++) {
       if (gWRGlyphAtlas[i]) {
         uint32_t handle = (uint32_t)(uintptr_t)gWRGlyphAtlas[i]->GetUserData(
             reinterpret_cast<UserDataKey*>(manager));
         if (handle) {
-          wr::RenderRoot renderRoot = (i & CONTENT_RECT_GLYPH_ATLAS)
-                                          ? wr::RenderRoot::Content
-                                          : wr::RenderRoot::Default;
+          wr::RenderRoot renderRoot = wr::RenderRoot::Default;
           manager->GetRenderRootStateManager(renderRoot)
               ->AddImageKeyForDiscard(
                   wr::ImageKey{manager->WrBridge()->GetNamespace(), handle});
@@ -245,7 +238,7 @@ static void PurgeWRGlyphAtlas() {
     gWRUsers.popFirst()->Remove();
   }
   
-  for (size_t i = 0; i < 16; i++) {
+  for (size_t i = 0; i < 8; i++) {
     gWRGlyphAtlas[i] = nullptr;
   }
 }
@@ -259,7 +252,7 @@ WRUserData::~WRUserData() {
   
   
   if (isInList()) {
-    for (size_t i = 0; i < 16; i++) {
+    for (size_t i = 0; i < 8; i++) {
       if (gWRGlyphAtlas[i]) {
         gWRGlyphAtlas[i]->RemoveUserData(
             reinterpret_cast<UserDataKey*>(mManager));
@@ -279,12 +272,6 @@ static already_AddRefed<SourceSurface> GetWRGlyphAtlas(DrawTarget& aDrawTarget,
       key |= (aMat->_11 < 0 ? 1 : 0) | (aMat->_22 < 0 ? 2 : 0);
     }
   }
-  
-  
-  auto* tdt = static_cast<layout::TextDrawTarget*>(&aDrawTarget);
-  if (tdt->GetRenderRoot() == wr::RenderRoot::Content) {
-    key |= CONTENT_RECT_GLYPH_ATLAS;
-  }
 
   
   RefPtr<SourceSurface> atlas = gWRGlyphAtlas[key];
@@ -293,6 +280,9 @@ static already_AddRefed<SourceSurface> GetWRGlyphAtlas(DrawTarget& aDrawTarget,
     gWRGlyphAtlas[key] = atlas;
   }
 
+  
+  
+  auto* tdt = static_cast<layout::TextDrawTarget*>(&aDrawTarget);
   auto* manager = tdt->WrLayerManager();
   if (!atlas->GetUserData(reinterpret_cast<UserDataKey*>(manager))) {
     
