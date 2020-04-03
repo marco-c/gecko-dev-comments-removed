c81a3166f979d75ec4beae4b97136871eed04111
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-04-03 15:29:42 +0000

    Bug 1622661 - Removed 'else' after 'return'. r=tnikkel
    Differential Revision: https://phabricator.services.mozilla.com/D68998
    
    UltraBlame original commit: e3ba3bcc2dae969396fad39338621fc641d0e513

--- image/decoders/nsICODecoder.cpp
+++ image/decoders/nsICODecoder.cpp
@@ -358,16 +358,16 @@ LexerTransition<ICOState> nsICODecoder::SniffResource(const char* aData) {
     size_t toRead = mDirEntry->mBytesInRes - BITMAPINFOSIZE;
     return Transition::ToUnbuffered(ICOState::FINISHED_RESOURCE,
                                     ICOState::READ_RESOURCE, toRead);
-  } else {
-    
-    int32_t bihSize = LittleEndian::readUint32(aData);
-    if (bihSize != static_cast<int32_t>(BITMAPINFOSIZE)) {
-      return Transition::TerminateFailure();
-    }
-
-    
-    return ReadBIH(aData);
   }
+
+  
+  int32_t bihSize = LittleEndian::readUint32(aData);
+  if (bihSize != static_cast<int32_t>(BITMAPINFOSIZE)) {
+    return Transition::TerminateFailure();
+  }
+
+  
+  return ReadBIH(aData);
 }
 
 LexerTransition<ICOState> nsICODecoder::ReadResource() {
