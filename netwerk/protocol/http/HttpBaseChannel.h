ed22bccf8275ce3c0d4c93dbfe089cb6ceb720a9
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-04-03 15:29:31 +0000

    Bug 1605895 - Add a diagnostic assertion when an http channel on the child process loading CSS does not notify OnStopRequest before being destroyed, r=michal
    Differential Revision: https://phabricator.services.mozilla.com/D68025
    
    UltraBlame original commit: 30fe730b5ab1a63d516ac7edcebf26e19c598f1a

--- netwerk/protocol/http/HttpBaseChannel.h
+++ netwerk/protocol/http/HttpBaseChannel.h
@@ -329,6 +329,7 @@ class HttpBaseChannel : public nsHashPropertyBag,
   }
   NS_IMETHOD HTTPUpgrade(const nsACString& aProtocolName,
                          nsIHttpUpgradeListener* aListener) override;
+  void DoDiagnosticAssertWhenOnStopNotCalledOnDestroy() override;
 
   
   NS_IMETHOD GetPriority(int32_t* value) override;
