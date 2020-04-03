92a6a1339f772d25647479303c4d7ffa16e8461c
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-04-03 15:27:48 +0000

    Bug 1595038 - Add a planeNormal variable so that it's clear that the same plane is used for the entire inner loop. r=kip
    Differential Revision: https://phabricator.services.mozilla.com/D68702
    
    UltraBlame original commit: e51404852968e6f3858bf480c10950ff1efdb2c6

--- gfx/2d/Matrix.h
+++ gfx/2d/Matrix.h
@@ -861,7 +861,8 @@ class Matrix4x4Typed {
     
     
     for (int plane = 0; plane < 4; plane++) {
-      planeNormals[plane].Normalize();
+      auto planeNormal = planeNormals[plane];
+      planeNormal.Normalize();
       Point4DTyped<UnknownUnits, F>* srcPoint = dstPointStart;
       Point4DTyped<UnknownUnits, F>* srcPointEnd = dstPoint;
 
@@ -877,10 +878,10 @@ class Matrix4x4Typed {
       
       
       Point4DTyped<UnknownUnits, F>* prevPoint = srcPointEnd - 1;
-      F prevDot = planeNormals[plane].DotProduct(*prevPoint);
+      F prevDot = planeNormal.DotProduct(*prevPoint);
       while (srcPoint < srcPointEnd &&
              ((dstPoint - dstPointStart) < kTransformAndClipRectMaxVerts)) {
-        F nextDot = planeNormals[plane].DotProduct(*srcPoint);
+        F nextDot = planeNormal.DotProduct(*srcPoint);
 
         if ((nextDot >= 0.0) != (prevDot >= 0.0)) {
           
