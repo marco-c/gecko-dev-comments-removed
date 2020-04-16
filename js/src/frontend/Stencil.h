c59540dbd4f762f1eee1bfc5510cf68eb999732d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-04-16 00:24:57 +0000

    Bug 1627975 - Add ImmutableFlags alias to ScriptStencil. r=caroline
    Differential Revision: https://phabricator.services.mozilla.com/D70027
    
    UltraBlame original commit: a3b7bc0072c3dec54f44a6fa69142403eee16a30

--- js/src/frontend/Stencil.h
+++ js/src/frontend/Stencil.h
@@ -436,6 +436,9 @@ using ScriptThingsVector = Vector<ScriptThingVariant>;
 
 class ScriptStencil {
  public:
+  using ImmutableFlags = ImmutableScriptFlagsEnum;
+
+ public:
   js::UniquePtr<js::ImmutableScriptData> immutableScriptData = nullptr;
 
   
@@ -461,7 +464,7 @@ class ScriptStencil {
       : immutableScriptData(std::move(immutableScriptData)), gcThings(cx) {}
 
   bool isFunction() const {
-    return immutableFlags.hasFlag(ImmutableScriptFlagsEnum::IsFunction);
+    return immutableFlags.hasFlag(ImmutableFlags::IsFunction);
   }
 
   
