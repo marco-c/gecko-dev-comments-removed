e20fb703763536b81a5fd2973837c21c2db444a6
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-04-16 01:16:10 +0000

    Bug 1620804 - Replace BrowserTestUtils.waitForCondition with TestUtils.waitForCondition in browser/base/content/test/captivePortal/head.js r=prathiksha
    Differential Revision: https://phabricator.services.mozilla.com/D70190
    
    UltraBlame original commit: dd06e93907aac5a029b596af3e5a780d174d6baa

--- browser/base/content/test/captivePortal/head.js
+++ browser/base/content/test/captivePortal/head.js
@@ -42,7 +42,7 @@ async function setupPrefsAndRecentWindowBehavior() {
 
 async function portalDetected() {
   Services.obs.notifyObservers(null, "captive-portal-login");
-  await BrowserTestUtils.waitForCondition(() => {
+  await TestUtils.waitForCondition(() => {
     return cps.state == cps.LOCKED_PORTAL;
   }, "Waiting for Captive Portal Service to update state after portal detected.");
 }
@@ -52,7 +52,7 @@ async function freePortal(aSuccess) {
     null,
     "captive-portal-login-" + (aSuccess ? "success" : "abort")
   );
-  await BrowserTestUtils.waitForCondition(() => {
+  await TestUtils.waitForCondition(() => {
     return cps.state != cps.LOCKED_PORTAL;
   }, "Waiting for Captive Portal Service to update state after portal freed.");
 }
@@ -81,7 +81,7 @@ async function focusWindowAndWaitForPortalUI(aLongRecheck, win) {
 
   
   
-  await BrowserTestUtils.waitForCondition(() => {
+  await TestUtils.waitForCondition(() => {
     return win.CaptivePortalWatcher._waitingForRecheck;
   }, "Waiting for CaptivePortalWatcher to trigger a recheck.");
   Services.obs.notifyObservers(null, "captive-portal-check-complete");
