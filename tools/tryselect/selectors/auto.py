43c052c49613c217f98fceb1b331e35bbecddbac
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-04-16 00:52:01 +0000

    Bug 1628810 - [tryselect] Use bugbug.debug optimization strategy by default in ./mach try auto, DONTBUILD, r=marco
    Differential Revision: https://phabricator.services.mozilla.com/D70419
    
    UltraBlame original commit: cca8829fd0924cb8f32e05282e7ee056440ecaae

--- tools/tryselect/selectors/auto.py
+++ tools/tryselect/selectors/auto.py
@@ -29,7 +29,7 @@ def run(message='{msg}', push=True, closed_tree=False, try_config=None):
     
     
     try_config.setdefault('optimize-strategies',
-                          'taskgraph.optimize:experimental.bugbug.all')
+                          'taskgraph.optimize:experimental.bugbug.debug')
 
     task_config = {
         'version': 2,
