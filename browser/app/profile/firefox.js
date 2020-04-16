eae681888dba3b2885ba1b7f5172e3c4422b6e8a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-04-16 00:58:41 +0000

    Bug 1615588 - Extended nsIPromptService to support tab modal prompts. r=johannh,MattN
    This patch introduces a new tab modal system prompt type. It can be opened via the nsIPromptService
    with a destination BrowsingContext. These tab system prompts overlap slightly with the upper
    chrome UI to differentiate them from content prompts (previously called tab prompts).
    
    - Extended nsIPromptService and nsIPrompt to accept 3 types of modal prompts:
      - Window prompts
      - Tab (system) prompts
      - Content prompts (the old tab prompts)
    - Removed prompt code from Prompter.jsm, always call PromptParent window actor instead
    - Added PromptChild window actor to forward pagehide events to parent actor
    - Created additional prompt methods in nsIPromptService to prompt by browsingContext and modalType
    - Backwards compatibility is maintained, consumers can still open content prompts calling nsIPrompt with a content window
    
    Differential Revision: https://phabricator.services.mozilla.com/D66446
    
    UltraBlame original commit: 264e642042b1fe17d01626f29341c40c60f9e3af

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1257,6 +1257,10 @@ pref("browser.menu.showCharacterEncoding", "chrome://browser/locale/browser.prop
 pref("prompts.tab_modal.enabled", true);
 
 
+
+pref("prompts.defaultModalType", 3);
+
+
 pref("browser.newtab.preload", true);
 
 
--- dom/base/nsGlobalWindowOuter.cpp
+++ dom/base/nsGlobalWindowOuter.cpp
@@ -4925,7 +4925,8 @@ bool nsGlobalWindowOuter::AlertOrConfirm(bool aAlert, const nsAString& aMessage,
 
   
   if (nsCOMPtr<nsIWritablePropertyBag2> promptBag = do_QueryInterface(prompt)) {
-    promptBag->SetPropertyAsBool(NS_LITERAL_STRING("allowTabModal"), true);
+    promptBag->SetPropertyAsUint32(NS_LITERAL_STRING("modalType"),
+                                   nsIPrompt::MODAL_TYPE_CONTENT);
   }
 
   bool result = false;
@@ -5014,7 +5015,8 @@ void nsGlobalWindowOuter::PromptOuter(const nsAString& aMessage,
 
   
   if (nsCOMPtr<nsIWritablePropertyBag2> promptBag = do_QueryInterface(prompt)) {
-    promptBag->SetPropertyAsBool(NS_LITERAL_STRING("allowTabModal"), true);
+    promptBag->SetPropertyAsUint32(NS_LITERAL_STRING("modalType"),
+                                   nsIPrompt::MODAL_TYPE_CONTENT);
   }
 
   
