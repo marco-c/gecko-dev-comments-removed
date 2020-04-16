5f289574b4af9a7c99b011c6a01e5cc0965546d0
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-04-16 00:13:47 +0000

    Backed out 8 changesets (bug 1610731) for reftest failures at dynamic-toolbar-fixed-bottom-1.html. CLOSED TREE
    Backed out changeset a26b63d0248e (bug 1610731)
    Backed out changeset c33e6fe22774 (bug 1610731)
    Backed out changeset 840550c070e8 (bug 1610731)
    Backed out changeset 7bffd6eb9ac7 (bug 1610731)
    Backed out changeset a383a7100973 (bug 1610731)
    Backed out changeset 8f2bae2ca274 (bug 1610731)
    Backed out changeset 8d43fbe05f38 (bug 1610731)
    Backed out changeset 035432f43d16 (bug 1610731)
    
    UltraBlame original commit: 8b41511236b7c7489718fd4826427f04ff0c24a3

--- gfx/layers/wr/WebRenderScrollDataWrapper.h
+++ gfx/layers/wr/WebRenderScrollDataWrapper.h
@@ -304,69 +304,46 @@ class MOZ_STACK_CLASS WebRenderScrollDataWrapper final {
 
   Maybe<uint64_t> GetFixedPositionAnimationId() const {
     MOZ_ASSERT(IsValid());
-
-    if (AtBottomLayer()) {
-      return mLayer->GetFixedPositionAnimationId();
-    }
-    return Nothing();
+    return mLayer->GetFixedPositionAnimationId();
   }
 
   ScrollableLayerGuid::ViewID GetFixedPositionScrollContainerId() const {
     MOZ_ASSERT(IsValid());
-
-    if (AtBottomLayer()) {
-      return mLayer->GetFixedPositionScrollContainerId();
-    }
-    return ScrollableLayerGuid::NULL_SCROLL_ID;
+    return mLayer->GetFixedPositionScrollContainerId();
   }
 
   SideBits GetFixedPositionSides() const {
     MOZ_ASSERT(IsValid());
+    return mLayer->GetFixedPositionSides();
+  }
 
-    if (AtBottomLayer()) {
-      return mLayer->GetFixedPositionSides();
-    }
-    return SideBits::eNone;
+  bool GetIsStickyPosition() const {
+    MOZ_ASSERT(IsValid());
+
+    
+    return false;
   }
 
   ScrollableLayerGuid::ViewID GetStickyScrollContainerId() const {
     MOZ_ASSERT(IsValid());
 
-    if (AtBottomLayer()) {
-      return mLayer->GetStickyPositionScrollContainerId();
-    }
+    
     return ScrollableLayerGuid::NULL_SCROLL_ID;
   }
 
   const LayerRectAbsolute& GetStickyScrollRangeOuter() const {
     MOZ_ASSERT(IsValid());
+    static const LayerRectAbsolute dummy;
 
-    if (AtBottomLayer()) {
-      return mLayer->GetStickyScrollRangeOuter();
-    }
-
-    static const LayerRectAbsolute empty;
-    return empty;
+    
+    return dummy;
   }
-
   const LayerRectAbsolute& GetStickyScrollRangeInner() const {
     MOZ_ASSERT(IsValid());
+    static const LayerRectAbsolute dummy;
 
-    if (AtBottomLayer()) {
-      return mLayer->GetStickyScrollRangeInner();
-    }
-
-    static const LayerRectAbsolute empty;
-    return empty;
-  }
-
-  Maybe<uint64_t> GetStickyPositionAnimationId() const {
-    MOZ_ASSERT(IsValid());
-
-    if (AtBottomLayer()) {
-      return mLayer->GetStickyPositionAnimationId();
-    }
-    return Nothing();
+    
+    return dummy;
   }
 
   Maybe<uint64_t> GetZoomAnimationId() const {
