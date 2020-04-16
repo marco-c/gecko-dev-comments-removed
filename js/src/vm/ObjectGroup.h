b6cd8d30bdd45888f7f3aa2f9dcdd1e99c814ca1
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-04-16 00:15:53 +0000

    Bug 1625212 - Add CellHeaderWithNonGCPointer class and use it for ObjectGroup r=sfink
    Differential Revision: https://phabricator.services.mozilla.com/D68830
    
    UltraBlame original commit: 90a41b1fa6e086f3d0fe6011e5d8f77bd89f8448

--- js/src/vm/ObjectGroup.h
+++ js/src/vm/ObjectGroup.h
@@ -90,7 +90,8 @@ class ObjectGroup : public gc::TenuredCell {
 
  private:
   
-  const JSClass* const clasp_;  
+  using HeaderWithJSClass = gc::CellHeaderWithNonGCPointer<const JSClass>;
+  HeaderWithJSClass headerAndClasp_;
 
   
   GCPtr<TaggedProto> proto_;  
@@ -152,7 +153,8 @@ class ObjectGroup : public gc::TenuredCell {
 
  private:
   static inline uint32_t offsetOfClasp() {
-    return offsetof(ObjectGroup, clasp_);
+    return offsetof(ObjectGroup, headerAndClasp_) +
+           HeaderWithJSClass::offsetOfPtr();
   }
 
   static inline uint32_t offsetOfProto() {
@@ -178,7 +180,7 @@ class ObjectGroup : public gc::TenuredCell {
   friend class js::jit::MacroAssembler;
 
  public:
-  const JSClass* clasp() const { return clasp_; }
+  const JSClass* clasp() const { return headerAndClasp_.ptr(); }
 
   bool hasDynamicPrototype() const { return proto_.isDynamic(); }
 
@@ -435,6 +437,7 @@ class ObjectGroup : public gc::TenuredCell {
   void finalize(JSFreeOp* fop);
 
   static const JS::TraceKind TraceKind = JS::TraceKind::ObjectGroup;
+  const gc::CellHeader& cellHeader() const { return headerAndClasp_; }
 
  public:
   const ObjectGroupFlags* addressOfFlags() const { return &flags_; }
