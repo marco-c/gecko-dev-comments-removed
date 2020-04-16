03488f4e837c50d5154b389cbee90c04309f4648
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-04-16 01:28:50 +0000

    Bug 949220 - Split NewBuiltinClassInstance into a generic form, a tenured form, and a kind-specified form.  r=jandem
    Differential Revision: https://phabricator.services.mozilla.com/D70501
    
    UltraBlame original commit: 01ced0fd2198ac22fe1028cf5b30eb9d4859a340

--- js/src/vm/Iteration.cpp
+++ js/src/vm/Iteration.cpp
@@ -1010,7 +1010,7 @@ PlainObject* Realm::createIterResultTemplateObject(
   
   Rooted<PlainObject*> templateObject(
       cx, withProto == WithObjectPrototype::Yes
-              ? NewBuiltinClassInstance<PlainObject>(cx, TenuredObject)
+              ? NewTenuredBuiltinClassInstance<PlainObject>(cx)
               : NewObjectWithGivenProto<PlainObject>(cx, nullptr));
   if (!templateObject) {
     return nullptr;
