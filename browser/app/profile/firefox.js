2e43cecb5be10825606d91fd605af7fc2e122ebf
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-04-16 00:56:48 +0000

    Backed out 2 changesets (bug 1628346) for mocha | webconsole node failures on a CLOSED TREE
    Backed out changeset acb119efbe24 (bug 1628346)
    Backed out changeset bbef701c7a1a (bug 1628346)
    
    UltraBlame original commit: 043afb53d46cfd2d5230319ddd88ae9fa2a1fc80

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -2138,16 +2138,7 @@ pref("devtools.webconsole.filter.netxhr", false);
 
 
 pref("devtools.webconsole.input.autocomplete",true);
-
-
-#if defined(NIGHTLY_BUILD)
-  pref("devtools.webconsole.input.context", true);
-#else
-  pref("devtools.webconsole.input.context", false);
-#endif
-
-
-pref("devtools.contenttoolbox.webconsole.input.context", false);
+pref("devtools.webconsole.input.context", false);
 
 
 
