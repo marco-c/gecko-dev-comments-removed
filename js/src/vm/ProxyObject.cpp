a987264475caa7c769e8e7b656010bb12ad305a5
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-04-16 01:28:53 +0000

    Bug 949220 - Make |NewProxyObject| return only non-singletons, and add |NewSingletonProxyObject| for the singleton case.  r=jandem
    Differential Revision: https://phabricator.services.mozilla.com/D70502
    
    UltraBlame original commit: 7ad1607be0b8d97083e3bb690d02adf879441b3d

--- js/src/vm/ProxyObject.cpp
+++ js/src/vm/ProxyObject.cpp
@@ -45,13 +45,28 @@ static gc::AllocKind GetProxyGCObjectKind(const JSClass* clasp,
   return kind;
 }
 
+void ProxyObject::init(const BaseProxyHandler* handler, HandleValue priv,
+                       JSContext* cx) {
+  setInlineValueArray();
+
+  detail::ProxyValueArray* values = detail::GetProxyDataLayout(this)->values();
+  values->init(numReservedSlots());
+
+  data.handler = handler;
+
+  if (IsCrossCompartmentWrapper(this)) {
+    MOZ_ASSERT(cx->global() == &cx->compartment()->globalForNewCCW());
+    setCrossCompartmentPrivate(priv);
+  } else {
+    setSameCompartmentPrivate(priv);
+  }
+}
+
 
 ProxyObject* ProxyObject::New(JSContext* cx, const BaseProxyHandler* handler,
                               HandleValue priv, TaggedProto proto_,
-                              const ProxyOptions& options) {
+                              const JSClass* clasp) {
   Rooted<TaggedProto> proto(cx, proto_);
-
-  const JSClass* clasp = options.clasp();
 
 #ifdef DEBUG
   MOZ_ASSERT(isValidProxyClass(clasp));
@@ -71,7 +86,7 @@ ProxyObject* ProxyObject::New(JSContext* cx, const BaseProxyHandler* handler,
 
 
 
-  if (proto.isObject() && !options.singleton() && !clasp->isDOMClass()) {
+  if (proto.isObject() && !clasp->isDOMClass()) {
     ObjectGroupRealm& realm = ObjectGroupRealm::getForNewObject(cx);
     RootedObject protoObj(cx, proto.toObject());
     if (!JSObject::setNewGroupUnknown(cx, realm, clasp, protoObj)) {
@@ -82,12 +97,8 @@ ProxyObject* ProxyObject::New(JSContext* cx, const BaseProxyHandler* handler,
   
   
   NewObjectKind newKind = NurseryAllocatedProxy;
-  if (options.singleton()) {
-    MOZ_ASSERT(priv.isNull() ||
-               (priv.isGCThing() && priv.toGCThing()->isTenured()));
-    newKind = SingletonObject;
-  } else if ((priv.isGCThing() && priv.toGCThing()->isTenured()) ||
-             !handler->canNurseryAllocate()) {
+  if ((priv.isGCThing() && priv.toGCThing()->isTenured()) ||
+      !handler->canNurseryAllocate()) {
     newKind = TenuredObject;
   }
 
@@ -100,26 +111,7 @@ ProxyObject* ProxyObject::New(JSContext* cx, const BaseProxyHandler* handler,
   JS_TRY_VAR_OR_RETURN_NULL(cx, proxy,
                             create(cx, clasp, proto, allocKind, newKind));
 
-  proxy->setInlineValueArray();
-
-  detail::ProxyValueArray* values = detail::GetProxyDataLayout(proxy)->values();
-  values->init(proxy->numReservedSlots());
-
-  proxy->data.handler = handler;
-  if (IsCrossCompartmentWrapper(proxy)) {
-    MOZ_ASSERT(cx->global() == &cx->compartment()->globalForNewCCW());
-    proxy->setCrossCompartmentPrivate(priv);
-  } else {
-    proxy->setSameCompartmentPrivate(priv);
-  }
-
-  if (newKind == SingletonObject) {
-    Rooted<ProxyObject*> rootedProxy(cx, proxy);
-    if (!JSObject::setSingleton(cx, rootedProxy)) {
-      return nullptr;
-    }
-    return rootedProxy;
-  }
+  proxy->init(handler, priv, cx);
 
   
   if (!clasp->isDOMClass()) {
@@ -127,6 +119,43 @@ ProxyObject* ProxyObject::New(JSContext* cx, const BaseProxyHandler* handler,
   }
 
   return proxy;
+}
+
+
+ProxyObject* ProxyObject::NewSingleton(JSContext* cx,
+                                       const BaseProxyHandler* handler,
+                                       HandleValue priv, TaggedProto proto_,
+                                       const JSClass* clasp) {
+  Rooted<TaggedProto> proto(cx, proto_);
+
+#ifdef DEBUG
+  MOZ_ASSERT(isValidProxyClass(clasp));
+  MOZ_ASSERT(clasp->shouldDelayMetadataBuilder());
+  MOZ_ASSERT_IF(proto.isObject(),
+                cx->compartment() == proto.toObject()->compartment());
+  MOZ_ASSERT(clasp->hasFinalize());
+  if (priv.isGCThing()) {
+    JS::AssertCellIsNotGray(priv.toGCThing());
+  }
+#endif
+
+  gc::AllocKind allocKind = GetProxyGCObjectKind(clasp, handler, priv);
+
+  AutoSetNewObjectMetadata metadata(cx);
+  
+  
+  ProxyObject* proxy;
+  JS_TRY_VAR_OR_RETURN_NULL(
+      cx, proxy, create(cx, clasp, proto, allocKind, SingletonObject));
+
+  proxy->init(handler, priv, cx);
+
+  Rooted<ProxyObject*> rootedProxy(cx, proxy);
+  if (!JSObject::setSingleton(cx, rootedProxy)) {
+    return nullptr;
+  }
+
+  return rootedProxy;
 }
 
 gc::AllocKind ProxyObject::allocKindForTenure() const {
