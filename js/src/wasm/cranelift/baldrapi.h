b10302b37ba870fb116c374b4c365f0889109784
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-04-16 00:57:02 +0000

    Bug 1626967: Use the regular Cranelift sinks to keep track of relocations; r=rhunt
    This goes hand in hand with
    https://github.com/bytecodealliance/wasmtime/pull/1460, which will need to be
    merged first.
    
    This removes most of the Spidermonkey-specific machinery to collect
    relocations. The only remaining place is for stack-maps, which will be handled
    later.
    
    Differential Revision: https://phabricator.services.mozilla.com/D69394
    
    UltraBlame original commit: fb80ccae7376ec82a7a058de97c33f5c7b47f6cd

--- js/src/wasm/cranelift/baldrapi.h
+++ js/src/wasm/cranelift/baldrapi.h
@@ -122,13 +122,7 @@ struct CraneliftFuncCompileInput {
 
 
 struct CraneliftMetadataEntry {
-  enum Which {
-    DirectCall,
-    IndirectCall,
-    Trap,
-    MemoryAccess,
-    SymbolicAccess
-  } which;
+  enum Which { DirectCall, IndirectCall, Trap, SymbolicAccess } which;
   uint32_t codeOffset;
   uint32_t moduleBytecodeOffset;
   size_t extra;
