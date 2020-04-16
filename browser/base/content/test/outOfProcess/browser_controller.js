ded3f5ba8168b473b3f9b83dee4a440add30f58f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-04-16 01:27:33 +0000

    Bug 1622057, try putting the focus in a known place at the beginning of the test to see if this fixes the intermittent, r=Gijs
    The failure might be caused by a race setting the active/focused window between processes
    
    Differential Revision: https://phabricator.services.mozilla.com/D70632
    
    UltraBlame original commit: 6396ce31334d74911b85e561aa89f1b6a4c59ed6

--- browser/base/content/test/outOfProcess/browser_controller.js
+++ browser/base/content/test/outOfProcess/browser_controller.js
@@ -40,10 +40,10 @@ add_task(async function test_controllers_subframes() {
     "<input id='input'><br><br>"
   );
 
+  gURLBar.focus();
+
   for (let stepNum = 0; stepNum < browsingContexts.length; stepNum++) {
-    if (stepNum > 0) {
-      await keyAndUpdate("VK_TAB", {}, 5);
-    }
+    await keyAndUpdate(stepNum > 0 ? "VK_TAB" : "VK_F6", {}, 6);
 
     
     
@@ -59,20 +59,16 @@ add_task(async function test_controllers_subframes() {
       goUpdateGlobalEditMenuItems(true);
     }
 
-    await SpecialPowers.spawn(
-      browsingContexts[stepNum],
-      [stepNum == 0],
-      bodyFocused => {
-        
-        
-        let document = content.document;
-        Assert.equal(
-          document.activeElement,
-          bodyFocused ? document.body : document.documentElement,
-          "root focused"
-        );
-      }
-    );
+    await SpecialPowers.spawn(browsingContexts[stepNum], [], () => {
+      
+      
+      let document = content.document;
+      Assert.equal(
+        document.activeElement,
+        document.documentElement,
+        "root focused"
+      );
+    });
     checkCommandState("step " + stepNum + " root focused", false, false);
 
     
