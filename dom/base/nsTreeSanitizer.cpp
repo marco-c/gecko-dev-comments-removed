d73fd1dabd79514e000e019b90b643c4d42bdf7f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-04-20 14:50:19 +0000

    Backed out changeset 136b9329a6f7 (bug 1581691) for failures on svg/styling/presentation-attributes-relevant.html.
    UltraBlame original commit: 3cec983cf6a6e506dad470030243b85f65171504

--- dom/base/nsTreeSanitizer.cpp
+++ dom/base/nsTreeSanitizer.cpp
@@ -563,12 +563,11 @@ const nsStaticAtom* const kAttributesSVG[] = {
     nsGkAtoms::text_anchor,        
     nsGkAtoms::text_decoration,    
     
-    nsGkAtoms::text_rendering,    
-    nsGkAtoms::title,             
-    nsGkAtoms::to,                
-    nsGkAtoms::transform,         
-    nsGkAtoms::transform_origin,  
-    nsGkAtoms::type,              
+    nsGkAtoms::text_rendering,  
+    nsGkAtoms::title,           
+    nsGkAtoms::to,              
+    nsGkAtoms::transform,       
+    nsGkAtoms::type,            
     
     
     
