42d94a962c8920c443729f803b2564a37fb7e084
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-04-20 14:45:35 +0000

    Bug 1629427 - static_assert remove extra symbol. r=sylvestre
    Differential Revision: https://phabricator.services.mozilla.com/D71146
    
    UltraBlame original commit: fe7c26093c483adda45b8eefef90876e003fea00

--- dom/media/doctor/gtest/TestRollingNumber.cpp
+++ dom/media/doctor/gtest/TestRollingNumber.cpp
@@ -17,7 +17,7 @@ using RN8 = mozilla::RollingNumber<uint8_t>;
 TEST(RollingNumber, Value)
 {
   
-  static_assert(std::is_same_v<RN8::ValueType, uint8_t>, "");
+  static_assert(std::is_same_v<RN8::ValueType, uint8_t>);
 
   
   const RN8 n;
