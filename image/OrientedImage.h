16384407f91ab068f6f60b24f3f8c8c9a95ea1ce
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-04-20 14:48:12 +0000

    Bug 1616411 - Part 1: Split out some helper methods from OrientedImage. r=tnikkel
    RasterImage will make use of them.
    
    Note that there is one bug fix in this patch, which is that
    OrientedImage::OrientSurface now creates a surface of the correct size.
    
    (Previously this code was creating a surface with the underlying
    image's size, rather than the correctly oriented size.  But we must
    not have been calling into that code with our current uses of
    OrientedImage.)
    
    Differential Revision: https://phabricator.services.mozilla.com/D70271
    
    UltraBlame original commit: a31395761d3b5565144082dbb6fdd424a9fa2657

--- image/OrientedImage.h
+++ image/OrientedImage.h
@@ -63,13 +63,42 @@ class OrientedImage : public ImageWrapper {
                                     gfx::SamplingFilter aSamplingFilter,
                                     uint32_t aFlags) override;
 
+  
+
+
+
+
+
+
+
+
+
+
+
+
+  static gfxMatrix OrientationMatrix(Orientation aOrientation,
+                                     const nsIntSize& aSize,
+                                     bool aInvert = false);
+
+  
+
+
+
+
+
+  static already_AddRefed<SourceSurface> OrientSurface(Orientation aOrientation,
+                                                       SourceSurface* aSurface,
+                                                       const nsIntSize& aSize);
+
  protected:
   OrientedImage(Image* aImage, Orientation aOrientation)
       : ImageWrapper(aImage), mOrientation(aOrientation) {}
 
   virtual ~OrientedImage() {}
 
-  gfxMatrix OrientationMatrix(const nsIntSize& aSize, bool aInvert = false);
+  gfxMatrix OrientationMatrix(const nsIntSize& aSize, bool aInvert = false) {
+    return OrientationMatrix(mOrientation, aSize, aInvert);
+  }
 
  private:
   Orientation mOrientation;
