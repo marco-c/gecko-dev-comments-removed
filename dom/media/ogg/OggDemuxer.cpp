a252421783d0850fb2a68bd706ecaa2f593046df
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-04-21 18:21:47 +0000

    Bug 1611622 - Allow nil packets in OGG demuxer. r=bryce
    Differential Revision: https://phabricator.services.mozilla.com/D71349
    
    UltraBlame original commit: 1177ae3a0ef3c28a8b5c859f3a4509c439d6dad8

--- dom/media/ogg/OggDemuxer.cpp
+++ dom/media/ogg/OggDemuxer.cpp
@@ -1238,7 +1238,8 @@ RefPtr<MediaRawData> OggTrackDemuxer::NextSample() {
   bool eos = packet->e_o_s;
   OggCodecState* state = mParent->GetTrackCodecState(mType);
   RefPtr<MediaRawData> data = state->PacketOutAsMediaRawData();
-  if (!data) {
+  
+  if (!data || (data->mEOS && data->Size() == 0)) {
     return nullptr;
   }
   if (mType == TrackInfo::kAudioTrack) {
