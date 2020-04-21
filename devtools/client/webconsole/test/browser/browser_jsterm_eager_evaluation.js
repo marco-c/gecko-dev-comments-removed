b8dc2decf710d231abf4668c7ae764d642609907
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-04-21 18:20:44 +0000

    Bug 1631277 - Allow 'instanceof' in eager evaluation. r=nchevobbe
    Differential Revision: https://phabricator.services.mozilla.com/D71477
    
    UltraBlame original commit: e26977345e27cdd06ae8e80f34e2b55f64e86d34

--- devtools/client/webconsole/test/browser/browser_jsterm_eager_evaluation.js
+++ devtools/client/webconsole/test/browser/browser_jsterm_eager_evaluation.js
@@ -221,6 +221,9 @@ add_task(async function() {
   setInputValue(hud, "Reflect.setPrototypeOf({}, null)");
   await waitForNoEagerEvaluationResult(hud);
 
+  setInputValue(hud, "[] instanceof Array");
+  await waitForEagerEvaluationResult(hud, "true");
+
   
   await toggleLayout(hud);
 });
