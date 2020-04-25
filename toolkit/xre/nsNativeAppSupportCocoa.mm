1ca78fdc8c67573beb0d3f5f0412819b310fc9f7
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-04-25 19:02:28 +0000

    Bug 1597508: Use WidgetUtils::DOMWindowToWidget to determine native window during reopen of Firefox on macOS. r=farre
    Differential Revision: https://phabricator.services.mozilla.com/D71408
    
    UltraBlame original commit: fc341103b84a7791f160f38c0ce3dfd076a5e032

--- toolkit/xre/nsNativeAppSupportCocoa.mm
+++ toolkit/xre/nsNativeAppSupportCocoa.mm
@@ -1,7 +1,7 @@
-/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+
+
+
 
 #include "nsString.h"
 
@@ -15,37 +15,29 @@
 #include "nsIBaseWindow.h"
 #include "nsCommandLine.h"
 #include "mozIDOMWindow.h"
-#include "nsIDocShellTreeItem.h"
-#include "nsIDocShellTreeOwner.h"
-#include "nsIInterfaceRequestorUtils.h"
 #include "nsIWebNavigation.h"
 #include "nsIWidget.h"
 #include "nsIWindowMediator.h"
+#include "nsPIDOMWindow.h"
+#include "WidgetUtils.h"
 
-// This must be included last:
+
 #include "nsObjCExceptions.h"
+
+using mozilla::widget::WidgetUtils;
 
 nsresult GetNativeWindowPointerFromDOMWindow(mozIDOMWindowProxy* a_window,
                                              NSWindow** a_nativeWindow) {
   *a_nativeWindow = nil;
   if (!a_window) return NS_ERROR_INVALID_ARG;
 
-  nsCOMPtr<nsIWebNavigation> mruWebNav(do_GetInterface(a_window));
-  if (mruWebNav) {
-    nsCOMPtr<nsIDocShellTreeItem> mruTreeItem(do_QueryInterface(mruWebNav));
-    nsCOMPtr<nsIDocShellTreeOwner> mruTreeOwner = nullptr;
-    mruTreeItem->GetTreeOwner(getter_AddRefs(mruTreeOwner));
-    if (mruTreeOwner) {
-      nsCOMPtr<nsIBaseWindow> mruBaseWindow(do_QueryInterface(mruTreeOwner));
-      if (mruBaseWindow) {
-        nsCOMPtr<nsIWidget> mruWidget = nullptr;
-        mruBaseWindow->GetMainWidget(getter_AddRefs(mruWidget));
-        if (mruWidget) {
-          *a_nativeWindow = (NSWindow*)mruWidget->GetNativeData(NS_NATIVE_WINDOW);
-        }
-      }
-    }
+  nsPIDOMWindowOuter* win = nsPIDOMWindowOuter::From(a_window);
+  nsCOMPtr<nsIWidget> widget = WidgetUtils::DOMWindowToWidget(win);
+  if (!widget) {
+    return NS_ERROR_FAILURE;
   }
+
+  *a_nativeWindow = (NSWindow*)widget->GetNativeData(NS_NATIVE_WINDOW);
 
   return NS_OK;
 }
@@ -74,11 +66,11 @@ NS_IMETHODIMP nsNativeAppSupportCocoa::Start(bool* _retval) {
 
   NS_OBJC_BEGIN_TRY_ABORT_BLOCK_NSRESULT;
 
-  // Check that the OS version is supported, if not return false,
-  // which will make the browser quit.  In principle we could display an
-  // alert here.  But the alert's message and buttons would require custom
-  // localization.  So (for now at least) we just log an English message
-  // to the console before quitting.
+  
+  
+  
+  
+  
   if (major < 10 || minor < 6) {
     NSLog(@"Minimum OS version requirement not met!");
     return NS_OK;
@@ -129,13 +121,13 @@ nsNativeAppSupportCocoa::ReOpen() {
       NSWindow* cocoaWindow = (NSWindow*)widget->GetNativeData(NS_NATIVE_WINDOW);
       if (![cocoaWindow isMiniaturized]) {
         haveNonMiniaturized = true;
-        break;  // have un-minimized windows, nothing to do
+        break;  
       }
       windowList->HasMoreElements(&more);
-    }  // end while
+    }  
 
     if (!haveNonMiniaturized) {
-      // Deminiaturize the most recenty used window
+      
       nsCOMPtr<mozIDOMWindowProxy> mru;
       wm->GetMostRecentWindow(nullptr, getter_AddRefs(mru));
 
@@ -147,12 +139,12 @@ nsNativeAppSupportCocoa::ReOpen() {
           done = true;
         }
       }
-    }  // end if have non miniaturized
+    }  
 
     if (!haveOpenWindows && !done) {
       char* argv[] = {nullptr};
 
-      // use an empty command line to make the right kind(s) of window open
+      
       nsCOMPtr<nsICommandLineRunner> cmdLine(new nsCommandLine());
 
       nsresult rv;
@@ -162,7 +154,7 @@ nsNativeAppSupportCocoa::ReOpen() {
       return cmdLine->Run();
     }
 
-  }  // got window mediator
+  }  
   return NS_OK;
 
   NS_OBJC_END_TRY_ABORT_BLOCK_NSRESULT;
@@ -170,7 +162,7 @@ nsNativeAppSupportCocoa::ReOpen() {
 
 #pragma mark -
 
-// Create and return an instance of class nsNativeAppSupportCocoa.
+
 nsresult NS_CreateNativeAppSupport(nsINativeAppSupport** aResult) {
   *aResult = new nsNativeAppSupportCocoa;
   if (!*aResult) return NS_ERROR_OUT_OF_MEMORY;
