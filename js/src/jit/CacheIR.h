a92714a7e6d14b3d73ace77a1aa2764f6f8337f5
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-04-25 19:07:23 +0000

    Bug 1632067 part 6 - Simplify arguments length code. r=iain
    Define the lengths directly in the Python script. Make CacheIRWriter assert the
    written arguments match this information to ensure everything stays in sync.
    
    Differential Revision: https://phabricator.services.mozilla.com/D71904
    
    UltraBlame original commit: 1a37730d1bd1fe8df7e613edf04d18db5ce75f6c

--- js/src/jit/CacheIR.h
+++ js/src/jit/CacheIR.h
@@ -181,31 +181,14 @@ enum class CacheKind : uint8_t {
 
 extern const char* const CacheKindNames[];
 
-
-
-
-
-namespace CacheIROpFormat {
-enum ArgType {
-  None,
-  Id,
-  Field,
-  Byte,
-  Int32,
-  UInt32,
-  Word,
-};
-
-extern const uint32_t ArgLengths[];
-}  
-
 enum class CacheOp {
 #define DEFINE_OP(op, ...) op,
   CACHE_IR_OPS(DEFINE_OP)
 #undef DEFINE_OP
 };
 
-extern const char* const CacheIrOpNames[];
+extern const char* const CacheIROpNames[];
+extern const uint32_t CacheIROpArgLengths[];
 
 class StubField {
  public:
@@ -481,12 +464,33 @@ class MOZ_RAII CacheIRWriter : public JS::CustomAutoRooter {
   mutable uint32_t lastOffset_;
   mutable uint32_t lastIndex_;
 
+#ifdef DEBUG
+  
+  mozilla::Maybe<CacheOp> currentOp_;
+  size_t currentOpArgsStart_ = 0;
+#endif
+
   void assertSameCompartment(JSObject*);
 
   void writeOp(CacheOp op) {
     MOZ_ASSERT(uint32_t(op) <= UINT8_MAX);
     buffer_.writeByte(uint32_t(op));
     nextInstructionId_++;
+#ifdef DEBUG
+    MOZ_ASSERT(currentOp_.isNothing(), "Missing call to assertLengthMatches?");
+    currentOp_.emplace(op);
+    currentOpArgsStart_ = buffer_.length();
+#endif
+  }
+
+  void assertLengthMatches() {
+#ifdef DEBUG
+    
+    size_t expectedLen = CacheIROpArgLengths[size_t(*currentOp_)];
+    MOZ_ASSERT_IF(!failed(),
+                  buffer_.length() - currentOpArgsStart_ == expectedLen);
+    currentOp_.reset();
+#endif
   }
 
   void writeOperandId(OperandId opId) {
