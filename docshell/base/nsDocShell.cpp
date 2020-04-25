c07fdb3f669939b3317394df53dc15613b9f8e6e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-04-25 19:10:06 +0000

    Bug 1602318 - Make sure rv is initialized correctly in all branches of OpenInitializedChannel. r=nika
    Differential Revision: https://phabricator.services.mozilla.com/D70621
    
    UltraBlame original commit: fc4efd11e643b65d1a9cb796fba33bd76bbccbec

--- docshell/base/nsDocShell.cpp
+++ docshell/base/nsDocShell.cpp
@@ -10070,6 +10070,7 @@ nsresult nsDocShell::OpenInitializedChannel(nsIChannel* aChannel,
     
     CreateReservedSourceIfNeeded(aChannel,
                                  win->EventTargetFor(TaskCategory::Other));
+    rv = NS_OK;
   } else {
     rv = AddClientChannelHelper(aChannel, std::move(noReservedClient),
                                 GetInitialClientInfo(),
