7db88b6fdbd957433c96a2020a19677113fdac56
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-04-25 19:09:25 +0000

    Bug 1632021 - part 4: Get rid of IID of `InsertTextTransaction` and `CompositionTransaction` r=m_kato
    `EditTransactionBase::GetAs*Transaction()` should be used instead.
    
    Differential Revision: https://phabricator.services.mozilla.com/D71911
    
    UltraBlame original commit: 55abc248f5249c13b2511bff48ecad5cfb365d5d

--- editor/libeditor/CompositionTransaction.cpp
+++ editor/libeditor/CompositionTransaction.cpp
@@ -73,9 +73,7 @@ NS_IMPL_CYCLE_COLLECTION_INHERITED(CompositionTransaction, EditTransactionBase,
 
 
 NS_INTERFACE_MAP_BEGIN_CYCLE_COLLECTION(CompositionTransaction)
-  NS_INTERFACE_MAP_ENTRY_CONCRETE(CompositionTransaction)
 NS_INTERFACE_MAP_END_INHERITING(EditTransactionBase)
-
 NS_IMPL_ADDREF_INHERITED(CompositionTransaction, EditTransactionBase)
 NS_IMPL_RELEASE_INHERITED(CompositionTransaction, EditTransactionBase)
 
