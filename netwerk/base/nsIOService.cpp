cd8ab2627a4b985fbf34753c027eda86a09115f8
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-04-25 19:07:46 +0000

    Backed out 4 changesets (bug 1602832) for causing multiple xpcshell failures.
    CLOSED TREE
    
    Backed out changeset a5cc1585581f (bug 1602832)
    Backed out changeset 3de4624b41dc (bug 1602832)
    Backed out changeset da106d35137e (bug 1602832)
    Backed out changeset f9388129262a (bug 1602832)
    
    UltraBlame original commit: fafc5b65980a1c9f2e834041a78e19a5c67a145c

--- netwerk/base/nsIOService.cpp
+++ netwerk/base/nsIOService.cpp
@@ -277,6 +277,7 @@ nsresult nsIOService::Init() {
     observerService->AddObserver(this, NS_NETWORK_LINK_TOPIC, true);
     observerService->AddObserver(this, NS_NETWORK_ID_CHANGED_TOPIC, true);
     observerService->AddObserver(this, NS_WIDGET_WAKE_OBSERVER_TOPIC, true);
+    observerService->AddObserver(this, NS_PREFSERVICE_READ_TOPIC_ID, true);
   } else
     NS_WARNING("failed to get observer service");
 
@@ -448,6 +449,11 @@ nsresult nsIOService::LaunchSocketProcess() {
     return NS_OK;
   }
 
+  if (!XRE_IsE10sParentProcess()) {
+    LOG(("nsIOService skipping LaunchSocketProcess because e10s is disabled"));
+    return NS_OK;
+  }
+
   if (!Preferences::GetBool("network.process.enabled", true)) {
     LOG(("nsIOService skipping LaunchSocketProcess because of the pref"));
     return NS_OK;
@@ -493,16 +499,15 @@ static bool sUseSocketProcess = false;
 static bool sUseSocketProcessChecked = false;
 
 
-bool nsIOService::UseSocketProcess(bool aCheckAgain) {
-  if (sUseSocketProcessChecked && !aCheckAgain) {
+bool nsIOService::UseSocketProcess() {
+  if (sUseSocketProcessChecked) {
     return sUseSocketProcess;
   }
 
   sUseSocketProcessChecked = true;
-  sUseSocketProcess = false;
-  if (StaticPrefs::network_process_enabled()) {
-    sUseSocketProcess =
-        StaticPrefs::network_http_network_access_on_socket_process_enabled();
+  if (Preferences::GetBool("network.process.enabled")) {
+    sUseSocketProcess = Preferences::GetBool(
+        "network.http.network_access_on_socket_process.enabled", true);
   }
   return sUseSocketProcess;
 }
@@ -1545,7 +1550,10 @@ nsIOService::Observe(nsISupports* subject, const char* topic,
       SetOffline(false);
     }
   } else if (!strcmp(topic, kProfileDoChange)) {
-    if (data && NS_LITERAL_STRING("startup").Equals(data)) {
+    if (!data) {
+      return NS_OK;
+    }
+    if (NS_LITERAL_STRING("startup").Equals(data)) {
       
       InitializeNetworkLinkService();
       
@@ -1560,6 +1568,9 @@ nsIOService::Observe(nsISupports* subject, const char* topic,
       
       nsCOMPtr<nsISupports> cookieServ =
           do_GetService(NS_COOKIESERVICE_CONTRACTID);
+    } else if (NS_LITERAL_STRING("xpcshell-do-get-profile").Equals(data)) {
+      
+      LaunchSocketProcess();
     }
   } else if (!strcmp(topic, NS_XPCOM_SHUTDOWN_OBSERVER_ID)) {
     
@@ -1598,6 +1609,10 @@ nsIOService::Observe(nsISupports* subject, const char* topic,
     
     nsCOMPtr<nsIRunnable> wakeupNotifier = new nsWakeupNotifier(this);
     NS_DispatchToMainThread(wakeupNotifier);
+  } else if (!strcmp(topic, NS_PREFSERVICE_READ_TOPIC_ID)) {
+    
+    
+    LaunchSocketProcess();
   }
 
   return NS_OK;
