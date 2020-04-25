8880462fd0dd57acf4766969bbd00121982ae76b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-04-25 19:00:35 +0000

    Bug 1629864 [wpt PR 22927] - Python 3: Add platform_name returned in Linux for Python 3 support, a=testonly
    Automatic update from web-platform-tests
    webdriver: fix platform_name returned on Linux for Python 3 support (#22927)
    
    --
    
    wpt-commits: 234b63443474a95376d5e83df4aca0cc7fd88147
    wpt-pr: 22927
    
    UltraBlame original commit: 7720a32599acd0cf7f90bd8c1827a99c3c8351e8

--- testing/web-platform/tests/webdriver/tests/support/__init__.py
+++ testing/web-platform/tests/webdriver/tests/support/__init__.py
@@ -3,6 +3,10 @@ import sys
 from .merge_dictionaries import merge_dictionaries
 
 platform_name = {
+    
+    
+    
+    "linux": "linux",
     "linux2": "linux",
     "win32": "windows",
     "cygwin": "windows",
