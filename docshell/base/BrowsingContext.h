3ccb581b345dd8b1589332d1c13ec1930d239959
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-04-28 01:02:36 +0000

    Bug 1576188 - Handle save-as for cross process iframes. r=peterv
    Depends on D70388
    
    Differential Revision: https://phabricator.services.mozilla.com/D70389
    
    UltraBlame original commit: 621a1ee180700f14007252542367db2009afe99d

--- docshell/base/BrowsingContext.h
+++ docshell/base/BrowsingContext.h
@@ -218,6 +218,12 @@ class BrowsingContext : public nsILoadContext, public nsWrapperCache {
 
   
   
+  Document* GetDocument() const {
+    return mDocShell ? mDocShell->GetDocument() : nullptr;
+  }
+
+  
+  
   
   
   
@@ -280,6 +286,8 @@ class BrowsingContext : public nsILoadContext, public nsWrapperCache {
   bool IsTop() const { return !GetParent(); }
 
   bool IsTopContent() const { return IsContent() && !GetParent(); }
+
+  bool IsInSubtreeOf(BrowsingContext* aContext);
 
   bool IsContentSubframe() const { return IsContent() && GetParent(); }
   uint64_t Id() const { return mBrowsingContextId; }
