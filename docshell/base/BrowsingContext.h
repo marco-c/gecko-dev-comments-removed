ec5b7d64ae0361809701dfd3ef714f340391992f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-04-28 12:27:21 +0000

    Backed out changeset 14568f3c84b6 (bug 1575356) for failures on test_iframe_referrer.html. CLOSED TREE
    UltraBlame original commit: a99c73301874690830624ae0a98c7940bc754c7d

--- docshell/base/BrowsingContext.h
+++ docshell/base/BrowsingContext.h
@@ -132,10 +132,7 @@ class WindowProxyHolder;
   FIELD(WatchedByDevtools, bool)                                             \
   FIELD(TextZoom, float)                                                     \
   /* See nsIRequest for possible flags. */                                   \
-  FIELD(DefaultLoadFlags, uint32_t)                                          \
-  /* Mixed-Content: If the corresponding documentURI is https,               \
-   * then this flag is true. */                                              \
-  FIELD(IsSecure, bool)
+  FIELD(DefaultLoadFlags, uint32_t)
 
 
 
