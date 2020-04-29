72b6101b9729514fe79801c7c22f6e067fa12a5d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-04-29 23:17:49 +0000

    Bug 1633437 - Support for test metadata r=acreskey
    This patch adds support for tests metadata. A test script parser is added as
    well as a new "doc" flavor that can be used to display the script info in the
    command line. This parser will be the basis for building automated docs and
    scripts verifications if we want to do this.
    
    Differential Revision: https://phabricator.services.mozilla.com/D72800
    
    UltraBlame original commit: c4a72c937045df517cbcb8e02307245bbbbe35b9

--- python/mozperftest/mozperftest/tests/samples/perftest_example.js
+++ python/mozperftest/mozperftest/tests/samples/perftest_example.js
@@ -4,26 +4,26 @@
 
 "use strict";
 
-async function test(context, commands) {
-  
-  
-  await commands.measure.start();
-  await commands.measure.browser.wait(commands.measure.pageCompleteCheck);
-  await commands.measure.stop();
+var someVar;
 
-  const browserScripts = commands.measure.result[0].browserScripts;
-
-  const processLaunchToNavStart =
-    browserScripts.pageinfo.navigationStartTime -
-    browserScripts.browser.processStartTime;
-
-  browserScripts.pageinfo.processLaunchToNavStart = processLaunchToNavStart;
-  console.log("processLaunchToNavStart: " + processLaunchToNavStart);
-
-  return true;
+async function setUp(context) {
+  context.log.info("setUp example!");
 }
 
+async function test(context, commands) {
+  context.log.info("Test with setUp/tearDown example!");
+  await commands.measure.start("https://www.sitespeed.io/");
+  await commands.measure.start("https://www.mozilla.org/en-US/");
+}
+
+async function tearDown(context) {
+  context.log.info("tearDown example!");
+}
+
+
 module.exports = {
+  setUp,
+  tearDown,
   test,
   owner: "Performance Team",
   description: "Measures cold process applink time",
