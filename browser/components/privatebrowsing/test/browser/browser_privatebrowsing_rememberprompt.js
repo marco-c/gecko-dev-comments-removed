52c045c443287f7a9e70441e3b30b75513f2edd2
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-04-29 23:17:58 +0000

    Bug 1628052 - Remove uses of dom.ipc.keepProcessesAlive.webIsolated.perOrigin to fix leaks. r=kmag
    Differential Revision: https://phabricator.services.mozilla.com/D70076
    
    UltraBlame original commit: e8cf96fbbc64df5b2e6c44acffa2498959eebe5f

--- browser/components/privatebrowsing/test/browser/browser_privatebrowsing_rememberprompt.js
+++ browser/components/privatebrowsing/test/browser/browser_privatebrowsing_rememberprompt.js
@@ -5,14 +5,9 @@
 
 
 
-
-
 add_task(async function setup() {
   await SpecialPowers.pushPrefEnv({
-    set: [
-      ["dom.ipc.keepProcessesAlive.webIsolated.perOrigin", 1],
-      ["dom.vr.always_support_vr", true],
-    ],
+    set: [["dom.vr.always_support_vr", true]],
   });
 });
 
