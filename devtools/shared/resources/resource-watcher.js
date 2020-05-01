136e45510af25f5649eab6921b75c59b66e65cfc
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-05-01 00:10:53 +0000

    Bug 1634238: Remove CONSOLE_MESSAGES added by mistake from ResourceWatcher. r=nchevobbe
    Differential Revision: https://phabricator.services.mozilla.com/D73182
    
    UltraBlame original commit: 61524a5c8fbdc94b94c1d319ba97dea9a533319d

--- devtools/shared/resources/resource-watcher.js
+++ devtools/shared/resources/resource-watcher.js
@@ -328,49 +328,6 @@ const LegacyListeners = {
     .ERROR_MESSAGES]: require("devtools/shared/resources/legacy-listeners/error-messages"),
   [ResourceWatcher.TYPES
     .PLATFORM_MESSAGES]: require("devtools/shared/resources/legacy-listeners/platform-messages"),
-  
-  
-  async [ResourceWatcher.TYPES.CONSOLE_MESSAGES]({
-    targetList,
-    targetType,
-    targetFront,
-    isTopLevel,
-    onAvailable,
-  }) {
-    
-    
-    
-    
-    
-    const isContentToolbox = targetList.targetFront.isLocalTab;
-    const listenForFrames =
-      isContentToolbox &&
-      Services.prefs.getBoolPref("devtools.contenttoolbox.fission");
-    const isAllowed =
-      isTopLevel ||
-      targetType === targetList.TYPES.PROCESS ||
-      (targetType === targetList.TYPES.FRAME && listenForFrames);
-
-    if (!isAllowed) {
-      return;
-    }
-
-    const webConsoleFront = await targetFront.getFront("console");
-
-    
-    await webConsoleFront.startListeners(["ConsoleAPI"]);
-
-    
-    
-    const { messages } = await webConsoleFront.getCachedMessages([
-      "ConsoleAPI",
-    ]);
-    
-    messages.map(message => ({ message })).forEach(onAvailable);
-
-    
-    webConsoleFront.on("consoleAPICall", onAvailable);
-  },
   async [ResourceWatcher.TYPES.DOCUMENT_EVENTS]({
     targetList,
     targetType,
