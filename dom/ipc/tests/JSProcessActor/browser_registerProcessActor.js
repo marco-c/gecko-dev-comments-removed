61571985ed72fad95418832d984ddb1e2c781847
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-05-01 00:11:12 +0000

    Bug 1580448 - JSProcessActor tests;r=nika
    Differential Revision: https://phabricator.services.mozilla.com/D69819
    
    UltraBlame original commit: 6db8de5fc125c19986ee1666330cb8417e873f82

---
+++ dom/ipc/tests/JSProcessActor/browser_registerProcessActor.js
@@ -0,0 +1,16 @@
+
+
+"use strict";
+
+declTest("double register", {
+  async test() {
+    SimpleTest.doesThrow(
+      () =>
+        ChromeUtils.registerContentActor(
+          "TestProcessActor",
+          processActorOptions
+        ),
+      "Should throw if register has duplicate name."
+    );
+  },
+});
