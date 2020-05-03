f59f1202f8ae6a7fa38081d74d91dab3c2c3f2ca
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-05-03 16:12:54 +0000

    Bug 1632905 [wpt PR 23235] - [client hints] Add WPTs for subresource redirects, a=testonly
    Automatic update from web-platform-tests
    [client hints] Add WPTs for subresource redirects
    
    Making sure subresource *don't* register client
    hint preferences on redirects.
    
    Bug: 1042828
    Change-Id: I050e81e9473ce7c771763d9b18fc2e1843ab5546
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2154849
    Commit-Queue: Aaron Tagliaboschi <aarontagchromium.org>
    Reviewed-by: Yoav Weiss <yoavweisschromium.org>
    Cr-Commit-Position: refs/heads/master{#762401}
    
    --
    
    wpt-commits: 39e9c51041a1be64b61281ddc2381ffe33d05b84
    wpt-pr: 23235
    
    UltraBlame original commit: 9a0cd8abc9b59f98033c72b68c831e4feb3ef966

--- testing/web-platform/tests/client-hints/accept-ch-stickiness/resources/accept-ch-test.js
+++ testing/web-platform/tests/client-hints/accept-ch-stickiness/resources/accept-ch-test.js
@@ -35,6 +35,20 @@ function verify_navigation_state(expect_url, test_name) {
   }, test_name + " got client hints according to expectations.");
 }
 
+function verify_subresource_state(expect_url, test_name) {
+  promise_test(t => {
+    return new Promise(resolve => {
+      let win;
+      window.addEventListener('message', t.step_func(function(e) {
+        win.close();
+        assert_equals(e.data, "PASS", "message from opened page");
+        fetch("/client-hints/accept-ch-stickiness/resources/clear-site-data.html").then(resolve);
+      }));
+      
+      fetch(expect_url).then(resolve);
+    });
+  }, test_name + " got client hints according to expectations.");
+}
 const run_test = test => {
   
   
