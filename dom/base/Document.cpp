d4c41add8a4505d02c679e1bf18892c5bbc5ba05
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-05-04 22:04:10 +0000

    Bug 1634732 - Unblock layout if nullifying DocumentL10n in LocalizationLinkRemoved. r=smaug
    Differential Revision: https://phabricator.services.mozilla.com/D73472
    
    UltraBlame original commit: 99f1393843581d4c0b3837ab538119f2dbf31b72

--- dom/base/Document.cpp
+++ dom/base/Document.cpp
@@ -3896,6 +3896,10 @@ void Document::LocalizationLinkRemoved(Element* aLinkElement) {
     aLinkElement->GetAttr(kNameSpaceID_None, nsGkAtoms::href, href);
     uint32_t remaining = mDocumentL10n->RemoveResourceId(href);
     if (remaining == 0) {
+      if (mDocumentL10n->mBlockingLayout) {
+        mDocumentL10n->mBlockingLayout = false;
+        UnblockOnload( false);
+      }
       mDocumentL10n = nullptr;
     }
   }
@@ -3936,8 +3940,8 @@ void Document::InitialTranslationCompleted() {
     
     
     
-    UnblockOnload( false);
     mDocumentL10n->mBlockingLayout = false;
+    UnblockOnload( false);
   }
 
   mL10nProtoElements.Clear();
