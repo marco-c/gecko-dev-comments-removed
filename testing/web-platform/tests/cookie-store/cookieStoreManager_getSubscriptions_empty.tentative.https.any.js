1e5c9309d4c48b7a8378721d2c794c3a46466bfd
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-05-06 12:04:30 +0000

    Bug 1631719 [wpt PR 23133] - *.any.js: explicitly list defaults, part 3, a=testonly
    Automatic update from web-platform-tests
    *.any.js: explicitly list defaults, part 3
    
    *.any.js's META global no longer supports negation or the default feature making everything a bit easier as a result.
    
    See https://github.com/web-platform-tests/rfcs/pull/52, #23117, and #23121.
    
    Closes #23111.
    --
    
    wpt-commits: 96c7f0b57b704c94a7535d58009500ea94c02995
    wpt-pr: 23133
    
    UltraBlame original commit: f317cbb519717184a349a52215dc3d203d8b5007

--- testing/web-platform/tests/cookie-store/cookieStoreManager_getSubscriptions_empty.tentative.https.any.js
+++ testing/web-platform/tests/cookie-store/cookieStoreManager_getSubscriptions_empty.tentative.https.any.js
@@ -1,6 +1,6 @@
-// META: title=Cookie Store API: ServiceWorker without cookie change subscriptions
-// META: global=!default,serviceworker,window
-// META: script=/service-workers/service-worker/resources/test-helpers.sub.js
+
+
+
 
 'use strict';
 
@@ -10,14 +10,14 @@ promise_test(async testCase => {
         testCase, 'resources/empty_sw.js', 'resources/does/not/exist');
     testCase.add_cleanup(() => registration.unregister());
 
-    // Wait for this service worker to become active before snapshotting the
-    // subscription state, for consistency with other tests.
+    
+    
     await wait_for_state(testCase, registration.installing, 'activated');
 
     self.registration = registration;
   } else {
-    // Wait for this service worker to become active before snapshotting the
-    // subscription state, for consistency with other tests.
+    
+    
     await new Promise(resolve => {
       self.addEventListener('activate', event => { resolve(); });
     });
