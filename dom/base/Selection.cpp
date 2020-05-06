cb0363b57f7ade483a8729aa0a4af05490d55355
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-05-06 12:06:21 +0000

    Bug 1633448: part 6) Change `IsUserSelectionNotCollapsed` to `IsUserSelectionCollapsed`. r=hsivonen
    Removes unnecessary double negations.
    
    Differential Revision: https://phabricator.services.mozilla.com/D73234
    
    UltraBlame original commit: a72df76630973741568cacda03e70baada344f45

--- dom/base/Selection.cpp
+++ dom/base/Selection.cpp
@@ -844,19 +844,19 @@ static bool MaybeDispatchSelectstartEvent(
 }
 
 
-bool Selection::IsUserSelectionNotCollapsed(
+bool Selection::IsUserSelectionCollapsed(
     const nsRange& aRange, nsTArray<RefPtr<nsRange>>& aTempRangesToAdd) {
   MOZ_ASSERT(aTempRangesToAdd.IsEmpty());
 
   RefPtr<nsRange> scratchRange = aRange.CloneRange();
   UserSelectRangesToAdd(scratchRange, aTempRangesToAdd);
-  const bool userSelectionNotCollapsed =
-      aTempRangesToAdd.Length() > 1 ||
-      (aTempRangesToAdd.Length() == 1 && !aTempRangesToAdd[0]->Collapsed());
+  const bool userSelectionCollapsed =
+      (aTempRangesToAdd.Length() == 0) ||
+      ((aTempRangesToAdd.Length() == 1) && aTempRangesToAdd[0]->Collapsed());
 
   aTempRangesToAdd.ClearAndRetainStorage();
 
-  return userSelectionNotCollapsed;
+  return userSelectionCollapsed;
 }
 
 nsresult Selection::AddRangesForUserSelectableNodes(
@@ -892,11 +892,10 @@ nsresult Selection::AddRangesForUserSelectableNodes(
     
     
 
-    const bool userSelectionNotCollapsed =
-        IsUserSelectionNotCollapsed(*aRange, rangesToAdd);
-    MOZ_ASSERT(!userSelectionNotCollapsed ||
-               nsContentUtils::IsSafeToRunScript());
-    if (userSelectionNotCollapsed && nsContentUtils::IsSafeToRunScript()) {
+    const bool userSelectionCollapsed =
+        IsUserSelectionCollapsed(*aRange, rangesToAdd);
+    MOZ_ASSERT(userSelectionCollapsed || nsContentUtils::IsSafeToRunScript());
+    if (!userSelectionCollapsed && nsContentUtils::IsSafeToRunScript()) {
       
       
       
