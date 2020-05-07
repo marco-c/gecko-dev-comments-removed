838c932fdf7b4281c982f69398ae3822b8b18905
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-05-07 22:33:24 +0000

    Backed out 5 changesets (bug 1617237) for devtools perma failures. CLOSED TREE
    Backed out changeset 3acc06a17b26 (bug 1617237)
    Backed out changeset dfdf021a542c (bug 1617237)
    Backed out changeset 358ce0a1518f (bug 1617237)
    Backed out changeset 2e647ae9ec48 (bug 1617237)
    Backed out changeset 3e340cc988c8 (bug 1617237)
    
    UltraBlame original commit: 7a1c4923a43de6e5be4aa139f4c4100afa25cae2

--- devtools/client/framework/toolbox.js
+++ devtools/client/framework/toolbox.js
@@ -355,7 +355,6 @@ function Toolbox(
   this._onResumedState = this._onResumedState.bind(this);
   this._onTargetAvailable = this._onTargetAvailable.bind(this);
   this._onTargetDestroyed = this._onTargetDestroyed.bind(this);
-  this._onNavigate = this._onNavigate.bind(this);
 
   this.isPaintFlashing = false;
 
@@ -720,7 +719,7 @@ Toolbox.prototype = {
       
       
       targetFront.on("will-navigate", this._onWillNavigate);
-      targetFront.on("navigate", this._onNavigate);
+      targetFront.on("navigate", this._refreshHostTitle);
       targetFront.on("frame-update", this._updateFrames);
       targetFront.on("inspect-object", this._onInspectObject);
 
@@ -820,6 +819,13 @@ Toolbox.prototype = {
         this._URL = this.win.location.href;
       }
 
+      if (this.hostType === Toolbox.HostType.PAGE) {
+        
+        
+        
+        this._debugTargetData = this._getDebugTargetData();
+      }
+
       const domReady = new Promise(resolve => {
         DOMHelpers.onceDOMReady(
           this.win,
@@ -872,17 +878,14 @@ Toolbox.prototype = {
       this._addChromeEventHandlerEvents();
       this._registerOverlays();
 
-      
-      this._tabBar = this.doc.querySelector(".devtools-tabbar");
-      this._tabBar.addEventListener("keypress", this._onToolbarArrowKeypress);
-
+      this._componentMount.addEventListener(
+        "keypress",
+        this._onToolbarArrowKeypress
+      );
       this._componentMount.setAttribute(
         "aria-label",
         L10N.getStr("toolbox.label")
       );
-
-      
-      this._setDebugTargetData();
 
       this.webconsolePanel = this.doc.querySelector(
         "#toolbox-panel-webconsole"
@@ -990,7 +993,7 @@ Toolbox.prototype = {
   detachTarget() {
     this.target.off("inspect-object", this._onInspectObject);
     this.target.off("will-navigate", this._onWillNavigate);
-    this.target.off("navigate", this._onNavigate);
+    this.target.off("navigate", this._refreshHostTitle);
     this.target.off("frame-update", this._updateFrames);
 
     
@@ -1823,6 +1826,7 @@ Toolbox.prototype = {
       closeToolbox: this.closeToolbox,
       focusButton: this._onToolbarFocus,
       toolbox: this,
+      debugTargetData: this._debugTargetData,
       onTabsOrderUpdated: this._onTabsOrderUpdated,
     });
 
@@ -1856,7 +1860,7 @@ Toolbox.prototype = {
       return;
     }
 
-    const buttons = [...this._tabBar.querySelectorAll("button")];
+    const buttons = [...this._componentMount.querySelectorAll("button")];
     const curIndex = buttons.indexOf(target);
 
     if (curIndex === -1) {
@@ -3700,13 +3704,12 @@ Toolbox.prototype = {
       this.webconsolePanel = null;
     }
     if (this._componentMount) {
-      this._tabBar.removeEventListener(
+      this._componentMount.removeEventListener(
         "keypress",
         this._onToolbarArrowKeypress
       );
       this.ReactDOM.unmountComponentAtNode(this._componentMount);
       this._componentMount = null;
-      this._tabBar = null;
     }
 
     const outstanding = [];
@@ -4201,25 +4204,5 @@ Toolbox.prototype = {
     }
 
     return id;
-  },
-
-  
-
-
-  _onNavigate: function() {
-    this._refreshHostTitle();
-    this._setDebugTargetData();
-  },
-
-  
-
-
-  _setDebugTargetData() {
-    if (this.hostType === Toolbox.HostType.PAGE) {
-      
-      
-      
-      this.component.setDebugTargetData(this._getDebugTargetData());
-    }
   },
 };
