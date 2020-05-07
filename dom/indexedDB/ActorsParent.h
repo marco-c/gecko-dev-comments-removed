294b4dbc24499994fd78dc8b92005294efde2524
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-05-07 01:20:53 +0000

    Bug 1623278 - Make Factory derive from SafeRefCounted. r=dom-workers-and-storage-reviewers,janv
    Differential Revision: https://phabricator.services.mozilla.com/D69398
    
    UltraBlame original commit: 296b2617fceb82bd87efffda2aabe38af402f7af

--- dom/indexedDB/ActorsParent.h
+++ dom/indexedDB/ActorsParent.h
@@ -1,8 +1,8 @@
-/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
-/* vim: set ts=8 sts=2 et sw=2 tw=80: */
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this file,
- * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+
+
+
+
 
 #ifndef mozilla_dom_indexeddb_actorsparent_h__
 #define mozilla_dom_indexeddb_actorsparent_h__
@@ -26,7 +26,7 @@ namespace quota {
 
 class Client;
 
-}  // namespace quota
+}  
 
 namespace indexedDB {
 
@@ -35,7 +35,7 @@ class LoggingInfo;
 class PBackgroundIDBFactoryParent;
 class PBackgroundIndexedDBUtilsParent;
 
-PBackgroundIDBFactoryParent* AllocPBackgroundIDBFactoryParent(
+already_AddRefed<PBackgroundIDBFactoryParent> AllocPBackgroundIDBFactoryParent(
     const LoggingInfo& aLoggingInfo);
 
 bool RecvPBackgroundIDBFactoryConstructor(PBackgroundIDBFactoryParent* aActor,
@@ -71,8 +71,8 @@ class PermissionRequestHelper final : public PermissionRequestBase {
   void OnPromptComplete(PermissionValue aPermissionValue) override;
 };
 
-}  // namespace indexedDB
-}  // namespace dom
-}  // namespace mozilla
+}  
+}  
+}  
 
-#endif  // mozilla_dom_indexeddb_actorsparent_h__
+#endif  
