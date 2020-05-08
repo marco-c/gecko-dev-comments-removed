9d7f01582cddaed496f3021ee226d13030c95844
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-05-08 16:40:09 +0000

    Bug 1635584 - Update BackdropFilter Test Expectations r=emilio
    - Add IsWebRenderEnabled() method to property_database.js
      - Check for WebRender in addition to backdrop-filter pref
      - Update relevant WPT cases to expect fail if not webrender
    
    Differential Revision: https://phabricator.services.mozilla.com/D74162
    
    UltraBlame original commit: 93835c722684458c75541c76f34a61c8ba6045df

--- layout/style/test/property_database.js
+++ layout/style/test/property_database.js
@@ -33,6 +33,19 @@ function IsCSSPropertyPrefEnabled(prefName) {
 }
 
 
+function IsWebRenderEnabled() {
+  try {
+    return SpecialPowers.Cc["@mozilla.org/gfx/info;1"].getService(
+      SpecialPowers.Ci.nsIGfxInfo
+    ).WebRenderEnabled;
+  } catch (ex) {
+    ok(false, "Failed to check WebRender's enabled state");
+  }
+
+  return false;
+}
+
+
 const CSS_TYPE_LONGHAND = 0;
 
 
@@ -13165,7 +13178,10 @@ if (IsCSSPropertyPrefEnabled("layout.css.step-position-jump.enabled")) {
   );
 }
 
-if (IsCSSPropertyPrefEnabled("layout.css.backdrop-filter.enabled")) {
+if (
+  IsCSSPropertyPrefEnabled("layout.css.backdrop-filter.enabled") &&
+  IsWebRenderEnabled()
+) {
   gCSSProperties["backdrop-filter"] = {
     domProp: "backdropFilter",
     inherited: false,
