075c4fd883aeacab65377c73c9867793e2368b69
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-05-08 16:38:23 +0000

    Bug 1636224 - Explicitly flush layout in an acceskey test. r=Gijs
    Accesskey for XUL works from frame construction, so we need to do this
    for the test to keep passing with LazyFC.
    
    I don't think this has a user-visible impact in practice.
    
    Differential Revision: https://phabricator.services.mozilla.com/D74280
    
    UltraBlame original commit: 695d8ba293ae09c196976b40e9dd4508b4046f56

--- browser/base/content/test/general/browser_accesskeys.js
+++ browser/base/content/test/general/browser_accesskeys.js
@@ -32,6 +32,8 @@ add_task(async function() {
 
   Services.focus.clearFocus(window);
 
+  newButton.getBoundingClientRect(); 
+
   focusedId = await performAccessKeyForChrome("z");
   is(focusedId, "chromebutton", "chromebutton accesskey");
 
