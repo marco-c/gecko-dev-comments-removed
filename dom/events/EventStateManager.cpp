a727747786f47d0c2468aad04c91c07025a0a306
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-05-08 16:38:52 +0000

    Bug 1636012 - Remove TYPES_OF_USER_CLICKS probe r=smaug
    Differential Revision: https://phabricator.services.mozilla.com/D74250
    
    UltraBlame original commit: d543b6ceacd49b099d4b80014e59592109a979da

--- dom/events/EventStateManager.cpp
+++ dom/events/EventStateManager.cpp
@@ -4959,10 +4959,6 @@ nsresult EventStateManager::InitAndDispatchClickEvent(
   nsresult rv = aPresShell->HandleEventWithTarget(
       &event, targetFrame, MOZ_KnownLive(target), &status);
 
-  if (event.mFlags.mHadNonPrivilegedClickListeners && !aNoContentDispatch) {
-    Telemetry::AccumulateCategorical(
-        Telemetry::LABELS_TYPES_OF_USER_CLICKS::Has_JS_Listener);
-  }
   
   
   
@@ -5087,14 +5083,6 @@ nsresult EventStateManager::DispatchClickEvents(
     if (NS_WARN_IF(NS_FAILED(rv))) {
       return rv;
     }
-  }
-
-  
-  
-  if (XRE_IsParentProcess() && !IsRemoteTarget(aClickTarget) &&
-      !notDispatchToContents) {
-    Telemetry::AccumulateCategorical(
-        Telemetry::LABELS_TYPES_OF_USER_CLICKS::Browser_Chrome);
   }
 
   return rv;
