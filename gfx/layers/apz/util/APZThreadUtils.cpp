1d6b2430fb62d23ff73c728dba5c785b6851d727
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-05-09 10:47:09 +0000

    Bug 1635001 - P3. Fix APZ controller thread-safety access. r=kats
    The APZ was keeping a raw pointer to the controller thread. This was a dangerous exercise.
    This was okay on desktop, as the controller thread was the main thread and would have outlived everything else. On Android however it's the UI thread and it could get deleted before we received a last input event.
    
    So we use a strong pointer instead to prevent the thread from being deleted and as such, we now needs to explicitly clear it on shutdown.
    
    This requires the various methods in APZThreadUtils to be made thread-safe so that the controller thread can be shutdown mid-air.
    
    Differential Revision: https://phabricator.services.mozilla.com/D73830
    
    UltraBlame original commit: 49df0c8504b8521913eca4fc6163017e0133e6bb

--- gfx/layers/apz/util/APZThreadUtils.cpp
+++ gfx/layers/apz/util/APZThreadUtils.cpp
@@ -6,19 +6,25 @@
 
 #include "mozilla/layers/APZThreadUtils.h"
 
+#include "mozilla/ClearOnShutdown.h"
+#include "mozilla/StaticMutex.h"
+
 namespace mozilla {
 namespace layers {
 
 static bool sThreadAssertionsEnabled = true;
-static nsISerialEventTarget* sControllerThread;
+static StaticRefPtr<nsISerialEventTarget> sControllerThread;
+static StaticMutex sControllerThreadMutex;
 
 
 void APZThreadUtils::SetThreadAssertionsEnabled(bool aEnabled) {
+  StaticMutexAutoLock lock(sControllerThreadMutex);
   sThreadAssertionsEnabled = aEnabled;
 }
 
 
 bool APZThreadUtils::GetThreadAssertionsEnabled() {
+  StaticMutexAutoLock lock(sControllerThreadMutex);
   return sThreadAssertionsEnabled;
 }
 
@@ -26,51 +32,73 @@ bool APZThreadUtils::GetThreadAssertionsEnabled() {
 void APZThreadUtils::SetControllerThread(nsISerialEventTarget* aThread) {
   
   
+  StaticMutexAutoLock lock(sControllerThreadMutex);
   MOZ_ASSERT(!sControllerThread || !aThread || sControllerThread == aThread);
-  sControllerThread = aThread;
+  if (aThread != sControllerThread) {
+    
+    sControllerThread = aThread;
+    ClearOnShutdown(&sControllerThread);
+  }
 }
 
 
 void APZThreadUtils::AssertOnControllerThread() {
+#if DEBUG
   if (!GetThreadAssertionsEnabled()) {
     return;
   }
-
-  MOZ_ASSERT(sControllerThread->IsOnCurrentThread());
+  StaticMutexAutoLock lock(sControllerThreadMutex);
+  MOZ_ASSERT(sControllerThread && sControllerThread->IsOnCurrentThread());
+#endif
 }
 
 
 void APZThreadUtils::RunOnControllerThread(RefPtr<Runnable>&& aTask) {
+  RefPtr<nsISerialEventTarget> thread;
+  {
+    StaticMutexAutoLock lock(sControllerThreadMutex);
+    thread = sControllerThread;
+  }
   RefPtr<Runnable> task = std::move(aTask);
 
-  if (!sControllerThread) {
+  if (!thread) {
     
     NS_WARNING("Dropping task posted to controller thread");
     return;
   }
 
-  if (sControllerThread->IsOnCurrentThread()) {
+  if (thread->IsOnCurrentThread()) {
     task->Run();
   } else {
-    sControllerThread->Dispatch(task.forget());
+    thread->Dispatch(task.forget());
   }
 }
 
 
 bool APZThreadUtils::IsControllerThread() {
-  return sControllerThread == NS_GetCurrentThread();
+  StaticMutexAutoLock lock(sControllerThreadMutex);
+  return sControllerThread && sControllerThread->IsOnCurrentThread();
 }
 
 
 void APZThreadUtils::DelayedDispatch(already_AddRefed<Runnable> aRunnable,
-                                     int aDelayMs) {
-  MOZ_ASSERT(sControllerThread && sControllerThread->IsOnCurrentThread());
+                                int aDelayMs) {
   MOZ_ASSERT(!XRE_IsContentProcess(),
              "ContentProcessController should only be used remotely.");
+  RefPtr<nsISerialEventTarget> thread;
+  {
+    StaticMutexAutoLock lock(sControllerThreadMutex);
+    thread = sControllerThread;
+  }
+  if (!thread) {
+    
+    NS_WARNING("Dropping task posted to controller thread");
+    return;
+  }
   if (aDelayMs) {
-    sControllerThread->DelayedDispatch(std::move(aRunnable), aDelayMs);
+    thread->DelayedDispatch(std::move(aRunnable), aDelayMs);
   } else {
-    sControllerThread->Dispatch(std::move(aRunnable));
+    thread->Dispatch(std::move(aRunnable));
   }
 }
 
