1b048b7161e506c6e59cc6c09cf3e1d5b3f97f07
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-05-12 01:39:25 +0000

    Bug 1625961 - Use the ResourceWatcher API to fetch Root NodeFront r=ochameau,nchevobbe
    Depends on D62625
    
    Differential Revision: https://phabricator.services.mozilla.com/D72668
    
    UltraBlame original commit: c24dddd0d63679bca92512a9d50e428029af2ab3

--- devtools/client/inspector/inspector.js
+++ devtools/client/inspector/inspector.js
@@ -166,6 +166,7 @@ function Inspector(toolbox) {
   this.onHostChanged = this.onHostChanged.bind(this);
   this.onMarkupLoaded = this.onMarkupLoaded.bind(this);
   this.onNewSelection = this.onNewSelection.bind(this);
+  this.onResourceAvailable = this.onResourceAvailable.bind(this);
   this.onRootNodeAvailable = this.onRootNodeAvailable.bind(this);
   this.onPanelWindowResize = this.onPanelWindowResize.bind(this);
   this.onShowBoxModelHighlighterForNode = this.onShowBoxModelHighlighterForNode.bind(
@@ -200,6 +201,10 @@ Inspector.prototype = {
       this._onTargetDestroyed
     );
 
+    await this.toolbox.resourceWatcher.watch(
+      [this.toolbox.resourceWatcher.TYPES.ROOT_NODE],
+      this.onResourceAvailable
+    );
     
     
     this.previousURL = this.currentTarget.url;
@@ -222,8 +227,6 @@ Inspector.prototype = {
       this._getCssProperties(),
       this._getAccessibilityFront(),
     ]);
-
-    this.walker.watchRootNode(this.onRootNodeAvailable);
   },
 
   _onTargetDestroyed({ type, targetFront, isTopLevel }) {
@@ -1276,6 +1279,16 @@ Inspector.prototype = {
         this.onEyeDropperButtonClicked
       );
       this.eyeDropperButton = null;
+    }
+  },
+
+  onResourceAvailable: function({ resourceType, targetFront, resource }) {
+    if (resourceType === this.toolbox.resourceWatcher.TYPES.ROOT_NODE) {
+      
+      
+      
+      
+      this.onRootNodeAvailable();
     }
   },
 
