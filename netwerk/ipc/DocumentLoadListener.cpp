1cced4d1fcfc10c1b0cd24a03d6fb0f6dd7c8eb1
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-06-03 04:39:12 +0000

    Bug 1639140 - Remove HasStoragePermissionInParent in DocumentLoadListener r=timhuang,necko-reviewers a=reland CLOSED TREE
    We don't need to call this API in DocumentLoadListener::AsyncOpen because we have
    already called this in nsHttpChannel::AsyncOpen
    
    Differential Revision: https://phabricator.services.mozilla.com/D76275
    
    UltraBlame original commit: 3c7061e3b8a00dccf50fe458cfe6d36a86600876

--- netwerk/ipc/DocumentLoadListener.cpp
+++ netwerk/ipc/DocumentLoadListener.cpp
@@ -425,10 +425,6 @@ bool DocumentLoadListener::Open(
     httpBaseChannel->SetTopWindowURI(topWindowURI);
   }
 
-  
-  Unused << loadInfo->SetHasStoragePermission(
-      AntiTrackingUtils::HasStoragePermissionInParent(mChannel));
-
   nsCOMPtr<nsIIdentChannel> identChannel = do_QueryInterface(mChannel);
   if (identChannel && aChannelId) {
     Unused << identChannel->SetChannelId(*aChannelId);
