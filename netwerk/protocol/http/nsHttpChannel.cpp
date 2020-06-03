f4f8e2452b6de84c6bf39e084777b34601cb0977
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-06-03 04:21:59 +0000

    Bug 1637634 - Update IsOnContentBlockingAllowList in nsHttpChannel::AsyncOpen r=timhuang,necko-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D76152
    
    UltraBlame original commit: efd4bcfa61dc7f7f654cb5702a7db4a60ca04a0b

--- netwerk/protocol/http/nsHttpChannel.cpp
+++ netwerk/protocol/http/nsHttpChannel.cpp
@@ -1863,14 +1863,20 @@ void nsHttpChannel::UpdateAntiTrackingInfo() {
 
   AntiTrackingUtils::ComputeIsThirdPartyToTopWindow(this);
 
-  
-  
   if (mLoadInfo->GetExternalContentPolicyType() ==
       nsIContentPolicy::TYPE_DOCUMENT) {
     nsCOMPtr<nsICookieJarSettings> cookieJarSettings;
     Unused << mLoadInfo->GetCookieJarSettings(
         getter_AddRefs(cookieJarSettings));
 
+    
+    
+    
+    mozilla::net::CookieJarSettings::Cast(cookieJarSettings)
+        ->UpdateIsOnContentBlockingAllowList(this);
+
+    
+    
     mozilla::net::CookieJarSettings::Cast(cookieJarSettings)
         ->SetFirstPartyDomain(mURI);
   }
