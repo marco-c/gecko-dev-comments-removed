917cae7c1edad13d7c9bc0d4ec7bda9dccd5419e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-06-03 03:42:50 +0000

    Bug 1309824 - Show proper descriptions for storage types. r=jdescottes,fluent-reviewers,flod
    Differential Revision: https://phabricator.services.mozilla.com/D70663
    
    UltraBlame original commit: ff07fdc7c70e1c0a45e10535e26e5dec7958a7a0

--- devtools/client/shared/widgets/TableWidget.js
+++ devtools/client/shared/widgets/TableWidget.js
@@ -71,6 +71,7 @@ Object.defineProperty(this, "EVENTS", {
 
 
 
+
 function TableWidget(node, options = {}) {
   EventEmitter.decorate(this);
 
@@ -87,6 +88,7 @@ function TableWidget(node, options = {}) {
     firstColumn,
     wrapTextInElements,
     cellContextMenuId,
+    l10n,
   } = options;
   this.emptyText = emptyText || "";
   this.uniqueId = uniqueId || "name";
@@ -95,6 +97,7 @@ function TableWidget(node, options = {}) {
   this.highlightUpdated = highlightUpdated || false;
   this.removableColumns = removableColumns !== false;
   this.cellContextMenuId = cellContextMenuId;
+  this.l10n = l10n;
 
   this.tbody = this.document.createXULElement("hbox");
   this.tbody.className = "table-widget-body theme-body";
@@ -104,10 +107,11 @@ function TableWidget(node, options = {}) {
   this.afterScroll = this.afterScroll.bind(this);
   this.tbody.addEventListener("scroll", this.onScroll.bind(this));
 
-  this.placeholder = this.document.createXULElement("label");
+  
+  this.placeholder = this.document.createElement("div");
   this.placeholder.className = "plain table-widget-empty-text";
-  this.placeholder.setAttribute("flex", "1");
   this._parent.appendChild(this.placeholder);
+  this.setPlaceholder(this.emptyText);
 
   this.items = new Map();
   this.columns = new Map();
@@ -121,8 +125,6 @@ function TableWidget(node, options = {}) {
 
   if (initialColumns) {
     this.setColumns(initialColumns, uniqueId);
-  } else if (this.emptyText) {
-    this.setPlaceholderText(this.emptyText);
   }
 
   this.bindSelectedRow = id => {
@@ -658,8 +660,28 @@ TableWidget.prototype = {
   
 
 
-  setPlaceholderText: function(text) {
-    this.placeholder.setAttribute("value", text);
+
+
+
+
+
+
+  setPlaceholder: function(l10nID, learnMoreURL) {
+    if (learnMoreURL) {
+      let placeholderLink = this.placeholder.firstElementChild;
+      if (!placeholderLink) {
+        placeholderLink = this.document.createElement("a");
+        placeholderLink.setAttribute("target", "_blank");
+        placeholderLink.setAttribute("data-l10n-name", "learn-more-link");
+        this.placeholder.appendChild(placeholderLink);
+      }
+      placeholderLink.setAttribute("href", learnMoreURL);
+    } else {
+      
+      this.placeholder.firstElementChild?.remove();
+    }
+
+    this.l10n.setAttributes(this.placeholder, l10nID);
   },
 
   
@@ -955,7 +977,7 @@ TableWidget.prototype = {
       column.clear();
     }
     this.tbody.setAttribute("empty", "empty");
-    this.setPlaceholderText(this.emptyText);
+    this.setPlaceholder(this.emptyText);
 
     this.selectedRow = null;
 
