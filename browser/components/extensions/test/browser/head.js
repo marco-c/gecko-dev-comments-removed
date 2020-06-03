d70514a74e66d12450a0d91e00af6c878f37ce0b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-06-03 04:08:56 +0000

    Backed out 2 changesets (bug 1633968) for browser_ext_menus_events.js failures CLOSED TREE
    Backed out changeset 9dc3a4d32630 (bug 1633968)
    Backed out changeset 55cc61b65e56 (bug 1633968)
    
    UltraBlame original commit: 9194f91b1fa27b48b18317ab806afdcc6c9dc9a3

--- browser/components/extensions/test/browser/head.js
+++ browser/components/extensions/test/browser/head.js
@@ -669,15 +669,14 @@ function closeActionContextMenu(itemToSelect, kind, win = window) {
   return closeChromeContextMenu(menuID, itemToSelect, win);
 }
 
-function openTabContextMenu(tab = gBrowser.selectedTab) {
+function openTabContextMenu(win = window) {
   
   
-  tab.focus();
-  let indexOfTab = Array.prototype.indexOf.call(tab.parentNode.children, tab);
+  gBrowser.selectedTab.focus();
   return openChromeContextMenu(
     "tabContextMenu",
-    `.tabbrowser-tab:nth-child(${indexOfTab + 1})`,
-    tab.ownerGlobal
+    ".tabbrowser-tab[selected]",
+    win
   );
 }
 
