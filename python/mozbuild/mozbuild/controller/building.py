9a484c937a10c0589c255a72ea55b1f376e774fb
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-06-03 03:41:16 +0000

    Bug 1635112 - Use `clang-tidy` as warning build error detector for our `static-analysis` targets. r=glandium
    Differential Revision: https://phabricator.services.mozilla.com/D73692
    
    UltraBlame original commit: 8ade9f1318ba08fed3e91ce3df7e8b28fc11131b

--- python/mozbuild/mozbuild/controller/building.py
+++ python/mozbuild/mozbuild/controller/building.py
@@ -225,6 +225,10 @@ class BuildMonitor(MozbuildObject):
         self.instance_warnings = WarningsDatabase()
 
         def on_warning(warning):
+            
+            if warning['type'] == 'error':
+                return
+
             filename = warning['filename']
 
             if not os.path.exists(filename):
