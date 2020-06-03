5103d9d6087a38ef6d4df58076e1469194ecedc5
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-06-03 02:34:05 +0000

    Bug 1607569 - Fix leak in browser_console_devtools_loader_exception. r=ochameau.
    In the Browser toolbox destroy function, we now wait for
    any pending connection initialization promise.
    In the test we close the browser toolbox at the end.
    We also need to guard a few method in the connection proxy
    to ensure the instances we use are still alive before using
    them.
    
    Differential Revision: https://phabricator.services.mozilla.com/D71171
    
    UltraBlame original commit: 0be5f9822cad3a24e098f7b46ed4c7c061330942

--- devtools/client/webconsole/browser-console.js
+++ devtools/client/webconsole/browser-console.js
@@ -113,6 +113,9 @@ class BrowserConsole extends WebConsole {
       
       this._telemetry.toolClosed("browserconsole", -1, this);
 
+      
+      await Promise.all(this.ui.getAllProxies().map(proxy => proxy._connecter));
+
       await this.targetList.stopListening();
       await super.destroy();
       await this.currentTarget.destroy();
