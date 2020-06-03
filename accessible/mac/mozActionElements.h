fc7b2f0751f5df3f36ac758cf7837cbfe78566c0
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-06-03 05:03:04 +0000

    Bug 1624909: Create and maintain radio siblings array for position information. r=eeejay
    Differential Revision: https://phabricator.services.mozilla.com/D72751
    
    UltraBlame original commit: 156d42f01488fb24914199fe4670c70cfffe4323

--- accessible/mac/mozActionElements.h
+++ accessible/mac/mozActionElements.h
@@ -1,12 +1,12 @@
-/* -*- Mode: Objective-C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+
+
+
 
 #import <Cocoa/Cocoa.h>
 #import "mozAccessible.h"
 
-/* Simple subclasses for things like checkboxes, buttons, etc. */
+
 
 @interface mozButtonAccessible : mozAccessible {
 }
@@ -17,20 +17,25 @@
 @end
 
 @interface mozCheckboxAccessible : mozButtonAccessible
-// returns one of the constants defined in CheckboxValue
+
 - (int)isChecked;
 @end
 
-/**
- * Accessible for a PANE
- */
+
+@interface mozRadioButtonAccessible : mozCheckboxAccessible
+- (id)accessibilityAttributeValue:(NSString*)attribute;
+@end
+
+
+
+
 @interface mozPaneAccessible : mozAccessible
 
 @end
 
-/**
- * Base accessible for an incrementable
- */
+
+
+
 @interface mozIncrementableAccessible : mozAccessible
 
 @end
