d7cdc27f4572f4c6a7591103d26ade3f8aa021db
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-06-03 03:41:25 +0000

    Bug 1557727 - Part 2: Add Intl.DisplayNames boilerplate. r=jwalden
    Adds the standard boilerplate for a new Intl object.
    
    Differential Revision: https://phabricator.services.mozilla.com/D52164
    
    UltraBlame original commit: 93aa11dd931b57852c0242e39889feaf873fd53f

--- js/src/builtin/intl/CommonFunctions.js
+++ js/src/builtin/intl/CommonFunctions.js
@@ -706,6 +706,7 @@ function initializeIntlObject(obj, type, lazyData) {
     assert(IsObject(obj), "Non-object passed to initializeIntlObject");
     assert((type === "Collator" && GuardToCollator(obj) !== null) ||
            (type === "DateTimeFormat" && GuardToDateTimeFormat(obj) !== null) ||
+           (type === "DisplayNames" && GuardToDisplayNames(obj) !== null) ||
            (type === "ListFormat" && GuardToListFormat(obj) !== null) ||
            (type === "NumberFormat" && GuardToNumberFormat(obj) !== null) ||
            (type === "PluralRules" && GuardToPluralRules(obj) !== null) ||
@@ -713,6 +714,12 @@ function initializeIntlObject(obj, type, lazyData) {
            "type must match the object's class");
     assert(IsObject(lazyData), "non-object lazy data");
 
+    
+    
+    
+    
+    
+    
     
     
     
@@ -775,6 +782,7 @@ function getIntlObjectInternals(obj) {
     assert(IsObject(obj), "getIntlObjectInternals called with non-Object");
     assert(GuardToCollator(obj) !== null ||
            GuardToDateTimeFormat(obj) !== null ||
+           GuardToDisplayNames(obj) !== null ||
            GuardToListFormat(obj) !== null ||
            GuardToNumberFormat(obj) !== null ||
            GuardToPluralRules(obj) !== null ||
@@ -787,6 +795,7 @@ function getIntlObjectInternals(obj) {
     assert(hasOwn("type", internals), "missing type");
     assert((internals.type === "Collator" && GuardToCollator(obj) !== null) ||
            (internals.type === "DateTimeFormat" && GuardToDateTimeFormat(obj) !== null) ||
+           (internals.type === "DisplayNames" && GuardToDisplayNames(obj) !== null) ||
            (internals.type === "ListFormat" && GuardToListFormat(obj) !== null) ||
            (internals.type === "NumberFormat" && GuardToNumberFormat(obj) !== null) ||
            (internals.type === "PluralRules" && GuardToPluralRules(obj) !== null) ||
@@ -816,6 +825,8 @@ function getInternals(obj) {
         internalProps = resolveCollatorInternals(internals.lazyData);
     else if (type === "DateTimeFormat")
         internalProps = resolveDateTimeFormatInternals(internals.lazyData);
+    else if (type === "DisplayNames")
+      internalProps = resolveDisplayNamesInternals(internals.lazyData);
     else if (type === "ListFormat")
         internalProps = resolveListFormatInternals(internals.lazyData);
     else if (type === "NumberFormat")
