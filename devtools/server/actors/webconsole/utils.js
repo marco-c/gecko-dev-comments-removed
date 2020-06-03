4a0fefac09e0f3346d5026e41c77f73a6961197a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-06-03 03:37:24 +0000

    Bug 1546394 Adds :block <url> command similar to netmonitor r=nchevobbe
    Differential Revision: https://phabricator.services.mozilla.com/D69803
    
    UltraBlame original commit: 1b05fad0314acc7e293d10789b609280d28fe83c

--- devtools/server/actors/webconsole/utils.js
+++ devtools/server/actors/webconsole/utils.js
@@ -655,6 +655,60 @@ WebConsoleCommands._registerOriginal("screenshot", function(owner, args = {}) {
 
 
 
+
+WebConsoleCommands._registerOriginal("block", function(owner, args = {}) {
+  if (!args.url) {
+    owner.helperResult = {
+      type: "error",
+      message: "webconsole.messages.commands.blockArgMissing",
+    };
+    return;
+  }
+
+  owner.helperResult = (async () => {
+    await owner.consoleActor.blockRequest(args);
+
+    return {
+      type: "blockURL",
+      args,
+    };
+  })();
+});
+
+
+
+
+
+
+
+
+
+WebConsoleCommands._registerOriginal("unblock", function(owner, args = {}) {
+  if (!args.url) {
+    owner.helperResult = {
+      type: "error",
+      message: "webconsole.messages.commands.blockArgMissing",
+    };
+    return;
+  }
+
+  owner.helperResult = (async () => {
+    await owner.consoleActor.unblockRequest(args);
+
+    return {
+      type: "unblockURL",
+      args,
+    };
+  })();
+});
+
+
+
+
+
+
+
+
 function addWebConsoleCommands(owner) {
   
   
