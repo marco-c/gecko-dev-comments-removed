8f9eaabcde77e7ef0c71d9ef8aa45ea4d3a4e41f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-06-03 04:53:11 +0000

    Bug 1638539 [wpt PR 23655] - Add interfaces/device-memory.idl and test, a=testonly
    Automatic update from web-platform-tests
    Add interfaces/device-memory.idl and test (#23655)
    
    Closes https://github.com/web-platform-tests/wpt/pull/21296
    --
    
    wpt-commits: c0cdd63f19507a784531ff4ccc1c71456f79b867
    wpt-pr: 23655
    
    UltraBlame original commit: 1ae814754d39f318d4e6ca0fd4f915b1f6824573

---
+++ testing/web-platform/tests/device-memory/idlharness.https.any.js
@@ -0,0 +1,19 @@
+
+
+
+
+
+
+"use strict";
+
+idl_test(
+  ['device-memory'],
+  ['html'],
+  async idl_array => {
+    if (self.GLOBAL.isWorker()) {
+      idl_array.add_objects({ WorkerNavigator: ['navigator'] });
+    } else {
+      idl_array.add_objects({ Navigator: ['navigator'] });
+    }
+  }
+);
