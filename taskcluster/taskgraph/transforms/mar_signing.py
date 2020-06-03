df84ef45df6b8faca00aeb2ba844cd46b6b55fc5
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-06-03 03:39:15 +0000

    Bug 1623082 - Add shippable attributes where there is nightly attributes in taskcluster/ci. r=aki
    Differential Revision: https://phabricator.services.mozilla.com/D67157
    
    UltraBlame original commit: 7b49f08c3a00881cb7a21f05bb699b97b0517e04

--- taskcluster/taskgraph/transforms/mar_signing.py
+++ taskcluster/taskgraph/transforms/mar_signing.py
@@ -125,13 +125,13 @@ def make_task_description(config, jobs):
         else:
             upstream_artifacts = generate_complete_artifacts(dep_job, config.kind)
 
-        is_nightly = job.get(
-            'nightly',  
+        is_shippable = job.get(
+            'shippable',  
             dep_job.attributes.get(
-                'nightly',  
-                dep_job.attributes.get('shippable')))  
+                'shippable',  
+                dep_job.attributes.get('nightly')))  
         signing_cert_scope = get_signing_cert_scope_per_platform(
-            build_platform, is_nightly, config
+            build_platform, is_shippable, config
         )
 
         scopes = [signing_cert_scope]
