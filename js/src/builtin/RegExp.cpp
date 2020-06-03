fa0d0ae207fa8f3e37eecd5369a29ca70bcbdcf4
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-06-03 04:17:58 +0000

    Bug 1362154: Part 2: Avoid rooting hazard in CheckPatternSyntax r=mgaudet
    In debug, CheckPatternSyntax holds an unrooted RegExpShared across a call to the regexp parser. This is the only remaining obstacle to allocating GC things while parsing regexps.
    
    Differential Revision: https://phabricator.services.mozilla.com/D76034
    
    UltraBlame original commit: e7d2300295143bf951d8bdecd7619ea78722bb80

--- js/src/builtin/RegExp.cpp
+++ js/src/builtin/RegExp.cpp
@@ -209,7 +209,8 @@ static RegExpShared* CheckPatternSyntax(JSContext* cx, HandleAtom pattern,
   
   
 
-  if (RegExpShared* shared = cx->zone()->regExps().maybeGet(pattern, flags)) {
+  RootedRegExpShared shared(cx, cx->zone()->regExps().maybeGet(pattern, flags));
+  if (shared) {
 #ifdef DEBUG
     
     if (!CheckPatternSyntaxSlow(cx, pattern, flags)) {
