681811a1675bbacb9fe55117e9ea904ed27542f5
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-06-03 05:26:10 +0000

    Bug 1641949. Removing the rest of the hacky print prefs code introduced by bug 1276717. r=bobowen
    Differential Revision: https://phabricator.services.mozilla.com/D77506
    
    UltraBlame original commit: 7d4fa9353c53affa279d362f5126c145eb2d2aad

--- widget/nsPrintSettingsService.cpp
+++ widget/nsPrintSettingsService.cpp
@@ -417,9 +417,6 @@ nsresult nsPrintSettingsService::ReadPrefs(nsIPrintSettings* aPS,
     
     
     
-#if defined(XP_WIN)
-    bool saveSanitizedSizePrefs = false;
-#endif
     if (success) {
       success = (sizeUnit != nsIPrintSettings::kPaperSizeInches) ||
                 (width < 100.0) || (height < 100.0);
@@ -434,12 +431,6 @@ nsresult nsPrintSettingsService::ReadPrefs(nsIPrintSettings* aPS,
       DUMP_DBL(kReadStr, kPrintPaperHeight, height);
       aPS->SetPaperName(str);
       DUMP_STR(kReadStr, kPrintPaperName, str.get());
-#if defined(XP_WIN)
-      if (saveSanitizedSizePrefs) {
-        SavePrintSettingsToPrefs(aPS, !aPrinterName.IsEmpty(),
-                                 nsIPrintSettings::kInitSavePaperSize);
-      }
-#endif
     }
   }
 
@@ -683,20 +674,6 @@ nsresult nsPrintSettingsService::WritePrefs(nsIPrintSettings* aPS,
       WritePrefDouble(GetPrefName(kPrintPaperHeight, aPrinterName), height);
       DUMP_STR(kWriteStr, kPrintPaperName, name.get());
       Preferences::SetString(GetPrefName(kPrintPaperName, aPrinterName), name);
-#if defined(XP_WIN)
-      
-      
-      
-      
-      
-      if (height == -1L && width == -1L) {
-        const char* paperSizeTypePref =
-            GetPrefName("print_paper_size_type", aPrinterName);
-        if (Preferences::HasUserValue(paperSizeTypePref)) {
-          Preferences::ClearUser(paperSizeTypePref);
-        }
-      }
-#endif
     }
   }
 
