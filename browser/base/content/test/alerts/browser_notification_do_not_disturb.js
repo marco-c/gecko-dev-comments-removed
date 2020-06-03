3665363c59cb0c7bccae1b25d47ee97f52959d62
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-06-03 04:47:51 +0000

    Bug 1640744 - Modernize and re-enable the browser_notification_do_not_disturb.js test. r=MattN
    Differential Revision: https://phabricator.services.mozilla.com/D76757
    
    UltraBlame original commit: a0a1d1b45d0dae95e7de32e9f7bd158be6b2d522

--- browser/base/content/test/alerts/browser_notification_do_not_disturb.js
+++ browser/base/content/test/alerts/browser_notification_do_not_disturb.js
@@ -1,16 +1,34 @@
+
+
+
 "use strict";
 
-var tab;
-var notificationURL =
-  "http://example.org/browser/browser/base/content/test/alerts/file_dom_notifications.html";
+
+
+
+
+
 
 const ALERT_SERVICE = Cc["@mozilla.org/alerts-service;1"]
   .getService(Ci.nsIAlertsService)
   .QueryInterface(Ci.nsIAlertsDoNotDisturb);
 
-function test() {
-  waitForExplicitFinish();
+const PAGE =
+  "http://example.org/browser/browser/base/content/test/alerts/file_dom_notifications.html";
 
+
+
+const NOTIFICATION_TIMEOUT_SECS = 2000;
+
+add_task(async function setup() {
+  await addNotificationPermission(PAGE);
+});
+
+
+
+
+
+add_task(async function test_manualDoNotDisturb() {
   try {
     
     
@@ -21,14 +39,14 @@ function test() {
       true,
       "Alert service doesn't implement do-not-disturb interface, exiting test"
     );
-    finish();
     return;
   }
 
-  registerCleanupFunction(function() {
+  
+  
+  
+  registerCleanupFunction(() => {
     ALERT_SERVICE.manualDoNotDisturb = false;
-    gBrowser.removeTab(tab);
-    window.restore();
   });
 
   
@@ -36,56 +54,54 @@ function test() {
     !ALERT_SERVICE.manualDoNotDisturb,
     "Alert service should not be disabled when test starts"
   );
-  ALERT_SERVICE.manualDoNotDisturb = false;
 
-  addNotificationPermission(notificationURL).then(function openTab() {
-    tab = BrowserTestUtils.addTab(gBrowser, notificationURL);
-    gBrowser.selectedTab = tab;
-    BrowserTestUtils.browserLoaded(tab.linkedBrowser).then(() => onLoad());
+  await BrowserTestUtils.withNewTab(PAGE, async browser => {
+    await openNotification(browser, "showNotification2");
+
+    info("Notification alert showing");
+
+    let alertWindow = Services.wm.getMostRecentWindow("alert:alert");
+
+    
+    
+    
+    
+    
+    ok(alertWindow, "Expected a XUL alert window.");
+
+    
+    
+    
+    let doNotDisturbMenuItem = alertWindow.document.getElementById(
+      "doNotDisturbMenuItem"
+    );
+    is(doNotDisturbMenuItem.localName, "menuitem", "menuitem found");
+
+    let unloadPromise = BrowserTestUtils.waitForEvent(
+      alertWindow,
+      "beforeunload"
+    );
+
+    doNotDisturbMenuItem.click();
+    info("Clicked on do-not-disturb menuitem");
+    await unloadPromise;
+
+    
+    
+    ok(
+      ALERT_SERVICE.manualDoNotDisturb,
+      "Alert service should be disabled after clicking menuitem"
+    );
+
+    
+    
+    
+    await Assert.rejects(
+      openNotification(browser, "showNotification2", NOTIFICATION_TIMEOUT_SECS),
+      /timed out/,
+      "The notification should never display."
+    );
+
+    ALERT_SERVICE.manualDoNotDisturb = false;
   });
-}
-
-function onLoad() {
-  openNotification(tab.linkedBrowser, "showNotification2").then(onAlertShowing);
-}
-
-function onAlertShowing() {
-  info("Notification alert showing");
-
-  let alertWindow = Services.wm.getMostRecentWindow("alert:alert");
-  if (!alertWindow) {
-    ok(true, "Notifications don't use XUL windows on all platforms.");
-    closeNotification(tab.linkedBrowser).then(finish);
-    return;
-  }
-  let doNotDisturbMenuItem = alertWindow.document.getElementById(
-    "doNotDisturbMenuItem"
-  );
-  is(doNotDisturbMenuItem.localName, "menuitem", "menuitem found");
-  alertWindow.addEventListener("beforeunload", onAlertClosing);
-  doNotDisturbMenuItem.click();
-  info("Clicked on do-not-disturb menuitem");
-}
-
-function onAlertClosing(event) {
-  event.target.removeEventListener("beforeunload", onAlertClosing);
-
-  ok(
-    ALERT_SERVICE.manualDoNotDisturb,
-    "Alert service should be disabled after clicking menuitem"
-  );
-
-  
-  
-  
-  
-  openNotification(tab.linkedBrowser, "showNotification2", 2000).then(
-    onAlert2Showing,
-    finish
-  );
-}
-
-function onAlert2Showing() {
-  ok(false, "the second alert should not have been shown");
-  closeNotification(tab.linkedBrowser).then(finish);
-}
+});
