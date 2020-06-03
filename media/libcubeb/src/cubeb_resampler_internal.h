78410bc4d80c039399f159ae17987f2cecc957ec
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-06-03 05:11:06 +0000

    Bug 1631724 - Temporarily disable an assert inside the cubeb's resampler. r=achronop
    Reenabling is tracked in https://bugzilla.mozilla.org/show_bug.cgi?id=1641234
    
    Differential Revision: https://phabricator.services.mozilla.com/D77064
    
    UltraBlame original commit: 17fc3a2a42b5d725e99c51de4b4dda8061415fab

--- media/libcubeb/src/cubeb_resampler_internal.h
+++ media/libcubeb/src/cubeb_resampler_internal.h
@@ -254,7 +254,7 @@ public:
     speex_resample(resampling_in_buffer.data(), &in_len,
                    resampling_out_buffer.data(), &out_len);
 
-    assert(out_len == output_frame_count);
+    
 
     
 
