1bcccf46f982322dbbf116c53bf2890108ad07fa
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-06-03 05:41:03 +0000

    Bug 1637708 - Convert the prefs in nsClassifierStreamUpdater to static prefs r=njn
    Converts `urlclassifier.update.timeout_ms` and `urlclassifier.update.response_timeout_ms` to static prefs and adds the `urlclassifier` header file.
    
    Differential Revision: https://phabricator.services.mozilla.com/D76373
    
    UltraBlame original commit: d1b0d2d65dad7d4d5c9b28f500737474c2c6ece8

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -4291,10 +4291,6 @@ pref("urlclassifier.gethashnoise", 4);
 
 pref("urlclassifier.gethash.timeout_ms", 5000);
 
-pref("urlclassifier.update.response_timeout_ms", 30000);
-
-pref("urlclassifier.update.timeout_ms", 90000);
-
 
 pref("urlclassifier.alternate_error_page", "blocked");
 
