459722f770b2af77c3fff916f75e57e38486a90f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-06-03 04:06:55 +0000

    Bug 1639486 - Fix shuffling of stack results in br_if / br_table r=lth
    Rather confusingly, sometimes stack height refers to a height with stack
    results, and sometimes a height without results.  We were confusing the
    two: the raw shuffleStackResultsTowardFP function that took uint32_t
    heights assumed the former, but the version that took StackHeight
    assumed the latter.  The result was a mis-shuffle that could be
    catastrophic.
    
    Differential Revision: https://phabricator.services.mozilla.com/D76136
    
    UltraBlame original commit: a1e751f0ab229fcf45353cc0cdeee8c8714544bf

--- js/src/wasm/WasmBaselineCompile.cpp
+++ js/src/wasm/WasmBaselineCompile.cpp
@@ -2088,6 +2088,7 @@ class BaseStackFrame final : public BaseStackFrameAllocator {
     popBytes(currentStackHeight() - end);
   }
 
+  
   void shuffleStackResultsTowardFP(uint32_t srcHeight, uint32_t destHeight,
                                    uint32_t bytes, Register temp) {
     MOZ_ASSERT(destHeight < srcHeight);
@@ -2110,6 +2111,8 @@ class BaseStackFrame final : public BaseStackFrameAllocator {
     }
   }
 
+  
+  
   void shuffleStackResultsTowardFP(StackHeight srcHeight,
                                    StackHeight destHeight, uint32_t bytes,
                                    Register temp) {
@@ -2119,9 +2122,10 @@ class BaseStackFrame final : public BaseStackFrameAllocator {
     uint32_t dest = computeHeightWithStackResults(destHeight, bytes);
     MOZ_ASSERT(src <= currentStackHeight());
     MOZ_ASSERT(dest <= currentStackHeight());
-    shuffleStackResultsTowardFP(src - bytes, dest - bytes, bytes, temp);
+    shuffleStackResultsTowardFP(src, dest, bytes, temp);
   }
 
+  
   void shuffleStackResultsTowardSP(uint32_t srcHeight, uint32_t destHeight,
                                    uint32_t bytes, Register temp) {
     MOZ_ASSERT(destHeight > srcHeight);
