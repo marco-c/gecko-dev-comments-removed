b9fd6847ed910cb525f16b4bbc334f7e45f4684f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-06-03 05:23:20 +0000

    Bug 1637727 - convert network.http.rcwn prefs to StaticPrefs. r=KrisWright,necko-reviewers,valentin
    converts:
     * network.http.rcwn.enabled
     * network.http.rcwn.cache_queue_normal_threshold
     * network.http.rcwn.cache_queue_priority_threshold
     * network.http.rcwn.small_resource_size_kb
     * network.http.rcwn.min_wait_before_racing_ms
     * network.http.rcwn.max_wait_before_racing_ms
    
    Differential Revision: https://phabricator.services.mozilla.com/D77108
    
    UltraBlame original commit: 3a6ad7dde82467989834748db5cd76990ac9809b

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -1512,17 +1512,6 @@ pref("network.http.enforce-framing.strict_chunked_encoding", true);
 pref("network.http.max_response_header_size", 393216);
 
 
-pref("network.http.rcwn.enabled", true);
-pref("network.http.rcwn.cache_queue_normal_threshold", 8);
-pref("network.http.rcwn.cache_queue_priority_threshold", 2);
-
-
-pref("network.http.rcwn.small_resource_size_kb", 256);
-
-pref("network.http.rcwn.min_wait_before_racing_ms", 0);
-pref("network.http.rcwn.max_wait_before_racing_ms", 500);
-
-
 
 pref("network.http.focused_window_transaction_ratio", "0.9");
 
