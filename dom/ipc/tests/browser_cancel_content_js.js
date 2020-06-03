beaa136b5666928ce83d1647628f070910251c3a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-06-03 05:28:01 +0000

    Bug 1548274 - try to make dom/ipc/tests/browser_cancel_content_js.js a bit less racy r=squib
    Differential Revision: https://phabricator.services.mozilla.com/D77445
    
    UltraBlame original commit: acc97286e3a2fdf8bdfa78d7a936fb5db01eac68

--- dom/ipc/tests/browser_cancel_content_js.js
+++ dom/ipc/tests/browser_cancel_content_js.js
@@ -11,14 +11,12 @@ const TEST_PAGE =
 const NEXT_PAGE = "https://example.org/";
 const JS_URI = "javascript:void(document.title = 'foo')";
 
-function sleep(ms) {
-  
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-
 async function test_navigation(nextPage, cancelContentJSPref, shouldCancel) {
   await SpecialPowers.pushPrefEnv({
-    set: [["dom.ipc.cancel_content_js_when_navigating", cancelContentJSPref]],
+    set: [
+      ["dom.ipc.cancel_content_js_when_navigating", cancelContentJSPref],
+      ["dom.max_script_run_time", 20],
+    ],
   });
   let tab = await BrowserTestUtils.openNewForegroundTab({
     gBrowser,
@@ -34,8 +32,9 @@ async function test_navigation(nextPage, cancelContentJSPref, shouldCancel) {
   });
 
   
-  
-  await sleep(1000);
+  await ContentTask.spawn(tab.linkedBrowser, [], function() {
+    content.dispatchEvent(new content.Event("StartLongLoop"));
+  });
 
   info(
     `navigating to ${nextPage} with cancel content JS ${
