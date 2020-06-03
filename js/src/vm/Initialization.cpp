f8e419d3db5c69028cfc7867995ad157af8246dd
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-06-03 04:13:08 +0000

    Bug 1639898 - Use volatile in CheckCanonicalNaN. r=iain
    This ensures we compute the NaN at runtime instead of compile time since this
    is this test is aimed at JIT compatibility. In practice, these should be the
    same, plus/minus the sign bit.
    
    Differential Revision: https://phabricator.services.mozilla.com/D76344
    
    UltraBlame original commit: 156314339700c9c4e60d7ee26ead6d6df54df1c8

--- js/src/vm/Initialization.cpp
+++ js/src/vm/Initialization.cpp
@@ -79,8 +79,8 @@ static void CheckCanonicalNaN() {
   
   
   
-  double infinity = mozilla::PositiveInfinity<double>();
-  double hardwareNaN = infinity - infinity;
+  volatile double infinity = mozilla::PositiveInfinity<double>();
+  volatile double hardwareNaN = infinity - infinity;
   uint64_t bits = mozilla::BitwiseCast<uint64_t>(hardwareNaN);
   bits &= ~mozilla::FloatingPoint<double>::kSignBit;
   MOZ_RELEASE_ASSERT(bits == JS::detail::CanonicalizedNaNBits,
