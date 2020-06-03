d61f89b881ac2af651104866faf1cdeaf24c8127
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-06-03 04:01:44 +0000

    Bug 1639247 - Isolate DNS cache per first-party when privacy.partition.network_state is set to true - part 3 - implementation, r=valentin,necko-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D75985
    
    UltraBlame original commit: ab42c2dd3b435e2f8fe4d367963cbb90c6b21e95

--- toolkit/components/antitracking/StoragePrincipalHelper.h
+++ toolkit/components/antitracking/StoragePrincipalHelper.h
@@ -250,6 +250,13 @@ class StoragePrincipalHelper final {
 
   static bool GetRegularPrincipalOriginAttributes(
       nsILoadGroup* aLoadGroup, OriginAttributes& aAttributes);
+
+  
+  
+  static bool GetOriginAttributesForNetworkState(nsIChannel* aChanel,
+                                                 OriginAttributes& aAttributes);
+  static void GetOriginAttributesForNetworkState(dom::Document* aDocument,
+                                                 OriginAttributes& aAttributes);
 };
 
 }  
