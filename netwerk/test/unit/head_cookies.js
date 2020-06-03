7a80c2e28276af8c67c6e79647739b55cc414639
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-06-03 03:23:25 +0000

    Bug 1636783 - Ensure default profile permissions do not confuse permission manager tests (test_permmgr.js). r=mayhemer,necko-reviewers
    This is a problem for Thunderbird due to bug 1168178.
    
    Differential Revision: https://phabricator.services.mozilla.com/D75031
    
    UltraBlame original commit: 0085a24d396a9b89265473c5a3f691f4ae71430e

--- netwerk/test/unit/head_cookies.js
+++ netwerk/test/unit/head_cookies.js
@@ -7,10 +7,12 @@
 "use strict";
 
 const { NetUtil } = ChromeUtils.import("resource://gre/modules/NetUtil.jsm");
-
 const { CookieXPCShellUtils } = ChromeUtils.import(
   "resource://testing-common/CookieXPCShellUtils.jsm"
 );
+
+
+Services.prefs.setCharPref("permissions.manager.defaultsUrl", "");
 
 CookieXPCShellUtils.init(this);
 
