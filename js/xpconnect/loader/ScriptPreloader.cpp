accb9735aa39b5bc7b6ad10b9b6160bfa14ca449
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-06-11 23:45:26 +0000

    Backed out changeset 91d9671c44b5 (bug 1640445) as requested. CLOSED TREE
    UltraBlame original commit: 22e9e058d1dbd6a24e485c798455b07c1d9ae349

--- js/xpconnect/loader/ScriptPreloader.cpp
+++ js/xpconnect/loader/ScriptPreloader.cpp
@@ -206,9 +206,6 @@ static void TraceOp(JSTracer* trc, void* data) {
 }  
 
 void ScriptPreloader::Trace(JSTracer* trc) {
-  mMonitor.AssertNotCurrentThreadOwns();
-  MonitorAutoLock mal(mMonitor);
-
   for (auto& script : IterHash(mScripts)) {
     JS::TraceEdge(trc, &script->mScript,
                   "ScriptPreloader::CachedScript.mScript");
