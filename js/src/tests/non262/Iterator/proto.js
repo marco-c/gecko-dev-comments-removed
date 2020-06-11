0f16afa9033e2c7355d136cddb41b564a15303ca
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-06-11 23:52:14 +0000

    Bug 1644581 - Remove Iterator.prototype[toStringTag]. r=jorendorff
    Differential Revision: https://phabricator.services.mozilla.com/D78983
    
    UltraBlame original commit: 4a3b83f31a8821f5b4cb2932d07acc036be8a277

--- js/src/tests/non262/Iterator/proto.js
+++ js/src/tests/non262/Iterator/proto.js
@@ -10,5 +10,8 @@ assertEq(propDesc.writable, false);
 assertEq(propDesc.enumerable, false);
 assertEq(propDesc.configurable, false);
 
+
+assertEq(Symbol.toStringTag in propDesc.value, false);
+
 if (typeof reportCompare === 'function')
   reportCompare(0, 0);
