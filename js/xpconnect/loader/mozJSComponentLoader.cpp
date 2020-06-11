e1bb395bff8ba81062058b9470c08f84e18a1671
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-06-11 23:09:57 +0000

    Bug 1642844 - Remove mozJSComponentLoader::ReuseGlobal. r=mccr8
    Differential Revision: https://phabricator.services.mozilla.com/D78029
    
    UltraBlame original commit: 7c627bd316f2d52f4100ee47ea60d26a368b59c0

--- js/xpconnect/loader/mozJSComponentLoader.cpp
+++ js/xpconnect/loader/mozJSComponentLoader.cpp
@@ -598,29 +598,6 @@ void mozJSComponentLoader::CreateLoaderGlobal(JSContext* aCx,
   aGlobal.set(global);
 }
 
-bool mozJSComponentLoader::ReuseGlobal(nsIURI* aURI) {
-  if (!mShareLoaderGlobal) {
-    return false;
-  }
-
-  nsCString spec;
-  NS_ENSURE_SUCCESS(aURI->GetSpec(spec), false);
-
-  
-  
-  if (spec.EqualsASCII("resource://gre/modules/commonjs/toolkit/loader.js")) {
-    return false;
-  }
-
-  
-  
-  if (spec.EqualsASCII("resource://gre/modules/jsdebugger.jsm")) {
-    return false;
-  }
-
-  return true;
-}
-
 JSObject* mozJSComponentLoader::GetSharedGlobal(JSContext* aCx) {
   if (!mLoaderGlobal) {
     JS::RootedObject globalObj(aCx);
@@ -647,7 +624,7 @@ JSObject* mozJSComponentLoader::PrepareObjectForLocation(
   nsAutoCString nativePath;
   NS_ENSURE_SUCCESS(aURI->GetSpec(nativePath), nullptr);
 
-  bool reuseGlobal = ReuseGlobal(aURI);
+  bool reuseGlobal = mShareLoaderGlobal;
 
   *aReuseGlobal = reuseGlobal;
 
