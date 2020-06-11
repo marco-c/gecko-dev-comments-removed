e5ebf2b36030b76f3c1e62e1b4686e79facfb496
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-06-11 23:43:04 +0000

    Backed out 2 changesets (bug 1640105) for perma failures on remoteautomation.py. CLOSED TREE
    Backed out changeset e12bb339406c (bug 1640105)
    Backed out changeset f66cacb5c833 (bug 1640105)
    
    UltraBlame original commit: f9309aeb145269ae817ab26e280ff3583e60deed

--- netwerk/ipc/SocketProcessChild.cpp
+++ netwerk/ipc/SocketProcessChild.cpp
@@ -123,26 +123,6 @@ bool SocketProcessChild::Init(base::ProcessId aParentPid,
   
   CGSShutdownServerConnections();
 #endif  
-
-  nsresult rv;
-  nsCOMPtr<nsIIOService> ios = do_GetIOService(&rv);
-  if (NS_FAILED(rv)) {
-    return false;
-  }
-
-  nsCOMPtr<nsIProtocolHandler> handler;
-  rv = ios->GetProtocolHandler("http", getter_AddRefs(handler));
-  if (NS_FAILED(rv)) {
-    return false;
-  }
-
-  
-  nsCOMPtr<nsIDNSService> dns =
-      do_GetService("@mozilla.org/network/dns-service;1", &rv);
-  if (NS_FAILED(rv)) {
-    return false;
-  }
-
   return true;
 }
 
@@ -333,8 +313,31 @@ PFileDescriptorSetChild* SocketProcessChild::SendPFileDescriptorSetConstructor(
 already_AddRefed<PHttpConnectionMgrChild>
 SocketProcessChild::AllocPHttpConnectionMgrChild() {
   LOG(("SocketProcessChild::AllocPHttpConnectionMgrChild \n"));
-  RefPtr<HttpConnectionMgrChild> actor = new HttpConnectionMgrChild();
-  return actor.forget();
+  if (!gHttpHandler) {
+    nsresult rv;
+    nsCOMPtr<nsIIOService> ios = do_GetIOService(&rv);
+    if (NS_FAILED(rv)) {
+      return nullptr;
+    }
+
+    nsCOMPtr<nsIProtocolHandler> handler;
+    rv = ios->GetProtocolHandler("http", getter_AddRefs(handler));
+    if (NS_FAILED(rv)) {
+      return nullptr;
+    }
+
+    
+    nsCOMPtr<nsIDNSService> dns =
+        do_GetService("@mozilla.org/network/dns-service;1", &rv);
+    if (NS_FAILED(rv)) {
+      return nullptr;
+    }
+
+    RefPtr<HttpConnectionMgrChild> actor = new HttpConnectionMgrChild();
+    return actor.forget();
+  }
+
+  return nullptr;
 }
 
 mozilla::ipc::IPCResult
