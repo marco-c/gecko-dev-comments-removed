1fd88f093eadd4dc261d7c3a2cfc51293ff972c5
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-06-11 23:29:48 +0000

    Bug 1643171: Add memory reporting to irregexp::Isolate r=mccr8
    A couple of notes:
    
    1. In the previous patch in this stack, I significantly reduced the default size of handleArena and uniquePtrArena (from 4K each down to 128 bytes). We are using SegmentedVector because we need to grow without moving the contents, not because we expect to grow frequently.
    
    2. I'm reporting the owned memory of the RegExpStack if it has any, but the owned memory is pretty transient. RegExp execution is wrapped in a RegExpStackScope, which should free the owned memory when we're done executing. (That might not always be the case when executing RegExps directly from jitcode, though.)
    
    Differential Revision: https://phabricator.services.mozilla.com/D78521
    
    UltraBlame original commit: d34b4f37cf8a401aeb99d191fd696688c7bcc717

--- js/src/vm/JSContext.cpp
+++ js/src/vm/JSContext.cpp
@@ -1135,7 +1135,8 @@ size_t JSContext::sizeOfExcludingThis(
 
 
 
-  return cycleDetectorVector().sizeOfExcludingThis(mallocSizeOf);
+  return cycleDetectorVector().sizeOfExcludingThis(mallocSizeOf) +
+         irregexp::IsolateSizeOfIncludingThis(isolate, mallocSizeOf);
 }
 
 #ifdef DEBUG
