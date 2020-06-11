485ed0a0d8d69f58fc595bce4085c38f95bc5ad6
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-06-11 23:51:10 +0000

    Bug 1643289 - Move elements where possible in nsTPriorityQueue instead of copying them. r=froydnj
    Also remove redundant check in Push method on result of infallible operation,
    and change its return type to void.
    
    Differential Revision: https://phabricator.services.mozilla.com/D78249
    
    UltraBlame original commit: f8faae6114e00d12dcfbb3ed07b6fbd89c400944

--- dom/media/platforms/apple/AppleVTDecoder.cpp
+++ dom/media/platforms/apple/AppleVTDecoder.cpp
@@ -446,7 +446,7 @@ void AppleVTDecoder::OutputFrame(CVPixelBufferRef aImage,
   
   
   MonitorAutoLock mon(mMonitor);
-  mReorderQueue.Push(data);
+  mReorderQueue.Push(std::move(data));
   MaybeResolveBufferedFrames();
 
   LOG("%llu decoded frames queued",
