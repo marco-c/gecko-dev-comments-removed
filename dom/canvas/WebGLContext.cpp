f4a8c8a4b9cf392f3494f32e6d40d10953251760
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-06-11 23:07:10 +0000

    Bug 1640912 - Update xrCompatible flag when WebGLContext::makeXRCompatible call succeeds r=jgilbert,kip,daoshengmu
    Currently WebGL xrCompatible flag is only set when enabling the xrCompatible attribute in the canvas.getContext() call. We also need to set it to true after succesful makeXRCompatible() calls
    
    Differential Revision: https://phabricator.services.mozilla.com/D76849
    
    UltraBlame original commit: 7e89a7274770f7db5cb0691fe8e064ef29ce8cb2

--- dom/canvas/WebGLContext.cpp
+++ dom/canvas/WebGLContext.cpp
@@ -1787,11 +1787,7 @@ WebGLContext::FuncScope::~FuncScope() {
 
 
 
-bool ClientWebGLContext::IsXRCompatible() const {
-  if (!mNotLost) return false;
-  const auto& options = mNotLost->info.options;
-  return options.xrCompatible;
-}
+bool ClientWebGLContext::IsXRCompatible() const { return mXRCompatible; }
 
 already_AddRefed<dom::Promise> ClientWebGLContext::MakeXRCompatible(
     ErrorResult& aRv) {
@@ -1823,7 +1819,7 @@ already_AddRefed<dom::Promise> ClientWebGLContext::MakeXRCompatible(
   
   
   
-
+  mXRCompatible = true;
   promise->MaybeResolveWithUndefined();
   return promise.forget();
 }
