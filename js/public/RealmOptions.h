1d5d670d2b33882b2a59a67cd18da74392ae85ac
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-06-11 23:06:39 +0000

    Bug 1635839 - Part 4: Add an option for private class fields, disabled by default r=jorendorff
    Differential Revision: https://phabricator.services.mozilla.com/D74105
    
    UltraBlame original commit: 851ee4729a31745d4845a584750f8858fcc0a455

--- js/public/RealmOptions.h
+++ js/public/RealmOptions.h
@@ -234,6 +234,12 @@ class JS_PUBLIC_API RealmCreationOptions {
     return *this;
   }
 
+  bool getPrivateClassFieldsEnabled() const { return privateClassFields_; }
+  RealmCreationOptions& setPrivateClassFieldsEnabled(bool flag) {
+    privateClassFields_ = flag;
+    return *this;
+  }
+
   
   
   
@@ -274,6 +280,7 @@ class JS_PUBLIC_API RealmCreationOptions {
   bool toSource_ = false;
   bool propertyErrorMessageFix_ = false;
   bool iteratorHelpers_ = false;
+  bool privateClassFields_ = false;
   bool secureContext_ = false;
 };
 
