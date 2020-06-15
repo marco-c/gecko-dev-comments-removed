c77866489544e6d64e4d0333e998708a514efa1f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-06-15 22:59:17 +0000

    Bug 1644694 - Do not call mozilla::dom::FallbackEncoding::FromLocale() in nsIndexedToHTML.cpp. r=valentin
    Differential Revision: https://phabricator.services.mozilla.com/D79100
    
    UltraBlame original commit: a644e527a77f216cff6e266e1311bd1d925d1dcb

--- netwerk/streamconv/converters/nsIndexedToHTML.cpp
+++ netwerk/streamconv/converters/nsIndexedToHTML.cpp
@@ -505,17 +505,9 @@ nsresult nsIndexedToHTML::DoOnStartRequest(nsIRequest* request,
   nsAutoString unEscapeSpec;
   rv = mTextToSubURI->UnEscapeAndConvert(NS_LITERAL_CSTRING("UTF-8"), titleUri,
                                          unEscapeSpec);
-  
-  
-  
-  
-  if (NS_FAILED(rv) && uri->SchemeIs("file") && !NS_IsNativeUTF8()) {
-    auto encoding = mozilla::dom::FallbackEncoding::FromLocale();
-    nsAutoCString charset;
-    encoding->Name(charset);
-    rv = mTextToSubURI->UnEscapeAndConvert(charset, titleUri, unEscapeSpec);
+  if (NS_FAILED(rv)) {
+    return rv;
   }
-  if (NS_FAILED(rv)) return rv;
 
   nsCString htmlEscSpecUtf8;
   nsAppendEscapedHTML(NS_ConvertUTF16toUTF8(unEscapeSpec), htmlEscSpecUtf8);
