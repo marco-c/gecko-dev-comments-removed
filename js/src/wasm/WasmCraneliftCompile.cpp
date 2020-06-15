41a4e48453cc52484fc5898b1f03426117029fa1
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-06-15 23:09:18 +0000

    Bug 1645336: Cranelift: don't paste function's body if there's none; r=cfallin
    This solves a remaining failure in wasm/spec/spec/left-to-right.wast.js, where
    the whole function's body is optimized away.
    
    Differential Revision: https://phabricator.services.mozilla.com/D79439
    
    UltraBlame original commit: 4b8bb44f41c18fc9eb546967044d0c8de3850152

--- js/src/wasm/WasmCraneliftCompile.cpp
+++ js/src/wasm/WasmCraneliftCompile.cpp
@@ -152,7 +152,7 @@ static bool GenerateCraneliftCode(WasmMacroAssembler& masm,
 
   
   uint32_t funcBase = masm.currentOffset();
-  if (!masm.appendRawCode(func.code, func.code_size)) {
+  if (func.code_size && !masm.appendRawCode(func.code, func.code_size)) {
     return false;
   }
 #if defined(JS_CODEGEN_X64) || defined(JS_CODEGEN_X86)
