910cf080d0f164b2cfa6a350bbc3783b67fbe9bd
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-06-15 23:03:18 +0000

    Bug 1643492 [wpt PR 24000] - Revert "[WebSocket] Test header + close frame in same packet", a=testonly
    Automatic update from web-platform-tests
    Revert "[WebSocket] Test header + close frame in same packet"
    
    This reverts commit 3be4a6e9fbba0ce3e593d856a4b3d98f977aa279.
    
    Reason for revert: Updated test is flaking on all platforms. crbug.com/1091377
    
    Original change's description:
    > [WebSocket] Test header + close frame in same packet
    >
    > Modify the existing web platform test opening-handshake/005.html to send
    > the response header and the close frame in the same packet. This
    > verifies that Chrome passes the close data to JavaScript correctly in
    > this case.
    >
    > This increases the coverage of the existing test, so there's no need to
    > retain the old behaviour.
    >
    > Also remove the timeout expectations for this test, and make sure it
    > doesn't timeout on failure.
    >
    > Closed: 1090210
    > Change-Id: I327c55d1517615511d6d71e1b464d5d3dc03b502
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2228252
    > Commit-Queue: Adam Rice <riceachromium.org>
    > Reviewed-by: Yutaka Hirano <yhiranochromium.org>
    > Cr-Commit-Position: refs/heads/master{#775124}
    
    TBR=riceachromium.org,yhiranochromium.org
    
    Change-Id: I729663e5d0f7f6e41304b67c135b9af8946e503e
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2231268
    Reviewed-by: Jonathan Ross <jonrosschromium.org>
    Commit-Queue: Jonathan Ross <jonrosschromium.org>
    Cr-Commit-Position: refs/heads/master{#775233}
    
    --
    
    wpt-commits: 7ab56937c9dbe1513534603354b1976a9e4d7f87
    wpt-pr: 24000
    
    UltraBlame original commit: d12e2d5ea882f00fe77dc6f453cde6f6ada70bca

--- testing/web-platform/tests/websockets/handlers/simple_handshake_wsh.py
+++ testing/web-platform/tests/websockets/handlers/simple_handshake_wsh.py
@@ -14,11 +14,10 @@ def web_socket_do_extra_handshake(request):
            b'Sec-WebSocket-Origin: %s\x0D\x0A'
            b'Sec-WebSocket-Accept: %s\x0D\x0A\x0D\x0A') % (request.ws_origin.encode(
                'UTF-8'), hybi.compute_accept_from_unicode(request.headers_in.get(common.SEC_WEBSOCKET_KEY_HEADER)))
+    request.connection.write(msg)
     
-    close_body = stream.create_closing_handshake_body(1001, 'PASS')
-    close_frame = stream.create_close_frame(close_body)
-    
-    request.connection.write(msg + close_frame)
+    body = stream.create_closing_handshake_body(1000, '')
+    request.connection.write(stream.create_close_frame(body))
     
     
     
