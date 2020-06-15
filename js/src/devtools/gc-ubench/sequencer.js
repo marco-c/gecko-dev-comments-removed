4c8a06bf790c44f3461b52eed47afb460d24dcda
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-06-15 23:08:31 +0000

    Bug 1633625 - Implement a Find50Sequencer that searches for the garbagePerFrame resulting in 50% frame drop at 60fps r=jonco
    Differential Revision: https://phabricator.services.mozilla.com/D75444
    
    UltraBlame original commit: 9920987a04bcaa4f2a4ede2770d2c744804ff386

--- js/src/devtools/gc-ubench/sequencer.js
+++ js/src/devtools/gc-ubench/sequencer.js
@@ -20,16 +20,14 @@ var Sequencer = class {
 
   
   
+  
+  
   tick(now = gHost.now()) {
     if (this.done()) {
       throw new Error("tick() called on completed sequencer");
     }
 
-    const advanced = this.do_tick(now);
-    if (advanced) {
-      this.started = now;
-    }
-    return advanced;
+    return this.do_tick(now);
   }
 
   
@@ -43,11 +41,17 @@ var Sequencer = class {
     throw new Error("unimplemented");
   }
 
+  restart(now = gHost.now()) {
+    this.reset();
+    this.start(now);
+  }
+
   
   currentLoadElapsed(now = gHost.now()) {
     return now - this.started;
   }
 };
+
 
 var SingleMutatorSequencer = class extends Sequencer {
   constructor(mutator, perf, duration_sec) {
@@ -59,6 +63,7 @@ var SingleMutatorSequencer = class extends Sequencer {
     }
     this.duration = duration_sec * 1000;
     this.state = 'init'; 
+    this.lastResult = undefined;
   }
 
   get current() {
@@ -83,15 +88,16 @@ var SingleMutatorSequencer = class extends Sequencer {
       return false;
     }
 
-    this.perf.on_load_end(this.current, now);
+    const load = this.current;
     this.state = 'done';
-    return true;
+    return this.perf.on_load_end(load, now);
   }
 
   done() {
     return this.state === 'done';
   }
 };
+
 
 var ChainSequencer = class extends Sequencer {
   constructor(sequencers) {
@@ -124,8 +130,14 @@ var ChainSequencer = class extends Sequencer {
 
   do_tick(now) {
     const sequencer = this.sequencers[this.idx];
-    if (!sequencer.do_tick(now)) {
-      return false;
+    const trial_result = sequencer.do_tick(now);
+    if (!trial_result) {
+      return false; 
+    }
+
+    if (!sequencer.done()) {
+      
+      return trial_result;
     }
 
     this.idx++;
@@ -136,10 +148,151 @@ var ChainSequencer = class extends Sequencer {
       this.state = 'done';
     }
 
-    return true;
+    return trial_result;
   }
 
   done() {
     return this.state === 'done';
+  }
+};
+
+var RunUntilSequencer = class extends Sequencer {
+  constructor(sequencer, loadMgr) {
+    super();
+    this.loadMgr = loadMgr;
+    this.sequencer = sequencer;
+
+    
+    this.state = sequencer.done() ? 'done' : 'init';
+  }
+
+  get current() {
+    return this.sequencer?.current;
+  }
+
+  reset() {
+    this.sequencer.reset();
+    this.state = 'init';
+  }
+
+  start(now) {
+    super.start(now);
+    this.sequencer.start(now);
+    this.initSearch(now);
+    this.state = 'running';
+  }
+
+  initSearch(now) {}
+
+  done() {
+    return this.state === 'done';
+  }
+
+  do_tick(now) {
+    const trial_result = this.sequencer.do_tick(now);
+    if (trial_result) {
+      if (this.searchComplete(trial_result)) {
+        this.state = 'done';
+      } else {
+        this.sequencer.restart(now);
+      }
+    }
+    return trial_result;
+  }
+
+  
+  
+  
+  searchComplete(result) {
+    throw new Error("must implement in subclass");
+  }
+};
+
+
+var Find50Sequencer = class extends RunUntilSequencer {
+  constructor(sequencer, loadMgr, goal=0.5, low_range=0.45, high_range=0.55) {
+    super(sequencer, loadMgr);
+
+    
+    
+    
+    this.low_range = low_range;
+    this.goal = goal;
+    this.high_range = high_range;
+    this.persistence = 3;
+
+    this.clear();
+  }
+
+  reset() {
+    super.reset();
+    this.clear();
+  }
+
+  clear() {
+    this.garbagePerFrame = undefined;
+
+    this.good = undefined;
+    this.goodAt = undefined;
+    this.bad = undefined;
+    this.badAt = undefined;
+
+    this.numInRange = 0;
+  }
+
+  start(now) {
+    super.start(now);
+    if (!this.done()) {
+      this.garbagePerFrame = this.sequencer.current.garbagePerFrame;
+    }
+  }
+
+  searchComplete(result) {
+    print(
+      `Saw ${percent(result.dropped_60fps_fraction)} with garbagePerFrame=${this.garbagePerFrame}`
+    );
+
+    
+    
+    if (result.dropped_60fps_fraction < this.goal) {
+      if (this.goodAt === undefined || this.goodAt < this.garbagePerFrame) {
+        this.goodAt = this.garbagePerFrame;
+        this.good = result.dropped_60fps_fraction;
+      }
+      if (this.badAt !== undefined) {
+        this.garbagePerFrame = Math.trunc(
+          (this.garbagePerFrame + this.badAt) / 2
+        );
+      } else {
+        this.garbagePerFrame *= 2;
+      }
+    } else {
+      if (this.badAt === undefined || this.badAt > this.garbagePerFrame) {
+        this.badAt = this.garbagePerFrame;
+        this.bad = result.dropped_60fps_fraction;
+      }
+      if (this.goodAt !== undefined) {
+        this.garbagePerFrame = Math.trunc(
+          (this.garbagePerFrame + this.goodAt) / 2
+        );
+      } else {
+        this.garbagePerFrame = Math.trunc(this.garbagePerFrame / 2);
+      }
+    }
+
+    if (
+      this.low_range < result.dropped_60fps_fraction &&
+      result.dropped_60fps_fraction < this.high_range
+    ) {
+      this.numInRange++;
+      if (this.numInRange >= this.persistence) {
+        return true;
+      }
+    }
+
+    print(`next run with ${this.garbagePerFrame}`);
+    this.loadMgr.change_garbagePerFrame(this.garbagePerFrame);
+
+    return false;
   }
 };
