2311f12ae511e166e1ec30c8005dec24c51cba10
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-06-15 22:59:06 +0000

    Bug 1636251: Patch Sentry events to ensure a raw username isn't sent to Sentry r=rstewart
    To avoid sending identifying information, common absolute paths are patched with placeholder values. For example, devs
    may place their Firefox repository within their home dir, so absolute paths are doctored to be prefixed with
    "<topsrcdir"> instead.
    
    Additionally, any paths including the user's home directory are patched to instead be a relate path from "~".
    
    Differential Revision: https://phabricator.services.mozilla.com/D78962
    
    UltraBlame original commit: ce8cb6373f889d42951674de13411ffb601098fe

--- python/mach/mach/sentry.py
+++ python/mach/mach/sentry.py
@@ -6,18 +6,20 @@ from __future__ import absolute_import
 
 import os
 import re
+import sys
+from os.path import expanduser
 
+import mozpack.path as mozpath
 import sentry_sdk
-from six.moves.configparser import SafeConfigParser, NoOptionError
-
 from mozboot.util import get_state_dir
-
+from six import string_types
+from six.moves.configparser import SafeConfigParser, NoOptionError
 
 
 _SENTRY_DSN = "https://8228c9aff64949c2ba4a2154dc515f55@sentry.prod.mozaws.net/525"
 
 
-def register_sentry():
+def register_sentry(topsrcdir):
     cfg_file = os.path.join(get_state_dir(), 'machrc')
     config = SafeConfigParser()
 
@@ -32,10 +34,17 @@ def register_sentry():
     if not telemetry_enabled:
         return
 
-    sentry_sdk.init(_SENTRY_DSN, before_send=_settle_mach_module_id)
+    sentry_sdk.init(_SENTRY_DSN,
+                    before_send=lambda event, _: _process_event(event, topsrcdir))
 
 
-def _settle_mach_module_id(sentry_event, exception):
+def _process_event(sentry_event, topsrcdir):
+    for map_fn in (_settle_mach_module_id, _patch_absolute_paths):
+        sentry_event = map_fn(sentry_event, topsrcdir)
+    return sentry_event
+
+
+def _settle_mach_module_id(sentry_event, _):
     
     
     
@@ -51,6 +60,50 @@ def _settle_mach_module_id(sentry_event, exception):
         module = re.sub("mach\\.commands\\.[a-f0-9]{32}", "mach.commands.<generated>",
                         module)
         frame["module"] = module
+    return sentry_event
+
+
+def _resolve_topobjdir():
+    topobjdir = os.path.join(os.path.dirname(sys.prefix), "..")
+    return mozpath.normsep(os.path.normpath(topobjdir))
+
+
+def _patch_absolute_paths(sentry_event, topsrcdir):
+    
+    
+    
+    
+    
+    
+    
+    
+    def recursive_patch(value, needle, replacement):
+        if isinstance(value, list):
+            return [recursive_patch(v, needle, replacement) for v in value]
+        elif isinstance(value, dict):
+            for key in list(value.keys()):
+                next_value = value.pop(key)
+                key = key.replace(needle, replacement)
+                value[key] = recursive_patch(next_value, needle, replacement)
+            return value
+        elif isinstance(value, string_types):
+            return value.replace(needle, replacement)
+        else:
+            return value
+
+    for (needle, replacement) in (
+            (get_state_dir(), "<statedir>"),
+            (_resolve_topobjdir(), "<topobjdir>"),
+            (topsrcdir, "<topsrcdir>"),
+            (expanduser("~"), "~"),
+            
+            
+            
+            
+            
+            (repr(expanduser("~"))[1:-1], "~"),
+    ):
+        sentry_event = recursive_patch(sentry_event, needle, replacement)
     return sentry_event
 
 
