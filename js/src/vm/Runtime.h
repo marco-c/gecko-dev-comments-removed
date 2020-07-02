d07a0a6b58d6d1c61b758780249333e550c46c01
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-07-02 12:28:04 +0000

    Bug 1645845 - Add ParserAtomsTable, parser atoms types, common parser names table, and base parser atoms implementation. r=mgaudet,tcampbell
    Differential Revision: https://phabricator.services.mozilla.com/D79714
    
    UltraBlame original commit: 434d959d73dbb95f3667cecb13aa3f3e73d4868b

--- js/src/vm/Runtime.h
+++ js/src/vm/Runtime.h
@@ -109,6 +109,10 @@ class Simulator;
 #endif
 }  
 
+namespace frontend {
+class WellKnownParserAtoms;
+}  
+
 
 
 
@@ -742,7 +746,9 @@ struct JSRuntime {
 
  public:
   bool initializeAtoms(JSContext* cx);
+  bool initializeParserAtoms(JSContext* cx);
   void finishAtoms();
+  void finishParserAtoms();
   bool atomsAreFinished() const {
     return !atoms_ && !permanentAtomsDuringInit_;
   }
@@ -783,6 +789,7 @@ struct JSRuntime {
 
   
   js::WriteOnceData<JSAtomState*> commonNames;
+  js::WriteOnceData<js::frontend::WellKnownParserAtoms*> commonParserNames;
 
   
   
