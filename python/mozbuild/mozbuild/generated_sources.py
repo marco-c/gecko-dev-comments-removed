e8bbffeae8b1de32d1f6a5eb89bbe39438da9670
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-07-02 13:25:21 +0000

    Bug 1645986 - Properly mark generated files required during compile as such. r=firefox-build-system-reviewers,rstewart
    Ideally, this kind of manual work wouldn't be required, but the frontend
    doesn't provide enough information and while backend could correlate the
    information, they currently don't.
    
    Differential Revision: https://phabricator.services.mozilla.com/D80606
    
    UltraBlame original commit: f0214b179f6979bd2028820850d1473627e9b2ba

--- python/mozbuild/mozbuild/generated_sources.py
+++ python/mozbuild/mozbuild/generated_sources.py
@@ -42,7 +42,9 @@ def get_generated_sources():
     with open(gen_sources, 'r') as f:
         data = json.load(f)
     for f in data['sources']:
-        yield f, mozpath.join(buildconfig.topobjdir, f)
+        
+        if mozpath.basename(f) != 'symverscript':
+            yield f, mozpath.join(buildconfig.topobjdir, f)
     
     base = 'ipc/ipdl/_ipdlheaders'
     finder = FileFinder(mozpath.join(buildconfig.topobjdir, base))
