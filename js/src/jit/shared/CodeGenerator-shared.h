dade52056eccbb6992a5a9c7ba552a9b370add36
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-07-02 13:42:19 +0000

    Bug 1648005 part 1 - Use Maybe<> for osrEntryOffset_ and add a release-assert. r=iain
    We now crash instead of silently using an offset of 0 (meaning start of the
    IonScript's JIT code) for the OSR entry point if the MOsrEntry is missing
    for some reason.
    
    Differential Revision: https://phabricator.services.mozilla.com/D80874
    
    UltraBlame original commit: 9a4078dbbe411834eacbd6f06c1d3fd68aa51a87

--- js/src/jit/shared/CodeGenerator-shared.h
+++ js/src/jit/shared/CodeGenerator-shared.h
@@ -116,15 +116,16 @@ class CodeGeneratorShared : public LElementVisitor {
  protected:
   
   
-  size_t osrEntryOffset_;
+  mozilla::Maybe<size_t> osrEntryOffset_ = {};
 
   TempAllocator& alloc() const { return graph.mir().alloc(); }
 
-  inline void setOsrEntryOffset(size_t offset) {
-    MOZ_ASSERT(osrEntryOffset_ == 0);
-    osrEntryOffset_ = offset;
+  void setOsrEntryOffset(size_t offset) { osrEntryOffset_.emplace(offset); }
+
+  size_t getOsrEntryOffset() const {
+    MOZ_RELEASE_ASSERT(osrEntryOffset_.isSome());
+    return *osrEntryOffset_;
   }
-  inline size_t getOsrEntryOffset() const { return osrEntryOffset_; }
 
   
   
