44c98aec0b568921c13fc438c2f0a97974d844a5
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-07-02 12:27:44 +0000

    Bug 1645818 - Add check for CacheIR or fallback before spewing health report. r=iain
    Differential Revision: https://phabricator.services.mozilla.com/D80024
    
    UltraBlame original commit: 0962ac03e8282fa78501a0658b260bff0cbb96c0

--- js/src/jit/CacheIRHealth.cpp
+++ js/src/jit/CacheIRHealth.cpp
@@ -1,8 +1,8 @@
-/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*-
- * vim: set ts=8 sts=2 et sw=2 tw=80:
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+
+
+
+
 #ifdef JS_CACHEIR_SPEW
 
 #  include "jit/CacheIRHealth.h"
@@ -138,7 +138,8 @@ bool CacheIRHealth::rateMyCacheIR(HandleScript script) {
         return false;
       }
 
-      if (entry) {
+      if (entry && (entry->firstStub()->isFallback() ||
+                    ICStub::IsCacheIRKind(entry->firstStub()->kind()))) {
         spewHealthForStubsInCacheIREntry(entry);
       }
 
@@ -150,4 +151,4 @@ bool CacheIRHealth::rateMyCacheIR(HandleScript script) {
   return true;
 }
 
-#endif /* JS_CACHEIR_SPEW */
+#endif 
