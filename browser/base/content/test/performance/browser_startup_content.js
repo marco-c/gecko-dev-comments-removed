4b13e7391955a7ac7af4f4f312afcbedb8bf8a5b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-07-02 13:25:03 +0000

    Bug 1645862: Part 1 - Split TelemetryController into parent and content modules. r=chutten
    Most of TelemetryController is only used in the parent process, but is
    currently loaded in all processes, where it needlessly consumes tens of KB of
    memory. This patch splits the parent- and content-specific parts, and only
    loads each in the processes where they're needed.
    
    Differential Revision: https://phabricator.services.mozilla.com/D79870
    
    UltraBlame original commit: b4643e4db774a45661a3994eeb90c8d6300154ee

--- browser/base/content/test/performance/browser_startup_content.js
+++ browser/base/content/test/performance/browser_startup_content.js
@@ -25,9 +25,7 @@ const whitelist = {
 
     
     "resource://gre/modules/AppConstants.jsm",
-    "resource://gre/modules/AsyncShutdown.jsm",
     "resource://gre/modules/DeferredTask.jsm",
-    "resource://gre/modules/PromiseUtils.jsm",
     "resource://gre/modules/Services.jsm", 
     "resource://gre/modules/Timer.jsm",
     "resource://gre/modules/XPCOMUtils.jsm",
@@ -50,6 +48,8 @@ const whitelist = {
 
     
     "resource://gre/modules/TelemetryController.jsm", 
+    "resource://gre/modules/TelemetryControllerBase.jsm", 
+    "resource://gre/modules/TelemetryControllerContent.jsm", 
     "resource://gre/modules/TelemetryUtils.jsm", 
 
     
