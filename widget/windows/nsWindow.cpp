99600f68930516643060023716c440bcaf9edb00
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-07-02 13:33:00 +0000

    Bug 1628948 - Dispatch a custom event for WM_NCLBUTTONDOWN in the draggable area. r=NeilDeakin
    Differential Revision: https://phabricator.services.mozilla.com/D77419
    
    UltraBlame original commit: b78a8d72ed9d8fd72e5ec084051b060164b0c63a

--- widget/windows/nsWindow.cpp
+++ widget/windows/nsWindow.cpp
@@ -4320,6 +4320,14 @@ void nsWindow::DispatchPluginSettingEvents() {
   }
 }
 
+void nsWindow::DispatchCustomEvent(const nsString& eventName) {
+  if (Document* doc = GetDocument()) {
+    if (nsPIDOMWindowOuter* win = doc->GetWindow()) {
+      win->DispatchCustomEvent(eventName, ChromeOnlyDispatch::eYes);
+    }
+  }
+}
+
 bool nsWindow::TouchEventShouldStartDrag(EventMessage aEventMessage,
                                          LayoutDeviceIntPoint aEventPoint) {
   
@@ -5952,6 +5960,17 @@ bool nsWindow::ProcessMessage(UINT msg, WPARAM& wParam, LPARAM& lParam,
                                   MouseButton::ePrimary, MOUSE_INPUT_SOURCE());
       DispatchPendingEvents();
       break;
+
+    case WM_NCLBUTTONDOWN: {
+      
+      
+      
+      
+      if (WithinDraggableRegion(GET_X_LPARAM(lParam), GET_Y_LPARAM(lParam))) {
+        DispatchCustomEvent(NS_LITERAL_STRING("draggableregionleftmousedown"));
+      }
+      break;
+    }
 
     case WM_APPCOMMAND: {
       MSG nativeMsg = WinUtils::InitMSG(msg, wParam, lParam, mWnd);
