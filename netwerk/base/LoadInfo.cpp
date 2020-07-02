7f62031d4b328d84fe4d67b5072a7719860cbf02
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-07-02 13:28:31 +0000

    Bug 1646476: Remove unnecessary GetInProcessScriptableParent usage in LoadInfo. r=nika
    Differential Revision: https://phabricator.services.mozilla.com/D80062
    
    UltraBlame original commit: c31ff94046794886e6b7b66fa2dac8c0f39f4b89

--- netwerk/base/LoadInfo.cpp
+++ netwerk/base/LoadInfo.cpp
@@ -445,7 +445,7 @@ LoadInfo::LoadInfo(nsPIDOMWindowOuter* aOuterWindow,
   
   
   if (aSecurityFlags != nsILoadInfo::SEC_ONLY_FOR_EXPLICIT_CONTENTSEC_CHECK) {
-    MOZ_ASSERT(aOuterWindow->GetInProcessScriptableParent() == aOuterWindow);
+    MOZ_ASSERT(aOuterWindow->GetBrowsingContext()->IsTop());
     MOZ_ASSERT(mTopOuterWindowID == FindTopOuterWindowID(aOuterWindow));
   }
 
