fad14e688541667425adcda109d3cfd987145b4b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-07-02 13:29:29 +0000

    Bug 1648304 - Record WebRender compositor status in telemetry. r=aosmond
    Differential Revision: https://phabricator.services.mozilla.com/D80994
    
    UltraBlame original commit: 97d53e781f77bfcadc28a95800e1e7a0e13aecd5

--- widget/GfxInfoBase.cpp
+++ widget/GfxInfoBase.cpp
@@ -1623,6 +1623,9 @@ void GfxInfoBase::DescribeFeatures(JSContext* aCx, JS::Handle<JSObject*> aObj) {
   gfx::FeatureState& webrender = gfxConfig::GetFeature(gfx::Feature::WEBRENDER);
   InitFeatureObject(aCx, aObj, "webrender", webrender, &obj);
 
+  gfx::FeatureState& wrCompositor = gfxConfig::GetFeature(gfx::Feature::WEBRENDER_COMPOSITOR);
+  InitFeatureObject(aCx, aObj, "wrCompositor", wrCompositor, &obj);
+
   
   gfx::FeatureState& advancedLayers =
       gfxConfig::GetFeature(gfx::Feature::ADVANCED_LAYERS);
