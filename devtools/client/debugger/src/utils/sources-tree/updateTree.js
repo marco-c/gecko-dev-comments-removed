5edcfc9e6b796f2905753b45d809ae6541807126
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-07-02 13:47:50 +0000

    Bug 1642397 - Part 3: Show query string in SourcesTree when files conflict. r=jlast,davidwalsh
    Differential Revision: https://phabricator.services.mozilla.com/D81268
    
    UltraBlame original commit: c00459529ec8f276eaccf4e72563b65a155b6ef6

--- devtools/client/debugger/src/utils/sources-tree/updateTree.js
+++ devtools/client/debugger/src/utils/sources-tree/updateTree.js
@@ -6,25 +6,45 @@
 
 import { addToTree } from "./addToTree";
 import { collapseTree } from "./collapseTree";
-import { createDirectoryNode, createParentMap } from "./utils";
-import { getDomain } from "./treeOrder";
+import {
+  createDirectoryNode,
+  createParentMap,
+  getPathParts,
+  isInvalidUrl,
+} from "./utils";
+import {
+  getDomain,
+  createTreeNodeMatcher,
+  findNodeInContents,
+} from "./treeOrder";
+
+import { getDisplayURL } from "./getURL";
 
 import type { SourcesMapByThread } from "../../reducers/types";
 import type { Thread, DisplaySource, URL } from "../../types";
-import type { TreeDirectory, TreeNode } from "./types";
+import type { TreeDirectory, TreeSource, TreeNode } from "./types";
 
-function getSourcesToAdd(newSources, prevSources): DisplaySource[] {
-  const sourcesToAdd = [];
+function getSourcesDiff(
+  newSources,
+  prevSources
+): {
+  toAdd: Array<DisplaySource>,
+  toUpdate: Array<[DisplaySource, DisplaySource]>,
+} {
+  const toAdd = [];
+  const toUpdate = [];
 
   for (const sourceId in newSources) {
     const newSource = newSources[sourceId];
     const prevSource = prevSources ? prevSources[sourceId] : null;
     if (!prevSource) {
-      sourcesToAdd.push(newSource);
+      toAdd.push(newSource);
+    } else if (prevSource.displayURL !== newSource.displayURL) {
+      toUpdate.push([prevSource, newSource]);
     }
   }
 
-  return sourcesToAdd;
+  return { toAdd, toUpdate };
 }
 
 type UpdateTreeParams = {
@@ -82,14 +102,25 @@ export function updateTree({
       continue;
     }
 
-    const sourcesToAdd = getSourcesToAdd(
+    const { toAdd, toUpdate } = getSourcesDiff(
       (Object.values(newSources[context]): any),
       prevSources[context] ? (Object.values(prevSources[context]): any) : null
     );
 
-    for (const source of sourcesToAdd) {
+    for (const source of toAdd) {
       shouldUpdate = true;
       addToTree(uncollapsedTree, source, debuggeeHost, thread.actor);
+    }
+
+    for (const [prevSource, newSource] of toUpdate) {
+      shouldUpdate = true;
+      updateInTree(
+        uncollapsedTree,
+        prevSource,
+        newSource,
+        debuggeeHost,
+        thread.actor
+      );
     }
   }
 
@@ -104,4 +135,128 @@ export function updateTree({
     sourceTree: newSourceTree,
     parentMap: createParentMap(newSourceTree),
   };
+}
+
+export function updateInTree(
+  tree: TreeDirectory,
+  prevSource: DisplaySource,
+  newSource: DisplaySource,
+  debuggeeHost: ?string,
+  thread: string
+): void {
+  const newUrl = getDisplayURL(newSource, debuggeeHost);
+  const prevUrl = getDisplayURL(prevSource, debuggeeHost);
+
+  const prevEntries = findEntries(
+    tree,
+    prevUrl,
+    prevSource,
+    thread,
+    debuggeeHost
+  );
+  if (!prevEntries) {
+    return;
+  }
+
+  if (!isInvalidUrl(newUrl, newSource)) {
+    const parts = getPathParts(newUrl, thread, debuggeeHost);
+
+    if (parts.length === prevEntries.length) {
+      let match = true;
+      for (let i = 0; i < parts.length - 2; i++) {
+        if (parts[i].path !== prevEntries[i + 1].node.path) {
+          match = false;
+          break;
+        }
+      }
+
+      if (match) {
+        const { node, index } = prevEntries.pop();
+        
+        
+        const fileNode: TreeSource = (node.contents[index]: any);
+        fileNode.name = parts[parts.length - 1].part;
+        fileNode.path = parts[parts.length - 1].path;
+        fileNode.contents = newSource;
+        return;
+      }
+    }
+  }
+
+  
+  
+  for (let i = prevEntries.length - 1; i >= 0; i--) {
+    const { node, index } = prevEntries[i];
+
+    
+    
+    if (node.contents.length > 1 || (i === 0 && thread)) {
+      node.contents.splice(index, 1);
+      break;
+    }
+  }
+  addToTree(tree, newSource, debuggeeHost, thread);
+}
+
+type Entry = {
+  node: TreeDirectory,
+  index: number,
+};
+
+function findEntries(tree, url, source, thread, debuggeeHost): ?Array<Entry> {
+  const parts = getPathParts(url, thread, debuggeeHost);
+
+  
+  
+  
+  
+  
+  parts.pop();
+
+  const entries = [];
+  let currentNode = tree;
+  for (const { part } of parts) {
+    const { found: childFound, index: childIndex } = findNodeInContents(
+      currentNode,
+      createTreeNodeMatcher(part, true, debuggeeHost)
+    );
+
+    if (!childFound || currentNode.type !== "directory") {
+      return null;
+    }
+
+    entries.push({
+      node: currentNode,
+      index: childIndex,
+    });
+
+    currentNode = currentNode.contents[childIndex];
+  }
+
+  
+  
+  const found = (function search(node) {
+    if (node.type !== "directory") {
+      if (node.contents.id === source.id) {
+        return [];
+      }
+      return null;
+    }
+
+    for (let i = 0; i < node.contents.length; i++) {
+      const child = node.contents[i];
+      const result = search(child);
+      if (result) {
+        result.unshift({
+          node,
+          index: i,
+        });
+        return result;
+      }
+    }
+
+    return null;
+  })(currentNode);
+
+  return found ? [...entries, ...found] : null;
 }
