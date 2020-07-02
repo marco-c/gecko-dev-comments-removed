4bc9d8f77ed42d3cf9a58c706a70c7be23a60cf3
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-07-02 12:34:58 +0000

    Bug 1639284 - Store records, timestamp and metadata in one IndexedDB transaction  r=Gijs,mixedpuppy
    Differential Revision: https://phabricator.services.mozilla.com/D78246
    
    UltraBlame original commit: a0af6d53c082d97a83c9c0891c36e35331330880

--- services/settings/RemoteSettingsWorker.js
+++ services/settings/RemoteSettingsWorker.js
@@ -199,6 +199,8 @@ async function importDumpIDB(bucket, collection, records) {
       [IDB_RECORDS_STORE, IDB_TIMESTAMPS_STORE],
       "readwrite",
       ([recordsStore, timestampStore], rejectTransaction) => {
+        
+        recordsStore.delete(IDBKeyRange.bound([cid], [cid, []], false, true));
         IDBHelpers.bulkOperationHelper(
           recordsStore,
           {
