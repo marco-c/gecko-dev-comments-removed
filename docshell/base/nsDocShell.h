0dd524fcd4fc0fcfcabbca912ebbf2717ae564fd
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-07-02 13:33:18 +0000

    Bug 1626362 - Combine two load error page functions into one. r=nika
    I don't think we need these to be done at different times, since the set of checks that happen before uri fixup are error codes that won't be affected by fixup.
    
    Differential Revision: https://phabricator.services.mozilla.com/D80107
    
    UltraBlame original commit: 741edfcbb68d57d6ed1204f77fc4eae603327d1b

--- docshell/base/nsDocShell.h
+++ docshell/base/nsDocShell.h
@@ -442,8 +442,6 @@ class nsDocShell final : public nsDocLoader,
       bool aNotifyKeywordSearchLoading = false,
       nsIInputStream** aNewPostData = nullptr);
 
-  static bool ShouldLoadErrorPageWithoutFixup(nsresult aStatus);
-
   
   
   
