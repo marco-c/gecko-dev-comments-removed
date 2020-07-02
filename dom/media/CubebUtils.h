d6f42e23e5a4dd1b5dd1e825c69a379213aa9fe2
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-07-02 13:23:23 +0000

    Bug 1646348 - Expose an AudioThreadRegistry in CubebUtils. r=achronop
    Differential Revision: https://phabricator.services.mozilla.com/D80465
    
    UltraBlame original commit: 40b5b4babc734ba305d8d1c864e06767aa0f4167

--- dom/media/CubebUtils.h
+++ dom/media/CubebUtils.h
@@ -16,6 +16,9 @@ class AudioDeviceInfo;
 MOZ_MAKE_ENUM_CLASS_BITWISE_OPERATORS(cubeb_stream_prefs)
 
 namespace mozilla {
+
+class AudioThreadRegistry;
+
 namespace CubebUtils {
 
 typedef cubeb_devid AudioDeviceID;
@@ -29,6 +32,10 @@ void InitLibrary();
 void ShutdownLibrary();
 
 bool SandboxEnabled();
+
+
+
+AudioThreadRegistry* GetAudioThreadRegistry();
 
 
 uint32_t MaxNumberOfChannels();
