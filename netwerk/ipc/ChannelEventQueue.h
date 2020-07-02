978085e721aca0e7b9a48237fc1bb1ebcb5bb8ea
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-07-02 13:48:32 +0000

    Bug 1646592 - P2 remove NeckoTargetChannelEvent which could not be specialized for HttpChannelChild, r=mayhemer,necko-reviewers
    NeckoTargetChannelEvent uses a protected member function, which couldn't compile with instantiation of HttpChannelChild
    https://searchfox.org/mozilla-central/rev/cfaa250d14e344834932de4c2eed0061701654da/netwerk/ipc/ChannelEventQueue.h#64
    
    Differential Revision: https://phabricator.services.mozilla.com/D81273
    
    UltraBlame original commit: de9ba1eaded125fa3c3c1ea50154dbe59ef608db

--- netwerk/ipc/ChannelEventQueue.h
+++ netwerk/ipc/ChannelEventQueue.h
@@ -44,30 +44,6 @@ class MainThreadChannelEvent : public ChannelEvent {
   }
 };
 
-
-
-
-
-template <typename T>
-class NeckoTargetChannelEvent : public ChannelEvent {
- public:
-  explicit NeckoTargetChannelEvent(T* aChild) : mChild(aChild) {
-    MOZ_COUNT_CTOR(NeckoTargetChannelEvent);
-  }
-  virtual ~NeckoTargetChannelEvent() {
-    MOZ_COUNT_DTOR(NeckoTargetChannelEvent);
-  }
-
-  already_AddRefed<nsIEventTarget> GetEventTarget() override {
-    MOZ_ASSERT(mChild);
-
-    return mChild->GetNeckoTarget();
-  }
-
- protected:
-  T* mChild;
-};
-
 class ChannelFunctionEvent : public ChannelEvent {
  public:
   ChannelFunctionEvent(
