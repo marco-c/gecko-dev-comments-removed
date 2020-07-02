f6ab6a2424c46bfa958c78e5968dc88561589264
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-07-02 13:15:42 +0000

    Backed out changeset 0238d868b580 (bug 1645725) for build bustage at FuzzyLayer.cpp. CLOSED TREE
    UltraBlame original commit: 530c537affa9efaa41ea4c45f24dc09be12852c1

--- mobile/android/locales/filter.py
+++ mobile/android/locales/filter.py
@@ -21,6 +21,7 @@ def test(mod, path, entity=None):
         
         if path in (
             "chrome/global/aboutReader.properties",
+            "chrome/global/charsetMenu.properties",
             "chrome/global/commonDialogs.properties",
             "chrome/global/intl.properties",
             "chrome/global/intl.css",
