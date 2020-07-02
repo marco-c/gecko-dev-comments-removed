84c3ba66c5a4ceee9366c9e760f77455772e7cd0
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-07-02 13:12:54 +0000

    Bug 1584396 - Do not assert about other thread dispatches at worker shutdown. r=dom-workers-and-storage-reviewers,ttung
    Differential Revision: https://phabricator.services.mozilla.com/D78836
    
    UltraBlame original commit: 383bbc14ece1f3296266f96c0d197772de07a314

--- dom/workers/WorkerThread.cpp
+++ dom/workers/WorkerThread.cpp
@@ -127,10 +127,12 @@ void WorkerThread::SetWorker(const WorkerThreadFriendKey& ,
 
       MOZ_ASSERT(mWorkerPrivate);
       MOZ_ASSERT(!mAcceptingNonWorkerRunnables);
-      MOZ_ASSERT(!mOtherThreadsDispatchingViaEventTarget,
-                 "XPCOM Dispatch hapenning at the same time our thread is "
-                 "being unset! This should not be possible!");
-
+      
+      
+      
+      
+      
+      
       while (mOtherThreadsDispatchingViaEventTarget) {
         mWorkerPrivateCondVar.Wait();
       }
