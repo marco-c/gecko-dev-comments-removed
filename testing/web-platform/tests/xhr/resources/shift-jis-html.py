8ae9fa66bf941ae0ddc5029bf04b310ec1b040c3
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-07-02 13:56:25 +0000

    Bug 1647268 [wpt PR 24278] - Python 3: port xhr tests [part 6], a=testonly
    Automatic update from web-platform-tests
    Python 3: port xhr tests [part 6] (#24278)
    
    --
    
    wpt-commits: 0db855f1a7721813e78d49e42a8c58088828b5ca
    wpt-pr: 24278
    
    UltraBlame original commit: a4b79555c7fc58be52038880f1976a0ea6c43f34

--- testing/web-platform/tests/xhr/resources/shift-jis-html.py
+++ testing/web-platform/tests/xhr/resources/shift-jis-html.py
@@ -1,5 +1,5 @@
 def main(request, response):
-    headers = [("Content-type", "text/html;charset=shift-jis")]
+    headers = [(b"Content-type", b"text/html;charset=shift-jis")]
     
     content = chr(0x83) + chr(0x65) + chr(0x83) + chr(0x58) + chr(0x83) + chr(0x67)
 
