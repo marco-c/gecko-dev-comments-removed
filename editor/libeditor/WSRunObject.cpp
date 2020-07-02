9d4051f8f41814e851da4c02507d899a3df64286
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-07-02 13:24:57 +0000

    Bug 1646325 - part 1-3: Rewrite remaining part of `WSRunScanner::GetRuns()` with early-return style r=m_kato
    Differential Revision: https://phabricator.services.mozilla.com/D80313
    
    UltraBlame original commit: fd4c915581203d795d7c5667b49d686555ce8698

--- editor/libeditor/WSRunObject.cpp
+++ editor/libeditor/WSRunObject.cpp
@@ -1061,45 +1061,47 @@ void WSRunScanner::GetRuns() {
       normalRun->mEndOffset = mEnd.PointRef().Offset();
     }
     mEndRun = normalRun;
-  } else {
-    
-    
-    
-    
-    if (mNBSPData.LastPointRef().IsSet() && mEnd.PointRef().IsSet() &&
-        mNBSPData.LastPointRef().GetContainer() ==
-            mEnd.PointRef().GetContainer() &&
-        mNBSPData.LastPointRef().Offset() == mEnd.PointRef().Offset() - 1) {
-      
-      normalRun->SetEndBy(mEnd.RawReason());
-      normalRun->mEndNode = mEnd.PointRef().GetContainer();
-      normalRun->mEndOffset = mEnd.PointRef().Offset();
-      mEndRun = normalRun;
-    } else {
-      if (mNBSPData.LastPointRef().IsSet()) {
-        normalRun->mEndNode = mNBSPData.LastPointRef().GetContainer();
-        normalRun->mEndOffset = mNBSPData.LastPointRef().Offset() + 1;
-      }
-      normalRun->SetEndByTrailingWhiteSpaces();
-
-      
-      WSFragment* lastRun = new WSFragment();
-      lastRun->MarkAsEndOfHardLine();
-      if (mNBSPData.LastPointRef().IsSet()) {
-        lastRun->mStartNode = mNBSPData.LastPointRef().GetContainer();
-        lastRun->mStartOffset = mNBSPData.LastPointRef().Offset() + 1;
-      }
-      if (mEnd.PointRef().IsSet()) {
-        lastRun->mEndNode = mEnd.PointRef().GetContainer();
-        lastRun->mEndOffset = mEnd.PointRef().Offset();
-      }
-      lastRun->SetStartFromNormalWhiteSpaces();
-      lastRun->mLeft = normalRun;
-      lastRun->SetEndBy(mEnd.RawReason());
-      mEndRun = lastRun;
-      normalRun->mRight = lastRun;
-    }
+    return;
   }
+
+  
+  
+  
+  
+  if (mNBSPData.LastPointRef().IsSet() && mEnd.PointRef().IsSet() &&
+      mNBSPData.LastPointRef().GetContainer() ==
+          mEnd.PointRef().GetContainer() &&
+      mNBSPData.LastPointRef().Offset() == mEnd.PointRef().Offset() - 1) {
+    
+    normalRun->SetEndBy(mEnd.RawReason());
+    normalRun->mEndNode = mEnd.PointRef().GetContainer();
+    normalRun->mEndOffset = mEnd.PointRef().Offset();
+    mEndRun = normalRun;
+    return;
+  }
+
+  if (mNBSPData.LastPointRef().IsSet()) {
+    normalRun->mEndNode = mNBSPData.LastPointRef().GetContainer();
+    normalRun->mEndOffset = mNBSPData.LastPointRef().Offset() + 1;
+  }
+  normalRun->SetEndByTrailingWhiteSpaces();
+
+  
+  WSFragment* lastRun = new WSFragment();
+  lastRun->MarkAsEndOfHardLine();
+  if (mNBSPData.LastPointRef().IsSet()) {
+    lastRun->mStartNode = mNBSPData.LastPointRef().GetContainer();
+    lastRun->mStartOffset = mNBSPData.LastPointRef().Offset() + 1;
+  }
+  if (mEnd.PointRef().IsSet()) {
+    lastRun->mEndNode = mEnd.PointRef().GetContainer();
+    lastRun->mEndOffset = mEnd.PointRef().Offset();
+  }
+  lastRun->SetStartFromNormalWhiteSpaces();
+  lastRun->mLeft = normalRun;
+  lastRun->SetEndBy(mEnd.RawReason());
+  mEndRun = lastRun;
+  normalRun->mRight = lastRun;
 }
 
 void WSRunScanner::ClearRuns() {
