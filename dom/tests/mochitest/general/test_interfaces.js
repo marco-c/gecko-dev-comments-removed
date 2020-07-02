774e595a6cfadcbc1a529ffdc07c9ae4efd4ee3d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-07-02 13:40:03 +0000

    Bug 1645046 - Enable HTML5 dialog in Nightly r=smaug
    Differential Revision: https://phabricator.services.mozilla.com/D80045
    
    UltraBlame original commit: 397ad8a55255d9471f3d39ec42e63b7bdfea80f6

--- dom/tests/mochitest/general/test_interfaces.js
+++ dom/tests/mochitest/general/test_interfaces.js
@@ -462,7 +462,7 @@ var interfaceNamesInGlobalScope = [
   
   { name: "HTMLDetailsElement", insecureContext: true },
   
-  { name: "HTMLDialogElement", insecureContext: true, disabled: true },
+  { name: "HTMLDialogElement", insecureContext: true, nightly: true },
   
   { name: "HTMLDirectoryElement", insecureContext: true },
   
