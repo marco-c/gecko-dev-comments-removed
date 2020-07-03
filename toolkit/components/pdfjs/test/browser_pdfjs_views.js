833ad8d9ee1cbee6f7fa13abb74a7498936e4355
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-07-03 00:24:50 +0000

    Bug 1618465 - Update references to pdf.js for the move to toolkit. r=bdahl
    Differential Revision: https://phabricator.services.mozilla.com/D72533
    
    UltraBlame original commit: 7563d04fc110df09e5530e21cec5d0c55485093d

--- toolkit/components/pdfjs/test/browser_pdfjs_views.js
+++ toolkit/components/pdfjs/test/browser_pdfjs_views.js
@@ -1,7 +1,7 @@
 
 
 
-const RELATIVE_DIR = "browser/extensions/pdfjs/test/";
+const RELATIVE_DIR = "toolkit/components/pdfjs/test/";
 const TESTROOT = "http://example.com/browser/" + RELATIVE_DIR;
 
 add_task(async function test() {
