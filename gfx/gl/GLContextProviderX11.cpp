2a6381cab2407854a2160a8db292c0311a146f7f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-07-03 00:25:57 +0000

    Bug 1640048 - [X11] Initial EGL runtime support, r=jgilbert,rmader
    Patch by Robert Mader [:rmader]
    
    This enables support for EGL on X11, toggled by setting the
    MOZ_X11_EGL env variable. It allows running the Webrender
    and OpenGL compositors basically like we do with GLX by default.
    
    Note that that there are several things missing:
    
    - no pixmap sharing support (not clear if we want/need it)
    - it still uses GLX for several things like finding the X11 visual, VSync and glxtest.
    
    It includes some cleanups, mostly in order to decouple the X11
    and Wayland implementations a bit more.
    
    Differential Revision: https://phabricator.services.mozilla.com/D76417
    
    UltraBlame original commit: 26aa0ccc6c5e29342acf553b44c9c43564224e03

---
+++ gfx/gl/GLContextProviderX11.cpp
@@ -0,0 +1,72 @@
+
+
+
+
+
+#include "prenv.h"
+
+#include "GLContextProvider.h"
+
+namespace mozilla::gl {
+
+using namespace mozilla::gfx;
+using namespace mozilla::widget;
+
+static class GLContextProviderGLX sGLContextProviderGLX;
+static class GLContextProviderEGL sGLContextProviderEGL;
+
+static bool UseGLX() {
+  static bool useGLX = !PR_GetEnv("MOZ_X11_EGL");
+  return useGLX;
+}
+
+already_AddRefed<GLContext> GLContextProviderX11::CreateWrappingExisting(
+    void* aContext, void* aSurface) {
+  if (UseGLX()) {
+    return sGLContextProviderGLX.CreateWrappingExisting(aContext, aSurface);
+  } else {
+    return sGLContextProviderEGL.CreateWrappingExisting(aContext, aSurface);
+  }
+}
+
+already_AddRefed<GLContext> GLContextProviderX11::CreateForCompositorWidget(
+    CompositorWidget* aCompositorWidget, bool aWebRender,
+    bool aForceAccelerated) {
+  if (UseGLX()) {
+    return sGLContextProviderGLX.CreateForCompositorWidget(
+        aCompositorWidget, aWebRender, aForceAccelerated);
+  } else {
+    return sGLContextProviderEGL.CreateForCompositorWidget(
+        aCompositorWidget, aWebRender, aForceAccelerated);
+  }
+}
+
+
+already_AddRefed<GLContext> GLContextProviderX11::CreateHeadless(
+    const GLContextCreateDesc& desc, nsACString* const out_failureId) {
+  if (UseGLX()) {
+    return sGLContextProviderGLX.CreateHeadless(desc, out_failureId);
+  } else {
+    return sGLContextProviderEGL.CreateHeadless(desc, out_failureId);
+  }
+}
+
+
+GLContext* GLContextProviderX11::GetGlobalContext() {
+  if (UseGLX()) {
+    return sGLContextProviderGLX.GetGlobalContext();
+  } else {
+    return sGLContextProviderEGL.GetGlobalContext();
+  }
+}
+
+
+void GLContextProviderX11::Shutdown() {
+  if (UseGLX()) {
+    sGLContextProviderGLX.Shutdown();
+  } else {
+    sGLContextProviderEGL.Shutdown();
+  }
+}
+
+}  
