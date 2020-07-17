f23a88d92c389c60e16381faa6a6e97eb54865b8
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-07-17 01:07:33 +0000

    Bug 1650765 - Iterate over raw ranges in EncodeAsString. r=dom-workers-and-storage-reviewers,janv
    Differential Revision: https://phabricator.services.mozilla.com/D82354
    
    UltraBlame original commit: 637bf1e1ee6bebd4f57edf1c7c3fb897a1624cae

--- dom/indexedDB/Key.cpp
+++ dom/indexedDB/Key.cpp
@@ -21,6 +21,7 @@
 #include "mozilla/CheckedInt.h"
 #include "mozilla/EndianUtils.h"
 #include "mozilla/FloatingPoint.h"
+#include "mozilla/ReverseIterator.h"
 #include "mozIStorageStatement.h"
 #include "mozIStorageValueArray.h"
 #include "nsAlgorithm.h"
@@ -568,7 +569,14 @@ IDBResult<void, IDBSpecialValue::Invalid> Key::EncodeAsString(
 
   MOZ_ASSERT(size.isValid());
 
-  for (const auto val : aInput) {
+  
+  
+  
+  
+  const auto inputRange = mozilla::detail::IteratorRange(
+      aInput.Elements(), aInput.Elements() + aInput.Length());
+
+  for (const auto val : inputRange) {
     if (val > ONE_BYTE_LIMIT) {
       size += char16_t(val) > TWO_BYTE_LIMIT ? 2 : 1;
       if (!size.isValid()) {
@@ -598,7 +606,7 @@ IDBResult<void, IDBSpecialValue::Invalid> Key::EncodeAsString(
   *(buffer++) = aType;
 
   
-  for (const auto val : aInput) {
+  for (const auto val : inputRange) {
     if (val <= ONE_BYTE_LIMIT) {
       *(buffer++) = val + ONE_BYTE_ADJUST;
     } else if (char16_t(val) <= TWO_BYTE_LIMIT) {
