f35771c3f9ad200a7ba5d9458970bdcab9a6d3e4
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-07-17 01:36:08 +0000

    Bug 1650613 - Fix about:cache appcache listing, let AppCacheStorage be visited with null load context info, r=michal,necko-reviewers
    Depends on D81885
    
    Differential Revision: https://phabricator.services.mozilla.com/D82542
    
    UltraBlame original commit: a3e58c3e98da22c3a289e3abacbf5aeab83e7ef4

--- netwerk/cache2/OldWrappers.cpp
+++ netwerk/cache2/OldWrappers.cpp
@@ -156,7 +156,7 @@ NS_IMETHODIMP _OldVisitCallbackWrapper::VisitDevice(
     nsCacheService::GetAppCacheDirectory(getter_AddRefs(dir));
   }
 
-  if (mLoadInfo->IsAnonymous()) {
+  if (mLoadInfo && mLoadInfo->IsAnonymous()) {
     
     
     mCB->OnCacheStorageInfo(0, 0, capacity, dir);
