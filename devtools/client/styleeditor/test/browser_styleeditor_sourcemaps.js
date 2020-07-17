bb4c58513eb89732e671cd874e6b2643cf7d7118
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-07-17 01:19:54 +0000

    Bug 1625930: Apply ResourceWatcher to StyleEditor. r=ochameau
    Depends on D78239
    
    Differential Revision: https://phabricator.services.mozilla.com/D78240
    
    UltraBlame original commit: a481df06a3d652bc18476e1698f643b2ac906edc

--- devtools/client/styleeditor/test/browser_styleeditor_sourcemaps.js
+++ devtools/client/styleeditor/test/browser_styleeditor_sourcemaps.js
@@ -127,7 +127,7 @@ function testEditor(editor, possibleNames) {
 
 
 function togglePref(UI) {
-  const editorsPromise = UI.once("stylesheets-reset");
+  const editorsPromise = UI.once("stylesheets-refreshed");
   const selectedPromise = UI.once("editor-selected");
 
   Services.prefs.setBoolPref(PREF, false);
