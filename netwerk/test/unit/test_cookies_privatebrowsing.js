e0e2c875c9459041d6c20c496fd4b1eb4133ae6f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-07-17 01:26:09 +0000

    Bug 1464542: Part 3c - Change Services.jsm to use the C++-implemented services cache. r=mccr8
    Differential Revision: https://phabricator.services.mozilla.com/D81420
    
    UltraBlame original commit: 9d3a0ea2cf657a837ad38f43e3cad5eb498fad8f

--- netwerk/test/unit/test_cookies_privatebrowsing.js
+++ netwerk/test/unit/test_cookies_privatebrowsing.js
@@ -45,7 +45,7 @@ add_task(async () => {
   let uri2 = NetUtil.newURI("http://bar.com/bar.html");
 
   
-  Services.cookies.setCookieStringFromHttp(
+  Services.cookiesvc.setCookieStringFromHttp(
     uri1,
     "oh=hai; max-age=1000",
     make_channel(uri1.spec)
@@ -61,7 +61,11 @@ add_task(async () => {
   chan2.QueryInterface(Ci.nsIPrivateBrowsingChannel);
   chan2.setPrivate(true);
 
-  Services.cookies.setCookieStringFromHttp(uri2, "oh=hai; max-age=1000", chan2);
+  Services.cookiesvc.setCookieStringFromHttp(
+    uri2,
+    "oh=hai; max-age=1000",
+    chan2
+  );
   Assert.equal(await getCookieStringFromPrivateDocument(uri1.spec), "");
   Assert.equal(await getCookieStringFromPrivateDocument(uri2.spec), "oh=hai");
 
@@ -70,7 +74,11 @@ add_task(async () => {
   Assert.equal(await getCookieStringFromPrivateDocument(uri1.spec), "");
   Assert.equal(await getCookieStringFromPrivateDocument(uri2.spec), "");
 
-  Services.cookies.setCookieStringFromHttp(uri2, "oh=hai; max-age=1000", chan2);
+  Services.cookiesvc.setCookieStringFromHttp(
+    uri2,
+    "oh=hai; max-age=1000",
+    chan2
+  );
   Assert.equal(await getCookieStringFromPrivateDocument(uri2.spec), "oh=hai");
 
   
@@ -89,7 +97,11 @@ add_task(async () => {
   
   Assert.equal(await getCookieStringFromPrivateDocument(uri1.spec), "");
   Assert.equal(await getCookieStringFromPrivateDocument(uri2.spec), "");
-  Services.cookies.setCookieStringFromHttp(uri2, "oh=hai; max-age=1000", chan2);
+  Services.cookiesvc.setCookieStringFromHttp(
+    uri2,
+    "oh=hai; max-age=1000",
+    chan2
+  );
   Assert.equal(await getCookieStringFromPrivateDocument(uri2.spec), "oh=hai");
 
   
