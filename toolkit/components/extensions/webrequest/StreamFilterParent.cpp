cac393325ea47c1869e8dcd529b1e40536aed0cb
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-07-17 02:12:08 +0000

    Bug 1647954 - not touching Atomic for redirection/alt-data disconnection, r=mixedpuppy
    Also address alt-data in comment
    
    Differential Revision: https://phabricator.services.mozilla.com/D83231
    
    UltraBlame original commit: 475ab29538a3c7796e97a75e35a6da553abbe391

--- toolkit/components/extensions/webrequest/StreamFilterParent.cpp
+++ toolkit/components/extensions/webrequest/StreamFilterParent.cpp
@@ -608,7 +608,7 @@ StreamFilterParent::OnDataAvailable(nsIRequest* aRequest,
                                     uint64_t aOffset, uint32_t aCount) {
   AssertIsIOThread();
 
-  if (mState == State::Disconnected || mDisconnectedByOnStartRequest) {
+  if (mDisconnectedByOnStartRequest || mState == State::Disconnected) {
     
     
     
