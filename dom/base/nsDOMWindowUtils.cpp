f75e58064081f8d053bcf2fcd538eb03414e4f20
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-07-17 00:56:07 +0000

    Bug 1324591 - Add nsLayoutUtils::IsInPositionFixedSubtree. r=TYLin
    Differential Revision: https://phabricator.services.mozilla.com/D81480
    
    UltraBlame original commit: 84657a3522fb9ad9db66955793692284c7ed720c

--- dom/base/nsDOMWindowUtils.cpp
+++ dom/base/nsDOMWindowUtils.cpp
@@ -2615,13 +2615,9 @@ nsDOMWindowUtils::ZoomToFocusedInput() {
     
     
     
-    if (frame->PresShell() == presShell) {
-      for (; frame; frame = frame->GetParent()) {
-        if (frame->StyleDisplay()->mPosition == StylePositionProperty::Fixed &&
-            nsLayoutUtils::IsReallyFixedPos(frame)) {
-          return true;
-        }
-      }
+    if (frame->PresShell() == presShell &&
+        nsLayoutUtils::IsInPositionFixedSubtree(frame)) {
+      return true;
     }
     return false;
   }();
