8d1c07e76eb701ba1d5d7e0b437a9771dcc23f11
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-07-17 02:05:38 +0000

    Bug 1652646 Part 1 - Use nsIFrame::IsGridItem() whenever possible. r=dholbert
    Differential Revision: https://phabricator.services.mozilla.com/D83453
    
    UltraBlame original commit: ddf16c9e09fcf19804a8c0cfa25c5ba005514773

--- layout/generic/nsIFrame.cpp
+++ layout/generic/nsIFrame.cpp
@@ -6059,8 +6059,7 @@ LogicalSize nsIFrame::ComputeSize(gfxContext* aRenderingContext,
 
   auto parentFrame = GetParent();
   auto alignCB = parentFrame;
-  bool isGridItem = parentFrame && parentFrame->IsGridContainerFrame() &&
-                    !HasAnyStateBits(NS_FRAME_OUT_OF_FLOW);
+  bool isGridItem = IsGridItem();
   if (parentFrame && parentFrame->IsTableWrapperFrame() && IsTableFrame()) {
     
     
