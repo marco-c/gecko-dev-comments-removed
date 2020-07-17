48d93975c50e3038d4afc20b113e5378acf2a2a4
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-07-17 01:28:50 +0000

    Bug 1580811 - Make the ContentPage browser work with out-of-process frames, r=nika,zombie
    Differential Revision: https://phabricator.services.mozilla.com/D80084
    
    UltraBlame original commit: f9c8318e3c0133c83703826b46868fd1a0e563e2

--- xpfe/appshell/nsAppShellService.cpp
+++ xpfe/appshell/nsAppShellService.cpp
@@ -428,8 +428,18 @@ WindowlessBrowser::GetDocShell(nsIDocShell** aDocShell) {
 }
 
 NS_IMETHODIMP
-nsAppShellService::CreateWindowlessBrowser(bool aIsChrome,
+nsAppShellService::CreateWindowlessBrowser(bool aIsChrome, uint32_t aChromeMask,
                                            nsIWindowlessBrowser** aResult) {
+  if (aChromeMask) {
+    MOZ_DIAGNOSTIC_ASSERT(aIsChrome, "Got chrome flags for non-chrome browser");
+    if (aChromeMask & ~(nsIWebBrowserChrome::CHROME_REMOTE_WINDOW |
+                        nsIWebBrowserChrome::CHROME_FISSION_WINDOW |
+                        nsIWebBrowserChrome::CHROME_PRIVATE_WINDOW)) {
+      NS_ERROR("Received unexpected chrome flags");
+      return NS_ERROR_FAILURE;
+    }
+  }
+
   
 
 
@@ -461,6 +471,16 @@ nsAppShellService::CreateWindowlessBrowser(bool aIsChrome,
   RefPtr<BrowsingContext> browsingContext = BrowsingContext::CreateIndependent(
       aIsChrome ? BrowsingContext::Type::Chrome
                 : BrowsingContext::Type::Content);
+
+  if (aChromeMask & nsIWebBrowserChrome::CHROME_REMOTE_WINDOW) {
+    browsingContext->SetRemoteTabs(true);
+  }
+  if (aChromeMask & nsIWebBrowserChrome::CHROME_FISSION_WINDOW) {
+    browsingContext->SetRemoteSubframes(true);
+  }
+  if (aChromeMask & nsIWebBrowserChrome::CHROME_PRIVATE_WINDOW) {
+    browsingContext->SetPrivateBrowsing(true);
+  }
 
   
 
