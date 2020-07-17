69b86d0a8a9b355c709e833089ac6d8c8515a458
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-07-17 01:43:25 +0000

    Bug 1648820 - Part 1: Add additional floating point registers. r=iain
    Differential Revision: https://phabricator.services.mozilla.com/D83021
    
    UltraBlame original commit: afe8b0a1d8183f4d575b894830ec4d9d04fbaafe

--- js/src/jit/x86/SharedICRegisters-x86.h
+++ js/src/jit/x86/SharedICRegisters-x86.h
@@ -31,6 +31,8 @@ static constexpr Register ExtractTemp1 = InvalidReg;
 
 static constexpr FloatRegister FloatReg0 = xmm0;
 static constexpr FloatRegister FloatReg1 = xmm1;
+static constexpr FloatRegister FloatReg2 = xmm2;
+static constexpr FloatRegister FloatReg3 = xmm3;
 
 }  
 }  
