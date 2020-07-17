2f411639c78ab9c6c393d84fd27702ba8d17c6f5
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-07-17 01:27:35 +0000

    Bug 1645234 - Add more warnings to document.requestStorageAccess(). r=annevk,englehardt,baku
    The only common failure case that's not being warned about now is when the user
    rejected the prompt, which I think is expected behavior.
    
    Differential Revision: https://phabricator.services.mozilla.com/D79597
    
    UltraBlame original commit: a90a3c1efdc5d84db1a29d67191cff7ed993a0ec

--- dom/base/Document.cpp
+++ dom/base/Document.cpp
@@ -15886,6 +15886,10 @@ already_AddRefed<mozilla::dom::Promise> Document::RequestStorageAccess(
 
   
   if (NodePrincipal()->GetIsNullPrincipal()) {
+    nsContentUtils::ReportToConsole(nsIScriptError::errorFlag,
+                                    nsLiteralCString("requestStorageAccess"),
+                                    this, nsContentUtils::eDOM_PROPERTIES,
+                                    "RequestStorageAccessNullPrincipal");
     promise->MaybeRejectWithUndefined();
     return promise.forget();
   }
@@ -15935,6 +15939,9 @@ already_AddRefed<mozilla::dom::Promise> Document::RequestStorageAccess(
   
   
   if (StorageAccessSandboxed()) {
+    nsContentUtils::ReportToConsole(
+        nsIScriptError::errorFlag, nsLiteralCString("requestStorageAccess"),
+        this, nsContentUtils::eDOM_PROPERTIES, "RequestStorageAccessSandboxed");
     promise->MaybeRejectWithUndefined();
     return promise.forget();
   }
@@ -15942,12 +15949,19 @@ already_AddRefed<mozilla::dom::Promise> Document::RequestStorageAccess(
   
   RefPtr<BrowsingContext> parentBC = bc->GetParent();
   if (parentBC && !parentBC->IsTopContent()) {
+    nsContentUtils::ReportToConsole(
+        nsIScriptError::errorFlag, nsLiteralCString("requestStorageAccess"),
+        this, nsContentUtils::eDOM_PROPERTIES, "RequestStorageAccessNested");
     promise->MaybeRejectWithUndefined();
     return promise.forget();
   }
 
   
   if (!UserActivation::IsHandlingUserInput()) {
+    nsContentUtils::ReportToConsole(nsIScriptError::errorFlag,
+                                    nsLiteralCString("requestStorageAccess"),
+                                    this, nsContentUtils::eDOM_PROPERTIES,
+                                    "RequestStorageAccessUserGesture");
     promise->MaybeRejectWithUndefined();
     return promise.forget();
   }
