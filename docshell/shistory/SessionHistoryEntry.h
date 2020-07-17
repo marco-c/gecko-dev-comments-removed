e5b93e73242002e94bb97d27f97b6248eeb2bae7
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-07-17 01:59:48 +0000

    Bug 1648038 - Don't require session history object when creating an entry. r=smaug
    Differential Revision: https://phabricator.services.mozilla.com/D80883
    
    UltraBlame original commit: b6b620d72e6b22ecf7d2db64a8dfa30f3974dece

--- docshell/shistory/SessionHistoryEntry.h
+++ docshell/shistory/SessionHistoryEntry.h
@@ -67,8 +67,8 @@ class SessionHistoryInfo {
 
 class SessionHistoryEntry : public nsISHEntry {
  public:
-  SessionHistoryEntry(nsISHistory* aSessionHistory,
-                      nsDocShellLoadState* aLoadState, nsIChannel* aChannel);
+  SessionHistoryEntry(nsDocShellLoadState* aLoadState, nsIChannel* aChannel);
+  explicit SessionHistoryEntry(SessionHistoryInfo* aInfo);
 
   NS_DECL_ISUPPORTS
   NS_DECL_NSISHENTRY
