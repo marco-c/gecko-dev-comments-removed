87b99fc57978b10be95e232625ea4f021387e91d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-07-17 01:02:26 +0000

    Bug 1649383 - don't do geckoview builds on esr. r=jcristau
    Differential Revision: https://phabricator.services.mozilla.com/D81742
    
    UltraBlame original commit: cc4c70f5e63f3627dd85ff0d563eef5bb06d29d8

--- taskcluster/taskgraph/target_tasks.py
+++ taskcluster/taskgraph/target_tasks.py
@@ -451,7 +451,7 @@ def target_tasks_push_desktop(full_task_graph, parameters, graph_config):
             return True
         
         
-        if is_geckoview(task, parameters):
+        if 'mozilla-esr' not in parameters['project'] and is_geckoview(task, parameters):
             return True
 
         if task.attributes.get('shipping_product') == parameters['release_product'] and \
