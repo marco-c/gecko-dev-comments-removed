0bb72c6987d408098e28f908ccc7c06426bd4acf
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-07-17 02:12:28 +0000

    Bug 1653003: Implement IOUtils::remove method r=barret,Gijs
    This patch introduces a remove method to the IOUtils interface, which allows
    for removing files and directories on disk. It is a simple wrapper around
    the nsIFile::Remove method.
    
    Differential Revision: https://phabricator.services.mozilla.com/D83663
    
    UltraBlame original commit: 8dee8cb525a2c7cff36a89be107eb50628619201

--- dom/system/IOUtils.h
+++ dom/system/IOUtils.h
@@ -57,6 +57,10 @@ class IOUtils final {
                                         const nsAString& aDestPath,
                                         const MoveOptions& aOptions);
 
+  static already_AddRefed<Promise> Remove(GlobalObject& aGlobal,
+                                          const nsAString& aPath,
+                                          const RemoveOptions& aOptions);
+
   static bool IsAbsolutePath(const nsAString& aPath);
 
  private:
@@ -107,6 +111,9 @@ class IOUtils final {
   static nsresult MoveSync(const nsAString& aSource, const nsAString& aDest,
                            bool noOverwrite);
 
+  static nsresult RemoveSync(const nsAString& aPath, bool aIgnoreAbsent = true,
+                             bool aRecursive = false);
+
   using IOReadMozPromise =
       mozilla::MozPromise<nsTArray<uint8_t>, const nsCString,
                            true>;
@@ -115,6 +122,10 @@ class IOUtils final {
       mozilla::MozPromise<uint32_t, const nsCString,  true>;
 
   using IOMoveMozPromise =
+      mozilla::MozPromise<bool , const nsresult,
+                           true>;
+
+  using IORemoveMozPromise =
       mozilla::MozPromise<bool , const nsresult,
                            true>;
 };
