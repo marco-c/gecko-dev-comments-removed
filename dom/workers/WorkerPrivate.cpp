f8946b935d558e6ce32c7d1dcd3147b1538b3c95
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-07-17 01:00:19 +0000

    Bug 1643613 - Check that ScheduleDeletion is only called once. r=asuth,dom-workers-and-storage-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D81853
    
    UltraBlame original commit: 53d4d0721ae9cfadd6cd281e8bdac07749af56c3

--- dom/workers/WorkerPrivate.cpp
+++ dom/workers/WorkerPrivate.cpp
@@ -3404,11 +3404,15 @@ bool WorkerPrivate::IsOnCurrentThread() {
 }
 
 void WorkerPrivate::ScheduleDeletion(WorkerRanOrNot aRanOrNot) {
+  AssertIsOnWorkerThread();
   {
     
     
     auto data = mWorkerThreadAccessible.Access();
     MOZ_ASSERT(data->mChildWorkers.IsEmpty());
+
+    MOZ_RELEASE_ASSERT(!data->mDeletionScheduled);
+    data->mDeletionScheduled.Flip();
   }
   MOZ_ASSERT(mSyncLoopStack.IsEmpty());
   MOZ_ASSERT(mPostSyncLoopOperations == 0);
