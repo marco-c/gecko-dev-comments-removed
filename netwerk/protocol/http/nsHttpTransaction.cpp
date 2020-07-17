5e0c6d76b6367a88800c87b0d252a4063ab3384a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-07-17 02:03:33 +0000

    Bug 1646358 - Remove unnecessary main thread dispatch, r=dragana
    Differential Revision: https://phabricator.services.mozilla.com/D80004
    
    UltraBlame original commit: a28eb84718a6969c0f5f061804c81ac00564545c

--- netwerk/protocol/http/nsHttpTransaction.cpp
+++ netwerk/protocol/http/nsHttpTransaction.cpp
@@ -331,12 +331,18 @@ nsresult nsHttpTransaction::Init(
 
   
   if (mActivityDistributor) {
-    rv = mActivityDistributor->ObserveActivityWithArgs(
-        HttpActivityArgs(mChannelId), NS_HTTP_ACTIVITY_TYPE_HTTP_TRANSACTION,
-        NS_HTTP_ACTIVITY_SUBTYPE_REQUEST_HEADER, PR_Now(), 0, mReqHeaderBuf);
-    if (NS_FAILED(rv)) {
-      LOG3(("ObserveActivity failed (%08x)", static_cast<uint32_t>(rv)));
-    }
+    RefPtr<nsHttpTransaction> self = this;
+    nsCString requestBuf(mReqHeaderBuf);
+    NS_DispatchToMainThread(
+        NS_NewRunnableFunction("ObserveActivityWithArgs", [self, requestBuf]() {
+          nsresult rv = self->mActivityDistributor->ObserveActivityWithArgs(
+              HttpActivityArgs(self->mChannelId),
+              NS_HTTP_ACTIVITY_TYPE_HTTP_TRANSACTION,
+              NS_HTTP_ACTIVITY_SUBTYPE_REQUEST_HEADER, PR_Now(), 0, requestBuf);
+          if (NS_FAILED(rv)) {
+            LOG3(("ObserveActivity failed (%08x)", static_cast<uint32_t>(rv)));
+          }
+        }));
   }
 
   
