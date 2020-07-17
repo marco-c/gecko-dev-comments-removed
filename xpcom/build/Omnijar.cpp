00ed593843bad02d039c7b534dfe56a166fc1369
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-07-17 01:00:32 +0000

    Bug 1627075 - Allow lazily initializing nsZipArchives r=froydnj
    Opening our Omnijars can be expensive, and it should be deferrable until after
    startup is completed, provided we have a startup cache. In a previous patch in this
    stack, we implemented caching of the zip central directory for omnijars, but we
    still have to open the file in order to hand the object off to various omnijar
    consumers. In a later patch, we will wrap nsZipArchive access in a class which
    will allow us to transparently cache nsZipArchive results. These two get us
    most of the way to not needing to read from the underlying omnijar files during
    startup, but there are still nontrivial pieces, like nsZipFind for instance,
    which we don't want to just duplicate inside of a wrapper class, so we would
    like to sort out a way in which we can use an nsZipArchive class, but not
    actually back it up with the real underlying file until we really need data
    from it which we can't find in a cache.
    
    Depends on D77633
    
    Differential Revision: https://phabricator.services.mozilla.com/D78584
    
    UltraBlame original commit: 2cb021a493d83f8d4fea988cad492dd879bf5cf4

--- xpcom/build/Omnijar.cpp
+++ xpcom/build/Omnijar.cpp
@@ -93,20 +93,30 @@ void Omnijar::InitOne(nsIFile* aPath, Type aType) {
       centralBufLength = 0;
     }
   }
-  if (NS_FAILED(zipReader->OpenArchive(file, centralBuf, centralBufLength))) {
+  if (NS_FAILED(zipReader->OpenArchive(
+          file, MakeSpan(centralBuf, centralBufLength)))) {
     return;
   }
 
-  if (cache && !centralBuf) {
-    size_t bufSize;
-
-    
-    
-    
-    UniquePtr<char[]> centralBuf(reinterpret_cast<char*>(
-        zipReader->CopyCentralDirectoryBuffer(&bufSize).release()));
-    if (centralBuf) {
-      cache->PutBuffer(startupCacheKey.get(), std::move(centralBuf), bufSize);
+  if (!centralBuf) {
+    if (NS_FAILED(zipReader->OpenArchive(file))) {
+      return;
+    }
+    if (cache) {
+      size_t bufSize;
+      
+      
+      
+      UniquePtr<char[]> centralBuf(reinterpret_cast<char*>(
+          zipReader->CopyCentralDirectoryBuffer(&bufSize).release()));
+      if (centralBuf) {
+        cache->PutBuffer(startupCacheKey.get(), std::move(centralBuf), bufSize);
+      }
+    }
+  } else {
+    if (NS_FAILED(zipReader->LazyOpenArchive(
+            file, MakeSpan(centralBuf, centralBufLength)))) {
+      return;
     }
   }
 
