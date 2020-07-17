eb50d3725bd68b3c6a0cc4042b9b64e3e378aa71
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-07-17 00:53:18 +0000

    Bug 1648419 [wpt PR 24335] - Python 3: port tests in service-workers [part 2], a=testonly
    Automatic update from web-platform-tests
    Python 3: port tests in service-workers [part 2] (#24335)
    
    --
    
    wpt-commits: f2c264e640ea941f720b94a3e804a2676a51c7f8
    wpt-pr: 24335
    
    UltraBlame original commit: d61690dcdb29394684dc4750e89b6acd21208502

--- testing/web-platform/tests/service-workers/service-worker/resources/fetch-request-no-freshness-headers-script.py
+++ testing/web-platform/tests/service-workers/service-worker/resources/fetch-request-no-freshness-headers-script.py
@@ -1,6 +1,6 @@
 def main(request, response):
     headers = []
     
-    headers.append(("ETag", "abc123"))
-    headers.append(("Content-Type", "text/javascript"))
-    return headers, "/* empty script */"
+    headers.append((b"ETag", b"abc123"))
+    headers.append((b"Content-Type", b"text/javascript"))
+    return headers, b"/* empty script */"
