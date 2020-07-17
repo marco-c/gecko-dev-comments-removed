1fcc8cc910a5126b292adbf2ca17231257d26f42
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-07-17 01:49:10 +0000

    Bug 1648468 - Part 6 - Resolve a11y focus issue causing a failure in browser_test_focus_urlbar.js. r=adw
    The line that adds `suggestedIndex` in the test should also fix bug 1652138.
    
    Differential Revision: https://phabricator.services.mozilla.com/D83244
    
    UltraBlame original commit: 6cb8b095be15819cd49781554eb28b9a55c78de6

--- accessible/tests/browser/events/browser_test_focus_urlbar.js
+++ accessible/tests/browser/events/browser_test_focus_urlbar.js
@@ -337,6 +337,9 @@ async function runTipTests() {
     ),
   ];
 
+  
+  matches[1].suggestedIndex = 2;
+
   let provider = new TipTestProvider(matches);
   UrlbarProvidersManager.registerProvider(provider);
 
