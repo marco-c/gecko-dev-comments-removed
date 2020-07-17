1261e13cf75431e1692f91a2605bb2fc077ada4f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-07-17 01:31:15 +0000

    Bug 1647556 - part 14: Get rid of unnecessary `WSFragment` memebers r=m_kato
    Differential Revision: https://phabricator.services.mozilla.com/D82293
    
    UltraBlame original commit: b9cae60a1da47d60d92bf15df02e6f8b012321e3

--- editor/libeditor/WSRunObject.cpp
+++ editor/libeditor/WSRunObject.cpp
@@ -1807,7 +1807,7 @@ char16_t WSRunScanner::GetCharAt(Text* aTextNode, int32_t aOffset) const {
 }
 
 nsresult WSRunObject::NormalizeWhiteSpacesAtEndOf(const WSFragment& aRun) {
-  MOZ_ASSERT(aRun.IsVisibleAndMiddleOfHardLine());
+  MOZ_ASSERT(aRun.IsVisible());
 
   
   if (!StaticPrefs::editor_white_space_normalization_blink_compatible()) {
