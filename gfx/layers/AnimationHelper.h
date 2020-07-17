7c0066459f97078ed96d5471ce4233cacd7f3654
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-07-17 00:56:10 +0000

    Bug 1324591 - Jank if we are trying to composite area which is outside of the the partial prerender rect on non WebRender. r=botond,jrmuizel
    Differential Revision: https://phabricator.services.mozilla.com/D75731
    
    UltraBlame original commit: fef36ff2ea3de3160d2a994dba9d15980b6e7ba6

--- gfx/layers/AnimationHelper.h
+++ gfx/layers/AnimationHelper.h
@@ -14,6 +14,7 @@
 #include "mozilla/webrender/WebRenderTypes.h"  
 #include "mozilla/TimeStamp.h"                 
 #include "mozilla/TimingParams.h"
+#include "mozilla/Types.h"  
 #include "mozilla/Variant.h"
 #include "X11UndefineNone.h"
 #include <unordered_map>
@@ -134,6 +135,16 @@ class AnimationHelper {
   static gfx::Matrix4x4 ServoAnimationValueToMatrix4x4(
       const nsTArray<RefPtr<RawServoAnimationValue>>& aValue,
       const TransformData& aTransformData, gfx::Path* aCachedMotionPath);
+
+  
+
+
+
+
+  static bool ShouldBeJank(const LayoutDeviceIntRect& aPrerenderedRect,
+                           SideBits aOverflowedSides,
+                           const gfx::Matrix4x4& aTransform,
+                           const ParentLayerRect& aClipRect);
 };
 
 }  
