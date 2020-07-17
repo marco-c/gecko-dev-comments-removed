0ef9e253c7f99612e0c3035d5829912de47fd84c
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-07-17 01:24:34 +0000

    Backed out changeset e106c60b7be4 (bug 1607212) for tsan fails on CacheEntry.cpp. CLOSED TREE
    UltraBlame original commit: 8a55b81e0bdc7fa79ef9ee96cdcfc3d1eac61566

--- mozglue/build/TsanOptions.cpp
+++ mozglue/build/TsanOptions.cpp
@@ -179,6 +179,9 @@ extern "C" const char* __tsan_default_suppressions() {
          "race:nsSocketTransport::OnSocketDetached\n"
 
          
+         "race:CacheEntry::InvokeCallback\n"
+
+         
          "race:gXPCOMThreadsShutDown\n"
 
          
@@ -233,6 +236,10 @@ extern "C" const char* __tsan_default_suppressions() {
          
          "race:_dl_deallocate_tls\n"
          "race:__libc_memalign\n"
+
+         
+         "race:CacheEntry::Purge\n"
+         "race:CacheEntry::MetaDataReady\n"
 
          
          "race:ScriptPreloader::OffThreadDecodeCallback\n"
