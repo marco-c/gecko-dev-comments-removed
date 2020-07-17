426bcba78ae1b13a353111cb54a040cc57bab908
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-07-17 01:01:39 +0000

    Bug 1649121: part 20) Move `GetNewCaretPointAfterInsertingHTML` to `HTMLWithContextInserter`. r=masayuki
    Differential Revision: https://phabricator.services.mozilla.com/D82385
    
    UltraBlame original commit: 11acdf44634d703714cbe2fcf1ff1b9a7a7e8949

--- editor/libeditor/HTMLEditorDataTransfer.cpp
+++ editor/libeditor/HTMLEditorDataTransfer.cpp
@@ -217,17 +217,22 @@ class MOZ_STACK_CLASS HTMLEditor::HTMLWithContextInserter final {
                                   bool aClearStyle);
 
  private:
+  EditorDOMPoint GetNewCaretPointAfterInsertingHTML(
+      const EditorDOMPoint& aLastInsertedPoint) const;
+
   HTMLEditor& mHTMLEditor;
 };
 
-EditorDOMPoint HTMLEditor::GetNewCaretPointAfterInsertingHTML(
+EditorDOMPoint
+HTMLEditor::HTMLWithContextInserter::GetNewCaretPointAfterInsertingHTML(
     const EditorDOMPoint& aLastInsertedPoint) const {
   EditorDOMPoint pointToPutCaret;
 
   
   nsIContent* containerContent = nullptr;
   if (!HTMLEditUtils::IsTable(aLastInsertedPoint.GetChild())) {
-    containerContent = GetLastEditableLeaf(*aLastInsertedPoint.GetChild());
+    containerContent =
+        mHTMLEditor.GetLastEditableLeaf(*aLastInsertedPoint.GetChild());
     if (containerContent) {
       Element* mostDistantInclusiveAncestorTableElement = nullptr;
       for (Element* maybeTableElement =
@@ -269,13 +274,15 @@ EditorDOMPoint HTMLEditor::GetNewCaretPointAfterInsertingHTML(
 
   
   
-  WSRunScanner wsRunScannerAtCaret(this, pointToPutCaret);
+  WSRunScanner wsRunScannerAtCaret(&mHTMLEditor, pointToPutCaret);
   if (wsRunScannerAtCaret
           .ScanPreviousVisibleNodeOrBlockBoundaryFrom(pointToPutCaret)
           .ReachedBRElement() &&
-      !IsVisibleBRElement(wsRunScannerAtCaret.GetStartReasonContent())) {
+      !mHTMLEditor.IsVisibleBRElement(
+          wsRunScannerAtCaret.GetStartReasonContent())) {
     WSRunScanner wsRunScannerAtStartReason(
-        this, EditorDOMPoint(wsRunScannerAtCaret.GetStartReasonContent()));
+        &mHTMLEditor,
+        EditorDOMPoint(wsRunScannerAtCaret.GetStartReasonContent()));
     WSScanResult backwardScanFromPointToCaretResult =
         wsRunScannerAtStartReason.ScanPreviousVisibleNodeOrBlockBoundaryFrom(
             pointToPutCaret);
@@ -791,7 +798,7 @@ nsresult HTMLEditor::HTMLWithContextInserter::Run(
   }
 
   const EditorDOMPoint pointToPutCaret =
-      mHTMLEditor.GetNewCaretPointAfterInsertingHTML(lastInsertedPoint);
+      GetNewCaretPointAfterInsertingHTML(lastInsertedPoint);
   
   rv = MOZ_KnownLive(mHTMLEditor).CollapseSelectionTo(pointToPutCaret);
   if (NS_WARN_IF(rv == NS_ERROR_EDITOR_DESTROYED)) {
