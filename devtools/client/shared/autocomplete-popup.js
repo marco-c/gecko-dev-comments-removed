c1e69155f18549cfc015924055cf9831aff4a6d4
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-08-01 00:56:32 +0000

    Bug 1492797 - Allow to pass an input to the autocomplete constructor. r=rcaliman.
    This will automatically add all the necessary event listeners
    for the autocomplete (navigating, selecting an item, closing).
    The input will also be used as a default anchor when showing
    the popup.
    
    Differential Revision: https://phabricator.services.mozilla.com/D72859
    
    UltraBlame original commit: 3ecf0fc44704f8cafa2fbaaef6853e3f3041a5b4

--- devtools/client/shared/autocomplete-popup.js
+++ devtools/client/shared/autocomplete-popup.js
@@ -35,6 +35,9 @@ let itemIdCounter = 0;
 
 
 
+
+
+
 function AutocompletePopup(toolboxDoc, options = {}) {
   EventEmitter.decorate(this);
 
@@ -55,6 +58,14 @@ function AutocompletePopup(toolboxDoc, options = {}) {
   this.selectedIndex = -1;
 
   this.onClick = this.onClick.bind(this);
+  this.onInputKeyDown = this.onInputKeyDown.bind(this);
+  this.onInputBlur = this.onInputBlur.bind(this);
+
+  if (options.input) {
+    this.input = options.input;
+    options.input.addEventListener("keydown", this.onInputKeyDown);
+    options.input.addEventListener("blur", this.onInputBlur);
+  }
 }
 
 AutocompletePopup.prototype = {
@@ -115,6 +126,66 @@ AutocompletePopup.prototype = {
     return this._tooltip;
   },
 
+  onInputKeyDown: function(event) {
+    
+    if (!this.isOpen) {
+      return;
+    }
+
+    if (
+      this.selectedItem &&
+      this.onClickCallback &&
+      (event.key === "Enter" ||
+        (event.key === "ArrowRight" && !event.shiftKey) ||
+        (event.key === "Tab" && !event.shiftKey))
+    ) {
+      this.onClickCallback(event, this.selectedItem);
+
+      
+      event.preventDefault();
+      event.stopPropagation();
+      return;
+    }
+
+    
+    
+    if (event.key === "ArrowLeft" && !event.shiftKey) {
+      this.clearItems();
+      this.hidePopup();
+      return;
+    }
+
+    
+    if (event.key === "Escape") {
+      this.clearItems();
+      this.hidePopup();
+      
+      event.preventDefault();
+      event.stopPropagation();
+      return;
+    }
+
+    if (event.key === "ArrowDown") {
+      this.selectNextItem();
+      event.preventDefault();
+      event.stopPropagation();
+      return;
+    }
+
+    if (event.key === "ArrowUp") {
+      this.selectPreviousItem();
+      event.preventDefault();
+      event.stopPropagation();
+    }
+  },
+
+  onInputBlur: function(event) {
+    if (this.isOpen) {
+      this.clearItems();
+      this.hidePopup();
+    }
+  },
+
   onSelect: function(e) {
     if (this.onSelectCallback) {
       this.onSelectCallback(e);
@@ -122,14 +193,22 @@ AutocompletePopup.prototype = {
   },
 
   onClick: function(e) {
-    const item = e.target.closest(".autocomplete-item");
-    if (item && typeof item.dataset.index !== "undefined") {
-      this.selectItemAtIndex(parseInt(item.dataset.index, 10));
+    const itemEl = e.target.closest(".autocomplete-item");
+    const index = itemEl?.dataset?.index;
+
+    if (typeof index !== "undefined") {
+      this.selectItemAtIndex(index);
     }
 
     this.emit("popup-click");
+
+    const item =
+      typeof index !== "undefined"
+        ? this.items[parseInt(itemEl.dataset.index, 10)]
+        : null;
+
     if (this.onClickCallback) {
-      this.onClickCallback(e);
+      this.onClickCallback(e, item);
     }
   },
 
@@ -149,6 +228,10 @@ AutocompletePopup.prototype = {
 
 
   openPopup: async function(anchor, xOffset = 0, yOffset = 0, index, options) {
+    if (!anchor && this.input) {
+      anchor = this.input;
+    }
+
     
     this._activeElement = anchor.ownerDocument.activeElement;
 
@@ -270,6 +353,12 @@ AutocompletePopup.prototype = {
     if (this._tooltip) {
       this._tooltip.destroy();
       this._tooltip = null;
+    }
+
+    if (this.input) {
+      this.input.addEventListener("keydown", this.onInputKeyDown);
+      this.input.addEventListener("blur", this.onInputBlur);
+      this.input = null;
     }
 
     this._document = null;
