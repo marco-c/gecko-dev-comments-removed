825e6c09f8159e35c46240a0d950bff418505458
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-08-01 01:41:01 +0000

    Bug 1651297 - [marionette] Improve debug logging for paintComplete in reftests. r=marionette-reviewers,jgraham,maja_zf
    Depends on D84525
    
    Differential Revision: https://phabricator.services.mozilla.com/D84526
    
    UltraBlame original commit: 18049c38b415c37c63d1cc3ca0e15755d83e64d0

--- testing/marionette/listener.js
+++ testing/marionette/listener.js
@@ -1793,13 +1793,16 @@ function paintComplete(win, remote) {
       flushRendering();
       if (remote) {
         
+        logger.debug("Force update of layer tree");
         windowUtils.updateLayerTree();
       }
+
       if (windowUtils.isMozAfterPaintPending) {
-        logger.debug(`reftestWait: ${windowUtils.isMozAfterPaintPending}`);
+        logger.debug("isMozAfterPaintPending: true");
         win.addEventListener("MozAfterPaint", maybeResolve, { once: true });
       } else {
         
+        logger.debug("isMozAfterPaintPending: false");
         win.requestAnimationFrame(() => {
           win.requestAnimationFrame(resolve);
         });
