3f3912061f0d067d46a3d3e776b1db2da0c405b0
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-08-01 00:59:24 +0000

    Backed out 3 changesets (bug 1653642, bug 1497249) for causing Bug 1654282.
    Backed out changeset 1c9946becbca (bug 1653642)
    Backed out changeset f7b6aac1b58b (bug 1497249)
    Backed out changeset 74bb3c2ab767 (bug 1497249)
    
    UltraBlame original commit: d72591a16d42a6cefcffe3df901fa9d0db440193

--- netwerk/protocol/http/nsHttpChannel.cpp
+++ netwerk/protocol/http/nsHttpChannel.cpp
@@ -7942,13 +7942,6 @@ nsresult nsHttpChannel::ContinueOnStopRequestAfterAuthRetry(
                         mResponseHead->Status() == 200;
 
   if (upgradeWebsocket || upgradeConnect) {
-    
-    if (nsIOService::UseSocketProcess() && upgradeConnect) {
-      Unused << mUpgradeProtocolCallback->OnUpgradeFailed(
-          NS_ERROR_NOT_IMPLEMENTED);
-      return ContinueOnStopRequest(aStatus, aIsFromNet, aContentComplete);
-    }
-
     nsresult rv = gHttpHandler->CompleteUpgrade(aTransWithStickyConn,
                                                 mUpgradeProtocolCallback);
     if (NS_FAILED(rv)) {
