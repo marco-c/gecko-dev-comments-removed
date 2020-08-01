0d8c75036fd6019de35027365e3fdb0cf51426e4
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-08-01 01:27:13 +0000

    Bug 1655398 - Don't move the client area rect for the root scroll frame. r=mats
    The spec really asks us to put the rect at GetUsedBorder().{top,left} in
    this case, but I don't think that really makes sense, see
    https://github.com/w3c/csswg-drafts/issues/5363.
    
    Differential Revision: https://phabricator.services.mozilla.com/D84959
    
    UltraBlame original commit: 1fa3c63f82d23ea49809c70a74bea90a8fd6f426

--- dom/base/Element.cpp
+++ dom/base/Element.cpp
@@ -860,8 +860,10 @@ nsRect Element::GetClientAreaRect() {
     MOZ_ASSERT(frame);
     nsRect scrollPort = sf->GetScrollPortRect();
     nsIFrame* scrollableAsFrame = do_QueryFrame(sf);
-    if (frame != scrollableAsFrame) {
-      
+    
+    
+    
+    if (frame != scrollableAsFrame && !sf->IsRootScrollFrameOfDocument()) {
       scrollPort.MoveBy(scrollableAsFrame->GetOffsetTo(frame));
     }
     
