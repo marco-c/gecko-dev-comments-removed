48b23c68d4eca14c6c7af22372b06431585cf061
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-08-01 00:58:56 +0000

    Bug 1653433 Part 2 - Use CUPS header in the CUPSShim to get struct declarations and function signatures. r=emilio
    Differential Revision: https://phabricator.services.mozilla.com/D84033
    
    UltraBlame original commit: dd8af8363ca562debf7dfe8256938d0baeabdbd4

--- widget/nsCUPSShim.h
+++ widget/nsCUPSShim.h
@@ -7,36 +7,7 @@
 #ifndef nsCUPSShim_h___
 #define nsCUPSShim_h___
 
-
-
-
-
-typedef struct 
-{
-  char* name;  
-  char* value; 
-} cups_option_t;
-
-typedef struct 
-{
-  char *name,             
-      *instance;          
-  int is_default;         
-  int num_options;        
-  cups_option_t* options; 
-} cups_dest_t;
-
-typedef cups_dest_t* (*CupsGetDestType)(const char* printer,
-                                        const char* instance, int num_dests,
-                                        cups_dest_t* dests);
-typedef int (*CupsGetDestsType)(cups_dest_t** dests);
-typedef int (*CupsFreeDestsType)(int num_dests, cups_dest_t* dests);
-typedef int (*CupsPrintFileType)(const char* printer, const char* filename,
-                                 const char* title, int num_options,
-                                 cups_option_t* options);
-typedef int (*CupsTempFdType)(char* filename, int length);
-typedef int (*CupsAddOptionType)(const char* name, const char* value,
-                                 int num_options, cups_option_t** options);
+#include <cups/cups.h>
 
 struct PRLibrary;
 
@@ -62,12 +33,12 @@ class nsCUPSShim {
   
 
 
-  CupsAddOptionType mCupsAddOption;
-  CupsFreeDestsType mCupsFreeDests;
-  CupsGetDestType mCupsGetDest;
-  CupsGetDestsType mCupsGetDests;
-  CupsPrintFileType mCupsPrintFile;
-  CupsTempFdType mCupsTempFd;
+  decltype(cupsAddOption)* mCupsAddOption;
+  decltype(cupsFreeDests)* mCupsFreeDests;
+  decltype(cupsGetDest)* mCupsGetDest;
+  decltype(cupsGetDests)* mCupsGetDests;
+  decltype(cupsPrintFile)* mCupsPrintFile;
+  decltype(cupsTempFd)* mCupsTempFd;
 
  private:
   bool mInited = false;
