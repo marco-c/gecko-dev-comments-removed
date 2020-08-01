d028a0ba612a9195420d393522c4ea0756140082
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-08-01 00:59:18 +0000

    Bug 1631460 - Render margin-indicator guides on each page of print preview if the nsIPrintSettings.showMarginGuides flag is set. r=dholbert
    Differential Revision: https://phabricator.services.mozilla.com/D84159
    
    UltraBlame original commit: 1b33cf4206f6a303ac860c1a94068f6bb35b2321

--- layout/generic/nsPageFrame.h
+++ layout/generic/nsPageFrame.h
@@ -45,6 +45,7 @@ class nsPageFrame final : public nsContainerFrame {
   void SetPageNumInfo(int32_t aPageNumber, int32_t aTotalPages);
 
   void SetSharedPageData(nsSharedPageData* aPD);
+  nsSharedPageData* GetSharedPageData() const { return mPD; }
 
   
   
