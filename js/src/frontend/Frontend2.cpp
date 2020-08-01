b3b7f8b3f1d1b9be4717a0cdc55599a736e0929c
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-08-01 01:33:53 +0000

    Bug 1650023 - Part 2: Support function-related scopes. r=nbp
    Support function scope, and extra body var scope.
    
    Differential Revision: https://phabricator.services.mozilla.com/D85221
    
    UltraBlame original commit: 4799f2dbd889e783983d6a61728be81caeddfc91

--- js/src/frontend/Frontend2.cpp
+++ js/src/frontend/Frontend2.cpp
@@ -23,10 +23,10 @@
 #include "frontend/Parser.h"  
 #include "frontend/smoosh_generated.h"  
 #include "frontend/SourceNotes.h"       
-#include "frontend/Stencil.h"           
-#include "frontend/TokenStream.h"       
-#include "gc/Rooting.h"                 
-#include "irregexp/RegExpAPI.h"         
+#include "frontend/Stencil.h"  
+#include "frontend/TokenStream.h"  
+#include "gc/Rooting.h"            
+#include "irregexp/RegExpAPI.h"    
 #include "js/CharacterEncoding.h"  
 #include "js/GCAPI.h"              
 #include "js/GCVector.h"           
@@ -92,6 +92,24 @@ void CopyBindingNames(JSContext* cx, CVec<SmooshBindingName>& from,
   }
 }
 
+void CopyBindingNames(JSContext* cx, CVec<COption<SmooshBindingName>>& from,
+                      JS::HandleVector<JSAtom*> allAtoms, BindingName* to) {
+  
+  JS::AutoCheckCannotGC nogc(cx);
+
+  size_t numBindings = from.len;
+  for (size_t i = 0; i < numBindings; i++) {
+    COption<SmooshBindingName>& maybeName = from.data[i];
+    if (maybeName.IsSome()) {
+      SmooshBindingName& name = maybeName.AsSome();
+      new (mozilla::KnownNotNull, &to[i]) BindingName(
+          allAtoms[name.name], name.is_closed_over, name.is_top_level_function);
+    } else {
+      new (mozilla::KnownNotNull, &to[i]) BindingName(nullptr, false, false);
+    }
+  }
+}
+
 
 
 bool ConvertScopeCreationData(JSContext* cx, const SmooshResult& result,
@@ -127,6 +145,36 @@ bool ConvertScopeCreationData(JSContext* cx, const SmooshResult& result,
         }
         break;
       }
+      case SmooshScopeData::Tag::Var: {
+        auto& var = scopeData.AsVar();
+
+        size_t numBindings = var.bindings.len;
+
+        JS::Rooted<VarScope::Data*> data(
+            cx, NewEmptyVarScopeData(cx, alloc, numBindings));
+        if (!data) {
+          return false;
+        }
+
+        CopyBindingNames(cx, var.bindings, allAtoms,
+                         data->trailingNames.start());
+
+        
+
+        data->length = numBindings;
+
+        uint32_t firstFrameSlot = var.first_frame_slot;
+        ScopeIndex enclosingIndex(var.enclosing);
+        Rooted<AbstractScopePtr> enclosing(
+            cx, AbstractScopePtr(compilationInfo, enclosingIndex));
+        if (!ScopeCreationData::create(
+                cx, compilationInfo, ScopeKind::FunctionBodyVar, data,
+                firstFrameSlot, var.function_has_extensible_scope, enclosing,
+                &index)) {
+          return false;
+        }
+        break;
+      }
       case SmooshScopeData::Tag::Lexical: {
         auto& lexical = scopeData.AsLexical();
 
@@ -152,6 +200,42 @@ bool ConvertScopeCreationData(JSContext* cx, const SmooshResult& result,
         if (!ScopeCreationData::create(cx, compilationInfo, ScopeKind::Lexical,
                                        data, firstFrameSlot, enclosing,
                                        &index)) {
+          return false;
+        }
+        break;
+      }
+      case SmooshScopeData::Tag::Function: {
+        auto& function = scopeData.AsFunction();
+
+        size_t numBindings = function.bindings.len;
+        JS::Rooted<FunctionScope::Data*> data(
+            cx, NewEmptyFunctionScopeData(cx, alloc, numBindings));
+        if (!data) {
+          return false;
+        }
+
+        CopyBindingNames(cx, function.bindings, allAtoms,
+                         data->trailingNames.start());
+
+        
+
+        data->hasParameterExprs = function.has_parameter_exprs;
+        data->nonPositionalFormalStart = function.non_positional_formal_start;
+        data->varStart = function.var_start;
+        data->length = numBindings;
+
+        bool hasParameterExprs = function.has_parameter_exprs;
+        bool needsEnvironment = function.non_positional_formal_start;
+        
+        FunctionIndex functionIndex = FunctionIndex(function.function_index);
+        bool isArrow = function.is_arrow;
+
+        ScopeIndex enclosingIndex(function.enclosing);
+        Rooted<AbstractScopePtr> enclosing(
+            cx, AbstractScopePtr(compilationInfo, enclosingIndex));
+        if (!ScopeCreationData::create(
+                cx, compilationInfo, data, hasParameterExprs, needsEnvironment,
+                functionIndex, isArrow, enclosing, &index)) {
           return false;
         }
         break;
