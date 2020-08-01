b580a19c4e132e286c191a870517c344c041fe4f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-08-01 01:00:35 +0000

    Bug 1654555 - Import v13.0.1 webcompat addon sources. r=webcompat-reviewers,miketaylr
    Differential Revision: https://phabricator.services.mozilla.com/D84544
    
    UltraBlame original commit: 2c1d74341949f8f34ce5f62ab05e3b2b2343d6c1

--- browser/extensions/webcompat/data/ua_overrides.js
+++ browser/extensions/webcompat/data/ua_overrides.js
@@ -581,13 +581,13 @@ const AVAILABLE_UA_OVERRIDES = [
 
 
 
-    id: "bug1563839",
+    id: "bug1646791",
     platform: "all",
     domain: "santanderbank.com",
-    bug: "1563839",
+    bug: "1646791",
     config: {
       matches: [
-        "*://*.bancosantander.es*",
+        "*://*.bancosantander.es/*",
         "*://*.santander.co.uk/*",
         "*://bob.santanderbank.com/*",
         "*://rolb.santanderbank.com/*",
