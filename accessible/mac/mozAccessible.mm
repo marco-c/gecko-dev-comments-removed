bf2a3607bdee881978f60b171b84982d835680b3
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-08-01 01:05:03 +0000

    Bug 1654679 - Add selected text marker range to text selection changed events. r=morgan
    Differential Revision: https://phabricator.services.mozilla.com/D84616
    
    UltraBlame original commit: 47667fbae73407a94c4f8e359814b468ad96a07d

--- accessible/mac/mozAccessible.mm
+++ accessible/mac/mozAccessible.mm
@@ -840,10 +840,21 @@ enum AXTextStateChangeType {
     case nsIAccessibleEvent::EVENT_SELECTION_WITHIN:
       [self moxPostNotification:NSAccessibilitySelectedChildrenChangedNotification];
       break;
-    case nsIAccessibleEvent::EVENT_TEXT_CARET_MOVED:
-    case nsIAccessibleEvent::EVENT_TEXT_SELECTION_CHANGED:
-      [self moxPostNotification:NSAccessibilitySelectedTextChangedNotification];
+    case nsIAccessibleEvent::EVENT_TEXT_CARET_MOVED: {
+      
+      
+      id<MOXTextMarkerSupport> delegate =  [self moxTextMarkerDelegate];
+      id selectedRange = [delegate moxSelectedTextMarkerRange];
+      NSDictionary* userInfo = @{
+        @"AXTextChangeElement": self,
+        @"AXSelectedTextMarkerRange": (selectedRange ? selectedRange : [NSNull null])
+      };
+
+      mozAccessible* webArea = GetNativeFromGeckoAccessible([self geckoDocument]);
+      [webArea moxPostNotification:NSAccessibilitySelectedTextChangedNotification withUserInfo:userInfo];
+      [self moxPostNotification:NSAccessibilitySelectedTextChangedNotification withUserInfo:userInfo];
       break;
+    }
   }
 }
 
