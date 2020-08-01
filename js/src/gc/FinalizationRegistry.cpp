90ed070c110f3667f9aa7ea066a994f4fd2e9717
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-08-01 00:41:34 +0000

    Bug 1648453 - Record incumbent global in FinalizationRegistry constructor and use when calling back into the JS engine to call callbacks r=sfink
    This changes the way the the host triggers cleanup, from calling an API to calling a JS function. This is done so we can use the existing DOM infrastructure that handles setting up the incumbent global for us.
    
    Differential Revision: https://phabricator.services.mozilla.com/D83613
    
    UltraBlame original commit: 623252539387bff544d97309104c7006d00c99f7

--- js/src/gc/FinalizationRegistry.cpp
+++ js/src/gc/FinalizationRegistry.cpp
@@ -118,14 +118,8 @@ void GCRuntime::queueFinalizationRegistryForCleanup(
     FinalizationRegistryObject* registry) {
   
   if (!registry->isQueuedForCleanup()) {
-    callHostCleanupFinalizationRegistryCallback(registry);
+    callHostCleanupFinalizationRegistryCallback(registry->doCleanupFunction(),
+                                                registry->incumbentGlobal());
     registry->setQueuedForCleanup(true);
   }
-}
-
-bool GCRuntime::cleanupQueuedFinalizationRegistry(
-    JSContext* cx, HandleFinalizationRegistryObject registry) {
-  registry->setQueuedForCleanup(false);
-  bool ok = FinalizationRegistryObject::cleanupQueuedRecords(cx, registry);
-  return ok;
 }
