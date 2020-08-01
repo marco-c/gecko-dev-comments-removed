cbc680a9e90eec84a194117ed4f2e67a61ec3651
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-08-01 00:59:40 +0000

    Bug 1652685 part 3 - Optimize IsPackedArray in CacheIR and Warp. r=anba
    Differential Revision: https://phabricator.services.mozilla.com/D84331
    
    UltraBlame original commit: b32115de87fc07f847ab67d6bf15188eef812163

--- js/src/jit/CacheIR.cpp
+++ js/src/jit/CacheIR.cpp
@@ -5404,6 +5404,30 @@ AttachDecision CallIRGenerator::tryAttachIsObject(HandleFunction callee) {
   return AttachDecision::Attach;
 }
 
+AttachDecision CallIRGenerator::tryAttachIsPackedArray(HandleFunction callee) {
+  
+  MOZ_ASSERT(argc_ == 1);
+  MOZ_ASSERT(args_[0].isObject());
+
+  
+  Int32OperandId argcId(writer.setInputOperandId(0));
+
+  
+
+  
+  ValOperandId argId = writer.loadArgumentFixedSlot(ArgumentKind::Arg0, argc_);
+  ObjOperandId objArgId = writer.guardToObject(argId);
+  writer.isPackedArrayResult(objArgId);
+
+  
+  
+  writer.returnFromIC();
+  cacheIRStubKind_ = BaselineCacheIRStubKind::Regular;
+
+  trackAttached("IsPackedArray");
+  return AttachDecision::Attach;
+}
+
 AttachDecision CallIRGenerator::tryAttachIsCallable(HandleFunction callee) {
   
   if (argc_ != 1) {
@@ -6477,6 +6501,8 @@ AttachDecision CallIRGenerator::tryAttachInlinableNative(
       return tryAttachToLength(callee);
     case InlinableNative::IntrinsicIsObject:
       return tryAttachIsObject(callee);
+    case InlinableNative::IntrinsicIsPackedArray:
+      return tryAttachIsPackedArray(callee);
     case InlinableNative::IntrinsicIsCallable:
       return tryAttachIsCallable(callee);
     case InlinableNative::IntrinsicIsConstructor:
