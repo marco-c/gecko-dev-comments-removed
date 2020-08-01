5f66881dff1c5fefb00397e0d67db88cb93be4d0
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-08-01 01:23:32 +0000

    Bug 1635260 - Update vendored glean_parser to 1.28.0 r=janerik
    Using ./mach vendor python glean_parser==1.28.0
    
    (this is the latest version)
    
    Differential Revision: https://phabricator.services.mozilla.com/D84746
    
    UltraBlame original commit: 62dd07b3dcdade1029212944591b509d6554184a

--- third_party/python/appdirs/appdirs.py
+++ third_party/python/appdirs/appdirs.py
@@ -13,8 +13,8 @@ See <http://github.com/ActiveState/appdirs> for details and usage.
 
 
 
-__version_info__ = (1, 4, 3)
-__version__ = '.'.join(map(str, __version_info__))
+__version__ = "1.4.4"
+__version_info__ = tuple(int(segment) for segment in __version__.split("."))
 
 
 import sys
