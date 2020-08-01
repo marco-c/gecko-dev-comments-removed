38c3fcac1c033f3901f01bd6c9ac3cb1845a8f70
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-08-01 00:51:22 +0000

    Bug 1644807 - Switch to Primary Password user-facing terminology in security/ .ftl files. r=keeler,fluent-reviewers,Pike
    Differential Revision: https://phabricator.services.mozilla.com/D83898
    
    UltraBlame original commit: 78ef46e35e654cb70769606332cce0b7b3c89ca8

--- security/manager/pki/resources/content/device_manager.js
+++ security/manager/pki/resources/content/device_manager.js
@@ -414,7 +414,7 @@ function toggleFIPS() {
     var internal_token = tokendb.getInternalKeyToken(); 
     if (!internal_token.hasPassword) {
       
-      doPrompt("fips-nonempty-password-required");
+      doPrompt("fips-nonempty-primary-password-required");
       return;
     }
   }
