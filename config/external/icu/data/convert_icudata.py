c4cd3055ce34b8530f1eb5f71cd67749818bd235
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-08-01 00:48:37 +0000

    Bug 1264836 - Automatically convert the little-endian ICU data file for big-endian builds. r=firefox-build-system-reviewers,rstewart
    Differential Revision: https://phabricator.services.mozilla.com/D83857
    
    UltraBlame original commit: d3004626988daf5515087538a64ed671d984f0cb

---
+++ config/external/icu/data/convert_icudata.py
@@ -0,0 +1,17 @@
+
+
+
+
+import os
+import subprocess
+import buildconfig
+
+
+def main(output, data_file):
+    output.close()
+    subprocess.run([
+        os.path.join(buildconfig.topobjdir, 'dist', 'host', 'bin', 'icupkg'),
+        '-tb',
+        data_file,
+        output.name,
+    ])
