367e6654e2a28c52f74441ca422f4774f8836138
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-08-01 00:46:23 +0000

    Bug 1647771 - Import v13.0.0 webcompat addon sources. r=miketaylr,webcompat-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D83875
    
    UltraBlame original commit: 02b3e5d7b0be2cd316e05105e9a48a8b951bdeb7

--- browser/extensions/webcompat/data/ua_overrides.js
+++ browser/extensions/webcompat/data/ua_overrides.js
@@ -275,29 +275,6 @@ const AVAILABLE_UA_OVERRIDES = [
 
 
 
-    id: "bug1508516",
-    platform: "android",
-    domain: "cineflix.com.br",
-    bug: "1508516",
-    config: {
-      matches: ["*://*.cineflix.com.br/m/*"],
-      uaTransformer: originalUA => {
-        return (
-          UAHelpers.getPrefix(originalUA) +
-          " AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.91 Mobile Safari/537.36"
-        );
-      },
-    },
-  },
-  {
-    
-
-
-
-
-
-
-
 
     id: "bug1509873",
     platform: "android",
@@ -617,6 +594,26 @@ const AVAILABLE_UA_OVERRIDES = [
       ],
       uaTransformer: originalUA => {
         return originalUA.replace("Gecko", "like Gecko");
+      },
+    },
+  },
+  {
+    
+
+
+
+
+
+
+
+    id: "bug1651292",
+    platform: "android",
+    domain: "www.jp.square-enix.com",
+    bug: "1651292",
+    config: {
+      matches: ["*://www.jp.square-enix.com/music/sem/page/FF7R/ost/*"],
+      uaTransformer: originalUA => {
+        return originalUA + " Chrome/83";
       },
     },
   },
