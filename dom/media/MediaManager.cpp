9a7979d3714f2fc37d469b1ca05538916d869d67
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-08-01 01:28:54 +0000

    Bug 1654430 - Replace 200ms mainthread sleep on inserting/removing USB/bluetooth cam/mic/speaker with WaitFor() promise. r=achronop
    Differential Revision: https://phabricator.services.mozilla.com/D84458
    
    UltraBlame original commit: 7e2fb6f29963825f09c5a75aac370a3c3fef2301

--- dom/media/MediaManager.cpp
+++ dom/media/MediaManager.cpp
@@ -2156,14 +2156,38 @@ void MediaManager::DeviceListChanged() {
 
   
   
-  PR_Sleep(PR_MillisecondsToInterval(200));
+
+  if (mDeviceChangeTimer) {
+    mDeviceChangeTimer->Cancel();
+  } else {
+    mDeviceChangeTimer = MakeRefPtr<MediaTimer>();
+  }
+  RefPtr<MediaManager> self = this;
   auto devices = MakeRefPtr<MediaDeviceSetRefCnt>();
-  EnumerateRawDevices(0, MediaSourceEnum::Camera, MediaSourceEnum::Microphone,
-                      MediaSinkEnum::Speaker, DeviceEnumerationType::Normal,
-                      DeviceEnumerationType::Normal, false, devices)
+  mDeviceChangeTimer->WaitFor(TimeDuration::FromMilliseconds(200), __func__)
       ->Then(
           GetCurrentSerialEventTarget(), __func__,
-          [self = RefPtr<MediaManager>(this), this, devices](bool) {
+          [self, this, devices]() {
+            if (!MediaManager::GetIfExists()) {
+              return MgrPromise::CreateAndReject(
+                  MakeRefPtr<MediaMgrError>(MediaMgrError::Name::AbortError,
+                                            u"In shutdown"_ns),
+                  __func__);
+            }
+            return EnumerateRawDevices(
+                0, MediaSourceEnum::Camera, MediaSourceEnum::Microphone,
+                MediaSinkEnum::Speaker, DeviceEnumerationType::Normal,
+                DeviceEnumerationType::Normal, false, devices);
+          },
+          []() {
+            
+            return MgrPromise::CreateAndReject(
+                MakeRefPtr<MediaMgrError>(MediaMgrError::Name::AbortError),
+                __func__);
+          })
+      ->Then(
+          GetCurrentSerialEventTarget(), __func__,
+          [self, this, devices](bool) {
             if (!MediaManager::GetIfExists()) {
               return;
             }
@@ -3538,6 +3562,12 @@ void MediaManager::Shutdown() {
                           this);
     prefs->RemoveObserver("media.getusermedia.channels", this);
 #endif
+  }
+
+  if (mDeviceChangeTimer) {
+    mDeviceChangeTimer->Cancel();
+    
+    mDeviceChangeTimer = nullptr;
   }
 
   {
