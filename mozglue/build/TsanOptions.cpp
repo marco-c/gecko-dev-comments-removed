63dcd812ba3dab85b4cb527c9e963e8659c28a6e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-08-01 01:00:14 +0000

    Bug 1648604: Suppress benign race in libusrsctp's system_base_info. r=decoder
    Differential Revision: https://phabricator.services.mozilla.com/D84364
    
    UltraBlame original commit: cbebb2f9e93f6e14856e20396356fe70949343bc

--- mozglue/build/TsanOptions.cpp
+++ mozglue/build/TsanOptions.cpp
@@ -302,6 +302,9 @@ extern "C" const char* __tsan_default_suppressions() {
          "race:libavcodec.so*\n"
          "race:libavutil.so*\n"
 
+         
+         "race:system_base_info\n"
+
       
       ;  
 }
