88f20233bf73d21fe852c374a89dfc0e34a38c1f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-08-01 01:01:18 +0000

    Bug 1607092 - Add new quote from Book of Mozilla for GeckoView r=geckoview-reviewers,Pike,agi
    Differential Revision: https://phabricator.services.mozilla.com/D80575
    
    UltraBlame original commit: fc1e5e4d5ed830fa29d47a51ce2bfc8ec8bb39f1

--- mobile/locales/filter.py
+++ mobile/locales/filter.py
@@ -26,7 +26,6 @@ def test(mod, path, entity=None):
             "chrome/global/intl.css",
             "chrome/search/search.properties",
             "chrome/pluginproblem/pluginproblem.dtd",
-            "chrome/global/mozilla.dtd",
             "chrome/global/aboutWebrtc.properties",
         ):
             return "error"
@@ -35,6 +34,9 @@ def test(mod, path, entity=None):
             return "error"
 
         if re.match(r"toolkit/about/[^/]*About.ftl", path):
+            
+            return "error"
+        if re.match(r"toolkit/about/[^/]*Mozilla.ftl", path):
             
             return "error"
         if re.match(r"toolkit/about/[^/]*Plugins.ftl", path):
