196c2962ecd8396bcecf8f2316c64c2e450d735d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-08-01 01:15:58 +0000

    Bug 1652288 - Expand the RCD-RSF's ink overflow rect by the dynamic toolbar height in DescendIntoChild(). r=hiro
    Differential Revision: https://phabricator.services.mozilla.com/D84780
    
    UltraBlame original commit: 236c6d4f049c5c3bd509297c6a3a720cb8688181

--- layout/generic/nsIFrame.cpp
+++ layout/generic/nsIFrame.cpp
@@ -3850,7 +3850,20 @@ static bool DescendIntoChild(nsDisplayListBuilder* aBuilder,
     return true;
   }
 
-  const nsRect overflow = aChild->InkOverflowRect();
+  nsRect overflow = aChild->InkOverflowRect();
+
+  
+  
+  
+  
+  
+  if (aBuilder->IsForEventDelivery() &&
+      aChild == aChild->PresShell()->GetRootScrollFrame() &&
+      aChild->PresContext()->IsRootContentDocumentCrossProcess() &&
+      aChild->PresContext()->HasDynamicToolbar()) {
+    overflow.SizeTo(nsLayoutUtils::ExpandHeightForDynamicToolbar(
+        aChild->PresContext(), overflow.Size()));
+  }
 
   if (aDirty.Intersects(overflow)) {
     return true;
