e3dba391d7816fbeed5ca918809f09eb450095e9
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-08-01 01:29:37 +0000

    Bug 1652530: Suppress error due to libX11 not being instrumented. r=decoder
    Depends on D84391
    
    Differential Revision: https://phabricator.services.mozilla.com/D84404
    
    UltraBlame original commit: 1aa3ff231258b6efc89a17623b4f5aced9be50bc

--- mozglue/build/TsanOptions.cpp
+++ mozglue/build/TsanOptions.cpp
@@ -316,6 +316,9 @@ extern "C" const char* __tsan_default_suppressions() {
          "race:sctp_handle_tick\n"
          "race:sctp_handle_sack\n"
 
+         
+         "mutex:XErrorTrap\n"
+
       
       ;  
 }
