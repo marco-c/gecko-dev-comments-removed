349371caf6ae481dfd5a1bfb1ad4555a21b8d193
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-08-01 01:15:33 +0000

    Backed out 2 changesets (bug 1653392, bug 1648868) for causing failures in browser_modal_print.js CLOSED TREE
    Backed out changeset 7fac5bd02bb3 (bug 1653392)
    Backed out changeset 9f8b675f0f6b (bug 1648868)
    
    UltraBlame original commit: 4a5520ecb0752ea1264c0c580964b995cda4e191

--- toolkit/components/printing/content/printUtils.js
+++ toolkit/components/printing/content/printUtils.js
@@ -118,67 +118,15 @@ var PrintUtils = {
 
 
 
-  _tabModalTemplate() {
-    let template = document.getElementById("printTabModalTemplate");
-    if (template.localName == "template") {
-      template.replaceWith(template.content);
-      return document.getElementById("printTabModalTemplate");
-    }
-    return template;
-  },
-
-  
-
-
-
-
 
   _openTabModalPrint(aBrowsingContext) {
-    const { SubDialog } = ChromeUtils.import(
-      "resource://gre/modules/SubDialog.jsm"
-    );
-
-    let container = gBrowser.getBrowserContainer(
-      aBrowsingContext.embedderElement
-    );
-    if (container.querySelector(".printDialogContainer")) {
-      
-      return;
-    }
-
-    let dialog = new SubDialog({
-      id: `printModal${aBrowsingContext.id}`,
-      template: this._tabModalTemplate(),
-      parentElement: container,
-    });
-
-    
-    dialog._overlay._ready = dialog._frameCreated;
-    let resolveDialogClosed;
-    dialog._overlay._closed = new Promise(
-      resolve => (resolveDialogClosed = resolve)
-    );
-
-    
-    container.prepend(dialog._overlay);
-
-    dialog.open(
-      `chrome://global/content/print.html?browsingContextId=${aBrowsingContext.id}`,
-      null,
-      null,
-      async () => {
-        
-        
-        let overlay = dialog._overlay;
-        await dialog._closingPromise;
-        overlay.remove();
-
-        
-        dialog = null;
-        overlay._dialog = null;
-
-        
-        resolveDialogClosed();
+    let printPath = "chrome://global/content/print.html";
+    gBrowser.loadOneTab(
+      `${printPath}?browsingContextId=${aBrowsingContext.id}`,
+      {
+        inBackground: false,
+        relatedToCurrent: true,
+        triggeringPrincipal: Services.scriptSecurityManager.getSystemPrincipal(),
       }
     );
   },
@@ -290,7 +238,7 @@ var PrintUtils = {
 
   printPreview(aListenerObj) {
     if (PRINT_TAB_MODAL) {
-      this._openTabModalPrint(gBrowser.selectedBrowser.browsingContext);
+      this._openTabModalPrint(aListenerObj.getSourceBrowser().browsingContext);
       return;
     }
 
