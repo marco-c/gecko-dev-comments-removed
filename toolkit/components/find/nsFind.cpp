ebf1cc686bf89c2c34b989873435bbc237611946
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-08-01 01:00:21 +0000

    Bug 1654270 - Properly restart after a partial match if we reach the end of the document. r=jfkthame
    This was kind of a pre-existing bug, though not surfaced because we
    didn't have conditionally-appearing text nodes I guess.
    
    If we are doing a match in a non-anonymous subtree, we'd skip the
    anonymous text (that's alright). But if we actually reach the end of the
    document (as it is the case on the test that starts failing with the
    other patch in this bug), we'd just early-return, rather than restarting
    where we left off, which is not fine and means that potential matches in
    anonymous subtrees that were skipped won't show up.
    
    This fixes the bug... Eventually we should probably come up with a
    better structure than this gigantic while (true), probably moving all
    these variables into their own struct, but today is not that day :)
    
    Differential Revision: https://phabricator.services.mozilla.com/D84425
    
    UltraBlame original commit: aa337bc4d6693ad1d25236229eb982ebe468e138

--- toolkit/components/find/nsFind.cpp
+++ toolkit/components/find/nsFind.cpp
@@ -283,6 +283,10 @@ struct nsFind::State final {
     
     if (aAlreadyMatching && aPrev &&
         !nsContentUtils::IsInSameAnonymousTree(&aNode, aPrev)) {
+      DEBUG_FIND_PRINTF("Skipping due to different anon subtrees: ");
+      DumpNode(aPrev);
+      DEBUG_FIND_PRINTF("Next: ");
+      DumpNode(&aNode);
       return false;
     }
     return true;
@@ -546,8 +550,6 @@ char32_t nsFind::PeekNextChar(State& aState, bool aAlreadyMatching) const {
 #define IsSpace(c) (nsCRT::IsAsciiSpace(c) || (c) == NBSP_CHARCODE)
 #define OVERFLOW_PINDEX (mFindBackward ? pindex < 0 : pindex > patLen)
 #define DONE_WITH_PINDEX (mFindBackward ? pindex <= 0 : pindex >= patLen)
-#define ALMOST_DONE_WITH_PINDEX \
-  (mFindBackward ? pindex <= 0 : pindex >= patLen - 1)
 
 
 
@@ -631,13 +633,44 @@ nsFind::Find(const nsAString& aPatText, nsRange* aSearchRange,
   State state(mFindBackward, *root, *aStartPoint);
   Text* current = nullptr;
 
+  auto EndPartialMatch = [&] {
+    
+    
+    if (matchAnchorNode) {  
+      findex = matchAnchorOffset;
+      state.mIterOffset = matchAnchorOffset;
+      
+
+      
+      if (matchAnchorNode != state.GetCurrentNode()) {
+        frag = nullptr;
+        state.PositionAt(*matchAnchorNode);
+        DEBUG_FIND_PRINTF("Repositioned anchor node\n");
+      }
+      DEBUG_FIND_PRINTF(
+          "Ending a partial match; findex -> %d, mIterOffset -> %d\n", findex,
+          state.mIterOffset);
+    }
+    matchAnchorNode = nullptr;
+    matchAnchorOffset = 0;
+    inWhitespace = false;
+    pindex = mFindBackward ? patLen : 0;
+    DEBUG_FIND_PRINTF("Setting findex back to %d, pindex to %d\n", findex,
+                      pindex);
+  };
+
   while (true) {
-    DEBUG_FIND_PRINTF("Loop ...\n");
+    DEBUG_FIND_PRINTF("Loop (pindex = %d)...\n", pindex);
 
     
     if (!frag) {
       current = state.GetNextNode(!!matchAnchorNode);
       if (!current) {
+        DEBUG_FIND_PRINTF("Reached the end, matching: %d\n", !!matchAnchorNode);
+        if (matchAnchorNode) {
+          EndPartialMatch();
+          continue;
+        }
         return NS_OK;
       }
 
@@ -721,6 +754,7 @@ nsFind::Find(const nsAString& aPatText, nsRange* aSearchRange,
     if (state.GetCurrentNode() == endNode &&
         ((mFindBackward && findex < static_cast<int32_t>(endOffset)) ||
          (!mFindBackward && findex > static_cast<int32_t>(endOffset)))) {
+      DEBUG_FIND_PRINTF("Reached the end and not in the middle of a match\n");
       return NS_OK;
     }
 
@@ -921,30 +955,7 @@ nsFind::Find(const nsAString& aPatText, nsRange* aSearchRange,
     }
 
     DEBUG_FIND_PRINTF("NOT: %c == %c\n", c, patc);
-
-    
-    
-    if (matchAnchorNode) {  
-      findex = matchAnchorOffset;
-      state.mIterOffset = matchAnchorOffset;
-      
-
-      
-      if (matchAnchorNode != state.GetCurrentNode()) {
-        frag = nullptr;
-        state.PositionAt(*matchAnchorNode);
-        DEBUG_FIND_PRINTF("Repositioned anchor node\n");
-      }
-      DEBUG_FIND_PRINTF(
-          "Ending a partial match; findex -> %d, mIterOffset -> %d\n", findex,
-          state.mIterOffset);
-    }
-    matchAnchorNode = nullptr;
-    matchAnchorOffset = 0;
-    inWhitespace = false;
-    pindex = mFindBackward ? patLen : 0;
-    DEBUG_FIND_PRINTF("Setting findex back to %d, pindex to %d\n", findex,
-                      pindex);
+    EndPartialMatch();
   }
 
   return NS_OK;
