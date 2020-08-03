f8a744a49a7872b52368a9dc1020ec03fd8bee5a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-08-03 14:46:17 +0000

    Bug 1656421 - Use the viewport frame size for the clip rect if there is no root scroll frame. r=botond
    Differential Revision: https://phabricator.services.mozilla.com/D85548
    
    UltraBlame original commit: 0ffbb586b294704076249db41ee65d3afca720a1

--- gfx/layers/AnimationInfo.cpp
+++ gfx/layers/AnimationInfo.cpp
@@ -691,27 +691,35 @@ GetClipRectAndTransformForPartialPrerender(const nsIFrame* aFrame,
       
       scrollFrame = aFrame->PresShell()->GetRootScrollFrameAsScrollable();
     }
-    aClipFrame = do_QueryFrame(scrollFrame);
+    if (scrollFrame) {
+      aClipFrame = do_QueryFrame(scrollFrame);
+    } else {
+      
+      aClipFrame = aFrame->PresShell()->GetRootFrame();
+    }
   } else {
     scrollFrame = do_QueryFrame(aClipFrame);
   }
 
-  MOZ_ASSERT(scrollFrame);
   MOZ_ASSERT(aClipFrame);
 
   gfx::Matrix4x4 transformInClip =
       nsLayoutUtils::GetTransformToAncestor(RelativeTo{aFrame->GetParent()},
                                             RelativeTo{aClipFrame})
           .GetMatrix();
-  transformInClip.PostTranslate(
-      LayoutDevicePoint::FromAppUnits(scrollFrame->GetScrollPosition(),
-                                      aDevPixelsToAppUnits)
-          .ToUnknownPoint());
+  if (scrollFrame) {
+    transformInClip.PostTranslate(
+        LayoutDevicePoint::FromAppUnits(scrollFrame->GetScrollPosition(),
+                                        aDevPixelsToAppUnits)
+            .ToUnknownPoint());
+  }
 
   
   
   return std::make_pair(
-      LayoutDeviceRect::FromAppUnits(scrollFrame->GetScrollPortRect(),
+      LayoutDeviceRect::FromAppUnits(scrollFrame
+                                         ? scrollFrame->GetScrollPortRect()
+                                         : aClipFrame->GetRectRelativeToSelf(),
                                      aDevPixelsToAppUnits) *
           LayoutDeviceToLayerScale2D() * LayerToParentLayerScale(),
       transformInClip);
