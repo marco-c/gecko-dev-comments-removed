de21660e786990966013084d12806000eaccf1d7
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-08-06 18:20:38 +0000

    Bug 1656927 - Assertion failure: aContentType.Equals("image/avif"), at /builds/worker/checkouts/gecko/image/imgLoader.cpp:2785. r=aosmond
    Differential Revision: https://phabricator.services.mozilla.com/D85803
    
    UltraBlame original commit: ae8eeb6d2b88e763004ab683551546ed1ea84051

--- image/test/gtest/TestLoader.cpp
+++ image/test/gtest/TestLoader.cpp
@@ -87,6 +87,16 @@ TEST_F(ImageLoader, DetectAVIFCompatibleBrand) {
   CheckMimeType(buffer, sizeof(buffer), IMAGE_AVIF);
 }
 
+TEST_F(ImageLoader, DetectNonImageMP4) {
+  const char buffer[] =
+      "\x00\x00\x00\x1c"  
+      "ftyp"              
+      "isom"              
+      "\x00\x00\x02\x00"  
+      "isomiso2mp41";     
+  CheckMimeType(buffer, sizeof(buffer), nullptr);
+}
+
 TEST_F(ImageLoader, DetectNone) {
   const char buffer[] = "abcdefghijklmnop";
   CheckMimeType(buffer, sizeof(buffer), nullptr);
