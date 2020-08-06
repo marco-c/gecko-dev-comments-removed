214f35da28491cf4a7cc3ae77c5f31456ad559fc
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-08-06 18:24:43 +0000

    Backed out changeset 75af8f017ad1 (bug 1636050) for assertion failure at CacheStorageChild.cpp. CLOSED TREE
    UltraBlame original commit: d5de5110f595e4da3c15ee06440cbbf6b5c38691

--- dom/cache/CacheOpChild.h
+++ dom/cache/CacheOpChild.h
@@ -28,10 +28,6 @@ class CacheOpChild final : public PCacheOpChild,
   friend class CacheStorageChild;
   friend class PCacheOpChild;
 
- public:
-  NS_DECL_OWNINGTHREAD
-  NS_INLINE_DECL_THREADSAFE_REFCOUNTING(CacheOpChild, override)
-
  private:
   
   
@@ -69,6 +65,8 @@ class CacheOpChild final : public PCacheOpChild,
   
   nsCOMPtr<nsISupports> mParent;
   RefPtr<Promise> mPromise;
+
+  NS_DECL_OWNINGTHREAD
 };
 
 }  
