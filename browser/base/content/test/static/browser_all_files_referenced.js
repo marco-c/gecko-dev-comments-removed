70d1a8574d8185b303bb1e3fcbd8316da3fb1016
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-08-06 18:23:21 +0000

    Bug 1656680 - Allow aboutGlean.ftl to be unreferenced r=Gijs
    But only on not-nightly or android builds.
    
    Differential Revision: https://phabricator.services.mozilla.com/D85926
    
    UltraBlame original commit: 1552fde523bc10615a898f3d6ecb60d3c9d978e3

--- browser/base/content/test/static/browser_all_files_referenced.js
+++ browser/base/content/test/static/browser_all_files_referenced.js
@@ -233,6 +233,15 @@ if (AppConstants.NIGHTLY_BUILD && AppConstants.platform != "win") {
   whitelist.push({ file: "chrome://fxr/content/fxrui.html" });
 }
 
+if (!AppConstants.NIGHTLY_BUILD || AppConstants.platform == "android") {
+  
+  
+  
+  whitelist.push({
+    file: "resource://gre/localization/en-US/toolkit/about/aboutGlean.ftl",
+  });
+}
+
 whitelist = new Set(
   whitelist
     .filter(
