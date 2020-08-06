1d431710990998ca31b0dd61fac1b17144a1a927
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-08-06 18:22:56 +0000

    Bug 1657315 [Linux] Remove unused X11 display connection from gfxPlatformGtk, r=jhorak
    Differential Revision: https://phabricator.services.mozilla.com/D85993
    
    UltraBlame original commit: 8af9a2bcd005bd36112e2770ef09aa1b4890e18b

--- gfx/thebes/gfxPlatformGtk.cpp
+++ gfx/thebes/gfxPlatformGtk.cpp
@@ -95,15 +95,6 @@ gfxPlatformGtk::gfxPlatformGtk() {
 
   InitBackendPrefs(GetBackendPrefs());
 
-#ifdef MOZ_X11
-  if (mIsX11Display) {
-    mCompositorDisplay = XOpenDisplay(nullptr);
-    MOZ_ASSERT(mCompositorDisplay, "Failed to create compositor display!");
-  } else {
-    mCompositorDisplay = nullptr;
-  }
-#endif  
-
 #ifdef MOZ_WAYLAND
   mUseWebGLDmabufBackend =
       IsWaylandDisplay() && GetDMABufDevice()->IsDMABufWebGLEnabled();
@@ -115,12 +106,6 @@ gfxPlatformGtk::gfxPlatformGtk() {
 }
 
 gfxPlatformGtk::~gfxPlatformGtk() {
-#ifdef MOZ_X11
-  if (mCompositorDisplay) {
-    XCloseDisplay(mCompositorDisplay);
-  }
-#endif  
-
   Factory::ReleaseFTLibrary(gPlatformFTLibrary);
   gPlatformFTLibrary = nullptr;
 }
