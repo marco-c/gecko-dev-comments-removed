2bfdc4bca771a3b8cd1cdc99cbd1b081df8e56cf
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-08-06 18:20:52 +0000

    Backed out changeset 2d126d9f7ef8 (bug 1653290) for /browser_markup_overflow_badge.js failures CLOSED TREE
    UltraBlame original commit: a518405abe4c93e6d8df622f59d5f01c4766e1d5

--- devtools/shared/specs/walker.js
+++ devtools/shared/specs/walker.js
@@ -85,10 +85,6 @@ const walkerSpec = generateActorSpec({
       type: "scrollable-change",
       nodes: Arg(0, "array:domnode"),
     },
-    "overflow-change": {
-      type: "scrollable-change",
-      nodes: Arg(0, "array:domnode"),
-    },
     
     
     
