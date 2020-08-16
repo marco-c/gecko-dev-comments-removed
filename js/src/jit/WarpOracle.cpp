584e9344cd6ec5be08a79dac239e31e353d32736
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-08-16 00:32:50 +0000

    Bug 1646378: Don't inline cross-realm r=jandem
    This fixes a crash in Elm-TodoMVC.
    
    Differential Revision: https://phabricator.services.mozilla.com/D86101
    
    UltraBlame original commit: 768bcbcd20e09129b3be6b79203516ab3b303cba

--- js/src/jit/WarpOracle.cpp
+++ js/src/jit/WarpOracle.cpp
@@ -909,7 +909,7 @@ AbortReasonOr<Ok> WarpScriptOracle::maybeInlineIC(WarpOpSnapshotList& snapshots,
     RootedScript targetScript(cx_, targetFunction->nonLazyScript());
     ICScript* icScript = callData->icScript;
     MOZ_ASSERT(targetScript->jitScript() == icScript->jitScript());
-    MOZ_ASSERT(TrialInliner::canInline(targetFunction));
+    MOZ_ASSERT(TrialInliner::canInline(targetFunction, script_));
 
     
     LifoAlloc* lifoAlloc = alloc_.lifoAlloc();
