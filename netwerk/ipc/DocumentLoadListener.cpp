efac4946fb60b9fa707a7a02abe5c592e74cc90e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-08-16 00:34:47 +0000

    Bug 1657583: Simplify TestSitePermission within nsHTTPSOnlyUtils r=necko-reviewers,JulianWels,dragana
    Differential Revision: https://phabricator.services.mozilla.com/D86178
    
    UltraBlame original commit: 5a2241b01c822f6f5a891857ec503d327f3ca0ac

--- netwerk/ipc/DocumentLoadListener.cpp
+++ netwerk/ipc/DocumentLoadListener.cpp
@@ -21,7 +21,6 @@
 #include "mozilla/dom/ClientChannelHelper.h"
 #include "mozilla/dom/ContentParent.h"
 #include "mozilla/dom/ContentProcessManager.h"
-#include "mozilla/dom/nsHTTPSOnlyUtils.h"
 #include "mozilla/dom/SessionHistoryEntry.h"
 #include "mozilla/dom/WindowGlobalParent.h"
 #include "mozilla/dom/ipc/IdType.h"
@@ -2275,33 +2274,7 @@ DocumentLoadListener::AsyncOnChannelRedirect(
 
   
   
-  RefPtr<CanonicalBrowsingContext> bc =
-      mParentChannelListener->GetBrowsingContext();
-  nsCOMPtr<nsILoadInfo> channelLoadInfo = mChannel->LoadInfo();
-  bool isPrivateWin =
-      channelLoadInfo->GetOriginAttributes().mPrivateBrowsingId > 0;
-  if (nsHTTPSOnlyUtils::IsHttpsOnlyModeEnabled(isPrivateWin) && bc &&
-      bc->IsTop()) {
-    bool isHttpsOnlyExempt = false;
-    if (httpChannel) {
-      nsCOMPtr<nsIPrincipal> resultPrincipal;
-      nsresult rv =
-          nsContentUtils::GetSecurityManager()->GetChannelResultPrincipal(
-              mChannel, getter_AddRefs(resultPrincipal));
-      if (NS_SUCCEEDED(rv)) {
-        isHttpsOnlyExempt =
-            nsHTTPSOnlyUtils::TestHttpsOnlySitePermission(resultPrincipal);
-      }
-    }
-
-    uint32_t httpsOnlyStatus = channelLoadInfo->GetHttpsOnlyStatus();
-    if (isHttpsOnlyExempt) {
-      httpsOnlyStatus |= nsILoadInfo::HTTPS_ONLY_EXEMPT;
-    } else {
-      httpsOnlyStatus &= ~nsILoadInfo::HTTPS_ONLY_EXEMPT;
-    }
-    channelLoadInfo->SetHttpsOnlyStatus(httpsOnlyStatus);
-  }
+  nsHTTPSOnlyUtils::TestSitePermissionAndPotentiallyAddExemption(mChannel);
 
   
   
