116ab02e2a6f8218f177658cb327ba458271c502
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-08-16 00:34:11 +0000

    Bug 1657515 - Fix bustages regarding nsDocumentViewer.cpp and 'pageCount'. a=bustage-fix
    CLOSED TREE
    
    UltraBlame original commit: 35c4666eb74282025e77ec16676866369fbddf5b

--- layout/base/nsDocumentViewer.cpp
+++ layout/base/nsDocumentViewer.cpp
@@ -3343,6 +3343,7 @@ nsDocumentViewer::GetPrintPreviewCurrentPageNumber(int32_t* aNumber) {
 
   
   auto [seqFrame, pageCount] = mPrintJob->GetSeqFrameAndCountPages();
+  Unused << pageCount;
   if (!seqFrame) {
     return NS_ERROR_FAILURE;
   }
