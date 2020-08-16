e3310c655bb00e36d68a8137e98ecd4f2a22524f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-08-16 00:35:46 +0000

    Bug 1652445 - Fix highlighters overlay to react to frame navigation r=rcaliman
    Depends on D84973
    
    This was highlighted by browser_grids_grid-list-on-iframe-reloaded.js which fails without this fix.
    
    Differential Revision: https://phabricator.services.mozilla.com/D84974
    
    UltraBlame original commit: 4d51d99a85a4fdd48b18aeeb5ccde996ce8e9e5f

--- devtools/client/inspector/shared/highlighters-overlay.js
+++ devtools/client/inspector/shared/highlighters-overlay.js
@@ -103,6 +103,8 @@ class HighlightersOverlay {
     this.onClick = this.onClick.bind(this);
     this.onDisplayChange = this.onDisplayChange.bind(this);
     this.onMarkupMutation = this.onMarkupMutation.bind(this);
+    this._onFrameRootAvailable = this._onFrameRootAvailable.bind(this);
+
     this.onMouseMove = this.onMouseMove.bind(this);
     this.onMouseOut = this.onMouseOut.bind(this);
     this.onWillNavigate = this.onWillNavigate.bind(this);
@@ -118,6 +120,7 @@ class HighlightersOverlay {
 
     
     this.inspector.on("markupmutation", this.onMarkupMutation);
+    this.inspector.on("frame-root-available", this._onFrameRootAvailable);
     this.target.on("will-navigate", this.onWillNavigate);
     this.walker.on("display-change", this.onDisplayChange);
 
@@ -1335,6 +1338,15 @@ class HighlightersOverlay {
 
 
 
+
+  async _onFrameRootAvailable() {
+    await this._updateHighlightersOnMutationOrNavigation();
+  }
+
+  
+
+
+
   async onMarkupMutation(mutations) {
     const hasInterestingMutation = mutations.some(
       mut => mut.type === "childList"
@@ -1345,6 +1357,10 @@ class HighlightersOverlay {
       return;
     }
 
+    await this._updateHighlightersOnMutationOrNavigation();
+  }
+
+  async _updateHighlightersOnMutationOrNavigation() {
     for (const node of this.gridHighlighters.keys()) {
       await this._hideHighlighterIfDeadNode(node, this.hideGridHighlighter);
     }
@@ -1448,6 +1464,8 @@ class HighlightersOverlay {
 
   destroy() {
     this.inspector.off("markupmutation", this.onMarkupMutation);
+    this.inspector.off("frame-root-available", this._onFrameRootAvailable);
+
     this.target.off("will-navigate", this.onWillNavigate);
     this.walker.off("display-change", this.onDisplayChange);
 
