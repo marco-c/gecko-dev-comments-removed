982ef61ff553dbf246502a3a27296bf15aef94eb
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-08-16 00:31:23 +0000

    Bug 1652976 - Add WarpBuilder support for CheckPrivateField r=iain
    Differential Revision: https://phabricator.services.mozilla.com/D86060
    
    UltraBlame original commit: 01e7a45cd5e29538412102e5ccb2eeb63b2ab3b4

--- js/src/jit/WarpBuilder.h
+++ js/src/jit/WarpBuilder.h
@@ -63,8 +63,6 @@ namespace jit {
   _(GetRval)                             \
   _(SetIntrinsic)                        \
   _(ThrowMsg)                            \
-  /* Private Fields */                   \
-  _(CheckPrivateField)                   \
   // === !! WARNING WARNING WARNING !! ===
   
   
