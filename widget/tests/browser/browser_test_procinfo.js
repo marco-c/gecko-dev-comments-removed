69d7ef6b566db9f4949c709cfd397f5d0bc01746
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-08-16 00:41:10 +0000

    Bug 1647695 - Uniformizing naming conventions within fields of ChildProcInfoDictionary;r=tarek
    Depends on D83666
    
    Differential Revision: https://phabricator.services.mozilla.com/D83667
    
    UltraBlame original commit: 2facd744d7c36989f9f0cf58021cefe525731f0c

--- widget/tests/browser/browser_test_procinfo.js
+++ widget/tests/browser/browser_test_procinfo.js
@@ -49,6 +49,16 @@ add_task(async function test_proc_info() {
             "browser",
             "Child proc type should not be browser"
           );
+
+          
+          
+          if (childProc.type.startsWith("web")) {
+            Assert.notEqual(
+              childProc.childID,
+              0,
+              "Child proc should have been set"
+            );
+          }
           Assert.notEqual(
             childProc.type,
             "unknown",
