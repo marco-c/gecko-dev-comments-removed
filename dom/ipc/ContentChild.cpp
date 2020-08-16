7863d1ed407b20d6dda8701dd23cee3b4f0d00f4
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-08-16 00:42:07 +0000

    Backed out 2 changesets (bug 1639328) for perma failures on test_bug1639328.html and browser_hidden_iframe.js. CLOSED TREE
    Backed out changeset be370bea0702 (bug 1639328)
    Backed out changeset 954bf9f3dca5 (bug 1639328)
    
    UltraBlame original commit: 682bc156c9719ab66f8ba182999c69210afb1528

--- dom/ipc/ContentChild.cpp
+++ dom/ipc/ContentChild.cpp
@@ -1014,8 +1014,7 @@ nsresult ContentChild::ProvideWindowCommon(
 
   
   
-  RefPtr<nsPIDOMWindowOuter> parentWindow =
-      parent ? parent->GetDOMWindow() : nullptr;
+  nsPIDOMWindowOuter* parentWindow = parent ? parent->GetDOMWindow() : nullptr;
   if (NS_FAILED(newChild->Init(parentWindow, windowChild))) {
     return NS_ERROR_ABORT;
   }
