352f3638b82bf2d3cd3e6d4e0d3daeabb13a6226
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-08-16 01:06:42 +0000

    Bug 1647536 - Support ref counted pointers in nsDeque r=froydnj
    Adds a new specialization of nsDeque, nsRefPtrDeque for use with
    reference counted pointers.
    
    Differential Revision: https://phabricator.services.mozilla.com/D84766
    
    UltraBlame original commit: 5838f8b581ed6cff8d9cc3c8a56a4508ce82ae74

--- netwerk/sctp/datachannel/DataChannel.cpp
+++ netwerk/sctp/datachannel/DataChannel.cpp
@@ -904,16 +904,15 @@ void DataChannelConnection::ProcessQueuedOpens() {
 
   
   
-  nsDeque<DataChannel> temp;
-  DataChannel* temp_channel;  
+  nsRefPtrDeque<DataChannel> temp;
+  RefPtr<DataChannel> temp_channel;
   while (nullptr != (temp_channel = mPending.PopFront())) {
-    temp.Push(temp_channel);
+    temp.Push(temp_channel.forget());
   }
 
   RefPtr<DataChannel> channel;
-  
-  
-  while (nullptr != (channel = dont_AddRef(temp.PopFront()))) {
+
+  while (nullptr != (channel = temp.PopFront())) {
     if (channel->mFlags & DATA_CHANNEL_FLAGS_FINISH_OPEN) {
       DC_DEBUG(("Processing queued open for %p (%u)", channel.get(),
                 channel->mStream));
@@ -2506,10 +2505,7 @@ already_AddRefed<DataChannel> DataChannelConnection::OpenFinish(
     DC_DEBUG(("Queuing channel %p (%u) to finish open", channel.get(), stream));
     
     channel->mFlags |= DATA_CHANNEL_FLAGS_FINISH_OPEN;
-    
-    DataChannel* rawChannel = channel;
-    rawChannel->AddRef();
-    mPending.Push(rawChannel);
+    mPending.Push(channel);
     return channel.forget();
   }
 
@@ -3055,7 +3051,7 @@ void DataChannelConnection::CloseAll() {
 
   
   RefPtr<DataChannel> channel;
-  while (nullptr != (channel = dont_AddRef(mPending.PopFront()))) {
+  while (nullptr != (channel = mPending.PopFront())) {
     DC_DEBUG(("closing pending channel %p, stream %u", channel.get(),
               channel->mStream));
     channel->Close();  
