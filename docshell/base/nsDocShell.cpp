bcc15e63bea33b6c3b0362b72d55c9c429729515
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-08-16 00:57:40 +0000

    Bug 1658572 - Do not propagate user activation state while loading loading-error-page (about:neterror); r=ckerschb
    Differential Revision: https://phabricator.services.mozilla.com/D86724
    
    UltraBlame original commit: f9f7ff74b5b8ffff192b0b8f015cdbce1fa8d3a6

--- docshell/base/nsDocShell.cpp
+++ docshell/base/nsDocShell.cpp
@@ -3803,9 +3803,6 @@ nsresult nsDocShell::LoadErrorPage(nsIURI* aErrorURI, nsIURI* aFailedURI,
   loadState->SetLoadType(LOAD_ERROR_PAGE);
   loadState->SetFirstParty(true);
   loadState->SetSourceBrowsingContext(mBrowsingContext);
-  loadState->SetHasValidUserGestureActivation(
-      mBrowsingContext &&
-      mBrowsingContext->HasValidTransientUserGestureActivation());
   return InternalLoad(loadState);
 }
 
@@ -9683,7 +9680,8 @@ nsresult nsDocShell::DoURILoad(nsDocShellLoadState* aLoadState,
   
   
   
-  if (mBrowsingContext->HasValidTransientUserGestureActivation() ||
+  if ((mLoadType != LOAD_ERROR_PAGE &&
+       mBrowsingContext->HasValidTransientUserGestureActivation()) ||
       aLoadState->HasValidUserGestureActivation() ||
       aLoadState->HasLoadFlags(LOAD_FLAGS_FROM_EXTERNAL)) {
     loadInfo->SetHasValidUserGestureActivation(true);
