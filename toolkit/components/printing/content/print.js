95258322dd2a463c4ec42884a9b7a476416c6fc2
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-08-16 00:40:46 +0000

    Bug 1656995, validate scale input for print preview r=sfoster,mstriemer
    Fixes bug in which an empty input value would always populate back to 100.
    Defines a valid input as numbers between 0 and 100.
    If there is an invalid input, the scaling property does not update.
    An empty range will set the scaling property back to the defalt value of 1.
    
    Differential Revision: https://phabricator.services.mozilla.com/D85795
    
    UltraBlame original commit: bbe270862437951a5afa598b8fe77271198bce2f

--- toolkit/components/printing/content/print.js
+++ toolkit/components/printing/content/print.js
@@ -8,6 +8,8 @@ const {
   Services,
 } = window.docShell.chromeEventHandler.ownerGlobal;
 
+const INVALID_INPUT_DELAY_MS = 500;
+
 document.addEventListener(
   "DOMContentLoaded",
   e => {
@@ -437,28 +439,51 @@ class ScaleInput extends PrintUIControlMixin(HTMLElement) {
 
   initialize() {
     super.initialize();
+
     this._percentScale = this.querySelector("#percent-scale");
     this._percentScale.addEventListener("input", this);
-    this._shrinkToFit = this.querySelector("#fit-choice");
+    this._shrinkToFitChoice = this.querySelector("#fit-choice");
+    this._scaleChoice = this.querySelector("#percent-scale-choice");
+
+    this.addEventListener("input", this);
   }
 
   update(settings) {
-    
     let { scaling, shrinkToFit } = settings;
-    this._shrinkToFit.checked = shrinkToFit;
-    this.querySelector("#percent-scale-choice").checked = !shrinkToFit;
+    this._shrinkToFitChoice.checked = shrinkToFit;
+    this._scaleChoice.checked = !shrinkToFit;
     this._percentScale.disabled = shrinkToFit;
 
     
-    this._percentScale.value = parseInt(scaling * 100, 10);
+    
+    if (!this._percentScale.value || this._shrinkToFitChoice.checked) {
+      
+      this._percentScale.value = parseInt(scaling * 100, 10);
+    }
   }
 
   handleEvent(e) {
     e.stopPropagation();
-    this.dispatchSettingsChange({
-      shrinkToFit: this._shrinkToFit.checked,
-      scaling: this._percentScale.value / 100,
-    });
+
+    if (e.target == this._shrinkToFitChoice || e.target == this._scaleChoice) {
+      this.dispatchSettingsChange({
+        shrinkToFit: this._shrinkToFitChoice.checked,
+      });
+      return;
+    }
+
+    window.clearTimeout(this.invalidTimeoutId);
+
+    if (this._percentScale.checkValidity() && e.type == "input") {
+      
+
+      this.invalidTimeoutId = window.setTimeout(() => {
+        this.dispatchSettingsChange({
+          scaling: Number(this._percentScale.value / 100),
+        });
+      }, INVALID_INPUT_DELAY_MS);
+    }
+    
   }
 }
 customElements.define("scale-input", ScaleInput);
