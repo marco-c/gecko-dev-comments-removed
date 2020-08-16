289cae76e184b75feaaac4befa195a4fdefbcae3
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-08-16 00:33:29 +0000

    Bug 1657231, part 4 - Don't allow using the URL preloader in the child. r=kmag
    It never gets initialized there anyways, so assert and clean up
    a bit of dead code.
    
    Differential Revision: https://phabricator.services.mozilla.com/D86032
    
    UltraBlame original commit: caa4f187b037aef3bd80039b2700825147039b11

--- js/xpconnect/loader/URLPreloader.cpp
+++ js/xpconnect/loader/URLPreloader.cpp
@@ -78,6 +78,11 @@ nsresult URLPreloader::CollectReports(nsIHandleReportCallback* aHandleReport,
 
 
 already_AddRefed<URLPreloader> URLPreloader::Create() {
+  
+  
+  
+  MOZ_RELEASE_ASSERT(XRE_IsParentProcess());
+
   RefPtr<URLPreloader> preloader = new URLPreloader();
   if (preloader->InitInternal().isOk()) {
     RegisterWeakMemoryReporter(preloader);
@@ -131,16 +136,11 @@ Result<Ok, nsresult> URLPreloader::InitInternal() {
     return Err(NS_ERROR_UNEXPECTED);
   }
 
-  if (XRE_IsParentProcess()) {
-    nsCOMPtr<nsIObserverService> obs = services::GetObserverService();
+  nsCOMPtr<nsIObserverService> obs = services::GetObserverService();
 
-    MOZ_TRY(obs->AddObserver(this, DELAYED_STARTUP_TOPIC, false));
+  MOZ_TRY(obs->AddObserver(this, DELAYED_STARTUP_TOPIC, false));
 
-    MOZ_TRY(NS_GetSpecialDirectory("ProfLDS", getter_AddRefs(mProfD)));
-  } else {
-    mStartupFinished = true;
-    mReaderInitialized = true;
-  }
+  MOZ_TRY(NS_GetSpecialDirectory("ProfLDS", getter_AddRefs(mProfD)));
 
   return Ok();
 }
