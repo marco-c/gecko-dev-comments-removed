3cb2155da7ef9296bdf55cc40d69245d4e2135de
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-08-16 01:02:16 +0000

    Backed out changeset 330b617b585c (bug 985141) for bustage and test info failures due to missing argument. CLOSED TREE
    UltraBlame original commit: 6330bdb55f34a780db5dcafc1a0b7f5ee995e98f

--- python/mach/mach/dispatcher.py
+++ python/mach/mach/dispatcher.py
@@ -264,7 +264,7 @@ class CommandAction(argparse.Action):
                 
                 
                 if handler.conditions:
-                    instance = handler.cls(self._context, handler.virtualenv_name)
+                    instance = handler.cls(self._context)
 
                     is_filtered = False
                     for c in handler.conditions:
