4e9c3fce799ddca86ea82e60c5e2704b33a43d3e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-09-01 12:06:14 +0000

    Backed out changeset 2a1fa73b1a63 (bug 1643776) for perma failures on browser_all_files_referenced.js. CLOSED TREE
    UltraBlame original commit: 5eed1f555203af951b7a2dcf35ea23c81134e6aa

--- browser/base/content/test/static/browser_all_files_referenced.js
+++ browser/base/content/test/static/browser_all_files_referenced.js
@@ -22,7 +22,6 @@ var isDevtools = SimpleTest.harnessParameters.subsuite == "devtools";
 
 var gExceptionPaths = [
   "chrome://browser/content/defaultthemes/",
-  "resource://app/modules/themes/alpenglow/images/",
   "resource://app/defaults/settings/blocklists/",
   "resource://app/defaults/settings/security-state/",
   "resource://app/defaults/settings/main/",
