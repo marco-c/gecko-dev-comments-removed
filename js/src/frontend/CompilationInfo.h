5ff2ec892919a34d0835b53c2feddcb99ed9a3c2
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-09-01 12:53:03 +0000

    Bug 1661079 - Part 11: Remove JSContext* parameter from StencilModuleMetadata. r=tcampbell
    Differential Revision: https://phabricator.services.mozilla.com/D88349
    
    UltraBlame original commit: 39daa05687a9e51b5f32acc0ad5f5f840390d1f2

--- js/src/frontend/CompilationInfo.h
+++ js/src/frontend/CompilationInfo.h
@@ -222,8 +222,7 @@ struct CompilationStencil {
   
   ParserAtomsTable parserAtoms;
 
-  explicit CompilationStencil(JSContext* cx)
-      : moduleMetadata(cx), parserAtoms(cx) {}
+  explicit CompilationStencil(JSContext* cx) : parserAtoms(cx) {}
 
 #if defined(DEBUG) || defined(JS_JITSPEW)
   void dump();
