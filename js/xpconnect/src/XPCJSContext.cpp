10c13680c8247a9d60c4c4ad768881d336c9695b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-09-01 11:54:10 +0000

    Bug 1659261 - Declare privateMethodsEnabled outside of just nightly. r=jorendorff
    Differential Revision: https://phabricator.services.mozilla.com/D87282
    
    UltraBlame original commit: c1bb5b123bde03f87200258ed4e73dae9b1fde00

--- js/xpconnect/src/XPCJSContext.cpp
+++ js/xpconnect/src/XPCJSContext.cpp
@@ -962,12 +962,13 @@ static void ReloadPrefsCallback(const char* pref, void* aXpccx) {
 
   
   bool privateFieldsEnabled = false;
+  bool privateMethodsEnabled = false;
 #ifdef NIGHTLY_BUILD
   sIteratorHelpersEnabled =
       Preferences::GetBool(JS_OPTIONS_DOT_STR "experimental.iterator_helpers");
   privateFieldsEnabled =
       Preferences::GetBool(JS_OPTIONS_DOT_STR "experimental.private_fields");
-  bool privateMethodsEnabled =
+  privateMethodsEnabled =
       Preferences::GetBool(JS_OPTIONS_DOT_STR "experimental.private_methods");
 #endif
 
