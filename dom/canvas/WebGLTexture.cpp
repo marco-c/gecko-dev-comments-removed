d2f71534832061b2b7095b0edf575f44ecfc8d65
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-09-01 12:11:05 +0000

    Bug 1659225 - Apply UNPACK_ params only after init. r=lsalzman
    + Add asserts that we are infact in the default state.
    
    Differential Revision: https://phabricator.services.mozilla.com/D87964
    
    UltraBlame original commit: 5f02f3442f26c1d052d10d1d7bf97819c8be5845

--- dom/canvas/WebGLTexture.cpp
+++ dom/canvas/WebGLTexture.cpp
@@ -588,6 +588,7 @@ static bool ZeroTextureData(const WebGLContext* webgl, GLuint tex,
 
     
     
+    WebGLPixelStore::AssertDefault(*gl, webgl->IsWebGL2());
     gl->fPixelStorei(LOCAL_GL_UNPACK_ALIGNMENT, 1);
     const auto revert = MakeScopeExit(
         [&]() { gl->fPixelStorei(LOCAL_GL_UNPACK_ALIGNMENT, 4); });
@@ -631,6 +632,7 @@ static bool ZeroTextureData(const WebGLContext* webgl, GLuint tex,
   if (!zeros) return false;
 
   
+  WebGLPixelStore::AssertDefault(*gl, webgl->IsWebGL2());
   gl->fPixelStorei(LOCAL_GL_UNPACK_ALIGNMENT, 1);
   const auto revert =
       MakeScopeExit([&]() { gl->fPixelStorei(LOCAL_GL_UNPACK_ALIGNMENT, 4); });
