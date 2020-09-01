3cfe3f8c54f225ff6a25a7148da3af8460cdb460
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-09-01 12:29:19 +0000

    Bug 1661077 - HTTPS-Only error page only shows up after a failed upgrade. r=ckerschb
    Differential Revision: https://phabricator.services.mozilla.com/D88166
    
    UltraBlame original commit: 082e8a2aa678b777b335b8d53c34f25e26406b47

--- dom/security/nsHTTPSOnlyUtils.cpp
+++ dom/security/nsHTTPSOnlyUtils.cpp
@@ -198,8 +198,18 @@ bool nsHTTPSOnlyUtils::CouldBeHttpsOnlyError(nsIChannel* aChannel,
   }
 
   
+  
+  
   uint32_t httpsOnlyStatus = loadInfo->GetHttpsOnlyStatus();
-  if (httpsOnlyStatus & nsILoadInfo::HTTPS_ONLY_EXEMPT) {
+  if (httpsOnlyStatus & nsILoadInfo::HTTPS_ONLY_UNINITIALIZED &&
+      !XRE_IsParentProcess() && aError == NS_ERROR_NET_TIMEOUT) {
+    return true;
+  }
+
+  
+  
+  if (httpsOnlyStatus & nsILoadInfo::HTTPS_ONLY_EXEMPT ||
+      httpsOnlyStatus & nsILoadInfo::HTTPS_ONLY_UNINITIALIZED) {
     return false;
   }
 
