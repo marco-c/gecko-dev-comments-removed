d84e2db0f6c80ec7688e5eea06f7c71bdf67cb5d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-09-01 13:04:52 +0000

    Backed out 5 changesets (bug 1658359) for bc crashes on application crashed [ mozilla::net::nsSocketTransport::InitiateSocket()] . CLOSED TREE
    Backed out changeset 2cb2786b8e97 (bug 1658359)
    Backed out changeset 859f1b00f6b0 (bug 1658359)
    Backed out changeset 27b7e050768d (bug 1658359)
    Backed out changeset 8d705f1fa355 (bug 1658359)
    Backed out changeset 555f1ebcef60 (bug 1658359)
    
    UltraBlame original commit: bb5ac7b0628b7c30c961250ca9285e654d385c06

--- browser/components/BrowserGlue.jsm
+++ browser/components/BrowserGlue.jsm
@@ -2576,17 +2576,6 @@ BrowserGlue.prototype = {
       },
 
       
-      
-      {
-        task: () => {
-          let FOG = Cc["@mozilla.org/toolkit/glean;1"].createInstance(
-            Ci.nsIFOG
-          );
-          FOG.initializeFOG();
-        },
-      },
-
-      
       {
         task: () => {
           
---
+++ toolkit/components/glean/aboutGlean/AboutGlean.cpp
@@ -0,0 +1,50 @@
+
+
+
+
+
+
+#include "nsIAboutGlean.h"
+#include "mozilla/AboutGlean.h"
+
+#include "mozilla/ClearOnShutdown.h"
+
+namespace mozilla {
+
+static StaticRefPtr<AboutGlean> gAboutGlean;
+
+
+already_AddRefed<AboutGlean> AboutGlean::GetSingleton() {
+  if (gAboutGlean) {
+    return do_AddRef(gAboutGlean);
+  }
+
+  gAboutGlean = new AboutGlean();
+  ClearOnShutdown(&gAboutGlean);
+  return do_AddRef(gAboutGlean);
+}
+
+extern "C" {
+nsresult fog_set_log_pings(bool aEnableLogPings);
+nsresult fog_set_debug_view_tag(const nsACString* aDebugTag);
+nsresult fog_submit_ping(const nsACString* aPingName);
+}
+
+NS_IMETHODIMP
+AboutGlean::SetLogPings(bool aEnableLogPings) {
+  return fog_set_log_pings(aEnableLogPings);
+}
+
+NS_IMETHODIMP
+AboutGlean::SetTagPings(const nsACString& aDebugTag) {
+  return fog_set_debug_view_tag(&aDebugTag);
+}
+
+NS_IMETHODIMP
+AboutGlean::SendPing(const nsACString& aPingName) {
+  return fog_submit_ping(&aPingName);
+}
+
+NS_IMPL_ISUPPORTS(AboutGlean, nsIAboutGlean)
+
+}  
