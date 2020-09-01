3d40224eb71d20ddabffead7507a8a876e40ef14
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-09-01 11:55:20 +0000

    Bug 1658472 - part 2: Make `AutoInclusiveAncestorBlockElementsJoiner` store left content and right content at construction r=m_kato
    Depends on D86881
    
    Differential Revision: https://phabricator.services.mozilla.com/D86882
    
    UltraBlame original commit: 05962fd0294f9734ff9295543cf8eeec8e735851

--- editor/libeditor/HTMLEditor.h
+++ editor/libeditor/HTMLEditor.h
@@ -2938,6 +2938,15 @@ class HTMLEditor final : public TextEditor,
 
     class MOZ_STACK_CLASS AutoInclusiveAncestorBlockElementsJoiner final {
      public:
+      AutoInclusiveAncestorBlockElementsJoiner() = delete;
+      AutoInclusiveAncestorBlockElementsJoiner(
+          nsIContent& aInclusiveDescendantOfLeftBlockElement,
+          nsIContent& aInclusiveDescendantOfRightBlockElement)
+          : mInclusiveDescendantOfLeftBlockElement(
+                aInclusiveDescendantOfLeftBlockElement),
+            mInclusiveDescendantOfRightBlockElement(
+                aInclusiveDescendantOfRightBlockElement) {}
+
       
 
 
@@ -2956,8 +2965,11 @@ class HTMLEditor final : public TextEditor,
 
 
       [[nodiscard]] MOZ_CAN_RUN_SCRIPT EditActionResult
-      Run(HTMLEditor& aHTMLEditor, nsIContent& aLeftContentInBlock,
-          nsIContent& aRightContentInBlock);
+      Run(HTMLEditor& aHTMLEditor);
+
+     private:
+      OwningNonNull<nsIContent> mInclusiveDescendantOfLeftBlockElement;
+      OwningNonNull<nsIContent> mInclusiveDescendantOfRightBlockElement;
     };
 
     enum class Mode {
