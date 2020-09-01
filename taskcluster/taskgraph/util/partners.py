43e221a2a9be5efe3e6023539f522c42e85bf47a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-09-01 12:48:01 +0000

    Bug 1630809 - python3 compatibility for off-cycle partner respins, typo fixes r=aki
    Differential Revision: https://phabricator.services.mozilla.com/D87723
    
    UltraBlame original commit: 9e38cbdb7b9612fc975f84326ee4a809479f026e

--- taskcluster/taskgraph/util/partners.py
+++ taskcluster/taskgraph/util/partners.py
@@ -195,8 +195,8 @@ def get_repo_params(repo):
 
 
 def get_partners(manifestRepo, token):
-    """ Given the url to a manifest repository, retieve the default.xml and parse it into a
-    list of parter repos.
+    """ Given the url to a manifest repository, retrieve the default.xml and parse it into a
+    list of partner repos.
     """
     log.debug("Querying for manifest in %s", manifestRepo)
     owner, repo = get_repo_params(manifestRepo)
@@ -335,9 +335,8 @@ def get_partner_config_by_kind(config, kind):
     
     if partner_subset:
         
-        for partner in kind_config.keys():
-            if partner not in partner_subset:
-                del(kind_config[partner])
+        for partner in [p for p in kind_config.keys() if p not in partner_subset]:
+            del(kind_config[partner])
 
     return kind_config
 
