9de092e1f95dc3706064ac4e7272b8b9a2b68bce
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-09-01 13:05:52 +0000

    Bug 1662072 - Reduce one data copy in parent process when sending chunks to child process r=valentin
    1. The data sent from `HttpBackgroundChannelParent` to `HttpBackgroundChannelChild` is using type `nsDependentCSubstring`, since I'd like to reduce one copy in parent process.
    2. The data sent from `HttpTransactionChild` to `HttpTransactionParent` is using type `nsCString`. The main reason is that the data is actually captured in `HttpTransactionParent::RecvOnDataAvailable` [1] and put in channel event queue. If we use `nsDependentCSubstring` here, the data would be copied in parent process.
    3.  In `HttpBackgroundChannelChild::RecvOnTransportAndData`, it's inevitable that the data is copied when assigning the data to a `nsCString`, since sometimes `HttpBackgroundChannelChild::RecvOnTransportAndData` needs to be queued and execute later.
    
    
    [1] https://searchfox.org/mozilla-central/rev/969fc7fa6c3c7fc489f53b7b7f8c902028b5169f/netwerk/protocol/http/HttpTransactionParent.cpp#556
    
    Differential Revision: https://phabricator.services.mozilla.com/D88781
    
    UltraBlame original commit: 0b8b700d637196c2ac98ac27afa0a98c97de9cf9

--- netwerk/protocol/http/HttpBackgroundChannelChild.cpp
+++ netwerk/protocol/http/HttpBackgroundChannelChild.cpp
@@ -213,31 +213,42 @@ IPCResult HttpBackgroundChannelChild::RecvOnStartRequest(
 
 IPCResult HttpBackgroundChannelChild::RecvOnTransportAndData(
     const nsresult& aChannelStatus, const nsresult& aTransportStatus,
-    const uint64_t& aOffset, const uint32_t& aCount, const nsCString& aData,
-    const bool& aDataFromSocketProcess) {
-  LOG(
-      ("HttpBackgroundChannelChild::RecvOnTransportAndData [this=%p, "
-       "aDataFromSocketProcess=%d, mFirstODASource=%d]\n",
-       this, aDataFromSocketProcess, mFirstODASource));
-  MOZ_ASSERT(OnSocketThread());
+    const uint64_t& aOffset, const uint32_t& aCount,
+    const nsDependentCSubstring& aData, const bool& aDataFromSocketProcess) {
+  RefPtr<HttpBackgroundChannelChild> self = this;
+  nsCString data(aData);
+  std::function<void()> callProcessOnTransportAndData =
+      [self, aChannelStatus, aTransportStatus, aOffset, aCount, data,
+       aDataFromSocketProcess]() {
+        LOG(
+            ("HttpBackgroundChannelChild::RecvOnTransportAndData [this=%p, "
+             "aDataFromSocketProcess=%d, mFirstODASource=%d]\n",
+             self.get(), aDataFromSocketProcess, self->mFirstODASource));
+        MOZ_ASSERT(OnSocketThread());
 
-  if (NS_WARN_IF(!mChannelChild)) {
-    return IPC_OK();
-  }
+        if (NS_WARN_IF(!self->mChannelChild)) {
+          return;
+        }
 
-  if (((mFirstODASource == ODA_FROM_SOCKET) && !aDataFromSocketProcess) ||
-      ((mFirstODASource == ODA_FROM_PARENT) && aDataFromSocketProcess)) {
-    return IPC_OK();
-  }
+        if (((self->mFirstODASource == ODA_FROM_SOCKET) &&
+             !aDataFromSocketProcess) ||
+            ((self->mFirstODASource == ODA_FROM_PARENT) &&
+             aDataFromSocketProcess)) {
+          return;
+        }
 
-  
-  
-  
-  nsresult channelStatus;
-  mChannelChild->GetStatus(&channelStatus);
-  if (NS_FAILED(channelStatus)) {
-    return IPC_OK();
-  }
+        
+        
+        
+        nsresult channelStatus;
+        self->mChannelChild->GetStatus(&channelStatus);
+        if (NS_FAILED(channelStatus)) {
+          return;
+        }
+
+        self->mChannelChild->ProcessOnTransportAndData(
+            aChannelStatus, aTransportStatus, aOffset, aCount, data);
+      };
 
   
   if (IsWaitingOnStartRequest()) {
@@ -245,19 +256,13 @@ IPCResult HttpBackgroundChannelChild::RecvOnTransportAndData(
          "]\n",
          aOffset, aCount));
 
-    mQueuedRunnables.AppendElement(
-        NewRunnableMethod<const nsresult, const nsresult, const uint64_t,
-                          const uint32_t, const nsCString, bool>(
-            "HttpBackgroundChannelChild::RecvOnTransportAndData", this,
-            &HttpBackgroundChannelChild::RecvOnTransportAndData, aChannelStatus,
-            aTransportStatus, aOffset, aCount, aData, aDataFromSocketProcess));
-
+    mQueuedRunnables.AppendElement(NS_NewRunnableFunction(
+        "HttpBackgroundChannelChild::RecvOnTransportAndData",
+        std::move(callProcessOnTransportAndData)));
     return IPC_OK();
   }
 
-  mChannelChild->ProcessOnTransportAndData(aChannelStatus, aTransportStatus,
-                                           aOffset, aCount, aData);
-
+  callProcessOnTransportAndData();
   return IPC_OK();
 }
 
