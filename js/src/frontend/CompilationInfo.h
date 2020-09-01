50c8257d7cac2812f3ef52529cc4f9516def2686
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-09-01 12:52:42 +0000

    Bug 1661079 - Part 4: Use SystemAllocPolicy in CompilationStencil.bigIntData. r=tcampbell
    Differential Revision: https://phabricator.services.mozilla.com/D88342
    
    UltraBlame original commit: 52c2d490e6ce344ac4dfae9a1931dc99e07cd0db

--- js/src/frontend/CompilationInfo.h
+++ js/src/frontend/CompilationInfo.h
@@ -192,7 +192,7 @@ struct CompilationStencil {
   
   
   Vector<RegExpStencil, 0, js::SystemAllocPolicy> regExpData;
-  Vector<BigIntStencil> bigIntData;
+  Vector<BigIntStencil, 0, js::SystemAllocPolicy> bigIntData;
   Vector<ObjLiteralStencil> objLiteralData;
 
   
@@ -221,8 +221,7 @@ struct CompilationStencil {
   ParserAtomsTable parserAtoms;
 
   explicit CompilationStencil(JSContext* cx)
-      : bigIntData(cx),
-        objLiteralData(cx),
+      : objLiteralData(cx),
         scriptData(cx),
         scopeData(cx),
         moduleMetadata(cx),
