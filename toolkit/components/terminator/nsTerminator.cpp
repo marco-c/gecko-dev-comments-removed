07ccc69c63c08af105a900e8ed01ead3e5d9a033
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-09-01 12:27:18 +0000

    Bug 1660950 - make shutdown steps reporting work r=smaug
    Differential Revision: https://phabricator.services.mozilla.com/D88124
    
    UltraBlame original commit: 5cf9e8cdb828d8cb3b8d04272f37d63788518599

--- toolkit/components/terminator/nsTerminator.cpp
+++ toolkit/components/terminator/nsTerminator.cpp
@@ -184,11 +184,15 @@ void RunWatchdog(void* arg) {
     
     if (!sShutdownNotified) {
       const char* lastStep = nullptr;
-      for (size_t i = 0; i < ArrayLength(sShutdownSteps); ++i) {
-        if (sShutdownSteps[i].mTicks == -1) {
+      
+      
+      
+      
+      for (int i = ArrayLength(sShutdownSteps) - 1; i >= 0; --i) {
+        if (sShutdownSteps[i].mTicks > -1) {
+          lastStep = sShutdownSteps[i].mTopic;
           break;
         }
-        lastStep = sShutdownSteps[i].mTopic;
       }
 
       if (lastStep) {
@@ -497,7 +501,7 @@ nsTerminator::Observe(nsISupports*, const char* aTopic, const char16_t*) {
 void nsTerminator::UpdateHeartbeat(const char* aTopic) {
   
   uint32_t ticks = gHeartbeat.exchange(0);
-  if (mCurrentStep > 0) {
+  if (mCurrentStep >= 0) {
     sShutdownSteps[mCurrentStep].mTicks = ticks;
   }
 
