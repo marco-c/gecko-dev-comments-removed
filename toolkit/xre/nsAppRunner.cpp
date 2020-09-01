a7aeb5cda5f2aeefa00a6b5a5e4a1d47ec5ec906
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-09-01 11:56:19 +0000

    Bug 1207700 [Linux] Enable XInput2 on Gtk 3.24 and newer, r=botond
    Differential Revision: https://phabricator.services.mozilla.com/D86954
    
    UltraBlame original commit: 6c7f2177e485c5eb00adb808e0356ac6b94f4400

--- toolkit/xre/nsAppRunner.cpp
+++ toolkit/xre/nsAppRunner.cpp
@@ -3835,8 +3835,13 @@ int XREMain::XRE_mainStartup(bool* aExitFlag) {
   
   
   
-  const char* useXI2 = PR_GetEnv("MOZ_USE_XINPUT2");
-  if (!useXI2 || (*useXI2 == '0')) gdk_disable_multidevice();
+  const char* useXI2env = PR_GetEnv("MOZ_USE_XINPUT2");
+  const bool disableXI2ByPref = (useXI2env && (*useXI2env == '0'));
+  const bool enabledXI2ByPref = (useXI2env && (*useXI2env == '1'));
+  if (!enabledXI2ByPref &&
+      (gtk_check_version(3, 24, 0) != nullptr || disableXI2ByPref)) {
+    gdk_disable_multidevice();
+  }
 #  endif
 
   
