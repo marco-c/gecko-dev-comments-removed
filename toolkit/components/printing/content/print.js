8b04b66210aefa7bdf289eee2b7ba62c56efc846
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-09-01 13:05:00 +0000

    Bug 1661645 - Set printInColor based on printer support r=emilio,emalysz
    Differential Revision: https://phabricator.services.mozilla.com/D88624
    
    UltraBlame original commit: 1a85b68ddab431b076993ef41150a1bd3f815c88

--- toolkit/components/printing/content/print.js
+++ toolkit/components/printing/content/print.js
@@ -210,6 +210,21 @@ var PrintEventHandler = {
 
     
     this.viewSettings.printerName = printerName;
+
+    
+    
+    let flags = 0;
+    if (!this.viewSettings.supportsColor) {
+      flags |= this.settingFlags.printInColor;
+      this.viewSettings.printInColor = false;
+    } else if (!this.viewSettings.supportsMonochrome) {
+      flags |= this.settingFlags.printInColor;
+      this.viewSettings.printInColor = true;
+    }
+
+    if (flags) {
+      this.saveSettingsToPrefs(flags);
+    }
   },
 
   async print(systemDialogSettings) {
@@ -268,9 +283,6 @@ var PrintEventHandler = {
 
     let printerChanged = flags & this.settingFlags.printerName;
     if (didSettingsChange) {
-      let PSSVC = Cc["@mozilla.org/gfx/printsettings-service;1"].getService(
-        Ci.nsIPrintSettingsService
-      );
       this._printerSettingsChangedFlags |= flags;
 
       if (printerChanged) {
@@ -280,7 +292,7 @@ var PrintEventHandler = {
       }
 
       if (flags) {
-        PSSVC.savePrintSettingsToPrefs(this.settings, true, flags);
+        this.saveSettingsToPrefs(flags);
       }
       if (printerChanged) {
         await this.refreshSettings(this.settings.printerName);
@@ -295,6 +307,13 @@ var PrintEventHandler = {
         })
       );
     }
+  },
+
+  saveSettingsToPrefs(flags) {
+    let PSSVC = Cc["@mozilla.org/gfx/printsettings-service;1"].getService(
+      Ci.nsIPrintSettingsService
+    );
+    PSSVC.savePrintSettingsToPrefs(this.settings, true, flags);
   },
 
   
@@ -614,11 +633,11 @@ const PrintSettingsViewProxy = {
       
       
       
-      case "supportsColorSwitch":
+      case "supportsMonochrome":
         return (
-          target.printerName != PrintUtils.SAVE_TO_PDF_PRINTER &&
-          AppConstants.platform !== "macosx" &&
-          this.get(target, "supportsColor")
+          !this.get(target, "supportsColor") ||
+          (target.printerName != PrintUtils.SAVE_TO_PDF_PRINTER &&
+            AppConstants.platform !== "macosx")
         );
     }
     return target[name];
@@ -786,21 +805,10 @@ customElements.define("destination-picker", DestinationPicker, {
 
 class ColorModePicker extends PrintSettingSelect {
   update(settings) {
-    let value = settings[this.settingName];
-    let supportsColor = settings.supportsColor;
-    let supportsColorSwitch = settings.supportsColorSwitch;
-    
-    
-    let forceChange = value != supportsColor && (!supportsColorSwitch || value);
-    if (forceChange) {
-      value = !value;
-    }
-    this.value = value ? "color" : "bw";
-    this.toggleAttribute("disallowed", !supportsColorSwitch);
-    this.disabled = !supportsColorSwitch;
-    if (forceChange) {
-      this.dispatchEvent(new Event("change", { bubbles: true }));
-    }
+    this.value = settings[this.settingName] ? "color" : "bw";
+    let canSwitch = settings.supportsColor && settings.supportsMonochrome;
+    this.toggleAttribute("disallowed", !canSwitch);
+    this.disabled = !canSwitch;
   }
 
   handleEvent(e) {
