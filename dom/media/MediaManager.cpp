7459cdfbf313c9be4a8a036eb90d3efd4646db03
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-09-01 12:02:39 +0000

    Bug 1652884 - Be ready to mute cameras while mobile is in the background r=agi,pehrsons
    (more work needed).
    
    Differential Revision: https://phabricator.services.mozilla.com/D87539
    
    UltraBlame original commit: f83951453e94df4d4bf9930127a06bc8f878242a

--- dom/media/MediaManager.cpp
+++ dom/media/MediaManager.cpp
@@ -2051,6 +2051,8 @@ MediaManager* MediaManager::Get() {
       obs->AddObserver(sSingleton, "getUserMedia:revoke", false);
       obs->AddObserver(sSingleton, "getUserMedia:muteVideo", false);
       obs->AddObserver(sSingleton, "getUserMedia:unmuteVideo", false);
+      obs->AddObserver(sSingleton, "application-background", false);
+      obs->AddObserver(sSingleton, "application-foreground", false);
     }
     
     nsCOMPtr<nsIPrefBranch> prefs = do_GetService(NS_PREFSERVICE_CONTRACTID);
@@ -3589,6 +3591,8 @@ void MediaManager::Shutdown() {
   obs->RemoveObserver(this, "getUserMedia:revoke");
   obs->RemoveObserver(this, "getUserMedia:muteVideo");
   obs->RemoveObserver(this, "getUserMedia:unmuteVideo");
+  obs->RemoveObserver(this, "application-background");
+  obs->RemoveObserver(this, "application-foreground");
 
   nsCOMPtr<nsIPrefBranch> prefs = do_GetService(NS_PREFSERVICE_CONTRACTID);
   if (prefs) {
@@ -3873,6 +3877,18 @@ nsresult MediaManager::Observe(nsISupports* aSubject, const char* aTopic,
   } else if (!strcmp(aTopic, "getUserMedia:muteVideo") ||
              !strcmp(aTopic, "getUserMedia:unmuteVideo")) {
     OnCameraMute(!strcmp(aTopic, "getUserMedia:muteVideo"));
+    return NS_OK;
+  } else if ((!strcmp(aTopic, "application-background") ||
+              !strcmp(aTopic, "application-foreground")) &&
+             StaticPrefs::media_getusermedia_camera_background_mute_enabled()) {
+    
+    
+    
+    
+    
+    
+    
+    OnCameraMute(!strcmp(aTopic, "application-background"));
     return NS_OK;
   }
   return NS_OK;
