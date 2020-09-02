eb53e56c9aff749a2f739f88ba44030eb872b7ae
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-09-02 10:29:09 +0000

    Bug 1662037 - Update fix-stacks to a version that does not need fileid. r=njn
    Differential Revision: https://phabricator.services.mozilla.com/D88959
    
    UltraBlame original commit: 2766680ff190899aa6ddf6e77167c36a4893525c

--- tools/rb/fix_stacks.py
+++ tools/rb/fix_stacks.py
@@ -54,14 +54,8 @@ def fixSymbols(line, jsonMode=False, slowWarning=False, breakpadSymsDir=None, hi
         if jsonMode:
             args.append('-j')
         if breakpadSymsDir:
-            
-            here = os.path.dirname(__file__)
-            fileid_exe = os.path.join(here, 'fileid')
-            if platform.system() == 'Windows':
-                fileid_exe = fileid_exe + '.exe'
-
             args.append('-b')
-            args.append(breakpadSymsDir + "," + fileid_exe)
+            args.append(breakpadSymsDir)
 
         
         stderr = open(os.devnull) if hide_errors else None
