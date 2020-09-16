3b26139d24b5046660bea37fc6a50cb159ec2743
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-09-16 01:22:32 +0000

    Bug 1663924 - Removed IDB_ENSURE_TRUE macro and convert its uses to IDB_TRY. r=dom-workers-and-storage-reviewers,janv
    Differential Revision: https://phabricator.services.mozilla.com/D89622
    
    UltraBlame original commit: af39c9bbd8fda75af11157460695878032b84fa4

--- dom/indexedDB/KeyPath.cpp
+++ dom/indexedDB/KeyPath.cpp
@@ -103,9 +103,10 @@ nsresult GetJSValFromKeyPathString(
       
       
       JS::Rooted<JS::PropertyDescriptor> desc(aCx);
-      bool ok = JS_GetOwnUCPropertyDescriptor(aCx, obj, keyPathChars,
-                                              keyPathLen, &desc);
-      IDB_ENSURE_TRUE(ok, NS_ERROR_DOM_INDEXEDDB_UNKNOWN_ERR);
+      IDB_TRY(OkIf(JS_GetOwnUCPropertyDescriptor(aCx, obj, keyPathChars,
+                                                 keyPathLen, &desc)),
+              NS_ERROR_DOM_INDEXEDDB_UNKNOWN_ERR,
+              IDB_REPORT_INTERNAL_ERR_LAMBDA);
 
       JS::Rooted<JS::Value> intermediate(aCx);
       bool hasProp = false;
@@ -249,7 +250,8 @@ nsresult GetJSValFromKeyPathString(
       IDB_REPORT_INTERNAL_ERR();
       return NS_ERROR_DOM_INDEXEDDB_UNKNOWN_ERR;
     }
-    IDB_ENSURE_TRUE(succeeded, NS_ERROR_DOM_INDEXEDDB_UNKNOWN_ERR);
+    IDB_TRY(OkIf(succeeded.ok()), NS_ERROR_DOM_INDEXEDDB_UNKNOWN_ERR,
+            IDB_REPORT_INTERNAL_ERR_LAMBDA);
   }
 
   
