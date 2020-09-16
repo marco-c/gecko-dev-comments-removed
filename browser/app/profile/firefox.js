e20651c7bd3b06beba62667fc496dbb4e83f8807
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-09-16 01:05:12 +0000

    Bug 1662292 - Enabling Pocket stories in more regions with German speakers. r=gvn
    Differential Revision: https://phabricator.services.mozilla.com/D88925
    
    UltraBlame original commit: 2efb7a933a9cd599121a9f85b6295d2fd73cb227

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1361,11 +1361,11 @@ pref("browser.newtabpage.activity-stream.discoverystream.spocs-endpoint", "");
 
 pref("browser.newtabpage.activity-stream.discoverystream.locale-list-config", "");
 
-pref("browser.newtabpage.activity-stream.discoverystream.region-stories-config", "US,DE,CA,GB,IE");
+pref("browser.newtabpage.activity-stream.discoverystream.region-stories-config", "US,DE,CA,GB,IE,CH,AT,BE");
 
 pref("browser.newtabpage.activity-stream.discoverystream.region-spocs-config", "US,CA");
 
-pref("browser.newtabpage.activity-stream.discoverystream.region-layout-config", "US,CA,GB,DE,IE");
+pref("browser.newtabpage.activity-stream.discoverystream.region-layout-config", "US,CA,GB,DE,IE,CH,AT,BE");
 
 pref("browser.newtabpage.activity-stream.discoverystream.isCollectionDismissible", true);
 pref("browser.newtabpage.activity-stream.discoverystream.personalization.version", 1);
