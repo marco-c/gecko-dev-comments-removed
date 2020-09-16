e3d1c9467c6fe94c492df611daf635a47514515b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-09-16 01:19:50 +0000

    Bug 1613054 part 2 - On iframe.focus() create the initial about:blank for in-process case and treat the iframe as focusable for OOP case. r=nika
    Test cases in https://bugzilla.mozilla.org/show_bug.cgi?id=1649099
    
    Differential Revision: https://phabricator.services.mozilla.com/D88978
    
    UltraBlame original commit: 73f6874cb3dd6bb4518e0d72445cb613d32d57fc

--- docshell/base/nsDocShell.cpp
+++ docshell/base/nsDocShell.cpp
@@ -6454,6 +6454,7 @@ nsresult nsDocShell::CreateAboutBlankContentViewer(
   bool hadTiming = mTiming;
   bool toBeReset = MaybeInitTiming();
   if (mContentViewer) {
+    MOZ_ASSERT(nsContentUtils::IsSafeToRunScript());
     if (aCheckPermitUnload) {
       
       
