4f3c34cfb0b99c113b66cbb137c35204aeb97a64
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-09-28 19:09:28 +0000

    Bug 1660057: Add preferences for Fission rollout Normandy experiment. r=nika,marionette-reviewers,perftest-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D91066
    
    UltraBlame original commit: 737f0e3e2f948d46cc39b44185e26df3021ab6ee

--- browser/components/extensions/test/browser/browser_ext_devtools_inspectedWindow_targetSwitch.js
+++ browser/components/extensions/test/browser/browser_ext_devtools_inspectedWindow_targetSwitch.js
@@ -108,7 +108,7 @@ add_task(async () => {
 
   
   
-  if (Services.prefs.getBoolPref("fission.autostart")) {
+  if (SpecialPowers.useRemoteSubframes) {
     info("Navigate to another page running on content process");
     await navigateTo(CONTENT_PROCESS_PAGE2, tab, toolbox, extension);
 
