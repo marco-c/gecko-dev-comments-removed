c48293c3402e2c553cfed383f5c51b5376ba99c9
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-09-28 18:31:03 +0000

    Bug 1665945 - Fix compilation. r=gerald
    Fix ""error: member access into incomplete type 'mozilla::layers::IGPUVideoSurfaceManager" build bustage with --disable-accessibility"
    we don't want to fully declare the class in the header as it would require to leak most of gfx headers.
    
    Differential Revision: https://phabricator.services.mozilla.com/D90776
    
    UltraBlame original commit: 1aad6539812fd0e9c6697bf62c1c6389fdc9b2d6

--- dom/media/ipc/RemoteImageHolder.h
+++ dom/media/ipc/RemoteImageHolder.h
@@ -17,30 +17,18 @@ namespace mozilla {
 namespace layers {
 class BufferRecycleBin;
 class IGPUVideoSurfaceManager;
-}
+}  
 class RemoteImageHolder final {
   friend struct ipc::IPDLParamTraits<RemoteImageHolder>;
 
  public:
-  RemoteImageHolder() = default;  
-                                  
+  RemoteImageHolder();  
+                        
   RemoteImageHolder(layers::IGPUVideoSurfaceManager* aManager,
                     layers::VideoBridgeSource aSource,
                     const gfx::IntSize& aSize,
-                    const layers::SurfaceDescriptor& aSD)
-      : mSource(aSource),
-        mSize(aSize),
-        mSD(aSD),
-        mManager(aManager),
-        mEmpty(false) {}
-  RemoteImageHolder(RemoteImageHolder&& aOther)
-      : mSource(aOther.mSource),
-        mSize(aOther.mSize),
-        mSD(aOther.mSD),
-        mManager(aOther.mManager),
-        mEmpty(aOther.mEmpty) {
-    aOther.mEmpty = true;
-  }
+                    const layers::SurfaceDescriptor& aSD);
+  RemoteImageHolder(RemoteImageHolder&& aOther);
   
   RemoteImageHolder(const RemoteImageHolder& aOther) = delete;
   RemoteImageHolder& operator=(const RemoteImageHolder& aOther) = delete;
