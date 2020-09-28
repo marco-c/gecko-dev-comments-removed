0a392964bf2385e58afe0172fad3be1d65a44b7c
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-09-28 18:19:18 +0000

    Bug 1656438 - Take the desired buffering as parameter in ClockDrift, AudioResampler, and friends. r=padenot
    This makes us able to write tests for different buffer lengths.
    
    Differential Revision: https://phabricator.services.mozilla.com/D89760
    
    UltraBlame original commit: 90c2ec79a8fc082613cf19b21e34dee4c5be9f3f

--- dom/media/DynamicResampler.cpp
+++ dom/media/DynamicResampler.cpp
@@ -137,8 +137,8 @@ void DynamicResampler::UpdateResampler(int aOutRate, int aChannels) {
     mInternalInBuffer.Clear();
     for (int i = 0; i < mChannels; ++i) {
       
-      AudioRingBuffer* b =
-          mInternalInBuffer.AppendElement(sizeof(float) * mInRate / 10);
+      AudioRingBuffer* b = mInternalInBuffer.AppendElement(
+          sizeof(float) * std::max(2 * mPreBufferFrames, mInRate / 10));
       if (mSampleFormat != AUDIO_FORMAT_SILENCE) {
         
         b->SetSampleFormat(mSampleFormat);
