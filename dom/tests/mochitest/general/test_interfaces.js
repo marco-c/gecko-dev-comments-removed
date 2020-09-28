dd974555658e69dd72612fca80d2d112aa9b2b03
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-09-28 18:24:33 +0000

    Bug 1518999 - Implement PerformancePaintTiming for FirstContentfulPaint r=smaug,mstange
    Spec: https://w3c.github.io/paint-timing/#sec-PerformancePaintTiming
    We only support FirstContentfulPaint at the moment.
    
    Differential Revision: https://phabricator.services.mozilla.com/D66463
    
    UltraBlame original commit: c3bb552e76a3d86de2b9f75ac2233b3a45cda06f

--- dom/tests/mochitest/general/test_interfaces.js
+++ dom/tests/mochitest/general/test_interfaces.js
@@ -858,6 +858,8 @@ var interfaceNamesInGlobalScope = [
   
   { name: "PerformanceObserverEntryList", insecureContext: true },
   
+  { name: "PerformancePaintTiming", insecureContext: true },
+  
   { name: "PerformanceResourceTiming", insecureContext: true },
   
   { name: "PerformanceServerTiming", insecureContext: false },
