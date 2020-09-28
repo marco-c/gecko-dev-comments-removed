b7efd79a05f076b46ea5ca76f816821f2360a34d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-09-28 19:08:53 +0000

    Bug 1647934 - Clean up a user's logins backup when it may no longer be useful. r=sfoster
    Differential Revision: https://phabricator.services.mozilla.com/D89078
    
    UltraBlame original commit: 047eec5c0b1c317dd8718e8a67bfb4cf1708977c

--- toolkit/components/passwordmgr/storage-json.js
+++ toolkit/components/passwordmgr/storage-json.js
@@ -35,6 +35,11 @@ XPCOMUtils.defineLazyServiceGetter(
   "nsIUUIDGenerator"
 );
 
+XPCOMUtils.defineLazyModuleGetters(this, {
+  FXA_PWDMGR_HOST: "resource://gre/modules/FxAccountsCommon.js",
+  FXA_PWDMGR_REALM: "resource://gre/modules/FxAccountsCommon.js",
+});
+
 class LoginManagerStorage_json {
   constructor() {
     this.__crypto = null; 
@@ -287,6 +292,7 @@ class LoginManagerStorage_json {
 
     LoginHelper.notifyStorageChanged("removeLogin", storedLogin);
     this._recordEntryPresent();
+    this._updateLoginsBackup();
   }
 
   modifyLogin(oldLogin, newLoginData) {
@@ -657,6 +663,7 @@ class LoginManagerStorage_json {
     this._store.saveSoon();
 
     LoginHelper.notifyStorageChanged("removeAllLogins", null);
+    this._updateLoginsBackup();
   }
 
   findLogins(origin, formActionOrigin, httpRealm) {
@@ -841,6 +848,47 @@ class LoginManagerStorage_json {
       "signon.usage.hasEntry",
       !!this._store.data.logins.length
     );
+  }
+
+  
+  
+  
+  
+  
+  async _updateLoginsBackup() {
+    
+    if (!this._store._options.backupTo) {
+      return;
+    }
+
+    const logins = this._store.data.logins;
+    
+    
+    if (logins.length > 1) {
+      return;
+    }
+
+    
+    
+    if (
+      logins.length &&
+      logins[0].hostname == FXA_PWDMGR_HOST &&
+      logins[0].httpRealm == FXA_PWDMGR_REALM
+    ) {
+      try {
+        await OS.File.copy(this._store.path, this._store._options.backupTo);
+      } catch (ex) {
+        Cu.reportError(ex);
+      }
+    } else if (!logins.length) {
+      
+      await OS.File.remove(this._store._options.backupTo, {
+        ignoreAbsent: true,
+      });
+    }
+
+    
+    Services.obs.notifyObservers(null, "logins-backup-updated");
   }
 }
 
