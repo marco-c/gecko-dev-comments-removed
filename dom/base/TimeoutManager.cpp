5ee80ce7c3e0c71a797df8ce16bd0e53a80d7e0a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-09-28 19:36:20 +0000

    Bug 1666975 - Remove PENDING_CRITICAL_INPUT_WHEN_TIMEOUT telemetry probe r=smaug
    This probe is no longer needed.
    
    Differential Revision: https://phabricator.services.mozilla.com/D91456
    
    UltraBlame original commit: 9a2ce795a69e68de482e8a78ae11821505ca808d

--- dom/base/TimeoutManager.cpp
+++ dom/base/TimeoutManager.cpp
@@ -400,27 +400,6 @@ void TimeoutManager::UpdateBudget(const TimeStamp& aNow,
   mLastBudgetUpdate = aNow;
 }
 
-size_t TimeoutManager::GetNumPendingInputs() {
-  ContentChild* contentChild = ContentChild::GetSingleton();
-  mozilla::ipc::MessageChannel* channel =
-      contentChild ? contentChild->GetIPCChannel() : nullptr;
-
-  if (channel) {
-    size_t count = 0;
-    channel->PeekMessages([&count](const IPC::Message& aMsg) -> bool {
-      if (nsContentUtils::IsMessageCriticalInputEvent(aMsg)) {
-        
-        
-        if (++count > 80) {
-          return false;
-        }
-      }
-      return true;
-    });
-    return count;
-  }
-  return 0;
-}
 
 
 
@@ -911,8 +890,6 @@ void TimeoutManager::RunTimeout(const TimeStamp& aNow,
         mLastFiringIndex = timeout->mFiringIndex;
 #endif
         
-        Telemetry::Accumulate(Telemetry::PENDING_CRITICAL_INPUT_WHEN_TIMEOUT,
-                              GetNumPendingInputs());
         bool timeout_was_cleared = window->RunTimeoutHandler(timeout, scx);
 #if MOZ_GECKO_PROFILER
         if (profiler_can_accept_markers()) {
