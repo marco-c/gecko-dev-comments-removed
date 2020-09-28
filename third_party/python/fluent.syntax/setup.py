1322970726bbfe33cc3096b178ee734f0b9bb7b8
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-09-28 18:34:29 +0000

    Bug 1666232 - Vendor fluent.syntax 0.18.1, fluent.migrate 0.10, compare-locales 8.1.0. r=flod
    Differential Revision: https://phabricator.services.mozilla.com/D90851
    
    UltraBlame original commit: 08e86ba18177437431a33a712956aa4c3dc91120

--- third_party/python/fluent.syntax/setup.py
+++ third_party/python/fluent.syntax/setup.py
@@ -1,10 +1,15 @@
 
 from setuptools import setup
+import os
+
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, 'README.rst'), 'rb') as f:
+    long_description = f.read().decode('utf-8')
 
 setup(name='fluent.syntax',
-      version='0.17.0',
       description='Localization library for expressive translations.',
-      long_description='See https://github.com/projectfluent/python-fluent/ for more info.',
+      long_description=long_description,
+      long_description_content_type='text/x-rst',
       author='Mozilla',
       author_email='l10n-drivers@mozilla.org',
       license='APL 2',
@@ -21,4 +26,4 @@ setup(name='fluent.syntax',
       
       tests_require=['six'],
       test_suite='tests.syntax'
-)
+      )
