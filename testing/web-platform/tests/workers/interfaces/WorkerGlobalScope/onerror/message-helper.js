b8bfe0bc68d07d52c8025fd19c63afaf38e6f49c
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-09-28 18:49:23 +0000

    Bug 1664391 [wpt PR 25480] - [WPT] Avoid timeout in workers/interfaces/WorkerGlobalScope/onerror/message.html, a=testonly
    Automatic update from web-platform-tests
    [WPT] Avoid timeout in workers/interfaces/WorkerGlobalScope/onerror/message.html
    
    To avoid timeout, this CL
    
    - Removes arbitrary timeout of 2 seconds, and
    - Splits `message.html` into multiple test files.
    
    This is to unblock V8 roll:
    https://chromium-review.googlesource.com/c/chromium/src/+/2401062
    
    Bug: 590219, 1127235
    Change-Id: I86a750f3b158797a393506bf7be32ecc573e73f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2405726
    Commit-Queue: Hiroshige Hayashizaki <hiroshigechromium.org>
    Reviewed-by: Kenichi Ishibashi <bashichromium.org>
    Cr-Commit-Position: refs/heads/master{#806098}
    
    --
    
    wpt-commits: 3191cc9966ef03e352698e1ec07d861cff79a24f
    wpt-pr: 25480
    
    UltraBlame original commit: 5b4fdebb1578726228a9e5e55e7bc9a22d62f8d4

---
+++ testing/web-platform/tests/workers/interfaces/WorkerGlobalScope/onerror/message-helper.js
@@ -0,0 +1,61 @@
+
+
+
+
+
+function prepareHandler(t, error, expectedCount) {
+  let count = 0;
+  return t.step_func(e => {
+      e.preventDefault();
+
+      assert_regexp_match(
+          e.message,
+          /Throw in/,
+          'e.message should contain the message of the thrown error');
+
+      if (error === 'DOMException-TypeError') {
+        assert_regexp_match(e.message, /TypeError/);
+      }
+
+      ++count;
+      if (count >= expectedCount) {
+        t.done();
+      }
+    });
+}
+
+function expectErrors(worker, error, expectedCount, title) {
+  async_test(t => {
+      worker.addEventListener('error',
+                              prepareHandler(t, error, expectedCount));
+    }, title+ ': listener');
+  async_test(t => {
+      worker.onerror = prepareHandler(t, error, expectedCount);
+    }, title + ': handler');
+}
+
+function runTest(type, error) {
+  for (const location of ['toplevel',
+                          'setTimeout-function',
+                          'setTimeout-string',
+                          'onmessage',
+                          'onerror']) {
+    const worker = new Worker(
+        'throw.js?throw-in-' + location + '&error=' + error,
+        {type});
+    let expectedCount = 1;
+    if (location === 'onmessage') {
+      
+      worker.postMessage('foo');
+    }
+    if (location === 'onerror') {
+      
+      
+      
+      worker.postMessage('foo');
+      expectedCount = 2;
+    }
+    expectErrors(worker, error, expectedCount,
+        'Throw ' + error + ' in ' + location + ': ' + type);
+  }
+}
