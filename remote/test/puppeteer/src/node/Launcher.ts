77326515f8946ef6d7de55e193e5cb3f46425757
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-09-28 19:34:33 +0000

    Bug 1665283 - [puppeteer] Remove datareporting.healthreport.about.reportUrl from Launcher.ts r=remote-protocol-reviewers,whimboo
    Differential Revision: https://phabricator.services.mozilla.com/D91450
    
    UltraBlame original commit: 6b6e8d1227368c764cde89788edfa1afc172407f

--- remote/test/puppeteer/src/node/Launcher.ts
+++ remote/test/puppeteer/src/node/Launcher.ts
@@ -561,7 +561,6 @@ class FirefoxLauncher implements ProductLauncher {
       'browser.warnOnQuit': false,
 
       
-      'datareporting.healthreport.about.reportUrl': `http://${server}/dummy/abouthealthreport/`,
       'datareporting.healthreport.documentServerURI': `http://${server}/dummy/healthreport/`,
       'datareporting.healthreport.logging.consoleEnabled': false,
       'datareporting.healthreport.service.enabled': false,
