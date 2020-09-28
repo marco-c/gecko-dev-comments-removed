9fc826047f630c7fb352820f6ff3b1fd7d11e0bd
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-09-28 19:01:20 +0000

    Bug 1663550 - Fix the alignment of the stack for the sandbox's clone() trampoline. r=gcp
    The ABI on ARM64 requires 16-byte stack alignment, and that includes the
    small temporary stack that exists only so that we can `longjmp` off of
    it in the child process after calling `clone`.
    
    Differential Revision: https://phabricator.services.mozilla.com/D90001
    
    UltraBlame original commit: 44f258418d94e4dacec6903aaa9dd975cf28d5ba

--- security/sandbox/linux/launch/SandboxLaunch.cpp
+++ security/sandbox/linux/launch/SandboxLaunch.cpp
@@ -499,7 +499,9 @@ static int CloneCallee(void* aPtr) {
 
 MOZ_NEVER_INLINE MOZ_ASAN_BLACKLIST static pid_t DoClone(int aFlags,
                                                          jmp_buf* aCtx) {
-  uint8_t miniStack[PTHREAD_STACK_MIN];
+  static constexpr size_t kStackAlignment = 16;
+  uint8_t miniStack[PTHREAD_STACK_MIN]
+      __attribute__((aligned(kStackAlignment)));
 #ifdef __hppa__
   void* stackPtr = miniStack;
 #else
