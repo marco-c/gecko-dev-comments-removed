25d23eed1d66aa16edc3e4ec081dc81278627260
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-10-21 17:18:24 +0000

    Bug 1668372 - Test GMPExtractor handles special characters. r=Gijs
    Differential Revision: https://phabricator.services.mozilla.com/D92149
    
    UltraBlame original commit: ac12524936de917d559e0c31455b9cca181f1612

--- toolkit/modules/tests/xpcshell/test_GMPInstallManager.js
+++ toolkit/modules/tests/xpcshell/test_GMPInstallManager.js
@@ -16,6 +16,7 @@ const { setTimeout } = ChromeUtils.import("resource://gre/modules/Timer.jsm");
 const { FileUtils } = ChromeUtils.import(
   "resource://gre/modules/FileUtils.jsm"
 );
+const { OS } = ChromeUtils.import("resource://gre/modules/osfile.jsm");
 const { HttpServer } = ChromeUtils.import("resource://testing-common/httpd.js");
 const { Preferences } = ChromeUtils.import(
   "resource://gre/modules/Preferences.jsm"
@@ -735,6 +736,72 @@ add_test(function test_installAddon_noServer() {
     () => {
       do_throw("check should not reject for install no server");
     }
+  );
+});
+
+
+
+
+
+
+add_task(async function test_GMPExtractor_paths() {
+  let GMPExtractor = GMPScope.GMPExtractor;
+  registerCleanupFunction(function() {
+    
+    let zipFile = new FileUtils.File(
+      OS.Path.join(tempDir.path, "dummy_gmp.zip")
+    );
+    Services.obs.notifyObservers(zipFile, "flush-cache-entry");
+    extractedDir.remove( true);
+    tempDir.remove( true);
+  });
+  
+  
+  
+  
+  let tempDirName = "TmpDir#猫";
+  let tempDir = FileUtils.getDir("TmpD", [tempDirName], true);
+  let zipPath = OS.Path.join(tempDir.path, "dummy_gmp.zip");
+  await OS.File.copy("zips/dummy_gmp.zip", zipPath, { noOverwrite: false });
+  
+  
+  let relativeExtractPath = "extracted#猫";
+  let extractor = new GMPExtractor(zipPath, relativeExtractPath);
+  let extractedPaths = await extractor.install();
+  
+  
+  
+  Assert.equal(extractedPaths.length, 1, "One file should be extracted");
+  Assert.ok(
+    extractedPaths[0].includes("dummy_file.txt"),
+    "dummy_file.txt should be on extracted path"
+  );
+  Assert.ok(
+    !extractedPaths[0].includes("verified_contents.json"),
+    "verified_contents.json should not be on extracted path"
+  );
+  let extractedDir = FileUtils.getDir("ProfD", [relativeExtractPath], false);
+  Assert.ok(
+    extractedDir.exists(),
+    "Extraction should have created a directory"
+  );
+  let extractedFile = FileUtils.getDir(
+    "ProfD",
+    [relativeExtractPath, "dummy_file.txt"],
+    false
+  );
+  Assert.ok(
+    extractedFile.exists(),
+    "Extraction should have created dummy_file.txt"
+  );
+  let unextractedFile = FileUtils.getDir(
+    "ProfD",
+    [relativeExtractPath, "verified_contents.json"],
+    false
+  );
+  Assert.ok(
+    !unextractedFile.exists(),
+    "Extraction should not have created verified_contents.json"
   );
 });
 
