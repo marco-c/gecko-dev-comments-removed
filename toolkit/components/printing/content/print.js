167665581ea6f9cf058ba416e63ec07b737900a3
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-10-21 18:34:44 +0000

    Bug 1670643 - Ignore DOMWindowClose events fired at the preview browser. r=jfkthame
    See the comment.
    
    https://phabricator.services.mozilla.com/D90808 made it so that we
    reused the print document (and thus <browser>) to do the actual print
    job.
    
    When a print job finishes, we call window.close(), which is used as a
    signal to the front-end to clean up the <browser> element. Before that
    patch we sent that signal to the browser created in printUtils.js, but
    afterwards we send it to the preview window which causes this badness
    when the event bubbles up.
    
    Ignoring it effectively restores the previous behavior, so let's do it.
    
    Differential Revision: https://phabricator.services.mozilla.com/D93499
    
    UltraBlame original commit: 02b822f97e3537517e6600ce763c4f28328f0e9c

--- toolkit/components/printing/content/print.js
+++ toolkit/components/printing/content/print.js
@@ -315,6 +315,18 @@ var PrintEventHandler = {
       initialBrowsingContextGroupId: sourceBrowsingContext.group.id,
       skipLoad: true,
     });
+    printPreviewBrowser.addEventListener("DOMWindowClose", function(e) {
+      
+      
+      
+      
+      
+      
+      
+      
+      e.preventDefault();
+      e.stopPropagation();
+    });
     printPreviewBrowser.classList.add("printPreviewBrowser");
     printPreviewBrowser.setAttribute("flex", "1");
     printPreviewBrowser.setAttribute("printpreview", "true");
