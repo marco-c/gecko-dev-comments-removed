44e11a9124efe86cc596495aa05ec4e6f818f3e2
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-10-21 17:29:36 +0000

    Bug 1663128 - part3 : the event source should do the cleanup when it closes. r=chunmin
    When close the event source, it should be responsible to clear up and reset the virtual control interface, rather than doing so by `Media Control Server` via setting some empty results.
    
    Differential Revision: https://phabricator.services.mozilla.com/D92116
    
    UltraBlame original commit: 0ea9bcad0ff8730c071fe0a13931e0b6d9d13742

--- widget/windows/WindowsSMTCProvider.cpp
+++ widget/windows/WindowsSMTCProvider.cpp
@@ -172,26 +172,19 @@ bool WindowsSMTCProvider::Open() {
 
 void WindowsSMTCProvider::Close() {
   MediaControlKeySource::Close();
-  if (mInitialized) {  
+  
+  if (mInitialized) {
     SetPlaybackState(mozilla::dom::MediaSessionPlaybackState::None);
+    UnregisterEvents();
+    ClearMetadata();
+    
+    
+    
+    
+    
     EnableControl(false);
     mInitialized = false;
   }
-
-  UnregisterEvents();
-
-  
-  mImageFetchRequest.DisconnectIfExists();
-
-  CancelPendingStoreAsyncOperation();
-
-  
-  mThumbnailUrl.Truncate();
-  mProcessingUrl.Truncate();
-
-  mNextImageIndex = 0;
-
-  mSupportedKeys = 0;
 }
 
 void WindowsSMTCProvider::SetPlaybackState(
@@ -234,6 +227,19 @@ void WindowsSMTCProvider::SetMediaMetadata(
   SetMusicMetadata(aMetadata.mArtist.get(), aMetadata.mTitle.get(),
                    aMetadata.mAlbum.get());
   LoadThumbnail(aMetadata.mArtwork);
+}
+
+void WindowsSMTCProvider::ClearMetadata() {
+  MOZ_ASSERT(mDisplay);
+  if (FAILED(mDisplay->ClearAll())) {
+    LOG("Failed to clear SMTC display");
+  }
+  mImageFetchRequest.DisconnectIfExists();
+  CancelPendingStoreAsyncOperation();
+  mThumbnailUrl.Truncate();
+  mProcessingUrl.Truncate();
+  mNextImageIndex = 0;
+  mSupportedKeys = 0;
 }
 
 void WindowsSMTCProvider::SetSupportedMediaKeys(
