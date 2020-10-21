9c76f45a87e41d88b6e7d49a5a490ed9ff006da1
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-10-21 17:29:13 +0000

    Bug 1664922 - Allow CPU information in the "utility" sandbox policy, for nsSystemInfo. r=gcp
    Differential Revision: https://phabricator.services.mozilla.com/D90603
    
    UltraBlame original commit: be90d6aec690e854f582f36b2c5c0b742b741af3

--- security/sandbox/linux/broker/SandboxBrokerPolicyFactory.cpp
+++ security/sandbox/linux/broker/SandboxBrokerPolicyFactory.cpp
@@ -683,6 +683,15 @@ SandboxBrokerPolicyFactory::GetUtilityPolicy(int aPid) {
 
   AddSharedMemoryPaths(policy.get(), aPid);
 
+  
+  
+  
+  policy->AddPath(rdonly, "/proc/cpuinfo");
+  policy->AddPath(rdonly,
+                  "/sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_max_freq");
+  policy->AddPath(rdonly, "/sys/devices/system/cpu/cpu0/cache/index2/size");
+  policy->AddPath(rdonly, "/sys/devices/system/cpu/cpu0/cache/index3/size");
+
   if (policy->IsEmpty()) {
     policy = nullptr;
   }
