74848a1957b3fe8f53c432cfabe53c7e4d228588
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-10-21 17:45:04 +0000

    Bug 1667499 [wpt PR 25799] - Reland "FontAccess: Sort fonts in lexicographical order", a=testonly
    Automatic update from web-platform-tests
    Reland "FontAccess: Sort fonts in lexicographical order"
    
    This is a reland of 404b7cc426e259c11276d673630c9f094628259f
    
    The original CL failed some tests because the code assumes that
    platform-specific APIs called returned unique data. This was a false
    assumption.
    
    This reland follows another CL: https://crrev.com/c/2438875, which
    ensures that fonts are not duplicated.
    
    Original change's description:
    > FontAccess: Sort fonts in lexicographical order
    >
    > This change ensures fonts are sorted before being cached. This change is
    > due to a concern about fingerprinting:
    > https://github.com/w3ctag/design-reviews/issues/399#issuecomment-530679815
    >
    > This change ensures that fonts are sorted in lexicographical order.
    >
    > Bug: 1043306
    > Change-Id: Ia3acf2a45cb473124df4e489683bdc7bac15dde4
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2427040
    > Commit-Queue: Olivier Yiptong <oyiptongchromium.org>
    > Reviewed-by: Joshua Bell <jsbellchromium.org>
    > Cr-Commit-Position: refs/heads/master{#810519}
    
    Bug: 1043306
    Change-Id: Ic3d70744661a838d82349c017a1cb0e8c23a3654
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2432815
    Commit-Queue: Olivier Yiptong <oyiptongchromium.org>
    Reviewed-by: Joshua Bell <jsbellchromium.org>
    Cr-Commit-Position: refs/heads/master{#812965}
    
    --
    
    wpt-commits: cd769106a3ea014cc1b0f8a90bfeac8e8d9a8ab7
    wpt-pr: 25799
    
    UltraBlame original commit: 3b541271d02cfdf6c8ce481bff8e695466daeb40

--- testing/web-platform/tests/font-access/resources/window-tests-enumeration.js
+++ testing/web-platform/tests/font-access/resources/window-tests-enumeration.js
@@ -22,3 +22,32 @@ font_access_test(async t => {
 
   assert_fonts_exist(availableFonts, getEnumerationTestSet());
 }, 'query(): standard fonts returned');
+
+font_access_test(async t => {
+  const iterator = navigator.fonts.query();
+
+  if (!isPlatformSupported()) {
+    await promise_rejects_dom(t, 'NotSupportedError', (async () => {
+                                for await (const f of iterator) {
+                                }
+                              })());
+    return;
+  }
+
+  
+  
+  
+  
+  
+  let previousFont = null;
+  for await (const font of iterator) {
+    if (previousFont) {
+      assert_true(
+          previousFont.postscriptName < font.postscriptName,
+          `font is not in expected order. expected: ${
+              previousFont.postscriptName} < ${font.postscriptName}`);
+    }
+
+    previousFont = font;
+  }
+}, 'query(): fonts are sorted');
