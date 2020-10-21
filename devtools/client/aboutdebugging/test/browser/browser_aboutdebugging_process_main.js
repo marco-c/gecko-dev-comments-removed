7d8200c019780bb5ea4b486e1458fe2d38260159
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-10-21 17:38:46 +0000

    Bug 1669353 - [devtools] Tweak about:debugging main process test to check no target is created r=ochameau
    Depends on D92499
    Tweak an existing mock in the main process about:debugging test to check that we are not creating any target.
    
    Differential Revision: https://phabricator.services.mozilla.com/D92553
    
    UltraBlame original commit: ff1750ffbc4d5e6b22215c2af60bd902e4cfb8cd

--- devtools/client/aboutdebugging/test/browser/browser_aboutdebugging_process_main.js
+++ devtools/client/aboutdebugging/test/browser/browser_aboutdebugging_process_main.js
@@ -43,10 +43,14 @@ async function testMainProcessTarget(name, description) {
     deviceName: RUNTIME_DEVICE_NAME,
     name: RUNTIME_APP_NAME,
   });
+
+  
+  
+  
   usbRuntime.getMainProcess = () => {
     return {
       getTarget: () => {
-        return { actorID: 0 };
+        ok(false, "about:debugging should not create the main process target");
       },
     };
   };
