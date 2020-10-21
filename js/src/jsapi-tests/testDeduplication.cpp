948773bcd47bca5af707daf3f6613198ca564f14
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-10-21 18:17:09 +0000

    Bug 1670323 - Prevent GC in between allocating strings and dependent strings in deduplication test. r=sfink
    Differential Revision: https://phabricator.services.mozilla.com/D93216
    
    UltraBlame original commit: facab8f08782a3e4e959c669176f641f1ece1b2f

--- js/src/jsapi-tests/testDeduplication.cpp
+++ js/src/jsapi-tests/testDeduplication.cpp
@@ -9,6 +9,8 @@
 
 #include "jsfriendapi.h"
 
+#include "gc/GC.h"
+
 #include "js/RootingAPI.h"
 #include "js/StableStringChars.h"
 #include "js/String.h"  
@@ -40,29 +42,44 @@ BEGIN_TEST(testDeduplication_ASSC) {
       "eearth.Theirtagsshallblinkuntiltheendofdays.";
 
   
-  JS::RootedString original(cx, JS_NewStringCopyZ(cx, text));
-  CHECK(original);
+  JS::RootedString original(cx);
+  JS::RootedString str(cx);
+  JS::RootedString dep(cx);
+  JS::RootedString depdep(cx);
+  JS::RootedString str2(cx);
+  JS::RootedString dep2(cx);
+  JS::RootedString depdep2(cx);
 
-  
-  
-  JS::RootedString str(cx, JS_NewStringCopyZ(cx, text));
-  CHECK(str);
+  {
+    
+    
+    
+    js::gc::AutoSuppressGC suppress(cx);
 
-  JS::RootedString dep(cx, JS_NewDependentString(cx, str, 10, 100));
-  CHECK(str);
+    original = JS_NewStringCopyZ(cx, text);
+    CHECK(original);
 
-  JS::RootedString depdep(cx, JS_NewDependentString(cx, dep, 10, 80));
-  CHECK(str);
+    
+    
+    str = JS_NewStringCopyZ(cx, text);
+    CHECK(str);
 
-  
-  JS::RootedString str2(cx, JS_NewStringCopyZ(cx, text));
-  CHECK(str);
+    dep = JS_NewDependentString(cx, str, 10, 100);
+    CHECK(dep);
 
-  JS::RootedString dep2(cx, JS_NewDependentString(cx, str2, 10, 100));
-  CHECK(str);
+    depdep = JS_NewDependentString(cx, dep, 10, 80);
+    CHECK(depdep);
 
-  JS::RootedString depdep2(cx, JS_NewDependentString(cx, dep2, 10, 80));
-  CHECK(str);
+    
+    str2 = JS_NewStringCopyZ(cx, text);
+    CHECK(str2);
+
+    dep2 = JS_NewDependentString(cx, str2, 10, 100);
+    CHECK(dep2);
+
+    depdep2 = JS_NewDependentString(cx, dep2, 10, 80);
+    CHECK(depdep2);
+  }
 
   
   
