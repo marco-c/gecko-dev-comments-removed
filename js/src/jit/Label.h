bc60dfab59c83c1ffc5f8e94f32e3d528e81bdb2
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-10-21 17:16:32 +0000

    Bug 1668531 - Part 9: Move jit::Label destructor out-of-line. r=jandem
    Move jit::Label destructor out-of-line, that way we can avoid multiple
    debug-only includes in Label.h.
    
    Differential Revision: https://phabricator.services.mozilla.com/D92056
    
    UltraBlame original commit: 13bccdf723093efb70d28abb29395284a1b88ddd

--- js/src/jit/Label.h
+++ js/src/jit/Label.h
@@ -7,9 +7,9 @@
 #ifndef jit_Label_h
 #define jit_Label_h
 
-#include "jit/CompileWrappers.h"
-#include "jit/JitContext.h"
-#include "js/Utility.h"
+#include "mozilla/Assertions.h"
+
+#include <stdint.h>
 
 namespace js {
 namespace jit {
@@ -77,17 +77,9 @@ struct LabelBase {
 
 class Label : public LabelBase {
  public:
-  ~Label() {
 #ifdef DEBUG
-    
-    JitContext* context = MaybeGetJitContext();
-    bool hadError =
-        js::oom::HadSimulatedOOM() ||
-        (context && context->runtime && context->runtime->hadOutOfMemory()) ||
-        (context && !context->runtime && context->hasOOM());
-    MOZ_ASSERT_IF(!hadError, !used());
+  ~Label();
 #endif
-  }
 };
 
 static_assert(sizeof(Label) == sizeof(uint32_t),
@@ -98,13 +90,13 @@ static_assert(sizeof(Label) == sizeof(uint32_t),
 
 class NonAssertingLabel : public Label {
  public:
-  ~NonAssertingLabel() {
 #ifdef DEBUG
+  ~NonAssertingLabel() {
     if (used()) {
       bind(0);
     }
-#endif
   }
+#endif
 };
 
 }  
