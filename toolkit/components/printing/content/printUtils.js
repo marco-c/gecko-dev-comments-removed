9a58ab36f694665601749b2c820a4539d1d2b897
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-10-21 17:57:23 +0000

    Bug 1669723. Stop unnecessarily catching exceptions from nsIPrintSettingsService.lastUsedPrinterName. r=mstriemer
    Differential Revision: https://phabricator.services.mozilla.com/D92768
    
    UltraBlame original commit: 1e7aade0b8e5ce2110b0c62c8c98552886e84804

--- toolkit/components/printing/content/printUtils.js
+++ toolkit/components/printing/content/printUtils.js
@@ -106,12 +106,11 @@ var PrintUtils = {
     
     
     
-    let lastUsedPrinterName = this._getLastUsedPrinterName();
-    if (!lastUsedPrinterName) {
+    let PSSVC = Cc["@mozilla.org/gfx/printsettings-service;1"].getService(
+      Ci.nsIPrintSettingsService
+    );
+    if (!PSSVC.lastUsedPrinterName) {
       if (printSettings.printerName) {
-        let PSSVC = Cc["@mozilla.org/gfx/printsettings-service;1"].getService(
-          Ci.nsIPrintSettingsService
-        );
         PSSVC.savePrintSettingsToPrefs(
           printSettings,
           false,
@@ -134,20 +133,6 @@ var PrintUtils = {
       return false;
     }
     return true;
-  },
-
-  _getLastUsedPrinterName() {
-    try {
-      let PSSVC = Cc["@mozilla.org/gfx/printsettings-service;1"].getService(
-        Ci.nsIPrintSettingsService
-      );
-
-      return PSSVC.lastUsedPrinterName;
-    } catch (e) {
-      Cu.reportError(e);
-    }
-
-    return null;
   },
 
   getPreviewBrowser(sourceBrowser) {
@@ -729,7 +714,11 @@ var PrintUtils = {
     }
     this._currentPPBrowser = ppBrowser;
     let mm = ppBrowser.messageManager;
-    let lastUsedPrinterName = this._getLastUsedPrinterName();
+
+    let PSSVC = Cc["@mozilla.org/gfx/printsettings-service;1"].getService(
+      Ci.nsIPrintSettingsService
+    );
+    let lastUsedPrinterName = PSSVC.lastUsedPrinterName;
     if (!lastUsedPrinterName) {
       
       
@@ -740,9 +729,6 @@ var PrintUtils = {
       
       let settings = this.getPrintSettings();
       if (settings.printerName) {
-        let PSSVC = Cc["@mozilla.org/gfx/printsettings-service;1"].getService(
-          Ci.nsIPrintSettingsService
-        );
         PSSVC.savePrintSettingsToPrefs(
           settings,
           false,
