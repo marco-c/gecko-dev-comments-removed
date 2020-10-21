c2ece28c28580f24687eb66622f1bce6ea264046
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-10-21 18:53:03 +0000

    Bug 1663523 - [devtools] Add the network event stacktrace watcher r=ochameau,devtools-backward-compat-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D86809
    
    UltraBlame original commit: de71a00c6f689bde4c5d8cbe331dd6f62fd19f76

--- devtools/server/actors/resources/network-events.js
+++ devtools/server/actors/resources/network-events.js
@@ -31,14 +31,11 @@ class NetworkEventWatcher {
 
 
 
-
-
-  async watch(watcherActor, { onAvailable, onUpdated, onDestroyed }) {
+  async watch(watcherActor, { onAvailable, onUpdated }) {
     this.networkEvents = new Map();
     this.watcherActor = watcherActor;
     this.onNetworkEventAvailable = onAvailable;
     this.onNetworkEventUpdated = onUpdated;
-    this.onNeworkEventDestroyed = onDestroyed;
 
     this.listener = new NetworkObserver(
       { browserId: watcherActor.browserId },
@@ -58,7 +55,7 @@ class NetworkEventWatcher {
       this,
       {
         onNetworkEventUpdate: this.onNetworkEventUpdate.bind(this),
-        onNetworkEventDestroy: this.onNeworkEventDestroyed,
+        onNetworkEventDestroy: this.onNetworkEventDestroy.bind(this),
       },
       event
     );
@@ -139,6 +136,12 @@ class NetworkEventWatcher {
         updateType: updateResource.updateType,
       },
     ]);
+  }
+
+  onNetworkEventDestroy(channelId) {
+    if (this.networkEvents.has(channelId)) {
+      this.networkEvents.delete(channelId);
+    }
   }
 
   
