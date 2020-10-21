28ddd647fc8a26d5aea3632d9b0e874442b166fd
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-10-21 18:56:09 +0000

    Bug 1671931 - Let VideoOutput roll its own FrameID generator. r=bwc
    There is a case where two VideoOutputs are feeding the same VideoFrameContainer
    with data: VideoStreamTrack::AddVideoOutput(VideoFrameContainer) and
    VideoStreamTrack::AddVideoOutput(FirstFrameVideoOutput) in HTMLMediaElement.
    This is racy since they both call VideoFrameContainer::NewFrameID().
    
    These VideoOutputs use different ProducerIDs however, so it makes sense for them
    to also use separate FrameID generators.
    
    Differential Revision: https://phabricator.services.mozilla.com/D94054
    
    UltraBlame original commit: d904a6ca71de40aefa269bba752d4d7287ae9abc

--- dom/media/VideoOutput.h
+++ dom/media/VideoOutput.h
@@ -125,6 +125,11 @@ class VideoOutput : public DirectMediaTrackListener {
                                             &VideoFrameContainer::Invalidate));
   }
 
+  FrameID NewFrameID() {
+    mMutex.AssertCurrentThreadOwns();
+    return ++mFrameID;
+  }
+
  public:
   VideoOutput(VideoFrameContainer* aContainer, AbstractThread* aMainThread)
       : mMutex("VideoOutput::mMutex"),
@@ -142,8 +147,7 @@ class VideoOutput : public DirectMediaTrackListener {
         
         mFrames.ClearAndRetainStorage();
       }
-      mFrames.AppendElement(
-          std::make_pair(mVideoFrameContainer->NewFrameID(), *i));
+      mFrames.AppendElement(std::make_pair(NewFrameID(), *i));
       mLastFrameTime = i->mTimeStamp;
     }
 
@@ -197,14 +201,13 @@ class VideoOutput : public DirectMediaTrackListener {
       
       
       for (auto& idChunkPair : mFrames) {
-        idChunkPair.first = mVideoFrameContainer->NewFrameID();
+        idChunkPair.first = NewFrameID();
       }
       if (mFrames.IsEmpty()) {
         VideoSegment v;
         v.AppendFrame(nullptr, gfx::IntSize(640, 480), PRINCIPAL_HANDLE_NONE,
                       true, TimeStamp::Now());
-        mFrames.AppendElement(std::make_pair(mVideoFrameContainer->NewFrameID(),
-                                             *v.GetLastChunk()));
+        mFrames.AppendElement(std::make_pair(NewFrameID(), *v.GetLastChunk()));
       }
       SendFramesEnsureLocked();
     }
@@ -219,6 +222,9 @@ class VideoOutput : public DirectMediaTrackListener {
   
   
   nsTArray<std::pair<ImageContainer::FrameID, VideoChunk>> mFrames;
+  
+  
+  FrameID mFrameID = 0;
   const RefPtr<VideoFrameContainer> mVideoFrameContainer;
   const RefPtr<AbstractThread> mMainThread;
   const layers::ImageContainer::ProducerID mProducerID =
