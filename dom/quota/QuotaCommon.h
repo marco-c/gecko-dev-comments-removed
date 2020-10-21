d12fe310ace3dfd7d3fa9b3c3c2c1c86da239c20
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-10-21 18:53:32 +0000

    Backed out changeset d2a1d34fe1f2 (bug 1671897) for causing bustage on TestQuotaCommon.cpp. CLOSED TREE
    UltraBlame original commit: 1956745be8f4582c9dfd6c0c02cadbf3e175bd54

--- dom/quota/QuotaCommon.h
+++ dom/quota/QuotaCommon.h
@@ -396,25 +396,13 @@
 
 #define QM_PROPAGATE Err(tryTempError)
 
-#ifdef DEBUG
-#  define QM_ASSERT_UNREACHABLE                       \
-    []() -> ::mozilla::GenericErrorResult<nsresult> { \
-      MOZ_CRASH("Should never be reached.");          \
-    }()
+#define QM_ASSERT_UNREACHABLE \
+  [&tryTempError] {           \
+    MOZ_ASSERT(false);        \
+    return Err(tryTempError); \
+  }()
 
-#  define QM_ASSERT_UNREACHABLE_VOID \
-    [] { MOZ_CRASH("Should never be reached."); }()
-#endif
-
-#ifdef MOZ_DIAGNOSTIC_ASSERT_ENABLED
-#  define QM_DIAGNOSTIC_ASSERT_UNREACHABLE            \
-    []() -> ::mozilla::GenericErrorResult<nsresult> { \
-      MOZ_CRASH("Should never be reached.");          \
-    }()
-
-#  define QM_DIAGNOSTIC_ASSERT_UNREACHABLE_VOID \
-    [] { MOZ_CRASH("Should never be reached."); }()
-#endif
+#define QM_ASSERT_UNREACHABLE_VOID [] { MOZ_ASSERT(false); }()
 
 
 
