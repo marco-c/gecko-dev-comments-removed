8281cbe62fc945a46c2262ea5d90dccd10876f36
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-10-21 18:34:23 +0000

    Bug 1671136: Assert to not use doc.write in system privileged contexts or about: pages. r=freddyb
    Differential Revision: https://phabricator.services.mozilla.com/D93485
    
    UltraBlame original commit: 2dff9d72e9ec0838c43a1008cf84141c610fee83

--- dom/base/Document.cpp
+++ dom/base/Document.cpp
@@ -9148,6 +9148,26 @@ void Document::WriteCommon(const Sequence<nsString>& aText,
 
 void Document::WriteCommon(const nsAString& aText, bool aNewlineTerminate,
                            ErrorResult& aRv) {
+#ifdef DEBUG
+  {
+    
+    
+    nsCOMPtr<nsIPrincipal> principal = NodePrincipal();
+    bool isAboutOrPrivContext = principal->IsSystemPrincipal();
+    if (!isAboutOrPrivContext) {
+      if (principal->SchemeIs("about")) {
+        
+        
+        nsAutoCString host;
+        principal->GetHost(host);
+        isAboutOrPrivContext = !host.EqualsLiteral("blank");
+      }
+    }
+    MOZ_ASSERT(!isAboutOrPrivContext,
+               "do not use doc.write in privileged context!");
+  }
+#endif
+
   mTooDeepWriteRecursion =
       (mWriteLevel > NS_MAX_DOCUMENT_WRITE_DEPTH || mTooDeepWriteRecursion);
   if (NS_WARN_IF(mTooDeepWriteRecursion)) {
