78e22c8920ab54e622236286572156fe79189882
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-10-21 17:57:27 +0000

    Bug 1653496 - Picture-in-Picture option not shown for VideoHTMLElement with VideoTrack from Twilio. r=mconley,Gijs
    Differential Revision: https://phabricator.services.mozilla.com/D91544
    
    UltraBlame original commit: 1581160e62e639714efe31ccf89e1f7f3c07d202

---
+++ toolkit/components/pictureinpicture/tests/browser_mediaStreamVideos.js
@@ -0,0 +1,49 @@
+
+
+
+"use strict";
+
+
+
+
+
+add_task(async function test_mediaStreamVideos() {
+  await testToggle(
+    TEST_ROOT + "test-media-stream.html",
+    {
+      "with-controls": { canToggle: true },
+      "no-controls": { canToggle: true },
+    },
+    async browser => {
+      await SpecialPowers.spawn(browser, [], async () => {
+        
+        
+        let newVideo = content.document.createElement("video");
+        newVideo.src = "test-video.mp4";
+        content.document.body.appendChild(newVideo);
+        newVideo.loop = true;
+      });
+      await ensureVideosReady(browser);
+
+      await SpecialPowers.spawn(browser, [], async () => {
+        let newVideo = content.document.body.lastChild;
+        newVideo.play();
+
+        for (let videoID of ["with-controls", "no-controls"]) {
+          let testedVideo = content.document.createElement("video");
+          testedVideo.id = videoID;
+          testedVideo.srcObject = newVideo.mozCaptureStream().clone();
+          content.document.body.prepend(testedVideo);
+          if (
+            testedVideo.readyState < content.HTMLMediaElement.HAVE_ENOUGH_DATA
+          ) {
+            info(`Waiting for 'canplaythrough' for '${testedVideo.id}'`);
+            await ContentTaskUtils.waitForEvent(testedVideo, "canplaythrough");
+          }
+          testedVideo.play();
+          testedVideo.pause();
+        }
+      });
+    }
+  );
+});
