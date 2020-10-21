60b91d4ec54414e88c1c1106e681b8772920dcd5
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-10-21 17:04:16 +0000

    Bug 1662122 - Dispatch timespan metric recording. r=chutten
    Differential Revision: https://phabricator.services.mozilla.com/D90848
    
    UltraBlame original commit: 47e17a661b8e7295443c8d85f63769ce7e55b629

--- toolkit/components/glean/api/src/private/timespan.rs
+++ toolkit/components/glean/api/src/private/timespan.rs
@@ -2,74 +2,89 @@
 
 
 
-use std::sync::RwLock;
+use std::sync::{Arc, RwLock};
 
-use super::CommonMetricData;
+use super::{CommonMetricData, Instant, TimeUnit};
 
-use super::{Instant, TimeUnit};
+use crate::dispatcher;
+use crate::ipc::{need_ipc, MetricId};
 
 
 
 
 #[derive(Debug)]
-pub struct TimespanMetric {
+pub enum TimespanMetric {
+    Parent(Arc<TimespanMetricImpl>),
+    Child(TimespanMetricIpc),
+}
+
+#[derive(Debug)]
+pub struct TimespanMetricImpl {
     inner: RwLock<glean_core::metrics::TimespanMetric>,
 }
+#[derive(Debug)]
+pub struct TimespanMetricIpc(MetricId);
 
 impl TimespanMetric {
     
     pub fn new(meta: CommonMetricData, time_unit: TimeUnit) -> Self {
-        let inner = RwLock::new(glean_core::metrics::TimespanMetric::new(meta, time_unit));
-        Self { inner }
+        if need_ipc() {
+            TimespanMetric::Child(TimespanMetricIpc(MetricId::new(meta)))
+        } else {
+            TimespanMetric::Parent(Arc::new(TimespanMetricImpl::new(meta, time_unit)))
+        }
     }
 
-    
-    
-    
-    
-    
     pub fn start(&self) {
-        crate::with_glean(move |glean| {
-            let now = Instant::now();
-
-            let mut inner = self
-                .inner
-                .write()
-                .expect("lock of wrapped metric was poisoned");
-            inner.set_start(glean, now.as_nanos())
-        })
+        match self {
+            TimespanMetric::Parent(p) => {
+                let now = Instant::now();
+                let metric = Arc::clone(&p);
+                dispatcher::launch(move || metric.start(now));
+            }
+            TimespanMetric::Child(_) => {
+                log::error!(
+                    "Unable to start timespan metric {:?} in non-main process. Ignoring.",
+                    self
+                );
+                
+            }
+        }
     }
 
-    
-    
-    
-    
-    
     pub fn stop(&self) {
-        crate::with_glean(move |glean| {
-            let now = Instant::now();
-
-            let mut inner = self
-                .inner
-                .write()
-                .expect("lock of wrapped metric was poisoned");
-            inner.set_stop(glean, now.as_nanos())
-        })
+        match self {
+            TimespanMetric::Parent(p) => {
+                let now = Instant::now();
+                let metric = Arc::clone(&p);
+                dispatcher::launch(move || metric.stop(now));
+            }
+            TimespanMetric::Child(_) => {
+                log::error!(
+                    "Unable to stop timespan metric {:?} in non-main process. Ignoring.",
+                    self
+                );
+                
+            }
+        }
     }
 
-    
-    
-    
     pub fn cancel(&self) {
-        let mut inner = self
-            .inner
-            .write()
-            .expect("lock of wrapped metric was poisoned");
-        inner.cancel()
+        match self {
+            TimespanMetric::Parent(p) => {
+                let metric = Arc::clone(&p);
+                dispatcher::launch(move || metric.cancel());
+            }
+            TimespanMetric::Child(_) => {
+                log::error!(
+                    "Unable to cancel timespan metric {:?} in non-main process. Ignoring.",
+                    self
+                );
+                
+            }
+        }
     }
 
-    
-    
     
     
     
@@ -81,6 +96,54 @@ impl TimespanMetric {
     
     
     pub fn test_get_value(&self, storage_name: &str) -> Option<u64> {
+        match self {
+            TimespanMetric::Parent(p) => {
+                dispatcher::block_on_queue();
+                p.test_get_value(storage_name)
+            }
+            TimespanMetric::Child(_c) => panic!(
+                "Cannot get test value for {:?} in non-parent process!",
+                self
+            ),
+        }
+    }
+}
+
+impl TimespanMetricImpl {
+    fn new(meta: CommonMetricData, time_unit: TimeUnit) -> Self {
+        let inner = RwLock::new(glean_core::metrics::TimespanMetric::new(meta, time_unit));
+        Self { inner }
+    }
+
+    fn start(&self, now: Instant) {
+        crate::with_glean(move |glean| {
+            let mut inner = self
+                .inner
+                .write()
+                .expect("lock of wrapped metric was poisoned");
+            inner.set_start(glean, now.as_nanos())
+        })
+    }
+
+    fn stop(&self, now: Instant) {
+        crate::with_glean(move |glean| {
+            let mut inner = self
+                .inner
+                .write()
+                .expect("lock of wrapped metric was poisoned");
+            inner.set_stop(glean, now.as_nanos())
+        })
+    }
+
+    fn cancel(&self) {
+        let mut inner = self
+            .inner
+            .write()
+            .expect("lock of wrapped metric was poisoned");
+        inner.cancel()
+    }
+
+    fn test_get_value(&self, storage_name: &str) -> Option<u64> {
         crate::with_glean(move |glean| {
             let inner = self
                 .inner
