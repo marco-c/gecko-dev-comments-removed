5691e9f2b202ffabc1dbc43d87dfa74f11596919
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-10-21 17:40:23 +0000

    Bug 1663128 - part3 : the event source should do the cleanup when it closes. r=chunmin
    When close the event source, it should be responsible to clear up and reset the virtual control interface, rather than doing so by `Media Control Server` via setting some empty results.
    
    Differential Revision: https://phabricator.services.mozilla.com/D92116
    
    UltraBlame original commit: 571dc734bba18b1d92bf13dfde81919bbfecbb16

--- dom/media/mediacontrol/MediaControlKeyManager.cpp
+++ dom/media/mediacontrol/MediaControlKeyManager.cpp
@@ -86,9 +86,20 @@ bool MediaControlKeyManager::StartMonitoringControlKeys() {
 }
 
 void MediaControlKeyManager::StopMonitoringControlKeys() {
-  if (mEventSource && mEventSource->IsOpened()) {
-    LOG_INFO("StopMonitoringControlKeys");
-    mEventSource->Close();
+  if (!mEventSource || !mEventSource->IsOpened()) {
+    return;
+  }
+
+  LOG_INFO("StopMonitoringControlKeys");
+  mEventSource->Close();
+  if (StaticPrefs::media_mediacontrol_testingevents_enabled()) {
+    
+    if (nsCOMPtr<nsIObserverService> obs = services::GetObserverService()) {
+      obs->NotifyObservers(nullptr, "media-displayed-playback-changed",
+                           nullptr);
+      obs->NotifyObservers(nullptr, "media-displayed-metadata-changed",
+                           nullptr);
+    }
   }
 }
 
