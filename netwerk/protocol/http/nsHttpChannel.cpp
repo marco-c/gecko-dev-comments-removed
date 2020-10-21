c628fab834f1928d47cb84b2c60a4dd47d538045
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-10-21 17:34:58 +0000

    Backed out changeset aaf5c5213d8d (bug 1665062) for perma failures on test_http_background_auth_request.html. CLOSED TREE
    UltraBlame original commit: 39f1a0cce418b35dea3beea097297e22840e0e90

--- netwerk/protocol/http/nsHttpChannel.cpp
+++ netwerk/protocol/http/nsHttpChannel.cpp
@@ -356,7 +356,6 @@ nsHttpChannel::nsHttpChannel()
       mCacheOpenWithPriority(false),
       mCacheQueueSizeWhenOpen(0),
       mCachedContentIsValid(false),
-      mIsAuthChannel(false),
       mAuthRetryPending(false),
       mCachedContentIsPartial(false),
       mCacheOnlyMetadata(false),
@@ -2875,7 +2874,6 @@ nsresult nsHttpChannel::ContinueProcessResponse3(nsresult rv) {
       if (rv == NS_ERROR_IN_PROGRESS) {
         
         
-        mIsAuthChannel = true;
         mAuthRetryPending = true;
         if (httpStatus == 407 ||
             (mTransaction && mTransaction->ProxyConnectFailed()))
@@ -2905,7 +2903,6 @@ nsresult nsHttpChannel::ContinueProcessResponse3(nsresult rv) {
         }
         rv = ProcessNormal();
       } else {
-        mIsAuthChannel = true;
         mAuthRetryPending = true;  
       }
       break;
@@ -6224,7 +6221,6 @@ NS_IMETHODIMP nsHttpChannel::OnAuthAvailable() {
   
   
   
-  mIsAuthChannel = true;
   mAuthRetryPending = true;
   mProxyAuthPending = false;
   LOG(("Resuming the transaction, we got credentials from user"));
@@ -7249,12 +7245,6 @@ nsHttpChannel::SetupFallbackChannel(const char* aFallbackKey) {
   mFallbackChannel = true;
   mFallbackKey = aFallbackKey;
 
-  return NS_OK;
-}
-
-NS_IMETHODIMP
-nsHttpChannel::GetIsAuthChannel(bool* aIsAuthChannel) {
-  *aIsAuthChannel = mIsAuthChannel;
   return NS_OK;
 }
 
