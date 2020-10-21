43858a48d5d3d56ac572d4b364385aede481f240
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-10-21 18:37:34 +0000

    Bug 1668139 - part2 : add a telemetry probe to know how many actions are handled by default handler or media session handler. r=chunmin
    It would be good for us to know how many websites actually use MediaSession API to control (play/pause/stop) their media playback, an how many websites use our default handler.
    
    Differential Revision: https://phabricator.services.mozilla.com/D93283
    
    UltraBlame original commit: 82f128a9261ced9bccc723da4d0cbf494ec60508

--- dom/media/mediacontrol/ContentMediaController.cpp
+++ dom/media/mediacontrol/ContentMediaController.cpp
@@ -10,6 +10,7 @@
 #include "mozilla/dom/CanonicalBrowsingContext.h"
 #include "mozilla/dom/ContentChild.h"
 #include "mozilla/StaticPtr.h"
+#include "mozilla/Telemetry.h"
 #include "nsDataHashtable.h"
 #include "nsGlobalWindowOuter.h"
 
@@ -328,6 +329,9 @@ void ContentMediaController::HandleMediaKey(MediaControlKey aKey) {
   }
   LOG("Handle '%s' event, receiver num=%zu", ToMediaControlKeyStr(aKey),
       mReceivers.Length());
+  AccumulateCategorical(
+      mozilla::Telemetry::LABELS_MEDIA_CONTROL_HANDLE_PLAY_PAUSE_STOP_ACTIONS::
+          DefaultHandler);
   
   
   switch (aKey) {
