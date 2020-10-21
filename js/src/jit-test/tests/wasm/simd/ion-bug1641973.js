ab3ce0df8a4230fca133bd095f45ab84cd4459f9
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-10-21 17:42:06 +0000

    Bug 1669668 - Rename instructions in test cases to track spec.  r=jseward
    This depends on a new wast crate landing concurrently.
    
    See https://github.com/WebAssembly/simd/issues/{297,316,332} for
    details of the new names.
    
    The renaming is completely mechanical except for two aspects:
    
    - Previously the tests used the token i8x16.shuffle to trigger a
      syntax error, but that is now the correct token.  The test has been
      updated to use v8x16.shuffle.
    
    - The wast crate has implemented a syntax restriction: a constant lane
      index cannot be prefixed with '+'.  A few tests in spec/simd_lane.js
      were removed.
    
    Any internal renaming will happen later.  We need to do a bigger job
    there anyway.
    
    Differential Revision: https://phabricator.services.mozilla.com/D92749
    
    UltraBlame original commit: f0c02e826fc85992113e6c455c44c6aa365b71e8

--- js/src/jit-test/tests/wasm/simd/ion-bug1641973.js
+++ js/src/jit-test/tests/wasm/simd/ion-bug1641973.js
@@ -8,6 +8,6 @@ new WebAssembly.Module(wasmTextToBinary(`
 (module
   (func (result v128)
     (unreachable)
-    (v8x16.shuffle 0 0 23 0 4 4 4 4 4 16 1 0 4 4 4 4)))
+    (i8x16.shuffle 0 0 23 0 4 4 4 4 4 16 1 0 4 4 4 4)))
 `))
 
