aeb714f5556a029df85894b0bda5dd6699647e43
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-10-21 17:15:44 +0000

    Bug 1668414 - Miscellaneous ResponsiveImageSelector cleanups. r=edgar
    Enum classes and a couple other simplifications.
    
    Differential Revision: https://phabricator.services.mozilla.com/D92132
    
    UltraBlame original commit: c30ee90cac6ae40b8da6b373a9950527e04caaf7

--- dom/base/ResponsiveImageSelector.h
+++ dom/base/ResponsiveImageSelector.h
@@ -153,8 +153,8 @@ class ResponsiveImageCandidate {
   
   bool HasSameParameter(const ResponsiveImageCandidate& aOther) const;
 
-  const nsAString& URLString() const;
-  nsIPrincipal* TriggeringPrincipal() const;
+  const nsAString& URLString() const { return mURLString; }
+  nsIPrincipal* TriggeringPrincipal() const { return mTriggeringPrincipal; }
 
   
   double Density(ResponsiveImageSelector* aSelector) const;
@@ -162,24 +162,30 @@ class ResponsiveImageCandidate {
   
   double Density(double aMatchingWidth) const;
 
-  
-  bool IsComputedFromWidth() const;
+  bool IsValid() const { return mType != CandidateType::Invalid; }
 
-  enum eCandidateType {
-    eCandidateType_Invalid,
-    eCandidateType_Density,
+  
+  bool IsComputedFromWidth() const {
+    return mType == CandidateType::ComputedFromWidth;
+  }
+
+  bool IsDefault() const { return mType == CandidateType::Default; }
+
+  enum class CandidateType : uint8_t {
+    Invalid,
+    Density,
     
     
-    eCandidateType_Default,
-    eCandidateType_ComputedFromWidth
+    Default,
+    ComputedFromWidth
   };
 
-  eCandidateType Type() const { return mType; }
+  CandidateType Type() const { return mType; }
 
  private:
   nsString mURLString;
   nsCOMPtr<nsIPrincipal> mTriggeringPrincipal;
-  eCandidateType mType;
+  CandidateType mType;
   union {
     double mDensity;
     int32_t mWidth;
