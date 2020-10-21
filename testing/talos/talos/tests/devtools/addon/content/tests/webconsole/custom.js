fdd3b0a7ee646b3c88d57e33ded9fcf4bf9f36e7
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-10-21 18:38:07 +0000

    Bug 1593728 Part 2: [devtools] Add a simple log message param and function to a console DAMP test. r=perftest-reviewers,nchevobbe,sparky
    This new param is used to allow testing of simple messages in high volume.
    
    Differential Revision: https://phabricator.services.mozilla.com/D93432
    
    UltraBlame original commit: f3d078512924b6355be2084b438b8874dc88b362

--- testing/talos/talos/tests/devtools/addon/content/tests/webconsole/custom.js
+++ testing/talos/talos/tests/devtools/addon/content/tests/webconsole/custom.js
@@ -1,6 +1,6 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+
+
 
 "use strict";
 
@@ -16,16 +16,18 @@ const { reloadConsoleAndLog } = require("./webconsole-helpers");
 const TEST_URL = PAGES_BASE_URL + "custom/console/index.html";
 
 module.exports = async function() {
-  // These numbers controls the number of console api calls we do in the test
+  
   const sync = 250,
     stream = 250,
-    batch = 250;
-  const params = `?sync=${sync}&stream=${stream}&batch=${batch}`;
+    batch = 250,
+    simple = 5000;
+
+  const params = `?sync=${sync}&stream=${stream}&batch=${batch}&simple=${simple}`;
   const url = TEST_URL + params;
   await testSetup(url, { disableCache: true });
 
   const toolbox = await openToolboxAndLog("custom.webconsole", "webconsole");
-  await reloadConsoleAndLog("custom", toolbox, sync + stream + batch);
+  await reloadConsoleAndLog("custom", toolbox, sync + stream + batch + simple);
   await closeToolboxAndLog("custom.webconsole", toolbox);
 
   await testTeardown();
