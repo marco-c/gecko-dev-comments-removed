9df5955868e59e80df91525da0f8ce5133a2dd7d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-10-21 18:23:41 +0000

    Bug 1662653: [devtools] Use other experimental properties instead of appearance. r=Honza
    Depends on D93070
    
    Differential Revision: https://phabricator.services.mozilla.com/D93297
    
    UltraBlame original commit: 7724055a0dc912aa77e2d943ecef3f3fc289c48a

--- devtools/client/inspector/rules/test/browser_rules_css-compatibility-check-add-fix.js
+++ devtools/client/inspector/rules/test/browser_rules_css-compatibility-check-add-fix.js
@@ -14,13 +14,26 @@
 
 
 
+const TARGET_BROWSERS = [
+  {
+    
+    id: "chrome",
+    version: "84",
+  },
+  {
+    
+    id: "safari_ios",
+    version: "13",
+  },
+];
+
 const TEST_URI = `
 <style>
   div {
     color: green;
     background-color: black;
     user-select: none;
-    -webkit-appearance: none;
+    text-size-adjust: none;
   }
 </style>
 <div>`;
@@ -37,7 +50,7 @@ const TEST_DATA_INITIAL = [
           value: "none",
           expected: COMPATIBILITY_TOOLTIP_MESSAGE.default,
         },
-        "-webkit-appearance": {
+        "text-size-adjust": {
           value: "none",
           expected: COMPATIBILITY_TOOLTIP_MESSAGE.experimental,
         },
@@ -56,7 +69,7 @@ const TEST_DATA_FIX_USER_SELECT = [
         "background-color": { value: "black" },
         "user-select": { value: "none" },
         "-webkit-user-select": { value: "none" },
-        "-webkit-appearance": {
+        "text-size-adjust": {
           value: "none",
           expected: COMPATIBILITY_TOOLTIP_MESSAGE.experimental,
         },
@@ -64,7 +77,6 @@ const TEST_DATA_FIX_USER_SELECT = [
     ],
   },
 ];
-
 
 
 
@@ -79,11 +91,7 @@ const TEST_DATA_FIX_EXPERIMENTAL_SUPPORTED = [
         "background-color": { value: "black" },
         "user-select": { value: "none" },
         "-webkit-user-select": { value: "none" },
-        "-webkit-appearance": {
-          value: "none",
-          expected: COMPATIBILITY_TOOLTIP_MESSAGE["experimental-supported"],
-        },
-        "-moz-appearance": {
+        "text-size-adjust": {
           value: "none",
           expected: COMPATIBILITY_TOOLTIP_MESSAGE["experimental-supported"],
         },
@@ -93,6 +101,10 @@ const TEST_DATA_FIX_EXPERIMENTAL_SUPPORTED = [
 ];
 
 add_task(async function() {
+  await pushPref(
+    "devtools.inspector.compatibility.target-browsers",
+    JSON.stringify(TARGET_BROWSERS)
+  );
   await pushPref(
     "devtools.inspector.ruleview.inline-compatibility-warning.enabled",
     true
@@ -109,9 +121,9 @@ add_task(async function() {
   await runCSSCompatibilityTests(view, inspector, TEST_DATA_FIX_USER_SELECT);
 
   info(
-    'Add -moz-appearance: "none" fixing issue but leaving an inline warning of an experimental property'
+    'Add -webkit-text-size-adjust: "none" fixing issue but leaving an inline warning of an experimental property'
   );
-  await addProperty(view, 1, "-moz-appearance", "none");
+  await addProperty(view, 1, "-webkit-text-size-adjust", "none");
   await runCSSCompatibilityTests(
     view,
     inspector,
