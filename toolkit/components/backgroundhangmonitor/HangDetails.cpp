452b0d2a909d02da0a068038532bf697e9a1bad7
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-10-21 18:54:59 +0000

    Backed out 7 changesets (bug 1661304) for xpcshell failures on test_UserInteraction_annotations.js. CLOSED TREE
    Backed out changeset 10459893968e (bug 1661304)
    Backed out changeset 234f7dd34c95 (bug 1661304)
    Backed out changeset e1954838e36f (bug 1661304)
    Backed out changeset 6f0f76bb873d (bug 1661304)
    Backed out changeset 5186bba48ce7 (bug 1661304)
    Backed out changeset 442ce3a64bab (bug 1661304)
    Backed out changeset 88b46f9a789e (bug 1661304)
    
    UltraBlame original commit: c04a62ee4ed93957fa4bfa21288b3c9642edba12

--- toolkit/components/backgroundhangmonitor/HangDetails.cpp
+++ toolkit/components/backgroundhangmonitor/HangDetails.cpp
@@ -64,47 +64,27 @@ NS_IMETHODIMP
 nsHangDetails::GetAnnotations(JSContext* aCx, JS::MutableHandleValue aVal) {
   
   
-  auto& annotations = mDetails.annotations();
-  size_t length = annotations.Length();
-  JS::RootedObject retObj(aCx, JS::NewArrayObject(aCx, length));
-  if (!retObj) {
+  JS::RootedObject jsAnnotation(aCx, JS_NewPlainObject(aCx));
+  if (!jsAnnotation) {
     return NS_ERROR_OUT_OF_MEMORY;
   }
 
-  for (size_t i = 0; i < length; ++i) {
-    const auto& annotation = annotations[i];
-    JS::RootedObject annotationPair(aCx, JS::NewArrayObject(aCx, 2));
-    if (!annotationPair) {
+  for (auto& annot : mDetails.annotations()) {
+    JSString* jsString =
+        JS_NewUCStringCopyN(aCx, annot.value().get(), annot.value().Length());
+    if (!jsString) {
       return NS_ERROR_OUT_OF_MEMORY;
     }
-
-    JS::RootedString key(aCx, JS_NewUCStringCopyN(aCx, annotation.name().get(),
-                                                  annotation.name().Length()));
-    if (!key) {
-      return NS_ERROR_OUT_OF_MEMORY;
-    }
-
-    JS::RootedString value(aCx,
-                           JS_NewUCStringCopyN(aCx, annotation.value().get(),
-                                               annotation.value().Length()));
-    if (!value) {
-      return NS_ERROR_OUT_OF_MEMORY;
-    }
-
-    if (!JS_DefineElement(aCx, annotationPair, 0, key, JSPROP_ENUMERATE)) {
-      return NS_ERROR_OUT_OF_MEMORY;
-    }
-
-    if (!JS_DefineElement(aCx, annotationPair, 1, value, JSPROP_ENUMERATE)) {
-      return NS_ERROR_OUT_OF_MEMORY;
-    }
-
-    if (!JS_DefineElement(aCx, retObj, i, annotationPair, JSPROP_ENUMERATE)) {
+    JS::RootedValue jsValue(aCx);
+    jsValue.setString(jsString);
+    if (!JS_DefineUCProperty(aCx, jsAnnotation, annot.name().get(),
+                             annot.name().Length(), jsValue,
+                             JSPROP_ENUMERATE)) {
       return NS_ERROR_OUT_OF_MEMORY;
     }
   }
 
-  aVal.setObject(*retObj);
+  aVal.setObject(*jsAnnotation);
   return NS_OK;
 }
 
--- toolkit/components/telemetry/core/Telemetry.cpp
+++ toolkit/components/telemetry/core/Telemetry.cpp
@@ -88,7 +88,6 @@
 #include "TelemetryHistogram.h"
 #include "TelemetryOrigin.h"
 #include "TelemetryScalar.h"
-#include "TelemetryUserInteraction.h"
 
 namespace {
 
@@ -1150,8 +1149,6 @@ already_AddRefed<nsITelemetry> TelemetryImpl::CreateTelemetryInstance() {
   TelemetryEvent::InitializeGlobalState(XRE_IsParentProcess(),
                                         XRE_IsParentProcess());
   TelemetryOrigin::InitializeGlobalState();
-  
-  TelemetryUserInteraction::InitializeGlobalState(useTelemetry, useTelemetry);
 
   
   TelemetryImpl* telemetry = new TelemetryImpl();
@@ -1189,7 +1186,6 @@ void TelemetryImpl::ShutdownTelemetry() {
   TelemetryScalar::DeInitializeGlobalState();
   TelemetryEvent::DeInitializeGlobalState();
   TelemetryOrigin::DeInitializeGlobalState();
-  TelemetryUserInteraction::DeInitializeGlobalState();
   TelemetryIPCAccumulator::DeInitializeGlobalState();
 }
 
