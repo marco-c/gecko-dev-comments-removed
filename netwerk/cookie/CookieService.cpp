b5869d9d40e1f9a5dce67319a648fcd215f58210
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-10-21 17:25:25 +0000

    Backed out changeset 71aa02ea22b6 (bug 1666678) for GTest failures on TestCookieMain
    UltraBlame original commit: 29ce12c1a47c07d6237a2ca1850bf4bbf8e083bd

--- netwerk/cookie/CookieService.cpp
+++ netwerk/cookie/CookieService.cpp
@@ -1700,12 +1700,6 @@ bool CookieService::CheckDomain(CookieStruct& aCookieData, nsIURI* aHostURI,
   aHostURI->GetAsciiHost(hostFromURI);
 
   
-  if (hostFromURI.Contains(':')) {
-    hostFromURI.Insert("[", 0);
-    hostFromURI.Append(']');
-  }
-
-  
   if (!aCookieData.host().IsEmpty()) {
     
     if (aCookieData.host().Length() > 1 && aCookieData.host().First() == '.') {
