12a5b2888c9e97404f772e5cb598863922ded96d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-10-21 18:18:00 +0000

    Bug 1668590: Write CSP test for intent: schemes. r=snorp
    Differential Revision: https://phabricator.services.mozilla.com/D92461
    
    UltraBlame original commit: 36320d8927f2c9ef724ead1566f899a77e7db63a

--- dom/security/test/gtest/TestCSPParser.cpp
+++ dom/security/test/gtest/TestCSPParser.cpp
@@ -399,6 +399,12 @@ TEST(CSPParser, SimplePolicies)
 {
   static const PolicyTest policies[] = {
       
+    { "frame-src intent:",
+      "frame-src intent:" },
+    { "frame-src intent://host.name",
+      "frame-src intent://host.name" },
+    { "frame-src intent://my.host.link/",
+      "frame-src intent://my.host.link/" },
     { "default-src *",
       "default-src *" },
     { "default-src https:",
