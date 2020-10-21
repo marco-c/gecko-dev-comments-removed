5ae694baf7faa31d13b8d1f8293e53a04b485585
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-10-21 19:01:55 +0000

    Bug 1672221: Add null check in NPZCSupport::InputEvent::Run; r=geckoview-reviewers,agi
    Differential Revision: https://phabricator.services.mozilla.com/D94131
    
    UltraBlame original commit: cb586468ae3643c6d130f5168f7c26816b946b72

--- widget/android/nsWindow.cpp
+++ widget/android/nsWindow.cpp
@@ -219,6 +219,12 @@ class NPZCSupport final
       }
 
       nsWindow* const window = win->GetNsWindow();
+      if (!window) {
+        
+        env->ExceptionClear();
+        return;
+      }
+
       window->UserActivity();
       return mLambda(window);
     }
