91852a79133512e9f62fd72de58b5ae06145978e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-10-21 17:06:58 +0000

    Bug 1667863 - Collapse the EventRegions ToString and AppendToString into an ostream operator. r=mattwoodrow
    Depends on D91650
    
    Differential Revision: https://phabricator.services.mozilla.com/D91651
    
    UltraBlame original commit: 8779624b7593fa5baf8f320cd866e44e3db60b62

--- gfx/layers/LayersTypes.h
+++ gfx/layers/LayersTypes.h
@@ -7,6 +7,7 @@
 #ifndef GFX_LAYERSTYPES_H
 #define GFX_LAYERSTYPES_H
 
+#include <iosfwd>    
 #include <stdint.h>  
 #include <stdio.h>   
 
@@ -251,6 +252,7 @@ struct EventRegions {
 
   bool operator==(const EventRegions& aRegions) const;
   bool operator!=(const EventRegions& aRegions) const;
+  friend std::ostream& operator<<(std::ostream& aStream, const EventRegions& e);
 
   void ApplyTranslationAndScale(float aXTrans, float aYTrans, float aXScale,
                                 float aYScale);
@@ -259,8 +261,6 @@ struct EventRegions {
 
   bool IsEmpty() const;
   void SetEmpty();
-
-  nsCString ToString() const;
 };
 
 
