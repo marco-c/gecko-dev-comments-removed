fdb7fc013414bc7cbd45a35b241b3710e1bf8f38
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-11-30 23:58:11 +0000

    Bug 1641595 - inline constants, part 1: shuffle. r=jseward
    Instead of loading instruction-internal simd constants separately (on
    x86/x64) and thus increasing register pressure by requiring temps,
    load them as part of the operation.
    
    This patch adapts the shuffle operation (pshufb).  There's a fair
    amount of plumbing in the assemblers but the change amounts to
    inlining the constant loads into the instruction, getting rid of a
    temp, and making sure we bias in favor of lhs == output.
    
    Differential Revision: https://phabricator.services.mozilla.com/D94556
    
    UltraBlame original commit: 23f94c8ec53b44eb1e3080e5da5026c58a572648

--- js/src/jit/MacroAssembler.h
+++ js/src/jit/MacroAssembler.h
@@ -2051,11 +2051,8 @@ class MacroAssembler : public MacroAssemblerSpecific {
 
   
   inline void shuffleInt8x16(const uint8_t lanes[16], FloatRegister rhs,
-                             FloatRegister lhsDest, FloatRegister temp)
-      DEFINED_ON(x86_shared);
-
-  inline void shuffleInt8x16(const uint8_t lanes[16], FloatRegister rhs,
-                             FloatRegister lhsDest) DEFINED_ON(arm64);
+                             FloatRegister lhsDest)
+      DEFINED_ON(x86_shared, arm64);
 
   
   inline void blendInt8x16(const uint8_t lanes[16], FloatRegister rhs,
