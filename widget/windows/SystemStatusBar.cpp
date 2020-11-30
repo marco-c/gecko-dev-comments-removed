424896b2d643c410d2aa62899dcc2f952e4c1285
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-11-30 23:43:02 +0000

    Bug 1614465 - Revert mistakenly landed changes to SystemStatusBar.cpp
    CLOSED TREE
    
    UltraBlame original commit: 0a719a2effaa979914f9270b7e59966d43333025

--- widget/windows/SystemStatusBar.cpp
+++ widget/windows/SystemStatusBar.cpp
@@ -208,10 +208,6 @@ LRESULT StatusBarEntry::OnMessage(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp) {
       return TRUE;
     }
 
-    double scale = GetDesktopToDeviceScale().scale;
-    int32_t x = NSToIntRound(GET_X_LPARAM(wp) * scale);
-    int32_t y = NSToIntRound(GET_Y_LPARAM(wp) * scale);
-
     
     
     
@@ -221,7 +217,8 @@ LRESULT StatusBarEntry::OnMessage(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp) {
     
     ::SetForegroundWindow(win);
     nsXULPopupManager* pm = nsXULPopupManager::GetInstance();
-    pm->ShowPopupAtScreen(popupFrame->GetContent(), x, y, false, nullptr);
+    pm->ShowPopupAtScreen(popupFrame->GetContent(), GET_X_LPARAM(wp),
+                          GET_Y_LPARAM(wp), false, nullptr);
   }
 
   return DefWindowProc(hWnd, msg, wp, lp);
