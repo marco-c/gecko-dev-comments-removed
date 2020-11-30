111e1b7b05fcb51ebc74d88a4daa30310e08f885
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-11-30 23:49:19 +0000

    Bug 1666519 - Part 1: [devtools] Add localStorage resource r=ochameau
    Tests are added in the next revision of the stack: D93808
    
    Differential Revision: https://phabricator.services.mozilla.com/D92634
    
    UltraBlame original commit: e1c88c0bd8546ba2186181e649a957ff3f58519a

--- devtools/shared/resources/legacy-listeners/storage-utils.js
+++ devtools/shared/resources/legacy-listeners/storage-utils.js
@@ -51,6 +51,7 @@ function makeStorageLegacyListener(storageKey, storageType) {
     }
 
     storage.resourceType = storageType;
+    storage.resourceKey = storageKey;
     
     
     storage.resourceId = storageType;
@@ -66,6 +67,7 @@ function makeStorageLegacyListener(storageKey, storageType) {
         {
           resourceId: storageType,
           resourceType: storageType,
+          resourceKey: storageKey,
           changed: response.changed,
           added: response.added,
           deleted: response.deleted,
