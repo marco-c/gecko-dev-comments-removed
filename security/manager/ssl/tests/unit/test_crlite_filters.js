23e677131bb41a2bc8d5c98d05bb7d519bcbc092
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-11-30 23:49:50 +0000

    Bug 1670985 - don't fall back to OCSP when the CRLite mode is "enforce" r=jcj
    When the CRLite mode is "enforce" and a certificate is found to be covered by
    CRLite, this patch makes it so the implementation will not fall back to
    processing OCSP (whether stapled, cached, or fetched). This also updates
    test_crlite_filters.js to use a more recent, realistic filter and stash.
    
    Differential Revision: https://phabricator.services.mozilla.com/D94499
    
    UltraBlame original commit: 09cb61c1b3eeba6c5dd3d57767aa5961d796874e

--- security/manager/ssl/tests/unit/test_crlite_filters.js
+++ security/manager/ssl/tests/unit/test_crlite_filters.js
@@ -62,8 +62,8 @@ async function syncAndDownload(filters) {
 
   for (let filter of filters) {
     const filename =
-      "test-filter." + (filter.type == "diff" ? "stash" : "crlite");
-    const file = do_get_file(`test_cert_storage_direct/${filename}`);
+      filter.type == "diff" ? "20201017-1-filter.stash" : "20201017-0-filter";
+    const file = do_get_file(`test_crlite_filters/${filename}`);
     const fileBytes = readFile(file);
 
     const record = {
@@ -74,7 +74,7 @@ async function syncAndDownload(filters) {
         hash: getHash(fileBytes),
         size: fileBytes.length,
         filename,
-        location: `security-state-workspace/cert-revocations/test_cert_storage_direct/${filename}`,
+        location: `security-state-workspace/cert-revocations/test_crlite_filters/${filename}`,
         mimetype: "application/octet-stream",
       },
       incremental: filter.type == "diff",
@@ -393,23 +393,13 @@ add_task(
     let certdb = Cc["@mozilla.org/security/x509certdb;1"].getService(
       Ci.nsIX509CertDB
     );
-    let validCertIssuer = constructCertFromFile(
-      "test_cert_storage_direct/valid-cert-issuer.pem"
-    );
-    let revokedCertIssuer = constructCertFromFile(
-      "test_cert_storage_direct/revoked-cert-issuer.pem"
-    );
-    let revokedInStashIssuer = constructCertFromFile(
-      "test_cert_storage_direct/revoked-in-stash-issuer.pem"
-    );
+    let issuerCert = constructCertFromFile("test_crlite_filters/issuer.pem");
     let noSCTCertIssuer = constructCertFromFile(
-      "test_cert_storage_direct/no-sct-issuer.pem"
+      "test_crlite_filters/no-sct-issuer.pem"
     );
 
     let crliteEnrollmentRecords = [
-      getCRLiteEnrollmentRecordFor(validCertIssuer),
-      getCRLiteEnrollmentRecordFor(revokedCertIssuer),
-      getCRLiteEnrollmentRecordFor(revokedInStashIssuer),
+      getCRLiteEnrollmentRecordFor(issuerCert),
       getCRLiteEnrollmentRecordFor(noSCTCertIssuer),
     ];
 
@@ -423,61 +413,62 @@ add_task(
     });
 
     let result = await syncAndDownload([
-      { timestamp: "2019-01-19T00:00:00Z", type: "full", id: "0000" },
+      { timestamp: "2020-10-17T00:00:00Z", type: "full", id: "0000" },
     ]);
     equal(
       result,
-      "finished;2019-01-19T00:00:00Z-full",
+      "finished;2020-10-17T00:00:00Z-full",
       "CRLite filter download should have run"
     );
 
-    let validCert = constructCertFromFile(
-      "test_cert_storage_direct/valid-cert.pem"
-    );
+    let validCert = constructCertFromFile("test_crlite_filters/valid.pem");
+    
+    
+    
+    
+    
     await checkCertErrorGenericAtTime(
       certdb,
       validCert,
       PRErrorCodeSuccess,
       certificateUsageSSLServer,
-      new Date("2019-11-20T00:00:00Z").getTime() / 1000,
+      new Date("2020-10-20T00:00:00Z").getTime() / 1000,
       false,
-      "skynew.jp",
-      Ci.nsIX509CertDB.FLAG_LOCAL_ONLY
+      "vpn.worldofspeed.org",
+      0
     );
 
-    let revokedCert = constructCertFromFile(
-      "test_cert_storage_direct/revoked-cert.pem"
-    );
+    let revokedCert = constructCertFromFile("test_crlite_filters/revoked.pem");
     await checkCertErrorGenericAtTime(
       certdb,
       revokedCert,
       SEC_ERROR_REVOKED_CERTIFICATE,
       certificateUsageSSLServer,
-      new Date("2019-11-20T00:00:00Z").getTime() / 1000,
+      new Date("2020-10-20T00:00:00Z").getTime() / 1000,
       false,
-      "schunk-group.com",
-      Ci.nsIX509CertDB.FLAG_LOCAL_ONLY
+      "us-datarecovery.com",
+      0
     );
 
     
     let revokedInStashCert = constructCertFromFile(
-      "test_cert_storage_direct/revoked-in-stash-cert.pem"
+      "test_crlite_filters/revoked-in-stash.pem"
     );
     await checkCertErrorGenericAtTime(
       certdb,
       revokedInStashCert,
       PRErrorCodeSuccess,
       certificateUsageSSLServer,
-      new Date("2020-11-20T00:00:00Z").getTime() / 1000,
+      new Date("2020-10-20T00:00:00Z").getTime() / 1000,
       false,
-      "gold-g2-valid-cert-demo.swisssign.net",
-      Ci.nsIX509CertDB.FLAG_LOCAL_ONLY
+      "stokedmoto.com",
+      0
     );
 
     result = await syncAndDownload([
-      { timestamp: "2019-01-20T00:00:00Z", type: "full", id: "0000" },
+      { timestamp: "2020-10-17T00:00:00Z", type: "full", id: "0000" },
       {
-        timestamp: "2019-01-20T06:00:00Z",
+        timestamp: "2020-10-17T03:00:00Z",
         type: "diff",
         id: "0001",
         parent: "0000",
@@ -487,7 +478,7 @@ add_task(
     equal(status, "finished", "CRLite filter download should have run");
     deepEqual(
       filters,
-      ["2019-01-20T00:00:00Z-full", "2019-01-20T06:00:00Z-diff"],
+      ["2020-10-17T00:00:00Z-full", "2020-10-17T03:00:00Z-diff"],
       "Should have downloaded the expected CRLite filters"
     );
 
@@ -497,10 +488,10 @@ add_task(
       revokedInStashCert,
       SEC_ERROR_REVOKED_CERTIFICATE,
       certificateUsageSSLServer,
-      new Date("2020-11-20T00:00:00Z").getTime() / 1000,
+      new Date("2020-10-20T00:00:00Z").getTime() / 1000,
       false,
-      "gold-g2-valid-cert-demo.swisssign.net",
-      Ci.nsIX509CertDB.FLAG_LOCAL_ONLY
+      "stokedmoto.com",
+      0
     );
 
     
@@ -509,10 +500,10 @@ add_task(
       validCert,
       PRErrorCodeSuccess,
       certificateUsageSSLServer,
-      new Date("2019-11-20T00:00:00Z").getTime() / 1000,
+      new Date("2020-10-20T00:00:00Z").getTime() / 1000,
       false,
-      "skynew.jp",
-      Ci.nsIX509CertDB.FLAG_LOCAL_ONLY
+      "vpn.worldofspeed.org",
+      0
     );
 
     await checkCertErrorGenericAtTime(
@@ -520,22 +511,18 @@ add_task(
       revokedCert,
       SEC_ERROR_REVOKED_CERTIFICATE,
       certificateUsageSSLServer,
-      new Date("2019-11-20T00:00:00Z").getTime() / 1000,
+      new Date("2020-10-20T00:00:00Z").getTime() / 1000,
       false,
-      "schunk-group.com",
-      Ci.nsIX509CertDB.FLAG_LOCAL_ONLY
+      "us-datarecovery.com",
+      0
     );
 
     
     
     
-    let noSCTCert = constructCertFromFile(
-      "test_cert_storage_direct/no-sct.pem"
-    );
     
     
-    
-    
+    let noSCTCert = constructCertFromFile("test_crlite_filters/no-sct.pem");
     
     
     
@@ -547,7 +534,7 @@ add_task(
       noSCTCert,
       SEC_ERROR_OCSP_SERVER_ERROR,
       certificateUsageSSLServer,
-      new Date("2020-11-20T00:00:00Z").getTime() / 1000,
+      new Date("2020-10-20T00:00:00Z").getTime() / 1000,
       false,
       "mail233.messagelabs.com",
       0
@@ -564,16 +551,20 @@ add_task(
     
     Services.prefs.setIntPref(
       "security.pki.crlite_ct_merge_delay_seconds",
-      60 * 60 * 24 * 360
+      60 * 60 * 24 * 60
     );
+    
+    
+    
+    
     await checkCertErrorGenericAtTime(
       certdb,
       revokedCert,
       PRErrorCodeSuccess,
       certificateUsageSSLServer,
-      new Date("2019-11-20T00:00:00Z").getTime() / 1000,
+      new Date("2020-10-20T00:00:00Z").getTime() / 1000,
       false,
-      "schunk-group.com",
+      "us-datarecovery.com",
       Ci.nsIX509CertDB.FLAG_LOCAL_ONLY
     );
     Services.prefs.clearUserPref("security.pki.crlite_ct_merge_delay_seconds");
