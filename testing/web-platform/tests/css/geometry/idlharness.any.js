0acf03f8a010bab99e5a9d3247c0efeedb43cae6
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-11-30 23:54:33 +0000

    Bug 1662666 [wpt PR 25354] - CSS Geometry: use idl_test and .any.js, a=testonly
    Automatic update from web-platform-tests
    CSS Geometry: use idl_test and .any.js (#25354)
    
    --
    
    wpt-commits: daeb1076798015a0bca4474bc77f1235bfd9612c
    wpt-pr: 25354
    
    UltraBlame original commit: 5b04b63682bbfeba0d01bf21584ee165e694cb3a

---
+++ testing/web-platform/tests/css/geometry/idlharness.any.js
@@ -0,0 +1,32 @@
+
+
+
+
+
+
+
+
+"use strict";
+
+idl_test(
+  ["geometry"],
+  [],
+  idlArray => {
+    const domRectListList = [];
+    if ("document" in self) {
+      domRectListList.push(document.getElementById('log').getClientRects());
+    }
+    idlArray.add_objects({
+      DOMPointReadOnly: ["new DOMPointReadOnly()"],
+      DOMPoint: ["new DOMPoint()"],
+      DOMRectReadOnly: ["new DOMRectReadOnly()"],
+      DOMRect: ["new DOMRect()"],
+      DOMRectList: domRectListList,
+      DOMQuad: ["new DOMQuad()"],
+      DOMMatrixReadOnly: ["new DOMMatrixReadOnly()", "DOMMatrixReadOnly.fromMatrix({is2D: false})"],
+      DOMMatrix: ["new DOMMatrix()", "DOMMatrix.fromMatrix({is2D: false})"]
+    });
+    idlArray.prevent_multiple_testing("DOMMatrixReadOnly");
+    idlArray.prevent_multiple_testing("DOMMatrix");
+  }
+);
