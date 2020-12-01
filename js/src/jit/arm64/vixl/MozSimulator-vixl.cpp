3b0918cb02192cb53fedcf3fd8207fd9ca81c03c
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-12-01 00:35:23 +0000

    Bug 1674714 - jsapi-tests: Use the simulator to execute JIT-ed functions when needed. r=jandem
    Differential Revision: https://phabricator.services.mozilla.com/D95567
    
    UltraBlame original commit: 4f88baca65b49180e0a7e18611dadcbb65e48a2c

--- js/src/jit/arm64/vixl/MozSimulator-vixl.cpp
+++ js/src/jit/arm64/vixl/MozSimulator-vixl.cpp
@@ -272,32 +272,34 @@ int64_t Simulator::call(uint8_t* entry, int argument_count, ...) {
   
   
   if (argument_count == 8) {
-      
-      set_xreg(0, va_arg(parameters, int64_t));
-      
-      set_xreg(1, va_arg(parameters, unsigned));
-      
-      set_xreg(2, va_arg(parameters, int64_t));
-      
-      set_xreg(3, va_arg(parameters, int64_t));
-      
-      set_xreg(4, va_arg(parameters, int64_t));
-      
-      set_xreg(5, va_arg(parameters, int64_t));
-      
-      set_xreg(6, va_arg(parameters, unsigned));
-      
-      set_xreg(7, va_arg(parameters, int64_t));
+    
+    set_xreg(0, va_arg(parameters, int64_t));
+    
+    set_xreg(1, va_arg(parameters, unsigned));
+    
+    set_xreg(2, va_arg(parameters, int64_t));
+    
+    set_xreg(3, va_arg(parameters, int64_t));
+    
+    set_xreg(4, va_arg(parameters, int64_t));
+    
+    set_xreg(5, va_arg(parameters, int64_t));
+    
+    set_xreg(6, va_arg(parameters, unsigned));
+    
+    set_xreg(7, va_arg(parameters, int64_t));
   } else if (argument_count == 2) {
-      
-      set_xreg(0, va_arg(parameters, int64_t));
-      
-      set_xreg(1, va_arg(parameters, int64_t));
+    
+    set_xreg(0, va_arg(parameters, int64_t));
+    
+    set_xreg(1, va_arg(parameters, int64_t));
   } else if (argument_count == 1) { 
-      
-      set_xreg(0, va_arg(parameters, int64_t));
+    
+    set_xreg(0, va_arg(parameters, int64_t));
+  } else if (argument_count == 0) { 
+    
   } else {
-      MOZ_CRASH("Unknown number of arguments");
+    MOZ_CRASH("Unknown number of arguments");
   }
 
   va_end(parameters);
@@ -312,8 +314,9 @@ int64_t Simulator::call(uint8_t* entry, int argument_count, ...) {
   VIXL_ASSERT(entryStack == exitStack);
 
   int64_t result = xreg(0);
-  if (getenv("USE_DEBUGGER"))
-      printf("LEAVE\n");
+  if (getenv("USE_DEBUGGER")) {
+    printf("LEAVE\n");
+  }
   return result;
 }
 
