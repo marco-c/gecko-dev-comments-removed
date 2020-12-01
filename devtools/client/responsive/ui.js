bb48e025efb2d6214fc76380d1afcc9385f814d4
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-12-01 00:38:48 +0000

    Bug 1654758 - [devtools] Remove usage of actorHasMethod in responsive/ui module r=bradwerth
    All the backward compatibility code can be removed.
    
    "setMaxTouchPoints" was added in https://bugzilla.mozilla.org/show_bug.cgi?id=1500672 (FF77)
    "simulateScreenOrientationChange" was added in https://bugzilla.mozilla.org/show_bug.cgi?id=1357774 (FF69)
    "captureScreenshot" was added in https://bugzilla.mozilla.org/show_bug.cgi?id=1579133 (FF71)
    
    Differential Revision: https://phabricator.services.mozilla.com/D95313
    
    UltraBlame original commit: 125ef6dda1eed02e20d4627f4276c52436dcc22a

--- devtools/client/responsive/ui.js
+++ devtools/client/responsive/ui.js
@@ -670,17 +670,10 @@ class ResponsiveUI {
   }
 
   async onScreenshot() {
-    const captureScreenshotSupported = await this.currentTarget.actorHasMethod(
-      "responsive",
-      "captureScreenshot"
-    );
+    const data = await this.responsiveFront.captureScreenshot();
+    await saveScreenshot(this.browserWindow, {}, data);
 
-    if (captureScreenshotSupported) {
-      const data = await this.responsiveFront.captureScreenshot();
-      await saveScreenshot(this.browserWindow, {}, data);
-
-      message.post(this.rdmFrame.contentWindow, "screenshot-captured");
-    }
+    message.post(this.rdmFrame.contentWindow, "screenshot-captured");
   }
 
   onToggleLeftAlignment(event) {
@@ -896,19 +889,11 @@ class ResponsiveUI {
 
 
   async updateScreenOrientation(type, angle, isViewportRotated = false) {
-    const simulateOrientationChangeSupported = await this.currentTarget.actorHasMethod(
-      "responsive",
-      "simulateScreenOrientationChange"
+    await this.responsiveFront.simulateScreenOrientationChange(
+      type,
+      angle,
+      isViewportRotated
     );
-
-    
-    if (simulateOrientationChangeSupported) {
-      await this.responsiveFront.simulateScreenOrientationChange(
-        type,
-        angle,
-        isViewportRotated
-      );
-    }
 
     
     if (!isViewportRotated) {
@@ -923,14 +908,7 @@ class ResponsiveUI {
 
 
   async updateMaxTouchPointsEnabled(touchSimulationEnabled) {
-    const setMaxTouchPointsSupported = await this.currentTarget.actorHasMethod(
-      "responsive",
-      "setMaxTouchPoints"
-    );
-
-    if (setMaxTouchPointsSupported) {
-      await this.responsiveFront.setMaxTouchPoints(touchSimulationEnabled);
-    }
+    return this.responsiveFront.setMaxTouchPoints(touchSimulationEnabled);
   }
 
   
