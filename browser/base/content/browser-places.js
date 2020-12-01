5c90085e5a37bbe3eb046f59e95af979ae68b46b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-12-01 00:43:04 +0000

    Backed out changeset e52abe08f115 (bug 1673403) for multiple bc perma failures. CLOSED TREE
    UltraBlame original commit: b03957242e70e0803fc99db6016c372b42b91aac

--- browser/base/content/browser-places.js
+++ browser/base/content/browser-places.js
@@ -1688,24 +1688,6 @@ var BookmarkingUI = {
     if (aWindow == window) {
       this._uninitView();
       this._isCustomizing = true;
-
-      if (
-        !Services.prefs.getBoolPref("browser.toolbars.bookmarks.2h2020", false)
-      ) {
-        return;
-      }
-
-      let isVisible =
-        Services.prefs.getCharPref(
-          "browser.toolbars.bookmarks.visibility",
-          "newtab"
-        ) != "never";
-      
-      
-      
-      
-      let toolbar = document.getElementById("PersonalToolbar");
-      setToolbarVisibility(toolbar, isVisible, false);
     }
   },
 
