b17e5b2af15782bc83b861849062b0dadcbd4967
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-12-01 00:03:30 +0000

    Bug 1607588 - Remove supression for seemingly fixed issue. r=decoder
    Differential Revision: https://phabricator.services.mozilla.com/D94853
    
    UltraBlame original commit: ac0770d9bdfabd9d6d11b42ba1ec1add586e2b85

--- mozglue/build/TsanOptions.cpp
+++ mozglue/build/TsanOptions.cpp
@@ -264,10 +264,6 @@ extern "C" const char* __tsan_default_suppressions() {
          "race:nsJARChannel::Resume\n"
 
          
-         "race:nssSlot_GetToken\n"
-         "race:nssToken_Destroy\n"
-
-         
          "race:nsUrlClassifierDBServiceWorker::OpenDb\n"
          "race:nsUrlClassifierDBServiceWorker::Shutdown\n"
 
