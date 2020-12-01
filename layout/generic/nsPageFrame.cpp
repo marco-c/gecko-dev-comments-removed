b2d048a309c5076bde914492bb834462c362e6dc
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-12-01 03:13:17 +0000

    Bug 1669333 - Make the fragmentation fallback code use logical coordinates.  r=jfkthame
    Differential Revision: https://phabricator.services.mozilla.com/D97864
    
    UltraBlame original commit: ef0dabb6cb6603bc3816faea6a414f97e78da4d2

--- layout/generic/nsPageFrame.cpp
+++ layout/generic/nsPageFrame.cpp
@@ -551,12 +551,13 @@ static nsTArray<PageAndOffset> GetPreviousPagesWithOverflow(
 
   nsPageContentFrame* pageCF = aPage;
   
-  nscoord offsetToCurrentPageTop = 0;
+  nscoord offsetToCurrentPageBStart = 0;
+  const auto wm = pageCF->GetWritingMode();
   while ((pageCF = GetPreviousPageContentFrame(pageCF))) {
-    offsetToCurrentPageTop += pageCF->GetSize().Height();
+    offsetToCurrentPageBStart += pageCF->BSize(wm);
 
     if (pageCF->HasOverflowAreas()) {
-      pages.EmplaceBack(pageCF, offsetToCurrentPageTop);
+      pages.EmplaceBack(pageCF, offsetToCurrentPageBStart);
     }
   }
 
@@ -570,28 +571,30 @@ static void BuildPreviousPageOverflow(nsDisplayListBuilder* aBuilder,
   const auto previousPagesAndOffsets =
       GetPreviousPagesWithOverflow(aCurrentPageCF);
 
+  const auto wm = aCurrentPageCF->GetWritingMode();
   for (const PageAndOffset& pair : Reversed(previousPagesAndOffsets)) {
     auto* prevPageCF = pair.first;
-    const nscoord offsetToCurrentPageTop = pair.second;
-    const auto inkOverflow = prevPageCF->InkOverflowRectRelativeToSelf();
-    const auto remainingOverflow = inkOverflow.YMost() - offsetToCurrentPageTop;
-
+    const nscoord offsetToCurrentPageBStart = pair.second;
+    const LogicalRect inkOverflow(
+        wm, prevPageCF->InkOverflowRectRelativeToSelf(), prevPageCF->GetSize());
+    const auto remainingOverflow =
+        inkOverflow.BEnd(wm) - offsetToCurrentPageBStart;
     if (remainingOverflow <= 0) {
       continue;
     }
 
     
     
-    nsRect overflowRect = inkOverflow;
-    overflowRect.y = offsetToCurrentPageTop;
-    overflowRect.height =
-        std::min(remainingOverflow, prevPageCF->GetSize().Height());
+    LogicalRect overflowRect(inkOverflow);
+    overflowRect.BStart(wm) = offsetToCurrentPageBStart;
+    overflowRect.BSize(wm) = std::min(remainingOverflow, prevPageCF->BSize(wm));
 
     {
       
       
       const nsRect visibleRect =
-          overflowRect + prevPageCF->GetOffsetTo(aPageFrame);
+          overflowRect.GetPhysicalRect(wm, aPageFrame->GetSize()) +
+          prevPageCF->GetOffsetTo(aPageFrame);
       nsDisplayListBuilder::AutoBuildingDisplayList buildingForChild(
           aBuilder, aPageFrame, visibleRect, visibleRect);
 
@@ -601,10 +604,12 @@ static void BuildPreviousPageOverflow(nsDisplayListBuilder* aBuilder,
       
       
       
-      const nsPoint pageOffset = aCurrentPageCF->GetOffsetTo(prevPageCF);
-      const nsPoint additionalOffset(pageOffset.X(),
-                                     pageOffset.Y() - offsetToCurrentPageTop);
-      buildingForChild.SetAdditionalOffset(additionalOffset);
+      const nsSize containerSize = aPageFrame->GetSize();
+      LogicalPoint pageOffset(wm, aCurrentPageCF->GetOffsetTo(prevPageCF),
+                              containerSize);
+      pageOffset.B(wm) -= offsetToCurrentPageBStart;
+      buildingForChild.SetAdditionalOffset(
+          pageOffset.GetPhysicalPoint(wm, containerSize));
 
       aPageFrame->BuildDisplayListForChild(aBuilder, prevPageCF, aLists);
     }
