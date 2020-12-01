1839a4b37b51c3674fe2d8a21c7f0427f25e48cb
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-12-01 00:05:34 +0000

    Bug 1660054 - Hook up the zoom-reset action to also reset APZ/scaling zoom. r=Gijs,nika,botond
    The goal here is to hook up the ctrl+0 keyboard shortcut to reset the scaling
    zoom applied by pinch gestures (on touchscreen or trackpad), in addition to
    resetting the reflow zoom (aka full zoom). This patch also makes other mechanisms
    to reset the reflow zoom (e.g. clicking on the "100%" label in the hamburger menu)
    also reset scaling zoom, which I think makes sense for consistency.
    
    Most of this patch is just plumbing, but I'm unfamiliar with these codepaths
    so requesting review from relevant owners to make sure it's sane.
    
    Differential Revision: https://phabricator.services.mozilla.com/D94624
    
    UltraBlame original commit: 952da6bff88756cb68e515b88d134e22c831a566

--- docshell/base/CanonicalBrowsingContext.h
+++ docshell/base/CanonicalBrowsingContext.h
@@ -246,6 +246,8 @@ class CanonicalBrowsingContext final : public BrowsingContext {
 
   void HistoryCommitIndexAndLength();
 
+  void ResetScalingZoom();
+
  protected:
   
   void CanonicalDiscard();
