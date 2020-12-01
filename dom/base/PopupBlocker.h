d747b6ea3415ea1802e13031860a5ffbf04eba42
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-12-01 03:13:45 +0000

    Bug 1678389 - Use transient user activation for <input> file/color picker. r=edgar,smaug
    This matches other browsers, see [1], and should fix this and other
    similar bugs like bug 1610726.
    
    [1]: https://source.chromium.org/chromium/chromium/src/+/master:third_party/blink/renderer/core/html/forms/file_input_type.cc;l=163;drc=8a5de62dd01360cfe3d150640d3ff4d974bbe842
    
    Differential Revision: https://phabricator.services.mozilla.com/D97640
    
    UltraBlame original commit: ec16588c816311440ce78d54c06edfd492fec41f

--- dom/base/PopupBlocker.h
+++ dom/base/PopupBlocker.h
@@ -44,10 +44,6 @@ class PopupBlocker final {
   static void PopupStatePusherCreated();
   static void PopupStatePusherDestroyed();
 
-  
-  
-  static bool CanShowPopupByPermission(nsIPrincipal* aPrincipal);
-
   static uint32_t GetPopupPermission(nsIPrincipal* aPrincipal);
 
   
