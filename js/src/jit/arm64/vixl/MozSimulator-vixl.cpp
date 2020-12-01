f0039481a1f09641251188257fb779489ca2d378
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-12-01 01:00:04 +0000

    Bug 1674714 - jsapi-tests: Use the simulator to execute JIT-ed functions when needed. r=jandem
    - Add the ability to call generated function with no arguments.
    - Add infered signature of js::wasm::PrintF32 to all Simulators.
    - Correctly return using the link register, and save it on platform where this
      is needed
    
    Differential Revision: https://phabricator.services.mozilla.com/D95567
    
    UltraBlame original commit: e9d035b60fe397f4f92a59fe225222ef9418ccca

--- js/src/jit/arm64/vixl/MozSimulator-vixl.cpp
+++ js/src/jit/arm64/vixl/MozSimulator-vixl.cpp
@@ -272,32 +272,34 @@ int64_t Simulator::call(uint8_t* entry, int argument_count, ...) {
   
   
   if (argument_count == 8) {
-      
-      set_xreg(0, va_arg(parameters, int64_t));
-      
-      set_xreg(1, va_arg(parameters, unsigned));
-      
-      set_xreg(2, va_arg(parameters, int64_t));
-      
-      set_xreg(3, va_arg(parameters, int64_t));
-      
-      set_xreg(4, va_arg(parameters, int64_t));
-      
-      set_xreg(5, va_arg(parameters, int64_t));
-      
-      set_xreg(6, va_arg(parameters, unsigned));
-      
-      set_xreg(7, va_arg(parameters, int64_t));
+    
+    set_xreg(0, va_arg(parameters, int64_t));
+    
+    set_xreg(1, va_arg(parameters, unsigned));
+    
+    set_xreg(2, va_arg(parameters, int64_t));
+    
+    set_xreg(3, va_arg(parameters, int64_t));
+    
+    set_xreg(4, va_arg(parameters, int64_t));
+    
+    set_xreg(5, va_arg(parameters, int64_t));
+    
+    set_xreg(6, va_arg(parameters, unsigned));
+    
+    set_xreg(7, va_arg(parameters, int64_t));
   } else if (argument_count == 2) {
-      
-      set_xreg(0, va_arg(parameters, int64_t));
-      
-      set_xreg(1, va_arg(parameters, int64_t));
+    
+    set_xreg(0, va_arg(parameters, int64_t));
+    
+    set_xreg(1, va_arg(parameters, int64_t));
   } else if (argument_count == 1) { 
-      
-      set_xreg(0, va_arg(parameters, int64_t));
+    
+    set_xreg(0, va_arg(parameters, int64_t));
+  } else if (argument_count == 0) { 
+    
   } else {
-      MOZ_CRASH("Unknown number of arguments");
+    MOZ_CRASH("Unknown number of arguments");
   }
 
   va_end(parameters);
@@ -312,8 +314,9 @@ int64_t Simulator::call(uint8_t* entry, int argument_count, ...) {
   VIXL_ASSERT(entryStack == exitStack);
 
   int64_t result = xreg(0);
-  if (getenv("USE_DEBUGGER"))
-      printf("LEAVE\n");
+  if (getenv("USE_DEBUGGER")) {
+    printf("LEAVE\n");
+  }
   return result;
 }
 
@@ -496,6 +499,7 @@ typedef int64_t (*Prototype_Int_IntDoubleIntInt)(uint64_t arg0, double arg1,
                                                  uint64_t arg2, uint64_t arg3);
 
 typedef float (*Prototype_Float32_Float32)(float arg0);
+typedef int64_t (*Prototype_Int_Float32)(float arg0);
 typedef float (*Prototype_Float32_Float32Float32)(float arg0, float arg1);
 
 typedef double (*Prototype_Double_None)();
@@ -705,6 +709,11 @@ Simulator::VisitCallRedirection(const Instruction* instr)
     case js::jit::Args_Float32_Float32: {
       float ret = reinterpret_cast<Prototype_Float32_Float32>(nativeFn)(s0);
       setFP32Result(ret);
+      break;
+    }
+    case js::jit::Args_Int_Float32: {
+      int64_t ret = reinterpret_cast<Prototype_Int_Float32>(nativeFn)(s0);
+      setGPR64Result(ret);
       break;
     }
     case js::jit::Args_Float32_Float32Float32: {
