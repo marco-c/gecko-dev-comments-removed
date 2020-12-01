32dfa27ba3288b5017fcd831ab0efd4fd12871a6
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-12-01 01:54:53 +0000

    Bug 1676843 - Make innerWidth/innerHeight return the actual CSS size rather than a rounded version of it. r=heycam
    On Nightly, for now. This allows websites to get the real viewport size.
    
    The rounded size has caused problems in the past (see bug 1648298 / bug
    1648265), and changing it would be ideal.
    
    I told the CSSWG that we can experiment with this on Nightly to
    alleviate (or prove) compat concerns, in which case we'd need to use a
    different solution, see https://github.com/w3c/csswg-drafts/issues/5260.
    
    Differential Revision: https://phabricator.services.mozilla.com/D96826
    
    UltraBlame original commit: ed3ef7c26759e4a4187c65527e789ccb0fbf773d

--- dom/base/nsScreen.cpp
+++ dom/base/nsScreen.cpp
@@ -297,9 +297,16 @@ nsresult nsScreen::GetWindowInnerRect(nsRect& aRect) {
   if (!win) {
     return NS_ERROR_FAILURE;
   }
-  nsresult rv = win->GetInnerWidth(&aRect.width);
+  double width;
+  double height;
+  nsresult rv = win->GetInnerWidth(&width);
   NS_ENSURE_SUCCESS(rv, rv);
-  return win->GetInnerHeight(&aRect.height);
+  rv = win->GetInnerHeight(&height);
+  NS_ENSURE_SUCCESS(rv, rv);
+  
+  
+  aRect.SizeTo(std::round(width), std::round(height));
+  return NS_OK;
 }
 
 bool nsScreen::ShouldResistFingerprinting() const {
