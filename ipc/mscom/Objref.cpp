3c972a62158fe2f8641ef824eac9ff514bac72db
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-12-01 00:38:56 +0000

    Bug 1674902: Part 2 - Use CLSID_NULL instead of a distinct null CLSID; r=Jamie
    Just a quick patch to reference an existing zeroed-out GUID instead of
    instantiating a new one.
    
    Differential Revision: https://phabricator.services.mozilla.com/D95598
    
    UltraBlame original commit: 35bbc3ee78622ae0b78941f729f0270734c9bc24

--- ipc/mscom/Objref.cpp
+++ ipc/mscom/Objref.cpp
@@ -262,8 +262,7 @@ bool StripHandlerFromOBJREF(NotNull<IStream*> aStream, const uint64_t aStartPos,
 
   
   
-  CLSID zeroClsid = {0};
-  hr = aStream->Write(&zeroClsid, sizeof(CLSID), &bytesWritten);
+  hr = aStream->Write(&CLSID_NULL, sizeof(CLSID), &bytesWritten);
   if (FAILED(hr) || bytesWritten != sizeof(CLSID)) {
     return false;
   }
