b9659736580e95b2c6b579b7e159440e46dedb41
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-12-01 00:11:11 +0000

    Bug 1671473 - Implement IPC for UUID metric type. r=chutten
    Differential Revision: https://phabricator.services.mozilla.com/D94935
    
    UltraBlame original commit: f5ea75ac7bfed6029ee5d47be87f5996e16dc076

--- toolkit/components/glean/api/src/private/uuid.rs
+++ toolkit/components/glean/api/src/private/uuid.rs
@@ -2,20 +2,39 @@
 
 
 
+use std::sync::Arc;
+
 use uuid::Uuid;
 
 use super::CommonMetricData;
 
+use crate::{
+    dispatcher,
+    ipc::{need_ipc, MetricId},
+};
 
 
+
+
+#[derive(Debug)]
+pub enum UuidMetric {
+    Parent(Arc<UuidMetricImpl>),
+    Child(UuidMetricIpc),
+}
 
 #[derive(Clone, Debug)]
-pub struct UuidMetric(glean_core::metrics::UuidMetric);
+pub struct UuidMetricImpl(pub(crate) glean_core::metrics::UuidMetric);
+#[derive(Debug)]
+pub struct UuidMetricIpc(MetricId);
 
 impl UuidMetric {
     
     pub fn new(meta: CommonMetricData) -> Self {
-        Self(glean_core::metrics::UuidMetric::new(meta))
+        if need_ipc() {
+            UuidMetric::Child(UuidMetricIpc(MetricId::new(meta)))
+        } else {
+            UuidMetric::Parent(Arc::new(UuidMetricImpl::new(meta)))
+        }
     }
 
     
@@ -24,12 +43,36 @@ impl UuidMetric {
     
     
     pub fn set(&self, value: Uuid) {
-        crate::with_glean(move |glean| self.0.set(glean, value))
+        match self {
+            UuidMetric::Parent(p) => {
+                let metric = Arc::clone(&p);
+                dispatcher::launch(move || metric.set(value));
+            }
+            UuidMetric::Child(_c) => {
+                log::error!(
+                    "Unable to set UUID metric {:?} in non-main process. Ignoring.",
+                    self
+                );
+                
+            }
+        };
     }
 
     
-    pub fn generate_and_set(&self) -> Uuid {
-        crate::with_glean(move |glean| self.0.generate_and_set(glean))
+    pub fn generate_and_set(&self) {
+        match self {
+            UuidMetric::Parent(p) => {
+                let metric = Arc::clone(&p);
+                dispatcher::launch(move || metric.generate_and_set());
+            }
+            UuidMetric::Child(_c) => {
+                log::error!(
+                    "Unable to set UUID metric {:?} in non-main process. Ignoring.",
+                    self
+                );
+                
+            }
+        };
     }
 
     
@@ -45,7 +88,35 @@ impl UuidMetric {
     
     
     pub fn test_get_value(&self, storage_name: &str) -> Option<Uuid> {
+        match self {
+            UuidMetric::Parent(p) => {
+                dispatcher::block_on_queue();
+                p.test_get_value(storage_name).map(|uuid| {
+                    Uuid::parse_str(&uuid).expect("can't parse internally created UUID value")
+                })
+            }
+            UuidMetric::Child(_c) => panic!(
+                "Cannot get test value for {:?} in non-parent process!",
+                self
+            ),
+        }
+    }
+}
+
+impl UuidMetricImpl {
+    fn new(meta: CommonMetricData) -> Self {
+        Self(glean_core::metrics::UuidMetric::new(meta))
+    }
+
+    fn set(&self, value: Uuid) {
+        crate::with_glean(move |glean| self.0.set(glean, value));
+    }
+
+    fn generate_and_set(&self) {
+        crate::with_glean(move |glean| self.0.generate_and_set(glean));
+    }
+
+    fn test_get_value(&self, storage_name: &str) -> Option<String> {
         crate::with_glean(move |glean| self.0.test_get_value(glean, storage_name))
-            .map(|uuid| Uuid::parse_str(&uuid).expect("can't parse internally created UUID value"))
     }
 }
