9fa52a345aeed3dddce20f08a6cdb4a77f929d09
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-12-01 02:24:54 +0000

    Bug 1670217 [wpt PR 26062] - [WPT] Add service worker cross-origin and redirect import tests, a=testonly
    Automatic update from web-platform-tests
    [WPT] Add service worker cross-origin and redirect import tests
    
    This CL adds WPT tests for:
    
    - Cross-origin importScripts() from classic service workers
    - Cross-origin static imports from module workers
    - Redirecting static imports from module workers
        - Failing on service workers on Chromium, while
          redirecting importScripts() also fails on Chromium
          due to https://crbug.com/889798.
    
    Bug: 1136767, 889798
    Change-Id: I318275c7772ab2854c972774f5ba49ca22f0018a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2463046
    Commit-Queue: Hiroshige Hayashizaki <hiroshigechromium.org>
    Reviewed-by: Hiroki Nakagawa <nhirokichromium.org>
    Cr-Commit-Position: refs/heads/master{#827534}
    
    --
    
    wpt-commits: d12d5f6e022ea8052c76b285930c536c35f5a144
    wpt-pr: 26062
    
    UltraBlame original commit: 8f61ccbd06a40e6b59d3025f95d34e31e6c11128

--- testing/web-platform/tests/workers/modules/resources/static-import-redirect-worker.js
+++ testing/web-platform/tests/workers/modules/resources/static-import-redirect-worker.js
@@ -1,5 +1,6 @@
 
-import * as module from 'https://{{domains[www1]}}:{{ports[https][0]}}/workers/modules/resources/export-on-load-script.py';
+
+import * as module from './redirect.py?location=/workers/modules/resources/export-on-load-script.js';
 if ('DedicatedWorkerGlobalScope' in self &&
     self instanceof DedicatedWorkerGlobalScope) {
   self.onmessage = e => {
