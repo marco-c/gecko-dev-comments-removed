74e366bacfa13116a4f48b5f042cd94b88ce2156
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-12-01 02:59:45 +0000

    Bug 1645528 - Connect nsRefreshDrivers in content processes with a widget-local vsync source r=mattwoodrow,emilio
    To allow `requestAnimationFrame()` and similar things to run at monitor
    speed if there is only a window-specific vsyncsource available.
    This is the case for Wayland and, in the future, EGL/X11. Other backends
    may opt for window specific vsyncsources as well at some point.
    
    The idea is to, instead of using global vsync objects, expose a vsyncsource
    from nsWindow and use it for refresh drivers. For the content process, move
    VsyncChild to BrowserChild, so for each Browserchild there is only one
    VsyncChild to which all refresh drivers connect.
    
    IPC in managed either by PBrowser or PBackground. Right now, PBrowser is
    only used on Wayland, as both PBrowser and the Wayland vsyncsource run
    on the main thread. Other backends keep using the background thread for
    now.
    
    While at it, make it so that we constantly update the refresh rate. This
    is necessary for Wayland, but also on other platforms variable refresh rates
    are increasingly common. When using PVsync, limit updates to once in every
    250ms in order to minimize overhead while still updating fast.
    
    How to test:
     - run the Wayland backend
     - enable `widget.wayland_vsync.enabled`
     - optionally: disable `privacy.reduceTimerPrecision`
     - run `vsynctester.com` or `testufo.com`
    
    Expected results:
    Instead of fixed 60Hz, things should update at monitor refresh rate -
    e.g. 144Hz
    
    Original patch by Kenny Levinsen.
    
    Differential Revision: https://phabricator.services.mozilla.com/D93173
    
    UltraBlame original commit: 1f42c376724db880e20a9a5285a1ab063f814f3a

--- dom/ipc/VsyncParent.h
+++ dom/ipc/VsyncParent.h
@@ -4,36 +4,31 @@
 
 
 
-#ifndef mozilla_layout_ipc_VsyncParent_h
-#define mozilla_layout_ipc_VsyncParent_h
+#ifndef mozilla_dom_ipc_VsyncParent_h
+#define mozilla_dom_ipc_VsyncParent_h
 
-#include "mozilla/layout/PVsyncParent.h"
+#include "mozilla/dom/PVsyncParent.h"
 #include "mozilla/VsyncDispatcher.h"
 #include "nsCOMPtr.h"
 #include "mozilla/RefPtr.h"
+#include "VsyncSource.h"
 
 class nsIThread;
 
-namespace mozilla {
-
-namespace ipc {
-class BackgroundParentImpl;
-}  
-
-namespace layout {
+namespace mozilla::dom {
 
 
 
 
 class VsyncParent final : public PVsyncParent, public VsyncObserver {
-  friend class mozilla::ipc::BackgroundParentImpl;
   friend class PVsyncParent;
 
- private:
-  static already_AddRefed<VsyncParent> Create();
-
+ public:
   VsyncParent();
-  virtual ~VsyncParent();
+  void UpdateVsyncSource(const RefPtr<gfx::VsyncSource>& aVsyncSource);
+
+ private:
+  virtual ~VsyncParent() = default;
 
   virtual bool NotifyVsync(const VsyncEvent& aVsync) override;
   mozilla::ipc::IPCResult RecvRequestVsyncRate();
@@ -44,13 +39,16 @@ class VsyncParent final : public PVsyncParent, public VsyncObserver {
 
   void DispatchVsyncEvent(const VsyncEvent& aVsync);
 
+  bool IsOnInitialThread();
+  void AssertIsOnInitialThread();
+
   bool mObservingVsync;
   bool mDestroyed;
-  nsCOMPtr<nsIThread> mBackgroundThread;
+  nsCOMPtr<nsIThread> mInitialThread;
+  RefPtr<gfx::VsyncSource> mVsyncSource;
   RefPtr<RefreshTimerVsyncDispatcher> mVsyncDispatcher;
 };
 
-}  
 }  
 
 #endif  
