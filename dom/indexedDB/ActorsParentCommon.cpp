61a66e18733d30a1aecd0df0c77210828892b990
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-12-01 02:18:02 +0000

    Backed out 8 changesets (bug 1671367) for ActorsParent.cpp related bustage CLOSED TREE
    Backed out changeset cacebe0fc34c (bug 1671367)
    Backed out changeset 37e2f1c36fc7 (bug 1671367)
    Backed out changeset c4a3fca623aa (bug 1671367)
    Backed out changeset 75e95ebade6a (bug 1671367)
    Backed out changeset bfc08841dafb (bug 1671367)
    Backed out changeset 445d4fd6004d (bug 1671367)
    Backed out changeset 90e38a8f77d6 (bug 1671367)
    Backed out changeset 7e5910c8a82e (bug 1671367)
    
    UltraBlame original commit: 280a06ab6f11d14d2dea3ca0ecb269c0dd873476

--- dom/indexedDB/ActorsParentCommon.cpp
+++ dom/indexedDB/ActorsParentCommon.cpp
@@ -415,20 +415,20 @@ GetStructuredCloneReadInfoFromExternalBlob(uint64_t aIntData,
     const nsCOMPtr<nsIFile> nativeFile = file.FileInfo().GetFileForFileInfo();
     IDB_TRY(OkIf(nativeFile), Err(NS_ERROR_FAILURE));
 
-    IDB_TRY_INSPECT(
-        const auto& fileInputStream,
-        NS_NewLocalFileInputStream(nativeFile)
-            .andThen([aMaybeKey](auto fileInputStream)
-                         -> Result<nsCOMPtr<nsIInputStream>, nsresult> {
-              if (aMaybeKey) {
-                return nsCOMPtr<nsIInputStream>{MakeRefPtr<
-                    quota::DecryptingInputStream<IndexedDBCipherStrategy>>(
-                    WrapNotNull(std::move(fileInputStream)),
-                    kEncryptedStreamBlockSize, *aMaybeKey)};
-              }
+    
+    
+    
+    
+    nsCOMPtr<nsIInputStream> fileInputStream;
+    IDB_TRY(NS_NewLocalFileInputStream(getter_AddRefs(fileInputStream),
+                                       nativeFile));
 
-              return fileInputStream;
-            }));
+    if (aMaybeKey) {
+      fileInputStream =
+          MakeRefPtr<quota::DecryptingInputStream<IndexedDBCipherStrategy>>(
+              WrapNotNull(std::move(fileInputStream)),
+              kEncryptedStreamBlockSize, *aMaybeKey);
+    }
 
     IDB_TRY(SnappyUncompressStructuredCloneData(*fileInputStream, data));
   }
