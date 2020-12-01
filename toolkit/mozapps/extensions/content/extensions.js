8889fd71a477e01e9ca93728ff4d74b84dfefded
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-12-01 02:22:31 +0000

    Bug 1677526 - Simplify isLoading property in gViewController. r=rpl
    Differential Revision: https://phabricator.services.mozilla.com/D97176
    
    UltraBlame original commit: 630eb9c351166a98884068bbdecb7e3c14de1886

--- toolkit/mozapps/extensions/content/extensions.js
+++ toolkit/mozapps/extensions/content/extensions.js
@@ -234,6 +234,7 @@ var gViewController = {
   currentViewId: "",
   currentViewObj: null,
   currentViewRequest: 0,
+  isLoading: true,
   
   
   
@@ -302,12 +303,6 @@ var gViewController = {
     var matchRegex = /^addons:\/\/([^\/]+)\/(.*)$/;
     var [, viewType, viewParam] = aViewId.match(matchRegex) || [];
     return { type: viewType, param: decodeURIComponent(viewParam) };
-  },
-
-  get isLoading() {
-    return (
-      !this.currentViewObj || this.currentViewObj.node.hasAttribute("loading")
-    );
   },
 
   loadView(aViewId) {
@@ -402,7 +397,7 @@ var gViewController = {
         if (canHide === false) {
           return;
         }
-        this.displayedView.removeAttribute("loading");
+        this.isLoading = false;
       } catch (e) {
         
         Cu.reportError(e);
@@ -413,7 +408,7 @@ var gViewController = {
     this.currentViewObj = viewObj;
 
     this.displayedView = this.currentViewObj;
-    this.currentViewObj.node.setAttribute("loading", "true");
+    this.isLoading = true;
 
     recordViewTelemetry(view.param);
 
@@ -437,7 +432,7 @@ var gViewController = {
   },
 
   notifyViewChanged() {
-    this.displayedView.removeAttribute("loading");
+    this.isLoading = false;
 
     if (this.viewChangeCallback) {
       this.viewChangeCallback();
