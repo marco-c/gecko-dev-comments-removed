b6f54f890956a9b8e27118bcb4324f31b46b29ca
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-12-01 01:24:45 +0000

    Bug 1675016 - Add more test for HTTP3 failure r=necko-reviewers,kershaw
    - Add 2 test: 1) server is not listening to the port and 2) server is not responding that will cause the connection to timeout and fall back to HTTP2
     - Adds a server that only reads packets but never sends any to simulate a handshake timing out
    
    Differential Revision: https://phabricator.services.mozilla.com/D95816
    
    UltraBlame original commit: ac23fff376cf0eb21d9443ba49d89eddc097399b

--- netwerk/test/unit/test_http3_error_before_connect.js
+++ netwerk/test/unit/test_http3_error_before_connect.js
@@ -15,36 +15,10 @@ registerCleanupFunction(async () => {
   dump("cleanup done\n");
 });
 
-let Http3FailedListener = function() {};
-
-Http3FailedListener.prototype = {
-  onStartRequest: function testOnStartRequest(request) {},
-
-  onDataAvailable: function testOnDataAvailable(request, stream, off, cnt) {
-    this.amount += cnt;
-    read_stream(stream, cnt);
-  },
-
-  onStopRequest: function testOnStopRequest(request, status) {
-    if (Components.isSuccessCode(status)) {
-      
-      let httpVersion = "";
-      try {
-        httpVersion = request.protocolVersion;
-      } catch (e) {}
-      Assert.equal(httpVersion, "h2");
-      this.finish(false);
-    } else {
-      Assert.equal(status, Cr.NS_ERROR_NET_PARTIAL_TRANSFER);
-      this.finish(true);
-    }
-  },
-};
-
 function chanPromise(chan, listener) {
   return new Promise(resolve => {
-    function finish(result) {
-      resolve(result);
+    function finish() {
+      resolve();
     }
     listener.finish = finish;
     chan.asyncOpen(listener);
@@ -77,14 +51,14 @@ add_task(async function test_fatal_error() {
 
   let h2Port = env.get("MOZHTTP2_PORT");
   Assert.notEqual(h2Port, null);
-
-  let h3Port = env.get("MOZHTTP3_PORT_FAILED");
+  let h3Port = env.get("MOZHTTP3_PORT_NO_RESPONSE");
   Assert.notEqual(h3Port, null);
   Assert.notEqual(h3Port, "");
 
   Services.prefs.setBoolPref("network.http.http3.enabled", true);
   Services.prefs.setCharPref("network.dns.localDomains", "foo.example.com");
   Services.prefs.setBoolPref("network.dns.disableIPv6", true);
+  
   Services.prefs.setCharPref(
     "network.http.http3.alt-svc-mapping-for-testing",
     "foo.example.com;h3-27=:" + h3Port
@@ -99,7 +73,9 @@ add_task(async function test_fatal_error() {
 });
 
 add_task(async function test_fatal_stream_error() {
-  let result = false;
+  let result = 1;
+  
+  
   
   
   do {
@@ -107,9 +83,10 @@ add_task(async function test_fatal_stream_error() {
     
     Services.obs.notifyObservers(null, "net:prune-all-connections");
     let chan = makeChan();
-    let listener = new Http3FailedListener();
-    result = await altsvcSetupPromise(chan, listener);
-  } while (result === false);
+    let listener = new CheckOnlyHttp2Listener();
+    await altsvcSetupPromise(chan, listener);
+    result++;
+  } while (result < 5);
 });
 
 let CheckOnlyHttp2Listener = function() {};
@@ -128,7 +105,7 @@ CheckOnlyHttp2Listener.prototype = {
       httpVersion = request.protocolVersion;
     } catch (e) {}
     Assert.equal(httpVersion, "h2");
-    this.finish(false);
+    this.finish();
   },
 };
 
