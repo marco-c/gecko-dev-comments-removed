ba1f0a2cf959a93d6775c4cd0796a132876c39ba
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-12-01 02:22:52 +0000

    Bug 1675349: Forbid elements representing blocked plugins from accepting focus r=jmathies
    As part of the fallback for unsupported plugin elements, we deny them focus.  Since elements can be programmatically changed, and object/embed elements can change internal "type" by updating their data/src attributes, and because that type is decided asynchronously, we may need to give up focus if our element has it.
    
    Differential Revision: https://phabricator.services.mozilla.com/D95903
    
    UltraBlame original commit: 2f3eaf9c342cd7ad71050728ec564b8c854633a4

--- dom/html/HTMLEmbedElement.cpp
+++ dom/html/HTMLEmbedElement.cpp
@@ -164,6 +164,17 @@ nsresult HTMLEmbedElement::AfterMaybeChangeAttr(int32_t aNamespaceID,
 bool HTMLEmbedElement::IsHTMLFocusable(bool aWithMouse, bool* aIsFocusable,
                                        int32_t* aTabIndex) {
   
+  if ((Type() == eType_Null) &&
+      (PluginFallbackType() == eFallbackBlockAllPlugins)) {
+    if (aTabIndex) {
+      *aTabIndex = -1;
+    }
+
+    *aIsFocusable = false;
+    return false;
+  }
+
+  
   
   if (aTabIndex) {
     *aTabIndex = TabIndex();
