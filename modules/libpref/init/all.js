8df89d05c45acc55fe54068e3664fcaa5aeb8ca1
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-12-01 00:00:11 +0000

    Bug 1673086 - Enable PDF forms by default on release. r=jaws
    Differential Revision: https://phabricator.services.mozilla.com/D94644
    
    UltraBlame original commit: a947b98efd10380133d3b0fab9db672743fd5d19

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -349,11 +349,7 @@ pref("browser.chrome.image_icons.max_size", 1024);
 pref("browser.triple_click_selects_paragraph", true);
 
 
-#ifdef EARLY_BETA_OR_EARLIER
-  pref("pdfjs.renderInteractiveForms", true);
-#else
-  pref("pdfjs.renderInteractiveForms", false);
-#endif
+pref("pdfjs.renderInteractiveForms", true);
 
 
 pref("mathml.disabled",    false);
