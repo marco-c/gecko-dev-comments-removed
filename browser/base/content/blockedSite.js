6d7b61cff39298852113a1d98fbbb96d4ae3a6db
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-12-01 00:34:45 +0000

    Bug 1672936 - Do not show "report a detection problem" in Safe Browsing malware interstitial page r=flod,fluent-reviewers,gcp
    Differential Revision: https://phabricator.services.mozilla.com/D94574
    
    UltraBlame original commit: 5ca9b886ee8cf8e281917f2bc22ca47e0778544e

--- browser/base/content/blockedSite.js
+++ browser/base/content/blockedSite.js
@@ -115,9 +115,19 @@ function initPage() {
     document.getElementById("report_detection").remove();
   }
 
-  document.l10n.setAttributes(innerDesc, innerDescL10nID, {
-    sitename: getHostString(),
-  });
+  
+  
+  
+  let descriptionMapping = {
+    malware: innerDescL10nID + "-sumo",
+  };
+  document.l10n.setAttributes(
+    innerDesc,
+    descriptionMapping[error] || innerDescL10nID,
+    {
+      sitename: getHostString(),
+    }
+  );
 
   
   
