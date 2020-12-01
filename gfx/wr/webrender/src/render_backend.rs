61d9d8ecd63794295c5d6bfae9b46f3b2b9166c9
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-12-01 02:47:09 +0000

    Backed out changeset e9ac2e3b9176 (bug 1675136) for causing bug 1677691.
    UltraBlame original commit: dbe6164c7ebd99eec9b78f1e95581288c41f924a

--- gfx/wr/webrender/src/render_backend.rs
+++ gfx/wr/webrender/src/render_backend.rs
@@ -1479,13 +1479,16 @@ impl RenderBackend {
         
         
         
-        
-        
         if invalidate_rendered_frame {
             doc.rendered_frame_is_valid = false;
+            if let CompositorKind::Draw { max_partial_present_rects, .. } = doc.scene.config.compositor_kind {
 
-            
-            doc.dirty_rects_are_valid = false;
+              
+              if max_partial_present_rects > 0 {
+                  let msg = ResultMsg::ForceRedraw;
+                  self.result_tx.send(msg).unwrap();
+              }
+            }
         }
 
         let mut frame_build_time = None;
