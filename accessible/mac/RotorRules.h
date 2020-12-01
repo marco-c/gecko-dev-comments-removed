e1a9b32e35ad1904234bdaa7b1a12a34b395b0b6
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-12-01 01:51:18 +0000

    Bug 1198336 - P3: Implement AXLiveRegionSearchKey. r=morgan
    I think VoiceOver uses this to pull in all live regions so it
    can check for deletions when they change later on.
    
    Depends on D96292
    
    Differential Revision: https://phabricator.services.mozilla.com/D96293
    
    UltraBlame original commit: 852dc98e8cf5d230b3c718396f0ed28d431dc6d3

--- accessible/mac/RotorRules.h
+++ accessible/mac/RotorRules.h
@@ -113,6 +113,15 @@ class RotorHeadingLevelRule : public RotorRoleRule {
   int32_t mLevel;
 };
 
+class RotorLiveRegionRule : public RotorRule {
+ public:
+  explicit RotorLiveRegionRule(AccessibleOrProxy& aDirectDescendantsFrom)
+      : RotorRule(aDirectDescendantsFrom) {}
+  explicit RotorLiveRegionRule() : RotorRule() {}
+
+  uint16_t Match(const AccessibleOrProxy& aAccOrProxy) override;
+};
+
 
 
 
