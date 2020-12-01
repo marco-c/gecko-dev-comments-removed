40df81b978b90515a66b62cd733effe517b7ed6b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-12-01 02:46:41 +0000

    Backed out changeset f6a72042dd56 (bug 1677293) for failures on fullscreen.py. CLOSED TREE
    UltraBlame original commit: fd1683e51ec5eae6a5c5b516492d6a81eb06e7ea

--- gfx/config/gfxConfigManager.cpp
+++ gfx/config/gfxConfigManager.cpp
@@ -30,7 +30,6 @@ void gfxConfigManager::Init() {
   EmplaceUserPref("gfx.webrender.compositor", mWrCompositorEnabled);
   mWrForceEnabled = gfxPlatform::WebRenderPrefEnabled();
   mWrForceDisabled = StaticPrefs::gfx_webrender_force_disabled_AtStartup();
-  mWrSoftwareForceEnabled = StaticPrefs::gfx_webrender_software_AtStartup();
   mWrCompositorForceEnabled =
       StaticPrefs::gfx_webrender_compositor_force_enabled_AtStartup();
   mGPUProcessAllowSoftware =
@@ -66,7 +65,6 @@ void gfxConfigManager::Init() {
   mIsNightly = true;
 #endif
   mSafeMode = gfxPlatform::InSafeMode();
-  mIsHeadless = gfxPlatform::IsHeadless();
 
   mGfxInfo = services::GetGfxInfo();
 
@@ -115,21 +113,8 @@ void gfxConfigManager::ConfigureWebRenderSoftware() {
 
   mFeatureWrSoftware->EnableByDefault();
 
-  
-  
-  
-  
-  if (mWrSoftwareForceEnabled) {
+  if (StaticPrefs::gfx_webrender_software_AtStartup()) {
     mFeatureWrSoftware->UserForceEnable("Force enabled by pref");
-  } else if (mWrEnvForceEnabled) {
-    mFeatureWrSoftware->UserDisable(
-        "User force-enabled full WR",
-        "FEATURE_FAILURE_USER_FORCE_ENABLED_FULL_WR"_ns);
-  } else if (mWrForceDisabled || mWrEnvForceDisabled) {
-    
-    
-    mFeatureWrSoftware->UserDisable("User force-disabled WR",
-                                    "FEATURE_FAILURE_USER_FORCE_DISABLED"_ns);
   }
 
   nsCString failureId;
@@ -145,10 +130,6 @@ void gfxConfigManager::ConfigureWebRenderSoftware() {
   switch (status) {
     case nsIGfxInfo::FEATURE_ALLOW_ALWAYS:
     case nsIGfxInfo::FEATURE_ALLOW_QUALIFIED:
-      if (mIsHeadless) {
-        mFeatureWrSoftware->Disable(FeatureStatus::Blocked,
-                                    "Headless not yet supported", failureId);
-      }
       break;
     case nsIGfxInfo::FEATURE_DENIED:
       mFeatureWrSoftware->Disable(FeatureStatus::Denied, "Not on allowlist",
