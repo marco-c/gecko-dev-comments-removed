f405842b77ca3d7f3efea8b13fa7e256754c387e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-12-01 02:08:52 +0000

    Bug 1198336 - P2: Implement live region AX attributes and AXLiveRegionCreated notification. r=morgan
    We use an ivar boolean flag to cache the existance of a live region. This lets us
    hide the live region AX attributes and avoid sync round trips to content.
    
    Depends on D96291
    
    Differential Revision: https://phabricator.services.mozilla.com/D96292
    
    UltraBlame original commit: f1d53dd3f6a54f574217ea7959a06d936e0e9784

--- accessible/mac/mozAccessible.h
+++ accessible/mac/mozAccessible.h
@@ -59,6 +59,8 @@ inline mozAccessible* GetNativeFromGeckoAccessible(
   uint64_t mCachedState;
 
   nsStaticAtom* mARIARole;
+
+  bool mIsLiveRegion;
 }
 
 
@@ -133,6 +135,8 @@ inline mozAccessible* GetNativeFromGeckoAccessible(
 
 - (id<MOXTextMarkerSupport>)moxTextMarkerDelegate;
 
+- (BOOL)moxIsLiveRegion;
+
 
 
 
@@ -182,6 +186,15 @@ inline mozAccessible* GetNativeFromGeckoAccessible(
 
 
 - (NSString*)moxARIACurrent;
+
+
+- (NSNumber*)moxARIAAtomic;
+
+
+- (NSString*)moxARIALive;
+
+
+- (NSString*)moxARIARelevant;
 
 
 - (id)moxTitleUIElement;
