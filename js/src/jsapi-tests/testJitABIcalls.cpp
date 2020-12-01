761b25e12999910da0e7180c53ba4095c30f777f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-12-01 00:35:20 +0000

    Bug 1674436 - Share Prepare/Execute functions across JIT test cases. r=jandem
    Differential Revision: https://phabricator.services.mozilla.com/D95384
    
    UltraBlame original commit: 2188f200b16a6df86cf0c937dfd1f7becde60925

--- js/src/jsapi-tests/testJitABIcalls.cpp
+++ js/src/jsapi-tests/testJitABIcalls.cpp
@@ -19,6 +19,7 @@
 #include "js/Value.h"
 
 #include "jsapi-tests/tests.h"
+#include "jsapi-tests/testsJit.h"
 
 #include "jit/ABIFunctionList-inl.h"
 #include "jit/MacroAssembler-inl.h"
@@ -615,55 +616,6 @@ JSAPITest* DefineCheckArgs<Res (*)(Args...)>::instance_ = nullptr;
 template <typename Res, typename... Args>
 bool* DefineCheckArgs<Res (*)(Args...)>::reportTo_ = nullptr;
 
-typedef void (*EnterTest)();
-
-
-static void Prepare(MacroAssembler& masm) {
-#if defined(JS_CODEGEN_ARM64)
-  masm.Mov(PseudoStackPointer64, sp);
-  masm.SetStackPointer64(PseudoStackPointer64);
-#endif
-  AllocatableRegisterSet regs(RegisterSet::All());
-  LiveRegisterSet save(regs.asLiveSet());
-  masm.PushRegsInMask(save);
-}
-
-
-
-static bool Execute(JSContext* cx, MacroAssembler& masm) {
-  AllocatableRegisterSet regs(RegisterSet::All());
-  LiveRegisterSet save(regs.asLiveSet());
-  masm.PopRegsInMask(save);
-#if defined(JS_CODEGEN_ARM64)
-  
-  masm.abiret();
-
-  
-  masm.SetStackPointer64(PseudoStackPointer64);
-#else
-  masm.ret();  
-#endif
-
-  if (masm.oom()) {
-    return false;
-  }
-
-  Linker linker(masm);
-  JitCode* code = linker.newCode(cx, CodeKind::Other);
-  if (!code) {
-    return false;
-  }
-  if (!ExecutableAllocator::makeExecutableAndFlushICache(
-          FlushICacheSpec::LocalThreadOnly, code->raw(), code->bufferSize())) {
-    return false;
-  }
-
-  JS::AutoSuppressGCAnalysis suppress;
-  EnterTest test = code->as<EnterTest>();
-  test();
-  return true;
-}
-
 
 
 
@@ -682,7 +634,7 @@ class JitABICall final : public JSAPITest, public DefineCheckArgs<Sig> {
     this->set_instance(this, &result);
 
     StackMacroAssembler masm(cx);
-    Prepare(masm);
+    PrepareJit(masm);
 
     AllocatableGeneralRegisterSet regs(GeneralRegisterSet::All());
     
@@ -713,7 +665,7 @@ class JitABICall final : public JSAPITest, public DefineCheckArgs<Sig> {
 
     this->generateCalls(masm, base, setup);
 
-    if (!Execute(cx, masm)) {
+    if (!ExecuteJit(cx, masm)) {
       return false;
     }
     this->set_instance(nullptr, nullptr);
