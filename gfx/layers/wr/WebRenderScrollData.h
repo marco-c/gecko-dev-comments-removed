ae1b880d10a2bbba4f5c6ed63363d76fdedc143e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-12-01 01:43:54 +0000

    Bug 1662800 - Make the WebRenderScrollData dump nicer. r=botond
    This makes the WebRenderScrollData dump more analogous to the layer tree dump,
    in that it prints the layer entries one per line showing in-order nested tree
    structure. It also omits printing things if they're not important.
    
    Differential Revision: https://phabricator.services.mozilla.com/D96312
    
    UltraBlame original commit: 77c19223a20f1ed0df92c8d26ae50467d9c783f8

--- gfx/layers/wr/WebRenderScrollData.h
+++ gfx/layers/wr/WebRenderScrollData.h
@@ -8,6 +8,7 @@
 #define GFX_WEBRENDERSCROLLDATA_H
 
 #include <map>
+#include <iosfwd>
 
 #include "chrome/common/ipc_message_utils.h"
 #include "FrameMetrics.h"
@@ -161,7 +162,7 @@ class WebRenderLayerScrollData final {
   }
   bool IsAsyncZoomContainer() const { return mAsyncZoomContainerId.isSome(); }
 
-  void Dump(const WebRenderScrollData& aOwner) const;
+  void Dump(std::ostream& aOut, const WebRenderScrollData& aOwner) const;
 
   friend struct IPC::ParamTraits<WebRenderLayerScrollData>;
 
@@ -240,12 +241,17 @@ class WebRenderScrollData final {
 
   friend struct IPC::ParamTraits<WebRenderScrollData>;
 
-  void Dump() const;
+  friend std::ostream& operator<<(std::ostream& aOut,
+                                  const WebRenderScrollData& aData);
 
  private:
   
   
   bool RepopulateMap();
+
+  
+  void DumpSubtree(std::ostream& aOut, size_t aIndex,
+                   const std::string& aIndent) const;
 
  private:
   
