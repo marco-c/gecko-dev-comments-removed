720e754e92569915ecc19e10b130efb2876e98a2
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-12-01 02:41:50 +0000

    Bug 1677759: Mark STUN/TURN client contexts failed when we encounter a socket read/write failure for TCP. r=mjf
    Differential Revision: https://phabricator.services.mozilla.com/D97637
    
    UltraBlame original commit: 4a5126ca5dc0390b3f1d031ca71623ef34301c9e

--- dom/media/webrtc/transport/third_party/nICEr/src/stun/nr_socket_buffered_stun.c
+++ dom/media/webrtc/transport/third_party/nICEr/src/stun/nr_socket_buffered_stun.c
@@ -604,6 +604,12 @@ abort:
   if (_status && _status != R_WOULDBLOCK) {
     r_log(LOG_GENERIC, LOG_ERR, "Failure in writable_cb: %d", _status);
     nr_socket_buffered_stun_failed(sock);
+    
+
+
+    if (sock->readable_cb) {
+      sock->readable_cb(s, NR_ASYNC_WAIT_READ, sock->readable_cb_arg);
+    }
   } else if (sock->pending) {
     nr_socket_buffered_stun_arm_writable_cb(sock);
   }
