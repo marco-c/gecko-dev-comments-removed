3c7598da71a237b2f8158980dbf9db42ed0afdb0
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-12-01 00:54:50 +0000

    Bug 1674845 - part4 : add 'browser' subfolder for autoplay browser tests. r=bryce
    subsuite media for browser tests.
    
    Differential Revision: https://phabricator.services.mozilla.com/D95881
    
    UltraBlame original commit: 8f20a34998c6b0ea8b849f2f3f6bf5b3ec47983a

--- dom/media/autoplay/test/browser/browser_autoplay_policy_user_gestures.js
+++ dom/media/autoplay/test/browser/browser_autoplay_policy_user_gestures.js
@@ -1,7 +1,6 @@
 
 
-const VIDEO_PAGE =
-  "https://example.com/browser/toolkit/content/tests/browser/file_video.html";
+const VIDEO_PAGE = GetTestWebBasedURL("file_video.html");
 
 const UserGestures = {
   MOUSE_CLICK: "mouse-click",
