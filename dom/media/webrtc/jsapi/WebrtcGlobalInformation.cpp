4ea2271823705e2e57303c1ed0cd5ceebf2b2927
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-12-03 23:08:44 +0000

    Bug 1626278: Filter stats for closed PeerConnections the same way we do for live ones. r=ng
    Differential Revision: https://phabricator.services.mozilla.com/D88498
    
    UltraBlame original commit: 4b2382b083335ae5d5ce4a6a41eff9d42a33c8e1

--- dom/media/webrtc/jsapi/WebrtcGlobalInformation.cpp
+++ dom/media/webrtc/jsapi/WebrtcGlobalInformation.cpp
@@ -96,8 +96,10 @@ GetStatsPromiseForThisProcess(const nsAString& aPcIdFilter) {
 
     
     for (const auto& report : ctx->mStatsForClosedPeerConnections) {
-      promises.AppendElement(dom::RTCStatsReportPromise::CreateAndResolve(
-          MakeUnique<dom::RTCStatsReportInternal>(report), __func__));
+      if (aPcIdFilter.IsEmpty() || aPcIdFilter == report.mPcid) {
+        promises.AppendElement(dom::RTCStatsReportPromise::CreateAndResolve(
+            MakeUnique<dom::RTCStatsReportInternal>(report), __func__));
+      }
     }
   }
 
