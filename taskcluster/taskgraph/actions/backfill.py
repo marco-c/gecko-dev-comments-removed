900585a72319fa4b5c441a1aaa576968c1fffcbb
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-12-14 12:26:47 +0000

    Bug 1681720 - fix addition of suffix '-bk' to backfilled tasks. r=aki,jmaher
    Differential Revision: https://phabricator.services.mozilla.com/D99347
    
    UltraBlame original commit: c6bc15934cab2a693d5fc8d5074dd7c68a9d747f

--- taskcluster/taskgraph/actions/backfill.py
+++ taskcluster/taskgraph/actions/backfill.py
@@ -160,12 +160,14 @@ def test_manifests_modifier(task, label, symbol, revision, test_manifests):
     task.task["metadata"]["name"] = task.label
     th_info = task.task["extra"]["treeherder"]
     
-    add_backfill_suffix(
+    th_info["symbol"] = add_backfill_suffix(
         SYMBOL_REGEX, th_info["symbol"], "-{}-bk".format(revision[0:11])
     )
     if th_info.get("groupSymbol"):
         
-        add_backfill_suffix(GROUP_SYMBOL_REGEX, th_info["groupSymbol"], "-bk")
+        th_info["groupSymbol"] = add_backfill_suffix(
+            GROUP_SYMBOL_REGEX, th_info["groupSymbol"], "-bk"
+        )
     task.task["tags"]["action"] = "backfill-task"
     return task
 
