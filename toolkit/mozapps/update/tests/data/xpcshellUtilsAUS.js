1e835a1c7cf7c2cb6b78028abf5afed7237d80d7
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-12-14 11:53:28 +0000

    Bug 1553982 Part 5 - Add a test for the update semaphore and make existing tests support it. r=bytesized,nalexander
    Differential Revision: https://phabricator.services.mozilla.com/D95630
    
    UltraBlame original commit: a207ff8ae135478dac66d7928d41a4b729a7c8f2

--- toolkit/mozapps/update/tests/data/xpcshellUtilsAUS.js
+++ toolkit/mozapps/update/tests/data/xpcshellUtilsAUS.js
@@ -1619,7 +1619,15 @@ function getSpecialFolderDir(aCSIDL) {
   );
 
   let aryPath = ctypes.char16_t.array()(260);
-  SHGetSpecialFolderPath(0, aryPath, aCSIDL, false);
+  let rv = SHGetSpecialFolderPath(0, aryPath, aCSIDL, false);
+  if (!rv) {
+    do_throw(
+      "SHGetSpecialFolderPath failed to retrieve " +
+        aCSIDL +
+        " with Win32 error " +
+        ctypes.winLastError
+    );
+  }
   lib.close();
 
   let path = aryPath.readString(); 
@@ -4346,6 +4354,7 @@ function adjustGeneralPaths() {
   let ds = Services.dirsvc.QueryInterface(Ci.nsIDirectoryService);
   ds.QueryInterface(Ci.nsIProperties).undefine(NS_GRE_DIR);
   ds.QueryInterface(Ci.nsIProperties).undefine(NS_GRE_BIN_DIR);
+  ds.QueryInterface(Ci.nsIProperties).undefine(XRE_EXECUTABLE_FILE);
   ds.registerProvider(dirProvider);
   registerCleanupFunction(function AGP_cleanup() {
     debugDump("start - unregistering directory provider");
@@ -4403,8 +4412,25 @@ function adjustGeneralPaths() {
     ds.unregisterProvider(dirProvider);
     cleanupTestCommon();
 
+    
+    
+    
+    
+    let syncManager = Cc[
+      "@mozilla.org/updates/update-sync-manager;1"
+    ].getService(Ci.nsIUpdateSyncManager);
+    syncManager.resetSemaphore();
+
     debugDump("finish - unregistering directory provider");
   });
+
+  
+  
+  debugDump("resetting update semaphore");
+  let syncManager = Cc["@mozilla.org/updates/update-sync-manager;1"].getService(
+    Ci.nsIUpdateSyncManager
+  );
+  syncManager.resetSemaphore();
 }
 
 
