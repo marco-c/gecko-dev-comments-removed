e006c8f9a3f8b5bed329cdf094b5d8b29f3644e8
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-12-14 11:54:33 +0000

    Bug 1519100 - Move HandlerFunction from streams into the builtin dir; r=mgaudet
    Differential Revision: https://phabricator.services.mozilla.com/D95883
    
    UltraBlame original commit: ff6fd518d03515025e83f5ebe6b9d0d07c3e3402

--- js/src/builtin/streams/ReadableStreamOperations.cpp
+++ js/src/builtin/streams/ReadableStreamOperations.cpp
@@ -29,7 +29,7 @@
 #include "vm/ObjectOperations.h"  
 #include "vm/PromiseObject.h"  
 
-#include "builtin/streams/HandlerFunction-inl.h"  
+#include "builtin/HandlerFunction-inl.h"  
 #include "builtin/streams/MiscellaneousOperations-inl.h"  
 #include "builtin/streams/ReadableStreamReader-inl.h"  
 #include "vm/Compartment-inl.h"  
