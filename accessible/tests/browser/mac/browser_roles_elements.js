5d84182d3cb7f6a852d396d3aa13c69d6f5a1fe9
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-12-14 12:03:23 +0000

    Bug 1625178 - Support aria-roledescription in mac. r=morgan
    Differential Revision: https://phabricator.services.mozilla.com/D98669
    
    UltraBlame original commit: 965482f9fa3d659fc241ba4ba3484a8721d96c17

--- accessible/tests/browser/mac/browser_roles_elements.js
+++ accessible/tests/browser/mac/browser_roles_elements.js
@@ -79,6 +79,9 @@ addAccessibleTask(
   <!-- A button morphed into a toggle via ARIA -->
   <button id="toggle" aria-pressed="false"></button>
 
+  <!-- A button with a 'banana' role description -->
+  <button id="banana" aria-roledescription="banana"></button>
+
   <!-- Other elements -->
   <del id="deletion">Deleted text</del>
   <dl id="dl"><dt id="dt">term</dt><dd id="dd">definition</dd></dl>
@@ -177,6 +180,9 @@ addAccessibleTask(
 
     
     testRoleAndSubRole(accDoc, "toggle", "AXCheckBox", "AXToggle");
+
+    
+    testRoleAndSubRole(accDoc, "banana", "AXButton", null, "banana");
 
     
     testRoleAndSubRole(accDoc, "deletion", "AXGroup", "AXDeleteStyleGroup");
