b82db6596197f87bbb290c75f6fca2028f5a6a39
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-12-14 12:22:37 +0000

    Bug 1533760- lint fix. a=fix. CLOSED TREE
    UltraBlame original commit: 84a41b4b69e6cb20d27ac50066a224d6db93ca4a

--- python/mozbuild/mozbuild/action/generate_symbols_file.py
+++ python/mozbuild/mozbuild/action/generate_symbols_file.py
@@ -70,7 +70,10 @@ def generate_symbols_file(output, *args):
         
         assert ext == ".def"
         output.write("LIBRARY %s\nEXPORTS\n  %s\n" % (libname, "\n  ".join(symbols)))
-    elif buildconfig.substs.get("GCC_USE_GNU_LD") or buildconfig.substs["OS_TARGET"] == "SunOS":
+    elif (
+        buildconfig.substs.get("GCC_USE_GNU_LD")
+        or buildconfig.substs["OS_TARGET"] == "SunOS"
+    ):
         
         
         
