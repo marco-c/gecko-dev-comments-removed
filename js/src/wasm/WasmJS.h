65f54bb22b2f0e8a330fd4d12d2125b8cb0e88aa
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-12-14 12:38:29 +0000

    Bug 1675602 - Move other ToWebAssemblyValue/ToJSValue implementations to WasmTypes.h. r=lth
    There are two independent implementations of ToWebAssemblyValue/
    ToJSValue. One in WasmJS.cpp and another in WasmInstance.cpp. The
    first was moved to WasmTypes.cpp in the previous commit. This commit
    moves the second over to WasmTypes.cpp as well. A later commit
    will unify the two implementations.
    
    Differential Revision: https://phabricator.services.mozilla.com/D96219
    
    UltraBlame original commit: 0926306a724932cde9afe237396ef2b343c9cede

--- js/src/wasm/WasmJS.h
+++ js/src/wasm/WasmJS.h
@@ -196,10 +196,6 @@ MOZ_MUST_USE bool DeserializeModule(JSContext* cx, const Bytes& serialized,
 
 
 bool IsWasmExportedFunction(JSFunction* fun);
-MOZ_MUST_USE bool CheckFuncRefValue(JSContext* cx, HandleValue v,
-                                    MutableHandleFunction fun);
-MOZ_MUST_USE bool CheckEqRefValue(JSContext* cx, HandleValue v,
-                                  MutableHandleAnyRef vp);
 
 Instance& ExportedFunctionToInstance(JSFunction* fun);
 WasmInstanceObject* ExportedFunctionToInstanceObject(JSFunction* fun);
@@ -207,17 +203,6 @@ uint32_t ExportedFunctionToFuncIndex(JSFunction* fun);
 
 bool IsSharedWasmMemoryObject(JSObject* obj);
 
-
-
-
-
-
-
-
-
-MOZ_MUST_USE bool CheckRefType(JSContext* cx, RefType targetType, HandleValue v,
-                               MutableHandleFunction fnval,
-                               MutableHandleAnyRef refval);
 
 
 
