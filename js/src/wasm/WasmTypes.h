6278e38edd00c8d012acd55fe8c73270300b9a1f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-12-14 12:38:07 +0000

    Bug 1675602 - Replace ModuleEnvironment::funcTypes with FuncDesc. r=lth
    This is a non-functional change. Adds a FuncDesc similar to TableDesc and GlobalDesc
    instead of the the array-of-structs funcTypes and funcTypeIndices. This will be
    expanded in a later commit that removes FuncTypeIdDesc from TypeDef.
    
    Differential Revision: https://phabricator.services.mozilla.com/D96212
    
    UltraBlame original commit: ad250e1589b76a0d4eff9d3d030a0df544d61e0e

--- js/src/wasm/WasmTypes.h
+++ js/src/wasm/WasmTypes.h
@@ -1858,6 +1858,21 @@ typedef Vector<Export, 0, SystemAllocPolicy> ExportVector;
 
 
 
+struct FuncTypeWithId;
+
+struct FuncDesc {
+  FuncTypeWithId* type;
+  uint32_t typeIndex;
+
+  FuncDesc() = default;
+  FuncDesc(FuncTypeWithId* type, uint32_t typeIndex)
+      : type(type), typeIndex(typeIndex) {}
+};
+
+typedef Vector<FuncDesc, 0, SystemAllocPolicy> FuncDescVector;
+
+
+
 
 
 
@@ -2255,15 +2270,6 @@ class TypeDef {
     return funcType_;
   }
 
-  
-  
-  static const TypeDef* fromFuncTypeWithIdPtr(const FuncTypeWithId* p) {
-    const TypeDef* q =
-        (const TypeDef*)((char*)p - offsetof(TypeDef, funcType_));
-    MOZ_ASSERT(q->tag_ == IsFuncType);
-    return q;
-  }
-
   const StructType& structType() const {
     MOZ_ASSERT(isStructType());
     return structType_;
@@ -2272,15 +2278,6 @@ class TypeDef {
   StructType& structType() {
     MOZ_ASSERT(isStructType());
     return structType_;
-  }
-
-  
-  
-  static const TypeDef* fromStructPtr(const StructType* p) {
-    const TypeDef* q =
-        (const TypeDef*)((char*)p - offsetof(TypeDef, structType_));
-    MOZ_ASSERT(q->tag_ == IsStructType);
-    return q;
   }
 };
 
