733189f38d254fd982b70573a9c78afab4af4285
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-12-14 12:00:17 +0000

    Bug 1675894 - Fire selectionchange from disconnected text controls r=masayuki
    Differential Revision: https://phabricator.services.mozilla.com/D98810
    
    UltraBlame original commit: 18c139fe88593d6ce061598d0abffc779cc2a1fd

--- dom/html/TextControlState.cpp
+++ dom/html/TextControlState.cpp
@@ -2098,10 +2098,19 @@ void TextControlState::SetSelectionRange(
 
   if (changed) {
     
-    RefPtr<AsyncEventDispatcher> asyncDispatcher =
-        new AsyncEventDispatcher(mTextCtrlElement, u"select"_ns,
-                                 CanBubble::eYes, ChromeOnlyDispatch::eNo);
+    RefPtr<AsyncEventDispatcher> asyncDispatcher = new AsyncEventDispatcher(
+        mTextCtrlElement, eFormSelect, CanBubble::eYes);
     asyncDispatcher->PostDOMEvent();
+
+    
+    
+    
+    if (IsSelectionCached() &&
+        StaticPrefs::dom_select_events_textcontrols_enabled()) {
+      asyncDispatcher = new AsyncEventDispatcher(
+          mTextCtrlElement, eSelectionChange, CanBubble::eNo);
+      asyncDispatcher->PostDOMEvent();
+    }
   }
 
   if (NS_FAILED(rv)) {
