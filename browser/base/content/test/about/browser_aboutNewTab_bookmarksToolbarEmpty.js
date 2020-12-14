4a3eaa161b353591582459973afc29acf39a88dc
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-12-14 12:16:23 +0000

    Bug 1680762 - ensure both link clicks and drag/drop into the bookmarks toolbar works, r=jaws,mak
    Differential Revision: https://phabricator.services.mozilla.com/D98995
    
    UltraBlame original commit: 82046518530a82b0f553fc8c12fbef23ce5b0171

--- browser/base/content/test/about/browser_aboutNewTab_bookmarksToolbarEmpty.js
+++ browser/base/content/test/about/browser_aboutNewTab_bookmarksToolbarEmpty.js
@@ -108,6 +108,19 @@ add_task(async function bookmarks_toolbar_not_shown_when_empty() {
           "Toolbar is visible when there are no items in the toolbar area",
       });
       ok(!emptyMessage.hidden, "Empty message is shown with toolbar empty");
+      
+      let winPromise = BrowserTestUtils.domWindowOpenedAndLoaded();
+      EventUtils.synthesizeMouseAtCenter(
+        emptyMessage.querySelector(".text-link"),
+        {}
+      );
+      let libraryWin = await winPromise;
+      is(
+        libraryWin.document.location.href,
+        "chrome://browser/content/places/places.xhtml",
+        "Should have opened library."
+      );
+      await BrowserTestUtils.closeWindow(libraryWin);
     }
 
     
