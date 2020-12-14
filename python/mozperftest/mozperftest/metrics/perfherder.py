3f13b84c785fefd7627a57706076c454d1008092
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-12-14 11:56:11 +0000

    Bug 1672142 - Add perf test for the controlled website r=sparky,dragana
    Differential Revision: https://phabricator.services.mozilla.com/D94089
    
    UltraBlame original commit: 3f15a75461ad3287c0423f5b7e77d1b5617ae239

--- python/mozperftest/mozperftest/metrics/perfherder.py
+++ python/mozperftest/mozperftest/metrics/perfherder.py
@@ -6,6 +6,7 @@ import jsonschema
 import os
 import pathlib
 import statistics
+import sys
 
 from mozperftest.utils import strtobool
 from mozperftest.layers import Layer
@@ -170,7 +171,14 @@ class Perfherder(Layer):
 
         
         
-        print("PERFHERDER_DATA: " + json.dumps(all_perfherder_data))
+
+        
+        
+        sys.stdout.write("PERFHERDER_DATA: ")
+        json.dump(all_perfherder_data, sys.stdout)
+        sys.stdout.write("\n")
+        sys.stdout.flush()
+
         metadata.set_output(write_json(all_perfherder_data, output, file))
         return metadata
 
