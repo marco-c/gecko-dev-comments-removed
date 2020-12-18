9c1dbd714e82eeb6256107ec3f1cf2626510667c
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-12-18 10:48:11 +0000

    Bug 1583109 - Use StringJoin(Append) where easily possible. r=nika
    Bug 1583109 introduced new function templates StringJoin and StringJoinAppend.
    These are now used to replace several custom loops across the codebase that
    implement string-joining algorithms to simplify the code.
    
    Differential Revision: https://phabricator.services.mozilla.com/D98750
    
    UltraBlame original commit: 2556404e58f7d98b7280132c1c921abce4e241c6

--- xpcom/io/nsLocalFileCommon.cpp
+++ xpcom/io/nsLocalFileCommon.cpp
@@ -347,13 +347,14 @@ nsLocalFile::GetRelativeDescriptor(nsIFile* aFromFile, nsACString& aResult) {
   for (nodeIndex = branchIndex; nodeIndex < fromNodes.Length(); ++nodeIndex) {
     aResult.AppendLiteral("../");
   }
-  for (nodeIndex = branchIndex; nodeIndex < thisNodes.Length(); ++nodeIndex) {
-    NS_ConvertUTF16toUTF8 nodeStr(thisNodes[nodeIndex]);
-    aResult.Append(nodeStr);
-    if (nodeIndex + 1 < thisNodes.Length()) {
-      aResult.Append('/');
-    }
-  }
+  StringJoinAppend(aResult, "/"_ns, mozilla::Span{thisNodes}.From(branchIndex),
+                   [](nsACString& dest, const auto& thisNode) {
+                     
+                     
+                     
+                     
+                     AppendUTF16toUTF8(nsDependentString{thisNode}, dest);
+                   });
 
   return NS_OK;
 }
