3d4713283112a5fac4c4333f0ea58018f7e2ddc9
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-12-18 10:34:44 +0000

    Bug 1678684 - Initialize DXVA on the media thread, remove the mostly-unused dll blocklist, and the crashguard. r=jya
    The main-thread requirements for DXVA appear to have been needed when we initialized a crash guard. We now only run DXVA in the GPU and RDD processes, which don't support crash guards. This removes the main thread dispatch and the crashguard code, and enforces that we're in the GPU/RDD process to init DXVA.
    
    This also removes the DLL blocklist code. This was disabled via pref when in the GPU process, which should be the majority of the time. In rare cases we would have been running DXVA in the RDD process (on older win7 when the GPU process isn't available). In these cases we can just do the same as the GPU process, allowing crashes and recovering from them (and disabling DXVA).
    
    Differential Revision: https://phabricator.services.mozilla.com/D98036
    
    UltraBlame original commit: f02502526be459a6fb864990d87afe416f97c9c4

--- gfx/src/DriverCrashGuard.h
+++ gfx/src/DriverCrashGuard.h
@@ -37,9 +37,7 @@ enum class DriverInitStatus {
 
 enum class CrashGuardType : uint32_t {
   D3D11Layers,
-  D3D9Video,
   GLContext,
-  D3D11Video,
   WMFVPXVideo,
   
   
@@ -146,24 +144,6 @@ class D3D11LayersCrashGuard final : public DriverCrashGuard {
 
  private:
   void RecordTelemetry(TelemetryState aState);
-};
-
-class D3D9VideoCrashGuard final : public DriverCrashGuard {
- public:
-  explicit D3D9VideoCrashGuard(dom::ContentParent* aContentParent = nullptr);
-
- protected:
-  void LogCrashRecovery() override;
-  void LogFeatureDisabled() override;
-};
-
-class D3D11VideoCrashGuard final : public DriverCrashGuard {
- public:
-  explicit D3D11VideoCrashGuard(dom::ContentParent* aContentParent = nullptr);
-
- protected:
-  void LogCrashRecovery() override;
-  void LogFeatureDisabled() override;
 };
 
 class GLContextCrashGuard final : public DriverCrashGuard {
