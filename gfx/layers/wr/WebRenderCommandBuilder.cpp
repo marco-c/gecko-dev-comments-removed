6e40703b39efe0538ec225b34347b107a0708709
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-12-18 10:42:34 +0000

    Bug 1682398. Be more conservative about promoting mixed-blend-mode to active. r=mstange
    Previously, if a mixed-blend-mode item was the child of the root svg
    element it would always end up active. Instead of using parent
    activeness as a proxy, track previous sibling activeness. This
    doesn't make things too much more complicated and is more accurate.
    
    Unfortunately, if a previous silbing is actually active we can get
    still get into a bad situation.
    
    Differential Revision: https://phabricator.services.mozilla.com/D99853
    
    UltraBlame original commit: f88facb3f69019d08ec20da2a2f66ac9c8525630

--- gfx/layers/wr/WebRenderCommandBuilder.cpp
+++ gfx/layers/wr/WebRenderCommandBuilder.cpp
@@ -1058,7 +1058,7 @@ static bool IsItemProbablyActive(
     mozilla::wr::IpcResourceUpdateQueue& aResources,
     const mozilla::layers::StackingContextHelper& aSc,
     mozilla::layers::RenderRootStateManager* aManager,
-    nsDisplayListBuilder* aDisplayListBuilder, bool aParentActive = true);
+    nsDisplayListBuilder* aDisplayListBuilder, bool aSiblingActive);
 
 static bool HasActiveChildren(const nsDisplayList& aList,
                               mozilla::wr::DisplayListBuilder& aBuilder,
@@ -1087,7 +1087,8 @@ static bool IsItemProbablyActive(
     mozilla::wr::IpcResourceUpdateQueue& aResources,
     const mozilla::layers::StackingContextHelper& aSc,
     mozilla::layers::RenderRootStateManager* aManager,
-    nsDisplayListBuilder* aDisplayListBuilder, bool aParentActive) {
+    nsDisplayListBuilder* aDisplayListBuilder,
+    bool aHasActivePrecedingSibling) {
   switch (aItem->GetType()) {
     case DisplayItemType::TYPE_TRANSFORM: {
       nsDisplayTransform* transformItem =
@@ -1121,8 +1122,7 @@ static bool IsItemProbablyActive(
     case DisplayItemType::TYPE_BLEND_MODE: {
       
 
-
-      return aParentActive ||
+      return aHasActivePrecedingSibling ||
              HasActiveChildren(*aItem->GetChildren(), aBuilder, aResources, aSc,
                                aManager, aDisplayListBuilder);
     }
@@ -1160,9 +1160,12 @@ void Grouper::ConstructGroups(nsDisplayListBuilder* aDisplayListBuilder,
   nsDisplayItem* startOfCurrentGroup = item;
   RenderRootStateManager* manager =
       aCommandBuilder->mManager->GetRenderRootStateManager();
+  
+  bool encounteredActiveItem = false;
   while (item) {
     if (IsItemProbablyActive(item, aBuilder, aResources, aSc, manager,
-                             mDisplayListBuilder)) {
+                             mDisplayListBuilder, encounteredActiveItem)) {
+      encounteredActiveItem = true;
       
       RefPtr<WebRenderGroupData> groupData =
           aCommandBuilder->CreateOrRecycleWebRenderUserData<WebRenderGroupData>(
