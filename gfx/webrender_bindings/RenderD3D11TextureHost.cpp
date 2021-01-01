5d3cdbcb1be3513497a87c290e512234bbd76c3a
Marco Castelluccio <mcastelluccio@mozilla.com>
2021-01-01 00:56:26 +0000

    Bug 1683247 - Stop to create shared GL and WebRenderShaders in RenderDXGITextureHost::GetD3D11Texture2D() r=nical
    sw-wr does not need shared GL and WebRenderShaders.
    
    Differential Revision: https://phabricator.services.mozilla.com/D100081
    
    UltraBlame original commit: e58a9a0be7025921364c42cb7b1a6a1c7da76f3a

--- gfx/webrender_bindings/RenderD3D11TextureHost.cpp
+++ gfx/webrender_bindings/RenderD3D11TextureHost.cpp
@@ -44,17 +44,20 @@ RenderDXGITextureHost::~RenderDXGITextureHost() {
   DeleteTextureHandle();
 }
 
-ID3D11Texture2D* RenderDXGITextureHost::GetD3D11Texture2D() {
+ID3D11Texture2D* RenderDXGITextureHost::GetD3D11Texture2DWithGL() {
+  if (mTexture) {
+    return mTexture;
+  }
+
   if (!mGL) {
     
     mGL = RenderThread::Get()->SharedGL();
   }
 
-  if (!mTexture) {
-    if (!EnsureD3D11Texture2D()) {
-      return nullptr;
-    }
+  if (!EnsureD3D11Texture2DWithGL()) {
+    return nullptr;
   }
+
   return mTexture;
 }
 
@@ -145,7 +148,7 @@ void RenderDXGITextureHost::UnmapPlanes() {
   mDeviceContext = nullptr;
 }
 
-bool RenderDXGITextureHost::EnsureD3D11Texture2D() {
+bool RenderDXGITextureHost::EnsureD3D11Texture2DWithGL() {
   if (mTexture) {
     return true;
   }
@@ -229,7 +232,7 @@ bool RenderDXGITextureHost::EnsureLockable(wr::ImageRendering aRendering) {
   }
 
   
-  if (!EnsureD3D11Texture2D()) {
+  if (!EnsureD3D11Texture2DWithGL()) {
     return false;
   }
 
