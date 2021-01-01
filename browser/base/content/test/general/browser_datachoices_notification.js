ce108303fffaed96bd828becc827d539cf49d4f0
Marco Castelluccio <mcastelluccio@mozilla.com>
2021-01-01 00:55:49 +0000

    Backed out changeset f4380ffa8aad (bug 1571932) for perma failures on browser_datachoices_notification.js
    UltraBlame original commit: 7f57f4171ac481cd3bd75cbd728d0c8471d6ba15

--- browser/base/content/test/general/browser_datachoices_notification.js
+++ browser/base/content/test/general/browser_datachoices_notification.js
@@ -132,6 +132,8 @@ add_task(async function setup() {
   });
 
   
+  Preferences.set(PREF_TELEMETRY_LOG_LEVEL, "Trace");
+  
   Preferences.set(PREF_BYPASS_NOTIFICATION, false);
   
   Preferences.set(PREF_CURRENT_POLICY_VERSION, TEST_POLICY_VERSION);
@@ -224,11 +226,13 @@ add_task(async function test_single_window() {
   );
 });
 
-add_task({ skip_if: () => true }, async function test_multiple_windows() {
+add_task(async function test_multiple_windows() {
   clearAcceptedPolicy();
+  assertCoherentInitialState(); 
 
   
   await closeAllNotifications();
+  assertCoherentInitialState(); 
 
   
   
