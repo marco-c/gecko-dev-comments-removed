fef81c382dee6afa2eac6cabbb1d4fc281487764
Marco Castelluccio <mcastelluccio@mozilla.com>
2021-01-01 00:54:51 +0000

    Bug 1679751 - Use the shelf allocator for alpha8_linear and color8_nearest textures. r=gw
    Differential Revision: https://phabricator.services.mozilla.com/D99235
    
    UltraBlame original commit: 007cb36f75ef560584a0cbff0183cf3d3bdae179

--- gfx/wr/webrender/src/texture_cache.rs
+++ gfx/wr/webrender/src/texture_cache.rs
@@ -235,8 +235,8 @@ impl EvictionNotice {
 #[cfg_attr(feature = "capture", derive(Serialize))]
 #[cfg_attr(feature = "replay", derive(Deserialize))]
 struct SharedTextures {
-    color8_nearest: AllocatorList<SlabAllocator, TextureParameters>,
-    alpha8_linear: AllocatorList<SlabAllocator, TextureParameters>,
+    color8_nearest: AllocatorList<ShelfAllocator, TextureParameters>,
+    alpha8_linear: AllocatorList<ShelfAllocator, TextureParameters>,
     alpha16_linear: AllocatorList<SlabAllocator, TextureParameters>,
     color8_linear: AllocatorList<ShelfAllocator, TextureParameters>,
     color8_glyphs: AllocatorList<BucketedShelfAllocator, TextureParameters>,
@@ -249,10 +249,17 @@ impl SharedTextures {
             
             
             
+            
+            
+            
+            
+            
             alpha8_linear: AllocatorList::new(
                 1024,
-                SlabAllocatorParameters {
-                    region_size: TEXTURE_REGION_DIMENSIONS,
+                ShelfAllocatorOptions {
+                    num_columns: 1,
+                    alignment: size2(8, 8),
+                    .. ShelfAllocatorOptions::default()
                 },
                 TextureParameters {
                     formats: TextureFormatPair::from(ImageFormat::R8),
@@ -301,10 +308,8 @@ impl SharedTextures {
             
             
             color8_nearest: AllocatorList::new(
-                TEXTURE_REGION_DIMENSIONS,
-                SlabAllocatorParameters {
-                    region_size: TEXTURE_REGION_DIMENSIONS,
-                },
+                512,
+                ShelfAllocatorOptions::default(),
                 TextureParameters {
                     formats: color_formats,
                     filter: TextureFilter::Nearest,
