d7e3b3ebe13e1faec7ae08ad9cc93810c45bcd1d
Marco Castelluccio <mcastelluccio@mozilla.com>
2021-01-01 01:00:04 +0000

    Bug 1679750 - Part 19: Add MacroAssembler::notPtr(Register). r=jandem
    The next part will call this method.
    
    Differential Revision: https://phabricator.services.mozilla.com/D98173
    
    UltraBlame original commit: c6840af4b13ee079846eefe98a8b1923ae4cfc79

--- js/src/jit/x64/Assembler-x64.h
+++ js/src/jit/x64/Assembler-x64.h
@@ -966,6 +966,8 @@ class Assembler : public AssemblerX86Shared {
 
   void negq(Register reg) { masm.negq_r(reg.encoding()); }
 
+  void notq(Register reg) { masm.notq_r(reg.encoding()); }
+
   void mov(ImmWord word, Register dest) {
     
     
