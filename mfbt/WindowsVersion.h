275bff15d2e9016b35d5a43ecb46805d454617eb
Marco Castelluccio <mcastelluccio@mozilla.com>
2021-01-06 02:14:22 +0000

    Bug 1684110 - Stop using some 'A'-functions in mfbt/. r=dmajor
    Depends on D100464
    
    Differential Revision: https://phabricator.services.mozilla.com/D100465
    
    UltraBlame original commit: 01f514ef6d20597820c4506b8058706c77ccc9da

--- mfbt/WindowsVersion.h
+++ mfbt/WindowsVersion.h
@@ -1,8 +1,8 @@
-
-
-
-
-
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=8 sts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #ifndef mozilla_WindowsVersion_h
 #define mozilla_WindowsVersion_h
@@ -26,9 +26,9 @@ inline bool IsWindowsVersionOrLater(uint32_t aVersion) {
     return false;
   }
 
-  OSVERSIONINFOEX info;
-  ZeroMemory(&info, sizeof(OSVERSIONINFOEX));
-  info.dwOSVersionInfoSize = sizeof(OSVERSIONINFOEX);
+  OSVERSIONINFOEXW info;
+  ZeroMemory(&info, sizeof(OSVERSIONINFOEXW));
+  info.dwOSVersionInfoSize = sizeof(OSVERSIONINFOEXW);
   info.dwMajorVersion = aVersion >> 24;
   info.dwMinorVersion = (aVersion >> 16) & 0xFF;
   info.wServicePackMajor = (aVersion >> 8) & 0xFF;
@@ -40,10 +40,10 @@ inline bool IsWindowsVersionOrLater(uint32_t aVersion) {
   VER_SET_CONDITION(conditionMask, VER_SERVICEPACKMAJOR, VER_GREATER_EQUAL);
   VER_SET_CONDITION(conditionMask, VER_SERVICEPACKMINOR, VER_GREATER_EQUAL);
 
-  if (VerifyVersionInfo(&info,
-                        VER_MAJORVERSION | VER_MINORVERSION |
-                            VER_SERVICEPACKMAJOR | VER_SERVICEPACKMINOR,
-                        conditionMask)) {
+  if (VerifyVersionInfoW(&info,
+                         VER_MAJORVERSION | VER_MINORVERSION |
+                             VER_SERVICEPACKMAJOR | VER_SERVICEPACKMINOR,
+                         conditionMask)) {
     minVersion = aVersion;
     return true;
   }
@@ -64,15 +64,15 @@ inline bool IsWindowsBuildOrLater(uint32_t aBuild) {
     return false;
   }
 
-  OSVERSIONINFOEX info;
-  ZeroMemory(&info, sizeof(OSVERSIONINFOEX));
-  info.dwOSVersionInfoSize = sizeof(OSVERSIONINFOEX);
+  OSVERSIONINFOEXW info;
+  ZeroMemory(&info, sizeof(OSVERSIONINFOEXW));
+  info.dwOSVersionInfoSize = sizeof(OSVERSIONINFOEXW);
   info.dwBuildNumber = aBuild;
 
   DWORDLONG conditionMask = 0;
   VER_SET_CONDITION(conditionMask, VER_BUILDNUMBER, VER_GREATER_EQUAL);
 
-  if (VerifyVersionInfo(&info, VER_BUILDNUMBER, conditionMask)) {
+  if (VerifyVersionInfoW(&info, VER_BUILDNUMBER, conditionMask)) {
     minBuild = aBuild;
     return true;
   }
@@ -93,9 +93,9 @@ inline bool IsWindows10BuildOrLater(uint32_t aBuild) {
     return false;
   }
 
-  OSVERSIONINFOEX info;
-  ZeroMemory(&info, sizeof(OSVERSIONINFOEX));
-  info.dwOSVersionInfoSize = sizeof(OSVERSIONINFOEX);
+  OSVERSIONINFOEXW info;
+  ZeroMemory(&info, sizeof(OSVERSIONINFOEXW));
+  info.dwOSVersionInfoSize = sizeof(OSVERSIONINFOEXW);
   info.dwMajorVersion = 10;
   info.dwBuildNumber = aBuild;
 
@@ -106,10 +106,10 @@ inline bool IsWindows10BuildOrLater(uint32_t aBuild) {
   VER_SET_CONDITION(conditionMask, VER_SERVICEPACKMAJOR, VER_GREATER_EQUAL);
   VER_SET_CONDITION(conditionMask, VER_SERVICEPACKMINOR, VER_GREATER_EQUAL);
 
-  if (VerifyVersionInfo(&info,
-                        VER_MAJORVERSION | VER_MINORVERSION | VER_BUILDNUMBER |
-                            VER_SERVICEPACKMAJOR | VER_SERVICEPACKMINOR,
-                        conditionMask)) {
+  if (VerifyVersionInfoW(&info,
+                         VER_MAJORVERSION | VER_MINORVERSION | VER_BUILDNUMBER |
+                             VER_SERVICEPACKMAJOR | VER_SERVICEPACKMINOR,
+                         conditionMask)) {
     minBuild = aBuild;
     return true;
   }
@@ -167,32 +167,32 @@ MOZ_ALWAYS_INLINE bool IsNotWin7PreRTM() {
 }
 
 inline bool IsWin7AndPre2000Compatible() {
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
+  /*
+   * See Bug 1279171.
+   * We'd like to avoid using WMF on specific OS version when compatibility
+   * mode is in effect. The purpose of this function is to check if FF runs on
+   * Win7 OS with application compatibility mode being set to 95/98/ME.
+   * Those compatibility mode options (95/98/ME) can only display and
+   * be selected for 32-bit application.
+   * If the compatibility mode is in effect, the GetVersionEx function will
+   * report the OS as it identifies itself, which may not be the OS that is
+   * installed.
+   * Note : 1) We only target for Win7 build number greater than 7600.
+   *        2) GetVersionEx may be altered or unavailable for release after
+   *           Win8.1. Set pragma to avoid build warning as error.
+   */
   bool isWin7 = IsNotWin7PreRTM() && !IsWin8OrLater();
   if (!isWin7) {
     return false;
   }
 
-  OSVERSIONINFOEX info;
-  ZeroMemory(&info, sizeof(OSVERSIONINFOEX));
+  OSVERSIONINFOEXW info;
+  ZeroMemory(&info, sizeof(OSVERSIONINFOEXW));
 
-  info.dwOSVersionInfoSize = sizeof(OSVERSIONINFOEX);
+  info.dwOSVersionInfoSize = sizeof(OSVERSIONINFOEXW);
 #pragma warning(push)
 #pragma warning(disable : 4996)
-  bool success = GetVersionEx((LPOSVERSIONINFO)&info);
+  bool success = GetVersionExW((LPOSVERSIONINFOW)&info);
 #pragma warning(pop)
   if (!success) {
     return false;
@@ -200,6 +200,6 @@ inline bool IsWin7AndPre2000Compatible() {
   return info.dwMajorVersion < 5;
 }
 
-}  
+}  // namespace mozilla
 
-#endif 
+#endif /* mozilla_WindowsVersion_h */
