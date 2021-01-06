d4e16cf095a65f8b1dde7756ba470a79b9e203fb
Marco Castelluccio <mcastelluccio@mozilla.com>
2021-01-06 02:15:29 +0000

    Bug 1681756 - Fix return of RenderCompositorANGLE::GetBufferSize() r=nical
    I missed to read a review comment of D100220.
    
    Differential Revision: https://phabricator.services.mozilla.com/D100456
    
    UltraBlame original commit: 6f144aa78811ad8697613056b01ad9d88829168c

--- gfx/webrender_bindings/RenderCompositorANGLE.cpp
+++ gfx/webrender_bindings/RenderCompositorANGLE.cpp
@@ -734,7 +734,7 @@ LayoutDeviceIntSize RenderCompositorANGLE::GetBufferSize() {
     
     size.width = std::max(size.width, 1);
     size.height = std::max(size.height, 1);
-    return mWidget->GetClientSize();
+    return size;
   }
 }
 
