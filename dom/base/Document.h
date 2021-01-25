2cc614576eb605fe37a61c1ef439dcdf7931977c
Marco Castelluccio <mcastelluccio@mozilla.com>
2021-01-25 19:49:02 +0000

    Bug 1551040 - Add const versions of Document casts. r=edgar
    Depends on D101516
    
    Differential Revision: https://phabricator.services.mozilla.com/D101517
    
    UltraBlame original commit: 32394df21f464e069a9ace35166c650691937ec4

--- dom/base/Document.h
+++ dom/base/Document.h
@@ -3556,12 +3556,14 @@ class Document : public nsINode,
 
 
   inline nsHTMLDocument* AsHTMLDocument();
+  inline const nsHTMLDocument* AsHTMLDocument() const;
 
   
 
 
 
   inline SVGDocument* AsSVGDocument();
+  inline const SVGDocument* AsSVGDocument() const;
 
   
 
