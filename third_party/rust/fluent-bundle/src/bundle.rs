d6e3f9d402cab43aeeaf7acc67d988d15a3125d6
Marco Castelluccio <mcastelluccio@mozilla.com>
2021-01-25 19:20:08 +0000

    Bug 1686275 - Update fluent-rs to 0.14. r=dminor
    Differential Revision: https://phabricator.services.mozilla.com/D101474
    
    UltraBlame original commit: 100fc2edfab238e89935d6bace79aa1c2a6e3d8d

--- third_party/rust/fluent-bundle/src/bundle.rs
+++ third_party/rust/fluent-bundle/src/bundle.rs
@@ -16,12 +16,12 @@ use unic_langid::LanguageIdentifier;
 use crate::args::FluentArgs;
 use crate::entry::Entry;
 use crate::entry::GetEntry;
-use crate::errors::FluentError;
+use crate::errors::{EntryKind, FluentError};
 use crate::memoizer::MemoizerKind;
+use crate::message::{FluentAttribute, FluentMessage};
 use crate::resolver::{ResolveValue, Scope, WriteValue};
 use crate::resource::FluentResource;
 use crate::types::FluentValue;
-use crate::message::{FluentMessage, FluentAttribute};
 
 
 
@@ -57,19 +57,15 @@ impl<R, M: MemoizerKind> FluentBundleBase<R, M> {
     
     
     
-    pub fn new<'a, L: 'a + Into<LanguageIdentifier> + PartialEq + Clone>(
-        locales: impl IntoIterator<Item = &'a L>,
-    ) -> Self {
-        let locales = locales
-            .into_iter()
-            .map(|s| s.clone().into())
-            .collect::<Vec<_>>();
-        let lang = locales.get(0).cloned().unwrap_or_default();
+    
+    
+    pub fn new(locales: Vec<LanguageIdentifier>) -> Self {
+        let first_locale = locales.get(0).cloned().unwrap_or_default();
         Self {
             locales,
             resources: vec![],
             entries: HashMap::new(),
-            intls: M::new(lang),
+            intls: M::new(first_locale),
             use_isolating: true,
             transform: None,
             formatter: None,
@@ -138,8 +134,10 @@ impl<R, M: MemoizerKind> FluentBundleBase<R, M> {
             };
 
             let (entry, kind) = match entry {
-                ast::Entry::Message(..) => (Entry::Message([res_pos, entry_pos]), "message"),
-                ast::Entry::Term(..) => (Entry::Term([res_pos, entry_pos]), "term"),
+                ast::Entry::Message(..) => {
+                    (Entry::Message([res_pos, entry_pos]), EntryKind::Message)
+                }
+                ast::Entry::Term(..) => (Entry::Term([res_pos, entry_pos]), EntryKind::Term),
                 _ => continue,
             };
 
@@ -348,8 +346,8 @@ impl<R, M: MemoizerKind> FluentBundleBase<R, M> {
 
         for attr in &message.attributes {
             attributes.push(FluentAttribute {
-                id: &attr.id.name,
-                value: &attr.value
+                id: attr.id.name,
+                value: &attr.value,
             });
         }
         Some(FluentMessage { value, attributes })
@@ -489,7 +487,7 @@ impl<R, M: MemoizerKind> FluentBundleBase<R, M> {
                 Ok(())
             }
             HashEntry::Occupied(_) => Err(FluentError::Overriding {
-                kind: "function",
+                kind: EntryKind::Function,
                 id: id.to_owned(),
             }),
         }
@@ -499,7 +497,7 @@ impl<R, M: MemoizerKind> FluentBundleBase<R, M> {
 impl<R, M: MemoizerKind> Default for FluentBundleBase<R, M> {
     fn default() -> Self {
         let langid = LanguageIdentifier::default();
-        FluentBundleBase {
+        Self {
             locales: vec![langid.clone()],
             resources: vec![],
             entries: Default::default(),
