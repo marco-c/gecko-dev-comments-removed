da98e0ab4a642731243ce578508fd85f75f40036
Marco Castelluccio <mcastelluccio@mozilla.com>
2021-01-25 18:54:21 +0000

    Bug 1681043 - P5. Recalculate decoder capabilities if CanUseHardwareVideoDecoding changes. r=alwu,bryce
    Not the most elegant, but reworking the PDMFactory to be fully re-initialized would be a significant change, as only the WMFDecoderModule requires it we take some shortcuts.
    
    Differential Revision: https://phabricator.services.mozilla.com/D100308
    
    UltraBlame original commit: 487a070f49899c3cf563f15fb772dcb423b7a87f

--- dom/media/platforms/PDMFactory.cpp
+++ dom/media/platforms/PDMFactory.cpp
@@ -618,8 +618,10 @@ void PDMFactory::SetCDMProxy(CDMProxy* aProxy) {
 }
 
 
-PDMFactory::MediaCodecsSupported PDMFactory::Supported() {
-  static MediaCodecsSupported supported = []() {
+PDMFactory::MediaCodecsSupported PDMFactory::Supported(bool aForceRefresh) {
+  MOZ_ASSERT(NS_IsMainThread());
+
+  static auto calculate = []() {
     auto pdm = MakeRefPtr<PDMFactory>();
     MediaCodecsSupported supported;
     
@@ -665,7 +667,11 @@ PDMFactory::MediaCodecsSupported PDMFactory::Supported() {
       supported += MediaCodecs::Wave;
     }
     return supported;
-  }();
+  };
+  static MediaCodecsSupported supported = calculate();
+  if (aForceRefresh) {
+    supported = calculate();
+  }
   return supported;
 }
 
