cb29f3fe9b5123dfacdde8ad1a8fa4757ab0f88a
Marco Castelluccio <mcastelluccio@mozilla.com>
2021-01-25 19:00:24 +0000

    Bug 1683404 - supress issue found while enabling full tsan shutdown. r=decoder
    Differential Revision: https://phabricator.services.mozilla.com/D100268
    
    UltraBlame original commit: cc7b2b5c39b3499428e6822738b85003190a26c6

--- mozglue/build/TsanOptions.cpp
+++ mozglue/build/TsanOptions.cpp
@@ -286,6 +286,10 @@ extern "C" const char* __tsan_default_suppressions() {
          "race:DataChannelConnection::SetSignals\n"
          "race:DataChannelConnection::SetReady\n"
 
+         
+         "race:nsTimerImpl::Shutdown\n"
+         "race:nsTimerImpl::CancelImpl\n"
+
       
       ;  
 }
