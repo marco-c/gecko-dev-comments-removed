7da0ce7e4cdc189d5303c5120f5b8e0766d4d1e9
Marco Castelluccio <mcastelluccio@mozilla.com>
2021-01-25 19:21:57 +0000

    Bug 1686507 - Raise diff-reproducible-linux32-generated-files to tier 1 r=glandium
    Unlike the binary diffs, the generated-files diffs have been stable for a long time. If these tasks ever start failing, it would be good to learn about that at tier-1 speed.
    
    Differential Revision: https://phabricator.services.mozilla.com/D101629
    
    UltraBlame original commit: c1607d3080b7540385083cb782ff56bf1449d4af

--- taskcluster/taskgraph/transforms/diffoscope.py
+++ taskcluster/taskgraph/transforms/diffoscope.py
@@ -29,6 +29,8 @@ diff_description_schema = Schema(
         
         Required("name"): text_type,
         
+        Required("tier"): int,
+        
         Required("symbol"): text_type,
         
         Optional("job-from"): text_type,
@@ -125,7 +127,7 @@ def fill_template(config, tasks):
                 "symbol": task["symbol"],
                 "platform": "diff/opt",
                 "kind": "other",
-                "tier": 2,
+                "tier": task["tier"],
             },
             "worker-type": "b-linux",
             "worker": {
