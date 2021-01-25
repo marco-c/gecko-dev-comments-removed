46dcf5e75da2bae25d21d4aa17f5f0d69e0a1885
Marco Castelluccio <mcastelluccio@mozilla.com>
2021-01-25 20:13:11 +0000

    Bug 1594234 remove extensions.content_script_csp preferences in favor of extensions.manifestV3.enabled r=robwu
    Differential Revision: https://phabricator.services.mozilla.com/D101212
    
    UltraBlame original commit: 94ae53f11f08964dcccda088def3465d1b0e3b3f

--- caps/nsScriptSecurityManager.cpp
+++ caps/nsScriptSecurityManager.cpp
@@ -414,9 +414,6 @@ bool nsScriptSecurityManager::ContentSecurityPolicyPermitsJSAction(
 
   nsCOMPtr<nsIPrincipal> subjectPrincipal = nsContentUtils::SubjectPrincipal();
   if (!csp) {
-    if (!StaticPrefs::extensions_content_script_csp_enabled()) {
-      return true;
-    }
     
     
     auto* basePrin = BasePrincipal::Cast(subjectPrincipal);
