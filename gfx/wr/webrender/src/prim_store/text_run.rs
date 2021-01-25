b8dedc0d02c7bc2d3ae3eb1bf14f16e23b427232
Marco Castelluccio <mcastelluccio@mozilla.com>
2021-01-25 20:13:41 +0000

    Bug 1561358 - Move WR storage module under prim_storage r=gw
    the storage logic was made for, and used exclusively in, the prim_store.
    Moving it into the sub-module and making it private allows for easier navigation in the code.
    
    Differential Revision: https://phabricator.services.mozilla.com/D102494
    
    UltraBlame original commit: 94dc187c2b50b0282b2c920ce637efd92e1b2cc7

--- gfx/wr/webrender/src/prim_store/text_run.rs
+++ gfx/wr/webrender/src/prim_store/text_run.rs
@@ -19,10 +19,12 @@ use crate::util::{MatrixHelpers};
 use crate::prim_store::{InternablePrimitive, PrimitiveInstanceKind};
 use crate::spatial_tree::{SpatialTree, SpatialNodeIndex};
 use crate::space::SpaceSnapper;
+use crate::util::PrimaryArc;
+
 use std::ops;
 use std::sync::Arc;
-use crate::storage;
-use crate::util::PrimaryArc;
+
+use super::storage;
 
 
 #[cfg_attr(feature = "capture", derive(Serialize))]
