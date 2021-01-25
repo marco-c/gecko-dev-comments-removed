a4ea88a561486bab9cac5bab37938162e448287b
Marco Castelluccio <mcastelluccio@mozilla.com>
2021-01-25 20:18:22 +0000

    Bug 1687298 [wpt PR 27223] - Update interfaces/custom-state-pseudo-class.idl and test, a=testonly
    Automatic update from web-platform-tests
    Update interfaces/custom-state-pseudo-class.idl and test (#27223)
    
    Closes https://github.com/web-platform-tests/wpt/pull/27222
    --
    
    wpt-commits: 4ed41fb5b16c9496757f13ccb2dadbc14063e396
    wpt-pr: 27223
    
    UltraBlame original commit: 1a388b9c3fbd3032abebfd1a56f0398b18207a5d

--- testing/web-platform/tests/custom-state-pseudo-class/idlharness.window.js
+++ testing/web-platform/tests/custom-state-pseudo-class/idlharness.window.js
@@ -3,12 +3,18 @@
 
 'use strict';
 
+class MyElement extends HTMLElement {};
+customElements.define('my-element', MyElement);
+
 idl_test(
   ['custom-state-pseudo-class'],
   ['html', 'wai-aria'],
   idl_array => {
     idl_array.add_objects({
-      
+      CustomStateSet: [ 'customStateSet' ],
     });
+
+    const myElement = document.createElement('my-element');
+    self.customStateSet = myElement.attachInternals().states;
   }
 );
