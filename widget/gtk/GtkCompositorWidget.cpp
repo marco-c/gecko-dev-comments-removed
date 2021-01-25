80b7421f721a4c98763d8fc3d8215250300c3931
Marco Castelluccio <mcastelluccio@mozilla.com>
2021-01-25 20:31:04 +0000

    Bug 1674473 - use ClearRect instead of FillRect. r=jrmuizel
    Differential Revision: https://phabricator.services.mozilla.com/D102796
    
    UltraBlame original commit: 3ab89af2c93d5453bcafd1e0a30f5823148d1a11

--- widget/gtk/GtkCompositorWidget.cpp
+++ widget/gtk/GtkCompositorWidget.cpp
@@ -133,21 +133,15 @@ void GtkCompositorWidget::ClearBeforePaint(
   
   if (mWidget->IsPopup()) {
     for (auto iter = aRegion.RectIter(); !iter.Done(); iter.Next()) {
-      LayoutDeviceIntRect r = iter.Get();
-      aTarget->FillRect(gfx::Rect(r.x, r.y, r.width, r.height),
-                        ColorPattern(DeviceColor(0, 0, 0, 0)),
-                        DrawOptions(1.0f, CompositionOp::OP_SOURCE));
+      aTarget->ClearRect(gfx::Rect(iter.Get().ToUnknownRect()));
     }
-    aTarget->Flush();
   }
 
   
   
   gfx::Rect rect;
   if (mWidget->GetTitlebarRect(rect)) {
-    aTarget->FillRect(rect, ColorPattern(DeviceColor(0, 0, 0, 0)),
-                      DrawOptions(1.0f, CompositionOp::OP_SOURCE));
-    aTarget->Flush();
+    aTarget->ClearRect(rect);
   }
 }
 
