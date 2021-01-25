a4fee3490b1e6d001e9b1583852703e0855a68d4
Marco Castelluccio <mcastelluccio@mozilla.com>
2021-01-25 19:40:17 +0000

    Bug 1686488 [wpt PR 27148] - wptserve: correct sorting in DirectoryHandler, a=testonly
    Automatic update from web-platform-tests
    wptserve: correct sorting in DirectoryHandler
    
    Found this while creating https://github.com/web-platform-tests/wpt/pull/27129.
    
    --
    
    wpt-commits: b4c87a442f82ff5e1de9a94e735b7b6c5f2e60c5
    wpt-pr: 27148
    
    UltraBlame original commit: 4b364b242ddcc4ac290b8a0685d76f8b158d002a

--- testing/web-platform/tests/tools/wptserve/wptserve/handlers.py
+++ testing/web-platform/tests/tools/wptserve/wptserve/handlers.py
@@ -98,7 +98,9 @@ class DirectoryHandler(object):
                    {"link": link, "name": ".."})
         items = []
         prev_item = None
-        for item in sorted(os.listdir(path)):
+        
+        
+        for item in sorted(os.listdir(path), key=lambda x: (x[:-len(".headers")], x) if x.endswith(".headers") else (x, x)):
             if prev_item and prev_item + ".headers" == item:
                 items[-1][1] = item
                 prev_item = None
