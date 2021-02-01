3fb4f68b86ee9f32913fe346f0616fa96448af96
Marco Castelluccio <mcastelluccio@mozilla.com>
2021-02-01 01:14:22 +0000

    Backed out changeset 91bf91b8a4b1 (bug 1689526) for Browser-chrome failures in accessible/tests/browser/fission/browser_take_focus.js. CLOSED TREE
    UltraBlame original commit: 47060c819b273592570f0459224680e9a22c6501

--- taskcluster/taskgraph/actions/isolate_test.py
+++ taskcluster/taskgraph/actions/isolate_test.py
@@ -191,15 +191,27 @@ def create_isolate_failure_tasks(task_definition, failures, level, times):
         else:
             task_definition["payload"]["command"] = command
 
+        
+        
+        
+        
+        
+
+        saved_command = copy.deepcopy(task_definition["payload"]["command"])
+
         for failure_path in failures[failure_group]:
             th_dict["symbol"] = symbol + failure_group_suffix
-            if is_wpt:
-                failure_path = "testing/web-platform/tests" + failure_path
-            if is_windows:
+            if is_windows and not is_wpt:
                 failure_path = "\\".join(failure_path.split("/"))
-            task_definition["payload"]["env"][
-                "MOZHARNESS_TEST_PATHS"
-            ] = six.ensure_text(json.dumps({suite: [failure_path]}, sort_keys=True))
+            if is_wpt:
+                include_args = ["--include={}".format(failure_path)]
+                task_definition["payload"]["command"] = add_args_to_command(
+                    saved_command, extra_args=include_args
+                )
+            else:
+                task_definition["payload"]["env"][
+                    "MOZHARNESS_TEST_PATHS"
+                ] = six.ensure_text(json.dumps({suite: [failure_path]}, sort_keys=True))
 
             logger.info(
                 "Creating task for path {} with command {}".format(
