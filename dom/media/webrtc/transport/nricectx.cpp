4c7a5d68860d519a1e4d47dfde627021af8b43dc
Marco Castelluccio <mcastelluccio@mozilla.com>
2021-02-01 01:10:56 +0000

    Bug 1416220: Get ice_unittest working. r=mjf
    * Allow nr_resolver to be used for TCP when not running in e10s/socket process mode.
    * Init IPv6 STUN/TURN servers appropriately.
    * Fix bug that was preventing STUN server hostname from being configured.
    * Disable some tests that relied on STUN TCP that hasn't been available for a long time.
      (This went unnoticed due to the previous problem)
    * A small logging improvement.
    
    Depends on D101660
    
    Differential Revision: https://phabricator.services.mozilla.com/D103332
    
    UltraBlame original commit: 6ee3e9718f05dc26d662a198ed9d9df3435c51fe

--- dom/media/webrtc/transport/nricectx.cpp
+++ dom/media/webrtc/transport/nricectx.cpp
@@ -44,6 +44,7 @@
 #include <vector>
 
 #include "nr_socket_proxy_config.h"
+#include "nsXULAppAPI.h"
 #include "mozilla/UniquePtr.h"
 #include "mozilla/Unused.h"
 
@@ -497,6 +498,14 @@ void NrIceCtx::InitializeGlobals(const GlobalConfig& aConfig) {
     if (!aConfig.mForceNetInterface.Length()) {
       NR_reg_set_string((char*)NR_ICE_REG_PREF_FORCE_INTERFACE_NAME,
                         const_cast<char*>(aConfig.mForceNetInterface.get()));
+    }
+
+    
+    NR_reg_set_char((char*)NR_ICE_REG_USE_NR_RESOLVER_FOR_UDP, 1);
+
+    
+    if (XRE_IsParentProcess()) {
+      NR_reg_set_char((char*)NR_ICE_REG_USE_NR_RESOLVER_FOR_TCP, 1);
     }
   }
 }
