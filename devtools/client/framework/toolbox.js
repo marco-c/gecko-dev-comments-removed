13d67b648438386afbc02db38a44dfc8ed287bbc
Marco Castelluccio <mcastelluccio@mozilla.com>
2021-02-01 01:12:36 +0000

    Bug 1688569 - [devtools] Watch DOM mutation breakpoints on all targets r=ochameau
    Differential Revision: https://phabricator.services.mozilla.com/D103072
    
    UltraBlame original commit: 0f185f5d97699ac7e2579d68e6030a35c18ad01b

--- devtools/client/framework/toolbox.js
+++ devtools/client/framework/toolbox.js
@@ -701,11 +701,13 @@ Toolbox.prototype = {
       targetFront.on("navigate", this._onNavigate);
       targetFront.on("frame-update", this._updateFrames);
       targetFront.on("inspect-object", this._onInspectObject);
-
-      targetFront.watchFronts("inspector", async inspectorFront => {
-        registerWalkerListeners(this.store, inspectorFront.walker);
-      });
     }
+
+    
+    
+    targetFront.watchFronts("inspector", async inspectorFront => {
+      registerWalkerListeners(this.store, inspectorFront.walker);
+    });
 
     const { threadFront } = targetFront;
     if (threadFront) {
