2c6b96c0d4f05618fcad219ba36983fc0dc46d6d
Marco Castelluccio <mcastelluccio@mozilla.com>
2021-02-01 01:12:08 +0000

    Bug 1681469 - Allow nsBaseHashtable to work with a non-default-constructible/non-movable DataType; r=nika
    nsBaseHashtable now supports non-default-constructible DataType and
    UserDataType, however not all methods can be instantiated.  All methods which
    can't be instantiated with non-default-constructible DataType or UserDataType
    are now described as such in method definitions.
    
    The public API of PLDHashTable and nsBaseHashtable/nsDataHashtable was changed:
    - A new method PLDHashTable::WithEntryHandle has been added. It allows to use a
      custom function for entry initialization (instead of the global hook).
    - A new method nsBaseHashtable::MaybeGet has been added.
    - A new overload nsBaseHashtable::Remove has been added.
    - The nsDataHashtable::GetAndRemove method has been pulled up to
      nsBaseHashtable.
    
    In addition, the following implementation details have changed:
    
    PLDHashTable:
    - The code from the Add method has been split into MakeEntryHandle and a helper
      object called EntryHandle. The Add method is now implemented on top of that.
    
    nsTHashtable:
    - A new (non-public) API for WithEntryHandle has been added.
    - The InitEntry hook is no longer used. Instead of using the hook, PutEntry
      methods now use nsTHashtable::WithEntryHandle instead of PLDHashTable::Add.
      This change allows to do custom initialization in derived classes.
    
    nsBaseHashtable:
    - A new (non-public) API for WithEntryHandle has been added.
    - Put methods no longer use nsTHashtable::PutEntry, they now use the new method
      nsBaseHashtable::WithEntryHandle.
    
    Differential Revision: https://phabricator.services.mozilla.com/D99428
    
    UltraBlame original commit: 78a57e651002e225f7da5b7ff50d2241eb3e1d3b

--- xpcom/ds/nsDataHashtable.h
+++ xpcom/ds/nsDataHashtable.h
@@ -44,23 +44,6 @@ class nsDataHashtable : public nsBaseHashtable<KeyClass, DataType, DataType> {
     }
     return nullptr;
   }
-
-  
-
-
-
-
-
-
-
-  mozilla::Maybe<DataType> GetAndRemove(KeyType aKey) {
-    mozilla::Maybe<DataType> value;
-    if (EntryType* ent = this->GetEntry(aKey)) {
-      value.emplace(std::move(*ent->GetModifiableData()));
-      this->RemoveEntry(ent);
-    }
-    return value;
-  }
 };
 
 #endif  
