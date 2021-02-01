b67b30d015644156ab2be6f743c8691984df3401
Marco Castelluccio <mcastelluccio@mozilla.com>
2021-02-01 01:02:22 +0000

    Bug 1643450 - Part 1: Allow creating windows from closing tabs, r=kmag
    Differential Revision: https://phabricator.services.mozilla.com/D100165
    
    UltraBlame original commit: baa6b71bfaef55ebec3e1d4a5d90571e35affb8d

--- dom/ipc/ContentParent.cpp
+++ dom/ipc/ContentParent.cpp
@@ -5432,7 +5432,8 @@ mozilla::ipc::IPCResult ContentParent::RecvCreateWindow(
   });
 
   
-  if (aParent.IsDiscarded()) {
+  RefPtr<BrowsingContext> parent = aParent.GetMaybeDiscarded();
+  if (NS_WARN_IF(!parent)) {
     rv = NS_ERROR_FAILURE;
     return IPC_OK();
   }
@@ -5443,8 +5444,8 @@ mozilla::ipc::IPCResult ContentParent::RecvCreateWindow(
     return IPC_FAIL(this, "Missing BrowsingContext for new tab");
   }
 
-  RefPtr<BrowsingContext> newBCOpener = newBC->GetOpener();
-  if (newBCOpener && aParent.get() != newBCOpener) {
+  uint64_t newBCOpenerId = newBC->GetOpenerId();
+  if (newBCOpenerId != 0 && parent->Id() != newBCOpenerId) {
     return IPC_FAIL(this, "Invalid opener BrowsingContext for new tab");
   }
   if (newBC->GetParent() != nullptr) {
@@ -5475,7 +5476,7 @@ mozilla::ipc::IPCResult ContentParent::RecvCreateWindow(
   nsCOMPtr<nsIRemoteTab> newRemoteTab;
   int32_t openLocation = nsIBrowserDOMWindow::OPEN_NEWWINDOW;
   mozilla::ipc::IPCResult ipcResult = CommonCreateWindow(
-      aThisTab, aParent.get(), !!newBCOpener, aChromeFlags, aCalledFromJS,
+      aThisTab, parent, newBCOpenerId != 0, aChromeFlags, aCalledFromJS,
       aWidthSpecified, aForPrinting, aForPrintPreview, aURIToLoad, aFeatures,
       aFullZoom, newTab, VoidString(), rv, newRemoteTab, &cwi.windowOpened(),
       openLocation, aTriggeringPrincipal, aReferrerInfo,  false,
@@ -5515,7 +5516,8 @@ mozilla::ipc::IPCResult ContentParent::RecvCreateWindowInDifferentProcess(
   MOZ_DIAGNOSTIC_ASSERT(!nsContentUtils::IsSpecialName(aName));
 
   
-  if (NS_WARN_IF(aParent.IsDiscarded())) {
+  RefPtr<BrowsingContext> parent = aParent.GetMaybeDiscarded();
+  if (NS_WARN_IF(!parent)) {
     return IPC_OK();
   }
 
@@ -5550,8 +5552,8 @@ mozilla::ipc::IPCResult ContentParent::RecvCreateWindowInDifferentProcess(
 
   nsresult rv;
   mozilla::ipc::IPCResult ipcResult = CommonCreateWindow(
-      aThisTab, aParent.get(),  false, aChromeFlags,
-      aCalledFromJS, aWidthSpecified,  false,
+      aThisTab, parent,  false, aChromeFlags, aCalledFromJS,
+      aWidthSpecified,  false,
        false, aURIToLoad, aFeatures, aFullZoom,
        nullptr, aName, rv, newRemoteTab, &windowIsNew,
       openLocation, aTriggeringPrincipal, aReferrerInfo,
