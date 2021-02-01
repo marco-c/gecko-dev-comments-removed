2ba5889fac8b4677f3b798fa2ff39b618e09d996
Marco Castelluccio <mcastelluccio@mozilla.com>
2021-02-01 01:06:39 +0000

    Bug 493683 part 5: When aria-labell/describedby is set, set HasName/DescriptionDependent on the target subtree. r=eeejay
    Differential Revision: https://phabricator.services.mozilla.com/D102680
    
    UltraBlame original commit: 2c9004c95a7a7d39c2c1417b66fda37892398263

--- accessible/generic/DocAccessible.cpp
+++ accessible/generic/DocAccessible.cpp
@@ -16,6 +16,7 @@
 #include "nsEventShell.h"
 #include "nsLayoutUtils.h"
 #include "nsTextEquivUtils.h"
+#include "Pivot.h"
 #include "Role.h"
 #include "RootAccessible.h"
 #include "TreeWalker.h"
@@ -851,15 +852,50 @@ void DocAccessible::AttributeChangedImpl(Accessible* aAccessible,
     return;
   }
 
+  dom::Element* elm = aAccessible->GetContent()->AsElement();
   if (aAttribute == nsGkAtoms::aria_describedby) {
     FireDelayedEvent(nsIAccessibleEvent::EVENT_DESCRIPTION_CHANGE, aAccessible);
+    if (aModType == dom::MutationEvent_Binding::MODIFICATION ||
+        aModType == dom::MutationEvent_Binding::ADDITION) {
+      
+      
+      
+      IDRefsIterator iter(this, aAccessible->Elm(),
+                          nsGkAtoms::aria_describedby);
+      while (Accessible* target = iter.Next()) {
+        Pivot pivot(target);
+        LocalAccInSameDocRule rule;
+        for (AccessibleOrProxy anchor(target); !anchor.IsNull();
+             anchor = pivot.Next(anchor, rule)) {
+          Accessible* acc = anchor.AsAccessible();
+          MOZ_ASSERT(acc);
+          acc->mContextFlags |= eHasDescriptionDependent;
+        }
+      }
+    }
     return;
   }
 
-  dom::Element* elm = aAccessible->GetContent()->AsElement();
   if (aAttribute == nsGkAtoms::aria_labelledby &&
       !elm->HasAttr(kNameSpaceID_None, nsGkAtoms::aria_label)) {
     FireDelayedEvent(nsIAccessibleEvent::EVENT_NAME_CHANGE, aAccessible);
+    if (aModType == dom::MutationEvent_Binding::MODIFICATION ||
+        aModType == dom::MutationEvent_Binding::ADDITION) {
+      
+      
+      
+      IDRefsIterator iter(this, aAccessible->Elm(), nsGkAtoms::aria_labelledby);
+      while (Accessible* target = iter.Next()) {
+        Pivot pivot(target);
+        LocalAccInSameDocRule rule;
+        for (AccessibleOrProxy anchor(target); !anchor.IsNull();
+             anchor = pivot.Next(anchor, rule)) {
+          Accessible* acc = anchor.AsAccessible();
+          MOZ_ASSERT(acc);
+          acc->mContextFlags |= eHasNameDependent;
+        }
+      }
+    }
     return;
   }
 
