0afc3c13492ce345413b30fbbab1454b6a8dfd68
Marco Castelluccio <mcastelluccio@mozilla.com>
2021-02-03 13:35:30 +0000

    Bug 1683126 - Make nsCanvasFrame::Reflow more robust.  r=TYLin
    This patch improves nsCanvasFrame::Reflow in a few ways.
    First, we now iterate over mFrames and reflow every child.
    This makes it more robust vis-à-vis the order of any placeholders
    and the root frame, and also resilient against a missing root
    frame (this fixes the fatal assertion in this bug).  We now
    also actually reflow all placeholders which wasn't the case
    before. It seems like a prudent thing to do.
    
    I also added a separate nsReflowStatus for each child.
    
    We now also call SetOverflowAreasToDesiredBounds() in all
    cases.  We failed to do that in the 'mFrames.IsEmpty()' case
    before, which triggered the assertions in bug 1655630 and
    bug 1392106.
    
    Differential Revision: https://phabricator.services.mozilla.com/D103592
    
    UltraBlame original commit: 35053583b9a751daed53f428f12219425a84004c

--- layout/generic/nsCanvasFrame.cpp
+++ layout/generic/nsCanvasFrame.cpp
@@ -728,20 +728,27 @@ void nsCanvasFrame::Reflow(nsPresContext* aPresContext,
   
   
   
-  ReflowOutput kidDesiredSize(aReflowInput);
-  if (mFrames.IsEmpty()) {
-    
-    aDesiredSize.Width() = aDesiredSize.Height() = 0;
-  } else if (mFrames.FirstChild() != mPopupSetFrame) {
-    nsIFrame* kidFrame = mFrames.FirstChild();
-    bool kidDirty = kidFrame->HasAnyStateBits(NS_FRAME_IS_DIRTY);
+  
+  
+  const WritingMode wm = aReflowInput.GetWritingMode();
+  aDesiredSize.SetSize(wm, aReflowInput.ComputedSize());
+  aDesiredSize.SetOverflowAreasToDesiredBounds();
+  nsIFrame* nextKid = nullptr;
+  for (auto* kidFrame = mFrames.FirstChild(); kidFrame; kidFrame = nextKid) {
+    nextKid = kidFrame->GetNextSibling();
+    if (kidFrame == mPopupSetFrame) {
+      
+      continue;
+    }
 
+    ReflowOutput kidDesiredSize(aReflowInput);
+    bool kidDirty = kidFrame->HasAnyStateBits(NS_FRAME_IS_DIRTY);
     WritingMode kidWM = kidFrame->GetWritingMode();
     auto availableSize = aReflowInput.AvailableSize(kidWM);
     nscoord bOffset = 0;
     nscoord canvasBSizeSum = 0;
-    WritingMode wm = aReflowInput.GetWritingMode();
     if (prevCanvasFrame && availableSize.BSize(kidWM) != NS_UNCONSTRAINEDSIZE &&
+        !kidFrame->IsPlaceholderFrame() &&
         StaticPrefs::layout_display_list_improve_fragmentation()) {
       for (auto* pif = prevCanvasFrame; pif;
            pif = static_cast<nsCanvasFrame*>(pif->GetPrevInFlow())) {
@@ -768,7 +775,6 @@ void nsCanvasFrame::Reflow(nsPresContext* aPresContext,
       availableSize.BSize(kidWM) -= bOffset;
     }
 
-    LogicalSize finalSize = aReflowInput.ComputedSize();
     if (MOZ_LIKELY(availableSize.BSize(kidWM) > 0)) {
       ReflowInput kidReflowInput(aPresContext, aReflowInput, kidFrame,
                                  availableSize);
@@ -785,18 +791,17 @@ void nsCanvasFrame::Reflow(nsPresContext* aPresContext,
       LogicalPoint kidPt(kidWM, margin.IStart(kidWM), margin.BStart(kidWM));
       (kidWM.IsOrthogonalTo(wm) ? kidPt.I(kidWM) : kidPt.B(kidWM)) += bOffset;
 
-      
+      nsReflowStatus kidStatus;
       ReflowChild(kidFrame, aPresContext, kidDesiredSize, kidReflowInput, kidWM,
-                  kidPt, containerSize, ReflowChildFlags::Default, aStatus);
+                  kidPt, containerSize, ReflowChildFlags::Default, kidStatus);
 
-      
       FinishReflowChild(kidFrame, aPresContext, kidDesiredSize, &kidReflowInput,
                         kidWM, kidPt, containerSize,
                         ReflowChildFlags::ApplyRelativePositioning);
 
-      if (!aStatus.IsFullyComplete()) {
+      if (!kidStatus.IsFullyComplete()) {
         nsIFrame* nextFrame = kidFrame->GetNextInFlow();
-        NS_ASSERTION(nextFrame || aStatus.NextInFlowNeedsReflow(),
+        NS_ASSERTION(nextFrame || kidStatus.NextInFlowNeedsReflow(),
                      "If it's incomplete and has no nif yet, it must flag a "
                      "nif reflow.");
         if (!nextFrame) {
@@ -809,10 +814,11 @@ void nsCanvasFrame::Reflow(nsPresContext* aPresContext,
           
           
         }
-        if (aStatus.IsOverflowIncomplete()) {
+        if (kidStatus.IsOverflowIncomplete()) {
           nextFrame->AddStateBits(NS_FRAME_IS_OVERFLOW_CONTAINER);
         }
       }
+      aStatus.MergeCompletionStatusFrom(kidStatus);
 
       
       
@@ -832,28 +838,29 @@ void nsCanvasFrame::Reflow(nsPresContext* aPresContext,
       
       
       
-      if (aReflowInput.ComputedBSize() == NS_UNCONSTRAINEDSIZE) {
+      if (aReflowInput.ComputedBSize() == NS_UNCONSTRAINEDSIZE &&
+          !kidFrame->IsPlaceholderFrame()) {
+        LogicalSize finalSize = aReflowInput.ComputedSize();
         finalSize.BSize(wm) =
             kidFrame->GetLogicalSize(wm).BSize(wm) +
             kidReflowInput.ComputedLogicalMargin(wm).BStartEnd(wm);
+        aDesiredSize.SetSize(wm, finalSize);
+        aDesiredSize.SetOverflowAreasToDesiredBounds();
       }
+      aDesiredSize.mOverflowAreas.UnionWith(kidDesiredSize.mOverflowAreas +
+                                            kidFrame->GetPosition());
+    } else if (kidFrame->IsPlaceholderFrame()) {
+      
     } else {
       
       
       
       
       
-      MOZ_ASSERT(!kidFrame->IsPlaceholderFrame(),
-                 "we should never push fixed pos placeholders");
       mFrames.RemoveFrame(kidFrame);
       SetOverflowFrames(nsFrameList(kidFrame, kidFrame));
       aStatus.SetIncomplete();
     }
-
-    aDesiredSize.SetSize(wm, finalSize);
-    aDesiredSize.SetOverflowAreasToDesiredBounds();
-    aDesiredSize.mOverflowAreas.UnionWith(kidDesiredSize.mOverflowAreas +
-                                          kidFrame->GetPosition());
   }
 
   if (prevCanvasFrame) {
