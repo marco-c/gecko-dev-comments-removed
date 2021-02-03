96e0253eb03959f160cb1d6388110b9db44bb05e
Marco Castelluccio <mcastelluccio@mozilla.com>
2021-02-03 13:34:28 +0000

    Bug 1690022 - Remove concept of paths for parallel phases r=sfink
    Parallel phases don't nest, unlike main thread phases. This removes the restriction that parallel phases must be recorded when in a suitable main thread parent, to allow the main thread to pause background work in any phase without associating that work with what is currently happening in the main thread.
    
    Depends on D103675
    
    Differential Revision: https://phabricator.services.mozilla.com/D103676
    
    UltraBlame original commit: 8a30599206853c9bc4c78a662d9e52199640c25a

--- js/src/gc/Statistics.h
+++ js/src/gc/Statistics.h
@@ -146,7 +146,9 @@ struct Statistics {
   using TimeStamp = mozilla::TimeStamp;
 
   
-  using PhaseTimeTable = EnumeratedArray<Phase, Phase::LIMIT, TimeDuration>;
+  using PhaseTimes = EnumeratedArray<Phase, Phase::LIMIT, TimeDuration>;
+  using PhaseKindTimes =
+      EnumeratedArray<PhaseKind, PhaseKind::LIMIT, TimeDuration>;
 
   static MOZ_MUST_USE bool initialize();
 
@@ -274,8 +276,8 @@ struct Statistics {
     TimeStamp end;
     size_t startFaults = 0;
     size_t endFaults = 0;
-    PhaseTimeTable phaseTimes;
-    PhaseTimeTable maxParallelTimes;
+    PhaseTimes phaseTimes;
+    PhaseKindTimes maxParallelTimes;
 
     TimeDuration duration() const { return end - start; }
     bool wasReset() const { return resetReason != GCAbortReason::None; }
@@ -351,7 +353,7 @@ struct Statistics {
   TimeDuration timedGCTime;
 
   
-  PhaseTimeTable phaseTimes;
+  PhaseTimes phaseTimes;
 
   
   EnumeratedArray<Count, COUNT_LIMIT,
@@ -456,20 +458,18 @@ struct Statistics {
 
   void reportLongestPhaseInMajorGC(PhaseKind longest, int telemetryId);
 
-  UniqueChars formatCompactSlicePhaseTimes(
-      const PhaseTimeTable& phaseTimes) const;
+  UniqueChars formatCompactSlicePhaseTimes(const PhaseTimes& phaseTimes) const;
 
   UniqueChars formatDetailedDescription() const;
   UniqueChars formatDetailedSliceDescription(unsigned i,
                                              const SliceData& slice) const;
-  UniqueChars formatDetailedPhaseTimes(const PhaseTimeTable& phaseTimes) const;
+  UniqueChars formatDetailedPhaseTimes(const PhaseTimes& phaseTimes) const;
   UniqueChars formatDetailedTotals() const;
 
   void formatJsonDescription(JSONPrinter&) const;
   void formatJsonSliceDescription(unsigned i, const SliceData& slice,
                                   JSONPrinter&) const;
-  void formatJsonPhaseTimes(const PhaseTimeTable& phaseTimes,
-                            JSONPrinter&) const;
+  void formatJsonPhaseTimes(const PhaseTimes& phaseTimes, JSONPrinter&) const;
   void formatJsonSlice(size_t sliceNum, JSONPrinter&) const;
 
   double computeMMU(TimeDuration resolution) const;
