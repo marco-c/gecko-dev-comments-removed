c9258564ac7b6d16c60ce33cb57a7586613db297
Marco Castelluccio <mcastelluccio@mozilla.com>
2021-02-03 13:36:52 +0000

    Bug 1630415 allow nativeMessaging in optional permissions r=rpl
    Differential Revision: https://phabricator.services.mozilla.com/D102998
    
    UltraBlame original commit: 4eebd98e49971b977dbd73b7ca23657d87c94542

--- toolkit/components/extensions/test/xpcshell/test_ext_permissions_uninstall.js
+++ toolkit/components/extensions/test/xpcshell/test_ext_permissions_uninstall.js
@@ -18,15 +18,6 @@ Services.prefs.setBoolPref(
   false
 );
 
-const observer = {
-  observe(subject, topic, data) {
-    if (topic == "webextension-optional-permission-prompt") {
-      let { resolve } = subject.wrappedJSObject;
-      resolve(true);
-    }
-  },
-};
-
 
 async function getCachedPermissions(extensionId) {
   const NotFound = Symbol("extension ID not found in permissions cache");
@@ -60,19 +51,12 @@ add_task(async function setup() {
   
   await ExtensionPermissions._uninit();
 
-  Services.prefs.setBoolPref(
-    "extensions.webextOptionalPermissionPrompts",
-    true
-  );
-  Services.obs.addObserver(observer, "webextension-optional-permission-prompt");
+  optionalPermissionsPromptHandler.init();
+  optionalPermissionsPromptHandler.acceptPrompt = true;
+
   await AddonTestUtils.promiseStartupManager();
   registerCleanupFunction(async () => {
     await AddonTestUtils.promiseShutdownManager();
-    Services.obs.removeObserver(
-      observer,
-      "webextension-optional-permission-prompt"
-    );
-    Services.prefs.clearUserPref("extensions.webextOptionalPermissionPrompts");
   });
 });
 
