e1e74ef0065d72cb442a2c8a13ca9f71dd7dff59
Marco Castelluccio <mcastelluccio@mozilla.com>
2021-02-06 02:18:49 +0000

    Bug 1674142 Part 2: Upgrade to a release assert in CompositorWidget::CreateLocal. r=jrmuizel
    Differential Revision: https://phabricator.services.mozilla.com/D103943
    
    UltraBlame original commit: cd39bfc7270316ee203ea5a72e13615ce339facf

--- widget/InProcessCompositorWidget.cpp
+++ widget/InProcessCompositorWidget.cpp
@@ -21,7 +21,10 @@ namespace widget {
 RefPtr<CompositorWidget> CompositorWidget::CreateLocal(
     const CompositorWidgetInitData& aInitData,
     const layers::CompositorOptions& aOptions, nsIWidget* aWidget) {
-  MOZ_ASSERT(aWidget);
+  
+  
+  
+  MOZ_RELEASE_ASSERT(aWidget);
 #  ifdef MOZ_WIDGET_ANDROID
   return new AndroidCompositorWidget(aOptions,
                                      static_cast<nsBaseWidget*>(aWidget));
