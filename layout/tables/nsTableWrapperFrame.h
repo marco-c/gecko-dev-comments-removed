80ce0ec94f16fd762d43c2f079567c52e2321a0f
Marco Castelluccio <mcastelluccio@mozilla.com>
2021-02-06 02:22:55 +0000

    Bug 1690901 Part 3 - Rename OuterDoReflowChild() to ReflowChild() for nsTableWrapperFrame. r=layout-reviewers,mats
    Differential Revision: https://phabricator.services.mozilla.com/D104107
    
    UltraBlame original commit: 48895a7f84a63b732793e57c98079c4367d5f089

--- layout/tables/nsTableWrapperFrame.h
+++ layout/tables/nsTableWrapperFrame.h
@@ -240,9 +240,9 @@ class nsTableWrapperFrame : public nsContainerFrame {
                                    const nscoord aAvailISize) const;
 
   
-  void OuterDoReflowChild(nsPresContext* aPresContext, nsIFrame* aChildFrame,
-                          const ReflowInput& aChildRI, ReflowOutput& aMetrics,
-                          nsReflowStatus& aStatus);
+  void ReflowChild(nsPresContext* aPresContext, nsIFrame* aChildFrame,
+                   const ReflowInput& aChildRI, ReflowOutput& aMetrics,
+                   nsReflowStatus& aStatus);
 
   
   void UpdateOverflowAreas(ReflowOutput& aMet);
