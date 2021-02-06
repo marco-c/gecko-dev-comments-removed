edea1d247336b8b19dd7344a9f5a6ffba8fa9624
Marco Castelluccio <mcastelluccio@mozilla.com>
2021-02-06 02:19:16 +0000

    Bug 1684967 - Don't dispose GeckoEditableChild during JNI call. r=geckoview-reviewers,aklotz
    This is a regression by bug 1651705.
    
    After landing it, we use read-write lock to access GeckoEditableChild object.
    But when using form submission with target=_blank, since we use some nested
    event loop to open new window in same process, we may try to dispose
    GeckoEditableChild even if it is still used. Then, it may be dead lock.
    
    So I add a blocker class helper not to dispose GeckoEditableChild immediately.
    
    Differential Revision: https://phabricator.services.mozilla.com/D103742
    
    UltraBlame original commit: e9d727985226c06744f33311b0d51f6600c4ad26

--- widget/android/GeckoEditableSupport.h
+++ widget/android/GeckoEditableSupport.h
@@ -86,11 +86,13 @@ class GeckoEditableSupport final
   AutoTArray<UniquePtr<mozilla::WidgetEvent>, 4> mIMEKeyEvents;
   AutoTArray<IMETextChange, 4> mIMETextChanges;
   RefPtr<TextRangeArray> mIMERanges;
+  RefPtr<Runnable> mDisposeRunnable;
   int32_t mIMEMaskEventsCount;         
   int32_t mIMEFocusCount;              
   bool mIMEDelaySynchronizeReply;      
   int32_t mIMEActiveSynchronizeCount;  
   int32_t mIMEActiveCompositionCount;  
+  uint32_t mDisposeBlockCount;
   bool mIMESelectionChanged;
   bool mIMETextChangedDuringFlush;
   bool mIMEMonitorCursor;
@@ -166,6 +168,7 @@ class GeckoEditableSupport final
         mIMEFocusCount(0),
         mIMEDelaySynchronizeReply(false),
         mIMEActiveSynchronizeCount(0),
+        mDisposeBlockCount(0),
         mIMESelectionChanged(false),
         mIMETextChangedDuringFlush(false),
         mIMEMonitorCursor(false) {}
@@ -195,6 +198,34 @@ class GeckoEditableSupport final
 
   InputContext GetInputContext();
 
+  bool HasIMEFocus() const { return mIMEFocusCount != 0; }
+
+  void AddBlocker() { mDisposeBlockCount++; }
+
+  void ReleaseBlocker() {
+    mDisposeBlockCount--;
+
+    if (!mDisposeBlockCount && mDisposeRunnable) {
+      if (HasIMEFocus()) {
+        
+        
+        mDisposeRunnable = nullptr;
+        return;
+      }
+
+      RefPtr<GeckoEditableSupport> self(this);
+      RefPtr<Runnable> disposer = std::move(mDisposeRunnable);
+
+      nsAppShell::PostEvent(
+          [self = std::move(self), disposer = std::move(disposer)] {
+            self->mEditableAttached = false;
+            disposer->Run();
+          });
+    }
+  }
+
+  bool IsGeckoEditableUsed() const { return mDisposeBlockCount != 0; }
+
   
   using EditableBase::AttachNative;
   using EditableBase::DisposeNative;
@@ -205,6 +236,12 @@ class GeckoEditableSupport final
     RefPtr<GeckoEditableSupport> self(this);
     nsAppShell::PostEvent(
         [self = std::move(self), disposer = RefPtr<Runnable>(aDisposer)] {
+          if (self->IsGeckoEditableUsed()) {
+            
+            
+            self->mDisposeRunnable = disposer;
+            return;
+          }
           self->mEditableAttached = false;
           disposer->Run();
         });
