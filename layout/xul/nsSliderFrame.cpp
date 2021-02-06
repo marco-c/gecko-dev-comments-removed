04ff9528ac4244130911bfaba97183c2000af662
Marco Castelluccio <mcastelluccio@mozilla.com>
2021-02-06 02:18:31 +0000

    Bug 259640, adjust the height of the find marks on scrollbars based on dpi, r=tnikkel
    Differential Revision: https://phabricator.services.mozilla.com/D101380
    
    UltraBlame original commit: 3bece74bc5516f96b1db6deb18a77accbfecdf1e

--- layout/xul/nsSliderFrame.cpp
+++ layout/xul/nsSliderFrame.cpp
@@ -32,6 +32,7 @@
 #include "nsContentUtils.h"
 #include "nsLayoutUtils.h"
 #include "nsDisplayList.h"
+#include "nsDeviceContext.h"
 #include "nsRefreshDriver.h"     
 #include "mozilla/Assertions.h"  
 #include "mozilla/DisplayPortUtils.h"
@@ -302,6 +303,14 @@ void nsDisplaySliderMarks::PaintMarks(nsDisplayListBuilder* aDisplayListBuilder,
 
   nsPoint refPoint = aDisplayListBuilder->ToReferenceFrame(mFrame);
 
+  
+  
+  
+  float increasePixels = sliderFrame->PresContext()
+                             ->DeviceContext()
+                             ->GetDesktopToDeviceScale()
+                             .scale;
+
   nsTArray<uint32_t>& marks = window->GetScrollMarks();
   for (uint32_t m = 0; m < marks.Length(); m++) {
     uint32_t markValue = marks[m];
@@ -319,17 +328,15 @@ void nsDisplaySliderMarks::PaintMarks(nsDisplayListBuilder* aDisplayListBuilder,
     markRect.y +=
         (nscoord)((double)markValue / (maxPos - minPos) * sliderRect.height);
 
-    
-    
     if (drawTarget) {
       Rect devPixelRect =
           NSRectToSnappedRect(markRect, appUnitsPerDevPixel, *drawTarget);
-      devPixelRect.Inflate(0, 2);
+      devPixelRect.Inflate(0, increasePixels);
       drawTarget->FillRect(devPixelRect, ColorPattern(fillColor));
     } else {
       LayoutDeviceIntRect dRect = LayoutDeviceIntRect::FromAppUnitsToNearest(
           markRect, appUnitsPerDevPixel);
-      dRect.Inflate(0, 2);
+      dRect.Inflate(0, increasePixels);
       wr::LayoutRect layoutRect = wr::ToLayoutRect(dRect);
       aBuilder->PushRect(layoutRect, layoutRect, BackfaceIsHidden(),
                          wr::ToColorF(fillColor));
