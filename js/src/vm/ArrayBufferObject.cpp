41d2021e1d1dd394c4ffbb1e593a43a580b7ca02
Marco Castelluccio <mcastelluccio@mozilla.com>
2021-02-10 00:32:16 +0000

    Bug 1689503 - Update ArrayBufferObject size accounting to handle external ArrayBuffers r=jorendorff
    Differential Revision: https://phabricator.services.mozilla.com/D104412
    
    UltraBlame original commit: b1472558e0c3d55ed53093dd849e0c3425869a61

--- js/src/vm/ArrayBufferObject.cpp
+++ js/src/vm/ArrayBufferObject.cpp
@@ -1464,6 +1464,10 @@ void ArrayBufferObject::addSizeOfExcludingThis(
     case USER_OWNED:
       
       break;
+    case EXTERNAL:
+      
+      
+      break;
     case MAPPED:
       info->objectsNonHeapElementsNormal += buffer.byteLength().get();
       break;
@@ -1472,9 +1476,6 @@ void ArrayBufferObject::addSizeOfExcludingThis(
       MOZ_ASSERT(buffer.wasmMappedSize() >= buffer.byteLength().get());
       info->wasmGuardPages +=
           buffer.wasmMappedSize() - buffer.byteLength().get();
-      break;
-    case EXTERNAL:
-      MOZ_CRASH("external buffers not currently supported");
       break;
     case BAD1:
       MOZ_CRASH("bad bufferKind()");
