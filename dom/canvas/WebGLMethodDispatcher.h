f91d3c4cfbfdecc90c2fb627dafec3ad5e90ab20
Marco Castelluccio <mcastelluccio@mozilla.com>
2021-02-10 00:29:45 +0000

    Bug 1691606 - Clean up dead code following WebGL out-of-process stabilization. r=lsalzman
    Differential Revision: https://phabricator.services.mozilla.com/D104495
    
    UltraBlame original commit: 5f240b003696c75b0d2a47cc94055285de02f559

--- dom/canvas/WebGLMethodDispatcher.h
+++ dom/canvas/WebGLMethodDispatcher.h
@@ -7,13 +7,10 @@
 #define WEBGLMETHODDISPATCHER_H_
 
 #include "TexUnpackBlob.h"
-#include "WebGLCrossProcessCommandQueue.h"
 #include "HostWebGLContext.h"
 #include "WebGLQueueParamTraits.h"
 
 namespace mozilla {
-
-
 
 template <size_t id = 0>
 class WebGLMethodDispatcher
@@ -22,11 +19,11 @@ class WebGLMethodDispatcher
 template <typename MethodT, MethodT Method>
 size_t IdByMethod() = delete;
 
-#define DEFINE_METHOD_DISPATCHER(_ID, _METHOD, _SYNC)                    \
+#define DEFINE_METHOD_DISPATCHER(_ID, _METHOD)                    \
   template <>                                                            \
   class WebGLMethodDispatcher<_ID>                                       \
       : public MethodDispatcher<WebGLMethodDispatcher, _ID,              \
-                                decltype(&_METHOD), &_METHOD, _SYNC> {}; \
+                                decltype(&_METHOD), &_METHOD> {}; \
   template <>                                                            \
   inline size_t IdByMethod<decltype(&_METHOD), &_METHOD>() {             \
     return _ID;                                                          \
@@ -34,14 +31,11 @@ size_t IdByMethod() = delete;
 
 
 
-#define DEFINE_METHOD_HELPER(_METHOD, _SYNC) \
-  DEFINE_METHOD_DISPATCHER(__COUNTER__, _METHOD, _SYNC)
 #define DEFINE_ASYNC(_METHOD) \
-  DEFINE_METHOD_HELPER(_METHOD, CommandSyncType::ASYNC)
+  DEFINE_METHOD_DISPATCHER(__COUNTER__, _METHOD)
 
 DEFINE_ASYNC(HostWebGLContext::CreateBuffer)
 DEFINE_ASYNC(HostWebGLContext::CreateFramebuffer)
-
 DEFINE_ASYNC(HostWebGLContext::CreateProgram)
 DEFINE_ASYNC(HostWebGLContext::CreateQuery)
 DEFINE_ASYNC(HostWebGLContext::CreateRenderbuffer)
@@ -67,26 +61,15 @@ DEFINE_ASYNC(HostWebGLContext::DeleteVertexArray)
 DEFINE_ASYNC(HostWebGLContext::Disable)
 DEFINE_ASYNC(HostWebGLContext::Enable)
 DEFINE_ASYNC(HostWebGLContext::GenerateError)
-
-
-
-
-
-
 DEFINE_ASYNC(HostWebGLContext::Resize)
 DEFINE_ASYNC(HostWebGLContext::RequestExtension)
-
-
 DEFINE_ASYNC(HostWebGLContext::DidRefresh)
-
-
 DEFINE_ASYNC(HostWebGLContext::AttachShader)
 DEFINE_ASYNC(HostWebGLContext::BindAttribLocation)
 DEFINE_ASYNC(HostWebGLContext::BindFramebuffer)
 DEFINE_ASYNC(HostWebGLContext::BlendColor)
 DEFINE_ASYNC(HostWebGLContext::BlendEquationSeparate)
 DEFINE_ASYNC(HostWebGLContext::BlendFuncSeparate)
-
 DEFINE_ASYNC(HostWebGLContext::Clear)
 DEFINE_ASYNC(HostWebGLContext::ClearColor)
 DEFINE_ASYNC(HostWebGLContext::ClearDepth)
@@ -99,15 +82,8 @@ DEFINE_ASYNC(HostWebGLContext::DepthMask)
 DEFINE_ASYNC(HostWebGLContext::DepthRange)
 DEFINE_ASYNC(HostWebGLContext::DetachShader)
 DEFINE_ASYNC(HostWebGLContext::Flush)
-
 DEFINE_ASYNC(HostWebGLContext::FramebufferAttach)
 DEFINE_ASYNC(HostWebGLContext::FrontFace)
-
-
-
-
-
-
 DEFINE_ASYNC(HostWebGLContext::Hint)
 DEFINE_ASYNC(HostWebGLContext::LineWidth)
 DEFINE_ASYNC(HostWebGLContext::LinkProgram)
@@ -123,14 +99,12 @@ DEFINE_ASYNC(HostWebGLContext::Viewport)
 DEFINE_ASYNC(HostWebGLContext::BindBuffer)
 DEFINE_ASYNC(HostWebGLContext::BindBufferRange)
 DEFINE_ASYNC(HostWebGLContext::CopyBufferSubData)
-
 DEFINE_ASYNC(HostWebGLContext::BufferData)
 DEFINE_ASYNC(HostWebGLContext::BufferSubData)
 DEFINE_ASYNC(HostWebGLContext::BlitFramebuffer)
 DEFINE_ASYNC(HostWebGLContext::InvalidateFramebuffer)
 DEFINE_ASYNC(HostWebGLContext::InvalidateSubFramebuffer)
 DEFINE_ASYNC(HostWebGLContext::ReadBuffer)
-
 DEFINE_ASYNC(HostWebGLContext::RenderbufferStorageMultisample)
 DEFINE_ASYNC(HostWebGLContext::ActiveTexture)
 DEFINE_ASYNC(HostWebGLContext::BindTexture)
@@ -139,28 +113,21 @@ DEFINE_ASYNC(HostWebGLContext::CopyTexImage)
 DEFINE_ASYNC(HostWebGLContext::TexStorage)
 DEFINE_ASYNC(HostWebGLContext::TexImage)
 DEFINE_ASYNC(HostWebGLContext::CompressedTexImage)
-
 DEFINE_ASYNC(HostWebGLContext::TexParameter_base)
 DEFINE_ASYNC(HostWebGLContext::UseProgram)
-
 DEFINE_ASYNC(HostWebGLContext::UniformData)
 DEFINE_ASYNC(HostWebGLContext::VertexAttrib4T)
 DEFINE_ASYNC(HostWebGLContext::VertexAttribDivisor)
-
 DEFINE_ASYNC(HostWebGLContext::UniformBlockBinding)
 DEFINE_ASYNC(HostWebGLContext::EnableVertexAttribArray)
 DEFINE_ASYNC(HostWebGLContext::DisableVertexAttribArray)
-
 DEFINE_ASYNC(HostWebGLContext::VertexAttribPointer)
 DEFINE_ASYNC(HostWebGLContext::ClearBufferTv)
 DEFINE_ASYNC(HostWebGLContext::ClearBufferfi)
-
 DEFINE_ASYNC(HostWebGLContext::ReadPixelsPbo)
 DEFINE_ASYNC(HostWebGLContext::BindSampler)
 DEFINE_ASYNC(HostWebGLContext::SamplerParameteri)
 DEFINE_ASYNC(HostWebGLContext::SamplerParameterf)
-
-
 DEFINE_ASYNC(HostWebGLContext::BindTransformFeedback)
 DEFINE_ASYNC(HostWebGLContext::BeginTransformFeedback)
 DEFINE_ASYNC(HostWebGLContext::EndTransformFeedback)
@@ -174,13 +141,10 @@ DEFINE_ASYNC(HostWebGLContext::DrawElementsInstanced)
 DEFINE_ASYNC(HostWebGLContext::BeginQuery)
 DEFINE_ASYNC(HostWebGLContext::EndQuery)
 DEFINE_ASYNC(HostWebGLContext::QueryCounter)
-
 DEFINE_ASYNC(HostWebGLContext::SetFramebufferIsInOpaqueRAF)
 DEFINE_ASYNC(HostWebGLContext::ClearVRSwapChain)
 
-#undef DEFINE_METHOD_HELPER
 #undef DEFINE_ASYNC
-
 #undef DEFINE_METHOD_DISPATCHER
 
 }  
