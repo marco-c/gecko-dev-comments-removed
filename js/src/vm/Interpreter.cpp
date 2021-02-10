0e7b51b688cece50344d6b89e9aef9def4127c87
Marco Castelluccio <mcastelluccio@mozilla.com>
2021-02-10 00:21:34 +0000

    Bug 1689273 - Remove expiring JS and wasm probes. r=lth
    This commit removes the expiring probes:
     * wasm.compile_time_baseline_us
     * wasm.compile_time_cranelift_us
     * wasm.compile_time_ion_us
     * js.run_time_us
    
    Execution runtime is still measured in the JS interpreter for still
    active probes.
    
    Differential Revision: https://phabricator.services.mozilla.com/D104143
    
    UltraBlame original commit: 5793c988794d3a1188f2707a07bd1f06f3087d96

--- js/src/vm/Interpreter.cpp
+++ js/src/vm/Interpreter.cpp
@@ -389,9 +389,6 @@ bool js::RunScript(JSContext* cx, RunState& state) {
       mozilla::TimeDuration delta = ReallyNow() - startTime;
       cx->realm()->timers.executionTime += delta;
       cx->setIsMeasuringExecutionTime(false);
-
-      
-      
     }
   });
 
