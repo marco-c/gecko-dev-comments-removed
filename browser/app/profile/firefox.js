b2fb2ad4b62ed1922a5f6c6f60e2c96fe6ef6442
Marco Castelluccio <mcastelluccio@mozilla.com>
2021-02-16 00:37:45 +0000

    Bug 1692271 - Enable color-scheme simulation on the Inspector only in Nightly. r=jdescottes
    Differential Revision: https://phabricator.services.mozilla.com/D104876
    
    UltraBlame original commit: 6d004c6cb34b4bb1ed0d33eb9fa61bed733d3da5

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -2171,7 +2171,11 @@ pref("devtools.inspector.compatibility.enabled", true);
 pref("devtools.inspector.compatibility.enabled", false);
 #endif
 
+#if defined(NIGHTLY_BUILD)
 pref("devtools.inspector.color-scheme-simulation.enabled", true);
+#else
+pref("devtools.inspector.color-scheme-simulation.enabled", false);
+#endif
 
 pref("devtools.overflow.debugging.enabled", true);
 
