123278a59c57f5b12bfa6dd941686fb945103cb5
Marco Castelluccio <mcastelluccio@mozilla.com>
2021-02-16 00:37:32 +0000

    Bug 1608272 - Extend an ESLint rule to disallow 'this' as the second argument to ChromeUtils.import. r=Gijs
    Depends on D104684
    
    Differential Revision: https://phabricator.services.mozilla.com/D104685
    
    UltraBlame original commit: e6e4ffe379f1ceef6f93bca245fb04efcd6b2590

---
+++ tools/lint/eslint/eslint-plugin-mozilla/lib/rules/reject-chromeutils-import-params.js
@@ -0,0 +1,70 @@
+
+
+
+
+
+
+
+
+
+
+"use strict";
+
+
+
+
+
+function isIdentifier(node, id) {
+  return node && node.type === "Identifier" && node.name === id;
+}
+
+module.exports = function(context) {
+  
+  
+  
+
+  return {
+    CallExpression(node) {
+      let { callee } = node;
+      if (
+        isIdentifier(callee.object, "ChromeUtils") &&
+        isIdentifier(callee.property, "import") &&
+        node.arguments.length >= 2
+      ) {
+        if (
+          node.arguments[1].type == "Literal" &&
+          node.arguments[1].raw == "null"
+        ) {
+          context.report(
+            node,
+            "ChromeUtils.import should not be called with (..., null) to " +
+              "retrieve the JSM global object. Rely on explicit exports instead."
+          );
+        } else if (node.arguments[1].type == "ThisExpression") {
+          context.report({
+            node,
+            message:
+              "ChromeUtils.import should not be called with (..., this) to " +
+              "retrieve the JSM global object. Use destructuring instead.",
+            suggest: [
+              {
+                desc: "Use destructuring for imports.",
+                fix: fixer => {
+                  let source = context.getSourceCode().getText(node);
+                  let match = source.match(
+                    /ChromeUtils.import\(\s*(".*\/(.*).jsm?")/m
+                  );
+
+                  return fixer.replaceText(
+                    node,
+                    `const { ${match[2]} } = ChromeUtils.import(${match[1]})`
+                  );
+                },
+              },
+            ],
+          });
+        }
+      }
+    },
+  };
+};
