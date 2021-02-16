d42d2ce4f30dea6bbb51a7102f7d29d21a97db62
Marco Castelluccio <mcastelluccio@mozilla.com>
2021-02-16 00:34:28 +0000

    Backed out changeset e3a59a1dc7ca (bug 1691184) for causing bustages in bug1691184.js CLOSED TREE
    UltraBlame original commit: 2f2fe1dadc7e799e8c073d817581336ab7289bb5

--- js/src/vm/RegExpObject.cpp
+++ js/src/vm/RegExpObject.cpp
@@ -668,14 +668,6 @@ RegExpRunStatus RegExpShared::execute(JSContext* cx,
           return RegExpRunStatus_Error;
         }
         if (interruptRetries++ < maxInterruptRetries) {
-          
-          
-          
-          
-          if (!compileIfNecessary(cx, re, input,
-                                  RegExpShared::CodeKind::Jitcode)) {
-            return RegExpRunStatus_Error;
-          }
           continue;
         }
       }
