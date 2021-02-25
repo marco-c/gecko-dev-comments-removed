56415d469e9b994a19270abc50d9b41e0b1f748f
Marco Castelluccio <mcastelluccio@mozilla.com>
2021-02-25 00:08:47 +0000

    Bug 1693144 - [devtools] Fallback to the initial javascriptEnabled value from the target r=ochameau
    Differential Revision: https://phabricator.services.mozilla.com/D105463
    
    UltraBlame original commit: a147bbc7e97a57bea44299fcad81c1c687492d6d

--- devtools/shared/resources/target-list.js
+++ devtools/shared/resources/target-list.js
@@ -606,6 +606,15 @@ class TargetList extends EventEmitter {
   async isJavascriptEnabled(configuration) {
     if (this.hasTargetWatcherSupport("target-configuration")) {
       const targetConfigurationFront = await this.watcherFront.getTargetConfigurationActor();
+
+      const { javascriptEnabled } = targetConfigurationFront.configuration;
+      if (typeof javascriptEnabled === "undefined") {
+        
+        
+        
+        
+        return this.targetFront._javascriptEnabled;
+      }
       return targetConfigurationFront.configuration.javascriptEnabled;
     }
 
