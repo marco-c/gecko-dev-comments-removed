f3209b3a4d5f41cc78bd3918a81002ddc67bb5c1
Marco Castelluccio <mcastelluccio@mozilla.com>
2021-02-25 00:40:22 +0000

    Bug 1690216 - Report whether to redraw on invalidation to WR. r=mattwoodrow
    This requires us to plumb CompositorCapabilities to support the extra field.
    This is complicated by the fact that since it is a Rust struct, it has no
    default constructor that can pass through to C++ via bindings, so every
    one of our RenderCompositors was forced to manually initialize fields. To
    get around this brittle footgun, instead the structure is initialized on
    the Rust side, and RenderCompositor's are encouraged to only change fields
    that actually diverge from the defaults as passed in via pointer.
    
    Finally, we can then do what we need to do, which is just to send the
    ForceRedraw message that needs to happen based on what we know about
    CompositorCapabilities.
    
    Differential Revision: https://phabricator.services.mozilla.com/D106246
    
    UltraBlame original commit: 45b36995cfd6ead7bcb3f0f8a390be6ef257ee04

--- gfx/webrender_bindings/RenderCompositorNative.cpp
+++ gfx/webrender_bindings/RenderCompositorNative.cpp
@@ -414,15 +414,6 @@ void RenderCompositorNative::AddSurface(
   }
 }
 
-CompositorCapabilities RenderCompositorNative::GetCompositorCapabilities() {
-  CompositorCapabilities caps;
-
-  
-  caps.virtual_surface_size = 0;
-
-  return caps;
-}
-
 
 UniquePtr<RenderCompositor> RenderCompositorNativeOGL::Create(
     RefPtr<widget::CompositorWidget>&& aWidget, nsACString& aError) {
