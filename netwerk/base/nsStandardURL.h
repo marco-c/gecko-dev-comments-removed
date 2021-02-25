02b4c78bf0c546d877fa2a1e83a30afbd78a0b37
Marco Castelluccio <mcastelluccio@mozilla.com>
2021-02-25 00:06:34 +0000

    Bug 1675540 - Perform URL sanity checks when changing it r=necko-reviewers,kershaw
    Differential Revision: https://phabricator.services.mozilla.com/D104541
    
    UltraBlame original commit: ae3e88b6b1beb53bacb95716f6a033a32c3c8423

--- netwerk/base/nsStandardURL.h
+++ netwerk/base/nsStandardURL.h
@@ -258,7 +258,7 @@ class nsStandardURL : public nsIFileURL,
                      nsACString::const_iterator& aEnd);
 
   
-  static void SanityCheck(const URLSegment&, const nsCString&);
+  void SanityCheck();
 
   
   nsCString mSpec;
