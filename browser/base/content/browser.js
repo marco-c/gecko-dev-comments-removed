3a207bca31578545d18610b716a2603ad2ce40d6
Marco Castelluccio <mcastelluccio@mozilla.com>
2021-02-25 00:05:46 +0000

    Backed out changeset 1a5c5bb711b4 (bug 1688802) for causing failures on browser_aboutwelcome_skipFocus.js. CLOSED TREE
    UltraBlame original commit: 2c3b389c60a25b99b32c2f664411aa04088340d5

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -583,15 +583,6 @@ XPCOMUtils.defineLazyPreferenceGetter(
   }
 );
 
-
-
-XPCOMUtils.defineLazyGetter(this, "aboutWelcomeFeature", () => {
-  const { ExperimentFeature } = ChromeUtils.import(
-    "resource://messaging-system/experiments/ExperimentAPI.jsm"
-  );
-  return new ExperimentFeature("aboutwelcome");
-});
-
 customElements.setElementCreationCallback("translation-notification", () => {
   Services.scriptloader.loadSubScript(
     "chrome://browser/content/translation-notification.js",
@@ -2208,16 +2199,7 @@ var gBrowserInit = {
     let shouldRemoveFocusedAttribute = true;
 
     this._callWithURIToLoad(uriToLoad => {
-      
-      
-      const aboutWelcomeSkipUrlBarFocus =
-        uriToLoad == "about:welcome" &&
-        aboutWelcomeFeature.getValue()?.skipFocus;
-
-      if (
-        (isBlankPageURL(uriToLoad) && !aboutWelcomeSkipUrlBarFocus) ||
-        uriToLoad == "about:privatebrowsing"
-      ) {
+      if (isBlankPageURL(uriToLoad) || uriToLoad == "about:privatebrowsing") {
         gURLBar.select();
         shouldRemoveFocusedAttribute = false;
         return;
