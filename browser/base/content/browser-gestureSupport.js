4bee680dbd28de3f0ad1f6bfa8c2edc7b84b9002
Marco Castelluccio <mcastelluccio@mozilla.com>
2021-02-25 00:24:41 +0000

    Bug 1690849 - Only trigger back/forward swipes when the mouse is over the content area. r=dao
    Differential Revision: https://phabricator.services.mozilla.com/D105874
    
    UltraBlame original commit: 3ad6dc801e90b591817df49a33ace6328311a878

--- browser/base/content/browser-gestureSupport.js
+++ browser/base/content/browser-gestureSupport.js
@@ -27,6 +27,8 @@ var gGestureSupport = {
 
 
 
+
+
   init: function GS_init(aAddListener) {
     const gestureEvents = [
       "SwipeGestureMayStart",
@@ -44,12 +46,12 @@ var gGestureSupport = {
       "PressTapGesture",
     ];
 
-    let addRemove = aAddListener
-      ? window.addEventListener
-      : window.removeEventListener;
-
     for (let event of gestureEvents) {
-      addRemove("Moz" + event, this, true);
+      if (aAddListener) {
+        gBrowser.tabbox.addEventListener("Moz" + event, this, true);
+      } else {
+        gBrowser.tabbox.removeEventListener("Moz" + event, this, true);
+      }
     }
   },
 
