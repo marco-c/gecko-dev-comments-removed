a0216222bd2ff4d7cac0867fb9775e19ef7e6a3b
Marco Castelluccio <mcastelluccio@mozilla.com>
2021-03-01 00:34:26 +0000

    Backed out 5 changesets (bug 1694229) for geckoview failures. CLOSED TREE
    Backed out changeset 5ce24c91b0c1 (bug 1694229)
    Backed out changeset 7fef19f47442 (bug 1694229)
    Backed out changeset a70e27ec7747 (bug 1694229)
    Backed out changeset 6a5d472e1b05 (bug 1694229)
    Backed out changeset d32085239f92 (bug 1694229)
    
    UltraBlame original commit: 8708c121e21c0c6a179ac782960b2f5a92de7ba8

--- js/xpconnect/src/XPCJSContext.cpp
+++ js/xpconnect/src/XPCJSContext.cpp
@@ -661,9 +661,12 @@ bool XPCJSContext::InterruptCallback(JSContext* cx) {
     return true;
   }
 
-  
-  
-  if (runningContentJS && XRE_IsContentProcess()) {
+  int32_t limitWithoutImportantUserInput =
+      StaticPrefs::dom_max_script_run_time_without_important_user_input();
+  if (runningContentJS && XRE_IsContentProcess() && limit &&
+      limitWithoutImportantUserInput > limit &&
+      limitWithoutImportantUserInput >
+          self->mSlowScriptActualWait.ToSeconds()) {
     
     
     ContentChild* contentChild = ContentChild::GetSingleton();
