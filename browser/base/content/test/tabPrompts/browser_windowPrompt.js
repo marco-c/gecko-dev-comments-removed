d491fbbe05c6b6181740036e8aebddd044db6226
Marco Castelluccio <mcastelluccio@mozilla.com>
2021-03-01 00:38:55 +0000

    Backed out changeset 06509c3fe5f4 (bug 1694415) for causing failure at test_prompt.html. CLOSED TREE
    UltraBlame original commit: 1f79e2ccd926e5074e968a6ad91fe56b91e83a15

--- browser/base/content/test/tabPrompts/browser_windowPrompt.js
+++ browser/base/content/test/tabPrompts/browser_windowPrompt.js
@@ -1,32 +1,28 @@
-
-
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
 
 "use strict";
 
-
-
-
+/**
+ * Check that the in-window modal dialogs work correctly.
+ */
 add_task(async function test_check_window_modal_prompt_service() {
   await SpecialPowers.pushPrefEnv({
     set: [["prompts.windowPromptSubDialog", true]],
   });
   let dialogPromise = BrowserTestUtils.promiseAlertDialogOpen(null);
-  
+  // Avoid blocking the test on the (sync) alert by sticking it in a timeout:
   setTimeout(
     () => Services.prompt.alert(window, "Some title", "some message"),
     0
   );
   let dialogWin = await dialogPromise;
 
-  
+  // Check dialog content:
   is(
     dialogWin.document.getElementById("infoTitle").textContent,
     "Some title",
     "Title should be correct."
-  );
-  ok(
-    !dialogWin.document.getElementById("infoTitle").hidden,
-    "Title should be shown."
   );
   is(
     dialogWin.document.getElementById("infoBody").textContent,
@@ -34,7 +30,7 @@ add_task(async function test_check_window_modal_prompt_service() {
     "Body text should be correct."
   );
 
-  
+  // Check circumstances of opening.
   ok(
     dialogWin?.docShell?.chromeEventHandler,
     "Should have embedded the dialog."
@@ -58,7 +54,7 @@ add_task(async function test_check_window_modal_prompt_service() {
   EventUtils.sendKey("ESCAPE");
   await closedPromise;
 
-  
+  // Check we cleaned up:
   is(
     window.getComputedStyle(document.body).getPropertyValue("-moz-user-input"),
     "auto",
