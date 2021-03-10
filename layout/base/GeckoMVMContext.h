0e6514fa756b58e1369d83fe71aafd821c0da73f
Marco Castelluccio <mcastelluccio@mozilla.com>
2021-03-10 18:27:34 +0000

    Bug 1696717 - Mark GeckoMVMContext final. r=hiro
    Again, just drive-by cleanup.
    
    Depends on D107363
    
    Differential Revision: https://phabricator.services.mozilla.com/D107364
    
    UltraBlame original commit: 3e627b1236768fa8fa9fe915c7fdab32f96670ab

--- layout/base/GeckoMVMContext.h
+++ layout/base/GeckoMVMContext.h
@@ -23,7 +23,7 @@ class EventTarget;
 
 
 
-class GeckoMVMContext : public MVMContext {
+class GeckoMVMContext final : public MVMContext {
  public:
   explicit GeckoMVMContext(dom::Document* aDocument, PresShell* aPresShell);
   void AddEventListener(const nsAString& aType, nsIDOMEventListener* aListener,
