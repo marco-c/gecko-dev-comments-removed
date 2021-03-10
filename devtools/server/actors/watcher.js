48e0392d7f727fb75241e800dc4ff12f37c5d0dd
Marco Castelluccio <mcastelluccio@mozilla.com>
2021-03-10 18:21:45 +0000

    Bug 1644196 - [devtools] Server side listening for Websocket resoources r=ochameau
    Differential Revision: https://phabricator.services.mozilla.com/D103847
    
    UltraBlame original commit: 02e7946dca5ebf7c20e21ac9fa2c714509030aa4

--- devtools/server/actors/watcher.js
+++ devtools/server/actors/watcher.js
@@ -159,6 +159,7 @@ exports.WatcherActor = protocol.ActorClassWithSpec(watcherSpec, {
           [Resources.TYPES.SOURCE]: hasBrowserElement,
           [Resources.TYPES.THREAD_STATE]: hasBrowserElement,
           [Resources.TYPES.SERVER_SENT_EVENT]: hasBrowserElement,
+          [Resources.TYPES.WEBSOCKET]: hasBrowserElement,
         },
         
         
