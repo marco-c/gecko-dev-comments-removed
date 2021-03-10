d2716b62450a6b84467dc7d498b7303d13672e2c
Marco Castelluccio <mcastelluccio@mozilla.com>
2021-03-10 18:26:49 +0000

    Bug 1689413 part 2 - Remove owned BaseShapes. r=jonco
    At this point all BaseShapes are unowned (= immutable, shared) so we can remove the
    UnownedBaseShape type and the `BaseShape::unowned_` field.
    
    Simplify `NextEnvironmentShape` by reusing the previous shape's BaseShape instead
    of doing a slow lookup resulting in the same BaseShape.
    
    Differential Revision: https://phabricator.services.mozilla.com/D106972
    
    UltraBlame original commit: e9c2f256240a1f9bb09b10ac641bbd141eedb809

--- js/src/gc/Tracer.cpp
+++ js/src/gc/Tracer.cpp
@@ -97,7 +97,6 @@ void js::gc::TraceIncomingCCWs(JSTracer* trc,
 void gc::TraceCycleCollectorChildren(JS::CallbackTracer* trc, Shape* shape) {
   do {
     MOZ_ASSERT(shape->base());
-    shape->base()->assertConsistency();
 
     
 
