699a821697c9263562a3abb3fc2cc2bbf113949b
Marco Castelluccio <mcastelluccio@mozilla.com>
2021-03-10 18:36:05 +0000

    Bug 1696023: Report accessible's position even when bounds rect is empty r=Jamie
    Differential Revision: https://phabricator.services.mozilla.com/D107001
    
    UltraBlame original commit: b317b788a83b141c9252d7593653947509a4cdf9

--- accessible/windows/msaa/GeckoCustom.cpp
+++ accessible/windows/msaa/GeckoCustom.cpp
@@ -24,11 +24,13 @@ GeckoCustom::get_boundsInCSSPixels(int32_t* aX, int32_t* aY, int32_t* aWidth,
                                    int32_t* aHeight) {
   nsIntRect bounds = mAcc->BoundsInCSSPixels();
   if (!bounds.IsEmpty()) {
-    *aX = bounds.X();
-    *aY = bounds.Y();
     *aWidth = bounds.Width();
     *aHeight = bounds.Height();
   }
+  
+  
+  *aX = bounds.X();
+  *aY = bounds.Y();
 
   return S_OK;
 }
