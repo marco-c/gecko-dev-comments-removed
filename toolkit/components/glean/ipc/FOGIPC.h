c304309269a01c24dd9aa23e263672cde9d6bae3
Marco Castelluccio <mcastelluccio@mozilla.com>
2021-03-10 18:30:42 +0000

    Bug 1694739 - FOG IPC flush after 5s of idle r=janerik
    Interestingly, on my machine, it appears to be after every 5s of idle,
    not just after the first 5s of idle. We may wish to tweak that.
    
    We also instrument the size and speed of IPC as seen from the POV of the parent
    process, for monitoring purposes.
    
    Differential Revision: https://phabricator.services.mozilla.com/D106947
    
    UltraBlame original commit: 123143e183f97172102f3064a8bb779689fd31c1

--- toolkit/components/glean/ipc/FOGIPC.h
+++ toolkit/components/glean/ipc/FOGIPC.h
@@ -9,6 +9,7 @@
 #include <functional>
 
 #include "mozilla/Maybe.h"
+#include "mozilla/MozPromise.h"
 #include "nsTArrayForwardDeclare.h"
 
 namespace mozilla {
@@ -36,7 +37,7 @@ void FlushFOGData(std::function<void(ipc::ByteBuf&&)>&& aResolver);
 
 
 void FlushAllChildData(
-    std::function<void(const nsTArray<ipc::ByteBuf>&&)>&& aResolver);
+    std::function<void(nsTArray<ipc::ByteBuf>&&)>&& aResolver);
 
 
 
@@ -49,6 +50,14 @@ void FOGData(ipc::ByteBuf&& buf);
 
 
 void SendFOGData(ipc::ByteBuf&& buf);
+
+
+
+
+
+
+
+RefPtr<GenericPromise> FlushAndUseFOGData();
 
 }  
 }  
