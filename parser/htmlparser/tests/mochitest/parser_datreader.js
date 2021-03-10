c16812c9d13b99720018a36aba9533d660aa5eb7
Marco Castelluccio <mcastelluccio@mozilla.com>
2021-03-10 18:21:44 +0000

    Bug 1360715 - Part 3: Remove remaining cross-context instanceof from tests r=edgar
    Differential Revision: https://phabricator.services.mozilla.com/D106663
    
    UltraBlame original commit: 39aee1028caf0654342d86096d146ae45865f79c

--- parser/htmlparser/tests/mochitest/parser_datreader.js
+++ parser/htmlparser/tests/mochitest/parser_datreader.js
@@ -1,18 +1,18 @@
-/* -*- indent-tabs-mode: nil; js-indent-level: 2 -*-
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-/**
- * A test suite that runs WHATWG HTML parser tests.
- * The tests are from html5lib.
- *
- * http://html5lib.googlecode.com/
- */
 
-/**
- * A few utility functions.
- */
+
+
+
+
+
+
+
+
+
+
+
+
+
 function log(entry) {}
 
 function startsWith(s, s2) {
@@ -23,17 +23,17 @@ function trimString(s) {
   return s.replace(/^\s+/, "").replace(/\s+$/, "");
 }
 
-/**
- * Parses an individual testcase into an array containing the input
- * string, a string representing the expected tree (DOM), and a list
- * of error messages.
- *
- * @param A string containing a single testcase
- */
+
+
+
+
+
+
+
 function parseTestcase(testcase) {
   var lines = testcase.split("\n");
 
-  /* check that the first non-empty, non-comment line is #data */
+  
   for (let line of lines) {
     if (!line || startsWith(line, "##")) {
       continue;
@@ -73,19 +73,19 @@ function parseTestcase(testcase) {
     }
   }
   while (!output[output.length - 1]) {
-    output.pop(); // zap trailing blank lines
+    output.pop(); 
   }
-  // logger.log(input.length, output.length, errors.length);
+  
   return [input.join("\n"), output.join("\n"), errors, fragment[0]];
 }
 
-/**
- * A generator function that accepts a list of strings. Each list
- * member corresponds to the contents of a ".dat" file from the
- * html5lib test suite.
- *
- * @param The list of strings
- */
+
+
+
+
+
+
+
 function* test_parser(testlist) {
   for (var testgroup of testlist) {
     var tests = testgroup.split("#data\n");
@@ -96,22 +96,22 @@ function* test_parser(testlist) {
   }
 }
 
-/**
- * Transforms a DOM document to a string matching the format in
- * the test cases.
- *
- * @param the DOM document
- */
+
+
+
+
+
+
 function docToTestOutput(doc) {
   var walker = doc.createTreeWalker(doc, NodeFilter.SHOW_ALL, null);
-  return addLevels(walker, "", "| ").slice(0, -1); // remove the last newline
+  return addLevels(walker, "", "| ").slice(0, -1); 
 }
 
-/**
- * Creates a walker for a fragment that skips over the root node.
- *
- * @param an element
- */
+
+
+
+
+
 function createFragmentWalker(elt) {
   return elt.ownerDocument.createTreeWalker(elt, NodeFilter.SHOW_ALL, function(
     node
@@ -120,15 +120,15 @@ function createFragmentWalker(elt) {
   });
 }
 
-/**
- * Transforms the descendants of an element to a string matching the format
- * in the test cases.
- *
- * @param an element
- */
+
+
+
+
+
+
 function fragmentToTestOutput(elt) {
   var walker = createFragmentWalker(elt);
-  return addLevels(walker, "", "| ").slice(0, -1); // remove the last newline
+  return addLevels(walker, "", "| ").slice(0, -1); 
 }
 
 function addLevels(walker, buf, indent) {
@@ -199,12 +199,12 @@ function addLevels(walker, buf, indent) {
           break;
       }
       buf += "\n";
-      // In the case of template elements, children do not get inserted as
-      // children of the template element, instead they are inserted
-      // as children of the template content (which is a document fragment).
-      if (walker.currentNode instanceof HTMLTemplateElement) {
+      
+      
+      
+      if (walker.currentNode.constructor.name === "HTMLTemplateElement") {
         buf += indent + "  content\n";
-        // Walk through the template content.
+        
         var templateWalker = createFragmentWalker(walker.currentNode.content);
         buf = addLevels(templateWalker, buf, indent + "    ");
       }
