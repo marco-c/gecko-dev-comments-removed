c2baa4f4204021d01abdd4ce892fba919bf3c7ad
Marco Castelluccio <mcastelluccio@mozilla.com>
2021-03-10 18:09:55 +0000

    Bug 1695549 - Run cargo fmt on qcms. r=aosmond
    Differential Revision: https://phabricator.services.mozilla.com/D106739
    
    UltraBlame original commit: 7245ce657a6427159c1285d17fcfb84715dc1b1d

--- gfx/qcms/src/transform.rs
+++ gfx/qcms/src/transform.rs
@@ -1337,7 +1337,7 @@ pub fn transform_create(
     debug_assert!(transform.transform_fn.is_some());
     Some(transform)
 }
- 
+
 pub struct Transform {
     ty: DataType,
     xfm: Box<qcms_transform>,
@@ -1345,12 +1345,7 @@ pub struct Transform {
 
 impl Transform {
     
-    pub fn new(
-        input: &Profile,
-        output: &Profile,
-        ty: DataType,
-        intent: Intent,
-    ) -> Option<Self> {
+    pub fn new(input: &Profile, output: &Profile, ty: DataType, intent: Intent) -> Option<Self> {
         transform_create(input, ty, output, ty, intent).map(|xfm| Transform { ty, xfm })
     }
 
