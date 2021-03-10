0b2147a0fefe86f17b3d14e7581a9977f79f1af8
Marco Castelluccio <mcastelluccio@mozilla.com>
2021-03-10 18:12:02 +0000

    Bug 1695573 - Add missing #include for MakeScopeExit. r=glandium DONTBUILD
    Differential Revision: https://phabricator.services.mozilla.com/D106753
    
    UltraBlame original commit: ec6bd7cf0ea48d684c6cc9c077658f058d2a8da5

--- layout/xul/nsXULTooltipListener.cpp
+++ layout/xul/nsXULTooltipListener.cpp
@@ -28,6 +28,7 @@
 #include "mozilla/LookAndFeel.h"
 #include "mozilla/Preferences.h"
 #include "mozilla/PresShell.h"
+#include "mozilla/ScopeExit.h"
 #include "mozilla/dom/Element.h"
 #include "mozilla/dom/Event.h"  
 #include "mozilla/dom/MouseEvent.h"
