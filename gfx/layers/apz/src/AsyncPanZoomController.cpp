be98c93695be2037e2e89dd15afa82d62989d379
Marco Castelluccio <mcastelluccio@mozilla.com>
2021-03-10 18:18:25 +0000

    Bug 1684520. Do not consider the scroll offset to be updated when we recieve a smooth scroll request. r=botond
    A smooth scroll request is a request to perform scrolling in the future, it does not indicate that the scroll offset has been updated. However we still need to run a few of the things from a scroll offset update.
    
    This seems kind of fragile. We could have more bugs of this type, I didn't look for them or audit the code, but we should.
    
    Differential Revision: https://phabricator.services.mozilla.com/D105280
    
    UltraBlame original commit: dda5061e661e2e50bfd31916cef6ae97cd037278

--- gfx/layers/apz/src/AsyncPanZoomController.cpp
+++ gfx/layers/apz/src/AsyncPanZoomController.cpp
@@ -4885,6 +4885,7 @@ void AsyncPanZoomController::NotifyLayersUpdated(
   }
 
   bool scrollOffsetUpdated = false;
+  bool smoothScrollRequested = false;
   for (const auto& scrollUpdate : aScrollMetadata.GetScrollUpdates()) {
     APZC_LOG("%p processing scroll update %s\n", this,
              ToString(scrollUpdate).c_str());
@@ -4908,7 +4909,7 @@ void AsyncPanZoomController::NotifyLayersUpdated(
 
     if (scrollUpdate.GetMode() == ScrollMode::Smooth ||
         scrollUpdate.GetMode() == ScrollMode::SmoothMsd) {
-      scrollOffsetUpdated = true;
+      smoothScrollRequested = true;
 
       
       
@@ -5077,6 +5078,12 @@ void AsyncPanZoomController::NotifyLayersUpdated(
     for (auto& sampledState : mSampledState) {
       sampledState.ClampVisualScrollOffset(Metrics());
     }
+  }
+
+  if (smoothScrollRequested && !scrollOffsetUpdated) {
+    mExpectedGeckoMetrics.UpdateFrom(aLayerMetrics);
+    
+    needContentRepaint = true;
   }
 
   
