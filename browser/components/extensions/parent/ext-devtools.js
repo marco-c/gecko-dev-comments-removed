d8c02049fe9b9bbb69edb2390db7bc42bbbcac4d
Marco Castelluccio <mcastelluccio@mozilla.com>
2021-03-10 18:35:59 +0000

    Bug 1694906 - [devtools] Wait for toolbox-ready instead of toolbox-created in ext-devtools.js r=rpl
    Depends on D107052
    
    toolbox-ready guarantees that the toolbox will have a valid target attached.
    
    Differential Revision: https://phabricator.services.mozilla.com/D107099
    
    UltraBlame original commit: c23343373584040b311c023977e4bfa58ea5f899

--- browser/components/extensions/parent/ext-devtools.js
+++ browser/components/extensions/parent/ext-devtools.js
@@ -321,7 +321,7 @@ this.devtools = class extends ExtensionAPI {
     
     this.pageDefinition = null;
 
-    this.onToolboxCreated = this.onToolboxCreated.bind(this);
+    this.onToolboxReady = this.onToolboxReady.bind(this);
     this.onToolboxDestroy = this.onToolboxDestroy.bind(this);
 
     
@@ -380,7 +380,7 @@ this.devtools = class extends ExtensionAPI {
       this.pageDefinition.build();
     }
 
-    DevToolsShim.on("toolbox-created", this.onToolboxCreated);
+    DevToolsShim.on("toolbox-ready", this.onToolboxReady);
     DevToolsShim.on("toolbox-destroy", this.onToolboxDestroy);
     this._initialized = true;
   }
@@ -393,7 +393,7 @@ this.devtools = class extends ExtensionAPI {
       return;
     }
 
-    DevToolsShim.off("toolbox-created", this.onToolboxCreated);
+    DevToolsShim.off("toolbox-ready", this.onToolboxReady);
     DevToolsShim.off("toolbox-destroy", this.onToolboxDestroy);
 
     
@@ -419,7 +419,7 @@ this.devtools = class extends ExtensionAPI {
     };
   }
 
-  onToolboxCreated(toolbox) {
+  onToolboxReady(toolbox) {
     if (
       !toolbox.descriptorFront.isLocalTab ||
       !this.extension.canAccessWindow(
