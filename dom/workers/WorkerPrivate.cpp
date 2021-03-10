fb10aa9100da7753c2aefe4d513e8eca8d30a7a1
Marco Castelluccio <mcastelluccio@mozilla.com>
2021-03-10 18:12:27 +0000

    Bug 1693967 - Avoid creating workers with the shutting down RuntimeService r=dom-worker-reviewers,asuth
    Differential Revision: https://phabricator.services.mozilla.com/D106851
    
    UltraBlame original commit: fcfc517be2b0dc15a979bf147a2fbb7f41c4dc62

--- dom/workers/WorkerPrivate.cpp
+++ dom/workers/WorkerPrivate.cpp
@@ -2377,6 +2377,12 @@ already_AddRefed<WorkerPrivate> WorkerPrivate::Constructor(
 
   MOZ_ASSERT(runtimeService);
 
+  
+  if (runtimeService->IsShuttingDown()) {
+    aRv.Throw(NS_ERROR_UNEXPECTED);
+    return nullptr;
+  }
+
   nsILoadInfo::CrossOriginOpenerPolicy agentClusterCoop =
       nsILoadInfo::OPENER_POLICY_UNSAFE_NONE;
   nsID agentClusterId;
