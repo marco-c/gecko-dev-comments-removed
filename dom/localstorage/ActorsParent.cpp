bf4137b97b1734dce10f4ae5bd2dbc2f4877f8de
Marco Castelluccio <mcastelluccio@mozilla.com>
2021-03-10 18:20:06 +0000

    Bug 1686031 - Add mPersistenceType to OriginMetadata; r=dom-storage-reviewers,sg
    The existing members of OriginMetadata have been extracted to a parent struct
    called PrincipalMetadata. Methods like GetOriginUsage,
    GetInfoFromValidatedPrincipalInfo, GetInfoFromPrincipal and GetInfoForChrome
    have been changed to take/return PrincipalMetadata instead of OriginMetadata.
    Having the persistence type doesn't make sense in those methods because the
    origin is not tied to a specific persistence type in context of the methods.
    
    Some places temporarily pass PERSISTENCE_TYPE_INVALID and will be fixed in
    following patches.
    
    Differential Revision: https://phabricator.services.mozilla.com/D106400
    
    UltraBlame original commit: bc41e3cc67544f8c9803907d0104a831cb5d555f

--- dom/localstorage/ActorsParent.cpp
+++ dom/localstorage/ActorsParent.cpp
@@ -6476,17 +6476,22 @@ nsresult PrepareDatastoreOp::Start() {
       commonParams.storagePrincipalInfo();
 
   if (storagePrincipalInfo.type() == PrincipalInfo::TSystemPrincipalInfo) {
-    mOriginMetadata = QuotaManager::GetInfoForChrome();
+    mOriginMetadata = {QuotaManager::GetInfoForChrome(),
+                       PERSISTENCE_TYPE_DEFAULT};
   } else {
     MOZ_ASSERT(storagePrincipalInfo.type() ==
                PrincipalInfo::TContentPrincipalInfo);
 
-    OriginMetadata originMetadata =
+    PrincipalMetadata principalMetadata =
         QuotaManager::GetInfoFromValidatedPrincipalInfo(storagePrincipalInfo);
 
-    mOriginMetadata.mSuffix = std::move(originMetadata.mSuffix);
-    mOriginMetadata.mGroup = std::move(originMetadata.mGroup);
-    mMainThreadOrigin = std::move(originMetadata.mOrigin);
+    mOriginMetadata.mSuffix = std::move(principalMetadata.mSuffix);
+    mOriginMetadata.mGroup = std::move(principalMetadata.mGroup);
+    
+    
+    
+    mMainThreadOrigin = std::move(principalMetadata.mOrigin);
+    mOriginMetadata.mPersistenceType = PERSISTENCE_TYPE_DEFAULT;
   }
 
   mState = State::Nesting;
