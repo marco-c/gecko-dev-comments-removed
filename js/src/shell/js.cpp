45e8c0e958c19c2a2f74975844cbe5dccaf58db2
Marco Castelluccio <mcastelluccio@mozilla.com>
2021-03-10 18:20:20 +0000

    Bug 1694916 - Part 2: Check the consistency of discardSource/instrumentationKinds in evaluate JS shell function. r=mgaudet
    asmJSOption can also be different between given CompileOptions and given global,
    but asmJSOption doesn't affect after compilation, so it's not checked.
    
    Depends on D107006
    
    Differential Revision: https://phabricator.services.mozilla.com/D107007
    
    UltraBlame original commit: 3fe9243aefea15c9341b01c77a65c2fa212a0137

--- js/src/shell/js.cpp
+++ js/src/shell/js.cpp
@@ -2333,25 +2333,6 @@ static bool Evaluate(JSContext* cx, unsigned argc, Value* vp) {
       }
     }
 
-    if (!JS_GetProperty(cx, opts, "global", &v)) {
-      return false;
-    }
-    if (!v.isUndefined()) {
-      if (v.isObject()) {
-        global = js::CheckedUnwrapDynamic(&v.toObject(), cx,
-                                           false);
-        if (!global) {
-          return false;
-        }
-      }
-      if (!global || !(JS::GetClass(global)->flags & JSCLASS_IS_GLOBAL)) {
-        JS_ReportErrorNumberASCII(
-            cx, GetErrorMessage, nullptr, JSMSG_UNEXPECTED_TYPE,
-            "\"global\" passed to evaluate()", "not a global object");
-        return false;
-      }
-    }
-
     if (!JS_GetProperty(cx, opts, "catchTermination", &v)) {
       return false;
     }
@@ -2606,6 +2587,46 @@ static bool Evaluate(JSContext* cx, unsigned argc, Value* vp) {
         saveCacheKind = BytecodeCacheKind::Stencil;
       } else {
         saveCacheKind = BytecodeCacheKind::Script;
+      }
+    }
+
+    
+    
+    if (!JS_GetProperty(cx, opts, "global", &v)) {
+      return false;
+    }
+    if (!v.isUndefined()) {
+      if (v.isObject()) {
+        global = js::CheckedUnwrapDynamic(&v.toObject(), cx,
+                                           false);
+        if (!global) {
+          return false;
+        }
+      }
+      if (!global || !(JS::GetClass(global)->flags & JSCLASS_IS_GLOBAL)) {
+        JS_ReportErrorNumberASCII(
+            cx, GetErrorMessage, nullptr, JSMSG_UNEXPECTED_TYPE,
+            "\"global\" passed to evaluate()", "not a global object");
+        return false;
+      }
+
+      
+      
+      {
+        JSAutoRealm ar(cx, global);
+        JS::CompileOptions globalOptions(cx);
+
+        
+        
+        if (globalOptions.discardSource && !options.discardSource) {
+          JS_ReportErrorASCII(cx, "discardSource option mismatch");
+          return false;
+        }
+        if (globalOptions.instrumentationKinds !=
+            options.instrumentationKinds) {
+          JS_ReportErrorASCII(cx, "instrumentationKinds mismatch");
+          return false;
+        }
       }
     }
   }
