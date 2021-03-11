2a6282319691801a647251a841a8b0dc643915d9
Marco Castelluccio <mcastelluccio@mozilla.com>
2021-03-11 20:54:10 +0000

    Bug 1696027 - Remove use of nsISearchEngine.addEngineWithDetails from mochitests. r=daleharvey
    Differential Revision: https://phabricator.services.mozilla.com/D107624
    
    UltraBlame original commit: 35bc754b70cf3c3d57f74ad9c49039ed87a14a5c

--- browser/components/urlbar/tests/browser/browser_autoFill_placeholder.js
+++ browser/components/urlbar/tests/browser/browser_autoFill_placeholder.js
@@ -44,14 +44,7 @@ add_task(async function origin() {
 add_task(async function tokenAlias() {
   
   
-  await Services.search.addEngineWithDetails("Test", {
-    alias: "@__example",
-    template: "http://example.com/?search={searchTerms}",
-  });
-  registerCleanupFunction(async function() {
-    let engine = Services.search.getEngineByName("Test");
-    await Services.search.removeEngine(engine);
-  });
+  await SearchTestUtils.installSearchExtension({ keyword: "@__example" });
 
   
   
@@ -177,18 +170,14 @@ add_task(async function noMatch2() {
 
 add_task(async function clear_placeholder_for_keyword_or_alias() {
   info("Clear the autofill placeholder if a keyword is typed");
-  await PlacesTestUtils.addVisits("http://example.com/");
+  await PlacesTestUtils.addVisits("https://example.com/");
   await PlacesUtils.keywords.insert({
     keyword: "ex",
-    url: "http://somekeyword.com/",
+    url: "https://somekeyword.com/",
   });
-  let engine = await Services.search.addEngineWithDetails("AutofillTest", {
-    alias: "exam",
-    template: "http://example.com/?search={searchTerms}",
-  });
+  await SearchTestUtils.installSearchExtension({ keyword: "exam" });
   registerCleanupFunction(async function() {
     await PlacesUtils.keywords.remove("ex");
-    await Services.search.removeEngine(engine);
   });
 
   
--- browser/components/urlbar/tests/browser/browser_autoFill_typed.js
+++ browser/components/urlbar/tests/browser/browser_autoFill_typed.js
@@ -99,14 +99,7 @@ add_task(async function url() {
 add_task(async function tokenAlias() {
   
   
-  await Services.search.addEngineWithDetails("Test", {
-    alias: "@__example",
-    template: "http://example.com/?search={searchTerms}",
-  });
-  registerCleanupFunction(async function() {
-    let engine = Services.search.getEngineByName("Test");
-    await Services.search.removeEngine(engine);
-  });
+  await SearchTestUtils.installSearchExtension({ keyword: "@__example" });
   
   await typeAndCheck([
     ["@", "@"],
