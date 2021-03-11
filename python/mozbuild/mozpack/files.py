6c64b0e07feb795f92a5452a5791be66d720d8eb
Marco Castelluccio <mcastelluccio@mozilla.com>
2021-03-11 20:52:50 +0000

    Bug 1694675 - Add _tests/modules to symlink denylist r=firefox-build-system-reviewers,glandium
    Differential Revision: https://phabricator.services.mozilla.com/D106288
    
    UltraBlame original commit: 32386e0d2f82ff3cb1228d3889a74f763a3da241

--- python/mozbuild/mozpack/files.py
+++ python/mozbuild/mozpack/files.py
@@ -367,6 +367,22 @@ class AbsoluteSymlinkFile(File):
 
         File.__init__(self, path)
 
+    @staticmethod
+    def excluded(dest):
+        if platform.system() != "Windows":
+            return False
+
+        
+        
+        from buildconfig import topobjdir
+
+        denylist = [("dist", "bin"), ("_tests", "modules")]
+        fulllist = [os.path.join(topobjdir, *paths) for paths in denylist]
+
+        fulldest = os.path.join(os.path.abspath(os.curdir), dest)
+
+        return mozpath.basedir(fulldest, fulllist) is not None
+
     def copy(self, dest, skip_if_older=True):
         assert isinstance(dest, six.string_types)
 
@@ -377,14 +393,7 @@ class AbsoluteSymlinkFile(File):
 
         
         
-        
-        
-        
-        distbin_on_windows = platform.system() == "Windows" and dest.startswith(
-            "dist\\bin\\"
-        )
-
-        if not hasattr(os, "symlink") or distbin_on_windows:
+        if not hasattr(os, "symlink") or AbsoluteSymlinkFile.excluded(dest):
             return File.copy(self, dest, skip_if_older=skip_if_older)
 
         
