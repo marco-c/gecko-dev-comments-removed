62b2a8ba5007f2742c2a63671d2f808926a06f44
Marco Castelluccio <mcastelluccio@mozilla.com>
2021-03-11 20:54:15 +0000

    Backed out 3 changesets (bug 1696176, bug 1342741) for test_nsIHTMLEditor_getSelectedElement.html and inert-retargeting-iframe.tentative.html failures CLOSED TREE
    Backed out changeset 1154df798888 (bug 1342741)
    Backed out changeset 02b69c938fbe (bug 1342741)
    Backed out changeset a7ceef946175 (bug 1696176)
    
    UltraBlame original commit: d1e50e1cfdd8f73c5f5d6516e96dd1963d159061

--- layout/generic/nsIFrame.cpp
+++ layout/generic/nsIFrame.cpp
@@ -4740,9 +4740,7 @@ nsresult nsIFrame::SelectByTypeAtPoint(nsPresContext* aPresContext,
   }
 
   ContentOffsets offsets = GetContentOffsetsFromPoint(aPoint, SKIP_HIDDEN);
-  if (!offsets.content) {
-    return NS_ERROR_FAILURE;
-  }
+  if (!offsets.content) return NS_ERROR_FAILURE;
 
   int32_t offset;
   nsIFrame* frame = nsFrameSelection::GetFrameForNodeOffset(
@@ -4831,19 +4829,14 @@ nsresult nsIFrame::PeekBackwardAndForward(nsSelectionAmount aAmountBack,
                               true,  
                               false, false, false);
   rv = baseFrame->PeekOffset(&startpos);
-  if (NS_FAILED(rv)) {
-    return rv;
-  }
+  if (NS_FAILED(rv)) return rv;
 
-  
-  nsPeekOffsetStruct endpos(aAmountForward, eDirNext, startpos.mContentOffset,
-                            nsPoint(0, 0), aJumpLines,
+  nsPeekOffsetStruct endpos(aAmountForward, eDirNext, aStartPos, nsPoint(0, 0),
+                            aJumpLines,
                             true,  
                             false, false, false);
   rv = PeekOffset(&endpos);
-  if (NS_FAILED(rv)) {
-    return rv;
-  }
+  if (NS_FAILED(rv)) return rv;
 
   
   RefPtr<nsFrameSelection> frameSelection = GetFrameSelection();
@@ -4856,17 +4849,13 @@ nsresult nsIFrame::PeekBackwardAndForward(nsSelectionAmount aAmountBack,
       MOZ_KnownLive(startpos.mResultContent) ,
       startpos.mContentOffset, startpos.mContentOffset, focusMode,
       CARET_ASSOCIATE_AFTER);
-  if (NS_FAILED(rv)) {
-    return rv;
-  }
+  if (NS_FAILED(rv)) return rv;
 
   rv = frameSelection->HandleClick(
       MOZ_KnownLive(endpos.mResultContent) ,
       endpos.mContentOffset, endpos.mContentOffset,
       nsFrameSelection::FocusMode::kExtendSelection, CARET_ASSOCIATE_BEFORE);
-  if (NS_FAILED(rv)) {
-    return rv;
-  }
+  if (NS_FAILED(rv)) return rv;
 
   
   return frameSelection->MaintainSelection(aAmountBack);
