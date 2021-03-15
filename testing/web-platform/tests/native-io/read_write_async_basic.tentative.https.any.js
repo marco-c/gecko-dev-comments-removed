d7fd19b14f36c16913139c311ef582c09f0d368a
Marco Castelluccio <mcastelluccio@mozilla.com>
2021-03-15 23:15:45 +0000

    Bug 1691054 [wpt PR 27507] - NativeIO: Add quota to async Blink interface, a=testonly
    Automatic update from web-platform-tests
    NativeIO: Add quota to async Blink interface
    
    This CL adds a quota management system to NativeIO’s Blink
    implementation. Before writing to a file (write, setLength), the
    application must explicitly ask for capacity. When deleting or shrinking
    files, the freed-up space is given back to the application. Capacity is
    allocated per Javascript execution context.
    
    This CL implements the support for the asynchronous methods of NativeIO.
    
    Blink requests capacity from the browser process. Implementing the
    browser side will be done in a followup CL. For now, the browser grants
    any request up to 8 GiB.
    
    The design doc for NativeIO's quota system is here:
    https://docs.google.com/document/d/1wUrtCOsyH3qGwKuqLhV9AJD-bDAjAzfPL5r1GT8H4IY
    
    Bug: 1137788
    Change-Id: I9f528373b1e7f5bf45fa8070964cdc3f225d1657
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2675973
    Commit-Queue: Richard Stotz <rstzchromium.org>
    Reviewed-by: Mike West <mkwstchromium.org>
    Reviewed-by: Victor Costan <pwnallchromium.org>
    Cr-Commit-Position: refs/heads/master{#857674}
    
    --
    
    wpt-commits: 16e619733e016325afebd30d6cbb97f2caf01d5f
    wpt-pr: 27507
    
    UltraBlame original commit: 2fcf5a846a69018b726ef16cc7f96bd6f881428a

--- testing/web-platform/tests/native-io/read_write_async_basic.tentative.https.any.js
+++ testing/web-platform/tests/native-io/read_write_async_basic.tentative.https.any.js
@@ -1,9 +1,12 @@
 
 
 
+
 'use strict';
 
 promise_test(async testCase => {
+  await reserveAndCleanupCapacity(testCase);
+
   const file = await storageFoundation.open('test_file');
   testCase.add_cleanup(async () => {
     await file.close();
