ac753207b3152c6a4e62f0b0a57640b336dbdc5c
Marco Castelluccio <mcastelluccio@mozilla.com>
2021-03-15 18:02:46 +0000

    Bug 1697504 - Part 13: Use NativeObject* for IsCacheableProtoChain. r=jandem
    `IsCacheableProtoChain()` is only called with `NativeObject*`. Also turn all
    `IsCacheableProtoChain()` checks into assertions to match the current code.
    
    The comment about `lookupProperty` potentially modifying the prototype chain
    dates back to bug 729099. At that point of time, the code was calling
    `JSObject::lookupProperty()` (today's `js::LookupProperty()`), which could
    indeed modify the prototype chain. But today CacheIR is always calling
    `js::LookupPropertyPure()` (or similar functions), which can't alter any
    prototypes.
    
    Differential Revision: https://phabricator.services.mozilla.com/D107851
    
    UltraBlame original commit: 3a2ced62cd9fbf8d20ff07f010adfbe2c150543f

--- js/src/jit/CacheIR.cpp
+++ js/src/jit/CacheIR.cpp
@@ -441,33 +441,29 @@ AttachDecision GetPropIRGenerator::tryAttachStub() {
   return AttachDecision::NoAction;
 }
 
-static bool IsCacheableProtoChain(JSObject* obj, JSObject* holder) {
+#ifdef DEBUG
+
+
+
+
+static bool IsCacheableProtoChain(NativeObject* obj, NativeObject* holder) {
   while (obj != holder) {
-    
-
-
-
-
     JSObject* proto = obj->staticPrototype();
     if (!proto || !proto->is<NativeObject>()) {
       return false;
     }
-    obj = proto;
+    obj = &proto->as<NativeObject>();
   }
   return true;
 }
+#endif
 
 static bool IsCacheableGetPropReadSlot(NativeObject* obj, NativeObject* holder,
                                        Shape* shape) {
-  if (!shape->isDataProperty()) {
-    return false;
-  }
+  MOZ_ASSERT(shape);
+  MOZ_ASSERT(IsCacheableProtoChain(obj, holder));
 
-  if (!IsCacheableProtoChain(obj, holder)) {
-    return false;
-  }
-
-  return true;
+  return shape->isDataProperty();
 }
 
 enum NativeGetPropCacheability {
@@ -481,10 +477,7 @@ static NativeGetPropCacheability IsCacheableGetPropCall(NativeObject* obj,
                                                         NativeObject* holder,
                                                         Shape* shape) {
   MOZ_ASSERT(shape);
-
-  if (!IsCacheableProtoChain(obj, holder)) {
-    return CanAttachNone;
-  }
+  MOZ_ASSERT(IsCacheableProtoChain(obj, holder));
 
   if (!shape->hasGetterValue() || !shape->getterValue().isObject()) {
     return CanAttachNone;
@@ -3221,11 +3214,9 @@ AttachDecision HasPropIRGenerator::tryAttachNative(NativeObject* obj,
                                                    ValOperandId keyId,
                                                    PropertyResult prop,
                                                    NativeObject* holder) {
-  if (!prop.isNativeProperty()) {
-    return AttachDecision::NoAction;
-  }
+  MOZ_ASSERT(IsCacheableProtoChain(obj, holder));
 
-  if (!IsCacheableProtoChain(obj, holder)) {
+  if (!prop.isNativeProperty()) {
     return AttachDecision::NoAction;
   }
 
@@ -3711,10 +3702,7 @@ void SetPropIRGenerator::trackAttached(const char* name) {
 static bool IsCacheableSetPropCallNative(NativeObject* obj,
                                          NativeObject* holder, Shape* shape) {
   MOZ_ASSERT(shape);
-
-  if (!IsCacheableProtoChain(obj, holder)) {
-    return false;
-  }
+  MOZ_ASSERT(IsCacheableProtoChain(obj, holder));
 
   if (!shape->hasSetterValue()) {
     return false;
@@ -3743,10 +3731,7 @@ static bool IsCacheableSetPropCallNative(NativeObject* obj,
 static bool IsCacheableSetPropCallScripted(NativeObject* obj,
                                            NativeObject* holder, Shape* shape) {
   MOZ_ASSERT(shape);
-
-  if (!IsCacheableProtoChain(obj, holder)) {
-    return false;
-  }
+  MOZ_ASSERT(IsCacheableProtoChain(obj, holder));
 
   if (IsWindow(obj)) {
     return false;
@@ -4649,10 +4634,7 @@ AttachDecision InstanceOfIRGenerator::tryAttachStub() {
   MOZ_ASSERT(!hasInstanceProp.shape()->configurable());
   MOZ_ASSERT(!hasInstanceProp.shape()->writable());
 
-  if (!IsCacheableProtoChain(fun, hasInstanceHolder)) {
-    trackAttached(IRGenerator::NotAttached);
-    return AttachDecision::NoAction;
-  }
+  MOZ_ASSERT(IsCacheableProtoChain(fun, hasInstanceHolder));
 
   
   Shape* shape = fun->lookupPure(cx_->names().prototype);
