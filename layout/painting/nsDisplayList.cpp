ef9a8b986de451e74a9bc50312a7442059550fb5
Marco Castelluccio <mcastelluccio@mozilla.com>
2021-03-15 18:02:29 +0000

    Backed out changeset b63a8619b481 (bug 1685888) for causing crashtest assertion failures in 1507961-1.html
    CLOSED TREE
    
    UltraBlame original commit: c2e2ebf84fd090d5453a62a0beca79b9f13669ef

--- layout/painting/nsDisplayList.cpp
+++ layout/painting/nsDisplayList.cpp
@@ -5404,6 +5404,33 @@ nsDisplayWrapList::nsDisplayWrapList(
   mListPtr = &mList;
   mListPtr->AppendToTop(aList);
   nsDisplayWrapList::UpdateBounds(aBuilder);
+
+  if (!aFrame || !aFrame->IsTransformed()) {
+    return;
+  }
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  nsDisplayItem* i = mListPtr->GetBottom();
+  if (i &&
+      (!i->GetAbove() || i->GetType() == DisplayItemType::TYPE_TRANSFORM) &&
+      i->Frame() == mFrame) {
+    mReferenceFrame = i->ReferenceFrame();
+    mToReferenceFrame = i->ToReferenceFrame();
+  }
+
+  nsRect visible = aBuilder->GetVisibleRect() +
+                   aBuilder->GetCurrentFrameOffsetToReferenceFrame();
+
+  SetBuildingRect(visible);
 }
 
 nsDisplayWrapList::nsDisplayWrapList(nsDisplayListBuilder* aBuilder,
@@ -7319,20 +7346,9 @@ void nsDisplayTransform::SetReferenceFrameToAncestor(
   if (mFrame == aBuilder->RootReferenceFrame()) {
     return;
   }
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+  nsIFrame* outerFrame = nsLayoutUtils::GetCrossDocParentFrame(mFrame);
+  mReferenceFrame = aBuilder->FindReferenceFrameFor(outerFrame);
   mToReferenceFrame = mFrame->GetOffsetToCrossDoc(mReferenceFrame);
-
   if (DisplayPortUtils::IsFixedPosFrameInDisplayPort(mFrame)) {
     
     
@@ -7358,6 +7374,8 @@ void nsDisplayTransform::SetReferenceFrameToAncestor(
       mAnimatedGeometryRoot = mAnimatedGeometryRoot->mParentAGR;
     }
   }
+
+  SetBuildingRect(aBuilder->GetVisibleRect() + mToReferenceFrame);
 }
 
 void nsDisplayTransform::Init(nsDisplayListBuilder* aBuilder,
