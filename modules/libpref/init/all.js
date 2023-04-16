3238dc2ecd6833f89e501ff777c6dc83cdcf7558
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-16 22:58:22 +0000

    Bug 1699490 - Enable HTTP3 on Nightly and beta r=necko-reviewers,kershaw,preferences-reviewers,ntim
    Differential Revision: https://phabricator.services.mozilla.com/D108970
    
    UltraBlame original commit: 76a285c237e9f9c6bf7ebc0b40831d560895a3f5

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -1411,7 +1411,11 @@ pref("network.http.spdy.websockets", true);
 pref("network.http.spdy.enable-hpack-dump", false);
 
 
+#if defined(EARLY_BETA_OR_EARLIER) && !defined(ANDROID)
+pref("network.http.http3.enabled", true);
+#else
 pref("network.http.http3.enabled", false);
+#endif
 
 
 pref("network.http.http3.default-qpack-table-size", 65536); 
