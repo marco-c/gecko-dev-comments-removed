41cda65ba829720cb882deb7589befea8cd34f62
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-16 23:59:35 +0000

    Bug 1523500 - Introduce nsLayoutUtils::IsAncestorFrameCrossDocInProcess and use it to find an ASR for perspective display items. r=mattwoodrow
    Differential Revision: https://phabricator.services.mozilla.com/D109447
    
    UltraBlame original commit: 6ca5eb7fca2a7d4a34a2e2d1a0490485103aa2ab

--- layout/painting/nsDisplayList.cpp
+++ layout/painting/nsDisplayList.cpp
@@ -8722,7 +8722,10 @@ bool nsDisplayPerspective::CreateWebRenderCommands(
 
   Maybe<uint64_t> scrollingRelativeTo;
   for (auto* asr = GetActiveScrolledRoot(); asr; asr = asr->mParent) {
-    if (nsLayoutUtils::IsAncestorFrameCrossDoc(
+    
+    
+    
+    if (nsLayoutUtils::IsAncestorFrameCrossDocInProcess(
             asr->mScrollableFrame->GetScrolledFrame(), perspectiveFrame)) {
       scrollingRelativeTo.emplace(asr->GetViewId());
       break;
