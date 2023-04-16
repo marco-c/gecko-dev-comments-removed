666653378678605278d8a3ede8677491e2063d6c
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-16 23:18:16 +0000

    Bug 708901 - Migrate to nsTHashSet in toolkit/components/url-classifier. r=gcp
    Depends on D109328
    
    Differential Revision: https://phabricator.services.mozilla.com/D108604
    
    UltraBlame original commit: 8c3a523ee4ea7cb9361051c76453bad10c5228b0

--- toolkit/components/url-classifier/nsUrlClassifierUtils.cpp
+++ toolkit/components/url-classifier/nsUrlClassifierUtils.cpp
@@ -34,6 +34,7 @@
 #include "nsPIDOMWindow.h"
 #include "nsServiceManagerUtils.h"
 #include "nsThreadManager.h"
+#include "nsTHashSet.h"
 #include "Classifier.h"
 #include "Entries.h"
 #include "prprf.h"
@@ -844,7 +845,7 @@ nsresult nsUrlClassifierUtils::ReadProvidersFromPrefs(ProviderDictType& aDict) {
   NS_ENSURE_SUCCESS(rv, rv);
 
   
-  nsTHashtable<nsCStringHashKey> providers;
+  nsTHashSet<nsCString> providers;
   for (auto& child : childArray) {
     auto dotPos = child.FindChar('.');
     if (dotPos < 0) {
@@ -853,16 +854,15 @@ nsresult nsUrlClassifierUtils::ReadProvidersFromPrefs(ProviderDictType& aDict) {
 
     nsDependentCSubstring provider = Substring(child, 0, dotPos);
 
-    providers.PutEntry(provider);
+    providers.Insert(provider);
   }
 
   
   
   
-  for (auto itr = providers.Iter(); !itr.Done(); itr.Next()) {
-    auto entry = itr.Get();
-    nsCString provider(entry->GetKey());
-    nsPrintfCString owninListsPref("%s.lists", provider.get());
+  for (const auto& provider : providers) {
+    nsPrintfCString owninListsPref("%s.lists",
+                                   nsPromiseFlatCString{provider}.get());
 
     nsAutoCString owningLists;
     nsresult rv = prefBranch->GetCharPref(owninListsPref.get(), owningLists);
