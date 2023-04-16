354effbf3f0f4facd5ef7efaa5db3176fc0ef48e
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-16 23:33:05 +0000

    Bug 1700492 - add some new debug log for MSE. r=bryce
    Differential Revision: https://phabricator.services.mozilla.com/D109563
    
    UltraBlame original commit: 7f535d74e7551f2f962b6886278e3cefc9384cf0

--- dom/media/mediasource/TrackBuffersManager.cpp
+++ dom/media/mediasource/TrackBuffersManager.cpp
@@ -1285,6 +1285,7 @@ void TrackBuffersManager::OnDemuxerInitDone(const MediaResult& aResult) {
     
     
     if (!mFirstInitializationSegmentReceived) {
+      MSE_DEBUG("Get first init data");
       mAudioTracks.mNumTracks = numAudios;
       
       
@@ -1420,6 +1421,7 @@ void TrackBuffersManager::OnDemuxerInitDone(const MediaResult& aResult) {
       
       mFirstInitializationSegmentReceived = true;
     } else {
+      MSE_DEBUG("Get new init data");
       mAudioTracks.mLastInfo = new TrackInfoSharedPtr(info.mAudio, streamID);
       mVideoTracks.mLastInfo = new TrackInfoSharedPtr(info.mVideo, streamID);
     }
@@ -1475,6 +1477,7 @@ void TrackBuffersManager::OnDemuxerInitDone(const MediaResult& aResult) {
 }
 
 void TrackBuffersManager::OnDemuxerInitFailed(const MediaResult& aError) {
+  MSE_DEBUG("");
   MOZ_ASSERT(aError != NS_ERROR_DOM_MEDIA_WAITING_FOR_DATA);
   mDemuxerInitRequest.Complete();
 
