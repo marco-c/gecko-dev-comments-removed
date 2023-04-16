bd27a4d9bc10fc310f66524f986d477d12544124
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-16 23:43:20 +0000

    Bug 1690746 - Browsertime benchmark measurements are generated only after the page load event r=perftest-reviewers,alexandru.irimovici,sparky
    Please leave the landing to the author of the patch!
    Build with all benchmarks running: https://treeherder.mozilla.org/#/jobs?repo=try&revision=7c672d70f770f207c746ca39b00abf7f29013a48
    
    Differential Revision: https://phabricator.services.mozilla.com/D106923
    
    UltraBlame original commit: 1c9c16a4ccdbfaf0f9aaed5641d7f35eadfd513c

--- testing/raptor/raptor/browsertime/base.py
+++ testing/raptor/raptor/browsertime/base.py
@@ -187,6 +187,16 @@ class Browsertime(Perftest):
                 "--browsertime.background_app",
                 test.get("background_app", "false"),
             ]
+        elif test.get("type", "") == "benchmark":
+            browsertime_script = [
+                os.path.join(
+                    os.path.dirname(__file__),
+                    "..",
+                    "..",
+                    "browsertime",
+                    "browsertime_benchmark.js",
+                )
+            ]
         else:
             browsertime_script = [
                 os.path.join(
@@ -358,20 +368,6 @@ class Browsertime(Perftest):
         
         
         cmd = self._compose_cmd(test, timeout)
-
-        if test.get("type", "") == "benchmark":
-            cmd.extend(
-                [
-                    "--script",
-                    os.path.join(
-                        os.path.dirname(__file__),
-                        "..",
-                        "..",
-                        "browsertime",
-                        "browsertime_benchmark.js",
-                    ),
-                ]
-            )
 
         if test.get("type", "") == "scenario":
             
