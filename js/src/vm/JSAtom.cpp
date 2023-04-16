4fd50c1a6c240557740abce722f8df319945ce19
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-16 23:10:05 +0000

    Backed out 3 changesets (bug 1698791) for causing valgrind failures.
    Backed out changeset 1bd16d8959e5 (bug 1698791)
    Backed out changeset 467a7afcdd3d (bug 1698791)
    Backed out changeset 48b921d8aeee (bug 1698791)
    
    UltraBlame original commit: 26f36067564cca38a530b6edc62a09481848de38

--- js/src/vm/JSAtom.cpp
+++ js/src/vm/JSAtom.cpp
@@ -954,14 +954,7 @@ static MOZ_ALWAYS_INLINE JSAtom* AllocateNewAtom(
   MOZ_ASSERT(atom->hash() == lookup.hash);
 
   if (indexValue) {
-    atom->setIsIndex(*indexValue);
-  } else {
-    
-    
-    uint32_t index;
-    if (atom->isIndexSlow(&index)) {
-      atom->setIsIndex(index);
-    }
+    atom->maybeInitializeIndex(*indexValue, true);
   }
 
   return atom;
