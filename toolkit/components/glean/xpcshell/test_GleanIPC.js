4346e9bfd4aefa434ce608ba18ff369c32852048
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-16 22:45:49 +0000

    Bug 1678567 - Implement `testGetValue` for events. r=chutten
    Differential Revision: https://phabricator.services.mozilla.com/D108450
    
    UltraBlame original commit: bd1d6895f1807f6cf2ecbeac9321515bc38cb315

--- toolkit/components/glean/xpcshell/test_GleanIPC.js
+++ toolkit/components/glean/xpcshell/test_GleanIPC.js
@@ -99,7 +99,7 @@ add_task({ skip_if: () => runningInParent }, async function run_child_stuff() {
   Glean.testOnly.cheesyStringList.add(CHEESIER_STRING);
 
   Glean.testOnlyIpc.noExtraEvent.record();
-  Glean.testOnlyIpc.anEvent.record();
+  Glean.testOnlyIpc.anEvent.record(EVENT_EXTRA);
 
   for (let memory of MEMORIES) {
     Glean.testOnly.doYouRemember.accumulate(memory);
@@ -150,12 +150,16 @@ add_task(
       Assert.ok(count == 1 && MEMORY_BUCKETS.includes(bucket));
     }
 
-    
-    
-    
-    
-    
-    
+    var events = Glean.testOnlyIpc.noExtraEvent.testGetValue();
+    Assert.equal(1, events.length);
+    Assert.equal("test_only.ipc", events[0].category);
+    Assert.equal("no_extra_event", events[0].name);
+
+    events = Glean.testOnlyIpc.anEvent.testGetValue();
+    Assert.equal(1, events.length);
+    Assert.equal("test_only.ipc", events[0].category);
+    Assert.equal("an_event", events[0].name);
+    Assert.deepEqual(EVENT_EXTRA, events[0].extra);
 
     
 
