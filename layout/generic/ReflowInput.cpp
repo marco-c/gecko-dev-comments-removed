849ad86e24a8bb2ea0ae35d4ccb30dec3f61375a
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 07:51:20 +0000

    Bug 1714457 - Make predefined symbolic counter styles not overridable. r=emilio
    Differential Revision: https://phabricator.services.mozilla.com/D116938
    
    UltraBlame original commit: 1a6ee358d1310ff927f10725d6f8dff9723d0450

--- layout/generic/ReflowInput.cpp
+++ layout/generic/ReflowInput.cpp
@@ -92,10 +92,6 @@ static nscoord FontSizeInflationListMarginAdjustment(const nsIFrame* aFrame) {
     return margin;
   }
 
-  
-  
-  
-  
   nsAtom* type = list->mCounterStyle.AsAtom();
   if (type != nsGkAtoms::none && type != nsGkAtoms::disc &&
       type != nsGkAtoms::circle && type != nsGkAtoms::square &&
