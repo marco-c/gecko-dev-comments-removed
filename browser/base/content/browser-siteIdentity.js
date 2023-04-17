af7f4b8258b2f92b9c032678128805b24bf65a92
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 04:16:36 +0000

    Bug 1706379: improve site identity css rules for smooth animation r=mconley
    Differential Revision: https://phabricator.services.mozilla.com/D113888
    
    UltraBlame original commit: 5aef1459fece56ef1443d06de2d3e8417d4d48a2

--- browser/base/content/browser-siteIdentity.js
+++ browser/base/content/browser-siteIdentity.js
@@ -966,9 +966,14 @@ var gIdentityHandler = {
     
     
     this._clearSiteDataFooter.hidden = true;
+    let securityButton = document.getElementById(
+      "identity-popup-security-button"
+    );
+    securityButton.removeAttribute("footerHidden");
     if (this._uriHasHost && !this._pageExtensionPolicy) {
       SiteDataManager.hasSiteData(this._uri.asciiHost).then(hasData => {
         this._clearSiteDataFooter.hidden = !hasData;
+        securityButton.setAttribute("footerHidden", !hasData);
       });
     }
 
