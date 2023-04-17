668bfb9296470d0aceac22c97f143f7f6a5c98b6
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 09:09:15 +0000

    Bug 1716518 - Upgrade toml to v0.4.10.
    Differential Revision: https://phabricator.services.mozilla.com/D117870
    
    Depends on D117869
    
    UltraBlame original commit: 53593e378af3cf453c2e254e52f743657165624a

--- third_party/rust/toml/src/lib.rs
+++ third_party/rust/toml/src/lib.rs
@@ -166,3 +166,10 @@ pub mod de;
 #[doc(no_inline)]
 pub use de::{from_slice, from_str, Deserializer};
 mod tokens;
+
+#[doc(hidden)]
+pub mod macros;
+
+mod spanned;
+#[doc(no_inline)]
+pub use spanned::Spanned;
