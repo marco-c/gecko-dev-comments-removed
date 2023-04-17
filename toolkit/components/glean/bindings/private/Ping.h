04f270089aaa30b168ff811af2eed32f3229df1d
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 02:51:43 +0000

    Bug 1685402 - FOG JS and C++ TestBeforeNextSubmit r=janerik
    Two notes:
    1) To make builds faster and hide the callback map, I've pulled the C++ impl
       into Ping.cpp.
       Doing this for the rest of the metrics is bug 1705394.
    2) Doesn't wait on the queue to drain, actually writing metric values.
       This means that the callback may be running before values reach the db.
       This is surprisingly okay! Because the only way to check metrics values is
       testGetValue which, you guessed it, waits on the queue to drain.
    
    Differential Revision: https://phabricator.services.mozilla.com/D112220
    
    UltraBlame original commit: f9eb32e83608aeedb90cad1afded194cf97810ca

--- toolkit/components/glean/bindings/private/Ping.h
+++ toolkit/components/glean/bindings/private/Ping.h
@@ -7,12 +7,15 @@
 #ifndef mozilla_glean_Ping_h
 #define mozilla_glean_Ping_h
 
+#include "mozilla/DataMutex.h"
 #include "mozilla/glean/fog_ffi_generated.h"
 #include "mozilla/Maybe.h"
 #include "nsIGleanMetrics.h"
 #include "nsString.h"
 
 namespace mozilla::glean {
+
+typedef std::function<void(const nsACString& aReason)> PingTestCallback;
 
 namespace impl {
 
@@ -40,13 +43,21 @@ class Ping {
 
 
 
-  void Submit(const nsACString& aReason = nsCString()) const {
-#ifdef MOZ_GLEAN_ANDROID
-    Unused << mId;
-#else
-    fog_submit_ping_by_id(mId, &aReason);
-#endif
-  }
+  void Submit(const nsACString& aReason = nsCString()) const;
+
+  
+
+
+
+
+
+
+
+
+
+
+
+  void TestBeforeNextSubmit(PingTestCallback&& aCallback) const;
 
  private:
   const uint32_t mId;
--- toolkit/components/glean/gtest/TestFog.cpp
+++ toolkit/components/glean/gtest/TestFog.cpp
@@ -169,11 +169,16 @@ TEST(FOG, TestCppMemoryDistWorks)
 
 TEST(FOG, TestCppPings)
 {
-  auto ping = mozilla::glean_pings::OnePingOnly;
-  mozilla::Unused << ping;
-  
-  
-  
+  test_only::one_ping_one_bool.Set(false);
+  const auto& ping = mozilla::glean_pings::OnePingOnly;
+  bool submitted = false;
+  ping.TestBeforeNextSubmit([&submitted](const nsACString& aReason) {
+    submitted = true;
+    ASSERT_EQ(false, test_only::one_ping_one_bool.TestGetValue().ref());
+  });
+  ping.Submit();
+  ASSERT_TRUE(submitted)
+  << "Must have actually called the lambda.";
 }
 
 TEST(FOG, TestCppStringLists)
