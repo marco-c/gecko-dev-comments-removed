9244ea04e9e0fb71f4c33d77ac87cb3ab2af49a5
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 00:24:30 +0000

    Bug 1700715 - Make document.popupNode work during the popupshowing event for native menus. r=tnikkel
    Differential Revision: https://phabricator.services.mozilla.com/D110141
    
    UltraBlame original commit: d81d522f8b5e13a6de60ca6cdf13ef76b8944229

--- layout/xul/nsXULPopupManager.cpp
+++ layout/xul/nsXULPopupManager.cpp
@@ -731,7 +731,12 @@ void nsXULPopupManager::ShowPopupAtScreen(nsIContent* aPopup, int32_t aXPos,
     bool haveNativeMenu = popupFrame->InitializePopupAsNativeContextMenu(
         triggerContent, aXPos, aYPos);
     if (haveNativeMenu) {
+      
+      
+      
+      mOpeningPopup = aPopup;
       popupFrame->ShowNativeMenu();
+      mOpeningPopup = nullptr;
       return;
     }
   }
