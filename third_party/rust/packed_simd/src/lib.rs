20483b253c3cdad05a601bbac84748b94f49340c
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 16:19:44 +0000

    Backed out changeset 74e579530088 (bug 1730397) as requested by Glandium. CLOSED TREE
    UltraBlame original commit: 445e3c9c7dfcd01d8408ddff09f9cec11158f928

--- third_party/rust/packed_simd/src/lib.rs
+++ third_party/rust/packed_simd/src/lib.rs
@@ -199,9 +199,8 @@
 
 
 
-#![cfg_attr(const_generics, feature(const_generics))]
-#![cfg_attr(not(const_generics), feature(adt_const_params))]
 #![feature(
+    const_generics,
     repr_simd,
     rustc_attrs,
     platform_intrinsics,
