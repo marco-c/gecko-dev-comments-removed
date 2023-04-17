2f239633e9777ffa245711b63d6bd0cda7a65bb3
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 08:18:31 +0000

    Bug 1682632 - part2: Extension API request handling. r=baku,mixedpuppy
    New XPCOM idl interfaces to define the components used to:
    - represent an API request (call to API methods, add/remove API event listener,
      get the value of an API property getter)
    - define the API request handler defined by privileged JS code running on the
      main thread
    
    New ExtensionAPIBase shared stub methods and helper methods to create and forward
    the API requests from the worker to the main thread and translate the results
    got from the API request handler in the value to return or resolve (or the errors
    to be raised) to the extension code running on the worker thread.
    
    Depends on D80604
    
    Differential Revision: https://phabricator.services.mozilla.com/D75311
    
    UltraBlame original commit: 6a9f2fdec9fff3eb709fe12824866bfd824d7fc2

--- toolkit/components/extensions/webidl-api/ExtensionEventManager.h
+++ toolkit/components/extensions/webidl-api/ExtensionEventManager.h
@@ -36,12 +36,26 @@ class ExtensionEventManager final : public nsISupports,
   nsCOMPtr<nsIGlobalObject> mGlobal;
   nsString mAPINamespace;
   nsString mEventName;
+  nsString mAPIObjectType;
+  nsString mAPIObjectId;
 
   ~ExtensionEventManager() = default;
 
+ protected:
+  
+  nsIGlobalObject* GetGlobalObject() const override { return mGlobal; }
+
+  nsString GetAPINamespace() const override { return mAPINamespace; }
+
+  nsString GetAPIObjectType() const override { return mAPIObjectType; }
+
+  nsString GetAPIObjectId() const override { return mAPIObjectId; }
+
  public:
   ExtensionEventManager(nsIGlobalObject* aGlobal, const nsAString& aNamespace,
-                        const nsAString& aEventName);
+                        const nsAString& aEventName,
+                        const nsAString& aObjectType = VoidString(),
+                        const nsAString& aObjectId = VoidString());
 
   
   JSObject* WrapObject(JSContext* aCx,
