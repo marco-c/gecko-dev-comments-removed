4cbad6fafc09d6d429c415ec2202c0b6fde29450
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 19:48:24 +0000

    Bug 1727084 - Memory64 - Test cases and testing code. r=yury
    There are supposedly test cases here for all operations introduced in
    the previous patches, and for a number of corner cases /
    optimizations.
    
    Although the proposal repo has some test cases, those are not here
    yet, they will come in with Milestone 2.
    
    Differential Revision: https://phabricator.services.mozilla.com/D127064
    
    UltraBlame original commit: 25456db35c176fcc71aaa73229da993a8aec5ec2

--- js/src/jit-test/tests/wasm/memory-cloning.js
+++ js/src/jit-test/tests/wasm/memory-cloning.js
@@ -2,11 +2,24 @@
 
 
 
+var memtypes = wasmMemory64Enabled() ? ['i32', 'i64'] : [''];
+
+function makeMemoryDesc(memtype, d) {
+    if (memtype != '') {
+        d.index = memtype;
+    }
+    return d;
+}
+
+function Zero(memtype) {
+    return memtype == 'i64' ? 0n : 0;
+}
 
 
 
-{
-    let mem1 = new WebAssembly.Memory({initial: 2, maximum: 4});
+
+for ( let memtype of memtypes ) {
+    let mem1 = new WebAssembly.Memory(makeMemoryDesc(memtype, {initial: 2, maximum: 4}));
     assertErrorMessage(() => serialize(mem1),
 		       TypeError,
 		       /unsupported type for structured data/);
@@ -18,8 +31,9 @@
 
 
 
-{
-    let mem1 = new WebAssembly.Memory({initial: 2, maximum: 4, shared: true});
+for ( let memtype of memtypes ) {
+    let ptrtype = memtype == 'i64' ? memtype : 'i32';
+    let mem1 = new WebAssembly.Memory(makeMemoryDesc(memtype, {initial: 2, maximum: 4, shared: true}));
     let buf1 = mem1.buffer;
 
     
@@ -31,6 +45,9 @@
 
     assertEq(buf1 !== buf2, true);
     assertEq(buf1.byteLength, buf2.byteLength);
+    if (memtype != '' && mem2.type) {
+        assertEq(mem2.type().index, memtype);
+    }
 
     
 
@@ -44,9 +61,9 @@
 
     let index = 2*65536 + 200;
     let access = wasmEvalText(`(module
-				(memory (import "" "memory") 2 4 shared)
-				(func (export "l") (result i32)
-				 (i32.load (i32.const ${index}))))`,
+				(memory (import "" "memory") ${memtype} 2 4 shared)
+				(func (export "l") (result ${ptrtype})
+				 (${ptrtype}.load (${ptrtype}.const ${index}))))`,
 			      {"": {memory: mem2}}).exports.l;
 
     
@@ -54,18 +71,18 @@
 
     
     wasmEvalText(`(module
-		   (memory (import "" "memory") 2 4 shared)
-		   (func (export "g") (drop (memory.grow (i32.const 1)))))`,
+		   (memory (import "" "memory") ${memtype} 2 4 shared)
+		   (func (export "g") (drop (memory.grow (${ptrtype}.const 1)))))`,
 		 {"": {memory: mem1}}).exports.g();
 
     
-    assertEq(access(), 0);
+    assertEq(access(), Zero(memtype));
 }
 
 
 
-{
-    let mem1 = new WebAssembly.Memory({initial: 2, maximum: 4, shared: true});
+for ( let memtype of memtypes ) {
+    let mem1 = new WebAssembly.Memory(makeMemoryDesc(memtype, {initial: 2, maximum: 4, shared: true}));
     assertErrorMessage(() => serialize(mem1, [mem1]),
 		       TypeError,
 		       /Shared memory objects must not be in the transfer list/);
@@ -76,8 +93,8 @@
 
 
 
-{
-    let mem = new WebAssembly.Memory({initial: 2, maximum: 4, shared: true});
+for ( let memtype of memtypes ) {
+    let mem = new WebAssembly.Memory(makeMemoryDesc(memtype, {initial: 2, maximum: 4, shared: true}));
     let buf = mem.buffer;
     let clonedbuf = serialize(buf, [], {SharedArrayBuffer: 'allow'});
     mem.grow(1);
