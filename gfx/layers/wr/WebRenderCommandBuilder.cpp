a405427b03ec1c91b8fb87ad7d7cc81a631bb89f
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 13:43:05 +0000

    Bug 1723200 - Give a deferred transform its own WebRenderLayerScrollData node when processing a scroll info layer item. r=tnikkel
    Depends on D123036
    
    Differential Revision: https://phabricator.services.mozilla.com/D123037
    
    UltraBlame original commit: 52ee9f1d9fbcbd71f304940b71f2c5976ed4bab9

--- gfx/layers/wr/WebRenderCommandBuilder.cpp
+++ gfx/layers/wr/WebRenderCommandBuilder.cpp
@@ -1835,13 +1835,25 @@ void WebRenderCommandBuilder::CreateWebRenderCommandsFromDisplayList(
         nsDisplayTransform* deferred = aSc.GetDeferredTransformItem();
         ScrollableLayerGuid::ViewID deferredId =
             ScrollableLayerGuid::NULL_SCROLL_ID;
+        bool transformShouldGetOwnLayer = false;
         if (deferred) {
           if (const auto* asr = deferred->GetActiveScrolledRoot()) {
             deferredId = asr->GetViewId();
           }
+
+          if (deferred->GetActiveScrolledRoot() !=
+              item->GetActiveScrolledRoot()) {
+            transformShouldGetOwnLayer = true;
+          } else if (item->GetType() ==
+                     DisplayItemType::TYPE_SCROLL_INFO_LAYER) {
+            
+            
+            
+            
+            transformShouldGetOwnLayer = true;
+          }
         }
-        if (deferred && deferred->GetActiveScrolledRoot() !=
-                            item->GetActiveScrolledRoot()) {
+        if (transformShouldGetOwnLayer) {
           
           
           
