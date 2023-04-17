6fcc33d46b5836a0df587ba6916aeb05ae21582a
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 19:19:41 +0000

    Bug 1733908 - Add one more js::wasm::Code suppression to TSan r=decoder
    Try pushes are showing one more data race that I didn't catch.
    
    Differential Revision: https://phabricator.services.mozilla.com/D128271
    
    UltraBlame original commit: e1333c140677cf485abf128daf154957853d4b77

--- mozglue/build/TsanOptions.cpp
+++ mozglue/build/TsanOptions.cpp
@@ -299,6 +299,7 @@ extern "C" const char* __tsan_default_suppressions() {
          
          "race:js::wasm::Code::bestTier\n"
          "race:js::wasm::Code::commitTier2\n"
+         "race:js::wasm::Code::setTier2\n"
 
       
       ;  
