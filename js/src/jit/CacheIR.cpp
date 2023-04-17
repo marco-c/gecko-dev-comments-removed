4850eeae5a82fd680d1d6b73f90bc0dfcdc8ac94
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 12:47:20 +0000

    Bug 1341265 - Part 11: Optimise Set.prototype.has for objects in CacheIR. r=iain
    Inline `Set.prototype.has` in CacheIR when called with objects.
    
    Implementing `MacroAssembler::hashObject()` on 32-bit platforms is difficult,
    because it uses 64-bit operations, so we either have to allocate twice as much
    registers for `Register64` or alternatively spill the values on the stack. For
    now just punt and only support this optimisation on 64-bit platforms.
    
    Differential Revision: https://phabricator.services.mozilla.com/D118977
    
    UltraBlame original commit: 186a7d8db8163ba7e35f0b48c25217c8f3edb4c1

--- js/src/jit/CacheIR.cpp
+++ js/src/jit/CacheIR.cpp
@@ -7820,9 +7820,16 @@ AttachDecision CallIRGenerator::tryAttachSetHas(HandleFunction callee) {
         writer.setHasBigIntResult(objId, bigIntId);
         break;
       }
-      case ValueType::Object:
+      case ValueType::Object: {
+        
+#  ifdef JS_PUNBOX64
+        ObjOperandId valId = writer.guardToObject(argId);
+        writer.setHasObjectResult(objId, valId);
+#  else
         writer.setHasResult(objId, argId);
+#  endif
         break;
+      }
 
       case ValueType::Magic:
       case ValueType::PrivateGCThing:
