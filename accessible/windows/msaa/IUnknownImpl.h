c4702dbbaf44ed06bb76f6054f71483c0d3e96d0
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 04:19:56 +0000

    Bug 1694865 part 23: Support inheritance/overriding of reference counting in IUnknownImpl. r=morgan
    Until now, our a11y COM implementation has relied on AddRef and Release from nsISupports, but this soon won't be possible.
    Instead, MsaaAccessible will implement its own reference counting using DECL_IUNKNOWN.
    
    DECL_IUNKNOWN previously declared AddRef and Release as final.
    This doesn't work for MsaaAccessible because there is an aggregatable subclass (MsaaRootAccessible) and because some subclasses will inherit additional interfaces (ia2AccessibleApplication, etc.).
    
    When subclasses inherit additional interfaces, they inherit an additional IUnknown, so the compiler doesn't know which AddRef/Release to call.
    To support this, IMPL_IUNKNOWN_REFCOUNTING_INHERITED HAS BEEN ADDED to specify which base class implements reference counting.
    
    Differential Revision: https://phabricator.services.mozilla.com/D112954
    
    UltraBlame original commit: 893c83f82c167cfd68489c45b969a0d0bc8e63d2

--- accessible/windows/msaa/IUnknownImpl.h
+++ accessible/windows/msaa/IUnknownImpl.h
@@ -50,12 +50,12 @@ class AutoRefCnt {
 #define DECL_IUNKNOWN                                               \
  public:                                                            \
   virtual HRESULT STDMETHODCALLTYPE QueryInterface(REFIID, void**); \
-  ULONG STDMETHODCALLTYPE AddRef() final {                          \
+  ULONG STDMETHODCALLTYPE AddRef() override {                       \
     MOZ_ASSERT(int32_t(mRefCnt) >= 0, "illegal refcnt");            \
     ++mRefCnt;                                                      \
     return mRefCnt;                                                 \
   }                                                                 \
-  ULONG STDMETHODCALLTYPE Release() final {                         \
+  ULONG STDMETHODCALLTYPE Release() override {                      \
     MOZ_ASSERT(int32_t(mRefCnt) > 0, "dup release");                \
     --mRefCnt;                                                      \
     if (mRefCnt) return mRefCnt;                                    \
@@ -146,6 +146,18 @@ class AutoRefCnt {
   IMPL_IUNKNOWN_QUERY_CLASS(Super1);                            \
   IMPL_IUNKNOWN_QUERY_CLASS(Super2);                            \
   IMPL_IUNKNOWN_QUERY_TAIL_INHERITED(Super0)
+
+
+
+
+
+
+
+
+#define IMPL_IUNKNOWN_REFCOUNTING_INHERITED(BaseClass)                      \
+ public:                                                                    \
+  ULONG STDMETHODCALLTYPE AddRef() override { return BaseClass::AddRef(); } \
+  ULONG STDMETHODCALLTYPE Release() override { return BaseClass::Release(); }
 
 namespace mozilla {
 namespace a11y {
