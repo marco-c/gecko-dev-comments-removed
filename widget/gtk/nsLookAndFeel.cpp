5c809b2ede5ac8a88fced8fed10c414d599a0f28
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 04:13:40 +0000

    Bug 1707957 - Extract both light and dark system colors in GTK. r=stransky
    And add code to use the appropriate variant like we do in macOS with
    respect-system-appearance (but this still needs more work as noted in
    StaticPrefList.yaml).
    
    Still, it cleans up a bunch, and allows to not depend on the content
    process boundary to provide light system colors.
    
    Depends on D113542
    
    Differential Revision: https://phabricator.services.mozilla.com/D113543
    
    UltraBlame original commit: 2f0462c1b2aace2c59e6e8f75292f7cd6ca77269

--- widget/gtk/nsLookAndFeel.cpp
+++ widget/gtk/nsLookAndFeel.cpp
@@ -7,6 +7,7 @@
 
 
 #include <stdlib.h>
+#include <dlfcn.h>
 
 #include "nsLookAndFeel.h"
 
@@ -296,11 +297,12 @@ static bool IsSelectionColorBackground(LookAndFeel::ColorID aID) {
   }
 }
 
-nsresult nsLookAndFeel::NativeGetColor(ColorID aID, ColorScheme,
+nsresult nsLookAndFeel::NativeGetColor(ColorID aID, ColorScheme aScheme,
                                        nscolor& aColor) {
   EnsureInit();
 
-  return mSystemTheme.GetColor(aID, aColor);
+  auto& theme = aScheme == ColorScheme::Light ? LightTheme() : DarkTheme();
+  return theme.GetColor(aID, aColor);
 }
 
 nsresult nsLookAndFeel::PerThemeData::GetColor(ColorID aID,
@@ -966,6 +968,18 @@ static bool GetPreferDarkTheme() {
   return preferDarkTheme == TRUE;
 }
 
+
+
+
+static bool GetThemeIsDark() {
+  GdkRGBA bg, fg;
+  GtkStyleContext* style = GetStyleContext(MOZ_GTK_WINDOW);
+  gtk_style_context_get_background_color(style, GTK_STATE_FLAG_NORMAL, &bg);
+  gtk_style_context_get_color(style, GTK_STATE_FLAG_NORMAL, &fg);
+  return RelativeLuminanceUtils::Compute(GDK_RGBA_TO_NS_RGBA(bg)) <
+         RelativeLuminanceUtils::Compute(GDK_RGBA_TO_NS_RGBA(fg));
+}
+
 void nsLookAndFeel::ConfigureTheme(const LookAndFeelTheme& aTheme) {
   MOZ_ASSERT(XRE_IsContentProcess());
   GtkSettings* settings = gtk_settings_get_default();
@@ -974,131 +988,73 @@ void nsLookAndFeel::ConfigureTheme(const LookAndFeelTheme& aTheme) {
                aTheme.preferDarkTheme() ? TRUE : FALSE, nullptr);
 }
 
-void nsLookAndFeel::WithThemeConfiguredForContent(
-    const std::function<void(const LookAndFeelTheme&, bool)>& aFn) {
+void nsLookAndFeel::WithAltThemeConfigured(
+    const std::function<void(bool)>& aFn) {
   nsWindow::WithSettingsChangesIgnored([&]() {
     
     static auto sGtkSettingsResetProperty =
         (void (*)(GtkSettings*, const gchar*))dlsym(
             RTLD_DEFAULT, "gtk_settings_reset_property");
 
-    nsCString themeName;
-    bool preferDarkTheme = false;
+    GtkSettings* settings = gtk_settings_get_default();
 
-    if (!sGtkSettingsResetProperty) {
-      
-      
-      
-      
-      
-      
-      themeName = GetGtkTheme();
-      preferDarkTheme = GetPreferDarkTheme();
-    }
+    bool fellBackToDefaultTheme = false;
 
-    bool changed = ConfigureContentGtkTheme();
-    if (changed) {
-      RefreshImpl();
-    }
+    
+    LOG(("    toggling gtk-application-prefer-dark-theme\n"));
+    g_object_set(settings, "gtk-application-prefer-dark-theme",
+                 !mSystemTheme.mIsDark, nullptr);
+    moz_gtk_refresh();
 
-    LookAndFeelTheme theme;
-    theme.themeName() = GetGtkTheme();
-    theme.preferDarkTheme() = GetPreferDarkTheme();
-
-    aFn(theme, changed);
-
-    if (changed) {
-      GtkSettings* settings = gtk_settings_get_default();
-      if (sGtkSettingsResetProperty) {
-        sGtkSettingsResetProperty(settings, "gtk-theme-name");
-        sGtkSettingsResetProperty(settings,
-                                  "gtk-application-prefer-dark-theme");
-      } else {
-        g_object_set(settings, "gtk-theme-name", themeName.get(),
-                     "gtk-application-prefer-dark-theme",
-                     preferDarkTheme ? TRUE : FALSE, nullptr);
+    
+    
+    
+    
+    if (mSystemTheme.mIsDark && mSystemTheme.mIsDark == GetThemeIsDark()) {
+      nsCString potentialLightThemeName = mSystemTheme.mName;
+      constexpr nsLiteralCString kSubstringsToRemove[] = {
+          "-dark"_ns,    "-darker"_ns,  "-darkest"_ns, "-Dark"_ns,
+          "-Darker"_ns,  "-Darkest"_ns, "_dark"_ns,    "_darker"_ns,
+          "_darkest"_ns, "_Dark"_ns,    "_Darker"_ns,  "_Darkest"_ns,
+      };
+      bool found = false;
+      for (auto& s : kSubstringsToRemove) {
+        potentialLightThemeName = mSystemTheme.mName;
+        potentialLightThemeName.ReplaceSubstring(s, ""_ns);
+        if (potentialLightThemeName.Length() != mSystemTheme.mName.Length()) {
+          found = true;
+          break;
+        }
       }
-      RefreshImpl();
+      if (found) {
+        g_object_set(settings, "gtk-theme-name", potentialLightThemeName.get(),
+                     nullptr);
+        moz_gtk_refresh();
+      }
     }
+
+    if (mSystemTheme.mIsDark == GetThemeIsDark()) {
+      
+      
+      g_object_set(settings, "gtk-theme-name",
+                   mSystemTheme.mIsDark ? "Adwaita" : "Adwaita Dark", nullptr);
+      moz_gtk_refresh();
+      fellBackToDefaultTheme = true;
+    }
+
+    aFn(fellBackToDefaultTheme);
+
+    
+    if (sGtkSettingsResetProperty) {
+      sGtkSettingsResetProperty(settings, "gtk-theme-name");
+      sGtkSettingsResetProperty(settings, "gtk-application-prefer-dark-theme");
+    } else {
+      g_object_set(settings, "gtk-theme-name", mSystemTheme.mName.get(),
+                   "gtk-application-prefer-dark-theme",
+                   mSystemTheme.mPreferDarkTheme, nullptr);
+    }
+    moz_gtk_refresh();
   });
-}
-
-bool nsLookAndFeel::FromParentTheme(IntID aID) {
-  switch (aID) {
-    case IntID::SystemUsesDarkTheme:
-    case IntID::UseAccessibilityTheme:
-      
-      
-      
-      
-      
-      return true;
-    default:
-      return false;
-  }
-}
-
-bool nsLookAndFeel::FromParentTheme(ColorID aID) {
-  if (IsSelectionColorBackground(aID) || IsSelectionColorForeground(aID)) {
-    return StaticPrefs::widget_content_allow_gtk_dark_theme_selection();
-  }
-  switch (aID) {
-    case ColorID::ThemedScrollbar:
-    case ColorID::ThemedScrollbarInactive:
-    case ColorID::ThemedScrollbarThumb:
-    case ColorID::ThemedScrollbarThumbHover:
-    case ColorID::ThemedScrollbarThumbInactive:
-      return StaticPrefs::widget_content_allow_gtk_dark_theme_scrollbar();
-    case ColorID::ThemedScrollbarThumbActive:
-      return StaticPrefs::
-          widget_content_allow_gtk_dark_theme_scrollbar_active();
-    case ColorID::MozAccentColor:
-    case ColorID::MozAccentColorForeground:
-      return StaticPrefs::widget_content_allow_gtk_dark_theme_accent();
-    default:
-      return false;
-  }
-}
-
-bool nsLookAndFeel::ConfigureContentGtkTheme() {
-  bool changed = false;
-
-  GtkSettings* settings = gtk_settings_get_default();
-
-  nsAutoCString themeOverride;
-  mozilla::Preferences::GetCString("widget.content.gtk-theme-override",
-                                   themeOverride);
-  if (!themeOverride.IsEmpty()) {
-    g_object_set(settings, "gtk-theme-name", themeOverride.get(), nullptr);
-    changed = true;
-    LOG(("ConfigureContentGtkTheme(%s)\n", themeOverride.get()));
-  } else {
-    LOG(("ConfigureContentGtkTheme(%s)\n", GetGtkTheme().get()));
-  }
-
-  
-  
-  
-  if (!themeOverride.IsEmpty() || mSystemTheme.mHighContrast ||
-      StaticPrefs::widget_content_allow_gtk_dark_theme()) {
-    return changed;
-  }
-
-  
-  if (GetPreferDarkTheme()) {
-    LOG(("    disabling gtk-application-prefer-dark-theme\n"));
-    g_object_set(settings, "gtk-application-prefer-dark-theme", FALSE, nullptr);
-    changed = true;
-  }
-
-  
-  if (!IsGtkThemeCompatibleWithHTMLColors()) {
-    LOG(("    Non-compatible dark theme, default to Adwaita\n"));
-    g_object_set(settings, "gtk-theme-name", "Adwaita", nullptr);
-    changed = true;
-  }
-
-  return changed;
 }
 
 static bool AnyColorChannelIsDifferent(nscolor aColor) {
@@ -1185,7 +1141,57 @@ void nsLookAndFeel::EnsureInit() {
     }
   }
 
+  WithAltThemeConfigured([&](bool aFellBackToDefaultTheme) {
+    mAltTheme.Init();
+    
+    
+    if (aFellBackToDefaultTheme) {
+      if (StaticPrefs::widget_gtk_alt_theme_selection()) {
+        mAltTheme.mTextSelectedText = mSystemTheme.mTextSelectedText;
+        mAltTheme.mTextSelectedBackground =
+            mSystemTheme.mTextSelectedBackground;
+      }
+
+      if (StaticPrefs::widget_gtk_alt_theme_scrollbar()) {
+        mAltTheme.mThemedScrollbar = mSystemTheme.mThemedScrollbar;
+        mAltTheme.mThemedScrollbarInactive =
+            mSystemTheme.mThemedScrollbarInactive;
+        mAltTheme.mThemedScrollbarThumb = mSystemTheme.mThemedScrollbarThumb;
+        mAltTheme.mThemedScrollbarThumbHover =
+            mSystemTheme.mThemedScrollbarThumbHover;
+        mAltTheme.mThemedScrollbarThumbInactive =
+            mSystemTheme.mThemedScrollbarThumbInactive;
+      }
+
+      if (StaticPrefs::widget_gtk_alt_theme_scrollbar_active()) {
+        mAltTheme.mThemedScrollbarThumbActive =
+            mSystemTheme.mThemedScrollbarThumbActive;
+      }
+
+      if (StaticPrefs::widget_gtk_alt_theme_selection()) {
+        mAltTheme.mAccentColor = mSystemTheme.mAccentColor;
+        mAltTheme.mAccentColorForeground = mSystemTheme.mAccentColorForeground;
+      }
+    }
+  });
+
+  LOG(("System Theme: %s. Alt Theme: %s\n", mSystemTheme.mName.get(),
+       mAltTheme.mName.get()));
+
   RecordTelemetry();
+}
+
+void nsLookAndFeel::GetGtkContentTheme(LookAndFeelTheme& aTheme) {
+  if (NS_SUCCEEDED(Preferences::GetCString("widget.content.gtk-theme-override",
+                                           aTheme.themeName()))) {
+    return;
+  }
+
+  auto& theme = StaticPrefs::widget_content_allow_gtk_dark_theme()
+                    ? mSystemTheme
+                    : LightTheme();
+  aTheme.preferDarkTheme() = theme.mPreferDarkTheme;
+  aTheme.themeName() = theme.mName;
 }
 
 void nsLookAndFeel::PerThemeData::Init() {
@@ -1198,17 +1204,10 @@ void nsLookAndFeel::PerThemeData::Init() {
 
   mPreferDarkTheme = GetPreferDarkTheme();
 
-  
-  
-  
-  {
-    GdkRGBA bg, fg;
-    style = GetStyleContext(MOZ_GTK_WINDOW);
-    gtk_style_context_get_background_color(style, GTK_STATE_FLAG_NORMAL, &bg);
-    gtk_style_context_get_color(style, GTK_STATE_FLAG_NORMAL, &fg);
-    mIsDark = (RelativeLuminanceUtils::Compute(GDK_RGBA_TO_NS_RGBA(bg)) <
-               RelativeLuminanceUtils::Compute(GDK_RGBA_TO_NS_RGBA(fg)));
-  }
+  mIsDark = GetThemeIsDark();
+
+  mCompatibleWithHTMLLightColors =
+      !mIsDark && IsGtkThemeCompatibleWithHTMLColors();
 
   GdkRGBA color;
   
--- widget/nsXPLookAndFeel.h
+++ widget/nsXPLookAndFeel.h
@@ -61,25 +61,7 @@ class nsXPLookAndFeel : public mozilla::LookAndFeel {
 
   virtual void NativeInit() = 0;
 
-  virtual void WithThemeConfiguredForContent(
-      const std::function<void(const LookAndFeelTheme&, bool aChanged)>& aFn) {
-    aFn(LookAndFeelTheme{}, false);
-  }
-
-  
-  
-  virtual bool FromParentTheme(IntID) {
-    MOZ_ASSERT_UNREACHABLE(
-        "Should override if WithThemeConfiguredForContent can change the "
-        "theme");
-    return false;
-  }
-  virtual bool FromParentTheme(ColorID) {
-    MOZ_ASSERT_UNREACHABLE(
-        "Should override if WithThemeConfiguredForContent can change the "
-        "theme");
-    return false;
-  }
+  virtual void GetGtkContentTheme(LookAndFeelTheme&) {}
 
  protected:
   nsXPLookAndFeel() = default;
