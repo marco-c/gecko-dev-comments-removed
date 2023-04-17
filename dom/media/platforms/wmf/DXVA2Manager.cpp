d6b06b1ed2e46bdf5235efa83c93337936e03bd8
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 18:18:07 +0000

    Bug 1730020 - part1 : implement a new class to record the time which each stage of media pinepline spends on. r=bryce,padenot
    Differential Revision: https://phabricator.services.mozilla.com/D125140
    
    UltraBlame original commit: 8799f103c043c1881aaadfd264040dd168078788

--- dom/media/platforms/wmf/DXVA2Manager.cpp
+++ dom/media/platforms/wmf/DXVA2Manager.cpp
@@ -12,6 +12,7 @@
 #include "ImageContainer.h"
 #include "MFTDecoder.h"
 #include "MediaTelemetryConstants.h"
+#include "PerformanceRecorder.h"
 #include "VideoUtils.h"
 #include "WMFUtils.h"
 #include "gfxCrashReporterUtils.h"
@@ -896,6 +897,9 @@ D3D11DXVA2Manager::CopyToImage(IMFSample* aVideoSample,
       NS_ENSURE_TRUE(mSyncObject, E_FAIL);
     }
 
+    PerformanceRecorder perfRecorder(
+        PerformanceRecorder::Stage::CopyDecodedVideo, height);
+    perfRecorder.Start();
     
     
     if (outDesc.Format == inDesc.Format) {
@@ -925,6 +929,7 @@ D3D11DXVA2Manager::CopyToImage(IMFSample* aVideoSample,
           [&]() -> void { hr = mTransform->Output(&sample); });
       NS_ENSURE_TRUE(SUCCEEDED(hr), hr);
     }
+    perfRecorder.End();
   }
 
   if (!mutex && mDevice != DeviceManagerDx::Get()->GetCompositorDevice() &&
