fbb57a3d1cf0b8a48129d0cea517c4960284aff1
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 19:30:30 +0000

    Bug 1722519 [wpt PR 29804] - Add support for rerunning testharness(+testdriver) tests, a=testonly
    Automatic update from web-platform-tests
    Add support for rerunning testharness(+testdriver) tests (#29804)
    
    When running a test that uses testdriver using reload doesn't work,
    because the testdriver parts won't rerun. This is annoying. To make
    the development experience better, add a rerun button to testharness
    tests. In the case where the test is being run directly from the
    server, this just ends up calling window.reload(). In the case we're
    running via wptrunner, it causes the test to be rerun from the start.
    
    Implementation wise, this works by dispatching an event (currently
    named `__test_restart`) from the rerun button. This can be caught by
    the wait() implementation in executors, which now provide a return
    value of True to rerun the test and False to continue with the next
    test.
    
    There is an edge case where the manual reruns are mixed with automatic
    reruns specified on the command line. In practice it's not clear what
    the use case is, but the behaviour as written is to ensure the test is
    run at least as many times as specified on the command line, counting
    manual reruns against that total.
    --
    
    wpt-commits: 335403228bc8359b6d74c75b4a5ac2efb0c767ce
    wpt-pr: 29804
    
    UltraBlame original commit: de6cfefccffc3516715d60b3443594ad70287f9d

--- testing/web-platform/tests/tools/wptrunner/wptrunner/testrunner.py
+++ testing/web-platform/tests/tools/wptrunner/wptrunner/testrunner.py
@@ -114,8 +114,8 @@ class TestRunner(object):
             raise
 
     def wait(self):
-        self.executor.wait()
-        self.send_message("wait_finished")
+        rerun = self.executor.wait()
+        self.send_message("wait_finished", rerun)
 
     def send_message(self, command, *args):
         self.result_queue.put((command, args))
@@ -696,17 +696,20 @@ class TestRunnerManager(threading.Thread):
         else:
             return self.after_test_end(test, restart_before_next)
 
-    def wait_finished(self):
+    def wait_finished(self, rerun=False):
         assert isinstance(self.state, RunnerManagerState.running)
         self.logger.debug("Wait finished")
 
         
         
-        return self.after_test_end(self.state.test, True)
+        return self.after_test_end(self.state.test, not rerun, force_rerun=rerun)
 
-    def after_test_end(self, test, restart):
+    def after_test_end(self, test, restart, force_rerun=False):
         assert isinstance(self.state, RunnerManagerState.running)
-        if self.run_count == self.rerun:
+        
+        
+        
+        if not force_rerun and self.run_count >= self.rerun:
             test, test_group, group_metadata = self.get_next_test()
             if test is None:
                 return RunnerManagerState.stop()
