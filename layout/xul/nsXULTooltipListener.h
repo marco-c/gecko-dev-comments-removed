9ced24cc5a78b170ed7686c8c559421482712edf
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 11:49:35 +0000

    Bug 1681941, ensure nsXULTooltipListener shows tooltips when entering new target nodes r=smaug
    Differential Revision: https://phabricator.services.mozilla.com/D101951
    
    UltraBlame original commit: d5c23cd282fd06b1e5d7d85d03aad24f07c48ac9

--- layout/xul/nsXULTooltipListener.h
+++ layout/xul/nsXULTooltipListener.h
@@ -72,6 +72,7 @@ class nsXULTooltipListener final : public nsIDOMEventListener {
   nsWeakPtr mSourceNode;
   nsWeakPtr mTargetNode;
   nsWeakPtr mCurrentTooltip;
+  nsWeakPtr mPreviousMouseMoveTarget;
 
   
   nsCOMPtr<nsITimer> mTooltipTimer;
