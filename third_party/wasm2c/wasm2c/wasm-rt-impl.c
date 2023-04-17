f46f5c4d06490a72a1616a91e9d2500324cd125b
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 19:10:06 +0000

    Bug 1734421 - Update wasm2c to support better use of 32-bit virtual space r=glandium
    Differential Revision: https://phabricator.services.mozilla.com/D127727
    
    UltraBlame original commit: 6e1011402c79e223af8038a38552fb538e6b9b94

--- third_party/wasm2c/wasm2c/wasm-rt-impl.c
+++ third_party/wasm2c/wasm2c/wasm-rt-impl.c
@@ -103,12 +103,12 @@ void wasm_rt_cleanup_func_types(wasm_func_type_t** p_func_type_structs, uint32_t
 # define WASM_HEAP_MAX_ALLOWED_PAGES 65536
 #elif UINTPTR_MAX == 0xffffffff
 
-# define WASM_HEAP_GUARD_PAGE_ALIGNMENT 0x800000ul
+# define WASM_HEAP_GUARD_PAGE_ALIGNMENT 0
 # define WASM_HEAP_RESERVE_SIZE 0x1000000ul
 # ifdef WASM_USE_INCREMENTAL_MOVEABLE_MEMORY_ALLOC
 #   define WASM_HEAP_MAX_ALLOWED_PAGES 65536
 # else
-#   define WASM_HEAP_MAX_ALLOWED_PAGES 128
+#   define WASM_HEAP_MAX_ALLOWED_PAGES 256
 # endif
 #else
 # error "Unknown pointer size"
