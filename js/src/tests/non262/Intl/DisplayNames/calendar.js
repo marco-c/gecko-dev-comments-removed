b9bc948e40546561ba674845e44084bbd8cb3b87
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 07:25:35 +0000

    Bug 1693575 - Part 3: Implement support for "calendar" option. r=tcampbell
    This last part to support "calendar" requires a bit more code.
    
    `GetCalendarDisplayName()` performs the following steps:
    1. Validate the input matches the `type` nonterminal, per
       CanonicalCodeForDisplayNames, step 4.a.
    2. Then convert it to lower case, because ICU expects lower case names and it
       needs to be in lower case for `LanguageTag::replaceUnicodeExtensionType()`.
    3. Replace calendar name aliases with the preferred name, e.g. use "ethioaa"
       instead of "ethiopic-amete-alem".
    4. Check the input isn't too large, just in case ICU somehow doesn't properly
       handle large string inputs.
    5. Retrieve the old-style value of the calendar name, for example "gregorian"
       for the calendar name "gregory".
    6. And finally call `uldn_keyValueDisplayName` to search for the localised name.
    
    Our ICU data file doesn't yet include calendar names. To include calendar names
    we need to enable them in "data_filter.json", rebuild the ICU data file, and
    then reapply the update for tzdata 2021a. (Reapplying the tzdata changed
    "intl/tzdata/GIT-INFO", but otherwise didn't change any time zone files.)
    
    Adding the calendar names to the ICU data file results in a slight increase of
    the overall data file size (79,184 bytes).
    
    The existing "calendar.js" test file was renamed to "month-calendar.js", so
    the test file for the new "calendar" type can be named "calendar.js".
    
    Differential Revision: https://phabricator.services.mozilla.com/D116008
    
    UltraBlame original commit: 37aca63b0ecfe427f7fbd7c117eb315dbf984a5b

--- js/src/tests/non262/Intl/DisplayNames/calendar.js
+++ js/src/tests/non262/Intl/DisplayNames/calendar.js
@@ -1,18 +1,123 @@
 
 
-addMozIntlDisplayNames(this);
+const tests = {
+  "en": {
+    long: {
+      "gregory": "Gregorian Calendar",
+      "iso8601": "ISO-8601 Calendar",
+      "japanese": "Japanese Calendar",
+      "islamic-civil": "Islamic Calendar (tabular, civil epoch)",
+      "islamicc": "Islamic Calendar (tabular, civil epoch)",
+      "ethioaa": "Ethiopic Amete Alem Calendar",
+      "ethiopic-amete-alem": "Ethiopic Amete Alem Calendar",
+    },
+    short: {},
+    narrow: {},
+  },
+  "de": {
+    long: {
+      "gregory": "Gregorianischer Kalender",
+      "iso8601": "ISO-8601-Kalender",
+      "japanese": "Japanischer Kalender",
+      "islamic-civil": "Bürgerlicher islamischer Kalender",
+      "islamicc": "Bürgerlicher islamischer Kalender",
+      "ethioaa": "Äthiopischer Kalender \"Amete Alem\"",
+      "ethiopic-amete-alem": "Äthiopischer Kalender \"Amete Alem\"",
+    },
+    short: {},
+    narrow: {},
+  },
+  "fr": {
+    long: {
+      "gregory": "calendrier grégorien",
+      "iso8601": "calendrier ISO 8601",
+      "japanese": "calendrier japonais",
+      "islamic-civil": "calendrier musulman (tabulaire, époque civile)",
+      "islamicc": "calendrier musulman (tabulaire, époque civile)",
+      "ethioaa": "calendrier éthiopien Amete Alem",
+      "ethiopic-amete-alem": "calendrier éthiopien Amete Alem",
+    },
+    short: {},
+    narrow: {},
+  },
+  "zh": {
+    long: {
+      "gregory": "公历",
+      "iso8601": "国际标准历法",
+      "japanese": "和历",
+      "islamic-civil": "伊斯兰希吉来日历",
+      "islamicc": "伊斯兰希吉来日历",
+      "ethioaa": "埃塞俄比亚阿米特阿莱姆日历",
+      "ethiopic-amete-alem": "埃塞俄比亚阿米特阿莱姆日历",
+    },
+    short: {},
+    narrow: {},
+  },
+};
 
-let dn1 = new Intl.DisplayNames("en", {type: "month", calendar: "gregory"});
-assertEq(dn1.of(1), "January");
-assertEq(dn1.resolvedOptions().calendar, "gregory");
+for (let [locale, localeTests] of Object.entries(tests)) {
+  for (let [style, styleTests] of Object.entries(localeTests)) {
+    let dn = new Intl.DisplayNames(locale, {type: "calendar", style});
 
-let dn2 = new Intl.DisplayNames("en", {type: "month", calendar: "hebrew"});
-assertEq(dn2.of(1), "Tishri");
-assertEq(dn2.resolvedOptions().calendar, "hebrew");
+    let resolved = dn.resolvedOptions();
+    assertEq(resolved.locale, locale);
+    assertEq(resolved.style, style);
+    assertEq(resolved.type, "calendar");
+    assertEq(resolved.fallback, "code");
 
-let dn3 = new Intl.DisplayNames("en", {type: "month", calendar: "islamicc"});
-assertEq(dn3.of(1), "Muharram");
-assertEq(dn3.resolvedOptions().calendar, "islamic-civil");
+    let inheritedTests = {...localeTests.long, ...localeTests.short, ...localeTests.narrow};
+    for (let [calendar, expected] of Object.entries({...inheritedTests, ...styleTests})) {
+      assertEq(dn.of(calendar), expected);
+
+      
+      assertEq(dn.of(Object(calendar)), expected);
+    }
+  }
+}
+
+{
+  let dn = new Intl.DisplayNames("en", {type: "calendar"});
+
+  
+  assertThrowsInstanceOf(() => dn.of(), RangeError);
+  assertThrowsInstanceOf(() => dn.of(undefined), RangeError);
+  assertThrowsInstanceOf(() => dn.of(Symbol()), TypeError);
+  assertThrowsInstanceOf(() => dn.of(0), RangeError);
+
+  
+  assertThrowsInstanceOf(() => dn.of("gregorian"), RangeError);
+  assertThrowsInstanceOf(() => dn.of("grëgory"), RangeError);
+  assertThrowsInstanceOf(() => dn.of("grēgory"), RangeError);
+}
+
+
+{
+  let dn1 = new Intl.DisplayNames("en", {type: "calendar"});
+  let dn2 = new Intl.DisplayNames("en", {type: "calendar", fallback: "code"});
+  let dn3 = new Intl.DisplayNames("en", {type: "calendar", fallback: "none"});
+
+  assertEq(dn1.resolvedOptions().fallback, "code");
+  assertEq(dn2.resolvedOptions().fallback, "code");
+  assertEq(dn3.resolvedOptions().fallback, "none");
+
+  
+  assertEq(dn1.of("invalid"), "invalid");
+  assertEq(dn2.of("invalid"), "invalid");
+  assertEq(dn3.of("invalid"), undefined);
+
+  
+  assertEq(dn1.of("INVALID"), "invalid");
+  assertEq(dn2.of("INVALID"), "invalid");
+  assertEq(dn3.of("INVALID"), undefined);
+}
+
+
+{
+  let dn = new Intl.DisplayNames("en", {type: "calendar", fallback: "none"});
+
+  assertEq(dn.of("gregory"), "Gregorian Calendar");
+  assertEq(dn.of("GREGORY"), "Gregorian Calendar");
+}
 
 if (typeof reportCompare === "function")
   reportCompare(true, true);
