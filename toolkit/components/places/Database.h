51ffa5def5eb541139f30395c33754c06b0e20c7
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 07:04:17 +0000

    Backed out changeset dee2affa2f87 (bug 1713073) for causing bc failures in browser_startup_mainthreadio.js. CLOSED TREE
    UltraBlame original commit: d737f398378d7b948d5dce0a2d702c93ac447fcc

--- toolkit/components/places/Database.h
+++ toolkit/components/places/Database.h
@@ -18,7 +18,7 @@
 
 
 
-#define DATABASE_SCHEMA_VERSION 55
+#define DATABASE_SCHEMA_VERSION 54
 
 
 #define TOPIC_PLACES_INIT_COMPLETE "places-init-complete"
@@ -327,7 +327,6 @@ class Database final : public nsIObserver, public nsSupportsWeakReference {
   nsresult MigrateV52Up();
   nsresult MigrateV53Up();
   nsresult MigrateV54Up();
-  nsresult MigrateV55Up();
 
   void MigrateV52OriginFrecencies();
 
