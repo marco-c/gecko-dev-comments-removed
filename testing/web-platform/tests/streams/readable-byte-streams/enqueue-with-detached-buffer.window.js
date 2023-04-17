cccd0ab97337e3423267aa6a85753fd7c5dc5d01
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 06:39:23 +0000

    Bug 1705848 [wpt PR 28557] - Streams: tests for readable byte stream fixes, a=testonly
    Automatic update from web-platform-tests
    Streams: tests for readable byte stream fixes
    
    Follows https://github.com/whatwg/streams/pull/1123.
    --
    
    wpt-commits: 7b29ee36cc22bdad06b4f98df73358ca959fe0a7
    wpt-pr: 28557
    
    UltraBlame original commit: 37a7e6dc64f2da3b5b64b5bac47542d6e3e699d1

--- testing/web-platform/tests/streams/readable-byte-streams/enqueue-with-detached-buffer.window.js
+++ testing/web-platform/tests/streams/readable-byte-streams/enqueue-with-detached-buffer.window.js
@@ -2,19 +2,17 @@ promise_test(async t => {
   const error = new Error('cannot proceed');
   const rs = new ReadableStream({
     type: 'bytes',
-    pull(controller) {
-      t.step(() => {
-        const buffer = controller.byobRequest.view.buffer;
-        
-        postMessage(buffer, '*', [buffer]);
+    pull: t.step_func((controller) => {
+      const buffer = controller.byobRequest.view.buffer;
+      
+      postMessage(buffer, '*', [buffer]);
 
-        
-        assert_throws_js(TypeError, () => controller.enqueue(new Uint8Array([42])));
+      
+      assert_throws_js(TypeError, () => controller.enqueue(new Uint8Array([42])));
 
-        
-        controller.error(error);
-      });
-    }
+      
+      controller.error(error);
+    })
   });
   const reader = rs.getReader({ mode: 'byob' });
   await promise_rejects_exactly(t, error, reader.read(new Uint8Array(1)));
