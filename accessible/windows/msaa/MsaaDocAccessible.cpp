e7e75dfb164f161f574af755b3fbecaadab9f0a9
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 08:00:29 +0000

    Bug 1695116 part 7: Support MSAA accParent, accChildCount and accNavigate for RemoteAccessibles. r=morgan
    Differential Revision: https://phabricator.services.mozilla.com/D116197
    
    UltraBlame original commit: 84745f573a558f562c1529e0a248c26c69c72e03

--- accessible/windows/msaa/MsaaDocAccessible.cpp
+++ accessible/windows/msaa/MsaaDocAccessible.cpp
@@ -59,15 +59,35 @@ IMPL_IUNKNOWN_QUERY_TAIL_INHERITED(ia2AccessibleHypertext)
 STDMETHODIMP
 MsaaDocAccessible::get_accParent(
      IDispatch __RPC_FAR* __RPC_FAR* ppdispParent) {
-  DocAccessible* docAcc = DocAcc();
-  if (!docAcc) {
+  if (!mAcc) {
     return CO_E_OBJNOTCONNECTED;
   }
+
+  if (mAcc->IsRemote()) {
+    MOZ_ASSERT(StaticPrefs::accessibility_cache_enabled_AtStartup());
+    DocAccessibleParent* remoteDoc = mAcc->AsRemote()->AsDoc();
+    if (nsWinUtils::IsWindowEmulationStarted() && remoteDoc->IsTopLevel()) {
+      
+      
+      HWND hwnd = remoteDoc->GetEmulatedWindowHandle();
+      MOZ_ASSERT(hwnd);
+      if (hwnd &&
+          SUCCEEDED(::AccessibleObjectFromWindow(
+              hwnd, OBJID_WINDOW, IID_IAccessible, (void**)ppdispParent))) {
+        return S_OK;
+      }
+    }
+    return MsaaAccessible::get_accParent(ppdispParent);
+  }
+
+  DocAccessible* docAcc = DocAcc();
+  MOZ_ASSERT(docAcc);
 
   
   DocAccessibleChild* ipcDoc = docAcc->IPCDoc();
   if (ipcDoc && static_cast<dom::BrowserChild*>(ipcDoc->Manager())
                         ->GetTopLevelDocAccessibleChild() == ipcDoc) {
+    MOZ_ASSERT(!StaticPrefs::accessibility_cache_enabled_AtStartup());
     
     
     RefPtr<IDispatch> dispParent = ipcDoc->GetEmulatedWindowIAccessible();
