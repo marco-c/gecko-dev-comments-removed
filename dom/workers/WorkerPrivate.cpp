d9aeda2d9d5a5adba67c319df5d6883db3820c98
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 11:13:49 +0000

    Bug 1720568: Be more explicit about the WorkerPrivate self-reference. r=dom-worker-reviewers,asuth
    Differential Revision: https://phabricator.services.mozilla.com/D120630
    
    UltraBlame original commit: bffd853147ad64586988109205a1ef26c3291979

--- dom/workers/WorkerPrivate.cpp
+++ dom/workers/WorkerPrivate.cpp
@@ -2508,6 +2508,11 @@ already_AddRefed<WorkerPrivate> WorkerPrivate::Constructor(
     return nullptr;
   }
 
+  
+  
+  
+  worker->mSelfRef = worker;
+
   worker->EnableDebugger();
 
   MOZ_DIAGNOSTIC_ASSERT(worker->PrincipalIsValid());
@@ -2523,8 +2528,6 @@ already_AddRefed<WorkerPrivate> WorkerPrivate::Constructor(
     aRv.Throw(NS_ERROR_UNEXPECTED);
     return nullptr;
   }
-
-  worker->mSelfRef = worker;
 
   return worker.forget();
 }
