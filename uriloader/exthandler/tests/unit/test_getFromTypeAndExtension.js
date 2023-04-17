7d07fe7225fbd5584162bb847704de35093e9e4b
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 10:58:22 +0000

    Bug 1719598 - fix unicode handling of file extensions in nsExternalHelperAppService, r=mtigley
    Differential Revision: https://phabricator.services.mozilla.com/D120218
    
    UltraBlame original commit: 70d540e8dd27bb7e3b0d383dc37991369e084a9e

---
+++ uriloader/exthandler/tests/unit/test_getFromTypeAndExtension.js
@@ -0,0 +1,17 @@
+
+
+
+"use strict";
+
+add_task(async function test_utf8_extension() {
+  const mimeService = Cc["@mozilla.org/mime;1"].getService(Ci.nsIMIMEService);
+  let someMIME = mimeService.getFromTypeAndExtension(
+    "application/x-gobbledygook",
+    ".тест"
+  );
+  Assert.stringContains(someMIME.description, "тест");
+  
+  if (AppConstants.platform != "macosx" && AppConstants.platform != "android") {
+    Assert.equal(someMIME.primaryExtension, ".тест");
+  }
+});
