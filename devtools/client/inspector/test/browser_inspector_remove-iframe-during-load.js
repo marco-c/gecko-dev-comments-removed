9efedcdff8be2a0b3faebbd53e0b086802ccd594
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 10:39:11 +0000

    Bug 1720785 - Re-enable a few test with bfcacheInParent disabled. r=jdescottes
    Differential Revision: https://phabricator.services.mozilla.com/D120023
    
    UltraBlame original commit: 9baa1f73a119c77f910c63ab7bca3c984fcbf31f

--- devtools/client/inspector/test/browser_inspector_remove-iframe-during-load.js
+++ devtools/client/inspector/test/browser_inspector_remove-iframe-during-load.js
@@ -8,12 +8,6 @@
 const TEST_URL = URL_ROOT + "doc_inspector_remove-iframe-during-load.html";
 
 add_task(async function() {
-  
-  
-  await SpecialPowers.pushPrefEnv({
-    set: [["fission.bfcacheInParent", false]],
-  });
-
   const { inspector, tab } = await openInspectorForURL("about:blank");
   await selectNode("body", inspector);
 
