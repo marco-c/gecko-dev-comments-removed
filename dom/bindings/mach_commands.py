217c19cb365ba306cff44b7076f697879e9df739
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 16:59:05 +0000

    Backed out changeset 53b1fa0faa6d (bug 1696251) for breaking the static-analysis integration. a=backout
    UltraBlame original commit: c38d34be7c3f757510f037fef5a78ea08af32980

--- dom/bindings/mach_commands.py
+++ dom/bindings/mach_commands.py
@@ -9,9 +9,11 @@ import sys
 
 from mach.decorators import (
     CommandArgument,
+    CommandProvider,
     Command,
 )
 
+from mozbuild.base import MachCommandBase
 from mozbuild.util import mkdir
 
 
@@ -21,48 +23,51 @@ def get_test_parser():
     return runtests.get_parser
 
 
-@Command(
-    "webidl-example",
-    category="misc",
-    description="Generate example files for a WebIDL interface.",
-)
-@CommandArgument(
-    "interface", nargs="+", help="Interface(s) whose examples to generate."
-)
-def webidl_example(command_context, interface):
-    from mozwebidlcodegen import BuildSystemWebIDL
+@CommandProvider
+class WebIDLProvider(MachCommandBase):
+    @Command(
+        "webidl-example",
+        category="misc",
+        description="Generate example files for a WebIDL interface.",
+    )
+    @CommandArgument(
+        "interface", nargs="+", help="Interface(s) whose examples to generate."
+    )
+    def webidl_example(self, command_context, interface):
+        from mozwebidlcodegen import BuildSystemWebIDL
 
-    manager = command_context._spawn(BuildSystemWebIDL).manager
-    for i in interface:
-        manager.generate_example_files(i)
+        manager = command_context._spawn(BuildSystemWebIDL).manager
+        for i in interface:
+            manager.generate_example_files(i)
 
+    @Command(
+        "webidl-parser-test",
+        category="testing",
+        parser=get_test_parser,
+        description="Run WebIDL tests (Interface Browser parser).",
+    )
+    def webidl_test(self, command_context, **kwargs):
+        sys.path.insert(
+            0, os.path.join(command_context.topsrcdir, "other-licenses", "ply")
+        )
 
-@Command(
-    "webidl-parser-test",
-    category="testing",
-    parser=get_test_parser,
-    description="Run WebIDL tests (Interface Browser parser).",
-)
-def webidl_test(command_context, **kwargs):
-    sys.path.insert(0, os.path.join(command_context.topsrcdir, "other-licenses", "ply"))
+        
+        
+        mkdir(command_context.topobjdir)
 
-    
-    
-    mkdir(command_context.topobjdir)
+        
+        
+        os.chdir(command_context.topobjdir)
 
-    
-    
-    os.chdir(command_context.topobjdir)
+        if kwargs["verbose"] is None:
+            kwargs["verbose"] = False
 
-    if kwargs["verbose"] is None:
-        kwargs["verbose"] = False
+        
+        
+        
+        
+        sys.path.insert(0, command_context.topobjdir)
 
-    
-    
-    
-    
-    sys.path.insert(0, command_context.topobjdir)
+        import runtests
 
-    import runtests
-
-    return runtests.run_tests(kwargs["tests"], verbose=kwargs["verbose"])
+        return runtests.run_tests(kwargs["tests"], verbose=kwargs["verbose"])
