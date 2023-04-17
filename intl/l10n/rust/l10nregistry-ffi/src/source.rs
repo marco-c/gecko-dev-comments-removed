d883370c98887344453cf7cc6f3a5dbbe6ca017d
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 13:42:59 +0000

    Bug 1726535 - Restore the ja-JP-macos -> ja-JP-mac L10nRegistry path overload. r=platform-i18n-reviewers,jfkthame
    Differential Revision: https://phabricator.services.mozilla.com/D123133
    
    UltraBlame original commit: b43a9e69e3bac4798a2f7b8183c9c041b9ced7eb

--- intl/l10n/rust/l10nregistry-ffi/src/source.rs
+++ intl/l10n/rust/l10nregistry-ffi/src/source.rs
@@ -12,7 +12,7 @@ use nsstring::{nsACString, nsCString};
 use thin_vec::ThinVec;
 use unic_langid::LanguageIdentifier;
 
-use std::{mem, rc::Rc};
+use std::{borrow::Cow, mem, rc::Rc};
 use xpcom::RefPtr;
 
 #[repr(C)]
@@ -22,6 +22,21 @@ pub enum L10nFileSourceStatus {
     EmptyPrePath,
     EmptyResId,
     InvalidLocaleCode,
+}
+
+
+
+
+
+
+
+
+fn get_locale_from_gecko<'s>(input: Cow<'s, str>) -> Cow<'s, str> {
+    if input == "ja-JP-mac" {
+        "ja-JP-macos".into()
+    } else {
+        input
+    }
 }
 
 #[no_mangle]
@@ -41,8 +56,10 @@ pub extern "C" fn l10nfilesource_new(
         return std::ptr::null();
     }
 
-    let locales: Result<Vec<LanguageIdentifier>, _> =
-        locales.iter().map(|l| l.to_utf8().parse()).collect();
+    let locales: Result<Vec<LanguageIdentifier>, _> = locales
+        .iter()
+        .map(|l| get_locale_from_gecko(l.to_utf8()).parse())
+        .collect();
 
     let locales = match locales {
         Ok(locales) => locales,
@@ -84,8 +101,10 @@ pub unsafe extern "C" fn l10nfilesource_new_with_index(
         return std::ptr::null();
     }
 
-    let locales: Result<Vec<LanguageIdentifier>, _> =
-        locales.iter().map(|l| l.to_utf8().parse()).collect();
+    let locales: Result<Vec<LanguageIdentifier>, _> = locales
+        .iter()
+        .map(|l| get_locale_from_gecko(l.to_utf8()).parse())
+        .collect();
 
     let index = if index_length > 0 {
         assert!(!index_elements.is_null());
@@ -142,8 +161,10 @@ pub extern "C" fn l10nfilesource_new_mock(
         return std::ptr::null();
     }
 
-    let locales: Result<Vec<LanguageIdentifier>, _> =
-        locales.iter().map(|l| l.to_utf8().parse()).collect();
+    let locales: Result<Vec<LanguageIdentifier>, _> = locales
+        .iter()
+        .map(|l| get_locale_from_gecko(l.to_utf8()).parse())
+        .collect();
 
     let locales = match locales {
         Ok(locales) => locales,
