6b96dd8040b72aac9d525a6e92294d025f6252f7
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 09:19:25 +0000

    Bug 1713857: Don't set PYTHONEXECUTABLE environment variable r=glandium
    After some testing in `try` and locally, the manual
    `PYTHONEXECUTABLE` definitions shouldn't be needed
    anymore.
    
    There's been some work on Brew's
    Python to improve its `sitecustomize` behaviour.
    The most likely improvement is:
    https://github.com/Homebrew/homebrew-core/pull/65297
    
    However, I'm not fully confident in this change.
    If it fails, it's more likely to affect developers
    than CI. I think it's worth attempting a landing,
    because if the variable is indeed obsolete, then
    deleting it will avoid some spicy "action-at-a-
    distance" behaviour.
    
    Differential Revision: https://phabricator.services.mozilla.com/D117452
    
    UltraBlame original commit: 97158aff906682c23f89dbd2374cce2120afa775

--- python/mozbuild/mozbuild/virtualenv.py
+++ python/mozbuild/mozbuild/virtualenv.py
@@ -223,8 +223,6 @@ class VirtualenvManager(VirtualenvHelper):
 
     def _log_process_output(self, *args, **kwargs):
         env = kwargs.pop("env", None) or os.environ.copy()
-        
-        env.pop("PYTHONEXECUTABLE", None)
         kwargs["env"] = ensure_subprocess_env(env)
 
         if hasattr(self.log_handle, "fileno"):
