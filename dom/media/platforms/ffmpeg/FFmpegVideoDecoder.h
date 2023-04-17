7c12ef0c83bc3e79a09023f3c6e821a91049ebad
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 08:55:06 +0000

    Bug 1715365 [Linux] Move DMABuf video frames allocation from FFmpegVideoFramePool to FFmpegDataDecoder, r=alwu
    - Replace DMABufSurfaceWrapper array by FFmpegVideoFramePool and create it only when DMABuf/VA-API is used.
    - Use FFmpegVideoFramePool to allocate/recycle video frames backed by DMABuf surfaces.
    - Enable SW decoding to DMABuf surfaces only when VA-API is enabled to avoid potential breakage (keep recent state).
    - Disable VA-API / DMABuf decoding when WebRender is not used.
    - Remove DMABufSurfaceWrapper class.
    
    Differential Revision: https://phabricator.services.mozilla.com/D117223
    
    UltraBlame original commit: d6f7f2c7a16c60a18dff7435175efcacfeb30807

--- dom/media/platforms/ffmpeg/FFmpegVideoDecoder.h
+++ dom/media/platforms/ffmpeg/FFmpegVideoDecoder.h
@@ -11,90 +11,10 @@
 #include "FFmpegLibWrapper.h"
 #include "SimpleMap.h"
 #ifdef MOZ_WAYLAND_USE_VAAPI
-#  include "mozilla/LinkedList.h"
-#  include "mozilla/widget/DMABufSurface.h"
+#  include "FFmpegVideoFramePool.h"
 #endif
 
 namespace mozilla {
-
-#ifdef MOZ_WAYLAND_USE_VAAPI
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-template <int V>
-class DMABufSurfaceWrapper {};
-
-template <>
-class DMABufSurfaceWrapper<LIBAV_VER>;
-
-template <>
-class DMABufSurfaceWrapper<LIBAV_VER> final {
- public:
-  DMABufSurfaceWrapper(DMABufSurface* aSurface, FFmpegLibWrapper* aLib);
-  ~DMABufSurfaceWrapper();
-
-  
-  void LockVAAPIData(AVCodecContext* aAVCodecContext, AVFrame* aAVFrame);
-
-  
-  
-  void ReleaseVAAPIData();
-
-  
-  
-  bool IsUsed() const { return mSurface->IsGlobalRefSet(); }
-
-  RefPtr<DMABufSurfaceYUV> GetDMABufSurface() const {
-    return mSurface->GetAsDMABufSurfaceYUV();
-  }
-
-  
-  
-  
-  DMABufSurfaceWrapper(const DMABufSurfaceWrapper&) = delete;
-  const DMABufSurfaceWrapper& operator=(DMABufSurfaceWrapper const&) = delete;
-
- private:
-  const RefPtr<DMABufSurface> mSurface;
-  const FFmpegLibWrapper* mLib;
-  AVBufferRef* mAVHWFramesContext;
-  AVBufferRef* mHWAVBuffer;
-};
-#endif
 
 template <int V>
 class FFmpegVideoDecoder : public FFmpegDataDecoder<V> {};
@@ -170,10 +90,6 @@ class FFmpegVideoDecoder<LIBAV_VER>
   MediaResult CreateImageDMABuf(int64_t aOffset, int64_t aPts,
                                 int64_t aDuration,
                                 MediaDataDecoder::DecodedData& aResults);
-
-  void ReleaseUnusedVAAPIFrames();
-  DMABufSurfaceWrapper<LIBAV_VER>* GetUnusedDMABufSurfaceWrapper();
-  void ReleaseDMABufSurfaces();
 #endif
 
 #ifdef MOZ_WAYLAND_USE_VAAPI
@@ -181,7 +97,7 @@ class FFmpegVideoDecoder<LIBAV_VER>
   bool mEnableHardwareDecoding;
   VADisplay mDisplay;
   bool mUseDMABufSurfaces;
-  nsTArray<DMABufSurfaceWrapper<LIBAV_VER>> mDMABufSurfaces;
+  UniquePtr<VideoFramePool> mVideoFramePool;
 #endif
   RefPtr<KnowsCompositor> mImageAllocator;
   RefPtr<ImageContainer> mImageContainer;
