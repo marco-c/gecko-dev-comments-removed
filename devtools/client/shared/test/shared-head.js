1f0278386814069461cc7a563b1fa6167fde8a1a
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 03:53:26 +0000

    Bug 1707823 - [devtools] Make navigateTo more secure. r=ochameau.
    In the `BrowserTestUtils.browserLoaded` call we use in `navigateTo`, we weren't
    checking for a specific uri to be loaded, which means the Promise could resolve
    on *any* page being loaded.
    This patch makes it more secure by waiting the load event for the uri we navigated to.
    
    Differential Revision: https://phabricator.services.mozilla.com/D113479
    
    UltraBlame original commit: 015d6eaef2cf61aa13479412d5e327dfae78c70e

--- devtools/client/shared/test/shared-head.js
+++ devtools/client/shared/test/shared-head.js
@@ -491,8 +491,10 @@ async function navigateTo(uri, { isErrorPage = false } = {}) {
   const currentBrowsingContextID = browser.browsingContext.id;
   const onBrowserLoaded = BrowserTestUtils.browserLoaded(
     browser,
+    
     false,
-    null,
+    
+    uri,
     isErrorPage
   );
   BrowserTestUtils.loadURI(browser, uri);
