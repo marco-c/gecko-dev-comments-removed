f83a4327d73fdf3e6b9fe1abbfca17aa125ef436
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 05:42:51 +0000

    Bug 1520983 - part 2: Make `IMEInputHandler::InsertTextAsComittingComposition()` use `eContentCommandInsertText` event if there is no composition r=m_kato
    Like the other browsers, we should stop dispatching a set of composition
    events when `insertText:` is called by Emoji picker, accent character
    picker of some Western keyboard layouts.  Then, corresponding
    `beforeinput` event becomes cancelable like the other browsers'.
    
    Differential Revision: https://phabricator.services.mozilla.com/D114827
    
    UltraBlame original commit: 9fd5512f7f4c517ed30c9e586de9a44acbcf3e93

--- widget/cocoa/TextInputHandler.mm
+++ widget/cocoa/TextInputHandler.mm
@@ -12,6 +12,7 @@
 #include "mozilla/AutoRestore.h"
 #include "mozilla/MiscEvents.h"
 #include "mozilla/MouseEvents.h"
+#include "mozilla/StaticPrefs_intl.h"
 #include "mozilla/Telemetry.h"
 #include "mozilla/TextEventDispatcher.h"
 #include "mozilla/TextEvents.h"
@@ -3789,6 +3790,8 @@ void IMEInputHandler::InsertTextAsCommittingComposition(NSAttributedString* aAtt
   nsCocoaUtils::GetStringForNSString([aAttrString string], str);
 
   if (!IsIMEComposing()) {
+    MOZ_DIAGNOSTIC_ASSERT(!str.IsEmpty());
+
     
     
     if (aReplacementRange && aReplacementRange->location != NSNotFound &&
@@ -3796,6 +3799,21 @@ void IMEInputHandler::InsertTextAsCommittingComposition(NSAttributedString* aAtt
       NS_ENSURE_TRUE_VOID(SetSelection(*aReplacementRange));
     }
 
+    if (!StaticPrefs::intl_ime_use_composition_events_for_insert_text()) {
+      
+      
+      
+      
+      WidgetContentCommandEvent insertTextEvent(true, eContentCommandInsertText, mWidget);
+      insertTextEvent.mString = Some(str);
+      DispatchEvent(insertTextEvent);
+      return;
+    }
+
+    
+    
+    
+    
     if (!DispatchCompositionStartEvent()) {
       MOZ_LOG(gLog, LogLevel::Info,
               ("%p IMEInputHandler::InsertTextAsCommittingComposition, "
