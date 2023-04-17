6637bedce4a25bc42ddc07b385be6c88b779b22b
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 22:45:30 +0000

    Bug 1737068 [Linux] Postpone compositor rendering until widget window is ready, r=rmader
    - When GtkCompositorWidget is created, make suspended rendering state as initial one as we can't render to widget window.
      Enable rendering when underlying window is visible (nsWindow::ConfigureGdkWindow) or GtkCompositorWidget is assigned (nsWindow::SetCompositorWidgetDelegate).
    - Use moz_container_wayland_add_initial_draw_callback() at nsWindow::ConfigureGdkWindow() to set Wayland EGL window as it was before.
    - Make moz_container_wayland_add_initial_draw_callback() to run callbacks instantly when mozcontainer is already visible.
    - Implement and use moz_container_wayland_clear_initial_draw_callback() to clear mozcontainer callbacks when it becomes hidden.
    - Rename COMPOSITOR_PAUSED_MISSING_EGL_WINDOW to COMPOSITOR_PAUSED_MISSING_WINDOW as it's used for GLX too.
    - Initially pause rendering for GLX too.
    
    Differential Revision: https://phabricator.services.mozilla.com/D127542
    
    UltraBlame original commit: 88ecfc06a2672fcf016d3a6cd5ec36ddd0d19ecf

--- widget/gtk/nsWindow.cpp
+++ widget/gtk/nsWindow.cpp
@@ -618,7 +618,14 @@ bool nsWindow::AreBoundsSane() {
 void nsWindow::Destroy() {
   if (mIsDestroyed || !mCreated) return;
 
-  LOG("nsWindow::Destroy [%p]\n", (void*)this);
+  LOG("nsWindow::Destroy\n");
+
+  
+  RevokeTransactionIdAllocator();
+
+  WaylandStopVsync();
+  PauseCompositorHiddenWindow();
+
   mIsDestroyed = true;
   mCreated = false;
 
@@ -3734,7 +3741,7 @@ gboolean nsWindow::OnConfigureEvent(GtkWidget* aWidget,
 }
 
 void nsWindow::OnMap() {
-  LOG(("nsWindow::OnMap [%p]\n", (void*)this));
+  LOG("nsWindow::OnMap [%p]\n", (void*)this);
   
   
   
@@ -3748,7 +3755,7 @@ void nsWindow::OnUnrealize() {
   
   
   
-  LOG(("nsWindow::OnUnrealize [%p] GdkWindow %p\n", (void*)this, mGdkWindow));
+  LOG("nsWindow::OnUnrealize [%p] GdkWindow %p\n", (void*)this, mGdkWindow);
   mIsMapped = false;
   ReleaseGdkWindow();
 }
@@ -5078,7 +5085,7 @@ static void GtkWidgetDisableUpdates(GtkWidget* aWidget) {
 }
 
 void nsWindow::ConfigureGdkWindow() {
-  LOG(("nsWindow::ConfigureGdkWindow() [%p]", this));
+  LOG("nsWindow::ConfigureGdkWindow() [%p]", this);
 
   mGdkWindow =
       gtk_widget_get_window(mDrawToContainer ? GTK_WIDGET(mContainer) : mShell);
@@ -5095,14 +5102,13 @@ void nsWindow::ConfigureGdkWindow() {
         mIsTransparent && !mHasAlphaVisual && !mTransparencyBitmapForTitlebar;
     mSurfaceProvider.Initialize(mXWindow, mXVisual, mXDepth, mIsShaped);
 
-    if (mIsTopLevel) {
-      
-      
-      
-      
-      
-      SetCompositorHint(GTK_WIDGET_COMPOSIDED_ENABLED);
-    }
+    
+    
+    
+    
+    
+    SetCompositorHint(GTK_WIDGET_COMPOSIDED_ENABLED);
+
     
     
     XShmQueryExtension(DefaultXDisplay());
@@ -5141,18 +5147,33 @@ void nsWindow::ConfigureGdkWindow() {
   RefreshWindowClass();
 
   if (mCompositorState == COMPOSITOR_PAUSED_INITIALLY) {
-    mCompositorState = COMPOSITOR_PAUSED_MISSING_EGL_WINDOW;
+    mCompositorState = COMPOSITOR_PAUSED_MISSING_WINDOW;
   }
 
-  ResumeCompositorHiddenWindow();
-  WaylandStartVsync();
+  if (GdkIsWaylandDisplay()) {
+#ifdef MOZ_WAYLAND
+    RefPtr<nsWindow> self(this);
+    moz_container_wayland_add_initial_draw_callback(
+        mContainer, [self]() -> void {
+          MOZ_LOG(self->IsPopup() ? gWidgetPopupLog : gWidgetLog,
+                  mozilla::LogLevel::Debug,
+                  ("moz_container_wayland initial create "
+                   "ResumeCompositorHiddenWindow()"));
+          self->ResumeCompositorHiddenWindow();
+          self->WaylandStartVsync();
+        });
+#endif
+  } else {
+    ResumeCompositorHiddenWindow();
+    WaylandStartVsync();
+  }
 
-  LOG(("  finished, new GdkWindow %p XID 0x%lx\n", mGdkWindow,
-       GdkIsX11Display() ? gdk_x11_window_get_xid(mGdkWindow) : 0));
+  LOG("  finished, new GdkWindow %p XID 0x%lx\n", mGdkWindow,
+      GdkIsX11Display() ? gdk_x11_window_get_xid(mGdkWindow) : 0);
 }
 
 void nsWindow::ReleaseGdkWindow() {
-  LOG(("nsWindow::ReleaseGdkWindow() [%p]", this));
+  LOG("nsWindow::ReleaseGdkWindow() [%p]", this);
 
   WaylandStopVsync();
   PauseCompositorHiddenWindow();
@@ -5401,7 +5422,7 @@ nsresult nsWindow::Create(nsIWidget* aParent, nsNativeWidget aNativeParent,
           gtk_window_set_type_hint(GTK_WINDOW(mShell),
                                    GDK_WINDOW_TYPE_HINT_DND);
           mIsDragPopup = true;
-          LOG_POPUP(("nsWindow::Create() Drag popup [%p]\n", this));
+          LOG_POPUP("nsWindow::Create() Drag popup [%p]\n", this);
         } else if (GdkIsX11Display()) {
           
           
@@ -5418,9 +5439,9 @@ nsresult nsWindow::Create(nsIWidget* aParent, nsNativeWidget aNativeParent,
               break;
           }
           gtk_window_set_type_hint(GTK_WINDOW(mShell), gtkTypeHint);
+          LOG_POPUP("nsWindow::Create() popup type %s\n",
+                    GetPopupTypeName().get());
         }
-        LOG_POPUP("nsWindow::Create() popup type %s\n",
-                  GetPopupTypeName().get());
         if (parentnsWindow) {
           LOG_POPUP("    set parent window [%p] %s\n", parentnsWindow,
                     parentnsWindow->mGtkWindowRoleName.get());
@@ -5467,12 +5488,7 @@ nsresult nsWindow::Create(nsIWidget* aParent, nsNativeWidget aNativeParent,
       mContainer = MOZ_CONTAINER(container);
 
       
-      if (mWindowType == eWindowType_invisible) {
-        mCompositorState = COMPOSITOR_PAUSED_INITIALLY;
-      }
-      if (mIsAccelerated && (GdkIsWaylandDisplay() || gfxVars::UseEGL())) {
-        mCompositorState = COMPOSITOR_PAUSED_INITIALLY;
-      }
+      mCompositorState = COMPOSITOR_PAUSED_INITIALLY;
 
       
       
@@ -5691,7 +5707,9 @@ nsresult nsWindow::Create(nsIWidget* aParent, nsNativeWidget aNativeParent,
   LOG("nsWindow [%p] type %d %s\n", (void*)this, mWindowType,
       mIsPIPWindow ? "PIP window" : "");
   LOG("\tmShell %p mContainer %p mGdkWindow %p XID 0x%lx\n", mShell, mContainer,
-      mGdkWindow, GdkIsX11Display() ? gdk_x11_window_get_xid(mGdkWindow) : 0);
+      mGdkWindow,
+      (GdkIsX11Display() && mGdkWindow) ? gdk_x11_window_get_xid(mGdkWindow)
+                                        : 0);
 
   
   if (mGtkWindowAppName.IsEmpty()) {
@@ -5831,7 +5849,7 @@ void nsWindow::NativeMoveResize(bool aMoved, bool aResized) {
 void nsWindow::ResumeCompositorHiddenWindow() {
   MOZ_RELEASE_ASSERT(NS_IsMainThread());
 
-  LOG("nsWindow::ResumeCompositorHiddenWindow\n";
+  LOG("nsWindow::ResumeCompositorHiddenWindow\n");
   if (mIsDestroyed || mCompositorState == COMPOSITOR_ENABLED) {
     LOG("  early quit, mCompositorState = %d\n", mCompositorState);
     return;
@@ -5855,18 +5873,17 @@ void nsWindow::ResumeCompositorHiddenWindow() {
 
 
 void nsWindow::PauseCompositorHiddenWindow() {
-  LOG(("nsWindow::PauseCompositorHiddenWindow [%p]\n", (void*)this));
+  LOG("nsWindow::PauseCompositorHiddenWindow [%p]\n", (void*)this);
 
   
   
   
-  if ((!mIsAccelerated && !gfx::gfxVars::UseWebRenderCompositor()) ||
-      mIsDestroyed) {
-    LOG(("  quit early, compositor state %d", mCompositorState));
+  if (mIsDestroyed || mCompositorState != COMPOSITOR_ENABLED) {
+    LOG("  quit early, compositor state %d", mCompositorState);
     return;
   }
 
-  mCompositorState = COMPOSITOR_PAUSED_MISSING_EGL_WINDOW;
+  mCompositorState = COMPOSITOR_PAUSED_MISSING_WINDOW;
 
   
   
@@ -5900,8 +5917,7 @@ static int WindowResumeCompositor(void* data) {
 void nsWindow::PauseCompositor() {
   bool pauseCompositor = (mWindowType == eWindowType_toplevel) &&
                          mCompositorState == COMPOSITOR_ENABLED &&
-                         mIsAccelerated && mCompositorWidgetDelegate &&
-                         !mIsDestroyed;
+                         mCompositorWidgetDelegate && !mIsDestroyed;
   if (!pauseCompositor) {
     return;
   }
@@ -5968,7 +5984,7 @@ void nsWindow::WaylandStartVsync() {
     return;
   }
 
-  LOG(("nsWindow::WaylandStartVsync() [%p]\n", (void*)this));
+  LOG("nsWindow::WaylandStartVsync() [%p]\n", (void*)this);
 
   if (!mWaylandVsyncSource) {
     mWaylandVsyncSource = new WaylandVsyncSource();
@@ -5994,7 +6010,7 @@ void nsWindow::WaylandStartVsync() {
 void nsWindow::WaylandStopVsync() {
 #ifdef MOZ_WAYLAND
   if (mWaylandVsyncSource) {
-    LOG(("nsWindow::WaylandStopVsync() [%p]\n", (void*)this));
+    LOG("nsWindow::WaylandStopVsync() [%p]\n", (void*)this);
     
     
     WaylandVsyncSource::WaylandDisplay& display =
@@ -8263,6 +8279,7 @@ void nsWindow::SetCompositorWidgetDelegate(CompositorWidgetDelegate* delegate) {
   
   
   if (mCompositorWidgetDelegate && mIsMapped) {
+    mCompositorWidgetDelegate->DisableRendering();
     PauseCompositorHiddenWindow();
     WaylandStopVsync();
   }
@@ -8275,6 +8292,7 @@ void nsWindow::SetCompositorWidgetDelegate(CompositorWidgetDelegate* delegate) {
     
     
     if (mIsMapped) {
+      mCompositorWidgetDelegate->EnableRendering(mXWindow, mIsShaped);
       ResumeCompositorHiddenWindow();
       WaylandStartVsync();
     }
@@ -8872,6 +8890,17 @@ void nsWindow::GetCompositorWidgetInitData(
     mozilla::widget::CompositorWidgetInitData* aInitData) {
   nsCString displayName;
 
+  LOG("nsWindow::GetCompositorWidgetInitData [%p]\n", (void*)this);
+#ifdef MOZ_X11
+  if (GdkIsX11Display() && !mGdkWindow) {
+    LOG("  create mGdkWindow/mXWindow\n");
+    mGdkWindow = gtk_widget_get_window(mDrawToContainer ? GTK_WIDGET(mContainer)
+                                                        : mShell);
+    if (mGdkWindow) {
+      g_object_set_data(G_OBJECT(mGdkWindow), "nsWindow", this);
+      mXWindow = gdk_x11_window_get_xid(mGdkWindow);
+    }
+  }
   if (GdkIsX11Display() && mXWindow != X11None) {
     
     
@@ -8879,6 +8908,7 @@ void nsWindow::GetCompositorWidgetInitData(
     XFlush(display);
     displayName = nsCString(XDisplayString(display));
   }
+#endif
   *aInitData = mozilla::widget::GtkCompositorWidgetInitData(
       (mXWindow != X11None) ? mXWindow : (uintptr_t) nullptr, displayName,
       mIsShaped, GdkIsX11Display(), GetClientSize());
