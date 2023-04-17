cadac74bec078f02779ccec949d429d26266c5d7
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 17:48:40 +0000

    Bug 1730822 - Disable HW-WR for child windows on X11/EGL,
    Currently child windows do not show up in this configuration and it's
    not clear yet what the root cause of the issue is.
    As we want to ship X11/EGL by default in 94, work around the issue
    for now.
    
    Differential Revision: https://phabricator.services.mozilla.com/D127264
    
    UltraBlame original commit: b949f21fe5b9ef1bd497655dd3312ca6902bdd04

--- widget/gtk/nsWindow.cpp
+++ widget/gtk/nsWindow.cpp
@@ -939,6 +939,11 @@ bool nsWindow::WidgetTypeSupportsAcceleration() {
     }
     return HasRemoteContent() && mCompositedScreen;
   }
+  
+  if (mWindowType == eWindowType_child && GdkIsX11Display() &&
+      gfxVars::UseEGL()) {
+    return false;
+  }
 
   return true;
 }
