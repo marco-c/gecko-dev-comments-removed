91bd5675081702f239c66c7f6a24626a57d84276
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 12:03:22 +0000

    Bug 1341265 - Part 5: Add branch64 for (Address, Register64). r=iain
    Add `branch64` with `(Address, Register64)` operands in preparation for a
    later patch of this series.
    
    Differential Revision: https://phabricator.services.mozilla.com/D118971
    
    UltraBlame original commit: b861bb6771c789aefc630db3711602e7abd04fae

--- js/src/jit/MacroAssembler.h
+++ js/src/jit/MacroAssembler.h
@@ -1404,6 +1404,8 @@ class MacroAssembler : public MacroAssemblerSpecific {
   
   inline void branch64(Condition cond, const Address& lhs, Imm64 val,
                        Label* label) PER_ARCH;
+  inline void branch64(Condition cond, const Address& lhs, Register64 rhs,
+                       Label* label) PER_ARCH;
 
   
   
