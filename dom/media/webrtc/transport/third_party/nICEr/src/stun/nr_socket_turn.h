3baf3a8c69407e7184a4a41730bdc815069af806
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 06:58:13 +0000

    Bug 857668: Remove some unneeded non-owning references to nr_socket. r=mjf
    This substantially simplifies the task of replacing the nr_socket a TURN
    context is using.
    
    Differential Revision: https://phabricator.services.mozilla.com/D115281
    
    UltraBlame original commit: 8aaf855abaed18250f7325566eb0a59518f77afe

--- dom/media/webrtc/transport/third_party/nICEr/src/stun/nr_socket_turn.h
+++ dom/media/webrtc/transport/third_party/nICEr/src/stun/nr_socket_turn.h
@@ -41,7 +41,7 @@
 
 
 
-int nr_socket_turn_create(nr_socket *sock, nr_socket **sockp);
+int nr_socket_turn_create(nr_socket **sockp);
 int nr_socket_turn_set_ctx(nr_socket *sock, nr_turn_client_ctx *ctx);
 
 #endif
