f1a69b00d721e4c4074f7ec2447c417a08cda5ff
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 08:38:59 +0000

    Bug 1715819 - Select the text node if exists in TextEditor::SelectEntireDocument r=masayuki
    Differential Revision: https://phabricator.services.mozilla.com/D117444
    
    UltraBlame original commit: dfe6447f4f8a033dd471d920a0665fd9645bdf3c

--- editor/libeditor/TextEditor.cpp
+++ editor/libeditor/TextEditor.cpp
@@ -584,7 +584,6 @@ nsresult TextEditor::InsertWithQuotationsAsSubAction(
 
 nsresult TextEditor::SelectEntireDocument() {
   MOZ_ASSERT(IsEditActionDataAvailable());
-  MOZ_ASSERT(IsTextEditor());
 
   if (NS_WARN_IF(!mInitSucceeded)) {
     return NS_ERROR_NOT_INITIALIZED;
@@ -595,46 +594,21 @@ nsresult TextEditor::SelectEntireDocument() {
     return NS_ERROR_NOT_INITIALIZED;
   }
 
-  
-  
-  if (IsEmpty()) {
-    nsresult rv = SelectionRef().CollapseInLimiter(anonymousDivElement, 0);
-    NS_WARNING_ASSERTION(NS_SUCCEEDED(rv),
-                         "Selection::CollapseInLimiter() failed");
-    return rv;
-  }
-
-  
-  
-
-  
-  nsCOMPtr<nsIContent> childNode;
-  nsresult rv =
-      EditorBase::GetEndChildNode(SelectionRef(), getter_AddRefs(childNode));
-  if (NS_FAILED(rv)) {
-    NS_WARNING("EditorBase::GetEndChildNode() failed");
-    return rv;
-  }
-  if (childNode) {
-    childNode = childNode->GetPreviousSibling();
-  }
-
-  if (childNode &&
-      EditorUtils::IsPaddingBRElementForEmptyLastLine(*childNode)) {
+  RefPtr<Text> text =
+      Text::FromNodeOrNull(anonymousDivElement->GetFirstChild());
+  if (!text) {
     ErrorResult error;
-    SelectionRef().SetStartAndEndInLimiter(
-        RawRangeBoundary(anonymousDivElement, 0u), EditorRawDOMPoint(childNode),
-        error);
+    SelectionRef().CollapseInLimiter(*anonymousDivElement, 0, error);
     NS_WARNING_ASSERTION(!error.Failed(),
                          "Selection::SetStartAndEndInLimiter() failed");
     return error.StealNSResult();
   }
 
-  ErrorResult error;
-  SelectionRef().SelectAllChildren(*anonymousDivElement, error);
-  NS_WARNING_ASSERTION(!error.Failed(),
-                       "Selection::SelectAllChildren() failed");
-  return error.StealNSResult();
+  MOZ_TRY(SelectionRef().SetStartAndEndInLimiter(
+      *text, 0, *text, text->TextDataLength(), eDirNext,
+      nsISelectionListener::SELECTALL_REASON));
+
+  return NS_OK;
 }
 
 EventTarget* TextEditor::GetDOMEventTarget() const { return mEventTarget; }
