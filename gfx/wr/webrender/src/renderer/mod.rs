b5172ee6f49d1655f2e460a216dcaefe0e721543
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 07:34:25 +0000

    Bug 1713486. Fix per context swgl memory reporting. r=lsalzman
    This passes the swgl context down from the renderer
    instead of using the current context. This ensures
    we report all of the contexts and don't double report.
    
    Differential Revision: https://phabricator.services.mozilla.com/D116474
    
    UltraBlame original commit: d1a5c98250fdb482d69993d7f22ec9bfeea3bc63

--- gfx/wr/webrender/src/renderer/mod.rs
+++ gfx/wr/webrender/src/renderer/mod.rs
@@ -110,6 +110,7 @@ use std::{
     cell::RefCell,
     collections::VecDeque,
     f32,
+    ffi::c_void,
     mem,
     num::NonZeroUsize,
     path::PathBuf,
@@ -5142,7 +5143,7 @@ impl Renderer {
     }
 
     
-    pub fn report_memory(&self) -> MemoryReport {
+    pub fn report_memory(&self, swgl: *mut c_void) -> MemoryReport {
         let mut report = MemoryReport::default();
 
         
@@ -5168,7 +5169,7 @@ impl Renderer {
         report += self.texture_upload_pbo_pool.report_memory();
 
         
-        report += self.device.report_memory(self.size_of_ops.as_ref().unwrap());
+        report += self.device.report_memory(self.size_of_ops.as_ref().unwrap(), swgl);
 
         report
     }
