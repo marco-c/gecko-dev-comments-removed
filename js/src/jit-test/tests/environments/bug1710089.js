6ff7b54b071f452bddd9c1b543fc1c65977ec25a
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 05:29:45 +0000

    Bug 1710830: Make testcase work with full parsing r=mgaudet
    Each `let` apparently creates a new hop in the emitter that isn't reflected in the number of hops seen by WarpBuilder. The problem only shows up when full parsing. The simplest fix is to convert `let` to `var`; the resulting testcase still triggers the original bug in WarpBuilder, but doesn't break code coverage builds.
    
    Differential Revision: https://phabricator.services.mozilla.com/D115072
    
    UltraBlame original commit: 2333c162dd5f2e6ebba230cb7239985905819095

--- js/src/jit-test/tests/environments/bug1710089.js
+++ js/src/jit-test/tests/environments/bug1710089.js
@@ -22,10 +22,10 @@ var iters = 250;
 var src = "return top_level_var + x0; "
 
 for (var i = 0; i < iters; i++) {
-  var def = "let x" + i + " = x" + (i+1) + "; ";
+  var def = "var x" + i + " = x" + (i+1) + "; ";
   src = "function f" + i + "() { " + def + src + "} return f" + i + "();"
 }
-src = "let x" + iters + " = 0;" + src;
+src = "var x" + iters + " = 0;" + src;
 src = "var top_level_var = 42; " + src;
 
 var outer = Function(src);
