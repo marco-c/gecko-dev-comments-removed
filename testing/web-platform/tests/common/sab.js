c258a7ef4b4ce4cfe7ff167b249af115aa6aa8e1
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 05:58:04 +0000

    Bug 1711225 [wpt PR 28998] - Handle WebAssembly.Memory throwing in common/sab.js, a=testonly
    Automatic update from web-platform-tests
    Handle WebAssembly.Memory throwing in common/sab.js
    
    See https://bugs.webkit.org/show_bug.cgi?id=220364 for more detail
    
    --
    
    wpt-commits: 07784dcb10a919a492a4bd4e0683425844010a75
    wpt-pr: 28998
    
    UltraBlame original commit: 6dbbea8395a0ee7271f8775358d1c513350032ec

--- testing/web-platform/tests/common/sab.js
+++ testing/web-platform/tests/common/sab.js
@@ -1,11 +1,16 @@
 const createBuffer = (() => {
   
-  const sabConstructor = new WebAssembly.Memory({ shared:true, initial:0, maximum:0 }).buffer.constructor;
+  let sabConstructor;
+  try {
+    sabConstructor = new WebAssembly.Memory({ shared:true, initial:0, maximum:0 }).buffer.constructor;
+  } catch(e) {
+    sabConstructor = null;
+  }
   return (type, length) => {
     if (type === "ArrayBuffer") {
       return new ArrayBuffer(length);
     } else if (type === "SharedArrayBuffer") {
-      if (sabConstructor.name !== "SharedArrayBuffer") {
+      if (sabConstructor && sabConstructor.name !== "SharedArrayBuffer") {
         throw new Error("WebAssembly.Memory does not support shared:true");
       }
       return new sabConstructor(length);
