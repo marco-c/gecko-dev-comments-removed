c7ff68ba71b25c15e2bd390824c5b2208049c28d
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 23:47:59 +0000

    Backed out changeset a045ce98e572 (bug 1738245) for causing xpcshell failures on test_MigrationUtils_timedRetry.js. CLOSED TREE
    UltraBlame original commit: 2d796003e362df8d7201b0df0d05e5d11718cf98

--- browser/components/migration/tests/unit/test_Chrome_bookmarks.js
+++ browser/components/migration/tests/unit/test_Chrome_bookmarks.js
@@ -57,14 +57,18 @@ async function testBookmarks(migratorKey, subDirs) {
   while (subDirs.length) {
     target.append(subDirs.shift());
   }
-
-  await IOUtils.makeDirectory(target.path, {
-    createAncestor: true,
+  
+  
+  
+  
+  const { OS } = ChromeUtils.import("resource://gre/modules/osfile.jsm");
+  await OS.File.makeDir(target.path, {
+    from: rootDir.parent.path,
     ignoreExisting: true,
   });
 
   target.append("Bookmarks");
-  await IOUtils.remove(target.path, { ignoreAbsent: true });
+  await OS.File.remove(target.path, { ignoreAbsent: true });
 
   let bookmarksData = {
     roots: { bookmark_bar: { children: [] }, other: { children: [] } },
@@ -104,7 +108,9 @@ async function testBookmarks(migratorKey, subDirs) {
     }
   }
 
-  await IOUtils.writeJSON(target.path, bookmarksData);
+  await OS.File.writeAtomic(target.path, JSON.stringify(bookmarksData), {
+    encoding: "utf-8",
+  });
 
   let migrator = await MigrationUtils.getMigrator(migratorKey);
   
