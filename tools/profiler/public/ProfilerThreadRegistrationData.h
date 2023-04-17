980cbb6cb2696784c68a1c29c5017848f367bf04
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 14:08:48 +0000

    Bug 1716959 - On-thread sampling uses a per-thread JS frame buffer that's only allocated when there's also a JSContext - r=canaltinova
    MergeStack requires a fairly large buffer to store JS frames, too big to be allocated on the stack without risking a stack overflow.
    Until now, there was only one buffer, stored in CorePS, and only accessible while holding the Profiler gPSMutex.
    
    Now each thread that has a JSContext, also has its own JS frame buffer, which is accessible on the thread without needing any lock.
    The Profiler's Sampler still uses the CorePS buffer for its periodic sampling, but it won't prevent parallel on-thread sampling anymore.
    
    The appropriate buffer is passed to ExtractJsFrames and then MergeStacks.
    MergeStacks accepts a null pointer, which happens on threads where there is no JSContext, and therefore no JS to sample.
    
    Differential Revision: https://phabricator.services.mozilla.com/D122087
    
    UltraBlame original commit: 9fb7bba8eec3aa600363ca080dbdeb0b5b42ade0

--- tools/profiler/public/ProfilerThreadRegistrationData.h
+++ tools/profiler/public/ProfilerThreadRegistrationData.h
@@ -37,6 +37,7 @@
 #ifndef ProfilerThreadRegistrationData_h
 #define ProfilerThreadRegistrationData_h
 
+#include "js/ProfilingFrameIterator.h"
 #include "js/ProfilingStack.h"
 #include "mozilla/Atomics.h"
 #include "mozilla/MemoryReporting.h"
@@ -68,6 +69,10 @@ class ThreadRegistrationData {
   size_t SizeOfIncludingThis(MallocSizeOf aMallocSizeOf) const {
     return aMallocSizeOf(this) + SizeOfExcludingThis(aMallocSizeOf);
   }
+
+  static constexpr size_t MAX_JS_FRAMES = 1024;
+  using JsFrame = JS::ProfilingFrameIterator::Frame;
+  using JsFrameBuffer = JsFrame[MAX_JS_FRAMES];
 
   
  protected:
@@ -104,6 +109,10 @@ class ThreadRegistrationData {
   
   
   JSContext* mJSContext = nullptr;
+
+  
+  
+  JsFrame* mJsFrameBuffer = nullptr;
 
   
   
@@ -352,6 +361,9 @@ class ThreadRegistrationUnlockedReaderAndAtomicRWOnThread
   
 
   [[nodiscard]] JSContext* GetJSContext() const { return mJSContext; }
+
+  
+  [[nodiscard]] JsFrame* GetJsFrameBuffer() const { return mJsFrameBuffer; }
 
  protected:
   ThreadRegistrationUnlockedReaderAndAtomicRWOnThread(const char* aName,
