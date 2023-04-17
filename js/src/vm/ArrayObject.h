096707cf899f46c828213f2f70efc3a2664619ca
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 13:13:15 +0000

    Bug 1725348 part 10 - Inline createArrayInternal in its sole caller. r=jonco
    Depends on D122429
    
    Differential Revision: https://phabricator.services.mozilla.com/D122430
    
    UltraBlame original commit: e74a2be001f29ff68af1d1fad2b6fdc884d75a4b

--- js/src/vm/ArrayObject.h
+++ js/src/vm/ArrayObject.h
@@ -47,15 +47,6 @@ class ArrayObject : public NativeObject {
                                          HandleShape shape, uint32_t length,
                                          AutoSetNewObjectMetadata& metadata,
                                          gc::AllocSite* site = nullptr);
-
- private:
-  
-  static inline ArrayObject* createArrayInternal(JSContext* cx,
-                                                 gc::AllocKind kind,
-                                                 gc::InitialHeap heap,
-                                                 HandleShape shape,
-                                                 AutoSetNewObjectMetadata&,
-                                                 gc::AllocSite* site = nullptr);
 };
 
 }  
