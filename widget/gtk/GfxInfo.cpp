1eadb3e1e25b9e2ae993eb9281ad6fdc82b44f70
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 16:53:18 +0000

    Bug 1730671 - Enable HW-WR on Xwayland in sync with EGL, r=aosmond
    With EGL enabled by default (bug 1695933) and the biggest blocker,
    bug 1635186, apparently not being reproducible anymore on recent
    distributions, lets finally enable HW-WR on Xwayland by default.
    
    Differential Revision: https://phabricator.services.mozilla.com/D125530
    
    UltraBlame original commit: a16b0b63d300a81ae9c3eaa8260bce7fdb42b0f1

--- widget/gtk/GfxInfo.cpp
+++ widget/gtk/GfxInfo.cpp
@@ -758,17 +758,25 @@ const nsTArray<GfxDriverInfo>& GfxInfo::GetGfxDriverInfo() {
         V(0, 0, 0, 0), "FEATURE_FAILURE_WEBRENDER_BUG_1673939",
         "https://gitlab.freedesktop.org/mesa/mesa/-/issues/3720");
 
-#ifndef EARLY_BETA_OR_EARLIER
+    
+    
     
     
     APPEND_TO_DRIVER_BLOCKLIST_EXT(
         OperatingSystem::Linux, ScreenSizeStatus::All, BatteryStatus::All,
-        DesktopEnvironment::All, WindowProtocol::XWayland, DriverVendor::All,
-        DeviceFamily::All, nsIGfxInfo::FEATURE_WEBRENDER,
-        nsIGfxInfo::FEATURE_BLOCKED_DEVICE, DRIVER_COMPARISON_IGNORED,
-        V(0, 0, 0, 0), "FEATURE_FAILURE_WEBRENDER_BUG_1635186",
-        "https://bugzilla.mozilla.org/show_bug.cgi?id=1635186");
-#endif
+        DesktopEnvironment::All, WindowProtocol::XWayland,
+        DriverVendor::MesaAll, DeviceFamily::All, nsIGfxInfo::FEATURE_WEBRENDER,
+        nsIGfxInfo::FEATURE_BLOCKED_DRIVER_VERSION, DRIVER_LESS_THAN,
+        V(21, 0, 0, 0), "FEATURE_FAILURE_WEBRENDER_BUG_1635186",
+        "Mesa 21.0.0.0");
+
+    APPEND_TO_DRIVER_BLOCKLIST_EXT(
+        OperatingSystem::Linux, ScreenSizeStatus::All, BatteryStatus::All,
+        DesktopEnvironment::All, WindowProtocol::XWayland,
+        DriverVendor::NonMesaAll, DeviceFamily::NvidiaAll,
+        nsIGfxInfo::FEATURE_WEBRENDER,
+        nsIGfxInfo::FEATURE_BLOCKED_DRIVER_VERSION, DRIVER_LESS_THAN,
+        V(470, 0, 0, 0), "FEATURE_FAILURE_WEBRENDER_BUG_1635186", "470.0.0");
 
     
     
