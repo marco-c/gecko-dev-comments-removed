d5473248affde3b2be3ad2c79c785a1a5dc667a9
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 07:28:22 +0000

    Bug 1711648 - Move all device rects to the Box2D representations. r=jrmuizel
    Differential Revision: https://phabricator.services.mozilla.com/D116401
    
    UltraBlame original commit: 9cf4e612dbac8a13f26962c724b41db9cdc6960f

--- gfx/webrender_bindings/RenderCompositorD3D11SWGL.cpp
+++ gfx/webrender_bindings/RenderCompositorD3D11SWGL.cpp
@@ -227,11 +227,10 @@ bool RenderCompositorD3D11SWGL::TileD3D11::Map(wr::DeviceIntRect aDirtyRect,
       return false;
     }
 
-    *aData =
-        map.mData + aValidRect.origin.y * map.mStride + aValidRect.origin.x * 4;
+    *aData = map.mData + aValidRect.min.y * map.mStride + aValidRect.min.x * 4;
     *aStride = map.mStride;
-    mValidRect = gfx::Rect(aValidRect.origin.x, aValidRect.origin.y,
-                           aValidRect.size.width, aValidRect.size.height);
+    mValidRect = gfx::Rect(aValidRect.min.x, aValidRect.min.y,
+                           aValidRect.width(), aValidRect.height());
     return true;
   }
 
@@ -293,13 +292,12 @@ bool RenderCompositorD3D11SWGL::TileD3D11::Map(wr::DeviceIntRect aDirtyRect,
   
   
   *aData = (uint8_t*)mappedSubresource.pData +
-           aValidRect.origin.y * mappedSubresource.RowPitch +
-           aValidRect.origin.x * 4;
+           aValidRect.min.y * mappedSubresource.RowPitch + aValidRect.min.x * 4;
   *aStride = mappedSubresource.RowPitch;
 
   
-  mValidRect = gfx::Rect(aValidRect.origin.x, aValidRect.origin.y,
-                         aValidRect.size.width, aValidRect.size.height);
+  mValidRect = gfx::Rect(aValidRect.min.x, aValidRect.min.y, aValidRect.width(),
+                         aValidRect.height());
 
   return true;
 }
