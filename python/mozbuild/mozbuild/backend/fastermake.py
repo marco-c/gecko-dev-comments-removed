91f591dbdae93d887010deaf0842107db912ecd4
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 02:00:55 +0000

    Bug 1391106 - Support `mach watch` for non-artifact builds. r=firefox-build-system-reviewers,mhentges
    I manually compared `$TOPOBJDIR/faster/unified_install_dist_bin` for
    an artifact and a non-artifact build, and found only a few
    pre-processing differences.  I can't recall why this wasn't supported
    initially; I think it used to be that manifests overlapped and that
    the interaction with the RecursiveMake backend was more complicated.
    But I see no reason why this shouldn't work, and would like to get
    some folks to test it in the wild.
    
    Differential Revision: https://phabricator.services.mozilla.com/D110311
    
    UltraBlame original commit: 29f712502e865c9b07b8dad25e52ef3221128bd3

--- python/mozbuild/mozbuild/backend/fastermake.py
+++ python/mozbuild/mozbuild/backend/fastermake.py
@@ -258,22 +258,21 @@ class FasterMakeBackend(MakeBackend, PartialBackend):
 
         
         
-        if self.environment.is_artifact_build:
-            unified_manifest = InstallManifest()
-            for base, install_manifest in six.iteritems(self._install_manifests):
-                
-                assert base.startswith("dist/bin")
-                base = base[len("dist/bin") :]
-                if base and base[0] == "/":
-                    base = base[1:]
-                unified_manifest.add_entries_from(install_manifest, base=base)
+        unified_manifest = InstallManifest()
+        for base, install_manifest in six.iteritems(self._install_manifests):
+            
+            assert base.startswith("dist/bin")
+            base = base[len("dist/bin") :]
+            if base and base[0] == "/":
+                base = base[1:]
+            unified_manifest.add_entries_from(install_manifest, base=base)
 
-            with self._write_file(
-                mozpath.join(
-                    self.environment.topobjdir, "faster", "unified_install_dist_bin"
-                )
-            ) as fh:
-                unified_manifest.write(fileobj=fh)
+        with self._write_file(
+            mozpath.join(
+                self.environment.topobjdir, "faster", "unified_install_dist_bin"
+            )
+        ) as fh:
+            unified_manifest.write(fileobj=fh)
 
         for obj in self._generated_files:
             for stmt in self._format_statements_for_generated_file(obj, "default"):
