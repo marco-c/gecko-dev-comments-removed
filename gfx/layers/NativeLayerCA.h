1bc948acbfe9e10595561d361fb6d045c35937d5
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 20:02:07 +0000

    Backed out changeset 9d531fd67a5f (bug 1736446) for causing failures at Mutex_posix.cpp. CLOSED TREE
    UltraBlame original commit: ae75b0ef474c9c5a777f42ba43150b149b5cfff1

--- gfx/layers/NativeLayerCA.h
+++ gfx/layers/NativeLayerCA.h
@@ -240,6 +240,7 @@ class NativeLayerCA : public NativeLayer {
   void AttachExternalImage(wr::RenderTextureHost* aExternalImage) override;
 
   bool IsVideo();
+  bool ShouldSpecializeVideo();
   void SetRootWindowIsFullscreen(bool aFullscreen);
 
  protected:
@@ -295,8 +296,6 @@ class NativeLayerCA : public NativeLayer {
 
   Maybe<SurfaceWithInvalidRegion> GetUnusedSurfaceAndCleanUp(
       const MutexAutoLock& aProofOfLock);
-
-  bool ShouldSpecializeVideo(const MutexAutoLock& aProofOfLock);
 
   
   struct Representation {
