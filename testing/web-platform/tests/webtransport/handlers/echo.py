8b239f9d94d9cb37dcd31773d476f4c334897a1d
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 17:57:43 +0000

    Bug 1732017 [wpt PR 30789] - Add Echo Stream WPTs for WebTransport, a=testonly
    Automatic update from web-platform-tests
    Add Echo Stream WPTs for WebTransport (#30789)
    
    This CL adds 3 test cases:
    - echo data on client-initiated bidirectional streams
    - echo data on server-initiated bidirectional streams
    - echo data using client and server-initiated unidirectional streams
    
    The echo server handler has also been modified to add support for
    creating server-initiated bidirectional streams when a WebTransport
    session is established. Furthermore, when stream data is received
    from a client-initiated unidirectional stream, the handler now keeps
    a record of incoming and outgoing unidirectional streams, and creates
    a new unidirectional stream when needed to echo back data to the
    client. The unidirectional streams are mapped using the session's id
    and incoming stream id as keys.
    
    Bug: 1201569
    Change-Id: Iaa7d412d090be96c2ad42da2cad416e5cc60a659
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3159808
    Reviewed-by: Kenichi Ishibashi <bashichromium.org>
    Reviewed-by: Adam Rice <riceachromium.org>
    Reviewed-by: Yutaka Hirano <yhiranochromium.org>
    Commit-Queue: Nidhi Jaju <nidhijajuchromium.org>
    Cr-Commit-Position: refs/heads/main{#922452}
    
    Co-authored-by: Nidhi Jaju <nidhijajuchromium.org>
    --
    
    wpt-commits: 6221566f214a8c67549c21e5e9cbf5232b2593ff
    wpt-pr: 30789
    
    UltraBlame original commit: 09dab0189bd8c73766581039366890bf834f34a9

--- testing/web-platform/tests/webtransport/handlers/echo.py
+++ testing/web-platform/tests/webtransport/handlers/echo.py
@@ -1,7 +1,32 @@
-def stream_data_received(session, stream_id: int, data: bytes, stream_ended: bool):
+streams_dict = {}
+
+
+def session_established(session):
+    
+    
+    
+    session.create_bidirectional_stream()
+
+
+def stream_data_received(session,
+                         stream_id: int,
+                         data: bytes,
+                         stream_ended: bool):
+    
+    
     if session.stream_is_unidirectional(stream_id):
+        if (session.session_id, stream_id) not in streams_dict.keys():
+            new_stream_id = session.create_unidirectional_stream()
+            streams_dict[(session.session_id, stream_id)] = new_stream_id
+        session.send_stream_data(streams_dict[(session.session_id, stream_id)],
+                                 data,
+                                 end_stream=stream_ended)
+        if (stream_ended):
+          del streams_dict[(session.session_id, stream_id)]
         return
-    session.send_stream_data(stream_id, data)
+    
+    
+    session.send_stream_data(stream_id, data, end_stream=stream_ended)
 
 
 def datagram_received(session, data: bytes):
