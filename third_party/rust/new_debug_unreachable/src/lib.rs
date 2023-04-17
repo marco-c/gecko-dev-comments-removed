3f661ba60439f179a462501fd1e2f1b1916843a0
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 09:04:46 +0000

    Bug 1716518 - Upgrade new_debug_unreachable to v1.0.4. r=emilio
    This removes the unreachable crate.
    
    Differential Revision: https://phabricator.services.mozilla.com/D117820
    
    UltraBlame original commit: 2fecc3d32423af6f7bca380a2462d1e5ba2ec2a3

--- third_party/rust/new_debug_unreachable/src/lib.rs
+++ third_party/rust/new_debug_unreachable/src/lib.rs
@@ -4,20 +4,32 @@
 
 
 
-extern crate unreachable;
-
-#[doc(hidden)]
-pub use unreachable::unreachable as __unreachable;
+#[doc(hidden)] pub mod _internal { pub use core::hint::unreachable_unchecked; }
 
 #[macro_export]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 macro_rules! debug_unreachable {
     () => { debug_unreachable!("entered unreachable code") };
     ($e:expr) => {
         if cfg!(debug_assertions) {
-            panic!($e);
+            panic!($e)
         } else {
-            $crate::__unreachable()
+            $crate::_internal::unreachable_unchecked()
         }
     }
 }
