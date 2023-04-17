2489630bafa1900aa4eeae5e8b13cdeca2731e8e
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 09:01:02 +0000

    Bug 1716518 - Upgrade atomic to v0.4.6. r=emilio
    Differential Revision: https://phabricator.services.mozilla.com/D117759
    
    UltraBlame original commit: 5e44d03a37e0765c48d9ebde8d8d6a0310000977

--- third_party/rust/atomic/src/lib.rs
+++ third_party/rust/atomic/src/lib.rs
@@ -36,7 +36,7 @@
     feature = "nightly", feature(const_fn, cfg_target_has_atomic, atomic_min_max)
 )]
 
-#[cfg(test)]
+#[cfg(any(test, feature = "std"))]
 #[macro_use]
 extern crate std;
 
@@ -45,6 +45,9 @@ pub use core::sync::atomic::{fence, Ordering};
 
 use core::cell::UnsafeCell;
 use core::fmt;
+
+#[cfg(feature = "std")]
+use std::panic::RefUnwindSafe;
 
 mod fallback;
 mod ops;
@@ -57,6 +60,15 @@ pub struct Atomic<T: Copy> {
 
 
 unsafe impl<T: Copy + Send> Sync for Atomic<T> {}
+
+
+
+
+
+
+
+#[cfg(feature = "std")]
+impl<T: Copy + RefUnwindSafe> RefUnwindSafe for Atomic<T> {}
 
 impl<T: Copy + Default> Default for Atomic<T> {
     #[inline]
