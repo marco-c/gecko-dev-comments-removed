4958a2cf961fc3c86d6e9d9845a23b5df04aec50
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 18:59:25 +0000

    Bug 1730254: Use 'trunc' instead of 'floor' to resolve percentage CSS length values. r=emilio
    trunc() is the same as floor() for positive values -- both round towards zero.
    So this won't change behavior in that part of the number-line.
    
    For negative values (e.g. negative CSS margins), floor() rounds away from zero
    (in the direction of more-massive negative numbers), whereas trunc() will round
    towards zero.
    
    The trunc() behavior feels more consistent and symmetric, and it happens to be
    what WPT test minmax-length-percent-computed.html requires in order to pass,
    due to an extremely-small amount of floating point imprecision that we get when
    we resolve 15% against 400px. You can see this imprecision in command-line gdb:
        (gdb) p (float).15*400*60
        $1 = 3600.00024
    
    For positive 15%, floor() strips away this fractional imprecision and we end up
    with the intended result of 3600 = 60px; but for negative 15%, floor() promotes
    the fractional part to produce an extra app unit of magnitude (i.e. we get
    -3601 = -60.0167px).  trunc() discards it and gives us -3600 = -60px, which is
    symmetric with the behavior for positive values and matches what we'd expect.
    
    Differential Revision: https://phabricator.services.mozilla.com/D127914
    
    UltraBlame original commit: f625e5088db86abcccdeb3500d7f0f7594759e4b

--- layout/style/ServoStyleConstsInlines.h
+++ layout/style/ServoStyleConstsInlines.h
@@ -762,13 +762,17 @@ nscoord LengthPercentage::Resolve(T aPercentageGetter, U aRounder) const {
   return AsCalc().node.Resolve(basis, aRounder);
 }
 
+
+
 nscoord LengthPercentage::Resolve(nscoord aPercentageBasis) const {
-  return Resolve([=] { return aPercentageBasis; }, NSToCoordFloorClamped);
+  return Resolve([=] { return aPercentageBasis; },
+                 static_cast<nscoord (*)(float)>(NSToCoordTruncClamped));
 }
 
 template <typename T>
 nscoord LengthPercentage::Resolve(T aPercentageGetter) const {
-  return Resolve(aPercentageGetter, NSToCoordFloorClamped);
+  return Resolve(aPercentageGetter,
+                 static_cast<nscoord (*)(float)>(NSToCoordTruncClamped));
 }
 
 template <typename T>
