a74440b0b98ce7341136c9bb947ad56eaf02600f
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 17:42:02 +0000

    Bug 1733109 - 'auto cdmProxy' can be declared as 'auto *cdmProxy' in HTMLMediaElement.cpp. r=alwu
    Depends on D126944
    
    Differential Revision: https://phabricator.services.mozilla.com/D126945
    
    UltraBlame original commit: 6f13eb57901f73bfbca03c6b23851b8a79fdf0d5

--- dom/html/HTMLMediaElement.cpp
+++ dom/html/HTMLMediaElement.cpp
@@ -6943,7 +6943,7 @@ bool HTMLMediaElement::AttachNewMediaKeys() {
 
   
   if (mIncomingMediaKeys) {
-    auto cdmProxy = mIncomingMediaKeys->GetCDMProxy();
+    auto* cdmProxy = mIncomingMediaKeys->GetCDMProxy();
     if (!cdmProxy) {
       SetCDMProxyFailure(MediaResult(
           NS_ERROR_DOM_INVALID_STATE_ERR,
