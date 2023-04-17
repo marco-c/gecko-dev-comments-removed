0ae93d5afeaf9e4cc98142ec83d48f0be085c20e
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 16:14:10 +0000

    Bug 1558793 - Remove invalid comment and assertion in nsHtml5TreeOperation::SetFormElement; r=hsivonen
    Per https://html.spec.whatwg.org/#form-associated-element, img is a form-associated
    element, and we do handle the img case well.
    
    Differential Revision: https://phabricator.services.mozilla.com/D125754
    
    UltraBlame original commit: 1c5d2eecb0f2b6d528b5d232a4745aff904a945e

--- parser/html/nsHtml5TreeOperation.cpp
+++ parser/html/nsHtml5TreeOperation.cpp
@@ -639,22 +639,16 @@ nsIContent* nsHtml5TreeOperation::CreateMathMLElement(
 
 void nsHtml5TreeOperation::SetFormElement(nsIContent* aNode,
                                           nsIContent* aParent) {
-  nsCOMPtr<nsIFormControl> formControl(do_QueryInterface(aNode));
-  RefPtr<dom::HTMLImageElement> domImageElement =
-      dom::HTMLImageElement::FromNodeOrNull(aNode);
-  
-  
-  
-  
   RefPtr<dom::HTMLFormElement> formElement =
       dom::HTMLFormElement::FromNodeOrNull(aParent);
   NS_ASSERTION(formElement,
                "The form element doesn't implement HTMLFormElement.");
-  
+  nsCOMPtr<nsIFormControl> formControl(do_QueryInterface(aNode));
   if (formControl &&
       !aNode->AsElement()->HasAttr(kNameSpaceID_None, nsGkAtoms::form)) {
     formControl->SetForm(formElement);
-  } else if (domImageElement) {
+  } else if (HTMLImageElement* domImageElement =
+                 dom::HTMLImageElement::FromNodeOrNull(aNode)) {
     domImageElement->SetForm(formElement);
   }
 }
