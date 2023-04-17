bb57012768353b6f2b3f86cf6d9782af381c2989
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 04:51:08 +0000

    Bug 1709887: Add missing #define in mingw builds. r=xpcom-reviewers,nika
    Differential Revision: https://phabricator.services.mozilla.com/D114569
    
    UltraBlame original commit: 4b8940a8a49e38d9abe2de8fec6b36b522e1b9f3

--- xpcom/io/nsLocalFileWin.cpp
+++ xpcom/io/nsLocalFileWin.cpp
@@ -82,6 +82,11 @@ using mozilla::FilePreferences::kPathSeparator;
 #  define DRIVE_REMOTE 4
 #endif
 
+
+#ifndef ERROR_DEVICE_HARDWARE_ERROR
+#  define ERROR_DEVICE_HARDWARE_ERROR 483L
+#endif
+
 namespace {
 
 nsresult NewLocalFile(const nsAString& aPath, bool aUseDOSDevicePathSyntax,
