a1794166380a230492f611c2176341f7a903d250
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 09:53:11 +0000

    Bug 1719254 - [geckodriver] Remove "webdriver.log.level" preference in favor of "remote.log.level". r=webdriver-reviewers,jgraham
    Differential Revision: https://phabricator.services.mozilla.com/D119169
    
    UltraBlame original commit: 27cbb13d5199bec57cab3a979f21f595a3cb8bc3

--- testing/geckodriver/src/browser.rs
+++ testing/geckodriver/src/browser.rs
@@ -225,11 +225,10 @@ fn set_prefs(
     }
 
     prefs.insert("marionette.port", Pref::new(port));
-    prefs.insert("webdriver.log.level", logging::max_level().into());
+    prefs.insert("remote.log.level", logging::max_level().into());
 
     
     prefs.insert("marionette.log.level", logging::max_level().into());
-    prefs.insert("remote.log.level", logging::max_level().into());
 
     prefs.write().map_err(|e| {
         WebDriverError::new(
@@ -299,12 +298,12 @@ mod tests {
     
     
     #[test]
-    fn test_webdriver_log_level() {
+    fn test_remote_log_level() {
         let mut profile = Profile::new().unwrap();
         set_prefs(2828, &mut profile, false, vec![], false).ok();
         let user_prefs = profile.user_prefs().unwrap();
 
-        let pref = user_prefs.get("webdriver.log.level").unwrap();
+        let pref = user_prefs.get("remote.log.level").unwrap();
         let value = match pref.value {
             PrefValue::String(ref s) => s,
             _ => panic!(),
