6da4c98eac02092f2ae501875846a18d9e623c84
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 17:19:02 +0000

    Bug 1732208 - Silence the unused-but-set-variable warning in platform-linux-android.cpp. r=gerald
    platform-linux-android.cpp:199:9: error: variable 'r' set but not used [-Werror,-Wunused-but-set-variable]
        int r = sem_init(&mMessage2, /* pshared */ 0, 0);
            ^
    platform-linux-android.cpp:206:9: error: variable 'r' set but not used [-Werror,-Wunused-but-set-variable]
     not used [-Werror,-Wunused-but-set-variable]
        int r = sem_destroy(&mMessage2);
            ^
    
    Differential Revision: https://phabricator.services.mozilla.com/D126459
    
    UltraBlame original commit: bf67b9659820b0cc65419a478931835cb44f1336

--- tools/profiler/core/platform-linux-android.cpp
+++ tools/profiler/core/platform-linux-android.cpp
@@ -194,6 +194,7 @@ struct SigHandlerCoordinator {
     r |= sem_init(&mMessage3,  0, 0);
     r |= sem_init(&mMessage4,  0, 0);
     MOZ_ASSERT(r == 0);
+    (void)r;
   }
 
   ~SigHandlerCoordinator() {
@@ -201,6 +202,7 @@ struct SigHandlerCoordinator {
     r |= sem_destroy(&mMessage3);
     r |= sem_destroy(&mMessage4);
     MOZ_ASSERT(r == 0);
+    (void)r;
   }
 
   sem_t mMessage2;       
