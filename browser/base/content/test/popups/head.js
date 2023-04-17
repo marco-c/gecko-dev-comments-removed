16d78768dfe31ebb94c1759b5f29d707ace92aab
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 10:05:32 +0000

    Bug 970675 - Allow changing popup blocker settings for the session in private windows. r=johannh
    The permission manager automatically only saves permissions for the session with private browsing principals:
    https://searchfox.org/mozilla-central/rev/da25888c4495585c532640f0e5efad07b1037621/extensions/permissions/PermissionManager.cpp#1586-1588
    
    Differential Revision: https://phabricator.services.mozilla.com/D118887
    
    UltraBlame original commit: 5a1853ea9693b8f9d5a12fe7cdfb056efd5dce78

--- browser/base/content/test/popups/head.js
+++ browser/base/content/test/popups/head.js
@@ -1,10 +1,11 @@
-/* Any copyright is dedicated to the Public Domain.
-   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+
 
 "use strict";
 
-async function waitForBlockedPopups(numberOfPopups) {
-  let menupopup = document.getElementById("blockedPopupOptions");
+async function waitForBlockedPopups(numberOfPopups, { doc }) {
+  let toolbarDoc = doc || document;
+  let menupopup = toolbarDoc.getElementById("blockedPopupOptions");
   await BrowserTestUtils.waitForCondition(() => {
     let popups = menupopup.querySelectorAll("[popupReportIndex]");
     return popups.length == numberOfPopups;
