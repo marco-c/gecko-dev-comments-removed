7914e2c291d276ae5949c636e235236f66c95e53
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 05:39:25 +0000

    Bug 1688188: Replace GC parameters and zeal mode with auto classes in jsapi-tests r=sfink
    Differential Revision: https://phabricator.services.mozilla.com/D113592
    
    UltraBlame original commit: 29493a9030a5b97f0b5c93f5ffde4dd1b48dc862

--- js/src/jsapi-tests/tests.h
+++ js/src/jsapi-tests/tests.h
@@ -527,6 +527,20 @@ class ExternalData {
   }
 };
 
+class AutoGCParameter {
+  JSContext* cx_;
+  JSGCParamKey key_;
+  uint32_t value_;
+
+ public:
+  explicit AutoGCParameter(JSContext* cx, JSGCParamKey key, uint32_t value)
+      : cx_(cx), key_(key), value_() {
+    value_ = JS_GetGCParameter(cx, key);
+    JS_SetGCParameter(cx, key, value);
+  }
+  ~AutoGCParameter() { JS_SetGCParameter(cx_, key_, value_); }
+};
+
 #ifdef JS_GC_ZEAL
 
 
@@ -561,6 +575,12 @@ class AutoLeaveZeal {
 #  endif
   }
 };
-#endif 
+
+#else
+class AutoLeaveZeal {
+ public:
+  explicit AutoLeaveZeal(JSContext* cx) {}
+};
+#endif
 
 #endif 
