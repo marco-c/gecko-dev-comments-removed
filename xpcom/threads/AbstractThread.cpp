6563d8e462ece8f6d1f1a8045361118523fab2d0
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 00:58:09 +0000

    Bug 1695580 - Break out DelayedRunnable into its own files. r=xpcom-reviewers,nika
    Differential Revision: https://phabricator.services.mozilla.com/D109778
    
    UltraBlame original commit: ae16f09be41b481f94c36218297185ceb6ad8a0e

--- xpcom/threads/AbstractThread.cpp
+++ xpcom/threads/AbstractThread.cpp
@@ -7,6 +7,7 @@
 #include "mozilla/AbstractThread.h"
 
 #include "mozilla/ClearOnShutdown.h"
+#include "mozilla/DelayedRunnable.h"
 #include "mozilla/Maybe.h"
 #include "mozilla/MozPromise.h"  
 #include "mozilla/StateWatching.h"  
