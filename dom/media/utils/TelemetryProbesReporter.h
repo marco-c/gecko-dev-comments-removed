e66b43f34a7ebb08dee5199ca2b0106583c79ca6
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 19:10:55 +0000

    Bug 1678373 - Add a way to specify the type of content (audio/video/both) in the TelemetryProbesReporter, change a bit visibility event handling. r=alwu
    Differential Revision: https://phabricator.services.mozilla.com/D125078
    
    UltraBlame original commit: 201148a194bed830c2941747d7fdc407cbb69533

--- dom/media/utils/TelemetryProbesReporter.h
+++ dom/media/utils/TelemetryProbesReporter.h
@@ -22,6 +22,14 @@ class TelemetryProbesReporterOwner {
   virtual void DispatchAsyncTestingEvent(const nsAString& aName) = 0;
 };
 
+enum class MediaContent : uint8_t {
+  MEDIA_HAS_NOTHING = (0 << 0),
+  MEDIA_HAS_VIDEO = (1 << 0),
+  MEDIA_HAS_AUDIO = (1 << 1)
+};
+
+MOZ_MAKE_ENUM_CLASS_BITWISE_OPERATORS(MediaContent)
+
 
 
 
@@ -33,16 +41,22 @@ class TelemetryProbesReporter final {
   ~TelemetryProbesReporter() = default;
 
   enum class Visibility {
+    eInitial,
     eVisible,
     eInvisible,
   };
 
-  void OnPlay(Visibility aVisibility);
+  static MediaContent MediaInfoToMediaContent(const MediaInfo& aInfo);
+
+  
+  void OnPlay(Visibility aVisibility, MediaContent aContent);
   void OnPause(Visibility aVisibility);
+  void OnShutdown();
+
   void OnVisibilityChanged(Visibility aVisibility);
+  void OnMediaContentChanged(MediaContent aContent);
   void OnDecodeSuspended();
   void OnDecodeResumed();
-  void OnShutdown();
 
   double GetTotalVideoPlayTimeInSeconds() const;
   double GetVisibleVideoPlayTimeInSeconds() const;
@@ -113,7 +127,11 @@ class TelemetryProbesReporter final {
   
   TimeDurationAccumulator mVideoDecodeSuspendedTime;
 
-  Visibility mMediaElementVisibility = Visibility::eInvisible;
+  Visibility mMediaElementVisibility = Visibility::eInitial;
+
+  MediaContent mMediaContent = MediaContent::MEDIA_HAS_NOTHING;
+
+  bool mIsPlaying = false;
 };
 
 }  
