ff36007868f87779327dad8d1680bbe94a08b06f
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 01:02:58 +0000

    Bug 1703003 - Migrate Bookmark panel Cancel/Remove buttons to Fluent and fix capitalization, r=mconley
    Differential Revision: https://phabricator.services.mozilla.com/D110894
    
    UltraBlame original commit: f89dba779add8e3c8b9a2ac85125e480376a5c6c

--- browser/base/content/browser-places.js
+++ browser/base/content/browser-places.js
@@ -255,29 +255,13 @@ var StarUI = {
 
     let removeButton = this._element("editBookmarkPanelRemoveButton");
     if (this._isNewBookmark) {
-      removeButton.label = gNavigatorBundle.getString(
-        "editBookmarkPanel.cancel.label"
-      );
-      removeButton.setAttribute(
-        "accesskey",
-        gNavigatorBundle.getString("editBookmarkPanel.cancel.accesskey")
-      );
+      document.l10n.setAttributes(removeButton, "bookmark-panel-cancel");
     } else {
       
       
-      let bookmarksCount = this._itemGuids.length;
-      let forms = gNavigatorBundle.getString(
-        "editBookmark.removeBookmarks.label"
-      );
-      let label = PluralForm.get(bookmarksCount, forms).replace(
-        "#1",
-        bookmarksCount
-      );
-      removeButton.label = label;
-      removeButton.setAttribute(
-        "accesskey",
-        gNavigatorBundle.getString("editBookmark.removeBookmarks.accesskey")
-      );
+      document.l10n.setAttributes(removeButton, "bookmark-panel-remove", {
+        count: this._itemGuids.length,
+      });
     }
 
     this._setIconAndPreviewImage();
