399c89fa737fdc78da580ca8c384aff96cc3f867
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 16:34:13 +0000

    Bug 1696251: Allow mach commands as stand-alone functions and adapt existing commands. r=mhentges,webdriver-reviewers,perftest-reviewers,sparky,whimboo
    This removes the `CommandProvider` decorator and the need to implement
    mach commands inside subclasses of `MachCommandBase`, and moves all
    existing commands out from classes to module level functions.
    
    Differential Revision: https://phabricator.services.mozilla.com/D121512
    
    UltraBlame original commit: 510dd46a9de764e8730e081235a5d4d3aa9507ee

--- tools/power/mach_commands.py
+++ tools/power/mach_commands.py
@@ -9,9 +9,7 @@ from distutils.version import StrictVersion
 from mach.decorators import (
     Command,
     CommandArgument,
-    CommandProvider,
 )
-from mozbuild.base import MachCommandBase
 
 
 def is_osx_10_10_or_greater(cls):
@@ -21,137 +19,136 @@ def is_osx_10_10_or_greater(cls):
     return release and StrictVersion(release) >= StrictVersion("10.10")
 
 
-@CommandProvider
-class MachCommands(MachCommandBase):
-    @Command(
-        "power",
-        category="misc",
-        conditions=[is_osx_10_10_or_greater],
-        description="Get system power consumption and related measurements for "
-        "all running browsers. Available only on Mac OS X 10.10 and above. "
-        "Requires root access.",
+
+@Command(
+    "power",
+    category="misc",
+    conditions=[is_osx_10_10_or_greater],
+    description="Get system power consumption and related measurements for "
+    "all running browsers. Available only on Mac OS X 10.10 and above. "
+    "Requires root access.",
+)
+@CommandArgument(
+    "-i",
+    "--interval",
+    type=int,
+    default=30000,
+    help="The sample period, measured in milliseconds. Defaults to 30000.",
+)
+def power(command_context, interval):
+    """
+    Get system power consumption and related measurements.
+    """
+    import os
+    import re
+    import subprocess
+
+    rapl = os.path.join(command_context.topobjdir, "dist", "bin", "rapl")
+
+    interval = str(interval)
+
+    
+    
+    
+    
+    try:
+        subprocess.check_call(["sudo", "true"])
+    except Exception:
+        print("\nsudo failed; aborting")
+        return 1
+
+    
+    
+    
+    subprocess.Popen([rapl, "-n", "1", "-i", interval])
+
+    lines = subprocess.check_output(
+        [
+            "sudo",
+            "powermetrics",
+            "--samplers",
+            "tasks",
+            "--show-process-coalition",
+            "--show-process-gpu",
+            "-n",
+            "1",
+            "-i",
+            interval,
+        ],
+        universal_newlines=True,
     )
-    @CommandArgument(
-        "-i",
-        "--interval",
-        type=int,
-        default=30000,
-        help="The sample period, measured in milliseconds. Defaults to 30000.",
-    )
-    def power(self, command_context, interval):
-        """
-        Get system power consumption and related measurements.
-        """
-        import os
-        import re
-        import subprocess
 
-        rapl = os.path.join(command_context.topobjdir, "dist", "bin", "rapl")
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
-        interval = str(interval)
+    print()  
 
+    for line in lines.splitlines():
         
         
         
         
-        try:
-            subprocess.check_call(["sudo", "true"])
-        except Exception:
-            print("\nsudo failed; aborting")
-            return 1
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        if re.search(
+            r"(^Name|firefox|plugin-container|Safari\b|WebKit|Chrome|Terminal|WindowServer|kernel)",  
+            line,
+        ):
+            print(line)
 
-        
-        
-        
-        subprocess.Popen([rapl, "-n", "1", "-i", interval])
-
-        lines = subprocess.check_output(
-            [
-                "sudo",
-                "powermetrics",
-                "--samplers",
-                "tasks",
-                "--show-process-coalition",
-                "--show-process-gpu",
-                "-n",
-                "1",
-                "-i",
-                interval,
-            ],
-            universal_newlines=True,
-        )
-
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-
-        print()  
-
-        for line in lines.splitlines():
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            if re.search(
-                r"(^Name|firefox|plugin-container|Safari\b|WebKit|Chrome|Terminal|WindowServer|kernel)",  
-                line,
-            ):
-                print(line)
-
-        return 0
+    return 0
