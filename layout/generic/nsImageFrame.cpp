4d97147fca4de41b409f85bb4767a9f3b7a60011
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 19:07:49 +0000

    Bug 1733238 - P2: Make ImageAccessible an imgINotificationObserver. r=Jamie
    Use imgINotificationObserver to get notified of size availability.
    Layout uses an observer to wait for this too. Our observer is notified
    after layout so we should have bounds by then.
    
    We need to store the request status because we get a lot of "replayed"
    status changes that would cause chatty and wrong state change events
    when the accessible is first created and bound to parent.
    
    We can use that status for both INVISIBLE and ANIMATED states in
    NativeState.
    
    Differential Revision: https://phabricator.services.mozilla.com/D127719
    
    UltraBlame original commit: 910aebecda65cc1885e8c6ff2bdb0f6587e78e64

--- layout/generic/nsImageFrame.cpp
+++ layout/generic/nsImageFrame.cpp
@@ -949,13 +949,6 @@ void nsImageFrame::UpdateImage(imgIRequest* aRequest, imgIContainer* aImage) {
     
     
     if (!(mState & IMAGE_SIZECONSTRAINED)) {
-#ifdef ACCESSIBILITY
-      if (mKind != Kind::ListStyleImage) {
-        if (nsAccessibilityService* accService = GetAccService()) {
-          accService->NotifyOfImageSizeAvailable(PresShell(), mContent);
-        }
-      }
-#endif
       PresShell()->FrameNeedsReflow(this, IntrinsicDirty::StyleChange,
                                     NS_FRAME_IS_DIRTY);
     } else if (PresShell()->IsActive()) {
