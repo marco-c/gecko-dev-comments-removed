e9bdfa874c2544e334f64ff4c1b3ded8b7f33f76
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 07:49:57 +0000

    Bug 1712324 - [Profiler] Filter out markers for the service worker file from the captured markers to avoid intermittents r=canaltinova
    Differential Revision: https://phabricator.services.mozilla.com/D116720
    
    UltraBlame original commit: 95198b503fc7259ac91fbee989ead51415f28bef

--- tools/profiler/tests/browser/browser_test_marker_network_serviceworker_cache_first.js
+++ tools/profiler/tests/browser/browser_test_marker_network_serviceworker_cache_first.js
@@ -1,20 +1,20 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-
-
-
-
-
-
-
-
-
+/**
+ * Test that we emit network markers accordingly.
+ * In this file we'll test a caching service worker. This service worker will
+ * fetch and store requests at install time, and serve them when the page
+ * requests them.
+ */
 
 const serviceWorkerFileName = "serviceworker_cache_first.js";
 registerCleanupFunction(() => SpecialPowers.removeAllServiceWorkerData());
 
 add_task(async function test_network_markers_service_worker_setup() {
-  
-  
+  // Disabling cache makes the result more predictable. Also this makes things
+  // simpler when dealing with service workers.
   await SpecialPowers.pushPrefEnv({
     set: [
       ["browser.cache.disk.enable", false],
@@ -24,9 +24,9 @@ add_task(async function test_network_markers_service_worker_setup() {
 });
 
 add_task(async function test_network_markers_service_worker_register() {
-  
-  
-  
+  // In this first step, we request an HTML page that will register a service
+  // worker. We'll wait until the service worker is fully installed before
+  // checking various things.
   if (!AppConstants.MOZ_GECKO_PROFILER) {
     return;
   }
@@ -61,15 +61,15 @@ add_task(async function test_network_markers_service_worker_register() {
       profile,
     } = await stopProfilerNowAndGetThreads(contentPid);
 
-    
-    
-    
-    
-    
-    
+    // The service worker work happens in a third "thread" or process, let's try
+    // to find it.
+    // Currently the fetches happen on the main thread for the content process,
+    // this may change in the future and we may have to adapt this function.
+    // Also please note this isn't necessarily the same content process as the
+    // ones for the tab.
     const { serviceWorkerParentThread } = findServiceWorkerThreads(profile);
 
-    
+    // Here are a few sanity checks.
     ok(
       serviceWorkerParentThread,
       "We should find a thread for the service worker."
@@ -86,7 +86,7 @@ add_task(async function test_network_markers_service_worker_register() {
       "We should have a different tid than the parent thread."
     );
 
-    
+    // Let's make sure we actually have a registered service workers.
     const workers = await SpecialPowers.registeredServiceWorkers();
     Assert.equal(
       workers.length,
@@ -94,7 +94,7 @@ add_task(async function test_network_markers_service_worker_register() {
       "One service worker should be properly registered."
     );
 
-    
+    // By logging a few information about the threads we make debugging easier.
     logInformationForThread("parentThread information", parentThread);
     logInformationForThread("contentThread information", contentThread);
     logInformationForThread(
@@ -102,20 +102,20 @@ add_task(async function test_network_markers_service_worker_register() {
       serviceWorkerParentThread
     );
 
-    
+    // Now let's check the marker payloads.
     const parentNetworkMarkers = getInflatedNetworkMarkers(parentThread)
-      
-      
-      
-      
-      
+      // When we load a page, Firefox will check the service worker freshness
+      // after a few seconds. So when the test lasts a long time (with some test
+      // environments) we might see spurious markers about that that we're not
+      // interesting in in this part of the test. They're only present in the
+      // parent process.
       .filter(marker => !marker.data.URI.includes(serviceWorkerFileName));
     const contentNetworkMarkers = getInflatedNetworkMarkers(contentThread);
     const serviceWorkerNetworkMarkers = getInflatedNetworkMarkers(
       serviceWorkerParentThread
     );
 
-    
+    // Some more logs for debugging purposes.
     info(
       "Parent network markers: " + JSON.stringify(parentNetworkMarkers, null, 2)
     );
@@ -134,8 +134,8 @@ add_task(async function test_network_markers_service_worker_register() {
       serviceWorkerNetworkMarkers
     );
 
-    
-    
+    // First, make sure we properly matched all start with stop markers. This
+    // means that both arrays should contain only arrays of 2 elements.
     parentPairs.forEach(pair =>
       Assert.equal(
         pair.length,
@@ -158,14 +158,14 @@ add_task(async function test_network_markers_service_worker_register() {
       )
     );
 
-    
+    // Let's look at all pairs and make sure we requested all expected files.
     const parentStopMarkers = parentPairs.map(([_, stopMarker]) => stopMarker);
     const serviceWorkerStopMarkers = serviceWorkerPairs.map(
       ([_, stopMarker]) => stopMarker
     );
 
-    
-    
+    // These are the files cached by the service worker. We should see markers
+    // for both the parent thread and the service worker thread.
     const expectedFiles = [
       "serviceworker_page.html",
       "firefox-logo-nightly.svg",
@@ -214,8 +214,8 @@ add_task(async function test_network_markers_service_worker_register() {
 });
 
 add_task(async function test_network_markers_service_worker_use() {
-  
-  
+  // In this test we request an HTML file that itself contains resources that
+  // are redirected.
   if (!AppConstants.MOZ_GECKO_PROFILER) {
     return;
   }
@@ -238,25 +238,40 @@ add_task(async function test_network_markers_service_worker_use() {
       contentPid
     );
 
-    const parentNetworkMarkers = getInflatedNetworkMarkers(parentThread);
+    // By logging a few information about the threads we make debugging easier.
+    logInformationForThread("parentThread information", parentThread);
+    logInformationForThread("contentThread information", contentThread);
+
+    const parentNetworkMarkers = getInflatedNetworkMarkers(parentThread)
+      // When we load a page, Firefox will check the service worker freshness
+      // after a few seconds. So when the test lasts a long time (with some test
+      // environments) we might see spurious markers about that that we're not
+      // interesting in in this part of the test. They're only present in the
+      // parent process.
+      .filter(marker => !marker.data.URI.includes(serviceWorkerFileName));
     const contentNetworkMarkers = getInflatedNetworkMarkers(contentThread);
 
-    
-    info(JSON.stringify(parentNetworkMarkers, null, 2));
-    info(JSON.stringify(contentNetworkMarkers, null, 2));
+    // Here are some logs to ease debugging.
+    info(
+      "Parent network markers: " + JSON.stringify(parentNetworkMarkers, null, 2)
+    );
+    info(
+      "Content network markers: " +
+        JSON.stringify(contentNetworkMarkers, null, 2)
+    );
 
     const parentPairs = getPairsOfNetworkMarkers(parentNetworkMarkers);
     const contentPairs = getPairsOfNetworkMarkers(contentNetworkMarkers);
 
-    
-    
+    // These are the files cached by the service worker. We should see markers
+    // for the parent thread and the content thread.
     const expectedFiles = [
       "serviceworker_page.html",
       "firefox-logo-nightly.svg",
     ].map(filename => `${BASE_URL_HTTPS}serviceworkers/${filename}`);
 
-    
-    
+    // First, make sure we properly matched all start with stop markers. This
+    // means that both arrays should contain only arrays of 2 elements.
     parentPairs.forEach(pair =>
       Assert.equal(
         pair.length,
@@ -273,7 +288,7 @@ add_task(async function test_network_markers_service_worker_use() {
       )
     );
 
-    
+    // Let's look at all pairs and make sure we requested all expected files.
     const parentStopMarkers = parentPairs.map(([_, stopMarker]) => stopMarker);
     const contentStopMarkers = contentPairs.map(
       ([_, stopMarker]) => stopMarker
@@ -317,9 +332,9 @@ add_task(async function test_network_markers_service_worker_use() {
         name: Expect.stringMatches(
           `Load \\d+:.*${escapeStringRegexp(expectedFile)}`
         ),
-        
-        
-        
+        // Note: in the future we may have more properties. We're using the
+        // "Only" flavor of the matcher so that we don't forget to update this
+        // test when this changes.
         data: Expect.objectContainsOnly(commonDataProperties),
       };
 
@@ -327,9 +342,9 @@ add_task(async function test_network_markers_service_worker_use() {
         name: Expect.stringMatches(
           `Load \\d+:.*${escapeStringRegexp(expectedFile)}`
         ),
-        
-        
-        
+        // Note: in the future we may have more properties. We're using the
+        // "Only" flavor of the matcher so that we don't forget to update this
+        // test when this changes.
         data: Expect.objectContainsOnly({
           ...commonDataProperties,
           innerWindowID: Expect.number(),
