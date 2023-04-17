582fc7b6de491f14cc914c49d5005fd94d82f838
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 06:04:13 +0000

    Bug 1707876 - Remove browsers.base.inherit and remaining usage, r=whimboo
    This was designed to allow reusing configuration across browsers, but
    in practice it's only used once and is adding additional complexity
    that isn't merited by the amount of use.
    
    Differential Revision: https://phabricator.services.mozilla.com/D113670
    
    UltraBlame original commit: b8ab0d84b8a7cbd0f4e74790516b29321d496b45

--- testing/web-platform/tests/tools/wptrunner/wptrunner/browsers/edge_webdriver.py
+++ testing/web-platform/tests/tools/wptrunner/wptrunner/browsers/edge_webdriver.py
@@ -1,12 +1,28 @@
-from .base import inherit
-from . import edge
+from .base import NullBrowser  
+from .edge import (EdgeBrowser,  
+                   EdgeDriverWdspecExecutor,  
+                   check_args,  
+                   browser_kwargs,  
+                   executor_kwargs,  
+                   env_extras,  
+                   env_options,  
+                   run_info_extras,  
+                   get_timeout_multiplier)  
 
 from ..executors.executorwebdriver import (WebDriverTestharnessExecutor,  
                                            WebDriverRefTestExecutor)  
 
 
-inherit(edge, globals(), "edge_webdriver")
-
-
-__wptrunner__["executor"]["testharness"] = "WebDriverTestharnessExecutor"  
-__wptrunner__["executor"]["reftest"] = "WebDriverRefTestExecutor"  
+__wptrunner__ = {"product": "edge_webdriver",
+                 "check_args": "check_args",
+                 "browser": {None: "EdgeBrowser",
+                             "wdspec": "NullBrowser"},
+                 "executor": {"testharness": "WebDriverTestharnessExecutor",
+                              "reftest": "WebDriverRefTestExecutor",
+                              "wdspec": "EdgeDriverWdspecExecutor"},
+                 "browser_kwargs": "browser_kwargs",
+                 "executor_kwargs": "executor_kwargs",
+                 "env_extras": "env_extras",
+                 "env_options": "env_options",
+                 "run_info_extras": "run_info_extras",
+                 "timeout_multiplier": "get_timeout_multiplier"}
