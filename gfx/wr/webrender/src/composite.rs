f43c575e8a4b157813a3e28ff26d78c24ff53675
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 02:10:42 +0000

    Bug 1704956 - Support non-opaque compositor surfaces. r=gfx-reviewers,lsalzman
    This patch enables the sub-slice support landed in previous
    patches, to allow non-opaque external images to be treated as
    native compositor surfaces.
    
    Each picture cache has one or more sub-slices, which form the
    tile grid of picture cache targets. Sub-slices are added as
    required based on the number of compositor surfaces that are
    found during scene building.
    
    As primitives are added to the picture cache dependencies, the
    appropriate sub-slice to add the primitive to is calculated. This
    depends on whether the primitive intersects any of the compositor
    surface rects (and thus must be drawn after that surface). Prims
    are added to the earliest sub-slice possible, to maximize the
    chance that the primitive is on the primary sub-slice. This means
    the prim can be rendered with subpixel AA due to an opaque background
    in most cases, and also reduces the number of alpha tiles that are
    allocated and must be composited.
    
    Creating extra sub-slices and tile grids is relatively cheap - the
    backing surface for a tile is only allocated once there are prims
    in that tile to draw. The valid rect tracked for each tile also
    ensures that a minimum rect is used during drawing and compositing.
    
    Differential Revision: https://phabricator.services.mozilla.com/D111967
    
    UltraBlame original commit: 82b5b3dfe2ba46fb252388105e05dbc280061056

--- gfx/wr/webrender/src/composite.rs
+++ gfx/wr/webrender/src/composite.rs
@@ -579,11 +579,7 @@ impl CompositeState {
                         let surface = descriptor.resolve(resource_cache, tile_cache.current_tile_size);
                         (
                             CompositeTileSurface::Texture { surface },
-                            
-                            
-                            
-                            
-                            tile.is_opaque || (!tile.has_compositor_surface && tile_cache.is_opaque()),
+                            tile.is_opaque
                         )
                     }
                 };
@@ -643,8 +639,28 @@ impl CompositeState {
             }
 
             
+            if visible_alpha_tile_count > 0 {
+                self.descriptor.surfaces.push(
+                    CompositeSurfaceDescriptor {
+                        surface_id: sub_slice.native_surface.as_ref().map(|s| s.alpha),
+                        clip_rect: surface_clip_rect,
+                        transform: CompositorSurfaceTransform::translation(
+                            tile_cache.device_position.x,
+                            tile_cache.device_position.y,
+                            0.0,
+                        ),
+                        image_dependencies: [ImageDependency::INVALID; 3],
+                        image_rendering: ImageRendering::CrispEdges,
+                        tile_descriptors: alpha_tile_descriptors,
+                    }
+                );
+            }
+
             
-            for external_surface in &tile_cache.external_surfaces {
+            
+            for compositor_surface in &sub_slice.compositor_surfaces {
+                let external_surface = &compositor_surface.descriptor;
+
                 let clip_rect = external_surface
                     .clip_rect
                     .intersection(&device_clip_rect)
@@ -722,25 +738,7 @@ impl CompositeState {
                     }
                 );
 
-                self.push_tile(tile, true);
-            }
-
-            
-            if visible_alpha_tile_count > 0 {
-                self.descriptor.surfaces.push(
-                    CompositeSurfaceDescriptor {
-                        surface_id: sub_slice.native_surface.as_ref().map(|s| s.alpha),
-                        clip_rect: surface_clip_rect,
-                        transform: CompositorSurfaceTransform::translation(
-                            tile_cache.device_position.x,
-                            tile_cache.device_position.y,
-                            0.0,
-                        ),
-                        image_dependencies: [ImageDependency::INVALID; 3],
-                        image_rendering: ImageRendering::CrispEdges,
-                        tile_descriptors: alpha_tile_descriptors,
-                    }
-                );
+                self.push_tile(tile, compositor_surface.is_opaque);
             }
         }
     }
@@ -869,7 +867,11 @@ impl CompositeState {
                 }
             }
             CompositeTileSurface::ExternalSurface { .. } => {
-                self.opaque_tiles.push(tile);
+                if is_opaque {
+                    self.opaque_tiles.push(tile);
+                } else {
+                    self.alpha_tiles.push(tile);
+                }
             }
         }
     }
