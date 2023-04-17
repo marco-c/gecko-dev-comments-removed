6be52c504c077c1a336b926868912c871ce0e85d
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 06:27:28 +0000

    Bug 1540037 - part 29: Move `nsIEditor.documentCharacterSet` definitions into `HTMLEditor` r=m_kato
    The attribute is used only with `HTMLEditor`, and it does not make sense to
    access document's character-set via `TextEditor`.  Therefore, this patch
    makes it implement in `HTMLEditor` (`EditorBase` will return
    `NS_ERROR_NOT_AVAILABLE` both getter and setter).
    
    Note that `EditorBase::GetDocumentCharsetInternal()` is required by
    `TextEditor::ComputeValueInternal()`.  Therefore, it needs to stay in
    `EditorBase`.
    
    Differential Revision: https://phabricator.services.mozilla.com/D115948
    
    UltraBlame original commit: 3c85e6dc75bfd41a0cc82dcd8dd2b63d582f415b

--- editor/libeditor/EditorBase.cpp
+++ editor/libeditor/EditorBase.cpp
@@ -6,7 +6,6 @@
 #include "EditorBase.h"
 
 #include "mozilla/DebugOnly.h"  
-#include "mozilla/Encoding.h"   
 
 #include <stdio.h>   
 #include <string.h>  
@@ -37,12 +36,13 @@
 #include "mozilla/EditorSpellCheck.h"         
 #include "mozilla/EditorUtils.h"              
 #include "mozilla/EditTransactionBase.h"      
-#include "mozilla/EventDispatcher.h"          
-#include "mozilla/FlushType.h"                
-#include "mozilla/HTMLEditor.h"               
-#include "mozilla/IMEContentObserver.h"       
-#include "mozilla/IMEStateManager.h"          
-#include "mozilla/InputEventOptions.h"        
+#include "mozilla/Encoding.h"  
+#include "mozilla/EventDispatcher.h"     
+#include "mozilla/FlushType.h"           
+#include "mozilla/HTMLEditor.h"          
+#include "mozilla/IMEContentObserver.h"  
+#include "mozilla/IMEStateManager.h"     
+#include "mozilla/InputEventOptions.h"   
 #include "mozilla/InternalMutationEvent.h"  
 #include "mozilla/mozalloc.h"               
 #include "mozilla/mozInlineSpellChecker.h"  
@@ -1390,37 +1390,22 @@ NS_IMETHODIMP EditorBase::GetDocumentModified(bool* aOutDocModified) {
   return NS_OK;
 }
 
-NS_IMETHODIMP EditorBase::GetDocumentCharacterSet(nsACString& aCharset) {
-  nsresult rv = GetDocumentCharsetInternal(aCharset);
-  NS_WARNING_ASSERTION(NS_SUCCEEDED(rv),
-                       "EditorBase::GetDocumentCharsetInternal() failed");
-  return rv;
+NS_IMETHODIMP EditorBase::GetDocumentCharacterSet(nsACString& aCharacterSet) {
+  return NS_ERROR_NOT_AVAILABLE;
 }
 
 nsresult EditorBase::GetDocumentCharsetInternal(nsACString& aCharset) const {
-  RefPtr<Document> document = GetDocument();
+  Document* document = GetDocument();
   if (NS_WARN_IF(!document)) {
-    return NS_ERROR_UNEXPECTED;
+    return NS_ERROR_NOT_INITIALIZED;
   }
   document->GetDocumentCharacterSet()->Name(aCharset);
   return NS_OK;
 }
 
 NS_IMETHODIMP EditorBase::SetDocumentCharacterSet(
-    const nsACString& characterSet) {
-  RefPtr<Document> document = GetDocument();
-  if (NS_WARN_IF(!document)) {
-    return NS_ERROR_UNEXPECTED;
-  }
-  
-  
-  auto encoding = Encoding::ForLabelNoReplacement(characterSet);
-  if (!encoding) {
-    NS_WARNING("Encoding::ForLabelNoReplacement() failed");
-    return NS_ERROR_INVALID_ARG;
-  }
-  document->SetDocumentCharacterSet(WrapNotNull(encoding));
-  return NS_OK;
+    const nsACString& aCharacterSet) {
+  return NS_ERROR_NOT_AVAILABLE;
 }
 
 bool EditorBase::AreClipboardCommandsUnconditionallyEnabled() const {
--- editor/libeditor/HTMLEditor.h
+++ editor/libeditor/HTMLEditor.h
@@ -151,6 +151,10 @@ class HTMLEditor final : public TextEditor,
   MOZ_CAN_RUN_SCRIPT_BOUNDARY NS_IMETHOD BeginningOfDocument() override;
   MOZ_CAN_RUN_SCRIPT NS_IMETHOD SetFlags(uint32_t aFlags) override;
 
+  NS_IMETHOD GetDocumentCharacterSet(nsACString& aCharacterSet) final;
+  MOZ_CAN_RUN_SCRIPT NS_IMETHOD
+  SetDocumentCharacterSet(const nsACString& aCharacterSet) final;
+
   bool IsEmpty() const final;
 
   bool CanPaste(int32_t aClipboardType) const final;
@@ -3258,6 +3262,15 @@ class HTMLEditor final : public TextEditor,
   [[nodiscard]] MOZ_CAN_RUN_SCRIPT EditActionResult OutdentAsSubAction();
 
   MOZ_CAN_RUN_SCRIPT nsresult LoadHTML(const nsAString& aInputString);
+
+  
+
+
+
+
+
+  MOZ_CAN_RUN_SCRIPT bool UpdateMetaCharsetWithTransaction(
+      Document& aDocument, const nsACString& aCharacterSet);
 
   
 
