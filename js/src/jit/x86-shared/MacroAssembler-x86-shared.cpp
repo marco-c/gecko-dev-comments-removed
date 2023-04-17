99a0881483580835fbedb32d2a1e5ab7fba2bb65
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 17:18:54 +0000

    Bug 1732208 - Silence the unused-but-set-variable warning in js/src. r=jandem
    vm/TraceLogging.cpp:1685:12: error: variable 'totalJSTime' set but not used [-Werror,-Wunused-but-set-variable]
      uint32_t totalJSTime = 0;
               ^
    wasm/WasmBinary.h:175:15: error: variable 'assertByte' set but not used [-Werror,-Wunused-but-set-variable]
          uint8_t assertByte = assertBits & 0x7f;
                  ^
    jit/x86-shared/MacroAssembler-x86-shared.cpp:406:11: error: variable 'diffG' set but not used [-Werror,-Wunused-but-set-variable]
      int32_t diffG = set.gprs().size() * sizeof(intptr_t);
              ^
    jit/x86-shared/MacroAssembler-x86-shared.cpp:404:12: error: variable 'numFpu' set but not used [-Werror,-Wunused-but-set-variable]
      unsigned numFpu = fpuSet.size();
               ^
    jit/x86-shared/MacroAssembler-x86-shared.cpp:455:11: error: variable 'diffG' set but not used [-Werror,-Wunused-but-set-variable]
      int32_t diffG = set.gprs().size() * sizeof(intptr_t);
              ^
    jit/x86-shared/MacroAssembler-x86-shared.cpp:453:12: error: variable 'numFpu' set but not used [-Werror,-Wunused-but-set-variable]
      unsigned numFpu = fpuSet.size();
               ^
    jit/x86-shared/MacroAssembler-x86-shared.cpp:503:12: error: variable 'numFpu' set but not used [-Werror,-Wunused-but-set-variable]
      unsigned numFpu = fpuSet.size();
               ^
    
    Differential Revision: https://phabricator.services.mozilla.com/D126456
    
    UltraBlame original commit: 7e04dd613276a5b9bf1e667f9cf20aef2953ab57

--- js/src/jit/x86-shared/MacroAssembler-x86-shared.cpp
+++ js/src/jit/x86-shared/MacroAssembler-x86-shared.cpp
@@ -412,6 +412,7 @@ void MacroAssembler::PushRegsInMask(LiveRegisterSet set) {
     Push(*iter);
   }
   MOZ_ASSERT(diffG == 0);
+  (void)diffG;
 
   reserveStack(diffF);
   for (FloatRegisterBackwardIterator iter(fpuSet); iter.more(); ++iter) {
@@ -430,6 +431,7 @@ void MacroAssembler::PushRegsInMask(LiveRegisterSet set) {
     }
   }
   MOZ_ASSERT(numFpu == 0);
+  (void)numFpu;
 
   
   
@@ -462,6 +464,7 @@ void MacroAssembler::storeRegsInMask(LiveRegisterSet set, Address dest,
     storePtr(*iter, dest);
   }
   MOZ_ASSERT(diffG == 0);
+  (void)diffG;
 
   for (FloatRegisterBackwardIterator iter(fpuSet); iter.more(); ++iter) {
     FloatRegister reg = *iter;
@@ -479,6 +482,7 @@ void MacroAssembler::storeRegsInMask(LiveRegisterSet set, Address dest,
     }
   }
   MOZ_ASSERT(numFpu == 0);
+  (void)numFpu;
 
   
   
@@ -527,6 +531,7 @@ void MacroAssembler::PopRegsInMaskIgnore(LiveRegisterSet set,
   }
   freeStack(reservedF);
   MOZ_ASSERT(numFpu == 0);
+  (void)numFpu;
   
   
   diffF -= diffF % sizeof(uintptr_t);
