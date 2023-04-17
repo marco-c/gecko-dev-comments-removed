16041a0161ef5ee918907a3db6161e263e0ea16c
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 10:52:19 +0000

    Bug 1691575 - [devtools] Don't rely on webconsole fronts to fetch longStrings fullText. r=bomsy.
    The Netmonitor used to call the WebConsoleFront `getString` method to get the full
    text of a long string. This could make some test to fail with Fission and target
    switching.
    Since the webconsole actor is not even involved in the webconsole method, there's
    no reason to keep using it. A similar function is added into a util file; the only
    difference is that it doesn't cache the front it creates, which does not matter
    as the netmonitor only calls this once and then put the full text in its redux
    store.
    This allows us to re-enable `browser_ext_devtools_network.js` with Fission.
    
    Differential Revision: https://phabricator.services.mozilla.com/D120333
    
    UltraBlame original commit: e30b2a84440d054b994292a4d59bab934c847867

--- devtools/client/netmonitor/src/connector/firefox-data-provider.js
+++ devtools/client/netmonitor/src/connector/firefox-data-provider.js
@@ -14,6 +14,10 @@ const {
   fetchHeaders,
 } = require("devtools/client/netmonitor/src/utils/request-utils");
 
+const {
+  getLongStringFullText,
+} = require("devtools/client/shared/string-utils");
+
 
 
 
@@ -289,10 +293,10 @@ class FirefoxDataProvider {
 
 
   async getLongString(stringGrip) {
-    const webConsoleFront = await this.commands.targetCommand.targetFront.getFront(
-      "console"
+    const payload = await getLongStringFullText(
+      this.commands.client,
+      stringGrip
     );
-    const payload = await webConsoleFront.getString(stringGrip);
     this.emitForTests(TEST_EVENTS.LONGSTRING_RESOLVED, { payload });
     return payload;
   }
