accbd69bb95e54ed26996b6bb97f45db696335c0
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 08:42:40 +0000

    Bug 1713334 - Part 1: Keep a text node inside text controls even if empty r=masayuki,Jamie
    Doing so prevents confusion when detecting selection change.
    
    Differential Revision: https://phabricator.services.mozilla.com/D116303
    
    UltraBlame original commit: 78b0cdebc890d9f94d8fb5f5256653da99ae427b

--- editor/libeditor/InsertNodeTransaction.cpp
+++ editor/libeditor/InsertNodeTransaction.cpp
@@ -91,6 +91,8 @@ NS_IMETHODIMP InsertNodeTransaction::DoTransaction() {
     return NS_ERROR_NOT_AVAILABLE;
   }
 
+  MOZ_ASSERT_IF(mEditorBase->IsTextEditor(), !mContentToInsert->IsText());
+
   if (!mPointToInsert.IsSetAndValid()) {
     
     
@@ -137,18 +139,6 @@ NS_IMETHODIMP InsertNodeTransaction::DoTransaction() {
     return error.StealNSResult();
   }
 
-  if (editorBase->IsTextEditor() && contentToInsert->IsText()) {
-    uint32_t length = contentToInsert->AsText()->TextLength();
-    if (length > 0) {
-      nsresult rv = MOZ_KnownLive(editorBase->AsTextEditor())
-                        ->DidInsertText(length, 0, length);
-      if (NS_FAILED(rv)) {
-        NS_WARNING("TextEditor::DidInsertText() failed");
-        return rv;
-      }
-    }
-  }
-
   if (!mEditorBase->AllowsTransactionsToChangeSelection()) {
     return NS_OK;
   }
@@ -178,12 +168,6 @@ NS_IMETHODIMP InsertNodeTransaction::UndoTransaction() {
   if (NS_WARN_IF(!mEditorBase) || NS_WARN_IF(!mContentToInsert) ||
       NS_WARN_IF(!mPointToInsert.IsSet())) {
     return NS_ERROR_NOT_INITIALIZED;
-  }
-  if (mEditorBase->IsTextEditor() && mContentToInsert->IsText()) {
-    uint32_t length = mContentToInsert->TextLength();
-    if (length > 0) {
-      mEditorBase->AsTextEditor()->WillDeleteText(length, 0, length);
-    }
   }
   
   
