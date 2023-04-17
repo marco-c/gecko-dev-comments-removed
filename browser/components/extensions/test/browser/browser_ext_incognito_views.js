968253d6399163fd8bfe87de35319e6492061fc2
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 14:36:36 +0000

    Bug 1707305 - Fix intermittent browser_ext_incognito_views.js failures due to a race in the test case. r=mixedpuppy
    browser_ext_incognito_views.js is failing intermittently in optimized builds due to a race between
    a browserAction popup being opened as part of the test task and the related window being closed at the
    end of the same test task.
    
    This patch moves the responsability of closing the browserAction popup and the window to the chrome-privileged
    part of the test, and makes sure that we are explicitly waiting for the browserAction popup to be opened and
    loaded and then completely closed before removing the related Firefox window.
    
    Differential Revision: https://phabricator.services.mozilla.com/D124047
    
    UltraBlame original commit: 112b8a38412466e291ea4d15eb49de987f8ad957

--- browser/components/extensions/test/browser/browser_ext_incognito_views.js
+++ browser/components/extensions/test/browser/browser_ext_incognito_views.js
@@ -82,8 +82,6 @@ add_task(async function testIncognitoViews() {
           await windowReady;
 
           await testWindow(window);
-
-          await browser.windows.remove(window.id);
         }
 
         browser.test.notifyPass("incognito-views");
@@ -163,20 +161,24 @@ add_task(async function testIncognitoViews() {
           windowId: win.id,
           incognito: browser.extension.inIncognitoContext,
         });
-
-        window.close();
       },
     },
   });
 
+  let win;
+  let promiseBrowserActionOpened;
   extension.onMessage("click-browserAction", () => {
-    clickBrowserAction(
-      extension,
-      Services.wm.getMostRecentWindow("navigator:browser")
-    );
+    win = Services.wm.getMostRecentWindow("navigator:browser");
+    promiseBrowserActionOpened = openBrowserActionPanel(extension, win, true);
   });
 
   await extension.startup();
   await extension.awaitFinish("incognito-views");
+  
+  
+  
+  await promiseBrowserActionOpened;
+  await closeBrowserAction(extension, win);
   await extension.unload();
+  await BrowserTestUtils.closeWindow(win);
 });
