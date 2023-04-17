8656e27dda42b80b03a5aeffde743bdc14deabb6
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 12:55:29 +0000

    Bug 1716825: Navigate to a sub-page rather than about:blank for warm pageloads;r=perftest-reviewers,Bebe,davehunt
    Differential Revision: https://phabricator.services.mozilla.com/D120718
    
    UltraBlame original commit: 0e14c23c5b75ac7e3f8e7b55fb4ccc9a33660fb7

--- testing/performance/perftest_record.js
+++ testing/performance/perftest_record.js
@@ -5,11 +5,23 @@
 "use strict";
 
 async function test(context, commands) {
-  let rootUrl = context.options.browsertime.url;
+  let testUrl = context.options.browsertime.url;
+  let secondaryUrl = context.options.browsertime.secondary_url;
   let testName = context.options.browsertime.testName;
 
-  await commands.measure.start(rootUrl);
-  commands.screenshot.take(testName);
+  
+  await commands.wait.byTime(1000);
+
+  await commands.measure.start(testUrl);
+  commands.screenshot.take("test_url_" + testName);
+
+  if (secondaryUrl !== null) {
+    
+    await commands.wait.byTime(1000);
+
+    await commands.measure.start(secondaryUrl);
+    commands.screenshot.take("secondary_url_" + testName);
+  }
 
   
   await commands.wait.byTime(1000);
