bdd491cedae6d62e92c0fff9cfe05a21a1899101
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 13:44:15 +0000

    Bug 1723266 - Return an infinite perspective in TransformOperation::Perspective::to_animated_zero. r=emilio
    Differential Revision: https://phabricator.services.mozilla.com/D122919
    
    UltraBlame original commit: 21e4ad4c85992394619ce34a7a83332ca7d0c43e

--- servo/components/style/values/generics/transform.rs
+++ servo/components/style/values/generics/transform.rs
@@ -582,10 +582,10 @@ impl<T: ToMatrix> Transform<T> {
 
 #[inline]
 pub fn create_perspective_matrix(d: CSSFloat) -> Transform3D<CSSFloat> {
-    if d < 0.0 {
-        Transform3D::identity()
-    } else {
+    if d.is_finite() {
         Transform3D::perspective(d.max(1.))
+    } else {
+        Transform3D::identity()
     }
 }
 
