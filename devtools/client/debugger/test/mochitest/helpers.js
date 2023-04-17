544106d1710b9c7a6968eeb84cd0dcd4e9ab2901
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 02:32:41 +0000

    Bug 1704628 Part 1: Make selectContextMenuItem use .activateItem() semantics. r=ochameau
    This change is necessary to make these tests pass on macOS with native context
    menus enabled.
    
    Differential Revision: https://phabricator.services.mozilla.com/D111724
    
    UltraBlame original commit: fea280db91bea4fcf4c8a337e6d401384d05c087

--- devtools/client/debugger/test/mochitest/helpers.js
+++ devtools/client/debugger/test/mochitest/helpers.js
@@ -1551,12 +1551,16 @@ async function cmdClickGutter(dbg, line) {
   clickDOMElement(dbg, el, cmdOrCtrl);
 }
 
-function findContextMenu(dbg, selector) {
+function findContextMenuPopup(dbg) {
   
   const doc = dbg.toolbox.topDoc;
 
   
-  const popup = doc.querySelector('menupopup[menu-api="true"]');
+  return doc.querySelector('menupopup[menu-api="true"]');
+}
+
+function findContextMenu(dbg, selector) {
+  const popup = findContextMenuPopup(dbg)
 
   return popup.querySelector(selector);
 }
@@ -1567,8 +1571,8 @@ async function waitForContextMenu(dbg, selector) {
 }
 
 function selectContextMenuItem(dbg, selector) {
-  const item = findContextMenu(dbg, selector);
-  return EventUtils.synthesizeMouseAtCenter(item, {}, dbg.toolbox.topWindow);
+  const popup = findContextMenuPopup(dbg);
+  popup.activateItem(popup.querySelector(selector));
 }
 
 async function assertContextMenuLabel(dbg, selector, label) {
