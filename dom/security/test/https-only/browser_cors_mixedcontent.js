379b1320a2afdddcf07cd5e25edb30301e7e1e2a
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 02:28:16 +0000

    Bug 1705127 - Enable more ESLint rules for some more dom/security/test directories. r=ckerschb
    Differential Revision: https://phabricator.services.mozilla.com/D112053
    
    UltraBlame original commit: e47106815d4ccf9f761af31f592940345bda8880

--- dom/security/test/https-only/browser_cors_mixedcontent.js
+++ dom/security/test/https-only/browser_cors_mixedcontent.js
@@ -97,6 +97,7 @@ async function runTest(test) {
 
     await loaded;
 
+    
     await SpecialPowers.spawn(browser, [test], async function(test) {
       const promise = new Promise(resolve => {
         content.addEventListener("FetchEnded", resolve, {
