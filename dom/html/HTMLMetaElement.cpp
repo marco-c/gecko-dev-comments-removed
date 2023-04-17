16fb5fc7820a960a2a4f53adad76030889126909
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 08:12:43 +0000

    Bug 1704473 - Remove <head> requirement for <meta name=referrer>. r=ckerschb
    Differential Revision: https://phabricator.services.mozilla.com/D116618
    
    UltraBlame original commit: 7b5d3986ef966c2a33ad6d38aaa29e1557998c4c

--- dom/html/HTMLMetaElement.cpp
+++ dom/html/HTMLMetaElement.cpp
@@ -41,13 +41,9 @@ void HTMLMetaElement::SetMetaReferrer(Document* aDocument) {
   }
   nsAutoString content;
   GetContent(content);
-
-  Element* headElt = aDocument->GetHeadElement();
-  if (headElt && IsInclusiveDescendantOf(headElt)) {
-    content = nsContentUtils::TrimWhitespace<nsContentUtils::IsHTMLWhitespace>(
-        content);
-    aDocument->UpdateReferrerInfoFromMeta(content, false);
-  }
+  content =
+      nsContentUtils::TrimWhitespace<nsContentUtils::IsHTMLWhitespace>(content);
+  aDocument->UpdateReferrerInfoFromMeta(content, false);
 }
 
 nsresult HTMLMetaElement::AfterSetAttr(int32_t aNameSpaceID, nsAtom* aName,
@@ -135,8 +131,6 @@ nsresult HTMLMetaElement::BindToTree(BindContext& aContext, nsINode& aParent) {
     }
   }
 
-  
-  
   SetMetaReferrer(&doc);
   CreateAndDispatchEvent(&doc, u"DOMMetaAdded"_ns);
   return rv;
