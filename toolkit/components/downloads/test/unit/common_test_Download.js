67108daf4ef2cb3a3eb2f245a754c5f42450a966
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 16:33:08 +0000

    Bug 1724319 - Fix toolkit downloads component tests for when new download changes are enabled. r=mtigley
    Differential Revision: https://phabricator.services.mozilla.com/D125901
    
    UltraBlame original commit: 8c5fe2e40cc5e7f87fc0beafa5f2ae00c1b6b81d

--- toolkit/components/downloads/test/unit/common_test_Download.js
+++ toolkit/components/downloads/test/unit/common_test_Download.js
@@ -2681,7 +2681,15 @@ add_task(async function test_launchWhenSucceeded_deleteTempFileOnExit() {
     "@mozilla.org/uriloader/external-helper-app-service;1"
   ].getService(Ci.nsIObserver);
   expire.observe(null, "profile-before-change", null);
-  Assert.equal(false, await OS.File.exists(autoDeleteTargetPathTwo));
+  
+  
+  
+  Assert.equal(
+    Services.prefs.getBoolPref(
+      "browser.download.improvements_to_download_panel"
+    ),
+    await OS.File.exists(autoDeleteTargetPathTwo)
+  );
   Assert.ok(await OS.File.exists(noAutoDeleteTargetPath));
 });
 
