3912d71db33b0cd49f57b3d63cf1339256b4244e
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 07:30:18 +0000

    Bug 1711648 - Move all device rects to the Box2D representations. r=jrmuizel
    Differential Revision: https://phabricator.services.mozilla.com/D116401
    
    Depends on D116266
    
    UltraBlame original commit: 3d51bfaa0d8d8ed09d420bb1cad9b9f347dfaacd

--- gfx/wr/wrench/src/reftest.rs
+++ gfx/wr/wrench/src/reftest.rs
@@ -966,7 +966,7 @@ impl<'a> ReftestHarness<'a> {
         );
 
         
-        let rect = FramebufferIntRect::new(
+        let rect = FramebufferIntRect::from_origin_and_size(
             FramebufferIntPoint::new(0, window_size.height - size.height),
             FramebufferIntSize::new(size.width, size.height),
         );
