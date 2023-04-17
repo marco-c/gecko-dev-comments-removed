36de8ed0b6c5af15c6fb96ddb576e813941967cc
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 10:14:27 +0000

    Bug 1718709 - part2 : filter out the first incorrect output that MFT decoder returns. r=bryce
    For some h264 videos, MFT decoder will return the incorrect first output which presentation time is always zero. We also saw this issue before after calling draining [1].
    
    If we accept that incorrect output, that will cause an out-of-order video frame get stored in our video queue and causes MDSM triggering the mechanism to request the next key frame because that out-of-order frame is way behind the current audio clock.
    
    Therefore, we should filter that incorrect output out, and make sure we won't incorrectly discard the real first frame when this issue doesn't happen.
    
    [1] https://stackoverflow.com/questions/39695131/windows-mft-media-foundation-transform-decoder-not-returning-proper-sample-tim
    
    Differential Revision: https://phabricator.services.mozilla.com/D119439
    
    UltraBlame original commit: a3b5e41c96ae1b267ce2d55945a133d069bab91f

--- dom/media/platforms/wmf/WMFMediaDataDecoder.h
+++ dom/media/platforms/wmf/WMFMediaDataDecoder.h
@@ -64,6 +64,10 @@ class MFTManager {
     }
   }
 
+  virtual bool HasSeekThreshold() const {
+    return mSeekTargetThreshold.isSome();
+  }
+
   virtual MediaDataDecoder::ConversionRequired NeedsConversion() const {
     return MediaDataDecoder::ConversionRequired::kNeedNone;
   }
@@ -140,6 +144,9 @@ class WMFMediaDataDecoder
   int64_t mLastStreamOffset;
   Maybe<media::TimeUnit> mLastTime;
   media::TimeUnit mLastDuration;
+  
+  
+  bool mHasGuardedAgainstIncorrectFirstSample = false;
   int64_t mSamplesCount = 0;
 
   bool mIsShutDown = false;
