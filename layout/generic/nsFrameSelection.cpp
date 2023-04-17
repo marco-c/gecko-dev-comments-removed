bd24a5b066e7643c6ef2d4bc4b8f3d6ea0f72725
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 08:33:17 +0000

    Bug 1713491 - Return std::pair from nsIFrame::GetOffsets r=emilio
    Differential Revision: https://phabricator.services.mozilla.com/D117520
    
    UltraBlame original commit: 17e7b969b83077db1b5043826a0e7ee311052693

--- layout/generic/nsFrameSelection.cpp
+++ layout/generic/nsFrameSelection.cpp
@@ -805,7 +805,7 @@ nsresult nsFrameSelection::MoveCaret(nsDirection aDirection,
       
       
       theFrame = pos.mResultFrame;
-      theFrame->GetOffsets(frameStart, frameEnd);
+      std::tie(frameStart, frameEnd) = theFrame->GetOffsets();
       currentOffset = pos.mContentOffset;
       if (frameEnd == currentOffset && !(frameStart == 0 && frameEnd == 0))
         tHint = CARET_ASSOCIATE_BEFORE;
@@ -820,7 +820,7 @@ nsresult nsFrameSelection::MoveCaret(nsDirection aDirection,
                                        tHint, &currentOffset);
       if (!theFrame) return NS_ERROR_FAILURE;
 
-      theFrame->GetOffsets(frameStart, frameEnd);
+      std::tie(frameStart, frameEnd) = theFrame->GetOffsets();
     }
 
     if (context->BidiEnabled()) {
@@ -933,25 +933,26 @@ nsPrevNextBidiLevels nsFrameSelection::GetPrevNextBidiLevels(
   
   nsIFrame* currentFrame;
   int32_t currentOffset;
-  int32_t frameStart, frameEnd;
   nsDirection direction;
 
-  nsPrevNextBidiLevels levels;
+  nsPrevNextBidiLevels levels{};
   levels.SetData(nullptr, nullptr, 0, 0);
 
-  currentFrame =
-      GetFrameForNodeOffset(aNode, aContentOffset, aHint, &currentOffset);
-  if (!currentFrame) return levels;
+  currentFrame = GetFrameForNodeOffset(
+      aNode, static_cast<int32_t>(aContentOffset), aHint, &currentOffset);
+  if (!currentFrame) {
+    return levels;
+  }
 
-  currentFrame->GetOffsets(frameStart, frameEnd);
+  auto [frameStart, frameEnd] = currentFrame->GetOffsets();
 
-  if (0 == frameStart && 0 == frameEnd)
+  if (0 == frameStart && 0 == frameEnd) {
     direction = eDirPrevious;
-  else if (frameStart == currentOffset)
+  } else if (frameStart == currentOffset) {
     direction = eDirPrevious;
-  else if (frameEnd == currentOffset)
+  } else if (frameEnd == currentOffset) {
     direction = eDirNext;
-  else {
+  } else {
     
     
     nsBidiLevel currentLevel = currentFrame->GetEmbeddingLevel();
@@ -1611,9 +1612,7 @@ bool nsFrameSelection::AdjustFrameForLineStart(nsIFrame*& aFrame,
     return false;
   }
 
-  int32_t start;
-  int32_t end;
-  aFrame->GetOffsets(start, end);
+  auto [start, end] = aFrame->GetOffsets();
   if (aFrameOffset != end) {
     return false;
   }
@@ -1624,7 +1623,7 @@ bool nsFrameSelection::AdjustFrameForLineStart(nsIFrame*& aFrame,
   }
 
   aFrame = nextSibling;
-  aFrame->GetOffsets(start, end);
+  std::tie(start, end) = aFrame->GetOffsets();
   aFrameOffset = start;
   return true;
 }
