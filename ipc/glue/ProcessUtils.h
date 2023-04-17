bfb6ff2cfbca988ac6e0d5720cd18cbf79947860
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 22:49:05 +0000

    Bug 1723505 - Convert IPC processes to GeckoArgs r=nika,kershaw
    Differential Revision: https://phabricator.services.mozilla.com/D123186
    
    UltraBlame original commit: 8fe18ab1e6c6788299847155fce866b85aff91c3

--- ipc/glue/ProcessUtils.h
+++ ipc/glue/ProcessUtils.h
@@ -53,9 +53,9 @@ class SharedPreferenceDeserializer final {
   SharedPreferenceDeserializer();
   ~SharedPreferenceDeserializer();
 
-  bool DeserializeFromSharedMemory(char* aPrefsHandleStr,
-                                   char* aPrefMapHandleStr, char* aPrefsLenStr,
-                                   char* aPrefMapSizeStr);
+  bool DeserializeFromSharedMemory(uint64_t aPrefsHandle,
+                                   uint64_t aPrefMapHandle, uint64_t aPrefsLen,
+                                   uint64_t aPrefMapSize);
 
   const base::SharedMemoryHandle& GetPrefsHandle() const;
   const FileDescriptor& GetPrefMapHandle() const;
@@ -83,7 +83,7 @@ void ExportSharedJSInit(GeckoChildProcessHost& procHost,
 
 
 
-bool ImportSharedJSInit(char* aJsInitHandleStr, char* aJsInitLenStr);
+bool ImportSharedJSInit(uint64_t aJsInitHandle, uint64_t aJsInitLen);
 
 }  
 }  
