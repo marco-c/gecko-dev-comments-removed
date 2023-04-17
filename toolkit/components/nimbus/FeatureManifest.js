833e91820547c8b139d130729e25755426fbdb59
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 17:23:34 +0000

    Bug 1731794 - Add search feature with experiment variable r=k88hudson,Standard8
    Differential Revision: https://phabricator.services.mozilla.com/D126249
    
    UltraBlame original commit: fd0e9454763674162ec0d0130911ca83435bec63

--- toolkit/components/nimbus/FeatureManifest.js
+++ toolkit/components/nimbus/FeatureManifest.js
@@ -9,6 +9,18 @@ const EXPORTED_SYMBOLS = ["FeatureManifest"];
 
 
 const FeatureManifest = {
+  search: {
+    description: "The Search Services",
+    hasExposure: false,
+    variables: {
+      experiment: {
+        type: "string",
+        fallbackPref: "browser.search.experiment",
+        description:
+          "Used to activate only matching configurations that contain the value in `experiment`",
+      },
+    },
+  },
   urlbar: {
     description: "The Address Bar",
     hasExposure: true,
