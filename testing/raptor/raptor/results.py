020556d3a762d2951de2de6c0a84b00f982de08d
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 01:59:36 +0000

    Bug 1702689 - Fix browsertime_benchmark.js to collect all pagecycle results r=perftest-reviewers,sparky
    Fixed browsertime benchmark script to correctly run the tests, fix python code to collect all the pagecycles data and disable unity-webgl test running on windows7.
    
    Differential Revision: https://phabricator.services.mozilla.com/D110683
    
    UltraBlame original commit: 6c03f84fe3798b6dd1f053979ba9bd1dc92aae53

--- testing/raptor/raptor/results.py
+++ testing/raptor/raptor/results.py
@@ -560,15 +560,8 @@ class BrowsertimeResultsHandler(PerftestResultsHandler):
             custom_types = raw_result["extras"][0]
             if custom_types:
                 for custom_type in custom_types:
-                    
-                    
-                    if any(["youtube" in k for k in custom_types[custom_type]]):
-                        for k, v in custom_types[custom_type].items():
-                            bt_result["measurements"].setdefault(k, []).append([v])
-                    else:
-                        bt_result["measurements"].update(
-                            {k: [v] for k, v in custom_types[custom_type].items()}
-                        )
+                    for k, v in custom_types[custom_type].items():
+                        bt_result["measurements"].setdefault(k, []).append(v)
             else:
                 
                 for bt, raptor in conversion:
