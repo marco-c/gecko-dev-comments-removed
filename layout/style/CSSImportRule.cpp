21aca04b39f35986df3dc6a1cba1c527e1b95e0f
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 06:18:43 +0000

    Bug 1711437 - Don't EnsureUniqueInner from the cssRules getter. r=layout-reviewers,jfkthame
    Instead, fix up the various content data structures when the stylesheet
    is mutated. This makes reading a stylesheet not disable style sharing.
    
    Differential Revision: https://phabricator.services.mozilla.com/D115203
    
    UltraBlame original commit: 2c95330ad7c743340b38707b1171363789b9ede3

--- layout/style/CSSImportRule.cpp
+++ layout/style/CSSImportRule.cpp
@@ -71,9 +71,28 @@ void CSSImportRule::List(FILE* out, int32_t aIndent) const {
 }
 #endif
 
-dom::MediaList* CSSImportRule::GetMedia() const {
+void CSSImportRule::SetRawAfterClone(RefPtr<RawServoImportRule> aRaw) {
+  mRawRule = std::move(aRaw);
+  mChildSheet =
+      const_cast<StyleSheet*>(Servo_ImportRule_GetSheet(mRawRule.get()));
+}
+
+StyleSheet* CSSImportRule::GetStyleSheetForBindings() {
   
-  return mChildSheet ? mChildSheet->Media() : nullptr;
+  
+  
+  
+  
+  if (StyleSheet* parent = GetParentStyleSheet()) {
+    parent->EnsureUniqueInner();
+  }
+  return mChildSheet;
+}
+
+dom::MediaList* CSSImportRule::GetMedia() {
+  auto* sheet = GetStyleSheetForBindings();
+  
+  return sheet ? sheet->Media() : nullptr;
 }
 
 void CSSImportRule::DropSheetReference() {
