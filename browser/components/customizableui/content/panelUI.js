777a21ea3bfea1c9969189164b481f82af30f3c9
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 10:38:16 +0000

    Bug 1720534 - Clean up styles in Bookmarks Menu Button menupopups on Windows 7 and 10. r=desktop-theme-reviewers,dao
    This also gets rid of a dead CSS class (panel-subview-footer).
    
    Differential Revision: https://phabricator.services.mozilla.com/D119906
    
    UltraBlame original commit: cc4e5ea0c9863a8c4f65a49cd51852f7f38dd2fe

--- browser/components/customizableui/content/panelUI.js
+++ browser/components/customizableui/content/panelUI.js
@@ -475,11 +475,6 @@ const PanelUI = {
       document
         .getElementById(CustomizableUI.AREA_NAVBAR)
         .appendChild(tempPanel);
-      
-      tempPanel.classList.toggle(
-        "cui-widget-panelWithFooter",
-        viewNode.querySelector(".panel-subview-footer")
-      );
 
       let multiView = document.createXULElement("panelmultiview");
       multiView.setAttribute("id", "customizationui-widget-multiview");
