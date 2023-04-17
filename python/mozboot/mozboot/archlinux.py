df792bd6962fb0e7c91345c0bc1cc1d3705575ca
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 10:47:56 +0000

    Bug 1718341 - Remove redundant override in MobileAndroidBootstrapper. r=nalexander
    Differential Revision: https://phabricator.services.mozilla.com/D119998
    
    UltraBlame original commit: 31a57851c6d194c26193dfc7e2bfb224aacec791

--- python/mozboot/mozboot/archlinux.py
+++ python/mozboot/mozboot/archlinux.py
@@ -23,12 +23,7 @@ if sys.version_info < (3,):
 class ArchlinuxBootstrapper(LinuxBootstrapper, BaseBootstrapper):
     """Archlinux experimental bootstrapper."""
 
-    SYSTEM_PACKAGES = [
-        "base-devel",
-        "nodejs",
-        "unzip",
-        "zip",
-    ]
+    SYSTEM_PACKAGES = ["base-devel", "nodejs", "unzip", "zip"]
 
     BROWSER_PACKAGES = [
         "alsa-lib",
@@ -48,7 +43,7 @@ class ArchlinuxBootstrapper(LinuxBootstrapper, BaseBootstrapper):
     ]
 
     BROWSER_AUR_PACKAGES = [
-        "https://aur.archlinux.org/cgit/aur.git/snapshot/uuid.tar.gz",
+        "https://aur.archlinux.org/cgit/aur.git/snapshot/uuid.tar.gz"
     ]
 
     MOBILE_ANDROID_COMMON_PACKAGES = [
@@ -76,12 +71,6 @@ class ArchlinuxBootstrapper(LinuxBootstrapper, BaseBootstrapper):
     def install_browser_artifact_mode_packages(self, mozconfig_builder):
         self.ensure_browser_packages(artifact_mode=True)
 
-    def install_mobile_android_packages(self, mozconfig_builder):
-        self.ensure_mobile_android_packages(mozconfig_builder)
-
-    def install_mobile_android_artifact_mode_packages(self, mozconfig_builder):
-        self.ensure_mobile_android_packages(mozconfig_builder, artifact_mode=True)
-
     def ensure_browser_packages(self, artifact_mode=False):
         
         self.aur_install(*self.BROWSER_AUR_PACKAGES)
@@ -91,7 +80,7 @@ class ArchlinuxBootstrapper(LinuxBootstrapper, BaseBootstrapper):
         
         pass
 
-    def ensure_mobile_android_packages(self, mozconfig_builder, artifact_mode=False):
+    def install_mobile_android_packages(self, mozconfig_builder, artifact_mode=False):
         
         
         
@@ -114,7 +103,7 @@ class ArchlinuxBootstrapper(LinuxBootstrapper, BaseBootstrapper):
 
         
         self.ensure_java(mozconfig_builder)
-        super().ensure_mobile_android_packages(artifact_mode=artifact_mode)
+        super().install_mobile_android_packages(artifact_mode=artifact_mode)
 
     def _update_package_manager(self):
         self.pacman_update()
