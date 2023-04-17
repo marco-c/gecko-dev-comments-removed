dc860d32a24ebd2eeb4c5ca12ca9d790581c2058
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 12:15:56 +0000

    Bug 1721395 - Don't send composing text to Gecko when selection is out of composing range. r=geckoview-reviewers,agi
    Actually, Gecko cannot set caret position that is out of composing range. When
    having composition, caret (in other word, IME caret) has to be within composing
    string.
    
    But Android's text framework can set caret position (selection) that is out of
    composing range. Example, Android can have the following. `"ABCD[]"` is current
    text and `[]` is selection and AB is composing string. But Gecko cannot.
    
    So if this situation, we have to remove composing state then set selection from
    Android's.
    
    Differential Revision: https://phabricator.services.mozilla.com/D121502
    
    UltraBlame original commit: f69399871fd24bca52efe2c290ff2fb0c467d3b5

--- mobile/android/geckoview/src/main/java/org/mozilla/geckoview/GeckoEditable.java
+++ mobile/android/geckoview/src/main/java/org/mozilla/geckoview/GeckoEditable.java
@@ -819,12 +819,26 @@ import android.view.inputmethod.EditorInfo;
             }
 
             if (found) {
-                icSendComposition(text, selStart, selEnd, composingStart, composingEnd);
-                if (notifyGecko) {
-                    mFocusedChild.onImeUpdateComposition(
-                            composingStart, composingEnd, updateFlags);
+                if (selStart < composingStart || selEnd > composingEnd) {
+                    
+                    
+                    
+                    
+                    
+                    if (DEBUG) {
+                        final StringBuilder sb = new StringBuilder("icSendComposition(): invalid caret position. ");
+                        sb.append("composing = ").append(composingStart).append("-").append(composingEnd)
+                          .append(", selection = ").append(selStart).append("-").append(selEnd);
+                        Log.d(LOGTAG, sb.toString());
+                    }
+                } else {
+                    icSendComposition(text, selStart, selEnd, composingStart, composingEnd);
+                    if (notifyGecko) {
+                        mFocusedChild.onImeUpdateComposition(
+                                composingStart, composingEnd, updateFlags);
+                    }
+                    return true;
                 }
-                return true;
             }
         }
 
@@ -855,12 +869,11 @@ import android.view.inputmethod.EditorInfo;
             throws RemoteException {
         if (DEBUG) {
             assertOnIcThread();
-            Log.d(LOGTAG, "icSendComposition(\"" + text + "\", " +
-                                             composingStart + ", " + composingEnd + ")");
-        }
-        if (DEBUG) {
-            Log.d(LOGTAG, " range = " + composingStart + "-" + composingEnd);
-            Log.d(LOGTAG, " selection = " + selStart + "-" + selEnd);
+            final StringBuilder sb = new StringBuilder("icSendComposition(");
+            sb.append("\"").append(text).append("\"")
+              .append(", range = ").append(composingStart).append("-").append(composingEnd)
+              .append(", selection = ").append(selStart).append("-").append(selEnd).append(")");
+            Log.d(LOGTAG, sb.toString());
         }
 
         if (selEnd >= composingStart && selEnd <= composingEnd) {
