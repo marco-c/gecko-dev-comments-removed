d0c3268b47844f46debd115de53d386692405c2f
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 16:41:40 +0000

    Bug 1722042 [wpt PR 29765] - EventSource: Convert some htm tests to JS, a=testonly
    Automatic update from web-platform-tests
    EventSource: use .any.js and .window.js more
    
    --
    
    wpt-commits: 00e8611b23fed04f014e882c12980cafc77e2c1b
    wpt-pr: 29765
    
    UltraBlame original commit: 4383a8a8723750747cc5b860bac234da3a395b9a

---
+++ testing/web-platform/tests/eventsource/eventsource-prototype.any.js
@@ -0,0 +1,10 @@
+
+
+      test(function() {
+        EventSource.prototype.ReturnTrue = function() { return true }
+        var source = new EventSource("resources/message.py")
+        assert_true(source.ReturnTrue())
+        assert_own_property(self, "EventSource")
+        source.close()
+      })
+
---
+++ testing/web-platform/tests/eventsource/request-accept.any.js
@@ -0,0 +1,13 @@
+
+      var test = async_test()
+      test.step(function() {
+        var source = new EventSource("resources/accept.event_stream?pipe=sub")
+        source.onmessage = function(e) {
+          test.step(function() {
+            assert_equals(e.data, "text/event-stream")
+            this.close()
+          }, this)
+          test.done()
+        }
+      })
+
