25298163a6781535d52906cf26e93626c3fc8db1
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 10:52:03 +0000

    Bug 1721333 part 9 - Use a reserved slot instead of private slot for RegExpStaticsObject. r=jonco
    Depends on D120314
    
    Differential Revision: https://phabricator.services.mozilla.com/D120315
    
    UltraBlame original commit: 422a5017356aeb3490ffb9ecea7e10f36280afd4

--- js/src/vm/RegExpStaticsObject.h
+++ js/src/vm/RegExpStaticsObject.h
@@ -11,14 +11,24 @@
 
 namespace js {
 
+class RegExpStatics;
+
 class RegExpStaticsObject : public NativeObject {
+  friend class js::RegExpStatics;
+
+  enum { StaticsSlot, SlotCount };
+
  public:
   static const JSClass class_;
+
+  RegExpStatics* regExpStatics() const {
+    return maybePtrFromReservedSlot<RegExpStatics>(StaticsSlot);
+  }
 
   size_t sizeOfData(mozilla::MallocSizeOf mallocSizeOf) {
     
     
-    return mallocSizeOf(getPrivate());
+    return mallocSizeOf(regExpStatics());
   }
 };
 
