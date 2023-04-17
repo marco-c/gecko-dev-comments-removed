57c4fa1b06fff2550891ec14b479d10f526053bb
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 12:16:04 +0000

    Bug 1721326 - Use small stack for DoClone(). r=jld
    Patch author is Florian Weimer <fweimer 'at' redhat.com>
    
    Differential Revision: https://phabricator.services.mozilla.com/D120709
    
    UltraBlame original commit: 7572652a4bbbdbea635e4158328f3bab3f91faad

--- security/sandbox/linux/launch/SandboxLaunch.cpp
+++ security/sandbox/linux/launch/SandboxLaunch.cpp
@@ -509,8 +509,7 @@ static int CloneCallee(void* aPtr) {
 MOZ_NEVER_INLINE MOZ_ASAN_BLACKLIST static pid_t DoClone(int aFlags,
                                                          jmp_buf* aCtx) {
   static constexpr size_t kStackAlignment = 16;
-  uint8_t miniStack[PTHREAD_STACK_MIN]
-      __attribute__((aligned(kStackAlignment)));
+  uint8_t miniStack[4096] __attribute__((aligned(kStackAlignment)));
 #ifdef __hppa__
   void* stackPtr = miniStack;
 #else
@@ -531,13 +530,19 @@ static pid_t ForkWithFlags(int aFlags) {
                                CLONE_CHILD_CLEARTID;
   MOZ_RELEASE_ASSERT((aFlags & kBadFlags) == 0);
 
+  
+  sigset_t oldSigs;
+  BlockAllSignals(&oldSigs);
+
+  int ret = 0;
   jmp_buf ctx;
   if (setjmp(ctx) == 0) {
     
-    return DoClone(aFlags | SIGCHLD, &ctx);
+    ret = DoClone(aFlags | SIGCHLD, &ctx);
   }
+  RestoreSignals(&oldSigs);
   
-  return 0;
+  return ret;
 }
 
 static bool WriteStringToFile(const char* aPath, const char* aStr,
