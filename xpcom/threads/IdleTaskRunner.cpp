2182074c5bc09fa0323db74f067cb16d4b438dd5
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 17:18:31 +0000

    Backed out 2 changesets (bug 1727960) by dev request
    Backed out changeset 14abef8826d0 (bug 1727960)
    Backed out changeset 2817b34b999e (bug 1727960)
    
    UltraBlame original commit: 0f3c29b12e5b0ff97775d6957b7b3f7c5a775bd9

--- xpcom/threads/IdleTaskRunner.cpp
+++ xpcom/threads/IdleTaskRunner.cpp
@@ -156,23 +156,6 @@ static void ScheduleTimedOut(nsITimer* aTimer, void* aClosure) {
   runnable->Schedule(true);
 }
 
-void IdleTaskRunner::ScheduleAfterDelay(TimeDuration aDelay) {
-  if (!mScheduleTimer) {
-    mScheduleTimer = NS_NewTimer();
-    if (!mScheduleTimer) {
-      return;
-    }
-  } else {
-    mScheduleTimer->Cancel();
-  }
-
-  mScheduleTimer->InitWithNamedFuncCallback(
-      ScheduleTimedOut, this,
-      static_cast<unsigned long>(aDelay.ToMilliseconds()),
-      nsITimer::TYPE_ONE_SHOT_LOW_PRIORITY, mName);
-  return;
-}
-
 void IdleTaskRunner::Schedule(bool aAllowIdleDispatch) {
   if (!mCallback) {
     return;
@@ -186,15 +169,17 @@ void IdleTaskRunner::Schedule(bool aAllowIdleDispatch) {
   mDeadline = TimeStamp();
 
   TimeStamp now = TimeStamp::Now();
-  
-  if (now < mStartTime) {
+  bool useRefreshDriver = false;
+  if (now >= mStartTime) {
     
     
-    ScheduleAfterDelay(mStartTime - now + TimeDuration::FromMilliseconds(1));
-    return;
+    useRefreshDriver = (nsRefreshDriver::GetIdleDeadlineHint(now) != now);
+  } else {
+    NS_WARNING_ASSERTION(!aAllowIdleDispatch,
+                         "early callback, or time went backwards");
   }
 
-  if (nsRefreshDriver::IsRegularRateTimerTicking()) {
+  if (useRefreshDriver) {
     if (!mTask) {
       
       mTask = new IdleTaskRunnerTask(this);
@@ -203,23 +188,38 @@ void IdleTaskRunner::Schedule(bool aAllowIdleDispatch) {
     }
     
     SetTimerInternal(mMaxDelay);
-    return;
-  }
-
-  
-  
-  if (!aAllowIdleDispatch) {
-    ScheduleAfterDelay(TimeDuration::FromMilliseconds(16));
-    return;
-  }
-
-  SetTimerInternal(mMaxDelay);
-  if (!mTask) {
+  } else {
     
-    
-    mTask = new IdleTaskRunnerTask(this);
-    RefPtr<Task> task(mTask);
-    TaskController::Get()->AddTask(task.forget());
+    if (aAllowIdleDispatch) {
+      SetTimerInternal(mMaxDelay);
+      if (!mTask) {
+        
+        
+        mTask = new IdleTaskRunnerTask(this);
+        RefPtr<Task> task(mTask);
+        TaskController::Get()->AddTask(task.forget());
+      }
+    } else {
+      if (!mScheduleTimer) {
+        mScheduleTimer = NS_NewTimer();
+        if (!mScheduleTimer) {
+          return;
+        }
+      } else {
+        mScheduleTimer->Cancel();
+      }
+      
+      
+      uint32_t waitToSchedule = 16; 
+      if (now < mStartTime) {
+        
+        
+        waitToSchedule = (mStartTime - now).ToMilliseconds() + 1;
+      }
+      mScheduleTimer->InitWithNamedFuncCallback(
+          ScheduleTimedOut, this, waitToSchedule,
+          nsITimer::TYPE_ONE_SHOT_LOW_PRIORITY, mName);
+    }
   }
 }
 
