077deb983321466eeb20a160395dbfbebe4df731
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 13:24:07 +0000

    Bug 1725339 - Restrict systemprincipal from loading type *DOCUMENT* via HTTP, HTTPS and data schemes (data restriction preffed OFF). r=ckerschb
    Differential Revision: https://phabricator.services.mozilla.com/D122420
    
    UltraBlame original commit: ed0cca70a9a523ddcf47ddaacfe87fbada4a462e

--- dom/security/nsContentSecurityManager.cpp
+++ dom/security/nsContentSecurityManager.cpp
@@ -957,16 +957,8 @@ void nsContentSecurityManager::MeasureUnexpectedPrivilegedLoads(
 
 nsresult nsContentSecurityManager::CheckAllowLoadInSystemPrivilegedContext(
     nsIChannel* aChannel) {
-  
-  
   nsCOMPtr<nsILoadInfo> loadInfo = aChannel->LoadInfo();
 
-  
-  
-  if (!loadInfo->GetLoadingPrincipal() ||
-      !loadInfo->GetLoadingPrincipal()->IsSystemPrincipal()) {
-    return NS_OK;
-  }
   
   
   if (loadInfo->GetAllowDeprecatedSystemRequests()) {
@@ -975,6 +967,20 @@ nsresult nsContentSecurityManager::CheckAllowLoadInSystemPrivilegedContext(
 
   ExtContentPolicyType contentPolicyType =
       loadInfo->GetExternalContentPolicyType();
+
+  
+  
+  nsCOMPtr<nsIPrincipal> inspectedPrincipal;
+  if (contentPolicyType != ExtContentPolicy::TYPE_DOCUMENT) {
+    inspectedPrincipal = loadInfo->GetLoadingPrincipal();
+  } else {
+    inspectedPrincipal = loadInfo->TriggeringPrincipal();
+  }
+
+  
+  if (!inspectedPrincipal || !inspectedPrincipal->IsSystemPrincipal()) {
+    return NS_OK;
+  }
 
   
   
@@ -1047,9 +1053,7 @@ nsresult nsContentSecurityManager::CheckAllowLoadInSystemPrivilegedContext(
   }
   
   
-  if (finalURI) {
-    MeasureUnexpectedPrivilegedLoads(loadInfo, finalURI, remoteType);
-  }
+  MeasureUnexpectedPrivilegedLoads(loadInfo, finalURI, remoteType);
 
   
   
@@ -1073,8 +1077,32 @@ nsresult nsContentSecurityManager::CheckAllowLoadInSystemPrivilegedContext(
   nsAutoCString requestedURL;
   finalURI->GetAsciiSpec(requestedURL);
   MOZ_LOG(sCSMLog, LogLevel::Warning,
-          ("SystemPrincipal must not load remote documents. URL: %s, type %d",
+          ("SystemPrincipal should not load remote resources. URL: %s, type %d",
            requestedURL.get(), int(contentPolicyType)));
+
+  
+  
+  
+  
+  
+  if (contentPolicyType == ExtContentPolicy::TYPE_DOCUMENT) {
+    if (StaticPrefs::security_disallow_privileged_https_documents_loads() &&
+        (finalURI->SchemeIs("http") || finalURI->SchemeIs("https"))) {
+#ifdef DEBUG
+      MOZ_CRASH("Disallowing SystemPrincipal load of documents on HTTP(S).");
+#endif
+      aChannel->Cancel(NS_ERROR_CONTENT_BLOCKED);
+      return NS_ERROR_CONTENT_BLOCKED;
+    }
+    if ((StaticPrefs::security_disallow_privileged_data_documents_loads()) &&
+        (finalURI->SchemeIs("data"))) {
+#ifdef DEBUG
+      MOZ_CRASH("Disallowing SystemPrincipal load of documents on data URL.");
+#endif
+      aChannel->Cancel(NS_ERROR_CONTENT_BLOCKED);
+      return NS_ERROR_CONTENT_BLOCKED;
+    }
+  }
 
   if (cancelNonLocalSystemPrincipal) {
     MOZ_ASSERT(false, "SystemPrincipal must not load remote documents.");
