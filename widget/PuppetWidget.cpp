e9ff7aab606ee2a88728d96b6987c9b191715e6a
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 19:44:08 +0000

    Bug 1736086 - Rename CreateBasicLayerManager to CreateFallbackRenderer. r=gfx-reviewers,mstange
    That's what it actually does now
    
    Differential Revision: https://phabricator.services.mozilla.com/D128634
    
    UltraBlame original commit: 8a81b9ad4e2977227046b5af9e7996b26d423dcb

--- widget/PuppetWidget.cpp
+++ widget/PuppetWidget.cpp
@@ -609,7 +609,7 @@ WindowRenderer* PuppetWidget::GetWindowRenderer() {
     
     MOZ_ASSERT(!mBrowserChild ||
                mBrowserChild->IsLayersConnected() != Some(true));
-    mWindowRenderer = CreateBasicLayerManager();
+    mWindowRenderer = CreateFallbackRenderer();
   }
 
   return mWindowRenderer;
