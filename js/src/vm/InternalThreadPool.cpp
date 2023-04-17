5b5adf6aa5f6c2fd4eef7fde44acc7560d3a0d77
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 08:41:55 +0000

    Bug 1714141 - Use dispatch callback for internal thread pool r=sfink
    We can use the dispatch task callback for the internal thread pool too,
    removing some code specific to the latter.
    
    Differential Revision: https://phabricator.services.mozilla.com/D117161
    
    UltraBlame original commit: 9993292e9eccd9d698b5c29930b117ee74a42923

--- js/src/vm/InternalThreadPool.cpp
+++ js/src/vm/InternalThreadPool.cpp
@@ -155,11 +155,12 @@ size_t InternalThreadPool::sizeOfIncludingThis(
          threads(lock).sizeOfExcludingThis(mallocSizeOf);
 }
 
-void InternalThreadPool::dispatchTask(const AutoLockHelperThreadState& lock) {
-  notifyOne(lock);
-}
 
-void InternalThreadPool::notifyOne(const AutoLockHelperThreadState& lock) {
+void InternalThreadPool::DispatchTask() { Get().dispatchTask(); }
+
+void InternalThreadPool::dispatchTask() {
+  gHelperThreadLock.assertOwnedByCurrentThread();
+  queuedTasks++;
   wakeup.notify_one();
 }
 
@@ -240,20 +241,13 @@ void HelperThread::threadLoop(InternalThreadPool* pool) {
   AutoLockHelperThreadState lock;
 
   while (!pool->terminating) {
-    
-    
-    
-    
-    
-
-    HelperThreadTask* task = HelperThreadState().findHighestPriorityTask(lock);
-    if (!task) {
-      AUTO_PROFILER_LABEL("HelperThread::threadLoop::wait", IDLE);
-      pool->wait(lock);
+    if (pool->queuedTasks != 0) {
+      pool->queuedTasks--;
+      HelperThreadState().runOneTask(lock);
       continue;
     }
 
-    HelperThreadState().runTaskLocked(task, lock);
-    HelperThreadState().notifyAll(GlobalHelperThreadState::CONSUMER, lock);
+    AUTO_PROFILER_LABEL("HelperThread::threadLoop::wait", IDLE);
+    pool->wait(lock);
   }
 }
