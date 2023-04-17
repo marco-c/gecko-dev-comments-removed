587cc378f17f0474394c0b31b5a4410f2d58b445
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 12:51:04 +0000

    Bug 1719550 - Unify collator in mozStorageService and SQLCollations; r=platform-i18n-reviewers,nordzilla
    Differential Revision: https://phabricator.services.mozilla.com/D121432
    
    UltraBlame original commit: b4b0d58778e9e3b366937aa6f28b75ab3a5ca631

--- storage/mozStorageService.h
+++ storage/mozStorageService.h
@@ -8,17 +8,21 @@
 #define MOZSTORAGESERVICE_H
 
 #include "nsCOMPtr.h"
-#include "nsICollation.h"
 #include "nsIFile.h"
 #include "nsIMemoryReporter.h"
 #include "nsIObserver.h"
 #include "nsTArray.h"
 #include "mozilla/Mutex.h"
+#include "mozilla/UniquePtr.h"
+#include "mozilla/intl/Collator.h"
 
 #include "mozIStorageService.h"
 
 class nsIMemoryReporter;
 struct sqlite3_vfs;
+namespace mozilla::intl {
+class Collator;
+}
 
 namespace mozilla {
 namespace storage {
@@ -47,7 +51,7 @@ class Service : public mozIStorageService,
 
 
   int localeCompareStrings(const nsAString& aStr1, const nsAString& aStr2,
-                           int32_t aComparisonStrength);
+                           mozilla::intl::Collator::Sensitivity aSensitivity);
 
   static already_AddRefed<Service> getSingleton();
 
@@ -153,7 +157,7 @@ class Service : public mozIStorageService,
 
 
 
-  nsICollation* getLocaleCollation();
+  mozilla::intl::Collator* getCollator();
 
   
 
@@ -162,13 +166,15 @@ class Service : public mozIStorageService,
 
 
 
-  nsCOMPtr<nsICollation> mLocaleCollation;
+  mozilla::UniquePtr<mozilla::intl::Collator> mCollator = nullptr;
 
   nsCOMPtr<nsIFile> mProfileStorageFile;
 
   nsCOMPtr<nsIMemoryReporter> mStorageSQLiteReporter;
 
   static Service* gService;
+
+  mozilla::intl::Collator::Sensitivity mLastSensitivity;
 };
 
 }  
