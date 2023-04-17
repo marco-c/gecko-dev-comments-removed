cf96d87ba977c932d6bcc8475711835e931446a3
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 00:53:25 +0000

    Bug 1702542 - Fixes eslint errors in browser/extensions/screenshots/background/takeshot.js. r=emalysz
    Differential Revision: https://phabricator.services.mozilla.com/D110757
    
    UltraBlame original commit: 92b6449bea12bca78b5f90e6d78708acaf0b434e

--- browser/extensions/screenshots/background/takeshot.js
+++ browser/extensions/screenshots/background/takeshot.js
@@ -8,13 +8,14 @@
 
 this.takeshot = (function() {
   const exports = {};
-  const Shot = shot.AbstractShot;
-  const { sendEvent, incrementCount } = analytics;
   const MAX_CANVAS_DIMENSION = 32767;
 
-  communication.register("screenshotPage", (sender, selectedPos, isFullPage, devicePixelRatio) => {
-    return screenshotPage(selectedPos, isFullPage, devicePixelRatio);
-  });
+  communication.register(
+    "screenshotPage",
+    (sender, selectedPos, isFullPage, devicePixelRatio) => {
+      return screenshotPage(selectedPos, isFullPage, devicePixelRatio);
+    }
+  );
 
   function screenshotPage(pos, isFullPage, devicePixelRatio) {
     pos.width = Math.min(pos.right - pos.left, MAX_CANVAS_DIMENSION);
@@ -22,14 +23,14 @@ this.takeshot = (function() {
 
     
     
-    let options = {format: "png"};
+    let options = { format: "png" };
     if (isFullPage) {
       let rectangle = {
         x: 0,
         y: 0,
         width: pos.width,
         height: pos.height,
-      }
+      };
       options.rect = rectangle;
 
       
@@ -41,138 +42,39 @@ this.takeshot = (function() {
         y: pos.top,
         width: pos.width,
         height: pos.height,
-      }
-      options.rect = rectangle
+      };
+      options.rect = rectangle;
     }
 
-    return catcher.watchPromise(browser.tabs.captureTab(
-      null,
-      options,
-    ).then((dataUrl) => {
-      const image = new Image();
-      image.src = dataUrl;
-      return new Promise((resolve, reject) => {
-        image.onload = catcher.watchFunction(() => {
-          const xScale = devicePixelRatio;
-          const yScale = devicePixelRatio;
-          const canvas = document.createElement("canvas");
-          canvas.height = pos.height * yScale;
-          canvas.width = pos.width * xScale;
-          const context = canvas.getContext("2d");
-          context.drawImage(
-            image,
-            0, 0,
-            pos.width * xScale, pos.height * yScale,
-            0, 0,
-            pos.width * xScale, pos.height * yScale
-          );
-          const result = canvas.toDataURL();
-          resolve(result);
+    return catcher.watchPromise(
+      browser.tabs.captureTab(null, options).then(dataUrl => {
+        const image = new Image();
+        image.src = dataUrl;
+        return new Promise((resolve, reject) => {
+          image.onload = catcher.watchFunction(() => {
+            const xScale = devicePixelRatio;
+            const yScale = devicePixelRatio;
+            const canvas = document.createElement("canvas");
+            canvas.height = pos.height * yScale;
+            canvas.width = pos.width * xScale;
+            const context = canvas.getContext("2d");
+            context.drawImage(
+              image,
+              0,
+              0,
+              pos.width * xScale,
+              pos.height * yScale,
+              0,
+              0,
+              pos.width * xScale,
+              pos.height * yScale
+            );
+            const result = canvas.toDataURL();
+            resolve(result);
+          });
         });
-      });
-    }));
-  }
-
-  
-  function concatBuffers(buffer1, buffer2) {
-    const tmp = new Uint8Array(buffer1.byteLength + buffer2.byteLength);
-    tmp.set(new Uint8Array(buffer1), 0);
-    tmp.set(new Uint8Array(buffer2), buffer1.byteLength);
-    return tmp.buffer;
-  }
-
-  
-
-
-
-
-
-  function createMultipart(fields, files) {
-    const boundary = "---------------------------ScreenshotBoundary" + Date.now();
-    let body = [];
-    for (const name in fields) {
-      body.push("--" + boundary);
-      body.push(`Content-Disposition: form-data; name="${name}"`);
-      body.push("");
-      body.push(fields[name]);
-    }
-    body.push("");
-    body = body.join("\r\n");
-    const enc = new TextEncoder("utf-8");
-    body = enc.encode(body).buffer;
-
-    const blobToArrayPromises = files.map(f => {
-      return blobConverters.blobToArray(f.blob);
-    });
-
-    return Promise.all(blobToArrayPromises).then(buffers => {
-      for (let i = 0; i < buffers.length; i++) {
-        let filePart = [];
-        filePart.push("--" + boundary);
-        filePart.push(`Content-Disposition: form-data; name="${files[i].fieldName}"; filename="${files[i].filename}"`);
-        filePart.push(`Content-Type: ${files[i].blob.type}`);
-        filePart.push("");
-        filePart.push("");
-        filePart = filePart.join("\r\n");
-        filePart = concatBuffers(enc.encode(filePart).buffer, buffers[i]);
-        body = concatBuffers(body, filePart);
-        body = concatBuffers(body, enc.encode("\r\n").buffer);
-      }
-
-      let tail = `\r\n--${boundary}--`;
-      tail = enc.encode(tail);
-      body = concatBuffers(body, tail.buffer);
-      return {
-        "content-type": `multipart/form-data; boundary=${boundary}`,
-        body,
-      };
-    });
-  }
-
-  function uploadShot(shot, blob, thumbnail) {
-    let headers;
-    return auth.authHeaders().then((_headers) => {
-      headers = _headers;
-      if (blob) {
-        const files = [ {fieldName: "blob", filename: "screenshot.png", blob} ];
-        if (thumbnail) {
-          files.push({fieldName: "thumbnail", filename: "thumbnail.png", blob: thumbnail});
-        }
-        return createMultipart(
-          {shot: JSON.stringify(shot)},
-
-          files
-        );
-      }
-      return {
-        "content-type": "application/json",
-        body: JSON.stringify(shot),
-      };
-
-    }).then((submission) => {
-      headers["content-type"] = submission["content-type"];
-      sendEvent("upload", "started", {eventValue: Math.floor(submission.body.length / 1000)});
-      return fetch(shot.jsonUrl, {
-        method: "PUT",
-        mode: "cors",
-        headers,
-        body: submission.body,
-      });
-    }).then((resp) => {
-      if (!resp.ok) {
-        sendEvent("upload-failed", `status-${resp.status}`);
-        const exc = new Error(`Response failed with status ${resp.status}`);
-        exc.popupMessage = "REQUEST_ERROR";
-        throw exc;
-      } else {
-        sendEvent("upload", "success");
-      }
-    }, (error) => {
-      
-      sendEvent("upload-failed", "connection");
-      error.popupMessage = "CONNECTION_ERROR";
-      throw error;
-    });
+      })
+    );
   }
 
   return exports;
