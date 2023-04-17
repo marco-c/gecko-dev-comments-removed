3ca01cb97869c36a1271edc4f4f0012d4301b5ac
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 19:32:19 +0000

    Bug 1730998 - Use `control` priority for RequestContentRepaint and NotifyFlushComplete, and process them in an early refresh driver runner. r=botond,smaug
    Depends on D127030
    
    Differential Revision: https://phabricator.services.mozilla.com/D127032
    
    UltraBlame original commit: 31bba792417df1e319efdac2ffdde3469ac89bc0

--- gfx/layers/ipc/APZChild.h
+++ gfx/layers/ipc/APZChild.h
@@ -8,6 +8,7 @@
 #define mozilla_layers_APZChild_h
 
 #include "mozilla/layers/PAPZChild.h"
+#include "mozilla/layers/APZTaskRunnable.h"
 
 namespace mozilla {
 namespace layers {
@@ -60,7 +61,16 @@ class APZChild final : public PAPZChild {
   mozilla::ipc::IPCResult RecvDestroy();
 
  private:
+  void EnsureAPZTaskRunnable() {
+    if (!mAPZTaskRunnable) {
+      mAPZTaskRunnable = new APZTaskRunnable(mController);
+    }
+  }
+
   RefPtr<GeckoContentController> mController;
+  
+  
+  RefPtr<APZTaskRunnable> mAPZTaskRunnable;
 };
 
 }  
