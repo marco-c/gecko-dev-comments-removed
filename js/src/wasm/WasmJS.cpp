234bb29117b33b5e2325c6f4d00b63ee9f264ce2
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 15:02:57 +0000

    Bug 1727374 - Root Init dictionaries to fix rooting hazards. r=peterv
    Differential Revision: https://phabricator.services.mozilla.com/D123633
    
    UltraBlame original commit: 65e88b6f9cee0f9ba9ef4466fcebbca266ce8cbe

--- js/src/wasm/WasmJS.cpp
+++ js/src/wasm/WasmJS.cpp
@@ -1764,7 +1764,8 @@ WasmInstanceObject* WasmInstanceObject::create(
     const GlobalDescVector& globals, const ValVector& globalImportValues,
     const WasmGlobalObjectVector& globalObjs, HandleObject proto,
     UniqueDebugState maybeDebug) {
-  UniquePtr<ExportMap> exports = js::MakeUnique<ExportMap>(cx->zone());
+  Rooted<UniquePtr<ExportMap>> exports(cx,
+                                       js::MakeUnique<ExportMap>(cx->zone()));
   if (!exports) {
     ReportOutOfMemory(cx);
     return nullptr;
@@ -1776,6 +1777,8 @@ WasmInstanceObject* WasmInstanceObject::create(
     ReportOutOfMemory(cx);
     return nullptr;
   }
+  
+  
 
   uint32_t indirectGlobals = 0;
 
