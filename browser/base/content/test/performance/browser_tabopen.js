7ab99aca749980865ba045f3526e7d621ea8fe2b
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 01:17:05 +0000

    Bug 1703560 - Make browser_tabopen.js flicker test more resilient to changes to newtab button size. r=jaws
    Differential Revision: https://phabricator.services.mozilla.com/D111124
    
    UltraBlame original commit: edf8a5f5141f4603a0494a5a7ce377514c1f6ff4

--- browser/base/content/test/performance/browser_tabopen.js
+++ browser/base/content/test/performance/browser_tabopen.js
@@ -53,6 +53,9 @@ add_task(async function() {
   let minTabWidth = firstTabRect.width - 2 * tabPaddingStart;
   let maxTabWidth = firstTabRect.width;
   let firstTabLabelRect = gBrowser.selectedTab.textLabel.getBoundingClientRect();
+  let newTabButtonRect = document
+    .getElementById("tabs-newtab-button")
+    .getBoundingClientRect();
   let textBoxRect = gURLBar
     .querySelector("moz-input-box")
     .getBoundingClientRect();
@@ -98,7 +101,10 @@ add_task(async function() {
                     firstTabRect.right - 1, 
                     firstTabRect.right + firstTabRect.width
                   ) &&
-                    r.x2 < firstTabRect.right + firstTabRect.width + 25) || 
+                    r.x2 <
+                      firstTabRect.right +
+                        firstTabRect.width +
+                        newTabButtonRect.width) || 
                     
                     
                     
