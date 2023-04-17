acb14c26dcff4bd6a45815a6d8cc6adcc24f155f
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 04:43:14 +0000

    Bug 1709413 - Fix platform check in the gpt cron. r=perftest-reviewers,Bebe
    Differential Revision: https://phabricator.services.mozilla.com/D114260
    
    UltraBlame original commit: d3892e98807b4b677ce87f3d023783998c395ff6

--- taskcluster/taskgraph/target_tasks.py
+++ taskcluster/taskgraph/target_tasks.py
@@ -754,7 +754,7 @@ def target_tasks_general_perf_testing(full_task_graph, parameters, graph_config)
             return False
 
         
-        if "windows7" in platform or "windows10-32":
+        if "windows7" in platform or "windows10-32" in platform:
             return False
 
         
