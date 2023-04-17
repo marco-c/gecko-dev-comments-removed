5a5184d463385c854e7f5252c7828e6f25f90cda
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 18:18:49 +0000

    Bug 1733569 - Improve windows OS dark mode support. r=dholbert,dao
    This exposes the system colors that were unexposed in bug 1733354, but
    builds up on top of bug 1733569 to get buttons and richlistboxes working
    properly.
    
    This causes windows dialogs to be dark in dark mode, etc.
    
    To be landed after the freeze, since it likely has some UI fallout that
    I missed.
    
    Differential Revision: https://phabricator.services.mozilla.com/D127260
    
    UltraBlame original commit: d39b23efefe7e7d89d8dfd0ba188ad6af2b0a9ed

--- widget/nsXPLookAndFeel.cpp
+++ widget/nsXPLookAndFeel.cpp
@@ -604,16 +604,6 @@ nscolor nsXPLookAndFeel::GetStandinForNativeColor(ColorID aID,
 #undef COLOR
 
 
-
-
-
-
-
-
-
-
-
-
 Maybe<nscolor> nsXPLookAndFeel::GenericDarkColor(ColorID aID) {
   nscolor color = NS_RGB(0, 0, 0);
   switch (aID) {
@@ -623,28 +613,23 @@ Maybe<nscolor> nsXPLookAndFeel::GenericDarkColor(ColorID aID) {
     case ColorID::TextBackground:
       color = NS_RGB(28, 27, 34);
       break;
-#ifndef XP_WIN
     case ColorID::MozDialog:  
       color = NS_RGB(35, 34, 43);
       break;
-#endif
     case ColorID::Windowtext:  
     case ColorID::WindowForeground:
     case ColorID::TextForeground:
-#ifndef XP_WIN
     case ColorID::MozDialogtext:
     case ColorID::Fieldtext:
     case ColorID::Buttontext:  
                                
     case ColorID::MozButtonhovertext:
     case ColorID::MozButtonactivetext:
-#endif
       color = NS_RGB(251, 251, 254);
       break;
     case ColorID::Graytext:  
       color = NS_RGB(191, 191, 201);
       break;
-#ifndef XP_WIN
     case ColorID::Selecteditem:  
                                  
       color = NS_RGB(0, 221, 255);
@@ -661,7 +646,6 @@ Maybe<nscolor> nsXPLookAndFeel::GenericDarkColor(ColorID aID) {
     case ColorID::MozButtonactiveface:  
       color = NS_RGB(91, 91, 102);
       break;
-#endif
     case ColorID::Highlight:
       color = NS_RGBA(0, 221, 255, 153);
       break;
