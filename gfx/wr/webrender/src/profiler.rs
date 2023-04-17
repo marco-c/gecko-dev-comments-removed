4a9ebcad5b87532b3ffc2bec31a42345cf8de679
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 18:24:40 +0000

    Backed out 4 changesets (bug 1733335) for causing build bustages. CLOSED TREE
    Backed out changeset 45a3ae02f837 (bug 1733335)
    Backed out changeset 41c62acfb5a6 (bug 1733335)
    Backed out changeset cf3b6b6cc6c6 (bug 1733335)
    Backed out changeset b919d9cfb8e5 (bug 1733335)
    
    UltraBlame original commit: abe797b3571afe7694de7e1d1e8e2a7627ce5cb8

--- gfx/wr/webrender/src/profiler.rs
+++ gfx/wr/webrender/src/profiler.rs
@@ -29,6 +29,7 @@ use api::units::DeviceIntSize;
 use std::collections::vec_deque::VecDeque;
 use std::fmt::{Write, Debug};
 use std::f32;
+use std::ffi::CStr;
 use std::ops::Range;
 use std::time::Duration;
 use time::precise_time_ns;
@@ -1186,22 +1187,25 @@ pub trait ProfilerHooks : Send + Sync {
     fn unregister_thread(&self);
 
     
-    fn begin_marker(&self, label: &str);
-
     
-    fn end_marker(&self, label: &str);
-
-    
-    fn event_marker(&self, label: &str);
+    fn begin_marker(&self, label: &CStr);
 
     
     
+    fn end_marker(&self, label: &CStr);
+
+    
+    
+    fn event_marker(&self, label: &CStr);
+
     
     
     
     
     
-    fn add_text_marker(&self, label: &str, text: &str, duration: Duration);
+    
+    
+    fn add_text_marker(&self, label: &CStr, text: &str, duration: Duration);
 
     
     fn thread_is_being_profiled(&self) -> bool;
@@ -1223,7 +1227,7 @@ pub fn set_profiler_hooks(hooks: Option<&'static dyn ProfilerHooks>) {
 
 
 pub struct ProfileScope {
-    name: &'static str,
+    name: &'static CStr,
 }
 
 
@@ -1247,7 +1251,7 @@ pub fn unregister_thread() {
 }
 
 
-pub fn add_text_marker(label: &str, text: &str, duration: Duration) {
+pub fn add_text_marker(label: &CStr, text: &str, duration: Duration) {
     unsafe {
         if let Some(ref hooks) = PROFILER_HOOKS {
             hooks.add_text_marker(label, text, duration);
@@ -1256,7 +1260,7 @@ pub fn add_text_marker(label: &str, text: &str, duration: Duration) {
 }
 
 
-pub fn add_event_marker(label: &str) {
+pub fn add_event_marker(label: &CStr) {
     unsafe {
         if let Some(ref hooks) = PROFILER_HOOKS {
             hooks.event_marker(label);
@@ -1273,7 +1277,7 @@ pub fn thread_is_being_profiled() -> bool {
 
 impl ProfileScope {
     
-    pub fn new(name: &'static str) -> Self {
+    pub fn new(name: &'static CStr) -> Self {
         unsafe {
             if let Some(ref hooks) = PROFILER_HOOKS {
                 hooks.begin_marker(name);
@@ -1299,7 +1303,7 @@ impl Drop for ProfileScope {
 
 macro_rules! profile_marker {
     ($string:expr) => {
-        let _scope = $crate::profiler::ProfileScope::new($string);
+        let _scope = $crate::profiler::ProfileScope::new(cstr!($string));
     };
 }
 
