e5c812abcc44dfee6540567ec16094e4bb0179cd
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 00:53:02 +0000

    Bug 1690956. Add a memory reporter for swgl. r=lsalzman
    Differential Revision: https://phabricator.services.mozilla.com/D110834
    
    UltraBlame original commit: e8e7a0761a1bd03ffb4a6c1b2de4c6be2d77412a

--- gfx/wr/webrender/src/renderer/mod.rs
+++ gfx/wr/webrender/src/renderer/mod.rs
@@ -5287,7 +5287,7 @@ impl Renderer {
         report += self.texture_upload_pbo_pool.report_memory();
 
         
-        report += self.device.report_memory();
+        report += self.device.report_memory(self.size_of_ops.as_ref().unwrap());
 
         report
     }
