e8254f10c5131e0cf5528c752480f1a3f309bf8c
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 01:57:18 +0000

    Bug 1704338. Factor out doubleTapOn test function. r=botond
    Differential Revision: https://phabricator.services.mozilla.com/D111553
    
    UltraBlame original commit: 02fa623250a4a2a43bb03b1e723e0f71467145aa

--- gfx/layers/apz/test/mochitest/apz_test_native_event_utils.js
+++ gfx/layers/apz/test/mochitest/apz_test_native_event_utils.js
@@ -783,7 +783,11 @@ function synthesizeNativeTap(aTarget, aX, aY, aObserver = null) {
   return true;
 }
 
+
 function synthesizeNativeTouchpadDoubleTap(aTarget, aX, aY) {
+  ok(getPlatform() == "mac",
+    "only implemented on mac. implement sendNativeTouchpadDoubleTap for this platform," +
+    " see bug 1696802 for how it was done on macOS");
   let pt = coordinatesRelativeToScreen({
     offsetX: aX,
     offsetY: aY,
@@ -1348,4 +1352,22 @@ async function pinchZoomOutWithTouchAtCenter() {
   var zoom_out = pinchZoomOutTouchSequenceAtCenter();
   var touchIds = [0, 1];
   await synthesizeNativeTouchAndWaitForTransformEnd(zoom_out, touchIds);
+}
+
+
+async function doubleTapOn(element, x, y, useTouchpad) {
+  let transformEndPromise = promiseTransformEnd();
+
+  if (useTouchpad) {
+    synthesizeNativeTouchpadDoubleTap(element, x, y);
+  } else {
+    synthesizeNativeTap(element, x, y);
+    synthesizeNativeTap(element, x, y);
+  }
+
+  
+  await transformEndPromise;
+
+  
+  await promiseOnlyApzControllerFlushed();
 }
