203e9ab7dfc9033ccb4f2ce3e416487d9e4f8461
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 15:40:17 +0000

    Bug 1721686 - Add test for exposed interfaces on WebAssembly namespace. r=smaug
    This commit modifies test_interfaces.js to also test the exposed interfaces
    in the WebAssembly namespace. We have conditional features that we'd like
    to have confidence that we're not accidentally exposing to web content.
    
    Currently there are:
      * WebAssembly exceptions, enabled only in nightly with a default-off pref
      * mozIntGemm accelerator function, available only in system or addon principals
    
    Depends on D120731
    
    Differential Revision: https://phabricator.services.mozilla.com/D120732
    
    UltraBlame original commit: 482ce06ec0d28d60ba272f433ee0d21f5a8fe7bf

--- dom/workers/test/test_worker_interfaces.js
+++ dom/workers/test/test_worker_interfaces.js
@@ -27,6 +27,28 @@
 
 
 
+var wasmGlobalEntry = {
+  name: "WebAssembly",
+  insecureContext: true,
+  disabled: !getJSTestingFunctions().wasmIsSupportedByHardware(),
+};
+var wasmGlobalInterfaces = [
+  { name: "Module", insecureContext: true },
+  { name: "Instance", insecureContext: true },
+  { name: "Memory", insecureContext: true },
+  { name: "Table", insecureContext: true },
+  { name: "Global", insecureContext: true },
+  { name: "CompileError", insecureContext: true },
+  { name: "LinkError", insecureContext: true },
+  { name: "RuntimeError", insecureContext: true },
+  {
+    name: "Function",
+    insecureContext: true,
+    nightly: true,
+  },
+];
+
+
 var ecmaGlobals = [
   { name: "AggregateError", insecureContext: true },
   { name: "Array", insecureContext: true },
@@ -85,11 +107,7 @@ var ecmaGlobals = [
   { name: "WeakMap", insecureContext: true },
   { name: "WeakRef", insecureContext: true },
   { name: "WeakSet", insecureContext: true },
-  {
-    name: "WebAssembly",
-    insecureContext: true,
-    disabled: !getJSTestingFunctions().wasmIsSupportedByHardware(),
-  },
+  wasmGlobalEntry,
 ];
 
 
@@ -293,16 +311,40 @@ var interfaceNamesInGlobalScope = [
 ];
 
 
-function createInterfaceMap({
-  isNightly,
-  isEarlyBetaOrEarlier,
-  isRelease,
-  isDesktop,
-  isAndroid,
-  isInsecureContext,
-  isFennec,
-  isCrossOringinIsolated,
-}) {
+function entryDisabled(
+  entry,
+  {
+    isNightly,
+    isEarlyBetaOrEarlier,
+    isRelease,
+    isDesktop,
+    isAndroid,
+    isInsecureContext,
+    isFennec,
+    isCrossOringinIsolated,
+  }
+) {
+  return (
+    entry.nightly === !isNightly ||
+    (entry.nightlyAndroid === !(isAndroid && isNightly) && isAndroid) ||
+    entry.desktop === !isDesktop ||
+    (entry.android === !isAndroid && !entry.nightlyAndroid) ||
+    entry.fennecOrDesktop === (isAndroid && !isFennec) ||
+    entry.fennec === !isFennec ||
+    entry.release === !isRelease ||
+    
+    
+    
+    
+    
+    (isInsecureContext && !entry.insecureContext) ||
+    entry.earlyBetaOrEarlier === !isEarlyBetaOrEarlier ||
+    entry.crossOringinIsolated === !isCrossOringinIsolated ||
+    entry.disabled
+  );
+}
+
+function createInterfaceMap(data, ...interfaceGroups) {
   var interfaceMap = {};
 
   function addInterfaces(interfaces) {
@@ -311,41 +353,21 @@ function createInterfaceMap({
         interfaceMap[entry] = !isInsecureContext;
       } else {
         ok(!("pref" in entry), "Bogus pref annotation for " + entry.name);
-        if (
-          entry.nightly === !isNightly ||
-          (entry.nightlyAndroid === !(isAndroid && isNightly) && isAndroid) ||
-          entry.desktop === !isDesktop ||
-          (entry.android === !isAndroid && !entry.nightlyAndroid) ||
-          entry.fennecOrDesktop === (isAndroid && !isFennec) ||
-          entry.fennec === !isFennec ||
-          entry.release === !isRelease ||
-          
-          
-          
-          
-          
-          (isInsecureContext && !entry.insecureContext) ||
-          entry.earlyBetaOrEarlier === !isEarlyBetaOrEarlier ||
-          entry.crossOringinIsolated === !isCrossOringinIsolated ||
-          entry.disabled
-        ) {
-          interfaceMap[entry.name] = false;
-        } else {
-          interfaceMap[entry.name] = true;
-        }
+        interfaceMap[entry.name] = !entryDisabled(entry, data);
       }
     }
   }
 
-  addInterfaces(ecmaGlobals);
-  addInterfaces(interfaceNamesInGlobalScope);
+  for (let interfaceGroup of interfaceGroups) {
+    addInterfaces(interfaceGroup);
+  }
 
   return interfaceMap;
 }
 
-function runTest(data) {
-  var interfaceMap = createInterfaceMap(data);
-  for (var name of Object.getOwnPropertyNames(self)) {
+function runTest(parentName, parent, data, ...interfaceGroups) {
+  var interfaceMap = createInterfaceMap(data, ...interfaceGroups);
+  for (var name of Object.getOwnPropertyNames(parent)) {
     
     if (!/^[A-Z]/.test(name)) {
       continue;
@@ -354,18 +376,21 @@ function runTest(data) {
       interfaceMap[name],
       "If this is failing: DANGER, are you sure you want to expose the new interface " +
         name +
-        " to all webpages as a property on the worker? Do not make a change to this file without a " +
+        " to all webpages as a property of " +
+        parentName +
+        "? Do not make a change to this file without a " +
         " review from a DOM peer for that specific change!!! (or a JS peer for changes to ecmaGlobals)"
     );
     delete interfaceMap[name];
   }
   for (var name of Object.keys(interfaceMap)) {
     ok(
-      name in self === interfaceMap[name],
+      name in parent === interfaceMap[name],
       name +
         " should " +
         (interfaceMap[name] ? "" : " NOT") +
-        " be defined on the global scope"
+        " be defined on " +
+        parentName
     );
     if (!interfaceMap[name]) {
       delete interfaceMap[name];
@@ -380,6 +405,9 @@ function runTest(data) {
 }
 
 workerTestGetHelperData(function(data) {
-  runTest(data);
+  runTest("self", self, data, ecmaGlobals, interfaceNamesInGlobalScope);
+  if (WebAssembly && !entryDisabled(wasmGlobalEntry, data)) {
+    runTest("WebAssembly", WebAssembly, data, wasmGlobalInterfaces);
+  }
   workerTestDone();
 });
