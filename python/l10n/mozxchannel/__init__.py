75bc3d8f95a33694f1ebd002771d25b3e6d9c798
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 11:00:24 +0000

    Bug 1721442 - Add support for ESR91 to cross-channel, r=aki DONTBUILD
    Differential Revision: https://phabricator.services.mozilla.com/D120370
    
    UltraBlame original commit: ecae5a5b747ca0852bf7f51a6c9c93962551ef65

--- python/l10n/mozxchannel/__init__.py
+++ python/l10n/mozxchannel/__init__.py
@@ -32,9 +32,14 @@ def get_default_config(topsrcdir, strings_path):
                 "path": topsrcdir,
                 "url": "https://hg.mozilla.org/mozilla-unified/",
                 "heads": {
+                    
+                    
+                    
+                    
                     "central": "mozilla-central",
                     "beta": "releases/mozilla-beta",
                     "release": "releases/mozilla-release",
+                    "esr91": "releases/mozilla-esr91",
                     "esr78": "releases/mozilla-esr78",
                 },
                 "config_files": [
@@ -46,8 +51,13 @@ def get_default_config(topsrcdir, strings_path):
                 "path": topsrcdir / "comm",
                 "url": "https://hg.mozilla.org/comm-central/",
                 "heads": {
+                    
+                    
+                    
+                    
                     "comm": "comm-central",
                     "comm-beta": "releases/comm-beta",
+                    "comm-esr91": "releases/comm-esr91",
                     "comm-esr78": "releases/comm-esr78",
                 },
                 "config_files": [
