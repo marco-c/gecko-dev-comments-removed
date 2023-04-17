a26778716bfe0c3a89d64b8b7c2742b578218bbf
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 18:46:12 +0000

    Bug 1730088 part 4: Move CaretOffset to HyperTextAccessibleBase. r=eeejay
    The implementation for RemoteAccessibleBase is just a stub; we don't cache the caret yet.
    However, this needs to be in HyperTextAccessibleBase because it is used by ConvertMagicOffset, which will be moved in an upcoming patch.
    
    Differential Revision: https://phabricator.services.mozilla.com/D127209
    
    UltraBlame original commit: 2606771b0e56f8cd6ebfd69b4bc75ec0d4e507b6

--- accessible/generic/HyperTextAccessible.h
+++ accessible/generic/HyperTextAccessible.h
@@ -260,7 +260,7 @@ class HyperTextAccessible : public AccessibleWrap,
   
 
 
-  int32_t CaretOffset() const;
+  virtual int32_t CaretOffset() const override;
   void SetCaretOffset(int32_t aOffset);
 
   
