6166564f89fdf77b7804d3df005bec8a66d44b80
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 03:17:15 +0000

    Bug 1699222 - browserSettings.ftpProtocolEnabled should return false r=mixedpuppy
    Differential Revision: https://phabricator.services.mozilla.com/D111252
    
    UltraBlame original commit: 8a2a04743c5f451068d8ad5f105a99dd2baee2ff

--- toolkit/components/extensions/test/xpcshell/test_ext_browserSettings.js
+++ toolkit/components/extensions/test/xpcshell/test_ext_browserSettings.js
@@ -214,11 +214,14 @@ add_task(async function test_browser_settings() {
     });
   }
 
-  
-  
   extension.sendMessage("get", "ftpProtocolEnabled");
   let data = await extension.awaitMessage("settingData");
-  equal(data.value, Services.prefs.getBoolPref("network.ftp.enabled"));
+  equal(data.value, false);
+  equal(
+    data.levelOfControl,
+    "not_controllable",
+    `ftpProtocolEnabled is not controllable.`
+  );
 
   await testSetting("newTabPosition", "afterCurrent", {
     "browser.tabs.insertRelatedAfterCurrent": false,
