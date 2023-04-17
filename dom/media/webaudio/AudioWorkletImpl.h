770202b34fa3a07a077056659ba5b07f13faf08c
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 14:17:03 +0000

    Bug 1724997 - Add profiler markers on AudioWorklet.addModule. r=karlt
    Differential Revision: https://phabricator.services.mozilla.com/D122735
    
    UltraBlame original commit: 246312ec9d0afa541d4170f858e3fa741b754a93

--- dom/media/webaudio/AudioWorkletImpl.h
+++ dom/media/webaudio/AudioWorkletImpl.h
@@ -43,6 +43,9 @@ class AudioWorkletImpl final : public WorkletImpl {
   
   AudioNodeTrack* DestinationTrack() const { return mDestinationTrack; }
 
+  void OnAddModuleStarted() const override;
+  void OnAddModulePromiseSettled() const override;
+
  protected:
   
   already_AddRefed<dom::WorkletGlobalScope> ConstructGlobalScope() override;
