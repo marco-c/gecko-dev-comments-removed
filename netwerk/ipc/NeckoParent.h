d8eae871e8ccec491fcb505f857fd1085d711278
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 05:48:04 +0000

    Bug 1491084 - Remove unused code that shows http auth prompt for nested content process, r=necko-reviewers,dragana
    Differential Revision: https://phabricator.services.mozilla.com/D115374
    
    UltraBlame original commit: 8bb3610069f5e082cc7c585254a103e8ac83bd5b

--- netwerk/ipc/NeckoParent.h
+++ netwerk/ipc/NeckoParent.h
@@ -56,36 +56,6 @@ class NeckoParent : public PNeckoParent {
     return PNeckoParent::RecvPCookieServiceConstructor(aActor);
   }
 
-  
-
-
-
-
-
-
-  class NestedFrameAuthPrompt final : public nsIAuthPrompt2 {
-    ~NestedFrameAuthPrompt() {}
-
-   public:
-    NS_DECL_ISUPPORTS
-
-    NestedFrameAuthPrompt(PNeckoParent* aParent, TabId aNestedFrameId);
-
-    NS_IMETHOD PromptAuth(nsIChannel*, uint32_t, nsIAuthInformation*,
-                          bool*) override {
-      return NS_ERROR_NOT_IMPLEMENTED;
-    }
-
-    NS_IMETHOD AsyncPromptAuth(nsIChannel* aChannel,
-                               nsIAuthPromptCallback* callback, nsISupports*,
-                               uint32_t, nsIAuthInformation* aInfo,
-                               nsICancelable**) override;
-
-   protected:
-    PNeckoParent* mNeckoParent;
-    TabId mNestedFrameId;
-  };
-
  protected:
   bool mSocketProcessBridgeInited;
 
@@ -193,13 +163,6 @@ class NeckoParent : public PNeckoParent {
 
   PTransportProviderParent* AllocPTransportProviderParent();
   bool DeallocPTransportProviderParent(PTransportProviderParent* aActor);
-
-  mozilla::ipc::IPCResult RecvOnAuthAvailable(const uint64_t& aCallbackId,
-                                              const nsString& aUser,
-                                              const nsString& aPassword,
-                                              const nsString& aDomain);
-  mozilla::ipc::IPCResult RecvOnAuthCancelled(const uint64_t& aCallbackId,
-                                              const bool& aUserCancel);
 
   
   mozilla::ipc::IPCResult RecvPredPredict(
