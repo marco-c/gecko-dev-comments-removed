3378a3e83847c986b278876ba9a8b99bedf3a2d4
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 17:05:29 +0000

    Bug 1697526 - Add telemetry related to the GC budget r=sfink
    This adds two measurements:
    
    1. GC_BUDGET_WAS_INCREASED
    
       Whether the budget was increased due to taking to long or approaching the
       incremental limit.
    
    2. GC_SLICE_WAS_LONG
    
       Whether a GC slice took 1.5 times or 5ms more than its budget.
    
    Differential Revision: https://phabricator.services.mozilla.com/D126347
    
    UltraBlame original commit: 00ec1254f44c18157cd4a14cc0e1e265df96db8a

--- js/src/gc/Statistics.cpp
+++ js/src/gc/Statistics.cpp
@@ -1130,7 +1130,8 @@ Statistics::SliceData::SliceData(const SliceBudget& budget,
       startFaults(startFaults) {}
 
 void Statistics::beginSlice(const ZoneGCStats& zoneStats, JS::GCOptions options,
-                            const SliceBudget& budget, JS::GCReason reason) {
+                            const SliceBudget& budget, JS::GCReason reason,
+                            bool budgetWasIncreased) {
   MOZ_ASSERT(phaseStack.empty() ||
              (phaseStack.length() == 1 && phaseStack[0] == Phase::MUTATOR));
 
@@ -1161,6 +1162,8 @@ void Statistics::beginSlice(const ZoneGCStats& zoneStats, JS::GCOptions options,
   }
 
   runtime->addTelemetry(JS_TELEMETRY_GC_REASON, uint32_t(reason));
+  runtime->addTelemetry(JS_TELEMETRY_GC_BUDGET_WAS_INCREASED,
+                        budgetWasIncreased);
 
   
   bool wasFullGC = zoneStats.isFullCollection();
@@ -1270,7 +1273,11 @@ void Statistics::sendSliceTelemetry(const SliceData& slice) {
 
     
     double longSliceThreshold = std::min(1.5 * budget_ms, budget_ms + 5.0);
-    if (sliceTime.ToMilliseconds() > longSliceThreshold) {
+    bool wasLongSlice = sliceTime.ToMilliseconds() > longSliceThreshold;
+    runtime->addTelemetry(JS_TELEMETRY_GC_SLICE_WAS_LONG, wasLongSlice);
+
+    
+    if (wasLongSlice) {
       PhaseKind longest = LongestPhaseSelfTimeInMajorGC(slice.phaseTimes);
       reportLongestPhaseInMajorGC(longest, JS_TELEMETRY_GC_SLOW_PHASE);
 
