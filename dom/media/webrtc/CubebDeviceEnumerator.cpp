dc0c07f859473b1a19dd6d77c76f2c2af2fc1d66
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 19:08:12 +0000

    Bug 1734748 - Mimic the non-Android path wrt manual invalidation. r=padenot
    Differential Revision: https://phabricator.services.mozilla.com/D127905
    
    UltraBlame original commit: a9df1f34cd72d8e31d3d1bda31fe1f75da148b0a

--- dom/media/webrtc/CubebDeviceEnumerator.cpp
+++ dom/media/webrtc/CubebDeviceEnumerator.cpp
@@ -256,7 +256,7 @@ void CubebDeviceEnumerator::EnumerateAudioDevices(
     name = u"Default audio output device"_ns;
   }
 
-  if (devices.IsEmpty()) {
+  if (devices.IsEmpty() || manualInvalidation) {
     
     
     
