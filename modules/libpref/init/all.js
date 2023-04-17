f74802c5609e8623afa7c51f6a689941b1a9853c
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 10:21:25 +0000

    Bug 1720402 - Enable XFA support in the PDF viewer. r=pdfjs-reviewers,marco
    Differential Revision: https://phabricator.services.mozilla.com/D119828
    
    UltraBlame original commit: c60b57574b37d66d3f777c1ae9f1a2d278c35d2e

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -337,6 +337,11 @@ pref("pdfjs.renderInteractiveForms", true);
 pref("pdfjs.enableScripting", true);
 
 
+#ifdef EARLY_BETA_OR_EARLIER
+pref("pdfjs.enableXfa", true);
+#endif
+
+
 pref("mathml.disabled",    false);
 
 
