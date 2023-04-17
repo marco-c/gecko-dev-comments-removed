820a3f293b1c5bbf234ee5e72cd345f4f9d2a14c
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 00:15:22 +0000

    Bug 1696111 - Extract the code for parsing range header string out and use it later; r=alwu,necko-reviewers,dragana
    Differential Revision: https://phabricator.services.mozilla.com/D106889
    
    UltraBlame original commit: c117ba41ccb1da9850c73073b873177b0a445f9a

--- netwerk/protocol/http/OpaqueResponseUtils.h
+++ netwerk/protocol/http/OpaqueResponseUtils.h
@@ -17,6 +17,11 @@ bool IsOpaqueBlockListedMIMEType(const nsACString& aContentType);
 
 bool IsOpaqueBlockListedNeverSniffedMIMEType(const nsACString& aContentType);
 
+
+
+Result<std::tuple<int64_t, int64_t, int64_t>, nsresult>
+ParseContentRangeHeaderString(const nsAutoCString& aRangeStr);
+
 }  
 }  
 
