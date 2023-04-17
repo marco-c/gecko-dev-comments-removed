59d0ed725627f066bf00eae6901618adc8071beb
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 01:33:44 +0000

    Bug 1702330 - Graduate infobars to main proton pref r=jaws,marionette-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D110638
    
    UltraBlame original commit: eb2a8569ed0265778e5aeca9dd848e40f173a2f3

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -396,7 +396,7 @@ XPCOMUtils.defineLazyGetter(this, "gHighPriorityNotificationBox", () => {
   return new MozElements.NotificationBox(element => {
     element.classList.add("global-notificationbox");
     element.setAttribute("notificationside", "top");
-    if (gProtonInfobarsEnabled) {
+    if (gProton) {
       
       let tabNotifications = document.getElementById("tab-notification-deck");
       gNavToolbox.insertBefore(element, tabNotifications);
@@ -408,7 +408,7 @@ XPCOMUtils.defineLazyGetter(this, "gHighPriorityNotificationBox", () => {
 
 
 XPCOMUtils.defineLazyGetter(this, "gNotificationBox", () => {
-  return gProtonInfobarsEnabled
+  return gProton
     ? gHighPriorityNotificationBox
     : new MozElements.NotificationBox(element => {
         element.classList.add("global-notificationbox");
@@ -585,14 +585,6 @@ XPCOMUtils.defineLazyPreferenceGetter(
       doc.documentElement.toggleAttribute("proton", newValue);
     }
   }
-);
-
-
-XPCOMUtils.defineLazyPreferenceGetter(
-  this,
-  "gProtonInfobarsEnabled",
-  "browser.proton.infobars.enabled",
-  false
 );
 
 
