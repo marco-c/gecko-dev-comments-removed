27a188d05bda73ab34c7239df247a811c7caf944
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 16:33:53 +0000

    Bug 1591366 - Always reserve 64 bits for frame property value r=dholbert
    Depends on D115711
    
    Differential Revision: https://phabricator.services.mozilla.com/D124955
    
    UltraBlame original commit: 19499821190df2a1577ba6250b05b806cdf8cbea

--- layout/base/FrameProperties.h
+++ layout/base/FrameProperties.h
@@ -64,7 +64,7 @@ struct FramePropertyDescriptorUntyped {
 template <typename T>
 struct FramePropertyDescriptor : public FramePropertyDescriptorUntyped {
   typedef void Destructor(T* aPropertyValue);
-  typedef void DestructorWithFrame(const nsIFrame* aaFrame, T* aPropertyValue);
+  typedef void DestructorWithFrame(const nsIFrame* aFrame, T* aPropertyValue);
 
   template <Destructor Dtor>
   static constexpr const FramePropertyDescriptor<T> NewWithDestructor() {
@@ -122,12 +122,6 @@ struct FramePropertyTypeHelper<SmallValueHolder<T>> {
 
 
 
-
-
-
-
-
-
 class FrameProperties {
  public:
   template <typename T>
@@ -156,8 +150,8 @@ class FrameProperties {
   template <typename T>
   void Set(Descriptor<T> aProperty, PropertyType<T> aValue,
            const nsIFrame* aFrame) {
-    void* ptr = ReinterpretHelper<T>::ToPointer(aValue);
-    SetInternal(aProperty, ptr, aFrame);
+    uint64_t v = ReinterpretHelper<T>::ToInternalValue(aValue);
+    SetInternal(aProperty, v, aFrame);
   }
 
   
@@ -166,8 +160,8 @@ class FrameProperties {
   template <typename T>
   void Add(Descriptor<T> aProperty, PropertyType<T> aValue) {
     MOZ_ASSERT(!Has(aProperty), "duplicate frame property");
-    void* ptr = ReinterpretHelper<T>::ToPointer(aValue);
-    AddInternal(aProperty, ptr);
+    uint64_t v = ReinterpretHelper<T>::ToInternalValue(aValue);
+    AddInternal(aProperty, v);
   }
 
   
@@ -204,8 +198,8 @@ class FrameProperties {
   template <typename T>
   PropertyType<T> Get(Descriptor<T> aProperty,
                       bool* aFoundResult = nullptr) const {
-    void* ptr = GetInternal(aProperty, aFoundResult);
-    return ReinterpretHelper<T>::FromPointer(ptr);
+    uint64_t v = GetInternal(aProperty, aFoundResult);
+    return ReinterpretHelper<T>::FromInternalValue(v);
   }
 
   
@@ -221,8 +215,8 @@ class FrameProperties {
 
   template <typename T>
   PropertyType<T> Take(Descriptor<T> aProperty, bool* aFoundResult = nullptr) {
-    void* ptr = TakeInternal(aProperty, aFoundResult);
-    return ReinterpretHelper<T>::FromPointer(ptr);
+    uint64_t v = TakeInternal(aProperty, aFoundResult);
+    return ReinterpretHelper<T>::FromInternalValue(v);
   }
 
   
@@ -279,62 +273,63 @@ class FrameProperties {
   FrameProperties(const FrameProperties&) = delete;
   FrameProperties& operator=(const FrameProperties&) = delete;
 
-  inline void SetInternal(UntypedDescriptor aProperty, void* aValue,
+  inline void SetInternal(UntypedDescriptor aProperty, uint64_t aValue,
                           const nsIFrame* aFrame);
 
-  inline void AddInternal(UntypedDescriptor aProperty, void* aValue);
+  inline void AddInternal(UntypedDescriptor aProperty, uint64_t aValue);
 
-  inline void* GetInternal(UntypedDescriptor aProperty,
-                           bool* aFoundResult) const;
+  inline uint64_t GetInternal(UntypedDescriptor aProperty,
+                              bool* aFoundResult) const;
 
-  inline void* TakeInternal(UntypedDescriptor aProperty, bool* aFoundResult);
+  inline uint64_t TakeInternal(UntypedDescriptor aProperty, bool* aFoundResult);
 
   inline void RemoveInternal(UntypedDescriptor aProperty,
                              const nsIFrame* aFrame);
 
   template <typename T>
   struct ReinterpretHelper {
-    static_assert(sizeof(PropertyType<T>) <= sizeof(void*),
-                  "size of the value must never be larger than a pointer");
+    static_assert(sizeof(PropertyType<T>) <= sizeof(uint64_t),
+                  "size of the value must never be larger than 64 bits");
 
-    static void* ToPointer(PropertyType<T> aValue) {
-      void* ptr = nullptr;
-      memcpy(&ptr, &aValue, sizeof(aValue));
-      return ptr;
+    static uint64_t ToInternalValue(PropertyType<T> aValue) {
+      uint64_t v = 0;
+      memcpy(&v, &aValue, sizeof(aValue));
+      return v;
     }
 
-    static PropertyType<T> FromPointer(void* aPtr) {
+    static PropertyType<T> FromInternalValue(uint64_t aInternalValue) {
       PropertyType<T> value;
-      memcpy(&value, &aPtr, sizeof(value));
+      memcpy(&value, &aInternalValue, sizeof(value));
       return value;
     }
-  };
-
-  template <typename T>
-  struct ReinterpretHelper<T*> {
-    static void* ToPointer(T* aValue) { return static_cast<void*>(aValue); }
-
-    static T* FromPointer(void* aPtr) { return static_cast<T*>(aPtr); }
   };
 
   
 
 
   struct PropertyValue {
-    PropertyValue() : mProperty(nullptr), mValue(nullptr) {}
-    PropertyValue(UntypedDescriptor aProperty, void* aValue)
+    PropertyValue() : mProperty(nullptr), mValue(0) {}
+    PropertyValue(UntypedDescriptor aProperty, uint64_t aValue)
         : mProperty(aProperty), mValue(aValue) {}
 
+    
+    
+    
+    
+    
+    
     void DestroyValueFor(const nsIFrame* aFrame) {
       if (mProperty->mDestructor) {
-        mProperty->mDestructor(mValue);
+        mProperty->mDestructor(
+            ReinterpretHelper<void*>::FromInternalValue(mValue));
       } else if (mProperty->mDestructorWithFrame) {
-        mProperty->mDestructorWithFrame(aFrame, mValue);
+        mProperty->mDestructorWithFrame(
+            aFrame, ReinterpretHelper<void*>::FromInternalValue(mValue));
       }
     }
 
     UntypedDescriptor mProperty;
-    void* mValue;
+    uint64_t mValue;
   };
 
   
@@ -357,28 +352,29 @@ class FrameProperties {
   nsTArray<PropertyValue> mProperties;
 };
 
-inline void* FrameProperties::GetInternal(UntypedDescriptor aProperty,
-                                          bool* aFoundResult) const {
+inline uint64_t FrameProperties::GetInternal(UntypedDescriptor aProperty,
+                                             bool* aFoundResult) const {
   MOZ_ASSERT(aProperty, "Null property?");
 
   return mProperties.ApplyIf(
       aProperty, 0, PropertyComparator(),
-      [&aFoundResult](const PropertyValue& aPV) -> void* {
+      [&aFoundResult](const PropertyValue& aPV) -> uint64_t {
         if (aFoundResult) {
           *aFoundResult = true;
         }
         return aPV.mValue;
       },
-      [&aFoundResult]() -> void* {
+      [&aFoundResult]() -> uint64_t {
         if (aFoundResult) {
           *aFoundResult = false;
         }
-        return nullptr;
+        return 0;
       });
 }
 
 inline void FrameProperties::SetInternal(UntypedDescriptor aProperty,
-                                         void* aValue, const nsIFrame* aFrame) {
+                                         uint64_t aValue,
+                                         const nsIFrame* aFrame) {
   MOZ_ASSERT(NS_IsMainThread());
   MOZ_ASSERT(aProperty, "Null property?");
 
@@ -392,15 +388,15 @@ inline void FrameProperties::SetInternal(UntypedDescriptor aProperty,
 }
 
 inline void FrameProperties::AddInternal(UntypedDescriptor aProperty,
-                                         void* aValue) {
+                                         uint64_t aValue) {
   MOZ_ASSERT(NS_IsMainThread());
   MOZ_ASSERT(aProperty, "Null property?");
 
   mProperties.AppendElement(PropertyValue(aProperty, aValue));
 }
 
-inline void* FrameProperties::TakeInternal(UntypedDescriptor aProperty,
-                                           bool* aFoundResult) {
+inline uint64_t FrameProperties::TakeInternal(UntypedDescriptor aProperty,
+                                              bool* aFoundResult) {
   MOZ_ASSERT(NS_IsMainThread());
   MOZ_ASSERT(aProperty, "Null property?");
 
@@ -409,14 +405,14 @@ inline void* FrameProperties::TakeInternal(UntypedDescriptor aProperty,
     if (aFoundResult) {
       *aFoundResult = false;
     }
-    return nullptr;
+    return 0;
   }
 
   if (aFoundResult) {
     *aFoundResult = true;
   }
 
-  void* result = mProperties.Elements()[index].mValue;
+  uint64_t result = mProperties.Elements()[index].mValue;
   mProperties.RemoveElementAt(index);
 
   return result;
