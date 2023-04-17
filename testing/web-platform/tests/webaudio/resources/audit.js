f6fbe571f71b1789bbe6f8eb4ec3317bb7e126ce
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 09:37:37 +0000

    Bug 1716830 [wpt PR 29400] - Throw error if connections are made between different contexts, a=testonly
    Automatic update from web-platform-tests
    Throw error if connections are made between different contexts
    
    The WebAudio spec says that if a node is connected/disconnected to
    another node or AudioParam that belongs to a different context, then
    an InvalidStateError must be thrown.[1][2]  Add a check for this and throw
    the required error.
    
    This also requires a fix to audit.js which wasn't properly catching
    DOM exceptions of the wrong type.
    
    Finally, ctor-channelsplitter.html needed to be updated to specify the
    error type because of the change in audit.js.
    
    Also adds new WPT test, different-contexts.html, to more thoroughly test
    connect/disconnects to different contexts.
    
    [1] https://webaudio.github.io/web-audio-api/#dom-audionode-connect-destinationnode-output-input-destinationnode
    [2] https://webaudio.github.io/web-audio-api/#dom-audionode-disconnect-destinationnode-destinationnode
    
    Bug: 1206927
    Change-Id: I320425268d1fd243e347dd36c79172c69dcc9733
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2965479
    Reviewed-by: Hongchan Choi <hongchanchromium.org>
    Commit-Queue: Raymond Toy <rtoychromium.org>
    Cr-Commit-Position: refs/heads/master{#895851}
    
    --
    
    wpt-commits: c632c827c408d77c09f63cba01f3ad526588c67f
    wpt-pr: 29400
    
    UltraBlame original commit: 8e84a684957de6466b5fe476b2d993e036fa886f

--- testing/web-platform/tests/webaudio/resources/audit.js
+++ testing/web-platform/tests/webaudio/resources/audit.js
@@ -320,11 +320,16 @@ window.Audit = (function() {
           didThrowCorrectly = true;
           passDetail = '${actual} threw ' + error.name + errorMessage + '.';
         } else if (this._expected === DOMException &&
-                   (this._expectedDescription === undefined ||
-                    this._expectedDescription === error.name)) {
+                   this._expectedDescription !== undefined) {
           
-          didThrowCorrectly = true;
-          passDetail = '${actual} threw ${expected}' + errorMessage + '.';
+          if (this._expectedDescription === error.name) {
+            didThrowCorrectly = true;
+            passDetail = '${actual} threw ${expected}' + errorMessage + '.';
+          } else {
+            didThrowCorrectly = false;
+            failDetail =
+                '${actual} threw "' + error.name + '" instead of ${expected}.';
+          }
         } else if (this._expected == error.constructor) {
           
           didThrowCorrectly = true;
