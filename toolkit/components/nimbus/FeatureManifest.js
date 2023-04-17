180b9255c6a8a4766542d862d291b12324dba2bf
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 17:24:38 +0000

    Backed out changeset fd0e94547636 (bug 1731794) for causing failures in test_Services.js.
    UltraBlame original commit: 191827e72cb1d18f93bc8eed8d29aa56d60f7681

--- toolkit/components/nimbus/FeatureManifest.js
+++ toolkit/components/nimbus/FeatureManifest.js
@@ -9,18 +9,6 @@ const EXPORTED_SYMBOLS = ["FeatureManifest"];
 
 
 const FeatureManifest = {
-  search: {
-    description: "The Search Services",
-    hasExposure: false,
-    variables: {
-      experiment: {
-        type: "string",
-        fallbackPref: "browser.search.experiment",
-        description:
-          "Used to activate only matching configurations that contain the value in `experiment`",
-      },
-    },
-  },
   urlbar: {
     description: "The Address Bar",
     hasExposure: true,
