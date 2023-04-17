40b1884f8285de014014a534975287cb4a1bc387
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 16:10:54 +0000

    Bug 1729925 - [devtools] Avoid leaking commands from NodePicker. r=nchevobbe
    Also avoid creating a NodePicker during toolbox's destroy.
    
    Differential Revision: https://phabricator.services.mozilla.com/D125539
    
    UltraBlame original commit: e9f40c89753f515bf64a63d14d53ffeb1e6ede4e

--- devtools/client/framework/toolbox.js
+++ devtools/client/framework/toolbox.js
@@ -3731,10 +3731,6 @@ Toolbox.prototype = {
       this._componentMount = null;
       this._tabBar = null;
     }
-    if (this._nodePicker) {
-      this._nodePicker.stop();
-      this._nodePicker = null;
-    }
     this.destroyHarAutomation();
 
     const outstanding = [];
@@ -3809,6 +3805,12 @@ Toolbox.prototype = {
         settleAll(outstanding)
           .catch(console.error)
           .then(async () => {
+            
+            
+            if (this._nodePicker) {
+              this._nodePicker.destroy();
+              this._nodePicker = null;
+            }
             this.selection.destroy();
             this.selection = null;
 
