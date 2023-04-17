52c53ae22d0ff40411b41709a1472ce20f7ae02e
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 12:25:12 +0000

    Bug 1635018 - Part 1: Limit dragexit event to chrome on Nightly r=edgar,masayuki
    Differential Revision: https://phabricator.services.mozilla.com/D121189
    
    UltraBlame original commit: 9cde7bcaba462e0656f7706253b85d6cb0a3d423

--- layout/base/PresShell.cpp
+++ layout/base/PresShell.cpp
@@ -8352,6 +8352,12 @@ bool PresShell::EventHandler::PrepareToDispatchEvent(
       }
       return true;
     }
+    case eDragExit: {
+      if (!StaticPrefs::dom_event_dragexit_enabled()) {
+        aEvent->mFlags.mOnlyChromeDispatch = true;
+      }
+      return true;
+    }
     case eContextMenu: {
       
       
