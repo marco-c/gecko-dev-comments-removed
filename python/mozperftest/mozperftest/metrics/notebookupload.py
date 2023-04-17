33b81a608c5c696f55e7d4d37798c974e8416b19
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 10:11:22 +0000

    Bug 1705420: Report the version of the application being tested in our perfherder data r=perftest-reviewers,sparky
    implementing get_browser_meta for mozperftest
    
    Differential Revision: https://phabricator.services.mozilla.com/D113861
    
    UltraBlame original commit: b533ae02d3679bbc590680fb8f9d53c266241dab

--- python/mozperftest/mozperftest/metrics/notebookupload.py
+++ python/mozperftest/mozperftest/metrics/notebookupload.py
@@ -71,12 +71,22 @@ class Notebook(Layer):
                     raise Exception(f"{dirpath} does not exist.")
                 if not dirpath.is_dir():
                     raise Exception(f"{dirpath} is not a directory")
+
+                
+                if self.get_arg("android"):
+                    binary_path = self.get_arg("android-app-name")
+                else:
+                    binary_path = (
+                        self.get_arg("binary") or self.mach_cmd.get_binary_path()
+                    )
+
                 
                 for jsonfile in dirpath.rglob("*.json"):
                     metadata.add_result(
                         {
                             "results": str(jsonfile.resolve()),
                             "name": jsonfile.parent.name,
+                            "binary": binary_path,
                         }
                     )
 
