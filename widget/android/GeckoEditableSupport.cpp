bfbd3ae6286439f8d6635eb01f360dd26eccde74
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 15:16:42 +0000

    Bug 1724811 - Use selection data from notification instead of get current selection. r=geckoview-reviewers,agi
    Although root cause isn't bug 1713334, this occurs after landing it.
    
    When inputting "." by Swiftkey, it generates key event then dispatches it to
    Gecko. After text is changed by this key event, we will update this change
    to GeckoView.
    
    But when this occurs, we post invalid selection change to GeckoView, then
    `GeckoEditableChild` throws `IllegalArgumentException`.
    
    Normal
    ======
    1. Dispatch "." text change.
    2. Receive text and selection change notification by 1.
    3. Posted text/selection change notification to GV's parent
    
    This situation
    ==============
    1. Dispatch "." text change.
    2. Receive text and selection change notification by 1.
    3. Dispatch next "." text change.
    4. Posted 2.'s text/selection change notification to GV's parent.
    5. `GeckoEditableChild` throws `IllegalArgumentException`.
    
    So when step 4.., we should use 2.'s change data. But since we query current
    Gecko's selection data, we may use invalid selection data.
    
    So we should cache selection change, then use it.
    
    Differential Revision: https://phabricator.services.mozilla.com/D124435
    
    UltraBlame original commit: 61088a662607165814372b68cd2464349eb53ed1

--- widget/android/GeckoEditableSupport.cpp
+++ widget/android/GeckoEditableSupport.cpp
@@ -687,18 +687,30 @@ void GeckoEditableSupport::FlushIMEChanges(FlushChangesFlag aFlags) {
   int32_t selEnd = -1;
 
   if (mIMESelectionChanged) {
-    WidgetQueryContentEvent querySelectedTextEvent(true, eQuerySelectedText,
-                                                   widget);
-    widget->DispatchEvent(&querySelectedTextEvent, status);
+    if (mCachedSelection.IsValid()) {
+      selStart = mCachedSelection.mStartOffset;
+      selEnd = mCachedSelection.mEndOffset;
+    } else {
+      
+      
+      
+      
+      
+      
+      WidgetQueryContentEvent querySelectedTextEvent(true, eQuerySelectedText,
+                                                     widget);
+      widget->DispatchEvent(&querySelectedTextEvent, status);
 
-    if (shouldAbort(NS_WARN_IF(querySelectedTextEvent.DidNotFindSelection()))) {
-      return;
+      if (shouldAbort(
+              NS_WARN_IF(querySelectedTextEvent.DidNotFindSelection()))) {
+        return;
+      }
+
+      selStart = static_cast<int32_t>(
+          querySelectedTextEvent.mReply->SelectionStartOffset());
+      selEnd = static_cast<int32_t>(
+          querySelectedTextEvent.mReply->SelectionEndOffset());
     }
-
-    selStart = static_cast<int32_t>(
-        querySelectedTextEvent.mReply->SelectionStartOffset());
-    selEnd = static_cast<int32_t>(
-        querySelectedTextEvent.mReply->SelectionEndOffset());
 
     if (aFlags == FLUSH_FLAG_RECOVER && textTransaction.IsValid()) {
       
@@ -1293,6 +1305,8 @@ nsresult GeckoEditableSupport::NotifyIME(
         mDispatcher = dispatcher;
         mIMEKeyEvents.Clear();
 
+        mCachedSelection.Reset();
+
         mIMEDelaySynchronizeReply = false;
         mIMEActiveCompositionCount = 0;
         FlushIMEText();
@@ -1332,6 +1346,12 @@ nsresult GeckoEditableSupport::NotifyIME(
     case NOTIFY_IME_OF_SELECTION_CHANGE: {
       ALOGIME("IME: NOTIFY_IME_OF_SELECTION_CHANGE: SelectionChangeData=%s",
               ToString(aNotification.mSelectionChangeData).c_str());
+
+      mCachedSelection.mStartOffset =
+          aNotification.mSelectionChangeData.mOffset;
+      mCachedSelection.mEndOffset =
+          aNotification.mSelectionChangeData.mString->Length() +
+          aNotification.mSelectionChangeData.mOffset;
 
       PostFlushIMEChanges();
       mIMESelectionChanged = true;
