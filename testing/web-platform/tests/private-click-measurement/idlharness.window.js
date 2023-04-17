bc29351e12e8ce65dd6cf4b87166f053aa295395
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 06:51:58 +0000

    Bug 1646599 [wpt PR 24220] - Add interfaces/private-click-measurement.idl and test, a=testonly
    Automatic update from web-platform-tests
    Add test for interfaces/private-click-measurement.idl (#24220)
    
    --
    
    wpt-commits: 93820fcc37d110f0d9d959776202124c01b4c426
    wpt-pr: 24220
    
    UltraBlame original commit: 2553347c17b22bbeb27c1621fea7f248c30e77a4

---
+++ testing/web-platform/tests/private-click-measurement/idlharness.window.js
@@ -0,0 +1,14 @@
+
+
+
+'use strict';
+
+idl_test(
+  ['private-click-measurement'],
+  ['html', 'dom'],
+  idl_array => {
+    idl_array.add_objects({
+      HTMLAnchorElement: ['document.createElement("a")'],
+    });
+  }
+);
