6cb4c8d3698c13fee10cd009e3100798aae89433
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 13:03:46 +0000

    Bug 1710123 - [devtools] Fix browser_accessibility_reload.js intermittent. r=bomsy.
    The test wasn't waiting for the panel to be properly reloaded.
    
    Differential Revision: https://phabricator.services.mozilla.com/D122363
    
    UltraBlame original commit: 930d51b021dfc25b2d35b71d56f148d3c7e36a34

--- devtools/client/accessibility/test/browser/browser_accessibility_reload.js
+++ devtools/client/accessibility/test/browser/browser_accessibility_reload.js
@@ -59,8 +59,11 @@ const tests = [
   },
   {
     desc: "Reload the page.",
-    setup: async ({ panel }) =>
-      panel.accessibilityProxy.commands.targetCommand.reloadTopLevelTarget(),
+    setup: async ({ panel }) => {
+      const onReloaded = panel.once("reloaded");
+      panel.accessibilityProxy.commands.targetCommand.reloadTopLevelTarget();
+      await onReloaded;
+    },
     expected: {
       tree: [
         {
@@ -76,7 +79,10 @@ const tests = [
   },
   {
     desc: "Navigate to a new page.",
-    setup: () => navigateTo(buildURL(TEST_URI_2)),
+    setup: async () => {
+      
+      await navigateTo(buildURL(TEST_URI_2));
+    },
     expected: {
       tree: [
         {
