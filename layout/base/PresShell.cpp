538a5150a0b7f56011fdc6f15fab00e1eae38f76
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 05:25:55 +0000

    Bug 1710643 - Fix encoding of the theme change kind so it doesn't crash when getting converted to JS if non-zero.
    MANUAL PUSH: Fixes orange in some tests that do restricted theme
    changes CLOSED TREE.
    
    UltraBlame original commit: e2e39e32a588dc6ac32d4d0d40640a820f4d3219

--- layout/base/PresShell.cpp
+++ layout/base/PresShell.cpp
@@ -9887,7 +9887,7 @@ PresShell::Observe(nsISupports* aSubject, const char* aTopic,
 
   if (!nsCRT::strcmp(aTopic, "look-and-feel-changed")) {
     
-    auto kind = widget::ThemeChangeKind(reinterpret_cast<uintptr_t>(aData));
+    auto kind = widget::ThemeChangeKind(aData[0]);
     ThemeChanged(kind);
     return NS_OK;
   }
