246cf24641f9f91f42ec9f8476b74c83aefedc0d
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 04:26:53 +0000

    Bug 1708472 - Part 2: Add recover instructions for MNewArrayObject and support scalar replacement r=nbp
    I didn't add any new tests for this yet - maybe there are some you could suggest?
    
    Differential Revision: https://phabricator.services.mozilla.com/D113924
    
    UltraBlame original commit: 01d4933c6e7f43253ffe1c30e3a0a5c8dfd8c20c

--- js/src/jit/MIR.cpp
+++ js/src/jit/MIR.cpp
@@ -4099,7 +4099,11 @@ MArrayState::MArrayState(MDefinition* arr) : MVariadicInstruction(classOpcode) {
   
   setResultType(MIRType::Object);
   setRecoveredOnBailout();
-  numElements_ = arr->toNewArray()->length();
+  if (arr->isNewArrayObject()) {
+    numElements_ = arr->toNewArrayObject()->length();
+  } else {
+    numElements_ = arr->toNewArray()->length();
+  }
 }
 
 bool MArrayState::init(TempAllocator& alloc, MDefinition* obj,
