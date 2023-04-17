e73cece5fa903acd66361df99da21f3e909ff885
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 18:15:45 +0000

    Bug 1733052 - Part 2: Revise popup abuse level if there is a valid transient user activation; r=smaug
    This is a intermediate solution to make popup block could work with new user
    activation model (timer base). I think this should cover most of async case for
    `window.open()`. For long term, we still need to clean up the popup state which
    is tracked by bug 1656444.
    
    Differential Revision: https://phabricator.services.mozilla.com/D126879
    
    UltraBlame original commit: d5ec74c1e5eaa942ff618d6ab02fe0e33a07ee67

--- docshell/base/BrowsingContext.cpp
+++ docshell/base/BrowsingContext.cpp
@@ -2161,6 +2161,7 @@ PopupBlocker::PopupControlState BrowsingContext::RevisePopupAbuseLevel(
     return PopupBlocker::openAllowed;
   }
 
+  RefPtr<Document> doc = GetExtantDocument();
   PopupBlocker::PopupControlState abuse = aControl;
   switch (abuse) {
     case PopupBlocker::openControlled:
@@ -2171,7 +2172,8 @@ PopupBlocker::PopupControlState BrowsingContext::RevisePopupAbuseLevel(
       }
       break;
     case PopupBlocker::openAbused:
-      if (IsPopupAllowed()) {
+      if (IsPopupAllowed() ||
+          (doc && doc->HasValidTransientUserGestureActivation())) {
         
         abuse = PopupBlocker::openControlled;
       }
@@ -2194,7 +2196,7 @@ PopupBlocker::PopupControlState BrowsingContext::RevisePopupAbuseLevel(
 
   
   
-  if (RefPtr<Document> doc = GetExtantDocument()) {
+  if (doc) {
     
     
     
