d10936dc462896730e8c7bb77c183428dd53c2aa
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 04:58:08 +0000

    Bug 1708543 [wpt PR 28748] - Replace int2byte from six with string literals or bytes(...), a=testonly
    Automatic update from web-platform-tests
    Replace int2byte from six with string literals or bytes(...) (#28748)
    
    The following expressions were evaluated as True in a Python 3.8.2 shell
    to gain confidence:
    ```py
    from six import int2byte
    b"".join(int2byte(byte) for byte in range(255)) == bytes(range(255))
    int2byte(0xff) == b"\xff"
    int2byte(0x83) + int2byte(0x65) + int2byte(0x83) + int2byte(0x58) + int2byte(0x83) + int2byte(0x67) == bytes([0x83, 0x65, 0x83, 0x58, 0x83, 0x67])
    b'<' + int2byte(0xff) + b'/>' == b'<\xff/>'
    ```
    --
    
    wpt-commits: 9c4b5c0b66b0d0d4edbeb33a2b20e42082a0bfe7
    wpt-pr: 28748
    
    UltraBlame original commit: c06c785f040c40c820adc06238ba83316adbcfb0

--- testing/web-platform/tests/xhr/resources/shift-jis-html.py
+++ testing/web-platform/tests/xhr/resources/shift-jis-html.py
@@ -1,8 +1,6 @@
-from six import int2byte
-
 def main(request, response):
     headers = [(b"Content-type", b"text/html;charset=shift-jis")]
     
-    content = int2byte(0x83) + int2byte(0x65) + int2byte(0x83) + int2byte(0x58) + int2byte(0x83) + int2byte(0x67)
+    content = bytes([0x83, 0x65, 0x83, 0x58, 0x83, 0x67])
 
     return headers, content
