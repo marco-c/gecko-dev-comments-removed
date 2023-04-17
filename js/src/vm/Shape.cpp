5740dd837449f5a71d867c5c00c1fecf55b4df54
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 03:07:21 +0000

    Bug 1706246 part 7 - Use ShapeProperty in more places, remove Shape::isAccessorDescriptor. r=jonco
    Depends on D112711
    
    Differential Revision: https://phabricator.services.mozilla.com/D112712
    
    UltraBlame original commit: 99c96a3a9035602cf149ef2c2df344d0361437d6

--- js/src/vm/Shape.cpp
+++ js/src/vm/Shape.cpp
@@ -962,7 +962,8 @@ bool NativeObject::putProperty(JSContext* cx, HandleNativeObject obj,
   ObjectFlags objectFlags =
       GetObjectFlagsForNewProperty(obj->lastProperty(), id, attrs, cx);
 
-  if (shape->isAccessorDescriptor()) {
+  ShapeProperty prop = ShapeProperty(shape);
+  if (prop.isAccessorProperty()) {
     objectFlags.setFlag(ObjectFlag::HadGetterSetterChange);
   }
 
@@ -1147,7 +1148,8 @@ bool NativeObject::removeProperty(JSContext* cx, HandleNativeObject obj,
   
   
   
-  if (shape->isAccessorDescriptor() && !obj->hadGetterSetterChange()) {
+  ShapeProperty prop = ShapeProperty(shape);
+  if (prop.isAccessorProperty() && !obj->hadGetterSetterChange()) {
     if (!NativeObject::setHadGetterSetterChange(cx, obj)) {
       return false;
     }
