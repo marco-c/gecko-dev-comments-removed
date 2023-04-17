73b019be58cf4d4ecdacfff7bd4dbca44028a86f
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 11:17:09 +0000

    Bug 1719194 part 1 - Use a stencil-like gc-things vector for compiling. r=arai
    When delazifying with a `CompileLazyFunction` call, we reused
    BaseScript::gcthings to skip over information already provided by the
    `SyntaxParseHandler`, such as functions and closed-over bindings.
    
    This change adds a stencil-like gcthings span, which is created from the
    BaseScript::gcthings() with the intent of later using the same API for mapping
    input data from a Stencil without having to allocate GC objects for eager
    delazifying functions.
    
    `CompilationState::init` is modified to call the convertion from the
    BaseScript::gcthings() to the `CompilationInput::cachedGcThings_`. The logic of
    the `FullParseHandler` is updated to work with the gcthings provided by the
    `CompilationInput` in a similar way, except that atoms contained in the original
    vector are internalized under `CompilationState::init` instead of being
    internalized when visiting each scope under
    `propagateFreeNamesAndMarkClosedOverBindings`.
    
    Differential Revision: https://phabricator.services.mozilla.com/D120231
    
    UltraBlame original commit: 6fbc8ee2c1bd599e4e9fe877f5ca09c12949cc6e

--- js/src/frontend/SyntaxParseHandler.h
+++ js/src/frontend/SyntaxParseHandler.h
@@ -24,6 +24,8 @@ namespace js {
 
 namespace frontend {
 
+struct CompilationState;
+
 
 
 
@@ -172,8 +174,9 @@ class SyntaxParseHandler {
   }
 
  public:
-  SyntaxParseHandler(JSContext* cx, LifoAlloc& alloc,
-                     BaseScript* lazyOuterFunction) {}
+  SyntaxParseHandler(JSContext* cx, CompilationState& compilationState) {
+    MOZ_ASSERT(!compilationState.input.isDelazifying());
+  }
 
   static NullNode null() { return NodeFailure; }
 
@@ -750,7 +753,7 @@ class SyntaxParseHandler {
 
   bool canSkipLazyInnerFunctions() { return false; }
   bool canSkipLazyClosedOverBindings() { return false; }
-  JSAtom* nextLazyClosedOverBinding() {
+  TaggedParserAtomIndex nextLazyClosedOverBinding() {
     MOZ_CRASH(
         "SyntaxParseHandler::canSkipLazyClosedOverBindings must return false");
   }
