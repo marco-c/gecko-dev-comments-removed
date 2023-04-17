f423a5a5e208919115bb46f4b7e814306c9ace43
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 08:40:10 +0000

    Bug 1542807 part 4 - Exclude fonts loaded from User and UserAgent origin sheets from 'document.fonts'.  r=jfkthame
    Per https://github.com/w3c/csswg-drafts/issues/6126 these were
    never intended to be included in the first place so this is just
    fixing a bug.  Note that I'm leaving them in the mRuleFaces array
    so that the font loading machinery works the same as before.
    I'm just excluding them when queried by document.fonts.
    
    Differential Revision: https://phabricator.services.mozilla.com/D111694
    
    UltraBlame original commit: fc9009240a43b9c323a1dc59bc4b8e1f4c61fdb7

--- layout/style/FontFaceSetIterator.cpp
+++ layout/style/FontFaceSetIterator.cpp
@@ -42,7 +42,14 @@ void FontFaceSetIterator::Next(JSContext* aCx,
     return;
   }
 
-  FontFace* face = mFontFaceSet->GetFontFaceAt(mNextIndex++);
+  
+  
+  FontFace* face;
+  while (!(face = mFontFaceSet->GetFontFaceAt(mNextIndex++))) {
+    if (mNextIndex >= mFontFaceSet->SizeIncludingNonAuthorOrigins()) {
+      break;  
+    }
+  }
 
   if (!face) {
     aResult.mValue.setUndefined();
