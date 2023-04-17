5596b79956cb42eb958d89a38a31bef9f1752ea7
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 14:43:57 +0000

    Bug 1723176 - Protect access to AsyncPanZoomController::mZoomAnimationId with the APZC lock. r=tnikkel,decoder
    Differential Revision: https://phabricator.services.mozilla.com/D123710
    
    UltraBlame original commit: 8dedbaa4c5616fa0bd2c7683c02bd2e82911aa53

--- mozglue/build/TsanOptions.cpp
+++ mozglue/build/TsanOptions.cpp
@@ -310,12 +310,6 @@ extern "C" const char* __tsan_default_suppressions() {
          
          "race:mozilla::layers::APZCTreeManager::NewAPZCInstance\n"
 
-         
-         "race:mozilla::layers::AsyncPanZoomController::GetZoomAnimationId\n"
-
-         
-         "race:mozilla::layers::AsyncPanZoomController::SetZoomAnimationId\n"
-
       
       ;  
 }
