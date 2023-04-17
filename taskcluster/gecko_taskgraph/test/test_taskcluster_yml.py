5cff6aea849e7b26395b525dc573c72c98d3e725
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 18:55:19 +0000

    Bug 1733950 - Use upstream's 'taskgraph.util.yaml' module, r=taskgraph-reviewers,aki
    Gecko taskgraph is now an official consumer of standalone taskgraph.
    
    I chose 'util/yaml.py' since both copies were identical. I've also synced the
    test for them, so no coverage is being lost.
    
    Depends on D127872
    
    Differential Revision: https://phabricator.services.mozilla.com/D127873
    
    UltraBlame original commit: 3242e697112df7afe7b281bedc32f13d6b1ddc70

--- taskcluster/gecko_taskgraph/test/test_taskcluster_yml.py
+++ taskcluster/gecko_taskgraph/test/test_taskcluster_yml.py
@@ -2,16 +2,14 @@
 
 
 
-
-import jsone
 import pprint
 
+import jsone
 import slugid
 import unittest
-
 from mozunit import main
+from taskgraph.util.yaml import load_yaml
 
-from gecko_taskgraph.util.yaml import load_yaml
 from gecko_taskgraph.util.time import current_json_time
 from gecko_taskgraph import GECKO
 
