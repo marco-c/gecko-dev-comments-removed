2b93369ac2e1992fd2abfc773d5d90536774d3d1
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 02:47:36 +0000

    Bug 1705777 - Move simulated traversal part of algorithm into the main loop r=jandem
    Currently when resuing the leftmost leaf's buffer we have duplicate code to run
    the traversal up until we reach the leftmost rope, because the left child has
    been already been processed as a sepcial case. This removes this and checks a
    flag in the main traversal code to handle this situation.
    
    Differential Revision: https://phabricator.services.mozilla.com/D112391
    
    UltraBlame original commit: a0bca47b08bfafe1d83bb559e6544d9c342774a8

--- js/src/vm/StringType.cpp
+++ js/src/vm/StringType.cpp
@@ -725,13 +725,15 @@ JSLinearString* JSRope::flattenInternal(JSRope* root) {
   gc::StoreBuffer* bufferIfNursery = root->storeBuffer();
 
   
-  JSRope* leftMostRope = root;
-  while (leftMostRope->leftChild()->isRope()) {
-    leftMostRope = &leftMostRope->leftChild()->asRope();
+  JSRope* leftmostRope = root;
+  while (leftmostRope->leftChild()->isRope()) {
+    leftmostRope = &leftmostRope->leftChild()->asRope();
   }
 
-  if (leftMostRope->leftChild()->isExtensible()) {
-    JSExtensibleString& left = leftMostRope->leftChild()->asExtensible();
+  bool reuseLeftmostBuffer = false;
+
+  if (leftmostRope->leftChild()->isExtensible()) {
+    JSExtensibleString& left = leftmostRope->leftChild()->asExtensible();
     size_t capacity = left.capacity();
     if (capacity >= wholeLength &&
         left.hasTwoByteChars() == std::is_same_v<CharT, char16_t>) {
@@ -745,24 +747,8 @@ JSLinearString* JSRope::flattenInternal(JSRope* root) {
         return nullptr;
       }
 
-      
-
-
-
-      MOZ_ASSERT(str->isRope());
-      while (str != leftMostRope) {
-        ropeBarrierDuringFlattening<usingBarrier>(str);
-        JSString* child = str->d.s.u2.left;
-        
-        MOZ_ASSERT(child->isRope());
-        str->setNonInlineChars(wholeChars);
-        child->setFlattenData(str, Flag_VisitRightChild);
-        str = child;
-      }
-      ropeBarrierDuringFlattening<usingBarrier>(str);
-      str->setNonInlineChars(wholeChars);
+      leftmostRope->setNonInlineChars(wholeChars);
       uint32_t left_len = left.length();
-      pos = wholeChars + left_len;
 
       
       
@@ -780,7 +766,10 @@ JSLinearString* JSRope::flattenInternal(JSRope* root) {
         
         bufferIfNursery->putWholeCell(&left);
       }
-      goto visit_right_child;
+
+      reuseLeftmostBuffer = true;
+      pos = wholeChars + left_len;
+      goto first_visit_node;
     }
   }
 
@@ -799,7 +788,10 @@ JSLinearString* JSRope::flattenInternal(JSRope* root) {
   pos = wholeChars;
 first_visit_node : {
   ropeBarrierDuringFlattening<usingBarrier>(str);
-
+  if (reuseLeftmostBuffer && str == leftmostRope) {
+    
+    goto visit_right_child;
+  }
   JSString& left = *str->d.s.u2.left;
   str->setNonInlineChars(pos);
   if (left.isRope()) {
