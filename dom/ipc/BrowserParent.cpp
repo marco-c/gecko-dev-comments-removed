c07c41163755718ce68a7a4fd47c0451593f47cb
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 01:55:18 +0000

    Bug 1704545, part 3 - Tell the priority manager when a browser parent is deactivated. r=gsvelto,kmag
    The priority manager tracks which browser parents are active in each process,
    so it needs to be told when a browser parent is no longer active. This
    is currently implemented by observing ipc:browser-destroyed, which happens
    in BrowserHost::DestroyComplete().
    
    However, with Fission, not all BrowserParents are held by BrowserHosts. I
    don't think this is an issue right now, but if the priority manager is going
    to properly prioritize processes due to the presence of active non-top-level
    frames, then it needs to also deprioritize them when they go away.
    
    This patch deals with this situation by directly telling the priority
    manager that the browser parent is becoming inactive via the existing
    ActivityChanged method, in the BrowserParent::Deactivated() method,
    which is called when a browser parent is being destroyed or entering
    the BF cache. I think it makes sense in both cases that we no longer
    want to prioritize the process that the page is in.
    
    This does mean that we are telling the priority manager about more
    ContentParents which is potentially more expensive, but the old code
    also ended up doing a hashtable remove in every single
    ParticularProcessPriorityManager, whereas the new code only does it
    for one, so hopefully it is a net win overall.
    
    (The reason for this is that in ParticularProcessPriorityManager's
    OnBrowserParentDestroyed() method the browserHost->GetContentParent()
    check will always return null because the browser host nulls out
    mRoot immediately before it does ipc:browser-destroyed, so the hash
    table remove is never skipped.)
    
    This patch also removes the last thing that ParticularProcessPriorityManager
    was observing, so it no longer needs to be an nsIObserver.
    
    Differential Revision: https://phabricator.services.mozilla.com/D111654
    
    UltraBlame original commit: b29fdf0da851cfd9eb8cd1f08263c09c2c997d21

--- dom/ipc/BrowserParent.cpp
+++ dom/ipc/BrowserParent.cpp
@@ -604,6 +604,7 @@ void BrowserParent::Deactivated() {
   PointerLockManager::ReleaseLockedRemoteTarget(this);
   PointerEventHandler::ReleasePointerCaptureRemoteTarget(this);
   PresShell::ReleaseCapturingRemoteTarget(this);
+  ProcessPriorityManager::ActivityChanged(this,  false);
 }
 
 void BrowserParent::DestroyInternal() {
