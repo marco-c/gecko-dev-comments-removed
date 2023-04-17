19aadacf1094504654f06c3939d8379816385845
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 03:47:31 +0000

    Bug 1706345: Do not report Telemetry for IPC based Principal vetting if chrome debugging is enabled r=freddyb,Honza,jdescottes
    Differential Revision: https://phabricator.services.mozilla.com/D112931
    
    UltraBlame original commit: 38e917aedf96132ad791bdd592562e13707f7454

--- dom/ipc/ContentParent.cpp
+++ dom/ipc/ContentParent.cpp
@@ -1312,10 +1312,15 @@ void ContentParent::LogAndAssertFailedPrincipalValidationInfo(
 
   extra.AppendElement(EventExtraEntry{"principalType"_ns, principalType});
 
-  Telemetry::EventID eventType =
-      Telemetry::EventID::Security_Fissionprincipals_Contentparent;
-  Telemetry::RecordEvent(eventType, mozilla::Some(aMethod),
-                         mozilla::Some(extra));
+  
+  bool isChromeDebuggingEnabled =
+      Preferences::GetBool("devtools.chrome.enabled", false);
+  if (!isChromeDebuggingEnabled) {
+    Telemetry::EventID eventType =
+        Telemetry::EventID::Security_Fissionprincipals_Contentparent;
+    Telemetry::RecordEvent(eventType, mozilla::Some(aMethod),
+                           mozilla::Some(extra));
+  }
 
   
   MOZ_LOG(
