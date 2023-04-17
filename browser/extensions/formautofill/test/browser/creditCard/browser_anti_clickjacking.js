243eb401aafe15c20c4f203b8b8a2003f8624f49
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 08:57:46 +0000

    Bug 1699794 - [2.0] Add address autocomplete tests r=geckoview-reviewers,agi
    Depends on D109139
    
    Differential Revision: https://phabricator.services.mozilla.com/D109137
    
    UltraBlame original commit: fcc24ac34f5d84900a20aed225293b2c81743890

--- browser/extensions/formautofill/test/browser/creditCard/browser_anti_clickjacking.js
+++ browser/extensions/formautofill/test/browser/creditCard/browser_anti_clickjacking.js
@@ -93,7 +93,6 @@ add_task(async function test_no_delay() {
       
       await SimpleTest.promiseFocus(browser);
       await focusAndWaitForFieldsIdentified(browser, focusInput);
-      await BrowserTestUtils.synthesizeKey("VK_DOWN", {}, browser);
       await expectPopupOpen(browser);
       const firstItem = getDisplayedPopupItems(browser)[0];
       ok(!firstItem.disabled, "Popup should be enabled upon opening.");
