470b6ac4af95d30fbd896687e7d6c3cf6143b112
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 09:39:40 +0000

    Bug 1718748 - Ensure that changing browser.theme.toolbar-theme triggers an style/layout update. r=stransky
    Otherwise nothing invalidates for the potential theme change that it
    causes on GTK / macOS with dark-mode enabled.
    
    Differential Revision: https://phabricator.services.mozilla.com/D119022
    
    UltraBlame original commit: bc079cbb02ff0dbebfb572f1d6e3a7e58aff57d7

--- widget/nsXPLookAndFeel.cpp
+++ widget/nsXPLookAndFeel.cpp
@@ -422,11 +422,17 @@ void nsXPLookAndFeel::OnPrefChanged(const char* aPref, void* aClosure) {
   }
 }
 
-static constexpr nsLiteralCString kMediaQueryPrefs[] = {
-    "browser.display.windows.native_menus"_ns,
-    "browser.proton.enabled"_ns,
-    "browser.proton.places-tooltip.enabled"_ns,
-    "browser.theme.toolbar-theme"_ns,
+static constexpr struct {
+  nsLiteralCString mName;
+  widget::ThemeChangeKind mChangeKind =
+      widget::ThemeChangeKind::MediaQueriesOnly;
+} kMediaQueryPrefs[] = {
+    {"browser.display.windows.native_menus"_ns},
+    {"browser.proton.enabled"_ns},
+    {"browser.proton.places-tooltip.enabled"_ns},
+    
+    
+    {"browser.theme.toolbar-theme"_ns, widget::ThemeChangeKind::AllBits},
 };
 
 
@@ -450,11 +456,12 @@ void nsXPLookAndFeel::Init() {
 
   for (auto& pref : kMediaQueryPrefs) {
     Preferences::RegisterCallback(
-        [](const char*, void*) {
-          LookAndFeel::NotifyChangedAllWindows(
-              widget::ThemeChangeKind::MediaQueriesOnly);
+        [](const char*, void* aChangeKind) {
+          auto changeKind =
+              widget::ThemeChangeKind(reinterpret_cast<uintptr_t>(aChangeKind));
+          LookAndFeel::NotifyChangedAllWindows(changeKind);
         },
-        pref);
+        pref.mName, reinterpret_cast<void*>(uintptr_t(pref.mChangeKind)));
   }
 }
 
