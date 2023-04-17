a4f4a270e027807e8bbb70b530a97d164152a386
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 12:50:36 +0000

    Bug 1308105 - Replace PL_strpbrk with strpbrk r=xpcom-reviewers,kmag
    Differential Revision: https://phabricator.services.mozilla.com/D116933
    
    UltraBlame original commit: 4844183d0b44d434bfd186459cdcec83dfed1a1b

--- modules/libjar/nsJAR.cpp
+++ modules/libjar/nsJAR.cpp
@@ -350,7 +350,7 @@ int32_t nsJAR::ReadLine(const char** src) {
   
   
   int32_t length;
-  char* eol = PL_strpbrk(*src, "\r\n");
+  const char* eol = strpbrk(*src, "\r\n");
 
   if (eol == nullptr)  
   {
