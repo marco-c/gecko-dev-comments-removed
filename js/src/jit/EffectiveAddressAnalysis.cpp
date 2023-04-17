5f998baaf9459dff4b1546787e03e2a56e8b203e
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 19:48:01 +0000

    Bug 1727084 - Memory64 - Preliminaries. r=yury
    Scaffolding: Add assertions everywhere execution currently depends on
    a memory being a memory32.  These assertions will disappear by and by.
    
    Add some useful predicates and guard memory64 on !huge-memory.
    
    Make it possible for the initial heap size to be a 64-bit value,
    update WasmBCE.cpp to deal with this, and note in a comment that this
    does not affect asm.js at all.
    
    Exclude Cranelift and MIPS64 from memory64 in moz.configure; this
    is extra work that does not pay off at this time.
    
    Enable the inclusion of memory64 code by default in moz.configure.
    
    Flip the about:config switch to false so that we can land in Nightly
    without exposing content to m64 until we have completed Milestone 2.
    
    Test cases are in the last patch in the queue.
    
    Differential Revision: https://phabricator.services.mozilla.com/D124887
    
    UltraBlame original commit: 63ce829d63b4d3cb462c23011be830b966cfdf14

--- js/src/jit/EffectiveAddressAnalysis.cpp
+++ js/src/jit/EffectiveAddressAnalysis.cpp
@@ -201,6 +201,7 @@ void EffectiveAddressAnalysis::analyzeAsmJSHeapAccess(AsmJSMemoryAccess* ins) {
   if (base->isConstant()) {
     
     
+    
     int32_t imm = base->toConstant()->toInt32();
     if (imm >= 0) {
       int32_t end = (uint32_t)imm + ins->byteSize();
--- js/src/wasm/WasmBCClass.h
+++ js/src/wasm/WasmBCClass.h
@@ -320,7 +320,10 @@ struct BaseCompiler final {
 
   inline const FuncType& funcType() const;
   inline const TypeIdDesc& funcTypeId() const;
+  inline bool usesMemory() const;
   inline bool usesSharedMemory() const;
+  inline bool isMem32() const;
+  inline bool isMem64() const;
 
   
   operator MacroAssembler&() const { return masm; }
