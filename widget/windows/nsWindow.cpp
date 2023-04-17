cad747159d456a7171938cf51c86b0b721c85b4c
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 23:57:56 +0000

    Bug 1696786 - Fix a couple bugs in Windows rollup code. r=mhowell
    Differential Revision: https://phabricator.services.mozilla.com/D130310
    
    UltraBlame original commit: 8d091e07033cc33c5ceb3f7d600b427a22b8a54a

--- widget/windows/nsWindow.cpp
+++ widget/windows/nsWindow.cpp
@@ -6915,26 +6915,6 @@ void nsWindow::UserActivity() {
   }
 }
 
-LayoutDeviceIntPoint nsWindow::GetTouchCoordinates(WPARAM wParam,
-                                                   LPARAM lParam) {
-  LayoutDeviceIntPoint ret;
-  uint32_t cInputs = LOWORD(wParam);
-  if (cInputs != 1) {
-    
-    return ret;
-  }
-  PTOUCHINPUT pInputs = new TOUCHINPUT[cInputs];
-  if (GetTouchInputInfo((HTOUCHINPUT)lParam, cInputs, pInputs,
-                        sizeof(TOUCHINPUT))) {
-    ret.x = TOUCH_COORD_TO_PIXEL(pInputs[0].x);
-    ret.y = TOUCH_COORD_TO_PIXEL(pInputs[0].y);
-  }
-  delete[] pInputs;
-  
-  
-  return ret;
-}
-
 
 
 static bool TouchDeviceNeedsPanGestureConversion(HANDLE aSource) {
@@ -7924,6 +7904,24 @@ static bool IsTouchSupportEnabled(HWND aWnd) {
   return topWindow ? topWindow->IsTouchWindow() : false;
 }
 
+static Maybe<POINT> GetSingleTouch(WPARAM wParam, LPARAM lParam) {
+  Maybe<POINT> ret;
+  uint32_t cInputs = LOWORD(wParam);
+  if (cInputs != 1) {
+    return ret;
+  }
+  TOUCHINPUT input;
+  if (GetTouchInputInfo((HTOUCHINPUT)lParam, cInputs, &input,
+                        sizeof(TOUCHINPUT))) {
+    ret.emplace();
+    ret->x = TOUCH_COORD_TO_PIXEL(input.x);
+    ret->y = TOUCH_COORD_TO_PIXEL(input.y);
+  }
+  
+  
+  return ret;
+}
+
 
 bool nsWindow::DealWithPopups(HWND aWnd, UINT aMessage, WPARAM aWParam,
                               LPARAM aLParam, LRESULT* aResult) {
@@ -7949,6 +7947,7 @@ bool nsWindow::DealWithPopups(HWND aWnd, UINT aMessage, WPARAM aWParam,
   uint32_t popupsToRollup = UINT32_MAX;
 
   bool consumeRollupEvent = false;
+  Maybe<POINT> touchPoint;  
 
   nsWindow* popupWindow = static_cast<nsWindow*>(popup.get());
   UINT nativeMessage = WinUtils::GetNativeMessage(aMessage);
@@ -7957,6 +7956,10 @@ bool nsWindow::DealWithPopups(HWND aWnd, UINT aMessage, WPARAM aWParam,
       if (!IsTouchSupportEnabled(aWnd)) {
         
         
+        return false;
+      }
+      touchPoint = GetSingleTouch(aWParam, aLParam);
+      if (!touchPoint) {
         return false;
       }
       [[fallthrough]];
@@ -7978,8 +7981,8 @@ bool nsWindow::DealWithPopups(HWND aWnd, UINT aMessage, WPARAM aWParam,
         
         return false;
       }
-      if (!EventIsInsideWindow(popupWindow) &&
-          GetPopupsToRollup(rollupListener, &popupsToRollup)) {
+      if (!EventIsInsideWindow(popupWindow, touchPoint) &&
+          GetPopupsToRollup(rollupListener, &popupsToRollup, touchPoint)) {
         break;
       }
       return false;
@@ -8180,14 +8183,16 @@ bool nsWindow::DealWithPopups(HWND aWnd, UINT aMessage, WPARAM aWParam,
       nativeMessage == WM_POINTERDOWN) {
     LayoutDeviceIntPoint pos;
     if (nativeMessage == WM_TOUCH) {
-      if (nsWindow* win = WinUtils::GetNSWindowPtr(aWnd)) {
-        pos = win->GetTouchCoordinates(aWParam, aLParam);
-      }
+      pos.x = touchPoint->x;
+      pos.y = touchPoint->y;
     } else {
       POINT pt;
       pt.x = GET_X_LPARAM(aLParam);
       pt.y = GET_Y_LPARAM(aLParam);
-      ::ClientToScreen(aWnd, &pt);
+      
+      if (nativeMessage == WM_LBUTTONDOWN) {
+        ::ClientToScreen(aWnd, &pt);
+      }
       pos = LayoutDeviceIntPoint(pt.x, pt.y);
     }
 
