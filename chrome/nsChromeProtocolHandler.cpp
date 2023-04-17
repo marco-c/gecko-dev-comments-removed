0c5bbdb8c662cafcc0da0cab30df7c338e639039
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 13:04:21 +0000

    Bug 1722925 - Avoid unnecessary string copies by nsCString constructor r=necko-reviewers,kershaw CLOSED TREE
    Since the Find method does not exist on nsACString, I had to use std::search
    to find the "data:" substring in the spec.
    
    Differential Revision: https://phabricator.services.mozilla.com/D122082
    
    UltraBlame original commit: a7bb8289f02e9f8cb3ba2d435998d942da49a46c

--- chrome/nsChromeProtocolHandler.cpp
+++ chrome/nsChromeProtocolHandler.cpp
@@ -67,11 +67,10 @@ nsChromeProtocolHandler::GetProtocolFlags(uint32_t* result) {
   
   nsresult rv;
   nsCOMPtr<nsIURI> surl;
-  nsCOMPtr<nsIURI> base(aBaseURI);
   rv =
       NS_MutateURI(new mozilla::net::nsStandardURL::Mutator())
           .Apply(&nsIStandardURLMutator::Init, nsIStandardURL::URLTYPE_STANDARD,
-                 -1, nsCString(aSpec), aCharset, aBaseURI, nullptr)
+                 -1, aSpec, aCharset, aBaseURI, nullptr)
 
           .Finalize(surl);
   if (NS_FAILED(rv)) {
