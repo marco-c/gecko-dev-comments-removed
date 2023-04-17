dc5b39757e9ea36b9a2e04173b052a20ff4c6a86
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 15:58:12 +0000

    Bug 1729280 - Fix links to WebDriver mailing list. r=webdriver-reviewers,jdescottes
    Differential Revision: https://phabricator.services.mozilla.com/D125455
    
    UltraBlame original commit: f12a02c0c5c814640232fa1f5652b71097f449d3

--- testing/firefox-ui/harness/setup.py
+++ testing/firefox-ui/harness/setup.py
@@ -38,7 +38,7 @@ setup(
     
     keywords="mozilla",
     author="Auto-tools",
-    author_email="tools-marionette@lists.mozilla.org",
+    author_email="dev-webdriver@mozilla.org",
     url="https://wiki.mozilla.org/Auto-tools/Projects/Marionette/Harnesses/FirefoxUI",
     license="MPL",
     packages=find_packages(),
