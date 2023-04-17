3e9d027963aba3acabc3b5068f2eb352afd3021c
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 01:34:55 +0000

    Bug 1668952: Part 1 - Remove GeckoView-specific limits on URI length from Necko; r=necko-reviewers,valentin
    For webcompat reasons, we have determined that we should only limit the length
    of URIs in specific cases. We're going to handle this on the GV side instead.
    
    Differential Revision: https://phabricator.services.mozilla.com/D109426
    
    UltraBlame original commit: dd2b562ed225817d77fcdebc0e863a414aadf9a9

--- netwerk/protocol/data/nsDataHandler.cpp
+++ netwerk/protocol/data/nsDataHandler.cpp
@@ -13,10 +13,6 @@
 #include "nsSimpleURI.h"
 #include "mozilla/dom/MimeType.h"
 
-#ifdef ANDROID
-#  include "mozilla/StaticPrefs_network.h"
-#endif
-
 
 
 NS_IMPL_ISUPPORTS(nsDataHandler, nsIProtocolHandler, nsISupportsWeakReference)
@@ -59,12 +55,6 @@ nsDataHandler::GetProtocolFlags(uint32_t* result) {
   nsCOMPtr<nsIURI> uri;
 
   const nsPromiseFlatCString& spec = PromiseFlatCString(aSpec);
-
-#ifdef ANDROID
-  
-  if (spec.Length() > StaticPrefs::network_data_max_uri_length_mobile())
-    return NS_ERROR_OUT_OF_MEMORY;
-#endif
 
   if (aBaseURI && !spec.IsEmpty() && spec[0] == '#') {
     
