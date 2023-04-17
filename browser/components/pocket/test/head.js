5f84921781b3c5b0eb067b23c765151ab6f13d3c
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 02:42:22 +0000

    Bug 1705273 - Fixing Pocket home panel test. r=gvn
    Differential Revision: https://phabricator.services.mozilla.com/D112398
    
    UltraBlame original commit: d1efd293ebd2de0c6a3dede8a1eb85ed120c2542

--- browser/components/pocket/test/head.js
+++ browser/components/pocket/test/head.js
@@ -8,6 +8,9 @@ ChromeUtils.defineModuleGetter(
   "pktApi",
   "chrome://pocket/content/pktApi.jsm"
 );
+const { ContentTaskUtils } = ChromeUtils.import(
+  "resource://testing-common/ContentTaskUtils.jsm"
+);
 const { sinon } = ChromeUtils.import("resource://testing-common/Sinon.jsm");
 
 
