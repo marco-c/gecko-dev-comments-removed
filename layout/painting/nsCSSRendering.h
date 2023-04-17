55eeebd8173165a0f0941bd2d73d81f2d6bf6e8f
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 00:37:30 +0000

    Bug 1702216 - Paint auto-style outline with webrender if possible. r=mstange
    I noticed we weren't doing this when looking at bug 1701910.
    
    We remove support for auto style outlines in trees, which is unused
    (checked that thunderbird and FF trees don't use outline: auto) and for
    some mathml debugging code, which seems ok.
    
    Differential Revision: https://phabricator.services.mozilla.com/D110399
    
    UltraBlame original commit: 96866f6d4543d67e033330461533f7cd5870679f

--- layout/painting/nsCSSRendering.h
+++ layout/painting/nsCSSRendering.h
@@ -199,10 +199,13 @@ struct nsCSSRendering {
       const nsStyleBorder& aBorderStyle, mozilla::ComputedStyle* aStyle,
       bool* aOutBorderIsEmpty, Sides aSkipSides = Sides());
 
-  static mozilla::Maybe<nsCSSBorderRenderer> CreateBorderRendererForOutline(
-      nsPresContext* aPresContext, gfxContext* aRenderingContext,
-      nsIFrame* aForFrame, const nsRect& aDirtyRect, const nsRect& aBorderArea,
-      mozilla::ComputedStyle* aStyle);
+  static mozilla::Maybe<nsCSSBorderRenderer>
+  CreateBorderRendererForNonThemedOutline(nsPresContext* aPresContext,
+                                          DrawTarget* aDrawTarget,
+                                          nsIFrame* aForFrame,
+                                          const nsRect& aDirtyRect,
+                                          const nsRect& aInnerRect,
+                                          mozilla::ComputedStyle* aStyle);
 
   static ImgDrawResult CreateWebRenderCommandsForBorder(
       nsDisplayItem* aItem, nsIFrame* aForFrame, const nsRect& aBorderArea,
@@ -231,11 +234,12 @@ struct nsCSSRendering {
   
 
 
-
-  static void PaintOutline(nsPresContext* aPresContext,
-                           gfxContext& aRenderingContext, nsIFrame* aForFrame,
-                           const nsRect& aDirtyRect, const nsRect& aBorderArea,
-                           mozilla::ComputedStyle* aStyle);
+  static void PaintNonThemedOutline(nsPresContext* aPresContext,
+                                    gfxContext& aRenderingContext,
+                                    nsIFrame* aForFrame,
+                                    const nsRect& aDirtyRect,
+                                    const nsRect& aInnerRect,
+                                    mozilla::ComputedStyle* aStyle);
 
   
 
