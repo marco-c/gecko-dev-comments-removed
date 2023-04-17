f02c61e3dadf572689ae42c8fb8b0d74014e9d53
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 09:17:38 +0000

    Bug 1713415 - Don't discard the unprocessed loading events. r=aklotz
    We had the constant `kMaxEvents` which limits the maximum number of loading
    events in `UntrustedModulesData`.  Because we had a check for the number of
    events in `UntrustedModulesData::AddNewLoads` where we already swapped
    `UntrustedModulesProcessor::mUnprocessedModuleLoads` with a local variable,
    when the array exceeds `kMaxEvents`, we discarded the remaining loading events.
    
    The proposed fix is to check for `kMaxEvents` before swapping the unprocessed
    events, so that the remaining events will be processed the next time.
    
    This was caught by the `UntrustedModulesFixture` GTest.  This test had
    another test bug that it always expected modules were loaded in the main
    thread except the predefined known modules.  This is not correct because
    the same process may have loaded a bunch of modules in the earlier GTests
    such as graphics drivers in different threads.  This patch includes a fix
    for that bug as well.
    
    Differential Revision: https://phabricator.services.mozilla.com/D117433
    
    UltraBlame original commit: a90edecdce6fbbfa571f0fdd6149d81707406d67

--- toolkit/xre/UntrustedModulesProcessor.cpp
+++ toolkit/xre/UntrustedModulesProcessor.cpp
@@ -553,7 +553,23 @@ void UntrustedModulesProcessor::ProcessModuleLoadQueue() {
   {  
     MutexAutoLock lock(mUnprocessedMutex);
     CancelScheduledProcessing(lock);
-    loadsToProcess.swap(mUnprocessedModuleLoads);
+
+    
+    
+    const size_t newDataLength = mProcessedModuleLoads.mEvents.length() +
+                                 mUnprocessedModuleLoads.length();
+    if (newDataLength <= UntrustedModulesData::kMaxEvents) {
+      loadsToProcess.swap(mUnprocessedModuleLoads);
+    } else {
+      
+      
+      
+      const size_t capacity = newDataLength - UntrustedModulesData::kMaxEvents;
+      auto moveRangeBegin = mUnprocessedModuleLoads.begin();
+      auto moveRangeEnd = moveRangeBegin + capacity;
+      Unused << loadsToProcess.moveAppend(moveRangeBegin, moveRangeEnd);
+      mUnprocessedModuleLoads.erase(moveRangeBegin, moveRangeEnd);
+    }
   }
 
   if (!mAllowProcessing || loadsToProcess.empty()) {
