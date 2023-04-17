576bffeb5e69185284f3eec56e66610f9418c777
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 23:58:47 +0000

    Bug 1730881 - Initialize CompilationSyntaxParseCache from InputScript variants. r=arai
    This change clone some of the functions used to initialize the
    CompilationSyntaxParseCache. These are specialized to copy the minimal set of
    information needed for skipping inner functions and for iterating over
    closed-over-bindings.
    
    Unforutnately, as opposed to what this structure was initialy designed for, we
    are not yet able to reuse the Stencils from the InputScript as ParseAtomIndex of
    the InputScript are in the context of the InputScript and not of the
    CompilationState which wraps the CompilationSyntaxParseCache. Until we are
    capable of reusing the same indexes of a previous compilation, we would have to
    duplicate the Stencil structures. Thus, copyScriptInfo and
    copyClosedOVerBindings are copied from the original functions and adapted to
    work with Stencil inputs.
    
    Differential Revision: https://phabricator.services.mozilla.com/D128180
    
    UltraBlame original commit: e304b65e2e2275c6295808dafa0cf6cdd6882f45

--- js/src/frontend/CompilationStencil.h
+++ js/src/frontend/CompilationStencil.h
@@ -657,7 +657,8 @@ struct CompilationInput {
 
   
   
-  BaseScript* lazyOuterScript() { return lazy_.raw().as<BaseScript*>(); }
+  InputScript lazyOuterScript() { return lazy_; }
+  BaseScript* lazyOuterBaseScript() { return lazy_.raw().as<BaseScript*>(); }
 
   
   
@@ -774,7 +775,8 @@ class CompilationSyntaxParseCache {
   
   [[nodiscard]] bool init(JSContext* cx, LifoAlloc& alloc,
                           ParserAtomsTable& parseAtoms,
-                          CompilationAtomCache& atomCache, BaseScript* lazy);
+                          CompilationAtomCache& atomCache,
+                          const InputScript& lazy);
 
  private:
   
@@ -794,15 +796,23 @@ class CompilationSyntaxParseCache {
   [[nodiscard]] bool copyFunctionInfo(JSContext* cx,
                                       ParserAtomsTable& parseAtoms,
                                       CompilationAtomCache& atomCache,
-                                      BaseScript* lazy);
+                                      const InputScript& lazy);
   [[nodiscard]] bool copyScriptInfo(JSContext* cx, LifoAlloc& alloc,
                                     ParserAtomsTable& parseAtoms,
                                     CompilationAtomCache& atomCache,
                                     BaseScript* lazy);
+  [[nodiscard]] bool copyScriptInfo(JSContext* cx, LifoAlloc& alloc,
+                                    ParserAtomsTable& parseAtoms,
+                                    CompilationAtomCache& atomCache,
+                                    const ScriptStencilRef& lazy);
   [[nodiscard]] bool copyClosedOverBindings(JSContext* cx, LifoAlloc& alloc,
                                             ParserAtomsTable& parseAtoms,
                                             CompilationAtomCache& atomCache,
                                             BaseScript* lazy);
+  [[nodiscard]] bool copyClosedOverBindings(JSContext* cx, LifoAlloc& alloc,
+                                            ParserAtomsTable& parseAtoms,
+                                            CompilationAtomCache& atomCache,
+                                            const ScriptStencilRef& lazy);
 };
 
 
@@ -1284,7 +1294,7 @@ struct MOZ_RAII CompilationState : public ExtensibleCompilationStencil {
 
     
     if (input.isDelazifying()) {
-      BaseScript* lazy = input.lazyOuterScript();
+      InputScript lazy = input.lazyOuterScript();
       auto& atomCache = input.atomCache;
       if (!previousParseCache.init(cx, alloc, parserAtoms, atomCache, lazy)) {
         return false;
