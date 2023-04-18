f8b1c1da7c24db97363343bb320038776e814d5f
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 01:06:32 +0000

    Bug 1740897 [X11] Make parent container of D&D popup transparent to avoid flickering, r=emilio
    Differential Revision: https://phabricator.services.mozilla.com/D131252
    
    UltraBlame original commit: ccb47922815f300ae3d02d1213b3561de6bb4271

--- widget/gtk/nsWindow.cpp
+++ widget/gtk/nsWindow.cpp
@@ -5252,13 +5252,22 @@ void nsWindow::ConfigureGdkWindow() {
   }
 #endif
 
-  if (mIsDragPopup && GdkIsWaylandDisplay()) {
-    GtkWidget* parent = gtk_widget_get_parent(mShell);
-    if (parent) {
-      GtkWidgetDisableUpdates(parent);
+  if (mIsDragPopup) {
+    if (GdkIsWaylandDisplay()) {
+      
+      
+      
+      if (GtkWidget* parent = gtk_widget_get_parent(mShell)) {
+        GtkWidgetDisableUpdates(parent);
+      }
+      GtkWidgetDisableUpdates(mShell);
+      GtkWidgetDisableUpdates(GTK_WIDGET(mContainer));
+    } else {
+      
+      if (GtkWidget* parent = gtk_widget_get_parent(mShell)) {
+        gtk_window_set_opacity(GTK_WINDOW(parent), 0.0);
+      }
     }
-    GtkWidgetDisableUpdates(mShell);
-    GtkWidgetDisableUpdates(GTK_WIDGET(mContainer));
   }
 
   if (mWindowType == eWindowType_popup) {
