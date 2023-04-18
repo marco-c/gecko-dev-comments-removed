5d50c9edb4ec7a2ad4d3d20c86dcd9fb3d1f9532
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 06:59:19 +0000

    Bug 1745248 - Allow each autofill feature to detect if it should be enabled depending on search region. r=dimi,sgalich,preferences-reviewers,skhamis,LougeniaBailey
    Differential Revision: https://phabricator.services.mozilla.com/D133818
    
    UltraBlame original commit: 06d57e874522277d1d92f263d70a03d6c9054b14

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -4597,11 +4597,19 @@ pref("security.external_protocol_requires_permission", true);
 
 
 
+
+
+
 pref("extensions.formautofill.available", "detect");
+pref("extensions.formautofill.addresses.supported", "detect");
 pref("extensions.formautofill.addresses.enabled", true);
 pref("extensions.formautofill.addresses.capture.enabled", false);
+pref("extensions.formautofill.addresses.supportedCountries", "US,CA");
+
 pref("extensions.formautofill.creditCards.available", true);
+pref("extensions.formautofill.creditCards.supported", "detect");
 pref("extensions.formautofill.creditCards.enabled", true);
+pref("extensions.formautofill.creditCards.supportedCountries", "US,CA");
 
 
 pref("extensions.formautofill.creditCards.hideui", false);
@@ -4620,5 +4628,4 @@ pref("extensions.formautofill.loglevel", "Warn");
 
 pref("toolkit.osKeyStore.loglevel", "Warn");
 
-pref("extensions.formautofill.supportedCountries", "US,CA");
 pref("extensions.formautofill.supportRTL", false);
