f92b08533b0ad03b94eede2fb32f3aeadf3baba0
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 16:48:18 +0000

    Bug 1764249: Make HyperTextAccessibleBase::CroppedSelectionRanges remove collapsed ranges when called on a document. r=morgan
    There's no point cropping ranges when called on the document, but we still want to remove collapsed ranges.
    The path to hell is paved with premature optimisation. :(
    
    Differential Revision: https://phabricator.services.mozilla.com/D143579
    
    UltraBlame original commit: bbf84e358f0b3d8d672fddcb33fdc0e3a194d060

--- accessible/basetypes/HyperTextAccessibleBase.cpp
+++ accessible/basetypes/HyperTextAccessibleBase.cpp
@@ -550,12 +550,18 @@ void HyperTextAccessibleBase::CroppedSelectionRanges(
     nsTArray<TextRange>& aRanges) const {
   SelectionRanges(&aRanges);
   const Accessible* acc = Acc();
-  if (!acc->IsDoc()) {
-    aRanges.RemoveElementsBy([acc](auto& range) {
-      return range.StartPoint() == range.EndPoint() ||
-             !range.Crop(const_cast<Accessible*>(acc));
-    });
-  }
+  aRanges.RemoveElementsBy([acc](auto& range) {
+    if (range.StartPoint() == range.EndPoint()) {
+      return true;  
+    }
+    
+    
+    if (!acc->IsDoc()) {
+      
+      return !range.Crop(const_cast<Accessible*>(acc));
+    }
+    return false;
+  });
 }
 
 int32_t HyperTextAccessibleBase::SelectionCount() {
