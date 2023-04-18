716d7411a2e414fdbd120a52f1674550ef85de16
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 17:54:42 +0000

    Bug 1724645 - add tests for 'new PerformanceMark' in WPT mark-errors. r=sefeng
    Differential Revision: https://phabricator.services.mozilla.com/D142900
    
    UltraBlame original commit: ddc932dd3d80149b0dfc6035c49c6810d455f299

--- testing/web-platform/tests/user-timing/mark-errors.any.js
+++ testing/web-platform/tests/user-timing/mark-errors.any.js
@@ -1,19 +1,50 @@
-test(function() {
-  assert_throws_js(TypeError, function() { self.performance.mark("mark1", 123); }, "Number passed as a dict argument should cause type-error.")
-}, "Number should be rejected as the mark-options.")
 
-test(function() {
-  assert_throws_js(TypeError, function() { self.performance.mark("mark1", NaN); }, "NaN passed as a dict argument should cause type-error.")
-}, "NaN should be rejected as the mark-options.")
 
-test(function() {
-  assert_throws_js(TypeError, function() { self.performance.mark("mark1", Infinity); }, "Infinity passed as a dict argument should cause type-error.")
-}, "Infinity should be rejected as the mark-options.")
+const markConstructionTests = [
+  {
+    testName: "Number should be rejected as the mark-options.",
+    testFunction: function(newMarkFunction) {
+      assert_throws_js(TypeError, function() { newMarkFunction("mark1", 123); }, "Number passed as a dict argument should cause type-error.");
+    },
+  },
 
-test(function() {
-  assert_throws_js(TypeError, function() { self.performance.mark("mark1", "string"); }, "String passed as a dict argument should cause type-error.")
-}, "String should be rejected as the mark-options.")
+  {
+    testName: "NaN should be rejected as the mark-options.",
+    testFunction: function(newMarkFunction) {
+      assert_throws_js(TypeError, function() { newMarkFunction("mark1", NaN); }, "NaN passed as a dict argument should cause type-error.");
+    },
+  },
 
-test(function() {
-  assert_throws_js(TypeError, function() { self.performance.mark("mark1", {startTime: -1}); }, "Negative startTime should cause type-error.")
-}, "Negative startTime in mark-options should be rejected");
+  {
+    testName: "Infinity should be rejected as the mark-options.",
+    testFunction: function(newMarkFunction) {
+      assert_throws_js(TypeError, function() { newMarkFunction("mark1", Infinity); }, "Infinity passed as a dict argument should cause type-error.");
+    },
+  },
+
+  {
+    testName: "String should be rejected as the mark-options.",
+    testFunction: function(newMarkFunction) {
+      assert_throws_js(TypeError, function() { newMarkFunction("mark1", "string"); }, "String passed as a dict argument should cause type-error.")
+    },
+  },
+
+  {
+    testName: "Negative startTime in mark-options should be rejected",
+    testFunction: function(newMarkFunction) {
+      assert_throws_js(TypeError, function() { newMarkFunction("mark1", {startTime: -1}); }, "Negative startTime should cause type-error.")
+    },
+  },
+];
+
+
+
+for (let testInfo of markConstructionTests) {
+  test(function() {
+    testInfo.testFunction(self.performance.mark);
+  }, `[performance.mark]: ${testInfo.testName}`);
+
+  test(function() {
+    testInfo.testFunction((markName, obj) => new PerformanceMark(markName, obj));
+  }, `[new PerformanceMark]: ${testInfo.testName}`);
+}
