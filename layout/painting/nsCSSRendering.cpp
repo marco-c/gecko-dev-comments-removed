3f6b88f2cb9e3d58df025ad58d3b17b38c938e33
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 20:43:01 +0000

    Bug 1769935. Only do partial bg image drawing in content process.  r=aosmond
    Differential Revision: https://phabricator.services.mozilla.com/D146686
    
    UltraBlame original commit: ee2ca87ef3d6d503a4a1ed79d501fc8cd29dd701

--- layout/painting/nsCSSRendering.cpp
+++ layout/painting/nsCSSRendering.cpp
@@ -2973,8 +2973,11 @@ nsBackgroundLayerState nsCSSRendering::PrepareImageLayer(
   if (aFlags & nsCSSRendering::PAINTBG_HIGH_QUALITY_SCALING) {
     irFlags |= nsImageRenderer::FLAG_HIGH_QUALITY_SCALING;
   }
+  
+  
+  
   if (StaticPrefs::layout_display_partial_background_images() &&
-      !aPresContext->IsChrome()) {
+      XRE_IsContentProcess() && !aPresContext->IsChrome()) {
     irFlags |= nsImageRenderer::FLAG_DRAW_PARTIAL_FRAMES;
   }
 
