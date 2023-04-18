dd88c572f15d394ca22f1313bef0983de6c02050
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 22:10:38 +0000

    Bug 1771950 - Factor out the PiP code to show the native window menu. r=stransky
    Differential Revision: https://phabricator.services.mozilla.com/D148224
    
    UltraBlame original commit: 98403cdbbd81354a7fcadf3a5367ae2c1842953c

--- widget/gtk/nsWindow.cpp
+++ widget/gtk/nsWindow.cpp
@@ -1490,7 +1490,7 @@ bool nsWindow::WaylandPopupIsAnchored() {
     
     return false;
   }
-  return popupFrame->GetAnchor() != nullptr;
+  return !!popupFrame->GetAnchor();
 }
 
 bool nsWindow::IsWidgetOverflowWindow() {
@@ -4286,6 +4286,12 @@ void nsWindow::DispatchContextMenuEventFromMouseEvent(uint16_t domButton,
   }
 }
 
+void nsWindow::TryToShowNativeWindowMenu(GdkEventButton* aEvent) {
+  if (!gdk_window_show_window_menu(GetToplevelGdkWindow(), (GdkEvent*)aEvent)) {
+    NS_WARNING("Native context menu wasn't shown");
+  }
+}
+
 void nsWindow::OnButtonPressEvent(GdkEventButton* aEvent) {
   LOG("Button %u press\n", aEvent->button);
 
@@ -4417,7 +4423,7 @@ void nsWindow::OnButtonReleaseEvent(GdkEventButton* aEvent) {
 
   
   
-  LayoutDeviceIntPoint pos = event.mRefPoint;
+  const LayoutDeviceIntPoint pos = event.mRefPoint;
 
   nsIWidget::ContentAndAPZEventStatus eventStatus = DispatchInputEvent(&event);
 
@@ -4445,10 +4451,7 @@ void nsWindow::OnButtonReleaseEvent(GdkEventButton* aEvent) {
   
   
   if (mIsPIPWindow && aEvent->button == 3) {
-    if (!gdk_window_show_window_menu(GetToplevelGdkWindow(),
-                                     (GdkEvent*)aEvent)) {
-      NS_WARNING("Native context menu wasn't shown");
-    }
+    TryToShowNativeWindowMenu(aEvent);
   }
 }
 
