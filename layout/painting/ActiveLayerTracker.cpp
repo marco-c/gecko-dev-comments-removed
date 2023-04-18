b35ce67d4a108b9a532da25a6fbdb890ce820228
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 06:55:53 +0000

    Bug 1751806. Remove usage of gfxVars::UseWebRender in ActiveLayerTracker which is always true. r=mstange
    Differential Revision: https://phabricator.services.mozilla.com/D136815
    
    UltraBlame original commit: 7b32aaf77f7a7d1f0e964b8ad0549bd81fe2fa4d

--- layout/painting/ActiveLayerTracker.cpp
+++ layout/painting/ActiveLayerTracker.cpp
@@ -173,11 +173,6 @@ void LayerActivityTracker::NotifyExpired(LayerActivity* aObject) {
              "its frame or its content");
 
   if (f) {
-    
-    
-    if (f->PresContext()->GetContainerWeak() && !gfxVars::UseWebRender()) {
-      f->SchedulePaint(nsIFrame::PAINT_DEFAULT, false);
-    }
     f->RemoveStateBits(NS_FRAME_HAS_LAYER_ACTIVITY_PROPERTY);
     f->RemoveProperty(LayerActivityProperty());
   } else {
