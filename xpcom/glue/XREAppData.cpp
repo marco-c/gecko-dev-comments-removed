3bf5de2fff046e6dadf3c7f3bc6e5b6b81bad895
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 19:10:29 +0000

    Bug 1762356 - Introduce XREAppData::GetDBUSAppName. r=jhorak
    And do a more in-depth sanitization than what we were doing.
    
    Differential Revision: https://phabricator.services.mozilla.com/D145863
    
    UltraBlame original commit: 668c01a82900f8f17a8352bdab709a4f16873648

--- xpcom/glue/XREAppData.cpp
+++ xpcom/glue/XREAppData.cpp
@@ -31,4 +31,25 @@ XREAppData& XREAppData::operator=(const StaticXREAppData& aOther) {
 
 XREAppData& XREAppData::operator=(const XREAppData& aOther) = default;
 
+void XREAppData::SanitizeNameForDBus(nsACString& aName) {
+  auto IsValidDBusNameChar = [](char aChar) {
+    return IsAsciiAlpha(aChar) || IsAsciiDigit(aChar) || aChar == '_';
+  };
+
+  
+  
+  char* cur = aName.BeginWriting();
+  char* end = aName.EndWriting();
+  for (; cur != end; cur++) {
+    if (!IsValidDBusNameChar(*cur)) {
+      *cur = '_';
+    }
+  }
+}
+
+void XREAppData::GetDBusAppName(nsACString& aName) const {
+  aName.Assign(remotingName);
+  SanitizeNameForDBus(aName);
+}
+
 }  
