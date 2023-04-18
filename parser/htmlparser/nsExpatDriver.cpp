ed586bc4e20c5d6a4a30b0317e709ace733f46ac
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 01:51:48 +0000

    Bug 1732201 - Sandbox woff2 in OTS using RLBox r=bholley
    Differential Revision: https://phabricator.services.mozilla.com/D126435
    
    UltraBlame original commit: dcd88b342c8c7dff9720d78bb83c2e4a29fa657e

--- parser/htmlparser/nsExpatDriver.cpp
+++ parser/htmlparser/nsExpatDriver.cpp
@@ -36,6 +36,7 @@
 
 #include "nsThreadUtils.h"
 #include "mozilla/ClearOnShutdown.h"
+#include "mozilla/RLBoxUtils.h"
 #include "mozilla/UniquePtr.h"
 
 #include "mozilla/Logging.h"
@@ -109,31 +110,9 @@ static const XML_Char* unverified_xml_string(uintptr_t ptr) {
 
 
 
-
 template <typename T>
-class MOZ_STACK_CLASS TransferBuffer {
- public:
-  TransferBuffer() = delete;
-  TransferBuffer(rlbox_sandbox_expat* aSandbox, const T* aBuf,
-                 const size_t aLen)
-      : mSandbox(aSandbox), mCopied(false), mBuf(nullptr) {
-    if (aBuf) {
-      mBuf = rlbox::copy_memory_or_grant_access(
-          *mSandbox, aBuf, aLen * sizeof(T), false, mCopied);
-    }
-  };
-  ~TransferBuffer() {
-    if (mCopied) {
-      mSandbox->free_in_sandbox(mBuf);
-    }
-  };
-  tainted_expat<const T*> operator*() const { return mBuf; };
-
- private:
-  rlbox_sandbox_expat* mSandbox;
-  bool mCopied;
-  tainted_expat<const T*> mBuf;
-};
+using TransferBuffer =
+    mozilla::RLBoxTransferBufferToSandbox<T, rlbox_expat_sandbox_type>;
 
 
 
