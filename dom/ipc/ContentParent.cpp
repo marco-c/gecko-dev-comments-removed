b0b8f46c58526d46f776199d915f83ed82cbf6f2
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 17:17:29 +0000

    Backed out changeset 95cee3ef708c (bug 1764251) for causing assertion failures at ipc/MessageChannel.h. CLOSED TREE
    UltraBlame original commit: d581fde398bab8a4e9c1de008134d8aeeef7ebd3

--- dom/ipc/ContentParent.cpp
+++ dom/ipc/ContentParent.cpp
@@ -1641,12 +1641,7 @@ void ContentParent::BroadcastMediaCodecsSupportedUpdate(
 
 const nsACString& ContentParent::GetRemoteType() const { return mRemoteType; }
 
-static StaticRefPtr<nsIAsyncShutdownClient> sXPCOMShutdownClient;
-static StaticRefPtr<nsIAsyncShutdownClient> sProfileBeforeChangeClient;
-
 void ContentParent::Init() {
-  MOZ_ASSERT(sXPCOMShutdownClient);
-
   nsCOMPtr<nsIObserverService> obs = mozilla::services::GetObserverService();
   if (obs) {
     size_t length = ArrayLength(sObserverTopics);
@@ -1654,6 +1649,8 @@ void ContentParent::Init() {
       obs->AddObserver(this, sObserverTopics[i], false);
     }
   }
+
+  AddShutdownBlockers();
 
   if (obs) {
     nsAutoString cpId;
@@ -2496,11 +2493,6 @@ bool ContentParent::BeginSubprocessLaunch(ProcessPriority aPriority) {
 
   
   
-  
-  AddShutdownBlockers();
-
-  
-  
   if (!ContentProcessManager::GetSingleton()) {
     NS_WARNING(
         "Shutdown has begun, we shouldn't spawn any more child processes");
@@ -2593,7 +2585,6 @@ void ContentParent::LaunchSubprocessReject() {
     mIsAPreallocBlocker = false;
   }
   MarkAsDead();
-  RemoveShutdownBlockers();
 }
 
 bool ContentParent::LaunchSubprocessResolve(bool aIsSync,
@@ -2706,12 +2697,13 @@ bool ContentParent::LaunchSubprocessSync(
   
   Telemetry::Accumulate(Telemetry::CONTENT_PROCESS_LAUNCH_IS_SYNC, 1);
 
-  if (BeginSubprocessLaunch(aInitialPriority)) {
-    const bool ok = mSubprocess->WaitForProcessHandle();
-    if (ok && LaunchSubprocessResolve( true, aInitialPriority)) {
-      ContentParent::DidLaunchSubprocess();
-      return true;
-    }
+  if (!BeginSubprocessLaunch(aInitialPriority)) {
+    return false;
+  }
+  const bool ok = mSubprocess->WaitForProcessHandle();
+  if (ok && LaunchSubprocessResolve( true, aInitialPriority)) {
+    ContentParent::DidLaunchSubprocess();
+    return true;
   }
   LaunchSubprocessReject();
   return false;
@@ -3570,6 +3562,9 @@ ContentParent::GetState(nsIPropertyBag** aResult) {
   *aResult = props.forget().downcast<nsIWritablePropertyBag>().take();
   return NS_OK;
 }
+
+static StaticRefPtr<nsIAsyncShutdownClient> sXPCOMShutdownClient;
+static StaticRefPtr<nsIAsyncShutdownClient> sProfileBeforeChangeClient;
 
 static void InitShutdownClients() {
   if (!sXPCOMShutdownClient) {
