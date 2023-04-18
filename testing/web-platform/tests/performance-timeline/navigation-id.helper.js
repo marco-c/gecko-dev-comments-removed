135e5b8a7f58a3ff8997cbedc0c6d78127fc3522
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 23:07:41 +0000

    Bug 1769564 [wpt PR 34073] - Add navigation_id to all types of performance entries., a=testonly
    Automatic update from web-platform-tests
    Add navigation_id to all types of performance entries.
    
    Add navigation_id field to all performance entries. The navigation_id starts from 1 and increments whenever there is a back/forward cache navigation. This way, its value represents different sessions in the navigation history.
    
    Bug: I15d9396303e868aa5850b86aa4cc624f15d55f7e
    Change-Id: I99652c42a166916294c1c9994ac2161534cdcf52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3599609
    Reviewed-by: Yoav Weiss <yoavweisschromium.org>
    Reviewed-by: Ian Clelland <iclellandchromium.org>
    Commit-Queue: Yoav Weiss <yoavweisschromium.org>
    Cr-Commit-Position: refs/heads/main{#1011846}
    
    --
    
    wpt-commits: f399d7b775fade572b2e99cff3ef181b805893aa
    wpt-pr: 34073
    
    UltraBlame original commit: 7c3fb7e97b05fd32394684c2d66c657b4e6da9a5

--- testing/web-platform/tests/performance-timeline/navigation-id.helper.js
+++ testing/web-platform/tests/performance-timeline/navigation-id.helper.js
@@ -3,51 +3,99 @@
 
 
 
-function runNavigationIdTest(params, description) {
-  const defaultParams = {
-    constants: {
-      performanceMarkName: 'mark_navigation_id',
-      performanceMeasureName: 'measure_navigation_id',
-    },
-    
-    
-    
-    funcBeforeNavigation: (constants) => {
-      window.performance.mark(constants.performanceMarkName);
-      return window.performance
-        .getEntriesByName(constants.performanceMarkName)[0]
-        .navigationId;
-    },
-    funcAfterBFCacheLoad: (expectedNavigationId, constants) => {
-      window.performance.mark(
-        constants.performanceMarkName + expectedNavigationId);
-      window.performance.measure(
-        constants.performanceMeasureName + expectedNavigationId,
-        constants.performanceMarkName,
-        constants.performanceMarkName + expectedNavigationId);
-      return [
-        window.performance
-          .getEntriesByName(
-            constants.performanceMarkName + expectedNavigationId)[0]
-          .navigationId,
-        window.performance
-          .getEntriesByName(
-            constants.performanceMeasureName + expectedNavigationId)[0]
-          .navigationId
-      ];
-    },
-  };
-  params = { ...defaultParams, ...params };
-  runBfcacheWithMultipleNavigationTest(params, description);
+
+
+let testInitial = () => {
+  return window.performance.getEntries().map(e => e.navigationId);
 }
 
-function runBfcacheWithMultipleNavigationTest(params, description) {
+let testMarkMeasure = (expectedNavigationId, markName, MeasureName) => {
+  const markName1 = 'test-mark';
+  const markName2 = 'test-mark' + expectedNavigationId;
+  const measureName = 'test-measure' + expectedNavigationId;
+
+  window.performance.mark(markName1);
+  window.performance.mark(markName2);
+  window.performance.measure(measureName, markName1, markName2);
+  return window.performance.getEntriesByName(markName2).concat(
+    window.performance.getEntriesByName(measureName)).map(e => e.navigationId);
+}
+
+let testResourceTiming = async (expectedNavigationId) => {
+  let navigationId = -1;
+
+  let p = new Promise(resolve => {
+    new PerformanceObserver((list) => {
+      const entry = list.getEntries().find(e => e.name.includes('json_resource') && e.navigationId == expectedNavigationId);
+      if (entry) {
+        navigationId = entry.navigationId;
+        resolve();
+      }
+    }).observe({ type: 'resource' });
+  });
+
+  const resp = await fetch('/performance-timeline/resources/json_resource.json');
+  await p;
+  return [navigationId];
+}
+
+let testElementTiming = async (expectedNavigationId) => {
+  let navigationId = -1;
+  let p = new Promise(resolve => {
+    new PerformanceObserver((list) => {
+      const entry = list.getEntries().find(e => e.entryType === 'element' && e.identifier === 'test-element-timing' + expectedNavigationId);
+      if (entry) {
+        navigationId = entry.navigationId;
+        resolve();
+      }
+    }).observe({ type: 'element' });
+  });
+
+  let el = document.createElement('p');
+  el.setAttribute('elementtiming', 'test-element-timing' + expectedNavigationId);
+  el.textContent = 'test element timing text';
+  document.body.appendChild(el);
+  await p;
+  return [navigationId];
+}
+
+let testLongTask = async () => {
+  let navigationIds = [];
+
+  let p = new Promise(resolve => {
+    new PerformanceObserver((list) => {
+      const entry = list.getEntries().find(e => e.entryType === 'longtask')
+      if (entry) {
+        navigationIds.push(entry.navigationId);
+        navigationIds = navigationIds.concat(
+          entry.attribution.map(a => a.navigationId));
+        resolve();
+      }
+    }).observe({ type: 'longtask' });
+  });
+
+  const script = document.createElement('script');
+  script.src = '/performance-timeline/resources/make_long_task.js';
+  document.body.appendChild(script);
+  await p;
+  document.body.removeChild(script);
+  return navigationIds;
+}
+
+const testFunctionMap = {
+  'mark_measure': testMarkMeasure,
+  'resource_timing': testResourceTiming,
+  'element_timing': testElementTiming,
+  'long_task_task_attribution': testLongTask,
+};
+
+function runNavigationIdTest(params, description) {
   const defaultParams = {
     openFunc: url => window.open(url, '_blank', 'noopener'),
     scripts: [],
     funcBeforeNavigation: () => { },
     targetOrigin: originCrossSite,
-    navigationTimes: 1,
+    navigationTimes: 4,
     funcAfterAssertion: () => { },
   }  
   params = { ...defaultParams, ...params };
@@ -58,25 +106,25 @@ function runBfcacheWithMultipleNavigationTest(params, description) {
 
     const urlA = executorPath + pageA.context_id;
     const urlB = params.targetOrigin + executorPath + pageB.context_id;
-
+    
     params.openFunc(urlA);
-
     await pageA.execute_script(waitForPageShow);
 
     
-    let navigationId = await pageA.execute_script(
-      params.funcBeforeNavigation, [params.constants])
-    assert_implements_optional(
-      navigationId === 1, 'Navigation Id should be 0.');
+    let navigationIds = await pageA.execute_script(testInitial);
+    assert_true(
+      navigationIds.every(t => t === 1), 'All Navigation Ids should be 1.');
 
     for (i = 1; i <= params.navigationTimes; i++) {
+      
       await navigateAndThenBack(pageA, pageB, urlB);
 
-      let navigationIds = await pageA.execute_script(
-        params.funcAfterBFCacheLoad, [i + 1, params.constants]);
-      assert_implements_optional(
+      
+      navigationIds = await pageA.execute_script(
+        testFunctionMap[params.testName], [i + 1]);
+      assert_true(
         navigationIds.every(t => t === (i + 1)),
-        'Navigation Id should all be ' + (i + 1) + '.');
+        params.testName + ' Navigation Id should all be ' + (i + 1) + '.');
     }
   }, description);
 }
