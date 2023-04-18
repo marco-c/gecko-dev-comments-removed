c55c2af8d4e3ef35219204fc2d42722217db4bda
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 19:04:48 +0000

    Bug 1668219 - [devtools] Remove now unused devtools/shared/performance folder. r=julienw.
    Differential Revision: https://phabricator.services.mozilla.com/D145466
    
    UltraBlame original commit: 90bc436df1c60961bab086d820e7a75e9a773e99

--- devtools/client/shared/test/head.js
+++ devtools/client/shared/test/head.js
@@ -155,27 +155,6 @@ async function openAndCloseToolbox(nbOfTimes, usageTime, toolId) {
 
 
 
-function synthesizeProfileForTest(samples) {
-  const RecordingUtils = require("devtools/shared/performance/recording-utils");
-
-  samples.unshift({
-    time: 0,
-    frames: [],
-  });
-
-  const uniqueStacks = new RecordingUtils.UniqueStacks();
-  return RecordingUtils.deflateThread(
-    {
-      samples: samples,
-      markers: [],
-    },
-    uniqueStacks
-  );
-}
-
-
-
-
 
 
 
