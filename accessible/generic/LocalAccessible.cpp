fa8c1a3c831c6afbe6d911bf50a7aa02dabcd725
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 23:32:29 +0000

    Bug 1760739 part 3: Expose ARIA object attributes for cached RemoteAccessibles. r=eeejay
    Differential Revision: https://phabricator.services.mozilla.com/D149222
    
    UltraBlame original commit: 4685e0b0464527fb8c3c454f1b8898f467288012

--- accessible/generic/LocalAccessible.cpp
+++ accessible/generic/LocalAccessible.cpp
@@ -1318,6 +1318,7 @@ void LocalAccessible::DOMAttributeChanged(int32_t aNameSpaceID,
     if (StringBeginsWith(nsDependentAtomString(aAttribute), u"aria-"_ns)) {
       uint8_t attrFlags = aria::AttrCharacteristicsFor(aAttribute);
       if (!(attrFlags & ATTR_BYPASSOBJ)) {
+        mDoc->QueueCacheUpdate(this, CacheDomain::ARIA);
         
         
         
@@ -3535,6 +3536,25 @@ already_AddRefed<AccAttributes> LocalAccessible::BundleFieldsForCache(
       } else {
         fields->SetAttribute(nsGkAtoms::headers, DeleteEntry());
       }
+    }
+  }
+
+  if (aCacheDomain & CacheDomain::ARIA && mContent) {
+    
+    
+    
+    RefPtr<AccAttributes> ariaAttrs;
+    aria::AttrIterator attrIt(mContent);
+    while (attrIt.Next()) {
+      if (!ariaAttrs) {
+        ariaAttrs = new AccAttributes();
+      }
+      attrIt.ExposeAttr(ariaAttrs);
+    }
+    if (ariaAttrs) {
+      fields->SetAttribute(nsGkAtoms::aria, std::move(ariaAttrs));
+    } else if (aUpdateType == CacheUpdateType::Update) {
+      fields->SetAttribute(nsGkAtoms::aria, DeleteEntry());
     }
   }
 
