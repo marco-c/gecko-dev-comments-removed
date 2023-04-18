56e1ecb208b219a34e960eb6addb3d526f85ee72
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 09:47:40 +0000

    Bug 1756252: Collect telemetry on synced tabs usage r=markh
    Differential Revision: https://phabricator.services.mozilla.com/D139221
    
    UltraBlame original commit: b557ab9ead864b08ec185a2e0e6a82be8fc5afb8

--- browser/base/content/browser-sync.js
+++ browser/base/content/browser-sync.js
@@ -255,8 +255,8 @@ this.SyncedTabsPanelList = class SyncedTabsPanelList {
       if (hasNextPage) {
         client.tabs = client.tabs.slice(0, maxTabs);
       }
-      for (let tab of client.tabs) {
-        let tabEnt = this._createSyncedTabElement(tab);
+      for (let [index, tab] of client.tabs.entries()) {
+        let tabEnt = this._createSyncedTabElement(tab, index);
         container.appendChild(tabEnt);
       }
       if (hasNextPage) {
@@ -266,7 +266,7 @@ this.SyncedTabsPanelList = class SyncedTabsPanelList {
     }
   }
 
-  _createSyncedTabElement(tabInfo) {
+  _createSyncedTabElement(tabInfo, index) {
     let item = document.createXULElement("toolbarbutton");
     let tooltipText = (tabInfo.title ? tabInfo.title + "\n" : "") + tabInfo.url;
     item.setAttribute("itemtype", "tab");
@@ -283,6 +283,15 @@ this.SyncedTabsPanelList = class SyncedTabsPanelList {
     
     
     item.addEventListener("click", e => {
+      
+      let object = "fxa_avatar_menu";
+      const appMenuPanel = document.getElementById("appMenu-popup");
+      if (appMenuPanel.contains(e.currentTarget)) {
+        object = "fxa_app_menu";
+      }
+      SyncedTabs.recordSyncedTabsTelemetry(object, "click", {
+        tab_pos: index.toString(),
+      });
       document.defaultView.openUILink(tabInfo.url, e, {
         triggeringPrincipal: Services.scriptSecurityManager.createNullPrincipal(
           {}
