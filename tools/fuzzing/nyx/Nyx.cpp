02ba541b2c8128143d3e176c351cdd07a44c5a26
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 18:44:41 +0000

    Bug 1765543 - Add snapshot fuzzing build using Nyx r=decoder,taskgraph-reviewers,ahal
    Depends on D144208
    
    Differential Revision: https://phabricator.services.mozilla.com/D144209
    
    UltraBlame original commit: 72ce45391bea3a148c19f815f880550469d9c6a2

--- tools/fuzzing/nyx/Nyx.cpp
+++ tools/fuzzing/nyx/Nyx.cpp
@@ -5,6 +5,7 @@
 
 
 #include "mozilla/Assertions.h"
+#include "mozilla/Unused.h"
 #include "mozilla/Vector.h"
 #include "mozilla/fuzzing/Nyx.h"
 #include "prinrval.h"
@@ -97,7 +98,7 @@ void Nyx::start(void) {
 
       auto buffer = new Vector<uint8_t>();
 
-      buffer->initLengthUninitialized(pktsize);
+      mozilla::Unused << buffer->initLengthUninitialized(pktsize);
       is.read(reinterpret_cast<char*>(buffer->begin()), buffer->length());
 
       MOZ_FUZZING_NYX_PRINTF("[Replay Mode] Read data packet of size %zu\n",
@@ -174,8 +175,6 @@ void Nyx::release(uint32_t iterations) {
     
     PR_Sleep(PR_MillisecondsToInterval(5000));
     _exit(1);
-
-    return;
   }
 
   nyx_release(iterations);
