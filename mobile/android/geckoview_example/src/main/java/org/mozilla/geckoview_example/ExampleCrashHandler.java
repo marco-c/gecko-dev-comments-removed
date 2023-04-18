76f3e00cf0570698b75102622eb9906907942fa2
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 21:35:50 +0000

    Backed out changeset 2ef92e77b85b (bug 1759560) for causing Android xpcshell timeouts.
    UltraBlame original commit: 27543b11dc5360b09ccc04b5e754033aad3a81c8

--- mobile/android/geckoview_example/src/main/java/org/mozilla/geckoview_example/ExampleCrashHandler.java
+++ mobile/android/geckoview_example/src/main/java/org/mozilla/geckoview_example/ExampleCrashHandler.java
@@ -75,7 +75,7 @@ public class ExampleCrashHandler extends Service {
       startForeground(NOTIFY_ID, notification);
     } else if (ACTION_REPORT_CRASH.equals(intent.getAction())) {
       StrictMode.ThreadPolicy oldPolicy = null;
-      if (BuildConfig.DEBUG_BUILD) {
+      if (BuildConfig.DEBUG) {
         oldPolicy = StrictMode.getThreadPolicy();
 
         
