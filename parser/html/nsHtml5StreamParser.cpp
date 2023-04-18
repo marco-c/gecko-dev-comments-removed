8c3750c9a857eac0a16e0d1d120c2c01d900ce5b
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 03:54:35 +0000

    Bug 1746412: Parser cleanup r=hsivonen
    Differential Revision: https://phabricator.services.mozilla.com/D134021
    
    UltraBlame original commit: b5cc80934aeca3d01357c80b45a5e242eebcefaa

--- parser/html/nsHtml5StreamParser.cpp
+++ parser/html/nsHtml5StreamParser.cpp
@@ -1555,14 +1555,14 @@ nsresult nsHtml5StreamParser::OnDataAvailable(nsIRequest* aRequest,
                                               uint64_t aSourceOffset,
                                               uint32_t aLength) {
   nsresult rv;
-  if (NS_FAILED(rv = mExecutor->IsBroken())) {
-    return rv;
-  }
 
   MOZ_ASSERT(mRequest == aRequest, "Got data on wrong stream.");
   uint32_t totalRead;
   
   if (MOZ_UNLIKELY(NS_IsMainThread())) {
+    if (NS_FAILED(rv = mExecutor->IsBroken())) {
+      return rv;
+    }
     Maybe<Buffer<uint8_t>> maybe = Buffer<uint8_t>::Alloc(aLength);
     if (maybe.isNothing()) {
       return mExecutor->MarkAsBroken(NS_ERROR_OUT_OF_MEMORY);
@@ -1581,9 +1581,13 @@ nsresult nsHtml5StreamParser::OnDataAvailable(nsIRequest* aRequest,
     }
     return rv;
   }
+
   MOZ_ASSERT(IsParserThread(), "Wrong thread!");
   mozilla::MutexAutoLock autoLock(mTokenizerMutex);
 
+  if (NS_FAILED(rv = mTreeBuilder->IsBroken())) {
+    return rv;
+  }
   if (mBufferingBytes) {
     Maybe<Buffer<uint8_t>> maybe = Buffer<uint8_t>::Alloc(aLength);
     if (maybe.isNothing()) {
@@ -1604,6 +1608,7 @@ nsresult nsHtml5StreamParser::OnDataAvailable(nsIRequest* aRequest,
   MOZ_ASSERT(totalRead == aLength);
   return rv;
 }
+
 
 
 nsresult nsHtml5StreamParser::CopySegmentsToParser(
