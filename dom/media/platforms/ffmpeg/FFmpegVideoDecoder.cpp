2a5979e9b4f313ac079975eb93f21427b3c43210
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 08:42:21 +0000

    Bug 1754274 - print the log eariler. r=media-playback-reviewers,bryce
    This is used for debugging bug 1752388.
    
    Differential Revision: https://phabricator.services.mozilla.com/D138360
    
    UltraBlame original commit: d5d10708a1677adb997d76eda89c255b2e381cea

--- dom/media/platforms/ffmpeg/FFmpegVideoDecoder.cpp
+++ dom/media/platforms/ffmpeg/FFmpegVideoDecoder.cpp
@@ -1028,11 +1028,11 @@ MediaResult FFmpegVideoDecoder<LIBAV_VER>::CreateImage(
     if (!texture) {
       NS_WARNING("Failed to get the texture client!");
     } else {
+      FFMPEG_LOGV("Create a video data from a shmem image=%p", wrapper.get());
       
       
       
       texture->Unlock();
-      FFMPEG_LOGV("Create a video data from a shmem image=%p", wrapper.get());
       v = VideoData::CreateFromImage(
           mInfo.mDisplay, aOffset, TimeUnit::FromMicroseconds(aPts),
           TimeUnit::FromMicroseconds(aDuration), image, !!mFrame->key_frame,
