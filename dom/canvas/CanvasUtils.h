17da261d7cc593e803e24f4e517b8b343e55dea3
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 07:47:11 +0000

    Bug 1746750 - Part 2. Add new WebIDL method variants to support OffscreenCanvasRenderingContext2D. r=lsalzman
    This patch is a non-functional change. It overloads some WebIDL methods
    of CanvasRenderingContext2D to account for the difference between the
    main thread and worker prototypes.
    
    Differential Revision: https://phabricator.services.mozilla.com/D135353
    
    UltraBlame original commit: 3e808ae032db7f6573215128cf14d3dabaabd849

--- dom/canvas/CanvasUtils.h
+++ dom/canvas/CanvasUtils.h
@@ -56,7 +56,7 @@ bool IsOffscreenCanvasEnabled(JSContext* aCx, JSObject* aObj);
 
 
 bool IsImageExtractionAllowed(dom::Document* aDocument, JSContext* aCx,
-                              nsIPrincipal& aPrincipal);
+                              Maybe<nsIPrincipal*> aPrincipal);
 
 
 
