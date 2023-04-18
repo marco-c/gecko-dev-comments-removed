cd3651b4773542f2715bd7ba6d6b903671ada7bc
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 20:39:11 +0000

    Bug 1754975 - Introduce a new error code for blocking a non-local connection, r=necko-reviewers,dragana
    Differential Revision: https://phabricator.services.mozilla.com/D146281
    
    UltraBlame original commit: 88ce6ce619da05f194dc4dd3156ff175e52aed5f

--- xpcom/base/ErrorList.py
+++ xpcom/base/ErrorList.py
@@ -340,6 +340,9 @@ with modules["NETWORK"]:
     errors["NS_ERROR_HTTPS_ONLY"] = FAILURE(86)
     
     errors["NS_ERROR_WEBSOCKET_CONNECTION_REFUSED"] = FAILURE(87)
+    
+    
+    errors["NS_ERROR_NON_LOCAL_CONNECTION_REFUSED"] = FAILURE(88)
 
     
     
