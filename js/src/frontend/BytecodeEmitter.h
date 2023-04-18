92f8139f3ce3002c7f9c56d38d98cd1081ccb9e1
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 15:53:56 +0000

    Bug 1282976 - Part 4: Ensure emitNewTarget() can only be called from valid parse nodes. r=jandem
    Only `super()` and `new.target` should be able to emit `JSOp::NewTarget`.
    
    Differential Revision: https://phabricator.services.mozilla.com/D142797
    
    UltraBlame original commit: 44d8fb5ba8d2e45fb79fe1c2c42d67d4f360b68e

--- js/src/frontend/BytecodeEmitter.h
+++ js/src/frontend/BytecodeEmitter.h
@@ -573,7 +573,12 @@ struct MOZ_STACK_CLASS BytecodeEmitter {
   [[nodiscard]] bool emitSetThis(BinaryNode* setThisNode);
   [[nodiscard]] bool emitCheckDerivedClassConstructorReturn();
 
+ private:
+  [[nodiscard]] bool emitNewTarget();
+
+ public:
   [[nodiscard]] bool emitNewTarget(NewTargetNode* pn);
+  [[nodiscard]] bool emitNewTarget(CallNode* pn);
 
   
   [[nodiscard]] bool emitJumpTargetOp(JSOp op, BytecodeOffset* off);
