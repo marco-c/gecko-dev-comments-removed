9b7fd288360b38c897d0d69c22376e03564932f4
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 21:53:01 +0000

    Bug 1723079 - added revision to Browser and updated tests.r=whimboo,webdriver-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D146691
    
    UltraBlame original commit: db34928f0ee73ba1263b71633a55a991d1b9ae64

--- remote/cdp/test/browser/browser_cdp.js
+++ remote/cdp/test/browser/browser_cdp.js
@@ -26,6 +26,11 @@ add_task(async function testCDP({ client }) {
     "Browser.getVersion().userAgent is correct"
   );
 
+  is(
+    version.revision,
+    Services.appinfo.sourceURL.split("/").pop(),
+    "Browser.getVersion().revision is correct"
+  );
   
   let result = await Log.enable();
   info("Log domain has been enabled");
