fad8518fb53406c1724712a6220b7c2c1f7d638d
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 09:34:29 +0000

    Bug 1756197. Avoid clipping if we're just going to fill the path. r=gfx-reviewers,nical
    This avoids having to rasterize the path twice. Once for the clip path and once for
    the fill. It will also give us correct antialiasing on the edges.
    
    Differential Revision: https://phabricator.services.mozilla.com/D139137
    
    UltraBlame original commit: 63f54f50c32dcbfa01321485c0cafad0c5bbfbba

--- layout/svg/SVGIntegrationUtils.cpp
+++ layout/svg/SVGIntegrationUtils.cpp
@@ -758,16 +758,19 @@ bool SVGIntegrationUtils::PaintMask(const PaintFramesParams& aParams,
     MoveContextOriginToUserSpace(firstFrame, aParams);
 
     basicShapeSR.SetContext(&ctx);
-    CSSClipPathInstance::ApplyBasicShapeOrPathClip(
-        ctx, frame, SVGUtils::GetCSSPxToDevPxMatrix(frame));
+    gfxMatrix mat = SVGUtils::GetCSSPxToDevPxMatrix(frame);
     if (!maskUsage.shouldGenerateMaskLayer) {
       
       
       ctx.SetDeviceColor(DeviceColor::MaskOpaqueWhite());
+      RefPtr<Path> path = CSSClipPathInstance::CreateClipPathForFrame(
+          ctx.GetDrawTarget(), frame, mat);
+      ctx.SetPath(path);
       ctx.Fill();
 
       return true;
     }
+    CSSClipPathInstance::ApplyBasicShapeOrPathClip(ctx, frame, mat);
   }
 
   
