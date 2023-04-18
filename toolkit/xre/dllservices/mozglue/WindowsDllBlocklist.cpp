04411de97b4cd7e5312ac7c1344dae414df8e282
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 10:37:53 +0000

    Bug 1757487 - Enable the LoadLibrary injection protection unconditionally.  r=mhowell
    Since no compat issues have been reported since we landed it in v98 beta,
    it's time to remove the early-beta limitation.
    The patch backs out 8ca70e015f3b863f9df6049d7b7d97123dafa640.
    
    Differential Revision: https://phabricator.services.mozilla.com/D139972
    
    UltraBlame original commit: e76373d064bc3b0da5d50c78028989efb324e5ed

--- toolkit/xre/dllservices/mozglue/WindowsDllBlocklist.cpp
+++ toolkit/xre/dllservices/mozglue/WindowsDllBlocklist.cpp
@@ -531,11 +531,9 @@ continue_loading:
   return ret;
 }
 
-#if defined(EARLY_BETA_OR_EARLIER)
 
 
 static void* gStartAddressesToBlock[4];
-#endif  
 
 static bool ShouldBlockThread(void* aStartAddress) {
   
@@ -544,13 +542,11 @@ static bool ShouldBlockThread(void* aStartAddress) {
     return false;
   }
 
-#if defined(EARLY_BETA_OR_EARLIER)
   for (auto p : gStartAddressesToBlock) {
     if (p == aStartAddress) {
       return true;
     }
   }
-#endif
 
   bool shouldBlock = false;
   MEMORY_BASIC_INFORMATION startAddressInfo = {0};
@@ -618,7 +614,6 @@ MFBT_API void DllBlocklist_Initialize(uint32_t aInitFlags) {
     }
   }
 
-#if defined(EARLY_BETA_OR_EARLIER)
   
   HMODULE hKernel = GetModuleHandleW(L"kernel32.dll");
   if (hKernel) {
@@ -636,7 +631,6 @@ MFBT_API void DllBlocklist_Initialize(uint32_t aInitFlags) {
     pProc = (void*)GetProcAddress(hKernel, "LoadLibraryExW");
     gStartAddressesToBlock[3] = pProc;
   }
-#endif
 
   if (aInitFlags & eDllBlocklistInitFlagWasBootstrapped) {
     GetNativeNtBlockSetWriter();
