85054f4d744d3e0f504b1eac917959fd4aeaf3cf
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 22:46:44 +0000

    Bug 1770366 part 1 - Rename *SlotCount to *SlotsSize in a few places. r=iain
    It used to be that slots were pointer-sized, but this was changed in bug 950703.
    Rename "SlotCount" to "SlotsSize" to make clear that it's number of bytes.
    
    Differential Revision: https://phabricator.services.mozilla.com/D148641
    
    UltraBlame original commit: 22b9fb4f1c7db13a299c602695e6003cbac64d3f

--- js/src/jit/LIR.h
+++ js/src/jit/LIR.h
@@ -1835,7 +1835,7 @@ class LIRGraph {
   uint32_t numInstructions_;
 
   
-  uint32_t localSlotCount_;
+  uint32_t localSlotsSize_;
   
   uint32_t argumentSlotCount_;
 
@@ -1867,29 +1867,28 @@ class LIRGraph {
   }
   uint32_t getInstructionId() { return numInstructions_++; }
   uint32_t numInstructions() const { return numInstructions_; }
-  void setLocalSlotCount(uint32_t localSlotCount) {
-    localSlotCount_ = localSlotCount;
+  void setLocalSlotsSize(uint32_t localSlotsSize) {
+    localSlotsSize_ = localSlotsSize;
   }
-  uint32_t localSlotCount() const { return localSlotCount_; }
+  uint32_t localSlotsSize() const { return localSlotsSize_; }
   
   
   
-  uint32_t paddedLocalSlotCount() const {
+  uint32_t paddedLocalSlotsSize() const {
     
     
     
     
     
-    return AlignBytes(localSlotCount(), JitStackAlignment);
+    return AlignBytes(localSlotsSize(), JitStackAlignment);
   }
-  size_t paddedLocalSlotsSize() const { return paddedLocalSlotCount(); }
   void setArgumentSlotCount(uint32_t argumentSlotCount) {
     argumentSlotCount_ = argumentSlotCount;
   }
   uint32_t argumentSlotCount() const { return argumentSlotCount_; }
   size_t argumentsSize() const { return argumentSlotCount() * sizeof(Value); }
-  uint32_t totalSlotCount() const {
-    return paddedLocalSlotCount() + argumentsSize();
+  uint32_t totalSlotsSize() const {
+    return paddedLocalSlotsSize() + argumentsSize();
   }
   [[nodiscard]] bool addConstantToPool(const Value& v, uint32_t* index);
   size_t numConstants() const { return constantPool_.length(); }
