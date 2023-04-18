456b3019d6774d7be7178072c0afef4f91f21aaa
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 02:33:52 +0000

    Bug 1736550 - Adjusting focus for screen readers on about:welcome r=Jamie
    Differential Revision: https://phabricator.services.mozilla.com/D131413
    
    UltraBlame original commit: 7b3b1024a24fa9a85bb832ad936f88b297f37652

--- browser/components/newtab/data/content/activity-stream.bundle.js
+++ browser/components/newtab/data/content/activity-stream.bundle.js
@@ -3186,8 +3186,7 @@ class _DiscoveryStreamBase extends react__WEBPACK_IMPORTED_MODULE_12___default.a
           const prefix = `.ds-layout > .ds-column:nth-child(${rowIndex + 1}) .ds-column-grid > :nth-child(${componentIndex + 1})`; 
           
 
-          rule.selectorText = selectors.split(",").map(selector => prefix + ( 
-          selector[0] === ":" ? "" : " ") + selector).join(","); 
+          rule.selectorText = selectors.split(",").map(selector => prefix + (selector[0] === ":" ? "" : " ") + selector).join(","); 
 
           if (rule.selectorText === DUMMY_CSS_SELECTOR) {
             console.error(`Bad CSS selector ${selectors}`); 
@@ -5838,10 +5837,7 @@ class Section extends react__WEBPACK_IMPORTED_MODULE_8___default.a.PureComponent
     const wasCollapsed = prevProps.pref.collapsed;
 
     if ( 
-    props.rows.length && ( 
-    
-    props.rows !== prevProps.rows && !isCollapsed || 
-    wasCollapsed && !isCollapsed)) {
+    props.rows.length && (props.rows !== prevProps.rows && !isCollapsed || wasCollapsed && !isCollapsed)) {
       this.sendImpressionStatsOrAddListener();
     }
   }
