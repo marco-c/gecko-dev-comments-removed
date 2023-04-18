e727a0498f6196bc37021c40f5ef34ff6ecb1b82
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 03:34:30 +0000

    Backed out 9 changesets (bug 1744025) for causing wpt failures on CheckedUnsafePtr.h CLOSED TREE
    Backed out changeset 89dca4fc5940 (bug 1744025)
    Backed out changeset 7aa395dcdbe4 (bug 1744025)
    Backed out changeset 1580a4ea1a85 (bug 1744025)
    Backed out changeset af171636a87f (bug 1744025)
    Backed out changeset a5edfa1c9cd6 (bug 1744025)
    Backed out changeset 8abd6ba69815 (bug 1744025)
    Backed out changeset cfb822df5b3f (bug 1744025)
    Backed out changeset 5598943a94fd (bug 1744025)
    Backed out changeset 43186fbbf8b4 (bug 1744025)
    
    UltraBlame original commit: ddf7a6c0a118b33abaab02b44522612d02a61b48

--- dom/workers/RuntimeService.cpp
+++ dom/workers/RuntimeService.cpp
@@ -76,6 +76,7 @@
 #include "WorkerDebuggerManager.h"
 #include "WorkerError.h"
 #include "WorkerLoadInfo.h"
+#include "WorkerPrivate.h"
 #include "WorkerRunnable.h"
 #include "WorkerScope.h"
 #include "WorkerThread.h"
@@ -1184,7 +1185,7 @@ bool RuntimeService::RegisterWorker(WorkerPrivate& aWorkerPrivate) {
 
   
   if (parent) {
-    if (!parent->AddChildWorker(aWorkerPrivate)) {
+    if (!parent->AddChildWorker(&aWorkerPrivate)) {
       UnregisterWorker(aWorkerPrivate);
       return false;
     }
@@ -1304,7 +1305,7 @@ void RuntimeService::UnregisterWorker(WorkerPrivate& aWorkerPrivate) {
   
 
   if (parent) {
-    parent->RemoveChildWorker(aWorkerPrivate);
+    parent->RemoveChildWorker(&aWorkerPrivate);
   } else if (aWorkerPrivate.IsSharedWorker()) {
     AssertIsOnMainThread();
 
