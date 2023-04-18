84281a5e40ad9b7b6adde790fb3e2a85eb0a4cef
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 08:50:37 +0000

    Bug 1730085 part 2: Implement support for BOUNDARY_LINE_END in TextLeafPoint and HyperTextAccessibleBase. r=eeejay
    BOUNDARY_LINE_END is implemented using BOUNDARY_LINE_START and adjusting for line feed characters, which are line end boundaries where present.
    
    Differential Revision: https://phabricator.services.mozilla.com/D138103
    
    UltraBlame original commit: 6ae088ec6a5b01c494e3d906547988507d5059e7

--- accessible/basetypes/HyperTextAccessibleBase.h
+++ accessible/basetypes/HyperTextAccessibleBase.h
@@ -178,6 +178,16 @@ class HyperTextAccessibleBase {
   std::pair<bool, int32_t> TransformOffset(Accessible* aDescendant,
                                            int32_t aOffset,
                                            bool aIsEndOffset) const;
+
+  
+
+
+
+
+
+
+  void AdjustOriginIfEndBoundary(TextLeafPoint& aOrigin,
+                                 AccessibleTextBoundary aBoundaryType) const;
 };
 
 }  
