9ff2cb108d04c7ac479e34ea68d61a3bdd0cd14f
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 08:57:01 +0000

    Backed out changeset eaeaf7286193 (bug 1748643) for causing mochitest failures on test_midi_permission_gated.html. CLOSED TREE
    UltraBlame original commit: 133f20b41c6571ab6cf23b8a86474ade931e9808

--- dom/midi/midirMIDIPlatformService.cpp
+++ dom/midi/midirMIDIPlatformService.cpp
@@ -35,10 +35,6 @@ class SendRunnable : public MIDIBackgroundRunnable {
   ~SendRunnable() = default;
   virtual void RunInternal() {
     AssertIsOnBackgroundThread();
-    if (!MIDIPlatformService::IsRunning()) {
-      
-      return;
-    }
     midirMIDIPlatformService* srv =
         static_cast<midirMIDIPlatformService*>(MIDIPlatformService::Get());
     srv->SendMessage(mPortID, mMessage);
