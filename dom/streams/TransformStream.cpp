8d640dfea8bbd66de03bc48b4f096079e7e366c7
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 09:20:03 +0000

    Bug 1734886 - Part 1: Throw when readably/writableType fields exist r=mgaudet
    Differential Revision: https://phabricator.services.mozilla.com/D138825
    
    UltraBlame original commit: 9d43d0e3ba23edf26ecbb482739108f3435e75ab

--- dom/streams/TransformStream.cpp
+++ dom/streams/TransformStream.cpp
@@ -40,8 +40,84 @@ already_AddRefed<TransformStream> TransformStream::Constructor(
     const Optional<JS::Handle<JSObject*>>& aTransformer,
     const QueuingStrategy& aWritableStrategy,
     const QueuingStrategy& aReadableStrategy, ErrorResult& aRv) {
+  
+  JS::Rooted<JSObject*> transformerObj(
+      aGlobal.Context(),
+      aTransformer.WasPassed() ? aTransformer.Value() : nullptr);
+
+  
+  
+  Transformer transformerDict;
+  if (transformerObj) {
+    JS::Rooted<JS::Value> objValue(aGlobal.Context(),
+                                   JS::ObjectValue(*transformerObj));
+    dom::BindingCallContext callCx(aGlobal.Context(),
+                                   "TransformStream.constructor");
+    aRv.MightThrowJSException();
+    if (!transformerDict.Init(callCx, objValue)) {
+      aRv.StealExceptionFromJSContext(aGlobal.Context());
+      return nullptr;
+    }
+  }
+
+  
+  
+  if (!transformerDict.mReadableType.isUndefined()) {
+    aRv.ThrowRangeError(
+        "`readableType` is unsupported and preserved for future use");
+    return nullptr;
+  }
+
+  
+  
+  if (!transformerDict.mWritableType.isUndefined()) {
+    aRv.ThrowRangeError(
+        "`writableType` is unsupported and preserved for future use");
+    return nullptr;
+  }
+
+  
+  
+  
+
+  
+  
+  
+
+  
+  
+  
+
+  
+  
+  
+
+  
   nsCOMPtr<nsIGlobalObject> global = do_QueryInterface(aGlobal.GetAsSupports());
+  RefPtr<Promise> startPromise = Promise::Create(global, aRv);
+  if (aRv.Failed()) {
+    return nullptr;
+  }
+
+  
+  
+  
   RefPtr<TransformStream> transformStream = new TransformStream(global);
+  
+  if (aRv.Failed()) {
+    return nullptr;
+  }
+
+  
+  
+  
+  
+
+  
+  
+  
+  
+
   return transformStream.forget();
 }
 
