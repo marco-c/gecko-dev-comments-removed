c6a47c467435432f2eeba05a6684399f59e18342
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 22:09:33 +0000

    Bug 1771609 - patch 2 - Cache the block's nsLineIterator in a property on nsBlockFrame, for better performance on large pages. r=emilio
    Differential Revision: https://phabricator.services.mozilla.com/D147578
    
    UltraBlame original commit: 644f1fae216e4a5c655323b0688c9f5001815387

--- layout/generic/nsLineBox.h
+++ layout/generic/nsLineBox.h
@@ -1641,9 +1641,8 @@ class nsLineIterator final : public nsILineIterator {
       mIndex = 0;
     }
   }
-  ~nsLineIterator() { MOZ_DIAGNOSTIC_ASSERT(!mMutationGuard.Mutated(0)); };
 
-  void DisposeLineIterator() final { delete this; }
+  void DisposeLineIterator() final {}
 
   int32_t GetNumLines() const final {
     if (mNumLines < 0) {
@@ -1675,13 +1674,32 @@ class nsLineIterator final : public nsILineIterator {
     MOZ_ASSERT(mIter != mLines.end(), "Already at end!");
     ++mIndex;
     ++mIter;
-    return mIter == mLines.end() ? nullptr : mIter.get();
+    if (mIter == mLines.end()) {
+      MOZ_ASSERT(mNumLines < 0 || mNumLines == mIndex);
+      mNumLines = mIndex;
+      return nullptr;
+    }
+    return mIter.get();
   }
 
   
   const nsLineBox* GetLineAt(int32_t aIndex) {
+    MOZ_ASSERT(mIndex >= 0);
     if (aIndex < 0 || (mNumLines >= 0 && aIndex >= mNumLines)) {
       return nullptr;
+    }
+    
+    
+    
+    if (aIndex < mIndex / 2) {
+      
+      mIter = mLines.begin();
+      mIndex = 0;
+    } else if (mNumLines > 0 && aIndex > (mNumLines + mIndex) / 2) {
+      
+      mIter = mLines.end();
+      --mIter;
+      mIndex = mNumLines - 1;
     }
     while (mIndex > aIndex) {
       
@@ -1694,6 +1712,8 @@ class nsLineIterator final : public nsILineIterator {
       
       
       if (mIter == mLines.end()) {
+        MOZ_ASSERT(mNumLines < 0 || mNumLines == mIndex);
+        mNumLines = mIndex;
         return nullptr;
       }
       ++mIter;
@@ -1707,9 +1727,6 @@ class nsLineIterator final : public nsILineIterator {
   int32_t mIndex = -1;
   mutable int32_t mNumLines = -1;
   const bool mRightToLeft;
-#ifdef MOZ_DIAGNOSTIC_ASSERT_ENABLED
-  nsMutationGuard mMutationGuard;
-#endif
 };
 
 #endif 
