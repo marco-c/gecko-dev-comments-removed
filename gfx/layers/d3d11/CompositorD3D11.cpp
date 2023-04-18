ac5644531e3e37885d4c509d98e43b9e1f977beb
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 02:38:37 +0000

    Bug 1744666 - Remove CompositorD3D11::mUseForSoftwareWebRender r=gfx-reviewers,lsalzman
    CompositorD3D11 is used only for software WebRender in gecko. We could remove CompositorD3D11::mUseForSoftwareWebRender.
    
    Differential Revision: https://phabricator.services.mozilla.com/D133039
    
    UltraBlame original commit: 24c81c9d42d1144518a7a40882360ae5f2d8c15e

--- gfx/layers/d3d11/CompositorD3D11.cpp
+++ gfx/layers/d3d11/CompositorD3D11.cpp
@@ -108,8 +108,7 @@ CompositorD3D11::CompositorD3D11(widget::CompositorWidget* aWidget)
       mAllowPartialPresents(false),
       mIsDoubleBuffered(false),
       mVerifyBuffersFailed(false),
-      mUseMutexOnPresent(false),
-      mUseForSoftwareWebRender(false) {
+      mUseMutexOnPresent(false) {
   mUseMutexOnPresent = StaticPrefs::gfx_use_mutex_on_present_AtStartup();
 }
 
@@ -822,15 +821,6 @@ Maybe<IntRect> CompositorD3D11::BeginFrame(const nsIntRegion& aInvalidRegion,
   if (mDevice->GetDeviceRemovedReason() != S_OK) {
     if (!mAttachments->IsDeviceReset()) {
       gfxCriticalNote << "GFX: D3D11 skip BeginFrame with device-removed.";
-
-      
-      
-      
-      
-      
-      if (XRE_IsGPUProcess() && !mUseForSoftwareWebRender) {
-        GPUParent::GetSingleton()->NotifyDeviceReset();
-      }
       mAttachments->SetDeviceReset();
     }
     return Nothing();
