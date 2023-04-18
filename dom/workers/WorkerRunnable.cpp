cfe9612fb39d16d28404b3aec9243f97c003a1e8
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 19:40:28 +0000

    Bug 1767214: Avoid executing WorkerMainThreadRunnable::Run late in shutdown. r=dom-worker-reviewers,jesup
    Differential Revision: https://phabricator.services.mozilla.com/D145577
    
    UltraBlame original commit: 4a6f7ba7402fe230c32dbdf20ec671b2564109ef

--- dom/workers/WorkerRunnable.cpp
+++ dom/workers/WorkerRunnable.cpp
@@ -11,6 +11,7 @@
 #include "jsapi.h"
 #include "jsfriendapi.h"
 #include "mozilla/AlreadyAddRefed.h"
+#include "mozilla/AppShutdown.h"
 #include "mozilla/Assertions.h"
 #include "mozilla/DebugOnly.h"
 #include "mozilla/ErrorResult.h"
@@ -583,6 +584,12 @@ void WorkerMainThreadRunnable::Dispatch(WorkerStatus aFailStatus,
 NS_IMETHODIMP
 WorkerMainThreadRunnable::Run() {
   AssertIsOnMainThread();
+
+  
+  
+  if (AppShutdown::IsInOrBeyond(ShutdownPhase::XPCOMShutdownThreads)) {
+    return NS_ERROR_ILLEGAL_DURING_SHUTDOWN;
+  }
 
   bool runResult = MainThreadRun();
 
