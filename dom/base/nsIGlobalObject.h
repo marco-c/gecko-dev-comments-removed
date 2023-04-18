c5c5644899ad00741346b2190ddda38afe6e0047
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 11:54:28 +0000

    Bug 1759163 - Add origin trial support for worklets. r=asuth
    Move the trial getter to nsIGlobalObject, make every implementation
    provide a reasonable value, and add a test.
    
    Differential Revision: https://phabricator.services.mozilla.com/D140848
    
    UltraBlame original commit: 558e256fcb315a4954f6f364696213ddae2733de

--- dom/base/nsIGlobalObject.h
+++ dom/base/nsIGlobalObject.h
@@ -12,6 +12,7 @@
 #include "mozilla/dom/ClientInfo.h"
 #include "mozilla/dom/DispatcherTrait.h"
 #include "mozilla/dom/ServiceWorkerDescriptor.h"
+#include "mozilla/OriginTrials.h"
 #include "nsHashKeys.h"
 #include "nsISupports.h"
 #include "nsStringFwd.h"
@@ -195,6 +196,9 @@ class nsIGlobalObject : public nsISupports,
 
 
   virtual mozilla::StorageAccess GetStorageAccess();
+
+  
+  virtual mozilla::OriginTrials Trials() const = 0;
 
   
   
