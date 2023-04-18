7c1893b4c1e0acae623062aa1d7ce6d06fe9934f
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 00:32:19 +0000

    Bug 1713030 - [puppeteer] Support for custom user data (profile) directory for Firefox. r=webdriver-reviewers,jdescottes
    When using a custom Firefox profile for Puppeteer the modified
    preferences as present in prefs.js need to be reset once the
    profile is no longer needed by Puppeteer. If not done this could
    cause side-effects when the profile is used next time outside
    of Puppeteer.
    
    Differential Revision: https://phabricator.services.mozilla.com/D128452
    
    UltraBlame original commit: 31302975681c8978f26438b9ce3454875a264aeb

--- remote/test/puppeteer/src/node/BrowserRunner.ts
+++ remote/test/puppeteer/src/node/BrowserRunner.ts
@@ -14,23 +14,29 @@
 
 
 
-import { debug } from '../common/Debug.js';
-
-import removeFolder from 'rimraf';
 import * as childProcess from 'child_process';
+import * as fs from 'fs';
+import * as path from 'path';
+import * as readline from 'readline';
+import removeFolder from 'rimraf';
+import { promisify } from 'util';
+
 import { assert } from '../common/assert.js';
+import { debug } from '../common/Debug.js';
 import { helper, debugError } from '../common/helper.js';
 import { LaunchOptions } from './LaunchOptions.js';
 import { Connection } from '../common/Connection.js';
 import { NodeWebSocketTransport as WebSocketTransport } from '../node/NodeWebSocketTransport.js';
 import { PipeTransport } from './PipeTransport.js';
 import { Product } from '../common/Product.js';
-import * as readline from 'readline';
 import { TimeoutError } from '../common/Errors.js';
-import { promisify } from 'util';
 
 const removeFolderAsync = promisify(removeFolder);
+const renameAsync = promisify(fs.rename);
+const unlinkAsync = promisify(fs.unlink);
+
 const debugLauncher = debug('puppeteer:launcher');
+
 const PROCESS_ERROR_EXPLANATION = `Puppeteer was unable to kill the process which ran the browser binary.
 This means that, on future Puppeteer launches, Puppeteer might not be able to launch the browser.
 Please check your open processes and ensure that the browser processes that Puppeteer launched have been killed.
@@ -40,7 +46,8 @@ export class BrowserRunner {
   private _product: Product;
   private _executablePath: string;
   private _processArguments: string[];
-  private _tempDirectory?: string;
+  private _userDataDir: string;
+  private _isTempUserDataDir?: boolean;
 
   proc = null;
   connection = null;
@@ -53,12 +60,14 @@ export class BrowserRunner {
     product: Product,
     executablePath: string,
     processArguments: string[],
-    tempDirectory?: string
+    userDataDir: string,
+    isTempUserDataDir?: boolean
   ) {
     this._product = product;
     this._executablePath = executablePath;
     this._processArguments = processArguments;
-    this._tempDirectory = tempDirectory;
+    this._userDataDir = userDataDir;
+    this._isTempUserDataDir = isTempUserDataDir;
   }
 
   start(options: LaunchOptions): void {
@@ -94,15 +103,40 @@ export class BrowserRunner {
       this.proc.stdout.pipe(process.stdout);
     }
     this._closed = false;
-    this._processClosing = new Promise((fulfill) => {
-      this.proc.once('exit', () => {
+    this._processClosing = new Promise((fulfill, reject) => {
+      this.proc.once('exit', async () => {
         this._closed = true;
         
-        if (this._tempDirectory) {
-          removeFolderAsync(this._tempDirectory)
-            .then(() => fulfill())
-            .catch((error) => console.error(error));
+        if (this._isTempUserDataDir) {
+          try {
+            await removeFolderAsync(this._userDataDir);
+            fulfill();
+          } catch (error) {
+            console.error(error);
+            reject(error);
+          }
         } else {
+          if (this._product === 'firefox') {
+            try {
+              
+              
+              await unlinkAsync(path.join(this._userDataDir, 'user.js'));
+
+              const prefsBackupPath = path.join(
+                this._userDataDir,
+                'prefs.js.puppeteer'
+              );
+              if (fs.existsSync(prefsBackupPath)) {
+                const prefsPath = path.join(this._userDataDir, 'prefs.js');
+                await unlinkAsync(prefsPath);
+                await renameAsync(prefsBackupPath, prefsPath);
+              }
+            } catch (error) {
+              console.error(error);
+              reject(error);
+            }
+          }
+
           fulfill();
         }
       });
@@ -129,7 +163,7 @@ export class BrowserRunner {
 
   close(): Promise<void> {
     if (this._closed) return Promise.resolve();
-    if (this._tempDirectory && this._product !== 'firefox') {
+    if (this._isTempUserDataDir && this._product !== 'firefox') {
       this.kill();
     } else if (this.connection) {
       
@@ -147,7 +181,9 @@ export class BrowserRunner {
   kill(): void {
     
     try {
-      removeFolder.sync(this._tempDirectory);
+      if (this._isTempUserDataDir) {
+        removeFolder.sync(this._userDataDir);
+      }
     } catch (error) {}
 
     
