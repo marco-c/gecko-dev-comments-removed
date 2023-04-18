4c891987f9deba0db77db04e81461ea9f556f8ec
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 19:05:00 +0000

    Bug 1767966 - Part 6: Handle boxed values in single-test blocks. r=jandem
    Handle the cases from parts 4-5 when the inputs can be boxed.
    
    Differential Revision: https://phabricator.services.mozilla.com/D145593
    
    UltraBlame original commit: b3b4b19b2fece9ff05e5b09ce4f84692225849d5

--- js/src/jit/IonAnalysis.cpp
+++ js/src/jit/IonAnalysis.cpp
@@ -614,6 +614,22 @@ static bool BlockIsSingleTest(MBasicBlock* phiBlock, MBasicBlock* testBlock,
 
       testOrNot = notIns;
       hasOddNumberOfNots = !hasOddNumberOfNots;
+    } else if (iter->isUnbox()) {
+      
+      auto* unbox = iter->toUnbox();
+      if (testOrNot->getOperand(0) != unbox) {
+        return false;
+      }
+      if (!unbox->hasOneUse()) {
+        return false;
+      }
+
+      
+      if (unbox != *testBlock->begin()) {
+        return false;
+      }
+
+      testOrNot = unbox;
     } else {
       
       return false;
@@ -625,7 +641,7 @@ static bool BlockIsSingleTest(MBasicBlock* phiBlock, MBasicBlock* testBlock,
     return false;
   }
 
-  MOZ_ASSERT(testOrNot->isTest() || testOrNot->isNot());
+  MOZ_ASSERT(testOrNot->isTest() || testOrNot->isNot() || testOrNot->isUnbox());
 
   MDefinition* testInput = testOrNot->getOperand(0);
   if (!testInput->isPhi()) {
@@ -684,6 +700,12 @@ static bool IsTestInputMaybeToBool(MTest* test, MDefinition* value) {
       continue;
     }
 
+    
+    if (input->isUnbox()) {
+      input = input->toUnbox()->input();
+      continue;
+    }
+
     return false;
   }
 }
@@ -713,6 +735,8 @@ static bool IsTestInputMaybeToBool(MTest* test, MDefinition* value) {
 [[nodiscard]] static bool UpdateTestSuccessors(
     TempAllocator& alloc, MBasicBlock* block, MDefinition* value,
     MBasicBlock* ifTrue, MBasicBlock* ifFalse, MBasicBlock* existingPred) {
+  MOZ_ASSERT(existingPred->lastIns()->isTest());
+
   MInstruction* ins = block->lastIns();
   if (ins->isTest()) {
     MTest* test = ins->toTest();
@@ -742,6 +766,19 @@ static bool IsTestInputMaybeToBool(MTest* test, MDefinition* value) {
   MOZ_ASSERT(ins->isGoto());
   ins->toGoto()->target()->removePredecessor(block);
   block->discardLastIns();
+
+  
+  
+  
+  if (existingPred->begin()->isUnbox() && value->type() == MIRType::Value) {
+    auto* existingUnbox = existingPred->begin()->toUnbox();
+    auto* unbox =
+        MUnbox::New(alloc, value, existingUnbox->type(), existingUnbox->mode());
+    unbox->setBailoutKind(existingUnbox->bailoutKind());
+    block->add(unbox);
+
+    value = unbox;
+  }
 
   MTest* test = MTest::New(alloc, value, ifTrue, ifFalse);
   block->end(test);
