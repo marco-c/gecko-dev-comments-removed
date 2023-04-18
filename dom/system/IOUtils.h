e09ce4eaea193814d01daae137cbbf8f59eb9c07
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 01:39:02 +0000

    Bug 1741548 - Add ignoreAbsent option to `IOUtils.getChildren` method. r=barret
    Remove an incorrect comment.
    
    Differential Revision: https://phabricator.services.mozilla.com/D131323
    
    UltraBlame original commit: 95498b2c7460860dac370d4b5e97d145684ba8a5

--- dom/system/IOUtils.h
+++ dom/system/IOUtils.h
@@ -112,8 +112,9 @@ class IOUtils final {
       GlobalObject& aGlobal, const nsAString& aPath,
       const Optional<int64_t>& aModification);
 
-  static already_AddRefed<Promise> GetChildren(GlobalObject& aGlobal,
-                                               const nsAString& aPath);
+  static already_AddRefed<Promise> GetChildren(
+      GlobalObject& aGlobal, const nsAString& aPath,
+      const GetChildrenOptions& aOptions);
 
   static already_AddRefed<Promise> SetPermissions(GlobalObject& aGlobal,
                                                   const nsAString& aPath,
@@ -339,7 +340,8 @@ class IOUtils final {
 
 
 
-  static Result<nsTArray<nsString>, IOError> GetChildrenSync(nsIFile* aFile);
+  static Result<nsTArray<nsString>, IOError> GetChildrenSync(
+      nsIFile* aFile, bool aIgnoreAbsent);
 
   
 
