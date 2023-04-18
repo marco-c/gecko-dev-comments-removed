ea9bff3cae30c75f0553d3a43ca16d9eeeb3e0fa
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 03:41:49 +0000

    Bug 1734867 - Use single static buffer when capturing stacks for main-thread markers - r=florian
    The main thread is the busiest, so it benefits the most from having its own chunked buffer. This removes one allocation when capturing a marker stack on the main thread of each process.
    That buffer is allocated when the first profiler starts, and is destroyed when the last profiler stops.
    
    Note: Further improvements are possible (e.g.: Pool of pre-allocated buffers, attempting to use a stack-based buffer, etc.), but they are more complex and will require more work in future bugs.
    
    Differential Revision: https://phabricator.services.mozilla.com/D133725
    
    UltraBlame original commit: bcddb167725ca3d31c07baf39eb1e918d1bbfc52

--- mozglue/baseprofiler/core/ProfilerMarkers.cpp
+++ mozglue/baseprofiler/core/ProfilerMarkers.cpp
@@ -5,7 +5,7 @@
 
 #include "mozilla/BaseProfilerMarkers.h"
 
-#include "mozilla/Likely.h"
+#include "mozilla/BaseProfilerUtils.h"
 
 #include <limits>
 
@@ -72,6 +72,51 @@ static Streaming::MarkerTypeFunctions
  Span<const Streaming::MarkerTypeFunctions>
 Streaming::MarkerTypeFunctionsArray() {
   return {sMarkerTypeFunctions1Based, sDeserializerCount};
+}
+
+
+
+
+static int sBufferForMainThreadAddMarkerRefCount = 0;
+static ProfileChunkedBuffer* sBufferForMainThreadAddMarker = nullptr;
+
+ProfileChunkedBuffer* GetClearedBufferForMainThreadAddMarker() {
+  if (!mozilla::baseprofiler::profiler_is_main_thread()) {
+    return nullptr;
+  }
+
+  if (sBufferForMainThreadAddMarker) {
+    sBufferForMainThreadAddMarker->Clear();
+  }
+
+  return sBufferForMainThreadAddMarker;
+}
+
+MFBT_API void EnsureBufferForMainThreadAddMarker() {
+  if (!mozilla::baseprofiler::profiler_is_main_thread()) {
+    return;
+  }
+
+  if (sBufferForMainThreadAddMarkerRefCount++ == 0) {
+    MOZ_ASSERT(!sBufferForMainThreadAddMarker);
+    sBufferForMainThreadAddMarker = new ProfileChunkedBuffer(
+        ProfileChunkedBuffer::ThreadSafety::WithoutMutex,
+        MakeUnique<ProfileBufferChunkManagerSingle>(
+            ProfileBufferChunkManager::scExpectedMaximumStackSize));
+  }
+}
+
+MFBT_API void ReleaseBufferForMainThreadAddMarker() {
+  if (!mozilla::baseprofiler::profiler_is_main_thread()) {
+    return;
+  }
+
+  MOZ_ASSERT(sBufferForMainThreadAddMarkerRefCount > 0);
+  MOZ_ASSERT(sBufferForMainThreadAddMarker);
+  if (--sBufferForMainThreadAddMarkerRefCount == 0) {
+    delete sBufferForMainThreadAddMarker;
+    sBufferForMainThreadAddMarker = nullptr;
+  }
 }
 
 }  
