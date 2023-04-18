dc7a9ee916872fb6b717888831e0a5ec92b47832
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 22:45:57 +0000

    Bug 1773491 - Partially back out bug 1773359 since Solaris also defines this constant. DONTBUILD
    MANUAL PUSH: Tier3 build fix / backout CLOSED TREE
    
    UltraBlame original commit: 74f223e774bb0bfeed2e4dd8422cc9cabb16b087

--- gfx/webrender_bindings/webrender_ffi.h
+++ gfx/webrender_bindings/webrender_ffi.h
@@ -95,7 +95,14 @@ void omta_sample(mozilla::wr::WrWindowId aWindowId,
 void omta_deregister_sampler(mozilla::wr::WrWindowId aWindowId);
 }  
 
+
+
+#pragma push_macro("TRANSPARENT")
+#undef TRANSPARENT
+
 #include "webrender_ffi_generated.h"
+
+#pragma pop_macro("TRANSPARENT")
 
 template struct mozilla::wr::Point2D<int32_t, mozilla::wr::DevicePixel>;
 template struct mozilla::wr::Point2D<int, mozilla::wr::WorldPixel>;
