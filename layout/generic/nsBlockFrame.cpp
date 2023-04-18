32226b58d9dab2f730f58d9a72a93834627c46e0
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 23:59:08 +0000

    Bug 847368 - Reset floating first letter's reflow status before checking break-inside:avoid. r=emilio
    As the comment in the moved hunk said, a floating first letter frame's
    incomplete status means that there is more content to be reflowed on the line.
    If the block containing the floating first letter has `break-inside:avoid`
    style, the old code would call `SetInlineLineBreakBeforeAndReset()`, and the
    block's parent frame would misinterpret the status as "this block needs to be
    push to the next fragment" even if we are *not* in a paginated environment.
    Hence the assertion "Shouldn't be incomplete if availableBSize is
    UNCONSTRAINED."
    
    To fix this bug, we can reset the float's reflow status for a first letter frame
    before checking `ShouldAvoidBreakInside`.
    
    Differential Revision: https://phabricator.services.mozilla.com/D149712
    
    UltraBlame original commit: 5c5e1891414c06272cd11e31751c0cebd6350c5a

--- layout/generic/nsBlockFrame.cpp
+++ layout/generic/nsBlockFrame.cpp
@@ -6711,6 +6711,15 @@ void nsBlockFrame::ReflowFloat(BlockReflowState& aState,
                     nullptr, floatRS, aReflowStatus, aState);
   } while (clearanceFrame);
 
+  if (aFloat->IsLetterFrame()) {
+    
+    
+    
+    if (aReflowStatus.IsIncomplete()) {
+      aReflowStatus.Reset();
+    }
+  }
+
   if (!aReflowStatus.IsFullyComplete() && ShouldAvoidBreakInside(floatRS)) {
     aReflowStatus.SetInlineLineBreakBeforeAndReset();
   } else if (aReflowStatus.IsIncomplete() &&
@@ -6722,15 +6731,6 @@ void nsBlockFrame::ReflowFloat(BlockReflowState& aState,
 
   if (aReflowStatus.NextInFlowNeedsReflow()) {
     aState.mReflowStatus.SetNextInFlowNeedsReflow();
-  }
-
-  if (aFloat->IsLetterFrame()) {
-    
-    
-    
-    if (aReflowStatus.IsIncomplete()) {
-      aReflowStatus.Reset();
-    }
   }
 
   
