9ba90e7110f30333a68018f17fa92ef0a8030754
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 15:30:40 +0000

    Bug 986973 - Remove nsConsoleService hack in ContentParent r=smaug
    D132270 removed the problematic line, so it's not needed anymore.
    
    Differential Revision: https://phabricator.services.mozilla.com/D142554
    
    UltraBlame original commit: 5cbfa9fc80d2c99bd75cc3e3ee71286c10052018

--- dom/ipc/ContentParent.cpp
+++ dom/ipc/ContentParent.cpp
@@ -354,14 +354,6 @@ extern mozilla::LazyLogModule gFocusLog;
 
 #define LOGFOCUS(args) MOZ_LOG(gFocusLog, mozilla::LogLevel::Debug, args)
 
-
-template <>
-struct nsIConsoleService::COMTypeInfo<nsConsoleService, void> {
-  static const nsIID kIID;
-};
-const nsIID nsIConsoleService::COMTypeInfo<nsConsoleService, void>::kIID =
-    NS_ICONSOLESERVICE_IID;
-
 namespace mozilla {
 namespace CubebUtils {
 extern FileDescriptor CreateAudioIPCConnection();
