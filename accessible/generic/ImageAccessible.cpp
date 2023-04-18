e6e14de3b0d9b36dd38a2f84014c2ec1ba0f2917
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 01:55:34 +0000

    Bug 1742595: Don't throw an assertion when calling ImageAccessible::NativeState on a XUL image. r=eeejay
    XUL images use nsImageBoxFrame, not nsImageFrame.
    AccessibleType() returns eNoType on nsImageBoxFrame.
    Changing this to return eImageType seems a bit risky and adding a new type just for an assertion seems pointless.
    Therefore, we just check the frame type.
    
    Differential Revision: https://phabricator.services.mozilla.com/D132051
    
    UltraBlame original commit: c8c0fe99587cae8ef0310ab5ca5a672fa43001dd

--- accessible/generic/ImageAccessible.cpp
+++ accessible/generic/ImageAccessible.cpp
@@ -75,7 +75,8 @@ uint64_t ImageAccessible::NativeState() const {
   if (!(mImageRequestStatus & imgIRequest::STATUS_SIZE_AVAILABLE)) {
     nsIFrame* frame = GetFrame();
     MOZ_ASSERT(!frame || frame->AccessibleType() == eImageType ||
-               frame->AccessibleType() == a11y::eHTMLImageMapType);
+               frame->AccessibleType() == a11y::eHTMLImageMapType ||
+               frame->IsImageBoxFrame());
     if (frame && !(frame->GetStateBits() & IMAGE_SIZECONSTRAINED)) {
       
       
