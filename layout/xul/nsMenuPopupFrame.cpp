49d5f85d319311757bd8ffb953447220ec7e7d12
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 05:15:33 +0000

    Backed out changeset e7b991987ae5 (bug 1742797) for causing bug 1748808 CLOSED TREE
    UltraBlame original commit: 09808a64bedd5aa00771ee1c8b73cf11d42a5077

--- layout/xul/nsMenuPopupFrame.cpp
+++ layout/xul/nsMenuPopupFrame.cpp
@@ -1066,12 +1066,6 @@ void nsMenuPopupFrame::HidePopup(bool aDeselectMenu, nsPopupState aNewState) {
   mCurrentMenu = nullptr;  
   mHFlip = mVFlip = false;
 
-  
-  
-  if (auto* widget = GetWidget()) {
-    widget->ClearCachedWebrenderResources();
-  }
-
   nsView* view = GetView();
   nsViewManager* viewManager = view->GetViewManager();
   viewManager->SetViewVisibility(view, nsViewVisibility_kHide);
