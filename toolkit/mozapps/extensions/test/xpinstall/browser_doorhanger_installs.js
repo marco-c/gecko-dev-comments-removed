df7c227c5e2e389715c7e7043663ba8edaf1b048
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 03:35:56 +0000

    Bug 1737165: bypass 3rd party panel for site permission addons r=rpl
    Differential Revision: https://phabricator.services.mozilla.com/D131318
    
    UltraBlame original commit: a9600d6acf27e4412fc27197de5631ac0e9e2ad6

--- toolkit/mozapps/extensions/test/xpinstall/browser_doorhanger_installs.js
+++ toolkit/mozapps/extensions/test/xpinstall/browser_doorhanger_installs.js
@@ -474,7 +474,7 @@ var TESTS = [
       ],
     });
 
-    let notificationPromise = waitForNotification("addon-install-blocked");
+    let notificationPromise = waitForNotification("addon-webext-permissions");
     let triggers = encodeURIComponent(
       JSON.stringify({
         XPI: TESTROOT + "webmidi_permission.xpi",
@@ -485,29 +485,7 @@ var TESTS = [
       TESTROOT + "installtrigger.html?" + triggers
     );
     let panel = await notificationPromise;
-
     let notification = panel.childNodes[0];
-    is(
-      notification.button.label,
-      "Continue to Installation",
-      "Should have seen the right button"
-    );
-    let message = panel.ownerDocument.getElementById(
-      "addon-install-blocked-message"
-    );
-    is(
-      message.textContent,
-      "You are attempting to install an add-on from example.com. Make sure you trust this site before continuing.",
-      "Should have seen the right message"
-    );
-
-    
-    notificationPromise = waitForNotification("addon-webext-permissions");
-    
-    notification.button.click();
-    panel = await notificationPromise;
-    notification = panel.childNodes[0];
-
     is(notification.button.label, "Add", "Should have seen the right button");
 
     is(
@@ -579,7 +557,7 @@ var TESTS = [
 
     let dialogPromise = waitForInstallDialog();
     
-    EventUtils.synthesizeMouse(notification.button, 20, 10, {});
+    notification.button.click();
 
     let installDialog = await dialogPromise;
 
