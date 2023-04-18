01028a3414be2abd39833f11a72697ad0476a891
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 18:20:09 +0000

    Bug 1767069 part 3: Modernize naming for some SizeComputationInput variables in float layout code. r=emilio
    This patch does not impact behavior.
    
    Before this patch, these variables' names are abbreviations of their original
    type-name, "nsCSSOffsetState".  We renamed that type to SizeComputationInput in
    bug 1277129, and this patch just updates the variable-names to reflect that
    renaming, so that now they're abbreviations of the new type-name, rather than
    the old one.
    
    This patch also takes this opportunity to move these variables declarations
    closer to their usage, and in one case we convert a variable to be a temporary
    anonymous value in the middle of another assignment.
    
    Differential Revision: https://phabricator.services.mozilla.com/D145137
    
    UltraBlame original commit: 604e8c73b573d2e43c5acb2e87a7b277296aafaa

--- layout/generic/nsBlockFrame.cpp
+++ layout/generic/nsBlockFrame.cpp
@@ -7702,9 +7702,6 @@ nsBlockFrame::FloatAvoidingISizeToClear nsBlockFrame::ISizeToClearPastFloats(
     nsIFrame* aFloatAvoidingBlock) {
   nscoord inlineStartOffset, inlineEndOffset;
   WritingMode wm = aState.mReflowInput.GetWritingMode();
-  SizeComputationInput offsetState(aFloatAvoidingBlock,
-                                   aState.mReflowInput.mRenderingContext, wm,
-                                   aState.mContentArea.ISize(wm));
 
   FloatAvoidingISizeToClear result;
   aState.ComputeFloatAvoidingOffsets(aFloatAvoidingBlock, aFloatAvailableSpace,
@@ -7725,9 +7722,13 @@ nsBlockFrame::FloatAvoidingISizeToClear nsBlockFrame::ISizeToClearPastFloats(
                           aFloatAvoidingBlock, availSpace);
   result.borderBoxISize =
       reflowInput.ComputedSizeWithBorderPadding(wm).ISize(wm);
+
   
   
-  LogicalMargin computedMargin = offsetState.ComputedLogicalMargin(wm);
+  SizeComputationInput sizingInput(aFloatAvoidingBlock,
+                                   aState.mReflowInput.mRenderingContext, wm,
+                                   aState.mContentArea.ISize(wm));
+  const LogicalMargin computedMargin = sizingInput.ComputedLogicalMargin(wm);
   result.marginIStart = computedMargin.IStart(wm);
   return result;
 }
