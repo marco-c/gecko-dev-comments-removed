7536c10987ab40c67a7b160e357a55818cb2fd64
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 02:49:33 +0000

    Backed out 2 changesets (bug 1735265) for causing reftest failures on 1735265-2-ref.html CLOSED TREE
    Backed out changeset 26205128ce34 (bug 1735265)
    Backed out changeset c897d241ad39 (bug 1735265)
    
    UltraBlame original commit: 47deea61c5ca3fa3bbfc00673b091cfd3c0ffd14

--- layout/tables/nsTableFrame.h
+++ layout/tables/nsTableFrame.h
@@ -95,12 +95,6 @@ class nsDisplayTableBackgroundSet {
 
   const nsRect& GetDirtyRect() { return mDirtyRect; }
 
-  const DisplayItemClipChain* GetTableClipChain() {
-    return mCombinedTableClipChain;
-  }
-
-  const ActiveScrolledRoot* GetTableASR() { return mTableASR; }
-
  private:
   
   
@@ -116,9 +110,6 @@ class nsDisplayTableBackgroundSet {
   nsTArray<nsTableColFrame*> mColumns;
   nsPoint mToReferenceFrame;
   nsRect mDirtyRect;
-
-  const DisplayItemClipChain* mCombinedTableClipChain;
-  const ActiveScrolledRoot* mTableASR;
 };
 
 }  
