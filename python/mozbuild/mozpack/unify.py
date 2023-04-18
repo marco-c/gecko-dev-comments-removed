f05b132d2fb6217e94dbe04a2e8f57065931637d
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 08:47:25 +0000

    Bug 1753292 - Don't re-order entries in macOS unified build omnijars. r=firefox-build-system-reviewers,mhentges
    Differential Revision: https://phabricator.services.mozilla.com/D138396
    
    UltraBlame original commit: c62c418a2f6c4fb9f19d8035f0a4fcd569e861c6

--- python/mozbuild/mozpack/unify.py
+++ python/mozbuild/mozpack/unify.py
@@ -2,6 +2,7 @@
 
 
 
+from collections import OrderedDict
 from mozpack.files import (
     BaseFinder,
     ExecutableFile,
@@ -126,10 +127,20 @@ class UnifiedFinder(BaseFinder):
         """
         UnifiedFinder.find() implementation.
         """
-        files1 = {p: f for p, f in self._finder1.find(path)}
-        files2 = {p: f for p, f in self._finder2.find(path)}
-        all_paths = set(files1) | set(files2)
-        for p in sorted(all_paths):
+        
+        
+        all_paths = OrderedDict()
+
+        files1 = OrderedDict()
+        for p, f in self._finder1.find(path):
+            files1[p] = f
+            all_paths[p] = True
+        files2 = OrderedDict()
+        for p, f in self._finder2.find(path):
+            files2[p] = f
+            all_paths[p] = True
+
+        for p in all_paths:
             err = errors.count
             unified = self.unify_file(p, files1.get(p), files2.get(p))
             if unified:
