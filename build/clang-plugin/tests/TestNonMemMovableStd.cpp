2e85ce544e49cca754b8a6873c430182b4c1451b
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 03:28:40 +0000

    Bug 1744604 - Part 2: Be less restrictive with the non-memmovable static analysis, r=andi
    This change adds an exception for stl types which implement
    `std::is_trivially_move_constructible` and `std::is_trivially_destructible`, as
    these are generally stable parts of the standard library type definition which
    may be relied on by downstream code.
    
    Differential Revision: https://phabricator.services.mozilla.com/D132996
    
    UltraBlame original commit: 1d7f0bd57bcacaca87639f07b07f78f536e1d79f

--- build/clang-plugin/tests/TestNonMemMovableStd.cpp
+++ build/clang-plugin/tests/TestNonMemMovableStd.cpp
@@ -6,16 +6,65 @@ class MOZ_NEEDS_MEMMOVABLE_TYPE Mover { T mForceInst; };
 namespace std {
 
 
-template<class C> class basic_string { }; 
+template<class C> class basic_string { 
+ public:
+  basic_string();
+  basic_string(const basic_string&);
+  basic_string(basic_string&&);
+  basic_string& operator=(const basic_string&);
+  basic_string& operator=(basic_string&&);
+  ~basic_string();
+};
 typedef basic_string<char> string;
 template<class T, class U> class pair { T mT; U mU; }; 
-class arbitrary_name { }; 
+
+struct has_nontrivial_dtor {  
+  has_nontrivial_dtor() = default;
+  ~has_nontrivial_dtor();
+};
+struct has_nontrivial_copy {  
+  has_nontrivial_copy() = default;
+  has_nontrivial_copy(const has_nontrivial_copy&);
+  has_nontrivial_copy& operator=(const has_nontrivial_copy&);
+};
+struct has_nontrivial_move {  
+  has_nontrivial_move() = default;
+  has_nontrivial_move(const has_nontrivial_move&);
+  has_nontrivial_move& operator=(const has_nontrivial_move&);
+};
+struct has_trivial_dtor {
+  has_trivial_dtor() = default;
+  ~has_trivial_dtor() = default;
+};
+struct has_trivial_copy {
+  has_trivial_copy() = default;
+  has_trivial_copy(const has_trivial_copy&) = default;
+  has_trivial_copy& operator=(const has_trivial_copy&) = default;
+};
+struct has_trivial_move {
+  has_trivial_move() = default;
+  has_trivial_move(const has_trivial_move&) = default;
+  has_trivial_move& operator=(const has_trivial_move&) = default;
+};
 }
 
 class HasString { std::string m; }; 
 
 static Mover<std::string> bad; 
 static Mover<HasString> bad_mem; 
-static Mover<std::arbitrary_name> assumed_bad; 
 static Mover<std::pair<bool, int>> good;
 static Mover<std::pair<bool, std::string>> not_good; 
+
+static Mover<std::has_nontrivial_dtor> nontrivial_dtor; 
+static Mover<std::has_nontrivial_copy> nontrivial_copy; 
+static Mover<std::has_nontrivial_move> nontrivial_move; 
+static Mover<std::has_trivial_dtor> trivial_dtor;
+static Mover<std::has_trivial_copy> trivial_copy;
+static Mover<std::has_trivial_move> trivial_move;
+
+static Mover<std::pair<bool, std::has_nontrivial_dtor>> pair_nontrivial_dtor; 
+static Mover<std::pair<bool, std::has_nontrivial_copy>> pair_nontrivial_copy; 
+static Mover<std::pair<bool, std::has_nontrivial_move>> pair_nontrivial_move; 
+static Mover<std::pair<bool, std::has_trivial_dtor>> pair_trivial_dtor;
+static Mover<std::pair<bool, std::has_trivial_copy>> pair_trivial_copy;
+static Mover<std::pair<bool, std::has_trivial_move>> pair_trivial_move;
