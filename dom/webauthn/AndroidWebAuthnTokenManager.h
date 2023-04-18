68764d8c1060084c54cf7adf1475540df3967a96
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 08:37:57 +0000

    Bug 1753919 - AndroidWebAuthnTokenManager uses GeckoResult directly instead of callback. r=geckoview-reviewers,agi
    Since WebAuthnTokenManager uses GeckoReuslt, we can remove callback of WebAuthnTokenManager.
    
    Differential Revision: https://phabricator.services.mozilla.com/D137973
    
    UltraBlame original commit: 921e6784ee11a5248ff8faa7f89d7d2f3783082b

--- dom/webauthn/AndroidWebAuthnTokenManager.h
+++ dom/webauthn/AndroidWebAuthnTokenManager.h
@@ -1,20 +1,21 @@
-/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
-/* vim: set ts=8 sts=2 et sw=2 tw=80: */
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+
+
+
+
 
 #ifndef mozilla_dom_AndroidWebAuthnTokenManager_h
 #define mozilla_dom_AndroidWebAuthnTokenManager_h
 
 #include "mozilla/dom/CryptoBuffer.h"
 #include "mozilla/dom/U2FTokenTransport.h"
+#include "mozilla/java/WebAuthnTokenManagerNatives.h"
 
 namespace mozilla {
 namespace dom {
 
-// Collected from
-// https://developers.google.com/android/reference/com/google/android/gms/fido/fido2/api/common/ErrorCode
+
+
 constexpr auto kSecurityError = u"SECURITY_ERR"_ns;
 constexpr auto kConstraintError = u"CONSTRAINT_ERR"_ns;
 constexpr auto kNotSupportedError = u"NOT_SUPPORTED_ERR"_ns;
@@ -32,7 +33,15 @@ class AndroidWebAuthnResult {
   explicit AndroidWebAuthnResult(const nsAString& aErrorCode)
       : mErrorCode(aErrorCode) {}
 
-  explicit AndroidWebAuthnResult() {}
+  explicit AndroidWebAuthnResult(
+      const java::WebAuthnTokenManager::MakeCredentialResponse::LocalRef&
+          aResponse);
+
+  explicit AndroidWebAuthnResult(
+      const java::WebAuthnTokenManager::GetAssertionResponse::LocalRef&
+          aResponse);
+
+  AndroidWebAuthnResult() = delete;
 
   bool IsError() const { return NS_FAILED(GetError()); }
 
@@ -42,8 +51,8 @@ class AndroidWebAuthnResult {
     } else if (mErrorCode.Equals(kSecurityError)) {
       return NS_ERROR_DOM_SECURITY_ERR;
     } else if (mErrorCode.Equals(kConstraintError)) {
-      // TODO: The message is right, but it's not about indexeddb.
-      // See https://heycam.github.io/webidl/#constrainterror
+      
+      
       return NS_ERROR_DOM_INDEXEDDB_CONSTRAINT_ERR;
     } else if (mErrorCode.Equals(kNotSupportedError)) {
       return NS_ERROR_DOM_NOT_SUPPORTED_ERR;
@@ -80,14 +89,14 @@ class AndroidWebAuthnResult {
         mUserHandle(aOther.mUserHandle.InfallibleClone()),
         mErrorCode(aOther.mErrorCode) {}
 
-  // Attestation-only
+  
   CryptoBuffer mAttObj;
 
-  // Attestations and assertions
+  
   CryptoBuffer mKeyHandle;
   nsCString mClientDataJSON;
 
-  // Assertions-only
+  
   CryptoBuffer mAuthData;
   CryptoBuffer mSignature;
   CryptoBuffer mUserHandle;
@@ -96,10 +105,10 @@ class AndroidWebAuthnResult {
   const nsString mErrorCode;
 };
 
-/*
- * WebAuthnAndroidTokenManager is a token implementation communicating with
- * Android Fido2 APIs.
- */
+
+
+
+
 class AndroidWebAuthnTokenManager final : public U2FTokenTransport {
  public:
   explicit AndroidWebAuthnTokenManager();
@@ -114,13 +123,13 @@ class AndroidWebAuthnTokenManager final : public U2FTokenTransport {
 
   void Drop() override;
 
+  static AndroidWebAuthnTokenManager* GetInstance();
+
+ private:
   void HandleRegisterResult(const AndroidWebAuthnResult& aResult);
 
   void HandleSignResult(const AndroidWebAuthnResult& aResult);
 
-  static AndroidWebAuthnTokenManager* GetInstance();
-
- private:
   void ClearPromises() {
     mRegisterPromise.RejectIfExists(NS_ERROR_DOM_UNKNOWN_ERR, __func__);
     mSignPromise.RejectIfExists(NS_ERROR_DOM_UNKNOWN_ERR, __func__);
@@ -132,7 +141,7 @@ class AndroidWebAuthnTokenManager final : public U2FTokenTransport {
   MozPromiseHolder<U2FSignPromise> mSignPromise;
 };
 
-}  // namespace dom
-}  // namespace mozilla
+}  
+}  
 
-#endif  // mozilla_dom_AndroidWebAuthnTokenManager_h
+#endif  
