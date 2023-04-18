201dab815a4f1923a9672811b44e46798cd8bc02
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 18:52:42 +0000

    Bug 1767833 - UTM params for STP reader links r=thecount
    Differential Revision: https://phabricator.services.mozilla.com/D145494
    
    UltraBlame original commit: 3d38391564303bcbf271d5cdb632cf92d25b21c0

--- browser/components/pocket/content/panels/js/main.bundle.js
+++ browser/components/pocket/content/panels/js/main.bundle.js
@@ -174,6 +174,10 @@ function Article(props) {
   const url = new URL(article.url || article.resolved_url || "");
   const urlSearchParams = new URLSearchParams(utmParams);
 
+  if (openInPocketReader && article.item_id && !url.href.match(/getpocket\.com\/read/)) {
+    url.href = `https://getpocket.com/read/${article.item_id}`;
+  }
+
   for (let [key, val] of urlSearchParams.entries()) {
     url.searchParams.set(key, val);
   } 
@@ -184,16 +188,10 @@ function Article(props) {
   const title = article.title || article.resolved_title; 
 
   const publisher = article.publisher || article.domain_metadata?.name || article.resolved_domain;
-  let constructedURL = url.href;
-
-  if (openInPocketReader && article.item_id && !url.href.match(/getpocket\.com\/read/)) {
-    constructedURL = `https://getpocket.com/read/${article.item_id}`;
-  }
-
   return react.createElement("li", {
     className: "stp_article_list_item"
   }, react.createElement(ArticleUrl, {
-    url: constructedURL,
+    url: url.href,
     savedArticle: savedArticle,
     position: position,
     source: source,
@@ -1008,7 +1006,8 @@ function Saved(props) {
     "data-l10n-id": "pocket-panel-button-remove"
   }))), savedStory && react.createElement(ArticleList_ArticleList, {
     articles: [savedStory],
-    openInPocketReader: true
+    openInPocketReader: true,
+    utmParams: utmParams
   }), react.createElement(TagPicker_TagPicker, {
     tags: [],
     itemUrl: itemUrl
