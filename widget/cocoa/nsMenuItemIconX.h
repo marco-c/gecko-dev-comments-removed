77fa3f062aa8e18a35df81482dbbcccea335c99e
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 21:36:00 +0000

    Bug 1771564 - Fix macOS builds.
    MANUAL PUSH: Bustage fix CLOSED TREE.
    
    UltraBlame original commit: bc528e3ae14e5356a276aee2d2e526accb755e5b

--- widget/cocoa/nsMenuItemIconX.h
+++ widget/cocoa/nsMenuItemIconX.h
@@ -61,7 +61,7 @@ class nsMenuItemIconX final : public mozilla::widget::IconLoader::Listener {
 
   Listener* mListener;            
   nsIntRect mImageRegionRect;
-  RefPtr<mozilla::ComputedStyle> mComputedStyle;
+  RefPtr<const mozilla::ComputedStyle> mComputedStyle;
   RefPtr<nsPresContext> mPresContext;
   NSImage* mIconImage = nil;  
   RefPtr<mozilla::widget::IconLoader> mIconLoader;
