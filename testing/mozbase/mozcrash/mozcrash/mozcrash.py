4b415bfdca2dbe42b7e3d59d0e43dc230362693d
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 01:23:42 +0000

    Bug 1741205 - Use rust-minidump's minidump-stackwalk for mozcrash.py r=gsvelto,KrisWright
    This is the first step in replacing a huge pile of our breakpad-based infra
    with our new implementation (rust-minidump). This stackwalker is only used
    for reporting crashes in local builds and CI, so it's a good first deploy.
    
    Although most of the work on rust-minidump has been focused on the JSON output,
    this uses the --human output, because it's primarily intended for humans to
    directly read. There is however some minor parsing done on this format. This
    is not *strictly* supported by --human (it has no schema) but it's not something
    we plan to break. (This parsing is pre-existing, just recording the facts.)
    
    The new build configs/scripts are hybridized from fix-stacks and dump_syms,
    as this basically is a hybrid of the two. In particular it needs the openssl
    vendoring tricks that dump_syms uses, but is a target binary that prefers
    win32 over win64 (like fix-stacks).
    
    Technically a regression but probably just culling legacy cruft at this point:
    this patchset removes support for building a local copy of minidump-stackwalk
    from source. You must now download a copy built on task-cluster using mozboot.
    mozboot *already* did this, which is why this feature appears to be legacy cruft
    -- there was little reason to build a local copy.
    
    However rust-minidump's minidump-stackwalk has a far better portability story,
    so you can build+install your own local copy by just running:
    
    cargo install minidump-stackwalk
    
    Differential Revision: https://phabricator.services.mozilla.com/D131315
    
    UltraBlame original commit: b0107c4d527481eec611527cd99cfaedc63a7303

--- testing/mozbase/mozcrash/mozcrash/mozcrash.py
+++ testing/mozbase/mozcrash/mozcrash/mozcrash.py
@@ -338,11 +338,11 @@ class CrashInfo(object):
             and os.access(self.stackwalk_binary, os.X_OK)
         ):
 
-            command = [self.stackwalk_binary, path, self.symbols_path]
+            command = [self.stackwalk_binary, "--human", path, self.symbols_path]
             
             
             if "MOZ_AUTOMATION" in os.environ:
-                command.append("https://symbols.mozilla.org/")
+                command.append("--symbols-url=https://symbols.mozilla.org/")
             self.logger.info(u"Copy/paste: {}".format(" ".join(command)))
             
             p = subprocess.Popen(
