5a9b5baac653e98006f78805133f668c4af2da66
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 20:00:25 +0000

    Bug 1765744 [wpt PR 33730] - Base patch for COOP WPTs refactoring., a=testonly
    Automatic update from web-platform-tests
    Base patch for COOP WPTs refactoring. (#33730)
    
    COOP WPTs have been written in a rigid way using postMessage which makes
    things complicated to modify given that opener cuts and multiple origins
    are involved. Since, new possibilities have made cross-origin
    communications easier and we leverage the dispatcher here to
    considerably increase readability.
    
    This is the first patch in a series of refactors that should eventually
    get rid of postMessage usage and coop_tests, url_tests, etc. in
    common.js
    
    BUG=1318373
    
    Change-Id: I516795b90224ac9390b099f855879d6d538e28a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3593078
    Reviewed-by: Arthur Sonzogni <arthursonzognichromium.org>
    Commit-Queue: Arthur Hemery <ahemerychromium.org>
    Cr-Commit-Position: refs/heads/main{#995203}
    
    Co-authored-by: Arthur Hemery <ahemerychromium.org>
    --
    
    wpt-commits: bb513c4e945bd468f0d8d8f455e1b9576c8101ed
    wpt-pr: 33730
    
    UltraBlame original commit: a7dc198d9879f219e6787dba9b45d9b358ffc97a

--- testing/web-platform/tests/html/cross-origin-opener-policy/resources/common.js
+++ testing/web-platform/tests/html/cross-origin-opener-policy/resources/common.js
@@ -1,3 +1,7 @@
+
+
+
+
 const SAME_ORIGIN = {origin: get_host_info().HTTPS_ORIGIN, name: "SAME_ORIGIN"};
 const SAME_SITE = {origin: get_host_info().HTTPS_REMOTE_ORIGIN, name: "SAME_SITE"};
 const CROSS_ORIGIN = {origin: get_host_info().HTTPS_NOTSAMESITE_ORIGIN, name: "CROSS_ORIGIN"}
