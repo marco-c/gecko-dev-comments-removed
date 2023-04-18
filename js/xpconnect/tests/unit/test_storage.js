1a0495ada19bcc1cd876eaf26820f5475c674d67
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 08:16:11 +0000

    Bug 1750934 - Support StorageManager in Sandbox and XPCComponents; r=smaug
    Differential Revision: https://phabricator.services.mozilla.com/D136316
    
    UltraBlame original commit: 4e71e4d9cf7ac4b1ba12a9dbd6dd6e8dc575a969

---
+++ js/xpconnect/tests/unit/test_storage.js
@@ -0,0 +1,12 @@
+
+
+
+function run_test() {
+  var sb = new Cu.Sandbox('http://www.example.com',
+                          { wantGlobalProperties: ["storage"] });
+  sb.ok = ok;
+  Cu.evalInSandbox('ok(storage instanceof StorageManager);',
+                   sb);
+  Cu.importGlobalProperties(["storage"]);
+  Assert.ok(storage instanceof StorageManager);
+}
