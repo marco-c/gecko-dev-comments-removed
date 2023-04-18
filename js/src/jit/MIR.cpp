10f25e1124df720b15f07ebf69b9ffce420f2122
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 00:19:06 +0000

    Bug 1739610 - Part 2: Improve MCheckReturn::foldsTo for construct calls. r=jandem
    Construct calls are always returning a boxed object. We can leverage this fact
    to remove `MCheckReturn` nodes for the case when the derived class constructor
    isn't inlined. When the class constructor is inlined, Warp already sees that
    the return value is a boxed object.
    
    Differential Revision: https://phabricator.services.mozilla.com/D130463
    
    UltraBlame original commit: c141a7c494e2015b6ac0581630a88a9972c6ee07

--- js/src/jit/MIR.cpp
+++ js/src/jit/MIR.cpp
@@ -6059,6 +6059,27 @@ MDefinition* MCheckIsObj::foldsTo(TempAllocator& alloc) {
   return this;
 }
 
+static bool IsBoxedObject(MDefinition* def) {
+  MOZ_ASSERT(def->type() == MIRType::Value);
+
+  if (def->isBox()) {
+    return def->toBox()->input()->type() == MIRType::Object;
+  }
+
+  
+  
+  
+  
+  if (def->isCall()) {
+    return def->toCall()->isConstructing();
+  }
+  if (def->isConstructArray()) {
+    return true;
+  }
+
+  return false;
+}
+
 MDefinition* MCheckReturn::foldsTo(TempAllocator& alloc) {
   auto* returnVal = returnValue();
   if (!returnVal->isBox()) {
@@ -6075,12 +6096,7 @@ MDefinition* MCheckReturn::foldsTo(TempAllocator& alloc) {
   }
 
   auto* thisVal = thisValue();
-  if (!thisVal->isBox()) {
-    return this;
-  }
-
-  auto* unboxedThisVal = thisVal->getOperand(0);
-  if (unboxedThisVal->type() == MIRType::Object) {
+  if (IsBoxedObject(thisVal)) {
     return thisVal;
   }
 
