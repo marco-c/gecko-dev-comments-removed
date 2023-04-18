f960fbca854576dfe7cf2a96e3754ff747f1c32d
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 08:51:36 +0000

    Backed out changeset 8a3535e92f63 (bug 1747172) for causing build bustages on AccessibleWrap.cpp. CLOSED TREE
    UltraBlame original commit: 130cad233ed25bfc2a2025e1310c188664bab909

--- accessible/base/Pivot.h
+++ accessible/base/Pivot.h
@@ -13,6 +13,8 @@
 namespace mozilla {
 namespace a11y {
 
+class LocalAccessible;
+class HyperTextAccessible;
 class DocAccessible;
 class Accessible;
 
@@ -54,12 +56,12 @@ class Pivot final {
   Accessible* Last(PivotRule& aRule);
 
   
-  Accessible* NextText(Accessible* aAnchor, int32_t* aStartOffset,
-                       int32_t* aEndOffset, int32_t aBoundaryType);
+  LocalAccessible* NextText(LocalAccessible* aAnchor, int32_t* aStartOffset,
+                            int32_t* aEndOffset, int32_t aBoundaryType);
 
   
-  Accessible* PrevText(Accessible* aAnchor, int32_t* aStartOffset,
-                       int32_t* aEndOffset, int32_t aBoundaryType);
+  LocalAccessible* PrevText(LocalAccessible* aAnchor, int32_t* aStartOffset,
+                            int32_t* aEndOffset, int32_t aBoundaryType);
 
   
   
@@ -78,7 +80,7 @@ class Pivot final {
                              bool aSearchCurrent);
 
   
-  Accessible* SearchForText(Accessible* aAnchor, bool aBackward);
+  HyperTextAccessible* SearchForText(LocalAccessible* aAnchor, bool aBackward);
 
   Accessible* mRoot;
 };
