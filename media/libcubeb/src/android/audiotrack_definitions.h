0b223c27407d44a69ed095262fd2072469b36edc
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 15:28:46 +0000

    Bug 1762104 - Switch media/libcubeb to mach vendor model.  r=cubeb-reviewers,padenot
    Differential Revision: https://phabricator.services.mozilla.com/D142521
    
    UltraBlame original commit: 7c3637a9cfc0f472ed5caa3f8fccbc93949865a4

--- media/libcubeb/src/android/audiotrack_definitions.h
+++ media/libcubeb/src/android/audiotrack_definitions.h
@@ -24,21 +24,23 @@
 
 
 
+
 typedef int32_t status_t;
 
 
 
 
+
 struct Buffer {
-  uint32_t    flags;
-  int         channelCount;
-  int         format;
-  size_t      frameCount;
-  size_t      size;
+  uint32_t flags;
+  int channelCount;
+  int format;
+  size_t frameCount;
+  size_t size;
   union {
-    void*       raw;
-    short*      i16;
-    int8_t*     i8;
+    void * raw;
+    short * i16;
+    int8_t * i8;
   };
 };
 
@@ -57,20 +59,23 @@ enum event_type {
 
 
 
+
 #define AUDIO_STREAM_TYPE_MUSIC 3
 
 enum {
-  AUDIO_CHANNEL_OUT_FRONT_LEFT_ICS  = 0x1,
+  AUDIO_CHANNEL_OUT_FRONT_LEFT_ICS = 0x1,
   AUDIO_CHANNEL_OUT_FRONT_RIGHT_ICS = 0x2,
-  AUDIO_CHANNEL_OUT_MONO_ICS     = AUDIO_CHANNEL_OUT_FRONT_LEFT_ICS,
-  AUDIO_CHANNEL_OUT_STEREO_ICS   = (AUDIO_CHANNEL_OUT_FRONT_LEFT_ICS | AUDIO_CHANNEL_OUT_FRONT_RIGHT_ICS)
+  AUDIO_CHANNEL_OUT_MONO_ICS = AUDIO_CHANNEL_OUT_FRONT_LEFT_ICS,
+  AUDIO_CHANNEL_OUT_STEREO_ICS =
+      (AUDIO_CHANNEL_OUT_FRONT_LEFT_ICS | AUDIO_CHANNEL_OUT_FRONT_RIGHT_ICS)
 } AudioTrack_ChannelMapping_ICS;
 
 enum {
   AUDIO_CHANNEL_OUT_FRONT_LEFT_Legacy = 0x4,
   AUDIO_CHANNEL_OUT_FRONT_RIGHT_Legacy = 0x8,
   AUDIO_CHANNEL_OUT_MONO_Legacy = AUDIO_CHANNEL_OUT_FRONT_LEFT_Legacy,
-  AUDIO_CHANNEL_OUT_STEREO_Legacy = (AUDIO_CHANNEL_OUT_FRONT_LEFT_Legacy | AUDIO_CHANNEL_OUT_FRONT_RIGHT_Legacy)
+  AUDIO_CHANNEL_OUT_STEREO_Legacy = (AUDIO_CHANNEL_OUT_FRONT_LEFT_Legacy |
+                                     AUDIO_CHANNEL_OUT_FRONT_RIGHT_Legacy)
 } AudioTrack_ChannelMapping_Legacy;
 
 typedef enum {
@@ -78,4 +83,3 @@ typedef enum {
   AUDIO_FORMAT_PCM_SUB_16_BIT = 0x1,
   AUDIO_FORMAT_PCM_16_BIT = (AUDIO_FORMAT_PCM | AUDIO_FORMAT_PCM_SUB_16_BIT),
 } AudioTrack_SampleType;
-
