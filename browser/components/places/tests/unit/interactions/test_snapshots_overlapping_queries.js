e83ff75d0986d61859232622c3be8561c21eea72
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 02:24:18 +0000

    Bug 1741646 - Extend overlapping snapshot scoring to give more weight to snapshots that were recently visited r=mossop
    This finer-grained ranking should produce more relevant suggestions.
    
    • Added scoring and tests for cases where interactions approach the overlap limit (scores between (1.0 and 0.0))
    • Replaced hard-coded overlap limit with a pref
    • Added tests to ensure correct behaviour when the the context / test urls are and are not snapshots
    
    Differential Revision: https://phabricator.services.mozilla.com/D131526
    
    UltraBlame original commit: 09f2e8522e22bbecbc35237143bae3e465ed9a65

--- browser/components/places/tests/unit/interactions/test_snapshots_overlapping_queries.js
+++ browser/components/places/tests/unit/interactions/test_snapshots_overlapping_queries.js
@@ -6,7 +6,11 @@
 
 
 const ONE_MINUTE = 1000 * 60;
-const ONE_HOUR = ONE_MINUTE * 60;
+
+const overlapLimit = Services.prefs.getIntPref(
+  "browser.places.interactions.snapshotOverlapLimit",
+  1800000
+); 
 
 
 
@@ -20,12 +24,24 @@ const ONE_HOUR = ONE_MINUTE * 60;
 
 
 
-async function create_snapshots(num, delay, currentTime = Date.now()) {
+
+
+
+async function create_snapshots({
+  num = 1,
+  duration = 5000,
+  delay = 2000,
+  currentTime = Date.now(),
+} = {}) {
   let now = currentTime - delay * num;
   for (let i = 0; i < num; i++) {
     let url = `https://example.com/${i}/`;
     await addInteractions([
-      { url, created_at: now + i * delay, updated_at: now + i * delay },
+      {
+        url,
+        created_at: now + i * delay,
+        updated_at: now + i * delay + duration,
+      },
     ]);
 
     await Snapshots.add({ url: `https://example.com/${i}/` });
@@ -45,7 +61,7 @@ async function reset_interactions_snapshots() {
 add_task(async function test_query_multiple_context_no_overlap() {
   await reset_interactions_snapshots();
 
-  await create_snapshots(10, ONE_HOUR);
+  await create_snapshots({ num: 10, delay: overlapLimit * 2.0 });
 
   for (let i = 0; i < 10; i++) {
     
@@ -58,7 +74,8 @@ add_task(async function test_query_multiple_context_no_overlap() {
 add_task(async function test_query_single_context_overlap() {
   await reset_interactions_snapshots();
 
-  await create_snapshots(10, 2000);
+  
+  await create_snapshots({ num: 10, duration: overlapLimit });
 
   
   let context = { url: "https://example.com/0/" };
@@ -80,11 +97,82 @@ add_task(async function test_query_single_context_overlap() {
 });
 
 
+add_task(async function test_query_context_not_a_snapshot_no_interaction() {
+  await reset_interactions_snapshots();
+
+  let context_url = "https://example.com/0/";
+
+  let now = Date.now();
+  let test_url = "https://example.com/1/";
+  await addInteractions([{ url: test_url, created_at: now, updated_at: now }]);
+  await Snapshots.add({ url: test_url });
+
+  await assertSnapshotsWithContext([], { url: context_url });
+});
+
+
+add_task(async function test_query_context_not_a_snapshot() {
+  await reset_interactions_snapshots();
+
+  let now = Date.now();
+  let context_url = "https://example.com/0/";
+  await addInteractions([
+    { url: context_url, created_at: now, updated_at: now },
+  ]);
+
+  let test_url = "https://example.com/1/";
+  await addInteractions([{ url: test_url, created_at: now, updated_at: now }]);
+  await Snapshots.add({ url: test_url });
+
+  await assertSnapshotsWithContext(
+    [{ url: "https://example.com/1/", overlappingVisitScoreGreaterThan: 0 }],
+    { url: context_url }
+  );
+});
+
+
+add_task(
+  async function test_query_context_not_a_snapshot_test_not_a_snapshot() {
+    await reset_interactions_snapshots();
+
+    let now = Date.now();
+    let context_url = "https://example.com/0/";
+    await addInteractions([
+      { url: context_url, created_at: now, updated_at: now },
+    ]);
+
+    let test_url = "https://example.com/1/";
+    await addInteractions([
+      { url: test_url, created_at: now, updated_at: now },
+    ]);
+
+    await assertSnapshotsWithContext([], { url: context_url });
+  }
+);
+
+
+add_task(async function test_query_context_a_snapshot_test_not_a_snapshot() {
+  await reset_interactions_snapshots();
+
+  let now = Date.now();
+  let context_url = "https://example.com/0/";
+  await addInteractions([
+    { url: context_url, created_at: now, updated_at: now },
+  ]);
+  await Snapshots.add({ url: context_url });
+
+  let test_url = "https://example.com/1/";
+  await addInteractions([{ url: test_url, created_at: now, updated_at: now }]);
+
+  await assertSnapshotsWithContext([], { url: context_url });
+});
+
+
 add_task(async function test_query_interact_before_no_overlap() {
   await reset_interactions_snapshots();
 
   let now = Date.now();
-  let before = now - ONE_HOUR / 2 - ONE_MINUTE;
+  let before = now - overlapLimit - ONE_MINUTE;
   let test_url = "https://example.com/1/";
   await addInteractions([
     { url: test_url, created_at: before, updated_at: before },
@@ -105,7 +193,7 @@ add_task(async function test_query_interact_before_overlap() {
   await reset_interactions_snapshots();
 
   let now = Date.now();
-  let before = now - ONE_HOUR / 2 + ONE_MINUTE;
+  let before = now - overlapLimit + ONE_MINUTE;
   let test_url = "https://example.com/1/";
   await addInteractions([
     { url: test_url, created_at: before, updated_at: before },
@@ -119,7 +207,13 @@ add_task(async function test_query_interact_before_overlap() {
   await Snapshots.add({ url: context_url });
 
   await assertSnapshotsWithContext(
-    [{ url: "https://example.com/1/", overlappingVisitScoreGreaterThan: 0 }],
+    [
+      {
+        url: "https://example.com/1/",
+        overlappingVisitScoreGreaterThan: 0.0,
+        overlappingVisitScoreLessThan: 0.1,
+      },
+    ],
     { url: context_url }
   );
 });
@@ -129,18 +223,19 @@ add_task(async function test_query_interact_after_no_overlap() {
   await reset_interactions_snapshots();
 
   let now = Date.now();
-  let after = now + ONE_HOUR / 2 + ONE_MINUTE;
-  let test_url = "https://example.com/1/";
-  await addInteractions([
-    { url: test_url, created_at: after, updated_at: after },
-  ]);
-  await Snapshots.add({ url: test_url });
 
   let context_url = "https://example.com/0/";
   await addInteractions([
     { url: context_url, created_at: now, updated_at: now },
   ]);
   await Snapshots.add({ url: context_url });
+
+  let after = now + overlapLimit + ONE_MINUTE;
+  let test_url = "https://example.com/1/";
+  await addInteractions([
+    { url: test_url, created_at: after, updated_at: after },
+  ]);
+  await Snapshots.add({ url: test_url });
 
   await assertSnapshotsWithContext([], { url: context_url });
 });
@@ -150,12 +245,6 @@ add_task(async function test_query_interact_after_overlap() {
   await reset_interactions_snapshots();
 
   let now = Date.now();
-  let after = now + ONE_HOUR / 2 - ONE_MINUTE;
-  let test_url = "https://example.com/1/";
-  await addInteractions([
-    { url: test_url, created_at: after, updated_at: after },
-  ]);
-  await Snapshots.add({ url: test_url });
 
   let context_url = "https://example.com/0/";
   await addInteractions([
@@ -163,8 +252,235 @@ add_task(async function test_query_interact_after_overlap() {
   ]);
   await Snapshots.add({ url: context_url });
 
+  let after = now + overlapLimit - ONE_MINUTE;
+  let test_url = "https://example.com/1/";
+  await addInteractions([
+    { url: test_url, created_at: after, updated_at: after },
+  ]);
+  await Snapshots.add({ url: test_url });
+
+  
   await assertSnapshotsWithContext(
-    [{ url: "https://example.com/1/", overlappingVisitScoreGreaterThan: 0 }],
+    [
+      {
+        url: "https://example.com/1/",
+        overlappingVisitScoreGreaterThan: 0.0,
+        overlappingVisitScoreLessThan: 0.1,
+      },
+    ],
+    { url: context_url }
+  );
+});
+
+
+add_task(async function test_query_interact_after_overlap() {
+  await reset_interactions_snapshots();
+
+  let now = Date.now();
+
+  let context_url = "https://example.com/0/";
+  await addInteractions([
+    { url: context_url, created_at: now, updated_at: now },
+  ]);
+  await Snapshots.add({ url: context_url });
+
+  let after = now + ONE_MINUTE;
+  let test_url = "https://example.com/1/";
+  await addInteractions([
+    { url: test_url, created_at: after, updated_at: after },
+  ]);
+  await Snapshots.add({ url: test_url });
+
+  
+  await assertSnapshotsWithContext(
+    [
+      {
+        url: "https://example.com/1/",
+        overlappingVisitScoreGreaterThan: 0.9,
+        overlappingVisitScoreLessThan: 1.0,
+      },
+    ],
+    { url: context_url }
+  );
+});
+
+
+add_task(async function test_query_single_context_increasing() {
+  await reset_interactions_snapshots();
+
+  await create_snapshots({ num: 10, duration: 1000, delay: 1000 * 60 });
+
+  
+  let context = { url: "https://example.com/0/" };
+
+  let snapshots = await Snapshots.queryOverlapping(context.url);
+
+  
+  let scores = {};
+  for (let s of snapshots) {
+    scores[s.url] = s.overlappingVisitScore;
+  }
+  await assertSnapshotsWithContext(
+    [
+      { url: "https://example.com/1/", overlappingVisitScoreGreaterThan: 0.0 },
+      {
+        url: "https://example.com/2/",
+        overlappingVisitScoreGreaterThan: 0.0,
+        overlappingVisitScoreLessThan: scores["https://example.com/1/"],
+      },
+      {
+        url: "https://example.com/3/",
+        overlappingVisitScoreGreaterThan: 0.0,
+        overlappingVisitScoreLessThan: scores["https://example.com/2/"],
+      },
+      {
+        url: "https://example.com/4/",
+        overlappingVisitScoreGreaterThan: 0.0,
+        overlappingVisitScoreLessThan: scores["https://example.com/3/"],
+      },
+      {
+        url: "https://example.com/5/",
+        overlappingVisitScoreGreaterThan: 0.0,
+        overlappingVisitScoreLessThan: scores["https://example.com/4/"],
+      },
+      {
+        url: "https://example.com/6/",
+        overlappingVisitScoreGreaterThan: 0.0,
+        overlappingVisitScoreLessThan: scores["https://example.com/5/"],
+      },
+      {
+        url: "https://example.com/7/",
+        overlappingVisitScoreGreaterThan: 0.0,
+        overlappingVisitScoreLessThan: scores["https://example.com/6/"],
+      },
+      {
+        url: "https://example.com/8/",
+        overlappingVisitScoreGreaterThan: 0.0,
+        overlappingVisitScoreLessThan: scores["https://example.com/7/"],
+      },
+      {
+        url: "https://example.com/9/",
+        overlappingVisitScoreGreaterThan: 0.0,
+        overlappingVisitScoreLessThan: scores["https://example.com/8/"],
+      },
+    ],
+    context
+  );
+});
+
+
+add_task(async function test_query_revisit() {
+  await reset_interactions_snapshots();
+
+  let now = Date.now();
+  let context_url = "https://example.com/0/";
+  await addInteractions([
+    { url: context_url, created_at: now, updated_at: now },
+  ]);
+  await Snapshots.add({ url: context_url });
+
+  
+  let after = now + overlapLimit - ONE_MINUTE;
+  await addInteractions([
+    { url: "https://example.com/1/", created_at: after, updated_at: after },
+  ]);
+  await Snapshots.add({ url: "https://example.com/1/" });
+
+  
+  await assertSnapshotsWithContext(
+    [
+      {
+        url: "https://example.com/1/",
+        overlappingVisitScoreGreaterThan: 0.0,
+        overlappingVisitScoreLessThan: 1.0,
+      },
+    ],
+    { url: context_url }
+  );
+
+  let snapshots = await Snapshots.queryOverlapping(context_url);
+  let scores = {};
+  for (let s of snapshots) {
+    scores[s.url] = s.overlappingVisitScore;
+  }
+
+  
+  await addInteractions([
+    {
+      url: "https://example.com/1/",
+      created_at: after - ONE_MINUTE,
+      updated_at: after - ONE_MINUTE,
+    },
+  ]);
+  await Snapshots.add({ url: "https://example.com/1/" });
+
+  await assertSnapshotsWithContext(
+    [
+      {
+        url: "https://example.com/1/",
+        overlappingVisitScoreGreaterThan: scores["https://example.com/1/"],
+        overlappingVisitScoreLessThanEqualTo: 1.0,
+      },
+    ],
+    { url: context_url }
+  );
+
+  
+  snapshots = await Snapshots.queryOverlapping(context_url);
+  scores = {};
+  for (let s of snapshots) {
+    scores[s.url] = s.overlappingVisitScore;
+  }
+
+  await addInteractions([
+    {
+      url: "https://example.com/1/",
+      created_at: now + overlapLimit / 2.0,
+      updated_at: now + overlapLimit / 2.0,
+    },
+  ]);
+  await Snapshots.add({ url: "https://example.com/1/" });
+
+  await assertSnapshotsWithContext(
+    [
+      {
+        url: "https://example.com/1/",
+        overlappingVisitScoreGreaterThan: scores["https://example.com/1/"],
+        overlappingVisitScoreLessThanEqualTo: 1.0,
+      },
+    ],
+    { url: context_url }
+  );
+});
+
+
+add_task(async function test_query_numerous_revisit() {
+  await reset_interactions_snapshots();
+
+  let now = Date.now();
+  let context_url = "https://example.com/0/";
+  await addInteractions([
+    { url: context_url, created_at: now, updated_at: now },
+  ]);
+  await Snapshots.add({ url: context_url });
+
+  for (let i = 0; i < 10; i++) {
+    let after = now + (overlapLimit / 10.0) * i;
+    await addInteractions([
+      { url: "https://example.com/1/", created_at: after, updated_at: after },
+    ]);
+    await Snapshots.add({ url: "https://example.com/1/" });
+  }
+
+  
+  await assertSnapshotsWithContext(
+    [
+      {
+        url: "https://example.com/1/",
+        overlappingVisitScoreGreaterThan: 0.0,
+        overlappingVisitScoreLessThanEqualTo: 1.0,
+      },
+    ],
     { url: context_url }
   );
 });
