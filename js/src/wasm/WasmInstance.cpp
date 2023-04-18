c917e62440957888fa26c4f1c21b47a44e044bd4
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 08:27:02 +0000

    Bug 1753061 - revert bug 1742053 part 2. r=rhunt
    Differential Revision: https://phabricator.services.mozilla.com/D137645
    
    UltraBlame original commit: 9bb72360ebd3c87719336b6c1da9a3613733dd77

--- js/src/wasm/WasmInstance.cpp
+++ js/src/wasm/WasmInstance.cpp
@@ -127,12 +127,6 @@ static bool IsImportedFunction(const uint32_t functionIndex,
   return functionIndex < metadataTier.funcImports.length();
 }
 
-static bool IsJSExportedFunction(JSFunction* fun) {
-  
-  
-  return !IsWasmExportedFunction(fun);
-}
-
 
 static bool IsNullFunction(const uint32_t functionIndex) {
   return functionIndex == NullFuncIndex;
@@ -878,7 +872,6 @@ bool Instance::initElems(uint32_t tableIndex, const ElemSegment& seg,
       if (IsImportedFunction(funcIndex, metadataTier)) {
         FuncImportTls& import = funcImportTls(funcImports[funcIndex]);
         JSFunction* fun = import.fun;
-        (void)IsJSExportedFunction(fun);  
         if (IsWasmExportedFunction(fun)) {
           
           
