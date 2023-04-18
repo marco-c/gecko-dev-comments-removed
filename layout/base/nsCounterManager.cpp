100e0f5e14b45994cff6182788bdd298bf85b24c
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 03:18:02 +0000

    Bug 1745506 - Add missing initialization of an INCREMENT node that isn't in a scope, nor first in the list.  r=emilio
    Differential Revision: https://phabricator.services.mozilla.com/D133574
    
    UltraBlame original commit: 56cf5b4eb7812650e60e935da251267ad832fe6e

--- layout/base/nsCounterManager.cpp
+++ layout/base/nsCounterManager.cpp
@@ -142,12 +142,16 @@ void nsCounterList::SetScope(nsCounterNode* aNode) {
   
   
 
-  if (aNode == First()) {
+  auto setNullScopeFor = [](nsCounterNode* aNode) {
     aNode->mScopeStart = nullptr;
     aNode->mScopePrev = nullptr;
     if (aNode->IsUnitializedIncrementNode()) {
       aNode->ChangeNode()->mChangeValue = 1;
     }
+  };
+
+  if (aNode == First()) {
+    setNullScopeFor(aNode);
     return;
   }
 
@@ -238,8 +242,7 @@ void nsCounterList::SetScope(nsCounterNode* aNode) {
     }
   }
 
-  aNode->mScopeStart = nullptr;
-  aNode->mScopePrev = nullptr;
+  setNullScopeFor(aNode);
 }
 
 void nsCounterList::RecalcAll() {
