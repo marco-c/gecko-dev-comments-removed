fda391d701716ef28662014af7d300d14b3deb58
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 11:07:47 +0000

    Bug 1735718 [wpt PR 31228] - [WPT] Eligibility: dedicated and shared workers, a=testonly
    Automatic update from web-platform-tests
    [WPT] Eligibility: dedicated and shared workers (#31228)
    
    When there are dedicated workers/shared workers,
    
    Chrome: The page is not BFCached.
    Firefox: The page is BFCached.
    Safari: The page is BFCached for dedicated workers.
            Shared workers are not supported.
    
    Bug: 1146955
    Change-Id: Ia901715436e1a0c64c68ad5b537e99f685825489
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3220944
    Reviewed-by: Ben Kelly <wanderviewchromium.org>
    Reviewed-by: Fergal Daly <fergalchromium.org>
    Reviewed-by: Rakina Zata Amni <rakinachromium.org>
    Commit-Queue: Hiroshige Hayashizaki <hiroshigechromium.org>
    Cr-Commit-Position: refs/heads/main{#968294}
    
    Co-authored-by: Hiroshige Hayashizaki <hiroshigechromium.org>
    --
    
    wpt-commits: 485d32c18d07b58d75c9997794987cfce3c35246
    wpt-pr: 31228
    
    UltraBlame original commit: e13f46108f28de6e3cfe88c7fef26a9ce4dd8c93

---
+++ testing/web-platform/tests/html/browsers/browsing-the-web/back-forward-cache/resources/echo-worker.js
@@ -0,0 +1,16 @@
+
+
+
+
+
+self.addEventListener('message', event => {
+  postMessage(event.data);
+});
+
+
+onconnect = e => {
+  const port = e.ports[0];
+  port.onmessage = event => {
+    port.postMessage(event.data);
+  }
+};
