153018d6ac6585e2f974c935015020706bc470da
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 05:31:29 +0000

    Bug 1746885 [wpt PR 32140] - Stream cancellation in page should be notified to service worker, a=testonly
    Automatic update from web-platform-tests
    Stream cancellation in page should be notified to service worker
    
    When the script transfers a body stream via FetchEvent.RespondWith and
    the stream consumption is cancelled in page, that cancellation should be
    observable in the service worker via UnderlyingSource.cancel.
    
    Bug: 638494
    Change-Id: Ib46c9ae73c17dd2c04432d23b06dad76466105ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3347484
    Reviewed-by: Ben Kelly <wanderviewchromium.org>
    Commit-Queue: Yutaka Hirano <yhiranochromium.org>
    Cr-Commit-Position: refs/heads/main{#956321}
    
    --
    
    wpt-commits: 222dab3304071fb479c07589d51bda846ce3ef08
    wpt-pr: 32140
    
    UltraBlame original commit: b1aadc65bb0d748b109303984a6a1362ab890848

--- testing/web-platform/tests/webcodecs/image-decoder.https.any.js
+++ testing/web-platform/tests/webcodecs/image-decoder.https.any.js
@@ -372,8 +372,9 @@ promise_test(async t => {
       })
       .then(_ => {
         
-        source.addFrame();
-        return promise_rejects_dom(t, 'InvalidStateError', decoder.decode());
+        assert_throws_js(TypeError, () => {
+          source.addFrame();
+        });
       });
 }, 'Test ReadableStream of gif');
 
