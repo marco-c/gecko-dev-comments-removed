32f81b3793476bb7bfefb0fe9c576df423254352
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 05:32:30 +0000

    Bug 1749394 - Remove now-expired validation metrics. r=chutten
    They served their purpose of validation Gecko integration.
    In followups we do some proper migration.
    
    Differential Revision: https://phabricator.services.mozilla.com/D135593
    
    UltraBlame original commit: 01e58b5ab43584b13dbf48158ba7a24ef6db9539

--- toolkit/xre/nsAppRunner.cpp
+++ toolkit/xre/nsAppRunner.cpp
@@ -29,7 +29,6 @@
 #include "mozilla/Utf8.h"
 #include "mozilla/intl/LocaleService.h"
 #include "mozilla/JSONWriter.h"
-#include "mozilla/glean/GleanMetrics.h"
 #include "BaseProfiler.h"
 
 #include "nsAppRunner.h"
@@ -5276,10 +5275,10 @@ nsresult XREMain::XRE_mainRun() {
 #endif 
 
     
-    nsAutoCString version(gAppData->version);
-    nsAutoCString buildID(gAppData->buildID);
-    mozilla::glean::geckoview_validation::version.Set(version);
-    mozilla::glean::geckoview_validation::build_id.Set(buildID);
+    Telemetry::ScalarSet(Telemetry::ScalarID::GECKO_VERSION,
+                         NS_ConvertASCIItoUTF16(gAppData->version));
+    Telemetry::ScalarSet(Telemetry::ScalarID::GECKO_BUILD_ID,
+                         NS_ConvertASCIItoUTF16(gAppData->buildID));
 
 #if defined(MOZ_SANDBOX) && defined(XP_LINUX)
     
