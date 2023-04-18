277a01aa32973973ec2f066f1dd3170d7a70dc8e
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 19:09:57 +0000

    Bug 1767970 - [devtools] Merge SplitView into StyleEditorUI. r=jdescottes.
    This allows to simplify what was done in SplitView and have
    direct calls to functions instead of registering callbacks.
    
    Differential Revision: https://phabricator.services.mozilla.com/D145607
    
    UltraBlame original commit: d6bd7d407172f942fe56128d4a44f4031842012e

--- devtools/client/styleeditor/test/browser_styleeditor_scroll.js
+++ devtools/client/styleeditor/test/browser_styleeditor_scroll.js
@@ -63,7 +63,7 @@ add_task(async function() {
   
   
   const summary = await ui.getEditorSummary(longEditor);
-  ui._view.setActiveSummary(summary);
+  summary.click();
 
   info("Waiting for doc_long.css to be selected.");
   await selectEventPromise;
