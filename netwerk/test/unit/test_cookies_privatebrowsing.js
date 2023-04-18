106849b4d4ea3c1e2d87f24e2f408ecdbefc1cdf
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 23:39:45 +0000

    Bug 1766228 - Replace manual definition of Services.cookiesvc with Services.cookies. r=necko-reviewers,kershaw
    This makes the tests consistent with the rest of the code, and avoids errors from the new ESLint valid-services rule.
    
    Depends on D149390
    
    Differential Revision: https://phabricator.services.mozilla.com/D149391
    
    UltraBlame original commit: 3240fa10558b78a6b06b2ffa9f1c6a3ce335f815

--- netwerk/test/unit/test_cookies_privatebrowsing.js
+++ netwerk/test/unit/test_cookies_privatebrowsing.js
@@ -49,7 +49,7 @@ add_task(async () => {
   let uri2 = NetUtil.newURI("http://bar.com/bar.html");
 
   
-  Services.cookiesvc.setCookieStringFromHttp(
+  Services.cookies.setCookieStringFromHttp(
     uri1,
     "oh=hai; max-age=1000",
     make_channel(uri1.spec)
@@ -65,11 +65,7 @@ add_task(async () => {
   chan2.QueryInterface(Ci.nsIPrivateBrowsingChannel);
   chan2.setPrivate(true);
 
-  Services.cookiesvc.setCookieStringFromHttp(
-    uri2,
-    "oh=hai; max-age=1000",
-    chan2
-  );
+  Services.cookies.setCookieStringFromHttp(uri2, "oh=hai; max-age=1000", chan2);
   Assert.equal(await getCookieStringFromPrivateDocument(uri1.spec), "");
   Assert.equal(await getCookieStringFromPrivateDocument(uri2.spec), "oh=hai");
 
@@ -78,11 +74,7 @@ add_task(async () => {
   Assert.equal(await getCookieStringFromPrivateDocument(uri1.spec), "");
   Assert.equal(await getCookieStringFromPrivateDocument(uri2.spec), "");
 
-  Services.cookiesvc.setCookieStringFromHttp(
-    uri2,
-    "oh=hai; max-age=1000",
-    chan2
-  );
+  Services.cookies.setCookieStringFromHttp(uri2, "oh=hai; max-age=1000", chan2);
   Assert.equal(await getCookieStringFromPrivateDocument(uri2.spec), "oh=hai");
 
   
@@ -101,11 +93,7 @@ add_task(async () => {
   
   Assert.equal(await getCookieStringFromPrivateDocument(uri1.spec), "");
   Assert.equal(await getCookieStringFromPrivateDocument(uri2.spec), "");
-  Services.cookiesvc.setCookieStringFromHttp(
-    uri2,
-    "oh=hai; max-age=1000",
-    chan2
-  );
+  Services.cookies.setCookieStringFromHttp(uri2, "oh=hai; max-age=1000", chan2);
   Assert.equal(await getCookieStringFromPrivateDocument(uri2.spec), "oh=hai");
 
   
