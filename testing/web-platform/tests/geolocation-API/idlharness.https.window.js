092abdbf7cfbb71132d2c3dddd46daad4a01eb05
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 12:23:35 +0000

    Bug 1755940 [wpt PR 32882] - Rewrite geolocation-API/getCurrentPosition_IDL.https.html, a=testonly
    Automatic update from web-platform-tests
    Remove geolocation-API/getCurrentPosition_IDL.https.html, use IDL harness (#32882)
    
    * Rewrite geolocation-API/getCurrentPosition_IDL.https.html
    
    * fixup
    
    * fixup assert_equals
    
    * Use idlharness instead
    
    * fixup
    
    * bug fix
    --
    
    wpt-commits: cab30b2f07a82a631bb359095c09ec56e57dd190
    wpt-pr: 32882
    
    UltraBlame original commit: 58e07ce2f167dd39892c8ae4c7fe725a78aaaf44

--- testing/web-platform/tests/geolocation-API/idlharness.https.window.js
+++ testing/web-platform/tests/geolocation-API/idlharness.https.window.js
@@ -3,13 +3,26 @@
 
 
 
-idl_test(
-  ['geolocation'],
-  ['hr-time', 'html'],
-  idl_array => {
+
+
+window.onload = async () => {
+  await test_driver.set_permission({ name: "geolocation" }, "denied");
+  const positionError = await new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(reject, resolve);
+  });
+
+  await test_driver.set_permission({ name: "geolocation" }, "granted");
+  const position = await new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+
+  idl_test(["geolocation"], ["hr-time", "html"], (idl_array) => {
     idl_array.add_objects({
-      Navigator: ['navigator'],
-      Geolocation: ['navigator.geolocation'],
+      Navigator: ["navigator"],
+      Geolocation: ["navigator.geolocation"],
+      GeolocationPositionError: [positionError],
+      GeolocationPosition: [position],
+      GeolocationCoordinates: [position.coords],
     });
-  }
-);
+  });
+};
