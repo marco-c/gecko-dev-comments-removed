821698569f17c793c92bdcf4c81b3a53c86f5758
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 08:33:26 +0000

    Bug 1752664 - sync shortly after a tab change event r=Gijs,markh
    Differential Revision: https://phabricator.services.mozilla.com/D137376
    
    UltraBlame original commit: a7773f9b80194a6a7af8c77752722026088329a2

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1432,6 +1432,11 @@ pref("services.sync.syncedTabs.showRemoteIcons", true);
 
 
 
+
+pref("services.sync.syncedTabs.syncDelayAfterTabChange", 0);
+
+
+
 pref("browser.menu.showCharacterEncoding", "chrome://browser/locale/browser.properties");
 
 
