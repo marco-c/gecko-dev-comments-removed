1c1510ce512d362de6cc9efab346523a82282446
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 22:01:20 +0000

    Bug 1770885 - Cache existing public key rather than recreating it over and over. r=keeler
    As per recommendation in the other revision.
    
    Differential Revision: https://phabricator.services.mozilla.com/D147648
    
    UltraBlame original commit: 2a21aa12623f7f7fcf6b2785ddc5b87ef402762c

--- dom/origin-trials/OriginTrials.cpp
+++ dom/origin-trials/OriginTrials.cpp
@@ -1,11 +1,12 @@
-
-
-
-
-
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=8 sts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "OriginTrials.h"
 #include "mozilla/Base64.h"
+#include "mozilla/Span.h"
 #include "nsString.h"
 #include "nsIPrincipal.h"
 #include "nsIURI.h"
@@ -15,61 +16,91 @@
 #include "jsapi.h"
 #include "js/Wrapper.h"
 #include "nsGlobalWindowInner.h"
+#include "mozilla/ClearOnShutdown.h"
 #include "mozilla/dom/Document.h"
 #include "mozilla/dom/WorkerPrivate.h"
 #include "mozilla/dom/WorkletThread.h"
 #include "mozilla/dom/WebCryptoCommon.h"
 #include "mozilla/StaticPrefs_dom.h"
 #include "ScopedNSSTypes.h"
+#include <mutex>
 
 namespace mozilla {
 
 LazyLogModule sOriginTrialsLog("OriginTrials");
 #define LOG(...) MOZ_LOG(sOriginTrialsLog, LogLevel::Debug, (__VA_ARGS__))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// prod.pub is the EcdsaP256 public key from the production key managed in
+// Google Cloud. See:
+//
+//   https://github.com/mozilla/origin-trial-token/blob/main/tools/README.md#get-the-public-key
+//
+// for how to get the public key.
+//
+// See also:
+//
+//   https://github.com/mozilla/origin-trial-token/blob/main/tools/README.md#sign-a-token-using-gcloud
+//
+// for how to sign using this key.
+//
+// test.pub is the EcdsaP256 public key from this key pair:
+//
+//  * https://github.com/mozilla/origin-trial-token/blob/64f03749e2e8c58f811f67044cecc7d6955fd51a/tools/test-keys/test-ecdsa.pkcs8
+//  * https://github.com/mozilla/origin-trial-token/blob/64f03749e2e8c58f811f67044cecc7d6955fd51a/tools/test-keys/test-ecdsa.pub
+//
 #include "keys.inc"
 
 constexpr auto kEcAlgorithm =
     NS_LITERAL_STRING_FROM_CSTRING(WEBCRYPTO_NAMED_CURVE_P256);
 
+using RawKeyRef = Span<const unsigned char, sizeof(kProdKey)>;
+
+struct StaticCachedPublicKey {
+  constexpr StaticCachedPublicKey() = default;
+
+  SECKEYPublicKey* Get(const RawKeyRef aRawKey);
+
+ private:
+  std::once_flag mFlag;
+  UniqueSECKEYPublicKey mKey;
+};
+
+SECKEYPublicKey* StaticCachedPublicKey::Get(const RawKeyRef aRawKey) {
+  std::call_once(mFlag, [&] {
+    const SECItem item{siBuffer, const_cast<unsigned char*>(aRawKey.data()),
+                       unsigned(aRawKey.Length())};
+    MOZ_RELEASE_ASSERT(item.data[0] == EC_POINT_FORM_UNCOMPRESSED);
+
+    // Key verification takes a lot of time when verifying tokens, and it is
+    // unnecessary work since the keys are trusted.
+    const bool kVerifyValid = false;
+    mKey = dom::CreateECPublicKey(&item, kEcAlgorithm, kVerifyValid).release();
+    if (mKey) {
+      // It's fine to capture [this] by pointer because we are always static.
+      if (NS_IsMainThread()) {
+        RunOnShutdown([this] { mKey = nullptr; });
+      } else {
+        NS_DispatchToMainThread(NS_NewRunnableFunction(
+            "ClearStaticCachedPublicKey",
+            [this] { RunOnShutdown([this] { mKey = nullptr; }); }));
+      }
+    }
+  });
+  return mKey.get();
+}
+
 bool VerifySignature(const uint8_t* aSignature, uintptr_t aSignatureLen,
                      const uint8_t* aData, uintptr_t aDataLen,
                      void* aUserData) {
   MOZ_RELEASE_ASSERT(aSignatureLen == 64);
+  static StaticCachedPublicKey sTestKey;
+  static StaticCachedPublicKey sProdKey;
+
   LOG("VerifySignature()\n");
 
-  const unsigned char* key =
-      StaticPrefs::dom_origin_trials_test_key_enabled() ? kTestKey : kProdKey;
-
-  static_assert(sizeof(kTestKey) == sizeof(kProdKey));
-  const SECItem rawKey{siBuffer, const_cast<unsigned char*>(key),
-                       sizeof(kProdKey)};
-  MOZ_RELEASE_ASSERT(rawKey.data[0] == EC_POINT_FORM_UNCOMPRESSED);
-
-  
-  
-  const bool kVerifyValid = false;
-
-  UniqueSECKEYPublicKey pubKey =
-      dom::CreateECPublicKey(&rawKey, kEcAlgorithm, kVerifyValid);
+  SECKEYPublicKey* pubKey = StaticPrefs::dom_origin_trials_test_key_enabled()
+                                ? sTestKey.Get(Span(kTestKey))
+                                : sProdKey.Get(Span(kProdKey));
   if (NS_WARN_IF(!pubKey)) {
     LOG("  Failed to create public key?");
     return false;
@@ -85,9 +116,9 @@ bool VerifySignature(const uint8_t* aSignature, uintptr_t aSignatureLen,
   const SECItem data{siBuffer, const_cast<unsigned char*>(aData),
                      unsigned(aDataLen)};
 
-  
+  // SEC_OID_ANSIX962_ECDSA_SHA256_SIGNATURE
   const SECStatus result = PK11_VerifyWithMechanism(
-      pubKey.get(), CKM_ECDSA_SHA256, nullptr, &signature, &data, nullptr);
+      pubKey, CKM_ECDSA_SHA256, nullptr, &signature, &data, nullptr);
   if (NS_WARN_IF(result != SECSuccess)) {
     LOG("  Failed to verify data.");
     return false;
@@ -104,7 +135,7 @@ bool MatchesOrigin(const uint8_t* aOrigin, size_t aOriginLen, bool aIsSubdomain,
       aIsUsageSubset, nsCString(origin).get());
 
   if (aIsThirdParty || aIsUsageSubset) {
-    
+    // TODO(emilio): Support third-party tokens and so on.
     return false;
   }
 
@@ -155,13 +186,13 @@ void OriginTrials::UpdateFromToken(const nsAString& aBase64EncodedToken,
   const origin_trials_ffi::OriginTrialValidationParams params{
       VerifySignature,
       MatchesOrigin,
-       aPrincipal,
+      /* user_data = */ aPrincipal,
   };
   auto result = origin_trials_ffi::origin_trials_parse_and_validate_token(
       decodedTokenSpan.data(), decodedTokenSpan.size(), &params);
   if (NS_WARN_IF(!result.IsOk())) {
     LOG("  result = %d\n", int(result.tag));
-    return;  
+    return;  // TODO(emilio): Maybe report to console or what not?
   }
   OriginTrial trial = result.AsOk().trial;
   LOG("  result = Ok(%d)\n", int(trial));
@@ -215,4 +246,4 @@ bool OriginTrials::IsEnabled(JSContext* aCx, JSObject* aObject,
 
 #undef LOG
 
-}  
+}  // namespace mozilla
