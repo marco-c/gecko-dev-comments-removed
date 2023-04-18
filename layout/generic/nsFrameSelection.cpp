7a582cbf8c31f3cfa34e44bfcb79d567532a081a
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 02:53:52 +0000

    Bug 1735446 - part 1: Make `Selection` use `uint32_t` for handling offset in DOM nodes r=smaug
    Differential Revision: https://phabricator.services.mozilla.com/D128844
    
    UltraBlame original commit: e6de69a93ae7401b66f24cc1face02cddad344b6

--- layout/generic/nsFrameSelection.cpp
+++ layout/generic/nsFrameSelection.cpp
@@ -1514,12 +1514,21 @@ UniquePtr<SelectionDetails> nsFrameSelection::LookUpSelection(
     return nullptr;
   }
 
+  
+  
+  MOZ_ASSERT(aContentOffset >= 0);
+  MOZ_ASSERT(aContentLength >= 0);
+  if (MOZ_UNLIKELY(aContentOffset < 0) || MOZ_UNLIKELY(aContentLength < 0)) {
+    return nullptr;
+  }
+
   UniquePtr<SelectionDetails> details;
 
   for (size_t j = 0; j < ArrayLength(mDomSelections); j++) {
     if (mDomSelections[j]) {
       details = mDomSelections[j]->LookUpSelection(
-          aContent, aContentOffset, aContentLength, std::move(details),
+          aContent, static_cast<uint32_t>(aContentOffset),
+          static_cast<uint32_t>(aContentLength), std::move(details),
           kPresentSelectionTypes[j], aSlowCheck);
     }
   }
