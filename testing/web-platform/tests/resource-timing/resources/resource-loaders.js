af85404ed8c5970559e8225986db4501e119e48f
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 22:54:44 +0000

    Bug 1772567 [wpt PR 34301] - Revert "Revamp object-not-found redirect resource timing tests", a=testonly
    Automatic update from web-platform-tests
    Revert "Revamp object-not-found redirect resource timing tests" (#34301)
    
    This reverts commit f1fda133e0491f023d674167e36e04bd1da4061b.
    
    Reason for revert: likely caused linux-bfcache-rel CI failures.
    
    Example failure:
    https://ci.chromium.org/ui/p/chromium/builders/ci/linux-bfcache-rel/33293/overview
    
    Original change's description:
    > Revamp object-not-found redirect resource timing tests
    >
    > I suspect that the object resource timing tests are flakily timing out
    > due to flakiness in <object>'s events themselves.
    >
    > This CL moves these tests to the more recent resource timing testing
    > infrastructure, and also ends the test once a PerformanceObserver runs
    > its callback.
    >
    > Bug: 1318592
    > Change-Id: I2b01188756b212b51b8c8d095a2257d4a0e54e91
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3682424
    > Commit-Queue: Yoav Weiss <yoavweisschromium.org>
    > Reviewed-by: Ian Clelland <iclellandchromium.org>
    > Cr-Commit-Position: refs/heads/main{#1010553}
    
    Bug: 1332556, 1318592
    Change-Id: I99dc11ffa01158bcd2c0e1814ff5df4c3a158d7d
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3688576
    Bot-Commit: Rubber Stamper <rubber-stamperappspot.gserviceaccount.com>
    Owners-Override: Igor Ruvinov <igorruvinovchromium.org>
    Commit-Queue: Igor Ruvinov <igorruvinovchromium.org>
    Cr-Commit-Position: refs/heads/main{#1010593}
    
    Co-authored-by: Igor Ruvinov <igorruvinovchromium.org>
    --
    
    wpt-commits: 4229253c55247610c2e7a68f9456ccb4bf51abc2
    wpt-pr: 34301
    
    UltraBlame original commit: 8335ddf9bd7bfff4adf720993533249543adc5af

--- testing/web-platform/tests/resource-timing/resources/resource-loaders.js
+++ testing/web-platform/tests/resource-timing/resources/resource-loaders.js
@@ -98,9 +98,6 @@ const load = {
     const object = document.createElement("object");
     const loaded = new Promise(resolve => {
       object.onload = object.onerror = resolve;
-      
-      
-      (new PerformanceObserver(resolve)).observe({type: "resource"});
     });
     object.data = load.cache_bust(path);
     if (type) {
