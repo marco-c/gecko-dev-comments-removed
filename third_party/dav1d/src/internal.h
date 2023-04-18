198319111ada6575d348f92c7df32b6f06d60088
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 03:20:47 +0000

    Backed out changeset b6b0a24a69e9 (bug 1744639) for causing Updatebot bustages. CLOSED TREE
    UltraBlame original commit: 0284a394524078a97e28aa90055f5c2cd51f91b3

--- third_party/dav1d/src/internal.h
+++ third_party/dav1d/src/internal.h
@@ -175,7 +175,6 @@ enum TaskType {
     DAV1D_TASK_TYPE_SUPER_RESOLUTION,
     DAV1D_TASK_TYPE_LOOP_RESTORATION,
     DAV1D_TASK_TYPE_ENTROPY_PROGRESS,
-    DAV1D_TASK_TYPE_RECONSTRUCTION_PROGRESS,
 };
 
 struct Dav1dTask {
@@ -184,7 +183,7 @@ struct Dav1dTask {
     int sby;                    
 
     
-    int recon_progress, deblock_progress;
+    int recon_progress, deblock_progress, cdef_progress, lr_progress;
     int deps_skip;
     struct Dav1dTask *next; 
 };
@@ -227,7 +226,7 @@ struct Dav1dFrameContext {
         filter_sbrow_fn filter_sbrow;
         filter_sbrow_fn filter_sbrow_deblock_cols;
         filter_sbrow_fn filter_sbrow_deblock_rows;
-        void (*filter_sbrow_cdef)(Dav1dTaskContext *tc, int sby);
+        filter_sbrow_fn filter_sbrow_cdef;
         filter_sbrow_fn filter_sbrow_resize;
         filter_sbrow_fn filter_sbrow_lr;
         backup_ipred_edge_fn backup_ipred_edge;
@@ -249,8 +248,7 @@ struct Dav1dFrameContext {
     struct {
         int next_tile_row[2 ];
         int entropy_progress;
-        atomic_int deblock_progress; 
-        atomic_uint *frame_progress, *copy_lpf_progress;
+        atomic_int deblock_progress, cdef_progress, lr_progress; 
         
         Av1Block *b;
         struct CodedBlockInfo {
@@ -262,7 +260,6 @@ struct Dav1dFrameContext {
         
         uint8_t *pal_idx;
         coef *cf;
-        int prog_sz;
         int pal_sz, pal_idx_sz, cf_sz;
         
         int *tile_start_off;
@@ -273,24 +270,21 @@ struct Dav1dFrameContext {
         uint8_t (*level)[4];
         Av1Filter *mask;
         Av1Restoration *lr_mask;
-        int mask_sz , lr_mask_sz;
-        int cdef_buf_plane_sz[2]; 
-        int cdef_buf_sbh;
-        int lr_buf_plane_sz[2]; 
+        int top_pre_cdef_toggle;
+        int mask_sz , lr_mask_sz, cdef_line_sz[2] ;
+        size_t lr_plane_sz; 
         int re_sz ;
         ALIGN(Av1FilterLUT lim_lut, 16);
         int last_sharpness;
         uint8_t lvl[8 ][4 ][8 ][2 ];
         uint8_t *tx_lpf_right_edge[2];
-        uint8_t *cdef_line_buf, *lr_line_buf;
+        uint8_t *cdef_line_buf;
         pixel *cdef_line[2 ][3 ];
-        pixel *cdef_lpf_line[3 ];
         pixel *lr_lpf_line[3 ];
 
         
         uint8_t *start_of_tile_row;
         int start_of_tile_row_sz;
-        int need_cdef_lpf_copy;
         pixel *p[3], *sr_p[3];
         Av1Filter *mask_ptr, *prev_mask_ptr;
         int restore_planes; 
@@ -302,7 +296,6 @@ struct Dav1dFrameContext {
         struct Dav1dTask *tasks, *tile_tasks[2], init_task;
         int num_tasks, num_tile_tasks;
         int done[2];
-        int retval;
         int update_set; 
         atomic_int error;
         int task_counter;
@@ -358,8 +351,7 @@ struct Dav1dTaskContext {
     Dav1dTileState *ts;
     int bx, by;
     BlockContext l, *a;
-    refmvs_tile rt;
-    ALIGN(union, 64) {
+    ALIGN(union, 32) {
         int16_t cf_8bpc [32 * 32];
         int32_t cf_16bpc[32 * 32];
     };
@@ -368,6 +360,7 @@ struct Dav1dTaskContext {
     uint16_t al_pal[2 ][32 ][3 ][8 ];
     uint8_t pal_sz_uv[2 ][32 ];
     uint8_t txtp_map[32 * 32]; 
+    refmvs_tile rt;
     ALIGN(union, 64) {
         struct {
             union {
@@ -395,7 +388,7 @@ struct Dav1dTaskContext {
             int16_t ac[32 * 32];
             uint8_t pal_idx[2 * 64 * 64];
             uint16_t pal[3 ][8 ];
-            ALIGN(union, 64) {
+            ALIGN(union, 32) {
                 struct {
                     uint8_t interintra_8bpc[64 * 64];
                     uint8_t edge_8bpc[257];
@@ -410,7 +403,6 @@ struct Dav1dTaskContext {
 
     Dav1dWarpedMotionParams warpmv;
     Av1Filter *lf_mask;
-    int top_pre_cdef_toggle;
     int8_t *cur_sb_cdef_idx_ptr;
     
     
