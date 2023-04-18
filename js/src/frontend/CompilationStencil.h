7a4f39a52b7b5a7a2803f1fd55602373faf41028
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 02:33:46 +0000

    Bug 1743449 part 3 - Use RefPtr<..>&& for CompilationStencil. r=arai
    Previously, we were using `UniquePtr<..>` for `CompilationStencil`, which is ok
    to move, as we have the garantee that we have a single entity.
    
    However, as we replaced the `UniquePtr<..>` by `RefPtr<..>`, using a move can be
    considered harmful if more than one reference exists.  This patch changes usage
    of `CompilationStencil&&` by `RefPtr<CompilationStencil>&&`, and check the
    number of references before moving the content out of the CompilationStencil.
    
    Differential Revision: https://phabricator.services.mozilla.com/D132726
    
    UltraBlame original commit: bf75d7c179757ab1350dd598c1d147fc0eda8e67

--- js/src/frontend/CompilationStencil.h
+++ js/src/frontend/CompilationStencil.h
@@ -1284,7 +1284,7 @@ struct ExtensibleCompilationStencil {
   }
 
   
-  [[nodiscard]] bool steal(JSContext* cx, CompilationStencil&& other);
+  [[nodiscard]] bool steal(JSContext* cx, RefPtr<CompilationStencil>&& other);
 
   bool isModule() const;
 
