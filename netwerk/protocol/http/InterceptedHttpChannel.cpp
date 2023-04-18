e8b7069efd67c0ca271a14bc9197e9ae2acedbde
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 04:49:04 +0000

    Bug 1747445 - Do not record time stamps for InterceptedHttpChannel if AsyncOpen() is never called. r=dom-worker-reviewers,smaug
    It  is an edge case that InterceptedHttpChannel::Cancel() could be called before calling AsyncOpenInternal().
    In the case, mTimeStamp status would be Created and we should not record any time stamp for InterceptedHttpChannel.
    
    Differential Revision: https://phabricator.services.mozilla.com/D134775
    
    UltraBlame original commit: c053ba827f29fd3452a37655b405a9d5abca7087

--- netwerk/protocol/http/InterceptedHttpChannel.cpp
+++ netwerk/protocol/http/InterceptedHttpChannel.cpp
@@ -1371,23 +1371,26 @@ void InterceptedHttpChannel::InterceptionTimeStamps::RecordTime(
   
   MOZ_ASSERT(mStatus == Initialized || aStatus == Canceled);
 
-  if (mStatus == Initialized) {
-    mStatus = aStatus;
-  } else {
-    switch (mStatus) {
-      case Synthesized:
-        mStatus = CanceledAfterSynthesized;
-        break;
-      case Reset:
-        mStatus = CanceledAfterReset;
-        break;
-      case Redirected:
-        mStatus = CanceledAfterRedirected;
-        break;
-      default:
-        MOZ_ASSERT(false);
-        break;
-    }
+  switch (mStatus) {
+    case Initialized:
+      mStatus = aStatus;
+      break;
+    case Synthesized:
+      mStatus = CanceledAfterSynthesized;
+      break;
+    case Reset:
+      mStatus = CanceledAfterReset;
+      break;
+    case Redirected:
+      mStatus = CanceledAfterRedirected;
+      break;
+    
+    
+    case Created:
+      return;
+    default:
+      MOZ_ASSERT(false);
+      break;
   }
 
   RecordTimeInternal(std::move(aTimeStamp));
