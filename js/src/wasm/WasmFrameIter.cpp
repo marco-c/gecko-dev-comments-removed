8034fdf8c16a4d62affb10dc891d72d0a45178d0
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 08:26:33 +0000

    Bug 1753061 - revert bug 1742053 part 10. r=rhunt
    Differential Revision: https://phabricator.services.mozilla.com/D137636
    
    UltraBlame original commit: 8dd038e0462c796adc93c6934af85c316e8fa024

--- js/src/wasm/WasmFrameIter.cpp
+++ js/src/wasm/WasmFrameIter.cpp
@@ -210,9 +210,7 @@ void WasmFrameIter::popFrame() {
   const CallSite* callsite = code_->lookupCallSite(returnAddress);
   MOZ_ASSERT(callsite);
 
-  if (callsite->isImportCall()) {
-    tls_ = ExtractCallerTlsFromFrameWithTls(prevFP);
-  } else if (callsite->isIndirectCall() && prevFP->callerIsTrampolineFP()) {
+  if (callsite->mightBeCrossInstance()) {
     tls_ = ExtractCallerTlsFromFrameWithTls(prevFP);
   }
 
@@ -978,14 +976,10 @@ const TlsData* js::wasm::GetNearestEffectiveTls(const Frame* fp) {
       return ExtractCalleeTlsFromFrameWithTls(fp);
     }
 
-    if (codeRange->isIndirectStub()) {
-      return ExtractCalleeTlsFromFrameWithTls(fp->wasmCaller());
-    }
-
     MOZ_ASSERT(codeRange->kind() == CodeRange::Function);
     MOZ_ASSERT(code);
     const CallSite* callsite = code->lookupCallSite(returnAddress);
-    if (callsite->isImportCall()) {
+    if (callsite->mightBeCrossInstance()) {
       return ExtractCalleeTlsFromFrameWithTls(fp);
     }
 
@@ -1204,16 +1198,8 @@ bool js::wasm::StartUnwinding(const RegisterState& registers,
       
       
       break;
-    case CodeRange::IndirectStub: {
-      
-      
-      fixedPC = pc;
-      fixedFP = fp;
-      *unwoundCaller = false;
-      AssertMatchesCallSite(Frame::fromUntaggedWasmExitFP(fp)->returnAddress(),
-                            Frame::fromUntaggedWasmExitFP(fp)->rawCaller());
-      break;
-    }
+    case CodeRange::IndirectStub:
+      MOZ_CRASH("NYI");
     case CodeRange::JitEntry:
       
       
