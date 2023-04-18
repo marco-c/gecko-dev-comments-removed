4891bc770fdd7c048c4a234d3de323e473c14e11
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 18:27:41 +0000

    Bug 1765167 - Part 6: Stop using Cu.import in layout/. r=mossop
    Differential Revision: https://phabricator.services.mozilla.com/D144099
    
    UltraBlame original commit: ce0a760da7757d651ea76d1c2ba14a4d650763be

--- layout/tools/reftest/reftest-content.js
+++ layout/tools/reftest/reftest-content.js
@@ -16,9 +16,13 @@ const IO_SERVICE_CONTRACTID = "@mozilla.org/network/io-service;1"
 
 const BLANK_URL_FOR_CLEARING = "data:text/html;charset=UTF-8,%3C%21%2D%2DCLEAR%2D%2D%3E";
 
-Cu.import("resource://gre/modules/Timer.jsm");
-Cu.import("resource://reftest/AsyncSpellCheckTestHelper.jsm");
-Cu.import("resource://gre/modules/Services.jsm");
+const { setTimeout, clearTimeout } = ChromeUtils.import(
+    "resource://gre/modules/Timer.jsm"
+);
+const { onSpellCheck } = ChromeUtils.import(
+    "resource://reftest/AsyncSpellCheckTestHelper.jsm"
+);
+const { Services } = ChromeUtils.import("resource://gre/modules/Services.jsm");
 
 
 ChromeUtils.import("resource://gre/modules/CustomElementsListener.jsm");
