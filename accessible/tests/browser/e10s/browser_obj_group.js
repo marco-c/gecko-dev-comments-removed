5f724dca6ede3f2d3018dd668d424f2e59c89da9
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 05:01:19 +0000

    Bug 1737944 - P12: Add better todo support to attribute tests, and todo failing cache tests. r=Jamie
    Changed how our attributes tests work a bit to be single "is" test
    instead of comparing each attribute separately. This allows passing a
    todo option without it failing on attributes that are identical.
    
    Differential Revision: https://phabricator.services.mozilla.com/D134974
    
    UltraBlame original commit: 3f25802790263235098a06d7656ff36ead49c8b1

--- accessible/tests/browser/e10s/browser_obj_group.js
+++ accessible/tests/browser/e10s/browser_obj_group.js
@@ -7,6 +7,11 @@
 
 loadScripts({ name: "attributes.js", dir: MOCHITESTS_DIR });
 
+const isCacheEnabled = Services.prefs.getBoolPref(
+  "accessibility.cache.enabled",
+  false
+);
+
 
 
 
@@ -398,7 +403,7 @@ addAccessibleTask(
     testGroupAttrs(getAcc("grid_row2"), 2, 2);
     testAbsentAttrs(getAcc("grid_cell3"), { posinset: "", setsize: "" });
     testAbsentAttrs(getAcc("grid_cell4"), { posinset: "", setsize: "" });
-    testGroupParentAttrs(getAcc("grid"), 2, false);
+    testGroupParentAttrs(getAcc("grid"), 2, false, isCacheEnabled);
   }
 );
 
@@ -436,7 +441,7 @@ addAccessibleTask(
 
     testGroupParentAttrs(getAcc("treegrid"), 2, true);
     
-    testGroupParentAttrs(getAcc("treegrid_row1"), 4, false);
+    testGroupParentAttrs(getAcc("treegrid_row1"), 4, false, isCacheEnabled);
   }
 );
 
@@ -498,13 +503,13 @@ addAccessibleTask(
 
     
     
-    testGroupAttrs(getAcc("table_cell"), 3, 4);
-    testGroupAttrs(getAcc("table_row"), 2, 2);
+    testGroupAttrs(getAcc("table_cell"), 3, 4, null, isCacheEnabled);
+    testGroupAttrs(getAcc("table_row"), 2, 2, null, isCacheEnabled);
 
     
-    testGroupParentAttrs(getAcc("table"), 2, false);
+    testGroupParentAttrs(getAcc("table"), 2, false, isCacheEnabled);
     
-    testGroupParentAttrs(getAcc("table_row"), 4, false);
+    testGroupParentAttrs(getAcc("table_row"), 4, false, isCacheEnabled);
   }
 );
 
@@ -525,8 +530,8 @@ addAccessibleTask(
     let getAcc = id => findAccessibleChildByID(accDoc, id);
 
     
-    testGroupAttrs(getAcc("wrapped_row_1"), 1, 2);
-    testGroupAttrs(getAcc("wrapped_row_2"), 2, 2);
+    testGroupAttrs(getAcc("wrapped_row_1"), 1, 2, null, isCacheEnabled);
+    testGroupAttrs(getAcc("wrapped_row_2"), 2, 2, null, isCacheEnabled);
   }
 );
 
