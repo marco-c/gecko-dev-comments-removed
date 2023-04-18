f43a61f5bfec20142484affe05c73d1edc8c8248
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 09:11:02 +0000

    Bug 1755093 - Support repo_url with no slash. r=ahal
    Differential Revision: https://phabricator.services.mozilla.com/D138609
    
    UltraBlame original commit: 954c4d8bbd3b85c24ee1eea68813afaa4e630fbe

--- third_party/python/taskcluster_taskgraph/taskgraph/parameters.py
+++ third_party/python/taskcluster_taskgraph/taskgraph/parameters.py
@@ -64,7 +64,8 @@ def _get_defaults(repo_root=None):
     try:
         repo_url = repo.get_url()
         project = repo_url.rsplit("/", 1)[1]
-    except CalledProcessError:
+    except (CalledProcessError, IndexError):
+        
         repo_url = ""
         project = ""
 
