6837967ef2a7bd8ab30b5dcfb321e2d60bb27caf
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 07:03:41 +0000

    Bug 1751154 - More consistent error handling for frameIds parameter in scripting API. r=robwu
    Differential Revision: https://phabricator.services.mozilla.com/D137005
    
    UltraBlame original commit: 7ba91106687b348ba40b46c27bb6955b105f6bd1

--- toolkit/components/extensions/parent/ext-tabs-base.js
+++ toolkit/components/extensions/parent/ext-tabs-base.js
@@ -692,14 +692,19 @@ class TabBase {
     
     let byProcess = new DefaultMap(() => []);
     let framesFound = 0;
+    
+    
+    
+    let frameIdsSet = new Set(frameIds);
 
     
     function visit(bc) {
       let win = bc.currentWindowGlobal;
       let frameId = bc.parent ? bc.id : 0;
 
-      if (win?.domProcess && (!frameIds || frameIds.includes(frameId))) {
+      if (win?.domProcess && (!frameIds || frameIdsSet.has(frameId))) {
         byProcess.get(win.domProcess).push(win.innerWindowId);
+        frameIdsSet.delete(frameId);
         framesFound++;
       }
 
@@ -708,6 +713,12 @@ class TabBase {
       }
     }
     visit(this.browsingContext);
+
+    if (frameIdsSet.size > 0) {
+      throw new ExtensionError(
+        `Invalid frame IDs: [${Array.from(frameIdsSet).join(", ")}].`
+      );
+    }
 
     let promises = Array.from(byProcess.entries(), ([proc, windows]) =>
       proc.getActor("ExtensionContent").sendQuery(message, { windows, options })
