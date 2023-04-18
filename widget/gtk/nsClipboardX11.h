facd9a5e3853b34401637ac38f3bc16738750625
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 07:52:37 +0000

    Bug 1753411 - Deduplicate a bunch of the X11 vs. Wayland clipboard code. r=stransky
    Differential Revision: https://phabricator.services.mozilla.com/D137735
    
    UltraBlame original commit: 26e996ca80e3fac537b0b8e42aa36cec2e053c54

--- widget/gtk/nsClipboardX11.h
+++ widget/gtk/nsClipboardX11.h
@@ -15,56 +15,18 @@
 
 class nsRetrievalContextX11 : public nsRetrievalContext {
  public:
-  enum class State { Initial, Completed, TimedOut };
-
   ClipboardData GetClipboardData(const char* aMimeType,
                                  int32_t aWhichClipboard) override;
   mozilla::GUniquePtr<char> GetClipboardText(int32_t aWhichClipboard) override;
-
   ClipboardTargets GetTargets(int32_t aWhichClipboard) override;
-
-  bool HasSelectionSupport(void) override;
-
-  
-  void Complete(ClipboardDataType aDataType, const void* aData,
-                int aDataRequestNumber);
 
   nsRetrievalContextX11();
 
  private:
+  
   ClipboardData WaitForClipboardData(ClipboardDataType aDataType,
                                      int32_t aWhichClipboard,
                                      const char* aMimeType = nullptr);
-
-  
-
-
-
-
-  ClipboardData WaitForX11Content();
-
-  State mState = State::Initial;
-  int mClipboardRequestNumber = 0;
-  mozilla::Maybe<ClipboardData> mClipboardData;
-  GdkAtom mTargetMIMEType;
-};
-
-class ClipboardRequestHandler {
- public:
-  ClipboardRequestHandler(nsRetrievalContextX11* aContext,
-                          ClipboardDataType aDataType, int aDataRequestNumber)
-      : mContext(aContext),
-        mDataRequestNumber(aDataRequestNumber),
-        mDataType(aDataType) {}
-
-  void Complete(const void* aData) {
-    mContext->Complete(mDataType, aData, mDataRequestNumber);
-  }
-
- private:
-  nsRetrievalContextX11* mContext;
-  int mDataRequestNumber;
-  ClipboardDataType mDataType;
 };
 
 #endif 
