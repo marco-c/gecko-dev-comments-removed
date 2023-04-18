4ccf1edbfec9bcd6593ce4c0665341ebdb717aab
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 07:45:55 +0000

    Bug 1753310 - Fix process CPU times reported by about:processes, r=gerald.
    Differential Revision: https://phabricator.services.mozilla.com/D137698
    
    UltraBlame original commit: 61491ef8a39c90992e711ff57d95f639cd00d506

--- toolkit/components/processtools/ProcInfo.mm
+++ toolkit/components/processtools/ProcInfo.mm
@@ -17,6 +17,15 @@
 #include <libproc.h>
 #include <sys/sysctl.h>
 #include <mach/mach.h>
+#include <mach/mach_time.h>
+
+static void GetTimeBase(mach_timebase_info_data_t* timebase) {
+  
+  if (mach_timebase_info(timebase) != KERN_SUCCESS) {
+    timebase->numer = 1;
+    timebase->denom = 1;
+  }
+}
 
 namespace mozilla {
 
@@ -26,7 +35,12 @@ nsresult GetCpuTimeSinceProcessStartInMs(uint64_t* aResult) {
       PROC_PIDTASKINFO_SIZE) {
     return NS_ERROR_FAILURE;
   }
-  *aResult = (pti.pti_total_user + pti.pti_total_system) / PR_NSEC_PER_MSEC;
+
+  mach_timebase_info_data_t timebase;
+  GetTimeBase(&timebase);
+
+  *aResult = (pti.pti_total_user + pti.pti_total_system) * timebase.numer / timebase.denom /
+             PR_NSEC_PER_MSEC;
   return NS_OK;
 }
 
@@ -53,6 +67,10 @@ ProcInfoPromise::ResolveOrRejectValue GetProcInfoSync(nsTArray<ProcInfoRequest>&
     result.SetReject(NS_ERROR_OUT_OF_MEMORY);
     return result;
   }
+
+  mach_timebase_info_data_t timebase;
+  GetTimeBase(&timebase);
+
   for (const auto& request : aRequests) {
     ProcInfo info;
     info.pid = request.pid;
@@ -69,7 +87,7 @@ ProcInfoPromise::ResolveOrRejectValue GetProcInfoSync(nsTArray<ProcInfoRequest>&
       
       continue;
     }
-    info.cpuTime = pti.pti_total_user + pti.pti_total_system;
+    info.cpuTime = (pti.pti_total_user + pti.pti_total_system) * timebase.numer / timebase.denom;
 
     mach_port_t selectedTask;
     
