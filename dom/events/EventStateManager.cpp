b8a5e01ac5c1355f3c08af21820f4f8213bcce70
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 03:39:42 +0000

    Bug 1539884 - Part 9: Mark EventStateManager::PostHandleKeyboardEvent as CAN_RUN_SCRIPT r=masayuki
    Depends on D133919
    
    Differential Revision: https://phabricator.services.mozilla.com/D133920
    
    UltraBlame original commit: de9e2829d42ad5e538e48a18a8f93c2cdd37b77d

--- dom/events/EventStateManager.cpp
+++ dom/events/EventStateManager.cpp
@@ -3186,6 +3186,8 @@ void EventStateManager::PostHandleKeyboardEvent(
     return;
   }
 
+  RefPtr<nsPresContext> presContext = mPresContext;
+
   if (!aKeyboardEvent->HasBeenPostedToRemoteProcess()) {
     if (aKeyboardEvent->IsWaitingReplyFromRemoteProcess()) {
       RefPtr<BrowserParent> remote =
@@ -3210,8 +3212,8 @@ void EventStateManager::PostHandleKeyboardEvent(
         
         WidgetKeyboardEvent keyEvent(*aKeyboardEvent);
         aKeyboardEvent->MarkAsHandledInRemoteProcess();
-        EventDispatcher::Dispatch(remote->GetOwnerElement(), mPresContext,
-                                  &keyEvent);
+        RefPtr<Element> ownerElement = remote->GetOwnerElement();
+        EventDispatcher::Dispatch(ownerElement, presContext, &keyEvent);
         if (keyEvent.DefaultPrevented()) {
           aKeyboardEvent->PreventDefault(!keyEvent.DefaultPreventedByContent());
           aStatus = nsEventStatus_eConsumeNoDefault;
@@ -3249,7 +3251,7 @@ void EventStateManager::PostHandleKeyboardEvent(
           break;
         }
 
-        EnsureDocument(mPresContext);
+        EnsureDocument(presContext);
         nsFocusManager* fm = nsFocusManager::GetFocusManager();
         if (fm && mDocument) {
           
