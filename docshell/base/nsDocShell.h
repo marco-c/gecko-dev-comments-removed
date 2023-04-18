29fb3e2be68142f82c4c00886b12423e32ef51e9
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 22:39:21 +0000

    Bug 1770403, part 2 - Remove mStorageAccessPermissionGranted from the outer window, r=anti-tracking-reviewers,timhuang
    With the use of mStorageAccessPermissionGranted reduced to a single meaning, it turns out to be entirely redundant with the variable on the inner window.
    
    Depends on D147867
    
    Differential Revision: https://phabricator.services.mozilla.com/D147868
    
    UltraBlame original commit: 1723828ce4bb583678058384ec0476d7e77addfc

--- docshell/base/nsDocShell.h
+++ docshell/base/nsDocShell.h
@@ -401,9 +401,6 @@ class nsDocShell final : public nsDocLoader,
       nsDocShellLoadState* aLoadState,
       mozilla::Maybe<uint32_t> aCacheKey = mozilla::Nothing());
 
-  
-  void MaybeClearStorageAccessFlag();
-
   void MaybeRestoreWindowName();
 
   void StoreWindowNameToSHEntries();
