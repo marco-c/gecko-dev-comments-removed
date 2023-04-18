4eaedf8506ebc9fa0bef4b0d354e2ed8921cff2b
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 05:25:18 +0000

    Bug 1749329 - Record GC time spent on helper threads r=sfink
    This records parallel task time as part of the current GC slice and reports it
    in the JS_GC_PROFILE output, plus some misc tidyup.
    
    Differential Revision: https://phabricator.services.mozilla.com/D135524
    
    UltraBlame original commit: 8a46f8c8288a212df86842080fb4ab90f6ad2de7

--- js/src/gc/Statistics.h
+++ js/src/gc/Statistics.h
@@ -144,6 +144,8 @@ struct Statistics {
   using PhaseKindTimes =
       EnumeratedArray<PhaseKind, PhaseKind::LIMIT, TimeDuration>;
 
+  using PhaseTimeStamps = EnumeratedArray<Phase, Phase::LIMIT, TimeStamp>;
+
   [[nodiscard]] static bool initialize();
 
   explicit Statistics(gc::GCRuntime* gc);
@@ -272,6 +274,7 @@ struct Statistics {
     size_t startFaults = 0;
     size_t endFaults = 0;
     PhaseTimes phaseTimes;
+    PhaseKindTimes totalParallelTimes;
     PhaseKindTimes maxParallelTimes;
 
     TimeDuration duration() const { return end - start; }
@@ -334,11 +337,11 @@ struct Statistics {
   SliceDataVector slices_;
 
   
-  EnumeratedArray<Phase, Phase::LIMIT, TimeStamp> phaseStartTimes;
+  PhaseTimeStamps phaseStartTimes;
 
 #ifdef DEBUG
   
-  EnumeratedArray<Phase, Phase::LIMIT, TimeStamp> phaseEndTimes;
+  PhaseTimeStamps phaseEndTimes;
 #endif
 
   TimeStamp creationTime_;
@@ -349,6 +352,9 @@ struct Statistics {
 
   
   PhaseTimes phaseTimes;
+
+  
+  PhaseKindTimes parallelTimes;
 
   
   EnumeratedArray<Count, COUNT_LIMIT,
@@ -419,6 +425,7 @@ struct Statistics {
 
   enum class ProfileKey {
     Total,
+    Background,
 #define DEFINE_TIME_KEY(name, text, phase) name,
     FOR_EACH_GC_PROFILE_TIME(DEFINE_TIME_KEY)
 #undef DEFINE_TIME_KEY
