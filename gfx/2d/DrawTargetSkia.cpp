a364d0196a61ea1058ba70a3615b35ce26ccd4ae
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 22:31:50 +0000

    Bug 1746356 - Null-check the result of CreateSimilarDrawTarget. r=gfx-reviewers,lsalzman
    Differential Revision: https://phabricator.services.mozilla.com/D148512
    
    UltraBlame original commit: af13e4c71c8d74eb08db3135f418b9588950ea35

--- gfx/2d/DrawTargetSkia.cpp
+++ gfx/2d/DrawTargetSkia.cpp
@@ -1596,9 +1596,13 @@ RefPtr<DrawTarget> DrawTargetSkia::CreateClippedDrawTarget(
   if (mCanvas->getDeviceClipBounds(&clipBounds)) {
     RefPtr<DrawTarget> dt = CreateSimilarDrawTarget(
         IntSize(clipBounds.width(), clipBounds.height()), aFormat);
-    result = gfx::Factory::CreateOffsetDrawTarget(
-        dt, IntPoint(clipBounds.x(), clipBounds.y()));
-    result->SetTransform(mTransform);
+    if (dt) {
+      result = gfx::Factory::CreateOffsetDrawTarget(
+          dt, IntPoint(clipBounds.x(), clipBounds.y()));
+      if (result) {
+        result->SetTransform(mTransform);
+      }
+    }
   } else {
     
     result = CreateSimilarDrawTarget(IntSize(1, 1), aFormat);
