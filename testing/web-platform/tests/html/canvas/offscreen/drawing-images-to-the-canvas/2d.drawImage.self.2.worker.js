5503bd1f1b1517bc07a1c7bb4e5d9a930b133a4d
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 15:08:49 +0000

    Bug 1760642 [wpt PR 33286] - Unify wpt test drawing-images-to-the-canvas, a=testonly
    Automatic update from web-platform-tests
    Unify wpt test drawing-images-to-the-canvas
    
    Unify wpt test drawing-images-to-the-canvas for both HTMLCanvas and
    OffscreenCanvas.
    
    Bug: 1275750
    
    Change-Id: I5eee4f187ad09214222e985d3b001168fdcc8871
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3536424
    Reviewed-by: Juanmi Huertas <juanmihdchromium.org>
    Commit-Queue: Yi Xu <yiyixchromium.org>
    Cr-Commit-Position: refs/heads/main{#984188}
    
    --
    
    wpt-commits: f917fa0fbbbdb64ea38382e2f69d5168ac4fc40c
    wpt-pr: 33286
    
    UltraBlame original commit: 3193e26b77a6a869290f3d4ff70581a3ec75412f

--- testing/web-platform/tests/html/canvas/offscreen/drawing-images-to-the-canvas/2d.drawImage.self.2.worker.js
+++ testing/web-platform/tests/html/canvas/offscreen/drawing-images-to-the-canvas/2d.drawImage.self.2.worker.js
@@ -3,7 +3,6 @@
 
 
 
-
 importScripts("/resources/testharness.js");
 importScripts("/html/canvas/resources/canvas-tests.js");
 
@@ -24,6 +23,7 @@ ctx.fillRect(0, 0, 100, 1);
 ctx.drawImage(canvas, 0, 1);
 ctx.fillStyle = '#0f0';
 ctx.fillRect(0, 0, 100, 2);
+
 _assertPixelApprox(canvas, 0,0, 0,255,0,255, "0,0", "0,255,0,255", 2);
 _assertPixelApprox(canvas, 99,0, 0,255,0,255, "99,0", "0,255,0,255", 2);
 _assertPixelApprox(canvas, 0,49, 0,255,0,255, "0,49", "0,255,0,255", 2);
