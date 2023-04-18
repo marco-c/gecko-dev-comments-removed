ffbe9142030ee759221b8f3b5eeb88e3c796aab5
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 22:53:37 +0000

    Bug 1772169 [wpt PR 34278] - Revamp object-not-found redirect resource timing tests, a=testonly
    Automatic update from web-platform-tests
    Revamp object-not-found redirect resource timing tests
    
    I suspect that the object resource timing tests are flakily timing out
    due to flakiness in <object>'s events themselves.
    
    This CL moves these tests to the more recent resource timing testing
    infrastructure, and also ends the test once a PerformanceObserver runs
    its callback.
    
    Bug: 1318592
    Change-Id: I2b01188756b212b51b8c8d095a2257d4a0e54e91
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3682424
    Commit-Queue: Yoav Weiss <yoavweisschromium.org>
    Reviewed-by: Ian Clelland <iclellandchromium.org>
    Cr-Commit-Position: refs/heads/main{#1010553}
    
    --
    
    wpt-commits: c7adce6fab1cd754f203a5cd012e8745961b4308
    wpt-pr: 34278
    
    UltraBlame original commit: 342a7420aa11166aa413fd87edf22ec0edb5c14e

--- testing/web-platform/tests/resource-timing/resources/resource-loaders.js
+++ testing/web-platform/tests/resource-timing/resources/resource-loaders.js
@@ -98,6 +98,9 @@ const load = {
     const object = document.createElement("object");
     const loaded = new Promise(resolve => {
       object.onload = object.onerror = resolve;
+      
+      
+      (new PerformanceObserver(resolve)).observe({type: "resource"});
     });
     object.data = load.cache_bust(path);
     if (type) {
