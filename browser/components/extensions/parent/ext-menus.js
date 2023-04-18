3e29a5ad004addfe475a68fe43786d7e3a7aac9f
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 16:24:48 +0000

    Bug 1762394 menu startupCache persistence r=robwu
    Add a StartupCache for menus so they are recreated when extensions use event pages.
    
    Differential Revision: https://phabricator.services.mozilla.com/D139789
    
    UltraBlame original commit: 9158cfea79125b0052f1b1983ecc49cc4eca2022

--- browser/components/extensions/parent/ext-menus.js
+++ browser/components/extensions/parent/ext-menus.js
@@ -24,7 +24,7 @@ var { ExtensionParent } = ChromeUtils.import(
   "resource://gre/modules/ExtensionParent.jsm"
 );
 
-var { IconDetails } = ExtensionParent;
+var { IconDetails, StartupCache } = ExtensionParent;
 
 const ACTION_MENU_TOP_LEVEL_LIMIT = 6;
 
@@ -32,6 +32,12 @@ const ACTION_MENU_TOP_LEVEL_LIMIT = 6;
 
 
 var gMenuMap = new Map();
+
+
+
+
+
+var gStartupCache = new Map();
 
 
 var gRootItems = new Map();
@@ -711,38 +717,42 @@ function addMenuEventInfo(info, contextData, extension, includeSensitiveData) {
   }
 }
 
-function MenuItem(extension, createProperties, isRoot = false) {
-  this.extension = extension;
-  this.children = [];
-  this.parent = null;
-  this.tabManager = extension.tabManager;
+class MenuItem {
+  constructor(extension, createProperties, isRoot = false) {
+    this.extension = extension;
+    this.children = [];
+    this.parent = null;
+    this.tabManager = extension.tabManager;
 
-  this.setDefaults();
-  this.setProps(createProperties);
+    this.setDefaults();
+    this.setProps(createProperties);
 
-  if (!this.hasOwnProperty("_id")) {
-    this.id = gNextMenuItemID++;
+    if (!this.hasOwnProperty("_id")) {
+      this.id = gNextMenuItemID++;
+    }
+    
+    
+    if (!isRoot && !this.parent) {
+      this.root.addChild(this);
+    }
   }
-  
-  
-  if (!isRoot && !this.parent) {
-    this.root.addChild(this);
-  }
-}
 
-MenuItem.prototype = {
-  setProps(createProperties) {
-    for (let propName in createProperties) {
-      if (createProperties[propName] === null) {
+  static mergeProps(obj, properties) {
+    for (let propName in properties) {
+      if (properties[propName] === null) {
         
         continue;
       }
-      this[propName] = createProperties[propName];
+      obj[propName] = properties[propName];
     }
 
-    if ("icons" in createProperties && createProperties.icons === null) {
-      this.icons = null;
+    if ("icons" in properties && properties.icons === null && obj.icons) {
+      obj.icons = null;
     }
+  }
+
+  setProps(createProperties) {
+    MenuItem.mergeProps(this, createProperties);
 
     if (createProperties.documentUrlPatterns != null) {
       this.documentUrlMatchPattern = parseMatchPatterns(
@@ -766,7 +776,7 @@ MenuItem.prototype = {
     if (createProperties.parentId && !createProperties.contexts) {
       this.contexts = this.parent.contexts;
     }
-  },
+  }
 
   setDefaults() {
     this.setProps({
@@ -776,7 +786,7 @@ MenuItem.prototype = {
       enabled: true,
       visible: true,
     });
-  },
+  }
 
   set id(id) {
     if (this.hasOwnProperty("_id")) {
@@ -787,11 +797,11 @@ MenuItem.prototype = {
       throw new ExtensionError(`ID already exists: ${id}`);
     }
     this._id = id;
-  },
+  }
 
   get id() {
     return this._id;
-  },
+  }
 
   get elementId() {
     let id = this.id;
@@ -803,7 +813,7 @@ MenuItem.prototype = {
       id = `_${id}`;
     }
     return `${makeWidgetId(this.extension.id)}-menuitem-${id}`;
-  },
+  }
 
   ensureValidParentId(parentId) {
     if (parentId === undefined) {
@@ -822,7 +832,25 @@ MenuItem.prototype = {
         );
       }
     }
-  },
+  }
+
+  
+
+
+
+
+
+  reparentInCache() {
+    let { id, extension } = this;
+    let cachedMap = gStartupCache.get(extension);
+    let createProperties = cachedMap.get(id);
+    cachedMap.delete(id);
+    cachedMap.set(id, createProperties);
+
+    for (let child of this.children) {
+      child.reparentInCache();
+    }
+  }
 
   set parentId(parentId) {
     this.ensureValidParentId(parentId);
@@ -837,11 +865,11 @@ MenuItem.prototype = {
       let menuMap = gMenuMap.get(this.extension);
       menuMap.get(parentId).addChild(this);
     }
-  },
+  }
 
   get parentId() {
     return this.parent ? this.parent.id : undefined;
-  },
+  }
 
   addChild(child) {
     if (child.parent) {
@@ -849,7 +877,7 @@ MenuItem.prototype = {
     }
     this.children.push(child);
     child.parent = this;
-  },
+  }
 
   detachChild(child) {
     let idx = this.children.indexOf(child);
@@ -858,7 +886,7 @@ MenuItem.prototype = {
     }
     this.children.splice(idx, 1);
     child.parent = null;
-  },
+  }
 
   get root() {
     let extension = this.extension;
@@ -872,7 +900,7 @@ MenuItem.prototype = {
     }
 
     return gRootItems.get(extension);
-  },
+  }
 
   remove() {
     if (this.parent) {
@@ -885,10 +913,14 @@ MenuItem.prototype = {
 
     let menuMap = gMenuMap.get(this.extension);
     menuMap.delete(this.id);
+    
+    if (gStartupCache.get(this.extension)?.delete(this.id)) {
+      StartupCache.save();
+    }
     if (this.root == this) {
       gRootItems.delete(this.extension);
     }
-  },
+  }
 
   getClickInfo(contextData, wasChecked) {
     let info = {
@@ -906,7 +938,7 @@ MenuItem.prototype = {
     }
 
     return info;
-  },
+  }
 
   enabledForContext(contextData) {
     if (!this.visible) {
@@ -968,8 +1000,8 @@ MenuItem.prototype = {
     }
 
     return true;
-  },
-};
+  }
+}
 
 
 
@@ -1199,6 +1231,38 @@ this.menusInternal = class extends ExtensionAPIPersistent {
     gMenuMap.set(extension, new Map());
   }
 
+  restoreFromCache() {
+    let { extension } = this;
+    
+    if (!this.extension) {
+      return;
+    }
+    for (let createProperties of gStartupCache.get(extension).values()) {
+      
+      let menuItem = new MenuItem(extension, createProperties);
+      gMenuMap.get(extension).set(menuItem.id, menuItem);
+    }
+    
+    extension.emit("webext-menus-created", gMenuMap.get(extension));
+  }
+
+  async onStartup() {
+    let { extension } = this;
+    if (extension.persistentBackground) {
+      return;
+    }
+    
+    let cachedMenus = await StartupCache.menus.get(extension.id, () => {
+      return new Map();
+    });
+    gStartupCache.set(extension, cachedMenus);
+    if (!cachedMenus.size) {
+      return;
+    }
+
+    this.restoreFromCache();
+  }
+
   onShutdown() {
     let { extension } = this;
 
@@ -1206,6 +1270,7 @@ this.menusInternal = class extends ExtensionAPIPersistent {
       gMenuMap.delete(extension);
       gRootItems.delete(extension);
       gShownMenuItems.delete(extension);
+      gStartupCache.delete(extension);
       gOnShownSubscribers.delete(extension);
       if (!gMenuMap.size) {
         menuTracker.unregister();
@@ -1333,7 +1398,7 @@ this.menusInternal = class extends ExtensionAPIPersistent {
       contextMenus: menus,
       menus,
       menusInternal: {
-        create: function(createProperties) {
+        create(createProperties) {
           
           if (!extension.persistentBackground) {
             if (!createProperties.id) {
@@ -1353,26 +1418,58 @@ this.menusInternal = class extends ExtensionAPIPersistent {
           
           let menuItem = new MenuItem(extension, createProperties);
           gMenuMap.get(extension).set(menuItem.id, menuItem);
-        },
-
-        update: function(id, updateProperties) {
-          let menuItem = gMenuMap.get(extension).get(id);
-          if (menuItem) {
-            menuItem.setProps(updateProperties);
+          if (!extension.persistentBackground) {
+            
+            let cached = {};
+            MenuItem.mergeProps(cached, createProperties);
+            gStartupCache.get(extension).set(menuItem.id, cached);
+            StartupCache.save();
           }
         },
 
-        remove: function(id) {
+        update(id, updateProperties) {
+          let menuItem = gMenuMap.get(extension).get(id);
+          if (!menuItem) {
+            return;
+          }
+          menuItem.setProps(updateProperties);
+
+          
+          if (extension.persistentBackground) {
+            return;
+          }
+
+          let cached = gStartupCache.get(extension).get(id);
+          let reparent =
+            updateProperties.parentId != null &&
+            cached.parentId != updateProperties.parentId;
+          MenuItem.mergeProps(cached, updateProperties);
+          if (reparent) {
+            
+            menuItem.reparentInCache();
+          }
+          StartupCache.save();
+        },
+
+        remove(id) {
           let menuItem = gMenuMap.get(extension).get(id);
           if (menuItem) {
             menuItem.remove();
           }
         },
 
-        removeAll: function() {
+        removeAll() {
           let root = gRootItems.get(extension);
           if (root) {
             root.remove();
+          }
+          
+          if (!extension.persistentBackground) {
+            let cached = gStartupCache.get(extension);
+            if (cached.size) {
+              cached.clear();
+              StartupCache.save();
+            }
           }
         },
 
