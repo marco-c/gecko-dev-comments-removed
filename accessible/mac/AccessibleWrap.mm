4f7c2fe5e4902bfe2a61872bed2f7631b3fb54bb
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 11:41:11 +0000

    Bug 1749633 - Return early if event target is defunct. r=morgan
    Differential Revision: https://phabricator.services.mozilla.com/D140884
    
    UltraBlame original commit: e15ca665c2633e70e4ec839ee8b434bf1e755f1f

--- accessible/mac/AccessibleWrap.mm
+++ accessible/mac/AccessibleWrap.mm
@@ -145,6 +145,11 @@ nsresult AccessibleWrap::HandleAccEvent(AccEvent* aEvent) {
   nsresult rv = LocalAccessible::HandleAccEvent(aEvent);
   NS_ENSURE_SUCCESS(rv, rv);
 
+  if (IsDefunct()) {
+    
+    return NS_OK;
+  }
+
   uint32_t eventType = aEvent->GetEventType();
 
   if (eventType == nsIAccessibleEvent::EVENT_SHOW) {
