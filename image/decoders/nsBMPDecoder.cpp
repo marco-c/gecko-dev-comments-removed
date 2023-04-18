6e7ee440225a945dc8c43448bd3afb31679e8f7f
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 18:49:32 +0000

    Bug 1757929 - Avoid some conflicts with WINDOWS define. r=tnikkel
    Differential Revision: https://phabricator.services.mozilla.com/D145569
    
    UltraBlame original commit: 439d865b46fba788b8248a9f3119d0c7b759a75b

--- image/decoders/nsBMPDecoder.cpp
+++ image/decoders/nsBMPDecoder.cpp
@@ -570,7 +570,7 @@ LexerTransition<nsBMPDecoder::State> nsBMPDecoder::ReadInfoHeaderRest(
         break;
       case InfoColorSpace::LINKED:
       case InfoColorSpace::SRGB:
-      case InfoColorSpace::WINDOWS:
+      case InfoColorSpace::WIN:
       default:
         
         break;
@@ -731,7 +731,7 @@ LexerTransition<nsBMPDecoder::State> nsBMPDecoder::ReadBitfields(
         PrepareCalibratedColorProfile();
         break;
       case InfoColorSpace::SRGB:
-      case InfoColorSpace::WINDOWS:
+      case InfoColorSpace::WIN:
         MOZ_LOG(sBMPLog, LogLevel::Debug, ("using sRGB color profile\n"));
         if (mColors) {
           
