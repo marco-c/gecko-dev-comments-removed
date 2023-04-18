39e28b87a4284ceb28fa901f36885a88792e9f2d
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 09:33:47 +0000

    Bug 1741671 - Enable BFCache for pages with beforeunload event listeners on Desktop r=smaug
    The changes only made it works in SHIP(session-history-in-parent) only.
    
    Differential Revision: https://phabricator.services.mozilla.com/D131715
    
    UltraBlame original commit: aa8767c463fc058f0d5810f331b1ee1784a0396a

--- dom/tests/browser/browser_hasbeforeunload.js
+++ dom/tests/browser/browser_hasbeforeunload.js
@@ -408,6 +408,12 @@ function assertHasBeforeUnload(browser, expected) {
 
 
 add_task(async function test_inner_window_scenarios() {
+  
+  await SpecialPowers.pushPrefEnv({
+    set: [
+      ["docshell.shistory.bfcache.ship_allow_beforeunload_listeners", false],
+    ],
+  });
   await BrowserTestUtils.withNewTab(
     {
       gBrowser,
@@ -592,6 +598,12 @@ add_task(async function test_inner_window_scenarios() {
 
 
 add_task(async function test_outer_window_scenarios() {
+  
+  await SpecialPowers.pushPrefEnv({
+    set: [
+      ["docshell.shistory.bfcache.ship_allow_beforeunload_listeners", false],
+    ],
+  });
   await BrowserTestUtils.withNewTab(
     {
       gBrowser,
@@ -787,6 +799,12 @@ add_task(async function test_outer_window_scenarios() {
 
 
 add_task(async function test_mixed_inner_and_outer_window_scenarios() {
+  
+  await SpecialPowers.pushPrefEnv({
+    set: [
+      ["docshell.shistory.bfcache.ship_allow_beforeunload_listeners", false],
+    ],
+  });
   await BrowserTestUtils.withNewTab(
     {
       gBrowser,
