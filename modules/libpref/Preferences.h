d90053024761b87d28c008857f766aabded6b81a
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 03:18:47 +0000

    Bug 1743079 - Remove ResetUserPrefs(), r=KrisWright
    Differential Revision: https://phabricator.services.mozilla.com/D133468
    
    UltraBlame original commit: 757d5a9de5a3b8d9c35630318a67403c64fadda1

--- modules/libpref/Preferences.h
+++ modules/libpref/Preferences.h
@@ -445,6 +445,8 @@ class Preferences final : public nsIPrefService,
   nsresult SavePrefFileInternal(nsIFile* aFile, SaveMethod aSaveMethod);
   nsresult WritePrefFile(nsIFile* aFile, SaveMethod aSaveMethod);
 
+  nsresult ResetUserPrefs();
+
   
   
  public:
