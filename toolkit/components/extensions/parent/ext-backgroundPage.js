192f5cbfe7c4dea679da863d350b56dad9cd8294
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 00:05:40 +0000

    Bug 1728326 - Rename internal WebExtensions events related to the background script (from background-page into background-script). r=zombie
    Differential Revision: https://phabricator.services.mozilla.com/D124783
    
    UltraBlame original commit: 39267f23ee633c754ebe46e07ca615dd5972887b

--- toolkit/components/extensions/parent/ext-backgroundPage.js
+++ toolkit/components/extensions/parent/ext-backgroundPage.js
@@ -83,7 +83,7 @@ class BackgroundPage extends HiddenExtensionPage {
       if (extension.persistentListeners) {
         EventManager.clearPrimedListeners(this.extension, false);
       }
-      extension.emit("background-page-aborted");
+      extension.emit("background-script-aborted");
       return;
     }
 
@@ -103,7 +103,7 @@ class BackgroundPage extends HiddenExtensionPage {
       EventManager.clearPrimedListeners(extension, !!this.extension);
     }
 
-    extension.emit("background-page-started");
+    extension.emit("background-script-started");
   }
 
   shutdown() {
@@ -175,8 +175,7 @@ class BackgroundWorker {
         EventManager.clearPrimedListeners(this.extension, false);
       }
 
-      
-      extension.emit("background-page-aborted");
+      extension.emit("background-script-aborted");
       return;
     }
 
@@ -194,8 +193,7 @@ class BackgroundWorker {
       EventManager.clearPrimedListeners(extension, !!this.extension);
     }
 
-    
-    extension.emit("background-page-started");
+    extension.emit("background-script-started");
   }
 
   shutdown(isAppShutdown) {
@@ -286,18 +284,18 @@ this.backgroundPage = class extends ExtensionAPI {
     
     let bgStartupPromise = new Promise(resolve => {
       let done = () => {
-        extension.off("background-page-started", done);
-        extension.off("background-page-aborted", done);
+        extension.off("background-script-started", done);
+        extension.off("background-script-aborted", done);
         extension.off("shutdown", done);
         resolve();
       };
-      extension.on("background-page-started", done);
-      extension.on("background-page-aborted", done);
+      extension.on("background-script-started", done);
+      extension.on("background-script-aborted", done);
       extension.on("shutdown", done);
     });
 
     extension.wakeupBackground = () => {
-      extension.emit("background-page-event");
+      extension.emit("background-script-event");
       extension.wakeupBackground = () => bgStartupPromise;
       return bgStartupPromise;
     };
@@ -308,7 +306,7 @@ this.backgroundPage = class extends ExtensionAPI {
 
     EventManager.primeListeners(extension);
 
-    extension.once("start-background-page", async () => {
+    extension.once("start-background-script", async () => {
       if (!this.extension) {
         
         
@@ -324,13 +322,13 @@ this.backgroundPage = class extends ExtensionAPI {
     
     
     
-    extension.once("background-page-event", async () => {
+    extension.once("background-script-event", async () => {
       await ExtensionParent.browserPaintedPromise;
-      extension.emit("start-background-page");
+      extension.emit("start-background-script");
     });
 
     ExtensionParent.browserStartupPromise.then(() => {
-      extension.emit("start-background-page");
+      extension.emit("start-background-script");
     });
   }
 
