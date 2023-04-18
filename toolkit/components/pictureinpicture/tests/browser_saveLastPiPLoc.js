17b4708fa187ee6ba987e0771a6592c5e7534e71
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 23:24:07 +0000

    Backed out changeset b616eb2e52ea (bug 1773813) for causing mochitest failures on browser_saveLastPiPLoc.js
    UltraBlame original commit: ddd43ecbbcf4e586d09e37bff361f7a6d55a0f92

--- toolkit/components/pictureinpicture/tests/browser_saveLastPiPLoc.js
+++ toolkit/components/pictureinpicture/tests/browser_saveLastPiPLoc.js
@@ -9,7 +9,8 @@
 
 
 
-async function doTest() {
+
+add_task(async () => {
   await BrowserTestUtils.withNewTab(
     {
       url: TEST_PAGE,
@@ -358,13 +359,4 @@ async function doTest() {
       await ensureMessageAndClosePiP(browser, "with-controls", pipWin, true);
     }
   );
-}
-
-add_task(async function test_pip_save_last_loc() {
-  await doTest();
-});
-
-add_task(async function test_pip_save_last_loc_with_os_zoom() {
-  await SpecialPowers.pushPrefEnv({ set: [["ui.textScaleFactor", 200]] });
-  await doTest();
 });
