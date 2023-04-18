f1521e76cdedf630b93b4dfb8efde37153a73219
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 17:16:53 +0000

    Bug 1743834 - Shut down the AudioSink when audio is muted. r=alwu,media-playback-reviewers
    This does the following:
    - When the media is muted, shut down and release the AudioSink ;
    - While the media is muted, use the system clock to make video advance ;
    - Each time the clock is queried, check if some audio packets should be
    discarded because they are in the past, compared to the media time ;
    - While muted, if the audio finished, resolve the EndedPromise ;
    - When the media is un-muted, a new AudioSink is created, and its clock starts
    to be in use.
    
    This works well and A/V sync is correct, but a micro-stuttering is perceptible
    on the video when looking carefully, because of the time it takes to open the
    audio stream. This is fixed in subsequent patches.
    
    Differential Revision: https://phabricator.services.mozilla.com/D136234
    
    UltraBlame original commit: 048e93d0a274fab7d58e5b601cbe55b911ae9c00

--- dom/media/mediasink/AudioSink.cpp
+++ dom/media/mediasink/AudioSink.cpp
@@ -140,7 +140,7 @@ void AudioSink::ReenqueueUnplayedAudioDataIfNeeded() {
 
   AlignedAudioBuffer queuedAudio(sampleCount);
   DebugOnly<int> samplesRead =
-    mProcessedSPSCQueue->Dequeue(queuedAudio.Data(), sampleCount);
+      mProcessedSPSCQueue->Dequeue(queuedAudio.Data(), sampleCount);
   MOZ_ASSERT(samplesRead == sampleCount);
 
   
@@ -149,9 +149,14 @@ void AudioSink::ReenqueueUnplayedAudioDataIfNeeded() {
   
   RefPtr<AudioData> frontPacket = mAudioQueue.PeekFront();
   RefPtr<AudioData> data =
-      new AudioData(frontPacket->mOffset, frontPacket->mTime - duration, std::move(queuedAudio),
-                    channelCount, rate);
+      new AudioData(frontPacket->mOffset, frontPacket->mTime - duration,
+                    std::move(queuedAudio), channelCount, rate);
   MOZ_DIAGNOSTIC_ASSERT(duration == data->mDuration, "must be equal");
+
+  SINK_LOG(
+      "Muting: Pushing back %u frames (%lfms) from the ring buffer back into "
+      "the audio queue",
+      frameCount, static_cast<float>(frameCount) / rate);
 
   mAudioQueue.PushFront(data);
 }
