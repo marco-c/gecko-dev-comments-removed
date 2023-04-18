f2e450a9367ce07fe519fa5ffea4dc6ff6261107
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 15:20:11 +0000

    Bug 1759858 - Make gBrowser.tabContainer.on_drop handle adoptTab failures. r=Gijs
    Differential Revision: https://phabricator.services.mozilla.com/D141237
    
    UltraBlame original commit: 1a63c7a2d4ea1e816e54bac7c397401acbe37c32

--- browser/base/content/tabbrowser-tabs.js
+++ browser/base/content/tabbrowser-tabs.js
@@ -752,26 +752,37 @@
       } else if (draggedTab) {
         
         
-        let dropIndex = this._getDropIndex(event, false);
+        const dropIndex = this._getDropIndex(event, false);
         let newIndex = dropIndex;
-        let selectedIndex = -1;
+        let selectedTab;
+        let indexForSelectedTab;
         for (let i = 0; i < movingTabs.length; ++i) {
-          let tab = movingTabs[i];
+          const tab = movingTabs[i];
           if (tab.selected) {
-            selectedIndex = i;
+            selectedTab = tab;
+            indexForSelectedTab = newIndex;
           } else {
-            gBrowser.adoptTab(tab, newIndex++, tab == draggedTab);
+            const newTab = gBrowser.adoptTab(tab, newIndex, tab == draggedTab);
+            if (newTab) {
+              ++newIndex;
+            }
           }
         }
-        if (selectedIndex >= 0) {
-          let tab = movingTabs[selectedIndex];
-          gBrowser.adoptTab(tab, dropIndex + selectedIndex, tab == draggedTab);
+        if (selectedTab) {
+          const newTab = gBrowser.adoptTab(
+            selectedTab,
+            indexForSelectedTab,
+            selectedTab == draggedTab
+          );
+          if (newTab) {
+            ++newIndex;
+          }
         }
 
         
         gBrowser.addRangeToMultiSelectedTabs(
           gBrowser.tabs[dropIndex],
-          gBrowser.tabs[dropIndex + movingTabs.length - 1]
+          gBrowser.tabs[newIndex - 1]
         );
       } else {
         
