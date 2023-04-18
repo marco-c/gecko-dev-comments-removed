761c168cd44443899fd824ed4e6650716e927927
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 16:35:52 +0000

    Bug 1763276 - Bump jsparagus for bug 1282976. r=nbp
    Differential Revision: https://phabricator.services.mozilla.com/D143440
    
    UltraBlame original commit: 16c1e24e61eb1f8bbe73aa25abfcc53364ff6239

--- third_party/rust/jsparagus-stencil/src/copy/Opcodes.h
+++ third_party/rust/jsparagus-stencil/src/copy/Opcodes.h
@@ -798,8 +798,6 @@
 
 
 
-
-
  \
     MACRO(NewTarget, new_target, NULL, 1, 0, 1, JOF_BYTE) \
     
@@ -1645,26 +1643,8 @@
 
 
 
-
-
  \
     MACRO(Lambda, lambda, NULL, 5, 0, 1, JOF_OBJECT) \
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
- \
-    MACRO(LambdaArrow, lambda_arrow, NULL, 5, 1, 1, JOF_OBJECT) \
     
 
 
@@ -3547,13 +3527,14 @@
 
 
 #define FOR_EACH_TRAILING_UNUSED_OPCODE(MACRO) \
+  IF_RECORD_TUPLE(, MACRO(225))     \
   IF_RECORD_TUPLE(, MACRO(226))     \
   IF_RECORD_TUPLE(, MACRO(227))     \
   IF_RECORD_TUPLE(, MACRO(228))     \
   IF_RECORD_TUPLE(, MACRO(229))     \
   IF_RECORD_TUPLE(, MACRO(230))     \
   IF_RECORD_TUPLE(, MACRO(231))     \
-  IF_RECORD_TUPLE(, MACRO(232))     \
+  MACRO(232)                                   \
   MACRO(233)                                   \
   MACRO(234)                                   \
   MACRO(235)                                   \
