2da2a666bcebc898334edfbacc58f6f4451976ca
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 18:35:07 +0000

    Bug 1766561 - Adjust UTF-16 string formatting. r=nika
    With MOZ_FORMAT_PRINTF annotations, the compiler expects a wchar_t*, and
    it won't automatically consider char16ptr_t to be compatible with that.
    
    While handling strings, there's one case of formatting that doesn't need
    to use %S at all.
    
    Differential Revision: https://phabricator.services.mozilla.com/D144919
    
    UltraBlame original commit: b8a9e8c33b900165ab1908d17634cfbca4cf30f3

--- browser/components/shell/nsWindowsShellService.cpp
+++ browser/components/shell/nsWindowsShellService.cpp
@@ -805,7 +805,7 @@ static nsresult CreateShortcutImpl(
   
   nsString arguments;
   for (auto& arg : aArguments) {
-    arguments.AppendPrintf("\"%S\" ", arg.get());
+    arguments.AppendPrintf("\"%S\" ", static_cast<const wchar_t*>(arg.get()));
   }
 
   link->SetArguments(arguments.get());
