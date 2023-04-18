e99addac10be5db5734b74eb672bcac68b54e496
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 09:19:24 +0000

    Backed out changeset 480ba71887c8 (bug 1755226) for causing build bustages. CLOSED TREE
    UltraBlame original commit: de0915875736250d12daab0e17c5a243d49042e0

--- xpcom/ds/nsObserverService.h
+++ xpcom/ds/nsObserverService.h
@@ -37,7 +37,6 @@ class nsObserverService final : public nsIObserverService,
   [[nodiscard]] static nsresult Create(nsISupports* aOuter, const nsIID& aIID,
                                        void** aInstancePtr);
 
-  NS_IMETHODIMP HasObservers(const char* aTopic, bool& aFound) override;
   
   
   NS_IMETHOD UnmarkGrayStrongObservers();
