ca99f8b7b70ee2737a813e401a799e7959ec0900
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 17:41:09 +0000

    Backed out changeset 0d23532d6a49 (bug 1757802) as requested by dev. CLOSED TREE
    UltraBlame original commit: 88a76c0a8d1cd349786eb1610cfa2f10907d7a1c

--- ipc/glue/Shmem.h
+++ ipc/glue/Shmem.h
@@ -19,12 +19,6 @@
 #include "mozilla/Range.h"
 #include "mozilla/UniquePtr.h"
 
-namespace IPC {
-template <typename T>
-struct ParamTraits;
-}
-
-namespace mozilla::ipc {
 
 
 
@@ -40,41 +34,76 @@ namespace mozilla::ipc {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+namespace mozilla {
+namespace layers {
+class ShadowLayerForwarder;
+}  
+
+namespace ipc {
+
+template <typename P>
+struct IPDLParamTraits;
 
 class Shmem final {
-  friend struct IPC::ParamTraits<mozilla::ipc::Shmem>;
+  friend struct IPDLParamTraits<mozilla::ipc::Shmem>;
+#ifdef DEBUG
+  
+  friend class mozilla::layers::ShadowLayerForwarder;
+#endif
 
  public:
+  typedef int32_t id_t;
   
   typedef mozilla::ipc::SharedMemory SharedMemory;
   typedef SharedMemory::SharedMemoryType SharedMemoryType;
+  
+  
+  
+  
+  struct PrivateIPDLCaller {};
 
-  Shmem() = default;
+  Shmem() : mSegment(nullptr), mData(nullptr), mSize(0), mId(0) {}
 
-  
-  
-  
-  Shmem(size_t aNBytes, SharedMemoryType aType, bool aUnsafe);
-
-  
-  
-  Shmem(RefPtr<SharedMemory> aSegment, size_t aNBytes, bool aUnsafe);
-
-  
   Shmem(const Shmem& aOther) = default;
+
+  Shmem(PrivateIPDLCaller, SharedMemory* aSegment, id_t aId);
+
+  ~Shmem() {
+    
+    
+    forget(PrivateIPDLCaller());
+  }
+
   Shmem& operator=(const Shmem& aRhs) = default;
 
   bool operator==(const Shmem& aRhs) const { return mSegment == aRhs.mSegment; }
-  bool operator!=(const Shmem& aRhs) const { return mSegment != aRhs.mSegment; }
 
   
   
-  bool IsValid() const { return mSegment != nullptr; }
+  bool IsWritable() const { return mSegment != nullptr; }
 
   
   
-  bool IsWritable() const { return IsValid(); }
-  bool IsReadable() const { return IsValid(); }
+  bool IsReadable() const { return mSegment != nullptr; }
 
   
   template <typename T>
@@ -103,35 +132,79 @@ class Shmem final {
   }
 
   
-  
-  
+  id_t Id(PrivateIPDLCaller) const { return mId; }
+
+  SharedMemory* Segment(PrivateIPDLCaller) const { return mSegment; }
+
 #ifndef DEBUG
-  void RevokeRights() { *this = Shmem(); }
+  void RevokeRights(PrivateIPDLCaller) {}
 #else
-  void RevokeRights();
+  void RevokeRights(PrivateIPDLCaller);
 #endif
+
+  void forget(PrivateIPDLCaller) {
+    mSegment = nullptr;
+    mData = nullptr;
+    mSize = 0;
+    mId = 0;
+  }
+
+  static already_AddRefed<Shmem::SharedMemory> Alloc(PrivateIPDLCaller,
+                                                     size_t aNBytes,
+                                                     SharedMemoryType aType,
+                                                     bool aUnsafe,
+                                                     bool aProtect = false);
+
+  
+  
+  
+  
+  UniquePtr<IPC::Message> MkCreatedMessage(PrivateIPDLCaller,
+                                           int32_t routingId);
+
+  
+  
+  
+  
+  UniquePtr<IPC::Message> MkDestroyedMessage(PrivateIPDLCaller,
+                                             int32_t routingId);
+
+  
+  
+  
+  
+  static already_AddRefed<SharedMemory> OpenExisting(
+      PrivateIPDLCaller, const IPC::Message& aDescriptor, id_t* aId,
+      bool aProtect = false);
+
+  static void Dealloc(PrivateIPDLCaller, SharedMemory* aSegment);
 
  private:
   template <typename T>
   void AssertAligned() const {
-    MOZ_RELEASE_ASSERT(0 == (mSize % sizeof(T)),
-                       "shmem size is not a multiple of sizeof(T)");
+    if (0 != (mSize % sizeof(T))) MOZ_CRASH("shmem is not T-aligned");
   }
 
-#ifndef DEBUG
+#if !defined(DEBUG)
   void AssertInvariants() const {}
+
+  static uint32_t* PtrToSize(SharedMemory* aSegment) {
+    char* endOfSegment =
+        reinterpret_cast<char*>(aSegment->memory()) + aSegment->Size();
+    return reinterpret_cast<uint32_t*>(endOfSegment - sizeof(uint32_t));
+  }
+
 #else
   void AssertInvariants() const;
 #endif
 
   RefPtr<SharedMemory> mSegment;
-  void* mData = nullptr;
-  size_t mSize = 0;
-#ifdef DEBUG
-  bool mUnsafe = false;
-#endif
+  void* mData;
+  size_t mSize;
+  id_t mId;
 };
 
+}  
 }  
 
 #endif  
