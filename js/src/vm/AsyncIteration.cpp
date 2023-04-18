6f045555f21553651e521b405de4727702a656ba
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 03:25:38 +0000

    Bug 1724123 - Part 41: Move setSuspendedYield call into empty queue case in AsyncGeneratorYield. r=mgaudet
    Depends on D133271
    
    Differential Revision: https://phabricator.services.mozilla.com/D133272
    
    UltraBlame original commit: a809dff7324e6624a280c59ab1648c21e102ba46

--- js/src/vm/AsyncIteration.cpp
+++ js/src/vm/AsyncIteration.cpp
@@ -291,17 +291,12 @@ AsyncGeneratorRequest* AsyncGeneratorRequest::create(
 
 [[nodiscard]] static bool AsyncGeneratorYield(
     JSContext* cx, Handle<AsyncGeneratorObject*> generator, HandleValue value) {
-  
-
-  
-  generator->setSuspendedYield();
-
-  
   if (!AsyncGeneratorCompleteStepNormal(cx, generator, value, false)) {
     return false;
   }
 
   if (generator->isQueueEmpty()) {
+    generator->setSuspendedYield();
     return true;
   }
 
