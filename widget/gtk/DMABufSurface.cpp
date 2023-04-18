5537d1f59246397b6ff0ded2ed33bb2419a941e3
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 05:20:55 +0000

    Bug 1749119 - Silence warning in DMABufSurface::FenceWait(), r=stransky,jgilbert
    We call into `FenceWait()` via `SharedSurface::WaitForBufferOwnership()`
    and it's a valid case that the fence was already deleted via
    `ReleaseTextures()`. Thus don't warn here.
    
    While on it, fix a typo.
    
    Differential Revision: https://phabricator.services.mozilla.com/D135444
    
    UltraBlame original commit: 17df17e408c34794f643dea0df1f22c95a92fe23

--- widget/gtk/DMABufSurface.cpp
+++ widget/gtk/DMABufSurface.cpp
@@ -246,14 +246,10 @@ void DMABufSurface::FenceSet() {
 }
 
 void DMABufSurface::FenceWait() {
-  if (!mGL) {
+  if (!mGL || mSyncFd < 0) {
     return;
   }
-  if (mSyncFd < 0) {
-    NS_WARNING(
-        "DMABufSurface::FenceWait(): We're missing fence file descriptor!");
-    return;
-  }
+
   const auto& gle = gl::GLContextEGL::Cast(mGL);
   const auto& egl = gle->mEgl;
 
