51a83e81c31c7360b2a05a06cb37d871463acc50
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 06:43:14 +0000

    Backed out 2 changesets (bug 1751241) for causing xpc failures in toolkit/components/glean/tests/xpcshell/test_GIFFT.js CLOSED TREE
    Backed out changeset 403e96a8429b (bug 1751241)
    Backed out changeset 7d047cf994cc (bug 1751241)
    
    UltraBlame original commit: 9298969861fe0b2ecd3fa983e938741f3e80f7ed

--- toolkit/components/glean/ipc/FOGIPC.cpp
+++ toolkit/components/glean/ipc/FOGIPC.cpp
@@ -5,7 +5,6 @@
 
 #include "FOGIPC.h"
 
-#include <limits>
 #include "mozilla/glean/fog_ffi_generated.h"
 #include "mozilla/glean/GleanMetrics.h"
 #include "mozilla/dom/ContentChild.h"
@@ -41,33 +40,28 @@ static void RecordPowerMetrics() {
   static uint64_t previousCpuTime = 0, previousGpuTime = 0;
 
   uint64_t cpuTime;
-  if (NS_SUCCEEDED(GetCpuTimeSinceProcessStartInMs(&cpuTime)) &&
-      cpuTime > previousCpuTime) {
-    uint64_t newCpuTime = cpuTime - previousCpuTime;
-    previousCpuTime += newCpuTime;
+  if (NS_FAILED(GetCpuTimeSinceProcessStartInMs(&cpuTime))) {
+    return;
+  }
 
+  uint64_t newCpuTime = cpuTime - previousCpuTime;
+  previousCpuTime += newCpuTime;
+
+  if (newCpuTime) {
     
     
     
-    
-    int32_t nNewCpuTime = int32_t(newCpuTime);
-    if (newCpuTime > std::numeric_limits<int32_t>::max()) {
-      nNewCpuTime = std::numeric_limits<int32_t>::max();
-    }
-    power::total_cpu_time_ms.Add(nNewCpuTime);
+    power::total_cpu_time_ms.Add(int32_t(newCpuTime));
   }
 
   uint64_t gpuTime;
-  if (NS_SUCCEEDED(GetGpuTimeSinceProcessStartInMs(&gpuTime)) &&
-      gpuTime > previousGpuTime) {
+  if (NS_SUCCEEDED(GetGpuTimeSinceProcessStartInMs(&gpuTime))) {
     uint64_t newGpuTime = gpuTime - previousGpuTime;
     previousGpuTime += newGpuTime;
 
-    int32_t nNewGpuTime = int32_t(newGpuTime);
-    if (newGpuTime > std::numeric_limits<int32_t>::max()) {
-      nNewGpuTime = std::numeric_limits<int32_t>::max();
+    if (newGpuTime) {
+      power::total_gpu_time_ms.Add(int32_t(newGpuTime));
     }
-    power::total_gpu_time_ms.Add(int32_t(nNewGpuTime));
   }
 }
 
