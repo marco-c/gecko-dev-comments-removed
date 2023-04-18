81256992735f6ffa320bf71be71a61f99a94603f
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 05:23:52 +0000

    Bug 1747684 - Rename editBookmark.js to instantEditBookmark.js. r=mak
    Differential Revision: https://phabricator.services.mozilla.com/D134723
    
    UltraBlame original commit: caf8b8a40574fee8befa48125b139c4e4ba3101a

--- tools/lint/eslint/eslint-plugin-mozilla/lib/environments/browser-window.js
+++ tools/lint/eslint/eslint-plugin-mozilla/lib/environments/browser-window.js
@@ -24,7 +24,7 @@ var { getScriptGlobals } = require("./utils");
 
 const EXTRA_SCRIPTS = [
   "browser/base/content/nsContextMenu.js",
-  "browser/components/places/content/editBookmark.js",
+  "browser/components/places/content/instantEditBookmark.js",
   "browser/components/downloads/content/downloads.js",
   "browser/components/downloads/content/indicator.js",
   "toolkit/content/customElements.js",
