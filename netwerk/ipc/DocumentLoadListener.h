eaa417228dd233b93601b1f3bc7b5821bc063456
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 03:47:06 +0000

    Bug 1721217 - Part 6: Don't cancel concurrent loads if they are downloads, r=nika,necko-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D126846
    
    UltraBlame original commit: c0c4625f45b40123628a2f24874b057c0350736b

--- netwerk/ipc/DocumentLoadListener.h
+++ netwerk/ipc/DocumentLoadListener.h
@@ -284,6 +284,8 @@ class DocumentLoadListener : public nsIInterfaceRequestor,
 
   uint64_t GetLoadIdentifier() const { return mLoadIdentifier; }
   uint32_t GetLoadType() const { return mLoadStateLoadType; }
+  bool IsDownload() const { return mIsDownload; }
+  bool IsLoadingJSURI() const { return mIsLoadingJSURI; }
 
   mozilla::dom::LoadingSessionHistoryInfo* GetLoadingSessionHistoryInfo() {
     return mLoadingSessionHistoryInfo.get();
@@ -539,6 +541,12 @@ class DocumentLoadListener : public nsIInterfaceRequestor,
   uint32_t mLoadStateExternalLoadFlags = 0;
   uint32_t mLoadStateInternalLoadFlags = 0;
   uint32_t mLoadStateLoadType = 0;
+
+  
+  bool mIsDownload = false;
+
+  
+  bool mIsLoadingJSURI = false;
 
   
   
