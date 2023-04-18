24526cced171b1a81d7e6740286ba3b22e120403
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 23:35:09 +0000

    Bug 1738637 - wasm: Emit 'funcref' instead of 'anyfunc' for .type() accessors. r=yury
    The specification for js-types now [1] defines the canonical type used
    for .type() accessors to be 'funcref' instead of 'anyfunc'. The implementation
    and WPTs are updated accordingly. 'anyfunc' is still accepted as an alias of
    'funcref' for constructors, but this is already implemented and requires no
    changes.
    
    [1] https://github.com/WebAssembly/js-types/pull/34/files
    [2] https://github.com/WebAssembly/js-types/blob/main/proposals/js-types/Overview.md
    
    Differential Revision: https://phabricator.services.mozilla.com/D149169
    
    UltraBlame original commit: 30e72f02feba35758bbd83d477c72ee3b289a9c4

--- js/src/wasm/WasmValType.h
+++ js/src/wasm/WasmValType.h
@@ -748,16 +748,6 @@ extern UniqueChars ToString(ValType type);
 
 extern UniqueChars ToString(const Maybe<ValType>& type);
 
-#ifdef ENABLE_WASM_TYPE_REFLECTIONS
-
-
-
-extern UniqueChars ToJSAPIString(RefType type);
-
-extern UniqueChars ToJSAPIString(ValType type);
-
-#endif
-
 }  
 }  
 
