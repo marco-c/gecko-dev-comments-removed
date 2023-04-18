8ebd2151e4b2aaf0b493ab208b200a8d288cfed3
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 10:46:23 +0000

    Bug 1756771 - Pack all Breakpad CPU context structures consistently r=glandium
    Also add out-of-tree patches for bugs we fixed in the meantime.
    
    Differential Revision: https://phabricator.services.mozilla.com/D139466
    
    UltraBlame original commit: a0b9218668431d54886652276b5959e9391dd098

--- toolkit/crashreporter/google-breakpad/src/google_breakpad/common/minidump_cpu_arm64.h
+++ toolkit/crashreporter/google-breakpad/src/google_breakpad/common/minidump_cpu_arm64.h
@@ -91,6 +91,10 @@ typedef struct {
                                MD_CONTEXT_ARM64_FLOATING_POINT)
 #define MD_CONTEXT_ARM64_ALL (MD_CONTEXT_ARM64_FULL | MD_CONTEXT_ARM64_DEBUG)
 
+
+
+#pragma pack(push, 4)
+
 typedef struct {
   
   uint32_t context_flags;
@@ -129,10 +133,6 @@ typedef struct {
   
   uint128_struct regs[MD_FLOATINGSAVEAREA_ARM64_FPR_COUNT];
 } MDFloatingSaveAreaARM64_Old;
-
-
-
-#pragma pack(push, 4)
 
 typedef struct {
   
