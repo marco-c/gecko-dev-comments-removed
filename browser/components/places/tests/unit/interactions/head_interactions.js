bbe973808b4ad732c45164101edf0707488469c2
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 01:40:40 +0000

    Bug 1742963: Disable the page thumbs service in tests. r=Standard8
    Differential Revision: https://phabricator.services.mozilla.com/D132142
    
    UltraBlame original commit: 4e123b6db0891427510c3e9939edc9df172fd529

--- browser/components/places/tests/unit/interactions/head_interactions.js
+++ browser/components/places/tests/unit/interactions/head_interactions.js
@@ -19,6 +19,7 @@ XPCOMUtils.defineLazyModuleGetters(this, {
 
 
 var gProfD = do_get_profile(true);
+Services.prefs.setBoolPref("browser.pagethumbnails.capturing_disabled", true);
 
 
 const TOPIC_ADDED = "places-snapshots-added";
