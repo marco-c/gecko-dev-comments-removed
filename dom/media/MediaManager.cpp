597e37f20d4a6863dd4d03e727fad33b30f2461f
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 04:12:37 +0000

    Bug 1732409 make getUserMedia() permission prompt requirement independent of "media.navigator.streams.fake" r=jib
    "media.navigator.permission.disabled" exists for controlling the permission
    prompt separately.
    
    This will allow consolidation of camera and microphone preference handling
    into EnumerateRawDevices().  Although auto-disabling permission with
    "media.navigator.streams.fake" is sometimes useful for tests, it is actually
    easier to consider them independently because sometimes the auto-disabling
    needs to be overridden.
    
    The logic to disable the permission prompt with content-exposed fake:true
    parameter is maintained as is.
    
    The CamerasParent usage of "media.navigator.permission.fake" seems to have
    been a misunderstanding in
    https://hg.mozilla.org/mozilla-central/rev/c5d6c3e00c91dd0595a12b145c66cf4f2a890591#l6.129
    
    Differential Revision: https://phabricator.services.mozilla.com/D132897
    
    UltraBlame original commit: 0a6943f2739245feb46d2adb3293e6785e557496

--- dom/media/MediaManager.cpp
+++ dom/media/MediaManager.cpp
@@ -2683,14 +2683,15 @@ RefPtr<MediaManager::StreamPromise> MediaManager::GetUserMedia(
       audioEnumerationType = DeviceEnumerationType::Fake;
     }
   }
-
-  bool realDevicesRequested =
-      (videoEnumerationType != DeviceEnumerationType::Fake && hasVideo) ||
-      (audioEnumerationType != DeviceEnumerationType::Fake && hasAudio);
+  
+  
+  bool hasOnlyForcedFakes =
+      forceFakes && (!hasVideo || videoType == MediaSourceEnum::Camera) &&
+      (!hasAudio || audioType == MediaSourceEnum::Microphone);
   bool askPermission =
       (!privileged ||
        Preferences::GetBool("media.navigator.permission.force")) &&
-      (realDevicesRequested ||
+      (!hasOnlyForcedFakes ||
        Preferences::GetBool("media.navigator.permission.fake"));
 
   LOG("%s: Preparing to enumerate devices. windowId=%" PRIu64
