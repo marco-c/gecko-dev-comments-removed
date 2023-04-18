8dd76695ec7f1c0ad15be91657416093523604f8
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 12:45:48 +0000

    Bug 1735970 part 3: Add TableAccessibleBase and TableCellAccessibleBase. r=morgan
    These are needed to support RemoteAccessible tables.
    Stuff specific to LocalAccessible is still in TableAccessible and TableCellAccessible, which now inherit from the new Base classes.
    
    Covariant return types have been used to minimise changes in LocalAccessible callers.
    
    Differential Revision: https://phabricator.services.mozilla.com/D141205
    
    UltraBlame original commit: 6d0f4821ddcfca1b59e4db3befc3ec9c12335891

--- accessible/generic/TableCellAccessible.h
+++ accessible/generic/TableCellAccessible.h
@@ -7,59 +7,22 @@
 #ifndef mozilla_a11y_TableCellAccessible_h__
 #define mozilla_a11y_TableCellAccessible_h__
 
-#include "nsTArray.h"
-#include <stdint.h>
+#include "mozilla/a11y/TableCellAccessibleBase.h"
+#include "TableAccessible.h"
 
 namespace mozilla {
 namespace a11y {
 
 class LocalAccessible;
-class TableAccessible;
 
 
 
 
-class TableCellAccessible {
+class TableCellAccessible : public TableCellAccessibleBase {
  public:
-  
-
-
-  virtual TableAccessible* Table() const = 0;
-
-  
-
-
-  virtual uint32_t ColIdx() const = 0;
-
-  
-
-
-  virtual uint32_t RowIdx() const = 0;
-
-  
-
-
-  virtual uint32_t ColExtent() const { return 1; }
-
-  
-
-
-  virtual uint32_t RowExtent() const { return 1; }
-
-  
-
-
-  virtual void ColHeaderCells(nsTArray<LocalAccessible*>* aCells);
-
-  
-
-
-  virtual void RowHeaderCells(nsTArray<LocalAccessible*>* aCells);
-
-  
-
-
-  virtual bool Selected() = 0;
+  virtual TableAccessible* Table() const override = 0;
+  virtual void ColHeaderCells(nsTArray<Accessible*>* aCells) override;
+  virtual void RowHeaderCells(nsTArray<Accessible*>* aCells) override;
 
  private:
   LocalAccessible* PrevColHeader();
