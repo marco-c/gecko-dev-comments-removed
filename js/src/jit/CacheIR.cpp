ca61204e9fefe0511b9937a1be19bfd1d20b081b
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 12:37:55 +0000

    Bug 1760605 part 3 - Optimize fun.apply for normal Call ops. r=iain
    Also support the other call optimizations for `JSOp::FunApply` ops.
    
    Differential Revision: https://phabricator.services.mozilla.com/D141616
    
    UltraBlame original commit: 0e59b292fb48ce75e6d04cac9ff12cc355f82962

--- js/src/jit/CacheIR.cpp
+++ js/src/jit/CacheIR.cpp
@@ -8893,8 +8893,9 @@ AttachDecision CallIRGenerator::tryAttachTypedArrayConstructor(
 }
 
 AttachDecision CallIRGenerator::tryAttachFunApply(HandleFunction calleeFunc) {
-  if (!calleeFunc->isNativeWithoutJitEntry() ||
-      calleeFunc->native() != fun_apply) {
+  MOZ_ASSERT(calleeFunc->isNativeWithoutJitEntry());
+
+  if (calleeFunc->native() != fun_apply) {
     return AttachDecision::NoAction;
   }
 
@@ -9868,9 +9869,6 @@ AttachDecision CallIRGenerator::tryAttachStub() {
   if (op_ == JSOp::FunCall) {
     return tryAttachFunCall(calleeFunc);
   }
-  if (op_ == JSOp::FunApply) {
-    return tryAttachFunApply(calleeFunc);
-  }
 
   
   if (calleeFunc->hasJitEntry()) {
@@ -9879,6 +9877,14 @@ AttachDecision CallIRGenerator::tryAttachStub() {
 
   
   MOZ_ASSERT(calleeFunc->isNativeWithoutJitEntry());
+
+  
+  
+  
+  if (op_ == JSOp::FunApply || op_ == JSOp::Call ||
+      op_ == JSOp::CallIgnoresRv) {
+    TRY_ATTACH(tryAttachFunApply(calleeFunc));
+  }
 
   return tryAttachCallNative(calleeFunc);
 }
