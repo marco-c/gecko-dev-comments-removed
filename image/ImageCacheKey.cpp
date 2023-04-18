14be821929784b806cffd766445a70a12bed38e1
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 22:39:28 +0000

    Bug 1765313, part 2 - Move the ShouldAllowAccessFor functions to StorageAccess.h, r=anti-tracking-reviewers,timhuang
    These functions are used to check if an object or context should have storage access.
    Therefore they belong in StorageAccess.h with other functions that serve this purpose.
    
    Differential Revision: https://phabricator.services.mozilla.com/D148285
    
    UltraBlame original commit: 7806f737966d0a42ce1d895ae35097b4fe562575

--- image/ImageCacheKey.cpp
+++ image/ImageCacheKey.cpp
@@ -8,7 +8,6 @@
 #include <utility>
 
 #include "mozilla/AntiTrackingUtils.h"
-#include "mozilla/ContentBlocking.h"
 #include "mozilla/HashFunctions.h"
 #include "mozilla/StorageAccess.h"
 #include "mozilla/StoragePrincipalHelper.h"
@@ -17,6 +16,7 @@
 #include "mozilla/dom/File.h"
 #include "mozilla/dom/ServiceWorkerManager.h"
 #include "mozilla/StaticPrefs_privacy.h"
+#include "mozilla/StorageAccess.h"
 #include "nsContentUtils.h"
 #include "nsHashKeys.h"
 #include "nsLayoutUtils.h"
@@ -153,8 +153,8 @@ nsCString ImageCacheKey::GetIsolationKey(Document* aDocument, nsIURI* aURI) {
   
   
   
-  if (!ContentBlocking::ApproximateAllowAccessForWithoutChannel(
-          aDocument->GetInnerWindow(), aURI)) {
+  if (!ApproximateAllowAccessForWithoutChannel(aDocument->GetInnerWindow(),
+                                               aURI)) {
     
     
     
