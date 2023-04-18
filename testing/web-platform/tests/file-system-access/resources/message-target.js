f522367af37ce7c6fbdeeab0c51a075413eb3f94
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 15:15:34 +0000

    Bug 1726785 [wpt PR 30111] - Add options parameter to createSyncAccessHandle(), a=testonly
    Automatic update from web-platform-tests
    Add options parameter to createSyncAccessHandle() (#30111)
    
    For now the only accepted (and required) option is "mode: 'in-place'".
    Having this parameter allows us to later define a more appropriate
    default behavior for access handles than the one we currently implement.
    This is specially useful in the context of expanding access handle usage
    outside of OPFS.
    
    For more details check:
    https://github.com/WICG/file-system-access/blob/main/AccessHandle.md#exposing-accesshandles-on-all-filesystems
    
    Bug: 1218431
    Change-Id: I50d0a4acfd81874e17b6b28d1c2bf446b0397a4d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3110487
    Reviewed-by: Marijn Kruisselbrink <mekchromium.org>
    Commit-Queue: Emanuel Krivoy <krivoygoogle.com>
    Cr-Commit-Position: refs/heads/main{#982209}
    
    Co-authored-by: Emanuel Krivoy <krivoygoogle.com>
    --
    
    wpt-commits: a7dca41905b1f3caa8274351dcc455e044d8e885
    wpt-pr: 30111
    
    UltraBlame original commit: 7802f4c04d45b9ab4615c81cad36bb27d84058da

--- testing/web-platform/tests/file-system-access/resources/message-target.js
+++ testing/web-platform/tests/file-system-access/resources/message-target.js
@@ -108,7 +108,8 @@ function add_message_event_handlers(receiver, target, target_origin) {
           
           let success = true;
           try {
-            const access_handle = await message_data.file_handle.createSyncAccessHandle();
+            const access_handle = await message_data.file_handle
+                                    .createSyncAccessHandle({mode: "in-place"});
             await access_handle.close();
           } catch (error) {
             success = false;
