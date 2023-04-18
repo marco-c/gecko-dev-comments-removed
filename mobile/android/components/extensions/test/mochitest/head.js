47ec95196e7c4f251060b2c27d1a213fcaff49d2
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 18:27:04 +0000

    Bug 1765156 - Part 8: Use SpecialPowers.ChromeUtils.import in mobile/. r=geckoview-reviewers,agi
    Differential Revision: https://phabricator.services.mozilla.com/D144089
    
    UltraBlame original commit: a06b5b3f366df0b5f1865653089856a3df746ed9

--- mobile/android/components/extensions/test/mochitest/head.js
+++ mobile/android/components/extensions/test/mochitest/head.js
@@ -2,9 +2,8 @@
 
 
 
-var { AppConstants } = SpecialPowers.Cu.import(
-  "resource://gre/modules/AppConstants.jsm",
-  {}
+var { AppConstants } = SpecialPowers.ChromeUtils.import(
+  "resource://gre/modules/AppConstants.jsm"
 );
 
 var TEST_ICON_DATA =
