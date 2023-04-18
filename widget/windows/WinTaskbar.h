eb327ca184e3d5a0d927d887b0f2b053d49e4204
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 10:05:49 +0000

    Bug 1751010: Make it possible to generate an AUMID without setting it, or retrieving the current one. r=mhowell
    The overarching change here is that it's now possible to get an AUMID without first trying to fetch the current one that's set -- this allows us to obtain the Private Browsing one from a non-Private window, which we will need in a later patch to supporting adding a Private Browsing taskbar pin during onboarding.
    
    Differential Revision: https://phabricator.services.mozilla.com/D138193
    
    UltraBlame original commit: 6ad85a8edf154373a8bd899694b8aa1745c672f3

--- widget/windows/WinTaskbar.h
+++ widget/windows/WinTaskbar.h
@@ -26,10 +26,13 @@ class WinTaskbar final : public nsIWinTaskbar {
   NS_DECL_THREADSAFE_ISUPPORTS
   NS_DECL_NSIWINTASKBAR
 
+  static bool GenerateAppUserModelID(nsAString& aAppUserModelId,
+                                     bool aPrivateBrowsing = false);
   
   
   static bool RegisterAppUserModelID();
-  static bool GetAppUserModelID(nsAString& aDefaultGroupId);
+  static bool GetAppUserModelID(nsAString& aDefaultGroupId,
+                                bool aPrivateBrowsing = false);
 
  private:
   bool Initialize();
