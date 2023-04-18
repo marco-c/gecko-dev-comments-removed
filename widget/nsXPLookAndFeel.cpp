e631fa4cd6b7409990482ea470d2b8ff5f77f90f
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 06:11:57 +0000

    Bug 1750483 - Add a generic dark color for ThreeDFace. r=jfkthame
    Differential Revision: https://phabricator.services.mozilla.com/D136405
    
    UltraBlame original commit: c6a31e57101a9914e60c135b527cef1e07c3b82b

--- widget/nsXPLookAndFeel.cpp
+++ widget/nsXPLookAndFeel.cpp
@@ -678,6 +678,7 @@ Maybe<nscolor> nsXPLookAndFeel::GenericDarkColor(ColorID aID) {
       break;
     case ColorID::Field:
     case ColorID::Buttonface:  
+    case ColorID::Threedface:
     case ColorID::MozCombobox:
     case ColorID::MozCellhighlighttext:
     case ColorID::Selecteditemtext:  
