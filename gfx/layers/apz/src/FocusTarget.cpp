17af38e1b782e1d4d7d5e548ea1cd30bfc832667
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 04:35:39 +0000

    Bug 1539884 - Part 23: Mark HasListenersFor* in FocusTarget as CAN_RUN_SCRIPT_BOUNDARY r=masayuki
    Differential Revision: https://phabricator.services.mozilla.com/D134355
    
    UltraBlame original commit: 843729ca64f6539b1943c4f65fd99a6a1b5767c5

--- gfx/layers/apz/src/FocusTarget.cpp
+++ gfx/layers/apz/src/FocusTarget.cpp
@@ -42,7 +42,9 @@ static PresShell* GetRetargetEventPresShell(PresShell* aRootPresShell) {
   return retargetEventDoc->GetPresShell();
 }
 
-static bool HasListenersForKeyEvents(nsIContent* aContent) {
+
+MOZ_CAN_RUN_SCRIPT_BOUNDARY static bool HasListenersForKeyEvents(
+    nsIContent* aContent) {
   if (!aContent) {
     return false;
   }
@@ -60,7 +62,9 @@ static bool HasListenersForKeyEvents(nsIContent* aContent) {
   return false;
 }
 
-static bool HasListenersForNonPassiveKeyEvents(nsIContent* aContent) {
+
+MOZ_CAN_RUN_SCRIPT_BOUNDARY static bool HasListenersForNonPassiveKeyEvents(
+    nsIContent* aContent) {
   if (!aContent) {
     return false;
   }
