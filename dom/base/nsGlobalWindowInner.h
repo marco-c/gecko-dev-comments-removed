57eea35fc7392b65a9d4c38a7bb320214840c83d
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 03:44:00 +0000

    Bug 1539884 - Part 6: Mark nsGlobalWindowInner::FireFrameLoadEvent as CAN_RUN_SCRIPT r=masayuki
    Depends on D133915
    
    Differential Revision: https://phabricator.services.mozilla.com/D133917
    
    UltraBlame original commit: a01c249b5af3ecf44a95b3040bb782c0fe7457e9

--- dom/base/nsGlobalWindowInner.h
+++ dom/base/nsGlobalWindowInner.h
@@ -313,7 +313,9 @@ class nsGlobalWindowInner final : public mozilla::dom::EventTarget,
 
   void GetEventTargetParent(mozilla::EventChainPreVisitor& aVisitor) override;
 
-  nsresult PostHandleEvent(mozilla::EventChainPostVisitor& aVisitor) override;
+  
+  MOZ_CAN_RUN_SCRIPT_BOUNDARY nsresult
+  PostHandleEvent(mozilla::EventChainPostVisitor& aVisitor) override;
 
   void Suspend(bool aIncludeSubWindows = true);
   void Resume(bool aIncludeSubWindows = true);
@@ -1258,7 +1260,7 @@ class nsGlobalWindowInner final : public mozilla::dom::EventTarget,
   bool MaybeCallDocumentFlushedResolvers(bool aUntilExhaustion);
 
   
-  void FireFrameLoadEvent();
+  MOZ_CAN_RUN_SCRIPT void FireFrameLoadEvent();
 
   void UpdateAutoplayPermission();
   void UpdateShortcutsPermission();
