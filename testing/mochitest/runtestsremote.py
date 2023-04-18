8e5eecd2e7fe46967c200fea908120f15e0a7d85
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 00:41:14 +0000

    Bug 1707591 - ride along - reformat the tree with black 21.10b0 r=webdriver-reviewers,whimboo,gerard-majax
    This changed with this:
    https://github.com/psf/black/pull/1740
    
    Differential Revision: https://phabricator.services.mozilla.com/D130965
    
    UltraBlame original commit: a730ab2d0dbe18fdd264e4dab2a2c3473be85bff

--- testing/mochitest/runtestsremote.py
+++ testing/mochitest/runtestsremote.py
@@ -150,7 +150,7 @@ class MochiRemote(MochitestDesktop):
     
     
     def switchToLocalPaths(self, options):
-        """ Set local paths in the options, return a function that will restore remote values """
+        """Set local paths in the options, return a function that will restore remote values"""
         remoteXrePath = options.xrePath
         remoteProfilePath = options.profilePath
         remoteUtilityPath = options.utilityPath
@@ -207,7 +207,7 @@ class MochiRemote(MochitestDesktop):
         return fixup
 
     def startServers(self, options, debuggerInfo, public=None):
-        """ Create the servers on the host and start them up """
+        """Create the servers on the host and start them up"""
         restoreRemotePaths = self.switchToLocalPaths(options)
         MochitestDesktop.startServers(self, options, debuggerInfo, public=True)
         restoreRemotePaths()
