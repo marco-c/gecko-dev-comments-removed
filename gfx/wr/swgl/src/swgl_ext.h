f153a384e8a9f43eaf672c2e4235a59f2fb19af9
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 11:29:19 +0000

    Bug 1758736 - Support RG16 textures in SWGL. r=bradwerth
    Differential Revision: https://phabricator.services.mozilla.com/D140716
    
    UltraBlame original commit: e998e2ebe5b9b141693ede7fa0f6e52398f2772f

--- gfx/wr/swgl/src/swgl_ext.h
+++ gfx/wr/swgl/src/swgl_ext.h
@@ -1035,7 +1035,7 @@ template <typename S0, typename S1>
 static ALWAYS_INLINE PackedRGBA8 sampleYUV(S0 sampler0, ivec2 uv0, S1 sampler1,
                                            ivec2 uv1,
                                            const YUVMatrix& rgb_from_ycbcr,
-                                           UNUSED int rescaleFactor) {
+                                           int rescaleFactor) {
   switch (sampler1->format) {
     case TextureFormat::RG8: {
       assert(sampler0->format == TextureFormat::R8);
@@ -1050,6 +1050,21 @@ static ALWAYS_INLINE PackedRGBA8 sampleYUV(S0 sampler0, ivec2 uv0, S1 sampler1,
       auto planar = textureLinearPlanarRGBA8(sampler1, uv1);
       return convertYUV(rgb_from_ycbcr, y, lowHalf(planar.ba),
                         highHalf(planar.rg));
+    }
+    case TextureFormat::RG16: {
+      assert(sampler0->format == TextureFormat::R16);
+      
+      
+      
+      
+      
+      
+      
+      int colorDepth = 16 - rescaleFactor;
+      int rescaleBits = (colorDepth - 1) - 8;
+      auto y = textureLinearUnpackedR16(sampler0, uv0) >> rescaleBits;
+      auto uv = textureLinearUnpackedRG16(sampler1, uv1) >> rescaleBits;
+      return rgb_from_ycbcr.convert(zip(y, y), uv);
     }
     default:
       assert(false);
