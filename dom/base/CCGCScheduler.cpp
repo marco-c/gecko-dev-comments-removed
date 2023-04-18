aafc41ed1af5c3b433aef211434ed577bab36eb6
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 08:20:53 +0000

    Bug 1747273 - Ignore the GC interrupt if the GC is running long r=smaug
    Differential Revision: https://phabricator.services.mozilla.com/D134534
    
    UltraBlame original commit: bd91af1f54bb3d58ec08d15bc8a91db5a22d89b5

--- dom/base/CCGCScheduler.cpp
+++ dom/base/CCGCScheduler.cpp
@@ -301,7 +301,7 @@ bool CCGCScheduler::GCRunnerFiredDoGC(TimeStamp aDeadline,
 
   MOZ_ASSERT(mActiveIntersliceGCBudget);
   TimeStamp startTimeStamp = TimeStamp::Now();
-  TimeDuration budget = ComputeInterSliceGCBudget(aDeadline, startTimeStamp);
+  js::SliceBudget budget = ComputeInterSliceGCBudget(aDeadline, startTimeStamp);
   TimeDuration duration = mGCUnnotifiedTotalTime;
   nsJSContext::RunIncrementalGCSlice(aStep.mReason, is_shrinking, budget);
 
@@ -630,8 +630,8 @@ js::SliceBudget CCGCScheduler::ComputeCCSliceBudget(
       std::max({delaySliceBudget, laterSliceBudget, baseBudget})));
 }
 
-TimeDuration CCGCScheduler::ComputeInterSliceGCBudget(TimeStamp aDeadline,
-                                                      TimeStamp aNow) const {
+js::SliceBudget CCGCScheduler::ComputeInterSliceGCBudget(TimeStamp aDeadline,
+                                                         TimeStamp aNow) {
   
   
   
@@ -639,14 +639,21 @@ TimeDuration CCGCScheduler::ComputeInterSliceGCBudget(TimeStamp aDeadline,
   TimeDuration budget =
       aDeadline.IsNull() ? mActiveIntersliceGCBudget * 2 : aDeadline - aNow;
   if (!mCCBlockStart) {
-    return budget;
+    return CreateGCSliceBudget(budget);
   }
 
   TimeDuration blockedTime = aNow - mCCBlockStart;
   TimeDuration maxSliceGCBudget = mActiveIntersliceGCBudget * 10;
   double percentOfBlockedTime =
       std::min(blockedTime / kMaxCCLockedoutTime, 1.0);
-  return std::max(budget, maxSliceGCBudget.MultDouble(percentOfBlockedTime));
+  TimeDuration extendedBudget =
+      maxSliceGCBudget.MultDouble(percentOfBlockedTime);
+  if (budget >= extendedBudget) {
+    return CreateGCSliceBudget(budget);
+  }
+
+  
+  return js::SliceBudget(js::TimeBudget(extendedBudget));
 }
 
 CCReason CCGCScheduler::ShouldScheduleCC(TimeStamp aNow,
