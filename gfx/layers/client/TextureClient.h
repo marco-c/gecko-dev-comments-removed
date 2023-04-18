605fe793d8bf7353838cf9611be6b20b9f42cecb
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 02:44:49 +0000

    Bug 1744868 - Remove KeepAlive r=gfx-reviewers,lsalzman
    Differential Revision: https://phabricator.services.mozilla.com/D133162
    
    UltraBlame original commit: 0efd7ee7049007080448f58f38be4b95969ffc39

--- gfx/layers/client/TextureClient.h
+++ gfx/layers/client/TextureClient.h
@@ -68,7 +68,6 @@ class ITextureClientRecycleAllocator;
 class TextureClientPool;
 #endif
 class TextureForwarder;
-class KeepAlive;
 
 
 
@@ -795,20 +794,6 @@ class MOZ_RAII TextureClientAutoLock {
   bool mChecked;
 #endif
   bool mSucceeded;
-};
-
-class KeepAlive {
- public:
-  virtual ~KeepAlive() = default;
-};
-
-template <typename T>
-class TKeepAlive : public KeepAlive {
- public:
-  explicit TKeepAlive(T* aData) : mData(aData) {}
-
- protected:
-  RefPtr<T> mData;
 };
 
 
