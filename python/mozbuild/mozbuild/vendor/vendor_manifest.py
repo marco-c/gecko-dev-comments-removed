ebe2d2b439a4106defa39f321fb9a3ef929a4447
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 16:45:39 +0000

    Bug 1761524: Add an option to force a library to vendor r=jewilde
    Depends on D142121
    
    Differential Revision: https://phabricator.services.mozilla.com/D142122
    
    UltraBlame original commit: 81c391c7c0bc1f0c5d8890bf68f21dddfa4c4a45

--- python/mozbuild/mozbuild/vendor/vendor_manifest.py
+++ python/mozbuild/mozbuild/vendor/vendor_manifest.py
@@ -39,6 +39,7 @@ class VendorManifest(MozbuildObject):
         manifest,
         revision,
         check_for_update,
+        force,
         add_to_exports,
         patch_mode,
     ):
@@ -77,7 +78,7 @@ class VendorManifest(MozbuildObject):
             )
             return
 
-        if self.manifest["origin"]["revision"] == ref:
+        if not force and self.manifest["origin"]["revision"] == ref:
             
             self.log(
                 logging.INFO,
