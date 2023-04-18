ada940ccc707c1ce11b1442531afaee09732c83e
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 07:50:26 +0000

    Bug 1738096 - Split out RunIncrementalGCSlice from GarbageCollectNow r=smaug
    Differential Revision: https://phabricator.services.mozilla.com/D131704
    
    UltraBlame original commit: eee1c8ccba69e687ca592346a99fef5c56eae46d

--- dom/base/nsJSEnvironment.h
+++ dom/base/nsJSEnvironment.h
@@ -57,17 +57,17 @@ class nsJSContext : public nsIScriptContext {
 
   enum IsShrinking { ShrinkingGC, NonShrinkingGC };
 
-  enum IsIncremental { IncrementalGC, NonIncrementalGC };
-
   
   static void EnsureStatics();
 
   static void SetLowMemoryState(bool aState);
 
   static void GarbageCollectNow(JS::GCReason reason,
-                                IsIncremental aIncremental = NonIncrementalGC,
-                                IsShrinking aShrinking = NonShrinkingGC,
-                                int64_t aSliceMillis = 0);
+                                IsShrinking aShrinking = NonShrinkingGC);
+
+  static void RunIncrementalGCSlice(JS::GCReason aReason,
+                                    IsShrinking aShrinking,
+                                    mozilla::TimeDuration aBudget);
 
   static void CycleCollectNow(mozilla::CCReason aReason,
                               nsICycleCollectorListener* aListener = nullptr);
