88115a2515f072290502b0c247193dfe8e20d192
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 20:43:49 +0000

    Bug 1769993 - Bring some sanity to nsPrintJob's scaling/shrink-to-fit logic. r=emilio
    The comments and structure of this code no longer made much sense after the
    last 20 years of churn, including those made in bug 1659432 and bug 1552785.
    
    Differential Revision: https://phabricator.services.mozilla.com/D146717
    
    UltraBlame original commit: e7b7ffcf6096fbe62d38df553a1b491a2c14dc6f

--- layout/printing/nsPrintJob.cpp
+++ layout/printing/nsPrintJob.cpp
@@ -731,7 +731,7 @@ void nsPrintJob::FirePrintingErrorEvent(nsresult aPrintError) {
 
 
 
-nsresult nsPrintJob::ReconstructAndReflow(bool doSetPixelScale) {
+nsresult nsPrintJob::ReconstructAndReflow() {
   if (NS_WARN_IF(!mPrt)) {
     return NS_ERROR_FAILURE;
   }
@@ -767,7 +767,7 @@ nsresult nsPrintJob::ReconstructAndReflow(bool doSetPixelScale) {
                "print object "
                "has been marked as \"print the document\"");
 
-    UpdateZoomRatio(po, doSetPixelScale);
+    UpdateZoomRatio(po);
 
     po->mPresContext->SetPageScale(po->mZoomRatio);
 
@@ -866,7 +866,7 @@ nsresult nsPrintJob::SetupToPrintContent() {
   
   
   if (mDidLoadDataForPrinting) {
-    nsresult rv = ReconstructAndReflow(DoSetPixelScale());
+    nsresult rv = ReconstructAndReflow();
     if (NS_WARN_IF(NS_FAILED(rv))) {
       return rv;
     }
@@ -886,7 +886,7 @@ nsresult nsPrintJob::SetupToPrintContent() {
     printData->mShrinkRatio = printData->mPrintObject->mShrinkRatio;
 
     if (printData->mShrinkRatio < 0.998f) {
-      nsresult rv = ReconstructAndReflow(true);
+      nsresult rv = ReconstructAndReflow();
       if (NS_WARN_IF(NS_FAILED(rv))) {
         return rv;
       }
@@ -1011,8 +1011,7 @@ nsresult nsPrintJob::SetupToPrintContent() {
 
 
 
-nsresult nsPrintJob::ReflowDocList(const UniquePtr<nsPrintObject>& aPO,
-                                   bool aSetPixelScale) {
+nsresult nsPrintJob::ReflowDocList(const UniquePtr<nsPrintObject>& aPO) {
   NS_ENSURE_ARG_POINTER(aPO);
 
   
@@ -1027,13 +1026,13 @@ nsresult nsPrintJob::ReflowDocList(const UniquePtr<nsPrintObject>& aPO,
     }
   }
 
-  UpdateZoomRatio(aPO.get(), aSetPixelScale);
+  UpdateZoomRatio(aPO.get());
 
   
   MOZ_TRY(ReflowPrintObject(aPO));
 
   for (const UniquePtr<nsPrintObject>& kid : aPO->mKids) {
-    MOZ_TRY(ReflowDocList(kid, aSetPixelScale));
+    MOZ_TRY(ReflowDocList(kid));
   }
   return NS_OK;
 }
@@ -1070,7 +1069,7 @@ nsresult nsPrintJob::InitPrintDocConstruction(bool aHandleError) {
     webProgress->AddProgressListener(static_cast<nsIWebProgressListener*>(this),
                                      nsIWebProgress::NOTIFY_STATE_REQUEST);
 
-    MOZ_TRY(ReflowDocList(printData->mPrintObject, DoSetPixelScale()));
+    MOZ_TRY(ReflowDocList(printData->mPrintObject));
 
     FirePrintPreviewUpdateEvent();
   }
@@ -1186,18 +1185,21 @@ nsPrintJob::OnContentBlockingEvent(nsIWebProgress* aWebProgress,
 
 
 
-void nsPrintJob::UpdateZoomRatio(nsPrintObject* aPO, bool aSetPixelScale) {
-  
-  
-  if (aSetPixelScale && aPO->mFrameType != eIFrame) {
-    
-    aPO->mZoomRatio = mPrt->mPrintSettings->GetPrintSelectionOnly()
-                          ? aPO->mShrinkRatio - 0.005f
-                          : mPrt->mShrinkRatio - 0.005f;
-  } else if (!mPrt->mShrinkToFit) {
-    double scaling;
-    mPrt->mPrintSettings->GetScaling(&scaling);
-    aPO->mZoomRatio = float(scaling);
+void nsPrintJob::UpdateZoomRatio(nsPrintObject* aPO) {
+  if (!aPO->mParent) {
+    if (mPrt->mShrinkToFit) {
+      aPO->mZoomRatio = mPrt->mShrinkRatio;
+      
+      
+      
+      if (aPO->mZoomRatio != 1.0f) {
+        aPO->mZoomRatio -= 0.005f;
+      }
+    } else {
+      double scaling;
+      mPrt->mPrintSettings->GetScaling(&scaling);
+      aPO->mZoomRatio = float(scaling);
+    }
   }
 }
 
@@ -1250,23 +1252,6 @@ nsresult nsPrintJob::UpdateSelectionAndShrinkPrintObject(
     }
   }
   return NS_OK;
-}
-
-bool nsPrintJob::DoSetPixelScale() {
-  
-  
-  
-  
-  
-  
-  
-  bool doSetPixelScale = false;
-  bool ppIsShrinkToFit = mPrtPreview && mPrtPreview->mShrinkToFit;
-  if (ppIsShrinkToFit) {
-    mPrt->mShrinkRatio = mPrtPreview->mShrinkRatio;
-    doSetPixelScale = true;
-  }
-  return doSetPixelScale;
 }
 
 nsView* nsPrintJob::GetParentViewForRoot() {
