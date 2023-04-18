8ceb61773642e6af5646e337a98e180ae1fa3267
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 08:27:39 +0000

    Bug 1710025 - Make Ion's regalloc debug output easier to read.  r=jandem.
    The backtracking allocator's debug printing of live ranges, bundles and passes
    is hard to read and occasionally misleading.  This patch has the following
    goals:
    
    * make printing of instruction-point ranges, live ranges and bundles less
      cluttered
    
    * enable consistent printing of fixed-register constraints in LUse and
      LDefinition
    
    * give LiveBundles identities of the form `LB<number>` in what is printed
    
    * be clearer and generally more elegant about when regalloc passes begin and
      end
    
    There are no functional changes, and for non-JS_JITSPEW builds, no storage
    changes.
    
    Changes in detail (may not be exhaustive):
    
    * Instruction-point ranges were previously printed as
      `[first_index, last_index+1)`, except when printing LIR, where the format
      `first_index,last_index` is used.  Both are replaced with
      `first_index-last_index`.
    
    * For live range and live bundle printing, the set of use points is enclosed
      in `{` and `}` so as to make it easier to see where the range ends.
    
    * For live range use/def points, the index is printed first, followed by `_`.
      If a def has an associated fixed assignment, that is now printed too.
    
    * For LIR `MoveGroup`s, the destinations are now printed on the left.  That
      fixes an inconsistency in LIR printing wherein "normal" nodes had
      destinations on the left but `MoveGroup`s had destinations on the right.
    
    * LUse policy printing has been changed to be less mysterious:
      - "register":   `r`       changed to  `R`
      - "fixed":      `r<reg>`  changed to  `F:reg`
      - "any":        `r?`      (which is misleading, since it includes stack)
                                changed to  `A`
      - "keepalive":  `*`       changed to  `KA`
      - "recovered_input": `**` changed to  `RI`
    
    * To make LiveBundle creation/splitting easier to follow, LiveBundles now have
      a zero-based integer index associated with them.  This gives them identities
      in the debug output.  Also, the spillParent of each bundle is shown, so as to
      expose the spill-parent/child relationships:
      ```
      LB4(parent=LB2 v1 3-3 { 3_def:F:rdi })
      LB5(parent=LB2 v1 6-7 { 7_v1:F:rax })
      LB2(parent=none v1 4-7 { })
      ```
    
    * There is some new debug printing in fn `ResolveControlFlow`.
    
    * The initial live ranges (prior to grouping) and initial live bundles (after
      grouping) are printed differently, to make it obvious that these things are
      different C++ types.  Function `dumpVregs` has accordingly been split into
      `dumpLiveRangesByVReg` and `dumpLiveRangesByBundle`.
    
    * Some finessing of blank lines to make the boundaries between passes clear.
    
    Finally, in `AllocationIntegrityState::checkIntegrity`, there is an existing
    assertion which checks that no definition has multiple conflicting
    assignments. This patch also adds printing to assist diagnosis of such
    failures.  It is guarded by both JS_JITSPEW and IONFLAGS=regalloc.
    
    Differential Revision: https://phabricator.services.mozilla.com/D137509
    
    UltraBlame original commit: 4e8e89273ddb890b904f2da44129f219dc0a7a55

--- js/src/jit/BacktrackingAllocator.h
+++ js/src/jit/BacktrackingAllocator.h
@@ -8,6 +8,7 @@
 #define jit_BacktrackingAllocator_h
 
 #include "mozilla/Array.h"
+#include "mozilla/Atomics.h"
 #include "mozilla/Attributes.h"
 
 #include "ds/PriorityQueue.h"
@@ -363,6 +364,14 @@ class SpillSet : public TempObject {
   void setAllocation(LAllocation alloc);
 };
 
+#ifdef JS_JITSPEW
+
+
+
+static mozilla::Atomic<uint32_t> LiveBundle_debugIdCounter =
+    mozilla::Atomic<uint32_t>{0};
+#endif
+
 
 
 
@@ -382,8 +391,19 @@ class LiveBundle : public TempObject {
   
   LiveBundle* spillParent_;
 
+#ifdef JS_JITSPEW
+  
+  
+  
+  uint32_t debugId_;
+#endif
+
   LiveBundle(SpillSet* spill, LiveBundle* spillParent)
-      : spill_(spill), spillParent_(spillParent) {}
+      : spill_(spill), spillParent_(spillParent) {
+#ifdef JS_JITSPEW
+    debugId_ = LiveBundle_debugIdCounter++;
+#endif
+  }
 
  public:
   static LiveBundle* FallibleNew(TempAllocator& alloc, SpillSet* spill,
@@ -421,6 +441,8 @@ class LiveBundle : public TempObject {
   LiveBundle* spillParent() const { return spillParent_; }
 
 #ifdef JS_JITSPEW
+  uint32_t debugId() const { return debugId_; }
+
   
   UniqueChars toString() const;
 #endif
@@ -764,7 +786,8 @@ class BacktrackingAllocator : protected RegisterAllocator {
 
   bool compilingWasm() { return mir->outerInfo().compilingWasm(); }
 
-  void dumpVregs(const char* who);
+  void dumpLiveRangesByVReg(const char* who);
+  void dumpLiveRangesByBundle(const char* who);
 };
 
 }  
