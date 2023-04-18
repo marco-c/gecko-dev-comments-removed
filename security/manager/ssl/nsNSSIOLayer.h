9b7ecece853061d49200d9e67e4a66e4215440bb
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 22:57:30 +0000

    Bug 1771479 - Add histograms for SSL_HANDSHAKE_RESULT and SSL_TIME_UNTIL_READY for connections using ECH. r=dragana
    Differential Revision: https://phabricator.services.mozilla.com/D147498
    
    UltraBlame original commit: 9bcda147affa5b8bc7ee4d8f9e206481e833b658

--- security/manager/ssl/nsNSSIOLayer.h
+++ security/manager/ssl/nsNSSIOLayer.h
@@ -34,6 +34,13 @@ using mozilla::OriginAttributes;
 
 class nsIObserver;
 
+
+enum class EchExtensionStatus {
+  kNotPresent,  
+  kGREASE,      
+  kReal         
+};
+
 class nsNSSSocketInfo final : public CommonSocketControl {
  public:
   nsNSSSocketInfo(mozilla::psm::SharedSSLState& aState, uint32_t providerFlags,
@@ -95,10 +102,12 @@ class nsNSSSocketInfo final : public CommonSocketControl {
   void SetFullHandshake() { mIsFullHandshake = true; }
   bool IsFullHandshake() const { return mIsFullHandshake; }
 
-  void SetEchGreaseUsed() { mEchGreaseUsed = true; }
-
-  bool WasEchUsed() const { return mEchConfig.Length() > 0; }
-  bool WasEchGreaseUsed() const { return mEchGreaseUsed; }
+  void UpdateEchExtensionStatus(EchExtensionStatus aEchExtensionStatus) {
+    mEchExtensionStatus = std::max(aEchExtensionStatus, mEchExtensionStatus);
+  }
+  EchExtensionStatus GetEchExtensionStatus() const {
+    return mEchExtensionStatus;
+  }
 
   bool GetJoined() { return mJoined; }
   void SetSentClientCert() { mSentClientCert = true; }
@@ -201,7 +210,7 @@ class nsNSSSocketInfo final : public CommonSocketControl {
   bool mFalseStarted;
   bool mIsFullHandshake;
   bool mNotedTimeUntilReady;
-  bool mEchGreaseUsed;
+  EchExtensionStatus mEchExtensionStatus;  
 
   
   
