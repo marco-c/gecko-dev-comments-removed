053c0bdc5bddd238913a59a7f57861ddb0796d37
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 19:28:05 +0000

    Bug 1764998 - Add geckosession API to set priority r=geckoview-reviewers,agi,owlish,jonalmeida
    Corresponding Design Doc: https://docs.google.com/document/d/1xGXQhVLnWBU84WmQ0ebkJ-MqI6o67mXosjxT1p3M26w/edit?usp=sharing
    
    This API will allow apps to set a geckosession/tab to either high priority or default with the priorityHint.
    
    Differential Revision: https://phabricator.services.mozilla.com/D145789
    
    UltraBlame original commit: 9f639bdff32bb98de93214153570547346c6059e

--- mobile/android/geckoview/src/main/java/org/mozilla/geckoview/GeckoSession.java
+++ mobile/android/geckoview/src/main/java/org/mozilla/geckoview/GeckoSession.java
@@ -1580,6 +1580,17 @@ public class GeckoSession {
     return mMagnifier;
   }
 
+  
+  @Retention(RetentionPolicy.SOURCE)
+  @IntDef({PRIORITY_DEFAULT, PRIORITY_HIGH})
+  public @interface Priority {}
+
+  
+  public static final int PRIORITY_DEFAULT = 0;
+
+  
+  public static final int PRIORITY_HIGH = 1;
+
   @Retention(RetentionPolicy.SOURCE)
   @IntDef(
       flag = true,
@@ -2245,6 +2256,22 @@ public class GeckoSession {
     final GeckoBundle msg = new GeckoBundle(1);
     msg.putBoolean("focused", focused);
     mEventDispatcher.dispatch("GeckoView:SetFocused", msg);
+  }
+
+  
+
+
+
+
+
+
+
+
+  @AnyThread
+  public void setPriorityHint(final @Priority int priorityHint) {
+    final GeckoBundle msg = new GeckoBundle(1);
+    msg.putInt("priorityHint", priorityHint);
+    mEventDispatcher.dispatch("GeckoView:SetPriorityHint", msg);
   }
 
   
