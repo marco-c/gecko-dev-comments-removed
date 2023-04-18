b0dacda60d200077f86b362e9a7c5d98cd0b4ede
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 05:27:01 +0000

    Bug 1743623 - Part 1: Remove worker-specific override from JS context options. r=jandem
    Differential Revision: https://phabricator.services.mozilla.com/D133975
    
    UltraBlame original commit: cee175fa09e7eced3e062a38182b98da0f376532

--- dom/workers/RuntimeService.cpp
+++ dom/workers/RuntimeService.cpp
@@ -65,7 +65,6 @@
 #include "nsXPCOMPrivate.h"
 #include "OSFileConstants.h"
 #include "xpcpublic.h"
-#include "XPCPrefableContextOptions.h"
 #include "XPCSelfHostedShmem.h"
 
 #if defined(XP_MACOSX)
@@ -228,23 +227,6 @@ T GetWorkerPref(const nsACString& aPref,
   return result;
 }
 
-
-
-
-bool GetWorkerBoolPref(const char* jsPref, const char* workerPref) {
-  using PrefHelper = PrefTraits<bool>;
-
-  if (PrefHelper::Exists(workerPref)) {
-    return PrefHelper::Get(workerPref);
-  }
-
-  if (PrefHelper::Exists(jsPref)) {
-    return PrefHelper::Get(jsPref);
-  }
-
-  return PrefHelper::kDefaultValue;
-}
-
 void LoadContextOptions(const char* aPrefName, void* ) {
   AssertIsOnMainThread();
 
@@ -275,7 +257,7 @@ void LoadContextOptions(const char* aPrefName, void* ) {
 #endif
 
   JS::ContextOptions contextOptions;
-  xpc::SetPrefableContextOptions(contextOptions, GetWorkerBoolPref);
+  xpc::SetPrefableContextOptions(contextOptions);
 
   nsCOMPtr<nsIXULRuntime> xr = do_GetService("@mozilla.org/xre/runtime;1");
   if (xr) {
