2a8337ab45ae52d6a3c4a7105aed1a4a4070767a
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 15:52:55 +0000

    Bug 1282976 - Part 13: Remove new.target access from frame classes. r=jandem
    Depends on D142806
    
    Differential Revision: https://phabricator.services.mozilla.com/D142835
    
    UltraBlame original commit: 0b35af3e37eabca58dbdf302d8799ee1a358281d

--- js/src/jit/BaselineFrame.h
+++ js/src/jit/BaselineFrame.h
@@ -193,17 +193,6 @@ class BaselineFrame {
                                         ArrayObject* dest) const;
 
  public:
-  Value newTarget() const {
-    MOZ_ASSERT(isFunctionFrame());
-    MOZ_ASSERT(!callee()->isArrow());
-    if (isConstructing()) {
-      return *(Value*)(reinterpret_cast<const uint8_t*>(this) +
-                       BaselineFrame::Size() +
-                       offsetOfArg(std::max(numFormalArgs(), numActualArgs())));
-    }
-    return UndefinedValue();
-  }
-
   void prepareForBaselineInterpreterToJitOSR() {
     
     
