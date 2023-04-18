45c52d72b86a509ce7ef234588cb9eb08ed40b81
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 03:50:26 +0000

    Bug 1746150 - [devtools] Reset vertical scroll position when updating HTMLTooltip container. r=jdescottes.
    This fixes issues with the iframe dropdown when it overflows and targets are
    added/removed.
    
    Differential Revision: https://phabricator.services.mozilla.com/D134039
    
    UltraBlame original commit: 5a47b2bff641cb984493da19843ec9a6afc35470

--- devtools/client/shared/widgets/tooltip/HTMLTooltip.js
+++ devtools/client/shared/widgets/tooltip/HTMLTooltip.js
@@ -560,6 +560,7 @@ HTMLTooltip.prototype = {
     
     
     let measuredHeight;
+    const currentScrollTop = this.panel.scrollTop;
     if (this.preferredWidth === "auto") {
       
       this.container.style.width = "auto";
@@ -663,6 +664,7 @@ HTMLTooltip.prototype = {
     );
 
     this.container.style.height = height + "px";
+    this.panel.scrollTop = currentScrollTop;
 
     return { left, top };
   },
