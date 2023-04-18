73498e4b030872323501ff038ec6e0ab315a4feb
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 05:40:01 +0000

    Bug 1749498 - PrintUsage doesn't exit anymore, exits are done explicitly - r=canaltinova
    `PrintUsageThenExit(code)` was supposed to exit when `code` was not zero, but:
    - The name didn't reflect that, so it was confusing that `PrintUsageThenExit(0)` would *not* exit.
    - The implementation in the Base Profiler exited anyway! This caused issues with some legacy code that still used the now-removed "threads" feature.
    
    This patch renames the function to just `PrintUsage()` and never exits, leaving the caller to invoke `exit(code)` as needed -- with the added benefit that it's possible to exit with a zero code, useful in cases where an exit is not actually an error.
    
    Differential Revision: https://phabricator.services.mozilla.com/D135666
    
    UltraBlame original commit: 7b3600abcc2212fba0d90c53266b579f6af345d1

--- mozglue/baseprofiler/core/platform.cpp
+++ mozglue/baseprofiler/core/platform.cpp
@@ -1985,7 +1985,7 @@ static char FeatureCategory(uint32_t aFeature) {
   return 'x';
 }
 
-static void PrintUsageThenExit(int aExitCode) {
+static void PrintUsage() {
   PrintToConsole(
       "\n"
       "Profiler environment variable usage:\n"
@@ -2088,8 +2088,6 @@ static void PrintUsageThenExit(int aExitCode) {
       "does not support"
 #endif
   );
-
-  exit(aExitCode);
 }
 
 
@@ -2462,7 +2460,7 @@ static uint32_t ParseFeature(const char* aFeature, bool aIsStartup) {
 
   PrintToConsole("\nUnrecognized feature \"%s\".\n\n", aFeature);
   
-  PrintUsageThenExit(0);
+  PrintUsage();
   return 0;
 }
 
@@ -2576,7 +2574,8 @@ void profiler_init(void* aStackTop) {
   MOZ_RELEASE_ASSERT(!CorePS::Exists());
 
   if (getenv("MOZ_BASE_PROFILER_HELP")) {
-    PrintUsageThenExit(0);  
+    PrintUsage();
+    exit(0);
   }
 
   SharedLibraryInfo::Initialize();
@@ -2652,7 +2651,8 @@ void profiler_init(void* aStackTop) {
         PrintToConsole(
             "- MOZ_PROFILER_STARTUP_ENTRIES unit must be one of the "
             "following: KB, KiB, MB, MiB, GB, GiB");
-        PrintUsageThenExit(1);
+        PrintUsage();
+        exit(1);
       }
 
       
@@ -2667,7 +2667,8 @@ void profiler_init(void* aStackTop) {
       } else {
         PrintToConsole("- MOZ_PROFILER_STARTUP_ENTRIES not a valid integer: %s",
                        startupCapacity);
-        PrintUsageThenExit(1);
+        PrintUsage();
+        exit(1);
       }
     }
 
@@ -2685,7 +2686,8 @@ void profiler_init(void* aStackTop) {
       } else {
         PrintToConsole("- MOZ_PROFILER_STARTUP_DURATION not a valid float: %s",
                        startupDuration);
-        PrintUsageThenExit(1);
+        PrintUsage();
+        exit(1);
       }
     }
 
@@ -2701,7 +2703,8 @@ void profiler_init(void* aStackTop) {
       } else {
         PrintToConsole("- MOZ_PROFILER_STARTUP_INTERVAL not a valid float: %s",
                        startupInterval);
-        PrintUsageThenExit(1);
+        PrintUsage();
+        exit(1);
       }
     }
 
@@ -2718,7 +2721,8 @@ void profiler_init(void* aStackTop) {
         PrintToConsole(
             "- MOZ_PROFILER_STARTUP_FEATURES_BITFIELD not a valid integer: %s",
             startupFeaturesBitfield);
-        PrintUsageThenExit(1);
+        PrintUsage();
+        exit(1);
       }
     } else {
       const char* startupFeatures = getenv("MOZ_PROFILER_STARTUP_FEATURES");
