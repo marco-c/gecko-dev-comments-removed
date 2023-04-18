7b2009dc2f1480f72c73d39e50385c61aca9ecfc
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 17:15:17 +0000

    Bug 1765401 - Simplify media query evaluation code a bit. r=boris
    This patch:
    
     * Removes generic <ident> support for media features. These were used
       for some privileged media features but are no longer used.
    
     * Simplifies media feature getters by shifting the responsibility of
       dealing with RangeOrOperator to the caller. This makes it easier to
       implement container-query / mediaqueries-4 syntax, and also cleans up
       the code a bunch.
    
    There should be no change in behavior.
    
    Differential Revision: https://phabricator.services.mozilla.com/D144051
    
    UltraBlame original commit: 115228fb2b7a4a5fbc18a3ae7ec5be0173af2645

--- servo/components/style/media_queries/media_feature.rs
+++ servo/components/style/media_queries/media_feature.rs
@@ -4,7 +4,6 @@
 
 
 
-use super::media_feature_expression::RangeOrOperator;
 use super::Device;
 use crate::parser::ParserContext;
 use crate::values::computed::Ratio;
@@ -17,12 +16,7 @@ use style_traits::ParseError;
 
 pub type KeywordDiscriminant = u8;
 
-type MediaFeatureEvaluator<T> = fn(
-    device: &Device,
-    
-    value: Option<T>,
-    range_or_operator: Option<RangeOrOperator>,
-) -> bool;
+type MediaFeatureGetter<T> = fn(device: &Device) -> T;
 
 
 
@@ -41,14 +35,14 @@ pub type KeywordParser = for<'a, 'i, 't> fn(
 
 #[allow(missing_docs)]
 pub enum Evaluator {
-    Length(MediaFeatureEvaluator<CSSPixelLength>),
-    Integer(MediaFeatureEvaluator<u32>),
-    Float(MediaFeatureEvaluator<f32>),
-    BoolInteger(MediaFeatureEvaluator<bool>),
+    Length(MediaFeatureGetter<CSSPixelLength>),
+    Integer(MediaFeatureGetter<u32>),
+    Float(MediaFeatureGetter<f32>),
+    BoolInteger(MediaFeatureGetter<bool>),
     
-    NumberRatio(MediaFeatureEvaluator<Ratio>),
+    NumberRatio(MediaFeatureGetter<Ratio>),
     
-    Resolution(MediaFeatureEvaluator<Resolution>),
+    Resolution(MediaFeatureGetter<Resolution>),
     
     Enumerated {
         
@@ -60,9 +54,8 @@ pub enum Evaluator {
         serializer: KeywordSerializer,
         
         
-        evaluator: MediaFeatureEvaluator<KeywordDiscriminant>,
+        evaluator: fn(&Device, Option<KeywordDiscriminant>) -> bool,
     },
-    Ident(MediaFeatureEvaluator<Atom>),
 }
 
 
@@ -93,14 +86,7 @@ macro_rules! keyword_evaluator {
         fn __evaluate(
             device: &$crate::media_queries::Device,
             value: Option<$crate::media_queries::media_feature::KeywordDiscriminant>,
-            range_or_operator: Option<
-                $crate::media_queries::media_feature_expression::RangeOrOperator,
-            >,
         ) -> bool {
-            debug_assert!(
-                range_or_operator.is_none(),
-                "Since when do keywords accept ranges?"
-            );
             // This unwrap is ok because the only discriminants that get
             // back to us is the ones that `parse` produces.
             let value: Option<$keyword_type> =
