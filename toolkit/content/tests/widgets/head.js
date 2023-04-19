7e0b094006139fc21fb9666c87619261074b00a3
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 00:02:11 +0000

    Bug 1759199 - Write a test for text recognition; r=nordzilla
    Depends on D148910
    
    Differential Revision: https://phabricator.services.mozilla.com/D148911
    
    UltraBlame original commit: d20f8c147fce4ca963acbe79ea2a7617041fed95

--- toolkit/content/tests/widgets/head.js
+++ toolkit/content/tests/widgets/head.js
@@ -34,6 +34,16 @@ function getElementWithinVideo(video, aValue) {
   return shadowRoot.getElementById(aValue);
 }
 
+
+
+
+
+
+function shadowRootQuerySelectorAll(element, selector) {
+  const shadowRoot = SpecialPowers.wrap(element).openOrClosedShadowRoot;
+  return shadowRoot?.querySelectorAll(selector);
+}
+
 function executeTests() {
   return tests
     .map(fn => () => new Promise(fn))
