1b81e9154a91f598b44db8a4b361aa35b2c74c24
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 05:54:07 +0000

    Bug 1783738 - Support decorator expressions on methods and fields; r=mgaudet
    This adds support for decorator expressions inside classes for methods
    and fields. Support for class declarations and expressions is added in
    the next commit.
    
    Differential Revision: https://phabricator.services.mozilla.com/D154189
    
    UltraBlame original commit: 9cfb9c8223e273a23d48c8e220e1f593d29da109

--- js/src/frontend/BytecodeEmitter.cpp
+++ js/src/frontend/BytecodeEmitter.cpp
@@ -1069,6 +1069,11 @@ restart:
       MOZ_CRASH("Record and Tuple are not supported yet");
 #endif
 
+#ifdef ENABLE_DECORATORS
+    case ParseNodeKind::DecoratorList:
+      MOZ_CRASH("Decorators are not supported yet");
+#endif
+
     
     
     
