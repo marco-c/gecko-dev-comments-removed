7dd54eb9fa84e801a4551bb5a0b33620c7344095
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 21:45:06 +0000

    Bug 1727820 - Add testing for `AppUpdater.stop` r=bhearsum
    `AppUpdater.stop()` is not currently well tested, and the new implementation relies on throwing exceptions. It's important that these exceptions be properly caught to ensure that we don't end up handling the exception as an internal error or not handling it properly at all.
    
    Differential Revision: https://phabricator.services.mozilla.com/D159308
    
    UltraBlame original commit: 9e6a904ec81dd4a9eab5addddb2a3e6642cc1a50

--- toolkit/mozapps/update/tests/browser/head.js
+++ toolkit/mozapps/update/tests/browser/head.js
@@ -662,6 +662,7 @@ function runAboutDialogUpdateTest(params, steps) {
       continueFile,
       downloadInfo,
       forceApply,
+      noContinue,
     } = step;
     return (async function() {
       await TestUtils.waitForCondition(
@@ -722,10 +723,11 @@ function runAboutDialogUpdateTest(params, steps) {
         ok(!gUpdateManager.readyUpdate, "There should not be a ready update");
       }
 
-      if (panelId == "downloading") {
+      
+      
+      if (panelId == "downloading" && continueFile) {
         for (let i = 0; i < downloadInfo.length; ++i) {
           let data = downloadInfo[i];
-          
           await continueFileHandler(continueFile);
           let patch = getPatchOfType(
             data.patchType,
@@ -794,8 +796,9 @@ function runAboutDialogUpdateTest(params, steps) {
         );
       }
 
+      
       let buttonPanels = ["downloadAndInstall", "apply"];
-      if (buttonPanels.includes(panelId)) {
+      if (buttonPanels.includes(panelId) && !noContinue) {
         let buttonEl = selectedPanel.querySelector("button");
         await TestUtils.waitForCondition(
           () => aboutDialog.document.activeElement == buttonEl,
