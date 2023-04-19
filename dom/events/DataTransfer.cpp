757234a3d41adaff6575b2416f47728c37be9052
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 22:28:38 +0000

    Bug 1308007 - Add a pref to allow files in DataTransfer from paste again. r=nika
    Differential Revision: https://phabricator.services.mozilla.com/D153100
    
    UltraBlame original commit: f9cc66c55a1334aba178137ed504368958842334

--- dom/events/DataTransfer.cpp
+++ dom/events/DataTransfer.cpp
@@ -1379,7 +1379,8 @@ void DataTransfer::CacheExternalData(const nsTArray<nsCString>& aTypes,
   for (const nsCString& type : aTypes) {
     if (type.EqualsLiteral(kCustomTypesMime)) {
       FillInExternalCustomTypes(0, aPrincipal);
-    } else if (type.EqualsLiteral(kFileMime) && XRE_IsContentProcess()) {
+    } else if (type.EqualsLiteral(kFileMime) && XRE_IsContentProcess() &&
+               !StaticPrefs::dom_events_dataTransfer_mozFile_enabled()) {
       
       
       
@@ -1390,9 +1391,10 @@ void DataTransfer::CacheExternalData(const nsTArray<nsCString>& aTypes,
       
       
       
-      if (type.EqualsLiteral(kFileMime) && !XRE_IsContentProcess()) {
+      if (type.EqualsLiteral(kFileMime)) {
         hasFileData = true;
       }
+
       
       CacheExternalData(
           type.get(), 0, aPrincipal,
