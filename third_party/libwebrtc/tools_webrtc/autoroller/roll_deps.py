c5421d2c5190d258b8b293b9843a78aebe2b2581
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 15:10:10 +0000

    Bug 1766646 - Vendor libwebrtc from c8f0502f0fd
    Upstream commit: https://webrtc.googlesource.com/src/+/c8f0502f0fd3c737b1dc18808b371d6e25ab1b5f
        Fix typo - Parenthesis was in the wrong place.
    
    
        No-Try: True
        Bug: webrtc:13607
        Change-Id: Iba13bcf94f7a89cdc0e124d42e879b9c900a56cc
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/252003
        Reviewed-by: Mirko Bonadei <mbonadeiwebrtc.org>
        Commit-Queue: Christoffer Jansson <janssongoogle.com>
        Cr-Commit-Position: refs/heads/main{#36043}
    
    UltraBlame original commit: 9608c9d3d766ee9102bf121ae1c7158eb0981ede

--- third_party/libwebrtc/tools_webrtc/autoroller/roll_deps.py
+++ third_party/libwebrtc/tools_webrtc/autoroller/roll_deps.py
@@ -196,7 +196,7 @@ def _ReadGitilesContent(url):
   
   
   base64_content = ReadUrlContent(url + '?format=TEXT')
-  return base64.b64decode(base64_content[0].decode('utf-8'))
+  return base64.b64decode(base64_content[0]).decode('utf-8')
 
 
 def ReadRemoteCrFile(path_below_src, revision):
