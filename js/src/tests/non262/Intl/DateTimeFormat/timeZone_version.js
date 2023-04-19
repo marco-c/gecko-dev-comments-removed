ca60f725322ae2b9982862cb97b8016d475efcf6
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 22:15:07 +0000

    Backed out changeset 5bb3611c95d7 (bug 1797248) for causing time zones failures. CLOSED TREE
    UltraBlame original commit: 87ec281ecf745f90c5600788b92db2b052934c3d

--- js/src/tests/non262/Intl/DateTimeFormat/timeZone_version.js
+++ js/src/tests/non262/Intl/DateTimeFormat/timeZone_version.js
@@ -2,7 +2,7 @@
 
 
 
-const tzdata = "2022e";
+const tzdata = "2022b";
 
 if (typeof getICUOptions === "undefined") {
     var getICUOptions = SpecialPowers.Cu.getJSTestingFunctions().getICUOptions;
