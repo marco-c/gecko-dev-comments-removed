2f5963d8929a04f854e351f6b73c8d10c8a5fd88
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 19:53:30 +0000

    Bug 1791782 - Connect CSS font-palette features to rendering via the presContext. r=emilio
    Differential Revision: https://phabricator.services.mozilla.com/D157959
    
    UltraBlame original commit: e42005941917cc3731902b0c66b77ef7ccfe728d

--- layout/base/nsPresContext.h
+++ layout/base/nsPresContext.h
@@ -92,6 +92,9 @@ class Document;
 class Element;
 enum class PrefersColorSchemeOverride : uint8_t;
 }  
+namespace gfx {
+class FontPaletteValueSet;
+}  
 }  
 
 
@@ -923,6 +926,9 @@ class nsPresContext : public nsISupports, public mozilla::SupportsWeakPtr {
   void FlushFontFeatureValues();
   void MarkFontFeatureValuesDirty() { mFontFeatureValuesDirty = true; }
 
+  void FlushFontPaletteValues();
+  void MarkFontPaletteValuesDirty() { mFontPaletteValuesDirty = true; }
+
   
   
   
@@ -1084,6 +1090,10 @@ class nsPresContext : public nsISupports, public mozilla::SupportsWeakPtr {
     return mFontFeatureValuesLookup;
   }
 
+  mozilla::gfx::FontPaletteValueSet* GetFontPaletteValueSet() const {
+    return mFontPaletteValueSet;
+  }
+
  protected:
   friend class nsRunnableMethod<nsPresContext>;
   void ThemeChangedInternal();
@@ -1185,6 +1195,7 @@ class nsPresContext : public nsISupports, public mozilla::SupportsWeakPtr {
   RefPtr<mozilla::CounterStyleManager> mCounterStyleManager;
   const nsStaticAtom* mMedium;
   RefPtr<gfxFontFeatureValueSet> mFontFeatureValuesLookup;
+  RefPtr<mozilla::gfx::FontPaletteValueSet> mFontPaletteValueSet;
 
   
   
@@ -1339,6 +1350,9 @@ class nsPresContext : public nsISupports, public mozilla::SupportsWeakPtr {
 
   
   unsigned mFontFeatureValuesDirty : 1;
+
+  
+  unsigned mFontPaletteValuesDirty : 1;
 
   unsigned mIsVisual : 1;
 
