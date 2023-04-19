2b67cf06e2eb9cdb14f3de6a6c5ccd5dbbe2fafc
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 15:06:26 +0000

    Bug 1766646 - Vendor libwebrtc from 5423c837317
    Upstream commit: https://webrtc.googlesource.com/src/+/5423c837317317ca55ff9a9d509aba2c83b683ec
        Switch to Popen and adding a timeout for PESQ measurements.
    
        After migrating to python3, the check_output doesn't return, this
        CL switches to communicate() with a timeout of 2 minutes (to avoid
        to block bots for 2 hours in a deadlock).
    
        No-Presubmit: True
        Bug: None
        Change-Id: I3248ab090c074bd35300ca11abc08536cd797664
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/250164
        Reviewed-by: Jeremy Leconte <jlecontegoogle.com>
        Reviewed-by: Christoffer Jansson <janssongoogle.com>
        Commit-Queue: Mirko Bonadei <mbonadeiwebrtc.org>
        Cr-Commit-Position: refs/heads/main{#35882}
    
    UltraBlame original commit: 69bd1453183aef09cc0e88fa86a8f0ecf1558c54

--- third_party/libwebrtc/audio/test/low_bandwidth_audio_test.py
+++ third_party/libwebrtc/audio/test/low_bandwidth_audio_test.py
@@ -177,9 +177,21 @@ def _RunPesq(executable_path,
   
   
   
-  out = subprocess.check_output(_LogCommand(command),
-                                cwd=directory,
-                                stderr=subprocess.STDOUT)
+  process = subprocess.Popen(_LogCommand(command),
+                             cwd=directory,
+                             stdout=subprocess.PIPE,
+                             stderr=subprocess.PIPE)
+
+  try:
+    out, err = process.communicate(timeout=120)
+  except TimeoutExpired:
+    process.kill()
+    out, err = process.communicate()
+
+  if process.returncode != 0:
+    logging.error('%s (exit_code: %d)',
+                  err.decode('utf-8').strip(), process.returncode)
+    return {}
 
   
   match = re.search(
