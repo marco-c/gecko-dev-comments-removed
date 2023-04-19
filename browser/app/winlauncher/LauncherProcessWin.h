adc27dc710a7fd228b4df3d63c136b4616207610
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 04:17:22 +0000

    Bug 1733821 - [8/9] Write deelevation-status enum into the launched parent process  r=mhowell
    Write the deelevation-status enum value into the parent process, where
    it will (hopefully) show up in the event of a crash report.
    
    (Presenting this value more conveniently -- _e.g._, in `about:support`
    and/or Firefox telemetry -- would require additional plumbing, and so
    has been left to future work as bug 1774703.)
    
    This commit was previously submitted as D149546, and has not changed.
    
    Differential Revision: https://phabricator.services.mozilla.com/D152326
    
    UltraBlame original commit: a48e7437ee175e21634e4256a4cffe7c002a7d71

--- browser/app/winlauncher/LauncherProcessWin.h
+++ browser/app/winlauncher/LauncherProcessWin.h
@@ -37,6 +37,7 @@ MOZ_MAKE_ENUM_CLASS_BITWISE_OPERATORS(LauncherFlags);
 
 enum class DeelevationStatus : uint32_t {
   
+  
   Unknown = 0,
 
   
@@ -52,7 +53,17 @@ enum class DeelevationStatus : uint32_t {
   
   
   UnsuccessfullyDeelevated = 5,
+
+  
+  
+  
+  
+  DefaultStaticValue = 0x55AA55AA,
 };
+
+
+
+extern const volatile DeelevationStatus gDeelevationStatus;
 
 }  
 
