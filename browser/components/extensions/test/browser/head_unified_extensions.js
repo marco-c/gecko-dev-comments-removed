b90b388f685fb371561234b2292367a7833e7ff2
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 03:54:31 +0000

    Bug 1777483 - Part 3 - Open context menu when clicking the menu button.  r=mixedpuppy
    This patch allows to open the context menu on (primary) button click,
    and that also works with keyboard navigation.
    
    Depends on D152755
    
    Differential Revision: https://phabricator.services.mozilla.com/D152853
    
    UltraBlame original commit: baf27e2f44d6297fc4aef99d0139ae891ef06fd8

--- browser/components/extensions/test/browser/head_unified_extensions.js
+++ browser/components/extensions/test/browser/head_unified_extensions.js
@@ -98,7 +98,8 @@ const openUnifiedExtensionsContextMenu = async (win, extensionId) => {
   ok(menu, "expected menu");
 
   const shown = BrowserTestUtils.waitForEvent(menu, "popupshown");
-  EventUtils.synthesizeMouseAtCenter(button, { type: "contextmenu" }, win);
+  
+  EventUtils.synthesizeMouseAtCenter(button, {}, win);
   await shown;
 
   return menu;
