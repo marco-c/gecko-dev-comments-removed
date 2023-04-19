a4a36ea32e4631bbc85fef242f9d813e1e952254
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 02:05:51 +0000

    Backed out changeset 2c17688069a7 (bug 1777872) for causing mochitest failures in browser_rules_flexbox-highlighter-on-mutation.js CLOSED TREE
    UltraBlame original commit: deb098aa313734ed8f78f5120572ba4831900b1b

--- dom/canvas/DrawTargetWebgl.h
+++ dom/canvas/DrawTargetWebgl.h
@@ -11,7 +11,6 @@
 #include "mozilla/LinkedList.h"
 #include "mozilla/WeakPtr.h"
 #include "mozilla/ThreadLocal.h"
-#include "mozilla/ipc/Shmem.h"
 #include <vector>
 
 namespace mozilla {
@@ -24,10 +23,6 @@ class WebGLRenderbufferJS;
 class WebGLTextureJS;
 class WebGLUniformLocationJS;
 class WebGLVertexArrayJS;
-
-namespace ipc {
-class ActorLifecycleProxy;
-}
 
 namespace layers {
 class SurfaceDescriptor;
@@ -73,8 +68,6 @@ class DrawTargetWebgl : public DrawTarget, public SupportsWeakPtr {
   RefPtr<WebGLFramebufferJS> mFramebuffer;
   RefPtr<WebGLTextureJS> mTex;
   RefPtr<DrawTargetSkia> mSkia;
-  
-  mozilla::ipc::Shmem mShmem;
   
   RefPtr<DataSourceSurface> mSnapshot;
   
@@ -205,10 +198,6 @@ class DrawTargetWebgl : public DrawTarget, public SupportsWeakPtr {
     
     
     Atomic<bool> mShouldClearCaches;
-    
-    
-    
-    bool mWaitForShmem = false;
 
     const Matrix& GetTransform() const { return mCurrentTarget->mTransform; }
 
@@ -283,8 +272,6 @@ class DrawTargetWebgl : public DrawTarget, public SupportsWeakPtr {
     void ClearAllTextures();
     void ClearEmptyTextureMemory();
     void ClearCachesIfNecessary();
-
-    void WaitForShmem();
   };
 
   RefPtr<SharedContext> mSharedContext;
@@ -445,14 +432,7 @@ class DrawTargetWebgl : public DrawTarget, public SupportsWeakPtr {
   void FlattenSkia();
   bool FlushFromSkia();
 
-  void WaitForShmem() {
-    if (mSharedContext->mWaitForShmem) {
-      mSharedContext->WaitForShmem();
-    }
-  }
-
   void MarkSkiaChanged() {
-    WaitForShmem();
     if (!mSkiaValid) {
       ReadIntoSkia();
     } else if (mSkiaLayer) {
