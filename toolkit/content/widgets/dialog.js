cca3c5cd0ade98fdb1d09a0f66df1c5fd69b0f43
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 17:47:53 +0000

    Backed out changeset 8de3f54dc3ca (bug 1781434) for causing mochitest failures on test_dialogfocus.xhtml . CLOSED TREE
    UltraBlame original commit: 39848ec75579e9f78cc56161649cf2a7075c1ec8

--- toolkit/content/widgets/dialog.js
+++ toolkit/content/widgets/dialog.js
@@ -240,80 +240,73 @@
       window.moveTo(xOffset, yOffset);
     }
 
-    
-    _setInitialFocusIfNeeded() {
-      let focusedElt = document.commandDispatcher.focusedElement;
-      if (focusedElt) {
-        return;
-      }
-
-      const defaultButton = this.getButton(this.defaultButton);
-      Services.focus.moveFocus(
-        window,
-        null,
-        Services.focus.MOVEFOCUS_FORWARD,
-        Services.focus.FLAG_NOPARENTFRAME
-      );
-
-      focusedElt = document.commandDispatcher.focusedElement;
-      if (!focusedElt) {
-        return; 
-      }
-
-      let firstFocusedElt = focusedElt;
-      while (
-        focusedElt.localName == "tab" ||
-        focusedElt.getAttribute("noinitialfocus") == "true"
-      ) {
-        Services.focus.moveFocus(
-          window,
-          focusedElt,
-          Services.focus.MOVEFOCUS_FORWARD,
-          Services.focus.FLAG_NOPARENTFRAME
-        );
-        focusedElt = document.commandDispatcher.focusedElement;
-        if (focusedElt == firstFocusedElt) {
-          if (focusedElt.getAttribute("noinitialfocus") == "true") {
-            focusedElt.blur();
-          }
-          
-          return;
-        }
-      }
-
-      if (firstFocusedElt.localName == "tab") {
-        if (focusedElt.hasAttribute("dlgtype")) {
-          
-          
-          firstFocusedElt.focus();
-        }
-      } else if (
-        AppConstants.platform != "macosx" &&
-        focusedElt.hasAttribute("dlgtype") &&
-        focusedElt != defaultButton
-      ) {
-        defaultButton.focus();
-        if (document.commandDispatcher.focusedElement != defaultButton) {
-          
-          
-          if (firstFocusedElt.getAttribute("noinitialfocus") == "true") {
-            focusedElt.blur();
-          } else {
-            firstFocusedElt.focus();
-          }
-        }
-      }
-    }
-
     postLoadInit(aEvent) {
-      this._setInitialFocusIfNeeded();
-
-      try {
+      let focusInit = () => {
         const defaultButton = this.getButton(this.defaultButton);
-        if (defaultButton) {
-          window.notifyDefaultButtonLoaded(defaultButton);
+
+        
+        let focusedElt = document.commandDispatcher.focusedElement;
+        if (!focusedElt) {
+          Services.focus.moveFocus(
+            window,
+            null,
+            Services.focus.MOVEFOCUS_FORWARD,
+            Services.focus.FLAG_NOPARENTFRAME
+          );
+
+          focusedElt = document.commandDispatcher.focusedElement;
+          if (focusedElt) {
+            var initialFocusedElt = focusedElt;
+            while (
+              focusedElt.localName == "tab" ||
+              focusedElt.getAttribute("noinitialfocus") == "true"
+            ) {
+              Services.focus.moveFocus(
+                window,
+                focusedElt,
+                Services.focus.MOVEFOCUS_FORWARD,
+                Services.focus.FLAG_NOPARENTFRAME
+              );
+              focusedElt = document.commandDispatcher.focusedElement;
+              if (focusedElt) {
+                if (focusedElt == initialFocusedElt) {
+                  if (focusedElt.getAttribute("noinitialfocus") == "true") {
+                    focusedElt.blur();
+                  }
+                  break;
+                }
+              }
+            }
+
+            if (initialFocusedElt.localName == "tab") {
+              if (focusedElt.hasAttribute("dlgtype")) {
+                
+                
+                initialFocusedElt.focus();
+              }
+            } else if (
+              AppConstants.platform != "macosx" &&
+              focusedElt.hasAttribute("dlgtype") &&
+              focusedElt != defaultButton
+            ) {
+              
+              defaultButton.focus();
+              if (document.commandDispatcher.focusedElement != defaultButton) {
+                initialFocusedElt.focus();
+              }
+            }
+          }
         }
-      } catch (e) {}
+
+        try {
+          if (defaultButton) {
+            window.notifyDefaultButtonLoaded(defaultButton);
+          }
+        } catch (e) {}
+      };
+
+      
+      setTimeout(focusInit, 0);
 
       if (this._l10nButtons.length) {
         document.l10n.translateElements(this._l10nButtons).then(() => {
