827d96b818ece6ed88c0c0066b2e40f456fcac74
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 19:38:34 +0000

    Bug 1774300 - Implement VideoFrame Constructor for OffscreenCanvas r=padenot
    This patch allow constructing a VideoFrame from a OffscreenCanvas.
    
    Depends on D149331
    
    Differential Revision: https://phabricator.services.mozilla.com/D149332
    
    UltraBlame original commit: cf5e4d9c1b91542cabf1c697a837ea7eaa211495

--- dom/media/webcodecs/VideoFrame.cpp
+++ dom/media/webcodecs/VideoFrame.cpp
@@ -20,10 +20,12 @@
 #include "mozilla/UniquePtr.h"
 #include "mozilla/dom/DOMRect.h"
 #include "mozilla/dom/ImageBitmap.h"
+#include "mozilla/dom/OffscreenCanvas.h"
 #include "mozilla/dom/Promise.h"
 #include "mozilla/dom/UnionTypes.h"
 #include "mozilla/gfx/2D.h"
 #include "mozilla/gfx/Swizzle.h"
+#include "nsLayoutUtils.h"
 #include "nsIPrincipal.h"
 
 namespace mozilla::dom {
@@ -1100,10 +1102,54 @@ already_AddRefed<VideoFrame> VideoFrame::Constructor(
 
 
 already_AddRefed<VideoFrame> VideoFrame::Constructor(
-    const GlobalObject& global, OffscreenCanvas& offscreenCanvas,
-    const VideoFrameInit& init, ErrorResult& aRv) {
-  aRv.Throw(NS_ERROR_DOM_NOT_SUPPORTED_ERR);
-  return nullptr;
+    const GlobalObject& aGlobal, OffscreenCanvas& aOffscreenCanvas,
+    const VideoFrameInit& aInit, ErrorResult& aRv) {
+  nsCOMPtr<nsIGlobalObject> global = do_QueryInterface(aGlobal.GetAsSupports());
+  if (!global) {
+    aRv.Throw(NS_ERROR_FAILURE);
+    return nullptr;
+  }
+
+  
+  if (aOffscreenCanvas.Width() == 0) {
+    aRv.ThrowInvalidStateError("The canvas has a width of 0");
+    return nullptr;
+  }
+  if (aOffscreenCanvas.Height() == 0) {
+    aRv.ThrowInvalidStateError("The canvas has a height of 0");
+    return nullptr;
+  }
+
+  
+  
+  
+  SurfaceFromElementResult res = nsLayoutUtils::SurfaceFromOffscreenCanvas(
+      &aOffscreenCanvas, nsLayoutUtils::SFE_WANT_FIRST_FRAME_IF_IMAGE);
+  if (res.mIsWriteOnly) {
+    
+    aRv.ThrowSecurityError("The canvas is not same-origin");
+    return nullptr;
+  }
+
+  RefPtr<gfx::SourceSurface> surface = res.GetSourceSurface();
+  if (NS_WARN_IF(!surface)) {
+    aRv.ThrowInvalidStateError("The canvas' surface acquisition failed");
+    return nullptr;
+  }
+
+  if (!aInit.mTimestamp.WasPassed()) {
+    aRv.ThrowTypeError("Missing timestamp");
+    return nullptr;
+  }
+
+  RefPtr<layers::SourceSurfaceImage> image =
+      new layers::SourceSurfaceImage(surface.get());
+  auto r = InitializeFrameWithResourceAndSize(global, aInit, image.forget());
+  if (r.isErr()) {
+    aRv.ThrowTypeError(r.unwrapErr());
+    return nullptr;
+  }
+  return r.unwrap();
 }
 
 
