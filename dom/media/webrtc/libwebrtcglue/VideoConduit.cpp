74581916d77cc3e51960974b5a931f6891b360e3
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 09:38:51 +0000

    Bug 1766646 (MOZ) - VideoReceiveStream::Decoder now has operator==
    UltraBlame original commit: c48c0497fc93978b5e0dd8489f48b33179092892

--- dom/media/webrtc/libwebrtcglue/VideoConduit.cpp
+++ dom/media/webrtc/libwebrtcglue/VideoConduit.cpp
@@ -314,13 +314,6 @@ bool operator==(const webrtc::RtpConfig& aThis,
 }
 #endif
 
-
-bool operator==(const webrtc::VideoReceiveStream::Decoder& aThis,
-                const webrtc::VideoReceiveStream::Decoder& aOther) {
-  return aThis.video_format == aOther.video_format &&
-         aThis.payload_type == aOther.payload_type;
-}
-
 }  
 
 
