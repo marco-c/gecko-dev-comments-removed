0f7d6ccb2470f9ec85a3f508fbb82c20f9d6dbf7
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 10:45:09 +0000

    Bug 1766646 - Vendor libwebrtc from 8498b7e7f6
    Upstream commit: https://webrtc.googlesource.com/src/+/8498b7e7f6b90fa036de2a6887d34256f0565b4f
        Revert "Add dav1d decoder to WebRTC."
    
        This reverts commit 147858577d4db6d257d3cc248fe571a1bbf887e3.
    
        Reason for revert: High binary size increase
    
        Original change's description:
        > Add dav1d decoder to WebRTC.
        >
        > Bug: none
        > Change-Id: I7642f42e592dcf510679f881f118bc4dab93b31c
        > Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/237504
        > Commit-Queue: Philip Eliasson <philipelwebrtc.org>
        > Reviewed-by: Mirko Bonadei <mbonadeiwebrtc.org>
        > Reviewed-by: Danil Chapovalov <danilchapwebrtc.org>
        > Reviewed-by: Magnus Flodman <mflodmanwebrtc.org>
        > Reviewed-by: Sergey Silkin <ssilkinwebrtc.org>
        > Reviewed-by: Ilya Nikolaevskiy <ilnikwebrtc.org>
        > Cr-Commit-Position: refs/heads/main{#35394}
    
        TBR=danilchapwebrtc.org,mbonadeiwebrtc.org,ilnikwebrtc.org,philipelwebrtc.org,mflodmanwebrtc.org,ssilkinwebrtc.org,webrtc-scopedluci-project-accounts.iam.gserviceaccount.com
    
        Change-Id: I00a8acd6ea94ce523c2d5ba705333c9174678180
        No-Presubmit: true
        No-Tree-Checks: true
        No-Try: true
        Bug: none
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/238560
        Reviewed-by: Artem Titov <titovartemwebrtc.org>
        Reviewed-by: Mirko Bonadei <mbonadeiwebrtc.org>
        Owners-Override: Mirko Bonadei <mbonadeiwebrtc.org>
        Owners-Override: Artem Titov <titovartemwebrtc.org>
        Commit-Queue: Mirko Bonadei <mbonadeiwebrtc.org>
        Cr-Commit-Position: refs/heads/main{#35395}
    
    UltraBlame original commit: a2439bd978130de50d8107ebebe6f54c7b5818f6

--- third_party/libwebrtc/media/engine/internal_decoder_factory.cc
+++ third_party/libwebrtc/media/engine/internal_decoder_factory.cc
@@ -15,19 +15,14 @@
 #include "api/video_codecs/video_codec.h"
 #include "media/base/codec.h"
 #include "media/base/media_constants.h"
-#include "modules/video_coding/codecs/av1/dav1d_decoder.h"
 #include "modules/video_coding/codecs/av1/libaom_av1_decoder.h"
 #include "modules/video_coding/codecs/h264/include/h264.h"
 #include "modules/video_coding/codecs/vp8/include/vp8.h"
 #include "modules/video_coding/codecs/vp9/include/vp9.h"
 #include "rtc_base/checks.h"
 #include "rtc_base/logging.h"
-#include "system_wrappers/include/field_trial.h"
 
 namespace webrtc {
-namespace {
-constexpr char kDav1dFieldTrial[] = "WebRTC-Dav1dDecoder";
-}  
 
 std::vector<SdpVideoFormat> InternalDecoderFactory::GetSupportedFormats()
     const {
@@ -37,8 +32,6 @@ std::vector<SdpVideoFormat> InternalDecoderFactory::GetSupportedFormats()
     formats.push_back(format);
   for (const SdpVideoFormat& h264_format : SupportedH264Codecs())
     formats.push_back(h264_format);
-  
-  
   
   
   return formats;
@@ -76,11 +69,6 @@ std::unique_ptr<VideoDecoder> InternalDecoderFactory::CreateVideoDecoder(
     return VP9Decoder::Create();
   if (absl::EqualsIgnoreCase(format.name, cricket::kH264CodecName))
     return H264Decoder::Create();
-  if (absl::EqualsIgnoreCase(format.name, cricket::kAv1CodecName) &&
-      field_trial::IsEnabled(kDav1dFieldTrial)) {
-    return CreateDav1dDecoder();
-  }
-  
   
   
   
