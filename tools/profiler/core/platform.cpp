f96e4dfda976ce3d5b7b2f8787eb056150c3b18e
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 20:07:38 +0000

    Bug 1725076 - Make profile.meta.oscpu show when we are running on Windows 11, r=canaltinova.
    Differential Revision: https://phabricator.services.mozilla.com/D158421
    
    UltraBlame original commit: 93090b1a044a5a0ee5fc0c71a895d40b184102f9

--- tools/profiler/core/platform.cpp
+++ tools/profiler/core/platform.cpp
@@ -2742,6 +2742,25 @@ static PreRecordedMetaInformation PreRecordMetaInformation() {
       info.mHttpOscpu.AppendInt(bugfix);
     } else
 #endif
+#if defined(GP_OS_windows)
+      
+      
+      OSVERSIONINFO ovi = {sizeof(OSVERSIONINFO)};
+    if (GetVersionEx(&ovi)) {
+      info.mHttpOscpu.AppendLiteral("Windows ");
+      
+      
+      info.mHttpOscpu.AppendInt(
+          ovi.dwBuildNumber >= 22000 ? 11 : int32_t(ovi.dwMajorVersion));
+      info.mHttpOscpu.AppendLiteral(".");
+      info.mHttpOscpu.AppendInt(int32_t(ovi.dwMinorVersion));
+#  if defined(_ARM64_)
+      info.mHttpOscpu.AppendLiteral(" Arm64");
+#  endif
+      info.mHttpOscpu.AppendLiteral("; build=");
+      info.mHttpOscpu.AppendInt(int32_t(ovi.dwBuildNumber));
+    } else
+#endif
     {
       Unused << http->GetOscpu(info.mHttpOscpu);
     }
