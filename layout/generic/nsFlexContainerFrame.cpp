60b1337e73194f61d2d79713b7b7f34f8baf7e35
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 18:19:16 +0000

    Bug 1791430 - Remove a "Not Implemented Yet" warning of bug 1480850 in flex container. r=dholbert
    Differential Revision: https://phabricator.services.mozilla.com/D157667
    
    UltraBlame original commit: e4539035f97fe84613a3bd534f05f639e1f6bf05

--- layout/generic/nsFlexContainerFrame.cpp
+++ layout/generic/nsFlexContainerFrame.cpp
@@ -3647,10 +3647,9 @@ CrossAxisPositionTracker::CrossAxisPositionTracker(
   if (mPackingSpaceRemaining != 0) {
     if (mAlignContent.primary == StyleAlignFlags::BASELINE ||
         mAlignContent.primary == StyleAlignFlags::LAST_BASELINE) {
-      NS_WARNING(
-          "NYI: "
-          "align-items/align-self:left/right/self-start/self-end/baseline/"
-          "last baseline");
+      
+      
+      
     } else if (mAlignContent.primary == StyleAlignFlags::FLEX_START) {
       
     } else if (mAlignContent.primary == StyleAlignFlags::FLEX_END) {
