29b4c20da9f910d5b91a385100c3fc80f1589f40
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 05:59:11 +0000

    Bug 1777264 Part 5: Define and emit VIDEO_HDR_PLAY_TIME_MS. r=media-playback-reviewers,alwu
    Differential Revision: https://phabricator.services.mozilla.com/D152820
    
    UltraBlame original commit: 753e0ca024a8a998672ed69ea6248e5fb7317124

--- dom/media/utils/TelemetryProbesReporter.cpp
+++ dom/media/utils/TelemetryProbesReporter.cpp
@@ -386,6 +386,8 @@ void TelemetryProbesReporter::ReportResultForVideo() {
   const double invisiblePlayTimeS = mInvisibleVideoPlayTime.GetAndClearTotal();
   const double videoDecodeSuspendTimeS =
       mVideoDecodeSuspendedTime.GetAndClearTotal();
+  const double totalVideoHDRPlayTimeS =
+      mTotalVideoHDRPlayTime.GetAndClearTotal();
 
   
   if (totalVideoPlayTimeS == 0.0) {
@@ -400,6 +402,15 @@ void TelemetryProbesReporter::ReportResultForVideo() {
   LOG("VIDEO_HIDDEN_PLAY_TIME_S = %f", invisiblePlayTimeS);
   Telemetry::Accumulate(Telemetry::VIDEO_HIDDEN_PLAY_TIME_MS,
                         SECONDS_TO_MS(invisiblePlayTimeS));
+
+  
+  
+  
+  if (totalVideoHDRPlayTimeS > 0.0) {
+    LOG("VIDEO_HDR_PLAY_TIME_S = %f", totalVideoHDRPlayTimeS);
+    Telemetry::Accumulate(Telemetry::VIDEO_HDR_PLAY_TIME_MS,
+                          SECONDS_TO_MS(totalVideoHDRPlayTimeS));
+  }
 
   if (mOwner->IsEncrypted()) {
     LOG("VIDEO_ENCRYPTED_PLAY_TIME_S = %f", totalVideoPlayTimeS);
