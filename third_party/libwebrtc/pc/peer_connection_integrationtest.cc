01684c7ddce58bece1522c993b384343e146d9e7
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 08:12:01 +0000

    Bug 1766646 - Vendor libwebrtc from cbacec52bc
    Upstream commit: https://webrtc.googlesource.com/src/+/cbacec52bc9c120f14329c08a106d43bb8bd1c27
        Monitor the "concealed samples" stat for the audio during negotiation.
    
        Bug: webrtc:12361
        Change-Id: Ib638314f78782d6c3c4ebbb0899f3d6d4cc8e869
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/201727
        Reviewed-by: Henrik Boström <hboswebrtc.org>
        Commit-Queue: Harald Alvestrand <htawebrtc.org>
        Cr-Commit-Position: refs/heads/master{#33057}
    
    UltraBlame original commit: 40d24e9a1dc9b91164a0efa94f4f43046ab82cc4

--- third_party/libwebrtc/pc/peer_connection_integrationtest.cc
+++ third_party/libwebrtc/pc/peer_connection_integrationtest.cc
@@ -617,6 +617,8 @@ class PeerConnectionWrapper : public webrtc::PeerConnectionObserver,
     ASSERT_EQ(audio_track_stats_id_, *rtp_stats->track_id);
     audio_packets_stat_ = *rtp_stats->packets_received;
     audio_delay_stat_ = *track_stats->relative_packet_arrival_delay;
+    audio_samples_stat_ = *track_stats->total_samples_received;
+    audio_concealed_stat_ = *track_stats->concealed_samples;
   }
 
   void UpdateDelayStats(std::string tag, int desc_size) {
@@ -634,10 +636,33 @@ class PeerConnectionWrapper : public webrtc::PeerConnectionObserver,
     
     
     
-    ASSERT_GT(0.1, recent_delay) << tag << " size " << desc_size;
+    EXPECT_GT(0.1, recent_delay) << tag << " size " << desc_size;
+    auto delta_samples =
+        *track_stats->total_samples_received - audio_samples_stat_;
+    auto delta_concealed =
+        *track_stats->concealed_samples - audio_concealed_stat_;
+    
+    
+    
+    
+    
+
+    
+    
+    
+    
+    EXPECT_GT(15000U, delta_concealed) << "Concealed " << delta_concealed
+                                       << " of " << delta_samples << " samples";
+    if (delta_samples > 0) {
+      EXPECT_GT(0.6, 1.0 * delta_concealed / delta_samples)
+          << "Concealed " << delta_concealed << " of " << delta_samples
+          << " samples";
+    }
     
     audio_packets_stat_ = *rtp_stats->packets_received;
     audio_delay_stat_ = *track_stats->relative_packet_arrival_delay;
+    audio_samples_stat_ = *track_stats->total_samples_received;
+    audio_concealed_stat_ = *track_stats->concealed_samples;
   }
 
  private:
@@ -1111,6 +1136,8 @@ class PeerConnectionWrapper : public webrtc::PeerConnectionObserver,
   
   int audio_packets_stat_ = 0;
   double audio_delay_stat_ = 0.0;
+  uint64_t audio_samples_stat_ = 0;
+  uint64_t audio_concealed_stat_ = 0;
   std::string rtp_stats_id_;
   std::string audio_track_stats_id_;
 
