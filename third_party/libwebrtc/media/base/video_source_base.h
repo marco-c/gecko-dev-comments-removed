ab287ac38752bede0bcc31a0fe10face1d6ef5bb
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 09:19:21 +0000

    Bug 1766646 - Vendor libwebrtc from 096ad02c02
    Upstream commit: https://webrtc.googlesource.com/src/+/096ad02c02b4bc6c046282b8793ef84d041dd0d8
        Revert "Fix race between enabled() and set_enabled() in VideoTrack."
    
        This reverts commit 5ffefe9d2d743c66f8a8bcbc5ad9662a3138840a.
    
        Reason for revert: Breaks Chromium Android browser tests on fyi bots.
    
        Original change's description:
        > Fix race between enabled() and set_enabled() in VideoTrack.
        >
        > Along the way I introduced VideoSourceBaseGuarded, which is equivalent
        > to VideoSourceBase except that it applies thread checks. I found that
        > it's easy to use VideoSourceBase incorrectly and in fact there appear
        > to be tests that do this.
        >
        > I made the source object const in VideoTrack, as it already was in
        > AudioTrack, and that allowed for making the GetSource() accessors
        > bypass the proxy thread hop and give the caller direct access.
        >
        > Bug: webrtc:12773, b/188139639, webrtc:12780
        > Change-Id: I022175c4239a1306ef54059c131d81411d5124fe
        > Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/219160
        > Reviewed-by: Mirko Bonadei <mbonadeiwebrtc.org>
        > Reviewed-by: Andrey Logvin <landreywebrtc.org>
        > Commit-Queue: Tommi <tommiwebrtc.org>
        > Cr-Commit-Position: refs/heads/master{#34096}
    
        TBR=mbonadeiwebrtc.org,tommiwebrtc.org,landreywebrtc.org,webrtc-scopedluci-project-accounts.iam.gserviceaccount.com
    
        Change-Id: I16323d459c76eb6a87cc602a0048f6ee01c81626
        No-Presubmit: true
        No-Tree-Checks: true
        No-Try: true
        Bug: webrtc:12773
        Bug: b/188139639
        Bug: webrtc:12780
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/219637
        Reviewed-by: Evan Shrubsole <eshrgoogle.com>
        Commit-Queue: Evan Shrubsole <eshrgoogle.com>
        Cr-Commit-Position: refs/heads/master{#34101}
    
    UltraBlame original commit: 3031436ebbec3658900c5e069543e89b06f6d7c5

--- third_party/libwebrtc/media/base/video_source_base.h
+++ third_party/libwebrtc/media/base/video_source_base.h
@@ -17,12 +17,8 @@
 #include "api/video/video_frame.h"
 #include "api/video/video_sink_interface.h"
 #include "api/video/video_source_interface.h"
-#include "rtc_base/system/no_unique_address.h"
 
 namespace rtc {
-
-
-
 
 
 class VideoSourceBase : public VideoSourceInterface<webrtc::VideoFrame> {
@@ -46,36 +42,6 @@ class VideoSourceBase : public VideoSourceInterface<webrtc::VideoFrame> {
 
  private:
   std::vector<SinkPair> sinks_;
-};
-
-
-
-class VideoSourceBaseGuarded : public VideoSourceInterface<webrtc::VideoFrame> {
- public:
-  VideoSourceBaseGuarded();
-  ~VideoSourceBaseGuarded() override;
-
-  void AddOrUpdateSink(VideoSinkInterface<webrtc::VideoFrame>* sink,
-                       const VideoSinkWants& wants) override;
-  void RemoveSink(VideoSinkInterface<webrtc::VideoFrame>* sink) override;
-
- protected:
-  struct SinkPair {
-    SinkPair(VideoSinkInterface<webrtc::VideoFrame>* sink, VideoSinkWants wants)
-        : sink(sink), wants(wants) {}
-    VideoSinkInterface<webrtc::VideoFrame>* sink;
-    VideoSinkWants wants;
-  };
-
-  SinkPair* FindSinkPair(const VideoSinkInterface<webrtc::VideoFrame>* sink);
-  const std::vector<SinkPair>& sink_pairs() const;
-
-  
-  
-  RTC_NO_UNIQUE_ADDRESS webrtc::SequenceChecker source_sequence_;
-
- private:
-  std::vector<SinkPair> sinks_ RTC_GUARDED_BY(&source_sequence_);
 };
 
 }  
