8b902e96471da344e72881d4f51765a880a4ebe8
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 17:14:12 +0000

    Bug 1789270 - Add support for workers in xpcshell tests; r=dom-storage-reviewers,jari
    Depends on D156474
    
    Differential Revision: https://phabricator.services.mozilla.com/D156475
    
    UltraBlame original commit: 8bfc85c67ae3e6952c9c800f2344a950ec35436a

---
+++ dom/quota/test/modules/system/worker/.eslintrc.js
@@ -0,0 +1,21 @@
+
+
+
+
+
+"use strict";
+
+module.exports = {
+  env: {
+    worker: true,
+  },
+
+  overrides: [
+    {
+      files: ["head.js"],
+      env: {
+        worker: true,
+      },
+    },
+  ],
+};
