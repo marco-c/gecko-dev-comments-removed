1b6d36b599c582805956bc16d1163a1436426463
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 04:00:55 +0000

    Bug 1781782 - Run frequent xpcshell parallel failing tests sequential by default. r=ahal,necko-reviewers,extension-reviewers,application-update-reviewers,bytesized,rpl,kershaw
    Differential Revision: https://phabricator.services.mozilla.com/D153081
    
    UltraBlame original commit: 9fe9e9146b01dd3b0ee684474f3c53cf9f2f80e4

--- testing/xpcshell/runxpcshelltests.py
+++ testing/xpcshell/runxpcshelltests.py
@@ -178,7 +178,7 @@ class XPCShellTestThread(Thread):
         self.crashAsPass = kwargs.get("crashAsPass")
         self.conditionedProfileDir = kwargs.get("conditionedProfileDir")
         if self.runFailures:
-            retry = False
+            self.retry = False
 
         
         self.prefsFile = self.rootPrefsFile
@@ -2116,11 +2116,12 @@ class XPCShellTests(object):
                         "after killing one with SIGINT)"
                     )
                     break
-                
-                test.retry = False
                 self.start_test(test)
                 test.join()
                 self.test_ended(test)
+                if test.failCount > 0 or test.passCount <= 0:
+                    self.try_again_list.append(test.test_object)
+                    continue
                 self.addTestResults(test)
                 
                 if test.exception:
