6dbb699dd7b50f1364e9096713fd5faa5e948285
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 03:54:03 +0000

    Bug 1780312 - Turn off the Linux nvidia driver's shader cache in the RDD process. r=gcp
    We were already turning off Mesa's shader cache in the RDD process,
    because it's not useful given that we're only using video codec
    acceleration and moving images around, and it does a few things related
    to trying to access the cache that the sandbox would have to accomodate.
    
    This patch does the equivalent thing for the nvidia proprietary driver;
    we don't support it for media codec acceleration, but it can still be
    loaded in that process (e.g., on multi-GPU systems) and it's trying to
    call `statfs` on startup which may be related.
    
    Differential Revision: https://phabricator.services.mozilla.com/D152932
    
    UltraBlame original commit: 1ba53f776c9af5a94799e30e713e13fa08b6d893

--- ipc/glue/GeckoChildProcessHost.cpp
+++ ipc/glue/GeckoChildProcessHost.cpp
@@ -428,6 +428,8 @@ GeckoChildProcessHost::GeckoChildProcessHost(GeckoProcessType aProcessType,
     
     mLaunchOptions->env_map["MESA_GLSL_CACHE_DISABLE"] = "true";
     mLaunchOptions->env_map["MESA_SHADER_CACHE_DISABLE"] = "true";
+    
+    mLaunchOptions->env_map["__GL_SHADER_DISK_CACHE"] = "0";
   }
 #endif
 #if defined(MOZ_ENABLE_FORKSERVER)
