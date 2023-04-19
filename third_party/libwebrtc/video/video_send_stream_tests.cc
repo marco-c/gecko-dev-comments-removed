e027b62428317c082c74ea2d8770aff031d77272
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 10:34:41 +0000

    Bug 1766646 - Vendor libwebrtc from 011b5d1545
    Upstream commit: https://webrtc.googlesource.com/src/+/011b5d15457b979639cac52c417cfb91372b0d98
        Disable VideoSendStreamTest.MinTransmitBitrateRespectsRemb in Mac.
    
        Test is flaky there, especially with the task queue pacer enabled.
    
        Bug: webrtc:13294
        Change-Id: I1b2fdbbf6b760bd0cbeb8992442549c7d50abb09
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/235660
        Reviewed-by: Evan Shrubsole <eshrwebrtc.org>
        Commit-Queue: Evan Shrubsole <eshrwebrtc.org>
        Cr-Commit-Position: refs/heads/main{#35232}
    
    UltraBlame original commit: 38facbe028adc85b601da6a3466a4aefa00e3a6b

--- third_party/libwebrtc/video/video_send_stream_tests.cc
+++ third_party/libwebrtc/video/video_send_stream_tests.cc
@@ -1458,7 +1458,14 @@ TEST_F(VideoSendStreamTest, PaddingIsPrimarilyRetransmissions) {
 
 
 
-TEST_F(VideoSendStreamTest, MinTransmitBitrateRespectsRemb) {
+
+#if defined(WEBRTC_MAC)
+#define MAYBE_MinTransmitBitrateRespectsRemb \
+  DISABLED_MinTransmitBitrateRespectsRemb
+#else
+#define MAYBE_MinTransmitBitrateRespectsRemb MinTransmitBitrateRespectsRemb
+#endif
+TEST_F(VideoSendStreamTest, MAYBE_MinTransmitBitrateRespectsRemb) {
   static const int kMinTransmitBitrateBps = 400000;
   static const int kHighBitrateBps = 150000;
   static const int kRembBitrateBps = 80000;
