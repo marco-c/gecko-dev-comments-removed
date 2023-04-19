3af054927e016fb0fd89f52ec59afc394d9c504b
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 21:11:37 +0000

    Bug 1790398 - Core implemention of the WebTransport session r=necko-reviewers,kershaw,valentin
    AltSvc and 0RTT will not be used with WebTransport.
    
    Differential Revision: https://phabricator.services.mozilla.com/D157821
    
    UltraBlame original commit: 55d75ccb094532983258d81f61de8436536ad4f7

--- netwerk/protocol/http/nsHttpTransaction.cpp
+++ netwerk/protocol/http/nsHttpTransaction.cpp
@@ -24,6 +24,7 @@
 #include "nsCRT.h"
 #include "nsComponentManagerUtils.h"  
 #include "nsHttpBasicAuth.h"
+#include "nsHttpChannel.h"
 #include "nsHttpChunkedDecoder.h"
 #include "nsHttpDigestAuth.h"
 #include "nsHttpHandler.h"
@@ -434,6 +435,13 @@ nsresult nsHttpTransaction::AsyncRead(nsIStreamListener* listener,
   transactionPump.forget(pump);
   MutexAutoLock lock(mLock);
   mEarlyHintObserver = do_QueryInterface(listener);
+
+  RefPtr<nsHttpChannel> httpChannel = do_QueryObject(listener);
+  if (httpChannel) {
+    mWebTransportSessionEventListener =
+        httpChannel->GetWebTransportSessionEventListener();
+  }
+
   return NS_OK;
 }
 
@@ -1350,6 +1358,7 @@ void nsHttpTransaction::Close(nsresult reason) {
   {
     MutexAutoLock lock(mLock);
     mEarlyHintObserver = nullptr;
+    mWebTransportSessionEventListener = nullptr;
   }
 
   if (!mClosed) {
@@ -2178,6 +2187,22 @@ nsresult nsHttpTransaction::HandleContentStart() {
 
     
     switch (mResponseHead->Status()) {
+      case 200: {
+        if (!mIsForWebTransport) {
+          break;
+        }
+        RefPtr<Http3WebTransportSession> wtSession =
+            mConnection->GetWebTransportSession(this);
+        if (wtSession) {
+          mWebTransportSessionEventListener->OnSessionReadyInternal(wtSession);
+          wtSession->SetWebTransportSessionEventListener(
+              mWebTransportSessionEventListener);
+        }
+        mWebTransportSessionEventListener = nullptr;
+      }
+        
+        
+        [[fallthrough]];
       case 101:
         mPreserveStream = true;
         [[fallthrough]];  
