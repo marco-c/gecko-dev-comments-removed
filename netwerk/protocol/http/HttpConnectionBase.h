2b67236379b5e3c8df3b9707f2e21f64d7494d00
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 21:01:22 +0000

    Bug 1790398 - Core implemention of the WebTransport session r=necko-reviewers,kershaw,valentin
    AltSvc and 0RTT will not be used with WebTransport.
    
    Differential Revision: https://phabricator.services.mozilla.com/D157821
    
    UltraBlame original commit: e9bf863986965ddfd169219cc1ac9fe312b19ffd

--- netwerk/protocol/http/HttpConnectionBase.h
+++ netwerk/protocol/http/HttpConnectionBase.h
@@ -31,6 +31,7 @@ namespace net {
 
 class nsHttpHandler;
 class ASpdySession;
+class Http3WebTransportSession;
 
 
 #define HTTPCONNECTIONBASE_IID                       \
@@ -81,6 +82,11 @@ class HttpConnectionBase : public nsSupportsWeakReference {
   [[nodiscard]] virtual nsresult TakeTransport(nsISocketTransport**,
                                                nsIAsyncInputStream**,
                                                nsIAsyncOutputStream**) = 0;
+
+  Http3WebTransportSession* GetWebTransportSession(
+      nsAHttpTransaction* aTransaction) {
+    return nullptr;
+  }
 
   virtual bool UsingSpdy() { return false; }
   virtual bool UsingHttp3() { return false; }
