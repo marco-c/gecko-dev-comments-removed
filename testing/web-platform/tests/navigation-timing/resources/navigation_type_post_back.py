d044e5fcc5f5e3f2c38b067e428c3c306db86c55
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 21:29:22 +0000

    Bug 1794627 [wpt PR 36141] - Change WebNavigationType assignment logic, a=testonly
    Automatic update from web-platform-tests
    Change WebNavigationType assignment logic
    
    In Chromium, when navigating back to a page that is loaded by POST method, the navigation type is not back_forward as Firefox is.
    This CL fix this issue. For investigation, see https://docs.google.com/document/d/
    1b3xntaIctQTwgF5gUGjtr2TUHA5uSoqmvM-fIza81Rs/edit?usp=sharing
    
    Bug: 1083373
    Change-Id: I895116f0c6d92df12283ce7b3359410ac76e12ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3887581
    Commit-Queue: Hao Liu <haoliukchromium.org>
    Reviewed-by: Yoav Weiss <yoavweisschromium.org>
    Reviewed-by: danakj <danakjchromium.org>
    Cr-Commit-Position: refs/heads/main{#1057477}
    
    --
    
    wpt-commits: 49545b75e90eaa9d143d7e01751d2bc86e12439d
    wpt-pr: 36141
    
    UltraBlame original commit: fad13a068f655acfe903ce2c39e982d4b9873783

---
+++ testing/web-platform/tests/navigation-timing/resources/navigation_type_post_back.py
@@ -0,0 +1,12 @@
+
+
+def main(request, response):
+    content = """
+    <!DOCTYPE html>
+      <html>
+        <body>
+            <a href="blank_page_green.html">navigate away</a>.
+        </body>
+      </html>
+    """
+    return content
