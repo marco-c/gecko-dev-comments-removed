c85fd8e400722ea523ac3292b21e0c116220f24c
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 00:17:17 +0000

    Bug 1774694: Function, browser, media and volume keys should not hide cursor on Windows r=cmartin
    Also includes the Fn key, which isn't documented to map to a special virtual key code but maps to 0xff, an undefined code, on tested machines.
    
    Differential Revision: https://phabricator.services.mozilla.com/D149928
    
    UltraBlame original commit: 88fa88f269e625353a68be2201a8470e7f45f31a

--- widget/windows/nsWindow.cpp
+++ widget/windows/nsWindow.cpp
@@ -671,11 +671,16 @@ static bool IsMouseVanishKey(WPARAM aVirtKey) {
     case VK_RIGHT:
     case VK_PRIOR:  
     case VK_NEXT:   
+    case 0xff:      
       return false;
     default:
       
+      
+      
+      
       return (GetKeyState(VK_CONTROL) & 0x8000) != 0x8000 &&
-             (GetKeyState(VK_MENU) & 0x8000) != 0x8000;
+             (GetKeyState(VK_MENU) & 0x8000) != 0x8000 &&
+             (aVirtKey < VK_F1 || aVirtKey > VK_LAUNCH_APP2);
   }
 }
 
