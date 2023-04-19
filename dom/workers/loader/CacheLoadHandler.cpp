3030fe3350bdd78036132e516b0f9ec3ec584895
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 02:29:49 +0000

    Bug 1742438 - Prep Part 4: Change cancellation to not rely on iterating over mLoadingRequests r=asuth
    The joys of multi-threaded programming:
    
    As you likely noticed, we now have list modification where previously we didn't.
    We need move all list modification on the worker side, not the main thread side.
    The only reason this isn't showing up on thread sanitizer now, is because the main
    thread is acting as the owner of the the lists.
    
    In order to implement Worker ownership, we need to remove all list traversal from the main thread
    runnable. There are two significant cases in which we traverse lists: Cancellation, and Dispatching.
    Here we handle part of the story for cancellation.
    
    The strategy we take is fundamentally the same as in the DOM Script Loader -- We use the
    "mCanceledMainThread" flag to indicate to our load handlers that the thread has been cancelled.
    This is taken directly from the DOM loader [1] approach.
    
    [1]: https://searchfox.org/mozilla-central/rev/997a56b018662e2940c99bbaf57a6ac9d1aa5422/dom/script/ScriptLoadHandler.cpp#128-132
    
    Differential Revision: https://phabricator.services.mozilla.com/D145446
    
    UltraBlame original commit: 8be847627149b75c5529417e6b980dc80efcfacf

--- dom/workers/loader/CacheLoadHandler.cpp
+++ dom/workers/loader/CacheLoadHandler.cpp
@@ -70,7 +70,7 @@ void CachePromiseHandler::RejectedCallback(JSContext* aCx,
 
   
   
-  mLoader->DeleteCache();
+  mLoader->DeleteCache(NS_ERROR_FAILURE);
 }
 
 CacheCreator::CacheCreator(WorkerPrivate* aWorkerPrivate)
@@ -196,7 +196,7 @@ void CacheCreator::ResolvedCallback(JSContext* aCx,
   }
 }
 
-void CacheCreator::DeleteCache() {
+void CacheCreator::DeleteCache(nsresult aReason) {
   AssertIsOnMainThread();
 
   
@@ -249,15 +249,10 @@ void CacheLoadHandler::Fail(nsresult aRv) {
 
   mLoadInfo->mCacheStatus = ScriptLoadInfo::Cancel;
 
-  
-  
-  if (mLoadInfo->mLoadingFinished) {
-    MOZ_ASSERT(!mLoadInfo->mChannel);
-    MOZ_ASSERT_IF(mLoadInfo->mCachePromise,
-                  mLoadInfo->mCacheStatus == ScriptLoadInfo::WritingToCache ||
-                      mLoadInfo->mCacheStatus == ScriptLoadInfo::Cancel);
-    return;
+  if (mLoadInfo->mCachePromise) {
+    mLoadInfo->mCachePromise->MaybeReject(aRv);
   }
+  mLoadInfo->mCachePromise = nullptr;
 
   mLoader->LoadingFinished(mLoadInfo, aRv);
 }
