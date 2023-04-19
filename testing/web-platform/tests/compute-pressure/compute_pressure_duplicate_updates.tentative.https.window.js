721510e85e9952d521e8188a82b5f04999c4de44
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 21:19:20 +0000

    Bug 1793421 [wpt PR 36103] - compute pressure: Implement pressure factor in Blink, a=testonly
    Automatic update from web-platform-tests
    compute pressure: Implement pressure factor in Blink
    
    As described in [1], pressureRecords contains also pressureFactors.
    This patch is introducing the pressureFactors from the pressureManager.
    
    The algorithm to extract the pressure factor from the platformCollector
    is not yet implemented and therefore pressureFactor value is hardcoded
    in this patch.
    
    The Blink part is fully functional and tested with new wpt tests.
    
    [1] https://wicg.github.io/compute-pressure/#contributing-factors
    
    Bug: 1356191
    Change-Id: I0e4b6c32244729d92108f4b58c573dc6df0cdbfb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3904313
    Reviewed-by: Fr <beaufort.francoisgmail.com>
    Reviewed-by: Raphael Kubo Da Costa <raphael.kubo.da.costaintel.com>
    Reviewed-by: Will Harris <wfhchromium.org>
    Commit-Queue: Arnaud Mandy <arnaud.mandyintel.com>
    Cr-Commit-Position: refs/heads/main{#1054113}
    
    --
    
    wpt-commits: 401f48c6eefeebbd3a3c20e4f6050bb03d8f69b5
    wpt-pr: 36103
    
    UltraBlame original commit: 5c022d64cf2a4f5145ad1677a1393531106e1468

--- testing/web-platform/tests/compute-pressure/compute_pressure_duplicate_updates.tentative.https.window.js
+++ testing/web-platform/tests/compute-pressure/compute_pressure_duplicate_updates.tentative.https.window.js
@@ -18,7 +18,7 @@ pressure_test(async (t, mockPressureService) => {
     mockPressureService.startPlatformCollector( 1.0);
     
     await t.step_wait(
-        () => mockPressureService.updatesDelivered() >= (updatesDelivered + 3),
+        () => mockPressureService.updatesDelivered() >= (updatesDelivered + 2),
         'Wait for more than one update to be delivered to the observer');
     mockPressureService.setPressureUpdate('nominal');
     
@@ -29,3 +29,33 @@ pressure_test(async (t, mockPressureService) => {
   assert_equals(pressureChanges[0][0].state, 'critical');
   assert_equals(pressureChanges[1][0].state, 'nominal');
 }, 'Changes that fail the "has change in data" test are discarded.');
+
+pressure_test(async (t, mockPressureService) => {
+  const pressureChanges = await new Promise(async resolve => {
+    const observer_changes = [];
+    let n = 0;
+    const observer = new PressureObserver(changes => {
+      observer_changes.push(changes);
+      if (++n === 2)
+        resolve(observer_changes);
+    }, {sampleRate: 1.0});
+    observer.observe('cpu');
+    const updatesDelivered = mockPressureService.updatesDelivered();
+    mockPressureService.setPressureUpdate('critical', ['thermal']);
+    mockPressureService.startPlatformCollector( 1.0);
+
+    
+    await t.step_wait(
+        () => mockPressureService.updatesDelivered() >= (updatesDelivered + 2),
+        'Wait for more than one update to be delivered to the observer');
+    mockPressureService.setPressureUpdate('critical', ['power-supply']);
+    
+    
+    
+  });
+  assert_equals(pressureChanges.length, 2);
+  assert_equals(pressureChanges[0][0].state, 'critical');
+  assert_equals(pressureChanges[0][0].factors[0], 'thermal');
+  assert_equals(pressureChanges[1][0].state, 'critical');
+  assert_equals(pressureChanges[1][0].factors[0], 'power-supply');
+}, 'Factors that fail the "has change in data" test are discarded.');
