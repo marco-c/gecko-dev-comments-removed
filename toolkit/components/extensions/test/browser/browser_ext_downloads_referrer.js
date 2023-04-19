fb576c72d598c7590b628b5bfc78df1b6371735d
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 06:50:36 +0000

    Bug 1775525 - Fix browser_ext_downloads_referrer.js intermittent failures on windows. r=robwu
    Differential Revision: https://phabricator.services.mozilla.com/D155395
    
    UltraBlame original commit: 383bc68b8ff3e9a12573c00156d4de8c5e72efea

--- toolkit/components/extensions/test/browser/browser_ext_downloads_referrer.js
+++ toolkit/components/extensions/test/browser/browser_ext_downloads_referrer.js
@@ -24,7 +24,7 @@ async function triggerSaveAs({ selector }) {
   contextMenu.activateItem(saveLinkCommand);
 }
 
-add_task(function test_setup() {
+add_setup(() => {
   const tempDir = Services.dirsvc.get("TmpD", Ci.nsIFile);
   tempDir.append("test-download-dir");
   if (!tempDir.exists()) {
@@ -62,6 +62,13 @@ add_task(async function test_download_item_referrer_info() {
       browser.downloads.onCreated.addListener(async downloadInfo => {
         browser.test.sendMessage("download-on-created", downloadInfo);
       });
+      browser.downloads.onChanged.addListener(async downloadInfo => {
+        
+        if (downloadInfo.state?.current !== "complete") {
+          return;
+        }
+        browser.test.sendMessage("download-completed");
+      });
 
       
       
@@ -80,6 +87,9 @@ add_task(async function test_download_item_referrer_info() {
     is(downloadInfo.url, DOWNLOAD_URL, "Got the expected download url");
     is(downloadInfo.referrer, TEST_URL, "Got the expected referrer");
   });
+
+  
+  await extension.awaitMessage("download-completed");
 
   await extension.unload();
 });
