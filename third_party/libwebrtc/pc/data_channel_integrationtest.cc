94569a3f4618bc115bd5faa5c7636330743a15d3
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 09:16:40 +0000

    Bug 1766646 - Vendor libwebrtc from 86bd92fca9
    Upstream commit: https://webrtc.googlesource.com/src/+/86bd92fca9f70bbb7e5c72ec6e2d447d0b324d44
        Add test for many non-dropped packets in unreliable mode.
    
        These tests document existing behavior; there are notes about where
        this behavior is either surprising or wrong.
    
        Bug: chromium:1148951
        Change-Id: If9875fb744c44c129ff9949d1bab3d3d99f17b81
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/217520
        Commit-Queue: Harald Alvestrand <htawebrtc.org>
        Reviewed-by: Victor Boivie <boiviewebrtc.org>
        Cr-Commit-Position: refs/heads/master{#34058}
    
    UltraBlame original commit: 32924e0ab6fa85ddf4e8fb4e14c7e74d3f8d4def

--- third_party/libwebrtc/pc/data_channel_integrationtest.cc
+++ third_party/libwebrtc/pc/data_channel_integrationtest.cc
@@ -27,6 +27,7 @@
 #include "rtc_base/gunit.h"
 #include "rtc_base/ref_counted_object.h"
 #include "rtc_base/virtual_socket_server.h"
+#include "system_wrappers/include/field_trial.h"
 #include "test/gtest.h"
 
 namespace webrtc {
@@ -622,7 +623,7 @@ TEST_P(DataChannelIntegrationTest, QueuedPacketsGetDeliveredInReliableMode) {
                  kDefaultTimeout);
 }
 
-TEST_P(DataChannelIntegrationTest, QueuedPacketsGetDeliveredInUnReliableMode) {
+TEST_P(DataChannelIntegrationTest, QueuedPacketsGetDroppedInUnreliableMode) {
   CreatePeerConnectionWrappers();
   ConnectFakeSignaling();
   DataChannelInit init;
@@ -637,6 +638,9 @@ TEST_P(DataChannelIntegrationTest, QueuedPacketsGetDeliveredInUnReliableMode) {
                  kDefaultTimeout);
   
   virtual_socket_server()->set_drop_probability(1.0);
+  
+  
+  
   for (int i = 1; i <= 10; i++) {
     caller()->data_channel()->Send(DataBuffer("Sent while blocked"));
   }
@@ -657,6 +661,116 @@ TEST_P(DataChannelIntegrationTest, QueuedPacketsGetDeliveredInUnReliableMode) {
   EXPECT_LE(2u, callee()->data_observer()->received_message_count());
   
   EXPECT_EQ(2u, callee()->data_observer()->received_message_count());
+}
+
+TEST_P(DataChannelIntegrationTest,
+       QueuedPacketsGetDroppedInLifetimeLimitedMode) {
+  CreatePeerConnectionWrappers();
+  ConnectFakeSignaling();
+  DataChannelInit init;
+  init.maxRetransmitTime = 1;
+  init.ordered = false;
+  caller()->CreateDataChannel(&init);
+  caller()->CreateAndSetAndSignalOffer();
+  ASSERT_TRUE_WAIT(SignalingStateStable(), kDefaultTimeout);
+  ASSERT_TRUE_WAIT(callee()->data_channel(), kDefaultTimeout);
+  caller()->data_channel()->Send(DataBuffer("hello first"));
+  ASSERT_EQ_WAIT(1u, callee()->data_observer()->received_message_count(),
+                 kDefaultTimeout);
+  
+  virtual_socket_server()->set_drop_probability(1.0);
+  for (int i = 1; i <= 200; i++) {
+    caller()->data_channel()->Send(DataBuffer("Sent while blocked"));
+  }
+  
+  
+  
+  WAIT(false, 10);
+  EXPECT_EQ(1u, callee()->data_observer()->received_message_count());
+  
+  virtual_socket_server()->set_drop_probability(0.0);
+  
+  caller()->data_channel()->Send(DataBuffer("After block"));
+  EXPECT_EQ_WAIT("After block", callee()->data_observer()->last_message(),
+                 kDefaultTimeout);
+  
+  
+  
+  EXPECT_GT(202u, callee()->data_observer()->received_message_count());
+  EXPECT_LE(2u, callee()->data_observer()->received_message_count());
+  
+  if (webrtc::field_trial::IsEnabled("WebRTC-DataChannel-Dcsctp")) {
+    
+    EXPECT_EQ(2u, callee()->data_observer()->received_message_count());
+  } else {
+    
+    
+    
+    
+    
+    EXPECT_EQ(90u, callee()->data_observer()->received_message_count());
+  }
+}
+
+TEST_P(DataChannelIntegrationTest,
+       SomeQueuedPacketsGetDroppedInMaxRetransmitsMode) {
+  CreatePeerConnectionWrappers();
+  ConnectFakeSignaling();
+  DataChannelInit init;
+  init.maxRetransmits = 0;
+  init.ordered = false;
+  caller()->CreateDataChannel(&init);
+  caller()->CreateAndSetAndSignalOffer();
+  ASSERT_TRUE_WAIT(SignalingStateStable(), kDefaultTimeout);
+  ASSERT_TRUE_WAIT(callee()->data_channel(), kDefaultTimeout);
+  caller()->data_channel()->Send(DataBuffer("hello first"));
+  ASSERT_EQ_WAIT(1u, callee()->data_observer()->received_message_count(),
+                 kDefaultTimeout);
+  
+  virtual_socket_server()->set_drop_probability(1.0);
+  
+  size_t packet_counter = 0;
+  while (caller()->data_channel()->buffered_amount() < 1 &&
+         packet_counter < 10000) {
+    packet_counter++;
+    caller()->data_channel()->Send(DataBuffer("Sent while blocked"));
+  }
+  if (caller()->data_channel()->buffered_amount()) {
+    RTC_LOG(LS_INFO) << "Buffered data after " << packet_counter << " packets";
+  } else {
+    RTC_LOG(LS_INFO) << "No buffered data after " << packet_counter
+                     << " packets";
+  }
+  
+  
+  WAIT(false, 10);
+  EXPECT_EQ(1u, callee()->data_observer()->received_message_count());
+  
+  virtual_socket_server()->set_drop_probability(0.0);
+  
+  caller()->data_channel()->Send(DataBuffer("After block"));
+  EXPECT_EQ_WAIT("After block", callee()->data_observer()->last_message(),
+                 kDefaultTimeout);
+  
+  
+  
+  
+  
+  
+  
+
+  
+  EXPECT_GT(packet_counter,
+            callee()->data_observer()->received_message_count());
+  EXPECT_LE(2u, callee()->data_observer()->received_message_count());
+  
+  
+  
+  
+  EXPECT_GT(2 + packet_counter - 100,
+            callee()->data_observer()->received_message_count());
+  EXPECT_LT(2 + packet_counter - 500,
+            callee()->data_observer()->received_message_count());
 }
 
 INSTANTIATE_TEST_SUITE_P(
