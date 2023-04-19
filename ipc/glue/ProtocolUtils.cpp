49b49dd7867bc2d7d2669e732252114fdb8a907e
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 04:17:53 +0000

    Bug 1782505 - Observe destroyed actors in IPC fuzzing. r=nika
    Differential Revision: https://phabricator.services.mozilla.com/D153344
    
    UltraBlame original commit: 830e31db6b7c321fa9e1e35498728fd4dae00d29

--- ipc/glue/ProtocolUtils.cpp
+++ ipc/glue/ProtocolUtils.cpp
@@ -546,6 +546,10 @@ void IProtocol::DestroySubtree(ActorDestroyReason aWhy) {
   MOZ_ASSERT(CanRecv(), "destroying non-connected actor");
   MOZ_ASSERT(mLifecycleProxy, "destroying zombie actor");
 
+#ifdef FUZZING_SNAPSHOT
+  fuzzing::IPCFuzzController::instance().OnActorDestroyed(this);
+#endif
+
   
   if (Manager()) {
     Unregister(Id());
