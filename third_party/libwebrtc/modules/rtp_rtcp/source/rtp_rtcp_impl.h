6f34d26d6ecf0455f9fbd4fe4e2f25bfe8422bca
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 07:49:09 +0000

    Bug 1766646 - Vendor libwebrtc from cd982137df
    Upstream commit: https://webrtc.googlesource.com/src/+/cd982137dfd88ca4e5fc595100d4b5c32b4d9e0f
        Add missing RTC_GUARDED_BY for ModuleRtpRtcpImpl::rtt_ms_
    
        Bug: None
        Change-Id: I7aef516e4310a7ff14a8bbc77c6edd488167d18d
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/195338
        Reviewed-by: Danil Chapovalov <danilchapwebrtc.org>
        Commit-Queue: Niels Moller <nissewebrtc.org>
        Cr-Commit-Position: refs/heads/master{#32711}
    
    UltraBlame original commit: e041c3549fb36c9c6bf7a585a52d24fd15542a81

--- third_party/libwebrtc/modules/rtp_rtcp/source/rtp_rtcp_impl.h
+++ third_party/libwebrtc/modules/rtp_rtcp/source/rtp_rtcp_impl.h
@@ -352,7 +352,7 @@ class ModuleRtpRtcpImpl : public RtpRtcp, public RTCPReceiver::ModuleRtpRtcp {
 
   
   mutable Mutex mutex_rtt_;
-  int64_t rtt_ms_;
+  int64_t rtt_ms_ RTC_GUARDED_BY(mutex_rtt_);
 };
 
 }  
