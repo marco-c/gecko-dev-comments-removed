b5cadd45d07f7703af5ed40e21e4e6ddbdf83f6b
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 17:05:09 +0000

    Bug 1783821 - [devtools] Load Launcher.sys.mjs via ChromeUtils.importESModule. r=perftest-reviewers,bomsy,arai,sparky
    This is the result of:
    $ ./mach esmify --imports . --prefix=devtools/client/framework/browser-toolbox
    
    Differential Revision: https://phabricator.services.mozilla.com/D154072
    
    UltraBlame original commit: 7c6d06d2adaa26766cd815576a63aea69bb57c4a

--- devtools/client/debugger/test/mochitest/browser_dbg-chrome-create.js
+++ devtools/client/debugger/test/mochitest/browser_dbg-chrome-create.js
@@ -16,8 +16,8 @@ PromiseTestUtils.allowMatchingRejectionsGlobally(/NS_ERROR_FAILURE/);
 
 requestLongerTimeout(5);
 
-const { BrowserToolboxLauncher } = ChromeUtils.import(
-  "resource://devtools/client/framework/browser-toolbox/Launcher.jsm"
+const { BrowserToolboxLauncher } = ChromeUtils.importESModule(
+  "resource://devtools/client/framework/browser-toolbox/Launcher.sys.mjs"
 );
 
 add_task(async function() {
