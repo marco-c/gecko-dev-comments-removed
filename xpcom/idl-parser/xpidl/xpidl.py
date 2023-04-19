109ec39f3de88dd01f997c928a1ad49c235e8c03
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 03:45:18 +0000

    Bug 1779326 - Handle a few more `native` types in rust-xpidl, r=xpcom-reviewers,kmag
    This makes the logic for the rust type line up a bit more with the C++
    logic for existing types, and adds support for 'char' and 'char16_t'
    native types (for 'charPtr').
    
    This specifically enables `nsIInputStream::Read` to be used from Rust.
    
    Differential Revision: https://phabricator.services.mozilla.com/D152715
    
    UltraBlame original commit: c480ca374be9daf9102a43b422c7c9f1ad01006f

--- xpcom/idl-parser/xpidl/xpidl.py
+++ xpcom/idl-parser/xpidl/xpidl.py
@@ -195,8 +195,8 @@ builtinNames = [
     Builtin("double", "double", "libc::c_double", True, False),
     Builtin("char", "char", "libc::c_char", True, False),
     Builtin("string", "char *", "*const libc::c_char", False, False),
-    Builtin("wchar", "char16_t", "i16", False, False),
-    Builtin("wstring", "char16_t *", "*const i16", False, False),
+    Builtin("wchar", "char16_t", "u16", False, False),
+    Builtin("wstring", "char16_t *", "*const u16", False, False),
 ]
 
 builtinMap = {}
@@ -647,33 +647,61 @@ class Native(object):
         if self.modifier not in ["ptr", "ref"]:
             raise RustNoncompat("Rust only supports [ref] / [ptr] native types")
 
-        prefix = "*mut " if "out" in calltype else "*const "
-        if "out" in calltype and self.modifier == "ptr":
-            prefix += "*mut "
+        if shared:
+            if calltype != "out":
+                raise IDLError(
+                    "[shared] only applies to out parameters.", self.location
+                )
+            const = True
 
-        if self.specialtype == "nsid":
-            if "element" in calltype:
-                if self.isPtr(calltype):
-                    raise IDLError(
-                        "Array<nsIDPtr> not yet supported. "
-                        "File an XPConnect bug if you need it.",
-                        self.location,
-                    )
-                return self.nativename
-            return prefix + self.nativename
-        if self.specialtype in ["cstring", "utf8string"]:
-            if "element" in calltype:
-                return "::nsstring::nsCString"
-            return prefix + "::nsstring::nsACString"
-        if self.specialtype == "astring":
-            if "element" in calltype:
-                return "::nsstring::nsString"
-            return prefix + "::nsstring::nsAString"
-        if self.nativename == "void":
-            return prefix + "libc::c_void"
+        
+        if self.specialtype == "nsid" and calltype == "in":
+            const = True
+
+        prefix = "*const " if const or shared else "*mut "
+        if "out" in calltype and self.isPtr(calltype):
+            prefix = "*mut " + prefix
 
         if self.specialtype:
-            raise RustNoncompat("specialtype %s unsupported" % self.specialtype)
+            
+            if self.specialtype in ["cstring", "utf8string"]:
+                if calltype == "in":
+                    return "*const ::nsstring::nsACString"
+                elif "out" in calltype:
+                    return "*mut ::nsstring::nsACString"
+                else:
+                    return "::nsstring::nsCString"
+            if self.specialtype == "astring":
+                if calltype == "in":
+                    return "*const ::nsstring::nsAString"
+                elif "out" in calltype:
+                    return "*mut ::nsstring::nsAString"
+                else:
+                    return "::nsstring::nsString"
+            
+            
+            if self.specialtype == "nsid":
+                if "element" in calltype:
+                    if self.isPtr(calltype):
+                        raise IDLError(
+                            "Array<nsIDPtr> not yet supported. "
+                            "File an XPConnect bug if you need it.",
+                            self.location,
+                        )
+                    return self.nativename
+                return prefix + self.nativename
+            raise RustNoncompat("special type %s unsupported" % self.specialtype)
+
+        
+        
+        
+        if self.nativename == "void":
+            return prefix + "libc::c_void"
+        if self.nativename == "char":
+            return prefix + "libc::c_char"
+        if self.nativename == "char16_t":
+            return prefix + "u16"
+
         raise RustNoncompat("native type %s unsupported" % self.nativename)
 
     def __str__(self):
