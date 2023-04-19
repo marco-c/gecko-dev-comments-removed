4bca9bdf8cb00e89f25217ecd11b2477a94ce6c4
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 18:58:34 +0000

    Backed out changeset 2afbb35af4d7 (bug 1492570) for causing build bustages in js/src/vm/Realm.cpp CLOSED TREE
    UltraBlame original commit: c11769369d46bd7037efd0430f9b549466f2298e

--- js/src/vm/Realm.h
+++ js/src/vm/Realm.h
@@ -56,22 +56,22 @@ struct NativeIterator;
 
 
 class DtoaCache {
-  double dbl;
+  double d;
   int base;
-  JSLinearString* str;  
+  JSLinearString* s;  
 
  public:
-  DtoaCache() : str(nullptr) {}
-  void purge() { str = nullptr; }
+  DtoaCache() : s(nullptr) {}
+  void purge() { s = nullptr; }
 
-  JSLinearString* lookup(int b, double d) {
-    return str && b == base && d == dbl ? str : nullptr;
+  JSLinearString* lookup(int base, double d) {
+    return this->s && base == this->base && d == this->d ? this->s : nullptr;
   }
 
-  void cache(int b, double d, JSLinearString* s) {
-    base = b;
-    dbl = d;
-    str = s;
+  void cache(int base, double d, JSLinearString* s) {
+    this->base = base;
+    this->d = d;
+    this->s = s;
   }
 
 #ifdef JSGC_HASH_TABLE_CHECKS
