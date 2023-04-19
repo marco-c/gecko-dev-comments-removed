dcbd9eb821d4f327eac5b7fc8fa18f68f7b9a707
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 19:26:05 +0000

    Bug 1784481 - Use ScriptLoaderInterface methods for reporting errors; r=jonco
    Fill out the ReportErrorToConsole method, and add documentation for ReportWarningToConsole
    
    Depends on D147313
    
    Differential Revision: https://phabricator.services.mozilla.com/D147314
    
    UltraBlame original commit: 9d91e7bba726af1193b210effdb921b90de3db79

--- dom/workers/ScriptLoader.h
+++ dom/workers/ScriptLoader.h
@@ -253,11 +253,13 @@ class WorkerScriptLoader : public JS::loader::ScriptLoaderInterface,
   }
 
   void ReportErrorToConsole(ScriptLoadRequest* aRequest,
-                            nsresult aResult) const override {}
+                            nsresult aResult) const override;
 
+  
   void ReportWarningToConsole(
       ScriptLoadRequest* aRequest, const char* aMessageName,
       const nsTArray<nsString>& aParams = nsTArray<nsString>()) const override {
+    MOZ_CRASH("Import maps have not been implemented for this context");
   }
 
   void LogExceptionToConsole(JSContext* aCx, WorkerPrivate* aWorkerPrivate);
