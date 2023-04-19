60bf3aa0b9262c9a63b06ec2eeb605d844bd233e
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 03:18:06 +0000

    Bug 1780372 - Make extension APIs deal with async window state changes. r=mixedpuppy
    Before my previous patch, the widget code lied, and always returned the
    requested window state before it had actually happened. Instead, teach
    the extension code to wait if necessary for the window state to be
    effective.
    
    Differential Revision: https://phabricator.services.mozilla.com/D152394
    
    UltraBlame original commit: 81c9cc81146b08ed739c7218b206d4bcfed1d9b3

--- browser/components/extensions/parent/ext-browser.js
+++ browser/components/extensions/parent/ext-browser.js
@@ -1013,35 +1013,51 @@ class Window extends WindowBase {
       [window.STATE_MAXIMIZED]: "maximized",
       [window.STATE_MINIMIZED]: "minimized",
       [window.STATE_NORMAL]: "normal",
+      [window.STATE_FULLSCREEN]: "fullscreen",
     };
-    let state = STATES[window.windowState];
-    if (window.fullScreen) {
-      state = "fullscreen";
-    }
-    return state;
+    return STATES[window.windowState];
   }
 
   get state() {
     return Window.getState(this.window);
   }
 
-  set state(state) {
+  async setState(state) {
     let { window } = this;
-    if (state !== "fullscreen" && window.fullScreen) {
+
+    const expectedState = (function() {
+      switch (state) {
+        case "maximized":
+          return window.STATE_MAXIMIZED;
+        case "minimized":
+        case "docked":
+          return window.STATE_MINIMIZED;
+        case "normal":
+          return window.STATE_NORMAL;
+        case "fullscreen":
+          return window.STATE_FULLSCREEN;
+      }
+      throw new Error(`Unexpected window state: ${state}`);
+    })();
+
+    const initialState = window.windowState;
+    if (expectedState == initialState) {
+      return;
+    }
+
+    if (initialState == window.STATE_FULLSCREEN) {
       window.fullScreen = false;
     }
 
-    switch (state) {
-      case "maximized":
+    switch (expectedState) {
+      case window.STATE_MAXIMIZED:
         window.maximize();
         break;
-
-      case "minimized":
-      case "docked":
+      case window.STATE_MINIMIZED:
         window.minimize();
         break;
 
-      case "normal":
+      case window.STATE_NORMAL:
         
         
         
@@ -1056,12 +1072,36 @@ class Window extends WindowBase {
         }
         break;
 
-      case "fullscreen":
+      case window.STATE_FULLSCREEN:
         window.fullScreen = true;
         break;
 
       default:
         throw new Error(`Unexpected window state: ${state}`);
+    }
+
+    if (window.windowState != expectedState) {
+      
+      
+      
+      const noWindowManagerTimeout = 2000;
+
+      let onSizeModeChange;
+      const promiseExpectedSizeMode = new Promise(resolve => {
+        onSizeModeChange = function() {
+          if (window.windowState == expectedState) {
+            resolve();
+          }
+        };
+        window.addEventListener("sizemodechange", onSizeModeChange);
+      });
+
+      await Promise.any([
+        promiseExpectedSizeMode,
+        new Promise(resolve => setTimeout(resolve, noWindowManagerTimeout)),
+      ]);
+
+      window.removeEventListener("sizemodechange", onSizeModeChange);
     }
   }
 
