66f31fff408bd4ffb072792e8a3fd0b8d6b9691c
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 05:20:24 +0000

    Bug 1782364 - [devtools] Test files with encoded characters in the debugger. r=bomsy
    This at least ensure that the sources display with the right display in the Source Tree.
    
    Differential Revision: https://phabricator.services.mozilla.com/D154082
    
    UltraBlame original commit: e9bd0a704230ddc9da6cded3f4ec607b3ecadced

--- devtools/client/debugger/test/mochitest/head.js
+++ devtools/client/debugger/test/mochitest/head.js
@@ -120,6 +120,8 @@ const INTEGRATION_TEST_PAGE_SOURCES = [
   "removed-original.js",
   "named-eval.js",
   
+  "文字コード.js",
+  
   "bootstrap 3b1a221408fdde86aa49",
   "bootstrap a1ecee2f86e1d0ea3fb5",
   "bootstrap 6fda1f7ea9ecbc1a2d5b",
