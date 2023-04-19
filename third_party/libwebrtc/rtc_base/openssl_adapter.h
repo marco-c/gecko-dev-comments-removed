d3f4ac7b482ca7cf01fbf30c203192ce3228b776
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 10:16:05 +0000

    Bug 1766646 - Vendor libwebrtc from b32650e219c
    Upstream commit: https://webrtc.googlesource.com/src/+/b32650e219caae46c651dc64d9eb2607e2fa9738
        Improve SSLVerifyCallback variable naming
    
        The names used were confusing.
    
        Bug: chromium:1247577
        Change-Id: I007f8b9b6b9c2188cbfc2dcfb2499acf3c14a9ce
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/231683
        Reviewed-by: Mirko Bonadei <mbonadeiwebrtc.org>
        Commit-Queue: Harald Alvestrand <htawebrtc.org>
        Cr-Commit-Position: refs/heads/main{#34967}
    
    UltraBlame original commit: bae9c9fb8dd8d9e31b5d7bc2f556c93bf7df3775

--- third_party/libwebrtc/rtc_base/openssl_adapter.h
+++ third_party/libwebrtc/rtc_base/openssl_adapter.h
@@ -131,7 +131,9 @@ class OpenSSLAdapter final : public SSLAdapter,
   enum ssl_verify_result_t SSLVerifyInternal(SSL* ssl, uint8_t* out_alert);
 #else
   static int SSLVerifyCallback(int ok, X509_STORE_CTX* store);
-  int SSLVerifyInternal(int ok, SSL* ssl, X509_STORE_CTX* store);
+  
+  
+  int SSLVerifyInternal(int status_on_error, SSL* ssl, X509_STORE_CTX* store);
 #endif
   friend class OpenSSLStreamAdapter;  
 
