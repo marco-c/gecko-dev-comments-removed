e8452166d1711ed2e81fb8f42870828a0c274391
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 19:57:47 +0000

    Bug 1792235 - Change ZydisAPI print LIMIT variable to 48 from 36 r=jandem
    If there's something I'm missing here that's totally fine, I just keep making
    this change locally and reverting it and it would be convenient to just leave
    it.
    
    Differential Revision: https://phabricator.services.mozilla.com/D158038
    
    UltraBlame original commit: e8e4f37c2afffc8c82d29917d44098358b0ad06e

--- js/src/zydis/ZydisAPI.cpp
+++ js/src/zydis/ZydisAPI.cpp
@@ -49,8 +49,8 @@ void zydisDisassemble(const uint8_t* code, size_t codeLen,
   while (ZYAN_SUCCESS(ZydisDecoderDecodeBuffer(&decoder, code + offset, length - offset,
                                                &instruction)))
   {
-#  define LIMIT 36
-#  define LIMSTR "36"
+#  define LIMIT 48
+#  define LIMSTR "48"
 
     
     
