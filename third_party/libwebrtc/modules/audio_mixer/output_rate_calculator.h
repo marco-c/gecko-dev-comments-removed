808cf1cf57cd6d1ecd2d21d3dc5d2f9dfdc9e122
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 07:32:52 +0000

    Bug 1766646 - Vendor libwebrtc from 81bf2fe945
    Upstream commit: https://webrtc.googlesource.com/src/+/81bf2fe9457f74898ea2add214947861ecf27837
        Cleanup after sorting out dependencies of OutputRateCalculator
    
        Bug: webrtc:12035,webrtc:12036
        Change-Id: I774f640a96b80e4942e4166f69475fe47f1bd0ea
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/189801
        Reviewed-by: Karl Wiberg <kwibergwebrtc.org>
        Commit-Queue: Olga Sharonova <olkawebrtc.org>
        Cr-Commit-Position: refs/heads/master{#32451}
    
    UltraBlame original commit: 167afa01b1a859681a4186b7159d47eca18fd3f7

--- third_party/libwebrtc/modules/audio_mixer/output_rate_calculator.h
+++ third_party/libwebrtc/modules/audio_mixer/output_rate_calculator.h
@@ -22,19 +22,7 @@ namespace webrtc {
 class OutputRateCalculator {
  public:
   virtual int CalculateOutputRateFromRange(
-      rtc::ArrayView<const int> preferred_sample_rates) {
-    
-    std::vector<int> sample_rates(preferred_sample_rates.cbegin(),
-                                  preferred_sample_rates.cend());
-    return CalculateOutputRate(sample_rates);
-  }
-
-  
-  
-  virtual int CalculateOutputRate(
-      const std::vector<int>& preferred_sample_rates) {
-    return CalculateOutputRateFromRange(preferred_sample_rates);
-  }
+      rtc::ArrayView<const int> preferred_sample_rates) = 0;
 
   virtual ~OutputRateCalculator() {}
 };
