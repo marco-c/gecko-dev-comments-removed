c99ca5241be7ece6acddaa1023ae8f06f11b6edf
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 17:16:36 +0000

    Bug 1785804 - Part 1: Move GC thing trace methods to an inline header file r=sfink
    This moves all GC thing trace methods (and optimised GCMarker versions that do
    eager marking) to their own header. With later patches this should allow
    devirtualisation of the tracer dispatch.
    
    A follow on bug will attempt to remove some of the GCMarker versions.
    
    Depends on D156558
    
    Differential Revision: https://phabricator.services.mozilla.com/D156559
    
    UltraBlame original commit: 271975a555d02ca138d63458de336e07d2b7fac3

--- js/src/gc/Tracer.h
+++ js/src/gc/Tracer.h
@@ -55,6 +55,8 @@ namespace js {
 
 
 
+class GCMarker;
+
 namespace gc {
 
 
@@ -87,8 +89,10 @@ bool TraceWeakMapKeyInternal(JSTracer* trc, Zone* zone, T* thingp,
 
 #ifdef DEBUG
 void AssertRootMarkingPhase(JSTracer* trc);
+void AssertShouldMarkInZone(GCMarker* marker, gc::Cell* thing);
 #else
 inline void AssertRootMarkingPhase(JSTracer* trc) {}
+inline void AssertShouldMarkInZone(GCMarker* marker, gc::Cell* thing) {}
 #endif
 
 }  
