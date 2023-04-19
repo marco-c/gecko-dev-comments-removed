5e354e9783762f7b0b3f7091172e96362655dbef
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 03:17:42 +0000

    Backed out changeset b083e38b2ed9 (bug 1776514) for causing failure at browser_all_files_referenced.js. CLOSED TREE
    UltraBlame original commit: 2174465e14880c21e5fa0e63aacd7bd1eff5fe5a

--- toolkit/components/backgroundtasks/BackgroundTasks.cpp
+++ toolkit/components/backgroundtasks/BackgroundTasks.cpp
@@ -209,7 +209,6 @@ bool BackgroundTasks::IsUpdatingTaskName(const nsCString& aName) {
 
 bool BackgroundTasks::IsEphemeralProfileTaskName(const nsCString& aName) {
   return !(aName.EqualsLiteral("backgroundupdate") ||
-           aName.EqualsLiteral("message") ||  
            aName.EqualsLiteral("not_ephemeral_profile"));
 }
 
