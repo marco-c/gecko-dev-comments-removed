7869c1d49ef390917a64812886696db35ceb773b
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 09:21:10 +0000

    Bug 1766646 - Vendor libwebrtc from a9af50f151
    Upstream commit: https://webrtc.googlesource.com/src/+/a9af50f151699282a394bf3be019c67cdf4adb69
        Introduce CreateDataChannelOrError
    
        Deprecate CreateDataChannel, and make it a simple wrapper function.
    
        Bug: webrtc:12796
        Change-Id: I053d75a264596ba87ca734a29df9241de93a80c3
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/219784
        Reviewed-by: Xavier Lepaul‎ <xalepwebrtc.org>
        Reviewed-by: Henrik Boström <hboswebrtc.org>
        Commit-Queue: Harald Alvestrand <htawebrtc.org>
        Cr-Commit-Position: refs/heads/master{#34130}
    
    UltraBlame original commit: d69781bcae2848bb3838194f961a0b0d0aa6b505

--- third_party/libwebrtc/api/peer_connection_interface.h
+++ third_party/libwebrtc/api/peer_connection_interface.h
@@ -920,9 +920,24 @@ class RTC_EXPORT PeerConnectionInterface : public rtc::RefCountInterface {
   
   
   
+  virtual RTCErrorOr<rtc::scoped_refptr<DataChannelInterface>>
+  CreateDataChannelOrError(const std::string& label,
+                           const DataChannelInit* config) {
+    return RTCError(RTCErrorType::INTERNAL_ERROR, "dummy function called");
+  }
+  
+  
+  ABSL_DEPRECATED("Use CreateDataChannelOrError")
   virtual rtc::scoped_refptr<DataChannelInterface> CreateDataChannel(
       const std::string& label,
-      const DataChannelInit* config) = 0;
+      const DataChannelInit* config) {
+    auto result = CreateDataChannelOrError(label, config);
+    if (!result.ok()) {
+      return nullptr;
+    } else {
+      return result.MoveValue();
+    }
+  }
 
   
   
