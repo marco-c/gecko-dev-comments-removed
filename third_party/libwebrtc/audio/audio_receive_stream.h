42cf213ada1083a4f745b935dd2ce2fe0154cff1
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 16:02:28 +0000

    Bug 1766646 - Vendor libwebrtc from cc50b04c02
    Upstream commit: https://webrtc.googlesource.com/src/+/cc50b04c021ae16f57a0b2dd11bb1ec62a2a6db1
        Remove config() getter from AudioReceiveStream().
    
        This reduces the surface of externally accessible state that belongs
        to the class, which makes it easier to control what state belongs to
        what thread. In this CL enforcing remote_ssrc() to be conceptually const
        and sync_group to conceptually belong to the packet delivery thread.
    
        Bug: webrtc:11993
        Change-Id: I7de9366dc0c2bf451b5c58595c2d073b4016f2e7
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/261450
        Reviewed-by: Niels Moller <nissewebrtc.org>
        Commit-Queue: Tomas Gunnarsson <tommiwebrtc.org>
        Cr-Commit-Position: refs/heads/main{#36813}
    
    UltraBlame original commit: 7392d1054df089296081615bbe67185faf1f7e68

--- third_party/libwebrtc/audio/audio_receive_stream.h
+++ third_party/libwebrtc/audio/audio_receive_stream.h
@@ -134,7 +134,10 @@ class AudioReceiveStream final : public webrtc::AudioReceiveStream,
     return config_.rtp.remote_ssrc;
   }
 
-  const webrtc::AudioReceiveStream::Config& config() const;
+  
+  
+  const std::string& sync_group() const;
+
   const AudioSendStream* GetAssociatedSendStreamForTesting() const;
 
   
