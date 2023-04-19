f5f1169e3c46d3d9dc3a13a203d5d6cba843c3b7
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 16:33:33 +0000

    Bug 1781929 - Part 3: Include `launchUrl` in Windows toast notification. r=nrishel
    Differential Revision: https://phabricator.services.mozilla.com/D155909
    
    UltraBlame original commit: 8e6fe1c18e8dba0534eebfd87fa276e99b07966a

--- widget/windows/ToastNotificationHandler.cpp
+++ widget/windows/ToastNotificationHandler.cpp
@@ -156,7 +156,7 @@ static bool AddActionNode(ComPtr<IXmlDocument>& toastXml,
 
 
 
-static Result<nsString, nsresult> GetLaunchArgument() {
+Result<nsString, nsresult> ToastNotificationHandler::GetLaunchArgument() {
   nsString launchArg;
 
   
@@ -211,6 +211,10 @@ static Result<nsString, nsresult> GetLaunchArgument() {
     nsAutoString profilePath;
     MOZ_TRY(profDir->GetPath(profilePath));
     launchArg += u"\nprofile\n"_ns + profilePath;
+  }
+
+  if (!mLaunchUrl.IsEmpty()) {
+    launchArg += u"\nlaunchUrl\n"_ns + mLaunchUrl;
   }
 
   return launchArg;
