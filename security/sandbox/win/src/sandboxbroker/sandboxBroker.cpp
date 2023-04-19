76f2ec8d948b2359d4484f423f7cad51424695c7
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 21:12:23 +0000

    Bug 1796391 - Always init signed policy rules r=bobowen
    Differential Revision: https://phabricator.services.mozilla.com/D159802
    
    UltraBlame original commit: 89d3bd40e892a676b43c207535cf0b6fbabf1319

--- security/sandbox/win/src/sandboxbroker/sandboxBroker.cpp
+++ security/sandbox/win/src/sandboxbroker/sandboxBroker.cpp
@@ -1339,13 +1339,6 @@ bool SandboxBroker::SetSecurityLevelForUtilityProcess(
   result = mPolicy->SetProcessMitigations(mitigations);
   SANDBOX_ENSURE_SUCCESS(result, "Invalid flags for SetProcessMitigations.");
 
-  if (exceptionModules.isSome()) {
-    
-    
-    result = InitSignedPolicyRulesToBypassCig(mPolicy, exceptionModules.ref());
-    SANDBOX_ENSURE_SUCCESS(result, "Failed to initialize signed policy rules.");
-  }
-
   
   
   if (IsWin10FallCreatorsUpdateOrLater()
@@ -1388,6 +1381,17 @@ bool SandboxBroker::SetSecurityLevelForUtilityProcess(
   result = mPolicy->SetDelayedProcessMitigations(mitigations);
   SANDBOX_ENSURE_SUCCESS(result,
                          "Invalid flags for SetDelayedProcessMitigations.");
+
+  
+  
+  if (exceptionModules.isSome()) {
+    result = InitSignedPolicyRulesToBypassCig(mPolicy, exceptionModules.ref());
+    SANDBOX_ENSURE_SUCCESS(result, "Failed to initialize signed policy rules.");
+  } else {
+    const Vector<const wchar_t*> emptyVector;
+    result = InitSignedPolicyRulesToBypassCig(mPolicy, emptyVector);
+    SANDBOX_ENSURE_SUCCESS(result, "Failed to initialize signed policy rules.");
+  }
 
   
   
