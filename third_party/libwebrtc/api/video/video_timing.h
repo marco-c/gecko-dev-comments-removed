e0eea85522b5a41620ba5c8152d6307ae38e6f12
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 15:11:02 +0000

    Bug 1766646 - Vendor libwebrtc from 385eb9714da
    Upstream commit: https://webrtc.googlesource.com/src/+/385eb9714daa80306d2f92d36678c42892dab555
        Represent RtpPacketToSend::capture_time with Timestamp
    
        Bug: webrtc:13757
        Change-Id: I0ede22cd34e3a59afe1477d8edd495dce64e3242
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/252586
        Reviewed-by: Erik Språng <sprangwebrtc.org>
        Reviewed-by: Rasmus Brandt <brandtrwebrtc.org>
        Commit-Queue: Danil Chapovalov <danilchapwebrtc.org>
        Cr-Commit-Position: refs/heads/main{#36083}
    
    UltraBlame original commit: c65ea74992975682a3a63bbb509adc6ed96198ba

--- third_party/libwebrtc/api/video/video_timing.h
+++ third_party/libwebrtc/api/video/video_timing.h
@@ -16,6 +16,8 @@
 #include <limits>
 #include <string>
 
+#include "api/units/time_delta.h"
+
 namespace webrtc {
 
 
@@ -34,6 +36,7 @@ struct VideoSendTiming {
   
   
   static uint16_t GetDeltaCappedMs(int64_t base_ms, int64_t time_ms);
+  static uint16_t GetDeltaCappedMs(TimeDelta delta);
 
   uint16_t encode_start_delta_ms;
   uint16_t encode_finish_delta_ms;
