065b119cb711b7d336babb2de4f34032960ce1f7
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 03:30:56 +0000

    Bug 1773324 - Add ErrorReportMixin::getAllocator() r=arai
    The type of the allocator will be varying separately from the type of the context.
    
    Differential Revision: https://phabricator.services.mozilla.com/D149997
    
    UltraBlame original commit: b31863ca872ef5403696d4445ed1b8ca262ec7e6

--- js/src/frontend/ErrorReporter.h
+++ js/src/frontend/ErrorReporter.h
@@ -40,6 +40,7 @@ class StrictModeGetter {
 
 
 
+
 class ErrorReportMixin : public StrictModeGetter {
  public:
   
@@ -48,6 +49,9 @@ class ErrorReportMixin : public StrictModeGetter {
 
   
   virtual JSContext* getContext() const = 0;
+
+  
+  virtual JSAllocator* getAllocator() const = 0;
 
   
   struct Current {};
@@ -144,7 +148,7 @@ class ErrorReportMixin : public StrictModeGetter {
       return;
     }
 
-    ReportCompileErrorLatin1(getContext(), getContext(), std::move(metadata),
+    ReportCompileErrorLatin1(getContext(), getAllocator(), std::move(metadata),
                              std::move(notes), errorNumber, args);
   }
 
@@ -298,7 +302,7 @@ class ErrorReportMixin : public StrictModeGetter {
       return false;
     }
 
-    ReportCompileErrorLatin1(getContext(), getContext(), std::move(metadata),
+    ReportCompileErrorLatin1(getContext(), getAllocator(), std::move(metadata),
                              std::move(notes), errorNumber, args);
     return false;
   }
@@ -307,8 +311,9 @@ class ErrorReportMixin : public StrictModeGetter {
   [[nodiscard]] bool compileWarning(ErrorMetadata&& metadata,
                                     UniquePtr<JSErrorNotes> notes,
                                     unsigned errorNumber, va_list* args) {
-    return ReportCompileWarning(getContext(), getContext(), std::move(metadata),
-                                std::move(notes), errorNumber, args);
+    return ReportCompileWarning(getContext(), getAllocator(),
+                                std::move(metadata), std::move(notes),
+                                errorNumber, args);
   }
 };
 
