a9da0a7a707017c332de10933c9094f74afe8ac9
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 03:29:53 +0000

    Bug 1781028 - Fix xpcshell no-unused-vars warnings in toolkit/components/url-classifier/. r=dimi
    Depends on D152624
    
    Differential Revision: https://phabricator.services.mozilla.com/D152625
    
    UltraBlame original commit: dfcead10309064b456e5c93e4833dc25055ce99a

--- toolkit/components/url-classifier/tests/unit/test_shouldclassify.js
+++ toolkit/components/url-classifier/tests/unit/test_shouldclassify.js
@@ -12,7 +12,7 @@ const { UrlClassifierTestUtils } = ChromeUtils.import(
 const defaultTopWindowURI = NetUtil.newURI("http://www.example.com/");
 
 var httpServer;
-var normalOrigin, trackingOrigin;
+var trackingOrigin;
 
 
 
@@ -21,7 +21,7 @@ var normalOrigin, trackingOrigin;
 
 
 
-const PARAM_LOAD_BYPASS_URL_CLASSIFIER = 1 << 0;
+
 const PARAM_CONTENT_POLICY_TYPE_DOCUMENT = 1 << 1;
 const PARAM_TRIGGERING_PRINCIPAL_SYSTEM = 1 << 2;
 const PARAM_CAP_BE_CONSERVATIVE = 1 << 3;
@@ -84,7 +84,6 @@ function setupHttpServer() {
     "example.org",
     httpServer.identity.primaryPort
   );
-  normalOrigin = "http://localhost:" + httpServer.identity.primaryPort;
   trackingOrigin =
     "http://tracking.example.org:" + httpServer.identity.primaryPort;
 }
