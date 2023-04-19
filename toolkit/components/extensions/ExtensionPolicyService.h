c4e63bc26b675c61d71b8738ce3c0d3bce932392
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 20:26:16 +0000

    Bug 1793995 - Part 6: Allow looking up a WebExtensionPolicyCore from any thread, r=kmag
    In order to make WebAccessibleResource threadsafe, as well as other places, it
    needs to be possible to look up a `WebExtensionPolicyCore` from any thread.
    This is handled by using a static method on the ExtensionPolicyService for this
    task, and keeping a seperate mutex-guarded static table under the hood.
    
    Theoretically the table within `ExtensionPolicyService()` could also be
    removed, however I held off on doing that in case it would have a negative
    performance impact to take extra locks and follow extra pointers.
    
    Differential Revision: https://phabricator.services.mozilla.com/D158884
    
    UltraBlame original commit: 4330821df43fd453180ed539cd48de9f07f93bfb

--- toolkit/components/extensions/ExtensionPolicyService.h
+++ toolkit/components/extensions/ExtensionPolicyService.h
@@ -60,6 +60,12 @@ class ExtensionPolicyService final : public nsIAddonPolicyService,
     return do_AddRef(&GetSingleton());
   }
 
+  
+  
+  
+  static RefPtr<extensions::WebExtensionPolicyCore> GetCoreByHost(
+      const nsACString& aHost);
+
   WebExtensionPolicy* GetByID(const nsAtom* aAddonId) {
     return mExtensions.GetWeak(aAddonId);
   }
