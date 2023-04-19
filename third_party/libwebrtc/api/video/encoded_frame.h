5d28d4376d51d6549cb06194f73028ad9fb14239
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 07:56:28 +0000

    Bug 1766646 - Vendor libwebrtc from 370e60098c
    Upstream commit: https://webrtc.googlesource.com/src/+/370e60098c7e80e41e71416c923149a53477c682
        Remove EncodedFrame::inter_layer_predicted.
    
        Bug: webrtc:12206
        Change-Id: I52246e81aa9a814fc211df19fbe27aff197a85b7
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/196743
        Commit-Queue: Philip Eliasson <philipelwebrtc.org>
        Reviewed-by: Niels Moller <nissewebrtc.org>
        Cr-Commit-Position: refs/heads/master{#32815}
    
    UltraBlame original commit: 9e84c0065965b425b7e415cf2655544ca074ce7c

--- third_party/libwebrtc/api/video/encoded_frame.h
+++ third_party/libwebrtc/api/video/encoded_frame.h
@@ -80,8 +80,6 @@ class EncodedFrame : public webrtc::VCMEncodedFrame {
   size_t num_references = 0;
   int64_t references[kMaxFrameReferences];
   
-  bool inter_layer_predicted = false;
-  
   
   bool is_last_spatial_layer = true;
 };
