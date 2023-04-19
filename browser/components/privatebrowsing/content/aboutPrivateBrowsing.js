423ede38f2b8f304f8542076bdb208be81ba60b0
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 00:32:13 +0000

    Bug 1770067 - Adding link support to private browsing promo button r=pdahiya,emcminn
    Differential Revision: https://phabricator.services.mozilla.com/D149571
    
    UltraBlame original commit: 074332a16756a975c4d30827c8e7aebb6c86bbd3

--- browser/components/privatebrowsing/content/aboutPrivateBrowsing.js
+++ browser/components/privatebrowsing/content/aboutPrivateBrowsing.js
@@ -82,7 +82,6 @@ async function renderPromo({
   promoTitle,
   promoTitleEnabled,
   promoLinkText,
-  promoLinkUrl,
   promoLinkType,
   promoSectionStyle,
   promoHeader,
@@ -98,27 +97,23 @@ async function renderPromo({
     return false;
   }
 
-  const titleEl = document.getElementById("private-browsing-vpn-text");
-  let linkEl = document.getElementById("private-browsing-vpn-link");
+  const titleEl = document.getElementById("private-browsing-promo-text");
+  const linkEl = document.getElementById("private-browsing-promo-link");
   const promoHeaderEl = document.getElementById("promo-header");
   const infoContainerEl = document.querySelector(".info");
   const promoImageLargeEl = document.querySelector(".promo-image-large img");
   const promoImageSmallEl = document.querySelector(".promo-image-small img");
   const dismissBtn = document.querySelector("#dismiss-btn");
 
-  if (promoLinkType === "button") {
-    linkEl.classList.add("button");
+  if (promoLinkType === "link") {
+    linkEl.classList.remove("primary");
+    linkEl.classList.add("text-link", "promo-link");
   }
 
-  if (promoLinkUrl) {
-    linkEl.setAttribute("href", promoLinkUrl);
-    linkEl.setAttribute("target", "_blank");
-    linkEl.addEventListener("click", () => {
-      window.PrivateBrowsingRecordClick("promo_link");
-    });
-  } else if (promoButton?.action) {
+  if (promoButton?.action) {
     linkEl.addEventListener("click", async event => {
       event.preventDefault();
+
       
       
       let isExperiment = window.PrivateBrowsingRecordClick("promo_link");
