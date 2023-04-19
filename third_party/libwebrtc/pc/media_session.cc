225c226c84eed777ffa7d18a7b7776d9c16f3f4b
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 11:03:22 +0000

    Bug 1766646 - Vendor libwebrtc from 3babb8af238
    Upstream commit: https://webrtc.googlesource.com/src/+/3babb8af238a531cbff27951604b09bb78b762cd
        Added support for H264 YUV444 (I444) decoding.
    
        Added Nutanix Inc. to the AUTHORS file.
    
        PS#1 is a reland of "Added support for H264 YUV444 (I444) decoding." https://webrtc-review.googlesource.com/c/src/+/234540
    
        Bug: chromium:1251096
        Change-Id: I99a1b1e4d8b60192ff96f92334a430240875c66c
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/235340
        Reviewed-by: Niels Moller <nissewebrtc.org>
        Reviewed-by: Ilya Nikolaevskiy <ilnikwebrtc.org>
        Reviewed-by: Harald Alvestrand <htawebrtc.org>
        Commit-Queue: Harald Alvestrand <htawebrtc.org>
        Cr-Commit-Position: refs/heads/main{#35684}
    
    UltraBlame original commit: 57025d5963ee22b4babb501e63b1cef64e686bd7

--- third_party/libwebrtc/pc/media_session.cc
+++ third_party/libwebrtc/pc/media_session.cc
@@ -2902,8 +2902,12 @@ void MediaSessionDescriptionFactory::ComputeVideoCodecsIntersectionAndUnion() {
   video_sendrecv_codecs_.clear();
   all_video_codecs_.clear();
   
+  
+  UsedPayloadTypes used_payload_types;
   for (const VideoCodec& send : video_send_codecs_) {
-    all_video_codecs_.push_back(send);
+    VideoCodec send_mutable = send;
+    used_payload_types.FindAndSetIdUsed(&send_mutable);
+    all_video_codecs_.push_back(send_mutable);
     if (!FindMatchingCodec<VideoCodec>(video_send_codecs_, video_recv_codecs_,
                                        send, nullptr)) {
       
@@ -2915,12 +2919,11 @@ void MediaSessionDescriptionFactory::ComputeVideoCodecsIntersectionAndUnion() {
       
     }
   }
-  for (const VideoCodec& recv : video_recv_codecs_) {
-    if (!FindMatchingCodec<VideoCodec>(video_recv_codecs_, video_send_codecs_,
-                                       recv, nullptr)) {
-      all_video_codecs_.push_back(recv);
-    }
-  }
+  
+  
+  MergeCodecs<VideoCodec>(video_recv_codecs_, &all_video_codecs_,
+                          &used_payload_types);
+
   
   
   
