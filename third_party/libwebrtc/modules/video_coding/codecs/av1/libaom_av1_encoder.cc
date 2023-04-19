a1d73ccfccd0c82deb6940ff254d7ac36e492e17
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 15:03:29 +0000

    Bug 1766646 - Vendor libwebrtc from 95701503f29
    (skip-generation)
    
    Upstream commit: https://webrtc.googlesource.com/src/+/95701503f29324359669324446f8d51d9fd15e48
        Make libaom_av1_encoder always build the libaom encoder.
    
        Currently `CreateLibaomAv1Encoder` will either return an actual libaom AV1 encoder or a nullptr depening on whether the build flag `enable_libaom` was configured to true or not. This CL updates the `libaom_av1_encoder` build target to no longer depend on `enable_libaom` so that `CreateLibaomAv1Encoder` will always return an encoder instance.
    
        Added `CreateLibaomAv1EncoderIfSupported` as a replacement to the old `CreateLibaomAv1Encoder`.
    
        Bug: webrtc:13573
        Change-Id: Ibdcd52c609acd79feefa2b86f19d1b4ca3e91d0a
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/242360
        Reviewed-by: Erik Språng <sprangwebrtc.org>
        Reviewed-by: Mirko Bonadei <mbonadeiwebrtc.org>
        Reviewed-by: Peter Hanspers <peterhansperswebrtc.org>
        Reviewed-by: Stefan Holmer <stefanwebrtc.org>
        Commit-Queue: Philip Eliasson <philipelwebrtc.org>
        Cr-Commit-Position: refs/heads/main{#35763}
    
    UltraBlame original commit: 2d055b479d4f4570c82bdab118859540beac456a

--- third_party/libwebrtc/modules/video_coding/codecs/av1/libaom_av1_encoder.cc
+++ third_party/libwebrtc/modules/video_coding/codecs/av1/libaom_av1_encoder.cc
@@ -820,17 +820,8 @@ VideoEncoder::EncoderInfo LibaomAv1Encoder::GetEncoderInfo() const {
 
 }  
 
-const bool kIsLibaomAv1EncoderSupported = true;
-
 std::unique_ptr<VideoEncoder> CreateLibaomAv1Encoder() {
   return std::make_unique<LibaomAv1Encoder>();
-}
-
-bool LibaomAv1EncoderSupportsScalabilityMode(
-    absl::string_view scalability_mode) {
-  
-  
-  return ScalabilityStructureConfig(scalability_mode) != absl::nullopt;
 }
 
 }  
