2eeb7683cb9eba253a081b772d9ced5c882375aa
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 09:33:49 +0000

    Bug 1766646 - Vendor libwebrtc from 7d5418233d
    Upstream commit: https://webrtc.googlesource.com/src/+/7d5418233d794cc42e64ac44b1fd388a45e811de
        Avoid assembling complicated but unused video rtp header extensions
    
        Bug: chromium:1219407
        Change-Id: I017de10813a1e80f4af0ba55d8d1aa73077dd131
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/222615
        Reviewed-by: Philip Eliasson <philipelwebrtc.org>
        Commit-Queue: Danil Chapovalov <danilchapwebrtc.org>
        Cr-Commit-Position: refs/heads/master{#34326}
    
    UltraBlame original commit: e34b9983570c69b5faf236f0980e95f4072b9772

--- third_party/libwebrtc/modules/rtp_rtcp/source/rtp_packet.h
+++ third_party/libwebrtc/modules/rtp_rtcp/source/rtp_packet.h
@@ -114,6 +114,11 @@ class RtpPacket {
   bool HasExtension() const;
   bool HasExtension(ExtensionType type) const;
 
+  
+  
+  template <typename Extension>
+  bool IsRegistered() const;
+
   template <typename Extension, typename FirstValue, typename... Values>
   bool GetExtension(FirstValue, Values...) const;
 
@@ -205,6 +210,11 @@ class RtpPacket {
 template <typename Extension>
 bool RtpPacket::HasExtension() const {
   return HasExtension(Extension::kId);
+}
+
+template <typename Extension>
+bool RtpPacket::IsRegistered() const {
+  return extensions_.IsRegistered(Extension::kId);
 }
 
 template <typename Extension, typename FirstValue, typename... Values>
