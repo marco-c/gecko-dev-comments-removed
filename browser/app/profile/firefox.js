0706db974d12becc4c32313b904fca91d8b095b2
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 17:25:42 +0000

    Bug 1782295: Rename private window separation pref. r=pbz
    I don't see any reason to keep this under the same namespace as the other former `privacySegmentation` prefs - but perhaps I'm missing something here.
    
    Depends on D156733
    
    Differential Revision: https://phabricator.services.mozilla.com/D156734
    
    UltraBlame original commit: b1462bbc7934f45eff16184644a1eaca75b044c0

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -654,7 +654,7 @@ pref("browser.dataFeatureRecommendations.enabled", false);
 
 
 
-pref("browser.privacySegmentation.windowSeparation.enabled", true);
+pref("browser.privateWindowSeparation.enabled", true);
 
 
 
