ef7e6e5eacd2d84a9bac0c95e5ef1f057a6bac4b
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 23:45:44 +0000

    Bug 1793453 - Prevent automatic suppression of popups on 'blank' pages when URL bar is focused r=dao,adw
    Differential Revision: https://phabricator.services.mozilla.com/D158850
    
    UltraBlame original commit: 3924518919682eae3ae92f9c6da486b361e4ea80

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -420,10 +420,21 @@ XPCOMUtils.defineLazyGetter(this, "PopupNotifications", () => {
     
     
     
-    let shouldSuppress = () =>
-      (gURLBar.getAttribute("pageproxystate") != "valid" &&
-        (gURLBar.focused || gBrowser.selectedBrowser._awaitingSetURI)) ||
-      shouldSuppressPopupNotifications();
+    let shouldSuppress = () => {
+      
+      
+      
+      
+      
+      const urlBarEdited = isBlankPageURL(gBrowser.currentURI.spec)
+        ? gURLBar.hasAttribute("usertyping")
+        : gURLBar.getAttribute("pageproxystate") != "valid";
+      return (
+        (urlBarEdited &&
+          (gURLBar.focused || gBrowser.selectedBrowser._awaitingSetURI)) ||
+        shouldSuppressPopupNotifications()
+      );
+    };
     return new PopupNotifications(
       gBrowser,
       document.getElementById("notification-popup"),
