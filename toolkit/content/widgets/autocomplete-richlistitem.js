c243bff1e5f4f87b8c60a9ff8d4e8eda79fcad1c
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 04:52:46 +0000

    Bug 1773483 - skip default subtitle rendering in login autocomplete for import r=dimi
    Differential Revision: https://phabricator.services.mozilla.com/D153900
    
    UltraBlame original commit: 8045967b0150ff29a02817d1cd1ae6f957888cba

--- toolkit/content/widgets/autocomplete-richlistitem.js
+++ toolkit/content/widgets/autocomplete-richlistitem.js
@@ -749,6 +749,14 @@
       MozXULElement.insertFTLIfNeeded("toolkit/main-window/autocomplete.ftl");
     }
 
+    static get inheritedAttributes() {
+      return {
+        
+        ".line1-label": "text=ac-value",
+        
+      };
+    }
+
     static get markup() {
       return `
       <div xmlns="http://www.w3.org/1999/xhtml"
@@ -764,6 +772,7 @@
     }
 
     _adjustAcItem() {
+      super._adjustAcItem();
       document.l10n.setAttributes(
         this.querySelector(".labels-wrapper"),
         `autocomplete-import-logins-${this.getAttribute("ac-value")}`,
@@ -774,7 +783,6 @@
           ),
         }
       );
-      super._adjustAcItem();
     }
   }
 
