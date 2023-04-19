c70fe44cb349e649f1b4b5e18934841ba63f7ccc
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 05:30:20 +0000

    Bug 1782770 - wasm: Transfer all type definitions to Metadata. r=yury
    Metadata stores a vector of TypeDef that comes from the type section of the module.
    We currently only transfer a type if it's a struct type, array type, or else a
    function type that cannot fit in an immediate for call_indirect signature checks.
    
    Because we filter out some types, this means we need to renumber the type index
    space to match. This is a bit hacky, and is difficult to do consistently.
    
    The original reason for only bringing function types that cannot fit in an
    immediate was to reduce the size of metadata as most function types can fit in an
    immediate. However, I discovered that we make a copies of function types for
    FuncExport and FuncImport metadata. This can create multiple copies of the original
    function type, e.g. when multiple exported functions reference the same type
    definition.
    
    If we instead transfer all types and store the index of the function type in
    FuncExport/FuncImport we can reduce total metadata size, reduce allocations,
    and remove type renumbering.
    
    This commit does this by:
      1. Transferring all type definitions to Metadata (WasmGenerator.cpp)
      2. Removing the typeRenumbering vector
      3. Replacing FuncType with typeIndex on FuncImport/FuncExport
      4. Updating all users of FuncImport/FuncExport to get the FuncType using
         the typeIndex and Metadata. This the bulk of the changes.
    
    Differential Revision: https://phabricator.services.mozilla.com/D153498
    
    UltraBlame original commit: c867a71812f81d4725c2a65d84bb007d82c48aa4

--- js/src/wasm/WasmTypeDef.h
+++ js/src/wasm/WasmTypeDef.h
@@ -101,15 +101,6 @@ class FuncType {
     return args_.appendAll(src.args_) && results_.appendAll(src.results_);
   }
 
-  void renumber(const RenumberVector& renumbering) {
-    for (auto& arg : args_) {
-      arg.renumber(renumbering);
-    }
-    for (auto& result : results_) {
-      result.renumber(renumbering);
-    }
-  }
-
   ValType arg(unsigned i) const { return args_[i]; }
   const ValTypeVector& args() const { return args_; }
   ValType result(unsigned i) const { return results_[i]; }
@@ -223,12 +214,6 @@ class StructType {
     return true;
   }
 
-  void renumber(const RenumberVector& renumbering) {
-    for (auto& field : fields_) {
-      field.type.renumber(renumbering);
-    }
-  }
-
   bool isDefaultable() const {
     for (auto& field : fields_) {
       if (!field.type.isDefaultable()) {
@@ -284,10 +269,6 @@ class ArrayType {
     elementType_ = src.elementType_;
     isMutable_ = src.isMutable_;
     return true;
-  }
-
-  void renumber(const RenumberVector& renumbering) {
-    elementType_.renumber(renumbering);
   }
 
   bool isDefaultable() const { return elementType_.isDefaultable(); }
@@ -441,30 +422,11 @@ class TypeDef {
     return arrayType_;
   }
 
-  void renumber(const RenumberVector& renumbering) {
-    switch (kind_) {
-      case TypeDefKind::Func:
-        funcType_.renumber(renumbering);
-        break;
-      case TypeDefKind::Struct:
-        structType_.renumber(renumbering);
-        break;
-      case TypeDefKind::Array:
-        arrayType_.renumber(renumbering);
-        break;
-      case TypeDefKind::None:
-        break;
-    }
-  }
-
   size_t sizeOfExcludingThis(mozilla::MallocSizeOf mallocSizeOf) const;
   WASM_DECLARE_FRIEND_SERIALIZE(TypeDef);
 };
 
 using TypeDefVector = Vector<TypeDef, 0, SystemAllocPolicy>;
-
-template <typename T>
-using DerivedTypeDefVector = Vector<T, 0, SystemAllocPolicy>;
 
 
 
@@ -545,8 +507,7 @@ class TypeContext : public AtomicRefCounted<TypeContext> {
   TypeContext(const FeatureArgs& features, TypeDefVector&& types)
       : features_(features), types_(std::move(types)) {}
 
-  template <typename T>
-  [[nodiscard]] bool cloneDerived(const DerivedTypeDefVector<T>& source) {
+  [[nodiscard]] bool clone(const TypeDefVector& source) {
     MOZ_ASSERT(types_.length() == 0);
     if (!types_.resize(source.length())) {
       return false;
@@ -739,20 +700,6 @@ class TypeHandle {
   uint32_t index() const { return index_; }
   const TypeDef& def() const { return context_->type(index_); }
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
