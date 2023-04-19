2da3b8d76544a59cd2219183066f93569d16ebec
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 20:38:56 +0000

    Bug 1793483 - [refactor] Migrate NS_STYLE_FRAME_* defines r=emilio
    Differential Revision: https://phabricator.services.mozilla.com/D158526
    
    UltraBlame original commit: 8afa6eaecd2fcf36ffc7f221b455d1fc10acfaeb

--- layout/style/nsStyleConsts.h
+++ layout/style/nsStyleConsts.h
@@ -402,18 +402,7 @@ enum class StylePositionProperty : uint8_t {
   Sticky,
 };
 
-
-
-
-#define NS_STYLE_FRAME_YES 0
-#define NS_STYLE_FRAME_NO 1
-#define NS_STYLE_FRAME_0 2
-#define NS_STYLE_FRAME_1 3
-#define NS_STYLE_FRAME_ON 4
-#define NS_STYLE_FRAME_OFF 5
-#define NS_STYLE_FRAME_AUTO 6
-#define NS_STYLE_FRAME_SCROLL 7
-#define NS_STYLE_FRAME_NOSCROLL 8
+enum class FrameBorderProperty : uint8_t { Yes, No };
 
 
 #define NS_STYLE_LIST_STYLE_CUSTOM -1  // for @counter-style
