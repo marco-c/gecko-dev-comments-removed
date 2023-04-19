d517017aebc87c3d87ba5982dd19e5d8cb746c17
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 04:13:43 +0000

    Bug 1782731 - Make gfx.font_rendering.fontconfig.max_generic_substitutions threadsafe. r=jfkthame
    This pref could be accessed off the main thread with workers. Convert it
    to a threadsafe StaticPref.
    
    Differential Revision: https://phabricator.services.mozilla.com/D153470
    
    UltraBlame original commit: 32689459fc627339fe17cbf6228c7309d3ad89c1

--- gfx/thebes/gfxFcPlatformFontList.cpp
+++ gfx/thebes/gfxFcPlatformFontList.cpp
@@ -2415,8 +2415,8 @@ gfxPlatformFontList::PrefFontList* gfxFcPlatformFontList::FindGenericFamilies(
           
           auto prefFonts = MakeUnique<PrefFontList>();  
                                                         
-          uint32_t limit =
-              gfxPlatformGtk::GetPlatform()->MaxGenericSubstitions();
+          uint32_t limit = StaticPrefs::
+              gfx_font_rendering_fontconfig_max_generic_substitutions();
           bool foundFontWithLang = false;
           for (int i = 0; i < faces->nfont; i++) {
             FcPattern* font = faces->fonts[i];
