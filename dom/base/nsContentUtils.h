642bace45c049217242514876a7943f85d05d2bf
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 21:10:58 +0000

    Bug 1778565 - Remove IsFileImage and SlurpFileToString. r=nika
    Differential Revision: https://phabricator.services.mozilla.com/D155754
    
    UltraBlame original commit: 47dfb29abf337366457836dfc9744d0c0bad27b1

--- dom/base/nsContentUtils.h
+++ dom/base/nsContentUtils.h
@@ -2854,21 +2854,6 @@ class nsContentUtils {
 
 
 
-
-
-  static nsresult SlurpFileToString(nsIFile* aFile, nsACString& aString);
-
-  
-
-
-
-
-  static bool IsFileImage(nsIFile* aFile, nsACString& aType);
-
-  
-
-
-
   static nsresult DeserializeDataTransferImageContainer(
       const mozilla::dom::IPCDataTransferImageContainer& aData,
       imgIContainer** aContainer);
