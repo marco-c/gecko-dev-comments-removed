3905eff7dee751ae19001880ee57349b3e890531
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 20:45:44 +0000

    Bug 1790274 - Convert browser/actors/DOMFullscreen* JSM modules to ESMs. r=mconley,kpatenio.
    Differential Revision: https://phabricator.services.mozilla.com/D159015
    
    UltraBlame original commit: fe59acd50c10047438a2c8649f3477d3140003ee

--- browser/actors/DOMFullscreenChild.sys.mjs
+++ browser/actors/DOMFullscreenChild.sys.mjs
@@ -1,12 +1,9 @@
+/* vim: set ts=2 sw=2 sts=2 et tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-
-
-
-"use strict";
-
-var EXPORTED_SYMBOLS = ["DOMFullscreenChild"];
-
-class DOMFullscreenChild extends JSWindowActorChild {
+export class DOMFullscreenChild extends JSWindowActorChild {
   receiveMessage(aMessage) {
     let window = this.contentWindow;
     let windowUtils = window?.windowUtils;
@@ -14,8 +11,8 @@ class DOMFullscreenChild extends JSWindowActorChild {
     switch (aMessage.name) {
       case "DOMFullscreen:Entered": {
         if (!windowUtils) {
-          
-          
+          // If we are not able to enter fullscreen, tell the parent to just
+          // exit.
           this.sendAsyncMessage("DOMFullscreen:Exit", {});
           break;
         }
@@ -24,9 +21,9 @@ class DOMFullscreenChild extends JSWindowActorChild {
         if (remoteFrameBC) {
           let remoteFrame = remoteFrameBC.embedderElement;
           if (!remoteFrame) {
-            
-            
-            
+            // This could happen when the page navigate away and trigger a
+            // process switching during fullscreen transition, tell the parent
+            // to just exit.
             this.sendAsyncMessage("DOMFullscreen:Exit", {});
             break;
           }
@@ -39,9 +36,9 @@ class DOMFullscreenChild extends JSWindowActorChild {
             !windowUtils.handleFullscreenRequests() &&
             !this.document.fullscreenElement
           ) {
-            
-            
-            
+            // If we don't actually have any pending fullscreen request
+            // to handle, neither we have been in fullscreen, tell the
+            // parent to just exit.
             this.sendAsyncMessage("DOMFullscreen:Exit", {});
           }
         }
@@ -49,32 +46,32 @@ class DOMFullscreenChild extends JSWindowActorChild {
       }
       case "DOMFullscreen:CleanUp": {
         let isNotTheRequestSource = !!aMessage.data.remoteFrameBC;
-        
-        
-        
-        
+        // If we've exited fullscreen at this point, no need to record
+        // transaction id or call exit fullscreen. This is especially
+        // important for pre-e10s, since in that case, it is possible
+        // that no more paint would be triggered after this point.
         if (this.document.fullscreenElement) {
           this._isNotTheRequestSource = isNotTheRequestSource;
-          
-          
+          // Need to wait for the MozAfterPaint after exiting fullscreen if
+          // this is the request source.
           this._waitForMozAfterPaint = !this._isNotTheRequestSource;
-          
-          
-          
+          // windowUtils could be null if the associated window is not current
+          // active window. In this case, document must be in the process of
+          // exiting fullscreen, it is okay to not ask it to exit fullscreen.
           if (windowUtils) {
             this._lastTransactionId = windowUtils.lastTransactionId;
             windowUtils.exitFullscreen();
           }
         } else if (isNotTheRequestSource) {
-          
-          
+          // If we are not the request source and have exited fullscreen, reply
+          // Exited to parent as parent is waiting for our reply.
           this.sendAsyncMessage("DOMFullscreen:Exited", {});
         } else {
-          
-          
-          
-          
-          
+          // If we've already exited fullscreen, it is possible that no more
+          // paint would be triggered, so don't wait for MozAfterPaint.
+          // TODO: There might be some way to move this code around a bit to
+          //       make it easier to follow. Somehow handle the "local" case in
+          //       one place and the isNotTheRequestSource case after that.
           this.sendAsyncMessage("DOMFullscreen:Painted", {});
         }
         break;
@@ -88,9 +85,9 @@ class DOMFullscreenChild extends JSWindowActorChild {
 
   handleEvent(aEvent) {
     if (this.hasBeenDestroyed()) {
-      
-      
-      
+      // Make sure that this actor is alive before going further because
+      // if it's not the case, any attempt to send a message or access
+      // objects such as 'contentWindow' will fail. (See bug 1590138)
       return;
     }
 
@@ -112,9 +109,9 @@ class DOMFullscreenChild extends JSWindowActorChild {
       case "MozDOMFullscreen:Entered":
       case "MozDOMFullscreen:Exited": {
         if (this._isNotTheRequestSource) {
-          
-          
-          
+          // Fullscreen change event for a frame in the
+          // middle (content frame embedding the oop frame where the
+          // request comes from)
 
           delete this._isNotTheRequestSource;
           this.sendAsyncMessage(aEvent.type.replace("Moz", ""), {});
@@ -128,18 +125,18 @@ class DOMFullscreenChild extends JSWindowActorChild {
         }
 
         if (!this.document || !this.document.fullscreenElement) {
-          
-          
-          
+          // If we receive any fullscreen change event, and find we are
+          // actually not in fullscreen, also ask the parent to exit to
+          // ensure that the parent always exits fullscreen when we do.
           this.sendAsyncMessage("DOMFullscreen:Exit", {});
         }
         break;
       }
       case "MozAfterPaint": {
-        
-        
-        
-        
+        // Only send Painted signal after we actually finish painting
+        // the transition for the fullscreen change.
+        // Note that this._lastTransactionId is not set when in pre-e10s
+        // mode, so we need to check that explicitly.
         if (
           !this._lastTransactionId ||
           aEvent.transactionId > this._lastTransactionId
@@ -154,10 +151,10 @@ class DOMFullscreenChild extends JSWindowActorChild {
   }
 
   hasBeenDestroyed() {
-    
-    
-    
-    
+    // The 'didDestroy' callback is not always getting called.
+    // So we can't rely on it here. Instead, we will try to access
+    // the browsing context to judge wether the actor has
+    // been destroyed or not.
     try {
       return !this.browsingContext;
     } catch {
--- browser/actors/DOMFullscreenParent.sys.mjs
+++ browser/actors/DOMFullscreenParent.sys.mjs
@@ -1,24 +1,21 @@
+/* vim: set ts=2 sw=2 sts=2 et tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-
-
-
-"use strict";
-
-var EXPORTED_SYMBOLS = ["DOMFullscreenParent"];
-
-class DOMFullscreenParent extends JSWindowActorParent {
-  
-  
-  
+export class DOMFullscreenParent extends JSWindowActorParent {
+  // These properties get set by browser-fullScreenAndPointerLock.js.
+  // TODO: Bug 1743703 - Consider moving the messaging component of
+  //       browser-fullScreenAndPointerLock.js into the actor
   waitingForChildEnterFullscreen = false;
   waitingForChildExitFullscreen = false;
-  
-  
-  
-  
-  
-  
-  
+  // Cache the next message recipient actor and in-process browsing context that
+  // is computed by _getNextMsgRecipientActor() of
+  // browser-fullScreenAndPointerLock.js, this is used to ensure the fullscreen
+  // cleanup messages goes the same route as fullscreen request, especially for
+  // the cleanup that happens after actor is destroyed.
+  // TODO: Bug 1743703 - Consider moving the messaging component of
+  //       browser-fullScreenAndPointerLock.js into the actor
   nextMsgRecipient = null;
 
   updateFullscreenWindowReference(aWindow) {
@@ -34,10 +31,10 @@ class DOMFullscreenParent extends JSWindowActorParent {
       return;
     }
 
-    
-    
-    
-    
+    // If we don't need to wait for child reply, i.e. cleanupDomFullscreen
+    // doesn't message to child, and we've exit the fullscreen, there won't be
+    // DOMFullscreen:Painted message from child and it is possible that no more
+    // paint would be triggered, so just notify fullscreen-painted observer.
     if (
       !aWindow.FullScreen.cleanupDomFullscreen(this) &&
       !aWindow.document.fullscreen
@@ -46,11 +43,11 @@ class DOMFullscreenParent extends JSWindowActorParent {
     }
   }
 
-  
-
-
-
-
+  /**
+   * Clean up fullscreen state and resume chrome UI if window is in fullscreen
+   * and this actor is the one where the original fullscreen enter or
+   * exit request comes.
+   */
   _cleanupFullscreenStateAndResumeChromeUI(aWindow) {
     this.cleanupDomFullscreen(aWindow);
     if (this.requestOrigin == this && aWindow.document.fullscreen) {
@@ -75,8 +72,8 @@ class DOMFullscreenParent extends JSWindowActorParent {
       ) {
         this.waitingForChildExitFullscreen = false;
         this.waitingForChildEnterFullscreen = false;
-        
-        
+        // We were destroyed while waiting for our DOMFullscreenChild to exit
+        // or enter fullscreen, run cleanup steps anyway.
         this._cleanupFullscreenStateAndResumeChromeUI(browser.ownerGlobal);
       }
       return;
@@ -85,23 +82,23 @@ class DOMFullscreenParent extends JSWindowActorParent {
     if (this.waitingForChildEnterFullscreen) {
       this.waitingForChildEnterFullscreen = false;
       if (window.document.fullscreen) {
-        
-        
-        
-        
-        
-        
-        
-        
-        
+        // We were destroyed while waiting for our DOMFullscreenChild
+        // to transition to fullscreen so we abort the entire
+        // fullscreen transition to prevent getting stuck in a
+        // partial fullscreen state. We need to go through the
+        // document since window.Fullscreen could be undefined
+        // at this point.
+        //
+        // This could reject if we're not currently in fullscreen
+        // so just ignore rejection.
         window.document.exitFullscreen().catch(() => {});
         return;
       }
       this.cleanupDomFullscreen(window);
     }
 
-    
-    
+    // Need to resume Chrome UI if the window is still in fullscreen UI
+    // to avoid the window stays in fullscreen problem. (See Bug 1620341)
     if (window.document.documentElement.hasAttribute("inDOMFullscreen")) {
       this.cleanupDomFullscreen(window);
       if (window.windowUtils) {
@@ -109,8 +106,8 @@ class DOMFullscreenParent extends JSWindowActorParent {
       }
     } else if (this.waitingForChildExitFullscreen) {
       this.waitingForChildExitFullscreen = false;
-      
-      
+      // We were destroyed while waiting for our DOMFullscreenChild to exit
+      // run cleanup steps anyway.
       this._cleanupFullscreenStateAndResumeChromeUI(window);
     }
     this.updateFullscreenWindowReference(window);
@@ -121,8 +118,8 @@ class DOMFullscreenParent extends JSWindowActorParent {
     let browser = topBrowsingContext.embedderElement;
 
     if (!browser) {
-      
-      
+      // No need to go further when the browser is not accessible anymore
+      // (which can happen when the tab is closed for instance),
       return;
     }
 
@@ -136,7 +133,7 @@ class DOMFullscreenParent extends JSWindowActorParent {
         break;
       }
       case "DOMFullscreen:NewOrigin": {
-        
+        // Don't show the warning if we've already exited fullscreen.
         if (window.document.fullscreen) {
           window.PointerlockFsWarning.showFullScreen(
             aMessage.data.originNoSuffix
@@ -177,12 +174,12 @@ class DOMFullscreenParent extends JSWindowActorParent {
     let window = aEvent.currentTarget.ownerGlobal;
     switch (aEvent.type) {
       case "MozDOMFullscreen:Entered": {
-        
-        
-        
-        
-        
-        
+        // The event target is the element which requested the DOM
+        // fullscreen. If we were entering DOM fullscreen for a remote
+        // browser, the target would be the browser which was the parameter of
+        // `remoteFrameFullscreenChanged` call. If the fullscreen
+        // request was initiated from an in-process browser, we need
+        // to get its corresponding browser here.
         let browser;
         if (aEvent.target.ownerGlobal == window) {
           browser = aEvent.target;
@@ -190,9 +187,9 @@ class DOMFullscreenParent extends JSWindowActorParent {
           browser = aEvent.target.ownerGlobal.docShell.chromeEventHandler;
         }
 
-        
-        
-        
+        // Addon installation should be cancelled when entering fullscreen for security and usability reasons.
+        // Installation prompts in fullscreen can trick the user into installing unwanted addons.
+        // In fullscreen the notification box does not have a clear visual association with its parent anymore.
         if (window.gXPInstallObserver) {
           window.gXPInstallObserver.removeAllNotifications(browser);
         }
@@ -205,11 +202,11 @@ class DOMFullscreenParent extends JSWindowActorParent {
       case "MozDOMFullscreen:Exited": {
         TelemetryStopwatch.start("FULLSCREEN_CHANGE_MS");
 
-        
-        
-        
-        
-        
+        // Make sure that the actor has not been destroyed before
+        // accessing its browsing context. Otherwise, a error may
+        // occur and hence cleanupDomFullscreen not executed, resulting
+        // in the browser window being in an unstable state.
+        // (Bug 1590138).
         if (!this.hasBeenDestroyed() && !this.requestOrigin) {
           this.requestOrigin = this;
         }
@@ -225,15 +222,15 @@ class DOMFullscreenParent extends JSWindowActorParent {
     aWindow.addEventListener(
       "MozDOMFullscreen:Entered",
       this,
-       true,
-      
+      /* useCapture */ true,
+      /* wantsUntrusted */
       false
     );
     aWindow.addEventListener(
       "MozDOMFullscreen:Exited",
       this,
-       true,
-       false
+      /* useCapture */ true,
+      /* wantsUntrusted */ false
     );
   }
 
@@ -242,20 +239,20 @@ class DOMFullscreenParent extends JSWindowActorParent {
     aWindow.removeEventListener("MozDOMFullscreen:Exited", this, true);
   }
 
-  
-
-
-
+  /**
+   * Get the actor where the original fullscreen
+   * enter or exit request comes from.
+   */
   get requestOrigin() {
     let requestOrigin = this.browsingContext.top.fullscreenRequestOrigin;
     return requestOrigin && requestOrigin.get();
   }
 
-  
-
-
-
-
+  /**
+   * Store the actor where the original fullscreen
+   * enter or exit request comes from in the top level
+   * browsing context.
+   */
   set requestOrigin(aActor) {
     if (aActor) {
       this.browsingContext.top.fullscreenRequestOrigin = Cu.getWeakReference(
@@ -271,10 +268,10 @@ class DOMFullscreenParent extends JSWindowActorParent {
       return true;
     }
 
-    
-    
-    
-    
+    // The 'didDestroy' callback is not always getting called.
+    // So we can't rely on it here. Instead, we will try to access
+    // the browsing context to judge wether the actor has
+    // been destroyed or not.
     try {
       return !this.browsingContext;
     } catch {
