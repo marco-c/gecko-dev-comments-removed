d23c27f2b835181aec11bd7d084bd0130033cdfe
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 20:32:22 +0000

    Bug 1793769 - Avoid recreating the main DrawTargetWebgl SharedContext. r=aosmond
    For canvas users that rapidly create and destroy canvases, we may end up creating
    a new SharedContext (and hence ClientWebGLContext) if there are no more canvases
    left between destruction and creation. To work around this, just keep alive the
    SharedContext for the main thread (other threads are unfortunately a bit tricky
    to support) so that canvas creation remains fast in this instance.
    
    Depends on D158903
    
    Differential Revision: https://phabricator.services.mozilla.com/D158904
    
    UltraBlame original commit: 6387a6e7bbd515af2ec3572961b95e566f1479a3

--- dom/canvas/DrawTargetWebgl.cpp
+++ dom/canvas/DrawTargetWebgl.cpp
@@ -7,6 +7,7 @@
 #include "DrawTargetWebglInternal.h"
 #include "SourceSurfaceWebgl.h"
 
+#include "mozilla/ClearOnShutdown.h"
 #include "mozilla/StaticPrefs_gfx.h"
 #include "mozilla/gfx/Blur.h"
 #include "mozilla/gfx/DrawTargetSkia.h"
@@ -335,6 +336,8 @@ static Atomic<bool> sContextInitError(false);
 MOZ_THREAD_LOCAL(DrawTargetWebgl::SharedContext*)
 DrawTargetWebgl::sSharedContext;
 
+RefPtr<DrawTargetWebgl::SharedContext> DrawTargetWebgl::sMainSharedContext;
+
 
 
 bool DrawTargetWebgl::Init(const IntSize& size, const SurfaceFormat format) {
@@ -357,6 +360,15 @@ bool DrawTargetWebgl::Init(const IntSize& size, const SurfaceFormat format) {
     }
 
     sSharedContext.set(mSharedContext.get());
+
+    if (NS_IsMainThread()) {
+      
+      
+      if (!sMainSharedContext) {
+        ClearOnShutdown(&sMainSharedContext);
+      }
+      sMainSharedContext = mSharedContext;
+    }
   } else {
     mSharedContext = sharedContext;
   }
