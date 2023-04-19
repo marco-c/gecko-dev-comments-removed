6a32b062336f94577438c3fbc646f9c0c314f722
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 09:56:25 +0000

    Bug 1766646 - Vendor libwebrtc from 3754e7c7ad
    Upstream commit: https://webrtc.googlesource.com/src/+/3754e7c7adb6a1d422a40cdfefaba2356f758689
        Revert "Updated the default VP9 per-spatial-temporal layer settings."
    
        This reverts commit 99fb5945b9c278cf33ef434ebacd5dfb9bde865d.
    
        Reason for revert: Speculative revert: breaks upstream project
    
        Original change's description:
        > Updated the default VP9 per-spatial-temporal layer settings.
        >
        > Bug: webrtc:11551
        > Change-Id: If2029df444f576b41bfef302985d6e18d7cdc3b5
        > Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/227782
        > Commit-Queue: Michael Horowitz <mhorogoogle.com>
        > Reviewed-by: Erik Språng <sprangwebrtc.org>
        > Cr-Commit-Position: refs/heads/master{#34668}
    
        # Not skipping CQ checks because original CL landed > 1 day ago.
    
        Bug: webrtc:11551
        Change-Id: Ief33f98d8a4e0ccf95d7004c2526c99829807d15
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/228162
        Reviewed-by: Erik Språng <sprangwebrtc.org>
        Commit-Queue: Andrey Logvin <landreywebrtc.org>
        Cr-Commit-Position: refs/heads/master{#34673}
    
    UltraBlame original commit: 6e55224bc597b8a295500576553de23031e90473

--- third_party/libwebrtc/modules/video_coding/codecs/vp9/libvpx_vp9_encoder.cc
+++ third_party/libwebrtc/modules/video_coding/codecs/vp9/libvpx_vp9_encoder.cc
@@ -1902,21 +1902,17 @@ LibvpxVp9Encoder::ParsePerformanceFlagsFromTrials(
 LibvpxVp9Encoder::PerformanceFlags
 LibvpxVp9Encoder::GetDefaultPerformanceFlags() {
   PerformanceFlags flags;
-  flags.use_per_layer_speed = true;
+  flags.use_per_layer_speed = false;
 #if defined(WEBRTC_ARCH_ARM) || defined(WEBRTC_ARCH_ARM64) || defined(ANDROID)
   
   flags.settings_by_resolution[0] = {8, 8, 0};
 #else
   
   
-  
-  
-  flags.settings_by_resolution[0] = {5, 8, 1};
+  flags.settings_by_resolution[0] = {5, 5, 0};
 
   
-  
-  
-  flags.settings_by_resolution[352 * 288] = {7, 8, 0};
+  flags.settings_by_resolution[352 * 288] = {7, 7, 0};
 #endif
   return flags;
 }
