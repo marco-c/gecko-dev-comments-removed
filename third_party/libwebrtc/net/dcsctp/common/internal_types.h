aea1011b8b9848c226a403b83bc8d65b3610f1cb
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 08:59:32 +0000

    Bug 1766646 - Vendor libwebrtc from 762f21ce8d
    Upstream commit: https://webrtc.googlesource.com/src/+/762f21ce8d6c1aedb597c035532c6d174ddae85d
        dcsctp: Add Send Queue
    
        When the client asks for a message to be sent, it's put in the
        SendQueue, which is available even when the socket is not yet connected.
    
        When the socket is connected, those messages will be sent on the wire,
        possibly fragmented if the message is large enough to not fit inside a
        single packet. When the message has been fully sent, it's removed from
        the send queue (but it will be in the RetransmissionQueue - which is
        added in a follow-up change, until the message has been ACKed).
    
        The Send Queue is a FIFO queue in this iteration, and in SCTP, that's
        called a "First Come, First Served" queue, or FCFS. In follow-up work,
        the queue and the actual scheduling algorithm which decides which
        message that is sent, when there are messages in multiple streams, will
        likely be decoupled. But in this iteration, they're in the same class.
    
        Bug: webrtc:12614
        Change-Id: Iec1183e625499a21e402e4f2a5ebcf989bc5c3ec
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/214044
        Reviewed-by: Harald Alvestrand <htawebrtc.org>
        Commit-Queue: Victor Boivie <boiviewebrtc.org>
        Cr-Commit-Position: refs/heads/master{#33798}
    
    UltraBlame original commit: c7ea204172ce9c61f66a2160714c5ffaa265eecb

--- third_party/libwebrtc/net/dcsctp/common/internal_types.h
+++ third_party/libwebrtc/net/dcsctp/common/internal_types.h
@@ -13,6 +13,7 @@
 #include <utility>
 
 #include "net/dcsctp/public/strong_alias.h"
+#include "net/dcsctp/public/types.h"
 
 namespace dcsctp {
 
@@ -33,6 +34,14 @@ using ReconfigRequestSN = StrongAlias<class ReconfigRequestSNTag, uint32_t>;
 
 
 using VerificationTag = StrongAlias<class VerificationTagTag, uint32_t>;
+
+
+struct UnorderedStreamHash {
+  size_t operator()(const std::pair<IsUnordered, StreamID>& p) const {
+    return std::hash<IsUnordered::UnderlyingType>{}(*p.first) ^
+           (std::hash<StreamID::UnderlyingType>{}(*p.second) << 1);
+  }
+};
 
 }  
 #endif  
