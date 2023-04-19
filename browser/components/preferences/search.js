174adbbbee3fb58c4ec56bea549997533aa82e31
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 23:10:29 +0000

    Bug 1779470 - Part 2: Add show search terms in url bar feature to about:preferences#search - r=adw,flod
    See https://www.figma.com/file/Ock0cYjzi7I1GmcctqOr9K/Search-settings-updates?node-id=2%3A112 for mockup.
    
    Depends on D159462
    
    Differential Revision: https://phabricator.services.mozilla.com/D155866
    
    UltraBlame original commit: 4dfcb6e877c9ec1a9560db06777ee984891908d4

--- browser/components/preferences/search.js
+++ browser/components/preferences/search.js
@@ -18,6 +18,7 @@ Preferences.addAll([
   { id: "browser.search.hiddenOneOffs", type: "unichar" },
   { id: "browser.search.widget.inNavBar", type: "bool" },
   { id: "browser.urlbar.showSearchSuggestionsFirst", type: "bool" },
+  { id: "browser.urlbar.showSearchTerms.enabled", type: "bool" },
   { id: "browser.search.separatePrivateDefault", type: "bool" },
   { id: "browser.search.separatePrivateDefault.ui.enabled", type: "bool" },
 ]);
@@ -93,6 +94,7 @@ var gSearchPane = {
     });
 
     this._initDefaultEngines();
+    this._initShowSearchTermsCheckbox();
     this._updateSuggestionCheckboxes();
     this._showAddEngineButton();
   },
@@ -122,6 +124,31 @@ var gSearchPane = {
 
     this._separatePrivateDefaultEnabledPref.on("change", listener);
     this._separatePrivateDefaultPref.on("change", listener);
+  },
+
+  _initShowSearchTermsCheckbox() {
+    let checkbox = document.getElementById("searchShowSearchTermCheckbox");
+
+    
+    let onNimbus = () => {
+      checkbox.hidden = !UrlbarPrefs.get("showSearchTermsFeatureGate");
+    };
+    NimbusFeatures.urlbar.onUpdate(onNimbus);
+
+    
+    
+    let searchBarPref = Preferences.get("browser.search.widget.inNavBar");
+    let updateCheckboxEnabled = () => {
+      checkbox.disabled = searchBarPref.value;
+    };
+
+    
+    onNimbus();
+    updateCheckboxEnabled();
+
+    window.addEventListener("unload", () => {
+      NimbusFeatures.urlbar.off(onNimbus);
+    });
   },
 
   _updatePrivateEngineDisplayBoxes() {
