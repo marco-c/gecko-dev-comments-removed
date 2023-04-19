fdaa5cd1d34eb9dd9b2ad6805eea9369a3cfbee6
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 01:04:08 +0000

    Bug 1712628: mark taskbar pinning methods as not implemented for MSIX packages r=nalexander
    Differential Revision: https://phabricator.services.mozilla.com/D150578
    
    UltraBlame original commit: ab394fa4b59bdb7f9c69462f859a961f4340dc5d

--- browser/components/shell/nsWindowsShellService.cpp
+++ browser/components/shell/nsWindowsShellService.cpp
@@ -1461,6 +1461,12 @@ NS_IMETHODIMP
 nsWindowsShellService::PinCurrentAppToTaskbarAsync(bool aPrivateBrowsing,
                                                    JSContext* aCx,
                                                    dom::Promise** aPromise) {
+  
+  
+  if (widget::WinUtils::HasPackageIdentity()) {
+    return NS_ERROR_NOT_IMPLEMENTED;
+  }
+
   return PinCurrentAppToTaskbarAsyncImpl(
        false, aPrivateBrowsing, aCx, aPromise);
 }
@@ -1468,6 +1474,12 @@ nsWindowsShellService::PinCurrentAppToTaskbarAsync(bool aPrivateBrowsing,
 NS_IMETHODIMP
 nsWindowsShellService::CheckPinCurrentAppToTaskbarAsync(
     bool aPrivateBrowsing, JSContext* aCx, dom::Promise** aPromise) {
+  
+  
+  if (widget::WinUtils::HasPackageIdentity()) {
+    return NS_ERROR_NOT_IMPLEMENTED;
+  }
+
   return PinCurrentAppToTaskbarAsyncImpl(
        true, aPrivateBrowsing, aCx, aPromise);
 }
@@ -1586,6 +1598,12 @@ static bool IsCurrentAppPinnedToTaskbarSync(const nsAutoString& aumid) {
 NS_IMETHODIMP
 nsWindowsShellService::IsCurrentAppPinnedToTaskbarAsync(
     const nsAString& aumid, JSContext* aCx,  dom::Promise** aPromise) {
+  
+  
+  if (widget::WinUtils::HasPackageIdentity()) {
+    return NS_ERROR_NOT_IMPLEMENTED;
+  }
+
   if (!NS_IsMainThread()) {
     return NS_ERROR_NOT_SAME_THREAD;
   }
