5b2ec59b601d51ca0fa87442f8873aca42082df4
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 07:25:14 +0000

    Bug 1766646 - Vendor libwebrtc from ff9f6461b6
    Upstream commit: https://webrtc.googlesource.com/src/+/ff9f6461b60c568ba13d2c7253d3b541c2117c9e
        Revert "Reland "Refactor NetEq delay manager logic.""
    
        This reverts commit 2a7c57c34f323ee1977f6e7809ee23bfcf9a7459.
    
        Reason for revert: unexpected big changes in behavior.
    
        Original change's description:
        > Reland "Refactor NetEq delay manager logic."
        >
        > This is a reland of f8e62fcb14e37a5be4f1e4f599d34c8483fea8e9
        >
        > Original change's description:
        > > Refactor NetEq delay manager logic.
        > >
        > > - Removes dependence on sequence number for calculating target delay.
        > > - Changes target delay unit to milliseconds instead of number of
        > >   packets.
        > > - Moves acceleration/preemptive expand thresholds to decision logic.
        > >   Tests for this will be added in a follow up cl.
        > >
        > > Bug: webrtc:10333
        > > Change-Id: If690aae4abf41ef1d9353f0ff01fb7d121cf8a26
        > > Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/186265
        > > Commit-Queue: Jakob Ivarsson <jakobiwebrtc.org>
        > > Reviewed-by: Ivo Creusen <ivocwebrtc.org>
        > > Cr-Commit-Position: refs/heads/master{#32326}
        >
        > Bug: webrtc:10333
        > Change-Id: Iad5e7063f63b84762959ee5b412f5f14a7b2cd06
        > Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/186943
        > Commit-Queue: Jakob Ivarsson <jakobiwebrtc.org>
        > Reviewed-by: Ivo Creusen <ivocwebrtc.org>
        > Cr-Commit-Position: refs/heads/master{#32332}
    
        TBR=ivocwebrtc.org,jakobiwebrtc.org
    
        Change-Id: Iffda0e8a7b647392d8dfc6724d49439fa13d71b2
        No-Presubmit: true
        No-Tree-Checks: true
        No-Try: true
        Bug: webrtc:10333
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/187100
        Reviewed-by: Jakob Ivarsson <jakobiwebrtc.org>
        Commit-Queue: Jakob Ivarsson <jakobiwebrtc.org>
        Cr-Commit-Position: refs/heads/master{#32341}
    
    UltraBlame original commit: 79c051518af5e6b1a39b754416c0125b03abd000

--- third_party/libwebrtc/modules/audio_coding/neteq/neteq_impl_unittest.cc
+++ third_party/libwebrtc/modules/audio_coding/neteq/neteq_impl_unittest.cc
@@ -805,9 +805,7 @@ TEST_P(NetEqImplTestSampleRateParameter,
   }
 
   
-  for (size_t i = 0; i < 5; ++i) {
-    insert_packet();
-  }
+  insert_packet();
 
   
   while (output.speech_type_ != AudioFrame::kNormalSpeech) {
@@ -884,9 +882,7 @@ TEST_P(NetEqImplTestSampleRateParameter, AudioInterruptionLogged) {
   }
 
   
-  for (size_t i = 0; i < 5; ++i) {
-    insert_packet();
-  }
+  insert_packet();
 
   
   while (output.speech_type_ != AudioFrame::kNormalSpeech) {
@@ -1303,7 +1299,7 @@ TEST_F(NetEqImplTest, DecodingError) {
                                           SdpAudioFormat("L16", 8000, 1)));
 
   
-  for (int i = 0; i < 20; ++i) {
+  for (int i = 0; i < 6; ++i) {
     rtp_header.sequenceNumber += 1;
     rtp_header.timestamp += kFrameLengthSamples;
     EXPECT_EQ(NetEq::kOK, neteq_->InsertPacket(rtp_header, payload));
