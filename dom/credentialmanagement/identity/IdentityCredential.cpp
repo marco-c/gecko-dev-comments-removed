8d61660316598dbd993c5815451f88056661729a
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 17:58:54 +0000

    Bug 1782085, part 1 - Create async call into static member of IdentityCredential, r=dveditz
    - Break promise creation out of CreateAndReject into its own function
    - Call DiscoverFromInternalSource with appropriate arguments when an IdentityCredential is requested
    
    Differential Revision: https://phabricator.services.mozilla.com/D155718
    
    UltraBlame original commit: 90a71258c15c86199f6fdc66a45d027abb1f0cbe

--- dom/credentialmanagement/identity/IdentityCredential.cpp
+++ dom/credentialmanagement/identity/IdentityCredential.cpp
@@ -30,4 +30,13 @@ void IdentityCredential::SetToken(const nsAString& aToken) {
   mToken.Assign(aToken);
 }
 
+
+RefPtr<IdentityCredential::GetIdentityCredentialPromise>
+IdentityCredential::DiscoverFromExternalSource(
+    nsPIDOMWindowInner* aParent, const CredentialRequestOptions& aOptions,
+    bool aSameOriginWithAncestors) {
+  return IdentityCredential::GetIdentityCredentialPromise::CreateAndReject(
+      NS_ERROR_FAILURE, __func__);
+}
+
 }  
