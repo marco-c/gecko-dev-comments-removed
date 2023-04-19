849352010b9b919c04e0c99363311b1d12895920
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 22:19:36 +0000

    Bug 1797686 - Esmify toolkit/modules/subprocess modules. r=kmag
    Differential Revision: https://phabricator.services.mozilla.com/D160488
    
    UltraBlame original commit: a70d2c6faaad9508111bc9e9697663fc971312dd

--- toolkit/modules/subprocess/subprocess_unix.sys.mjs
+++ toolkit/modules/subprocess/subprocess_unix.sys.mjs
@@ -1,22 +1,17 @@
+/* -*- Mode: indent-tabs-mode: nil; js-indent-level: 2 -*- */
+/* vim: set sts=2 sw=2 et tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-
-
-
-
-"use strict";
-
-
-
-
-
-var EXPORTED_SYMBOLS = ["SubprocessImpl", "libc"];
+import {
+  BaseProcess,
+  PromiseWorker,
+} from "resource://gre/modules/subprocess/subprocess_common.sys.mjs";
 
 const { ctypes } = ChromeUtils.import("resource://gre/modules/ctypes.jsm");
-const { BaseProcess, PromiseWorker } = ChromeUtils.import(
-  "resource://gre/modules/subprocess/subprocess_common.jsm"
-);
 
-var obj = {};
+var obj = { ctypes };
 Services.scriptloader.loadSubScript(
   "resource://gre/modules/subprocess/subprocess_shared.js",
   obj
@@ -26,7 +21,10 @@ Services.scriptloader.loadSubScript(
   obj
 );
 
-const { SubprocessConstants, libc, LIBC } = obj;
+const { SubprocessConstants, LIBC } = obj;
+
+// libc is exported for tests.
+export var libc = obj.libc;
 
 class UnixPromiseWorker extends PromiseWorker {
   constructor(...args) {
@@ -79,9 +77,9 @@ class Process extends BaseProcess {
   }
 }
 
-
-
-
+// Convert a null-terminated char pointer into a sized char array, and then
+// convert that into a JS typed array.
+// The resulting array will not be null-terminated.
 function ptrToUint8Array(input) {
   let { cast, uint8_t } = ctypes;
 
@@ -148,30 +146,30 @@ var SubprocessUnix = {
     try {
       let info = await IOUtils.stat(path);
 
-      
-      
+      // FIXME: We really want access(path, X_OK) here, but IOUtils does not
+      // support it.
       return info.type !== "directory" && info.permissions & 0o111;
     } catch (e) {
       return false;
     }
   },
 
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  /**
+   * Searches for the given executable file in the system executable
+   * file paths as specified by the PATH environment variable.
+   *
+   * On Windows, if the unadorned filename cannot be found, the
+   * extensions in the semicolon-separated list in the PATHEXT
+   * environment variable are successively appended to the original
+   * name and searched for in turn.
+   *
+   * @param {string} bin
+   *        The name of the executable to find.
+   * @param {object} environment
+   *        An object containing a key for each environment variable to be used
+   *        in the search.
+   * @returns {Promise<string>}
+   */
   async pathSearch(bin, environment) {
     if (PathUtils.isAbsolute(bin)) {
       if (await this.isExecutableFile(bin)) {
@@ -202,4 +200,4 @@ var SubprocessUnix = {
   },
 };
 
-var SubprocessImpl = SubprocessUnix;
+export var SubprocessImpl = SubprocessUnix;
--- toolkit/modules/subprocess/subprocess_win.sys.mjs
+++ toolkit/modules/subprocess/subprocess_win.sys.mjs
@@ -1,26 +1,17 @@
+/* -*- Mode: indent-tabs-mode: nil; js-indent-level: 2 -*- */
+/* vim: set sts=2 sw=2 et tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+import { AppConstants } from "resource://gre/modules/AppConstants.sys.mjs";
+import { XPCOMUtils } from "resource://gre/modules/XPCOMUtils.sys.mjs";
+import {
+  BaseProcess,
+  PromiseWorker,
+} from "resource://gre/modules/subprocess/subprocess_common.sys.mjs";
 
-
-
-
-"use strict";
-
-
-
-
-
-var EXPORTED_SYMBOLS = ["SubprocessImpl", "libc", "win32"];
-
-const { AppConstants } = ChromeUtils.importESModule(
-  "resource://gre/modules/AppConstants.sys.mjs"
-);
 const { ctypes } = ChromeUtils.import("resource://gre/modules/ctypes.jsm");
-const { XPCOMUtils } = ChromeUtils.importESModule(
-  "resource://gre/modules/XPCOMUtils.sys.mjs"
-);
-const { BaseProcess, PromiseWorker } = ChromeUtils.import(
-  "resource://gre/modules/subprocess/subprocess_common.jsm"
-);
 
 const lazy = {};
 
@@ -31,7 +22,7 @@ XPCOMUtils.defineLazyServiceGetter(
   "nsIEnvironment"
 );
 
-var obj = {};
+var obj = { ctypes };
 Services.scriptloader.loadSubScript(
   "resource://gre/modules/subprocess/subprocess_shared.js",
   obj
@@ -41,7 +32,11 @@ Services.scriptloader.loadSubScript(
   obj
 );
 
-const { SubprocessConstants, libc, win32 } = obj;
+const { SubprocessConstants } = obj;
+
+// libc and win32 are exported for tests.
+export const libc = obj.libc;
+export const win32 = obj.win32;
 
 class WinPromiseWorker extends PromiseWorker {
   constructor(...args) {
@@ -124,30 +119,30 @@ var SubprocessWin = {
 
     try {
       let info = await IOUtils.stat(path);
-      
-      
+      // On Windows, a FileType of "other" indicates it is a reparse point
+      // (i.e., a link).
       return info.type !== "directory" && info.type !== "other";
     } catch (e) {
       return false;
     }
   },
 
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  /**
+   * Searches for the given executable file in the system executable
+   * file paths as specified by the PATH environment variable.
+   *
+   * On Windows, if the unadorned filename cannot be found, the
+   * extensions in the semicolon-separated list in the PATHEXT
+   * environment variable are successively appended to the original
+   * name and searched for in turn.
+   *
+   * @param {string} bin
+   *        The name of the executable to find.
+   * @param {object} environment
+   *        An object containing a key for each environment variable to be used
+   *        in the search.
+   * @returns {Promise<string>}
+   */
   async pathSearch(bin, environment) {
     if (PathUtils.isAbsolute(bin)) {
       if (await this.isExecutableFile(bin)) {
@@ -190,4 +185,4 @@ var SubprocessWin = {
   },
 };
 
-var SubprocessImpl = SubprocessWin;
+export var SubprocessImpl = SubprocessWin;
