52a2ddcdb6116aba8a99faf81bfd243bcd9592da
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 05:53:10 +0000

    Bug 1784957 - [marionette] Don't fail when unregistering "browser-delayed-startup-finished" observer. r=webdriver-reviewers,jdescottes
    Differential Revision: https://phabricator.services.mozilla.com/D154687
    
    UltraBlame original commit: ae85f2046a908d9a6277ca21ba79a6a9f0849d74

--- remote/marionette/driver.js
+++ remote/marionette/driver.js
@@ -88,6 +88,9 @@ const SUPPORTED_STRATEGIES = new Set([
 const TIMEOUT_NO_WINDOW_MANAGER = 5000;
 
 
+const TOPIC_BROWSER_READY = "browser-delayed-startup-finished";
+
+
 
 
 
@@ -489,7 +492,7 @@ GeckoDriver.prototype.newSession = async function(cmd) {
     lazy.registerCommandsActor();
     lazy.enableEventsActor();
 
-    Services.obs.addObserver(this, "browser-delayed-startup-finished");
+    Services.obs.addObserver(this, TOPIC_BROWSER_READY);
   } catch (e) {
     throw new lazy.error.SessionNotCreatedError(e);
   }
@@ -542,7 +545,7 @@ GeckoDriver.prototype.handleEvent = function({ target, type }) {
 
 GeckoDriver.prototype.observe = function(subject, topic, data) {
   switch (topic) {
-    case "browser-delayed-startup-finished":
+    case TOPIC_BROWSER_READY:
       this.registerListenersForWindow(subject);
       break;
   }
@@ -2267,7 +2270,11 @@ GeckoDriver.prototype.deleteSession = function() {
     this.dialogObserver = null;
   }
 
-  Services.obs.removeObserver(this, "browser-delayed-startup-finished");
+  try {
+    Services.obs.removeObserver(this, TOPIC_BROWSER_READY);
+  } catch (e) {
+    lazy.logger.debug(`Failed to remove observer "${TOPIC_BROWSER_READY}"`);
+  }
 
   lazy.clearElementIdCache();
 
