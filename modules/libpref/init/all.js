d56682a5d989e3ceea660c14d5d8a100262db26d
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 02:35:54 +0000

    Bug 1779000 - Part 2: Replace use of AtomicBoolPrefChangedCallback with StaticPrefs r=asuth
    Differential Revision: https://phabricator.services.mozilla.com/D151504
    
    UltraBlame original commit: df896d73aafc305eb90afde041bdf82258ff939b

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -131,8 +131,6 @@ pref("browser.cache.offline.capacity",         512000);
 pref("browser.download.forbid_open_with", false);
 
 
-pref("dom.indexedDB.experimental", false);
-
 pref("dom.indexedDB.logging.enabled", true);
 
 pref("dom.indexedDB.logging.details", true);
