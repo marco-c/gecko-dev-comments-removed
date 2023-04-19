ff8a96519da120492e7afc55ecbcca7e6643de0c
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 06:07:33 +0000

    Bug 1783181 - Use correct scaling vector when panning the left channel values using NEON intrinsics. r=karlt
    Differential Revision: https://phabricator.services.mozilla.com/D154911
    
    UltraBlame original commit: 1facaa986f25ac4bf2dad6fa7d9e15c5e36e11f1

--- dom/media/webaudio/AudioNodeEngineNEON.cpp
+++ dom/media/webaudio/AudioNodeEngineNEON.cpp
@@ -321,7 +321,7 @@ void AudioBlockPanStereoToStereo_NEON(
 
     
     onleft0 = vmlaq_f32(vinL0, vinR0, vscaleL0);
-    onleft1 = vmlaq_f32(vinL1, vinR1, vscaleL0);
+    onleft1 = vmlaq_f32(vinL1, vinR1, vscaleL1);
 
     
     notonleft0 = vmulq_f32(vinL0, vscaleL0);
