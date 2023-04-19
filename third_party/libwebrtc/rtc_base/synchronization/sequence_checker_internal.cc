80fd2eeb3e6334ba3c9da30465d411215bd14f60
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 08:18:06 +0000

    Bug 1766646 - Vendor libwebrtc from 3f7990d38b
    Upstream commit: https://webrtc.googlesource.com/src/+/3f7990d38b1ad8d4ae2d8e31385f727560f58b2a
        Split sequence checker on two headers before moving to API
    
        Bug: webrtc:12419
        Change-Id: I8d5acfec0c0654efc70ca089dc6a862503939220
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/205524
        Commit-Queue: Artem Titov <titovartemwebrtc.org>
        Reviewed-by: Danil Chapovalov <danilchapwebrtc.org>
        Reviewed-by: Mirko Bonadei <mbonadeiwebrtc.org>
        Cr-Commit-Position: refs/heads/master{#33152}
    
    UltraBlame original commit: 8bc4f8ded23de1b1321bfb7c92fa2e3f5a131a4e

--- third_party/libwebrtc/rtc_base/synchronization/sequence_checker_internal.cc
+++ third_party/libwebrtc/rtc_base/synchronization/sequence_checker_internal.cc
@@ -7,15 +7,19 @@
 
 
 
-#include "rtc_base/synchronization/sequence_checker.h"
+#include "rtc_base/synchronization/sequence_checker_internal.h"
+
+#include <string>
 
 #if defined(WEBRTC_MAC)
 #include <dispatch/dispatch.h>
 #endif
 
+#include "rtc_base/checks.h"
 #include "rtc_base/strings/string_builder.h"
 
 namespace webrtc {
+namespace webrtc_sequence_checker_internal {
 namespace {
 
 
@@ -29,20 +33,11 @@ const void* GetSystemQueueRef() {
 
 }  
 
-std::string ExpectationToString(const webrtc::SequenceChecker* checker) {
-#if RTC_DCHECK_IS_ON
-  return checker->ExpectationToString();
-#endif
-  return std::string();
-}
-
 SequenceCheckerImpl::SequenceCheckerImpl()
     : attached_(true),
       valid_thread_(rtc::CurrentThreadRef()),
       valid_queue_(TaskQueueBase::Current()),
       valid_system_queue_(GetSystemQueueRef()) {}
-
-SequenceCheckerImpl::~SequenceCheckerImpl() = default;
 
 bool SequenceCheckerImpl::IsCurrent() const {
   const TaskQueueBase* const current_queue = TaskQueueBase::Current();
@@ -109,4 +104,12 @@ std::string SequenceCheckerImpl::ExpectationToString() const {
 }
 #endif  
 
+std::string ExpectationToString(const SequenceCheckerImpl* checker) {
+#if RTC_DCHECK_IS_ON
+  return checker->ExpectationToString();
+#endif
+  return std::string();
+}
+
+}  
 }  
