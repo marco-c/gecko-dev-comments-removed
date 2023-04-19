c6fd917c7d80373c7c71e90bc8e22507a6cafcb5
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 00:19:50 +0000

    Backed out changeset 8e304bf129b7 (bug 1758055) for causing build bustages in nsISupportsImpl CLOSED TREE
    UltraBlame original commit: d595070ce37139fff6e6688a4f192737fc0f8bc8

--- dom/fs/api/FileSystemFileHandle.cpp
+++ dom/fs/api/FileSystemFileHandle.cpp
@@ -1,11 +1,10 @@
-
-
-
-
-
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=8 sts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "FileSystemFileHandle.h"
-#include "fs/FileSystemRequestHandler.h"
 
 #include "mozilla/ErrorResult.h"
 #include "mozilla/dom/FileSystemFileHandleBinding.h"
@@ -18,26 +17,16 @@ NS_IMPL_ISUPPORTS_CYCLE_COLLECTION_INHERITED_0(FileSystemFileHandle,
                                                FileSystemHandle)
 NS_IMPL_CYCLE_COLLECTION_INHERITED(FileSystemFileHandle, FileSystemHandle)
 
-FileSystemFileHandle::FileSystemFileHandle(
-    nsIGlobalObject* aGlobal, const fs::FileSystemEntryMetadata& aMetadata,
-    fs::FileSystemRequestHandler* aRequestHandler)
-    : FileSystemHandle(aGlobal, aMetadata, aRequestHandler) {}
-
-FileSystemFileHandle::FileSystemFileHandle(
-    nsIGlobalObject* aGlobal, const fs::FileSystemEntryMetadata& aMetadata)
-    : FileSystemFileHandle(aGlobal, aMetadata,
-                           new fs::FileSystemRequestHandler()) {}
-
-
+// WebIDL Boilerplate
 
 JSObject* FileSystemFileHandle::WrapObject(JSContext* aCx,
                                            JS::Handle<JSObject*> aGivenProto) {
   return FileSystemFileHandle_Binding::Wrap(aCx, this, aGivenProto);
 }
 
+// WebIDL Interface
 
-
-FileSystemHandleKind FileSystemFileHandle::Kind() const {
+FileSystemHandleKind FileSystemFileHandle::Kind() {
   return FileSystemHandleKind::File;
 }
 
@@ -76,4 +65,4 @@ already_AddRefed<Promise> FileSystemFileHandle::CreateSyncAccessHandle(
   return promise.forget();
 }
 
-}  
+}  // namespace mozilla::dom
