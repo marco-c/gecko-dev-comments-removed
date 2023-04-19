6babb18071f3b7c9bdcb70324945660c2b5d6c7b
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 15:02:43 +0000

    Bug 1766646 - Vendor libwebrtc from a62dcd815d0
    Upstream commit: https://webrtc.googlesource.com/src/+/a62dcd815d0261b0d1ca9f69c7196c318eec4f4a
        Export rtc::tracing symbols for use in Chrome Remote Desktop.
    
        Bug: webrtc:13585
        Change-Id: I12d88ae59c59bd3bc81b823e7e5a5b56e5e9fb6d
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/247440
        Reviewed-by: Mirko Bonadei <mbonadeiwebrtc.org>
        Reviewed-by: Tomas Gunnarsson <tommiwebrtc.org>
        Reviewed-by: Harald Alvestrand <htawebrtc.org>
        Commit-Queue: Harald Alvestrand <htawebrtc.org>
        Cr-Commit-Position: refs/heads/main{#35739}
    
    UltraBlame original commit: 454859ac8d2b3054b5ef7f2c04dc8e1451b1475e

--- third_party/libwebrtc/rtc_base/event_tracer.h
+++ third_party/libwebrtc/rtc_base/event_tracer.h
@@ -28,6 +28,8 @@
 
 #include <stdio.h>
 
+#include "rtc_base/system/rtc_export.h"
+
 namespace webrtc {
 
 typedef const unsigned char* (*GetCategoryEnabledPtr)(const char* name);
@@ -70,12 +72,12 @@ class EventTracer {
 namespace rtc {
 namespace tracing {
 
-void SetupInternalTracer();
-bool StartInternalCapture(const char* filename);
-void StartInternalCaptureToFile(FILE* file);
-void StopInternalCapture();
+RTC_EXPORT void SetupInternalTracer();
+RTC_EXPORT bool StartInternalCapture(const char* filename);
+RTC_EXPORT void StartInternalCaptureToFile(FILE* file);
+RTC_EXPORT void StopInternalCapture();
 
-void ShutdownInternalTracer();
+RTC_EXPORT void ShutdownInternalTracer();
 }  
 }  
 
