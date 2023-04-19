d95699616b2ae5efeef7e7343b4a53b68d4e7e40
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 09:48:27 +0000

    Bug 1766646 - Vendor libwebrtc from 33fdb3430d
    Upstream commit: https://webrtc.googlesource.com/src/+/33fdb3430d6d838f9be9d42abf432ae35a07d8b9
        Migrate away from legacy rtp parser in test/
    
        Bug: None
        Change-Id: I71e4a352b67a304df44454b36352285e8b11e4b5
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/226742
        Reviewed-by: Erik Språng <sprangwebrtc.org>
        Commit-Queue: Danil Chapovalov <danilchapwebrtc.org>
        Cr-Commit-Position: refs/heads/master{#34551}
    
    UltraBlame original commit: c020e9ef43c8163fe214eec176b17e04cbd38ba9

--- third_party/libwebrtc/test/peer_scenario/tests/remote_estimate_test.cc
+++ third_party/libwebrtc/test/peer_scenario/tests/remote_estimate_test.cc
@@ -8,8 +8,10 @@
 
 
 
+#include "modules/rtp_rtcp/include/rtp_header_extension_map.h"
+#include "modules/rtp_rtcp/source/rtp_header_extensions.h"
+#include "modules/rtp_rtcp/source/rtp_packet.h"
 #include "modules/rtp_rtcp/source/rtp_util.h"
-#include "modules/rtp_rtcp/source/rtp_utility.h"
 #include "pc/media_session.h"
 #include "pc/session_description.h"
 #include "test/field_trial.h"
@@ -24,19 +26,6 @@ RtpHeaderExtensionMap AudioExtensions(
   auto* audio_desc =
       cricket::GetFirstAudioContentDescription(session.description());
   return RtpHeaderExtensionMap(audio_desc->rtp_header_extensions());
-}
-
-absl::optional<RTPHeaderExtension> GetRtpPacketExtensions(
-    const rtc::ArrayView<const uint8_t> packet,
-    const RtpHeaderExtensionMap& extension_map) {
-  RtpUtility::RtpHeaderParser rtp_parser(packet.data(), packet.size());
-  if (IsRtpPacket(packet)) {
-    RTPHeader header;
-    if (rtp_parser.Parse(&header, &extension_map, true)) {
-      return header.extension;
-    }
-  }
-  return absl::nullopt;
 }
 
 }  
@@ -106,13 +95,10 @@ TEST(RemoteEstimateEndToEnd, AudioUsesAbsSendTimeExtension) {
         
         
         
-        
-        if (packet.size() > 1 && packet.cdata()[0] != 0) {
-          auto extensions = GetRtpPacketExtensions(packet.data, extension_map);
-          if (extensions) {
-            EXPECT_TRUE(extensions->hasAbsoluteSendTime);
-            received_abs_send_time = true;
-          }
+        RtpPacket rtp_packet(&extension_map);
+        if (rtp_packet.Parse(packet.data)) {
+          EXPECT_TRUE(rtp_packet.HasExtension<AbsoluteSendTime>());
+          received_abs_send_time = true;
         }
       });
   RTC_CHECK(s.WaitAndProcess(&received_abs_send_time));
