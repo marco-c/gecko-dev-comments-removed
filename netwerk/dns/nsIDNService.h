a527dbcaebf3a179c5fe9da2cee9887d8a5c842f
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 19:25:47 +0000

    Bug 1792858 - Part 3: Simplify locking in nsIDNService, r=necko-reviewers,kershaw
    Previously a complex locking system was used to handle pref updates in the
    interface while allowing off-main-thread use. With the previous changes, only a
    small set of methods now depend on the custom pref observer. This substantially
    narrows the scope of locking in nsIDNService to where it is needed, and
    switches to a normal lock which can be acquired on all threads.
    
    Depends on D158521
    
    Differential Revision: https://phabricator.services.mozilla.com/D158522
    
    UltraBlame original commit: 150f5cfd55fd27c75ce0d3302c2447be47b7e3f5

--- netwerk/dns/nsIDNService.h
+++ netwerk/dns/nsIDNService.h
@@ -9,9 +9,8 @@
 #include "nsIIDNService.h"
 #include "nsCOMPtr.h"
 #include "nsWeakReference.h"
-#include "nsThreadUtils.h"
 
-#include "mozilla/Mutex.h"
+#include "mozilla/RWLock.h"
 #include "mozilla/intl/UnicodeScriptCodes.h"
 #include "mozilla/net/IDNBlocklistUtils.h"
 #include "mozilla/intl/IDNA.h"
@@ -26,15 +25,12 @@ class nsIPrefBranch;
 
 
 class nsIDNService final : public nsIIDNService,
-                           public nsSupportsWeakReference,
-                           public mozilla::SingleWriterLockOwner {
+                           public nsSupportsWeakReference {
  public:
   NS_DECL_THREADSAFE_ISUPPORTS
   NS_DECL_NSIIDNSERVICE
 
   nsIDNService();
-
-  bool OnWritingThread() const override { return NS_IsMainThread(); }
 
   nsresult Init();
 
@@ -104,7 +100,6 @@ class nsIDNService final : public nsIIDNService,
 
   static void PrefChanged(const char* aPref, void* aSelf) {
     auto* self = static_cast<nsIDNService*>(aSelf);
-    mozilla::MutexSingleWriterAutoLock lock(self->mLock);
     self->prefsChanged(aPref);
   }
 
@@ -135,7 +130,7 @@ class nsIDNService final : public nsIIDNService,
 
 
 
-  bool isLabelSafe(const nsAString& label);
+  bool isLabelSafe(const nsAString& label) MOZ_EXCLUDES(mLock);
 
   
 
@@ -151,7 +146,8 @@ class nsIDNService final : public nsIIDNService,
 
 
 
-  bool illegalScriptCombo(mozilla::intl::Script script, int32_t& savedScript);
+  bool illegalScriptCombo(mozilla::intl::Script script, int32_t& savedScript)
+      MOZ_REQUIRES_SHARED(mLock);
 
   
 
@@ -164,15 +160,12 @@ class nsIDNService final : public nsIIDNService,
   nsresult IDNA2008StringPrep(const nsAString& input, nsAString& output,
                               stringPrepFlag flag);
 
+  
   mozilla::UniquePtr<mozilla::intl::IDNA> mIDNA;
 
   
   
-  
-  
-  
-  
-  mozilla::MutexSingleWriter mLock;
+  mozilla::RWLock mLock{"nsIDNService"};
 
   
   nsTArray<mozilla::net::BlocklistRange> mIDNBlocklist MOZ_GUARDED_BY(mLock);
