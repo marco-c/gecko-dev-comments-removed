d6ed7d8908214e8e7f2f71c157312b18dacded58
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 07:41:33 +0000

    Bug 1766646 - Vendor libwebrtc from 06bbeb3398
    Upstream commit: https://webrtc.googlesource.com/src/+/06bbeb3398afa8651ca76ea7eb56b7d046edfca8
        in Av1 encoder wrapper communicate end_of_picture flag similar to VP9
    
        In particular move end_of_picture flag out of vp9 specific information
        since VP9 is not the only codec that can use spatial scalability and
        thus need to distinguish layer frame and picture (aka temporal unit).
    
        Bug: webrtc:12167
        Change-Id: I0d046d8785fbea55281209ad099738c03ea7db96
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/192542
        Reviewed-by: Sami Kalliomäki <sakalwebrtc.org>
        Reviewed-by: Erik Språng <sprangwebrtc.org>
        Commit-Queue: Danil Chapovalov <danilchapwebrtc.org>
        Cr-Commit-Position: refs/heads/master{#32588}
    
    UltraBlame original commit: 49f8505e8b82243af2a2508977197737bcba1440

--- third_party/libwebrtc/modules/video_coding/codecs/vp9/test/vp9_impl_unittest.cc
+++ third_party/libwebrtc/modules/video_coding/codecs/vp9/test/vp9_impl_unittest.cc
@@ -919,8 +919,8 @@ TEST_F(TestVp9Impl, EndOfPicture) {
   std::vector<EncodedImage> frames;
   std::vector<CodecSpecificInfo> codec_specific;
   ASSERT_TRUE(WaitForEncodedFrames(&frames, &codec_specific));
-  EXPECT_FALSE(codec_specific[0].codecSpecific.VP9.end_of_picture);
-  EXPECT_TRUE(codec_specific[1].codecSpecific.VP9.end_of_picture);
+  EXPECT_FALSE(codec_specific[0].end_of_picture);
+  EXPECT_TRUE(codec_specific[1].end_of_picture);
 
   
   
@@ -935,7 +935,7 @@ TEST_F(TestVp9Impl, EndOfPicture) {
 
   ASSERT_TRUE(WaitForEncodedFrames(&frames, &codec_specific));
   EXPECT_FALSE(frames[0].SpatialIndex());
-  EXPECT_TRUE(codec_specific[0].codecSpecific.VP9.end_of_picture);
+  EXPECT_TRUE(codec_specific[0].end_of_picture);
 }
 
 TEST_F(TestVp9Impl, InterLayerPred) {
