3edd0111acd6c572d9d86d830bdef1c8ffc0df8b
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 16:28:41 +0000

    Bug 1788536 - [devtools] Use descriptorType instead of a type decipher from toolbox URL. r=jdescottes
    This helps having the right type, even if we open the toolbox without any search params
    in about:devtools-toolbox URL.
    
    Differential Revision: https://phabricator.services.mozilla.com/D156134
    
    UltraBlame original commit: e1038a2827e0af4493b4c2d25ad0e21f381e80c9

--- devtools/client/framework/components/ToolboxToolbar.js
+++ devtools/client/framework/components/ToolboxToolbar.js
@@ -125,7 +125,7 @@ class ToolboxToolbar extends Component {
       
       debugTargetData: PropTypes.shape({
         runtimeInfo: PropTypes.object.isRequired,
-        targetType: PropTypes.string.isRequired,
+        descriptorType: PropTypes.string.isRequired,
       }),
       
       fluentBundles: PropTypes.array.isRequired,
