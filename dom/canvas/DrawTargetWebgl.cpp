c9219b52e045c7186d47bc5cf9ec91b87b4f3286
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 02:34:07 +0000

    Bug 1779342 - Force any Skia snapshots to copy the shmem before it deallocs. r=aosmond
    Differential Revision: https://phabricator.services.mozilla.com/D151876
    
    UltraBlame original commit: 09e0fa770021501e4cb1c243109f437b6b47cb18

--- dom/canvas/DrawTargetWebgl.cpp
+++ dom/canvas/DrawTargetWebgl.cpp
@@ -217,6 +217,8 @@ DrawTargetWebgl::~DrawTargetWebgl() {
   ClearSnapshot(false);
   if (mSharedContext) {
     if (mShmem.IsWritable()) {
+      
+      mSkia->DetachAllSnapshots();
       auto* child = mSharedContext->mWebgl->GetChild();
       if (child && child->CanSend()) {
         child->DeallocShmem(mShmem);
