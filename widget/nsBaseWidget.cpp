98f4ce5ea3b23987169e562977f0167ef4f50c65
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 06:03:52 +0000

    Bug 1776895 - [5/6] Save and restore original relative position r=cmartin
    When entering fullscreen and saving the original position of a window,
    also save the original position of its screen.
    
    When leaving fullscreen, keep the window on the same screen. Restore its
    _screen-relative_ position, rather than its absolute position.
    
    Differential Revision: https://phabricator.services.mozilla.com/D153410
    
    UltraBlame original commit: 3d2dc8de0e7147d0d991c16c29ad9a8678a25635

--- widget/nsBaseWidget.cpp
+++ widget/nsBaseWidget.cpp
@@ -775,10 +775,11 @@ void nsBaseWidget::InfallibleMakeFullScreen(bool aFullScreen) {
   };
 
   if (aFullScreen) {
-    if (!mOriginalBounds) {
-      mOriginalBounds = mozilla::MakeUnique<DesktopRect>();
+    if (!mSavedBounds) {
+      mSavedBounds = mozilla::MakeUnique<FullscreenSavedState>();
     }
-    *mOriginalBounds = GetScreenBounds() / GetDesktopToDeviceScale();
+    
+    mSavedBounds->windowRect = GetScreenBounds() / GetDesktopToDeviceScale();
 
     nsCOMPtr<nsIScreen> screen = GetWidgetScreen();
     if (!screen) {
@@ -787,16 +788,80 @@ void nsBaseWidget::InfallibleMakeFullScreen(bool aFullScreen) {
 
     
     const auto screenRect = screen->GetRectDisplayPix();
+    mSavedBounds->screenRect = screenRect;
     doReposition(screenRect);
   } else {
-    if (!mOriginalBounds) {
+    if (!mSavedBounds) {
       
       
       MOZ_ASSERT(false, "fullscreen window did not have saved position");
       return;
     }
 
-    doReposition(*mOriginalBounds);
+    
+    
+    
+    
+    
+    
+
+    const DesktopRect currentWinRect =
+        GetScreenBounds() / GetDesktopToDeviceScale();
+
+    
+    
+    if (currentWinRect == DesktopRect(mSavedBounds->screenRect)) {
+      doReposition(mSavedBounds->windowRect);
+      return;
+    }
+
+    
+
+
+
+
+
+
+
+
+
+
+
+    
+    const auto splat = [](auto rect) {
+      return std::tuple(rect.X(), rect.Y(), rect.Width(), rect.Height());
+    };
+
+    
+    
+    using Range = std::pair<float, float>;
+    const auto remap = [](Range dst, Range src, float val) {
+      
+      const auto lerp = [](float lo, float hi, float t) {
+        return lo + t * (hi - lo);
+      };
+      const auto colerp = [](float lo, float hi, float mid) {
+        return (mid - lo) / (hi - lo);
+      };
+
+      const auto [dst_a, dst_b] = dst;
+      const auto [src_a, src_b] = src;
+      return lerp(dst_a, dst_b, colerp(src_a, src_b, val));
+    };
+
+    
+    const auto [px, py, pw, ph] = splat(mSavedBounds->windowRect);
+    
+    const auto [sx, sy, sw, sh] = splat(mSavedBounds->screenRect);
+    
+    const auto [tx, ty, tw, th] = splat(currentWinRect);
+
+    const float nx = remap({tx, tx + tw}, {sx, sx + sw}, px);
+    const float ny = remap({ty, ty + th}, {sy, sy + sh}, py);
+    const float nw = remap({0, tw}, {0, sw}, pw);
+    const float nh = remap({0, th}, {0, sh}, ph);
+
+    doReposition(DesktopRect{nx, ny, nw, nh});
   }
 }
 
