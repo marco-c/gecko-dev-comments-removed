80369aaa3fa4d4e8b662cec8eb2d2f01785602d7
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 06:31:28 +0000

    Bug 1783496 - Add cookieBehavior5,cookieBehaviorPBM5 back to strict ETP pref so dFPI item is shown in the strict category. r=timhuang
    Differential Revision: https://phabricator.services.mozilla.com/D155066
    
    UltraBlame original commit: d3f34611f29698e27bfbff2ee6135d319d57dc6b

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1905,7 +1905,7 @@ pref("browser.contentblocking.reject-and-isolate-cookies.preferences.ui.enabled"
 
 
 
-pref("browser.contentblocking.features.strict", "tp,tpPrivate,cm,fp,stp,lvl2,rp,rpTop,ocsp,qps,qpsPBM");
+pref("browser.contentblocking.features.strict", "tp,tpPrivate,cookieBehavior5,cookieBehaviorPBM5,cm,fp,stp,lvl2,rp,rpTop,ocsp,qps,qpsPBM");
 
 
 
