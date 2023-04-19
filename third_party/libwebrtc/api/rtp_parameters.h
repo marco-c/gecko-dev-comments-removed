c403d6cf3d59db6662fdac59e541d7c25e65dc6a
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 10:17:34 +0000

    Bug 1766646 - (fix-d0321c5e5ab) convert to using kCsrcAudioLevelsUri vs kCsrcAudioLevelUri (see Bug 1771332)
    UltraBlame original commit: 6993c7601558afb992600f236bcf2462c4dd4ca7

--- third_party/libwebrtc/api/rtp_parameters.h
+++ third_party/libwebrtc/api/rtp_parameters.h
@@ -305,13 +305,6 @@ struct RTC_EXPORT RtpExtension {
   static constexpr char kAudioLevelUri[] =
       "urn:ietf:params:rtp-hdrext:ssrc-audio-level";
 
-#if defined(WEBRTC_MOZILLA_BUILD)
-  
-  
-  static constexpr char kCsrcAudioLevelUri[] =
-      "urn:ietf:params:rtp-hdrext:csrc-audio-level";
-#endif
-
   
   
   static constexpr char kTimestampOffsetUri[] =
@@ -387,12 +380,10 @@ struct RTC_EXPORT RtpExtension {
   static constexpr char kVideoFrameTrackingIdUri[] =
       "http://www.webrtc.org/experiments/rtp-hdrext/video-frame-tracking-id";
 
-#if !defined(WEBRTC_MOZILLA_BUILD)
   
   
   static constexpr char kCsrcAudioLevelsUri[] =
       "urn:ietf:params:rtp-hdrext:csrc-audio-level";
-#endif
 
   
   
