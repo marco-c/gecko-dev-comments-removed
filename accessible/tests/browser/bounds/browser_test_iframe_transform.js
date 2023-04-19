7fb6176b6ee50568b0b2179f2688cb09507c6153
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 20:25:07 +0000

    Bug 1794654 - Wait for cache update before comparing bounds to content. r=morgan
    We first retrieve accessible bounds and then pass them to a content task to compare against content layout bounds. We are not guaranteed to have the cache in sync in that manner, most tests will wait for a content rAF but that is not sufficient for the parent cache's state.
    
    Instead we need to pull the content layout bounds (since those are guaranteed to be in sync via rAF) into parent and then wait for the a11y cache to catch up.
    
    Differential Revision: https://phabricator.services.mozilla.com/D159080
    
    UltraBlame original commit: c6e6251e298892f132124cd8197cb4df13878195

--- accessible/tests/browser/bounds/browser_test_iframe_transform.js
+++ accessible/tests/browser/bounds/browser_test_iframe_transform.js
@@ -72,7 +72,7 @@ addAccessibleTask(
   async function(browser, iframeDocAcc, contentDocAcc) {
     ok(iframeDocAcc, "IFRAME document accessible is present");
 
-    await testBoundsInContent(iframeDocAcc, ELEM_ID, browser);
+    await testBoundsWithContent(iframeDocAcc, ELEM_ID, browser);
 
     
     await invokeSetStyleIframe(
