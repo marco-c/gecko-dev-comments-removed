485565f854f2310cabb41a08c785afe04a029cf6
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 04:54:20 +0000

    Backed out changeset 48d2a85f55af (bug 1783606) for causing failure at browser_inspector-iframe.js. CLOSED TREE
    UltraBlame original commit: 918dfe91237aeebf9cf85314b9b72ea924fb6ca4

--- devtools/client/fronts/node.js
+++ devtools/client/fronts/node.js
@@ -320,15 +320,7 @@ class NodeFront extends FrontClassWithSpec(nodeSpec) {
   
   get childrenUnavailable() {
     return (
-      
-      
-      
-      
-      !!this._form.useChildTargetToFetchChildren &&
-      
-      
-      
-      
+      this._form.useChildTargetToFetchChildren &&
       !this.useChildTargetToFetchChildren
     );
   }
@@ -351,7 +343,7 @@ class NodeFront extends FrontClassWithSpec(nodeSpec) {
       return false;
     }
 
-    return !!this._form.useChildTargetToFetchChildren;
+    return this._form.useChildTargetToFetchChildren;
   }
   get hasEventListeners() {
     return this._form.hasEventListeners;
