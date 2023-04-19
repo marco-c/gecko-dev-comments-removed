41e85457c09263d0d2de8222afddbff512f81e8a
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 17:17:35 +0000

    Bug 1787159 - It can be necessary to sniff more data in some cases. r=necko-reviewers,valentin
    Sometimes, media needs more than 512 bytes to determine the content type.
    
    Differential Revision: https://phabricator.services.mozilla.com/D155587
    
    UltraBlame original commit: 0f9e3836fef569adc85c41967e3ff483ec6fe252

--- netwerk/streamconv/converters/nsUnknownDecoder.cpp
+++ netwerk/streamconv/converters/nsUnknownDecoder.cpp
@@ -24,10 +24,9 @@
 #include "nsQueryObject.h"
 #include "nsComponentManagerUtils.h"
 #include "nsServiceManagerUtils.h"
+#include "mozilla/StaticPrefs_network.h"
 
 #include <algorithm>
-
-#define MAX_BUFFER_SIZE 512u
 
 using namespace mozilla;
 
@@ -174,8 +173,9 @@ nsUnknownDecoder::OnDataAvailable(nsIRequest* request, nsIInputStream* aStream,
     
     
     
-    if (mBufferLen + aCount >= MAX_BUFFER_SIZE) {
-      count = MAX_BUFFER_SIZE - mBufferLen;
+    if (mBufferLen + aCount >=
+        StaticPrefs::network_http_mimesniff_max_length()) {
+      count = StaticPrefs::network_http_mimesniff_max_length() - mBufferLen;
     } else {
       count = aCount;
     }
@@ -239,7 +239,7 @@ nsUnknownDecoder::OnStartRequest(nsIRequest* request) {
 
   
   if (NS_SUCCEEDED(rv) && !mBuffer) {
-    mBuffer = new char[MAX_BUFFER_SIZE];
+    mBuffer = new char[StaticPrefs::network_http_mimesniff_max_length()];
 
     if (!mBuffer) {
       rv = NS_ERROR_OUT_OF_MEMORY;
@@ -417,7 +417,9 @@ void nsUnknownDecoder::DetermineContentType(nsIRequest* aRequest) {
     }
     if (!decodedData.IsEmpty()) {
       testData = decodedData.get();
-      testDataLen = std::min<uint32_t>(decodedData.Length(), MAX_BUFFER_SIZE);
+      testDataLen =
+          std::min<uint32_t>(decodedData.Length(),
+                             StaticPrefs::network_http_mimesniff_max_length());
     }
   }
 
@@ -504,7 +506,8 @@ bool nsUnknownDecoder::SniffForHTML(nsIRequest* aRequest) {
   } else {
     str = mDecodedData.get();
     end = mDecodedData.get() +
-          std::min<uint32_t>(mDecodedData.Length(), MAX_BUFFER_SIZE);
+          std::min<uint32_t>(mDecodedData.Length(),
+                             StaticPrefs::network_http_mimesniff_max_length());
   }
 
   
@@ -606,10 +609,13 @@ bool nsUnknownDecoder::LastDitchSniff(nsIRequest* aRequest) {
     testData = mBuffer;
     
     
-    testDataLen = std::min<uint32_t>(mBufferLen, MAX_BUFFER_SIZE);
+    testDataLen = std::min<uint32_t>(
+        mBufferLen, StaticPrefs::network_http_mimesniff_max_length());
   } else {
     testData = mDecodedData.get();
-    testDataLen = std::min<uint32_t>(mDecodedData.Length(), MAX_BUFFER_SIZE);
+    testDataLen =
+        std::min<uint32_t>(mDecodedData.Length(),
+                           StaticPrefs::network_http_mimesniff_max_length());
   }
 
   
@@ -715,8 +721,9 @@ nsresult nsUnknownDecoder::FireListenerNotifications(nsIRequest* request,
     nsCOMPtr<nsIOutputStream> out;
 
     
-    rv = NS_NewPipe(getter_AddRefs(in), getter_AddRefs(out), MAX_BUFFER_SIZE,
-                    MAX_BUFFER_SIZE);
+    rv = NS_NewPipe(getter_AddRefs(in), getter_AddRefs(out),
+                    StaticPrefs::network_http_mimesniff_max_length(),
+                    StaticPrefs::network_http_mimesniff_max_length());
 
     if (NS_SUCCEEDED(rv)) {
       rv = out->Write(mBuffer, mBufferLen, &len);
