44d2c781e8f1db961bdd7a19fa5e50cc7d2d9165
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 08:49:48 +0000

    Bug 1766646 - Vendor libwebrtc from 11bd143974
    Upstream commit: https://webrtc.googlesource.com/src/+/11bd143974d6bade27394703eb574e2ac053d8ae
        AGC2 add an interface for the noise level estimator
    
        Done in preparation for the child CL which adds an alternative
        implementation.
    
        Bug: webrtc:7494
        Change-Id: I4963376afc917eae434a0d0ccee18f21880eefe0
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/214125
        Reviewed-by: Jakob Ivarsson <jakobiwebrtc.org>
        Commit-Queue: Alessio Bazzica <alessiobwebrtc.org>
        Cr-Commit-Position: refs/heads/master{#33646}
    
    UltraBlame original commit: fb8f745d8576f3a2cf42065077ee5ba11dd4cad3

--- third_party/libwebrtc/modules/audio_processing/agc2/noise_level_estimator.cc
+++ third_party/libwebrtc/modules/audio_processing/agc2/noise_level_estimator.cc
@@ -18,11 +18,11 @@
 
 #include "api/array_view.h"
 #include "common_audio/include/audio_util.h"
+#include "modules/audio_processing/agc2/signal_classifier.h"
 #include "modules/audio_processing/logging/apm_data_dumper.h"
 #include "rtc_base/checks.h"
 
 namespace webrtc {
-
 namespace {
 constexpr int kFramesPerSecond = 100;
 
@@ -41,86 +41,106 @@ float EnergyToDbfs(float signal_energy, size_t num_samples) {
   const float rms = std::sqrt(signal_energy / num_samples);
   return FloatS16ToDbfs(rms);
 }
-}  
 
-NoiseLevelEstimator::NoiseLevelEstimator(ApmDataDumper* data_dumper)
-    : data_dumper_(data_dumper), signal_classifier_(data_dumper) {
-  Initialize(48000);
-}
-
-NoiseLevelEstimator::~NoiseLevelEstimator() {}
-
-void NoiseLevelEstimator::Initialize(int sample_rate_hz) {
-  sample_rate_hz_ = sample_rate_hz;
-  noise_energy_ = 1.0f;
-  first_update_ = true;
-  min_noise_energy_ = sample_rate_hz * 2.0f * 2.0f / kFramesPerSecond;
-  noise_energy_hold_counter_ = 0;
-  signal_classifier_.Initialize(sample_rate_hz);
-}
-
-float NoiseLevelEstimator::Analyze(const AudioFrameView<const float>& frame) {
-  data_dumper_->DumpRaw("agc2_noise_level_estimator_hold_counter",
-                        noise_energy_hold_counter_);
-  const int sample_rate_hz =
-      static_cast<int>(frame.samples_per_channel() * kFramesPerSecond);
-  if (sample_rate_hz != sample_rate_hz_) {
-    Initialize(sample_rate_hz);
+class NoiseLevelEstimatorImpl : public NoiseLevelEstimator {
+ public:
+  NoiseLevelEstimatorImpl(ApmDataDumper* data_dumper)
+      : data_dumper_(data_dumper), signal_classifier_(data_dumper) {
+    Initialize(48000);
   }
-  const float frame_energy = FrameEnergy(frame);
-  if (frame_energy <= 0.f) {
-    RTC_DCHECK_GE(frame_energy, 0.f);
-    data_dumper_->DumpRaw("agc2_noise_level_estimator_signal_type", -1);
-    return EnergyToDbfs(noise_energy_, frame.samples_per_channel());
-  }
+  NoiseLevelEstimatorImpl(const NoiseLevelEstimatorImpl&) = delete;
+  NoiseLevelEstimatorImpl& operator=(const NoiseLevelEstimatorImpl&) = delete;
+  ~NoiseLevelEstimatorImpl() = default;
 
-  if (first_update_) {
+  float Analyze(const AudioFrameView<const float>& frame) {
+    data_dumper_->DumpRaw("agc2_noise_level_estimator_hold_counter",
+                          noise_energy_hold_counter_);
+    const int sample_rate_hz =
+        static_cast<int>(frame.samples_per_channel() * kFramesPerSecond);
+    if (sample_rate_hz != sample_rate_hz_) {
+      Initialize(sample_rate_hz);
+    }
+    const float frame_energy = FrameEnergy(frame);
+    if (frame_energy <= 0.f) {
+      RTC_DCHECK_GE(frame_energy, 0.f);
+      data_dumper_->DumpRaw("agc2_noise_level_estimator_signal_type", -1);
+      return EnergyToDbfs(noise_energy_, frame.samples_per_channel());
+    }
+
+    if (first_update_) {
+      
+      first_update_ = false;
+      data_dumper_->DumpRaw("agc2_noise_level_estimator_signal_type", -1);
+      noise_energy_ = std::max(frame_energy, min_noise_energy_);
+      return EnergyToDbfs(noise_energy_, frame.samples_per_channel());
+    }
+
+    const SignalClassifier::SignalType signal_type =
+        signal_classifier_.Analyze(frame.channel(0));
+    data_dumper_->DumpRaw("agc2_noise_level_estimator_signal_type",
+                          static_cast<int>(signal_type));
+
     
-    first_update_ = false;
-    data_dumper_->DumpRaw("agc2_noise_level_estimator_signal_type", -1);
-    noise_energy_ = std::max(frame_energy, min_noise_energy_);
-    return EnergyToDbfs(noise_energy_, frame.samples_per_channel());
-  }
+    if (signal_type == SignalClassifier::SignalType::kStationary) {
+      if (frame_energy > noise_energy_) {
+        
+        
+        noise_energy_hold_counter_ =
+            std::max(noise_energy_hold_counter_ - 1, 0);
 
-  const SignalClassifier::SignalType signal_type =
-      signal_classifier_.Analyze(frame.channel(0));
-  data_dumper_->DumpRaw("agc2_noise_level_estimator_signal_type",
-                        static_cast<int>(signal_type));
-
-  
-  if (signal_type == SignalClassifier::SignalType::kStationary) {
-    if (frame_energy > noise_energy_) {
-      
-      
-      noise_energy_hold_counter_ = std::max(noise_energy_hold_counter_ - 1, 0);
-
-      if (noise_energy_hold_counter_ == 0) {
-        constexpr float kMaxNoiseEnergyFactor = 1.01f;
+        if (noise_energy_hold_counter_ == 0) {
+          constexpr float kMaxNoiseEnergyFactor = 1.01f;
+          noise_energy_ =
+              std::min(noise_energy_ * kMaxNoiseEnergyFactor, frame_energy);
+        }
+      } else {
+        
+        constexpr float kMinNoiseEnergyFactor = 0.9f;
+        constexpr float kNoiseEnergyDeltaFactor = 0.05f;
         noise_energy_ =
-            std::min(noise_energy_ * kMaxNoiseEnergyFactor, frame_energy);
+            std::max(noise_energy_ * kMinNoiseEnergyFactor,
+                     noise_energy_ - kNoiseEnergyDeltaFactor *
+                                         (noise_energy_ - frame_energy));
+        
+        constexpr int kNumFramesToEnergyIncreaseAllowed = 1000;
+        noise_energy_hold_counter_ = kNumFramesToEnergyIncreaseAllowed;
       }
     } else {
       
-      constexpr float kMinNoiseEnergyFactor = 0.9f;
-      constexpr float kNoiseEnergyDeltaFactor = 0.05f;
-      noise_energy_ =
-          std::max(noise_energy_ * kMinNoiseEnergyFactor,
-                   noise_energy_ - kNoiseEnergyDeltaFactor *
-                                       (noise_energy_ - frame_energy));
       
-      constexpr int kNumFramesToEnergyIncreaseAllowed = 1000;
-      noise_energy_hold_counter_ = kNumFramesToEnergyIncreaseAllowed;
+      
+      noise_energy_ = noise_energy_ * 0.99f;
     }
-  } else {
+
     
-    
-    
-    noise_energy_ = noise_energy_ * 0.99f;
+    noise_energy_ = std::max(noise_energy_, min_noise_energy_);
+    return EnergyToDbfs(noise_energy_, frame.samples_per_channel());
   }
 
-  
-  noise_energy_ = std::max(noise_energy_, min_noise_energy_);
-  return EnergyToDbfs(noise_energy_, frame.samples_per_channel());
+ private:
+  void Initialize(int sample_rate_hz) {
+    sample_rate_hz_ = sample_rate_hz;
+    noise_energy_ = 1.0f;
+    first_update_ = true;
+    min_noise_energy_ = sample_rate_hz * 2.0f * 2.0f / kFramesPerSecond;
+    noise_energy_hold_counter_ = 0;
+    signal_classifier_.Initialize(sample_rate_hz);
+  }
+
+  ApmDataDumper* const data_dumper_;
+  int sample_rate_hz_;
+  float min_noise_energy_;
+  bool first_update_;
+  float noise_energy_;
+  int noise_energy_hold_counter_;
+  SignalClassifier signal_classifier_;
+};
+
+}  
+
+std::unique_ptr<NoiseLevelEstimator> CreateNoiseLevelEstimator(
+    ApmDataDumper* data_dumper) {
+  return std::make_unique<NoiseLevelEstimatorImpl>(data_dumper);
 }
 
 }  
