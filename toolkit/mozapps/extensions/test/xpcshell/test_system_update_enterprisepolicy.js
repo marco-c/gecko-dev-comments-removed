114c522bbc456dc38c486856f258efaa7aea95d0
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 17:25:28 +0000

    Bug 1789254 - Migrate enterprisepolicies consumers to use ESM imports directly. r=mkaply,necko-reviewers,application-update-reviewers,nalexander
    Differential Revision: https://phabricator.services.mozilla.com/D156464
    
    UltraBlame original commit: bea1e1ba4a14e676e4ebefa371d8fb5fea9d6001

--- toolkit/mozapps/extensions/test/xpcshell/test_system_update_enterprisepolicy.js
+++ toolkit/mozapps/extensions/test/xpcshell/test_system_update_enterprisepolicy.js
@@ -5,8 +5,8 @@
 
 
 
-const { EnterprisePolicyTesting } = ChromeUtils.import(
-  "resource://testing-common/EnterprisePolicyTesting.jsm"
+const { EnterprisePolicyTesting } = ChromeUtils.importESModule(
+  "resource://testing-common/EnterprisePolicyTesting.sys.mjs"
 );
 
 
