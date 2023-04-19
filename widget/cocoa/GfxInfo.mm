1de0faf71de87a062c1897ecdb5ea21f7af58382
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 22:25:23 +0000

    Bug 1794722 - Part 1. Remove WebRender from the GfxInfo allowlist. r=jrmuizel
    WebRender is a mature feature. We should start blocking it on known bad
    devices over allowlisting known good devices. This may enable WebRender
    in a few more obscure places than we shipped before.
    
    Differential Revision: https://phabricator.services.mozilla.com/D160120
    
    UltraBlame original commit: f0dd55319351336090da268b2ff2c2b4be7cc065

--- widget/cocoa/GfxInfo.mm
+++ widget/cocoa/GfxInfo.mm
@@ -406,22 +406,14 @@ const nsTArray<GfxDriverInfo>& GfxInfo::GetGfxDriverInfo() {
         nsIGfxInfo::FEATURE_BLOCKED_DEVICE, "FEATURE_FAILURE_MAC_GPU_SWITCHING_NO_SWIZZLE");
 
     
-    IMPLEMENT_MAC_DRIVER_BLOCKLIST(OperatingSystem::OSX, DeviceFamily::IntelRolloutWebRender,
-                                   nsIGfxInfo::FEATURE_WEBRENDER, nsIGfxInfo::FEATURE_ALLOW_ALWAYS,
-                                   "FEATURE_ROLLOUT_INTEL_MAC");
+    IMPLEMENT_MAC_DRIVER_BLOCKLIST(
+        OperatingSystem::OSX, DeviceFamily::IntelWebRenderBlocked, nsIGfxInfo::FEATURE_WEBRENDER,
+        nsIGfxInfo::FEATURE_BLOCKED_DEVICE, "FEATURE_FAILURE_INTEL_GEN5_OR_OLDER");
+
     
     IMPLEMENT_MAC_DRIVER_BLOCKLIST(
         OperatingSystem::OSX, DeviceFamily::IntelSandyBridge, nsIGfxInfo::FEATURE_WEBRENDER,
         nsIGfxInfo::FEATURE_BLOCKED_DEVICE, "FEATURE_FAILURE_INTEL_MAC_HD3000_NO_WEBRENDER");
-    IMPLEMENT_MAC_DRIVER_BLOCKLIST(OperatingSystem::OSX, DeviceFamily::AtiRolloutWebRender,
-                                   nsIGfxInfo::FEATURE_WEBRENDER, nsIGfxInfo::FEATURE_ALLOW_ALWAYS,
-                                   "FEATURE_ROLLOUT_AMD_MAC");
-    IMPLEMENT_MAC_DRIVER_BLOCKLIST(OperatingSystem::OSX, DeviceFamily::NvidiaRolloutWebRender,
-                                   nsIGfxInfo::FEATURE_WEBRENDER, nsIGfxInfo::FEATURE_ALLOW_ALWAYS,
-                                   "FEATURE_ROLLOUT_NVIDIA_MAC");
-    IMPLEMENT_MAC_DRIVER_BLOCKLIST(OperatingSystem::OSX, DeviceFamily::AppleAll,
-                                   nsIGfxInfo::FEATURE_WEBRENDER, nsIGfxInfo::FEATURE_ALLOW_ALWAYS,
-                                   "FEATURE_ROLLOUT_APPLE_SILICON_MAC");
   }
   return *sDriverInfo;
 }
