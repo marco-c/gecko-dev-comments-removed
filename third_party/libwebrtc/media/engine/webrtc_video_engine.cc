824d40fbb40e9e9fbdc5a84a83da0aea5a41b74a
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 10:30:50 +0000

    Bug 1766646 - Vendor libwebrtc from eb42ab77cf
    Upstream commit: https://webrtc.googlesource.com/src/+/eb42ab77cfcee464a43f4bf95ce9142e185a3393
        Dont use simulcast for AV1.
    
        Bug: none
        Change-Id: I9d3bfb3bff497db740e317fcad0e8f91bfa88d1c
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/234744
        Reviewed-by: Ilya Nikolaevskiy <ilnikwebrtc.org>
        Commit-Queue: Philip Eliasson <philipelwebrtc.org>
        Cr-Commit-Position: refs/heads/main{#35177}
    
    UltraBlame original commit: f9f5f802b891e67efed28ac31b4573598816bd47

--- third_party/libwebrtc/media/engine/webrtc_video_engine.cc
+++ third_party/libwebrtc/media/engine/webrtc_video_engine.cc
@@ -296,10 +296,16 @@ static bool ValidateStreamParams(const StreamParams& sp) {
 
 bool IsCodecDisabledForSimulcast(const std::string& codec_name,
                                  const webrtc::WebRtcKeyValueConfig& trials) {
-  return !absl::StartsWith(trials.Lookup("WebRTC-H264Simulcast"), "Disabled")
-             ? absl::EqualsIgnoreCase(codec_name, kVp9CodecName)
-             : absl::EqualsIgnoreCase(codec_name, kH264CodecName) ||
-                   absl::EqualsIgnoreCase(codec_name, kVp9CodecName);
+  if (absl::EqualsIgnoreCase(codec_name, kVp9CodecName) ||
+      absl::EqualsIgnoreCase(codec_name, kAv1CodecName)) {
+    return true;
+  }
+
+  if (absl::EqualsIgnoreCase(codec_name, kH264CodecName)) {
+    return absl::StartsWith(trials.Lookup("WebRTC-H264Simulcast"), "Disabled");
+  }
+
+  return false;
 }
 
 
