95af1bf10bfde3653a3f8683844e97887b859f30
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 17:22:23 +0000

    Bug 1780912 - [devtools] Open toolboxes for local WebExtension in dedicated window. r=jdescottes
    This should hopefully help debug addons displaying panels/popups.
    
    Differential Revision: https://phabricator.services.mozilla.com/D155393
    
    UltraBlame original commit: 21f1e9031be0698dcc03220d69be32ca9fb21426

--- devtools/client/aboutdebugging/test/browser/head.js
+++ devtools/client/aboutdebugging/test/browser/head.js
@@ -85,16 +85,33 @@ async function openAboutDevtoolsToolbox(
   info("Open about:devtools-toolbox page");
   const target = findDebugTargetByText(targetText, doc);
   ok(target, `${targetText} tab target appeared`);
+
+  const {
+    DEBUG_TARGETS,
+  } = require("devtools/client/aboutdebugging/src/constants");
+  const isWebExtension = target.dataset.qaTargetType == DEBUG_TARGETS.EXTENSION;
+
   const inspectButton = target.querySelector(".qa-debug-target-inspect-button");
   ok(inspectButton, `Inspect button for ${targetText} appeared`);
   inspectButton.click();
+  const onToolboxReady = gDevTools.once("toolbox-ready");
   await Promise.all([
-    waitUntil(() => tab.nextElementSibling),
     waitForAboutDebuggingRequests(win.AboutDebugging.store),
-    shouldWaitToolboxReady
-      ? gDevTools.once("toolbox-ready")
-      : Promise.resolve(),
+    shouldWaitToolboxReady ? onToolboxReady : Promise.resolve(),
   ]);
+
+  
+  if (isWebExtension) {
+    const toolbox = await onToolboxReady;
+    return {
+      devtoolsBrowser: null,
+      devtoolsDocument: toolbox.doc,
+      devtoolsTab: null,
+      devtoolsWindow: toolbox.win,
+    };
+  }
+
+  await waitUntil(() => tab.nextElementSibling);
 
   info("Wait for about:devtools-toolbox tab will be selected");
   const devtoolsTab = tab.nextElementSibling;
@@ -151,6 +168,17 @@ async function closeAboutDevtoolsToolbox(
   await waitUntil(
     () => !findDebugTargetByText("Toolbox - ", aboutDebuggingDocument)
   );
+
+  await waitForAboutDebuggingRequests(win.AboutDebugging.store);
+}
+
+async function closeWebExtAboutDevtoolsToolbox(devtoolsWindow, win) {
+  
+  const toolbox = getToolbox(devtoolsWindow);
+  await toolbox.commands.client.waitForRequestsToSettle();
+
+  info("Close the toolbox and wait for its destruction");
+  await toolbox.destroy();
 
   await waitForAboutDebuggingRequests(win.AboutDebugging.store);
 }
@@ -444,6 +472,12 @@ async function synthesizeUrlKeyInput(toolbox, inputEl, url) {
 
 function clickOnAddonWidget(addonId) {
   
+  
+  const focusedWin = Services.focus.focusedWindow;
+  if (focusedWin != window) {
+    window.focus();
+  }
+  
   const widgetId = addonId.toLowerCase().replace(/[^a-z0-9_-]/g, "_");
   const browserActionId = widgetId + "-browser-action";
   const browserActionEl = window.document.getElementById(browserActionId);
@@ -451,4 +485,9 @@ function clickOnAddonWidget(addonId) {
 
   info("Show the web extension popup");
   browserActionEl.click();
+
+  
+  if (focusedWin != window) {
+    focusedWin.focus();
+  }
 }
