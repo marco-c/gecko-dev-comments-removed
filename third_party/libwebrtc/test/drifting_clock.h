98431cbee5a65a03210191f47298fb23fa456068
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 09:17:04 +0000

    Bug 1766646 - Vendor libwebrtc from 0de1ed0244
    Upstream commit: https://webrtc.googlesource.com/src/+/0de1ed0244a8da395811ad8952e5f5f8a2643e2a
        Have only two pure virtual methods for webrtc::Clock,
        `CurrentTime` and `CurrentNtpTime`. Make all other methods non-virtual.
    
        Bug: webrtc:11327
        Change-Id: I391d9eaec1ba27ec4f8e1901498c68c28a7ec4ff
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/219466
        Reviewed-by: Niels Moller <nissewebrtc.org>
        Commit-Queue: Paul Hallak <phallakgoogle.com>
        Cr-Commit-Position: refs/heads/master{#34065}
    
    UltraBlame original commit: 9d0a708f20849002504d4619069f371edd89b1ec

--- third_party/libwebrtc/test/drifting_clock.h
+++ third_party/libwebrtc/test/drifting_clock.h
@@ -1,12 +1,12 @@
-/*
- *  Copyright (c) 2016 The WebRTC project authors. All Rights Reserved.
- *
- *  Use of this source code is governed by a BSD-style license
- *  that can be found in the LICENSE file in the root of the source
- *  tree. An additional intellectual property rights grant can be found
- *  in the file PATENTS.  All contributing project authors may
- *  be found in the AUTHORS file in the root of the source tree.
- */
+
+
+
+
+
+
+
+
+
 #ifndef TEST_DRIFTING_CLOCK_H_
 #define TEST_DRIFTING_CLOCK_H_
 
@@ -32,7 +32,6 @@ class DriftingClock : public Clock {
 
   Timestamp CurrentTime() override;
   NtpTime CurrentNtpTime() override;
-  int64_t CurrentNtpInMilliseconds() override;
 
  private:
   TimeDelta Drift() const;
@@ -41,7 +40,7 @@ class DriftingClock : public Clock {
   const float drift_;
   const Timestamp start_time_;
 };
-}  // namespace test
-}  // namespace webrtc
+}  
+}  
 
-#endif  // TEST_DRIFTING_CLOCK_H_
+#endif  
