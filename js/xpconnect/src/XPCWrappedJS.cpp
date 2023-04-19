a09344f91a4cc42e6f86c514f312fc1604675632
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 18:55:22 +0000

    Bug 1543537, part 2 - Maintain and suspect a list of WJS that are subject to finalization. r=kmag
    The cycle collector needs to be able to see nsXPCWrappedJS that are held
    alive only from JS in order to unlink them. This patch adds a linked
    list of WJS that are subject to finalization, and suspects those WJS in
    the CC. Wrapped JS are normally suspected via the JS holder mechanism,
    but that skips things, like WJS subject to finalization, that aren't holding
    alive JS objects.
    
    Differential Revision: https://phabricator.services.mozilla.com/D157671
    
    UltraBlame original commit: f40d36a8487124f2e91cdcc875792681b936682f

--- js/xpconnect/src/XPCWrappedJS.cpp
+++ js/xpconnect/src/XPCWrappedJS.cpp
@@ -253,6 +253,11 @@ MozExternalRefCountType nsXPCWrappedJS::AddRef(void) {
 
   if (2 == cnt && IsValid()) {
     GetJSObject();  
+
+    
+    if (isInList()) {
+      remove();
+    }
   }
 
   return cnt;
@@ -285,6 +290,10 @@ MozExternalRefCountType nsXPCWrappedJS::Release(void) {
     
     if (!HasWeakReferences()) {
       return Release();
+    }
+
+    if (IsValid()) {
+      XPCJSRuntime::Get()->AddSubjectToFinalizationWJS(this);
     }
 
     MOZ_ASSERT(IsRootWrapper(),
@@ -499,6 +508,9 @@ void nsXPCWrappedJS::Unlink() {
   }
 
   if (IsRootWrapper()) {
+    if (isInList()) {
+      remove();
+    }
     ClearWeakReferences();
   } else if (mRoot) {
     
@@ -597,6 +609,9 @@ void nsXPCWrappedJS::SystemIsBeingShutDown() {
   
   MOZ_ASSERT(!IsIncrementalGCInProgress(xpc_GetSafeJSContext()));
   *mJSObj.unsafeGet() = nullptr;
+  if (isInList()) {
+    remove();
+  }
 
   
   if (mNext) {
