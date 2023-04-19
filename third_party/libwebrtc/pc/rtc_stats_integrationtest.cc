20a258d76adbc58ea7383f6ed9ec3b43e06a9558
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 09:35:34 +0000

    Bug 1766646 - Vendor libwebrtc from fe6580fb87
    Upstream commit: https://webrtc.googlesource.com/src/+/fe6580fb873a40fc2fe028288eff72bf4bef1eef
        Revert "Fix echo return loss stats and add to RTCAudioSourceStats."
    
        This reverts commit a27cfbffdfa0bf359628d2164db5b9d6321f9c9c.
    
        Reason for revert: WebRtcBrowserTest.RunsAudioVideoWebRTCCallInTwoTabsGetStatsPromise failing.
    
        Original change's description:
        > Fix echo return loss stats and add to RTCAudioSourceStats.
        >
        > This solves two problems:
        > * Echo return loss stats weren't being gathered in Chrome, because they
        >   need to be taken from the audio processor attached to the track
        >   rather than the audio send stream.
        > * The standardized location is in RTCAudioSourceStats, not
        >   RTCMediaStreamTrackStats. For now, will populate the stats in both
        >   locations.
        >
        > Bug: webrtc:12770
        > Change-Id: I47eaf7f2b50b914a1be84156aa831e27497d07e3
        > Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/223182
        > Commit-Queue: Taylor Brandstetter <deadbeefwebrtc.org>
        > Reviewed-by: Henrik Boström <hboswebrtc.org>
        > Cr-Commit-Position: refs/heads/master{#34344}
    
        TBR=deadbeefwebrtc.org,hboswebrtc.org,hboschromium.org,webrtc-scopedluci-project-accounts.iam.gserviceaccount.com
    
        Change-Id: I6b2587d762f005adef67c0d5121f1b58c3b76688
        No-Presubmit: true
        No-Tree-Checks: true
        No-Try: true
        Bug: webrtc:12770
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/223068
        Reviewed-by: Evan Shrubsole <eshrgoogle.com>
        Reviewed-by: Henrik Boström <hboswebrtc.org>
        Commit-Queue: Evan Shrubsole <eshrgoogle.com>
        Cr-Commit-Position: refs/heads/master{#34352}
    
    UltraBlame original commit: 21a88a110afbda0024186e75259b3fd1f2f22e76

--- third_party/libwebrtc/pc/rtc_stats_integrationtest.cc
+++ third_party/libwebrtc/pc/rtc_stats_integrationtest.cc
@@ -1082,12 +1082,6 @@ class RTCStatsReportVerifier {
     verifier.TestMemberIsNonNegative<double>(audio_source.audio_level);
     verifier.TestMemberIsPositive<double>(audio_source.total_audio_energy);
     verifier.TestMemberIsPositive<double>(audio_source.total_samples_duration);
-    
-    
-    
-    
-    verifier.MarkMemberTested(audio_source.echo_return_loss, true);
-    verifier.MarkMemberTested(audio_source.echo_return_loss_enhancement, true);
     return verifier.ExpectAllMembersSuccessfullyTested();
   }
 
