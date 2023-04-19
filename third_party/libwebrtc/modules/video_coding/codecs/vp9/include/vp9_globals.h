f0299869da93c2854d2bb3463fc07b48bdb8ed45
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 15:02:46 +0000

    Bug 1766646 - Vendor libwebrtc from 16cbed4782a
    Upstream commit: https://webrtc.googlesource.com/src/+/16cbed4782ab28a290f2075c841e53b85078689a
        Update how VP9 temporal up switch is populated
    
        This CL updates both the static GOF pattern with the correct flags for
        temporal_up_switch, as well the flexible mode logic to base the flag
        on dependency descriptors instead use reference buffers.
    
        Bug: webrtc:13576
        Change-Id: I578f744bec51d1f3531da5f4a89d12f05a16a6c0
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/247187
        Reviewed-by: Danil Chapovalov <danilchapwebrtc.org>
        Commit-Queue: Erik Språng <sprangwebrtc.org>
        Cr-Commit-Position: refs/heads/main{#35741}
    
    UltraBlame original commit: fe249b77a680df84861e568088365f383b52a820

--- third_party/libwebrtc/modules/video_coding/codecs/vp9/include/vp9_globals.h
+++ third_party/libwebrtc/modules/video_coding/codecs/vp9/include/vp9_globals.h
@@ -46,14 +46,14 @@ struct GofInfoVP9 {
       case kTemporalStructureMode1:
         num_frames_in_gof = 1;
         temporal_idx[0] = 0;
-        temporal_up_switch[0] = false;
+        temporal_up_switch[0] = true;
         num_ref_pics[0] = 1;
         pid_diff[0][0] = 1;
         break;
       case kTemporalStructureMode2:
         num_frames_in_gof = 2;
         temporal_idx[0] = 0;
-        temporal_up_switch[0] = false;
+        temporal_up_switch[0] = true;
         num_ref_pics[0] = 1;
         pid_diff[0][0] = 2;
 
@@ -65,7 +65,7 @@ struct GofInfoVP9 {
       case kTemporalStructureMode3:
         num_frames_in_gof = 4;
         temporal_idx[0] = 0;
-        temporal_up_switch[0] = false;
+        temporal_up_switch[0] = true;
         num_ref_pics[0] = 1;
         pid_diff[0][0] = 4;
 
@@ -87,7 +87,7 @@ struct GofInfoVP9 {
       case kTemporalStructureMode4:
         num_frames_in_gof = 8;
         temporal_idx[0] = 0;
-        temporal_up_switch[0] = false;
+        temporal_up_switch[0] = true;
         num_ref_pics[0] = 1;
         pid_diff[0][0] = 4;
 
@@ -97,12 +97,12 @@ struct GofInfoVP9 {
         pid_diff[1][0] = 1;
 
         temporal_idx[2] = 1;
-        temporal_up_switch[2] = true;
+        temporal_up_switch[2] = false;
         num_ref_pics[2] = 1;
         pid_diff[2][0] = 2;
 
         temporal_idx[3] = 2;
-        temporal_up_switch[3] = false;
+        temporal_up_switch[3] = true;
         num_ref_pics[3] = 2;
         pid_diff[3][0] = 1;
         pid_diff[3][1] = 2;
@@ -113,7 +113,7 @@ struct GofInfoVP9 {
         pid_diff[4][0] = 4;
 
         temporal_idx[5] = 2;
-        temporal_up_switch[5] = false;
+        temporal_up_switch[5] = true;
         num_ref_pics[5] = 2;
         pid_diff[5][0] = 1;
         pid_diff[5][1] = 2;
@@ -125,7 +125,7 @@ struct GofInfoVP9 {
         pid_diff[6][1] = 4;
 
         temporal_idx[7] = 2;
-        temporal_up_switch[7] = false;
+        temporal_up_switch[7] = true;
         num_ref_pics[7] = 2;
         pid_diff[7][0] = 1;
         pid_diff[7][1] = 2;
@@ -195,6 +195,9 @@ struct RTPVideoHeaderVP9 {
   uint8_t temporal_idx;     
   uint8_t spatial_idx;      
   bool temporal_up_switch;  
+                            
+                            
+                            
                             
   bool inter_layer_predicted;  
                                
