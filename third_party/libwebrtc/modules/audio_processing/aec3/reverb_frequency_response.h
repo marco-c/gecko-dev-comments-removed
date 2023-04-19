9dbdb2b237cdd3cff1330cb423ea6b29c724e0e4
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 10:16:02 +0000

    Bug 1766646 - Vendor libwebrtc from 5eb5bb50070
    Upstream commit: https://webrtc.googlesource.com/src/+/5eb5bb50070176caed9e90fb3ba6ffa200a97520
        AEC3: Using a more conservative frequency response representation of the tails.
    
        Bug: webrtc:13173
        Change-Id: Ic469f6226fe079c306cec6f941eeb70d6d9094f3
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/231682
        Commit-Queue: Jesus de Vicente Pena <devicentepenawebrtc.org>
        Reviewed-by: Per Åhgren <peahwebrtc.org>
        Cr-Commit-Position: refs/heads/main{#34966}
    
    UltraBlame original commit: d169565329eeba86b8a12a7abd87c34b5a1db438

--- third_party/libwebrtc/modules/audio_processing/aec3/reverb_frequency_response.h
+++ third_party/libwebrtc/modules/audio_processing/aec3/reverb_frequency_response.h
@@ -23,7 +23,8 @@ namespace webrtc {
 
 class ReverbFrequencyResponse {
  public:
-  ReverbFrequencyResponse();
+  explicit ReverbFrequencyResponse(
+      bool use_conservative_tail_frequency_response);
   ~ReverbFrequencyResponse();
 
   
@@ -44,6 +45,7 @@ class ReverbFrequencyResponse {
               int filter_delay_blocks,
               float linear_filter_quality);
 
+  const bool use_conservative_tail_frequency_response_;
   float average_decay_ = 0.f;
   std::array<float, kFftLengthBy2Plus1> tail_response_;
 };
