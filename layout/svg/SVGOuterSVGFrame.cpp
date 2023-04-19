6ef2edd07fba1b1e4e213bfbbaea94d5481d6573
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 17:46:02 +0000

    Bug 1790167 - Make percent isize also fall back to fallback intrinsic size. r=TYLin
    Matches other browsers and makes sense that width="100%" would behave
    the same as width: 100% via CSS, I guess.
    
    Differential Revision: https://phabricator.services.mozilla.com/D157021
    
    UltraBlame original commit: 3f3f7b8d088e33f0ac60dba31200de37dc61c1c9

--- layout/svg/SVGOuterSVGFrame.cpp
+++ layout/svg/SVGOuterSVGFrame.cpp
@@ -178,7 +178,7 @@ nscoord SVGOuterSVGFrame::GetPrefISize(gfxContext* aRenderingContext) {
           ContainSizeAxesIfApplicable(this).ContainIntrinsicISize(*this)) {
     result = *containISize;
   } else if (isize.IsPercentage()) {
-    if (isize.IsExplicitlySet()) {
+    if (isize.IsExplicitlySet() || StylePosition()->ISize(wm).HasPercent()) {
       
       
       
