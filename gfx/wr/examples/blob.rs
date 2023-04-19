4cc301c95f02cf672e241352f30feab1a119af0c
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 05:18:36 +0000

    Bug 1690244 - Move WebRender initialization code out of renderer.rs. r=kvark
    There is a lot going on in renderer.rs, the initialization code takes a non-negligible chunk of it and touches all aspects of WebRender including setting up all of the threads. This patch moves it into its own init module.
    In addition, RendererOptions is renamed into WebRenderOptions to better reflect that it configures every aspect of the engine, and init::create_webrender_instance replaces Renderer::new.
    
    This a rebased version of an pld patch approved by kvark a while back.
    
    Differential Revision: https://phabricator.services.mozilla.com/D103767
    
    UltraBlame original commit: 6237668e5fca50be5adbf0bd6e45168bcb5caabd

--- gfx/wr/examples/blob.rs
+++ gfx/wr/examples/blob.rs
@@ -276,7 +276,7 @@ fn main() {
 
     let workers = Arc::new(workers.unwrap());
 
-    let opts = webrender::RendererOptions {
+    let opts = webrender::WebRenderOptions {
         workers: Some(Arc::clone(&workers)),
         
         
