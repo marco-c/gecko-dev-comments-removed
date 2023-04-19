04dc97f0b07e45c9455108e48e783072c4e44366
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 23:32:37 +0000

    Bug 1794693 - [devtools] Convert source-map jest test into mochitests. r=bomsy
    Differential Revision: https://phabricator.services.mozilla.com/D159116
    
    UltraBlame original commit: 0c0dc33dbc887f8ffee186de83cbe38bc6f3dd2d

---
+++ devtools/client/shared/source-map-loader/test/browser/browser_getContentType.js
@@ -0,0 +1,32 @@
+
+
+
+"use strict";
+
+
+
+const {
+  getContentType,
+  contentMapForTesting,
+} = require("resource://devtools/client/shared/source-map-loader/utils/index.js");
+
+add_task(async () => {
+  for (const ext in contentMapForTesting) {
+    is(
+      getContentType(`whatever.${ext}`),
+      contentMapForTesting[ext],
+      `${ext} file extension is correctly mapping the expected content type`
+    );
+  }
+  is(
+    getContentType(`whateverjs`),
+    "text/plain",
+    `A valid extension in file name doesn't cause a special content type mapping`
+  );
+
+  is(
+    getContentType("whatever.platypus"),
+    "text/plain",
+    "Test unknown extension defaults to text plain"
+  );
+});
