c24515b7e037b6bed6410ec6c58ae5b4c979ccb9
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 02:31:10 +0000

    Bug 1779269 - Fix AccessibleCaret's display on disabled form controls. r=emilio,webdriver-reviewers,whimboo
    A disabled form controls cannot be focused, and its frame selection is different
    from the one for not-editable content. Use GetLastFocusedFrameSelection() (added
    in Bug 253870) to get the correct frame selection that is visible to the user.
    
    Add some basic tests for disabled <textarea> such as long pressing to select,
    dragging, etc. They should behave the same as normal <textarea>.
    
    Differential Revision: https://phabricator.services.mozilla.com/D151800
    
    UltraBlame original commit: 429187de34ff15c1e0423302515c0a9ba69e22f4

--- layout/base/AccessibleCaretManager.cpp
+++ layout/base/AccessibleCaretManager.cpp
@@ -820,23 +820,9 @@ already_AddRefed<nsFrameSelection> AccessibleCaretManager::GetFrameSelection()
     return nullptr;
   }
 
-  nsFocusManager* fm = nsFocusManager::GetFocusManager();
-  MOZ_ASSERT(fm);
-
-  nsIContent* focusedContent = fm->GetFocusedElement();
-  if (!focusedContent) {
-    
-    return mPresShell->FrameSelection();
-  }
-
-  nsIFrame* focusFrame = focusedContent->GetPrimaryFrame();
-  if (!focusFrame) {
-    return nullptr;
-  }
-
   
   
-  RefPtr<nsFrameSelection> fs = focusFrame->GetFrameSelection();
+  RefPtr<nsFrameSelection> fs = mPresShell->GetLastFocusedFrameSelection();
   if (!fs || fs->GetPresShell() != mPresShell) {
     return nullptr;
   }
