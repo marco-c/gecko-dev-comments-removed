10020c27d4939d6daf8a3d87f536faf0ee228243
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 09:41:52 +0000

    Bug 1766646 - Vendor libwebrtc from e09a174746
    Upstream commit: https://webrtc.googlesource.com/src/+/e09a174746712aea14c8a87c8cad27c3c871dbc3
        Fix ssl_certificate_fuzzer
    
        Bug: webrtc:10395
        Change-Id: Iba79f257c427545c36052e74296d3c07a166ee7d
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/225540
        Reviewed-by: Sam Zackrisson <sazawebrtc.org>
        Commit-Queue: Danil Chapovalov <danilchapwebrtc.org>
        Cr-Commit-Position: refs/heads/master{#34446}
    
    UltraBlame original commit: 5d772e475723eaf4c51c04f5f503af491b603b58

--- third_party/libwebrtc/test/fuzzers/ssl_certificate_fuzzer.cc
+++ third_party/libwebrtc/test/fuzzers/ssl_certificate_fuzzer.cc
@@ -1,18 +1,19 @@
-/*
- *  Copyright (c) 2019 The WebRTC project authors. All Rights Reserved.
- *
- *  Use of this source code is governed by a BSD-style license
- *  that can be found in the LICENSE file in the root of the source
- *  tree. An additional intellectual property rights grant can be found
- *  in the file PATENTS.  All contributing project authors may
- *  be found in the AUTHORS file in the root of the source tree.
- */
+
+
+
+
+
+
+
+
+
 
 #include <stddef.h>
 #include <stdint.h>
 
 #include <string>
 
+#include "rtc_base/message_digest.h"
 #include "rtc_base/ssl_certificate.h"
 #include "rtc_base/string_encode.h"
 
@@ -34,7 +35,7 @@ void FuzzOneInput(const uint8_t* data, size_t size) {
   cert->CertificateExpirationTime();
 
   std::string algorithm;
-  cert->GetSignatureDigestAlgorithm(algorithm);
+  cert->GetSignatureDigestAlgorithm(&algorithm);
 
   unsigned char digest[rtc::MessageDigest::kMaxSize];
   size_t digest_len;
@@ -45,4 +46,4 @@ void FuzzOneInput(const uint8_t* data, size_t size) {
   cert->ToDER(&der_buffer);
 }
 
-}  // namespace webrtc
+}  
