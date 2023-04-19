4a5d9b33fcd6783adeff07c103425f08fb36984c
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 15:50:57 +0000

    Bug 1766646 - Vendor libwebrtc from a92d051e0f
    Upstream commit: https://webrtc.googlesource.com/src/+/a92d051e0f4f3dcd4c43086a8f1edbbe5859235f
        [PCLF] Introduce API to safely mutate ConfigurableParams in TestPeer
    
        Bug: b/213863770
        Change-Id: I90b7b5cd55ac5a8ebee5d790205a4fa6700dfff4
        No-Try: True
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/260117
        Reviewed-by: Mirko Bonadei <mbonadeiwebrtc.org>
        Commit-Queue: Artem Titov <titovartemwebrtc.org>
        Cr-Commit-Position: refs/heads/main{#36668}
    
    UltraBlame original commit: 652d2c0411cd95704ddea954b9eee6fbd62acba8

--- third_party/libwebrtc/api/test/peerconnection_quality_test_fixture.h
+++ third_party/libwebrtc/api/test/peerconnection_quality_test_fixture.h
@@ -233,10 +233,10 @@ class PeerConnectionE2EQualityTestFixture {
           stream_label(std::move(stream_label)) {}
 
     
-    const size_t width;
+    size_t width;
     
-    const size_t height;
-    const int32_t fps;
+    size_t height;
+    int32_t fps;
     VideoResolution GetResolution() const {
       return VideoResolution(width, height, fps);
     }
