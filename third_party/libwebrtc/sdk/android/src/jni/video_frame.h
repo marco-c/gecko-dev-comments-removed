e807905c5b67ae70f6314ba6c7111d38ea1084cc
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 07:28:42 +0000

    Bug 1766646 - Vendor libwebrtc from 299c839919
    Upstream commit: https://webrtc.googlesource.com/src/+/299c83991904846ccd88f64f2ed39338a0e2a8b6
        Add back AndroidVideoBuffer::CropAndScale
    
        Now compatible with the recently added interface method
        VideoFramebuffer::CropAndScale.
    
        Bug: webrtc:11976
        Change-Id: I461cf2de1d73ca953fda0ecad84d216b8b7ac879
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/187493
        Reviewed-by: Sami Kalliomäki <sakalwebrtc.org>
        Reviewed-by: Ilya Nikolaevskiy <ilnikwebrtc.org>
        Commit-Queue: Niels Moller <nissewebrtc.org>
        Cr-Commit-Position: refs/heads/master{#32391}
    
    UltraBlame original commit: f03eb7ca7c3f5a561b8eadac4d0e12f7e0e615ce

--- third_party/libwebrtc/sdk/android/src/jni/video_frame.h
+++ third_party/libwebrtc/sdk/android/src/jni/video_frame.h
@@ -40,6 +40,15 @@ class AndroidVideoBuffer : public VideoFrameBuffer {
 
   const ScopedJavaGlobalRef<jobject>& video_frame_buffer() const;
 
+  
+  
+  rtc::scoped_refptr<VideoFrameBuffer> CropAndScale(int crop_x,
+                                                    int crop_y,
+                                                    int crop_width,
+                                                    int crop_height,
+                                                    int scale_width,
+                                                    int scale_height) override;
+
  protected:
   
   
