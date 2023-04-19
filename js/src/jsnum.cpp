42d37679463821b7ad39a866957cda330d6a2292
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 02:56:03 +0000

    Bug 1780010 - Part 4: Store "Infinity" as string_view in js_strtod. r=jandem
    `std::string_view` stores a pointer and its length, so we no longer have to
    use two different variables. `std::string_view` methods are also `const_expr`,
    so the compiler will inline them at compile-time.
    
    Depends on D152043
    
    Differential Revision: https://phabricator.services.mozilla.com/D152044
    
    UltraBlame original commit: 4617fbe2c524c77b557e9fb5f9bffc7a619bb00a

--- js/src/jsnum.cpp
+++ js/src/jsnum.cpp
@@ -24,6 +24,7 @@
 #endif
 #include <math.h>
 #include <string.h>  
+#include <string_view>
 
 #include "jstypes.h"
 
@@ -2188,18 +2189,17 @@ double js_strtod(const CharT* begin, const CharT* end, const CharT** dEnd) {
   
   
   
-  static constexpr char InfinityStr[] = "Infinity";
-  static constexpr size_t InfinityLen = sizeof(InfinityStr) - 1;
-  if (length >= InfinityLen) {
+  static constexpr std::string_view Infinity = "Infinity";
+  if (length >= Infinity.length()) {
     const CharT* afterSign = s;
     bool negative = (*afterSign == '-');
     if (negative || *afterSign == '+') {
       afterSign++;
     }
     MOZ_ASSERT(afterSign < end);
-    if (*afterSign == 'I' && size_t(end - afterSign) >= InfinityLen &&
-        EqualChars(afterSign, InfinityStr, InfinityLen)) {
-      *dEnd = afterSign + InfinityLen;
+    if (*afterSign == 'I' && size_t(end - afterSign) >= Infinity.length() &&
+        EqualChars(afterSign, Infinity.data(), Infinity.length())) {
+      *dEnd = afterSign + Infinity.length();
       return negative ? NegativeInfinity<double>() : PositiveInfinity<double>();
     }
   }
