12df6d1abb46c79874653f3bd76a158cc45d6b12
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 07:51:56 +0000

    Bug 1766646 - (fix-6e7167456b) Cherry-pick ead570c963 - reverts 6e7167456b
    UltraBlame original commit: 1b7f3af8edfc3ccf5bc5a71aad64989d4f230bc5

--- third_party/libwebrtc/modules/video_coding/codecs/vp9/vp9_impl.h
+++ third_party/libwebrtc/modules/video_coding/codecs/vp9/vp9_impl.h
@@ -115,6 +115,7 @@ class VP9EncoderImpl : public VP9Encoder {
   const VP9Profile profile_;
   bool inited_;
   int64_t timestamp_;
+  int cpu_speed_;
   uint32_t rc_max_intra_target_;
   vpx_codec_ctx_t* encoder_;
   vpx_codec_enc_cfg_t* config_;
@@ -197,28 +198,11 @@ class VP9EncoderImpl : public VP9Encoder {
       const WebRtcKeyValueConfig& trials);
   const bool external_ref_ctrl_;
 
-  
-  
-  struct PerformanceFlags {
-    
-    int min_pixel_count = 0;
-
-    int base_layer_speed = -1;  
-    int high_layer_speed = -1;  
-    
-    
-    
-    int deblock_mode = 0;
-  };
-  
-  const std::vector<PerformanceFlags> performance_flags_;
-  
-  
-  std::vector<PerformanceFlags> performance_flags_by_spatial_index_;
-  void UpdatePerformanceFlags();
-  static std::vector<PerformanceFlags> ParsePerformanceFlagsFromTrials(
-      const WebRtcKeyValueConfig& trials);
-  static std::vector<PerformanceFlags> GetDefaultPerformanceFlags();
+  const struct SpeedSettings {
+    bool enabled;
+    int layers[kMaxSpatialLayers];
+  } per_layer_speed_;
+  static SpeedSettings ParsePerLayerSpeed(const WebRtcKeyValueConfig& trials);
 
   int num_steady_state_frames_;
   
