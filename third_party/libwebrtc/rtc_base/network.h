4b8f3318f83fe53166d410e0d2ab27d1f8a908b4
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 15:44:09 +0000

    Bug 1766646 - Vendor libwebrtc from 7679e9bf07
    Upstream commit: https://webrtc.googlesource.com/src/+/7679e9bf071250e8e98ef6ef58962ddcc73cd498
        Delete deprecated versions of MergeNetworkList
    
        Bug: webrtc:13869
        Change-Id: I6b888ba14ca664a1f28de2fb59b7d1343cb18bd8
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/259300
        Reviewed-by: Harald Alvestrand <htawebrtc.org>
        Commit-Queue: Niels Moller <nissewebrtc.org>
        Cr-Commit-Position: refs/heads/main{#36611}
    
    UltraBlame original commit: 7535544c14417f82ed21b08ae690338b4d704a49

--- third_party/libwebrtc/rtc_base/network.h
+++ third_party/libwebrtc/rtc_base/network.h
@@ -117,9 +117,6 @@ class NetworkMask {
 class RTC_EXPORT NetworkManager : public DefaultLocalAddressProvider,
                                   public MdnsResponderProvider {
  public:
-  using NetworkList ABSL_DEPRECATED("bugs.webrtc.org/13869") =
-      std::vector<Network*>;
-
   
   enum EnumerationPermission {
     ENUMERATION_ALLOWED,  
@@ -213,13 +210,6 @@ class RTC_EXPORT NetworkManagerBase : public NetworkManager {
 
   
   void MergeNetworkList(std::vector<std::unique_ptr<Network>> list,
-                        bool* changed,
-                        NetworkManager::Stats* stats);
-  ABSL_DEPRECATED("bugs.webrtc.org/13869")
-  void MergeNetworkList(std::vector<Network*> list, bool* changed);
-
-  ABSL_DEPRECATED("bugs.webrtc.org/13869")
-  void MergeNetworkList(std::vector<Network*> list,
                         bool* changed,
                         NetworkManager::Stats* stats);
 
