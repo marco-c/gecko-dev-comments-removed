1e2a78165a867dec7ba42a0e7a31a106c46650c8
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 17:54:20 +0000

    Bug 1787505 - land NSS NSS_3_83_BETA2 UPGRADE_NSS_RELEASE, r=nss-reviewers,djackson
    Differential Revision: https://phabricator.services.mozilla.com/D156982
    
    UltraBlame original commit: 82c1e9cb9e0fedac10187342630d2072ca859348

--- security/nss/lib/ssl/sslsnce.c
+++ security/nss/lib/ssl/sslsnce.c
@@ -45,7 +45,7 @@
 
 #include "seccomon.h"
 
-#if defined(XP_UNIX) || defined(XP_WIN32) || defined(XP_OS2) || defined(XP_BEOS)
+#if defined(XP_UNIX) || defined(XP_WIN32) || defined(XP_OS2)
 
 #include "cert.h"
 #include "ssl.h"
@@ -60,7 +60,7 @@
 #include "selfencrypt.h"
 #include <stdio.h>
 
-#if defined(XP_UNIX) || defined(XP_BEOS)
+#if defined(XP_UNIX)
 
 #include <syslog.h>
 #include <fcntl.h>
@@ -237,8 +237,6 @@ static PRBool isMultiProcess = PR_FALSE;
 
 #if defined(AIX) || defined(LINUX) || defined(NETBSD) || defined(OPENBSD)
 #define MAX_SID_CACHE_LOCKS 8 /* two FDs per lock */
-#elif defined(OSF1)
-#define MAX_SID_CACHE_LOCKS 16 /* one FD per lock */
 #else
 #define MAX_SID_CACHE_LOCKS 256
 #endif
@@ -268,7 +266,7 @@ typedef struct inheritanceStr inheritance;
 
 #endif 
 
-#if defined(XP_UNIX) || defined(XP_BEOS)
+#if defined(XP_UNIX)
 
 #define DEFAULT_CACHE_DIRECTORY "/tmp"
 
@@ -1047,7 +1045,7 @@ InitCache(cacheDesc *cache, int maxCacheEntries, int maxCertCacheEntries,
 
     if (shared) {
 
-#if defined(XP_UNIX) || defined(XP_BEOS)
+#if defined(XP_UNIX)
         
 
 
@@ -1231,7 +1229,7 @@ SSL_ShutdownServerSessionIDCacheInstance(cacheDesc *cache)
 SECStatus
 SSL_ShutdownServerSessionIDCache(void)
 {
-#if defined(XP_UNIX) || defined(XP_BEOS)
+#if defined(XP_UNIX)
     
     StopLockPoller(&globalCache);
 #endif
@@ -1295,7 +1293,7 @@ ssl_ConfigMPServerSIDCacheWithOpt(PRUint32 ssl3_timeout,
         result = SECFailure;
     }
 
-#if defined(XP_UNIX) || defined(XP_BEOS)
+#if defined(XP_UNIX)
     
     LaunchLockPoller(cache);
 #endif
@@ -1519,7 +1517,7 @@ SSL_InheritMPServerSIDCache(const char *envString)
     return SSL_InheritMPServerSIDCacheInstance(&globalCache, envString);
 }
 
-#if defined(XP_UNIX) || defined(XP_BEOS)
+#if defined(XP_UNIX)
 
 #define SID_LOCK_EXPIRATION_TIMEOUT 30 /* seconds */
 
