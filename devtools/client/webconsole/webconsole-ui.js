c7e4caa233784d8175257fca0830aab855135277
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 00:18:38 +0000

    Bug 1776144 - [devtools] Remove unused TARGET_AVAILABLE action in WebConsole. r=ochameau.
    Differential Revision: https://phabricator.services.mozilla.com/D150084
    
    UltraBlame original commit: 92862991e711e3e9ac6a57645244b112f460ec4a

--- devtools/client/webconsole/webconsole-ui.js
+++ devtools/client/webconsole/webconsole-ui.js
@@ -544,22 +544,11 @@ class WebConsoleUI {
 
 
   async _onTargetAvailable({ targetFront }) {
-    const dispatchTargetAvailable = () => {
-      const store = this.wrapper && this.wrapper.getStore();
-      if (store) {
-        this.wrapper.getStore().dispatch({
-          type: constants.TARGET_AVAILABLE,
-          targetType: targetFront.targetType,
-        });
-      }
-    };
-
     
     
     if (targetFront.isTopLevel) {
       this.proxy = new WebConsoleConnectionProxy(this, targetFront);
       await this.proxy.connect();
-      dispatchTargetAvailable();
       return;
     }
 
@@ -591,7 +580,6 @@ class WebConsoleUI {
     const proxy = new WebConsoleConnectionProxy(this, targetFront);
     this.additionalProxies.set(targetFront, proxy);
     await proxy.connect();
-    dispatchTargetAvailable();
   }
 
   
