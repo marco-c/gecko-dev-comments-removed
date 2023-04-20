fc526b160d0341914be2f8df98e36ed69e2dde1f
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 07:19:38 +0000

    Backed out changeset 7aeaa20a812d (bug 1800370) for causing failures on /disabled-event-dispatch.tentative.html. CLOSED TREE
    UltraBlame original commit: 80698a53532d599892142683fd06f8fcb5557b9c

--- dom/html/HTMLElement.h
+++ dom/html/HTMLElement.h
@@ -21,9 +21,6 @@ class HTMLElement final : public nsGenericHTMLFormElement {
                                            nsGenericHTMLFormElement)
 
   
-  void GetEventTargetParent(EventChainPreVisitor& aVisitor) override;
-
-  
   nsresult Clone(dom::NodeInfo*, nsINode** aResult) const override;
 
   
@@ -39,7 +36,6 @@ class HTMLElement final : public nsGenericHTMLFormElement {
   
   already_AddRefed<mozilla::dom::ElementInternals> AttachInternals(
       ErrorResult& aRv) override;
-  bool IsDisabledForEvents(WidgetEvent* aEvent) override;
 
   
   bool IsFormAssociatedElement() const override;
