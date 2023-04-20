9daf496988b53b323287b76c2b80e4af7f5324cb
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 22:17:06 +0000

    Backed out changeset bc8c5f1973ff (bug 1825673) for causing mochitest failures in test_GridElementWidthResizer.html CLOSED TREE
    UltraBlame original commit: f55cea6faa5f5e74dfa6cf4ec03b43c065769eaa

--- dom/canvas/HostWebGLContext.h
+++ dom/canvas/HostWebGLContext.h
@@ -776,11 +776,6 @@ class HostWebGLContext final : public SupportsWeakPtr {
   }
 
   
-  void ProvokingVertex(const webgl::ProvokingVertex mode) const {
-    mContext->ProvokingVertex(mode);
-  }
-
-  
   
   
  public:
--- gfx/gl/GLContextSymbols.h
+++ gfx/gl/GLContextSymbols.h
@@ -456,9 +456,6 @@ struct GLContextSymbols final {
                                 realGLboolean, realGLboolean);
   void(GLAPIENTRY* fDisablei)(GLenum, GLuint);
   void(GLAPIENTRY* fEnablei)(GLenum, GLuint);
-
-  
-  void(GLAPIENTRY* fProvokingVertex)(GLenum);
 };
 
 }  
