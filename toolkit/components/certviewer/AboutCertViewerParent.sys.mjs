c777fc439ddbd64ee205af81c60a26f54a490062
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 20:20:32 +0000

    Bug 1778109 - Convert toolkit/components/certviewer/ to ES modules. r=keeler
    Differential Revision: https://phabricator.services.mozilla.com/D172796
    
    UltraBlame original commit: 576a12d4be5ba8f5905dc8de864b6dbae282b79b

--- toolkit/components/certviewer/AboutCertViewerParent.sys.mjs
+++ toolkit/components/certviewer/AboutCertViewerParent.sys.mjs
@@ -1,17 +1,13 @@
-
-
-
-
-"use strict";
-
-var EXPORTED_SYMBOLS = ["AboutCertViewerParent"];
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 const TYPE_CA = 1;
 const TYPE_USER = 2;
 const TYPE_EMAIL = 4;
 const TYPE_SERVER = 8;
 
-class AboutCertViewerParent extends JSWindowActorParent {
+export class AboutCertViewerParent extends JSWindowActorParent {
   getCertificates() {
     let certs = {
       [TYPE_CA]: [],
