fd0b18824449718bb99ddd1c657c3d8e0c7a01f0
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 04:00:14 +0000

    Bug 1803914 - Automatically replace Cu.reportError with console.error (misc). r=mossop
    Differential Revision: https://phabricator.services.mozilla.com/D163772
    
    UltraBlame original commit: 6f6c3f9376ee26f1300da7dee4a5d8b96b28744f

--- security/.eslintrc.js
+++ security/.eslintrc.js
@@ -25,7 +25,7 @@ module.exports = {
     "no-case-declarations": "error",
 
     
-    "no-console": "error",
+    "no-console": ["error", { allow: ["error"] }],
 
     
     "no-constant-condition": ["error", { checkLoops: false }],
