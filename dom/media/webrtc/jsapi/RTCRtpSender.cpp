9ed47e387b340e81a18f20d9389f2230dd93a593
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 12:01:21 +0000

    Bug 1815328: Add mediaType back, since there are still services that use it (somehow). r=jib,webidl,smaug
    Differential Revision: https://phabricator.services.mozilla.com/D169106
    
    UltraBlame original commit: ef77c6270c2b0f20116efeca676970f7f97c4cbd

--- dom/media/webrtc/jsapi/RTCRtpSender.cpp
+++ dom/media/webrtc/jsapi/RTCRtpSender.cpp
@@ -217,6 +217,8 @@ nsTArray<RefPtr<dom::RTCStatsPromise>> RTCRtpSender::GetStatsInternal(
                 aRemote.mType.Construct(RTCStatsType::Remote_inbound_rtp);
                 aRemote.mSsrc = ssrc;
                 aRemote.mKind = kind;
+                aRemote.mMediaType.Construct(
+                    kind);  
                 aRemote.mLocalId.Construct(localId);
                 if (base_seq) {
                   if (aRtcpData.report_block()
@@ -239,6 +241,8 @@ nsTArray<RefPtr<dom::RTCStatsPromise>> RTCRtpSender::GetStatsInternal(
                 aLocal.mId.Construct(localId);
                 aLocal.mType.Construct(RTCStatsType::Outbound_rtp);
                 aLocal.mKind = kind;
+                aLocal.mMediaType.Construct(
+                    kind);  
                 if (remoteId.Length()) {
                   aLocal.mRemoteId.Construct(remoteId);
                 }
