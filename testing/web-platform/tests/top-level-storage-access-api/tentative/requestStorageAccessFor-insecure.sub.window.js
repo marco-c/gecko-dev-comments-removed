181486ff667420d2835e4373329dd753b951fd9a
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 21:53:05 +0000

    Bug 1822398 [wpt PR 38996] - [rsafor] Rename requestStorageAccessForOrigin to requestStorageAccessFor, a=testonly
    Automatic update from web-platform-tests
    [rsafor] Rename requestStorageAccessForOrigin to requestStorageAccessFor
    
    This allows for evolution of the scope of the permission without
    requiring deprecation of the old name.
    
    Change-Id: Iee11f56a41ec116b800515adaf4ee3fe7fc91e26
    Bug: 1424452
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4337982
    Reviewed-by: Dominic Farolino <domchromium.org>
    Reviewed-by: David Benjamin <davidbenchromium.org>
    Reviewed-by: Andrey Kosyakov <caseqchromium.org>
    Commit-Queue: Matt Reichhoff <mreichhoffchromium.org>
    Reviewed-by: Ben Kelly <wanderviewchromium.org>
    Cr-Commit-Position: refs/heads/main{#1118038}
    
    --
    
    wpt-commits: b185124819558842359d60f2715797ceaf997fa6
    wpt-pr: 38996
    
    UltraBlame original commit: 4c689013eb7fa749e288720b591c5a18e5370eae

---
+++ testing/web-platform/tests/top-level-storage-access-api/tentative/requestStorageAccessFor-insecure.sub.window.js
@@ -0,0 +1,109 @@
+
+
+
+'use strict';
+
+
+
+
+
+
+
+
+
+let testPrefix = 'insecure-context';
+
+
+
+let topLevelDocument = true;
+
+
+
+const queryParams = window.location.search.substring(1).split('&');
+queryParams.forEach((param) => {
+  if (param.toLowerCase() == 'rootdocument=false') {
+    topLevelDocument = false;
+  } else if (param.split('=')[0].toLowerCase() == 'testcase') {
+    testPrefix = param.split('=')[1];
+  }
+});
+
+
+
+function RunRequestStorageAccessForInDetachedFrame(site) {
+  const nestedFrame = document.createElement('iframe');
+  document.body.append(nestedFrame);
+  const inner_doc = nestedFrame.contentDocument;
+  nestedFrame.remove();
+  return inner_doc.requestStorageAccessFor(site);
+}
+
+function RunRequestStorageAccessForViaDomParser(site) {
+  const parser = new DOMParser();
+  const doc = parser.parseFromString('<html></html>', 'text/html');
+  return doc.requestStorageAccessFor(site);
+}
+
+
+test(
+    () => {
+      assert_not_equals(document.requestStorageAccessFor, undefined);
+    },
+    '[' + testPrefix +
+        '] document.requestStorageAccessFor() should be supported on the document interface');
+
+if (topLevelDocument) {
+  promise_test(
+      t => {
+        return promise_rejects_dom(t, 'NotAllowedError',
+          document.requestStorageAccessFor('https://test.com'),
+         'document.requestStorageAccessFor() call without user gesture');
+      },
+      '[' + testPrefix +
+          '] document.requestStorageAccessFor() should be rejected by default with no user gesture');
+
+  promise_test(async t => {
+    const description =
+        'document.requestStorageAccessFor() call in a detached frame';
+    
+    return RunRequestStorageAccessForInDetachedFrame('https://foo.com')
+        .then(t.unreached_func('Should have rejected: ' + description))
+        .catch((e) => {
+          assert_equals(e.name, 'InvalidStateError', description);
+        });
+  }, '[non-fully-active] document.requestStorageAccessFor() should not resolve when run in a detached frame');
+
+  promise_test(async t => {
+    const description =
+        'document.requestStorageAccessFor() in a detached DOMParser result';
+    return RunRequestStorageAccessForViaDomParser('https://foo.com')
+        .then(t.unreached_func('Should have rejected: ' + description))
+        .catch((e) => {
+          assert_equals(e.name, 'InvalidStateError', description);
+        });
+  }, '[non-fully-active] document.requestStorageAccessFor() should not resolve when run in a detached DOMParser document');
+
+  
+  
+  
+  RunTestsInIFrame(
+      './resources/requestStorageAccessFor-iframe.html?testCase=frame-on-insecure-page&rootdocument=false');
+
+  promise_test(
+      async t => {
+        await RunCallbackWithGesture(
+          () => promise_rejects_dom(t, 'NotAllowedError', document.requestStorageAccessFor(document.location.origin), 'document.requestStorageAccessFor() call in insecure context'));
+      },
+      '[' + testPrefix +
+          '] document.requestStorageAccessFor() should be rejected when called in an insecure context');
+
+} else {
+  promise_test(
+      async t => {
+        await RunCallbackWithGesture(
+          () => promise_rejects_dom(t, 'NotAllowedError', document.requestStorageAccessFor(document.location.origin),
+            'document.requestStorageAccessFor() call in a non-top-level context'));
+      },
+      '[' + testPrefix +
+          '] document.requestStorageAccessFor() should be rejected when called in an iframe');
+}
