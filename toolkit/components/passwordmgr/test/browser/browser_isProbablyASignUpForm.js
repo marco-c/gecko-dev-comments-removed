cd47f15fc7d15246e23c0c04f2a685d572ad1a57
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 23:57:14 +0000

    Bug 1815449 - Adding telemetry for signup scenario detection using Fathom r=credential-management-reviewers,dimi
    Differential Revision: https://phabricator.services.mozilla.com/D175130
    
    UltraBlame original commit: c180767b71d31e8a13e0da4e077c9f80a4a5ddd4

--- toolkit/components/passwordmgr/test/browser/browser_isProbablyASignUpForm.js
+++ toolkit/components/passwordmgr/test/browser/browser_isProbablyASignUpForm.js
@@ -1,15 +1,42 @@
-
-
-
-
-
-
-
 "use strict";
+
+const SIGNUP_DETECTION_HISTOGRAM = "PWMGR_SIGNUP_FORM_DETECTION_MS";
 const TEST_URL = `https://example.com${DIRECTORY_PATH}form_signup_detection.html`;
 
+
+
+
+
+
+function countEntries(histogramData) {
+  info(typeof histogramData);
+  return histogramData
+    ? Object.values(histogramData.values).reduce((a, b) => a + b, 0)
+    : null;
+}
+
+
+
+
+
+async function countEntriesOfChildHistogram(id, expected) {
+  let histogram;
+  await TestUtils.waitForCondition(() => {
+    let histograms = Services.telemetry.getSnapshotForHistograms("main", false)
+      .content;
+
+    histogram = histograms[id];
+
+    return !!histogram && countEntries(histogram) == expected;
+  }, `The histogram ${id} was expected to have ${expected} entries.`);
+  Assert.equal(countEntries(histogram), expected);
+}
+
+add_setup(async () => {
+  Services.telemetry.getSnapshotForHistograms("main", true);
+});
+
 add_task(async () => {
-  info("Test case: Obvious non signup form is detected as non sign up form");
   await BrowserTestUtils.withNewTab(
     {
       gBrowser,
@@ -25,18 +52,30 @@ add_task(async () => {
         const docState = loginManagerChild.stateForDocument(doc);
         let isSignUpForm;
 
+        info("Test case: Obvious signup form is detected as sign up form");
         const signUpForm = doc.getElementById("obvious-signup-form");
         isSignUpForm = docState.isProbablyASignUpForm(signUpForm);
         Assert.equal(isSignUpForm, true);
 
+        info(
+          "Test case: Obvious non signup form is detected as non sign up form"
+        );
         const loginForm = doc.getElementById("obvious-login-form");
         isSignUpForm = docState.isProbablyASignUpForm(loginForm);
         Assert.equal(isSignUpForm, false);
 
+        info(
+          "Test case: An <input> HTML element is detected as non sign up form"
+        );
         const inputField = doc.getElementById("obvious-signup-username");
         isSignUpForm = docState.isProbablyASignUpForm(inputField);
         Assert.equal(isSignUpForm, false);
       });
     }
   );
+
+  info(
+    "Test case: After running isProbablyASignUpForm against two <form> HTML elements, the histogram PWMGR_SIGNUP_FORM_DETECTION_MS should have two entries."
+  );
+  await countEntriesOfChildHistogram(SIGNUP_DETECTION_HISTOGRAM, 2);
 });
