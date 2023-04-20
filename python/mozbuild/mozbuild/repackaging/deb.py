8fc37ed4cd3ce45f40eeb4957913a46f1264fe5c
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 17:22:02 +0000

    Bug 1818275 - deb: Use mozilla-version to tell nightlies apart r=gabriel
    Differential Revision: https://phabricator.services.mozilla.com/D170747
    
    UltraBlame original commit: 446a00ba4e0100c92665e01c21af340ef8cc7592

--- python/mozbuild/mozbuild/repackaging/deb.py
+++ python/mozbuild/mozbuild/repackaging/deb.py
@@ -14,6 +14,7 @@ from string import Template
 
 import mozfile
 import mozpack.path as mozpath
+from mozilla_version.gecko import GeckoVersion
 
 from mozbuild.repackaging.application_ini import get_application_ini_values
 
@@ -107,16 +108,16 @@ def _extract_application_ini_data(application_director):
 def _get_build_variables(
     application_ini_data,
     arch,
-    version,
+    version_string,
     build_number,
 ):
+    version = GeckoVersion.parse(version_string)
     
-    if "a" in version:
-        
-        deb_pkg_version = f"{version}~{application_ini_data['build_id']}"
-    else:
-        
-        deb_pkg_version = f"{version}~build{build_number}"
+    deb_pkg_version = (
+        f"{version}~{application_ini_data['build_id']}"
+        if version.is_nightly
+        else f"{version}~build{build_number}"
+    )
 
     return {
         "DEB_DESCRIPTION": f"{application_ini_data['vendor']} {application_ini_data['display_name']}",
