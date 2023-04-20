d5ea244483b1e52aec78c8f518c9c5a217ba8fcc
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 09:36:07 +0000

    Bug 1806510 - Vendor libwebrtc from 97bdfa32d4
    Upstream commit: https://webrtc.googlesource.com/src/+/97bdfa32d4696b8c0fd924a8bc2239d631f18e01
        Remove dependency on rtc::MessageHandler in session description factory
    
        Bug: webrtc:9702
        Change-Id: Iedbcc1f8d223c4df3e0e8c5811d5a4b78dfe8d3d
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/272620
        Reviewed-by: Tomas Gunnarsson <tommiwebrtc.org>
        Commit-Queue: Danil Chapovalov <danilchapwebrtc.org>
        Cr-Commit-Position: refs/heads/main{#37891}
    
    UltraBlame original commit: 8013d882eafc21c7e3724b8bc5b777d3a29a117b

--- third_party/libwebrtc/pc/webrtc_session_description_factory.h
+++ third_party/libwebrtc/pc/webrtc_session_description_factory.h
@@ -18,18 +18,17 @@
 #include <queue>
 #include <string>
 
+#include "absl/functional/any_invocable.h"
 #include "api/jsep.h"
 #include "api/peer_connection_interface.h"
 #include "api/scoped_refptr.h"
+#include "api/task_queue/task_queue_base.h"
 #include "p2p/base/transport_description.h"
 #include "p2p/base/transport_description_factory.h"
 #include "pc/media_session.h"
 #include "pc/sdp_state_provider.h"
-#include "rtc_base/message_handler.h"
 #include "rtc_base/rtc_certificate.h"
 #include "rtc_base/rtc_certificate_generator.h"
-#include "rtc_base/thread.h"
-#include "rtc_base/thread_message.h"
 #include "rtc_base/unique_id_generator.h"
 #include "rtc_base/weak_ptr.h"
 
@@ -39,7 +38,7 @@ namespace webrtc {
 
 
 
-class WebRtcSessionDescriptionFactory : public rtc::MessageHandler {
+class WebRtcSessionDescriptionFactory {
  public:
   
   
@@ -55,7 +54,7 @@ class WebRtcSessionDescriptionFactory : public rtc::MessageHandler {
       std::function<void(const rtc::scoped_refptr<rtc::RTCCertificate>&)>
           on_certificate_ready,
       const FieldTrialsView& field_trials);
-  virtual ~WebRtcSessionDescriptionFactory();
+  ~WebRtcSessionDescriptionFactory();
 
   WebRtcSessionDescriptionFactory(const WebRtcSessionDescriptionFactory&) =
       delete;
@@ -114,9 +113,6 @@ class WebRtcSessionDescriptionFactory : public rtc::MessageHandler {
     cricket::MediaSessionOptions options;
   };
 
-  
-  virtual void OnMessage(rtc::Message* msg);
-
   void InternalCreateOffer(CreateSessionDescriptionRequest request);
   void InternalCreateAnswer(CreateSessionDescriptionRequest request);
   
@@ -127,13 +123,16 @@ class WebRtcSessionDescriptionFactory : public rtc::MessageHandler {
   void PostCreateSessionDescriptionSucceeded(
       CreateSessionDescriptionObserver* observer,
       std::unique_ptr<SessionDescriptionInterface> description);
+  
+  
+  void Post(absl::AnyInvocable<void() &&> callback);
 
   void OnCertificateRequestFailed();
   void SetCertificate(rtc::scoped_refptr<rtc::RTCCertificate> certificate);
 
   std::queue<CreateSessionDescriptionRequest>
       create_session_description_requests_;
-  rtc::Thread* const signaling_thread_;
+  TaskQueueBase* const signaling_thread_;
   cricket::TransportDescriptionFactory transport_desc_factory_;
   cricket::MediaSessionDescriptionFactory session_desc_factory_;
   uint64_t session_version_;
@@ -141,6 +140,7 @@ class WebRtcSessionDescriptionFactory : public rtc::MessageHandler {
   const SdpStateProvider* sdp_info_;
   const std::string session_id_;
   CertificateRequestState certificate_request_state_;
+  std::queue<absl::AnyInvocable<void() &&>> callbacks_;
 
   std::function<void(const rtc::scoped_refptr<rtc::RTCCertificate>&)>
       on_certificate_ready_;
