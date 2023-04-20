1bfd24b5d58969bdf528c173fb80bc163b626b16
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 21:31:45 +0000

    Bug 1823989 - Fix android jsshell builds. r=jandem,firefox-build-system-reviewers,nalexander
    Stop trying to use system zlib when building standalone SpiderMonkey for Android
    targets and instead rely on the mozglue version. Additionally, use 'get_adb_path'
    used by other geckoview tests to locate `adb` in a consistent way.
    
    Also do some house-keeping in the documentation and log messages.
    
    Differential Revision: https://phabricator.services.mozilla.com/D173466
    
    UltraBlame original commit: 80053e9f68a5ced0792dfa9c4c1621af903017e8

--- js/src/tests/lib/remote.py
+++ js/src/tests/lib/remote.py
@@ -64,21 +64,38 @@ def init_device(options):
             
             options.local_lib = posixpath.dirname(options.js_shell)
 
-        DEVICE = ADBDeviceFactory(
-            device=options.device_serial, test_root=options.remote_test_root
-        )
+        
+        
+        
+        try:
+            from mozbuild.base import MozbuildObject
+            from mozrunner.devices.android_device import get_adb_path
 
-        init_remote_dir(DEVICE, options.remote_test_root)
+            context = MozbuildObject.from_environment()
+            adb_path = get_adb_path(context)
+        except (ImportError):
+            adb_path = "adb"
+
+        DEVICE = ADBDeviceFactory(
+            adb=adb_path,
+            device=options.device_serial,
+            test_root=options.remote_test_root,
+        )
 
         bin_dir = posixpath.join(options.remote_test_root, "bin")
         tests_dir = posixpath.join(options.remote_test_root, "tests")
         temp_dir = posixpath.join(options.remote_test_root, "tmp")
+
         
+        init_remote_dir(DEVICE, options.remote_test_root)
         init_remote_dir(DEVICE, tests_dir)
         init_remote_dir(DEVICE, bin_dir)
         init_remote_dir(DEVICE, temp_dir)
+
+        
         push_libs(options, DEVICE, bin_dir)
         push_progs(options, DEVICE, [options.js_shell], bin_dir)
+
         
         options.js_shell = os.path.join(bin_dir, "js")
 
