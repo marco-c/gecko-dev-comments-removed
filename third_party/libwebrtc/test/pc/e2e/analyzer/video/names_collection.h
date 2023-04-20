359390ecbca3de42f173d06e772bb80aea1aa5f1
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 00:47:35 +0000

    Bug 1790097 - Vendor libwebrtc from 42d09cb1ba
    Upstream commit: https://webrtc.googlesource.com/src/+/42d09cb1ba54f7d5108c055f2323abeb2d9e2ce1
        [DVQA] Extract FrameInFlight into separate file
    
        Also add ability to remove expected receivers for the frame.
    
        Bug: b/231397778
        Change-Id: Id1fb2df05a69e0dca4f05eaa995521b6be2ac52a
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/265396
        Commit-Queue: Artem Titov <titovartemwebrtc.org>
        Reviewed-by: Mirko Bonadei <mbonadeiwebrtc.org>
        Cr-Commit-Position: refs/heads/main{#37191}
    
    UltraBlame original commit: a4c940f80af95412e99d96a92dd2c48f5ad766ac

--- third_party/libwebrtc/test/pc/e2e/analyzer/video/names_collection.h
+++ third_party/libwebrtc/test/pc/e2e/analyzer/video/names_collection.h
@@ -12,6 +12,7 @@
 #define TEST_PC_E2E_ANALYZER_VIDEO_NAMES_COLLECTION_H_
 
 #include <map>
+#include <set>
 #include <string>
 #include <vector>
 
@@ -69,6 +70,10 @@ class NamesCollection {
   
   
   absl::optional<size_t> RemoveIfPresent(absl::string_view name);
+
+  
+  
+  std::set<size_t> GetPresentIndexes() const;
 
  private:
   std::vector<std::string> names_;
