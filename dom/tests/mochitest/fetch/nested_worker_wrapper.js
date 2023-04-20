7a83071581c6b5623ebd7b6538b8b2b270a921ba
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 17:09:19 +0000

    Bug 1809518 - Update more fetch tests that use `fetch_test_framework.js` based on the status of ORB r=farre
    These tests involve a bunch of no-cors fetch requests which use
    `file_CrossSiteXHR_server.sjs` for sending the response back.
    Depends on the request's URL, the response can possibly be
    
    ```
      response.setHeader("Content-Type", "application/xml", false);
      response.write("<res>hello pass</res>\n");
    ```
    
    which will make JS validator to run and blocks.
    
    The fix here is to toggle the pref of ORB when running the tests,
    so we test both with ORB and without ORB behaviors, with different
    expectations.
    
    Differential Revision: https://phabricator.services.mozilla.com/D169277
    
    UltraBlame original commit: 2299a5bdc7a9a74c2cbb4b31a782777964ba8326

--- dom/tests/mochitest/fetch/nested_worker_wrapper.js
+++ dom/tests/mochitest/fetch/nested_worker_wrapper.js
@@ -1,5 +1,10 @@
 function getScriptUrl() {
-  return new URL(location.href).searchParams.get("script");
+  const params = new URL(location.href).searchParams;
+  let scriptUrl = params.get("script");
+  if (params.has("orbEnabled")) {
+    scriptUrl += "&orbEnabled";
+  }
+  return scriptUrl;
 }
 
 
