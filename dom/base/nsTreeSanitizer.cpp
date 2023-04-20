b3f11de24475528869d04729fc956c1027520f7e
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 08:31:06 +0000

    Bug 1784040 - Support experimental ElementList and AttributeList with namespace. r=emilio
    Differential Revision: https://phabricator.services.mozilla.com/D159147
    
    UltraBlame original commit: 52b822cb8a4c375fcfb6134220a40c0e3fc37fcf

--- dom/base/nsTreeSanitizer.cpp
+++ dom/base/nsTreeSanitizer.cpp
@@ -33,6 +33,7 @@
 #include "nsAttrName.h"
 #include "nsIScriptError.h"
 #include "nsIScriptSecurityManager.h"
+#include "nsNameSpaceManager.h"
 #include "nsNetUtil.h"
 #include "nsComponentManagerUtils.h"
 #include "nsContentUtils.h"
@@ -1972,14 +1973,13 @@ bool nsTreeSanitizer::MatchesElementName(ElementNameSet& aNames,
 
 
 bool nsTreeSanitizer::MatchesAttributeMatchList(
-    ElementToAttributeSetTable& aMatchList, Element& aElement,
+    AttributesToElementsMap& aMatchList, Element& aElement,
     int32_t aAttrNamespace, nsAtom* aAttrLocalName) {
   
   
   ElementNameSet* names;
-  if (auto lookup = aMatchList.Lookup(aAttrLocalName)) {
-    names = lookup->get();
-  } else if (auto lookup = aMatchList.Lookup(nsGkAtoms::_asterisk)) {
+  if (auto lookup =
+          aMatchList.Lookup(AttributeName(aAttrNamespace, aAttrLocalName))) {
     names = lookup->get();
   } else {
     return false;
@@ -1990,19 +1990,20 @@ bool nsTreeSanitizer::MatchesAttributeMatchList(
   
   
   
+
+  
   int32_t namespaceID = aElement.NodeInfo()->NamespaceID();
   RefPtr<nsAtom> nameAtom = aElement.NodeInfo()->NameAtom();
-  ElementName elemName(namespaceID, nameAtom);
 
   
   
-  if (!names->Contains(elemName) &&
-      !names->Contains(ElementName(kNameSpaceID_XHTML, nsGkAtoms::_asterisk))) {
-    return false;
+  
+
+  
+  if (!names) {
+    return true;
   }
-
-  
-  return true;
+  return MatchesElementName(*names, namespaceID, nameAtom);
 }
 
 
@@ -2025,19 +2026,19 @@ bool nsTreeSanitizer::MustDropAttribute(Element* aElement,
 
   
   
-  if (mDroppedAttributes &&
-      MatchesAttributeMatchList(*mDroppedAttributes, *aElement, aAttrNamespace,
+  if (mDropAttributes &&
+      MatchesAttributeMatchList(*mDropAttributes, *aElement, aAttrNamespace,
                                 aAttrLocalName)) {
     return true;
   }
 
   
-  if (mAllowedAttributes) {
+  if (mAllowAttributes) {
     
     
     
-    if (!MatchesAttributeMatchList(*mAllowedAttributes, *aElement,
-                                   aAttrNamespace, aAttrLocalName)) {
+    if (!MatchesAttributeMatchList(*mAllowAttributes, *aElement, aAttrNamespace,
+                                   aAttrLocalName)) {
       return true;
     }
   } else {
@@ -2458,48 +2459,88 @@ void nsTreeSanitizer::ReleaseStatics() {
   NS_IF_RELEASE(sNullPrincipal);
 }
 
-UniquePtr<nsTreeSanitizer::ElementNameSet> nsTreeSanitizer::ConvertElementNames(
-    const Sequence<nsString>& aNames) {
-  auto names = MakeUnique<ElementNameSet>(aNames.Length());
+static int32_t ConvertNamespaceString(const nsAString& aNamespace,
+                                      bool aForAttribute,
+                                      mozilla::ErrorResult& aRv) {
+  int32_t namespaceID = nsNameSpaceManager::GetInstance()->GetNameSpaceID(
+      aNamespace,  false);
+  if (namespaceID == kNameSpaceID_XHTML || namespaceID == kNameSpaceID_MathML ||
+      namespaceID == kNameSpaceID_SVG) {
+    return namespaceID;
+  }
+  if (aForAttribute && (namespaceID == kNameSpaceID_XMLNS ||
+                        namespaceID == kNameSpaceID_XLink)) {
+    return namespaceID;
+  }
 
-  
-  for (const nsString& name : aNames) {
-    
-    
-    int32_t index = name.FindChar(':');
+  aRv.ThrowTypeError("Invalid namespace: \""_ns +
+                     NS_ConvertUTF16toUTF8(aNamespace) + "\"."_ns);
+  return kNameSpaceID_Unknown;
+}
 
-    
-    if (index == kNotFound) {
-      RefPtr<nsAtom> nameAtom = NS_AtomizeMainThread(name);
+UniquePtr<nsTreeSanitizer::ElementNameSet> nsTreeSanitizer::ConvertElements(
+    const nsTArray<OwningStringOrSanitizerElementNamespace>& aElements,
+    mozilla::ErrorResult& aRv) {
+  auto set = MakeUnique<ElementNameSet>(aElements.Length());
+  for (const auto& entry : aElements) {
+    if (entry.IsString()) {
+      RefPtr<nsAtom> nameAtom = NS_AtomizeMainThread(entry.GetAsString());
+      
       ElementName elemName(kNameSpaceID_XHTML, std::move(nameAtom));
-      names->Insert(elemName);
-      continue;
-    }
+      set->Insert(elemName);
+    } else {
+      const auto& elemNamespace = entry.GetAsSanitizerElementNamespace();
 
+      int32_t namespaceID =
+          ConvertNamespaceString(elemNamespace.mNamespace, false, aRv);
+      if (aRv.Failed()) {
+        return {};
+      }
+
+      RefPtr<nsAtom> nameAtom = NS_AtomizeMainThread(elemNamespace.mName);
+      ElementName elemName(namespaceID, std::move(nameAtom));
+      set->Insert(elemName);
+    }
+  }
+
+  return set;
+}
+
+UniquePtr<nsTreeSanitizer::ElementNameSet> nsTreeSanitizer::ConvertElements(
+    const OwningStarOrStringOrSanitizerElementNamespaceSequence& aElements,
+    mozilla::ErrorResult& aRv) {
+  if (aElements.IsStar()) {
+    return nullptr;
+  }
+  return ConvertElements(
+      aElements.GetAsStringOrSanitizerElementNamespaceSequence(), aRv);
+}
+
+UniquePtr<nsTreeSanitizer::AttributesToElementsMap>
+nsTreeSanitizer::ConvertAttributes(
+    const nsTArray<SanitizerAttribute>& aAttributes, ErrorResult& aRv) {
+  auto map = MakeUnique<AttributesToElementsMap>();
+
+  for (const auto& entry : aAttributes) {
     
-    
-    if (name.FindChar(':', index + 1) == kNotFound) {
-      auto prefix = Substring(name, 0, index);
-      
-      
-      
-      
-      
-      RefPtr<nsAtom> nameAtom =
-          NS_AtomizeMainThread(Substring(name, index + 1));
-      if (prefix.EqualsLiteral("svg")) {
-        ElementName elemName(kNameSpaceID_SVG, std::move(nameAtom));
-        names->Insert(elemName);
-      } else if (prefix.EqualsLiteral("math")) {
-        ElementName elemName(kNameSpaceID_MathML, std::move(nameAtom));
-        names->Insert(elemName);
+    int32_t namespaceID = kNameSpaceID_None;
+    if (!entry.mNamespace.IsVoid()) {
+      namespaceID = ConvertNamespaceString(entry.mNamespace, true, aRv);
+      if (aRv.Failed()) {
+        return {};
       }
     }
+    RefPtr<nsAtom> attrAtom = NS_AtomizeMainThread(entry.mName);
+    AttributeName attrName(namespaceID, std::move(attrAtom));
 
-    
-    
+    UniquePtr<ElementNameSet> elements = ConvertElements(entry.mElements, aRv);
+    if (aRv.Failed()) {
+      return {};
+    }
+    map->InsertOrUpdate(attrName, std::move(elements));
   }
-  return names;
+
+  return map;
 }
 
 void nsTreeSanitizer::WithWebSanitizerOptions(
@@ -2525,36 +2566,35 @@ void nsTreeSanitizer::WithWebSanitizerOptions(
   }
 
   if (aOptions.mAllowElements.WasPassed()) {
-    mAllowElements = ConvertElementNames(aOptions.mAllowElements.Value());
+    mAllowElements = ConvertElements(aOptions.mAllowElements.Value(), aRv);
+    if (aRv.Failed()) {
+      return;
+    }
   }
 
   if (aOptions.mBlockElements.WasPassed()) {
-    mBlockElements = ConvertElementNames(aOptions.mBlockElements.Value());
+    mBlockElements = ConvertElements(aOptions.mBlockElements.Value(), aRv);
+    if (aRv.Failed()) {
+      return;
+    }
   }
 
   if (aOptions.mDropElements.WasPassed()) {
-    mDropElements = ConvertElementNames(aOptions.mDropElements.Value());
+    mDropElements = ConvertElements(aOptions.mDropElements.Value(), aRv);
+    if (aRv.Failed()) {
+      return;
+    }
   }
 
   if (aOptions.mAllowAttributes.WasPassed()) {
-    const Record<nsString, Sequence<nsString>>& allowedAttributes =
-        aOptions.mAllowAttributes.Value();
-    mAllowedAttributes = MakeUnique<ElementToAttributeSetTable>();
-    for (const auto& entry : allowedAttributes.Entries()) {
-      RefPtr<nsAtom> attrAtom = NS_AtomizeMainThread(entry.mKey);
-      UniquePtr<ElementNameSet> elements = ConvertElementNames(entry.mValue);
-      mAllowedAttributes->InsertOrUpdate(attrAtom, std::move(elements));
+    mAllowAttributes =
+        ConvertAttributes(aOptions.mAllowAttributes.Value(), aRv);
+    if (aRv.Failed()) {
+      return;
     }
   }
 
   if (aOptions.mDropAttributes.WasPassed()) {
-    const Record<nsString, Sequence<nsString>>& droppedAttributes =
-        aOptions.mDropAttributes.Value();
-    mDroppedAttributes = MakeUnique<ElementToAttributeSetTable>();
-    for (const auto& entry : droppedAttributes.Entries()) {
-      RefPtr<nsAtom> attrAtom = NS_AtomizeMainThread(entry.mKey);
-      UniquePtr<ElementNameSet> elements = ConvertElementNames(entry.mValue);
-      mDroppedAttributes->InsertOrUpdate(attrAtom, std::move(elements));
-    }
+    mDropAttributes = ConvertAttributes(aOptions.mDropAttributes.Value(), aRv);
   }
 }
