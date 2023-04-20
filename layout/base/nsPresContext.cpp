997586acbb15eafa583ed22ef385dd4ce94130a5
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 10:31:37 +0000

    Bug 1811863 - Use a more precise RFP check in nsPresContext::UpdateFontVisibility. r=jfkthame
    Depends on D167822
    
    Differential Revision: https://phabricator.services.mozilla.com/D167823
    
    UltraBlame original commit: 55e21caf8a0f6ee5de83362e5918a27bf44b9853

--- layout/base/nsPresContext.cpp
+++ layout/base/nsPresContext.cpp
@@ -754,7 +754,7 @@ bool nsPresContext::UpdateFontVisibility() {
   
   
   int32_t level;
-  if (StaticPrefs::privacy_resistFingerprinting()) {
+  if (mDocument->ShouldResistFingerprinting()) {
     level = StaticPrefs::layout_css_font_visibility_resistFingerprinting();
   } else if (StaticPrefs::privacy_trackingprotection_enabled() ||
              (isPrivate &&
