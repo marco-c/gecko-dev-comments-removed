8526da830b6abbfb1863726f02f95d39525ebfd9
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 16:04:02 +0000

    Bug 1812154 - Vendor libwebrtc from b15faaa264
    Upstream commit: https://webrtc.googlesource.com/src/+/b15faaa2640d137d6341b09fd3670813e3aaf99e
        [PCLF] Annotate video metrics with peer, stream and receiver
    
        Bug: b/240540204
        Change-Id: I05eddea33a2eb680b59c8247f2acba1e7c7d6a84
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/278260
        Reviewed-by: Andrey Logvin <landreygoogle.com>
        Commit-Queue: Artem Titov <titovartemwebrtc.org>
        Cr-Commit-Position: refs/heads/main{#38316}
    
    UltraBlame original commit: c319fd843e5c6ddb7dc55f51e72f23e1d558401c

--- third_party/libwebrtc/test/pc/e2e/analyzer/video/default_video_quality_analyzer.h
+++ third_party/libwebrtc/test/pc/e2e/analyzer/video/default_video_quality_analyzer.h
@@ -124,7 +124,7 @@ class DefaultVideoQualityAnalyzer : public VideoQualityAnalyzerInterface {
 
   
   void ReportResults();
-  void ReportResults(const std::string& test_case_name,
+  void ReportResults(const InternalStatsKey& key,
                      const StreamStats& stats,
                      const FrameCounters& frame_counters)
       RTC_EXCLUSIVE_LOCKS_REQUIRED(mutex_);
