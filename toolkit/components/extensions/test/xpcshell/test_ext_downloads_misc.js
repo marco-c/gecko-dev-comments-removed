f7a93b0cf10a6908b14194eddbc7c1dd9db06a66
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 17:31:49 +0000

    Bug 1772932 - Port osfile.jsm usage to IOUtils in toolkit/components/extensions r=mixedpuppy
    Differential Revision: https://phabricator.services.mozilla.com/D159955
    
    UltraBlame original commit: 4e2a9894fb4c44e1b32f676705aa67e4617c8fdb

--- toolkit/components/extensions/test/xpcshell/test_ext_downloads_misc.js
+++ toolkit/components/extensions/test/xpcshell/test_ext_downloads_misc.js
@@ -6,8 +6,6 @@ const { Downloads } = ChromeUtils.importESModule(
   "resource://gre/modules/Downloads.sys.mjs"
 );
 
-const { OS } = ChromeUtils.import("resource://gre/modules/osfile.jsm");
-
 const { TestUtils } = ChromeUtils.importESModule(
   "resource://testing-common/TestUtils.sys.mjs"
 );
@@ -232,19 +230,16 @@ let downloadDir;
 let extension;
 
 async function waitForCreatedPartFile(baseFilename = "interruptible.html") {
-  const partFilePath = `${downloadDir.path}/${baseFilename}.part`;
+  const partFilePath = PathUtils.join(downloadDir.path, `${baseFilename}.part`);
 
   info(`Wait for ${partFilePath} to be created`);
   let lastError;
   await TestUtils.waitForCondition(
-    async () =>
-      OS.File.stat(partFilePath).then(
-        () => true,
-        err => {
-          lastError = err;
-          return false;
-        }
-      ),
+    () =>
+      IOUtils.exists(partFilePath).catch(err => {
+        lastError = err;
+        return false;
+      }),
     `Wait for the ${partFilePath} to exists before pausing the download`
   ).catch(err => {
     if (lastError) {
@@ -859,17 +854,8 @@ add_task(async function test_file_removeFile_permission_failure() {
   
   equal(msg.errmsg, "An unexpected error occurred", "Error message redacted");
 
-  
-  
-  
-  const expectedErrorMessagePattern =
-    AppConstants.platform === "win"
-      ? 
-        /Win error 32 during operation remove on file .*subdir.*downloaded_filename/
-      : 
-        /Unix error 13 during operation remove on file .*subdir.*downloaded_filename/;
   AddonTestUtils.checkMessages(consoleOutput.messages, {
-    expected: [{ message: expectedErrorMessagePattern }],
+    expected: [{ message: /NotAllowedError/ }],
   });
 
   ok(await IOUtils.exists(expectedPath), "File exists before removeFile()");
