86af210f2a0c1c40e938f0cfdd36a89d4cb787d7
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 05:35:36 +0000

    Bug 1800920 - Vendor libwebrtc from 7f0a7acb0a
    Upstream commit: https://webrtc.googlesource.com/src/+/7f0a7acb0a5d24d2d1471562a5b524645c8212b9
        Improve PacketArrivalTimeMap perfomance
    
        replace std::deque implementation with a manually controlled circular buffer.
        replace Timestamp validity check from 'IsInfinite()' accesser to cheaper comparison to zero.
        These greatly increase PacketArrivalTimeMap::AddPacket perfomance when packet arrive with large sequence number gaps.
    
        Bug: chromium:1349880
        Change-Id: I6f4e814b1086ca9d0b48608531e3a387d9e542dc
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/270564
        Reviewed-by: Victor Boivie <boiviewebrtc.org>
        Reviewed-by: Philip Eliasson <philipelwebrtc.org>
        Reviewed-by: Erik Språng <sprangwebrtc.org>
        Commit-Queue: Danil Chapovalov <danilchapwebrtc.org>
        Cr-Commit-Position: refs/heads/main{#37722}
    
    UltraBlame original commit: c63bd67b9fccbd9a7907f03d2f7b87deb2aa8ff9

--- third_party/libwebrtc/modules/remote_bitrate_estimator/packet_arrival_map.h
+++ third_party/libwebrtc/modules/remote_bitrate_estimator/packet_arrival_map.h
@@ -10,9 +10,10 @@
 #ifndef MODULES_REMOTE_BITRATE_ESTIMATOR_PACKET_ARRIVAL_MAP_H_
 #define MODULES_REMOTE_BITRATE_ESTIMATOR_PACKET_ARRIVAL_MAP_H_
 
+#include <algorithm>
 #include <cstddef>
 #include <cstdint>
-#include <deque>
+#include <memory>
 
 #include "api/units/timestamp.h"
 #include "rtc_base/checks.h"
@@ -32,10 +33,19 @@ class PacketArrivalTimeMap {
  public:
   
   
-  static constexpr size_t kMaxNumberOfPackets = (1 << 15);
+  static constexpr int kMaxNumberOfPackets = (1 << 15);
+
+  PacketArrivalTimeMap() = default;
+  PacketArrivalTimeMap(const PacketArrivalTimeMap&) = delete;
+  PacketArrivalTimeMap& operator=(const PacketArrivalTimeMap&) = delete;
+  ~PacketArrivalTimeMap() = default;
 
   
-  bool has_received(int64_t sequence_number) const;
+  bool has_received(int64_t sequence_number) const {
+    return sequence_number >= begin_sequence_number() &&
+           sequence_number < end_sequence_number() &&
+           arrival_times_[Index(sequence_number)] >= Timestamp::Zero();
+  }
 
   
   
@@ -43,21 +53,22 @@ class PacketArrivalTimeMap {
 
   
   
-  int64_t end_sequence_number() const {
-    return begin_sequence_number_ + arrival_times_.size();
-  }
+  int64_t end_sequence_number() const { return end_sequence_number_; }
 
   
   
   Timestamp get(int64_t sequence_number) {
-    int64_t pos = sequence_number - begin_sequence_number_;
-    RTC_DCHECK(pos >= 0 && pos < static_cast<int64_t>(arrival_times_.size()));
-    return arrival_times_[pos];
+    RTC_DCHECK_GE(sequence_number, begin_sequence_number());
+    RTC_DCHECK_LT(sequence_number, end_sequence_number());
+    return arrival_times_[Index(sequence_number)];
   }
 
   
   
-  int64_t clamp(int64_t sequence_number) const;
+  int64_t clamp(int64_t sequence_number) const {
+    return std::clamp(sequence_number, begin_sequence_number(),
+                      end_sequence_number());
+  }
 
   
   void EraseTo(int64_t sequence_number);
@@ -71,17 +82,44 @@ class PacketArrivalTimeMap {
   void RemoveOldPackets(int64_t sequence_number, Timestamp arrival_time_limit);
 
  private:
-  
-  
-  std::deque<Timestamp> arrival_times_;
+  static constexpr int kMinCapacity = 128;
 
+  
+  int Index(int64_t sequence_number) const {
+    
+    
+    
+    return sequence_number & capacity_minus_1_;
+  }
+
+  void SetNotReceived(int64_t begin_sequence_number_inclusive,
+                      int64_t end_sequence_number_exclusive);
+
+  void TrimLeadingNotReceivedEntries();
+
+  
+  
+  void AdjustToSize(int new_size);
+  void Reallocate(int new_capacity);
+
+  int capacity() const { return capacity_minus_1_ + 1; }
+  bool has_seen_packet() const { return arrival_times_ != nullptr; }
+
+  
+  
+  std::unique_ptr<Timestamp[]> arrival_times_ = nullptr;
+
+  
+  
+  
+  
+  int capacity_minus_1_ = -1;
+
+  
   
   
   int64_t begin_sequence_number_ = 0;
-
-  
-  
-  bool has_seen_packet_ = false;
+  int64_t end_sequence_number_ = 0;
 };
 
 }  
