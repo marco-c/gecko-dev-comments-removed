361c0366a93145cf4134efbba7f6495c3e4471d9
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 23:52:42 +0000

    Bug 1824473 - Update consumers of toolkit/components/formautofill to import ES modules directly. r=Standard8,geckoview-reviewers,credential-management-reviewers,sgalich,m_kato
    Differential Revision: https://phabricator.services.mozilla.com/D173587
    
    UltraBlame original commit: e28e812ef06df354365a9cc6675db98155c2c94c

--- browser/extensions/formautofill/test/unit/test_getAdaptedProfiles_locales.js
+++ browser/extensions/formautofill/test/unit/test_getAdaptedProfiles_locales.js
@@ -10,8 +10,8 @@
 
 let FormAutofillHandler;
 add_task(async function() {
-  ({ FormAutofillHandler } = ChromeUtils.import(
-    "resource://autofill/FormAutofillHandler.jsm"
+  ({ FormAutofillHandler } = ChromeUtils.importESModule(
+    "resource://autofill/FormAutofillHandler.sys.mjs"
   ));
 });
 
