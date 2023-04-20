74dc70fa2afa3df288948a42a527e247dcaf7dd0
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 07:08:51 +0000

    Bug 1805523 - Implement temporary access state and attention, r=rpl
    Differential Revision: https://phabricator.services.mozilla.com/D165490
    
    UltraBlame original commit: 1c135e14142e63b8668bd8fa21ea1f73a8a1f94b

--- browser/base/content/browser-addons.js
+++ browser/base/content/browser-addons.js
@@ -1430,10 +1430,15 @@ var gUnifiedExtensions = {
   },
 
   onPanelViewHiding(panelview) {
+    if (window.closed) {
+      return;
+    }
     const list = panelview.querySelector(".unified-extensions-list");
     while (list.lastChild) {
       list.lastChild.remove();
     }
+    
+    requestAnimationFrame(() => this.updateAttention());
   },
 
   _panel: null,
