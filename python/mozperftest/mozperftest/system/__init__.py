7043308138847da73b191585f93430ae901a46fb
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 05:44:17 +0000

    Bug 1779822 - Create new mozperftest later for Fenix and nightly startup bechmarks. r=perftest-reviewers,sparky
    Differential Revision: https://phabricator.services.mozilla.com/D154126
    
    UltraBlame original commit: 311eb6a3a3edab8bede7bc1dc2cfffde17d2be91

--- python/mozperftest/mozperftest/system/__init__.py
+++ python/mozperftest/mozperftest/system/__init__.py
@@ -2,15 +2,16 @@
 
 
 from mozperftest.layers import Layers
-from mozperftest.system.proxy import ProxyRunner
 from mozperftest.system.android import AndroidDevice
-from mozperftest.system.profile import Profile
+from mozperftest.system.android_startup import AndroidStartUp
 from mozperftest.system.macos import MacosDevice
 from mozperftest.system.pingserver import PingServer
+from mozperftest.system.profile import Profile
+from mozperftest.system.proxy import ProxyRunner
 
 
 def get_layers():
-    return PingServer, Profile, ProxyRunner, AndroidDevice, MacosDevice
+    return PingServer, Profile, ProxyRunner, AndroidDevice, MacosDevice, AndroidStartUp
 
 
 def pick_system(env, flavor, mach_cmd):
@@ -26,7 +27,9 @@ def pick_system(env, flavor, mach_cmd):
             ),
         )
     if flavor == "mobile-browser":
-        return Layers(env, mach_cmd, (Profile, ProxyRunner, AndroidDevice))
+        return Layers(
+            env, mach_cmd, (Profile, ProxyRunner, AndroidDevice, AndroidStartUp)
+        )
     if flavor == "webpagetest":
         return Layers(env, mach_cmd, (Profile,))
     raise NotImplementedError(flavor)
