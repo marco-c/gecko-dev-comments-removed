ad7745c6ff849ea923431e2550adf4df37a3c779
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 23:56:06 +0000

    Bug 1826452 - Use non-rounded external scroll offsets behind a pref. r=botond,gw
    Differential Revision: https://phabricator.services.mozilla.com/D175295
    
    UltraBlame original commit: 95470214c6cb0939f7fcbb847f6dbae772a0c9cc

--- gfx/layers/wr/ClipManager.cpp
+++ gfx/layers/wr/ClipManager.cpp
@@ -338,7 +338,7 @@ Maybe<wr::WrSpatialId> ClipManager::DefineScrollLayers(
   nsIFrame* scrollFrame = do_QueryFrame(scrollableFrame);
   nsPoint offset = scrollFrame->GetOffsetToCrossDoc(aItem->Frame()) +
                    aItem->ToReferenceFrame();
-  float auPerDevPixel = aItem->Frame()->PresContext()->AppUnitsPerDevPixel();
+  int32_t auPerDevPixel = aItem->Frame()->PresContext()->AppUnitsPerDevPixel();
   nsRect scrollPort = scrollableFrame->GetScrollPortRect() + offset;
   LayoutDeviceRect clipBounds =
       LayoutDeviceRect::FromAppUnits(scrollPort, auPerDevPixel);
@@ -366,8 +366,14 @@ Maybe<wr::WrSpatialId> ClipManager::DefineScrollLayers(
   
   
   
-  LayoutDevicePoint scrollOffset = LayoutDevicePoint::FromAppUnitsRounded(
-      scrollableFrame->GetScrollPosition(), auPerDevPixel);
+  const bool useRoundedOffset =
+      StaticPrefs::apz_rounded_external_scroll_offset();
+  LayoutDevicePoint scrollOffset =
+      useRoundedOffset
+          ? LayoutDevicePoint::FromAppUnitsRounded(
+                scrollableFrame->GetScrollPosition(), auPerDevPixel)
+          : LayoutDevicePoint::FromAppUnits(
+                scrollableFrame->GetScrollPosition(), auPerDevPixel);
 
   
   
