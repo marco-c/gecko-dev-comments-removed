8aa56d5ddb457e1bd8b95842739dc0488824f123
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 02:12:37 +0000

    Bug 1772733 - Change directory in xpcshell without OS.File r=jmaher,nika
    Instead of relying on OS.File.setCurrentDirectory in the xpcshell test harness
    we instead provide a function to directly change directory.
    
    Differential Revision: https://phabricator.services.mozilla.com/D148977
    
    UltraBlame original commit: 8b9447d43358dcc399e2c72b2372825946da6b42

--- testing/xpcshell/head.js
+++ testing/xpcshell/head.js
@@ -60,8 +60,6 @@ let { XPCOMUtils: _XPCOMUtils } = ChromeUtils.importESModule(
   "resource://gre/modules/XPCOMUtils.sys.mjs"
 );
 
-let { OS: _OS } = ChromeUtils.import("resource://gre/modules/osfile.jsm");
-
 
 var { Assert: AssertCls } = ChromeUtils.importESModule(
   "resource://testing-common/Assert.sys.mjs"
@@ -501,19 +499,12 @@ function _initDebugging(port) {
 
 function _execute_test() {
   if (typeof _TEST_CWD != "undefined") {
-    let cwd_complete = false;
-    _OS.File.setCurrentDirectory(_TEST_CWD)
-      .then(_ => (cwd_complete = true))
-      .catch(e => {
-        _testLogger.error(_exception_message(e));
-        cwd_complete = true;
-      });
-    _Services.tm.spinEventLoopUntil(
-      "Test(xpcshell/head.js:setCurrentDirectory)",
-      () => cwd_complete
-    );
+    try {
+      changeTestShellDir(_TEST_CWD);
+    } catch (e) {
+      _testLogger.error(_exception_message(e));
+    }
   }
-
   if (runningInParent && _AppConstants.platform == "android") {
     try {
       
