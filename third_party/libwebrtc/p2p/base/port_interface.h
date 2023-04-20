40273579637ed446e0c77f9b6c9946f36879a6c3
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 00:59:21 +0000

    Bug 1790097 - Vendor libwebrtc from 58ecd42deb
    Upstream commit: https://webrtc.googlesource.com/src/+/58ecd42deb0d2588d20ecccb4668a3c7a800de21
        Remove std::string version of some functions that are no longer needed.
    
        The absl::string_view versions can now be made pure virtual due to
        downstream client changes. The std::string versions in the base
        classes are still needed by downstream users, but will be removed
        eventually.
    
        Bug: webrtc:13579
        Change-Id: Id757a07380f0518edf407ff5d0644511eb1e53d3
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/265980
        Reviewed-by: Jonas Oreland <jonasowebrtc.org>
        Commit-Queue: Ali Tofigh <alitowebrtc.org>
        Cr-Commit-Position: refs/heads/main{#37349}
    
    UltraBlame original commit: 750eb173400977544b30549352b721be3a885e40

--- third_party/libwebrtc/p2p/base/port_interface.h
+++ third_party/libwebrtc/p2p/base/port_interface.h
@@ -63,8 +63,8 @@ class PortInterface {
 
   
   
-  virtual bool SupportsProtocol(const std::string& protocol) const = 0;
-  virtual bool SupportsProtocol(absl::string_view protocol) const;
+  virtual bool SupportsProtocol(const std::string& protocol) const;
+  virtual bool SupportsProtocol(absl::string_view protocol) const = 0;
 
   
   
