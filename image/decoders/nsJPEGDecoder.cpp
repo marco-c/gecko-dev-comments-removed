2f9661f590c213a7afef537a1915b0dd5fb5db6c
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 21:21:11 +0000

    Bug 1823614. Limit the number of scans we allow in jpeg images to some finite value. r=gfx-reviewers,bradwerth a=reland
    We want to provide some finite limit to prevent small jpeg files from being able to tie up cpus for a much larger time than their small size would normally allow. We want to choose a number high enough so that no sane jpeg file would approach it, unless it had been crafted to take advantage of this problem.
    
    Skia's jpeg decoder limit's it to 100:
    
    https://searchfox.org/mozilla-central/rev/f078cd02746b29652c134b144f0629d47e378166/gfx/skia/skia/src/codec/SkJpegDecoderMgr.cpp#33
    
    The OSS_Fuzz targets for libjpeg-turbo limit it to 500:
    
    https://bugzilla.mozilla.org/show_bug.cgi?id=1252196#c11
    
    Differential Revision: https://phabricator.services.mozilla.com/D173120
    
    UltraBlame original commit: 12d26e785e8168970623347a51536b470c08d768

--- image/decoders/nsJPEGDecoder.cpp
+++ image/decoders/nsJPEGDecoder.cpp
@@ -67,6 +67,7 @@ METHODDEF(boolean) fill_input_buffer(j_decompress_ptr jd);
 METHODDEF(void) skip_input_data(j_decompress_ptr jd, long num_bytes);
 METHODDEF(void) term_source(j_decompress_ptr jd);
 METHODDEF(void) my_error_exit(j_common_ptr cinfo);
+METHODDEF(void) progress_monitor(j_common_ptr info);
 
 
 #define MAX_JPEG_MARKER_LENGTH (((uint32_t)1 << 16) - 1)
@@ -101,6 +102,7 @@ nsJPEGDecoder::nsJPEGDecoder(RasterImage* aImage,
   mBytesToSkip = 0;
   memset(&mInfo, 0, sizeof(jpeg_decompress_struct));
   memset(&mSourceMgr, 0, sizeof(mSourceMgr));
+  memset(&mProgressMgr, 0, sizeof(mProgressMgr));
   mInfo.client_data = (void*)this;
 
   mSegment = nullptr;
@@ -159,6 +161,9 @@ nsresult nsJPEGDecoder::InitInternal() {
 
   mInfo.mem->max_memory_to_use = static_cast<long>(
       std::min<size_t>(SurfaceCache::MaximumCapacity(), LONG_MAX));
+
+  mProgressMgr.progress_monitor = &progress_monitor;
+  mInfo.progress = &mProgressMgr;
 
   
   for (uint32_t m = 0; m < 16; m++) {
@@ -732,6 +737,17 @@ my_error_exit(j_common_ptr cinfo) {
   
   
   longjmp(err->setjmp_buffer, static_cast<int>(error_code));
+}
+
+static void progress_monitor(j_common_ptr info) {
+  int scan = ((j_decompress_ptr)info)->input_scan_number;
+  
+  
+  
+  
+  if (scan >= 1000) {
+    my_error_exit(info);
+  }
 }
 
 
