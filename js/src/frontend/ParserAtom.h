7bc2325eb1ba23b8a1b3a7c0539ffa7750a3d6f7
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 02:36:47 +0000

    Bug 1797024 - Part 2: Move WellKnownParserAtoms out of JSRuntime. r=nbp
    Differential Revision: https://phabricator.services.mozilla.com/D160207
    
    UltraBlame original commit: 8967cf32fb68ed92f365cd4d5fcfcdab68239932

--- js/src/frontend/ParserAtom.h
+++ js/src/frontend/ParserAtom.h
@@ -25,13 +25,13 @@
 #include "js/TypeDecls.h"         
 #include "js/Utility.h"           
 #include "js/Vector.h"            
+#include "threading/Mutex.h"      
 #include "util/Text.h"            
 #include "vm/CommonPropertyNames.h"
 #include "vm/StaticStrings.h"
 #include "vm/WellKnownAtom.h"  
 
 struct JS_PUBLIC_API JSContext;
-struct JSRuntime;
 
 class JSAtom;
 class JSString;
@@ -572,7 +572,8 @@ using ParserAtomSpan = mozilla::Span<ParserAtom*>;
 
 
 class WellKnownParserAtoms {
- public:
+  static WellKnownParserAtoms singleton_;
+
   
   
   using EntryMap = HashMap<const WellKnownAtomInfo*, TaggedParserAtomIndex,
@@ -581,8 +582,17 @@ class WellKnownParserAtoms {
 
   bool initSingle(const WellKnownAtomInfo& info, TaggedParserAtomIndex index);
 
- public:
   bool init();
+  void free();
+
+ public:
+  static bool initSingleton();
+  static void freeSingleton();
+
+  static WellKnownParserAtoms& getSingleton() {
+    MOZ_ASSERT(!singleton_.wellKnownMap_.empty());
+    return singleton_;
+  }
 
   
   static constexpr size_t MaxWellKnownLength = 32;
@@ -658,8 +668,6 @@ class ParserAtomsTable {
   friend struct CompilationStencil;
 
  private:
-  const WellKnownParserAtoms& wellKnownTable_;
-
   LifoAlloc* alloc_;
 
   
@@ -669,7 +677,7 @@ class ParserAtomsTable {
   ParserAtomVector entries_;
 
  public:
-  ParserAtomsTable(JSRuntime* rt, LifoAlloc& alloc);
+  explicit ParserAtomsTable(LifoAlloc& alloc);
   ParserAtomsTable(ParserAtomsTable&&) = default;
   ParserAtomsTable& operator=(ParserAtomsTable&& other) noexcept {
     entryMap_ = std::move(other.entryMap_);
