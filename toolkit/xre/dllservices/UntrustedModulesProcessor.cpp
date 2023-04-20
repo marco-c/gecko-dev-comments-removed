2f788f44262e220f1ee402603b68e48f3b781371
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 22:52:04 +0000

    Backed out changeset 0655ae1a589e (bug 1826784) for bustages on UtilityProcessHost.cpp
    UltraBlame original commit: 14c8c01bc869b38bdb81f716e8d51b2af37e36e2

--- toolkit/xre/dllservices/UntrustedModulesProcessor.cpp
+++ toolkit/xre/dllservices/UntrustedModulesProcessor.cpp
@@ -15,8 +15,6 @@
 #include "mozilla/Likely.h"
 #include "mozilla/net/SocketProcessChild.h"
 #include "mozilla/net/SocketProcessParent.h"
-#include "mozilla/ipc/UtilityProcessParent.h"
-#include "mozilla/ipc/UtilityProcessChild.h"
 #include "mozilla/RDDChild.h"
 #include "mozilla/RDDParent.h"
 #include "mozilla/RDDProcessManager.h"
@@ -80,7 +78,6 @@ bool UntrustedModulesProcessor::IsSupportedProcessType() {
     case GeckoProcessType_Socket:
       return Telemetry::CanRecordReleaseData();
     case GeckoProcessType_RDD:
-    case GeckoProcessType_Utility:
       
       
       
@@ -752,11 +749,6 @@ UntrustedModulesProcessor::SendGetModulesTrust(ModulePaths&& aModules,
     case GeckoProcessType_Socket: {
       return ::mozilla::SendGetModulesTrust(
           net::SocketProcessChild::GetSingleton(), std::move(aModules),
-          runNormal);
-    }
-    case GeckoProcessType_Utility: {
-      return ::mozilla::SendGetModulesTrust(
-          ipc::UtilityProcessChild::GetSingleton().get(), std::move(aModules),
           runNormal);
     }
     default: {
