d09bce83bfbb2d05ca086c3e05869eb6fea55f4f
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 04:09:46 +0000

    Backed out changeset 4d031b559f26 (bug 1803959) for causing pdf content failures. CLOSED TREE
    UltraBlame original commit: dec0c0888e95cb8db893a910083cedba54b1dad5

--- mobile/android/app/geckoview-prefs.js
+++ mobile/android/app/geckoview-prefs.js
@@ -99,9 +99,8 @@ pref("extensions.formautofill.loglevel", "Warn");
 pref("pdfjs.handleOctetStream", true);
 pref("browser.download.open_pdf_attachments_inline", true);
 pref("pdfjs.annotationEditorMode", -1);
+pref("pdfjs.disabled", true);
 
 
 pref("fission.webContentIsolationStrategy", 0);
-#else
-pref("pdfjs.disabled", true);
 #endif
