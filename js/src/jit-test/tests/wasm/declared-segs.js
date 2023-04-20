c6cae937a953e768550276ce7f1463eb4e698100
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 03:47:31 +0000

    Bug 1803138: Drop declarative element segments on instantiation. r=rhunt
    Differential Revision: https://phabricator.services.mozilla.com/D163394
    
    UltraBlame original commit: 83bc1ec5e4ae70b44c17c0cb2b7a618edf417c99

--- js/src/jit-test/tests/wasm/declared-segs.js
+++ js/src/jit-test/tests/wasm/declared-segs.js
@@ -19,15 +19,28 @@ wasmFullPass(`
 `);
 
 
+{
+	let inst = wasmEvalText(`
+		(module
+			(func $f1)
+			(table 1 1 funcref)
+			(elem $e1 declare $f1)
+			(func (export "testfn") (table.init $e1 (i32.const 0) (i32.const 0) (i32.const 1)))
+		)
+	`);
+	assertErrorMessage(() => inst.exports.testfn(), WebAssembly.RuntimeError, /index out of bounds/);
+}
+
+
 wasmEvalText(`
 	(module
 		(func $f1)
 		(table 1 1 funcref)
-		(elem declare $f1)
-		(func $start (table.init 0 (i32.const 0) (i32.const 0) (i32.const 1)))
+		(elem $e1 declare $f1)
+		(func $start (elem.drop $e1) (elem.drop $e1))
 		(start $start)
 	)
-`);
+`)
 
 
 wasmAssert(`
