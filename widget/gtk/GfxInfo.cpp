ead7fab96125e4085902e1b7eee51b3b10a44846
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 17:32:16 +0000

    Bug 1777849 - Disable FEATURE_THREADSAFE_GL on libX11 < 1.7. r=gfx-reviewers,ahale
    LibX11 versions prior to 1.7 have a race condition that made it unsafe to use
    in multiple threads. Since Firefox 104, we created a CanvasRender thread that
    is used to service WebGL commands off the Renderer thread so that WebGL does
    not block WebRender. If WebGL uses GLX, this can lead us to using GLX on the
    two different threads. In combination with an unsafe version of libX11, this
    can lead to severe instability.
    
    We already have a FEATURE_THREADSAFE_GL in place to denote this, which was
    currently used mainly for Nouveau which is also unsafe to use on multiple
    threads. If this feature fails, the CanvasRender thread is not used, and
    all WebGL commands go back to being on the Renderer thread as normal, avoiding
    the problem.
    
    Thus, this detects the presence of libX11 version 1.7 or greater, and if not
    found, we disable FEATURE_THREADSAFE_GL to disable the CanvasRender thread.
    
    Differential Revision: https://phabricator.services.mozilla.com/D170992
    
    UltraBlame original commit: 536f32f1cf4708ab2f25a91a199b819ec63aa921

--- widget/gtk/GfxInfo.cpp
+++ widget/gtk/GfxInfo.cpp
@@ -15,6 +15,7 @@
 #include <sys/types.h>
 #include <sys/utsname.h>
 #include <sys/wait.h>
+#include <dlfcn.h>
 
 #include "mozilla/gfx/Logging.h"
 #include "mozilla/SSE.h"
@@ -65,6 +66,7 @@ nsresult GfxInfo::Init() {
   mHasMultipleGPUs = false;
   mGlxTestError = false;
   mIsVAAPISupported = false;
+  mIsX11ThreadSafe = false;
   return GfxInfoBase::Init();
 }
 
@@ -524,6 +526,14 @@ void GfxInfo::GetData() {
   mIsWayland = GdkIsWaylandDisplay();
   mIsXWayland = IsXWaylandProtocol();
 
+  
+  
+  
+  
+  if (!mIsWayland) {
+    mIsX11ThreadSafe = dlsym(RTLD_DEFAULT, "XSetIOErrorExitHandler") != nullptr;
+  }
+
   if (!ddxDriver.IsEmpty()) {
     PRInt32 start = 0;
     PRInt32 loc = ddxDriver.Find(";", start);
@@ -955,6 +965,15 @@ nsresult GfxInfo::GetFeatureStatusImpl(
       !mIsVAAPISupported) {
     *aStatus = nsIGfxInfo::FEATURE_BLOCKED_PLATFORM_TEST;
     aFailureId = "FEATURE_FAILURE_VIDEO_DECODING_TEST_FAILED";
+    return NS_OK;
+  }
+
+  if (aFeature == nsIGfxInfo::FEATURE_THREADSAFE_GL && !mIsWayland &&
+      !mIsX11ThreadSafe) {
+    
+    
+    *aStatus = nsIGfxInfo::FEATURE_BLOCKED_OS_VERSION;
+    aFailureId = "FEATURE_FAILURE_THREADSAFE_GL";
     return NS_OK;
   }
 
