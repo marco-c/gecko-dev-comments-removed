4d105da2074f6fcb56f902e2d98c0ed061b23a2b
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 09:07:59 +0000

    Bug 1753349 (2/9): refactor: inline `size` argument to `GLContextEGL::CreateEGLPBufferOffscreenContext` r=jgilbert
    This is basically an implementation detail that the existing callers doesn't really need to know.
    Factor it out.
    
    Split out from jgilbert's D164308.
    
    Differential Revision: https://phabricator.services.mozilla.com/D166707
    
    UltraBlame original commit: 1e30feb6482b0d2d5b2f6376e69ab96da6518768

--- gfx/webrender_bindings/RenderThread.cpp
+++ gfx/webrender_bindings/RenderThread.cpp
@@ -1267,9 +1267,8 @@ static already_AddRefed<gl::GLContext> CreateGLContextANGLE(
   
   
 
-  const auto dummySize = mozilla::gfx::IntSize(16, 16);
-  auto gl = gl::GLContextEGL::CreateEGLPBufferOffscreenContext(
-      egl, {flags}, dummySize, &failureId);
+  auto gl = gl::GLContextEGL::CreateEGLPBufferOffscreenContext(egl, {flags},
+                                                               &failureId);
   if (!gl || !gl->IsANGLE()) {
     aError.Assign(nsPrintfCString("RcANGLE(create GL context failed: %p, %s)",
                                   gl.get(), failureId.get()));
