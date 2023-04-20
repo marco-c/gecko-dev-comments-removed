4bab32e72c2d26650fe17a982173fdcb06da9ff7
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 07:56:55 +0000

    Bug 1693495 - [devtools] Remove getTabTarget in favor of Connector.currentTarget. r=devtools-reviewers,nchevobbe
    Differential Revision: https://phabricator.services.mozilla.com/D166053
    
    UltraBlame original commit: 9f0c815966fdf4cbf6be3f4a090fe2f44ad8e912

--- devtools/client/netmonitor/src/connector/index.js
+++ devtools/client/netmonitor/src/connector/index.js
@@ -1,6 +1,6 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+
+
 
 "use strict";
 
@@ -18,7 +18,7 @@ const {
   TYPES,
 } = require("resource://devtools/shared/commands/resource/resource-command.js");
 
-// Network throttling
+
 loader.lazyRequireGetter(
   this,
   "throttlingProfiles",
@@ -27,25 +27,24 @@ loader.lazyRequireGetter(
 
 const DEVTOOLS_ENABLE_PERSISTENT_LOG_PREF = "devtools.netmonitor.persistlog";
 
-/**
- * Connector to Firefox backend.
- */
+
+
+
 class Connector {
   constructor() {
-    // Public methods
+    
     this.connect = this.connect.bind(this);
     this.disconnect = this.disconnect.bind(this);
     this.willNavigate = this.willNavigate.bind(this);
     this.navigate = this.navigate.bind(this);
     this.sendHTTPRequest = this.sendHTTPRequest.bind(this);
     this.triggerActivity = this.triggerActivity.bind(this);
-    this.getTabTarget = this.getTabTarget.bind(this);
     this.viewSourceInDebugger = this.viewSourceInDebugger.bind(this);
     this.requestData = this.requestData.bind(this);
     this.getTimingMarker = this.getTimingMarker.bind(this);
     this.updateNetworkThrottling = this.updateNetworkThrottling.bind(this);
 
-    // Internals
+    
     this.getLongString = this.getLongString.bind(this);
     this.onResourceAvailable = this.onResourceAvailable.bind(this);
     this.onResourceUpdated = this.onResourceUpdated.bind(this);
@@ -65,20 +64,20 @@ class Connector {
     return this.commands.targetCommand.targetFront;
   }
 
-  /**
-   * Connect to the backend.
-   *
-   * @param {Object} connection object with e.g. reference to the Toolbox.
-   * @param {Object} actions (optional) is used to fire Redux actions to update store.
-   * @param {Object} getState (optional) is used to get access to the state.
-   */
+  
+
+
+
+
+
+
   async connect(connection, actions, getState) {
     this.actions = actions;
     this.getState = getState;
     this.toolbox = connection.toolbox;
     this.commands = this.toolbox.commands;
 
-    // The owner object (NetMonitorAPI) received all events.
+    
     this.owner = connection.owner;
 
     this.networkFront = await this.commands.watcherFront.getNetworkParentActor();
@@ -95,8 +94,8 @@ class Connector {
 
     await this.resume(false);
 
-    // Server side persistance of the data across reload is disabled by default.
-    // Ensure enabling it, if the related frontend pref is true.
+    
+    
     if (Services.prefs.getBoolPref(DEVTOOLS_ENABLE_PERSISTENT_LOG_PREF)) {
       await this.updatePersist();
     }
@@ -107,7 +106,7 @@ class Connector {
   }
 
   disconnect() {
-    // As this function might be called twice, we need to guard if already called.
+    
     if (this._destroyed) {
       return;
     }
@@ -134,13 +133,13 @@ class Connector {
   }
 
   clear() {
-    // Clear all the caches in the data provider
+    
     this.dataProvider.clear();
 
     this.commands.resourceCommand.clearResources(Connector.NETWORK_RESOURCES);
     this.emitForTests("clear-network-resources");
 
-    // Disable the realted network logs in the webconsole
+    
     this.toolbox.disableAllConsoleNetworkLogs();
   }
 
@@ -254,19 +253,19 @@ class Connector {
         this.actions.batchReset();
         this.actions.clearRequests();
       } else {
-        // If the log is persistent, just clear all accumulated timing markers.
+        
         this.actions.clearTimingMarkers();
       }
     }
 
     if (this.actions && this.getState) {
       const state = this.getState();
-      // Resume is done automatically on page reload/navigation.
+      
       if (!state.requests.recording) {
         this.actions.toggleRecording();
       }
 
-      // Stop any ongoing search.
+      
       if (state.search.ongoingSearch) {
         this.actions.stopOngoingSearch();
       }
@@ -285,8 +284,8 @@ class Connector {
       if (this.owner) {
         this.owner.off(EVENTS.PAYLOAD_READY, listener);
       }
-      // Netmonitor may already be destroyed,
-      // so do not try to notify the listeners
+      
+      
       if (this.dataProvider) {
         this.onReloaded();
       }
@@ -303,18 +302,18 @@ class Connector {
     }
   }
 
-  /**
-   * The "DOMContentLoaded" and "Load" events sent by the console actor.
-   *
-   * @param {object} resource The DOCUMENT_EVENT resource
-   */
+  
+
+
+
+
   onDocEvent(resource, { areExistingResources }) {
     if (!resource.targetFront.isTopLevel) {
-      // Only consider top level document, and ignore remote iframes top document
+      
       return;
     }
 
-    // Netmonitor does not support dom-loading
+    
     if (
       resource.name != "dom-interactive" &&
       resource.name != "dom-complete" &&
@@ -324,10 +323,10 @@ class Connector {
     }
 
     if (resource.name == "will-navigate") {
-      // When we open the netmonitor while the page already started loading,
-      // we don't want to clear it. So here, we ignore will-navigate events
-      // which were stored in the ResourceCommand cache and only consider
-      // the live one coming straight from the server.
+      
+      
+      
+      
       if (!areExistingResources) {
         this.willNavigate();
       }
@@ -345,11 +344,11 @@ class Connector {
     this.emitForTests(TEST_EVENTS.TIMELINE_EVENT, resource);
   }
 
-  /**
-   * Send a HTTP request data payload
-   *
-   * @param {object} data data payload would like to sent to backend
-   */
+  
+
+
+
+
   async sendHTTPRequest(data) {
     const networkContentFront = await this.currentTarget.getFront(
       "networkContent"
@@ -358,9 +357,9 @@ class Connector {
     return { channelId };
   }
 
-  /*
-   * Get the list of blocked URLs
-   */
+  
+
+
   async getBlockedUrls() {
     return this.networkFront.getBlockedUrls();
   }
@@ -375,37 +374,37 @@ class Connector {
     this.emitForTests(TEST_EVENTS.PERSIST_CHANGED, enabled);
   }
 
-  /**
-   * Updates the list of blocked URLs
-   *
-   * @param {object} urls An array of URL strings
-   */
+  
+
+
+
+
   async setBlockedUrls(urls) {
     return this.networkFront.setBlockedUrls(urls);
   }
 
-  /**
-   * Triggers a specific "activity" to be performed by the frontend.
-   * This can be, for example, triggering reloads or enabling/disabling cache.
-   *
-   * @param {number} type The activity type. See the ACTIVITY_TYPE const.
-   * @return {object} A promise resolved once the activity finishes and the frontend
-   *                  is back into "standby" mode.
-   */
+  
+
+
+
+
+
+
+
   triggerActivity(type) {
-    // Puts the frontend into "standby" (when there's no particular activity).
+    
     const standBy = () => {
       this.currentActivity = ACTIVITY_TYPE.NONE;
     };
 
-    // Reconfigures the tab, optionally triggering a reload.
+    
     const reconfigureTab = async options => {
       await this.commands.targetConfigurationCommand.updateConfiguration(
         options
       );
     };
 
-    // Reconfigures the tab and waits for the target to finish navigating.
+    
     const reconfigureTabAndReload = async options => {
       await reconfigureTab(options);
       await this.commands.targetCommand.reloadTopLevelTarget();
@@ -465,54 +464,46 @@ class Connector {
     return Promise.reject(new Error("Invalid activity type"));
   }
 
-  /**
-   * Fetches the full text of a LongString.
-   *
-   * @param {object|string} stringGrip
-   *        The long string grip containing the corresponding actor.
-   *        If you pass in a plain string (by accident or because you're lazy),
-   *        then a promise of the same string is simply returned.
-   * @return {object}
-   *         A promise that is resolved when the full string contents
-   *         are available, or rejected if something goes wrong.
-   */
+  
+
+
+
+
+
+
+
+
+
+
   getLongString(stringGrip) {
     return this.dataProvider.getLongString(stringGrip);
   }
 
-  /**
-   * Getter that access tab target instance.
-   * @return {object} browser tab target instance
-   */
-  getTabTarget() {
-    return this.currentTarget;
-  }
+  
 
-  /**
-   * Getter that returns the current toolbox instance.
-   * @return {Toolbox} toolbox instance
-   */
+
+
   getToolbox() {
     return this.toolbox;
   }
 
-  /**
-   * Open a given source in Debugger
-   * @param {string} sourceURL source url
-   * @param {number} sourceLine source line number
-   */
+  
+
+
+
+
   viewSourceInDebugger(sourceURL, sourceLine, sourceColumn) {
     if (this.toolbox) {
       this.toolbox.viewSourceInDebugger(sourceURL, sourceLine, sourceColumn);
     }
   }
 
-  /**
-   * Fetch networkEventUpdate websocket message from back-end when
-   * data provider is connected.
-   * @param {object} request network request instance
-   * @param {string} type NetworkEventUpdate type
-   */
+  
+
+
+
+
+
   requestData(request, type) {
     return this.dataProvider.requestData(request, type);
   }
@@ -530,9 +521,9 @@ class Connector {
     if (!enabled) {
       this.networkFront.clearNetworkThrottling();
     } else {
-      // The profile can be either a profile id which is used to
-      // search the predefined throttle profiles or a profile object
-      // as defined in the trottle tests.
+      
+      
+      
       if (typeof profile === "string") {
         profile = throttlingProfiles.find(({ id }) => id == profile);
       }
@@ -547,10 +538,10 @@ class Connector {
     this.emitForTests(TEST_EVENTS.THROTTLING_CHANGED, { profile });
   }
 
-  /**
-   * Fire events for the owner object. These events are only
-   * used in tests so, don't fire them in production release.
-   */
+  
+
+
+
   emitForTests(type, data) {
     if (this.owner) {
       this.owner.emitForTests(type, data);
