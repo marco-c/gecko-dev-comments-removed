176acae16bdac9bbc69d7c46ea0d14cbab8d5e7a
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 19:14:11 +0000

    Bug 1820883 - Convert toolkit/components/autocomplete to ES modules. r=credential-management-reviewers,dimi
    Differential Revision: https://phabricator.services.mozilla.com/D171964
    
    UltraBlame original commit: a3f7b587ff20fad87459c3be9e20eb19b9634926

--- toolkit/components/autocomplete/AutoCompleteSimpleSearch.sys.mjs
+++ toolkit/components/autocomplete/AutoCompleteSimpleSearch.sys.mjs
@@ -1,11 +1,11 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-
-
-
-
-
-
-class AutoCompleteSimpleSearch {
+/**
+ * See nsIAutoCompleteSimpleSearch
+ */
+export class AutoCompleteSimpleSearch {
   constructor() {
     this.classID = Components.ID("{dc185a77-ba88-4caa-8f16-465253f7599a}");
     this.QueryInterface = ChromeUtils.generateQI([
@@ -22,26 +22,24 @@ class AutoCompleteSimpleSearch {
 
   _result = null;
 
-  
-
-
+  /**
+   * See nsIAutoCompleteSimpleSearch
+   */
   overrideNextResult(result) {
     this._result = result;
   }
 
-  
-
-
+  /**
+   * See nsIAutoCompleteSearch
+   */
   startSearch(searchString, searchParam, previousResult, listener) {
     let result = this._result;
     result.setSearchString(searchString);
     listener.onSearchResult(this, result);
   }
 
-  
-
-
+  /**
+   * See nsIAutoCompleteSearch
+   */
   stopSearch() {}
 }
-
-const EXPORTED_SYMBOLS = ["AutoCompleteSimpleSearch"];
