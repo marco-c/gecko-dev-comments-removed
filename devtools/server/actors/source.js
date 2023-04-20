1953637f042c71ff9aae77adc1575eff1d7f34be
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 20:26:01 +0000

    Backed out 2 changesets (bug 1815937) for causing wpt failures at blockeduri-inline.html CLOSED TREE
    Backed out changeset 668971329051 (bug 1815937)
    Backed out changeset a4a3430c9bab (bug 1815937)
    
    UltraBlame original commit: 28a86c6f1e078c8a1bc465fead7f51425c1ef97e

--- devtools/server/actors/source.js
+++ devtools/server/actors/source.js
@@ -117,6 +117,7 @@ class SourceActor extends Actor {
     this._url = undefined;
     this._source = source;
     this.__isInlineSource = undefined;
+    this._startLineColumnDisplacement = null;
   }
 
   get _isInlineSource() {
@@ -294,8 +295,8 @@ class SourceActor extends Actor {
     return true;
   }
 
-  getBreakableLines() {
-    const positions = this._getBreakpointPositions();
+  async getBreakableLines() {
+    const positions = await this._getBreakpointPositions();
     const lines = new Set();
     for (const position of positions) {
       if (!lines.has(position.line)) {
@@ -304,6 +305,106 @@ class SourceActor extends Actor {
     }
 
     return Array.from(lines);
+  }
+
+  
+  
+  
+  
+  _getStartLineColumnDisplacement() {
+    if (this._startLineColumnDisplacement) {
+      return this._startLineColumnDisplacement;
+    }
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    const fileContents = this.sourcesManager.urlContents(
+      this.url,
+       true,
+       this._isInlineSource
+    );
+    if (fileContents.then) {
+      return fileContents.then(contents =>
+        this._setStartLineColumnDisplacement(contents)
+      );
+    }
+    return this._setStartLineColumnDisplacement(fileContents);
+  }
+
+  _setStartLineColumnDisplacement(fileContents) {
+    const d = this._calculateStartLineColumnDisplacement(fileContents);
+    this._startLineColumnDisplacement = d;
+    return d;
+  }
+
+  _calculateStartLineColumnDisplacement(fileContents) {
+    const startLine = this._source.startLine;
+
+    const lineBreak = /\r\n?|\n|\u2028|\u2029/;
+    const fileStartLine =
+      fileContents.content.split(lineBreak)[startLine - 1] || "";
+
+    const sourceContents = this._source.text;
+
+    if (lineBreak.test(sourceContents)) {
+      
+      const firstLine = sourceContents.split(lineBreak)[0];
+      if (firstLine.length && fileStartLine.endsWith(firstLine)) {
+        const column = fileStartLine.length - firstLine.length;
+        return { startLine, column };
+      }
+      return {};
+    }
+
+    
+    
+    
+    
+    const column = fileStartLine.indexOf(sourceContents);
+    if (column != -1) {
+      return { startLine, column };
+    }
+    return {};
+  }
+
+  
+  
+  
+  _adjustInlineScriptLocation(location, upward) {
+    if (!this._isInlineSource) {
+      return location;
+    }
+
+    const info = this._getStartLineColumnDisplacement();
+    if (info.then) {
+      return info.then(i =>
+        this._adjustInlineScriptLocationFromDisplacement(i, location, upward)
+      );
+    }
+    return this._adjustInlineScriptLocationFromDisplacement(
+      info,
+      location,
+      upward
+    );
+  }
+
+  _adjustInlineScriptLocationFromDisplacement(info, location, upward) {
+    const { line, column } = location;
+    if (this._startLineColumnDisplacement.startLine == line) {
+      let displacement = this._startLineColumnDisplacement.column;
+      if (!upward) {
+        displacement = -displacement;
+      }
+      return { line, column: column + displacement };
+    }
+    return location;
   }
 
   
@@ -439,7 +540,7 @@ class SourceActor extends Actor {
     }
   }
 
-  _getBreakpointPositions(query) {
+  async _getBreakpointPositions(query) {
     const scripts = this._findDebuggeeScripts(
       query,
        true
@@ -447,7 +548,7 @@ class SourceActor extends Actor {
 
     const positions = [];
     for (const script of scripts) {
-      this._addScriptBreakpointPositions(query, script, positions);
+      await this._addScriptBreakpointPositions(query, script, positions);
     }
 
     return (
@@ -460,7 +561,7 @@ class SourceActor extends Actor {
     );
   }
 
-  _addScriptBreakpointPositions(query, script, positions) {
+  async _addScriptBreakpointPositions(query, script, positions) {
     const {
       start: { line: startLine = 0, column: startColumn = 0 } = {},
       end: { line: endLine = Infinity, column: endColumn = Infinity } = {},
@@ -477,15 +578,22 @@ class SourceActor extends Actor {
         continue;
       }
 
-      positions.push({
-        line: lineNumber,
-        column: columnNumber,
-      });
+      
+      
+      const position = await this._adjustInlineScriptLocation(
+        {
+          line: lineNumber,
+          column: columnNumber,
+        },
+         true
+      );
+
+      positions.push(position);
     }
   }
 
-  getBreakpointPositionsCompressed(query) {
-    const items = this._getBreakpointPositions(query);
+  async getBreakpointPositionsCompressed(query) {
+    const items = await this._getBreakpointPositions(query);
     const compressed = {};
     for (const { line, column } of items) {
       if (!compressed[line]) {
@@ -595,7 +703,7 @@ class SourceActor extends Actor {
 
 
   async applyBreakpoint(actor) {
-    const { line, column } = actor.location;
+    let { line, column } = actor.location;
 
     
     const entryPoints = [];
@@ -633,6 +741,20 @@ class SourceActor extends Actor {
         }
       }
     } else {
+      
+      
+      
+      
+      let adjusted = this._adjustInlineScriptLocation(
+        { line, column },
+         false
+      );
+      if (adjusted.then) {
+        adjusted = await adjusted;
+      }
+      line = adjusted.line;
+      column = adjusted.column;
+
       
       
       const query = { start: { line, column }, end: { line, column } };
