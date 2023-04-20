acac4e0d214c07ed1bf2785ea601578e86ec487f
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 18:03:12 +0000

    Bug 1817126 - Implement GetUnsnappedLayoutScrollRange and use it in WantAsyncScroll. r=tnikkel
    Depends on D170465
    
    Differential Revision: https://phabricator.services.mozilla.com/D170466
    
    UltraBlame original commit: 38a4e166571c688f77738923b7f3bd4165f8209c

--- layout/generic/nsGfxScrollFrame.h
+++ layout/generic/nsGfxScrollFrame.h
@@ -203,6 +203,7 @@ class ScrollFrameHelper : public nsIReflowCallback {
     return pt;
   }
   nsRect GetLayoutScrollRange() const;
+  nsRect GetUnsnappedLayoutScrollRange() const;
   
   nsRect GetScrollRange(nscoord aWidth, nscoord aHeight) const;
   nsSize GetVisualViewportSize() const;
