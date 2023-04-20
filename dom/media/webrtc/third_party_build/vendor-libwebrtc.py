d429d6b09850871fded80a08e0dec36a4edb3475
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 21:10:26 +0000

    Bug 1818599 - pt8 - teach vendor-libwebrtc.py how to remove subdirectories as well as files r=ng
    Depends on D173349
    
    Differential Revision: https://phabricator.services.mozilla.com/D173350
    
    UltraBlame original commit: 8b6b13f196cdaa84c464d4e7ad14414a465ceb79

--- dom/media/webrtc/third_party_build/vendor-libwebrtc.py
+++ dom/media/webrtc/third_party_build/vendor-libwebrtc.py
@@ -34,6 +34,16 @@ LIBWEBRTC_UNUSED_IN_FIREFOX = [
     "style-guide.md",
     
     
+    "sdk/android/AndroidManifest.xml",
+    "sdk/android/BUILD.gn",
+    "sdk/android/OWNERS",
+    "sdk/android/README",
+    "sdk/android/api/org/webrtc/OWNERS",
+    "sdk/android/instrumentationtests",
+    "sdk/android/src/jni",
+    "sdk/android/native_api",
+    "sdk/android/native_unittests",
+    "sdk/android/tests",
     "sdk/android/api/org/webrtc/CandidatePairChangeEvent.java",
     "sdk/android/api/org/webrtc/DefaultVideoEncoderFactory.java",
     "sdk/android/api/org/webrtc/HardwareVideoEncoderFactory.java",
@@ -207,7 +217,10 @@ def unpack(target):
         
         for path in LIBWEBRTC_UNUSED_IN_FIREFOX:
             try:
-                os.remove(os.path.join(LIBWEBRTC_DIR, path))
+                if os.path.isdir(os.path.join(LIBWEBRTC_DIR, path)):
+                    shutil.rmtree(os.path.join(LIBWEBRTC_DIR, path))
+                else:
+                    os.remove(os.path.join(LIBWEBRTC_DIR, path))
             except OSError:
                 
                 pass
