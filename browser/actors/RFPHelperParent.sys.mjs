c9d9d1c84f0ef33df1e597da8d241dcc61d92152
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 21:00:12 +0000

    Bug 1821158 - Convert browser/actors/RFPHelper* JSMs to ESMs updated r=kpatenio
    Differential Revision: https://phabricator.services.mozilla.com/D172899
    
    UltraBlame original commit: 12382c5e0f169a0aa297b29f4195d21786903bb1

--- browser/actors/RFPHelperParent.sys.mjs
+++ browser/actors/RFPHelperParent.sys.mjs
@@ -1,13 +1,10 @@
-1; 
+1; /* -*- indent-tabs-mode: nil; js-indent-level: 2 -*- */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+import { XPCOMUtils } from "resource://gre/modules/XPCOMUtils.sys.mjs";
 
-
-
-var EXPORTED_SYMBOLS = ["RFPHelperParent"];
-
-const { XPCOMUtils } = ChromeUtils.importESModule(
-  "resource://gre/modules/XPCOMUtils.sys.mjs"
-);
 const lazy = {};
 ChromeUtils.defineModuleGetter(
   lazy,
@@ -24,7 +21,7 @@ XPCOMUtils.defineLazyPreferenceGetter(
   false
 );
 
-class RFPHelperParent extends JSWindowActorParent {
+export class RFPHelperParent extends JSWindowActorParent {
   receiveMessage(aMessage) {
     if (
       lazy.isLetterboxingEnabled &&
