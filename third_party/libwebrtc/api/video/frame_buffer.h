9073b92f4fc97def48d052af864ef9366baa03ba
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 00:49:42 +0000

    Bug 1790097 - Vendor libwebrtc from 66fcd16a41
    Upstream commit: https://webrtc.googlesource.com/src/+/66fcd16a416d755377cca39f9703970fdbc3e026
        FrameBuffer::InsertFrame returns true on successful insertion
    
        This is cleaner than checking the size before and after, as is currently
        done in FrameBufferProxy
    
        Bug: webrtc:14168
        Change-Id: Iac896ddf7b1b0b8513159451de7cd8a10668a49a
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/265663
        Reviewed-by: Niels Moller <nissewebrtc.org>
        Reviewed-by: Philip Eliasson <philipelwebrtc.org>
        Commit-Queue: Evan Shrubsole <eshrwebrtc.org>
        Reviewed-by: Erik Språng <sprangwebrtc.org>
        Cr-Commit-Position: refs/heads/main{#37222}
    
    UltraBlame original commit: d8f7b651cd8a49c6d375bd4cd08d8622dba0a91a

--- third_party/libwebrtc/api/video/frame_buffer.h
+++ third_party/libwebrtc/api/video/frame_buffer.h
@@ -18,7 +18,6 @@
 #include "absl/container/inlined_vector.h"
 #include "absl/types/optional.h"
 #include "api/field_trials_view.h"
-#include "api/units/timestamp.h"
 #include "api/video/encoded_frame.h"
 #include "modules/video_coding/utility/decoded_frames_history.h"
 
@@ -49,7 +48,9 @@ class FrameBuffer {
 
   
   
-  void InsertFrame(std::unique_ptr<EncodedFrame> frame);
+  
+  
+  bool InsertFrame(std::unique_ptr<EncodedFrame> frame);
 
   
   
