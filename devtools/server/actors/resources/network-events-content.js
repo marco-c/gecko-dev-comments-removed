b3c9487ea6baf7292612449026fcad5a36c5107b
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 22:09:28 +0000

    Bug 1710401 - [devtools] Stop using resourceOverrides for content process network requests r=ochameau
    Differential Revision: https://phabricator.services.mozilla.com/D174120
    
    UltraBlame original commit: 69a9791568998414e64516b365ba3e2e076f9195

--- devtools/server/actors/resources/network-events-content.js
+++ devtools/server/actors/resources/network-events-content.js
@@ -38,9 +38,7 @@ class NetworkEventContentWatcher {
 
   async watch(targetActor, { onAvailable, onUpdated }) {
     
-    
-    
-    this._imageCacheURIs = new Map();
+    this.networkEvents = new Map();
 
     this.targetActor = targetActor;
     this.onAvailable = onAvailable;
@@ -63,7 +61,7 @@ class NetworkEventContentWatcher {
 
 
   clear() {
-    this._imageCacheURIs.clear();
+    this.networkEvents.clear();
   }
 
   httpFailedOpeningRequest(subject, topic) {
@@ -88,7 +86,6 @@ class NetworkEventContentWatcher {
       networkEventOptions: {
         blockedReason: channel.loadInfo.requestBlockingReason,
       },
-      resourceOverrides: null,
     });
   }
 
@@ -111,33 +108,25 @@ class NetworkEventContentWatcher {
     }
 
     
-    const hasURI = Array.from(this._imageCacheURIs.values()).some(
-      uri => uri === channel.URI.spec
+    const hasURI = Array.from(this.networkEvents.values()).some(
+      networkEvent => networkEvent.uri === channel.URI.spec
     );
 
     if (hasURI) {
       return;
     }
 
-    this._imageCacheURIs.set(channel.channelId, channel.URI.spec);
-
     this.onNetworkEventAvailable(channel, {
       networkEventOptions: { fromCache: true },
-      resourceOverrides: {
-        status: 200,
-        statusText: "OK",
-        totalTime: 0,
-        mimeType: channel.contentType,
-        contentSize: channel.contentLength,
-      },
     });
   }
 
-  onNetworkEventAvailable(channel, { networkEventOptions, resourceOverrides }) {
+  onNetworkEventAvailable(channel, { networkEventOptions }) {
     const actor = new NetworkEventActor(
       this.targetActor.conn,
       this.targetActor.sessionContext,
       {
+        onNetworkEventUpdate: this.onNetworkEventUpdate.bind(this),
         onNetworkEventDestroy: this.onNetworkEventDestroyed.bind(this),
       },
       networkEventOptions,
@@ -147,32 +136,116 @@ class NetworkEventContentWatcher {
 
     const resource = actor.asResource();
 
-    
-    if (resourceOverrides) {
-      for (const prop in resourceOverrides) {
-        resource[prop] = resourceOverrides[prop];
-      }
-    }
+    const networkEvent = {
+      browsingContextID: resource.browsingContextID,
+      innerWindowId: resource.innerWindowId,
+      resourceId: resource.resourceId,
+      resourceType: resource.resourceType,
+      receivedUpdates: [],
+      resourceUpdates: {
+        
+        
+        
+        
+        requestCookiesAvailable: true,
+        requestHeadersAvailable: true,
+      },
+      uri: channel.URI.spec,
+    };
+    this.networkEvents.set(resource.resourceId, networkEvent);
 
     this.onAvailable([resource]);
+    const isBlocked = !!resource.blockedReason;
+    if (isBlocked) {
+      this._emitUpdate(networkEvent);
+    } else {
+      actor.addResponseStart({ channel, fromCache: true });
+      actor.addEventTimings(
+        0 ,
+        {} ,
+        {} 
+      );
+      actor.addResponseContent(
+        {
+          mimeType: channel.contentType,
+          size: channel.contentLength,
+          text: "",
+          transferredSize: 0,
+        },
+        {}
+      );
+    }
+  }
 
+  onNetworkEventUpdate(updateResource) {
+    const networkEvent = this.networkEvents.get(updateResource.resourceId);
+
+    if (!networkEvent) {
+      return;
+    }
+
+    const { resourceUpdates, receivedUpdates } = networkEvent;
+
+    switch (updateResource.updateType) {
+      case "responseStart":
+        
+        
+        
+        let statusText = updateResource.statusText;
+        if (!statusText && updateResource.status === "200") {
+          statusText = "OK";
+        }
+        resourceUpdates.httpVersion = updateResource.httpVersion;
+        resourceUpdates.status = updateResource.status;
+        resourceUpdates.statusText = statusText;
+        resourceUpdates.remoteAddress = updateResource.remoteAddress;
+        resourceUpdates.remotePort = updateResource.remotePort;
+        resourceUpdates.waitingTime = updateResource.waitingTime;
+
+        resourceUpdates.responseHeadersAvailable = true;
+        resourceUpdates.responseCookiesAvailable = true;
+        break;
+      case "responseContent":
+        resourceUpdates.contentSize = updateResource.contentSize;
+        resourceUpdates.mimeType = updateResource.mimeType;
+        resourceUpdates.transferredSize = updateResource.transferredSize;
+        break;
+      case "eventTimings":
+        resourceUpdates.totalTime = updateResource.totalTime;
+        break;
+    }
+
+    resourceUpdates[`${updateResource.updateType}Available`] = true;
+    receivedUpdates.push(updateResource.updateType);
+
+    
+    
+    
+    const isComplete =
+      receivedUpdates.includes("responseStart") &&
+      receivedUpdates.includes("responseContent") &&
+      receivedUpdates.includes("eventTimings");
+
+    if (isComplete) {
+      this._emitUpdate(networkEvent);
+    }
+  }
+
+  _emitUpdate(networkEvent) {
     this.onUpdated([
       {
-        browsingContextID: resource.browsingContextID,
-        innerWindowId: resource.innerWindowId,
-        resourceType: resource.resourceType,
-        resourceId: resource.resourceId,
-        resourceUpdates: {
-          requestCookiesAvailable: true,
-          requestHeadersAvailable: true,
-        },
+        resourceType: networkEvent.resourceType,
+        resourceId: networkEvent.resourceId,
+        resourceUpdates: networkEvent.resourceUpdates,
+        browsingContextID: networkEvent.browsingContextID,
+        innerWindowId: networkEvent.innerWindowId,
       },
     ]);
   }
 
   onNetworkEventDestroyed(channelId) {
-    if (this._imageCacheURIs.has(channelId)) {
-      this._imageCacheURIs.delete(channelId);
+    if (this.networkEvents.has(channelId)) {
+      this.networkEvents.delete(channelId);
     }
   }
 
