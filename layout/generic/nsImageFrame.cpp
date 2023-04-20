9c10d3c0dcd97cf972ab1cf4e581415c19826c8b
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 03:41:43 +0000

    Bug 1771867 - Early Hints Phase 2 - Part 7: Pass early hint preload to image preloader r=necko-reviewers,valentin
    Differential Revision: https://phabricator.services.mozilla.com/D161177
    
    UltraBlame original commit: d33ccbbf407dcaeaef1ca8c42b41fd66d851c959

--- layout/generic/nsImageFrame.cpp
+++ layout/generic/nsImageFrame.cpp
@@ -2692,7 +2692,7 @@ nsresult nsImageFrame::LoadIcon(const nsAString& aSpec,
       loadFlags, nullptr, contentPolicyType, u""_ns,
       false, 
       false, 
-      aRequest);
+      0, aRequest);
 }
 
 void nsImageFrame::GetDocumentCharacterSet(nsACString& aCharset) const {
