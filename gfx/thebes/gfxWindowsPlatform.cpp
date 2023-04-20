dacc8966805bb5367f86e486c3201c0e0ce53eca
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 11:44:14 +0000

    Bug 1799258 - Do color-management on Windows+DComp via IDCompositionFilterEffects. r=sotaro
    + Add gfx.color_management.rec709_gamma_as_srgb:true. :'(
    
    In particular, rec709(16/255) -> srgb(31/255). Even though it's
    technically correct, it's practically-speaking incorrect, since that's
    not what Chrome does, nor what the web expected for years and years.
    
    In practice, basically everyone expects gamma to just be completely
    ignored.
    
    What people expect:
    * Pretend gamut is srgb(==rec709), but stretch this naively for the
      display. If you have a display-p3-gamut display, srgb:0.5 expects to
      be displayed as display:0.5, which will be display-p3:0.5 to the eyes.
    * Pretend all content gammas (TFs) are srgb(!=rec790), and then bitcast this
      naively for the display. E.g. rec709(16/255) should
      display the same as srgb(16/255), not srgb(31/255). (Note: display-p3
      uses srgb gamma) But if your display has e.g. gamma=3.0, don't
      convert or compensate.
    
    This is a formalization of what you get when you spend decades ignoring
    color management, and people build things based on behavior-in-practice,
    not behavior-in-theory.
    
    Also:
    + gfx.color_management.native_srgb:true for Windows, so we don't use the
      display color profile, which no one else does.
    + Add rec2020_gamma_as_rec709, so we have a path towards maybe having
      rec2020 use its correct transfer function, rather than srgb (like
      rec709).
    
    Differential Revision: https://phabricator.services.mozilla.com/D161857
    
    UltraBlame original commit: b4b4977857c7805b6e8c3599ce2e0b34badb655a

--- gfx/thebes/gfxWindowsPlatform.cpp
+++ gfx/thebes/gfxWindowsPlatform.cpp
@@ -1036,15 +1036,20 @@ nsTArray<uint8_t> gfxWindowsPlatform::GetPlatformCMSOutputProfileData() {
     return result;
   }
 
-  if (!mCachedOutputColorProfile.IsEmpty()) {
-    return mCachedOutputColorProfile.Clone();
-  }
+  return GetPlatformCMSOutputProfileData_Impl();
+}
 
-  mCachedOutputColorProfile = [&] {
-    nsTArray<uint8_t> prefProfileData = GetPrefCMSOutputProfileData();
+nsTArray<uint8_t> gfxWindowsPlatform::GetPlatformCMSOutputProfileData_Impl() {
+  static nsTArray<uint8_t> sCached = [&] {
+    
+    nsTArray<uint8_t> prefProfileData =
+        gfxPlatform::GetPrefCMSOutputProfileData();
     if (!prefProfileData.IsEmpty()) {
       return prefProfileData;
     }
+
+    
+    
 
     HDC dc = ::GetDC(nullptr);
     if (!dc) {
@@ -1078,7 +1083,7 @@ nsTArray<uint8_t> gfxWindowsPlatform::GetPlatformCMSOutputProfileData() {
     return result;
   }();
 
-  return mCachedOutputColorProfile.Clone();
+  return sCached.Clone();
 }
 
 void gfxWindowsPlatform::GetDLLVersion(char16ptr_t aDLLPath,
