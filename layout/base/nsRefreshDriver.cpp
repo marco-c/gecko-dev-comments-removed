45de5b1b8ff3be6d405d114bfb392ae9cf7658de
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 09:29:19 +0000

    Bug 1809126 - Remove moz-fonttable protocol. r=jfkthame,necko-reviewers
    We only use it to generate a dummy URI for SVG-in-Opentype documents. We
    don't really need the URIs to be unique or anything in practice.
    
    I noticed this code while looking at the load flags set up for
    bug 1809006.
    
    Differential Revision: https://phabricator.services.mozilla.com/D166291
    
    UltraBlame original commit: 2e58811445f578489cea715b410a5b415d3abf13

--- layout/base/nsRefreshDriver.cpp
+++ layout/base/nsRefreshDriver.cpp
@@ -40,7 +40,6 @@
 #include "mozilla/InputTaskManager.h"
 #include "mozilla/IntegerRange.h"
 #include "mozilla/PresShell.h"
-#include "mozilla/dom/FontTableURIProtocolHandler.h"
 #include "nsITimer.h"
 #include "nsLayoutUtils.h"
 #include "nsPresContext.h"
@@ -1738,8 +1737,7 @@ void nsRefreshDriver::EnsureTimerStarted(EnsureTimerStartedFlags aFlags) {
     
     
     
-    nsIURI* uri = mPresContext->Document()->GetDocumentURI();
-    if (!uri || !mozilla::dom::IsFontTableURI(uri)) {
+    if (!mPresContext->Document()->IsSVGGlyphsDocument()) {
       MOZ_ASSERT(!mActiveTimer,
                  "image doc refresh driver should never have its own timer");
       return;
