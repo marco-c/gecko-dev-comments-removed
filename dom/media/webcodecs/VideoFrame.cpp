6eebb27de7ef105fb44bdea1bb19611ddb3a3c40
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 23:25:44 +0000

    Bug 1826910 - Move VideoFrameData ctor from .h to .cpp r=padenot
    mozilla::layers::Image is forward declared in header file. However, the
    ctor implementation in header needs a complete type instead of a forward
    declaration to make build success. Moving ctor from header file to
    implementation file should solve the problem.
    
    Differential Revision: https://phabricator.services.mozilla.com/D174935
    
    UltraBlame original commit: 2bd6516ae777393f2a5e2e6e04788325af91a918

--- dom/media/webcodecs/VideoFrame.cpp
+++ dom/media/webcodecs/VideoFrame.cpp
@@ -1179,6 +1179,35 @@ InitializeFrameFromOtherFrame(nsIGlobalObject* aGlobal, VideoFrameData&& aData,
 
 
 
+VideoFrameData::VideoFrameData(layers::Image* aImage,
+                               const VideoPixelFormat& aFormat,
+                               gfx::IntRect aVisibleRect,
+                               gfx::IntSize aDisplaySize,
+                               Maybe<uint64_t> aDuration, int64_t aTimestamp,
+                               const VideoColorSpaceInit& aColorSpace)
+    : mImage(aImage),
+      mFormat(aFormat),
+      mVisibleRect(aVisibleRect),
+      mDisplaySize(aDisplaySize),
+      mDuration(aDuration),
+      mTimestamp(aTimestamp),
+      mColorSpace(aColorSpace) {}
+
+VideoFrameSerializedData::VideoFrameSerializedData(
+    layers::Image* aImage, const VideoPixelFormat& aFormat,
+    gfx::IntSize aCodedSize, gfx::IntRect aVisibleRect,
+    gfx::IntSize aDisplaySize, Maybe<uint64_t> aDuration, int64_t aTimestamp,
+    const VideoColorSpaceInit& aColorSpace,
+    already_AddRefed<nsIURI> aPrincipalURI)
+    : VideoFrameData(aImage, aFormat, aVisibleRect, aDisplaySize, aDuration,
+                     aTimestamp, aColorSpace),
+      mCodedSize(aCodedSize),
+      mPrincipalURI(aPrincipalURI) {}
+
+
+
+
+
 VideoFrame::VideoFrame(nsIGlobalObject* aParent,
                        const RefPtr<layers::Image>& aImage,
                        const VideoPixelFormat& aFormat, gfx::IntSize aCodedSize,
