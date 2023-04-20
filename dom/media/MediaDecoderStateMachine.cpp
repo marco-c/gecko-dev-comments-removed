3b98f964fc69f52a7d7faa5d5c164411be2f1d08
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 02:21:35 +0000

    Bug 1799826 - ensure no further sample request when transitioning from looping decoding state to the completed state. r=padenot
    I assume one possible situation of causing crash is that the looping
    state is transitioning to the completed state due to both tracks can't
    get new data after reaching EOS [1].
    
    In this case, we should also finish queue and cancal any ongoing
    sample request in order not to trigger EOS again after entering
    the completed state.
    
    [1]
    https://searchfox.org/mozilla-central/rev/650c19c96529eb28d081062c1ca274bc50ef3635/dom/media/MediaDecoderStateMachine.cpp#3048
    
    Differential Revision: https://phabricator.services.mozilla.com/D162676
    
    UltraBlame original commit: 7e51ce1f158188f67b39c23e517958ffe70aa456

--- dom/media/MediaDecoderStateMachine.cpp
+++ dom/media/MediaDecoderStateMachine.cpp
@@ -1408,7 +1408,8 @@ class MediaDecoderStateMachine::LoopingDecodingState
     
     return mAudioDataRequest.Exists() || mAudioSeekRequest.Exists() ||
            ShouldDiscardLoopedData(MediaData::Type::AUDIO_DATA) ||
-           IsDataWaitingForTimestampAdjustment(MediaData::Type::AUDIO_DATA);
+           IsDataWaitingForTimestampAdjustment(MediaData::Type::AUDIO_DATA) ||
+           mIsReachingAudioEOS;
   }
 
   bool HasDecodedLastVideoFrame() const {
@@ -1416,7 +1417,8 @@ class MediaDecoderStateMachine::LoopingDecodingState
     
     return mVideoDataRequest.Exists() || mVideoSeekRequest.Exists() ||
            ShouldDiscardLoopedData(MediaData::Type::VIDEO_DATA) ||
-           IsDataWaitingForTimestampAdjustment(MediaData::Type::VIDEO_DATA);
+           IsDataWaitingForTimestampAdjustment(MediaData::Type::VIDEO_DATA) ||
+           mIsReachingVideoEOS;
   }
 
   bool ShouldStopPrerolling() const override {
