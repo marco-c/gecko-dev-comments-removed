f3ecbd7ebaa6b6455e1a33b3f727cc8c813652b5
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 23:59:57 +0000

    Bug 1289186 - wait for the server certificate to verify successfully before asking for a client auth certificate r=jschanck
    If a TLS server asks for a client authentication certificate, no dialog asking
    the user to select one should be shown until the server's certificate verifies
    successfully.
    
    Differential Revision: https://phabricator.services.mozilla.com/D175170
    
    UltraBlame original commit: 95f234678970d141c612fd1880bd5a94c61ab6c4

--- security/manager/ssl/tests/mochitest/browser/browser_clientAuth_connection.js
+++ security/manager/ssl/tests/mochitest/browser/browser_clientAuth_connection.js
@@ -171,8 +171,11 @@ add_setup(async function() {
 
 
 
+
+
 async function testHelper(
   prefValue,
+  urlToNavigate,
   expectedURL,
   expectCallingChooseCertificate,
   options = undefined,
@@ -185,10 +188,7 @@ async function testHelper(
 
   let win = await BrowserTestUtils.openNewBrowserWindow(options);
 
-  BrowserTestUtils.loadURIString(
-    win.gBrowser.selectedBrowser,
-    "https://requireclientcert.example.com:443"
-  );
+  BrowserTestUtils.loadURIString(win.gBrowser.selectedBrowser, urlToNavigate);
   if (expectedURL) {
     await BrowserTestUtils.browserLoaded(
       win.gBrowser.selectedBrowser,
@@ -250,6 +250,7 @@ add_task(async function testCertChosenAutomatically() {
   await testHelper(
     "Select Automatically",
     "https://requireclientcert.example.com/",
+    "https://requireclientcert.example.com/",
     false
   );
   
@@ -263,6 +264,7 @@ add_task(async function testCertNotChosenByUser() {
   gClientAuthDialogs.state = DialogState.RETURN_CERT_NOT_SELECTED;
   await testHelper(
     "Ask Every Time",
+    "https://requireclientcert.example.com/",
     undefined,
     true,
     undefined,
@@ -280,6 +282,7 @@ add_task(async function testCertChosenByUser() {
   await testHelper(
     "Ask Every Time",
     "https://requireclientcert.example.com/",
+    "https://requireclientcert.example.com/",
     true
   );
   cars.clearRememberedDecisions();
@@ -289,8 +292,18 @@ add_task(async function testCertChosenByUser() {
 add_task(async function testEmptyCertChosenByUser() {
   gClientAuthDialogs.state = DialogState.RETURN_CERT_NOT_SELECTED;
   gClientAuthDialogs.rememberClientAuthCertificate = true;
-  await testHelper("Ask Every Time", undefined, true);
-  await testHelper("Ask Every Time", undefined, false);
+  await testHelper(
+    "Ask Every Time",
+    "https://requireclientcert.example.com/",
+    undefined,
+    true
+  );
+  await testHelper(
+    "Ask Every Time",
+    "https://requireclientcert.example.com/",
+    undefined,
+    false
+  );
   cars.clearRememberedDecisions();
 });
 
@@ -308,13 +321,6 @@ add_task(async function testClearPrivateBrowsingState() {
   await testHelper(
     "Ask Every Time",
     "https://requireclientcert.example.com/",
-    true,
-    {
-      private: true,
-    }
-  );
-  await testHelper(
-    "Ask Every Time",
     "https://requireclientcert.example.com/",
     true,
     {
@@ -323,6 +329,16 @@ add_task(async function testClearPrivateBrowsingState() {
   );
   await testHelper(
     "Ask Every Time",
+    "https://requireclientcert.example.com/",
+    "https://requireclientcert.example.com/",
+    true,
+    {
+      private: true,
+    }
+  );
+  await testHelper(
+    "Ask Every Time",
+    "https://requireclientcert.example.com/",
     "https://requireclientcert.example.com/",
     true
   );
@@ -359,6 +375,7 @@ add_task(async function testCertFilteringWithIntermediate() {
   await testHelper(
     "Ask Every Time",
     "https://requireclientcert.example.com/",
+    "https://requireclientcert.example.com/",
     true
   );
   cars.clearRememberedDecisions();
@@ -366,4 +383,19 @@ add_task(async function testCertFilteringWithIntermediate() {
   await SpecialPowers.pushPrefEnv({
     set: [["security.enterprise_roots.enabled", true]],
   });
+});
+
+
+
+add_task(async function testNoDialogForUntrustedServerCertificate() {
+  gClientAuthDialogs.state = DialogState.ASSERT_NOT_CALLED;
+  await testHelper(
+    "Ask Every Time",
+    "https://requireclientcert-untrusted.example.com/",
+    undefined,
+    false
+  );
+  
+  
+  cars.clearRememberedDecisions();
 });
