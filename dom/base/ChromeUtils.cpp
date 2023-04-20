1c509a1b31d5e2d78c22e0cf5542f5103919663d
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 09:09:43 +0000

    Backed out changeset c04e43c43057 (bug 1806501) for mochitest failures on test_aboutmemory.xhtml. CLOSED TREE
    UltraBlame original commit: 67ae2ed2e2912d9d74694c46f02e828752a6dbef

--- dom/base/ChromeUtils.cpp
+++ dom/base/ChromeUtils.cpp
@@ -6,7 +6,6 @@
 
 #include "ChromeUtils.h"
 
-#include "JSOracleParent.h"
 #include "js/CharacterEncoding.h"
 #include "js/Object.h"              
 #include "js/PropertyAndElement.h"  
@@ -1763,9 +1762,4 @@ bool ChromeUtils::IsDarkBackground(GlobalObject&, Element& aElement) {
 
 double ChromeUtils::DateNow(GlobalObject&) { return JS_Now() / 1000.0; }
 
-
-void ChromeUtils::EnsureJSOracleStarted(GlobalObject&) {
-  JSOracleParent::WithJSOracle(
-      [](JSOracleParent* aParent) { MOZ_DIAGNOSTIC_ASSERT(aParent); });
-}
 }  
