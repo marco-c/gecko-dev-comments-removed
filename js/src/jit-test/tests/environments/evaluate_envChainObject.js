aa9b9cd0630932185f2a90dfbd079b0aa0d5e95a
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 12:06:40 +0000

    Backed out changeset 3dd4a5d6bef6 (bug 1814880) for causing SM bustages CLOSED TREE
    UltraBlame original commit: ab1ca2678cfe79a8080924c835e86a4d94e604ab

--- js/src/jit-test/tests/environments/evaluate_envChainObject.js
+++ js/src/jit-test/tests/environments/evaluate_envChainObject.js
@@ -1,5 +1,3 @@
-load(libdir + "asserts.js");
-
 
 {
     let global = newGlobal();
@@ -14,12 +12,3 @@ load(libdir + "asserts.js");
     assertEq(envChainObject.a, "test2");
     assertEq(evaluate("a", { global, envChainObject }), "test2");
 }
-
-
-assertThrowsInstanceOf(() => {
-    evaluate("10", { envChainObject: evalcx("") });
-}, Error);
-
-assertThrowsInstanceOf(() => {
-    evaluate("10", { envChainObject: evalReturningScope("1") });
-}, Error);
