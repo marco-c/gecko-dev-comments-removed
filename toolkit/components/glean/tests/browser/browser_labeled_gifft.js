45b39a66e212494d320265aefdd7f193b3792660
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 18:07:31 +0000

    Bug 1819258 - Clear storage before beginning test to appease test-verify r=TravisLong
    Differential Revision: https://phabricator.services.mozilla.com/D171354
    
    UltraBlame original commit: 5857f68ee7a24668b0a3f8acc17383b67483e58d

--- toolkit/components/glean/tests/browser/browser_labeled_gifft.js
+++ toolkit/components/glean/tests/browser/browser_labeled_gifft.js
@@ -9,6 +9,10 @@ function keyedScalarValue(aScalarName) {
 }
 
 add_task(async () => {
+  
+  Services.fog.testResetFOG();
+  Services.telemetry.clearScalars();
+
   Assert.equal(
     undefined,
     Glean.testOnlyIpc.aLabeledCounter.a_label.testGetValue(),
