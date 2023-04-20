67b069cf8ffa543808bc0cde0be343b3573d6aca
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 15:57:05 +0000

    Bug 1812154 - Vendor libwebrtc from 2d0ba28e25
    Upstream commit: https://webrtc.googlesource.com/src/+/2d0ba28e2521ab1ff907b98f6bb12272b4346823
        Audio stack traces
    
        Bug: webrtc:0
        Change-Id: I90ea6301f02c2ebe72711ddbeda0bf000a6873aa
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/276940
        Auto-Submit: Olga Sharonova <olkawebrtc.org>
        Commit-Queue: Henrik Andreassson <henrikawebrtc.org>
        Reviewed-by: Henrik Andreassson <henrikawebrtc.org>
        Cr-Commit-Position: refs/heads/main{#38223}
    
    UltraBlame original commit: 5a1eeb1db700ed71cbfa85221f39ed6fcdb627aa

--- third_party/libwebrtc/modules/audio_device/audio_device_buffer.cc
+++ third_party/libwebrtc/modules/audio_device/audio_device_buffer.cc
@@ -20,6 +20,7 @@
 #include "rtc_base/checks.h"
 #include "rtc_base/logging.h"
 #include "rtc_base/time_utils.h"
+#include "rtc_base/trace_event.h"
 #include "system_wrappers/include/metrics.h"
 
 namespace webrtc {
@@ -297,6 +298,9 @@ int32_t AudioDeviceBuffer::DeliverRecordedData() {
 }
 
 int32_t AudioDeviceBuffer::RequestPlayoutData(size_t samples_per_channel) {
+  TRACE_EVENT1("webrtc", "AudioDeviceBuffer::RequestPlayoutData",
+               "samples_per_channel", samples_per_channel);
+
   
   
   
