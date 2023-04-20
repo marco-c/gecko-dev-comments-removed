8f43f131a81739f7911e3903d73339411773b775
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 22:34:36 +0000

    Bug 1691658 - block http redirects to data: protocol, r=necko-reviewers,ckerschb,valentin
    This also adds a network.allow_redirect_to_data pref in case we need to turn
    this back on for compat reasons.
    
    Differential Revision: https://phabricator.services.mozilla.com/D173988
    
    UltraBlame original commit: 8bab888ea4d4e2a5b55dab0fc92b934360417996

--- netwerk/test/unit/test_redirect_different-protocol.js
+++ netwerk/test/unit/test_redirect_different-protocol.js
@@ -37,6 +37,9 @@ function finish_test(request, buffer) {
 }
 
 function run_test() {
+  
+  Services.prefs.setBoolPref("network.allow_redirect_to_data", true);
+
   httpServer = new HttpServer();
   httpServer.registerPathHandler(randomPath, redirectHandler);
   httpServer.start(-1);
--- netwerk/test/unit/test_redirect_protocol_telemetry.js
+++ netwerk/test/unit/test_redirect_protocol_telemetry.js
@@ -14,6 +14,9 @@ add_task(async function check_protocols() {
     true
   );
 
+  
+  Services.prefs.setBoolPref("network.allow_redirect_to_data", true);
+
   let httpserv = new HttpServer();
   httpserv.registerPathHandler("/redirect", redirectHandler);
   httpserv.registerPathHandler("/content", contentHandler);
