b1dce925503c3e6281e952a4f0aac878bfe45398
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 11:09:56 +0000

    Bug 646323 - Rewrite mfbt/Casting.h assertion in modern style, and teach it to deal with floating point values. r=kinetik
    This now uses `if constexpr (...)` which is a lot more readable, and still
    compiles to almost no assembly instructions, as expected.
    
    Floating point casting assert when casting an integer that's too large to be
    represented exactly as a floating point (e.g. UINT64_MAX to double, since double
    have less than 64 bytes of mantissa), or when casting a double that's too large
    to be represented in a float.
    
    Differential Revision: https://phabricator.services.mozilla.com/D167955
    
    UltraBlame original commit: 8bf5abdd5b4f269e05e566ec901d8e0f4c6383a1

--- mfbt/Casting.h
+++ mfbt/Casting.h
@@ -1,10 +1,10 @@
-/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
-/* vim: set ts=8 sts=2 et sw=2 tw=80: */
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-/* Cast operations to supplement the built-in casting operations. */
+
+
+
+
+
+
 
 #ifndef mozilla_Casting_h
 #define mozilla_Casting_h
@@ -12,39 +12,40 @@
 #include "mozilla/Assertions.h"
 
 #include <cstring>
-#include <limits.h>
 #include <type_traits>
+#include <limits>
+#include <cmath>
 
 namespace mozilla {
 
-/**
- * Sets the outparam value of type |To| with the same underlying bit pattern of
- * |aFrom|.
- *
- * |To| and |From| must be types of the same size; be careful of cross-platform
- * size differences, or this might fail to compile on some but not all
- * platforms.
- *
- * There is also a variant that returns the value directly.  In most cases, the
- * two variants should be identical.  However, in the specific case of x86
- * chips, the behavior differs: returning floating-point values directly is done
- * through the x87 stack, and x87 loads and stores turn signaling NaNs into
- * quiet NaNs... silently.  Returning floating-point values via outparam,
- * however, is done entirely within the SSE registers when SSE2 floating-point
- * is enabled in the compiler, which has semantics-preserving behavior you would
- * expect.
- *
- * If preserving the distinction between signaling NaNs and quiet NaNs is
- * important to you, you should use the outparam version.  In all other cases,
- * you should use the direct return version.
- */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 template <typename To, typename From>
 inline void BitwiseCast(const From aFrom, To* aResult) {
   static_assert(sizeof(From) == sizeof(To),
                 "To and From must have the same size");
 
-  // We could maybe downgrade these to std::is_trivially_copyable, but the
-  // various STLs we use don't all provide it.
+  
+  
   static_assert(std::is_trivial<From>::value,
                 "shouldn't bitwise-copy a type having non-trivial "
                 "initialization");
@@ -65,160 +66,138 @@ inline To BitwiseCast(const From aFrom) {
 
 namespace detail {
 
-enum ToSignedness { ToIsSigned, ToIsUnsigned };
-enum FromSignedness { FromIsSigned, FromIsUnsigned };
-
-template <typename From, typename To,
-          FromSignedness =
-              std::is_signed_v<From> ? FromIsSigned : FromIsUnsigned,
-          ToSignedness = std::is_signed_v<To> ? ToIsSigned : ToIsUnsigned>
-struct BoundsCheckImpl;
-
-// Implicit conversions on operands to binary operations make this all a bit
-// hard to verify.  Attempt to ease the pain below by *only* comparing values
-// that are obviously the same type (and will undergo no further conversions),
-// even when it's not strictly necessary, for explicitness.
-
-enum UUComparison { FromIsBigger, FromIsNotBigger };
-
-// Unsigned-to-unsigned range check
-
-template <typename From, typename To,
-          UUComparison =
-              (sizeof(From) > sizeof(To)) ? FromIsBigger : FromIsNotBigger>
-struct UnsignedUnsignedCheck;
-
-template <typename From, typename To>
-struct UnsignedUnsignedCheck<From, To, FromIsBigger> {
- public:
-  static bool checkBounds(const From aFrom) { return aFrom <= From(To(-1)); }
-};
-
-template <typename From, typename To>
-struct UnsignedUnsignedCheck<From, To, FromIsNotBigger> {
- public:
-  static bool checkBounds(const From aFrom) { return true; }
-};
-
-template <typename From, typename To>
-struct BoundsCheckImpl<From, To, FromIsUnsigned, ToIsUnsigned> {
- public:
-  static bool checkBounds(const From aFrom) {
-    return UnsignedUnsignedCheck<From, To>::checkBounds(aFrom);
-  }
-};
-
-// Signed-to-unsigned range check
-
-template <typename From, typename To>
-struct BoundsCheckImpl<From, To, FromIsSigned, ToIsUnsigned> {
- public:
-  static bool checkBounds(const From aFrom) {
-    if (aFrom < 0) {
-      return false;
-    }
-    if (sizeof(To) >= sizeof(From)) {
-      return true;
-    }
-    return aFrom <= From(To(-1));
-  }
-};
-
-// Unsigned-to-signed range check
-
-enum USComparison { FromIsSmaller, FromIsNotSmaller };
-
-template <typename From, typename To,
-          USComparison =
-              (sizeof(From) < sizeof(To)) ? FromIsSmaller : FromIsNotSmaller>
-struct UnsignedSignedCheck;
-
-template <typename From, typename To>
-struct UnsignedSignedCheck<From, To, FromIsSmaller> {
- public:
-  static bool checkBounds(const From aFrom) { return true; }
-};
-
-template <typename From, typename To>
-struct UnsignedSignedCheck<From, To, FromIsNotSmaller> {
- public:
-  static bool checkBounds(const From aFrom) {
-    const To MaxValue = To((1ULL << (CHAR_BIT * sizeof(To) - 1)) - 1);
-    return aFrom <= From(MaxValue);
-  }
-};
-
-template <typename From, typename To>
-struct BoundsCheckImpl<From, To, FromIsUnsigned, ToIsSigned> {
- public:
-  static bool checkBounds(const From aFrom) {
-    return UnsignedSignedCheck<From, To>::checkBounds(aFrom);
-  }
-};
-
-// Signed-to-signed range check
-
-template <typename From, typename To>
-struct BoundsCheckImpl<From, To, FromIsSigned, ToIsSigned> {
- public:
-  static bool checkBounds(const From aFrom) {
-    if (sizeof(From) <= sizeof(To)) {
-      return true;
-    }
-    const To MaxValue = To((1ULL << (CHAR_BIT * sizeof(To) - 1)) - 1);
-    const To MinValue = -MaxValue - To(1);
-    return From(MinValue) <= aFrom && From(aFrom) <= From(MaxValue);
-  }
-};
-
-template <typename From, typename To,
-          bool TypesAreIntegral =
-              std::is_integral_v<From>&& std::is_integral_v<To>>
-class BoundsChecker;
-
-template <typename From>
-class BoundsChecker<From, From, true> {
- public:
-  static bool checkBounds(const From aFrom) { return true; }
-};
-
-template <typename From, typename To>
-class BoundsChecker<From, To, true> {
- public:
-  static bool checkBounds(const From aFrom) {
-    return BoundsCheckImpl<From, To>::checkBounds(aFrom);
-  }
-};
-
-template <typename From, typename To>
-inline bool IsInBounds(const From aFrom) {
-  return BoundsChecker<From, To>::checkBounds(aFrom);
+template <typename T>
+constexpr int64_t safe_integer() {
+  static_assert(std::is_floating_point_v<T>);
+  return std::pow(2, std::numeric_limits<T>::digits);
 }
 
-}  // namespace detail
+template <typename T>
+constexpr uint64_t safe_integer_unsigned() {
+  static_assert(std::is_floating_point_v<T>);
+  return std::pow(2, std::numeric_limits<T>::digits);
+}
 
-/**
- * Cast a value of integral type |From| to a value of integral type |To|,
- * asserting that the cast will be a safe cast per C++ (that is, that |to| is in
- * the range of values permitted for the type |From|).
- */
+
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-but-set-variable"
+template <typename In, typename Out>
+bool IsInBounds(In aIn) {
+  constexpr bool inSigned = std::is_signed_v<In>;
+  constexpr bool outSigned = std::is_signed_v<Out>;
+  constexpr bool bothSigned = inSigned && outSigned;
+  constexpr bool bothUnsigned = !inSigned && !outSigned;
+  constexpr bool inFloat = std::is_floating_point_v<In>;
+  constexpr bool outFloat = std::is_floating_point_v<Out>;
+  constexpr bool bothFloat = inFloat && outFloat;
+  constexpr bool noneFloat = !inFloat && !outFloat;
+  constexpr Out outMax = std::numeric_limits<Out>::max();
+  constexpr Out outMin = std::numeric_limits<Out>::lowest();
+
+  
+  using select_widest = std::conditional_t<(sizeof(In) > sizeof(Out)), In, Out>;
+
+  if constexpr (bothFloat) {
+    if (aIn > select_widest(outMax) || aIn < select_widest(outMin)) {
+      return false;
+    }
+  }
+  
+  if constexpr (inFloat && !outFloat) {
+    static_assert(sizeof(aIn) <= sizeof(int64_t));
+    
+    
+    
+    if (aIn < static_cast<double>(outMin) ||
+        aIn > static_cast<double>(outMax)) {
+      return false;
+    }
+    
+    
+    if (outSigned) {
+      int64_t asInteger = static_cast<int64_t>(aIn);
+      if (asInteger < outMin || asInteger > outMax) {
+        return false;
+      }
+    } else {
+      uint64_t asInteger = static_cast<uint64_t>(aIn);
+      if (asInteger < 0 || asInteger > outMax) {
+        return false;
+      }
+    }
+  }
+
+  
+  
+  if constexpr (!inFloat && outFloat) {
+    if constexpr (inSigned) {
+      if (aIn < -safe_integer<Out>() || aIn > safe_integer<Out>()) {
+        return false;
+      }
+    } else {
+      if (aIn >= safe_integer_unsigned<Out>()) {
+        return false;
+      }
+    }
+  }
+
+  if constexpr (noneFloat) {
+    if constexpr (bothUnsigned) {
+      if (aIn > select_widest(outMax)) {
+        return false;
+      }
+    }
+    if constexpr (bothSigned) {
+      if (aIn > select_widest(outMax) || aIn < select_widest(outMin)) {
+        return false;
+      }
+    }
+    if constexpr (inSigned && !outSigned) {
+      if (aIn < 0 || std::make_unsigned_t<In>(aIn) > outMax) {
+        return false;
+      }
+    }
+    if constexpr (!inSigned && outSigned) {
+      if (aIn > select_widest(outMax)) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+#pragma GCC diagnostic pop
+
+}  
+
+
+
+
+
+
+
+
 template <typename To, typename From>
 inline To AssertedCast(const From aFrom) {
+  static_assert(std::is_arithmetic_v<To> && std::is_arithmetic_v<From>);
   MOZ_ASSERT((detail::IsInBounds<From, To>(aFrom)));
   return static_cast<To>(aFrom);
 }
 
-/**
- * Cast a value of integral type |From| to a value of integral type |To|,
- * release asserting that the cast will be a safe cast per C++ (that is, that
- * |to| is in the range of values permitted for the type |From|).
- */
+
+
+
+
+
+
+
 template <typename To, typename From>
 inline To ReleaseAssertedCast(const From aFrom) {
+  static_assert(std::is_arithmetic_v<To> && std::is_arithmetic_v<From>);
   MOZ_RELEASE_ASSERT((detail::IsInBounds<From, To>(aFrom)));
   return static_cast<To>(aFrom);
 }
 
-}  // namespace mozilla
+}  
 
-#endif /* mozilla_Casting_h */
+#endif 
