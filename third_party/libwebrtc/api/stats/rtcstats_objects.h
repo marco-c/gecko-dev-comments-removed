21b1d96395cd6a4d412cb2664ff15c309fc32702
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 16:03:29 +0000

    Bug 1812154 - Vendor libwebrtc from c57a28c46b
    Upstream commit: https://webrtc.googlesource.com/src/+/c57a28c46bd2b831ed870f1c8d9f54b7d0b68399
        Move pause and freeze metrics to standardized location.
    
        These metrics were recently standardized. Part of the standardization
        effort was to move them from obsolete "track" stats (on track for
        deprecation and removal: https://crbug.com/webrtc/14175) into the
        "inbound-rtp" stats which are not deprecated.
    
        To ease transition for downstream projects, the metrics are temporarily
        duplicated in both the old and new locations. In a follow-up CL, they
        will be deleted from "track".
    
        Bug: webrtc:14521
        Change-Id: I0d9036472607a8c717ec823a458a79a49ddb80c7
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/278080
        Reviewed-by: Harald Alvestrand <htawebrtc.org>
        Commit-Queue: Henrik Boström <hboswebrtc.org>
        Cr-Commit-Position: refs/heads/main{#38308}
    
    UltraBlame original commit: 7aba09eb041bb37a5796c0bb4477f8eb240f7c1b

--- third_party/libwebrtc/api/stats/rtcstats_objects.h
+++ third_party/libwebrtc/api/stats/rtcstats_objects.h
@@ -357,12 +357,13 @@ class RTC_EXPORT RTCMediaStreamTrackStats final : public RTCStats {
   RTCNonStandardStatsMember<double> total_interruption_duration;
   
   
+  RTCNonStandardStatsMember<double> total_frames_duration;
+  RTCNonStandardStatsMember<double> sum_squared_frame_durations;
+  
   RTCNonStandardStatsMember<uint32_t> freeze_count;
   RTCNonStandardStatsMember<uint32_t> pause_count;
   RTCNonStandardStatsMember<double> total_freezes_duration;
   RTCNonStandardStatsMember<double> total_pauses_duration;
-  RTCNonStandardStatsMember<double> total_frames_duration;
-  RTCNonStandardStatsMember<double> sum_squared_frame_durations;
 };
 
 
@@ -485,6 +486,10 @@ class RTC_EXPORT RTCInboundRTPStreamStats final
   RTCNonStandardStatsMember<uint32_t> frames_assembled_from_multiple_packets;
   RTCStatsMember<double> total_inter_frame_delay;
   RTCStatsMember<double> total_squared_inter_frame_delay;
+  RTCStatsMember<uint32_t> pause_count;
+  RTCStatsMember<double> total_pauses_duration;
+  RTCStatsMember<uint32_t> freeze_count;
+  RTCStatsMember<double> total_freezes_duration;
   
   RTCStatsMember<std::string> content_type;
   
