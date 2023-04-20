ef3ea6e9eec1f0152ebcf28044a5a3494593fd9e
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 06:47:16 +0000

    Bug 1807402 - Fix two tests in ice_unittest.cpp r=webrtc-reviewers,bwc
    Set ICE_POLICY_NO_HOST in each test to ensure that an srflx candidate is
    not falsely discarded.
    
    Differential Revision: https://phabricator.services.mozilla.com/D165492
    
    UltraBlame original commit: 77bba94bb7604e30061a79ba936c053b38304252

--- dom/media/webrtc/transport/test/ice_unittest.cpp
+++ dom/media/webrtc/transport/test/ice_unittest.cpp
@@ -2142,10 +2142,21 @@ TEST_F(WebRtcIceGatherTest, TestGatherDNSStunServerIpAddress) {
     return;
   }
 
-  NrIceCtx::GlobalConfig config;
-  config.mTcpEnabled = false;
-  NrIceCtx::InitializeGlobals(config);
-  EnsurePeer();
+  {
+    NrIceCtx::GlobalConfig config;
+    config.mTcpEnabled = false;
+    NrIceCtx::InitializeGlobals(config);
+  }
+
+  
+  
+  
+  
+  
+  NrIceCtx::Config config;
+  config.mPolicy = NrIceCtx::ICE_POLICY_NO_HOST;
+  peer_ = MakeUnique<IceTestPeer>("P1", test_utils_, true, config);
+
   peer_->SetStunServer(stun_server_address_, kDefaultStunServerPort);
   peer_->SetDNSResolver();
   peer_->AddStream(1);
@@ -2181,10 +2192,21 @@ TEST_F(WebRtcIceGatherTest, TestGatherDNSStunServerHostname) {
     return;
   }
 
-  NrIceCtx::GlobalConfig config;
-  config.mTcpEnabled = false;
-  NrIceCtx::InitializeGlobals(config);
-  EnsurePeer();
+  {
+    NrIceCtx::GlobalConfig config;
+    config.mTcpEnabled = false;
+    NrIceCtx::InitializeGlobals(config);
+  }
+
+  
+  
+  
+  
+  
+  NrIceCtx::Config config;
+  config.mPolicy = NrIceCtx::ICE_POLICY_NO_HOST;
+  peer_ = MakeUnique<IceTestPeer>("P1", test_utils_, true, config);
+
   peer_->SetStunServer(stun_server_hostname_, kDefaultStunServerPort);
   peer_->SetDNSResolver();
   peer_->AddStream(1);
