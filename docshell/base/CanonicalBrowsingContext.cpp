b2b7f0fdea2c9eefd5fce689aa4edf0608896c15
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 11:53:27 +0000

    Bug 1814603 - Change more ContentParent::IsDead checks to ContentParent::IsShuttingDown. r=smaug
    Differential Revision: https://phabricator.services.mozilla.com/D168995
    
    UltraBlame original commit: 7d42e9474afc64f804988a5c88bd8814519618a8

--- docshell/base/CanonicalBrowsingContext.cpp
+++ docshell/base/CanonicalBrowsingContext.cpp
@@ -1611,7 +1611,7 @@ nsresult CanonicalBrowsingContext::PendingRemotenessChange::FinishTopContent() {
                         "non-remote iframes");
 
   
-  if (mContentParent && NS_WARN_IF(mContentParent->IsDead())) {
+  if (mContentParent && NS_WARN_IF(mContentParent->IsShuttingDown())) {
     return NS_ERROR_FAILURE;
   }
 
@@ -1741,7 +1741,7 @@ nsresult CanonicalBrowsingContext::PendingRemotenessChange::FinishSubframe() {
   
   
   if (mContentParent != embedderBrowser->Manager() &&
-      NS_WARN_IF(mContentParent->IsDead())) {
+      NS_WARN_IF(mContentParent->IsShuttingDown())) {
     return NS_ERROR_FAILURE;
   }
 
