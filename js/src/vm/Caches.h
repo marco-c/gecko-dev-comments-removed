30cdd8740a5dc6d4592132e4bce6a2079511f48b
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 22:47:03 +0000

    Bug 1826510: Evaluate hash-shift at compile time. r=dthayer
    `mozilla::FloorLog2` can't be used in `constexpr` expressions (, it's incorrectly
    marked as `constexpr`), but we can instead use `mozilla::tl::FloorLog2`.
    
    Differential Revision: https://phabricator.services.mozilla.com/D174720
    
    UltraBlame original commit: 0557c59248ada3dbb291656f1a84a6cbb36ccc23

--- js/src/vm/Caches.h
+++ js/src/vm/Caches.h
@@ -8,14 +8,17 @@
 #define vm_Caches_h
 
 #include "mozilla/Array.h"
+#include "mozilla/MathAlgorithms.h"
 #include "mozilla/Maybe.h"
 #include "mozilla/MruCache.h"
+#include "mozilla/TemplateLib.h"
 
 #include "frontend/ScopeBindingCache.h"
 #include "gc/Tracer.h"
 #include "js/RootingAPI.h"
 #include "js/TypeDecls.h"
 #include "vm/JSScript.h"
+#include "vm/Shape.h"
 #include "vm/StencilCache.h"  
 #include "vm/StringType.h"
 
@@ -181,10 +184,15 @@ class MegamorphicCache {
   using Entry = MegamorphicCacheEntry;
 
   static constexpr size_t NumEntries = 1024;
-  
-  static constexpr uint8_t ShapeHashShift1 = 3;
-  
-  static constexpr uint8_t ShapeHashShift2 = ShapeHashShift1 + 10;
+  static constexpr uint8_t ShapeHashShift1 =
+      mozilla::tl::FloorLog2<alignof(Shape)>::value;
+  static constexpr uint8_t ShapeHashShift2 =
+      ShapeHashShift1 + mozilla::tl::FloorLog2<NumEntries>::value;
+
+  static_assert(mozilla::IsPowerOfTwo(alignof(Shape)) &&
+                    mozilla::IsPowerOfTwo(NumEntries),
+                "FloorLog2 is exact because alignof(Shape) and NumEntries are "
+                "both powers of two");
 
  private:
   mozilla::Array<Entry, NumEntries> entries_;
@@ -299,10 +307,15 @@ class MegamorphicSetPropCache {
   
   
   static constexpr size_t NumEntries = 256;
-  
-  static constexpr uint8_t ShapeHashShift1 = 3;
-  
-  static constexpr uint8_t ShapeHashShift2 = ShapeHashShift1 + 8;
+  static constexpr uint8_t ShapeHashShift1 =
+      mozilla::tl::FloorLog2<alignof(Shape)>::value;
+  static constexpr uint8_t ShapeHashShift2 =
+      ShapeHashShift1 + mozilla::tl::FloorLog2<NumEntries>::value;
+
+  static_assert(mozilla::IsPowerOfTwo(alignof(Shape)) &&
+                    mozilla::IsPowerOfTwo(NumEntries),
+                "FloorLog2 is exact because alignof(Shape) and NumEntries are "
+                "both powers of two");
 
  private:
   mozilla::Array<Entry, NumEntries> entries_;
