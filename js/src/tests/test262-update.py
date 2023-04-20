342047e3363d7a7aabefe4727e3ebf0395ccf699
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 08:09:41 +0000

    Bug 1808795 - Enable fromAsync tests r=arai
    Differential Revision: https://phabricator.services.mozilla.com/D166189
    
    UltraBlame original commit: 274b82009138df48707f565b282fc7a99a563c14

--- js/src/tests/test262-update.py
+++ js/src/tests/test262-update.py
@@ -34,7 +34,6 @@ UNSUPPORTED_FEATURES = set(
         "String.prototype.isWellFormed",  
         "String.prototype.toWellFormed",  
         "symbols-as-weakmap-keys",  
-        "Array.fromAsync",  
     ]
 )
 FEATURE_CHECK_NEEDED = {
@@ -55,6 +54,7 @@ SHELL_OPTIONS = {
     "ShadowRealm": "--enable-shadow-realms",
     "array-grouping": "--enable-array-grouping",
     "change-array-by-copy": "--enable-change-array-by-copy",
+    "Array.fromAsync": "--enable-array-from-async",
 }
 
 
