b7cb72a65f45088e9bfad6c9141c21108a1ff4f8
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 02:39:37 +0000

    Bug 1744687 - Part 2. Use windows backend for screen.orientation.lock(). r=smaug
    Since I have added Windows backend for lock orientation by Part 1. fix, I
    would like to use it in `screen.orientation.lock()`.
    
    Since `dom.screenorientation.allow-lock` is still false on all platfroms even
    if this fix is applied, orientation lock isn't turned on as default.
    
    If lock orientation isn't supported in the device, the backend will throw
    `NS_ERROR_DOM_NOT_SUPPORTED_ERR`.
    
    Depends on D162451
    
    Differential Revision: https://phabricator.services.mozilla.com/D162452
    
    UltraBlame original commit: cb4c463e552fac5ad4ccbc450e0da719d4839fc6

--- dom/base/ScreenOrientation.cpp
+++ dom/base/ScreenOrientation.cpp
@@ -331,7 +331,7 @@ already_AddRefed<Promise> ScreenOrientation::LockInternal(
     return nullptr;
   }
 
-#if !defined(MOZ_WIDGET_ANDROID)
+#if !defined(MOZ_WIDGET_ANDROID) && !defined(XP_WIN)
   
   p->MaybeReject(NS_ERROR_DOM_NOT_SUPPORTED_ERR);
   return p.forget();
