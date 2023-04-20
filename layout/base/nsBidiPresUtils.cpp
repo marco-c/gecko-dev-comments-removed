2004b8a2428dfe9b9c45579e78336490ed705bf9
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 19:23:31 +0000

    Bug 1821735 part 2: Drop 'Get' prefix from 'nsPresContext::GetBidiEngine' since it's infallible. r=jfkthame
    For pointer-type getters, we have a convention that a "Get" prefix implies
    "warning, this might return null" , whereas lack-of-a-"Get" implies that the
    return value is guaranteed to be non-null.
    
    In the case of this `GetBidiEngine` API, this convention doesn't directly apply
    since the return value isn't pointer-typed. But it's still backed by a
    dynamically-allocated resource which we dereference at the return statement,
    and some of the callers take the pointer-value of what's returned and pass that
    along elsewhere. So it's helpful (and more concise) to drop "Get" to convey
    that yes, we're sure the returned thing is indeed a real object.
    
    Differential Revision: https://phabricator.services.mozilla.com/D172301
    
    UltraBlame original commit: 83f53ddd26e8dc35c7523a9281b0ee1f67419c10

--- layout/base/nsBidiPresUtils.cpp
+++ layout/base/nsBidiPresUtils.cpp
@@ -343,9 +343,7 @@ struct MOZ_STACK_CLASS BidiParagraphData {
   }
 
   nsresult SetPara() {
-    if (mPresContext->GetBidiEngine()
-            .SetParagraph(mBuffer, mParaLevel)
-            .isErr()) {
+    if (mPresContext->BidiEngine().SetParagraph(mBuffer, mParaLevel).isErr()) {
       return NS_ERROR_FAILURE;
     };
     return NS_OK;
@@ -362,17 +360,17 @@ struct MOZ_STACK_CLASS BidiParagraphData {
     BidiEmbeddingLevel paraLevel = mParaLevel;
     if (paraLevel == BidiEmbeddingLevel::DefaultLTR() ||
         paraLevel == BidiEmbeddingLevel::DefaultRTL()) {
-      paraLevel = mPresContext->GetBidiEngine().GetParagraphEmbeddingLevel();
+      paraLevel = mPresContext->BidiEngine().GetParagraphEmbeddingLevel();
     }
     return paraLevel;
   }
 
   BidiEngine::ParagraphDirection GetParagraphDirection() {
-    return mPresContext->GetBidiEngine().GetParagraphDirection();
+    return mPresContext->BidiEngine().GetParagraphDirection();
   }
 
   nsresult CountRuns(int32_t* runCount) {
-    auto result = mPresContext->GetBidiEngine().CountRuns();
+    auto result = mPresContext->BidiEngine().CountRuns();
     if (result.isErr()) {
       return NS_ERROR_FAILURE;
     }
@@ -382,8 +380,8 @@ struct MOZ_STACK_CLASS BidiParagraphData {
 
   void GetLogicalRun(int32_t aLogicalStart, int32_t* aLogicalLimit,
                      BidiEmbeddingLevel* aLevel) {
-    mPresContext->GetBidiEngine().GetLogicalRun(aLogicalStart, aLogicalLimit,
-                                                aLevel);
+    mPresContext->BidiEngine().GetLogicalRun(aLogicalStart, aLogicalLimit,
+                                             aLevel);
     if (mIsVisual) {
       *aLevel = GetParagraphEmbeddingLevel();
     }
@@ -2476,7 +2474,7 @@ nsresult nsBidiPresUtils::ProcessTextForRenderingContext(
   text.ReplaceChar(kSeparators, ' ');
   return ProcessText(text.BeginReading(), text.Length(), aBaseLevel,
                      aPresContext, processor, aMode, aPosResolve,
-                     aPosResolveCount, aWidth, &aPresContext->GetBidiEngine());
+                     aPosResolveCount, aWidth, &aPresContext->BidiEngine());
 }
 
 
