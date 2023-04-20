ed868019c604cbf08c1d116747adebedda6a1869
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 20:27:49 +0000

    Backed out changeset 6dc913a054e9 (bug 1822521) for bc failures on browser_parsable_css.js. CLOSED TREE
    UltraBlame original commit: 25ee1980e1ff8d96c60fb86ec5c57409b6d57f10

--- servo/components/style/gecko/media_features.rs
+++ servo/components/style/gecko/media_features.rs
@@ -643,7 +643,7 @@ macro_rules! bool_pref_feature {
 
 
 
-pub static MEDIA_FEATURES: [QueryFeatureDescription; 65] = [
+pub static MEDIA_FEATURES: [QueryFeatureDescription; 64] = [
     feature!(
         atom!("width"),
         AllowsRanges::Yes,
@@ -956,10 +956,5 @@ pub static MEDIA_FEATURES: [QueryFeatureDescription; 65] = [
     bool_pref_feature!(
         atom!("-moz-mathml-core-ms"),
         "mathml.ms_lquote_rquote_attributes.disabled"
-    ),
-    
-    bool_pref_feature!(
-        atom!("-moz-popover-enabled"),
-        "dom.element.popover.enabled"
     ),
 ];
