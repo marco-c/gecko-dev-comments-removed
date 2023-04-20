c4ba9d4566dae0cee63bf29c425a6dca07bd66a0
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 06:55:00 +0000

    Backed out changeset ddf5f22cd611 (bug 1806072) for blocking Android Components from being updated in Fenix nightly. CLOSED TREE
    UltraBlame original commit: 23f995f7972e3c98a1a7d2426f9052e1692b2e42

--- mobile/android/geckoview/src/main/java/org/mozilla/geckoview/GeckoView.java
+++ mobile/android/geckoview/src/main/java/org/mozilla/geckoview/GeckoView.java
@@ -635,9 +635,7 @@ public class GeckoView extends FrameLayout {
     if (mSession != null) {
       final GeckoRuntime runtime = mSession.getRuntime();
       if (runtime != null) {
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR1
-            || Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-          
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR1) {
           
           
           
