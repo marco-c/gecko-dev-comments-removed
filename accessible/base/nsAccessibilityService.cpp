9361918b4647aa088decd0a8f1f9f5d0d0a13096
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 08:14:56 +0000

    Bug 1793941: Don't fire EVENT_TABLE_STYLING_CHANGED when the cache is enabled r=Jamie
    Differential Revision: https://phabricator.services.mozilla.com/D166117
    
    UltraBlame original commit: f5ee2900f8e3eb44477d38a406e4be55b755d65a

--- accessible/base/nsAccessibilityService.cpp
+++ accessible/base/nsAccessibilityService.cpp
@@ -573,8 +573,13 @@ void nsAccessibilityService::TableLayoutGuessMaybeChanged(
   if (DocAccessible* document = GetDocAccessible(aPresShell)) {
     if (LocalAccessible* acc = document->GetAccessible(aContent)) {
       if (LocalAccessible* table = nsAccUtils::TableFor(acc)) {
-        document->FireDelayedEvent(
-            nsIAccessibleEvent::EVENT_TABLE_STYLING_CHANGED, table);
+        if (!StaticPrefs::accessibility_cache_enabled_AtStartup()) {
+          
+          
+          
+          document->FireDelayedEvent(
+              nsIAccessibleEvent::EVENT_TABLE_STYLING_CHANGED, table);
+        }
         document->QueueCacheUpdate(table, CacheDomain::Table);
       }
     }
