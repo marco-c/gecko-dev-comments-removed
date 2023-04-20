0cb653d7266ef31a10d737b8efbdc98ed25292f6
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 22:07:13 +0000

    Bug 1824346 - Only uninstall temporary add-ons. r=jdescottes,robwu,devtools-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D173696
    
    UltraBlame original commit: a1b3b4fb4c40ac09a47baf72bd593fa7441746c0

--- devtools/server/actors/addon/addons.js
+++ devtools/server/actors/addon/addons.js
@@ -69,9 +69,13 @@ class AddonsActor extends Actor {
   async uninstallAddon(addonId) {
     const addon = await AddonManager.getAddonByID(addonId);
 
-    if (addon) {
-      await addon.uninstall();
+    
+    
+    if (!addon?.temporarilyInstalled) {
+      throw new Error(`Could not uninstall add-on "${addonId}"`);
     }
+
+    await addon.uninstall();
   }
 }
 
--- devtools/server/tests/xpcshell/test_webext_apis.js
+++ devtools/server/tests/xpcshell/test_webext_apis.js
@@ -6,6 +6,11 @@
 const { AddonManager } = ChromeUtils.import(
   "resource://gre/modules/AddonManager.jsm"
 );
+const { ExtensionTestUtils } = ChromeUtils.import(
+  "resource://testing-common/ExtensionXPCShellUtils.jsm"
+);
+
+ExtensionTestUtils.init(this);
 
 
 
@@ -120,6 +125,39 @@ add_task(async function test_webext_run_apis() {
     type: "listAddons",
   }));
   equal(addons.length, 0, "expected no add-on installed");
+
+  
+  let error = await sendRequest(transport, {
+    to: getRootResponse.addonsActor,
+    type: "uninstallAddon",
+    addonId,
+  });
+  equal(
+    error?.message,
+    `Could not uninstall add-on "${addonId}"`,
+    "expected error"
+  );
+
+  
+  
+  
+  const id = "not-a-temporary@extension";
+  const extension = ExtensionTestUtils.loadExtension({
+    manifest: {
+      browser_specific_settings: { gecko: { id } },
+    },
+    useAddonManager: "permanent",
+  });
+  await extension.startup();
+
+  error = await sendRequest(transport, {
+    to: getRootResponse.addonsActor,
+    type: "uninstallAddon",
+    addonId: id,
+  });
+  equal(error?.message, `Could not uninstall add-on "${id}"`, "expected error");
+
+  await extension.unload();
 
   transport.close();
 });
