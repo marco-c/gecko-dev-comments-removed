8369a7f1b410473376c95ee2ec9ea5b51ea79460
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 20:39:47 +0000

    Bug 1821954: Make HyperTextAccessible::Char/TextBounds use HyperTextAccessibleBase when the cache is enabled. r=morgan
    This is consistent with other HyperTextAccessible methods such as TextAtOffset.
    It allows us to test the new implementation with LocalAccessible.
    This is especially useful because we have better test coverage in our HyperText tests than we do in our TextLeafPoint tests.
    
    In fact, running the tests with this change uncovered some bugs which I had to fix here.
    
    First, TextLeafPoint::ComputeBoundsFromFrame was preferring to stick to the end of the first frame when on a boundary, rather than using the next frame.
    For example, if we had the text "a\nb" where "b" is split into a continuation, offset 2 "b" was previously using the primary frame instead of the continuation.
    This isn't a problem in HyperTextAccessible::GetBoundsInFrame because it walks to the next continuation if needed, since it is dealing with a range of offsets.
    This is fixed by preferring the beginning of the next frame instead.
    
    Second, passing the text length as the offset (i.e. one past the last character) should return a 0 rect.
    TextLeafPoint::CharBounds needed to be tweaked accordingly.
    
    Third, if TextLeafPoint returns a 0 rect (e.g. because a RemoteAccessible doesn't have a cache yet), HyperTextAccessibleBase needs to respect that and return it untouched.
    
    Differential Revision: https://phabricator.services.mozilla.com/D172533
    
    UltraBlame original commit: 908a298d2fdd967fda1fdcd09a017952c46c4e7a

--- accessible/base/TextLeafRange.cpp
+++ accessible/base/TextLeafRange.cpp
@@ -1484,7 +1484,7 @@ LayoutDeviceIntRect TextLeafPoint::ComputeBoundsFromFrame() const {
   
   
   nsresult rv = frame->GetChildFrameContainingOffset(
-      contentOffset, false, &contentOffsetInFrame, &frame);
+      contentOffset, true, &contentOffsetInFrame, &frame);
   NS_ENSURE_SUCCESS(rv, LayoutDeviceIntRect());
 
   
@@ -1716,7 +1716,7 @@ LayoutDeviceIntRect TextLeafPoint::CharBounds() {
     }
 
     if (mOffset >= 0 &&
-        static_cast<uint32_t>(mOffset) > nsAccUtils::TextLength(local)) {
+        static_cast<uint32_t>(mOffset) >= nsAccUtils::TextLength(local)) {
       NS_ERROR("Wrong in offset");
       return LayoutDeviceIntRect();
     }
--- accessible/generic/HyperTextAccessible.cpp
+++ accessible/generic/HyperTextAccessible.cpp
@@ -1488,6 +1488,14 @@ int32_t HyperTextAccessible::OffsetAtPoint(int32_t aX, int32_t aY,
 LayoutDeviceIntRect HyperTextAccessible::TextBounds(int32_t aStartOffset,
                                                     int32_t aEndOffset,
                                                     uint32_t aCoordType) {
+  if (StaticPrefs::accessibility_cache_enabled_AtStartup()) {
+    
+    
+    
+    return HyperTextAccessibleBase::TextBounds(aStartOffset, aEndOffset,
+                                               aCoordType);
+  }
+
   index_t startOffset = ConvertMagicOffset(aStartOffset);
   index_t endOffset = ConvertMagicOffset(aEndOffset);
   if (!startOffset.IsValid() || !endOffset.IsValid() ||
@@ -1553,6 +1561,22 @@ LayoutDeviceIntRect HyperTextAccessible::TextBounds(int32_t aStartOffset,
   nsAccUtils::ConvertScreenCoordsTo(&boundsX, &boundsY, aCoordType, this);
   bounds.MoveTo(boundsX, boundsY);
   return bounds;
+}
+
+LayoutDeviceIntRect HyperTextAccessible::CharBounds(int32_t aOffset,
+                                                    uint32_t aCoordType) {
+  if (StaticPrefs::accessibility_cache_enabled_AtStartup()) {
+    
+    
+    
+    return HyperTextAccessibleBase::CharBounds(aOffset, aCoordType);
+  }
+
+  index_t startOffset = ConvertMagicOffset(aOffset);
+  int32_t endOffset = startOffset == CharacterCount()
+                          ? static_cast<int32_t>(startOffset)
+                          : static_cast<int32_t>(startOffset) + 1;
+  return TextBounds(static_cast<int32_t>(startOffset), endOffset, aCoordType);
 }
 
 already_AddRefed<EditorBase> HyperTextAccessible::GetEditor() const {
