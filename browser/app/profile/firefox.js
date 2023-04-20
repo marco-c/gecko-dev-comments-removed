31a50839a142e05e67aeeed5bfa214fd85fc3bd7
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 05:56:36 +0000

    Bug 1751763 - Firefox Relay integration r=dimi,flod,mconley,desktop-theme-reviewers,dao,sfoster,geckoview-reviewers,jonalmeida
    Differential Revision: https://phabricator.services.mozilla.com/D158155
    
    UltraBlame original commit: c8ae77a066393f62938020933caceb8f342f41b8

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -2179,9 +2179,18 @@ pref("extensions.pocket.refresh.hideRecentSaves.enabled", false);
 pref("signon.management.page.fileImport.enabled", false);
 
 #ifdef NIGHTLY_BUILD
-pref("signon.management.page.os-auth.enabled", true);
+pref("signon.management.page.os-auth.enabled", false);
+
+
+
+
+
+
+
+pref("signon.firefoxRelay.feature", "not available");
 #else
 pref("signon.management.page.os-auth.enabled", false);
+pref("signon.firefoxRelay.feature", "not available");
 #endif
 pref("signon.management.page.breach-alerts.enabled", true);
 pref("signon.management.page.vulnerable-passwords.enabled", true);
