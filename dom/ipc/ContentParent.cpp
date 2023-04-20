aab770aab108441758a384cc3dc4a0440f70f26e
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 16:26:04 +0000

    Bug 1817166 - Remove unused argument from nsContentUtils::TransferableToIPCTransferable; r=nika
    After Bug 1781129, aChild is no longer needed to allocate Shmem, but aParent is
    still needed to grant access for file in https://searchfox.org/mozilla-central/rev/b579290e6b830d1b3f0a941203b0c0e1e56c42a3/dom/base/nsContentUtils.cpp#8171-8182.
    
    Differential Revision: https://phabricator.services.mozilla.com/D170063
    
    UltraBlame original commit: eda3c11e203cfcdb86421ba7d60d801f1e12e837

--- dom/ipc/ContentParent.cpp
+++ dom/ipc/ContentParent.cpp
@@ -3544,7 +3544,7 @@ mozilla::ipc::IPCResult ContentParent::RecvGetClipboard(
   clipboard->GetData(trans, aWhichClipboard);
 
   nsContentUtils::TransferableToIPCTransferable(
-      trans, aDataTransfer, true , nullptr, this);
+      trans, aDataTransfer, true , this);
   return IPC_OK();
 }
 
@@ -3626,8 +3626,7 @@ mozilla::ipc::IPCResult ContentParent::RecvGetClipboardAsync(
                  GenericPromise::ResolveOrRejectValue&& aValue) {
                IPCDataTransfer ipcDataTransfer;
                nsContentUtils::TransferableToIPCTransferable(
-                   trans, &ipcDataTransfer, false , nullptr,
-                   self);
+                   trans, &ipcDataTransfer, false , self);
                aResolver(std::move(ipcDataTransfer));
              });
   return IPC_OK();
@@ -5438,7 +5437,7 @@ void ContentParent::MaybeInvokeDragSession(BrowserParent* aParent) {
           aParent ? aParent->GetLoadContext() : nullptr;
       nsCOMPtr<nsIArray> transferables = transfer->GetTransferables(lc);
       nsContentUtils::TransferablesToIPCTransferables(
-          transferables, dataTransfers, false, nullptr, this);
+          transferables, dataTransfers, false, this);
       uint32_t action;
       session->GetDragAction(&action);
 
