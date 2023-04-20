4d923f59db75b6c5064b2894f37ba31ce2855fe6
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 09:45:11 +0000

    Bug 1806510 - Vendor libwebrtc from 59020bd88b
    Upstream commit: https://webrtc.googlesource.com/src/+/59020bd88b968df71e3789a4f00355b76cc8b929
        Add AV1 profile-1 video decode support to WebRTC
    
        The Chrome Remote Desktop team is looking to support AV1 profile-1
        w/ I444 for screen sharing however only I420 is currently supported.
    
        This CL adds I444 support for the Dav1dDecoder, which appears to be
        the preferred decoder and adds profile-1 to the
        InternalDecoderFactory when the Dav1dDecoder is being used.
    
        I've tested this CL using a CRD host w/ I444 enabled and it seems to
        work as expected, though I've only tested on a debug build so I plan
        to do some perf testing once this is available in a release build.
    
        Bug: chromium:1329660
        Change-Id: I2b8b7b7fd530727456ac5c46e694e7dbad6deff2
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/273986
        Reviewed-by: Erik Språng <sprangwebrtc.org>
        Reviewed-by: Philip Eliasson <philipelwebrtc.org>
        Commit-Queue: Joe Downing <joedowgoogle.com>
        Cr-Commit-Position: refs/heads/main{#38022}
    
    UltraBlame original commit: 2535490c753014681af0a61193dd94f4c38e0d94

--- third_party/libwebrtc/media/engine/internal_decoder_factory.cc
+++ third_party/libwebrtc/media/engine/internal_decoder_factory.cc
@@ -11,6 +11,7 @@
 #include "media/engine/internal_decoder_factory.h"
 
 #include "absl/strings/match.h"
+#include "api/video_codecs/av1_profile.h"
 #include "api/video_codecs/sdp_video_format.h"
 #include "api/video_codecs/video_codec.h"
 #include "media/base/codec.h"
@@ -50,10 +51,16 @@ std::vector<SdpVideoFormat> InternalDecoderFactory::GetSupportedFormats()
   for (const SdpVideoFormat& h264_format : SupportedH264DecoderCodecs())
     formats.push_back(h264_format);
 
-  
-  
-  
-  
+
+
+
+
+
+
+
+
+
+
 
   return formats;
 }
