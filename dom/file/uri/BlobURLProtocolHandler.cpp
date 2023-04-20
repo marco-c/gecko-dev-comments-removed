884202929386995f00d78e78d56c4661da1605a6
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 03:39:49 +0000

    Bug 1443925 - Part 1: Support running GetBlobURLPrincipal off-main-thread, r=asuth
    This method isn't actually safe to run off-main-thread yet, as nsIPrincipal
    manipulation isn't being made threadsafe until a later part, however it can be
    called off-main-thread when creating a new content principal with a Blob URL
    spec.
    
    Differential Revision: https://phabricator.services.mozilla.com/D163031
    
    UltraBlame original commit: 36d6e57846bc5a11ce9a0c17389a71b9b7794f65

--- dom/file/uri/BlobURLProtocolHandler.cpp
+++ dom/file/uri/BlobURLProtocolHandler.cpp
@@ -881,8 +881,7 @@ bool BlobURLProtocolHandler::GetBlobURLPrincipal(nsIURI* aURI,
     return false;
   }
 
-  MOZ_ASSERT(NS_IsMainThread(),
-             "without locking gDataTable is main-thread only");
+  StaticMutexAutoLock lock(sMutex);
   mozilla::dom::DataInfo* info =
       GetDataInfoFromURI(aURI, true );
   if (!info || info->mObjectType != mozilla::dom::DataInfo::eBlobImpl ||
