1d50d8ae3875dd4abf824cd12b6ff7b17c614f03
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 09:41:11 +0000

    Bug 1806510 - Vendor libwebrtc from 43e11c881b
    UltraBlame original commit: 4c29dcf1cf937eccb1bc3aa4ea3a947a1de9de64

--- third_party/libwebrtc/modules/congestion_controller/goog_cc/probe_controller.h
+++ third_party/libwebrtc/modules/congestion_controller/goog_cc/probe_controller.h
@@ -50,7 +50,13 @@ struct ProbeControllerConfig {
 
   
   FieldTrialParameter<TimeDelta> network_state_estimate_probing_interval;
+  
+  
+  FieldTrialParameter<double> network_state_estimate_fast_rampup_rate;
   FieldTrialParameter<double> network_state_probe_scale;
+  
+  
+  FieldTrialParameter<TimeDelta> network_state_probe_duration;
 
   
   FieldTrialOptional<double> first_allocation_probe_scale;
@@ -142,6 +148,7 @@ class ProbeController {
   DataRate min_bitrate_to_probe_further_ = DataRate::PlusInfinity();
   Timestamp time_last_probing_initiated_ = Timestamp::MinusInfinity();
   DataRate estimated_bitrate_ = DataRate::Zero();
+  bool send_probe_on_next_process_interval_;
   absl::optional<webrtc::NetworkStateEstimate> network_estimate_;
   DataRate start_bitrate_ = DataRate::Zero();
   DataRate max_bitrate_ = DataRate::PlusInfinity();
