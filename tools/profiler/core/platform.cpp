fc2150ffc0b2c5a1fe89934e5ca220c3d577aa04
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 04:54:12 +0000

    Bug 1803751 - Make some marker fields searchable and bump the profile version r=julienw
    Differential Revision: https://phabricator.services.mozilla.com/D163714
    
    UltraBlame original commit: 27f8690ebc506618c0af26f57cecd85adb0669d5

--- tools/profiler/core/platform.cpp
+++ tools/profiler/core/platform.cpp
@@ -2804,7 +2804,7 @@ static void StreamMetaJSCustomObject(
     const PreRecordedMetaInformation& aPreRecordedMetaInformation) {
   MOZ_RELEASE_ASSERT(CorePS::Exists() && ActivePS::Exists(aLock));
 
-  aWriter.IntProperty("version", 25);
+  aWriter.IntProperty("version", 26);
 
   
   
@@ -3055,7 +3055,8 @@ struct JavaMarkerWithDetails {
     schema.SetTooltipLabel("{marker.name}");
     schema.SetChartLabel("{marker.data.name}");
     schema.SetTableLabel("{marker.name} - {marker.data.name}");
-    schema.AddKeyLabelFormat("name", "Details", MS::Format::String);
+    schema.AddKeyLabelFormatSearchable("name", "Details", MS::Format::String,
+                                       MS::Searchable::Searchable);
     return schema;
   }
 };
