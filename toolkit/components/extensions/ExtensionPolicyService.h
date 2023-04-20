7e5a7beffecda838ec98b3f00f3137018723e12b
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 03:40:15 +0000

    Bug 1443925 - Part 8: Make WebExtensionPolicy::IsRestrictedURI threadsafe, r=extension-reviewers,kmag
    This migrates the restricted domain preference handling to be performed on the
    ExtensionPolicyService, wrapping it in an RWLock, making it threadsafe to
    interact with.
    
    Differential Revision: https://phabricator.services.mozilla.com/D163038
    
    UltraBlame original commit: 988661e2839fd4c0859e0dd1137e6daaae547f50

--- toolkit/components/extensions/ExtensionPolicyService.h
+++ toolkit/components/extensions/ExtensionPolicyService.h
@@ -56,6 +56,11 @@ class ExtensionPolicyService final : public nsIAddonPolicyService,
 
   static ExtensionPolicyService& GetSingleton();
 
+  
+  
+  
+  static RefPtr<extensions::AtomSet> RestrictedDomains();
+
   static already_AddRefed<ExtensionPolicyService> GetInstance() {
     return do_AddRef(&GetSingleton());
   }
@@ -113,6 +118,8 @@ class ExtensionPolicyService final : public nsIAddonPolicyService,
   RefPtr<dom::Promise> ExecuteContentScripts(
       JSContext* aCx, nsPIDOMWindowInner* aWindow,
       const nsTArray<RefPtr<extensions::WebExtensionContentScript>>& aScripts);
+
+  void UpdateRestrictedDomains();
 
   nsRefPtrHashtable<nsPtrHashKey<const nsAtom>, WebExtensionPolicy> mExtensions;
 
