6dbd93f94fc52109288cb4dc81be459c4938704f
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 19:02:58 +0000

    Backed out 5 changesets (bug 1774569) because changes are incompatible with flake8. CLOSED TREE
    Backed out changeset ac6c7441099b (bug 1774569)
    Backed out changeset 1081432f3f97 (bug 1774569)
    Backed out changeset 111d7f2021c3 (bug 1774569)
    Backed out changeset 2cc0b2a54106 (bug 1774569)
    Backed out changeset cb45ff55284c (bug 1774569)
    
    UltraBlame original commit: bc1494d55dcf14d2df4bd2b52e6c9de36a6d4320

--- third_party/python/packaging/packaging/__about__.py
+++ third_party/python/packaging/packaging/__about__.py
@@ -1,6 +1,7 @@
 
 
 
+from __future__ import absolute_import, division, print_function
 
 __all__ = [
     "__title__",
@@ -17,7 +18,7 @@ __title__ = "packaging"
 __summary__ = "Core utilities for Python packages"
 __uri__ = "https://github.com/pypa/packaging"
 
-__version__ = "21.3"
+__version__ = "20.9"
 
 __author__ = "Donald Stufft and individual contributors"
 __email__ = "donald@stufft.io"
