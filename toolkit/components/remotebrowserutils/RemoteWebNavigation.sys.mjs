f685bb7ee616d0045442469e091eab4c6c78f96b
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 21:22:33 +0000

    Bug 1823719 - Convert toolkit/components/remotebrowserutils to ES modules. r=Standard8.
    Differential Revision: https://phabricator.services.mozilla.com/D173631
    
    UltraBlame original commit: 735b73193dc663078843621b2eeccbc2d4abe328

--- toolkit/components/remotebrowserutils/RemoteWebNavigation.sys.mjs
+++ toolkit/components/remotebrowserutils/RemoteWebNavigation.sys.mjs
@@ -1,15 +1,15 @@
-
-
-
-
+// -*- indent-tabs-mode: nil; js-indent-level: 2 -*-
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 const lazy = {};
 ChromeUtils.defineESModuleGetters(lazy, {
   PrivateBrowsingUtils: "resource://gre/modules/PrivateBrowsingUtils.sys.mjs",
 });
 
-
-class RemoteWebNavigation {
+// This object implements the JS parts of nsIWebNavigation.
+export class RemoteWebNavigation {
   constructor(aBrowser) {
     this._browser = aBrowser;
     this._cancelContentJSEpoch = 1;
@@ -77,18 +77,18 @@ class RemoteWebNavigation {
 
   _speculativeConnect(uri, loadURIOptions) {
     try {
-      
-      
-      
-      
+      // Let's start a network connection before the content process asks.
+      // Note that we might have already set up the speculative connection in
+      // some cases, especially when the url is from location bar or its popup
+      // menu.
       if (uri.schemeIs("http") || uri.schemeIs("https")) {
         let isBrowserPrivate = lazy.PrivateBrowsingUtils.isBrowserPrivate(
           this._browser
         );
         let principal = loadURIOptions.triggeringPrincipal;
-        
-        
-        
+        // We usually have a triggeringPrincipal assigned, but in case we
+        // don't have one or if it's a SystemPrincipal, let's create it with OA
+        // inferred from the current context.
         if (!principal || principal.isSystemPrincipal) {
           let attrs = {
             userContextId: this._browser.getAttribute("usercontextid") || 0,
@@ -102,8 +102,8 @@ class RemoteWebNavigation {
         Services.io.speculativeConnect(uri, principal, null);
       }
     } catch (ex) {
-      
-      
+      // Can't setup speculative connection for this uri for some
+      // reason, just ignore it.
     }
   }
 
@@ -136,9 +136,9 @@ class RemoteWebNavigation {
       uri = Services.uriFixup.getFixupURIInfo(uriString, fixupFlags)
         .preferredURI;
     } catch (ex) {
-      
-      
-      
+      // In rare cases `uriFixup` can throw. We ignore this here, but it's
+      // likely that the fixupAndLoadURIString call below will still throw,
+      // hopefully with more details.
     }
     if (uri) {
       this._speculativeConnect(uri, loadURIOptions);
@@ -148,9 +148,9 @@ class RemoteWebNavigation {
       Ci.nsIRemoteTab.NAVIGATE_URL,
       { uri }
     );
-    
-    
-    
+    // For now, continue to use fixup here, but note that ideally we should be
+    // doing fixup only once and reusing the URI we created above. Addressing
+    // this is bug 1815509.
     this._browser.browsingContext.fixupAndLoadURIString(uriString, {
       ...loadURIOptions,
       cancelContentJSEpoch,
@@ -175,15 +175,15 @@ class RemoteWebNavigation {
     return this._currentURI;
   }
   set currentURI(aURI) {
-    
+    // Bug 1498600 verify usages of systemPrincipal here
     let loadURIOptions = {
       triggeringPrincipal: Services.scriptSecurityManager.getSystemPrincipal(),
     };
     this.loadURI(aURI.spec, loadURIOptions);
   }
 
-  
-  
+  // Bug 1233803 - accessing the sessionHistory of remote browsers should be
+  // done in content scripts.
   get sessionHistory() {
     throw new Components.Exception(
       "Not implemented",
@@ -205,5 +205,3 @@ class RemoteWebNavigation {
     }
   }
 }
-
-var EXPORTED_SYMBOLS = ["RemoteWebNavigation"];
