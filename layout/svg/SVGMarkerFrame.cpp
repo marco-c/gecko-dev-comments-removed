4238683ef1c4f8b7664145f76bba83e404c82cee
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 10:13:45 +0000

    Bug 752638 - make the context-fill and context-stroke SVG paint values work in marker content r=emilio
    The specification is here https://www.w3.org/TR/SVG2/painting.html#TermContextElement
    
    Differential Revision: https://phabricator.services.mozilla.com/D167020
    
    UltraBlame original commit: 9a4dbcc711edd4ef9585e4376020fef0e74fe2df

--- layout/svg/SVGMarkerFrame.cpp
+++ layout/svg/SVGMarkerFrame.cpp
@@ -10,6 +10,7 @@
 
 #include "gfxContext.h"
 #include "mozilla/PresShell.h"
+#include "mozilla/SVGContextPaint.h"
 #include "mozilla/SVGGeometryFrame.h"
 #include "mozilla/SVGObserverUtils.h"
 #include "mozilla/SVGUtils.h"
@@ -133,6 +134,12 @@ void SVGMarkerFrame::PaintMark(gfxContext& aContext,
   ISVGDisplayableFrame* SVGFrame = do_QueryFrame(kid);
   
   SVGFrame->NotifySVGChanged(ISVGDisplayableFrame::TRANSFORM_CHANGED);
+  RefPtr<SVGContextPaintImpl> contextPaint = new SVGContextPaintImpl();
+  contextPaint->Init(aContext.GetDrawTarget(), aContext.CurrentMatrixDouble(),
+                     aMarkedFrame, SVGContextPaint::GetContextPaint(marker),
+                     aImgParams);
+  AutoSetRestoreSVGContextPaint autoSetRestore(contextPaint,
+                                               marker->OwnerDoc());
   SVGUtils::PaintFrameWithEffects(kid, aContext, markTM, aImgParams);
 
   if (StyleDisplay()->IsScrollableOverflow()) aContext.Restore();
