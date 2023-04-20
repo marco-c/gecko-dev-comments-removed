12f52f34852bb2063f2403bea25ffb1f49a4302a
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 03:34:01 +0000

    Bug 1765732 - Use KillHard() in case of failed dispatch (in order to r=smaug
    prevent resource leak). r=smaug
    
    Differential Revision: https://phabricator.services.mozilla.com/D163601
    
    UltraBlame original commit: acbc960161408e749555ceac1ef15d288c6dea6c

--- dom/ipc/ContentParent.cpp
+++ dom/ipc/ContentParent.cpp
@@ -3773,7 +3773,7 @@ ContentParent::BlockShutdown(nsIAsyncShutdownClient* aClient) {
     
     
     if (!ShutDownProcess(SEND_SHUTDOWN_MESSAGE)) {
-      RemoveShutdownBlockers();
+      KillHard("Failed to send Shutdown message. Destroying the process...");
       return NS_OK;
     }
   } else if (IsLaunching()) {
