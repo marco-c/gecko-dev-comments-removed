3e5f2738fe179ed3169de4ee28c194e60490d1ba
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 05:53:57 +0000

    Backed out changeset 3fb9690b38b7 (bug 1804958) for causing reftest failures on 1804958.html CLOSED TREE
    UltraBlame original commit: c925451629727f1f8d3badb05c9473b0634a96ef

--- layout/svg/SVGUtils.cpp
+++ layout/svg/SVGUtils.cpp
@@ -459,9 +459,6 @@ class MixModeBlender {
     
     
     IntRect drawRect = ComputeClipExtsInDeviceSpace(aTransform);
-    if (drawRect.IsEmpty()) {
-      return nullptr;
-    }
 
     RefPtr<DrawTarget> targetDT =
         mSourceCtx->GetDrawTarget()->CreateSimilarDrawTarget(
