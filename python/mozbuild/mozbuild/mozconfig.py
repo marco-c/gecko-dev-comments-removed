ebcf7ccfb38c11cfbfb0cc92f4d9ab0ffd049320
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 09:21:04 +0000

    Bug 1810499 - Prefer the mozillabuild path for mozconfig loading. r=firefox-build-system-reviewers,glandium
    Since we're explicitly calling the mozillabuild shell, it is the most
    reasonable thing to do, much like we do for old configure and other bits
    of the build.
    
    Otherwise, when called from an external msys2 environment, commands like
    `cygpath` might fail to execute because we find them in the caller's
    msys2 install instead of on mozillabuild's install.
    
    This duplicates a bit of code with:
    
      https://searchfox.org/mozilla-central/rev/a8187e40b492dff78e3d3225e652cc06f447484b/build/moz.configure/init.configure#1179
    
    Not sure how to best factor this out / if it's worth it even.
    Suggestions welcome.
    
    Differential Revision: https://phabricator.services.mozilla.com/D166907
    
    UltraBlame original commit: c0718fd3c85e3f51339b0053d36463a34f57d245

--- python/mozbuild/mozbuild/mozconfig.py
+++ python/mozbuild/mozbuild/mozconfig.py
@@ -132,12 +132,21 @@ class MozconfigLoader(object):
         
         
         shell = "sh"
+        env = dict(os.environ)
+        env["PYTHONIOENCODING"] = "utf-8"
+
         if "MOZILLABUILD" in os.environ:
             mozillabuild = os.environ["MOZILLABUILD"]
             if (Path(mozillabuild) / "msys2").exists():
                 shell = mozillabuild + "/msys2/usr/bin/sh"
             else:
                 shell = mozillabuild + "/msys/bin/sh"
+            prefer_mozillabuild_path = [
+                os.path.dirname(shell),
+                str(Path(mozillabuild) / "bin"),
+                env["PATH"],
+            ]
+            env["PATH"] = os.pathsep.join(prefer_mozillabuild_path)
         if sys.platform == "win32":
             shell = shell + ".exe"
 
@@ -151,8 +160,6 @@ class MozconfigLoader(object):
         ]
 
         try:
-            env = dict(os.environ)
-            env["PYTHONIOENCODING"] = "utf-8"
             
             
             output = six.ensure_text(
