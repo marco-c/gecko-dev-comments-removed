b44fe76862cae99168b62f85c23723fa263760f8
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 10:42:04 +0000

    Bug 1813411 - Force-upgrade env_logger to 0.10. r=emilio
    This preemptively avoids the addition of a new version of the crate.
    
    Differential Revision: https://phabricator.services.mozilla.com/D168221
    
    UltraBlame original commit: f973933cb7faffc46d47f0be7c5b0fa99656174a

--- third_party/rust/env_logger/src/fmt/writer/atty.rs
+++ third_party/rust/env_logger/src/fmt/writer/atty.rs
@@ -6,19 +6,20 @@
 
 
 
-
-#[cfg(feature = "atty")]
+#[cfg(feature = "auto-color")]
 mod imp {
+    use is_terminal::IsTerminal;
+
     pub(in crate::fmt) fn is_stdout() -> bool {
-        atty::is(atty::Stream::Stdout)
+        std::io::stdout().is_terminal()
     }
 
     pub(in crate::fmt) fn is_stderr() -> bool {
-        atty::is(atty::Stream::Stderr)
+        std::io::stderr().is_terminal()
     }
 }
 
-#[cfg(not(feature = "atty"))]
+#[cfg(not(feature = "auto-color"))]
 mod imp {
     pub(in crate::fmt) fn is_stdout() -> bool {
         false
--- third_party/rust/env_logger/src/fmt/writer/mod.rs
+++ third_party/rust/env_logger/src/fmt/writer/mod.rs
@@ -165,6 +165,7 @@ impl Builder {
     }
 
     
+    #[allow(clippy::wrong_self_convention)]
     pub(crate) fn is_test(&mut self, is_test: bool) -> &mut Self {
         self.is_test = is_test;
         self
