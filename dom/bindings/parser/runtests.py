0838c05a95ccd5c1e125b2abdd093defe14ba152
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 05:59:31 +0000

    Backed out changeset 02b8e04c764c (bug 1622677) for causing multiple failures related to WebIDL. CLOSED TREE
    UltraBlame original commit: 5cb330f52d5b87326c2c36ada2ca49ab018e1955

--- dom/bindings/parser/runtests.py
+++ dom/bindings/parser/runtests.py
@@ -3,12 +3,11 @@
 
 from __future__ import print_function
 
-import argparse
-import glob
 import os
 import sys
+import glob
+import argparse
 import traceback
-
 import WebIDL
 
 
@@ -61,7 +60,7 @@ class TestHarness(object):
         try:
             parser.parse(code)
             parser.finish()
-        except Exception:
+        except:
             threw = True
 
         self.ok(threw, "Should have thrown: %s" % msg)
--- dom/bindings/parser/tests/test_union.py
+++ dom/bindings/parser/tests/test_union.py
@@ -1,3 +1,5 @@
+import WebIDL
+import itertools
 import string
 
 
@@ -188,7 +190,7 @@ def WebIDLTest(parser, harness):
         try:
             parser.parse(interface)
             results = parser.finish()
-        except Exception:
+        except:
             threw = True
 
         harness.ok(threw, "Should have thrown.")
