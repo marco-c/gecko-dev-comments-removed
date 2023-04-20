3eaf63c6706bf381c187d0c31c2bcc81340720eb
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 19:55:01 +0000

    Bug 1821927 - Convert toolkit/components/tooltiptext to ES modules. r=Standard8
    Differential Revision: https://phabricator.services.mozilla.com/D172515
    
    UltraBlame original commit: e58fc5e4884f9d2e9960e13cc4b3ae1619d83e27

--- toolkit/components/tooltiptext/TooltipTextProvider.sys.mjs
+++ toolkit/components/tooltiptext/TooltipTextProvider.sys.mjs
@@ -1,13 +1,13 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-
-
-
-function TooltipTextProvider() {}
+export function TooltipTextProvider() {}
 
 TooltipTextProvider.prototype = {
   getNodeText(tipElement, textOut, directionOut) {
-    
-    
+    // Don't show the tooltip if the tooltip node is a document or browser.
+    // Caller should ensure the node is in (composed) document.
     if (
       !tipElement ||
       !tipElement.ownerDocument ||
@@ -17,8 +17,8 @@ TooltipTextProvider.prototype = {
     }
 
     var defView = tipElement.ownerGlobal;
-    
-    
+    // XXX Work around bug 350679:
+    // "Tooltips can be fired in documents with no view".
     if (!defView) {
       return false;
     }
@@ -34,8 +34,8 @@ TooltipTextProvider.prototype = {
     var lookingForSVGTitle = true;
     var direction = tipElement.ownerDocument.dir;
 
-    
-    
+    // If the element is invalid per HTML5 Forms specifications and has no title,
+    // show the constraint validation error message.
     if (
       (defView.HTMLInputElement.isInstance(tipElement) ||
         defView.HTMLTextAreaElement.isInstance(tipElement) ||
@@ -44,13 +44,13 @@ TooltipTextProvider.prototype = {
       !tipElement.hasAttribute("title") &&
       (!tipElement.form || !tipElement.form.noValidate)
     ) {
-      
-      
+      // If the element is barred from constraint validation or valid,
+      // the validation message will be the empty string.
       titleText = tipElement.validationMessage || null;
     }
 
-    
-    
+    // If the element is an <input type='file'> without a title, we should show
+    // the current file selection.
     if (
       !titleText &&
       defView.HTMLInputElement.isInstance(tipElement) &&
@@ -71,9 +71,9 @@ TooltipTextProvider.prototype = {
           }
         } else {
           titleText = files[0].name;
-          
-          
-          
+          // For UX and performance (jank) reasons we cap the number of
+          // files that we list in the tooltip to 20 plus a "and xxx more"
+          // line, or to 21 if exactly 21 files were picked.
           const TRUNCATED_FILE_COUNT = 20;
           let count = Math.min(files.length, TRUNCATED_FILE_COUNT);
           for (let i = 1; i < count; ++i) {
@@ -96,8 +96,8 @@ TooltipTextProvider.prototype = {
       } catch (e) {}
     }
 
-    
-    
+    // Check texts against null so that title="" can be used to undefine a
+    // title on a child element.
     let usedTipElement = null;
     while (
       tipElement &&
@@ -149,7 +149,7 @@ TooltipTextProvider.prototype = {
     return [titleText, XLinkTitleText, SVGTitleText, XULtooltiptextText].some(
       function(t) {
         if (t && /\S/.test(t)) {
-          
+          // Make CRLF and CR render one line break each.
           textOut.value = t.replace(/\r\n?/g, "\n");
 
           if (usedTipElement) {
@@ -170,5 +170,3 @@ TooltipTextProvider.prototype = {
   classID: Components.ID("{f376627f-0bbc-47b8-887e-fc92574cc91f}"),
   QueryInterface: ChromeUtils.generateQI(["nsITooltipTextProvider"]),
 };
-
-var EXPORTED_SYMBOLS = ["TooltipTextProvider"];
