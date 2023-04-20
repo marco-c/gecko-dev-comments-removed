b402ca7d0c4c90921667b2d98fba00e84bfbcbc5
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 01:52:19 +0000

    Bug 1791974 - Add set as default (private) search engine options to SearchTestUtils.installSearchExtension. r=mcheang
    and convert existing would-be-broken consumers to use the new format arguments
    
    Differential Revision: https://phabricator.services.mozilla.com/D161044
    
    UltraBlame original commit: 182ff17fa5081df1dc8d4ece076723f66edc21e2

--- browser/components/urlbar/tests/browser/browser_searchMode_engineRemoval.js
+++ browser/components/urlbar/tests/browser/browser_searchMode_engineRemoval.js
@@ -10,7 +10,10 @@
 
 
 add_task(async function activeTab() {
-  let extension = await SearchTestUtils.installSearchExtension({}, true);
+  let extension = await SearchTestUtils.installSearchExtension(
+    {},
+    { skipUnload: true }
+  );
   let engine = Services.search.getEngineByName("Example");
   await Services.search.moveEngine(engine, 0);
 
@@ -32,7 +35,10 @@ add_task(async function activeTab() {
 
 
 add_task(async function backgroundTab() {
-  let extension = await SearchTestUtils.installSearchExtension({}, true);
+  let extension = await SearchTestUtils.installSearchExtension(
+    {},
+    { skipUnload: true }
+  );
   let engine = Services.search.getEngineByName("Example");
   await Services.search.moveEngine(engine, 0);
 
@@ -67,7 +73,10 @@ add_task(async function backgroundTab() {
 
 
 add_task(async function backgroundWindow() {
-  let extension = await SearchTestUtils.installSearchExtension({}, true);
+  let extension = await SearchTestUtils.installSearchExtension(
+    {},
+    { skipUnload: true }
+  );
   let engine = Services.search.getEngineByName("Example");
   await Services.search.moveEngine(engine, 0);
 
