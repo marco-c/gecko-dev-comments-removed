3953f073b3d3a89e7f1b83e107aab35c8417ca07
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 05:50:20 +0000

    Bug 1805545 part 1: Don't query the display style on image map areas when pushing the cache. r=morgan
    See the code comments for an explanation.
    This fixes assertions and crashes when pushing the cache when a page contains a map which is unslotted in a shadow host.
    
    Differential Revision: https://phabricator.services.mozilla.com/D164783
    
    UltraBlame original commit: 8f0a910858ab1b8285223c27113f8eedb1a688ab

--- accessible/generic/LocalAccessible.cpp
+++ accessible/generic/LocalAccessible.cpp
@@ -3820,6 +3820,12 @@ nsAtom* LocalAccessible::TagName() const {
 
 already_AddRefed<nsAtom> LocalAccessible::DisplayStyle() const {
   if (dom::Element* elm = Elm()) {
+    if (elm->IsHTMLElement(nsGkAtoms::area)) {
+      
+      
+      
+      return nullptr;
+    }
     StyleInfo info(elm);
     return info.Display();
   }
