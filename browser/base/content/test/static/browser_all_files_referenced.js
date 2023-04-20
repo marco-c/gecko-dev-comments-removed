d828e1bf767bb3b89de7229a64167d590b28fc1e
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 07:28:00 +0000

    Bug 1808197 - Use a specific UI for pdf.js in GeckoView r=florian,pdfjs-reviewers,marco
    Differential Revision: https://phabricator.services.mozilla.com/D165967
    
    UltraBlame original commit: 2820927daa6b8a22062c349bf123a55011d7d91b

--- browser/base/content/test/static/browser_all_files_referenced.js
+++ browser/base/content/test/static/browser_all_files_referenced.js
@@ -211,10 +211,6 @@ var whitelist = [
   
   { file: "resource://pdf.js/web/debugger.js" },
   { file: "resource://pdf.js/web/debugger.css" },
-  
-  { file: "resource://pdf.js/web/viewer-geckoview.css" },
-  { file: "resource://pdf.js/web/viewer-geckoview.html" },
-  { file: "resource://pdf.js/web/viewer-geckoview.js" },
 
   
   { file: "resource://app/modules/translation/BingTranslator.jsm" },
