3c0e0759ba9a2b6d45216bad0e46d769dbb544cf
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 10:51:53 +0000

    Backed out changeset bc47fb3cd3e3 (bug 1813063) for causing crashtest failures on noindex.mp4. CLOSED TREE
    UltraBlame original commit: de13de97096f9cde8adffe42eb2fcdb504d27cea

--- dom/media/mp4/DecoderData.cpp
+++ dom/media/mp4/DecoderData.cpp
@@ -198,25 +198,20 @@ MediaResult MP4AudioInfo::Update(const Mp4parseTrackInfo* aTrack,
 
     
     MP4SampleIndex::Indice indice = {0};
+    bool rv = aIndices->GetIndice(aIndices->Length() - 1, indice);
     uint64_t mediaFrameCount = 0;
-    if (aIndices) {
-      bool rv = aIndices->GetIndice(aIndices->Length() - 1, indice);
-      if (rv) {
-        
-        
-        
-        
-        mediaFrameCount = static_cast<uint64_t>(indice.end_composition) *
-                          aAudio->sample_info->sample_rate / USECS_PER_S;
-        LOG("AAC stream in MP4 container, total media duration is %" PRIu64
-            " frames",
-            mediaFrameCount);
-      } else {
-        LOG("AAC stream in MP4 container, couldn't determine total media time");
-      }
+    if (rv) {
+      
+      
+      
+      
+      mediaFrameCount = static_cast<uint64_t>(indice.end_composition) *
+                        aAudio->sample_info->sample_rate / USECS_PER_S;
+      LOG("AAC stream in MP4 container, total media duration is %" PRIu64
+          " frames",
+          mediaFrameCount);
     } else {
-      LOG("AAC stream in MP4 container, couldn't determine total media time: "
-          "no index");
+      LOG("AAC stream in MP4 container, couldn't determine total media time");
     }
 
     AacCodecSpecificData aacCodecSpecificData{};
