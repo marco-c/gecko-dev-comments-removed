ab738bbca3a113de05a141b07c3c632f6b5422d9
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 03:13:10 +0000

    Bug 1219128 - Initialize Object and Function constructors when creating globals. r=jandem
    Object and Function prototype initialization is stateful, and mutate the global
    as it is initializing the content of the Global. Attempts at making this process
    transactional was not successful.
    
    Thus, `JSProto_Object` and `JSProto_Function` are now initialized as part of the
    Global creation.
    
    Failures to initialize the global might lead to have a Realm with a partially
    initialized global. Such realm appears to have a valid global using
    `hasLiveGlobal()`. When catching failures, such global might still be used
    despite being partially initialized. A new field `initializingGlobal_` is added
    to `Realm` to track whether the global is live or not. Failures to comply with
    this requirement would cause the garbage collector to reclaim the Realm and
    global in case of failure. This should be avoided during the initialization as
    the global is rooted by the `GlobalObject::new_` function.
    
    Differential Revision: https://phabricator.services.mozilla.com/D163016
    
    UltraBlame original commit: 8ccf33e282b4632bcba7d9912e1db8ab1ac72ce7

--- js/src/vm/Realm-inl.h
+++ js/src/vm/Realm-inl.h
@@ -30,7 +30,7 @@ inline bool JS::Realm::hasLiveGlobal() const {
   
   
   MOZ_ASSERT_IF(global_, !js::gc::IsAboutToBeFinalized(global_));
-  return bool(global_);
+  return bool(global_) && !initializingGlobal_;
 }
 
 inline bool JS::Realm::marked() const {
