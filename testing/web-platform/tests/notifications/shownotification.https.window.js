f9102c29f0550c0dd6961b13c08f571ecb6f75ba
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 15:22:27 +0000

    Backed out 6 changesets (bug 1781588) for causing wpt failures in notifications/event-onclose.https.html CLOSED TREE
    Backed out changeset ae63cba16964 (bug 1781588)
    Backed out changeset 4fbfea4c3705 (bug 1781588)
    Backed out changeset de861b9cc324 (bug 1781588)
    Backed out changeset 91d66b1ac793 (bug 1781588)
    Backed out changeset 7797ce6ecbe2 (bug 1781588)
    Backed out changeset 656d12b072e0 (bug 1781588)
    
    UltraBlame original commit: 631ab23e48ba3e6646c481d41fb12047a5f012b9

--- testing/web-platform/tests/notifications/shownotification.https.window.js
+++ testing/web-platform/tests/notifications/shownotification.https.window.js
@@ -1,7 +1,6 @@
 
 
 
-
 "use strict";
 
 
@@ -16,7 +15,6 @@ function reset() {
 async function registerSw() {
   await reset();
   const reg = await navigator.serviceWorker.register("noop-sw.js");
-  add_completion_callback(() => reg.unregister());
   await navigator.serviceWorker.ready;
   return reg;
 }
@@ -72,35 +70,3 @@ promise_test(async t => {
   const notifications = await registration.getNotifications();
   assert_equals(notifications.length, 3, "Should return three notifications");
 }, "fetching multiple notifications");
-
-
-
-
-promise_test(async t => {
-  t.add_cleanup(cleanup);
-  const another = await navigator.serviceWorker.register("noop-sw.js", { scope: "./scope" });
-  await registration.showNotification("Hello");
-  const notifications = await another.getNotifications();
-  assert_equals(notifications.length, 0, "Should return no notification");
-}, "fetching from another registration")
-
-
-
-
-promise_test(async t => {
-  t.add_cleanup(cleanup);
-  const nonPersistent = new Notification("Non-persistent");
-  t.add_cleanup(() => nonPersistent.close());
-  await registration.showNotification("Hello");
-  const notifications = await registration.getNotifications();
-  assert_equals(notifications.length, 1, "Should return a notification");
-  assert_equals(notifications[0].title, "Hello", "Title should match");
-}, "fetching only persistent notifications")
-
-promise_test(async t => {
-  t.add_cleanup(cleanup);
-  await registration.showNotification("Hello", { data: fakeCustomData });
-  const notifications = await registration.getNotifications();
-  assert_equals(notifications.length, 1, "Should return a notification");
-  assert_custom_data(notifications[0].data);
-}, "fetching a notification with custom data")
