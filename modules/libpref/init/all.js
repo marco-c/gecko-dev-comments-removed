9de11772df8291bdd9a4138716d775c928748639
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 01:40:21 +0000

    Bug 1800703 - Disable `extensions.install_origins.enabled` pref everywhere. r=rpl
    Differential Revision: https://phabricator.services.mozilla.com/D162148
    
    UltraBlame original commit: 9be918c82dfe0d41940fd0633aa76bd4a82abe37

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -1971,6 +1971,8 @@ pref("extensions.manifestV2.actionsPopupURLRestricted", false);
   pref("extensions.openPopupWithoutUserGesture.enabled", false);
 #endif
 
+pref("extensions.install_origins.enabled", false);
+
 
 
 
