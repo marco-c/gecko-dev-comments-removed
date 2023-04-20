6138c7d5a8b1cee66ef0c27f6ed23e13763192dc
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 07:36:54 +0000

    Bug 1808202 - Add babel-plugin-jsm-to-commonjs directly into the tree rather than importing. r=Mardak
    This will allow easier development/changes as we proceed to migrate newtab code to ES modules.
    
    The file was imported from https://github.com/k88hudson/babel-plugin-jsm-to-commonjs/blob/3fefb99f274c26dc743fadeb79153594a9e2b900/plugin.js
    with LICENSE added, and formatted/fixed to ESLint standards.
    
    Differential Revision: https://phabricator.services.mozilla.com/D165819
    
    UltraBlame original commit: 5862825b3e622a9bd71c50850f1b22b103c80a3c

--- browser/components/newtab/karma.mc.config.js
+++ browser/components/newtab/karma.mc.config.js
@@ -261,13 +261,13 @@ module.exports = function(config) {
                   plugins: [
                     
                     [
-                      "jsm-to-commonjs",
+                      "./tools/babel-jsm-to-commonjs.js",
                       {
                         basePath: PATHS.resourcePathRegEx,
                         removeOtherImports: true,
                         replace: true,
                       },
-                    ], 
+                    ],
                     "@babel/plugin-proposal-nullish-coalescing-operator",
                     "@babel/plugin-proposal-optional-chaining",
                     "@babel/plugin-proposal-class-properties",
