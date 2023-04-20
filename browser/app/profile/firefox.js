06d1ee43906e7d62ff62b9f65335dfb34f8f93e7
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 16:29:38 +0000

    Backed out changeset 8107b1fc07e0 (bug 1812083) for causing failures at browser_bookmarks_change_url.js. CLOSED TREE
    UltraBlame original commit: e7ab92dd3181f06a08a2a4332d13cd2f4b883ae7

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1269,11 +1269,7 @@ pref("browser.bookmarks.editDialog.maxRecentFolders", 7);
 
 
 
-#ifdef NIGHTLY_BUILD
-  pref("browser.bookmarks.editDialog.delayedApply.enabled", true);
-#else
-  pref("browser.bookmarks.editDialog.delayedApply.enabled", false);
-#endif
+pref("browser.bookmarks.editDialog.delayedApply.enabled", false);
 
 #if defined(XP_WIN) && defined(MOZ_SANDBOX)
   
