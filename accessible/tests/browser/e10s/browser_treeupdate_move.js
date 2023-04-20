b11a56c747f0501c89bffb9ab4c194cc81a442fa
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 10:54:32 +0000

    Bug 1813256 follow-up: Add test. r=nlapre
    Differential Revision: https://phabricator.services.mozilla.com/D168331
    
    UltraBlame original commit: e42393aed3efe33454a232f06fd307f2c4d2b0d2

--- accessible/tests/browser/e10s/browser_treeupdate_move.js
+++ accessible/tests/browser/e10s/browser_treeupdate_move.js
@@ -62,3 +62,23 @@ addAccessibleTask(
   },
   { chrome: true, topLevel: true, iframe: true, remoteIframe: true }
 );
+
+
+
+
+
+addAccessibleTask(
+  `
+<div id="container">
+  <iframe id="iframe"></iframe>
+  <div aria-owns="iframe"></div>
+</div>
+  `,
+  async function(browser, docAcc) {
+    const container = findAccessibleChildByID(docAcc, "container");
+    testAccessibleTree(container, {
+      SECTION: [{ SECTION: [{ INTERNAL_FRAME: [{ DOCUMENT: [] }] }] }],
+    });
+  },
+  { topLevel: true }
+);
