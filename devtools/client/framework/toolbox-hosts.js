7d5a9dcb763657173fb854a0b93cc499a10fa720
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 20:18:41 +0000

    Bug 1822578 - Make flex="1" on XUL set a zero flex basis like the flex shorthand does. r=Gijs,mconley,settings-reviewers,desktop-theme-reviewers,dao
    In a setup with:
    
    <hbox>
     <something flex="1"/>
     <something-else/>
    </hbox>
    
    Before bug 1822131 <something flex="1"> ended up with flex-basis: auto,
    but was the only thing able to shrink, so <something-else> stayed the
    same size.
    
    After that bug however <something-else> is able to shrink too, so both
    elements shrink. This wouldn't happen if flex="1" actually worked like
    flex: 1 does.
    
    However flex: 1 causes stuff like explicit main sizes to be
    (effectively) ignored, so we need to fix up a few cases where now we'd
    start flexing too much. For that, add a debug assert to
    nsFlexContainerFrame to catch the would-be behavior changes here.
    
    For the most part they're actually no-op since they're setting tiny
    sizes, but preferences and devtools needed a couple real fixes.
    
    The profile selection spacer is useless (zero-size).
    
    Hopefully the last xul.css change I need to do :')
    
    Differential Revision: https://phabricator.services.mozilla.com/D172704
    
    UltraBlame original commit: b50655fcce3faf31b2c1779c263ac7c1bca94863

--- devtools/client/framework/toolbox-hosts.js
+++ devtools/client/framework/toolbox-hosts.js
@@ -436,7 +436,7 @@ function focusTab(tab) {
 function createDevToolsFrame(doc, className) {
   const frame = doc.createXULElement("browser");
   frame.setAttribute("type", "content");
-  frame.setAttribute("flex", "1"); 
+  frame.style.flex = "1 auto"; 
   frame.className = className;
 
   const inXULDocument = doc.documentElement.namespaceURI === XUL_NS;
