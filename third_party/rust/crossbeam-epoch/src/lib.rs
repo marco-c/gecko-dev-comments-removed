96e65691e89c89bcccc5bf8d7bd7bbadca079e97
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 06:35:26 +0000

    Bug 1806766 - Update crossbeam-epoch to 0.9.13. r=emilio,supply-chain-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D165330
    
    UltraBlame original commit: c5038cf9096cbcfee651a480b56a7fdac5f1ab2c

--- third_party/rust/crossbeam-epoch/src/lib.rs
+++ third_party/rust/crossbeam-epoch/src/lib.rs
@@ -107,7 +107,7 @@ mod primitive {
         
         impl<T> UnsafeCell<T> {
             #[inline]
-            pub(crate) fn new(data: T) -> UnsafeCell<T> {
+            pub(crate) const fn new(data: T) -> UnsafeCell<T> {
                 UnsafeCell(::core::cell::UnsafeCell::new(data))
             }
 
