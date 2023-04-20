af5797ed26e0dc4ab78ccb376fe18209a1a75f23
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 21:07:28 +0000

    Bug 1823757 - add PopoverInvokerElement interface r=emilio
    Differential Revision: https://phabricator.services.mozilla.com/D173180
    
    UltraBlame original commit: 69eb90a972795d067c67dc99e77c7a557d048e39

--- dom/html/nsGenericHTMLElement.h
+++ dom/html/nsGenericHTMLElement.h
@@ -1205,12 +1205,34 @@ class nsGenericHTMLFormControlElement : public nsGenericHTMLFormElement,
   mozilla::dom::HTMLFieldSetElement* mFieldSet;
 };
 
+enum class PopoverTargetAction : uint8_t {
+  Toggle,
+  Show,
+  Hide,
+};
+
 class nsGenericHTMLFormControlElementWithState
     : public nsGenericHTMLFormControlElement {
  public:
   nsGenericHTMLFormControlElementWithState(
       already_AddRefed<mozilla::dom::NodeInfo>&& aNodeInfo,
       mozilla::dom::FromParser aFromParser, FormControlType);
+
+  
+  bool ParseAttribute(int32_t aNamespaceID, nsAtom* aAttribute,
+                      const nsAString& aValue,
+                      nsIPrincipal* aMaybeScriptedPrincipal,
+                      nsAttrValue& aResult) override;
+
+  
+  mozilla::dom::Element* GetPopoverTargetElement() const;
+  void SetPopoverTargetElement(mozilla::dom::Element*);
+  void GetPopoverTargetAction(nsAString& aValue) const {
+    GetHTMLEnumAttr(nsGkAtoms::popovertargetaction, aValue);
+  }
+  void SetPopoverTargetAction(const nsAString& aValue) {
+    SetHTMLAttr(nsGkAtoms::popovertargetaction, aValue);
+  }
 
   
 
