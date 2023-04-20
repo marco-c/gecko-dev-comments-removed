7bf880aec7a04429ab0f9a3c12fd41d423bc69b2
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 16:34:39 +0000

    Bug 1816934 - Update consumers of services/settings to import ES modules directly. r=leplatrem,extension-reviewers,settings-reviewers,credential-management-reviewers,sgalich,mconley,robwu
    Differential Revision: https://phabricator.services.mozilla.com/D169934
    
    UltraBlame original commit: ae6ba90f918a243e3b4b90c412cae4182cb37208

--- toolkit/components/url-classifier/tests/unit/test_exceptionListService.js
+++ toolkit/components/url-classifier/tests/unit/test_exceptionListService.js
@@ -6,8 +6,8 @@
 
 
 
-const { RemoteSettings } = ChromeUtils.import(
-  "resource://services-settings/remote-settings.js"
+const { RemoteSettings } = ChromeUtils.importESModule(
+  "resource://services-settings/remote-settings.sys.mjs"
 );
 
 const COLLECTION_NAME = "url-classifier-skip-urls";
