7f71678334c5a5b46548d17ec245cbaa3ce056bd
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 03:27:33 +0000

    Bug 1802957 - Remove windows-specific condition for system font font-size adjustment. r=jfkthame
    Now that the non-native theme is everywhere, let's do this consistently
    across platforms.
    
    Note that these fonts aren't used for XUL anyways, so this shouldn't
    change behavior in practice.
    
    Depends on D163269
    
    Differential Revision: https://phabricator.services.mozilla.com/D163270
    
    UltraBlame original commit: 5fd98b1efb0a2f32451347cecc3146492c10c4ca

--- layout/base/nsLayoutUtils.cpp
+++ layout/base/nsLayoutUtils.cpp
@@ -9709,24 +9709,15 @@ void nsLayoutUtils::ComputeSystemFont(nsFont* aSystemFont,
   if (aFontID == LookAndFeel::FontID::MozField ||
       aFontID == LookAndFeel::FontID::MozButton ||
       aFontID == LookAndFeel::FontID::MozList) {
-    const bool isWindowsOrNonNativeTheme =
-#ifdef XP_WIN
-        true;
-#else
-        aDocument->ShouldAvoidNativeTheme();
-#endif
-
-    if (isWindowsOrNonNativeTheme) {
-      
-      
-      
-      
-      
-      
-      auto newSize =
-          aDefaultVariableFont.size.ToCSSPixels() - CSSPixel::FromPoints(2.0f);
-      aSystemFont->size = Length::FromPixels(std::max(float(newSize), 0.0f));
-    }
+    
+    
+    
+    
+    
+    
+    auto newSize =
+        aDefaultVariableFont.size.ToCSSPixels() - CSSPixel::FromPoints(2.0f);
+    aSystemFont->size = Length::FromPixels(std::max(float(newSize), 0.0f));
   }
 }
 
