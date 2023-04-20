62e00d25118090ce909f894433dae0324499f316
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 19:21:34 +0000

    Bug 1810306 - Stop setting icon on menuitem in the View->Show Sidebars menu. r=desktop-theme-reviewers,mixedpuppy,dao
    Differential Revision: https://phabricator.services.mozilla.com/D166871
    
    UltraBlame original commit: 6ac0d009f1ef90ebe6a83a86e89792b73065b567

--- browser/components/extensions/test/browser/browser_ext_sidebarAction_windows.js
+++ browser/components/extensions/test/browser/browser_ext_sidebarAction_windows.js
@@ -40,12 +40,13 @@ add_task(async function sidebar_windows() {
     "sidebar box is visible in first window"
   );
   
-  let elements = document.getElementsByClassName("webextension-menuitem");
-  
-  
-  ok(!!elements.length, "have a menuitem");
-  let style = elements[0].getAttribute("style");
-  ok(style.includes("webextension-menuitem-image"), "this menu has style");
+  let button = document.querySelector(".sidebar-extensions-subviewbutton");
+  ok(!!button, "have a toolbarbutton");
+  let style = button.getAttribute("style");
+  ok(
+    style.includes("webextension-sidebar-subviewbutton-image"),
+    "this button has our style"
+  );
 
   let secondSidebar = extension.awaitMessage("sidebar");
 
@@ -58,11 +59,14 @@ add_task(async function sidebar_windows() {
     !win.document.getElementById("sidebar-box").hidden,
     "sidebar box is visible in second window"
   );
-  
-  elements = win.document.getElementsByClassName("webextension-menuitem");
-  ok(!!elements.length, "have a menuitem");
-  style = elements[0].getAttribute("style");
-  ok(style.includes("webextension-menuitem-image"), "this menu has style");
+
+  button = win.document.querySelector(".sidebar-extensions-subviewbutton");
+  ok(!!button, "have a toolbarbutton");
+  style = button.getAttribute("style");
+  ok(
+    style.includes("webextension-sidebar-subviewbutton-image"),
+    "this button has our style"
+  );
 
   await extension.unload();
   await BrowserTestUtils.closeWindow(win);
