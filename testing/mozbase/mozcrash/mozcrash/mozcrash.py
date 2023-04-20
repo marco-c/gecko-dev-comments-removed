f210b079db8658c04309c4738a225f27b43cb928
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 19:16:33 +0000

    Bug 1731355 - Strip parameters from mozcrash signatures r=gsvelto
    Differential Revision: https://phabricator.services.mozilla.com/D161483
    
    UltraBlame original commit: 8b713e48064fe6f387db7019135406b80733dd11

--- testing/mozbase/mozcrash/mozcrash/mozcrash.py
+++ testing/mozbase/mozcrash/mozcrash/mozcrash.py
@@ -397,6 +397,11 @@ class CrashInfo(object):
                 if retcode == 0:
                     signature = self._generate_signature(json_output)
 
+                    
+                    pmatch = re.search(r"(.*)\(.*\)", signature)
+                    if pmatch:
+                        signature = pmatch.group(1)
+
         else:
             if not self.stackwalk_binary:
                 errors.append(
