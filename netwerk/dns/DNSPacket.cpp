3b4da04e95d096f4ba44eddf070c75f78abe3f60
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 07:51:28 +0000

    Backed out changeset 0d4cb3529fa8 (bug 1808631) for failures on test_httpssvc_iphint.js. CLOSED TREE
    UltraBlame original commit: 064b9864add1613761b2220cfcbef8014ca7179a

--- netwerk/dns/DNSPacket.cpp
+++ netwerk/dns/DNSPacket.cpp
@@ -767,7 +767,6 @@ nsresult DNSPacket::DecodeInternal(
           if (!available.isValid()) {
             return NS_ERROR_UNEXPECTED;
           }
-          aTTL = TTL;
           while (available.value() >= 4) {
             
             
@@ -847,6 +846,7 @@ nsresult DNSPacket::DecodeInternal(
             results.AppendElement(parsed);
           }
 
+          aTTL = TTL;
           break;
         }
         default:
