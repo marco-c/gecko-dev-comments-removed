0f79efdbeb8627c6cd14dcc3668e427a8ecafe29
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 06:41:05 +0000

    Bug 1724687 - Remove osfile.jsm usage from remote/cdp r=webdriver-reviewers,nika,whimboo
    This patch replaces the internals of the StreamRegistry et al to use a new
    Stream class as the stream type. Under the hood, it delegates the IO to
    IOUtils.
    
    In the case where a PDF's data is returned directly (i.e., they don't request a
    stream), we write the data to an nsIStorageStream in memory and hand over the
    contents without touching the disk.
    
    Differential Revision: https://phabricator.services.mozilla.com/D163372
    
    UltraBlame original commit: 631e22733b8bb3794b47baf0bfa24fe10c567a36

--- remote/cdp/test/browser/head.js
+++ remote/cdp/test/browser/head.js
@@ -3,8 +3,6 @@
 
 "use strict";
 
-const { OS } = ChromeUtils.import("resource://gre/modules/osfile.jsm");
-
 const { RemoteAgent } = ChromeUtils.importESModule(
   "chrome://remote/content/components/RemoteAgent.sys.mjs"
 );
@@ -13,6 +11,9 @@ const { RemoteAgentError } = ChromeUtils.importESModule(
 );
 const { TabManager } = ChromeUtils.importESModule(
   "chrome://remote/content/shared/TabManager.sys.mjs"
+);
+const { Stream } = ChromeUtils.importESModule(
+  "chrome://remote/content/cdp/StreamRegistry.sys.mjs"
 );
 
 const TIMEOUT_MULTIPLIER = SpecialPowers.isDebugBuild ? 4 : 1;
@@ -399,40 +400,24 @@ function fail(message) {
 
 
 
-
-
-async function createFile(contents, options = {}) {
+async function createFileStream(contents, options = {}) {
   let { path = null, remove = true } = options;
 
   if (!path) {
-    const basePath = OS.Path.join(OS.Constants.Path.tmpDir, "remote-agent.txt");
-    const { file, path: tmpPath } = await OS.File.openUnique(basePath, {
-      humanReadable: true,
-    });
-    await file.close();
-    path = tmpPath;
+    path = await IOUtils.createUniqueFile(
+      PathUtils.tempDir,
+      "remote-agent.txt"
+    );
   }
 
-  let encoder = new TextEncoder();
-  let array = encoder.encode(contents);
+  await IOUtils.writeUTF8(path, contents);
 
-  const count = await OS.File.writeAtomic(path, array, {
-    encoding: "utf-8",
-    tmpPath: path + ".tmp",
-  });
-  is(count, contents.length, "All data has been written to file");
-
-  const file = await OS.File.open(path);
-
-  
+  const stream = new Stream(path);
   if (remove) {
-    registerCleanupFunction(async () => {
-      await file.close();
-      await OS.File.remove(path, { ignoreAbsent: true });
-    });
+    registerCleanupFunction(() => stream.destroy());
   }
 
-  return { file, path };
+  return stream;
 }
 
 async function throwScriptError(options = {}) {
