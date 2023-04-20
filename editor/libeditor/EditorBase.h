7225c24b3aea67109faf86c64618542919e397b2
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 16:19:46 +0000

    Bug 1797247 - part 4: Make `EditorBase::DeleteRangesWithTransaction` not touch `Selection` directly r=m_kato
    Differential Revision: https://phabricator.services.mozilla.com/D169041
    
    UltraBlame original commit: 716218023d3e4c4f5cf13ad1c5e28c714a9a41ca

--- editor/libeditor/EditorBase.h
+++ editor/libeditor/EditorBase.h
@@ -2547,7 +2547,7 @@ class EditorBase : public nsIEditor,
 
 
 
-  [[nodiscard]] MOZ_CAN_RUN_SCRIPT nsresult
+  [[nodiscard]] MOZ_CAN_RUN_SCRIPT Result<CaretPoint, nsresult>
   DeleteRangesWithTransaction(nsIEditor::EDirection aDirectionAndAmount,
                               nsIEditor::EStripWrappers aStripWrappers,
                               const AutoRangeArray& aRangesToDelete);
