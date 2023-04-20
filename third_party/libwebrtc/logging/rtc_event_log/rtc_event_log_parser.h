078ae6eb78ce7a7484bd31f8d0e3c699ab05fb09
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 05:37:35 +0000

    Bug 1800920 - Vendor libwebrtc from 12053ec64a
    Upstream commit: https://webrtc.googlesource.com/src/+/12053ec64ad71dde3b1603296f7df50ae93e68a7
        Encode remote link capacity estimates in legacy RTC event log format
    
        Bug: None
        Change-Id: I36037d0c654e773d5e1c6e9821031eafea54fe0d
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/271162
        Reviewed-by: Per Kjellander <perkjwebrtc.org>
        Commit-Queue: Björn Terelius <tereliuswebrtc.org>
        Cr-Commit-Position: refs/heads/main{#37749}
    
    UltraBlame original commit: 58ff84fd7a114e17275b2828a953d35d7ced75a3

--- third_party/libwebrtc/logging/rtc_event_log/rtc_event_log_parser.h
+++ third_party/libwebrtc/logging/rtc_event_log/rtc_event_log_parser.h
@@ -725,6 +725,9 @@ class ParsedRtcEventLog {
   ParsedRtcEventLog::ParseStatusOr<LoggedIceCandidatePairEvent>
   GetIceCandidatePairEvent(const rtclog::Event& event) const;
 
+  ParsedRtcEventLog::ParseStatusOr<LoggedRemoteEstimateEvent>
+  GetRemoteEstimateEvent(const rtclog::Event& event) const;
+
   
   ParseStatus StoreAlrStateEvent(const rtclog2::AlrState& proto);
   ParseStatus StoreAudioNetworkAdaptationEvent(
