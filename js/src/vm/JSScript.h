dcbcc210c1dd0de7573eb94a0b270a2241cda72f
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 02:40:14 +0000

    Bug 1786834 - Part 1: Move SharedImmutableStringsCache out of JSRuntime and make it singleton. r=nbp
    Differential Revision: https://phabricator.services.mozilla.com/D160204
    
    UltraBlame original commit: 07efd170adbf210c5a08e7f74fe0a2e38a34e171

--- js/src/vm/JSScript.h
+++ js/src/vm/JSScript.h
@@ -36,6 +36,7 @@
 #include "vm/BytecodeIterator.h"
 #include "vm/BytecodeLocation.h"
 #include "vm/BytecodeUtil.h"
+#include "vm/MutexIDs.h"  
 #include "vm/NativeObject.h"
 #include "vm/SharedImmutableStringsCache.h"
 #include "vm/SharedStencil.h"  
