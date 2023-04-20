0dae501c833566f0d70b50de571957df0e62690f
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 07:11:53 +0000

    Bug 1808171 part 1 - Replace JS_InitClass's clasp argument with protoClass/name arguments. r=sfink
    This decouples the two uses of the passed `JSClass`, to make it easier to use
    `JS_InitClass` when you don't want to use the same `JSClass` for the prototype
    and the instance objects.
    
    Later patches will change most of the callers to pass `nullptr` for `protoClass`.
    
    Also adds documentation for `JS_InitClass`.
    
    Differential Revision: https://phabricator.services.mozilla.com/D165796
    
    UltraBlame original commit: 7e8db9e52c91e55e43072bce6913425139819d4d

--- js/src/vm/JSObject.cpp
+++ js/src/vm/JSObject.cpp
@@ -1425,13 +1425,14 @@ static NativeObject* DefineConstructorAndPrototype(
 }
 
 NativeObject* js::InitClass(JSContext* cx, HandleObject obj,
-                            HandleObject protoProto_, const JSClass* clasp,
-                            Native constructor, unsigned nargs,
-                            const JSPropertySpec* ps, const JSFunctionSpec* fs,
+                            const JSClass* protoClass, HandleObject protoProto_,
+                            const char* name, Native constructor,
+                            unsigned nargs, const JSPropertySpec* ps,
+                            const JSFunctionSpec* fs,
                             const JSPropertySpec* static_ps,
                             const JSFunctionSpec* static_fs,
                             NativeObject** ctorp) {
-  Rooted<JSAtom*> atom(cx, Atomize(cx, clasp->name, strlen(clasp->name)));
+  Rooted<JSAtom*> atom(cx, Atomize(cx, name, strlen(name)));
   if (!atom) {
     return nullptr;
   }
@@ -1443,12 +1444,16 @@ NativeObject* js::InitClass(JSContext* cx, HandleObject obj,
 
 
 
+
   RootedObject protoProto(cx, protoProto_);
   if (!protoProto) {
     protoProto = &cx->global()->getObjectPrototype();
   }
+  if (!protoClass) {
+    protoClass = &PlainObject::class_;
+  }
 
-  return DefineConstructorAndPrototype(cx, obj, atom, protoProto, clasp,
+  return DefineConstructorAndPrototype(cx, obj, atom, protoProto, protoClass,
                                        constructor, nargs, ps, fs, static_ps,
                                        static_fs, ctorp);
 }
