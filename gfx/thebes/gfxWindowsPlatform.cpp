89645447a2217a57131cd18c3db02de393b0097c
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 03:04:28 +0000

    Bug 1798067 Part 2: Implement gfxWindowsPlatform::SupportsHDR. r=jgilbert,gfx-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D161365
    
    UltraBlame original commit: f87001406f297f154e98d5744d3ef0483544591b

--- gfx/thebes/gfxWindowsPlatform.cpp
+++ gfx/thebes/gfxWindowsPlatform.cpp
@@ -273,6 +273,7 @@ NS_IMPL_ISUPPORTS(D3DSharedTexturesReporter, nsIMemoryReporter)
 
 gfxWindowsPlatform::gfxWindowsPlatform()
     : mRenderMode(RENDER_GDI),
+      mSupportsHDR(false),
       mDwmCompositionStatus(DwmCompositionStatus::Unknown) {
   
   
@@ -432,6 +433,7 @@ void gfxWindowsPlatform::InitAcceleration() {
   
   
   UpdateCanUseHardwareVideoDecoding();
+  UpdateSupportsHDR();
 
   RecordStartupTelemetry();
 }
@@ -560,6 +562,43 @@ void gfxWindowsPlatform::UpdateRenderMode() {
           "GFX: Failed to update reference draw target after device reset");
     }
   }
+}
+
+void gfxWindowsPlatform::UpdateSupportsHDR() {
+  
+  
+  
+  DeviceManagerDx* dx = DeviceManagerDx::Get();
+  nsTArray<DXGI_OUTPUT_DESC1> outputs = dx->EnumerateOutputs();
+
+  for (auto& output : outputs) {
+    if (output.BitsPerColor <= 8) {
+      continue;
+    }
+
+    switch (output.ColorSpace) {
+      case DXGI_COLOR_SPACE_RGB_STUDIO_G22_NONE_P2020:
+      case DXGI_COLOR_SPACE_YCBCR_STUDIO_G22_LEFT_P2020:
+      case DXGI_COLOR_SPACE_YCBCR_FULL_G22_LEFT_P2020:
+      case DXGI_COLOR_SPACE_RGB_FULL_G2084_NONE_P2020:
+      case DXGI_COLOR_SPACE_YCBCR_STUDIO_G2084_LEFT_P2020:
+      case DXGI_COLOR_SPACE_RGB_STUDIO_G2084_NONE_P2020:
+      case DXGI_COLOR_SPACE_YCBCR_STUDIO_G22_TOPLEFT_P2020:
+      case DXGI_COLOR_SPACE_YCBCR_STUDIO_G2084_TOPLEFT_P2020:
+      case DXGI_COLOR_SPACE_RGB_FULL_G22_NONE_P2020:
+      case DXGI_COLOR_SPACE_YCBCR_STUDIO_GHLG_TOPLEFT_P2020:
+      case DXGI_COLOR_SPACE_YCBCR_FULL_GHLG_TOPLEFT_P2020:
+      case DXGI_COLOR_SPACE_RGB_STUDIO_G24_NONE_P2020:
+      case DXGI_COLOR_SPACE_YCBCR_STUDIO_G24_LEFT_P2020:
+      case DXGI_COLOR_SPACE_YCBCR_STUDIO_G24_TOPLEFT_P2020:
+        mSupportsHDR = true;
+        return;
+      default:
+        break;
+    }
+  }
+
+  mSupportsHDR = false;
 }
 
 mozilla::gfx::BackendType gfxWindowsPlatform::GetContentBackendFor(
