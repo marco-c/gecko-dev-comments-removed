50b177d84350491f0186a176b9f959a551bdc0fa
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 00:36:19 +0000

    Bug 1790097 - Vendor libwebrtc from fc2c24ef44
    Upstream commit: https://webrtc.googlesource.com/src/+/fc2c24ef4406484a2e6bec4d7aa44a2c78e6cdd0
        [FlexfecReceiveStream] Use explicit member variables for state.
    
        This changes FlexfecReceiveStreamImpl so that instead of holding on to
        the entire config structure, the state is broken down into member
        variables whose constness and thread access can be individually set.
    
        Bug: none
        Change-Id: I497b5816d40678774dee76d8a97012e8539629b8
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/263723
        Reviewed-by: Rasmus Brandt <brandtrwebrtc.org>
        Commit-Queue: Tomas Gunnarsson <tommiwebrtc.org>
        Cr-Commit-Position: refs/heads/main{#37027}
    
    UltraBlame original commit: 8e4061651292431251362a8c62cc2e18fb3a2e1c

--- third_party/libwebrtc/call/flexfec_receive_stream_impl.h
+++ third_party/libwebrtc/call/flexfec_receive_stream_impl.h
@@ -63,10 +63,10 @@ class FlexfecReceiveStreamImpl : public FlexfecReceiveStream {
   
   void SetLocalSsrc(uint32_t local_ssrc);
 
-  uint32_t remote_ssrc() const { return config_.rtp.remote_ssrc; }
+  uint32_t remote_ssrc() const { return remote_ssrc_; }
   bool transport_cc() const override {
     RTC_DCHECK_RUN_ON(&packet_sequence_checker_);
-    return config_.rtp.transport_cc;
+    return transport_cc_;
   }
 
  private:
@@ -74,10 +74,8 @@ class FlexfecReceiveStreamImpl : public FlexfecReceiveStream {
 
   RtpHeaderExtensionMap extension_map_;
 
-  
-  
-  
-  const Config config_;
+  const uint32_t remote_ssrc_;
+  bool transport_cc_ RTC_GUARDED_BY(packet_sequence_checker_);
 
   
   const std::unique_ptr<FlexfecReceiver> receiver_;
