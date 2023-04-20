b12817bca379c449cc74b8666268c58f11504bef
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 05:28:20 +0000

    Bug 1800920 - Vendor libwebrtc from 93b107d0f0
    Upstream commit: https://webrtc.googlesource.com/src/+/93b107d0f026d4ce73bf1d11dfaeb37c4f28ac8e
        Remove unused field trial WebRTC-JitterUpperBound
    
        Bug: webrtc:9898
        Change-Id: I89f2baca9b96fc00fc73d24086e7c6f5ed8307a8
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/269401
        Reviewed-by: Emil Lundmark <lndmrkwebrtc.org>
        Auto-Submit: Erik Språng <sprangwebrtc.org>
        Commit-Queue: Erik Språng <sprangwebrtc.org>
        Cr-Commit-Position: refs/heads/main{#37620}
    
    UltraBlame original commit: 4d5a1b244f8ee139d6dfa16f8d64ab8c8d4d1792

--- third_party/libwebrtc/modules/video_coding/timing/jitter_estimator.h
+++ third_party/libwebrtc/modules/video_coding/timing/jitter_estimator.h
@@ -148,7 +148,6 @@ class JitterEstimator {
 
   
   rtc::RollingAccumulator<uint64_t> fps_counter_;
-  const double time_deviation_upper_bound_;
   const bool enable_reduced_delay_;
   Clock* clock_;
 };
