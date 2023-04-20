a79b63683eaf824754578bb55d70904d0083e36a
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 02:28:15 +0000

    Backed out changeset 41bebff09700 (bug 1800033) for causing failures in browser_utility_audioDecodeCrash.js CLOSED TREE
    UltraBlame original commit: 17a7f82366276081b22f16184e69a2cb99f747db

--- ipc/glue/UtilityAudioDecoderParent.cpp
+++ ipc/glue/UtilityAudioDecoderParent.cpp
@@ -67,7 +67,7 @@ UtilityAudioDecoderParent::~UtilityAudioDecoderParent() {
 
 
 void UtilityAudioDecoderParent::GenericPreloadForSandbox() {
-#if defined(MOZ_SANDBOX) && defined(XP_WIN) && defined(MOZ_FFPVX)
+#if defined(MOZ_SANDBOX) && defined(OS_WIN)
   
   
   UtilityProcessImpl::LoadLibraryOrCrash(L"mozavcodec.dll");
