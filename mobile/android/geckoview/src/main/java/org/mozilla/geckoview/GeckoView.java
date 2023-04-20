5dc26aa8eae17f45bd73586ea05f7f7b86f7e4c9
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 06:42:14 +0000

    Bug 1806072 - screen.orientation.angle isn't updated on Pixel 6a. r=geckoview-reviewers,owlish
    When using Pixel 6a, orientation information seems to be invalid. It seems that
    this is similar to bug 1754802.
    
    So we should get the rotation information from ScreenCompat per API level.
    
    And, when changing screen orientation by `screen.orientation.lock()`, Display
    Manager API observes changing, but it may get previous information at this
    timing. After changing display information, `onConfigureChanges` is notified,
    then we an get correct information, so we have to update information via it if
    newer Android.
    
    Differential Revision: https://phabricator.services.mozilla.com/D164885
    
    UltraBlame original commit: ddf5f22cd611b0636fe10fe99faf57cb21e52ac3

--- mobile/android/geckoview/src/main/java/org/mozilla/geckoview/GeckoView.java
+++ mobile/android/geckoview/src/main/java/org/mozilla/geckoview/GeckoView.java
@@ -635,7 +635,9 @@ public class GeckoView extends FrameLayout {
     if (mSession != null) {
       final GeckoRuntime runtime = mSession.getRuntime();
       if (runtime != null) {
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR1) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR1
+            || Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+          
           
           
           
