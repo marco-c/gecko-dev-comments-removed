00b0757d56039a1cfce48931d782d10966c8e82b
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 08:17:08 +0000

    Bug 1792057 - Make sure to focus inner text field when creating / rebuilding edit fields in datetime widget. r=smaug
    The blur is a regression from bug 1740989, but it's really uncovering a
    regression from bug 1729342, sorta. Before that, we used to focus the
    inner text field on rebuild via focusInnerTextBox().
    
    One could argue that the focus fixup rule should really deal with this,
    but I guess since content can't really unattach shadow DOM, it's less of
    a general issue and more of an implementation detail.
    
    The test uncovers another pre-existing issue (switching from date ->
    test was also broken, and we left a bogus ElementState::FOCUS state in
    the element).
    
    Differential Revision: https://phabricator.services.mozilla.com/D158167
    
    UltraBlame original commit: 14c3be47e10268481ac05a0405554953e394208a

--- dom/base/Element.cpp
+++ dom/base/Element.cpp
@@ -1406,14 +1406,14 @@ void Element::NotifyUAWidgetTeardown(UnattachShadowRoot aUnattachShadowRoot) {
 }
 
 void Element::UnattachShadow() {
-  ShadowRoot* shadowRoot = GetShadowRoot();
+  RefPtr<ShadowRoot> shadowRoot = GetShadowRoot();
   if (!shadowRoot) {
     return;
   }
 
   nsAutoScriptBlocker scriptBlocker;
 
-  if (Document* doc = GetComposedDoc()) {
+  if (RefPtr<Document> doc = GetComposedDoc()) {
     if (PresShell* presShell = doc->GetPresShell()) {
       presShell->DestroyFramesForAndRestyle(this);
 #ifdef ACCESSIBILITY
@@ -1428,13 +1428,18 @@ void Element::UnattachShadow() {
       }
 #endif
     }
+    
+    
+    [&]() MOZ_CAN_RUN_SCRIPT_BOUNDARY {
+      if (RefPtr<nsFocusManager> fm = nsFocusManager::GetFocusManager()) {
+        fm->ContentRemoved(doc, shadowRoot);
+      }
+    }();
   }
   MOZ_ASSERT(!GetPrimaryFrame());
 
   shadowRoot->Unattach();
   SetShadowRoot(nullptr);
-
-  
 }
 
 void Element::GetAttribute(const nsAString& aName, DOMString& aReturn) {
