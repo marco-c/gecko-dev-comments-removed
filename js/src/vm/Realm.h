17c715e274bf82e5018507a4bda7b01ee7df86ba
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 04:13:11 +0000

    Bug 1804394 part 8 - Use SharedShape more for plain object shapes. r=jonco
    Depends on D164057
    
    Differential Revision: https://phabricator.services.mozilla.com/D164058
    
    UltraBlame original commit: 427fc6ae28a3fdb3677e661e0c774a8e95703fca

--- js/src/vm/Realm.h
+++ js/src/vm/Realm.h
@@ -126,13 +126,13 @@ class NewProxyCache {
 
 class NewPlainObjectWithPropsCache {
   static const size_t NumEntries = 4;
-  mozilla::Array<Shape*, NumEntries> entries_;
+  mozilla::Array<SharedShape*, NumEntries> entries_;
 
  public:
   NewPlainObjectWithPropsCache() { purge(); }
 
-  Shape* lookup(IdValuePair* properties, size_t nproperties) const;
-  void add(Shape* shape);
+  SharedShape* lookup(IdValuePair* properties, size_t nproperties) const;
+  void add(SharedShape* shape);
 
   void purge() {
     for (size_t i = 0; i < NumEntries; i++) {
