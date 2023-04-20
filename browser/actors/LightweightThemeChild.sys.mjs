3f90fa8f5b67b6916271a81789ab28717184f46c
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 22:27:50 +0000

    Bug 1824491 - Convert browser/actors/LightweightThemeChild.jsm to ESM. r=kpatenio
    Differential Revision: https://phabricator.services.mozilla.com/D174521
    
    UltraBlame original commit: c50f34ab7d5ffff0a5b99db665e4109d4aa3840c

--- browser/actors/LightweightThemeChild.sys.mjs
+++ browser/actors/LightweightThemeChild.sys.mjs
@@ -1,15 +1,11 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-
-
-
-"use strict";
-
-var EXPORTED_SYMBOLS = ["LightweightThemeChild"];
-
-
-
-
-class LightweightThemeChild extends JSWindowActorChild {
+/**
+ * LightweightThemeChild forwards theme data to in-content pages.
+ */
+export class LightweightThemeChild extends JSWindowActorChild {
   constructor() {
     super();
     this._initted = false;
@@ -22,7 +18,7 @@ class LightweightThemeChild extends JSWindowActorChild {
 
   _getChromeOuterWindowID() {
     try {
-      
+      // Getting the browserChild throws an exception when it is null.
       let browserChild = this.docShell.browserChild;
       if (browserChild) {
         return browserChild.chromeOuterWindowID;
@@ -35,18 +31,18 @@ class LightweightThemeChild extends JSWindowActorChild {
       return this.browsingContext.topChromeWindow.docShell.outerWindowID;
     }
 
-    
-    
+    // Return a false-y outerWindowID if we can't manage to get a proper one.
+    // Note that no outerWindowID will ever have this ID.
     return 0;
   }
 
-  
-
-
-
+  /**
+   * Handles "change" events on the child sharedData map, and notifies
+   * our content page if its theme data was among the changed keys.
+   */
   handleEvent(event) {
     switch (event.type) {
-      
+      // Make sure to update the theme data on first page show.
       case "pageshow":
       case "DOMContentLoaded":
         if (!this._initted && this._getChromeOuterWindowID()) {
@@ -65,9 +61,9 @@ class LightweightThemeChild extends JSWindowActorChild {
     }
   }
 
-  
-
-
+  /**
+   * Forward the theme data to the page.
+   */
   update() {
     const event = Cu.cloneInto(
       {
