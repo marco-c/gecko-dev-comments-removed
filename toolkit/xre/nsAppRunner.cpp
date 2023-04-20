410ba888f406353b204325a06600b03aca43c2bd
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 20:54:32 +0000

    Bug 1787182 [Linux] Don't fire glxtest process unless we know we really want to run r=emilio
    Right now we fire glxtest on every Firefox start, even if we going to update, restart or ping running remote instance.
    When we're running on system with broken/unstable gfx drivers (drivers/glx freezes or crashes) every such action is delayed or coredumps are generated on systems.
    
    In this patch we launch glx test proces later if we know we need it.
    
    Depends on D168650
    
    Differential Revision: https://phabricator.services.mozilla.com/D168651
    
    UltraBlame original commit: e75ee46f3a6fcaa371913c8979dd2509416bf0f8

--- toolkit/xre/nsAppRunner.cpp
+++ toolkit/xre/nsAppRunner.cpp
@@ -3950,17 +3950,6 @@ int XREMain::XRE_mainInit(bool* aExitFlag) {
 
   mozilla::startup::IncreaseDescriptorLimits();
 
-#ifdef USE_GLX_TEST
-  
-  
-  
-  
-  
-  
-  
-  fire_glxtest_process();
-#endif
-
   SetupErrorHandling(gArgv[0]);
 
 #ifdef CAIRO_HAS_DWRITE_FONT
@@ -5121,6 +5110,10 @@ int XREMain::XRE_mainStartup(bool* aExitFlag) {
 
   
   mozilla::glean_pings::Pageload.Submit("startup"_ns);
+
+#ifdef USE_GLX_TEST
+  fire_glxtest_process();
+#endif
 
   return 0;
 }
