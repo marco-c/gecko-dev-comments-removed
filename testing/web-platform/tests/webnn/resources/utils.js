3b096ce1e2074428b96c28ed78e8aebed84b0b41
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 08:51:57 +0000

    Bug 1803548 [wpt PR 37275] - Add webnn conv2d tests, a=testonly
    Automatic update from web-platform-tests
    [webnn] Add float32 tests for WebNN conv2d op. (#37275)
    
    --
    
    wpt-commits: c15ee8241ba3a3e66669a1eabdc5bd6b1285b904
    wpt-pr: 37275
    
    UltraBlame original commit: 62db0330e8ff11a270c361ee3a38b4cbe77e6e41

--- testing/web-platform/tests/webnn/resources/utils.js
+++ testing/web-platform/tests/webnn/resources/utils.js
@@ -65,6 +65,57 @@ const getExpectedDataAndType = (resources, outputName) => {
 
 
 
+const getConv2dPrecisionTolerance = (resources) => {
+  
+  const inputNameArray = Object.keys(resources.inputs);
+  const inputShape = resources.inputs[inputNameArray[0]].shape;
+  const filterShape = resources.inputs[inputNameArray[1]].shape;
+  const options = resources.options;
+  let groups = 1;
+  let inputChannels = inputShape[1]; 
+  let filterWidth = filterShape[3]; 
+  let filterHeight = filterShape[2];
+  if (options) {
+    if (options.groups) {
+      groups = options.groups;
+    }
+    if (options.inputLayout) {
+      if (!['nchw', 'nhwc'].includes(options.inputLayout)) {
+        throw new Error(`Unsupported inputLayout ${options.inputLayout}`);
+      }
+      inputChannels = options.inputLayout === 'nchw' ? inputChannels : inputShape[3];
+    }
+    if (options.filterLayout) {
+      if (!['oihw', 'hwio', 'ohwi', 'ihwo'].includes(options.filterLayout)) {
+        throw new Error(`Unsupported filterLayout ${options.filterLayout}`);
+      }
+      switch (options.filterLayout) {
+        case 'oihw':
+          
+          break;
+        case 'hwio':
+          filterWidth = filterShape[1];
+          filterHeight = filterShape[0];
+          break;
+        case 'ohwi':
+        case 'ihwo':
+          filterWidth = filterShape[2];
+          filterHeight = filterShape[1];
+          break;
+        default:
+          break;
+      }
+    }
+  }
+  const tolerance = filterWidth * filterHeight * (inputChannels / groups) * 2;
+  return tolerance;
+};
+
+
+
+
+
+
 const getMatmulPrecisionTolerance = (resources) => {
   
   
@@ -90,6 +141,7 @@ const getSoftmaxPrecisionTolerance = (resources) => {
 const PrecisionMetrics = {
   clamp: {ULP: {float32: 0, float16: 0}},
   concat: {ULP: {float32: 0, float16: 0}},
+  conv2d: {ULP: {float32: getConv2dPrecisionTolerance, float16: getConv2dPrecisionTolerance}},
   leakyRelu: {ULP: {float32: 1, float16: 1}},
   matmul: {ULP: {float32: getMatmulPrecisionTolerance, float16: getMatmulPrecisionTolerance}},
   relu: {ULP: {float32: 0, float16: 0}},
@@ -227,6 +279,17 @@ const checkResults = (operationName, namedOutputOperands, outputs, resources) =>
     tolerance = getPrecisonTolerance(operationName, metricType, operandType);
     doAssert(operationName, outputData, expectedData, tolerance, operandType, metricType)
   }
+};
+
+
+
+
+
+
+
+const createConstantOperand = (builder, resources) => {
+  const bufferView = new TypedArrayDict[resources.type](resources.data);
+  return builder.constant({type: resources.type, dimensions: resources.shape}, bufferView);
 };
 
 
