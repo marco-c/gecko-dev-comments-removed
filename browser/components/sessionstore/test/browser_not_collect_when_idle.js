59ce5778743fd97173108373d1bf761d4cd100f9
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 07:55:30 +0000

    Bug 1808729 - Limit session store writes to once per hour when the user is idle, r=dao.
    Differential Revision: https://phabricator.services.mozilla.com/D166099
    
    UltraBlame original commit: 1a38da4f6de9be57a0c305176990964cac1ac7e3

--- browser/components/sessionstore/test/browser_not_collect_when_idle.js
+++ browser/components/sessionstore/test/browser_not_collect_when_idle.js
@@ -51,7 +51,7 @@ add_task(async function testIntervalChanges() {
   
   
   
-  Services.prefs.setIntPref("browser.sessionstore.idleDelay", 86400000);
+  Services.prefs.setIntPref("browser.sessionstore.idleDelay", 86400);
 
   
   let fakeIdleService = MockRegistrar.register(
@@ -67,7 +67,7 @@ add_task(async function testIntervalChanges() {
 
   
   
-  Services.prefs.setIntPref("browser.sessionstore.idleDelay", 5000000);
+  Services.prefs.setIntPref("browser.sessionstore.idleDelay", 5000);
 
   
   
