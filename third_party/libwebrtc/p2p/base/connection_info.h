57ef81eec2d865b7e8341f00817678745821748f
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 19:35:42 +0000

    Bug 1817024 - Vendor libwebrtc from 0487c5797a
    Upstream commit: https://webrtc.googlesource.com/src/+/0487c5797ae81b021ca5edcce9703e3958c81972
        stats: implement candidate-pair lastPacket(Sent|Received)Timestamp
    
        https://w3c.github.io/webrtc-stats/#dom-rtcicecandidatepairstats-lastpacketsenttimestamp
        https://w3c.github.io/webrtc-stats/#dom-rtcicecandidatepairstats-lastpacketreceivedtimestamp
    
        which are useful together with the ice-restart-necessary logic mentioned
        in
          https://w3c.github.io/webrtc-pc/#dictionary-rtcofferoptions-members
    
        BUG=webrtc:14619
    
        Change-Id: I4a8ab00a37fbd4af8b948720c83787cbdfc6b9a3
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/281281
        Reviewed-by: Henrik Boström <hboswebrtc.org>
        Reviewed-by: Harald Alvestrand <htawebrtc.org>
        Commit-Queue: Philipp Hancke <phanckemicrosoft.com>
        Cr-Commit-Position: refs/heads/main{#38534}
    
    UltraBlame original commit: bda2cf643ae12dde037edab60097b6ea079f3270

--- third_party/libwebrtc/p2p/base/connection_info.h
+++ third_party/libwebrtc/p2p/base/connection_info.h
@@ -15,6 +15,7 @@
 
 #include "absl/types/optional.h"
 #include "api/candidate.h"
+#include "api/units/timestamp.h"
 
 namespace cricket {
 
@@ -72,6 +73,10 @@ struct ConnectionInfo {
   uint64_t total_round_trip_time_ms;
   
   absl::optional<uint32_t> current_round_trip_time_ms;
+
+  
+  absl::optional<webrtc::Timestamp> last_data_received;
+  absl::optional<webrtc::Timestamp> last_data_sent;
 };
 
 
