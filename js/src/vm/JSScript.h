ce91079e995d16e56f43b4a09e0ab819a5d42f9a
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-21 00:52:58 +0000

    Bug 1824772: part 2 - Hash the filename used by script source and cache it. r=iain
    The hashed filename + sourceStart is used as a key for the in-process eager baseline hint cache.  Caching the filename hash in the script source also avoids the need to hash it repeatedly.
    
    Depends on D175521
    
    Differential Revision: https://phabricator.services.mozilla.com/D175522
    
    UltraBlame original commit: 1e10afa76d168e54603a6caea0b9de109ed92c78

--- js/src/vm/JSScript.h
+++ js/src/vm/JSScript.h
@@ -555,10 +555,10 @@ class ScriptSource {
   ExclusiveData<ReaderInstances> readers_;
 
   
-  bool hadCompressionTask_ = false;
+  SharedImmutableString filename_;
 
   
-  SharedImmutableString filename_;
+  HashNumber filenameHash_ = 0;
 
   
   
@@ -606,6 +606,9 @@ class ScriptSource {
   
   JS::DelazificationOption delazificationMode_ =
       JS::DelazificationOption::OnDemandOnly;
+
+  
+  bool hadCompressionTask_ = false;
 
   
   
@@ -991,6 +994,7 @@ class ScriptSource {
       SharedImmutableString compressed);
 
  public:
+  HashNumber filenameHash() const { return filenameHash_; }
   const char* filename() const {
     return filename_ ? filename_.chars() : nullptr;
   }
@@ -1492,6 +1496,7 @@ class BaseScript : public gc::TenuredCellWithNonGCPointer<uint8_t> {
   bool mutedErrors() const { return scriptSource()->mutedErrors(); }
 
   const char* filename() const { return scriptSource()->filename(); }
+  HashNumber filenameHash() const { return scriptSource()->filenameHash(); }
   const char* maybeForwardedFilename() const {
     return maybeForwardedScriptSource()->filename();
   }
