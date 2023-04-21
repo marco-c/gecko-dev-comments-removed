69cff341d3bd5a90219e75a758284b9974330afa
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-21 00:22:38 +0000

    Bug 1827156 - Ensure custom fontconfig per-font settings such as fontfeatures are picked up. r=gfx-reviewers,lsalzman
    Differential Revision: https://phabricator.services.mozilla.com/D175481
    
    UltraBlame original commit: 7a4fe637c12acf1a1f04b6b5f4798c276f43712d

--- gfx/thebes/gfxFcPlatformFontList.cpp
+++ gfx/thebes/gfxFcPlatformFontList.cpp
@@ -1898,20 +1898,36 @@ void gfxFcPlatformFontList::InitSharedFontListForPlatform() {
 
       
       
+      FcPattern* clone = FcPatternDuplicate(pattern);
+
+      
+      
+      if (!FcConfigSubstitute(nullptr, clone, FcMatchFont)) {
+        
+        FcPatternDestroy(clone);
+        continue;
+      }
+      
+      
+      FcPatternDel(clone, FC_HINT_STYLE);
+      FcPatternDel(clone, FC_HINTING);
+
+      
+      
       
       
       FcChar8* fontFormat;
-      if (FcPatternGetString(pattern, FC_FONTFORMAT, 0, &fontFormat) ==
+      if (FcPatternGetString(clone, FC_FONTFORMAT, 0, &fontFormat) ==
               FcResultMatch &&
           (!FcStrCmp(fontFormat, (const FcChar8*)"TrueType") ||
            !FcStrCmp(fontFormat, (const FcChar8*)"CFF"))) {
-        FcPattern* clone = FcPatternDuplicate(pattern);
         FcPatternDel(clone, FC_CHARSET);
         addPattern(clone, lastFamilyName, familyName, aAppFonts);
-        FcPatternDestroy(clone);
       } else {
-        addPattern(pattern, lastFamilyName, familyName, aAppFonts);
+        addPattern(clone, lastFamilyName, familyName, aAppFonts);
       }
+
+      FcPatternDestroy(clone);
     }
   };
 
