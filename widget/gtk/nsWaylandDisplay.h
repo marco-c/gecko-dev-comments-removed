47d3e08af8838e369f492e164df1b3fa93899d2e
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-21 00:44:34 +0000

    Bug 1828071 [Wayland] Move wl_registry from DMABufLibWrapper to nsWaylandDisplay r=emilio
    To speed up Firefox start on Wayland don't use extra wl_registry for dmabuf but use nsWaylandDisplay ones.
    
    In this patch we do:
    - Move GbmFormat setup from DMABufLibWrapper to nsWaylandDisplay
    - Make GbmFormat handlers static as we don't need per display setup
    - Get dmabuf pixel format configs on nsWaylandDisplay main thread handlers
    
    Differential Revision: https://phabricator.services.mozilla.com/D175467
    
    UltraBlame original commit: 328ba92c5afe28acbe56a3ac866122eebe34c6c8

--- widget/gtk/nsWaylandDisplay.h
+++ widget/gtk/nsWaylandDisplay.h
@@ -23,6 +23,14 @@
 namespace mozilla {
 namespace widget {
 
+struct GbmFormat {
+  bool mIsSupported;
+  bool mHasAlpha;
+  int mFormat;
+  uint64_t* mModifiers;
+  int mModifiersCount;
+};
+
 
 
 
@@ -78,6 +86,10 @@ class nsWaylandDisplay {
 
   bool IsExplicitSyncEnabled() { return mExplicitSync; }
 
+  static GbmFormat* GetGbmFormat(bool aHasAlpha);
+  static void AddFormatModifier(bool aHasAlpha, int aFormat,
+                                uint32_t aModifierHi, uint32_t aModifierLo);
+
  private:
   ~nsWaylandDisplay();
 
@@ -95,6 +107,9 @@ class nsWaylandDisplay {
   zwp_linux_dmabuf_v1* mDmabuf = nullptr;
   xdg_activation_v1* mXdgActivation = nullptr;
   bool mExplicitSync = false;
+
+  static GbmFormat sXRGBFormat;
+  static GbmFormat sARGBFormat;
 };
 
 void WaylandDispatchDisplays();
