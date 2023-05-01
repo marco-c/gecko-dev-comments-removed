2ce4f0072fdd7c57e676446d04538b66e2eb827f
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-05-01 00:25:51 +0000

    Bug 1828461 - Load LoadURIDelegate as lazy module. r=geckoview-reviewers,calu
    After landing bug 1673068, `LoadURIDelegate.jsm` isn't used until error page
    is shown. This should move to lazy and convert to ES system module.
    
    Differential Revision: https://phabricator.services.mozilla.com/D175661
    
    UltraBlame original commit: 5796faa7290767cb8068b4b37bbb582ef2586653

--- mobile/android/modules/geckoview/LoadURIDelegate.sys.mjs
+++ mobile/android/modules/geckoview/LoadURIDelegate.sys.mjs
@@ -1,20 +1,15 @@
+// -*- indent-tabs-mode: nil; js-indent-level: 2 -*-
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-
-
-
-"use strict";
-
-var EXPORTED_SYMBOLS = ["LoadURIDelegate"];
-
-const { GeckoViewUtils } = ChromeUtils.importESModule(
-  "resource://gre/modules/GeckoViewUtils.sys.mjs"
-);
+import { GeckoViewUtils } from "resource://gre/modules/GeckoViewUtils.sys.mjs";
 
 const { debug, warn } = GeckoViewUtils.initLogging("LoadURIDelegate");
 
-const LoadURIDelegate = {
-  
-  
+export const LoadURIDelegate = {
+  // Delegate URI loading to the app.
+  // Return whether the loading has been handled.
   load(aWindow, aEventDispatcher, aUri, aWhere, aFlags, aTriggeringPrincipal) {
     if (!aWindow) {
       return false;
@@ -39,8 +34,8 @@ const LoadURIDelegate = {
         handled = response;
       },
       () => {
-        
-        
+        // There was an error or listener was not registered in GeckoSession,
+        // treat as unhandled.
         handled = false;
       }
     );
