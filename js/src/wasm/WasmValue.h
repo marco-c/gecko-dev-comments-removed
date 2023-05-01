9fa96d91acb7c2d91dc9f9edfd63cfb72c6b4c27
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-05-01 00:31:04 +0000

    Bug 1827952: Implement bottom types (rhunt's patch). r=jseward,jpages
    Differential Revision: https://phabricator.services.mozilla.com/D175488
    
    UltraBlame original commit: 83bbf217916b4c102219ecf7a48109bb289da400

--- js/src/wasm/WasmValue.h
+++ js/src/wasm/WasmValue.h
@@ -23,7 +23,7 @@
 #include "vm/JSObject.h"
 #include "vm/NativeObject.h"  
 #include "wasm/WasmSerialize.h"
-#include "wasm/WasmValType.h"
+#include "wasm/WasmTypeDef.h"
 
 namespace js {
 namespace wasm {
@@ -387,7 +387,7 @@ class MOZ_NON_PARAM Val : public LitVal {
     cell_.ref_ = val;
   }
   explicit Val(ValType type, FuncRef val) : LitVal(type, AnyRef::null()) {
-    MOZ_ASSERT(type.isFuncRef());
+    MOZ_ASSERT(type.refType().isFuncHierarchy());
     cell_.ref_ = val.asAnyRef();
   }
 
@@ -491,6 +491,18 @@ using RootedValVectorN = Rooted<ValVectorN<N>>;
                                            MutableHandleAnyRef vp);
 
 
+[[nodiscard]] extern bool CheckNullExternRefValue(JSContext* cx, HandleValue v,
+                                                  MutableHandleAnyRef vp);
+
+
+[[nodiscard]] extern bool CheckNullFuncRefValue(JSContext* cx, HandleValue v,
+                                                MutableHandleFunction fun);
+
+
+[[nodiscard]] extern bool CheckNullRefValue(JSContext* cx, HandleValue v,
+                                            MutableHandleAnyRef vp);
+
+
 [[nodiscard]] extern bool CheckEqRefValue(JSContext* cx, HandleValue v,
                                           MutableHandleAnyRef vp);
 
@@ -507,7 +519,6 @@ using RootedValVectorN = Rooted<ValVectorN<N>>;
                                             const TypeDef* typeDef,
                                             HandleValue v,
                                             MutableHandleAnyRef vp);
-
 class NoDebug;
 class DebugCodegenVal;
 
