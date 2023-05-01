426862d2f27f4d81c9ae1d99cf11f190680dd9dc
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-05-01 00:52:10 +0000

    Bug 1830182 - Fix broken calls to Promise.all in 2 anti-tracking tests. r=pbz
    Differential Revision: https://phabricator.services.mozilla.com/D176589
    
    UltraBlame original commit: 5d922f4c8fc54ccfd38988fda4bd10cadcd9d51e

--- toolkit/components/antitracking/test/browser/browser_urlQueryStringStripping_nimbus.js
+++ toolkit/components/antitracking/test/browser/browser_urlQueryStringStripping_nimbus.js
@@ -137,6 +137,8 @@ add_task(async function test() {
   }
 
   
-  await Promise
-    .all[(BrowserTestUtils.closeWindow(normalWindow), BrowserTestUtils.closeWindow(pbWindow))];
+  await Promise.all([
+    BrowserTestUtils.closeWindow(normalWindow),
+    BrowserTestUtils.closeWindow(pbWindow),
+  ]);
 });
