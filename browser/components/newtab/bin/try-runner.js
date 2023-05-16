9cb0d46404b2d8c0252445ed0dd2bb3f2eb4b0d0
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-05-16 00:49:11 +0000

    Bug 1619842 - Migrate from sass-lint to stylelint. r=omc-reviewers,thecount,emcminn
    Fix a bug where VS Code's eslint integration does not work for the
    newtab project because of a conflict caused by sass-lint's eslint
    dependency. Switching over to stylelint eliminates that dependency and
    generally modernizes our SCSS linting. stylelint doesn't have a 1 to 1
    replacement for every sass-lint rule, so a few rules have been changed.
    
    Differential Revision: https://phabricator.services.mozilla.com/D173605
    
    UltraBlame original commit: 84a1894bdd4d1911445275f18dc10789d6ed1f69

--- browser/components/newtab/bin/try-runner.js
+++ browser/components/newtab/bin/try-runner.js
@@ -49,6 +49,8 @@ function logStart(name) {
   console.log(`TEST START | ${name}`);
 }
 
+const npmCommand = process.platform === "win32" ? "npm.cmd" : "npm";
+
 function checkBundles() {
   logStart("checkBundles");
 
@@ -63,7 +65,7 @@ function checkBundles() {
   let ASbefore = readFileSync(ASbundle, "utf8");
   let AWbefore = readFileSync(AWbundle, "utf8");
 
-  let bundleExitCode = execOut("npm", ["run", "bundle"]).exitCode;
+  let bundleExitCode = execOut(npmCommand, ["run", "bundle"]).exitCode;
 
   let ASafter = readFileSync(ASbundle, "utf8");
   let AWafter = readFileSync(AWbundle, "utf8");
@@ -88,7 +90,7 @@ function karma() {
   logStart("karma");
 
   const errors = [];
-  const { exitCode, out } = execOut("npm", [
+  const { exitCode, out } = execOut(npmCommand, [
     "run",
     "testmc:unit",
     
@@ -141,14 +143,14 @@ function karma() {
   return errors.length === 0 && !exitCode;
 }
 
-function sasslint() {
-  logStart("sasslint");
-  const { exitCode, out } = execOut("npm", [
+function stylelint() {
+  logStart("stylelint");
+  const { exitCode, out } = execOut(npmCommand, [
     "run",
     "--silent",
-    "lint:sasslint",
+    "lint:stylelint",
     "--",
-    "--format",
+    "--formatter",
     "json",
   ]);
 
@@ -158,20 +160,35 @@ function sasslint() {
   }
 
   let fileObjects = JSON.parse(out);
-  let filesWithIssues = fileObjects.filter(
-    file => file.warningCount || file.errorCount
-  );
 
   let errs = [];
   let errorString;
-  filesWithIssues.forEach(file => {
-    file.messages.forEach(messageObj => {
-      errorString = `${file.filePath}(${messageObj.line}, ${messageObj.column}): ${messageObj.message} (${messageObj.ruleId})`;
-      errs.push(errorString);
-    });
-  });
+  for (const file of fileObjects) {
+    let fileErrs = [];
+    for (const warningObj of file.warnings) {
+      let relativePath = path.relative(
+        path.join(__dirname, ".."),
+        path.join(...file.source.split("/"))
+      );
+      errorString = `${relativePath}(${warningObj.line}, ${warningObj.column}): ${warningObj.text}`;
+      fileErrs.push(errorString);
+    }
+    for (const warningObj of file.invalidOptionWarnings) {
+      errorString = `config: ${warningObj.text}`;
+      fileErrs.push(errorString);
+    }
+    for (const warningObj of file.parseErrors) {
+      errorString = `construct-specific parser error: ${warningObj.text}`;
+      fileErrs.push(errorString);
+    }
+    if (fileErrs.length) {
+      errs.push(...fileErrs);
+    } else if (file.errored) {
+      errs.push(`unknown error: ${file.source}`);
+    }
+  }
 
-  const errors = logErrors("sasslint", errs);
+  const errors = logErrors("stylelint", errs);
 
   
   return errors.length === 0 && !exitCode;
@@ -195,7 +212,7 @@ function zipCodeCoverage() {
 }
 
 const tests = {};
-const success = [checkBundles, karma, zipCodeCoverage, sasslint].every(
+const success = [checkBundles, karma, zipCodeCoverage, stylelint].every(
   t => (tests[t.name] = t())
 );
 console.log(tests);
