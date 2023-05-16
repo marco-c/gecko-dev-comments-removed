e76e1e86a70806a0b4da2f69704506f1d52dc6cc
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-05-16 01:41:46 +0000

    Bug 1828517 - Vendor libwebrtc from 7a67dce582
    Upstream commit: https://webrtc.googlesource.com/src/+/7a67dce582bd75b9aee0226d5c477dd1c8b49981
        prefer absl::optional for rtx-time
    
        BUG=webrtc:12420
    
        Change-Id: I1876369a43370ddbd223da866823a497108a8655
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/291336
        Reviewed-by: Ilya Nikolaevskiy <ilnikwebrtc.org>
        Reviewed-by: Florent Castelli <orphiswebrtc.org>
        Commit-Queue: Philipp Hancke <phanckemicrosoft.com>
        Cr-Commit-Position: refs/heads/main{#39198}
    
    UltraBlame original commit: 0e28288e373d66715ef7ee047ebed2f33d7d37dd

--- third_party/libwebrtc/media/engine/webrtc_video_engine.h
+++ third_party/libwebrtc/media/engine/webrtc_video_engine.h
@@ -252,7 +252,7 @@ class WebRtcVideoChannel : public VideoMediaChannel,
     webrtc::UlpfecConfig ulpfec;
     int flexfec_payload_type;  
     int rtx_payload_type;      
-    int rtx_time;              
+    absl::optional<int> rtx_time;
   };
 
   struct ChangedSendParameters {
@@ -465,7 +465,7 @@ class WebRtcVideoChannel : public VideoMediaChannel,
     void SetFeedbackParameters(bool lntf_enabled,
                                bool nack_enabled,
                                webrtc::RtcpMode rtcp_mode,
-                               int rtx_time);
+                               absl::optional<int> rtx_time);
     void SetRecvParameters(const ChangedRecvParameters& recv_params);
 
     void OnFrame(const webrtc::VideoFrame& frame) override;
