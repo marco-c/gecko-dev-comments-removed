80f3e565be7f77fc6a57de6aa055fffe908b73b7
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-05-16 00:39:29 +0000

    Bug 1829660: pass `true` for `aFireEvents` and defer hiding the popover and updating the popover states. r=emilio
    Pass `true` for `aFireEvents` to match the spec.
    Defer hiding the popover because it may run script, which is forbidden,
    because `SetAttrAndNotify` runs with a script blocker
    (`mozAutoDocUpdate`).
    
    Updated test expectations:
      The assertion count increased because
      <https://searchfox.org/mozilla-central/rev/4e6970cd336f1b642c0be6c9b697b4db5f7b6aeb/dom/base/Document.cpp#14948-14949>
      is violated more often, which was violated before. No new
      `NS_ASSERTION`s are violated.
    
      "[Changing a popover from auto to manual (via attr), and then auto during 'beforetoggle' works]
         expected: FAIL" still fails because
         <https://searchfox.org/mozilla-central/rev/4e6970cd336f1b642c0be6c9b697b4db5f7b6aeb/testing/web-platform/tests/html/semantics/popovers/popover-attribute-basic.html#334>
         is violated, which is unrelated to this change.
      Other instances of
      <https://searchfox.org/mozilla-central/rev/4e6970cd336f1b642c0be6c9b697b4db5f7b6aeb/testing/web-platform/tests/html/semantics/popovers/popover-attribute-basic.html#339>
      still fail too, but the general mechanism of firing events seems to
      work. Therefore it seems reasonable to investigate the remaining
      failures in bug 1816851.
    
    Differential Revision: https://phabricator.services.mozilla.com/D176660
    
    UltraBlame original commit: c64635691eee8d36cfc41a7b105841ebb559d155

--- dom/html/nsGenericHTMLElement.cpp
+++ dom/html/nsGenericHTMLElement.cpp
@@ -653,6 +653,39 @@ void nsGenericHTMLElement::BeforeSetAttr(int32_t aNamespaceID, nsAtom* aName,
                                                  aNotify);
 }
 
+void nsGenericHTMLElement::AfterSetPopoverAttr() {
+  const nsAttrValue* newValue = GetParsedAttr(nsGkAtoms::popover);
+
+  
+  PopoverState newState;
+  if (newValue) {
+    if (newValue->Type() == nsAttrValue::eEnum) {
+      newState = static_cast<dom::PopoverState>(newValue->GetEnumValue());
+    } else {
+      
+      newState = PopoverState::Manual;
+    }
+  } else {
+    
+    newState = PopoverState::None;
+  }
+  PopoverState oldState = GetPopoverState();
+  if (newState != oldState) {
+    if (oldState != PopoverState::None) {
+      HidePopoverInternal( true,
+                           true, IgnoreErrors());
+    }
+    
+    if (newState != PopoverState::None) {
+      EnsurePopoverData().SetPopoverState(newState);
+      PopoverPseudoStateUpdate(false, true);
+    } else {
+      ClearPopoverData();
+      RemoveStates(ElementState::POPOVER_OPEN);
+    }
+  }
+}
+
 void nsGenericHTMLElement::AfterSetAttr(int32_t aNamespaceID, nsAtom* aName,
                                         const nsAttrValue* aValue,
                                         const nsAttrValue* aOldValue,
@@ -667,33 +700,9 @@ void nsGenericHTMLElement::AfterSetAttr(int32_t aNamespaceID, nsAtom* aName,
       SyncEditorsOnSubtree(this);
     } else if (aName == nsGkAtoms::popover &&
                StaticPrefs::dom_element_popover_enabled()) {
-      
-      PopoverState newState;
-      if (aValue) {
-        if (aValue->Type() == nsAttrValue::eEnum) {
-          newState = static_cast<dom::PopoverState>(aValue->GetEnumValue());
-        } else {
-          
-          newState = PopoverState::Manual;
-        }
-      } else {
-        
-        newState = PopoverState::None;
-      }
-      PopoverState oldState = GetPopoverState();
-      if (newState != oldState) {
-        if (oldState != PopoverState::None) {
-          HidePopoverInternal( true,
-                               false, IgnoreErrors());
-        }
-        if (newState != PopoverState::None) {
-          EnsurePopoverData().SetPopoverState(newState);
-          PopoverPseudoStateUpdate(false, true);
-        } else {
-          ClearPopoverData();
-          RemoveStates(ElementState::POPOVER_OPEN);
-        }
-      }
+      nsContentUtils::AddScriptRunner(
+          NewRunnableMethod("nsGenericHTMLElement::AfterSetPopoverAttr", this,
+                            &nsGenericHTMLElement::AfterSetPopoverAttr));
     } else if (aName == nsGkAtoms::dir) {
       Directionality dir = eDir_LTR;
       
