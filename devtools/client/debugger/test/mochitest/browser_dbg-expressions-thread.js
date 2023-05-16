755ac6c2663c1c2247b53f094d00b1159f438a6c
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-05-16 00:46:46 +0000

    Bug 1831358 - [devtools] Remove debugger workers preference. r=devtools-reviewers,bomsy
    This is a builtin feature enabled by default for years.
    
    Addtionaly, if disabled it can prevent debugging page correctly with fission enabled.
    
    Also renamed workers to threads as this code is no longer specific to workers!
    
    Differential Revision: https://phabricator.services.mozilla.com/D177171
    
    UltraBlame original commit: 1bb9dd86e607e4e5f7384c9f6cdce37a684fdd75

--- devtools/client/debugger/test/mochitest/browser_dbg-expressions-thread.js
+++ devtools/client/debugger/test/mochitest/browser_dbg-expressions-thread.js
@@ -21,12 +21,10 @@ add_task(async function() {
     "simple2.js"
   );
 
-  
   const threadsPaneEl = await waitForElementWithSelector(
     dbg,
     ".threads-pane .header-label"
   );
-  threadsPaneEl.click();
 
   await waitForElement(dbg, "threadsPaneItems");
   const threadsEl = findAllElements(dbg, "threadsPaneItems");
