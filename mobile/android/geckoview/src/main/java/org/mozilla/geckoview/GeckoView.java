400bf6bace3bd5298558787efd149a84374e9cd9
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-05-16 01:03:26 +0000

    Bug 1824083 - Add API to allow Gecko to request a new Surface from the application. r=geckoview-reviewers,m_kato
    Adds a new interface, GeckoDisplay.NewSurfaceProvider, of which an
    implementation can be provided in the GeckoDisplay.SurfaceInfo passed
    to GeckoDisplay.surfaceChanged(). We include an implementation in
    the GeckoView class, which works by toggling the SurfaceView's
    visibility, causing a new surfaceChanged callback to be fired.
    
    Differential Revision: https://phabricator.services.mozilla.com/D176720
    
    UltraBlame original commit: e26fa9885b0e296812a73a34b657f11694d29533

--- mobile/android/geckoview/src/main/java/org/mozilla/geckoview/GeckoView.java
+++ mobile/android/geckoview/src/main/java/org/mozilla/geckoview/GeckoView.java
@@ -62,7 +62,7 @@ import org.mozilla.gecko.SurfaceViewWrapper;
 import org.mozilla.gecko.util.ThreadUtils;
 
 @UiThread
-public class GeckoView extends FrameLayout {
+public class GeckoView extends FrameLayout implements GeckoDisplay.NewSurfaceProvider {
   private static final String LOGTAG = "GeckoView";
   private static final boolean DEBUG = false;
 
@@ -115,6 +115,7 @@ public class GeckoView extends FrameLayout {
         mDisplay.surfaceChanged(
             new GeckoDisplay.SurfaceInfo.Builder(wrapper.getSurface())
                 .surfaceControl(wrapper.getSurfaceControl())
+                .newSurfaceProvider(GeckoView.this)
                 .size(wrapper.getWidth(), wrapper.getHeight())
                 .build());
         mDisplay.setDynamicToolbarMaxHeight(mDynamicToolbarMaxHeight);
@@ -145,6 +146,7 @@ public class GeckoView extends FrameLayout {
         mDisplay.surfaceChanged(
             new GeckoDisplay.SurfaceInfo.Builder(surface)
                 .surfaceControl(surfaceControl)
+                .newSurfaceProvider(GeckoView.this)
                 .size(width, height)
                 .build());
         mDisplay.setDynamicToolbarMaxHeight(mDynamicToolbarMaxHeight);
@@ -1208,5 +1210,24 @@ public class GeckoView extends FrameLayout {
       final PrintDocumentAdapter pda = new GeckoViewPrintDocumentAdapter(pdfStream, getContext());
       printManager.print("Firefox", pda, null);
     }
+  }
+
+  
+
+  @Override
+  public void requestNewSurface() {
+    
+    
+    
+    
+    
+    post(
+        new Runnable() {
+          @Override
+          public void run() {
+            mSurfaceWrapper.getView().setVisibility(View.INVISIBLE);
+            mSurfaceWrapper.getView().setVisibility(View.VISIBLE);
+          }
+        });
   }
 }
