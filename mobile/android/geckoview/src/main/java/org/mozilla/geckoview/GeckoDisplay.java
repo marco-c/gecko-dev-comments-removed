6503b94a04bc9d2bfa99cb51c8f4c73441d4ba08
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-05-16 01:02:26 +0000

    Bug 1824083 - Add API to allow Gecko to request a new Surface from the application. r=geckoview-reviewers,m_kato
    Adds a new interface, GeckoDisplay.NewSurfaceProvider, of which an
    implementation can be provided in the GeckoDisplay.SurfaceInfo passed
    to GeckoDisplay.surfaceChanged(). We include an implementation in
    the GeckoView class, which works by toggling the SurfaceView's
    visibility, causing a new surfaceChanged callback to be fired.
    
    Differential Revision: https://phabricator.services.mozilla.com/D176720
    
    UltraBlame original commit: bf5c5929ef7b668ca636f0d57d828ee6cd81f05e

--- mobile/android/geckoview/src/main/java/org/mozilla/geckoview/GeckoDisplay.java
+++ mobile/android/geckoview/src/main/java/org/mozilla/geckoview/GeckoDisplay.java
@@ -33,9 +33,33 @@ public class GeckoDisplay {
 
 
 
+
+
+  public interface NewSurfaceProvider {
+    
+
+
+
+
+
+
+
+
+
+
+
+    @UiThread
+    void requestNewSurface();
+  }
+
+  
+
+
+
   public static class SurfaceInfo {
      final @NonNull Surface mSurface;
      final @Nullable SurfaceControl mSurfaceControl;
+     final @Nullable NewSurfaceProvider mNewSurfaceProvider;
      final int mLeft;
      final int mTop;
      final int mWidth;
@@ -44,6 +68,7 @@ public class GeckoDisplay {
     private SurfaceInfo(final @NonNull Builder builder) {
       mSurface = builder.mSurface;
       mSurfaceControl = builder.mSurfaceControl;
+      mNewSurfaceProvider = builder.mNewSurfaceProvider;
       mLeft = builder.mLeft;
       mTop = builder.mTop;
       mWidth = builder.mWidth;
@@ -54,6 +79,7 @@ public class GeckoDisplay {
     public static class Builder {
       private Surface mSurface;
       private SurfaceControl mSurfaceControl;
+      private NewSurfaceProvider mNewSurfaceProvider;
       private int mLeft;
       private int mTop;
       private int mWidth;
@@ -82,6 +108,24 @@ public class GeckoDisplay {
       @UiThread
       public @NonNull Builder surfaceControl(final @Nullable SurfaceControl surfaceControl) {
         mSurfaceControl = surfaceControl;
+        return this;
+      }
+
+      
+
+
+
+
+
+
+
+
+
+
+      @UiThread
+      public @NonNull Builder newSurfaceProvider(
+          final @Nullable NewSurfaceProvider newSurfaceProvider) {
+        mNewSurfaceProvider = newSurfaceProvider;
         return this;
       }
 
