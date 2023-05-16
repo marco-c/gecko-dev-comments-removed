1392a216fd0a7dd03e20fd54b17598fe39720322
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-05-16 01:23:05 +0000

    Bug 1828517 - Vendor libwebrtc from 0b17d1466b
    Upstream commit: https://webrtc.googlesource.com/src/+/0b17d1466b971ec8e6b617a6cfc73d55307b6b50
        [Unwrap] Remove unused TimestampWrapAroundHandler from VideoChannel
    
        Bug: webrtc:13982
        Change-Id: I070e823e9e3866bb22ce802d85fb46e522aa9230
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/288743
        Reviewed-by: Ilya Nikolaevskiy <ilnikwebrtc.org>
        Auto-Submit: Evan Shrubsole <eshrwebrtc.org>
        Commit-Queue: Ilya Nikolaevskiy <ilnikwebrtc.org>
        Cr-Commit-Position: refs/heads/main{#38941}
    
    UltraBlame original commit: 4d83a5b0b7d61024a0a4ceccabe6cd4199aeb44f

--- third_party/libwebrtc/media/engine/webrtc_video_engine.h
+++ third_party/libwebrtc/media/engine/webrtc_video_engine.h
@@ -547,10 +547,6 @@ class WebRtcVideoChannel : public VideoMediaChannel,
     webrtc::Mutex sink_lock_;
     rtc::VideoSinkInterface<webrtc::VideoFrame>* sink_
         RTC_GUARDED_BY(sink_lock_);
-    
-    
-    rtc::TimestampWrapAroundHandler timestamp_wraparound_handler_
-        RTC_GUARDED_BY(sink_lock_);
     int64_t first_frame_timestamp_ RTC_GUARDED_BY(sink_lock_);
     
     
