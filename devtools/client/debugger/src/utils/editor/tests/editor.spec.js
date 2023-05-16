2768e5cf2ab2204a56fb8e5f59b900ca94723223
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-05-16 00:45:53 +0000

    Bug 1830240 - [devtools] Remove unused traverseResults methods. r=devtools-reviewers,bomsy
    Differential Revision: https://phabricator.services.mozilla.com/D176671
    
    UltraBlame original commit: 8414e854a500b8479d6a98491d973c5eab05b3cb

--- devtools/client/debugger/src/utils/editor/tests/editor.spec.js
+++ devtools/client/debugger/src/utils/editor/tests/editor.spec.js
@@ -3,7 +3,6 @@
 
 
 import {
-  traverseResults,
   toEditorLine,
   toEditorPosition,
   toEditorRange,
@@ -20,32 +19,6 @@ import {
 } from "../index";
 
 import { makeMockSource } from "../../test-mockup";
-
-describe("traverseResults", () => {
-  const e = { stopPropagation: jest.fn(), preventDefault: jest.fn() };
-  const ctx = {};
-  const query = "Awesome books";
-  const modifiers = {
-    caseSensitive: false,
-    regexMatch: false,
-    wholeWord: false,
-  };
-  it("traverses next", () => {
-    traverseResults(e, ctx, query, "next", modifiers);
-    expect(e.stopPropagation).toHaveBeenCalled();
-    expect(e.preventDefault).toHaveBeenCalled();
-    
-  });
-
-  it("traverses previous", () => {
-    e.stopPropagation.mockClear();
-    e.preventDefault.mockClear();
-    traverseResults(e, ctx, query, "prev", modifiers);
-    expect(e.stopPropagation).toHaveBeenCalled();
-    expect(e.preventDefault).toHaveBeenCalled();
-    
-  });
-});
 
 describe("toEditorLine", () => {
   it("returns an editor line", () => {
