0db19031ffd04b42d159c1e614f5ce9aa2fd0ac3
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-05-16 00:27:28 +0000

    Bug 1825808 - Popover: Complete ShowPopover and HidePopover, r=emilio
    Differential Revision: https://phabricator.services.mozilla.com/D175395
    
    UltraBlame original commit: daa0bcea7818bbf9be6f99c18979b0650d5be568

--- dom/base/Document.cpp
+++ dom/base/Document.cpp
@@ -14977,6 +14977,14 @@ nsTArray<Element*> Document::GetTopLayer() const {
   return elements;
 }
 
+bool Document::TopLayerContains(Element& aElement) const {
+  if (mTopLayer.IsEmpty()) {
+    return false;
+  }
+  nsWeakPtr weakElement = do_GetWeakReference(&aElement);
+  return mTopLayer.Contains(weakElement);
+}
+
 void Document::HideAllPopoversUntil(nsINode& aEndpoint,
                                     bool aFocusPreviousElement,
                                     bool aFireEvents) {
@@ -15029,7 +15037,19 @@ void Document::HidePopover(Element& aPopover, bool aFocusPreviousElement,
     return;
   }
 
-  
+  if (popoverHTMLEl->IsAutoPopover()) {
+    
+    
+    HideAllPopoversUntil(*popoverHTMLEl, aFocusPreviousElement, aFireEvents);
+    if (!popoverHTMLEl->CheckPopoverValidity(PopoverVisibilityState::Showing,
+                                             nullptr, aRv)) {
+      return;
+    }
+    
+    
+    
+    MOZ_ASSERT(GetTopmostAutoPopover() == popoverHTMLEl);
+  }
 
   aPopover.SetHasPopoverInvoker(false);
 
@@ -15046,7 +15066,7 @@ void Document::HidePopover(Element& aPopover, bool aFocusPreviousElement,
     }
   }
 
-  
+  RemovePopoverFromTopLayer(aPopover);
 
   popoverHTMLEl->PopoverPseudoStateUpdate(false, true);
   popoverHTMLEl->GetPopoverData()->SetPopoverVisibilityState(
@@ -15093,6 +15113,16 @@ void Document::AddToAutoPopoverList(Element& aElement) {
 
 void Document::RemoveFromAutoPopoverList(Element& aElement) {
   MOZ_ASSERT(aElement.IsAutoPopover());
+  TopLayerPop(aElement);
+}
+
+void Document::AddPopoverToTopLayer(Element& aElement) {
+  MOZ_ASSERT(aElement.GetPopoverData());
+  TopLayerPush(aElement);
+}
+
+void Document::RemovePopoverFromTopLayer(Element& aElement) {
+  MOZ_ASSERT(aElement.GetPopoverData());
   TopLayerPop(aElement);
 }
 
--- dom/html/nsGenericHTMLElement.cpp
+++ dom/html/nsGenericHTMLElement.cpp
@@ -3291,6 +3291,7 @@ void nsGenericHTMLElement::ShowPopover(ErrorResult& aRv) {
     return;
   }
   RefPtr<Document> document = OwnerDoc();
+  MOZ_ASSERT(!OwnerDoc()->TopLayerContains(*this));
 
   
   if (FireToggleEvent(PopoverVisibilityState::Hidden,
@@ -3300,7 +3301,21 @@ void nsGenericHTMLElement::ShowPopover(ErrorResult& aRv) {
   if (!CheckPopoverValidity(PopoverVisibilityState::Hidden, document, aRv)) {
     return;
   }
-  
+
+  if (IsAutoPopover()) {
+    RefPtr<Element> ancestor = GetTopmostPopoverAncestor();
+    if (!ancestor) {
+      ancestor = document->GetDocumentElement();
+    }
+    document->HideAllPopoversUntil(*ancestor, false, true);
+
+    
+    
+    if (!IsAutoPopover() ||
+        !CheckPopoverValidity(PopoverVisibilityState::Hidden, document, aRv)) {
+      return;
+    }
+  }
 
   const bool shouldRestoreFocus = !document->GetTopmostAutoPopover();
   
@@ -3312,7 +3327,7 @@ void nsGenericHTMLElement::ShowPopover(ErrorResult& aRv) {
         do_GetWeakReference(unretargetedFocus->AsElement());
   }
 
-  
+  document->AddPopoverToTopLayer(*this);
 
   PopoverPseudoStateUpdate(true, true);
   GetPopoverData()->SetPopoverVisibilityState(PopoverVisibilityState::Showing);
