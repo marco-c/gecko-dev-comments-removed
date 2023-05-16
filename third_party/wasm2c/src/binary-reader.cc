75d190f917f1368f940b2bdaf1f41054a6814072
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-05-16 00:43:04 +0000

    Bug 1831227 - Remove windows.h dependency in wasm2c host program r=glandium
    Differential Revision: https://phabricator.services.mozilla.com/D177123
    
    UltraBlame original commit: cc7b419c4bbea93ee3364cf6749a0d6dcbb0a991

--- third_party/wasm2c/src/binary-reader.cc
+++ third_party/wasm2c/src/binary-reader.cc
@@ -560,7 +560,14 @@ Index BinaryReader::NumTotalFuncs() {
 
 Result BinaryReader::ReadInitExpr(Index index) {
   
-  return ReadInstructions(true, read_end_, NULL);
+  Opcode final_opcode(Opcode::Invalid);
+  CHECK_RESULT(
+      ReadInstructions(true, read_end_, &final_opcode));
+  ERROR_UNLESS(state_.offset <= read_end_,
+               "init expression longer than given size");
+  ERROR_UNLESS(final_opcode == Opcode::End,
+               "init expression must end with END opcode");
+  return Result::Ok;
 }
 
 Result BinaryReader::ReadTable(Type* out_elem_type, Limits* out_elem_limits) {
