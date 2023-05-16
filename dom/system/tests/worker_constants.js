d21a0e56176348b1df7cd2b3496c4abb8ae1a272
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-05-16 01:45:27 +0000

    Bug 1776480 - Remove OS.File et al. r=Gijs,webidl,smaug
    This patch removes the vast majority of OS.File and support code. A few things remain:
    
    - The nsIOSFileConstantsService still exists, but the path related constants
      (OS.Constants.Path.*) are no longer added to the OS object. The plan is to
      replace this with a proper service e.g. Services.osConstants or similar) in
      bug 1786885.
    - There is still support for OS.File errors in ErrorSanitizer, which will be
      removed in bug 1775167.
    - The OS.File to IOUtils migration guide will be rewritten as general IOUtils
      documentation in bug 1830097.
    - dom/base/Document.cpp has a workaround for not loading osfile.jsm at startup,
      which may want to be reconsidered in bug 1830100.
    
    So long, and thanks for all the I/O.
    
    Differential Revision: https://phabricator.services.mozilla.com/D176543
    
    UltraBlame original commit: c698636dbfea4e9d7a50338981b7a4862404ea92

--- dom/system/tests/worker_constants.js
+++ dom/system/tests/worker_constants.js
@@ -15,13 +15,9 @@ self.onmessage = function(msg) {
   self.onmessage = function(msgInner) {
     log("ignored message " + JSON.stringify(msgInner.data));
   };
-  let { isDebugBuild, umask } = msg.data;
+  let { umask } = msg.data;
   try {
-    test_name();
-    test_xul();
-    test_debugBuildWorkerThread(isDebugBuild);
     test_umaskWorkerThread(umask);
-    test_bits();
   } catch (x) {
     log("Catching error: " + x);
     log("Stack: " + x.stack);
@@ -46,50 +42,11 @@ function isnot(a, b, description) {
 }
 
 
-function test_name() {
-  isnot(null, OS.Constants.Sys.Name, "OS.Constants.Sys.Name is defined");
-}
-
-
-function test_debugBuildWorkerThread(isDebugBuild) {
-  is(
-    isDebugBuild,
-    !!OS.Constants.Sys.DEBUG,
-    "OS.Constants.Sys.DEBUG is set properly on worker thread"
-  );
-}
-
-
 function test_umaskWorkerThread(umask) {
   is(
     umask,
     OS.Constants.Sys.umask,
     "OS.Constants.Sys.umask is set properly on worker thread: " +
       ("0000" + umask.toString(8)).slice(-4)
-  );
-}
-
-
-function test_xul() {
-  let lib;
-  isnot(null, OS.Constants.Path.libxul, "libxul is defined");
-  try {
-    lib = ctypes.open(OS.Constants.Path.libxul);
-    lib.declare("DumpJSStack", ctypes.default_abi, ctypes.void_t);
-  } catch (x) {
-    ok(false, "test_xul: Could not open libxul: " + x);
-  }
-  if (lib) {
-    lib.close();
-  }
-  ok(true, "test_xul: opened libxul successfully");
-}
-
-
-function test_bits() {
-  is(
-    OS.Constants.Sys.bits,
-    ctypes.int.ptr.size * 8,
-    "OS.Constants.Sys.bits is either 32 or 64"
   );
 }
