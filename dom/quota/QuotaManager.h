e263e365011cd17d848773eb481db21eb0a75252
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-05-16 00:34:04 +0000

    Bug 1781201 - Generate unique anonymous origins for private browsing; r=hsingh
    The generated origins are cached in a hash map. There's also a hash map for
    getting the original origin from a generated origin which will be needed for
    metadata loading once generated origins are actually used for creating origin
    directories and for creating metadata files.
    
    The patch is partially based on D176751.
    
    Differential Revision: https://phabricator.services.mozilla.com/D176875
    
    UltraBlame original commit: c42230da142c4548d96004bf8d4d7013f481f4e5

--- dom/quota/QuotaManager.h
+++ dom/quota/QuotaManager.h
@@ -579,6 +579,12 @@ class QuotaManager final : public BackgroundThreadObject {
 
   bool IsSanitizedOriginValid(const nsACString& aSanitizedOrigin);
 
+  Result<nsCString, nsresult> EnsureStorageOriginFromOrigin(
+      const nsACString& aOrigin);
+
+  Result<nsCString, nsresult> GetOriginFromStorageOrigin(
+      const nsACString& aStorageOrigin);
+
   int64_t GenerateDirectoryLockId();
 
   bool ShutdownStarted() const;
@@ -656,6 +662,10 @@ class QuotaManager final : public BackgroundThreadObject {
   
   
   nsTHashMap<nsCStringHashKey, bool> mValidOrigins;
+
+  
+  nsTHashMap<nsCStringHashKey, nsCString> mOriginToStorageOriginMap;
+  nsTHashMap<nsCStringHashKey, nsCString> mStorageOriginToOriginMap;
 
   
   
