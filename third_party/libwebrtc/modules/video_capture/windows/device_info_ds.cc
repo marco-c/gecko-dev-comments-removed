5b0633233fc4c04e53886fa3d5d67ac55381e434
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-05-16 00:57:39 +0000

    Bug 1828065 - cherry-pick upstream libwebrtc commit adf55790b6. r=webrtc-reviewers,mjf
    Upstream commit: https://webrtc.googlesource.com/src/+/adf55790b6ecf50c4bb2b2cf7d58441303b9d946
        In DeviceInfoDS free the frame duration list after use
    
        Per the docs, the caller is responsible for freeing the memory.
    
        Bug: chromium:1441804
        Change-Id: I9aaae493a1a86d8ab4f03930715a643a3c9fb61b
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/304061
        Reviewed-by: Per Kjellander <perkjwebrtc.org>
        Commit-Queue: Ilya Nikolaevskiy <ilnikwebrtc.org>
        Reviewed-by: Ilya Nikolaevskiy <ilnikwebrtc.org>
        Cr-Commit-Position: refs/heads/main{#39983}
    
    Differential Revision: https://phabricator.services.mozilla.com/D177237
    
    UltraBlame original commit: 854cc79e99130e6537eebe8433a6a7adf5e1578d

--- third_party/libwebrtc/modules/video_capture/windows/device_info_ds.cc
+++ third_party/libwebrtc/modules/video_capture/windows/device_info_ds.cc
@@ -520,11 +520,18 @@ int32_t DeviceInfoDS::CreateCapabilityMap(const char* deviceUniqueIdUTF8)
         hrVC = videoControlConfig->GetFrameRateList(
             outputCapturePin, tmp, size, &listSize, &frameDurationList);
 
+        if (hrVC == S_OK) {
+          maxFPS = GetMaxOfFrameArray(frameDurationList, listSize);
+        }
+
+        CoTaskMemFree(frameDurationList);
+        frameDurationList = NULL;
+        listSize = 0;
+
         
         
         
-        if (hrVC == S_OK &&
-            0 != (maxFPS = GetMaxOfFrameArray(frameDurationList, listSize))) {
+        if (0 != maxFPS) {
           capability.maxFPS = static_cast<int>(10000000 / maxFPS);
           capability.supportFrameRateControl = true;
         } else  
