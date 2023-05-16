70a2172c4ac5c779ed506617328e6f6b3c5a1d2e
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-05-16 00:29:41 +0000

    Backed out changeset f271e25759b2 (bug 1818567) for causing marionette and web-platform failures
    UltraBlame original commit: 1135b77e09f0574881e98f78ed1079939ab8a9c1

--- dom/media/platforms/PEMFactory.h
+++ dom/media/platforms/PEMFactory.h
@@ -22,7 +22,7 @@ class PEMFactory final {
   
   
   already_AddRefed<MediaDataEncoder> CreateEncoder(
-      const CreateEncoderParams& aParams, const bool aHardwareNotAllowed);
+      const CreateEncoderParams& aParams);
 
   bool SupportsMimeType(const nsACString& aMimeType) const;
 
