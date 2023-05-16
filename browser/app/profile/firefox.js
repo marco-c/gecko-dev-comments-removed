ef0f2f18dd98dbe0dd83e5b21f82d036883143ea
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-05-16 00:29:17 +0000

    Bug 1830913 - Pocket new tab turn on new regions. r=gvn
    Differential Revision: https://phabricator.services.mozilla.com/D176954
    
    UltraBlame original commit: 7f77fc993508c1392228d0b2aee8e02f4a65407f

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1616,7 +1616,7 @@ pref("browser.newtabpage.activity-stream.discoverystream.region-stories-block", 
   pref("browser.newtabpage.activity-stream.discoverystream.locale-list-config", "");
 #endif
 
-pref("browser.newtabpage.activity-stream.discoverystream.region-stories-config", "US,DE,CA,GB,IE,CH,AT,BE,IN");
+pref("browser.newtabpage.activity-stream.discoverystream.region-stories-config", "US,DE,CA,GB,IE,CH,AT,BE,IN,FR,IT,ES");
 
 pref("browser.newtabpage.activity-stream.discoverystream.region-bff-config", "FR,IT,ES");
 
@@ -1636,7 +1636,7 @@ pref("browser.newtabpage.activity-stream.discoverystream.spocs.personalized", tr
 
 
 pref("browser.newtabpage.activity-stream.discoverystream.onboardingExperience.dismissed", false);
-pref("browser.newtabpage.activity-stream.discoverystream.onboardingExperience.enabled", false);
+pref("browser.newtabpage.activity-stream.discoverystream.onboardingExperience.enabled", true);
 
 
 pref("browser.newtabpage.activity-stream.feeds.section.topstories", true);
