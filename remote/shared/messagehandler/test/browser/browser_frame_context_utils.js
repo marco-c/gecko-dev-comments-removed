5a83055d8e6776bb7963523fe5eb06b148e55a13
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-05-16 00:28:52 +0000

    Bug 1830404 - [remote] Rename transport helpers referring to frame r=webdriver-reviewers,whimboo
    Differential Revision: https://phabricator.services.mozilla.com/D176713
    
    UltraBlame original commit: a8099d7bcd4f558f440b244a9b1b26c880f6fd97

--- remote/shared/messagehandler/test/browser/browser_frame_context_utils.js
+++ remote/shared/messagehandler/test/browser/browser_frame_context_utils.js
@@ -4,7 +4,7 @@
 "use strict";
 
 const { isBrowsingContextCompatible } = ChromeUtils.importESModule(
-  "chrome://remote/content/shared/messagehandler/transports/FrameContextUtils.sys.mjs"
+  "chrome://remote/content/shared/messagehandler/transports/BrowsingContextUtils.sys.mjs"
 );
 const TEST_COM_PAGE = "https://example.com/document-builder.sjs?html=com";
 const TEST_NET_PAGE = "https://example.net/document-builder.sjs?html=net";
@@ -81,13 +81,16 @@ async function checkBrowsingContextCompatible(browser, browserId, expected) {
     browser,
     [browserId, expected],
     (_browserId, _expected) => {
-      const FrameContextUtils = ChromeUtils.importESModule(
-        "chrome://remote/content/shared/messagehandler/transports/FrameContextUtils.sys.mjs"
+      const BrowsingContextUtils = ChromeUtils.importESModule(
+        "chrome://remote/content/shared/messagehandler/transports/BrowsingContextUtils.sys.mjs"
       );
       is(
-        FrameContextUtils.isBrowsingContextCompatible(content.browsingContext, {
-          browserId: _browserId,
-        }),
+        BrowsingContextUtils.isBrowsingContextCompatible(
+          content.browsingContext,
+          {
+            browserId: _browserId,
+          }
+        ),
         _expected
       );
     }
