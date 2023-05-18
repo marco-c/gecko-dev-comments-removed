b07ac1d2d8f26f898a64b98c8425f9ed9fd59de6
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-05-18 15:59:29 +0000

    Backed out changeset 9b71e3543b10 (bug 1828537) for bc failure on HttpChannelChild.cpp . CLOSED TREE
    UltraBlame original commit: aff2a62355e4d337a902a4ca2144a72e7a646a32

--- netwerk/protocol/http/HttpChannelChild.cpp
+++ netwerk/protocol/http/HttpChannelChild.cpp
@@ -108,12 +108,6 @@ HttpChannelChild::~HttpChannelChild() {
   LOG(("Destroying HttpChannelChild @%p\n", this));
 
 #ifdef MOZ_DIAGNOSTIC_ASSERT_ENABLED
-  
-  
-  MOZ_DIAGNOSTIC_ASSERT(NS_IsMainThread());
-  
-  MOZ_DIAGNOSTIC_ASSERT(mEventQ->IsEmpty());
-
   if (mDoDiagnosticAssertWhenOnStopNotCalledOnDestroy && mAsyncOpenSucceeded &&
       !mSuccesfullyRedirected && !LoadOnStopRequestCalled()) {
     bool emptyBgChildQueue, nullBgChild;
