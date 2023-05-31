3e79e009faafbf6bbb19b0c350b6ef4c59e60fc9
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-05-31 11:09:51 +0000

    Bug 1834301 - Explicitely mark OOM when we suspect it r=gsvelto
    Differential Revision: https://phabricator.services.mozilla.com/D178657
    
    UltraBlame original commit: a615632d120ea343e07cd1e5480b1bf41d3e462b

--- ipc/glue/UtilityProcessImpl.cpp
+++ ipc/glue/UtilityProcessImpl.cpp
@@ -8,6 +8,10 @@
 #include "mozilla/ipc/IOThreadChild.h"
 #include "mozilla/GeckoArgs.h"
 
+#if defined(OS_WIN)
+#  include "nsExceptionHandler.h"
+#endif
+
 #if defined(OS_WIN) && defined(MOZ_SANDBOX)
 #  include "mozilla/sandboxTarget.h"
 #  include "WMF.h"
@@ -28,6 +32,20 @@ void UtilityProcessImpl::LoadLibraryOrCrash(LPCWSTR aLib) {
   HMODULE module = ::LoadLibraryW(aLib);
   if (!module) {
     DWORD err = ::GetLastError();
+    switch (err) {
+      
+      
+      
+      case ERROR_MOD_NOT_FOUND:
+      case ERROR_COMMITMENT_LIMIT:
+        
+        
+        CrashReporter::AnnotateOOMAllocationSize(1);
+        break;
+
+      default:
+        break;
+    }
     MOZ_CRASH_UNSAFE_PRINTF("Unable to preload module: 0x%lx", err);
   }
 }
