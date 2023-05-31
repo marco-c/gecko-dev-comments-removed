0b1a03931117ab466f1bd30ceae0743a6b2c363b
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-05-31 10:44:17 +0000

    Bug 1833266 - part5 : make sure the engine play would be triggered properly whenever starting running the media engine. r=jolin
    Differential Revision: https://phabricator.services.mozilla.com/D178207
    
    UltraBlame original commit: 641133062e8ff417f3873270ebc6ed3a91f0e101

--- dom/media/ExternalEngineStateMachine.cpp
+++ dom/media/ExternalEngineStateMachine.cpp
@@ -158,8 +158,8 @@ const char* ExternalEngineStateMachine::GetStateStr() const {
 }
 
 void ExternalEngineStateMachine::ChangeStateTo(State aNextState) {
-  LOG("Change state : '%s' -> '%s'", StateToStr(mState.mName),
-      StateToStr(aNextState));
+  LOG("Change state : '%s' -> '%s' (play-state=%d)", StateToStr(mState.mName),
+      StateToStr(aNextState), mPlayState.Ref());
   
   MOZ_ASSERT_IF(mState.IsInitEngine(), aNextState == State::ReadingMetadata ||
                                            aNextState == State::ShutdownEngine);
@@ -238,10 +238,6 @@ void ExternalEngineStateMachine::OnEngineInitSuccess() {
   mEngine->SetMediaInfo(*mInfo);
   SeekTarget target(mCurrentPosition.Ref(), SeekTarget::Type::Accurate);
   Seek(target);
-  
-  if (mPlayState == MediaDecoder::PLAY_STATE_PLAYING) {
-    mEngine->Play();
-  }
 }
 
 void ExternalEngineStateMachine::OnEngineInitFailure() {
@@ -728,6 +724,11 @@ void ExternalEngineStateMachine::MaybeFinishWaitForData() {
 
 void ExternalEngineStateMachine::StartRunningEngine() {
   ChangeStateTo(State::RunningEngine);
+  
+  
+  if (mPlayState == MediaDecoder::PLAY_STATE_PLAYING) {
+    mEngine->Play();
+  }
   if (HasAudio()) {
     RunningEngineUpdate(MediaData::Type::AUDIO_DATA);
   }
