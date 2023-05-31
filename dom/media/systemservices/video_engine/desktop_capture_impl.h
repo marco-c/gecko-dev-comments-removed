ec9d7370f12a029f38746d9338d6a1f6ee7f531a
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-05-31 11:17:02 +0000

    Bug 1809672 - Remove DesktopCaptureImpl::mRunning. r=ng
    Differential Revision: https://phabricator.services.mozilla.com/D174294
    
    UltraBlame original commit: 1f7f58b5735778bfae2aa8a6115a2818fbfbd840

--- dom/media/systemservices/video_engine/desktop_capture_impl.h
+++ dom/media/systemservices/video_engine/desktop_capture_impl.h
@@ -234,9 +234,6 @@ class DesktopCaptureImpl : public DesktopCapturer::Callback,
   webrtc::Timestamp mNextFrameMinimumTime;
   
   
-  bool mRunning;
-  
-  
   mozilla::DataMutex<std::set<rtc::VideoSinkInterface<VideoFrame>*>> mCallbacks;
 };
 
