e43f5df3e6a0f7272b4c0151b1c4e5d309fcea2c
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-05-31 10:52:46 +0000

    Bug 1822169 [wpt PR 38963] - Move path-object test generator file to common folder, a=testonly
    Automatic update from web-platform-tests
    Move path-object test generator file to common folder
    
    Move path-object tests generator file to the common folder, yaml-new.
    Note that the tests are common in HTML and OffscreenCanvas, so there is
    no new test added.
    
    Bug: 1275750
    
    Change-Id: I2aeff2ced2a102a1bf9ecfe2a829ed3542f14f50
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4307355
    Reviewed-by: Jean-Philippe Gravel <jpgravelchromium.org>
    Commit-Queue: Yi Xu <yiyixgoogle.com>
    Cr-Commit-Position: refs/heads/main{#1137960}
    
    --
    
    wpt-commits: fa9eab2eb50c9642845bed1cf23ede43b9588cce
    wpt-pr: 38963
    
    UltraBlame original commit: 3420d48199fc8a14abfc5d2d0cb5eef18be88a2c

--- testing/web-platform/tests/html/canvas/offscreen/path-objects/2d.path.roundrect.2.radii.2.dompoint.worker.js
+++ testing/web-platform/tests/html/canvas/offscreen/path-objects/2d.path.roundrect.2.radii.2.dompoint.worker.js
@@ -13,31 +13,30 @@ var t_fail = t.step_func(function(reason) {
 });
 t.step(function() {
 
-var canvas = new OffscreenCanvas(100, 50);
-var ctx = canvas.getContext('2d');
+  var canvas = new OffscreenCanvas(100, 50);
+  var ctx = canvas.getContext('2d');
 
-ctx.fillStyle = '#f00';
-ctx.fillRect(0, 0, 100, 50);
-ctx.roundRect(0, 0, 100, 50, [0, new DOMPoint(40, 20)]);
-ctx.fillStyle = '#0f0';
-ctx.fill();
+  ctx.fillStyle = '#f00';
+  ctx.fillRect(0, 0, 100, 50);
+  ctx.roundRect(0, 0, 100, 50, [0, new DOMPoint(40, 20)]);
+  ctx.fillStyle = '#0f0';
+  ctx.fill();
 
+  
+  _assertPixel(canvas, 79,1, 255,0,0,255);
+  _assertPixel(canvas, 58,1, 0,255,0,255);
+  _assertPixel(canvas, 98,10, 255,0,0,255);
+  _assertPixel(canvas, 98,21, 0,255,0,255);
 
-_assertPixel(canvas, 79,1, 255,0,0,255);
-_assertPixel(canvas, 58,1, 0,255,0,255);
-_assertPixel(canvas, 98,10, 255,0,0,255);
-_assertPixel(canvas, 98,21, 0,255,0,255);
+  
+  _assertPixel(canvas, 20,48, 255,0,0,255);
+  _assertPixel(canvas, 41,48, 0,255,0,255);
+  _assertPixel(canvas, 1,39, 255,0,0,255);
+  _assertPixel(canvas, 1,28, 0,255,0,255);
 
-
-_assertPixel(canvas, 20,48, 255,0,0,255);
-_assertPixel(canvas, 41,48, 0,255,0,255);
-_assertPixel(canvas, 1,39, 255,0,0,255);
-_assertPixel(canvas, 1,28, 0,255,0,255);
-
-
-_assertPixel(canvas, 1,1, 0,255,0,255);
-_assertPixel(canvas, 98,48, 0,255,0,255);
-t.done();
-
+  
+  _assertPixel(canvas, 1,1, 0,255,0,255);
+  _assertPixel(canvas, 98,48, 0,255,0,255);
+  t.done();
 });
 done();
--- testing/web-platform/tests/html/canvas/offscreen/path-objects/2d.path.roundrect.3.radii.2.dompointinit.worker.js
+++ testing/web-platform/tests/html/canvas/offscreen/path-objects/2d.path.roundrect.3.radii.2.dompointinit.worker.js
@@ -6,38 +6,37 @@
 importScripts("/resources/testharness.js");
 importScripts("/html/canvas/resources/canvas-tests.js");
 
-var t = async_test("Verify that when three radii are given to roundRect(), the second radius, specified as a DOMPoint, applies to the top-right and bottomInit-left corners.");
+var t = async_test("Verify that when three radii are given to roundRect(), the second radius, specified as a DOMPoint, applies to the top-right and bottom-left corners.");
 var t_pass = t.done.bind(t);
 var t_fail = t.step_func(function(reason) {
     throw reason;
 });
 t.step(function() {
 
-var canvas = new OffscreenCanvas(100, 50);
-var ctx = canvas.getContext('2d');
+  var canvas = new OffscreenCanvas(100, 50);
+  var ctx = canvas.getContext('2d');
 
-ctx.fillStyle = '#f00';
-ctx.fillRect(0, 0, 100, 50);
-ctx.roundRect(0, 0, 100, 50, [0, {x: 40, y: 20}, 0]);
-ctx.fillStyle = '#0f0';
-ctx.fill();
+  ctx.fillStyle = '#f00';
+  ctx.fillRect(0, 0, 100, 50);
+  ctx.roundRect(0, 0, 100, 50, [0, {x: 40, y: 20}, 0]);
+  ctx.fillStyle = '#0f0';
+  ctx.fill();
 
+  
+  _assertPixel(canvas, 79,1, 255,0,0,255);
+  _assertPixel(canvas, 58,1, 0,255,0,255);
+  _assertPixel(canvas, 98,10, 255,0,0,255);
+  _assertPixel(canvas, 98,21, 0,255,0,255);
 
-_assertPixel(canvas, 79,1, 255,0,0,255);
-_assertPixel(canvas, 58,1, 0,255,0,255);
-_assertPixel(canvas, 98,10, 255,0,0,255);
-_assertPixel(canvas, 98,21, 0,255,0,255);
+  
+  _assertPixel(canvas, 20,48, 255,0,0,255);
+  _assertPixel(canvas, 41,48, 0,255,0,255);
+  _assertPixel(canvas, 1,39, 255,0,0,255);
+  _assertPixel(canvas, 1,28, 0,255,0,255);
 
-
-_assertPixel(canvas, 20,48, 255,0,0,255);
-_assertPixel(canvas, 41,48, 0,255,0,255);
-_assertPixel(canvas, 1,39, 255,0,0,255);
-_assertPixel(canvas, 1,28, 0,255,0,255);
-
-
-_assertPixel(canvas, 1,1, 0,255,0,255);
-_assertPixel(canvas, 98,48, 0,255,0,255);
-t.done();
-
+  
+  _assertPixel(canvas, 1,1, 0,255,0,255);
+  _assertPixel(canvas, 98,48, 0,255,0,255);
+  t.done();
 });
 done();
