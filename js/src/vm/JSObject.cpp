c105add3c2c1e53e0b986402feadf5b9c612a07e
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-05-31 11:45:08 +0000

    Bug 1795816 - Remove Nightly Build guarding around Array.fromAsync in anticipation of train riding r=arai
    Differential Revision: https://phabricator.services.mozilla.com/D178481
    
    UltraBlame original commit: 7c2aa1e8b11283d9ea5797cc0589bdce265ab5cd

--- js/src/vm/JSObject.cpp
+++ js/src/vm/JSObject.cpp
@@ -2228,6 +2228,7 @@ JS_PUBLIC_API bool js::ShouldIgnorePropertyDefinition(JSContext* cx,
        id == NameToId(cx->names().groupToMap))) {
     return true;
   }
+#endif
 
   
   
@@ -2237,7 +2238,6 @@ JS_PUBLIC_API bool js::ShouldIgnorePropertyDefinition(JSContext* cx,
       id == NameToId(cx->names().fromAsync)) {
     return true;
   }
-#endif
 
 #ifdef ENABLE_CHANGE_ARRAY_BY_COPY
   if (key == JSProto_Array &&
