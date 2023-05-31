547b44efd02df961c5d85c0d9478d79210c4e31d
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-05-31 10:45:13 +0000

    Bug 1832850 - Part 5: Move the allocateObject definition into gc/Nursery.h r=jandem
    In line with allocateString and allocateBigInt, this definition can easily be
    moved to the header.
    
    Differential Revision: https://phabricator.services.mozilla.com/D177931
    
    UltraBlame original commit: 3e0a5d26dc60543a1af0ff6783587fb8915ecf66

--- js/src/gc/Nursery.h
+++ js/src/gc/Nursery.h
@@ -134,11 +134,11 @@ class alignas(TypicalCacheLineSize) Nursery {
 
   
   
-  void* allocateObject(gc::AllocSite* site, size_t size, const JSClass* clasp);
-
-  
-  
-  void* allocateCell(gc::AllocSite* site, size_t size, JS::TraceKind kind);
+  void* allocateObject(gc::AllocSite* site, size_t size, const JSClass* clasp) {
+    MOZ_ASSERT_IF(clasp->hasFinalize() && !clasp->isProxyObject(),
+                  CanNurseryAllocateFinalizedClass(clasp));
+    return allocateCell(site, size, JS::TraceKind::Object);
+  }
 
   void* allocateBigInt(gc::AllocSite* site, size_t size) {
     MOZ_ASSERT(canAllocateBigInts());
@@ -148,6 +148,10 @@ class alignas(TypicalCacheLineSize) Nursery {
     MOZ_ASSERT(canAllocateStrings());
     return allocateCell(site, size, JS::TraceKind::String);
   }
+
+  
+  
+  void* allocateCell(gc::AllocSite* site, size_t size, JS::TraceKind kind);
 
   static size_t nurseryCellHeaderSize() {
     return sizeof(gc::NurseryCellHeader);
