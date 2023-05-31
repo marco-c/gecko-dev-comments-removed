19e9af8c8c45035c3f05ce9eea68d10c6f341649
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-05-31 12:05:19 +0000

    Bug 1834053 - Shorten task metadata name for side-by-side job r=perftest-reviewers,taskgraph-reviewers,jmaher,kshampur
    treeherder.job_type.name which stores task.task["metadata"]["name"] has a max length of 140 characters.
    test-android-hw-a51-11-0-aarch64-shippable-qr browsertime-tp6m-essential-geckoview-microsoft-support-nofis has the longest task name from all pageload jobs, and it fits in the current form of the string in 140 chaaracters.
    
    Differential Revision: https://phabricator.services.mozilla.com/D179062
    
    UltraBlame original commit: 1565861edeebf64845658a3f48cb89fa4515bb68

--- taskcluster/gecko_taskgraph/actions/side_by_side.py
+++ taskcluster/gecko_taskgraph/actions/side_by_side.py
@@ -48,7 +48,7 @@ def side_by_side_modifier(task, input):
     
     task.task["metadata"][
         "name"
-    ] = f"{input['label']} for {input['platform']} {input['test_name']} from revision {input['base_revision'][:12]} to {input['new_revision'][:12]}"
+    ] = f"{input['platform']} {input['test_name']} {input['base_revision'][:12]} {input['new_revision'][:12]}"
     
     
     task.task["extra"]["treeherder"]["symbol"] += f"-{input['symbol']}"
