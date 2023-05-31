cc38be2c3dbc255ba09d078f0ab2c517108108d3
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-05-31 12:13:49 +0000

    Bug 1833427 - Add bookmark import from HTML file in new migration wizard. r=fluent-reviewers,kpatenio,flod
    Original patch by Tim Giles <tgilesmozilla.com>, completed by Mike Conley <mconleymozilla.com>.
    
    Differential Revision: https://phabricator.services.mozilla.com/D179273
    
    UltraBlame original commit: 8dee8a344f4f5177ec644ded088054651cb3c488

--- browser/components/migration/tests/browser/browser_no_browsers_state.js
+++ browser/components/migration/tests/browser/browser_no_browsers_state.js
@@ -51,8 +51,13 @@ add_task(async function test_browser_no_programs() {
     );
   });
 
+  
+  
   await SpecialPowers.pushPrefEnv({
-    set: [["signon.management.page.fileImport.enabled", false]],
+    set: [
+      ["signon.management.page.fileImport.enabled", false],
+      ["browser.migrate.bookmarks-file.enabled", false],
+    ],
   });
 
   await withMigrationWizardDialog(async prefsWin => {
