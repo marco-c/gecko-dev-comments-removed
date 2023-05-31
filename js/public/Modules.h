3bdefa79512515aacce1fec821ee38d8837f9772
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-05-31 11:57:21 +0000

    Bug 1803984 - Add assertion that we only remove unlinked modules from the map r=allstarschh
    It would not be correct to remove modules that were already linked or evaluated.
    
    
    
    Depends on D178298
    
    Differential Revision: https://phabricator.services.mozilla.com/D178787
    
    UltraBlame original commit: bcbcbdf4f449c192d93d71cacd7ab0a6707f11ea

--- js/public/Modules.h
+++ js/public/Modules.h
@@ -299,6 +299,11 @@ extern JS_PUBLIC_API JSObject* GetModuleEnvironment(
 
 extern JS_PUBLIC_API void ClearModuleEnvironment(JSObject* moduleObj);
 
+
+
+
+extern JS_PUBLIC_API void AssertModuleUnlinked(JSObject* moduleObj);
+
 }  
 
 #endif  
