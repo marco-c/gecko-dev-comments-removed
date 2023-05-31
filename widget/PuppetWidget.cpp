e2cc7e13a6c7d294c7aa12c10d22158d3aa2170a
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-05-31 11:14:06 +0000

    Bug 1834042 - Make nsWindow::ConstrainPosition account for decorations. r=stransky
    Differential Revision: https://phabricator.services.mozilla.com/D178545
    
    UltraBlame original commit: ff1720f8fc346c5d19b53805337e223135c46b6e

--- widget/PuppetWidget.cpp
+++ widget/PuppetWidget.cpp
@@ -72,9 +72,6 @@ static bool MightNeedIMEFocus(const widget::InitData* aInitData) {
 #endif
 }
 
-
-const size_t PuppetWidget::kMaxDimension = 4000;
-
 NS_IMPL_ISUPPORTS_INHERITED(PuppetWidget, nsBaseWidget,
                             TextEventDispatcherListener)
 
--- widget/PuppetWidget.h
+++ widget/PuppetWidget.h
@@ -57,8 +57,6 @@ class PuppetWidget : public nsBaseWidget,
   typedef nsBaseWidget Base;
 
   
-  static const size_t kMaxDimension;
-
  public:
   explicit PuppetWidget(BrowserChild* aBrowserChild);
 
@@ -89,12 +87,6 @@ class PuppetWidget : public nsBaseWidget,
   virtual void Show(bool aState) override;
 
   virtual bool IsVisible() const override { return mVisible; }
-
-  virtual void ConstrainPosition(bool , int32_t* aX,
-                                 int32_t* aY) override {
-    *aX = kMaxDimension;
-    *aY = kMaxDimension;
-  }
 
   
   virtual void Move(double aX, double aY) override {}
