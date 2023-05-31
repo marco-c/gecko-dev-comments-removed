be21a6de7957c1375c1c15c2e344af0c0c33684a
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-05-31 11:19:47 +0000

    Bug 1821996 - [wdspec] Move all webdriver classic tests to webdriver/tests/classic r=webdriver-reviewers,whimboo
    Differential Revision: https://phabricator.services.mozilla.com/D177295
    
    UltraBlame original commit: 02d9c5625cf4017a5e9e7a6eeeab6953587dc08c

--- testing/web-platform/tests/webdriver/tests/classic/new_session/create_alwaysMatch.py
+++ testing/web-platform/tests/webdriver/tests/classic/new_session/create_alwaysMatch.py
@@ -4,13 +4,12 @@ import pytest
 
 from .conftest import product, flatten
 
-
 from tests.support.asserts import assert_success
-from tests.new_session.support.create import valid_data
+from tests.classic.new_session.support.create import valid_data
 
 
 @pytest.mark.parametrize("key,value", flatten(product(*item) for item in valid_data))
 def test_valid(new_session, add_browser_capabilities, key, value):
     response, _ = new_session({"capabilities": {
-        "firstMatch": [add_browser_capabilities({key: value})]}})
+        "alwaysMatch": add_browser_capabilities({key: value})}})
     assert_success(response)
