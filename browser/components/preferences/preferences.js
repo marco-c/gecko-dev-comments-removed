9096feb79e8e75f68543c69ffbf4910894ff8ba2
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-06-01 00:27:57 +0000

    Backed out changeset af6ffefb5572 (bug 1831259) for causing bc failures in browser_aboutPrefs_fc_check_cantApply.js CLOSED TREE
    UltraBlame original commit: e3b54b179d77bcfbc580bbceb69527343fb0265e

--- browser/components/preferences/preferences.js
+++ browser/components/preferences/preferences.js
@@ -155,24 +155,43 @@ var gLastCategory = { category: undefined, subcategory: undefined };
 const gXULDOMParser = new DOMParser();
 var gCategoryModules = new Map();
 var gCategoryInits = new Map();
+function init_category_if_required(category) {
+  let categoryInfo = gCategoryInits.get(category);
+  if (!categoryInfo) {
+    throw new Error(
+      "Unknown in-content prefs category! Can't init " + category
+    );
+  }
+  if (categoryInfo.inited) {
+    return null;
+  }
+  return categoryInfo.init();
+}
 
 function register_module(categoryName, categoryObject) {
   gCategoryModules.set(categoryName, categoryObject);
   gCategoryInits.set(categoryName, {
-    _initted: false,
-    init() {
+    inited: false,
+    async init() {
       let startTime = performance.now();
-      if (this._initted) {
-        return;
-      }
-      this._initted = true;
       let template = document.getElementById("template-" + categoryName);
       if (template) {
         
-        template.replaceWith(template.content);
+        let frag = template.content;
+        await document.l10n.translateFragment(frag);
+
+        
+        document.l10n.pauseObserving();
+        template.replaceWith(frag);
+        document.l10n.resumeObserving();
+
+        
+        
+        Preferences.queueUpdateOfAllElements();
       }
 
       categoryObject.init();
+      this.inited = true;
       ChromeUtils.addProfilerMarker(
         "Preferences",
         { startTime },
@@ -367,28 +386,24 @@ async function gotoPref(
   }
   window.history.replaceState(category, document.title);
 
-  let categoryInfo = gCategoryInits.get(category);
-  if (!categoryInfo) {
-    let err = new Error(
-      "Unknown in-content prefs category! Can't init " + category
+  try {
+    await init_category_if_required(category);
+  } catch (ex) {
+    console.error(
+      new Error(
+        "Error initializing preference category " + category + ": " + ex
+      )
     );
-    console.error(err);
-    throw err;
+    throw ex;
   }
-  categoryInfo.init();
 
-  if (document.hasPendingL10nMutations) {
-    await new Promise(r =>
-      document.addEventListener("L10nMutationsFinished", r, { once: true })
-    );
-    
-    
-    if (
-      gLastCategory.category !== category ||
-      gLastCategory.subcategory !== subcategory
-    ) {
-      return;
-    }
+  
+  
+  if (
+    gLastCategory.category !== category ||
+    gLastCategory.subcategory !== subcategory
+  ) {
+    return;
   }
 
   search(category, "data-category");
