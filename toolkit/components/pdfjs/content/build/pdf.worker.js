f743dfb6e1a5357b3cf2cf3d47e18b8a431c2bcc
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-06-16 02:08:13 +0000

    Bug 1838552 - Update PDF.js to 9af50dc358fe4e4fa1ca93db1f5eb5560391f382 r=pdfjs-reviewers,calixte
    Differential Revision: https://phabricator.services.mozilla.com/D181009
    
    UltraBlame original commit: 401066ea9e1fd55d147e72bc4a5cc141e7d3cd3e

--- toolkit/components/pdfjs/content/build/pdf.worker.js
+++ toolkit/components/pdfjs/content/build/pdf.worker.js
@@ -101,7 +101,7 @@ class WorkerMessageHandler {
       docId,
       apiVersion
     } = docParams;
-    const workerVersion = '3.8.49';
+    const workerVersion = '3.8.58';
     if (apiVersion !== workerVersion) {
       throw new Error(`The API version "${apiVersion}" does not match ` + `the Worker version "${workerVersion}".`);
     }
@@ -2056,6 +2056,17 @@ class Ref {
     }
     return `${this.num}R${this.gen}`;
   }
+  static fromString(str) {
+    const ref = RefCache[str];
+    if (ref) {
+      return ref;
+    }
+    const m = /^(\d+)R(\d*)$/.exec(str);
+    if (!m || m[1] === "0") {
+      return null;
+    }
+    return RefCache[str] = new Ref(parseInt(m[1]), !m[2] ? 0 : parseInt(m[2]));
+  }
   static get(num, gen) {
     const key = gen === 0 ? `${num}R` : `${num}R${gen}`;
     return RefCache[key] ||= new Ref(num, gen);
@@ -3061,6 +3072,23 @@ class Page {
       bbox: this.xfaFactory.getBoundingBox(this.pageIndex)
     } : null);
   }
+  #replaceIdByRef(annotations, deletedAnnotations) {
+    for (const annotation of annotations) {
+      if (annotation.id) {
+        const ref = _primitives.Ref.fromString(annotation.id);
+        if (!ref) {
+          (0, _util.warn)(`A non-linked annotation cannot be modified: ${annotation.id}`);
+          continue;
+        }
+        if (annotation.deleted) {
+          deletedAnnotations.put(ref);
+          continue;
+        }
+        annotation.ref = ref;
+        delete annotation.id;
+      }
+    }
+  }
   async saveNewAnnotations(handler, task, annotations) {
     if (this.xfaFactory) {
       throw new Error("XFA: Cannot save new annotations.");
@@ -3077,8 +3105,10 @@ class Page {
       systemFontCache: this.systemFontCache,
       options: this.evaluatorOptions
     });
+    const deletedAnnotations = new _primitives.RefSet();
+    this.#replaceIdByRef(annotations, deletedAnnotations);
     const pageDict = this.pageDict;
-    const annotationsArray = this.annotations.slice();
+    const annotationsArray = this.annotations.filter(a => !(a instanceof _primitives.Ref && deletedAnnotations.has(a)));
     const newData = await _annotation.AnnotationFactory.saveNewAnnotations(partialEvaluator, task, annotations);
     for (const {
       ref
@@ -3164,10 +3194,13 @@ class Page {
       options: this.evaluatorOptions
     });
     const newAnnotationsByPage = !this.xfaFactory ? (0, _core_utils.getNewAnnotationsMap)(annotationStorage) : null;
+    let deletedAnnotations = null;
     let newAnnotationsPromise = Promise.resolve(null);
     if (newAnnotationsByPage) {
       const newAnnotations = newAnnotationsByPage.get(this.pageIndex);
       if (newAnnotations) {
+        deletedAnnotations = new _primitives.RefSet();
+        this.#replaceIdByRef(newAnnotations, deletedAnnotations);
         newAnnotationsPromise = _annotation.AnnotationFactory.printNewAnnotations(partialEvaluator, task, newAnnotations);
       }
     }
@@ -3190,6 +3223,18 @@ class Page {
     });
     return Promise.all([pageListPromise, this._parsedAnnotations, newAnnotationsPromise]).then(function ([pageOpList, annotations, newAnnotations]) {
       if (newAnnotations) {
+        annotations = annotations.filter(a => !(a.ref && deletedAnnotations.has(a.ref)));
+        for (let i = 0, ii = newAnnotations.length; i < ii; i++) {
+          const newAnnotation = newAnnotations[i];
+          if (newAnnotation.refToReplace) {
+            const j = annotations.findIndex(a => a.ref && (0, _primitives.isRefsEqual)(a.ref, newAnnotation.refToReplace));
+            if (j >= 0) {
+              annotations.splice(j, 1, newAnnotation);
+              newAnnotations.splice(i--, 1);
+              ii--;
+            }
+          }
+        }
         annotations = annotations.concat(newAnnotations);
       }
       if (annotations.length === 0 || intent & _util.RenderingIntentFlag.ANNOTATIONS_DISABLE) {
@@ -4153,9 +4198,9 @@ class PDFDocument {
 }
 exports.PDFDocument = PDFDocument;
 
- }),
-
- ((__unused_webpack_module, exports, __w_pdfjs_require__) => {
+/***/ }),
+/* 10 */
+/***/ ((__unused_webpack_module, exports, __w_pdfjs_require__) => {
 
 
 
@@ -4293,6 +4338,9 @@ class AnnotationFactory {
     const dependencies = [];
     const promises = [];
     for (const annotation of annotations) {
+      if (annotation.deleted) {
+        continue;
+      }
       switch (annotation.annotationType) {
         case _util.AnnotationEditorType.FREETEXT:
           if (!baseFontRef) {
@@ -4334,6 +4382,9 @@ class AnnotationFactory {
     } = evaluator.options;
     const promises = [];
     for (const annotation of annotations) {
+      if (annotation.deleted) {
+        continue;
+      }
       switch (annotation.annotationType) {
         case _util.AnnotationEditorType.FREETEXT:
           promises.push(FreeTextAnnotation.createNewPrintAnnotation(xref, annotation, {
@@ -4442,6 +4493,7 @@ class Annotation {
     const MK = dict.get("MK");
     this.setBorderAndBackgroundColors(MK);
     this.setRotation(MK);
+    this.ref = params.ref instanceof _primitives.Ref ? params.ref : null;
     this._streams = [];
     if (this.appearance) {
       this._streams.push(this.appearance);
@@ -5050,7 +5102,7 @@ class MarkupAnnotation extends Annotation {
     this._streams.push(this.appearance, appearanceStream);
   }
   static async createNewAnnotation(xref, annotation, dependencies, params) {
-    const annotationRef = xref.getNewTemporaryRef();
+    const annotationRef = annotation.ref || xref.getNewTemporaryRef();
     const ap = await this.createNewAppearanceStream(annotation, xref, params);
     const buffer = [];
     let annotationDict;
@@ -5081,11 +5133,15 @@ class MarkupAnnotation extends Annotation {
     const annotationDict = this.createNewDict(annotation, xref, {
       ap
     });
-    return new this.prototype.constructor({
+    const newAnnotation = new this.prototype.constructor({
       dict: annotationDict,
       xref,
       isOffscreenCanvasSupported: params.isOffscreenCanvasSupported
     });
+    if (annotation.ref) {
+      newAnnotation.ref = newAnnotation.refToReplace = annotation.ref;
+    }
+    return newAnnotation;
   }
 }
 exports.MarkupAnnotation = MarkupAnnotation;
@@ -5097,7 +5153,6 @@ class WidgetAnnotation extends Annotation {
       xref
     } = params;
     const data = this.data;
-    this.ref = params.ref;
     this._needAppearances = params.needAppearances;
     data.annotationType = _util.AnnotationType.WIDGET;
     if (data.fieldName === undefined) {
@@ -7083,9 +7138,9 @@ class FileAttachmentAnnotation extends MarkupAnnotation {
   }
 }
 
- }),
-
- ((__unused_webpack_module, exports, __w_pdfjs_require__) => {
+/***/ }),
+/* 11 */
+/***/ ((__unused_webpack_module, exports, __w_pdfjs_require__) => {
 
 
 
@@ -8846,10 +8901,8 @@ class PartialEvaluator {
       }
       return;
     }
-    const softMask = dict.get("SM", "SMask") || false;
-    const mask = dict.get("Mask") || false;
     const SMALL_IMAGE_DIMENSIONS = 200;
-    if (isInline && !softMask && !mask && w + h < SMALL_IMAGE_DIMENSIONS) {
+    if (isInline && !dict.has("SMask") && !dict.has("Mask") && w + h < SMALL_IMAGE_DIMENSIONS) {
       const imageObj = new _image.PDFImage({
         xref: this.xref,
         res: resources,
@@ -12192,9 +12245,9 @@ class EvaluatorPreprocessor {
 }
 exports.EvaluatorPreprocessor = EvaluatorPreprocessor;
 
- }),
-
- ((__unused_webpack_module, exports, __w_pdfjs_require__) => {
+/***/ }),
+/* 14 */
+/***/ ((__unused_webpack_module, exports, __w_pdfjs_require__) => {
 
 
 
@@ -12654,9 +12707,9 @@ class CMapFactory {
 }
 exports.CMapFactory = CMapFactory;
 
- }),
-
- ((__unused_webpack_module, exports, __w_pdfjs_require__) => {
+/***/ }),
+/* 15 */
+/***/ ((__unused_webpack_module, exports, __w_pdfjs_require__) => {
 
 
 
@@ -12921,9 +12974,9 @@ class BinaryCMapReader {
 }
 exports.BinaryCMapReader = BinaryCMapReader;
 
- }),
-
- ((__unused_webpack_module, exports, __w_pdfjs_require__) => {
+/***/ }),
+/* 16 */
+/***/ ((__unused_webpack_module, exports, __w_pdfjs_require__) => {
 
 
 
@@ -14001,9 +14054,9 @@ class Linearization {
 }
 exports.Linearization = Linearization;
 
- }),
-
- ((__unused_webpack_module, exports, __w_pdfjs_require__) => {
+/***/ }),
+/* 17 */
+/***/ ((__unused_webpack_module, exports, __w_pdfjs_require__) => {
 
 
 
@@ -14078,9 +14131,9 @@ class Ascii85Stream extends _decode_stream.DecodeStream {
 }
 exports.Ascii85Stream = Ascii85Stream;
 
- }),
-
- ((__unused_webpack_module, exports, __w_pdfjs_require__) => {
+/***/ }),
+/* 18 */
+/***/ ((__unused_webpack_module, exports, __w_pdfjs_require__) => {
 
 
 
@@ -14224,9 +14277,9 @@ class StreamsSequenceStream extends DecodeStream {
 }
 exports.StreamsSequenceStream = StreamsSequenceStream;
 
- }),
-
- ((__unused_webpack_module, exports, __w_pdfjs_require__) => {
+/***/ }),
+/* 19 */
+/***/ ((__unused_webpack_module, exports, __w_pdfjs_require__) => {
 
 
 
@@ -14285,9 +14338,9 @@ class AsciiHexStream extends _decode_stream.DecodeStream {
 }
 exports.AsciiHexStream = AsciiHexStream;
 
- }),
-
- ((__unused_webpack_module, exports, __w_pdfjs_require__) => {
+/***/ }),
+/* 20 */
+/***/ ((__unused_webpack_module, exports, __w_pdfjs_require__) => {
 
 
 
@@ -14335,9 +14388,9 @@ class CCITTFaxStream extends _decode_stream.DecodeStream {
 }
 exports.CCITTFaxStream = CCITTFaxStream;
 
- }),
-
- ((__unused_webpack_module, exports, __w_pdfjs_require__) => {
+/***/ }),
+/* 21 */
+/***/ ((__unused_webpack_module, exports, __w_pdfjs_require__) => {
 
 
 
@@ -14863,9 +14916,9 @@ class CCITTFaxDecoder {
 }
 exports.CCITTFaxDecoder = CCITTFaxDecoder;
 
- }),
-
- ((__unused_webpack_module, exports, __w_pdfjs_require__) => {
+/***/ }),
+/* 22 */
+/***/ ((__unused_webpack_module, exports, __w_pdfjs_require__) => {
 
 
 
@@ -15111,9 +15164,9 @@ class FlateStream extends _decode_stream.DecodeStream {
 }
 exports.FlateStream = FlateStream;
 
- }),
-
- ((__unused_webpack_module, exports, __w_pdfjs_require__) => {
+/***/ }),
+/* 23 */
+/***/ ((__unused_webpack_module, exports, __w_pdfjs_require__) => {
 
 
 
@@ -15172,9 +15225,9 @@ class Jbig2Stream extends _decode_stream.DecodeStream {
 }
 exports.Jbig2Stream = Jbig2Stream;
 
- }),
-
- ((__unused_webpack_module, exports, __w_pdfjs_require__) => {
+/***/ }),
+/* 24 */
+/***/ ((__unused_webpack_module, exports, __w_pdfjs_require__) => {
 
 
 
@@ -16923,9 +16976,9 @@ class Jbig2Image {
 }
 exports.Jbig2Image = Jbig2Image;
 
- }),
-
- ((__unused_webpack_module, exports) => {
+/***/ }),
+/* 25 */
+/***/ ((__unused_webpack_module, exports) => {
 
 
 
@@ -17259,9 +17312,9 @@ class ArithmeticDecoder {
 }
 exports.ArithmeticDecoder = ArithmeticDecoder;
 
- }),
-
- ((__unused_webpack_module, exports, __w_pdfjs_require__) => {
+/***/ }),
+/* 26 */
+/***/ ((__unused_webpack_module, exports, __w_pdfjs_require__) => {
 
 
 
@@ -17340,9 +17393,9 @@ class JpegStream extends _decode_stream.DecodeStream {
 }
 exports.JpegStream = JpegStream;
 
- }),
-
- ((__unused_webpack_module, exports, __w_pdfjs_require__) => {
+/***/ }),
+/* 27 */
+/***/ ((__unused_webpack_module, exports, __w_pdfjs_require__) => {
 
 
 
@@ -18424,9 +18477,9 @@ class JpegImage {
 }
 exports.JpegImage = JpegImage;
 
- }),
-
- ((__unused_webpack_module, exports, __w_pdfjs_require__) => {
+/***/ }),
+/* 28 */
+/***/ ((__unused_webpack_module, exports, __w_pdfjs_require__) => {
 
 
 
@@ -18542,9 +18595,9 @@ function grayToRGBA(src, dest) {
   }
 }
 
- }),
-
- ((__unused_webpack_module, exports, __w_pdfjs_require__) => {
+/***/ }),
+/* 29 */
+/***/ ((__unused_webpack_module, exports, __w_pdfjs_require__) => {
 
 
 
@@ -18607,9 +18660,9 @@ class JpxStream extends _decode_stream.DecodeStream {
 }
 exports.JpxStream = JpxStream;
 
- }),
-
- ((__unused_webpack_module, exports, __w_pdfjs_require__) => {
+/***/ }),
+/* 30 */
+/***/ ((__unused_webpack_module, exports, __w_pdfjs_require__) => {
 
 
 
@@ -20532,9 +20585,9 @@ class ReversibleTransform extends Transform {
   }
 }
 
- }),
-
- ((__unused_webpack_module, exports, __w_pdfjs_require__) => {
+/***/ }),
+/* 31 */
+/***/ ((__unused_webpack_module, exports, __w_pdfjs_require__) => {
 
 
 
@@ -20659,9 +20712,9 @@ class LZWStream extends _decode_stream.DecodeStream {
 }
 exports.LZWStream = LZWStream;
 
- }),
-
- ((__unused_webpack_module, exports, __w_pdfjs_require__) => {
+/***/ }),
+/* 32 */
+/***/ ((__unused_webpack_module, exports, __w_pdfjs_require__) => {
 
 
 
@@ -20860,9 +20913,9 @@ class PredictorStream extends _decode_stream.DecodeStream {
 }
 exports.PredictorStream = PredictorStream;
 
- }),
-
- ((__unused_webpack_module, exports, __w_pdfjs_require__) => {
+/***/ }),
+/* 33 */
+/***/ ((__unused_webpack_module, exports, __w_pdfjs_require__) => {
 
 
 
@@ -20907,9 +20960,9 @@ class RunLengthStream extends _decode_stream.DecodeStream {
 }
 exports.RunLengthStream = RunLengthStream;
 
- }),
-
- ((__unused_webpack_module, exports, __w_pdfjs_require__) => {
+/***/ }),
+/* 34 */
+/***/ ((__unused_webpack_module, exports, __w_pdfjs_require__) => {
 
 
 
@@ -23341,9 +23394,9 @@ class ErrorFont {
 }
 exports.ErrorFont = ErrorFont;
 
- }),
-
- ((__unused_webpack_module, exports, __w_pdfjs_require__) => {
+/***/ }),
+/* 35 */
+/***/ ((__unused_webpack_module, exports, __w_pdfjs_require__) => {
 
 
 
@@ -24463,9 +24516,9 @@ class CFFCompiler {
       data: [],
       length: 0,
       add(data) {
-        if (data.length <= 65536) {
+        try {
           this.data.push(...data);
-        } else {
+        } catch {
           this.data = this.data.concat(data);
         }
         this.length = this.data.length;
@@ -24835,9 +24888,9 @@ class CFFCompiler {
 }
 exports.CFFCompiler = CFFCompiler;
 
- }),
-
- ((__unused_webpack_module, exports) => {
+/***/ }),
+/* 36 */
+/***/ ((__unused_webpack_module, exports) => {
 
 
 
@@ -24852,9 +24905,9 @@ exports.ExpertCharset = ExpertCharset;
 const ExpertSubsetCharset = [".notdef", "space", "dollaroldstyle", "dollarsuperior", "parenleftsuperior", "parenrightsuperior", "twodotenleader", "onedotenleader", "comma", "hyphen", "period", "fraction", "zerooldstyle", "oneoldstyle", "twooldstyle", "threeoldstyle", "fouroldstyle", "fiveoldstyle", "sixoldstyle", "sevenoldstyle", "eightoldstyle", "nineoldstyle", "colon", "semicolon", "commasuperior", "threequartersemdash", "periodsuperior", "asuperior", "bsuperior", "centsuperior", "dsuperior", "esuperior", "isuperior", "lsuperior", "msuperior", "nsuperior", "osuperior", "rsuperior", "ssuperior", "tsuperior", "ff", "fi", "fl", "ffi", "ffl", "parenleftinferior", "parenrightinferior", "hyphensuperior", "colonmonetary", "onefitted", "rupiah", "centoldstyle", "figuredash", "hypheninferior", "onequarter", "onehalf", "threequarters", "oneeighth", "threeeighths", "fiveeighths", "seveneighths", "onethird", "twothirds", "zerosuperior", "onesuperior", "twosuperior", "threesuperior", "foursuperior", "fivesuperior", "sixsuperior", "sevensuperior", "eightsuperior", "ninesuperior", "zeroinferior", "oneinferior", "twoinferior", "threeinferior", "fourinferior", "fiveinferior", "sixinferior", "seveninferior", "eightinferior", "nineinferior", "centinferior", "dollarinferior", "periodinferior", "commainferior"];
 exports.ExpertSubsetCharset = ExpertSubsetCharset;
 
- }),
-
- ((__unused_webpack_module, exports) => {
+/***/ }),
+/* 37 */
+/***/ ((__unused_webpack_module, exports) => {
 
 
 
@@ -24897,9 +24950,9 @@ function getEncoding(encodingName) {
   }
 }
 
- }),
-
- ((__unused_webpack_module, exports, __w_pdfjs_require__) => {
+/***/ }),
+/* 38 */
+/***/ ((__unused_webpack_module, exports, __w_pdfjs_require__) => {
 
 
 
@@ -25012,9 +25065,9 @@ function normalizeFontName(name) {
   return name.replaceAll(/[,_]/g, "-").replaceAll(/\s/g, "");
 }
 
- }),
-
- ((__unused_webpack_module, exports, __w_pdfjs_require__) => {
+/***/ }),
+/* 39 */
+/***/ ((__unused_webpack_module, exports, __w_pdfjs_require__) => {
 
 
 
@@ -29559,9 +29612,9 @@ const getDingbatsGlyphsUnicode = (0, _core_utils.getLookupTableFactory)(function
 });
 exports.getDingbatsGlyphsUnicode = getDingbatsGlyphsUnicode;
 
- }),
-
- ((__unused_webpack_module, exports, __w_pdfjs_require__) => {
+/***/ }),
+/* 40 */
+/***/ ((__unused_webpack_module, exports, __w_pdfjs_require__) => {
 
 
 
@@ -57657,8 +57710,8 @@ Object.defineProperty(exports, "WorkerMessageHandler", ({
   }
 }));
 var _worker = __w_pdfjs_require__(1);
-const pdfjsVersion = '3.8.49';
-const pdfjsBuild = 'f2a29e858';
+const pdfjsVersion = '3.8.58';
+const pdfjsBuild = '9af50dc35';
 })();
 
  	return __webpack_exports__;
