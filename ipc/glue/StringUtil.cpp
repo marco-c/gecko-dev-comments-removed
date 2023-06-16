d87c18a159fa5259516d3eddb20181e2f23d270b
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-06-16 00:41:25 +0000

    Bug 1836989 - Rename the hacky string conversion function in ipc/glue/StringUtil.cpp. r=ipc-reviewers,nika
    Differential Revision: https://phabricator.services.mozilla.com/D180095
    
    UltraBlame original commit: ed7804ffc016687db27f68ab46e27c0ffefda3af

--- ipc/glue/StringUtil.cpp
+++ ipc/glue/StringUtil.cpp
@@ -31,7 +31,7 @@ namespace base {
 
 
 template <typename FromType, typename ToType>
-ToType GhettoStringConvert(const FromType& in) {
+ToType HackyStringConvert(const FromType& in) {
   
   ToType out;
   out.resize(in.length());
@@ -62,14 +62,14 @@ namespace base {
 #if !defined(XP_DARWIN) && !defined(XP_WIN)
 std::string SysWideToUTF8(const std::wstring& wide) {
   
-  return GhettoStringConvert<std::wstring, std::string>(wide);
+  return HackyStringConvert<std::wstring, std::string>(wide);
 }
 #endif
 
 #if !defined(XP_DARWIN) && !defined(XP_WIN)
 std::wstring SysUTF8ToWide(const StringPiece& utf8) {
   
-  return GhettoStringConvert<StringPiece, std::wstring>(utf8);
+  return HackyStringConvert<StringPiece, std::wstring>(utf8);
 }
 
 std::string SysWideToNativeMB(const std::wstring& wide) {
