b63957b8531d8dcb083dfdadbd271c02ebd0e1ff
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-06-16 01:59:53 +0000

    Bug 1837979 - Remove unused security.identitypopup.recordEventTelemetry pref r=pbz
    Differential Revision: https://phabricator.services.mozilla.com/D180684
    
    UltraBlame original commit: f0dc85b2ee809a170df719f567d44a63acd923bb

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1717,7 +1717,6 @@ pref("pdfjs.handleOctetStream", true);
 
 pref("sidebar.position_start", true);
 
-pref("security.identitypopup.recordEventTelemetry", true);
 pref("security.protectionspopup.recordEventTelemetry", true);
 pref("security.app_menu.recordEventTelemetry", true);
 
