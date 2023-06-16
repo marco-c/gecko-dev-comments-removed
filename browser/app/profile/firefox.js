b12cc9bf31cc2e0e8d23e8946665e7c19fa5f8d1
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-06-16 00:47:07 +0000

    Bug 1836265 - add Nimbus feature and fallback pref for 2023 shopping work, r=jhirsch
    Differential Revision: https://phabricator.services.mozilla.com/D179934
    
    UltraBlame original commit: 26223a83a24f664231ce6fd54367411ffff5389b

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -700,6 +700,11 @@ pref("browser.search.separatePrivateDefault.ui.banner.max", 0);
 pref("browser.search.serpEventTelemetry.enabled", false);
 
 
+
+
+pref("browser.shopping.experience2023.enabled", false);
+
+
 pref("browser.privatebrowsing.vpnpromourl", "https://vpn.mozilla.org/?utm_source=firefox-browser&utm_medium=firefox-%CHANNEL%-browser&utm_campaign=private-browsing-vpn-link");
 
 
