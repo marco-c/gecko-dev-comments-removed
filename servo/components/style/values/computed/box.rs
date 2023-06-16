6f950b7e9f30e6eb684a14e5c2207d70c6cf096e
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-06-16 00:29:46 +0000

    Backed out changeset 9e2c7d3018de (bug 1804761) for causing wpt failures on content-visibility-068.html CLOSED TREE
    UltraBlame original commit: 1cf3742ac6dcf4fa57256506dad12b389e1e3071

--- servo/components/style/values/computed/box.rs
+++ servo/components/style/values/computed/box.rs
@@ -25,20 +25,6 @@ pub type VerticalAlign = GenericVerticalAlign<LengthPercentage>;
 
 pub type ContainIntrinsicSize = GenericContainIntrinsicSize<NonNegativeLength>;
 
-impl ContainIntrinsicSize {
-    
-    pub fn add_auto_if_needed(&self) -> Option<Self> {
-        use crate::Zero;
-        
-        
-        Some(match *self {
-            Self::None => Self::AutoLength(Zero::zero()),
-            Self::Length(ref l) => Self::AutoLength(*l),
-            Self::AutoLength(..) => return None,
-        })
-    }
-}
-
 
 pub type LineClamp = GenericLineClamp<Integer>;
 
