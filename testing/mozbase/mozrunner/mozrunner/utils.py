45c37a6f0815107b7bb9a92729481373e16aec70
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-06-16 02:10:29 +0000

    Bug 1836825 - Set MOZ_DEVELOPER_REPO_DIR in mozrunner.test_environment() r=gbrown
    Moves the relevant piece from mochitest/reftest to mozrunner. This does not set MOZ_DEVELOPER_OBJ_DIR because mozinfo.json doesn't have topobjdir. We could include it in https://searchfox.org/mozilla-central/rev/fb55a4ee44a9a95d099aa806ca494eb988252ded/python/mozbuild/mozbuild/mozinfo.py#32 but that's out of scope here.
    
    (Not sure why Linux is okay without this. macOS is okay without this as it does not depend on MOZ_DEVELOPER_REPO_DIR since https://bugzilla.mozilla.org/show_bug.cgi?id=1380690.)
    
    Differential Revision: https://phabricator.services.mozilla.com/D180948
    
    UltraBlame original commit: 762fe7706638080e51583790b4fccd5f642fa2c9

--- testing/mozbase/mozrunner/mozrunner/utils.py
+++ testing/mozbase/mozrunner/mozrunner/utils.py
@@ -120,6 +120,9 @@ def test_environment(
         env[envVar] = os.path.pathsep.join([path for path in envValue if path])
 
     
+    env["MOZ_DEVELOPER_REPO_DIR"] = mozinfo.info.get("topsrcdir")
+
+    
     env["GNOME_DISABLE_CRASH_DIALOG"] = "1"
     env["XRE_NO_WINDOWS_CRASH_DIALOG"] = "1"
 
