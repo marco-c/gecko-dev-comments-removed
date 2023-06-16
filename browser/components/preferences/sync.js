ec6502a8891342c9a7819c49a36f713f89a4caca
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-06-16 02:10:19 +0000

    Bug 1838549 - fix sync now button label disappearing by using separate fluent strings for both states, r=markh,fluent-reviewers,flod,mconley
    Differential Revision: https://phabricator.services.mozilla.com/D181080
    
    UltraBlame original commit: 8fa5403a47ee05e4324a8d096d42a05b8e42b8c6

--- browser/components/preferences/sync.js
+++ browser/components/preferences/sync.js
@@ -300,21 +300,15 @@ var gSyncPane = {
 
   _updateSyncNow(syncing) {
     let butSyncNow = document.getElementById("syncNow");
-    if (syncing) {
-      butSyncNow.setAttribute("label", butSyncNow.getAttribute("labelsyncing"));
+    let fluentID = syncing ? "prefs-syncing-button" : "prefs-sync-now-button";
+    if (document.l10n.getAttributes(butSyncNow).id != fluentID) {
+      
+      
+      
       butSyncNow.removeAttribute("accesskey");
-      butSyncNow.disabled = true;
-    } else {
-      butSyncNow.setAttribute(
-        "label",
-        butSyncNow.getAttribute("labelnotsyncing")
-      );
-      butSyncNow.setAttribute(
-        "accesskey",
-        butSyncNow.getAttribute("accesskeynotsyncing")
-      );
-      butSyncNow.disabled = false;
+      document.l10n.setAttributes(butSyncNow, fluentID);
     }
+    butSyncNow.disabled = syncing;
   },
 
   updateWeavePrefs() {
