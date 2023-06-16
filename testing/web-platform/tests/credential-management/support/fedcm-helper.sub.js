9130870cf14fd6fc31e241ed59edde3aab5ed3f2
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-06-16 01:44:24 +0000

    Bug 1837057 [wpt PR 40400] - [FedCM] Fix WPT test defaults, a=testonly
    Automatic update from web-platform-tests
    [FedCM] Fix WPT test defaults
    
    Currently, the mediation parameter is not passed, which means it will be
    set to 'optional'. It's better for most tests to use 'required', and
    instead use 'optional' for the auto re-authn test.
    
    Change-Id: I744a3b486c8edb356510f55a465c8db4b1c58589
    
    --
    
    wpt-commits: c966f9c87c60601dff9f30aadf26fd3eaac0cd9b
    wpt-pr: 40400
    
    UltraBlame original commit: ba79aef55533034d6d07dfe9d29e1c3128b3827a

--- testing/web-platform/tests/credential-management/support/fedcm-helper.sub.js
+++ testing/web-platform/tests/credential-management/support/fedcm-helper.sub.js
@@ -29,7 +29,7 @@ export function set_alt_fedcm_cookie() {
 
 
 
-export function default_request_options(manifest_filename) {
+export function request_options_with_mediation_required(manifest_filename) {
   if (manifest_filename === undefined) {
     manifest_filename = "manifest.py";
   }
@@ -40,15 +40,16 @@ credential-management/support/fedcm/${manifest_filename}`;
       providers: [{
         configURL: manifest_path,
         clientId: '1',
-        nonce: '2',
-      }]
+        nonce: '2'
+      }],
+      mediation: 'required'
     }
   };
 }
 
 
 
-export function default_alt_request_options(manifest_filename) {
+export function alt_request_options_with_mediation_required(manifest_filename) {
   if (manifest_filename === undefined) {
     manifest_filename = "manifest.py";
   }
@@ -59,19 +60,20 @@ credential-management/support/fedcm/${manifest_filename}`;
       providers: [{
         configURL: manifest_path,
         clientId: '1',
-        nonce: '2',
-      }]
+        nonce: '2'
+      }],
+      mediation: 'required'
     }
   };
 }
 
 
 
-export function request_options_with_auto_reauthn(manifest_filename) {
-  let options = default_request_options(manifest_filename);
+export function request_options_with_mediation_optional(manifest_filename) {
+  let options = alt_request_options_with_mediation_required(manifest_filename);
   
   options.identity.providers[0].clientId = '123';
-  options.identity.autoReauthn = true;
+  options.identity.mediation = 'optional';
 
   return options;
 }
