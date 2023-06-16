183a0e7c28c0c188cf66fb90703e2b83b6379315
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-06-16 02:01:25 +0000

    Bug 1837061 - part10 : make MFCDMProxy refcount thread-safe. r=jolin
    Depends on D180399
    
    Differential Revision: https://phabricator.services.mozilla.com/D180497
    
    UltraBlame original commit: bda83c36ca2d0462cd252574556d4e5470af4ee9

--- dom/media/platforms/wmf/MFCDMProxy.h
+++ dom/media/platforms/wmf/MFCDMProxy.h
@@ -21,7 +21,7 @@ namespace mozilla {
 
 
 class MFCDMProxy {
-  NS_INLINE_DECL_REFCOUNTING(MFCDMProxy);
+  NS_INLINE_DECL_THREADSAFE_REFCOUNTING(MFCDMProxy);
 
   explicit MFCDMProxy(IMFContentDecryptionModule* aCDM) : mCDM(aCDM) {}
 
