b363c6dc8dbb305a4be821a928a986f51149d65d
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-06-16 01:23:53 +0000

    Bug 1837722 - Remove unused toolkit.aboutPerformance.showInternals pref r=florian
    Differential Revision: https://phabricator.services.mozilla.com/D180518
    
    UltraBlame original commit: 14b1a630b5a0d46be7cc823014b5b9fadbb9df76

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -3744,12 +3744,6 @@ pref("dom.payments.request.supportedRegions", "US,CA");
   pref("toolkit.telemetry.overrideUpdateChannel", "nightly-asan");
 #endif
 
-#ifdef RELEASE_OR_BETA
-  pref("toolkit.aboutPerformance.showInternals", false);
-#else
-  pref("toolkit.aboutPerformance.showInternals", true);
-#endif
-
 
 pref("toolkit.aboutProcesses.showAllSubframes", false);
 
