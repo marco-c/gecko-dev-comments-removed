effe48dd693d6b76cf7eeef2e933eb65173ac24f
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-06-16 02:01:50 +0000

    Bug 1659763 - Fix failing x-frame-options web platform tests; r=freddyb,necko-reviewers,valentin
    Differential Revision: https://phabricator.services.mozilla.com/D180234
    
    UltraBlame original commit: 3fc04d8eb32b11cf44fd5ad011b0b37d26d39ffe

--- dom/security/FramingChecker.cpp
+++ dom/security/FramingChecker.cpp
@@ -5,26 +5,26 @@
 
 
 #include "FramingChecker.h"
-#include "nsCharSeparatedTokenizer.h"
+
+#include <stdint.h>  
+
+#include "nsCOMPtr.h"
 #include "nsContentUtils.h"
-#include "nsCSPUtils.h"
-#include "nsDocShell.h"
+#include "nsDebug.h"
+#include "nsError.h"
 #include "nsHttpChannel.h"
 #include "nsContentSecurityUtils.h"
-#include "nsGlobalWindowOuter.h"
-#include "nsIChannel.h"
-#include "nsIConsoleReportCollector.h"
-#include "nsIContentSecurityPolicy.h"
+#include "nsIContentPolicy.h"
 #include "nsIScriptError.h"
-#include "nsNetUtil.h"
-#include "nsQueryObject.h"
+#include "nsLiteralString.h"
 #include "nsTArray.h"
-#include "mozilla/BasePrincipal.h"
-#include "mozilla/dom/nsCSPUtils.h"
-#include "mozilla/dom/LoadURIOptionsBinding.h"
+#include "nsStringFwd.h"
+#include "mozilla/Assertions.h"
 #include "mozilla/dom/WindowGlobalParent.h"
-#include "mozilla/NullPrincipal.h"
 #include "mozilla/net/HttpBaseChannel.h"
+#include "mozilla/RefPtr.h"
+#include "mozilla/Services.h"
+#include "mozilla/Unused.h"
 
 #include "nsIObserverService.h"
 
@@ -57,72 +57,13 @@ void FramingChecker::ReportError(const char* aMessageTag,
   httpChannel->AddConsoleReport(nsIScriptError::errorFlag, "X-Frame-Options"_ns,
                                 nsContentUtils::eSECURITY_PROPERTIES, spec, 0,
                                 0, nsDependentCString(aMessageTag), params);
-}
-
-
-bool FramingChecker::CheckOneFrameOptionsPolicy(nsIHttpChannel* aHttpChannel,
-                                                const nsAString& aPolicy) {
-  nsCOMPtr<nsIURI> uri;
-  aHttpChannel->GetURI(getter_AddRefs(uri));
-
-  
-  if (!aPolicy.LowerCaseEqualsLiteral("deny") &&
-      !aPolicy.LowerCaseEqualsLiteral("sameorigin")) {
-    ReportError("XFrameOptionsInvalid", aHttpChannel, uri, aPolicy);
-    return true;
-  }
 
   
   
-  bool checkSameOrigin = aPolicy.LowerCaseEqualsLiteral("sameorigin");
-
-  nsCOMPtr<nsILoadInfo> loadInfo = aHttpChannel->LoadInfo();
-  RefPtr<mozilla::dom::BrowsingContext> ctx;
-  loadInfo->GetBrowsingContext(getter_AddRefs(ctx));
-
-  while (ctx) {
-    nsCOMPtr<nsIPrincipal> principal;
-    
-    
-    
-    
-    
-    if (XRE_IsParentProcess()) {
-      WindowGlobalParent* window = ctx->Canonical()->GetCurrentWindowGlobal();
-      if (window) {
-        
-        
-        
-        principal = window->DocumentPrincipal();
-      }
-    } else if (nsPIDOMWindowOuter* windowOuter = ctx->GetDOMWindow()) {
-      principal = nsGlobalWindowOuter::Cast(windowOuter)->GetPrincipal();
-    }
-
-    if (principal && principal->IsSystemPrincipal()) {
-      return true;
-    }
-
-    if (checkSameOrigin) {
-      bool isSameOrigin = principal && principal->IsSameOrigin(uri);
-      
-      if (!isSameOrigin) {
-        ReportError("XFrameOptionsDeny", aHttpChannel, uri, aPolicy);
-        return false;
-      }
-    }
-    ctx = ctx->GetParent();
-  }
-
-  
-  
-  
-  if (aPolicy.LowerCaseEqualsLiteral("deny")) {
-    ReportError("XFrameOptionsDeny", aHttpChannel, uri, aPolicy);
-    return false;
-  }
-
-  return true;
+  nsCOMPtr<nsIObserverService> observerService =
+      mozilla::services::GetObserverService();
+  nsAutoString policy(aPolicy);
+  observerService->NotifyObservers(aURI, "xfo-on-violate-policy", policy.get());
 }
 
 
@@ -149,32 +90,33 @@ static bool ShouldIgnoreFrameOptions(nsIChannel* aChannel,
 
 
 
+
+
 bool FramingChecker::CheckFrameOptions(nsIChannel* aChannel,
                                        nsIContentSecurityPolicy* aCsp,
                                        bool& outIsFrameCheckingSkipped) {
+  
   if (!aChannel) {
     return true;
   }
 
+  
+  
   nsCOMPtr<nsILoadInfo> loadInfo = aChannel->LoadInfo();
   ExtContentPolicyType contentType = loadInfo->GetExternalContentPolicyType();
-
-  
-  
   if (contentType != ExtContentPolicy::TYPE_SUBDOCUMENT &&
       contentType != ExtContentPolicy::TYPE_OBJECT) {
     return true;
   }
 
+  
+  
   nsCOMPtr<nsIHttpChannel> httpChannel;
   nsresult rv = nsContentSecurityUtils::GetHttpChannelFromPotentialMultiPart(
       aChannel, getter_AddRefs(httpChannel));
   if (NS_WARN_IF(NS_FAILED(rv))) {
     return true;
   }
-
-  
-  
   if (!httpChannel) {
     return true;
   }
@@ -182,20 +124,20 @@ bool FramingChecker::CheckFrameOptions(nsIChannel* aChannel,
   
   uint32_t responseStatus;
   rv = httpChannel->GetResponseStatus(&responseStatus);
-  
-  
   if (NS_FAILED(rv)) {
+    
+    
     return true;
   }
   if (mozilla::net::nsHttpChannel::IsRedirectStatus(responseStatus)) {
     return true;
   }
 
-  nsAutoCString xfoHeaderCValue;
+  nsAutoCString xfoHeaderValue;
   Unused << httpChannel->GetResponseHeader("X-Frame-Options"_ns,
-                                           xfoHeaderCValue);
-  NS_ConvertUTF8toUTF16 xfoHeaderValue(xfoHeaderCValue);
+                                           xfoHeaderValue);
 
+  
   
   if (xfoHeaderValue.IsEmpty()) {
     return true;
@@ -210,22 +152,72 @@ bool FramingChecker::CheckFrameOptions(nsIChannel* aChannel,
 
   
   
-  nsCharSeparatedTokenizer tokenizer(xfoHeaderValue, ',');
-  while (tokenizer.hasMoreTokens()) {
-    const nsAString& tok = tokenizer.nextToken();
-    if (!CheckOneFrameOptionsPolicy(httpChannel, tok)) {
-      
-      
-      
-      nsCOMPtr<nsIURI> uri;
-      httpChannel->GetURI(getter_AddRefs(uri));
-      nsCOMPtr<nsIObserverService> observerService =
-          mozilla::services::GetObserverService();
-      nsAutoString policy(tok);
-      observerService->NotifyObservers(uri, "xfo-on-violate-policy",
-                                       policy.get());
-      return false;
+  
+  
+  XFOHeader xfoOptions;
+  for (const nsACString& next : xfoHeaderValue.Split(',')) {
+    nsAutoCString option(next);
+    option.StripWhitespace();
+
+    if (option.LowerCaseEqualsLiteral("allowall")) {
+      xfoOptions.ALLOWALL = true;
+    } else if (option.LowerCaseEqualsLiteral("sameorigin")) {
+      xfoOptions.SAMEORIGIN = true;
+    } else if (option.LowerCaseEqualsLiteral("deny")) {
+      xfoOptions.DENY = true;
+    } else {
+      xfoOptions.INVALID = true;
     }
   }
+
+  nsCOMPtr<nsIURI> uri;
+  httpChannel->GetURI(getter_AddRefs(uri));
+
+  
+  
+  uint32_t xfoUniqueOptions = xfoOptions.DENY + xfoOptions.ALLOWALL +
+                              xfoOptions.SAMEORIGIN + xfoOptions.INVALID;
+  if (xfoUniqueOptions > 1 &&
+      (xfoOptions.DENY || xfoOptions.ALLOWALL || xfoOptions.SAMEORIGIN)) {
+    ReportError("XFrameOptionsInvalid", httpChannel, uri, u"invalid"_ns);
+    return false;
+  }
+
+  
+  
+  
+  if (xfoOptions.INVALID) {
+    ReportError("XFrameOptionsInvalid", httpChannel, uri, u"invalid"_ns);
+    return true;
+  }
+
+  
+  if (xfoOptions.DENY) {
+    ReportError("XFrameOptionsDeny", httpChannel, uri, u"deny"_ns);
+    return false;
+  }
+
+  
+  
+  RefPtr<mozilla::dom::BrowsingContext> ctx;
+  loadInfo->GetBrowsingContext(getter_AddRefs(ctx));
+
+  while (ctx && xfoOptions.SAMEORIGIN) {
+    nsCOMPtr<nsIPrincipal> principal =
+        ctx->Canonical()->GetCurrentWindowGlobal()->DocumentPrincipal();
+
+    if (principal && principal->IsSystemPrincipal()) {
+      return true;
+    }
+
+    
+    if (!principal || !principal->IsSameOrigin(uri)) {
+      ReportError("XFrameOptionsDeny", httpChannel, uri, u"sameorigin"_ns);
+      return false;
+    }
+    ctx = ctx->GetParent();
+  }
+
+  
   return true;
 }
--- netwerk/protocol/http/nsHttpHeaderArray.cpp
+++ netwerk/protocol/http/nsHttpHeaderArray.cpp
@@ -46,13 +46,10 @@ nsresult nsHttpHeaderArray::SetHeader(
       "Net original headers can only be set using SetHeader_internal().");
 
   nsEntry* entry = nullptr;
-  int32_t index;
-
-  index = LookupEntry(header, &entry);
+  int32_t index = LookupEntry(header, &entry);
 
   
-  
-  if (value.IsEmpty()) {
+  if (value.IsEmpty() && header != nsHttp::X_Frame_Options) {
     if (!merge && entry) {
       if (entry->variety == eVarietyResponseNetOriginalAndResponse) {
         MOZ_ASSERT(variety == eVarietyResponse);
