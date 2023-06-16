f4c2dc8c3bcb921b7742181580088959eb18d366
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-06-16 01:57:11 +0000

    Bug 1829128 - Improve some code style r=gsvelto
    Differential Revision: https://phabricator.services.mozilla.com/D180195
    
    UltraBlame original commit: c93bc35c52890c6f85c3264d0d3e8758d9418962

--- toolkit/crashreporter/breakpad-client/mac/handler/exception_handler.cc
+++ toolkit/crashreporter/breakpad-client/mac/handler/exception_handler.cc
@@ -453,17 +453,18 @@ bool ExceptionHandler::WriteMinidumpWithException(
                         exception_type,
                         exception_code,
                         exception_subcode,
-                        thread_name) ) {
-      if (exit_after_write)
-        _exit(exception_type);
+                        thread_name) &&
+        exit_after_write) {
+      _exit(exception_type);
     }
 #if !TARGET_OS_IPHONE
   } else if (IsOutOfProcess()) {
     if (exception_type && exception_code) {
       
       
-      if (filter_ && !filter_(callback_context_))
+      if (filter_ && !filter_(callback_context_)) {
         return false;
+      }
       result = crash_generation_client_->RequestDumpForException(
           exception_type,
           exception_code,
@@ -494,8 +495,9 @@ bool ExceptionHandler::WriteMinidumpWithException(
       if (exception_type && exception_code) {
         
         
-        if (filter_ && !filter_(callback_context_))
+        if (filter_ && !filter_(callback_context_)) {
           return false;
+        }
 
         md.SetExceptionInformation(exception_type, exception_code,
                                    exception_subcode, thread_name);
@@ -511,9 +513,8 @@ bool ExceptionHandler::WriteMinidumpWithException(
       
       
       
-      if (result) {
-        if (exit_after_write)
-          _exit(exception_type);
+      if (result && exit_after_write) {
+        _exit(exception_type);
       }
     }
   }
