d72ec0ccfcd571a99e348cb8f92389eeb8b147e9
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-06-16 01:50:15 +0000

    Bug 1747473 - Remove FileUtils.getFile from widget/ r=Gijs
    Differential Revision: https://phabricator.services.mozilla.com/D155893
    
    UltraBlame original commit: f45199353648b32c9489251ee4a7fd5e47ed8574

--- widget/tests/browser/browser_test_clipboardcache.js
+++ widget/tests/browser/browser_test_clipboardcache.js
@@ -29,7 +29,7 @@ var SHORT_STRING_NO_CACHE = "short string that will not be cached to the disk";
 
 
 function getClipboardCacheFDCount() {
-  let dir;
+  let dir = Cc["@mozilla.org/file/local;1"].createInstance(Ci.nsIFile);
   if (AppConstants.platform === "win") {
     
     
@@ -42,13 +42,9 @@ function getClipboardCacheFDCount() {
     
     
 
-    let { FileUtils } = ChromeUtils.importESModule(
-      "resource://gre/modules/FileUtils.sys.mjs"
-    );
     
-    dir = FileUtils.getFile("TmpD", ["mozilla-temp-files"]);
+    dir.initWithPath(PathUtils.join(PathUtils.tempDir, "mozilla-temp-files"));
   } else {
-    dir = Cc["@mozilla.org/file/local;1"].createInstance(Ci.nsIFile);
     dir.initWithPath("/dev/fd");
   }
   let count = 0;
