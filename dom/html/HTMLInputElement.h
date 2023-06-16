01963970ef9ed188b2263dfff0b7d29137ccd81e
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-06-16 00:36:09 +0000

    Bug 675943 - Part 1: Implement dirname attr for input elements within forms. r=edgar
    Differential Revision: https://phabricator.services.mozilla.com/D175626
    
    UltraBlame original commit: 2e52989251aecf16ea2adf62b3fabb94af7eaee7

--- dom/html/HTMLInputElement.h
+++ dom/html/HTMLInputElement.h
@@ -730,6 +730,13 @@ class HTMLInputElement final : public TextControlElement,
     SetHTMLAttr(nsGkAtoms::usemap, aValue, aRv);
   }
 
+  void GetDirName(nsAString& aValue) {
+    GetHTMLAttr(nsGkAtoms::dirname, aValue);
+  }
+  void SetDirName(const nsAString& aValue, ErrorResult& aRv) {
+    SetHTMLAttr(nsGkAtoms::dirname, aValue, aRv);
+  }
+
   nsIControllers* GetControllers(ErrorResult& aRv);
   
   nsresult GetControllers(nsIControllers** aResult);
