8c3a9ed9cee97d62adde47f1b82d65b974691b0b
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-06-18 10:29:46 +0000

    Bug 1836974 - Use fastText pref in full-page translation r=gregtatum
    Uses the fastText pref to determine which method of language
    identification to use in full-page translation
    
    Differential Revision: https://phabricator.services.mozilla.com/D180746
    
    UltraBlame original commit: 2da6acc9b08dab0e47d3ed45f3fd88c75440715a

--- browser/base/content/test/performance/browser_startup_content.js
+++ browser/base/content/test/performance/browser_startup_content.js
@@ -75,6 +75,7 @@ const intermittently_loaded_scripts = {
 
     
     "resource://gre/actors/TranslationsChild.sys.mjs",
+    "resource://gre/modules/translation/LanguageDetector.sys.mjs",
     "chrome://global/content/translations/language-id-engine.sys.mjs",
     "resource://gre/modules/ConsoleAPIStorage.sys.mjs", 
 
