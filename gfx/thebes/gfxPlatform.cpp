567be87255b1a1adae45457314607fba281bb9d9
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-06-18 10:30:49 +0000

    Bug 1838760 - Remove pref gfx.webrender.fallback.software-d3d11 r=gfx-reviewers,lsalzman
    pef gfx.webrender.fallback.software-d3d11 is no longer necessary.
    
    Differential Revision: https://phabricator.services.mozilla.com/D181183
    
    UltraBlame original commit: 47d72e533020fb0ed4a8d38a5a80c41184fae3bd

--- gfx/thebes/gfxPlatform.cpp
+++ gfx/thebes/gfxPlatform.cpp
@@ -3687,8 +3687,7 @@ bool gfxPlatform::FallbackFromAcceleration(FeatureStatus aStatus,
 #ifdef XP_WIN
   
   
-  if (StaticPrefs::gfx_webrender_fallback_software_d3d11_AtStartup() &&
-      swglFallbackAllowed && gfxVars::AllowSoftwareWebRenderD3D11() &&
+  if (swglFallbackAllowed && gfxVars::AllowSoftwareWebRenderD3D11() &&
       gfxConfig::IsEnabled(Feature::D3D11_COMPOSITING) &&
       !gfxVars::UseSoftwareWebRender()) {
     
@@ -3697,8 +3696,7 @@ bool gfxPlatform::FallbackFromAcceleration(FeatureStatus aStatus,
     return true;
   }
 
-  if (StaticPrefs::gfx_webrender_fallback_software_d3d11_AtStartup() &&
-      swglFallbackAllowed && gfxVars::AllowSoftwareWebRenderD3D11() &&
+  if (swglFallbackAllowed && gfxVars::AllowSoftwareWebRenderD3D11() &&
       gfxVars::UseSoftwareWebRender()) {
     
     gfxCriticalNote << "Fallback SW-WR + D3D11 to SW-WR";
