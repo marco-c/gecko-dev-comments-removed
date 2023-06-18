786fe7c86f6614960c02fb49fbd96062e7fdeec6
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-06-18 10:33:00 +0000

    Bug 1836870 - Restore scrollwheel overshoot detection code and telemetry. r=dlrobertson
    Differential Revision: https://phabricator.services.mozilla.com/D180634
    
    UltraBlame original commit: bf5f13960f2fc5db277aa3ae5d5c138b05922ffb

--- gfx/layers/apz/src/APZCTreeManager.h
+++ gfx/layers/apz/src/APZCTreeManager.h
@@ -34,6 +34,7 @@
 #include "mozilla/UniquePtr.h"       
 #include "nsCOMPtr.h"                
 #include "nsTArray.h"
+#include "OvershootDetector.h"
 
 namespace mozilla {
 class MultiTouchInput;
@@ -1038,6 +1039,10 @@ class APZCTreeManager : public IAPZCTreeManager, public APZInputBridge {
   std::unordered_map<LayersId, UniquePtr<APZTestData>, LayersId::HashFn>
       mTestData;
   mutable mozilla::Mutex mTestDataLock;
+
+  
+  
+  OvershootDetector mOvershootDetector;
 
   
   float mDPI;
