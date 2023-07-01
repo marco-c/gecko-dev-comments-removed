b0914020f9af17edb3e3ac7175a8fcb495bd36a8
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-07-01 01:11:41 +0000

    Backed out changeset 8be4693aecbb (bug 1754905) for causing bustages in xpcAccessiblePivot.h. CLOSED TREE
    UltraBlame original commit: d9d61c7bc75210cb5926dc04660a66878a4af735

--- accessible/generic/DocAccessible.h
+++ accessible/generic/DocAccessible.h
@@ -6,6 +6,8 @@
 #ifndef mozilla_a11y_DocAccessible_h__
 #define mozilla_a11y_DocAccessible_h__
 
+#include "nsIAccessiblePivot.h"
+
 #include "HyperTextAccessibleWrap.h"
 #include "AccEvent.h"
 
@@ -16,6 +18,8 @@
 #include "nsITimer.h"
 #include "nsTHashSet.h"
 #include "nsWeakReference.h"
+
+class nsAccessiblePivot;
 
 const uint32_t kDefaultCacheLength = 128;
 
@@ -44,9 +48,12 @@ class TNotification;
 
 class DocAccessible : public HyperTextAccessibleWrap,
                       public nsIDocumentObserver,
-                      public nsSupportsWeakReference {
+                      public nsSupportsWeakReference,
+                      public nsIAccessiblePivotObserver {
   NS_DECL_ISUPPORTS_INHERITED
   NS_DECL_CYCLE_COLLECTION_CLASS_INHERITED(DocAccessible, LocalAccessible)
+
+  NS_DECL_NSIACCESSIBLEPIVOTOBSERVER
 
  protected:
   typedef mozilla::dom::Document Document;
@@ -127,6 +134,11 @@ class DocAccessible : public HyperTextAccessibleWrap,
 
 
   void QueueCacheUpdateForDependentRelations(LocalAccessible* aAcc);
+
+  
+
+
+  nsIAccessiblePivot* VirtualCursor();
 
   
 
@@ -693,6 +705,11 @@ class DocAccessible : public HyperTextAccessibleWrap,
   uint64_t mPrevStateBits;
 
   nsTArray<RefPtr<DocAccessible>> mChildDocuments;
+
+  
+
+
+  RefPtr<nsAccessiblePivot> mVirtualCursor;
 
   
 
