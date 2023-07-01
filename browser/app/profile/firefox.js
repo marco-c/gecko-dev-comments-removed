00b313b447ce28310e12908347384a23ebb3eb96
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-07-01 01:14:33 +0000

    Bug 1834622 - Enable fingerprinting protection in private windows in Nightly. r=tjr
    Depends on D180869
    
    Differential Revision: https://phabricator.services.mozilla.com/D180870
    
    UltraBlame original commit: b5cf5c207c4e212968fa8422bd0e0c72927adc4f

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -2060,6 +2060,11 @@ pref("privacy.webrtc.sharedTabWarning", false);
 pref("privacy.webrtc.deviceGracePeriodTimeoutMs", 3600000);
 
 
+#ifdef NIGHTLY_BUILD
+pref("privacy.fingerprintingProtection.pbmode", true);
+#endif
+
+
 pref("browser.tabs.remote.autostart", true);
 pref("browser.tabs.remote.desktopbehavior", true);
 
