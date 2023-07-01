13b1336636261dde3b6af9e9a30aab93a0ec5697
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-07-01 01:34:58 +0000

    Bug 1556358 - Part 2: Save and restore custom element form data. r=edgar
    Differential Revision: https://phabricator.services.mozilla.com/D174115
    
    UltraBlame original commit: 3561a683c068d23852f75386165e9a727a563da6

--- dom/html/nsGenericHTMLElement.h
+++ dom/html/nsGenericHTMLElement.h
@@ -936,10 +936,14 @@ class HTMLFieldSetElement;
 enum {
   
   HTML_ELEMENT_ACTIVE_FOR_KEYBOARD = HTML_ELEMENT_FLAG_BIT(0),
+  
+  
+  
+  HTML_ELEMENT_INHIBIT_RESTORATION = HTML_ELEMENT_FLAG_BIT(1),
 
   
   HTML_ELEMENT_TYPE_SPECIFIC_BITS_OFFSET =
-      ELEMENT_TYPE_SPECIFIC_BITS_OFFSET + 1,
+      ELEMENT_TYPE_SPECIFIC_BITS_OFFSET + 2,
 };
 
 ASSERT_NODE_FLAGS_SPACE(HTML_ELEMENT_TYPE_SPECIFIC_BITS_OFFSET);
@@ -983,6 +987,7 @@ class nsGenericHTMLFormElement : public nsGenericHTMLElement {
       already_AddRefed<mozilla::dom::NodeInfo>&& aNodeInfo);
 
   
+  void SaveSubtreeState() override;
   nsresult BindToTree(BindContext&, nsINode& aParent) override;
   void UnbindFromTree(bool aNullParent = true) override;
 
@@ -1009,6 +1014,15 @@ class nsGenericHTMLFormElement : public nsGenericHTMLElement {
   void ForgetFieldSet(nsIContent* aFieldset);
 
   void ClearForm(bool aRemoveFromForm, bool aUnbindOrDelete);
+
+  
+
+
+
+
+
+
+  already_AddRefed<nsILayoutHistoryState> GetLayoutHistory(bool aRead);
 
  protected:
   virtual ~nsGenericHTMLFormElement() = default;
@@ -1103,6 +1117,13 @@ class nsGenericHTMLFormElement : public nsGenericHTMLElement {
 
 
   virtual bool IsFormAssociatedElement() const { return false; }
+
+  
+
+
+
+
+  virtual void SaveState() {}
 };
 
 class nsGenericHTMLFormControlElement : public nsGenericHTMLFormElement,
@@ -1121,9 +1142,7 @@ class nsGenericHTMLFormControlElement : public nsGenericHTMLFormElement,
   bool IsHTMLFormControlElement() const final { return true; }
 
   
-  void SaveSubtreeState() override;
   IMEState GetDesiredIMEState() override;
-  void UnbindFromTree(bool aNullParent = true) override;
 
   
   
@@ -1165,13 +1184,6 @@ class nsGenericHTMLFormControlElement : public nsGenericHTMLFormElement,
   void UpdateRequiredState(bool aIsRequired, bool aNotify);
 
   bool IsAutocapitalizeInheriting() const;
-
-  
-
-
-
-
-  virtual void SaveState() {}
 
   nsresult SubmitDirnameDir(mozilla::dom::FormData* aFormData);
 
@@ -1225,15 +1237,6 @@ class nsGenericHTMLFormControlElementWithState
 
 
   mozilla::PresState* GetPrimaryPresState();
-
-  
-
-
-
-
-
-
-  already_AddRefed<nsILayoutHistoryState> GetLayoutHistory(bool aRead);
 
   
 
