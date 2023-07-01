e12350934a217fff68f3beb0c59306be071b2f5c
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-07-01 01:03:30 +0000

    Bug 1838078 - Ensure page-rules respect layer order for styling r=emilio
    Differential Revision: https://phabricator.services.mozilla.com/D180733
    
    UltraBlame original commit: 0b7d5d5c02953c937a93835831495531ab11e324

--- servo/components/style/stylesheets/page_rule.rs
+++ servo/components/style/stylesheets/page_rule.rs
@@ -146,7 +146,7 @@ impl PageSelector {
     
     
     #[inline]
-    pub fn matches(&self, name: &CustomIdent, flags: &PagePseudoClassFlags) -> bool {
+    pub fn matches(&self, name: &CustomIdent, flags: PagePseudoClassFlags) -> bool {
         self.ident_matches(name) && self.flags_match(flags)
     }
 
@@ -158,7 +158,7 @@ impl PageSelector {
     
     
     
-    pub fn flags_match(&self, flags: &PagePseudoClassFlags) -> bool {
+    pub fn flags_match(&self, flags: PagePseudoClassFlags) -> bool {
         self.pseudos.iter().all(|pc| flags.contains_class(pc))
     }
 
@@ -172,7 +172,7 @@ impl PageSelector {
     
     
     
-    pub fn match_specificity(&self, flags: &PagePseudoClassFlags) -> Option<u32> {
+    pub fn match_specificity(&self, flags: PagePseudoClassFlags) -> Option<u32> {
         let mut g: usize = 0;
         let mut h: usize = 0;
         for pc in self.pseudos.iter() {
@@ -298,7 +298,7 @@ impl PageRule {
     
     
     
-    pub fn match_specificity(&self, flags: &PagePseudoClassFlags) -> Option<u32> {
+    pub fn match_specificity(&self, flags: PagePseudoClassFlags) -> Option<u32> {
         let mut specificity = None;
         for s in self.selectors.0.iter().map(|s| s.match_specificity(flags)) {
             specificity = s.max(specificity);
