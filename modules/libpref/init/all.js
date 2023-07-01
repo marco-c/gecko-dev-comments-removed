fd6e615ab2a855afee1c018df6eb0e1f6cb2ef36
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-07-01 01:26:19 +0000

    Bug 1836974 - Add language detection fastText pref r=gregtatum
    Adds a pref that will be used to determine whether Translations
    language identification should use fastText or not.
    
    Depends on D180743
    
    Differential Revision: https://phabricator.services.mozilla.com/D180744
    
    UltraBlame original commit: c444aaed4af2dd50ef6e2306e9a993bde240c966

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -3678,6 +3678,13 @@ pref("browser.translations.chaos.timeoutMS", 0);
 
 
 
+pref("browser.translations.languageDetection.fastText", false);
+
+
+
+
+
+
 
 pref("prompts.authentication_dialog_abuse_limit", 2);
 
