936ffdcb5110468f620246a03aed30ce5d86e977
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-07-01 00:33:21 +0000

    Bug 1838738 - Remove a very noisy (when it is hit) warning. r=longsonr
    Differential Revision: https://phabricator.services.mozilla.com/D181558
    
    UltraBlame original commit: aa4642d5b71463428de7a6c74cd9cd4403e301b9

--- dom/svg/SVGUseElement.cpp
+++ dom/svg/SVGUseElement.cpp
@@ -266,8 +266,7 @@ static bool IsTooMuchRecursion(uint32_t aCount) {
       }
       break;
   }
-  return NS_WARN_IF(aCount >=
-                    StaticPrefs::svg_use_element_recursive_clone_limit());
+  return aCount >= StaticPrefs::svg_use_element_recursive_clone_limit();
 }
 
 
