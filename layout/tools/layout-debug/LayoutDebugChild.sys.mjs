25d68815e384e6655fcbd95c3e8ad586f97462ba
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-07-01 00:35:26 +0000

    Bug 1839518 - Convert LayoutDebugChild to an ES module. r=emilio
    Differential Revision: https://phabricator.services.mozilla.com/D181579
    
    UltraBlame original commit: 3dabb9290349e7a9ea00dc24a9f9a8d9645dcf1d

--- layout/tools/layout-debug/LayoutDebugChild.sys.mjs
+++ layout/tools/layout-debug/LayoutDebugChild.sys.mjs
@@ -1,15 +1,12 @@
-
-
-
-
-"use strict";
-
-var EXPORTED_SYMBOLS = ["LayoutDebugChild"];
+/* vim: set ts=2 sw=2 sts=2 et tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 const NS_LAYOUT_DEBUGGINGTOOLS_CONTRACTID =
   "@mozilla.org/layout-debug/layout-debuggingtools;1";
 
-class LayoutDebugChild extends JSWindowActorChild {
+export class LayoutDebugChild extends JSWindowActorChild {
   receiveMessage(msg) {
     if (!this._debuggingTools) {
       this._debuggingTools = Cc[
