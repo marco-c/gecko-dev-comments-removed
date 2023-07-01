76bbf2c51ad421400b05539c1a012187a2d5e11f
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-07-01 01:42:37 +0000

    Bug 1839550 - Back out increased clamping range. r=dholbert
    Was introduced in bug 1831148, regresses the behaviour fixed by
    bug 1419225.
    
    Differential Revision: https://phabricator.services.mozilla.com/D182346
    
    UltraBlame original commit: e55230080383bddf1b725a372b2e80e0a511b40a

--- layout/base/nsLayoutUtils.cpp
+++ layout/base/nsLayoutUtils.cpp
@@ -2361,13 +2361,9 @@ static Rect TransformGfxRectToAncestor(
   }
   const nsIFrame* ancestor = aOutAncestor ? *aOutAncestor : aAncestor.mFrame;
   float factor = ancestor->PresContext()->AppUnitsPerDevPixel();
-  
-  
-  
-  const auto boundsAppUnits = Rect::MaxIntRect();
   Rect maxBounds =
-      Rect(boundsAppUnits.x / factor, boundsAppUnits.y / factor,
-           boundsAppUnits.width / factor, boundsAppUnits.height / factor);
+      Rect(float(nscoord_MIN) / factor * 0.5, float(nscoord_MIN) / factor * 0.5,
+           float(nscoord_MAX) / factor, float(nscoord_MAX) / factor);
   return ctm.TransformAndClipBounds(aRect, maxBounds);
 }
 
