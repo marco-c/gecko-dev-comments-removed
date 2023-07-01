2f88d9d3249a49665b5eb949631e6087f1266e07
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-07-01 01:33:20 +0000

    Bug 1839198 - Skip processing swipe-to-navigation stuff in PiP windows. r=botond,pip-reviewers,mconley
    Differential Revision: https://phabricator.services.mozilla.com/D181418
    
    UltraBlame original commit: 613e83b36c1ad458fbd4a0a79ddecfee0cd5ad38

--- widget/nsBaseWidget.cpp
+++ widget/nsBaseWidget.cpp
@@ -2316,6 +2316,11 @@ WidgetWheelEvent nsBaseWidget::MayStartSwipeForAPZ(
     const PanGestureInput& aPanInput, const APZEventResult& aApzResult) {
   WidgetWheelEvent event = aPanInput.ToWidgetEvent(this);
 
+  
+  if (mIsPIPWindow) {
+    return event;
+  }
+
   if (aPanInput.AllowsSwipe()) {
     SwipeInfo swipeInfo = SendMayStartSwipe(aPanInput);
     event.mCanTriggerSwipe = swipeInfo.wantsSwipe;
@@ -2357,6 +2362,11 @@ WidgetWheelEvent nsBaseWidget::MayStartSwipeForAPZ(
 }
 
 bool nsBaseWidget::MayStartSwipeForNonAPZ(const PanGestureInput& aPanInput) {
+  
+  if (mIsPIPWindow) {
+    return false;
+  }
+
   if (aPanInput.mType == PanGestureInput::PANGESTURE_MAYSTART ||
       aPanInput.mType == PanGestureInput::PANGESTURE_START) {
     mCurrentPanGestureBelongsToSwipe = false;
