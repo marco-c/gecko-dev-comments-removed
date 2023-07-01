8596767415cc67114b7b81039442b6bb279e010a
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-07-01 01:19:50 +0000

    Bug 1839192 - [devtools] Avoid instantiating new frame objects when its location isn't mapped. r=devtools-reviewers,bomsy
    Differential Revision: https://phabricator.services.mozilla.com/D181454
    
    UltraBlame original commit: 884bef02aff69040f0efa4daf56ec49a247e655d

--- devtools/client/debugger/src/actions/pause/mapFrames.js
+++ devtools/client/debugger/src/actions/pause/mapFrames.js
@@ -38,6 +38,10 @@ async function updateFrameLocation(frame, thunkArgs) {
     return Promise.resolve(frame);
   }
   const location = await getOriginalLocation(frame.location, thunkArgs, true);
+  
+  if (location == frame.location) {
+    return frame;
+  }
   return {
     ...frame,
     location,
