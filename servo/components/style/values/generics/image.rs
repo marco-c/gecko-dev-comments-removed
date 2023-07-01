3bedc0eb9390ac232743128b5010bfafec532b0d
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-07-01 01:17:54 +0000

    Bug 1838740 - linear-gradient() and friends can parse a color interpolation method r=emilio
    Allow the following syntax for gradients:
    `linear-gradient(in lab to right, red, blue)`
    
    The patch will parse and make available the interpolation method, but no
    rendering changes are made, so all gradients will still render in srgb.
    
    Differential Revision: https://phabricator.services.mozilla.com/D181162
    
    UltraBlame original commit: e34ec04eb98147f60e72fa1d523b8d0308deb83a

--- servo/components/style/values/generics/image.rs
+++ servo/components/style/values/generics/image.rs
@@ -6,6 +6,7 @@
 
 
 
+use crate::color::{mix::ColorInterpolationMethod, ColorSpace};
 use crate::custom_properties;
 use crate::values::generics::position::PositionComponent;
 use crate::values::generics::Optional;
@@ -177,6 +178,8 @@ pub enum GenericGradient<
         
         direction: LineDirection,
         
+        color_interpolation_method: ColorInterpolationMethod,
+        
         items: crate::OwnedSlice<GenericGradientItem<Color, LengthPercentage>>,
         
         repeating: bool,
@@ -190,6 +193,8 @@ pub enum GenericGradient<
         
         position: Position,
         
+        color_interpolation_method: ColorInterpolationMethod,
+        
         items: crate::OwnedSlice<GenericGradientItem<Color, LengthPercentage>>,
         
         repeating: bool,
@@ -202,6 +207,8 @@ pub enum GenericGradient<
         angle: Angle,
         
         position: Position,
+        
+        color_interpolation_method: ColorInterpolationMethod,
         
         items: crate::OwnedSlice<GenericGradientItem<Color, AngleOrPercentage>>,
         
@@ -483,17 +490,24 @@ where
         match *self {
             Gradient::Linear {
                 ref direction,
+                ref color_interpolation_method,
                 ref items,
                 compat_mode,
                 ..
             } => {
                 dest.write_str("linear-gradient(")?;
-                let mut skip_comma = if !direction.points_downwards(compat_mode) {
+                let mut skip_comma = true;
+                if !direction.points_downwards(compat_mode) {
                     direction.to_css(dest, compat_mode)?;
-                    false
-                } else {
-                    true
-                };
+                    skip_comma = false;
+                }
+                if !matches!(color_interpolation_method.space, ColorSpace::Srgb) {
+                    if !skip_comma {
+                        dest.write_char(' ')?;
+                    }
+                    color_interpolation_method.to_css(dest)?;
+                    skip_comma = false;
+                }
                 for item in &**items {
                     if !skip_comma {
                         dest.write_str(", ")?;
@@ -505,6 +519,7 @@ where
             Gradient::Radial {
                 ref shape,
                 ref position,
+                ref color_interpolation_method,
                 ref items,
                 compat_mode,
                 ..
@@ -538,7 +553,16 @@ where
                         shape.to_css(dest)?;
                     }
                 }
-                let mut skip_comma = omit_shape && omit_position;
+                let omit_color_interpolation_method =
+                    matches!(color_interpolation_method.space, ColorSpace::Srgb);
+                if !omit_color_interpolation_method {
+                    if !omit_shape || !omit_position {
+                        dest.write_char(' ')?;
+                    }
+                    color_interpolation_method.to_css(dest)?;
+                }
+
+                let mut skip_comma = omit_shape && omit_position && omit_color_interpolation_method;
                 for item in &**items {
                     if !skip_comma {
                         dest.write_str(", ")?;
