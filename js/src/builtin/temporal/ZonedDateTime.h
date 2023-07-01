f3603e7bc314850e46cd0be362797881accb2c7c
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-07-01 00:48:43 +0000

    Bug 1519167 - Part 83: Implement Temporal.ZonedDateTime.prototype.{add,subtract}. r=spidermonkey-reviewers,dminor
    Differential Revision: https://phabricator.services.mozilla.com/D174597
    
    UltraBlame original commit: d998f548b2d2b9c514ccea116fe8ebce703229c5

--- js/src/builtin/temporal/ZonedDateTime.h
+++ js/src/builtin/temporal/ZonedDateTime.h
@@ -67,6 +67,16 @@ ZonedDateTimeObject* CreateTemporalZonedDateTime(
     JSContext* cx, const Instant& instant, JS::Handle<JSObject*> timeZone,
     JS::Handle<JSObject*> calendar);
 
+
+
+
+
+
+bool AddZonedDateTime(JSContext* cx, const Instant& epochInstant,
+                      JS::Handle<JSObject*> timeZone,
+                      JS::Handle<JSObject*> calendar, const Duration& duration,
+                      Instant* result);
+
 struct NanosecondsAndDays final {
   JS::BigInt* days = nullptr;
   int64_t daysInt = 0;
