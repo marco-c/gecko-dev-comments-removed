3d94d13676a0dadf34c872fc23e29d54874422af
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-07-16 00:47:20 +0000

    Bug 1839451 - Vendor libwebrtc from 452d94047b
    Upstream commit: https://webrtc.googlesource.com/src/+/452d94047b5cdce41fdbdad2bec3f24f603dfcf4
        Add mock for NetworkControllerInterface
    
        Bug: none
        Change-Id: Ibdd72011932a36348a4382caa5d0bf0ab2c02dd9
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/298742
        Reviewed-by: Danil Chapovalov <danilchapwebrtc.org>
        Auto-Submit: Per Kjellander <perkjwebrtc.org>
        Commit-Queue: Per Kjellander <perkjwebrtc.org>
        Commit-Queue: Danil Chapovalov <danilchapwebrtc.org>
        Cr-Commit-Position: refs/heads/main{#39641}
    
    UltraBlame original commit: 900188f8c3b0181d3a97b6e54e9d19ff911ac232

---
+++ third_party/libwebrtc/api/transport/test/mock_network_control.h
@@ -0,0 +1,84 @@
+
+
+
+
+
+
+
+
+
+
+#ifndef API_TRANSPORT_TEST_MOCK_NETWORK_CONTROL_H_
+#define API_TRANSPORT_TEST_MOCK_NETWORK_CONTROL_H_
+
+#include "api/transport/network_control.h"
+#include "test/gmock.h"
+
+namespace webrtc {
+
+class MockNetworkControllerInterface : public NetworkControllerInterface {
+ public:
+  MOCK_METHOD(NetworkControlUpdate,
+              OnNetworkAvailability,
+              (NetworkAvailability),
+              (override));
+  MOCK_METHOD(NetworkControlUpdate,
+              OnProcessInterval,
+              (ProcessInterval),
+              (override));
+  MOCK_METHOD(NetworkControlUpdate,
+              OnNetworkRouteChange,
+              (NetworkRouteChange),
+              (override));
+  MOCK_METHOD(NetworkControlUpdate,
+              OnRemoteBitrateReport,
+              (RemoteBitrateReport),
+              (override));
+  MOCK_METHOD(NetworkControlUpdate,
+              OnRoundTripTimeUpdate,
+              (RoundTripTimeUpdate),
+              (override));
+  MOCK_METHOD(NetworkControlUpdate, OnSentPacket, (SentPacket), (override));
+  MOCK_METHOD(NetworkControlUpdate,
+              OnReceivedPacket,
+              (ReceivedPacket),
+              (override));
+  MOCK_METHOD(NetworkControlUpdate,
+              OnStreamsConfig,
+              (StreamsConfig),
+              (override));
+  MOCK_METHOD(NetworkControlUpdate,
+              OnTargetRateConstraints,
+              (TargetRateConstraints),
+              (override));
+  MOCK_METHOD(NetworkControlUpdate,
+              OnTransportLossReport,
+              (TransportLossReport),
+              (override));
+  MOCK_METHOD(NetworkControlUpdate,
+              OnTransportPacketsFeedback,
+              (TransportPacketsFeedback),
+              (override));
+  MOCK_METHOD(NetworkControlUpdate,
+              OnNetworkStateEstimate,
+              (NetworkStateEstimate),
+              (override));
+};
+
+class MockNetworkStateEstimator : public NetworkStateEstimator {
+ public:
+  MOCK_METHOD(absl::optional<NetworkStateEstimate>,
+              GetCurrentEstimate,
+              (),
+              (override));
+  MOCK_METHOD(void,
+              OnTransportPacketsFeedback,
+              (const TransportPacketsFeedback&),
+              (override));
+  MOCK_METHOD(void, OnReceivedPacket, (const PacketResult&), (override));
+  MOCK_METHOD(void, OnRouteChange, (const NetworkRouteChange&), (override));
+};
+
+}  
+
+#endif  
