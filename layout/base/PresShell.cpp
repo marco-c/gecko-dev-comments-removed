8b385be3e07cb6324df531667eab3c1b947d0611
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-07-16 01:10:24 +0000

    Bug 1759303 - Allow disabling Shift+Right Click force-opening the context menu r=emilio
    Differential Revision: https://phabricator.services.mozilla.com/D183075
    
    UltraBlame original commit: 2ac0f03815253b96c145a52b1762ed04c50f079d

--- layout/base/PresShell.cpp
+++ layout/base/PresShell.cpp
@@ -8320,7 +8320,8 @@ bool PresShell::EventHandler::PrepareToDispatchEvent(
       
       
       
-      if (mouseEvent->IsShift()) {
+      if (mouseEvent->IsShift() &&
+          StaticPrefs::dom_event_contextmenu_shift_suppresses_event()) {
         aEvent->mFlags.mOnlyChromeDispatch = true;
         aEvent->mFlags.mRetargetToNonNativeAnonymous = true;
       }
