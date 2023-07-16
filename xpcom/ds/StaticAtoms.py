8355ed0e94028d235eb51caedda4a13a3d01bb17
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-07-16 01:51:30 +0000

    Bug 1821980: Align mathvariant implementation on MathML Core. r=emilio,jfkthame
    In MathML Core, the recommendation is to directly use the proper
    character from the Mathematical Alphanumeric Symbols instead of the
    mathvariant attribute. The exception is for automatic italicization on
    single-char `<mi>` element. This is implemented via a new text-transform
    value "math-auto" [1] which is the default on the `<mi>` element. The
    mathvariant attribute is now restricted to that element and to value
    "normal" in order to force upright text instead [2]. This CL implements
    this restriction together with that new text-transform value under the
    mathml.legacy_mathvariant_attribute.disabled flag.
    
    Some legacy MathML cases where math-auto alone does not work are
    still handled via MathMLTextRunFactory.
    
    [1] https://w3c.github.io/mathml-core/#new-text-transform-values
    [2] https://w3c.github.io/mathml-core/#the-mathvariant-attribute
    
    Differential Revision: https://phabricator.services.mozilla.com/D172395
    
    UltraBlame original commit: 6cceaca5e65378578d497c70b63cf0aabbdbe91d

--- xpcom/ds/StaticAtoms.py
+++ xpcom/ds/StaticAtoms.py
@@ -2407,6 +2407,8 @@ STATIC_ATOMS = [
     Atom("_moz_mathml_core_ms", "-moz-mathml-core-ms"),
     Atom("_moz_popover_enabled", "-moz-popover-enabled"),
     
+    Atom("_moz_mathml_core_mi", "-moz-mathml-core-mi"),
+    
     Atom("usercontextid", "usercontextid"),
     Atom("geckoViewSessionContextId", "geckoViewSessionContextId"),
     
