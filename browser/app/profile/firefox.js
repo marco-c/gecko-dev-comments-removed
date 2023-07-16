bd792308e0963860fc65e6b5c47e96122e4c653a
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-07-16 00:27:31 +0000

    Bug 1836740 - set up opt-in pref for 2023 shopping experiment. r=shopping-reviewers,jhirsch
    Differential Revision: https://phabricator.services.mozilla.com/D182495
    
    UltraBlame original commit: 8dcf1893187e27fa31aee2de08df6ecf1b4451a1

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -700,6 +700,10 @@ pref("browser.search.serpEventTelemetry.enabled", false);
 pref("browser.shopping.experience2023.enabled", false);
 
 
+
+pref("browser.shopping.experience2023.optedIn", false);
+
+
 pref("browser.privatebrowsing.vpnpromourl", "https://vpn.mozilla.org/?utm_source=firefox-browser&utm_medium=firefox-%CHANNEL%-browser&utm_campaign=private-browsing-vpn-link");
 
 
