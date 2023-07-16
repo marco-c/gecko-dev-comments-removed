5c154c67acb7c6a68eabb913c9bb30898802c719
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-07-16 01:27:50 +0000

    Bug 1839224 [wpt PR 40627] - SDA: Fix handling of active range boundaries., a=testonly
    Automatic update from web-platform-tests
    SDA: Fix handling of active range boundaries.
    
    Bug: 1445082
    Change-Id: I15be987efd28ade1be096e5677a06e760deb10a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4622511
    Reviewed-by: Robert Flack <flackrchromium.org>
    Commit-Queue: Kevin Ellis <keverschromium.org>
    Cr-Commit-Position: refs/heads/main{#1162380}
    
    --
    
    wpt-commits: 45e7d923b046dc36650776acd9431d8dde09dabd
    wpt-pr: 40627
    
    UltraBlame original commit: e9751970953edaa7a97af55f78c39431ce994a64

--- testing/web-platform/tests/web-animations/testcommon.js
+++ testing/web-platform/tests/web-animations/testcommon.js
@@ -288,7 +288,12 @@ function assert_rotate3d_equals(actual, expected, description) {
 
 function assert_phase_at_time(animation, phase, currentTime) {
   animation.currentTime = currentTime;
+  assert_phase(animation, phase);
+}
+
+function assert_phase(animation, phase) {
   const fillMode = animation.effect.getTiming().fill;
+  const currentTime = animation.currentTime;
 
   if (phase === 'active') {
     
