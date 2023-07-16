babb1d5a8186172f03b545e087edcdb6c04a564d
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-07-16 01:21:09 +0000

    Bug 1837192: Cache Ion hints in the process r=iain
    Cache a hint whenever a script is Ion compiled.  The next time we encounter the same script within the same process, use an eager threshold value to accelerate into the Ion tier.  If the script bailout invalidates, then increment the threshold for that script.  Repeat until we reach a maximum threshold value, or the script stops invalidating.
    
    Differential Revision: https://phabricator.services.mozilla.com/D181683
    
    UltraBlame original commit: ab845ce2e82282800712be228b9b7409af94ecb3

--- js/src/jit/BaselineBailouts.cpp
+++ js/src/jit/BaselineBailouts.cpp
@@ -1655,6 +1655,12 @@ static void InvalidateAfterBailout(JSContext* cx, HandleScript outerScript,
     return;
   }
 
+  
+  if (cx->runtime()->jitRuntime()->hasJitHintsMap()) {
+    JitHintsMap* jitHints = cx->runtime()->jitRuntime()->getJitHintsMap();
+    jitHints->recordInvalidation(outerScript);
+  }
+
   MOZ_ASSERT(!outerScript->ionScript()->invalidated());
 
   JitSpew(JitSpew_BaselineBailouts, "Invalidating due to %s", reason);
--- js/src/jit/JitHints.h
+++ js/src/jit/JitHints.h
@@ -8,6 +8,8 @@
 #define jit_JitHints_h
 
 #include "mozilla/BloomFilter.h"
+#include "mozilla/HashTable.h"
+#include "mozilla/LinkedList.h"
 #include "vm/JSScript.h"
 
 namespace js::jit {
@@ -28,9 +30,80 @@ namespace js::jit {
 class JitHintsMap {
   
   using ScriptKey = HashNumber;
+  ScriptKey getScriptKey(JSScript* script) const;
 
-  static constexpr uint32_t CacheSize_ = 16;
-  mozilla::BitBloomFilter<CacheSize_, ScriptKey> map_;
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+  class IonHint : public mozilla::LinkedListElement<IonHint> {
+    ScriptKey key_ = 0;
+    uint32_t threshold_ = 0;
+
+   public:
+    explicit IonHint(ScriptKey key) {
+      key_ = key;
+      threshold_ = IonHintEagerThresholdValue();
+    }
+
+    uint32_t threshold() { return threshold_; }
+
+    void incThreshold(uint32_t inc) {
+      uint32_t newThreshold = threshold() + inc;
+      threshold_ = (newThreshold > JitOptions.normalIonWarmUpThreshold)
+                       ? JitOptions.normalIonWarmUpThreshold
+                       : newThreshold;
+    }
+
+    ScriptKey key() {
+      MOZ_ASSERT(key_ != 0, "Should have valid key.");
+      return key_;
+    }
+  };
+
+  using ScriptToHintMap =
+      HashMap<ScriptKey, IonHint*, js::DefaultHasher<ScriptKey>,
+              js::SystemAllocPolicy>;
+  using IonHintPriorityQueue = mozilla::LinkedList<IonHint>;
+
+  static constexpr uint32_t InvalidationThresholdIncrement = 500;
+  static constexpr uint32_t IonHintCacheSize = 5000;
+  static constexpr uint32_t InitialIonHintThresholdModifier = 50;
+
+  static uint32_t IonHintEagerThresholdValue() {
+    return JitOptions.trialInliningWarmUpThreshold +
+           InitialIonHintThresholdModifier;
+  }
+
+  ScriptToHintMap ionHintMap_;
+  IonHintPriorityQueue ionHintQueue_;
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+  static constexpr uint32_t EagerBaselineCacheSize_ = 16;
+  mozilla::BitBloomFilter<EagerBaselineCacheSize_, ScriptKey> baselineHintMap_;
 
   
 
@@ -39,17 +112,25 @@ class JitHintsMap {
 
 
   static constexpr uint32_t MaxEntries_ = 4281;
-  static_assert(CacheSize_ == 16 && MaxEntries_ == 4281,
+  static_assert(EagerBaselineCacheSize_ == 16 && MaxEntries_ == 4281,
                 "MaxEntries should be recalculated for given CacheSize.");
 
-  uint32_t entryCount_ = 0;
+  uint32_t baselineEntryCount_ = 0;
+  void incrementBaselineEntryCount();
 
-  ScriptKey getScriptKey(JSScript* script) const;
-  void incrementEntryCount();
+  void updateAsRecentlyUsed(IonHint* hint);
+  IonHint* addIonHint(ScriptKey key, ScriptToHintMap::AddPtr& p);
 
  public:
+  ~JitHintsMap();
+
   void setEagerBaselineHint(JSScript* script);
   bool mightHaveEagerBaselineHint(JSScript* script) const;
+
+  bool recordIonCompilation(JSScript* script);
+  bool getIonThresholdHint(JSScript* script, uint32_t& thresholdOut);
+
+  void recordInvalidation(JSScript* script);
 };
 
 }  
