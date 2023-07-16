9a32c4e9395bb6d29cb67ed669b7916134a00ee3
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-07-16 01:21:11 +0000

    Bug 1835792: Enable HTTPS-Only exception button if HTTPS-First is enabled r=freddyb,settings-reviewers
    This should be seen as a temporary change since the UI probably needs a slightly bigger update if it should manage both HTTPS-Only and HTTPS-First.
    
    Depends on D179424
    
    Differential Revision: https://phabricator.services.mozilla.com/D179425
    
    UltraBlame original commit: f6b8e389ecdb14f71d85fa4da4a00ab7ae3174e6

--- browser/components/preferences/tests/browser_https_only_exceptions.js
+++ browser/components/preferences/tests/browser_https_only_exceptions.js
@@ -39,6 +39,21 @@ add_task(async function testButtons() {
     "HTTPS-Only exception button should be enabled when HTTPS-Only Mode enabled everywhere."
   );
 
+  await setHttpsOnlyPref("off");
+  await setHttpsFirstPref("private");
+  is(
+    exceptionButton.disabled,
+    true,
+    "HTTPS-Only exception button should be disabled when HTTPS-Only Mode is disabled and HTTPS-First Mode is only enabled in private browsing."
+  );
+
+  await setHttpsFirstPref("everywhere");
+  is(
+    exceptionButton.disabled,
+    false,
+    "HTTPS-Only exception button should be enabled when HTTPS-Only Mode is disabled and HTTPS-First Mode enabled everywhere."
+  );
+
   
   
   let promiseSubDialogLoaded = promiseLoadSubDialog(
@@ -205,6 +220,19 @@ async function setHttpsOnlyPref(state) {
     set: [
       ["dom.security.https_only_mode", state === "everywhere"],
       ["dom.security.https_only_mode_pbm", state === "private"],
+    ],
+  });
+}
+
+
+
+
+
+async function setHttpsFirstPref(state) {
+  await SpecialPowers.pushPrefEnv({
+    set: [
+      ["dom.security.https_first", state === "everywhere"],
+      ["dom.security.https_first_pbm", state === "private"],
     ],
   });
 }
