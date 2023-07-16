8320d19148cf1e34e2381f012e917053258fc98d
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-07-16 00:59:26 +0000

    Bug 1842010 - Recalculate frecency when a valid visit is added to a 0 frecency page. r=daisuke
    The trigger was checking for frecency <> 0 to skip error pages, though this way
    a page with frecency 0 that gets valid visits, will remain at 0, while it should
    become positive.
    This makes us always set the recalc flags on visit addition, then history will
    revert the change for error pages. Unfortunately atm there's no way to detect
    from the database if a visit was successfull. In the future we could be able to
    remove this workaround by improving visits metadata.
    
    Differential Revision: https://phabricator.services.mozilla.com/D182898
    
    UltraBlame original commit: f607e7ef4ffc1a7beb0a5cf27c6ba3dc8fbe7d98

--- toolkit/components/places/tests/browser/browser_visited_notfound.js
+++ toolkit/components/places/tests/browser/browser_visited_notfound.js
@@ -1,60 +1,76 @@
+
+
+
 add_task(async function test() {
-  const TEST_URL = "http://mochi.test:8888/notFoundPage.html";
-  
-  
-  Services.prefs.setCharPref("places.frecency.decayRate", "1.0");
-  let tab = await BrowserTestUtils.openNewForegroundTab(gBrowser);
-  registerCleanupFunction(async function () {
-    Services.prefs.clearUserPref("places.frecency.decayRate");
-    BrowserTestUtils.removeTab(tab);
-    await PlacesUtils.history.clear();
-  });
+  const url = "http://mochi.test:8888/notFoundPage.html";
 
   
   
-  await PlacesTestUtils.addVisits({ uri: TEST_URL });
+  await SpecialPowers.pushPrefEnv({
+    set: [["places.frecency.decayRate", "1.0"]],
+  });
+  await registerCleanupFunction(PlacesUtils.history.clear);
+
+  
+  
+  await PlacesTestUtils.addVisits(url);
   let frecency = await PlacesTestUtils.getDatabaseValue(
     "moz_places",
     "frecency",
-    { url: TEST_URL }
+    { url }
   );
-  is(frecency, 100, "Check initial frecency");
+  Assert.equal(frecency, 100, "Check initial frecency");
 
   
-  PlacesUtils.history.markPageAsTyped(NetUtil.newURI(TEST_URL));
+  PlacesUtils.history.markPageAsTyped(NetUtil.newURI(url));
 
-  let promiseVisit = new Promise(resolve => {
-    function onVisits(events) {
-      PlacesObservers.removeListener(["page-visited"], onVisits);
-      is(events.length, 1, "Right number of visits");
-      is(events[0].type, "page-visited");
-      is(events[0].url, TEST_URL, "Check visited url");
-      resolve();
+  await BrowserTestUtils.withNewTab(
+    {
+      gBrowser,
+      url,
+    },
+    async browser => {
+      info("awaiting for the visit");
+
+      Assert.equal(
+        await PlacesTestUtils.getDatabaseValue("moz_places", "frecency", {
+          url,
+        }),
+        frecency,
+        "Frecency should be unchanged"
+      );
+      Assert.equal(
+        await PlacesTestUtils.getDatabaseValue("moz_places", "hidden", {
+          url,
+        }),
+        0,
+        "Page should not be hidden"
+      );
+      Assert.equal(
+        await PlacesTestUtils.getDatabaseValue("moz_places", "typed", {
+          url,
+        }),
+        0,
+        "page should not be marked as typed"
+      );
+      Assert.equal(
+        await PlacesTestUtils.getDatabaseValue(
+          "moz_places",
+          "recalc_frecency",
+          { url }
+        ),
+        0,
+        "page should not be marked for frecency recalculation"
+      );
+      Assert.equal(
+        await PlacesTestUtils.getDatabaseValue(
+          "moz_places",
+          "recalc_alt_frecency",
+          { url }
+        ),
+        0,
+        "page should not be marked for alt frecency recalculation"
+      );
     }
-    PlacesObservers.addListener(["page-visited"], onVisits);
-  });
-  BrowserTestUtils.loadURIString(gBrowser.selectedBrowser, TEST_URL);
-  await promiseVisit;
-
-  is(
-    await PlacesTestUtils.getDatabaseValue("moz_places", "frecency", {
-      url: TEST_URL,
-    }),
-    frecency,
-    "Frecency should be unchanged"
-  );
-  is(
-    await PlacesTestUtils.getDatabaseValue("moz_places", "hidden", {
-      url: TEST_URL,
-    }),
-    0,
-    "Page should not be hidden"
-  );
-  is(
-    await PlacesTestUtils.getDatabaseValue("moz_places", "typed", {
-      url: TEST_URL,
-    }),
-    0,
-    "page should not be marked as typed"
   );
 });
