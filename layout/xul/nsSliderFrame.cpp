a2b2ead423e9bc7021b6b70954f7603b9dfdb9bb
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-07-16 01:08:18 +0000

    Bug 1842276 - Allow dynamically choosing whether scrollbar track click-and-hold has the 'Linux behavior' or the 'Windows/Mac behavior'. r=rzvncj
    Differential Revision: https://phabricator.services.mozilla.com/D183038
    
    UltraBlame original commit: 493bee1017812eb8955dee686a2c7e9798a1f7c3

--- layout/xul/nsSliderFrame.cpp
+++ layout/xul/nsSliderFrame.cpp
@@ -38,6 +38,7 @@
 #include "mozilla/MouseEvents.h"
 #include "mozilla/Preferences.h"
 #include "mozilla/PresShell.h"
+#include "mozilla/StaticPrefs_layout.h"
 #include "mozilla/SVGIntegrationUtils.h"
 #include "mozilla/Telemetry.h"
 #include "mozilla/dom/Document.h"
@@ -1403,21 +1404,17 @@ nsSliderFrame::HandlePress(nsPresContext* aPresContext, WidgetGUIEvent* aEvent,
 
   mRepeatDirection = change;
   DragThumb(true);
-  
-  
-  
-  
-#ifdef MOZ_WIDGET_GTK
-  
-  
-  if (change > 0) {
-    mDestinationPoint = nsPoint(GetRect().width, GetRect().height);
+  if (StaticPrefs::layout_scrollbars_click_and_hold_track_continue_to_end()) {
+    
+    
+    if (change > 0) {
+      mDestinationPoint = nsPoint(GetRect().width, GetRect().height);
+    } else {
+      mDestinationPoint = nsPoint(0, 0);
+    }
   } else {
-    mDestinationPoint = nsPoint(0, 0);
+    mDestinationPoint = eventPoint;
   }
-#else
-  mDestinationPoint = eventPoint;
-#endif
   StartRepeat();
   PageScroll(false);
 
