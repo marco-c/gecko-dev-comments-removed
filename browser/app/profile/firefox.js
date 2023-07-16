1382472074251e77514f2d87e4951c404a8e8eb0
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-07-16 01:46:45 +0000

    Backed out changeset 2b33eb976812 (bug 1843261) for causing assertion failures in widget/PuppetWidget.cpp CLOSED TREE
    UltraBlame original commit: fd8fb7be79bb55c487b5259c4a57ccf507c61403

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -2076,6 +2076,7 @@ pref("privacy.fingerprintingProtection.pbmode", true);
 
 
 pref("browser.tabs.remote.autostart", true);
+pref("browser.tabs.remote.desktopbehavior", true);
 
 
 pref("media.peerconnection.mtransport_process", true);
