444b4495fa07902e80cef27f9d72757c084a6145
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-07-16 01:45:29 +0000

    Bug 1843086: Line participants use block start ascent, instead of not-yet stored baseline. r=emilio
    Differential Revision: https://phabricator.services.mozilla.com/D183432
    
    UltraBlame original commit: 63e6ab3c3ef623c6fbfa6b565c0ebd710c44ce42

--- layout/generic/nsLineLayout.cpp
+++ layout/generic/nsLineLayout.cpp
@@ -1349,16 +1349,19 @@ void nsLineLayout::PlaceFrame(PerFrameData* pfd, ReflowOutput& aMetrics) {
                        ? lineWM.IsLineInverted() ? 0 : aMetrics.BSize(lineWM)
                        : aMetrics.BSize(lineWM) / 2;
   } else {
-    if (pfd->mFrame->StyleDisplay()->mBaselineSource ==
-        StyleBaselineSource::Auto) {
+    
+    
+    const auto baselineSource = pfd->mFrame->StyleDisplay()->mBaselineSource;
+    if (baselineSource == StyleBaselineSource::Auto ||
+        pfd->mFrame->IsFrameOfType(nsIFrame::eLineParticipant)) {
       if (aMetrics.BlockStartAscent() == ReflowOutput::ASK_FOR_BASELINE) {
         pfd->mAscent = pfd->mFrame->GetLogicalBaseline(lineWM);
       } else {
         pfd->mAscent = aMetrics.BlockStartAscent();
       }
     } else {
-      const auto sourceGroup = [pfd]() {
-        switch (pfd->mFrame->StyleDisplay()->mBaselineSource) {
+      const auto sourceGroup = [baselineSource]() {
+        switch (baselineSource) {
           case StyleBaselineSource::First:
             return BaselineSharingGroup::First;
           case StyleBaselineSource::Last:
