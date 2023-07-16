0edc76ed083f06647f3cc282cc2452bbd7e8de11
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-07-16 01:03:07 +0000

    Bug 1837686.  r=jandem,rhunt.
    Differential Revision: https://phabricator.services.mozilla.com/D182324
    
    UltraBlame original commit: 9c24846204b8dc4ede7a736a8d62fa6d7cfdc965

--- js/src/jit/AliasAnalysis.cpp
+++ js/src/jit/AliasAnalysis.cpp
@@ -1,8 +1,8 @@
-
-
-
-
-
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ * vim: set ts=8 sts=2 et sw=2 tw=80:
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "jit/AliasAnalysis.h"
 
@@ -39,8 +39,8 @@ class LoopAliasInfo : public TempObject {
   MInstruction* firstInstruction() const { return *loopHeader_->begin(); }
 };
 
-}  
-}  
+}  // namespace jit
+}  // namespace js
 
 void AliasAnalysis::spewDependencyList() {
 #ifdef JS_JITSPEW
@@ -73,9 +73,9 @@ void AliasAnalysis::spewDependencyList() {
 #endif
 }
 
-
-
-
+// Whether there might be a path from src to dest, excluding loop backedges.
+// This is approximate and really ought to depend on precomputed reachability
+// information.
 static inline bool BlockMightReach(MBasicBlock* src, MBasicBlock* dest) {
   while (src->id() <= dest->id()) {
     if (src == dest) {
@@ -87,7 +87,7 @@ static inline bool BlockMightReach(MBasicBlock* src, MBasicBlock* dest) {
       case 1: {
         MBasicBlock* successor = src->getSuccessor(0);
         if (successor->id() <= src->id()) {
-          return true;  
+          return true;  // Don't iloop.
         }
         src = successor;
         break;
@@ -131,28 +131,28 @@ static void IonSpewAliasInfo(const char* pre, MInstruction* ins,
 #endif
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// [SMDOC] IonMonkey Alias Analysis
+//
+// This pass annotates every load instruction with the last store instruction
+// on which it depends. The algorithm is optimistic in that it ignores explicit
+// dependencies and only considers loads and stores.
+//
+// Loads inside loops only have an implicit dependency on a store before the
+// loop header if no instruction inside the loop body aliases it. To calculate
+// this efficiently, we maintain a list of maybe-invariant loads and the
+// combined alias set for all stores inside the loop. When we see the loop's
+// backedge, this information is used to mark every load we wrongly assumed to
+// be loop invariant as having an implicit dependency on the last instruction of
+// the loop header, so that it's never moved before the loop header.
+//
+// The algorithm depends on the invariant that both control instructions and
+// effectful instructions (stores) are never hoisted.
 bool AliasAnalysis::analyze() {
   JitSpew(JitSpew_Alias, "Begin");
   Vector<MInstructionVector, AliasSet::NumCategories, JitAllocPolicy> stores(
       alloc());
 
-  
+  // Initialize to the first instruction.
   MInstruction* firstIns = *graph_.entryBlock()->begin();
   for (unsigned i = 0; i < AliasSet::NumCategories; i++) {
     MInstructionVector defs(alloc());
@@ -164,8 +164,8 @@ bool AliasAnalysis::analyze() {
     }
   }
 
-  
-  
+  // Type analysis may have inserted new instructions. Since this pass depends
+  // on the instruction number ordering, all instructions are renumbered.
   uint32_t newId = 0;
 
   for (ReversePostorderIterator block(graph_.rpoBegin());
@@ -187,8 +187,7 @@ bool AliasAnalysis::analyze() {
       def->setId(newId++);
     }
 
-    for (MInstructionIterator def(block->begin()),
-         end(block->begin(block->lastIns()));
+    for (MInstructionIterator def(block->begin()), end(block->end());
          def != end; ++def) {
       def->setId(newId++);
 
@@ -197,9 +196,9 @@ bool AliasAnalysis::analyze() {
         continue;
       }
 
-      
-      
-      
+      // For the purposes of alias analysis, all recoverable operations
+      // are treated as effect free as the memory represented by these
+      // operations cannot be aliased by others.
       if (def->canRecoverOnBailout()) {
         continue;
       }
@@ -221,7 +220,7 @@ bool AliasAnalysis::analyze() {
         }
 #endif
       } else {
-        
+        // Find the most recent store on which this instruction depends.
         MInstruction* lastStore = firstIns;
 
         for (AliasSetIterator iter(set); iter; iter++) {
@@ -241,9 +240,9 @@ bool AliasAnalysis::analyze() {
         def->setDependency(lastStore);
         IonSpewDependency(*def, lastStore, "depends", "");
 
-        
-        
-        
+        // If the last store was before the current loop, we assume this load
+        // is loop invariant. If a later instruction writes to the same
+        // location, we will fix this at the end of the loop.
         if (loop_ && lastStore->id() < loop_->firstInstruction()->id()) {
           if (!loop_->addInvariantLoad(*def)) {
             return false;
@@ -251,10 +250,6 @@ bool AliasAnalysis::analyze() {
         }
       }
     }
-
-    
-    
-    block->lastIns()->setId(newId++);
 
     if (block->isLoopBackedge()) {
       MOZ_ASSERT(loop_->loopHeader() == block->loopHeaderOfBackedge());
@@ -290,10 +285,10 @@ bool AliasAnalysis::analyze() {
         }
 
         if (hasAlias) {
-          
-          
-          
-          
+          // This instruction depends on stores inside the loop body. Mark it as
+          // having a dependency on the last instruction of the loop header. The
+          // last instruction is a control instruction and these are never
+          // hoisted.
           MControlInstruction* controlIns = loop_->loopHeader()->lastIns();
           IonSpewDependency(ins, controlIns, "depends",
                             "due to stores in loop body");
