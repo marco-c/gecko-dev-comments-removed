4439b448a76042806ea062edc915035c8e775cfc
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-07-16 01:21:33 +0000

    Bug 1832733 - Unexpose sizeToContent() in Nightly. r=emilio
    Differential Revision: https://phabricator.services.mozilla.com/D177867
    
    UltraBlame original commit: 0a174d6682ba7a03467bc3b5282b9c61e1c9e64e

--- dom/tests/mochitest/general/test_interfaces.js
+++ dom/tests/mochitest/general/test_interfaces.js
@@ -1933,7 +1933,7 @@ let interfaceNamesInGlobalScope = [
   
   { name: "setTimeout", insecureContext: true },
   
-  { name: "sizeToContent", insecureContext: true },
+  { name: "sizeToContent", insecureContext: true, nightly: false },
   
   { name: "speechSynthesis", insecureContext: true },
   
