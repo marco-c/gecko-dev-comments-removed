515a77021c174a64b537e9618be562c7e1262298
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-07-16 01:43:23 +0000

    Bug 1836248 - Integrate the extended stripOnShare feature into the URLQueryStringStripper. r=pbz,anti-tracking-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D181462
    
    UltraBlame original commit: e371dc4f5daf31617af3f72dd2e73f2c2340e1de

--- toolkit/components/antitracking/test/browser/browser_urlQueryStrippingListService.js
+++ toolkit/components/antitracking/test/browser/browser_urlQueryStrippingListService.js
@@ -25,6 +25,18 @@ const TEST_THIRD_PARTY_URI = TEST_DOMAIN_2 + TEST_PATH + "empty.html";
 
 
 class UpdateEvent extends EventTarget {}
+
+
+
+class ListObserver {
+  updateEvent = new UpdateEvent();
+
+  onQueryStrippingListUpdate(stripList, allowList) {
+    let event = new CustomEvent("update", { detail: { stripList, allowList } });
+    this.updateEvent.dispatchEvent(event);
+  }
+}
+
 function waitForEvent(element, eventName) {
   return BrowserTestUtils.waitForEvent(element, eventName).then(e => e.detail);
 }
@@ -100,12 +112,8 @@ add_task(async function testPrefSettings() {
   });
 
   
-  let updateEvent = new UpdateEvent();
-  let obs = (stripList, allowList) => {
-    let event = new CustomEvent("update", { detail: { stripList, allowList } });
-    updateEvent.dispatchEvent(event);
-  };
-  let promise = waitForEvent(updateEvent, "update");
+  let obs = new ListObserver();
+  let promise = waitForEvent(obs.updateEvent, "update");
   urlQueryStrippingListService.registerAndRunObserver(obs);
   let lists = await promise;
   is(lists.stripList, "", "No strip list at the beginning.");
@@ -116,7 +124,7 @@ add_task(async function testPrefSettings() {
   await check("pref_query2=456", "pref_query2=456");
 
   
-  promise = waitForEvent(updateEvent, "update");
+  promise = waitForEvent(obs.updateEvent, "update");
   await SpecialPowers.pushPrefEnv({
     set: [["privacy.query_stripping.strip_list", "pref_query1 pref_query2"]],
   });
@@ -134,7 +142,7 @@ add_task(async function testPrefSettings() {
   await check("pref_query2=456", "");
 
   
-  promise = waitForEvent(updateEvent, "update");
+  promise = waitForEvent(obs.updateEvent, "update");
   await SpecialPowers.pushPrefEnv({
     set: [["privacy.query_stripping.allow_list", "example.net"]],
   });
@@ -173,12 +181,8 @@ add_task(async function testRemoteSettings() {
   await db.importChanges({}, Date.now(), []);
 
   
-  let updateEvent = new UpdateEvent();
-  let obs = (stripList, allowList) => {
-    let event = new CustomEvent("update", { detail: { stripList, allowList } });
-    updateEvent.dispatchEvent(event);
-  };
-  let promise = waitForEvent(updateEvent, "update");
+  let obs = new ListObserver();
+  let promise = waitForEvent(obs.updateEvent, "update");
   urlQueryStrippingListService.registerAndRunObserver(obs);
   let lists = await promise;
   is(lists.stripList, "", "No strip list at the beginning.");
@@ -189,7 +193,7 @@ add_task(async function testRemoteSettings() {
   await check("remote_query2=456", "remote_query2=456");
 
   
-  promise = waitForEvent(updateEvent, "update");
+  promise = waitForEvent(obs.updateEvent, "update");
   await RemoteSettings(COLLECTION_NAME).emit("sync", {
     data: {
       current: [
@@ -216,7 +220,7 @@ add_task(async function testRemoteSettings() {
   await check("remote_query2=456", "");
 
   
-  promise = waitForEvent(updateEvent, "update");
+  promise = waitForEvent(obs.updateEvent, "update");
   await RemoteSettings(COLLECTION_NAME).emit("sync", {
     data: {
       current: [
