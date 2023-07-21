b4c4a500257c1acbf03df79360a8733e436e4686
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-07-21 19:10:58 +0000

    Bug 1843035: part 1) Align unbinding a popover node from the tree closer to the spec. r=emilio
    Differential Revision: https://phabricator.services.mozilla.com/D183381
    
    UltraBlame original commit: 238e99b4449ccbe55588e7b9425b631bf656837d

--- dom/html/nsGenericHTMLElement.cpp
+++ dom/html/nsGenericHTMLElement.cpp
@@ -489,10 +489,8 @@ nsresult nsGenericHTMLElement::BindToTree(BindContext& aContext,
 void nsGenericHTMLElement::UnbindFromTree(bool aNullParent) {
   if (IsInComposedDoc()) {
     
-    
-    
-    
-    if (GetPopoverData()) {
+    AssertPopoverAttributeStateCorrespondsToAttributePresence();
+    if (GetPopoverAttributeState() != PopoverAttributeState::None) {
       HidePopoverWithoutRunningScript();
     }
     RegUnRegAccessKey(false);
@@ -3211,15 +3209,13 @@ bool nsGenericHTMLElement::PopoverOpen() const {
 bool nsGenericHTMLElement::CheckPopoverValidity(
     PopoverVisibilityState aExpectedState, Document* aExpectedDocument,
     ErrorResult& aRv) {
+  AssertPopoverAttributeStateCorrespondsToAttributePresence();
   const PopoverData* data = GetPopoverData();
   if (!data ||
       data->GetPopoverAttributeState() == PopoverAttributeState::None) {
-    MOZ_ASSERT(!HasAttr(nsGkAtoms::popover));
     aRv.ThrowNotSupportedError("Element is in the no popover state");
     return false;
   }
-
-  MOZ_ASSERT(HasAttr(nsGkAtoms::popover));
 
   if (data->GetPopoverVisibilityState() != aExpectedState) {
     return false;
@@ -3248,6 +3244,20 @@ bool nsGenericHTMLElement::CheckPopoverValidity(
   }
 
   return true;
+}
+
+void nsGenericHTMLElement::
+    AssertPopoverAttributeStateCorrespondsToAttributePresence() const {
+#ifdef DEBUG
+  if (GetPopoverAttributeState() == PopoverAttributeState::None) {
+    MOZ_ASSERT(!HasAttr(nsGkAtoms::popover));
+  } else {
+    MOZ_ASSERT(HasAttr(nsGkAtoms::popover));
+  }
+#else
+  {
+  }
+#endif  
 }
 
 PopoverAttributeState nsGenericHTMLElement::GetPopoverAttributeState() const {
