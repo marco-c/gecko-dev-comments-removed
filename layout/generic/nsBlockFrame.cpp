9c4d6d90fad8aba38c44dc3926539b039049e2e4
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-07-21 19:07:15 +0000

    Bug 1816474 - Assertion failure: IsHiddenByContentVisibilityOfInFlowParentForLayout() in nsIFrame.cpp, r=emilio
    Differential Revision: https://phabricator.services.mozilla.com/D182805
    
    UltraBlame original commit: df6629fd2971502d13a5fd42f0ad8cf11e169921

--- layout/generic/nsBlockFrame.cpp
+++ layout/generic/nsBlockFrame.cpp
@@ -3420,8 +3420,12 @@ void nsBlockFrame::ReflowLine(BlockReflowState& aState, LineIterator aLine,
   
   
   
+  
+  
+  
   nsIFrame* firstChild = aLine->mFirstChild;
-  if (firstChild->IsHiddenByContentVisibilityOfInFlowParentForLayout()) {
+  if (firstChild->IsHiddenByContentVisibilityOfInFlowParentForLayout() &&
+      !HasAnyStateBits(NS_FRAME_OWNS_ANON_BOXES)) {
     return;
   }
 
@@ -7726,10 +7730,25 @@ void nsBlockFrame::CheckFloats(BlockReflowState& aState) {
 
   AutoTArray<nsIFrame*, 8> storedFloats;
   bool equal = true;
+  bool hasHiddenFloats = false;
   uint32_t i = 0;
   for (nsIFrame* f : mFloats) {
     if (f->HasAnyStateBits(NS_FRAME_IS_PUSHED_FLOAT)) {
       continue;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    if (!hasHiddenFloats &&
+        f->IsHiddenByContentVisibilityOfInFlowParentForLayout()) {
+      hasHiddenFloats = true;
     }
     storedFloats.AppendElement(f);
     if (i < lineFloats.Length() && lineFloats.ElementAt(i) != f) {
@@ -7739,7 +7758,7 @@ void nsBlockFrame::CheckFloats(BlockReflowState& aState) {
   }
 
   if ((!equal || lineFloats.Length() != storedFloats.Length()) &&
-      !anyLineDirty) {
+      !anyLineDirty && !hasHiddenFloats) {
     NS_ERROR(
         "nsBlockFrame::CheckFloats: Explicit float list is out of sync with "
         "float cache");
