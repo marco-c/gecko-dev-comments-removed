121c79bd111d1978e5793bdaaa8c2308c97226f2
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-07-21 19:10:44 +0000

    Bug 1574540 - Remove devtools.markup.mutationBreakpoints.enabled pref r=devtools-reviewers,nchevobbe
    Differential Revision: https://phabricator.services.mozilla.com/D183822
    
    UltraBlame original commit: 85254efcd99b198328d68a191c74258bfff941e4

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -2498,9 +2498,6 @@ pref("devtools.markup.collapseAttributeLength", 120);
 pref("devtools.markup.beautifyOnCopy", false);
 
 
-pref("devtools.markup.mutationBreakpoints.enabled", true);
-
-
 pref("devtools.defaultColorUnit", "authored");
 
 
