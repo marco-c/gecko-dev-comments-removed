5ff594928e9657f192b744490d674e6076964635
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-07-27 15:03:20 +0000

    Bug 1845064 - Update files in browser/base/content/test/static/ to pass the rejected-words linter. r=Gijs
    Differential Revision: https://phabricator.services.mozilla.com/D184365
    
    UltraBlame original commit: 6588b692c52d94e933485af2756257f00c143078

--- browser/base/content/test/static/browser_parsable_script.js
+++ browser/base/content/test/static/browser_parsable_script.js
@@ -8,7 +8,7 @@
 
 requestLongerTimeout(2);
 
-const kWhitelist = new Set([
+const kAllowlist = new Set([
   /browser\/content\/browser\/places\/controller.js$/,
 ]);
 
@@ -40,9 +40,9 @@ init();
 
 
 
-function uriIsWhiteListed(uri) {
-  for (let whitelistItem of kWhitelist) {
-    if (whitelistItem.test(uri.spec)) {
+function uriIsAllowed(uri) {
+  for (let allowlistItem of kAllowlist) {
+    if (allowlistItem.test(uri.spec)) {
       return true;
     }
   }
@@ -60,8 +60,8 @@ function uriIsESModule(uri) {
     return true;
   }
 
-  for (let whitelistItem of kESModuleList) {
-    if (whitelistItem.test(uri.spec)) {
+  for (let allowlistItem of kESModuleList) {
+    if (allowlistItem.test(uri.spec)) {
       return true;
     }
   }
@@ -153,8 +153,8 @@ add_task(async function checkAllTheJS() {
   
   
   await PerfTestHelpers.throttledMapPromises(uris, uri => {
-    if (uriIsWhiteListed(uri)) {
-      info("Not checking whitelisted " + uri.spec);
+    if (uriIsAllowed(uri)) {
+      info("Not checking allowlisted " + uri.spec);
       return undefined;
     }
     let target = "script";
