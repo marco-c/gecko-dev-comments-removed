3a0a8839a65e0b8e1a4bd876e8c7a97aec421ad7
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-07-27 15:06:03 +0000

    Bug 1844908 - Remove pre-Win10-specific codepath from uriloader/exthandler/. r=Gijs
    Differential Revision: https://phabricator.services.mozilla.com/D184306
    
    UltraBlame original commit: 225dc3aeb22057b38d3dda142fde78360b3d1f90

--- uriloader/exthandler/win/nsOSHelperAppService.cpp
+++ uriloader/exthandler/win/nsOSHelperAppService.cpp
@@ -17,7 +17,6 @@
 #include "nsIWindowsRegKey.h"
 #include "nsXULAppAPI.h"
 #include "mozilla/UniquePtrExtensions.h"
-#include "mozilla/WindowsVersion.h"
 
 
 #include <shellapi.h>
@@ -119,23 +118,21 @@ NS_IMETHODIMP nsOSHelperAppService::GetApplicationDescription(
 
   NS_ConvertASCIItoUTF16 buf(aScheme);
 
-  if (mozilla::IsWin8OrLater()) {
-    wchar_t result[1024];
-    DWORD resultSize = 1024;
-    HRESULT hr = AssocQueryString(0x1000 ,
-                                  ASSOCSTR_FRIENDLYAPPNAME, buf.get(), NULL,
-                                  result, &resultSize);
-    if (SUCCEEDED(hr)) {
-      _retval = result;
-      return NS_OK;
-    }
+  wchar_t result[1024];
+  DWORD resultSize = 1024;
+  HRESULT hr = AssocQueryString(0x1000 ,
+                                ASSOCSTR_FRIENDLYAPPNAME, buf.get(), NULL,
+                                result, &resultSize);
+  if (SUCCEEDED(hr)) {
+    _retval = result;
+    return NS_OK;
   }
 
   NS_ENSURE_TRUE(mAppAssoc, NS_ERROR_NOT_AVAILABLE);
   wchar_t* pResult = nullptr;
   
-  HRESULT hr = mAppAssoc->QueryCurrentDefault(buf.get(), AT_URLPROTOCOL,
-                                              AL_EFFECTIVE, &pResult);
+  hr = mAppAssoc->QueryCurrentDefault(buf.get(), AT_URLPROTOCOL, AL_EFFECTIVE,
+                                      &pResult);
   if (SUCCEEDED(hr)) {
     nsCOMPtr<nsIFile> app;
     nsAutoString appInfo(pResult);
