548534fe9ef39795e8f69486edb42465dea50579
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-07-27 15:07:51 +0000

    Bug 1844908 - Remove pre-Win10-specific codepath from hal/. r=gsvelto
    Differential Revision: https://phabricator.services.mozilla.com/D184293
    
    UltraBlame original commit: 98708293466ce40c6b4018f64c407e5a7ea733fb

--- hal/windows/WindowsScreenConfiguration.cpp
+++ hal/windows/WindowsScreenConfiguration.cpp
@@ -8,7 +8,6 @@
 #endif
 
 #include "Hal.h"
-#include "mozilla/WindowsVersion.h"
 #include "mozilla/widget/ScreenManager.h"
 #include "nsIWindowsUIUtils.h"
 #include "WinUtils.h"
@@ -23,12 +22,6 @@ static decltype(SetDisplayAutoRotationPreferences)*
 
 RefPtr<GenericNonExclusivePromise> LockScreenOrientation(
     const hal::ScreenOrientation& aOrientation) {
-  
-  
-  if (!IsWin8OrLater()) {
-    return GenericNonExclusivePromise::CreateAndReject(
-        NS_ERROR_DOM_NOT_SUPPORTED_ERR, __func__);
-  }
   AR_STATE state;
   if (!widget::WinUtils::GetAutoRotationState(&state)) {
     return GenericNonExclusivePromise::CreateAndReject(
