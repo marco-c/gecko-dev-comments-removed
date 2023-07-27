1de4af15b8e8d436df8e68073e46013173cb108d
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-07-27 15:07:49 +0000

    Bug 1844908 - Remove pre-Win10-specific codepath from browser/components/shell/. r=nalexander
    Differential Revision: https://phabricator.services.mozilla.com/D184288
    
    UltraBlame original commit: ba1f4b46cfe0bdcd523216aa885a09d79af78c2c

--- browser/components/shell/WindowsDefaultBrowser.cpp
+++ browser/components/shell/WindowsDefaultBrowser.cpp
@@ -177,7 +177,7 @@ bool LaunchModernSettingsDialogDefaultApps() {
     return LaunchMsSettingsProtocol();
   }
 
-  if (!mozilla::IsWindowsBuildOrLater(14965) && !IsWindowsLogonConnected() &&
+  if (!mozilla::IsWindows10BuildOrLater(14965) && !IsWindowsLogonConnected() &&
       SettingsAppBelievesConnected()) {
     
     
