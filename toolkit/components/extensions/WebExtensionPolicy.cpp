4864a981b27473d6a3a83faad3b56bbcfa39ed60
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-07-27 15:07:04 +0000

    Bug 1841483 - activeTab should always match same-origin frames r=zombie
    Differential Revision: https://phabricator.services.mozilla.com/D184179
    
    UltraBlame original commit: 17f9ed6698aaa87c31dced8cd2d939cae19f367a

--- toolkit/components/extensions/WebExtensionPolicy.cpp
+++ toolkit/components/extensions/WebExtensionPolicy.cpp
@@ -1007,8 +1007,13 @@ bool DocInfo::IsTopLevel() const {
 }
 
 bool WindowShouldMatchActiveTab(nsPIDOMWindowOuter* aWin) {
-  for (WindowContext* wc = aWin->GetCurrentInnerWindow()->GetWindowContext();
-       wc; wc = wc->GetParentWindowContext()) {
+  WindowContext* wc = aWin->GetCurrentInnerWindow()->GetWindowContext();
+  if (wc && wc->SameOriginWithTop()) {
+    
+    
+    return true;
+  }
+  for (; wc; wc = wc->GetParentWindowContext()) {
     BrowsingContext* bc = wc->GetBrowsingContext();
     if (bc->IsTopContent()) {
       return true;
