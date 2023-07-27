0131883c4546b6092072d823087a303b9a80ea51
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-07-27 15:04:41 +0000

    Backed out changeset 954433fb5cc5 (bug 1687440) for causing failures in browser_dbg-preview-frame.js CLOSED TREE
    UltraBlame original commit: d03615dc9504c50f6a54e5deb1d66d59b482def5

--- devtools/client/debugger/src/components/Editor/Preview/ExceptionPopup.js
+++ devtools/client/debugger/src/components/Editor/Preview/ExceptionPopup.js
@@ -61,7 +61,7 @@ class ExceptionPopup extends Component {
   onTopWindowClick = e => {
     
     
-    if (!e.target.closest(POPUP_SELECTOR) && this.props.clearPreview) {
+    if (!e.target.closest(POPUP_SELECTOR)) {
       this.props.clearPreview();
     }
   };
--- devtools/client/debugger/test/mochitest/shared-head.js
+++ devtools/client/debugger/test/mochitest/shared-head.js
@@ -1757,6 +1757,7 @@ const selectors = {
     `${selectors.resultItems}:nth-child(${i})[title$="${name}"]`,
   fileMatch: ".project-text-search .line-value",
   popup: ".popover",
+  tooltip: ".tooltip",
   previewPopup: ".preview-popup",
   openInspector: "button.open-inspector",
   outlineItem: i =>
@@ -2262,20 +2263,11 @@ async function tryHovering(dbg, line, column, elementName) {
   return { element, tokenEl };
 }
 
-
-
-
-
-
-
-
-
-
 async function assertPreviewTextValue(
   dbg,
   line,
   column,
-  { result, expression, doNotClose = false }
+  { text, expression, doNotClose = false }
 ) {
   const { element: previewEl, tokenEl } = await tryHovering(
     dbg,
@@ -2293,11 +2285,11 @@ async function assertPreviewTextValue(
   );
 
   ok(
-    previewEl.innerText.includes(result),
+    previewEl.innerText.includes(text),
     "Popup preview text shown to user. Got: " +
       previewEl.innerText +
       " Expected: " +
-      result
+      text
   );
 
   if (!doNotClose) {
@@ -2305,10 +2297,26 @@ async function assertPreviewTextValue(
   }
 }
 
+async function assertPreviewTooltip(dbg, line, column, { result, expression }) {
+  const { element: previewEl, tokenEl } = await tryHovering(
+    dbg,
+    line,
+    column,
+    "tooltip"
+  );
 
+  ok(
+    tokenEl.innerText.includes(expression),
+    "Tooltip preview hovered expression is correct. Got: " +
+      tokenEl.innerText +
+      " Expected: " +
+      expression
+  );
 
+  is(previewEl.innerText, result, "Tooltip preview text shown to user");
 
-
+  await closePreviewForToken(dbg, tokenEl);
+}
 
 async function assertPreviews(dbg, previews) {
   for (const { line, column, expression, result, fields } of previews) {
@@ -2324,16 +2332,6 @@ async function assertPreviews(dbg, previews) {
         column,
         "popup"
       );
-
-      info("Wait for top level node to expand and child nodes to load");
-      await waitForElementWithSelector(
-        dbg,
-        ".preview-popup .node:first-of-type .arrow.expanded"
-      );
-      await waitUntil(
-        () => popupEl.querySelectorAll(".preview-popup .node").length > 1
-      );
-
       const oiNodes = Array.from(
         popupEl.querySelectorAll(".preview-popup .node")
       );
@@ -2357,7 +2355,7 @@ async function assertPreviews(dbg, previews) {
     } else {
       await assertPreviewTextValue(dbg, line, column, {
         expression,
-        result,
+        text: result,
       });
     }
   }
