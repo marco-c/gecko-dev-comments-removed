40bfaad8e2b9f4b128a8b2bb86bd976212c9ab1b
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-08-01 00:37:10 +0000

    Bug 1845095: Bloom filter for fast-rejecting `:has()`. r=emilio
    Differential Revision: https://phabricator.services.mozilla.com/D184525
    
    UltraBlame original commit: 2b7db53664a6271833f1442dd1f0e4f300d866e0

--- servo/components/selectors/tree.rs
+++ servo/components/selectors/tree.rs
@@ -6,6 +6,7 @@
 
 
 use crate::attr::{AttrSelectorOperation, CaseSensitivity, NamespaceConstraint};
+use crate::bloom::BloomFilter;
 use crate::matching::{ElementSelectorFlags, MatchingContext};
 use crate::parser::SelectorImpl;
 use std::fmt::Debug;
@@ -160,4 +161,8 @@ pub trait Element: Sized + Clone + Debug {
     fn ignores_nth_child_selectors(&self) -> bool {
         false
     }
+
+    
+    
+    fn add_element_unique_hashes(&self, filter: &mut BloomFilter) -> bool;
 }
