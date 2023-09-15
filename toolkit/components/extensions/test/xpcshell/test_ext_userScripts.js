d8299c7b31c458e2dcbda08a5a097f0949c5c30b
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-09-15 23:02:05 +0000

    Bug 1700482 - Fix and re-enable test_ext_userScripts.js on Android r=willdurand
    test_ext_userScripts.js was failing on Android because the
    "webextension-optional-permission-prompt" notification triggers the
    "GeckoView:WebExtension:OptionalPrompt" message. While this message is
    handled in GVE, TestRunnerActivity and Fenix, that is not the case for
    XpcshellTestRunnerService. This results in an uncaught promise
    rejection, thus a test failure.
    
    Since the test doesn't really care about the underlying prompting
    implementation, disable the prompt logic with the relevant pref.
    
    Differential Revision: https://phabricator.services.mozilla.com/D184189
    
    UltraBlame original commit: e499148c97a0c7f0d5d3f64d01f390e3f432785e

--- toolkit/components/extensions/test/xpcshell/test_ext_userScripts.js
+++ toolkit/components/extensions/test/xpcshell/test_ext_userScripts.js
@@ -21,21 +21,12 @@ add_task(async function setup_test_environment() {
   }
 
   
-  function permissionObserver(subject, topic, data) {
-    if (topic == "webextension-optional-permission-prompt") {
-      let { resolve } = subject.wrappedJSObject;
-      resolve(true);
-    }
-  }
-  Services.obs.addObserver(
-    permissionObserver,
-    "webextension-optional-permission-prompt"
+  Services.prefs.setBoolPref(
+    "extensions.webextOptionalPermissionPrompts",
+    false
   );
   registerCleanupFunction(() => {
-    Services.obs.removeObserver(
-      permissionObserver,
-      "webextension-optional-permission-prompt"
-    );
+    Services.prefs.clearUserPref("extensions.webextOptionalPermissionPrompts");
   });
 });
 
