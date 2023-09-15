f5f5328d8259d60fa78cbcf2c39530e3f42cfa65
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-09-15 23:03:05 +0000

    Bug 1844543 [Wayland] Use nsWindow::GetCachedCeiledScaleFactor() to get scale factor from renderer/conpositor thread r=emilio
    GdkCeiledScaleFactor() can't be called from non-main thread (compositor, renderer etc.).
    In such case just return recent scale factor and don't update it.
    If it's changed it will be updated from main thread later.
    
    Differential Revision: https://phabricator.services.mozilla.com/D184171
    
    UltraBlame original commit: f19933e3f455b6e0c0a206b3fcb49626e1140724

--- widget/gtk/nsWindow.cpp
+++ widget/gtk/nsWindow.cpp
@@ -8895,7 +8895,13 @@ GtkWindow* nsWindow::GetCurrentTopmostWindow() const {
   return topmostParentWindow;
 }
 
+
+
+gint nsWindow::GetCachedCeiledScaleFactor() const { return mWindowScaleFactor; }
+
 gint nsWindow::GdkCeiledScaleFactor() {
+  MOZ_ASSERT(NS_IsMainThread());
+
   
   
   if (mWindowType == WindowType::TopLevel && !mWindowScaleFactorChanged) {
@@ -9717,7 +9723,10 @@ void nsWindow::SetEGLNativeWindowSize(
     return;
   }
 
-  gint scale = GdkCeiledScaleFactor();
+  
+  
+  gint scale =
+      NS_IsMainThread() ? GdkCeiledScaleFactor() : GetCachedCeiledScaleFactor();
   LOG("nsWindow::SetEGLNativeWindowSize() %d x %d scale %d (unscaled %d x %d)",
       aEGLWindowSize.width, aEGLWindowSize.height, scale,
       aEGLWindowSize.width / scale, aEGLWindowSize.height / scale);
