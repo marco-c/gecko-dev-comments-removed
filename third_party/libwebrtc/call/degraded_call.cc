da2eeb9e1c0060ea2135b6834da7b5e80a8ebc8b
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-09-15 23:25:43 +0000

    Bug 1843113 - Vendor libwebrtc from f9ffd68d8e
    Upstream commit: https://webrtc.googlesource.com/src/+/f9ffd68d8eb3b668056664bf67d0e0c540cd200c
        Migrate PostTask+Wait to BlockingCall to avoid deadlock in DegradedCall.
    
        The deadlock happens when the WebRtcCombinedNetworkAndWorkerThread
        experiment is running because the worker thread doing the PostTask is
        the same thread as the network thread. When using BlockingCall instead
        this method will avoid the PostTask and just execute in-line instead
        if the experiment is running and otherwise do what the old path did.
    
        As per webrtc:15099, we do not want to increase uses of rtc::Thread in
        general, and adding more block-invokes in is also discouraged
        (webrtc:12649) so instead of adding new methods to TaskQueueBase we
        simply do a static_cast<rtc::Thread*>.
    
        When WebRtcCombinedNetworkAndWorkerThread has launched the blocking
        call can be removed because then we're on a single thread always.
    
        Bug: webrtc:15098
        Change-Id: I6dcc09bcf6ee0ad12e4beffef3b206989265540b
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/301880
        Reviewed-by: Tomas Gunnarsson <tommiwebrtc.org>
        Commit-Queue: Henrik Boström <hboswebrtc.org>
        Cr-Commit-Position: refs/heads/main{#39894}
    
    UltraBlame original commit: 1cc8c615ebc8e7f4d6e9666b4c41d5638f4f31fd

--- third_party/libwebrtc/call/degraded_call.cc
+++ third_party/libwebrtc/call/degraded_call.cc
@@ -16,7 +16,7 @@
 #include "absl/strings/string_view.h"
 #include "api/sequence_checker.h"
 #include "modules/rtp_rtcp/source/rtp_util.h"
-#include "rtc_base/event.h"
+#include "rtc_base/thread.h"
 
 namespace webrtc {
 
@@ -174,13 +174,10 @@ DegradedCall::~DegradedCall() {
   
   
   
-  rtc::Event event;
-  call_->network_thread()->PostTask(
-      [flag = std::move(call_alive_), &event]() mutable {
-        flag->SetNotAlive();
-        event.Set();
-      });
-  event.Wait(rtc::Event::kForever);
+  
+  static_cast<rtc::Thread*>(call_->network_thread())
+      ->BlockingCall(
+          [flag = std::move(call_alive_)]() mutable { flag->SetNotAlive(); });
 }
 
 AudioSendStream* DegradedCall::CreateAudioSendStream(
