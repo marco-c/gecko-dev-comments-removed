71047ab24dfe9e4777f7ace1750b62b778d2a771
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-09-15 23:30:40 +0000

    Bug 1839214 [wpt PR 40625] - [no-vary-search-hint] Add WPT internal test for NVS hint non-eager, a=testonly
    Automatic update from web-platform-tests
    [no-vary-search-hint] Add WPT internal test for NVS hint non-eager
    
    Add WPT internal test for No-Vary-Search non eager trigger
    functionality.
    
    Use eventSender for now but migrate to test_driver once
    crbug.com/1453780 is fixed.
    
    Note: Chrome implementation of `test_driver`, which uses `chrome.gpuBenchmarking.pointerActionSequence`, does not work in a new window opened with `window.open`. I had to use `eventSender` for this scenario and ignored the `eventSender` deprecation message in the presubmit hook.
    
    Bug: 1416659
    Change-Id: Ia9f92fe1becbe6de765bd408fd1599f442cc7e39
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4626898
    Commit-Queue: Liviu Tinta <liviutintachromium.org>
    Reviewed-by: Weizhong Xia <weizhonggoogle.com>
    Reviewed-by: Kevin McNee <mcneechromium.org>
    Cr-Commit-Position: refs/heads/main{#1170766}
    
    --
    
    wpt-commits: ccf5f1b8f92b7008f900b5105ad48673d80fa421
    wpt-pr: 40625
    
    UltraBlame original commit: 3b228f27c5a0ae1752fcd767572b2caad26aa676

--- testing/web-platform/tests/speculation-rules/prefetch/resources/utils.sub.js
+++ testing/web-platform/tests/speculation-rules/prefetch/resources/utils.sub.js
@@ -41,10 +41,13 @@ class PrefetchAgent extends RemoteContext {
   
   
   
-  async forceSinglePrefetch(url, extra = {}) {
+  async forceSinglePrefetch(url, extra = {}, wait_for_completion = true) {
     await this.execute_script((url, extra) => {
       insertSpeculationRules({ prefetch: [{source: 'list', urls: [url], ...extra}] });
     }, [url, extra]);
+    if (!wait_for_completion) {
+      return Promise.resolve();
+    }
     return new Promise(resolve => this.t.step_timeout(resolve, 2000));
   }
 
@@ -129,11 +132,17 @@ async function isUrlPrefetched(url) {
 }
 
 
-async function spawnWindow(t, options = {}, uuid = token()) {
+async function spawnWindowWithReference(t, options = {}, uuid = token()) {
   let agent = new PrefetchAgent(uuid, t);
   let w = window.open(agent.getExecutorURL(options), '_blank', options);
   t.add_cleanup(() => w.close());
-  return agent;
+  return {"agent":agent, "window":w};
+}
+
+
+async function spawnWindow(t, options = {}, uuid = token()) {
+  let agent_window_pair = await spawnWindowWithReference(t, options, uuid);
+  return agent_window_pair.agent;
 }
 
 function insertSpeculationRules(body) {
@@ -175,4 +184,12 @@ function assert_prefetched (requestHeaders, description) {
 function assert_not_prefetched (requestHeaders, description){
   assert_equals(requestHeaders.purpose, "", description);
   assert_equals(requestHeaders.sec_purpose, "", description);
+}
+
+
+
+function addNoVarySearchHeaderUsingQueryParam(url, value){
+  if(value){
+    url.searchParams.append("nvs_header", value);
+  }
 }
