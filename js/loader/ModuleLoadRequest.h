51675a233eb83587163231ca4c8bf8f609025b20
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-09-15 23:34:33 +0000

    Bug 1842798 - Part 2: Remove use of MozPromise to wait for module imports to load r=smaug
    This replaces the use of a promise to wait for all imports of a module to load
    with a counter of reminaing imports in the parent and a pointer to the parent
    that is waiting in the child. The parent request is updated immediately rather
    than by dispatching a runnable.
    
    Differential Revision: https://phabricator.services.mozilla.com/D183273
    
    UltraBlame original commit: 507da9d36f91be4bfda8890df86f52b26a0fa176

--- js/loader/ModuleLoadRequest.h
+++ js/loader/ModuleLoadRequest.h
@@ -11,7 +11,6 @@
 #include "ScriptLoadRequest.h"
 #include "ModuleLoaderBase.h"
 #include "mozilla/Assertions.h"
-#include "mozilla/MozPromise.h"
 #include "js/RootingAPI.h"
 #include "js/Value.h"
 #include "nsURIHashKey.h"
@@ -36,7 +35,10 @@ class VisitedURLSet : public nsTHashtable<nsURIHashKey> {
 
 
 class ModuleLoadRequest final : public ScriptLoadRequest {
-  ~ModuleLoadRequest() = default;
+  ~ModuleLoadRequest() {
+    MOZ_ASSERT(!mWaitingParentRequest);
+    MOZ_ASSERT(mAwaitingImports == 0);
+  }
 
   ModuleLoadRequest(const ModuleLoadRequest& aOther) = delete;
   ModuleLoadRequest(ModuleLoadRequest&& aOther) = delete;
@@ -46,10 +48,6 @@ class ModuleLoadRequest final : public ScriptLoadRequest {
   NS_DECL_CYCLE_COLLECTION_SCRIPT_HOLDER_CLASS_INHERITED(ModuleLoadRequest,
                                                          ScriptLoadRequest)
   using SRIMetadata = mozilla::dom::SRIMetadata;
-
-  template <typename T>
-  using MozPromiseHolder = mozilla::MozPromiseHolder<T>;
-  using GenericPromise = mozilla::GenericPromise;
 
   ModuleLoadRequest(nsIURI* aURI, ScriptFetchOptions* aFetchOptions,
                     const SRIMetadata& aIntegrity, nsIURI* aReferrer,
@@ -114,6 +112,8 @@ class ModuleLoadRequest final : public ScriptLoadRequest {
   void StartDynamicImport() { mLoader->StartDynamicImport(this); }
   void ProcessDynamicImport() { mLoader->ProcessDynamicImport(this); }
 
+  void ChildLoadComplete(bool aSuccess);
+
  private:
   void LoadFinished();
   void CancelImports();
@@ -147,12 +147,15 @@ class ModuleLoadRequest final : public ScriptLoadRequest {
   RefPtr<ModuleScript> mModuleScript;
 
   
-  
-  
-  MozPromiseHolder<GenericPromise> mReady;
+  nsTArray<RefPtr<ModuleLoadRequest>> mImports;
 
   
-  nsTArray<RefPtr<ModuleLoadRequest>> mImports;
+  
+  RefPtr<ModuleLoadRequest> mWaitingParentRequest;
+
+  
+  
+  size_t mAwaitingImports = 0;
 
   
   
