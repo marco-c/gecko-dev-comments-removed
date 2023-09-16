c5dba50b29b1be3b23f6bca3e7a93b1f6ce5023e
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-09-16 01:06:24 +0000

    Backed out 2 changesets (bug 1848491) for causing reftest failures in unicode-lang.html CLOSED TREE
    Backed out changeset cc1e56d6f6ab (bug 1848491)
    Backed out changeset c2e6e49bdeef (bug 1848491)
    
    UltraBlame original commit: 92ae76d39a79168d16b39e2efe7f23dd8b6bdb74

--- layout/style/nsStyleUtil.cpp
+++ layout/style/nsStyleUtil.cpp
@@ -82,78 +82,32 @@ bool nsStyleUtil::LangTagCompare(const nsACString& aAttributeValue,
     return false;
   }
 
-  int32_t attrPriv = -1;
   AutoLangId attrLangId(aAttributeValue);
   if (!attrLangId.IsValid()) {
-    
-    
-    attrPriv = aAttributeValue.LowerCaseFindASCII("-x-");
-    if (attrPriv >= 0) {
-      nsAutoCString temp(aAttributeValue);
-      temp.Truncate(attrPriv);
-      attrLangId.Reset(temp);
-      if (!attrLangId.IsValid()) {
-        return false;
-      }
-    }
+    return false;
   }
 
-  int32_t selPriv = -1;
   AutoLangId selectorId(aSelectorValue);
   if (!selectorId.IsValid()) {
     
     
     
-    bool wildcard = false;
     if (aSelectorValue[0] == '*') {
-      wildcard = true;
       nsAutoCString temp(aSelectorValue);
       temp.Replace(0, 1, "und");
       selectorId.Reset(temp);
-      if (selectorId.IsValid()) {
-        intl::ffi::unic_langid_clear_language(selectorId);
+      if (!selectorId.IsValid()) {
+        return false;
       }
-    }
-    
-    if (!selectorId.IsValid()) {
-      selPriv = aSelectorValue.LowerCaseFindASCII("-x-");
-      if (selPriv >= 0) {
-        nsAutoCString temp(aSelectorValue);
-        temp.Truncate(selPriv);
-        
-        if (wildcard) {
-          temp.Replace(0, 1, "und");
-        }
-        selectorId.Reset(temp);
-        if (!selectorId.IsValid()) {
-          return false;
-        }
-        if (wildcard) {
-          intl::ffi::unic_langid_clear_language(selectorId);
-        }
-      }
-    }
-  }
-
-  if (!intl::ffi::unic_langid_matches(attrLangId, selectorId,
-                                       false,
-                                       true)) {
-    return false;
-  }
-
-  
-  
-  
-  
-  if (selPriv >= 0) {
-    if (attrPriv < 0) {
+      intl::ffi::unic_langid_clear_language(selectorId);
+    } else {
       return false;
     }
-    return Substring(aAttributeValue, attrPriv)
-        .EqualsIgnoreCase(Substring(aSelectorValue, selPriv));
   }
 
-  return true;
+  return intl::ffi::unic_langid_matches(attrLangId, selectorId,
+                                         false,
+                                         true);
 }
 
 bool nsStyleUtil::ValueIncludes(const nsAString& aValueList,
