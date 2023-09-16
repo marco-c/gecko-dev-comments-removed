93d03e0bd9377b0f072aa94885065c9691e605bb
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-09-16 02:01:13 +0000

    Bug 1847074 - Vendor libwebrtc from 9ffd697a88
    Upstream commit: https://webrtc.googlesource.com/src/+/9ffd697a88862cde59eaf67d2517571c547e4431
        Add killswitch for RTX receive stats
    
        guarding the change in receive stats behind the
          WebRTC-Stats-RtxReceiveStats
        field trial which acts as a killswitch.
    
        BUG=webrtc:15096
    
        Change-Id: I475a2ce4fe4bddd454aa6477f8818384696c007b
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/304160
        Reviewed-by: Henrik Boström <hboswebrtc.org>
        Commit-Queue: Philipp Hancke <phanckemicrosoft.com>
        Reviewed-by: Harald Alvestrand <htawebrtc.org>
        Cr-Commit-Position: refs/heads/main{#40005}
    
    UltraBlame original commit: b724e520671d511a621cb65fe0cc306a40155f9c

--- third_party/libwebrtc/video/video_receive_stream2.cc
+++ third_party/libwebrtc/video/video_receive_stream2.cc
@@ -569,7 +569,10 @@ VideoReceiveStreamInterface::Stats VideoReceiveStream2::GetStats() const {
         rtp_receive_statistics_->GetStatistician(rtx_ssrc());
     if (rtx_statistician) {
       stats.total_bitrate_bps += rtx_statistician->BitrateReceived();
-      stats.rtx_rtp_stats = rtx_statistician->GetStats();
+      
+      if (!call_->trials().IsDisabled("WebRTC-Stats-RtxReceiveStats")) {
+        stats.rtx_rtp_stats = rtx_statistician->GetStats();
+      }
     }
   }
 
