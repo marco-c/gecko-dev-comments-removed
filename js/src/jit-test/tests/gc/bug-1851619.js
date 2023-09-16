cecae43303a5438e764a73a552966963e934246f
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-09-16 03:26:45 +0000

    Bug 1851619 - Do not call ShellCleanupFinalizationRegistryCallback after ShellContext is dead r=jandem
    Differential Revision: https://phabricator.services.mozilla.com/D187947
    
    UltraBlame original commit: 6e93d9434d00eb5f61af581791975943a6eca05b

---
+++ js/src/jit-test/tests/gc/bug-1851619.js
@@ -0,0 +1,12 @@
+
+
+
+
+enableShellAllocationMetadataBuilder();
+var registry = new FinalizationRegistry(()=>{
+});
+registry.register({}, 1, {});
+function recurse(obj) {
+    recurse(obj);
+}
+recurse();
