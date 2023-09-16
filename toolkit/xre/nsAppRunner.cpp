da9ee1d39a43766b6de08aae4dffc5ff155d09b6
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-09-16 03:03:49 +0000

    Bug 1532782 - Stop using plugin-container on regular Unix. r=nika
    Differential Revision: https://phabricator.services.mozilla.com/D187331
    
    UltraBlame original commit: 65081d68166d39d96b11bc5f602ccc466147d9d3

--- toolkit/xre/nsAppRunner.cpp
+++ toolkit/xre/nsAppRunner.cpp
@@ -6144,6 +6144,9 @@ mozilla::BinPathType XRE_GetChildProcBinPathType(
     return BinPathType::PluginContainer;
   }
 
+#ifdef XP_WIN
+  
+  
   switch (aProcessType) {
 #define GECKO_PROCESS_TYPE(enum_value, enum_name, string_name, proc_typename, \
                            process_bin_type, procinfo_typename,               \
@@ -6155,6 +6158,13 @@ mozilla::BinPathType XRE_GetChildProcBinPathType(
     default:
       return BinPathType::PluginContainer;
   }
+#else
+  
+  
+  
+  
+  return BinPathType::Self;
+#endif
 }
 
 
