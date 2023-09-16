cb842bcbed68d573582cdcc36cf88a655e4b092a
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-09-16 02:27:27 +0000

    Bug 1816519 - implement getAuthenticatorData for AuthenticatorAttestationResponse. r=keeler,webidl,emilio,smaug
    Depends on D186806
    
    Differential Revision: https://phabricator.services.mozilla.com/D186374
    
    UltraBlame original commit: 96cdbead4d1557abdf866c6d65d40cfac4995800

--- dom/webauthn/WebAuthnUtil.cpp
+++ dom/webauthn/WebAuthnUtil.cpp
@@ -5,7 +5,6 @@
 
 
 #include "mozilla/dom/WebAuthnUtil.h"
-#include "mozilla/dom/WebAuthnCBORUtil.h"
 #include "nsComponentManagerUtils.h"
 #include "nsICryptoHash.h"
 #include "nsIEffectiveTLDService.h"
@@ -106,29 +105,6 @@ bool EvaluateAppID(nsPIDOMWindowInner* aParent, const nsString& aOrigin,
   }
 
   return false;
-}
-
-nsresult ReadToCryptoBuffer(pkix::Reader& aSrc,  CryptoBuffer& aDest,
-                            uint32_t aLen) {
-  if (aSrc.EnsureLength(aLen) != pkix::Success) {
-    return NS_ERROR_DOM_UNKNOWN_ERR;
-  }
-
-  if (!aDest.SetCapacity(aLen, mozilla::fallible)) {
-    return NS_ERROR_OUT_OF_MEMORY;
-  }
-
-  for (uint32_t offset = 0; offset < aLen; ++offset) {
-    uint8_t b;
-    if (aSrc.Read(b) != pkix::Success) {
-      return NS_ERROR_DOM_UNKNOWN_ERR;
-    }
-    if (!aDest.AppendElement(b, mozilla::fallible)) {
-      return NS_ERROR_OUT_OF_MEMORY;
-    }
-  }
-
-  return NS_OK;
 }
 
 static nsresult HashCString(nsICryptoHash* aHashService, const nsACString& aIn,
