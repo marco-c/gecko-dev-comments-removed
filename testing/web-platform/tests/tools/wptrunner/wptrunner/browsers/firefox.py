2b2bac34eda3155ff504328981762a544c08b854
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-09-16 01:19:32 +0000

    Bug 1847338 [wpt PR 41353] - Bump mozrunner from 8.2.1 to 8.3.0 in /tools, a=testonly
    Automatic update from web-platform-tests
    Bump mozrunner from 8.2.1 to 8.3.0 in /tools
    
    Bumps [mozrunner](https://wiki.mozilla.org/Auto-tools/Projects/Mozbase) from 8.2.1 to 8.3.0.
    
    ---
    updated-dependencies:
    - dependency-name: mozrunner
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <supportgithub.com>
    --
    Work around some changes in mozrunner that assume it's running in mozilla-central
    
    --
    
    wpt-commits: d1b668b991f627db2284003fed2663afa3073d4a, 60085b2b74427e19a65b3e220d4e3a0fdaebc75e
    wpt-pr: 41353
    
    UltraBlame original commit: 894578a5ef518c059a642d2bf0204c6b04f6ade3

--- testing/web-platform/tests/tools/wptrunner/wptrunner/browsers/firefox.py
+++ testing/web-platform/tests/tools/wptrunner/wptrunner/browsers/firefox.py
@@ -5,6 +5,7 @@ import os
 import platform
 import signal
 import subprocess
+import sys
 import tempfile
 import time
 from abc import ABCMeta, abstractmethod
@@ -252,10 +253,21 @@ def log_gecko_crashes(logger, process, test, profile_dir, symbols_path, stackwal
 
 
 def get_environ(logger, binary, debug_info, headless, chaos_mode_flags=None):
-    env = test_environment(xrePath=os.path.abspath(os.path.dirname(binary)),
-                           debugger=debug_info is not None,
-                           useLSan=True,
-                           log=logger)
+    
+    
+    if "bin_suffix" not in mozinfo.info:
+        mozinfo.info["bin_suffix"] = (".exe" if sys.platform in ["win32", "msys", "cygwin"]
+                                      else "")
+
+    
+    
+    env = {key: value for key, value in
+           test_environment(xrePath=os.path.abspath(os.path.dirname(binary)),
+                            debugger=debug_info is not None,
+                            useLSan=True,
+                            log=logger).items()
+           if value is not None}
+
     
     env["MOZ_WINDOW_OCCLUSION"] = "0"
     if chaos_mode_flags is not None:
