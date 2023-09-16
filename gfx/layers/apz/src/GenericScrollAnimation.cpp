676ed9e6ee4b26a726a1c7bb16822fa8ecb3b8a9
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-09-16 00:22:36 +0000

    Backed out 2 changesets (bug 1847689) for causing windows Gtest failures. CLOSED TREE
    Backed out changeset 9fe21761f365 (bug 1847689)
    Backed out changeset 6e9cff07d365 (bug 1847689)
    
    UltraBlame original commit: e6f2508469f04d04626f34ee53d95cdcf6e33796

--- gfx/layers/apz/src/GenericScrollAnimation.cpp
+++ gfx/layers/apz/src/GenericScrollAnimation.cpp
@@ -8,7 +8,6 @@
 
 #include "AsyncPanZoomController.h"
 #include "FrameMetrics.h"
-#include "mozilla/layers/APZPublicUtils.h"
 #include "nsPoint.h"
 #include "ScrollAnimationPhysics.h"
 #include "ScrollAnimationBezierPhysics.h"
@@ -18,9 +17,9 @@
 namespace mozilla {
 namespace layers {
 
-GenericScrollAnimation::GenericScrollAnimation(AsyncPanZoomController& aApzc,
-                                               const nsPoint& aInitialPosition,
-                                               ScrollOrigin aOrigin)
+GenericScrollAnimation::GenericScrollAnimation(
+    AsyncPanZoomController& aApzc, const nsPoint& aInitialPosition,
+    const ScrollAnimationBezierPhysicsSettings& aSettings)
     : mApzc(aApzc), mFinalDestination(aInitialPosition) {
   
   
@@ -29,9 +28,8 @@ GenericScrollAnimation::GenericScrollAnimation(AsyncPanZoomController& aApzc,
       StaticPrefs::general_smoothScroll_msdPhysics_enabled()) {
     mAnimationPhysics = MakeUnique<ScrollAnimationMSDPhysics>(aInitialPosition);
   } else {
-    mAnimationPhysics = MakeUnique<ScrollAnimationBezierPhysics>(
-        aInitialPosition,
-        apz::ComputeBezierAnimationSettingsForOrigin(aOrigin));
+    mAnimationPhysics =
+        MakeUnique<ScrollAnimationBezierPhysics>(aInitialPosition, aSettings);
   }
 }
 
