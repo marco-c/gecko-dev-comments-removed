af9f8be28a74e3df3343abf51c7d11b2a963574c
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-09-16 03:50:52 +0000

    Bug 1849995 [wpt PR 41618] - peerconnection: Add RTCRtpEncodingParameters.codec, a=testonly
    Automatic update from web-platform-tests
    peerconnection: Add RTCRtpEncodingParameters.codec (#41618)
    
    See https://w3c.github.io/webrtc-extensions/#dom-rtcrtpencodingparameters-codec
    The feature is behind the feature flag RTCRtpEncodingParametersCodec.
    
    Bug: 1442194
    
    Change-Id: I949cfa6b9f44e26eff5fd1bc2679e37a0a681954
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4808133
    Auto-Submit: Florent Castelli <orphischromium.org>
    Reviewed-by: Harald Alvestrand <htachromium.org>
    Commit-Queue: Yoav Weiss <yoavweisschromium.org>
    Reviewed-by: Yoav Weiss <yoavweisschromium.org>
    Cr-Commit-Position: refs/heads/main{#1190720}
    
    Co-authored-by: Florent Castelli <orphischromium.org>
    --
    
    wpt-commits: 396bc8e88933cf4b43bf810fe4324d514ec3a497
    wpt-pr: 41618
    
    UltraBlame original commit: 51079ba02c509dda00ab6b818a93b34e41ec905a

--- testing/web-platform/tests/webrtc/RTCPeerConnection-helper.js
+++ testing/web-platform/tests/webrtc/RTCPeerConnection-helper.js
@@ -657,6 +657,13 @@ function preferCodec(transceiver, mimeType, sdpFmtpLine) {
   return transceiver.setCodecPreferences(codecs);
 }
 
+function findSendCodecCapability(mimeType, sdpFmtpLine) {
+  return RTCRtpSender.getCapabilities(mimeType.split('/')[0])
+    .codecs
+    .filter(c => c.mimeType.localeCompare(name, undefined, { sensitivity: 'base' }) === 0
+      && (c.sdpFmtpLine === sdpFmtpLine || !sdpFmtpLine))[0];
+}
+
 
 class UniqueSet extends Set {
   constructor(items) {
