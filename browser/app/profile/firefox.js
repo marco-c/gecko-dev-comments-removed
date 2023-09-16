9c7c9d36474d307c15cb8b5289554aa42e801424
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-09-16 01:12:05 +0000

    Bug 1843261 - Remove browser.tabs.remote.desktopbehavior pref r=emilio
    Differential Revision: https://phabricator.services.mozilla.com/D183490
    
    UltraBlame original commit: 8a5caf74dcfe00dfe080d1c9afa206aea00b9f0a

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -2110,7 +2110,6 @@ pref("privacy.fingerprintingProtection.pbmode", true);
 
 
 pref("browser.tabs.remote.autostart", true);
-pref("browser.tabs.remote.desktopbehavior", true);
 
 
 pref("media.peerconnection.mtransport_process", true);
