62fd1d42b174b04b75712b79ccc25cbaae6c8ae6
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-09-16 02:10:18 +0000

    Bug 1847074 - Vendor libwebrtc from 2abe6e2214
    We already cherry-picked this when we vendored 151be743d4.
    
    Upstream commit: https://webrtc.googlesource.com/src/+/2abe6e2214fa4fcecdb9614715c55a82c0067e25
        [M115] Bail out early if the RTP send module for a SSRC was not found
    
        since it might have been deregistered previously.
    
        BUG=chromium:1454860,chromium:1459124
    
        (cherry picked from commit c0ed83eac258032a2416b7e4200bd074a6455f4b)
    
        Change-Id: I70ba43265361d040e568f83b6400ff8f3c2a8e98
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/311800
        Reviewed-by: Harald Alvestrand <htawebrtc.org>
        Commit-Queue: Philipp Hancke <phanckemicrosoft.com>
        Cr-Original-Commit-Position: refs/heads/main{#40431}
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/312121
        Cr-Commit-Position: refs/branch-heads/5790{#7}
        Cr-Branched-From: 2eacbbc03a4a41ea658661225eb1c8fc07884c33-refs/heads/main{#40122}
    
    UltraBlame original commit: 510340769248a8731a59dc5f62939ebdde6b9bd0

--- third_party/libwebrtc/modules/pacing/packet_router_unittest.cc
+++ third_party/libwebrtc/modules/pacing/packet_router_unittest.cc
@@ -541,18 +541,24 @@ TEST_F(PacketRouterDeathTest, DoubleRegistrationOfReceiveModuleDisallowed) {
   packet_router_.RemoveReceiveRtpModule(&module);
 }
 
-TEST_F(PacketRouterDeathTest, RemovalOfNeverAddedSendModuleDisallowed) {
-  NiceMock<MockRtpRtcpInterface> module;
-
-  EXPECT_DEATH(packet_router_.RemoveSendRtpModule(&module), "");
-}
-
 TEST_F(PacketRouterDeathTest, RemovalOfNeverAddedReceiveModuleDisallowed) {
   NiceMock<MockRtpRtcpInterface> module;
 
   EXPECT_DEATH(packet_router_.RemoveReceiveRtpModule(&module), "");
 }
 #endif  
+
+TEST_F(PacketRouterTest, RemovalOfNeverAddedSendModuleIgnored) {
+  NiceMock<MockRtpRtcpInterface> module;
+  packet_router_.RemoveSendRtpModule(&module);
+}
+
+TEST_F(PacketRouterTest, DuplicateRemovalOfSendModuleIgnored) {
+  NiceMock<MockRtpRtcpInterface> module;
+  packet_router_.AddSendRtpModule(&module, false);
+  packet_router_.RemoveSendRtpModule(&module);
+  packet_router_.RemoveSendRtpModule(&module);
+}
 
 TEST(PacketRouterRembTest, ChangeSendRtpModuleChangeRembSender) {
   rtc::ScopedFakeClock clock;
