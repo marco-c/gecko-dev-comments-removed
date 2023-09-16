41910cc7d7ec4e7e8534ee560a5a55d880b5ff19
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-09-16 04:07:38 +0000

    Bug 1839396 part 17 - Make QuoteString and JSONQuoteString infallible. r=mgaudet
    The previous patch reimplemented QuoteString to use a `StringSegmentRange`,
    which should avoid mutating the inner parts of `JSRope`, while remaining fast on
    `JSAtom`-s.
    
    As the StringSegmentRange does not need to allocate with `ensureLinear`, then
    all the failures are reported by the `put` functions using the `GenericPrinter`
    mechanism, and the `release` functions of `Sprinter` would report the failure if
    any.
    
    Thus, there is no need for retuning a boolean value anymore from `QuoteString`
    and `JSONQuoteString`, while this is still necessary to check the returned value
    variant of `QuoteString` which returns `UniqueChars` by using the `release`
    function of `Sprinter`.
    
    Differential Revision: https://phabricator.services.mozilla.com/D183759
    
    UltraBlame original commit: 9c97403ef422f2afd7025a3f11cdb1c155d5d377

--- js/public/Printer.h
+++ js/public/Printer.h
@@ -337,18 +337,18 @@ extern JS_PUBLIC_API JS::UniqueChars QuoteString(JSContext* cx, JSString* str,
 
 
 
-extern JS_PUBLIC_API bool QuoteString(Sprinter* sp, JSString* str,
+extern JS_PUBLIC_API void QuoteString(Sprinter* sp, JSString* str,
                                       char quote = '\0');
 
 
 
-extern JS_PUBLIC_API bool JSONQuoteString(Sprinter* sp, JSString* str);
+extern JS_PUBLIC_API void JSONQuoteString(Sprinter* sp, JSString* str);
 
 
 enum class QuoteTarget { String, JSON };
 
 template <QuoteTarget target, typename CharT>
-bool JS_PUBLIC_API QuoteString(Sprinter* sp,
+void JS_PUBLIC_API QuoteString(Sprinter* sp,
                                const mozilla::Range<const CharT> chars,
                                char quote = '\0');
 
