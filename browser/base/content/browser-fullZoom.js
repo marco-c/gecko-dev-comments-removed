ea7691589b44d3f56346e5216cb060c4674ad6d6
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-09-16 00:11:55 +0000

    Bug 1847065 - ensure zoom reset and shopping url bar icons check for non-leftclicks before doing something, r=fchasen
    Differential Revision: https://phabricator.services.mozilla.com/D185443
    
    UltraBlame original commit: b19ed5a6579d312e71c03201698107835378c612

--- browser/base/content/browser-fullZoom.js
+++ browser/base/content/browser-fullZoom.js
@@ -482,6 +482,19 @@ var FullZoom = {
     return result;
   },
 
+  
+
+
+
+
+  resetFromURLBar(event) {
+    if (event.button > 0) {
+      return;
+    }
+    this.reset();
+    this.resetScalingZoom();
+  },
+
   resetScalingZoom: function FullZoom_resetScaling(
     browser = gBrowser.selectedBrowser
   ) {
