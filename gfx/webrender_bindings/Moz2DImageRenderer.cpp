9d745866dc04a988f758182a44413c84c5388e7e
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-09-16 02:55:42 +0000

    Bug 1851861: Null-check more calls that attempt to create a DrawTarget. r=gfx-reviewers,lsalzman
    Most of our calls that create a DrawTarget are null-checked. This patch
    fixes up the few that were not already checked.
    
    Differential Revision: https://phabricator.services.mozilla.com/D187586
    
    UltraBlame original commit: 0600aa34323ae0199e68f4c68b7aad6397b6d5ec

--- gfx/webrender_bindings/Moz2DImageRenderer.cpp
+++ gfx/webrender_bindings/Moz2DImageRenderer.cpp
@@ -372,19 +372,22 @@ static bool Moz2DRenderCallback(const Range<const uint8_t> aBlob,
 
   
   
+  IntPoint origin = gfx::IntPoint(aRenderRect->min.x, aRenderRect->min.y);
+
+  dt = gfx::Factory::CreateOffsetDrawTarget(dt, origin);
+  if (!dt) {
+    return false;
+  }
+
+  
+  
   size_t footerSize = sizeof(size_t);
   MOZ_RELEASE_ASSERT(aBlob.length() >= footerSize);
   size_t indexOffset = ConvertFromBytes<size_t>(aBlob.end().get() - footerSize);
 
-  
-  
-  IntPoint origin = gfx::IntPoint(aRenderRect->min.x, aRenderRect->min.y);
-
   MOZ_RELEASE_ASSERT(indexOffset <= aBlob.length() - footerSize);
   Reader reader(aBlob.begin().get() + indexOffset,
                 aBlob.length() - footerSize - indexOffset);
-
-  dt = gfx::Factory::CreateOffsetDrawTarget(dt, origin);
 
   auto bounds = gfx::IntRect(origin, size);
 
