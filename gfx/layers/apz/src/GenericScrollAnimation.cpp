0a158d26a58e1dfd337576c75373ce8f93db553b
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-09-16 00:39:57 +0000

    Bug 1847689 - Have GenericScrollAnimation constructor take a ScrollOrigin parameter instead of ScrollAnimationBezierPhysics. r=dlrobertson
    This allows only computing the Bezier physics in the branch where we
    actually use it.
    
    Differential Revision: https://phabricator.services.mozilla.com/D185635
    
    UltraBlame original commit: f252c38d6ed2436b72d4e5cc3bb704d1b9c7c978

--- gfx/layers/apz/src/GenericScrollAnimation.cpp
+++ gfx/layers/apz/src/GenericScrollAnimation.cpp
@@ -8,6 +8,7 @@
 
 #include "AsyncPanZoomController.h"
 #include "FrameMetrics.h"
+#include "mozilla/layers/APZPublicUtils.h"
 #include "nsPoint.h"
 #include "ScrollAnimationPhysics.h"
 #include "ScrollAnimationBezierPhysics.h"
@@ -17,9 +18,9 @@
 namespace mozilla {
 namespace layers {
 
-GenericScrollAnimation::GenericScrollAnimation(
-    AsyncPanZoomController& aApzc, const nsPoint& aInitialPosition,
-    const ScrollAnimationBezierPhysicsSettings& aSettings)
+GenericScrollAnimation::GenericScrollAnimation(AsyncPanZoomController& aApzc,
+                                               const nsPoint& aInitialPosition,
+                                               ScrollOrigin aOrigin)
     : mApzc(aApzc), mFinalDestination(aInitialPosition) {
   
   
@@ -28,8 +29,9 @@ GenericScrollAnimation::GenericScrollAnimation(
       StaticPrefs::general_smoothScroll_msdPhysics_enabled()) {
     mAnimationPhysics = MakeUnique<ScrollAnimationMSDPhysics>(aInitialPosition);
   } else {
-    mAnimationPhysics =
-        MakeUnique<ScrollAnimationBezierPhysics>(aInitialPosition, aSettings);
+    mAnimationPhysics = MakeUnique<ScrollAnimationBezierPhysics>(
+        aInitialPosition,
+        apz::ComputeBezierAnimationSettingsForOrigin(aOrigin));
   }
 }
 
