a25bbbf4829721c134dc9e64caab575356949363
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-09-16 02:50:20 +0000

    Backed out 3 changesets (bug 1850045) for causing decision task bustages CLOSED TREE
    Backed out changeset b5870683550d (bug 1850045)
    Backed out changeset 4ecd1e0ccff8 (bug 1850045)
    Backed out changeset 74ec20ac8f53 (bug 1850045)
    
    UltraBlame original commit: 42399a5468097d503e4dd0e3d3769c67529e7055

--- taskcluster/gecko_taskgraph/transforms/job/mach.py
+++ taskcluster/gecko_taskgraph/transforms/job/mach.py
@@ -27,6 +27,10 @@ mach_schema = Schema(
         Required("comm-checkout"): bool,
         
         Optional("workdir"): str,
+        
+        
+        
+        Optional("command-context"): dict,
     }
 )
 
