074b91d56bfe66894ef12d3805db05e08c261988
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-09-16 00:40:27 +0000

    Bug 1776831 - [devtools] Refresh rules view on STYLESHEET resource available. r=jdescottes,devtools-reviewers.
    Differential Revision: https://phabricator.services.mozilla.com/D185393
    
    UltraBlame original commit: c306b234b8caf703ceefa3ad3e86683ef5261f9c

--- devtools/client/inspector/test/head.js
+++ devtools/client/inspector/test/head.js
@@ -1008,9 +1008,14 @@ async function assertTooltipHiddenOnMouseOut(tooltip, target) {
 
 
 function getRuleViewRuleEditor(view, childrenIndex, nodeIndex) {
+  const child = view.element.children[childrenIndex];
+  if (!child) {
+    return null;
+  }
+
   return nodeIndex !== undefined
-    ? view.element.children[childrenIndex].childNodes[nodeIndex]._ruleEditor
-    : view.element.children[childrenIndex]._ruleEditor;
+    ? child.childNodes[nodeIndex]?._ruleEditor
+    : child._ruleEditor;
 }
 
 
