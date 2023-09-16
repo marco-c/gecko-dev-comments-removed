e7c786e37c180fc9a1f0b53c8be07bd6bdca467b
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-09-16 01:56:17 +0000

    Bug 1850293 - Make INCREMENT_SCRIPT_LEVEL state non intrinsic. r=smaug
    Differential Revision: https://phabricator.services.mozilla.com/D186894
    
    UltraBlame original commit: a9ca65a951fc00477b604d8e7e868e8b693977f0

--- dom/mathml/MathMLElement.h
+++ dom/mathml/MathMLElement.h
@@ -69,7 +69,9 @@ class MathMLElement final : public MathMLElementBase, public Link {
   
   
   void SetIncrementScriptLevel(bool aIncrementScriptLevel, bool aNotify);
-  bool GetIncrementScriptLevel() const { return mIncrementScriptLevel; }
+  bool GetIncrementScriptLevel() const {
+    return Element::State().HasState(ElementState::INCREMENT_SCRIPT_LEVEL);
+  }
 
   int32_t TabIndexDefault() final;
 
@@ -102,9 +104,6 @@ class MathMLElement final : public MathMLElementBase, public Link {
   void AfterSetAttr(int32_t aNameSpaceID, nsAtom* aName,
                     const nsAttrValue* aValue, const nsAttrValue* aOldValue,
                     nsIPrincipal* aSubjectPrincipal, bool aNotify) override;
-
- private:
-  bool mIncrementScriptLevel;
 };
 
 }  
