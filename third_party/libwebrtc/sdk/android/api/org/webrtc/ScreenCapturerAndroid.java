770f8522357ca56dcb5879015e1871e09ea422f4
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-09-16 02:07:00 +0000

    Bug 1847074 - Vendor libwebrtc from 510890ba19
    Upstream commit: https://webrtc.googlesource.com/src/+/510890ba19e7e02986a0f57a7d6067e2e30284ce
        Revert "Avoid recreating VirtualDisplay on format changes."
    
        This reverts commit fcd1dfad1f855346a1fb6741322ff48c61601df7.
    
        Reason for revert: Breaks downstream test.
    
        Original change's description:
        > Avoid recreating VirtualDisplay on format changes.
        >
        > Recreating the VirtualDisplay will require new user permission dialog,
        > so resize instead when possible.
        >
        > Bug: b/281978124
        > Change-Id: I3b6939720897c038c9e598433372342cf72e001e
        > Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/305560
        > Reviewed-by: Xavier Lepaul‎ <xalepwebrtc.org>
        > Commit-Queue: Linus Nilsson <lnilssonwebrtc.org>
        > Cr-Commit-Position: refs/heads/main{#40084}
    
        Bug: b/281978124, b/283063961
        Change-Id: I8ec2ba3321be225a673af2a6192819a8a1b79b2c
        No-Presubmit: true
        No-Tree-Checks: true
        No-Try: true
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/305641
        Owners-Override: Mirko Bonadei <mbonadeiwebrtc.org>
        Commit-Queue: Mirko Bonadei <mbonadeiwebrtc.org>
        Bot-Commit: rubber-stamperappspot.gserviceaccount.com <rubber-stamperappspot.gserviceaccount.com>
        Cr-Commit-Position: refs/heads/main{#40085}
    
    UltraBlame original commit: bddf1e3125073737fc3614e0cb77b19ad0ac38aa

--- third_party/libwebrtc/sdk/android/api/org/webrtc/ScreenCapturerAndroid.java
+++ third_party/libwebrtc/sdk/android/api/org/webrtc/ScreenCapturerAndroid.java
@@ -17,8 +17,6 @@ import android.hardware.display.DisplayManager;
 import android.hardware.display.VirtualDisplay;
 import android.media.projection.MediaProjection;
 import android.media.projection.MediaProjectionManager;
-import android.os.Build.VERSION;
-import android.os.Build.VERSION_CODES;
 import android.view.Surface;
 import androidx.annotation.Nullable;
 
@@ -115,7 +113,7 @@ public class ScreenCapturerAndroid implements VideoCapturer, VideoSink {
     
     mediaProjection.registerCallback(mediaProjectionCallback, surfaceTextureHelper.getHandler());
 
-    updateVirtualDisplay();
+    createVirtualDisplay();
     capturerObserver.onCapturerStarted(true);
     surfaceTextureHelper.startListening(ScreenCapturerAndroid.this);
   }
@@ -180,26 +178,17 @@ public class ScreenCapturerAndroid implements VideoCapturer, VideoSink {
     
     
     
-    ThreadUtils.invokeAtFrontUninterruptibly(
-        surfaceTextureHelper.getHandler(), this::updateVirtualDisplay);
+    ThreadUtils.invokeAtFrontUninterruptibly(surfaceTextureHelper.getHandler(), new Runnable() {
+      @Override
+      public void run() {
+        virtualDisplay.release();
+        createVirtualDisplay();
+      }
+    });
   }
 
-  private void updateVirtualDisplay() {
-    surfaceTextureHelper.setTextureSize(width, height);
-    
-    
-    
-    if (virtualDisplay == null || VERSION.SDK_INT < VERSION_CODES.S) {
-      createVirtualDisplay();
-    } else {
-      virtualDisplay.resize(width, height, VIRTUAL_DISPLAY_DPI);
-      virtualDisplay.setSurface(new Surface(surfaceTextureHelper.getSurfaceTexture()));
-    }
-  }
   private void createVirtualDisplay() {
-    if (virtualDisplay != null) {
-      virtualDisplay.release();
-    }
+    surfaceTextureHelper.setTextureSize(width, height);
     virtualDisplay = mediaProjection.createVirtualDisplay("WebRTC_ScreenCapture", width, height,
         VIRTUAL_DISPLAY_DPI, DISPLAY_FLAGS, new Surface(surfaceTextureHelper.getSurfaceTexture()),
         null , null );
