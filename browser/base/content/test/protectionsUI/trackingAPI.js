f41f00b91cdf6df8b41d5afff6f27d29e16d4a8f
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-09-16 00:13:34 +0000

    Bug 1840902 - Adjusted browser chrome tests under the Anti-tracking test suite to support only secure context storage access. r=bvandersloot
    Depends on D182366
    
    Differential Revision: https://phabricator.services.mozilla.com/D184979
    
    UltraBlame original commit: a8267c9267daac307ec0b06882b52db6d8d2fb09

--- browser/base/content/test/protectionsUI/trackingAPI.js
+++ browser/base/content/test/protectionsUI/trackingAPI.js
@@ -58,7 +58,7 @@ onmessage = event => {
     case "image":
       createImage(
         
-        "http://trackertest.org/browser/browser/base/content/test/protectionsUI/cookieServer.sjs?type=image-no-cookie"
+        "https://trackertest.org/browser/browser/base/content/test/protectionsUI/cookieServer.sjs?type=image-no-cookie"
       );
       break;
     case "window-open":
--- toolkit/components/antitracking/test/browser/browser_partitionedLockManager.js
+++ toolkit/components/antitracking/test/browser/browser_partitionedLockManager.js
@@ -18,7 +18,13 @@ PartitionedStorageHelper.runTest(
 
     is(locks.length, 2, "We should have granted 2 lock requests at this point");
   },
-   undefined,
+  async _ => {
+    await new Promise(resolve => {
+      Services.clearData.deleteData(Ci.nsIClearDataService.CLEAR_ALL, value =>
+        resolve()
+      );
+    });
+  },
    undefined,
   { runInSecureContext: true }
 );
