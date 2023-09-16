6514c296f6da59f72acafe3cf4dc2b85617a8675
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-09-16 00:58:20 +0000

    Bug 1848675 - Add pref for ads in shopping sidebar. r=shopping-reviewers,kpatenio
    Differential Revision: https://phabricator.services.mozilla.com/D186155
    
    UltraBlame original commit: 77f0af912beeab0907bfece96b6d03642dfc5c40

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -737,6 +737,10 @@ pref("browser.shopping.experience2023.optedIn", 0);
 pref("browser.shopping.experience2023.active", true);
 
 
+
+pref("browser.shopping.experience2023.ads.enabled", true);
+
+
 pref("browser.privatebrowsing.vpnpromourl", "https://vpn.mozilla.org/?utm_source=firefox-browser&utm_medium=firefox-%CHANNEL%-browser&utm_campaign=private-browsing-vpn-link");
 
 
