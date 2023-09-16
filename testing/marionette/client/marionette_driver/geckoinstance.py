0c5ba7a3fb08e2656e86e0ba76cfbee85d1e7c24
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-09-16 00:57:19 +0000

    Backed out changeset 181edf360c56 (bug 1847787) for bc failure on browser_newtab_ping.js
    UltraBlame original commit: a4d47be53b99d0b267deb3ee478b18b44848b7a4

--- testing/marionette/client/marionette_driver/geckoinstance.py
+++ testing/marionette/client/marionette_driver/geckoinstance.py
@@ -42,8 +42,6 @@ class GeckoInstance(object):
         
         "apz.content_response_timeout": 60000,
         
-        "browser.newtabpage.activity-stream.showSponsoredTopSites": False,
-        
         "browser.region.network.url": "",
         
         "browser.topsites.contile.enabled": False,
--- testing/profiles/common/user.js
+++ testing/profiles/common/user.js
@@ -78,8 +78,6 @@ user_pref("browser.tabs.unloadOnLowMemory", false);
 
 user_pref("browser.topsites.contile.enabled", false);
 
-user_pref("browser.newtabpage.activity-stream.showSponsoredTopSites", false);
-
 
 user_pref("telemetry.fog.test.localhost_port", -1);
 
