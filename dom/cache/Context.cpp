32e382f639de8057c683c8f3898920bf6204d27c
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-09-21 16:57:28 +0000

    Bug 1749504 - Convert Cache API to use the new way to access a client directory; r=dom-storage-reviewers,jstutte
    Cache API  currently creates a client directory lock first and then when the
    client directorylock is acquired, it bounces to the QuotaManager I/O thread
    where it ensures that storage is initialized. This can be now replaced by just
    calling QuotaManager::OpenClientDirectory.
    
    Changes done in this patch:
    - replaced QuotaManager::CreateDirectoryLock call with
      QuotaManager::OpenClientDirectory
    - removed EnsureStorageIsInitializedInternal call from
      Context::QuotaInitRunnable::Run
    - changed Context::QuotaInitRunnable to not inherit from OpenDirectoryListener
    
    Differential Revision: https://phabricator.services.mozilla.com/D186119
    
    UltraBlame original commit: 745d8130d1ac2d3e0ae51f2f22896853f0d34479

--- dom/cache/Context.cpp
+++ dom/cache/Context.cpp
@@ -49,7 +49,6 @@ namespace mozilla::dom::cache {
 
 using mozilla::dom::quota::AssertIsOnIOThread;
 using mozilla::dom::quota::DirectoryLock;
-using mozilla::dom::quota::OpenDirectoryListener;
 using mozilla::dom::quota::PERSISTENCE_TYPE_DEFAULT;
 using mozilla::dom::quota::PersistenceType;
 using mozilla::dom::quota::QuotaManager;
@@ -92,8 +91,7 @@ class Context::Data final : public Action::Data {
 
 
 
-class Context::QuotaInitRunnable final : public nsIRunnable,
-                                         public OpenDirectoryListener {
+class Context::QuotaInitRunnable final : public nsIRunnable {
  public:
   QuotaInitRunnable(SafeRefPtr<Context> aContext, SafeRefPtr<Manager> aManager,
                     Data* aData, nsISerialEventTarget* aTarget,
@@ -142,10 +140,9 @@ class Context::QuotaInitRunnable final : public nsIRunnable,
     mInitAction->CancelOnInitiatingThread();
   }
 
-  
-  virtual void DirectoryLockAcquired(DirectoryLock* aLock) override;
+  void DirectoryLockAcquired(DirectoryLock* aLock);
 
-  virtual void DirectoryLockFailed() override;
+  void DirectoryLockFailed();
 
  private:
   class SyncResolver final : public Action::Resolver {
@@ -369,16 +366,21 @@ Context::QuotaInitRunnable::Run() {
 
       mDirectoryMetadata.emplace(std::move(principalMetadata));
 
-      
-      RefPtr<DirectoryLock> directoryLock = quotaManager->CreateDirectoryLock(
-          {*mDirectoryMetadata, quota::Client::DOMCACHE},
-           false);
-
-      
-      
-      
       mState = STATE_WAIT_FOR_DIRECTORY_LOCK;
-      directoryLock->Acquire(this);
+
+      quotaManager
+          ->OpenClientDirectory({*mDirectoryMetadata, quota::Client::DOMCACHE})
+          ->Then(
+              GetCurrentSerialEventTarget(), __func__,
+              [self = RefPtr(this)](
+                  const quota::ClientDirectoryLockPromise::ResolveOrRejectValue&
+                      aValue) {
+                if (aValue.IsResolve()) {
+                  self->DirectoryLockAcquired(aValue.ResolveValue());
+                } else {
+                  self->DirectoryLockFailed();
+                }
+              });
 
       break;
     }
@@ -393,9 +395,6 @@ Context::QuotaInitRunnable::Run() {
 
         QuotaManager* quotaManager = QuotaManager::Get();
         MOZ_DIAGNOSTIC_ASSERT(quotaManager);
-
-        QM_TRY(
-            MOZ_TO_RESULT(quotaManager->EnsureStorageIsInitializedInternal()));
 
         QM_TRY(
             MOZ_TO_RESULT(quotaManager->EnsureTemporaryStorageIsInitialized()));
