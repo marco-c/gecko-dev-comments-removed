43dba6976b57ef3301f92fab41ce9b51d21e0eb4
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-09-21 16:59:26 +0000

    Bug 1851962 - Remove dom.forms.autocapitalize pref r=emilio,geckoview-reviewers,m_kato
    Differential Revision: https://phabricator.services.mozilla.com/D188367
    
    UltraBlame original commit: 0613453268c272ac9a3cd69160dcdc9dbaad6bfc

--- mobile/android/geckoview/src/androidTest/java/org/mozilla/geckoview/test/TextInputDelegateTest.kt
+++ mobile/android/geckoview/src/androidTest/java/org/mozilla/geckoview/test/TextInputDelegateTest.kt
@@ -1218,8 +1218,6 @@ class TextInputDelegateTest : BaseSessionTest() {
         
         assumeThat("Not in designmode", id, not(equalTo("#designmode")))
 
-        sessionRule.setPrefsUntilTestEnd(mapOf("dom.forms.autocapitalize" to true))
-
         mainSession.textInput.view = View(InstrumentationRegistry.getInstrumentation().targetContext)
 
         mainSession.loadTestPath(INPUTS_PATH)
