8723c88b528f4fbec936ca41b1701e9961f48164
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-09-21 17:13:07 +0000

    Bug 1853427 - Remove subsuite argument when invoked via mach test, r=Sasha
    Unfortunately `mach test` makes unverified assumptions about the top
    level kwargs of all test suites. This broke wpt because it was passing
    around a `subsuite` argument that was previously ignored, but recently
    caused a conflict in functions with signatures like
    function(subsuite, **kwargs).
    
    Fow now just remove the argument in the part of the wpt frontend that
    handles data expected to be from `mach test`.
    
    Differential Revision: https://phabricator.services.mozilla.com/D188446
    
    UltraBlame original commit: be2a2dbdf656280676772a4b4a5b5cbf21b9c4a6

--- testing/web-platform/mach_commands.py
+++ testing/web-platform/mach_commands.py
@@ -502,6 +502,9 @@ def run_web_platform_tests(command_context, **params):
             params["test_types"] = list(test_types)
         params["include"] = include
         del params["test_objects"]
+        
+        if "subsuite" in params:
+            del params["subsuite"]
     if params.get("debugger", None):
         import mozdebug
 
