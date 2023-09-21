5fa7b2deeffd59aa44345db3e96b6c89be800c27
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-09-21 17:21:08 +0000

    Bug 1848783, part 3 - Add the site (sans port) to the permission keys of a content process -  r=anti-tracking-reviewers,timhuang
    without this, permissions set in the main process that are site-scoped on origins with non-null ports are not pushed out to the content process.
    
    Differential Revision: https://phabricator.services.mozilla.com/D186984
    
    UltraBlame original commit: b58e27b64cb074766daf2b15b370fac4900e282b

--- dom/ipc/ContentParent.cpp
+++ dom/ipc/ContentParent.cpp
@@ -6468,6 +6468,16 @@ nsresult ContentParent::TransmitPermissionsForPrincipal(
     EnsurePermissionsByKey(pair.first, pair.second);
   }
 
+  
+  
+  
+  nsAutoCString siteKey;
+  nsresult rv =
+      PermissionManager::GetKeyForPrincipal(aPrincipal, false, true, siteKey);
+  if (NS_SUCCEEDED(rv) && !siteKey.IsEmpty()) {
+    mActiveSecondaryPermissionKeys.EnsureInserted(siteKey);
+  }
+
   return NS_OK;
 }
 
@@ -6560,6 +6570,11 @@ void ContentParent::EnsurePermissionsByKey(const nsACString& aKey,
 bool ContentParent::NeedsPermissionsUpdate(
     const nsACString& aPermissionKey) const {
   return mActivePermissionKeys.Contains(aPermissionKey);
+}
+
+bool ContentParent::NeedsSecondaryKeyPermissionsUpdate(
+    const nsACString& aPermissionKey) const {
+  return mActiveSecondaryPermissionKeys.Contains(aPermissionKey);
 }
 
 mozilla::ipc::IPCResult ContentParent::RecvAccumulateChildHistograms(
