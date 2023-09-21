4caed3c620ac84b5795e1b56a47e72620bf74faa
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-09-21 17:16:15 +0000

    Bug 1839396 part 19 - Add documentation to GenericPrinter classes. r=mgaudet
    Differential Revision: https://phabricator.services.mozilla.com/D183761
    
    UltraBlame original commit: 771c47934e728b9233301b1c88b5fe89ce9debe1

--- js/public/Printer.h
+++ js/public/Printer.h
@@ -18,6 +18,111 @@
 #include "js/TypeDecls.h"
 #include "js/Utility.h"
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 namespace js {
 
 class LifoAlloc;
@@ -41,10 +146,19 @@ class JS_PUBLIC_API GenericPrinter {
   
   virtual void put(const char* s, size_t len) = 0;
   inline void put(const char* s) { put(s, strlen(s)); }
-  virtual void put(mozilla::Span<const JS::Latin1Char> str);
+
   
+  
+  
+  
+  
+  virtual void put(mozilla::Span<const JS::Latin1Char> str);
   virtual void put(mozilla::Span<const char16_t> str);
 
+  
+  
+  
+  
   virtual inline void putChar(const char c) { put(&c, 1); }
   virtual inline void putChar(const JS::Latin1Char c) { putChar(char(c)); }
   virtual inline void putChar(const char16_t c) {
@@ -57,10 +171,29 @@ class JS_PUBLIC_API GenericPrinter {
   void printf(const char* fmt, ...) MOZ_FORMAT_PRINTF(2, 3);
   void vprintf(const char* fmt, va_list ap) MOZ_FORMAT_PRINTF(2, 0);
 
+  
+  
+  
+  
+  
+  
+  
+  
   virtual bool canPutFromIndex() const { return false; }
+
+  
+  
   virtual void putFromIndex(size_t index, size_t length) {
     MOZ_CRASH("Calls to putFromIndex should be guarded by canPutFromIndex.");
   }
+
+  
+  
+  
+  
+  
+  
+  
   virtual size_t index() const { return 0; }
 
   
