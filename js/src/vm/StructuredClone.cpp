972b86aaef5be59834d255ebc00892b38e424ee9
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-09-21 16:59:01 +0000

    Bug 1853305 - Part 3: Use AutoSelectGCHeap for structured clone deserialization r=sfink
    Differential Revision: https://phabricator.services.mozilla.com/D188335
    
    UltraBlame original commit: a1a185be5df0570f801f13814343baa74bd336a7

--- js/src/vm/StructuredClone.cpp
+++ js/src/vm/StructuredClone.cpp
@@ -43,6 +43,7 @@
 
 #include "builtin/DataViewObject.h"
 #include "builtin/MapObject.h"
+#include "gc/GC.h"           
 #include "js/Array.h"        
 #include "js/ArrayBuffer.h"  
 #include "js/ColumnNumber.h"  
@@ -422,7 +423,6 @@ struct JSStructuredCloneReader {
                                    const JS::CloneDataPolicy& cloneDataPolicy,
                                    const JSStructuredCloneCallbacks* cb,
                                    void* cbClosure);
-  ~JSStructuredCloneReader();
 
   SCInput& input() { return in; }
   bool read(MutableHandleValue vp, size_t nbytes);
@@ -481,10 +481,6 @@ struct JSStructuredCloneReader {
       MutableHandleValue vp,
       ShouldAtomizeStrings atomizeStrings = DontAtomizeStrings);
 
-  static void NurseryCollectionCallback(JSContext* cx,
-                                        JS::GCNurseryProgress progress,
-                                        JS::GCReason reason, void* data);
-
   SCInput& in;
 
   
@@ -538,7 +534,7 @@ struct JSStructuredCloneReader {
   
   
   
-  gc::Heap gcHeap = gc::Heap::Default;
+  AutoSelectGCHeap gcHeap;
 
   friend bool JS_ReadString(JSStructuredCloneReader* r,
                             JS::MutableHandleString str);
@@ -2457,31 +2453,12 @@ JSStructuredCloneReader::JSStructuredCloneReader(
       allObjs(in.context()),
       numItemsRead(0),
       callbacks(cb),
-      closure(cbClosure) {
+      closure(cbClosure),
+      gcHeap(in.context()) {
   
   
   
   MOZ_ALWAYS_TRUE(objState.append(std::make_pair(nullptr, true)));
-
-  JS::AddGCNurseryCollectionCallback(in.context(), &NurseryCollectionCallback,
-                                     this);
-}
-
-JSStructuredCloneReader::~JSStructuredCloneReader() {
-  JS::RemoveGCNurseryCollectionCallback(in.context(),
-                                        &NurseryCollectionCallback, this);
-}
-
-
-void JSStructuredCloneReader::NurseryCollectionCallback(
-    JSContext* cx, JS::GCNurseryProgress progress, JS::GCReason reason,
-    void* data) {
-  auto* reader = static_cast<JSStructuredCloneReader*>(data);
-
-  
-  if (progress == JS::GCNurseryProgress::GC_NURSERY_COLLECTION_END) {
-    reader->gcHeap = gc::Heap::Tenured;
-  }
 }
 
 template <typename CharT>
