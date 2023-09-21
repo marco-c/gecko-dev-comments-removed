261602b920e48f681ada9b8063508f43ae9f362b
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-09-21 17:13:46 +0000

    Bug 1847660 - Integrate the GMP process with the Windows DLL blocklisting. r=media-playback-reviewers,alwu
    Differential Revision: https://phabricator.services.mozilla.com/D187714
    
    UltraBlame original commit: af963517d1d2577f7afee86bf9daf0ac3df02eaf

--- toolkit/xre/dllservices/UntrustedModulesProcessor.cpp
+++ toolkit/xre/dllservices/UntrustedModulesProcessor.cpp
@@ -8,6 +8,8 @@
 
 #include <windows.h>
 
+#include "GMPPlatform.h"
+#include "GMPServiceParent.h"
 #include "mozilla/CmdLineAndEnvUtils.h"
 #include "mozilla/DebugOnly.h"
 #include "mozilla/dom/ContentChild.h"
@@ -81,6 +83,8 @@ bool UntrustedModulesProcessor::IsSupportedProcessType() {
       return Telemetry::CanRecordReleaseData();
     case GeckoProcessType_RDD:
     case GeckoProcessType_Utility:
+    case GeckoProcessType_GMPlugin:
+      
       
       
       
@@ -196,6 +200,10 @@ NS_IMETHODIMP UntrustedModulesProcessor::Observe(nsISupports* aSubject,
         if (auto* proc = rddMgr->GetRDDChild()) {
           Unused << proc->SendUnblockUntrustedModulesThread();
         }
+      }
+      if (RefPtr<gmp::GeckoMediaPluginServiceParent> gmps =
+              gmp::GeckoMediaPluginServiceParent::GetSingleton()) {
+        gmps->SendUnblockUntrustedModulesThread();
       }
     }
 
@@ -758,6 +766,10 @@ UntrustedModulesProcessor::SendGetModulesTrust(ModulePaths&& aModules,
       return ::mozilla::SendGetModulesTrust(
           ipc::UtilityProcessChild::GetSingleton().get(), std::move(aModules),
           runNormal);
+    }
+    case GeckoProcessType_GMPlugin: {
+      return ::mozilla::gmp::SendGetModulesTrust(std::move(aModules),
+                                                 runNormal);
     }
     default: {
       MOZ_ASSERT_UNREACHABLE("Unsupported process type");
