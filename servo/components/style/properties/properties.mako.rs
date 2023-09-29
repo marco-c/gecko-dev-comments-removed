5703ecd1c94f75e562ea1118fdb2ffe4cbffa92d
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-09-29 22:24:18 +0000

    Bug 1840478 - Introduce ComputedCustomProperties struct to handle inherited/non-inherited properties. r=emilio
    The CSS Properties and Values API allows to register CSS properties that
    can be either inherited or non-inherited, and which can also have
    initial values specified.
    
    Stylo currently implements efficient inheritance of classical CSS
    variables (inherited, guaranteed-invalid initial value) by using
    a ref-counted CustomPropertiesMap in the cascade. Such a map
    contains computed values differing from the parent (or default)
    style. For non-inherited properties, the computed value is
    instead generally the initial value and only inherits the
    computed value from the parent if explicitly specified on the node.
    
    This patch converts the computed value for custom properties into a
    pair of maps CustomPropertiesMap, one map which is ref-counted
    (containing inherited custom properties) and one which is not
    (containing non-inherited properties). The code is refactored to
    continue to deal with the `inherited` map of the pair and behavior
    is unchanged. A follow-up patch will modify the code to deal the
    `non-inherited` map too.
    
    Additionally, two different structs are used to represent the pair
    of maps. A mutable version MutableCustomProperties used when
    building the computed style and an immutable version
    ComputedCustomProperties used during subsequent substitutions of
    var/env in other properties. As an improvement to the current code,
    the ref-counted map is actually represented as a UniqueArc for
    the MutableCustomProperties instead of an Arc.
    
    [1] https://drafts.css-houdini.org/css-properties-values-api-1/
    
    Differential Revision: https://phabricator.services.mozilla.com/D188727
    
    UltraBlame original commit: 8a7d9524a1b92a56ebcd305c82940c1e75ec360b

--- servo/components/style/properties/properties.mako.rs
+++ servo/components/style/properties/properties.mako.rs
@@ -1708,7 +1708,7 @@ impl UnparsedValue {
         &self,
         longhand_id: LonghandId,
         writing_mode: WritingMode,
-        custom_properties: Option<<&Arc<crate::custom_properties::CustomPropertiesMap>>,
+        custom_properties: &crate::custom_properties::ComputedCustomProperties,
         quirks_mode: QuirksMode,
         stylist: &Stylist,
         shorthand_cache: &'cache mut ShorthandsWithPropertyReferencesCache,
@@ -2998,7 +2998,7 @@ pub struct ComputedValuesInner {
     % for style_struct in data.active_style_structs():
         ${style_struct.ident}: Arc<style_structs::${style_struct.name}>,
     % endfor
-    custom_properties: Option<Arc<crate::custom_properties::CustomPropertiesMap>>,
+    custom_properties: crate::custom_properties::ComputedCustomProperties,
 
     
     pub writing_mode: WritingMode,
@@ -3068,29 +3068,13 @@ impl ComputedValues {
     }
 
     
-    pub fn custom_properties(&self) -> Option<<&Arc<crate::custom_properties::CustomPropertiesMap>> {
-        self.custom_properties.as_ref()
+    pub fn custom_properties(&self) -> &crate::custom_properties::ComputedCustomProperties {
+        &self.custom_properties
     }
 
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
     pub fn custom_properties_equal(&self, other: &Self) -> bool {
-        match (self.custom_properties(), other.custom_properties()) {
-            (Some(l), Some(r)) => {
-                l.len() == r.len() && l.iter().zip(r.iter()).all(|((k1, v1), (k2, v2))| k1 == k2 && v1 == v2)
-            },
-            (None, None) => true,
-            _ => false,
-        }
+      self.custom_properties() == other.custom_properties()
     }
 
 % for prop in data.longhands:
@@ -3227,7 +3211,7 @@ impl ComputedValues {
     
     pub fn new(
         pseudo: Option<<&PseudoElement>,
-        custom_properties: Option<Arc<crate::custom_properties::CustomPropertiesMap>>,
+        custom_properties: crate::custom_properties::ComputedCustomProperties,
         writing_mode: WritingMode,
         flags: ComputedValueFlags,
         rules: Option<StrongRuleNode>,
@@ -3266,8 +3250,9 @@ impl ComputedValues {
                 s
             }
             PropertyDeclarationId::Custom(name) => {
-                self.custom_properties
-                    .as_ref()
+              
+              self.custom_properties
+                    .as_ref().inherited.unwrap()
                     .and_then(|map| map.get(name))
                     .map_or(String::new(), |value| value.to_css_string())
             }
@@ -3637,7 +3622,7 @@ pub struct StyleBuilder<'a> {
     pub rules: Option<StrongRuleNode>,
 
     
-    pub custom_properties: Option<Arc<crate::custom_properties::CustomPropertiesMap>>,
+    pub custom_properties: crate::custom_properties::ComputedCustomProperties,
 
     
     pub pseudo: Option<<&'a PseudoElement>,
@@ -3691,7 +3676,7 @@ impl<'a> StyleBuilder<'a> {
             rules,
             modified_reset: false,
             is_root_element,
-            custom_properties: None,
+            custom_properties: crate::custom_properties::ComputedCustomProperties::default(),
             writing_mode: inherited_style.writing_mode,
             flags: Cell::new(flags),
             visited_style: None,
@@ -3728,7 +3713,7 @@ impl<'a> StyleBuilder<'a> {
             modified_reset: false,
             is_root_element: false,
             rules: None,
-            custom_properties: style_to_derive_from.custom_properties().cloned(),
+            custom_properties: style_to_derive_from.custom_properties().clone(),
             writing_mode: style_to_derive_from.writing_mode,
             flags: Cell::new(style_to_derive_from.flags),
             visited_style: None,
@@ -3848,7 +3833,7 @@ impl<'a> StyleBuilder<'a> {
                 ).build()
             })
         });
-        let custom_properties = parent.and_then(|p| p.custom_properties().cloned());
+        let custom_properties = if let Some(p) = parent { p.custom_properties().clone() } else { crate::custom_properties::ComputedCustomProperties::default() };
         let mut ret = Self::new(
             device,
             stylist,
@@ -3991,8 +3976,8 @@ impl<'a> StyleBuilder<'a> {
     }
 
     
-    pub fn custom_properties(&self) -> Option<<&Arc<crate::custom_properties::CustomPropertiesMap>> {
-        self.custom_properties.as_ref()
+    pub fn custom_properties(&self) -> &crate::custom_properties::ComputedCustomProperties {
+        &self.custom_properties
     }
 
     
@@ -4059,7 +4044,7 @@ mod lazy_static_module {
                         % endif
                     }),
                 % endfor
-                custom_properties: None,
+                custom_properties,
                 writing_mode: WritingMode::empty(),
                 rules: None,
                 visited_style: None,
@@ -4186,7 +4171,7 @@ macro_rules! longhand_properties_idents {
 }
 
 
-size_of_test!(ComputedValues, 232);
+size_of_test!(ComputedValues, 240);
 
 size_of_test!(Option<Arc<ComputedValues>>, 8);
 
