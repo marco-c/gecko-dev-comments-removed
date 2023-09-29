10fa395cb7a084343fb94cb795bba1f141a5ca04
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-09-29 22:15:49 +0000

    Bug 1848881 - Make test a required value when compiling a list of tests from the try and CI yml files. r=perftest-reviewers,taskgraph-reviewers,gbrown,sparky
    Differential Revision: https://phabricator.services.mozilla.com/D188301
    
    UltraBlame original commit: 7095adada0469a26b7fb47fc05017c5d8b95e1fb

--- taskcluster/gecko_taskgraph/transforms/test/raptor.py
+++ taskcluster/gecko_taskgraph/transforms/test/raptor.py
@@ -131,7 +131,8 @@ def split_raptor_subtests(config, tests):
         
         subtests = test["raptor"].pop("subtests", None)
         if not subtests:
-            yield test
+            if "macosx1300" not in test["test-platform"]:
+                yield test
             continue
 
         for chunk_number, subtest in enumerate(subtests):
