0ce68bdbc017475b659bb5825fdadc656cb7ad2c
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-09-29 22:23:25 +0000

    Bug 1823475 - Drop DefaultPosition and omit "at <position>" if it is not specified. r=devtools-reviewers,emilio
    `at <position>` is optional value and we should omit it if author doesn't
    specify it, for all basic-shape functions, and ray().
    
    Note that there is a related interpolation issue [1] if one of the end values
    doesn't specify `at <position>`. We didn't address this issue in this patch.
    
    [1] https://github.com/w3c/csswg-drafts/issues/9068
    
    Also, this updates the devtool code to give it the default value, "50% 50%",
    for circle() and ellipse() if `at <position>` is not specified.
    
    Differential Revision: https://phabricator.services.mozilla.com/D181918
    
    UltraBlame original commit: 150d4fac35369ce8b1f5ae336a5a5474993d640d

--- layout/base/ShapeUtils.cpp
+++ layout/base/ShapeUtils.cpp
@@ -51,7 +51,12 @@ nsPoint ShapeUtils::ComputeCircleOrEllipseCenter(
   const auto& position = aBasicShape.IsCircle()
                              ? aBasicShape.AsCircle().position
                              : aBasicShape.AsEllipse().position;
-  MOZ_ASSERT(position.IsPosition(), "A default position should be given");
+  
+  if (position.IsAuto()) {
+    return ComputePosition(StylePosition::FromPercentage(0.5), aRefBox);
+  }
+
+  MOZ_ASSERT(position.IsPosition());
   return ComputePosition(position.AsPosition(), aRefBox);
 }
 
