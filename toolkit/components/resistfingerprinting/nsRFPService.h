00f59b97ab8c9e67d2488671f7277020a1925a2b
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-09-29 22:22:16 +0000

    Bug 1834274 - Part 1: Implement nsIRFPService.idl for nsRFPService. r=emilio
    To allow remote settings client to set fingerprinting overrides from
    Javascript, we need to implement nsIRFPService for nsRFPService.
    
    This patch changes the return format of nsRFPService::GetOrCreate() to
    match the singleton implementation of nsIRFPService. As a result of it,
    we need to change the first init call in nsContentUtils because we
    cannot forward a already_AddRefed<> to mozilla::Unused. It will cause a
    memory leak.
    
    Differential Revision: https://phabricator.services.mozilla.com/D182809
    
    UltraBlame original commit: 79b200c1060e3eba94dea597da093617ec8fc868

--- toolkit/components/resistfingerprinting/nsRFPService.h
+++ toolkit/components/resistfingerprinting/nsRFPService.h
@@ -16,6 +16,7 @@
 #include "nsICookieJarSettings.h"
 #include "nsIObserver.h"
 #include "nsISupports.h"
+#include "nsIRFPService.h"
 #include "nsStringFwd.h"
 
 
@@ -157,12 +158,13 @@ MOZ_MAKE_ENUM_CLASS_BITWISE_OPERATORS(RFPTarget);
 
 
 
-class nsRFPService final : public nsIObserver {
+class nsRFPService final : public nsIObserver, public nsIRFPService {
  public:
   NS_DECL_ISUPPORTS
   NS_DECL_NSIOBSERVER
+  NS_DECL_NSIRFPSERVICE
 
-  static nsRFPService* GetOrCreate();
+  static already_AddRefed<nsRFPService> GetOrCreate();
 
   
   
