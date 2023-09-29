7605faaf49cfedf9108afd92289f5c62a5ada1ac
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-09-29 22:17:12 +0000

    Backed out changeset c2341f2f6212 (bug 1812135) for causing multiple crashes. CLOSED TREE
    UltraBlame original commit: c3e06701d54ffe6acbfb4d0e86241d6463d88508

--- browser/components/newtab/test/browser/browser_customize_menu_content.js
+++ browser/components/newtab/test/browser/browser_customize_menu_content.js
@@ -45,12 +45,8 @@ test_newtab({
     );
 
     
-    
-    
-    
-    
-    let shortcutsSwitch = Cu.waiveXrays(
-      content.document.querySelector("#shortcuts-section moz-toggle")
+    let shortcutsSwitch = content.document.querySelector(
+      "#shortcuts-section .switch"
     );
     Assert.ok(
       !Services.prefs.getBoolPref(TOPSITES_PREF),
@@ -65,12 +61,8 @@ test_newtab({
     Assert.ok(getSection("topsites"), "Shortcuts section is rendered");
 
     
-    
-    
-    
-    
-    let pocketSwitch = Cu.waiveXrays(
-      content.document.querySelector("#pocket-section moz-toggle")
+    let pocketSwitch = content.document.querySelector(
+      "#pocket-section .switch"
     );
     Assert.ok(
       !Services.prefs.getBoolPref(TOPSTORIES_PREF),
@@ -85,12 +77,8 @@ test_newtab({
     Assert.ok(getSection("topstories"), "Pocket section is rendered");
 
     
-    
-    
-    
-    
-    let highlightsSwitch = Cu.waiveXrays(
-      content.document.querySelector("#recent-section moz-toggle")
+    let highlightsSwitch = content.document.querySelector(
+      "#recent-section .switch"
     );
     Assert.ok(
       !Services.prefs.getBoolPref(HIGHLIGHTS_PREF),
