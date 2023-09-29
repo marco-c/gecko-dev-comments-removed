977e61e64bed37b226957fa5b5347f00ce206b89
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-09-29 22:12:15 +0000

    Bug 1854305 - Don't enable thread safety annotations before clang 11. r=nika
    Arbitrary capability names weren't allowed until clang 11, and while we
    could probably make things somehow work with earlier versions of clang,
    it's not that interesting, as long as we keep the thread safety analysis
    running with newer versions of clang on our CI (which we obviously do).
    
    Differential Revision: https://phabricator.services.mozilla.com/D188791
    
    UltraBlame original commit: 9ee2919433c14032b8967194075d9b0c87fd0b02

--- mfbt/ThreadSafety.h
+++ mfbt/ThreadSafety.h
@@ -22,7 +22,7 @@
 #define mozilla_ThreadSafety_h
 #include "mozilla/Attributes.h"
 
-#if defined(__clang__) && (__clang_major__ >= 8) && !defined(SWIG)
+#if defined(__clang__) && (__clang_major__ >= 11) && !defined(SWIG)
 #  define MOZ_THREAD_ANNOTATION_ATTRIBUTE__(x) __attribute__((x))
 
 
