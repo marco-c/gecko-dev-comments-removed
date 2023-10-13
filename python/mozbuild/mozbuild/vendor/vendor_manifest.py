9058e74ccf7822d14f9f10e9fbb3a10aad3151b5
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-10-13 16:48:09 +0000

    Bug 1856347: Allow copy_contents to take an optional parameter ignore_dangling_symlinks r=gbrown
    Differential Revision: https://phabricator.services.mozilla.com/D189855
    
    UltraBlame original commit: 7a84bc5801e62c2e84afcbfa337f0daed4fe8de6

--- python/mozbuild/mozbuild/vendor/vendor_manifest.py
+++ python/mozbuild/mozbuild/vendor/vendor_manifest.py
@@ -470,7 +470,9 @@ class VendorManifest(MozbuildObject):
                 
                 if has_prefix:
                     tardir = mozpath.join(tmpextractdir.name, one_prefix)
-                    mozfile.copy_contents(tardir, tmpextractdir.name)
+                    mozfile.copy_contents(
+                        tardir, tmpextractdir.name, ignore_dangling_symlinks=True
+                    )
                     mozfile.remove(tardir)
 
                 if self.should_perform_step("include"):
