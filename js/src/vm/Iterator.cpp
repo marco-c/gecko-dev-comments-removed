1ca453586ad3877b77494c39bb269925510a63a3
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-10-13 17:09:13 +0000

    Backed out changeset 8822469d696e (bug 1846893) for causing bustages at JSContext.h. CLOSED TREE
    UltraBlame original commit: 8d5a73a0659ce01315ca6a77db72aeb962a93feb

--- js/src/vm/Iterator.cpp
+++ js/src/vm/Iterator.cpp
@@ -22,8 +22,6 @@ namespace JS {
 
 
 JSObject* GetIteratorObject(JSContext* cx, HandleValue obj, bool isAsync) {
-  cx->check(obj);
-
   FixedInvokeArgs<3> args(cx);
   args[0].set(obj);
   args[1].setBoolean(isAsync);
@@ -42,8 +40,6 @@ JSObject* GetIteratorObject(JSContext* cx, HandleValue obj, bool isAsync) {
 
 bool IteratorNext(JSContext* cx, HandleObject iteratorRecord,
                   MutableHandleValue result) {
-  cx->check(iteratorRecord);
-
   FixedInvokeArgs<1> args(cx);
   args[0].setObject(*iteratorRecord);
   return CallSelfHostedFunction(cx, cx->names().IteratorNext,
@@ -52,8 +48,6 @@ bool IteratorNext(JSContext* cx, HandleObject iteratorRecord,
 
 
 bool IteratorComplete(JSContext* cx, HandleObject iterResult, bool* done) {
-  cx->check(iterResult);
-
   RootedValue doneV(cx);
   if (!GetProperty(cx, iterResult, iterResult, cx->names().done, &doneV)) {
     return false;
@@ -66,17 +60,14 @@ bool IteratorComplete(JSContext* cx, HandleObject iterResult, bool* done) {
 
 bool IteratorValue(JSContext* cx, HandleObject iterResult,
                    MutableHandleValue value) {
-  cx->check(iterResult);
   return GetProperty(cx, iterResult, iterResult, cx->names().value, value);
 }
 
 bool GetIteratorRecordIterator(JSContext* cx, HandleObject iteratorRecord,
                                MutableHandleValue iterator) {
-  cx->check(iteratorRecord);
   return GetProperty(cx, iteratorRecord, iteratorRecord, cx->names().iterator,
                      iterator);
 }
-
 
 static bool GetMethod(JSContext* cx, HandleValue v, Handle<PropertyName*> name,
                       MutableHandleValue result) {
@@ -104,7 +95,6 @@ static bool GetMethod(JSContext* cx, HandleValue v, Handle<PropertyName*> name,
 
 bool GetReturnMethod(JSContext* cx, HandleValue iterator,
                      MutableHandleValue result) {
-  cx->check(iterator);
   
   return GetMethod(cx, iterator, cx->names().return_, result);
 }
