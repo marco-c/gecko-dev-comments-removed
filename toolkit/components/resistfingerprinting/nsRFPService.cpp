5c95cdc5c623d5c729155586490912727fcaa94b
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-10-13 17:16:05 +0000

    Bug 1852541: Do not randomize the canvas if it is one uniform pattern r=timhuang,jgilbert
    Differential Revision: https://phabricator.services.mozilla.com/D188133
    
    UltraBlame original commit: 8832d7e6c0dc58c16f4ea888d785222118f4fc83

--- toolkit/components/resistfingerprinting/nsRFPService.cpp
+++ toolkit/components/resistfingerprinting/nsRFPService.cpp
@@ -34,6 +34,7 @@
 #include "mozilla/Preferences.h"
 #include "mozilla/RefPtr.h"
 #include "mozilla/Services.h"
+#include "mozilla/Sprintf.h"
 #include "mozilla/StaticPrefs_javascript.h"
 #include "mozilla/StaticPrefs_network.h"
 #include "mozilla/StaticPrefs_privacy.h"
@@ -1288,7 +1289,8 @@ nsresult nsRFPService::GenerateCanvasKeyFromImageData(
 
 
 nsresult nsRFPService::RandomizePixels(nsICookieJarSettings* aCookieJarSettings,
-                                       uint8_t* aData, uint32_t aSize,
+                                       uint8_t* aData, uint32_t aWidth,
+                                       uint32_t aHeight, uint32_t aSize,
                                        gfx::SurfaceFormat aSurfaceFormat) {
   NS_ENSURE_ARG_POINTER(aData);
 
@@ -1296,7 +1298,25 @@ nsresult nsRFPService::RandomizePixels(nsICookieJarSettings* aCookieJarSettings,
     return NS_OK;
   }
 
-  if (aSize == 0) {
+  if (aSize <= 4) {
+    return NS_OK;
+  }
+
+  
+  static constexpr size_t bytesPerPixel = 4;
+  MOZ_ASSERT(aSize == aWidth * aHeight * bytesPerPixel,
+             "Pixels must be tightly-packed");
+  const bool allPixelsMatch = [&]() {
+    auto itr = RangedPtr<const uint8_t>(aData, aSize);
+    const auto itrEnd = itr + aSize;
+    for (; itr != itrEnd; itr += bytesPerPixel) {
+      if (memcmp(itr.get(), aData, bytesPerPixel) != 0) {
+        return false;
+      }
+    }
+    return true;
+  }();
+  if (allPixelsMatch) {
     return NS_OK;
   }
 
@@ -1343,6 +1363,22 @@ nsresult nsRFPService::RandomizePixels(nsICookieJarSettings* aCookieJarSettings,
 
   
   uint8_t numNoises = std::clamp<uint8_t>(rnd3, 15, 255);
+
+  if (false) {
+    
+    
+    
+    
+    
+    static int calls = 0;
+    char filename[256];
+    SprintfLiteral(filename, "rendered_image_%dx%d_%d_pre", aWidth, aHeight,
+                   calls);
+    FILE* outputFile = fopen(filename, "wb");  
+    fwrite(aData, 1, aSize, outputFile);
+    fclose(outputFile);
+    calls++;
+  }
 
   for (uint8_t i = 0; i <= numNoises; i++) {
     
