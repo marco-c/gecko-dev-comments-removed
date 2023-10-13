b0368e8b176a912d0ade146ebbb48be58a0540d5
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-10-13 16:22:11 +0000

    Bug 1799036 - Make top layer elements be fixed-pos containing blocks. r=dholbert
    This is the most reasonable behavior to preserve our layout invariants.
    
    Once https://github.com/w3c/csswg-drafts/issues/8040 gets resolved we
    can reconsider this.
    
    I think at the very least we should promote fixed-pos elements inside
    the top layer to the top layer, but that has its own set of
    implications.
    
    Differential Revision: https://phabricator.services.mozilla.com/D189924
    
    UltraBlame original commit: 1c7289369f603f40746d9d9be3765e9d16be2480

--- servo/components/style/values/specified/box.rs
+++ servo/components/style/values/specified/box.rs
@@ -1065,7 +1065,9 @@ fn change_bits_for_longhand(longhand: LonghandId) -> WillChangeBits {
         LonghandId::Rotate |
         LonghandId::Scale |
         LonghandId::OffsetPath => WillChangeBits::TRANSFORM,
-        LonghandId::BackdropFilter | LonghandId::Filter => {
+        LonghandId::MozTopLayer | LonghandId::BackdropFilter | LonghandId::Filter => {
+            
+            
             WillChangeBits::STACKING_CONTEXT_UNCONDITIONAL | WillChangeBits::FIXPOS_CB_NON_SVG
         },
         LonghandId::MixBlendMode |
