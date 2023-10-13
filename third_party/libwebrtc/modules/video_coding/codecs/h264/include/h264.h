63665119dc64750d5090f498a58e20fde5a24fcd
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-10-13 16:09:40 +0000

    Bug 1851693 - Vendor libwebrtc from 5278b39fab
    Upstream commit: https://webrtc.googlesource.com/src/+/5278b39fab0edc113a4e125345a11515a71a29ca
        Add H264Encoder::Create()
    
        Most of the usage of the H264Encoder::Create(codec) method passes a
        simple codec with just the H264 codec name. This simplified the call
        sites in many places and removes references to the codec types.
    
        Bug: webrtc:15214
        Change-Id: I4039c0be4ce6e3147c14c7853df4635f344b7d70
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/307222
        Reviewed-by: Harald Alvestrand <htawebrtc.org>
        Commit-Queue: Florent Castelli <orphiswebrtc.org>
        Cr-Commit-Position: refs/heads/main{#40214}
    
    UltraBlame original commit: 95caf70a542a68c8413f57a2ea7113d3719ca376

--- third_party/libwebrtc/modules/video_coding/codecs/h264/include/h264.h
+++ third_party/libwebrtc/modules/video_coding/codecs/h264/include/h264.h
@@ -52,6 +52,7 @@ std::vector<SdpVideoFormat> SupportedH264DecoderCodecs();
 class RTC_EXPORT H264Encoder : public VideoEncoder {
  public:
   static std::unique_ptr<H264Encoder> Create(const cricket::VideoCodec& codec);
+  static std::unique_ptr<H264Encoder> Create();
   
   static bool IsSupported();
   static bool SupportsScalabilityMode(ScalabilityMode scalability_mode);
