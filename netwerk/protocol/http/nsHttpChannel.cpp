a6b7a338695a977579de3d0fa4093c9c3d67a594
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-10-13 16:12:23 +0000

    Bug 1156659 - Added set offline per tab to BrowsingContext. r=valentin,nika,necko-reviewers,kershaw
    Differential Revision: https://phabricator.services.mozilla.com/D187704
    
    UltraBlame original commit: 7e9b86bce76b2a80021d8489ad561afe15f30aa0

--- netwerk/protocol/http/nsHttpChannel.cpp
+++ netwerk/protocol/http/nsHttpChannel.cpp
@@ -560,6 +560,12 @@ nsresult nsHttpChannel::OnBeforeConnect() {
   
   mLoadInfo->SetHstsStatus(isSecureURI);
 
+  RefPtr<mozilla::dom::BrowsingContext> bc;
+  mLoadInfo->GetBrowsingContext(getter_AddRefs(bc));
+  if (bc && bc->Top()->GetForceOffline()) {
+    return NS_ERROR_OFFLINE;
+  }
+
   
   
   StoreUpgradableToSecure(false);
@@ -1031,8 +1037,12 @@ void nsHttpChannel::SpeculativeConnect() {
   
   
   
+  RefPtr<mozilla::dom::BrowsingContext> bc;
+  mLoadInfo->GetBrowsingContext(getter_AddRefs(bc));
+
   if (gIOService->IsOffline() || mUpgradeProtocolCallback ||
-      !(mCaps & NS_HTTP_ALLOW_KEEPALIVE)) {
+      !(mCaps & NS_HTTP_ALLOW_KEEPALIVE) ||
+      (bc && bc->Top()->GetForceOffline())) {
     return;
   }
 
@@ -3672,6 +3682,9 @@ nsresult nsHttpChannel::OpenCacheEntryInternal(bool isHttps) {
   uint32_t cacheEntryOpenFlags;
   bool offline = gIOService->IsOffline();
 
+  RefPtr<mozilla::dom::BrowsingContext> bc;
+  mLoadInfo->GetBrowsingContext(getter_AddRefs(bc));
+
   bool maybeRCWN = false;
 
   nsAutoCString cacheControlRequestHeader;
@@ -3682,7 +3695,8 @@ nsresult nsHttpChannel::OpenCacheEntryInternal(bool isHttps) {
     return NS_OK;
   }
 
-  if (offline || (mLoadFlags & INHIBIT_CACHING)) {
+  if (offline || (mLoadFlags & INHIBIT_CACHING) ||
+      (bc && bc->Top()->GetForceOffline())) {
     if (BYPASS_LOCAL_CACHE(mLoadFlags, LoadPreferCacheLoadOverBypass()) &&
         !offline) {
       return NS_OK;
