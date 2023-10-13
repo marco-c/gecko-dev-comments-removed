6067954048b7234613e505c962b9d2dde37e6a42
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-10-13 17:26:51 +0000

    Bug 1816576 - Apply mozilla patches for libyuv 2bdc210be9eb11ded16bf3ef1f6cadb0d4dcb0c2 r=ng
    Depends on D185940
    
    Differential Revision: https://phabricator.services.mozilla.com/D185941
    
    UltraBlame original commit: f8d3ca1ad1bc8dbfc166f1088ac085ae14697531

--- media/libyuv/libyuv/include/libyuv/row.h
+++ media/libyuv/libyuv/include/libyuv/row.h
@@ -177,8 +177,9 @@ extern "C" {
 
 
 
-#if defined(__x86_64__) || !defined(__pic__) || defined(__clang__) || \
-    defined(_MSC_VER)
+#if !defined(MOZ_PROFILING) && \
+     (defined(__x86_64__) || !defined(__pic__) || defined(__clang__) || \
+      defined(_MSC_VER))
 
 
 #define HAS_I422ALPHATOARGBROW_SSSE3
@@ -247,8 +248,9 @@ extern "C" {
 #define HAS_ARGBATTENUATEROW_AVX2
 #endif
 
-#if defined(__x86_64__) || !defined(__pic__) || defined(__clang__) || \
-    defined(_MSC_VER)
+#if !defined(MOZ_PROFILING) && \
+  (defined(__x86_64__) || !defined(__pic__) || defined(__clang__) || \
+   defined(_MSC_VER))
 
 
 #define HAS_I422ALPHATOARGBROW_AVX2
