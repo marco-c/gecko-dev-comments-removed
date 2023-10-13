ec1700b42f07141237704f69543c26e035a32ca5
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-10-13 17:26:48 +0000

    Bug 1816576 - Update libyuv to 2bdc210be9eb11ded16bf3ef1f6cadb0d4dcb0c2 r=ng
    Differential Revision: https://phabricator.services.mozilla.com/D185940
    
    UltraBlame original commit: 6587c3d17b34108b8ff250c5cb9403d7cf223e4c

--- media/libyuv/libyuv/source/row_lasx.cc
+++ media/libyuv/libyuv/source/row_lasx.cc
@@ -2036,8 +2036,8 @@ static void ARGBToYMatrixRow_LASX(const uint8_t* src_argb,
                                   uint8_t* dst_y,
                                   int width,
                                   const struct RgbConstants* rgbconstants) {
-    int32_t shuff[8] = {0, 4, 1, 5, 2, 6, 3, 7};
-    asm volatile(
+  int32_t shuff[8] = {0, 4, 1, 5, 2, 6, 3, 7};
+  asm volatile(
       "xvldrepl.b      $xr0,  %3,    0             \n\t"  
       "xvldrepl.b      $xr1,  %3,    1             \n\t"  
       "xvldrepl.b      $xr2,  %3,    2             \n\t"  
@@ -2048,9 +2048,11 @@ static void ARGBToYMatrixRow_LASX(const uint8_t* src_argb,
       "xvld            $xr5,  %0,    32            \n\t"
       "xvld            $xr6,  %0,    64            \n\t"
       "xvld            $xr7,  %0,    96            \n\t"  
+                                                          
       "xvor.v          $xr12, $xr3,  $xr3          \n\t"
       "xvor.v          $xr13, $xr3,  $xr3          \n\t"
       "addi.d          %2,    %2,    -32           \n\t"  
+                                                          
       "xvpickev.b      $xr8,  $xr5,  $xr4          \n\t"  
       "xvpickev.b      $xr10, $xr7,  $xr6          \n\t"
       "xvpickod.b      $xr9,  $xr5,  $xr4          \n\t"  
@@ -2067,13 +2069,11 @@ static void ARGBToYMatrixRow_LASX(const uint8_t* src_argb,
       "xvst            $xr11, %1,    0             \n\t"
       "addi.d          %1,    %1,    32            \n\t"
       "bnez            %2,    1b                   \n\t"
-      : "+&r"(src_argb),    // %0
-        "+&r"(dst_y),       // %1
-        "+&r"(width)        // %2
-      : "r"(rgbconstants),
-        "r"(shuff)
-      : "memory"
-    );
+      : "+&r"(src_argb),  // %0
+        "+&r"(dst_y),     // %1
+        "+&r"(width)      // %2
+      : "r"(rgbconstants), "r"(shuff)
+      : "memory");
 }
 
 void ARGBToYRow_LASX(const uint8_t* src_argb, uint8_t* dst_y, int width) {
@@ -2098,8 +2098,8 @@ static void RGBAToYMatrixRow_LASX(const uint8_t* src_rgba,
                                   uint8_t* dst_y,
                                   int width,
                                   const struct RgbConstants* rgbconstants) {
-    int32_t shuff[8] = {0, 4, 1, 5, 2, 6, 3, 7};
-    asm volatile(
+  int32_t shuff[8] = {0, 4, 1, 5, 2, 6, 3, 7};
+  asm volatile(
       "xvldrepl.b      $xr0,  %3,    0             \n\t"  
       "xvldrepl.b      $xr1,  %3,    1             \n\t"  
       "xvldrepl.b      $xr2,  %3,    2             \n\t"  
@@ -2110,9 +2110,11 @@ static void RGBAToYMatrixRow_LASX(const uint8_t* src_rgba,
       "xvld            $xr5,  %0,    32            \n\t"
       "xvld            $xr6,  %0,    64            \n\t"
       "xvld            $xr7,  %0,    96            \n\t"  
+                                                          
       "xvor.v          $xr12, $xr3,  $xr3          \n\t"
       "xvor.v          $xr13, $xr3,  $xr3          \n\t"
       "addi.d          %2,    %2,    -32           \n\t"  
+                                                          
       "xvpickev.b      $xr8,  $xr5,  $xr4          \n\t"  
       "xvpickev.b      $xr10, $xr7,  $xr6          \n\t"
       "xvpickod.b      $xr9,  $xr5,  $xr4          \n\t"  
@@ -2129,13 +2131,11 @@ static void RGBAToYMatrixRow_LASX(const uint8_t* src_rgba,
       "xvst            $xr11, %1,    0             \n\t"
       "addi.d          %1,    %1,    32            \n\t"
       "bnez            %2,    1b                   \n\t"
-      : "+&r"(src_rgba),    // %0
-        "+&r"(dst_y),       // %1
-        "+&r"(width)        // %2
-      : "r"(rgbconstants),
-        "r"(shuff)
-      : "memory"
-    );
+      : "+&r"(src_rgba),  // %0
+        "+&r"(dst_y),     // %1
+        "+&r"(width)      // %2
+      : "r"(rgbconstants), "r"(shuff)
+      : "memory");
 }
 
 void RGBAToYRow_LASX(const uint8_t* src_rgba, uint8_t* dst_y, int width) {
@@ -2151,18 +2151,19 @@ void BGRAToYRow_LASX(const uint8_t* src_bgra, uint8_t* dst_y, int width) {
 }
 
 static void RGBToYMatrixRow_LASX(const uint8_t* src_rgba,
-                                  uint8_t* dst_y,
-                                  int width,
-                                  const struct RgbConstants* rgbconstants) {
-    int8_t shuff[128] = {0, 2, 3, 5, 6, 8, 9, 11, 12, 14, 15, 17, 18, 20, 21, 23,
-                         0, 2, 3, 5, 6, 8, 9, 11, 12, 14, 15, 17, 18, 20, 21, 23,
-                         24, 26, 27, 29, 30, 0, 1, 3, 4, 6, 7, 9, 10, 12, 13, 15,
-                         24, 26, 27, 29, 30, 0, 1, 3, 4, 6, 7, 9, 10, 12, 13, 15,
-                         1, 0, 4, 0, 7, 0, 10, 0, 13, 0, 16, 0, 19, 0, 22, 0,
-                         1, 0, 4, 0, 7, 0, 10, 0, 13, 0, 16, 0, 19, 0, 22, 0,
-                         25, 0, 28, 0, 31, 0, 2, 0, 5, 0, 8, 0, 11, 0, 14, 0,
-                         25, 0, 28, 0, 31, 0, 2, 0, 5, 0, 8, 0, 11, 0, 14, 0};
-    asm volatile(
+                                 uint8_t* dst_y,
+                                 int width,
+                                 const struct RgbConstants* rgbconstants) {
+  int8_t shuff[128] = {
+      0,  2,  3,  5,  6,  8, 9,  11, 12, 14, 15, 17, 18, 20, 21, 23,
+      0,  2,  3,  5,  6,  8, 9,  11, 12, 14, 15, 17, 18, 20, 21, 23,
+      24, 26, 27, 29, 30, 0, 1,  3,  4,  6,  7,  9,  10, 12, 13, 15,
+      24, 26, 27, 29, 30, 0, 1,  3,  4,  6,  7,  9,  10, 12, 13, 15,
+      1,  0,  4,  0,  7,  0, 10, 0,  13, 0,  16, 0,  19, 0,  22, 0,
+      1,  0,  4,  0,  7,  0, 10, 0,  13, 0,  16, 0,  19, 0,  22, 0,
+      25, 0,  28, 0,  31, 0, 2,  0,  5,  0,  8,  0,  11, 0,  14, 0,
+      25, 0,  28, 0,  31, 0, 2,  0,  5,  0,  8,  0,  11, 0,  14, 0};
+  asm volatile(
       "xvldrepl.b      $xr0,  %3,    0             \n\t"  
       "xvldrepl.b      $xr1,  %3,    1             \n\t"  
       "xvldrepl.b      $xr2,  %3,    2             \n\t"  
@@ -2175,10 +2176,12 @@ static void RGBToYMatrixRow_LASX(const uint8_t* src_rgba,
       "xvld            $xr8,  %0,    0             \n\t"
       "xvld            $xr9,  %0,    32            \n\t"
       "xvld            $xr10, %0,    64            \n\t"  
+                                                          
       "xvor.v          $xr12, $xr3,  $xr3          \n\t"
       "xvor.v          $xr13, $xr3,  $xr3          \n\t"
       "xvor.v          $xr11, $xr9,  $xr9          \n\t"
       "addi.d          %2,    %2,    -32           \n\t"  
+                                                          
       "xvpermi.q       $xr9,  $xr8,  0x30          \n\t"  
       "xvpermi.q       $xr8,  $xr10, 0x03          \n\t"  
       "xvpermi.q       $xr10, $xr11, 0x30          \n\t"  
@@ -2202,8 +2205,7 @@ static void RGBToYMatrixRow_LASX(const uint8_t* src_rgba,
         "+&r"(width)        // %2
       : "r"(rgbconstants),  // %3
         "r"(shuff)          // %4
-      : "memory"
-    );
+      : "memory");
 }
 
 void RGB24ToYJRow_LASX(const uint8_t* src_rgb24, uint8_t* dst_yj, int width) {
@@ -2299,4 +2301,4 @@ void ARGBToUVJRow_LASX(const uint8_t* src_argb,
 }  
 #endif
 
-#endif  
+#endif
