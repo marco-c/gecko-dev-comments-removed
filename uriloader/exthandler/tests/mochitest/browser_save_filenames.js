79a7de4bda28b9506802f8fa95ada5b805be66d8
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-10-13 16:55:25 +0000

    Bug 1854747 - Stop exposing the image data as file to clipboard; r=evilpie
    Differential Revision: https://phabricator.services.mozilla.com/D189242
    
    UltraBlame original commit: 35622e43aeba2042c84382c6e9e8d632f45d255a

--- uriloader/exthandler/tests/mochitest/browser_save_filenames.js
+++ uriloader/exthandler/tests/mochitest/browser_save_filenames.js
@@ -372,7 +372,11 @@ if (AppConstants.platform != "macosx") {
 
 
 
-if (AppConstants.platform == "win") {
+const imageAsFileEnabled = SpecialPowers.getBoolPref(
+  "clipboard.imageAsFile.enabled",
+  false
+);
+if (AppConstants.platform == "win" && imageAsFileEnabled) {
   add_task(async function copy_image() {
     for (let idx = 0; idx < expectedItems.length; idx++) {
       if (!expectedItems[idx].draggable) {
