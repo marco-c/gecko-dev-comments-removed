2c22f1f9b8811434c0181b8882bea5703ae0333a
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-10-18 19:29:54 +0000

    Bug 1855174 - Process xpcshell node output in realtime r=jmaher,necko-reviewers,kershaw
    Spawned nodejs servers sometimes print output to stdout or stderr.
    While it's not strictly necessary to display, it's sometimes useful for
    debugging, but it's problematic that we only dump the output when killing
    the node server.
    
    After landing D189212 this becomes even more problematic, as killing the
    process via os.killpg means the file descriptors are already closed.
    
    This patch makes it so for each started server we also spawn a new thread
    that reads from stdout and stderr. This way the output is present in the
    logs at the correct time.
    
    Depends on D189212
    
    Differential Revision: https://phabricator.services.mozilla.com/D189213
    
    UltraBlame original commit: ee5517b7872627464d10893f99130fe96a841f40

--- netwerk/test/unit/node_execute/test_node_execute_loop.js
+++ netwerk/test/unit/node_execute/test_node_execute_loop.js
@@ -13,6 +13,8 @@ const { NodeServer } = ChromeUtils.importESModule(
 
 add_task(async function killOnEnd() {
   let id = await NodeServer.fork();
+  await NodeServer.execute(id, `console.log("hello");`);
+  await NodeServer.execute(id, `console.error("hello");`);
   
   NodeServer.execute(id, "while (true) {}").catch(e => {});
   await new Promise(resolve => do_timeout(10, resolve));
