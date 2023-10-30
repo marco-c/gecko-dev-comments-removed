bfb87406e4f3d0d8c3d1e515a5d7387fb5923040
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-10-30 10:42:55 +0000

    Bug 1857862 - Vendor libwebrtc from 0689cfc6ce
    Upstream commit: https://webrtc.googlesource.com/src/+/0689cfc6ce15d7c622ce225475c60817493fb048
        Reland "[fuchsia] remove Scenic/UseFlatland dependency in DesktopCapturer"
    
        This reverts commit 726992d7a4722b8a283d797d04432d0c6335ca96.
    
        Reason for revert: Relanding with original errors fixed (tested by building the patch locally against Chromium)
    
        This change no longer attempts to migrate the display size protocol from fuchsia.ui.scenic.Scenic/GetDisplayInfo to fuchsia.ui.display.singleton.Info/GetMetrics because the latter API was introduced in Fuchsia API 12, which is not yet supported in Chrome (hence some of the build errors causing the revert).
    
        Original change's description:
        > Revert "[fuchsia] remove Scenic and GFX  dependencies in DesktopCapturer"
        >
        > This reverts commit fe5be2eb4ff8dccd96257fb8cbf32500c636c358.
        >
        > Reason for revert: This breaks the WebRTC roll into Chromium:
        >
        > - https://chromium-review.googlesource.com/c/chromium/src/+/4688561
        > - https://ci.chromium.org/ui/p/chromium/builders/try/fuchsia-binary-size/399140/overview
        >
        > Error:
        >
        > [4273/4389] CXX obj/third_party/webrtc/modules/desktop_capture/desktop_capture/screen_capturer_fuchsia.o
        > FAILED: obj/third_party/webrtc/modules/desktop_capture/desktop_capture/screen_capturer_fuchsia.o
        > ../../buildtools/reclient/rewrapper -cfg=../../buildtools/reclient_cfgs/chromium-browser-clang/rewra...(too long)
        > ../../third_party/webrtc/modules/desktop_capture/screen_capturer_fuchsia.cc:59:10: error: use of undeclared identifier 'capturer'
        > 59 |   return capturer(new ScreenCapturerFuchsia());
        > |          ^
        > ../../third_party/webrtc/modules/desktop_capture/screen_capturer_fuchsia.cc:199:36: error: no type named 'InfoSyncPtr' in namespace 'fuchsia::ui::display::singleton'
        >
        > Original change's description:
        > > [fuchsia] remove Scenic and GFX  dependencies in DesktopCapturer
        > >
        > > We previously used:
        > > - fuchsia.ui.scenic.Scenic/UsesFlatland to determine whether to use
        > >   Flatland; from now on it should always be the case, so this check is
        > >   no longer necessary.
        > > - fuchsia.ui.scenic.Scenic/GetDisplayInfo to get
        > >   fuchsia.ui.gfx.DisplayInfo. This has been migrated to
        > >   fuchsia.ui.display.singleton.Info/GetMetrics and
        > >   fuchsia.ui.display.singleton.Metrics.
        > >
        > > Bug: fuchsia:100303
        > > Change-Id: I147da9ffdf0ca49e1c5bde5d188e434fc660becc
        > > Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/311860
        > > Reviewed-by: Emircan Uysaler <emircangoogle.com>
        > > Reviewed-by: Alexander Cooper <alcooperchromium.org>
        > > Commit-Queue: Caroline Liu <carolineliugoogle.com>
        > > Cr-Commit-Position: refs/heads/main{#40432}
        >
        > Bug: fuchsia:100303, b/291393959
        > Change-Id: Iae70e568a8c9819e40e48069af8cea0d4ef2b6c5
        > Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/311801
        > Owners-Override: Mirko Bonadei <mbonadeiwebrtc.org>
        > Bot-Commit: rubber-stamperappspot.gserviceaccount.com <rubber-stamperappspot.gserviceaccount.com>
        > Commit-Queue: Mirko Bonadei <mbonadeiwebrtc.org>
        > Cr-Commit-Position: refs/heads/main{#40436}
    
        Bug: fuchsia:100303, b/291393959
        Change-Id: Icb7074ac86c1804ab2bdf809ea1496539ee2bf80
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/312000
        Commit-Queue: Caroline Liu <carolineliugoogle.com>
        Reviewed-by: Mirko Bonadei <mbonadeiwebrtc.org>
        Reviewed-by: Alexander Cooper <alcooperchromium.org>
        Cr-Commit-Position: refs/heads/main{#40452}
    
    UltraBlame original commit: 8f0275f5aaacc5cc7ab764f3f480f8e58715f2dd

--- third_party/libwebrtc/modules/desktop_capture/screen_capturer_fuchsia.h
+++ third_party/libwebrtc/modules/desktop_capture/screen_capturer_fuchsia.h
@@ -29,8 +29,6 @@ class ScreenCapturerFuchsia final : public DesktopCapturer {
   ScreenCapturerFuchsia();
   ~ScreenCapturerFuchsia() override;
 
-  static bool CheckRequirements();
-
   
   void Start(Callback* callback) override;
   void CaptureFrame() override;
