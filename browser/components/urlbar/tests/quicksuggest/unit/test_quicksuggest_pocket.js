22e4020b5cfd44cf02e6b4ae4fa8c7fdfbd92031
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-10-30 10:36:57 +0000

    Bug 1859341 - Modify desktop Suggest integration so it uses addon and Pocket suggestions from Rust. r=daisuke
    This builds on the recent vendor that was done in bug 1859069 and D191004 in
    particular.
    
    The parameter to the Rust `SuggestStore.query()` function now takes an object
    with a `providers` property, instead of separate boolean sponsored and
    nonsponsored properties. `providers` is an array whose items are integers that
    identify Rust providers (defined in [the RustSuggest bindings here](https://searchfox.org/mozilla-central/rev/c3c92500ef8ffab4fa53d4b2d5c5e2b49025e89b/toolkit/components/uniffi-bindgen-gecko-js/components/generated/RustSuggest.sys.mjs#953-958)).
    
    `MockRustSuggest` in `QuickSuggestTestUtils.sys.mjs` is becoming kind of a pain
    to keep updated. More tasks in `test_quicksuggest_pocket.js` should use Rust
    than the ones I've updated here, but they don't because I would need to
    (re)implement the prefix-matching behavior in `MockRustSuggest`. In a follow-up
    I'd like to explore using a mock remote settings server instead so that the real
    Rust component can be tested.
    
    Differential Revision: https://phabricator.services.mozilla.com/D191351
    
    UltraBlame original commit: 1b9473fe1ebe95bc900a9e69f02d7718061574fa

--- browser/components/urlbar/tests/quicksuggest/unit/test_quicksuggest_pocket.js
+++ browser/components/urlbar/tests/quicksuggest/unit/test_quicksuggest_pocket.js
@@ -55,7 +55,7 @@ add_task(async function telemetryType() {
 
 
 
-add_task(async function nonsponsoredDisabled() {
+add_tasks_with_rust(async function nonsponsoredDisabled() {
   
   
   UrlbarPrefs.set("suggest.quicksuggest.sponsored", false);
@@ -87,7 +87,7 @@ add_task(async function nonsponsoredDisabled() {
 
 
 
-add_task(async function pocketSpecificPrefsDisabled() {
+add_tasks_with_rust(async function pocketSpecificPrefsDisabled() {
   const prefs = ["suggest.pocket", "pocket.featureGate"];
   for (const pref of prefs) {
     
@@ -117,7 +117,7 @@ add_task(async function pocketSpecificPrefsDisabled() {
 
 
 
-add_task(async function nimbus() {
+add_tasks_with_rust(async function nimbus() {
   
   UrlbarPrefs.set("pocket.featureGate", false);
   await check_results({
@@ -166,7 +166,7 @@ add_task(async function nimbus() {
 
 
 
-add_task(async function topPick() {
+add_tasks_with_rust(async function topPick() {
   await check_results({
     context: createContext(HIGH_KEYWORD, {
       providers: [UrlbarProviderQuickSuggest.name],
@@ -528,6 +528,31 @@ function makeExpectedResult({
   source = "remote-settings",
   isTopPick = false,
 } = {}) {
+  if (
+    source == "remote-settings" &&
+    UrlbarPrefs.get("quicksuggest.rustEnabled")
+  ) {
+    source = "rust";
+  }
+
+  let provider;
+  let keywordSubstringNotTyped = fullKeyword.substring(searchString.length);
+  let description = suggestion.description;
+  switch (source) {
+    case "remote-settings":
+      provider = "PocketSuggestions";
+      break;
+    case "rust":
+      provider = "Pocket";
+      
+      keywordSubstringNotTyped = "";
+      description = suggestion.title;
+      break;
+    case "merino":
+      provider = "pocket";
+      break;
+  }
+
   let url = new URL(suggestion.url);
   url.searchParams.set("utm_medium", "firefox-desktop");
   url.searchParams.set("utm_source", "firefox-suggest");
@@ -542,13 +567,13 @@ function makeExpectedResult({
     heuristic: false,
     payload: {
       source,
-      provider: source == "remote-settings" ? "PocketSuggestions" : "pocket",
+      provider,
       telemetryType: "pocket",
       title: suggestion.title,
       url: url.href,
       displayUrl: url.href.replace(/^https:\/\//, ""),
       originalUrl: suggestion.url,
-      description: isTopPick ? suggestion.description : "",
+      description: isTopPick ? description : "",
       icon: isTopPick
         ? "chrome://global/skin/icons/pocket.svg"
         : "chrome://global/skin/icons/pocket-favicon.ico",
@@ -558,7 +583,7 @@ function makeExpectedResult({
         id: "firefox-suggest-pocket-bottom-text",
         args: {
           keywordSubstringTyped: searchString,
-          keywordSubstringNotTyped: fullKeyword.substring(searchString.length),
+          keywordSubstringNotTyped,
         },
       },
     },
@@ -566,6 +591,10 @@ function makeExpectedResult({
 }
 
 async function waitForSuggestions(keyword = LOW_KEYWORD) {
+  if (UrlbarPrefs.get("quicksuggest.rustEnabled")) {
+    return;
+  }
+
   let feature = QuickSuggest.getFeature("PocketSuggestions");
   await TestUtils.waitForCondition(async () => {
     let suggestions = await feature.queryRemoteSettings(keyword);
