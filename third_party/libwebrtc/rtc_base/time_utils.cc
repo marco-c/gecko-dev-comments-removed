de87813e5e816a72cf0468edeb53cdc2af2b6a9d
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-10-30 10:38:43 +0000

    Bug 1857862 - Vendor libwebrtc from 3adaeefbc6
    Upstream commit: https://webrtc.googlesource.com/src/+/3adaeefbc6b3bade8c07ac49d8977a44a2503bfe
        Fix TimeUTCMicros resolution on Windows
    
        making it return actual microseconds instead of being limited to
        millisecond resolution.
    
        This uses GetSystemTimeAsFileTime
          https://learn.microsoft.com/en-us/windows/win32/api/sysinfoapi/nf-sysinfoapi-getsystemtimeasfiletime
        which returns a timestamp in multiples of 100ns since January 1st 1601.
    
        BUG=webrtc:15212
    
        Change-Id: I293868d8f683289a9dbcf6eccb910bd9c6694e57
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/306440
        Reviewed-by: Henrik Boström <hboswebrtc.org>
        Commit-Queue: Philipp Hancke <phanckemicrosoft.com>
        Reviewed-by: Mirko Bonadei <mbonadeiwebrtc.org>
        Cr-Commit-Position: refs/heads/main{#40360}
    
    UltraBlame original commit: c6a46f57fd70b00e5c362f673e9233ec27f2ef11

--- third_party/libwebrtc/rtc_base/time_utils.cc
+++ third_party/libwebrtc/rtc_base/time_utils.cc
@@ -14,16 +14,26 @@
 #include <sys/time.h>
 #endif
 
-#if defined(WEBRTC_WIN)
-#include <sys/timeb.h>
-#endif
-
 #include "rtc_base/checks.h"
 #include "rtc_base/numerics/safe_conversions.h"
 #include "rtc_base/system_time.h"
 #include "rtc_base/time_utils.h"
+#if defined(WEBRTC_WIN)
+#include "rtc_base/win32.h"
+#endif
+#if defined(WEBRTC_WIN)
+#include <minwinbase.h>
+#endif
 
 namespace rtc {
+
+#if defined(WEBRTC_WIN) || defined(WINUWP)
+
+
+static constexpr uint64_t kFileTimeToUnixTimeEpochOffset =
+    116444736000000000ULL;
+static constexpr uint64_t kFileTimeToMicroSeconds = 10LL;
+#endif
 
 ClockInterface* g_clock = nullptr;
 
@@ -115,8 +125,6 @@ class TimeHelper final {
   }
 
  private:
-  static constexpr uint64_t kFileTimeToUnixTimeEpochOffset =
-      116444736000000000ULL;
   static constexpr uint64_t kNTPTimeToUnixTimeEpochOffset = 2208988800000L;
 
   
@@ -231,13 +239,15 @@ int64_t TimeUTCMicros() {
   
   return (static_cast<int64_t>(time.tv_sec) * rtc::kNumMicrosecsPerSec +
           time.tv_usec);
-
 #elif defined(WEBRTC_WIN)
-  struct _timeb time;
-  _ftime(&time);
+  FILETIME ft;
   
-  return (static_cast<int64_t>(time.time) * rtc::kNumMicrosecsPerSec +
-          static_cast<int64_t>(time.millitm) * rtc::kNumMicrosecsPerMillisec);
+  GetSystemTimeAsFileTime(&ft);
+  LARGE_INTEGER li;
+  li.HighPart = ft.dwHighDateTime;
+  li.LowPart = ft.dwLowDateTime;
+  return (li.QuadPart - kFileTimeToUnixTimeEpochOffset) /
+         kFileTimeToMicroSeconds;
 #endif
 }
 
