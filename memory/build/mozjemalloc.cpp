af68e1dfa3f7f7ee80aca00211503a95de81f131
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-10-30 10:34:49 +0000

    Bug 1854550 - pt 8. Rename a variable r=glandium
    Differential Revision: https://phabricator.services.mozilla.com/D190423
    
    UltraBlame original commit: e656c6464f14b61686d40e94d2159e65a2c800e8

--- memory/build/mozjemalloc.cpp
+++ memory/build/mozjemalloc.cpp
@@ -4356,18 +4356,18 @@ static bool malloc_init_hard() {
   }
 
   
-  const size_t result = GetKernelPageSize();
+  const size_t page_size = GetKernelPageSize();
   
-  MOZ_ASSERT(((result - 1) & result) == 0);
+  MOZ_ASSERT(IsPowerOfTwo(page_size));
 #ifdef MALLOC_STATIC_PAGESIZE
-  if (gPageSize % result) {
+  if (gPageSize % page_size) {
     _malloc_message(
         _getprogname(),
         "Compile-time page size does not divide the runtime one.\n");
     MOZ_CRASH();
   }
 #else
-  gRealPageSize = gPageSize = result;
+  gRealPageSize = gPageSize = page_size;
 #endif
 
   
