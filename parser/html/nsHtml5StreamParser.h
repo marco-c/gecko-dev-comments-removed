f89bdd1578d6cb82e49005622788cb3bf1c8b4ec
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-10-30 11:48:59 +0000

    Bug 1857615: Add Telemetry for OnStopRequest/OnDataFinished/etc r=necko-reviewers,kershaw
    Also add OnStartRequest telemetry back
    
    Differential Revision: https://phabricator.services.mozilla.com/D190470
    
    UltraBlame original commit: cfc1bbb8edcc79491d2e1267e84338d155b2ee3c

--- parser/html/nsHtml5StreamParser.h
+++ parser/html/nsHtml5StreamParser.h
@@ -18,6 +18,7 @@
 #include "mozilla/ReentrantMonitor.h"
 #include "mozilla/RefPtr.h"
 #include "mozilla/Span.h"
+#include "mozilla/TimeStamp.h"
 #include "mozilla/UniquePtr.h"
 #include "nsCharsetSource.h"
 #include "nsCOMPtr.h"
@@ -771,6 +772,13 @@ class nsHtml5StreamParser final : public nsISupports {
 
 
   bool mOnStopCalled{false};
+
+  
+
+
+  
+  mozilla::TimeStamp mOnStopRequestTime;
+  mozilla::TimeStamp mOnDataFinishedTime;
 };
 
 #endif  
