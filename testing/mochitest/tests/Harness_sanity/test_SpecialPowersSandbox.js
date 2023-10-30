fcfe4373885e06af90198b5d0095ae99a2e3185b
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-10-30 11:11:29 +0000

    Bug 1856464 - Include stack in Assert.rejects r=jmaher
    Assert.rejects uses callbacks in a way that the stack no longer includes
    the original caller in the stack when the stack is generated.
    Consequently, when AssertionError tries to identify the caller, it is
    unable to do so, resulting in a `null` stack.
    
    This `null` stack is passed by the SpecialPowers glue to
    `do_report_result` in xpcshell/head.js, which doesn't expect a `null`
    stack, and consequently the following error is reported:
    "TypeError: can't access property "filename", stack is null"
    
    To fix this issue, we save the stack (including the caller) upon
    entering `Assert.rejects`, and forward that to `AssertionError`.
    
    Differential Revision: https://phabricator.services.mozilla.com/D189863
    
    UltraBlame original commit: 4a8a737bf972f06efa9883f8af6c873c5e10d765

--- testing/mochitest/tests/Harness_sanity/test_SpecialPowersSandbox.js
+++ testing/mochitest/tests/Harness_sanity/test_SpecialPowersSandbox.js
@@ -146,5 +146,20 @@ add_task(async function () {
     let results = diags.map(diag => [diag.passed, diag.msg]);
 
     deepEqual(results, expected, "Got expected assertions");
+    for (let { msg, stack } of diags) {
+      ok(stack, `Got stack for: ${msg}`);
+      
+      
+      
+      let expectedFilenamePart = "test_SpecialPowersSandbox.js:";
+      if (name === "SpecialPowers.loadChromeScript") {
+        
+        
+        expectedFilenamePart = "loadChromeScript anonymous function>:";
+      }
+      if (!stack.includes(expectedFilenamePart)) {
+        ok(false, `Stack does not contain ${expectedFilenamePart}: ${stack}`);
+      }
+    }
   }
 });
