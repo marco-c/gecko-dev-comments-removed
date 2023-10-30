0f93c08a9038fa43893515ff64ca50171f129ba4
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-10-30 10:37:20 +0000

    Bug 1855986: Don't attempt to use in-tree double-conversion for ICU standalone builds. r=sergesanspaille,glandium
    Updates the ICU patch "double-conversion.diff" to only use the in-tree copy of
    double-conversion when building Firefox/SpiderMonkey. Standalone ICU builds
    instead use the ICU copy of double-conversion. Standalone ICU builds happen
    when running "intl/icu_sources_data.py" and when converting the in-tree
    little-endian ICU data file to the big-endian format.
    
    `JS_HAS_INTL_API` is used to detect how ICU is built: When building ICU as part
    of Firefox/SpiderMonkey, `JS_HAS_INTL_API` is guaranteed to be set, whereas in
    standalone ICU builds `JS_HAS_INTL_API` isn't defined.
    
    After applying the updated "double-conversion.diff" patch, ICU was reimported
    to restore the previously deleted double-conversion sources files from ICU.
    
    Depends on D190791
    
    Differential Revision: https://phabricator.services.mozilla.com/D190792
    
    UltraBlame original commit: 8f4fc4b6cc05c16482986671d19d546faf6cb212

--- intl/icu/source/i18n/units_converter.cpp
+++ intl/icu/source/i18n/units_converter.cpp
@@ -7,7 +7,11 @@
 
 #include "charstr.h"
 #include "cmemory.h"
+#ifdef JS_HAS_INTL_API
 #include "double-conversion/string-to-double.h"
+#else
+#include "double-conversion-string-to-double.h"
+#endif
 #include "measunit_impl.h"
 #include "putilimp.h"
 #include "uassert.h"
@@ -106,7 +110,11 @@ namespace {
 
 
 
+#ifdef JS_HAS_INTL_API
 using double_conversion::StringToDoubleConverter;
+#else
+using icu::double_conversion::StringToDoubleConverter;
+#endif
 
 
 
