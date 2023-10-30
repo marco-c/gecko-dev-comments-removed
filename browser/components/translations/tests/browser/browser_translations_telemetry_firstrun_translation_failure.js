8f221f438c64e176308633d6df19989bd69f8736
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-10-30 11:13:43 +0000

    Bug 1815339 - Handle a missed promise rejection; r=nordzilla
    This is another drive-by that I noticed while working on this patch
    stack. It was causing additional errors in tests.
    
    Differential Revision: https://phabricator.services.mozilla.com/D190726
    
    UltraBlame original commit: b06cbb9addb2b403ab68d7c66cca52216edbb724

--- browser/components/translations/tests/browser/browser_translations_telemetry_firstrun_translation_failure.js
+++ browser/components/translations/tests/browser/browser_translations_telemetry_firstrun_translation_failure.js
@@ -12,10 +12,6 @@ const { PromiseTestUtils } = ChromeUtils.importESModule(
 
 
 add_task(async function test_translations_telemetry_firstrun_failure() {
-  PromiseTestUtils.expectUncaughtRejection(
-    /Intentionally rejecting downloads./
-  );
-
   const { cleanup, rejectDownloads, runInPage } = await loadTestPage({
     page: SPANISH_PAGE_URL,
     languagePairs: LANGUAGE_PAIRS,
