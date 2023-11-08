2b731d4a479e9fb4ef1277102a11e5a18bf5e8e7
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-11-08 15:53:07 +0000

    Bug 1863197 - Fix intermittent failure caused by DST r=arai
    Differential Revision: https://phabricator.services.mozilla.com/D192804
    
    UltraBlame original commit: 58f61f19462af6371405866cd0506983856d5433

--- js/src/tests/non262/Date/15.9.5.7.js
+++ js/src/tests/non262/Date/15.9.5.7.js
@@ -25,11 +25,12 @@
 
 
 
+
 var SECTION = "15.9.5.7";
 var TITLE   = "Date.prototype.toLocaleTimeString()";
-  
+
 var status = '';
-var actual = ''; 
+var actual = '';
 var expect = '';
 var givenDate;
 var year = '';
@@ -37,75 +38,73 @@ var regexp = '';
 var TimeString = '';
 var reducedDateString = '';
 var hopeThisIsLocaleTimeString = '';
-var cnERR ='OOPS! FATAL ERROR: no regexp match in extractLocaleTimeString()';
+var cnERR = 'OOPS! FATAL ERROR: no regexp match in extractLocaleTimeString()';
 
-writeHeaderToLog( SECTION + " "+ TITLE);
+writeHeaderToLog(SECTION + " " + TITLE);
 
-var now = new Date();
+inTimeZone("PST", () => {
+  var now = new Date();
 
+  
+  
+  var TIME_NOW = now.valueOf();
 
-
-var TIME_NOW = now.valueOf();
-
-
-
-status = "typeof (now.toLocaleTimeString())"; 
-actual =   typeof (now.toLocaleTimeString());
-expect = "string";
-addTestCase();
-
-status = "Date.prototype.toLocaleTimeString.length";  
-actual =  Date.prototype.toLocaleTimeString.length;
-expect =  0;  
-addTestCase();
-
-
-addDateTestCase(0);
-addDateTestCase(TZ_ADJUST);
   
 
-addDateTestCase(UTC_01_JAN_1900);
-addDateTestCase(UTC_01_JAN_1900 - TZ_ADJUST);
-
-
-addDateTestCase(UTC_01_JAN_2000);
-addDateTestCase(UTC_01_JAN_2000 - TZ_ADJUST);
-   
-
-addDateTestCase(UTC_29_FEB_2000);
-addDateTestCase(UTC_29_FEB_2000 - 1000);
-addDateTestCase(UTC_29_FEB_2000 - TZ_ADJUST);
-
-
-addDateTestCase( TIME_NOW);
-addDateTestCase( TIME_NOW - TZ_ADJUST);
-
-
-addDateTestCase(UTC_01_JAN_2005);
-addDateTestCase(UTC_01_JAN_2005 - 1000);
-addDateTestCase(UTC_01_JAN_2005 - TZ_ADJUST);
-
-test();
-
-function addTestCase()
-{
-  new TestCase(
-    status,
-    expect,
-    actual);
-}
-
-
-function addDateTestCase(date_given_in_milliseconds)
-{
-  var s = 'new Date(' +  date_given_in_milliseconds + ')';
-  givenDate = new Date(date_given_in_milliseconds);
-  
-  status = 'd = ' + s +
-    '; d == new Date(d.toDateString() + " " + d.toLocaleTimeString())';  
-  expect = givenDate.toString();
-  actual = new Date(givenDate.toDateString() +
-                    ' ' + givenDate.toLocaleTimeString()).toString();
+  status = "typeof (now.toLocaleTimeString())";
+  actual =   typeof (now.toLocaleTimeString());
+  expect = "string";
   addTestCase();
-}
 
+  status = "Date.prototype.toLocaleTimeString.length";
+  actual =  Date.prototype.toLocaleTimeString.length;
+  expect =  0;
+  addTestCase();
+
+  
+  addDateTestCase(0);
+  addDateTestCase(TZ_ADJUST);
+
+  
+  addDateTestCase(UTC_01_JAN_1900);
+  addDateTestCase(UTC_01_JAN_1900 - TZ_ADJUST);
+
+  
+  addDateTestCase(UTC_01_JAN_2000);
+  addDateTestCase(UTC_01_JAN_2000 - TZ_ADJUST);
+
+  
+  addDateTestCase(UTC_29_FEB_2000);
+  addDateTestCase(UTC_29_FEB_2000 - 1000);
+  addDateTestCase(UTC_29_FEB_2000 - TZ_ADJUST);
+
+  
+  addDateTestCase(TIME_NOW);
+  addDateTestCase(TIME_NOW - TZ_ADJUST);
+
+  
+  addDateTestCase(UTC_01_JAN_2005);
+  addDateTestCase(UTC_01_JAN_2005 - 1000);
+  addDateTestCase(UTC_01_JAN_2005 - TZ_ADJUST);
+
+  test();
+
+  function addTestCase() {
+    new TestCase(
+      status,
+      expect,
+      actual);
+  }
+
+  function addDateTestCase(date_given_in_milliseconds) {
+    var s = 'new Date(' +  date_given_in_milliseconds + ')';
+    givenDate = new Date(date_given_in_milliseconds);
+
+    status = 'd = ' + s +
+      '; d == new Date(d.toDateString() + " " + d.toLocaleTimeString())';
+    expect = givenDate.toString();
+    actual = new Date(givenDate.toDateString() +
+                      ' ' + givenDate.toLocaleTimeString()).toString();
+    addTestCase();
+  }
+});
