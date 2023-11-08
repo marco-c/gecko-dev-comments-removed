f0c4e1b50de3712cf7873cdb8108f7e7f7ff7f67
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-11-08 15:26:43 +0000

    Bug 1856109 - Allow <dialog> to move back to previously focused element even if they are in different BC r=emilio
    Differential Revision: https://phabricator.services.mozilla.com/D190957
    
    UltraBlame original commit: 6e6cd11123b77ea33b4ebb340c02d384d3d8358a

--- dom/html/HTMLDialogElement.cpp
+++ dom/html/HTMLDialogElement.cpp
@@ -88,10 +88,15 @@ void HTMLDialogElement::RemoveFromTopLayerIfNeeded() {
 }
 
 void HTMLDialogElement::StorePreviouslyFocusedElement() {
-  if (Document* doc = GetComposedDoc()) {
+  if (Element* element = nsFocusManager::GetFocusedElementStatic()) {
+    if (NS_SUCCEEDED(nsContentUtils::CheckSameOrigin(this, element))) {
+      mPreviouslyFocusedElement = do_GetWeakReference(element);
+    }
+  } else if (Document* doc = GetComposedDoc()) {
+    
+    
     if (nsIContent* unretargetedFocus = doc->GetUnretargetedFocusedContent()) {
-      mPreviouslyFocusedElement =
-          do_GetWeakReference(unretargetedFocus->AsElement());
+      mPreviouslyFocusedElement = do_GetWeakReference(element);
     }
   }
 }
