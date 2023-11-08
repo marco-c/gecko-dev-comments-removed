7fced45dd8454301543c49f3cf7554a2f86f97cf
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-11-08 16:06:54 +0000

    Bug 1851816: Add tests for RFP in PBMode and FPP in Regular r=timhuang
    It fails without any patch as expected.
    
    Differential Revision: https://phabricator.services.mozilla.com/D192500
    
    UltraBlame original commit: 7d0d2fe95e67030be4c72415514b8ded198c43f7

--- browser/components/resistfingerprinting/test/browser/browser_hwconcurrency_popups_data.js
+++ browser/components/resistfingerprinting/test/browser/browser_hwconcurrency_popups_data.js
@@ -65,3 +65,9 @@ add_task(testA.bind(null, uri, testHWConcurrency, expectedResults));
 
 expectedResults = structuredClone(allSpoofed);
 add_task(testE.bind(null, uri, testHWConcurrency, expectedResults));
+
+
+expectedResults = structuredClone(allNotSpoofed);
+add_task(
+  simpleRFPPBMFPPTest.bind(null, uri, testHWConcurrency, expectedResults)
+);
--- browser/components/resistfingerprinting/test/browser/browser_hwconcurrency_popups_noopener.js
+++ browser/components/resistfingerprinting/test/browser/browser_hwconcurrency_popups_noopener.js
@@ -87,3 +87,15 @@ add_task(testE.bind(null, uri, testHWConcurrency, expectedResults, extraData));
 
 expectedResults = structuredClone(allNotSpoofed);
 add_task(testG.bind(null, uri, testHWConcurrency, expectedResults, extraData));
+
+
+expectedResults = structuredClone(allNotSpoofed);
+add_task(
+  simpleRFPPBMFPPTest.bind(
+    null,
+    uri,
+    testHWConcurrency,
+    expectedResults,
+    extraData
+  )
+);
