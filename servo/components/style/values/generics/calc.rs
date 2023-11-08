18d9721acc20382ee2739db11ba69fb0437e90ea
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-11-08 15:45:29 +0000

    Bug 1860205 - Swap Up/Down rounding strategy when negating a Round calc node. r=firefox-style-system-reviewers,emilio
    Differential Revision: https://phabricator.services.mozilla.com/D192742
    
    UltraBlame original commit: db7079f0040895bafac59b0494cb660f4137f533

--- servo/components/style/values/generics/calc.rs
+++ servo/components/style/values/generics/calc.rs
@@ -566,10 +566,23 @@ impl<L: CalcNodeLeaf> CalcNode<L> {
                 }
             },
             CalcNode::Round {
+                ref mut strategy,
                 ref mut value,
                 ref mut step,
-                ..
             } => {
+                match *strategy {
+                    RoundingStrategy::Nearest => {
+                        
+                        
+                        
+                        
+                        wrap_self_in_negate(self);
+                        return;
+                    },
+                    RoundingStrategy::Up => *strategy = RoundingStrategy::Down,
+                    RoundingStrategy::Down => *strategy = RoundingStrategy::Up,
+                    RoundingStrategy::ToZero => (),
+                }
                 value.negate();
                 step.negate();
             },
