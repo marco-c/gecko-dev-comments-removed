49edc93198542665b91e20d9d420d9e0906844c7
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-11-08 15:53:23 +0000

    Bug 1831236 - Increase shutdown wait time for debug builds. r=jld
    As we are seeing test failures that suggest that proper shutdown
    sometimes requires extra time in debug builds, let's try to upgrade
    kShutdownWaitMs from 8 to 20 seconds for those.
    
    Differential Revision: https://phabricator.services.mozilla.com/D192694
    
    UltraBlame original commit: 4cbecf0c0844d0bde111bf4b7b3ee2ea374ee695

--- ipc/chromium/src/chrome/common/process_watcher_win.cc
+++ ipc/chromium/src/chrome/common/process_watcher_win.cc
@@ -28,7 +28,7 @@ static constexpr DWORD kShutdownWaitMs = 80000;
 
 static constexpr DWORD kShutdownWaitMs = 40000;
 #else
-static constexpr DWORD kShutdownWaitMs = 8000;
+static constexpr DWORD kShutdownWaitMs = 20000;
 #endif
 
 namespace {
