ea6cd3bfd88bd716854e0c833685ddc55c664619
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-11-16 00:35:09 +0000

    Bug 1863391 - tests. r=jonco
    Differential Revision: https://phabricator.services.mozilla.com/D193176
    
    UltraBlame original commit: 0c78e4ad365987822f59322a434aa60c25617fc1

---
+++ js/src/jit-test/tests/collections/bug-1863391-1.js
@@ -0,0 +1,8 @@
+
+evalInWorker(`
+  a = new WeakMap
+  b = Symbol
+  a.set(b )
+  c = b.hasInstance;
+  a.get(c)
+`);
---
+++ js/src/jit-test/tests/collections/bug-1863391-2.js
@@ -0,0 +1,6 @@
+
+evalInWorker(`
+  a = new WeakMap
+  b = Symbol.hasInstance;
+  a.set(b, 2);
+`);
