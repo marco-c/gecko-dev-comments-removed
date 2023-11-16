87cf33918a83ae5de1f6c7473159a2ade12f0b9e
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-11-16 01:00:01 +0000

    Bug 1861973 - Signedness confusion in js::wasm::PackedTypeCode::pack.  r=rhunt.
    `PackedTypeCode::pack` stores a pointer in an unsigned 48-bit field,
    PackedRepr typeDef_ : TypeDefBits, where TypeDefbits == 48.
    
    This is problematic, although in release builds OK, on both 32- and 64-bit
    targets:
    
    * on an arm32 bit target (native, not simulator), the assertion
      `MOZ_ASSERT((uint64_t)typeDef <= ((uint64_t)1 << TypeDefBits) - 1)` has been
      observed to fail. The assertion assumes that `(uint64_t)typeDef` unsignedly
      widens typeDef to 64 bits; but that is implementation dependent and on this
      target it is signedly widened, which causes the assertion to fail if bit 31
      of `typeDef` is 1.
    
    * on 64 bit targets, TypeDef::typeDef reconstitutes the pointer by unsignedly
      widening the 48 bit field back out to 64 bits. That works OK because, at
      least for "canonical addresses" on x86_64, only kernel addresses exist in
      the high-half canonical space. So we'll never encounter them. But it's
      conceptually confusing because existing literature, and our own practices
      (eg ds/PointerAndUint7) regard 64-bit canonical addresses as split equally
      high and low -- that is, as signed.
    
    This patch:
    
    * on 32 bit targets, removes the failing assertion (it is pointless)
    
    * on 64 bit targets, fixes up the assertion and also `PackedTypeCode::typeDef`
      to treat the stored value as a sighed 48 bit entity.  Despite the presence
      of more code, this routine becomes cheaper on x86_64: previously it required
      3 insns and 2 regs.  Now it requires 3 insns and 1 reg.
    
    As a passing observation, if `PackedTypeCode::typeCodeAbstracted` is really as
    hot as its comment claims, it would be better -- at least on x86_64 - to place
    `typeCode_` either at the start or end of the union.  Extracting bits from the
    middle of a 64-bit word requires 2 insns, a shift and a mask; but if the field
    is at the top or bottom it would require respectively only a shift (top) or a
    mask (bottom).
    
    Differential Revision: https://phabricator.services.mozilla.com/D192354
    
    UltraBlame original commit: 6f3be95d65116af346971c9a559f77dbb8e9e14a

--- js/src/wasm/WasmValType.h
+++ js/src/wasm/WasmValType.h
@@ -88,9 +88,17 @@ union PackedTypeCode {
     MOZ_ASSERT(uint32_t(tc) <= ((1 << TypeCodeBits) - 1));
     MOZ_ASSERT_IF(tc != AbstractTypeRefCode, typeDef == nullptr);
     MOZ_ASSERT_IF(tc == AbstractTypeRefCode, typeDef != nullptr);
+#if defined(JS_64BIT) && defined(DEBUG)
     
     
-    MOZ_ASSERT((uint64_t)typeDef <= ((uint64_t)1 << TypeDefBits) - 1);
+    
+    
+    
+    int64_t w = int64_t(typeDef);
+    w <<= (64 - TypeDefBits);
+    w >>= (64 - TypeDefBits);
+    MOZ_ASSERT(w == int64_t(typeDef));
+#endif
     PackedTypeCode ptc = {};
     ptc.typeCode_ = PackedRepr(tc);
     ptc.typeDef_ = (uintptr_t)typeDef;
@@ -141,7 +149,17 @@ union PackedTypeCode {
 
   const TypeDef* typeDef() const {
     MOZ_ASSERT(isValid());
+#if defined(JS_64BIT)
+    
+    static_assert(sizeof(int64_t) == sizeof(uintptr_t));
+    int64_t w = int64_t(typeDef_);
+    w <<= (64 - TypeDefBits);
+    w >>= (64 - TypeDefBits);
+    return (const TypeDef*)(uintptr_t)w;
+#else
+    
     return (const TypeDef*)(uintptr_t)typeDef_;
+#endif
   }
 
   bool isNullable() const {
