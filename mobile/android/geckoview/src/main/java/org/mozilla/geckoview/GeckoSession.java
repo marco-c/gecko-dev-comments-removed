4f649795c596ec780e1fb3acf57beea145cede8c
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-11-16 00:35:11 +0000

    Backed out changeset a9c17170dc67 (bug 1837601) for causing android related bustages CLOSED TREE
    UltraBlame original commit: 29abb2ec3bc594b3a3eb2b93944290b4e58adb4e

--- mobile/android/geckoview/src/main/java/org/mozilla/geckoview/GeckoSession.java
+++ mobile/android/geckoview/src/main/java/org/mozilla/geckoview/GeckoSession.java
@@ -684,11 +684,7 @@ public class GeckoSession {
               final GeckoBundle[] perms = message.getBundleArray("permissions");
               final List<PermissionDelegate.ContentPermission> permList =
                   PermissionDelegate.ContentPermission.fromBundleArray(perms);
-              delegate.onLocationChange(
-                  GeckoSession.this,
-                  message.getString("uri"),
-                  permList,
-                  message.getBoolean("hasUserGesture"));
+              delegate.onLocationChange(GeckoSession.this, message.getString("uri"), permList);
             }
             delegate.onCanGoBack(GeckoSession.this, message.getBoolean("canGoBack"));
             delegate.onCanGoForward(GeckoSession.this, message.getBoolean("canGoForward"));
@@ -4835,28 +4831,10 @@ public class GeckoSession {
 
 
     @UiThread
-    @Deprecated
-    @DeprecationSchedule(id = "geckoview-onlocationchange", version = 123)
     default void onLocationChange(
         @NonNull GeckoSession session,
         @Nullable String url,
         final @NonNull List<PermissionDelegate.ContentPermission> perms) {}
-
-    
-
-
-
-
-
-
-
-
-    @UiThread
-    default void onLocationChange(
-        @NonNull GeckoSession session,
-        @Nullable String url,
-        final @NonNull List<PermissionDelegate.ContentPermission> perms,
-        final @NonNull Boolean hasUserGesture) {}
 
     
 
