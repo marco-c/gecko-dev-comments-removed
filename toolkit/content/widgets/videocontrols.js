ca795374cfd659127232fdfdb5e30cb497075ea1
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-11-16 00:32:28 +0000

    Bug 1858977 - Remove dead code to lock orientation on video controls. r=smaug,webidl,reusable-components-reviewers,mconley,hjones
    mozLockOrientation etc haven't done anything for a while.
    
    Depends on D193212
    
    Differential Revision: https://phabricator.services.mozilla.com/D193213
    
    UltraBlame original commit: 9565b6ba1cc8879157c00d3beb4816c27ab4175f

--- toolkit/content/widgets/videocontrols.js
+++ toolkit/content/widgets/videocontrols.js
@@ -428,10 +428,6 @@ this.VideoControlsImplWidget = class {
 
         
         
-        this.updateOrientationState(this.isVideoInFullScreen);
-
-        
-        
         
         if (this.document.fullscreenElement) {
           this.videocontrols.setAttribute("inDOMFullscreen", true);
@@ -1754,43 +1750,11 @@ this.VideoControlsImplWidget = class {
           this.videocontrols.removeAttribute("inDOMFullscreen");
         }
 
-        this.updateOrientationState(this.isVideoInFullScreen);
-
         if (this.isVideoInFullScreen) {
           this.startFadeOut(this.controlBar, true);
         }
 
         this.setFullscreenButtonState();
-      },
-
-      updateOrientationState(lock) {
-        if (!this.video.mozOrientationLockEnabled) {
-          return;
-        }
-        if (lock) {
-          if (this.video.mozIsOrientationLocked) {
-            return;
-          }
-          let dimenDiff = this.video.videoWidth - this.video.videoHeight;
-          if (dimenDiff > 0) {
-            this.video.mozIsOrientationLocked =
-              this.window.screen.mozLockOrientation("landscape");
-          } else if (dimenDiff < 0) {
-            this.video.mozIsOrientationLocked =
-              this.window.screen.mozLockOrientation("portrait");
-          } else {
-            this.video.mozIsOrientationLocked =
-              this.window.screen.mozLockOrientation(
-                this.window.screen.orientation
-              );
-          }
-        } else {
-          if (!this.video.mozIsOrientationLocked) {
-            return;
-          }
-          this.window.screen.mozUnlockOrientation();
-          this.video.mozIsOrientationLocked = false;
-        }
       },
 
       clickToPlayClickHandler(e) {
@@ -2964,7 +2928,6 @@ this.VideoControlsImplWidget = class {
   teardown() {
     this.Utils.terminate();
     this.TouchUtils.terminate();
-    this.Utils.updateOrientationState(false);
     this.l10n.disconnectRoot(this.shadowRoot);
     this.l10n = null;
   }
