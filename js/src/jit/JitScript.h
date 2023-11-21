1d573941e94926e4b82e90987485e65e0eb75c52
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-11-21 12:44:46 +0000

    Bug 1847258: Save the warm up counter when the last IC stub is attached. r=iain
    Differential Revision: https://phabricator.services.mozilla.com/D192878
    
    UltraBlame original commit: 0f9bc02e30c3521b2651d4f1c95077c77ddb4f23

--- js/src/jit/JitScript.h
+++ js/src/jit/JitScript.h
@@ -332,6 +332,10 @@ class alignas(uintptr_t) JitScript final
   LifoAlloc allocSitesSpace_{AllocSiteChunkSize};
   Vector<gc::AllocSite*, 0, SystemAllocPolicy> allocSites_;
 
+  
+  
+  uint32_t warmUpCountAtLastICStub_ = 0;
+
   ICScript icScript_;
   
 
@@ -428,6 +432,9 @@ class alignas(uintptr_t) JitScript final
   gc::AllocSite* createAllocSite(JSScript* script);
 
   bool resetAllocSites(bool resetNurserySites, bool resetPretenuredSites);
+
+  void updateLastICStubCounter() { warmUpCountAtLastICStub_ = warmUpCount(); }
+  uint32_t warmUpCountAtLastICStub() const { return warmUpCountAtLastICStub_; }
 
  private:
   
