f9297e562ff9b0601683ccdad367a22951ac8260
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-11-21 12:25:19 +0000

    Bug 1864985: Add hasUserPass attribute to nsIURI r=necko-reviewers,valentin
    We will remove the user and pass from the URL for Histories and Favicons using
    nsIOService::CreateExposableURI()[1], then store them to the database. And, for
    Favicons. And, for Favicons, we will also read them. Therefore, as the
    frequency calling CreateExposableURI() will be higher, we want to optimize it.
    Currently, CreateExposableURI() gets/copies the String of UserPass to check whether there is a user pass info in the URL[2]. Its copying will be waste. To
    prevent it, implement hasUserPass in nsIURI, and refer to it instead.
    
    [1] https://searchfox.org/mozilla-central/rev/90dce6b0223b4dc17bb10f1125b44f70951585f9/netwerk/base/nsIOService.cpp#1031
    [2] https://searchfox.org/mozilla-central/rev/90dce6b0223b4dc17bb10f1125b44f70951585f9/netwerk/base/nsIOService.cpp#1017
    
    Differential Revision: https://phabricator.services.mozilla.com/D193623
    
    UltraBlame original commit: 10a60139ccb5be1f89b23f02b4011f777eb6a319

--- netwerk/base/nsStandardURL.cpp
+++ netwerk/base/nsStandardURL.cpp
@@ -2980,6 +2980,12 @@ nsStandardURL::GetHasRef(bool* result) {
   return NS_OK;
 }
 
+NS_IMETHODIMP
+nsStandardURL::GetHasUserPass(bool* result) {
+  *result = (mUsername.mLen >= 0) || (mPassword.mLen >= 0);
+  return NS_OK;
+}
+
 
 NS_IMETHODIMP
 nsStandardURL::GetDirectory(nsACString& result) {
