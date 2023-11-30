94fc39bd725c1a276d9e836c09f170ef433ed8e1
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-11-30 15:46:30 +0000

    Bug 1864168 - Part 4: Use 1-origin column number for WebSocket-related console message. r=smaug,devtools-reviewers,ochameau
    Differential Revision: https://phabricator.services.mozilla.com/D193372
    
    UltraBlame original commit: d4db9576559b1a4d629554fa7c6190956a8e1e21

--- dom/websocket/WebSocket.cpp
+++ dom/websocket/WebSocket.cpp
@@ -134,7 +134,7 @@ class WebSocketImpl final : public nsIInterfaceRequestor,
         mCloseEventCode(nsIWebSocketChannel::CLOSE_ABNORMAL),
         mPort(0),
         mScriptLine(0),
-        mScriptColumn(0),
+        mScriptColumn(1),
         mInnerWindowID(0),
         mPrivateBrowsing(false),
         mIsChromeContext(false),
@@ -1382,7 +1382,7 @@ already_AddRefed<WebSocket> WebSocket::ConstructorCommon(
         workerPrivate, webSocketImpl,
         workerPrivate->GlobalScope()->GetClientInfo(), !!aTransportProvider,
         aUrl, protocolArray, nsDependentCString(file.get()), lineno,
-        column.zeroOriginValue());
+        column.oneOriginValue());
     runnable->Dispatch(Canceling, aRv);
     if (NS_WARN_IF(aRv.Failed())) {
       return nullptr;
@@ -1615,7 +1615,7 @@ nsresult WebSocketImpl::Init(JSContext* aCx, bool aIsSecure,
     if (JS::DescribeScriptedCaller(aCx, &file, &lineno, &column)) {
       mScriptFile = file.get();
       mScriptLine = lineno;
-      mScriptColumn = column.zeroOriginValue();
+      mScriptColumn = column.oneOriginValue();
     }
   }
 
