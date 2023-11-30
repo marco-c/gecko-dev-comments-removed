669f42d4d44ba1f0f8b46a8eef7cd80cb1662111
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-11-30 16:47:32 +0000

    Backed out changeset aa34b45b5cda (bug 1866142) for causing bc failures on browser_keyboard_navigation.js CLOSED TREE
    UltraBlame original commit: 0cba1f19245cc18880528fbc5c5e0e64099adde1

--- browser/components/reportbrokensite/test/browser/head.js
+++ browser/components/reportbrokensite/test/browser/head.js
@@ -190,15 +190,11 @@ class ReportBrokenSiteHelper {
     );
   }
 
-  waitForSendMoreInfoTab() {
-    return BrowserTestUtils.waitForNewTab(
+  async clickSendMoreInfo() {
+    const newTabPromise = BrowserTestUtils.waitForNewTab(
       this.win.gBrowser,
       NEW_REPORT_ENDPOINT_TEST_URL
     );
-  }
-
-  async clickSendMoreInfo() {
-    const newTabPromise = this.waitForSendMoreInfoTab();
     EventUtils.synthesizeMouseAtCenter(this.sendMoreInfoLink, {}, this.win);
     const newTab = await newTabPromise;
     const receivedData = await SpecialPowers.spawn(
@@ -437,10 +433,6 @@ class MenuHelper {
     this.win = win;
   }
 
-  get showsBackButton() {
-    return true;
-  }
-
   get reportBrokenSite() {}
 
   get reportSiteIssue() {}
@@ -566,7 +558,7 @@ class AppMenuHelpSubmenuHelper extends MenuHelper {
 
     const appMenuHelpSubview =
       this.win.document.getElementById("PanelUI-helpView");
-    await BrowserTestUtils.waitForEvent(appMenuHelpSubview, "ViewShown");
+    await BrowserTestUtils.waitForEvent(appMenuHelpSubview, "ViewShowing");
   }
 
   async close() {
@@ -578,10 +570,6 @@ class AppMenuHelpSubmenuHelper extends MenuHelper {
 
 class HelpMenuHelper extends MenuHelper {
   menuDescription = "Help Menu";
-
-  get showsBackButton() {
-    return false;
-  }
 
   get reportBrokenSite() {
     return this.win.PanelMultiView.getViewNode(
@@ -600,20 +588,37 @@ class HelpMenuHelper extends MenuHelper {
   get popup() {
     return this.win.PanelMultiView.getViewNode(
       this.win.document,
-      "PanelUI-helpView"
-    );
-  }
-
-  get helpMenu() {
-    return this.win.PanelMultiView.getViewNode(
-      this.win.document,
       "menu_HelpPopup"
     );
   }
 
+  async open() {
+    const popup = this.popup;
+    const promise = BrowserTestUtils.waitForEvent(popup, "popupshown");
+
+    
+    
+    
+    popup.dispatchEvent(new MouseEvent("popupshowing", { bubbles: true }));
+    popup.dispatchEvent(new MouseEvent("popupshown", { bubbles: true }));
+
+    await promise;
+  }
+
+  async close() {
+    const popup = this.popup;
+    const promise = BrowserTestUtils.waitForEvent(popup, "popuphidden");
+
+    
+    
+    
+    popup.dispatchEvent(new MouseEvent("popuphiding", { bubbles: true }));
+    popup.dispatchEvent(new MouseEvent("popuphidden", { bubbles: true }));
+
+    await promise;
+  }
+
   async openReportBrokenSite() {
-    
-    
     
     await this.open();
     const shownPromise = BrowserTestUtils.waitForEvent(
@@ -622,35 +627,14 @@ class HelpMenuHelper extends MenuHelper {
       true,
       e => e.target.classList.contains("report-broken-site-view")
     );
-    this.reportBrokenSite.click();
+    ReportBrokenSite.open({
+      sourceEvent: {
+        target: this.reportBrokenSite,
+      },
+      view: this.win,
+    });
     await shownPromise;
     return new ReportBrokenSiteHelper(this);
-  }
-
-  async open() {
-    const { helpMenu } = this;
-    const promise = BrowserTestUtils.waitForEvent(helpMenu, "popupshown");
-
-    
-    
-    
-    helpMenu.dispatchEvent(new MouseEvent("popupshowing", { bubbles: true }));
-    helpMenu.dispatchEvent(new MouseEvent("popupshown", { bubbles: true }));
-
-    await promise;
-  }
-
-  async close() {
-    const { helpMenu } = this;
-    const promise = BrowserTestUtils.waitForEvent(helpMenu, "popuphidden");
-
-    
-    
-    
-    helpMenu.dispatchEvent(new MouseEvent("popuphiding", { bubbles: true }));
-    helpMenu.dispatchEvent(new MouseEvent("popuphidden", { bubbles: true }));
-
-    await promise;
   }
 }
 
