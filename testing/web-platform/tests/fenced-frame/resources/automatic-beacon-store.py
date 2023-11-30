6ffa507fc5df12ef344d969c117bd631c1300ad4
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-11-30 16:24:53 +0000

    Bug 1865912 [wpt PR 43271] - Fenced frames: Fix reportEvent request initiator, a=testonly
    Automatic update from web-platform-tests
    Fenced frames: Fix reportEvent request initiator
    
    The request initiator for beacons is now whoever chose the destination
    URL:
    * For destination enum beacons (including all automatic beacons), this
      is the reporting worklet's origin.
    * For destination URL beacons (with macro substitution), this is the
      reportEvent caller's origin.
    
    Bug: 1498474
    Change-Id: I426ac8a05f8c197669df72fb6a82394029993029
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5037514
    Reviewed-by: Dominic Farolino <domchromium.org>
    Reviewed-by: Matt Menke <mmenkechromium.org>
    Commit-Queue: Garrett Tanzer <gtanzerchromium.org>
    Cr-Commit-Position: refs/heads/main{#1227504}
    
    --
    
    wpt-commits: 88636307328b801137dafe43b41d5480a071e17e
    wpt-pr: 43271
    
    UltraBlame original commit: ae8a62684da176f8aae8a6837f4c48ca95c9288e

--- testing/web-platform/tests/fenced-frame/resources/automatic-beacon-store.py
+++ testing/web-platform/tests/fenced-frame/resources/automatic-beacon-store.py
@@ -31,7 +31,8 @@ def main(request, response):
         
         if request.method == "POST":
             request_body = request.body or NO_DATA_STRING
-            stash.put(string_to_uuid(request_body), request_body)
+            request_headers = request.headers.get("Origin") or NO_DATA_STRING
+            stash.put(string_to_uuid(request_body), request_headers)
             return (200, [], b"")
 
         
