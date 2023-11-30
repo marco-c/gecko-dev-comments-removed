fcfcb5e42f3ddb75167450a88e03008e5fe750d8
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-11-30 16:04:42 +0000

    Bug 1860958 - Implement Texture::Destroy. r=webgpu-reviewers,jimb
    Differential Revision: https://phabricator.services.mozilla.com/D192080
    
    UltraBlame original commit: 2b8905730799b7480cb335dde06a22185cadbadc

--- dom/webgpu/Texture.cpp
+++ dom/webgpu/Texture.cpp
@@ -45,17 +45,25 @@ Texture::Texture(Device* const aParent, RawId aId,
   MOZ_RELEASE_ASSERT(aId);
 }
 
-Texture::~Texture() { Cleanup(); }
-
 void Texture::Cleanup() {
-  if (mValid && mParent) {
-    mValid = false;
-    auto bridge = mParent->GetBridge();
-    if (bridge && bridge->IsOpen()) {
-      bridge->SendTextureDrop(mId);
-    }
+  if (!mParent) {
+    return;
   }
+
+  auto bridge = mParent->GetBridge();
+  if (bridge && bridge->IsOpen()) {
+    bridge->SendTextureDrop(mId);
+  }
+
+  
+  
+  
+  
+  
+  mParent = nullptr;
 }
+
+Texture::~Texture() { Cleanup(); }
 
 already_AddRefed<TextureView> Texture::CreateView(
     const dom::GPUTextureViewDescriptor& aDesc) {
@@ -70,12 +78,10 @@ already_AddRefed<TextureView> Texture::CreateView(
 }
 
 void Texture::Destroy() {
-  
-  
-
-  
+  auto bridge = mParent->GetBridge();
+  if (bridge && bridge->IsOpen()) {
+    bridge->SendTextureDestroy(mId, mParent->GetId());
+  }
 }
-
-void Texture::ForceDestroy() { Cleanup(); }
 
 }  
