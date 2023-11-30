b338af7435f0dbadb0ab73b0e5f4b030c03e50f5
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-11-30 16:11:25 +0000

    Bug 1866063 - Prevent flickering of the source editor by hiding it till loaded. r=devtools-reviewers,nchevobbe
    Much like we we do here:
    
      https://searchfox.org/mozilla-central/rev/d7f837add602d270f2b2958b3ab5206dc85965c0/devtools/client/framework/toolbox.js#2605
    
    This is not an ideal fix but it fixes the immediate flicker.
    
    The real fix would be addressing
    https://github.com/w3c/csswg-drafts/issues/9624
    
    Depends on D194395
    
    Differential Revision: https://phabricator.services.mozilla.com/D194396
    
    UltraBlame original commit: 8de13ec9dc9397cb816d233753e77ac5c3f94f9f

--- devtools/client/shared/sourceeditor/editor.js
+++ devtools/client/shared/sourceeditor/editor.js
@@ -339,12 +339,16 @@ Editor.prototype = {
       }
 
       const onLoad = () => {
+        
+        
+        env.style.visibility = "";
         const win = env.contentWindow.wrappedJSObject;
         this.container = env;
         this._setup(win.document.body, el.ownerDocument);
         resolve();
       };
 
+      env.style.visibility = "hidden";
       env.addEventListener("load", onLoad, { capture: true, once: true });
       env.src = CM_IFRAME;
       el.appendChild(env);
