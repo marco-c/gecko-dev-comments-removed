923ca771171b7c856648d39e6818f623ba4a9555
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-11-30 15:16:47 +0000

    Bug 1865256 - Add a telemetry probe to collect IME names which inserted CRLFs on Windows r=chutten,m_kato,win-reviewers,mhowell
    It'll be notify IMEs on Windows of text/selection with only LFs instead of
    CRLFs because the conversion cost from LFs to CRLFs is serious for DOM tree
    change performance and Chrome also uses only LFs too.
    
    However, we know some old TIPs inserted CRLFs.  TIPs should use emulating
    `Enter` key press instead for the issue, "insert paragraph" vs.
    "insert line break" in rich text editors, some IMEs may still insert CRLFs
    directly.  However, nobody knows whether there is such TIPs.
    
    Therefore, let's collect the IME names which inserted CRLFs via TSF directly.
    
    Differential Revision: https://phabricator.services.mozilla.com/D193879
    
    UltraBlame original commit: d7fd417affa6fd884339c722c0be7c53d3733ef1

--- widget/windows/TSFTextStore.cpp
+++ widget/windows/TSFTextStore.cpp
@@ -1033,6 +1033,40 @@ class TSFStaticSink final : public ITfInputProcessorProfileActivationSink {
     return sInstance->mActiveTIP;
   }
 
+  static bool GetActiveTIPNameForTelemetry(nsAString& aName) {
+    if (!sInstance || !sInstance->EnsureInitActiveTIPKeyboard()) {
+      return false;
+    }
+    if (sInstance->mActiveTIPGUID == GUID_NULL) {
+      aName.Truncate();
+      aName.AppendPrintf("0x%04X", sInstance->mLangID);
+      return true;
+    }
+    
+    
+    
+    
+    
+    
+    aName.Truncate();
+    aName.AppendPrintf("0x%04X|", sInstance->mLangID);
+    nsAutoString description;
+    description.Assign(sInstance->mActiveTIPKeyboardDescription);
+    static const uint32_t kMaxDescriptionLength = 72 - aName.Length();
+    if (description.Length() > kMaxDescriptionLength) {
+      if (NS_IS_LOW_SURROGATE(description[kMaxDescriptionLength - 1]) &&
+          NS_IS_HIGH_SURROGATE(description[kMaxDescriptionLength - 2])) {
+        description.Truncate(kMaxDescriptionLength - 2);
+      } else {
+        description.Truncate(kMaxDescriptionLength - 1);
+      }
+      
+      description.Append(char16_t(0x2026));
+    }
+    aName.Append(description);
+    return true;
+  }
+
   static bool IsMSChangJieOrMSQuickActive() {
     
     
@@ -1590,20 +1624,7 @@ TSFStaticSink::OnActivated(DWORD dwProfileType, LANGID langid, REFCLSID rclsid,
       
       
       nsAutoString key;
-      key.AppendPrintf("0x%04X|", mLangID);
-      nsAutoString description(mActiveTIPKeyboardDescription);
-      static const uint32_t kMaxDescriptionLength = 72 - key.Length();
-      if (description.Length() > kMaxDescriptionLength) {
-        if (NS_IS_LOW_SURROGATE(description[kMaxDescriptionLength - 1]) &&
-            NS_IS_HIGH_SURROGATE(description[kMaxDescriptionLength - 2])) {
-          description.Truncate(kMaxDescriptionLength - 2);
-        } else {
-          description.Truncate(kMaxDescriptionLength - 1);
-        }
-        
-        description.Append(char16_t(0x2026));
-      }
-      key.Append(description);
+      TSFStaticSink::GetActiveTIPNameForTelemetry(key);
       Telemetry::ScalarSet(Telemetry::ScalarID::WIDGET_IME_NAME_ON_WINDOWS, key,
                            true);
     }
@@ -5174,6 +5195,26 @@ bool TSFTextStore::InsertTextAtSelectionInternal(const nsAString& aInsertStr,
          "destroyed during dispatching a keyboard event",
          this));
     return false;
+  }
+
+  const auto numberOfCRLFs = [&]() -> uint32_t {
+    const auto* str = aInsertStr.BeginReading();
+    uint32_t num = 0;
+    for (uint32_t i = 0; i + 1 < aInsertStr.Length(); i++) {
+      if (str[i] == '\r' && str[i + 1] == '\n') {
+        num++;
+        i++;
+      }
+    }
+    return num;
+  }();
+  if (numberOfCRLFs) {
+    nsAutoString key;
+    if (TSFStaticSink::GetActiveTIPNameForTelemetry(key)) {
+      Telemetry::ScalarSet(
+          Telemetry::ScalarID::WIDGET_IME_NAME_ON_WINDOWS_INSERTED_CRLF, key,
+          true);
+    }
   }
 
   TS_SELECTION_ACP oldSelection = contentForTSF->Selection()->ACPRef();
