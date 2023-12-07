2548cd917cf507800266d89bf094a346ca6da5b3
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-12-07 15:46:14 +0000

    Backed out 2 changesets (bug 1865199) for causing bc failures on browser_partitioned_telemetry.js CLOSED TREE
    Backed out changeset 2ec416e3c7f7 (bug 1865199)
    Backed out changeset 92f07e8c3190 (bug 1865199)
    
    UltraBlame original commit: 1bdf2f1948ad3f85aafa4d44c7c84d534602ce41

--- netwerk/cookie/CookieService.cpp
+++ netwerk/cookie/CookieService.cpp
@@ -853,7 +853,7 @@ CookieService::AddNative(const nsACString& aHost, const nsACString& aPath,
   CookieStruct cookieData(nsCString(aName), nsCString(aValue), nsCString(aHost),
                           nsCString(aPath), aExpiry, currentTimeInUsec,
                           Cookie::GenerateUniqueCreationTime(currentTimeInUsec),
-                          aIsHttpOnly, aIsSession, aIsSecure, false, aSameSite,
+                          aIsHttpOnly, aIsSession, aIsSecure, aSameSite,
                           aSameSite, aSchemeMap);
 
   RefPtr<Cookie> cookie = Cookie::Create(cookieData, key.mOriginAttributes);
@@ -1126,21 +1126,6 @@ static void RecordUnicodeTelemetry(const CookieStruct& cookieData) {
   Telemetry::AccumulateCategorical(label);
 }
 
-static void RecordPartitionedTelemetry(const CookieStruct& aCookieData,
-                                       bool aIsForeign) {
-  mozilla::glean::networking::set_cookie.Add(1);
-  if (aCookieData.isPartitioned()) {
-    mozilla::glean::networking::set_cookie_partitioned.AddToNumerator(1);
-  }
-  if (aIsForeign) {
-    mozilla::glean::networking::set_cookie_foreign.AddToNumerator(1);
-  }
-  if (aIsForeign && aCookieData.isPartitioned()) {
-    mozilla::glean::networking::set_cookie_foreign_partitioned.AddToNumerator(
-        1);
-  }
-}
-
 
 
 bool CookieService::CanSetCookie(
@@ -1213,12 +1198,6 @@ bool CookieService::CanSetCookie(
   }
 
   RecordUnicodeTelemetry(aCookieData);
-
-  
-  
-  if (XRE_IsParentProcess() || !aFromHttp) {
-    RecordPartitionedTelemetry(aCookieData, aIsForeignAndNotAddon);
-  }
 
   if (!CookieCommons::CheckName(aCookieData)) {
     COOKIE_LOGFAILURE(SET_COOKIE, aHostURI, savedCookieHeader,
@@ -1526,7 +1505,6 @@ bool CookieService::ParseAttributes(nsIConsoleReportCollector* aCRC,
   static const char kSameSiteLax[] = "lax";
   static const char kSameSiteNone[] = "none";
   static const char kSameSiteStrict[] = "strict";
-  static const char kPartitioned[] = "partitioned";
 
   nsACString::const_char_iterator cookieStart;
   aCookieHeader.BeginReading(cookieStart);
@@ -1579,10 +1557,6 @@ bool CookieService::ParseAttributes(nsIConsoleReportCollector* aCRC,
       
     } else if (tokenString.LowerCaseEqualsLiteral(kSecure)) {
       aCookieData.isSecure() = true;
-
-      
-    } else if (tokenString.LowerCaseEqualsLiteral(kPartitioned)) {
-      aCookieData.isPartitioned() = true;
 
       
       
