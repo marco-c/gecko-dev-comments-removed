9270db3373e231eb77241b64444810002ca5dc52
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-12-07 15:43:40 +0000

    Bug 1867527 - Remove unused argument aPrincipal from DragDataProducer::Produce; r=smaug
    Differential Revision: https://phabricator.services.mozilla.com/D195106
    
    UltraBlame original commit: 66cef17571c0c05107acdae1553a43566a5a69a3

--- dom/base/nsContentAreaDragDrop.h
+++ dom/base/nsContentAreaDragDrop.h
@@ -50,15 +50,13 @@ class nsContentAreaDragDrop {
 
 
 
-
-
   static nsresult GetDragData(nsPIDOMWindowOuter* aWindow, nsIContent* aTarget,
                               nsIContent* aSelectionTargetNode,
                               bool aIsAltKeyPressed,
                               mozilla::dom::DataTransfer* aDataTransfer,
                               bool* aCanDrag,
                               mozilla::dom::Selection** aSelection,
-                              nsIContent** aDragNode, nsIPrincipal** aPrincipal,
+                              nsIContent** aDragNode,
                               nsIContentSecurityPolicy** aCsp,
                               nsICookieJarSettings** aCookieJarSettings);
 };
