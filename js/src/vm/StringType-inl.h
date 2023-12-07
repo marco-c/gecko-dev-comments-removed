c0d86d5d11ba5903e4dfc77ed64143bb59bddb1c
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-12-07 16:15:13 +0000

    Bug 1866953: fix swapped arguments for JSString::OwnedChars r=sfink
    Differential Revision: https://phabricator.services.mozilla.com/D194845
    
    UltraBlame original commit: 6ca0bce858e2ec9a92ba045d19252239fc50fc12

--- js/src/vm/StringType-inl.h
+++ js/src/vm/StringType-inl.h
@@ -185,7 +185,7 @@ JSString::OwnedChars<CharT>::OwnedChars(CharT* chars, size_t length,
 template <typename CharT>
 JSString::OwnedChars<CharT>::OwnedChars(JSString::OwnedChars<CharT>&& other)
     : OwnedChars(other.chars_.Length() ? other.chars_.data() : nullptr,
-                 other.chars_.Length(), other.needsFree_, other.isMalloced_) {
+                 other.chars_.Length(), other.isMalloced_, other.needsFree_) {
   
   
   
