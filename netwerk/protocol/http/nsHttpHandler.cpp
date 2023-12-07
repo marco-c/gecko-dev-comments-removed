1e43905a7ae4b3e7dca6a5b5a0ddfa8e2a4769d8
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-12-07 15:44:16 +0000

    Backed out 2 changesets (bug 1865766) for causing reftests failures in multiple-chunks-multiple-dx-different-anchor-rtl-ref.svg and on Android. CLOSED TREE
    Backed out changeset aa8eb5897329 (bug 1865766)
    Backed out changeset bf1bc5feca4e (bug 1865766)
    
    UltraBlame original commit: 8ac2cec94ec71ed3411fa86cfe909c8033a51ece

--- netwerk/protocol/http/nsHttpHandler.cpp
+++ netwerk/protocol/http/nsHttpHandler.cpp
@@ -824,7 +824,7 @@ void nsHttpHandler::InitUserAgentComponents() {
   
   mPlatform.AssignLiteral(
 #if defined(ANDROID)
-      "Android 10"
+      "Android"
 #elif defined(XP_WIN)
       "Windows"
 #elif defined(XP_MACOSX)
@@ -849,6 +849,13 @@ void nsHttpHandler::InitUserAgentComponents() {
       do_GetService("@mozilla.org/system-info;1");
   MOZ_ASSERT(infoService, "Could not find a system info service");
   nsresult rv;
+  
+  nsAutoString androidVersion;
+  rv = infoService->GetPropertyAsAString(u"release_version"_ns, androidVersion);
+  if (NS_SUCCEEDED(rv)) {
+    mPlatform += " ";
+    mPlatform += NS_LossyConvertUTF16toASCII(androidVersion);
+  }
 
   
   bool isTV;
