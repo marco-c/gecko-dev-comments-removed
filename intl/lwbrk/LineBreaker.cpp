1d91689fde992b3c19c87019060b1588c9212513
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-12-08 21:24:37 +0000

    Bug 1865865 - Add fast-path when computing segment's length is 1. r=TYLin
    When running wikipedia's page by browsertime benchmark, 0.5%-1% calls of
    `LineBreaker::ComputeBreakPositions` is that aLength is 1. If this is 1,
    we only set SOT break in ICU4X's line segmenter.
    
    So we can add a fast-path for this situation. `ICU4XLineBreakIterator*`
    always allocate rust heap, so we can reduce a few heap allocation costs.
    
    Differential Revision: https://phabricator.services.mozilla.com/D195523
    
    UltraBlame original commit: 835a82df3a193c03ef53f6e30983c6abe8d0771f

--- intl/lwbrk/LineBreaker.cpp
+++ intl/lwbrk/LineBreaker.cpp
@@ -1093,6 +1093,15 @@ void LineBreaker::ComputeBreakPositions(
     LineBreakRule aLevel, bool aIsChineseOrJapanese, uint8_t* aBreakBefore) {
 #if defined(MOZ_ICU4X) && defined(JS_HAS_INTL_API)
   if (StaticPrefs::intl_icu4x_segmenter_enabled()) {
+    if (aLength == 1) {
+      
+      
+      
+      
+      aBreakBefore[0] = 1;
+      return;
+    }
+
     memset(aBreakBefore, 0, aLength);
 
     CheckedInt<int32_t> length = aLength;
@@ -1253,6 +1262,15 @@ void LineBreaker::ComputeBreakPositions(const uint8_t* aChars, uint32_t aLength,
                                         uint8_t* aBreakBefore) {
 #if defined(MOZ_ICU4X) && defined(JS_HAS_INTL_API)
   if (StaticPrefs::intl_icu4x_segmenter_enabled()) {
+    if (aLength == 1) {
+      
+      
+      
+      
+      aBreakBefore[0] = 1;
+      return;
+    }
+
     memset(aBreakBefore, 0, aLength);
 
     CheckedInt<int32_t> length = aLength;
