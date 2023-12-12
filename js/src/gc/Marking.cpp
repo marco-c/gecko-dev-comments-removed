eeecd4a8a60bad20dafd1d7551ddf742d34644d1
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-12-12 16:32:04 +0000

    Bug 1866636 - Check if the cell is in Atoms Zone while sweeping. r=jonco
    Differential Revision: https://phabricator.services.mozilla.com/D195798
    
    UltraBlame original commit: 67fc2755b73d46725f543980ca2d03fb3df844fc

--- js/src/gc/Marking.cpp
+++ js/src/gc/Marking.cpp
@@ -2526,7 +2526,7 @@ inline void SweepingTracer::onEdge(T** thingp, const char* name) {
   
   
   
-  if (zone->isGCSweeping() && !cell->isMarkedAny()) {
+  if ((zone->isGCSweeping() || zone->isAtomsZone()) && !cell->isMarkedAny()) {
     *thingp = nullptr;
   }
 }
