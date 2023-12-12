b599174938317e4bced453228ff9561871abf8bc
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-12-12 16:30:27 +0000

    Backed out changeset 76af96bf48b4 (bug 1868868) for causing reftest failures on root-scrollbar-zoomed-in-async-scroll.html CLOSED TREE
    UltraBlame original commit: cb01f3faa22fd50ea9fb032e8138ba3f9acb3230

--- layout/xul/nsSliderFrame.cpp
+++ layout/xul/nsSliderFrame.cpp
@@ -614,13 +614,6 @@ void nsSliderFrame::Reflow(nsPresContext* aPresContext,
     thumbPos.y = NSToCoordRound(pos * mRatio);
   }
 
-  
-  
-  
-  nscoord appUnitsPerPixel = PresContext()->AppUnitsPerDevPixel();
-  thumbPos =
-      ToAppUnits(thumbPos.ToNearestPixels(appUnitsPerPixel), appUnitsPerPixel);
-
   const LogicalPoint logicalPos(wm, thumbPos, availSize);
   
   
