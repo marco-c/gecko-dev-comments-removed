280604a0a6b8973e89c287840a975aa51f6522d3
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-12-16 00:55:43 +0000

    Bug 1868368 - Show suggestions without icons when richsuggestions are disabled. r=adw
    Differential Revision: https://phabricator.services.mozilla.com/D196140
    
    UltraBlame original commit: bf5ee34a91ea0a9005a96c9f6eb8bf42ce5b5f77

--- browser/components/urlbar/tests/unit/test_richsuggestions.js
+++ browser/components/urlbar/tests/unit/test_richsuggestions.js
@@ -8,12 +8,10 @@
 
 const SUGGEST_ENABLED_PREF = "browser.search.suggest.enabled";
 const RICH_SUGGESTIONS_PREF = "browser.urlbar.richSuggestions.featureGate";
-
 const QUICKACTIONS_URLBAR_PREF = "quickactions.enabled";
 
 add_setup(async function () {
   let engine = await addTestTailSuggestionsEngine(defaultRichSuggestionsFn);
-
   
   let oldDefaultEngine = await Services.search.getDefault();
   registerCleanupFunction(async () => {
@@ -34,15 +32,11 @@ add_setup(async function () {
 
 
 
+add_task(async function test_richsuggestions_disabled() {
+  Services.prefs.setBoolPref(RICH_SUGGESTIONS_PREF, false);
 
-add_task(async function test_richsuggestions_order() {
   const query = "what time is it in t";
   let context = createContext(query, { isPrivate: false });
-
-  let defaultRichResult = {
-    engineName: TAIL_SUGGESTIONS_ENGINE_NAME,
-    isRichSuggestion: true,
-  };
 
   await check_results({
     context,
@@ -51,22 +45,18 @@ add_task(async function test_richsuggestions_order() {
         engineName: TAIL_SUGGESTIONS_ENGINE_NAME,
         heuristic: true,
       }),
-      makeSearchResult(
-        context,
-        Object.assign(defaultRichResult, {
-          suggestion: query + "oronto",
-        })
-      ),
+      makeSearchResult(context, {
+        engineName: TAIL_SUGGESTIONS_ENGINE_NAME,
+        suggestion: query + "oronto",
+      }),
       makeSearchResult(context, {
         engineName: TAIL_SUGGESTIONS_ENGINE_NAME,
         suggestion: query + "unisia",
       }),
-      makeSearchResult(
-        context,
-        Object.assign(defaultRichResult, {
-          suggestion: query + "acoma",
-        })
-      ),
+      makeSearchResult(context, {
+        engineName: TAIL_SUGGESTIONS_ENGINE_NAME,
+        suggestion: query + "acoma",
+      }),
       makeSearchResult(context, {
         engineName: TAIL_SUGGESTIONS_ENGINE_NAME,
         suggestion: query + "aipei",
