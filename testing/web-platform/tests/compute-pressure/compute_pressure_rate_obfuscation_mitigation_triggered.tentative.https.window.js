498de0ef77ba99d717badc773877075355247813
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-12-16 00:47:17 +0000

    Bug 1867660 [wpt PR 43459] - compute-pressure: Ensure clean test exit, a=testonly
    Automatic update from web-platform-tests
    compute-pressure: Ensure clean test exit
    
    It can happen on certain bots that the pressure updates are still sent
    before disconnect() is processed in the mockPressureService, which can
    lead in certain cases to receive more updates than allowed in the test.
    
    This patch adds a `gotPenalty` flag, to make sure that at least one
    penalty was applied when reaching the maximum allowed updates.
    
    Change-Id: I535b8c21370d14f13f0d6140d1eb2cc043cbeac2
    Bug: 1501324
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5076963
    Reviewed-by: Fr <beaufort.francoisgmail.com>
    Commit-Queue: Arnaud Mandy <arnaud.mandyintel.com>
    Reviewed-by: Raphael Kubo Da Costa <raphael.kubo.da.costaintel.com>
    Cr-Commit-Position: refs/heads/main{#1232652}
    
    --
    
    wpt-commits: da0a56526aa23f752fc0186b8cf22fa9f05e2fc8
    wpt-pr: 43459
    
    UltraBlame original commit: fc539dfd4cc63c6a8c59a643eab43dcc5cb0122f

--- testing/web-platform/tests/compute-pressure/compute_pressure_rate_obfuscation_mitigation_triggered.tentative.https.window.js
+++ testing/web-platform/tests/compute-pressure/compute_pressure_rate_obfuscation_mitigation_triggered.tentative.https.window.js
@@ -13,22 +13,27 @@ pressure_test(async (t, mockPressureService) => {
   const minPenaltyTimeInMs = 5000;
   const maxChangesThreshold = 100;
   const minChangesThreshold = 50;
+  let gotPenalty = false;
   await new Promise(async resolve => {
     const observerChanges = [];
     const observer = new PressureObserver(changes => {
-      if (observerChanges.length >= minChangesThreshold) {
+      if (observerChanges.length >= minChangesThreshold && !gotPenalty) {
         
         t.step(() => {
           assert_less_than_equal(observerChanges.length, maxChangesThreshold,
                                  "Sample count reaching maxChangesThreshold.");
         });
 
-        if (observerChanges.length > 0) {
-          const lastSample = observerChanges.at(-1);
-          if ((changes[0].time - lastSample[0].time) >= minPenaltyTimeInMs) {
-            observer.disconnect();
-            resolve();
-          }
+        const lastSample = observerChanges.at(-1);
+        if ((changes[0].time - lastSample[0].time) >= minPenaltyTimeInMs) {
+          
+          
+          
+          
+          
+          gotPenalty = true;
+          observer.disconnect();
+          resolve();
         }
       }
       observerChanges.push(changes);
