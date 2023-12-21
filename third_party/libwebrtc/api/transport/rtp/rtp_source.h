ed12d6eb6029851aa018f940ca65f0fd06c9420c
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-12-21 11:47:52 +0000

    Bug 1867099 - Vendor libwebrtc from 090699a01b
    Upstream commit: https://webrtc.googlesource.com/src/+/090699a01bfc2f91af97e6461ff126bef996905a
        Delete deprecated RtpSource timestamp_ms constructor and accessor
    
        Bug: webrtc:13756
        Change-Id: Ic43cf82451785b4fbe184fce466e77b16b2c781a
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/319581
        Reviewed-by: Björn Terelius <tereliuswebrtc.org>
        Commit-Queue: Danil Chapovalov <danilchapwebrtc.org>
        Cr-Commit-Position: refs/heads/main{#40765}
    
    UltraBlame original commit: 6ee4973b999d170c2c2ef075b873a674dcb2c3ac

--- third_party/libwebrtc/api/transport/rtp/rtp_source.h
+++ third_party/libwebrtc/api/transport/rtp/rtp_source.h
@@ -56,30 +56,11 @@ class RtpSource {
         extensions_(extensions),
         rtp_timestamp_(rtp_timestamp) {}
 
-  
-  [[deprecated]] RtpSource(int64_t timestamp_ms,
-                           uint32_t source_id,
-                           RtpSourceType source_type,
-                           uint32_t rtp_timestamp,
-                           const RtpSource::Extensions& extensions)
-      : timestamp_(Timestamp::Millis(timestamp_ms)),
-        source_id_(source_id),
-        source_type_(source_type),
-        extensions_(extensions),
-        rtp_timestamp_(rtp_timestamp) {}
-
   RtpSource(const RtpSource&) = default;
   RtpSource& operator=(const RtpSource&) = default;
   ~RtpSource() = default;
 
   Timestamp timestamp() const { return timestamp_; }
-
-  
-  [[deprecated]] int64_t timestamp_ms() const { return timestamp_.ms(); }
-  [[deprecated]] void update_timestamp_ms(int64_t timestamp_ms) {
-    RTC_DCHECK_LE(timestamp_.ms(), timestamp_ms);
-    timestamp_ = Timestamp::Millis(timestamp_ms);
-  }
 
   
   uint32_t source_id() const { return source_id_; }
