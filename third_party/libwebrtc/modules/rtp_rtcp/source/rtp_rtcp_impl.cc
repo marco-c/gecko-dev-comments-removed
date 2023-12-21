d6084fa15eb4265113427b19caa60bc5deee499f
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-12-21 11:55:00 +0000

    Bug 1867099 - revert libwebrtc 8602f604e0. r=bwc
    Upstream 8602f604e0 removed code sending BYEs which breaks some of
    our wpt.  They've opened a bug for a real fix here:
    https://bugs.chromium.org/p/webrtc/issues/detail?id=15664
    
    I've opened Bug 1870643 to track the real fix and upstream bug.
    
    Differential Revision: https://phabricator.services.mozilla.com/D196729
    
    UltraBlame original commit: bf7327b1483b5714e28d334b2fcca58a42c3f526

--- third_party/libwebrtc/modules/rtp_rtcp/source/rtp_rtcp_impl.cc
+++ third_party/libwebrtc/modules/rtp_rtcp/source/rtp_rtcp_impl.cc
@@ -296,6 +296,7 @@ RTCPSender::FeedbackState ModuleRtpRtcpImpl::GetFeedbackState() {
 
 int32_t ModuleRtpRtcpImpl::SetSendingStatus(const bool sending) {
   if (rtcp_sender_.Sending() != sending) {
+    
     rtcp_sender_.SetSendingStatus(GetFeedbackState(), sending);
   }
   return 0;
