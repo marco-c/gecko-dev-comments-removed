c0ab0ed8fbe717b86e7c6d3f2a6bdc0370c14e99
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-12-21 11:39:45 +0000

    Bug 1855742 - Part 11. Allow recording based canvases with OffscreenCanvas. r=gfx-reviewers,lsalzman
    It is disabled in this patch via the gfx.canvas.remote.allow-offscreen
    pref. A follow up patch will enable this by default.
    
    Differential Revision: https://phabricator.services.mozilla.com/D189532
    
    UltraBlame original commit: 7704291111b402593a0d5a9bd49aef786477fcec

--- gfx/layers/ipc/CanvasTranslator.cpp
+++ gfx/layers/ipc/CanvasTranslator.cpp
@@ -13,6 +13,7 @@
 #include "mozilla/gfx/DrawTargetWebgl.h"
 #include "mozilla/gfx/gfxVars.h"
 #include "mozilla/gfx/GPUParent.h"
+#include "mozilla/gfx/GPUProcessManager.h"
 #include "mozilla/gfx/Logging.h"
 #include "mozilla/ipc/Endpoint.h"
 #include "mozilla/layers/BufferTexture.h"
@@ -648,9 +649,15 @@ bool CanvasTranslator::CheckForFreshCanvasDevice(int aLineNumber) {
     NotifyDeviceChanged();
   }
 
-  RefPtr<Runnable> runnable = NS_NewRunnableFunction(
-      "CanvasTranslator NotifyDeviceReset",
-      []() { gfx::GPUParent::GetSingleton()->NotifyDeviceReset(); });
+  RefPtr<Runnable> runnable =
+      NS_NewRunnableFunction("CanvasTranslator NotifyDeviceReset", []() {
+        if (XRE_IsGPUProcess()) {
+          gfx::GPUParent::GetSingleton()->NotifyDeviceReset();
+        } else {
+          gfx::GPUProcessManager::Get()->OnInProcessDeviceReset(
+               false);
+        }
+      });
 
   
   
