0fd2d688f3da9de8f40d38eee1e19505f225e673
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-12-21 11:36:05 +0000

    Bug 1870252 - Pull the JDK 17.0.9+9.1 build when bootstrapping on Windows. r=geckoview-reviewers,m_kato
    Differential Revision: https://phabricator.services.mozilla.com/D196587
    
    UltraBlame original commit: b46057158b0694677a8174089de1cb8929d3116b

--- python/mozboot/mozboot/android.py
+++ python/mozboot/mozboot/android.py
@@ -825,18 +825,34 @@ def ensure_java(os_name, os_arch):
     if not java_path.exists():
         
         
-        java_url = (
-            "https://github.com/adoptium/temurin{major}-binaries/releases/"
-            "download/jdk-{major}.{minor}%2B{patch}/"
-            "OpenJDK{major}U-jdk_{arch}_{os}_hotspot_{major}.{minor}_{patch}.{ext}"
-        ).format(
-            major=JAVA_VERSION_MAJOR,
-            minor=JAVA_VERSION_MINOR,
-            patch=JAVA_VERSION_PATCH,
-            os=os_tag,
-            arch=arch,
-            ext=ext,
-        )
+        if os_name != "windows":
+            java_url = (
+                "https://github.com/adoptium/temurin{major}-binaries/releases/"
+                "download/jdk-{major}.{minor}%2B{patch}/"
+                "OpenJDK{major}U-jdk_{arch}_{os}_hotspot_{major}.{minor}_{patch}.{ext}"
+            ).format(
+                major=JAVA_VERSION_MAJOR,
+                minor=JAVA_VERSION_MINOR,
+                patch=JAVA_VERSION_PATCH,
+                os=os_tag,
+                arch=arch,
+                ext=ext,
+            )
+        
+        
+        else:
+            java_url = (
+                "https://github.com/adoptium/temurin{major}-binaries/releases/"
+                "download/jdk-{major}.{minor}%2B{patch}.1/"
+                "OpenJDK{major}U-jdk_{arch}_{os}_hotspot_{major}.{minor}_{patch}.{ext}"
+            ).format(
+                major=JAVA_VERSION_MAJOR,
+                minor=JAVA_VERSION_MINOR,
+                patch=JAVA_VERSION_PATCH,
+                os=os_tag,
+                arch=arch,
+                ext=ext,
+            )
         install_mobile_android_sdk_or_ndk(java_url, mozbuild_path / "jdk")
     return java_path
 
