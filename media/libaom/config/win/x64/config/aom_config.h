b45319ae7be523c82aa805e99d27b7d13bcccdc0
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-12-21 11:41:31 +0000

    Backed out 7 changesets (bug 1869332) for Android bustages. CLOSED TREE
    Backed out changeset e1a7852ec85a (bug 1869332)
    Backed out changeset 886e59d6495f (bug 1869332)
    Backed out changeset 03ac3598f84f (bug 1869332)
    Backed out changeset 5e775f2c2b9c (bug 1869332)
    Backed out changeset 295a1e1f01d5 (bug 1869332)
    Backed out changeset 2909ac6aeb41 (bug 1869332)
    Backed out changeset dfab47873174 (bug 1869332)
    
    UltraBlame original commit: ebd8fb3263179f1be7461922a31519856b0e07cd

--- media/libaom/config/win/x64/config/aom_config.h
+++ media/libaom/config/win/x64/config/aom_config.h
@@ -1,13 +1,13 @@
-/*
- * Copyright (c) 2023, Alliance for Open Media. All rights reserved
- *
- * This source code is subject to the terms of the BSD 2 Clause License and
- * the Alliance for Open Media Patent License 1.0. If the BSD 2 Clause License
- * was not distributed with this source code in the LICENSE file, you can
- * obtain it at www.aomedia.org/license/software. If the Alliance for Open
- * Media Patent License 1.0 was not distributed with this source code in the
- * PATENTS file, you can obtain it at www.aomedia.org/license/patent.
- */
+
+
+
+
+
+
+
+
+
+
 #ifndef AOM_CONFIG_H_
 #define AOM_CONFIG_H_
 
@@ -20,7 +20,7 @@
 #define CONFIG_ACCOUNTING 0
 #define CONFIG_ANALYZER 0
 #define CONFIG_AV1_DECODER 1
-#define CONFIG_AV1_ENCODER 1
+#define CONFIG_AV1_ENCODER 0
 #define CONFIG_BIG_ENDIAN 0
 #define CONFIG_BITSTREAM_DEBUG 0
 #define CONFIG_COEFFICIENT_RANGE_CHECKING 0
@@ -79,4 +79,4 @@
 #define INCLUDE_INSTALL_DIR INSTALLDIR/include
 #define INLINE inline
 #define LIB_INSTALL_DIR INSTALLDIR/lib
-#endif /* AOM_CONFIG_H_ */
+#endif 
