3af2258c8eb8407d13cddfcfd8d1ea9e8164c628
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-12-21 11:56:04 +0000

    Backed out changeset 7b2ffe9a4d06 (bug 1869605) for causing bc failures on browser_notification_dot.js. CLOSED TREE
    UltraBlame original commit: d212aefe4ab7fdabc079fa32865c3693b0da335d

--- browser/components/firefoxview/tests/browser/browser_firefoxview.js
+++ browser/components/firefoxview/tests/browser/browser_firefoxview.js
@@ -2,14 +2,22 @@
 
 
 add_task(async function about_firefoxview_smoke_test() {
+  await SpecialPowers.pushPrefEnv({
+    set: [["browser.tabs.firefox-view-next", false]],
+  });
+
   await withFirefoxView({}, async browser => {
     const { document } = browser.contentWindow;
 
     
     ok(
-      document.querySelector("fxview-category-navigation"),
-      "fxview-category-navigation element exists"
+      document.getElementById("tab-pickup-container"),
+      "tab-pickup-container element exists"
     );
-    ok(document.querySelector("named-deck"), "named-deck element exists");
+    ok(
+      document.getElementById("recently-closed-tabs-container"),
+      "recently-closed-tabs-container element exists"
+    );
   });
+  await SpecialPowers.popPrefEnv();
 });
