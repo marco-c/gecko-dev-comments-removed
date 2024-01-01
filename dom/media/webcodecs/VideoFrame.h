0c186ee1c9ec08d72152e053bedc4eeddf329965
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-01-01 00:36:57 +0000

    Bug 1749047 - Add VideoFrame::ToString. r=chunmin
    This is debugging-only.
    
    Differential Revision: https://phabricator.services.mozilla.com/D195697
    
    UltraBlame original commit: 029f5470314e019b723445d48e5a78d896cbf1c1

--- dom/media/webcodecs/VideoFrame.h
+++ dom/media/webcodecs/VideoFrame.h
@@ -184,6 +184,8 @@ class VideoFrame final : public nsISupports, public nsWrapperCache {
   const gfx::IntRect& NativeVisibleRect() const { return mVisibleRect; }
   already_AddRefed<layers::Image> GetImage() const;
 
+  nsCString ToString() const;
+
  public:
   
   class Format final {
