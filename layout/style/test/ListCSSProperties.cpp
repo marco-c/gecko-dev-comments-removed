64e52a25ab4fc649bc47db57e110fa13a0f36c96
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-01-01 00:47:07 +0000

    Backed out changeset 65525b63d6cf (bug 1871745) for causing bc failures on browser_bug462289.js, browser_PanelMultiView_focus.js CLOSED TREE
    UltraBlame original commit: 7784ec6ab129151002c3fd7458095530b90da724

--- layout/style/test/ListCSSProperties.cpp
+++ layout/style/test/ListCSSProperties.cpp
@@ -103,7 +103,6 @@ const char* gInaccessibleProperties[] = {
     "-moz-min-font-size-ratio",           
     "-moz-box-collapse",                  
     "-moz-subtree-hidden-only-visually",  
-    "-moz-user-focus",                    
     "-moz-window-input-region-margin",    
     "-moz-window-opacity",                
     "-moz-window-transform",              
