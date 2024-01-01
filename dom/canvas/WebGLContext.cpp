aa42596859ad900636da50d3bbdf8d747e5ac38b
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-01-01 00:51:12 +0000

    Backed out 3 changesets (bug 1872087, bug 1767344, bug 1872159) for causing gpu related leak failures CLOSED TREE
    Backed out changeset 2c0ef884644c (bug 1872087)
    Backed out changeset 0cf50076e7ee (bug 1872159)
    Backed out changeset a4148511920c (bug 1767344)
    
    UltraBlame original commit: b3ce46bdbb031a490d96f234375c06ba59d59e68

--- dom/canvas/WebGLContext.cpp
+++ dom/canvas/WebGLContext.cpp
@@ -1152,8 +1152,7 @@ bool WebGLContext::PushRemoteTexture(WebGLFramebuffer* fb,
     swapChain.SetDestroyedCallback(destroyedCallback);
     mRemoteTextureOwner->RegisterTextureOwner(
         ownerId,
-         gfx::gfxVars::WebglOopAsyncPresentForceSync(),
-         !!fb);
+         gfx::gfxVars::WebglOopAsyncPresentForceSync());
   }
 
   MOZ_ASSERT(fb || surf);
