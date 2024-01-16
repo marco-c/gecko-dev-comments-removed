d9420b6cff7d3ba0a6e866cf5b7fb75644e5836f
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-01-16 00:29:56 +0000

    Bug 1872536 - Use MakeUniqueFallible for large buffers in image decoders/encoders. r=gfx-reviewers,lsalzman
    Differential Revision: https://phabricator.services.mozilla.com/D197461
    
    UltraBlame original commit: f5c29c2f095a5c97bcaf25cf863c9a41485fe9a2

--- image/decoders/nsBMPDecoder.cpp
+++ image/decoders/nsBMPDecoder.cpp
@@ -100,6 +100,7 @@
 #include "mozilla/Attributes.h"
 #include "mozilla/EndianUtils.h"
 #include "mozilla/Likely.h"
+#include "mozilla/UniquePtrExtensions.h"
 
 #include "RasterImage.h"
 #include "SurfacePipeFactory.h"
@@ -716,7 +717,10 @@ LexerTransition<nsBMPDecoder::State> nsBMPDecoder::ReadBitfields(
 
     
     
-    mColors = MakeUnique<ColorTableEntry[]>(256);
+    mColors = MakeUniqueFallible<ColorTableEntry[]>(256);
+    if (NS_WARN_IF(!mColors)) {
+      return Transition::TerminateFailure();
+    }
     memset(mColors.get(), 0, 256 * sizeof(ColorTableEntry));
 
     
