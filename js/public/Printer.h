7a99498b449e6ab315d310126081ccca856b9722
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-01-16 01:02:11 +0000

    Bug 1794063 - Create an SEprinter for printing via printf_stderr r=jonco
    Differential Revision: https://phabricator.services.mozilla.com/D158819
    
    UltraBlame original commit: c729aa80e73ec8483a7d9f9a0cd20b6a4be4200a

--- js/public/Printer.h
+++ js/public/Printer.h
@@ -8,6 +8,7 @@
 #define js_Printer_h
 
 #include "mozilla/Attributes.h"
+#include "mozilla/glue/Debug.h"
 #include "mozilla/Range.h"
 
 #include <stdarg.h>
@@ -339,7 +340,22 @@ class JS_PUBLIC_API Fprinter final : public GenericPrinter {
 
   
   
-  virtual void put(const char* s, size_t len) override;
+  bool put(const char* s, size_t len) override;
+  using GenericPrinter::put;  
+};
+
+
+
+class SEprinter final : public GenericPrinter {
+ public:
+  constexpr SEprinter() {}
+
+  
+  
+  virtual bool put(const char* s, size_t len) override {
+    printf_stderr("%.*s", int(len), s);
+    return true;
+  }
   using GenericPrinter::put;  
 };
 
