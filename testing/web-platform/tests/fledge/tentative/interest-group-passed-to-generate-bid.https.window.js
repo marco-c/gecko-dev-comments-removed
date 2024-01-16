e68e77a5b5a23ecbfee87acd98e291e60c270191
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-01-16 01:22:57 +0000

    Bug 1873971 [wpt PR 43926] - [Protected Audiences] enableBiddingSignalsPrioritization in generateBid, a=testonly
    Automatic update from web-platform-tests
    [Protected Audiences] enableBiddingSignalsPrioritization in generateBid
    
    This CL fixes `enableBiddingSignalsPrioritization` not being correctly
    set on interest groups being passed to generateBid(). We were setting
    useBiddingSignalsPrioritization instead (which may have been a name used
    in earlier draft specs when adding the field).
    
    Fixed: 1516980
    Change-Id: I759bc99c2f656f6f2fd5c2413fad17acacefe54d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5182114
    Reviewed-by: Maks Orlovich <morlovichchromium.org>
    Commit-Queue: Matt Menke <mmenkechromium.org>
    Cr-Commit-Position: refs/heads/main{#1245506}
    
    --
    
    wpt-commits: 871c3cb7f61569ea2d70dcb96fb40c1b409ad0e1
    wpt-pr: 43926
    
    UltraBlame original commit: b94f73e6765fcc21a97fbf13f60e5f092745ae18

--- testing/web-platform/tests/fledge/tentative/interest-group-passed-to-generate-bid.https.window.js
+++ testing/web-platform/tests/fledge/tentative/interest-group-passed-to-generate-bid.https.window.js
@@ -141,28 +141,29 @@ makeTest({
 makeTest({
   name: 'InterestGroup.enableBiddingSignalsPrioritization not set.',
   fieldName: 'enableBiddingSignalsPrioritization',
-  fieldValue: undefined
+  fieldValue: false,
+  interestGroupOverrides: { enableBiddingSignalsPrioritization: undefined }
 });
 
 makeTest({
   name: 'InterestGroup.enableBiddingSignalsPrioritization unrecognized.',
   fieldName: 'enableBiddingSignalsPrioritization',
-  fieldValue: undefined,
+  
+  
+  fieldValue: true,
   interestGroupOverrides: { enableBiddingSignalsPrioritization: 'foo' }
 });
 
 makeTest({
   name: 'InterestGroup.enableBiddingSignalsPrioritization false.',
   fieldName: 'enableBiddingSignalsPrioritization',
-  fieldValue: undefined,
-  interestGroupOverrides: { enableBiddingSignalsPrioritization: false }
+  fieldValue: false
 });
 
 makeTest({
   name: 'InterestGroup.enableBiddingSignalsPrioritization true.',
   fieldName: 'enableBiddingSignalsPrioritization',
-  fieldValue: undefined,
-  interestGroupOverrides: { enableBiddingSignalsPrioritization: true }
+  fieldValue: true
 });
 
 makeTest({
