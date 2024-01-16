bef20604670407932d6acc2e6e2def413cc3c0b6
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-01-16 01:01:26 +0000

    Bug 1872907 - stop marking SVG symbol elements as display:none r=emilio
    Differential Revision: https://phabricator.services.mozilla.com/D197760
    
    UltraBlame original commit: 620716295eed32be5d4281554176937c156c6984

--- dom/svg/SVGSymbolElement.cpp
+++ dom/svg/SVGSymbolElement.cpp
@@ -29,6 +29,20 @@ SVGSymbolElement::SVGSymbolElement(
     already_AddRefed<mozilla::dom::NodeInfo>&& aNodeInfo)
     : SVGSymbolElementBase(std::move(aNodeInfo)) {}
 
+Focusable SVGSymbolElement::IsFocusableWithoutStyle(bool aWithMouse) {
+  if (!CouldBeRendered()) {
+    return {};
+  }
+  return SVGSymbolElementBase::IsFocusableWithoutStyle(aWithMouse);
+}
+
+bool SVGSymbolElement::CouldBeRendered() const {
+  
+  
+  auto* shadowRoot = ShadowRoot::FromNodeOrNull(GetParentNode());
+  return shadowRoot && shadowRoot->Host()->IsSVGElement(nsGkAtoms::use);
+}
+
 
 
 
