2fc0e005e01b733b2aae24f9b0a32d0157525b8a
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-01-16 00:53:40 +0000

    Backed out changeset 11bda0f9d0fc (bug 1872997) for causing mochitest-chrome failures on test_bug413277.html. CLOSED TREE
    UltraBlame original commit: 979ef2f2e7708464499a2d048021d2ee91ee40fb

--- widget/cocoa/nsCocoaWindow.mm
+++ widget/cocoa/nsCocoaWindow.mm
@@ -297,6 +297,44 @@ static NSScreen* FindTargetScreenForRect(const DesktopIntRect& aRect) {
   return targetScreen;
 }
 
+
+
+
+static void FitRectToVisibleAreaForScreen(DesktopIntRect& aRect,
+                                          NSScreen* aScreen) {
+  if (!aScreen) {
+    aScreen = FindTargetScreenForRect(aRect);
+  }
+
+  DesktopIntRect screenBounds =
+      nsCocoaUtils::CocoaRectToGeckoRect([aScreen visibleFrame]);
+
+  if (aRect.width > screenBounds.width) {
+    aRect.width = screenBounds.width;
+  }
+  if (aRect.height > screenBounds.height) {
+    aRect.height = screenBounds.height;
+  }
+
+  if (aRect.x - screenBounds.x + aRect.width > screenBounds.width) {
+    aRect.x += screenBounds.width - (aRect.x - screenBounds.x + aRect.width);
+  }
+  if (aRect.y - screenBounds.y + aRect.height > screenBounds.height) {
+    aRect.y += screenBounds.height - (aRect.y - screenBounds.y + aRect.height);
+  }
+
+  
+  
+  if (aRect.x < screenBounds.x ||
+      aRect.x > (screenBounds.x + screenBounds.width)) {
+    aRect.x = screenBounds.x;
+  }
+  if (aRect.y < screenBounds.y ||
+      aRect.y > (screenBounds.y + screenBounds.height)) {
+    aRect.y = screenBounds.y;
+  }
+}
+
 DesktopToLayoutDeviceScale ParentBackingScaleFactor(nsIWidget* aParent,
                                                     NSView* aParentView) {
   if (aParent) {
@@ -354,6 +392,9 @@ nsresult nsCocoaWindow::Create(nsIWidget* aParent, nsNativeWidget aNativeParent,
   
   nsAutoreleasePool localPool;
 
+  DesktopIntRect newBounds = aRect;
+  FitRectToVisibleAreaForScreen(newBounds, nullptr);
+
   
   mWindowType = WindowType::TopLevel;
   mBorderStyle = BorderStyle::Default;
@@ -390,7 +431,7 @@ nsresult nsCocoaWindow::Create(nsIWidget* aParent, nsNativeWidget aNativeParent,
     
     
     LayoutDeviceIntRect devRect =
-        RoundedToInt(aRect * GetDesktopToDeviceScale());
+        RoundedToInt(newBounds * GetDesktopToDeviceScale());
     return CreatePopupContentView(devRect, aInitData);
   }
 
@@ -2124,6 +2165,10 @@ void nsCocoaWindow::DoResize(double aX, double aY, double aWidth,
   DesktopIntRect newBounds(NSToIntRound(aX), NSToIntRound(aY),
                            NSToIntRound(width / scale),
                            NSToIntRound(height / scale));
+
+  
+  FitRectToVisibleAreaForScreen(
+      newBounds, aConstrainToCurrentScreen ? [mWindow screen] : nullptr);
 
   
   NSRect newFrame = nsCocoaUtils::GeckoRectToCocoaRect(newBounds);
