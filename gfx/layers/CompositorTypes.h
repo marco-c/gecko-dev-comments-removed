2b2936f08b6b732ba0b6239fe4499c825e490d85
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-01-16 01:30:26 +0000

    Bug 1874592 - Only wait for remote texture owner in GetRemoteTexture for Canvas2D. r=aosmond
    Canvas2D currently requires GetRemoteTexture to wait for an available texture owner. However,
    WebGL and WebGPU should not actually wait in GetRemoteTexture because they implement the wait
    elsewhere on the client-side, nor do they actually implement forwarder transaction id logging
    so it is not safe to use the timed wait for them here.
    
    This takes the path of least resistance by plumbing a flag in TextureFlags to pass this information
    along.
    
    Differential Revision: https://phabricator.services.mozilla.com/D198499
    
    UltraBlame original commit: b24d63d72f77a64fe5d8bce6420b88e7e0c4d9da

--- gfx/layers/CompositorTypes.h
+++ gfx/layers/CompositorTypes.h
@@ -97,9 +97,11 @@ enum class TextureFlags : uint32_t {
   DUMMY_TEXTURE = 1 << 22,
   
   SOFTWARE_DECODED_VIDEO = 1 << 23,
+  
+  WAIT_FOR_REMOTE_TEXTURE_OWNER = 1 << 24,
 
   
-  ALL_BITS = (1 << 24) - 1,
+  ALL_BITS = (1 << 25) - 1,
   
   DEFAULT = NO_FLAGS
 };
