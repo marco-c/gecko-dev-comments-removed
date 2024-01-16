99e089982890e757143e205d8704e1ae245f7bd2
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-01-16 00:37:20 +0000

    Bug 1873085 - Prefer disabling acceleration over GPU process with excessive device resets. r=gfx-reviewers,jrmuizel
    When we encounter an excessive number of device resets in the GPU
    process, we would previously just disable the GPU process over blaming
    the acceleration itself. Now we either restart the GPU process in a
    last ditch attempt to clear the device state, and failing that, disable
    acceleration due to excessive resets.
    
    Differential Revision: https://phabricator.services.mozilla.com/D197721
    
    UltraBlame original commit: 4ad0cf510725615a5eae763427f0928f5e968b41

--- gfx/ipc/GPUProcessManager.cpp
+++ gfx/ipc/GPUProcessManager.cpp
@@ -767,10 +767,9 @@ void GPUProcessManager::OnInProcessDeviceReset(bool aTrackThreshold) {
 }
 
 void GPUProcessManager::OnRemoteProcessDeviceReset(GPUProcessHost* aHost) {
-  if (OnDeviceReset( true)) {
-    DestroyProcess();
-    DisableGPUProcess("GPU processed experienced too many device resets");
-    HandleProcessLost();
+  if (OnDeviceReset( true) &&
+      !DisableWebRenderConfig(wr::WebRenderError::EXCESSIVE_RESETS,
+                              nsCString())) {
     return;
   }
 
