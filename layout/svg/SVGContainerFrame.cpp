6e70d416f2702709085eef29f665637c446c35b0
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-01-16 00:59:45 +0000

    Bug 1872907 - stop marking SVG symbol elements as display:none r=emilio
    Differential Revision: https://phabricator.services.mozilla.com/D197760
    
    UltraBlame original commit: b1f46ae76bf0ee1f886d4f6bce615976657fa53f

--- layout/svg/SVGContainerFrame.cpp
+++ layout/svg/SVGContainerFrame.cpp
@@ -162,9 +162,7 @@ void SVGDisplayContainerFrame::InsertFrames(
     for (nsIFrame* kid = firstNewFrame; kid != nextFrame;
          kid = kid->GetNextSibling()) {
       ISVGDisplayableFrame* SVGFrame = do_QueryFrame(kid);
-      if (SVGFrame) {
-        MOZ_ASSERT(!kid->HasAnyStateBits(NS_FRAME_IS_NONDISPLAY),
-                   "Check for this explicitly in the |if|, then");
+      if (SVGFrame && !kid->HasAnyStateBits(NS_FRAME_IS_NONDISPLAY)) {
         bool isFirstReflow = kid->HasAnyStateBits(NS_FRAME_FIRST_REFLOW);
         
         kid->RemoveStateBits(NS_FRAME_FIRST_REFLOW | NS_FRAME_IS_DIRTY |
@@ -190,8 +188,10 @@ void SVGDisplayContainerFrame::RemoveFrame(DestroyContext& aContext,
   
   
   SchedulePaint();
-  PresContext()->RestyleManager()->PostRestyleEvent(
-      mContent->AsElement(), RestyleHint{0}, nsChangeHint_UpdateOverflow);
+  if (!HasAnyStateBits(NS_FRAME_IS_NONDISPLAY)) {
+    PresContext()->RestyleManager()->PostRestyleEvent(
+        mContent->AsElement(), RestyleHint{0}, nsChangeHint_UpdateOverflow);
+  }
 
   SVGContainerFrame::RemoveFrame(aContext, aListID, aOldFrame);
 }
@@ -332,9 +332,7 @@ void SVGDisplayContainerFrame::ReflowSVG() {
 
   for (auto* kid : mFrames) {
     ISVGDisplayableFrame* SVGFrame = do_QueryFrame(kid);
-    if (SVGFrame) {
-      MOZ_ASSERT(!kid->HasAnyStateBits(NS_FRAME_IS_NONDISPLAY),
-                 "Check for this explicitly in the |if|, then");
+    if (SVGFrame && !kid->HasAnyStateBits(NS_FRAME_IS_NONDISPLAY)) {
       SVGFrame->ReflowSVG();
 
       
