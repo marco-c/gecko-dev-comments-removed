03fabe4b4b211dd3f1098009165b2c61388de814
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-01-16 01:01:03 +0000

    Bug 1817794 - Remove Firefox View feature pref r=fxview-reviewers,sclements
    As we are no longer experimenting with turning this pref on and off and have moved to the newer
    implementation of Firefox View, this pref has been removed.
    
    We may want to notify users who prefer this feature hidden.
    
    Differential Revision: https://phabricator.services.mozilla.com/D196887
    
    UltraBlame original commit: c7c11549e8960a8989ead448955570f2a7cb5e28

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -9955,16 +9955,11 @@ var ConfirmationHint = {
 var FirefoxViewHandler = {
   tab: null,
   BUTTON_ID: "firefox-view-button",
-  _enabled: false,
   get button() {
     return document.getElementById(this.BUTTON_ID);
   },
   init() {
     CustomizableUI.addListener(this);
-
-    this._updateEnabledState = this._updateEnabledState.bind(this);
-    this._updateEnabledState();
-    NimbusFeatures.majorRelease2022.onUpdate(this._updateEnabledState);
 
     ChromeUtils.defineESModuleGetters(this, {
       SyncedTabs: "resource://services-sync/SyncedTabs.sys.mjs",
@@ -9974,18 +9969,6 @@ var FirefoxViewHandler = {
   uninit() {
     CustomizableUI.removeListener(this);
     Services.obs.removeObserver(this, "firefoxview-notification-dot-update");
-    NimbusFeatures.majorRelease2022.offUpdate(this._updateEnabledState);
-  },
-  _updateEnabledState() {
-    this._enabled = NimbusFeatures.majorRelease2022.getVariable("firefoxView");
-    
-    
-    
-    document.documentElement.toggleAttribute(
-      "firefoxviewhidden",
-      !this._enabled
-    );
-    document.getElementById("menu_openFirefoxView").hidden = !this._enabled;
   },
   onWidgetRemoved(aWidgetId) {
     if (aWidgetId == this.BUTTON_ID && this.tab) {
