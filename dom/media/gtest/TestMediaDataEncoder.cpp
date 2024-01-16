c71bd6f492968211ba88eb1b89d5b50c42a3f69e
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-01-16 00:31:22 +0000

    Bug 1870401 - clang-format TestMediaDataEncoder.cpp r=media-playback-reviewers,alwu
    Run
    ```
    ./mach clang-format --path dom/media/gtest/TestMediaDataEncoder.cpp
    ```
    
    Differential Revision: https://phabricator.services.mozilla.com/D197364
    
    UltraBlame original commit: 5d9e038ddb39ac8e123393fad7765fbdb618307d

--- dom/media/gtest/TestMediaDataEncoder.cpp
+++ dom/media/gtest/TestMediaDataEncoder.cpp
@@ -174,7 +174,8 @@ already_AddRefed<MediaDataEncoder> CreateVideoEncoder(
       EncoderConfig(aCodec, gfx::IntSize{aWidth, aHeight}, aUsage, aPixelFormat,
                     aPixelFormat, FRAME_RATE ,
                     KEYFRAME_INTERVAL ,
-                    BIT_RATE , BIT_RATE_MODE, pref, MediaDataEncoder::ScalabilityMode::None, aSpecific),
+                    BIT_RATE , BIT_RATE_MODE, pref,
+                    MediaDataEncoder::ScalabilityMode::None, aSpecific),
       taskQueue);
 
   return e.forget();
