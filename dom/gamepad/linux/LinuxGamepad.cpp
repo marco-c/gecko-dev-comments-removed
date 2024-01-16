1d0f8b41e6f63cbbb7463f965268e6921040b5d3
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-01-16 01:19:06 +0000

    Bug 1680982 - Implement Linux/Unix Gamepad support using evdev. r=stransky
    Based on work by: Val Packett <valpackett.cool> and coolreader18
    <coolreader18gmail.com>.
    
    Differential Revision: https://phabricator.services.mozilla.com/D197865
    
    UltraBlame original commit: c0050d05b7cefc3a6902c6aa620f19f3a525e59c

--- dom/gamepad/linux/LinuxGamepad.cpp
+++ dom/gamepad/linux/LinuxGamepad.cpp
@@ -10,10 +10,11 @@
 
 
 #include <algorithm>
+#include <unordered_map>
 #include <cstddef>
 
 #include <glib.h>
-#include <linux/joystick.h>
+#include <linux/input.h>
 #include <stdio.h>
 #include <stdint.h>
 #include <sys/ioctl.h>
@@ -21,9 +22,14 @@
 #include "nscore.h"
 #include "mozilla/dom/GamepadHandle.h"
 #include "mozilla/dom/GamepadPlatformService.h"
+#include "mozilla/dom/GamepadRemapping.h"
 #include "mozilla/Tainting.h"
 #include "mozilla/UniquePtr.h"
+#include "mozilla/Sprintf.h"
 #include "udev.h"
+
+#define BITS_PER_LONG ((sizeof(unsigned long)) * 8)
+#define BITS_TO_LONGS(x) (((x) + BITS_PER_LONG - 1) / BITS_PER_LONG)
 
 namespace {
 
@@ -36,19 +42,42 @@ using mozilla::udev_list_entry;
 using mozilla::udev_monitor;
 using mozilla::UniquePtr;
 
-static const float kMaxAxisValue = 32767.0;
-static const char kJoystickPath[] = "/dev/input/js";
+static const char kEvdevPath[] = "/dev/input/event";
+
+static inline bool TestBit(const unsigned long* arr, size_t bit) {
+  return !!(arr[bit / BITS_PER_LONG] & (1LL << (bit % BITS_PER_LONG)));
+}
+
+static inline double ScaleAxis(const input_absinfo& info, int value) {
+  return 2.0 * (value - info.minimum) / (double)(info.maximum - info.minimum) -
+         1.0;
+}
 
 
 
-typedef struct {
+struct Gamepad {
   GamepadHandle handle;
-  guint source_id;
-  int numAxes;
-  int numButtons;
-  char idstring[256];
-  char devpath[PATH_MAX];
-} Gamepad;
+  bool isStandardGamepad = false;
+  RefPtr<GamepadRemapper> remapper = nullptr;
+  guint source_id = UINT_MAX;
+  char idstring[256] = {0};
+  char devpath[PATH_MAX] = {0};
+  uint8_t key_map[KEY_MAX] = {0};
+  uint8_t abs_map[ABS_MAX] = {0};
+  std::unordered_map<uint16_t, input_absinfo> abs_info;
+};
+
+static inline bool LoadAbsInfo(int fd, Gamepad* gamepad, uint16_t code) {
+  input_absinfo info{0};
+  if (ioctl(fd, EVIOCGABS(code), &info) < 0) {
+    return false;
+  }
+  if (info.minimum == info.maximum) {
+    return false;
+  }
+  gamepad->abs_info.emplace(code, std::move(info));
+  return true;
+}
 
 class LinuxGamepadService {
  public:
@@ -63,7 +92,7 @@ class LinuxGamepadService {
   void ScanForDevices();
   void AddMonitor();
   void RemoveMonitor();
-  bool is_gamepad(struct udev_device* dev);
+  bool IsDeviceGamepad(struct udev_device* dev);
   void ReadUdevChange();
 
   
@@ -114,37 +143,143 @@ void LinuxGamepadService::AddDevice(struct udev_device* dev) {
   g_io_channel_set_encoding(channel, nullptr, nullptr);
   g_io_channel_set_buffered(channel, FALSE);
   int fd = g_io_channel_unix_get_fd(channel);
-  char name[128];
-  if (ioctl(fd, JSIOCGNAME(sizeof(name)), &name) == -1) {
-    strcpy(name, "unknown");
+
+  input_id id{0};
+  if (ioctl(fd, EVIOCGID, &id) < 0) {
+    return;
   }
-  const char* vendor_id =
-      mUdev.udev_device_get_property_value(dev, "ID_VENDOR_ID");
-  const char* model_id =
-      mUdev.udev_device_get_property_value(dev, "ID_MODEL_ID");
-  if (!vendor_id || !model_id) {
-    struct udev_device* parent =
-        mUdev.udev_device_get_parent_with_subsystem_devtype(dev, "input",
-                                                            nullptr);
-    if (parent) {
-      vendor_id = mUdev.udev_device_get_sysattr_value(parent, "id/vendor");
-      model_id = mUdev.udev_device_get_sysattr_value(parent, "id/product");
+
+  char name[128]{0};
+  if (ioctl(fd, EVIOCGNAME(sizeof(name)), &name) < 0) {
+    strcpy(name, "Unknown Device");
+  }
+
+  SprintfLiteral(gamepad->idstring, "%04" PRIx16 "-%04" PRIx16 "-%s", id.vendor,
+                 id.product, name);
+
+  unsigned long keyBits[BITS_TO_LONGS(KEY_CNT)] = {0};
+  unsigned long absBits[BITS_TO_LONGS(ABS_CNT)] = {0};
+  if (ioctl(fd, EVIOCGBIT(EV_KEY, sizeof(keyBits)), keyBits) < 0 ||
+      ioctl(fd, EVIOCGBIT(EV_ABS, sizeof(absBits)), absBits) < 0) {
+    return;
+  }
+
+  
+
+
+  
+
+  const std::array<uint16_t, BUTTON_INDEX_COUNT> kStandardButtons = {
+       BTN_SOUTH,
+       BTN_EAST,
+       BTN_WEST,
+       BTN_NORTH,
+       BTN_TL,
+       BTN_TR,
+       BTN_TL2,
+       BTN_TR2,
+       BTN_SELECT,
+       BTN_START,
+       BTN_THUMBL,
+       BTN_THUMBR,
+       BTN_DPAD_UP,
+       BTN_DPAD_DOWN,
+       BTN_DPAD_LEFT,
+       BTN_DPAD_RIGHT,
+       BTN_MODE,
+  };
+  const std::array<uint16_t, AXIS_INDEX_COUNT> kStandardAxes = {
+       ABS_X,
+       ABS_Y,
+       ABS_RX,
+       ABS_RY,
+  };
+
+  
+
+
+
+
+
+  uint32_t numButtons = 0;
+  if (TestBit(keyBits, BTN_GAMEPAD)) {
+    gamepad->isStandardGamepad = true;
+    for (uint8_t button = 0; button < BUTTON_INDEX_COUNT; button++) {
+      gamepad->key_map[kStandardButtons[button]] = button;
+    }
+    numButtons = BUTTON_INDEX_COUNT;
+  }
+
+  
+  for (uint16_t key = 0; key < KEY_MAX; key++) {
+    
+    if (gamepad->isStandardGamepad &&
+        std::find(kStandardButtons.begin(), kStandardButtons.end(), key) !=
+            kStandardButtons.end()) {
+      continue;
+    }
+
+    if (TestBit(keyBits, key)) {
+      gamepad->key_map[key] = numButtons++;
     }
   }
-  snprintf(gamepad->idstring, sizeof(gamepad->idstring), "%s-%s-%s",
-           vendor_id ? vendor_id : "unknown", model_id ? model_id : "unknown",
-           name);
 
-  char numAxes = 0, numButtons = 0;
-  ioctl(fd, JSIOCGAXES, &numAxes);
-  gamepad->numAxes = numAxes;
-  ioctl(fd, JSIOCGBUTTONS, &numButtons);
-  gamepad->numButtons = numButtons;
+  uint32_t numAxes = 0;
+  if (gamepad->isStandardGamepad) {
+    for (uint8_t i = 0; i < AXIS_INDEX_COUNT; i++) {
+      gamepad->abs_map[kStandardAxes[i]] = i;
+      LoadAbsInfo(fd, gamepad.get(), kStandardAxes[i]);
+    }
+    numAxes = AXIS_INDEX_COUNT;
 
-  gamepad->handle = service->AddGamepad(
-      gamepad->idstring, mozilla::dom::GamepadMappingType::_empty,
-      mozilla::dom::GamepadHand::_empty, gamepad->numButtons, gamepad->numAxes,
-      0, 0, 0);  
+    
+    LoadAbsInfo(fd, gamepad.get(), ABS_HAT0X);
+    LoadAbsInfo(fd, gamepad.get(), ABS_HAT0Y);
+  }
+
+  for (uint16_t i = 0; i < ABS_MAX; ++i) {
+    if (gamepad->isStandardGamepad &&
+        (std::find(kStandardAxes.begin(), kStandardAxes.end(), i) !=
+             kStandardAxes.end() ||
+         i == ABS_HAT0X || i == ABS_HAT0Y)) {
+      continue;
+    }
+
+    if (TestBit(absBits, i)) {
+      if (LoadAbsInfo(fd, gamepad.get(), i)) {
+        gamepad->abs_map[i] = numAxes++;
+      }
+    }
+  }
+
+  if (numAxes == 0) {
+    NS_WARNING("Gamepad with zero axes detected?");
+  }
+  if (numButtons == 0) {
+    NS_WARNING("Gamepad with zero buttons detected?");
+  }
+
+  
+  
+  if (gamepad->isStandardGamepad) {
+    gamepad->handle =
+        service->AddGamepad(gamepad->idstring, GamepadMappingType::Standard,
+                            GamepadHand::_empty, numButtons, numAxes, 0, 0, 0);
+  } else {
+    bool defaultRemapper = false;
+    RefPtr<GamepadRemapper> remapper =
+        GetGamepadRemapper(id.vendor, id.product, defaultRemapper);
+    MOZ_ASSERT(remapper);
+    remapper->SetAxisCount(numAxes);
+    remapper->SetButtonCount(numButtons);
+
+    gamepad->handle = service->AddGamepad(
+        gamepad->idstring, remapper->GetMappingType(), GamepadHand::_empty,
+        remapper->GetButtonCount(), remapper->GetAxisCount(), 0,
+        remapper->GetLightIndicatorCount(), remapper->GetTouchEventCount());
+    gamepad->remapper = remapper.forget();
+  }
+  
   
 
   gamepad->source_id =
@@ -192,7 +327,7 @@ void LinuxGamepadService::ScanForDevices() {
     const char* path = mUdev.udev_list_entry_get_name(dev_list_entry);
     struct udev_device* dev =
         mUdev.udev_device_new_from_syspath(mUdev.udev, path);
-    if (is_gamepad(dev)) {
+    if (IsDeviceGamepad(dev)) {
       AddDevice(dev);
     }
 
@@ -235,7 +370,9 @@ void LinuxGamepadService::RemoveMonitor() {
 
 void LinuxGamepadService::Startup() {
   
-  if (!mUdev) return;
+  if (!mUdev) {
+    return;
+  }
 
   AddMonitor();
   ScanForDevices();
@@ -249,25 +386,23 @@ void LinuxGamepadService::Shutdown() {
   RemoveMonitor();
 }
 
-bool LinuxGamepadService::is_gamepad(struct udev_device* dev) {
-  if (!mUdev.udev_device_get_property_value(dev, "ID_INPUT_JOYSTICK"))
+bool LinuxGamepadService::IsDeviceGamepad(struct udev_device* aDev) {
+  if (!mUdev.udev_device_get_property_value(aDev, "ID_INPUT_JOYSTICK")) {
     return false;
+  }
 
-  const char* devpath = mUdev.udev_device_get_devnode(dev);
+  const char* devpath = mUdev.udev_device_get_devnode(aDev);
   if (!devpath) {
     return false;
   }
-  if (strncmp(kJoystickPath, devpath, sizeof(kJoystickPath) - 1) != 0) {
-    return false;
-  }
 
-  return true;
+  return strncmp(devpath, kEvdevPath, strlen(kEvdevPath)) == 0;
 }
 
 void LinuxGamepadService::ReadUdevChange() {
   struct udev_device* dev = mUdev.udev_monitor_receive_device(mMonitor);
-  const char* action = mUdev.udev_device_get_action(dev);
-  if (is_gamepad(dev)) {
+  if (IsDeviceGamepad(dev)) {
+    const char* action = mUdev.udev_device_get_action(dev);
     if (strcmp(action, "add") == 0) {
       AddDevice(dev);
     } else if (strcmp(action, "remove") == 0) {
@@ -289,10 +424,12 @@ gboolean LinuxGamepadService::OnGamepadData(GIOChannel* source,
   auto* gamepad = static_cast<Gamepad*>(data);
 
   
-  if (condition & G_IO_ERR || condition & G_IO_HUP) return FALSE;
+  if (condition & (G_IO_ERR | G_IO_HUP)) {
+    return FALSE;
+  }
 
   while (true) {
-    struct js_event event;
+    struct input_event event {};
     gsize count;
     GError* err = nullptr;
     if (g_io_channel_read_chars(source, (gchar*)&event, sizeof(event), &count,
@@ -301,19 +438,47 @@ gboolean LinuxGamepadService::OnGamepadData(GIOChannel* source,
       break;
     }
 
-    
-    if (event.type & JS_EVENT_INIT) {
-      continue;
-    }
-
     switch (event.type) {
-      case JS_EVENT_BUTTON:
-        service->NewButtonEvent(gamepad->handle, event.number, !!event.value);
+      case EV_KEY:
+        if (gamepad->isStandardGamepad) {
+          service->NewButtonEvent(gamepad->handle, gamepad->key_map[event.code],
+                                  !!event.value);
+        } else {
+          gamepad->remapper->RemapButtonEvent(
+              gamepad->handle, gamepad->key_map[event.code], !!event.value);
+        }
         break;
-      case JS_EVENT_AXIS:
-        service->NewAxisMoveEvent(gamepad->handle, event.number,
-                                  ((float)event.value) / kMaxAxisValue);
-        break;
+      case EV_ABS: {
+        if (!gamepad->abs_info.count(event.code)) {
+          continue;
+        }
+
+        double scaledValue =
+            ScaleAxis(gamepad->abs_info[event.code], event.value);
+        if (gamepad->isStandardGamepad) {
+          switch (event.code) {
+            case ABS_HAT0X:
+              service->NewButtonEvent(gamepad->handle, BUTTON_INDEX_DPAD_LEFT,
+                                      AxisNegativeAsButton(scaledValue));
+              service->NewButtonEvent(gamepad->handle, BUTTON_INDEX_DPAD_RIGHT,
+                                      AxisPositiveAsButton(scaledValue));
+              break;
+            case ABS_HAT0Y:
+              service->NewButtonEvent(gamepad->handle, BUTTON_INDEX_DPAD_UP,
+                                      AxisNegativeAsButton(scaledValue));
+              service->NewButtonEvent(gamepad->handle, BUTTON_INDEX_DPAD_DOWN,
+                                      AxisPositiveAsButton(scaledValue));
+              break;
+            default:
+              service->NewAxisMoveEvent(
+                  gamepad->handle, gamepad->abs_map[event.code], scaledValue);
+              break;
+          }
+        } else {
+          gamepad->remapper->RemapAxisMoveEvent(
+              gamepad->handle, gamepad->abs_map[event.code], scaledValue);
+        }
+      } break;
     }
   }
 
@@ -324,7 +489,9 @@ gboolean LinuxGamepadService::OnGamepadData(GIOChannel* source,
 gboolean LinuxGamepadService::OnUdevMonitor(GIOChannel* source,
                                             GIOCondition condition,
                                             gpointer data) {
-  if (condition & G_IO_ERR || condition & G_IO_HUP) return FALSE;
+  if (condition & (G_IO_ERR | G_IO_HUP)) {
+    return FALSE;
+  }
 
   gService->ReadUdevChange();
   return TRUE;
