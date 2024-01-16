09dce20c56c39f977547dc3172255b73d3711508
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-01-16 00:55:24 +0000

    Bug 1871597 - Update assertions around OptimizedGetIterator fuse status r=iain
    Differential Revision: https://phabricator.services.mozilla.com/D197975
    
    UltraBlame original commit: 27da583cfc28e23420a4c6b58a08d58ba556ffa1

--- js/src/jit/CacheIR.cpp
+++ js/src/jit/CacheIR.cpp
@@ -13539,6 +13539,9 @@ AttachDecision OptimizeGetIteratorIRGenerator::tryAttachArray() {
   Rooted<JSFunction*> iterFun(cx_);
   if (!IsArrayPrototypeOptimizable(cx_, obj.as<ArrayObject>(), &arrProto,
                                    &arrProtoIterSlot, &iterFun)) {
+    
+    MOZ_ASSERT(
+        !obj->nonCCWRealm()->realmFuses.optimizeGetIteratorFuse.intact());
     return AttachDecision::NoAction;
   }
 
@@ -13549,6 +13552,9 @@ AttachDecision OptimizeGetIteratorIRGenerator::tryAttachArray() {
   Rooted<JSFunction*> nextFun(cx_);
   if (!IsArrayIteratorPrototypeOptimizable(
           cx_, AllowIteratorReturn::No, &arrayIteratorProto, &slot, &nextFun)) {
+    
+    MOZ_ASSERT(
+        !obj->nonCCWRealm()->realmFuses.optimizeGetIteratorFuse.intact());
     return AttachDecision::NoAction;
   }
 
@@ -13559,10 +13565,20 @@ AttachDecision OptimizeGetIteratorIRGenerator::tryAttachArray() {
   MOZ_ASSERT(obj->is<ArrayObject>());
   writer.guardShape(objId, obj->shape());
   writer.guardArrayIsPacked(objId);
+  bool intact = obj->nonCCWRealm()->realmFuses.optimizeGetIteratorFuse.intact();
 
-  MOZ_ASSERT(obj->nonCCWRealm()->realmFuses.optimizeGetIteratorFuse.intact());
-
-  if (!cx_->options().enableDestructuringFuse()) {
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  bool useDynamicCheck = !intact || !cx_->options().enableDestructuringFuse();
+  if (useDynamicCheck) {
     
     ObjOperandId arrProtoId = writer.loadObject(arrProto);
     ObjOperandId iterId = writer.loadObject(iterFun);
@@ -13588,7 +13604,11 @@ AttachDecision OptimizeGetIteratorIRGenerator::tryAttachArray() {
   writer.loadBooleanResult(true);
   writer.returnFromIC();
 
-  trackAttached("OptimizeGetIterator.Array");
+  if (useDynamicCheck) {
+    trackAttached("OptimizeGetIterator.Array.Dynamic");
+  } else {
+    trackAttached("OptimizeGetIterator.Array.Fuse");
+  }
   return AttachDecision::Attach;
 }
 
