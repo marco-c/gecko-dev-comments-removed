b7327fd2ed45a4f858c5b74216e141e79202d54b
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-01-19 16:15:40 +0000

    Bug 1870488 - Part 1. Implement ImageBridgeChild::GetTileLockAllocator. r=gfx-reviewers,lsalzman
    This patch adds support for allocationg shmem sections for
    ImageBridgeChild. The recording infrastructure depends on it.
    
    Differential Revision: https://phabricator.services.mozilla.com/D189528
    
    UltraBlame original commit: a8db51f724578cf6a4bdcfeea7dce6f7e23ecbac

--- gfx/layers/ipc/ISurfaceAllocator.h
+++ gfx/layers/ipc/ISurfaceAllocator.h
@@ -245,6 +245,8 @@ class GfxMemoryImageReporter final : public nsIMemoryReporter {
 
 class FixedSizeSmallShmemSectionAllocator final : public ShmemSectionAllocator {
  public:
+  NS_DECL_OWNINGTHREAD
+
   enum AllocationStatus { STATUS_ALLOCATED, STATUS_FREED };
 
   struct ShmemSectionHeapHeader {
