db637fc5e8a1e2a70d27570160b631c4756a7291
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-01-25 19:19:10 +0000

    Bug 1875980 - Always notify context lost RemoteTextureOwner in WebGLContext::Present() r=gfx-reviewers,lsalzman
    During testing Bug 1863257, there were cases that test failed with timeout in RemoteTextureMap::WaitRemoteTextureReady(). In this case, RemoteTextureOwner was not registered since first Surface allocation was failed at GeckoProcessManager.getSurfaceAllocator().
    
    Even with the case, RemoteTextureOwner should be registered as context lost.
    
    Differential Revision: https://phabricator.services.mozilla.com/D199312
    
    UltraBlame original commit: 6bd565bc4e0a55535ad874c05acbeebdc6ac12e5

--- third_party/wayland-proxy/wayland-proxy.cpp
+++ third_party/wayland-proxy/wayland-proxy.cpp
@@ -461,10 +461,14 @@ bool ProxiedConnection::Process() {
 }
 
 bool WaylandProxy::SetupWaylandDisplays() {
-  char* waylandDisplay = getenv("WAYLAND_DISPLAY");
+  char* waylandDisplay = getenv("WAYLAND_DISPLAY_COMPOSITOR");
   if (!waylandDisplay) {
-    Error("Init(), Missing Wayland display, WAYLAND_DISPLAY is empty.", false);
-    return false;
+    waylandDisplay = getenv("WAYLAND_DISPLAY");
+    if (!waylandDisplay) {
+      Error("Init(), Missing Wayland display, WAYLAND_DISPLAY is empty.",
+            false);
+      return false;
+    }
   }
 
   char* XDGRuntimeDir = getenv("XDG_RUNTIME_DIR");
@@ -488,6 +492,10 @@ bool WaylandProxy::SetupWaylandDisplays() {
       return false;
     }
   }
+
+  
+  setenv("WAYLAND_DISPLAY_COMPOSITOR", waylandDisplay,  true);
+
   int ret = snprintf(mWaylandProxy, sMaxDisplayNameLen,
                      "%s/wayland-proxy-%d", XDGRuntimeDir, getpid());
   if (ret < 0 || ret >= sMaxDisplayNameLen) {
@@ -541,13 +549,18 @@ bool WaylandProxy::Init() {
 
 void WaylandProxy::SetWaylandProxyDisplay() {
   Info("SetWaylandProxyDisplay() WAYLAND_DISPLAY %s\n", mWaylandDisplay);
-  setenv("WAYLAND_DISPLAY", mWaylandProxy, true);
+  setenv("WAYLAND_DISPLAY", mWaylandProxy,  true);
 }
 
-void WaylandProxy::SetWaylandDisplay() {
-  Info("SetWaylandDisplay() WAYLAND_DISPLAY %s\n", mWaylandDisplay);
-  setenv("WAYLAND_DISPLAY", mWaylandDisplay, true);
+void WaylandProxy::RestoreWaylandDisplay() {
   unlink(mWaylandProxy);
+  char* waylandDisplay = getenv("WAYLAND_DISPLAY_COMPOSITOR");
+  if (waylandDisplay) {
+    Info("RestoreWaylandDisplay() WAYLAND_DISPLAY restored to %s\n",
+         waylandDisplay);
+    setenv("WAYLAND_DISPLAY", waylandDisplay,  true);
+    unsetenv("WAYLAND_DISPLAY_COMPOSITOR");
+  }
 }
 
 bool WaylandProxy::IsChildAppTerminated() {
@@ -681,7 +694,7 @@ WaylandProxy::~WaylandProxy() {
   if (mProxyServerSocket != -1) {
     close(mProxyServerSocket);
   }
-  SetWaylandDisplay();
+  RestoreWaylandDisplay();
 }
 
 void* WaylandProxy::RunProxyThread(WaylandProxy* aProxy) {
@@ -745,7 +758,7 @@ bool WaylandProxy::RunThread() {
   if (!mThreadRunning) {
     ErrorPlain("pthread_create() failed\n");
     
-    SetWaylandDisplay();
+    RestoreWaylandDisplay();
   }
 
   pthread_attr_destroy(&attr);
