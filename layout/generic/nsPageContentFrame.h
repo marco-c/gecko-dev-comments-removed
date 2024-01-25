e06e4255744ea9780878109647925108285f608a
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-01-25 19:35:30 +0000

    Bug 1865012 Part 3 - Remove FirstContinuation overrides for nsPageFrame and nsPageContentFrame. r=dholbert
    The overrides were added in bug 1803861. With Part 1 , they are no longer
    needed.
    
    Actually, without this patch, the assertion "First descendent of
    nsPageSequenceFrame should not have a previous continuation" in
    `nsPageFrame::FirstContinuation` will be triggered when constructing frame tree,
    because `nsSplittableFrame::SetPrevContinuation` now accesses
    `FirstContinuation()`, but the parent-child relationship might not properly set
    up yet.
    
    Differential Revision: https://phabricator.services.mozilla.com/D197758
    
    UltraBlame original commit: a54fc613ccde72bdf2ecb9839fb770f0d3045d65

--- layout/generic/nsPageContentFrame.h
+++ layout/generic/nsPageContentFrame.h
@@ -46,13 +46,6 @@ class nsPageContentFrame final : public mozilla::ViewportFrame {
 
   void EnsurePageName();
 
-  
-  
-  
-  
-  
-  nsIFrame* FirstContinuation() const final;
-
 #ifdef DEBUG_FRAME_DUMP
   
   nsresult GetFrameName(nsAString& aResult) const override;
