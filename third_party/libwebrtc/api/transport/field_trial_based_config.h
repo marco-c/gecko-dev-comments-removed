b6724bd4962abf0137df0210c8c459e1552df1ff
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-01-25 19:25:16 +0000

    Bug 1871981 - Vendor libwebrtc from 49dcb200ab
    Upstream commit: https://webrtc.googlesource.com/src/+/49dcb200ab481d0bd3688c7b172a9dd19441c26d
        Export FieldTrialBasedConfig in order to use it in Chromium.
    
        Goal is to initialize peerconnections in Chromium using this based
        field trial config until a proper config that doesn't rely on the
        global field trial string can be used (https://crrev.com/c/4936314).
    
        Change-Id: I3d006e2445ccc4880b73b564c8ad4408242d3696
        Bug: None
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/323621
        Reviewed-by: Björn Terelius <tereliuswebrtc.org>
        Reviewed-by: Emil Lundmark <lndmrkwebrtc.org>
        Commit-Queue: Jeremy Leconte <jlecontegoogle.com>
        Cr-Commit-Position: refs/heads/main{#40941}
    
    UltraBlame original commit: 62d20e07097b16a0e0ec0c6689663216ac94a3a6

--- third_party/libwebrtc/api/transport/field_trial_based_config.h
+++ third_party/libwebrtc/api/transport/field_trial_based_config.h
@@ -14,10 +14,11 @@
 
 #include "absl/strings/string_view.h"
 #include "api/field_trials_registry.h"
+#include "rtc_base/system/rtc_export.h"
 
 namespace webrtc {
 
-class FieldTrialBasedConfig : public FieldTrialsRegistry {
+class RTC_EXPORT FieldTrialBasedConfig : public FieldTrialsRegistry {
  private:
   std::string GetValue(absl::string_view key) const override;
 };
