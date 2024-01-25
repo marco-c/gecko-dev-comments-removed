b95df2a1eb1e577f72e519044ded928ff9fb832b
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-01-25 19:15:02 +0000

    Bug 1875079 - Part 1: Toggle touchmode from nsXULPopupManager r=desktop-theme-reviewers,dao,emilio
    Differential Revision: https://phabricator.services.mozilla.com/D198801
    
    UltraBlame original commit: 3a62cca5ea266449290b88360d1b495c7fbece5d

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -1955,7 +1955,6 @@ var gBrowserInit = {
     }
 
     FullScreen.init();
-    MenuTouchModeObserver.init();
 
     if (AppConstants.MOZ_DATA_REPORTING) {
       gDataNotificationInfoBar.init();
@@ -2569,7 +2568,6 @@ var gBrowserInit = {
       );
       Services.obs.removeObserver(gKeywordURIFixup, "keyword-uri-fixup");
 
-      MenuTouchModeObserver.uninit();
       BrowserOffline.uninit();
       CanvasPermissionPromptHelper.uninit();
       WebAuthnPromptHelper.uninit();
@@ -8722,27 +8720,6 @@ var RestoreLastSessionObserver = {
     "nsIObserver",
     "nsISupportsWeakReference",
   ]),
-};
-
-
-
-var MenuTouchModeObserver = {
-  init() {
-    window.addEventListener("popupshowing", this, true);
-  },
-
-  handleEvent(event) {
-    let target = event.originalTarget;
-    if (event.inputSource == MouseEvent.MOZ_SOURCE_TOUCH) {
-      target.setAttribute("touchmode", "true");
-    } else {
-      target.removeAttribute("touchmode");
-    }
-  },
-
-  uninit() {
-    window.removeEventListener("popupshowing", this, true);
-  },
 };
 
 
