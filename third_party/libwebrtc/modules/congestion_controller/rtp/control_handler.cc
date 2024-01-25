4f3d8d7fb0fdadea5ecc33af61ed706c23b7ee90
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-01-25 19:27:42 +0000

    Bug 1871981 - Vendor libwebrtc from ef4c71c204
    Upstream commit: https://webrtc.googlesource.com/src/+/ef4c71c204803493cf0b27d3c146494a01830c5c
        Change expectation of GoogCCNetworkController::OnNetworkAvailability
    
        Expect OnNetworkAvailabability to be invoked when the transport becomes writable.
        Before this change, ProbeController in GoogCC was expected to be created when the transport is writable or explicitly  notifed after creation that network is not writable.
    
        Bug: None
        Change-Id: I623b1c34e40a82e912f85b92fea49629e7e72d4e
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/323463
        Reviewed-by: Diep Bui <diepbpwebrtc.org>
        Commit-Queue: Per Kjellander <perkjwebrtc.org>
        Reviewed-by: Erik Språng <sprangwebrtc.org>
        Cr-Commit-Position: refs/heads/main{#40975}
    
    UltraBlame original commit: 4238a122ff648a76e82912ae92642c2f7daaad46

--- third_party/libwebrtc/modules/congestion_controller/rtp/control_handler.cc
+++ third_party/libwebrtc/modules/congestion_controller/rtp/control_handler.cc
@@ -30,9 +30,7 @@ bool IsPacerEmergencyStopDisabled() {
 
 }  
 CongestionControlHandler::CongestionControlHandler()
-    : disable_pacer_emergency_stop_(IsPacerEmergencyStopDisabled()) {
-  sequenced_checker_.Detach();
-}
+    : disable_pacer_emergency_stop_(IsPacerEmergencyStopDisabled()) {}
 
 CongestionControlHandler::~CongestionControlHandler() {}
 
