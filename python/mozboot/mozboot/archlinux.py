0b7096cff80d648c225591e50c9a3429a55b0cf6
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-01-25 19:21:23 +0000

    Bug 1869093 - When bootstrapping on Arch Linux check if packages are already installed before try to install them. r=ahochheiden,firefox-build-system-reviewers,sergesanspaille
    pacman_install() uses sudo pacman -S to install packages, which requires the user to enter their password for sudo even if there's nothing to do. Checking for installed packages using pacman -Q allows us to skip calling sudo when all packages are  already installed.
    
    Differential Revision: https://phabricator.services.mozilla.com/D199242
    
    UltraBlame original commit: ee4c3e7d20ff07ae9b38415476b4bd18d7be2b28

--- python/mozboot/mozboot/archlinux.py
+++ python/mozboot/mozboot/archlinux.py
@@ -2,6 +2,7 @@
 
 
 
+import subprocess
 import sys
 
 from mozboot.base import BaseBootstrapper
@@ -24,6 +25,24 @@ class ArchlinuxBootstrapper(LinuxBootstrapper, BaseBootstrapper):
         self.pacman_install("mercurial")
 
     def pacman_install(self, *packages):
+        def is_installed(package):
+            pacman_query = subprocess.run(
+                ["pacman", "-Q", package],
+                stdout=subprocess.PIPE,
+                stderr=subprocess.STDOUT,
+                check=False,
+            )
+            if pacman_query.returncode not in [0, 1]:
+                raise Exception(
+                    f'Failed to query pacman whether "{package}" is installed: "{pacman_query.stdout}"'
+                )
+            return pacman_query.returncode == 0
+
+        packages = [p for p in packages if not is_installed(p)]
+        
+        if not packages:
+            return
+
         command = ["pacman", "-S", "--needed"]
         if self.no_interactive:
             command.append("--noconfirm")
