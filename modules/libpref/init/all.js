c2bb56da1e487ab3f79ec2533f287266b68e9127
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-01-25 19:18:42 +0000

    Bug 1873223 - Modified firefox and firefox-private protocol handler behavior r=Gijs,mhughes
    Differential Revision: https://phabricator.services.mozilla.com/D197836
    
    UltraBlame original commit: 41378df4fd06bd29de847d28ee34e73cfea3ecf6

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -1068,6 +1068,8 @@ pref("network.protocol-handler.external.disk", false);
 pref("network.protocol-handler.external.disks", false);
 pref("network.protocol-handler.external.afp", false);
 pref("network.protocol-handler.external.moz-icon", false);
+pref("network.protocol-handler.external.firefox", false);
+pref("network.protocol-handler.external.firefox-private", false);
 
 
 pref("network.protocol-handler.external.ttp", false);  
