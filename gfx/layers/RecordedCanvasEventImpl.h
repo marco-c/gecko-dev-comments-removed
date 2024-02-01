157d2a073c33c5f6f4efb63ddecba3146870a693
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-02-01 00:32:28 +0000

    Bug 1876538 - Backout changes from bug 1875364 to cache current filter for recordings. r=gfx-reviewers,jrmuizel
    Differential Revision: https://phabricator.services.mozilla.com/D199723
    
    UltraBlame original commit: 828e8cbfdf572aa6c7a5ddddc636f9ffaff75605

--- gfx/layers/RecordedCanvasEventImpl.h
+++ gfx/layers/RecordedCanvasEventImpl.h
@@ -522,9 +522,6 @@ inline bool RecordedCanvasDrawTargetCreation::PlayCanvasEvent(
     CanvasTranslator* aTranslator) const {
   RefPtr<DrawTarget> newDT = aTranslator->CreateDrawTarget(
       mRefPtr, mTextureId, mTextureOwnerId, mSize, mFormat);
-  if (newDT) {
-    aTranslator->SetCurrentDrawTarget(mRefPtr);
-  }
 
   
   
