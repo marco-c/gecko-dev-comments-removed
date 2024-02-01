970fa399fad6184a786267c494d99883ba8a5834
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-02-01 00:50:16 +0000

    Bug 1868838 - Move ASRouter into browser/components/asrouter/modules. r=pdahiya,pip-reviewers,fxview-reviewers,sclements
    Differential Revision: https://phabricator.services.mozilla.com/D198874
    
    UltraBlame original commit: 57f85b3f95629d9cd1e5d23348c416cb6ca04311

--- browser/components/newtab/test/browser/browser_trigger_messagesLoaded.js
+++ browser/components/newtab/test/browser/browser_trigger_messagesLoaded.js
@@ -3,7 +3,7 @@
 
 
 const { ASRouter } = ChromeUtils.import(
-  "resource://activity-stream/lib/ASRouter.jsm"
+  "resource:///modules/asrouter/ASRouter.jsm"
 );
 const { RemoteSettings } = ChromeUtils.importESModule(
   "resource://services-settings/remote-settings.sys.mjs"
