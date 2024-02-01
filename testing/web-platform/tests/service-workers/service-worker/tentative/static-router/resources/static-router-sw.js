e43792b0c083f51ec1205695529fca9541774b29
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-02-01 00:37:46 +0000

    Bug 1876222 [wpt PR 44157] - Change requestMethod from USVString to ByteString., a=testonly
    Automatic update from web-platform-tests
    Change requestMethod from USVString to ByteString.
    
    Since Request API's method is ByteString, requestMethod in the static
    routing API should also be aligned with it.
    
    This is for addressing comments in:
    https://github.com/yoshisatoyanagisawa/ServiceWorker/pull/6#discussion_r1461372512
    
    Bug: 1371756, 1519727
    Change-Id: I9ccfaa11c9d45e09579d3c9a43576f8abc9f8c3a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5223724
    Commit-Queue: Yoshisato Yanagisawa <yyanagisawachromium.org>
    Reviewed-by: Shunya Shishido <sisidovskichromium.org>
    Reviewed-by: Minoru Chikamune <chikamunechromium.org>
    Cr-Commit-Position: refs/heads/main{#1251895}
    
    --
    
    wpt-commits: e7b773cb7892b3db5190e94cb0945d84bf88a5f4
    wpt-pr: 44157
    
    UltraBlame original commit: 31f601817308d15aa16e0d075bac60bcc7fd7c00

--- testing/web-platform/tests/service-workers/service-worker/tentative/static-router/resources/static-router-sw.js
+++ testing/web-platform/tests/service-workers/service-worker/tentative/static-router/resources/static-router-sw.js
@@ -3,11 +3,19 @@
 import {routerRules} from './router-rules.js';
 
 var requests = [];
+var errors = [];
 
 self.addEventListener('install', async e => {
+  e.waitUntil(caches.open('v1').then(
+      cache => {cache.put('cache.txt', new Response('From cache'))}));
+
   const params = new URLSearchParams(location.search);
   const key = params.get('key');
-  await e.addRoutes(routerRules[key]);
+  try {
+    await e.addRoutes(routerRules[key]);
+  } catch (e) {
+    errors.push(e);
+  }
   self.skipWaiting();
 });
 
@@ -23,6 +31,13 @@ self.addEventListener('fetch', function(event) {
 });
 
 self.addEventListener('message', function(event) {
-  event.data.port.postMessage({requests: requests});
-  requests = [];
+  let r = requests;
+  let e = errors;
+  if (event.data.reset) {
+    requests = [];
+    errors = [];
+  }
+  if (event.data.port) {
+    event.data.port.postMessage({requests: r, errors: e});
+  }
 });
