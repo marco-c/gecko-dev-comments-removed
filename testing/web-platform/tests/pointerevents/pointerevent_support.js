406b4673fc9ff1b6d21cfa2e9d921cec6c3f6f00
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-02-01 01:03:02 +0000

    Bug 1877131 [wpt PR 44264] - Reland #44147 with test split, a=testonly
    Automatic update from web-platform-tests
    Consolidate pointer attribute testing
    
    The primary goal of this patch was to address memory leaks. These
    leaks were likely due to pending promises at the completion of the test.
    
    The 3 tests for hover pointers, non-hover pointers and hover with
    right click were all very similar in structure and were consolidated
    in the patch.  We now have 5 variants instead of 3 separate tests.
    
    In the process the tests were modernized to be promise tests instead
    of an async test. The structure was reformatted to remove the manual
    test feel.
    
    Bug: 1430360, 1499775, 1446799
    Change-Id: Ia6dc48405d2e2197e3c020fdb803d836043698f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5229777
    Commit-Queue: Kevin Ellis <keverschromium.org>
    Reviewed-by: Mustaq Ahmed <mustaqchromium.org>
    Cr-Commit-Position: refs/heads/main{#1252074}
    
    --
    Move toElement and fromElement tests into a seperate variant
    
    These properties are currently not standardised.
    
    Since this test is in Interop we can't just use assert_precondition
    for the nonstandard parts. Instead move these properties into a
    separate variant whislt we figure out the long term interop story for them.
    
    --
    
    wpt-commits: 6d5a75c52050fcfb47d246ba2f41e1bc7455b919, e29c9b8888580e3425e9b0b2f4b21fd49d9f24ed
    wpt-pr: 44264
    
    UltraBlame original commit: 7309d7e7cb2ec7b80cc1f5e47cf05be60edfae28

--- testing/web-platform/tests/pointerevents/pointerevent_support.js
+++ testing/web-platform/tests/pointerevents/pointerevent_support.js
@@ -44,7 +44,7 @@ const ButtonsBitfield = {
 
 
 
-function check_PointerEvent(event, testNamePrefix) {
+function check_PointerEvent(event, testNamePrefix, standardAttrs = true) {
   if (testNamePrefix === undefined)
     testNamePrefix = "";
 
@@ -52,16 +52,17 @@ function check_PointerEvent(event, testNamePrefix) {
   var pointerTestName = (testNamePrefix ? testNamePrefix + ' ' : '')
     + (expectedPointerType == null ? event.pointerType : expectedPointerType) + ' ' + event.type;
 
-  if (expectedPointerType != null) {
+  if (standardAttrs) {
+    if (expectedPointerType != null) {
+      test(function () {
+        assert_equals(event.pointerType, expectedPointerType);
+      }, pointerTestName + ".pointerType is correct.");
+    }
+
     test(function () {
-      assert_equals(event.pointerType, expectedPointerType);
-    }, pointerTestName + ".pointerType is correct.");
+      assert_true(event instanceof event.target.ownerDocument.defaultView.PointerEvent);
+    }, pointerTestName + " event is a PointerEvent event");
   }
-
-  test(function () {
-    assert_true(event instanceof event.target.ownerDocument.defaultView.PointerEvent);
-  }, pointerTestName + " event is a PointerEvent event");
-
 
   
   var idl_type_check = {
@@ -74,65 +75,68 @@ function check_PointerEvent(event, testNamePrefix) {
 
   
   
-  test(function () {
-    assert_implements_optional("fromElement" in event);
-    assert_equals(event.fromElement, null);
-  }, pointerTestName + ".fromElement value is null");
-  test(function () {
-    assert_implements_optional("toElement" in event);
-    assert_equals(event.toElement, null);
-  }, pointerTestName + ".toElement value is null");
-  test(function () {
-    assert_equals(event.isTrusted, true);
-  }, pointerTestName + ".isTrusted value is true");
-  test(function () {
-    let expected = (event.type != 'pointerenter' && event.type != 'pointerleave');
-    assert_equals(event.composed, expected);
-  }, pointerTestName + ".composed value is valid");
-  test(function () {
-    let expected = (event.type != 'pointerenter' && event.type != 'pointerleave');
-    assert_equals(event.bubbles, expected);
-  }, pointerTestName + ".bubbles value is valid");
-  test(function () {
-    let cancelable_events = [
-      'pointerdown', 'pointermove', 'pointerup', 'pointerover', 'pointerout'
-    ];
-    assert_equals(event.cancelable, cancelable_events.includes(event.type));
-  }, pointerTestName + ".cancelable value is valid");
+  if (!standardAttrs) {
+    test(function () {
+      assert_implements_optional("fromElement" in event);
+      assert_equals(event.fromElement, null);
+    }, pointerTestName + ".fromElement value is null");
+    test(function () {
+      assert_implements_optional("toElement" in event);
+      assert_equals(event.toElement, null);
+    }, pointerTestName + ".toElement value is null");
+  } else {
+    test(function () {
+      assert_equals(event.isTrusted, true);
+    }, pointerTestName + ".isTrusted value is true");
+    test(function () {
+      let expected = (event.type != 'pointerenter' && event.type != 'pointerleave');
+      assert_equals(event.composed, expected);
+    }, pointerTestName + ".composed value is valid");
+    test(function () {
+      let expected = (event.type != 'pointerenter' && event.type != 'pointerleave');
+      assert_equals(event.bubbles, expected);
+    }, pointerTestName + ".bubbles value is valid");
+    test(function () {
+      let cancelable_events = [
+        'pointerdown', 'pointermove', 'pointerup', 'pointerover', 'pointerout'
+      ];
+      assert_equals(event.cancelable, cancelable_events.includes(event.type));
+    }, pointerTestName + ".cancelable value is valid");
 
-  
-  
-  test(function () {
-    assert_greater_than_equal(event.pressure, 0, "pressure is greater than or equal to 0");
-    assert_less_than_equal(event.pressure, 1, "pressure is less than or equal to 1");
+    
+    
+    test(function () {
+      assert_greater_than_equal(event.pressure, 0, "pressure is greater than or equal to 0");
+      assert_less_than_equal(event.pressure, 1, "pressure is less than or equal to 1");
 
-    if (event.buttons === 0) {
-      assert_equals(event.pressure, 0, "pressure is 0 with no buttons pressed");
-    } else {
-      assert_greater_than(event.pressure, 0, "pressure is greater than 0 with a button pressed");
-      if (event.pointerType === "mouse") {
-        assert_equals(event.pressure, 0.5, "pressure is 0.5 for mouse with a button pressed");
+      if (event.buttons === 0) {
+        assert_equals(event.pressure, 0, "pressure is 0 with no buttons pressed");
+      } else {
+        assert_greater_than(event.pressure, 0, "pressure is greater than 0 with a button pressed");
+        if (event.pointerType === "mouse") {
+          assert_equals(event.pressure, 0.5, "pressure is 0.5 for mouse with a button pressed");
+        }
       }
+    }, pointerTestName + ".pressure value is valid");
+
+    
+    if (event.pointerType === "mouse") {
+      test(function () {
+        assert_equals(event.width, 1, "width of mouse should be 1");
+        assert_equals(event.height, 1, "height of mouse should be 1");
+        assert_equals(event.tiltX, 0, event.type + ".tiltX is 0 for mouse");
+        assert_equals(event.tiltY, 0, event.type + ".tiltY is 0 for mouse");
+        assert_true(event.isPrimary, event.type + ".isPrimary is true for mouse");
+      }, pointerTestName + " properties for pointerType = mouse");
     }
-  }, pointerTestName + ".pressure value is valid");
 
-  
-  if (event.pointerType === "mouse") {
-    test(function () {
-      assert_equals(event.width, 1, "width of mouse should be 1");
-      assert_equals(event.height, 1, "height of mouse should be 1");
-      assert_equals(event.tiltX, 0, event.type + ".tiltX is 0 for mouse");
-      assert_equals(event.tiltY, 0, event.type + ".tiltY is 0 for mouse");
-      assert_true(event.isPrimary, event.type + ".isPrimary is true for mouse");
-    }, pointerTestName + " properties for pointerType = mouse");
-  }
-
-  
-  if (event.type == "pointerup") {
-    test(function () {
-      assert_equals(event.width, 1, "width of pointerup should be 1");
-      assert_equals(event.height, 1, "height of pointerup should be 1");
-    }, pointerTestName + " properties for pointerup");
+    
+    if (event.type == "pointerup") {
+      test(function () {
+        assert_equals(event.width, 1, "width of pointerup should be 1");
+        assert_equals(event.height, 1, "height of pointerup should be 1");
+      }, pointerTestName + " properties for pointerup");
+    }
   }
 }
 
