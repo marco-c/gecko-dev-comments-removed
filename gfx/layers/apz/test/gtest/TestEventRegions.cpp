2d10fc4098b823d30eeccf523ad272330743d0c3
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-02-08 15:06:08 +0000

    Bug 1877316 - Remove redundant parameter of APZCTesterBase::Tap() r=botond
    As of bug 1678505, APZCTesterBase::Tap() returns an APZEventResult
    which contains a mInputBlockId field that callers should use instead
    of passing an optional output parameter to this function.
    
    Differential Revision: https://phabricator.services.mozilla.com/D200062
    
    UltraBlame original commit: c1ca3cb878af776483480bf0e3ee3b3b232d9ba4

--- gfx/layers/apz/test/gtest/TestEventRegions.cpp
+++ gfx/layers/apz/test/gtest/TestEventRegions.cpp
@@ -150,14 +150,14 @@ TEST_F(APZEventRegionsTesterMock, HitRegionImmediateResponse) {
   check.Call("Tapped on bottom again");
 
   
-  uint64_t inputBlockId = 0;
   QueueMockHitResult(ScrollableLayerGuid::START_SCROLL_ID + 2,
                      {CompositorHitTestFlags::eVisibleToHitTest,
                       CompositorHitTestFlags::eIrregularArea});
-  Tap(manager, ScreenIntPoint(10, 110), tapDuration, nullptr, &inputBlockId);
+  APZEventResult result =
+      Tap(manager, ScreenIntPoint(10, 110), tapDuration, nullptr);
   nsTArray<ScrollableLayerGuid> targets;
   targets.AppendElement(left->GetGuid());
-  manager->SetTargetAPZC(inputBlockId, targets);
+  manager->SetTargetAPZC(result.mInputBlockId, targets);
   while (mcc->RunThroughDelayedTasks())
     ;  
   check.Call("Tapped on left this time");
@@ -184,12 +184,11 @@ TEST_F(APZEventRegionsTesterMock, Bug1117712) {
 
   
   
-  uint64_t inputBlockId = 0;
   QueueMockHitResult(ScrollableLayerGuid::START_SCROLL_ID + 1,
                      {CompositorHitTestFlags::eVisibleToHitTest,
                       CompositorHitTestFlags::eIrregularArea});
-  Tap(manager, ScreenIntPoint(55, 5), TimeDuration::FromMilliseconds(100),
-      nullptr, &inputBlockId);
+  APZEventResult result = Tap(manager, ScreenIntPoint(55, 5),
+                              TimeDuration::FromMilliseconds(100), nullptr);
   
   
   EXPECT_CALL(*mcc, HandleTap(TapType::eSingleTap, LayoutDevicePoint(55, 5), 0,
@@ -198,5 +197,5 @@ TEST_F(APZEventRegionsTesterMock, Bug1117712) {
 
   nsTArray<ScrollableLayerGuid> targets;
   targets.AppendElement(apzc2->GetGuid());
-  manager->SetTargetAPZC(inputBlockId, targets);
+  manager->SetTargetAPZC(result.mInputBlockId, targets);
 }
