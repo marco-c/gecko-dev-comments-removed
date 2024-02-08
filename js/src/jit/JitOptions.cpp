8fab9b26c4a55ce0672920dc34142d8fedddedb8
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-02-08 15:05:55 +0000

    Backed out changeset dabc78ed3644 (bug 1877605) for causing mochitest failures at js/xpconnect/tests/mochitest/test_spectre_mitigations.html CLOSED TREE
    UltraBlame original commit: 5078f642465ee9fb0566b22583704f6edeeb4fc5

--- js/src/jit/JitOptions.cpp
+++ js/src/jit/JitOptions.cpp
@@ -297,7 +297,7 @@ DefaultJitOptions::DefaultJitOptions() {
   SET_DEFAULT(spectreObjectMitigations, true);
   SET_DEFAULT(spectreStringMitigations, true);
   SET_DEFAULT(spectreValueMasking, true);
-  SET_DEFAULT(spectreJitToCxxCalls, false);
+  SET_DEFAULT(spectreJitToCxxCalls, true);
 #endif
 
   
