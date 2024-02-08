fcbd316c2ac29e66d7777b6ffb32416e6d49e236
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-02-08 15:08:30 +0000

    Bug 1874723 - Accept imprecision when trimming audio content media in certain cases. r=alwu
    In the test case, the time base is 69 (generated by a fuzzer), and so there's a
    very large loss in precision in the computation, and eventually the assumption
    doesn't hold. It should however hold for the common case.
    
    Differential Revision: https://phabricator.services.mozilla.com/D200176
    
    UltraBlame original commit: 0d9c1ac74d9cf147aa99fad91c7e1eca0e24fa1b

--- dom/media/TimeUnits.h
+++ dom/media/TimeUnits.h
@@ -118,6 +118,9 @@ class TimeUnit final {
   int64_t ToMicroseconds() const;
   int64_t ToNanoseconds() const;
   int64_t ToTicksAtRate(int64_t aRate) const;
+  
+  
+  bool IsBase(int64_t aBase) const;
   double ToSeconds() const;
   nsCString ToString() const;
   TimeDuration ToTimeDuration() const;
