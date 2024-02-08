dbc8cbda4baeeb48697f053de2601c3db83f3cac
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-02-08 15:10:25 +0000

    Bug 1876104 - Convert MomentsPageHub.jsm to an ESM. r=amy
    Differential Revision: https://phabricator.services.mozilla.com/D199413
    
    UltraBlame original commit: 0008b401ee9cb8d99a7ba91b67da1e2401c2603b

--- browser/components/newtab/lib/MomentsPageHub.sys.mjs
+++ browser/components/newtab/lib/MomentsPageHub.sys.mjs
@@ -1,7 +1,6 @@
-
-
-
-"use strict";
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 const lazy = {};
 
@@ -10,18 +9,18 @@ ChromeUtils.defineESModuleGetters(lazy, {
   clearInterval: "resource://gre/modules/Timer.sys.mjs",
 });
 
-
+// Frequency at which to check for new messages
 const SYSTEM_TICK_INTERVAL = 5 * 60 * 1000;
 const HOMEPAGE_OVERRIDE_PREF = "browser.startup.homepage_override.once";
 
-
+// For the "reach" event of Messaging Experiments
 const REACH_EVENT_CATEGORY = "messaging_experiments";
 const REACH_EVENT_METHOD = "reach";
-
-
+// Note it's not "moments-page" as Telemetry Events only accepts understores
+// for the event `object`
 const REACH_EVENT_OBJECT = "moments_page";
 
-class _MomentsPageHub {
+export class _MomentsPageHub {
   constructor() {
     this.id = "moments-page-hub";
     this.state = {};
@@ -43,7 +42,7 @@ class _MomentsPageHub {
     this._blockMessageById = blockMessageById;
     this._sendTelemetry = sendTelemetry;
 
-    
+    // Need to wait for ASRouter to initialize before trying to fetch messages
     await waitForInitialized;
 
     this.messageRequest({
@@ -73,13 +72,13 @@ class _MomentsPageHub {
     });
   }
 
-  
-
-
-
-
-
-
+  /**
+   * If we don't have `expire` defined with the message it could be because
+   * it depends on user dependent parameters. Since the message matched
+   * targeting we calculate `expire` based on the current timestamp and the
+   * `expireDelta` which defines for how long it should be available.
+   * @param expireDelta {number} - Offset in milliseconds from the current date
+   */
   getExpirationDate(expireDelta) {
     return Date.now() + expireDelta;
   }
@@ -93,13 +92,13 @@ class _MomentsPageHub {
         if (!expire) {
           expire = this.getExpirationDate(expireDelta);
         }
-        
-        
+        // In order to reset this action we can dispatch a new message that
+        // will overwrite the prev value with an expiration date from the past.
         Services.prefs.setStringPref(
           HOMEPAGE_OVERRIDE_PREF,
           JSON.stringify({ message_id: message.id, url, expire })
         );
-        
+        // Add impression and block immediately after taking the action
         this.sendUserEventTelemetry(message);
         this._addImpression(message);
         this._blockMessageById(message.id);
@@ -128,8 +127,8 @@ class _MomentsPageHub {
     });
     TelemetryStopwatch.finish("MS_MESSAGE_REQUEST_TIME_MS", telemetryObject);
 
-    
-    
+    // Record the "reach" event for all the messages with `forReachEvent`,
+    // only execute action for the first message without forReachEvent.
     const nonReachMessages = [];
     for (const message of messages) {
       if (message.forReachEvent) {
@@ -146,11 +145,11 @@ class _MomentsPageHub {
     }
   }
 
-  
-
-
-
-
+  /**
+   * Pref is set via Remote Settings message. We want to continously
+   * monitor new messages that come in to ensure the one with the
+   * highest priority is set.
+   */
   checkHomepageOverridePref() {
     this.messageRequest({
       triggerId: "momentsUpdate",
@@ -165,10 +164,8 @@ class _MomentsPageHub {
   }
 }
 
-
-
-
-
-const MomentsPageHub = new _MomentsPageHub();
-
-const EXPORTED_SYMBOLS = ["_MomentsPageHub", "MomentsPageHub"];
+/**
+ * ToolbarBadgeHub - singleton instance of _ToolbarBadgeHub that can initiate
+ * message requests and render messages.
+ */
+export const MomentsPageHub = new _MomentsPageHub();
