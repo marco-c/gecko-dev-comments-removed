e01ec86e61e58522acd71199d293cb469fad0ad7
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-02-20 09:31:08 +0000

    Bug 1880082 - Let the off-main-thread WindowsJumpList backend ride the trains. r=florian
    Differential Revision: https://phabricator.services.mozilla.com/D201717
    
    UltraBlame original commit: 262041b644913954a0fa86aeeb3b960b2b007abf

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1483,15 +1483,8 @@ pref("browser.bookmarks.editDialog.maxRecentFolders", 7);
   pref("browser.taskbar.previews.enable", false);
   pref("browser.taskbar.previews.max", 20);
   pref("browser.taskbar.previews.cachetime", 5);
-  pref("browser.taskbar.lists.legacyBackend", true);
 
-  
-  
-#ifdef NIGHTLY_BUILD
   pref("browser.taskbar.lists.legacyBackend", false);
-#else
-  pref("browser.taskbar.lists.legacyBackend", true);
-#endif
 
   pref("browser.taskbar.lists.enabled", true);
   pref("browser.taskbar.lists.frequent.enabled", true);
