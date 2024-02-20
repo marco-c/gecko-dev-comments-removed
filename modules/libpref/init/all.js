8202a407af245e86132cb8a124dc58d9c28783c1
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-02-20 09:38:59 +0000

    Bug 1862223: Set 'full-screen-api.macos-native-full-screen' pref to true. r=mstange
    Differential Revision: https://phabricator.services.mozilla.com/D200234
    
    UltraBlame original commit: 76cdab273373d590de28a7061bdffadb31deb727

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -3186,11 +3186,7 @@ pref("alerts.showFavicons", false);
 
 #ifdef XP_MACOSX
   
-  #ifdef NIGHTLY_BUILD
-    pref("full-screen-api.macos-native-full-screen", true);
-  #else
-    pref("full-screen-api.macos-native-full-screen", false);
-  #endif
+  pref("full-screen-api.macos-native-full-screen", true);
 #endif
 
 #ifndef MOZ_WIDGET_GTK
