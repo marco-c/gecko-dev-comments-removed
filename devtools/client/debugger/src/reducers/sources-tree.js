dca49c927ca77e1fc6be7da4107f59c61c60b443
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-02-20 09:29:35 +0000

    Bug 1879575 - [devtools] Add source items in the right position instead of sorting the whole array. r=devtools-reviewers,bomsy
    Differential Revision: https://phabricator.services.mozilla.com/D201386
    
    UltraBlame original commit: 57ab9d9420d98d6d120b7aaa9ae0eaa141db6aeb

--- devtools/client/debugger/src/reducers/sources-tree.js
+++ devtools/client/debugger/src/reducers/sources-tree.js
@@ -210,7 +210,8 @@ function addThread(state, thread) {
   });
   if (!threadItem) {
     threadItem = createThreadTreeItem(threadActorID);
-    state.threadItems = [...state.threadItems, threadItem];
+    state.threadItems = [...state.threadItems];
+    addSortedItem(state.threadItems, threadItem, sortThreadItems);
   } else {
     
     
@@ -221,7 +222,6 @@ function addThread(state, thread) {
   
   
   threadItem.thread = thread;
-  state.threadItems.sort(sortThreadItems);
 }
 
 function updateBlackbox(state, sources, shouldBlackBox) {
@@ -290,6 +290,24 @@ function isSourceVisibleInSourceTree(
   );
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+function addSortedItem(array, newValue, sortFunction) {
+  let index = array.findIndex(value => sortFunction(value, newValue) === 1);
+  index = index >= 0 ? index : array.length;
+  array.splice(index, 0, newValue);
+}
+
 function addSource(threadItems, source, sourceActor) {
   
   let threadItem = threadItems.find(item => {
@@ -299,8 +317,7 @@ function addSource(threadItems, source, sourceActor) {
     threadItem = createThreadTreeItem(sourceActor.thread);
     
     
-    threadItems.push(threadItem);
-    threadItems.sort(sortThreadItems);
+    addSortedItem(threadItems, threadItem, sortThreadItems);
   }
 
   
@@ -316,9 +333,9 @@ function addSource(threadItems, source, sourceActor) {
     groupItem = createGroupTreeItem(group, threadItem, source);
     
     
-    threadItem.children = [...threadItem.children, groupItem];
-    
-    threadItem.children.sort(sortItems);
+    threadItem.children = [...threadItem.children];
+
+    addSortedItem(threadItem.children, groupItem, sortItems);
   }
 
   
@@ -340,9 +357,8 @@ function addSource(threadItems, source, sourceActor) {
   const sourceItem = createSourceTreeItem(source, sourceActor, directoryItem);
   
   
-  directoryItem.children = [...directoryItem.children, sourceItem];
-  
-  directoryItem.children.sort(sortItems);
+  directoryItem.children = [...directoryItem.children];
+  addSortedItem(directoryItem.children, sourceItem, sortItems);
 
   return true;
 }
@@ -476,9 +492,9 @@ function addOrGetParentDirectory(groupItem, path) {
   const directory = createDirectoryTreeItem(path, parentDirectory);
   
   
-  parentDirectory.children = [...parentDirectory.children, directory];
-  
-  parentDirectory.children.sort(sortItems);
+  parentDirectory.children = [...parentDirectory.children];
+
+  addSortedItem(parentDirectory.children, directory, sortItems);
 
   
   
