d3ee90ee1260d24f7b54be6c1d6172662a7a7aad
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-05-01 03:55:04 +0000

    Bug 1885559 - Allow to listen to http-on-before-stop-request from child processes r=valentin
    Differential Revision: https://phabricator.services.mozilla.com/D204771
    
    UltraBlame original commit: 382ad38d0fa4aea83b99388613858d3c50f5edda

--- xpcom/ds/nsObserverService.cpp
+++ xpcom/ds/nsObserverService.cpp
@@ -183,10 +183,11 @@ nsresult nsObserverService::FilterHttpOnTopics(const char* aTopic) {
   
   
   if (mozilla::net::IsNeckoChild() && !strncmp(aTopic, "http-on-", 8) &&
+      strcmp(aTopic, "http-on-before-stop-request") &&
       strcmp(aTopic, "http-on-failed-opening-request") &&
+      strcmp(aTopic, "http-on-image-cache-response") &&
       strcmp(aTopic, "http-on-opening-request") &&
-      strcmp(aTopic, "http-on-stop-request") &&
-      strcmp(aTopic, "http-on-image-cache-response")) {
+      strcmp(aTopic, "http-on-stop-request")) {
     nsCOMPtr<nsIConsoleService> console(
         do_GetService(NS_CONSOLESERVICE_CONTRACTID));
     nsCOMPtr<nsIScriptError> error(
