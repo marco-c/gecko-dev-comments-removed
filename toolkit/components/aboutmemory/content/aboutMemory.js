71324e96a9763394339bba5e215a599fa7274d4b
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-05-01 02:05:06 +0000

    Bug 1878401 - part 3 - Update nsIFilePicker users in toolkit/ to pass BrowsingContext to init r=extension-reviewers,Gijs,ng,robwu
    Depends on D200547
    
    Differential Revision: https://phabricator.services.mozilla.com/D200548
    
    UltraBlame original commit: cf7bb73f0006475ce48f4f0c5b2f19245c73d15c

--- toolkit/components/aboutmemory/content/aboutMemory.js
+++ toolkit/components/aboutmemory/content/aboutMemory.js
@@ -2469,7 +2469,11 @@ function saveReportsToFile() {
   };
 
   try {
-    fp.init(window, "Save Memory Reports", Ci.nsIFilePicker.modeSave);
+    fp.init(
+      window.browsingContext,
+      "Save Memory Reports",
+      Ci.nsIFilePicker.modeSave
+    );
   } catch (ex) {
     
     
