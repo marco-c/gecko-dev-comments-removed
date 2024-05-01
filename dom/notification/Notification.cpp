57ef8c889db25cf9a6b39c668cc88bc0880c48f6
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-05-01 02:18:27 +0000

    Bug 1879112 - Check permission earlier and mark as closed if failed r=asuth
    Differential Revision: https://phabricator.services.mozilla.com/D201140
    
    UltraBlame original commit: 2b3b6edc0cff57f6c249e09795b24ef5ac1952c0

--- dom/notification/Notification.cpp
+++ dom/notification/Notification.cpp
@@ -1316,6 +1316,8 @@ bool Notification::IsInPrivateBrowsing() {
   return false;
 }
 
+
+
 void Notification::ShowInternal() {
   AssertIsOnMainThread();
   MOZ_ASSERT(mTempRef,
@@ -1330,13 +1332,15 @@ void Notification::ShowInternal() {
   std::swap(ownership, mTempRef);
   MOZ_ASSERT(ownership->GetNotification() == this);
 
-  nsresult rv = PersistNotification();
-  if (NS_FAILED(rv)) {
-    NS_WARNING("Could not persist Notification");
-  }
-
   nsCOMPtr<nsIAlertsService> alertService = components::Alerts::Service();
 
+  
+  
+  
+  
+  
+  
+  
   ErrorResult result;
   NotificationPermission permission = NotificationPermission::Denied;
   if (mWorkerPrivate) {
@@ -1357,12 +1361,27 @@ void Notification::ShowInternal() {
     } else {
       DispatchTrustedEvent(u"error"_ns);
     }
+    mIsClosed = true;
     return;
   }
 
+  
+  
   nsAutoString iconUrl;
   nsAutoString soundUrl;
   ResolveIconAndSoundURL(iconUrl, soundUrl);
+
+  
+  
+  
+  
+  
+
+  
+  nsresult rv = PersistNotification();
+  if (NS_FAILED(rv)) {
+    NS_WARNING("Could not persist Notification");
+  }
 
   bool isPersistent = false;
   nsCOMPtr<nsIObserver> observer;
