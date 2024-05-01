b00c275c2f1c7341a2e4bc6f1acabae501abf792
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-05-01 00:48:59 +0000

    Bug 1876843 - Vendor libwebrtc from 9aa115358e
    Upstream commit: https://webrtc.googlesource.com/src/+/9aa115358e9b1f3abb83d048d9a35e1accad2989
        Delete CreatePeerConnectionFactory variant that accepts unowned audio frame processor
    
        Bug: webrtc:15111
        Change-Id: I44d8262f0e0a96f3b4d9d53641ec291d35a32579
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/325263
        Reviewed-by: Harald Alvestrand <htawebrtc.org>
        Reviewed-by: Peter Hanspers <peterhanspersgoogle.com>
        Commit-Queue: Danil Chapovalov <danilchapwebrtc.org>
        Cr-Commit-Position: refs/heads/main{#41066}
    
    UltraBlame original commit: f68577a5d2141dd9dff87bc482863630c2fe2aa0

--- third_party/libwebrtc/api/create_peerconnection_factory.h
+++ third_party/libwebrtc/api/create_peerconnection_factory.h
@@ -37,9 +37,6 @@ class AudioProcessing;
 
 
 
-
-
-
 RTC_EXPORT rtc::scoped_refptr<PeerConnectionFactoryInterface>
 CreatePeerConnectionFactory(
     rtc::Thread* network_thread,
@@ -52,21 +49,7 @@ CreatePeerConnectionFactory(
     std::unique_ptr<VideoDecoderFactory> video_decoder_factory,
     rtc::scoped_refptr<AudioMixer> audio_mixer,
     rtc::scoped_refptr<AudioProcessing> audio_processing,
-    AudioFrameProcessor* audio_frame_processor = nullptr);
-
-RTC_EXPORT rtc::scoped_refptr<PeerConnectionFactoryInterface>
-CreatePeerConnectionFactory(
-    rtc::Thread* network_thread,
-    rtc::Thread* worker_thread,
-    rtc::Thread* signaling_thread,
-    rtc::scoped_refptr<AudioDeviceModule> default_adm,
-    rtc::scoped_refptr<AudioEncoderFactory> audio_encoder_factory,
-    rtc::scoped_refptr<AudioDecoderFactory> audio_decoder_factory,
-    std::unique_ptr<VideoEncoderFactory> video_encoder_factory,
-    std::unique_ptr<VideoDecoderFactory> video_decoder_factory,
-    rtc::scoped_refptr<AudioMixer> audio_mixer,
-    rtc::scoped_refptr<AudioProcessing> audio_processing,
-    std::unique_ptr<AudioFrameProcessor> owned_audio_frame_processor,
+    std::unique_ptr<AudioFrameProcessor> audio_frame_processor = nullptr,
     std::unique_ptr<FieldTrialsView> field_trials = nullptr);
 
 }  
