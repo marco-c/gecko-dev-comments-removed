faa378a15a1c5599a542d79fe2ffc15e3bb6d473
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-05-01 03:12:25 +0000

    Bug 1751995 - Add TestOnlyWaitForTransactionCompletion to MessageChannel r=ipc-reviewers,nika
    This method (intended to be used only in tests) blocks until the current transaction stack is
    complete, and is used by TestHangs to determine when to continue in the HangButReceive subtest.
    
    Differential Revision: https://phabricator.services.mozilla.com/D203964
    
    UltraBlame original commit: ee06381e3e1bd5677282bb4e929a37ef9d68fcb8

--- ipc/glue/MessageChannel.h
+++ ipc/glue/MessageChannel.h
@@ -294,6 +294,11 @@ class MessageChannel : HasResultCodes {
 
   
   
+  
+  void TestOnlyWaitForTransactionCompletion() const MOZ_EXCLUDES(*mMonitor);
+
+  
+  
   bool IsClosed() MOZ_EXCLUDES(*mMonitor) {
     MonitorAutoLock lock(*mMonitor);
     return IsClosedLocked();
