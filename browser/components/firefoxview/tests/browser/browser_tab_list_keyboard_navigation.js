a52d8db2de70295b6258bbef892284f665647e2e
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-05-01 02:21:58 +0000

    Backed out changeset 901f008190e4 (bug 1882605) for causing bc failures on browser_tab_list_keyboard_navigation.js. CLOSED TREE
    UltraBlame original commit: 9b59ab05597cb522afa9b0f57dc36c1282c77838

--- browser/components/firefoxview/tests/browser/browser_tab_list_keyboard_navigation.js
+++ browser/components/firefoxview/tests/browser/browser_tab_list_keyboard_navigation.js
@@ -80,9 +80,8 @@ add_task(async function test_open_tab_row_with_sound_navigation() {
 
     await navigateToViewAndWait(document, "opentabs");
     const openTabs = document.querySelector("view-opentabs[name=opentabs]");
-    await TestUtils.waitForCondition(
-      () => tabWithSound.hasAttribute("soundplaying"),
-      "Tab is playing sound"
+    await TestUtils.waitForCondition(() =>
+      tabWithSound.hasAttribute("soundplaying")
     );
     await tabsUpdated;
     await openTabs.updateComplete;
@@ -93,9 +92,7 @@ add_task(async function test_open_tab_row_with_sound_navigation() {
     );
 
     
-    let tabRow = openTabs.viewCards[0].tabList.rowEls.find(rowEl =>
-      rowEl.indicators.includes("soundplaying")
-    );
+    let tabRow = openTabs.viewCards[0].tabList.rowEls[1];
     let tabRowFocused = BrowserTestUtils.waitForEvent(tabRow, "focus", win);
     tabRow.focus();
     await tabRowFocused;
