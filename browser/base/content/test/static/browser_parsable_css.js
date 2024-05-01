cf8ea42429bb4f9aa053d0fd59a42a61e140a02f
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-05-01 02:11:49 +0000

    Bug 1882144 - Remove layout.css.math-style.enabled and layout.css.math-depth.enabled prefs. r=layout-reviewers,emilio
    Differential Revision: https://phabricator.services.mozilla.com/D202940
    
    UltraBlame original commit: aff056572c2790f447e93d3839f4ac7a61aa4b8f

--- browser/base/content/test/static/browser_parsable_css.js
+++ browser/base/content/test/static/browser_parsable_css.js
@@ -68,24 +68,6 @@ if (!Services.prefs.getBoolPref("layout.css.zoom.enabled")) {
   });
 }
 
-if (!Services.prefs.getBoolPref("layout.css.math-depth.enabled")) {
-  
-  ignoreList.push({
-    sourceName: /\b(scrollbars|mathml)\.css$/i,
-    errorMessage: /Unknown property .*\bmath-depth\b/i,
-    isFromDevTools: false,
-  });
-}
-
-if (!Services.prefs.getBoolPref("layout.css.math-style.enabled")) {
-  
-  ignoreList.push({
-    sourceName: /(?:res|gre-resources)\/mathml\.css$/i,
-    errorMessage: /Unknown property .*\bmath-style\b/i,
-    isFromDevTools: false,
-  });
-}
-
 if (!Services.prefs.getBoolPref("layout.css.scroll-anchoring.enabled")) {
   ignoreList.push({
     sourceName: /webconsole\.css$/i,
