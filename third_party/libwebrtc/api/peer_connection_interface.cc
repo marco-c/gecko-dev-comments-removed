6ddaea70a0a3c8981dcd57161d39f1097b34de47
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-05-01 01:07:04 +0000

    Bug 1876843 - Vendor libwebrtc from fe66dda733
    Upstream commit: https://webrtc.googlesource.com/src/+/fe66dda7339bb1f868e514fdb847bf41fbca5ab1
        Delete deprecated call_factory and media_engine
    
        from PeerConnectionFactoryDependencies
    
        Bug: webrtc:15574
        Change-Id: Id0ead8086ddd41f6792e2a3c224d8705cd797d49
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/326003
        Commit-Queue: Danil Chapovalov <danilchapwebrtc.org>
        Reviewed-by: Harald Alvestrand <htawebrtc.org>
        Cr-Commit-Position: refs/heads/main{#41309}
    
    UltraBlame original commit: 6ed3783ba0408ab8b9b0f9e38db5d88589b34c2b

--- third_party/libwebrtc/api/peer_connection_interface.cc
+++ third_party/libwebrtc/api/peer_connection_interface.cc
@@ -60,13 +60,8 @@ PeerConnectionDependencies::~PeerConnectionDependencies() = default;
 PeerConnectionFactoryDependencies::PeerConnectionFactoryDependencies() =
     default;
 
-
-
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wdeprecated-declarations"
 PeerConnectionFactoryDependencies::PeerConnectionFactoryDependencies(
     PeerConnectionFactoryDependencies&&) = default;
-#pragma clang diagnostic pop
 
 PeerConnectionFactoryDependencies::~PeerConnectionFactoryDependencies() =
     default;
