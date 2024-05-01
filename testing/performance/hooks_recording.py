e4d2c7c710b02da6025b9a45793970e3a8ecbbd4
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-05-01 01:51:58 +0000

    Bug 1881862 - Re-record sina pageload test. r=perftest-reviewers,aglavic
    This patch re-records the sina pageload test. It also adds some additional code to the recording script to attempt to remove the cookie banners.
    
    Differential Revision: https://phabricator.services.mozilla.com/D202639
    
    UltraBlame original commit: 21e730d5979d92d75737721b7d46e3b95154ddd5

--- testing/performance/hooks_recording.py
+++ testing/performance/hooks_recording.py
@@ -144,6 +144,12 @@ def before_runs(env):
         for pref, val in prefs.items():
             add_option(env, "firefox.preference", f"{pref}:{val}")
 
+        
+        add_option(
+            env, "firefox.preference", "cookiebanners.bannerClicking.enabled:true"
+        )
+        add_option(env, "firefox.preference", "cookiebanners.service.mode:2")
+
         second_url = test_site.get("secondary_url", None)
         if second_url:
             add_option(env, "browsertime.secondary_url", second_url)
