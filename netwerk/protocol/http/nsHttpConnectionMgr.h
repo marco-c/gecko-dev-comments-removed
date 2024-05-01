8b95e5c9948b6cb7e6e38503db0713b24c4d8d0b
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-05-01 02:43:11 +0000

    Backed out changeset b2dd957f853b (bug 1873263) for causing xpc failures  netwerk/test/unit/test_webtransport_simple.js CLOSED TREE
    UltraBlame original commit: ca5de2cea700f810aa89ebe1fee009d8110fa0c5

--- netwerk/protocol/http/nsHttpConnectionMgr.h
+++ netwerk/protocol/http/nsHttpConnectionMgr.h
@@ -62,12 +62,6 @@ class nsHttpConnectionMgr final : public HttpConnectionMgrShell,
 
   
   
-  nsresult StoreServerCertHashes(
-      nsHttpConnectionInfo* aConnInfo, bool aNoSpdy, bool aNoHttp3,
-      nsTArray<RefPtr<nsIWebTransportHash>>&& aServerCertHashes);
-
-  
-  
   
 
   
@@ -148,11 +142,6 @@ class nsHttpConnectionMgr final : public HttpConnectionMgrShell,
   
   void NewIdleConnectionAdded(uint32_t timeToLive);
   void DecrementNumIdleConns();
-
-  const nsTArray<RefPtr<nsIWebTransportHash>>* GetServerCertHashes(
-      nsHttpConnectionInfo* aConnInfo);
-
-  uint64_t GenerateNewWebTransportId() { return mMaxWebTransportId++; }
 
  private:
   virtual ~nsHttpConnectionMgr();
@@ -345,7 +334,6 @@ class nsHttpConnectionMgr final : public HttpConnectionMgrShell,
   void OnMsgPruneNoTraffic(int32_t, ARefBase*);
   void OnMsgUpdateCurrentBrowserId(int32_t, ARefBase*);
   void OnMsgClearConnectionHistory(int32_t, ARefBase*);
-  void OnMsgStoreServerCertHashes(int32_t, ARefBase*);
 
   
   
@@ -474,10 +462,6 @@ class nsHttpConnectionMgr final : public HttpConnectionMgrShell,
   void NotifyConnectionOfBrowserIdChange(uint64_t previousId);
 
   void CheckTransInPendingQueue(nsHttpTransaction* aTrans);
-
-  
-  
-  Atomic<uint64_t> mMaxWebTransportId{1};
 };
 
 }  
