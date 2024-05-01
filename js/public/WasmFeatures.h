c751ebeffba41f07ae436cb1fa075d9e07d5566a
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-05-01 01:52:18 +0000

    Bug 1879179 - wasm: Use JSPrefs for WasmFeatures.h. r=bvisness
    Replace the part of WasmFeatures.h which would manually
    read prefs through touching a bunch of Gecko stuff and
    instead just use JSPrefs for that. Also use JSPrefs for
    the shell instead of rolling our own shell flags. This
    commit removes the 'stage' distinction because that only
    changed how shell flags worked.
    
    Differential Revision: https://phabricator.services.mozilla.com/D201869
    
    UltraBlame original commit: 9125a74241251354dc6974a86da3752a8f085e29

--- js/public/WasmFeatures.h
+++ js/public/WasmFeatures.h
@@ -37,24 +37,6 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 #ifdef ENABLE_WASM_RELAXED_SIMD
 #  define WASM_RELAXED_SIMD_ENABLED 1
 #else
@@ -106,168 +88,125 @@
 #  define WASM_JS_STRING_BUILTINS_ENABLED 0
 #endif
 
-enum class WasmFeatureStage {
-  Experimental = 0,
-  Tentative,
-  Default,
-};
-
 
 #define JS_FOR_WASM_FEATURES(FEATURE)                                   \
   FEATURE(                                                              \
     /* capitalized name   */ ExtendedConst,                             \
     /* lower case name    */ extendedConst,                             \
-    /* stage              */ WasmFeatureStage::Tentative,               \
     /* compile predicate  */ WASM_EXTENDED_CONST_ENABLED,               \
     /* compiler predicate */ true,                                      \
     /* flag predicate     */ true,                                      \
     /* flag force enable  */ false,                                     \
     /* flag fuzz enable   */ true,                                      \
-    /* shell flag         */ "extended-const",                          \
-    /* preference name    */ "extended_const")                          \
+    /* preference name    */ extended_const)                            \
   FEATURE(                                                              \
     /* capitalized name   */ Exceptions,                                \
     /* lower case name    */ exceptions,                                \
-    /* stage              */ WasmFeatureStage::Default,                 \
     /* compile predicate  */ true,                                      \
     /* compiler predicate */ AnyCompilerAvailable(cx),                  \
     /* flag predicate     */ true,                                      \
     /* flag force enable  */ WasmExnRefFlag(cx),                        \
     /* flag fuzz enable   */ true,                                      \
-    /* shell flag         */ "exceptions",                              \
-    /* preference name    */ "exceptions")                              \
+    /* preference name    */ exceptions)                                \
   FEATURE(                                                              \
     /* capitalized name   */ ExnRef,                                    \
     /* lower case name    */ exnref,                                    \
-    /* stage              */ WasmFeatureStage::Experimental,            \
     /* compile predicate  */ true,                                      \
     /* compiler predicate */ AnyCompilerAvailable(cx),                  \
     /* flag predicate     */ true,                                      \
     /* flag force enable  */ false,                                     \
     /* flag fuzz enable   */ true,                                      \
-    /* shell flag         */ "exnref",                                  \
-    /* preference name    */ "exnref ")                                 \
+    /* preference name    */ exnref)                                    \
   FEATURE(                                                              \
     /* capitalized name   */ FunctionReferences,                        \
     /* lower case name    */ functionReferences,                        \
-    /* stage              */ WasmFeatureStage::Tentative,               \
     /* compile predicate  */ WASM_FUNCTION_REFERENCES_ENABLED,          \
     /* compiler predicate */ AnyCompilerAvailable(cx),                  \
     /* flag predicate     */ true,                                      \
     /* flag force enable  */ WasmGcFlag(cx),                            \
     /* flag fuzz enable   */ false,                                     \
-    /* shell flag         */ "function-references",                     \
-    /* preference name    */ "function_references")                     \
+    /* preference name    */ function_references)                       \
   FEATURE(                                                              \
     /* capitalized name   */ Gc,                                        \
     /* lower case name    */ gc,                                        \
-    /* stage              */ WasmFeatureStage::Tentative,               \
     /* compile predicate  */ WASM_GC_ENABLED,                           \
     /* compiler predicate */ AnyCompilerAvailable(cx),                  \
     /* flag predicate     */ true,                                      \
     /* flag force enable  */ false,                                     \
     /* flag fuzz enable   */ false,                                     \
-    /* shell flag         */ "gc",                                      \
-    /* preference name    */ "gc")                                      \
+    /* preference name    */ gc)                                        \
   FEATURE(                                                              \
     /* capitalized name   */ JSStringBuiltins,                          \
     /* lower case name    */ jsStringBuiltins,                          \
-    /* stage              */ WasmFeatureStage::Experimental,            \
     /* compile predicate  */ WASM_JS_STRING_BUILTINS_ENABLED,           \
     /* compiler predicate */ AnyCompilerAvailable(cx),                  \
     /* flag predicate     */ true,                                      \
     /* flag force enable  */ false,                                     \
     /* flag fuzz enable   */ true,                                      \
-    /* shell flag         */ "js-string-builtins",                      \
-    /* preference name    */ "js_string_builtins")                      \
+    /* preference name    */ js_string_builtins)                        \
   FEATURE(                                                              \
     /* capitalized name   */ RelaxedSimd,                               \
     /* lower case name    */ v128Relaxed,                               \
-    /* stage              */ WasmFeatureStage::Tentative,               \
     /* compile predicate  */ WASM_RELAXED_SIMD_ENABLED,                 \
     /* compiler predicate */ AnyCompilerAvailable(cx),                  \
     /* flag predicate     */ js::jit::JitSupportsWasmSimd(),            \
     /* flag force enable  */ false,                                     \
     /* flag fuzz enable   */ true,                                      \
-    /* shell flag         */ "relaxed-simd",                            \
-    /* preference name    */ "relaxed_simd")                            \
+    /* preference name    */ relaxed_simd)                              \
   FEATURE(                                                              \
     /* capitalized name   */ Memory64,                                  \
     /* lower case name    */ memory64,                                  \
-    /* stage              */ WasmFeatureStage::Tentative,               \
     /* compile predicate  */ WASM_MEMORY64_ENABLED,                     \
     /* compiler predicate */ AnyCompilerAvailable(cx),                  \
     /* flag predicate     */ true,                                      \
     /* flag force enable  */ false,                                     \
     /* flag fuzz enable   */ true,                                      \
-    /* shell flag         */ "memory64",                                \
-    /* preference name    */ "memory64")                                \
+    /* preference name    */ memory64)                                  \
   FEATURE(                                                              \
     /* capitalized name   */ MemoryControl,                             \
     /* lower case name    */ memoryControl,                             \
-    /* stage              */ WasmFeatureStage::Experimental,            \
     /* compile predicate  */ WASM_MEMORY_CONTROL_ENABLED,               \
     /* compiler predicate */ AnyCompilerAvailable(cx),                  \
     /* flag predicate     */ true,                                      \
     /* flag force enable  */ false,                                     \
     /* flag fuzz enable   */ false,                                     \
-    /* shell flag         */ "memory-control",                          \
-    /* preference name    */ "memory_control")                          \
+    /* preference name    */ memory_control)                            \
   FEATURE(                                                              \
     /* capitalized name   */ MultiMemory,                               \
     /* lower case name    */ multiMemory,                               \
-    /* stage              */ WasmFeatureStage::Experimental,            \
     /* compile predicate  */ WASM_MULTI_MEMORY_ENABLED,                 \
     /* compiler predicate */ AnyCompilerAvailable(cx),                  \
     /* flag predicate     */ true,                                      \
     /* flag force enable  */ false,                                     \
     /* flag fuzz enable  */  false,                                     \
-    /* shell flag         */ "multi-memory",                            \
-    /* preference name    */ "multi_memory")                            \
+    /* preference name    */ multi_memory)                              \
   FEATURE(                                                              \
     /* capitalized name   */ TailCalls,                                 \
     /* lower case name    */ tailCalls,                                 \
-    /* stage              */ WasmFeatureStage::Tentative,            \
     /* compile predicate  */ WASM_TAIL_CALLS_ENABLED,                   \
     /* compiler predicate */ AnyCompilerAvailable(cx),                  \
     /* flag predicate     */ true,                                      \
     /* flag force enable  */ false,                                     \
     /* flag fuzz enable   */ true,                                      \
-    /* shell flag         */ "tail-calls",                              \
-    /* preference name    */ "tail_calls")                              \
+    /* preference name    */ tail_calls)                                \
   FEATURE(                                                              \
     /* capitalized name   */ MozIntGemm,                                \
     /* lower case name    */ mozIntGemm,                                \
-    /* stage              */ WasmFeatureStage::Experimental,            \
     /* compile predicate  */ WASM_MOZ_INTGEMM_ENABLED,                  \
     /* compiler predicate */ AnyCompilerAvailable(cx),                  \
     /* flag predicate     */ IsSimdPrivilegedContext(cx),               \
     /* flag force enable  */ false,                                     \
     /* flag fuzz enable   */ false,                                     \
-    /* shell flag         */ "moz-intgemm",                             \
-    /* preference name    */ "moz_intgemm")                             \
+    /* preference name    */ moz_intgemm)                               \
   FEATURE(                                                              \
     /* capitalized name   */ TestSerialization,                         \
     /* lower case name    */ testSerialization,                         \
-    /* stage              */ WasmFeatureStage::Experimental,            \
     /* compile predicate  */ 1,                                         \
     /* compiler predicate */ IonAvailable(cx),                          \
     /* flag predicate     */ true,                                      \
     /* flag force enable  */ false,                                     \
     /* flag fuzz enable   */ false,                                     \
-    /* shell flag         */ "test-serialization",                      \
-    /* preference name    */ "test-serialization")                      \
-  FEATURE(                                                              \
-    /* capitalized name   */ TestMetadata,                              \
-    /* lower case name    */ testMetadata,                              \
-    /* stage              */ WasmFeatureStage::Experimental,            \
-    /* compile predicate  */ 1,                                         \
-    /* compiler predicate */ AnyCompilerAvailable(cx),                  \
-    /* flag predicate     */ true,                                      \
-    /* flag force enable  */ false,                                     \
-    /* flag fuzz enable   */ false,                                     \
-    /* shell flag         */ "test-metadata",                           \
-    /* preference name    */ "test_metadata")
+    /* preference name    */ test_serialization)
 
 
 
