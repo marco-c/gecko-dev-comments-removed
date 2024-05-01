c388786104ff8631b0a3ae1325e410f5224b7e9b
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-05-01 01:48:18 +0000

    Bug 1842999 - Part 19: Transpile TypedArrayByteOffset intrinsic in Warp. r=jandem,nbp
    Differential Revision: https://phabricator.services.mozilla.com/D200659
    
    UltraBlame original commit: 5c60df3c3dfba2c57414dbb4bfda4f915ba444ed

--- js/src/jit/MIR.cpp
+++ js/src/jit/MIR.cpp
@@ -6365,6 +6365,13 @@ AliasSet MGuardHasAttachedArrayBuffer::getAliasSet() const {
   return AliasSet::Load(AliasSet::ObjectFields | AliasSet::FixedSlot);
 }
 
+AliasSet MResizableTypedArrayByteOffsetMaybeOutOfBounds::getAliasSet() const {
+  
+  
+  return AliasSet::Load(AliasSet::ArrayBufferViewLengthOrOffset |
+                        AliasSet::ObjectFields | AliasSet::FixedSlot);
+}
+
 AliasSet MResizableTypedArrayLength::getAliasSet() const {
   
   
