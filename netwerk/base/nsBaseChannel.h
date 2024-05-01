100551b2dd0204e57fdf03f1e2607f6739db3232
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-05-01 02:45:35 +0000

    Bug 1847358 part 1: move nsBaseChannel::ContentRange content out into its own class as mozilla::net::ContentRange; r=dlrobertson,necko-reviewers,valentin
    Differential Revision: https://phabricator.services.mozilla.com/D203378
    
    UltraBlame original commit: 553b59b0c2a5ea258689379439105d14e44c8db1

--- netwerk/base/nsBaseChannel.h
+++ netwerk/base/nsBaseChannel.h
@@ -9,6 +9,7 @@
 #include "mozilla/Maybe.h"
 #include "mozilla/MozPromise.h"
 #include "mozilla/UniquePtr.h"
+#include "mozilla/net/ContentRange.h"
 #include "mozilla/net/NeckoTargetHolder.h"
 #include "mozilla/net/PrivateBrowsingChannel.h"
 #include "nsHashPropertyBag.h"
@@ -200,34 +201,16 @@ class nsBaseChannel
   
   
   
-  class ContentRange {
-   private:
-    uint64_t mStart;
-    uint64_t mEnd;
-    uint64_t mSize;
-
-   public:
-    uint64_t Start() const { return mStart; }
-    uint64_t End() const { return mEnd; }
-    uint64_t Size() const { return mSize; }
-    bool IsValid() const { return mStart < mSize; }
-    ContentRange() : mStart(0), mEnd(0), mSize(0) {}
-    ContentRange(uint64_t aStart, uint64_t aEnd, uint64_t aSize)
-        : mStart(aStart), mEnd(aEnd), mSize(aSize) {}
-    ContentRange(const nsACString& aRangeHeader, uint64_t aSize);
-    void AsHeader(nsACString& aOutString) const;
-  };
-
-  const mozilla::Maybe<ContentRange>& GetContentRange() const {
+  const mozilla::Maybe<mozilla::net::ContentRange>& GetContentRange() const {
     return mContentRange;
   }
 
   void SetContentRange(uint64_t aStart, uint64_t aEnd, uint64_t aSize) {
-    mContentRange.emplace(ContentRange(aStart, aEnd, aSize));
+    mContentRange.emplace(mozilla::net::ContentRange(aStart, aEnd, aSize));
   }
 
   bool SetContentRange(const nsACString& aRangeHeader, uint64_t aSize) {
-    auto range = ContentRange(aRangeHeader, aSize);
+    auto range = mozilla::net::ContentRange(aRangeHeader, aSize);
     if (!range.IsValid()) {
       return false;
     }
@@ -325,7 +308,7 @@ class nsBaseChannel
   bool mWaitingOnAsyncRedirect{false};
   bool mOpenRedirectChannel{false};
   uint32_t mRedirectFlags{0};
-  mozilla::Maybe<ContentRange> mContentRange;
+  mozilla::Maybe<mozilla::net::ContentRange> mContentRange;
 
  protected:
   nsCString mContentType;
