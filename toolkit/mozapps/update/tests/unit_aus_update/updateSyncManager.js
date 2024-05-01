dc2403c39b04f74fcabcbe8c445a41e0d97bb741
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-05-01 02:58:58 +0000

    Bug 1864896: Fix unused function arguments (toolkit/mozapps/update) r=application-update-reviewers,mossop,nalexander
    Differential Revision: https://phabricator.services.mozilla.com/D202995
    
    UltraBlame original commit: cf755d27fb6937544256d0c4bb2663f1ce9824b1

--- toolkit/mozapps/update/tests/unit_aus_update/updateSyncManager.js
+++ toolkit/mozapps/update/tests/unit_aus_update/updateSyncManager.js
@@ -79,7 +79,7 @@ add_task(async function () {
     await TestUtils.waitForCondition(
       () => syncManager.isOtherInstanceRunning(),
       "waiting for child process to take the lock"
-    ).catch(e => {
+    ).catch(_e => {
       
       
       Assert.ok(
@@ -93,7 +93,7 @@ add_task(async function () {
     await TestUtils.waitForCondition(
       () => !syncManager.isOtherInstanceRunning(),
       "waiting for child process to release the lock"
-    ).catch(e => {
+    ).catch(_e => {
       Assert.ok(
         !syncManager.isOtherInstanceRunning(),
         "child process has released the lock"
