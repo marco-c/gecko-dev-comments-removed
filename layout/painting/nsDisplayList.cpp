14b7551263a4c8abbfac77d052ea896bddf6a320
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-05-01 03:05:07 +0000

    Bug 1860328 - Fix a crash which a caret frame misses a MarkNeedsDisplayItemRebuild() call r=emilio
    Generally, we mark a caret frame for display first, and then
    nsCaret tracks this frame in nsCaret::SchedulePaint to call
    MarkNeedsDisplayItemRebuild() accordingly. However, it's possible
    for nsCaret::SchedulePaint fails to find the caret frame (i.e, selection changes),
    so we end up not calling MarkNeedsDisplayItemRebuild() on this frame.
    
    This patch improves this case by manually setting this caret frame
    to nsCaret, so that it's always tracked.
    
    Differential Revision: https://phabricator.services.mozilla.com/D200880
    
    UltraBlame original commit: b32811ea1c61e1891454d826f7ba003416c9eb57

--- layout/painting/nsDisplayList.cpp
+++ layout/painting/nsDisplayList.cpp
@@ -1124,6 +1124,14 @@ void nsDisplayListBuilder::EnterPresShell(const nsIFrame* aReferenceFrame,
   
   if (state->mCaretFrame) {
     MOZ_ASSERT(state->mCaretFrame->PresShell() == state->mPresShell);
+    
+    
+    
+    
+    
+    
+    
+    caret->SetLastCaretFrame(state->mCaretFrame);
     MarkFrameForDisplay(state->mCaretFrame, aReferenceFrame);
   }
 }
