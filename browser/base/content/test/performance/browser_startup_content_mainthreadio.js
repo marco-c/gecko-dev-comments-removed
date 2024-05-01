c63aab792567a9619e7ddb2693c9e0528e975080
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-05-01 02:45:26 +0000

    Bug 1789727 - Default to the component implementation of Screenshots for nightly builds. r=extension-reviewers,sfoster,robwu,chutten
    * Flip the component pref to true by default for nightly builds only
    * Move the pref check and initialization to a startup idle task
    * And be a bit smarter about when we get and disable the addon
    * Fix a bug where we try to communicate with the overlay after the window actor is destroyed when
      the component pref gets flipped off during use
    
    Differential Revision: https://phabricator.services.mozilla.com/D196888
    
    UltraBlame original commit: 7a66012be9d90e12b91af4556288fc1798514a8d

--- browser/base/content/test/performance/browser_startup_content_mainthreadio.js
+++ browser/base/content/test/performance/browser_startup_content_mainthreadio.js
@@ -152,6 +152,13 @@ const processes = {
       condition: WIN,
       stat: 1,
     },
+    {
+      
+      path: "*screenshots@mozilla.org.xpi",
+      condition: true,
+      ignoreIfUnused: true,
+      close: 1,
+    },
   ],
 };
 
