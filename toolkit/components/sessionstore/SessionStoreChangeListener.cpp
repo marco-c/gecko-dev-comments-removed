30bdc2052ac071d31d3a297d94ecdfb7f62457aa
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-05-01 03:20:27 +0000

    Bug 1883278 - Force-enable platform collection code of session store when SHIP is enabled. r=farre,geckoview-reviewers,sessionstore-reviewers
    If session history in the parent is enabled then session store only works
    correctly if the platform collection code is turned on.
    
    Differential Revision: https://phabricator.services.mozilla.com/D203375
    
    UltraBlame original commit: 332781271bbed76641b5b56e532477a702598432

--- toolkit/components/sessionstore/SessionStoreChangeListener.cpp
+++ toolkit/components/sessionstore/SessionStoreChangeListener.cpp
@@ -134,8 +134,7 @@ SessionStoreChangeListener::HandleEvent(dom::Event* aEvent) {
 
  already_AddRefed<SessionStoreChangeListener>
 SessionStoreChangeListener::Create(BrowsingContext* aBrowsingContext) {
-  MOZ_RELEASE_ASSERT(
-      StaticPrefs::browser_sessionstore_platform_collection_AtStartup());
+  MOZ_RELEASE_ASSERT(SessionStorePlatformCollection());
   if (!aBrowsingContext) {
     return nullptr;
   }
