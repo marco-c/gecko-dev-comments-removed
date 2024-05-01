cb41d87a736588ffb5e06d2fe2b404d5d3df18db
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-05-01 02:43:16 +0000

    Bug 1882506 - Use linux64/opt builds for external desktop browser tests. r=kshampur,perftest-reviewers
    This patch adds a transform that changes the build dependency for all tasks that test an external browser. Instead of having to do a full build of Firefox that might not be used (which can waste resources, and take longer than necessary), these tasks will now use the build-linux64/opt task to get the mozharness requirements. At the same time, some new mozharness config files are added to skip the install stage.
    
    Differential Revision: https://phabricator.services.mozilla.com/D203172
    
    UltraBlame original commit: 190f2393c2d2ae73e2b9f44862bd976da0c968e9

---
+++ taskcluster/gecko_taskgraph/util/perftest.py
@@ -0,0 +1,16 @@
+
+
+
+
+
+def is_external_browser(label):
+    if any(
+        external_browser in label
+        for external_browser in (
+            "safari",
+            "chrome",
+            "custom-car",
+        )
+    ):
+        return True
+    return False
