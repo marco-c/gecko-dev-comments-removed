3d8df610a162e220aafe960c62cb535d662be7df
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-05-01 01:48:26 +0000

    Bug 1842999 - Part 21: Transpile TypedArrayLength intrinsics in Warp. r=jandem
    Reading the typed array's length is already implemented, so inlining the
    intrinsic only requires transpiler support for
    `GuardResizableArrayBufferViewInBoundsOrDetached`.
    
    Differential Revision: https://phabricator.services.mozilla.com/D200661
    
    UltraBlame original commit: 0002a87b69237448f597c63ad5da0e2acdce7a36

--- js/src/jit/MIR.cpp
+++ js/src/jit/MIR.cpp
@@ -6433,6 +6433,13 @@ AliasSet MGuardResizableArrayBufferViewInBounds::getAliasSet() const {
   return AliasSet::Load(AliasSet::ArrayBufferViewLengthOrOffset);
 }
 
+AliasSet MGuardResizableArrayBufferViewInBoundsOrDetached::getAliasSet() const {
+  
+  
+  return AliasSet::Load(AliasSet::ArrayBufferViewLengthOrOffset |
+                        AliasSet::ObjectFields | AliasSet::FixedSlot);
+}
+
 AliasSet MArrayPush::getAliasSet() const {
   return AliasSet::Store(AliasSet::ObjectFields | AliasSet::Element);
 }
