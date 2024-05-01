c8a0275cd7aa1846ff9f7fa205c2e7691b0f0fb9
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-05-01 00:40:17 +0000

    Bug 1770944 - remove dangling textual references of mozbrowser, r=smaug
    Depends on D200797
    
    Differential Revision: https://phabricator.services.mozilla.com/D189655
    
    UltraBlame original commit: 3a149ef794c249167b843b6da1db5a07113bc5f0

--- browser/actors/ContextMenuChild.sys.mjs
+++ browser/actors/ContextMenuChild.sys.mjs
@@ -471,17 +471,6 @@ export class ContextMenuChild extends JSWindowActorChild {
     return this.contentWindow.HTMLTextAreaElement.isInstance(node);
   }
 
-  
-
-
-  _isTargetRDMFrame(node) {
-    return (
-      Services.appinfo.processType === Services.appinfo.PROCESS_TYPE_DEFAULT &&
-      node.tagName === "iframe" &&
-      node.hasAttribute("mozbrowser")
-    );
-  }
-
   _isSpellCheckEnabled(aNode) {
     
     if (this._isTargetATextBox(aNode)) {
@@ -542,12 +531,6 @@ export class ContextMenuChild extends JSWindowActorChild {
     }
 
     if (defaultPrevented) {
-      return;
-    }
-
-    if (this._isTargetRDMFrame(aEvent.composedTarget)) {
-      
-      
       return;
     }
 
