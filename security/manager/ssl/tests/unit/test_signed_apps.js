f8aa5b1e2d51b31d2ea136e52ecf93d9d66c7423
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-05-01 03:56:01 +0000

    Bug 1885349 - Replace addons-stage.crt with cas-cur r=jschanck
    addons-stage.crt is the DER-encoded version of the
    cas-cur-root-2024-03-12.crt file from bug 1882192, generated with:
    
    openssl x509 -inform PEM -in /tmp/cas-cur-root-2024-03-12.crt -outform DER -out security/manager/ssl/addons-stage.crt
    
    The four zip files are from bug 1885457, and are the same file, but
    signed with the new certificate.
    
    Differential Revision: https://phabricator.services.mozilla.com/D204706
    
    UltraBlame original commit: 98ae4cd205cf36db4097eebc6b99518cbdaaa5df

--- security/manager/ssl/tests/unit/test_signed_apps.js
+++ security/manager/ssl/tests/unit/test_signed_apps.js
@@ -768,9 +768,14 @@ var cosePolicies = [
 ];
 
 
+
+
+
+
+
 var coseTestcasesStage = [
   {
-    name: "autograph-714ba248-stage-tomato-clock-PKCS7-SHA1-ES256-ES384",
+    name: "addons-stage-tomato-clock-sha1-es256-es384",
     expectedResult: Cr.NS_OK,
     expectedSignatureAlgorithms: [
       Ci.nsIAppSignatureInfo.COSE_WITH_SHA256,
@@ -779,13 +784,14 @@ var coseTestcasesStage = [
     root: Ci.nsIX509CertDB.AddonsStageRoot,
   },
   {
-    name: "autograph-714ba248-stage-tomato-clock-PKCS7-SHA1-ES256-PS256",
+    name: "addons-stage-tomato-clock-sha1-es256-ps256",
+    
     expectedResult: Cr.NS_ERROR_SIGNED_JAR_MANIFEST_INVALID,
     expectedSignatureAlgorithms: [],
     root: Ci.nsIX509CertDB.AddonsStageRoot,
   },
   {
-    name: "autograph-714ba248-stage-tomato-clock-PKCS7-SHA1-ES256",
+    name: "addons-stage-tomato-clock-sha1-es256",
     expectedResult: Cr.NS_OK,
     expectedSignatureAlgorithms: [
       Ci.nsIAppSignatureInfo.COSE_WITH_SHA256,
@@ -794,7 +800,8 @@ var coseTestcasesStage = [
     root: Ci.nsIX509CertDB.AddonsStageRoot,
   },
   {
-    name: "autograph-714ba248-stage-tomato-clock-PKCS7-SHA1-PS256",
+    name: "addons-stage-tomato-clock-sha1-ps256",
+    
     expectedResult: Cr.NS_ERROR_SIGNED_JAR_MANIFEST_INVALID,
     expectedSignatureAlgorithms: [],
     root: Ci.nsIX509CertDB.AddonsStageRoot,
@@ -813,6 +820,7 @@ var coseTestcasesProd = [
   },
   {
     name: "autograph-714ba248-prod-tomato-clock-PKCS7-SHA1-ES256-PS256",
+    
     expectedResult: Cr.NS_ERROR_SIGNED_JAR_MANIFEST_INVALID,
     expectedSignatureAlgorithms: [],
     root: Ci.nsIX509CertDB.AddonsPublicRoot,
@@ -828,6 +836,7 @@ var coseTestcasesProd = [
   },
   {
     name: "autograph-714ba248-prod-tomato-clock-PKCS7-SHA1-PS256",
+    
     expectedResult: Cr.NS_ERROR_SIGNED_JAR_MANIFEST_INVALID,
     expectedSignatureAlgorithms: [],
     root: Ci.nsIX509CertDB.AddonsPublicRoot,
