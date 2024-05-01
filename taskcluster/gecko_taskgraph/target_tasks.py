69bf553515c5f21bd98d05283feb1b77ed18973a
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-05-01 01:23:08 +0000

    Bug 1848400 - Upgrade M2s to use OSX 14 instead of OSX 13. r=perftest-reviewers,sparky
    During the enabling of safaridriver we ran into user permission issues and as a result were unable to enable run safari tests on M2s
    The issue we were encountering was `kernel: (Sandbox) Sandbox: com.apple.WebDriver.HTTPService(30714) deny(1) network-bind local:*:7055`.
    This issue was precenting from webdriver from starting and the permission issue was caused by running a taskcluster-worker as system daemon vs. a per-user launch agent.
    
    The issue was specific to OSX 13, and so it was decided to upgrade the machines from OSX 13 to OSX 14 to resolve the issue and give us a more modern OS to test speedometer against in general
    
    Differential Revision: https://phabricator.services.mozilla.com/D202503
    
    UltraBlame original commit: a700ab961a223f6d614abe258766bed35dab027b

--- taskcluster/gecko_taskgraph/target_tasks.py
+++ taskcluster/gecko_taskgraph/target_tasks.py
@@ -851,9 +851,6 @@ def target_tasks_general_perf_testing(full_task_graph, parameters, graph_config)
                     if "speedometer" in try_name:
                         return True
                 if "safari" and "benchmark" in try_name:
-                    
-                    if "speedometer3" in try_name:
-                        return False
                     return True
         
         elif accept_raptor_android_build(platform):
--- taskcluster/gecko_taskgraph/transforms/test/raptor.py
+++ taskcluster/gecko_taskgraph/transforms/test/raptor.py
@@ -136,7 +136,7 @@ def split_raptor_subtests(config, tests):
         
         subtests = test["raptor"].pop("subtests", None)
         if not subtests:
-            if "macosx1300" not in test["test-platform"]:
+            if "macosx1400" not in test["test-platform"]:
                 yield test
             continue
 
