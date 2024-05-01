a44048ad713f23f897a3772cbd61d50e85c4f77b
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-05-01 02:49:23 +0000

    Backed out changeset b1dedc536b88 (bug 1883724) for causing build bustages with StaticPrefList.yaml CLOSED TREE
    UltraBlame original commit: abf52ffe890dfe2a569930d50a2d1a97b53f12ea

--- dom/tests/mochitest/general/test_interfaces.js
+++ dom/tests/mochitest/general/test_interfaces.js
@@ -1967,9 +1967,9 @@ let interfaceNamesInGlobalScope = [
   
   { name: "visualViewport", insecureContext: true },
   
-  { name: "WakeLock", earlyBetaOrEarlier: true },
+  { name: "WakeLock" },
   
-  { name: "WakeLockSentinel", earlyBetaOrEarlier: true },
+  { name: "WakeLockSentinel" },
   
   { name: "webkitURL", insecureContext: true },
   
