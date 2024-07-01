9e43bceed8291d0be35a2d40e7de7f5ceb17a11f
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-07-01 14:29:34 +0000

    [focus] Support Cookie blocking Closes https://github.com/mozilla-mobile/focus-android/issues/2330
    UltraBlame original commit: 9b4c268b6e24b39bfff334cb09fe00c329376ec9

--- mobile/android/focus-android/app/src/webview/java/org/mozilla/focus/web/WebViewProvider.java
+++ mobile/android/focus-android/app/src/webview/java/org/mozilla/focus/web/WebViewProvider.java
@@ -68,7 +68,7 @@ public class WebViewProvider {
 
         setupView(webkitView);
         configureDefaultSettings(context, settings);
-        applyAppSettings(context, settings);
+        applyAppSettings(context, settings, webkitView);
 
         return webkitView;
     }
@@ -124,16 +124,22 @@ public class WebViewProvider {
         settings.setSavePassword(false);
     }
 
-    public static void applyAppSettings(Context context, WebSettings settings) {
+    public static void applyAppSettings(Context context, WebSettings settings, WebView webView) {
         
         settings.setBlockNetworkImage(Settings.getInstance(context).shouldBlockImages());
         settings.setJavaScriptEnabled(!Settings.getInstance(context).shouldBlockJavaScript());
+        CookieManager.getInstance().setAcceptThirdPartyCookies(webView, !Settings.getInstance
+                (context).shouldBlockThirdPartyCookies());
+        CookieManager.getInstance().setAcceptCookie(!Settings.getInstance(context)
+                .shouldBlockCookies());
     }
 
     @SuppressLint("SetJavaScriptEnabled") 
-    public static void disableBlocking(WebSettings settings) {
+    public static void disableBlocking(WebSettings settings, WebView webView) {
         settings.setBlockNetworkImage(false);
         settings.setJavaScriptEnabled(true);
+        CookieManager.getInstance().setAcceptThirdPartyCookies(webView, true);
+        CookieManager.getInstance().setAcceptCookie(true);
     }
 
     
