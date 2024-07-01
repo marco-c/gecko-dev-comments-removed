b5593a22caed213f1deb9e4f05e5562859a7a4e9
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-07-01 14:23:24 +0000

    [focus] Separate regular and custom tab sessions in SessionManager (https://github.com/mozilla-mobile/focus-android/issues/770)
    UltraBlame original commit: 4180f39634cba73544ef6fd70ba887ace6519bbb

--- mobile/android/focus-android/app/src/test/java/org/mozilla/focus/customtabs/CustomTabConfigTest.java
+++ mobile/android/focus-android/app/src/test/java/org/mozilla/focus/customtabs/CustomTabConfigTest.java
@@ -21,6 +21,7 @@ import org.robolectric.RuntimeEnvironment;
 import org.robolectric.annotation.Config;
 
 import java.lang.reflect.Field;
+import java.util.UUID;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
@@ -99,6 +100,7 @@ public class CustomTabConfigTest {
         builder.addMenuItem("menuitemIGNORED", null);
 
         final CustomTabsIntent customTabsIntent = builder.build();
+        customTabsIntent.intent.putExtra(CustomTabConfig.EXTRA_CUSTOM_TAB_ID, UUID.randomUUID().toString());
 
         final CustomTabConfig config = CustomTabConfig.parseCustomTabIntent(RuntimeEnvironment.application, new SafeIntent(customTabsIntent.intent));
 
@@ -137,6 +139,8 @@ public class CustomTabConfigTest {
 
         
         assertFalse(CustomTabConfig.isCustomTabIntent(new SafeIntent(customTabsIntent.intent)));
+
+        customTabsIntent.intent.putExtra(CustomTabConfig.EXTRA_CUSTOM_TAB_ID, UUID.randomUUID().toString());
 
         
         final CustomTabConfig c = CustomTabConfig.parseCustomTabIntent(RuntimeEnvironment.application, new SafeIntent(customTabsIntent.intent));
@@ -260,5 +264,14 @@ public class CustomTabConfigTest {
         final Bitmap bitmap = CustomTabConfig.getCloseButtonIcon(RuntimeEnvironment.application, new SafeIntent(customTabsIntent.intent));
 
         assertNull(bitmap);
+    }
+
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void idIsRequired() {
+        final CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
+        final CustomTabsIntent customTabsIntent = builder.build();
+
+        CustomTabConfig.parseCustomTabIntent(RuntimeEnvironment.application, new SafeIntent(customTabsIntent.intent));
     }
 }
