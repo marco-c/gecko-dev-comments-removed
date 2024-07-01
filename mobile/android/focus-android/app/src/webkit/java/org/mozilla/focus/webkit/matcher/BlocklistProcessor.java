8a035af41b6f6f1b0f83a99f90be0aa0402d276f
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-07-01 12:47:54 +0000

    [focus] Only move Twitter/FB from "Disconnect"/"Social"
    I forgot to implement the filtering of other sites in https://github.com/mozilla-mobile/focus-android/issues/20. Only
    Twitter and FB remain, and are moved into "Social". Google is added in a
    separate list (https://github.com/mozilla-mobile/focus-android/issues/94). That only leaves itisatracker.com which is unneeded.
    
    UltraBlame original commit: 5477e4c3b4e636c44380b67475572afa68f6c983

--- mobile/android/focus-android/app/src/webkit/java/org/mozilla/focus/webkit/matcher/BlocklistProcessor.java
+++ mobile/android/focus-android/app/src/webkit/java/org/mozilla/focus/webkit/matcher/BlocklistProcessor.java
@@ -35,6 +35,20 @@ public class BlocklistProcessor {
         IGNORED_CATEGORIES = Collections.unmodifiableSet(ignored);
     }
 
+    
+
+
+    private static final Set<String> DISCONNECT_MOVED;
+
+    static {
+        final Set<String> moved = new HashSet<>();
+
+        moved.add("Facebook");
+        moved.add("Twitter");
+
+        DISCONNECT_MOVED = Collections.unmodifiableSet(moved);
+    }
+
     public static Map<String, Trie> loadCategoryMap(final JsonReader reader) throws IOException {
         final Map<String, Trie> categoryMap = new HashMap<>(5);
 
@@ -59,19 +73,27 @@ public class BlocklistProcessor {
     }
 
     private interface UrlListCallback {
-        void put(final String url);
+        void put(final String url, final String siteOwner);
     }
 
     private static class ListCallback implements UrlListCallback {
         final List<String> list;
+        final Set<String> desiredOwners;
 
-        ListCallback(final List<String> list) {
+        
+
+
+
+        ListCallback(final List<String> list, final Set<String> desiredOwners) {
             this.list = list;
+            this.desiredOwners = desiredOwners;
         }
 
         @Override
-        public void put(final String url) {
-            list.add(url);
+        public void put(final String url, final String siteOwner) {
+            if (desiredOwners.contains(siteOwner)) {
+                list.add(url);
+            }
         }
     }
 
@@ -83,7 +105,7 @@ public class BlocklistProcessor {
         }
 
         @Override
-        public void put(String url) {
+        public void put(final String url, final String siteOwner) {
             trie.put(FocusString.create(url).reverse());
         }
     }
@@ -100,7 +122,7 @@ public class BlocklistProcessor {
                 reader.skipValue();
             } else if (categoryName.equals(DISCONNECT)) {
                 
-                ListCallback callback = new ListCallback(socialOverrides);
+                ListCallback callback = new ListCallback(socialOverrides, DISCONNECT_MOVED);
                 extractCategory(reader, callback);
             } else {
                 final Trie categoryTrie = Trie.createRootNode();
@@ -137,7 +159,7 @@ public class BlocklistProcessor {
     private static void extractSite(final JsonReader reader, final UrlListCallback callback) throws IOException {
         reader.beginObject();
 
-        final String siteName = reader.nextName();
+        final String siteOwner = reader.nextName();
         {
             reader.beginObject();
 
@@ -153,7 +175,7 @@ public class BlocklistProcessor {
 
                     while (reader.hasNext()) {
                         final String blockURL = reader.nextString();
-                        callback.put(blockURL);
+                        callback.put(blockURL, siteOwner);
                     }
 
                     reader.endArray();
