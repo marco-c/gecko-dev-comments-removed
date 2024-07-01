30013fbaa799a04986b3cb813e2cb4e4e894e7b3
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-07-01 14:05:28 +0000

    [focus] [detekt] Wrap code that exceed the maximum line length. (https://github.com/mozilla-mobile/focus-android/issues/1308).
    UltraBlame original commit: a1bcdbcd1eb6d9f4799478637c0bebbf167f2999

--- mobile/android/focus-android/app/src/main/java/org/mozilla/focus/utils/FileUtils.kt
+++ mobile/android/focus-android/app/src/main/java/org/mozilla/focus/utils/FileUtils.kt
@@ -25,12 +25,12 @@ class FileUtils {
         fun deleteWebViewDirectory(context: Context): Boolean {
             val webviewDirectory = File(context.applicationInfo.dataDir, WEBVIEW_DIRECTORY)
             return deleteContent(webviewDirectory, doNotEraseWhitelist = setOf(
-                    LOCAL_STORAGE_DIR 
+                    
+                    
+                    LOCAL_STORAGE_DIR
             ))
         }
 
-        @SuppressFBWarnings("BC_BAD_CAST_TO_ABSTRACT_COLLECTION",
-                "filter casts to Collection and storing in val casts back to List: https://youtrack.jetbrains.com/issue/KT-18311")
         private fun deleteContent(directory: File, doNotEraseWhitelist: Set<String> = emptySet()): Boolean {
             val filesToDelete = directory.listFiles()?.filter { !doNotEraseWhitelist.contains(it.name) } ?: return false
             return filesToDelete.all { it.deleteRecursively() }
