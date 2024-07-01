46511913a957d405fd299a149cf8034f8cfb0988
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-07-01 15:17:57 +0000

    [focus] Closes https://github.com/mozilla-mobile/focus-android/issues/3523 Set All Fields of Session Security Info in onPageFinished
    UltraBlame original commit: 45392a7dc01e9f91114db8c56be9f67eb66493b8

--- mobile/android/focus-android/app/src/main/java/org/mozilla/focus/webview/FocusWebViewClient.java
+++ mobile/android/focus-android/app/src/main/java/org/mozilla/focus/webview/FocusWebViewClient.java
@@ -23,6 +23,9 @@ import org.mozilla.focus.utils.IntentUtils;
 import org.mozilla.focus.utils.UrlUtils;
 import org.mozilla.focus.web.IWebView;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
 import static android.view.View.IMPORTANT_FOR_ACCESSIBILITY_NO;
 import static android.view.View.IMPORTANT_FOR_ACCESSIBILITY_YES;
 
@@ -216,7 +219,14 @@ import static android.view.View.IMPORTANT_FOR_ACCESSIBILITY_YES;
             final boolean isSecure = certificate != null || UrlUtils.isLocalizedContent(view.getUrl());
 
             callback.onPageFinished(isSecure);
-            callback.onSecurityChanged(isSecure, null, (certificate != null) ? certificate.getIssuedBy().getOName() : null);
+
+            String host = null;
+            try {
+                host = new URI(url).getHost();
+            } catch (URISyntaxException e) {
+                e.printStackTrace();
+            }
+            callback.onSecurityChanged(isSecure, host, (certificate != null) ? certificate.getIssuedBy().getOName() : null);
             
             
             final String viewURL = view.getUrl();
