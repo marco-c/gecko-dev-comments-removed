e6f693921b2986cf171e25240663c1b400ab8dbe
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-07-01 14:19:43 +0000

    [focus] sign-preview-builds.py: Update build output paths. (https://github.com/mozilla-mobile/focus-android/issues/2181)
    UltraBlame original commit: 0eeff295fccff4e49b1be8aff1cdf0e5667dbb56

--- mobile/android/focus-android/tools/taskcluster/sign-preview-builds.py
+++ mobile/android/focus-android/tools/taskcluster/sign-preview-builds.py
@@ -40,12 +40,12 @@ write_secret_to_file('key_password', data, key='keyPassword')
 build_output_path = os.path.join(os.path.dirname(__file__), '../../app/build/outputs/apk')
 
 
-for apk in glob.glob(build_output_path + "/*-unsigned.apk"):
+for apk in glob.glob(build_output_path + "/*/*/*-unsigned.apk"):
 	split = os.path.splitext(apk)
 	print subprocess.check_output(["zipalign", "-f", "-v", "-p", "4", apk, split[0] + "-aligned" + split[1]])
 
 
-for apk in glob.glob(build_output_path + "/*-aligned.apk"):
+for apk in glob.glob(build_output_path + "/*/*/*-aligned.apk"):
 	
 	print "Signing", apk
 	print subprocess.check_output([
@@ -64,7 +64,7 @@ if not os.path.exists(artifacts_path):
     os.makedirs(artifacts_path)
 
 
-for apk in glob.glob(build_output_path + "/*-signed-*.apk"):
+for apk in glob.glob(build_output_path + "/*/*/*-signed-*.apk"):
 	print "Verifying", apk
 	print subprocess.check_output(['apksigner', 'verify', apk])
 
