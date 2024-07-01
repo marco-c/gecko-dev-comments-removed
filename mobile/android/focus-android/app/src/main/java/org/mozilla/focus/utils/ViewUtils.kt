a6809b9c0fad6b8f3db6046786e2d5e996777017
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-07-01 15:12:22 +0000

    [focus] Consider going directly to website when clicking on a URL in suggestions
    Fixes https://github.com/mozilla-mobile/focus-android/issues/3232
    
    UltraBlame original commit: 470d5cd417a9f4dd3ada4cb605e096a03e53104d

---
+++ mobile/android/focus-android/app/src/main/java/org/mozilla/focus/utils/ViewUtils.kt
@@ -0,0 +1,137 @@
+
+
+
+
+
+package org.mozilla.focus.utils
+
+import android.app.Activity
+import android.content.Context
+import android.os.Handler
+import android.os.Looper
+import android.support.annotation.StringRes
+import android.support.design.widget.Snackbar
+import android.support.v4.content.ContextCompat
+import android.support.v4.view.ViewCompat
+import android.view.Gravity
+import android.view.MenuItem
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.TextView
+import org.mozilla.focus.R
+import java.lang.ref.WeakReference
+
+object ViewUtils {
+
+    private const val MENU_ITEM_ALPHA_ENABLED = 255
+    private const val MENU_ITEM_ALPHA_DISABLED = 130
+
+    
+
+
+
+
+    const val IME_FLAG_NO_PERSONALIZED_LEARNING = 0x01000000
+
+    
+
+
+    @Suppress("ReturnCount")
+    private class ShowKeyboard internal constructor(view: View?) : Runnable {
+        companion object {
+            private const val INTERVAL_MS = 100
+            private const val MAX_TRIES = 10
+        }
+
+        private val viewReference: WeakReference<View?> = WeakReference(view)
+        private val handler: Handler = Handler(Looper.getMainLooper())
+        private var tries: Int = MAX_TRIES
+
+        override fun run() {
+            val myView = viewReference.get() ?: return
+            val activity: Activity? = myView.context?.asActivity() ?: return
+            val imm = activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager ?: return
+
+            when {
+                tries <= 0 -> return
+                !myView.isFocusable -> return
+                !myView.isFocusableInTouchMode -> return
+                !myView.requestFocus() -> {
+                    post()
+                    return
+                }
+                !imm.isActive(myView) -> {
+                    post()
+                    return
+                }
+                !imm.showSoftInput(myView, InputMethodManager.SHOW_IMPLICIT) -> {
+                    post()
+                }
+            }
+        }
+
+        internal fun post() {
+            tries--
+            handler.postDelayed(this, INTERVAL_MS.toLong())
+        }
+    }
+
+    fun showKeyboard(view: View?) {
+        val showKeyboard = ShowKeyboard(view)
+        showKeyboard.post()
+    }
+
+    fun hideKeyboard(view: View?) {
+        val imm = view?.context?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
+    
+
+
+    fun showBrandedSnackbar(view: View?, @StringRes resId: Int, delayMillis: Int) {
+        val context = view!!.context
+        val snackbar = Snackbar.make(view, resId, Snackbar.LENGTH_LONG)
+
+        val snackbarView = snackbar.view
+        snackbarView.setBackgroundColor(ContextCompat.getColor(context, R.color.snackbarBackground))
+
+        val snackbarTextView = snackbarView.findViewById<View>(android.support.design.R.id.snackbar_text) as TextView
+        snackbarTextView.setTextColor(ContextCompat.getColor(context, R.color.snackbarTextColor))
+        snackbarTextView.gravity = Gravity.CENTER
+        snackbarTextView.textAlignment = View.TEXT_ALIGNMENT_CENTER
+
+        view.postDelayed({ snackbar.show() }, delayMillis.toLong())
+    }
+
+    fun getBrandedSnackbar(view: View, @StringRes resId: Int): Snackbar {
+        val context = view.context
+        val snackbar = Snackbar.make(view, resId, Snackbar.LENGTH_LONG)
+        val snackbarView = snackbar.view
+        snackbarView.setBackgroundColor(ContextCompat.getColor(context, R.color.snackbarBackground))
+        val snackbarTextView = snackbarView.findViewById<TextView>(android.support.design.R.id.snackbar_text)
+        snackbarTextView.setTextColor(ContextCompat.getColor(context, R.color.snackbarTextColor))
+        snackbar.setActionTextColor(ContextCompat.getColor(context, R.color.snackbarActionText))
+        return snackbar
+    }
+
+    fun isRTL(view: View): Boolean {
+        return ViewCompat.getLayoutDirection(view) == ViewCompat.LAYOUT_DIRECTION_RTL
+    }
+
+    
+
+
+
+
+
+
+    fun setMenuItemEnabled(menuItem: MenuItem, enabled: Boolean) {
+        menuItem.isEnabled = enabled
+        val icon = menuItem.icon
+        if (icon != null) {
+            icon.mutate().alpha = if (enabled) MENU_ITEM_ALPHA_ENABLED else MENU_ITEM_ALPHA_DISABLED
+        }
+    }
+}
