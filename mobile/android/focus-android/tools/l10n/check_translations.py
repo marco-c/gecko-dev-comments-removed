98365797b775001d3ca28aa438d26ab3c3331dd8
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-07-01 13:54:11 +0000

    [focus] Update check_translations.py
    UltraBlame original commit: 768c7d25b3b310358a186ea94125d3f80808ff68

--- mobile/android/focus-android/tools/l10n/check_translations.py
+++ mobile/android/focus-android/tools/l10n/check_translations.py
@@ -23,21 +23,21 @@ def etree_to_dict(tree):
 
 def missing_target_exception(lang, code):
     
-    print(" * [{lang}/strings.xml] missing placeholder in translation, key: {code}".format(
+    print("Error: * [{lang}/strings.xml] missing placeholder in translation, key: {code}".format(
         lang=lang,
         code=code
     ))
 
 def missing_source_exception(lang, code):
     
-    print(" * [{lang}/strings.xml] placeholder missing in source, key: {code}".format(
+    print("Error:  * [{lang}/strings.xml] placeholder missing in source, key: {code}".format(
         lang=lang,
         code=code
     ))
 
 def count_mismatch_warning(lang, code):
     
-    print(" * [{lang}/strings.xml] number of placeholders not matching, key: {code}".format(
+    print("Warning: * [{lang}/strings.xml] number of placeholders not matching, key: {code}".format(
         lang=lang,
         code=code
     ))
