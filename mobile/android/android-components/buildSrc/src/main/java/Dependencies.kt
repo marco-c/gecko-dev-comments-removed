1fd5a9b5e068bddd30eef30a9d88792519f056d6
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-07-01 04:27:33 +0000

    [components] Closes https://github.com/mozilla-mobile/android-components/issues/5007: Add Tabs engine to the sync manager
    UltraBlame original commit: 9264c52c0548d501e2d347d94669b442ca3c46db

--- mobile/android/android-components/buildSrc/src/main/java/Dependencies.kt
+++ mobile/android/android-components/buildSrc/src/main/java/Dependencies.kt
@@ -4,7 +4,7 @@
 
 // Synchronized version numbers for dependencies used by (some) modules
 object Versions {
-    const val kotlin = "1.3.61"
+    const val kotlin = "1.3.50"
     const val coroutines = "1.3.2"
 
     const val junit = "4.12"
@@ -27,7 +27,7 @@ object Versions {
     const val disklrucache = "2.0.2"
     const val leakcanary = "1.6.3"
 
-    const val mozilla_appservices = "0.42.2"
+    const val mozilla_appservices = "0.44.0"
 
     const val mozilla_glean = "21.2.0"
 
