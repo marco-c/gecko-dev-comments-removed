5883abb82d885504cf5a7367e53d5bfcb4e5d7e7
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-07-01 04:02:46 +0000

    [components] Update GeckoView (nightly) (20191031-142454)
    UltraBlame original commit: e56cdcbcc30c9d705c5c39c669fae7cd051d2a0e

--- mobile/android/android-components/buildSrc/src/main/java/Gecko.kt
+++ mobile/android/android-components/buildSrc/src/main/java/Gecko.kt
@@ -6,12 +6,12 @@ internal object GeckoVersions {
     /**
      * GeckoView Nightly Version.
      */
-    const val nightly_version = "72.0.20191030050308"
+    const val nightly_version = "72.0.20191031095309"
 
     /**
      * GeckoView Beta Version.
      */
-    const val beta_version = "71.0.20191031091835"
+    const val beta_version = "71.0.20191027125310"
 
     /**
      * GeckoView Release Version.
