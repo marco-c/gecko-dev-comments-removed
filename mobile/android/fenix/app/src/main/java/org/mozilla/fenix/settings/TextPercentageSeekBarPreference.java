677891729ef4987cc60331814e0599a457d39040
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-07-01 19:39:05 +0000

    [fenix] Update mapping to represent steps of 5 from 50-200
    UltraBlame original commit: 423df4c590c715371c4bba0f14d9807a4d05af6a

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/settings/TextPercentageSeekBarPreference.java
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/settings/TextPercentageSeekBarPreference.java
@@ -50,8 +50,11 @@ import java.text.NumberFormat;
 
 
 
-public class PercentageSeekBarPreference extends Preference {
+public class TextPercentageSeekBarPreference extends Preference {
     private static final String TAG = "SeekBarPreference";
+    private static final int STEP_SIZE = 5;
+    private static final int MIN_VALUE = 50;
+    private static final float DECIMAL_CONVERSION = 100f;
     @SuppressWarnings("WeakerAccess") 
             int mSeekBarValue;
     @SuppressWarnings("WeakerAccess") 
@@ -134,7 +137,7 @@ public class PercentageSeekBarPreference extends Preference {
         }
     };
 
-    public PercentageSeekBarPreference(
+    public TextPercentageSeekBarPreference(
             Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
 
@@ -154,15 +157,15 @@ public class PercentageSeekBarPreference extends Preference {
         a.recycle();
     }
 
-    public PercentageSeekBarPreference(Context context, AttributeSet attrs, int defStyleAttr) {
+    public TextPercentageSeekBarPreference(Context context, AttributeSet attrs, int defStyleAttr) {
         this(context, attrs, defStyleAttr, 0);
     }
 
-    public PercentageSeekBarPreference(Context context, AttributeSet attrs) {
+    public TextPercentageSeekBarPreference(Context context, AttributeSet attrs) {
         this(context, attrs, R.attr.seekBarPreferenceStyle);
     }
 
-    public PercentageSeekBarPreference(Context context) {
+    public TextPercentageSeekBarPreference(Context context) {
         this(context, null);
     }
 
@@ -415,8 +418,9 @@ public class PercentageSeekBarPreference extends Preference {
     @SuppressWarnings("WeakerAccess") 
     void updateLabelValue(int value) {
         if (mSeekBarValueTextView != null) {
-            double m = value / 100d;
-            final String percentage = NumberFormat.getPercentInstance().format(m);
+            value = (value * STEP_SIZE) + MIN_VALUE;
+            final double decimalValue = value / DECIMAL_CONVERSION;
+            final String percentage = NumberFormat.getPercentInstance().format(decimalValue);
             mSeekBarValueTextView.setText(percentage);
         }
     }
@@ -429,9 +433,10 @@ public class PercentageSeekBarPreference extends Preference {
     @SuppressWarnings("WeakerAccess") 
     void updateExampleTextValue(int value) {
         if (mExampleTextTextView != null) {
-            float decimal = value / 100f;
-            final float textsize = 16f * decimal;
-            mExampleTextTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, textsize);
+            value = (value * STEP_SIZE) + MIN_VALUE;
+            final float decimal = value / DECIMAL_CONVERSION;
+            final float textSize = 16f * decimal;
+            mExampleTextTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, textSize);
         }
     }
 
