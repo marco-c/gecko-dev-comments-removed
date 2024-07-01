e2970186cf39fc6b6e849edb2a20ed0da8ea0065
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-07-01 08:01:13 +0000

    [components] Bump 'settings' migration version to re-run it
    We retain Fennec's preferences on disk, so re-running this migration will
    make sure that marketing/experiments settings in Fenix correspond to the FHR setting
    in Fennec (they'll be turned off if FHR settings is 'off', and left 'on' if FHR is 'on').
    
    For the users running this migration for the first time, it's just a regular migration.
    
    For the users who already ran this migration, we will re-align data collections settings around
    their choices in Fennec.
    
    UltraBlame original commit: c6843e604c319d186355ee9b6c9f6902ed3a5bf8

--- mobile/android/android-components/components/support/migration/src/main/java/mozilla/components/support/migration/FennecMigrator.kt
+++ mobile/android/android-components/components/support/migration/src/main/java/mozilla/components/support/migration/FennecMigrator.kt
@@ -91,7 +91,7 @@ sealed class Migration(val currentVersion: Int) {
     /**
      * Migrates all Fennec settings backed by SharedPreferences.
      */
-    object Settings : Migration(currentVersion = 1)
+    object Settings : Migration(currentVersion = 2)
 
     /**
      * Migrates / Disables all currently unsupported Add-ons.
@@ -1085,12 +1085,14 @@ class FennecMigrator private constructor(
                 return when (val failure = migrationFailureWrapper.failure) {
                     is SearchEngineMigrationResult.Failure.NoDefault -> {
                         logger.error("Missing search engine default: $failure")
+                        crashReporter.submitCaughtException(migrationFailureWrapper)
                         MigrationSearch.failureReason.add(FailureReasonTelemetryCodes.SEARCH_NO_DEFAULT.code)
                         result
                     }
 
                     is SearchEngineMigrationResult.Failure.NoMatch -> {
                         logger.error("Could not find matching search engine: $failure")
+                        crashReporter.submitCaughtException(migrationFailureWrapper)
                         MigrationSearch.failureReason.add(FailureReasonTelemetryCodes.SEARCH_NO_MATCH.code)
                         result
                     }
