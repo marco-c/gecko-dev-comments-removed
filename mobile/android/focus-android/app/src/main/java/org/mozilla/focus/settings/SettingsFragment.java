e9f4f7b18bf16ebacb87eb49a31e3fcef935d790
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-07-01 12:49:23 +0000

    [focus] Add setting to make Focus default browser on Android N+. (https://github.com/mozilla-mobile/focus-android/issues/72)
    UltraBlame original commit: b3c5c74d9cffc4e77451f612a37d227fb8755856

--- mobile/android/focus-android/app/src/main/java/org/mozilla/focus/settings/SettingsFragment.java
+++ mobile/android/focus-android/app/src/main/java/org/mozilla/focus/settings/SettingsFragment.java
@@ -1,10 +1,18 @@
+
+
+
+
+
 package org.mozilla.focus.settings;
 
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceGroup;
 import android.support.annotation.Nullable;
 
 import org.mozilla.focus.R;
+import org.mozilla.focus.widget.DefaultBrowserPreference;
 
 public class SettingsFragment extends PreferenceFragment {
     @Override
@@ -12,5 +20,23 @@ public class SettingsFragment extends PreferenceFragment {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.settings);
+
+        setupPreferences(getPreferenceScreen());
+    }
+
+    private void setupPreferences(PreferenceGroup group) {
+        for (int i = 0; i < group.getPreferenceCount(); i++) {
+            final Preference preference = group.getPreference(i);
+
+            if (preference instanceof PreferenceGroup) {
+                setupPreferences((PreferenceGroup) preference);
+            }
+
+            if (preference instanceof DefaultBrowserPreference
+                    && !((DefaultBrowserPreference) preference).shouldBeVisible()) {
+                group.removePreference(preference);
+                i--;
+            }
+        }
     }
 }
