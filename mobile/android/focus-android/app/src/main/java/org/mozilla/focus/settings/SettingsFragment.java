ee467db23836d826c8eef04c494b8e42a54d4b1b
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-07-01 12:54:00 +0000

    [focus] Use new About page, and cleanup old AboutActivity (https://github.com/mozilla-mobile/focus-android/issues/23)
    UltraBlame original commit: 6364de06f25c3e1073a59335a42e9e8d470404a8

--- mobile/android/focus-android/app/src/main/java/org/mozilla/focus/settings/SettingsFragment.java
+++ mobile/android/focus-android/app/src/main/java/org/mozilla/focus/settings/SettingsFragment.java
@@ -14,8 +14,8 @@ import android.preference.PreferenceScreen;
 import android.support.annotation.Nullable;
 
 import org.mozilla.focus.R;
-import org.mozilla.focus.activity.AboutActivity;
 import org.mozilla.focus.activity.HelpActivity;
+import org.mozilla.focus.activity.InfoActivity;
 import org.mozilla.focus.activity.RightsActivity;
 import org.mozilla.focus.widget.DefaultBrowserPreference;
 
@@ -33,7 +33,7 @@ public class SettingsFragment extends PreferenceFragment {
         
         
         if (preference.getTitle().equals(getResources().getString(R.string.menu_about))) {
-            final Intent intent = new Intent(getActivity(), AboutActivity.class);
+            final Intent intent = InfoActivity.getAboutIntent(getActivity());
             startActivity(intent);
         } else if (preference.getTitle().equals(getResources().getString(R.string.menu_help))) {
             final Intent intent = new Intent(getActivity(), RightsActivity.class);
