d8744f35fc0428e17be587d589fec85ca9f1f15a
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-07-01 06:13:17 +0000

    [components] Closes https://github.com/mozilla-mobile/android-components/issues/4397: Migrate feature-readerview to browser-state
    UltraBlame original commit: 20f043a5a41a96a72762fca9bc29646d7045d58e

--- mobile/android/android-components/samples/browser/src/main/java/org/mozilla/samples/browser/DefaultComponents.kt
+++ mobile/android/android-components/samples/browser/src/main/java/org/mozilla/samples/browser/DefaultComponents.kt
@@ -53,18 +53,17 @@ import mozilla.components.feature.pwa.WebAppShortcutManager
 import mozilla.components.feature.pwa.WebAppUseCases
 import mozilla.components.feature.pwa.intent.TrustedWebActivityIntentProcessor
 import mozilla.components.feature.pwa.intent.WebAppIntentProcessor
+import mozilla.components.feature.readerview.ReaderViewMiddleware
 import mozilla.components.feature.search.SearchUseCases
 import mozilla.components.feature.session.HistoryDelegate
 import mozilla.components.feature.session.SessionUseCases
 import mozilla.components.feature.tabs.TabsUseCases
 import mozilla.components.feature.webnotifications.WebNotificationFeature
 import mozilla.components.lib.fetch.httpurlconnection.HttpURLConnectionClient
-import mozilla.components.lib.nearby.NearbyConnection
 import org.mozilla.samples.browser.addons.AddonsActivity
 import org.mozilla.samples.browser.ext.components
 import org.mozilla.samples.browser.integration.FindInPageIntegration
 import org.mozilla.samples.browser.media.MediaService
-import org.mozilla.samples.browser.integration.P2PIntegration
 import org.mozilla.samples.browser.request.SampleRequestInterceptor
 import java.util.concurrent.TimeUnit
 
@@ -110,7 +109,8 @@ open class DefaultComponents(private val applicationContext: Context) {
 
     val store by lazy {
         BrowserStore(middleware = listOf(
-            MediaMiddleware(applicationContext, MediaService::class.java)
+            MediaMiddleware(applicationContext, MediaService::class.java),
+            ReaderViewMiddleware()
         ))
     }
 
@@ -182,11 +182,6 @@ open class DefaultComponents(private val applicationContext: Context) {
     val webAppShortcutManager by lazy { WebAppShortcutManager(applicationContext, client, webAppManifestStorage) }
     val webAppUseCases by lazy { WebAppUseCases(applicationContext, sessionManager, webAppShortcutManager) }
 
-    // P2P communication
-    val nearbyConnection by lazy {
-        NearbyConnection(applicationContext)
-    }
-
     // Intent
     val tabIntentProcessor by lazy {
         TabIntentProcessor(sessionManager, sessionUseCases.loadUrl, searchUseCases.newTabSearch)
@@ -233,9 +228,6 @@ open class DefaultComponents(private val applicationContext: Context) {
             },
             SimpleBrowserMenuItem("Find In Page") {
                 FindInPageIntegration.launch?.invoke()
-            },
-            SimpleBrowserMenuItem("P2P") {
-                P2PIntegration.launch?.invoke()
             },
             BrowserMenuDivider()
         )
@@ -294,28 +286,27 @@ open class DefaultComponents(private val applicationContext: Context) {
 
     private val menuToolbar by lazy {
         val forward = BrowserMenuItemToolbar.Button(
-            mozilla.components.ui.icons.R.drawable.mozac_ic_forward,
-            iconTintColorResource = R.color.photonBlue90,
-            contentDescription = "Forward",
-            isEnabled = { sessionManager.selectedSession?.canGoForward == true }
+                mozilla.components.ui.icons.R.drawable.mozac_ic_forward,
+                iconTintColorResource = R.color.photonBlue90,
+                contentDescription = "Forward",
+                isEnabled = { sessionManager.selectedSession?.canGoForward == true }
         ) {
             sessionUseCases.goForward()
         }
 
         val refresh = BrowserMenuItemToolbar.Button(
-            mozilla.components.ui.icons.R.drawable.mozac_ic_refresh,
-            iconTintColorResource = R.color.photonBlue90,
-            contentDescription = "Refresh",
-            isEnabled = { sessionManager.selectedSession?.loading != true }
+                mozilla.components.ui.icons.R.drawable.mozac_ic_refresh,
+                iconTintColorResource = R.color.photonBlue90,
+                contentDescription = "Refresh",
+                isEnabled = { sessionManager.selectedSession?.loading != true }
         ) {
             sessionUseCases.reload()
         }
 
         val stop = BrowserMenuItemToolbar.Button(
-            mozilla.components.ui.icons.R.drawable.mozac_ic_stop,
-            iconTintColorResource = R.color.photonBlue90,
-            contentDescription = "Stop"
-        ) {
+                mozilla.components.ui.icons.R.drawable.mozac_ic_stop,
+                iconTintColorResource = R.color.photonBlue90,
+                contentDescription = "Stop") {
             sessionUseCases.stopLoading()
         }
 
