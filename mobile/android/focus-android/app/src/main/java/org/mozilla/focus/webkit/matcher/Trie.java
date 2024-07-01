a853f97bd9cb858dad21d26fcedf63279d9527b8
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-07-01 12:44:38 +0000

    [focus] Move whitelist code into WhitelistTrie implementation
    UltraBlame original commit: 5ec58d565f253073d7614c5d8200dd710303ba7d

--- mobile/android/focus-android/app/src/main/java/org/mozilla/focus/webkit/matcher/Trie.java
+++ mobile/android/focus-android/app/src/main/java/org/mozilla/focus/webkit/matcher/Trie.java
@@ -4,6 +4,7 @@
 
 package org.mozilla.focus.webkit.matcher;
 
+import android.util.Pair;
 import android.util.SparseArray;
 
  class Trie {
@@ -28,8 +29,14 @@ import android.util.SparseArray;
         }
 
         
-        public WhiteListTrie put(final String string) {
-            return (WhiteListTrie) super.put(string);
+        public void putWhiteList(final String string, final Trie whitelist) {
+            WhiteListTrie node = (WhiteListTrie) super.put(string);
+
+            if (node.whitelist != null) {
+                throw new IllegalStateException("Whitelist already set for node " + string);
+            }
+
+            node.whitelist = whitelist;
         }
     }
 
