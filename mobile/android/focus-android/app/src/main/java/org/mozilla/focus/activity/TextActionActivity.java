1cce5b63c1ad5c01749f63094b95d008c971b331
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-07-01 14:24:29 +0000

    [focus] Use mozilla.components.utils and remove unneeded code.
    UltraBlame original commit: a46350d70d67d6da8edfa2acabe6f99342eef412

--- mobile/android/focus-android/app/src/main/java/org/mozilla/focus/activity/TextActionActivity.java
+++ mobile/android/focus-android/app/src/main/java/org/mozilla/focus/activity/TextActionActivity.java
@@ -13,8 +13,9 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
 
-import org.mozilla.focus.utils.SafeIntent;
 import org.mozilla.focus.utils.UrlUtils;
+
+import mozilla.components.utils.SafeIntent;
 
 
 
