4ff51b8503f0462fb2e5cd8c45f3d042fc163fe2
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-07-01 13:21:40 +0000

    [focus] Use urlbar URL instead of webview URL for reliability and null safety (https://github.com/mozilla-mobile/focus-android/issues/648) (https://github.com/mozilla-mobile/focus-android/issues/649)
    UltraBlame original commit: 63267f08be3ee2b1c37eb31d0a7375039fe3e2e6

--- mobile/android/focus-android/app/src/main/java/org/mozilla/focus/fragment/BrowserFragment.java
+++ mobile/android/focus-android/app/src/main/java/org/mozilla/focus/fragment/BrowserFragment.java
@@ -499,14 +499,9 @@ public class BrowserFragment extends WebFragment implements View.OnClickListener
             }
 
             case R.id.share: {
-                final IWebView webView = getWebView();
-                if (webView == null) {
-                    return;
-                }
-
                 final Intent shareIntent = new Intent(Intent.ACTION_SEND);
                 shareIntent.setType("text/plain");
-                shareIntent.putExtra(Intent.EXTRA_TEXT, webView.getUrl());
+                shareIntent.putExtra(Intent.EXTRA_TEXT, getUrl());
                 startActivity(Intent.createChooser(shareIntent, getString(R.string.share_dialog_title)));
 
                 TelemetryWrapper.shareEvent();
@@ -518,12 +513,7 @@ public class BrowserFragment extends WebFragment implements View.OnClickListener
                 break;
 
             case R.id.open_default: {
-                final IWebView webView = getWebView();
-                if (webView == null) {
-                    return;
-                }
-
-                final Browsers browsers = new Browsers(getContext(), webView.getUrl());
+                final Browsers browsers = new Browsers(getContext(), getUrl());
 
                 final ActivityInfo defaultBrowser = browsers.getDefaultBrowser();
 
@@ -533,7 +523,7 @@ public class BrowserFragment extends WebFragment implements View.OnClickListener
                     throw new IllegalStateException("<Open with $Default> was shown when no default browser is set");
                 }
 
-                final Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(webView.getUrl()));
+                final Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(getUrl()));
                 intent.setPackage(defaultBrowser.packageName);
                 startActivity(intent);
 
@@ -542,15 +532,10 @@ public class BrowserFragment extends WebFragment implements View.OnClickListener
             }
 
             case R.id.open_firefox: {
-                final IWebView webView = getWebView();
-                if (webView == null) {
-                    return;
-                }
-
-                final Browsers browsers = new Browsers(getContext(), webView.getUrl());
+                final Browsers browsers = new Browsers(getContext(), getUrl());
 
                 if (browsers.hasFirefoxBrandedBrowserInstalled()) {
-                    final Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(webView.getUrl()));
+                    final Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(getUrl()));
                     intent.setPackage(browsers.getFirefoxBrandedBrowser().packageName);
                     startActivity(intent);
                 } else {
@@ -562,15 +547,10 @@ public class BrowserFragment extends WebFragment implements View.OnClickListener
             }
 
             case R.id.open_select_browser: {
-                final IWebView webView = getWebView();
-                if (webView == null) {
-                    return;
-                }
-
-                final Browsers browsers = new Browsers(getContext(), webView.getUrl());
+                final Browsers browsers = new Browsers(getContext(), getUrl());
 
                 final OpenWithFragment fragment = OpenWithFragment.newInstance(
-                        browsers.getInstalledBrowsers(), webView.getUrl());
+                        browsers.getInstalledBrowsers(), getUrl());
                 fragment.show(getFragmentManager(), OpenWithFragment.FRAGMENT_TAG);
 
                 TelemetryWrapper.openSelectionEvent();
@@ -612,10 +592,13 @@ public class BrowserFragment extends WebFragment implements View.OnClickListener
         stopButton.setVisibility(isLoading ? View.VISIBLE : View.GONE);
     }
 
-    @Nullable
+    @NonNull
     public String getUrl() {
-        final IWebView webView = getWebView();
-        return webView != null ? webView.getUrl() : null;
+        
+        
+        
+        
+        return urlView.getText().toString();
     }
 
     public boolean canGoForward() {
