64c7ca31baa9193ee64aac3699ef3fce175c78d1
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-07-01 12:38:26 +0000

    Bug 1800611 - part 4: Do not strip out tasks when using `try_task_config.json`
    UltraBlame original commit: 33ed9bf9719afc4812783686ebe97f729b356817

--- mobile/android/taskcluster/ac_taskgraph/loader/build_config.py
+++ mobile/android/taskcluster/ac_taskgraph/loader/build_config.py
@@ -120,7 +120,14 @@ def loader(kind, path, config, params, loaded_tasks):
     
     
     
-    if params["tasks_for"] in ("github-pull-request", "github-pull-request-untrusted", "github-push") and params["head_ref"] not in ("main", "refs/heads/main"):
+    should_take_out_tasks = all((
+        params["tasks_for"] in ("github-pull-request", "github-pull-request-untrusted", "github-push"),
+        params["head_ref"] not in ("main", "refs/heads/main"),
+        
+        
+        params["target_tasks_method"] == "default",
+    ))
+    if should_take_out_tasks:
         logger.info("Looking for changed files to rebuild the modified components only...")
         files_changed = get_changed_files(params["head_repository"], params["head_rev"], params["base_rev"])
         affected_components = get_affected_components(files_changed, config.get("files-affecting-components"), upstream_component_dependencies, downstream_component_dependencies)
