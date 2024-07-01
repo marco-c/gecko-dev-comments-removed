51a7f4e69b14100c3aa1a4bdfdb4b5da045de2fb
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-07-01 15:37:34 +0000

    [focus] No issue: Add missing categories of blocklist entries
    UltraBlame original commit: 54e0579bbf33fda2ff24cc485456435c9a04e695

--- mobile/android/focus-android/app/src/main/java/org/mozilla/focus/webview/matcher/UrlMatcher.java
+++ mobile/android/focus-android/app/src/main/java/org/mozilla/focus/webview/matcher/UrlMatcher.java
@@ -51,6 +51,8 @@ public class UrlMatcher implements  SharedPreferences.OnSharedPreferenceChangeLi
         tempMap.put(context.getString(R.string.pref_key_privacy_block_analytics), "Analytics");
         tempMap.put(context.getString(R.string.pref_key_privacy_block_social), "Social");
         tempMap.put(context.getString(R.string.pref_key_privacy_block_other), "Content");
+        tempMap.put(context.getString(R.string.pref_key_privacy_block_fingerprinting), "Fingerprinting");
+        tempMap.put(context.getString(R.string.pref_key_privacy_block_cryptomining), "Cryptomining");
 
         
         tempMap.put(context.getString(R.string.pref_key_performance_block_webfonts), WEBFONTS);
@@ -113,7 +115,7 @@ public class UrlMatcher implements  SharedPreferences.OnSharedPreferenceChangeLi
         
         for (final Map.Entry<String, Trie> entry: categoryMap.entrySet()) {
             if (!categoryPrefMap.values().contains(entry.getKey())) {
-                throw new IllegalArgumentException("categoryMap contains undeclared category");
+                throw new IllegalArgumentException("categoryMap contains undeclared category: " + entry.getKey());
             }
 
             
