dbfee11db46f1f79413add12f3838362507992fe
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-07-01 15:02:44 +0000

    [focus] Issue https://github.com/mozilla-mobile/focus-android/issues/3190: Convert fragments from Java to Kotlin.
    UltraBlame original commit: 0048bd05e8e5e6a257d900f84099e285bc8a63fa

---
+++ mobile/android/focus-android/app/src/main/java/org/mozilla/focus/fragment/WebFragment.kt
@@ -0,0 +1,135 @@
+
+
+
+
+
+package org.mozilla.focus.fragment
+
+import android.os.Bundle
+import android.text.TextUtils
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.webkit.WebView
+
+import org.mozilla.focus.R
+import org.mozilla.focus.locale.LocaleAwareFragment
+import org.mozilla.focus.locale.LocaleManager
+import org.mozilla.focus.session.Session
+import org.mozilla.focus.utils.AppConstants
+import org.mozilla.focus.web.IWebView
+
+import java.util.Locale
+
+
+
+
+abstract class WebFragment : LocaleAwareFragment() {
+    private var webViewInstance: IWebView? = null
+    private var isWebViewAvailable: Boolean = false
+
+    abstract val session: Session?
+
+    
+
+
+    abstract val initialUrl: String?
+
+    
+
+
+
+    abstract fun inflateLayout(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View
+
+    abstract fun createCallback(): IWebView.Callback
+
+    
+
+
+    abstract fun onCreateViewCalled()
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflateLayout(inflater, container, savedInstanceState)
+
+        val actualWebView = view.findViewById<View>(R.id.webview)
+        webViewInstance = actualWebView as IWebView
+
+        isWebViewAvailable = true
+        webViewInstance!!.setCallback(createCallback())
+
+        val session = session
+
+        if (session != null) {
+            webViewInstance!!.setBlockingEnabled(session.isBlockingEnabled)
+            webViewInstance!!.setRequestDesktop(session.shouldRequestDesktopSite())
+        }
+
+        if (session == null || !session.hasWebViewState()) {
+            val url = initialUrl
+            if (!TextUtils.isEmpty(url)) {
+                webViewInstance!!.loadUrl(url)
+            }
+        } else if (!AppConstants.isGeckoBuild) {
+            webViewInstance!!.restoreWebViewState(session)
+        }
+
+        onCreateViewCalled()
+        return view
+    }
+
+    override fun applyLocale() {
+        val context = context ?: return
+        val localeManager = LocaleManager.getInstance()
+        if (!localeManager.isMirroringSystemLocale(context)) {
+            val currentLocale = localeManager.getCurrentLocale(context)
+            Locale.setDefault(currentLocale)
+            val resources = context.resources
+            val config = resources.configuration
+            config.setLocale(currentLocale)
+
+            @Suppress("DEPRECATION")
+            context.resources.updateConfiguration(config, null)
+        }
+        
+        
+        val unneeded = WebView(getContext())
+        unneeded.destroy()
+    }
+
+    override fun onPause() {
+        val session = session
+        if (session != null) {
+            webViewInstance!!.saveWebViewState(session)
+        }
+
+        webViewInstance!!.onPause()
+
+        super.onPause()
+    }
+
+    override fun onResume() {
+        webViewInstance!!.onResume()
+
+        super.onResume()
+    }
+
+    override fun onDestroy() {
+        if (webViewInstance != null) {
+            webViewInstance!!.setCallback(null)
+            webViewInstance!!.destroy()
+            webViewInstance = null
+        }
+
+        super.onDestroy()
+    }
+
+    override fun onDestroyView() {
+        isWebViewAvailable = false
+
+        super.onDestroyView()
+    }
+
+    protected fun getWebView(): IWebView? {
+        return if (isWebViewAvailable) webViewInstance else null
+    }
+}
