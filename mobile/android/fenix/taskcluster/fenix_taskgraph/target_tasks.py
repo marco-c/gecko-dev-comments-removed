5b10f080ebe6ccce1347d736eab2636c34f68374
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-08-22 22:12:37 +0000

    [fenix] Bug 1580778 - Migrate raptor tests to taskgraph (https://github.com/mozilla-mobile/fenix/pull/5361)
    UltraBlame original commit: f783f9bcf90dd001d6ad3b99ee0a1c2eb2464afc

--- mobile/android/fenix/taskcluster/fenix_taskgraph/target_tasks.py
+++ mobile/android/fenix/taskcluster/fenix_taskgraph/target_tasks.py
@@ -7,17 +7,19 @@ from __future__ import absolute_import, print_function, unicode_literals
 from taskgraph.target_tasks import _target_task, standard_filter
 
 
+
 @_target_task('default')
 def target_tasks_default(full_task_graph, parameters, graph_config):
     """Target the tasks which have indicated they should be run on this project
     via the `run_on_projects` attributes."""
-    def filter(t, params):
-        if t.kind == 'old-decision':
-            return True
+    return [l for l, t in full_task_graph.tasks.iteritems() if _old_decision_filter(t, parameters)]
 
-        return standard_filter(t, params)
 
-    return [l for l, t in full_task_graph.tasks.iteritems() if filter(t, parameters)]
+def _old_decision_filter(task, parameters):
+    if task.kind == 'old-decision':
+        return True
+
+    return standard_filter(task, parameters)
 
 
 @_target_task('nightly')
@@ -28,5 +30,10 @@ def target_tasks_raptor(full_task_graph, parameters, graph_config):
 
 @_target_task('raptor')
 def target_tasks_raptor(full_task_graph, parameters, graph_config):
-    
-    return target_tasks_default(full_task_graph, parameters, graph_config)
+    def filter(t, params):
+        if t.kind == 'raptor':
+            return True
+
+        return _old_decision_filter(t, params)
+
+    return [l for l, t in full_task_graph.tasks.iteritems() if filter(t, parameters)]
