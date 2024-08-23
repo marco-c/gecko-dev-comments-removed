98db135f89fc8a86cf9b2539ce545ae6d695f600
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-08-23 14:17:51 +0000

    Bug 1764201 Part 3: Remove screen info stuff from gfxPlatform. r=jgilbert,geckoview-reviewers
    The gfxPlatform::GetScreen* methods have always been redundant. Removing
    them in favor of ScreenManager or nsDeviceContext methods will
    discourage future code authors from adding more redundancy in
    gfxPlatform.
    
    Differential Revision: https://phabricator.services.mozilla.com/D203672
    
    UltraBlame original commit: f42d24696e4330f7845317877bbab5a0eb2cd2e5

--- gfx/thebes/gfxAndroidPlatform.cpp
+++ gfx/thebes/gfxAndroidPlatform.cpp
@@ -89,8 +89,12 @@ gfxAndroidPlatform::gfxAndroidPlatform() {
 
   RegisterStrongMemoryReporter(new FreetypeReporter());
 
-  mOffscreenFormat = GetScreenDepth() == 16 ? SurfaceFormat::R5G6B5_UINT16
-                                            : SurfaceFormat::X8R8G8B8_UINT32;
+  
+  
+  
+  int32_t screenDepth = 0;
+  mOffscreenFormat = screenDepth == 16 ? SurfaceFormat::R5G6B5_UINT16
+                                       : SurfaceFormat::X8R8G8B8_UINT32;
 
   if (StaticPrefs::gfx_android_rgb16_force_AtStartup()) {
     mOffscreenFormat = SurfaceFormat::R5G6B5_UINT16;
--- gfx/thebes/gfxPlatform.h
+++ gfx/thebes/gfxPlatform.h
@@ -646,9 +646,6 @@ class gfxPlatform : public mozilla::layers::MemoryPressureListener {
 
   static mozilla::LogModule* GetLog(eGfxLog aWhichLog);
 
-  int GetScreenDepth() const { return mScreenDepth; }
-  mozilla::gfx::IntSize GetScreenSize() const { return mScreenSize; }
-
   static void PurgeSkiaFontCache();
 
   static bool UsesOffMainThreadCompositing();
@@ -1024,9 +1021,6 @@ class gfxPlatform : public mozilla::layers::MemoryPressureListener {
   
   
   mozilla::layers::LayersBackend mCompositorBackend;
-
-  int32_t mScreenDepth;
-  mozilla::gfx::IntSize mScreenSize;
 
   mozilla::Maybe<mozilla::layers::OverlayInfo> mOverlayInfo;
   mozilla::Maybe<mozilla::layers::SwapChainInfo> mSwapChainInfo;
