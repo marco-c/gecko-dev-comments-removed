3f353b493ab025ca98d3b44458dc4e7e7e237703
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-08-23 15:00:57 +0000

    Bug 1885825 [wpt PR 45152] - webnn: fix input validation tests for large dimensions, a=testonly
    Automatic update from web-platform-tests
    webnn: fix input validation tests for large dimensions
    
    The existing test case didn't actually calculate the number of
    elements because the input dimension value exceeded the max
    `unsigned long`
    
    Fixed: 330060043
    Change-Id: Ie052139b28f86504b4476925d175f4c0c01c5b82
    Cq-Include-Trybots: luci.chromium.try​:win11-blink-rel
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5375859
    Auto-Submit: Feng Dai <feng.daiintel.com>
    Reviewed-by: Austin Sullivan <asullychromium.org>
    Commit-Queue: Feng Dai <feng.daiintel.com>
    Reviewed-by: ningxin hu <ningxin.huintel.com>
    Cr-Commit-Position: refs/heads/main{#1276979}
    
    --
    
    wpt-commits: 4ab7ce11f813676c926d08dcc0e09cbda83402d3
    wpt-pr: 45152
    
    UltraBlame original commit: cc28d6524dcf4783bc56f492ef958bbac56dd4ef

--- testing/web-platform/tests/webnn/resources/utils_validation.js
+++ testing/web-platform/tests/webnn/resources/utils_validation.js
@@ -12,6 +12,11 @@ const allWebNNOperandDataTypes = [
   'uint8'
 ];
 
+
+
+
+
+const kMaxUnsignedLong = 2 ** 32 - 1;
 const unsignedLongType = 'unsigned long';
 
 const dimensions0D = [];
@@ -173,9 +178,7 @@ function generateOutOfRangeValuesArray(type) {
   let range, outsideValueArray;
   switch (type) {
     case 'unsigned long':
-      
-      
-      range = [0, 4294967295];
+      range = [0, kMaxUnsignedLong];
       break;
     default:
       throw new Error(`Unsupport ${type}`);
