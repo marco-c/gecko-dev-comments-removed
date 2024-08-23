0eebf3fd5e58fb781a634cbf1059a63d784483e1
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-08-23 12:52:28 +0000

    Bug 1824856 - android_taskgraph: register payload builder with gecko_taskgraph. r=bhearsum,releng-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D201441
    
    UltraBlame original commit: 9034587678d5274a183602a67e118cc0d9a67729

--- taskcluster/android_taskgraph/worker_types.py
+++ taskcluster/android_taskgraph/worker_types.py
@@ -3,7 +3,7 @@
 
 
 
-from taskgraph.transforms.task import payload_builder
+from gecko_taskgraph.transforms.task import payload_builder
 from taskgraph.util.schema import taskref_or_string
 from voluptuous import Any, Optional, Required
 
