5bb5a386cf83e83cee2d9e3b6a921ec906f634cf
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-08-23 14:54:27 +0000

    Bug 1882344 - Have Document instead of WakeLockJS release locks on loss of activity. r=edgar,dom-core
    Differential Revision: https://phabricator.services.mozilla.com/D202853
    
    UltraBlame original commit: 8130451713b9fe89ab08a5c4d066cd1c9b3b143a

--- dom/base/Document.cpp
+++ dom/base/Document.cpp
@@ -7683,6 +7683,10 @@ static void NotifyActivityChangedCallback(nsISupports* aSupports) {
 
 void Document::NotifyActivityChanged() {
   EnumerateActivityObservers(NotifyActivityChangedCallback);
+  
+  if (!IsActive()) {
+    UnlockAllWakeLocks(WakeLockType::Screen);
+  }
 }
 
 void Document::SetContainer(nsDocShell* aContainer) {
@@ -15690,6 +15694,11 @@ void Document::UpdateVisibilityState(DispatchVisibilityChange aDispatchEvent) {
     for (auto* listener : mWorkerListeners) {
       listener->OnVisible(visible);
     }
+
+    
+    if (!visible) {
+      UnlockAllWakeLocks(WakeLockType::Screen);
+    }
   }
 }
 
@@ -18339,9 +18348,13 @@ class UnlockAllWakeLockRunnable final : public Runnable {
 
 void Document::UnlockAllWakeLocks(WakeLockType aType) {
   
-  RefPtr<UnlockAllWakeLockRunnable> runnable =
-      MakeRefPtr<UnlockAllWakeLockRunnable>(aType, this);
-  NS_DispatchToMainThread(runnable);
+  if (!ActiveWakeLocks(aType).IsEmpty()) {
+    RefPtr<UnlockAllWakeLockRunnable> runnable =
+        MakeRefPtr<UnlockAllWakeLockRunnable>(aType, this);
+    nsresult rv = NS_DispatchToMainThread(runnable);
+    MOZ_DIAGNOSTIC_ASSERT(NS_SUCCEEDED(rv));
+    Unused << rv;
+  }
 }
 
 RefPtr<Document::AutomaticStorageAccessPermissionGrantPromise>
