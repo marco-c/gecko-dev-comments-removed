a1247d018b23eba485f3e0c3e4996f1d4b47f05e
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-08-23 13:45:10 +0000

    Bug 1885877 - Update xsimd to new version 7080469620c2145fbedf4ef8950406066e1ca2d6. r=sergesanspaille
    Differential Revision: https://phabricator.services.mozilla.com/D205059
    
    UltraBlame original commit: dc2c79ec121bc45d80e22abc7b35f291f62d7386

--- third_party/xsimd/include/xsimd/types/xsimd_fma3_avx_register.hpp
+++ third_party/xsimd/include/xsimd/types/xsimd_fma3_avx_register.hpp
@@ -1,13 +1,13 @@
-
-
-
-
-
-
-
-
-
-
+/***************************************************************************
+ * Copyright (c) Johan Mabille, Sylvain Corlay, Wolf Vollprecht and         *
+ * Martin Renou                                                             *
+ * Copyright (c) QuantStack                                                 *
+ * Copyright (c) Serge Guelton                                              *
+ *                                                                          *
+ * Distributed under the terms of the BSD 3-Clause License.                 *
+ *                                                                          *
+ * The full license is in the file LICENSE, distributed with this software. *
+ ****************************************************************************/
 
 #ifndef XSIMD_FMA3_AVX_REGISTER_HPP
 #define XSIMD_FMA3_AVX_REGISTER_HPP
@@ -19,17 +19,16 @@ namespace xsimd
     template <typename arch>
     struct fma3;
 
-    
-
-
-
-
+    /**
+     * @ingroup architectures
+     *
+     * AVX + FMA instructions
+     */
     template <>
     struct fma3<avx> : avx
     {
         static constexpr bool supported() noexcept { return XSIMD_WITH_FMA3_AVX; }
         static constexpr bool available() noexcept { return true; }
-        static constexpr unsigned version() noexcept { return generic::version(2, 1, 1); }
         static constexpr char const* name() noexcept { return "fma3+avx"; }
     };
 
