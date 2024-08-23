bd2ffe7d7f3fd2eb378efc84e825c4fdb46dd3f4
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-08-23 13:17:22 +0000

    Bug 1883116 - Vendor libwebrtc from 91a7beb057
    Upstream commit: https://webrtc.googlesource.com/src/+/91a7beb057fc5e9806e0044550ae172ef2d44467
        Add missing licenses for the third_party target 'jni_zero'.
    
        [Skipping presubmits because there are indentation issues that we decided to fix separately]
    
        Bug: None
        No-Presubmit: true
        Change-Id: I441864626ec44207167164f4dd6d7f09cbbb5574
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/330060
        Commit-Queue: Mirko Bonadei <mbonadeiwebrtc.org>
        Reviewed-by: Mirko Bonadei <mbonadeiwebrtc.org>
        Cr-Commit-Position: refs/heads/main{#41322}
    
    UltraBlame original commit: 74a9c19235cd03b572d8b5f4251b74befdbe2059

--- third_party/libwebrtc/tools_webrtc/libs/generate_licenses.py
+++ third_party/libwebrtc/tools_webrtc/libs/generate_licenses.py
@@ -77,7 +77,7 @@ LIB_TO_LICENSES_DICT = {
     'ooura': ['common_audio/third_party/ooura/LICENSE'],
     'spl_sqrt_floor': ['common_audio/third_party/spl_sqrt_floor/LICENSE'],
     'kotlin_stdlib': ['third_party/kotlin_stdlib/LICENSE'],
-
+    'jni_zero': ['third_party/jni_zero/LICENSE'],
     
     
     'android_deps': [],
