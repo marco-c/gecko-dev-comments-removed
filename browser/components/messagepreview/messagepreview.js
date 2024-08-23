6364a627a7e43319946bca05f94e6f25d63aa86c
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-08-23 15:07:05 +0000

    Bug 1886856 - Allow about:messagepreview to decode non-latin text r=omc-reviewers,dmose
    Differential Revision: https://phabricator.services.mozilla.com/D205344
    
    UltraBlame original commit: 66d93ead16c48ad23ffdaf2943d518bf478b6555

--- browser/components/messagepreview/messagepreview.js
+++ browser/components/messagepreview/messagepreview.js
@@ -6,13 +6,25 @@
 
 "use strict";
 
+
+
+
+function fromBinary(encoded) {
+  const binary = atob(decodeURIComponent(encoded));
+  const bytes = new Uint8Array(binary.length);
+  for (let i = 0; i < bytes.length; i++) {
+    bytes[i] = binary.charCodeAt(i);
+  }
+  return String.fromCharCode(...new Uint16Array(bytes.buffer));
+}
+
 function decodeMessageFromUrl() {
   const url = new URL(document.location.href);
 
   if (url.searchParams.has("json")) {
     const encodedMessage = url.searchParams.get("json");
 
-    return atob(encodedMessage);
+    return fromBinary(encodedMessage);
   }
   return null;
 }
