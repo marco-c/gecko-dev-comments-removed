62607606cae68cecb1bccc02cf35a9058bf2855f
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-08-23 09:07:28 +0000

    Bug 1832289 - Migrate Locale from Java to Kotlin.
    UltraBlame original commit: 1017462a41bd63868e69bfeda60931f646cf4aed

--- mobile/android/focus-android/app/src/main/java/org/mozilla/focus/locale/LocaleManager.kt
+++ mobile/android/focus-android/app/src/main/java/org/mozilla/focus/locale/LocaleManager.kt
@@ -1,112 +1,80 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+package org.mozilla.focus.locale
 
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.preference.PreferenceManager
+import org.mozilla.focus.R
+import org.mozilla.focus.generated.LocaleList
+import java.util.Locale
+import java.util.concurrent.atomic.AtomicReference
 
+/**
+ * This class manages persistence, application, and otherwise handling of
+ * user-specified locales.
+ *
+ * Of note:
+ *
+ * * It's a singleton, because its scope extends to that of the application,
+ * and definitionally all changes to the locale of the app must go through
+ * this.
+ * * It's lazy.
+ * * It relies on using the SharedPreferences file owned by the app for performance.
+ */
+class LocaleManager {
+    // These are volatile because we don't impose restrictions over which thread calls our methods.
+    @Volatile
+    private var currentLocale: Locale? = null
 
-
-
-package org.mozilla.focus.locale;
-
-import android.content.Context;
-import android.content.SharedPreferences;
-import android.content.res.Configuration;
-import android.content.res.Resources;
-import androidx.preference.PreferenceManager;
-import org.mozilla.focus.R;
-import org.mozilla.focus.generated.LocaleList;
-import java.util.Collection;
-import java.util.Locale;
-import java.util.concurrent.atomic.AtomicReference;
-
-
-
-
-
-
-
-
-
-
-
-
-
-public class LocaleManager {
-    private static String PREF_LOCALE = null;
-
-    
-    
-    private volatile Locale currentLocale;
-    private boolean systemLocaleDidChange;
-    private static final AtomicReference<LocaleManager> instance = new AtomicReference<>();
-
-    public static LocaleManager getInstance() {
-        LocaleManager localeManager = instance.get();
-        if (localeManager != null) {
-            return localeManager;
-        }
-
-        localeManager = new LocaleManager();
-        if (instance.compareAndSet(null, localeManager)) {
-            return localeManager;
-        } else {
-            return instance.get();
-        }
-    }
-
-    
-
-
-
-
-    public void updateConfiguration(Context context, Locale locale) {
-        Resources res = context.getResources();
-        Configuration config = res.getConfiguration();
-
-        
-        
-        config.locale = locale;
-
-        config.setLayoutDirection(locale);
-
-        res.updateConfiguration(config, null);
-    }
-
-    private SharedPreferences getSharedPreferences(final Context context) {
+    private fun getSharedPreferences(context: Context): SharedPreferences {
         if (PREF_LOCALE == null) {
-            PREF_LOCALE = context.getResources().getString(R.string.pref_key_locale);
+            PREF_LOCALE = context.resources.getString(R.string.pref_key_locale)
         }
 
-        return PreferenceManager.getDefaultSharedPreferences(context);
+        return PreferenceManager.getDefaultSharedPreferences(context)
     }
 
-    
-
-
-    private String getPersistedLocale(Context context) {
-        final SharedPreferences settings = getSharedPreferences(context);
-        final String locale = settings.getString(PREF_LOCALE, "");
-
-        if ("".equals(locale)) {
-            return null;
-        }
-        return locale;
+    /**
+     * @return the persisted locale in Java format: "en_US".
+     */
+    private fun getPersistedLocale(context: Context): String? {
+        val settings: SharedPreferences = getSharedPreferences(context)
+        return settings.getString(PREF_LOCALE, null)
     }
 
-    public Locale getCurrentLocale(Context context) {
+    /**
+     * @return the current locale in Java format: "en_US".
+     */
+    fun getCurrentLocale(context: Context): Locale? {
         if (currentLocale != null) {
-            return currentLocale;
+            return currentLocale
         }
 
-        final String current = getPersistedLocale(context);
-        if (current == null) {
-            return null;
-        }
-        return currentLocale = Locales.parseLocaleCode(current);
+        val current = getPersistedLocale(context) ?: return null
+        return Locales.parseLocaleCode(current).also { currentLocale = it }
     }
 
-    
+    companion object {
+        private var PREF_LOCALE: String? = null
+        val instance = AtomicReference<LocaleManager?>()
+            get() {
+                var localeManager = field.get()
+                if (localeManager != null) {
+                    return field
+                }
+                localeManager = LocaleManager()
+                return if (field.compareAndSet(null, localeManager)) {
+                    AtomicReference(localeManager)
+                } else {
+                    field
+                }
+            }
 
-
-    public static Collection<String> getPackagedLocaleTags() {
-        return LocaleList.BUNDLED_LOCALES;
+        /**
+         * Returns a list of supported locale codes
+         */
+        val packagedLocaleTags: Collection<String> = LocaleList.BUNDLED_LOCALES
     }
-
 }
