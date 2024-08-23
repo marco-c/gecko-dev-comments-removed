b1c0a2c60b64ee067d8e594239e2f8bfa130f01e
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-08-23 09:53:48 +0000

    Bug 1842886 - update the webcompat-reporter feature to un-break it after ESMification changes
    UltraBlame original commit: 402b8f27377d5de273d69bb6bc7033662c3e2bd4

--- mobile/android/android-components/components/feature/webcompat-reporter/src/main/assets/extensions/webcompat-reporter/experimentalAPIs/aboutConfigPrefs.js
+++ mobile/android/android-components/components/feature/webcompat-reporter/src/main/assets/extensions/webcompat-reporter/experimentalAPIs/aboutConfigPrefs.js
@@ -6,8 +6,6 @@
 
 
 
-var { Services } = ChromeUtils.import("resource://gre/modules/Services.jsm");
-
 this.aboutConfigPrefs = class extends ExtensionAPI {
   getAPI(context) {
     const EventManager = ExtensionCommon.EventManager;
