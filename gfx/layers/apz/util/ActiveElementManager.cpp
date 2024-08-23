128de311bc25b6b3d411f3feaf615bbabf1757c4
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-08-23 14:37:45 +0000

    Bug 1806400 - Ensure to invoke DelayedClearElementActivation::MarkSingleTapProcessed even if DelayedClearElementActivation hasn't yet been created. r=dlrobertson
    Differential Revision: https://phabricator.services.mozilla.com/D199028
    
    UltraBlame original commit: 4acd237193a7a11478095248f3d074e887382250

--- gfx/layers/apz/util/ActiveElementManager.cpp
+++ gfx/layers/apz/util/ActiveElementManager.cpp
@@ -137,7 +137,10 @@ void DelayedClearElementActivation::ClearGlobalActiveContent() {
 NS_IMPL_ISUPPORTS(DelayedClearElementActivation, nsITimerCallback, nsINamed)
 
 ActiveElementManager::ActiveElementManager()
-    : mCanBePan(false), mCanBePanSet(false), mSetActiveTask(nullptr) {}
+    : mCanBePan(false),
+      mCanBePanSet(false),
+      mSingleTapBeforeActivation(false),
+      mSetActiveTask(nullptr) {}
 
 ActiveElementManager::~ActiveElementManager() = default;
 
@@ -194,6 +197,9 @@ void ActiveElementManager::TriggerElementActivation() {
     SetActive(mTarget);
 
     if (mDelayedClearElementActivation) {
+      if (mSingleTapBeforeActivation) {
+        mDelayedClearElementActivation->MarkSingleTapProcessed();
+      }
       mDelayedClearElementActivation->StartTimer();
     }
   } else {
@@ -265,6 +271,9 @@ bool ActiveElementManager::MaybeChangeActiveState(bool aWasClick) {
 
 void ActiveElementManager::ProcessSingleTap() {
   if (!mDelayedClearElementActivation) {
+    
+    
+    mSingleTapBeforeActivation = true;
     return;
   }
 
@@ -315,6 +324,7 @@ void ActiveElementManager::ResetTouchBlockState() {
   mTarget = nullptr;
   mCanBePanSet = false;
   mTouchEndState.clear();
+  mSingleTapBeforeActivation = false;
 }
 
 void ActiveElementManager::SetActiveTask(
