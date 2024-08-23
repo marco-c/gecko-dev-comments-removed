9910cba72b2483a2cd9feb24a5be1c4c9506989c
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-08-23 09:07:23 +0000

    Bug 1832289 - Rename .java to .kt
    UltraBlame original commit: d357a8f360f6cbdf3cdfb690271ca76c5e4bb4c6

--- mobile/android/focus-android/app/src/main/java/org/mozilla/focus/locale/Locales.kt
+++ mobile/android/focus-android/app/src/main/java/org/mozilla/focus/locale/Locales.kt
@@ -1,6 +1,6 @@
-
-
-
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 package org.mozilla.focus.locale;
 
@@ -9,29 +9,29 @@ import android.content.res.Configuration;
 import android.content.res.Resources;
 import java.util.Locale;
 
-
-
-
-
-
-
-
+/**
+ * This is a helper class to do typical locale switching operations without
+ * hitting StrictMode errors or adding boilerplate to common activity
+ * subclasses.
+ *
+ * Inherit from <code>LocaleAwareFragmentActivity</code> or <code>LocaleAwareActivity</code>.
+ */
 public class Locales {
 
-    
-
-
-
-
-
-
-
-
+    /**
+     * Sometimes we want just the language for a locale, not the entire language
+     * tag. But Java's .getLanguage method is wrong.
+     *
+     * This method is equivalent to the first part of
+     * {@link Locales#getLanguageTag(Locale)}.
+     *
+     * @return a language string, such as "he" for the Hebrew locales.
+     */
     public static String getLanguage(final Locale locale) {
-        
+        // Can, but should never be, an empty string.
         final String language = locale.getLanguage();
 
-        
+        // Modernize certain language codes.
         if (language.equals("iw")) {
             return "he";
         }
@@ -47,21 +47,21 @@ public class Locales {
         return language;
     }
 
-    
-
-
-
-
-
-
-
-
+    /**
+     * Gecko uses locale codes like "es-ES", whereas a Java {@link Locale}
+     * stringifies as "es_ES".
+     *
+     * This method approximates the Java 7 method
+     * <code>Locale#toLanguageTag()</code>.
+     *
+     * @return a locale string suitable for passing to Gecko.
+     */
     public static String getLanguageTag(final Locale locale) {
-        
-        
+        // If this were Java 7:
+        // return locale.toLanguageTag();
 
         final String language = getLanguage(locale);
-        final String country = locale.getCountry(); 
+        final String country = locale.getCountry(); // Can be an empty string.
         if (country.equals("")) {
             return language;
         }
@@ -80,9 +80,9 @@ public class Locales {
         return new Locale(localeCode);
     }
 
-    
-
-
+    /**
+     * Get a Resources instance with the currently selected locale applied.
+     */
     public static Resources getLocalizedResources(Context context) {
         final Resources currentResources = context.getResources();
 
