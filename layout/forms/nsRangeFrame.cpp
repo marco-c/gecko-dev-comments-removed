f838ae66ce0fadd01b5f9300d780d6b496add788
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-08-23 14:50:00 +0000

    Bug 1887539 - Fix nsRangeFrame::UpdateForValueChange after bug 1740580. r=dholbert
    In bug 1740580 I updated these functions to take the content size, but
    forgot to update two existing callers.
    
    Differential Revision: https://phabricator.services.mozilla.com/D205549
    
    UltraBlame original commit: 303bd35a1f94a36045dbc3f000f2b2a3c25e9a18

--- layout/forms/nsRangeFrame.cpp
+++ layout/forms/nsRangeFrame.cpp
@@ -330,7 +330,7 @@ Decimal nsRangeFrame::GetValueAtEventPoint(WidgetGUIEvent* aEvent) {
         ->GetValueAsDecimal();
   }
 
-  nsRect rangeContentRect = GetContentRectRelativeToSelf();
+  const nsRect rangeContentRect = GetContentRectRelativeToSelf();
   nsSize thumbSize;
 
   if (IsThemed()) {
@@ -397,11 +397,12 @@ void nsRangeFrame::UpdateForValueChange() {
   if (!rangeProgressFrame && !thumbFrame) {
     return;  
   }
+  const nsSize contentBoxSize = GetContentRect().Size();
   if (rangeProgressFrame) {
-    DoUpdateRangeProgressFrame(rangeProgressFrame, GetSize());
+    DoUpdateRangeProgressFrame(rangeProgressFrame, contentBoxSize);
   }
   if (thumbFrame) {
-    DoUpdateThumbPosition(thumbFrame, GetSize());
+    DoUpdateThumbPosition(thumbFrame, contentBoxSize);
   }
   if (IsThemed()) {
     
