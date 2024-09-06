c82be1c32877addd8009c6865ca958623a6dc260
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-06 12:51:51 +0000

    Bug 1895502 - fix toolkit/mozapps/extensions tests with https-first r=zombie
    Differential Revision: https://phabricator.services.mozilla.com/D209679
    
    UltraBlame original commit: ed7b6f65910afcd687ef5aaa25605666805796bb

--- toolkit/mozapps/extensions/test/xpinstall/browser_required_useractivation.js
+++ toolkit/mozapps/extensions/test/xpinstall/browser_required_useractivation.js
@@ -17,6 +17,7 @@ async function runTestCase(spawnArgs, spawnFn, { expectInstall, clickLink }) {
     set: [
       
       ["xpinstall.userActivation.required", true],
+      ["dom.security.https_first", false],
     ],
   });
   await BrowserTestUtils.withNewTab(TESTROOT, async browser => {
