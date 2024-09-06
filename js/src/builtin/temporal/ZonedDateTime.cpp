657b7641f774712f78df1a95066c5e1d5023e395
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-06 09:28:45 +0000

    Bug 1874684 - Part 37: Fix unified compilation. r=allstarschh
    Fix unified compilation when `FILES_PER_UNIFIED_FILE` is set to a larger value.
    
    Depends on D204400
    
    Differential Revision: https://phabricator.services.mozilla.com/D204401
    
    UltraBlame original commit: 09c240f8a03913a5e493c2102104a8c598ae04a5

--- js/src/builtin/temporal/ZonedDateTime.cpp
+++ js/src/builtin/temporal/ZonedDateTime.cpp
@@ -778,8 +778,8 @@ static bool AddZonedDateTime(JSContext* cx, const Instant& epochNanoseconds,
   
   PlainDate addedDate;
   if (maybeOptions) {
-    if (!CalendarDateAdd(cx, calendar, datePart, dateDuration, maybeOptions,
-                         &addedDate)) {
+    if (!temporal::CalendarDateAdd(cx, calendar, datePart, dateDuration,
+                                   maybeOptions, &addedDate)) {
       return false;
     }
   } else {
