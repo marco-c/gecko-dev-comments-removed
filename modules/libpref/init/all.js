f60db5d43efc18f521deb5314dfa742265a60058
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-06 20:17:50 +0000

    Bug 1899366 - Flip prefs to enable new text and layout + theme menus by default. r=accessibility-frontend-reviewers,cmkm,sfoster,fchasen
    Differential Revision: https://phabricator.services.mozilla.com/D213632
    
    UltraBlame original commit: 2862aed5ce2b42809c5fd2af968614f2da2e4285

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -3679,7 +3679,7 @@ pref("reader.content_width", 3);
 pref("reader.line_height", 4);
 
 
-pref("reader.improved_text_menu.enabled", false);
+pref("reader.improved_text_menu.enabled", true);
 
 
 pref("reader.character_spacing", 0);
@@ -3699,7 +3699,7 @@ pref("reader.color_scheme", "auto");
 pref("reader.color_scheme.values", "[\"auto\",\"light\",\"dark\",\"sepia\",\"contrast\",\"gray\"]");
 
 
-pref("reader.colors_menu.enabled", false);
+pref("reader.colors_menu.enabled", true);
 
 
 pref("reader.custom_colors.foreground", "");
