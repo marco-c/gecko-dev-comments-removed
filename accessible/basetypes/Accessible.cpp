db8cc586c30e25c6d53d64ae94c5c97a2858c815
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-06 12:38:32 +0000

    Bug 1887789 part 1: Fixes to Accessible::LandmarkRole. r=morgan
    1. The ARIA role should take precedence over the HTML tag.
    2. The ARIA region role should only be conditionally treated as a landmark. Use the Gecko role to determine this.
    3. Change some other cases to use the Gecko role instead of directly checking the name for consistency and to avoid duplicated logic.
    
    These fixes are tested in the next patch.
    
    Differential Revision: https://phabricator.services.mozilla.com/D209099
    
    UltraBlame original commit: 670a7e6fd98d48ea7e30c09db07390a6d589bb19

--- accessible/basetypes/Accessible.cpp
+++ accessible/basetypes/Accessible.cpp
@@ -508,6 +508,20 @@ const Accessible* Accessible::ActionAncestor() const {
 }
 
 nsStaticAtom* Accessible::LandmarkRole() const {
+  
+  
+  
+  if (const nsRoleMapEntry* roleMapEntry = ARIARoleMap()) {
+    
+    if (roleMapEntry->Is(nsGkAtoms::region)) {
+      if (Role() == roles::REGION) {
+        return nsGkAtoms::region;
+      }
+    } else if (roleMapEntry->IsOfType(eLandmark)) {
+      return roleMapEntry->roleAtom;
+    }
+  }
+
   nsAtom* tagName = TagName();
   if (!tagName) {
     
@@ -539,13 +553,13 @@ nsStaticAtom* Accessible::LandmarkRole() const {
   }
 
   if (tagName == nsGkAtoms::section) {
-    if (!NameIsEmpty()) {
+    if (Role() == roles::REGION) {
       return nsGkAtoms::region;
     }
   }
 
   if (tagName == nsGkAtoms::form) {
-    if (!NameIsEmpty()) {
+    if (Role() == roles::FORM_LANDMARK) {
       return nsGkAtoms::form;
     }
   }
@@ -554,10 +568,7 @@ nsStaticAtom* Accessible::LandmarkRole() const {
     return nsGkAtoms::search;
   }
 
-  const nsRoleMapEntry* roleMapEntry = ARIARoleMap();
-  return roleMapEntry && roleMapEntry->IsOfType(eLandmark)
-             ? roleMapEntry->roleAtom
-             : nullptr;
+  return nullptr;
 }
 
 nsStaticAtom* Accessible::ComputedARIARole() const {
