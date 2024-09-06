b4902112c00a948ec91c00e36cd18f50afdaf8fa
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-06 10:09:47 +0000

    Bug 1884962 - report kProtectionHDCP for the protection mask. r=media-playback-reviewers,padenot
    According to what Widevine told us, `kProtectionNone` will cause playback
    to stop if HDCP_V1 is required. Therefore, we would report HDCP protection
    if there is no potential capturing.
    
    Differential Revision: https://phabricator.services.mozilla.com/D207483
    
    UltraBlame original commit: b9cbb65614b0c078d37df58db47dfbf246bd6683

--- dom/media/gmp/ChromiumCDMProxy.cpp
+++ dom/media/gmp/ChromiumCDMProxy.cpp
@@ -382,12 +382,18 @@ void ChromiumCDMProxy::NotifyOutputProtectionStatus(
   }
 
   uint32_t linkMask{};
-  uint32_t protectionMask{};  
+  uint32_t protectionMask{};
   if (aCheckStatus == OutputProtectionCheckStatus::CheckSuccessful &&
       aCaptureStatus == OutputProtectionCaptureStatus::CapturePossilbe) {
     
     
     linkMask |= cdm::OutputLinkTypes::kLinkTypeNetwork;
+  }
+  
+  
+  if (linkMask == cdm::OutputLinkTypes::kLinkTypeNone &&
+      StaticPrefs::media_widevine_hdcp_protection_mask()) {
+    protectionMask = cdm::OutputProtectionMethods::kProtectionHDCP;
   }
   mGMPThread->Dispatch(NewRunnableMethod<bool, uint32_t, uint32_t>(
       "gmp::ChromiumCDMParent::NotifyOutputProtectionStatus", cdm,
