f87801a0ff1179357b450862f8745cb0d0df4300
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-06 22:18:16 +0000

    Bug 1906017 - Display non-Sponsored Suggestions at the bottom as default r=adw
    Differential Revision: https://phabricator.services.mozilla.com/D215706
    
    UltraBlame original commit: e8ba4ca729d222b92951b8c027191d7f2ca5c9e4

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -508,7 +508,7 @@ pref("browser.urlbar.quicksuggest.showOnboardingDialogAfterNRestarts", 0);
 
 
 pref("browser.urlbar.quicksuggest.sponsoredIndex", 0);
-pref("browser.urlbar.quicksuggest.nonSponsoredIndex", 0);
+pref("browser.urlbar.quicksuggest.nonSponsoredIndex", -1);
 
 
 
