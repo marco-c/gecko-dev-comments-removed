b8a396bfc9f7758c4ce56ce337d3de3c86bf3c48
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-06 12:10:34 +0000

    Backed out 2 changesets (bug 1892947) due to critical failure with clear on shutdown CLOSED TREE
    Backed out changeset 5c4c36caa85d (bug 1892947)
    Backed out changeset eeabe25b2ac3 (bug 1892947)
    
    UltraBlame original commit: b034eb8252983ca8aa3bbe76f995573f761bd900

--- browser/components/preferences/tests/browser_privacy_syncDataClearing.js
+++ browser/components/preferences/tests/browser_privacy_syncDataClearing.js
@@ -6,12 +6,6 @@
 
 
 
-add_setup(async function () {
-  await SpecialPowers.pushPrefEnv({
-    set: [["privacy.sanitize.useOldClearHistoryDialog", true]],
-  });
-});
-
 add_task(async function test_syncWithoutCustomPrefs() {
   await openPreferencesViaOpenPreferencesAPI("panePrivacy", {
     leaveOpen: true,
