6449e4f5f8c35b47cbeee6df91541d54e3a94269
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-06 12:44:39 +0000

    Bug 1894928 - Don't use template strings for logging console errors in the search service. r=search-reviewers,mcheang
    This allows better output on the browser console and provides full stacks for exceptions.
    
    Differential Revision: https://phabricator.services.mozilla.com/D209383
    
    UltraBlame original commit: c9a9cc50de3c4f965de1ee36c237c236656d9297

--- toolkit/components/search/tests/xpcshell/test_webextensions_startup_duplicate.js
+++ toolkit/components/search/tests/xpcshell/test_webextensions_startup_duplicate.js
@@ -25,11 +25,7 @@ add_setup(async function () {
 
 add_task(async function test_install_duplicate_engine_startup() {
   let name = "Plain";
-  let id = "plain@tests.mozilla.org";
-  consoleAllowList.push(
-    `#createAndAddAddonEngine failed for ${id}`,
-    `An engine called ${name} already exists`
-  );
+  consoleAllowList.push("#createAndAddAddonEngine failed for");
   
   
   let extensionInfo = {
