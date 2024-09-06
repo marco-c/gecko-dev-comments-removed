dcf7f0f5b6ff32f87e7a9322b136b86ac2e746ed
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-06 12:15:26 +0000

    Bug 1879631 part 3 - add tests for clipboard content analysis r=dlp-reviewers,handyman
    Differential Revision: https://phabricator.services.mozilla.com/D207194
    
    UltraBlame original commit: 151f8f4a1096495c3b2b55ba39877b18a10ff5f6

--- toolkit/components/contentanalysis/tests/browser/browser_print_content_analysis.js
+++ toolkit/components/contentanalysis/tests/browser/browser_print_content_analysis.js
@@ -12,73 +12,7 @@ const PSSVC = Cc["@mozilla.org/gfx/printsettings-service;1"].getService(
   Ci.nsIPrintSettingsService
 );
 
-let mockCA = {
-  isActive: true,
-  mightBeActive: true,
-  errorValue: undefined,
-
-  setupForTest(shouldAllowRequest) {
-    this.shouldAllowRequest = shouldAllowRequest;
-    this.errorValue = undefined;
-    this.calls = [];
-  },
-
-  setupForTestWithError(errorValue) {
-    this.errorValue = errorValue;
-    this.calls = [];
-  },
-
-  clearCalls() {
-    this.calls = [];
-  },
-
-  getAction() {
-    if (this.shouldAllowRequest === undefined) {
-      this.shouldAllowRequest = true;
-    }
-    return this.shouldAllowRequest
-      ? Ci.nsIContentAnalysisResponse.eAllow
-      : Ci.nsIContentAnalysisResponse.eBlock;
-  },
-
-  
-  async analyzeContentRequest(request, _autoAcknowledge) {
-    info(
-      "Mock ContentAnalysis service: analyzeContentRequest, this.shouldAllowRequest=" +
-        this.shouldAllowRequest +
-        ", this.errorValue=" +
-        this.errorValue
-    );
-    this.calls.push(request);
-    if (this.errorValue) {
-      throw this.errorValue;
-    }
-    
-    await new Promise(res => setTimeout(res, 0));
-    return makeContentAnalysisResponse(this.getAction(), request.requestToken);
-  },
-
-  analyzeContentRequestCallback(request, autoAcknowledge, callback) {
-    info(
-      "Mock ContentAnalysis service: analyzeContentRequestCallback, this.shouldAllowRequest=" +
-        this.shouldAllowRequest +
-        ", this.errorValue=" +
-        this.errorValue
-    );
-    this.calls.push(request);
-    if (this.errorValue) {
-      throw this.errorValue;
-    }
-    let response = makeContentAnalysisResponse(
-      this.getAction(),
-      request.requestToken
-    );
-    
-    setTimeout(() => {
-      callback.contentResult(response);
-    }, 0);
-  },
-};
+let mockCA = makeMockContentAnalysis();
 
 add_setup(async function test_setup() {
   mockCA = mockContentAnalysisService(mockCA);
