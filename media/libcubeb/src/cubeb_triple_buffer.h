cfbdcbf5a1dfd47f90018090a7ab28c4ef22ac25
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-06 12:55:25 +0000

    Bug 1895471 - Uplift cubeb AAudio timing fix for reused streams.  r=cubeb-reviewers,pehrsons
    Differential Revision: https://phabricator.services.mozilla.com/D209774
    
    UltraBlame original commit: f262bae431fad18dcefd788a10d8586fad563c22

--- media/libcubeb/src/cubeb_triple_buffer.h
+++ media/libcubeb/src/cubeb_triple_buffer.h
@@ -42,6 +42,13 @@ public:
   {
     return (shared_state.load(std::memory_order_relaxed) & BACK_DIRTY_BIT) != 0;
   }
+  
+  void invalidate()
+  {
+    shared_state.store(0, std::memory_order_release);
+    input_idx = 1;
+    output_idx = 2;
+  }
 
 private:
   
