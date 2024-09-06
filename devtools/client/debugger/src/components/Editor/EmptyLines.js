b2d28898be6824293d235f4203c05cacea7cb338
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-06 23:11:25 +0000

    Bug 1906496 - [devtools] Avoid throwing from debugger components when there is no selected source. r=devtools-reviewers,bomsy
    Differential Revision: https://phabricator.services.mozilla.com/D216146
    
    UltraBlame original commit: 8bb77f99b1a3897f3a02517eaec0ffc93f9985b4

--- devtools/client/debugger/src/components/Editor/EmptyLines.js
+++ devtools/client/debugger/src/components/Editor/EmptyLines.js
@@ -41,6 +41,9 @@ class EmptyLines extends Component {
 
   shouldComponentUpdate(nextProps) {
     const { breakableLines, selectedSource } = this.props;
+    if (!selectedSource) {
+      return false;
+    }
     return (
       
       
@@ -52,6 +55,9 @@ class EmptyLines extends Component {
 
   disableEmptyLines() {
     const { breakableLines, selectedSource, editor } = this.props;
+    if (!selectedSource) {
+      return;
+    }
 
     const { codeMirror } = editor;
     const isSourceWasm = isWasm(selectedSource.id);
@@ -78,7 +84,7 @@ class EmptyLines extends Component {
 const mapStateToProps = state => {
   const selectedSource = getSelectedSource(state);
   if (!selectedSource) {
-    throw new Error("no selectedSource");
+    return {};
   }
   const breakableLines = getSelectedBreakableLines(state);
 
