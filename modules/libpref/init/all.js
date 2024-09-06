143eea9f8f69a4471108de595fc86940f87222ef
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-06 15:20:42 +0000

    Bug 1895787 - Re-enable platform processing on macOS. r=padenot
    Differential Revision: https://phabricator.services.mozilla.com/D211048
    
    UltraBlame original commit: ea162d71f17803e6c1405e40898ea167ad0533b7

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -349,8 +349,7 @@ pref("media.videocontrols.keyboard-tab-to-all-controls", true);
   pref("media.peerconnection.dtls.version.max", 772);
 
 #if defined(XP_MACOSX)
-  
-  pref("media.getusermedia.audio.processing.platform.enabled", false);
+  pref("media.getusermedia.audio.processing.platform.enabled", true);
 #else
   pref("media.getusermedia.audio.processing.platform.enabled", false);
 #endif
