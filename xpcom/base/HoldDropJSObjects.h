5bd40d07c8ad80e3447352759af08d0b515813e3
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-06 21:34:59 +0000

    Bug 1904810 - Add an API to drop a JS holder that has already been cleared r=mccr8
    This adds an optional second argument to DropJSObjects to allow skipping
    clearing of JS references in cases where that has already happened. In debug
    builds it asserts that such references have already been cleared.
    
    This is then used in CallbackObject::Reset.
    
    Differential Revision: https://phabricator.services.mozilla.com/D215493
    
    UltraBlame original commit: 316531363a0531815d23ee44f07956887c9e9d3a

--- xpcom/base/HoldDropJSObjects.h
+++ xpcom/base/HoldDropJSObjects.h
@@ -21,13 +21,17 @@ class Zone;
 
 
 namespace mozilla {
+
+enum class ShouldClearJSRefs : bool { Clear = true, AlreadyCleared = false };
+
 namespace cyclecollector {
 
 void HoldJSObjectsImpl(void* aHolder, nsScriptObjectTracer* aTracer,
                        JS::Zone* aZone = nullptr);
 void HoldJSObjectsImpl(nsISupports* aHolder);
-void DropJSObjectsImpl(void* aHolder);
-void DropJSObjectsImpl(nsISupports* aHolder);
+void DropJSObjectsImpl(void* aHolder,
+                       ShouldClearJSRefs aClearRefs = ShouldClearJSRefs::Clear);
+void DropJSObjectsImpl(nsISupports* aHolder, ShouldClearJSRefs aClearRefs);
 
 }  
 
@@ -38,7 +42,9 @@ struct HoldDropJSObjectsHelper {
     cyclecollector::HoldJSObjectsImpl(aHolder,
                                       NS_CYCLE_COLLECTION_PARTICIPANT(T));
   }
-  static void Drop(T* aHolder) { cyclecollector::DropJSObjectsImpl(aHolder); }
+  static void Drop(T* aHolder, ShouldClearJSRefs aClearRefs) {
+    cyclecollector::DropJSObjectsImpl(aHolder, aClearRefs);
+  }
 };
 
 template <class T>
@@ -46,10 +52,20 @@ struct HoldDropJSObjectsHelper<T, true> {
   static void Hold(T* aHolder) {
     cyclecollector::HoldJSObjectsImpl(ToSupports(aHolder));
   }
-  static void Drop(T* aHolder) {
-    cyclecollector::DropJSObjectsImpl(ToSupports(aHolder));
+  static void Drop(T* aHolder, ShouldClearJSRefs aClearRefs) {
+    cyclecollector::DropJSObjectsImpl(ToSupports(aHolder), aClearRefs);
   }
 };
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -71,12 +87,13 @@ void HoldJSObjects(T* aHolder) {
 }
 
 template <class T>
-void DropJSObjects(T* aHolder) {
+void DropJSObjects(T* aHolder,
+                   ShouldClearJSRefs aClearRefs = ShouldClearJSRefs::Clear) {
   static_assert(!std::is_base_of<nsCycleCollectionParticipant, T>::value,
                 "Don't call this on the CC participant but on the object that "
                 "it's for (in an Unlink implementation it's usually stored in "
                 "a variable named 'tmp').");
-  HoldDropJSObjectsHelper<T>::Drop(aHolder);
+  HoldDropJSObjectsHelper<T>::Drop(aHolder, aClearRefs);
 }
 
 }  
