b5ffe1d60eaba1165eef5ae7705ffccbe8a2d20d
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-06 12:29:36 +0000

    Bug 1895017 - Relax serialization check for LinkData::symbolicLinks. r=bvisness
    Differential Revision: https://phabricator.services.mozilla.com/D209465
    
    UltraBlame original commit: 1b37c04d31f162da56058c27811099fcbdee231a

--- js/src/wasm/WasmSerialize.cpp
+++ js/src/wasm/WasmSerialize.cpp
@@ -957,7 +957,10 @@ CoderResult CodeSymbolicLinkArray(
 template <CoderMode mode>
 CoderResult CodeLinkData(Coder<mode>& coder,
                          CoderArg<mode, wasm::LinkData> item) {
-  WASM_VERIFY_SERIALIZATION_FOR_SIZE(wasm::LinkData, 9048);
+  
+  
+  WASM_VERIFY_SERIALIZATION_FOR_SIZE(
+      wasm::LinkData, 48 + sizeof(wasm::LinkData::SymbolicLinkArray));
   if constexpr (mode == MODE_ENCODE) {
     MOZ_ASSERT(item->tier == Tier::Serialized);
   }
