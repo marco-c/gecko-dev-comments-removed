e1beebd7d567c84f029c0a66f2b83e718a76d0a1
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-06 17:47:19 +0000

    Bug 1901570 - Rename workers to use the .worker.(m)js pattern for ESLint (devtools/). r=devtools-reviewers,nchevobbe
    Differential Revision: https://phabricator.services.mozilla.com/D213091
    
    UltraBlame original commit: 22bcfc25fc0d6f6cf4843ff63a0e596abf4411db

--- devtools/client/aboutdebugging/test/browser/browser_aboutdebugging_serviceworker_unregister.js
+++ devtools/client/aboutdebugging/test/browser/browser_aboutdebugging_serviceworker_unregister.js
@@ -10,7 +10,7 @@ Services.scriptloader.loadSubScript(
 );
 
 const SW_TAB_URL = URL_ROOT_SSL + "resources/service-workers/empty-sw.html";
-const SW_URL = URL_ROOT_SSL + "resources/service-workers/empty-sw.js";
+const SW_URL = URL_ROOT_SSL + "resources/service-workers/empty-sw.worker.js";
 
 
 
