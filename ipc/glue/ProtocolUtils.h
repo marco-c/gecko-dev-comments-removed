fc39821bd2adce7c66f258d1bcb507245564a2c3
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-06 14:08:16 +0000

    Bug 1895358 - Part 2: Handle ManagedContainer membership more centrally, r=ipc-reviewers,mccr8
    This patch adjusts ManagedContainer to have a common base class, and exposes
    methods for interacting with this base class from generic code on IProtocol.
    This avoids the need for some specialized methods which were previously
    required in order to manipulate the managed lists which allows the ordering to
    be more precisely controlled in generic actor lifecycle methods.
    
    Differential Revision: https://phabricator.services.mozilla.com/D209855
    
    UltraBlame original commit: 24ecfabfc3308ae06a6095f4ea85a9104592640e

--- ipc/glue/ProtocolUtils.h
+++ ipc/glue/ProtocolUtils.h
@@ -89,6 +89,7 @@ class nsISerialEventTarget;
 
 namespace mozilla {
 class SchedulerGroup;
+class UntypedManagedContainer;
 
 namespace dom {
 class ContentParent;
@@ -199,6 +200,8 @@ class IProtocol : public HasResultCodes {
   int32_t Id() const { return mId; }
   IRefCountedProtocol* Manager() const { return mManager; }
 
+  uint32_t AllManagedActorsCount() const;
+
   ActorLifecycleProxy* GetLifecycleProxy() { return mLifecycleProxy; }
   WeakActorLifecycleProxy* GetWeakLifecycleProxy();
 
@@ -213,7 +216,6 @@ class IProtocol : public HasResultCodes {
   }
 
   
-  virtual void RemoveManagee(int32_t, IProtocol*) = 0;
   virtual void DeallocManagee(int32_t, IProtocol*) = 0;
 
   Maybe<IProtocol*> ReadActor(IPC::MessageReader* aReader, bool aNullable,
@@ -271,8 +273,6 @@ class IProtocol : public HasResultCodes {
     }
   }
 
-  virtual uint32_t AllManagedActorsCount() const = 0;
-
   
   already_AddRefed<ActorLifecycleProxy> ActorConnected();
 
@@ -280,18 +280,15 @@ class IProtocol : public HasResultCodes {
   void ActorDisconnected(ActorDestroyReason aWhy);
 
   
-  
-  void SetDoomed() {
-    MOZ_ASSERT(mLinkStatus == LinkStatus::Connected ||
-                   mLinkStatus == LinkStatus::Doomed,
-               "Invalid link status for SetDoomed");
-    mLinkStatus = LinkStatus::Doomed;
-  }
-  virtual void DoomSubtree() = 0;
+  virtual Span<const ProtocolId> ManagedProtocolIds() const = 0;
 
   
   
-  virtual IProtocol* PeekManagedActor() = 0;
+  
+  virtual UntypedManagedContainer* GetManagedActors(ProtocolId aProtocol) = 0;
+  const UntypedManagedContainer* GetManagedActors(ProtocolId aProtocol) const {
+    return const_cast<IProtocol*>(this)->GetManagedActors(aProtocol);
+  }
 
   
   
@@ -317,9 +314,15 @@ class IProtocol : public HasResultCodes {
   void WarnMessageDiscarded(IPC::Message*) {}
 #endif
 
+  void DoomSubtree();
+
+  
+  
+  IProtocol* PeekManagedActor() const;
+
   int32_t mId;
-  ProtocolId mProtocolId;
-  Side mSide;
+  const ProtocolId mProtocolId;
+  const Side mSide;
   LinkStatus mLinkStatus;
   ActorLifecycleProxy* mLifecycleProxy;
   RefPtr<IRefCountedProtocol> mManager;
@@ -742,28 +745,37 @@ class IPDLResolverInner final {
 
 }  
 
-template <typename Protocol>
-class ManagedContainer {
- public:
-  using iterator = typename nsTArray<Protocol*>::const_iterator;
 
-  iterator begin() const { return mArray.begin(); }
-  iterator end() const { return mArray.end(); }
-  iterator cbegin() const { return begin(); }
-  iterator cend() const { return end(); }
+
+
+class UntypedManagedContainer {
+ public:
+  using iterator = nsTArray<mozilla::ipc::IProtocol*>::const_iterator;
+
+  iterator begin() const { return mArray.cbegin(); }
+  iterator end() const { return mArray.cend(); }
 
   bool IsEmpty() const { return mArray.IsEmpty(); }
   uint32_t Count() const { return mArray.Length(); }
 
-  void ToArray(nsTArray<Protocol*>& aArray) const {
-    aArray.AppendElements(mArray);
+ protected:
+  explicit UntypedManagedContainer(mozilla::ipc::ProtocolId aProtocolId)
+#ifdef DEBUG
+      : mProtocolId(aProtocolId)
+#endif
+  {
   }
 
-  bool EnsureRemoved(Protocol* aElement) {
+ private:
+  friend class mozilla::ipc::IProtocol;
+
+  bool EnsureRemoved(mozilla::ipc::IProtocol* aElement) {
     return mArray.RemoveElementSorted(aElement);
   }
 
-  void Insert(Protocol* aElement) {
+  void Insert(mozilla::ipc::IProtocol* aElement) {
+    MOZ_ASSERT(aElement->GetProtocolId() == mProtocolId,
+               "ManagedContainer can only contain a single protocol");
     
     
     size_t index = mArray.IndexOfFirstElementGt(aElement);
@@ -772,12 +784,59 @@ class ManagedContainer {
     }
   }
 
-  Protocol* Peek() { return mArray.IsEmpty() ? nullptr : mArray.LastElement(); }
+  nsTArray<mozilla::ipc::IProtocol*> mArray;
+#ifdef DEBUG
+  mozilla::ipc::ProtocolId mProtocolId;
+#endif
+};
 
-  void Clear() { mArray.Clear(); }
+template <typename Protocol>
+class ManagedContainer : public UntypedManagedContainer {
+ public:
+  ManagedContainer() : UntypedManagedContainer(Protocol::kProtocolId) {}
 
- private:
-  nsTArray<Protocol*> mArray;
+  
+  
+  class iterator {
+   public:
+    using value_type = Protocol*;
+    using difference_type = ptrdiff_t;
+    using pointer = value_type*;
+    using reference = value_type;
+    using iterator_category = std::input_iterator_tag;
+
+   private:
+    friend class ManagedContainer;
+    explicit iterator(const UntypedManagedContainer::iterator& aIter)
+        : mIter(aIter) {}
+    UntypedManagedContainer::iterator mIter;
+
+   public:
+    iterator() = default;
+
+    bool operator==(const iterator& aRhs) const { return mIter == aRhs.mIter; }
+    bool operator!=(const iterator& aRhs) const { return mIter != aRhs.mIter; }
+
+    
+    
+    reference operator*() const { return static_cast<value_type>(*mIter); }
+
+    iterator& operator++() {
+      ++mIter;
+      return *this;
+    }
+    iterator operator++(int) { return iterator{mIter++}; }
+  };
+
+  iterator begin() const { return iterator{UntypedManagedContainer::begin()}; }
+  iterator end() const { return iterator{UntypedManagedContainer::end()}; }
+
+  void ToArray(nsTArray<Protocol*>& aArray) const {
+    aArray.SetCapacity(Count());
+    for (Protocol* p : *this) {
+      aArray.AppendElement(p);
+    }
+  }
 };
 
 template <typename Protocol>
@@ -787,7 +846,7 @@ Protocol* LoneManagedOrNullAsserts(
     return nullptr;
   }
   MOZ_ASSERT(aManagees.Count() == 1);
-  return *aManagees.cbegin();
+  return *aManagees.begin();
 }
 
 template <typename Protocol>
@@ -795,7 +854,7 @@ Protocol* SingleManagedOrNull(const ManagedContainer<Protocol>& aManagees) {
   if (aManagees.Count() != 1) {
     return nullptr;
   }
-  return *aManagees.cbegin();
+  return *aManagees.begin();
 }
 
 }  
