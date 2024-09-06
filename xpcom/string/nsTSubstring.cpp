4b7a33a2eea1944c4569c3a93248b06df4a1d56f
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-06 11:15:42 +0000

    Bug 1893668 - Make string stats more obviously debug-only. r=xpcom-reviewers,mccr8
    Also, make them relaxed atomic. Sequentially consistent atomics might
    help paper concurrency issues in debug builds, and there's really no
    good reason these counts need to be sequentially consistent.
    
    Differential Revision: https://phabricator.services.mozilla.com/D208759
    
    UltraBlame original commit: c77d9ee9ea34985577302bee9499d2ab94e62488

--- xpcom/string/nsTSubstring.cpp
+++ xpcom/string/nsTSubstring.cpp
@@ -16,12 +16,7 @@
 #include "nsISupports.h"
 #include "nsString.h"
 #include "nsTArray.h"
-
-#ifdef DEBUG
-#  include "nsStringStats.h"
-#else
-#  define STRING_STAT_INCREMENT(_s)
-#endif
+#include "nsStringStats.h"
 
 
 
