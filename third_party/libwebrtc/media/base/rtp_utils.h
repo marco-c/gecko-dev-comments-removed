92f7567c6632f4b286d333860141d687e0342dc3
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-06 13:53:06 +0000

    Bug 1892299 - Vendor libwebrtc from f4aadf3774
    Upstream commit: https://webrtc.googlesource.com/src/+/f4aadf37749a00ed6f182c76ae61c356deb64e2b
        Change RtpTransport and DsctTransport to receives packets through ReceivedPacketCallback
    
        Instead of using PacketTransportInternal::SignalReadPacket.
    
        Bug: webrtc:15368
        Change-Id: Icdc2d7f85df6db944f0ba0232891e6c5a8986a66
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/340440
        Commit-Queue: Per Kjellander <perkjwebrtc.org>
        Reviewed-by: Harald Alvestrand <htawebrtc.org>
        Cr-Commit-Position: refs/heads/main{#41823}
    
    UltraBlame original commit: 7053a7592b3eef1f08c48fbbdbfbe258fa61208c

--- third_party/libwebrtc/media/base/rtp_utils.h
+++ third_party/libwebrtc/media/base/rtp_utils.h
@@ -11,6 +11,8 @@
 #ifndef MEDIA_BASE_RTP_UTILS_H_
 #define MEDIA_BASE_RTP_UTILS_H_
 
+#include <cstdint>
+
 #include "absl/strings/string_view.h"
 #include "api/array_view.h"
 #include "rtc_base/byte_order.h"
@@ -46,7 +48,7 @@ bool GetRtcpType(const void* data, size_t len, int* value);
 bool GetRtcpSsrc(const void* data, size_t len, uint32_t* value);
 
 
-RtpPacketType InferRtpPacketType(rtc::ArrayView<const char> packet);
+RtpPacketType InferRtpPacketType(rtc::ArrayView<const uint8_t> packet);
 
 bool IsValidRtpPayloadType(int payload_type);
 
