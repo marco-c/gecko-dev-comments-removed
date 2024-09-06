1bb1fdd00423b8b9483235d1ae79d686891e873e
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-06 12:23:52 +0000

    Bug 1893996: part 4) Change some of `nsCSPContext::GatherSecurityPolicyViolationEventData`'s arguments to a `Variant`. r=tschuster
    Clearer and closer to the spec.
    
    Differential Revision: https://phabricator.services.mozilla.com/D208893
    
    UltraBlame original commit: 97de80038466acc07a67993c0c897087fc7fe509

--- dom/security/nsCSPContext.h
+++ dom/security/nsCSPContext.h
@@ -32,6 +32,8 @@ class nsIEventTarget;
 struct ConsoleMsgQueueElem;
 
 namespace mozilla {
+template <typename... Ts>
+class Variant;
 namespace dom {
 class Element;
 }
@@ -77,6 +79,18 @@ class nsCSPContext : public nsIContentSecurityPolicy {
                     uint32_t aLineNumber, uint32_t aColumnNumber,
                     uint32_t aSeverityFlag);
 
+  enum BlockedContentSource {
+    eUnknown,
+    eInline,
+    eEval,
+    eSelf,
+    eWasmEval,
+  };
+
+  
+  
+  using Resource = mozilla::Variant<nsIURI*, BlockedContentSource>;
+
   
 
 
@@ -98,10 +112,10 @@ class nsCSPContext : public nsIContentSecurityPolicy {
 
 
   nsresult GatherSecurityPolicyViolationEventData(
-      nsIURI* aBlockedURI, const nsACString& aBlockedString,
-      nsIURI* aOriginalURI, const nsAString& aViolatedDirective,
-      uint32_t aViolatedPolicyIndex, const nsAString& aSourceFile,
-      const nsAString& aScriptSample, uint32_t aLineNum, uint32_t aColumnNum,
+      Resource& aResource, nsIURI* aOriginalURI,
+      const nsAString& aViolatedDirective, uint32_t aViolatedPolicyIndex,
+      const nsAString& aSourceFile, const nsAString& aScriptSample,
+      uint32_t aLineNum, uint32_t aColumnNum,
       mozilla::dom::SecurityPolicyViolationEventInit& aViolationEventInit);
 
   nsresult SendReports(
@@ -113,14 +127,6 @@ class nsCSPContext : public nsIContentSecurityPolicy {
       nsICSPEventListener* aCSPEventListener,
       const mozilla::dom::SecurityPolicyViolationEventInit&
           aViolationEventInit);
-
-  enum BlockedContentSource {
-    eUnknown,
-    eInline,
-    eEval,
-    eSelf,
-    eWasmEval,
-  };
 
   nsresult AsyncReportViolation(
       mozilla::dom::Element* aTriggeringElement,
