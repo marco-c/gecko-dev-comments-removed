bd3bc14bda1a8302d21b958ef431611b2cf479ba
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-06 21:13:55 +0000

    Bug 1903582 - Synthesize fake pts when encoding AV1 using ffmpeg. r=chunmin
    Differential Revision: https://phabricator.services.mozilla.com/D214614
    
    UltraBlame original commit: 9d2f54b895dfedf2701ddebe326e1b13a9d954c0

--- dom/media/platforms/ffmpeg/FFmpegVideoEncoder.h
+++ dom/media/platforms/ffmpeg/FFmpegVideoEncoder.h
@@ -23,6 +23,7 @@ class FFmpegVideoEncoder : public MediaDataEncoder {};
 template <>
 class FFmpegVideoEncoder<LIBAV_VER> : public FFmpegDataEncoder<LIBAV_VER> {
   using DurationMap = SimpleMap<int64_t, int64_t, ThreadSafePolicy>;
+  using PtsMap = SimpleMap<int64_t, int64_t, NoOpPolicy>;
 
  public:
   NS_INLINE_DECL_THREADSAFE_REFCOUNTING(FFmpegVideoEncoder, final);
@@ -70,6 +71,12 @@ class FFmpegVideoEncoder<LIBAV_VER> : public FFmpegDataEncoder<LIBAV_VER> {
     uint8_t CurrentTemporalLayerId();
   };
   Maybe<SVCInfo> mSVCInfo{};
+  
+  
+  
+  int64_t mFakePts = 0;
+  int64_t mCurrentFramePts = 0;
+  PtsMap mPtsMap;
 };
 
 }  
