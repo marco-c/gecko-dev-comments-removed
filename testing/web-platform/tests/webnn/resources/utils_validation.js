2406ccde0d3a8761471507f4c17e748e1742b556
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-06 16:43:16 +0000

    Bug 1897863 [wpt PR 46387] - webnn: Migrate argMinMax validation tests to WPTs, a=testonly
    Automatic update from web-platform-tests
    webnn: Migrate argMinMax validation tests to WPTs
    
    This CL adds WPT tests for argMinMax and removes the unit tests
    `MLGraphBuilderTest.ArgMinMaxTest` and `MLGraphTestMojo.ArgMinMaxTest`.
    
    Bug: 327337526, 328026885
    Change-Id: I8fcce0db9e4b1673e721baf0b7abdfe3a06bdf2b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5547296
    Reviewed-by: Austin Sullivan <asullychromium.org>
    Commit-Queue: Shanxing Mei <shanxing.meiintel.com>
    Reviewed-by: ningxin hu <ningxin.huintel.com>
    Cr-Commit-Position: refs/heads/main{#1307229}
    
    --
    
    wpt-commits: afefbc876e7c3baf744ead6c9b6dfe9378d1668b
    wpt-pr: 46387
    
    UltraBlame original commit: aca0a0e2e45592a4e1d2c2895406de41041f484b

--- testing/web-platform/tests/webnn/resources/utils_validation.js
+++ testing/web-platform/tests/webnn/resources/utils_validation.js
@@ -307,6 +307,7 @@ function validateOptionsAxes(operationName) {
     }, `[${subOperationName}] TypeError is expected if any of options.axes elements is not an unsigned long interger`);
 
     
+    
     promise_test(async t => {
       for (let dataType of allWebNNOperandDataTypes) {
         for (let dimensions of allWebNNDimensionsArray) {
@@ -314,16 +315,16 @@ function validateOptionsAxes(operationName) {
           if (rank >= 1) {
             const input =
                 builder.input(`input${++inputIndex}`, {dataType, dimensions});
-            assert_throws_dom(
-                'DataError',
+            assert_throws_js(
+                TypeError,
                 () => builder[subOperationName](input, {axes: [rank]}));
-            assert_throws_dom(
-                'DataError',
+            assert_throws_js(
+                TypeError,
                 () => builder[subOperationName](input, {axes: [rank + 1]}));
           }
         }
       }
-    }, `[${subOperationName}] DataError is expected if any of options.axes elements is greater or equal to the size of input`);
+    }, `[${subOperationName}] TypeError is expected if any of options.axes elements is greater or equal to the size of input`);
 
     
     promise_test(async t => {
@@ -336,13 +337,13 @@ function validateOptionsAxes(operationName) {
             const axesArrayContainSameValues =
                 getAxesArrayContainSameValues(dimensions);
             for (let axes of axesArrayContainSameValues) {
-              assert_throws_dom(
-                  'DataError', () => builder[subOperationName](input, {axes}));
+              assert_throws_js(
+                  TypeError, () => builder[subOperationName](input, {axes}));
             }
           }
         }
       }
-    }, `[${subOperationName}] DataError is expected if two or more values are same in the axes sequence`);
+    }, `[${subOperationName}] TypeError is expected if two or more values are same in the axes sequence`);
   }
 }
 
