223758ad058054ce7be2b674c4d05d70e9fb0f36
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-06 20:00:08 +0000

    Bug 1896083 - Add functionality to eTLD service to check if hostname ends in known public suffix r=mak,necko-reviewers,kershaw
    Differential Revision: https://phabricator.services.mozilla.com/D211982
    
    UltraBlame original commit: 1e2450e990cef4b50f1db50353291d3fa6003604

---
+++ netwerk/dns/tests/unit/test_nsEffectiveTLDService_hasKnownPublicSuffix.js
@@ -0,0 +1,37 @@
+
+
+
+"use strict";
+
+add_task(async function () {
+  for (const [host, shouldKnowTld] of [
+    ["example.com", true], 
+    ["example.local", false],
+    ["example.vermögensberatung", true], 
+    ["example.xn--vermgensberatung-pwb", true],
+    ["example.löcal", false],
+    ["example.xn--lcal-5qa", false],
+    ["localhost", false],
+    ["com", true],
+    ["za", false],
+    ["co.za", true],
+    ["example.co.za", true],
+    ["example.com.", true],
+    ["example.local.", false],
+  ]) {
+    Assert.equal(
+      Services.eTLD.hasKnownPublicSuffixFromHost(host),
+      shouldKnowTld,
+      `"${host}" should ${
+        shouldKnowTld ? " " : "not "
+      }have a known public suffix`
+    );
+    Assert.equal(
+      Services.eTLD.hasKnownPublicSuffix(Services.io.newURI("http://" + host)),
+      shouldKnowTld,
+      `"http://${host}" should ${
+        shouldKnowTld ? " " : "not "
+      }have a known public suffix`
+    );
+  }
+});
