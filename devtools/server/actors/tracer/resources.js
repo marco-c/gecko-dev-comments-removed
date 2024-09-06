fbc9a725c4fec9ba5c90ab69b5ee8db95d9466a9
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-06 22:36:38 +0000

    Bug 1906291 - [devtools] Align events array with trace. r=devtools-reviewers,nchevobbe
    This will help use the same indexes for all the shared attributes.
    
    Differential Revision: https://phabricator.services.mozilla.com/D215128
    
    UltraBlame original commit: cc75c837b71c742ec53143e5c9231942aaf0674f

--- devtools/server/actors/tracer/resources.js
+++ devtools/server/actors/tracer/resources.js
@@ -313,7 +313,10 @@ class ResourcesTracingListener {
       this.#throttledTraces.push([
         "event",
         prefix,
+        null,
         ChromeUtils.dateNow(),
+        
+        -1,
         currentDOMEvent,
       ]);
     }
