e301404ab98c62bca39636392133f4e0a6e2d87c
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-06 18:38:49 +0000

    Bug 1902139 - Fix zone state assertion for incremental marking verifier r=sfink
    The assertion should check that all zones being collected are in the expected
    state, not all zones in the runtime.
    
    Differential Revision: https://phabricator.services.mozilla.com/D213532
    
    UltraBlame original commit: 3360f550085607f2f2ebb89dbc9ca12e88a2c5d9

--- js/src/gc/Sweeping.cpp
+++ js/src/gc/Sweeping.cpp
@@ -587,7 +587,7 @@ IncrementalProgress GCRuntime::markAllWeakReferences() {
 void GCRuntime::markAllGrayReferences(gcstats::PhaseKind phase) {
 #ifdef DEBUG
   
-  for (AllZonesIter zone(this); !zone.done(); zone.next()) {
+  for (GCZonesIter zone(this); !zone.done(); zone.next()) {
     MOZ_ASSERT(zone->isGCMarkingBlackAndGray());
   }
 #endif
