0d8323f53ce6840215bb455fca2e8ac6c085d85d
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-06 19:29:36 +0000

    Bug 1873915 - %2e entered in URL bar not normalized leading to denormalized request r=necko-reviewers,kershaw,jesup
    Differential Revision: https://phabricator.services.mozilla.com/D210800
    
    UltraBlame original commit: 1134e476cb57fc45b4775504dc1ba4681334fc4b

--- netwerk/base/nsStandardURL.cpp
+++ netwerk/base/nsStandardURL.cpp
@@ -696,12 +696,20 @@ bool nsStandardURL::ValidIPv6orHostname(const char* host, uint32_t length) {
 }
 
 void nsStandardURL::CoalescePath(netCoalesceFlags coalesceFlag, char* path) {
-  net_CoalesceDirs(coalesceFlag, path);
+  auto resultCoalesceDirs = net_CoalesceDirs(coalesceFlag, path);
   int32_t newLen = strlen(path);
-  if (newLen < mPath.mLen) {
+  if (newLen < mPath.mLen && resultCoalesceDirs) {
+    
+    uint32_t lastSlash = resultCoalesceDirs->first();
+    uint32_t endOfBasename = resultCoalesceDirs->second();
+
     int32_t diff = newLen - mPath.mLen;
     mPath.mLen = newLen;
-    mDirectory.mLen += diff;
+    
+    mDirectory.mLen = static_cast<int32_t>(lastSlash) + 1;
+    
+    
+    mBasename.mLen = static_cast<int32_t>(endOfBasename - mDirectory.mLen);
     mFilepath.mLen += diff;
     ShiftFromBasename(diff);
   }
