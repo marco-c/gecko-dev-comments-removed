464e0fa628f0b1e31e28af9c6e749d0d27e0c2a0
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-06 13:07:54 +0000

    Bug 1894517 - Update Pocket feed URL default value. r=home-newtab-reviewers,nbarrett,thecount
    Differential Revision: https://phabricator.services.mozilla.com/D209954
    
    UltraBlame original commit: d76f810099bb514518128014736efffd0f6c6904

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1780,7 +1780,7 @@ pref("browser.newtabpage.activity-stream.discoverystream.region-spocs-config", "
 pref("browser.newtabpage.activity-stream.discoverystream.region-basic-config", "");
 
 
-pref("browser.newtabpage.activity-stream.discoverystream.pocket-feed-parameters", "&enableRankingByRegion=1");
+pref("browser.newtabpage.activity-stream.discoverystream.pocket-feed-parameters", "");
 
 
 pref("browser.newtabpage.activity-stream.discoverystream.isCollectionDismissible", true);
