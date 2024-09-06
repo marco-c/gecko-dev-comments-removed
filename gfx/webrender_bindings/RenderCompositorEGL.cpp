e0d8f271c0f6b962df56befe8aaab5b086e9a1c2
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-06 21:53:55 +0000

    Bug 1898476 [Wayland] Lock Wayland surface before Swap buffers in RenderCompositorEGL r=emilio
    Differential Revision: https://phabricator.services.mozilla.com/D214885
    
    UltraBlame original commit: b56930a4d8bc3bef0d29b672cfe0fefb0a08c510

--- gfx/webrender_bindings/RenderCompositorEGL.cpp
+++ gfx/webrender_bindings/RenderCompositorEGL.cpp
@@ -154,6 +154,16 @@ RenderedFrameId RenderCompositorEGL::EndFrame(
     }
     gl()->SetDamage(bufferInvalid);
   }
+
+#ifdef MOZ_WIDGET_GTK
+  
+  
+  
+  UniquePtr<MozContainerSurfaceLock> lock;
+  if (auto* gtkWidget = mWidget->AsGTK()) {
+    lock = gtkWidget->LockSurface();
+  }
+#endif
   gl()->SwapBuffers();
   return frameId;
 }
