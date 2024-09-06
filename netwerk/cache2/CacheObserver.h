932d16848294fe04f8a964d72912fdf96130f5fd
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-06 15:31:48 +0000

    Bug 1895978 - Add the new clear on shutdown cache pref to the cache purging task code. r=pbz,necko-reviewers,valentin
    Differential Revision: https://phabricator.services.mozilla.com/D210681
    
    UltraBlame original commit: 1ab060d1469d8da59e54fc3c6b35d5f1301a1edb

--- netwerk/cache2/CacheObserver.h
+++ netwerk/cache2/CacheObserver.h
@@ -73,8 +73,14 @@ class CacheObserver : public nsIObserver, public nsSupportsWeakReference {
   }
   static uint32_t HalfLifeSeconds() { return sHalfLifeHours * 60.0F * 60.0F; }
   static bool ClearCacheOnShutdown() {
-    return StaticPrefs::privacy_sanitize_sanitizeOnShutdown() &&
-           StaticPrefs::privacy_clearOnShutdown_cache();
+    if (!StaticPrefs::privacy_sanitize_sanitizeOnShutdown()) {
+      return false;
+    }
+    if (StaticPrefs::privacy_sanitize_useOldClearHistoryDialog()) {
+      return StaticPrefs::privacy_clearOnShutdown_cache();
+    }
+    
+    return StaticPrefs::privacy_clearOnShutdown_v2_cache();
   }
   static void ParentDirOverride(nsIFile** aDir);
 
