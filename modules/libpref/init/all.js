f670feec145accee1b0929f4e5d830b0c903dfbf
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-06 17:03:28 +0000

    Bug 1900714 - Update DAP Leader URL to 09 r=simonf!
    Differential Revision: https://phabricator.services.mozilla.com/D212360
    
    UltraBlame original commit: 5ee8d972f5c0730a006903a49a51e02362d209be

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -592,7 +592,7 @@ pref("toolkit.telemetry.dap_visit_counting_enabled", false);
 
 pref("toolkit.telemetry.dap_visit_counting_experiment_list", "[]");
 
-pref("toolkit.telemetry.dap_leader", "https://dap-07-1.api.divviup.org/");
+pref("toolkit.telemetry.dap_leader", "https://dap-09-3.api.divviup.org/");
 
 pref("toolkit.telemetry.dap_leader_owner", "ISRG");
 
