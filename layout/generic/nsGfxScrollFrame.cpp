96070ba5e50f9a8fcfb16b88d3195791b79ad102
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-06 08:23:29 +0000

    Backed out changeset 0506d9964b32 (bug 306344) for causing reftests failures in 371466-1.xhtml and mochitests failures in test_memoryReporters.xhtml.
    UltraBlame original commit: 1598f8fe49ec3378b52a2b1c9191b1a4ac192253

--- layout/generic/nsGfxScrollFrame.cpp
+++ layout/generic/nsGfxScrollFrame.cpp
@@ -1265,7 +1265,17 @@ nsMargin nsHTMLScrollFrame::ComputeStableScrollbarGutter(
 
 
 static bool IsMarqueeScrollbox(const nsIFrame& aScrollFrame) {
-  return HTMLMarqueeElement::FromNodeOrNull(aScrollFrame.GetContent());
+  if (!aScrollFrame.GetContent()) {
+    return false;
+  }
+  if (MOZ_LIKELY(!aScrollFrame.GetContent()->HasBeenInUAWidget())) {
+    return false;
+  }
+  MOZ_ASSERT(aScrollFrame.GetParent() &&
+             aScrollFrame.GetParent()->GetContent());
+  return aScrollFrame.GetParent() &&
+         HTMLMarqueeElement::FromNodeOrNull(
+             aScrollFrame.GetParent()->GetContent());
 }
 
 
