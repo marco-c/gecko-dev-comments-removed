f262bd51620d9af654456c2e89a4e2cdd702cf65
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-06 16:27:30 +0000

    Bug 1897659. Implement parsing of CSS 'position-try-order' property. r=emilio,firefox-style-system-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D212220
    
    UltraBlame original commit: e06dd7a599a322559982f4cb356b509d2dda3248

--- layout/style/nsStyleStruct.h
+++ layout/style/nsStyleStruct.h
@@ -747,6 +747,7 @@ struct MOZ_NEEDS_MEMMOVABLE_MEMBERS nsStylePosition {
   
   mozilla::StylePositionAnchor mPositionAnchor;
   mozilla::StylePositionVisibility mPositionVisibility;
+  mozilla::StylePositionTryOrder mPositionTryOrder;
   mozilla::StyleInsetArea mInsetArea;
 
   mozilla::StyleFlexBasis mFlexBasis;
