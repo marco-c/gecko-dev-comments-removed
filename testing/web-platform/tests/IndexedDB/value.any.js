8290567e7aec112ecc06e756179e0d2544c36f3b
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-06 21:01:18 +0000

    Bug 1904631 [wpt PR 46896] - IDB WPTs: Extend bigint_value and value WPTs to run on workers, a=testonly
    Automatic update from web-platform-tests
    IDB WPTs: Extend bigint_value and value WPTs to run on workers
    
    Currently, the bigint_value and value IndexedDB WPTs only run in a
    window environment. This change combines them into a single file and
    extends them to also run in dedicated, shared, and service worker
    environments.
    
    Bug: 41455766
    Change-Id: I0c73a5069961aa6727d90f9ddbdf1e5dff4ffc87
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5560559
    Reviewed-by: Evan Stade <estadechromium.org>
    Commit-Queue: Rahul Singh <rahsinmicrosoft.com>
    Cr-Commit-Position: refs/heads/main{#1319470}
    
    --
    
    wpt-commits: 4298bb7fb5e5008977b2cd14ea5734a877b77db9
    wpt-pr: 46896
    
    UltraBlame original commit: 98f8747f7632efaf1bf26d87c908961959502ada

---
+++ testing/web-platform/tests/IndexedDB/value.any.js
@@ -0,0 +1,88 @@
+
+
+
+
+
+'use_strict';
+
+function setOnUpgradeNeeded(t, predicate, _instanceof, value) {
+  createdb(t).onupgradeneeded = t.step_func(e => {
+    const db = e.target.result;
+    const store = db.createObjectStore("store");
+    store.add(value, 1);
+
+    e.target.onsuccess = t.step_func(e => {
+      const transaction = db.transaction("store", "readonly", { durability: "relaxed" });
+      const objectStore = transaction.objectStore("store");
+      objectStore.get(1).onsuccess = t.step_func(e => {
+        if (predicate) {
+          assert_true(predicate(e.target.result),
+            "Predicate should return true for the deserialized result.");
+        } else if (_instanceof) {
+          assert_true(e.target.result instanceof _instanceof, "instanceof");
+        }
+        t.done();
+      });
+    });
+  });
+}
+
+
+
+
+
+function value_test(value, predicate, name) {
+  async_test(t => {
+    t.step(function () {
+      assert_true(predicate(value),
+        "Predicate should return true for the initial value.");
+    });
+
+    setOnUpgradeNeeded(t, predicate, null, value);
+  }, "BigInts as values in IndexedDB - " + name);
+}
+
+value_test(1n,
+  x => x === 1n,
+  "primitive BigInt");
+value_test(Object(1n),
+  x => typeof x === 'object' &&
+    x instanceof BigInt &&
+    x.valueOf() === 1n,
+  "BigInt object");
+value_test({ val: 1n },
+  x => x.val === 1n,
+  "primitive BigInt inside object");
+value_test({ val: Object(1n) },
+  x => x.val.valueOf() === 1n &&
+    x.val instanceof BigInt &&
+    x.val.valueOf() === 1n,
+  "BigInt object inside object");
+
+
+
+
+
+
+function invalidKey(key, name) {
+  test(t => {
+    assert_throws_dom("DataError", () => indexedDB.cmp(0, key));
+  }, "BigInts as keys in IndexedDB - " + name);
+}
+
+invalidKey(1n, "primitive BigInt");
+
+invalidKey(Object(1n), "BigInt object");
+
+function value(value, _instanceof) {
+  async_test(t => {
+    t.step(function () {
+      assert_true(value instanceof _instanceof, "TEST ERROR, instanceof");
+    });
+
+    setOnUpgradeNeeded(t, null, _instanceof, value);
+  }, "Values - " + _instanceof.name);
+}
+
+value(new Date(), Date);
+value(new Array(), Array);
