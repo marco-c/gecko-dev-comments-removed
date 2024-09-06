7cb31b38f7893c59ba10889b32f38bdc8ec22f4e
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-06 07:22:12 +0000

    Bug 1886892 - Part 1: Remove non-SHIP code from desktop SessionStore, r=sessionstore-reviewers,tabbrowser-reviewers,mconley
    In bug 1804140, we shipped SHIP everywhere on desktop. Unfortunately, we
    never removed the now-dead code from SessionStore.
    
    This patch removes the easy to identify SessionStore code for non-SHIP,
    now that it is a fully unsupported configuration.
    
    Differential Revision: https://phabricator.services.mozilla.com/D205479
    
    UltraBlame original commit: 389163cbd6492666e1dad1c569e74d26870d0560

--- browser/base/content/test/performance/browser_startup_content.js
+++ browser/base/content/test/performance/browser_startup_content.js
@@ -57,12 +57,6 @@ const known_scripts = {
   ]),
 };
 
-if (!Services.appinfo.sessionHistoryInParent) {
-  known_scripts.modules.add(
-    "resource:///modules/sessionstore/ContentSessionStore.sys.mjs"
-  );
-}
-
 
 
 const intermittently_loaded_scripts = {
