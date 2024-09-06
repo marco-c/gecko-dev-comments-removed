b8d407c37a901ad5f6a79e641000b2d59b6e4f0f
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-06 22:25:09 +0000

    Bug 1888946 - Update the event layers id to that of the APZ wheel transaction. r=botond
    Ensure that weel events are sent to the content process for the layers
    id of the current APZ wheel transaction. The corresponding content
    process should also have a content side wheel transaction that will
    ensure that content that is scrolled over cannot halt a user scroll with
    preventDefault on a wheel event listener.
    
    Differential Revision: https://phabricator.services.mozilla.com/D212967
    
    UltraBlame original commit: 1b83813dee994a520b704290eeaede63716af8ae

--- gfx/layers/apz/src/InputBlockState.h
+++ gfx/layers/apz/src/InputBlockState.h
@@ -97,6 +97,8 @@ class InputBlockState : public RefCounted<InputBlockState> {
   virtual void UpdateTargetApzc(
       const RefPtr<AsyncPanZoomController>& aTargetApzc);
 
+  const AsyncPanZoomController* TargetApzc() const { return mTargetApzc.get(); }
+
  private:
   
   
@@ -270,6 +272,8 @@ class WheelBlockState : public CancelableBlockState {
   ScrollDirections GetAllowedScrollDirections() const {
     return mAllowedScrollDirections;
   }
+
+  LayersId GetLayersId() const;
 
  protected:
   void UpdateTargetApzc(
