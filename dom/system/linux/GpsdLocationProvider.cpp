538be440e9e17eac8af8f56ce0aec2389b9b21e9
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-06 23:12:45 +0000

    Bug 1905928 - Part 3: Add glean telemetry entries r=emilio,win-reviewers,handyman
    Each provider has different fallback behavior:
    * Windows/macOS: Fallback only on error
    * Gpsd/Portal: Fallback only on timeout
    * Geoclue: Fallback on both
    
    To meet all the behaviors MLSFallback got FallbackReason/ShutdownReason and report via Glean based on that.
    
    Differential Revision: https://phabricator.services.mozilla.com/D216085
    
    UltraBlame original commit: 027487f4a3db320c96a37ec90fd2f4f8f4ef4999

--- dom/system/linux/GpsdLocationProvider.cpp
+++ dom/system/linux/GpsdLocationProvider.cpp
@@ -340,7 +340,7 @@ void GpsdLocationProvider::Update(nsIDOMGeoPosition* aPosition) {
 
   if (mMLSProvider) {
     
-    mMLSProvider->Shutdown();
+    mMLSProvider->Shutdown(MLSFallback::ShutdownReason::ProviderResponded);
     mMLSProvider = nullptr;
   }
 
@@ -425,7 +425,7 @@ GpsdLocationProvider::Watch(nsIGeolocationUpdate* aCallback) {
 NS_IMETHODIMP
 GpsdLocationProvider::Shutdown() {
   if (mMLSProvider) {
-    mMLSProvider->Shutdown();
+    mMLSProvider->Shutdown(MLSFallback::ShutdownReason::ProviderShutdown);
     mMLSProvider = nullptr;
   }
 
