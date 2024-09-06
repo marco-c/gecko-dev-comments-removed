014f4b16c7ceebcbdcdee17ef05ae521e16efc03
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-06 08:00:34 +0000

    Bug 1888238 - part3 : create a helper class WMFCDMCapabilities to get capabilities asynchronously. r=jolin
    In following patches, we will use this new method to replace
    `WMFCDMImpl::GetCapabilities()`.
    
    Differential Revision: https://phabricator.services.mozilla.com/D205890
    
    UltraBlame original commit: 309a46bb6661d4c000c38a468f3ac93efe973340

--- dom/media/eme/KeySystemConfig.h
+++ dom/media/eme/KeySystemConfig.h
@@ -9,6 +9,7 @@
 
 #include "nsString.h"
 #include "nsTArray.h"
+#include "mozilla/MozPromise.h"
 #include "mozilla/dom/MediaKeysBinding.h"
 
 namespace mozilla {
@@ -17,6 +18,10 @@ struct KeySystemConfigRequest;
 
 struct KeySystemConfig {
  public:
+  using SupportedConfigsPromise =
+      MozPromise<nsTArray<KeySystemConfig>, bool ,
+                  true>;
+
   
   
   enum class Requirement {
