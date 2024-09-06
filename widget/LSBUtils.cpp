267382394dfe145f6ef2e740e7a11ccb2c7bed8f
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-06 11:47:14 +0000

    Bug 1894412 - When reading /etc/os-release, fall back to BUILD_ID when VERSION_ID is not set. r=xpcom-reviewers,emilio
    Arch Linux doesn't come with a VERSION_ID at all, but it has a BUILD_ID.
    Semantically, falling back to BUILD_ID when VERSION_ID is not present
    would seem fair play:
    
    Per https://www.freedesktop.org/software/systemd/man/latest/os-release.html:
    
    BUILD_ID=
        A string uniquely identifying the system image originally used as the
        installation base. In most cases, VERSION_ID or IMAGE_ID+IMAGE_VERSION
        are updated when the entire system image is replaced during an update.
        BUILD_ID may be used in distributions where the original installation
        image version is important: VERSION_ID would change during incremental
        system updates, but BUILD_ID would not. This field is optional.
    
    Differential Revision: https://phabricator.services.mozilla.com/D209089
    
    UltraBlame original commit: 252deaf4865809082dac8eeeadeefc046bac64f8

--- widget/LSBUtils.cpp
+++ widget/LSBUtils.cpp
@@ -45,6 +45,7 @@ bool GetOSRelease(nsACString& aDistributor, nsACString& aDescription,
   bool seen_id = false, seen_pretty_name = false, seen_version_id = false;
   std::string rawline;
   nsAutoCString name;
+  nsAutoCString build_id;
   while (std::getline(stream, rawline)) {
     std::string_view line(rawline);
     size_t pos = line.find('=');
@@ -68,6 +69,8 @@ bool GetOSRelease(nsACString& aDistributor, nsACString& aDescription,
         if (ExtractAndSetValue(aDescription, value)) seen_pretty_name = true;
       } else if (key == "VERSION_ID") {
         if (ExtractAndSetValue(aRelease, value)) seen_version_id = true;
+      } else if (key == "BUILD_ID") {
+        ExtractAndSetValue(build_id, value);
       } else if (key == "VERSION_CODENAME") {
         ExtractAndSetValue(aCodename, value);
       }
@@ -76,6 +79,11 @@ bool GetOSRelease(nsACString& aDistributor, nsACString& aDescription,
   
   if (seen_id && !name.IsEmpty() && name.EqualsIgnoreCase(aDistributor)) {
     aDistributor = name;
+  }
+  
+  if (!seen_version_id && !build_id.IsEmpty()) {
+    aRelease = build_id;
+    seen_version_id = true;
   }
   
   
