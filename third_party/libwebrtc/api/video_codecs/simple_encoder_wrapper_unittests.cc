11dee9148098894b0c659093c5d026f209895f5b
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-06 22:13:01 +0000

    Bug 1903098 - Vendor libwebrtc from cf4764e062
    Upstream commit: https://webrtc.googlesource.com/src/+/cf4764e062781927c4f90484ffb42f75f2483c6f
        Move EncodeResultCallback to FrameEncodeSettings.
    
        Bug: b/336978562
        Change-Id: I25cb6662a6b66fba9d22d974e1b6da48e137d0ea
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/349225
        Reviewed-by: Erik Språng <sprangwebrtc.org>
        Commit-Queue: Philip Eliasson <philipelwebrtc.org>
        Cr-Commit-Position: refs/heads/main{#42284}
    
    UltraBlame original commit: 2e16d071cf96bf700aae200ef8525c5a47f4c657

--- third_party/libwebrtc/api/video_codecs/simple_encoder_wrapper_unittests.cc
+++ third_party/libwebrtc/api/video_codecs/simple_encoder_wrapper_unittests.cc
@@ -171,7 +171,7 @@ TEST(SimpleEncoderWrapper, EncodeL1T1) {
       });
 }
 
-TEST(SimpleEncoderWrapper, DISABLED_EncodeL2T2_KEY) {
+TEST(SimpleEncoderWrapper, EncodeL2T2_KEY) {
   auto encoder = LibaomAv1EncoderFactory().CreateEncoder(
       {.max_encode_dimensions = {1080, 720},
        .encoding_format = {.sub_sampling = EncodingFormat::k420,
@@ -231,7 +231,7 @@ TEST(SimpleEncoderWrapper, DISABLED_EncodeL2T2_KEY) {
   EXPECT_THAT(num_callbacks, Eq(4));
 }
 
-TEST(SimpleEncoderWrapper, DISABLED_EncodeL1T3ForceKeyframe) {
+TEST(SimpleEncoderWrapper, EncodeL1T3ForceKeyframe) {
   auto encoder = LibaomAv1EncoderFactory().CreateEncoder(
       {.max_encode_dimensions = {1080, 720},
        .encoding_format = {.sub_sampling = EncodingFormat::k420,
