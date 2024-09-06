cf80929455b06541aae005e8befc98e1f4aa9997
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-06 21:08:02 +0000

    Bug 1898153 - wasm: Merge MetadataTier into CodeTier. r=jseward
    MetadataTier and CodeTier are 1:1, and so there's no point
    in having separate classes for them. This patch merges them
    to reduce the number of concepts in our code hierarchy to
    worry about. Some references to metadataTier still remain,
    but will be cleaned up in future patches.
    
    Differential Revision: https://phabricator.services.mozilla.com/D213138
    
    UltraBlame original commit: 1de85a4ce7ae00553d42a7d258974b9bc4194ea9

--- js/src/wasm/WasmCode.h
+++ js/src/wasm/WasmCode.h
@@ -77,8 +77,6 @@ class MacroAssembler;
 
 namespace wasm {
 
-struct MetadataTier;
-
 
 
 
@@ -226,8 +224,7 @@ class ModuleSegment : public CodeSegment {
 
   bool initialize(const CodeTier& codeTier, const LinkData& linkData,
                   const CodeMetadata& codeMeta,
-                  const CodeMetadataForAsmJS* codeMetaForAsmJS,
-                  const MetadataTier& metadataTier);
+                  const CodeMetadataForAsmJS* codeMetaForAsmJS);
 
   Tier tier() const { return tier_; }
 
@@ -254,39 +251,6 @@ extern void StaticallyUnlink(uint8_t* base, const LinkData& linkData);
 
 
 
-
-struct MetadataTier {
-  explicit MetadataTier(Tier tier = Tier::Serialized)
-      : tier(tier), debugTrapOffset(0) {}
-
-  const Tier tier;
-
-  Uint32Vector funcToCodeRange;
-  CodeRangeVector codeRanges;
-  CallSiteVector callSites;
-  TrapSiteVectorArray trapSites;
-  FuncImportVector funcImports;
-  FuncExportVector funcExports;
-  StackMaps stackMaps;
-  TryNoteVector tryNotes;
-  CodeRangeUnwindInfoVector codeRangeUnwindInfos;
-
-  
-  uint32_t debugTrapOffset;
-
-  FuncExport& lookupFuncExport(uint32_t funcIndex,
-                               size_t* funcExportIndex = nullptr);
-  const FuncExport& lookupFuncExport(uint32_t funcIndex,
-                                     size_t* funcExportIndex = nullptr) const;
-
-  const CodeRange& codeRange(const FuncExport& funcExport) const {
-    return codeRanges[funcToCodeRange[funcExport.funcIndex()]];
-  }
-
-  size_t sizeOfExcludingThis(mozilla::MallocSizeOf mallocSizeOf) const;
-};
-
-using UniqueMetadataTier = UniquePtr<MetadataTier>;
 
 
 
@@ -403,33 +367,44 @@ using UniqueCodeTier = UniquePtr<CodeTier>;
 using UniqueConstCodeTier = UniquePtr<const CodeTier>;
 
 class CodeTier {
-  const Code* code_;
+ public:
+  
+  const Code* code;
 
   
-  const UniqueMetadataTier metadata_;
-  const UniqueModuleSegment segment_;
+  UniqueModuleSegment segment;
+  const Tier tier;
+  Uint32Vector funcToCodeRange;
+  CodeRangeVector codeRanges;
+  CallSiteVector callSites;
+  TrapSiteVectorArray trapSites;
+  FuncImportVector funcImports;
+  FuncExportVector funcExports;
+  StackMaps stackMaps;
+  TryNoteVector tryNotes;
+  CodeRangeUnwindInfoVector codeRangeUnwindInfos;
 
- public:
-  CodeTier(UniqueMetadataTier metadata, UniqueModuleSegment segment)
-      : code_(nullptr),
-        metadata_(std::move(metadata)),
-        segment_(std::move(segment)) {}
+  
+  uint32_t debugTrapOffset;
 
-  bool initialized() const { return !!code_ && segment_->initialized(); }
+  explicit CodeTier(Tier tier)
+      : code(nullptr), tier(tier), debugTrapOffset(0) {}
+
+  bool initialized() const { return !!code && segment->initialized(); }
   bool initialize(const Code& code, const LinkData& linkData,
                   const CodeMetadata& codeMeta,
                   const CodeMetadataForAsmJS* codeMetaForAsmJS);
 
-  Tier tier() const { return segment_->tier(); }
-  const MetadataTier& metadata() const { return *metadata_.get(); }
-  const ModuleSegment& segment() const { return *segment_.get(); }
-  const Code& code() const {
-    MOZ_ASSERT(initialized());
-    return *code_;
+  const CodeRange& codeRange(const FuncExport& funcExport) const {
+    return codeRanges[funcToCodeRange[funcExport.funcIndex()]];
   }
 
   const CodeRange* lookupRange(const void* pc) const;
   const TryNote* lookupTryNote(const void* pc) const;
+  FuncExport& lookupFuncExport(uint32_t funcIndex,
+                               size_t* funcExportIndex = nullptr);
+  const FuncExport& lookupFuncExport(uint32_t funcIndex,
+                                     size_t* funcExportIndex = nullptr) const;
 
   void addSizeOfMisc(MallocSizeOf mallocSizeOf, size_t* code,
                      size_t* data) const;
@@ -622,10 +597,7 @@ class Code : public ShareableBase<Code> {
   }
 
   const ModuleSegment& segment(Tier iter) const {
-    return codeTier(iter).segment();
-  }
-  const MetadataTier& metadata(Tier iter) const {
-    return codeTier(iter).metadata();
+    return *codeTier(iter).segment.get();
   }
 
   const RWExclusiveData<LazyStubTier>& lazyStubs() const { return lazyStubs_; }
--- js/src/wasm/WasmStubs.cpp
+++ js/src/wasm/WasmStubs.cpp
@@ -1507,9 +1507,8 @@ void wasm::GenerateDirectCallFromJit(MacroAssembler& masm, const FuncExport& fe,
 
   
   const CodeTier& codeTier = inst.code().codeTier(inst.code().bestTier());
-  const MetadataTier& metadata = codeTier.metadata();
-  const CodeRange& codeRange = metadata.codeRange(fe);
-  void* callee = codeTier.segment().base() + codeRange.funcUncheckedCallEntry();
+  const CodeRange& codeRange = codeTier.codeRange(fe);
+  void* callee = codeTier.segment->base() + codeRange.funcUncheckedCallEntry();
 
   masm.assertStackAlignment(WasmStackAlignment);
   MoveSPForJitABI(masm);
