d00888d393f47720e022346fee2e3a3be3d246ea
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-06 23:16:52 +0000

    Bug 1907230: Fix error handling in xpc::CreateSandboxObject r=mccr8
    Differential Revision: https://phabricator.services.mozilla.com/D216599
    
    UltraBlame original commit: 438d0a5ed0b3aa690cc5fc046b124b173ee34d93

--- js/xpconnect/src/Sandbox.cpp
+++ js/xpconnect/src/Sandbox.cpp
@@ -1500,7 +1500,10 @@ nsresult xpc::CreateSandboxObject(JSContext* cx, MutableHandleValue vp,
   
   xpc::SetLocationForGlobal(sandbox, options.sandboxName);
 
-  xpc::SetSandboxMetadata(cx, sandbox, options.metadata);
+  nsresult rv = xpc::SetSandboxMetadata(cx, sandbox, options.metadata);
+  if (NS_WARN_IF(NS_FAILED(rv))) {
+    return rv;
+  }
 
   JSAutoRealm ar(cx, sandbox);
   JS_FireOnNewGlobalObject(cx, sandbox);
