b1e6926b6f1544956efc8223b6d853ecb85752d3
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-06 13:07:51 +0000

    Bug 1890136 - Add preferences, hook up toggle button on new tab customization panel and about:preferences#home r=thecount,fluent-reviewers,home-newtab-reviewers,bolsson,settings-reviewers,nanj,Gijs
    Differential Revision: https://phabricator.services.mozilla.com/D207775
    
    UltraBlame original commit: 4b8a8c556d9d57290b650bfa2ea2928306369f4e

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1679,7 +1679,9 @@ pref("browser.newtab.preload", true);
 pref("browser.newtabpage.activity-stream.system.showWeather", false);
 pref("browser.newtabpage.activity-stream.showWeather", false);
 pref("browser.newtabpage.activity-stream.weather.query", "");
-
+pref("browser.newtabpage.activity-stream.weather.locationSearchEnabled", false);
+pref("browser.newtabpage.activity-stream.weather.temperatureUnits", "f");
+pref("browser.newtabpage.activity-stream.weather.display", "simple");
 
 
 pref("browser.newtabpage.activity-stream.newtabWallpapers.enabled", false);
