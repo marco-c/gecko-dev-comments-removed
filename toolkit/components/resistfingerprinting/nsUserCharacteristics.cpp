df954cbe2530cb2c18886bcf4e8324973b917335
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-06 16:29:56 +0000

    Bug 1898744: Collect Scaling/Zoom Information. r=tjr
    Differential Revision: https://phabricator.services.mozilla.com/D211557
    
    UltraBlame original commit: 56ea192038014e2e2e878ea6a57aeee8132956e6

--- toolkit/components/resistfingerprinting/nsUserCharacteristics.cpp
+++ toolkit/components/resistfingerprinting/nsUserCharacteristics.cpp
@@ -38,6 +38,7 @@
 #include "prsystem.h"
 #if defined(XP_WIN)
 #  include "WinUtils.h"
+#  include "mozilla/gfx/DisplayConfigWindows.h"
 #elif defined(MOZ_WIDGET_ANDROID)
 #  include "mozilla/java/GeckoAppShellWrappers.h"
 #elif defined(XP_MACOSX)
@@ -326,6 +327,25 @@ void PopulateFontPrefs() {
       Preferences::HasUserValue("font.name-list.emoji"));
 }
 
+void PopulateScaling() {
+  nsCString output = "["_ns;
+
+  auto& screenManager = widget::ScreenManager::GetSingleton();
+  const auto& screens = screenManager.CurrentScreenList();
+  for (const auto& screen : screens) {
+    
+    
+    output.Append(std::to_string(screen->GetContentsScaleFactor()));
+    if (&screen != &screens.LastElement()) {
+      output.Append(",");
+    }
+  }
+
+  output.Append("]");
+
+  glean::characteristics::scalings.Set(output);
+}
+
 
 
 
@@ -458,6 +478,7 @@ void nsUserCharacteristics::PopulateDataAndEventuallySubmit(
     PopulateScreenProperties();
     PopulatePrefs();
     PopulateFontPrefs();
+    PopulateScaling();
 
     glean::characteristics::target_frame_rate.Set(
         gfxPlatform::TargetFrameRate());
