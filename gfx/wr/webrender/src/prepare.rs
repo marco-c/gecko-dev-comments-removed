9626b0b72f132295acf1e04aff3505c932a2f8c3
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-06 17:12:02 +0000

    Bug 1887837 - Enable quad conic gradients when the GPU is used. r=gw
    Conic gradients are too slow on swgl, we have to add support for caching them. Meanwhile, enable the code path when swgl is disabled.
    
    Differential Revision: https://phabricator.services.mozilla.com/D212242
    
    UltraBlame original commit: eb5eb6f5972ff537d2f189e3d2ebb2ba2d087857

--- gfx/wr/webrender/src/prepare.rs
+++ gfx/wr/webrender/src/prepare.rs
@@ -231,12 +231,11 @@ fn prepare_prim_for_render(
                 !prim_data.brush_segments.is_empty() ||
                     may_need_repetition(prim_data.stretch_size, prim_data.common.prim_rect)
             }
-            PrimitiveInstanceKind::ConicGradient { .. } => {
-                
-                true
-                
-                
-                
+            
+            PrimitiveInstanceKind::ConicGradient { data_handle, .. } if !frame_context.fb_config.is_software => {
+                let prim_data = &data_stores.conic_grad[*data_handle];
+                !prim_data.brush_segments.is_empty() ||
+                    may_need_repetition(prim_data.stretch_size, prim_data.common.prim_rect)
             }
             _ => true,
         };
