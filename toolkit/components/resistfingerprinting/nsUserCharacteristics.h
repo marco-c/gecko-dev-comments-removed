0afc8f1dd8fc249b9459a28921ba192d658b2a49
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-06 20:55:21 +0000

    Bug 1902086: Collect pointer info. r=timhuang
    Differential Revision: https://phabricator.services.mozilla.com/D213453
    
    UltraBlame original commit: 119854cde65632e7b0986dd7039ebd39e2356f98

--- toolkit/components/resistfingerprinting/nsUserCharacteristics.h
+++ toolkit/components/resistfingerprinting/nsUserCharacteristics.h
@@ -7,10 +7,15 @@
 #define __nsUserCharacteristics_h__
 
 #include "ErrorList.h"
+#include "mozilla/MouseEvents.h"
 
 class nsUserCharacteristics {
  public:
   static void MaybeSubmitPing();
+
+  
+  
+  static void StealPointerEvent(const mozilla::WidgetPointerEvent& aEvent);
 
   
 
