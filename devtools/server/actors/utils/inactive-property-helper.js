3a263d9c7d3e52c1819a69ef91813f82002a21cc
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-06 18:13:00 +0000

    Bug 1583894 - [devtools] Handle 'box-sizing' property in inactive CSS. r=devtools-reviewers,fluent-reviewers,bolsson,nchevobbe.
    Differential Revision: https://phabricator.services.mozilla.com/D212784
    
    UltraBlame original commit: 4c71d288b97e14547ed57746d1c5f7996c287d04

--- devtools/server/actors/utils/inactive-property-helper.js
+++ devtools/server/actors/utils/inactive-property-helper.js
@@ -575,6 +575,13 @@ class InactivePropertyHelper {
         fixId: "inactive-css-text-wrap-balance-fragmented-fix",
         msgId: "inactive-css-text-wrap-balance-fragmented",
       },
+      
+      {
+        invalidProperties: ["box-sizing"],
+        when: () => this.nonReplacedInlineBox,
+        fixId: "learn-more",
+        msgId: "inactive-css-no-width-height",
+      },
     ];
   }
 
