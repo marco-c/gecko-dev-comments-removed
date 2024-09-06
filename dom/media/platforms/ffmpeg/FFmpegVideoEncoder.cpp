68ac2ab257e9bf450fa710f1bd6e1ed62ea8178b
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-06 20:59:53 +0000

    Bug 1903582 - Put the temporal scalability mode at the end of the svc parameter list. r=chunmin
    See https://searchfox.org/mozilla-central/rev/74518d4f6979b088e28405ba7e6238f4707639cf/media/ffvpx/libavcodec/libvpxenc.c#695-696
    
    Differential Revision: https://phabricator.services.mozilla.com/D214611
    
    UltraBlame original commit: c059c4081bf001f2addfea2ffa0ce101eba49ff2

--- dom/media/platforms/ffmpeg/FFmpegVideoEncoder.cpp
+++ dom/media/platforms/ffmpeg/FFmpegVideoEncoder.cpp
@@ -9,6 +9,7 @@
 #include <aom/aomcx.h>
 
 #include "BufferReader.h"
+#include "EncoderConfig.h"
 #include "FFmpegLog.h"
 #include "FFmpegUtils.h"
 #include "H264.h"
@@ -615,6 +616,7 @@ Result<MediaDataEncoder::EncodedData, nsresult> FFmpegVideoEncoder<
   mFrame->pts = aSample->mTime.ToMicroseconds();
 #  if LIBAVCODEC_VERSION_MAJOR >= 60
   mFrame->duration = aSample->mDuration.ToMicroseconds();
+
 #  else
   
   mDurationMap.Insert(mFrame->pts, aSample->mDuration.ToMicroseconds());
@@ -776,20 +778,16 @@ FFmpegVideoEncoder<LIBAV_VER>::GetSVCSettings() {
 
     
     name = "ts-parameters"_ns;
-    parameters.AppendPrintf(
-        "ts_layering_mode=%u",
-        svc->mCodecAppendix->as<VPXSVCAppendix>().mLayeringMode);
-    parameters.Append(":ts_target_bitrate=");
+    parameters.Append("ts_target_bitrate=");
     for (size_t i = 0; i < svc->mTargetBitrates.Length(); ++i) {
       if (i > 0) {
         parameters.Append(",");
       }
       parameters.AppendPrintf("%d", svc->mTargetBitrates[i]);
     }
-
-    
-    
-    
+    parameters.AppendPrintf(
+        ":ts_layering_mode=%u",
+        svc->mCodecAppendix->as<VPXSVCAppendix>().mLayeringMode);
   }
 
   if (codecType == CodecType::AV1) {
