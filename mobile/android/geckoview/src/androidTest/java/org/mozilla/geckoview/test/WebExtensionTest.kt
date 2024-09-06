be5f528a755c962f6e9daa59bcf9b54587d6bc1d
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-06 12:43:13 +0000

    Bug 1895478 - Update WebExtensionTest#onOptionalPermissionsChanged r=rpl
    Differential Revision: https://phabricator.services.mozilla.com/D209722
    
    UltraBlame original commit: dc17824289cb5842ab139a41eeb1f933e628e130

--- mobile/android/geckoview/src/androidTest/java/org/mozilla/geckoview/test/WebExtensionTest.kt
+++ mobile/android/geckoview/src/androidTest/java/org/mozilla/geckoview/test/WebExtensionTest.kt
@@ -484,6 +484,14 @@ class WebExtensionTest : BaseSessionTest() {
 
     @Test
     fun optionalOriginsNormalized() {
+        
+        
+        sessionRule.setPrefsUntilTestEnd(
+            mapOf(
+                "extensions.originControls.grantByDefault" to false,
+            ),
+        )
+
         var extension = sessionRule.waitForResult(
             controller.ensureBuiltIn(
                 "resource://android/assets/web_extensions/optional-permission-all-urls/",
@@ -494,8 +502,11 @@ class WebExtensionTest : BaseSessionTest() {
         assertEquals("optional-permission-all-urls@example.com", extension.id)
 
         var grantedOptionalOrigins = extension.metaData.grantedOptionalOrigins
-
-        assertThat("grantedOptionalOrigins must be 0.", grantedOptionalOrigins.size, equalTo(0))
+        assertArrayEquals(
+            "grantedOptionalPermissions must be initially empty",
+            arrayOf(),
+            grantedOptionalOrigins,
+        )
 
         extension = sessionRule.waitForResult(
             controller.addOptionalPermissions(
