df06de7048bf5c3f8f1d3ba17ebde363b36e293d
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-06 22:56:57 +0000

    Bug 1907320 - Allow sharing styles that specify transitions / animations. r=firefox-style-system-reviewers,layout-reviewers,boris
    Most styles that specify transitions don't actually have a transition
    running at the very same time, and we can check for that in the cache
    already.
    
    We need to avoid reusing-by-rule-node for starting-style rules, but
    that's fine because that's effectively what already happened before this
    change, due to the check we're removing (we only resolve starting-style
    when we're about to maybe-start a transition).
    
    Differential Revision: https://phabricator.services.mozilla.com/D216286
    
    UltraBlame original commit: cf8b2df49bf0901b08109fe6f4d5ba4c178c1a0f

--- servo/components/style/style_resolver.rs
+++ servo/components/style/style_resolver.rs
@@ -236,6 +236,7 @@ where
             },
             parent_style,
             layout_parent_style,
+            include_starting_style,
             primary_results.has_starting_style,
         )
     }
@@ -245,6 +246,7 @@ where
         inputs: CascadeInputs,
         parent_style: Option<&ComputedValues>,
         layout_parent_style: Option<&ComputedValues>,
+        include_starting_style: IncludeStartingStyle,
         may_have_starting_style: bool,
     ) -> PrimaryStyle {
         
@@ -252,6 +254,7 @@ where
         let may_reuse = self.element.matches_user_and_content_rules() &&
             parent_style.is_some() &&
             inputs.rules.is_some() &&
+            include_starting_style == IncludeStartingStyle::No &&
             
             
             
@@ -413,6 +416,7 @@ where
                 inputs.primary,
                 parent_style,
                 layout_parent_style,
+                IncludeStartingStyle::No,
                 may_have_starting_style,
             );
 
