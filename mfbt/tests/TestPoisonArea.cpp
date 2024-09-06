ea552a660f139c456a900ad395a01bd0708eda23
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-06 20:22:55 +0000

    Bug 1903966 - Skip part of TestPoisonArea on Apple Silicon r=glandium
    Apple Silicon doesn't support W+X pages so this part of the test can't run.
    
    Differential Revision: https://phabricator.services.mozilla.com/D214497
    
    UltraBlame original commit: 93469f496ea8f804e5ebdba76593f29a2a8493c6

--- mfbt/tests/TestPoisonArea.cpp
+++ mfbt/tests/TestPoisonArea.cpp
@@ -354,7 +354,7 @@ static uintptr_t ReserveNegativeControl() {
   
 
   if (MakeRegionExecutable(result)) {
-    printf("ERROR | making negative control executable | %s\n", LastErrMsg());
+    ReleaseRegion(result);
     return 0;
   }
 
@@ -514,15 +514,29 @@ int main() {
 #endif
 
   uintptr_t ncontrol = ReserveNegativeControl();
+  if (!ncontrol) {
+#if (defined __aarch64__ || defined _M_ARM64) && defined(XP_DARWIN)
+    
+    
+    printf("TEST-SKIP | making negative control executable | %s\n",
+           LastErrMsg());
+#else
+    printf("ERROR | making negative control executable | %s\n", LastErrMsg());
+    return 2;
+#endif
+  }
+
   uintptr_t pcontrol = ReservePositiveControl();
   uintptr_t poison = ReservePoisonArea();
 
-  if (!ncontrol || !pcontrol || !poison) {
+  if (!pcontrol || !poison) {
     return 2;
   }
 
   bool failed = false;
-  failed |= TestPage("negative control", ncontrol, 1);
+  if (ncontrol) {
+    failed |= TestPage("negative control", ncontrol, 1);
+  }
   failed |= TestPage("positive control", pcontrol, 0);
   failed |= TestPage("poison area", poison, 0);
 
