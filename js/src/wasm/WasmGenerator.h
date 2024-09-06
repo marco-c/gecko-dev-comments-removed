82908b14277ec8889f4c61bee8e6b0109d3b7a1b
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-06 19:30:32 +0000

    Bug 1891182 - part 06 - move instance layout code to ModuleMetadata::doInstanceLayout.  r=rhunt.
    This patch extracts the Instance layout code from ModuleGenerator::init,
    ::allocateInstanceDataBytesN and allocateInstanceDataBytes, and moves it to a
    new method, ModuleMetadata::doInstanceLayout, which is a better place for it.
    The code is also somewhat simpler, although functionally identical.
    
    Differential Revision: https://phabricator.services.mozilla.com/D211161
    
    UltraBlame original commit: 27b0da0ed147cf5d362e3e799559930217ec5d75

--- js/src/wasm/WasmGenerator.h
+++ js/src/wasm/WasmGenerator.h
@@ -1,20 +1,20 @@
-/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*-
- * vim: set ts=8 sts=2 et sw=2 tw=80:
- *
- * Copyright 2015 Mozilla Foundation
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #ifndef wasm_generator_h
 #define wasm_generator_h
@@ -39,7 +39,7 @@ namespace wasm {
 struct CompileTask;
 using CompileTaskPtrVector = Vector<CompileTask*, 0, SystemAllocPolicy>;
 
-// FuncCompileInput contains the input for compiling a single function.
+
 
 struct FuncCompileInput {
   const uint8_t* begin;
@@ -60,8 +60,8 @@ struct FuncCompileInput {
 
 using FuncCompileInputVector = Vector<FuncCompileInput, 8, SystemAllocPolicy>;
 
-// CompiledCode contains the resulting code and metadata for a set of compiled
-// input functions or stubs.
+
+
 
 struct CompiledCode {
   CompiledCode() : featureUsage(FeatureUsage::None) {}
@@ -106,10 +106,10 @@ struct CompiledCode {
   size_t sizeOfExcludingThis(mozilla::MallocSizeOf mallocSizeOf) const;
 };
 
-// The CompileTaskState of a ModuleGenerator contains the mutable state shared
-// between helper threads executing CompileTasks. Each CompileTask started on a
-// helper thread eventually either ends up in the 'finished' list or increments
-// 'numFailed'.
+
+
+
+
 
 struct CompileTaskState {
   HelperThreadLockData<CompileTaskPtrVector> finished_;
@@ -129,8 +129,8 @@ struct CompileTaskState {
   ConditionVariable& condVar() { return condVar_.ref(); }
 };
 
-// A CompileTask holds a batch of input functions that are to be compiled on a
-// helper thread as well as, eventually, the results of compilation.
+
+
 
 struct CompileTask : public HelperThreadTask {
   const ModuleMetadata& moduleMeta;
@@ -157,11 +157,11 @@ struct CompileTask : public HelperThreadTask {
   ThreadType threadType() override;
 };
 
-// A ModuleGenerator encapsulates the creation of a wasm module. During the
-// lifetime of a ModuleGenerator, a sequence of FunctionGenerators are created
-// and destroyed to compile the individual function bodies. After generating all
-// functions, ModuleGenerator::finish() must be called to complete the
-// compilation and extract the resulting wasm module.
+
+
+
+
+
 
 class MOZ_STACK_CLASS ModuleGenerator {
   using CompileTaskVector = Vector<CompileTask, 0, SystemAllocPolicy>;
@@ -173,7 +173,7 @@ class MOZ_STACK_CLASS ModuleGenerator {
   };
   using CallFarJumpVector = Vector<CallFarJump, 0, SystemAllocPolicy>;
 
-  // Constant parameters
+  
   SharedCompileArgs const compileArgs_;
   UniqueChars* const error_;
   UniqueCharsVector* const warnings_;
@@ -181,12 +181,12 @@ class MOZ_STACK_CLASS ModuleGenerator {
   ModuleMetadata* const moduleMeta_;
   CompilerEnvironment* const compilerEnv_;
 
-  // Data that is moved into the result of finish()
+  
   UniqueLinkData linkData_;
   UniqueMetadataTier metadataTier_;
   MutableMetadata metadata_;
 
-  // Data scoped to the ModuleGenerator's lifetime
+  
   CompileTaskState taskState_;
   LifoAlloc lifo_;
   jit::TempAllocator masmAlloc_;
@@ -198,7 +198,7 @@ class MOZ_STACK_CLASS ModuleGenerator {
   uint32_t lastPatchedCallSite_;
   uint32_t startOfUnpatchedCallsites_;
 
-  // Parallel compilation
+  
   bool parallel_;
   uint32_t outstanding_;
   CompileTaskVector tasks_;
@@ -206,13 +206,8 @@ class MOZ_STACK_CLASS ModuleGenerator {
   CompileTask* currentTask_;
   uint32_t batchedBytecode_;
 
-  // Assertions
+  
   DebugOnly<bool> finishedFuncDefs_;
-
-  bool allocateInstanceDataBytes(uint32_t bytes, uint32_t align,
-                                 uint32_t* instanceDataOffset);
-  bool allocateInstanceDataBytesN(uint32_t bytes, uint32_t align,
-                                  uint32_t count, uint32_t* instanceDataOffset);
 
   bool funcIsCompiled(uint32_t funcIndex) const;
   const CodeRange& funcCodeRange(uint32_t funcIndex) const;
@@ -243,21 +238,21 @@ class MOZ_STACK_CLASS ModuleGenerator {
   ~ModuleGenerator();
   [[nodiscard]] bool init(Metadata* maybeAsmJSMetadata = nullptr);
 
-  // Before finishFuncDefs() is called, compileFuncDef() must be called once
-  // for each funcIndex in the range [0, env->numFuncDefs()).
+  
+  
 
   [[nodiscard]] bool compileFuncDef(
       uint32_t funcIndex, uint32_t lineOrBytecode, const uint8_t* begin,
       const uint8_t* end, Uint32Vector&& callSiteLineNums = Uint32Vector());
 
-  // Must be called after the last compileFuncDef() and before finishModule()
-  // or finishTier2().
+  
+  
 
   [[nodiscard]] bool finishFuncDefs();
 
-  // If env->mode is Once or Tier1, finishModule() must be called to generate
-  // a new Module. Otherwise, if env->mode is Tier2, finishTier2() must be
-  // called to augment the given Module with tier 2 code.
+  
+  
+  
 
   SharedModule finishModule(
       const ShareableBytes& bytecode,
@@ -265,7 +260,7 @@ class MOZ_STACK_CLASS ModuleGenerator {
   [[nodiscard]] bool finishTier2(const Module& module);
 };
 
-}  // namespace wasm
-}  // namespace js
+}  
+}  
 
-#endif  // wasm_generator_h
+#endif  
