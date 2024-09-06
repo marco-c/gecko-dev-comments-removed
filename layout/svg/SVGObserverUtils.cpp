4036c2cfe9f9d32594d7d8fb0232ea5ad24d82a1
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-06 13:37:58 +0000

    Bug 1896707: Improve consistency in RefPtr handling in SVGObserverUtils. r=layout-reviewers,emilio
    Specifically:
    - Use MakeRefPtr to help us ensure the new object is stored in a refcounted
      pointer ASAP (since initially storing a refcounted object in raw pointers can
      be a footgun).
    - Use std::move to transfer the existing reference when appending these objects
      to a nsTArrays-of-RefPtrs, to avoid an unnecessary AddRef/Release dance.
    
    Differential Revision: https://phabricator.services.mozilla.com/D210338
    
    UltraBlame original commit: c45f4c26637bcf4e3a9d3759b37853dc934a709c

--- layout/svg/SVGObserverUtils.cpp
+++ layout/svg/SVGObserverUtils.cpp
@@ -920,9 +920,9 @@ SVGFilterObserverList::SVGFilterObserverList(Span<const StyleFilter> aFilters,
       }
     }
 
-    RefPtr<SVGFilterObserver> observer =
-        new SVGFilterObserver(filterURL, aFilteredElement, this);
-    mObservers.AppendElement(observer);
+    auto observer =
+        MakeRefPtr<SVGFilterObserver>(filterURL, aFilteredElement, this);
+    mObservers.AppendElement(std::move(observer));
   }
 }
 
@@ -1026,9 +1026,9 @@ SVGMaskObserverList::SVGMaskObserverList(nsIFrame* aFrame) : mFrame(aFrame) {
     
     
     
-    SVGPaintingProperty* prop = new SVGPaintingProperty(
+    auto prop = MakeRefPtr<SVGPaintingProperty>(
         hasRef ? maskUri.get() : nullptr, aFrame, false);
-    mProperties.AppendElement(prop);
+    mProperties.AppendElement(std::move(prop));
   }
 }
 
