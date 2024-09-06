2ac4ad3893d210618b3eb81759b99a43b59b22d7
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-06 15:50:09 +0000

    Bug 1890899 - Make CrossShadowBoundaryRange an mutation observer to observe DOM nodes removal r=jjaschke,dom-core
    So that when a node is removed and this node is related to the start or
    the end point of the `CrossShadowBoundaryRange`, we can clear this
    `CrossShadowBoundaryRange`. We clear it for now because we aren't sure
    about what the new points should be.
    
    `nsRange` does a similar thing to `mRoot`, and we can't rely on `nsRange` to
    observer `mRoot` because `mRoot` could be root of a collapsed range, so
    it's not the root of `CrossShadowBoundaryRange`.
    
    Differential Revision: https://phabricator.services.mozilla.com/D211246
    
    UltraBlame original commit: 96c453489a51762c5e24b2511572a6d28e305690

--- dom/base/nsRange.cpp
+++ dom/base/nsRange.cpp
@@ -712,21 +712,6 @@ void nsRange::ContentRemoved(nsIContent* aChild, nsIContent* aPreviousSibling) {
   nsINode* startContainer = mStart.Container();
   nsINode* endContainer = mEnd.Container();
 
-  
-  
-  
-  if (mCrossShadowBoundaryRange) {
-    if (mCrossShadowBoundaryRange->GetStartContainer() == aChild ||
-        mCrossShadowBoundaryRange->GetEndContainer() == aChild) {
-      ResetCrossShadowBoundaryRange();
-    } else if (ShadowRoot* shadowRoot = aChild->GetShadowRoot()) {
-      if (mCrossShadowBoundaryRange->GetStartContainer() == shadowRoot ||
-          mCrossShadowBoundaryRange->GetEndContainer() == shadowRoot) {
-        ResetCrossShadowBoundaryRange();
-      }
-    }
-  }
-
   RawRangeBoundary newStart;
   RawRangeBoundary newEnd;
   Maybe<bool> gravitateStart;
@@ -3558,7 +3543,7 @@ void nsRange::CreateOrUpdateCrossShadowBoundaryRangeIfNeeded(
 
   if (!mCrossShadowBoundaryRange) {
     mCrossShadowBoundaryRange =
-        CrossShadowBoundaryRange::Create(aStartBoundary, aEndBoundary);
+        CrossShadowBoundaryRange::Create(aStartBoundary, aEndBoundary, this);
     return;
   }
 
