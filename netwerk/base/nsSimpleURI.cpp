6b764f64bda8c212dc70519afb496cf27a875464
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-06 17:41:57 +0000

    Bug 1874118 - Fix handling of trailing characters in opaque paths in URL r=valentin,necko-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D213036
    
    UltraBlame original commit: 5573d35897e644f1b6f31d462150ec0931dec5ce

--- netwerk/base/nsSimpleURI.cpp
+++ netwerk/base/nsSimpleURI.cpp
@@ -431,9 +431,13 @@ nsresult nsSimpleURI::SetPathQueryRefInternal(const nsACString& aPath) {
   const auto* queryEnd =
       std::find_if(pathEnd, end, [](char c) { return c == '#'; });
 
-  rv = SetQuery(Substring(pathEnd, queryEnd));
-  if (NS_FAILED(rv)) {
-    return rv;
+  
+  
+  if (pathEnd != end && *pathEnd == '?') {
+    rv = SetQuery(Substring(pathEnd, queryEnd));
+    if (NS_FAILED(rv)) {
+      return rv;
+    }
   }
 
   if (queryEnd == end) {
