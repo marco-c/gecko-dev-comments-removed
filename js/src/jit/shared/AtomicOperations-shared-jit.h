03a7accf2367f09998b648b19d4e77ca967f25b6
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-06 11:14:12 +0000

    Bug 1833647 - Add Float16Array; r=anba,nbp
    This adds the Float16Array implementation. This code is derived from an
    earlier patch written by Benjamin Dahse.
    
    Differential Revision: https://phabricator.services.mozilla.com/D203417
    
    UltraBlame original commit: a4640f7b9be76dc63388f118325f5c345579265e

--- js/src/jit/shared/AtomicOperations-shared-jit.h
+++ js/src/jit/shared/AtomicOperations-shared-jit.h
@@ -23,6 +23,7 @@
 #include <stdint.h>
 
 #include "jit/AtomicOperationsGenerated.h"
+#include "vm/Float16.h"
 #include "vm/Uint8Clamped.h"
 
 namespace js {
@@ -405,6 +406,14 @@ inline uint8_clamped js::jit::AtomicOperations::loadSafeWhenRacy(
   return uint8_clamped(loadSafeWhenRacy((uint8_t*)addr));
 }
 
+
+template <>
+inline float16 js::jit::AtomicOperations::loadSafeWhenRacy(float16* addr) {
+  float16 f16;
+  f16.val = loadSafeWhenRacy((uint16_t*)addr);
+  return f16;
+}
+
 }  
 }  
 
@@ -462,6 +471,13 @@ template <>
 inline void js::jit::AtomicOperations::storeSafeWhenRacy(uint8_clamped* addr,
                                                          uint8_clamped val) {
   storeSafeWhenRacy((uint8_t*)addr, (uint8_t)val);
+}
+
+
+template <>
+inline void js::jit::AtomicOperations::storeSafeWhenRacy(float16* addr,
+                                                         float16 val) {
+  storeSafeWhenRacy((uint16_t*)addr, val.val);
 }
 
 }  
