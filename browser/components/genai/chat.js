81186dc1a2a0f89cd4404df554939d8bd30c4586
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-06 19:36:51 +0000

    Bug 1903443 - Support providers that accept prompts via headers r=tarek
    Allow optional header configuration for providers to decide how to pass the prompt.
    
    Differential Revision: https://phabricator.services.mozilla.com/D214243
    
    UltraBlame original commit: 415bb50d0805d2cf04dc5464238b50d854bc5b5b

--- browser/components/genai/chat.js
+++ browser/components/genai/chat.js
@@ -87,11 +87,18 @@ function handleChange({ target }) {
       break;
   }
 }
-
-async function handleLoad() {
-  node.chat = renderChat();
-  node.provider = await renderProviders();
-}
-
 addEventListener("change", handleChange);
-addEventListener("load", handleLoad);
+
+
+var browserPromise = new Promise((resolve, reject) => {
+  addEventListener("load", async () => {
+    try {
+      node.chat = renderChat();
+      node.provider = await renderProviders();
+      resolve(node.chat);
+    } catch (ex) {
+      console.error("Failed to render on load", ex);
+      reject(ex);
+    }
+  });
+});
