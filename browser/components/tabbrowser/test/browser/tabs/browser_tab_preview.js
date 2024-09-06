4c664104d96996ad9d8bc21f7f829f8ab72bc20a
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-06 23:36:03 +0000

    Bug 1876558 - deactivate tab preview when urlbar receives input. r=tabbrowser-reviewers,dao,urlbar-reviewers,mak
    Differential Revision: https://phabricator.services.mozilla.com/D214870
    
    UltraBlame original commit: a5d60affe1089d275812db7569fea9d85ca19145

--- browser/components/tabbrowser/test/browser/tabs/browser_tab_preview.js
+++ browser/components/tabbrowser/test/browser/tabs/browser_tab_preview.js
@@ -20,7 +20,7 @@ async function openPreview(tab, win = window) {
     win.document.getElementById("tab-preview-panel"),
     "shown"
   );
-  EventUtils.synthesizeMouseAtCenter(tab, { type: "mouseover" }, win);
+  EventUtils.synthesizeMouse(tab, 1, 1, { type: "mouseover" }, win);
   return previewShown;
 }
 
@@ -540,6 +540,40 @@ add_task(async function panelSuppressionOnPanelTests() {
 
 
 
+add_task(async function urlBarInputTests() {
+  const previewElement = document.getElementById("tab-preview-panel");
+  const tab1 = await BrowserTestUtils.openNewForegroundTab(
+    gBrowser,
+    "about:blank"
+  );
+
+  await openPreview(tab1);
+  gURLBar.focus();
+  Assert.equal(previewElement.state, "open", "Preview is open");
+
+  let previewHidden = BrowserTestUtils.waitForEvent(
+    previewElement,
+    "popuphidden"
+  );
+  EventUtils.sendChar("q", window);
+  await previewHidden;
+
+  Assert.equal(previewElement.state, "closed", "Preview is closed");
+  await closePreviews();
+  await openPreview(tab1);
+  Assert.equal(previewElement.state, "open", "Preview is open");
+
+  previewHidden = BrowserTestUtils.waitForEvent(previewElement, "popuphidden");
+  EventUtils.sendChar("q", window);
+  await previewHidden;
+  Assert.equal(previewElement.state, "closed", "Preview is closed");
+
+  BrowserTestUtils.removeTab(tab1);
+});
+
+
+
+
 
 add_task(async function wheelTests() {
   const previewPanel = document.getElementById("tab-preview-panel");
@@ -553,9 +587,14 @@ add_task(async function wheelTests() {
     "Panel does not have rolluponmousewheel when no overflow"
   );
 
-  await BrowserTestUtils.overflowTabs(registerCleanupFunction, window, {
+  let scrollOverflowEvent = BrowserTestUtils.waitForEvent(
+    document.getElementById("tabbrowser-arrowscrollbox"),
+    "overflow"
+  );
+  BrowserTestUtils.overflowTabs(registerCleanupFunction, window, {
     overflowAtStart: false,
   });
+  await scrollOverflowEvent;
   await openPreview(tab1);
 
   Assert.equal(
