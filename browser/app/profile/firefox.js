1fa123d51578531d8449d58d47800028816493c7
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-06 21:39:24 +0000

    Bug 1906138 - Home and newtab turn on merino recs provider for nightly only r=home-newtab-reviewers,nbarrett
    Differential Revision: https://phabricator.services.mozilla.com/D215651
    
    UltraBlame original commit: 4a3c2126700b70c3a595eeac500027dd539cc655

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1836,7 +1836,12 @@ pref("browser.newtabpage.activity-stream.discoverystream.region-stories-block", 
 pref("browser.newtabpage.activity-stream.discoverystream.region-stories-config", "US,DE,CA,GB,IE,CH,AT,BE,IN,FR,IT,ES");
 
 pref("browser.newtabpage.activity-stream.discoverystream.region-bff-config", "US,DE,CA,GB,IE,CH,AT,BE,IN,FR,IT,ES");
-pref("browser.newtabpage.activity-stream.discoverystream.merino-provider.enabled", false);
+
+#ifdef NIGHTLY_BUILD
+  pref("browser.newtabpage.activity-stream.discoverystream.merino-provider.enabled", true);
+#else
+  pref("browser.newtabpage.activity-stream.discoverystream.merino-provider.enabled", false);
+#endif
 pref("browser.newtabpage.activity-stream.discoverystream.merino-provider.endpoint", "merino.services.mozilla.com");
 
 pref("browser.newtabpage.activity-stream.discoverystream.region-spocs-config", "US,CA,DE,GB,FR,IT,ES");
--- browser/components/newtab/test/xpcshell/test_AboutHomeStartupCacheWorker.js
+++ browser/components/newtab/test/xpcshell/test_AboutHomeStartupCacheWorker.js
@@ -89,6 +89,13 @@ add_setup(async function () {
     "browser.newtabpage.activity-stream.newtabWallpapers.v2.enabled",
     false
   );
+  
+  
+  
+  Services.prefs.setBoolPref(
+    "browser.newtabpage.activity-stream.discoverystream.merino-provider.enabled",
+    false
+  );
 
   let defaultDSConfig = JSON.parse(
     PREFS_CONFIG.get("discoverystream.config").getValue({
