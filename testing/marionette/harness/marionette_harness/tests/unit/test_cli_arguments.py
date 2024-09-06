cc91b00169241ceac79cade920ca9d9f140f1ec7
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-06 17:43:39 +0000

    Bug 1882089 - [remote] Update tests for explictly enabling the CDP protocol if needed. r=webdriver-reviewers,jdescottes
    Differential Revision: https://phabricator.services.mozilla.com/D212908
    
    UltraBlame original commit: c094d12577169623d1045d20493a5c4b784c1bab

--- testing/marionette/harness/marionette_harness/tests/unit/test_cli_arguments.py
+++ testing/marionette/harness/marionette_harness/tests/unit/test_cli_arguments.py
@@ -29,7 +29,22 @@ class TestCommandLineArguments(MarionetteTestCase):
         self.assertIsNone(debugger_address)
 
         
-        self.marionette.set_pref("remote.active-protocols", 1)
+        self.marionette.set_pref("remote.active-protocols", 2)
+        self.marionette.quit()
+
+        self.marionette.instance.app_args.append("-remote-debugging-port")
+        self.marionette.start_session()
+
+        debugger_address = self.marionette.session_capabilities.get(
+            "moz:debuggerAddress"
+        )
+
+        self.assertEqual(debugger_address, "127.0.0.1:9222")
+        result = requests.get(url="http://{}/json/version".format(debugger_address))
+        self.assertTrue(result.ok)
+
+        
+        self.marionette.clear_pref("remote.active-protocols")
         self.marionette.quit()
 
         self.marionette.instance.app_args.append("-remote-debugging-port")
@@ -42,16 +57,6 @@ class TestCommandLineArguments(MarionetteTestCase):
 
         
         self.marionette.quit(in_app=False, clean=True)
-
-        
-        self.marionette.start_session()
-        debugger_address = self.marionette.session_capabilities.get(
-            "moz:debuggerAddress"
-        )
-
-        self.assertEqual(debugger_address, "127.0.0.1:9222")
-        result = requests.get(url="http://{}/json/version".format(debugger_address))
-        self.assertTrue(result.ok)
 
     def test_websocket_url(self):
         
@@ -67,9 +72,9 @@ class TestCommandLineArguments(MarionetteTestCase):
         self.assertNotIn("webSocketUrl", self.marionette.session_capabilities)
 
         
-        self.marionette.quit(in_app=False, clean=True)
-
-        
+        self.marionette.clear_pref("remote.active-protocols")
+        self.marionette.quit()
+        self.marionette.instance.app_args.append("-remote-debugging-port")
         self.marionette.start_session({"webSocketUrl": True})
 
         session_id = self.marionette.session_id
@@ -78,6 +83,9 @@ class TestCommandLineArguments(MarionetteTestCase):
         self.assertEqual(
             websocket_url, "ws://127.0.0.1:9222/session/{}".format(session_id)
         )
+
+        
+        self.marionette.quit(in_app=False, clean=True)
 
     
     
