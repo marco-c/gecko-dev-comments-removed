4199ab6afb122230627cd8423af70e3f9537b1eb
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-06 15:17:54 +0000

    Bug 1896681 - Home and newtab adding region control for weather widget r=home-newtab-reviewers,amy
    Differential Revision: https://phabricator.services.mozilla.com/D211146
    
    UltraBlame original commit: 268ec529801376871b0c698e28d1b14366deaccc

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1696,12 +1696,13 @@ pref("browser.partnerlink.campaign.topsites", "amzn_2020_a1");
 pref("browser.newtab.preload", true);
 
 
-pref("browser.newtabpage.activity-stream.system.showWeather", false);
 pref("browser.newtabpage.activity-stream.showWeather", true);
 pref("browser.newtabpage.activity-stream.weather.query", "");
 pref("browser.newtabpage.activity-stream.weather.locationSearchEnabled", false);
 pref("browser.newtabpage.activity-stream.weather.temperatureUnits", "f");
 pref("browser.newtabpage.activity-stream.weather.display", "simple");
+
+pref("browser.newtabpage.activity-stream.discoverystream.region-weather-config", "");
 
 
 pref("browser.newtabpage.activity-stream.newtabWallpapers.enabled", false);
