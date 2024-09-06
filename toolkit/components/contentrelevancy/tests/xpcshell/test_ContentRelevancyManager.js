beb4f1cf15626c2f9c923feab0b5f7bf02bedff9
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-06 15:18:58 +0000

    Bug 1894028 - construct relevancy store at startup. r=nanj
    The new lifetime for the store is:
    
      - `init` constructs it, however it can not be used yet.
      - `#enable` flags it as ready to be used.
      - `#disable` closes any DB connections, then flags it as not ready.
      - `uninit` destroys the it.
    
    Created a new class to hold the `RustRelevancyStore`.  This allows us to
    test the logic there without needing to fiddle with prefs, nimbus, or
    timers.
    
    Differential Revision: https://phabricator.services.mozilla.com/D211254
    
    UltraBlame original commit: 825b6fd697fcddc13b4648d0bf494b4b05dd790a

--- toolkit/components/contentrelevancy/tests/xpcshell/test_ContentRelevancyManager.js
+++ toolkit/components/contentrelevancy/tests/xpcshell/test_ContentRelevancyManager.js
@@ -7,6 +7,7 @@ ChromeUtils.defineESModuleGetters(this, {
   AsyncShutdown: "resource://gre/modules/AsyncShutdown.sys.mjs",
   ContentRelevancyManager:
     "resource://gre/modules/ContentRelevancyManager.sys.mjs",
+  exposedForTesting: "resource://gre/modules/ContentRelevancyManager.sys.mjs",
   TestUtils: "resource://testing-common/TestUtils.sys.mjs",
   setTimeout: "resource://gre/modules/Timer.sys.mjs",
   sinon: "resource://testing-common/Sinon.sys.mjs",
@@ -44,6 +45,35 @@ add_task(function test_uninit() {
   ContentRelevancyManager.uninit();
 
   Assert.ok(!ContentRelevancyManager.initialized, "Uninit should succeed");
+});
+
+add_task(function test_store_manager() {
+  const RustRelevancyStoreManager = exposedForTesting.RustRelevancyStoreManager;
+  const fakeStore = {
+    close: gSandbox.fake(),
+  };
+  const fakeRustRelevancyStore = {
+    init: gSandbox.fake.returns(fakeStore),
+  };
+  const storeManager = new RustRelevancyStoreManager(
+    "test-path",
+    fakeRustRelevancyStore
+  );
+  Assert.equal(fakeRustRelevancyStore.init.callCount, 1);
+  Assert.deepEqual(fakeRustRelevancyStore.init.firstCall.args, ["test-path"]);
+  
+  Assert.throws(() => storeManager.store, /StoreDisabledError/);
+  
+  storeManager.enable();
+  Assert.equal(storeManager.store, fakeStore);
+  
+  Assert.equal(fakeStore.close.callCount, 0);
+  storeManager.disable();
+  Assert.equal(fakeStore.close.callCount, 1);
+  Assert.throws(() => storeManager.store, /StoreDisabledError/);
+  
+  storeManager.enable();
+  Assert.equal(storeManager.store, fakeStore);
 });
 
 add_task(async function test_timer() {
