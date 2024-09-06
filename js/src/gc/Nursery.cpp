4f34ec4e3f4d799e7c7ad2affd0d2f6b80623537
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-06 12:30:21 +0000

    Bug 1894604: Adjust assertions in requestMinorGC r=jonco
    With this change, the only difference between the assertions here and StoreBuffer::checkAccess is that requestMinorGC will assert if the HeapState is Tracing or CycleCollecting. If we wanted to relax that restriction, we could just call StoreBuffer::checkAccess here instead of duplicating the logic.
    
    Differential Revision: https://phabricator.services.mozilla.com/D209437
    
    UltraBlame original commit: 43ddb2f11b36f37b92eb14c1bbfd6db0790e3a96

--- js/src/gc/Nursery.cpp
+++ js/src/gc/Nursery.cpp
@@ -1813,7 +1813,7 @@ void Nursery::requestMinorGC(JS::GCReason reason) {
   } else if (heapState == JS::HeapState::MajorCollecting) {
     
     
-    MOZ_ASSERT(CurrentThreadIsGCSweeping());
+    MOZ_ASSERT(!CurrentThreadIsGCMarking());
     runtime()->gc.assertCurrentThreadHasLockedStoreBuffer();
   } else {
     MOZ_CRASH("Unexpected heap state");
