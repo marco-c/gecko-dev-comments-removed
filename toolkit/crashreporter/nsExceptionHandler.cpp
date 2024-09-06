c13af88cb2c8e34826895c5a5d0254867ab4abc7
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-06 09:33:02 +0000

    Bug 1890930 - Initialize fstreams from a std::filesystem::path on Windows. r=gsvelto
    Differential Revision: https://phabricator.services.mozilla.com/D207245
    
    UltraBlame original commit: 30d1890a8dcbc7342f4b10e3d728273280f2e27a

--- toolkit/crashreporter/nsExceptionHandler.cpp
+++ toolkit/crashreporter/nsExceptionHandler.cpp
@@ -96,6 +96,9 @@
 using mozilla::InjectCrashRunnable;
 #endif
 
+#ifdef XP_WIN
+#  include <filesystem>
+#endif
 #include <fstream>
 #include <optional>
 
@@ -2773,7 +2776,17 @@ static nsresult PrefSubmitReports(bool* aSubmitReports, bool writePref) {
   reporterSettings->AppendNative("Crash Reports"_ns);
   reporterSettings->AppendNative("crashreporter_settings.json"_ns);
 
+  
+  
+  
+  
+  
+#  ifdef XP_WIN
+  std::optional<std::filesystem::path> file_path =
+      CreatePathFromFile(reporterSettings);
+#  else
   std::optional<xpstring> file_path = CreatePathFromFile(reporterSettings);
+#  endif
 
   if (!file_path) {
     return NS_ERROR_FAILURE;
