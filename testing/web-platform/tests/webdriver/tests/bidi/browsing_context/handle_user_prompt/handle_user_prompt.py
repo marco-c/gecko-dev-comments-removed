72cdac5f1ce8564ee08d34dce59d4501c1fd7b92
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-06 21:01:35 +0000

    Bug 1904576 [wpt PR 46895] - [WebDriver BiDi] missing `unhandledPromptBehavior`, a=testonly
    Automatic update from web-platform-tests
    [WebDriver BiDi] missing `unhandledPromptBehavior` (#46895)
    
    Provide missing `unhandledPromptBehavior` capabilities to override default `dismiss` behavior and allow for prompt handling tests.
    --
    
    wpt-commits: 1d4b4f0c274587133dc1eae7429c617fc2961ed4
    wpt-pr: 46895
    
    UltraBlame original commit: 63d9a6a339700d1e4d7f6a994b7c2b3f912e5174

--- testing/web-platform/tests/webdriver/tests/bidi/browsing_context/handle_user_prompt/handle_user_prompt.py
+++ testing/web-platform/tests/webdriver/tests/bidi/browsing_context/handle_user_prompt/handle_user_prompt.py
@@ -3,7 +3,6 @@ import pytest
 
 import webdriver.bidi.error as error
 from webdriver.bidi.modules.script import ContextTarget
-from webdriver.error import TimeoutException
 
 
 pytestmark = pytest.mark.asyncio
@@ -12,6 +11,7 @@ USER_PROMPT_CLOSED_EVENT = "browsingContext.userPromptClosed"
 USER_PROMPT_OPENED_EVENT = "browsingContext.userPromptOpened"
 
 
+@pytest.mark.capabilities({"unhandledPromptBehavior": {'default': 'ignore'}})
 async def test_alert(
     bidi_session, wait_for_event, wait_for_future_safe, new_tab, subscribe_events
 ):
@@ -38,6 +38,7 @@ async def test_alert(
     assert result == {"type": "undefined"}
 
 
+@pytest.mark.capabilities({"unhandledPromptBehavior": {'default': 'ignore'}})
 @pytest.mark.parametrize("accept", [True, False])
 async def test_confirm(
     bidi_session,
@@ -72,6 +73,7 @@ async def test_confirm(
     assert result == {"type": "boolean", "value": accept}
 
 
+@pytest.mark.capabilities({"unhandledPromptBehavior": {'default': 'ignore'}})
 @pytest.mark.parametrize("accept", [True, False])
 async def test_prompt(
     bidi_session,
@@ -110,7 +112,9 @@ async def test_prompt(
         assert result == {"type": "null"}
 
 
-@pytest.mark.parametrize("accept", [True, False])
+@pytest.mark.capabilities({"unhandledPromptBehavior": {'default': 'ignore'}})
+@pytest.mark.parametrize("accept", [False])
+
 async def test_beforeunload(
     bidi_session,
     subscribe_events,
@@ -138,7 +142,7 @@ async def test_beforeunload(
     )
 
     
-    event = await wait_for_future_safe(on_entry)
+    await wait_for_future_safe(on_entry)
 
     on_prompt_closed = wait_for_event(USER_PROMPT_CLOSED_EVENT)
 
@@ -147,7 +151,7 @@ async def test_beforeunload(
     )
 
     
-    event = await wait_for_future_safe(on_prompt_closed)
+    await wait_for_future_safe(on_prompt_closed)
 
     if accept:
         
@@ -155,8 +159,10 @@ async def test_beforeunload(
         assert navigated["url"] == page_target
     else:
         
-        with pytest.raises(TimeoutException):
-            await wait_for_future_safe(navigated_future, timeout=0.5)
+        
+        
+        with pytest.raises(error.UnknownErrorException):
+            await wait_for_future_safe(navigated_future)
 
         contexts = await bidi_session.browsing_context.get_tree(
             root=new_tab["context"], max_depth=0
@@ -164,6 +170,7 @@ async def test_beforeunload(
         assert contexts[0]["url"] != page_target
 
 
+@pytest.mark.capabilities({"unhandledPromptBehavior": {'default': 'ignore'}})
 @pytest.mark.parametrize("type_hint", ["tab", "window"])
 async def test_two_top_level_contexts(
     bidi_session,
@@ -201,6 +208,7 @@ async def test_two_top_level_contexts(
     await bidi_session.browsing_context.close(context=new_context["context"])
 
 
+@pytest.mark.capabilities({"unhandledPromptBehavior": {'default': 'ignore'}})
 async def test_multiple_frames(
     bidi_session,
     new_tab,
