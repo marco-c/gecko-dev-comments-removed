09a1edcde7b44dd1ab2109d7b418d80187866ecf
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-06 20:30:05 +0000

    Bug 1903791 - [devtools] Increase max-depth tracer limit and make it configurable via a preference. r=devtools-reviewers,nchevobbe
    When a JS stack is longer than this 200 limit, the tracer will automatically stop and consider
    the current execution as an infinite loop.
    
    Differential Revision: https://phabricator.services.mozilla.com/D214459
    
    UltraBlame original commit: 39e9da228aad65fca1c156f847a1db812da3554f

--- devtools/server/tracer/tests/xpcshell/test_tracer.js
+++ devtools/server/tracer/tests/xpcshell/test_tracer.js
@@ -563,3 +563,56 @@ add_task(async function testTracingAllGlobals() {
   Assert.stringContains(logs[2], "sandbox2.js:1:18");
   Assert.equal(logs[3], "Stop tracing JavaScript\n");
 });
+
+add_task(async function testTracingInfiniteLoop() {
+  
+  Services.prefs.setIntPref(
+    "devtools.debugger.javascript-tracing-max-depth",
+    5
+  );
+
+  
+  const sandbox = Cu.Sandbox("https://example.com");
+  Cu.evalInSandbox(
+    `function foo(i=0) { if (i < 10) bar(i); } function bar(i) { foo(i+1) };`,
+    sandbox
+  );
+
+  
+  const logs = [];
+  function loggingMethod(str) {
+    logs.push(str);
+  }
+
+  info("Start tracing");
+  JSTracer.startTracing({
+    global: sandbox,
+    loggingMethod,
+  });
+
+  info("Call some code");
+  sandbox.foo();
+
+  Assert.equal(logs.length, 8);
+  Assert.equal(logs[0], "Start tracing JavaScript\n");
+
+  
+  Assert.stringContains(logs[1], "λ foo");
+  Assert.stringContains(logs[2], "λ bar");
+  Assert.stringContains(logs[3], "λ foo");
+  Assert.stringContains(logs[4], "λ bar");
+  Assert.stringContains(logs[5], "λ foo");
+
+  Assert.stringContains(logs[6], "Looks like an infinite recursion");
+  Assert.stringContains(
+    logs[7],
+    "Stop tracing JavaScript (reason: infinite-loop)"
+  );
+
+  info("Stop tracing");
+  JSTracer.stopTracing();
+
+  Services.prefs.clearUserPref(
+    "devtools.debugger.javascript-tracing-max-depth"
+  );
+}); 
