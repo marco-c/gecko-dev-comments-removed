e1ae1f54e4ffd0636cdfb1bdd6dced35d7da3248
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-06 12:36:07 +0000

    Bug 1892659 - Add an all tabs menu item to deduplicate all open tabs r=dao,emilio,fluent-reviewers,tabbrowser-reviewers,flod
    Differential Revision: https://phabricator.services.mozilla.com/D208546
    
    UltraBlame original commit: 4c7b1942b455d9c365693e5584f7ea86c5b33615

--- browser/base/content/tabbrowser.js
+++ browser/base/content/tabbrowser.js
@@ -162,6 +162,7 @@
       TO_END: 3,
       MULTI_SELECTED: 4,
       DUPLICATES: 6,
+      ALL_DUPLICATES: 7,
     },
 
     _lastRelatedTabMap: new WeakMap(),
@@ -399,6 +400,37 @@
         }
       }
 
+      return duplicateTabs;
+    },
+
+    getAllDuplicateTabsToClose() {
+      let lastSeenTabs = this.tabs.sort(
+        (a, b) => b.lastSeenActive - a.lastSeenActive
+      );
+      let duplicateTabs = [];
+      let keys = [];
+      for (let tab of lastSeenTabs) {
+        const uri = tab.linkedBrowser?.currentURI;
+        if (!uri) {
+          
+          
+          continue;
+        }
+
+        const key = {
+          uri,
+          userContextId: tab.userContextId,
+        };
+        if (
+          !tab.pinned &&
+          keys.some(
+            k => k.userContextId == key.userContextId && k.uri.equals(key.uri)
+          )
+        ) {
+          duplicateTabs.push(tab);
+        }
+        keys.push(key);
+      }
       return duplicateTabs;
     },
 
@@ -3311,6 +3343,24 @@
         return true;
       }
 
+      const shownDupeDialogPref =
+        "browser.tabs.haveShownCloseAllDuplicateTabsWarning";
+      if (
+        aCloseTabs == this.closingTabsEnum.ALL_DUPLICATES &&
+        !Services.prefs.getBoolPref(shownDupeDialogPref, false)
+      ) {
+        
+        
+        Services.prefs.setBoolPref(shownDupeDialogPref, true);
+
+        window.focus();
+        const [title, text] = this.tabLocalization.formatValuesSync([
+          { id: "tabbrowser-confirm-close-duplicate-tabs-title" },
+          { id: "tabbrowser-confirm-close-duplicate-tabs-text" },
+        ]);
+        return Services.prompt.confirm(window, title, text);
+      }
+
       const pref =
         aCloseTabs == this.closingTabsEnum.ALL
           ? "browser.tabs.warnOnClose"
@@ -3551,25 +3601,39 @@
     },
 
     removeDuplicateTabs(aTab) {
-      let tabs = this.getDuplicateTabsToClose(aTab);
+      this._removeDuplicateTabs(
+        aTab,
+        this.getDuplicateTabsToClose(aTab),
+        this.closingTabsEnum.DUPLICATES
+      );
+    },
+
+    _removeDuplicateTabs(aConfirmationAnchor, tabs, aCloseTabs) {
       if (!tabs.length) {
         return;
       }
 
-      if (
-        !this.warnAboutClosingTabs(tabs.length, this.closingTabsEnum.DUPLICATES)
-      ) {
+      if (!this.warnAboutClosingTabs(tabs.length, aCloseTabs)) {
         return;
       }
 
       this.removeTabs(tabs);
-      if (tabs.length) {
-        ConfirmationHint.show(aTab, "confirmation-hint-duplicate-tabs-closed", {
-          l10nArgs: {
-            tabCount: tabs.length,
-          },
-        });
-      }
+      ConfirmationHint.show(
+        aConfirmationAnchor,
+        "confirmation-hint-duplicate-tabs-closed",
+        { l10nArgs: { tabCount: tabs.length } }
+      );
+    },
+
+    removeAllDuplicateTabs() {
+      
+      
+      let alltabsButton = document.getElementById("alltabs-button");
+      this._removeDuplicateTabs(
+        alltabsButton,
+        this.getAllDuplicateTabsToClose(),
+        this.closingTabsEnum.ALL_DUPLICATES
+      );
     },
 
     
