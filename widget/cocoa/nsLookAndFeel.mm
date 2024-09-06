a053290b78ec88dcbaac7df44cf4a88d1618d521
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-06 07:22:54 +0000

    Bug 1888862 - Wash the selected item color a little bit on macOS. r=mac-reviewers,bradwerth
    This is closer to what the native menus do.
    
    Ideally, we'd want "the color you use for native menus without desktop
    tinting and transparency", but there's no way to get that
    (NSColor.selectedMenuItemColor would ideally be that but it doesn't do
    what we want, it doesn't even match the user's accent color).
    
    So this should be good enough, and looks way better. Tested on both
    light and dark mode, for all the different accent colors.
    
    Differential Revision: https://phabricator.services.mozilla.com/D206204
    
    UltraBlame original commit: 5c3cdca8a429b1100cc332128c04553969e52d60

--- widget/cocoa/nsLookAndFeel.mm
+++ widget/cocoa/nsLookAndFeel.mm
@@ -69,7 +69,7 @@ void nsLookAndFeel::RefreshImpl() {
 
 static nscolor GetColorFromNSColor(NSColor* aColor) {
   NSColor* deviceColor =
-      [aColor colorUsingColorSpace:[NSColorSpace deviceRGBColorSpace]];
+      [aColor colorUsingColorSpace:NSColorSpace.deviceRGBColorSpace];
   return NS_RGBA((unsigned int)(deviceColor.redComponent * 255.0),
                  (unsigned int)(deviceColor.greenComponent * 255.0),
                  (unsigned int)(deviceColor.blueComponent * 255.0),
@@ -156,7 +156,11 @@ nsresult nsLookAndFeel::NativeGetColor(ColorID aID, ColorScheme aScheme,
       break;
     case ColorID::MozMenuhover:
     case ColorID::Selecteditem:
-      color = GetColorFromNSColor(NSColor.selectedContentBackgroundColor);
+      
+      
+      color = NS_ComposeColors(
+          GetColorFromNSColor(NSColor.selectedContentBackgroundColor),
+          NS_RGBA(255, 255, 255, 51));
       break;
     case ColorID::Accentcolortext:
     case ColorID::MozMenuhovertext:
