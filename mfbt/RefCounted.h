61d4751ecaf73f931f6d68aabf286af88341b5f9
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-06 20:24:45 +0000

    Bug 1903037 part 1 - Support leak checking in MFBT code when included in SpiderMonkey. r=mccr8,glandium
    `mozilla::StringBuffer` uses `RefCountLogger`, but this was a no-op when included in
    SpiderMonkey code (`MOZILLA_INTERNAL_API` is not defined in that case). This resulted
    in leakcheck failures when string buffers are used directly in SpiderMonkey.
    
    This patch changes the calls to `NS_LogAddRef` and `NS_LogRelease` to go through a
    function pointer. This also makes it possible to use a different implementation in
    SpiderMonkey shell builds if we ever want to.
    
    Differential Revision: https://phabricator.services.mozilla.com/D213968
    
    UltraBlame original commit: cef762ba8c2c1ab847c560e80cb8cba548cf340b

--- mfbt/RefCounted.h
+++ mfbt/RefCounted.h
@@ -24,18 +24,11 @@
 #  include <atomic>
 #endif  
 
-#if defined(MOZILLA_INTERNAL_API)
-#  include "nsXPCOM.h"
-#endif
-
-#if defined(MOZILLA_INTERNAL_API) && defined(NS_BUILD_REFCNT_LOGGING)
+#if defined(MOZ_SUPPORT_LEAKCHECKING) && defined(NS_BUILD_REFCNT_LOGGING)
 #  define MOZ_REFCOUNTED_LEAK_CHECKING
 #endif
 
 namespace mozilla {
-
-
-
 
 
 
@@ -68,6 +61,20 @@ const MozRefCountType DEAD = 0xffffdead;
 
 
 class RefCountLogger {
+#ifdef MOZ_REFCOUNTED_LEAK_CHECKING
+  
+  
+  
+  using LogAddRefFunc = void (*)(void* aPtr, MozRefCountType aNewRefCnt,
+                                 const char* aTypeName, uint32_t aClassSize);
+  using LogReleaseFunc = void (*)(void* aPtr, MozRefCountType aNewRefCnt,
+                                  const char* aTypeName);
+  static MFBT_DATA LogAddRefFunc sLogAddRefFunc;
+  static MFBT_DATA LogReleaseFunc sLogReleaseFunc;
+  static MFBT_DATA size_t sNumStaticCtors;
+  static MFBT_DATA const char* sLastStaticCtorTypeName;
+#endif
+
  public:
   
   
@@ -78,9 +85,19 @@ class RefCountLogger {
     const void* pointer = aPointer;
     const char* typeName = aPointer->typeName();
     uint32_t typeSize = aPointer->typeSize();
-    NS_LogAddRef(const_cast<void*>(pointer), aRefCount, typeName, typeSize);
+    if (sLogAddRefFunc) {
+      sLogAddRefFunc(const_cast<void*>(pointer), aRefCount, typeName, typeSize);
+    } else {
+      sNumStaticCtors++;
+      sLastStaticCtorTypeName = typeName;
+    }
 #endif
   }
+
+#ifdef MOZ_REFCOUNTED_LEAK_CHECKING
+  static MFBT_DATA void SetLeakCheckingFunctions(
+      LogAddRefFunc aLogAddRefFunc, LogReleaseFunc aLogReleaseFunc);
+#endif
 
   
   
@@ -100,7 +117,12 @@ class RefCountLogger {
     void logRelease(MozRefCountType aRefCount) {
 #ifdef MOZ_REFCOUNTED_LEAK_CHECKING
       MOZ_ASSERT(aRefCount != DEAD);
-      NS_LogRelease(const_cast<void*>(mPointer), aRefCount, mTypeName);
+      if (sLogReleaseFunc) {
+        sLogReleaseFunc(const_cast<void*>(mPointer), aRefCount, mTypeName);
+      } else {
+        sNumStaticCtors++;
+        sLastStaticCtorTypeName = mTypeName;
+      }
 #endif
     }
 
