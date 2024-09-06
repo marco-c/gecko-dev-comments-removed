61475903d85d529d6a701f5e72d9cbbe4d89bc8c
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-06 15:51:08 +0000

    Backed out changeset 96c453489a51 (bug 1890899) for causing crashtests in dom/base/crashtests/1890899.html. CLOSED TREE
    UltraBlame original commit: d9d405ecdd8f87020fbb8b513b06ca15209f8006

--- dom/base/nsRange.cpp
+++ dom/base/nsRange.cpp
@@ -712,6 +712,21 @@ void nsRange::ContentRemoved(nsIContent* aChild, nsIContent* aPreviousSibling) {
   nsINode* startContainer = mStart.Container();
   nsINode* endContainer = mEnd.Container();
 
+  
+  
+  
+  if (mCrossShadowBoundaryRange) {
+    if (mCrossShadowBoundaryRange->GetStartContainer() == aChild ||
+        mCrossShadowBoundaryRange->GetEndContainer() == aChild) {
+      ResetCrossShadowBoundaryRange();
+    } else if (ShadowRoot* shadowRoot = aChild->GetShadowRoot()) {
+      if (mCrossShadowBoundaryRange->GetStartContainer() == shadowRoot ||
+          mCrossShadowBoundaryRange->GetEndContainer() == shadowRoot) {
+        ResetCrossShadowBoundaryRange();
+      }
+    }
+  }
+
   RawRangeBoundary newStart;
   RawRangeBoundary newEnd;
   Maybe<bool> gravitateStart;
@@ -3543,7 +3558,7 @@ void nsRange::CreateOrUpdateCrossShadowBoundaryRangeIfNeeded(
 
   if (!mCrossShadowBoundaryRange) {
     mCrossShadowBoundaryRange =
-        CrossShadowBoundaryRange::Create(aStartBoundary, aEndBoundary, this);
+        CrossShadowBoundaryRange::Create(aStartBoundary, aEndBoundary);
     return;
   }
 
