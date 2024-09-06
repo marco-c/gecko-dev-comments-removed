ab8d1ddc941195271b6b0610cd011528b34d0b0e
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-06 15:21:30 +0000

    Bug 1860457 - implement popover invoke defaults r=dom-core,sefeng
    Differential Revision: https://phabricator.services.mozilla.com/D206184
    
    UltraBlame original commit: 4cce744a9cbbae76f1229f5e7f484d8873699d14

--- dom/base/nsINode.h
+++ dom/base/nsINode.h
@@ -544,6 +544,8 @@ class nsINode : public mozilla::dom::EventTarget {
 
   mozilla::dom::Element* GetNearestInclusiveTargetPopoverForInvoker() const;
 
+  nsGenericHTMLElement* GetEffectiveInvokeTargetElement() const;
+
   
 
 
