a070d1864e25e05a683e66cf3ff5e5285cf1d6d2
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-06 14:14:34 +0000

    Bug 1894012 - Part 3: Generate alloc sites for nursery allocations that don't have them in baseline code r=jandem
    This adds a new kind of 'missing' allocation site and uses them to replace the
    unknown alloction site when used from baseline. These count allocations and
    promotions like normal sites but do not actually pretenure anything. Since this
    requires extra machinery it is only supported in GC zeal builds.
    
    This feature is enabled by setting the generateMissingAllocSites GC pref.
    
    Differential Revision: https://phabricator.services.mozilla.com/D210507
    
    UltraBlame original commit: b1644f447bbe9c7989676030a063f72b6626fa0d

--- js/src/gc/Zone.h
+++ js/src/gc/Zone.h
@@ -25,6 +25,7 @@
 #include "gc/FindSCCs.h"
 #include "gc/GCMarker.h"
 #include "gc/NurseryAwareHashMap.h"
+#include "gc/Policy.h"
 #include "gc/Pretenuring.h"
 #include "gc/Statistics.h"
 #include "gc/ZoneAllocator.h"
@@ -66,6 +67,23 @@ class ZoneAllCellIter;
 
 template <typename T>
 class ZoneCellIter;
+
+#ifdef JS_GC_ZEAL
+
+class MissingAllocSites {
+ public:
+  using SiteMap = JS::GCHashMap<uint32_t, UniquePtr<AllocSite>,
+                                DefaultHasher<uint32_t>, SystemAllocPolicy>;
+
+  using ScriptMap = JS::GCHashMap<WeakHeapPtr<JSScript*>, SiteMap,
+                                  StableCellHasher<WeakHeapPtr<JSScript*>>,
+                                  SystemAllocPolicy>;
+  WeakCache<ScriptMap> scriptMap;
+
+  explicit MissingAllocSites(JS::Zone* zone) : scriptMap(zone) {}
+};
+
+#endif  
 
 }  
 
@@ -483,6 +501,11 @@ class Zone : public js::ZoneAllocator, public js::gc::GraphNodeBase<JS::Zone> {
   js::MainThreadOrGCTaskData<ObjectVector> objectsWithWeakPointers;
 
  public:
+#ifdef JS_GC_ZEAL
+  
+  js::UniquePtr<js::gc::MissingAllocSites> missingSites;
+#endif  
+
   static JS::Zone* from(ZoneAllocator* zoneAlloc) {
     return static_cast<Zone*>(zoneAlloc);
   }
