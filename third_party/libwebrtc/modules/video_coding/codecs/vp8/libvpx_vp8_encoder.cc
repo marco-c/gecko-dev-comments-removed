c71a09b6de8d7834b859e76a6eeafbc5051a8be2
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-06 13:48:46 +0000

    Bug 1892299 - Vendor libwebrtc from 2a3db3131d
    Upstream commit: https://webrtc.googlesource.com/src/+/2a3db3131d4d6f4a51831a0c19f5da3ebbd9d92e
        Disable Android specific threading settings in libvpx VP8 encoder
    
        It used up to 3 threads for QVGA on Android before. This change disables Android-specific code path in NumberOfThreads() and uses the generic settings, which configure 1 thread for resolutions <=VGA, instead. The change is guarded by a killswitch.
    
        For reference, frame encode time for VGA 512kbps using 1 thread on Pixel 2 (7 years old device; SD835) is ~5.5ms: https://chromeperf.appspot.com/report?sid=6e80c701ef6ff0d008a299fb122a16f0d2600ddfcd9981d3d75cd722c92b2869
    
        Bug: webrtc:15828, b/316494683
        Change-Id: I0e9571ede64c6cb77d529d21ccb0310ccb8bfdaf
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/337601
        Commit-Queue: Sergey Silkin <ssilkinwebrtc.org>
        Reviewed-by: Philip Eliasson <philipelwebrtc.org>
        Cr-Commit-Position: refs/heads/main{#41770}
    
    UltraBlame original commit: 632036dc3796a4a7d0e8cea1c2cd2df66d9fc794

--- third_party/libwebrtc/modules/video_coding/codecs/vp8/libvpx_vp8_encoder.cc
+++ third_party/libwebrtc/modules/video_coding/codecs/vp8/libvpx_vp8_encoder.cc
@@ -317,7 +317,9 @@ LibvpxVp8Encoder::LibvpxVp8Encoder(std::unique_ptr<LibvpxInterface> interface,
       variable_framerate_experiment_(ParseVariableFramerateConfig(
           "WebRTC-VP8VariableFramerateScreenshare")),
       framerate_controller_(variable_framerate_experiment_.framerate_limit),
-      max_frame_drop_interval_(ParseFrameDropInterval()) {
+      max_frame_drop_interval_(ParseFrameDropInterval()),
+      android_specific_threading_settings_(webrtc::field_trial::IsEnabled(
+          "WebRTC-LibvpxVp8Encoder-AndroidSpecificThreadingSettings")) {
   
   
   raw_images_.reserve(kMaxSimulcastStreams);
@@ -782,21 +784,26 @@ int LibvpxVp8Encoder::GetCpuSpeed(int width, int height) {
 }
 
 int LibvpxVp8Encoder::NumberOfThreads(int width, int height, int cpus) {
+#if defined(WEBRTC_ANDROID)
+  if (android_specific_threading_settings_) {
+#endif
 #if defined(WEBRTC_ANDROID) || defined(WEBRTC_ARCH_MIPS)
-  if (width * height >= 320 * 180) {
-    if (cpus >= 4) {
-      
-      
-      return 3;
-    } else if (cpus == 3 || cpus == 2) {
-      return 2;
-    } else {
-      return 1;
+    if (width * height >= 320 * 180) {
+      if (cpus >= 4) {
+        
+        
+        return 3;
+      } else if (cpus == 3 || cpus == 2) {
+        return 2;
+      } else {
+        return 1;
+      }
     }
+    return 1;
+#if defined(WEBRTC_ANDROID)
   }
-  return 1;
-#else
-#if defined(WEBRTC_IOS)
+#endif
+#elif defined(WEBRTC_IOS)
   std::string trial_string =
       field_trial::FindFullName(kVP8IosMaxNumberOfThreadFieldTrial);
   FieldTrialParameter<int> max_thread_number(
@@ -823,11 +830,10 @@ int LibvpxVp8Encoder::NumberOfThreads(int width, int height, int cpus) {
       return 3;
     }
     return 2;
-  } else {
-    
-    return 1;
   }
-#endif
+
+  
+  return 1;
 }
 
 int LibvpxVp8Encoder::InitAndSetControlSettings() {
