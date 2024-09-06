c16882fdc0a7e347e2ccf566b9c8010717c479a5
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-06 08:23:09 +0000

    Bug 1875481 - Part 1: move MOZ_CAN_RUN_SCRIPT_BOUNDARY_LAMBDA into a shared header r=handyman
    Differential Revision: https://phabricator.services.mozilla.com/D200977
    
    UltraBlame original commit: d1da53c0673b114d5c9baa0b357b095895201dae

--- dom/streams/TransformStream.cpp
+++ dom/streams/TransformStream.cpp
@@ -20,14 +20,6 @@
 #include "mozilla/dom/TransformerBinding.h"
 #include "nsWrapperCache.h"
 
-
-
-#ifdef __clang__
-#  define MOZ_CAN_RUN_SCRIPT_BOUNDARY_LAMBDA MOZ_CAN_RUN_SCRIPT_BOUNDARY
-#else
-#  define MOZ_CAN_RUN_SCRIPT_BOUNDARY_LAMBDA
-#endif
-
 namespace mozilla::dom {
 
 using namespace streams_abstract;
