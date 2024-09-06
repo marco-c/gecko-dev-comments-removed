97970e5e42ce2f26c66f9f4d5d068a5bef936f45
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-06 07:16:42 +0000

    Bug 1884783 - AnyRef::fromUint32Truncate should error on unknown architectures. r=jpages
    Differential Revision: https://phabricator.services.mozilla.com/D204300
    
    UltraBlame original commit: b0aac76dbb797b612328b870315d065cfb4947ec

--- js/src/wasm/WasmAnyRef.h
+++ js/src/wasm/WasmAnyRef.h
@@ -208,16 +208,19 @@ class AnyRef {
   
   static AnyRef fromUint32Truncate(uint32_t value) {
     
-#if defined(JS_CODEGEN_X64) || defined(JS_CODEGEN_ARM64)
+#if defined(JS_CODEGEN_NONE) || defined(JS_CODEGEN_X64) || \
+    defined(JS_CODEGEN_ARM64)
     
     uintptr_t wideValue = uintptr_t(value & 0x7FFFFFFF);
 #elif defined(JS_CODEGEN_LOONG64) || defined(JS_CODEGEN_MIPS64) || \
     defined(JS_CODEGEN_RISCV64)
     
     uintptr_t wideValue = uintptr_t(int64_t((uint64_t(value) << 33)) >> 33);
-#else
+#elif !defined(JS_64BIT)
     
     uintptr_t wideValue = (uintptr_t)value;
+#else
+#  error "unknown architecture"
 #endif
 
     
