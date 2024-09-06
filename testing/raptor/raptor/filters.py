Bug 1893066 - Fix geomean formula for Raptor. r=perftest-reviewers,sparky
This patch updates the geometric mean formula in Raptor. The
previous implementation was old and provided only an approximate value.

The scipy implementation is used here as it conveniently handles the
zero cases (by returning 0) and negative numbers (by returning NaN)
without failing in either case.

Differential Revision: https://phabricator.services.mozilla.com/D208917

UltraBlame original commit: 92d53efb67c7efa140ae3a92bf3b04dadc882c97

testing/raptor/raptor/filters.py
@@ -4,7 +4,6 @@
 
 
 
-import math
 
 """
 data filters:
@@ -175,11 +174,10 @@ def geometric_mean(series):
     """
     geometric_mean: http://en.wikipedia.org/wiki/Geometric_mean
     """
-    total = 0
-    for i in series:
-        total += math.log(i + 1)
-    
-    return math.exp(total / len(series)) - 1
+
+    from scipy.stats.mstats import gmean
+
+    return gmean(series)
 
 
 