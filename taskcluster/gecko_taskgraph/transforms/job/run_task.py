0e06bccdb87c1e55d2f7acdf626372d294c5ee5c
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-06 20:21:47 +0000

    Bug 1903502 - Use in-tree robustcheckout.py in tasks running on generic-worker. r=releng-reviewers,sheehan,taskgraph-reviewers,jcristau
    The robustcheckout.py that is in-tree ends up in docker images, used by
    tasks running on docker-worker, but for tasks running on generic-worker,
    we currently rely on whatever version of robustcheckout.py is there.
    It currently seems out of date, but whenever we make changes to the
    in-tree version, we don't end up with the fix on these jobs without some
    additional manual work.
    
    With this change, we do the same as with run-task and fetch-content: we
    pull the in-tree version that we store as an artifact of the decision
    task.
    
    Differential Revision: https://phabricator.services.mozilla.com/D214234
    
    UltraBlame original commit: ce13b4bf7d35f6e303656fe6cf8db046fef810eb

--- taskcluster/gecko_taskgraph/transforms/job/run_task.py
+++ taskcluster/gecko_taskgraph/transforms/job/run_task.py
@@ -203,6 +203,17 @@ def generic_worker_run_task(config, job, taskdesc):
                 "file": "./fetch-content",
             }
         )
+    
+    
+    if run.get("checkout") and not is_mac:
+        worker["mounts"].append(
+            {
+                "content": {
+                    "url": script_url(config, "robustcheckout.py"),
+                },
+                "file": "./robustcheckout.py",
+            }
+        )
 
     run_command = run["command"]
     run_cwd = run.get("cwd")
