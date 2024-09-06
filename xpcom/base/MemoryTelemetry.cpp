e70c43c7df37f8bb6b6f0c091c7a26e58025e2a4
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-06 12:58:15 +0000

    Backed out changeset 3efd6c035ad4 (bug 1794529) for causing bp-nu bustages in MemoryTelemetry.h. CLOSED TREE
    UltraBlame original commit: a29fdc2dc375136a94e8920894171eaa7b08ce5c

--- xpcom/base/MemoryTelemetry.cpp
+++ xpcom/base/MemoryTelemetry.cpp
@@ -385,22 +385,17 @@ void MemoryTelemetry::GatherTotalMemory() {
 
         
         
-        bool success = true;
         for (const auto& info : infos) {
 #ifdef XP_MACOSX
           int64_t memory =
               nsMemoryReporterManager::PhysicalFootprint(info.mHandle);
 #else
-          int64_t memory =
-              nsMemoryReporterManager::ResidentUnique(info.mHandle);
+	  int64_t memory =
+	      nsMemoryReporterManager::ResidentUnique(info.mHandle);
 #endif
           if (memory > 0) {
             childSizes.AppendElement(memory);
             totalMemory += memory;
-          } else {
-            
-            
-            success = false;
           }
 
 #if defined(XP_WIN)
@@ -410,22 +405,17 @@ void MemoryTelemetry::GatherTotalMemory() {
 #endif
         }
 
-        Maybe<int64_t> mbTotal;
-        if (success) {
-          mbTotal = Some(totalMemory);
-        }
-
         NS_DispatchToMainThread(NS_NewRunnableFunction(
             "MemoryTelemetry::FinishGatheringTotalMemory",
-            [mbTotal, childSizes = std::move(childSizes)] {
-              MemoryTelemetry::Get().FinishGatheringTotalMemory(mbTotal,
+            [totalMemory, childSizes = std::move(childSizes)] {
+              MemoryTelemetry::Get().FinishGatheringTotalMemory(totalMemory,
                                                                 childSizes);
             }));
       }));
 }
 
 nsresult MemoryTelemetry::FinishGatheringTotalMemory(
-    Maybe<int64_t> aTotalMemory, const nsTArray<int64_t>& aChildSizes) {
+    int64_t aTotalMemory, const nsTArray<int64_t>& aChildSizes) {
   mGatheringTotalMemory = false;
 
   
@@ -434,10 +424,8 @@ nsresult MemoryTelemetry::FinishGatheringTotalMemory(
   
   
   
-  if (aTotalMemory) {
-    HandleMemoryReport(Telemetry::MEMORY_TOTAL, nsIMemoryReporter::UNITS_BYTES,
-                       aTotalMemory.value());
-  }
+  HandleMemoryReport(Telemetry::MEMORY_TOTAL, nsIMemoryReporter::UNITS_BYTES,
+                     aTotalMemory);
 
   if (aChildSizes.Length() > 1) {
     int32_t tabsCount;
