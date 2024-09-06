f8f898bed5996bbb1fa26fbd29dc6bdc7c7edf68
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-06 10:12:46 +0000

    Bug 1891717: Update tests within browser/base/ to work with https-first enabled, r=Gijs
    Differential Revision: https://phabricator.services.mozilla.com/D207554
    
    UltraBlame original commit: 65ef9e9c90d5734d2b01e596fb436f32e6ca1131

--- browser/base/content/test/tabPrompts/browser_promptFocus.js
+++ browser/base/content/test/tabPrompts/browser_promptFocus.js
@@ -20,8 +20,7 @@ add_task(async function test_tabdialogbox_tab_switch_focus() {
     tabPromises.push(
       BrowserTestUtils.openNewForegroundTab(
         gBrowser,
-        
-        "http://example.com",
+        "https://example.com",
         true
       )
     );
