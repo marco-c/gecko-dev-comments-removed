33590eaa3847523141a510398e7245c74b358d3a
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-06 08:47:32 +0000

    Bug 1890462 - Update fetchEngineSelector to match against subVariants in search-config-v2. r=Standard8
    Differential Revision: https://phabricator.services.mozilla.com/D206977
    
    UltraBlame original commit: c650c87e21cee6b4262c0782ec004d03e6f29b16

---
+++ toolkit/components/search/tests/xpcshell/test_engine_selector_subvariants.js
@@ -0,0 +1,142 @@
+
+
+
+"use strict";
+
+const CONFIG = [
+  {
+    recordType: "engine",
+    identifier: "engine-1",
+    base: {},
+    variants: [
+      {
+        environment: {
+          allRegionsAndLocales: true,
+        },
+        partnerCode: "variant-partner-code",
+        subVariants: [
+          {
+            environment: { regions: ["CA", "FR"] },
+            telemetrySuffix: "subvariant-telemetry",
+          },
+          {
+            environment: { regions: ["GB", "FR"] },
+            partnerCode: "subvariant-partner-code",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    recordType: "defaultEngines",
+    specificDefaults: [],
+  },
+  {
+    recordType: "engineOrders",
+    orders: [],
+  },
+];
+
+const engineSelector = new SearchEngineSelector();
+let settings;
+let configStub;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+async function assertActualEnginesEqualsExpected(
+  config,
+  userEnv,
+  expectedEngines,
+  message
+) {
+  engineSelector._configuration = null;
+  configStub.returns(config);
+  let { engines } = await engineSelector.fetchEngineConfiguration(userEnv);
+
+  Assert.deepEqual(engines, expectedEngines, message);
+}
+
+add_setup(async function () {
+  settings = await RemoteSettings(SearchUtils.NEW_SETTINGS_KEY);
+  configStub = sinon.stub(settings, "get");
+});
+
+add_task(async function test_no_subvariants_match() {
+  await assertActualEnginesEqualsExpected(
+    CONFIG,
+    {
+      locale: "fi",
+      region: "FI",
+    },
+    [
+      {
+        identifier: "engine-1",
+        partnerCode: "variant-partner-code",
+      },
+    ],
+    "Should match no subvariants."
+  );
+});
+
+add_task(async function test_matching_subvariant_with_properties() {
+  await assertActualEnginesEqualsExpected(
+    CONFIG,
+    {
+      locale: "en-GB",
+      region: "GB",
+    },
+    [
+      {
+        identifier: "engine-1",
+        partnerCode: "subvariant-partner-code",
+      },
+    ],
+    "Should match subvariant with subvariant properties."
+  );
+});
+
+add_task(async function test_matching_variant_and_subvariant_with_properties() {
+  await assertActualEnginesEqualsExpected(
+    CONFIG,
+    {
+      locale: "en-CA",
+      region: "CA",
+    },
+    [
+      {
+        identifier: "engine-1",
+        partnerCode: "variant-partner-code",
+        telemetrySuffix: "subvariant-telemetry",
+      },
+    ],
+    "Should match subvariant with subvariant properties."
+  );
+});
+
+add_task(async function test_matching_two_subvariant_with_properties() {
+  await assertActualEnginesEqualsExpected(
+    CONFIG,
+    {
+      locale: "fr",
+      region: "FR",
+    },
+    [
+      {
+        identifier: "engine-1",
+        partnerCode: "subvariant-partner-code",
+      },
+    ],
+    "Should match the last subvariant with subvariant properties."
+  );
+});
