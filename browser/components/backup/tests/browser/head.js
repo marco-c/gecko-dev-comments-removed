de1e05b3d542ee036ac032b9f5a41c30f35f44ca
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-06 19:33:26 +0000

    Bug 1893301 - Build confirmation dialog element for removing encryption password from backup. r=backup-reviewers,firefox-desktop-core-reviewers ,fluent-reviewers,mconley
    This patch adds a new dialog for disabling a backup's password protection.
    
    Steps to test this feature:
    1. First ensure that the following prefs are enabled: browser.backup.enabled and browser.backup.preferences.ui.enabled
    2. Then ensure that the existing backup for the current profile has encryption enabled (should have enc-state.json)
    3. Else, access the debug page (chrome://browser/content/backup/debug.html) to enable encryption
    4. Once encryption is enabled, the sensitive data checkbox should be checked in about:settings / about:preferences
    5. Clicking the checked checkbox should now show the new dialog for removing password protection
    6. If the dialog is confirmed, the checkbox should be unchecked in both the settings/preferences page and the debug page
    
    Other notes:
    - If the checkbox is *not* checked, nothing will happen. This is because the dialog for enabling password protection is not yet implemented
    - Added tests and Storybook entries as well
    
    Figma: https://www.figma.com/design/vNbX4c0ws0L1qr0mxpKvsW/Fx-Backup?node-id=147-4568&t=9NNUojWMeOLwe3rD-0
    
    Differential Revision: https://phabricator.services.mozilla.com/D213171
    
    UltraBlame original commit: 72a048a9cfdb1909476fc9e913fb3eb32d4eafc7

---
+++ browser/components/backup/tests/browser/head.js
@@ -0,0 +1,14 @@
+
+
+
+"use strict";
+
+const { BackupService } = ChromeUtils.importESModule(
+  "resource:///modules/backup/BackupService.sys.mjs"
+);
+
+const { MockFilePicker } = SpecialPowers;
+
+const { sinon } = ChromeUtils.importESModule(
+  "resource://testing-common/Sinon.sys.mjs"
+);
