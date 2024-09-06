5b7c983e4d10dc012b6a514b5c136e60658871cb
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-06 08:15:11 +0000

    Bug 1888303 [wpt PR 45397] - [digital credential] Move IdentityRequestProvider to a separate dict, a=testonly
    Automatic update from web-platform-tests
    [digital credential] Move IdentityRequestProvider to a separate dict
    
    In this CL, we further decouple the Digital Credential API [1] from
    the FedCM API [2] by moving the protocol/request parameters into its
    own dictionary that is only accessible by the Digital Credential API
    but not from FedCM, and vice versa, so that the appropriate
    parameters are parsed (or discarded) in each of these two APIs.
    
    [1] https://wicg.github.io/digital-identities/
    [2] https://fedidcg.github.io/FedCM/
    
    Bug: 1416939
    Change-Id: I322bcac9fb38eb52e3466162753e000146d652fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5386528
    Reviewed-by: Emily Stark <estarkchromium.org>
    Commit-Queue: Sam Goto <gotochromium.org>
    Cr-Commit-Position: refs/heads/main{#1279609}
    
    --
    
    wpt-commits: 3bfcd2d3f1d691fe9ae88bcf9db5d0eef0c71715
    wpt-pr: 45397
    
    UltraBlame original commit: 65b5ae44ff533ab3f386e411e1f7330bcd50962a

--- testing/web-platform/tests/credential-management/support/digital-identity-helper.js
+++ testing/web-platform/tests/credential-management/support/digital-identity-helper.js
@@ -3,20 +3,16 @@ export function buildValidNavigatorIdentityRequest() {
   return {
       digital: {
         providers: [{
-          protocol: "protocol",
-          selector: {
-            format: ['mdoc'],
-            doctype: 'org.iso.18013.5.1.mDL',
-            fields: [
-              'org.iso.18013.5.1.family_name',
-              'org.iso.18013.5.1.portrait',
-            ]
-          },
-          params: {
-            nonce: '1234',
-            readerPublicKey: 'test_reader_public_key',
-            extraParamAsNeededByDigitalCredentials: true,
-          },
+          protocol: "urn:openid.net:oid4vp",
+          request: JSON.stringify({
+            
+            client_id: "client.example.org",
+            client_id_scheme: "web-origin",
+            nonce: "n-0S6_WzA2Mj",
+            presentation_definition: {
+              
+            }
+          }),
         }],
       },
   };
