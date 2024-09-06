4974bfb4baba303b644be27d7207892ee6f4ffc9
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-06 10:23:35 +0000

    Bug 1891176 - Allow tab navigation out of multiline edit controls r=gsvelto
    Differential Revision: https://phabricator.services.mozilla.com/D207574
    
    UltraBlame original commit: 8be2a0b00aa2df26ca31722395d285d1b772537f

--- toolkit/crashreporter/client/app/src/ui/windows/mod.rs
+++ toolkit/crashreporter/client/app/src/ui/windows/mod.rs
@@ -719,6 +719,13 @@ impl<'a> WindowChildRenderer<'a> {
                             .expect("failed to select font gdi object");
                         }
                     }
+
+                    
+                    
+                    if msg == win::WM_GETDLGCODE && wparam == KeyboardAndMouse::VK_TAB as usize {
+                        return 0;
+                    }
+
                     if msg == win::WM_DESTROY {
                         drop(unsafe { Box::from_raw(dw_ref_data as *mut SubClassData) });
                     }
