18e5e0a4ac9a7c5ee0cc565601c8fd1d84658e2a
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-06 18:41:52 +0000

    Bug 1882214 - Ensure initializeMacApp is called before the prompt for master password on startup for Thunderbird. r=kaie,eeejay,wsmwk
    This is necessary for Thunderbird when using a master password on startup.
    Per eeejay notes in D199786.
    > calling InitializeMacApp more than once does nothing.
    
    Differential Revision: https://phabricator.services.mozilla.com/D213513
    
    UltraBlame original commit: dd427ba7922ae0d26febb9f60511a120b9347eb1

--- toolkit/xre/nsXREDirProvider.cpp
+++ toolkit/xre/nsXREDirProvider.cpp
@@ -55,6 +55,9 @@
 #ifdef MOZ_THUNDERBIRD
 #  include "nsIPK11TokenDB.h"
 #  include "nsIPK11Token.h"
+#  ifdef XP_MACOSX
+#    include "MacApplicationDelegate.h"
+#  endif
 #endif
 
 #include <stdlib.h>
@@ -789,6 +792,11 @@ nsXREDirProvider::DoStartup() {
     if (!bgtaskMode &&
         mozilla::Preferences::GetBool(
             "security.prompt_for_master_password_on_startup", false)) {
+#  ifdef XP_MACOSX
+      
+      
+      InitializeMacApp();
+#  endif
       
       
       
