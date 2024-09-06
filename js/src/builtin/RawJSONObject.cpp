a341c5e30c2c122f749a7c9e06b3fc75414ffbf2
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-06 20:19:07 +0000

    Backed out 9 changesets (bug 1890771) for causing mochitest plain failures CLOSED TREE
    Backed out changeset 7a1ccaa09fc0 (bug 1890771)
    Backed out changeset 70a20173bda0 (bug 1890771)
    Backed out changeset e4a0f6fcac10 (bug 1890771)
    Backed out changeset 95bf36cdb7c5 (bug 1890771)
    Backed out changeset fd197b7a873e (bug 1890771)
    Backed out changeset 90aceddc9988 (bug 1890771)
    Backed out changeset 1566570e97e7 (bug 1890771)
    Backed out changeset e87cbf65f386 (bug 1890771)
    Backed out changeset 231a98c0f826 (bug 1890771)
    
    UltraBlame original commit: d63641241a5df86293ce726fa7a35787a6df8b35

--- js/src/builtin/RawJSONObject.cpp
+++ js/src/builtin/RawJSONObject.cpp
@@ -17,17 +17,14 @@ const JSClass RawJSONObject::class_ = {"RawJSON",
 
 RawJSONObject* RawJSONObject::create(JSContext* cx,
                                      Handle<JSString*> jsonString) {
-  
-  
   Rooted<RawJSONObject*> obj(
       cx, NewObjectWithGivenProto<RawJSONObject>(cx, nullptr));
   if (!obj) {
     return nullptr;
   }
-  
   Rooted<PropertyKey> id(cx, NameToId(cx->names().rawJSON));
   Rooted<Value> jsonStringVal(cx, StringValue(jsonString));
-  if (!NativeDefineDataProperty(cx, obj, id, jsonStringVal, JSPROP_ENUMERATE)) {
+  if (!NativeDefineDataProperty(cx, obj, id, jsonStringVal, 0)) {
     return nullptr;
   }
   return obj;
