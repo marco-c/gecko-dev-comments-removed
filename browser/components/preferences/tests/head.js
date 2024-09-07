16718b5bf09c4841c534354580a84e4bcf55846d
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-07 06:58:37 +0000

    Backed out changeset b825b6411765 (bug 1906140) for causing failures in browser_view_sidebar_menu.js CLOSED TREE
    UltraBlame original commit: 8fa2889392a23767f8364e37343ce234270ee6e5

--- browser/components/preferences/tests/head.js
+++ browser/components/preferences/tests/head.js
@@ -203,7 +203,7 @@ class DefinitionServer {
       id: "test-feature",
       
       title: "experimental-features-media-jxl",
-      group: "experimental-features-group-customize-browsing",
+      group: "experimental-features-group-browsing",
       description: "pane-experimental-description3",
       restartRequired: false,
       type: "boolean",
