3ecf48da664f65492a6bf594ffb127e4a8d6878c
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-07 01:04:11 +0000

    Bug 1907296 - Fix test_ext_cookies_partitionKey.js when blocking 3pc by disabling optInPartitioning since xpcshell tests do not support https which is required by CHIPS. r=extension-reviewers,robwu
    Differential Revision: https://phabricator.services.mozilla.com/D217267
    
    UltraBlame original commit: 8a6055b2d9c3b0c341589a8672c76aa47cc9db78

--- toolkit/components/extensions/test/xpcshell/test_ext_cookies_partitionKey.js
+++ toolkit/components/extensions/test/xpcshell/test_ext_cookies_partitionKey.js
@@ -265,6 +265,13 @@ add_task(async function setup() {
     "network.cookie.sameSite.noneRequiresSecure",
     false
   );
+  
+  
+  
+  Services.prefs.setBoolPref(
+    "network.cookie.cookieBehavior.optInPartitioning",
+    false
+  );
 });
 
 add_task(async function test_no_partitioning() {
