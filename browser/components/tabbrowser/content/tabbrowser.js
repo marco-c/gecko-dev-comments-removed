7350b9f0021437fe0a13e6808f44f0e2c5369c1a
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-07 10:17:13 +0000

    Bug 1908411: API to remove tab group r=dao,dwalker,tabbrowser-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D220273
    
    UltraBlame original commit: 5eaa004e72672a8301c2a86139fbf801972012ae

--- browser/components/tabbrowser/content/tabbrowser.js
+++ browser/components/tabbrowser/content/tabbrowser.js
@@ -2940,6 +2940,12 @@
       return group;
     },
 
+    removeTabGroup(group) {
+      this.removeTabs(group.tabs);
+      
+      group.remove();
+    },
+
     _determineURIToLoad(uriString, createLazyBrowser) {
       uriString = uriString || "about:blank";
       let aURIObject = null;
