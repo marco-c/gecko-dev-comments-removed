cefbda5577e6995fa7192a63d8dfc844036e9479
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-07 08:11:47 +0000

    Backed out changeset e007fa3293a7 (bug 1891486) for causing mass failures
    UltraBlame original commit: 6df21990d2183c4e410c68a2e93cf847d4ed4194

--- layout/style/nsStyleStruct.cpp
+++ layout/style/nsStyleStruct.cpp
@@ -263,14 +263,7 @@ nsStyleFont::nsStyleFont(const Document& aDocument)
 nsChangeHint nsStyleFont::CalcDifference(const nsStyleFont& aNewData) const {
   MOZ_ASSERT(mXTextScale == aNewData.mXTextScale,
              "expected -x-text-scale to be the same on both nsStyleFonts");
-  
-  
-  
-  if (mLanguage != aNewData.mLanguage) {
-    return nsChangeHint_ReconstructFrame;
-  }
-
-  if (mSize != aNewData.mSize ||
+  if (mSize != aNewData.mSize || mLanguage != aNewData.mLanguage ||
       mExplicitLanguage != aNewData.mExplicitLanguage ||
       mMathVariant != aNewData.mMathVariant ||
       mMathStyle != aNewData.mMathStyle ||
