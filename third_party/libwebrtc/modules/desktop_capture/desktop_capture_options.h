8ee489650e2ce4e235da15eb82069c5c0f1ef24f
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-07 09:38:45 +0000

    Bug 1914196 - Cherry-pick upstream libwebrtc commit 3069c60ada r=webrtc-reviewers,mjf
    Upstream commit: https://webrtc.googlesource.com/src/+/3069c60adafff3a40ec679c29f9e685f9ac59a45
           Add desktop-capture option for ScreenCaptureKit on macOS.
    
           This option will allow clients to control which ScreenCapturer is used,
           for versions of macOS that support ScreenCaptureKit. The default is to
           use the previous code, to avoid breaking current users of the module.
    
           Bug: chromium:327458809
           Change-Id: Ib0f9390c85d726016a39eea4fda9b8bd14a094c3
           Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/355020
           Reviewed-by: Alexander Cooper <alcooperchromium.org>
           Commit-Queue: Lambros Lambrou <lambroslambrouchromium.org>
           Cr-Commit-Position: refs/heads/main{#42518}
    
    Differential Revision: https://phabricator.services.mozilla.com/D219809
    
    UltraBlame original commit: 41ab7aaba858dfe10d4be5a2300ec4a45238cc0a

--- third_party/libwebrtc/modules/desktop_capture/desktop_capture_options.h
+++ third_party/libwebrtc/modules/desktop_capture/desktop_capture_options.h
@@ -72,6 +72,13 @@ class RTC_EXPORT DesktopCaptureOptions {
 
   bool allow_iosurface() const { return allow_iosurface_; }
   void set_allow_iosurface(bool allow) { allow_iosurface_ = allow; }
+
+  
+  
+  
+  
+  bool allow_sck_capturer() const { return allow_sck_capturer_; }
+  void set_allow_sck_capturer(bool allow) { allow_sck_capturer_ = allow; }
 #endif
 
   const rtc::scoped_refptr<FullScreenWindowDetector>&
@@ -235,6 +242,7 @@ class RTC_EXPORT DesktopCaptureOptions {
 #if defined(WEBRTC_MAC) && !defined(WEBRTC_IOS)
   rtc::scoped_refptr<DesktopConfigurationMonitor> configuration_monitor_;
   bool allow_iosurface_ = false;
+  bool allow_sck_capturer_ = false;
 #endif
 
   rtc::scoped_refptr<FullScreenWindowDetector> full_screen_window_detector_;
