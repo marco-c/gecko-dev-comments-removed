16bb8e2e8d570caa8fc5fe723650404a1c7d4535
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-07 11:01:28 +0000

    Backed out changeset d232f991af6a (bug 1882670) for causing cdp failures at frame.spec.js CLOSED TREE
    UltraBlame original commit: 582853abacbaee62bb9c888ebee346516d2c06ea

--- docshell/base/nsDocShell.cpp
+++ docshell/base/nsDocShell.cpp
@@ -10049,14 +10049,6 @@ nsresult nsDocShell::DoURILoad(nsDocShellLoadState* aLoadState,
                    contentPolicyType == nsIContentPolicy::TYPE_INTERNAL_FRAME,
                "DoURILoad thinks this is a frame and InternalLoad does not");
 
-    if (auto* iframe = HTMLIFrameElement::FromNodeOrNull(
-            mBrowsingContext->GetEmbedderElement())) {
-      
-      if (!(aLoadState->LoadType() & LOAD_RELOAD_NORMAL)) {
-        iframe->CancelLazyLoading(true );
-      }
-    }
-
     if (StaticPrefs::dom_block_external_protocol_in_iframes()) {
       
       if (nsContentUtils::IsExternalProtocol(aLoadState->URI())) {
