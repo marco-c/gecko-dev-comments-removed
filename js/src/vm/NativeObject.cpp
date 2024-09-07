0fbed4469bd1c097965a9d04dd7f7b8874adc5e3
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-07 10:02:54 +0000

    Bug 1772858 - Part 2: Move resolving check to the outer if-statement. r=spidermonkey-reviewers,jandem
    Instead of checking `desc_.resolving()` repeatedly, move the check into the
    outer if-statement.
    
    Drive-by change:
    - Remove some unnecessary re-rooting of already rooted variables.
    
    Differential Revision: https://phabricator.services.mozilla.com/D220230
    
    UltraBlame original commit: c24248022ef227dfb9f7be6d0e19aa1226dc67b0

--- js/src/vm/NativeObject.cpp
+++ js/src/vm/NativeObject.cpp
@@ -1604,7 +1604,7 @@ bool js::NativeDefineProperty(JSContext* cx, Handle<NativeObject*> obj,
   
   if (obj->is<ArrayObject>()) {
     
-    Rooted<ArrayObject*> arr(cx, &obj->as<ArrayObject>());
+    auto arr = HandleObject(obj).as<ArrayObject>();
     if (id == NameToId(cx->names().length)) {
       
       if (desc_.isAccessorDescriptor()) {
@@ -1624,41 +1624,32 @@ bool js::NativeDefineProperty(JSContext* cx, Handle<NativeObject*> obj,
   } else if (obj->is<TypedArrayObject>()) {
     
     if (mozilla::Maybe<uint64_t> index = ToTypedArrayIndex(id)) {
-      Rooted<TypedArrayObject*> tobj(cx, &obj->as<TypedArrayObject>());
+      auto tobj = HandleObject(obj).as<TypedArrayObject>();
       return DefineTypedArrayElement(cx, tobj, index.value(), desc_, result);
     }
-  } else if (obj->is<ArgumentsObject>()) {
-    Rooted<ArgumentsObject*> argsobj(cx, &obj->as<ArgumentsObject>());
+  } else if (obj->is<ArgumentsObject>() && !desc_.resolving()) {
+    auto argsobj = HandleObject(obj).as<ArgumentsObject>();
     if (id.isAtom(cx->names().length)) {
       
       
       
-      if (!desc_.resolving()) {
-        if (!ArgumentsObject::reifyLength(cx, argsobj)) {
-          return false;
-        }
+      if (!ArgumentsObject::reifyLength(cx, argsobj)) {
+        return false;
       }
     } else if (id.isAtom(cx->names().callee) &&
-               argsobj->is<MappedArgumentsObject>()) {
+               obj->is<MappedArgumentsObject>()) {
       
-      if (!desc_.resolving()) {
-        Rooted<MappedArgumentsObject*> mapped(
-            cx, &argsobj->as<MappedArgumentsObject>());
-        if (!MappedArgumentsObject::reifyCallee(cx, mapped)) {
-          return false;
-        }
+      auto mapped = HandleObject(argsobj).as<MappedArgumentsObject>();
+      if (!MappedArgumentsObject::reifyCallee(cx, mapped)) {
+        return false;
       }
     } else if (id.isWellKnownSymbol(JS::SymbolCode::iterator)) {
       
-      if (!desc_.resolving()) {
-        if (!ArgumentsObject::reifyIterator(cx, argsobj)) {
-          return false;
-        }
+      if (!ArgumentsObject::reifyIterator(cx, argsobj)) {
+        return false;
       }
     } else if (id.isInt()) {
-      if (!desc_.resolving()) {
-        argsobj->markElementOverridden();
-      }
+      argsobj->markElementOverridden();
     }
   }
 
