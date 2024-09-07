4413b70546360d6f1739b884988f1605d9e560fe
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-07 08:54:21 +0000

    Bug 1870687 - Investigate removing the _locale and _extensionID options from within AppProvidedSearchEngine. r=Standard8,urlbar-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D218684
    
    UltraBlame original commit: 440770be9e5e2f4395ba772a2b6eaf7161795018

--- browser/extensions/search-detection/extension/api.js
+++ browser/extensions/search-detection/extension/api.js
@@ -76,12 +76,7 @@ this.addonsSearchDetection = class extends ExtensionAPI {
                     patterns[pattern] = [];
                   }
 
-                  
-                  
-                  if (
-                    !patterns[pattern].includes(_extensionID) &&
-                    !_extensionID.endsWith("@search.mozilla.org")
-                  ) {
+                  if (!patterns[pattern].includes(_extensionID)) {
                     patterns[pattern].push(_extensionID);
                   }
                 });
