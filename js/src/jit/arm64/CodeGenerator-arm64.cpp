f6a05814e70f44255765e0132f0a9954c1a6c09b
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-07 08:50:16 +0000

    Bug 1913413 - Part 24: Move visitCompareI64 into shared CodeGenerator. r=spidermonkey-reviewers,iain
    Differential Revision: https://phabricator.services.mozilla.com/D219334
    
    UltraBlame original commit: 6ae14ffaa8c405a02234a6ed3bf79e0834b618f7

--- js/src/jit/arm64/CodeGenerator-arm64.cpp
+++ js/src/jit/arm64/CodeGenerator-arm64.cpp
@@ -152,43 +152,6 @@ static Operand toWOperand(const LAllocation* a) {
   return Operand(toWRegister(a));
 }
 
-
-
-
-
-static Assembler::Condition GetCondForSwappedOperands(
-    Assembler::Condition cond) {
-  
-  
-  
-  
-  
-  
-  switch (cond) {
-    case vixl::eq:
-    case vixl::ne:
-      return cond;
-    case vixl::hs:
-      return vixl::ls;
-    case vixl::ls:
-      return vixl::hs;
-    case vixl::lo:
-      return vixl::hi;
-    case vixl::hi:
-      return vixl::lo;
-    case vixl::ge:
-      return vixl::le;
-    case vixl::le:
-      return vixl::ge;
-    case vixl::gt:
-      return vixl::lt;
-    case vixl::lt:
-      return vixl::gt;
-    default:
-      MOZ_CRASH("no meaningful swapped-operand condition");
-  }
-}
-
 void CodeGenerator::visitAddI(LAddI* ins) {
   const LAllocation* lhs = ins->getOperand(0);
   const LAllocation* rhs = ins->getOperand(1);
@@ -2263,30 +2226,6 @@ void CodeGenerator::visitWasmStore(LWasmStore* lir) {
 
   masm.wasmStore(mir->access(), ToAnyRegister(lir->value()),
                  ToRegister(lir->memoryBase()), ToRegister(lir->ptr()));
-}
-
-void CodeGenerator::visitCompareI64(LCompareI64* lir) {
-  MCompare* mir = lir->mir();
-  MOZ_ASSERT(mir->compareType() == MCompare::Compare_Int64 ||
-             mir->compareType() == MCompare::Compare_UInt64);
-
-  const LInt64Allocation lhs = lir->getInt64Operand(LCompareI64::Lhs);
-  const LInt64Allocation rhs = lir->getInt64Operand(LCompareI64::Rhs);
-  Register lhsReg = ToRegister64(lhs).reg;
-  Register output = ToRegister(lir->output());
-  bool isSigned = mir->compareType() == MCompare::Compare_Int64;
-
-  if (IsConstant(rhs)) {
-    masm.cmpPtrSet(JSOpToCondition(lir->jsop(), isSigned), lhsReg,
-                   ImmWord(ToInt64(rhs)), output);
-  } else if (rhs.value().isGeneralReg()) {
-    masm.cmpPtrSet(JSOpToCondition(lir->jsop(), isSigned), lhsReg,
-                   ToRegister64(rhs).reg, output);
-  } else {
-    masm.cmpPtrSet(
-        GetCondForSwappedOperands(JSOpToCondition(lir->jsop(), isSigned)),
-        ToAddress(rhs.value()), lhsReg, output);
-  }
 }
 
 void CodeGenerator::visitWasmSelect(LWasmSelect* lir) {
