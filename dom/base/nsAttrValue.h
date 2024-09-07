3d55bc528534195fd0238fbe4cbb23d74388a132
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-07 00:38:04 +0000

    Bug 1906727 - Make `HTMLEditor::SplitAncestorStyledInlineElementsAt` check attr value type first r=peterv
    Differential Revision: https://phabricator.services.mozilla.com/D215990
    
    UltraBlame original commit: 80bfb01e76a59aa8c8b8aeacf3ef63b000c42332

--- dom/base/nsAttrValue.h
+++ dom/base/nsAttrValue.h
@@ -515,7 +515,7 @@ class nsAttrValue {
   
   void SetDoubleValueAndType(double aValue, ValueType aType,
                              const nsAString* aStringValue);
-  void SetColorValue(nscolor aColor, const nsAString& aString);
+  bool SetColorValue(nscolor aColor, const nsAString& aString);
   void SetMiscAtomOrString(const nsAString* aValue);
   void ResetMiscAtomOrString();
   void SetSVGType(ValueType aType, const void* aValue,
