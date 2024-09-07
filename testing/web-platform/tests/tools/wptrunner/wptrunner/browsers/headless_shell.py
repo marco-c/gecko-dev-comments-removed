073d1c8d64f36bb7354a2c9d5507ab9acbd9ddf5
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-07 07:04:27 +0000

    Bug 1911006 [wpt PR 47399] - [wptrunner] Don't override `HeadlessShellBrowser.make_command()`, a=testonly
    Automatic update from web-platform-tests
    [wptrunner] Don't override `HeadlessShellBrowser.make_command()` (#47399)
    
    web-platform-tests/wpt#47229 changed the implementation of
    `ChromeBrowser.port` so that it's dynamically set from a free port that
    `chromedriver` finds. This broke `HeadlessShellBrowser`, which tries to
    use the port number before it's available [0].
    
    As it turns out, there's no need for `HeadlessShellBrowser` to override
    `ChromeBrowser.make_command()`.
    
    [0]: https://ci.chromium.org/ui/p/chromium/builders/ci/linux-wpt-chromium-rel/9991/overview
    --
    
    wpt-commits: 29c14472098d77df411c93b824edc54612e5641a
    wpt-pr: 47399
    
    UltraBlame original commit: 908e6ea048dd5d897a9c7bc18db3daa29e62c6d9

--- testing/web-platform/tests/tools/wptrunner/wptrunner/browsers/headless_shell.py
+++ testing/web-platform/tests/tools/wptrunner/wptrunner/browsers/headless_shell.py
@@ -1,6 +1,6 @@
 
 
-from .base import cmd_arg, require_arg
+from .base import require_arg
 from .base import get_timeout_multiplier   
 from .chrome import ChromeBrowser, debug_args, executor_kwargs  
 from ..executors.base import WdspecExecutor  
@@ -55,8 +55,4 @@ def update_properties():
 
 
 class HeadlessShellBrowser(ChromeBrowser):
-    def make_command(self):
-        return [self.webdriver_binary,
-                cmd_arg("port", str(self.port)),
-                cmd_arg("url-base", self.base_path),
-                cmd_arg("enable-chrome-logs")] + self.webdriver_args
+    pass
