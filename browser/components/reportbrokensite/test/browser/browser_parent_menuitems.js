e200ab931e4e258551661c45be4ea11f26c44c4e
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-07 08:31:05 +0000

    Bug 1896783 - don't rely on the ReportBrokenSite object existing as a window property, r=twisniewski,pbz,webcompat-reviewers
    Unfortunately the addition in browser-siteProtections.js still makes this a
    variable on the browser window, but refactoring the contents of that file to
    be module-based is a separate effort.
    
    Differential Revision: https://phabricator.services.mozilla.com/D210434
    
    UltraBlame original commit: bd066196ee77868018fd3a8514690b0db3c72a7f

--- browser/components/reportbrokensite/test/browser/browser_parent_menuitems.js
+++ browser/components/reportbrokensite/test/browser/browser_parent_menuitems.js
@@ -43,7 +43,7 @@ add_task(async function testOtherMenus() {
   const menus = [appMenu, ProtectionsPanel(), HelpMenu()];
 
   async function forceMenuItemStateUpdate() {
-    window.ReportBrokenSite.enableOrDisableMenuitems(window);
+    ReportBrokenSite.enableOrDisableMenuitems(window);
 
     
     
