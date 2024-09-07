4a02e47ba5cb87aef5732a2e0e4c56640d3eb281
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-07 08:01:50 +0000

    Bug 1909181 - Revert bug 1788504 partially. r=emilio,geckoview-reviewers,ohall
    Since bug 1788504 the root scroll container's clipped region had been
    *over-clipped* because GetScrollPortRectAccountingForMaxDynamicToolbar returns
    the toolbar height divided by the current pinch zoom ratio. For example, given
    that the toolbar height is 60px (in screen units) and the pinch zoom ratio is
    2.0, then GetScrollPortRectAccountingForMaxDynamicToolbar returns 30px (in CSS
    units), it's not sufficient for the clipped region.
    
    The JUnit test in this change fails without this change, succeeds with this
    change.
    
    Differential Revision: https://phabricator.services.mozilla.com/D217924
    
    UltraBlame original commit: 8d92be4e0f88c55cece420e4bcf6d152ce77d6ec

--- mobile/android/geckoview/src/androidTest/java/org/mozilla/geckoview/test/DynamicToolbarTest.kt
+++ mobile/android/geckoview/src/androidTest/java/org/mozilla/geckoview/test/DynamicToolbarTest.kt
@@ -21,6 +21,8 @@ import org.mozilla.geckoview.GeckoResult
 import org.mozilla.geckoview.GeckoSession
 import org.mozilla.geckoview.GeckoSession.ContentDelegate
 import org.mozilla.geckoview.GeckoSession.ScrollDelegate
+import org.mozilla.geckoview.PanZoomController
+import org.mozilla.geckoview.ScreenLength
 import org.mozilla.geckoview.test.rule.GeckoSessionTestRule.AssertCalled
 import org.mozilla.geckoview.test.rule.GeckoSessionTestRule.WithDisplay
 import java.io.ByteArrayOutputStream
@@ -1044,5 +1046,40 @@ class DynamicToolbarTest : BaseSessionTest() {
             backgroundColor,
             equalTo("rgb(0, 128, 0)"),
         )
+    }
+
+    @WithDisplay(height = SCREEN_HEIGHT, width = SCREEN_WIDTH)
+    @Test
+    fun bug1909181() {
+        val reference = getComparisonScreenshot(SCREEN_WIDTH, SCREEN_HEIGHT)
+
+        val dynamicToolbarMaxHeight = SCREEN_HEIGHT / 2
+        sessionRule.display?.run { setDynamicToolbarMaxHeight(dynamicToolbarMaxHeight) }
+
+        
+        mainSession.setActive(true)
+
+        mainSession.loadTestPath(BaseSessionTest.BUG1909181_HTML_PATH)
+        mainSession.waitForPageStop()
+
+        
+        mainSession.setResolutionAndScaleTo(5.0f)
+        mainSession.flushApzRepaints()
+
+        mainSession.panZoomController.scrollBy(
+            ScreenLength.zero(),
+            ScreenLength.fromPixels(500.0),
+            PanZoomController.SCROLL_BEHAVIOR_AUTO,
+        )
+
+        
+        sessionRule.display?.run { setVerticalClipping(-dynamicToolbarMaxHeight) }
+
+        mainSession.flushApzRepaints()
+        mainSession.flushApzRepaints()
+
+        sessionRule.display?.let {
+            assertScreenshotResult(it.capturePixels(), reference)
+        }
     }
 }
