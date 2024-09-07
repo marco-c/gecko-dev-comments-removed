1b38ff2b796f33c320a04b07dcd691e091d923de
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-07 08:24:04 +0000

    Bug 1908496 - adjust defaults for news/snews/nntp protocols, r=dveditz
    Differential Revision: https://phabricator.services.mozilla.com/D216938
    
    UltraBlame original commit: 4c1e4e121b5ddf777e0aadf9236c0030e39160f2

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1217,18 +1217,12 @@ pref("network.manage-offline-status", true);
 
 
 pref("network.protocol-handler.external.mailto", true); 
-pref("network.protocol-handler.external.news", true);   
-pref("network.protocol-handler.external.snews", true);  
-pref("network.protocol-handler.external.nntp", true);   
 #ifdef XP_WIN
   pref("network.protocol-handler.external.ms-windows-store", true);
 #endif
 
 
 pref("network.protocol-handler.warn-external.mailto", false);
-pref("network.protocol-handler.warn-external.news", false);
-pref("network.protocol-handler.warn-external.snews", false);
-pref("network.protocol-handler.warn-external.nntp", false);
 #ifdef XP_WIN
   pref("network.protocol-handler.warn-external.ms-windows-store", false);
 #endif
