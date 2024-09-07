67203eb27fabb1de482cf3c4d75426040e2a215f
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-07 07:38:32 +0000

    Bug 1911920 - Gtest for ColorProfileConversionDesc between Srgb and Displayp3. r=gfx-reviewers,lsalzman
    Differential Revision: https://phabricator.services.mozilla.com/D218697
    
    UltraBlame original commit: 1dcb80bc6b27bca51750bc4c27b7bea55b408fc9

--- gfx/gl/Colorspaces.h
+++ gfx/gl/Colorspaces.h
@@ -10,8 +10,6 @@
 
 
 
-
-
 #include <algorithm>
 #include <array>
 #include <cmath>
@@ -19,6 +17,7 @@
 #include <cstdlib>
 #include <functional>
 #include <optional>
+#include <ostream>
 #include <vector>
 
 #include "AutoMappable.h"
@@ -269,6 +268,8 @@ struct avec final {
 
   
 
+  constexpr auto size() const { return data.size(); }
+
   const auto& operator[](const size_t n) const { return data[n]; }
   auto& operator[](const size_t n) { return data[n]; }
 
@@ -330,6 +331,16 @@ struct avec final {
       eq &= (a[i] == b[i]);
     }
     return eq;
+  }
+
+  friend std::ostream& operator<<(std::ostream& s, const avec& a) {
+    s << "avec(";
+    const char* delim = "";
+    for (const auto& v : a.data) {
+      s << delim << v;
+      delim = ", ";
+    }
+    return s << ")";
   }
 };
 using vec2 = avec<float, 2>;
