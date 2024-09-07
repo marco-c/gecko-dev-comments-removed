59208fe22bcb416b7f07c7852888c3dfc1a95192
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-07 06:40:08 +0000

    Bug 1865896 - Sort include headers in AudioParam.cpp per coding style r=media-playback-reviewers,padenot
    Depends on D209451
    
    Differential Revision: https://phabricator.services.mozilla.com/D209452
    
    UltraBlame original commit: b71c89fc4c6be88156a537b67023e186bf5994ff

--- dom/media/webaudio/AudioParam.cpp
+++ dom/media/webaudio/AudioParam.cpp
@@ -5,10 +5,11 @@
 
 
 #include "AudioParam.h"
-#include "mozilla/dom/AudioParamBinding.h"
+
+#include "AudioContext.h"
 #include "AudioNodeEngine.h"
 #include "AudioNodeTrack.h"
-#include "AudioContext.h"
+#include "mozilla/dom/AudioParamBinding.h"
 
 namespace mozilla::dom {
 
