495ea32297f7b1c6e11d6c9458e73e0d6e7057bd
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-07 08:56:02 +0000

    Bug 1910287 - Make `NativeKey::GetFollowingCharMessage()` return `true` even if `wParam` of `WM_DEADCHAR` is `0` r=m_kato
    Although I don't know why Ergo-L keyboard layout behave differently on my
    environment and the reporter's environment.  In the reporter's environment,
    second dead key press causes `WM_DEADCHAR` with `0` `wParam`. Then,
    `GetFollowingCharMessage()` returns `false` so that it's not stored into
    `mFollowingCharMsgs`.  Then, `NativeKey::IsFollowedByDeadCharMessage()` returns
    `false` and `NativeKey::mIsDeadKey` is set to `false`.  Therefore, `NativeKey`
    dispatches only `eKeyDown` event but whose `.key` is `"g"` rather than
    `DeadKey` and finally, fails to call `KeyboardLayout::ActivateDeadKeyState()`.
    Therefore the following dead key sequence is not treated as so.
    
    This patch makes it return `true` for such `WM_DEADCHAR` message so that the
    `WM_KEYDOWN` message will be treated as a dead key sequence.
    
    Differential Revision: https://phabricator.services.mozilla.com/D219317
    
    UltraBlame original commit: ce5d28c24de5df5b6752947828e25fd686aef05e

--- widget/windows/KeyboardLayout.cpp
+++ widget/windows/KeyboardLayout.cpp
@@ -1598,7 +1598,9 @@ void NativeKey::InitWithKeyOrChar() {
       MOZ_LOG(gKeyLog, LogLevel::Info,
               ("%p   NativeKey::InitWithKeyOrChar(), removed char message, %s",
                this, ToString(charMsg).get()));
-      Unused << NS_WARN_IF(charMsg.hwnd != mMsg.hwnd);
+      NS_WARNING_ASSERTION(
+          charMsg.hwnd == mMsg.hwnd,
+          "The retrieved char message was targeted to differnet window");
       mFollowingCharMsgs.AppendElement(charMsg);
     }
     if (mFollowingCharMsgs.Length() == 1) {
@@ -1636,14 +1638,15 @@ void NativeKey::InitWithKeyOrChar() {
                "surrogate input, but received lone low surrogate input",
                this));
         }
-      } else {
+      } else if (MOZ_UNLIKELY(pendingHighSurrogate)) {
         MOZ_LOG(gKeyLog, LogLevel::Warning,
                 ("%p   NativeKey::InitWithKeyOrChar(), there is pending "
                  "high surrogate input, but received non-surrogate input.  "
                  "The high surrogate input is discarded",
                  this));
       }
-    } else if (pendingHighSurrogate && !mFollowingCharMsgs.IsEmpty()) {
+    } else if (MOZ_UNLIKELY(pendingHighSurrogate &&
+                            !mFollowingCharMsgs.IsEmpty())) {
       MOZ_LOG(gKeyLog, LogLevel::Warning,
               ("%p   NativeKey::InitWithKeyOrChar(), there is pending "
                "high surrogate input, but received 2 or more character input.  "
@@ -3159,8 +3162,7 @@ bool NativeKey::GetFollowingCharMessage(MSG& aCharMsg) {
             gKeyLog, LogLevel::Warning,
             ("%p   NativeKey::GetFollowingCharMessage(), WARNING, failed to "
              "remove a char message and next key message becomes differnt "
-             "key's "
-             "char message, nextKeyMsgInAllWindows=%s, nextKeyMsg=%s, "
+             "key's char message, nextKeyMsgInAllWindows=%s, nextKeyMsg=%s, "
              "kFoundCharMsg=%s",
              this, ToString(nextKeyMsgInAllWindows).get(),
              ToString(nextKeyMsg).get(), ToString(kFoundCharMsg).get()));
@@ -3308,7 +3310,8 @@ bool NativeKey::GetFollowingCharMessage(MSG& aCharMsg) {
     
     
     
-    if (removedMsg.message == nextKeyMsg.message && !removedMsg.wParam) {
+    if (removedMsg.message != WM_DEADCHAR &&
+        removedMsg.message == nextKeyMsg.message && !removedMsg.wParam) {
       MOZ_LOG(
           gKeyLog, LogLevel::Warning,
           ("%p   NativeKey::GetFollowingCharMessage(), WARNING, succeeded to "
@@ -4121,13 +4124,13 @@ bool KeyboardLayout::MaybeInitNativeKeyAsDeadKey(NativeKey& aNativeKey) {
 
   
   
-  bool isDeadKeyDownEvent =
+  const bool isDeadKeyDownEvent =
       aNativeKey.IsKeyDownMessage() && aNativeKey.IsFollowedByDeadCharMessage();
 
   
   
   
-  bool isDeadKeyUpEvent =
+  const bool isDeadKeyUpEvent =
       !aNativeKey.IsKeyDownMessage() &&
       mActiveDeadKeys.Contains(aNativeKey.GenericVirtualKeyCode());
 
