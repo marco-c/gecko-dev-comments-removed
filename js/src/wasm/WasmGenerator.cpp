4d761b540b4842ee229d282ba7262fcb6775afbf
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-07 07:30:09 +0000

    Bug 1855960 - [MIPS64] Implement SummarizeTrapInstruction. r=jseward
    Ref: https://bugzilla.mozilla.org/attachment.cgi?id=9365788&action=diff
    
    Co-Authored-By: Zhao Jiazhong <zhaojiazhong-hfloongson.cn>
    
    Patch contributed by gur3n (Jiangjin Wang <thesalted.fish>)
    
    Differential Revision: https://phabricator.services.mozilla.com/D195052
    
    UltraBlame original commit: 5a0b16abb1e7ac6942666a9fcc98f5aa14a46023

--- js/src/wasm/WasmGenerator.cpp
+++ js/src/wasm/WasmGenerator.cpp
@@ -804,7 +804,7 @@ static void CheckCodeBlock(const CodeBlock& codeBlock) {
 
 #  if (defined(JS_CODEGEN_X64) || defined(JS_CODEGEN_X86) ||   \
        defined(JS_CODEGEN_ARM64) || defined(JS_CODEGEN_ARM) || \
-       defined(JS_CODEGEN_LOONG64))
+       defined(JS_CODEGEN_LOONG64) || defined(JS_CODEGEN_MIPS64))
   
   
   
