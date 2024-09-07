3933fd47dbe726899ff918af55a9a985b23cd6be
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-07 07:54:47 +0000

    Bug 1911644 [wpt PR 47464] - Split sandboxed iframe tests into two files., a=testonly
    Automatic update from web-platform-tests
    Split sandboxed iframe tests into two files.
    
    This is an attempt to address the flakiness in the tests introduced by
    crrev.com/5685772. Instead of creating two iframes in the same window in
    and running the tests in parallel, this will run as two entirely
    separate tests.
    
    Bug: 353116462
    Change-Id: I446977baf1c8fc99dec6ae85434a08e4bb4b2ccf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5756961
    Reviewed-by: Chris Fredrickson <cfredricchromium.org>
    Auto-Submit: Erica Kovac <ekovacgoogle.com>
    Commit-Queue: Erica Kovac <ekovacgoogle.com>
    Reviewed-by: Dylan Cutler <dylancutlergoogle.com>
    Cr-Commit-Position: refs/heads/main{#1337428}
    
    --
    
    wpt-commits: e441a482834a242f57e9eee2db4458cdaf0633a0
    wpt-pr: 47464
    
    UltraBlame original commit: c0a9ea42f9e69351b124e4e39706069bddaed64b

--- testing/web-platform/tests/storage-access-api/requestStorageAccess-sandboxed-iframe-allow-storage-access.sub.https.window.js
+++ testing/web-platform/tests/storage-access-api/requestStorageAccess-sandboxed-iframe-allow-storage-access.sub.https.window.js
@@ -1,29 +1,68 @@
 
 
 
+
 'use strict';
 
-test(() => {
-  let iframe = document.createElement('iframe');
-  assert_true(iframe.sandbox.supports('allow-storage-access-by-user-activation'), '`allow-storage-access-by-user-activation`' +
-    'sandbox attribute should be supported');
-}, "`allow-storage-access-by-user-activation` sandbox attribute is supported");
 
-(async function () {
-  const frameSourceUrl = 'https://{{hosts[alt][www]}}:{{ports[https][0]}}/storage-access-api/requestStorageAccess-sandboxed-iframe.sub.https.window.html';
 
-  let sandboxAttribute =
-    'allow-scripts allow-same-origin';
-  let testCase = 'sandboxed-iframe';
 
-  RunTestsInIFrame(
-    frameSourceUrl + `?testCase=${testCase}`,
-    sandboxAttribute);
 
-  sandboxAttribute += ' allow-storage-access-by-user-activation';
-  testCase = 'sandboxed-iframe-allow-storage-access-by-user-activation';
+
+
+
+const {testPrefix, topLevelDocument} = processQueryParams();
+
+if (topLevelDocument) {
+  const frameSourceUrl =
+      'https://{{hosts[alt][www]}}:{{ports[https][0]}}/storage-access-api/requestStorageAccess-sandboxed-iframe-allow-storage-access.sub.https.window.html';
+
+  const sandboxAttribute =
+      'allow-scripts allow-same-origin allow-storage-access-by-user-activation';
+  const testCase = 'sandboxed-iframe-allow-storage-access-by-user-activation';
 
   RunTestsInIFrame(
     frameSourceUrl + `?testCase=${testCase}`,
     sandboxAttribute);
-})();
+} else {
+  test(() => {
+    let iframe = document.createElement('iframe');
+    assert_true(
+        iframe.sandbox.supports('allow-storage-access-by-user-activation'),
+        '`allow-storage-access-by-user-activation`' +
+            'sandbox attribute should be supported');
+  }, '`allow-storage-access-by-user-activation` sandbox attribute is supported');
+
+  
+  
+  
+  
+  promise_test(async t => {
+    await test_driver.set_permission({name: 'storage-access'}, 'granted');
+    await MaybeSetStorageAccess('*', '*', 'blocked');
+    await document.requestStorageAccess();
+
+    assert_true(
+        await CanAccessCookiesViaHTTP(),
+        'After obtaining storage access, subresource requests from the frame should send and set cookies.');
+    assert_true(
+        CanAccessCookiesViaJS(),
+        'After obtaining storage access, scripts in the frame should be able to access cookies.');
+  }, `[${testPrefix}] document.requestStorageAccess() should resolve even without a user gesture when already granted.`);
+
+  promise_test(async () => {
+    await test_driver.set_permission({name: 'storage-access'}, 'granted');
+    await MaybeSetStorageAccess('*', '*', 'blocked');
+
+    await RunCallbackWithGesture(async () => {
+      await document.requestStorageAccess();
+    });
+
+    assert_true(
+        await CanAccessCookiesViaHTTP(),
+        'After obtaining storage access, subresource requests from the frame should send and set cookies.');
+    assert_true(
+        CanAccessCookiesViaJS(),
+        'After obtaining storage access, scripts in the frame should be able to access cookies.');
+  }, `[${testPrefix}] document.requestStorageAccess() should resolve with a user gesture`);
+}
---
+++ testing/web-platform/tests/storage-access-api/requestStorageAccess-sandboxed-iframe-no-storage-access.sub.https.window.js
@@ -0,0 +1,53 @@
+
+
+
+
+'use strict';
+
+
+
+
+
+
+
+
+const {testPrefix, topLevelDocument} = processQueryParams();
+
+if (topLevelDocument) {
+  const frameSourceUrl =
+      'https://{{hosts[alt][www]}}:{{ports[https][0]}}/storage-access-api/requestStorageAccess-sandboxed-iframe-no-storage-access.sub.https.window.html';
+
+  let sandboxAttribute = 'allow-scripts allow-same-origin';
+  let testCase = 'sandboxed-iframe';
+
+  RunTestsInIFrame(frameSourceUrl + `?testCase=${testCase}`, sandboxAttribute);
+} else {
+  test(() => {
+    let iframe = document.createElement('iframe');
+    assert_true(
+        iframe.sandbox.supports('allow-storage-access-by-user-activation'),
+        '`allow-storage-access-by-user-activation`' +
+            'sandbox attribute should be supported');
+  }, '`allow-storage-access-by-user-activation` sandbox attribute is supported');
+  promise_test(
+      async t => {
+        await test_driver.set_permission({name: 'storage-access'}, 'granted');
+        await MaybeSetStorageAccess('*', '*', 'blocked');
+        return promise_rejects_dom(
+            t, 'NotAllowedError', document.requestStorageAccess(),
+            'document.requestStorageAccess() call without user gesture.');
+      },
+      '[' + testPrefix +
+          '] document.requestStorageAccess() should reject with a NotAllowedError with no user gesture.');
+
+  promise_test(async t => {
+    await test_driver.set_permission({name: 'storage-access'}, 'granted');
+    await MaybeSetStorageAccess('*', '*', 'blocked');
+
+    await RunCallbackWithGesture(async () => {
+      await promise_rejects_dom(
+          t, 'NotAllowedError', document.requestStorageAccess(),
+          'document.requestStorageAccess() call with user gesture.');
+    });
+  }, `[${testPrefix}] document.requestStorageAccess() should reject with a NotAllowedError, even with a user gesture`);
+}
