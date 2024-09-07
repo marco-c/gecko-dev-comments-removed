bc718f748e01a3c3082031c2bda9733888dc339a
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-07 08:38:51 +0000

    Bug 1892400: Remove the startup token from the non-unix remote implementations. r=jhirsch,glandium
    This simplifies the call signatures of all versions since this is only needed in unix.
    
    Differential Revision: https://phabricator.services.mozilla.com/D216032
    
    UltraBlame original commit: 56ee677019292f3577bc1402e2aeafa051ad70c4

--- toolkit/components/remote/nsRemoteClient.h
+++ toolkit/components/remote/nsRemoteClient.h
@@ -37,16 +37,8 @@ class nsRemoteClient {
 
 
 
-
-
-
-
-
-
-
   virtual nsresult SendCommandLine(const char* aProgram, const char* aProfile,
-                                   int32_t argc, const char** argv,
-                                   const char* aStartupToken) = 0;
+                                   int32_t argc, const char** argv) = 0;
 };
 
 #endif  
