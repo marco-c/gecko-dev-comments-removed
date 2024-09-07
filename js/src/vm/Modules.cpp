a28c2d780dd9bfd85a735946547ad34128f456ef
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-07 10:54:26 +0000

    Bug 1877792 - Part 5: Use AutoReportFrontendContext in CompileJsonModule for error reporting r=bthrall
    We need to change `JSONFullParseHandler::reportError` (when compiling JSON
    modules), so that we create syntax errors with the required metadata attached
    (line, column, filename).
    
    Differential Revision: https://phabricator.services.mozilla.com/D204174
    
    UltraBlame original commit: 0e95f906a9991b96d6ec395bd2d23d62bb113036

--- js/src/vm/Modules.cpp
+++ js/src/vm/Modules.cpp
@@ -31,6 +31,7 @@
 #include "vm/JSAtomUtils.h"             
 #include "vm/JSContext.h"               
 #include "vm/JSObject.h"                
+#include "vm/JSONParser.h"              
 #include "vm/List.h"                    
 #include "vm/Runtime.h"                 
 
@@ -159,10 +160,16 @@ JS_PUBLIC_API JSObject* JS::CompileJsonModule(
   AssertHeapIsIdle();
   CHECK_THREAD(cx);
 
-  JS::RootedValue jsonValue(cx);
   auto charRange =
       mozilla::Range<const char16_t>(srcBuf.get(), srcBuf.length());
-  if (!js::ParseJSONWithReviver(cx, charRange, NullHandleValue, &jsonValue)) {
+  Rooted<JSONParser<char16_t>> parser(
+      cx, cx, charRange, JSONParser<char16_t>::ParseType::JSONParse);
+
+  parser.reportLineNumbersFromParsedData(true);
+  parser.setFilename(options.filename());
+
+  JS::RootedValue jsonValue(cx);
+  if (!parser.parse(&jsonValue)) {
     return nullptr;
   }
 
