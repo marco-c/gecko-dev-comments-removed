a0c35e29d85dd5799e606a075b233ac490b02233
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-07 07:58:31 +0000

    Bug 1901507 - Upgrade ohttp crate to latest version (0.5.1) r=necko-reviewers,glandium,supply-chain-reviewers,kershaw
    Differential Revision: https://phabricator.services.mozilla.com/D213526
    
    UltraBlame original commit: b029aa8259d69305ec157f0a848ad7f5d56a8327

--- third_party/rust/ohttp/src/rh/aead.rs
+++ third_party/rust/ohttp/src/rh/aead.rs
@@ -54,7 +54,7 @@ impl AeadEngine {
 
 pub struct Aead {
     mode: Mode,
-    aead: AeadEngine,
+    engine: AeadEngine,
     nonce_base: [u8; NONCE_LEN],
     seq: SequenceNumber,
 }
@@ -80,7 +80,7 @@ impl Aead {
         };
         Ok(Self {
             mode,
-            aead,
+            engine: aead,
             nonce_base,
             seq: 0,
         })
@@ -105,14 +105,14 @@ impl Aead {
         
         let nonce = self.nonce(self.seq);
         self.seq += 1;
-        let ct = self.aead.encrypt(&nonce, Payload { msg: pt, aad })?;
+        let ct = self.engine.encrypt(&nonce, Payload { msg: pt, aad })?;
         Ok(ct)
     }
 
     pub fn open(&mut self, aad: &[u8], seq: SequenceNumber, ct: &[u8]) -> Res<Vec<u8>> {
         assert_eq!(self.mode, Mode::Decrypt);
         let nonce = self.nonce(seq);
-        let pt = self.aead.decrypt(&nonce, Payload { msg: ct, aad })?;
+        let pt = self.engine.decrypt(&nonce, Payload { msg: ct, aad })?;
         Ok(pt)
     }
 }
