62f022e6b0bc3dab7db8840f2d0f231a684ff9d5
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-07 10:45:57 +0000

    Bug 1915971 - Remove some no longer needed code to handle transform attribute changes. r=longsonr
    These are handled as part of the transform property changing now.
    
    Differential Revision: https://phabricator.services.mozilla.com/D220729
    
    UltraBlame original commit: 0a2beb7878dfb20aaf868e20812f1aca0239042a

--- dom/svg/SVGTransformableElement.h
+++ dom/svg/SVGTransformableElement.h
@@ -34,15 +34,8 @@ class SVGTransformableElement : public SVGElement {
   already_AddRefed<DOMSVGAnimatedTransformList> Transform();
 
   
-  nsChangeHint GetAttributeChangeHint(const nsAtom* aAttribute,
-                                      int32_t aModType) const override;
-
-  
   bool IsEventAttributeNameInternal(nsAtom* aName) override;
 
-  gfxMatrix PrependLocalTransformsTo(
-      const gfxMatrix& aMatrix,
-      SVGTransformTypes aWhich = eAllTransforms) const override;
   const gfx::Matrix* GetAnimateMotionTransform() const override;
   void SetAnimateMotionTransform(const gfx::Matrix* aMatrix) override;
   NS_IMETHOD_(bool) IsAttributeMapped(const nsAtom* aAttribute) const override;
