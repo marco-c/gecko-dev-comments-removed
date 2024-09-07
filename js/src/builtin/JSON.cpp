921893f79feeb16c77e2150f1ea04d16110ccb79
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-07 09:44:05 +0000

    Bug 1904184 - Blame JSON parsing on the JS profiling category r=spidermonkey-reviewers,jesup,jandem
    Differential Revision: https://phabricator.services.mozilla.com/D219702
    
    UltraBlame original commit: 8661a1a78386a6be0d802a70a76f4a94eee8b67e

--- js/src/builtin/JSON.cpp
+++ js/src/builtin/JSON.cpp
@@ -25,6 +25,7 @@
 #include "js/friend/StackLimits.h"    
 #include "js/Object.h"                
 #include "js/Prefs.h"                 
+#include "js/ProfilingCategory.h"
 #include "js/PropertySpec.h"
 #include "js/StableStringChars.h"
 #include "js/TypeDecls.h"
@@ -1962,6 +1963,8 @@ template <typename CharT>
 bool js::ParseJSONWithReviver(JSContext* cx,
                               const mozilla::Range<const CharT> chars,
                               HandleValue reviver, MutableHandleValue vp) {
+  js::AutoGeckoProfilerEntry pseudoFrame(cx, "parse JSON",
+                                         JS::ProfilingCategoryPair::JS_Parsing);
   
   Rooted<ParseRecordObject> pro(cx);
 #ifdef ENABLE_JSON_PARSE_WITH_SOURCE
