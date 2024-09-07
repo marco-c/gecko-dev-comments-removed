28388ddf301ce0881f454f6221cb7ceb9206d612
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-07 10:46:37 +0000

    Bug 1907034 - Allow using shift-enter/shift-click to open the search engine page without a search from the search bar. r=Standard8
    Differential Revision: https://phabricator.services.mozilla.com/D220446
    
    UltraBlame original commit: fe632ea0e3ecac7d6160bbe7ff8f483579519134

--- browser/components/search/content/autocomplete-popup.js
+++ browser/components/search/content/autocomplete-popup.js
@@ -69,11 +69,11 @@
         if (!engine) {
           return;
         }
-        
-        if (!this.searchbar.value) {
-          return;
+        if (this.searchbar.value) {
+          this.oneOffButtons.handleSearchCommand(event, engine);
+        } else if (event.shiftKey) {
+          this.openSearchForm(event, engine);
         }
-        this.oneOffButtons.handleSearchCommand(event, engine);
       });
 
       this._bundle = null;
@@ -260,6 +260,14 @@
     
     handleOneOffSearch(event, engine, where, params) {
       this.searchbar.handleSearchCommandWhere(event, engine, where, params);
+    }
+
+    openSearchForm(event, engine, forceNewTab = false) {
+      let { where, params } = this.oneOffButtons._whereToOpen(
+        event,
+        forceNewTab
+      );
+      this.searchbar.openSearchFormWhere(event, engine, where, params);
     }
 
     
--- browser/components/search/content/searchbar.js
+++ browser/components/search/content/searchbar.js
@@ -306,52 +306,14 @@
     }
 
     handleSearchCommand(aEvent, aEngine, aForceNewTab) {
-      let where = "current";
-      let params;
-      const newTabPref = Services.prefs.getBoolPref("browser.search.openintab");
-
-      
       if (
         aEvent &&
-        aEvent.originalTarget.classList.contains("search-go-button")
+        aEvent.originalTarget.classList.contains("search-go-button") &&
+        aEvent.button == 2
       ) {
-        if (aEvent.button == 2) {
-          return;
-        }
-        where = lazy.BrowserUtils.whereToOpenLink(aEvent, false, true);
-        if (
-          newTabPref &&
-          !aEvent.altKey &&
-          !aEvent.getModifierState("AltGraph") &&
-          where == "current" &&
-          !gBrowser.selectedTab.isEmpty
-        ) {
-          where = "tab";
-        }
-      } else if (aForceNewTab) {
-        where = "tab";
-        if (Services.prefs.getBoolPref("browser.tabs.loadInBackground")) {
-          where += "-background";
-        }
-      } else {
-        if (
-          (KeyboardEvent.isInstance(aEvent) &&
-            (aEvent.altKey || aEvent.getModifierState("AltGraph"))) ^
-            newTabPref &&
-          !gBrowser.selectedTab.isEmpty
-        ) {
-          where = "tab";
-        }
-        if (
-          MouseEvent.isInstance(aEvent) &&
-          (aEvent.button == 1 || aEvent.getModifierState("Accel"))
-        ) {
-          where = "tab";
-          params = {
-            inBackground: true,
-          };
-        }
+        return;
       }
+      let { where, params } = this._whereToOpen(aEvent, aForceNewTab);
       this.handleSearchCommandWhere(aEvent, aEngine, where, params);
     }
 
@@ -447,6 +409,97 @@
         }
       }
       openTrustedLinkIn(submission.uri.spec, aWhere, params);
+    }
+
+    
+
+
+
+
+
+
+
+
+
+
+
+    _whereToOpen(aEvent, aForceNewTab = false) {
+      let where = "current";
+      let params = {};
+      const newTabPref = Services.prefs.getBoolPref("browser.search.openintab");
+
+      
+      if (aEvent?.originalTarget.classList.contains("search-go-button")) {
+        where = lazy.BrowserUtils.whereToOpenLink(aEvent, false, true);
+        if (
+          newTabPref &&
+          !aEvent.altKey &&
+          !aEvent.getModifierState("AltGraph") &&
+          where == "current" &&
+          !gBrowser.selectedTab.isEmpty
+        ) {
+          where = "tab";
+        }
+      } else if (aForceNewTab) {
+        where = "tab";
+        if (Services.prefs.getBoolPref("browser.tabs.loadInBackground")) {
+          params = {
+            inBackground: true,
+          };
+        }
+      } else {
+        if (
+          (KeyboardEvent.isInstance(aEvent) &&
+            (aEvent.altKey || aEvent.getModifierState("AltGraph"))) ^
+            newTabPref &&
+          !gBrowser.selectedTab.isEmpty
+        ) {
+          where = "tab";
+        }
+        if (
+          MouseEvent.isInstance(aEvent) &&
+          (aEvent.button == 1 || aEvent.getModifierState("Accel"))
+        ) {
+          where = "tab";
+          params = {
+            inBackground: true,
+          };
+        }
+      }
+
+      return { where, params };
+    }
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+    openSearchFormWhere(aEvent, aEngine, where, params = {}) {
+      let engine = aEngine || this.currentEngine;
+      let searchForm = engine.wrappedJSObject.searchForm;
+
+      if (where === "tab" && !!params.inBackground) {
+        
+        params.avoidBrowserFocus = true;
+      } else if (
+        where !== "window" &&
+        aEvent.keyCode === KeyEvent.DOM_VK_RETURN
+      ) {
+        
+        params.avoidBrowserFocus = true;
+        this._needBrowserFocusAtEnterKeyUp = true;
+      }
+
+      openTrustedLinkIn(searchForm, where, params);
     }
 
     disconnectedCallback() {
@@ -807,6 +860,11 @@
             )
           )
         ) {
+          if (event.shiftKey) {
+            let engine = this.textbox.selectedButton?.engine;
+            let { where, params } = this._whereToOpen(event);
+            this.openSearchFormWhere(event, engine, where, params);
+          }
           return true;
         }
         
