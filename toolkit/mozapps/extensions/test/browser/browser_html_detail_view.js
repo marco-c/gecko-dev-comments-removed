076ecedb387b508b570dedbda9d6ad983edd5be0
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-16 01:22:27 +0000

    Bug 1918317 - Remove --button-bgcolor, --button-hover-bgcolor, --button-active-bgcolor. r=desktop-theme-reviewers,emilio
    Differential Revision: https://phabricator.services.mozilla.com/D221922
    
    UltraBlame original commit: 8a5109897528f074dcd9458263cdec48e41100de

--- toolkit/mozapps/extensions/test/browser/browser_html_detail_view.js
+++ toolkit/mozapps/extensions/test/browser/browser_html_detail_view.js
@@ -827,7 +827,7 @@ add_task(async function testDefaultTheme() {
   
   let version = rows.shift();
   await checkLabel(version, "version");
-  is(version.lastChild.textContent, "1.3", "It's always version 1.3");
+  is(version.lastChild.textContent, "1.4", "It's always version 1.4");
 
   
   let lastUpdated = rows.shift();
