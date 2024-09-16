53d51a5b6b49706682d1644e22b0942d927edba1
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-16 01:14:54 +0000

    Bug 1475599 - part 9 - CookieStore API - Extra security for cookie propagation child to parent, r=tschuster,cookie-reviewers,valentin
    Differential Revision: https://phabricator.services.mozilla.com/D217734
    
    UltraBlame original commit: 8620daea37ad7dbe01ccd5d7973c579bc1e85faf

--- dom/cookiestore/CookieStoreParent.h
+++ dom/cookiestore/CookieStoreParent.h
@@ -8,6 +8,7 @@
 #define mozilla_dom_CookieStoreParent_h
 
 #include "mozilla/dom/PCookieStoreParent.h"
+#include "mozilla/dom/ContentParent.h"
 #include "mozilla/MozPromise.h"
 
 namespace mozilla::dom {
@@ -56,7 +57,8 @@ class CookieStoreParent final : public PCookieStoreParent {
 
   
   
-  bool SetRequestOnMainThread(const nsAString& aDomain,
+  bool SetRequestOnMainThread(ThreadsafeContentParentHandle* aParent,
+                              const nsAString& aDomain,
                               const OriginAttributes& aOriginAttributes,
                               const nsAString& aName, const nsAString& aValue,
                               bool aSession, int64_t aExpires,
@@ -65,7 +67,8 @@ class CookieStoreParent final : public PCookieStoreParent {
 
   
   
-  bool DeleteRequestOnMainThread(const nsAString& aDomain,
+  bool DeleteRequestOnMainThread(ThreadsafeContentParentHandle* aParent,
+                                 const nsAString& aDomain,
                                  const OriginAttributes& aOriginAttributes,
                                  const nsAString& aName, const nsAString& aPath,
                                  bool aPartitioned, const nsID& aOperationID);
