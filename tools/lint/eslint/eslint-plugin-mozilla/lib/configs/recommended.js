3d6eae76b65c347518d22c63b585c9efb539bee3
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-16 01:28:43 +0000

    Bug 1917530 - Enable ESLint rule no-shadow on system modules where no-shadow is enabled already, and warn for remaining failures. r=frontend-codestyle-reviewers,mossop
    This moves the ESLint rule to apply to jsx, system modules and workers. It is excluded from most system
    modules by the roll out, which we can work on later. However, it will be enabled where no-shadow is
    already enabled - previously these were hidden due to the order in which ESLint was applying the rules.
    
    Differential Revision: https://phabricator.services.mozilla.com/D221444
    
    UltraBlame original commit: ebda61c817e4df78b307f4201cff5414c068aa7b

--- tools/lint/eslint/eslint-plugin-mozilla/lib/configs/recommended.js
+++ tools/lint/eslint/eslint-plugin-mozilla/lib/configs/recommended.js
@@ -71,6 +71,7 @@ module.exports = {
     {
       files: ["**/*.mjs", "**/*.jsx", "**/?(*.)worker.?(m)js"],
       rules: {
+        "no-shadow": ["error", { allow: ["event"], builtinGlobals: true }],
         
         
         "no-unused-vars": [
@@ -88,7 +89,6 @@ module.exports = {
       rules: {
         "mozilla/reject-import-system-module-from-non-system": "error",
         "mozilla/reject-lazy-imports-into-globals": "error",
-        "no-shadow": ["error", { allow: ["event"], builtinGlobals: true }],
       },
     },
     {
