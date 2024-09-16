7f2bda00420fd14a2e61a030a785cc127b2d3ca3
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-16 01:33:56 +0000

    Bug 1917763 - Remove eCSSUnit_Number handling from nsMathMLFrame::ParseNumericValue. r=emilio
    This case was used to handle non-zero unitless values, but support was
    removed in D89920.
    
    Differential Revision: https://phabricator.services.mozilla.com/D221789
    
    UltraBlame original commit: 8bf30d120d3356061ce8b2faa2b3936e2e671738

--- layout/mathml/nsMathMLFrame.cpp
+++ layout/mathml/nsMathMLFrame.cpp
@@ -244,7 +244,7 @@ void nsMathMLFrame::ParseNumericValue(const nsString& aString,
 
   nsCSSUnit unit = cssValue.GetUnit();
 
-  if (unit == eCSSUnit_Percent || unit == eCSSUnit_Number) {
+  if (unit == eCSSUnit_Percent) {
     
     *aLengthValue = NSToCoordRound(
         *aLengthValue * (unit == eCSSUnit_Percent ? cssValue.GetPercentValue()
