2c2399dc6d68a9c8e5a54409f043fafebc4af6ce
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-16 01:37:27 +0000

    Bug 1166408 - Part 4: Remove an unused out-param from LookupNameNoGC. r=spidermonkey-reviewers,jandem
    Differential Revision: https://phabricator.services.mozilla.com/D221668
    
    UltraBlame original commit: 231fde84c62f722197e01d03856e668ed4cae9b8

--- js/src/vm/JSObject.h
+++ js/src/vm/JSObject.h
@@ -862,8 +862,8 @@ extern bool LookupName(JSContext* cx, Handle<PropertyName*> name,
                        MutableHandleObject pobjp, PropertyResult* propp);
 
 extern bool LookupNameNoGC(JSContext* cx, PropertyName* name,
-                           JSObject* envChain, JSObject** objp,
-                           NativeObject** pobjp, PropertyResult* propp);
+                           JSObject* envChain, NativeObject** pobjp,
+                           PropertyResult* propp);
 
 
 
