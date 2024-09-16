b022e9cb2d3bfa1c575bfe13e4a699de1bb73630
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-16 01:30:11 +0000

    Bug 1918096 - In ScreenCapturerSck adapt to sources that change resolution. r=webrtc-reviewers,ng
    If the resolution is smaller than the allocated surface, we crop.
    If the resolution is larger than the allocated surface, we reconfigure with a
    larger surface.
    
    The allocated surface has the size we told it to have in the
    SCStreamConfiguration, in pixels.
    
    If the source is a screen, the size is pretty static. Changing display settings
      could affect it.
    If the source is a window, the size is initially the size of the window. The
      user resizing the window affects the size.
    If the source is multiple windows, the size initially seems to be that of the
      display they're sitting on. Windows across multiple displays are not captured
      into the same surface, as of macOS 14.
    
    Differential Revision: https://phabricator.services.mozilla.com/D221941
    
    UltraBlame original commit: bc6215142feaab1a3f7820006129219504851bc8

--- third_party/libwebrtc/modules/desktop_capture/mac/desktop_frame_iosurface.h
+++ third_party/libwebrtc/modules/desktop_capture/mac/desktop_frame_iosurface.h
@@ -26,7 +26,7 @@ class DesktopFrameIOSurface final : public DesktopFrame {
   
   
   static std::unique_ptr<DesktopFrameIOSurface> Wrap(
-      rtc::ScopedCFTypeRef<IOSurfaceRef> io_surface);
+      rtc::ScopedCFTypeRef<IOSurfaceRef> io_surface, CGRect rect = {});
 
   ~DesktopFrameIOSurface() override;
 
@@ -35,7 +35,12 @@ class DesktopFrameIOSurface final : public DesktopFrame {
 
  private:
   
-  explicit DesktopFrameIOSurface(rtc::ScopedCFTypeRef<IOSurfaceRef> io_surface);
+  DesktopFrameIOSurface(
+      rtc::ScopedCFTypeRef<IOSurfaceRef> io_surface,
+      uint8_t* data,
+      int32_t width,
+      int32_t height,
+      int32_t stride);
 
   const rtc::ScopedCFTypeRef<IOSurfaceRef> io_surface_;
 };
