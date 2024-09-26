b4fbef62555479c34d5dfb0387f07ad426920591
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-26 00:34:29 +0000

    Bug 1920071 - Fix JS load ordering r=atossou
    Differential Revision: https://phabricator.services.mozilla.com/D222948
    
    UltraBlame original commit: d3a9a96860bd585c103f55a0ea074948088b7d77

--- toolkit/components/aboutinference/content/aboutInference.js
+++ toolkit/components/aboutinference/content/aboutInference.js
@@ -548,10 +548,10 @@ function fillSelect(elementId, values) {
 
 
 window.onload = async function () {
-  await displayInfo();
-  loadExample("summary");
   fillSelect("dtype", DTYPE);
   fillSelect("taskName", TASKS);
+  loadExample("summary");
+  await displayInfo();
 
   document
     .getElementById("inferenceButton")
