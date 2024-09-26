d0e82fb928e6f79e3ce30d36829dd0c36361e6d5
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-26 00:13:35 +0000

    Bug 1913613 - Fix wrapping behaviour of setTimeout IDs and consolidate types to int32 r=smaug
    Differential Revision: https://phabricator.services.mozilla.com/D219404
    
    UltraBlame original commit: 81190e44c58650b45cb3eb5ae2c1e60bfae730de

--- dom/base/Timeout.h
+++ dom/base/Timeout.h
@@ -38,7 +38,7 @@ class Timeout final : protected LinkedListElement<RefPtr<Timeout>> {
   };
 
   struct TimeoutIdAndReason {
-    uint32_t mId;
+    int32_t mId;
     Reason mReason;
   };
 
@@ -157,7 +157,7 @@ class Timeout final : protected LinkedListElement<RefPtr<Timeout>> {
   UniquePtr<ProfileChunkedBuffer> mCause;
 
   
-  uint32_t mTimeoutId;
+  int32_t mTimeoutId;
 
   
   
