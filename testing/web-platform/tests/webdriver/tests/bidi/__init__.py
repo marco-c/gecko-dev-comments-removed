425fb3fe8328848402281b336e456ba0bd0b4d56
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-26 01:11:07 +0000

    Bug 1919079 [wpt PR 48198] - fix viewport test, a=testonly
    Automatic update from web-platform-tests
    fix viewport test (#48198)
    
    --
    
    wpt-commits: 561b344978dd11588c7ca6cd9b2c23308c17f9d9
    wpt-pr: 48198
    
    UltraBlame original commit: 2778c54a77b83dbb31f89782e298a46b894e93fe

--- testing/web-platform/tests/webdriver/tests/bidi/__init__.py
+++ testing/web-platform/tests/webdriver/tests/bidi/__init__.py
@@ -149,8 +149,9 @@ async def get_element_dimensions(bidi_session, context, element):
     return remote_mapping_to_dict(result["value"])
 
 
-async def get_viewport_dimensions(bidi_session, context: str, with_scrollbar: bool = True):
-    if with_scrollbar == True:
+async def get_viewport_dimensions(bidi_session, context: str,
+      with_scrollbar: bool = True, quirk_mode: bool = False):
+    if with_scrollbar:
         expression = """
             ({
                 height: window.innerHeight,
@@ -158,11 +159,17 @@ async def get_viewport_dimensions(bidi_session, context: str, with_scrollbar: bo
             });
         """
     else:
-        expression = """
-            ({
-                height: document.documentElement.clientHeight,
-                width: document.documentElement.clientWidth,
-            });
+        
+        
+        
+        
+        element_expression = \
+            "document.body" if quirk_mode else "document.documentElement"
+        expression = f"""
+            ({{
+                height: {element_expression}.clientHeight,
+                width: {element_expression}.clientWidth,
+            }});
         """
     result = await bidi_session.script.evaluate(
         expression=expression,
