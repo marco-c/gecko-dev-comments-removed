726d17b5a9778c1e06fd05f5c0d4d8489401ac21
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-26 00:22:32 +0000

    Bug 1916685 - [wdspec] Update several network bidi tests to assert timings r=webdriver-reviewers,whimboo
    Depends on D221033
    
    Differential Revision: https://phabricator.services.mozilla.com/D221034
    
    UltraBlame original commit: e1aade50c0eb0a2bf43c77266ebf7cc15afbab77

--- testing/web-platform/tests/webdriver/tests/bidi/network/__init__.py
+++ testing/web-platform/tests/webdriver/tests/bidi/network/__init__.py
@@ -22,6 +22,7 @@ from .. import (
 )
 
 
+
 def assert_bytes_value(bytes_value):
     assert bytes_value["type"] in ["string", "base64"]
     any_string(bytes_value["value"])
@@ -35,28 +36,45 @@ def assert_headers(event_headers, expected_headers):
         assert next(h for h in event_headers if header == h) is not None
 
 
-def assert_timing_info(timing_info):
+def assert_timing_info(timing_info, expected_time_range=None):
+    
+    time_origin = timing_info.get("timeOrigin")
+    any_number(time_origin)
+
+    def assert_timing(actual):
+        
+        any_number(actual)
+
+        
+        
+        
+        
+        if expected_time_range is not None and actual != 0:
+            
+            
+            expected_time_range(actual + time_origin)
+
+    
     recursive_compare(
         {
-            "timeOrigin": any_number,
-            "requestTime": any_number,
-            "redirectStart": any_number,
-            "redirectEnd": any_number,
-            "fetchStart": any_number,
-            "dnsStart": any_number,
-            "dnsEnd": any_number,
-            "connectStart": any_number,
-            "connectEnd": any_number,
-            "tlsStart": any_number,
-            "requestStart": any_number,
-            "responseStart": any_number,
-            "responseEnd": any_number,
+            "requestTime": assert_timing,
+            "redirectStart": assert_timing,
+            "redirectEnd": assert_timing,
+            "fetchStart": assert_timing,
+            "dnsStart": assert_timing,
+            "dnsEnd": assert_timing,
+            "connectStart": assert_timing,
+            "connectEnd": assert_timing,
+            "tlsStart": assert_timing,
+            "requestStart": assert_timing,
+            "responseStart": assert_timing,
+            "responseEnd": assert_timing,
         },
         timing_info,
     )
 
 
-def assert_request_data(request_data, expected_request):
+def assert_request_data(request_data, expected_request, expected_time_range):
     recursive_compare(
         {
             "bodySize": any_int_or_null,
@@ -70,8 +88,6 @@ def assert_request_data(request_data, expected_request):
         },
         request_data,
     )
-
-    assert_timing_info(request_data["timings"])
 
     for cookie in request_data["cookies"]:
         assert_bytes_value(cookie["value"])
@@ -92,6 +108,8 @@ def assert_request_data(request_data, expected_request):
         
         del expected_request["headers"]
 
+    assert_timing_info(request_data["timings"], expected_time_range)
+
     recursive_compare(expected_request, request_data)
 
 
@@ -103,6 +121,7 @@ def assert_base_parameters(
     navigation=None,
     redirect_count=None,
     expected_request=None,
+    expected_time_range=None,
 ):
     recursive_compare(
         {
@@ -141,7 +160,7 @@ def assert_base_parameters(
 
     
     if expected_request is not None:
-        assert_request_data(event["request"], expected_request)
+        assert_request_data(event["request"], expected_request, expected_time_range)
 
 
 def assert_before_request_sent_event(
@@ -152,6 +171,7 @@ def assert_before_request_sent_event(
     navigation=None,
     redirect_count=None,
     expected_request=None,
+    expected_time_range=None,
 ):
     
     assert isinstance(event["initiator"], dict)
@@ -166,6 +186,7 @@ def assert_before_request_sent_event(
         navigation=navigation,
         redirect_count=redirect_count,
         expected_request=expected_request,
+        expected_time_range=expected_time_range,
     )
 
 
@@ -178,6 +199,7 @@ def assert_fetch_error_event(
     navigation=None,
     redirect_count=None,
     expected_request=None,
+    expected_time_range=None,
 ):
     
     assert isinstance(event["errorText"], str)
@@ -194,6 +216,7 @@ def assert_fetch_error_event(
         navigation=navigation,
         redirect_count=redirect_count,
         expected_request=expected_request,
+        expected_time_range=expected_time_range,
     )
 
 
@@ -244,6 +267,7 @@ def assert_response_event(
     redirect_count=None,
     expected_request=None,
     expected_response=None,
+    expected_time_range=None,
 ):
     
     any_dict(event["response"])
@@ -259,6 +283,7 @@ def assert_response_event(
         navigation=navigation,
         redirect_count=redirect_count,
         expected_request=expected_request,
+        expected_time_range=expected_time_range,
     )
 
 
