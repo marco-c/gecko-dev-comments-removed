5274420a0307b1a7009c472503a8d9b6d2026085
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-26 00:35:24 +0000

    Bug 1915419 - ClearDataService: Fix IPv6 host brackets before comparing them. r=bvandersloot
    Differential Revision: https://phabricator.services.mozilla.com/D222666
    
    UltraBlame original commit: 8185b8989a78a7e2e137ccd4cc02e721b498965e

--- toolkit/components/antitracking/bouncetrackingprotection/BounceTrackingProtection.cpp
+++ toolkit/components/antitracking/bouncetrackingprotection/BounceTrackingProtection.cpp
@@ -1052,8 +1052,14 @@ nsresult BounceTrackingProtection::PurgeBounceTrackersForStateGlobal(
       cb->OnDataDeleted(0);
     } else {
       
+
+      
+      
+      nsAutoCString hostToPurge(host);
+      nsContentUtils::MaybeFixIPv6Host(hostToPurge);
+
       rv = clearDataService->DeleteDataFromSiteAndOriginAttributesPatternString(
-          host, u""_ns, false, TRACKER_PURGE_FLAGS, cb);
+          hostToPurge, u""_ns, false, TRACKER_PURGE_FLAGS, cb);
       if (NS_WARN_IF(NS_FAILED(rv))) {
         clearPromise->Reject(0, __func__);
       }
