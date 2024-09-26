71baa56bc0b868a2350a2b701994f56897ba9b55
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-26 00:04:24 +0000

    Bug 1603760 - Remove navigation.search legacy event telemetry. r=Standard8,urlbar-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D222362
    
    UltraBlame original commit: 0a3a586fbbec2dc327436327775126a13685966a

--- browser/components/urlbar/tests/browser/browser_urlbar_telemetry_persisted.js
+++ browser/components/urlbar/tests/browser/browser_urlbar_telemetry_persisted.js
@@ -6,7 +6,6 @@
 
 
 
-
 "use strict";
 
 const { SearchSERPTelemetry } = ChromeUtils.importESModule(
@@ -111,25 +110,6 @@ function assertScalarDoesNotExist(scalar) {
   Assert.ok(!(scalar in scalars), scalar + " must not be recorded.");
 }
 
-function assertTelemetryEvents() {
-  TelemetryTestUtils.assertEvents(
-    [
-      ["navigation", "search", "urlbar", "enter", { engine: "Example" }],
-      [
-        "navigation",
-        "search",
-        "urlbar_persisted",
-        "enter",
-        { engine: "Example" },
-      ],
-    ],
-    {
-      category: "navigation",
-      method: "search",
-    }
-  );
-}
-
 
 
 add_task(async function search_after_search() {
@@ -154,9 +134,6 @@ add_task(async function search_after_search() {
     "Example.urlbar-persisted",
     1
   );
-
-  
-  assertTelemetryEvents();
 
   BrowserTestUtils.removeTab(tab);
 });
@@ -184,9 +161,6 @@ add_task(async function switch_to_tab_and_search() {
     1
   );
 
-  
-  assertTelemetryEvents();
-
   BrowserTestUtils.removeTab(tab1);
   BrowserTestUtils.removeTab(tab2);
 });
@@ -212,9 +186,6 @@ add_task(async function handle_revert() {
     "Example.urlbar-persisted",
     1
   );
-
-  
-  assertTelemetryEvents();
 
   BrowserTestUtils.removeTab(tab);
 });
@@ -249,9 +220,6 @@ add_task(async function back_and_forth() {
     "Example.urlbar-persisted",
     1
   );
-
-  
-  assertTelemetryEvents();
 
   BrowserTestUtils.removeTab(tab);
 });
