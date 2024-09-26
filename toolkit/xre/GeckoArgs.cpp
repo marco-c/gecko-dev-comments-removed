116d0d332eac38edfdb4d52117c9ea9ea423af39
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-26 01:05:05 +0000

    Bug 1440207 - Part 3g: Basic posix support for file geckoargs, r=ipc-reviewers,jld
    This effectively mimics the old system using fixed FDs for passing the
    arguments down.
    
    An in-memory array of FDs is used to track which FD corresponds with
    with file argument, which is initialized to the fixed offsets, in order
    to allow platforms which cannot use fixed FDs in their child processes
    to specify explicit files being passed down. This will be used in the
    fork server, for Android, and for iOS.
    
    Differential Revision: https://phabricator.services.mozilla.com/D221377
    
    UltraBlame original commit: ea81a08ce18122decd88c68a99c8a0de6df5f54f

--- toolkit/xre/GeckoArgs.cpp
+++ toolkit/xre/GeckoArgs.cpp
@@ -8,6 +8,49 @@
 
 namespace mozilla::geckoargs {
 
+#ifdef XP_UNIX
+
+
+
+
+
+
+
+static int gInitialFileHandles[]{3,  4,  5,  6,  7,  8,  9, 10,
+                                 11, 12, 13, 14, 15, 16, 17};
+
+void SetPassedFileHandles(Span<int> aFiles) {
+  MOZ_RELEASE_ASSERT(aFiles.Length() <= ArrayLength(gInitialFileHandles));
+  for (size_t i = 0; i < ArrayLength(gInitialFileHandles); ++i) {
+    if (i < aFiles.Length()) {
+      gInitialFileHandles[i] = aFiles[i];
+    } else {
+      gInitialFileHandles[i] = -1;
+    }
+  }
+}
+
+void SetPassedFileHandles(std::vector<UniqueFileHandle>&& aFiles) {
+  MOZ_RELEASE_ASSERT(aFiles.size() <= ArrayLength(gInitialFileHandles));
+  for (size_t i = 0; i < ArrayLength(gInitialFileHandles); ++i) {
+    if (i < aFiles.size()) {
+      gInitialFileHandles[i] = aFiles[i].release();
+    } else {
+      gInitialFileHandles[i] = -1;
+    }
+  }
+}
+
+void AddToFdsToRemap(const ChildProcessArgs& aArgs,
+                     std::vector<std::pair<int, int>>& aFdsToRemap) {
+  MOZ_RELEASE_ASSERT(aArgs.mFiles.size() <= ArrayLength(gInitialFileHandles));
+  for (size_t i = 0; i < aArgs.mFiles.size(); ++i) {
+    aFdsToRemap.push_back(
+        std::pair{aArgs.mFiles[i].get(), gInitialFileHandles[i]});
+  }
+}
+#endif
+
 template <>
 Maybe<UniqueFileHandle> CommandLineArg<UniqueFileHandle>::GetCommon(
     const char* aMatch, int& aArgc, char** aArgv, const CheckArgFlag aFlags) {
@@ -20,7 +63,10 @@ Maybe<UniqueFileHandle> CommandLineArg<UniqueFileHandle>::GetCommon(
     return Some(UniqueFileHandle{reinterpret_cast<HANDLE>(
         static_cast<uintptr_t>(static_cast<int32_t>(*arg)))});
 #else
-    MOZ_CRASH("not implemented yet");
+    
+    
+    MOZ_RELEASE_ASSERT(*arg < ArrayLength(gInitialFileHandles));
+    return Some(UniqueFileHandle{std::exchange(gInitialFileHandles[*arg], -1)});
 #endif
   }
   return Nothing();
@@ -40,7 +86,8 @@ void CommandLineArg<UniqueFileHandle>::PutCommon(const char* aName,
         aName, static_cast<uint32_t>(reinterpret_cast<uintptr_t>(value)),
         aArgs);
 #else
-    MOZ_CRASH("not implemented yet");
+    CommandLineArg<uint32_t>::PutCommon(
+        aName, static_cast<uint32_t>(aArgs.mFiles.size()), aArgs);
 #endif
     aArgs.mFiles.push_back(std::move(aValue));
   }
