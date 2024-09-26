3599a86fc55819421ec9a60e4c345e174a4370a7
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-26 00:40:51 +0000

    Bug 1918684 - tab group label style fixes r=jswinarton,desktop-theme-reviewers,tabbrowser-reviewers,dao
    - match tab typography
    - better match tab group design spec spacing
    - better match "empty label" tab group design spec
    
    Differential Revision: https://phabricator.services.mozilla.com/D222671
    
    UltraBlame original commit: e7303d31247493d983133e0e482394e5f8c4ee60

--- browser/components/tabbrowser/content/tabgroup.js
+++ browser/components/tabbrowser/content/tabgroup.js
@@ -9,7 +9,9 @@
 {
   class MozTabbrowserTabGroup extends MozXULElement {
     static markup = `
-      <label class="tab-group-label" crop="end"/>
+      <box class="tab-group-label-container" align="center" pack="center">
+        <label class="tab-group-label" crop="end"/>
+      </box>
       <html:slot/>
       `;
 
@@ -39,16 +41,41 @@
       this.#labelElement = this.querySelector(".tab-group-label");
       this.#labelElement.addEventListener("click", this);
 
-      this._lastTabRemovedObserver = new window.MutationObserver(() => {
+      this._tabsChangedObserver = new window.MutationObserver(mutationList => {
+        for (let mutation of mutationList) {
+          mutation.addedNodes.forEach(node => {
+            node.tagName === "tab" &&
+              node.dispatchEvent(
+                new CustomEvent("TabGrouped", {
+                  bubbles: true,
+                  detail: this,
+                })
+              );
+          });
+          mutation.removedNodes.forEach(node => {
+            node.tagName === "tab" &&
+              node.dispatchEvent(
+                new CustomEvent("TabUngrouped", {
+                  bubbles: true,
+                  detail: this,
+                })
+              );
+          });
+        }
         if (!this.tabs.length) {
+          this.dispatchEvent(
+            new CustomEvent("TabGroupRemove", { bubbles: true })
+          );
           this.remove();
         }
       });
-      this._lastTabRemovedObserver.observe(this, { childList: true });
+      this._tabsChangedObserver.observe(this, { childList: true });
+
+      this.dispatchEvent(new CustomEvent("TabGroupCreate", { bubbles: true }));
     }
 
     disconnectedCallback() {
-      this._lastTabRemovedObserver.disconnect();
+      this._tabsChangedObserver.disconnect();
     }
 
     get color() {
