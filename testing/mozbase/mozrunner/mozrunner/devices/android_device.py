8fe73a776d6ec48e59c823c824a62daba4f4a7b0
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-10-01 00:57:41 +0000

    Bug 1921009 - Fix scope of local variable metadata r=ohall,nalexander
    Differential Revision: https://phabricator.services.mozilla.com/D223637
    
    UltraBlame original commit: 20ffa140f69b4ebf51054d24556979f6f618569c

--- testing/mozbase/mozrunner/mozrunner/devices/android_device.py
+++ testing/mozbase/mozrunner/mozrunner/devices/android_device.py
@@ -283,6 +283,8 @@ def metadata_for_app(app, aab=False):
     return Android metadata including launch `activity_name`, `package_name`, 'subcommand'.
     to reduce special-casing throughout the code base"""
     metadata = namedtuple("metadata", ["activity_name", "package_name", "subcommand"])
+    if not app:
+        app = "org.mozilla.geckoview.test_runner"
     package_name = app
     activity_name = None
     subcommand = None
@@ -388,6 +390,8 @@ def verify_android_device(
                 os.environ["DEVICE_SERIAL"] = d["device_serial"]
                 break
 
+    metadata = metadata_for_app(app, aab)
+
     if device_verified and install != InstallIntent.NO:
         
         
@@ -401,11 +405,8 @@ def verify_android_device(
         
         
         
-        if not app:
-            app = "org.mozilla.geckoview.test_runner"
         device = _get_device(build_obj.substs, device_serial)
         response = ""
-        metadata = metadata_for_app(app, aab)
         installed = device.is_app_installed(metadata.package_name)
 
         if not installed:
