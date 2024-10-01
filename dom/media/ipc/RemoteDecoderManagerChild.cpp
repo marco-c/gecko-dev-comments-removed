ccaa01a05bd48d5c12162f74cc8f40ed76c2be32
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-10-01 01:05:22 +0000

    Bug 1919778 - Flag EnumeratedArray variable from dom/media/ipc/RemoteDecoderManagerChild.cpp as MOZ_CONSTINIT r=media-playback-reviewers,padenot
    This makes sure we don't have associated global constructor.
    
    It requires flagging some default constructor as constexpr, including
    one from a union which we circumvent by adding a default field for the
    uninitialized state of the union.
    
    Differential Revision: https://phabricator.services.mozilla.com/D222799
    
    UltraBlame original commit: 5f99810531456a108591d52df3071bb39772b1cd

--- dom/media/ipc/RemoteDecoderManagerChild.cpp
+++ dom/media/ipc/RemoteDecoderManagerChild.cpp
@@ -70,6 +70,9 @@ static StaticAutoPtr<nsTArray<RefPtr<Runnable>>> sRecreateTasks;
 
 
 StaticMutex sProcessSupportedMutex;
+#ifndef MOZ_DEBUG
+MOZ_CONSTINIT
+#endif
 static EnumeratedArray<RemoteDecodeIn, Maybe<media::MediaCodecsSupported>,
                        size_t(RemoteDecodeIn::SENTINEL)>
     sProcessSupported MOZ_GUARDED_BY(sProcessSupportedMutex);
