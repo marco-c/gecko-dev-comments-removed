8634de33620ac2f33cd77e0618bcd423c5199b3a
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-10-01 00:59:26 +0000

    Bug 1920941 - Store secureConnectionStartin mBootstrappedTimings for NullTransaction, r=necko-reviewers,valentin
    Differential Revision: https://phabricator.services.mozilla.com/D223591
    
    UltraBlame original commit: 3b9114742881c208b62b0841f6b80fc06be24666

--- netwerk/protocol/http/nsHttpConnection.cpp
+++ netwerk/protocol/http/nsHttpConnection.cpp
@@ -547,6 +547,9 @@ nsresult nsHttpConnection::Activate(nsAHttpTransaction* trans, uint32_t caps,
   }
 
   
+  mTransaction = trans;
+
+  
   nsCOMPtr<nsIInterfaceRequestor> callbacks;
   trans->GetSecurityCallbacks(getter_AddRefs(callbacks));
   SetSecurityCallbacks(callbacks);
@@ -557,9 +560,6 @@ nsresult nsHttpConnection::Activate(nsAHttpTransaction* trans, uint32_t caps,
   } else {
     ChangeConnectionState(ConnectionState::TLS_HANDSHAKING);
   }
-
-  
-  mTransaction = trans;
 
   nsCOMPtr<nsITLSSocketControl> tlsSocketControl;
   if (NS_SUCCEEDED(mSocketTransport->GetTlsSocketControl(
@@ -2267,6 +2267,11 @@ void nsHttpConnection::CheckForTraffic(bool check) {
 }
 
 void nsHttpConnection::SetEvent(nsresult aStatus) {
+  LOG(("nsHttpConnection::SetEvent [this=%p status=%" PRIx32 "]\n", this,
+       static_cast<uint32_t>(aStatus)));
+  if (!mBootstrappedTimingsSet) {
+    mBootstrappedTimingsSet = true;
+  }
   switch (aStatus) {
     case NS_NET_STATUS_RESOLVING_HOST:
       mBootstrappedTimings.domainLookupStart = TimeStamp::Now();
