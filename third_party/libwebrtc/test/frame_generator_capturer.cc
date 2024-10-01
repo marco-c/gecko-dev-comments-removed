da7ba733e37b84857d47b79f840253eac89ebc84
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-10-01 01:12:17 +0000

    Bug 1918268 - Vendor libwebrtc from f9ddf7fed6
    Upstream commit: https://webrtc.googlesource.com/src/+/f9ddf7fed6617c21f8be6f32737953751fb5701b
        Replace test frame capturer wanted_fps_ by target_capture_fps_.
    
        wanted_fps_ seems redundant with target_capture_fps_.
        The problem with wanted_fps_ is that it lowers the capture fps but does not decimate frames so that a 30 fps stream played at 5 fps is played slowly instead of played at the normal speed with dropped frames.
    
        Change-Id: I1440953f9909ad1d4a102a0671fe933d95498a1f
        Bug: b/355120692
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/357780
        Reviewed-by: Mirko Bonadei <mbonadeiwebrtc.org>
        Commit-Queue: Jeremy Leconte <jlecontegoogle.com>
        Cr-Commit-Position: refs/heads/main{#42670}
    
    UltraBlame original commit: e302aef594f2ce0cab7e3bac1a7e01c2ec730e0e

--- third_party/libwebrtc/test/frame_generator_capturer.cc
+++ third_party/libwebrtc/test/frame_generator_capturer.cc
@@ -189,28 +189,20 @@ void FrameGeneratorCapturer::AddOrUpdateSink(
     rtc::VideoSinkInterface<VideoFrame>* sink,
     const rtc::VideoSinkWants& wants) {
   TestVideoCapturer::AddOrUpdateSink(sink, wants);
-  MutexLock lock(&lock_);
-  if (sink_wants_observer_) {
-    
-    sink_wants_observer_->OnSinkWantsChanged(sink, wants);
+  {
+    MutexLock lock(&lock_);
+    if (sink_wants_observer_) {
+      
+      sink_wants_observer_->OnSinkWantsChanged(sink, wants);
+    }
   }
-  UpdateFps(GetSinkWants().max_framerate_fps);
+  ChangeFramerate(GetSinkWants().max_framerate_fps);
 }
 
 void FrameGeneratorCapturer::RemoveSink(
     rtc::VideoSinkInterface<VideoFrame>* sink) {
   TestVideoCapturer::RemoveSink(sink);
-
-  MutexLock lock(&lock_);
-  UpdateFps(GetSinkWants().max_framerate_fps);
-}
-
-void FrameGeneratorCapturer::UpdateFps(int max_fps) {
-  if (max_fps < target_capture_fps_) {
-    wanted_fps_.emplace(max_fps);
-  } else {
-    wanted_fps_.reset();
-  }
+  ChangeFramerate(GetSinkWants().max_framerate_fps);
 }
 
 void FrameGeneratorCapturer::ForceFrame() {
@@ -220,8 +212,6 @@ void FrameGeneratorCapturer::ForceFrame() {
 
 int FrameGeneratorCapturer::GetCurrentConfiguredFramerate() {
   MutexLock lock(&lock_);
-  if (wanted_fps_ && *wanted_fps_ < target_capture_fps_)
-    return *wanted_fps_;
   return target_capture_fps_;
 }
 
