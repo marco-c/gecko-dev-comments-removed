73b191cff0da2c710dcb02542f788914e36749c5
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-10-01 00:56:38 +0000

    Bug 1919205 - Clean-up initialization of nsJARChannel. r=necko-reviewers,valentin
    I wrote this while debugging, so no reason not to do it I guess.
    
    No behavior change.
    
    Differential Revision: https://phabricator.services.mozilla.com/D223212
    
    UltraBlame original commit: a75df76f05eec752fdf97b27d2cf7a7528b3ec23

--- modules/libjar/nsJARChannel.cpp
+++ modules/libjar/nsJARChannel.cpp
@@ -159,15 +159,7 @@ nsJARInputThunk::IsNonBlocking(bool* nonBlocking) {
 
 
 
-nsJARChannel::nsJARChannel()
-    : mOpened(false),
-      mCanceled(false),
-      mContentLength(-1),
-      mLoadFlags(LOAD_NORMAL),
-      mStatus(NS_OK),
-      mIsPending(false),
-      mEnableOMT(true),
-      mPendingEvent() {
+nsJARChannel::nsJARChannel() {
   LOG(("nsJARChannel::nsJARChannel [this=%p]\n", this));
   
   mJarHandler = gJarHandler;
