2bb473ca9bc478db53fe0073d7447e7902a15f81
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-10-01 01:20:04 +0000

    Bug 1918268 - Vendor libwebrtc from ea233be459
    Upstream commit: https://webrtc.googlesource.com/src/+/ea233be459bb4c415355fdcad14fa2acf3888364
        Remove passing FieldTrialsView into AudioDecoderFactory as unused
    
        Plan is to pass field trials using Environment when creating individual AudioDecoder rather than providing single set of field trials for the factory.
        Current implementation is not used, and doesn't pass field trials when actually creating an AudioDecoder
    
        Bug: webrtc:356878416
        Change-Id: I0f79f09f7a6aa63e20fbdd783e90e8d026158330
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/359221
        Reviewed-by: Jakob Ivarsson‎ <jakobiwebrtc.org>
        Commit-Queue: Danil Chapovalov <danilchapwebrtc.org>
        Cr-Commit-Position: refs/heads/main{#42762}
    
    UltraBlame original commit: d84219d7e25ef04e90af4083d2538211ea18c26b

--- third_party/libwebrtc/api/audio_codecs/audio_decoder_factory_template.h
+++ third_party/libwebrtc/api/audio_codecs/audio_decoder_factory_template.h
@@ -20,7 +20,6 @@
 #include "api/audio_codecs/audio_decoder.h"
 #include "api/audio_codecs/audio_decoder_factory.h"
 #include "api/audio_codecs/audio_format.h"
-#include "api/field_trials_view.h"
 #include "api/make_ref_counted.h"
 #include "api/scoped_refptr.h"
 
@@ -38,8 +37,7 @@ struct Helper<> {
   static bool IsSupportedDecoder(const SdpAudioFormat& format) { return false; }
   static std::unique_ptr<AudioDecoder> MakeAudioDecoder(
       const SdpAudioFormat& format,
-      absl::optional<AudioCodecPairId> codec_pair_id,
-      const FieldTrialsView* field_trials) {
+      absl::optional<AudioCodecPairId> codec_pair_id) {
     return nullptr;
   }
 };
@@ -62,22 +60,16 @@ struct Helper<T, Ts...> {
   }
   static std::unique_ptr<AudioDecoder> MakeAudioDecoder(
       const SdpAudioFormat& format,
-      absl::optional<AudioCodecPairId> codec_pair_id,
-      const FieldTrialsView* field_trials) {
+      absl::optional<AudioCodecPairId> codec_pair_id) {
     auto opt_config = T::SdpToConfig(format);
     return opt_config ? T::MakeAudioDecoder(*opt_config, codec_pair_id)
-                      : Helper<Ts...>::MakeAudioDecoder(format, codec_pair_id,
-                                                        field_trials);
+                      : Helper<Ts...>::MakeAudioDecoder(format, codec_pair_id);
   }
 };
 
 template <typename... Ts>
 class AudioDecoderFactoryT : public AudioDecoderFactory {
  public:
-  explicit AudioDecoderFactoryT(const FieldTrialsView* field_trials) {
-    field_trials_ = field_trials;
-  }
-
   std::vector<AudioCodecSpec> GetSupportedDecoders() override {
     std::vector<AudioCodecSpec> specs;
     Helper<Ts...>::AppendSupportedDecoders(&specs);
@@ -91,11 +83,8 @@ class AudioDecoderFactoryT : public AudioDecoderFactory {
   std::unique_ptr<AudioDecoder> MakeAudioDecoder(
       const SdpAudioFormat& format,
       absl::optional<AudioCodecPairId> codec_pair_id) override {
-    return Helper<Ts...>::MakeAudioDecoder(format, codec_pair_id,
-                                           field_trials_);
+    return Helper<Ts...>::MakeAudioDecoder(format, codec_pair_id);
   }
-
-  const FieldTrialsView* field_trials_;
 };
 
 }  
@@ -131,8 +120,7 @@ class AudioDecoderFactoryT : public AudioDecoderFactory {
 
 
 template <typename... Ts>
-rtc::scoped_refptr<AudioDecoderFactory> CreateAudioDecoderFactory(
-    const FieldTrialsView* field_trials = nullptr) {
+scoped_refptr<AudioDecoderFactory> CreateAudioDecoderFactory() {
   
   
   
@@ -140,9 +128,8 @@ rtc::scoped_refptr<AudioDecoderFactory> CreateAudioDecoderFactory(
   static_assert(sizeof...(Ts) >= 1,
                 "Caller must give at least one template parameter");
 
-  return rtc::make_ref_counted<
-      audio_decoder_factory_template_impl::AudioDecoderFactoryT<Ts...>>(
-      field_trials);
+  return make_ref_counted<
+      audio_decoder_factory_template_impl::AudioDecoderFactoryT<Ts...>>();
 }
 
 }  
