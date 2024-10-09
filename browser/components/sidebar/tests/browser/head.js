78ed5a18585278f5247b45dc37eeae81df82ba56
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-10-09 16:24:07 +0000

    Bug 1920575 - Fix right click on new tab to show container tab menu r=sidebar-reviewers,Gijs,sfoster
    Differential Revision: https://phabricator.services.mozilla.com/D223771
    
    UltraBlame original commit: 350cbb95263323a35accdb31cc8b134d953ee93a

--- browser/components/sidebar/tests/browser/head.js
+++ browser/components/sidebar/tests/browser/head.js
@@ -120,10 +120,8 @@ function openAndWaitForContextMenu(popup, button, onShown, onHidden) {
 
     button.scrollIntoView();
     const eventDetails = { type: "contextmenu", button: 2 };
-    EventUtils.synthesizeMouse(
+    EventUtils.synthesizeMouseAtCenter(
       button,
-      5,
-      2,
       eventDetails,
       
       button.ownerDocument.defaultView
