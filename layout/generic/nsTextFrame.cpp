66e67ea17648b646b1d928554e690d9f945deb95
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-10-09 16:55:59 +0000

    Bug 1921348 - Remove global constructor from layout/generic/nsTextFrame.cpp r=dholbert
    Differential Revision: https://phabricator.services.mozilla.com/D223872
    
    UltraBlame original commit: d0876494acfa9f267a9bca1431561af7c3ac6fd0

--- layout/generic/nsTextFrame.cpp
+++ layout/generic/nsTextFrame.cpp
@@ -5472,7 +5472,7 @@ gfxFloat nsTextFrame::ComputeDescentLimitForSelectionUnderline(
 }
 
 
-static const SelectionTypeMask kSelectionTypesWithDecorations =
+static constexpr SelectionTypeMask kSelectionTypesWithDecorations =
     ToSelectionTypeMask(SelectionType::eSpellCheck) |
     ToSelectionTypeMask(SelectionType::eURLStrikeout) |
     ToSelectionTypeMask(SelectionType::eIMERawClause) |
