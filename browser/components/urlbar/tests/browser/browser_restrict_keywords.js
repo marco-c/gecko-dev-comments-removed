eb6df013d7533e82cd9da8784672862e50251479
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-10-09 16:54:26 +0000

    Backed out changeset 737f799b6c6c (bug 1919180) for causing mochitest fails on browser_secondaryActions.js. CLOSED TREE
    UltraBlame original commit: c3e0cc4aa329385655762fe6b8242c94036c6461

--- browser/components/urlbar/tests/browser/browser_restrict_keywords.js
+++ browser/components/urlbar/tests/browser/browser_restrict_keywords.js
@@ -75,7 +75,6 @@ async function assertRestrictKeywordResult(window, restrictToken) {
   await UrlbarTestUtils.assertSearchMode(window, {
     ...searchMode,
     entry: "keywordoffer",
-    restrictType: "keyword",
   });
 
   const scalars = TelemetryTestUtils.getProcessScalars("parent", true, true);
@@ -96,7 +95,6 @@ add_task(async function test_search_restrict_keyword_results() {
     UrlbarTokenizer.RESTRICT.HISTORY,
     UrlbarTokenizer.RESTRICT.BOOKMARK,
     UrlbarTokenizer.RESTRICT.OPENPAGE,
-    UrlbarTokenizer.RESTRICT.ACTION,
   ];
 
   for (const restrictToken of restrictTokens) {
