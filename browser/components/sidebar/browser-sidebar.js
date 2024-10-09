8414cc36c6e6eebfbb85e2ffbf1cafd58ee7f918
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-10-09 16:24:08 +0000

    Bug 1921539 - Sidebar button has no label when moved to the overflow menu r=sidebar-reviewers,fluent-reviewers,Gijs,sclements
    Fluent messages were missing `label` attribute. Use the existing one from customizableWidgets.properties.
    
    Differential Revision: https://phabricator.services.mozilla.com/D224167
    
    UltraBlame original commit: 7f333f2523fa9a9cfa3dd33d35fdad33e333c30b

--- browser/components/sidebar/browser-sidebar.js
+++ browser/components/sidebar/browser-sidebar.js
@@ -996,25 +996,25 @@ var SidebarController = {
   
 
 
-  updateToolbarButton() {
-    if (!this.sidebarRevampEnabled || !this.toolbarButton) {
+  updateToolbarButton(toolbarButton = this.toolbarButton) {
+    if (!this.sidebarRevampEnabled || !toolbarButton) {
       
       return;
     }
     switch (this.sidebarRevampVisibility) {
       case "always-show":
         
-        this.toolbarButton.checked = this.sidebarMain.expanded;
-        this.toolbarButton.dataset.l10nId = this.toolbarButton.checked
-          ? "sidebar-toolbar-collapse-sidebar"
-          : "sidebar-toolbar-expand-sidebar";
+        toolbarButton.checked = this.sidebarMain.expanded;
+        toolbarButton.dataset.l10nId = toolbarButton.checked
+          ? "sidebar-widget-collapse-sidebar"
+          : "sidebar-widget-expand-sidebar";
         break;
       case "hide-sidebar":
         
-        this.toolbarButton.checked = !this.sidebarContainer.hidden;
-        this.toolbarButton.dataset.l10nId = this.toolbarButton.checked
-          ? "sidebar-toolbar-hide-sidebar"
-          : "sidebar-toolbar-show-sidebar";
+        toolbarButton.checked = !this.sidebarContainer.hidden;
+        toolbarButton.dataset.l10nId = toolbarButton.checked
+          ? "sidebar-widget-hide-sidebar"
+          : "sidebar-widget-show-sidebar";
         break;
     }
   },
