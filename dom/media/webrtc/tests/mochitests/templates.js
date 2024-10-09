30e3519aa721f65f39cab62324645b2a3278194b
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-10-09 17:17:24 +0000

    Bug 1922959: Make waitForAnIceCandidate ignore candidates from previously completed generations. r=ng
    Depends on D224646
    
    Differential Revision: https://phabricator.services.mozilla.com/D224647
    
    UltraBlame original commit: fcd3e93966d84b932b7e4305317a3c91cb21780f

--- dom/media/webrtc/tests/mochitests/templates.js
+++ dom/media/webrtc/tests/mochitests/templates.js
@@ -131,7 +131,7 @@ function dumpSdp(test) {
 
 function waitForAnIceCandidate(pc) {
   return new Promise(resolve => {
-    if (!pc.localRequiresTrickleIce || pc._local_ice_candidates.length) {
+    if (!pc.localRequiresTrickleIce || pc._new_local_ice_candidates.length) {
       resolve();
     } else {
       
