d3a63e01fb97daab734d862c77cef46c064de189
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-10-09 16:30:48 +0000

    Bug 1919115 - Check per-edge resizability in CheckResizerEdge(). r=stransky
    Differential Revision: https://phabricator.services.mozilla.com/D223977
    
    UltraBlame original commit: 71eff8a27139982e78e3de99d8a3141a04b435fd

--- widget/gtk/nsWindow.h
+++ widget/gtk/nsWindow.h
@@ -21,6 +21,7 @@
 #include "mozilla/TouchEvents.h"
 #include "mozilla/UniquePtr.h"
 #include "mozilla/RWLock.h"
+#include "mozilla/gfx/BaseMargin.h"
 #include "mozilla/widget/WindowSurface.h"
 #include "mozilla/widget/WindowSurfaceProvider.h"
 #include "nsBaseWidget.h"
@@ -1003,6 +1004,7 @@ class nsWindow final : public nsBaseWidget {
   LayoutDeviceIntRect mLastLoggedBoundSize;
   int mLastLoggedScale = -1;
 #endif
+  mozilla::Sides mResizableEdges;
   
   
   mozilla::Maybe<int> mKioskMonitor;
