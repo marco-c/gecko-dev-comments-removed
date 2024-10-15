2174d28b2847c534c2c739beeed025313c059845
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-10-15 10:09:19 +0000

    Bug 1829006 - Remove unnecessary restriction about opaque scrollable windows. r=dholbert
    This comment doesn't make sense any more:
    
     * Scrollers don't use native anything.
     * Extensions can't openDialog themselves anymore.
    
    This would allow user CSS to e.g. make PiP windows transparent. An
    alternative would be to use scrolling="false" on the PiP window, but
    since the restriction doesn't really seem necessary seems worth
    removing.
    
    Differential Revision: https://phabricator.services.mozilla.com/D175952
    
    UltraBlame original commit: 4b237406332a04a1e7de93603bbed14826dce0a2

--- layout/base/PresShell.cpp
+++ layout/base/PresShell.cpp
@@ -11827,28 +11827,14 @@ void PresShell::SyncWindowProperties(bool aSync) {
     return;
   }
 
+  
+  windowWidget->SetColorScheme(
+      Some(LookAndFeel::ColorSchemeForFrame(rootFrame)));
+
   AutoWeakFrame weak(rootFrame);
-  if (!GetRootScrollContainerFrame()) {
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    auto* canvas = GetCanvasFrame();
-    widget::TransparencyMode mode = nsLayoutUtils::GetFrameTransparency(
-        canvas ? canvas : rootFrame, rootFrame);
-    windowWidget->SetTransparencyMode(mode);
-
-    
-    windowWidget->SetColorScheme(
-        Some(LookAndFeel::ColorSchemeForFrame(rootFrame)));
-  }
-
+  auto* canvas = GetCanvasFrame();
+  windowWidget->SetTransparencyMode(nsLayoutUtils::GetFrameTransparency(
+      canvas ? canvas : rootFrame, rootFrame));
   if (!weak.IsAlive()) {
     return;
   }
