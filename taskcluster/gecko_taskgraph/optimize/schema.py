180bd6bc0b2de1a2f77ebb1d458e76b8aa07269b
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-10-15 10:13:18 +0000

    Bug 1919287 - Ensure addon-tps-xpi task re-runs if it ever goes missing on the index, r=taskgraph-reviewers,jmaher
    Differential Revision: https://phabricator.services.mozilla.com/D224350
    
    UltraBlame original commit: bb6fdebf90b4a8734df9daa897b3ca1ad7d7b012

--- taskcluster/gecko_taskgraph/optimize/schema.py
+++ taskcluster/gecko_taskgraph/optimize/schema.py
@@ -29,6 +29,7 @@ default_optimizations = (
     {"skip-unless-android-perftest-backstop": None},
     
     {"skip-unless-changed": [str]},
+    {"skip-unless-missing-or-changed": [voluptuous.Any(str, [str])]},
     
     {"skip-unless-schedules": list(schedules.ALL_COMPONENTS)},
     
