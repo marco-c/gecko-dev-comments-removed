806374939fbf5baa9ac1da4013e551137d069d38
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-10-15 10:14:09 +0000

    Bug 1866402 - Use the cached value if it's available in PersistOp::DoDirectoryWork; r=dom-storage-reviewers,jari
    If temporary storage is already initialized, we can use the cached value and
    avoid reading it from the metadata file (which was used for loading the cached
    value).
    
    Differential Revision: https://phabricator.services.mozilla.com/D198664
    
    UltraBlame original commit: 9feaa90862225910a995c2d8ee568e5deaf1db6d

--- dom/quota/OriginOperations.cpp
+++ dom/quota/OriginOperations.cpp
@@ -3116,12 +3116,27 @@ nsresult PersistOp::DoDirectoryWork(QuotaManager& aQuotaManager) {
         *directory, timestamp,
          true, originMetadata)));
   } else {
-    
-    
-    QM_TRY_INSPECT(const auto& metadata,
-                   aQuotaManager.LoadFullOriginMetadataWithRestore(directory));
+    QM_TRY_INSPECT(
+        const bool& persisted,
+        ([&aQuotaManager, &originMetadata,
+          &directory]() -> mozilla::Result<bool, nsresult> {
+          Nullable<bool> persisted =
+              aQuotaManager.OriginPersisted(originMetadata);
 
-    if (!metadata.mPersisted) {
+          if (!persisted.IsNull()) {
+            return persisted.Value();
+          }
+
+          
+          
+          QM_TRY_INSPECT(
+              const auto& metadata,
+              aQuotaManager.LoadFullOriginMetadataWithRestore(directory));
+
+          return metadata.mPersisted;
+        }()));
+
+    if (!persisted) {
       QM_TRY_INSPECT(const auto& file,
                      CloneFileAndAppend(
                          *directory, nsLiteralString(METADATA_V2_FILE_NAME)));
