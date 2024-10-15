cdd1ea0436ad723d9e14b3026ef6eb545c48ddba
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-10-15 10:09:21 +0000

    Bug 1905851 - Enable `dom.block_multiple_popups` by default for tests; r=smaug
    Some tests only set `dom.disable_open_during_load` to `true` and expect the
    popup blocker to be enabled, e.g. https://searchfox.org/mozilla-central/rev/b368ed8b48c0ea8ed2f1948e4776a6fbb5976dff/browser/base/content/test/popups/browser_popup_blocker.js#21,
    but it might not have complete popup blocker behavior as we also check `dom.block_multiple_popups`
    in https://searchfox.org/mozilla-central/rev/b368ed8b48c0ea8ed2f1948e4776a6fbb5976dff/docshell/base/BrowsingContext.cpp#2246.
    
    I think it would be nice if we could simplify this by having `dom.block_multiple_popups`
    enabled on test profile by default as well, so we could use only `dom.disable_open_during_load`
    to control whether popup blocker is enabled and also test the popup blocker properly.
    
    Differential Revision: https://phabricator.services.mozilla.com/D216007
    
    UltraBlame original commit: e299f0dda29b3a9ed0a0b2e56c086f0362c5147b

--- browser/actors/test/browser/browser_untrusted_click_event.js
+++ browser/actors/test/browser/browser_untrusted_click_event.js
@@ -42,8 +42,8 @@ add_task(async function test_unused_click_doesnt_consume_activation() {
   
   await SpecialPowers.pushPrefEnv({
     set: [
+      
       ["dom.disable_open_during_load", true],
-      ["dom.block_multiple_popups", true],
     ],
   });
   await BrowserTestUtils.withNewTab(TEST_PATH + "click.html", async browser => {
@@ -75,8 +75,8 @@ add_task(async function test_click_without_href_doesnt_consume_activation() {
   
   await SpecialPowers.pushPrefEnv({
     set: [
+      
       ["dom.disable_open_during_load", true],
-      ["dom.block_multiple_popups", true],
     ],
   });
   await BrowserTestUtils.withNewTab(TEST_PATH + "click.html", async browser => {
