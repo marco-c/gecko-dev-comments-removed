26b345bae6b2eed91b9ffcbd54c81a628deb78c2
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-10-15 09:49:06 +0000

    Bug 1918681 - part 2: Ensure the list-all-tabs button is visible the first time an extension hides a tab r=extension-reviewers,mconley,dao,willdurand
    Depends on D224623
    
    Differential Revision: https://phabricator.services.mozilla.com/D224624
    
    UltraBlame original commit: 565c384eafd0c6a71f27d729554d5e8e291aa87e

--- browser/components/extensions/parent/ext-tabs.js
+++ browser/components/extensions/parent/ext-tabs.js
@@ -8,6 +8,7 @@
 
 ChromeUtils.defineESModuleGetters(this, {
   BrowserUIUtils: "resource:///modules/BrowserUIUtils.sys.mjs",
+  CustomizableUI: "resource:///modules/CustomizableUI.sys.mjs",
   DownloadPaths: "resource://gre/modules/DownloadPaths.sys.mjs",
   ExtensionControlledPopup:
     "resource:///modules/ExtensionControlledPopup.sys.mjs",
@@ -1605,6 +1606,17 @@ this.tabs = class extends ExtensionAPIPersistent {
           }
           if (hidden.length) {
             let win = Services.wm.getMostRecentWindow("navigator:browser");
+
+            
+            
+            if (!CustomizableUI.widgetIsLikelyVisible("alltabs-button", win)) {
+              CustomizableUI.addWidgetToArea(
+                "alltabs-button",
+                CustomizableUI.verticalTabsEnabled
+                  ? CustomizableUI.AREA_NAVBAR
+                  : CustomizableUI.AREA_TABSTRIP
+              );
+            }
             tabHidePopup.open(win, extension.id);
           }
           return hidden;
