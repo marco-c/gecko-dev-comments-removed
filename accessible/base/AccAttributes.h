a385be0b5a68a1c8ed570f6babec166d758a8a96
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-10-15 09:50:15 +0000

    Backed out changeset e05604fb874e (bug 1922696) for causing bustages at AccAttributes.cpp. CLOSED TREE
    UltraBlame original commit: dc587585ef5b61b0af1cbdf2201924b26e5b315b

--- accessible/base/AccAttributes.h
+++ accessible/base/AccAttributes.h
@@ -316,10 +316,6 @@ class AccAttributes {
                                      const AttrValueType& aValue,
                                      nsAString& aValueString);
 
-  
-  friend std::ostream& operator<<(std::ostream& aStream,
-                                  const AccAttributes& aAttributes);
-
   AtomVariantMap mData;
 
   friend struct IPC::ParamTraits<AccAttributes*>;
