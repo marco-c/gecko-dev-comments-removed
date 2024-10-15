95e2c2dfe03bb249f2b7a7a68dfb3291bf35aa67
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-10-15 10:16:20 +0000

    Bug 1922250 - Align skeleton ui with nsWindow more closely. r=win-reviewers,rkraesig
    This maps better to the code we have in nsWindow, and fixes a couple
    bugs which caused maximized skeleton UI-consuming windows to be
    mispositioned with the following patches.
    
    Differential Revision: https://phabricator.services.mozilla.com/D225100
    
    UltraBlame original commit: 97dce070567ff73d308c9eb4bcc3c6d918802666

--- mozglue/misc/PreXULSkeletonUI.cpp
+++ mozglue/misc/PreXULSkeletonUI.cpp
@@ -97,6 +97,13 @@ NormalizedRGB Lerp(const NormalizedRGB& a, const NormalizedRGB& b, double x) {
 
 double SmoothStep3(double x) { return x * x * (3.0 - 2.0 * x); }
 
+struct Margin {
+  int top = 0;
+  int right = 0;
+  int bottom = 0;
+  int left = 0;
+};
+
 static const wchar_t kPreXULSkeletonUIKeyPath[] =
     L"SOFTWARE"
     L"\\" MOZ_APP_VENDOR L"\\" MOZ_APP_BASENAME L"\\PreXULSkeletonUISettings";
@@ -115,9 +122,20 @@ static Vector<ColorRect>* sAnimatedRects = nullptr;
 static int sTotalChromeHeight = 0;
 static volatile LONG sAnimationControlFlag = 0;
 static bool sMaximized = false;
-static int sNonClientVerticalMargins = 0;
-static int sNonClientHorizontalMargins = 0;
 static uint32_t sDpi = 0;
+
+static Margin sNonClientOffset;
+static int sCaptionHeight = 0;
+static int sHorizontalResizeMargin = 0;
+static int sVerticalResizeMargin = 0;
+
+
+static Margin NonClientSizeMargin() {
+  return Margin{sCaptionHeight - sNonClientOffset.top,
+                sHorizontalResizeMargin - sNonClientOffset.right,
+                sVerticalResizeMargin - sNonClientOffset.bottom,
+                sHorizontalResizeMargin - sNonClientOffset.left};
+}
 
 
 static uint32_t sAnimationColor;
@@ -701,9 +719,9 @@ Result<Ok, PreXULSkeletonUIError> DrawSkeletonUI(
   bool rtlEnabled = flags.contains(SkeletonUIFlag::RtlEnabled);
 
   int chromeHorMargin = CSSToDevPixels(2, sCSSToDevPixelScaling);
-  int verticalOffset = sMaximized ? sNonClientVerticalMargins : 0;
+  int verticalOffset = sMaximized ? sVerticalResizeMargin : 0;
   int horizontalOffset =
-      sNonClientHorizontalMargins - (sMaximized ? 0 : chromeHorMargin);
+      sHorizontalResizeMargin - (sMaximized ? 0 : chromeHorMargin);
 
   
   int tabBarHeight = CSSToDevPixels(44, sCSSToDevPixelScaling);
@@ -1274,19 +1292,12 @@ LRESULT WINAPI PreXULSkeletonUIProc(HWND hWnd, UINT msg, WPARAM wParam,
         wParam ? &(reinterpret_cast<NCCALCSIZE_PARAMS*>(lParam))->rgrc[0]
                : (reinterpret_cast<RECT*>(lParam));
 
-    
-    
-    
-    int horizontalOffset =
-        sNonClientHorizontalMargins -
-        (sMaximized ? 0 : CSSToDevPixels(2, sCSSToDevPixelScaling));
-    int verticalOffset =
-        sNonClientHorizontalMargins -
-        (sMaximized ? 0 : CSSToDevPixels(2, sCSSToDevPixelScaling));
-    clientRect->top = clientRect->top;
-    clientRect->left += horizontalOffset;
-    clientRect->right -= horizontalOffset;
-    clientRect->bottom -= verticalOffset;
+    Margin margin = NonClientSizeMargin();
+    clientRect->top += margin.top;
+    clientRect->left += margin.left;
+    clientRect->right -= margin.right;
+    clientRect->bottom -= margin.bottom;
+
     return 0;
   }
 
@@ -1967,11 +1978,30 @@ static Result<Ok, PreXULSkeletonUIError> CreateAndStorePreXULSkeletonUIImpl(
   }
 
   sDpi = sGetDpiForWindow(sPreXULSkeletonUIWindow);
-  sNonClientHorizontalMargins =
-      sGetSystemMetricsForDpi(SM_CXFRAME, sDpi) +
-      sGetSystemMetricsForDpi(SM_CXPADDEDBORDER, sDpi);
-  sNonClientVerticalMargins = sGetSystemMetricsForDpi(SM_CYFRAME, sDpi) +
-                              sGetSystemMetricsForDpi(SM_CXPADDEDBORDER, sDpi);
+  sHorizontalResizeMargin = sGetSystemMetricsForDpi(SM_CXFRAME, sDpi) +
+                            sGetSystemMetricsForDpi(SM_CXPADDEDBORDER, sDpi);
+  sVerticalResizeMargin = sGetSystemMetricsForDpi(SM_CYFRAME, sDpi) +
+                          sGetSystemMetricsForDpi(SM_CXPADDEDBORDER, sDpi);
+  sCaptionHeight =
+      sVerticalResizeMargin + sGetSystemMetricsForDpi(SM_CYCAPTION, sDpi);
+
+  
+  
+  
+  const Margin nonClientMargin{0, 2, 2, 2};
+
+  if (sMaximized) {
+    sNonClientOffset.top = sCaptionHeight - sVerticalResizeMargin;
+  } else {
+    
+    sNonClientOffset.top = sCaptionHeight;
+    sNonClientOffset.bottom =
+        std::min(sVerticalResizeMargin, nonClientMargin.bottom);
+    sNonClientOffset.left =
+        std::min(sHorizontalResizeMargin, nonClientMargin.left);
+    sNonClientOffset.right =
+        std::min(sHorizontalResizeMargin, nonClientMargin.right);
+  }
 
   if (sMaximized) {
     HMONITOR monitor =
@@ -1989,9 +2019,9 @@ static Result<Ok, PreXULSkeletonUIError> CreateAndStorePreXULSkeletonUIImpl(
     }
 
     sWindowWidth =
-        mi.rcWork.right - mi.rcWork.left + sNonClientHorizontalMargins * 2;
+        mi.rcWork.right - mi.rcWork.left + sHorizontalResizeMargin * 2;
     sWindowHeight =
-        mi.rcWork.bottom - mi.rcWork.top + sNonClientVerticalMargins * 2;
+        mi.rcWork.bottom - mi.rcWork.top + sVerticalResizeMargin * 2;
   } else {
     sWindowWidth = static_cast<int>(windowWidth);
     sWindowHeight = static_cast<int>(windowHeight);
