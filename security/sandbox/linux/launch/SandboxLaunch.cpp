3abb5759b296ec5c99c588b4ba992ae5381184a0
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-10-22 22:12:09 +0000

    Bug 1925971 - Only call the interposer when the fork server is enabled. r=gerard-majax
    Differential Revision: https://phabricator.services.mozilla.com/D226303
    
    UltraBlame original commit: 36b54309304b74b86e85ece83299e50bb0aac71a

--- security/sandbox/linux/launch/SandboxLaunch.cpp
+++ security/sandbox/linux/launch/SandboxLaunch.cpp
@@ -625,14 +625,24 @@ pid_t SandboxLaunch::Fork() {
   
   sigset_t oldSigs;
   BlockAllSignals(&oldSigs);
+
+#if defined(MOZ_ENABLE_FORKSERVER)
   run_moz_pthread_atfork_handlers_prefork();
+#endif
+
   pid_t pid = ForkWithFlags(mFlags);
   if (pid != 0) {
+#if defined(MOZ_ENABLE_FORKSERVER)
     run_moz_pthread_atfork_handlers_postfork_parent();
+#endif
+
     RestoreSignals(&oldSigs);
     return pid;
   }
+
+#if defined(MOZ_ENABLE_FORKSERVER)
   run_moz_pthread_atfork_handlers_postfork_child();
+#endif
 
   
   
