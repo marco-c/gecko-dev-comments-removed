2d24de8dcfc18be96edc768be63e51050c26ffe3
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-10-22 22:08:57 +0000

    Bug 1924541 - Add and use assertThrowsInstanceOf helpers; r=dminor
    Differential Revision: https://phabricator.services.mozilla.com/D225521
    
    UltraBlame original commit: bce63f669a15a8dd455e90a0c74b5334d376bc07

--- js/src/tests/non262/regress/regress-619003-1.js
+++ js/src/tests/non262/regress/regress-619003-1.js
@@ -2,19 +2,15 @@
 
 
 
-var expect = 'SyntaxError: duplicate argument names not allowed in this context';
-var actual = 'No error';
 
 var a = [];
 
 
 for (var i = 0; i < 200; i++) {
     a.push("b" + i);
-    try {
-        eval("(function ([" + a.join("],[") + "],a,a){})");
-    } catch (e) {
-        actual = '' + e;
-    }
-    assertEq(actual, expect);
+    assertThrowsInstanceOfWithMessage(
+        () => eval("(function ([" + a.join("],[") + "],a,a){})"),
+        SyntaxError,
+        'duplicate argument names not allowed in this context');
 }
 reportCompare(0, 0, "ok");
