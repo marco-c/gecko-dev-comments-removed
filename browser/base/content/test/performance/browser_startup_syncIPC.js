1921984ca384e09135151b9764fa3b4330d7b8af
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-10-22 21:41:29 +0000

    Bug 1917652 Part 4: Update IPC timing expectations for gpu process platforms. r=mconley
    The message timings that were modified in this test are sent by the
    GPUProcessManager, and this test was using WIN and !WIN as a proxy for
    whether or not a GPU process was in place. The modifications here make
    that more explicit, although they remain approximate. Anything that is
    conditioned on GPUPROCESS needs to "ignoreIfUnused" because those
    markers will be sent or not sent depending on the presence of the GPU
    process, which has complex controls not completely covered by the logic
    in this test.
    
    Differential Revision: https://phabricator.services.mozilla.com/D225565
    
    UltraBlame original commit: 8002391fe2ead2d0bcddd115d0f4ca94b6963759

--- browser/base/content/test/performance/browser_startup_syncIPC.js
+++ browser/base/content/test/performance/browser_startup_syncIPC.js
@@ -15,6 +15,11 @@ const SKELETONUI = Services.prefs.getBoolPref(
   false
 );
 
+const GPUPROCESS =
+  ((WIN || LINUX) &&
+    Services.prefs.getBoolPref("layers.gpu-process.enabled")) ||
+  Services.prefs.getBoolPref("layers.gpu-process.force-enabled");
+
 
 
 
@@ -52,19 +57,14 @@ const startupPhases = {
     {
       
       name: "PCompositorBridge::Msg_NotifyChildCreated",
-      condition: !WIN,
+      condition: !GPUPROCESS,
+      ignoreIfUnused: true,
       maxCount: 1,
     },
     {
-      name: "PCompositorBridge::Msg_NotifyChildCreated",
-      condition: WIN,
-      ignoreIfUnused: true, 
-      maxCount: 2,
-    },
-    {
       name: "PCompositorBridge::Msg_MapAndNotifyChildCreated",
-      condition: WIN,
-      ignoreIfUnused: true, 
+      condition: GPUPROCESS,
+      ignoreIfUnused: true,
       maxCount: 2,
     },
     {
@@ -80,8 +80,8 @@ const startupPhases = {
     },
     {
       name: "PCompositorBridge::Msg_Initialize",
-      condition: WIN,
-      ignoreIfUnused: true, 
+      condition: GPUPROCESS,
+      ignoreIfUnused: true,
       maxCount: 3,
     },
     {
@@ -92,8 +92,8 @@ const startupPhases = {
     },
     {
       name: "PGPU::Msg_AddLayerTreeIdMapping",
-      condition: WIN,
-      ignoreIfUnused: true, 
+      condition: GPUPROCESS,
+      ignoreIfUnused: true,
       maxCount: 5,
     },
     {
@@ -321,7 +321,7 @@ const startupPhases = {
     
     {
       name: "PGPU::Msg_AddLayerTreeIdMapping",
-      condition: WIN,
+      condition: GPUPROCESS,
       ignoreIfUnused: true,
       maxCount: 1,
     },
