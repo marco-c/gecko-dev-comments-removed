ed6c8e5d88f5f199d1da3fcbcb2aac6e858af13c
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-11-01 01:02:02 +0000

    Bug 1925740 - Fix TestBundleSupportWithZeroPort test case;r=ng
    Differential Revision: https://phabricator.services.mozilla.com/D226518
    
    UltraBlame original commit: 00b83cdd96d9942837f8c4f0346516b8a31c7179

--- media/webrtc/signaling/gtest/jsep_session_unittest.cpp
+++ media/webrtc/signaling/gtest/jsep_session_unittest.cpp
@@ -7753,7 +7753,7 @@ TEST_F(JsepSessionTest, TestBundleSupportWithZeroPort) {
   mSessionOff->SetRemoteDescription(kJsepSdpAnswer, answerSdp);
 
   
-  for (const auto offerTransceiver : GetTransceivers(*mSessionOff)) {
+  for (const auto& offerTransceiver : GetTransceivers(*mSessionOff)) {
     ASSERT_TRUE(offerTransceiver.mRecvTrack.GetActive());
     ASSERT_TRUE(offerTransceiver.mSendTrack.GetActive());
   }
