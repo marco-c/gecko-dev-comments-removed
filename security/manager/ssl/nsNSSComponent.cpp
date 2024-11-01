b8166f5b4a915b045dfd8f178727936069fc408c
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-11-01 02:05:29 +0000

    Bug 1927501: apply code formatting via Lando
    # ignore-this-changeset
    
    UltraBlame original commit: 4f7833cdcbcd6fc1783a72941f94492b0e0ee82f

--- security/manager/ssl/nsNSSComponent.cpp
+++ security/manager/ssl/nsNSSComponent.cpp
@@ -1308,14 +1308,14 @@ static nsresult GetNSSProfilePath(nsAutoCString& aProfilePath) {
 
 static nsresult AttemptToRenamePKCS11ModuleDB(const nsACString& profilePath) {
   nsCOMPtr<nsIFile> profileDir;
-#ifdef XP_WIN
+#  ifdef XP_WIN
   
   
   MOZ_TRY(NS_NewLocalFile(u""_ns, getter_AddRefs(profileDir)));
   MOZ_TRY(profileDir->InitWithPath(NS_ConvertUTF8toUTF16(profilePath)));
-#else
+#  else
   MOZ_TRY(NS_NewNativeLocalFile(profilePath, getter_AddRefs(profileDir)));
-#endif
+#  endif
   const char* moduleDBFilename = "pkcs11.txt";
   nsAutoCString destModuleDBFilename(moduleDBFilename);
   destModuleDBFilename.Append(".fips");
