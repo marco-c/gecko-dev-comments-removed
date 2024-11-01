3008a3cb8a718509991a7ef6c55c7e9dd13a2cc7
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-11-01 02:01:01 +0000

    Bug 1916679 - Enable ScotchBonnet on nightly. r=daisuke
    Differential Revision: https://phabricator.services.mozilla.com/D226781
    
    UltraBlame original commit: 96fe9c781b68d282f7d4debe45042ad06c178f62

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -444,7 +444,11 @@ pref("browser.urlbar.suggest.quickactions",         true);
 pref("browser.urlbar.deduplication.enabled", false);
 pref("browser.urlbar.deduplication.thresholdDays", 7);
 
+#ifdef NIGHTLY_BUILD
+pref("browser.urlbar.scotchBonnet.enableOverride", true);
+#else
 pref("browser.urlbar.scotchBonnet.enableOverride", false);
+#endif
 
 
 pref("browser.urlbar.trending.featureGate", true);
