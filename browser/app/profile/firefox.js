91105cf5cbec81f44cd934879dc69a47755bafbe
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-11-01 00:59:53 +0000

    Bug 1925982: Enable USER_RESTRICTED for content processes on early Beta. r=handyman
    Differential Revision: https://phabricator.services.mozilla.com/D226330
    
    UltraBlame original commit: fabe674eca2a585fdb20bc0c89c2e1885a6284b8

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1483,7 +1483,7 @@ pref("browser.bookmarks.editDialog.maxRecentFolders", 7);
   
   
   
-  #if defined(NIGHTLY_BUILD)
+  #if defined(EARLY_BETA_OR_EARLIER)
     pref("security.sandbox.content.level", 8);
   #else
     pref("security.sandbox.content.level", 7);
