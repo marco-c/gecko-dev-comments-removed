b8e5b2634f6a6fea49240cebc39bc12190d52e7b
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-11-01 01:07:15 +0000

    Bug 1926253 - Only enable fmt static assert for double conversion on recent clang. r=glandium
    Differential Revision: https://phabricator.services.mozilla.com/D226656
    
    UltraBlame original commit: 8db300296a58df2a544398079732a76484ad111f

--- third_party/fmt/include/fmt/format.h
+++ third_party/fmt/include/fmt/format.h
@@ -3150,7 +3150,7 @@ FMT_CONSTEXPR20 auto format_float(Float value, int precision,
                                   buffer<char>& buf) -> int {
   
   #ifdef __clang__
-  #if __clang_major__ > 8
+  #if __clang_major__ >= 17
     static_assert(false,
       "This method is not to be used in Gecko, use format_float_gecko");
   #endif
