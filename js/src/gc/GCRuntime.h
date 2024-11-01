842ae8eaae183147657cfc15de3083f190a435bc
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-11-01 02:29:21 +0000

    Bug 1786451 - Part 4: Stall and retry on failure when allocating chunks in GC r=sfink
    Normally we trigger a GC on chunk allocation failure (either last ditch major
    GC or minor GC when trying to extend the nursery). We can't recover from
    allocation failure during GC however so this adds the stall and retry behaviour
    there.
    
    Differential Revision: https://phabricator.services.mozilla.com/D226465
    
    UltraBlame original commit: 6b63840889b6910a3eb8b7931d5da6cdc4bbdd68

--- js/src/gc/GCRuntime.h
+++ js/src/gc/GCRuntime.h
@@ -19,6 +19,7 @@
 #include "gc/GCMarker.h"
 #include "gc/GCParallelTask.h"
 #include "gc/IteratorUtils.h"
+#include "gc/Memory.h"
 #include "gc/Nursery.h"
 #include "gc/Scheduling.h"
 #include "gc/Statistics.h"
@@ -572,10 +573,12 @@ class GCRuntime {
 
   
   
-  ArenaChunk* getOrAllocChunk(AutoLockGCBgAlloc& lock);
+  ArenaChunk* getOrAllocChunk(StallAndRetry stallAndRetry,
+                              AutoLockGCBgAlloc& lock);
 
   
-  ArenaChunk* takeOrAllocChunk(AutoLockGCBgAlloc& lock);
+  ArenaChunk* takeOrAllocChunk(StallAndRetry stallAndRetry,
+                               AutoLockGCBgAlloc& lock);
 
   void recycleChunk(ArenaChunk* chunk, const AutoLockGC& lock);
 
@@ -701,7 +704,7 @@ class GCRuntime {
 
   
   friend class ArenaLists;
-  ArenaChunk* pickChunk(AutoLockGCBgAlloc& lock);
+  ArenaChunk* pickChunk(StallAndRetry stallAndRetry, AutoLockGCBgAlloc& lock);
   Arena* allocateArena(ArenaChunk* chunk, Zone* zone, AllocKind kind,
                        ShouldCheckThresholds checkThresholds,
                        const AutoLockGC& lock);
