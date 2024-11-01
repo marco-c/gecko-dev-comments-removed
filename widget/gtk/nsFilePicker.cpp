96d754d015e66bfd01e01e045dcd79a1dd7d44af
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-11-01 02:26:13 +0000

    Backed out changeset 7b8177dc3299 (bug 1927101) for causing bp-nu bustages in nsFilePicker.cpp.
    UltraBlame original commit: 29c342a01e7a3f942447e277323c1c2b8e7f05ec

--- widget/gtk/nsFilePicker.cpp
+++ widget/gtk/nsFilePicker.cpp
@@ -421,11 +421,6 @@ nsFilePicker::Open(nsIFilePickerShownCallback* aCallback) {
     return NS_OK;
   }
 
-  
-  if (gfxPlatform::IsHeadless()) {
-    return NS_ERROR_NOT_AVAILABLE;
-  }
-
   NS_ConvertUTF16toUTF8 title(mTitle);
 
   GtkWindow* parent_widget =
