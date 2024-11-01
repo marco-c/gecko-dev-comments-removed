9c26e8a3b66dde26bf2b31a2b52d03d481ba01ff
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-11-01 01:09:06 +0000

    Bug 1924100 - More fine-grained settings change. r=stransky
    Avoid calling OnSettingsChange when we start watching dbus if nothing
    changed.
    
    OnSettingsChanged already checks sIgnoreChangedSettings.
    
    Differential Revision: https://phabricator.services.mozilla.com/D226693
    
    UltraBlame original commit: 703fb617c8b70339e2c95cc43de4b8116d867f3e

--- widget/gtk/nsLookAndFeel.h
+++ widget/gtk/nsLookAndFeel.h
@@ -52,7 +52,8 @@ class nsLookAndFeel final : public nsXPLookAndFeel {
 
   static const nscolor kBlack = NS_RGB(0, 0, 0);
   static const nscolor kWhite = NS_RGB(255, 255, 255);
-  void RecomputeDBusSettings();
+  
+  bool RecomputeDBusSettings();
   
   bool RecomputeDBusAppearanceSetting(const nsACString& aKey, GVariant* aValue);
 
