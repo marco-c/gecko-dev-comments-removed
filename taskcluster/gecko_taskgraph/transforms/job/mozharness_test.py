2e88fd50dc6c5aef9faf544e289d661be8eb61ac
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-11-16 02:24:19 +0000

    Bug 1926291 - start pipewire instead of pulseaudio on ubuntu 24.04 r=taskgraph-reviewers,bhearsum
    Differential Revision: https://phabricator.services.mozilla.com/D226491
    
    UltraBlame original commit: 97a3ac2f72c0c47f7804cf10455edb954f968802

--- taskcluster/gecko_taskgraph/transforms/job/mozharness_test.py
+++ taskcluster/gecko_taskgraph/transforms/job/mozharness_test.py
@@ -147,7 +147,6 @@ def mozharness_test_on_docker(config, job, taskdesc):
             "MOZHARNESS_CONFIG": " ".join(mozharness["config"]),
             "MOZHARNESS_SCRIPT": mozharness["script"],
             "MOZILLA_BUILD_URL": {"task-reference": installer},
-            "NEED_PULSEAUDIO": "true",
             "NEED_WINDOW_MANAGER": "true",
             "ENABLE_E10S": str(bool(test.get("e10s"))).lower(),
             "WORKING_DIR": "/builds/worker",
@@ -156,14 +155,19 @@ def mozharness_test_on_docker(config, job, taskdesc):
 
     env["PYTHON"] = "python3"
 
-    
-    
     if test.get("docker-image", {}).get("in-tree") == "ubuntu1804-test":
+        env["NEED_PULSEAUDIO"] = "true"
+
+        
+        
         if "wdspec" in job["run"]["test"]["suite"] or (
             "marionette" in job["run"]["test"]["suite"]
             and "headless" not in job["label"]
         ):
             env.update({"NEED_COMPIZ": "true"})
+
+    if test.get("docker-image", {}).get("in-tree") == "ubuntu2404-test":
+        env["NEED_PIPEWIRE"] = "true"
 
     
     if "wayland" in job["label"]:
