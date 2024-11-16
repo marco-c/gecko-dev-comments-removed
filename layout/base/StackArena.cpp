f812d9363eb340a25f8c99b7d20a289a5a3f7f13
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-11-16 02:46:03 +0000

    Bug 1931356 - Remove NS_ROUNDUP (replace usage by RoundUpToMultiple) r=emilio
    Differential Revision: https://phabricator.services.mozilla.com/D229017
    
    UltraBlame original commit: 3deea2a8fd3f286d291bc1e46fd1936ab8f89b78

--- layout/base/StackArena.cpp
+++ layout/base/StackArena.cpp
@@ -5,7 +5,6 @@
 
 
 #include "StackArena.h"
-#include "nsAlgorithm.h"
 #include "nsDebug.h"
 
 namespace mozilla {
@@ -114,7 +113,7 @@ void* StackArena::Allocate(size_t aSize) {
   NS_ASSERTION(mStackTop > 0, "Allocate called without Push");
 
   
-  aSize = NS_ROUNDUP<size_t>(aSize, 8);
+  aSize = RoundUpToMultiple(aSize, 8);
 
   
   if (mPos + aSize >= StackBlock::MAX_USABLE_SIZE) {
