f44a72d4b7a06bb6717f923b1b7fcde7788bf893
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-11-16 01:33:53 +0000

    Bug 1924247 - [1/5] Fix WM_MOUSEHWHEEL return code  r=masayuki
    The semantics of the old technically-third-party MSH_MOUSEWHEEL message
    were different from that of the modern WM_MOUSEWHEEL. [1] Unfortunately,
    when we updated the scroll-wheel code back in Firefox 1.8/2.0, we copied
    the MSH_MOUSEWHEEL semantics over into the WM_MOUSEWHEEL code. (See bug
    347875.)
    
    This has probably never caused issues, but we might as well fix it.
    
    [1] https://devblogs.microsoft.com/oldnewthing/20080806-00/?p=21353
    
    Differential Revision: https://phabricator.services.mozilla.com/D226266
    
    UltraBlame original commit: 42d6b08aed1169c7e4bbf25b5b95abdc00d612d9

--- widget/windows/WinMouseScrollHandler.cpp
+++ widget/windows/WinMouseScrollHandler.cpp
@@ -178,7 +178,7 @@ bool MouseScrollHandler::ProcessMessage(nsWindow* aWidget, UINT msg,
       
       
       aResult.mConsumed = true;
-      aResult.mResult = (msg != WM_MOUSEHWHEEL);
+      aResult.mResult = 0;
       return true;
 
     case WM_HSCROLL:
