687e7e0cd080e6a34d176d2a34580a0c2e3f106a
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-11-16 02:26:01 +0000

    Bug 1780062 - The text information on the profile manager's pages is not read by NVDA - r=Jamie,reusable-components-reviewers,mstriemer
    Differential Revision: https://phabricator.services.mozilla.com/D227058
    
    UltraBlame original commit: 75115dec418590f8ec5998b13c598d7580f19096

--- toolkit/profile/content/createProfileWizard.js
+++ toolkit/profile/content/createProfileWizard.js
@@ -50,6 +50,10 @@ function initWizard() {
 
 
 function initSecondWizardPage() {
+  document
+    .getElementById("createProfileWizard")
+    .removeAttribute("aria-describedby");
+
   var profileName = document.getElementById("profileName");
   profileName.select();
   profileName.focus();
