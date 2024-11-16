b60120a12dfd07e37e58629795615b7ae1b50ef6
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-11-16 02:53:58 +0000

    Bug 1920798 - Fix display issues with New Tab Weather widget location 'close' button r=home-newtab-reviewers,nbarrett
    - Updated code with hjones's patch and verified that everything is working as expected.
    
    Combined screenshots of the search field with the close button after the change:
    
    {F8993926}
    
    Differential Revision: https://phabricator.services.mozilla.com/D228285
    
    UltraBlame original commit: 57caaf214bdf8c45f9bd2451cea60be8c80b6ddd

--- browser/components/newtab/data/content/activity-stream.bundle.js
+++ browser/components/newtab/data/content/activity-stream.bundle.js
@@ -10811,8 +10811,11 @@ function LocationSearch({
     onChange: handleChange,
     value: userInput,
     onKeyDown: handleKeyDown
-  }), external_React_default().createElement("button", {
-    className: "close-icon",
+  }), external_React_default().createElement("moz-button", {
+    class: "close-icon",
+    type: "icon ghost",
+    size: "small",
+    iconSrc: "chrome://global/skin/icons/close.svg",
     onClick: handleCloseSearch
   }), external_React_default().createElement("datalist", {
     id: "merino-location-list"
