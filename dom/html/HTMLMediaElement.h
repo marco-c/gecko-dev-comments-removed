eb08e182cd36c53cc38ffc1d51f4155432388350
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-11-16 02:41:42 +0000

    Bug 1929335 - part1 : add new probe to record all types of media element error. r=media-playback-reviewers,aosmond
    Differential Revision: https://phabricator.services.mozilla.com/D228733
    
    UltraBlame original commit: e729337a65dc5abacb24679cab76d3e8ef3342da

--- dom/html/HTMLMediaElement.h
+++ dom/html/HTMLMediaElement.h
@@ -1248,7 +1248,7 @@ class HTMLMediaElement : public nsGenericHTMLElement,
 
 
   void Error(uint16_t aErrorCode,
-             const nsACString& aErrorDetails = nsCString());
+             const Maybe<MediaResult>& aResult = Nothing());
 
   
 
