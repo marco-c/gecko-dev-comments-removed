e0a2cf22dbb521c42a21a8ee3ee6a9107de4686f
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-11-16 02:36:12 +0000

    Bug 1929372 - [mozversioncontrol] Fix missing import shim to SrcRepository, r=sheehan
    Differential Revision: https://phabricator.services.mozilla.com/D228819
    
    UltraBlame original commit: 1631a4d2ffc43099bb6e9ae202667fb97a4aaebe

--- python/mozversioncontrol/mozversioncontrol/__init__.py
+++ python/mozversioncontrol/mozversioncontrol/__init__.py
@@ -11,3 +11,4 @@ from mozversioncontrol.factory import (
 from mozversioncontrol.repo.base import Repository  
 from mozversioncontrol.repo.git import GitRepository  
 from mozversioncontrol.repo.mercurial import HgRepository  
+from mozversioncontrol.repo.source import SrcRepository  
