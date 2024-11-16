3c0f467528982b776f503449ae8d4c67c31124f2
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-11-16 02:15:13 +0000

    Bug 1929384 - do not close window if there are still collapsed tab groups r=dao,tabbrowser-reviewers
    When closing the last visible tab, if there are any collapsed tab groups, make one of the tabs in a tab group active. Based on pre-existing behavior, activating a tab in a collapsed tab group causes the tab group to expand.
    
    Currently, if you close the last visible tab while you still have collapsed tab groups present on the tab bar, the browser window will close. This is surprising and sad behavior for users.
    
    Differential Revision: https://phabricator.services.mozilla.com/D228274
    
    UltraBlame original commit: fb62d287a0167a7462a8230062a0f461d1d8c3f1

--- browser/components/tabbrowser/content/tabbrowser.js
+++ browser/components/tabbrowser/content/tabbrowser.js
@@ -359,6 +359,13 @@
       return this.tabContainer.allGroups;
     },
 
+    get tabsInCollapsedTabGroups() {
+      return this.tabGroups
+        .filter(tabGroup => tabGroup.collapsed)
+        .flatMap(tabGroup => tabGroup.tabs)
+        .filter(tab => !tab.hidden && !tab.isClosing);
+    },
+
     get tabGroupMenu() {
       delete this.tabGroupMenu;
       return (this.tabGroupMenu = document.getElementById("tab-group-editor"));
@@ -4541,7 +4548,13 @@
 
       var closeWindow = false;
       var newTab = false;
-      if (aTab.visible && this.visibleTabs.length == 1) {
+      let anyRemainingTabsInCollapsedTabGroups =
+        !!this.tabsInCollapsedTabGroups.length;
+      if (
+        aTab.visible &&
+        this.visibleTabs.length == 1 &&
+        !anyRemainingTabsInCollapsedTabGroups
+      ) {
         closeWindow =
           closeWindowWithLastTab != null
             ? closeWindowWithLastTab
@@ -4955,6 +4968,28 @@
         tab = this.tabContainer.findNextTab(aTab, {
           direction: -1,
           filter: _tab => remainingTabs.includes(_tab),
+        });
+      }
+
+      if (tab) {
+        return tab;
+      }
+
+      
+      
+      let eligibleTabs = new Set(this.tabsInCollapsedTabGroups).difference(
+        excludeTabs
+      );
+
+      tab = this.tabContainer.findNextTab(aTab, {
+        direction: 1,
+        filter: _tab => eligibleTabs.has(_tab),
+      });
+
+      if (!tab) {
+        tab = this.tabContainer.findNextTab(aTab, {
+          direction: -1,
+          filter: _tab => eligibleTabs.has(_tab),
         });
       }
 
