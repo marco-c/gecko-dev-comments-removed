a80547a2340530550928aa95516b8a248cc818f9
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-11-26 23:28:24 +0000

    Bug 1931629 - feat(webgpu): recognize unimplemented `clip-distances` feature r=webgpu-reviewers,webidl,teoxoy,saschanaz
    Differential Revision: https://phabricator.services.mozilla.com/D229830
    
    UltraBlame original commit: 84af6e88d4a794b5defef062441f48b19e73aef4

--- dom/webgpu/Adapter.cpp
+++ dom/webgpu/Adapter.cpp
@@ -127,6 +127,10 @@ static Maybe<ffi::WGPUFeatures> ToWGPUFeatures(
     case dom::GPUFeatureName::Float32_filterable:
       return Some(WGPUFeatures_FLOAT32_FILTERABLE);
 
+    case dom::GPUFeatureName::Clip_distances:
+      
+      return Nothing();
+
     case dom::GPUFeatureName::Dual_source_blending:
       
       return Nothing();  
