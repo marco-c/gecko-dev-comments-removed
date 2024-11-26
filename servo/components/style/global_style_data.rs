22ce8dd8045673544cdd9bb3ab4133cef6dedaf4
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-11-26 23:27:08 +0000

    Bug 1933015: Allow stylo to compile for wasm32-unknown-unknown (r=emilio)
    I have not tested whether this runs correctly, but this fixes the build.
    
    Signed-off-by: Nico Burns <niconicoburns.com>
    
    Differential Revision: https://phabricator.services.mozilla.com/D230018
    
    UltraBlame original commit: 2b57b9cb9286959997b7ac808cc54fede672fd3d

--- servo/components/style/global_style_data.rs
+++ servo/components/style/global_style_data.rs
@@ -26,6 +26,20 @@ pub type PlatformThreadHandle = RawPthread;
 pub type PlatformThreadHandle = RawHandle;
 
 
+#[cfg(target_arch = "wasm32")]
+pub struct DummyThreadHandle;
+#[cfg(target_arch = "wasm32")]
+impl DummyThreadHandle {
+    
+    pub fn join(&self) {
+        
+    }
+}
+#[cfg(target_arch = "wasm32")]
+
+pub type PlatformThreadHandle = DummyThreadHandle;
+
+
 pub struct GlobalStyleData {
     
     pub shared_lock: SharedRwLock,
@@ -131,6 +145,8 @@ impl StyleThreadPool {
             let handle = join_handle.as_pthread_t();
             #[cfg(windows)]
             let handle = join_handle.as_raw_handle();
+            #[cfg(target_arch = "wasm32")]
+            let handle = DummyThreadHandle;
 
             handles.push(handle);
         }
