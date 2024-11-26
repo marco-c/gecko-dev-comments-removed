e32c6ee97e49876660db62322aacc95b4771811a
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-11-26 23:29:52 +0000

    Bug 1930673 - Restore Clamp operation that works on empty Ranges
    Given that an empty ranges is any range where the upper bound is
    strictly lower than the lower bound, std::clamp is not a good candidate
    
    Differential Revision: https://phabricator.services.mozilla.com/D229129
    
    UltraBlame original commit: c870f71ba8d9aef09f6bb9b3e4f5127058d8b465

--- dom/media/webrtc/MediaTrackConstraints.h
+++ dom/media/webrtc/MediaTrackConstraints.h
@@ -63,7 +63,11 @@ class NormalizedConstraintSet {
 
     template <class ConstrainRange>
     void SetFrom(const ConstrainRange& aOther);
-    ValueType Clamp(ValueType n) const { return std::clamp(n, mMin, mMax); }
+
+    
+    ValueType Clamp(ValueType n) const {
+      return std::max(mMin, std::min(n, mMax));
+    }
     ValueType Get(ValueType defaultValue) const {
       return Clamp(mIdeal.valueOr(defaultValue));
     }
