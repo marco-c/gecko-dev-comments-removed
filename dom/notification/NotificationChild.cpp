72fce4472c7ae974dc506d209db5e5382bb7c2f1
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-11-26 22:47:54 +0000

    Bug 1931497 - Part 2: Implement tab focusing for workers on RecvNotifyClick r=asuth
    Differential Revision: https://phabricator.services.mozilla.com/D218666
    
    UltraBlame original commit: 914675ac5b1f9d10632f62193072bb281a53d8c1

--- dom/notification/NotificationChild.cpp
+++ dom/notification/NotificationChild.cpp
@@ -9,6 +9,7 @@
 #include "WindowGlobalChild.h"
 #include "mozilla/dom/Event.h"
 #include "mozilla/dom/Notification.h"
+#include "mozilla/dom/WorkerRunnable.h"
 #include "nsFocusManager.h"
 
 namespace mozilla::dom::notification {
@@ -25,6 +26,24 @@ NotificationChild::NotificationChild(Notification* aNonPersistentNotification,
     return;
   }
 }
+
+class FocusWindowRunnable : public WorkerMainThreadRunnable {
+ public:
+  explicit FocusWindowRunnable(WorkerPrivate* aWorkerPrivate)
+      : WorkerMainThreadRunnable(aWorkerPrivate,
+                                 "Notification :: FocusWindowRunnable"_ns) {}
+
+ protected:
+  
+  MOZ_CAN_RUN_SCRIPT_BOUNDARY bool MainThreadRun() override {
+    RefPtr<nsPIDOMWindowInner> inner = mWorkerRef->Private()->GetWindow();
+    if (inner->IsCurrentInnerWindow()) {
+      nsCOMPtr<nsPIDOMWindowOuter> outer = inner->GetOuterWindow();
+      nsFocusManager::FocusWindow(outer, CallerType::System);
+    }
+    return true;
+  }
+};
 
 
 
@@ -61,6 +80,15 @@ MOZ_CAN_RUN_SCRIPT_BOUNDARY IPCResult NotificationChild::RecvNotifyClick() {
         nsFocusManager::FocusWindow(outer, CallerType::System);
       }
     }
+  } else if (WorkerPrivate* wp = GetCurrentThreadWorkerPrivate()) {
+    if (!wp->IsDedicatedWorker()) {
+      
+      return IPC_OK();
+    }
+
+    RefPtr<FocusWindowRunnable> runnable =
+        new FocusWindowRunnable(wp->GetTopLevelWorker());
+    runnable->Dispatch(wp, Canceling, IgnoreErrors());
   }
   return IPC_OK();
 }
