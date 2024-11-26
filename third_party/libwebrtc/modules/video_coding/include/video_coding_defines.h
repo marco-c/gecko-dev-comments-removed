1e226efa7e5425a4786b30a7327c01f372c41bda
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-11-26 23:59:32 +0000

    Bug 1928110 - Vendor libwebrtc from 215401f651
    Upstream commit: https://webrtc.googlesource.com/src/+/215401f651cca1b5e91e087b551f0077a209cba1
        Reland "Add a FrameToRender argument struct as input to FrameToRender"
    
        This is a reland of commit 01f91c81f7660be842fa44e96bf804a8b2402f47
    
        Original change's description:
        > Add a FrameToRender argument struct as input to FrameToRender
        >
        > This is to make it easier to add new arguments to the method in the
        > future. We will remove the already existing method accordingly to WebRTCs deprecation rules.
        >
        > Bug: webrtc:358039777
        > Change-Id: Id0706de5216fbd0182cac80ebfccfc4a6a055ee8
        > Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/364642
        > Commit-Queue: Fanny Linderborg <linderborgwebrtc.org>
        > Reviewed-by: Erik Språng <sprangwebrtc.org>
        > Auto-Submit: Fanny Linderborg <linderborgwebrtc.org>
        > Cr-Commit-Position: refs/heads/main{#43181}
    
        Bug: webrtc:358039777
        Change-Id: I404bb9660d9f4436c0658814fd3ac7d74e483f0f
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/364900
        Reviewed-by: Erik Språng <sprangwebrtc.org>
        Commit-Queue: Fanny Linderborg <linderborgwebrtc.org>
        Cr-Commit-Position: refs/heads/main{#43188}
    
    UltraBlame original commit: 2f3ee09dab0b0ca38e3a11958f3f9244d7923656

--- third_party/libwebrtc/modules/video_coding/include/video_coding_defines.h
+++ third_party/libwebrtc/modules/video_coding/include/video_coding_defines.h
@@ -16,8 +16,10 @@
 
 #include <optional>
 
+#include "api/units/time_delta.h"
 #include "api/video/video_content_type.h"
 #include "api/video/video_frame.h"
+#include "api/video/video_frame_type.h"
 #include "api/video/video_timing.h"
 #include "api/video_codecs/video_decoder.h"
 
@@ -51,11 +53,26 @@ enum VCMVideoProtection {
 
 class VCMReceiveCallback {
  public:
+  struct FrameToRender {
+    VideoFrame& video_frame;
+    std::optional<uint8_t> qp;
+    TimeDelta decode_time;
+    VideoContentType content_type;
+    VideoFrameType frame_type;
+  };
+
+  
   virtual int32_t FrameToRender(VideoFrame& videoFrame,  
                                 std::optional<uint8_t> qp,
                                 TimeDelta decode_time,
                                 VideoContentType content_type,
                                 VideoFrameType frame_type) = 0;
+  
+  virtual int32_t FrameToRender(const struct FrameToRender& arguments) {
+    return FrameToRender(arguments.video_frame, arguments.qp,
+                         arguments.decode_time, arguments.content_type,
+                         arguments.frame_type);
+  }
 
   virtual void OnDroppedFrames(uint32_t frames_dropped);
 
