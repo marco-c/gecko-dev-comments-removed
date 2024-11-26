ea4ec5ea8cd68e3c538e2ce34f532ae2fec81a66
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-11-26 23:08:15 +0000

    Bug 1921154 - add AV1 pref;r=pehrsons
    Differential Revision: https://phabricator.services.mozilla.com/D223803
    
    UltraBlame original commit: fef404e8c0ce79f1eb6d8ff7c2918637d686f724

--- dom/media/webrtc/libwebrtcglue/VideoConduit.h
+++ dom/media/webrtc/libwebrtcglue/VideoConduit.h
@@ -69,6 +69,8 @@ class WebrtcVideoConduit
  public:
   
   static bool HasH264Hardware();
+  
+  static bool HasAv1();
 
   Maybe<int> ActiveSendPayloadType() const override;
   Maybe<int> ActiveRecvPayloadType() const override;
