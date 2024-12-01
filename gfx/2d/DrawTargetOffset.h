2b65c16a7a1a860e8275b5fc519c7bd6a1d978a1
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-12-01 01:15:33 +0000

    Bug 1929461 - Don't let Translator::GetCurrentDrawTarget() return an invalid DT. r=gfx-reviewers,nical
    Differential Revision: https://phabricator.services.mozilla.com/D230532
    
    UltraBlame original commit: fddbb58a555bd3e715bf0070d28c6bf1e43e82a8

--- gfx/2d/DrawTargetOffset.h
+++ gfx/2d/DrawTargetOffset.h
@@ -52,6 +52,8 @@ class DrawTargetOffset : public DrawTarget {
 
   bool Init(DrawTarget* aDrawTarget, IntPoint aOrigin);
 
+  virtual bool IsValid() const override { return mDrawTarget->IsValid(); }
+
   
   virtual bool IsTiledDrawTarget() const override { return true; }
 
