11bfa4d4cad059f7408ae9a1faaab52ed5bb4b91
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-12-13 02:34:07 +0000

    Bug 1928619 - test262-export: skip some shell.js files; r=dminor
    Differential Revision: https://phabricator.services.mozilla.com/D227655
    
    UltraBlame original commit: 296ad5859b4dfaebf0c57ca0e65302000eb17703

--- js/src/tests/non262/reflect-parse/shell.js
+++ js/src/tests/non262/reflect-parse/shell.js
@@ -1,3 +1,4 @@
+
 loadRelativeToScript('PatternAsserts.js');
 
 
--- js/src/tests/test262-export.py
+++ js/src/tests/test262-export.py
@@ -45,12 +45,16 @@ def findAndCopyIncludes(dirPath: str, baseDir: str, includeDir: str) -> "list[st
 
         
         if os.path.exists(shellFile) and os.path.getsize(shellFile) > 0:
-            
-            includeFileName = "sm/" + relPath.replace("/", "-") + "-shell.js"
-            includes.append(includeFileName)
+            with open(shellFile, "rb") as f:
+                testSource = f.read()
 
-            includesPath = os.path.join(includeDir, includeFileName)
-            shutil.copyfile(shellFile, includesPath)
+            if b"// SKIP test262 export" not in testSource:
+                
+                includeFileName = "sm/" + relPath.replace("/", "-") + "-shell.js"
+                includes.append(includeFileName)
+
+                includesPath = os.path.join(includeDir, includeFileName)
+                shutil.copyfile(shellFile, includesPath)
 
         relPath = os.path.split(relPath)[0]
 
