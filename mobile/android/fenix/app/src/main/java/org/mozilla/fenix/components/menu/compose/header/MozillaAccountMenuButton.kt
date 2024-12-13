65dd5251cc9459f51bec1edf4e02c9ec4dba001e
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-12-13 03:43:57 +0000

    Bug 1902373 - Part 5: Refactor LightDarkPreview annotation into compose-base r=android-reviewers,007
    - Optimize imports was ran on the Fenix project. This will include some import optimizations.
    
    Differential Revision: https://phabricator.services.mozilla.com/D220453
    
    UltraBlame original commit: e04155caa4f008764d218005d9c2ec8b41006387

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/components/menu/compose/header/MozillaAccountMenuButton.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/components/menu/compose/header/MozillaAccountMenuButton.kt
@@ -24,8 +24,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import mozilla.components.compose.base.annotation.LightDarkPreview
 import mozilla.components.service.fxa.manager.AccountState
 import mozilla.components.service.fxa.manager.AccountState.Authenticated
 import mozilla.components.service.fxa.manager.AccountState.Authenticating
@@ -34,7 +36,6 @@ import mozilla.components.service.fxa.manager.AccountState.NotAuthenticated
 import mozilla.components.service.fxa.store.Account
 import org.mozilla.fenix.R
 import org.mozilla.fenix.compose.Image
-import org.mozilla.fenix.compose.annotation.LightDarkPreview
 import org.mozilla.fenix.theme.FirefoxTheme
 import org.mozilla.fenix.theme.Theme
 
@@ -110,6 +111,7 @@ internal fun MozillaAccountMenuButton(
                         color = FirefoxTheme.colors.textSecondary,
                         maxLines = 1,
                         style = FirefoxTheme.typography.headline7,
+                        overflow = TextOverflow.Ellipsis,
                     )
                 }
 
