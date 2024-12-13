14349b13f258c2974f2bbba4d49d43be34f5712b
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-12-13 02:36:05 +0000

    Bug 1933272 - Update dav1d to f8d2620d82dc769833cf3e708b806ec35529b168 r=chunmin
    Differential Revision: https://phabricator.services.mozilla.com/D230147
    
    UltraBlame original commit: 8f75d1867bdb9f67fafb6f1d6b22417853e629c5

--- third_party/dav1d/include/dav1d/headers.h
+++ third_party/dav1d/include/dav1d/headers.h
@@ -187,14 +187,10 @@ typedef struct Dav1dContentLightLevel {
 } Dav1dContentLightLevel;
 
 typedef struct Dav1dMasteringDisplay {
-    
-    uint16_t primaries[3][2];
-    
-    uint16_t white_point[2];
-    
-    uint32_t max_luminance;
-    
-    uint32_t min_luminance;
+    uint16_t primaries[3][2]; 
+    uint16_t white_point[2]; 
+    uint32_t max_luminance; 
+    uint32_t min_luminance; 
 } Dav1dMasteringDisplay;
 
 typedef struct Dav1dITUTT35 {
