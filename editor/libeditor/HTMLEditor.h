6164eb29279769d72529bd00f5c72f1ea1f0c84c
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-12-13 03:21:04 +0000

    Backed out 6 changesets (bug 1923251, bug 1925635) for causing failures at test_editor.xhtml. CLOSED TREE
    Backed out changeset 9cef3db4f874 (bug 1925635)
    Backed out changeset 37e3129c7a6a (bug 1925635)
    Backed out changeset b9ee5a52e135 (bug 1925635)
    Backed out changeset 3433a2d6cbcd (bug 1925635)
    Backed out changeset 5c700bed542e (bug 1923251)
    Backed out changeset d11aeeb31a71 (bug 1923251)
    
    UltraBlame original commit: 47ba45802ce02c53a87b5df103afe118ab266c30

--- editor/libeditor/HTMLEditor.h
+++ editor/libeditor/HTMLEditor.h
@@ -1602,26 +1602,6 @@ class HTMLEditor final : public EditorBase,
 
 
 
-
-
-
-
-
-
-  [[nodiscard]] MOZ_CAN_RUN_SCRIPT Result<CreateElementResult, nsresult>
-  InsertPaddingBRElementIfNeeded(const EditorDOMPoint& aPoint,
-                                 nsIEditor::EStripWrappers aDeleteEmptyInlines,
-                                 const Element& aEditingHost);
-
-  [[nodiscard]] MOZ_CAN_RUN_SCRIPT Result<CaretPoint, nsresult>
-  DeleteRangesWithTransaction(nsIEditor::EDirection aDirectionAndAmount,
-                              nsIEditor::EStripWrappers aStripWrappers,
-                              const AutoRangeArray& aRangesToDelete) override;
-
-  
-
-
-
   [[nodiscard]] MOZ_CAN_RUN_SCRIPT nsresult
   MaybeInsertPaddingBRElementForEmptyLastLineAtSelection();
 
@@ -2096,10 +2076,8 @@ class HTMLEditor final : public EditorBase,
 
 
 
-
-  [[nodiscard]] MOZ_CAN_RUN_SCRIPT Result<CaretPoint, nsresult>
-  DeleteEmptyInclusiveAncestorInlineElements(nsIContent& aContent,
-                                             const Element& aEditingHost);
+  [[nodiscard]] MOZ_CAN_RUN_SCRIPT nsresult
+  RemoveEmptyInclusiveAncestorInlineElements(nsIContent& aContent);
 
   
 
@@ -2120,7 +2098,7 @@ class HTMLEditor final : public EditorBase,
       const EditorDOMPointInText& aStartToDelete,
       const EditorDOMPointInText& aEndToDelete,
       TreatEmptyTextNodes aTreatEmptyTextNodes,
-      DeleteDirection aDeleteDirection, const Element& aEditingHost);
+      DeleteDirection aDeleteDirection);
 
   
 
@@ -3298,24 +3276,6 @@ class HTMLEditor final : public EditorBase,
 
   MOZ_CAN_RUN_SCRIPT Result<EditorDOMPoint, nsresult> PrepareToInsertBRElement(
       const EditorDOMPoint& aPointToInsert);
-
-  
-
-
-
-
-
-
-
-
-
-
-
-
-  [[nodiscard]] MOZ_CAN_RUN_SCRIPT nsresult
-  EnsureNoFollowingUnnecessaryLineBreak(
-      const EditorDOMPoint& aNextOrAfterModifiedPoint,
-      const Element& aEditingHost);
 
   
 
@@ -4563,6 +4523,7 @@ class HTMLEditor final : public EditorBase,
   friend class CSSEditUtils;            
                                         
   friend class EditorBase;              
+                            
                             
                             
                             
