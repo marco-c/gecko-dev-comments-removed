371915fa1dbc10bc88025f7e2fd4279a62e0a880
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-12-13 02:20:54 +0000

    Bug 1933803 - Remove explicitly stored per-span data from nsGridContainerFrame::Tracks::ResolveIntrinsicSize r=dholbert
    Whether not this is actually useful to store separately is debatable.
    While we will need to iterate spans to recalculate this in step 3, we
    already need to iterate the tracks multiple times for step 3 so this
    doesn't affect the complexity of the operation and this iteration is
    relatively cheap.
    
    This data was stored in an auto-array. This means that we only allocate
    when the max span is large, but this means we either would do a larger
    allocation before, or do more iterations now. At least now, we don't
    allocate a big array for a single span, and the complexity of the
    iteration is linear in the dimensions of the number of total spans and
    the number of tracks in all spans combined, multiplied together. This
    is the same complexity of each call to GrowSizeForSpanningItems, and so
    is already the complexity of step 3 as a whole.
    
    Differential Revision: https://phabricator.services.mozilla.com/D230438
    
    UltraBlame original commit: 2abf05710e067377af61339dbc7d3a170c48bab0

--- layout/generic/nsGridContainerFrame.cpp
+++ layout/generic/nsGridContainerFrame.cpp
@@ -6637,13 +6637,6 @@ void nsGridContainerFrame::Tracks::ResolveIntrinsicSize(
   gfxContext* rc = &aState.mRenderingContext;
   WritingMode wm = aState.mWM;
 
-  
-  struct PerSpanData {
-    uint32_t mItemCountWithSameSpan = 0;
-    TrackSize::StateBits mStateBits = TrackSize::StateBits{0};
-  };
-  AutoTArray<PerSpanData, 16> perSpanData;
-
   nsTArray<SpanningItemData> spanningItems;
   uint32_t maxSpan = 0;  
 
@@ -6739,15 +6732,7 @@ void nsGridContainerFrame::Tracks::ResolveIntrinsicSize(
         gridItem.mState[mAxis] |= ItemState::eIsFlexing;
       } else if (state & (TrackSize::eIntrinsicMinSizing |
                           TrackSize::eIntrinsicMaxSizing)) {
-        
         maxSpan = std::max(maxSpan, span);
-        if (span >= perSpanData.Length()) {
-          perSpanData.SetLength(2 * span);
-        }
-
-        perSpanData[span].mItemCountWithSameSpan++;
-        perSpanData[span].mStateBits |= state;
-
         CachedIntrinsicSizes cache;
 
         
@@ -6792,6 +6777,7 @@ void nsGridContainerFrame::Tracks::ResolveIntrinsicSize(
                "clamping only applies to Automatic Minimum Size");
   }
 
+  MOZ_ASSERT(maxSpan != 1, "Should only count spans greater than 1");
   
   
   if (maxSpan) {
@@ -6824,10 +6810,19 @@ void nsGridContainerFrame::Tracks::ResolveIntrinsicSize(
     auto spanGroupStart = spanningItems.begin();
     auto spanGroupEnd = spanGroupStart;
     const auto end = spanningItems.end();
+
+    
+    
     for (; spanGroupStart != end; spanGroupStart = spanGroupEnd) {
       const uint32_t span = spanGroupStart->mSpan;
-      spanGroupEnd = spanGroupStart + perSpanData[span].mItemCountWithSameSpan;
-      TrackSize::StateBits stateBitsForSpan = perSpanData[span].mStateBits;
+      TrackSize::StateBits stateBitsForSpan{0};
+      MOZ_ASSERT(spanGroupEnd == spanGroupStart);
+      
+      
+      
+      do {
+        stateBitsForSpan |= StateBitsForRange(spanGroupEnd->mLineRange);
+      } while (++spanGroupEnd != end && spanGroupEnd->mSpan == span);
       bool updatedBase = false;  
       TrackSize::StateBits selector(TrackSize::eIntrinsicMinSizing);
       if (stateBitsForSpan & selector) {
