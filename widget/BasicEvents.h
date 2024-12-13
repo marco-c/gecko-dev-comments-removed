e3a4e5354d877e2f2624d91f01d4a8d7e604f6ad
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-12-13 02:23:56 +0000

    Bug 1680669 - part 3: Add a utility method to `WidgetEvent` to check whether the corresponding DOM event supports coordinates r=smaug
    Differential Revision: https://phabricator.services.mozilla.com/D222725
    
    UltraBlame original commit: aefcf10217ab75ebb5ae82cf4fef71a16db897fa

--- widget/BasicEvents.h
+++ widget/BasicEvents.h
@@ -742,6 +742,24 @@ class WidgetEvent : public WidgetEventTime {
   
 
 
+  [[nodiscard]] inline bool DOMEventSupportsCoords() const {
+    switch (mClass) {
+      case eMouseEventClass:
+      case eMouseScrollEventClass:
+      case eWheelEventClass:
+      case eTouchEventClass:
+      case eDragEventClass:
+      case ePointerEventClass:
+      case eSimpleGestureEventClass:
+        return true;
+      default:
+        return false;
+    }
+  }
+
+  
+
+
 
   
 
