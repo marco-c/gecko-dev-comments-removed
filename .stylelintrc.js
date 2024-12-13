da041f15623a75922ee2a1ee9a5ba736348836d7
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-12-13 03:29:30 +0000

    Bug 1837658 - enable Stylelint length-zero-no-unit r=frontend-codestyle-reviewers,desktop-theme-reviewers,mtigley,settings-reviewers,pip-reviewers,credential-management-reviewers,devtools-reviewers,tabbrowser-reviewers,translations-reviewers,android-reviewers,backup-reviewers,nchevobbe,dao,mconley,tthibaud
    This use of units for zero length occasionally gets flagged during theme reviews so it makes sense to establish our preference and automate checks via Stylelint.
    
    Differential Revision: https://phabricator.services.mozilla.com/D230287
    
    UltraBlame original commit: 391784aa5573743bea4ce604a839520df675886d

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -31,7 +31,6 @@ module.exports = {
   ],
   ignoreFiles,
   rules: {
-    "stylelint-plugin-mozilla/no-base-design-tokens": true,
     
     "color-no-invalid-hex": null,
     "font-family-no-missing-generic-family-keyword": [
@@ -54,6 +53,13 @@ module.exports = {
           "light-dark" ,
           "add" ,
         ],
+      },
+    ],
+
+    "length-zero-no-unit": [
+      true,
+      {
+        ignore: ["custom-properties"],
       },
     ],
 
@@ -261,6 +267,7 @@ module.exports = {
         ignorePseudoElements: ["slider-track", "slider-fill", "slider-thumb"],
       },
     ],
+    "stylelint-plugin-mozilla/no-base-design-tokens": true,
   },
 
   overrides: [
