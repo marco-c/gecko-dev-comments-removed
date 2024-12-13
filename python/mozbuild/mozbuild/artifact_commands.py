8b3af66a962ea36b20a24e0a3a65291af46eb7b3
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-12-13 03:44:49 +0000

    Bug 1935333 - Avoid trying to install dist toolchains when bootstrapping from a spidermonkey standalone tree. r=firefox-build-system-reviewers,taskgraph-reviewers,ahochheiden,jcristau
    The auto-bootstrap code gracefully handles the lack of information as to
    what toolchains are available in spidermonkey standalone trees, but the
    fallback code path for when toolchains are not to be unpacked doesn't
    handle the situation.
    
    By making the toolchains themselves define whether they are to be
    unpacked, we can remove the fallback code path and use auto-bootstrap in
    all cases.
    
    This makes `mach artifact toolchain --from-build` stop requiring a
    manual `--no-unpack`, but `--from-task` still needs it because it
    can't infer that information, as it doesn't have the task graph.
    
    Differential Revision: https://phabricator.services.mozilla.com/D231342
    
    UltraBlame original commit: b31e8879f3007fcf9d2c4b918f1c73930efa1786

--- python/mozbuild/mozbuild/artifact_commands.py
+++ python/mozbuild/mozbuild/artifact_commands.py
@@ -480,6 +480,7 @@ def artifact_toolchain(
             )
 
             record = ArtifactRecord(task_id, artifact_name)
+            record.unpack = task.attributes.get("toolchain-extract", True)
             records[record.filename] = record
 
     
