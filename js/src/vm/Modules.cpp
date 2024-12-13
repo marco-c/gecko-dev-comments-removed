9f11e690452829258cd91f9253e41a3d9453d439
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-12-13 02:12:51 +0000

    Bug 1929623 - The result of evaluating a JSON module should be |undefined| r=jandem
    Differential Revision: https://phabricator.services.mozilla.com/D228663
    
    UltraBlame original commit: 72c0f9e48dfb29cc2ac5aa1b870fbcbb5a3ea20e

--- js/src/vm/Modules.cpp
+++ js/src/vm/Modules.cpp
@@ -46,9 +46,9 @@ using mozilla::Utf8Unit;
 
 static bool ModuleLink(JSContext* cx, Handle<ModuleObject*> module);
 static bool ModuleEvaluate(JSContext* cx, Handle<ModuleObject*> module,
-                           MutableHandle<Value> result);
+                           MutableHandle<Value> rval);
 static bool SyntheticModuleEvaluate(JSContext* cx, Handle<ModuleObject*> module,
-                                    MutableHandle<Value> result);
+                                    MutableHandle<Value> rval);
 
 
 
@@ -1493,7 +1493,7 @@ static bool InnerModuleLinking(JSContext* cx, Handle<ModuleObject*> module,
 
 static bool SyntheticModuleEvaluate(JSContext* cx,
                                     Handle<ModuleObject*> moduleArg,
-                                    MutableHandle<Value> result) {
+                                    MutableHandle<Value> rval) {
   
 
   
@@ -1503,14 +1503,17 @@ static bool SyntheticModuleEvaluate(JSContext* cx,
   }
 
   
+  
 
   
-  if (!AsyncFunctionReturned(cx, resultPromise, result)) {
+
+  
+  if (!AsyncFunctionReturned(cx, resultPromise, JS::UndefinedHandleValue)) {
     return false;
   }
 
   
-  result.set(ObjectValue(*resultPromise));
+  rval.set(ObjectValue(*resultPromise));
   return true;
 }
 
