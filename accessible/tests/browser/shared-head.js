d9e95b1fd9153964d911dbaf20db1e1d752b4451
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-12-13 02:22:54 +0000

    Bug 1933631: Propagate the name of the task function passed to addAccessibleTask in browser tests. r=nlapre
    For a11y browser tests, we use addAccessibleTask:
    
    ```
    addAccessibleTask(`...`, async function testProtected(browser, docAcc) {
    ```
    
    Previously, the task function name (e.g. testProtected) didn't appear in test run output:
    
    `0:39.16 INFO Entering test bound`
    
    Even though the function is named, addAccessibleTask wraps it in an anonymous function, so the test harness can't get the original name.
    This patch overrides the name of the wrapper function to match the name of the supplied task function, resulting in test run output like this:
    
    `0:39.16 INFO Entering test bound testProtected`
    
    This should make it easier to determine what test you're dealing with when examining test run output.
    
    Differential Revision: https://phabricator.services.mozilla.com/D230355
    
    UltraBlame original commit: cf80c76b3107ed1c27c49b1fead7df14f4d54517

--- accessible/tests/browser/shared-head.js
+++ accessible/tests/browser/shared-head.js
@@ -463,7 +463,7 @@ const CacheDomain = {
 };
 
 function accessibleTask(doc, task, options = {}) {
-  return async function () {
+  const wrapped = async function () {
     let cacheDomains;
     if (!("cacheDomains" in options)) {
       cacheDomains = CacheDomain.All;
@@ -615,6 +615,16 @@ function accessibleTask(doc, task, options = {}) {
       await runPython(`__reset__`);
     }
   };
+  
+  
+  
+  
+  
+  
+  
+  
+  Object.defineProperty(wrapped, "name", { value: task.name });
+  return wrapped;
 }
 
 
