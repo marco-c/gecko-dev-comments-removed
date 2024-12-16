345da036552357bd68e5569c9cf6f87e6de6484a
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-12-16 00:59:00 +0000

    Bug 1936164 - Don't set HWND parent of popups. r=win-reviewers,rkraesig
    As per the docs[1]:
    
    > This parameter is optional for pop-up windows
    
    It _looks_ harmless, but when reparenting the widget (via
    swapFrameLoaders or so for DevTools), we keep that association, because
    DidChangeParent doesn't fix it up, which is what reorders our windows.
    
    It would seem that the right thing would be to make DidChangeParent also
    fix the association (so, remove the mWindowType == Popup check), but
    that doesn't work (makes the popup not show up), so just restore
    pre-regression behavior.
    
    [1]: https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-createwindowexa
    
    Differential Revision: https://phabricator.services.mozilla.com/D231708
    
    UltraBlame original commit: 855890e2cd16cf21ac6f740b83aa69ab2519b1b9

--- browser/components/places/tests/browser/browser_bookmark_popup.js
+++ browser/components/places/tests/browser/browser_bookmark_popup.js
@@ -562,7 +562,9 @@ add_task(async function enter_on_remove_bookmark_should_remove_bookmark() {
 });
 
 add_task(async function mouse_hovering_panel_should_prevent_autoclose() {
-  if (AppConstants.platform != "win") {
+  if (true) {
+    
+    
     
     
     return;
