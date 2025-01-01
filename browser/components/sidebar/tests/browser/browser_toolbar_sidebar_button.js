874a0792c8ff717b3147d4fc58aaf678a6f63d8c
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-01-01 01:09:03 +0000

    Backed out changeset f76adacaf89e (bug 1933592) for causing sidebar related bc failures
    UltraBlame original commit: 0462c32db584fc3573fb66a96c7b2114d0d0f9ad

--- browser/components/sidebar/tests/browser/browser_toolbar_sidebar_button.js
+++ browser/components/sidebar/tests/browser/browser_toolbar_sidebar_button.js
@@ -294,13 +294,6 @@ add_task(async function test_sidebar_button_runtime_pref_enabled() {
     CustomizableUI.AREA_NAVBAR,
     "The sidebar button is in the nav-bar"
   );
-
-  
-  
-  await SpecialPowers.pushPrefEnv({
-    set: [[SIDEBAR_VISIBILITY_PREF, "always-show"]],
-  });
-  button.click();
 });
 
 
