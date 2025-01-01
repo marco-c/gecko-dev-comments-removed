d1d208f95778c3acea791e8d2e7dba25caac5f8c
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-01-01 01:35:08 +0000

    Bug 1938432 - newtab sections personalization cleanup a few small things around telemetry and actions r=home-newtab-reviewers,npypchenko
    Differential Revision: https://phabricator.services.mozilla.com/D232664
    
    UltraBlame original commit: f67927cbd3ad7d591e1fb6ab268b7f4a59b279db

--- browser/components/newtab/data/content/activity-stream.bundle.js
+++ browser/components/newtab/data/content/activity-stream.bundle.js
@@ -113,6 +113,7 @@ for (const type of [
   "DELETE_FROM_POCKET",
   "DELETE_HISTORY_URL",
   "DIALOG_CANCEL",
+  "DIALOG_CLOSE",
   "DIALOG_OPEN",
   "DISABLE_SEARCH",
   "DISCOVERY_STREAM_COLLECTION_DISMISSIBLE_TOGGLE",
@@ -1798,6 +1799,11 @@ const LinkMenuOptions = {
               siteInfo
             )
           ),
+          
+          
+          actionCreators.AlsoToMain({
+            type: actionTypes.DIALOG_CLOSE,
+          }),
         ],
         eventSource,
         body_string_id: [
@@ -2035,12 +2041,7 @@ const LinkMenuOptions = {
       type: actionTypes.OPEN_ABOUT_FAKESPOT,
     }),
   }),
-  SectionBlock: ({
-    blockedSections,
-    sectionKey,
-    section,
-    sectionPosition,
-  }) => ({
+  SectionBlock: ({ blockedSections, sectionKey, sectionPosition }) => ({
     id: "newtab-menu-section-block",
     icon: "delete",
     action: {
@@ -2058,10 +2059,18 @@ const LinkMenuOptions = {
             },
           }),
           
+          actionCreators.OnlyToMain({
+            type: actionTypes.BLOCK_SECTION,
+            data: {
+              section: sectionKey,
+              section_position: sectionPosition,
+              event_source: "CONTEXT_MENU",
+            },
+          }),
+          
           
           actionCreators.AlsoToMain({
-            type: actionTypes.BLOCK_SECTION,
-            data: null,
+            type: actionTypes.DIALOG_CLOSE,
           }),
         ],
         
@@ -2075,24 +2084,10 @@ const LinkMenuOptions = {
       },
     },
     userEvent: "DIALOG_OPEN",
-    
-    impression: actionCreators.OnlyToMain({
-      type: actionTypes.BLOCK_SECTION,
-      data: {
-        section,
-        section_position: sectionPosition,
-        event_source: "CONTEXT_MENU",
-      },
-    }),
   }),
-  SectionUnfollow: ({
-    followedSections,
-    section,
-    sectionKey,
-    sectionPosition,
-  }) => ({
+  SectionUnfollow: ({ followedSections, sectionKey, sectionPosition }) => ({
     id: "newtab-menu-section-unfollow",
-    action: actionCreators.OnlyToMain({
+    action: actionCreators.AlsoToMain({
       type: actionTypes.SET_PREF,
       data: {
         name: "discoverystream.sections.following",
@@ -2104,7 +2099,7 @@ const LinkMenuOptions = {
     impression: actionCreators.OnlyToMain({
       type: actionTypes.UNFOLLOW_SECTION,
       data: {
-        section,
+        section: sectionKey,
         section_position: sectionPosition,
         event_source: "CONTEXT_MENU",
       },
@@ -6975,9 +6970,7 @@ function Dialog(prevState = INITIAL_STATE.Dialog, action) {
       return Object.assign({}, prevState, { visible: true, data: action.data });
     case actionTypes.DIALOG_CANCEL:
       return Object.assign({}, prevState, { visible: false });
-    case actionTypes.DELETE_HISTORY_URL:
-      return Object.assign({}, INITIAL_STATE.Dialog);
-    case actionTypes.BLOCK_SECTION:
+    case actionTypes.DIALOG_CLOSE:
       
       return Object.assign({}, INITIAL_STATE.Dialog);
     default:
@@ -9980,7 +9973,6 @@ function SectionContextMenu({
   following,
   followedSections,
   blockedSections,
-  section,
   sectionPosition
 }) {
   
@@ -10014,7 +10006,6 @@ function SectionContextMenu({
     site: {
       followedSections,
       blockedSections,
-      section,
       sectionKey,
       sectionPosition
     }
@@ -10192,7 +10183,6 @@ function CardSection({
     sectionKey: sectionKey,
     title: title,
     type: type,
-    section: sectionKey,
     sectionPosition: sectionPosition
   }));
   return external_React_default().createElement("section", {
