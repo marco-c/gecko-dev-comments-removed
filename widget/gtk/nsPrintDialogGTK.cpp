1a4be522fd5a8105a5d49f36e87ebcfa1ccef613
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-01-01 01:32:04 +0000

    Bug 1919414 - Don't use Gecko's pages per sheet code in the native print dialog. r=dholbert
    Differential Revision: https://phabricator.services.mozilla.com/D232628
    
    UltraBlame original commit: 32adfbef0de10fc228653647a0ac24b8dfa8f6c3

--- widget/gtk/nsPrintDialogGTK.cpp
+++ widget/gtk/nsPrintDialogGTK.cpp
@@ -390,7 +390,7 @@ nsresult nsPrintDialogWidgetGTK::ImportSettings(nsIPrintSettings* aNSSettings) {
                                aNSSettings->GetPrintBGImages());
 
   
-  int32_t pagesPerSide;
+  int32_t pagesPerSide = 1;
   aNSSettings->GetNumPagesPerSheet(&pagesPerSide);
   gtk_print_settings_set_number_up(settings, pagesPerSide);
 
@@ -431,12 +431,7 @@ nsresult nsPrintDialogWidgetGTK::ExportSettings(nsIPrintSettings* aNSSettings) {
         GTK_TOGGLE_BUTTON(print_bg_images_toggle)));
 
     
-    
-    
-    int32_t pagesPerSide = gtk_print_settings_get_number_up(settings);
-    gtk_print_settings_set_number_up(settings, 1);
-    aNSSettings->SetNumPagesPerSheet(pagesPerSide);
-
+    aNSSettings->SetNumPagesPerSheet(1);
     
     nsCOMPtr<nsPrintSettingsGTK> aNSSettingsGTK(do_QueryInterface(aNSSettings));
     if (aNSSettingsGTK) {
