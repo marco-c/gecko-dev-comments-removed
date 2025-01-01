e07aa5058c1606e0629975005c39e3311646588e
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-01-01 01:27:04 +0000

    Bug 1658072 - Rewrite the Unix implementation of IPC process termination handling. r=nika
    This patch rewrites the Unix backend of ProcessWatcher for two reasons:
    
    1. To remove the use of libevent's signal handling, which has concurrency
       bugs that can't be easily fixed upstream (see Bugzilla for details)
    
    2. To simplify the code in general; in particular, the new version has one
       place where the process and its exit status are consumed from the OS
    
    The new implementation uses the same pipe-to-self technique as libevent
    (and which we use elsewhere) to deal with async signal safety.  Unlike
    the previous version, there is a single object which manages all
    monitored child processes rather than one each.  (Previously, this
    multiplexing was done inside libevent.)
    
    Differential Revision: https://phabricator.services.mozilla.com/D141309
    
    UltraBlame original commit: 35b64d418961f8227e6f5cf6a76757bca69e2775

--- ipc/chromium/src/chrome/common/process_watcher_posix_sigchld.cc
+++ ipc/chromium/src/chrome/common/process_watcher_posix_sigchld.cc
@@ -5,18 +5,50 @@
 
 
 #include <errno.h>
+#include <fcntl.h>
+#include <mutex>
 #include <signal.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
-#include <time.h>
 
 #include "base/eintr_wrapper.h"
+#include "base/logging.h"
 #include "base/message_loop.h"
 #include "base/process_util.h"
+#include "mozilla/DataMutex.h"
+#include "mozilla/StaticPtr.h"
+#include "nsITimer.h"
+#include "nsTArray.h"
+#include "nsThreadUtils.h"
+#include "nsXULAppAPI.h"
 #include "prenv.h"
 
 #include "chrome/common/process_watcher.h"
+
+#ifdef MOZ_ENABLE_FORKSERVER
+#  include "mozilla/ipc/ForkServiceChild.h"
+#endif
+
+#if !defined(XP_MACOSX)
+
+#  define HAVE_PIPE2 1
+#endif
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -46,19 +78,37 @@ using base::BlockingWait;
 
 
 
-static bool IsProcessDead(pid_t pid, bool blocking = false) {
-  int info = 0;
-  auto blockFlag = blocking ? BlockingWait::Yes : BlockingWait::No;
+struct PendingChild {
+  pid_t mPid;
+  nsCOMPtr<nsITimer> mForce;
+};
 
-  auto status = WaitForProcess(pid, blockFlag, &info);
-  while (blockFlag == BlockingWait::Yes &&
+
+
+
+
+static mozilla::StaticDataMutex<mozilla::StaticAutoPtr<nsTArray<PendingChild>>>
+    gPendingChildren("ProcessWatcher::gPendingChildren");
+static int gSignalPipe[2] = {-1, -1};
+
+
+
+
+
+
+
+static bool IsProcessDead(pid_t pid, BlockingWait aBlock) {
+  int info = 0;
+
+  auto status = WaitForProcess(pid, aBlock, &info);
+  while (aBlock == BlockingWait::Yes &&
          status == base::ProcessStatus::Running) {
     
     
     
     
     sleep(1);
-    status = WaitForProcess(pid, blockFlag, &info);
+    status = WaitForProcess(pid, aBlock, &info);
   }
 
   switch (status) {
@@ -89,175 +139,230 @@ static bool IsProcessDead(pid_t pid, bool blocking = false) {
   }
 }
 
-class ChildReaper : public base::MessagePumpLibevent::SignalEvent,
-                    public base::MessagePumpLibevent::SignalWatcher {
+
+
+
+
+already_AddRefed<nsITimer> DelayedKill(pid_t aPid) {
+  nsCOMPtr<nsITimer> timer;
+
+  nsresult rv = NS_NewTimerWithCallback(
+      getter_AddRefs(timer),
+      [aPid](nsITimer*) {
+        if (kill(aPid, SIGKILL) != 0) {
+          CHROMIUM_LOG(ERROR) << "failed to send SIGKILL to process " << aPid;
+        }
+      },
+      kMaxWaitMs, nsITimer::TYPE_ONE_SHOT, "ProcessWatcher::DelayedKill",
+      XRE_GetAsyncIOEventTarget());
+
+  
+  
+  if (NS_FAILED(rv)) {
+    CHROMIUM_LOG(WARNING) << "failed to start kill timer for process " << aPid
+                          << "; killing immediately";
+    kill(aPid, SIGKILL);
+    return nullptr;
+  }
+
+  return timer.forget();
+}
+
+bool CrashProcessIfHanging(pid_t aPid) {
+  if (IsProcessDead(aPid, BlockingWait::No)) {
+    return false;
+  }
+
+  
+  
+  
+  
+  
+  
+  static int sWaitMs = kShutdownWaitMs;
+  if (sWaitMs > 0) {
+    CHROMIUM_LOG(WARNING)
+        << "Process " << aPid
+        << " may be hanging at shutdown; will wait for up to " << sWaitMs
+        << "ms";
+  }
+  
+  
+  
+  while (sWaitMs > 0) {
+    static constexpr int kWaitTickMs = 200;
+    struct timespec ts = {kWaitTickMs / 1000, (kWaitTickMs % 1000) * 1000000};
+    HANDLE_EINTR(nanosleep(&ts, &ts));
+    sWaitMs -= kWaitTickMs;
+
+    if (IsProcessDead(aPid, BlockingWait::No)) {
+      return false;
+    }
+  }
+
+  
+  
+  
+  CHROMIUM_LOG(ERROR)
+      << "Process " << aPid
+      << " hanging at shutdown; attempting crash report (fatal error).";
+
+  kill(aPid, SIGABRT);
+  return true;
+}
+
+
+
+
+
+class ProcessCleaner final : public MessageLoopForIO::Watcher,
+                             public MessageLoop::DestructionObserver {
  public:
-  explicit ChildReaper(pid_t process) : process_(process) {}
-
-  virtual ~ChildReaper() {
-    
-    DCHECK(!process_);
-
-    
+  
+  void Register() {
+    MessageLoopForIO* loop = MessageLoopForIO::current();
+    loop->AddDestructionObserver(this);
+    loop->WatchFileDescriptor(gSignalPipe[0],  true,
+                              MessageLoopForIO::WATCH_READ, &mWatcher, this);
   }
 
-  virtual void OnSignal(int sig) override {
-    DCHECK(SIGCHLD == sig);
-    DCHECK(process_);
-
+  void OnFileCanReadWithoutBlocking(int fd) override {
+    DCHECK(fd == gSignalPipe[0]);
+    ssize_t rv;
     
-    if (IsProcessDead(process_)) {
-      process_ = 0;
-      StopCatching();
+    do {
+      char msg[32];
+      rv = HANDLE_EINTR(read(gSignalPipe[0], msg, sizeof msg));
+      CHECK(rv != 0);
+      if (rv < 0) {
+        DCHECK(errno == EAGAIN || errno == EWOULDBLOCK);
+      } else {
+#ifdef DEBUG
+        for (size_t i = 0; i < (size_t)rv; ++i) {
+          DCHECK(msg[i] == 0);
+        }
+#endif
+      }
+    } while (rv > 0);
+    PruneDeadProcesses();
+  }
+
+  void OnFileCanWriteWithoutBlocking(int fd) override {
+    CHROMIUM_LOG(FATAL) << "unreachable";
+  }
+
+  void WillDestroyCurrentMessageLoop() override {
+    mWatcher.StopWatchingFileDescriptor();
+    auto lock = gPendingChildren.Lock();
+    auto& children = lock.ref();
+    if (children) {
+      for (const auto& child : *children) {
+        
+        if (child.mForce) {
+          
+          
+          
+          
+          
+          if (kill(child.mPid, SIGKILL) != 0) {
+            CHROMIUM_LOG(ERROR)
+                << "failed to send SIGKILL to process " << child.mPid;
+            continue;
+          }
+        } else {
+          
+          
+          if (!PR_GetEnv("MOZ_TEST_CHILD_EXIT_HANG") &&
+              !CrashProcessIfHanging(child.mPid)) {
+            continue;
+          }
+        }
+        
+        
+        IsProcessDead(child.mPid, BlockingWait::Yes);
+      }
+      children = nullptr;
     }
-  }
-
- protected:
-  void WaitForChildExit() {
-    CHECK(process_);
-    IsProcessDead(process_, true);
-  }
-
-  pid_t process_;
-
- private:
-  ChildReaper(const ChildReaper&) = delete;
-
-  const ChildReaper& operator=(const ChildReaper&) = delete;
-};
-
-
-class ChildGrimReaper : public ChildReaper, public mozilla::Runnable {
- public:
-  explicit ChildGrimReaper(pid_t process)
-      : ChildReaper(process), mozilla::Runnable("ChildGrimReaper") {}
-
-  virtual ~ChildGrimReaper() {
-    if (process_) KillProcess();
-  }
-
-  NS_IMETHOD Run() override {
-    
-    if (process_) KillProcess();
-
-    return NS_OK;
-  }
-
- private:
-  void KillProcess() {
-    DCHECK(process_);
-
-    if (IsProcessDead(process_)) {
-      process_ = 0;
-      return;
-    }
-
-    if (0 == kill(process_, SIGKILL)) {
-      
-      
-      
-      WaitForChildExit();
-    } else {
-      CHROMIUM_LOG(ERROR) << "Failed to deliver SIGKILL to " << process_ << "!"
-                          << "(" << errno << ").";
-    }
-    process_ = 0;
-  }
-
-  ChildGrimReaper(const ChildGrimReaper&) = delete;
-
-  const ChildGrimReaper& operator=(const ChildGrimReaper&) = delete;
-};
-
-class ChildLaxReaper : public ChildReaper,
-                       public MessageLoop::DestructionObserver {
- public:
-  explicit ChildLaxReaper(pid_t process) : ChildReaper(process) {}
-
-  virtual ~ChildLaxReaper() {
-    
-    DCHECK(!process_);
-  }
-
-  virtual void OnSignal(int sig) override {
-    ChildReaper::OnSignal(sig);
-
-    if (!process_) {
-      MessageLoop::current()->RemoveDestructionObserver(this);
-      delete this;
-    }
-  }
-
-  virtual void WillDestroyCurrentMessageLoop() override {
-    DCHECK(process_);
-    if (!process_) {
-      return;
-    }
-
-    
-    if (!PR_GetEnv("MOZ_TEST_CHILD_EXIT_HANG")) {
-      CrashProcessIfHanging();
-    }
-    if (process_) {
-      WaitForChildExit();
-      process_ = 0;
-    }
-
-    
-    
-    MessageLoop::current()->RemoveDestructionObserver(this);
     delete this;
   }
 
  private:
-  ChildLaxReaper(const ChildLaxReaper&) = delete;
+  MessageLoopForIO::FileDescriptorWatcher mWatcher;
 
-  void CrashProcessIfHanging() {
-    if (IsProcessDead(process_)) {
-      process_ = 0;
+  static void PruneDeadProcesses() {
+    auto lock = gPendingChildren.Lock();
+    auto& children = lock.ref();
+    if (!children || children->IsEmpty()) {
       return;
     }
-
-    
-    
-    
-    
-    
-    
-    static int sWaitMs = kShutdownWaitMs;
-    if (sWaitMs > 0) {
-      CHROMIUM_LOG(WARNING)
-          << "Process " << process_
-          << " may be hanging at shutdown; will wait for up to " << sWaitMs
-          << "ms";
-    }
-    
-    
-    
-    while (sWaitMs > 0) {
-      static constexpr int kWaitTickMs = 200;
-      struct timespec ts = {kWaitTickMs / 1000, (kWaitTickMs % 1000) * 1000000};
-      HANDLE_EINTR(nanosleep(&ts, &ts));
-      sWaitMs -= kWaitTickMs;
-
-      if (IsProcessDead(process_)) {
-        process_ = 0;
-        return;
+    nsTArray<PendingChild> live;
+    for (const auto& child : *children) {
+      if (IsProcessDead(child.mPid, BlockingWait::No)) {
+        if (child.mForce) {
+          child.mForce->Cancel();
+        }
+      } else {
+        live.AppendElement(child);
       }
     }
-
-    
-    
-    
-    CHROMIUM_LOG(ERROR)
-        << "Process " << process_
-        << " hanging at shutdown; attempting crash report (fatal error).";
-
-    kill(process_, SIGABRT);
+    *children = std::move(live);
   }
-
-  const ChildLaxReaper& operator=(const ChildLaxReaper&) = delete;
 };
+
+static void HandleSigChld(int signum) {
+  DCHECK(signum == SIGCHLD);
+  char msg = 0;
+  HANDLE_EINTR(write(gSignalPipe[1], &msg, 1));
+  
+  
+  
+  
+  
+  
+  
+}
+
+static void ProcessWatcherInit() {
+  int rv;
+
+#ifdef HAVE_PIPE2
+  rv = pipe2(gSignalPipe, O_NONBLOCK | O_CLOEXEC);
+  CHECK(rv == 0)
+  << "pipe2() failed";
+#else
+  rv = pipe(gSignalPipe);
+  CHECK(rv == 0)
+  << "pipe() failed";
+  for (int fd : gSignalPipe) {
+    rv = fcntl(fd, F_SETFL, O_NONBLOCK);
+    CHECK(rv == 0)
+    << "O_NONBLOCK failed";
+    rv = fcntl(fd, F_SETFD, FD_CLOEXEC);
+    CHECK(rv == 0)
+    << "FD_CLOEXEC failed";
+  }
+#endif  
+
+  
+  
+  
+  
+  auto oldHandler = signal(SIGCHLD, HandleSigChld);
+  CHECK(oldHandler != SIG_ERR);
+  DCHECK(oldHandler == SIG_DFL);
+
+  
+  
+  
+  
+  
+  
+  XRE_GetAsyncIOEventTarget()->Dispatch(
+      NS_NewRunnableFunction("ProcessCleaner::Register", [] {
+        ProcessCleaner* pc = new ProcessCleaner();
+        pc->Register();
+      }));
+}
 
 }  
 
@@ -283,20 +388,69 @@ void ProcessWatcher::EnsureProcessTerminated(base::ProcessHandle process,
   DCHECK(process != base::GetCurrentProcId());
   DCHECK(process > 0);
 
-  if (IsProcessDead(process)) return;
+  static std::once_flag sInited;
+  std::call_once(sInited, ProcessWatcherInit);
 
-  MessageLoopForIO* loop = MessageLoopForIO::current();
-  if (force) {
-    RefPtr<ChildGrimReaper> reaper = new ChildGrimReaper(process);
+  auto lock = gPendingChildren.Lock();
+  auto& children = lock.ref();
 
-    loop->CatchSignal(SIGCHLD, reaper, reaper);
-    
-    loop->PostDelayedTask(reaper.forget(), kMaxWaitMs);
-  } else {
-    ChildLaxReaper* reaper = new ChildLaxReaper(process);
-
-    loop->CatchSignal(SIGCHLD, reaper, reaper);
-    
-    loop->AddDestructionObserver(reaper);
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  if (IsProcessDead(process, BlockingWait::No)) {
+    return;
   }
+
+  if (!children) {
+    children = new nsTArray<PendingChild>();
+  }
+  
+  
+  
+  
+  
+  for (const auto& child : *children) {
+    if (child.mPid == process) {
+#ifdef MOZ_ENABLE_FORKSERVER
+      if (mozilla::ipc::ForkServiceChild::WasUsed()) {
+        
+        
+        
+        
+
+        CHROMIUM_LOG(WARNING) << "EnsureProcessTerminated: duplicate process"
+                                 " ID "
+                              << process
+                              << "; assuming this is because of the fork"
+                                 " server.";
+
+        
+        
+        
+        
+        
+        
+        return;
+      }
+#endif
+      MOZ_ASSERT(false,
+                 "EnsureProcessTerminated must be called at most once for a "
+                 "given process");
+      return;
+    }
+  }
+
+  PendingChild child{};
+  child.mPid = process;
+  if (force) {
+    child.mForce = DelayedKill(process);
+  }
+  children->AppendElement(std::move(child));
 }
