56197e3572f3b49306a82f382c557c7cc08b872e
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-01-01 01:50:48 +0000

    Bug 1935253 - Ensure we don't present old frames with requestVideoFrameCallback. r=media-playback-reviewers,azebrowski
    This could happen if somehow the vsync went backwards (unlikely) or if
    we somehow presented the cached first frame again in
    FirstFrameVideoOutput. This can only happen if a video finishes playing
    or is stopped, and then we hit play again. If the decoding pipeline was
    flushed, then the only frame we have is the first frame again, so we
    display that as a placeholder while we redecode.
    
    It should be safe to simply ignore these frames for these edge cases and
    wait for the next frame to be presented (which might be the first frame
    again since we don't cache the audio sample).
    
    Differential Revision: https://phabricator.services.mozilla.com/D232757
    
    UltraBlame original commit: c7987f3dee25fc9415eb4cdc56849df6d2cc6e6c

--- dom/html/HTMLVideoElement.cpp
+++ dom/html/HTMLVideoElement.cpp
@@ -744,7 +744,7 @@ void HTMLVideoElement::TakeVideoFrameRequestCallbacks(
   
   
   if (!selected || selected->mFrameID == layers::kContainerFrameID_Invalid ||
-      selected->mFrameID == mLastPresentedFrameID) {
+      selected->mFrameID <= mLastPresentedFrameID) {
     return;
   }
 
