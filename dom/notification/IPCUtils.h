ef1545cccbdec9120b6568ea2a3f11e3d8a564c9
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-01-01 01:13:58 +0000

    Bug 1893983 - Remove mozbehavior r=asuth
    mozbehavior is B2G-era API that we don't use anymore.
    
    Differential Revision: https://phabricator.services.mozilla.com/D232118
    
    UltraBlame original commit: a53c095af6ca6b02d803c0f20e3e79468875b301

--- dom/notification/IPCUtils.h
+++ dom/notification/IPCUtils.h
@@ -20,10 +20,6 @@ struct ParamTraits<NotificationDirection>
                                                NotificationDirection::Auto,
                                                NotificationDirection::Rtl> {};
 
-DEFINE_IPC_SERIALIZER_WITH_FIELDS(mozilla::dom::NotificationBehavior, mNoclear,
-                                  mNoscreen, mShowOnlyOnce, mSoundFile,
-                                  mVibrationPattern);
-
 }  
 
 #endif  
