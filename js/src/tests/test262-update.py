cb481c0f12ae85ae1082a8a5761b146f7a811cd8
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-01-01 01:52:55 +0000

    Bug 1938117 - Part 1: Update test262 import script. r=spidermonkey-reviewers,dminor
    Changes:
    - Handle new feature tags.
    - Remove duplicate line to include "temporalHelpers.js".
    - Handle "sm/non262.js" harness file, which we can't import as-is, because it
      overwrites our own test harness with stub functions.
    
    Differential Revision: https://phabricator.services.mozilla.com/D232511
    
    UltraBlame original commit: 76e9e96a14cfe6e210bb86f9d24c4206fc55fc95

--- js/src/tests/test262-update.py
+++ js/src/tests/test262-update.py
@@ -22,10 +22,10 @@ UNSUPPORTED_FEATURES = set(
         "Intl.Locale-info",  
         "Atomics.waitAsync",  
         "legacy-regexp",  
-        "set-methods",  
         "source-phase-imports",
         "source-phase-imports-module-source",
         "Math.sumPrecise",
+        "import-defer",
     ]
 )
 FEATURE_CHECK_NEEDED = {
@@ -46,6 +46,8 @@ FEATURE_CHECK_NEEDED = {
     "promise-try": "!Promise.try",
     "explicit-resource-management": "!(this.hasOwnProperty('getBuildConfiguration')&&getBuildConfiguration('explicit-resource-management'))",  
     "Atomics.pause": "!this.hasOwnProperty('Atomics')||!Atomics.pause",
+    "Error.isError": "!Error.isError",
+    "iterator-sequencing": "!Iterator.concat",
 }
 RELEASE_OR_BETA = set(
     [
@@ -69,6 +71,8 @@ SHELL_OPTIONS = {
     "explicit-resource-management": "--enable-explicit-resource-management",
     "Atomics.pause": "--enable-atomics-pause",
     "Temporal": "--enable-temporal",
+    "Error.isError": "--enable-error-iserror",
+    "iterator-sequencing": "--enable-iterator-sequencing",
 }
 
 INCLUDE_FEATURE_DETECTED_OPTIONAL_SHELL_OPTIONS = {
@@ -541,7 +545,10 @@ def process_test262(test262Dir, test262OutDir, strictTests, externManifests):
     explicitIncludes[os.path.join("built-ins", "TypedArrays")] = [
         "detachArrayBuffer.js"
     ]
-    explicitIncludes[os.path.join("built-ins", "Temporal")] = ["temporalHelpers.js"]
+
+    
+    
+    localIncludesMap[os.path.join("staging", "sm")] = ["test262-non262.js"]
 
     
     for dirPath, dirNames, fileNames in os.walk(testDir):
@@ -601,6 +608,10 @@ def process_test262(test262Dir, test262OutDir, strictTests, externManifests):
                             "reftest": externRefTest,
                         }
                     )
+
+        
+        
+        includeSet.discard("sm/non262.js")
 
         
         writeShellAndBrowserFiles(
