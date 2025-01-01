eab09c106a4a8903748765a2c958c8c5e833425a
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-01-01 01:16:50 +0000

    Bug 1938054 - Add a zero width space to tab group labels so we always create a text node and get consistent layout even if the group is unnamed. r=jswinarton,tabbrowser-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D232453
    
    UltraBlame original commit: b4a55fa1a5fdceef92e52e561dd13c2f66e00f65

--- browser/components/tabbrowser/content/tabgroup.js
+++ browser/components/tabbrowser/content/tabgroup.js
@@ -16,8 +16,12 @@
       `;
 
     
+    #label;
+
+    
     #labelElement;
 
+    
     #colorCode;
 
     constructor() {
@@ -44,8 +48,8 @@
       this.#labelElement = this.querySelector(".tab-group-label");
       this.#labelElement.addEventListener("click", this);
 
-      this.#updateLabelAriaAttributes(this.label);
-      this.#updateCollapsedAriaAttributes(this.collapsed);
+      this.#updateLabelAriaAttributes();
+      this.#updateCollapsedAriaAttributes();
 
       this.createdDate = Date.now();
 
@@ -121,12 +125,17 @@
     }
 
     get label() {
-      return this.getAttribute("label");
+      return this.#label;
     }
 
     set label(val) {
-      this.setAttribute("label", val);
-      this.#updateLabelAriaAttributes(val);
+      this.#label = val;
+
+      
+      
+      this.setAttribute("label", "\u200b" + val);
+
+      this.#updateLabelAriaAttributes();
     }
 
     get collapsed() {
@@ -138,26 +147,20 @@
         return;
       }
       this.toggleAttribute("collapsed", val);
-      this.#updateCollapsedAriaAttributes(val);
+      this.#updateCollapsedAriaAttributes();
       const eventName = val ? "TabGroupCollapse" : "TabGroupExpand";
       this.dispatchEvent(new CustomEvent(eventName, { bubbles: true }));
     }
 
-    
-
-
-    #updateLabelAriaAttributes(label) {
-      const ariaLabel = label == "" ? "unnamed" : label;
+    #updateLabelAriaAttributes() {
+      const ariaLabel = this.#label || "unnamed";
       const ariaDescription = `${ariaLabel} tab group`;
       this.#labelElement?.setAttribute("aria-label", ariaLabel);
       this.#labelElement?.setAttribute("aria-description", ariaDescription);
     }
 
-    
-
-
-    #updateCollapsedAriaAttributes(collapsed) {
-      const ariaExpanded = collapsed ? "false" : "true";
+    #updateCollapsedAriaAttributes() {
+      const ariaExpanded = this.collapsed ? "false" : "true";
       this.#labelElement?.setAttribute("aria-expanded", ariaExpanded);
     }
 
