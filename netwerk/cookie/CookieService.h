e59341c44dec86907bbf019858efd417b5f650ad
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-01-16 01:57:34 +0000

    Bug 1915355 - Part 4: Implement the third-party cookie blocking excpetion in CookieService. r=cookie-reviewers,baku,valentin
    The patch integrates the third-party cookie blocking excpetion in
    CookieService.
    
    Depends on D223381
    
    Differential Revision: https://phabricator.services.mozilla.com/D223383
    
    UltraBlame original commit: 336214ad789156999085eb7607fe6a6cbf7bbfe9

--- netwerk/cookie/CookieService.h
+++ netwerk/cookie/CookieService.h
@@ -13,10 +13,12 @@
 
 #include "Cookie.h"
 #include "CookieCommons.h"
+#include "ThirdPartyCookieBlockingExceptions.h"
 
 #include "nsString.h"
 #include "nsIMemoryReporter.h"
 #include "mozilla/MemoryReporting.h"
+#include "mozilla/MozPromise.h"
 
 class nsIConsoleReportCollector;
 class nsICookieJarSettings;
@@ -57,6 +59,12 @@ class CookieService final : public nsICookieService,
   CookieService();
   static already_AddRefed<nsICookieService> GetXPCOMSingleton();
   nsresult Init();
+
+  static void Update3PCBExceptionInfo(nsIChannel* aChannel);
+
+  ThirdPartyCookieBlockingExceptions& ThirdPartyCookieBlockingExceptionsRef() {
+    return mThirdPartyCookieBlockingExceptions;
+  }
 
   
 
@@ -125,6 +133,8 @@ class CookieService final : public nsICookieService,
   
   nsCOMPtr<mozIThirdPartyUtil> mThirdPartyUtil;
   nsCOMPtr<nsIEffectiveTLDService> mTLDService;
+
+  ThirdPartyCookieBlockingExceptions mThirdPartyCookieBlockingExceptions;
 
   
   
