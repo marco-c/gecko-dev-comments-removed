95e4f64fb4f53d2ee6162234c43c849ba9628858
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-01-16 02:40:50 +0000

    Bug 1855144 -  Use plain array to store statically initialized hash map in dom/html/MediaError.cpp r=smaug
    Differential Revision: https://phabricator.services.mozilla.com/D189200
    
    UltraBlame original commit: 750d66437a0f0a8bb195ac10c8d0be54238bcaa4

--- dom/html/MediaError.cpp
+++ dom/html/MediaError.cpp
@@ -6,9 +6,6 @@
 
 #include "mozilla/dom/MediaError.h"
 
-#include <string>
-#include <unordered_set>
-
 #include "mozilla/dom/Document.h"
 #include "mozilla/dom/MediaErrorBinding.h"
 #include "nsContentUtils.h"
@@ -34,13 +31,13 @@ MediaError::MediaError(HTMLMediaElement* aParent, uint16_t aCode,
 void MediaError::GetMessage(nsAString& aResult) const {
   
   
-  
-  static const std::unordered_set<std::string> whitelist = {
-      "404: Not Found"
+  static constexpr nsLiteralCString whitelist[] = {
+      "404: Not Found"_ns
       
   };
 
-  const bool shouldBlank = whitelist.find(mMessage.get()) == whitelist.end();
+  const bool shouldBlank = std::find(std::begin(whitelist), std::end(whitelist),
+                                     mMessage) == std::end(whitelist);
 
   if (shouldBlank) {
     
