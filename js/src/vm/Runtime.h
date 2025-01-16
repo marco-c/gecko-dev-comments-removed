60bcdf265b7b9b5ff0ee192c651f6fbd7814da86
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-01-16 01:31:38 +0000

    Bug 1935289: Add prefs r=jandem
    Differential Revision: https://phabricator.services.mozilla.com/D232134
    
    UltraBlame original commit: 92f7637737c1ef3fae7d125fcbdf4c4922b1d7c0

--- js/src/vm/Runtime.h
+++ js/src/vm/Runtime.h
@@ -959,6 +959,8 @@ struct JSRuntime {
  private:
   
   mozilla::Atomic<bool, mozilla::SequentiallyConsistent>
+      offthreadBaselineCompilationEnabled_;
+  mozilla::Atomic<bool, mozilla::SequentiallyConsistent>
       offthreadIonCompilationEnabled_;
   mozilla::Atomic<bool, mozilla::SequentiallyConsistent>
       parallelParsingEnabled_;
@@ -968,8 +970,18 @@ struct JSRuntime {
  public:
   
   
+  void setOffthreadBaselineCompilationEnabled(bool value) {
+    offthreadBaselineCompilationEnabled_ = value;
+  }
+  bool canUseOffthreadBaselineCompilation() const {
+    return offthreadBaselineCompilationEnabled_;
+  }
   void setOffthreadIonCompilationEnabled(bool value) {
     offthreadIonCompilationEnabled_ = value;
+  }
+  void setOffthreadCompilationEnabled(bool value) {
+    setOffthreadBaselineCompilationEnabled(value);
+    setOffthreadIonCompilationEnabled(value);
   }
   bool canUseOffthreadIonCompilation() const {
     return offthreadIonCompilationEnabled_;
