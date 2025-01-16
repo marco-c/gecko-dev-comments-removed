a2b9f1348b7ddd4e0eef869c120cc723669ca51e
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-01-16 02:17:29 +0000

    Backed out changeset f594e6f00208 (bug 1940883) for causing crashes in bug 1941164.
    UltraBlame original commit: abc92a41910764a2dc98aea04074bc746fa2c194

--- dom/media/MediaFormatReader.cpp
+++ dom/media/MediaFormatReader.cpp
@@ -2477,10 +2477,8 @@ void MediaFormatReader::Update(TrackType aTrack) {
       
       
       
-      if (UpdateReceivedNewData(aTrack) || decoder.HasInternalSeekPending()) {
-        LOGV("%s: Nothing more to do", decoder.HasInternalSeekWaiting()
-                                           ? "Waiting during internal seek"
-                                           : "Completed drain");
+      if (UpdateReceivedNewData(aTrack) || decoder.mSeekRequest.Exists()) {
+        LOGV("Nothing more to do");
         return;
       }
     } else if (decoder.mDemuxEOS && !decoder.HasPendingDrain() &&
