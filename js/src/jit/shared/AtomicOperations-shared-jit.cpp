51d1b108a3db102e7d800b5dd36d9a1e46b71fc7
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-01-16 02:33:56 +0000

    Bug 1940795: Don't tear aligned subwords in AtomicMemcpy. r=spidermonkey-reviewers,jandem
    Reads/writes to integer typed arrays mustn't tear, so they can't be split into
    single byte operations.
    
    Replace single byte copy loops with `AtomicCopy{Down,Up}NoTearIfAlignedUnsynchronized`,
    which performs additional checks when aligned word or dword copying is needed.
    
    Test262 tests: <https://github.com/tc39/test262/pull/4369>
    
    Differential Revision: https://phabricator.services.mozilla.com/D233710
    
    UltraBlame original commit: 968d34f043635337c9ab03c4c3f975c39e52f057

--- js/src/jit/shared/AtomicOperations-shared-jit.cpp
+++ js/src/jit/shared/AtomicOperations-shared-jit.cpp
@@ -4,17 +4,25 @@
 
 
 
+#include "mozilla/Assertions.h"
+#include "mozilla/Attributes.h"
+#include "mozilla/MathAlgorithms.h"
+
+#include <atomic>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <tuple>
+#include <utility>
+
 #include "jit/AtomicOperations.h"
+#include "js/GCAPI.h"
 
 #if defined(__arm__)
 #  include "jit/arm/Architecture-arm.h"
 #endif
 
 #ifdef JS_HAVE_GENERATED_ATOMIC_OPS
-
-#  include <atomic>
-
-#  include "js/GCAPI.h"
 
 using namespace js;
 using namespace js::jit;
@@ -70,6 +78,64 @@ void AtomicCompilerFence() {
 }
 #  endif
 
+
+
+
+template <size_t Alignment>
+static inline bool CanCopyAligned(const uint8_t* dest, const uint8_t* src,
+                                  const uint8_t* lim) {
+  static_assert(mozilla::IsPowerOfTwo(Alignment));
+  return ((uintptr_t(dest) | uintptr_t(src) | uintptr_t(lim)) &
+          (Alignment - 1)) == 0;
+}
+
+
+
+
+
+template <size_t Alignment>
+static inline bool CanAlignTo(const uint8_t* dest, const uint8_t* src) {
+  static_assert(mozilla::IsPowerOfTwo(Alignment));
+  return ((uintptr_t(dest) ^ uintptr_t(src)) & (Alignment - 1)) == 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+static MOZ_ALWAYS_INLINE auto AtomicCopyDownNoTearIfAlignedUnsynchronized(
+    uint8_t* dest, const uint8_t* src, const uint8_t* srcEnd) {
+  MOZ_ASSERT(src <= srcEnd);
+  MOZ_ASSERT(size_t(srcEnd - src) < WORDSIZE);
+
+  if (WORDSIZE > 4 && CanCopyAligned<4>(dest, src, srcEnd)) {
+    static_assert(WORDSIZE <= 8, "copies 32-bits at most once");
+
+    if (src < srcEnd) {
+      AtomicCopy32Unsynchronized(dest, src);
+      dest += 4;
+      src += 4;
+    }
+  } else if (CanCopyAligned<2>(dest, src, srcEnd)) {
+    while (src < srcEnd) {
+      AtomicCopy16Unsynchronized(dest, src);
+      dest += 2;
+      src += 2;
+    }
+  } else {
+    while (src < srcEnd) {
+      AtomicCopy8Unsynchronized(dest++, src++);
+    }
+  }
+  return std::pair{dest, src};
+}
+
 void AtomicMemcpyDownUnsynchronized(uint8_t* dest, const uint8_t* src,
                                     size_t nbytes) {
   JS::AutoSuppressGCAnalysis nogc;
@@ -85,12 +151,14 @@ void AtomicMemcpyDownUnsynchronized(uint8_t* dest, const uint8_t* src,
     void (*copyBlock)(uint8_t* dest, const uint8_t* src);
     void (*copyWord)(uint8_t* dest, const uint8_t* src);
 
-    if (((uintptr_t(dest) ^ uintptr_t(src)) & WORDMASK) == 0) {
+    if (CanAlignTo<WORDSIZE>(dest, src)) {
       const uint8_t* cutoff = (const uint8_t*)RoundUp(uintptr_t(src), WORDSIZE);
       MOZ_ASSERT(cutoff <= lim);  
-      while (src < cutoff) {
-        AtomicCopyByteUnsynchronized(dest++, src++);
-      }
+
+      
+      std::tie(dest, src) =
+          AtomicCopyDownNoTearIfAlignedUnsynchronized(dest, src, cutoff);
+
       copyBlock = AtomicCopyBlockDownUnsynchronized;
       copyWord = AtomicCopyWordUnsynchronized;
     } else if (UnalignedAccessesAreOK()) {
@@ -120,9 +188,44 @@ void AtomicMemcpyDownUnsynchronized(uint8_t* dest, const uint8_t* src,
 
   
 
-  while (src < lim) {
-    AtomicCopyByteUnsynchronized(dest++, src++);
+  AtomicCopyDownNoTearIfAlignedUnsynchronized(dest, src, lim);
+}
+
+
+
+
+
+
+
+
+
+
+
+static MOZ_ALWAYS_INLINE auto AtomicCopyUpNoTearIfAlignedUnsynchronized(
+    uint8_t* dest, const uint8_t* src, const uint8_t* srcBegin) {
+  MOZ_ASSERT(src >= srcBegin);
+  MOZ_ASSERT(size_t(src - srcBegin) < WORDSIZE);
+
+  if (WORDSIZE > 4 && CanCopyAligned<4>(dest, src, srcBegin)) {
+    static_assert(WORDSIZE <= 8, "copies 32-bits at most once");
+
+    if (src > srcBegin) {
+      dest -= 4;
+      src -= 4;
+      AtomicCopy32Unsynchronized(dest, src);
+    }
+  } else if (CanCopyAligned<2>(dest, src, srcBegin)) {
+    while (src > srcBegin) {
+      dest -= 2;
+      src -= 2;
+      AtomicCopy16Unsynchronized(dest, src);
+    }
+  } else {
+    while (src > srcBegin) {
+      AtomicCopy8Unsynchronized(--dest, --src);
+    }
   }
+  return std::pair{dest, src};
 }
 
 void AtomicMemcpyUpUnsynchronized(uint8_t* dest, const uint8_t* src,
@@ -134,16 +237,23 @@ void AtomicMemcpyUpUnsynchronized(uint8_t* dest, const uint8_t* src,
   src += nbytes;
   dest += nbytes;
 
+  
+  
+  
+  
+
   if (nbytes >= WORDSIZE) {
     void (*copyBlock)(uint8_t* dest, const uint8_t* src);
     void (*copyWord)(uint8_t* dest, const uint8_t* src);
 
-    if (((uintptr_t(dest) ^ uintptr_t(src)) & WORDMASK) == 0) {
+    if (CanAlignTo<WORDSIZE>(dest, src)) {
       const uint8_t* cutoff = (const uint8_t*)(uintptr_t(src) & ~WORDMASK);
       MOZ_ASSERT(cutoff >= lim);  
-      while (src > cutoff) {
-        AtomicCopyByteUnsynchronized(--dest, --src);
-      }
+
+      
+      std::tie(dest, src) =
+          AtomicCopyUpNoTearIfAlignedUnsynchronized(dest, src, cutoff);
+
       copyBlock = AtomicCopyBlockUpUnsynchronized;
       copyWord = AtomicCopyWordUnsynchronized;
     } else if (UnalignedAccessesAreOK()) {
@@ -153,6 +263,8 @@ void AtomicMemcpyUpUnsynchronized(uint8_t* dest, const uint8_t* src,
       copyBlock = AtomicCopyUnalignedBlockUpUnsynchronized;
       copyWord = AtomicCopyUnalignedWordUpUnsynchronized;
     }
+
+    
 
     const uint8_t* blocklim = src - ((src - lim) & ~BLOCKMASK);
     while (src > blocklim) {
@@ -169,9 +281,9 @@ void AtomicMemcpyUpUnsynchronized(uint8_t* dest, const uint8_t* src,
     }
   }
 
-  while (src > lim) {
-    AtomicCopyByteUnsynchronized(--dest, --src);
-  }
+  
+
+  AtomicCopyUpNoTearIfAlignedUnsynchronized(dest, src, lim);
 }
 
 }  
