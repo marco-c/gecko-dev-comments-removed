ade1e8340cde8e4851391fa2f839a567bf6032c0
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-01-16 01:29:11 +0000

    Bug 1940084: Print whether a frame's content has a display port to debug frame dump. r=layout-reviewers,emilio
    Differential Revision: https://phabricator.services.mozilla.com/D233262
    
    UltraBlame original commit: c11fd125a1c55c1d1ef78c78f6d11771e3f6155b

--- layout/generic/nsIFrame.cpp
+++ layout/generic/nsIFrame.cpp
@@ -8700,7 +8700,13 @@ void nsIFrame::ListGeneric(nsACString& aTo, const char* aPrefix,
     aTo += nsPrintfCString(" combines-3d-transform-with-ancestors");
   }
   if (mContent) {
-    aTo += nsPrintfCString(" [content=%p]", static_cast<void*>(mContent));
+    aTo += nsPrintfCString(" [content=%p", static_cast<void*>(mContent));
+    if (IsPrimaryFrame() && DisplayPortUtils::HasDisplayPort(mContent)) {
+      
+      
+      aTo += ",displayport"_ns;
+    }
+    aTo += "]"_ns;
   }
   aTo += nsPrintfCString(" [cs=%p", static_cast<void*>(mComputedStyle));
   if (mComputedStyle) {
