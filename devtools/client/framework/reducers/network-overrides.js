1c68c81eacbef6a2acecfedc717779fe6cad0548
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-01-16 02:25:00 +0000

    Bug 1940998 - [devtools] Do not update the initial state for the network overrides reducer r=devtools-reviewers,nchevobbe
    Depends on D233897
    
    Modules such as devtools/client/framework/reducers/network-overrides.js are only
    loaded once, making the initial state a singleton.
    
    If we mutate this, it will therefore apply to all toolboxes until the browser is
    closed.
    
    Differential Revision: https://phabricator.services.mozilla.com/D233898
    
    UltraBlame original commit: 4c700f28e869f8b3b6a4798024045e67be26e971

--- devtools/client/framework/reducers/network-overrides.js
+++ devtools/client/framework/reducers/network-overrides.js
@@ -3,12 +3,13 @@
 
 "use strict";
 
-const initialReducerState = {
-  mutableOverrides: {},
-};
-
 exports.reducer = networkOverridesReducer;
-function networkOverridesReducer(state = initialReducerState, action) {
+function networkOverridesReducer(
+  state = {
+    mutableOverrides: {},
+  },
+  action
+) {
   switch (action.type) {
     case "SET_NETWORK_OVERRIDE": {
       state.mutableOverrides[action.url] = action.path;
