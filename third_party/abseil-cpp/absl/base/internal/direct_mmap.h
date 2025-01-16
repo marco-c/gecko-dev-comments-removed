a19d0421ca90a38085858a95eb8f4212ec046c6e
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-01-16 02:44:07 +0000

    Bug 1935474 - only apply android api < 17 fix for SYS_mmap2 if actually on < 17 api. r=ng
    Differential Revision: https://phabricator.services.mozilla.com/D234118
    
    UltraBlame original commit: eb39000958d3a1ea61db3dca7b3d7ca008ad7f49

--- third_party/abseil-cpp/absl/base/internal/direct_mmap.h
+++ third_party/abseil-cpp/absl/base/internal/direct_mmap.h
@@ -94,7 +94,7 @@ inline void* DirectMmap(void* start, size_t length, int prot, int flags, int fd,
     errno = EINVAL;
     return MAP_FAILED;
   }
-#ifdef __BIONIC__
+#if defined(__BIONIC__) && (!defined(__ANDROID_API__) || __ANDROID_API__ < 17)
   
   
   return __mmap2(start, length, prot, flags, fd,
