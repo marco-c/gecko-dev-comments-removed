bd74d0f1a348eb088a464b5ae001a325de781b1e
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-01-16 02:26:57 +0000

    Bug 1921060 - Implement full mute button spec r=desktop-theme-reviewers,tabbrowser-reviewers,dao,sessionstore-reviewers,sclements,sidebar-reviewers,fluent-reviewers,bolsson
    Differential Revision: https://phabricator.services.mozilla.com/D231182
    
    UltraBlame original commit: d448ac7b0d9e0786534375771c9d42c19e749fdc

--- browser/components/tabbrowser/test/browser/tabs/browser_multiselect_tabs_mute_unmute.js
+++ browser/components/tabbrowser/test/browser/tabs/browser_multiselect_tabs_mute_unmute.js
@@ -39,7 +39,7 @@ add_task(async function muteTabs_usingButton() {
   }
 
   
-  let tab0MuteAudioBtn = tab0.overlayIcon;
+  let tab0MuteAudioBtn = tab0.audioButton;
   await test_mute_tab(tab0, tab0MuteAudioBtn, true);
 
   ok(muted(tab0), "Tab0 is muted");
@@ -72,7 +72,7 @@ add_task(async function muteTabs_usingButton() {
   
   
   
-  let tab1MuteAudioBtn = tab1.overlayIcon;
+  let tab1MuteAudioBtn = tab1.audioButton;
   await test_mute_tab(tab1, tab1MuteAudioBtn, true);
 
   
@@ -130,7 +130,7 @@ add_task(async function unmuteTabs_usingButton() {
   
   
   
-  let tab3MuteAudioBtn = tab3.overlayIcon;
+  let tab3MuteAudioBtn = tab3.audioButton;
   await test_mute_tab(tab3, tab3MuteAudioBtn, false);
 
   ok(!muted(tab0), "Tab0 is not muted");
@@ -243,7 +243,7 @@ add_task(async function playTabs_usingButton() {
   
   
   
-  let tab2MuteAudioBtn = tab2.overlayIcon;
+  let tab2MuteAudioBtn = tab2.audioButton;
   await test_mute_tab(tab2, tab2MuteAudioBtn, false);
 
   ok(muted(tab0), "Tab0 is muted");
