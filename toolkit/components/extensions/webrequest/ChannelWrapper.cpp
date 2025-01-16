1f339732a0b98f148fe986c47bad621382ed9767
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-01-16 01:39:36 +0000

    Bug 1909298 - Expand test coverage for StreamFilter r=rpl
    Differential Revision: https://phabricator.services.mozilla.com/D217578
    
    UltraBlame original commit: c40882b5fac285fee2f268b99bd593cfd0b5ed50

--- toolkit/components/extensions/webrequest/ChannelWrapper.cpp
+++ toolkit/components/extensions/webrequest/ChannelWrapper.cpp
@@ -817,6 +817,13 @@ already_AddRefed<nsITraceableChannel> ChannelWrapper::GetTraceableChannel(
     dom::ContentParent* aContentParent) const {
   nsCOMPtr<nsIRemoteTab> remoteTab;
   if (mAddonEntries.Get(aAddon.Id(), getter_AddRefs(remoteTab))) {
+    
+    
+    
+    
+    
+    
+    
     if (!HaveChannel() ||
         !aAddon.CanAccessURI(FinalURLInfo(), false, true, true)) {
       return nullptr;
