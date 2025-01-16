a63fb49f65a3b4322be942b0f7d9e830361e8432
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-01-16 01:06:04 +0000

    Backed out changeset 2e1d003838c2 (bug 1939180) for causing bc failures at browser_domfullscreen_sidebar.js. CLOSED TREE
    UltraBlame original commit: 549fd90dce24beee158ef0fa1fa5acb43db739b7

--- browser/components/sidebar/tests/browser/browser_domfullscreen_sidebar.js
+++ browser/components/sidebar/tests/browser/browser_domfullscreen_sidebar.js
@@ -1,5 +1,5 @@
-/* Any copyright is dedicated to the Public Domain.
- * http://creativecommons.org/publicdomain/zero/1.0/ */
+
+
 const { DOMFullscreenTestUtils } = ChromeUtils.importESModule(
   "resource://testing-common/DOMFullscreenTestUtils.sys.mjs"
 );
@@ -16,39 +16,32 @@ registerCleanupFunction(async () => {
 });
 
 add_task(async function test_dom_fullscreen() {
-  // ensure the sidebar becomes hidden in dom fullscreen
+  
   const url = "https://example.com/";
-  const { SidebarController, gBrowser } = win;
-  const { sidebarMain, toolbarButton } = SidebarController;
-  await SidebarController.promiseInitialized;
+  const sidebarLauncher = win.document.getElementById("sidebar-main");
 
   ok(
-    BrowserTestUtils.isVisible(sidebarMain),
-    "Sidebar main is initially visible"
+    BrowserTestUtils.isVisible(sidebarLauncher),
+    "Sidebar launcher is initially visible"
   );
 
-  info("Toggle expanded state via toolbar button.");
-  EventUtils.synthesizeMouseAtCenter(toolbarButton, {}, win);
-  await TestUtils.waitForCondition(
-    () => sidebarMain.expanded,
-    "Sidebar main is expanded"
+  await BrowserTestUtils.withNewTab(
+    { gBrowser: win.gBrowser, url },
+    async browser => {
+      
+      await DOMFullscreenTestUtils.changeFullscreen(browser, true);
+
+      is(win.document.fullscreenElement, browser, "Entered DOM fullscreen");
+      ok(
+        BrowserTestUtils.isHidden(sidebarLauncher),
+        "Sidebar launcher is hidden in DOMFullscreen"
+      );
+
+      await DOMFullscreenTestUtils.changeFullscreen(browser, false);
+      ok(
+        BrowserTestUtils.isVisible(sidebarLauncher),
+        "Sidebar launcher becomes visible when we exit DOMFullscreen"
+      );
+    }
   );
-
-  await BrowserTestUtils.withNewTab({ gBrowser, url }, async browser => {
-    // the newly opened tab should have focus
-    await DOMFullscreenTestUtils.changeFullscreen(browser, true);
-
-    is(win.document.fullscreenElement, browser, "Entered DOM fullscreen");
-    ok(
-      BrowserTestUtils.isHidden(sidebarMain),
-      "Sidebar main is hidden in DOMFullscreen"
-    );
-
-    await DOMFullscreenTestUtils.changeFullscreen(browser, false);
-    ok(
-      BrowserTestUtils.isVisible(sidebarMain),
-      "Sidebar main becomes visible when we exit DOMFullscreen"
-    );
-    ok(sidebarMain.expanded, "Sidebar main is still expanded");
-  });
 });
