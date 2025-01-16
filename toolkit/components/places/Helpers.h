fe06a2a3fcd28e24d00c8b704fb250cbda8b0b36
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-01-16 02:51:32 +0000

    Bug 1928020 - Create a Places component exposing an independent read-only connection. r=places-reviewers,Standard8
    Differential Revision: https://phabricator.services.mozilla.com/D230496
    
    UltraBlame original commit: 0ce2a98a7a53216304777c14ac70c3f0c7d6b18c

--- toolkit/components/places/Helpers.h
+++ toolkit/components/places/Helpers.h
@@ -20,8 +20,10 @@
 
 class nsIFile;
 
-namespace mozilla {
-namespace places {
+namespace mozilla::places {
+
+
+
 
 
 
@@ -35,6 +37,9 @@ class WeakAsyncStatementCallback : public mozIStorageStatementCallback {
   virtual ~WeakAsyncStatementCallback() = default;
 };
 
+
+
+
 class AsyncStatementCallback : public WeakAsyncStatementCallback {
  public:
   NS_DECL_ISUPPORTS
@@ -42,6 +47,21 @@ class AsyncStatementCallback : public WeakAsyncStatementCallback {
 
  protected:
   virtual ~AsyncStatementCallback() = default;
+};
+
+
+
+
+class PendingStatementCallback : public AsyncStatementCallback {
+ public:
+  NS_INLINE_DECL_REFCOUNTING_INHERITED(PendingStatementCallback,
+                                       AsyncStatementCallback);
+  PendingStatementCallback() = default;
+
+  virtual nsresult BindParams(mozIStorageBindingParamsArray*) MOZ_MUST_OVERRIDE;
+
+ protected:
+  virtual ~PendingStatementCallback() = default;
 };
 
 
@@ -200,8 +220,9 @@ class QueryKeyValuePair final {
                     int32_t aEquals, int32_t aPastEnd) {
     if (aEquals == aKeyBegin) aEquals = aPastEnd;
     key = Substring(aSource, aKeyBegin, aEquals - aKeyBegin);
-    if (aPastEnd - aEquals > 0)
+    if (aPastEnd - aEquals > 0) {
       value = Substring(aSource, aEquals + 1, aPastEnd - aEquals - 1);
+    }
   }
   nsCString key;
   nsCString value;
@@ -305,7 +326,6 @@ class AsyncStatementTelemetryTimer : public AsyncStatementCallback {
   const TimeStamp mStart;
 };
 
-}  
-}  
+} 
 
 #endif  
