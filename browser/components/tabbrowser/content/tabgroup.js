cfcdbdd2acb05c3d2fc44f5201d05763460ae72e
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-01-16 01:25:37 +0000

    Bug 1939593 - Unbreak ::first-letter for tab group labels. r=jswinarton,desktop-theme-reviewers,tabbrowser-reviewers,emilio
    Differential Revision: https://phabricator.services.mozilla.com/D233347
    
    UltraBlame original commit: cc0c1a6bfccf38004d8845e3b99e3bf79eda3e5e

--- browser/components/tabbrowser/content/tabgroup.js
+++ browser/components/tabbrowser/content/tabgroup.js
@@ -1,11 +1,11 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+
+
 
 "use strict";
 
-// This is loaded into chrome windows with the subscript loader. Wrap in
-// a block to prevent accidentally leaking globals onto `window`.
+
+
 {
   class MozTabbrowserTabGroup extends MozXULElement {
     static markup = `
@@ -15,16 +15,16 @@
       <html:slot/>
       `;
 
-    /** @type {string} */
+    
     #label;
 
-    /** @type {MozTextLabel} */
+    
     #labelElement;
 
-    /** @type {string} */
+    
     #colorCode;
 
-    /** @type {MutationObserver} */
+    
     #tabChangeObserver;
 
     constructor() {
@@ -38,11 +38,11 @@
     }
 
     connectedCallback() {
-      // Always set the mutation observer to listen for tab change events, even
-      // if we are already initialized.
-      // This is needed to ensure events continue to fire even if the tab group is
-      // moved from the horizontal to vertical tab layout or vice-versa, which
-      // causes the component to be repositioned in the DOM.
+      
+      
+      
+      
+      
       this.#observeTabChanges();
 
       if (this._initialized) {
@@ -145,14 +145,14 @@
     set label(val) {
       this.#label = val;
 
-      // Add a zero width space so we always create a text node and get
-      // consistent layout even if the group name is empty.
-      this.setAttribute("label", "\u200b" + val);
+      
+      
+      this.setAttribute("label", val || "\u200b");
 
       this.#updateLabelAriaAttributes();
     }
 
-    // alias for label
+    
     get name() {
       return this.label;
     }
@@ -191,18 +191,18 @@
       return Array.from(this.children).filter(node => node.matches("tab"));
     }
 
-    /**
-     * @returns {MozTextLabel}
-     */
+    
+
+
     get labelElement() {
       return this.#labelElement;
     }
 
-    /**
-     * add tabs to the group
-     *
-     * @param tabs array of tabs to add
-     */
+    
+
+
+
+
     addTabs(tabs) {
       for (let tab of tabs) {
         let tabToMove =
@@ -217,26 +217,26 @@
       }
     }
 
-    /**
-     * Remove all tabs from the group and delete the group.
-     *
-     */
+    
+
+
+
     ungroupTabs() {
       for (let i = this.tabs.length - 1; i >= 0; i--) {
         gBrowser.ungroupTab(this.tabs[i]);
       }
     }
 
-    /**
-     * Save group data to session store.
-     */
+    
+
+
     save() {
       SessionStore.addSavedTabGroup(this);
     }
 
-    /**
-     * @param {PointerEvent} event
-     */
+    
+
+
     on_click(event) {
       if (event.target === this.#labelElement && event.button === 0) {
         event.preventDefault();
@@ -249,11 +249,11 @@
       this.collapsed = false;
     }
 
-    /**
-     * If one of this group's tabs is the selected tab, this will do nothing.
-     * Otherwise, it will expand the group if collapsed, and select the first
-     * tab in its list.
-     */
+    
+
+
+
+
     select() {
       this.collapsed = false;
       if (gBrowser.selectedTab.group == this) {
