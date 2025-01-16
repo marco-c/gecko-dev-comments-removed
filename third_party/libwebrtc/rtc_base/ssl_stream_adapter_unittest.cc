9e0418184dfff37bad173b0f7bc25eca54a448c0
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-01-16 01:15:28 +0000

    Bug 1934695 - Vendor libwebrtc from ddac9f2739
    Upstream commit: https://webrtc.googlesource.com/src/+/ddac9f27399902bb9bf3f5cd654c769a733e6dfe
        Reduce RSA key size for SSLStreamAdapter test
    
        and remove the testing of the nondefault key size from the "server" parameters to speed up tests
    
        BUG=webrtc:375552698
    
        Change-Id: Ibc1bd491300964aa45826b98962ed3e56c6d4974
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/366941
        Reviewed-by: Harald Alvestrand <htawebrtc.org>
        Reviewed-by: Christoffer Dewerin <janssonwebrtc.org>
        Commit-Queue: Philipp Hancke <phanckemeta.com>
        Cr-Commit-Position: refs/heads/main{#43321}
    
    UltraBlame original commit: 0c6d8a003d4009aac0ac249642be29dd77887580

--- third_party/libwebrtc/rtc_base/ssl_stream_adapter_unittest.cc
+++ third_party/libwebrtc/rtc_base/ssl_stream_adapter_unittest.cc
@@ -1128,16 +1128,17 @@ TEST_P(SSLStreamAdapterTestDTLSHandshake, TestGetSslCipherSuite) {
 
 
 
+
+
 INSTANTIATE_TEST_SUITE_P(
     SSLStreamAdapterTestDTLSHandshakeKeyParameters,
     SSLStreamAdapterTestDTLSHandshake,
     Combine(Values(rtc::KeyParams::RSA(rtc::kRsaDefaultModSize,
                                        rtc::kRsaDefaultExponent),
-                   rtc::KeyParams::RSA(2 * 1152, rtc::kRsaDefaultExponent),
+                   rtc::KeyParams::RSA(1152, rtc::kRsaDefaultExponent),
                    rtc::KeyParams::ECDSA(rtc::EC_NIST_P256)),
             Values(rtc::KeyParams::RSA(rtc::kRsaDefaultModSize,
                                        rtc::kRsaDefaultExponent),
-                   rtc::KeyParams::RSA(2 * 1152, rtc::kRsaDefaultExponent),
                    rtc::KeyParams::ECDSA(rtc::EC_NIST_P256)),
             Values(std::make_pair(rtc::DIGEST_SHA_256, SHA256_DIGEST_LENGTH))));
 
@@ -1478,4 +1479,3 @@ TEST_F(SSLStreamAdapterTestDTLS, TestGetSslVersionBytes) {
   ASSERT_TRUE(GetSslVersionBytes(false, &server_version));
   EXPECT_EQ(server_version, kDtls1_2);
 }
-
