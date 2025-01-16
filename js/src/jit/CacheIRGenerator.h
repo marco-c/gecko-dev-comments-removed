19a468d4474a5d7d47b029f70fe4c1433d112e4d
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-01-16 02:34:47 +0000

    Bug 1940680 - Part 1: Add separate "target" member to InlinableNativeIRGenerator. r=jandem
    `callee` and `target` both currently still point to the same object, but this
    will change in part 4.
    
    Differential Revision: https://phabricator.services.mozilla.com/D233688
    
    UltraBlame original commit: 329a6769b4730c2782273ca885d62ba899565fc7

--- js/src/jit/CacheIRGenerator.h
+++ js/src/jit/CacheIRGenerator.h
@@ -586,7 +586,8 @@ class MOZ_RAII InlinableNativeIRGenerator {
   CacheIRWriter& writer;
   JSContext* cx_;
 
-  HandleFunction callee_;
+  HandleObject callee_;
+  HandleFunction target_;
   HandleValue newTarget_;
   HandleValue thisval_;
   HandleValueArray args_;
@@ -596,6 +597,11 @@ class MOZ_RAII InlinableNativeIRGenerator {
   HandleScript script() const { return generator_.script_; }
   bool isFirstStub() const { return generator_.isFirstStub_; }
   bool ignoresResult() const { return generator_.op_ == JSOp::CallIgnoresRv; }
+
+  
+  
+  
+  bool isCalleeBoundFunction() const { return callee_ != target_; }
 
   void emitNativeCalleeGuard();
   void emitOptimisticClassGuard(ObjOperandId objId, JSObject* obj,
@@ -789,13 +795,15 @@ class MOZ_RAII InlinableNativeIRGenerator {
   }
 
  public:
-  InlinableNativeIRGenerator(CallIRGenerator& generator, HandleFunction callee,
-                             HandleValue newTarget, HandleValue thisValue,
-                             HandleValueArray args, CallFlags flags)
+  InlinableNativeIRGenerator(CallIRGenerator& generator, HandleObject callee,
+                             HandleFunction target, HandleValue newTarget,
+                             HandleValue thisValue, HandleValueArray args,
+                             CallFlags flags)
       : generator_(generator),
         writer(generator.writer),
         cx_(generator.cx_),
         callee_(callee),
+        target_(target),
         newTarget_(newTarget),
         thisval_(thisValue),
         args_(args),
