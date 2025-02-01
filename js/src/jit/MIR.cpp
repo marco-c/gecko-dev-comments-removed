45be1ac88a7979f0a46533c8d7e7c00c47a994ef
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-02-01 02:05:04 +0000

    Bug 1943372: Directly use NumberIsInt32 in EvaluateConstantOperands. r=iain
    And remove the unused `ptypeChange` out-param.
    
    Differential Revision: https://phabricator.services.mozilla.com/D235329
    
    UltraBlame original commit: bf91cfc6172a5bd2555fcd10ecf34e7e6f36efb1

--- js/src/jit/MIR.cpp
+++ js/src/jit/MIR.cpp
@@ -211,8 +211,7 @@ static MConstant* EvaluateInt64ConstantOperands(TempAllocator& alloc,
 }
 
 static MConstant* EvaluateConstantOperands(TempAllocator& alloc,
-                                           MBinaryInstruction* ins,
-                                           bool* ptypeChange = nullptr) {
+                                           MBinaryInstruction* ins) {
   MDefinition* left = ins->getOperand(0);
   MDefinition* right = ins->getOperand(1);
 
@@ -293,22 +292,15 @@ static MConstant* EvaluateConstantOperands(TempAllocator& alloc,
   if (ins->type() == MIRType::Double) {
     return MConstant::New(alloc, DoubleValue(ret));
   }
-
-  Value retVal;
-  retVal.setNumber(JS::CanonicalizeNaN(ret));
-
-  
-  
-  
   MOZ_ASSERT(ins->type() == MIRType::Int32);
-  if (!retVal.isInt32()) {
-    if (ptypeChange) {
-      *ptypeChange = true;
-    }
+
+  
+  
+  int32_t intRet;
+  if (!mozilla::NumberIsInt32(ret, &intRet)) {
     return nullptr;
   }
-
-  return MConstant::New(alloc, retVal);
+  return MConstant::New(alloc, Int32Value(intRet));
 }
 
 static MConstant* EvaluateConstantNaNOperand(MBinaryInstruction* ins) {
