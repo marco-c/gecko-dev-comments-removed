bae0b80b1cbded2dd4a31f50f6d9b9a4896692b2
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-02-01 01:07:29 +0000

    Bug 1901492 - Add RequireTrustedTypesForDirective info to IPC structures. r=smaug,tschuster
    Expose dom::nsCSPPolicy::mHasRequireTrustedTypesForDirective and
    dom::nsCSPContext::mRequireTrustedTypesForDirectiveState on
    ipc::ContentSecurityPolicy and ipc::CSPInfo respectively, making sure
    that things remain in sync when converting between dom and ipc
    structures and when serializing/deserializing ipc structures.
    
    There is no exposed behavior change but having
    RequireTrustedTypesForDirectiveState on ipc::CSPInfo will allow
    to implement GetTrustedTypesCompliantString for worker thread.
    The info on ipc::ContentSecurityPolicy is needed to ensure
    nsCSPContext::mRequireTrustedTypesForDirectiveState is correctly
    set after we append IPC policies and before EnsureIPCPoliciesRead()
    is called.
    
    Differential Revision: https://phabricator.services.mozilla.com/D233636
    
    UltraBlame original commit: c6296179fcb45709864a2e327b0632b255d7691a

--- dom/security/nsCSPUtils.h
+++ dom/security/nsCSPUtils.h
@@ -706,6 +706,10 @@ class nsCSPPolicy {
     return mDeliveredViaMetaTag;
   }
 
+  inline bool hasRequireTrustedTypesForDirective() const {
+    return mHasRequireTrustedTypesForDirective;
+  }
+
   inline void setReportOnlyFlag(bool aFlag) { mReportOnly = aFlag; }
 
   
