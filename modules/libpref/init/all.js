b0f8c55bf9c01d65f6191fae69c36bd0e13a678b
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-02-01 01:10:26 +0000

    Backed out changeset 6299e1989056 (bug 1941875) for causing mass failures. CLOSED TREE
    UltraBlame original commit: 6b9dd01e57404eee3c8218e667a2bd4b631d5f21

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -4137,8 +4137,3 @@ pref("privacy.query_stripping.strip_on_share.canDisable", true);
 
 
 pref("captchadetection.loglevel", "Warn");
-#if defined(NIGHTLY_BUILD)
-pref("captchadetection.actor.enabled", true);
-else
-pref("captchadetection.actor.enabled", false);
-#endif
