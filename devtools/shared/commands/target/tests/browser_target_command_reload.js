0e5a10b889fa8ba1b936f42e8c99b04c14454db9
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-02-01 02:58:53 +0000

    Bug 1928535 - [devtools] Fix browser_target_command_reload.js to run with --repeat r=devtools-reviewers,ochameau
    Differential Revision: https://phabricator.services.mozilla.com/D235971
    
    UltraBlame original commit: e4fee8872931935b3d873066b73c5ff6df304b73

--- devtools/shared/commands/target/tests/browser_target_command_reload.js
+++ devtools/shared/commands/target/tests/browser_target_command_reload.js
@@ -17,6 +17,9 @@ add_task(async function () {
   info(" ### Test reloading a Tab");
 
   
+  await fetch(TEST_URL + "?setup");
+
+  
   const tab = await addTab(TEST_URL);
   const commands = await CommandsFactory.forTab(tab);
   const targetCommand = commands.targetCommand;
