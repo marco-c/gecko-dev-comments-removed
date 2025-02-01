713c24ec36c942746906b1228f9a5c89912a5561
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-02-01 02:03:35 +0000

    Bug 1943339 - [devtools] Fix memoization issues for netmonitor getColumns selector r=devtools-reviewers,ochameau
    The individual input selectors of reselect selectors should be as close as possible to the
    data used by the selector, so that we can efficiently memoize the selector.
    Since this selector returns an object which will always fail shallow comparisons it's important to
    make sure we memoize efficiently.
    
    Differential Revision: https://phabricator.services.mozilla.com/D234517
    
    UltraBlame original commit: b3f1de9a3ea3c01d2d68806612b6eb3458b8f74e

--- devtools/client/netmonitor/src/selectors/ui.js
+++ devtools/client/netmonitor/src/selectors/ui.js
@@ -53,23 +53,17 @@ function getVisibleColumns(columns) {
 }
 
 const getColumns = createSelector(
-  state => state.ui,
-  state => state.search,
-  (ui, search) => {
-    if (
-      ((ui.networkDetailsOpen || search.panelOpen) &&
-        getVisibleColumns(ui.columns).length === 1 &&
-        ui.columns.waterfall) ||
-      (!ui.networkDetailsOpen && !search.panelOpen)
-    ) {
-      return ui.columns;
+  state => state.ui.columns,
+  state => state.ui.networkDetailsOpen || state.search.panelOpen,
+  (columns, isSidePanelOpen) => {
+    const isWaterfallOnly =
+      getVisibleColumns(columns).length === 1 && columns.waterfall;
+    if (isSidePanelOpen && !isWaterfallOnly) {
+      
+      
+      columns = { ...columns };
+      delete columns.waterfall;
     }
-
-    
-    
-    
-    const columns = { ...ui.columns };
-    delete columns.waterfall;
     return columns;
   }
 );
