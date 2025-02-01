364911163ab3c17d22ccae9e33a90cddeb58c891
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-02-01 03:02:19 +0000

    Backed out changeset 2e538573f7d9 (bug 1940760) for causing perma wpt failures  /clear-site-data/clear-cache-partitioning.sub.https.html CLOSED TREE
    UltraBlame original commit: 54e0771b9db4ec6e90b132f42f71e3c07825b6dc

--- testing/web-platform/tests/clear-site-data/support/clear-site-data-cache.py
+++ testing/web-platform/tests/clear-site-data/support/clear-site-data-cache.py
@@ -41,43 +41,14 @@ def main(request, response):
             headers += [(b"Clear-Site-Data", b'"' + clear + b'"')]
 
     if response_type == b"single_html":
-        iframe = ""
-        if b"iframe" in request.GET:
-            
-            iframe_url = request.GET.first(b"iframe").decode()
-            content = f'''
-                <script>
-                    // forward iframe uuid to opener
-                    window.addEventListener('message', function(event) {{
-                        if(window.opener) {{
-                            window.opener.postMessage(event.data, "*");
-                        }} else {{
-                            window.parent.postMessage(event.data, "*");
-                        }}
-                        window.close();
-                    }});
-                </script>
-                <br>
-                {request.url}<br>
-                {iframe_url}<br>
-                <iframe src="{iframe_url}"></iframe>
-                </body>
-            '''
-        else:
-            
-            u = uuid.uuid4()
-            content = f'''
-                <script>
-                    if(window.opener) {{
-                        window.opener.postMessage("{u}", "*");
-                    }} else {{
-                        window.parent.postMessage("{u}", "*");
-                    }}
-                    window.close();
-                </script>
-                <body>
-                    {request.url}
-                </body>'''
+        
+        content = f'''
+            <script>
+                window.opener.postMessage("{uuid.uuid4()}" , "*");
+            </script>
+            <body>
+                {request.url}
+            </body>'''
     elif response_type == b"json":
         
         content = f'''["{uuid.uuid4()}"]'''
@@ -87,10 +58,7 @@ def main(request, response):
             <script>
                 fetch("{url}")
                     .then(response => response.json())
-                    .then(uuid => {{
-                        window.opener.postMessage(uuid[0], "*");
-                        window.close();
-                    }});
+                    .then(uuid => window.opener.postMessage(uuid[0], "*"));
             </script>
             <body>
                 {request.url}<br>
