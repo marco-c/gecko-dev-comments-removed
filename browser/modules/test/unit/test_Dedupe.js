99e6862d27436c31eb22675e335c7348a5b62eee
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-02-01 03:19:18 +0000

    Bug 1943458 - Decouple LinksCache and Dedupe dependency on newtab r=home-newtab-reviewers,mconley,jteow
    Differential Revision: https://phabricator.services.mozilla.com/D236042
    
    UltraBlame original commit: 8156588ad0bb27f19008324cbe297d78f0e4b53b

---
+++ browser/modules/test/unit/test_Dedupe.js
@@ -0,0 +1,53 @@
+
+
+
+
+"use strict";
+
+const { Dedupe } = ChromeUtils.importESModule(
+  "resource:///modules/Dedupe.sys.mjs"
+);
+
+add_task(async function test_dedupe_group() {
+  let instance = new Dedupe();
+
+  
+  let beforeItems = [
+    [1, 1, 1],
+    [2, 2, 2],
+    [3, 3, 3],
+  ];
+  let afterItems = [[1], [2], [3]];
+  Assert.deepEqual(
+    instance.group(...beforeItems),
+    afterItems,
+    "Should remove duplicates inside the groups"
+  );
+
+  
+  beforeItems = [
+    [1, 2, 3],
+    [2, 3, 4],
+    [3, 4, 5],
+  ];
+  afterItems = [[1, 2, 3], [4], [5]];
+  Assert.deepEqual(
+    instance.group(...beforeItems),
+    afterItems,
+    "Should remove duplicates between groups"
+  );
+
+  
+  instance = new Dedupe(item => item.id);
+  beforeItems = [
+    [{ id: 1 }, { id: 1 }, { id: 2 }],
+    [{ id: 1 }, { id: 3 }, { id: 2 }],
+    [{ id: 1 }, { id: 2 }, { id: 5 }],
+  ];
+  afterItems = [[{ id: 1 }, { id: 2 }], [{ id: 3 }], [{ id: 5 }]];
+  Assert.deepEqual(
+    instance.group(...beforeItems),
+    afterItems,
+    "Should remove duplicates from groups of objects"
+  );
+});
