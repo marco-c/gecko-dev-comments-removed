7dd59401c11c5e2513c900607bfcdded76d8a20a
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-02-01 01:33:52 +0000

    Bug 1942059 - Part 1: Avoid generating unused OOL fallback code when Fjcvtzs is available. r=jandem
    Differential Revision: https://phabricator.services.mozilla.com/D234503
    
    UltraBlame original commit: 55e9fda0197fbd5165e0b13e4c78c0a495643a62

--- js/src/jit/arm64/MacroAssembler-arm64.h
+++ js/src/jit/arm64/MacroAssembler-arm64.h
@@ -593,6 +593,10 @@ class MacroAssemblerCompat : public vixl::MacroAssembler {
 
   using vixl::MacroAssembler::B;
 
+  bool hasFjcvtzs() const {
+    return CPUHas(vixl::CPUFeatures::kFP, vixl::CPUFeatures::kJSCVT);
+  }
+
   void convertDoubleToInt32(FloatRegister src, Register dest, Label* fail,
                             bool negativeZeroCheck = true) {
     ARMFPRegister fsrc64(src, 64);
@@ -600,7 +604,7 @@ class MacroAssemblerCompat : public vixl::MacroAssembler {
 
     
     
-    if (CPUHas(vixl::CPUFeatures::kFP, vixl::CPUFeatures::kJSCVT)) {
+    if (hasFjcvtzs()) {
       
       
       Fjcvtzs(dest32, fsrc64);
