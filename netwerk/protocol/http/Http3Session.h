31bb92f551b65b6d72eb624cf80ae1e4c57601b4
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-02-01 03:09:55 +0000

    Bug 1934402 - Fix Webtransport serverCertificateHashes in production. r=kershaw,valentin,necko-reviewers
    A recent change broke serverCertificateHashes outside the wpt tests in Firefox by enforcing a
    known root certificate. This commit adds an exceptions for certificates verified by a hash.
    
    Differential Revision: https://phabricator.services.mozilla.com/D231479
    
    UltraBlame original commit: 5b031fc405f92e47bc43eb5f96116fcb35f4d7cb

--- netwerk/protocol/http/Http3Session.h
+++ netwerk/protocol/http/Http3Session.h
@@ -179,7 +179,7 @@ class Http3Session final : public nsAHttpTransaction, public nsAHttpConnection {
 
   
   
-  void Authenticated(int32_t aError);
+  void Authenticated(int32_t aError, bool aServCertHashesSucceeded = false);
 
   nsresult ProcessOutputAndEvents(nsIUDPSocket* socket);
 
