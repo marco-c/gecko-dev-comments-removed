8b919a8695e8d958a5b8e4d07ed5f292951ea80e
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-02-01 01:01:07 +0000

    Backed out changeset 7ebf2aee0c7b (bug 1941350) for causing xpcshell failures in test_TelemetryReportingPolicy.js. CLOSED TREE
    UltraBlame original commit: 04e758627303044019496e56def1e1d74e51461d

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -2020,6 +2020,8 @@ pref("browser.aboutwelcome.enabled", true);
 
 pref("browser.aboutwelcome.screens", "");
 
+pref("browser.aboutwelcome.showModal", false);
+
 
 
 pref("messaging-system.log", "warn");
