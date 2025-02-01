76632a021bc7776252981182a5de06e7a2293ff5
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-02-01 03:13:00 +0000

    Bug 1943977 - Ensure collection-disabled pings work as expected for JOG pings. r=chutten
    Differential Revision: https://phabricator.services.mozilla.com/D235948
    
    UltraBlame original commit: 82457719faa7fbc9a0065f5eb041235f1dbff364

--- toolkit/components/glean/tests/xpcshell/test_Glean.js
+++ toolkit/components/glean/tests/xpcshell/test_Glean.js
@@ -799,3 +799,34 @@ add_task(async function test_submit_throws() {
     "Should throw inside callback"
   );
 });
+
+add_task(function test_collection_disabled_pings_work() {
+  
+
+  Assert.ok("collectionDisabledPing" in GleanPings);
+
+  
+  
+  Glean.testOnly.collectionDisabledCounter.add(1);
+  Assert.equal(
+    undefined,
+    Glean.testOnly.collectionDisabledCounter.testGetValue()
+  );
+
+  
+  GleanPings.collectionDisabledPing.setEnabled(true);
+  Glean.testOnly.collectionDisabledCounter.add(2);
+  Assert.equal(2, Glean.testOnly.collectionDisabledCounter.testGetValue());
+
+  let submitted = false;
+  GleanPings.collectionDisabledPing.testBeforeNextSubmit(() => {
+    submitted = true;
+    Assert.equal(2, Glean.testOnly.collectionDisabledCounter.testGetValue());
+  });
+  GleanPings.collectionDisabledPing.submit();
+  Assert.ok(submitted, "Ping was submitted, callback was called.");
+  Assert.equal(
+    undefined,
+    Glean.testOnly.collectionDisabledCounter.testGetValue()
+  );
+});
