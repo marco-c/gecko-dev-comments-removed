009f3b990d0a49475281da69d0134ea69905e50d
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-02-01 02:02:48 +0000

    Bug 1899786: make nsParser handle event loop spinning causing OnStopRequest to run r=smaug
    ChannelEventQueue guarantees ordering of OnFoo events, but doesn't make
    assertions about if they can be on the event queue or not when other
    OnFoo's are running.  nsParser spins the EventQueue from script, and so
    can run OnStopRequest from within OnDataAvailable.
    
    Differential Revision: https://phabricator.services.mozilla.com/D219915
    
    UltraBlame original commit: c502d2025998fab69cc630543980c599571bf76f

--- parser/htmlparser/nsParser.cpp
+++ parser/htmlparser/nsParser.cpp
@@ -126,6 +126,7 @@ void nsParser::Initialize() {
   mFlags = NS_PARSER_FLAG_CAN_TOKENIZE;
 
   mProcessingNetworkData = false;
+  mOnStopPending = false;
   mIsAboutBlank = false;
 }
 
@@ -1025,6 +1026,19 @@ nsresult nsParser::OnDataAvailable(nsIRequest* request,
         sinkDeathGrip->WillParse();
       }
       rv = ResumeParse();
+      
+      
+      
+      if ((mParserContext->mRequest == request) && mOnStopPending) {
+        mOnStopPending = false;
+        mParserContext->mStreamListenerState = eOnStop;
+        mParserContext->mScanner.SetIncremental(false);
+
+        if (sinkDeathGrip) {
+          sinkDeathGrip->WillParse();
+        }
+        rv = ResumeParse(true, true);
+      }
       mProcessingNetworkData = false;
     }
   } else {
@@ -1047,20 +1061,23 @@ nsresult nsParser::OnStopRequest(nsIRequest* request, nsresult status) {
 
   nsresult rv = NS_OK;
 
-  if (mParserContext->mRequest == request) {
-    mParserContext->mStreamListenerState = eOnStop;
-    mParserContext->mScanner.SetIncremental(false);
-  }
-
   mStreamStatus = status;
 
-  if (IsOkToProcessNetworkData() && NS_SUCCEEDED(rv)) {
+  if (IsOkToProcessNetworkData()) {
+    if (mParserContext->mRequest == request) {
+      mParserContext->mStreamListenerState = eOnStop;
+      mParserContext->mScanner.SetIncremental(false);
+    }
+
     mProcessingNetworkData = true;
     if (mSink) {
       mSink->WillParse();
     }
     rv = ResumeParse(true, true);
     mProcessingNetworkData = false;
+  } else {
+    
+    mOnStopPending = true;
   }
 
   
