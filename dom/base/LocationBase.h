03bbb54af5fd3755445aa2cc8634b8cd9d843a69
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-02-01 03:13:27 +0000

    Bug 1944220 Avoid JSString -> UTF-16 -> UTF-8 conversions in Location fields. r=webidl,emilio
    Differential Revision: https://phabricator.services.mozilla.com/D235753
    
    UltraBlame original commit: 5a944e0391a0140dea5c3cd1270556a5d2e594a5

--- dom/base/LocationBase.h
+++ dom/base/LocationBase.h
@@ -1,8 +1,8 @@
-/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
-/* vim: set ts=8 sts=2 et sw=2 tw=80: */
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+
+
+
+
 
 #ifndef mozilla_dom_LocationBase_h
 #define mozilla_dom_LocationBase_h
@@ -21,17 +21,17 @@ class ErrorResult;
 namespace dom {
 class BrowsingContext;
 
-//*****************************************************************************
-// Location: Script "location" object
-//*****************************************************************************
+
+
+
 
 class LocationBase {
  public:
-  // WebIDL API:
-  void Replace(const nsAString& aUrl, nsIPrincipal& aSubjectPrincipal,
+  
+  void Replace(const nsACString& aUrl, nsIPrincipal& aSubjectPrincipal,
                ErrorResult& aError);
 
-  void SetHref(const nsAString& aHref, nsIPrincipal& aSubjectPrincipal,
+  void SetHref(const nsACString& aHref, nsIPrincipal& aSubjectPrincipal,
                ErrorResult& aError);
 
  protected:
@@ -40,26 +40,26 @@ class LocationBase {
 
   void SetURI(nsIURI* aURL, nsIPrincipal& aSubjectPrincipal, ErrorResult& aRv,
               bool aReplace = false);
-  void SetHrefWithBase(const nsAString& aHref, nsIURI* aBase,
+  void SetHrefWithBase(const nsACString& aHref, nsIURI* aBase,
                        nsIPrincipal& aSubjectPrincipal, bool aReplace,
                        ErrorResult& aRv);
 
-  // Helper for Assign/SetHref/Replace
-  void DoSetHref(const nsAString& aHref, nsIPrincipal& aSubjectPrincipal,
+  
+  void DoSetHref(const nsACString& aHref, nsIPrincipal& aSubjectPrincipal,
                  bool aReplace, ErrorResult& aRv);
 
-  // Get the base URL we should be using for our relative URL
-  // resolution for SetHref/Assign/Replace.
+  
+  
   nsIURI* GetSourceBaseURL();
 
-  // Check whether it's OK to load the given url with the given subject
-  // principal, and if so construct the right nsDocShellLoadInfo for the load
-  // and return it.
+  
+  
+  
   already_AddRefed<nsDocShellLoadState> CheckURL(
       nsIURI* url, nsIPrincipal& aSubjectPrincipal, ErrorResult& aRv);
 };
 
-}  // namespace dom
-}  // namespace mozilla
+}  
+}  
 
-#endif  // mozilla_dom_LocationBase_h
+#endif  
