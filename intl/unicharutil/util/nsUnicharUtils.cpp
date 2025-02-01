f3ad4e60f486beea4dfddc2cfa1d88ea17358be5
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-02-01 02:31:58 +0000

    Backed out changeset 72b0ae433f15 (bug 1940947) for causing failures at seg-break-transformation-004.tentative.html.
    UltraBlame original commit: d6e3b79331ac5382f61590e1784c48624a0b853f

--- intl/unicharutil/util/nsUnicharUtils.cpp
+++ intl/unicharutil/util/nsUnicharUtils.cpp
@@ -515,21 +515,14 @@ uint32_t HashUTF8AsUTF16(const char* aUTF8, size_t aLength, bool* aErr) {
   return hash;
 }
 
-
-
-
-
-static constexpr uint32_t kWonCurrencySign = 0x20A9;
-
 bool IsSegmentBreakSkipChar(uint32_t u) {
   return intl::UnicodeProperties::IsEastAsianWidthFHWexcludingEmoji(u) &&
-         intl::UnicodeProperties::GetScriptCode(u) != intl::Script::HANGUL &&
-         u != kWonCurrencySign;
+         intl::UnicodeProperties::GetScriptCode(u) != intl::Script::HANGUL;
 }
 
 bool IsEastAsianPunctuation(uint32_t u) {
   return intl::UnicodeProperties::IsEastAsianWidthFHW(u) &&
-         intl::UnicodeProperties::IsPunctuation(u) && u != kWonCurrencySign;
+         intl::UnicodeProperties::IsPunctuation(u);
 }
 
 bool IsPunctuationForWordSelect(char16_t aCh) {
