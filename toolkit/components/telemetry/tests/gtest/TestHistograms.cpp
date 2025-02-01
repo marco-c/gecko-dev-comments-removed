4dbd4219a2e7f79629229ca0be2ce070e3834da3
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-02-01 02:18:02 +0000

    Bug 1942908 - Fixing build bustages/Gtest failures - part 2.
    UltraBlame original commit: 8f85c9357564c8381ab888969e93636a5da12a0a

--- toolkit/components/telemetry/tests/gtest/TestHistograms.cpp
+++ toolkit/components/telemetry/tests/gtest/TestHistograms.cpp
@@ -705,10 +705,6 @@ TEST_F(TelemetryTestFixture, AccumulateTimeDelta) {
   Telemetry::AccumulateTimeDelta(Telemetry::TELEMETRY_TEST_COUNT, start, start);
 
   
-  Telemetry::AccumulateTimeDelta(Telemetry::TELEMETRY_TEST_COUNT, start + delta,
-                                 start);
-
-  
   JS::Rooted<JS::Value> snapshot(cx.GetJSContext());
   GetSnapshots(cx.GetJSContext(), mTelemetry, "TELEMETRY_TEST_COUNT", &snapshot,
                false);
@@ -744,10 +740,6 @@ TEST_F(TelemetryTestFixture, AccumulateKeyedTimeDelta) {
 
   Telemetry::AccumulateTimeDelta(Telemetry::TELEMETRY_TEST_KEYED_COUNT,
                                  "sample"_ns, start - delta, start);
-
-  
-  Telemetry::AccumulateTimeDelta(Telemetry::TELEMETRY_TEST_KEYED_COUNT,
-                                 "sample"_ns, start + delta, start);
 
   Telemetry::AccumulateTimeDelta(Telemetry::TELEMETRY_TEST_KEYED_COUNT,
                                  "sample"_ns, start, start);
