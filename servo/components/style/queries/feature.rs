30a297597e2574a1d405f57bc4a6de0f0af09454
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-02-16 03:00:14 +0000

    Bug 1946764 - Support non-boolean media query prefs via -moz-pref(). r=firefox-style-system-reviewers,desktop-theme-reviewers,settings-reviewers,tabbrowser-reviewers,places-reviewers,sidebar-reviewers,urlbar-reviewers,firefox-ai-ml-reviewers,dao,mconley,dshin,mak
    Allow querying int and string prefs too via functional syntax:
    
    ```
    media -moz-pref("pref-name", <value>) {
    ```
    
    No argument implies bool. Unfortunately stylelint is still unhappy with
    this so we need to keep the annoying comments...
    
    Differential Revision: https://phabricator.services.mozilla.com/D237275
    
    UltraBlame original commit: f1089681a6c6412455efbc04eb71674ce2abf9db

--- servo/components/style/queries/feature.rs
+++ servo/components/style/queries/feature.rs
@@ -6,7 +6,6 @@
 
 use crate::parser::ParserContext;
 use crate::values::computed::{self, CSSPixelLength, Ratio, Resolution};
-use crate::values::AtomString;
 use crate::Atom;
 use cssparser::Parser;
 use selectors::kleene_value::KleeneValue;
@@ -45,7 +44,6 @@ pub enum Evaluator {
     OptionalNumberRatio(QueryFeatureGetter<Option<Ratio>>),
     
     Resolution(QueryFeatureGetter<Resolution>),
-    String(fn(&computed::Context, Option<&AtomString>) -> KleeneValue),
     
     Enumerated {
         
