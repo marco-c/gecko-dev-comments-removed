a5eea7609fe45e56b110a6043af0163b79825748
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-02-16 02:42:16 +0000

    Bug 1938604 - [wdspec] Extend test suite for "userContexts" argument of "sessions.subscribe" command. r=webdriver-reviewers,jdescottes
    Differential Revision: https://phabricator.services.mozilla.com/D236694
    
    UltraBlame original commit: e60f809fa4bc5f38abac8db1240d97d708be9d3f

--- testing/web-platform/tests/webdriver/tests/bidi/session/unsubscribe/subscriptions.py
+++ testing/web-platform/tests/webdriver/tests/bidi/session/unsubscribe/subscriptions.py
@@ -5,7 +5,9 @@ from ... import create_console_api_message, recursive_compare
 pytestmark = pytest.mark.asyncio
 
 
-async def test_unsubscribe_with_subscription_id(bidi_session, top_context):
+async def test_unsubscribe_with_subscription_id(
+    bidi_session, top_context, wait_for_events
+):
     result = await bidi_session.session.subscribe(
         events=["log.entryAdded"], contexts=[top_context["context"]]
     )
@@ -13,19 +15,11 @@ async def test_unsubscribe_with_subscription_id(bidi_session, top_context):
     
     await bidi_session.session.unsubscribe(subscriptions=[result["subscription"]])
 
-    
-    events = []
-
-    async def on_event(method, data):
-        events.append(data)
-
-    remove_listener = bidi_session.add_event_listener("log.entryAdded", on_event)
-
-    
-    await create_console_api_message(bidi_session, top_context, "text1")
-    assert len(events) == 0
-
-    remove_listener()
+    with wait_for_events(["log.entryAdded"]) as waiter:
+        
+        await create_console_api_message(bidi_session, top_context, "text1")
+        events = await waiter.get_events(lambda events: len(events) >= 0)
+        assert len(events) == 0
 
 
 async def test_unsubscribe_with_multiple_subscription_ids(
@@ -69,7 +63,7 @@ async def test_unsubscribe_with_multiple_subscription_ids(
 
 
 async def test_unsubscribe_from_one_of_the_context(
-    bidi_session, top_context, new_tab, wait_for_event, wait_for_future_safe
+    bidi_session, top_context, new_tab, wait_for_events
 ):
     
     result_1 = await bidi_session.session.subscribe(
@@ -81,32 +75,93 @@ async def test_unsubscribe_from_one_of_the_context(
     
     await bidi_session.session.unsubscribe(subscriptions=[result_1["subscription"]])
 
-    
-    events = []
+    with wait_for_events(["log.entryAdded"]) as waiter:
+        
+        await create_console_api_message(bidi_session, top_context, "text1")
+        events = await waiter.get_events(lambda events: len(events) >= 0)
+        assert len(events) == 0
 
-    async def on_event(method, data):
-        events.append(data)
+        
+        expected_text = await create_console_api_message(bidi_session, new_tab, "text2")
+        events = await waiter.get_events(lambda events: len(events) >= 1)
 
-    remove_listener = bidi_session.add_event_listener("log.entryAdded", on_event)
+        assert len(events) == 1
+        recursive_compare(
+            {
+                "text": expected_text,
+            },
+            events[0][1],
+        )
 
-    
-    await create_console_api_message(bidi_session, top_context, "text1")
-    assert len(events) == 0
+    await bidi_session.session.unsubscribe(subscriptions=[result_2["subscription"]])
 
-    
-    on_entry_added = wait_for_event("log.entryAdded")
-    expected_text = await create_console_api_message(bidi_session, new_tab, "text2")
-    await wait_for_future_safe(on_entry_added)
 
-    assert len(events) == 1
-    recursive_compare(
-        {
-            "text": expected_text,
-        },
-        events[0],
+async def test_unsubscribe_from_closed_context(bidi_session):
+    new_context = await bidi_session.browsing_context.create(type_hint="tab")
+    result = await bidi_session.session.subscribe(
+        events=["log.entryAdded"], contexts=[new_context["context"]]
     )
 
-    remove_listener()
+    
+    await bidi_session.browsing_context.close(context=new_context["context"])
+
+    
+    await bidi_session.session.unsubscribe(subscriptions=[result["subscription"]])
+
+
+async def test_subscribe_twice_to_context_and_unsubscribe_once(
+    bidi_session, new_tab, wait_for_events
+):
+    
+    result_1 = await bidi_session.session.subscribe(
+        events=["log.entryAdded"], contexts=[new_tab["context"]]
+    )
+    result_2 = await bidi_session.session.subscribe(
+        events=["log.entryAdded"], contexts=[new_tab["context"]]
+    )
+
+    
+    await bidi_session.session.unsubscribe(subscriptions=[result_1["subscription"]])
+
+    with wait_for_events(["log.entryAdded"]) as waiter:
+        
+        expected_text = await create_console_api_message(bidi_session, new_tab, "text")
+        events = await waiter.get_events(lambda events: len(events) >= 1)
+
+        assert len(events) == 1
+        recursive_compare(
+            {
+                "text": expected_text,
+            },
+            events[0][1],
+        )
+
+    await bidi_session.session.unsubscribe(subscriptions=[result_2["subscription"]])
+
+
+async def test_subscribe_twice_to_globally_and_unsubscribe_once(
+    bidi_session, new_tab, wait_for_events
+):
+    
+    result_1 = await bidi_session.session.subscribe(events=["log.entryAdded"])
+    result_2 = await bidi_session.session.subscribe(events=["log.entryAdded"])
+
+    
+    await bidi_session.session.unsubscribe(subscriptions=[result_1["subscription"]])
+
+    with wait_for_events(["log.entryAdded"]) as waiter:
+        
+        expected_text = await create_console_api_message(bidi_session, new_tab, "text")
+        events = await waiter.get_events(lambda events: len(events) >= 1)
+
+        assert len(events) == 1
+        recursive_compare(
+            {
+                "text": expected_text,
+            },
+            events[0][1],
+        )
+
     await bidi_session.session.unsubscribe(subscriptions=[result_2["subscription"]])
 
 
@@ -161,7 +216,7 @@ async def test_unsubscribe_partially_from_one_event(bidi_session, top_context, i
 
 
 async def test_unsubscribe_partially_from_one_context(
-    bidi_session, top_context, new_tab, wait_for_event, wait_for_future_safe
+    bidi_session, top_context, new_tab, wait_for_events
 ):
     
     result = await bidi_session.session.subscribe(
@@ -172,41 +227,33 @@ async def test_unsubscribe_partially_from_one_context(
         events=["log.entryAdded"], contexts=[top_context["context"]]
     )
 
-    
-    events = []
+    with wait_for_events(["log.entryAdded"]) as waiter:
+        
+        await create_console_api_message(bidi_session, top_context, "text1")
+        events = await waiter.get_events(lambda events: len(events) >= 0)
+        assert len(events) == 0
 
-    async def on_event(method, data):
-        events.append(data)
+        
+        expected_text = await create_console_api_message(bidi_session, new_tab, "text2")
+        events = await waiter.get_events(lambda events: len(events) >= 1)
 
-    remove_listener = bidi_session.add_event_listener("log.entryAdded", on_event)
+        assert len(events) == 1
+        recursive_compare(
+            {
+                "text": expected_text,
+            },
+            events[0][1],
+        )
 
-    
-    await create_console_api_message(bidi_session, top_context, "text1")
-    assert len(events) == 0
+        
+        await bidi_session.session.unsubscribe(subscriptions=[result["subscription"]])
 
-    
-    on_entry_added = wait_for_event("log.entryAdded")
-    expected_text = await create_console_api_message(bidi_session, new_tab, "text2")
-    await wait_for_future_safe(on_entry_added)
+        
+        await create_console_api_message(bidi_session, new_tab, "text2")
 
-    assert len(events) == 1
-    recursive_compare(
-        {
-            "text": expected_text,
-        },
-        events[0],
-    )
-
-    
-    await bidi_session.session.unsubscribe(subscriptions=[result["subscription"]])
-
-    
-    await create_console_api_message(bidi_session, new_tab, "text2")
-
-    
-    assert len(events) == 1
-
-    remove_listener()
+        
+        events = await waiter.get_events(lambda events: len(events) >= 1)
+        assert len(events) == 1
 
 
 async def test_unsubscribe_with_event_and_subscriptions(bidi_session, new_tab, inline):
@@ -241,3 +288,344 @@ async def test_unsubscribe_with_event_and_subscriptions(bidi_session, new_tab, i
 
     remove_listener_domContentLoaded()
     remove_listener_load()
+
+
+async def test_unsubscribe_globally_with_one_context_subscription(
+    bidi_session, new_tab, wait_for_events
+):
+    
+    result_one_context = await bidi_session.session.subscribe(
+        events=["log.entryAdded"], contexts=[new_tab["context"]]
+    )
+    
+    result_globally = await bidi_session.session.subscribe(events=["log.entryAdded"])
+
+    
+    await bidi_session.session.unsubscribe(
+        subscriptions=[result_globally["subscription"]]
+    )
+
+    with wait_for_events(["log.entryAdded"]) as waiter:
+        
+        expected_text = await create_console_api_message(bidi_session, new_tab, "text")
+        events = await waiter.get_events(lambda events: len(events) >= 1)
+
+        assert len(events) == 1
+        recursive_compare(
+            {
+                "text": expected_text,
+            },
+            events[0][1],
+        )
+
+    
+    await bidi_session.session.unsubscribe(
+        subscriptions=[result_one_context["subscription"]]
+    )
+
+
+async def test_unsubscribe_from_one_context_with_global_subscription(
+    bidi_session, new_tab, wait_for_events
+):
+    
+    result_one_context = await bidi_session.session.subscribe(
+        events=["log.entryAdded"], contexts=[new_tab["context"]]
+    )
+    
+    result_globally = await bidi_session.session.subscribe(events=["log.entryAdded"])
+
+    
+    await bidi_session.session.unsubscribe(
+        subscriptions=[result_one_context["subscription"]]
+    )
+
+    with wait_for_events(["log.entryAdded"]) as waiter:
+        
+        expected_text = await create_console_api_message(bidi_session, new_tab, "text")
+        events = await waiter.get_events(lambda events: len(events) >= 1)
+
+        assert len(events) == 1
+        recursive_compare(
+            {
+                "text": expected_text,
+            },
+            events[0][1],
+        )
+
+    
+    await bidi_session.session.unsubscribe(
+        subscriptions=[result_globally["subscription"]]
+    )
+
+
+async def test_unsubscribe_globally_with_user_context_subscription(
+    bidi_session, wait_for_events, create_user_context
+):
+    user_context = await create_user_context()
+    new_context_in_user_context = await bidi_session.browsing_context.create(
+        user_context=user_context, type_hint="tab"
+    )
+    
+    result_user_context = await bidi_session.session.subscribe(
+        events=["log.entryAdded"], user_contexts=[user_context]
+    )
+    
+    result_globally = await bidi_session.session.subscribe(events=["log.entryAdded"])
+    
+    await bidi_session.session.unsubscribe(
+        subscriptions=[result_globally["subscription"]]
+    )
+
+    with wait_for_events(["log.entryAdded"]) as waiter:
+        
+        expected_text = await create_console_api_message(
+            bidi_session, new_context_in_user_context, "text"
+        )
+        events = await waiter.get_events(lambda events: len(events) >= 1)
+
+        assert len(events) == 1
+        recursive_compare(
+            {
+                "text": expected_text,
+            },
+            events[0][1],
+        )
+
+    
+    await bidi_session.session.unsubscribe(
+        subscriptions=[result_user_context["subscription"]]
+    )
+
+
+async def test_unsubscribe_globally_with_user_context_and_context_subscription(
+    bidi_session, wait_for_events, create_user_context
+):
+    user_context = await create_user_context()
+    new_context_in_user_context = await bidi_session.browsing_context.create(
+        user_context=user_context, type_hint="tab"
+    )
+
+    
+    result_user_context = await bidi_session.session.subscribe(
+        events=["log.entryAdded"], contexts=[new_context_in_user_context["context"]]
+    )
+    
+    result_user_context = await bidi_session.session.subscribe(
+        events=["log.entryAdded"], user_contexts=[user_context]
+    )
+    
+    result_globally = await bidi_session.session.subscribe(events=["log.entryAdded"])
+
+    
+    await bidi_session.session.unsubscribe(
+        subscriptions=[result_globally["subscription"]]
+    )
+
+    with wait_for_events(["log.entryAdded"]) as waiter:
+        
+        expected_text = await create_console_api_message(
+            bidi_session, new_context_in_user_context, "text"
+        )
+        events = await waiter.get_events(lambda events: len(events) >= 1)
+
+        assert len(events) == 1
+        recursive_compare(
+            {
+                "text": expected_text,
+            },
+            events[0][1],
+        )
+
+    
+    await bidi_session.session.unsubscribe(
+        subscriptions=[result_user_context["subscription"]]
+    )
+
+
+async def test_unsubscribe_from_user_context_with_global_subscription(
+    bidi_session, wait_for_events, create_user_context
+):
+    user_context = await create_user_context()
+    new_context_in_user_context = await bidi_session.browsing_context.create(
+        user_context=user_context, type_hint="tab"
+    )
+
+    
+    result_user_context = await bidi_session.session.subscribe(
+        events=["log.entryAdded"], user_contexts=[user_context]
+    )
+    
+    result_globally = await bidi_session.session.subscribe(events=["log.entryAdded"])
+
+    
+    await bidi_session.session.unsubscribe(
+        subscriptions=[result_user_context["subscription"]]
+    )
+
+    with wait_for_events(["log.entryAdded"]) as waiter:
+        
+        expected_text = await create_console_api_message(
+            bidi_session, new_context_in_user_context, "text"
+        )
+        events = await waiter.get_events(lambda events: len(events) >= 1)
+
+        assert len(events) == 1
+        recursive_compare(
+            {
+                "text": expected_text,
+            },
+            events[0][1],
+        )
+
+    
+    await bidi_session.session.unsubscribe(
+        subscriptions=[result_globally["subscription"]]
+    )
+
+
+async def test_unsubscribe_from_user_context(
+    bidi_session, create_user_context, wait_for_events
+):
+    user_context = await create_user_context()
+    new_context_in_user_context = await bidi_session.browsing_context.create(
+        user_context=user_context, type_hint="tab"
+    )
+
+    result = await bidi_session.session.subscribe(
+        events=["log.entryAdded"], user_contexts=[user_context]
+    )
+
+    
+    await bidi_session.session.unsubscribe(subscriptions=[result["subscription"]])
+
+    with wait_for_events(["log.entryAdded"]) as waiter:
+        
+        await create_console_api_message(
+            bidi_session, new_context_in_user_context, "text1"
+        )
+        events = await waiter.get_events(lambda events: len(events) >= 0)
+        assert len(events) == 0
+
+
+async def test_unsubscribe_from_user_context_twice(
+    bidi_session, create_user_context, wait_for_events
+):
+    user_context = await create_user_context()
+    new_context_in_user_context = await bidi_session.browsing_context.create(
+        user_context=user_context, type_hint="tab"
+    )
+
+    
+    result_1 = await bidi_session.session.subscribe(
+        events=["log.entryAdded"], user_contexts=[user_context]
+    )
+    result_2 = await bidi_session.session.subscribe(
+        events=["log.entryAdded"], user_contexts=[user_context]
+    )
+
+    
+    await bidi_session.session.unsubscribe(subscriptions=[result_1["subscription"]])
+
+    with wait_for_events(["log.entryAdded"]) as waiter:
+        
+        expected_text = await create_console_api_message(
+            bidi_session, new_context_in_user_context, "text"
+        )
+        events = await waiter.get_events(lambda events: len(events) >= 1)
+
+        assert len(events) == 1
+        recursive_compare(
+            {
+                "text": expected_text,
+            },
+            events[0][1],
+        )
+
+    
+    await bidi_session.session.unsubscribe(subscriptions=[result_2["subscription"]])
+
+
+async def test_unsubscribe_from_user_context_with_context_subscription(
+    bidi_session, wait_for_events, create_user_context
+):
+    user_context = await create_user_context()
+    new_context_in_user_context = await bidi_session.browsing_context.create(
+        user_context=user_context, type_hint="tab"
+    )
+
+    
+    result_context = await bidi_session.session.subscribe(
+        events=["log.entryAdded"], contexts=[new_context_in_user_context["context"]]
+    )
+    
+    result_user_context = await bidi_session.session.subscribe(
+        events=["log.entryAdded"], user_contexts=[user_context]
+    )
+
+    
+    await bidi_session.session.unsubscribe(
+        subscriptions=[result_user_context["subscription"]]
+    )
+
+    with wait_for_events(["log.entryAdded"]) as waiter:
+        
+        expected_text = await create_console_api_message(
+            bidi_session, new_context_in_user_context, "text"
+        )
+        events = await waiter.get_events(lambda events: len(events) >= 1)
+
+        assert len(events) == 1
+        recursive_compare(
+            {
+                "text": expected_text,
+            },
+            events[0][1],
+        )
+
+    
+    await bidi_session.session.unsubscribe(
+        subscriptions=[result_context["subscription"]]
+    )
+
+
+async def test_unsubscribe_from_context_with_user_context_subscription(
+    bidi_session, wait_for_events, create_user_context
+):
+    user_context = await create_user_context()
+    new_context_in_user_context = await bidi_session.browsing_context.create(
+        user_context=user_context, type_hint="tab"
+    )
+
+    
+    result_context = await bidi_session.session.subscribe(
+        events=["log.entryAdded"], contexts=[new_context_in_user_context["context"]]
+    )
+    
+    result_user_context = await bidi_session.session.subscribe(
+        events=["log.entryAdded"], user_contexts=[user_context]
+    )
+
+    
+    await bidi_session.session.unsubscribe(
+        subscriptions=[result_context["subscription"]]
+    )
+
+    with wait_for_events(["log.entryAdded"]) as waiter:
+        
+        expected_text = await create_console_api_message(
+            bidi_session, new_context_in_user_context, "text"
+        )
+        events = await waiter.get_events(lambda events: len(events) >= 1)
+
+        assert len(events) == 1
+        recursive_compare(
+            {
+                "text": expected_text,
+            },
+            events[0][1],
+        )
+
+    
+    await bidi_session.session.unsubscribe(
+        subscriptions=[result_user_context["subscription"]]
+    )
