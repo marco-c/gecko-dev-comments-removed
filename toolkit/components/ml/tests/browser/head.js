7232f77770728ce83ff509ceb817f827d6333851
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-02-16 01:16:42 +0000

    Bug 1945268 - Add NLP utils for Smart Tab Grouping. r=tarek
    Differential Revision: https://phabricator.services.mozilla.com/D236431
    
    UltraBlame original commit: cbe8864e756b664f6e86eab0b04a913488ffd5bd

--- toolkit/components/ml/tests/browser/head.js
+++ toolkit/components/ml/tests/browser/head.js
@@ -515,3 +515,10 @@ async function perfTest(
   Assert.ok(true);
   reportMetrics(journal);
 }
+
+
+
+
+function isEqualWithTolerance(A, B, epsilon = 0.000001) {
+  return Math.abs(Math.abs(A) - Math.abs(B)) < epsilon;
+}
