36cf2f8f8a181d78f1986065a1ca1f0e90f09a26
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-02-16 03:45:57 +0000

    Bug 1948355 - Enable adding a signature in PDF by default in Nightly and early Beta r=marco
    Differential Revision: https://phabricator.services.mozilla.com/D238302
    
    UltraBlame original commit: 154860b4453b1a88be8f804e464a6a77f5f69cc5

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -4111,3 +4111,10 @@ pref("privacy.query_stripping.strip_on_share.canDisable", true);
 
 pref("captchadetection.loglevel", "Warn");
 pref("captchadetection.actor.enabled", true);
+
+
+#ifdef EARLY_BETA_OR_EARLIER
+  pref("pdfjs.enableSignatureEditor", true);
+#else
+  pref("pdfjs.enableSignatureEditor", false);
+#endif
