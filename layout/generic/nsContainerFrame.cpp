541122f4fde813f32afb49518c26ecd92622443c
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-02-16 01:48:25 +0000

    Bug 1945944: Add flag to dump frame tree with only deterministic information for diffing. r=layout-reviewers,emilio
    Differential Revision: https://phabricator.services.mozilla.com/D236797
    
    UltraBlame original commit: fa0df14ef305afd7a864dfc239073232841af490

--- layout/generic/nsContainerFrame.cpp
+++ layout/generic/nsContainerFrame.cpp
@@ -3068,7 +3068,8 @@ void nsContainerFrame::List(FILE* out, const char* aPrefix,
                             ListFlags aFlags) const {
   nsCString str;
   ListGeneric(str, aPrefix, aFlags);
-  ExtraContainerFrameInfo(str);
+  ExtraContainerFrameInfo(str,
+                          aFlags.contains(ListFlag::OnlyListDeterministicInfo));
 
   
   fprintf_stderr(out, "%s <\n", str.get());
@@ -3120,8 +3121,9 @@ void nsContainerFrame::ListChildLists(FILE* aOut, const char* aPrefix,
 
     
     
-    const nsPrintfCString str("%s%s@%p <\n", aPrefix, ChildListName(listID),
-                              &GetChildList(listID));
+    nsCString str{nsPrintfCString("%s%s", aPrefix, ChildListName(listID))};
+    ListPtr(str, aFlags, &GetChildList(listID), "@");
+    str += " <\n";
     fprintf_stderr(aOut, "%s", str.get());
 
     for (nsIFrame* kid : list) {
@@ -3133,8 +3135,6 @@ void nsContainerFrame::ListChildLists(FILE* aOut, const char* aPrefix,
   }
 }
 
-void nsContainerFrame::ExtraContainerFrameInfo(nsACString& aTo) const {
-  (void)aTo;
-}
+void nsContainerFrame::ExtraContainerFrameInfo(nsACString&, bool) const {}
 
 #endif
