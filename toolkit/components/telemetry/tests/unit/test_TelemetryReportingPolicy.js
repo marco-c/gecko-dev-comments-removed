fe92246c5b71783e3ef0a553d9ef4e2c84441dbc
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-02-16 01:37:35 +0000

    Bug 1945672 - Part 2: Allow Nimbus feature to control policy notification. r=mviar,chutten
    This is the meat of the approach: we consult the Nimbus feature at
    `sessionstore-windows-restored` time and potentially use the Messaging
    System to notify the user.
    
    Differential Revision: https://phabricator.services.mozilla.com/D236623
    
    UltraBlame original commit: 1a5006cd4cff0f4e37b147ae16709779c2ac9721

--- toolkit/components/telemetry/tests/unit/test_TelemetryReportingPolicy.js
+++ toolkit/components/telemetry/tests/unit/test_TelemetryReportingPolicy.js
@@ -6,11 +6,17 @@
 
 "use strict";
 
-const { TelemetryReportingPolicy } = ChromeUtils.importESModule(
+ChromeUtils.defineESModuleGetters(this, {
+  ExperimentAPI: "resource://nimbus/ExperimentAPI.sys.mjs",
+  ExperimentFakes: "resource://testing-common/NimbusTestUtils.sys.mjs",
+  ExperimentManager: "resource://nimbus/lib/ExperimentManager.sys.mjs",
+  NimbusFeatures: "resource://nimbus/ExperimentAPI.sys.mjs",
+  UpdateUtils: "resource://gre/modules/UpdateUtils.sys.mjs",
+  sinon: "resource://testing-common/Sinon.sys.mjs",
+});
+
+const { Policy, TelemetryReportingPolicy } = ChromeUtils.importESModule(
   "resource://gre/modules/TelemetryReportingPolicy.sys.mjs"
-);
-const { UpdateUtils } = ChromeUtils.importESModule(
-  "resource://gre/modules/UpdateUtils.sys.mjs"
 );
 
 const TEST_CHANNEL = "TestChannelABC";
@@ -19,9 +25,6 @@ const PREF_MINIMUM_CHANNEL_POLICY_VERSION =
   TelemetryUtils.Preferences.MinimumPolicyVersion + ".channel-" + TEST_CHANNEL;
 
 function fakeShowPolicyTimeout(set, clear) {
-  let { Policy } = ChromeUtils.importESModule(
-    "resource://gre/modules/TelemetryReportingPolicy.sys.mjs"
-  );
   Policy.setShowInfobarTimeout = set;
   Policy.clearShowInfobarTimeout = clear;
 }
@@ -30,6 +33,14 @@ function fakeResetAcceptedPolicy() {
   Services.prefs.clearUserPref(TelemetryUtils.Preferences.AcceptedPolicyDate);
   Services.prefs.clearUserPref(
     TelemetryUtils.Preferences.AcceptedPolicyVersion
+  );
+}
+
+
+function fakeInteractWithModal() {
+  Services.obs.notifyObservers(
+    null,
+    "datareporting:notify-data-policy:interacted"
   );
 }
 
@@ -54,6 +65,23 @@ function setMinimumPolicyVersion(aNewPolicyVersion) {
   );
 }
 
+function unsetMinimumPolicyVersion() {
+  const CHANNEL_NAME = UpdateUtils.getUpdateChannel(false);
+  
+  const CHANNEL_DEPENDENT_PREF =
+    TelemetryUtils.Preferences.MinimumPolicyVersion +
+    ".channel-" +
+    CHANNEL_NAME;
+
+  
+  if (Services.prefs.getIntPref(CHANNEL_DEPENDENT_PREF, undefined)) {
+    Services.prefs.clearUserPref(CHANNEL_DEPENDENT_PREF);
+  }
+
+  
+  Services.prefs.clearUserPref(TelemetryUtils.Preferences.MinimumPolicyVersion);
+}
+
 add_setup(async function test_setup() {
   
   do_get_profile(true);
@@ -76,6 +104,10 @@ add_setup(async function test_setup() {
   );
 
   TelemetryReportingPolicy.setup();
+
+  
+  await ExperimentManager.onStartup();
+  await ExperimentAPI.ready();
 });
 
 add_task(
@@ -409,4 +441,226 @@ add_task(async function test_canSend() {
   );
 
   await PingServer.stop();
+});
+
+add_task(async function test_feature_prefs() {
+  
+  
+  function assertPrefs(
+    currentPolicyVersion,
+    minimumPolicyVersion,
+    firstRunURL
+  ) {
+    Assert.equal(
+      Services.prefs.getIntPref(
+        TelemetryUtils.Preferences.CurrentPolicyVersion
+      ),
+      currentPolicyVersion,
+      "datareporting.policy.currentPolicyVersion is set"
+    );
+
+    Assert.equal(
+      Services.prefs.getIntPref(
+        TelemetryUtils.Preferences.MinimumPolicyVersion
+      ),
+      minimumPolicyVersion,
+      "datareporting.policy.minimumPolicyVersion is set"
+    );
+
+    Assert.equal(
+      Services.prefs.getCharPref(TelemetryUtils.Preferences.FirstRunURL),
+      firstRunURL,
+      "datareporting.policy.firstRunURL is set"
+    );
+  }
+
+  unsetMinimumPolicyVersion();
+  Services.prefs.clearUserPref(TelemetryUtils.Preferences.CurrentPolicyVersion);
+
+  let doCleanup = await ExperimentFakes.enrollWithFeatureConfig(
+    {
+      featureId: NimbusFeatures.preonboarding.featureId,
+      value: {
+        enabled: true,
+        currentPolicyVersion: 900,
+        minimumPolicyVersion: 899,
+        firstRunURL: "http://mochi.test/v900",
+      },
+    },
+    { isRollout: false }
+  );
+
+  Assert.ok(NimbusFeatures.preonboarding.getVariable("enabled"));
+
+  
+  TelemetryReportingPolicy.reset();
+
+  Assert.ok(
+    !Services.prefs.prefHasUserValue(
+      TelemetryUtils.Preferences.CurrentPolicyVersion
+    ),
+    "datareporting.policy.currentPolicyVersion is not set"
+  );
+
+  Assert.ok(
+    !Services.prefs.prefHasUserValue(
+      TelemetryUtils.Preferences.MinimumPolicyVersion
+    ),
+    "datareporting.policy.minimumPolicyVersion is not set"
+  );
+
+  Assert.ok(
+    !Services.prefs.prefHasUserValue(TelemetryUtils.Preferences.FirstRunURL),
+    "datareporting.policy.firstRunURL is not set"
+  );
+
+  
+  await Policy.fakeSessionRestoreNotification();
+  assertPrefs(900, 899, "http://mochi.test/v900");
+
+  
+  doCleanup();
+  Assert.ok(!NimbusFeatures.preonboarding.getVariable("enabled"));
+  assertPrefs(900, 899, "http://mochi.test/v900");
+
+  
+  doCleanup = await ExperimentFakes.enrollWithFeatureConfig(
+    {
+      featureId: NimbusFeatures.preonboarding.featureId,
+      value: {
+        enabled: true,
+        currentPolicyVersion: 901,
+        minimumPolicyVersion: 900,
+        firstRunURL: "http://mochi.test/v901",
+      },
+    },
+    { isRollout: false }
+  );
+  Assert.ok(NimbusFeatures.preonboarding.getVariable("enabled"));
+  assertPrefs(900, 899, "http://mochi.test/v900");
+  doCleanup();
+});
+
+async function doOneModalFlow(version) {
+  let doCleanup = await ExperimentFakes.enrollWithFeatureConfig(
+    {
+      featureId: NimbusFeatures.preonboarding.featureId,
+      value: {
+        enabled: true,
+        currentPolicyVersion: version,
+        minimumPolicyVersion: version,
+        firstRunURL: `http://mochi.test/v${version}`,
+        
+        screens: [{ id: "test" }],
+      },
+    },
+    { isRollout: false }
+  );
+
+  let displayStub = sinon.stub(Policy, "showModal").returns(true);
+
+  
+  TelemetryReportingPolicy.reset();
+  await Policy.fakeSessionRestoreNotification();
+
+  Assert.equal(displayStub.callCount, 1, "showModal is invoked");
+
+  Assert.equal(
+    TelemetryReportingPolicy.testIsUserNotified(),
+    false,
+    "Before interaction, the user should be reported as not notified"
+  );
+
+  let completed = false;
+  let p = TelemetryReportingPolicy.ensureUserIsNotified().then(
+    () => (completed = true)
+  );
+
+  Assert.equal(
+    completed,
+    false,
+    "The notification promise should not resolve before the user interacts"
+  );
+
+  fakeInteractWithModal();
+
+  await p;
+
+  Assert.equal(
+    completed,
+    true,
+    "The notification promise should resolve after user interacts"
+  );
+
+  Assert.equal(
+    TelemetryReportingPolicy.testIsUserNotified(),
+    true,
+    "After interaction, the state should be notified."
+  );
+
+  doCleanup();
+
+  sinon.restore();
+}
+
+add_task(async function test_modal_flow_before_notification() {
+  
+  
+  
+  
+
+  fakeResetAcceptedPolicy();
+  Services.prefs.clearUserPref(TelemetryUtils.Preferences.FirstRun);
+
+  await doOneModalFlow(900);
+
+  
+  Assert.equal(
+    Services.prefs.getIntPref(TelemetryUtils.Preferences.AcceptedPolicyVersion),
+    900
+  );
+});
+
+add_task(async function test_modal_flow_after_notification() {
+  
+  
+  
+  
+
+  unsetMinimumPolicyVersion();
+  Services.prefs.clearUserPref(TelemetryUtils.Preferences.CurrentPolicyVersion);
+
+  fakeResetAcceptedPolicy();
+  Services.prefs.setBoolPref(TelemetryUtils.Preferences.FirstRun, false);
+
+  TelemetryReportingPolicy.reset();
+
+  
+  fakeNow(2012, 11, 11);
+  TelemetryReportingPolicy.testInfobarShown();
+  Assert.ok(
+    TelemetryReportingPolicy.testIsUserNotified(),
+    "User is notified after seeing the legacy infobar"
+  );
+
+  Assert.ok(
+    Services.prefs.getIntPref(
+      TelemetryUtils.Preferences.AcceptedPolicyVersion
+    ) < 900,
+    "Before, the user has not accepted experiment/rollout version"
+  );
+
+  
+  await doOneModalFlow(900);
+
+  Assert.ok(
+    TelemetryReportingPolicy.testIsUserNotified(),
+    "User is notified after seeing the experiment modal"
+  );
+
+  Assert.equal(
+    Services.prefs.getIntPref(TelemetryUtils.Preferences.AcceptedPolicyVersion),
+    900,
+    "After, the user has accepted the experiment/rollout version."
+  );
 });
