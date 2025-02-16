c181c95625f74130b7fada0a7359482402683e4b
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-02-16 01:52:26 +0000

    Backed out changeset 21c27f2ca5fa (bug 1779688) for causing mochitests failures in browser_copy_link_to_highlight.js. CLOSED TREE
    UltraBlame original commit: 0b105eff0fb9132fefc5913305569a0b69550a51

--- browser/base/content/browser-context.js
+++ browser/base/content/browser-context.js
@@ -297,15 +297,6 @@ document.addEventListener(
         case "context-media-eme-learnmore":
           gContextMenu.drmLearnMore(event);
           break;
-        case "context-copy-link-to-highlight":
-          gContextMenu.copyLinkToHighlight();
-          break;
-        case "context-copy-clean-link-to-highlight":
-          gContextMenu.copyLinkToHighlight( true);
-          break;
-        case "context-remove-all-highlights":
-          gContextMenu.removeAllTextFragments();
-          break;
       }
     });
     contextMenuPopup.addEventListener("popupshowing", event => {
@@ -320,13 +311,6 @@ document.addEventListener(
 
           if (!IS_WEBEXT_PANELS) {
             updateEditUIVisibility();
-          }
-
-          
-          
-          
-          if (gContextMenu.isContentSelected) {
-            gContextMenu.getTextDirective();
           }
           break;
         }
