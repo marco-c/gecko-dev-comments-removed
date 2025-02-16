40d2a339e56b7093be96b5b475a0a17f0a61cc2d
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-02-16 01:36:42 +0000

    Bug 1946016 - Remove macOS special case too. r=dao,tabbrowser-reviewers
    Just tested, doesn't change behavior.
    
    Differential Revision: https://phabricator.services.mozilla.com/D236871
    
    UltraBlame original commit: 4eb8702042393838aefcfad9aa3fdbac762f9d0f

--- browser/components/tabbrowser/content/tabs.js
+++ browser/components/tabbrowser/content/tabs.js
@@ -1398,11 +1398,6 @@
         winHeight /= screenCssToDesktopScale;
 
         let props = { screenX: left, screenY: top, suppressanimation: 1 };
-        if (AppConstants.platform == "macosx") {
-          
-          props.outerWidth = winWidth;
-          props.outerHeight = winHeight;
-        }
         gBrowser.replaceTabsWithWindow(draggedTab, props);
       }
       event.stopPropagation();
