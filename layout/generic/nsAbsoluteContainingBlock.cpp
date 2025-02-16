4a90436b9afb72f715ad598dff7297e55dca6919
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-02-16 03:05:15 +0000

    Bug 1942715: Part 5 - Remove Maybe* optimizations for insets, always working with anchor-resolved insets. r=firefox-style-system-reviewers,emilio
    Reasoning with these functions gets confusing, because anchor
    functions can become percentage as well as auto.
    Defer optimization for now.
    
    Differential Revision: https://phabricator.services.mozilla.com/D237098
    
    UltraBlame original commit: d9ce2be09cd05e7d04e8bece15fa0e6b5f4fdaab

--- layout/generic/nsAbsoluteContainingBlock.cpp
+++ layout/generic/nsAbsoluteContainingBlock.cpp
@@ -285,10 +285,11 @@ static inline bool IsFixedPaddingSize(const LengthPercentage& aCoord) {
 static inline bool IsFixedMarginSize(const StyleMargin& aCoord) {
   return aCoord.ConvertsToLength();
 }
-static inline bool IsFixedOffset(const StyleInset& aInset) {
+static inline bool IsFixedOffset(
+    const nsStylePosition::AnchorResolvedInset& aInset) {
   
   
-  return aInset.ConvertsToLength();
+  return aInset->ConvertsToLength();
 }
 
 bool nsAbsoluteContainingBlock::FrameDependsOnContainer(nsIFrame* f,
@@ -330,6 +331,7 @@ bool nsAbsoluteContainingBlock::FrameDependsOnContainer(nsIFrame* f,
       return true;
     }
   }
+  const auto positionProperty = f->StyleDisplay()->mPosition;
   if (wm.IsVertical() ? aCBWidthChanged : aCBHeightChanged) {
     
     
@@ -342,8 +344,11 @@ bool nsAbsoluteContainingBlock::FrameDependsOnContainer(nsIFrame* f,
     
     if ((pos->BSizeDependsOnContainer(wm) &&
          !(pos->BSize(wm).IsAuto() &&
-           pos->mOffset.Get(LogicalSide::BEnd, wm).MaybeAuto() &&
-           !pos->mOffset.Get(LogicalSide::BStart, wm).MaybeAuto())) ||
+           pos->GetAnchorResolvedInset(LogicalSide::BEnd, wm, positionProperty)
+               ->IsAuto() &&
+           !pos->GetAnchorResolvedInset(LogicalSide::BStart, wm,
+                                        positionProperty)
+                ->IsAuto())) ||
         pos->MinBSizeDependsOnContainer(wm) ||
         pos->MaxBSizeDependsOnContainer(wm) ||
         !IsFixedPaddingSize(padding->mPadding.GetBStart(wm)) ||
@@ -365,7 +370,8 @@ bool nsAbsoluteContainingBlock::FrameDependsOnContainer(nsIFrame* f,
   
   
   if (aCBWidthChanged) {
-    if (!IsFixedOffset(pos->mOffset.Get(eSideLeft))) {
+    if (!IsFixedOffset(
+            pos->GetAnchorResolvedInset(eSideLeft, positionProperty))) {
       return true;
     }
     
@@ -377,17 +383,18 @@ bool nsAbsoluteContainingBlock::FrameDependsOnContainer(nsIFrame* f,
     
     if ((wm.GetInlineDir() == WritingMode::InlineDir::RTL ||
          wm.GetBlockDir() == WritingMode::BlockDir::RL) &&
-        !pos->mOffset.Get(eSideRight).MaybeAuto()) {
+        !pos->GetAnchorResolvedInset(eSideRight, positionProperty)->IsAuto()) {
       return true;
     }
   }
   if (aCBHeightChanged) {
-    if (!IsFixedOffset(pos->mOffset.Get(eSideTop))) {
+    if (!IsFixedOffset(
+            pos->GetAnchorResolvedInset(eSideTop, positionProperty))) {
       return true;
     }
     
     if (wm.GetInlineDir() == WritingMode::InlineDir::BTT &&
-        !pos->mOffset.Get(eSideBottom).MaybeAuto()) {
+        !pos->GetAnchorResolvedInset(eSideBottom, positionProperty)->IsAuto()) {
       return true;
     }
   }
