d763d8b5049461cd8b08ae711a7a1517a635c5f6
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-02-16 01:06:09 +0000

    Bug 1945142: Skip canvas randomization if caller is chrome. r=tjr,jgilbert
    Differential Revision: https://phabricator.services.mozilla.com/D236358
    
    UltraBlame original commit: 68cf604c60347b93bb1d5ed7c570bb52c69e80a8

--- dom/canvas/CanvasRenderingContext2D.cpp
+++ dom/canvas/CanvasRenderingContext2D.cpp
@@ -2104,10 +2104,25 @@ UniquePtr<uint8_t[]> CanvasRenderingContext2D::GetImageBuffer(
   mBufferProvider->ReturnSnapshot(snapshot.forget());
 
   if (ret && ShouldResistFingerprinting(RFPTarget::CanvasRandomization)) {
-    nsRFPService::RandomizePixels(
-        GetCookieJarSettings(), ret.get(), out_imageSize->width,
-        out_imageSize->height, out_imageSize->width * out_imageSize->height * 4,
-        SurfaceFormat::A8R8G8B8_UINT32);
+    bool randomize = true;
+    
+    
+    
+    if (StaticPrefs::
+            privacy_resistFingerprinting_randomization_canvas_disable_for_chrome()) {
+      bool isCallerChrome =
+          NS_IsMainThread() && nsContentUtils::IsCallerChrome();
+      if (isCallerChrome) {
+        randomize = false;
+      }
+    }
+    if (randomize) {
+      nsRFPService::RandomizePixels(
+          GetCookieJarSettings(), ret.get(), out_imageSize->width,
+          out_imageSize->height,
+          out_imageSize->width * out_imageSize->height * 4,
+          SurfaceFormat::A8R8G8B8_UINT32);
+    }
   }
 
   return ret;
