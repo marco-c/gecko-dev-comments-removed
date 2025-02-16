ee0f9ae47597bf77774d8ed3e006bf85fc03422a
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-02-16 01:58:56 +0000

    Backed out 2 changesets (bug 1945716) for causing dt failures  `browser_codemirror.js` CLOSED TREE
    Backed out changeset 85125b9112b8 (bug 1945716)
    Backed out changeset 9424d7ad4424 (bug 1945716)
    
    UltraBlame original commit: 70c92c403a46832298efa75850df4d78b6e41de1

--- devtools/client/shared/sourceeditor/codemirror/addon/search/match-highlighter.js
+++ devtools/client/shared/sourceeditor/codemirror/addon/search/match-highlighter.js
@@ -21,7 +21,7 @@
 
 (function(mod) {
   if (typeof exports == "object" && typeof module == "object") 
-    mod(require("../../lib/codemirror"), require("./matchesonscrollbar"));
+    mod(require("resource://devtools/client/shared/sourceeditor/codemirror/lib/codemirror.js"), require("resource://devtools/client/shared/sourceeditor/codemirror/addon/search/matchesonscrollbar.js"));
   else if (typeof define == "function" && define.amd) 
     define(["../../lib/codemirror", "./matchesonscrollbar"], mod);
   else 
@@ -90,9 +90,7 @@
     var state = cm.state.matchHighlighter;
     cm.addOverlay(state.overlay = makeOverlay(query, hasBoundary, style));
     if (state.options.annotateScrollbar && cm.showMatchesOnScrollbar) {
-      var searchFor = hasBoundary ? new RegExp((/\w/.test(query.charAt(0)) ? "\\b" : "") +
-                                               query.replace(/[\\\[.+*?(){|^$]/g, "\\$&") +
-                                               (/\w/.test(query.charAt(query.length - 1)) ? "\\b" : "")) : query;
+      var searchFor = hasBoundary ? new RegExp("\\b" + query.replace(/[\\\[.+*?(){|^$]/g, "\\$&") + "\\b") : query;
       state.matchesonscroll = cm.showMatchesOnScrollbar(searchFor, false,
         {className: "CodeMirror-selection-highlight-scrollbar"});
     }
