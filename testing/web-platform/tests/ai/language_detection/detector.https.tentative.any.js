79f22635c47562351ca16c16c1c7725555368579
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-02-16 01:06:21 +0000

    Bug 1944637 [wpt PR 50369] - Language Detector: Reject when called with aborted signal, a=testonly
    Automatic update from web-platform-tests
    Language Detector: Reject when called with aborted signal
    
    Rejects the promise returned by AILanguageDetector.detect() when the
    signal passed in is already aborted.
    
    Bug: 391713293
    Change-Id: Icbeb5648458397e92fa7ec0ba8e14fd31ddd1754
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6210161
    Commit-Queue: Nathan Memmott <memmottchromium.org>
    Reviewed-by: Jiacheng Guo <gjcgoogle.com>
    Cr-Commit-Position: refs/heads/main{#1412982}
    
    --
    
    wpt-commits: 344a03be82e877514add8ca332c790ff2e707a40
    wpt-pr: 50369
    
    UltraBlame original commit: 5674d0e69e082252e26ae12dbf290fc1ff401dd7

--- testing/web-platform/tests/ai/language_detection/detector.https.tentative.any.js
+++ testing/web-platform/tests/ai/language_detection/detector.https.tentative.any.js
@@ -5,11 +5,21 @@
 
 promise_test(async t => {
   const detector = await ai.languageDetector.create();
-  const results = await detector.detect("this string is in English");
+  const results = await detector.detect('this string is in English');
   
-  assert_equals(results[0].detectedLanguage, "en");
+  assert_equals(results[0].detectedLanguage, 'en');
   
   for (let i = 0; i < results.length - 1; i++) {
     assert_greater_than_equal(results[i].confidence, results[i + 1].confidence);
   }
-});
+}, 'Simple AILanguageDetector.detect() call');
+
+promise_test(async t => {
+  const controller = new AbortController();
+  controller.abort();
+
+  const detector = await ai.languageDetector.create();
+  const detectPromise = await detector.detect('this string is in English');
+
+  await promise_rejects_dom(t, 'AbortError', detectPromise);
+}, 'AILanguageDetector.detect() call with an aborted signal.')
