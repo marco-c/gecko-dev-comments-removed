b3d3e9552379834130314e1b8212a6f45ed8089d
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-02-16 01:18:14 +0000

    Bug 1880913 - Part 6: Remove BrowserSearch and BrowserSearchTelemetry from browser.js. r=Standard8
    Differential Revision: https://phabricator.services.mozilla.com/D236218
    
    UltraBlame original commit: b76ccb2dff6e2480e7935f21dc7e88e0d4bbf59b

--- browser/components/search/content/autocomplete-popup.js
+++ browser/components/search/content/autocomplete-popup.js
@@ -7,6 +7,8 @@
 
 {
   ChromeUtils.defineESModuleGetters(this, {
+    BrowserSearchTelemetry:
+      "resource:///modules/BrowserSearchTelemetry.sys.mjs",
     BrowserUtils: "resource://gre/modules/BrowserUtils.sys.mjs",
     SearchOneOffs: "resource:///modules/SearchOneOffs.sys.mjs",
   });
