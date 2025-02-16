7ce7f888418e59e2f373b5f647af0af67eb068e7
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-02-16 01:32:27 +0000

    Backed out changeset 1be8354cedcd (bug 1930201) for causing mochitests failures. CLOSED TREE
    UltraBlame original commit: d7537bba53a76978e545aa8012d567393d3574db

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -2076,7 +2076,6 @@ pref("sidebar.revamp.round-content-area", false);
 #endif
 pref("sidebar.animation.enabled", true);
 pref("sidebar.animation.duration-ms", 200);
-pref("sidebar.animation.expand-on-hover.duration-ms", 400);
 pref("sidebar.main.tools", "aichat,syncedtabs,history");
 pref("sidebar.verticalTabs", false);
 pref("sidebar.visibility", "always-show");
@@ -2084,7 +2083,6 @@ pref("sidebar.visibility", "always-show");
 
 
 pref("sidebar.backupState", "{}");
-pref("sidebar.expandOnHover", false);
 
 pref("browser.ml.chat.enabled", true);
 pref("browser.ml.chat.hideLocalhost", true);
--- browser/components/tabbrowser/content/tabbrowser.js
+++ browser/components/tabbrowser/content/tabbrowser.js
@@ -2197,18 +2197,6 @@
       browserSidebarContainer.className = "browserSidebarContainer";
       browserSidebarContainer.appendChild(browserContainer);
 
-      let visibility = Services.prefs.getStringPref(
-        "sidebar.visibility",
-        "always-show"
-      );
-      let expandOnHover = Services.prefs.getBoolPref(
-        "sidebar.expandOnHover",
-        false
-      );
-      if (visibility === "expand-on-hover" && expandOnHover) {
-        SidebarController.toggleExpandOnHover(true);
-      }
-
       
       
       if (!uriIsAboutBlank || skipLoad) {
