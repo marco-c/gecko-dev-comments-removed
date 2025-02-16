20385ecbd0e9a84489869573a4f6790c876395c0
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-02-16 02:03:59 +0000

    Bug 1946529 [wpt PR 50536] - IDB WPTs: Extend idbobjectstore_opencursor related tests to workers, a=testonly
    Automatic update from web-platform-tests
    IDB WPTs: Extend idbobjectstore_opencursor related tests to workers
    
    The update modifies idbobjectstore_opencursor related WPTs to run not
    only in window environments but also on dedicated, service, and shared
    workers.
    
    Change-Id: Ibc1d96253b65182cf94ce4769389b127149d89e4
    Bug: 41455766
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6201393
    Auto-Submit: Garima Chadha <garimachadhamicrosoft.com>
    Commit-Queue: Rahul Singh <rahsinmicrosoft.com>
    Reviewed-by: Steve Becker <stevebemicrosoft.com>
    Reviewed-by: Rahul Singh <rahsinmicrosoft.com>
    Cr-Commit-Position: refs/heads/main{#1416897}
    
    --
    
    wpt-commits: 764632a48b420d30868fd9cf8c735f141f74e46b
    wpt-pr: 50536
    
    UltraBlame original commit: 3fc15724ce1487dca4528694da82aaef589ae720

---
+++ testing/web-platform/tests/IndexedDB/idbobjectstore_openCursor.any.js
@@ -0,0 +1,35 @@
+
+
+
+
+'use strict';
+
+async_test(t => {
+  let db;
+  const open_rq = createdb(t);
+
+  open_rq.onupgradeneeded = t.step_func(e => {
+    db = e.target.result;
+    let store = db.createObjectStore('store');
+
+    for (let i = 0; i < 100; i++)
+      store.add('record_' + i, i);
+  });
+
+  open_rq.onsuccess = t.step_func(e => {
+    let count = 0;
+    let txn = db.transaction('store', 'readonly');
+
+    txn.objectStore('store').openCursor().onsuccess = t.step_func(function(e) {
+      if (e.target.result) {
+        count += 1;
+        e.target.result.continue();
+      }
+    })
+
+    txn.oncomplete = t.step_func(function() {
+      assert_equals(count, 100);
+      t.done();
+    })
+  });
+});
