51b058cde6516ca6a6a9f7c1d98d35dcf0edac6c
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-02-16 01:06:16 +0000

    Bug 1945228: Change mathml values to strings. r=tjr
    Differential Revision: https://phabricator.services.mozilla.com/D236381
    
    UltraBlame original commit: d5ef5ac637293cd07804e669be07276620b03b39

--- toolkit/components/resistfingerprinting/content/usercharacteristics.js
+++ toolkit/components/resistfingerprinting/content/usercharacteristics.js
@@ -959,7 +959,7 @@ async function populateMathML() {
 
   return mathElements.reduce((acc, el) => {
     
-    acc["mathml" + el.id] = el.getBoundingClientRect().width * 10 ** 15;
+    acc["mathml" + el.id] = el.getBoundingClientRect().width.toString();
     return acc;
   }, {});
 }
