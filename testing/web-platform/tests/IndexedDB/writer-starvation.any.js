fabd9588ebd555d384419d4baa1936001a6b6b2d
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-02-16 01:46:19 +0000

    Bug 1945991 [wpt PR 50506] - IDB WPTs: Extend several IDB WPTs to run on workers (Part 8), a=testonly
    Automatic update from web-platform-tests
    IDB WPTs: Extend several IDB WPTs to run on workers (Part 8)
    
    This set of IndexedDB WPTs currently only run in a window environment.
    This change extends them to also run in dedicated, shared, and service
    worker environments.
    
    Bug: 41455766
    Change-Id: I76a29b8b5a583f1bdbed2cba1c39981a05aedb48
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6222486
    Reviewed-by: Steve Becker <stevebemicrosoft.com>
    Commit-Queue: Rahul Singh <rahsinmicrosoft.com>
    Cr-Commit-Position: refs/heads/main{#1415963}
    
    --
    
    wpt-commits: ebccc9d428a4dacc3216b76514b2956f454aa8b9
    wpt-pr: 50506
    
    UltraBlame original commit: 7ea8389c434c1c81b8527e1cf36da3bc9b9282c1

---
+++ testing/web-platform/tests/IndexedDB/writer-starvation.any.js
@@ -0,0 +1,91 @@
+
+
+
+
+
+'use strict';
+
+async_test(t => {
+  let db;
+  let read_request_count = 0;
+  let read_success_count = 0;
+  let write_request_count = 0;
+  let write_success_count = 0;
+  const RQ_COUNT = 25;
+
+  const open_rq = createdb(t);
+  open_rq.onupgradeneeded = t.step_func((e) => {
+    db = e.target.result;
+    db.createObjectStore('s').add('1', 1);
+  });
+
+  open_rq.onsuccess = t.step_func((e) => {
+    let i = 0;
+
+    
+    for (i = 0; i < RQ_COUNT; i++) {
+      read_request_count++;
+
+      db.transaction('s', 'readonly').objectStore('s').get(1).onsuccess =
+          t.step_func((e) => {
+            read_success_count++;
+            assert_equals(e.target.transaction.mode, 'readonly');
+          });
+    }
+
+    t.step(loop);
+
+    function loop() {
+      read_request_count++;
+
+      db.transaction('s', 'readonly').objectStore('s').get(1).onsuccess =
+          t.step_func((e) => {
+            read_success_count++;
+            assert_equals(e.target.transaction.mode, 'readonly');
+
+            if (read_success_count >= RQ_COUNT && write_request_count == 0) {
+              write_request_count++;
+
+              db.transaction('s', 'readwrite')
+                  .objectStore('s')
+                  .add('written', read_request_count)
+                  .onsuccess = t.step_func((e) => {
+                write_success_count++;
+                assert_equals(e.target.transaction.mode, 'readwrite');
+                assert_equals(
+                    e.target.result, read_success_count,
+                    'write cb came before later read cb\'s');
+              });
+
+              
+              for (i = 0; i < 5; i++) {
+                read_request_count++;
+
+                db.transaction('s', 'readonly')
+                    .objectStore('s')
+                    .get(1)
+                    .onsuccess = t.step_func((e) => {
+                  read_success_count++;
+                });
+              }
+            }
+          });
+
+      if (read_success_count < RQ_COUNT + 5) {
+        step_timeout(t.step_func(loop), write_request_count ? 1000 : 100);
+      } else {
+        
+        db.transaction('s', 'readonly').objectStore('s').count().onsuccess =
+            t.step_func(() => {
+              step_timeout(t.step_func(finish), 100);
+            });
+      }
+    }
+  });
+
+  function finish() {
+    assert_equals(read_request_count, read_success_count, 'read counts');
+    assert_equals(write_request_count, write_success_count, 'write counts');
+    t.done();
+  }
+}, 'IDB read requests should not starve write requests');
