300810d56f6d21e54afc8b0e04ba7d6ab4b8e04c
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-03-01 01:56:41 +0000

    Bug 421436 - Remove the hack that forces all <br> frames to be 1 app unit wide. r=emilio
    That is, after this patch, the BRFrame's inline-size becomes zero.
    
    This patch is based on the previous attempt [1] in this bug, and removes the
    code added in Bug 1242781.
    
    The test_bug1216483.html change is worth an explanation. Each sub-test verifies
    the blinking cursor (caret) position after pressing down arrow key in an
    contenteditable container. The expected position is to the right of the letter
    "a". Before this patch, the `(anchorNode, anchorOffset)` was
    `("element with '.second' style", 1)`. After this patch, it becomes
    `("a", 1)`, which is a valid representation of the caret position and
    matches Google Chrome's behavior.
    
    [1] https://hg.mozilla.org/mozilla-central/rev/c3f1ab75ea51
    
    Differential Revision: https://phabricator.services.mozilla.com/D238162
    
    UltraBlame original commit: 6448e1fd65ee729b1db0b5d570e5c52a5fe75e15

--- layout/generic/BRFrame.cpp
+++ layout/generic/BRFrame.cpp
@@ -146,15 +146,6 @@ void BRFrame::Reflow(nsPresContext* aPresContext, ReflowOutput& aMetrics,
       } else {
         aMetrics.SetBlockStartAscent(aMetrics.BSize(wm) = 0);
       }
-
-      
-      
-      
-      
-      
-      
-      
-      finalSize.ISize(wm) = 1;
     }
 
     
@@ -181,8 +172,6 @@ void BRFrame::AddInlineMinISize(const IntrinsicSizeInput& aInput,
 void BRFrame::AddInlinePrefISize(const IntrinsicSizeInput& aInput,
                                  InlinePrefISizeData* aData) {
   if (!GetParent()->Style()->ShouldSuppressLineBreak()) {
-    
-    aData->mCurrentLine += 1;
     aData->ForceBreak();
   }
 }
