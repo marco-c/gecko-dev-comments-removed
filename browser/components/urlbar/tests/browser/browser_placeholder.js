5032b8a752d6903eaf57541ac2a9fc3c519fb202
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-03-01 02:24:23 +0000

    Bug 1936557: Remove delayUpdate parameter and #searchInitComplete flag r=search-reviewers,daleharvey,Standard8
    Depends on D239701
    
    Differential Revision: https://phabricator.services.mozilla.com/D239702
    
    UltraBlame original commit: db0fee0b120508fc11950317e4b2962da3723a83

--- browser/components/urlbar/tests/browser/browser_placeholder.js
+++ browser/components/urlbar/tests/browser/browser_placeholder.js
@@ -126,87 +126,55 @@ add_task(async function test_change_default_engine_updates_placeholder() {
 });
 
 add_task(async function test_delayed_update_placeholder() {
-  
-  
-  
-  
-  Services.obs.removeObserver(gURLBar, "browser-search-engine-modified");
+  await doDelayedUpdatePlaceholderTest({ defaultEngine: extraEngine });
+  await doDelayedUpdatePlaceholderTest({ defaultEngine: appDefaultEngine });
+});
 
-  
-  
-  let urlTab = await BrowserTestUtils.openNewForegroundTab(
-    gBrowser,
-    "about:mozilla"
-  );
-  tabs.push(urlTab);
-
-  
-  let blankTab = await BrowserTestUtils.openNewForegroundTab(gBrowser);
-  tabs.push(blankTab);
-
+async function doDelayedUpdatePlaceholderTest({ defaultEngine }) {
+  info("Set default search engine");
   await Services.search.setDefault(
-    extraEngine,
+    defaultEngine,
     Ci.nsISearchService.CHANGE_REASON_UNKNOWN
   );
-  
-  gURLBar._updatePlaceholder(extraEngine.name, true);
+
+  info("Clear placeholder cache");
+  Services.prefs.clearUserPref("browser.urlbar.placeholderName");
+
+  info("Open a new window");
+  let newWin = await BrowserTestUtils.openNewBrowserWindow();
 
   Assert.equal(
-    gURLBar.placeholder,
-    expectedString,
-    "Placeholder should be unchanged."
-  );
-
-  
-  await BrowserTestUtils.switchTab(gBrowser, urlTab);
-
-  await TestUtils.waitForCondition(
-    () => gURLBar.placeholder == noEngineString,
-    "The placeholder should have updated in the background."
-  );
-
-  
-  await BrowserTestUtils.switchTab(gBrowser, blankTab);
-
-  await Services.search.setDefault(
-    appDefaultEngine,
-    Ci.nsISearchService.CHANGE_REASON_UNKNOWN
-  );
-  gURLBar._updatePlaceholder(appDefaultEngine.name, true);
-
-  Assert.equal(
-    gURLBar.placeholder,
+    newWin.gURLBar.placeholder,
     noEngineString,
     "Placeholder should be unchanged."
   );
   Assert.deepEqual(
-    document.l10n.getAttributes(gURLBar.inputField),
+    newWin.document.l10n.getAttributes(newWin.gURLBar.inputField),
     { id: "urlbar-placeholder", args: null },
     "Placeholder data should be unchanged."
   );
 
-  await BrowserTestUtils.switchTab(gBrowser, urlTab);
+  info("Simulate user interaction");
+  let urlTab = await BrowserTestUtils.addTab(newWin.gBrowser, "about:mozilla");
+  await BrowserTestUtils.switchTab(newWin.gBrowser, urlTab);
+  if (defaultEngine.isAppProvided) {
+    await TestUtils.waitForCondition(
+      () => newWin.gURLBar.placeholder == expectedString,
+      "The placeholder should include the engine name for built-in engines."
+    );
+    Assert.ok(true, "Placeholder should be updated");
+  } else {
+    
+    await new Promise(r => setTimeout(r, 1000));
+    Assert.equal(
+      newWin.gURLBar.placeholder,
+      noEngineString,
+      "Placeholder should be unchanged."
+    );
+  }
 
-  await TestUtils.waitForCondition(
-    () => gURLBar.placeholder == expectedString,
-    "The placeholder should include the engine name for built-in engines."
-  );
-
-  
-  gURLBar._updatePlaceholder(extraEngine.name);
-
-  await TestUtils.waitForCondition(
-    () => gURLBar.placeholder == noEngineString,
-    "The placeholder should go back to the default"
-  );
-  Assert.equal(
-    gURLBar.placeholder,
-    noEngineString,
-    "Placeholder should be the default."
-  );
-
-  Services.obs.addObserver(gURLBar, "browser-search-engine-modified", true);
-});
+  await BrowserTestUtils.closeWindow(newWin);
+}
 
 add_task(async function test_private_window_no_separate_engine() {
   const win = await BrowserTestUtils.openNewBrowserWindow({ private: true });
