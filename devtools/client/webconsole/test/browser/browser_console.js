e19655f4709546b81523cdd0f3c551ea4439ee3b
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-03-01 01:11:02 +0000

    Bug 1943360 - Add a CSP to the devtool's Console. r=devtools-reviewers,bomsy
    Differential Revision: https://phabricator.services.mozilla.com/D237888
    
    UltraBlame original commit: 5fe5ab69c5c9b1c2ba8cd3f3a6fea27e0455b133

--- devtools/client/webconsole/test/browser/browser_console.js
+++ devtools/client/webconsole/test/browser/browser_console.js
@@ -86,13 +86,8 @@ async function testMessages() {
   hud.iframeWindow.console.log("message from chrome window");
 
   
-  const workerCode = `console.log("message in parent worker");
-        throw new Error("error in parent worker");`;
-  const blob = new hud.iframeWindow.Blob([workerCode], {
-    type: "application/javascript",
-  });
   const chromeSpawnedWorker = new hud.iframeWindow.Worker(
-    URL.createObjectURL(blob)
+    getRootDirectory(gTestPath) + "test-parent-worker.js"
   );
 
   
