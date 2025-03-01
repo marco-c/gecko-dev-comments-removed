f161843f576fbf9211bfde2213c07df9a498f658
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-03-01 02:21:35 +0000

    Bug 1949265 [wpt PR 50809] - IDB WPTs: Extend three IDB WPTs to run on workers., a=testonly
    Automatic update from web-platform-tests
    IDB WPTs: Extend three IDB WPTs to run on workers.
    
    This set of IndexedDB WPTs currently only run in a window environment.
    This change extends them to also run in dedicated, shared, and service
    worker environments.
    
    Bug: 41455766
    Change-Id: I57a1781edc007b0134489725d713e6ebabddd60f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6251264
    Reviewed-by: Steve Becker <stevebemicrosoft.com>
    Reviewed-by: Rahul Singh <rahsinmicrosoft.com>
    Commit-Queue: Sneha Agarwal <snehagarwalmicrosoft.com>
    Cr-Commit-Position: refs/heads/main{#1422100}
    
    --
    
    wpt-commits: b47379065e1ce48b956a9099fcfc0e5fd5f440ba
    wpt-pr: 50809
    
    UltraBlame original commit: f85c92e229e3ff41968637de96761124368ad581

---
+++ testing/web-platform/tests/IndexedDB/idbindex-request-source.any.js
@@ -0,0 +1,32 @@
+
+
+
+
+
+
+'use strict';
+
+[
+    index => index.get(0),
+    index => index.getKey(0),
+    index => index.getAll(),
+    index => index.getAllKeys(),
+    index => index.count(),
+    index => index.openCursor(),
+    index => index.openKeyCursor()
+].forEach(func => indexeddb_test(
+    (t, db) => {
+        const store =
+            db.createObjectStore('store', {autoIncrement: true});
+        store.createIndex('index', 'kp');
+    },
+    (t, db) => {
+        const tx = db.transaction('store', 'readwrite');
+        const index = tx.objectStore('store').index('index');
+        assert_equals(
+            func(index).source, index,
+            `${func}.source should be the index itself`);
+        t.done();
+    },
+    `The source of the request from ${func} is the index itself`
+));
