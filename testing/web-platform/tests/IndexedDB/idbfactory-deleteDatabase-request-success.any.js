8e89203499a81af970fa8fbe649d2feef06aef74
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-03-01 02:28:45 +0000

    Bug 1949335 [wpt PR 50817] - IDB WPTs: Extend IDB WPTs to run on workers., a=testonly
    Automatic update from web-platform-tests
    IDB WPTs: Extend IDB WPTs to run on workers.
    
    This set of IndexedDB WPTs currently only run in a window environment.
    This change extends them to also run in dedicated, shared, and service
    worker environments.
    
    Bug: 41455766
    Change-Id: I9d0ef0ae915fe7656349954d6d62dfdbc9873f2d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6271652
    Reviewed-by: Steve Becker <stevebemicrosoft.com>
    Reviewed-by: Rahul Singh <rahsinmicrosoft.com>
    Commit-Queue: Sneha Agarwal <snehagarwalmicrosoft.com>
    Cr-Commit-Position: refs/heads/main{#1422312}
    
    --
    
    wpt-commits: 04fbb221ccdc45b48347cd7603e5000fcc4a4a8d
    wpt-pr: 50817
    
    UltraBlame original commit: d7498971d5e1460c379114504a52f4f960a6696e

---
+++ testing/web-platform/tests/IndexedDB/idbfactory-deleteDatabase-request-success.any.js
@@ -0,0 +1,22 @@
+
+
+
+
+
+
+'use strict';
+
+async_test(t => {
+  const dbname = self.location + '-' + t.name;
+  const rq = indexedDB.deleteDatabase(dbname);
+  rq.onerror = t.unreached_func('deleteDatabase should succeed');
+  rq.onsuccess = t.step_func(() => {
+    assert_equals(
+        rq.readyState, 'done', 'request done flag should be set on success');
+    assert_equals(
+        rq.result, undefined,
+        'request result should be undefined on success');
+    assert_equals(rq.error, null, 'request error should be null on success');
+    t.done();
+  });
+}, 'IDBFactory deleteDatabase() request properties on success');
