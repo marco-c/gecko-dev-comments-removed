e4c6d55c6203a2db0b80e205196cbefee8d74abb
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-03-01 01:01:45 +0000

    Bug 1947866 [wpt PR 50661] - IDB WPTs: Extend idbcursor related tests to workers, a=testonly
    Automatic update from web-platform-tests
    IDB WPTs: Extend idbcursor related tests to workers
    
    The update modifies idbcursor related WPTs to run not
    only in window environments but also on dedicated, service, and shared
    workers.
    
    Change-Id: I291ae2fdd2cb2ff72e6061a9405090ac40aed247
    Bug: 41455766
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6203388
    Reviewed-by: Rahul Singh <rahsinmicrosoft.com>
    Auto-Submit: Garima Chadha <garimachadhamicrosoft.com>
    Reviewed-by: Steve Becker <stevebemicrosoft.com>
    Commit-Queue: Steve Becker <stevebemicrosoft.com>
    Cr-Commit-Position: refs/heads/main{#1419468}
    
    --
    
    wpt-commits: 8f2f34035aa3908845908ffaabe06d2b5204ac59
    wpt-pr: 50661
    
    UltraBlame original commit: 48bab9d5f0915526f5cc7c2a62e5a17f97317d2c

---
+++ testing/web-platform/tests/IndexedDB/idbcursor-delete-exception-order.any.js
@@ -0,0 +1,72 @@
+
+
+
+
+
+
+'use strict';
+
+indexeddb_test(
+    (t, db) => {
+      const s = db.createObjectStore('s');
+      s.put('value', 'key');
+    },
+    (t, db) => {
+      const s = db.transaction('s', 'readonly').objectStore('s');
+      const r = s.openCursor();
+      r.onsuccess = t.step_func(() => {
+        r.onsuccess = null;
+        const cursor = r.result;
+        setTimeout(
+            t.step_func(() => {
+              assert_throws_dom(
+                  'TransactionInactiveError', () => cursor.delete(),
+                  '"Transaction inactive" check (TransactionInactiveError)' +
+                      'should precede "read only" check(ReadOnlyError)');
+              t.done();
+            }),
+            0);
+      });
+    },
+    'IDBCursor.delete exception order: TransactionInactiveError vs. ReadOnlyError');
+
+indexeddb_test(
+    (t, db) => {
+      const s = db.createObjectStore('s');
+      s.put('value', 'key');
+    },
+    (t, db) => {
+      const s = db.transaction('s', 'readonly').objectStore('s');
+      const r = s.openCursor();
+      r.onsuccess = t.step_func(() => {
+        r.onsuccess = null;
+        const cursor = r.result;
+        cursor.continue();
+        assert_throws_dom(
+            'ReadOnlyError', () => cursor.delete(),
+            '"Read only" check (ReadOnlyError) should precede ' +
+                '"got value flag" (InvalidStateError) check');
+        t.done();
+      });
+    },
+    'IDBCursor.delete exception order: ReadOnlyError vs. InvalidStateError #1');
+
+indexeddb_test(
+    (t, db) => {
+      const s = db.createObjectStore('s');
+      s.put('value', 'key');
+    },
+    (t, db) => {
+      const s = db.transaction('s', 'readonly').objectStore('s');
+      const r = s.openKeyCursor();
+      r.onsuccess = t.step_func(() => {
+        r.onsuccess = null;
+        const cursor = r.result;
+        assert_throws_dom(
+            'ReadOnlyError', () => cursor.delete(),
+            '"Read only" check (ReadOnlyError) should precede ' +
+                '"key only flag" (InvalidStateError) check');
+        t.done();
+      });
+    },
+    'IDBCursor.delete exception order: ReadOnlyError vs. InvalidStateError #2');
