8966404bf672216d62e7af0da16fdc6675ef7ef3
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-03-01 01:58:59 +0000

    Bug 1949805 - Window opener heuristic with user interaction should not only work for trackers - r=timhuang
    Differential Revision: https://phabricator.services.mozilla.com/D239165
    
    UltraBlame original commit: b809946b04a9792153b61d1332abde659d4ec2eb

--- dom/base/Document.cpp
+++ dom/base/Document.cpp
@@ -17575,11 +17575,6 @@ void Document::MaybeAllowStorageForOpenerAfterUserInteraction() {
     return;
   }
 
-  
-  if (!nsContentUtils::IsFirstPartyTrackingResourceWindow(inner)) {
-    return;
-  }
-
   auto* outer = nsGlobalWindowOuter::Cast(inner->GetOuterWindow());
   if (NS_WARN_IF(!outer)) {
     return;
