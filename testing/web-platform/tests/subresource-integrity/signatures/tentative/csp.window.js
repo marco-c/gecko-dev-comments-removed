c1609ac03e0662746d3c29a2c3e6d3f319fae0a3
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-03-01 01:05:19 +0000

    Bug 1948285 [wpt PR 50712] - [Signature-based SRI] Support `ed25519-...` assertions in CSP., a=testonly
    Automatic update from web-platform-tests
    [Signature-based SRI] Support `ed25519-...` assertions in CSP.
    
    As noted in https://github.com/wicg/signature-based-sri/issues/36, we
    should support signature-based assertions in CSP. This CL makes the
    smallest change necessary to support the functionality, but we'll likely
    want to come back to refactor/rename some things in CSP's infrastructure
    now that we're supporting more than hashes.
    
    Bug: 396434688
    Change-Id: Ib52e88df46b9828722e986f49901ba712febef02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6268639
    Reviewed-by: Yoav Weiss (Shopify) <yoavweisschromium.org>
    Reviewed-by: Antonio Sartori <antoniosartorichromium.org>
    Commit-Queue: Mike West <mkwstchromium.org>
    Cr-Commit-Position: refs/heads/main{#1420462}
    
    --
    
    wpt-commits: 251028f32f4c23e84dd2b8b1736bf693840f7ff5
    wpt-pr: 50712
    
    UltraBlame original commit: 4e394612c0128091671b638fb449af10a2b17e80

---
+++ testing/web-platform/tests/subresource-integrity/signatures/tentative/csp.window.js
@@ -0,0 +1,47 @@
+
+
+
+
+
+
+
+const el = document.createElement('meta');
+el.httpEquiv = "content-security-policy";
+el.content = `script-src 'ed25519-${kValidKeys['rfc']}'`;
+document.head.appendChild(el);
+
+
+generate_script_test(kUnsignedShouldBlock, "", EXPECT_BLOCKED,
+                     "No signature, no integrity check: loads.");
+
+generate_script_test(kUnsignedShouldBlock, "ed25519-???", EXPECT_BLOCKED,
+                     "No signature, malformed integrity check: loads.");
+
+generate_script_test(kUnsignedShouldBlock, `ed25519-${kValidKeys['rfc']}`, EXPECT_BLOCKED,
+                     "No signature, integrity check: blocked.");
+
+
+generate_script_test(kSignedShouldBlock, "", EXPECT_BLOCKED,
+                     "Valid signature, no integrity check: blocked.");
+generate_script_test(kSignedShouldBlock, "ed25519-???", EXPECT_BLOCKED,
+                     "Valid signature, malformed integrity check: blocked.");
+generate_script_test(kSignedShouldExecute, `ed25519-${kValidKeys['rfc']}`, EXPECT_LOADED,
+                     "Valid signature, valid integrity check: loads.");
+generate_script_test(kSignedShouldExecute, `ed25519-${kValidKeys['rfc']} ed25519-${kValidKeys['arbitrary']}`, EXPECT_BLOCKED,
+                     "Valid signature, one matching and one mismatched integrity check: blocked.");
+generate_script_test(kSignedShouldBlock, `ed25519-${kValidKeys['arbitrary']}`, EXPECT_BLOCKED,
+                     "Valid signature, mismatched integrity check: blocked.");
+
+
+generate_script_test(kMultiplySignedShouldBlock, "", EXPECT_BLOCKED,
+                     "Valid signatures, no integrity check: blocked.");
+generate_script_test(kMultiplySignedShouldBlock, "ed25519-???", EXPECT_BLOCKED,
+                     "Valid signatures, malformed integrity check: blocked.");
+generate_script_test(kMultiplySignedShouldExecute, `ed25519-${kValidKeys['rfc']}`, EXPECT_LOADED,
+                     "Valid signatures, integrity check matches one: loads.");
+generate_script_test(kMultiplySignedShouldBlock, `ed25519-${kValidKeys['arbitrary']}`, EXPECT_BLOCKED,
+                     "Valid signatures, integrity check matches the other: blocked.");
+generate_script_test(kMultiplySignedShouldBlock, `ed25519-${kValidKeys['rfc']} ed25519-${kValidKeys['arbitrary']}`, EXPECT_BLOCKED,
+                     "Valid signatures, integrity check matches both, but only one in CSP: blocked.");
+generate_script_test(kMultiplySignedShouldBlock, `ed25519-${kInvalidKey}`, EXPECT_BLOCKED,
+                     "Valid signatures, integrity check matches neither: blocked.");
