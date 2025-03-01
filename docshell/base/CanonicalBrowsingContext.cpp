d16345a18b8918a0ed6ec75b275444cdada20dbc
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-03-01 01:53:07 +0000

    Bug 1949587 - Restore picker behavior for hidden extension pages r=edgar
    Since Firefox 136, pickers cannot be opened from non-focused tabs.
    Unintentionally, background pages of extensions and devtools pages of
    extensions were also affected, because their browsers are embedded in a
    hidden XUL window, which is considered not focused.
    
    This patch updates CanonicalBrowsingContext::CanOpenModalPicker to allow
    pickers to be opened from specific hidden extension browsers again.
    
    To evaluate the feasibility of enforcing the restriction for hidden
    background pages, this patch puts the restriction behind a pref,
    enabled by default on Nightly only. If no signs of broken extensions
    are reported for a few releases, we can set the pref by default, to
    enforce the restriction (and document the change accordingly).
    After another few releases we can evaluate whether to enforce the
    restriction permanently (effectively removing the exception from
    this patch) or to revert the pref.
    
    Differential Revision: https://phabricator.services.mozilla.com/D239046
    
    UltraBlame original commit: e23b500f5049254fdd0f40f96737d3abf926f7a1

--- docshell/base/CanonicalBrowsingContext.cpp
+++ docshell/base/CanonicalBrowsingContext.cpp
@@ -3282,6 +3282,17 @@ bool CanonicalBrowsingContext::CanOpenModalPicker() {
     return false;
   }
 
+  mozilla::dom::Element* topFrameElement = GetTopFrameElement();
+  if (!mozilla::StaticPrefs::
+          browser_disable_pickers_in_hidden_extension_pages() &&
+      Windowless()) {
+    WindowGlobalParent* wgp = GetCurrentWindowGlobal();
+    if (wgp && BasePrincipal::Cast(wgp->DocumentPrincipal())->AddonPolicy()) {
+      
+      return true;
+    }
+  }
+
   RefPtr<Document> chromeDoc = TopCrossChromeBoundary()->GetExtantDocument();
   if (!chromeDoc || !chromeDoc->HasFocus(mozilla::IgnoreErrors())) {
     return false;
@@ -3292,7 +3303,6 @@ bool CanonicalBrowsingContext::CanOpenModalPicker() {
   
   
   
-  mozilla::dom::Element* topFrameElement = GetTopFrameElement();
   while (topFrameElement) {
     RefPtr<Document> doc = topFrameElement->OwnerDoc();
     if (doc->GetActiveElement() != topFrameElement) {
