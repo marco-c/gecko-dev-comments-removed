4d9222de719b878819c251fa61cd7cdc9ed4098b
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-03-01 01:13:11 +0000

    Bug 1924576 - Fix "connect additional devices" link in new sidebar empty state for add a device in sync tabs r=sidebar-reviewers,fxview-reviewers,Gijs,kcochrane
    Differential Revision: https://phabricator.services.mozilla.com/D237267
    
    UltraBlame original commit: 20b06d8c5e34cf57b429d99ea3f95386b03d5236

--- browser/components/firefoxview/tests/browser/browser_syncedtabs_firefoxview.js
+++ browser/components/firefoxview/tests/browser/browser_syncedtabs_firefoxview.js
@@ -106,6 +106,43 @@ add_task(async function test_signed_in() {
       "Add device message is shown"
     );
 
+    const supportPageSuffix =
+      "kb/how-do-i-set-sync-my-computer#w_connect-additional-devices-to-sync";
+    const mockConnectAdditionDevicesPath =
+      "https://example.com/" + supportPageSuffix;
+    let expectedUrl = "https://support.mozilla.org/" + supportPageSuffix;
+    let connectAdditionalDevicesLink = await TestUtils.waitForCondition(
+      () => emptyState?.shadowRoot.querySelector("a"),
+      "Support url is visible"
+    );
+    is(
+      connectAdditionalDevicesLink.href,
+      expectedUrl,
+      "Support link href is correct"
+    );
+    connectAdditionalDevicesLink.href = mockConnectAdditionDevicesPath;
+    info("Mock click on support link");
+    let tabOpened = BrowserTestUtils.waitForDocLoadAndStopIt(
+      mockConnectAdditionDevicesPath,
+      gBrowser,
+      channel => {
+        is(
+          channel.originalURI.spec,
+          mockConnectAdditionDevicesPath,
+          "URL matched"
+        );
+        return true;
+      }
+    );
+    EventUtils.synthesizeMouseAtCenter(
+      connectAdditionalDevicesLink,
+      {},
+      
+      connectAdditionalDevicesLink.ownerDocument.defaultView
+    );
+    await tabOpened;
+
+    await openFirefoxViewTab(window);
     
     await clearAllParentTelemetryEvents();
     EventUtils.synthesizeMouseAtCenter(
@@ -127,7 +164,10 @@ add_task(async function test_signed_in() {
       { category: "firefoxview_next" },
       { clear: true, process: "parent" }
     );
-    await BrowserTestUtils.removeTab(browser.ownerGlobal.gBrowser.selectedTab);
+    
+    while (gBrowser.tabs.length > 1) {
+      await BrowserTestUtils.removeTab(gBrowser.tabs.at(-1));
+    }
   });
   await tearDown(sandbox);
 });
