1d96dfa935bf8ca804ccc7ab4f7e763e199b78d4
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-03-01 02:02:29 +0000

    Bug 1949281 - Wallpaper category subpanel resets after closing the customize panel  r=home-newtab-reviewers,nbarrett
    Differential Revision: https://phabricator.services.mozilla.com/D238835
    
    UltraBlame original commit: 7c8b027a2bc73d58fb88a7118d8476306b83e2c5

--- browser/extensions/newtab/data/content/activity-stream.bundle.js
+++ browser/extensions/newtab/data/content/activity-stream.bundle.js
@@ -11260,6 +11260,12 @@ class _WallpaperCategories extends (external_React_default()).PureComponent {
   componentDidMount() {
     this.prefersDarkQuery = globalThis.matchMedia("(prefers-color-scheme: dark)");
   }
+  componentDidUpdate(prevProps) {
+    
+    if (this.props.exitEventFired && this.props.exitEventFired !== prevProps.exitEventFired) {
+      this.handleBack();
+    }
+  }
   handleColorInput(event) {
     let {
       id
@@ -11744,7 +11750,8 @@ class ContentSection extends (external_React_default()).PureComponent {
       className: "wallpapers-section"
     }, external_React_default().createElement(WallpaperCategories, {
       setPref: setPref,
-      activeWallpaper: activeWallpaper
+      activeWallpaper: activeWallpaper,
+      exitEventFired: exitEventFired
     })), external_React_default().createElement("span", {
       className: "divider",
       role: "separator"
