3e508ab67b6cdaf4879b3942b3d5ef921004525f
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-03-01 00:58:59 +0000

    Bug 1942850 - Part 2: Delegate Duration.prototype.toLocaleString to DurationFormat. r=dminor
    Change the implementation for `Temporal.Duration.prototype.toLocaleString` to
    use an `Intl.DurationFormat` formatter.
    
    Differential Revision: https://phabricator.services.mozilla.com/D238209
    
    UltraBlame original commit: de1af832f7198892509a319fe531b0c9acca2b07

--- js/src/builtin/intl/DurationFormat.h
+++ js/src/builtin/intl/DurationFormat.h
@@ -39,6 +39,12 @@ class DurationFormatObject : public NativeObject {
 [[nodiscard]] extern bool intl_GetTimeSeparator(JSContext* cx, unsigned argc,
                                                 Value* vp);
 
+
+
+
+[[nodiscard]] extern bool TemporalDurationToLocaleString(
+    JSContext* cx, const JS::CallArgs& args);
+
 }  
 
 #endif 
