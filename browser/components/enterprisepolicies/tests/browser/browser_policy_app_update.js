462d6976649711e085387b2e4c4005ebf2a2e596
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-03-01 02:18:51 +0000

    Bug 1880963 - Fix intermittent in browser_policy_app_update.js. r=mkaply
    Ensure that the updates disabled for testing preference is reset before re-enabling automatic updates.
    
    Differential Revision: https://phabricator.services.mozilla.com/D239303
    
    UltraBlame original commit: c042c15ac12c299e2efd82f662c1f308f1dc1e51

--- browser/components/enterprisepolicies/tests/browser/browser_policy_app_update.js
+++ browser/components/enterprisepolicies/tests/browser/browser_policy_app_update.js
@@ -20,6 +20,9 @@ add_task(async function test_updates_pre_policy() {
   let originalUpdateAutoValue = await UpdateUtils.getAppUpdateAutoEnabled();
   await UpdateUtils.setAppUpdateAutoEnabled(false);
   registerCleanupFunction(async () => {
+    
+    
+    await SpecialPowers.popPrefEnv();
     await UpdateUtils.setAppUpdateAutoEnabled(originalUpdateAutoValue);
   });
 
