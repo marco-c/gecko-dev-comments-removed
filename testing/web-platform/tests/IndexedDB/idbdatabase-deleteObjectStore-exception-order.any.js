28497c1903ad2c4863453ddf6cb6fd3789ca5cf7
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-03-01 01:01:33 +0000

    Bug 1947860 [wpt PR 50659] - IDB WPTs: Extend idbdatabase related tests to workers, a=testonly
    Automatic update from web-platform-tests
    IDB WPTs: Extend idbdatabase related tests to workers
    
    The update modifies idbdatabase related WPTs to run not
    only in window environments but also on dedicated, service, and shared
    workers.
    
    Change-Id: I3706384e30023352e5590978bc3bafc94d080a75
    Bug: 41455766
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6204202
    Reviewed-by: Rahul Singh <rahsinmicrosoft.com>
    Auto-Submit: Garima Chadha <garimachadhamicrosoft.com>
    Reviewed-by: Steve Becker <stevebemicrosoft.com>
    Commit-Queue: Steve Becker <stevebemicrosoft.com>
    Cr-Commit-Position: refs/heads/main{#1419465}
    
    --
    
    wpt-commits: ded374cfa21e02321b0d230b08b8994d065c9a74
    wpt-pr: 50659
    
    UltraBlame original commit: b83be44a16e94e09f1e249f7ee94d3178eae5cc6

---
+++ testing/web-platform/tests/IndexedDB/idbdatabase-deleteObjectStore-exception-order.any.js
@@ -0,0 +1,46 @@
+
+
+
+
+
+
+'use strict';
+
+indexeddb_test(
+    (t, db, txn) => {
+      db.createObjectStore('s');
+      txn.onabort = () => {
+        setTimeout(
+            t.step_func(() => {
+              assert_throws_dom(
+                  'InvalidStateError', () => db.deleteObjectStore('s'),
+                  '"running an upgrade transaction" check (InvalidStateError) ' +
+                      'should precede "not active" check (TransactionInactiveError)');
+              t.done();
+            }),
+            0);
+      };
+      txn.abort();
+    },
+    (t, db) => {
+      t.assert_unreached('open should fail');
+    },
+    'IDBDatabase.deleteObjectStore exception order: ' +
+        'InvalidStateError vs. TransactionInactiveError',
+    {upgrade_will_abort: true});
+
+indexeddb_test(
+    (t, db, txn) => {
+      txn.abort();
+      assert_throws_dom(
+          'TransactionInactiveError', () => db.deleteObjectStore('nope'),
+          '"not active" check (TransactionInactiveError) should precede ' +
+              '"name in database" check (NotFoundError)');
+      t.done();
+    },
+    (t, db) => {
+      t.assert_unreached('open should fail');
+    },
+    'IDBDatabase.deleteObjectStore exception order: ' +
+        'TransactionInactiveError vs. NotFoundError',
+    {upgrade_will_abort: true});
---
+++ testing/web-platform/tests/IndexedDB/idbdatabase-transaction-exception-order.any.js
@@ -0,0 +1,62 @@
+
+
+
+
+
+
+'use strict';
+
+indexeddb_test(
+    (t, db) => {
+      db.createObjectStore('s');
+    },
+    (t, db) => {
+      db.close();
+      assert_throws_dom(
+          'InvalidStateError', () => db.transaction('no-such-store'),
+          '"Connection is closed" check (InvalidStateError) should precede ' +
+              '"store names" check (NotFoundError)');
+      t.done();
+    },
+    'IDBDatabase.transaction exception order: InvalidStateError vs. NotFoundError');
+
+indexeddb_test(
+    (t, db) => {
+      db.createObjectStore('s');
+    },
+    (t, db) => {
+      db.close();
+      assert_throws_dom(
+          'InvalidStateError', () => db.transaction([]),
+          '"Connection is closed" check (InvalidStateError) should precede ' +
+              '"stores is empty" check (InvalidAccessError)');
+      t.done();
+    },
+    'IDBDatabase.transaction exception order: InvalidStateError vs. InvalidAccessError');
+
+
+indexeddb_test(
+    (t, db) => {
+      db.createObjectStore('s');
+    },
+    (t, db) => {
+      assert_throws_js(
+          TypeError, () => db.transaction('s', 'versionchange'),
+          '"invalid mode" check should throw TypeError');
+      t.done();
+    },
+    'IDBDatabase.transaction throws exception on invalid mode');
+
+indexeddb_test(
+    (t, db) => {
+      db.createObjectStore('s');
+    },
+    (t, db) => {
+      assert_throws_dom(
+          'NotFoundError',
+          () => db.transaction('no-such-store', 'versionchange'),
+          '"No such store" check (NotFoundError) should precede ' +
+              '"invalid mode" check (TypeError)');
+      t.done();
+    },
+    'IDBDatabase.transaction exception order: NotFoundError vs. TypeError');
