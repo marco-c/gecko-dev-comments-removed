cf80a3f88f11f55426963549442c96db3e65aaca
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-03-01 01:49:42 +0000

    Bug 1775795 - In testResetFOG, ensure to override custom ping registration after reinit r=TravisLong
    Also, while we're here:
    * Clear some state that was hanging around in JOG across resets
    * Add a missing verbose log for registering pings in JOG
    * Add a test so that any regression of this behaviour fails inside the component first.
    
    Differential Revision: https://phabricator.services.mozilla.com/D239014
    
    UltraBlame original commit: 3623dfc5c7f729f36c140a571f44a79dc6641bd3

--- toolkit/components/glean/src/init/mod.rs
+++ toolkit/components/glean/src/init/mod.rs
@@ -358,10 +358,11 @@ fn fog_test_reset_internal(
     
     conf.uploader = Some(Box::new(ViaductUploader) as Box<dyn glean::net::PingUploader>);
 
+    glean::test_reset_glean(conf, client_info, true);
+
     
     pings::register_pings(None);
 
-    glean::test_reset_glean(conf, client_info, true);
     Ok(())
 }
 
