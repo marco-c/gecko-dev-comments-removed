7c3eb5eb9dbce6e5ae6f65f9516f92513ff7830d
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-03-16 01:58:52 +0000

    Bug 1931407 - wasm: Disallow shared memory for buffer compilation methods. r=yury
    The JS-API specifies BufferSource without a [AllowShared] attribute and
    so we should disallow SAB from compile methods [1] [2]. This makes it
    possible to decode directly from the array buffer memory without a copy
    and without worrying about data races.
    
    I've verified that Chrome disallows SAB, and so I think there is minimal
    risk of breakage from this.
    
    Also ensure that the buffer source pointer is not moveable under GC so that
    we're safe to use it during a synchronous compile if it does a last-ditch
    GC under code pressure.
    
    [1] https://webidl.spec.whatwg.org/#js-buffer-source-types
    [2] https://webassembly.github.io/spec/js-api/index.html#webassembly-namespace
    
    Depends on D235239
    
    Differential Revision: https://phabricator.services.mozilla.com/D235240
    
    UltraBlame original commit: d566e482a2a6af74fe34eaf7502c1b0db800a9c5

--- js/src/vm/TypedArrayObject.h
+++ js/src/vm/TypedArrayObject.h
@@ -239,7 +239,17 @@ JSNative TypedArrayConstructorNative(Scalar::Type type);
 
 
 
-bool IsBufferSource(JSObject* object, SharedMem<uint8_t*>* dataPointer,
+
+
+
+
+
+
+
+
+
+bool IsBufferSource(JSContext* cx, JSObject* object, bool allowShared,
+                    bool allowResizable, SharedMem<uint8_t*>* dataPointer,
                     size_t* byteLength);
 
 inline Scalar::Type TypedArrayObject::type() const {
