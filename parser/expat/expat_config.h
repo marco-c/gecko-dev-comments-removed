48b2ded5ad797e9b3b541affc6c0940471b8fda1
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-03-16 02:03:42 +0000

    Bug 1950575 - Change support code for Expat 2.6.4. r=mccr8
    Differential Revision: https://phabricator.services.mozilla.com/D239720
    
    UltraBlame original commit: 99620432e3b8c8b968ac6a7c195a5756fcc7af1a

--- parser/expat/expat_config.h
+++ parser/expat/expat_config.h
@@ -15,6 +15,9 @@
 
 #define HAVE_MEMMOVE 1
 
+
+#define XML_POOR_ENTROPY 1
+
 #define XMLCALL
 #define XML_STATIC
 #ifdef HAVE_VISIBILITY_HIDDEN_ATTRIBUTE
@@ -38,6 +41,8 @@ typedef uint16_t XML_Char;
 #endif
 
 #define XML_DTD
+#define XML_GE 1
 #define XML_NS
+#define XML_CONTEXT_BYTES 0
 
 #endif 
