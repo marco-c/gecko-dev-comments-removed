27e1c6a5b701859c39c200fd00e29303103da09a
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-03-16 01:54:19 +0000

    Bug 1948908 - Cover system builtin location in test_system_repository.js. r=baku,willdurand
    Differential Revision: https://phabricator.services.mozilla.com/D238990
    
    UltraBlame original commit: 6ba077560f509306a04ef0a59750faa81b7ca2ec

--- toolkit/mozapps/extensions/test/xpcshell/test_system_repository.js
+++ toolkit/mozapps/extensions/test/xpcshell/test_system_repository.js
@@ -1,27 +1,42 @@
 
 
-const PREF_GETADDONS_CACHE_ENABLED = "extensions.getAddons.cache.enabled";
 
-var gServer = new HttpServer();
-gServer.start(-1);
+let scopes = AddonManager.SCOPE_PROFILE | AddonManager.SCOPE_APPLICATION;
+Services.prefs.setIntPref("extensions.enabledScopes", scopes);
+
+const PREF_GETADDONS_CACHE_ENABLED = "extensions.getAddons.cache.enabled";
 
 createAppInfo("xpcshell@tests.mozilla.org", "XPCShell", "0");
 
+var gServer = new HttpServer();
 
-add_task(async function test_app_addons() {
+add_setup(() => {
+  gServer.start(-1);
+  gServer.registerPathHandler("/get", () => {
+    do_throw("Unexpected request to server.");
+  });
+  registerCleanupFunction(async () => {
+    await new Promise(resolve => gServer.stop(resolve));
+  });
+});
+
+
+async function test_app_addons({ asBuiltIn = true } = {}) {
   
-  var distroDir = FileUtils.getDir("ProfD", ["sysfeatures"]);
-  distroDir.create(Ci.nsIFile.DIRECTORY_TYPE, FileUtils.PERMS_DIRECTORY);
-  let xpi = await getSystemAddonXPI(1, "1.0");
-  xpi.copyTo(distroDir, "system1@tests.mozilla.org.xpi");
+  if (!asBuiltIn) {
+    var distroDir = FileUtils.getDir("ProfD", ["sysfeatures"]);
+    distroDir.create(Ci.nsIFile.DIRECTORY_TYPE, FileUtils.PERMS_DIRECTORY);
+    let xpi = await getSystemAddonXPI(1, "1.0");
+    xpi.copyTo(distroDir, "system1@tests.mozilla.org.xpi");
 
-  xpi = await getSystemAddonXPI(2, "1.0");
-  xpi.copyTo(distroDir, "system2@tests.mozilla.org.xpi");
+    xpi = await getSystemAddonXPI(2, "1.0");
+    xpi.copyTo(distroDir, "system2@tests.mozilla.org.xpi");
 
-  xpi = await getSystemAddonXPI(3, "1.0");
-  xpi.copyTo(distroDir, "system3@tests.mozilla.org.xpi");
+    xpi = await getSystemAddonXPI(3, "1.0");
+    xpi.copyTo(distroDir, "system3@tests.mozilla.org.xpi");
 
-  registerDirectory("XREAppFeat", distroDir);
+    registerDirectory("XREAppFeat", distroDir);
+  }
 
   Services.prefs.setBoolPref(PREF_GETADDONS_CACHE_ENABLED, true);
   Services.prefs.setCharPref(
@@ -29,16 +44,21 @@ add_task(async function test_app_addons() {
     `http://localhost:${gServer.identity.primaryPort}/get?%IDS%`
   );
 
-  gServer.registerPathHandler("/get", () => {
-    do_throw("Unexpected request to server.");
-  });
-
   await overrideBuiltIns({
-    system: [
-      "system1@tests.mozilla.org",
-      "system2@tests.mozilla.org",
-      "system3@tests.mozilla.org",
-    ],
+    builtins: asBuiltIn
+      ? [
+          await getSystemBuiltin(1),
+          await getSystemBuiltin(2),
+          await getSystemBuiltin(3),
+        ]
+      : [],
+    system: asBuiltIn
+      ? []
+      : [
+          "system1@tests.mozilla.org",
+          "system2@tests.mozilla.org",
+          "system3@tests.mozilla.org",
+        ],
   });
 
   await promiseStartupManager();
@@ -65,5 +85,13 @@ add_task(async function test_app_addons() {
   Assert.equal(cached, null);
 
   await promiseShutdownManager();
-  await new Promise(resolve => gServer.stop(resolve));
+}
+
+
+add_task(async function test_app_addons_xpi() {
+  await test_app_addons({ asBuiltIn: false });
+});
+
+add_task(async function test_app_addons_systembuiltin() {
+  await test_app_addons();
 });
