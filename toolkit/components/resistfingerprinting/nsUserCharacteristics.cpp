62e68f44b8c23c668ebefc742588f2a21d5c98f2
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-03-16 02:31:13 +0000

    Bug 1953226 - Use Date.UTC instead of new Date when parsing buildId into buildDate. r=tjr
    Differential Revision: https://phabricator.services.mozilla.com/D241070
    
    UltraBlame original commit: 3c889b5d889e48f46458e5eadec87c9cc54ea322

--- toolkit/components/resistfingerprinting/nsUserCharacteristics.cpp
+++ toolkit/components/resistfingerprinting/nsUserCharacteristics.cpp
@@ -681,7 +681,7 @@ const RefPtr<PopulatePromise>& TimoutPromise(
 
 
 
-const int kSubmissionSchema = 21;
+const int kSubmissionSchema = 22;
 
 const auto* const kUUIDPref =
     "toolkit.telemetry.user_characteristics_ping.uuid";
