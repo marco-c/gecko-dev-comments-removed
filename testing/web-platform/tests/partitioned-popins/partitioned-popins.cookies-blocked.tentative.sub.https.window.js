bd55f4c549305d92185632f58b460f2b5be35061
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-03-16 02:21:10 +0000

    Bug 1951451 [wpt PR 51056] - [Partitioned Popins] Test RSA cookies via WPTs, a=testonly
    Automatic update from web-platform-tests
    [Partitioned Popins] Test RSA cookies via WPTs
    
    We need to run these in the chrome shell to see any changes due to
    calling requestStorageAccess.
    
    Bug: 340606651
    Change-Id: Ica905acf4dc212a728271a48610bb311b515ee91
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6317865
    Commit-Queue: Ari Chivukula <arichivchromium.org>
    Reviewed-by: Rick Byers <rbyerschromium.org>
    Auto-Submit: Ari Chivukula <arichivchromium.org>
    Cr-Commit-Position: refs/heads/main{#1428089}
    
    --
    
    wpt-commits: 74d66d60b2239e8d1ba06d7748c4f4825709c625
    wpt-pr: 51056
    
    UltraBlame original commit: 4376d5c3d50a3ee82cfb4d3ccd0cfb67537829b2

--- testing/web-platform/tests/partitioned-popins/partitioned-popins.cookies-blocked.tentative.sub.https.window.js
+++ testing/web-platform/tests/partitioned-popins/partitioned-popins.cookies-blocked.tentative.sub.https.window.js
@@ -39,8 +39,7 @@ async_test(t => {
       case 'popin-read':
         
         
-        
-        assert_equals(e.data.message, "ReadOnLoad:ThirdPartyNone-,ReadOnFetch:ThirdPartyNone-ThirdPartyNonePopin-,ReadOnDocument:ThirdPartyNone-ThirdPartyNonePopin-,ReadOnFetchAfterRSA:ThirdPartyNone-ThirdPartyNonePopin-ThirdPartyNonePopinAfterRSA-,ReadOnDocumentAfterRSA:ThirdPartyNone-ThirdPartyNonePopin-ThirdPartyNonePopinAfterRSA-,ReadInPopinIframe:ThirdPartyNone-,ReadInPopinIframeAfterRSA:FirstPartyNone-ThirdPartyNone-FirstPartyNoneAfterRSA-ThirdPartyNoneAfterRSA-");
+        assert_equals(e.data.message, "ReadOnLoad:ThirdPartyNone-,ReadOnFetch:ThirdPartyNone-ThirdPartyNonePopin-,ReadOnDocument:ThirdPartyNone-ThirdPartyNonePopin-,ReadOnFetchAfterRSA:FirstPartyNone-ThirdPartyNone-ThirdPartyNonePopin-FirstPartyNonePopinAfterRSA-ThirdPartyNonePopinAfterRSA-,ReadOnDocumentAfterRSA:FirstPartyNone-ThirdPartyNone-ThirdPartyNonePopin-FirstPartyNonePopinAfterRSA-ThirdPartyNonePopinAfterRSA-,ReadInPopinIframe:ThirdPartyNone-,ReadInPopinIframeAfterRSA:FirstPartyNone-ThirdPartyNone-FirstPartyNoneAfterRSA-ThirdPartyNoneAfterRSA-");
         t.done();
         break;
     }
