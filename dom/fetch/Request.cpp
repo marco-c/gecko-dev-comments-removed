519a42c238ee932c2e6508a5a09e42867e8e0927
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-03-16 02:51:45 +0000

    Backed out 2 changesets (bug 1945565) for causing wpt failures.
    Backed out changeset fbdc65ce6f84 (bug 1945565)
    Backed out changeset 5296261171bc (bug 1945565)
    
    UltraBlame original commit: 8c8369f5cef0eac1b57040f1b567fb5ddd87c5d1

--- dom/fetch/Request.cpp
+++ dom/fetch/Request.cpp
@@ -354,15 +354,6 @@ SafeRefPtr<Request> Request::Constructor(
     request->SetTriggeringPrincipal(aInit.mTriggeringPrincipal.Value());
   }
 
-  if (aInit.mNeverTaint) {
-    if (!XRE_IsParentProcess()) {
-      aRv.ThrowNotAllowedError(
-          "Taint has to happen outside of the parent process.");
-      return nullptr;
-    }
-    request->SetNeverTaint(aInit.mNeverTaint);
-  }
-
   
   if (aInit.mMethod.WasPassed()) {
     nsAutoCString method(aInit.mMethod.Value());
