70ad7dae592db71fb6af2763265b4fca35f0c406
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-03-16 01:12:21 +0000

    Bug 1948685 - Vendor libwebrtc from 32f3c6cef1
    Upstream commit: https://webrtc.googlesource.com/src/+/32f3c6cef117e1baea078f562a919adb6ee6a5f4
        Add AbslStringify for RtcErrorType and RtcErrorDetail
    
        Drive-by: fix MakeVal in rtc_base/checks.h to ensure that StrCat is
        used for types that have AbslStringify.
    
        Bug: None
        Change-Id: Ia78c65da18b4a826365a6a2c741f11809640197f
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/374345
        Reviewed-by: Evan Shrubsole <eshrwebrtc.org>
        Commit-Queue: Harald Alvestrand <htawebrtc.org>
        Cr-Commit-Position: refs/heads/main{#43779}
    
    UltraBlame original commit: cc96fb5034c5ee576a9dfbfef2a8f4f989b7b762

--- third_party/libwebrtc/api/rtc_error.h
+++ third_party/libwebrtc/api/rtc_error.h
@@ -105,6 +105,16 @@ enum class RTCErrorDetailType {
 RTC_EXPORT absl::string_view ToString(RTCErrorType error);
 RTC_EXPORT absl::string_view ToString(RTCErrorDetailType error);
 
+template <typename Sink>
+void AbslStringify(Sink& sink, RTCErrorType error) {
+  sink.Append(ToString(error));
+}
+
+template <typename Sink>
+void AbslStringify(Sink& sink, RTCErrorDetailType error_detail) {
+  sink.Append(ToString(error_detail));
+}
+
 
 
 
