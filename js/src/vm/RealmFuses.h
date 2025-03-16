c4bdc74bbe382325eb583f1e8e154cb4f9b810f6
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-03-16 03:02:35 +0000

    Bug 1953503 part 6 - Add use counter for PromiseLookup fuse. r=mgaudet
    Differential Revision: https://phabricator.services.mozilla.com/D241391
    
    UltraBlame original commit: 323e394bb03843e167de4b1ed497c8b1d0baed61

--- js/src/vm/RealmFuses.h
+++ js/src/vm/RealmFuses.h
@@ -160,6 +160,7 @@ struct OptimizeArraySpeciesFuse final : public InvalidatingRealmFuse {
 struct OptimizePromiseLookupFuse final : public InvalidatingRealmFuse {
   virtual const char* name() override { return "OptimizePromiseLookupFuse"; }
   virtual bool checkInvariant(JSContext* cx) override;
+  virtual void popFuse(JSContext* cx, RealmFuses& realmFuses) override;
 };
 
 
