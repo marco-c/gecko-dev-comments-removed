37246510edad11b790fd600cb8fd1eb9161427dd
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-03-16 00:56:02 +0000

    Backed out 2 changesets (bug 1948605, bug 1948606) for causing build bustages  nsHTTPCompressConv.cpp CLOSED TREE
    Backed out changeset 6a2e28dddbb0 (bug 1948606)
    Backed out changeset 86485025decb (bug 1948605)
    
    UltraBlame original commit: d73774951a90e7e96ea529267440a2fb6711512c

--- netwerk/streamconv/converters/nsHTTPCompressConv.cpp
+++ netwerk/streamconv/converters/nsHTTPCompressConv.cpp
@@ -509,17 +509,12 @@ nsHTTPCompressConv::OnDataAvailable(nsIRequest* request, nsIInputStream* iStr,
       [[fallthrough]];
 
     case HTTP_COMPRESS_DEFLATE:
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wuse-after-free"
-      
-      
-      
+
       if (mInpBuffer != nullptr && streamLen > mInpBufferLen) {
         unsigned char* originalInpBuffer = mInpBuffer;
         if (!(mInpBuffer = (unsigned char*)realloc(
-                  mInpBuffer, mInpBufferLen = streamLen))) {
+                  originalInpBuffer, mInpBufferLen = streamLen))) {
           free(originalInpBuffer);
-          mInpBufferLen = 0;
         }
 
         if (mOutBufferLen < streamLen * 2) {
@@ -527,10 +522,9 @@ nsHTTPCompressConv::OnDataAvailable(nsIRequest* request, nsIInputStream* iStr,
           if (!(mOutBuffer = (unsigned char*)realloc(
                     mOutBuffer, mOutBufferLen = streamLen * 3))) {
             free(originalOutBuffer);
-            mOutBufferLen = 0;
           }
         }
-#pragma GCC diagnostic pop
+
         if (mInpBuffer == nullptr || mOutBuffer == nullptr) {
           return NS_ERROR_OUT_OF_MEMORY;
         }
