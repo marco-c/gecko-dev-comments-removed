2228e5d87bb01d30c808e609fb9e11df3204d13c
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-03-16 00:56:36 +0000

    Backed out changeset 2a5e9bdc7e8e (bug 1951341) for causing build bustages. CLOSED TREE
    UltraBlame original commit: c30fc822188ca5953c31a85733a53278491a2cdc

--- devtools/client/debugger/test/mochitest/browser_dbg-inline-preview.js
+++ devtools/client/debugger/test/mochitest/browser_dbg-inline-preview.js
@@ -8,7 +8,6 @@
 
 add_task(async function testInlinePreviews() {
   await pushPref("devtools.debugger.features.inline-preview", true);
-  await pushPref("javascript.options.experimental.import_attributes", true);
 
   const dbg = await initDebugger(
     "doc-inline-preview.html",
@@ -53,7 +52,6 @@ add_task(async function testInlinePreviews() {
   
   await checkInlinePreview(dbg, "runInModule", [
     { identifier: "val:", value: "4" },
-    { identifier: "ids:", value: "Array [ 1, 2 ]" },
   ]);
 
   
