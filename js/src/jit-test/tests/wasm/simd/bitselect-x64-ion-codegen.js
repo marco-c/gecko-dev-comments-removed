a20ce88ee17f66124626d098211cd68f99e87efb
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-03-16 02:52:20 +0000

    Bug 1953381 - Fix x86 PBLEND optimizations for v128.bitselect. r=jseward
    Differential Revision: https://phabricator.services.mozilla.com/D241259
    
    UltraBlame original commit: 2bcb074c9bdb5fc438b87d4914b4ef912196c8a1

--- js/src/jit-test/tests/wasm/simd/bitselect-x64-ion-codegen.js
+++ js/src/jit-test/tests/wasm/simd/bitselect-x64-ion-codegen.js
@@ -29,7 +29,7 @@ codegenTestX64_adhoc(
       (func (export "f") (param v128) (param v128) (param v128) (result v128)
         (v128.bitselect (local.get 0) (local.get 1) (v128.const i32x4 -1 0 0 -1))))`,
       'f',
-  `66 0f 3a 0e c1 c3         pblendw \\$0xC3, %xmm1, %xmm0`);
+  `66 0f 3a 0e c1 3c         pblendw \\$0x3C, %xmm1, %xmm0`);
 
 
 
