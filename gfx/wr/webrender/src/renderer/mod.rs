defd7721a44cdeecab2fec8fd84bed53b9ea0b55
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-03-16 01:44:15 +0000

    Bug 1951548 - Fix occlusion tracking with layer-compositor with video + overlay swapchains present r=gfx-reviewers,lsalzman
    Differential Revision: https://phabricator.services.mozilla.com/D240247
    
    UltraBlame original commit: b7f88a07173c910d73cdac61c620db44d67ca5c3

--- gfx/wr/webrender/src/renderer/mod.rs
+++ gfx/wr/webrender/src/renderer/mod.rs
@@ -265,12 +265,10 @@ const GPU_TAG_COMPOSITE: GpuProfileTag = GpuProfileTag {
     color: debug_colors::TOMATO,
 };
 
-#[derive(Debug)]
 
 
 struct SwapChainLayer {
-    opaque_items: Vec<occlusion::Item<usize>>,
-    alpha_items: Vec<occlusion::Item<usize>>,
+    occlusion: occlusion::FrontToBackBuilder<usize>,
     clear_tiles: Vec<occlusion::Item<usize>>,
 }
 
@@ -3383,7 +3381,6 @@ impl Renderer {
         projection: &default::Transform3D<f32>,
         results: &mut RenderResults,
         partial_present_mode: Option<PartialPresentMode>,
-        occlusion: &occlusion::FrontToBackBuilder<usize>,
         layer: &SwapChainLayer,
     ) {
         self.device.bind_draw_target(draw_target);
@@ -3410,7 +3407,7 @@ impl Renderer {
                 
                 
                 
-                if !dirty_rect.is_empty() && occlusion.test(&dirty_rect) {
+                if !dirty_rect.is_empty() && layer.occlusion.test(&dirty_rect) {
                     
                     self.device.clear_target(clear_color,
                                              None,
@@ -3426,11 +3423,12 @@ impl Renderer {
         }
 
         
-        if !layer.opaque_items.is_empty() {
+        let opaque_items = layer.occlusion.opaque_items();
+        if !opaque_items.is_empty() {
             let opaque_sampler = self.gpu_profiler.start_sampler(GPU_SAMPLER_TAG_OPAQUE);
             self.set_blend(false, FramebufferKind::Main);
             self.draw_tile_list(
-                layer.opaque_items.iter(),
+                opaque_items.iter(),
                 &composite_state,
                 &composite_state.external_surfaces,
                 projection,
@@ -3455,12 +3453,13 @@ impl Renderer {
         }
 
         
-        if !layer.alpha_items.is_empty() {
+        let alpha_items = layer.occlusion.alpha_items();
+        if !alpha_items.is_empty() {
             let transparent_sampler = self.gpu_profiler.start_sampler(GPU_SAMPLER_TAG_TRANSPARENT);
             self.set_blend(true, FramebufferKind::Main);
             self.set_blend_mode_premultiplied_alpha(FramebufferKind::Main);
             self.draw_tile_list(
-                layer.alpha_items.iter(),
+                alpha_items.iter().rev(),
                 &composite_state,
                 &composite_state.external_surfaces,
                 projection,
@@ -3486,6 +3485,13 @@ impl Renderer {
         let _gm = self.gpu_profiler.start_marker("framebuffer");
         let _timer = self.gpu_profiler.start_timer(GPU_TAG_COMPOSITE);
 
+        
+        
+        let num_tiles = composite_state.tiles
+            .iter()
+            .filter(|tile| tile.kind != TileKind::Clear).count();
+        self.profile.set(profiler::PICTURE_TILES, num_tiles);
+
         let window_is_opaque = match self.compositor_config.layer_compositor() {
             Some(ref compositor) => {
                 let props = compositor.get_window_properties();
@@ -3497,20 +3503,8 @@ impl Renderer {
         let mut input_layers: Vec<CompositorInputLayer> = Vec::new();
         let mut swapchain_layers = Vec::new();
         let cap = composite_state.tiles.len();
-        let mut occlusion = occlusion::FrontToBackBuilder::with_capacity(cap, cap);
-        let mut clear_tiles = Vec::new();
-
-        
-        
-        let num_tiles = composite_state.tiles
-            .iter()
-            .filter(|tile| tile.kind != TileKind::Clear).count();
-        self.profile.set(profiler::PICTURE_TILES, num_tiles);
 
         for (idx, tile) in composite_state.tiles.iter().enumerate() {
-            
-            let is_opaque = tile.kind != TileKind::Alpha;
-
             let device_tile_box = composite_state.get_device_rect(
                 &tile.local_rect,
                 tile.transform_index
@@ -3535,30 +3529,6 @@ impl Renderer {
             if rect.is_empty() {
                 continue;
             }
-
-            
-            
-            match tile.kind {
-                TileKind::Opaque | TileKind::Alpha => {
-                    
-                    occlusion.add(&rect, is_opaque, idx);
-                }
-                TileKind::Clear => {
-                    
-                    
-                    
-                    
-                    clear_tiles.push(occlusion::Item { rectangle: rect, key: idx });
-                }
-            }
-        }
-
-        assert_eq!(swapchain_layers.len(), input_layers.len());
-
-        for item in occlusion.opaque_items().iter().chain(occlusion.alpha_items().iter().rev()) {
-            let tile = &composite_state.tiles[item.key];
-            
-            let is_opaque = tile.kind != TileKind::Alpha;
 
             
             let usage = match tile.surface {
@@ -3648,6 +3618,7 @@ impl Renderer {
                         );
 
                         let clip_rect = tile.device_clip_rect.to_i32();
+                        let is_opaque = tile.kind != TileKind::Alpha;
 
                         (rect.min.to_i32(), clip_rect, is_opaque)
                     }
@@ -3662,24 +3633,36 @@ impl Renderer {
                 });
 
                 swapchain_layers.push(SwapChainLayer {
-                    opaque_items: Vec::new(),
-                    alpha_items: Vec::new(),
                     clear_tiles: Vec::new(),
+                    occlusion: occlusion::FrontToBackBuilder::with_capacity(cap, cap),
                 })
             }
 
-            let item = occlusion::Item {
-                rectangle: item.rectangle,
-                key: item.key,
-            };
-
+            
+            
             let layer = swapchain_layers.last_mut().unwrap();
-            if is_opaque {
-                layer.opaque_items.push(item);
-            } else {
-                layer.alpha_items.push(item);
+
+            
+            match tile.kind {
+                TileKind::Opaque => {
+                    
+                    layer.occlusion.add(&rect, true, idx);
+                }
+                TileKind::Alpha => {
+                    
+                    layer.occlusion.add(&rect, false, idx);
+                }
+                TileKind::Clear => {
+                    
+                    
+                    
+                    
+                    layer.clear_tiles.push(occlusion::Item { rectangle: rect, key: idx });
+                }
             }
         }
+
+        assert_eq!(swapchain_layers.len(), input_layers.len());
 
         
         
@@ -3693,9 +3676,8 @@ impl Renderer {
             });
 
             swapchain_layers.push(SwapChainLayer {
-                opaque_items: Vec::new(),
-                alpha_items: Vec::new(),
                 clear_tiles: Vec::new(),
+                occlusion: occlusion::FrontToBackBuilder::with_capacity(cap, cap),
             })
         }
 
@@ -3711,9 +3693,8 @@ impl Renderer {
             });
 
             swapchain_layers.push(SwapChainLayer {
-                opaque_items: Vec::new(),
-                alpha_items: Vec::new(),
                 clear_tiles: Vec::new(),
+                occlusion: occlusion::FrontToBackBuilder::with_capacity(cap, cap),
             })
         }
 
@@ -3768,7 +3749,6 @@ impl Renderer {
                 projection,
                 results,
                 partial_present_mode,
-                &occlusion,
                 swapchain_layer,
             );
 
