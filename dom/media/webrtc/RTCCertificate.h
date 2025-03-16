8b0be6cfdde9079657b1e74829ed509c2a5de839
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-03-16 02:54:49 +0000

    Bug 1525241 - implement RTCCertificate.getFingerprints method. r=bwc,webidl,smaug
    Differential Revision: https://phabricator.services.mozilla.com/D241112
    
    UltraBlame original commit: e788fc9baa880e7859f6156631aa2346a79199ae

--- dom/media/webrtc/RTCCertificate.h
+++ dom/media/webrtc/RTCCertificate.h
@@ -40,6 +40,7 @@ namespace dom {
 class GlobalObject;
 class ObjectOrString;
 class Promise;
+struct RTCDtlsFingerprint;
 
 class RTCCertificate final : public nsISupports, public nsWrapperCache {
  public:
@@ -63,6 +64,7 @@ class RTCCertificate final : public nsISupports, public nsWrapperCache {
   
   
   uint64_t Expires() const { return mExpires / PR_USEC_PER_MSEC; }
+  void GetFingerprints(nsTArray<dom::RTCDtlsFingerprint>& aFingerprintsOut);
 
   
   RefPtr<DtlsIdentity> CreateDtlsIdentity() const;
@@ -90,6 +92,7 @@ class RTCCertificate final : public nsISupports, public nsWrapperCache {
   UniqueCERTCertificate mCertificate;
   SSLKEAType mAuthType;
   PRTime mExpires;
+  nsTArray<RTCDtlsFingerprint> mFingerprints;
 };
 
 }  
