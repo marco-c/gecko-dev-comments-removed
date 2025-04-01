142274bb1575360d756d9bbe92ba0e05f3a378cb
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-04-01 03:18:04 +0000

    Bug 1956738 - Use the new TypeScript path resolution for toolkit/components/{search|translations}. r=zombie,translations-reviewers,gregtatum
    Differential Revision: https://phabricator.services.mozilla.com/D243260
    
    UltraBlame original commit: 5af84c93433b942cf775eb02181e3e384ad8c4f9

--- tools/@types/index.d.ts
+++ tools/@types/index.d.ts
@@ -18,11 +18,6 @@
 
 
 
-
-declare module "resource://gre/modules/AppConstants.sys.mjs" {
-  const AppConstants: typeof import("./substitutions/AppConstants.sys.d.mts").AppConstants;
-}
-
 declare global {
   const Cc: nsXPCComponents_Classes;
   const Ci: nsIXPCComponents_Interfaces;
