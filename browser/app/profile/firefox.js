e9fc5d77b1f2e94a65bb80a84597d6d42a3da7f6
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-04-01 03:23:03 +0000

    Backed out changeset 331377959f1d (bug 1954653) for causing newtab failures in activity-stream.html CLOSED TREE
    UltraBlame original commit: 4f90e0df61d13ab2a3db351430da22bdba31f158

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1940,9 +1940,6 @@ pref("browser.newtabpage.activity-stream.discoverystream.spocMessageVariant", ""
 pref("browser.newtabpage.activity-stream.discoverystream.sendToPocket.enabled", true);
 
 
-pref("browser.newtabpage.activity-stream.discoverystream.reportContent.enabled", false);
-
-
 pref("browser.newtabpage.activity-stream.discoverystream.region-stories-block", "");
 
 #ifdef NIGHTLY_BUILD
