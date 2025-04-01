7735682f20e0a1643cd268909b5fda4760baf2d9
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-04-01 02:46:49 +0000

    Bug 1931334 - [bidi] Fix race condition in NetworkDecodedBodySizeMap r=webdriver-reviewers,whimboo
    Differential Revision: https://phabricator.services.mozilla.com/D239495
    
    UltraBlame original commit: be6aafb658cc300fffab7649582742645b671f73

--- remote/shared/test/xpcshell/head.js
+++ remote/shared/test/xpcshell/head.js
@@ -23,3 +23,19 @@ function trackPromise(promise) {
 
   return trackedPromise;
 }
+
+const hasPromiseResolved = async function (promise) {
+  let resolved = false;
+  promise.finally(() => (resolved = true)).catch(() => {});
+  
+  await new Promise(resolve => Services.tm.dispatchToMainThread(resolve));
+  return resolved;
+};
+
+const hasPromiseRejected = async function (promise) {
+  let rejected = false;
+  promise.catch(() => (rejected = true));
+  
+  await new Promise(resolve => Services.tm.dispatchToMainThread(resolve));
+  return rejected;
+};
