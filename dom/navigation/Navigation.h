697548d139c873bfb3120f6587e7c94029a6b038
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-04-01 01:34:55 +0000

    Bug 1948254 - Part 4: Implement #navigate-event-firing. r=webidl,smaug,jjaschke
    This includes:
    
    * #fire-a-traverse-navigate-event
    * #fire-a-push/replace/reload-navigate-event
    * #fire-a-download-request-navigate-event
    
    which are all implemented in terms of #inner-navigate-event-firing-algorithm.
    
    Actually calling any of the above is only added for History.pushState and
    History.replaceState using #fire-a-push/replace/reload-navigate-event.
    
    Differential Revision: https://phabricator.services.mozilla.com/D238786
    
    UltraBlame original commit: 9e352a9fb9979aa119759cfcf7a0aa5651ed6695

--- dom/navigation/Navigation.h
+++ dom/navigation/Navigation.h
@@ -7,12 +7,24 @@
 #ifndef mozilla_dom_Navigation_h___
 #define mozilla_dom_Navigation_h___
 
+#include "nsHashtablesFwd.h"
+#include "nsStringFwd.h"
+
+#include "mozilla/AlreadyAddRefed.h"
 #include "mozilla/DOMEventTargetHelper.h"
+#include "mozilla/Maybe.h"
+#include "mozilla/RefPtr.h"
+
+#include "mozilla/dom/NavigateEvent.h"
 #include "mozilla/dom/NavigationBinding.h"
+
+class nsIDHashKey;
 
 namespace mozilla::dom {
 
+class FormData;
 class NavigationActivation;
+class NavigationDestination;
 class NavigationHistoryEntry;
 struct NavigationNavigateOptions;
 struct NavigationOptions;
@@ -22,6 +34,8 @@ struct NavigationReloadOptions;
 struct NavigationResult;
 
 class SessionHistoryInfo;
+
+struct NavigationAPIMethodTracker;
 
 class Navigation final : public DOMEventTargetHelper {
  public:
@@ -37,8 +51,8 @@ class Navigation final : public DOMEventTargetHelper {
   void UpdateCurrentEntry(JSContext* aCx,
                           const NavigationUpdateCurrentEntryOptions& aOptions,
                           ErrorResult& aRv);
-  already_AddRefed<NavigationTransition> GetTransition() { return {}; }
-  already_AddRefed<NavigationActivation> GetActivation() { return {}; }
+  NavigationTransition* GetTransition() const;
+  NavigationActivation* GetActivation() const;
 
   bool CanGoBack() {
     return !HasEntriesAndEventsDisabled() && mCurrentEntryIndex &&
@@ -89,7 +103,32 @@ class Navigation final : public DOMEventTargetHelper {
   static bool IsAPIEnabled(JSContext*  = nullptr,
                            JSObject*  = nullptr);
 
+  
+  
+
+  MOZ_CAN_RUN_SCRIPT bool FireTraverseNavigateEvent(
+      SessionHistoryInfo* aDestinationSessionHistoryInfo,
+      Maybe<UserNavigationInvolvement> aUserInvolvement);
+
+  MOZ_CAN_RUN_SCRIPT bool FirePushReplaceReloadNavigateEvent(
+      NavigationType aNavigationType, nsIURI* aDestinationURL,
+      bool aIsSameDocument, Maybe<UserNavigationInvolvement> aUserInvolvement,
+      Element* aSourceElement, Maybe<const FormData&> aFormDataEntryList,
+      nsIStructuredCloneContainer* aNavigationAPIState,
+      nsIStructuredCloneContainer* aClassicHistoryAPIState);
+
+  MOZ_CAN_RUN_SCRIPT bool FireDownloadRequestNavigateEvent(
+      nsIURI* aDestinationURL, UserNavigationInvolvement aUserInvolvement,
+      Element* aSourceElement, const nsAString& aFilename);
+
+  bool FocusedChangedDuringOngoingNavigation() const;
+  void SetFocusedChangedDuringOngoingNavigation(
+      bool aFocusChangedDUringOngoingNavigation);
+
  private:
+  using UpcomingTraverseAPIMethodTrackers =
+      nsTHashMap<nsIDHashKey, RefPtr<NavigationAPIMethodTracker>>;
+
   ~Navigation() = default;
 
   
@@ -100,12 +139,56 @@ class Navigation final : public DOMEventTargetHelper {
       const RefPtr<NavigationHistoryEntry>& aPreviousEntry,
       nsTArray<RefPtr<NavigationHistoryEntry>>&& aDisposedEntries);
 
+  nsresult FireEvent(const nsAString& aName);
+
+  
+  MOZ_CAN_RUN_SCRIPT bool InnerFireNavigateEvent(
+      NavigationType aNavigationType, NavigationDestination* aDestination,
+      UserNavigationInvolvement aUserInvolvement, Element* aSourceElement,
+      Maybe<const FormData&> aFormDataEntryList,
+      nsIStructuredCloneContainer* aClassicHistoryAPIState,
+      const nsAString& aDownloadRequestFilename);
+
+  NavigationHistoryEntry* FindNavigationHistoryEntry(
+      SessionHistoryInfo* aSessionHistoryInfo) const;
+
+  void PromoteUpcomingAPIMethodTrackerToOngoing(Maybe<nsID>&& aDestinationKey);
+
+  static void CleanUp(NavigationAPIMethodTracker* aNavigationAPIMethodTracker);
+
+  void AbortOngoingNavigation();
+
   void LogHistory() const;
 
   
   nsTArray<RefPtr<NavigationHistoryEntry>> mEntries;
+
   
   Maybe<uint64_t> mCurrentEntryIndex;
+
+  
+  RefPtr<NavigateEvent> mOngoingNavigateEvent;
+
+  
+  bool mFocusChangedDUringOngoingNavigation = false;
+
+  
+  bool mSuppressNormalScrollRestorationDuringOngoingNavigation = false;
+
+  
+  RefPtr<NavigationAPIMethodTracker> mOngoingAPIMethodTracker;
+
+  
+  RefPtr<NavigationAPIMethodTracker> mUpcomingNonTraverseAPIMethodTracker;
+
+  
+  UpcomingTraverseAPIMethodTrackers mUpcomingTraverseAPIMethodTrackers;
+
+  
+  RefPtr<NavigationTransition> mTransition;
+
+  
+  RefPtr<NavigationActivation> mActivation;
 };
 
 }  
