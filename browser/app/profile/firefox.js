5dc4f9390fdb76c4fd532e4807aa3338e3ee93f0
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-04-01 02:17:15 +0000

    Bug 1954648 - Smart Tab Group settings in Preferences->Tabs  r=ngrato,fluent-reviewers,settings-reviewers,tabbrowser-reviewers,dwalker,flod
    Differential Revision: https://phabricator.services.mozilla.com/D241922
    
    UltraBlame original commit: b5bcc402cde9d2eb54bbf9873d012b1ff8cba116

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1070,6 +1070,8 @@ pref("browser.tabs.dragDrop.moveOverThresholdPercent", 80);
 
 pref("browser.tabs.firefox-view.logLevel", "Warn");
 
+pref("browser.tabs.groups.smart.userEnabled", true);
+
 
 
 pref("security.allow_eval_with_system_principal", false);
