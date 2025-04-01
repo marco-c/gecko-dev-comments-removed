5f0a741941a7b35dac152fb2963814074d6d40fc
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-04-01 03:42:39 +0000

    Bug 1956540 - Allow to initiate the add-on update flow from `about:addons`. r=rpl,fluent-reviewers,bolsson
    Differential Revision: https://phabricator.services.mozilla.com/D243118
    
    UltraBlame original commit: eb1fe7698b54ba3542885136998c9f70f4e74299

--- toolkit/mozapps/extensions/test/browser/browser_page_options_install_addon.js
+++ toolkit/mozapps/extensions/test/browser/browser_page_options_install_addon.js
@@ -4,10 +4,39 @@
 
 
 
-var MockFilePicker = SpecialPowers.MockFilePicker;
-MockFilePicker.init(window.browsingContext);
+const { AddonTestUtils } = ChromeUtils.importESModule(
+  "resource://testing-common/AddonTestUtils.sys.mjs"
+);
 
-async function checkInstallConfirmation(...names) {
+var MockFilePicker = SpecialPowers.MockFilePicker;
+
+AddonTestUtils.initMochitest(this);
+
+const loadExtensionView = async () => {
+  let win = await loadInitialView("extension");
+  let doc = win.document;
+
+  let pageOptionsMenu = doc.querySelector("addon-page-options panel-list");
+
+  function openPageOptions() {
+    let opened = BrowserTestUtils.waitForEvent(pageOptionsMenu, "shown");
+    pageOptionsMenu.open = true;
+    return opened;
+  }
+
+  function closePageOptions() {
+    let closed = BrowserTestUtils.waitForEvent(pageOptionsMenu, "hidden");
+    pageOptionsMenu.open = false;
+    return closed;
+  }
+
+  return [win, openPageOptions, closePageOptions];
+};
+
+async function checkInstallConfirmation(
+  names,
+  { confirmInstall = false, expectUpdate = false } = {}
+) {
   let notificationCount = 0;
   let observer = {
     observe(aSubject) {
@@ -30,26 +59,47 @@ async function checkInstallConfirmation(...names) {
   };
   Services.obs.addObserver(observer, "addon-install-started");
 
-  let results = [];
+  
+  
+  
+  if (expectUpdate) {
+    await waitAppMenuNotificationShown(
+      "addon-installed",
+       null,
+       true
+    );
+  } else {
+    let results = [];
 
-  let promise = promisePopupNotificationShown("addon-webext-permissions");
-  for (let i = 0; i < names.length; i++) {
-    let panel = await promise;
-    let name = panel.getAttribute("name");
-    results.push(name);
+    let promise = promisePopupNotificationShown("addon-webext-permissions");
+    for (let i = 0; i < names.length; i++) {
+      let panel = await promise;
+      let name = panel.getAttribute("name");
+      results.push(name);
 
-    info(`Saw install for ${name}`);
-    if (results.length < names.length) {
-      info(
-        `Waiting for installs for ${names.filter(n => !results.includes(n))}`
-      );
+      info(`Saw install for ${name}`);
+      if (results.length < names.length) {
+        info(
+          `Waiting for installs for ${names.filter(n => !results.includes(n))}`
+        );
 
-      promise = promisePopupNotificationShown("addon-webext-permissions");
+        promise = promisePopupNotificationShown("addon-webext-permissions");
+      }
+      if (confirmInstall) {
+        const postInstallPromptShown = waitAppMenuNotificationShown(
+          "addon-installed",
+           null,
+           true
+        );
+        panel.button.click();
+        await postInstallPromptShown;
+      } else {
+        panel.secondaryButton.click();
+      }
     }
-    panel.secondaryButton.click();
-  }
 
-  Assert.deepEqual(results.sort(), names.sort(), "Got expected installs");
+    Assert.deepEqual(results.sort(), names.sort(), "Got expected installs");
+  }
 
   is(
     notificationCount,
@@ -58,6 +108,13 @@ async function checkInstallConfirmation(...names) {
   );
   Services.obs.removeObserver(observer, "addon-install-started");
 }
+
+add_setup(() => {
+  MockFilePicker.init(window.browsingContext);
+  registerCleanupFunction(() => {
+    MockFilePicker.cleanup();
+  });
+});
 
 add_task(async function test_install_from_file() {
   let win = await loadInitialView("extension");
@@ -74,10 +131,10 @@ add_task(async function test_install_from_file() {
 
   
   
-  let pInstallURIClosed = checkInstallConfirmation(
+  let pInstallURIClosed = checkInstallConfirmation([
     "Drag Drop test 1",
-    "Drag Drop test 2"
-  );
+    "Drag Drop test 2",
+  ]);
 
   win.document
     .querySelector('#page-options [action="install-from-file"]')
@@ -85,31 +142,22 @@ add_task(async function test_install_from_file() {
 
   await pInstallURIClosed;
 
-  MockFilePicker.cleanup();
   await closeView(win);
 });
 
 add_task(async function test_install_disabled() {
-  let win = await loadInitialView("extension");
-  let doc = win.document;
-
-  let pageOptionsMenu = doc.querySelector("addon-page-options panel-list");
-
-  function openPageOptions() {
-    let opened = BrowserTestUtils.waitForEvent(pageOptionsMenu, "shown");
-    pageOptionsMenu.open = true;
-    return opened;
-  }
-
-  function closePageOptions() {
-    let closed = BrowserTestUtils.waitForEvent(pageOptionsMenu, "hidden");
-    pageOptionsMenu.open = false;
-    return closed;
-  }
+  const [win, openPageOptions, closePageOptions] = await loadExtensionView();
 
   await openPageOptions();
-  let installButton = doc.querySelector('[action="install-from-file"]');
+  let installButton = win.document.querySelector(
+    '[action="install-from-file"]'
+  );
   ok(!installButton.hidden, "The install button is shown");
+  Assert.equal(
+    installButton.getAttribute("data-l10n-id"),
+    "addon-install-from-file",
+    "got expected l10n ID on the button"
+  );
   await closePageOptions();
 
   await SpecialPowers.pushPrefEnv({ set: [[PREF_XPI_ENABLED, false]] });
@@ -125,4 +173,69 @@ add_task(async function test_install_disabled() {
   await closePageOptions();
 
   await closeView(win);
+});
+
+add_task(async function test_install_from_file_with_pref_set() {
+  const addonId = "dragdrop-1@tests.mozilla.org";
+
+  await SpecialPowers.pushPrefEnv({
+    set: [
+      [
+        "extensions.webextensions.prefer-update-over-install-for-existing-addon",
+        true,
+      ],
+    ],
+  });
+  const [win, openPageOptions, closePageOptions] = await loadExtensionView();
+
+  await openPageOptions();
+  let installButton = win.document.querySelector(
+    '#page-options [action="install-from-file"]'
+  );
+  Assert.equal(
+    installButton.getAttribute("data-l10n-id"),
+    "addon-install-or-update-from-file",
+    "got expected l10n ID on the button"
+  );
+  await closePageOptions();
+
+  info("install add-on");
+  let fileUrl = get_addon_file_url("browser_dragdrop1.xpi");
+  MockFilePicker.setFiles([fileUrl.file]);
+
+  
+  
+  
+  let pInstallURIClosed = checkInstallConfirmation(["Drag Drop test 1"], {
+    confirmInstall: true,
+  });
+  let addonStarted = AddonTestUtils.promiseWebExtensionStartup(addonId);
+  installButton.click();
+  await Promise.all([pInstallURIClosed, addonStarted]);
+
+  let addon = await AddonManager.getAddonByID(addonId);
+  ok(!!addon, "Got add-on installed");
+  is(addon.version, "1.0", "Got correct add-on version");
+
+  info("update add-on");
+  fileUrl = get_addon_file_url("browser_dragdrop1.1.xpi");
+  MockFilePicker.setFiles([fileUrl.file]);
+
+  
+  
+  
+  pInstallURIClosed = checkInstallConfirmation(["Drag Drop test 1.1"], {
+    confirmInstall: true,
+    expectUpdate: true,
+  });
+  addonStarted = AddonTestUtils.promiseWebExtensionStartup(addonId);
+  installButton.click();
+  await Promise.all([pInstallURIClosed, addonStarted]);
+
+  addon = await AddonManager.getAddonByID(addonId);
+  is(addon.version, "1.1", "Got correct add-on version after update");
+
+  await closeView(win);
+  await addon.uninstall();
+  await SpecialPowers.popPrefEnv();
 });
