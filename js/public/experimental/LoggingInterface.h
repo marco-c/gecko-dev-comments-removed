97552e2893931dd724affe20ff22439b9f247220
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-04-01 02:53:36 +0000

    Bug 1951723 - Add Formatted Logging to JS Logging. r=mgaudet
    Differential Revision: https://phabricator.services.mozilla.com/D241743
    
    UltraBlame original commit: 1122eecaae255aa3c16236cfc60a995c91ecf898

--- js/public/experimental/LoggingInterface.h
+++ js/public/experimental/LoggingInterface.h
@@ -13,6 +13,7 @@
 #include "mozilla/LoggingCore.h"
 
 #include "jstypes.h"
+#include "fmt/format.h"
 #include "js/GCAPI.h"
 
 struct JSContext;
@@ -55,6 +56,9 @@ struct LoggingInterface {
                      const char* aFmt, va_list ap)
       MOZ_FORMAT_PRINTF(3, 0) = nullptr;
 
+  void (*logPrintFMT)(const OpaqueLogger aModule, mozilla::LogLevel aLevel,
+                      fmt::string_view, fmt::format_args);
+
   
   
   
@@ -69,6 +73,13 @@ struct LoggingInterface {
     va_start(ap, aFmt);
     this->logPrintVA(aModule, aLevel, aFmt, ap);
     va_end(ap);
+  }
+
+  template <typename... T>
+  void logPrintFmt(const OpaqueLogger aModule, mozilla::LogLevel aLevel,
+                   fmt::format_string<T...> aFmt, T&&... aArgs) {
+    JS::AutoSuppressGCAnalysis suppress;
+    this->logPrintFMT(aModule, aLevel, aFmt, fmt::make_format_args(aArgs...));
   }
 
   
