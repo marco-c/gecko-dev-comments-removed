7e12d749aaf2565ac65b45dd0684dae4a52ce6e9
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-04-01 02:15:28 +0000

    Bug 1954641 - Part 1: Do not use SharedScriptCache in system principal. r=nbp
    Differential Revision: https://phabricator.services.mozilla.com/D242316
    
    UltraBlame original commit: 080ff23bfeb45639809625579c86730c5da7117f

--- dom/script/ScriptLoader.cpp
+++ dom/script/ScriptLoader.cpp
@@ -215,7 +215,13 @@ ScriptLoader::ScriptLoader(Document* aDocument)
       dom_script_loader_external_scripts_speculative_omt_parse_enabled();
 
 #ifdef NIGHTLY_BUILD
-  if (StaticPrefs::dom_script_loader_navigation_cache()) {
+  
+  
+  
+  
+  
+  if (!LoaderPrincipal()->IsSystemPrincipal() &&
+      StaticPrefs::dom_script_loader_navigation_cache()) {
     mCache = SharedScriptCache::Get();
     RegisterToCache();
   }
@@ -3050,6 +3056,12 @@ void ScriptLoader::InstantiateClassicScriptFromAny(
 ScriptLoader::CacheBehavior ScriptLoader::GetCacheBehavior(
     ScriptLoadRequest* aRequest) {
   if (!mCache) {
+    return CacheBehavior::DoNothing;
+  }
+
+  if (!aRequest->mURI->SchemeIs("http") && !aRequest->mURI->SchemeIs("https")) {
+    
+    
     return CacheBehavior::DoNothing;
   }
 
