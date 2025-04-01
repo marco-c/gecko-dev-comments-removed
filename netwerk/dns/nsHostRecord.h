e7e2a0b9af144e60d7a35a2d0420ac2f5cd3fcbd
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-04-01 02:44:15 +0000

    Bug 1952221 - Add telemetry for grace period induced DNS cache renewals. r=necko-reviewers,valentin
    Differential Revision: https://phabricator.services.mozilla.com/D242027
    
    UltraBlame original commit: d50b0d4c7302a064752090ed463a9aeb42c11383

--- netwerk/dns/nsHostRecord.h
+++ netwerk/dns/nsHostRecord.h
@@ -259,12 +259,8 @@ class nsHostRecord : public mozilla::LinkedListElement<RefPtr<nsHostRecord>>,
 };
 
 
-#define ADDRHOSTRECORD_IID                           \
-  {                                                  \
-    0xb020e996, 0xf6ab, 0x45e5, {                    \
-      0x9b, 0xf5, 0x1d, 0xa7, 0x1d, 0xd0, 0x05, 0x3a \
-    }                                                \
-  }
+#define ADDRHOSTRECORD_IID \
+  {0xb020e996, 0xf6ab, 0x45e5, {0x9b, 0xf5, 0x1d, 0xa7, 0x1d, 0xd0, 0x05, 0x3a}}
 
 class AddrHostRecord final : public nsHostRecord {
   using Mutex = mozilla::Mutex;
@@ -357,12 +353,8 @@ class AddrHostRecord final : public nsHostRecord {
 NS_DEFINE_STATIC_IID_ACCESSOR(AddrHostRecord, ADDRHOSTRECORD_IID)
 
 
-#define TYPEHOSTRECORD_IID                           \
-  {                                                  \
-    0x77b786a7, 0x04be, 0x44f2, {                    \
-      0x98, 0x7c, 0xab, 0x8a, 0xa9, 0x66, 0x76, 0xe0 \
-    }                                                \
-  }
+#define TYPEHOSTRECORD_IID \
+  {0x77b786a7, 0x04be, 0x44f2, {0x98, 0x7c, 0xab, 0x8a, 0xa9, 0x66, 0x76, 0xe0}}
 
 class TypeHostRecord final : public nsHostRecord,
                              public nsIDNSTXTRecord,
