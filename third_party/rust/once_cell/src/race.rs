831a441b9767c911f78533e9ea130a42c9a5fbed
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-04-01 01:54:10 +0000

    Bug 1955085 - build(rust): audit and update `once_cell` 1.20.3 → 1.21.1 r=supply-chain-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D242216
    
    UltraBlame original commit: bc03f14a7c66762dab4258627c1206c1e388b128

--- third_party/rust/once_cell/src/race.rs
+++ third_party/rust/once_cell/src/race.rs
@@ -54,6 +54,47 @@ impl OnceNonZeroUsize {
     
     
     
+    
+    
+    
+    pub unsafe fn get_unchecked(&self) -> NonZeroUsize {
+        #[inline(always)]
+        fn as_const_ptr(r: &AtomicUsize) -> *const usize {
+            use core::mem::align_of;
+
+            let p: *const AtomicUsize = r;
+            
+            
+            
+            
+            const _ALIGNMENT_COMPATIBLE: () =
+                assert!(align_of::<AtomicUsize>() % align_of::<usize>() == 0);
+            p.cast::<usize>()
+        }
+
+        
+        
+        let p = as_const_ptr(&self.inner);
+
+        
+        
+        
+        
+        
+        
+        
+        
+        let val = unsafe { p.read() };
+
+        
+        
+        unsafe { NonZeroUsize::new_unchecked(val) }
+    }
+
+    
+    
+    
+    
     #[inline]
     pub fn set(&self, value: NonZeroUsize) -> Result<(), ()> {
         let exchange =
@@ -333,6 +374,11 @@ mod once_box {
         }
 
         
+        pub fn with_value(value: Box<T>) -> Self {
+            OnceBox { inner: AtomicPtr::new(Box::into_raw(value)), ghost: PhantomData }
+        }
+
+        
         pub fn get(&self) -> Option<&T> {
             let ptr = self.inner.load(Ordering::Acquire);
             if ptr.is_null() {
@@ -409,6 +455,15 @@ mod once_box {
     }
 
     unsafe impl<T: Sync + Send> Sync for OnceBox<T> {}
+
+    impl<T: Clone> Clone for OnceBox<T> {
+        fn clone(&self) -> Self {
+            match self.get() {
+                Some(value) => OnceBox::with_value(Box::new(value.clone())),
+                None => OnceBox::new(),
+            }
+        }
+    }
 
     
     
