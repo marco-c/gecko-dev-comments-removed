9574fbbb6db9d082d8edc5f2341cc82e133fa397
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-04-01 01:05:10 +0000

    Bug 1952421 [wpt PR 51183] - Add WPT for navigation request content encoding, a=testonly
    Automatic update from web-platform-tests
    Add WPT for navigation request content encoding
    
    I noticed that there are no WPT for navigation request content encoding.
    
    So this CL adds WPT of navigating br/gzip/zstd encoded page.
    
    Bug: 391950057
    Change-Id: I55125e2eb219499be8cfc0aef5c5067f68d19ef9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6329859
    Reviewed-by: Shunya Shishido <sisidovskichromium.org>
    Commit-Queue: Tsuyoshi Horo <horochromium.org>
    Cr-Commit-Position: refs/heads/main{#1429286}
    
    --
    
    wpt-commits: fa4477ae5ce31b672238ea37759642369b4eaec2
    wpt-pr: 51183
    
    UltraBlame original commit: 87a9c55acb0159feaf27df508abb8c26509406a2

---
+++ testing/web-platform/tests/fetch/content-encoding/gzip/gzip-navigation.https.window.js
@@ -0,0 +1,6 @@
+
+
+promise_test(async t => {
+  const frame = await with_iframe('./resources/hello.html.gz');
+  assert_equals(frame.contentDocument.body.textContent, 'Hello world');
+}, `Naigation to gzip encoded page`);
---
+++ testing/web-platform/tests/fetch/content-encoding/zstd/zstd-navigation.https.window.js
@@ -0,0 +1,6 @@
+
+
+promise_test(async t => {
+  const frame = await with_iframe('./resources/hello.html.zst');
+  assert_equals(frame.contentDocument.body.textContent, 'Hello world');
+}, `Naigation to zstd encoded page`);
