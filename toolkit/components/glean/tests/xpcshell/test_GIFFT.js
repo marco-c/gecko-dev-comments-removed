2f3eac9c8081e6cf1c44c1998697d14d52ce9e67
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-04-01 01:53:16 +0000

    Bug 1954857 - Mirror calls to labeled_timing_distribution's sample APIs r=TravisLong
    It's not consistent with the non-labeled case which converts to time_unit units
    because labeled_timing_distribution doesn't know them.
    
    But it's no more inconsistent than before, and unblocks further FOG Migration.
    
    Hopefully in bug 1943453 we'll figure out a way to get around the weirdness.
    And all else fails, Legacy Telemetry is set to be removed (along with GIFFT)
    in the medium term.
    
    Differential Revision: https://phabricator.services.mozilla.com/D242234
    
    UltraBlame original commit: 10296d0618747f84b919200562289c5e4726d825

--- toolkit/components/glean/tests/xpcshell/test_GIFFT.js
+++ toolkit/components/glean/tests/xpcshell/test_GIFFT.js
@@ -782,7 +782,6 @@ add_task(async function test_gifft_labeled_timing_dist() {
   
   
   
-  
   Glean.testOnly.whereHasTheTimeGone["hourglass sands"].accumulateSamples([
     1, 2, 3,
   ]);
@@ -793,7 +792,9 @@ add_task(async function test_gifft_labeled_timing_dist() {
   data = Telemetry.getKeyedHistogramById(
     "TELEMETRY_TEST_MIRROR_FOR_LABELED_TIMING"
   ).snapshot();
-  Assert.ok(!("hourglass sands" in data), "Doesn't have the key");
+  Assert.ok("hourglass sands" in data, "Has the key");
+  data = data["hourglass sands"];
+  Assert.equal(data.sum, 7);
 });
 
 add_task(async function test_gifft_labeled_quantity() {
