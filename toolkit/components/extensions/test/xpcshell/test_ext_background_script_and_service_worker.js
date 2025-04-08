05b9e5b0cde22658feb1c5c72f21b086b6423133
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-04-08 15:01:53 +0000

    Bug 1953937 - Show service_worker warning only when relevant r=rpl
    Differential Revision: https://phabricator.services.mozilla.com/D241498
    
    UltraBlame original commit: c28f8c07e8284c9b8e85812af8743832fdfef41e

--- toolkit/components/extensions/test/xpcshell/test_ext_background_script_and_service_worker.js
+++ toolkit/components/extensions/test/xpcshell/test_ext_background_script_and_service_worker.js
@@ -78,6 +78,11 @@ add_task(
   { skip_if: () => !WebExtensionPolicy.backgroundServiceWorkerEnabled },
   async function test_service_worker_and_document() {
     await testExtensionWithBackground({
+      with_service_worker: true,
+      expected_background_type: "service_worker",
+      expected_manifest_warnings: [],
+    });
+    await testExtensionWithBackground({
       with_scripts: true,
       with_service_worker: true,
       expected_background_type: "scripts",
@@ -99,6 +104,7 @@ add_task(
       with_service_worker: true,
       expected_background_type: "page",
       expected_manifest_warnings: [
+        "Reading manifest: Warning processing background: Both background.page and background.scripts specified. background.scripts will be ignored.",
         "Reading manifest: Warning processing background: with both background.service_worker and background.page, only background.page will be loaded. This can be changed with background.preferred_environment.",
       ],
     });
@@ -147,7 +153,12 @@ add_task(async function test_preferred_environment_unrecognized() {
   await testExtensionWithBackground({
     with_scripts: true,
     with_service_worker: true,
+    
+    
     with_preferred_environment: ["page", "scripts", "service_worker"],
+    
+    
+    
     expected_background_type: WebExtensionPolicy.backgroundServiceWorkerEnabled
       ? "service_worker"
       : "scripts",
@@ -159,10 +170,11 @@ add_task(async function test_preferred_environment_unrecognized() {
   await testExtensionWithBackground({
     with_page: true,
     with_service_worker: true,
+    
     with_preferred_environment: ["serviceWorker", "serviceworker", "worker"],
-    expected_background_type: WebExtensionPolicy.backgroundServiceWorkerEnabled
-      ? "service_worker"
-      : "page",
+    
+    
+    expected_background_type: "page",
     expected_manifest_warnings: [
       'Reading manifest: Warning processing background.preferred_environment: Error processing background.preferred_environment.0: Invalid enumeration value "serviceWorker"',
       'Reading manifest: Warning processing background.preferred_environment: Error processing background.preferred_environment.1: Invalid enumeration value "serviceworker"',
