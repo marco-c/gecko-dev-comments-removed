1f7503f2817b6924e4a2204bd4ed7e5eb8c63495
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-04-09 15:50:42 +0000

    Bug 1610399 - Add support for extended attributes on union types; r=webidl,emilio
    Differential Revision: https://phabricator.services.mozilla.com/D60520
    
    UltraBlame original commit: c979cd68da16b5eaf5b2a9cd6b40a97a0813140a

--- dom/bindings/test/TestFunctions.cpp
+++ dom/bindings/test/TestFunctions.cpp
@@ -278,6 +278,9 @@ void TestFunctions::TestUnionOfBufferSource(
 void TestFunctions::TestUnionOfAllowSharedBufferSource(
     const AllowSharedBufferSource& aUnion) {}
 
+void TestFunctions::TestUnionWithAllowShared(
+    const MaybeSharedInt8ArrayOrMaybeSharedInt16Array& aUnion) {}
+
 bool TestFunctions::ObjectFromAboutBlank(JSContext* aCx, JSObject* aObj) {
   
   
