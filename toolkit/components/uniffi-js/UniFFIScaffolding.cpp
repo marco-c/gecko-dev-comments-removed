711090c44447a1c2656ad900520f4cc77d532289
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-04-16 01:17:24 +0000

    Bug 1951241 - Refactor ScaffoldingConverter.h, r=markh,nika,frontend-codestyle-reviewers
    Replaced `ScaffoldingConverter` with a set of `FfiValue*` classes.  The
    differences are:
    
    * The new classes better match other `uniffi-bindgen-gecko-js` names,
      and also use familiar UniFFI terms like `Lift` and `Lower`.
    * Object handles are now freed if there's an error.
    * The new classes store the FFI value internal rather than defining an
      `IntermediateType` associated type.
    * Moved header files into `mozilla/uniffi/` and removed the `UniFFI`
      prefix from the filename.  This avoids weird filenames like
      `UniFFIFfiValue.h`
    
    Differential Revision: https://phabricator.services.mozilla.com/D240696
    
    UltraBlame original commit: 7b01510aee4ef99a94e1aef4b62ff678df6fa7a3

--- toolkit/components/uniffi-js/UniFFIScaffolding.cpp
+++ toolkit/components/uniffi-js/UniFFIScaffolding.cpp
@@ -12,8 +12,8 @@
 #include "mozilla/Maybe.h"
 #include "mozilla/UniquePtr.h"
 #include "mozilla/dom/Promise.h"
-#include "mozilla/dom/UniFFICall.h"
-#include "mozilla/dom/UniFFICallbacks.h"
+#include "mozilla/uniffi/Call.h"
+#include "mozilla/uniffi/Callbacks.h"
 #include "mozilla/dom/UniFFIScaffolding.h"
 
 
