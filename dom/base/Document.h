f57621bcc2aefb3d23cf91c116efeade1112b641
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-04-16 01:16:13 +0000

    Backed out changeset 7f838c1e8e78 (bug 1948596) for causing multiple mochitests failures. CLOSED TREE
    UltraBlame original commit: 13dd8f727b7a9e7daff45a2be4983fd382fb1bce

--- dom/base/Document.h
+++ dom/base/Document.h
@@ -2699,12 +2699,6 @@ class Document : public nsINode,
 
 
 
-  bool CanRewriteURL(nsIURI* aTargetURL) const;
-
-  
-
-
-
   bool IsFullyActive() const {
     nsPIDOMWindowInner* inner = GetInnerWindow();
     return inner && inner->IsFullyActive();
