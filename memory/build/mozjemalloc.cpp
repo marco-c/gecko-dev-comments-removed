1d4967781315bd8d42d1d45f323df8483c926b08
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-04-17 03:41:01 +0000

    Bug 1955878 - Annotate unnecessary lock for Coverity. r=pbone
    Differential Revision: https://phabricator.services.mozilla.com/D245681
    
    UltraBlame original commit: 9b003651dbf08d7aadc553e1ab5a188ceb95977e

--- memory/build/mozjemalloc.cpp
+++ memory/build/mozjemalloc.cpp
@@ -6012,6 +6012,7 @@ inline arena_t* ArenaCollection::GetById(arena_id_t aArenaId, bool aIsPrivate) {
   MOZ_RELEASE_ASSERT(aIsPrivate);
   
   
+  
   MOZ_RELEASE_ASSERT(mArenaIdKey);
   arena_id_t id = (aArenaId << mArenaIdRotation) |
                   (aArenaId >> (sizeof(void*) * 8 - mArenaIdRotation));
