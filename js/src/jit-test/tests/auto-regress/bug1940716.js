94f97cec65c122988a816baa94b9b46f31cb08aa
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-04-17 03:41:41 +0000

    Bug 1940716 - Add test case. r=jandem
    Differential Revision: https://phabricator.services.mozilla.com/D235472
    
    UltraBlame original commit: 5bba79924f4474ff3f3a2eb7b818669149eb3e4d

---
+++ js/src/jit-test/tests/auto-regress/bug1940716.js
@@ -0,0 +1,13 @@
+
+
+function a(b) {
+  c = new WebAssembly.Module(b);
+  return new WebAssembly.Instance(c);
+}
+function d(e) {
+  return a(wasmTextToBinary(e));
+}
+f = [ , Number.MIN_VALUE ]
+let { refTest } = d(`(func (export "refTest") (param externref))`).exports;
+for (h of f)
+  refTest(h);
