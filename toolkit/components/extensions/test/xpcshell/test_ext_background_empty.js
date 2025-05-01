134cb0b4ef717753d590c3a2f9182d8057d71d57
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-05-01 03:15:05 +0000

    Bug 1956847 - remove empty background error r=robwu
    Differential Revision: https://phabricator.services.mozilla.com/D243640
    
    UltraBlame original commit: 938d1d25ffc876261b935f2221c9fc585cd928d5

--- toolkit/components/extensions/test/xpcshell/test_ext_background_empty.js
+++ toolkit/components/extensions/test/xpcshell/test_ext_background_empty.js
@@ -1,7 +1,7 @@
 "use strict";
 
 async function testExtensionWithBackground({
-  background = {},
+  background,
   expected_manifest_warnings,
 }) {
   let extension = ExtensionTestUtils.loadExtension({
@@ -32,5 +32,35 @@ add_task(async function test_empty_background_scripts() {
     expected_manifest_warnings: [
       "Reading manifest: Warning processing background: background.scripts is empty.",
     ],
+  });
+});
+
+add_task(async function test_empty_background_object() {
+  
+  
+
+  const requireAtLeastOneOfWarning =
+    WebExtensionPolicy.backgroundServiceWorkerEnabled
+      ? 'Reading manifest: Warning processing background: Error processing background: background requires at least one of "service_worker", "scripts" or "page".'
+      : 'Reading manifest: Warning processing background: Error processing background: background requires at least one of "scripts" or "page".';
+
+  await testExtensionWithBackground({
+    background: {},
+    expected_manifest_warnings: [requireAtLeastOneOfWarning],
+  });
+
+  await testExtensionWithBackground({
+    background: {
+      unknown: true,
+    },
+    expected_manifest_warnings: [
+      "Reading manifest: Warning processing background.unknown: An unexpected property was found in the WebExtension manifest.",
+      requireAtLeastOneOfWarning,
+    ],
+  });
+
+  await testExtensionWithBackground({
+    background: null,
+    expected_manifest_warnings: [],
   });
 });
