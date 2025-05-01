8b21704a1ca39b2fe7d96c5a44fd9191f6c5d53c
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-05-01 01:14:13 +0000

    Bug 1960383 - Remove vestigial logic related to `browser.translations.panelShown` r=nordzilla,translations-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D245636
    
    UltraBlame original commit: eab4be2b0880fe08c9734b66e533cac6c216dc1b

--- toolkit/components/translations/tests/browser/shared-head.js
+++ toolkit/components/translations/tests/browser/shared-head.js
@@ -1050,7 +1050,6 @@ async function loadTestPage({
         
         ["browser.translations.enable", true],
         ["browser.translations.logLevel", "All"],
-        ["browser.translations.panelShown", true],
         ["browser.translations.automaticallyPopup", true],
         ["browser.translations.alwaysTranslateLanguages", ""],
         ["browser.translations.neverTranslateLanguages", ""],
