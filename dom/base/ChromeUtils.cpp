af50f128064d7660f8e1c8393a61c191b27568f5
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-05-01 01:52:02 +0000

    Bug 1961744 - [devtools] Add ChromeUtils.isJSIdentifier . r=arai
    Differential Revision: https://phabricator.services.mozilla.com/D246178
    
    UltraBlame original commit: 0702f6b7a84ca60dc03d31d874d2814d64d66650

--- dom/base/ChromeUtils.cpp
+++ dom/base/ChromeUtils.cpp
@@ -2549,6 +2549,11 @@ void ChromeUtils::NotifyDevToolsClosed(GlobalObject& aGlobal) {
   ChromeUtils::sDevToolsOpenedCount--;
 }
 
+
+bool ChromeUtils::IsJSIdentifier(GlobalObject& aGlobal, const nsAString& aStr) {
+  return JS_IsIdentifier(aStr.BeginReading(), aStr.Length());
+}
+
 #ifdef MOZ_WMF_CDM
 
 already_AddRefed<Promise> ChromeUtils::GetWMFContentDecryptionModuleInformation(
