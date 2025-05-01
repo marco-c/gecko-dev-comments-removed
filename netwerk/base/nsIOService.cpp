fc7bc5b26b641fe5a87b6f166f90c545210ca6d2
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-05-01 02:41:56 +0000

    Backed out 2 changesets (bug 1943804) for causing xpcshell failures  test_ext_proxy_speculative.js
    Backed out changeset d4c83c44d6e2 (bug 1943804)
    Backed out changeset 4d793c8aade9 (bug 1943804)
    
    UltraBlame original commit: 289684f78132916f82909c00d62293c2abdc80ec

--- netwerk/base/nsIOService.cpp
+++ netwerk/base/nsIOService.cpp
@@ -2191,20 +2191,6 @@ nsresult nsIOService::SpeculativeConnectInternal(
     channel->SetLoadFlags(loadFlags);
   }
 
-  if (!aCallbacks) {
-    
-    
-    
-    bool hasProxyFilterRegistered = false;
-    Unused << pps->GetHasProxyFilterRegistered(&hasProxyFilterRegistered);
-    if (hasProxyFilterRegistered) {
-      return NS_ERROR_FAILURE;
-    }
-  } else {
-    rv = channel->SetNotificationCallbacks(aCallbacks);
-    NS_ENSURE_SUCCESS(rv, rv);
-  }
-
   nsCOMPtr<nsICancelable> cancelable;
   RefPtr<IOServiceProxyCallback> callback = new IOServiceProxyCallback(
       aCallbacks, this, std::move(aOriginAttributes));
