69b2e46424a68bbea6ca2203670f6c810e8e6fb0
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-05-01 01:45:43 +0000

    Bug 1961264 - Code Connect for Desktop Components moz-button r=reusable-components-reviewers,mkennedy
    Differential Revision: https://phabricator.services.mozilla.com/D246000
    
    UltraBlame original commit: c28955d8a56fc607acb515f99f84a7347ba2991d

--- toolkit/content/widgets/moz-button/moz-button.figma.ts
+++ toolkit/content/widgets/moz-button/moz-button.figma.ts
@@ -1,11 +1,13 @@
 import figma, { html } from "@figma/code-connect/html"
 
-
-
-
-
-
-
+const example = (props) => html`
+<moz-button
+  type=${props.type}
+  disabled=${props.disabled}
+  size=${props.size}
+  iconsrc=${props.iconSrc}
+>${props.label}</moz-button>
+`;
 
 
 figma.connect(
@@ -20,22 +22,65 @@ figma.connect(
       type: figma.enum("Type", {
         Primary: "primary",
         Destructive: "destructive",
+        Ghost: "ghost",
       }),
-      isDisabled: figma.enum("State", {
+      disabled: figma.enum("State", {
         Disabled: true,
       }),
       size: figma.enum("Size", {
         Small: "small",
       }),
     },
-    example: (props) => html`\
-<moz-button
-      type=${props.type}
-      disabled=${props.isDisabled}
-      label=${props.label}
-      size=${props.size}
-      iconsrc=${props.iconSrc}
-></moz-button>
-`,
+    example,
+  },
+)
+
+
+figma.connect(
+  "https://www.figma.com/design/2ruSnPauajQGprFy6K333u/Desktop-Components?node-id=800-5099&m=dev",
+  {
+    props: {
+      type: figma.enum("Type", {
+        Primary: "primary",
+        Destructive: "destructive",
+      }),
+      size: figma.enum("Size", {
+        Small: "small",
+      }),
+      disabled: figma.boolean("Disabled"),
+      label: "Label",
+      
+      iconSrc: figma.boolean("Disabled", {
+        true: undefined,
+        false: undefined,
+      }),
+    },
+    example,
+  },
+)
+
+
+figma.connect(
+  "https://www.figma.com/design/2ruSnPauajQGprFy6K333u/Desktop-Components?node-id=1103-14274&m=dev",
+  {
+    props: {
+      disabled: figma.enum("State", {
+        Disabled: true,
+      }),
+      size: figma.enum("Size", {
+        Small: "small",
+      }),
+      type: figma.boolean("Background", {
+        true: "icon",
+        false: "ghost icon",
+      }),
+      iconSrc: "chrome://example.svg",
+      
+      label: figma.boolean("Background", {
+        true: undefined,
+        false: undefined,
+      }),
+    },
+    example,
   },
 )
