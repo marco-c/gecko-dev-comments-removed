90e9f31cea620086d0fcaccb873e2a8dbd046403
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-05-01 03:10:31 +0000

    Bug 1961674: wasm: Add whole-cell store buffer support. r=rhunt
    Profiles indicate that using whole-cell buffer entries yields better
    overall performance than using single-edge entries, particularly for
    structs (where one tenured struct may have many edges into the nursery,
    and hence only needs one store buffer entry).
    
    Differential Revision: https://phabricator.services.mozilla.com/D246132
    
    UltraBlame original commit: a10d28921fd24256fe4a5fead935826d88358bca

--- js/src/wasm/WasmIonCompile.cpp
+++ js/src/wasm/WasmIonCompile.cpp
@@ -2252,11 +2252,9 @@ class FunctionCompiler {
   
   [[nodiscard]] bool postBarrierImmediate(uint32_t lineOrBytecode,
                                           MDefinition* object,
-                                          MDefinition* valueBase,
-                                          uint32_t valueOffset,
                                           MDefinition* newValue) {
     auto* barrier = MWasmPostWriteBarrierImmediate::New(
-        alloc(), instancePointer_, object, valueBase, valueOffset, newValue);
+        alloc(), instancePointer_, object, newValue);
     if (!barrier) {
       return false;
     }
@@ -4656,8 +4654,7 @@ class FunctionCompiler {
       curBlock_->add(store);
 
       
-      if (!postBarrierImmediate(bytecodeOffset, exception, data, offset,
-                                argValues[i])) {
+      if (!postBarrierImmediate(bytecodeOffset, exception, argValues[i])) {
         return false;
       }
     }
@@ -4857,7 +4854,7 @@ class FunctionCompiler {
     curBlock_->add(store);
 
     
-    return postBarrierImmediate(lineOrBytecode, keepAlive, base, offset, value);
+    return postBarrierImmediate(lineOrBytecode, keepAlive, value);
   }
 
   
