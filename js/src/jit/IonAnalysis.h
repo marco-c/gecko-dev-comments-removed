0c5b643ef219c39f704e1cf3c306a2b0a50ea5fa
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-05-01 02:05:34 +0000

    Bug 1950284 - part 1: improvments to MIR debug printing routines.  r=rhunt.
    This patch expands and regularises the DumpMIR* family of routines, so that:
    
    * There are now DumpMIRDefinition, DumpMIRBlock and DumpMIRGraph, with
      compatible signatures, and helper routines DumpMIRDefinitionID and
      DumpMIRBlockID.
    
    * DumpMIRBlock is extended to print the loop depth, block kind, and
      successor-with-phis fields.
    
    * Optionally, the routines can print hashed versions of block and definition
      pointers, in base 26.  This is to avoid ambiguity in cases where ID values
      are duplicate or not set.  This is controlled by a defaulted-false boolean
      argument to the functions.
    
    A block that was previously printed as
    
      Block3:
        24 = Int32.Phi 58 46
        25 = None.Goto Block4
    
    will now be shown as
    
      Block3 -- preds=[Block2, Block7] -- LD=1 -- K=LOOP_HEADER -- s-w-phis=(null)
        24 = Int32.Phi 58 46
        25 = None.Goto Block4
    
    or, if hashed-pointer printing is enabled, as
    
      Block.bvufv.3 -- preds=[Block.nbloq.2, Block.zrexd.7] -- LD=1 -- K=LOOP_HEADER -- s-w-phis=(null)
        rdzcr.24 = Int32.Phi bfyya.58 bfqhy.46
        tmiue.25 = None.Goto Block4
    
    Differential Revision: https://phabricator.services.mozilla.com/D242514
    
    UltraBlame original commit: 125f7322442417c9d3914cb73cc797db178afe14

--- js/src/jit/IonAnalysis.h
+++ js/src/jit/IonAnalysis.h
@@ -188,10 +188,37 @@ bool IsDiscardable(const MDefinition* def);
 bool IsDiscardableAllowEffectful(const MDefinition* def);
 
 class CompileInfo;
-void DumpMIRExpressions(GenericPrinter& out, MIRGraph& graph,
-                        const CompileInfo& info, const char* phase);
-void DumpMIRDefinition(GenericPrinter& out, const MDefinition* def);
 
+
+
+
+
+
+
+void DumpHashedPointer(GenericPrinter& out, const void* p);
+
+
+void DumpMIRDefinitionID(GenericPrinter& out, const MDefinition* def,
+                         bool showHashedPointers = false);
+
+void DumpMIRDefinition(GenericPrinter& out, const MDefinition* def,
+                       bool showHashedPointers = false);
+
+
+void DumpMIRBlockID(GenericPrinter& out, const MBasicBlock* block,
+                    bool showHashedPointers = false);
+
+void DumpMIRBlock(GenericPrinter& out, MBasicBlock* block,
+                  bool showHashedPointers = false);
+
+
+void DumpMIRGraph(GenericPrinter& out, MIRGraph& graph,
+                  bool showHashedPointers = false);
+
+
+void DumpMIRExpressions(GenericPrinter& out, MIRGraph& graph,
+                        const CompileInfo& info, const char* phase,
+                        bool showHashedPointers = false);
 }  
 }  
 
