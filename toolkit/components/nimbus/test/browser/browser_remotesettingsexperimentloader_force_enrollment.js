dcf6c82b00342d46962e2d9e19a34988f43ba2a6
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-05-01 01:22:02 +0000

    Bug 1854189 - Only check isEarlyStartup cache before Nimbus is ready r=nimbus-reviewers,chumphreys
    Additionally we only check this cache for features that are
    isEarlyStartup.
    
    Differential Revision: https://phabricator.services.mozilla.com/D245664
    
    UltraBlame original commit: 491296342c442434db80ef7a3d9924f7e8b405bd

--- toolkit/components/nimbus/test/browser/browser_remotesettingsexperimentloader_force_enrollment.js
+++ toolkit/components/nimbus/test/browser/browser_remotesettingsexperimentloader_force_enrollment.js
@@ -27,6 +27,13 @@ async function setup(recipes) {
 }
 
 add_setup(async function () {
+  const sandbox = sinon.createSandbox();
+
+  
+  
+  
+  sandbox.stub(RemoteSettingsExperimentLoader, "updateRecipes").resolves();
+
   await SpecialPowers.pushPrefEnv({
     set: [
       ["messaging-system.log", "all"],
@@ -37,7 +44,14 @@ add_setup(async function () {
   });
 
   registerCleanupFunction(async () => {
+    
+    
+    
+    
+    sandbox.stub(RemoteSettingsExperimentLoader, "onEnabledPrefChanged");
+    await RemoteSettingsExperimentLoader.disable();
     await SpecialPowers.popPrefEnv();
+    sandbox.restore();
   });
 });
 
