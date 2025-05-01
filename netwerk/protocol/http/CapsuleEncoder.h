7925e4ef65ab47f2dff8415bd78cadf0bd272773
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-05-01 02:59:18 +0000

    Bug 1874102 - Implement WT_RESET_STREAM capsule, r=necko-reviewers,valentin
    Differential Revision: https://phabricator.services.mozilla.com/D245206
    
    UltraBlame original commit: 4f31a14077ead5d193c46d758d7d7893de7b1393

--- netwerk/protocol/http/CapsuleEncoder.h
+++ netwerk/protocol/http/CapsuleEncoder.h
@@ -7,6 +7,7 @@
 #ifndef mozilla_net_capsule_encoder_h
 #define mozilla_net_capsule_encoder_h
 
+#include "mozilla/Maybe.h"
 #include "mozilla/net/NeqoHttp3Conn.h"
 #include "mozilla/RefPtr.h"
 #include "mozilla/Span.h"
@@ -14,6 +15,12 @@
 #include "nsTArray.h"
 
 namespace mozilla::net {
+
+struct StreamMetadata {
+  uint64_t mID{0};
+  size_t mDataSize{0};
+  uint64_t mStartOfData{0};
+};
 
 
 class CapsuleEncoder final {
@@ -27,6 +34,8 @@ class CapsuleEncoder final {
   
   
   mozilla::Span<const uint8_t> GetBuffer();
+
+  Maybe<StreamMetadata>& GetStreamMetadata() { return mStreamMetadata; }
 
  private:
   RefPtr<NeqoEncoder> mEncoder;
@@ -44,6 +53,10 @@ class CapsuleEncoder final {
   CapsuleEncoder& EncodeBufferWithVarintLen(nsTArray<uint8_t>& aData);
 
   static size_t VarintLength(uint64_t aValue);
+
+  
+  
+  Maybe<StreamMetadata> mStreamMetadata;
 };
 
 }  
