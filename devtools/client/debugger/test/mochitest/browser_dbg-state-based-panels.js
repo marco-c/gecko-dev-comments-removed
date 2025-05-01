c1e686727a2b2af8a640b3c82d9270eba49c2c85
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-05-01 02:12:13 +0000

    Bug 1959055 - [devtools] Fix tests overlapping in browser_dbg-state-based-panels.js r=devtools-reviewers,nchevobbe
    Test was not properly waiting for an additional breakpoint hit
    
    Differential Revision: https://phabricator.services.mozilla.com/D246469
    
    UltraBlame original commit: 2b86c3131ac330ae1ce28a0e8d16ec245d3b6aa9

--- devtools/client/debugger/test/mochitest/browser_dbg-state-based-panels.js
+++ devtools/client/debugger/test/mochitest/browser_dbg-state-based-panels.js
@@ -128,6 +128,10 @@ add_task(async function testBreakpointsPanePersistOnPauseToggle() {
 
   await resume(dbg);
 
+  
+  await waitForPaused(dbg, "simple3.js");
+  await resume(dbg);
+
   is(getPaneElements(dbg).length, 2, "Breakpoints pane is still open");
 });
 
