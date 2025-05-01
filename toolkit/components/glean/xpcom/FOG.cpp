89a831840f9715c22cd37c4b3455003fe8b471fa
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-05-01 01:13:57 +0000

    Bug 1955429 - Permit FOG singleton creation in tests where the idle service is unavailable r=TravisLong
    Differential Revision: https://phabricator.services.mozilla.com/D245791
    
    UltraBlame original commit: 3fd7b7cba0ffdc949ddcbe2cd0e1b5ed7a2ea0fa

--- toolkit/components/glean/xpcom/FOG.cpp
+++ toolkit/components/glean/xpcom/FOG.cpp
@@ -76,6 +76,11 @@ already_AddRefed<FOG> FOG::GetSingleton() {
     nsresult rv;
     nsCOMPtr<nsIUserIdleService> idleService =
         do_GetService("@mozilla.org/widget/useridleservice;1", &rv);
+    if (NS_FAILED(rv) && xpc::IsInAutomation()) {
+      
+      
+      return do_AddRef(gFOG);
+    }
     NS_ENSURE_SUCCESS(rv, nullptr);
     MOZ_ASSERT(idleService);
     if (NS_WARN_IF(NS_FAILED(idleService->AddIdleObserver(gFOG, kIdleSecs)))) {
