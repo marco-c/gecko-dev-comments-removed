8c7cc7ec1259258e0d7a3f07f833fbca1ac453cb
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-05-01 01:11:48 +0000

    Bug 1918978: Add test case. r=jandem
    Differential Revision: https://phabricator.services.mozilla.com/D222268
    
    UltraBlame original commit: 7f785d9ee965f1cd007e97e7bc850ee29502525e

---
+++ js/src/jit-test/tests/auto-regress/bug1918978.js
@@ -0,0 +1,14 @@
+
+
+const x = new ArrayBuffer(1, {
+  maxByteLength: 1,
+});
+
+oomTest(() => {
+  for (let i = 0; i < 5; ++i) {
+    new Int8Array(x);
+  }
+});
+
+fullcompartmentchecks(true);
+gc();
