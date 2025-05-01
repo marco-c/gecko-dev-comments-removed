8eec0d3e050f1577d82046ed9398b466fe42a5b1
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-05-01 03:09:59 +0000

    Backed out changeset c62c5b5629ff (bug 1959542) for causing bc failures  browser_browserGlue_showModal_trigger.js CLOSED TREE
    UltraBlame original commit: 8eae95ea1da13b2b9bced84f96fae2b3736eaf1e

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1789,6 +1789,11 @@ pref("browser.partnerlink.campaign.topsites", "amzn_2020_a1");
 pref("browser.newtab.preload", true);
 
 
+
+
+pref("browser.preonboarding.onTrainRolloutPopulation",  2500);
+
+
 pref("browser.newtabpage.activity-stream.mobileDownloadModal.enabled", false);
 pref("browser.newtabpage.activity-stream.mobileDownloadModal.variant-a", false);
 pref("browser.newtabpage.activity-stream.mobileDownloadModal.variant-b", false);
