ec5b7caf778b313deace0e4a933f9ebd49400455
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-05-14 13:12:11 +0000

    Bug 1963911 - Return Result from ToMediaRawData* to handle OOM errors safely r=media-playback-reviewers,padenot
    This patch updates `ToMediaRawData` and `ToMediaRawDataCommon` to return
    a `Result` instead of a `RefPtr`, enabling proper handling of
    out-of-memory (OOM) errors. Previously, `EncodeWithModernAPIs` silently
    accepts a `nullptr` to accommodate a valid `nullptr` return from
    `FFmpegAudioEncoder::ToMediaRawData`. This behavior risked masking OOM
    error and could lead to unexpected or incorrect behavior. By using
    `Result`, OOM conditions are now explicitly handled, improving the
    robustness and reliability of the encoding pipeline.
    
    Differential Revision: https://phabricator.services.mozilla.com/D247486
    
    UltraBlame original commit: 9b8f7a1b06321377a7eebc633fcfe1c435aa25f7

--- dom/media/platforms/ffmpeg/FFmpegDataEncoder.h
+++ dom/media/platforms/ffmpeg/FFmpegDataEncoder.h
@@ -80,8 +80,10 @@ class FFmpegDataEncoder<LIBAV_VER> : public MediaDataEncoder {
   
   
   
-  virtual RefPtr<MediaRawData> ToMediaRawData(AVPacket* aPacket) = 0;
-  RefPtr<MediaRawData> ToMediaRawDataCommon(AVPacket* aPacket);
+  virtual Result<RefPtr<MediaRawData>, MediaResult> ToMediaRawData(
+      AVPacket* aPacket) = 0;
+  Result<RefPtr<MediaRawData>, MediaResult> ToMediaRawDataCommon(
+      AVPacket* aPacket);
   virtual Result<already_AddRefed<MediaByteBuffer>, nsresult> GetExtraData(
       AVPacket* aPacket) = 0;
   void ForceEnablingFFmpegDebugLogs();
