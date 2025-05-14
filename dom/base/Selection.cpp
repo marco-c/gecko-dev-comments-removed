7399d36d689dd16caaddad9cb8ff33d2d44c03c1
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-05-14 12:55:53 +0000

    Revert "Bug 1932150 - Make selection code to correctly iterate flattened tree r=smaug"
    This reverts commit bce1570f96a2620d5d8678300b3765160aba9723.
    
    UltraBlame original commit: d47b133ab1373fa1b89bf3bf1d593eec001bb482

--- dom/base/Selection.cpp
+++ dom/base/Selection.cpp
@@ -26,7 +26,7 @@
 #include "mozilla/dom/SelectionBinding.h"
 #include "mozilla/dom/ShadowRoot.h"
 #include "mozilla/dom/StaticRange.h"
-#include "mozilla/dom/TreeIterator.h"
+#include "mozilla/dom/ShadowIncludingTreeIterator.h"
 #include "mozilla/ErrorResult.h"
 #include "mozilla/HTMLEditor.h"
 #include "mozilla/IntegerRange.h"
@@ -333,7 +333,7 @@ const nsTHashSet<const nsINode*>& SelectionNodeCache::MaybeCollect(
     for (size_t rangeIndex = 0; rangeIndex < sel->RangeCount(); ++rangeIndex) {
       AbstractRange* range = sel->GetAbstractRangeAt(rangeIndex);
       MOZ_ASSERT(range);
-      if (range->AreNormalRangeAndCrossShadowBoundaryRangeCollapsed()) {
+      if (range->Collapsed()) {
         continue;
       }
       if (range->IsStaticRange() && !range->AsStaticRange()->IsValid()) {
@@ -346,44 +346,19 @@ const nsTHashSet<const nsINode*>& SelectionNodeCache::MaybeCollect(
           startRef.IsStartOfContainer() ? nullptr : startRef.GetContainer();
       const nsINode* endContainer =
           endRef.IsEndOfContainer() ? nullptr : endRef.GetContainer();
-
-      auto AddNodeIfFullySelected = [&](const nsINode* aNode) {
-        if (!aNode) {
-          return;
-        }
-        
-        
-        if (aNode == startContainer || aNode == endContainer) {
-          return;
-        }
-        fullySelectedNodes.Insert(aNode);
-      };
-
-      if (!StaticPrefs::dom_shadowdom_selection_across_boundary_enabled()) {
-        UnsafePreContentIterator iter;
-        nsresult rv = iter.Init(range);
-        if (NS_FAILED(rv)) {
-          continue;
-        }
-        for (; !iter.IsDone(); iter.Next()) {
-          AddNodeIfFullySelected(iter.GetCurrentNode());
-        }
-      } else {
-        ContentSubtreeIterator subtreeIter;
-        nsresult rv = subtreeIter.InitWithAllowCrossShadowBoundary(range);
-        if (NS_FAILED(rv)) {
-          continue;
-        }
-
-        for (; !subtreeIter.IsDone(); subtreeIter.Next()) {
-          MOZ_DIAGNOSTIC_ASSERT(subtreeIter.GetCurrentNode());
-          if (subtreeIter.GetCurrentNode()->IsContent()) {
-            TreeIterator<FlattenedChildIterator> iter(
-                *(subtreeIter.GetCurrentNode()->AsContent()));
-            for (; iter.GetCurrent(); iter.GetNext()) {
-              AddNodeIfFullySelected(iter.GetCurrent());
-            }
+      UnsafePreContentIterator iter;
+      nsresult rv = iter.Init(range);
+      if (NS_FAILED(rv)) {
+        continue;
+      }
+      for (; !iter.IsDone(); iter.Next()) {
+        if (const nsINode* node = iter.GetCurrentNode()) {
+          
+          
+          if (node == startContainer || node == endContainer) {
+            continue;
           }
+          fullySelectedNodes.Insert(node);
         }
       }
     }
@@ -1881,6 +1856,16 @@ nsresult Selection::SelectFramesOfInclusiveDescendantsOfContent(
   return NS_OK;
 }
 
+void Selection::SelectFramesOfShadowIncludingDescendantsOfContent(
+    nsIContent* aContent, bool aSelected) const {
+  MOZ_ASSERT(aContent);
+  MOZ_ASSERT(StaticPrefs::dom_shadowdom_selection_across_boundary_enabled());
+  for (nsINode* node : ShadowIncludingTreeIterator(*aContent)) {
+    nsIContent* innercontent = node->IsContent() ? node->AsContent() : nullptr;
+    SelectFramesOf(innercontent, aSelected);
+  }
+}
+
 void Selection::SelectFramesInAllRanges(nsPresContext* aPresContext) {
   
   
@@ -2009,7 +1994,7 @@ nsresult Selection::SelectFrames(nsPresContext* aPresContext,
     if (nsIContent* const content =
             nsIContent::FromNodeOrNull(subtreeIter.GetCurrentNode())) {
       if (StaticPrefs::dom_shadowdom_selection_across_boundary_enabled()) {
-        SelectFramesOfFlattenedTreeOfContent(content, aSelect);
+        SelectFramesOfShadowIncludingDescendantsOfContent(content, aSelect);
       } else {
         SelectFramesOfInclusiveDescendantsOfContent(postOrderIter, content,
                                                     aSelect);
@@ -2030,16 +2015,6 @@ nsresult Selection::SelectFrames(nsPresContext* aPresContext,
     }
   }
   return NS_OK;
-}
-
-void Selection::SelectFramesOfFlattenedTreeOfContent(nsIContent* aContent,
-                                                     bool aSelected) const {
-  MOZ_ASSERT(aContent);
-  MOZ_ASSERT(StaticPrefs::dom_shadowdom_selection_across_boundary_enabled());
-  TreeIterator<FlattenedChildIterator> iter(*aContent);
-  for (; iter.GetCurrent(); iter.GetNext()) {
-    SelectFramesOf(iter.GetCurrent(), aSelected);
-  }
 }
 
 
