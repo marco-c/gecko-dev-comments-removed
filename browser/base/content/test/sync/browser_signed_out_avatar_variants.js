d81c8238cb69e021b06d5522d12ead0a846ef116
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-05-14 13:50:48 +0000

    Bug 1959483 - Port tests from ExperimentFakes and ExperimentTestUtils to NimbusTestUtils for browser/base/content r=mconley,nimbus-reviewers,beth
    Differential Revision: https://phabricator.services.mozilla.com/D247806
    
    UltraBlame original commit: 483554380695d41dc88cb26a82b3934c51caded9

--- browser/base/content/test/sync/browser_signed_out_avatar_variants.js
+++ browser/base/content/test/sync/browser_signed_out_avatar_variants.js
@@ -3,7 +3,7 @@
 
 "use strict";
 
-const { ExperimentFakes } = ChromeUtils.importESModule(
+const { NimbusTestUtils } = ChromeUtils.importESModule(
   "resource://testing-common/NimbusTestUtils.sys.mjs"
 );
 
@@ -185,7 +185,7 @@ add_task(async function test_default() {
 
 
 add_task(async function test_control() {
-  let doCleanup = await ExperimentFakes.enrollWithFeatureConfig(
+  let doCleanup = await NimbusTestUtils.enrollWithFeatureConfig(
     {
       featureId: NimbusFeatures.fxaButtonVisibility.featureId,
       value: {
@@ -206,7 +206,7 @@ add_task(async function test_control() {
 
 
 add_task(async function test_human_circle() {
-  let doCleanup = await ExperimentFakes.enrollWithFeatureConfig(
+  let doCleanup = await NimbusTestUtils.enrollWithFeatureConfig(
     {
       featureId: NimbusFeatures.fxaButtonVisibility.featureId,
       value: {
@@ -228,7 +228,7 @@ add_task(async function test_human_circle() {
 
 
 add_task(async function test_fox_circle() {
-  let doCleanup = await ExperimentFakes.enrollWithFeatureConfig(
+  let doCleanup = await NimbusTestUtils.enrollWithFeatureConfig(
     {
       featureId: NimbusFeatures.fxaButtonVisibility.featureId,
       value: {
