00e5bb423cc1b580a8a38d803d27fc0e10d28ca7
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-05-14 11:56:18 +0000

    Bug 1962845 - Don't dismiss text substitution panel on parent process content. r=masayuki
    When content is parent process, text change notification will received into
    `MaybeDispatchKeypressEvents` synchronously. So we should block to dismiss
    this panel at this situation.
    
    Differential Revision: https://phabricator.services.mozilla.com/D247036
    
    UltraBlame original commit: fea03333e04251e234073f8fddc19b887bdf039f

--- widget/cocoa/TextInputHandler.mm
+++ widget/cocoa/TextInputHandler.mm
@@ -2608,8 +2608,16 @@ void TextInputHandler::InsertText(NSString* aString,
   
   
   nsEventStatus status = nsEventStatus_eIgnore;
-  bool keyPressDispatched = mDispatcher->MaybeDispatchKeypressEvents(
-      keypressEvent, status, currentKeyEvent);
+  bool keyPressDispatched = [&]() {
+    
+    
+    
+    AutoRestore<bool> block(mBlockDismissTextSubstitutionPanel);
+    mBlockDismissTextSubstitutionPanel = true;
+
+    return mDispatcher->MaybeDispatchKeypressEvents(keypressEvent, status,
+                                                    currentKeyEvent);
+  }();
   bool keyPressHandled = (status == nsEventStatus_eConsumeNoDefault);
 
   
@@ -3112,9 +3120,17 @@ bool TextInputHandler::DoCommandBySelector(const char* aSelector) {
     currentKeyEvent->InitKeyEvent(this, keypressEvent, false);
 
     nsEventStatus status = nsEventStatus_eIgnore;
-    currentKeyEvent->mKeyPressDispatched =
-        mDispatcher->MaybeDispatchKeypressEvents(keypressEvent, status,
-                                                 currentKeyEvent);
+    
+    
+    
+    {
+      AutoRestore<bool> block(mBlockDismissTextSubstitutionPanel);
+      mBlockDismissTextSubstitutionPanel = true;
+
+      currentKeyEvent->mKeyPressDispatched =
+          mDispatcher->MaybeDispatchKeypressEvents(keypressEvent, status,
+                                                   currentKeyEvent);
+    }
     currentKeyEvent->mKeyPressHandled =
         (status == nsEventStatus_eConsumeNoDefault);
     MOZ_LOG_KEY_OR_IME(
@@ -5022,8 +5038,10 @@ void IMEInputHandler::HandleTextSubstitution(
   }
 
   
-  mProcessTextSubstitution = false;
-  DismissTextSubstitutionPanel();
+  if (!mBlockDismissTextSubstitutionPanel) {
+    mProcessTextSubstitution = false;
+    DismissTextSubstitutionPanel();
+  }
 
   
   
@@ -5254,6 +5272,11 @@ void IMEInputHandler::ShowTextSubstitutionPanel() {
 
 void IMEInputHandler::DismissTextSubstitutionPanel() {
   NS_OBJC_BEGIN_TRY_IGNORE_BLOCK;
+
+  MOZ_LOG(gIMELog, LogLevel::Info,
+          ("%p IMEInputHandler::DismissTextSubstitutionPanel, "
+           "mProcessTextSubstitution=%s",
+           this, mProcessTextSubstitution ? "true" : "false"));
 
   NSSpellChecker* spellchecker = [NSSpellChecker sharedSpellChecker];
   if (!spellchecker) {
