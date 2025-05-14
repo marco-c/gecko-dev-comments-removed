324aecc601e4901cf43fbe5f755804ffc46b30a4
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-05-14 12:40:55 +0000

    Bug 1932150 - Update ranges to correctly set and update nodes for flattened selection r=jjaschke,smaug,dom-core
    Differential Revision: https://phabricator.services.mozilla.com/D231589
    
    UltraBlame original commit: 8a42114140af9d0a310c50d8c0fcc0a3e3740352

--- dom/base/AbstractRange.h
+++ dom/base/AbstractRange.h
@@ -1,8 +1,8 @@
-/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
-/* vim: set ts=8 sts=2 et sw=2 tw=80: */
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+
+
+
+
 
 #ifndef mozilla_dom_AbstractRange_h
 #define mozilla_dom_AbstractRange_h
@@ -30,12 +30,13 @@ namespace mozilla::dom {
 class Document;
 class Selection;
 class StaticRange;
+class HTMLSlotElement;
 
 enum class AllowRangeCrossShadowBoundary : bool { No, Yes };
 
 class AbstractRange : public nsISupports,
                       public nsWrapperCache,
-                      // For linking together selection-associated ranges.
+                      
                       public mozilla::LinkedListElement<AbstractRange> {
   using AllowRangeCrossShadowBoundary =
       mozilla::dom::AllowRangeCrossShadowBoundary;
@@ -50,20 +51,20 @@ class AbstractRange : public nsISupports,
   AbstractRange() = delete;
   explicit AbstractRange(const AbstractRange& aOther) = delete;
 
-  /**
-   * Called when the process is shutting down.
-   */
+  
+
+
   static void Shutdown();
 
   NS_DECL_CYCLE_COLLECTING_ISUPPORTS
   NS_DECL_CYCLE_COLLECTION_WRAPPERCACHE_CLASS(AbstractRange)
 
-  /**
-   * All of the MayCrossShadowBoundary* methods are used to get the boundary
-   * endpoints that cross shadow boundaries. They would return
-   * the same value as the non-MayCrossShadowBoundary* methods if the range
-   * boundaries don't cross shadow boundaries.
-   */
+  
+
+
+
+
+
   const RangeBoundary& StartRef() const { return mStart; }
   const RangeBoundary& MayCrossShadowBoundaryStartRef() const;
 
@@ -79,22 +80,22 @@ class AbstractRange : public nsISupports,
   nsIContent* GetMayCrossShadowBoundaryChildAtEndOffset() const;
 
   bool IsPositioned() const { return mIsPositioned; }
-  /**
-   * https://dom.spec.whatwg.org/#concept-tree-inclusive-ancestor
-   */
+  
+
+
   nsINode* GetClosestCommonInclusiveAncestor(
       AllowRangeCrossShadowBoundary aAllowCrossShadowBoundary =
           AllowRangeCrossShadowBoundary::No) const;
 
-  // WebIDL
+  
 
-  // If Range is created from JS, it's initialized with Document.createRange()
-  // and it collaps the range to start of the Document.  Therefore, the
-  // following WebIDL methods are called only when `mIsPositioned` is true.
-  // So, it does not make sense to take `ErrorResult` as their parameter
-  // since its destruction cost may appear in profile.  If you create range
-  // object from C++ and needs to check whether it's positioned, should call
-  // `IsPositioned()` directly.
+  
+  
+  
+  
+  
+  
+  
 
   nsINode* GetStartContainer() const { return mStart.GetContainer(); }
   nsINode* GetMayCrossShadowBoundaryStartContainer() const;
@@ -102,17 +103,17 @@ class AbstractRange : public nsISupports,
   nsINode* GetEndContainer() const { return mEnd.GetContainer(); }
   nsINode* GetMayCrossShadowBoundaryEndContainer() const;
 
-  /**
-   * Return GetStartContainer() and GetEndContainer() if this is positioned.
-   */
+  
+
+
   [[nodiscard]] bool IsPositionedAndSameContainer() const {
     return MOZ_LIKELY(mIsPositioned) &&
            mStart.GetContainer() == mEnd.GetContainer();
   }
-  /**
-   * Return GetMayCrossShadowBoundaryStartContainer() and
-   * GetMayCrossShadowBoundaryEndContainer() if this is positioned.
-   */
+  
+
+
+
   [[nodiscard]] bool IsPositionedAndSameContainerMayCrossShadowBoundary()
       const {
     return MOZ_LIKELY(mIsPositioned) &&
@@ -126,14 +127,14 @@ class AbstractRange : public nsISupports,
     return mStart.GetComposedDoc();
   }
 
-  // FYI: Returns 0 if it's not positioned.
+  
   uint32_t StartOffset() const {
     return static_cast<uint32_t>(
         *mStart.Offset(RangeBoundary::OffsetFilter::kValidOrInvalidOffsets));
   }
   uint32_t MayCrossShadowBoundaryStartOffset() const;
 
-  // FYI: Returns 0 if it's not positioned.
+  
   uint32_t EndOffset() const {
     return static_cast<uint32_t>(
         *mEnd.Offset(RangeBoundary::OffsetFilter::kValidOrInvalidOffsets));
@@ -161,10 +162,10 @@ class AbstractRange : public nsISupports,
   inline StaticRange* AsStaticRange();
   inline const StaticRange* AsStaticRange() const;
 
-  /**
-   * Return true if this range is part of a Selection object
-   * and isn't detached.
-   */
+  
+
+
+
   bool IsInAnySelection() const { return !mSelections.IsEmpty(); }
 
   MOZ_CAN_RUN_SCRIPT void RegisterSelection(
@@ -173,19 +174,19 @@ class AbstractRange : public nsISupports,
   void UnregisterSelection(const mozilla::dom::Selection& aSelection,
                            IsUnlinking aIsUnlinking = IsUnlinking::No);
 
-  /**
-   * Returns a list of all Selections the range is associated with.
-   */
+  
+
+
   const nsTArray<WeakPtr<Selection>>& GetSelections() const;
 
-  /**
-   * Return true if this range is in |aSelection|.
-   */
+  
+
+
   bool IsInSelection(const mozilla::dom::Selection& aSelection) const;
 
-  /**
-   * Return true if aRoot is a UA shadow root.
-   */
+  
+
+
   static bool IsRootUAWidget(const nsINode* aRoot);
 
  protected:
@@ -193,7 +194,9 @@ class AbstractRange : public nsISupports,
             typename RangeType>
   static nsresult SetStartAndEndInternal(
       const RangeBoundaryBase<SPT, SRT>& aStartBoundary,
-      const RangeBoundaryBase<EPT, ERT>& aEndBoundary, RangeType* aRange);
+      const RangeBoundaryBase<EPT, ERT>& aEndBoundary, RangeType* aRange,
+      AllowRangeCrossShadowBoundary aAllowCrossShadowBoundary =
+          AllowRangeCrossShadowBoundary::No);
 
   template <class RangeType>
   static bool MaybeCacheToReuse(RangeType& aInstance);
@@ -215,20 +218,27 @@ class AbstractRange : public nsISupports,
                    << (aRange.mIsDynamicRange ? "true" : "false") << " }";
   }
 
-  /**
-   * https://dom.spec.whatwg.org/#concept-tree-inclusive-ancestor
-   */
+  
+
+
   void RegisterClosestCommonInclusiveAncestor(nsINode* aNode);
-  /**
-   * https://dom.spec.whatwg.org/#concept-tree-inclusive-ancestor
-   */
+  
+
+
   void UnregisterClosestCommonInclusiveAncestor(
       IsUnlinking aIsUnlinking = IsUnlinking::No);
 
   void UpdateCommonAncestorIfNecessary();
 
-  static void MarkDescendants(const nsINode& aNode);
-  static void UnmarkDescendants(const nsINode& aNode);
+  static void MarkDescendants(nsINode& aNode);
+  static void UnmarkDescendants(nsINode& aNode);
+
+  static void UpdateDescendantsInFlattenedTree(nsINode& aNode,
+                                               bool aMarkDescendants);
+  friend void mozilla::SlotAssignedNodeAdded(dom::HTMLSlotElement* aSlot,
+                                             nsIContent& aAssignedNode);
+  friend void mozilla::SlotAssignedNodeRemoved(HTMLSlotElement* aSlot,
+                                               nsIContent& aUnassignedNode);
 
  private:
   void ClearForReuse();
@@ -238,27 +248,27 @@ class AbstractRange : public nsISupports,
   RangeBoundary mStart;
   RangeBoundary mEnd;
 
-  // A Range can be part of multiple |Selection|s. This is a very rare use case.
+  
   AutoTArray<WeakPtr<Selection>, 1> mSelections;
-  // mRegisteredClosestCommonInclusiveAncestor is only non-null when the range
-  // IsInAnySelection().
+  
+  
   nsCOMPtr<nsINode> mRegisteredClosestCommonInclusiveAncestor;
 
-  // `true` if `mStart` and `mEnd` are set for StaticRange or set and valid
-  // for nsRange.
+  
+  
   bool mIsPositioned;
 
-  // Used by nsRange, but this should have this for minimizing the size.
+  
   bool mIsGenerated;
-  // Used by nsRange, but this should have this for minimizing the size.
+  
   bool mCalledByJS;
 
-  // true if this is an `nsRange` object.
+  
   const bool mIsDynamicRange;
 
   static bool sHasShutDown;
 };
 
-}  // namespace mozilla::dom
+}  
 
-#endif  // #ifndef mozilla_dom_AbstractRange_h
+#endif  
