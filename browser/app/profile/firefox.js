b38e86adbc49f65b44c3dea1bf60409abe19b476
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-05-14 13:07:12 +0000

    Bug 1964091 - Clean up RC migration prefs - r=shopping-reviewers,kpatenio
    Differential Revision: https://phabricator.services.mozilla.com/D247651
    
    UltraBlame original commit: 8d0ad86296465783051de7bf4f1a683d40db0790

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -883,17 +883,6 @@ pref("browser.shopping.experience2023.sidebarClosedCount", 0);
 pref("browser.shopping.experience2023.showKeepSidebarClosedMessage", true);
 
 
-
-
-pref("browser.shopping.experience2023.integratedSidebar", false);
-
-
-
-
-
-pref("browser.shopping.experience2023.newPositionCard.hasSeen", false);
-
-
 pref("browser.spin_cursor_while_busy", false);
 
 
