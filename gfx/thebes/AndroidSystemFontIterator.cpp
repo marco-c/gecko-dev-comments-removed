2b98b65d4493e6f6addf5e9a58349f83861d73ab
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-05-16 01:21:06 +0000

    Bug 1966309 - Use weak android symbol instead of dlopen/dlsym for AndroidSystemFontIterator.cpp r=jnicol
    Differential Revision: https://phabricator.services.mozilla.com/D249224
    
    UltraBlame original commit: 192739fba23c9af974c32f0418f7978c9bcca99a

---
+++ gfx/thebes/AndroidSystemFontIterator.cpp
@@ -0,0 +1,95 @@
+
+
+
+
+
+#include "AndroidSystemFontIterator.h"
+
+#include "mozilla/Assertions.h"
+#include "nsDebug.h"
+
+#include <android/system_fonts.h>
+
+namespace mozilla {
+
+_ASystemFontIterator_open AndroidSystemFontIterator::sSystemFontIterator_open =
+    nullptr;
+_ASystemFontIterator_next AndroidSystemFontIterator::sSystemFontIterator_next =
+    nullptr;
+_ASystemFontIterator_close
+    AndroidSystemFontIterator::sSystemFontIterator_close = nullptr;
+
+_AFont_getFontFilePath AndroidFont::sFont_getFontFilePath = nullptr;
+_AFont_close AndroidFont::sFont_close = nullptr;
+
+AndroidSystemFontIterator::~AndroidSystemFontIterator() {
+  if (!sSystemFontIterator_open) {
+    return;
+  }
+
+  if (!mIterator) {
+    return;
+  }
+
+  sSystemFontIterator_close(mIterator);
+}
+
+bool AndroidSystemFontIterator::Init() {
+  if (!sSystemFontIterator_open) {
+    if (__builtin_available(android 29, *)) {
+      sSystemFontIterator_open = ASystemFontIterator_open;
+      sSystemFontIterator_next = ASystemFontIterator_next;
+      sSystemFontIterator_close = ASystemFontIterator_close;
+      AndroidFont::sFont_getFontFilePath = AFont_getFontFilePath;
+      AndroidFont::sFont_close = AFont_close;
+    } else {
+      NS_WARN_IF(false);
+      return false;
+    }
+  }
+
+  mIterator = sSystemFontIterator_open();
+
+  return true;
+}
+
+Maybe<AndroidFont> AndroidSystemFontIterator::Next() {
+  if (NS_WARN_IF(!sSystemFontIterator_open)) {
+    return Nothing();
+  }
+
+  if (!mIterator) {
+    return Nothing();
+  }
+
+  AFont* font = sSystemFontIterator_next(mIterator);
+  if (!font) {
+    sSystemFontIterator_close(mIterator);
+    mIterator = nullptr;
+    return Nothing();
+  }
+
+  return Some(AndroidFont(font));
+}
+
+AndroidFont::~AndroidFont() {
+  if (NS_WARN_IF(!sFont_close)) {
+    return;
+  }
+
+  if (!mFont) {
+    return;
+  }
+
+  sFont_close(mFont);
+}
+
+const char* AndroidFont::GetFontFilePath() {
+  if (NS_WARN_IF(!sFont_getFontFilePath) || NS_WARN_IF(!mFont)) {
+    return nullptr;
+  }
+
+  return sFont_getFontFilePath(mFont);
+}
+
+}  
