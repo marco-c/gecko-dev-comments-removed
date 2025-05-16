00ef6c134d7a3804ce03b1289e1cf0d8c0b09f83
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-05-16 01:15:44 +0000

    Revert "Bug 1965313 - Reimplement enum discriminents, r=bgruber"
    This reverts commit 02d50105b920ac855bf5a456c4525e0cf9499da6.
    
    Revert "Bug 1955298 - uniffi-bindgen-gecko-js: start using the IR pipeline, r=markh,frontend-codestyle-reviewers"
    
    This reverts commit 90368cc237a67d59241addde8f207d73b9b96286.
    
    UltraBlame original commit: d3ddc2e298d71defb84e93c2e68f9c48bce93441

--- toolkit/components/uniffi-js/Callbacks.h
+++ toolkit/components/uniffi-js/Callbacks.h
@@ -49,8 +49,8 @@ void DeregisterCallbackHandler(uint64_t aInterfaceId, ErrorResult& aError);
 class UniffiCallbackMethodHandlerBase {
  protected:
   
-  const char* mUniffiInterfaceName;
-  FfiValueInt<uint64_t> mUniffiHandle;
+  const char* mInterfaceName;
+  uint64_t mObjectHandle;
 
   
   MOZ_CAN_RUN_SCRIPT
@@ -59,9 +59,8 @@ class UniffiCallbackMethodHandlerBase {
 
  public:
   UniffiCallbackMethodHandlerBase(const char* aInterfaceName,
-                                  uint64_t aUniffiHandle)
-      : mUniffiInterfaceName(aInterfaceName),
-        mUniffiHandle(FfiValueInt<uint64_t>::FromRust(aUniffiHandle)) {}
+                                  uint64_t aObjectHandle)
+      : mInterfaceName(aInterfaceName), mObjectHandle(aObjectHandle) {}
 
   virtual ~UniffiCallbackMethodHandlerBase() = default;
 
