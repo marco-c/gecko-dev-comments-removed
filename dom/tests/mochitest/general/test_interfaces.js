4dd934c62253cd7eaa892e1a0c45bb52d743c02d
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-05-16 01:22:06 +0000

    Revert "Bug 1966459 - Enable close watcher in desktop nightly r=smaug" for causing failures at dialog-requestclose-3-crash.html.
    This reverts commit 5d2d8bf73c5a423029608761f93286e902f9d134.
    
    UltraBlame original commit: 0fd60cc1e407acc53e4893171b45f154f302a6c2

--- dom/tests/mochitest/general/test_interfaces.js
+++ dom/tests/mochitest/general/test_interfaces.js
@@ -264,13 +264,6 @@ let interfaceNamesInGlobalScope = [
   
   { name: "CloseEvent", insecureContext: true },
   
-  {
-    name: "CloseWatcher",
-    insecureContext: true,
-    nightly: true,
-    android: false,
-  },
-  
   { name: "Comment", insecureContext: true },
   
   { name: "CompositionEvent", insecureContext: true },
