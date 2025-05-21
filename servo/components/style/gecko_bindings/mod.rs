eafa3f6967c3ddaed805fc2b9a89fd26adf34238
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-05-21 13:55:23 +0000

    Bug 1967103 - Allow unnecessary transmutes in rust bindings. r=dshin
    Rust nightly warns for this and is very noisy, and somewhat useless for
    this autogenerated code.
    
    Differential Revision: https://phabricator.services.mozilla.com/D249786
    
    UltraBlame original commit: 550af95d570a100a099b923746dce3bfed5cb5d7

--- servo/components/style/gecko_bindings/mod.rs
+++ servo/components/style/gecko_bindings/mod.rs
@@ -8,13 +8,15 @@
 
 
 
+#[allow(unknown_lints)]
 #[allow(
     dead_code,
     improper_ctypes,
     non_camel_case_types,
     non_snake_case,
     non_upper_case_globals,
-    missing_docs
+    missing_docs,
+    unnecessary_transmutes,
 )]
 
 
