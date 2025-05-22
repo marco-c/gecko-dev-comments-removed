11f91a8cd7476938477d7234cbed3534dbcccbe0
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-05-22 10:27:14 +0000

    Bug 1820252 - Rework assertPageIsTranslated r=translations-reviewers,gregtatum
    This patch reworks the way that we assert that the page
    is translated in Full-Page Translations tests. In addition
    to ensuring that the translated content matches the expectations,
    each test now ensures that the TranslationsDocument has fulfilled
    every translation request that we expect it to, as well as ensures
    that the intersection-observed nodes within the TranslationsDocument
    match what we expect, since nodes are no longer observed once they
    are fully translated (until they are mutated).
    
    These changes give us extremely strong correctness guarantees in
    terms of assurance that the TranslationsDocument algorithms are
    working as intended, and maintaining their invariants in all cases.
    
    Differential Revision: https://phabricator.services.mozilla.com/D249987
    
    UltraBlame original commit: 449ed1c52d8cc0c6165cb7e253bd4a3abccfb138

--- browser/components/translations/tests/browser/browser_translations_select_telemetry_translation_failure_with_full_page_translations_active.js
+++ browser/components/translations/tests/browser/browser_translations_select_telemetry_translation_failure_with_full_page_translations_active.js
@@ -122,11 +122,13 @@ add_task(
       }
     );
 
-    await FullPageTranslationsTestUtils.assertPageIsTranslated({
-      fromLanguage: "es",
-      toLanguage: "fr",
-      runInPage,
-    });
+    await FullPageTranslationsTestUtils.assertOnlyIntersectingContentIsTranslated(
+      {
+        fromLanguage: "es",
+        toLanguage: "fr",
+        runInPage,
+      }
+    );
 
     await SelectTranslationsTestUtils.openPanel(runInPage, {
       selectFrenchSection: true,
@@ -171,9 +173,9 @@ add_task(
           auto_translate: false,
           source_text_code_units:
             AppConstants.platform === "win"
-              ? 1718 
-              : 1709, 
-          source_text_word_count: 281,
+              ? 1616 
+              : 1607, 
+          source_text_word_count: 257,
         },
       }
     );
