5d5e8b3b716575c6f22838e39b9342638204db42
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-05-28 15:44:21 +0000

    Bug 1964126 - [devtools] Don't restrict width of DPR button in RDM. r=devtools-reviewers,nchevobbe
    Differential Revision: https://phabricator.services.mozilla.com/D251158
    
    UltraBlame original commit: 0fad18a4294267494d456a1c0076d7fc3a4aca05

--- devtools/client/responsive/ui.js
+++ devtools/client/responsive/ui.js
@@ -241,7 +241,7 @@ class ResponsiveUI {
           
           this.rdmFrame.classList.toggle(
             "accomodate-ua",
-            entry.contentBoxSize[0].inlineSize < 520
+            entry.contentBoxSize[0].inlineSize <= 800
           );
         }
       }
