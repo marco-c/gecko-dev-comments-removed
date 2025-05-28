c4ae0b0365fb2504102f84cebaba7ada8e5b3dde
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-05-28 15:45:49 +0000

    Bug 1968400 - Add PointerEvent.persistentDeviceId,
    (Lando is broken, creating a new patch)
    
    Differential Revision: https://phabricator.services.mozilla.com/D251256
    
    UltraBlame original commit: 1fc349568fcf087a54b84e9e32dcfff97fbfdd2b

--- dom/events/PointerEvent.cpp
+++ dom/events/PointerEvent.cpp
@@ -149,6 +149,9 @@ already_AddRefed<PointerEvent> PointerEvent::Constructor(
   if (aParam.mAzimuthAngle.WasPassed()) {
     e->mAzimuthAngle.emplace(aParam.mAzimuthAngle.Value());
   }
+
+  e->mPersistentDeviceId.emplace(aParam.mPersistentDeviceId);
+
   if (!aParam.mCoalescedEvents.IsEmpty()) {
     e->mCoalescedEvents.AppendElements(aParam.mCoalescedEvents);
   }
@@ -332,6 +335,24 @@ double PointerEvent::AzimuthAngle() {
 }
 
 bool PointerEvent::IsPrimary() { return mEvent->AsPointerEvent()->mIsPrimary; }
+
+int32_t PointerEvent::PersistentDeviceId() {
+  if (mPersistentDeviceId.isNothing()) {
+    if (mEvent->IsTrusted() &&
+        mEvent->AsPointerEvent()->mInputSource ==
+            MouseEvent_Binding::MOZ_SOURCE_MOUSE &&
+        IsPointerEventMessage(mEvent->mMessage) &&
+        !IsPointerEventMessageOriginallyMouseEventMessage(mEvent->mMessage)) {
+      
+      mPersistentDeviceId.emplace(1);
+    } else {
+      
+      mPersistentDeviceId.emplace(0);
+    }
+  }
+
+  return mPersistentDeviceId.value();
+}
 
 bool PointerEvent::EnableGetCoalescedEvents(JSContext* aCx, JSObject* aGlobal) {
   return !StaticPrefs::
