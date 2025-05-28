997173e53b1616993c13f7d2d52c8b221eb2718b
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-05-28 15:06:10 +0000

    Bug 1967661: Fix type of catch_ref and catch_all_ref. r=rhunt
    Differential Revision: https://phabricator.services.mozilla.com/D250468
    
    UltraBlame original commit: 31337431a21223a6404f726891fe9faedf3fc33c

--- js/src/wasm/WasmOpIter.h
+++ js/src/wasm/WasmOpIter.h
@@ -1696,7 +1696,7 @@ inline bool OpIter<Policy>::readTryTable(BlockType* type,
 
     
     if (tryTableCatch.captureExnRef &&
-        !tryTableCatch.labelType.append(ValType(RefType::exn()))) {
+        !tryTableCatch.labelType.append(ValType(RefType::exn().asNonNullable()))) {
       return false;
     }
 
