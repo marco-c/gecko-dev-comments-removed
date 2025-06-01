062ef374c59f55c7c4c41db8224b7b0f7ba62d16
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-06-01 01:14:43 +0000

    Bug 1465105 - Remove non-standard isDOMClass check from Object.prototype.toString. r=mgaudet
    This was used to treat callable DOM objects such as `HTMLObjectElement` as non-callable
    for `toString`, but in bug 909656 we changed `HTMLObjectElement` and `HTMLEmbedElement`
    to not be callable.
    
    Furthermore, DOM objects now have `Symbol.toStringTag` properties and these values are
    used instead of the builtin string tag.
    
    Differential Revision: https://phabricator.services.mozilla.com/D251385
    
    UltraBlame original commit: 339069be7791edb153857d3e8c9dcb988ecde5d1

--- js/src/builtin/Object.cpp
+++ js/src/builtin/Object.cpp
@@ -554,11 +554,7 @@ static JSString* GetBuiltinTagSlow(JSContext* cx, HandleObject obj) {
       return cx->names().object_RegExp_;
     default:
       if (obj->isCallable()) {
-        
-        JSObject* unwrapped = CheckedUnwrapDynamic(obj, cx);
-        if (!unwrapped || !unwrapped->getClass()->isDOMClass()) {
-          return cx->names().object_Function_;
-        }
+        return cx->names().object_Function_;
       }
       return cx->names().object_Object_;
   }
@@ -611,8 +607,7 @@ static MOZ_ALWAYS_INLINE JSString* GetBuiltinTagFast(JSObject* obj,
     return cx->names().object_Error_;
   }
 
-  if (obj->isCallable() && !obj->getClass()->isDOMClass()) {
-    
+  if (obj->isCallable()) {
     return cx->names().object_Function_;
   }
 
