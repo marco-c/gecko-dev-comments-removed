3a3aefe277781136ae0b2ad5e111c2aa5cee1b1e
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-06-01 01:48:18 +0000

    Bug 1962485 - Fix `jj` tests in `mozversioncontrol` r=sfink,ahal
    - Inject `jj` into `PATH` in CI.
    - Windows tempdir from pytest was too long for some `jj` files created on init.
    - Comments in `steps` were being interpreted as commands.
    - The `diff` from `jj` is different from `hg`/`git`, so check for both.
    - MacOS workers don't have a `jj` compatible version of `git`, so we skip `jj` tests on MacOS in CI for now. `Error: Git does not recognize required option: no-write-fetch-head (note: supported version is 2.40.4)`
    
    Differential Revision: https://phabricator.services.mozilla.com/D251119
    
    UltraBlame original commit: 967c336f6ed9704fc15602f50e32dd5d506fcca1

--- python/mozversioncontrol/test/test_get_commits.py
+++ python/mozversioncontrol/test/test_get_commits.py
@@ -27,12 +27,13 @@ STEPS = {
         """
     ],
     "jj": [
+        
         """
         jj new -m "commit 1"
         echo bar >> bar
         jj commit -m "commit 2"
         echo baz > baz
-        jj log -n0 # snapshot, since mach's jj integration avoids doing this automatically
+        jj log -n0
         """
     ],
 }
