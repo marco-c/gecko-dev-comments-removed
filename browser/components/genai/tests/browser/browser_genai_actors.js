e6c891e4238b0f67d124f1da89749bfcba17c250
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-06-11 02:24:32 +0000

    Bug 1970229 - add summrize page logics and dismissal new badge for the feature after first click r=Mardak,firefox-ai-ml-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D252707
    
    UltraBlame original commit: 2adfb372b8a57f2d6980825aba759ffd3283a286

--- browser/components/genai/tests/browser/browser_genai_actors.js
+++ browser/components/genai/tests/browser/browser_genai_actors.js
@@ -19,6 +19,26 @@ add_task(async function test_got_actor() {
 
 
 
+add_task(async function test_actor_extract_text() {
+  await BrowserTestUtils.withNewTab(
+    {
+      gBrowser,
+      url: "data:text/html, <body>Hello world!</body>",
+    },
+    async browser => {
+      const actor =
+        browser.browsingContext.currentWindowContext.getActor("GenAI");
+      Assert.ok(actor, "GenAI should be attached to this tab");
+
+      const innerText = await actor.sendQuery("GetReadableText");
+      Assert.ok(innerText.includes("Hello world!"), "Page text was extracted");
+    }
+  );
+});
+
+
+
+
 add_task(async function test_actor_disabled() {
   await SpecialPowers.pushPrefEnv({
     set: [["browser.ml.chat.provider", ""]],
