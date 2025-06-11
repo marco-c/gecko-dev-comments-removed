fb4baa29e1b9e49d7dda66703d90d433cf8e8a77
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-06-11 01:14:50 +0000

    Bug 1961981 - Move AV1 ordering preference to DefaultCodecPreferences. r=ng
    Differential Revision: https://phabricator.services.mozilla.com/D251491
    
    UltraBlame original commit: aca0c07ce32975cba57b6a5d4f97cbff61683c13

--- dom/media/webrtc/jsapi/PeerConnectionImpl.cpp
+++ dom/media/webrtc/jsapi/PeerConnectionImpl.cpp
@@ -605,32 +605,12 @@ RefPtr<DtlsIdentity> PeerConnectionImpl::Identity() const {
   return mCertificate->CreateDtlsIdentity();
 }
 
-class CompareCodecPriority {
- public:
-  void SetPreferredCodec(const nsCString& preferredCodec) {
-    mPreferredCodec = preferredCodec;
-  }
-
+struct CompareCodecPriority {
   bool operator()(const UniquePtr<JsepCodecDescription>& lhs,
                   const UniquePtr<JsepCodecDescription>& rhs) const {
     
-    if (!mPreferredCodec.IsEmpty()) {
-      const bool lhsMatches = mPreferredCodec.EqualsIgnoreCase(lhs->mName) ||
-                              mPreferredCodec.EqualsIgnoreCase(lhs->mDefaultPt);
-      const bool rhsMatches = mPreferredCodec.EqualsIgnoreCase(rhs->mName) ||
-                              mPreferredCodec.EqualsIgnoreCase(rhs->mDefaultPt);
-      
-      if (lhsMatches && !rhsMatches) {
-        return true;
-      }
-    }
-    
-    return (lhs->mStronglyPreferred && !rhs->mStronglyPreferred);
+    return lhs->mStronglyPreferred && !rhs->mStronglyPreferred;
   }
-
- private:
-  
-  nsCString mPreferredCodec;
 };
 
 void RecordCodecTelemetry() {
@@ -660,9 +640,6 @@ nsresult PeerConnectionImpl::SortJsepSessionCodecs() {
 
   
   CompareCodecPriority comparator;
-  if (StaticPrefs::media_webrtc_codec_video_av1_experimental_preferred()) {
-    comparator.SetPreferredCodec(nsCString("av1"));
-  }
   
   mJsepSession->SortCodecs(comparator);
   return NS_OK;
@@ -2065,9 +2042,6 @@ void PeerConnectionImpl::GetDefaultVideoCodecs(
       JsepVideoCodecDescription::CreateDefaultRed(prefs));
 
   CompareCodecPriority comparator;
-  if (StaticPrefs::media_webrtc_codec_video_av1_experimental_preferred()) {
-    comparator.SetPreferredCodec(nsCString("av1"));
-  }
   std::stable_sort(aSupportedCodecs.begin(), aSupportedCodecs.end(),
                    comparator);
 }
