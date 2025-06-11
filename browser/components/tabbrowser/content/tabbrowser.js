81b1b5bb969a8119c4ccc670413929ec6f11f6ac
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-06-11 02:22:30 +0000

    Revert "Bug 1969164 - close window when dragging tab group out of window r=jswinarton,tabbrowser-reviewers,dao" for causing failures complaining about tabs.
    This reverts commit bec9682417873c356a7f72ae7d6176ea22785756.
    
    UltraBlame original commit: 740dab811f0b8563cbb9a5551dafca20b4cf9531

--- browser/components/tabbrowser/content/tabbrowser.js
+++ browser/components/tabbrowser/content/tabbrowser.js
@@ -4678,6 +4678,7 @@
       }
 
       let isVisibleTab = aTab.visible;
+      let isLastTab = isVisibleTab && this.visibleTabs.length == 1;
       
       
       
@@ -4718,7 +4719,7 @@
       if (
         !animate  ||
         gReduceMotion ||
-        this.#isLastTabInWindow(aTab) ||
+        isLastTab ||
         aTab.pinned ||
         !isVisibleTab ||
         this.tabContainer.verticalMode ||
@@ -4761,23 +4762,6 @@
         aTab,
         this
       );
-    }
-
-    
-
-
-
-
-
-
-
-    #isLastTabInWindow(tab) {
-      for (const otherTab of this.tabs) {
-        if (otherTab != tab && otherTab.isOpen && !otherTab.hidden) {
-          return false;
-        }
-      }
-      return true;
     }
 
     _hasBeforeUnload(aTab) {
@@ -4859,7 +4843,11 @@
 
       var closeWindow = false;
       var newTab = false;
-      if (this.#isLastTabInWindow(aTab)) {
+      if (
+        aTab.visible &&
+        this.visibleTabs.length == 1 &&
+        !this.tabsInCollapsedTabGroups.length
+      ) {
         closeWindow =
           closeWindowWithLastTab != null
             ? closeWindowWithLastTab
