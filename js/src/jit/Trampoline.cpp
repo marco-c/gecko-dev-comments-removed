3905990813c17830d2f8f3d3c3309c79b1a217be
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-06-11 02:11:20 +0000

    Bug 1960433: Make FrameDescriptor a real type r=jandem
    Differential Revision: https://phabricator.services.mozilla.com/D251069
    
    UltraBlame original commit: 8018a8316e7e3235dc436a624259b0852a237232

--- js/src/jit/Trampoline.cpp
+++ js/src/jit/Trampoline.cpp
@@ -1,8 +1,8 @@
-/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*-
- * vim: set ts=8 sts=2 et sw=2 tw=80:
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+
+
+
+
 
 #include <initializer_list>
 
@@ -42,14 +42,14 @@ void JitRuntime::generateProfilerExitFrameTailStub(MacroAssembler& masm,
   static constexpr size_t CallerFPOffset =
       CommonFrameLayout::offsetOfCallerFramePtr();
 
-  // Assert the caller frame's type is one of the types we expect.
+  
   auto emitAssertPrevFrameType = [&masm](
                                      Register framePtr, Register scratch,
                                      std::initializer_list<FrameType> types) {
 #ifdef DEBUG
     masm.loadPtr(Address(framePtr, CommonFrameLayout::offsetOfDescriptor()),
                  scratch);
-    masm.and32(Imm32(FRAMETYPE_MASK), scratch);
+    masm.and32(Imm32(FrameDescriptor::TypeMask), scratch);
 
     Label checkOk;
     for (FrameType type : types) {
@@ -66,49 +66,49 @@ void JitRuntime::generateProfilerExitFrameTailStub(MacroAssembler& masm,
   regs.take(JSReturnOperand);
   Register scratch = regs.takeAny();
 
-  // The code generated below expects that the current frame pointer points
-  // to an Ion or Baseline frame, at the state it would be immediately before
-  // the frame epilogue and ret(). Thus, after this stub's business is done, it
-  // restores the frame pointer and stack pointer, then executes a ret() and
-  // returns directly to the caller frame, on behalf of the callee script that
-  // jumped to this code.
-  //
-  // Thus the expected state is:
-  //
-  //    [JitFrameLayout] <-- FramePointer
-  //    [frame contents] <-- StackPointer
-  //
-  // The generated jitcode is responsible for overwriting the
-  // jitActivation->lastProfilingFrame field with a pointer to the previous
-  // Ion or Baseline jit-frame that was pushed before this one. It is also
-  // responsible for overwriting jitActivation->lastProfilingCallSite with
-  // the return address into that frame.
-  //
-  // So this jitcode is responsible for "walking up" the jit stack, finding
-  // the previous Ion or Baseline JS frame, and storing its address and the
-  // return address into the appropriate fields on the current jitActivation.
-  //
-  // There are a fixed number of different path types that can lead to the
-  // current frame, which is either a Baseline or Ion frame:
-  //
-  // <Baseline-Or-Ion>
-  // ^
-  // |
-  // ^--- Ion (or Baseline JSOp::Resume)
-  // |
-  // ^--- Baseline Stub <---- Baseline
-  // |
-  // ^--- IonICCall <---- Ion
-  // |
-  // ^--- Entry Frame (BaselineInterpreter) (unwrapped)
-  // |
-  // ^--- Arguments Rectifier (unwrapped)
-  // |
-  // ^--- Trampoline Native (unwrapped)
-  // |
-  // ^--- Entry Frame (CppToJSJit or WasmToJSJit)
-  //
-  // NOTE: Keep this in sync with JSJitProfilingFrameIterator::moveToNextFrame!
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
 
   Register actReg = regs.takeAny();
   masm.loadJSContext(actReg);
@@ -121,7 +121,7 @@ void JitRuntime::generateProfilerExitFrameTailStub(MacroAssembler& masm,
                                 JitActivation::offsetOfLastProfilingCallSite());
 
 #ifdef DEBUG
-  // Ensure that frame we are exiting is current lastProfilingFrame
+  
   {
     masm.loadPtr(lastProfilingFrame, scratch);
     Label checkOk;
@@ -134,21 +134,21 @@ void JitRuntime::generateProfilerExitFrameTailStub(MacroAssembler& masm,
   }
 #endif
 
-  // Move FP into a scratch register and use that scratch register below, to
-  // allow unwrapping rectifier frames without clobbering FP.
+  
+  
   Register fpScratch = regs.takeAny();
   masm.mov(FramePointer, fpScratch);
 
   Label again;
   masm.bind(&again);
 
-  // Load the frame descriptor into |scratch|, figure out what to do depending
-  // on its type.
+  
+  
   masm.loadPtr(Address(fpScratch, JitFrameLayout::offsetOfDescriptor()),
                scratch);
-  masm.and32(Imm32(FRAMETYPE_MASK), scratch);
+  masm.and32(Imm32(FrameDescriptor::TypeMask), scratch);
 
-  // Handling of each case is dependent on FrameDescriptor.type
+  
   Label handle_BaselineOrIonJS;
   Label handle_BaselineStub;
   Label handle_Rectifier;
@@ -157,8 +157,8 @@ void JitRuntime::generateProfilerExitFrameTailStub(MacroAssembler& masm,
   Label handle_IonICCall;
   Label handle_Entry;
 
-  // We check for IonJS and BaselineStub first because these are the most common
-  // types. Calls from Baseline are usually from a BaselineStub frame.
+  
+  
   masm.branch32(Assembler::Equal, scratch, Imm32(FrameType::IonJS),
                 &handle_BaselineOrIonJS);
   masm.branch32(Assembler::Equal, scratch, Imm32(FrameType::BaselineStub),
@@ -186,14 +186,14 @@ void JitRuntime::generateProfilerExitFrameTailStub(MacroAssembler& masm,
 
   masm.bind(&handle_BaselineOrIonJS);
   {
-    // Returning directly to a Baseline or Ion frame.
+    
 
-    // lastProfilingCallSite := ReturnAddress
+    
     masm.loadPtr(Address(fpScratch, JitFrameLayout::offsetOfReturnAddress()),
                  scratch);
     masm.storePtr(scratch, lastProfilingCallSite);
 
-    // lastProfilingFrame := CallerFrame
+    
     masm.loadPtr(Address(fpScratch, CallerFPOffset), scratch);
     masm.storePtr(scratch, lastProfilingFrame);
 
@@ -202,18 +202,18 @@ void JitRuntime::generateProfilerExitFrameTailStub(MacroAssembler& masm,
     masm.ret();
   }
 
-  // Shared implementation for BaselineStub and IonICCall frames.
+  
   auto emitHandleStubFrame = [&](FrameType expectedPrevType) {
-    // Load pointer to stub frame and assert type of its caller frame.
+    
     masm.loadPtr(Address(fpScratch, CallerFPOffset), fpScratch);
     emitAssertPrevFrameType(fpScratch, scratch, {expectedPrevType});
 
-    // lastProfilingCallSite := StubFrame.ReturnAddress
+    
     masm.loadPtr(Address(fpScratch, CommonFrameLayout::offsetOfReturnAddress()),
                  scratch);
     masm.storePtr(scratch, lastProfilingCallSite);
 
-    // lastProfilingFrame := StubFrame.CallerFrame
+    
     masm.loadPtr(Address(fpScratch, CallerFPOffset), scratch);
     masm.storePtr(scratch, lastProfilingFrame);
 
@@ -224,20 +224,20 @@ void JitRuntime::generateProfilerExitFrameTailStub(MacroAssembler& masm,
 
   masm.bind(&handle_BaselineStub);
   {
-    // BaselineJS => BaselineStub frame.
+    
     emitHandleStubFrame(FrameType::BaselineJS);
   }
 
   masm.bind(&handle_IonICCall);
   {
-    // IonJS => IonICCall frame.
+    
     emitHandleStubFrame(FrameType::IonJS);
   }
 
   masm.bind(&handle_Rectifier);
   {
-    // There can be multiple previous frame types so just "unwrap" the arguments
-    // rectifier frame and try again.
+    
+    
     masm.loadPtr(Address(fpScratch, CallerFPOffset), fpScratch);
     emitAssertPrevFrameType(
         fpScratch, scratch,
@@ -248,7 +248,7 @@ void JitRuntime::generateProfilerExitFrameTailStub(MacroAssembler& masm,
 
   masm.bind(&handle_TrampolineNative);
   {
-    // Unwrap this frame, similar to arguments rectifier frames.
+    
     masm.loadPtr(Address(fpScratch, CallerFPOffset), fpScratch);
     emitAssertPrevFrameType(
         fpScratch, scratch,
@@ -260,7 +260,7 @@ void JitRuntime::generateProfilerExitFrameTailStub(MacroAssembler& masm,
   if (JitOptions.emitInterpreterEntryTrampoline) {
     masm.bind(&handle_BaselineInterpreterEntry);
     {
-      // Unwrap the baseline interpreter entry frame and try again.
+      
       masm.loadPtr(Address(fpScratch, CallerFPOffset), fpScratch);
       emitAssertPrevFrameType(
           fpScratch, scratch,
@@ -273,11 +273,11 @@ void JitRuntime::generateProfilerExitFrameTailStub(MacroAssembler& masm,
 
   masm.bind(&handle_Entry);
   {
-    // FrameType::CppToJSJit / FrameType::WasmToJSJit
-    //
-    // A fast-path wasm->jit transition frame is an entry frame from the point
-    // of view of the JIT.
-    // Store null into both fields.
+    
+    
+    
+    
+    
     masm.movePtr(ImmPtr(nullptr), scratch);
     masm.storePtr(scratch, lastProfilingCallSite);
     masm.storePtr(scratch, lastProfilingFrame);
