be05d8cf5bcc809f0c2a270fb4a8bdc029879bb0
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-06-11 02:29:20 +0000

    Bug 1960577 - LNA modifications to http transactions. r=necko-reviewers,kershaw,valentin
    Differential Revision: https://phabricator.services.mozilla.com/D238480
    
    UltraBlame original commit: 1b6b9b7e3d89eecd17696c5305c11f80ed731000

--- netwerk/protocol/http/nsHttpTransaction.cpp
+++ netwerk/protocol/http/nsHttpTransaction.cpp
@@ -171,7 +171,10 @@ nsresult nsHttpTransaction::Init(
     uint64_t browserId, HttpTrafficCategory trafficCategory,
     nsIRequestContext* requestContext, ClassOfService classOfService,
     uint32_t initialRwin, bool responseTimeoutEnabled, uint64_t channelId,
-    TransactionObserverFunc&& transactionObserver) {
+    TransactionObserverFunc&& transactionObserver,
+    nsILoadInfo::IPAddressSpace aParentIpAddressSpace,
+    const dom::ContentPermissionRequestBase::PromptResult
+        aLnaPermissionStatus) {
   nsresult rv;
 
   LOG1(("nsHttpTransaction::Init [this=%p caps=%x]\n", this, caps));
@@ -213,6 +216,9 @@ nsresult nsHttpTransaction::Init(
   mCallbacks = callbacks;
   mConsumerTarget = target;
   mCaps = caps;
+
+  mParentIPAddressSpace = aParentIpAddressSpace;
+  mLnaPermissionStatus = aLnaPermissionStatus;
   
   
   mEarlyHintObserver = do_QueryInterface(eventsink);
