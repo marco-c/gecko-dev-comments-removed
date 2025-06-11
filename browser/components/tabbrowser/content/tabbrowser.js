4e6c4e93a59576ddd4bcb578a0ed105e10815f22
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-06-11 02:20:41 +0000

    Bug 1969164 - close window when dragging tab group out of window r=jswinarton,tabbrowser-reviewers,dao
    When a window consists only of a tab group and its tabs, dragging the tab group into the tab strip of another window can leave the starting window in a broken, empty state.
    
    The normal behavior when dragging a tab group is to temporarily collapse the tab group, but this seems to have an unintended interaction with the Tabbrowser logic that decides whether to close a window when the last tab is removed. At the time of the last tab being removed, it is in a temporarily collapsed group and therefore not visible.
    
    This patch hardens the logic for calculating whether a tab is the last tab in a window. It ensures that the window will close if the last tab in the window is in a collapsed tab group.
    
    Differential Revision: https://phabricator.services.mozilla.com/D251943
    
    UltraBlame original commit: 367bc904b4fd00b55b628badd4a50a63bf578a9a

--- browser/components/tabbrowser/content/tabbrowser.js
+++ browser/components/tabbrowser/content/tabbrowser.js
@@ -4678,7 +4678,6 @@
       }
 
       let isVisibleTab = aTab.visible;
-      let isLastTab = isVisibleTab && this.visibleTabs.length == 1;
       
       
       
@@ -4719,7 +4718,7 @@
       if (
         !animate  ||
         gReduceMotion ||
-        isLastTab ||
+        this.#isLastTabInWindow(aTab) ||
         aTab.pinned ||
         !isVisibleTab ||
         this.tabContainer.verticalMode ||
@@ -4762,6 +4761,23 @@
         aTab,
         this
       );
+    }
+
+    
+
+
+
+
+
+
+
+    #isLastTabInWindow(tab) {
+      for (const otherTab of this.tabs) {
+        if (otherTab != tab && otherTab.isOpen && !otherTab.hidden) {
+          return false;
+        }
+      }
+      return true;
     }
 
     _hasBeforeUnload(aTab) {
@@ -4843,11 +4859,7 @@
 
       var closeWindow = false;
       var newTab = false;
-      if (
-        aTab.visible &&
-        this.visibleTabs.length == 1 &&
-        !this.tabsInCollapsedTabGroups.length
-      ) {
+      if (this.#isLastTabInWindow(aTab)) {
         closeWindow =
           closeWindowWithLastTab != null
             ? closeWindowWithLastTab
