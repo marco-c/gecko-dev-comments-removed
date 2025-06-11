beebbd3de130076c5078ecf29188169dbec963f7
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-06-11 01:55:31 +0000

    Bug 1969015 - Remove Lightweight FedCM specific codepaths, to be integrated in directly with full FedCM - r=anti-tracking-reviewers,webidl,smaug,timhuang
    Differential Revision: https://phabricator.services.mozilla.com/D251616
    
    UltraBlame original commit: ff3135366145bb446e065e09404d74b1e3d3ed2d

--- toolkit/components/credentialmanagement/IdentityCredentialRequestManager.h
+++ toolkit/components/credentialmanagement/IdentityCredentialRequestManager.h
@@ -7,13 +7,7 @@
 #ifndef MOZILLA_IDENTITYCREDENTIALREQUESTMANAGER_H_
 #define MOZILLA_IDENTITYCREDENTIALREQUESTMANAGER_H_
 
-#include "mozilla/ClearOnShutdown.h"
-#include "mozilla/PrincipalHashKey.h"
-#include "mozilla/dom/IdentityCredential.h"
-#include "mozilla/dom/IdentityCredentialBinding.h"
-#include "mozilla/dom/IPCIdentityCredential.h"
-#include "nsIPrincipal.h"
-#include "nsTHashMap.h"
+#include "nsISupports.h"
 
 namespace mozilla {
 
@@ -22,23 +16,6 @@ class IdentityCredentialRequestManager final : nsISupports {
   NS_DECL_ISUPPORTS
 
   static IdentityCredentialRequestManager* GetInstance();
-
-  
-  
-  
-  nsresult StorePendingRequest(
-      const nsCOMPtr<nsIPrincipal>& aRPPrincipal,
-      const dom::IdentityCredentialRequestOptions& aRequest,
-      const RefPtr<
-          dom::IdentityCredential::GetIPCIdentityCredentialPromise::Private>&
-          aPromise,
-      const RefPtr<dom::CanonicalBrowsingContext>& aBrowsingContext);
-
-  
-  
-  
-  void NotifyOfStoredCredential(const nsCOMPtr<nsIPrincipal>& aIDPPrincipal,
-                                const dom::IPCIdentityCredential& aCredential);
 
   IdentityCredentialRequestManager(IdentityCredentialRequestManager& other) =
       delete;
@@ -49,26 +26,6 @@ class IdentityCredentialRequestManager final : nsISupports {
   IdentityCredentialRequestManager() {};
   ~IdentityCredentialRequestManager() = default;
 
-  struct PendingRequestEntry {
-    nsCOMPtr<nsIPrincipal> mRPPrincipal;
-    dom::IdentityCredentialRequestOptions mRequestOptions;
-    RefPtr<dom::IdentityCredential::GetIPCIdentityCredentialPromise::Private>
-        mPromise;
-    RefPtr<dom::CanonicalBrowsingContext> mBrowsingContext;
-
-    PendingRequestEntry(
-        nsIPrincipal* aRPPrincipal,
-        const dom::IdentityCredentialRequestOptions& aRequestOptions,
-        const RefPtr<
-            dom::IdentityCredential::GetIPCIdentityCredentialPromise::Private>&
-            aPromise,
-        const RefPtr<dom::CanonicalBrowsingContext>& aBrowsingContext)
-        : mRPPrincipal(aRPPrincipal),
-          mRequestOptions(aRequestOptions),
-          mPromise(aPromise),
-          mBrowsingContext(aBrowsingContext) {}
-  };
-  nsTHashMap<PrincipalHashKey, nsTArray<PendingRequestEntry>> mPendingRequests;
 };
 
 }  
