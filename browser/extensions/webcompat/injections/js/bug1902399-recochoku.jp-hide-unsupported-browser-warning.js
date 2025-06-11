cd419df7e08038e4dd265a44c704cf17bfc139d3
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-06-11 01:19:40 +0000

    Bug 1902399 - add a JS webcompat intervention to hide the browser warning on recochoku.jp; r=denschub,webcompat-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D250870
    
    UltraBlame original commit: 2c9584b682cb983589d41f9ec6c66446c42f00a7

---
+++ browser/extensions/webcompat/injections/js/bug1902399-recochoku.jp-hide-unsupported-browser-warning.js
@@ -0,0 +1,38 @@
+
+
+
+
+"use strict";
+
+
+
+
+
+const callback = (mutations, observer) => {
+  console.error(mutations, observer);
+  const search = document.evaluate(
+    "//*[text()[contains(., 'Chromeブラウザの最新版をご利用ください')]]",
+    document,
+    null,
+    4
+  );
+  const found = search.iterateNext();
+  if (found) {
+    found.closest(".header-caution").remove();
+    observer?.disconnect();
+  }
+};
+
+const observer = new MutationObserver(callback);
+observer.observe(document.documentElement, {
+  childList: true,
+  subtree: true,
+});
+
+window.addEventListener("DOMContentLoaded", () => {
+  const mutations = observer.takeRecords();
+  observer.disconnect();
+  if (mutations.length) {
+    callback(mutations);
+  }
+});
