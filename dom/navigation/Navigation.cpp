b64465d20f72f1ece7c7f17c2763ae5d49b2ab1c
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-06-11 01:02:12 +0000

    Revert "Bug 1968194 - Part 2: Make sure to resolve or reject method trackers. r=jjaschke"
    This reverts commit bbbcabd371402af687b1f622cc81e9c2dfbb29a6.
    
    UltraBlame original commit: 0ec0b17b043c77d6c6e0a817cded944facb30921

--- dom/navigation/Navigation.cpp
+++ dom/navigation/Navigation.cpp
@@ -62,44 +62,6 @@ struct NavigationAPIMethodTracker final : public nsISupports {
     mozilla::HoldJSObjects(this);
   }
 
-  
-  void CleanUp() { Navigation::CleanUp(this); }
-
-  
-  void NotifyAboutCommittedToEntry(NavigationHistoryEntry* aNHE) {
-    
-    mCommittedToEntry = aNHE;
-    if (mSerializedState) {
-      
-      aNHE->SetState(
-          static_cast<nsStructuredCloneContainer*>(mSerializedState.get()));
-      
-      
-      mSerializedState = nullptr;
-    }
-    mCommittedPromise->MaybeResolve(aNHE);
-  }
-
-  
-  void ResolveFinishedPromise() {
-    
-    MOZ_DIAGNOSTIC_ASSERT(mCommittedToEntry);
-    
-    mFinishedPromise->MaybeResolve(mCommittedToEntry);
-    
-    CleanUp();
-  }
-
-  
-  void RejectFinishedPromise(JS::Handle<JS::Value> aException) {
-    
-    mCommittedPromise->MaybeReject(aException);
-    
-    mFinishedPromise->MaybeReject(aException);
-    
-    CleanUp();
-  }
-
   RefPtr<Navigation> mNavigationObject;
   Maybe<nsID> mKey;
   JS::Heap<JS::Value> mInfo;
@@ -297,10 +259,6 @@ void Navigation::UpdateEntriesForSameDocumentNavigation(
   }
 
   
-  if (mOngoingAPIMethodTracker) {
-    RefPtr<NavigationHistoryEntry> currentEntry = GetCurrentEntry();
-    mOngoingAPIMethodTracker->NotifyAboutCommittedToEntry(currentEntry);
-  }
 
   
   {
@@ -776,9 +734,7 @@ bool Navigation::InnerFireNavigateEvent(
   init.mDownloadRequest = aDownloadRequestFilename;
 
   
-  if (apiMethodTracker) {
-    init.mInfo = apiMethodTracker->mInfo;
-  }
+  
 
   
   init.mHasUAVisualTransition =
@@ -952,7 +908,7 @@ bool Navigation::InnerFireNavigateEvent(
 
               
               if (apiMethodTracker) {
-                apiMethodTracker->ResolveFinishedPromise();
+                apiMethodTracker->mFinishedPromise->MaybeResolveWithUndefined();
               }
 
               
@@ -1015,7 +971,7 @@ bool Navigation::InnerFireNavigateEvent(
 
   
   if (apiMethodTracker) {
-    apiMethodTracker->CleanUp();
+    CleanUp(apiMethodTracker);
   }
 
   
@@ -1123,7 +1079,7 @@ void Navigation::AbortOngoingNavigation(JSContext* aCx,
 
   
   if (mOngoingAPIMethodTracker) {
-    mOngoingAPIMethodTracker->RejectFinishedPromise(error);
+    mOngoingAPIMethodTracker->mFinishedPromise->MaybeReject(error);
   }
 
   
