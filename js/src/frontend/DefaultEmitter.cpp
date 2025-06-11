e708420fdb5cdfb58e9e7506515b7b8b8b95cfa4
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-06-11 02:15:44 +0000

    Bug 1969950: Use StrictConstantEq in undefined comparison in default arguments. r=jandem
    Differential Revision: https://phabricator.services.mozilla.com/D252568
    
    UltraBlame original commit: bef475eacfbf08fc2a727c519d69481808a89113

--- js/src/frontend/DefaultEmitter.cpp
+++ js/src/frontend/DefaultEmitter.cpp
@@ -1,15 +1,15 @@
-/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*-
- * vim: set ts=8 sts=2 et sw=2 tw=80:
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+
+
+
+
 
 #include "frontend/DefaultEmitter.h"
 
-#include "mozilla/Assertions.h"  // MOZ_ASSERT
+#include "mozilla/Assertions.h"  
 
-#include "frontend/BytecodeEmitter.h"  // BytecodeEmitter
-#include "vm/Opcodes.h"                // JSOp
+#include "frontend/BytecodeEmitter.h"  
+#include "vm/Opcodes.h"                
 
 using namespace js;
 using namespace js::frontend;
@@ -21,7 +21,7 @@ DefaultEmitter::DefaultEmitter(BytecodeEmitter* bce) : bce_(bce) {}
 bool DefaultEmitter::prepareForDefault() {
   MOZ_ASSERT(state_ == State::Start);
 
-  //                [stack] VALUE
+  
 
   ifUndefined_.emplace(bce_);
   if (!ifUndefined_->emitIf(Nothing())) {
@@ -29,25 +29,24 @@ bool DefaultEmitter::prepareForDefault() {
   }
 
   if (!bce_->emit1(JSOp::Dup)) {
-    //              [stack] VALUE VALUE
+    
     return false;
   }
-  if (!bce_->emit1(JSOp::Undefined)) {
-    //              [stack] VALUE VALUE UNDEFINED
-    return false;
-  }
-  if (!bce_->emit1(JSOp::StrictEq)) {
-    //              [stack] VALUE EQ?
+
+  ConstantCompareOperand operand(
+      ConstantCompareOperand::EncodedType::Undefined);
+  if (!bce_->emitUint16Operand(JSOp::StrictConstantEq, operand.rawValue())) {
+    
     return false;
   }
 
   if (!ifUndefined_->emitThen()) {
-    //              [stack] VALUE
+    
     return false;
   }
 
   if (!bce_->emit1(JSOp::Pop)) {
-    //              [stack]
+    
     return false;
   }
 
@@ -60,10 +59,10 @@ bool DefaultEmitter::prepareForDefault() {
 bool DefaultEmitter::emitEnd() {
   MOZ_ASSERT(state_ == State::Default);
 
-  //                [stack] DEFAULTVALUE
+  
 
   if (!ifUndefined_->emitEnd()) {
-    //              [stack] VALUE/DEFAULTVALUE
+    
     return false;
   }
   ifUndefined_.reset();
