5dbff93df2b3c7f3c623729b1f5d65220647a662
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-06-11 00:52:30 +0000

    Bug 1969782 - Account for the aToMarkedFrameUserSpace transform when setting up contextPaint for an SVG marker. r=longsonr
    This makes the context-fill gradient used in the marker match how it's painted
    in the context, regardless of the zoom or other transforms present. (This also
    matches the rendering I see in Chrome.)
    
    Includes a test that fails without the patch.
    
    Differential Revision: https://phabricator.services.mozilla.com/D252021
    
    UltraBlame original commit: 55d1614880870a5ded0205e894f0299d56ef1364

--- layout/svg/SVGMarkerFrame.cpp
+++ layout/svg/SVGMarkerFrame.cpp
@@ -135,7 +135,8 @@ void SVGMarkerFrame::PaintMark(gfxContext& aContext,
   
   SVGFrame->NotifySVGChanged(ISVGDisplayableFrame::TRANSFORM_CHANGED);
   auto contextPaint = MakeRefPtr<SVGContextPaintImpl>();
-  contextPaint->Init(aContext.GetDrawTarget(), aContext.CurrentMatrixDouble(),
+  contextPaint->Init(aContext.GetDrawTarget(),
+                     aToMarkedFrameUserSpace * aContext.CurrentMatrixDouble(),
                      aMarkedFrame, SVGContextPaint::GetContextPaint(marker),
                      aImgParams);
   AutoSetRestoreSVGContextPaint autoSetRestore(contextPaint,
