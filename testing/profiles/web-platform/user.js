3cee5b1cf174fb7b48f4a33226ea149294c0b4ae
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-06-11 01:09:15 +0000

    Bug 1967268 - initial wpt annotations for migrating linux from 18.04 -> 24.04 x11. r=webdriver-reviewers,jgraham
    Differential Revision: https://phabricator.services.mozilla.com/D251784
    
    UltraBlame original commit: b0ddf05b46762d8b4ae3bbad2b615c46d14b6570

--- testing/profiles/web-platform/user.js
+++ testing/profiles/web-platform/user.js
@@ -102,3 +102,11 @@ user_pref("geo.provider.network.url", "https://web-platform.test:8444/_mozilla/g
 
 
 user_pref("geo.prompt.open_system_prefs", false);
+
+
+user_pref("font.name.serif.x-western", "DejaVu Serif");
+user_pref("font.name.sans-serif.x-western", "DejaVu Sans");
+user_pref("font.name.monospace.x-western", "DejaVu Sans Mono");
+user_pref("font.name.serif.x-unicode", "DejaVu Serif");
+user_pref("font.name.sans-serif.x-unicode", "DejaVu Sans");
+user_pref("font.name.monospace.x-unicode", "DejaVu Sans Mono");
