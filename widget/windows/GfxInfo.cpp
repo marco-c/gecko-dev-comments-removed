aea16d55e602668eafc4d4b3528bdf58a8ae3bf0
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-06-11 02:09:06 +0000

    Bug 1969255 - Part 2. Switch over to managed pointers in GfxInfo. r=geckoview-reviewers,win-reviewers,gfx-reviewers,bradwerth,nalexander,gstoll
    This is a non-functional change to clean up some of the memory
    management in GfxInfo and GfxDriverInfo. This makes it easier to track
    what owns what and helps later patches in the stack.
    
    Differential Revision: https://phabricator.services.mozilla.com/D251789
    
    UltraBlame original commit: 3fd4addfcaa9680663d4fef8b7d124bb9b178e2f

--- widget/windows/GfxInfo.cpp
+++ widget/windows/GfxInfo.cpp
@@ -1198,7 +1198,7 @@ static inline bool DetectBrokenAVX() {
 }
 #endif
 
-const nsTArray<GfxDriverInfo>& GfxInfo::GetGfxDriverInfo() {
+const nsTArray<RefPtr<GfxDriverInfo>>& GfxInfo::GetGfxDriverInfo() {
   if (!sDriverInfo->Length()) {
     
 
@@ -1917,7 +1917,7 @@ OperatingSystem GfxInfo::GetOperatingSystem() {
 
 nsresult GfxInfo::GetFeatureStatusImpl(
     int32_t aFeature, int32_t* aStatus, nsAString& aSuggestedDriverVersion,
-    const nsTArray<GfxDriverInfo>& aDriverInfo, nsACString& aFailureId,
+    const nsTArray<RefPtr<GfxDriverInfo>>& aDriverInfo, nsACString& aFailureId,
     OperatingSystem* aOS ) {
   AssertNotWin32kLockdown();
 
