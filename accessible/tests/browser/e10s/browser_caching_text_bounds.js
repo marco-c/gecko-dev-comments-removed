f1a53290ef27832cb46f67d7e8dbed602b5b8f9f
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-06-16 01:25:53 +0000

    Bug 1971001 - Set the test.wait300msAfterTabSwitch pref in accessible/tests/browser/e10s/browser_caching_text_bounds.js, r=jmaher.
    Differential Revision: https://phabricator.services.mozilla.com/D253528
    
    UltraBlame original commit: a821f88f68f5b147303d81a28bca0f0a7027ef50

--- accessible/tests/browser/e10s/browser_caching_text_bounds.js
+++ accessible/tests/browser/e10s/browser_caching_text_bounds.js
@@ -148,6 +148,12 @@ async function testLineWithNonRenderedSpace(docAcc, browser, id, length) {
   }
 }
 
+add_setup(async function () {
+  await SpecialPowers.pushPrefEnv({
+    set: [["test.wait300msAfterTabSwitch", true]],
+  });
+});
+
 
 
 
