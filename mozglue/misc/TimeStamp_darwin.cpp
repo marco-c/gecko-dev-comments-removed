ad5bb145070cfba41b328ba6e688d6356f659ef0
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-06-16 01:22:59 +0000

    Bug 1971462: Fix overflow check in TicksFromMilliseconds r=glandium
    We got this right [elsewhere](https://searchfox.org/mozilla-central/source/mozglue/misc/TimeStamp.h#282-286), but wrong here. `(double) INT64_MAX` can't be correctly converted back to an int64_t.
    
    I don't think it's strictly necessary to make the same change for the lower bound, because `double(INT64_MIN)` is 0xc3e0000000000000, which is exactly equal to INT64_MIN. I made the change anyway for consistency. We get the same result either way.
    
    Differential Revision: https://phabricator.services.mozilla.com/D253413
    
    UltraBlame original commit: 7a447298d8454e9f2473f226759089bcaad3ba6d

--- mozglue/misc/TimeStamp_darwin.cpp
+++ mozglue/misc/TimeStamp_darwin.cpp
@@ -96,9 +96,11 @@ int64_t BaseTimeDurationPlatformUtils::TicksFromMilliseconds(
     double aMilliseconds) {
   MOZ_ASSERT(gInitialized, "calling TimeDuration too early");
   double result = (aMilliseconds * kNsPerMsd) / sNsPerTick;
-  if (result > double(INT64_MAX)) {
+  
+  
+  if (result >= double(INT64_MAX)) {
     return INT64_MAX;
-  } else if (result < double(INT64_MIN)) {
+  } else if (result <= double(INT64_MIN)) {
     return INT64_MIN;
   }
 
