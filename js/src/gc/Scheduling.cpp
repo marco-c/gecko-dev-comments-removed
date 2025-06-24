a10f5842b7a236f046cf9cb8610b8b260282cf7e
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-06-24 10:34:45 +0000

    Bug 1972760: Remove references to ForOfPIC. r=jandem
    `ForOfPIC` was removed in bug 1947767.
    
    Differential Revision: https://phabricator.services.mozilla.com/D254170
    
    UltraBlame original commit: 1cf34121fee383dbbc660293da93fcf4d74115ed

--- js/src/gc/Scheduling.cpp
+++ js/src/gc/Scheduling.cpp
@@ -654,7 +654,7 @@ inline bool MemoryTracker::allowMultipleAssociations(MemoryUse use) {
   
   return isNonGCMemoryUse(use) || use == MemoryUse::RegExpSharedBytecode ||
          use == MemoryUse::BreakpointSite || use == MemoryUse::Breakpoint ||
-         use == MemoryUse::ForOfPICStub || use == MemoryUse::ICUObject;
+         use == MemoryUse::ICUObject;
 }
 
 void MemoryTracker::trackGCMemory(Cell* cell, size_t nbytes, MemoryUse use) {
