84e91d88b8c34bf922e38583795da1bbe6ab5aa7
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-06-24 10:33:30 +0000

    Bug 1972681 - Bump vendored `importlib-metadata` to version `8.7.0` r=ahal,mach-reviewers
    Updated importlib-metadata v6.11.0 -> v8.7.0
    
    Differential Revision: https://phabricator.services.mozilla.com/D254121
    
    UltraBlame original commit: 8674bf821b6639eb55a64e644640e4471411accd

--- third_party/python/importlib_metadata/importlib_metadata/_meta.py
+++ third_party/python/importlib_metadata/importlib_metadata/_meta.py
@@ -1,63 +1,71 @@
-from typing import Protocol
-from typing import Any, Dict, Iterator, List, Optional, TypeVar, Union, overload
+from __future__ import annotations
 
+import os
+from collections.abc import Iterator
+from typing import (
+    Any,
+    Protocol,
+    TypeVar,
+    overload,
+)
 
 _T = TypeVar("_T")
 
 
 class PackageMetadata(Protocol):
-    def __len__(self) -> int:
-        ...  
+    def __len__(self) -> int: ...  
 
-    def __contains__(self, item: str) -> bool:
-        ...  
+    def __contains__(self, item: str) -> bool: ...  
 
-    def __getitem__(self, key: str) -> str:
-        ...  
+    def __getitem__(self, key: str) -> str: ...  
 
-    def __iter__(self) -> Iterator[str]:
-        ...  
+    def __iter__(self) -> Iterator[str]: ...  
 
     @overload
-    def get(self, name: str, failobj: None = None) -> Optional[str]:
-        ...  
+    def get(
+        self, name: str, failobj: None = None
+    ) -> str | None: ...  
 
     @overload
-    def get(self, name: str, failobj: _T) -> Union[str, _T]:
-        ...  
+    def get(self, name: str, failobj: _T) -> str | _T: ...  
 
     
     @overload
-    def get_all(self, name: str, failobj: None = None) -> Optional[List[Any]]:
-        ...  
+    def get_all(
+        self, name: str, failobj: None = None
+    ) -> list[Any] | None: ...  
 
     @overload
-    def get_all(self, name: str, failobj: _T) -> Union[List[Any], _T]:
+    def get_all(self, name: str, failobj: _T) -> list[Any] | _T:
         """
         Return all values associated with a possibly multi-valued key.
         """
 
     @property
-    def json(self) -> Dict[str, Union[str, List[str]]]:
+    def json(self) -> dict[str, str | list[str]]:
         """
         A JSON-compatible form of the metadata.
         """
 
 
-class SimplePath(Protocol[_T]):
+class SimplePath(Protocol):
     """
-    A minimal subset of pathlib.Path required by PathDistribution.
+    A minimal subset of pathlib.Path required by Distribution.
     """
 
-    def joinpath(self, other: Union[str, _T]) -> _T:
-        ...  
+    def joinpath(
+        self, other: str | os.PathLike[str]
+    ) -> SimplePath: ...  
 
-    def __truediv__(self, other: Union[str, _T]) -> _T:
-        ...  
+    def __truediv__(
+        self, other: str | os.PathLike[str]
+    ) -> SimplePath: ...  
 
     @property
-    def parent(self) -> _T:
-        ...  
+    def parent(self) -> SimplePath: ...  
 
-    def read_text(self) -> str:
-        ...  
+    def read_text(self, encoding=None) -> str: ...  
+
+    def read_bytes(self) -> bytes: ...  
+
+    def exists(self) -> bool: ...  
