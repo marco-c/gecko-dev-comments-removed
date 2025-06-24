5d6db05a78ec4d7c3967f5ac0d2fbf82e42bedf6
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-06-24 09:32:23 +0000

    Bug 1972008 - Fix "Read More" link for Tracking Protection r=devtools-reviewers,nchevobbe
    Differential Revision: https://phabricator.services.mozilla.com/D253667
    
    UltraBlame original commit: ed92fb47e85762c0fe025a502e92b51843be68fb

--- devtools/client/netmonitor/src/utils/doc-utils.js
+++ devtools/client/netmonitor/src/utils/doc-utils.js
@@ -182,7 +182,7 @@ function getFilterBoxURL() {
 
 
 function getTrackingProtectionURL() {
-  return `${MDN_URL}Mozilla/Firefox/Privacy/Guides/Tracking_Protection${getGAParams()}`;
+  return `${MDN_URL}Web/Privacy/Guides/Firefox_tracking_protection${getGAParams()}`;
 }
 
 
