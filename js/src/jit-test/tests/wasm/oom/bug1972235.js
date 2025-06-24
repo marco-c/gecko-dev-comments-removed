a40947e3af23fb7698da0da4bc65386343ca68a4
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-06-24 10:24:30 +0000

    Bug 1972235 - Handle properly OOM in toFixedLengthBufferImpl. r=rhunt
    Differential Revision: https://phabricator.services.mozilla.com/D253879
    
    UltraBlame original commit: 73c94c0054da006331bf776894f3e8c8a15a5474

---
+++ js/src/jit-test/tests/wasm/oom/bug1972235.js
@@ -0,0 +1,13 @@
+
+
+
+
+function f() {
+  var x = { initial: 0, maximum: 1 };
+  x.shared = {};
+  y = new WebAssembly.Memory(x);
+  y.toFixedLengthBuffer(y.grow(x.maximum));
+}
+oomTest(() => {
+  for (let i = 0; i < 4; i++) f();
+});
