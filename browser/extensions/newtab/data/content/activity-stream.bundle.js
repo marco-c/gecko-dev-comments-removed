42da61d700a50c54f0aaf555cc1b32c2db2f7b02
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-06-24 09:43:58 +0000

    Bug 1972389 – Only show pinned shortcuts on hover. r=home-newtab-reviewers,nbarrett
    Differential Revision: https://phabricator.services.mozilla.com/D253861
    
    UltraBlame original commit: f865cd5e57814dcb4d46114d2fa5a2e65aa2f196

--- browser/extensions/newtab/data/content/activity-stream.bundle.js
+++ browser/extensions/newtab/data/content/activity-stream.bundle.js
@@ -9280,7 +9280,9 @@ class TopSiteLink extends (external_React_default()).PureComponent {
       "data-is-sponsored-link": !!link.sponsored_tile_id,
       title: title,
       onFocus: this.props.onFocus
-    }, external_React_default().createElement("div", {
+    }, shortcutsRefresh && link.isPinned && external_React_default().createElement("div", {
+      className: "icon icon-pin-small"
+    }), external_React_default().createElement("div", {
       className: "tile",
       "aria-hidden": true
     }, external_React_default().createElement("div", {
@@ -9296,9 +9298,7 @@ class TopSiteLink extends (external_React_default()).PureComponent {
       className: "top-site-icon default-icon",
       "data-fallback": smallFaviconStyle ? "" : letterFallback,
       style: smallFaviconStyle
-    })), shortcutsRefresh && link.isPinned && external_React_default().createElement("div", {
-      className: "icon icon-pin-small"
-    }), !shortcutsRefresh && link.searchTopSite && external_React_default().createElement("div", {
+    })), !shortcutsRefresh && link.searchTopSite && external_React_default().createElement("div", {
       className: "top-site-icon search-topsite"
     })), external_React_default().createElement("div", {
       className: `title${link.isPinned ? " has-icon pinned" : ""}${link.type === SPOC_TYPE || link.show_sponsored_label ? " sponsored" : ""}`
