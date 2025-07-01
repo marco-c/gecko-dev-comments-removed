85949266a46f2288085b5516c0da26041eac69cb
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-07-01 02:08:22 +0000

    Bug 1973981 - Remove unused NormalizedConstraints::mBadConstraint. r=jib
    Differential Revision: https://phabricator.services.mozilla.com/D224719
    
    UltraBlame original commit: ec1fbdacafbefcb8d6acf4e4b0c72a0880fb0d5e

--- dom/media/webrtc/MediaTrackConstraints.h
+++ dom/media/webrtc/MediaTrackConstraints.h
@@ -276,7 +276,6 @@ struct NormalizedConstraints : public NormalizedConstraintSet {
                                  nsTArray<MemberPtrType>* aList = nullptr);
 
   std::vector<NormalizedConstraintSet> mAdvanced;
-  const char* mBadConstraint;
 };
 
 
