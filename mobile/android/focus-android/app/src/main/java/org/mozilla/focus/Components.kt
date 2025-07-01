aee364da4488088768e179430fd415af5b3b34e0
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-07-01 01:13:35 +0000

    Bug 1956172 - The downloaded file should be completely deleted from the phone. r=android-reviewers,tthibaud,android-l10n-reviewers,delphine
    Differential Revision: https://phabricator.services.mozilla.com/D252490
    
    UltraBlame original commit: cebed3c91dbb9a199ae9f62cfd5533c3dee523a0

--- mobile/android/focus-android/app/src/main/java/org/mozilla/focus/Components.kt
+++ mobile/android/focus-android/app/src/main/java/org/mozilla/focus/Components.kt
@@ -36,7 +36,6 @@ import mozilla.components.feature.prompts.file.FileUploadsDirCleaner
 import mozilla.components.feature.prompts.file.FileUploadsDirCleanerMiddleware
 import mozilla.components.feature.search.SearchApplicationName
 import mozilla.components.feature.search.SearchDeviceType
-import mozilla.components.feature.search.SearchEngineSelector
 import mozilla.components.feature.search.SearchUpdateChannel
 import mozilla.components.feature.search.SearchUseCases
 import mozilla.components.feature.search.middleware.AdsTelemetryMiddleware
@@ -186,7 +185,11 @@ class Components(
         BrowserStore(
             middleware = listOf(
                 TelemetryMiddleware(),
-                DownloadMiddleware(context, DownloadService::class.java),
+                DownloadMiddleware(
+                    applicationContext = context,
+                    downloadServiceClass = DownloadService::class.java,
+                    deleteFileFromStorage = { false },
+                ),
                 SanityCheckMiddleware(),
                 // We are currently using the default location service. We should consider using
                 // an actual implementation:
@@ -404,7 +407,6 @@ private fun getSearchEngineSelectorConfig(context: Context): SearchEngineSelecto
         deviceType = deviceType,
         experiment = "",
         updateChannel = updateChannel,
-        selector = SearchEngineSelector(),
         service = context.components.remoteSettingsService,
     )
 }
