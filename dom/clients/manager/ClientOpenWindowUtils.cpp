8f1648ff34022ef301ec096e86c71a2c1d3492d8
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-07-01 01:51:53 +0000

    Bug 1973269 - Pass the URI directly to browser chrome page r=asuth,Gijs
    Currently we load the URI using the resulting BrowsingContext, but that causes race with session restore. Passing the URI as an argument to the browser chrome page solves the issue.
    
    Differential Revision: https://phabricator.services.mozilla.com/D254539
    
    UltraBlame original commit: 8b31565485180ab95747dfe6c01b3cd4552930e0

--- dom/clients/manager/ClientOpenWindowUtils.cpp
+++ dom/clients/manager/ClientOpenWindowUtils.cpp
@@ -215,6 +215,16 @@ static Result<Ok, nsresult> OpenNewWindow(
     nsOpenWindowInfo* aOpenWindowInfo) {
   nsresult rv;
 
+  
+  
+  nsAutoCString uriToLoad;
+  MOZ_TRY(aArgsValidated.uri->GetSpec(uriToLoad));
+
+  nsCOMPtr<nsISupportsCString> nsUriToLoad =
+      do_CreateInstance(NS_SUPPORTS_CSTRING_CONTRACTID, &rv);
+  MOZ_TRY(rv);
+  MOZ_TRY(nsUriToLoad->SetData(uriToLoad));
+
   nsCOMPtr<nsISupportsPRBool> nsFalse =
       do_CreateInstance(NS_SUPPORTS_PRBOOL_CONTRACTID, &rv);
   MOZ_TRY(rv);
@@ -227,7 +237,7 @@ static Result<Ok, nsresult> OpenNewWindow(
 
   nsCOMPtr<nsIMutableArray> args = do_CreateInstance(NS_ARRAY_CONTRACTID);
   
-  args->AppendElement(nullptr);                   
+  args->AppendElement(nsUriToLoad);               
   args->AppendElement(nullptr);                   
   args->AppendElement(nullptr);                   
   args->AppendElement(nullptr);                   
@@ -256,7 +266,11 @@ static Result<Ok, nsresult> OpenNewWindow(
   return Ok();
 }
 
-void OpenWindow(const ClientOpenWindowArgsParsed& aArgsValidated,
+
+
+
+
+bool OpenWindow(const ClientOpenWindowArgsParsed& aArgsValidated,
                 nsOpenWindowInfo* aOpenInfo, BrowsingContext** aBC,
                 ErrorResult& aRv) {
   MOZ_DIAGNOSTIC_ASSERT(aBC);
@@ -279,15 +293,15 @@ void OpenWindow(const ClientOpenWindowArgsParsed& aArgsValidated,
     auto result = OpenNewWindow(aArgsValidated, aOpenInfo);
     if (NS_WARN_IF(result.isErr())) {
       aRv.ThrowTypeError("Unable to open window");
-      return;
+      return false;
     }
-    return;
+    return false;
   }
 
   if (NS_WARN_IF(!nsGlobalWindowOuter::Cast(browserWindow)->IsChromeWindow())) {
     
     aRv.ThrowTypeError("Unable to open window");
-    return;
+    return false;
   }
 
   nsCOMPtr<nsIBrowserDOMWindow> bwin =
@@ -295,7 +309,7 @@ void OpenWindow(const ClientOpenWindowArgsParsed& aArgsValidated,
 
   if (NS_WARN_IF(!bwin)) {
     aRv.ThrowTypeError("Unable to open window");
-    return;
+    return false;
   }
   nsresult rv = bwin->CreateContentWindow(
       nullptr, aOpenInfo, nsIBrowserDOMWindow::OPEN_DEFAULTWINDOW,
@@ -303,14 +317,15 @@ void OpenWindow(const ClientOpenWindowArgsParsed& aArgsValidated,
       aArgsValidated.csp, aBC);
   if (NS_WARN_IF(NS_FAILED(rv))) {
     aRv.ThrowTypeError("Unable to open window");
-    return;
+    return false;
   }
+  return true;
 }
 #endif
 
 void WaitForLoad(const ClientOpenWindowArgsParsed& aArgsValidated,
                  BrowsingContext* aBrowsingContext,
-                 ClientOpPromise::Private* aPromise) {
+                 ClientOpPromise::Private* aPromise, bool aShouldLoadURI) {
   MOZ_DIAGNOSTIC_ASSERT(aBrowsingContext);
 
   RefPtr<ClientOpPromise::Private> promise = aPromise;
@@ -342,24 +357,27 @@ void WaitForLoad(const ClientOpenWindowArgsParsed& aArgsValidated,
     return;
   }
 
-  
-  RefPtr<nsDocShellLoadState> loadState =
-      new nsDocShellLoadState(aArgsValidated.uri);
-  loadState->SetTriggeringPrincipal(aArgsValidated.principal);
-  loadState->SetFirstParty(true);
-  loadState->SetLoadFlags(
-      nsIWebNavigation::LOAD_FLAGS_DISALLOW_INHERIT_PRINCIPAL);
-  loadState->SetTriggeringRemoteType(
-      aArgsValidated.originContent
-          ? aArgsValidated.originContent->GetRemoteType()
-          : NOT_REMOTE_TYPE);
+  if (aShouldLoadURI) {
+    
+    RefPtr<nsDocShellLoadState> loadState =
+        new nsDocShellLoadState(aArgsValidated.uri);
+    loadState->SetTriggeringPrincipal(aArgsValidated.principal);
+    loadState->SetFirstParty(true);
+    loadState->SetLoadFlags(
+        nsIWebNavigation::LOAD_FLAGS_DISALLOW_INHERIT_PRINCIPAL);
+    loadState->SetTriggeringRemoteType(
+        aArgsValidated.originContent
+            ? aArgsValidated.originContent->GetRemoteType()
+            : NOT_REMOTE_TYPE);
 
-  rv = aBrowsingContext->LoadURI(loadState, true);
-  if (NS_FAILED(rv)) {
-    CopyableErrorResult result;
-    result.ThrowInvalidStateError("Unable to start the load of the actual URI");
-    promise->Reject(result, __func__);
-    return;
+    rv = aBrowsingContext->LoadURI(loadState, true);
+    if (NS_FAILED(rv)) {
+      CopyableErrorResult result;
+      result.ThrowInvalidStateError(
+          "Unable to start the load of the actual URI");
+      promise->Reject(result, __func__);
+      return;
+    }
   }
 
   
@@ -371,8 +389,7 @@ void WaitForLoad(const ClientOpenWindowArgsParsed& aArgsValidated,
 
 #ifdef MOZ_GECKOVIEW
 void GeckoViewOpenWindow(const ClientOpenWindowArgsParsed& aArgsValidated,
-                         nsOpenWindowInfo* aOpenInfo, BrowsingContext** aBC,
-                         ErrorResult& aRv) {
+                         nsOpenWindowInfo* aOpenInfo, ErrorResult& aRv) {
   MOZ_ASSERT(aOpenInfo);
 
   
@@ -487,11 +504,13 @@ RefPtr<ClientOpPromise> ClientOpenWindow(
 
   RefPtr<BrowsingContext> bc;
   IgnoredErrorResult errResult;
+  bool shouldLoadURI = true;
 #ifdef MOZ_GECKOVIEW
   
-  GeckoViewOpenWindow(argsValidated, openInfo, getter_AddRefs(bc), errResult);
+  GeckoViewOpenWindow(argsValidated, openInfo, errResult);
 #else
-  OpenWindow(argsValidated, openInfo, getter_AddRefs(bc), errResult);
+  shouldLoadURI =
+      OpenWindow(argsValidated, openInfo, getter_AddRefs(bc), errResult);
 #endif
   if (NS_WARN_IF(errResult.Failed())) {
     promise->Reject(errResult, __func__);
@@ -500,8 +519,9 @@ RefPtr<ClientOpPromise> ClientOpenWindow(
 
   browsingContextReadyPromise->Then(
       GetCurrentSerialEventTarget(), __func__,
-      [argsValidated, promise](const RefPtr<BrowsingContext>& aBC) {
-        WaitForLoad(argsValidated, aBC, promise);
+      [argsValidated, promise,
+       shouldLoadURI](const RefPtr<BrowsingContext>& aBC) {
+        WaitForLoad(argsValidated, aBC, promise, shouldLoadURI);
       },
       [promise]() {
         
