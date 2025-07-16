c11d9c2355dd51b7e965c9c21e09d43a385b6d4d
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-07-16 01:16:29 +0000

    Revert "Bug 1915629 - Implement Trusted Types support for the "pre-navigation check". r=smaug" for causing failures at nsDocShell.cpp.
    This reverts commit 13b8b008475234fd30789fa1f75a885a33dd682b.
    
    UltraBlame original commit: db779c89652ec9cf7a43b7c7c40c606f08849edd

--- dom/security/trusted-types/TrustedTypeUtils.h
+++ dom/security/trusted-types/TrustedTypeUtils.h
@@ -61,14 +61,6 @@ enum class TrustedType : int8_t {
 };
 nsString GetTrustedTypeName(TrustedType aTrustedType);
 
-void ReportSinkTypeMismatchViolations(nsIContentSecurityPolicy* aCSP,
-                                      nsICSPEventListener* aCSPEventListener,
-                                      const nsCString& aFileName,
-                                      uint32_t aLine, uint32_t aColumn,
-                                      const nsAString& aSink,
-                                      const nsAString& aSinkGroup,
-                                      const nsAString& aSource);
-
 
 
 
@@ -180,16 +172,6 @@ AreArgumentsTrustedForEnsureCSPDoesNotBlockStringCompilation(
     JS::Handle<JSString*> aBodyString,
     JS::Handle<JS::StackGCVector<JS::Value>> aParameterArgs,
     JS::Handle<JS::Value> aBodyArg, ErrorResult& aError);
-
-
-
-
-
-MOZ_CAN_RUN_SCRIPT const nsAString*
-GetConvertedScriptSourceForPreNavigationCheck(
-    nsIGlobalObject& aGlobalObject, const nsAString& aEncodedScriptSource,
-    const nsAString& aSink, Maybe<nsAutoString>& aResultHolder,
-    ErrorResult& aError);
 
 }  
 
