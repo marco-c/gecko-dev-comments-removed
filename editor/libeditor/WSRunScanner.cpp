3068046ef54300c5e9f80c748314a2e9ff821e9c
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-07-16 03:32:27 +0000

    Bug 1976608 - Make `WSRunScanner::Scan*VisibleNodeOrBlockBoundary` ignore `Comment`s r=m_kato
    They are internal APIs to return next/previous visible thing or
    a padding `<br>` element.  However, when they reach a `Comment`, they
    don't skip it and return it as a special content.  `Comment` is always
    invisible so that they should skip such invisible nodes.
    
    Differential Revision: https://phabricator.services.mozilla.com/D257125
    
    UltraBlame original commit: 74be70b2b1d44254e05d3ccbc6af961236c06792

--- editor/libeditor/WSRunScanner.cpp
+++ editor/libeditor/WSRunScanner.cpp
@@ -12,6 +12,7 @@
 
 #include "mozilla/Assertions.h"
 #include "mozilla/Casting.h"  
+#include "mozilla/dom/Comment.h"
 
 #include "nsDebug.h"
 #include "nsError.h"
@@ -204,6 +205,30 @@ WSScanResult WSRunScanner::ScanPreviousVisibleNodeOrBlockBoundaryFrom(
       return WSScanResult(*this, WSScanResult::ScanDirection::Backward,
                           TextFragmentDataAtStartRef().StartRef(),
                           TextFragmentDataAtStartRef().StartRawReason());
+    case WSType::SpecialContent: {
+      const Comment* comment = Comment::FromNode(
+          TextFragmentDataAtStartRef().GetStartReasonContent());
+      if (!comment) {
+        break;
+      }
+      
+      
+      while (true) {
+        const EditorRawDOMPoint atComment(comment);
+        WSRunScanner scanner(mScanMode, atComment,
+                             mTextFragmentDataAtStart.BlockInlineCheckMode(),
+                             mTextFragmentDataAtStart.GetAncestorLimiter());
+        if (scanner.TextFragmentDataAtStartRef().StartRawReason() ==
+            WSType::SpecialContent) {
+          if ((comment = Comment::FromNode(scanner.TextFragmentDataAtStartRef()
+                                               .GetStartReasonContent()))) {
+            
+            continue;
+          }
+        }
+        return scanner.ScanPreviousVisibleNodeOrBlockBoundaryFrom(atComment);
+      }
+    }
     default:
       break;
   }
@@ -298,6 +323,32 @@ WSScanResult WSRunScanner::ScanInclusiveNextVisibleNodeOrBlockBoundaryFrom(
       return WSScanResult(*this, WSScanResult::ScanDirection::Forward,
                           TextFragmentDataAtStartRef().EndRef(),
                           TextFragmentDataAtStartRef().EndRawReason());
+    case WSType::SpecialContent: {
+      const Comment* comment =
+          Comment::FromNode(TextFragmentDataAtStartRef().GetEndReasonContent());
+      if (!comment) {
+        break;
+      }
+      
+      
+      while (true) {
+        const EditorRawDOMPoint afterComment =
+            EditorRawDOMPoint::After(*comment);
+        WSRunScanner scanner(mScanMode, afterComment,
+                             mTextFragmentDataAtStart.BlockInlineCheckMode(),
+                             mTextFragmentDataAtStart.GetAncestorLimiter());
+        if (scanner.TextFragmentDataAtStartRef().EndRawReason() ==
+            WSType::SpecialContent) {
+          if ((comment = Comment::FromNode(scanner.TextFragmentDataAtStartRef()
+                                               .GetEndReasonContent()))) {
+            
+            continue;
+          }
+        }
+        return scanner.ScanInclusiveNextVisibleNodeOrBlockBoundaryFrom(
+            afterComment);
+      }
+    }
     default:
       break;
   }
--- editor/libeditor/WSRunScannerNestedClasses.cpp
+++ editor/libeditor/WSRunScannerNestedClasses.cpp
@@ -122,7 +122,9 @@ WSRunScanner::TextFragmentData::TextFragmentData(
     Scan aScanMode, const EditorDOMPointType& aPoint,
     BlockInlineCheck aBlockInlineCheck,
     const Element* aAncestorLimiter )
-    : mBlockInlineCheck(aBlockInlineCheck), mScanMode(aScanMode) {
+    : mAncestorLimiter(aAncestorLimiter),
+      mBlockInlineCheck(aBlockInlineCheck),
+      mScanMode(aScanMode) {
   if (NS_WARN_IF(!aPoint.IsInContentNodeAndValidInComposedDoc()) ||
       NS_WARN_IF(!aPoint.GetContainerOrContainerParentElement())) {
     
