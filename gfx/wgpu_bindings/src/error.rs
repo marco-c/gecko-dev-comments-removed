1d1fe6107a26e7bfb43c4b9749c7784b9971d61a
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-07-16 01:12:25 +0000

    Bug 1840926 - fix(webgpu): handle many missing error classification cases r=webgpu-reviewers,nical
    Differential Revision: https://phabricator.services.mozilla.com/D253606
    
    UltraBlame original commit: f02ab621c5b2f0e68889a91c8a1df5a12cec0763

--- gfx/wgpu_bindings/src/error.rs
+++ gfx/wgpu_bindings/src/error.rs
@@ -236,6 +236,8 @@ mod foreign {
             match self {
                 RequestDeviceError::Device(e) => e.error_type(),
 
+                RequestDeviceError::TimestampNormalizerInitFailed(..) => ErrorBufferType::Internal,
+
                 RequestDeviceError::UnsupportedFeature(_)
                 | RequestDeviceError::LimitsExceeded(_) => ErrorBufferType::Validation,
 
@@ -250,6 +252,8 @@ mod foreign {
             match self {
                 CreateBufferError::Device(e) => e.error_type(),
                 CreateBufferError::AccessError(e) => e.error_type(),
+
+                CreateBufferError::IndirectValidationBindGroup(_) => ErrorBufferType::Internal,
 
                 CreateBufferError::UnalignedSize
                 | CreateBufferError::InvalidUsage(_)
@@ -293,6 +297,7 @@ mod foreign {
         fn error_type(&self) -> ErrorBufferType {
             match self {
                 CreateTextureError::Device(e) => e.error_type(),
+                CreateTextureError::CreateTextureView(e) => e.error_type(),
 
                 CreateTextureError::InvalidUsage(_)
                 | CreateTextureError::InvalidDimension(_)
@@ -477,8 +482,25 @@ mod foreign {
         }
     }
 
+    impl HasErrorBufferType for EncoderStateError {
+        fn error_type(&self) -> ErrorBufferType {
+            match self {
+                EncoderStateError::Invalid
+                | EncoderStateError::Ended
+                | EncoderStateError::Locked
+                | EncoderStateError::Unlocked
+                | EncoderStateError::Submitted => ErrorBufferType::Validation,
+
+                
+                _ => ErrorBufferType::Validation,
+            }
+        }
+    }
+
     impl HasErrorBufferType for RenderBundleError {
         fn error_type(&self) -> ErrorBufferType {
+            
+            
             
             
             ErrorBufferType::Validation
@@ -493,6 +515,8 @@ mod foreign {
                 | DeviceError::Lost => ErrorBufferType::DeviceLost,
                 DeviceError::OutOfMemory => ErrorBufferType::OutOfMemory,
                 DeviceError::ResourceCreationFailed => ErrorBufferType::Internal,
+
+                
                 _ => ErrorBufferType::Internal,
             }
         }
@@ -501,6 +525,8 @@ mod foreign {
     impl HasErrorBufferType for CreateTextureViewError {
         fn error_type(&self) -> ErrorBufferType {
             match self {
+                CreateTextureViewError::Device(e) => e.error_type(),
+
                 CreateTextureViewError::InvalidTextureViewDimension { .. }
                 | CreateTextureViewError::InvalidResource(_)
                 | CreateTextureViewError::InvalidMultisampledTextureViewDimension(_)
@@ -572,6 +598,8 @@ mod foreign {
             
             
             
+            
+            
             ErrorBufferType::Validation
         }
     }
@@ -579,6 +607,7 @@ mod foreign {
     impl HasErrorBufferType for QueryError {
         fn error_type(&self) -> ErrorBufferType {
             match self {
+                QueryError::Device(e) => e.error_type(),
                 QueryError::EncoderState(e) => e.error_type(),
                 QueryError::Use(e) => e.error_type(),
                 QueryError::Resolve(e) => e.error_type(),
@@ -596,12 +625,16 @@ mod foreign {
             
             
             
+            
+            
             ErrorBufferType::Validation
         }
     }
 
     impl HasErrorBufferType for ResolveError {
         fn error_type(&self) -> ErrorBufferType {
+            
+            
             
             
             
@@ -624,12 +657,16 @@ mod foreign {
             
             
             
+            
+            
             ErrorBufferType::Validation
         }
     }
 
     impl HasErrorBufferType for CommandEncoderError {
         fn error_type(&self) -> ErrorBufferType {
+            
+            
             
             
             
@@ -642,6 +679,7 @@ mod foreign {
             match self {
                 QueueSubmitError::Queue(e) => e.error_type(),
                 QueueSubmitError::Unmap(e) => e.error_type(),
+                QueueSubmitError::CommandEncoder(e) => e.error_type(),
 
                 QueueSubmitError::DestroyedResource(_)
                 | QueueSubmitError::BufferStillMapped(_)
@@ -671,12 +709,16 @@ mod foreign {
             
             
             
+            
+            
             ErrorBufferType::Validation
         }
     }
 
     impl HasErrorBufferType for CreateRenderBundleError {
         fn error_type(&self) -> ErrorBufferType {
+            
+            
             
             
             
@@ -691,6 +733,7 @@ mod foreign {
                 CreateQuerySetError::ZeroCount
                 | CreateQuerySetError::TooManyQueries { .. }
                 | CreateQuerySetError::MissingFeatures(..) => ErrorBufferType::Validation,
+
                 
                 _ => ErrorBufferType::Validation,
             }
