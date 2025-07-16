2ad4da34685279db364e9a939d80207185696215
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-07-16 02:20:23 +0000

    Revert "Bug 1974328 - Check if newtab_message is displayed in selected browser r=home-newtab-reviewers,omc-reviewers,aminomancer,maxx" for causing newtab failures
    This reverts commit 9965b701a0e26886d9d70783161e5a04c7447048.
    
    UltraBlame original commit: d5372c77aec1538797a1546d3cf346502d391605

--- browser/extensions/newtab/data/content/activity-stream.bundle.js
+++ browser/extensions/newtab/data/content/activity-stream.bundle.js
@@ -181,7 +181,6 @@ for (const type of [
   "MESSAGE_CLICK",
   "MESSAGE_DISMISS",
   "MESSAGE_IMPRESSION",
-  "MESSAGE_NOTIFY_VISIBILITY",
   "MESSAGE_SET",
   "MESSAGE_TOGGLE_VISIBILITY",
   "NEW_TAB_INIT",
@@ -7710,7 +7709,7 @@ const INITIAL_STATE = {
   
   Messages: {
     
-    isVisible: true,
+    isHidden: false,
     
     portID: "",
     
@@ -8161,7 +8160,7 @@ function Messages(prevState = INITIAL_STATE.Messages, action) {
         portID: action.data.portID || "",
       };
     case actionTypes.MESSAGE_TOGGLE_VISIBILITY:
-      return { ...prevState, isVisible: action.data };
+      return { ...prevState, isHidden: action.data };
     default:
       return prevState;
   }
@@ -11431,52 +11430,41 @@ function MessageWrapper({
 }) {
   const message = (0,external_ReactRedux_namespaceObject.useSelector)(state => state.Messages);
   const [isIntersecting, setIsIntersecting] = (0,external_React_namespaceObject.useState)(false);
-  const [tabIsVisible, setTabIsVisible] = (0,external_React_namespaceObject.useState)(() => typeof document !== "undefined" && document.visibilityState === "visible");
   const [hasRun, setHasRun] = (0,external_React_namespaceObject.useState)();
   const handleIntersection = (0,external_React_namespaceObject.useCallback)(() => {
     setIsIntersecting(true);
+    const isVisible = document?.visibilityState && document.visibilityState === "visible";
     
-    if (tabIsVisible && message.messageData.id && !hasRun) {
+    if (isVisible && message.messageData.id) {
       setHasRun(true);
       dispatch(actionCreators.AlsoToMain({
         type: actionTypes.MESSAGE_IMPRESSION,
         data: message.messageData
       }));
     }
-  }, [dispatch, message, tabIsVisible, hasRun]);
-  (0,external_React_namespaceObject.useEffect)(() => {
-    
-    if (message.isVisible && tabIsVisible) {
-      dispatch(actionCreators.AlsoToMain({
-        type: actionTypes.MESSAGE_NOTIFY_VISIBILITY,
-        data: true
-      }));
-    }
-  }, [message, dispatch, tabIsVisible]);
+  }, [dispatch, message]);
   (0,external_React_namespaceObject.useEffect)(() => {
     const handleVisibilityChange = () => {
-      setTabIsVisible(document.visibilityState === "visible");
+      if (document.visibilityState === "visible" && !hasRun) {
+        handleIntersection();
+      }
     };
     document.addEventListener("visibilitychange", handleVisibilityChange);
     return () => {
       document.removeEventListener("visibilitychange", handleVisibilityChange);
     };
-  }, []);
+  }, [handleIntersection, hasRun]);
   const ref = useIntersectionObserver(handleIntersection);
   const handleClose = (0,external_React_namespaceObject.useCallback)(() => {
     const action = {
       type: actionTypes.MESSAGE_TOGGLE_VISIBILITY,
-      data: false 
+      data: true
     };
     if (message.portID) {
       dispatch(actionCreators.OnlyToOneContent(action, message.portID));
     } else {
       dispatch(actionCreators.AlsoToMain(action));
     }
-    dispatch(actionCreators.AlsoToMain({
-      type: actionTypes.MESSAGE_NOTIFY_VISIBILITY,
-      data: false
-    }));
     onDismiss?.();
   }, [dispatch, message, onDismiss]);
   function handleDismiss() {
@@ -11518,7 +11506,7 @@ function MessageWrapper({
       }));
     }
   }
-  if (!message || !hiddenOverride && !message.isVisible) {
+  if (!message || !hiddenOverride && message.isHidden) {
     return null;
   }
 
