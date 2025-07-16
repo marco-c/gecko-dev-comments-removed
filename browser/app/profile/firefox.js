14317af0806e3067b355666f4d2c8790875053d5
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-07-16 01:15:02 +0000

    Bug 1972900 - Newtab smart shortcuts feed integration r=home-newtab-reviewers,nbarrett,mlplyler
    Differential Revision: https://phabricator.services.mozilla.com/D254290
    
    UltraBlame original commit: 0b604031a3bc2056c446f5eba773d753db7784fe

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1930,6 +1930,12 @@ pref("browser.newtabpage.activity-stream.discoverystream.sections.personalizatio
 
 pref("browser.newtabpage.activity-stream.discoverystream.sections.personalization.inferred.user.enabled", true);
 
+
+pref("browser.newtabpage.activity-stream.discoverystream.shortcuts.personalization.region-config", "");
+
+pref("browser.newtabpage.activity-stream.discoverystream.shortcuts.personalization.locale-config", "en-US,en-GB,en-CA");
+
+
 pref("browser.newtabpage.activity-stream.discoverystream.sections.personalization.inferred.model.override", "");
 
 pref("browser.newtabpage.activity-stream.discoverystream.sections.interestPicker.enabled", false);
