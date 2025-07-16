b5cb82d94fe1561cb6f25294e52995a58a12e434
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-07-16 03:31:43 +0000

    Bug 1975628 - Use an existing crash sequence for mips64 CPUs. r=glandium
    mips64 and riscv64 share the same assembly syntax for 64-bit writes.
    
    Differential Revision: https://phabricator.services.mozilla.com/D256089
    
    UltraBlame original commit: 253beecc6229b298ea3c0c9bd272b4f4615c911d

--- mfbt/Assertions.h
+++ mfbt/Assertions.h
@@ -254,7 +254,7 @@ static inline void MOZ_CrashSequence(void* aAddress, intptr_t aLine) {
       "str %1,[%0];\n"  
       :                 
       : "r"(aAddress), "r"(aLine));
-#  elif defined(__riscv) && (__riscv_xlen == 64)
+#  elif (defined(__riscv) && (__riscv_xlen == 64)) || defined(__mips64)
   asm volatile(
       "sd %1,0(%0);\n"  
       :                 
