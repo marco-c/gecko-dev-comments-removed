9c6d71c5c4e5f1a278416c9e63330aa2afd860c6
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-07-16 01:43:43 +0000

    Bug 1973599 [wpt PR 53308] - Make virtualenv.broken_links catch more cases of broken links, a=testonly
    Automatic update from web-platform-tests
    Make virtualenv.broken_links catch more cases of broken links
    
    .Python seems to be gone (never having made it into stdlib venv), but
    we sometimes still end up creating _venv3 directories with
    overly-specific Homebrew/nix/etc. paths, which inevitably break when
    things get cleaned up.
    
    --
    
    wpt-commits: e8ba9f4c454de7eaebe4440e5dee26bc5396308f
    wpt-pr: 53308
    
    UltraBlame original commit: d459b9ed29189a01f8f73a75879adc77d7eff06f

--- testing/web-platform/tests/tools/wpt/virtualenv.py
+++ testing/web-platform/tests/tools/wpt/virtualenv.py
@@ -40,8 +40,22 @@ class Virtualenv:
 
     @property
     def broken_link(self):
-        python_link = os.path.join(self.path, ".Python")
-        return os.path.lexists(python_link) and not os.path.exists(python_link)
+        
+        virtualenv_python_link = os.path.join(self.path, ".Python")
+        if os.path.lexists(virtualenv_python_link) and not os.path.exists(virtualenv_python_link):
+            return True
+
+        
+        if not os.path.exists(self.bin_path):
+            return True
+
+        with os.scandir(self.bin_path) as it:
+            for entry in it:
+                
+                if entry.is_symlink() and not os.path.exists(entry.path):
+                    return True
+
+        return False
 
     def create(self):
         if os.path.exists(self.path):
