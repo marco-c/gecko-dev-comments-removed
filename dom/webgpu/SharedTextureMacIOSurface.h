247d899e938ff7475c63af7692a24048ad5e52c4
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-07-16 01:36:42 +0000

    Bug 1975457 - Rename webgpu::ExternalTexture to SharedTexture. r=webgpu-reviewers,nical
    An ExternalTexture is a texture that is created and owned by gecko,
    but shared with the webgpu implementation. It is used for webgpu
    presentation. While this name describes the class well, it is
    confusing due to webgpu having an entirely unrelated concept of
    GPUExternalTexture.
    
    This patch renames ExternalTexture, and all of its subclasses and
    uses, to SharedTexture. This still describes the purpose accurately,
    and has similarities to gl::SharedSurface which serves a similar
    purpose for our webgl implementation.
    
    Differential Revision: https://phabricator.services.mozilla.com/D255976
    
    UltraBlame original commit: eacde11c30c3a6c78f08d2f34a37b11a0996a308

---
+++ dom/webgpu/SharedTextureMacIOSurface.h
@@ -0,0 +1,55 @@
+
+
+
+
+
+#ifndef GPU_SharedTextureMacIOSurface_H_
+#define GPU_SharedTextureMacIOSurface_H_
+
+#include "mozilla/gfx/FileHandleWrapper.h"
+#include "mozilla/WeakPtr.h"
+#include "mozilla/webgpu/SharedTexture.h"
+
+class MacIOSurface;
+
+namespace mozilla {
+
+namespace webgpu {
+
+class SharedTextureMacIOSurface final : public SharedTexture {
+ public:
+  static UniquePtr<SharedTextureMacIOSurface> Create(
+      WebGPUParent* aParent, const ffi::WGPUDeviceId aDeviceId,
+      const uint32_t aWidth, const uint32_t aHeight,
+      const struct ffi::WGPUTextureFormat aFormat,
+      const ffi::WGPUTextureUsages aUsage);
+
+  SharedTextureMacIOSurface(WebGPUParent* aParent,
+                            const ffi::WGPUDeviceId aDeviceId,
+                            const uint32_t aWidth, const uint32_t aHeight,
+                            const struct ffi::WGPUTextureFormat aFormat,
+                            const ffi::WGPUTextureUsages aUsage,
+                            RefPtr<MacIOSurface>&& aSurface);
+  virtual ~SharedTextureMacIOSurface();
+
+  Maybe<layers::SurfaceDescriptor> ToSurfaceDescriptor() override;
+
+  void GetSnapshot(const ipc::Shmem& aDestShmem,
+                   const gfx::IntSize& aSize) override;
+
+  SharedTextureMacIOSurface* AsSharedTextureMacIOSurface() override {
+    return this;
+  }
+
+  uint32_t GetIOSurfaceId();
+
+ protected:
+  const WeakPtr<WebGPUParent> mParent;
+  const RawId mDeviceId;
+  const RefPtr<MacIOSurface> mSurface;
+};
+
+}  
+}  
+
+#endif  
