c7cf97bc90c1d2ea6738b41f9abe9c1f98fe33a6
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-07-16 02:53:42 +0000

    Revert "Bug 1974983 Screenshot location should fallback to browser.download.folderList by default. r=niklas" for causing bc failures on browser_screenshots_test_downloads.js.
    This reverts commit 4ec645cd199b55e9e8ba0d78f368fafbe3dc1854.
    
    UltraBlame original commit: 1700c287983d6d5272484c322ed78eae095cbe22

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -2765,8 +2765,7 @@ pref("screenshots.browser.component.preventContentEvents", true);
 
 
 
-
-pref("browser.screenshots.folderList", 4);
+pref("browser.screenshots.folderList", 1);
 pref("browser.screenshots.dir", "");
 
 
