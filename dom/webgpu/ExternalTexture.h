d574f233cea0ab90699a9eb9d8b91ffb6278794b
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-07-16 01:36:45 +0000

    Bug 1975457 - Rename webgpu::ExtTex to ExternalTexture. r=webgpu-reviewers,nical
    Now that the previous patch has renamed ExternalTexture to
    SharedTexture, we can rename our (currently just a stub)
    implementation of webgpu's GPUExternalTexture to ExternalTexture.
    
    Differential Revision: https://phabricator.services.mozilla.com/D255977
    
    UltraBlame original commit: 55f4bdcc4bd5029910a7e757cadbf43e85601a23

--- dom/webgpu/ExternalTexture.h
+++ dom/webgpu/ExternalTexture.h
@@ -13,19 +13,19 @@ namespace mozilla::webgpu {
 
 
 
-class ExtTex : public ObjectBase {
+class ExternalTexture : public ObjectBase {
  public:
-  GPU_DECL_CYCLE_COLLECTION(ExtTex)
-  GPU_DECL_JS_WRAP(ExtTex)
+  GPU_DECL_CYCLE_COLLECTION(ExternalTexture)
+  GPU_DECL_JS_WRAP(ExternalTexture)
 
-  explicit ExtTex(nsIGlobalObject* const aGlobal) : mGlobal(aGlobal) {}
+  explicit ExternalTexture(nsIGlobalObject* const aGlobal) : mGlobal(aGlobal) {}
 
   nsIGlobalObject* GetParentObject() const { return mGlobal; }
 
  private:
   nsCOMPtr<nsIGlobalObject> mGlobal;
 
-  ~ExtTex() = default;
+  ~ExternalTexture() = default;
   void Cleanup() {}
 };
 
