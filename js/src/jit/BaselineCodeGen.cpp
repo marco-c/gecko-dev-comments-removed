1d868735e2c3e44257e8e09d705b1a21357fc3b0
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-07-16 03:01:47 +0000

    Bug 1972804: apply code formatting via Lando
    # ignore-this-changeset
    
    UltraBlame original commit: 69b5b87c162d712f2b6ad1e580f0754669aa11fc

--- js/src/jit/BaselineCodeGen.cpp
+++ js/src/jit/BaselineCodeGen.cpp
@@ -6205,7 +6205,8 @@ bool BaselineCompilerCodeGen::emitAfterYieldDebugInstrumentation(Register) {
 }
 
 template <>
-bool BaselineInterpreterCodeGen::emitAfterYieldDebugInstrumentation(Register scratch) {
+bool BaselineInterpreterCodeGen::emitAfterYieldDebugInstrumentation(
+    Register scratch) {
   
   
 
@@ -6309,7 +6310,8 @@ bool BaselineCodeGen<Handler>::emitEnterGeneratorCode(Register script,
   masm.bind(&noBaselineScript);
 
   
-  masm.or32(Imm32(BaselineFrame::RUNNING_IN_INTERPRETER), frame.addressOfFlags());
+  masm.or32(Imm32(BaselineFrame::RUNNING_IN_INTERPRETER),
+            frame.addressOfFlags());
   
 
   
@@ -6685,10 +6687,11 @@ bool BaselineCompilerCodeGen::emit_AfterYield() {
   }
 
   if (handler.realmIndependentJitcode()) {
-    masm.or32(Imm32(BaselineFrame::Flags::REALM_INDEPENDENT), frame.addressOfFlags());
+    masm.or32(Imm32(BaselineFrame::Flags::REALM_INDEPENDENT),
+              frame.addressOfFlags());
   }
 
-  return emitAfterYieldDebugInstrumentation( R0.scratchReg());
+  return emitAfterYieldDebugInstrumentation(R0.scratchReg());
 }
 
 template <>
@@ -6697,7 +6700,7 @@ bool BaselineInterpreterCodeGen::emit_AfterYield() {
     return false;
   }
 
-  return emitAfterYieldDebugInstrumentation( R0.scratchReg());
+  return emitAfterYieldDebugInstrumentation(R0.scratchReg());
 }
 
 template <typename Handler>
