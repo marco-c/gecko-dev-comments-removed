23482d38223b9e32753f57682888a8c8f891a5c2
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-07-16 03:07:40 +0000

    Bug 1976884 - Track blend-mode and backdrop-filter together in the same bitfield. r=dshin
    I'm going to need to add a bit more tracking for 3d transforms
    (see bug 1972694). I'd rather not duplicate the set-up.
    
    No behavior change.
    
    Differential Revision: https://phabricator.services.mozilla.com/D256999
    
    UltraBlame original commit: 0ad16b8f47588f57401ce0617b227067cfa9b7dd

--- layout/painting/nsDisplayList.h
+++ layout/painting/nsDisplayList.h
@@ -277,6 +277,17 @@ class nsDisplayTableItem;
 class RetainedDisplayList;
 
 
+enum class StackingContextBits : uint8_t {
+  
+  
+  
+  ContainsMixBlendMode = 1 << 0,
+  
+  ContainsBackdropFilter = 1 << 1,
+};
+MOZ_MAKE_ENUM_CLASS_BITWISE_OPERATORS(StackingContextBits);
+
+
 
 
 
@@ -1498,20 +1509,29 @@ class nsDisplayListBuilder {
                                     : mWindowOpaqueRegion;
   }
 
-  
-
-
-
-
-  void SetContainsBlendMode(bool aContainsBlendMode) {
-    mContainsBlendMode = aContainsBlendMode;
+  StackingContextBits GetStackingContextBits() const {
+    return mStackingContextBits;
   }
-  bool ContainsBlendMode() const { return mContainsBlendMode; }
-
-  void SetContainsBackdropFilter(bool aContainsBackdropFilter) {
-    mContainsBackdropFilter = aContainsBackdropFilter;
+  void SetStackingContextBits(StackingContextBits aBits) {
+    mStackingContextBits = aBits;
   }
-  bool ContainsBackdropFilter() const { return mContainsBackdropFilter; }
+  void AddStackingContextBits(StackingContextBits aBits) {
+    mStackingContextBits |= aBits;
+  }
+  void ClearStackingContextBits(StackingContextBits aBits) {
+    mStackingContextBits &= ~aBits;
+  }
+  void ClearStackingContextBits() {
+    mStackingContextBits = StackingContextBits(0);
+  }
+  bool ContainsBlendMode() const {
+    return bool(mStackingContextBits &
+                StackingContextBits::ContainsMixBlendMode);
+  }
+  bool ContainsBackdropFilter() const {
+    return bool(mStackingContextBits &
+                StackingContextBits::ContainsBackdropFilter);
+  }
 
   DisplayListClipState& ClipState() { return mClipState; }
   const ActiveScrolledRoot* CurrentActiveScrolledRoot() {
@@ -1902,8 +1922,7 @@ class nsDisplayListBuilder {
 
   uint32_t mNumActiveScrollframesEncountered = 0;
 
-  bool mContainsBlendMode;
-  bool mContainsBackdropFilter;
+  StackingContextBits mStackingContextBits{0};
   bool mIsBuildingScrollbar;
   bool mCurrentScrollbarWillHaveLayer;
   bool mBuildCaret;
