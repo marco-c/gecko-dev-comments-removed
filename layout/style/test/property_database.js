e086518ffc7b86106fa63f572663fb1b283982bd
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-07-28 19:00:11 +0000

    Revert "Bug 1977489 - Hide MozAppearance from CSSStyleProperties r=emilio" for mochitest failures on test_inherit_storage.html
    This reverts commit 0d15c1ad1f2f675a26c381e34d892969d0e29304.
    
    UltraBlame original commit: ad5afefda393d8f5443d42e83b765b60de9d2d81

--- layout/style/test/property_database.js
+++ layout/style/test/property_database.js
@@ -1736,8 +1736,7 @@ var gCSSProperties = {
     invalid_values: [],
   },
   "-moz-appearance": {
-    
-    
+    domProp: "MozAppearance",
     inherited: false,
     type: CSS_TYPE_SHORTHAND_AND_LONGHAND,
     alias_for: "appearance",
