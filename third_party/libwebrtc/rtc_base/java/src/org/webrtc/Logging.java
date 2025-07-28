e18b3d429ede0834dbe84cb07332237d099ce2a1
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-07-28 20:54:52 +0000

    Bug 1971201 - Vendor libwebrtc from 050da3b358
    Upstream commit: https://webrtc.googlesource.com/src/+/050da3b358846ffd0e471ad775c3307f6e6c4b34
        Remove ancient TODOs
    
        Bug: none
        Change-Id: Iaf20a7c49dfafdf0bd187e3a31398494165125d0
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/385920
        Reviewed-by: Harald Alvestrand <htawebrtc.org>
        Commit-Queue: Fredrik Solenberg <solenbergwebrtc.org>
        Cr-Commit-Position: refs/heads/main{#44361}
    
    UltraBlame original commit: 5bd0126166a95a2c12587042ba503cf11575aa7c

--- third_party/libwebrtc/rtc_base/java/src/org/webrtc/Logging.java
+++ third_party/libwebrtc/rtc_base/java/src/org/webrtc/Logging.java
@@ -63,31 +63,6 @@ public class Logging {
   }
 
   
-  @Deprecated
-  public enum TraceLevel {
-    TRACE_NONE(0x0000),
-    TRACE_STATEINFO(0x0001),
-    TRACE_WARNING(0x0002),
-    TRACE_ERROR(0x0004),
-    TRACE_CRITICAL(0x0008),
-    TRACE_APICALL(0x0010),
-    TRACE_DEFAULT(0x00ff),
-    TRACE_MODULECALL(0x0020),
-    TRACE_MEMORY(0x0100),
-    TRACE_TIMER(0x0200),
-    TRACE_STREAM(0x0400),
-    TRACE_DEBUG(0x0800),
-    TRACE_INFO(0x1000),
-    TRACE_TERSEINFO(0x2000),
-    TRACE_ALL(0xffff);
-
-    public final int level;
-    TraceLevel(int level) {
-      this.level = level;
-    }
-  }
-
-  
   public enum Severity { LS_VERBOSE, LS_INFO, LS_WARNING, LS_ERROR, LS_NONE }
 
   public static void enableLogThreads() {
@@ -97,10 +72,6 @@ public class Logging {
   public static void enableLogTimeStamps() {
     nativeEnableLogTimeStamps();
   }
-
-  
-  @Deprecated
-  public static void enableTracing(String path, EnumSet<TraceLevel> levels) {}
 
   
   
