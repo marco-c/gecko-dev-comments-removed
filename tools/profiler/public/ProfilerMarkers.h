d38c693e14ef2c0183b87d0b32823a94385934a8
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-07-28 20:23:51 +0000

    Bug 1978250: Add tests for new simple payload markers. r=mstange,profiler-reviewers
    This also implements doubles and signed ints, as well as making strings
    a little bit more conveniently handled.
    
    Differential Revision: https://phabricator.services.mozilla.com/D257952
    
    UltraBlame original commit: b897f1f83b8bf6d049a23565b136213186014906

--- tools/profiler/public/ProfilerMarkers.h
+++ tools/profiler/public/ProfilerMarkers.h
@@ -264,13 +264,28 @@ using Tracing = mozilla::baseprofiler::markers::Tracing;
     }                                                                         \
   } while (false)
 
+
+namespace mozilla {
+template <>
+constexpr MarkerSchema::InputType
+MarkerSchema::getDefaultInputTypeForType<nsCString>() {
+  return InputType::CString;
+}
+
+template <>
+constexpr MarkerSchema::Format
+MarkerSchema::getDefaultFormatForType<nsCString>() {
+  return Format::SanitizedString;
+}
+}  
+
 namespace geckoprofiler::markers {
 
 
 template <const char* ArgName, typename ArgType>
 struct FieldDescription {
   static constexpr const char* name = ArgName;
-  static constexpr ArgType var = {};
+  using type = ArgType;
 };
 
 
@@ -284,11 +299,13 @@ struct SimplePayloadMarkerTemplate
 
   using MS = mozilla::MarkerSchema;
 
-  static constexpr MS::PayloadField PayloadFields[] = {
+  
+  
+  static constexpr MS::PayloadField PayloadFields[sizeof...(ArgTypes)] = {
       {ArgTypes::name,
-       MS::getDefaultInputTypeForType<decltype(ArgTypes::var)>(),
+       MS::getDefaultInputTypeForType<typename ArgTypes::type>(),
        ArgTypes::name,
-       MS::getDefaultFormatForType<decltype(ArgTypes::var)>()}...};
+       MS::getDefaultFormatForType<typename ArgTypes::type>()}...};
 
   static constexpr const char* TableLabel = ArgTableLabel;
 
@@ -297,7 +314,7 @@ struct SimplePayloadMarkerTemplate
 
   static void StreamJSONMarkerData(
       mozilla::baseprofiler::SpliceableJSONWriter& aWriter,
-      const decltype(ArgTypes::var)&... args) {
+      const typename ArgTypes::type&... args) {
     mozilla::BaseMarkerType<
         SimplePayloadMarkerTemplate<ArgName, ArgTableLabel, ArgTypes...>>::
         StreamJSONMarkerDataImpl(aWriter, args...);
