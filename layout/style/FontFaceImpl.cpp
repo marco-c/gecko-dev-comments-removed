aa68d6d9ea8451950a286beb9f95ff72ec236dc9
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-07-28 20:42:06 +0000

    Bug 1872780 - Remove layout.css.size-adjust.enabled pref r=webidl,emilio
    Differential Revision: https://phabricator.services.mozilla.com/D258800
    
    UltraBlame original commit: f62b0173427d9b1b164870f5052fff78d0cfa16f

--- layout/style/FontFaceImpl.cpp
+++ layout/style/FontFaceImpl.cpp
@@ -489,8 +489,7 @@ bool FontFaceImpl::SetDescriptors(const nsACString& aFamily,
         !setDesc(eCSSFontDesc_DescentOverride, aDescriptors.mDescentOverride) ||
         !setDesc(eCSSFontDesc_LineGapOverride,
                  aDescriptors.mLineGapOverride))) ||
-      (StaticPrefs::layout_css_size_adjust_enabled() &&
-       !setDesc(eCSSFontDesc_SizeAdjust, aDescriptors.mSizeAdjust))) {
+      !setDesc(eCSSFontDesc_SizeAdjust, aDescriptors.mSizeAdjust)) {
     
 
     
