52fe2fc55b3247da1e602b7baac36701b3650752
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-07-28 19:09:21 +0000

    Bug 1977852: Ignore presence of indexed selector in compound when considering collapsing relative dependencies. r=firefox-style-system-reviewers,emilio
    Previously, we didn't pull out indexed selectors from the inner selector of
    `:has(:not(:first-child))` - now we it is like `:has(:first-child:not(*))`.
    On any invalidation, we see that our compound contains `:first-child`, so
    we become unable to consider the invalidation for collapsing with the impllied
    `*` invalidation, effectively resulting in redundant invalidations.
    Because in this case we're considering two different invalidations for the
    same element, we can in fact ignore the presence of the indexed selector
    and allow collapsing.
    
    Differential Revision: https://phabricator.services.mozilla.com/D257848
    
    UltraBlame original commit: 1871fa2b54f186e603b03ff65a9e567cc2465d14

--- servo/components/style/invalidation/element/relative_selector.rs
+++ servo/components/style/invalidation/element/relative_selector.rs
@@ -350,7 +350,11 @@ impl<'a, E: TElement + 'a> Default for ToInvalidate<'a, E> {
     }
 }
 
-fn invalidation_can_collapse(a: &Dependency, b: &Dependency, invalidations_in_subtree: bool) -> bool {
+fn invalidation_can_collapse(
+    a: &Dependency,
+    b: &Dependency,
+    allow_indexed_selectors: bool,
+) -> bool {
     
     
     
@@ -406,9 +410,7 @@ fn invalidation_can_collapse(a: &Dependency, b: &Dependency, invalidations_in_su
             return false;
         }
         let Some(component) = a_component else { return true };
-        
-        
-        if !invalidations_in_subtree && component.has_indexed_selector_in_subject() {
+        if !allow_indexed_selectors && component.has_indexed_selector_in_subject() {
             
             return false;
         }
@@ -436,8 +438,17 @@ where
     ) {
         let in_subtree = element != self.top;
         if let Some(entry) = self.invalidations.iter_mut().find(|entry| {
+            
+            
             let both_in_subtree = in_subtree && entry.element != self.top;
-            invalidation_can_collapse(dependency, entry.dependency, both_in_subtree)
+            
+            
+            let same_element = element == entry.element;
+            invalidation_can_collapse(
+                dependency,
+                entry.dependency,
+                both_in_subtree || same_element,
+            )
         }) {
             entry.update(element, host, dependency)
         } else {
