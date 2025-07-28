9dea49a324473030e90d6fb84c734171dbe10534
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-07-28 19:45:27 +0000

    Bug 1773313 - Extract ELF file identifier from memory during shared library iteration r=mstange,profiler-reviewers
    This commit modifies the dl_iterate_callback to extract ELF file identifier
    directly from PT_NOTE segments in memory instead of always reading from the
    file system. This makes sure that if Firefox is updated in between and shared
    libraries are changed, we still read the correct in-memory library.
    
    If we fail to get the ELF file identifier from PT_NOTE, we fall back to reading
    it from disk again.
    
    I avoided using the `FileID` code as much as possible. But still used
    `ElfClassBuildIDNoteIdentifier` inside because this was going to be nearly
    identical.
    
    Differential Revision: https://phabricator.services.mozilla.com/D257816
    
    UltraBlame original commit: 543a577471306c01348e4ffaf5f823cc6cb2272d

--- mozglue/baseprofiler/core/shared-libraries-linux.cc
+++ mozglue/baseprofiler/core/shared-libraries-linux.cc
@@ -645,16 +645,19 @@ class FileID {
 struct LoadedLibraryInfo {
   LoadedLibraryInfo(const char* aName, unsigned long aBaseAddress,
                     unsigned long aFirstMappingStart,
-                    unsigned long aLastMappingEnd)
+                    unsigned long aLastMappingEnd,
+                    std::optional<std::vector<uint8_t>>&& aElfFileIdentifier)
       : mName(aName),
         mBaseAddress(aBaseAddress),
         mFirstMappingStart(aFirstMappingStart),
-        mLastMappingEnd(aLastMappingEnd) {}
+        mLastMappingEnd(aLastMappingEnd),
+        mElfFileIdentifier(std::move(aElfFileIdentifier)) {}
 
   std::string mName;
   unsigned long mBaseAddress;
   unsigned long mFirstMappingStart;
   unsigned long mLastMappingEnd;
+  std::optional<std::vector<uint8_t>> mElfFileIdentifier;
 };
 
 static std::string IDtoUUIDString(const std::vector<uint8_t>& aIdentifier) {
@@ -672,7 +675,7 @@ static std::string IDtoString(const std::vector<uint8_t>& aIdentifier) {
 
 
 
-static std::optional<std::vector<uint8_t>> getElfFileIdentifier(
+static std::optional<std::vector<uint8_t>> getElfFileIdentifierFromFile(
     const char* bin_name) {
   std::vector<uint8_t> identifier;
   identifier.reserve(kDefaultBuildIdSize);
@@ -705,17 +708,20 @@ static std::string getCodeId(
   return {};
 }
 
-static SharedLibrary SharedLibraryAtPath(const char* path,
-                                         unsigned long libStart,
-                                         unsigned long libEnd,
-                                         unsigned long offset = 0) {
+static SharedLibrary SharedLibraryAtPath(
+    const char* path, unsigned long libStart, unsigned long libEnd,
+    unsigned long offset = 0,
+    const std::optional<std::vector<uint8_t>>& elfFileIdentifier =
+        std::nullopt) {
   std::string pathStr = path;
 
   size_t pos = pathStr.rfind('/');
   std::string nameStr =
       (pos != std::string::npos) ? pathStr.substr(pos + 1) : pathStr;
 
-  const auto identifier = getElfFileIdentifier(path);
+  const auto identifier = elfFileIdentifier
+                              ? elfFileIdentifier
+                              : getElfFileIdentifierFromFile(path);
 
   return SharedLibrary(libStart, libEnd, offset, getBreakpadId(identifier),
                        getCodeId(identifier), nameStr, pathStr, nameStr,
@@ -731,26 +737,42 @@ static int dl_iterate_callback(struct dl_phdr_info* dl_info, size_t size,
   unsigned long baseAddress = dl_info->dlpi_addr;
   unsigned long firstMappingStart = -1;
   unsigned long lastMappingEnd = 0;
+  std::vector<uint8_t> elfFileIdentifier;
 
   for (size_t i = 0; i < dl_info->dlpi_phnum; i++) {
-    if (dl_info->dlpi_phdr[i].p_type != PT_LOAD) {
-      continue;
+    
+    if (dl_info->dlpi_phdr[i].p_type == PT_LOAD) {
+      unsigned long start = dl_info->dlpi_addr + dl_info->dlpi_phdr[i].p_vaddr;
+      unsigned long end = start + dl_info->dlpi_phdr[i].p_memsz;
+      if (start < firstMappingStart) {
+        firstMappingStart = start;
+      }
+      if (end > lastMappingEnd) {
+        lastMappingEnd = end;
+      }
     }
-    unsigned long start = dl_info->dlpi_addr + dl_info->dlpi_phdr[i].p_vaddr;
-    unsigned long end = start + dl_info->dlpi_phdr[i].p_memsz;
-    if (start < firstMappingStart) {
-      firstMappingStart = start;
-    }
-    if (end > lastMappingEnd) {
-      lastMappingEnd = end;
+
+    
+    
+    if (dl_info->dlpi_phdr[i].p_type == PT_NOTE && elfFileIdentifier.empty()) {
+      const void* section_start = reinterpret_cast<const void*>(
+          dl_info->dlpi_addr + dl_info->dlpi_phdr[i].p_vaddr);
+      size_t section_length = dl_info->dlpi_phdr[i].p_memsz;
+      FileID::ElfClassBuildIDNoteIdentifier(section_start, section_length,
+                                            elfFileIdentifier);
     }
   }
 
+  auto optionalElfFileId =
+      elfFileIdentifier.size() > 0
+          ? std::make_optional(std::move(elfFileIdentifier))
+          : std::nullopt;
   
   
   const char* libName = dl_info->dlpi_name ? dl_info->dlpi_name : "";
   libInfoList->push_back(LoadedLibraryInfo(libName, baseAddress,
-                                           firstMappingStart, lastMappingEnd));
+                                           firstMappingStart, lastMappingEnd,
+                                           std::move(optionalElfFileId)));
 
   return 0;
 }
@@ -860,7 +882,8 @@ SharedLibraryInfo SharedLibraryInfo::GetInfoForSelf() {
 
     info.AddSharedLibrary(SharedLibraryAtPath(
         libraryName, libInfo.mFirstMappingStart, libInfo.mLastMappingEnd,
-        libInfo.mFirstMappingStart - libInfo.mBaseAddress));
+        libInfo.mFirstMappingStart - libInfo.mBaseAddress,
+        libInfo.mElfFileIdentifier));
   }
 
   return info;
