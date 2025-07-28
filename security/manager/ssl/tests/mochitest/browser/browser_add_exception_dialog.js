29c523f403a6b8fe9781120b72c8f7399030761e
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-07-28 19:19:45 +0000

    Bug 1977770 - Fix "View…" button in certificate exception dialog. r=simonf,tschuster
    Take two, this time with the right event listener for a XUL button instead of an HTML button. :-(
    
    Differential Revision: https://phabricator.services.mozilla.com/D257650
    
    UltraBlame original commit: a6e587dc0f760cc84fb07fa613514396dd2c0deb

--- security/manager/ssl/tests/mochitest/browser/browser_add_exception_dialog.js
+++ security/manager/ssl/tests/mochitest/browser/browser_add_exception_dialog.js
@@ -24,14 +24,26 @@ function test() {
       Services.obs.removeObserver(onCertUI, "cert-exception-ui-ready");
       ok(win.gCert, "The certificate information should be available now");
 
+      
       let viewButton = win.document.getElementById("viewCertButton");
       let tabPromise = BrowserTestUtils.waitForNewTab(
         gBrowser,
         url => url.startsWith("about:certificate?cert="),
         true
       );
-      viewButton.click();
+      EventUtils.synthesizeMouseAtCenter(viewButton, {}, win);
       BrowserTestUtils.removeTab(await tabPromise);
+
+      if (AppConstants.platform != "macosx") {
+        
+        tabPromise = BrowserTestUtils.waitForNewTab(
+          gBrowser,
+          url => url.startsWith("about:certificate?cert="),
+          true
+        );
+        EventUtils.synthesizeKey("KEY_Enter", {}, win);
+        BrowserTestUtils.removeTab(await tabPromise);
+      }
 
       let dialog = win.document.getElementById("exceptiondialog");
       let confirmButton = dialog.getButton("extra1");
