35ccc1dc4188eadac200f4c65c613739af4d3b37
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-08-01 01:35:07 +0000

    Bug 1974778 - Prevent pinned tabs container from resizing while dragging pinned tab r=kcochrane,tabbrowser-reviewers,dwalker
    Differential Revision: https://phabricator.services.mozilla.com/D259099
    
    UltraBlame original commit: b094cbf4dee0eb85cee0cbb63e37048dae0a103f

--- browser/components/tabbrowser/content/tabs.js
+++ browser/components/tabbrowser/content/tabs.js
@@ -2217,13 +2217,16 @@
       let allTabs = this.ariaFocusableItems;
       let isGrid = this.#isContainerVerticalPinnedGrid(tab);
 
-      if (isGrid) {
+      if (isPinned && this.verticalMode) {
         this.pinnedTabsContainer.setAttribute("dragActive", "");
       }
 
       
       let pinnedRect = window.windowUtils.getBoundsWithoutFlushing(
         this.pinnedTabsContainer.scrollbox
+      );
+      let pinnedContainerRect = window.windowUtils.getBoundsWithoutFlushing(
+        this.pinnedTabsContainer
       );
       let unpinnedRect = window.windowUtils.getBoundsWithoutFlushing(
         this.arrowScrollbox.scrollbox
@@ -2232,6 +2235,9 @@
       if (this.pinnedTabsContainer.firstChild) {
         this.pinnedTabsContainer.scrollbox.style.height =
           pinnedRect.height + "px";
+        
+        this.pinnedTabsContainer.style.minHeight =
+          pinnedContainerRect.height + "px";
         this.pinnedTabsContainer.scrollbox.style.width =
           pinnedRect.width + "px";
       }
@@ -3291,6 +3297,7 @@
       );
       pinnedPeriphery && pinnedTabsContainer.removeChild(pinnedPeriphery);
       pinnedTabsContainer.removeAttribute("dragActive");
+      pinnedTabsContainer.style.minHeight = "";
       draggedTabDocument.defaultView.SidebarController.updatePinnedTabsHeightOnResize();
       pinnedTabsContainer.scrollbox.style.height = "";
       pinnedTabsContainer.scrollbox.style.width = "";
