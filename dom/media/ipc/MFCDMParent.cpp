d8a1d7281cd88ebdeefebee5d027f22a8248eb13
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-08-01 01:20:35 +0000

    Bug 1978434 - part4 : handle DRM_E_TEE_INVALID_HWDRM_STATE properly and trigger close method with correct reason. r=media-playback-reviewers,padenot
    When DRM_E_TEE_INVALID_HWDRM_STATE occurs, it signifies that the current session
    is unusable due to the hardware reset. [1]
    
    To ensure the web application is informed of this error and can take appropriate
    action, such as creating a new session, we must correctly notify the error
    through the MediaKeySession's closed promise. See detailed discussion in [2].
    
    [1] https://w3c.github.io/encrypted-media/#dom-mediakeysessionclosedreason-hardware-context-reset
    [2] https://github.com/w3c/encrypted-media/issues/494
    
    Differential Revision: https://phabricator.services.mozilla.com/D258793
    
    UltraBlame original commit: f0bf25db87e4de494310f19710993b7d40a142f2

--- dom/media/ipc/MFCDMParent.cpp
+++ dom/media/ipc/MFCDMParent.cpp
@@ -20,6 +20,7 @@
 #include "mozilla/DataMutex.h"
 #include "mozilla/EMEUtils.h"
 #include "mozilla/ProfilerMarkers.h"
+#include "mozilla/RandomNum.h"
 #include "mozilla/KeySystemConfig.h"
 #include "mozilla/StaticMutex.h"
 #include "mozilla/StaticPrefs_media.h"
@@ -107,6 +108,19 @@ MOZ_RUNINIT static StaticDataMutex<ComPtr<IUnknown>> sMediaEngineClassFactory(
 
 #define ASSERT_CDM_ACCESS_ON_MANAGER_THREAD() \
   mCDMAccessLock.Target().AssertOnCurrentThread();
+
+
+
+
+static nsString GenerateDummySessionId() {
+  nsString sessionId;
+  sessionId.AppendLiteral(u"DUMMY_");
+  char buf[17];
+  uint64_t randomValue = mozilla::RandomUint64OrDie();
+  SprintfLiteral(buf, "%016" PRIX64, randomValue);
+  sessionId.AppendASCII(buf);
+  return sessionId;
+}
 
 
 
@@ -478,7 +492,7 @@ void MFCDMParent::Destroy() {
   ShutdownCDM();
   mFactory = nullptr;
   for (auto& iter : mSessions) {
-    iter.second->Close();
+    iter.second->Close(dom::MediaKeySessionClosedReason::Closed_by_application);
   }
   mSessions.clear();
   mIPDLSelfRef = nullptr;
@@ -1247,12 +1261,27 @@ mozilla::ipc::IPCResult MFCDMParent::RecvCreateSessionAndGenerateRequest(
     aResolver(NS_ERROR_DOM_MEDIA_CDM_NO_SESSION_ERR);
     return IPC_OK();
   }
-
-  MFCDM_REJECT_IF_FAILED(session->GenerateRequest(aParams.initDataType(),
-                                                  aParams.initData().Elements(),
-                                                  aParams.initData().Length()),
-                         NS_ERROR_DOM_MEDIA_CDM_SESSION_OPERATION_ERR);
   ConnectSessionEvents(session.get());
+
+  HRESULT hr = session->GenerateRequest(aParams.initDataType(),
+                                        aParams.initData().Elements(),
+                                        aParams.initData().Length());
+  if (hr == DRM_E_TEE_INVALID_HWDRM_STATE) {
+    MFCDM_PARENT_LOG(
+        "Failed to generate request due to DRM_E_TEE_INVALID_HWDRM_STATE");
+    mCDMProxy->OnHardwareContextReset();
+    session->Close(dom::MediaKeySessionClosedReason::Hardware_context_reset);
+    aResolver(GenerateDummySessionId());
+    return IPC_OK();
+  }
+
+  if (FAILED(hr)) {
+    MFCDM_PARENT_LOG("Failed to generate request (hr=%lx)!", hr);
+    aResolver(NS_ERROR_DOM_MEDIA_CDM_NO_SESSION_ERR);
+    
+    
+    return IPC_OK();
+  }
 
   
   
@@ -1340,8 +1369,9 @@ mozilla::ipc::IPCResult MFCDMParent::RecvCloseSession(
     PROFILER_MARKER_TEXT("MFCDMParent::RecvCloseSession", MEDIA_PLAYBACK, {},
                          msg);
   }
-  MFCDM_REJECT_IF_FAILED(session->Close(),
-                         NS_ERROR_DOM_MEDIA_CDM_SESSION_OPERATION_ERR);
+  MFCDM_REJECT_IF_FAILED(
+      session->Close(dom::MediaKeySessionClosedReason::Closed_by_application),
+      NS_ERROR_DOM_MEDIA_CDM_SESSION_OPERATION_ERR);
   aResolver(rv);
   return IPC_OK();
 }
