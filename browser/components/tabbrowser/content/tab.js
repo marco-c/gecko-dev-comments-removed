add0129a2b1a7f1a2c9b9f2a44b61bb4b8eeea1a
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-08-01 01:17:48 +0000

    Revert "Bug 1964646 - Fixed tabs being dragged when focusing on the close button r=tabbrowser-reviewers,nsharpley" for causing failures at browser_file_xpi_no_process_switch.js.
    This reverts commit f39f1d3df04ec7f9b256dc43ae8abb2ebe8ada6c.
    
    UltraBlame original commit: 8ac6aa922c49cb9dc93d1608b6d7c13bc2fc157b

--- browser/components/tabbrowser/content/tab.js
+++ browser/components/tabbrowser/content/tab.js
@@ -447,11 +447,6 @@
         this.style.MozUserFocus = "";
       } else if (
         this.mOverCloseButton ||
-        
-        
-        
-        
-        event.composedTarget?.classList.contains("tab-close-button") ||
         gSharedTabWarning.willShowSharedTabWarning(this)
       ) {
         event.stopPropagation();
