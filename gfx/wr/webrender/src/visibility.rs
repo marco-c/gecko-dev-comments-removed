e40439d5f14d9e59199018f01de0ce890fefa974
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-08-04 15:38:14 +0000

    Bug 1977111 - Restrict picture allocations to the snapshot area. r=gfx-reviewers,lsalzman
    Differential Revision: https://phabricator.services.mozilla.com/D258422
    
    UltraBlame original commit: 821fbf12397ce89a8ea7e4bea96e7b5c63f3a58d

--- gfx/wr/webrender/src/visibility.rs
+++ gfx/wr/webrender/src/visibility.rs
@@ -380,6 +380,18 @@ pub fn update_prim_visibility(
         }
     }
 
+    if let Some(snapshot) = &pic.snapshot {
+        if snapshot.detached {
+            
+            
+            
+            let prim_surface_index = frame_state.surface_stack.last().unwrap().1;
+            let surface = &mut frame_state.surfaces[prim_surface_index.0];
+            let clip = snapshot.area.round_out().cast_unit();
+            surface.clipped_local_rect = surface.clipped_local_rect.intersection_unchecked(&clip);
+        }
+    }
+
     if pop_surface {
         frame_state.pop_surface();
     }
