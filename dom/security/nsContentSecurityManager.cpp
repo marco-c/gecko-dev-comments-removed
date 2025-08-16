5cfbe33caa4b0693b2c6bc6c586cdd9150681abe
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-08-16 03:24:44 +0000

    Bug 1970490: Use loading principal (rather than triggering principal) for CORS checks, by default. r=smaug
    This is essentially a backout of bug 1496505, putting its change behind a new
    off-by-default about:config pref[1] for now, in case there are use cases that
    require it.
    
    [1] content.cors.use_triggering_principal
    
    Differential Revision: https://phabricator.services.mozilla.com/D252611
    
    UltraBlame original commit: 476f23adddc70a5ceb4ffed5f8c5d81f13df915a

--- dom/security/nsContentSecurityManager.cpp
+++ dom/security/nsContentSecurityManager.cpp
@@ -16,6 +16,7 @@
 #include "mozilla/Logging.h"
 #include "mozilla/Maybe.h"
 #include "mozilla/Preferences.h"
+#include "mozilla/StaticPrefs_content.h"
 #include "mozilla/StaticPrefs_dom.h"
 #include "mozilla/StaticPrefs_security.h"
 #include "mozilla/dom/BrowserChild.h"
@@ -357,10 +358,17 @@ static nsresult DoCORSChecks(nsIChannel* aChannel, nsILoadInfo* aLoadInfo,
     return NS_OK;
   }
 
-  
-  
-  
-  nsIPrincipal* principal = aLoadInfo->TriggeringPrincipal();
+  nsIPrincipal* principal = aLoadInfo->GetLoadingPrincipal();
+  if (StaticPrefs::content_cors_use_triggering_principal()) {
+    
+    
+    
+    
+    
+    
+    
+    principal = aLoadInfo->TriggeringPrincipal();
+  }
   RefPtr<nsCORSListenerProxy> corsListener = new nsCORSListenerProxy(
       aInAndOutListener, principal,
       aLoadInfo->GetCookiePolicy() == nsILoadInfo::SEC_COOKIES_INCLUDE);
