1a6667a3ea1b187b284a77ebb73e8e69e5b1c74f
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-08-16 02:58:41 +0000

    Bug 1981356 - [devtools] Add STRs section in devtools error bug filer r=devtools-reviewers,nchevobbe
    Also block the devtools-toolbox-crash meta by default.
    
    Differential Revision: https://phabricator.services.mozilla.com/D260694
    
    UltraBlame original commit: 16d0880856f5c1b5f3f2d459ee957b2101030036

--- devtools/client/shared/components/AppErrorBoundary.js
+++ devtools/client/shared/components/AppErrorBoundary.js
@@ -32,7 +32,7 @@ loader.lazyGetter(this, "RELOAD_PAGE_INFO", function () {
 
 
 const bugLink =
-  "https://bugzilla.mozilla.org/enter_bug.cgi?format=__default__&product=DevTools&component=";
+  "https://bugzilla.mozilla.org/enter_bug.cgi?format=__default__&blocked=devtools-toolbox-crash&product=DevTools&component=";
 
 
 
@@ -170,7 +170,12 @@ class AppErrorBoundary extends Component {
   getBugLink() {
     const { componentStack, clientPacket, serverPacket } = this.state.errorInfo;
 
-    let msg = `## Error in ${this.props.panel}: \n${this.state.errorMsg}\n\n`;
+    let msg =
+      "## Steps to reproduce:\n\n" +
+      "If possible, please share specific steps to reproduce the error.\n" +
+      "Otherwise add any additional information useful to investigate the issue.\n\n";
+
+    msg += `## Error in ${this.props.panel}: \n${this.state.errorMsg}\n\n`;
 
     if (componentStack) {
       msg += `## React Component Stack:${componentStack}\n\n`;
