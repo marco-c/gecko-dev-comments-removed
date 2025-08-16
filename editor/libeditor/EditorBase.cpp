ca15f5d1482f5cbff26edf250bce1bdd51c969bc
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-08-16 01:33:53 +0000

    Bug 1980568 - Rename `To()` to `RefOrTo()` of `EditorDOMPointBase` and create new `To()` which always return new instance r=m_kato
    It's a template hell. The method returns a reference of itself if the
    specified type is the self type instead of creating a new instance for
    the performance. Therefore, this gets unexpected result:
    
    ```cpp
    auto newPoint = oldPoint.To();
    newPoint.AdvanceOffset();
    ```
    
    Then, oldPoint is also modified. I found this bug with this code at
    writing a patch:
    ```cpp
    EditorDOMPoint point(content);
    if (anotherPoint.EqualsOrIsBefore(point.NextPoint())) {
      return point.NextPoint();
    }
    ```
    
    Therefore, I don't know whether this causes actual bug in current code.
    
    Differential Revision: https://phabricator.services.mozilla.com/D259545
    
    UltraBlame original commit: fbf32fb7861d75ca9d6a264e54b69b4a51698a24

--- editor/libeditor/EditorBase.cpp
+++ editor/libeditor/EditorBase.cpp
@@ -3528,7 +3528,8 @@ EditorBase::InsertTextIntoTextNodeWithTransaction(
     auto [begin, end] = ComputeInsertedRange(pointToInsert, aStringToInsert);
     if (begin.IsSet() && end.IsSet()) {
       TopLevelEditSubActionDataRef().DidInsertText(
-          *this, begin.To<EditorRawDOMPoint>(), end.To<EditorRawDOMPoint>());
+          *this, begin.RefOrTo<EditorRawDOMPoint>(),
+          end.RefOrTo<EditorRawDOMPoint>());
     }
     if (isIMETransaction) {
       
