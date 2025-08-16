6fd2d32f44b7d29c68ae0922d481e707dc25ddbd
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-08-16 03:01:04 +0000

    Bug 1920227 - Use firefox -xpcshell for mach xpcshell r=ahal
    Differential Revision: https://phabricator.services.mozilla.com/D257559
    
    UltraBlame original commit: 299e0f8bec92a070372b833be5f3fe4e4d4dec0a

--- js/xpconnect/mach_commands.py
+++ js/xpconnect/mach_commands.py
@@ -3,32 +3,30 @@
 
 
 import argparse
-import sys
-from pathlib import Path
 
 from mach.decorators import Command, CommandArgument
 
 
-@Command("xpcshell", category="misc", description="Run the xpcshell binary")
-@CommandArgument(
-    "args", nargs=argparse.REMAINDER, help="Arguments to provide to xpcshell"
+@Command(
+    "xpcshell",
+    category="misc",
+    description="Run the xpcshell binary",
 )
-def xpcshell(command_context, args):
-    dist_bin = Path(command_context.topobjdir, "dist", "bin")
-    browser_dir = dist_bin / "browser"
+@CommandArgument(
+    "--app",
+    help="The path to the Firefox binary (default: the binary in $TOPOBJDIR)",
+    dest="firefox_bin",
+)
+@CommandArgument(
+    "args",
+    nargs=argparse.REMAINDER,
+    help="Arguments to provide to xpcshell",
+)
+def xpcshell(command_context, *, firefox_bin, args):
+    if not firefox_bin:
+        firefox_bin = command_context.get_binary_path("app")
 
-    if sys.platform == "win32":
-        xpcshell = dist_bin / "xpcshell.exe"
-    else:
-        xpcshell = dist_bin / "xpcshell"
-
-    command = [
-        str(xpcshell),
-        "-g",
-        str(dist_bin),
-        "-a",
-        str(browser_dir),
-    ]
+    command = [str(firefox_bin), "-xpcshell"]
 
     
     env = {
