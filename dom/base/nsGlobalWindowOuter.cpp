659d950f10996c4254ab1a8652cd1c1ca3731e30
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-08-16 03:18:02 +0000

    Revert "Bug 1964901 - Call UpdateCommands("focus") when the window goes into the foreground / background. r=masayuki" for causing bc failures at browser_replacewithwindow_commands.js and wpt failures at append-image-using-illegal-map.html
    This reverts commit 2db302bab56bdc6decb0dec8c10081d6673b0fb3.
    
    UltraBlame original commit: 6393ebcde6595a595f7d86d73ed1df022a3b9e9e

--- dom/base/nsGlobalWindowOuter.cpp
+++ dom/base/nsGlobalWindowOuter.cpp
@@ -6275,7 +6275,8 @@ void nsGlobalWindowOuter::UpdateCommands(const nsAString& anAction) {
   
   if (nsIDocShell* docShell = GetDocShell()) {
     if (nsCOMPtr<nsIBrowserChild> child = docShell->GetBrowserChild()) {
-      if (nsCOMPtr<nsPIWindowRoot> root = GetTopWindowRoot()) {
+      nsCOMPtr<nsPIWindowRoot> root = GetTopWindowRoot();
+      if (root) {
         nsContentUtils::AddScriptRunner(
             new ChildCommandDispatcher(root, child, this, anAction));
       }
@@ -6443,12 +6444,6 @@ Location* nsGlobalWindowOuter::GetLocation() {
 void nsGlobalWindowOuter::SetIsBackground(bool aIsBackground) {
   const bool changed = aIsBackground != IsBackground();
   SetIsBackgroundInternal(aIsBackground);
-
-  if (changed) {
-    
-    
-    UpdateCommands(u"focus"_ns);
-  }
 
   nsGlobalWindowInner* inner = GetCurrentInnerWindowInternal(this);
   if (!inner) {
