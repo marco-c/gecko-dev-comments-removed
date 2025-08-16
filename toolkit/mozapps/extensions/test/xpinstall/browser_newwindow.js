82b22f72c3172da0f76a6637f9225c953d88c15f
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-08-16 02:09:09 +0000

    Bug 1980926 - Migrate browser_newwindow.js away from deprecated InstallTrigger API r=extension-reviewers,rpl
    Differential Revision: https://phabricator.services.mozilla.com/D259814
    
    UltraBlame original commit: 5403429250c4d5e644d98c4efaaae64d48a09206

--- toolkit/mozapps/extensions/test/xpinstall/browser_newwindow.js
+++ toolkit/mozapps/extensions/test/xpinstall/browser_newwindow.js
@@ -2,57 +2,86 @@
 
 
 
+"use strict";
 
-let win;
-let popupPromise;
-let newtabPromise;
-const exampleURI = Services.io.newURI("http://example.com");
-async function test() {
-  waitForExplicitFinish(); 
+const { AddonTestUtils } = ChromeUtils.importESModule(
+  "resource://testing-common/AddonTestUtils.sys.mjs"
+);
 
-  
-  setInstallTriggerPrefs();
+AddonTestUtils.initMochitest(this);
 
-  await SpecialPowers.pushPrefEnv({
-    set: [["dom.security.https_first", false]],
-  });
-  Harness.installConfirmCallback = confirm_install;
-  Harness.installEndedCallback = install => {
-    return install.addon.uninstall();
-  };
-  Harness.installsCompletedCallback = finish_test;
-  Harness.finalContentEvent = "InstallComplete";
-  win = await BrowserTestUtils.openNewBrowserWindow();
-  Harness.setup(win);
 
-  PermissionTestUtils.add(exampleURI, "install", Services.perms.ALLOW_ACTION);
 
-  const triggers = encodeURIComponent(
-    JSON.stringify({
-      "Unsigned XPI": {
-        URL: TESTROOT + "amosigned.xpi",
-        IconURL: TESTROOT + "icon.png",
-      },
-    })
-  );
 
-  const url = `${TESTROOT}installtrigger.html?${triggers}`;
-  newtabPromise = BrowserTestUtils.openNewForegroundTab(win.gBrowser, url);
-  popupPromise = BrowserTestUtils.waitForEvent(
-    win.PanelUI.notificationPanel,
-    "popupshown"
-  );
-}
+
+
+let promiseAddonStarted = null;
 
 function confirm_install(panel) {
   is(panel.getAttribute("name"), "XPI Test", "Should have seen the name");
+  promiseAddonStarted = AddonTestUtils.promiseWebExtensionStartup(
+    "amosigned-xpi@tests.mozilla.org"
+  );
   return true;
 }
 
-async function finish_test(count) {
-  is(count, 1, "1 Add-on should have been successfully installed");
+function install_ended(install, addon) {
+  is(
+    addon.__AddonInternal__._install,
+    null,
+    "Expect addon._install to not be set after install is completed"
+  );
+  promiseAddonStarted.then(() => addon.uninstall());
+}
 
-  PermissionTestUtils.remove(exampleURI, "install");
+add_task(async function test_newwindow_leak() {
+  
+  
+  
+  ok(AppConstants.DEBUG, "Test is running on a DEBUG build as expected");
+
+  await SpecialPowers.pushPrefEnv({
+    set: [
+      ["extensions.webapi.testing", true],
+      [PREF_INSTALL_REQUIREBUILTINCERTS, false],
+    ],
+  });
+
+  const deferredInstallCompleted = Promise.withResolvers();
+  Harness.installConfirmCallback = confirm_install;
+  Harness.installStartedCallback = install => {
+    
+    
+    is(
+      install.addon.__AddonInternal__._install,
+      install.__AddonInstallInternal__,
+      "Expect addon._install to be set when install is started"
+    );
+  };
+  Harness.installEndedCallback = install_ended;
+  Harness.installsCompletedCallback = deferredInstallCompleted.resolve;
+  Harness.finalContentEvent = "InstallComplete";
+
+  let win = await BrowserTestUtils.openNewBrowserWindow();
+  Harness.setup(win);
+
+  const triggers = encodeURIComponent(
+    JSON.stringify({
+      url: SECURE_TESTROOT + "amosigned.xpi",
+    })
+  );
+
+  const url = `${SECURE_TESTROOT}mozaddonmanager.html?${triggers}`;
+
+  let newtabPromise = BrowserTestUtils.openNewForegroundTab(win.gBrowser, url);
+  let popupPromise = BrowserTestUtils.waitForEvent(
+    win.PanelUI.notificationPanel,
+    "popupshown"
+  );
+
+  info("Wait for the install to be completed");
+  const count = await deferredInstallCompleted.promise;
+  is(count, 1, "1 Add-on should have been successfully installed");
 
   const results = await SpecialPowers.spawn(
     win.gBrowser.selectedBrowser,
@@ -65,8 +94,8 @@ async function finish_test(count) {
     }
   );
 
-  is(results.return, "true", "installTrigger should have claimed success");
-  is(results.status, "0", "Callback should have seen a success");
+  is(results.return, "true", "mozAddonManager should have claimed success");
+  is(results.status, "STATE_INSTALLED", "Callback should have seen a success");
 
   
   
@@ -82,8 +111,8 @@ async function finish_test(count) {
   
   await newtabPromise;
 
-  
   await BrowserTestUtils.closeWindow(win);
   Harness.finish(win);
-  win = null;
-}
+
+  await SpecialPowers.popPrefEnv();
+});
