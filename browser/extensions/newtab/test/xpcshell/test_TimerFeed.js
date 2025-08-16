ab61b204e6cd56990856a68fc0619126ac036c4e
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-08-16 03:27:57 +0000

    Bug 1980710 - Add toggles for focus timer notifications. r=home-newtab-reviewers,maxx,npypchenko
    Differential Revision: https://phabricator.services.mozilla.com/D260095
    
    UltraBlame original commit: 96a0c5c6484307ffc5c8488bfeaf2159ea580a68

--- browser/extensions/newtab/test/xpcshell/test_TimerFeed.js
+++ browser/extensions/newtab/test/xpcshell/test_TimerFeed.js
@@ -3,16 +3,20 @@
 
 "use strict";
 
+
 ChromeUtils.defineESModuleGetters(this, {
   actionTypes: "resource://newtab/common/Actions.mjs",
   TimerFeed: "resource://newtab/lib/Widgets/TimerFeed.sys.mjs",
   sinon: "resource://testing-common/Sinon.sys.mjs",
 });
 
+
+
+
+
 const PREF_TIMER_ENABLED = "widgets.timer.enabled";
 const PREF_SYSTEM_TIMER_ENABLED = "widgets.system.timer.enabled";
-const PREF_TIMER_SHOW_NOTIFICATIONS =
-  "widgets.focusTimer.showSystemNotifications";
+
 
 add_task(async function test_construction() {
   let feed = new TimerFeed();
@@ -87,43 +91,96 @@ add_task(async function test_isEnabled() {
   Assert.ok(feed.enabled);
 });
 
-add_task(async function test_system_notification_on_timer_end() {
-  const feed = new TimerFeed();
 
-  
-  const TIMER_END_NOTIFICATION_TITLE = "Timer Finished";
-  const TIMER_END_NOTIFICATION_TEXT = "Time's up! Take a break.";
 
-  feed.store = {
-    getState() {
-      return this.state;
-    },
-    dispatch: sinon.spy(),
-    state: {
-      Prefs: {
-        values: {
-          [PREF_TIMER_ENABLED]: true,
-          [PREF_SYSTEM_TIMER_ENABLED]: true,
-          [PREF_TIMER_SHOW_NOTIFICATIONS]: true,
-        },
-      },
-      TimerWidget: {},
-    },
-  };
 
-  feed.showSystemNotification = sinon.spy();
 
-  await feed.onAction({
-    type: actionTypes.WIDGETS_TIMER_END,
-    data: { duration: 1500 },
-  });
 
-  Assert.ok(
-    feed.showSystemNotification.calledOnce,
-    "TimerFeed WIDGETS_TIMER_END event should show notification"
-  );
 
-  const [title, text] = feed.showSystemNotification.firstCall.args;
-  Assert.equal(title, TIMER_END_NOTIFICATION_TITLE, "Title is correct");
-  Assert.equal(text, TIMER_END_NOTIFICATION_TEXT, "Text is correct");
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
