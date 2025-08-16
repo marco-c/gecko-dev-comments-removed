e27de881c28c818b9a2e87f8a36d5e907c7bf906
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-08-16 03:31:02 +0000

    Bug 1975753 - Force site specific zoom when RFPTarget::SiteSpecificZoom is active. r=timhuang,tabbrowser-reviewers,dao
    I wasn't around when RFPTarget::SiteSpecificZoom was implemented, but the current implementation is very confusing.
    
    In Firefox, we have two modes of zoom level. Per tab zoom, and site specific zoom. It is controlled in two (maybe more) places. browser-fullZoom.js and in [CanonicalBrowsingContext.cpp](https://searchfox.org/mozilla-central/rev/ac81a39dfe0663eb40a34c7c36d89e34be29cb20/docshell/base/CanonicalBrowsingContext.cpp#285-289).
    
    Our current implementation disables site specific zoom in `browser-fullZoom.js` by checking the RFP target, but it doesn't modify `CanonicalBrowsingContext.cpp`. So,`CanonicalBrowsingContext` still thinks we are using site specific zoom.
    
    Pre-bug1914149 this method worked because we didn't keep/inherit zoom level across navigations. Post-bug1914149, it no longer works because we keep the zoom level across navigations in `CanonicalBrowsingContext` and let `browser-fullZoom.js` reset to its correct value back.
    
    The issue is caused because `CanonicalBrowsingContext` keeps the previous page's zoom level, but `browser-fullZoom.js` thinks we use tab zoom mode, so it doesn't bother setting the zoom level for the site/page. So, we end up keeping the zoom level.
    
    The solution here I'm suggesting is, doing the opposite of what we are doing in `browser-fullZoom.js`. So, now we should force SiteSpecificZoom with RFP. The reason I'm suggesting this is because within the same site, even acrss tabs, we persist cookies, so fingerprinting isn't much of concern here. We also don't persist zoom levels in private browsing. So, linking normal to PBM isn't a concern either.
    
    So, in summary,
    - If you open the same site, in 100 tabs, all of them will get the same zoom level with this patch (just like default normal Firefox)
    - If you are in PBM, the zoom level is NOT persisted.
    - If you are in normal browsing, the zoom level is persisted, but so are cookies.
    
    Differential Revision: https://phabricator.services.mozilla.com/D257497
    
    UltraBlame original commit: 44e0d730298b917ad2d5ee66aa63d74da14586cc

--- browser/components/tabbrowser/content/browser-fullZoom.js
+++ browser/components/tabbrowser/content/browser-fullZoom.js
@@ -27,7 +27,7 @@ var FullZoom = {
   
   _initialLocations: new WeakMap(),
 
-  get siteSpecific() {
+  get siteSpecificPref() {
     if (this._siteSpecificPref === undefined) {
       this._siteSpecificPref = Services.prefs.getBoolPref(
         "browser.zoom.siteSpecific"
@@ -236,7 +236,7 @@ var FullZoom = {
     
     this._ignorePendingZoomAccesses(browser);
 
-    if (!aURI || (aIsTabSwitch && !this._isSiteSpecific(browser))) {
+    if (!aURI || (aIsTabSwitch && !this.siteSpecificPref)) {
       this._notifyOnLocationChange(browser);
       return;
     }
@@ -530,13 +530,13 @@ var FullZoom = {
     if (
       !aBrowser.mInitialized ||
       aBrowser.isSyntheticDocument ||
-      (!this._isSiteSpecific(aBrowser) && aBrowser.tabHasCustomZoom)
+      (!this.siteSpecificPref && aBrowser.tabHasCustomZoom)
     ) {
       this._executeSoon(aCallback);
       return;
     }
 
-    if (aValue !== undefined && this._isSiteSpecific(aBrowser)) {
+    if (aValue !== undefined && this.siteSpecificPref) {
       ZoomManager.setZoomForBrowser(aBrowser, this._ensureValid(aValue));
       this._ignorePendingZoomAccesses(aBrowser);
       this._executeSoon(aCallback);
@@ -563,11 +563,11 @@ var FullZoom = {
 
 
   _applyZoomToPref: function FullZoom__applyZoomToPref(browser) {
-    if (!this._isSiteSpecific(browser) || browser.isSyntheticDocument) {
+    if (!this.siteSpecificPref || browser.isSyntheticDocument) {
       
       
       
-      browser.tabHasCustomZoom = !this._isSiteSpecific(browser);
+      browser.tabHasCustomZoom = !this.siteSpecificPref;
       return null;
     }
 
@@ -695,23 +695,6 @@ var FullZoom = {
     }
 
     return aValue;
-  },
-
-  
-  
-  
-  _isSiteSpecific(aBrowser) {
-    if (!this.siteSpecific) {
-      return false;
-    }
-    return (
-      !aBrowser?.browsingContext?.topWindowContext.shouldResistFingerprinting ||
-      !ChromeUtils.shouldResistFingerprinting(
-        "SiteSpecificZoom",
-        aBrowser?.browsingContext?.topWindowContext
-          .overriddenFingerprintingSettings
-      )
-    );
   },
 
   
