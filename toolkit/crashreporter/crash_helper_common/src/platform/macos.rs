ea35adffb5dc53a7f733607ea1d758774d5057da
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-08-16 02:25:12 +0000

    Bug 1977514 - Use nix 0.30.x in the crash helper code r=afranchuk
    Differential Revision: https://phabricator.services.mozilla.com/D258708
    
    UltraBlame original commit: 6f5074693f7a70797f59f762d951ba6ec605dbb0

--- toolkit/crashreporter/crash_helper_common/src/platform/macos.rs
+++ toolkit/crashreporter/crash_helper_common/src/platform/macos.rs
@@ -44,16 +44,15 @@ pub(crate) fn unix_socketpair() -> Result<(OwnedFd, OwnedFd)> {
 
 pub(crate) fn set_socket_default_flags(socket: BorrowedFd) -> Result<()> {
     
-    let fd = socket.as_raw_fd();
-    let flags = OFlag::from_bits_retain(fcntl(fd, F_GETFL)?);
-    fcntl(fd, F_SETFL(flags.union(OFlag::O_NONBLOCK)))?;
+    let flags = OFlag::from_bits_retain(fcntl(socket, F_GETFL)?);
+    fcntl(socket, F_SETFL(flags.union(OFlag::O_NONBLOCK)))?;
 
     
     
     
     let res = unsafe {
         setsockopt(
-            fd,
+            socket.as_raw_fd(),
             SOL_SOCKET,
             SO_NOSIGPIPE,
             (&1 as *const i32).cast(),
@@ -69,7 +68,7 @@ pub(crate) fn set_socket_default_flags(socket: BorrowedFd) -> Result<()> {
 }
 
 pub(crate) fn set_socket_cloexec(socket: BorrowedFd) -> Result<()> {
-    fcntl(socket.as_raw_fd(), F_SETFD(FdFlag::FD_CLOEXEC)).map(|_res| ())
+    fcntl(socket, F_SETFD(FdFlag::FD_CLOEXEC)).map(|_res| ())
 }
 
 pub fn server_addr(pid: Pid) -> Result<UnixAddr> {
