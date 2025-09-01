27a42e7f7a0d2d3dc138f88045e92eb4fd03e607
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-09-01 05:39:13 +0000

    Bug 1286945 - Fix some includes. r=jib
    Differential Revision: https://phabricator.services.mozilla.com/D262092
    
    UltraBlame original commit: 71bd1c5d777294615c11010463fc0a49dfabffb2

--- dom/media/webrtc/MediaEngineRemoteVideoSource.h
+++ dom/media/webrtc/MediaEngineRemoteVideoSource.h
@@ -7,37 +7,23 @@
 #ifndef MEDIAENGINE_REMOTE_VIDEO_SOURCE_H_
 #define MEDIAENGINE_REMOTE_VIDEO_SOURCE_H_
 
+#include "CamerasChild.h"
 #include "DOMMediaStream.h"
-#include "mozilla/Mutex.h"
-#include "nsCOMPtr.h"
-#include "nsComponentManagerUtils.h"
-#include "nsDirectoryServiceDefs.h"
-#include "nsThreadUtils.h"
-#include "prcvar.h"
-#include "prthread.h"
-
-
-#include "AudioSegment.h"
 #include "MediaEngineSource.h"
 #include "MediaTrackGraph.h"
-#include "VideoSegment.h"
-#include "VideoUtils.h"
-#include "ipc/IPCMessageUtils.h"
-#include "mozilla/dom/MediaStreamTrackBinding.h"
-
-
-#include "CamerasChild.h"
-#include "NullTransport.h"
-
-
 #include "common_video/include/video_frame_buffer_pool.h"
 #include "modules/video_capture/video_capture_defines.h"
+#include "mozilla/Mutex.h"
+#include "mozilla/dom/MediaStreamTrackBinding.h"
 
 namespace webrtc {
 using CaptureCapability = VideoCaptureCapability;
 }
 
 namespace mozilla {
+namespace dom {
+enum class VideoResizeModeEnum : uint8_t;
+}
 
 
 
