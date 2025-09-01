0f9a85b45cc23a91b1ce1e614fc2dbfacae91ee6
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-09-01 04:45:00 +0000

    Bug 1984126 - Part 6: Update RFP to use time zone override. r=tschuster
    Change RFP to use new time zone Realm creation option added in parts 3-4.
    
    Differential Revision: https://phabricator.services.mozilla.com/D261773
    
    UltraBlame original commit: 4338515033f9d89cc7ba85db8c461b8b1b8bfce3

--- toolkit/components/resistfingerprinting/nsRFPService.cpp
+++ toolkit/components/resistfingerprinting/nsRFPService.cpp
@@ -1063,6 +1063,11 @@ NS_IMETHODIMP nsRFPService::GetSpoofedUserAgentService(bool aDesktopMode,
 nsCString nsRFPService::GetSpoofedJSLocale() { return "en-US"_ns; }
 
 
+nsCString nsRFPService::GetSpoofedJSTimeZone() {
+  return "Atlantic/Reykjavik"_ns;
+}
+
+
 
 
 
