464d254485e52668da1fbd09a84ebfc011f9554b
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-09-01 04:38:50 +0000

    Bug 1978615 [Wayland] Use frame callback to set opaque region of WaylandSurface r=emilio
    The opaque region change takes action even before we have window content painted, so deffer the change
    until there's anything painted in the window and we get frame callback for it.
    
    Differential Revision: https://phabricator.services.mozilla.com/D262062
    
    UltraBlame original commit: 3f8a3d4da7d679a36b7ac09f48bd1b84a6006a8b

--- widget/gtk/WaylandSurface.h
+++ widget/gtk/WaylandSurface.h
@@ -12,7 +12,6 @@
 #include "mozilla/Mutex.h"
 #include "mozilla/Atomics.h"
 #include "WaylandSurfaceLock.h"
-#include "mozilla/GRefPtr.h"
 
 
 
@@ -179,6 +178,7 @@ class WaylandSurface final {
                              const gfx::IntRegion& aRegion);
   void SetOpaqueLocked(const WaylandSurfaceLock& aProofOfLock);
   void ClearOpaqueRegionLocked(const WaylandSurfaceLock& aProofOfLock);
+  void OpaqueCallbackHandler();
 
   bool DisableUserInputLocked(const WaylandSurfaceLock& aProofOfLock);
   void InvalidateRegionLocked(const WaylandSurfaceLock& aProofOfLock,
@@ -409,6 +409,9 @@ class WaylandSurface final {
 
   
   FrameCallback mFrameCallbackHandler;
+
+  wl_region* mPendingOpaqueRegion = nullptr;
+  wl_callback* mOpaqueRegionFrameCallback = nullptr;
 
   
   mozilla::Mutex mMutex{"WaylandSurface"};
