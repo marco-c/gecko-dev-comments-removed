7759ae7db4353dd2c0b69a934cd3e2249aa07c78
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-09-01 05:18:55 +0000

    Bug 1982173 [wpt PR 54219] - WebKit export of https://bugs.webkit.org/show_bug.cgi?id=297161, a=testonly
    Automatic update from web-platform-tests
    WebKit export: JSON & XML MIME types should only contain valid HTTP tokens (#54219)
    
    https://bugs.webkit.org/show_bug.cgi?id=297161
    --
    
    wpt-commits: d2adc80ecd6c500f33302f38009f52c0a6fe7082
    wpt-pr: 54219
    
    UltraBlame original commit: 0ca70668cabb9c0a97a58a95302ea91fa30ff914

--- testing/web-platform/tests/html/semantics/scripting-1/the-script-element/json-module/invalid-content-type.any.js
+++ testing/web-platform/tests/html/semantics/scripting-1/the-script-element/json-module/invalid-content-type.any.js
@@ -13,7 +13,116 @@ const content_types = [
   "text/html;+json",
   "text/html+json+xml",
   "text/json/json",
+
+  
+  "applic\x00ation/vnd.api+json",     
+  "applic\x09ation/vnd.api+json",     
+  "applic\x0Aation/vnd.api+json",     
+  "applic\x0Dation/vnd.api+json",     
+  "applic ation/vnd.api+json",        
+  "applic\x7Fation/vnd.api+json",     
+  "\x01application/vnd.api+json",     
+  "application\x1F/vnd.api+json",     
+
+  
+  "application/vnd\x00.api+json",     
+  "application/vnd\x09.api+json",     
+  "application/vnd\x0A.api+json",     
+  "application/vnd\x0D.api+json",     
+  "application/vnd .api+json",        
+  "application/vnd\x7F.api+json",     
+  "application/\x01vnd.api+json",     
+  "application/vnd.api\x1F+json",     
+
+  
+  "applic\"ation/vnd.api+json",       
+  "applic(ation/vnd.api+json",        
+  "applic)ation/vnd.api+json",        
+  "applic,ation/vnd.api+json",        
+  "applic:ation/vnd.api+json",        
+  "applic;ation/vnd.api+json",        
+  "applic<ation/vnd.api+json",        
+  "applic>ation/vnd.api+json",        
+  "applic=ation/vnd.api+json",        
+  "applic?ation/vnd.api+json",        
+  "applic@ation/vnd.api+json",        
+  "applic[ation/vnd.api+json",        
+  "applic]ation/vnd.api+json",        
+  "applic{ation/vnd.api+json",        
+  "applic}ation/vnd.api+json",        
+
+  
+  "application/vnd\"api\"+json",      
+  "application/vnd(api+json",         
+  "application/vnd)api+json",         
+  "application/vnd,api+json",         
+  "application/vnd:api+json",         
+  "application/vnd;api+json",         
+  "application/vnd<api+json",         
+  "application/vnd>api+json",         
+  "application/vnd=api+json",         
+  "application/vnd?api+json",         
+  "application/vnd@api+json",         
+  "application/vnd[api+json",         
+  "application/vnd]api+json",         
+  "application/vnd{api+json",         
+  "application/vnd}api+json",         
+
+  
+  "aplicación/vnd.api+json",          
+  "申请/vnd.api+json",                 
+  "app™lication/vnd.api+json",        
+  "appli€cation/vnd.api+json",        
+  "🚀application/vnd.api+json",       
+  "applicatioñ/vnd.api+json",         
+
+  
+  "application/vñd.api+json",         
+  "application/vnd.apí+json",         
+  "application/vnd.api™+json",        
+  "application/vnd.api€+json",        
+  "application/vnd.中文+json",         
+  "application/vnd.api🚀+json",       
+  "application/café.api+json",        
+
+  
+  "applic ation/vnd api+json",        
+  "applic\"ation/vnd\"api+json",      
+  "applic(ation/vnd(api+json",        
+  "applic)ation/vnd)api+json",        
+  "applic,ation/vnd,api+json",        
+  "applic=ation/vnd=api+json",        
+  "申请/中文.api+json",                 
+  "app™/vnd€.api+json",               
+  "applic\x00ation/vnd\x00api+json",  
+  "applic;ation/vnd;api+json",        
+  "applic{ation/vnd{api+json",        
+  "applic}ation/vnd}api+json",        
+  "applic[ation/vnd[api+json",        
+  "applic]ation/vnd]api+json",        
+  "applic<ation/vnd<api+json",        
+  "applic>ation/vnd>api+json",        
+
+  
+  "\"application/vnd.api+json",       
+  "application\"/vnd.api+json",       
+  "application /vnd.api+json",        
+  "/vnd.api+json",                    
+  "app\x00lication/vnd.api+json",     
+
+  
+  "application/\"vnd.api+json",       
+  "application/vnd.api\"+json",       
+  "application/ vnd.api+json",        
+  "application/vnd.api +json",        
+  "application/vnd.api+json\"",       
+
+  
+  "\"application\"/\"vnd.api\"+json", 
+  "app(lic)ation/vnd(api)+json",      
+  "application\x00/\x00vnd.api+json",  
 ];
+
 for (const content_type of content_types) {
   promise_test(async test => {
     await promise_rejects_js(test, TypeError,
