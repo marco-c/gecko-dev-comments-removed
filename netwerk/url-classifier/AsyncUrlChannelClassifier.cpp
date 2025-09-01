db186131cf39af6f818e82fdf831d128130d6603
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-09-01 05:25:44 +0000

    Revert "Bug 1978936 - Increase priority of url classifier return thread priority r=acreskey,kershaw" for causing failures at test_beaconFrame.html.
    This reverts commit 1b69284e122a5522bea19e9d82c46431b9cc3d8e.
    
    UltraBlame original commit: 0e8dd623676401ba4cb168ece62af7ce199d5bc1

--- netwerk/url-classifier/AsyncUrlChannelClassifier.cpp
+++ netwerk/url-classifier/AsyncUrlChannelClassifier.cpp
@@ -5,7 +5,6 @@
 
 
 #include "Classifier.h"
-#include "HttpBaseChannel.h"
 #include "mozilla/Components.h"
 #include "mozilla/ErrorNames.h"
 #include "mozilla/net/AsyncUrlChannelClassifier.h"
@@ -912,43 +911,19 @@ nsresult AsyncUrlChannelClassifier::CheckChannel(
     return NS_ERROR_FAILURE;
   }
 
-  
-  
-  EventQueuePriority eventPriority = EventQueuePriority::Normal;
-  if (nsCOMPtr<HttpBaseChannel> baseChannel = do_QueryInterface(aChannel)) {
-    uint32_t classOfServiceFlags = 0;
-    baseChannel->GetClassFlags(&classOfServiceFlags);
-    if (classOfServiceFlags &
-        (nsIClassOfService::Leader | nsIClassOfService::UrgentStart |
-         nsIClassOfService::Unblocked)) {
-      eventPriority = EventQueuePriority::MediumHigh;
-    }
-  }
-  if (nsCOMPtr<nsISupportsPriority> supportsPriority =
-          do_QueryInterface(aChannel)) {
-    int32_t priority = nsISupportsPriority::PRIORITY_NORMAL;
-    supportsPriority->GetPriority(&priority);
-    
-    if (priority <= nsISupportsPriority::PRIORITY_HIGH) {
-      eventPriority = EventQueuePriority::MediumHigh;
-    }
-  }
-
   nsCOMPtr<nsIRunnable> r = NS_NewRunnableFunction(
       "AsyncUrlChannelClassifier::CheckChannel",
-      [task, workerClassifier, eventPriority]() -> void {
+      [task, workerClassifier]() -> void {
         MOZ_ASSERT(!NS_IsMainThread());
         task->DoLookup(workerClassifier);
 
-        NS_DispatchToMainThreadQueue(
-            NS_NewRunnableFunction(
-                "AsyncUrlChannelClassifier::CheckChannel - return",
-                [task]() -> void { task->CompleteClassification(); }),
-            eventPriority);
+        nsCOMPtr<nsIRunnable> r = NS_NewRunnableFunction(
+            "AsyncUrlChannelClassifier::CheckChannel - return",
+            [task]() -> void { task->CompleteClassification(); });
+
+        NS_DispatchToMainThread(r);
       });
 
-  
-  
   return nsUrlClassifierDBService::BackgroundThread()->Dispatch(
       r, NS_DISPATCH_NORMAL);
 }
