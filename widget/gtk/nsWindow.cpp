2b44813f3b07637aa8bd3f56fcaf6237dd359a0b
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-09-01 02:51:29 +0000

    Bug 1983750 [Wayland] Use drag failed for Wayland D&D workaround on Nightly only r=emilio
    Differential Revision: https://phabricator.services.mozilla.com/D261520
    
    UltraBlame original commit: 675ad00eee15a8d1dcfe8293baaf083cb3db89e9

--- widget/gtk/nsWindow.cpp
+++ widget/gtk/nsWindow.cpp
@@ -7518,7 +7518,14 @@ MOZ_CAN_RUN_SCRIPT static void WaylandDragWorkaround(nsWindow* aWindow,
   NS_WARNING(
       "Quit unfinished Wayland Drag and Drop operation. Buggy Wayland "
       "compositor?");
+#if defined(NIGHTLY_BUILD)
+  
+  
+  
+  currentDragSession->EndDragSession(false, 0);
+#else
   currentDragSession->EndDragSession(true, 0);
+#endif
 }
 
 static nsWindow* get_window_for_gtk_widget(GtkWidget* widget) {
