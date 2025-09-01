9543f4881c9c75d56c82f009f870c737ef717d60
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-09-01 04:55:25 +0000

    Bug 1985159: Update nodes with role='grouping' to the correct role='group' r=Jamie
    Differential Revision: https://phabricator.services.mozilla.com/D262504
    
    UltraBlame original commit: 49a143528aebbf8cdacbf8dc7e0f49bcbc620bcf

--- accessible/tests/browser/mac/browser_rootgroup.js
+++ accessible/tests/browser/mac/browser_rootgroup.js
@@ -8,7 +8,7 @@
 
 
 addAccessibleTask(
-  `<div role="grouping" id="group"><p>hello</p><p>world</p></div>`,
+  `<div role="group" id="group"><p>hello</p><p>world</p></div>`,
   async (browser, accDoc) => {
     let doc = accDoc.nativeInterface.QueryInterface(
       Ci.nsIAccessibleMacInterface
