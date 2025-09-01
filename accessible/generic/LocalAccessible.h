d0b19329698ebaa694ceeb76a6fd90d72095afe3
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-09-01 05:42:20 +0000

    Bug 1982212 - Use RelatedAccIterator for aria-activedescendant instead of QuerySelector. r=Jamie,perftest-reviewers,mozperftest-reviewers,sparky
    Caching the active descendant "relation" allows us to not do slow
    queries with selectors.
    
    With the added perftest I see a local improvement in A11Y_TotalTime from
    852ms to 44ms.
    
    Differential Revision: https://phabricator.services.mozilla.com/D262616
    
    UltraBlame original commit: 91e83bec38eac0575b9615da21588c0dafb44acc

--- accessible/generic/LocalAccessible.h
+++ accessible/generic/LocalAccessible.h
@@ -602,7 +602,7 @@ class LocalAccessible : public nsISupports, public Accessible {
 
 
 
-  bool IsActiveDescendantId(LocalAccessible** aWidget = nullptr) const;
+  bool IsActiveDescendant(LocalAccessible** aWidget = nullptr) const;
 
   
 
