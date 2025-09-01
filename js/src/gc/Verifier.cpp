009af6a2923016d101a94097bd052d085484774f
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-09-01 02:43:17 +0000

    Revert "Bug 1965748 - Do not abort incremental validation after clearing mark bits r=jonco" for causing build bustages bug1965748.js.
    This reverts commit 75515179ca7cadb4dd2ef1062077b6e054636672.
    
    UltraBlame original commit: 193c2a68f496947d318cfed5ea5189d13ada3c51

--- js/src/gc/Verifier.cpp
+++ js/src/gc/Verifier.cpp
@@ -519,16 +519,6 @@ void js::gc::MarkingValidator::nonIncrementalMark(AutoGCSession& session) {
 
   MOZ_ASSERT(!gcmarker->isWeakMarking());
 
-#  ifdef DEBUG
-  
-  
-  
-  
-  if (gc->testMarkQueueRemaining() > 0) {
-    return;
-  }
-#  endif
-
   
   MOZ_ASSERT(gc->nursery().isEmpty());
 
@@ -580,14 +570,22 @@ void js::gc::MarkingValidator::nonIncrementalMark(AutoGCSession& session) {
       MOZ_ASSERT(r.front().key()->asTenured().zone() == zone);
       if (!savedEphemeronEdges.putNew(r.front().key(),
                                       std::move(r.front().value()))) {
-        
-        
         oomUnsafe.crash("saving weak keys table for validator");
       }
     }
 
     zone->gcEphemeronEdges().clearAndCompact();
   }
+
+#  ifdef DEBUG
+  
+  
+  
+  
+  if (gc->testMarkQueueRemaining() > 0) {
+    return;
+  }
+#  endif
 
   
 
