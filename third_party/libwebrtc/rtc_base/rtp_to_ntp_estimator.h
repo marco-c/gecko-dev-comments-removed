d683fbab7537ccdf137f65f9e69c2e82f18620ed
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-09-01 03:36:50 +0000

    Bug 1980479 - Vendor libwebrtc from 09d832b1be
    Upstream commit: https://webrtc.googlesource.com/src/+/09d832b1be7539e782d1205377126b502accf42b
        Move RtpToNtpEstimator to rtc_base/
    
        Bug: webrtc:42228262
        Change-Id: I7814f13009f38b5546e68ec6753c5437baa770e2
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/391580
        Commit-Queue: Fredrik Solenberg <solenbergwebrtc.org>
        Reviewed-by: Harald Alvestrand <htawebrtc.org>
        Cr-Commit-Position: refs/heads/main{#44652}
    
    UltraBlame original commit: 00d02992071883b3e0b81b7795a998440d144823

--- third_party/libwebrtc/rtc_base/rtp_to_ntp_estimator.h
+++ third_party/libwebrtc/rtc_base/rtp_to_ntp_estimator.h
@@ -8,15 +8,13 @@
 
 
 
-#ifndef SYSTEM_WRAPPERS_INCLUDE_RTP_TO_NTP_ESTIMATOR_H_
-#define SYSTEM_WRAPPERS_INCLUDE_RTP_TO_NTP_ESTIMATOR_H_
+#ifndef RTC_BASE_RTP_TO_NTP_ESTIMATOR_H_
+#define RTC_BASE_RTP_TO_NTP_ESTIMATOR_H_
 
-#include <stdint.h>
-
+#include <cstdint>
 #include <list>
 #include <optional>
 
-#include "rtc_base/checks.h"
 #include "rtc_base/numerics/sequence_number_unwrapper.h"
 #include "system_wrappers/include/ntp_time.h"
 
