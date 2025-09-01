0794c4bc21499dc752fb00af2a95592e665f86e7
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-09-01 05:14:27 +0000

    Bug 1960433: Enable trial inlining of baseline-interpreted scripts r=jandem
    Differential Revision: https://phabricator.services.mozilla.com/D251073
    
    UltraBlame original commit: fddd1ffd8f40e5c45c81573364be4d900a4a39d3

--- js/src/jit/BaselineCodeGen.cpp
+++ js/src/jit/BaselineCodeGen.cpp
@@ -420,6 +420,7 @@ void BaselineCompilerHandler::maybeDisableIon() {
   if (analysis_.isInliningDisabled()) {
     script()->setUninlineable();
   }
+  script()->jitScript()->setRanBytecodeAnalysis();
 }
 
 
@@ -1361,9 +1362,21 @@ void BaselineInterpreterCodeGen::emitInitFrameFields(Register nonFunctionEnv) {
   masm.storePtr(scratch1, frame.addressOfInterpreterScript());
 
   
+  Label inlined, haveICScript;
+  masm.branchTest32(Assembler::NonZero, frame.addressOfDescriptor(),
+                    Imm32(FrameDescriptor::HasInlinedICScript), &inlined);
   masm.loadJitScript(scratch1, scratch2);
   masm.computeEffectiveAddress(Address(scratch2, JitScript::offsetOfICScript()),
                                scratch2);
+  masm.jump(&haveICScript);
+  masm.bind(&inlined);
+  masm.loadPtr(Address(FramePointer, 0), scratch2);
+  masm.loadPtr(
+      Address(scratch2, BaselineStubFrameLayout::InlinedICScriptOffsetFromFP),
+      scratch2);
+  masm.bind(&haveICScript);
+
+  
   masm.storePtr(scratch2, frame.addressOfICScript());
   masm.computeEffectiveAddress(Address(scratch2, ICScript::offsetOfICEntries()),
                                scratch2);
@@ -1739,6 +1752,11 @@ bool BaselineCompilerCodeGen::emitWarmUpCounterIncrement() {
 
 template <>
 bool BaselineInterpreterCodeGen::emitWarmUpCounterIncrement() {
+  
+  if (!JitOptions.baselineJit) {
+    return true;
+  }
+
   Register scriptReg = R2.scratchReg();
   Register countReg = R0.scratchReg();
 
