c351b5767fd89da669459d62c8d89a8171726e38
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-09-01 03:37:38 +0000

    Bug 1980479 - Vendor libwebrtc from cc46fb91c5
    Upstream commit: https://webrtc.googlesource.com/src/+/cc46fb91c5f8bd4cba637f6ebfaa5f278efc4458
        Use std::numbers constants
    
        Part of modernizing to C++20.
    
        Bug: None
        No-IWYU: Other cleanup
        Change-Id: I2c53789d845486436f37c887b216e7d502c915ac
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/392104
        Commit-Queue: Harald Alvestrand <htawebrtc.org>
        Reviewed-by: Evan Shrubsole <eshrwebrtc.org>
        Cr-Commit-Position: refs/heads/main{#44662}
    
    UltraBlame original commit: 3d5c4e26841d7e437206baf39f8da86aca77f6ef

--- third_party/libwebrtc/modules/audio_processing/aec3/comfort_noise_generator.cc
+++ third_party/libwebrtc/modules/audio_processing/aec3/comfort_noise_generator.cc
@@ -21,6 +21,7 @@
 #include <cmath>
 #include <cstdint>
 #include <functional>
+#include <numbers>
 #include <numeric>
 
 #include "common_audio/signal_processing/include/signal_processing_library.h"
@@ -31,6 +32,8 @@ namespace webrtc {
 
 namespace {
 
+using std::numbers::sqrt2_v;
+
 
 float GetNoiseFloorFactor(float noise_floor_dbfs) {
   
@@ -39,14 +42,16 @@ float GetNoiseFloorFactor(float noise_floor_dbfs) {
 }
 
 
+
 constexpr float kSqrt2Sin[32] = {
     +0.0000000f, +0.2758994f, +0.5411961f, +0.7856950f, +1.0000000f,
-    +1.1758756f, +1.3065630f, +1.3870398f, +1.4142136f, +1.3870398f,
+    +1.1758756f, +1.3065630f, +1.3870398f, +sqrt2_v<float>, +1.3870398f,
     +1.3065630f, +1.1758756f, +1.0000000f, +0.7856950f, +0.5411961f,
     +0.2758994f, +0.0000000f, -0.2758994f, -0.5411961f, -0.7856950f,
-    -1.0000000f, -1.1758756f, -1.3065630f, -1.3870398f, -1.4142136f,
+    -1.0000000f, -1.1758756f, -1.3065630f, -1.3870398f, -sqrt2_v<float>,
     -1.3870398f, -1.3065630f, -1.1758756f, -1.0000000f, -0.7856950f,
     -0.5411961f, -0.2758994f};
+
 
 void GenerateComfortNoise(Aec3Optimization optimization,
                           const std::array<float, kFftLengthBy2Plus1>& N2,
