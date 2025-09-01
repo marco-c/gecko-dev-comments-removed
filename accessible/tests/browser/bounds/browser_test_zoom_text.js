a1515044f30ae20de15b198e77b5d67af6240352
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-09-01 03:55:20 +0000

    Bug 1979968 - P2: Introduce TextLeafPoint::InsertionPointBounds. r=Jamie
    As mentioned in the bug this method does not return bounds for special insertion points.
    
    The bounds are probably correct 90% of the time, the exceptions are transition points in embedded bidi segments,
    among other unforseen situations. These edge cases are things we can probably live with for the time being.
    
    This addition also has a few changes:
    1. HyperTextAccessible::TextBounds now returns bounds for identical start and end offsets.
    2. TextLeafRange::Bounds now returns the insertion point for a collapsed range.
    3. The browser caret rect test now uses the insertion point rect to compare against the caret rect.
    
    Differential Revision: https://phabricator.services.mozilla.com/D259134
    
    UltraBlame original commit: 615582fb0d81e1050f19770d05d9818b74bc2424

--- accessible/tests/browser/bounds/browser_test_zoom_text.js
+++ accessible/tests/browser/bounds/browser_test_zoom_text.js
@@ -20,14 +20,6 @@ async function runTests(browser, accDoc) {
       [x, y, width, height],
       COORDTYPE_SCREEN_RELATIVE
     );
-    
-    testTextBounds(
-      hyperTextNode,
-      0,
-      0,
-      [0, 0, 0, 0],
-      COORDTYPE_SCREEN_RELATIVE
-    );
   }
 
   async function testEmptyInputNode(id) {
