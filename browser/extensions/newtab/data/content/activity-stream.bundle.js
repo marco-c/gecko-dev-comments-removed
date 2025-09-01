dce9ddd1011c830b893c665a28a4b58cd27eb7a5
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-09-01 03:55:47 +0000

    Bug 1981720 - Add limit to number of list items an individual list can have r=home-newtab-reviewers,nbarrett
    Differential Revision: https://phabricator.services.mozilla.com/D261829
    
    UltraBlame original commit: af13a6124dba57af515b7af47a1cdf3c740f35d3

--- browser/extensions/newtab/data/content/activity-stream.bundle.js
+++ browser/extensions/newtab/data/content/activity-stream.bundle.js
@@ -12055,6 +12055,7 @@ const USER_ACTION_TYPES = {
   TASK_COMPLETE: "task_complete"
 };
 const PREF_WIDGETS_LISTS_MAX_LISTS = "widgets.lists.maxLists";
+const PREF_WIDGETS_LISTS_MAX_LISTITEMS = "widgets.lists.maxListItems";
 function Lists({
   dispatch
 }) {
@@ -12457,6 +12458,14 @@ function Lists({
   
   const maxListsCount = Math.max(1, prefs[PREF_WIDGETS_LISTS_MAX_LISTS]);
   const isAtMaxListsLimit = currentListsCount >= maxListsCount;
+
+  
+  
+  const currentSelectedListItemsCount = selectedList?.tasks.length + selectedList?.completed.length;
+
+  
+  const maxListItemsCount = Math.max(1, prefs[PREF_WIDGETS_LISTS_MAX_LISTITEMS]);
+  const isAtMaxListItemsLimit = currentSelectedListItemsCount >= maxListItemsCount;
   return external_React_default().createElement("article", {
     className: "lists",
     ref: el => {
@@ -12512,7 +12521,7 @@ function Lists({
   }))), external_React_default().createElement("div", {
     className: "add-task-container"
   }, external_React_default().createElement("span", {
-    className: "icon icon-add"
+    className: `icon icon-add ${isAtMaxListItemsLimit ? "icon-disabled" : ""}`
   }), external_React_default().createElement("input", {
     ref: inputRef,
     onBlur: () => saveTask(),
@@ -12522,7 +12531,8 @@ function Lists({
     className: "add-task-input",
     onKeyDown: handleKeyDown,
     type: "text",
-    maxLength: 100
+    maxLength: 100,
+    disabled: isAtMaxListItemsLimit
   })), external_React_default().createElement("div", {
     className: "task-list-wrapper"
   }, external_React_default().createElement("moz-reorderable-list", {
