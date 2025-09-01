c6a1ccde6c533dcb89a7d6256a2031781d808b21
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-09-01 03:25:07 +0000

    Bug 1980479 - Vendor libwebrtc from b8404180d8
    Upstream commit: https://webrtc.googlesource.com/src/+/b8404180d8136c9fa19f32acdf7a4737505a819c
        Add SDP munging detection for transceiver direction
    
        Changing direction of transceiver is unexpected in most cases.
    
        Bug: webrtc:40567530
        Change-Id: I7b8c4132ac04bf20649ce6152d4289de23a7a60c
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/387180
        Reviewed-by: Harald Alvestrand <htawebrtc.org>
        Reviewed-by: Evan Shrubsole <eshrwebrtc.org>
        Commit-Queue: Philipp Hancke <phanckemeta.com>
        Reviewed-by: Philipp Hancke <phanckemeta.com>
        Cr-Commit-Position: refs/heads/main{#44503}
    
    UltraBlame original commit: 69355137f7f3235053de8f35d0b24c482eda4345

--- third_party/libwebrtc/api/uma_metrics.h
+++ third_party/libwebrtc/api/uma_metrics.h
@@ -195,6 +195,7 @@ enum SdpMungingType {
   kPayloadTypes = 26,
   kSsrcs = 27,
   kIceOptionsRenomination = 28,
+  kDirection = 29,
   
   kRtpHeaderExtensionRemoved = 40,
   kRtpHeaderExtensionAdded = 41,
