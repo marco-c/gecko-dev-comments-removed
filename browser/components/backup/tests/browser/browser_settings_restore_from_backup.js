b1cad8c2b5c494349e1cd4eecd8468d198ace1e0
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-09-08 14:45:18 +0000

    Bug 1983916 - Add backup file finder api to the BackupService. r=mconley
    Differential Revision: https://phabricator.services.mozilla.com/D262046
    
    UltraBlame original commit: 3e69b3fba5c1ebf27c535f3acd3fc177b121b641

--- browser/components/backup/tests/browser/browser_settings_restore_from_backup.js
+++ browser/components/backup/tests/browser/browser_settings_restore_from_backup.js
@@ -3,9 +3,26 @@
 
 "use strict";
 
+let TEST_PROFILE_PATH;
+
 add_setup(async () => {
   MockFilePicker.init(window.browsingContext);
-  registerCleanupFunction(() => {
+  TEST_PROFILE_PATH = await IOUtils.createUniqueDirectory(
+    PathUtils.tempDir,
+    "testBackup"
+  );
+
+  await SpecialPowers.pushPrefEnv({
+    set: [["browser.backup.location", TEST_PROFILE_PATH]],
+  });
+
+  
+  
+  
+  let bs = BackupService.get();
+  bs.resetLastBackupInternalState();
+
+  registerCleanupFunction(async () => {
     MockFilePicker.cleanup();
   });
 });
@@ -21,9 +38,10 @@ add_task(async function test_restore_from_backup() {
       .resolves();
 
     const mockBackupFilePath = await IOUtils.createUniqueFile(
-      PathUtils.tempDir,
+      TEST_PROFILE_PATH,
       "backup.html"
     );
+
     const mockBackupFile = Cc["@mozilla.org/file/local;1"].createInstance(
       Ci.nsIFile
     );
@@ -138,6 +156,15 @@ add_task(async function test_restore_in_progress() {
     let restoreFromBackup = settings.restoreFromBackupEl;
 
     Assert.ok(restoreFromBackup, "restore-from-backup should be found");
+
+    Assert.equal(
+      restoreFromBackup.filePicker.value,
+      "",
+      "File picker has no value assigned automatically"
+    );
+
+    
+    
     Assert.ok(
       restoreFromBackup.confirmButtonEl.disabled,
       "Confirm button should be disabled."
@@ -217,6 +244,77 @@ add_task(async function test_restore_in_progress() {
     Assert.ok(
       !settings.restoreFromBackupDialogEl.open,
       "Restore dialog should now be closed."
+    );
+
+    sandbox.restore();
+  });
+});
+
+
+
+
+add_task(async function test_finding_a_valid_backup() {
+  await BrowserTestUtils.withNewTab("about:preferences", async browser => {
+    let sandbox = sinon.createSandbox();
+    let bs = BackupService.get();
+
+    let { archivePath } = await bs.createBackup();
+
+    Assert.stringContains(
+      archivePath,
+      TEST_PROFILE_PATH,
+      "archive is in our test dir"
+    );
+
+    let settings = browser.contentDocument.querySelector("backup-settings");
+
+    
+    
+    bs.resetLastBackupInternalState();
+
+    await settings.updateComplete;
+
+    registerCleanupFunction(async () => {
+      
+      await IOUtils.remove(TEST_PROFILE_PATH, { recursive: true });
+    });
+
+    settings.restoreFromBackupButtonEl.click();
+    await settings.updateComplete;
+
+    let restoreFromBackup = settings.restoreFromBackupEl;
+    Assert.ok(restoreFromBackup, "restore-from-backup should be found");
+
+    let infoPromise = BrowserTestUtils.waitForEvent(
+      window,
+      "getBackupFileInfo"
+    );
+    let infoEvent = await infoPromise;
+
+    
+    Assert.equal(
+      infoEvent.detail.backupFile,
+      archivePath,
+      "Component asked for info for the detected archive"
+    );
+
+    await restoreFromBackup.updateComplete;
+
+    Assert.ok(
+      settings.restoreFromBackupDialogEl.open,
+      "Restore dialog should be open."
+    );
+
+    Assert.equal(
+      restoreFromBackup.backupFileToRestore,
+      archivePath,
+      "backupFileToRestore was updated to the detected archive path"
+    );
+
+    Assert.equal(
+      restoreFromBackup.filePicker.value,
+      archivePath,
+      "Text input reflects the detected archive path"
     );
 
     sandbox.restore();
