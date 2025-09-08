fadb8bc78a14edca7b882ff1716fc6ab2e496f69
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-09-08 14:53:04 +0000

    Bug 1985891 - Support dynamic pagesize in jemalloc for Android x86_64 r=glandium
    As Android begins to support 16kB page size on ARM64 hardware, there is also
    support for 16 kB pages in the x86_64 emulator images. This extends the support
    for dynamic pagesize in mozjemalloc to cover non-ARM64 Android targets.
    
    Differential Revision: https://phabricator.services.mozilla.com/D263026
    
    UltraBlame original commit: 66fd92f7c0938d79598c20047ef46e3358095f87

--- memory/build/Globals.h
+++ memory/build/Globals.h
@@ -41,9 +41,9 @@
 
 
 #ifndef MALLOC_RUNTIME_CONFIG
-#  if !defined(__ia64__) && !defined(__sparc__) && !defined(__mips__) &&       \
-      !defined(__aarch64__) && !defined(__powerpc__) && !defined(XP_MACOSX) && \
-      !defined(__loongarch__)
+#  if !defined(XP_MACOSX) && !defined(ANDROID) && !defined(__ia64__) &&     \
+      !defined(__sparc__) && !defined(__mips__) && !defined(__aarch64__) && \
+      !defined(__powerpc__) && !defined(__loongarch__)
 #    define MALLOC_STATIC_PAGESIZE 1
 #  endif
 #endif
