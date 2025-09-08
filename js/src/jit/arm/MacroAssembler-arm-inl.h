b39c959245ba5313dcfad1d2982dd2452edc8717
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-09-08 15:09:13 +0000

    Bug 1984984 - Part 1: Use conditional moves for AMR32. r=iain
    Also merge `minMaxPtr` with `minMax32` to avoid code duplication and
    handle the case when `rhs == dest` (not currently used, but doesn't hurt to
    have this covered.)
    
    Differential Revision: https://phabricator.services.mozilla.com/D262359
    
    UltraBlame original commit: 711ae3395824302241683b9309e9e56a9238730d

--- js/src/jit/arm/MacroAssembler-arm-inl.h
+++ js/src/jit/arm/MacroAssembler-arm-inl.h
@@ -663,19 +663,19 @@ void MacroAssembler::max32(Register lhs, Imm32 rhs, Register dest) {
 }
 
 void MacroAssembler::minPtr(Register lhs, Register rhs, Register dest) {
-  minMaxPtr(lhs, rhs, dest,  false);
+  minMax32(lhs, rhs, dest,  false);
 }
 
 void MacroAssembler::minPtr(Register lhs, ImmWord rhs, Register dest) {
-  minMaxPtr(lhs, rhs, dest,  false);
+  minMax32(lhs, Imm32(rhs.value), dest,  false);
 }
 
 void MacroAssembler::maxPtr(Register lhs, Register rhs, Register dest) {
-  minMaxPtr(lhs, rhs, dest,  true);
+  minMax32(lhs, rhs, dest,  true);
 }
 
 void MacroAssembler::maxPtr(Register lhs, ImmWord rhs, Register dest) {
-  minMaxPtr(lhs, rhs, dest,  true);
+  minMax32(lhs, Imm32(rhs.value), dest,  true);
 }
 
 void MacroAssembler::minFloat32(FloatRegister other, FloatRegister srcDest,
