89135bcc4c51b4e263e895a7f3fd33b3b86800d5
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-09-08 14:44:16 +0000

    Revert "Bug 1986062 - Send context_id deletion requests over OHTTP. r=nanj" for causing xpc failures test_quicksuggest_mdn.js.
    This reverts commit 8fe8c80cce3abc11ad9926f94658a101bc620aed.
    
    UltraBlame original commit: 73e955ac55f86ce0f164b767109a2dc86511436c

--- browser/modules/test/unit/test_ContextId_RustBackend.js
+++ browser/modules/test/unit/test_ContextId_RustBackend.js
@@ -7,14 +7,6 @@ const { _ContextId } = ChromeUtils.importESModule(
   "moz-src:///browser/modules/ContextId.sys.mjs"
 );
 
-const { ObliviousHTTP } = ChromeUtils.importESModule(
-  "resource://gre/modules/ObliviousHTTP.sys.mjs"
-);
-
-const { sinon } = ChromeUtils.importESModule(
-  "resource://testing-common/Sinon.sys.mjs"
-);
-
 const CONTEXT_ID_PREF = "browser.contextual-services.contextId";
 const CONTEXT_ID_TIMESTAMP_PREF =
   "browser.contextual-services.contextId.timestamp-in-seconds";
@@ -24,10 +16,6 @@ const UUID_REGEX =
   /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i;
 const TEST_CONTEXT_ID = "decafbad-0cd1-0cd2-0cd3-decafbad1000";
 const TEST_CONTEXT_ID_WITH_BRACES = "{" + TEST_CONTEXT_ID + "}";
-const UNIFIED_ADS_ENDPOINT = Services.prefs.getCharPref(
-  "browser.newtabpage.activity-stream.unifiedAds.endpoint",
-  ""
-);
 
 do_get_profile();
 
@@ -56,45 +44,13 @@ function waitForPersist(instance) {
 
 
 
-
 function waitForRotated(rotatedFromContextId, taskFn) {
-  let sandbox = sinon.createSandbox();
-  sandbox.stub(ObliviousHTTP, "getOHTTPConfig").resolves({});
-
-  let { promise, resolve } = Promise.withResolvers();
-
-  sandbox
-    .stub(ObliviousHTTP, "ohttpRequest")
-    .callsFake((_url, _config, endpoint, options) => {
-      Assert.equal(
-        endpoint,
-        `${UNIFIED_ADS_ENDPOINT}v1/delete_user`,
-        "Sent to the MARS endpoint"
-      );
-      Assert.equal(options.method, "DELETE", "Sent using DELETE");
-      Assert.deepEqual(
-        JSON.parse(options.body),
-        {
-          context_id: rotatedFromContextId,
-        },
-        "Sent the old context_id"
-      );
-
-      resolve();
-      return Promise.resolve({
-        status: 200,
-        json: async () => [],
-      });
-    });
-
-  return GleanPings.contextIdDeletionRequest.testSubmission(async () => {
+  return GleanPings.contextIdDeletionRequest.testSubmission(() => {
     Assert.equal(
       Glean.contextualServices.contextId.testGetValue(),
       rotatedFromContextId,
       "Sent the right context ID to be deleted."
     );
-    await promise;
-    sandbox.restore();
   }, taskFn);
 }
 
