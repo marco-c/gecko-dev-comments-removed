05c9f229a82688b11e0f1441adb20d434e0944c4
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-09-08 14:44:43 +0000

    Bug 1928723 - Factor out callback return handling, r=markh
    Create separate class to handle callback returns.  I want to re-use
    these for sync callback methods. This means a flatter class hierarchy --
    the async handler classes now inherit from AsyncCallbackMethodHandlerBase
    directly.
    
    Separated out the async/fire-and-forget cases in the template code.  I
    think this makes things a bit easier to read.
    
    This is just a refactor, everything should work the same as before.
    
    Differential Revision: https://phabricator.services.mozilla.com/D258609
    
    UltraBlame original commit: 351e97f2186bed04c067a8a4611fda7051df75cd

--- toolkit/components/uniffi-bindgen-gecko-js/src/pipeline/nodes.rs
+++ toolkit/components/uniffi-bindgen-gecko-js/src/pipeline/nodes.rs
@@ -27,8 +27,8 @@ pub struct CppScaffolding {
     pub ffi_definitions: CombinedItems<FfiDefinition>,
     pub scaffolding_calls: CombinedItems<ScaffoldingCall>,
     pub pointer_types: CombinedItems<PointerType>,
+    pub callback_return_handler_classes: CombinedItems<CallbackReturnHandlerClass>,
     pub callback_interfaces: CombinedItems<CppCallbackInterface>,
-    pub async_callback_method_handler_bases: CombinedItems<AsyncCallbackMethodHandlerBase>,
 }
 
 
@@ -109,9 +109,9 @@ pub struct CppCallbackInterfaceMethod {
     
     pub fn_name: String,
     pub kind: CallbackMethodKind,
-    pub base_class_name: String,
+    pub return_handler_class_name: String,
     
-    pub handler_class_name: String,
+    pub async_handler_class_name: String,
     pub ffi_func: FfiFunctionType,
     pub arguments: Vec<FfiValueArgument>,
     pub return_ty: Option<FfiValueReturnType>,
@@ -131,9 +131,15 @@ pub enum CallbackMethodKind {
 
 #[derive(Debug, Clone, Node)]
 pub struct CppCallbackInterfaceMethodAsyncData {
-    pub callback_handler_base_class: String,
     pub complete_callback_type_name: String,
     pub result_type_name: String,
+}
+
+#[derive(Debug, Clone, Node)]
+pub struct CallbackReturnHandlerClass {
+    pub name: String,
+    pub return_ty: Option<FfiValueReturnType>,
+    pub return_type_name: String,
 }
 
 
