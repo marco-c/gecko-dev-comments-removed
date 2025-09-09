bda3f571fc171bfae3dfdb080f551e995e84a3b9
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-09-09 22:09:57 +0000

    Revert "Bug 1986058: apply code formatting via Lando"
    This reverts commit 789a2de4b2372812628fee85c8380c385470e87a.
    
    Revert "Bug 1986058 - part6 : add a test to test unsupported MKV codecs. r=media-playback-reviewers,padenot"
    
    This reverts commit bdd724c07d71e4c04006e7b9d5c03d93febbbc96.
    
    Revert "Bug 1986058 - part5 : add mkv playback test cases. r=media-playback-reviewers,padenot"
    
    This reverts commit b7a923e3d8816ea034974246350b8edae6089fd6.
    
    Revert "Bug 1986058 - part4 : report codec types on Glean. r=media-playback-reviewers,padenot"
    
    This reverts commit 4ec32d2f32ede331f807805870bafd2dddd00870.
    
    Revert "Bug 1986058 - part3 : add AVC support in Matroska. r=media-playback-reviewers,padenot"
    
    This reverts commit b337bd3420aa0527503c13acccf1b0ec799595b8.
    
    Revert "Bug 1986058 - part2 : add AAC support in Matroska. r=media-playback-reviewers,padenot"
    
    This reverts commit 52c87ea69aa623c369cd3c755345460f57cb1fa6.
    
    Revert "Bug 1986058 - part1 : add plumbings to support Matroska r=media-playback-reviewers,padenot"
    
    This reverts commit 94554289b04b1b1c6991b2f7bd68b606a0bccd5e.
    
    UltraBlame original commit: fd046d187894a40ba9ab596ac7d005c4049dabf3

--- dom/media/MediaData.h
+++ dom/media/MediaData.h
@@ -703,7 +703,7 @@ class MediaRawData final : public MediaData {
            mAlphaBuffer.ComputedSizeOfExcludingThis();
   }
   
-  operator Span<const uint8_t>() const { return Span{Data(), Size()}; }
+  operator Span<const uint8_t>() { return Span{Data(), Size()}; }
 
   const CryptoSample& mCrypto;
   RefPtr<MediaByteBuffer> mExtraData;
