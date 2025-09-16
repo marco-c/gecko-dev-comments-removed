4e58b347b06ea5646367a9919ecf2193735c8f11
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-09-16 01:22:15 +0000

    No Bug, mozilla-central repo-update HSTS HPKP remote-settings ct-logs - r=release-managers,dmeehan
    Differential Revision: https://phabricator.services.mozilla.com/D264771
    
    UltraBlame original commit: ba3b777f65e65d15507d30568de9e2f155aba0de

--- security/ct/CTKnownLogs.h
+++ security/ct/CTKnownLogs.h
@@ -14,7 +14,7 @@
 
 #include <stddef.h>
 
-static const PRTime kCTExpirationTime = INT64_C(1763379445000000);
+static const PRTime kCTExpirationTime = INT64_C(1763982159000000);
 
 namespace mozilla::ct {
 
