1dea4cf4f08ba391770666b2999987818567fd89
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-10-01 03:54:44 +0000

    Bug 1990729 - Don't #include flat_set.h when compiling libwebrtc as C++20. r=mjf,webrtc-reviewers
    This change allows libwebrtc to be compiled as C++17 or C++20, but doesn't switch to compiling as C++20 yet.
    
    After we do compile libwebrtc as C++20, we can remove third_party/libwebrtc/moz-patch-stack/s0122.patch, but in the meantime we still need it for C++17.
    
    Differential Revision: https://phabricator.services.mozilla.com/D266114
    
    UltraBlame original commit: aadccad67497469c79a9239725e26e0867e20277

--- third_party/libwebrtc/rtc_base/containers/flat_set.h
+++ third_party/libwebrtc/rtc_base/containers/flat_set.h
@@ -17,7 +17,10 @@
 #include <vector>
 
 #include "rtc_base/containers/flat_tree.h"  
-#include "rtc_base/containers/identity.h"  
+
+#if __cplusplus < 202002
+#  include "rtc_base/containers/identity.h"  
+#endif
 
 namespace webrtc {
 
