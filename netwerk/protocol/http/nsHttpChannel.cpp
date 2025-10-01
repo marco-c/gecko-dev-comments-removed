814df3a95d506ed0822dd07e238af3526b69784c
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-10-01 02:41:56 +0000

    Bug 1988782 - Revert 1984469 and remove the wrong assertion, r=necko-reviewers,valentin
    In nsHttpChannel::ContinueDoAuthRetry we have historically always set the NS_HTTP_STICKY_CONNECTION flag. That means my change in bug 1984469 was incorrect—we should continue to set NS_HTTP_STICKY_CONNECTION on the auth-retry channel.
    
    Also, removing the assertion is fine, and it’s acceptable for TakeHttpConnection() to return nullptr: nsHttpConnectionMgr will still find the existing h2/h3 connection in the entry and dispatch the transaction to it.
    
    Differential Revision: https://phabricator.services.mozilla.com/D265572
    
    UltraBlame original commit: fbf7c03b10fe5e56fc438354ad78c36a50e7f0fc

--- netwerk/protocol/http/nsHttpChannel.cpp
+++ netwerk/protocol/http/nsHttpChannel.cpp
@@ -3747,8 +3747,9 @@ nsresult nsHttpChannel::RedirectToNewChannelForAuthRetry() {
     if (mTransaction->Http3Disabled()) {
       httpChannelImpl->mCaps |= NS_HTTP_DISALLOW_HTTP3;
     }
-    httpChannelImpl->mCaps |= NS_HTTP_STICKY_CONNECTION;
   }
+  
+  httpChannelImpl->mCaps |= NS_HTTP_STICKY_CONNECTION;
 
   if (LoadAuthConnectionRestartable()) {
     httpChannelImpl->mCaps |= NS_HTTP_CONNECTION_RESTARTABLE;
