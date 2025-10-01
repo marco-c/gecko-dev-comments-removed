2b2563354789de73201d3b75a30d0b8c0b69a45b
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-10-01 03:58:42 +0000

    Bug 1962089 - Ensure connection is removed from acrive conns, r=necko-reviewers,jesup
    Instead of playing whack-a-mole, a better approach is to add a WeakPtr in connection that tracks its ConnectionEntry.
    When the connection is added to ActiveConns, set the owner entry. This guarantees the connection is removed from ActiveConns in nsHttpConnectionMgr::OnMsgReclaimConnection.
    
    Differential Revision: https://phabricator.services.mozilla.com/D266674
    
    UltraBlame original commit: c7174d383c136aa85d3e5050224b11074d906043

--- netwerk/protocol/http/HttpConnectionBase.h
+++ netwerk/protocol/http/HttpConnectionBase.h
@@ -18,6 +18,7 @@
 #include "HttpTrafficAnalyzer.h"
 
 #include "mozilla/net/DNS.h"
+#include "mozilla/WeakPtr.h"
 #include "nsIAsyncInputStream.h"
 #include "nsIAsyncOutputStream.h"
 #include "nsIInterfaceRequestor.h"
@@ -29,6 +30,7 @@ class nsITLSSocketControl;
 namespace mozilla {
 namespace net {
 
+class ConnectionEntry;
 class nsHttpHandler;
 class ASpdySession;
 class WebTransportSessionBase;
@@ -179,11 +181,17 @@ class HttpConnectionBase : public nsSupportsWeakReference {
                                       bool aIsExtendedCONNECT = false) = 0;
   virtual void SetInTunnel() {};
 
+  void SetOwner(ConnectionEntry* aEntry);
+  ConnectionEntry* OwnerEntry() const;
+
  protected:
   
   uint32_t mTransactionCaps{0};
 
   RefPtr<nsHttpConnectionInfo> mConnInfo;
+  
+  
+  WeakPtr<ConnectionEntry> mOwnerEntry;
 
   bool mExperienced{false};
   
