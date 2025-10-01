1ffcd2641ee4c0607fc4b1b527e156238a4d4217
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-10-01 02:49:16 +0000

    Bug 1989441 - Remove stale legacy pingsender references. r=gsvelto
    Bug 1962160 removed the legacy telemetry ping, and with it the
    use of the legacy pingsender, but some references remained.
    
    Differential Revision: https://phabricator.services.mozilla.com/D265358
    
    UltraBlame original commit: 9005f95b30e52cc5ca7978c401eb422dcc72cb03

--- toolkit/crashreporter/client/app/src/main.rs
+++ toolkit/crashreporter/client/app/src/main.rs
@@ -161,10 +161,6 @@ fn report_main() {
     
     let mut mock = mock::builder();
     mock.set(
-        Command::mock("work_dir/pingsender"),
-        Box::new(|_| Ok(crate::std::process::success_output())),
-    )
-    .set(
         Command::mock("curl"),
         Box::new(|_| {
             let mut output = crate::std::process::success_output();
