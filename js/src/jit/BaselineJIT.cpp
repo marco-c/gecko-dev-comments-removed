25b69c3d0dd08604ea954b568a6634a0ccb73ad5
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-10-01 03:41:21 +0000

    Bug 1989107: apply code formatting via Lando
    # ignore-this-changeset
    
    UltraBlame original commit: daab717b33f882db0f07f08f85a8b325d19a698c

--- js/src/jit/BaselineJIT.cpp
+++ js/src/jit/BaselineJIT.cpp
@@ -131,7 +131,7 @@ static JitExecStatus EnterBaseline(JSContext* cx, EnterJitData& data) {
   
   MOZ_ASSERT_IF(data.constructing,
                 data.thisv().isObject() ||
-                data.thisv().isMagic(JS_UNINITIALIZED_LEXICAL));
+                    data.thisv().isMagic(JS_UNINITIALIZED_LEXICAL));
 
   data.result.setInt32(data.numActualArgs);
   {
