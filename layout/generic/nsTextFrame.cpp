fd34d740cd784ab2f0998ad82acf1af4106ed591
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-10-01 03:27:00 +0000

    Bug 1990319 - Prevent gfxSkipCharsIterator from traversing past the start of the textframe in LastNonMarkCharClass(). r=jfkthame,layout-jp-market-reviewers,layout-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D265923
    
    UltraBlame original commit: 1b92b1da827dad5bb0cb993d4c0bb3147140cf91

--- layout/generic/nsTextFrame.cpp
+++ layout/generic/nsTextFrame.cpp
@@ -3887,12 +3887,15 @@ static gfxFloat ComputeTabWidthAppUnits(const nsIFrame* aFrame) {
 
 
 
+
+
+
 static Maybe<TextAutospace::CharClass> LastNonMarkCharClass(
-    gfxSkipCharsIterator& aIter, const gfxTextRun* aTextRun,
-    const CharacterDataBuffer& aBuffer) {
-  while (aIter.GetOriginalOffset() > 0) {
+    gfxSkipCharsIterator& aIter, int32_t aContentOffsetAtFrameStart,
+    const gfxTextRun* aTextRun, const CharacterDataBuffer& aBuffer) {
+  while (aIter.GetOriginalOffset() > aContentOffsetAtFrameStart) {
     aIter.AdvanceOriginal(-1);
-    FindClusterStart(aTextRun, 0, &aIter);
+    FindClusterStart(aTextRun, aContentOffsetAtFrameStart, &aIter);
     const char32_t ch = aBuffer.ScalarValueAt(aIter.GetOriginalOffset());
     auto cls = TextAutospace::GetCharClass(ch);
     if (cls != TextAutospace::CharClass::CombiningMark) {
@@ -3912,7 +3915,8 @@ static Maybe<TextAutospace::CharClass> LastNonMarkCharClassInFrame(
   gfxSkipCharsIterator iter = aFrame->EnsureTextRun(nsTextFrame::eInflated);
   iter.SetOriginalOffset(aFrame->GetContentEnd());
   Maybe<CharClass> prevClass =
-      LastNonMarkCharClass(iter, aFrame->GetTextRun(nsTextFrame::eInflated),
+      LastNonMarkCharClass(iter, aFrame->GetContentOffset(),
+                           aFrame->GetTextRun(nsTextFrame::eInflated),
                            aFrame->CharacterDataBuffer());
   if (prevClass) {
     return prevClass;
@@ -4066,7 +4070,8 @@ void nsTextFrame::PropertyProvider::GetSpacingInternal(Range aRange,
       Maybe<CharClass> prevClass;
       if (aRange.start > 0) {
         gfxSkipCharsIterator iter = start;
-        prevClass = LastNonMarkCharClass(iter, mTextRun, mCharacterDataBuffer);
+        prevClass = LastNonMarkCharClass(iter, mFrame->GetContentOffset(),
+                                         mTextRun, mCharacterDataBuffer);
       }
       
       
