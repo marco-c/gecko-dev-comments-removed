68c561bdc7717918b60abe2408079ae98eb61bad
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-10-01 02:43:57 +0000

    Bug 1835615 - Closed other outer connections gracefully, r=necko-reviewers,valentin
    Differential Revision: https://phabricator.services.mozilla.com/D264406
    
    UltraBlame original commit: 8a18511abacd1982fc47cd461d987d1985555ff1

--- netwerk/protocol/http/nsHttpConnectionMgr.cpp
+++ netwerk/protocol/http/nsHttpConnectionMgr.cpp
@@ -3763,6 +3763,8 @@ void nsHttpConnectionMgr::MoveToWildCardConnEntry(
        wcEnt->DnsAndConnectSocketsLength(), wcEnt->PendingQueueLength()));
 
   ent->MoveConnection(proxyConn, wcEnt);
+  
+  wcEnt->MakeAllDontReuseExcept(proxyConn);
 }
 
 bool nsHttpConnectionMgr::RemoveTransFromConnEntry(nsHttpTransaction* aTrans,
