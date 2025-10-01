fb9d5d540267421fe21f1e1aa9c550e96b7c384e
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-10-01 03:14:30 +0000

    Bug 1989222 [wpt PR 54915] - Add WPTs to cover all valid Opus frame durations, a=testonly
    Automatic update from web-platform-tests
    Add WPTs to cover all valid Opus frame durations
    
    According to RFC6716 section 2.1.4, "[Opus] can also combine multiple
    frames into packets". This means that packets such as 7.5ms (3 * 2.5ms
    packets) are considered valid frame durations. This CL adds valid frame
    durations that are not of the standard durations.
    
    Bug: 40243924
    Change-Id: Iea0e6ebcebfda68ed4fdd8cc144494d51532d9a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6952086
    Reviewed-by: Thomas Guilbert <tguilbertchromium.org>
    Auto-Submit: Syed AbuTalib <lowkeygoogle.com>
    Commit-Queue: Syed AbuTalib <lowkeygoogle.com>
    Cr-Commit-Position: refs/heads/main{#1516965}
    
    --
    
    wpt-commits: 7342619635eccfa4aa6e87c025aac2614674dca6
    wpt-pr: 54915
    
    UltraBlame original commit: 4f9052f0eea5c93fa44b563585984099a0f1cbfa

--- testing/web-platform/tests/webcodecs/audio-encoder-config.https.any.js
+++ testing/web-platform/tests/webcodecs/audio-encoder-config.https.any.js
@@ -317,6 +317,22 @@ const validConfigs = [
     numberOfChannels: 2,
     opus: {},  
   },
+  {
+    codec: 'opus',
+    sampleRate: 48000,
+    numberOfChannels: 2,
+    opus: {
+      frameDuration: 7500, 
+    }
+  },
+  {
+    codec: 'opus',
+    sampleRate: 48000,
+    numberOfChannels: 2,
+    opus: {
+      frameDuration: 120000,
+    }
+  },
 ];
 
 validConfigs.forEach(config => {
