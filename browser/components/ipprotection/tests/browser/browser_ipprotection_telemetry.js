199bcbe1d0db45a5a7d1c726feb6b1ca4bfd8e9f
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-10-01 03:27:53 +0000

    Bug 1990043 - Set inited=true earlier since a couple method return early if !inited r=rking
    Differential Revision: https://phabricator.services.mozilla.com/D265716
    
    UltraBlame original commit: 128433303b13df784046bff1e9ed6d061cc3128d

--- browser/components/ipprotection/tests/browser/browser_ipprotection_telemetry.js
+++ browser/components/ipprotection/tests/browser/browser_ipprotection_telemetry.js
@@ -182,7 +182,7 @@ add_task(async function toggle_off_on_shutdown() {
   Services.fog.testResetFOG();
   
   cleanupService();
-  lazy.IPProtectionService.init();
+  await lazy.IPProtectionService.init();
   let widget = document.getElementById(IPProtectionWidget.WIDGET_ID);
   Assert.ok(
     BrowserTestUtils.isVisible(widget),
