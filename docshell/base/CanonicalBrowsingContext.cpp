a8943053b47ab9cc9b77d4fb5c8f9fd11753f1cd
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-10-01 03:24:11 +0000

    Bug 1988927 - Part 1: Refactor same-origin check out of WalkContiguousEntriesInOrder callback. r=farre
    Differential Revision: https://phabricator.services.mozilla.com/D265047
    
    UltraBlame original commit: acb30bb661d36e600126cb4f92aff4d8e746578e

--- docshell/base/CanonicalBrowsingContext.cpp
+++ docshell/base/CanonicalBrowsingContext.cpp
@@ -677,38 +677,26 @@ CanonicalBrowsingContext::CreateLoadingSessionHistoryEntryForLoad(
     bool sameOrigin =
         NS_SUCCEEDED(nsContentUtils::GetSecurityManager()->CheckSameOriginURI(
             targetURI, uri, false, false));
-    if (entry->isInList() || (mActiveEntry && mActiveEntry->isInList())) {
-      nsCOMPtr<nsIURI> sameOriginURI = entry->GetURI();
+    if (entry->isInList() ||
+        (mActiveEntry && mActiveEntry->isInList() && sameOrigin)) {
       nsSHistory::WalkContiguousEntriesInOrder(
           entry->isInList() ? entry : mActiveEntry,
-          [sameOriginURI, activeEntry = mActiveEntry,
+          [activeEntry = mActiveEntry,
            entries = &loadingInfo->mContiguousEntries,
            navigationType = *navigationType](auto* aEntry) {
-            if (nsCOMPtr<SessionHistoryEntry> entry = do_QueryObject(aEntry)) {
-              nsCOMPtr candidateURI = entry->GetURI();
-              if (NS_FAILED(
-                      nsContentUtils::GetSecurityManager()->CheckSameOriginURI(
-                          candidateURI, sameOriginURI, false, false))) {
-                return false;
-              }
-
-              if (navigationType == NavigationType::Replace &&
-                  entry == activeEntry) {
-                
-                
-                return false;
-              }
-              entries->AppendElement(entry->Info());
-              if (navigationType == NavigationType::Push &&
-                  entry == activeEntry) {
-                
-                
-                return false;
-              }
-
-              return true;
+            nsCOMPtr<SessionHistoryEntry> entry = do_QueryObject(aEntry);
+            MOZ_ASSERT(entry);
+            if (navigationType == NavigationType::Replace &&
+                entry == activeEntry) {
+              
+              
+              return false;
             }
-            return false;
+            entries->AppendElement(entry->Info());
+            
+            
+            return !(navigationType == NavigationType::Push &&
+                     entry == activeEntry);
           });
     }
 
