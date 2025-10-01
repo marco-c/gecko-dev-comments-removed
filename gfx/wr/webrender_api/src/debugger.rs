c2928645c8c7c85470e646cf37e39080c77afc44
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-10-01 02:55:52 +0000

    Bug 1989493 - Add basic composite overlay view to WR debugger r=gfx-reviewers,lsalzman
    Differential Revision: https://phabricator.services.mozilla.com/D265396
    
    UltraBlame original commit: 9f374e28d4780e46186c16d08f90012e794f3ca3

--- gfx/wr/webrender_api/src/debugger.rs
+++ gfx/wr/webrender_api/src/debugger.rs
@@ -2,7 +2,7 @@
 
 
 
-use crate::DebugFlags;
+use crate::{DebugFlags, PictureRect, DeviceRect};
 
 
 
@@ -41,4 +41,18 @@ pub enum DebuggerMessage {
     SetDebugFlags(SetDebugFlagsMessage),
     InitProfileCounters(InitProfileCountersMessage),
     UpdateProfileCounters(UpdateProfileCountersMessage),
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct CompositorDebugTile {
+    pub local_rect: PictureRect,
+    pub device_rect: DeviceRect,
+    pub clip_rect: DeviceRect,
+    pub z_id: i32,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct CompositorDebugInfo {
+    pub enabled_z_layers: u64,
+    pub tiles: Vec<CompositorDebugTile>,
 }
