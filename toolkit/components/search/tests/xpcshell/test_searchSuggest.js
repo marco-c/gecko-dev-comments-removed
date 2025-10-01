f0f041ca6fcc5d414c450579701e8d93a5f56bdc
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-10-01 01:40:06 +0000

    Bug 1987911 - Change search.suggestions_latency key to be the search engine identifier rather than the telemetry identifier. r=search-reviewers,scunnane
    Additionally renames the probe, to avoid issues with matching prefixes when duplicating for ohttp, and for more consistency
    with the other search.suggestions probes.
    
    Differential Revision: https://phabricator.services.mozilla.com/D264841
    
    UltraBlame original commit: 15251b122be258a4f5dff8ef5edefa39d427492f

--- toolkit/components/search/tests/xpcshell/test_searchSuggest.js
+++ toolkit/components/search/tests/xpcshell/test_searchSuggest.js
@@ -1005,9 +1005,9 @@ function updateSearchHistory(operation, value) {
 
 function assertLatencyCollection(engine, shouldRecord) {
   let latencyDistribution =
-    Glean.search.suggestionsLatency[
+    Glean.searchSuggestions.latency[
       
-      engine.isConfigEngine ? engine.telemetryId : "other"
+      engine.isConfigEngine ? engine.id : "other"
     ].testGetValue();
 
   if (shouldRecord) {
