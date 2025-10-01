63e5b9694cebebbf81ad0562cad7a2729385d0fa
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-10-01 03:49:02 +0000

    Bug 1983154 - Support JS::Value in TraceableFifo r=jonco
    Differential Revision: https://phabricator.services.mozilla.com/D264130
    
    UltraBlame original commit: 3188150fb2dce3ea2c30e3203c325b8e4fcbd478

--- js/public/GCVector.h
+++ js/public/GCVector.h
@@ -155,6 +155,12 @@ class GCVector {
   void popBack() { return vector.popBack(); }
   T popCopy() { return vector.popCopy(); }
 
+  void swap(GCVector& other) {
+    
+    
+    vector.swap(other.vector);
+  }
+
   size_t sizeOfExcludingThis(mozilla::MallocSizeOf mallocSizeOf) const {
     return vector.sizeOfExcludingThis(mallocSizeOf);
   }
