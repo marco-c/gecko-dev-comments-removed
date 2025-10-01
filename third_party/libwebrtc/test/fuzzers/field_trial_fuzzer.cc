75b37ca0fe4179c32aebbaa7515cb2a7c8f97b44
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-10-01 02:29:14 +0000

    Bug 1985396 - Vendor libwebrtc from b98efd4b57
    Upstream commit: https://webrtc.googlesource.com/src/+/b98efd4b57d927b119a0c5fd70181c423d179339
        Repurporse field_trials_fuzzer to validing parsing of the FieldTrials
    
        Bug: webrtc:419453427
        Change-Id: I55f200fd349625ccf9200ba27cde86fa3eb25dc2
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/396401
        Commit-Queue: Danil Chapovalov <danilchapwebrtc.org>
        Reviewed-by: Sam Zackrisson <sazawebrtc.org>
        Cr-Commit-Position: refs/heads/main{#44942}
    
    UltraBlame original commit: 22c3997899fbca44e8cb186a57ad3846b8608ff0

--- third_party/libwebrtc/test/fuzzers/field_trial_fuzzer.cc
+++ third_party/libwebrtc/test/fuzzers/field_trial_fuzzer.cc
@@ -8,19 +8,20 @@
 
 
 
-#include <stddef.h>
-#include <stdint.h>
+#include <cstddef>
+#include <cstdint>
 
-#include <string>
-
-#include "system_wrappers/include/field_trial.h"
+#include "absl/strings/string_view.h"
+#include "api/field_trials.h"
 
 namespace webrtc {
 
 void FuzzOneInput(const uint8_t* data, size_t size) {
-  std::string field_trial(reinterpret_cast<const char*>(data), size);
-  field_trial::InitFieldTrialsFromString(field_trial.c_str());
-  field_trial::FindFullName(field_trial);
+  
+  
+  
+  FieldTrials::Create(
+      absl::string_view(reinterpret_cast<const char*>(data), size));
 }
 
 }  
