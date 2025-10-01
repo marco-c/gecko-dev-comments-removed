b975a2aea2f943ac7568c1ad4075f62c9f8436f6
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-10-01 03:39:34 +0000

    Bug 1972623 - Remove code related to list feed and contexual content r=home-newtab-reviewers,npypchenko
    Differential Revision: https://phabricator.services.mozilla.com/D254048
    
    UltraBlame original commit: e1f335f0e4db4840a990d7d4034a3b03da5c99ba

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1999,11 +1999,6 @@ pref("browser.newtabpage.activity-stream.discoverystream.topicSelection.locale-t
 pref("browser.newtabpage.activity-stream.discoverystream.topicLabels.locale-topic-label-config", "en-US, en-GB, en-CA");
 
 
-pref("browser.newtabpage.activity-stream.discoverystream.contextualContent.locale-content-config", "en-US,en-GB,en-CA,de");
-
-pref("browser.newtabpage.activity-stream.discoverystream.contextualContent.region-content-config", "");
-
-
 pref("browser.newtabpage.activity-stream.discoverystream.sections.locale-content-config", "en-US,en-CA");
 
 pref("browser.newtabpage.activity-stream.discoverystream.sections.region-content-config", "");
