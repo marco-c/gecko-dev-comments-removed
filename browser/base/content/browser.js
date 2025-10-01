9b1103cab4618009375e7884ded566ff0856a84a
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-10-01 02:35:09 +0000

    Revert "Bug 1898356 - go back one page arrow (button) turns grey after clearing browsing & download history. r=hsohaney" for causing bc failures  browser_purgehistory_clears_sh.js
    This reverts commit 54aaf25ea02ac876fc6f1fa5b6639b8f1783897a.
    
    UltraBlame original commit: 0819c87986c00cd7b2ac47f52f8af4bea6a481b1

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -1025,11 +1025,6 @@ const gClickAndHoldListenersOnElement = {
   },
 };
 
-
-
-
-
-
 const gSessionHistoryObserver = {
   observe(subject, topic) {
     if (topic != "browser:purge-session-history") {
@@ -1037,9 +1032,9 @@ const gSessionHistoryObserver = {
     }
 
     var backCommand = document.getElementById("Browser:Back");
-    backCommand.setAttribute("wasdisabled", "true");
+    backCommand.setAttribute("disabled", "true");
     var fwdCommand = document.getElementById("Browser:Forward");
-    fwdCommand.setAttribute("wasdisabled", "true");
+    fwdCommand.setAttribute("disabled", "true");
 
     
     gURLBar.editor.clearUndoRedo();
