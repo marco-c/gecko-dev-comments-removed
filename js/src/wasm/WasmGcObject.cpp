5b12fb2df57d93515f62d93cc48688fcb443f696
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-10-01 03:00:04 +0000

    Bug 1989525 - Part 1: Refactor StoreBuffer.h r=sfink
    This file gets included everywhere in the JS engine so it's good to pull out
    anything that doesn't need to be there.
    
    Differential Revision: https://phabricator.services.mozilla.com/D265425
    
    UltraBlame original commit: 5b974056b58c9dcee85c5052749f82cd8cb2a346

--- js/src/wasm/WasmGcObject.cpp
+++ js/src/wasm/WasmGcObject.cpp
@@ -26,6 +26,7 @@
 #include "vm/TypedArrayObject.h"
 #include "vm/Uint8Clamped.h"
 
+#include "gc/BufferAllocator-inl.h"
 #include "gc/GCContext-inl.h"  
 #include "gc/ObjectKind-inl.h"
 #include "vm/JSContext-inl.h"
