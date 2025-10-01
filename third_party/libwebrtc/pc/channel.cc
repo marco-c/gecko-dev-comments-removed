af8758b156b41bd34a1e31104da779868a111815
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-10-01 02:29:54 +0000

    Bug 1985396 - Vendor libwebrtc from 9496cc0e1b
    Upstream commit: https://webrtc.googlesource.com/src/+/9496cc0e1b8cc50185ded19522b68d7c94c17298
        Change SetLocalContent / SetRemoteContent to update header extensions
    
        The functions were skipping over updating the sender and receiver when
        the list of header extensions was changed. Now it should be better.
    
        Bug: webrtc:383078466
        Change-Id: I1b93ed1ba4bbbf6c5b13861a6e21a7a7c82a5a66
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/396640
        Reviewed-by: Per Kjellander <perkjwebrtc.org>
        Commit-Queue: Harald Alvestrand <htawebrtc.org>
        Cr-Commit-Position: refs/heads/main{#44951}
    
    UltraBlame original commit: 1bf25fcac7f8fecec05eada7fc98eb678261e5ea

--- third_party/libwebrtc/pc/channel.cc
+++ third_party/libwebrtc/pc/channel.cc
@@ -915,7 +915,6 @@ bool VoiceChannel::SetLocalContent_w(const MediaContentDescription* content,
       }
     }
   }
-
   last_recv_params_ = recv_params;
 
   if (!UpdateLocalStreams_w(content->streams(), type, error_desc)) {
@@ -1042,7 +1041,6 @@ bool VideoChannel::SetLocalContent_w(const MediaContentDescription* content,
   media_send_channel()->SetExtmapAllowMixed(content->extmap_allow_mixed());
 
   VideoReceiverParameters recv_params = last_recv_params_;
-
   MediaChannelParametersFromMediaDescription(
       content, header_extensions,
       RtpTransceiverDirectionHasRecv(content->direction()), &recv_params);
@@ -1117,7 +1115,12 @@ bool VideoChannel::SetLocalContent_w(const MediaContentDescription* content,
 
   last_recv_params_ = recv_params;
 
+  
+  needs_send_params_update |=
+      (last_send_params_.extensions != header_extensions &&
+       !send_params.codecs.empty());
   if (needs_send_params_update) {
+    send_params.extensions = header_extensions;
     if (!media_send_channel()->SetSenderParameters(send_params)) {
       error_desc = StringFormat(
           "Failed to set send parameters for m-section with mid='%s'.",
@@ -1228,7 +1231,11 @@ bool VideoChannel::SetRemoteContent_w(const MediaContentDescription* content,
       media_send_channel()->SendCodecRtxTime());
   last_send_params_ = send_params;
 
+  needs_recv_params_update |=
+      (recv_params.extensions != send_params.extensions &&
+       !recv_params.codecs.empty());
   if (needs_recv_params_update) {
+    recv_params.extensions = send_params.extensions;
     if (!media_receive_channel()->SetReceiverParameters(recv_params)) {
       error_desc = StringFormat(
           "Failed to set recv parameters for m-section with mid='%s'.",
