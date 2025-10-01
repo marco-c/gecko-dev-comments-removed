35bff5d2e3d21d9561d7ee457138d64226f3fd61
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-10-01 02:32:03 +0000

    Bug 1972075 - Convert Downloads to config-based prefs r=fluent-reviewers,mstriemer,flod,desktop-theme-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D261463
    
    UltraBlame original commit: d88835b9918ea4c0a771831b1ac20d59122f47e2

--- browser/components/preferences/widgets/setting-group/setting-group.mjs
+++ browser/components/preferences/widgets/setting-group/setting-group.mjs
@@ -5,7 +5,7 @@
 import { html, ifDefined } from "chrome://global/content/vendor/lit.all.mjs";
 import { MozLitElement } from "chrome://global/content/lit-utils.mjs";
 
-const CLICK_HANDLERS = ["moz-box-link"];
+const CLICK_HANDLERS = ["moz-box-link", "moz-box-item", "moz-button"];
 
 export class SettingGroup extends MozLitElement {
   static properties = {
@@ -64,6 +64,7 @@ export class SettingGroup extends MozLitElement {
     }
     return html`<moz-fieldset
       data-l10n-id=${ifDefined(this.config.l10nId)}
+      .headingLevel=${this.config.headingLevel}
       @change=${this.onChange}
       @click=${this.onClick}
       >${this.config.items.map(item => this.itemTemplate(item))}</moz-fieldset
