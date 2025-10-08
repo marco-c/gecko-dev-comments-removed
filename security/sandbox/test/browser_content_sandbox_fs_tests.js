0552cfd1c4214fb22c3c5a16a57137ccc37fb244
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-10-08 09:16:39 +0000

    Bug 1991801 - Remove unnecessary macOS version checks. r=firefox-desktop-core-reviewers ,Gijs
    Differential Revision: https://phabricator.services.mozilla.com/D266938
    
    UltraBlame original commit: ecb4db8c7ab1df4f84b3391432e6afad604dd3ee

--- security/sandbox/test/browser_content_sandbox_fs_tests.js
+++ security/sandbox/test/browser_content_sandbox_fs_tests.js
@@ -334,20 +334,6 @@ async function testFileAccessMacOnly() {
   });
 
   
-  
-  if (AppConstants.isPlatformAndVersionAtMost("macosx", 18)) {
-    
-    let network = GetDir("/Network");
-    tests.push({
-      desc: "/Network",
-      ok: false,
-      browser: webBrowser,
-      file: network,
-      minLevel: minHomeReadSandboxLevel(),
-      func: readDir,
-    });
-  }
-  
   let users = GetDir("/Users");
   tests.push({
     desc: "/Users",
