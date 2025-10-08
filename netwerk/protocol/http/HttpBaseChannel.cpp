6c4371f7a410822072ec2d2ad5d2ab58e88c2860
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-10-08 09:25:11 +0000

    Bug 1917970: only remove dcb and dcz Content-Encodings r=necko-reviewers,kershaw
    Differential Revision: https://phabricator.services.mozilla.com/D264525
    
    UltraBlame original commit: ccd35748b62f2af9b3e14bf4abe2db6fa4c413fa

--- netwerk/protocol/http/HttpBaseChannel.cpp
+++ netwerk/protocol/http/HttpBaseChannel.cpp
@@ -1520,6 +1520,7 @@ HttpBaseChannel::DoApplyContentConversions(nsIStreamListener* aNextListener,
   nsAutoCString newEncoding;
   char* cePtr = contentEncoding.BeginWriting();
   uint32_t count = 0;
+  bool removed_encoding = false;
   while (char* val = nsCRT::strtok(cePtr, HTTP_LWS ",", &cePtr)) {
     if (++count > 16) {
       
@@ -1559,6 +1560,9 @@ HttpBaseChannel::DoApplyContentConversions(nsIStreamListener* aNextListener,
         }
         glean::http::content_encoding.AccumulateSingleSample(mode);
       }
+      if (from.EqualsLiteral("dcb") || from.EqualsLiteral("dcz")) {
+        removed_encoding = true;
+      }
       nextListener = converter;
     } else {
       if (val) LOG(("Unknown content encoding '%s', ignoring\n", val));
@@ -1569,11 +1573,19 @@ HttpBaseChannel::DoApplyContentConversions(nsIStreamListener* aNextListener,
     }
   }
 
-  LOG(("Changing Content-Encoding from %s to %s", contentEncoding.get(),
-       newEncoding.get()));
   
-  rv = mResponseHead->SetHeaderOverride(nsHttp::Content_Encoding, newEncoding);
-
+  
+  
+  
+  
+  
+  if (removed_encoding) {
+    LOG(("Changing Content-Encoding from %s to %s", contentEncoding.get(),
+         newEncoding.get()));
+    
+    rv =
+        mResponseHead->SetHeaderOverride(nsHttp::Content_Encoding, newEncoding);
+  }
   *aNewNextListener = do_AddRef(nextListener).take();
   return NS_OK;
 }
