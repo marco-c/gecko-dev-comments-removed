73516971c0a055b59fd601e14ea7257a5ff9c65c
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-10-08 09:19:52 +0000

    Bug 1991224 - Misc. Translations test infrastructue fixes r=translations-reviewers,gregtatum
    This patch contains a few small ergonomic changes
    to the Translations test infrastructure.
    
    Differential Revision: https://phabricator.services.mozilla.com/D266487
    
    UltraBlame original commit: d8119ec72c61ebf41598fa2d8b6dcb808177b09c

--- toolkit/components/translations/tests/browser/shared-head.js
+++ toolkit/components/translations/tests/browser/shared-head.js
@@ -1360,7 +1360,10 @@ class MockedA11yUtils {
 
 
 async function ensureWindowSize(win, width, height) {
-  if (win.outerWidth < width + 50 && win.outerHeight < height + 50) {
+  if (
+    Math.abs(win.outerWidth - width) < 1 &&
+    Math.abs(win.outerHeight - height) < 1
+  ) {
     return;
   }
 
@@ -3155,9 +3158,14 @@ class AboutTranslationsTestUtils {
       const unexpectedEventMap = {};
       for (const eventName of unexpected) {
         unexpectedEventMap[eventName] = false;
-        this.waitForEvent(eventName).then(() => {
-          unexpectedEventMap[eventName] = true;
-        });
+        this.waitForEvent(eventName)
+          .then(() => {
+            unexpectedEventMap[eventName] = true;
+          })
+          .catch(() => {
+            
+            
+          });
       }
 
       await callback();
