e039046298b8fea11632dd218db4d2235af6fa92
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-10-08 14:00:13 +0000

    Bug 1992422 - part 3: Eliminate mNumRows and mNumCols. r=layout-reviewers,emilio
    Now that these two data members are set in exactly one place after
    initialization, we can see that they always have the same value as the length
    of mRowSizes and mColSizes, so remove the members. Hopefully the extra overhead
    from the integer conversions isn't relevant.
    
    Differential Revision: https://phabricator.services.mozilla.com/D267674
    
    UltraBlame original commit: 2c8da230fe831ae4b5abe0b90e264b5299f1d559

--- layout/generic/nsFrameSetFrame.h
+++ layout/generic/nsFrameSetFrame.h
@@ -185,8 +185,6 @@ class nsHTMLFramesetFrame final : public nsContainerFrame {
   nsTArray<nscoord> mRowSizes;  
   nsTArray<nscoord> mColSizes;  
   mozilla::LayoutDeviceIntPoint mFirstDragPoint;
-  int32_t mNumRows;
-  int32_t mNumCols;
   int32_t mNonBorderChildCount;
   int32_t mNonBlankChildCount;
   int32_t mEdgeVisibility;
