bbb0de05ca29ab544b3a05fbaef481a17aa41d05
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-10-16 01:54:25 +0000

    Bug 1993383 - Trigger a repaint of the window in the parent process after a GPU process crash. r=bradwerth
    Differential Revision: https://phabricator.services.mozilla.com/D268052
    
    UltraBlame original commit: 7ff056f5d0ef6a8773778a4a3661fbeb65f35423

--- widget/cocoa/nsCocoaWindow.mm
+++ widget/cocoa/nsCocoaWindow.mm
@@ -974,6 +974,30 @@ void nsCocoaWindow::DestroyCompositor() {
   nsBaseWidget::DestroyCompositor();
 }
 
+void nsCocoaWindow::NotifyCompositorSessionLost(
+    mozilla::layers::CompositorSession* aSession) {
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  const double kTriggerPaintDelayAfterGpuProcessCrash = 0.4;  
+  [mChildView performSelector:@selector(markLayerForDisplay)
+                   withObject:nil
+                   afterDelay:kTriggerPaintDelayAfterGpuProcessCrash];
+
+  nsBaseWidget::NotifyCompositorSessionLost(aSession);
+}
+
 void nsCocoaWindow::SetCompositorWidgetDelegate(
     mozilla::widget::CompositorWidgetDelegate* aDelegate) {
   if (aDelegate) {
