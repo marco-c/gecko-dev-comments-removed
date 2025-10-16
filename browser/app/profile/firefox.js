77e475bee2f7ad4486523e8ae5336e7d2d4dd42f
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-10-16 02:18:24 +0000

    Revert "Bug 1986946 - Add context menu entry point for split view, and handle the single tab and multi-selected cases r=tabbrowser-reviewers,fluent-reviewers,jsudiaman,flod,sthompson,sclements,desktop-theme-reviewers,sfoster" for bc failure on browser_tab_splitview.js
    This reverts commit 0f7b9bd615a8bda5995a0e58a71728ab76c27d58.
    
    UltraBlame original commit: 4f933461ddfdf75248d13271408dc7730993eb4d

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -2634,9 +2634,6 @@ pref("browser.tabs.fadeOutExplicitlyUnloadedTabs", true);
 pref("browser.tabs.fadeOutUnloadedTabs", false);
 
 
-pref("browser.tabs.splitView.enabled", false);
-
-
 
 pref("extensions.experiments.enabled", false);
 
