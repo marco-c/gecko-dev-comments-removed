a9117ca9c9d6fd91f00dd100e04fa2b2e8fc928d
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-10-29 18:05:40 +0000

    Revert "Bug 1993015 - include preset into fuzzy commit msg, r=marco" for causing py failures  test_selectors.py
    This reverts commit ae02939f4d6b391cec23e0d96d0ed257b8b2d252.
    
    UltraBlame original commit: a90a3498cd07f4bd588bcad683e8e767f9ad95bd

--- tools/tryselect/mach_commands.py
+++ tools/tryselect/mach_commands.py
@@ -114,7 +114,6 @@ def handle_presets(
         name = preset
         preset = presets(command_context)[name]
         selector = preset.pop("selector")
-        kwargs["preset_id"] = name
         preset.pop("description", None)  
 
         if not subcommand:
