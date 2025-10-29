954c582f399a819c7e53ce90d980dc1fdec4a120
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-10-29 18:52:06 +0000

    Bug 1993939 Part 1 - Fix text-decoration-trim direction for sideways-left text in a block frame r=layout-reviewers,layout-jp-market-reviewers,jfkthame
    Differential Revision: https://phabricator.services.mozilla.com/D269905
    
    UltraBlame original commit: fdf746352e716d1e940623981ee077c06a57ec25

--- layout/painting/nsCSSRendering.cpp
+++ layout/painting/nsCSSRendering.cpp
@@ -4659,7 +4659,7 @@ gfxRect nsCSSRendering::GetTextDecorationRectInternal(
   }
 
   
-  r.x += aParams.sidewaysLeft ? aParams.trimRight : aParams.trimLeft;
+  r.x += aParams.trimLeft;
   r.width -= aParams.trimLeft + aParams.trimRight;
   r.width = std::max(r.width, 0.0);
 
