bf3b39ab57c298b61056fb5c541afdb0d1860d7a
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-10-29 18:29:43 +0000

    Bug 1973591 - Avoid freeing swapchain buffer IDs too early. r=webgpu-reviewers,aleiserson
    Differential Revision: https://phabricator.services.mozilla.com/D269784
    
    UltraBlame original commit: acca32fb0d6b97953cbf16de0581daa5ebfad01c

--- dom/webgpu/Device.cpp
+++ dom/webgpu/Device.cpp
@@ -933,8 +933,8 @@ already_AddRefed<dom::Promise> Device::CreateRenderPipelineAsync(
 already_AddRefed<Texture> Device::InitSwapChain(
     const dom::GPUCanvasConfiguration* const aConfig,
     const layers::RemoteTextureOwnerId aOwnerId,
-    mozilla::Span<RawId const> aBufferIds, bool aUseSharedTextureInSwapChain,
-    gfx::SurfaceFormat aFormat, gfx::IntSize aCanvasSize) {
+    bool aUseSharedTextureInSwapChain, gfx::SurfaceFormat aFormat,
+    gfx::IntSize aCanvasSize) {
   MOZ_ASSERT(aConfig);
 
   
@@ -948,8 +948,7 @@ already_AddRefed<Texture> Device::InitSwapChain(
 
   ffi::wgpu_client_create_swap_chain(
       GetClient(), GetId(), mQueue->GetId(), rgbDesc.size().Width(),
-      rgbDesc.size().Height(), (int8_t)rgbDesc.format(),
-      {aBufferIds.Elements(), aBufferIds.Length()}, aOwnerId.mId,
+      rgbDesc.size().Height(), (int8_t)rgbDesc.format(), aOwnerId.mId,
       aUseSharedTextureInSwapChain);
 
   
