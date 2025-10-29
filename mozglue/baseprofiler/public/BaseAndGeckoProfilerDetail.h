50136cc1140a00a9a27c412971b9073a9e213757
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-10-29 18:03:26 +0000

    Bug 1959977 - Bump the gecko profiler format version after adding sourceIndexes r=mstange,profiler-reviewers
    This requires frontend changes:
    https://github.com/firefox-devtools/profiler/pull/5506
    
    Differential Revision: https://phabricator.services.mozilla.com/D259256
    
    UltraBlame original commit: edc8fe4c0ecf9f9067ab6bfcf0a1f630a94ce1b4

--- mozglue/baseprofiler/public/BaseAndGeckoProfilerDetail.h
+++ mozglue/baseprofiler/public/BaseAndGeckoProfilerDetail.h
@@ -24,7 +24,7 @@ namespace mozilla {
 class ProfileBufferChunkManagerWithLocalLimit;
 
 
-const int GECKO_PROFILER_FORMAT_VERSION = 31;
+const int GECKO_PROFILER_FORMAT_VERSION = 32;
 
 namespace baseprofiler::detail {
 
