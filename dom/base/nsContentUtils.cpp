256b0537e27b0cf32ff84667f985113c0a093e8d
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-10-29 17:37:32 +0000

    Bug 1994964 - Simplify nsContentUtils::WidgetForDocument. r=tnikkel,layout-reviewers
    Use the closest widget of the root frame.
    
    Differential Revision: https://phabricator.services.mozilla.com/D269024
    
    UltraBlame original commit: 80be2865bbee46a7e5433dc4e32f935200b94faa

--- dom/base/nsContentUtils.cpp
+++ dom/base/nsContentUtils.cpp
@@ -132,7 +132,6 @@
 #include "mozilla/Tokenizer.h"
 #include "mozilla/UniquePtr.h"
 #include "mozilla/Unused.h"
-#include "mozilla/Variant.h"
 #include "mozilla/ViewportUtils.h"
 #include "mozilla/dom/AncestorIterator.h"
 #include "mozilla/dom/AutoEntryScript.h"
@@ -7799,19 +7798,8 @@ nsIWidget* nsContentUtils::WidgetForDocument(const Document* aDocument) {
   if (!presShell) {
     return nullptr;
   }
-  nsViewManager* vm = presShell->GetViewManager();
-  if (!vm) {
-    return nullptr;
-  }
-  nsView* rootView = vm->GetRootView();
-  if (!rootView) {
-    return nullptr;
-  }
-  nsView* displayRoot = nsViewManager::GetDisplayRootFor(rootView);
-  if (!displayRoot) {
-    return nullptr;
-  }
-  return displayRoot->GetNearestWidget(nullptr);
+  nsIFrame* rootFrame = presShell->GetRootFrame();
+  return rootFrame ? rootFrame->GetNearestWidget() : nullptr;
 }
 
 nsIWidget* nsContentUtils::WidgetForContent(const nsIContent* aContent) {
