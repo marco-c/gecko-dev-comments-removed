b30d5cead19ace6e1e37b71a3964ab013a192c43
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-10-29 17:59:27 +0000

    Bug 1994205 part 1 - Tidy up InstanceOfIRGenerator::tryAttachStub a bit. r=iain
    Differential Revision: https://phabricator.services.mozilla.com/D269252
    
    UltraBlame original commit: 4efd82e089191fb2f4288fa6e665895fd5cb514d

--- js/src/jit/CacheIR.cpp
+++ js/src/jit/CacheIR.cpp
@@ -5688,10 +5688,16 @@ AttachDecision InstanceOfIRGenerator::tryAttachStub() {
   MOZ_ASSERT(cacheKind_ == CacheKind::InstanceOf);
   AutoAssertNoPendingException aanpe(cx_);
 
+  TRY_ATTACH(tryAttachFunction());
+
+  trackAttached(IRGenerator::NotAttached);
+  return AttachDecision::NoAction;
+}
+
+AttachDecision InstanceOfIRGenerator::tryAttachFunction() {
   
   
   if (!rhsObj_->is<JSFunction>()) {
-    trackAttached(IRGenerator::NotAttached);
     return AttachDecision::NoAction;
   }
 
@@ -5709,13 +5715,11 @@ AttachDecision InstanceOfIRGenerator::tryAttachStub() {
   if (!LookupPropertyPure(cx_, fun, hasInstanceID, &hasInstanceHolder,
                           &hasInstanceProp) ||
       !hasInstanceProp.isNativeProperty()) {
-    trackAttached(IRGenerator::NotAttached);
     return AttachDecision::NoAction;
   }
 
   JSObject& funProto = cx_->global()->getPrototype(JSProto_Function);
   if (hasInstanceHolder != &funProto) {
-    trackAttached(IRGenerator::NotAttached);
     return AttachDecision::NoAction;
   }
 
@@ -5728,16 +5732,19 @@ AttachDecision InstanceOfIRGenerator::tryAttachStub() {
   MOZ_ASSERT(IsCacheableProtoChain(fun, hasInstanceHolder));
 
   
-  Maybe<PropertyInfo> prop = fun->lookupPure(cx_->names().prototype);
-  if (prop.isNothing() || !prop->isDataProperty()) {
-    trackAttached(IRGenerator::NotAttached);
+  Maybe<PropertyInfo> prototypeProp = fun->lookupPure(cx_->names().prototype);
+  if (prototypeProp.isNothing()) {
+    return AttachDecision::NoAction;
+  }
+  if (!prototypeProp->isDataProperty()) {
     return AttachDecision::NoAction;
   }
 
-  uint32_t slot = prop->slot();
-  MOZ_ASSERT(slot >= fun->numFixedSlots(), "Stub code relies on this");
-  if (!fun->getSlot(slot).isObject()) {
-    trackAttached(IRGenerator::NotAttached);
+  
+  uint32_t prototypeSlot = prototypeProp->slot();
+  MOZ_ASSERT(prototypeSlot >= fun->numFixedSlots(),
+             "LoadDynamicSlot expects a dynamic slot");
+  if (!fun->getSlot(prototypeSlot).isObject()) {
     return AttachDecision::NoAction;
   }
 
@@ -5759,7 +5766,7 @@ AttachDecision InstanceOfIRGenerator::tryAttachStub() {
 
   
   ValOperandId protoValId =
-      writer.loadDynamicSlot(rhsId, slot - fun->numFixedSlots());
+      writer.loadDynamicSlot(rhsId, prototypeSlot - fun->numFixedSlots());
   ObjOperandId protoId = writer.guardToObject(protoValId);
 
   
