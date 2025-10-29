6b0a883cc104d697aed30815cc228184cd7869ea
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-10-29 18:55:23 +0000

    Bug 1767228 - Refactor: Rename nsFormFillController focusedElement to controlledElement. r=emilio,credential-management-reviewers,dimi
    Differential Revision: https://phabricator.services.mozilla.com/D269370
    
    UltraBlame original commit: 1a86397216dd5e82291a3bfa69d3512b165401e0

--- toolkit/components/satchel/nsFormFillController.h
+++ toolkit/components/satchel/nsFormFillController.h
@@ -110,7 +110,7 @@ class nsFormFillController final : public nsIFormFillController,
   
 
   nsCOMPtr<nsIAutoCompleteController> mController;
-  mozilla::dom::Element* mFocusedElement;
+  mozilla::dom::Element* mControlledElement;
   RefPtr<mozilla::CancelableRunnable> mRestartAfterAttributeChangeTask;
 
   
