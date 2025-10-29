382795a134287514768cf0bbdae3d84e37d031e4
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-10-29 18:22:56 +0000

    Revert "Bug 1995872 - Add sections as default for US r=ini,home-newtab-reviewers" for causing bc failures on browser_discovery_card.js
    This reverts commit 59821e7b046fb18ebf0fcea877f07acc08547501.
    
    UltraBlame original commit: c165ea20bb11fee6a51e3fc6328626b84353b84e

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1986,7 +1986,7 @@ pref("browser.newtabpage.activity-stream.discoverystream.topicLabels.locale-topi
 
 pref("browser.newtabpage.activity-stream.discoverystream.sections.locale-content-config", "en-US,en-CA");
 
-pref("browser.newtabpage.activity-stream.discoverystream.sections.region-content-config", "US");
+pref("browser.newtabpage.activity-stream.discoverystream.sections.region-content-config", "");
 
 pref("browser.newtabpage.activity-stream.discoverystream.sections.cards.enabled", true);
 
