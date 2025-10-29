3933176fae20baa8167cfebd7e478bd8912aa6cd
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-10-29 19:16:07 +0000

    Bug 1991990: Update MSIX Packaging Branding r=bhearsum
    Thunderbird already uses a " Beta" suffix in its branding. Only add a " Beta" suffix if one does not already exist.
    
    Also adds an esr channel with a corresponding " ESR" suffix if one does not aleady exist in the products branding.
    
    Differential Revision: https://phabricator.services.mozilla.com/D268806
    
    UltraBlame original commit: dc5c2af7e5c84fc902e502134e63603f15d482fe

--- python/mozbuild/mozbuild/repackaging/msix.py
+++ python/mozbuild/mozbuild/repackaging/msix.py
@@ -354,6 +354,7 @@ def repackage_msix(
     if channel not in (
         "official",
         "beta",
+        "esr",
         "aurora",
         "nightly",
         "unofficial",
@@ -412,9 +413,17 @@ def repackage_msix(
     if not displayname:
         displayname = f"Mozilla {first}"
 
+        
         if channel == "beta":
+            suffix = " Beta"
+            if not displayname.endswith(suffix):
+                displayname += suffix
+
+        elif channel == "esr":
             
-            displayname += " Beta"
+            suffix = " ESR"
+            if not displayname.endswith(suffix):
+                displayname += suffix
 
     second = next(values)
     vendor = vendor or second
@@ -472,9 +481,17 @@ def repackage_msix(
     _, _, brandFullName = brandFullName.partition("=")
     brandFullName = brandFullName.strip()
 
+    
     if channel == "beta":
+        suffix = " Beta"
+        if not brandFullName.endswith(suffix):
+            brandFullName += suffix
+
+    elif channel == "esr":
         
-        brandFullName += " Beta"
+        suffix = " ESR"
+        if not brandFullName.endswith(suffix):
+            brandFullName += suffix
 
     branding = get_branding(
         use_official_branding, topsrcdir, build_app, unpack_finder, log
