e05f91d0eb0d559a78852c8bfb77b9f4082b554a
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-10-29 19:11:46 +0000

    Revert "Bug 1979518 - [devtools] Always specify the size of the screenshot to be taken. r=ochameau,devtools-reviewers" as requested by ochameau.
    This reverts commit a8fb4f85a8f750054e15233b0398eec7ce313136.
    
    UltraBlame original commit: e2cd210ea96a34e24d94cad4ab67243b3c78fb07

--- devtools/server/actors/screenshot-content.js
+++ devtools/server/actors/screenshot-content.js
@@ -66,16 +66,23 @@ exports.ScreenshotContentActor = class ScreenshotContentActor extends Actor {
     const windowZoom = getCurrentZoom(window);
     const messages = [];
 
+    
+    
+    if (!fullpage && !selector && !nodeActorID) {
+      return {
+        rect: null,
+        messages,
+        windowDpr,
+        windowZoom,
+      };
+    }
+
     let left;
     let top;
     let width;
     let height;
 
-    
-    
-    
-    
-    if (fullpage || (!selector && !nodeActorID)) {
+    if (fullpage) {
       
       const winUtils = window.windowUtils;
       const scrollbarHeight = {};
@@ -84,12 +91,16 @@ exports.ScreenshotContentActor = class ScreenshotContentActor extends Actor {
 
       left = 0;
       top = 0;
-      width = window.innerWidth - scrollbarWidth.value;
-      height = window.innerHeight - scrollbarHeight.value;
-      if (fullpage) {
-        width = width + window.scrollMaxX - window.scrollMinX;
-        height = height + window.scrollMaxY - window.scrollMinY;
-      }
+      width =
+        window.innerWidth +
+        window.scrollMaxX -
+        window.scrollMinX -
+        scrollbarWidth.value;
+      height =
+        window.innerHeight +
+        window.scrollMaxY -
+        window.scrollMinY -
+        scrollbarHeight.value;
     } else if (selector) {
       const node = window.document.querySelector(selector);
 
