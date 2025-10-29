88fb6b75747d0cafbfabd04baaf3854d604dd930
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-10-29 17:36:41 +0000

    Bug 1993938 - Add network.lna.websocket.enabled preference for WebSocket LNA control. r=necko-reviewers,kershaw,valentin
    This commit implements enhanced Local Network Access (LNA) configuration
    for WebSocket connections by adding the network.lna.websocket.enabled preference.
    
    Changes:
    - Add network.lna.websocket.enabled preference (default: true)
    - Modify nsHttpTransaction::AllowedToConnectToIpAddressSpace to skip LNA
      checks for WebSocket connections when preference is disabled
    - Add browser test for WebSocket LNA preference functionality
    
    The preference allows administrators to selectively disable LNA checks
    for WebSocket connections while maintaining checks for regular HTTP requests.
    
    Differential Revision: https://phabricator.services.mozilla.com/D266461
    
    UltraBlame original commit: bd490f57536437a11353daf000febfc0983659e0

--- netwerk/protocol/http/nsHttpTransaction.cpp
+++ netwerk/protocol/http/nsHttpTransaction.cpp
@@ -63,6 +63,7 @@
 #include "nsTransportUtils.h"
 #include "sslerr.h"
 #include "SpeculativeTransaction.h"
+#include "mozilla/Preferences.h"
 
 
 
@@ -3737,8 +3738,21 @@ nsILoadInfo::IPAddressSpace nsHttpTransaction::GetTargetIPAddressSpace() {
 bool nsHttpTransaction::AllowedToConnectToIpAddressSpace(
     nsILoadInfo::IPAddressSpace aTargetIpAddressSpace) {
   
+
   if (!StaticPrefs::network_lna_enabled()) {
     return true;
+  }
+
+  
+  if (mConnInfo && gIOService &&
+      gIOService->ShouldSkipDomainForLNA(mConnInfo->GetOrigin())) {
+    return true;
+  }
+
+  
+  
+  if (!StaticPrefs::network_lna_websocket_enabled() && IsWebsocketUpgrade()) {
+    return true;  
   }
 
   
