2bdbd63f932845f3f3fcf80e82d3f71e3834c9d7
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-10-29 17:37:28 +0000

    Revert "Bug 1992107: Handle near overrecursion in RegExpDepthCheck. r=iain" for causing failures at RegExpCompiler.
    This reverts commit 7c3cc4a7aa4ee76df736855165b44cca0d1a20c9.
    
    UltraBlame original commit: e07ad27dd923913fe7034083090234f5d1e9f954

--- js/src/irregexp/RegExpAPI.cpp
+++ js/src/irregexp/RegExpAPI.cpp
@@ -417,10 +417,12 @@ class RegExpDepthCheck final : public v8::internal::RegExpVisitor {
   }
 
   
-#define LEAF_DEPTH(Kind)                                                  \
-  void* Visit##Kind(v8::internal::RegExp##Kind* node, void*) override {   \
-    AutoCheckRecursionLimit recursion(cx_);                               \
-    return (void*)recursion.checkWithExtraDontReport(cx_, FRAME_PADDING); \
+#define LEAF_DEPTH(Kind)                                                \
+  void* Visit##Kind(v8::internal::RegExp##Kind* node, void*) override { \
+    uint8_t padding[FRAME_PADDING];                                     \
+    dummy_ = padding; /* Prevent padding from being optimized away.*/   \
+    AutoCheckRecursionLimit recursion(cx_);                             \
+    return (void*)recursion.checkDontReport(cx_);                       \
   }
 
   LEAF_DEPTH(Assertion)
@@ -435,8 +437,10 @@ class RegExpDepthCheck final : public v8::internal::RegExpVisitor {
   
 #define WRAPPER_DEPTH(Kind)                                             \
   void* Visit##Kind(v8::internal::RegExp##Kind* node, void*) override { \
+    uint8_t padding[FRAME_PADDING];                                     \
+    dummy_ = padding; /* Prevent padding from being optimized away.*/   \
     AutoCheckRecursionLimit recursion(cx_);                             \
-    if (!recursion.checkWithExtraDontReport(cx_, FRAME_PADDING)) {      \
+    if (!recursion.checkDontReport(cx_)) {                              \
       return nullptr;                                                   \
     }                                                                   \
     return node->body()->Accept(this, nullptr);                         \
@@ -450,8 +454,10 @@ class RegExpDepthCheck final : public v8::internal::RegExpVisitor {
 
   void* VisitAlternative(v8::internal::RegExpAlternative* node,
                          void*) override {
+    uint8_t padding[FRAME_PADDING];
+    dummy_ = padding; 
     AutoCheckRecursionLimit recursion(cx_);
-    if (!recursion.checkWithExtraDontReport(cx_, FRAME_PADDING)) {
+    if (!recursion.checkDontReport(cx_)) {
       return nullptr;
     }
     for (auto* child : *node->nodes()) {
@@ -463,8 +469,10 @@ class RegExpDepthCheck final : public v8::internal::RegExpVisitor {
   }
   void* VisitDisjunction(v8::internal::RegExpDisjunction* node,
                          void*) override {
+    uint8_t padding[FRAME_PADDING];
+    dummy_ = padding; 
     AutoCheckRecursionLimit recursion(cx_);
-    if (!recursion.checkWithExtraDontReport(cx_, FRAME_PADDING)) {
+    if (!recursion.checkDontReport(cx_)) {
       return nullptr;
     }
     for (auto* child : *node->alternatives()) {
@@ -476,8 +484,10 @@ class RegExpDepthCheck final : public v8::internal::RegExpVisitor {
   }
   void* VisitClassSetExpression(v8::internal::RegExpClassSetExpression* node,
                                 void*) override {
+    uint8_t padding[FRAME_PADDING];
+    dummy_ = padding; 
     AutoCheckRecursionLimit recursion(cx_);
-    if (!recursion.checkWithExtraDontReport(cx_, FRAME_PADDING)) {
+    if (!recursion.checkDontReport(cx_)) {
       return nullptr;
     }
     for (auto* child : *node->operands()) {
@@ -490,6 +500,7 @@ class RegExpDepthCheck final : public v8::internal::RegExpVisitor {
 
  private:
   JSContext* cx_;
+  void* dummy_ = nullptr;
 
   
   
@@ -783,7 +794,8 @@ bool CompilePattern(JSContext* cx, MutableHandleRegExpShared re,
   
   RegExpDepthCheck depthCheck(cx);
   if (!depthCheck.check(data.tree)) {
-    ReportOverRecursed(cx);
+    JS_ReportErrorASCII(cx, "regexp too big");
+    cx->reportResourceExhaustion();
     return false;
   }
 
