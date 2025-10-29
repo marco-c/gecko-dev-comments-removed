b07f90cc4a15dee28df53c9d30175b45130a709d
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-10-29 18:07:50 +0000

    Bug 1990028 - Part 1: Changes to settings fragments to enable scrollToPreference in navigation r=android-reviewers,petru
    Differential Revision: https://phabricator.services.mozilla.com/D267853
    
    UltraBlame original commit: 9b097a4e61b9109732c3d0862e869adf4427a62c

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/settings/CustomizationFragment.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/settings/CustomizationFragment.kt
@@ -8,6 +8,7 @@ import android.os.Build
 import android.os.Build.VERSION.SDK_INT
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.navigation.fragment.navArgs
 import androidx.preference.Preference
 import androidx.preference.PreferenceCategory
 import androidx.preference.PreferenceFragmentCompat
@@ -38,6 +39,7 @@ class CustomizationFragment : PreferenceFragmentCompat() {
     private lateinit var radioDarkTheme: RadioButtonPreference
     private lateinit var radioAutoBatteryTheme: RadioButtonPreference
     private lateinit var radioFollowDeviceTheme: RadioButtonPreference
+    private val args by navArgs<CustomizationFragmentArgs>()
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.customization_preferences, rootKey)
@@ -48,6 +50,9 @@ class CustomizationFragment : PreferenceFragmentCompat() {
     override fun onResume() {
         super.onResume()
         showToolbar(getString(R.string.preferences_customize))
+        args.preferenceToScrollTo?.let {
+            scrollToPreference(it)
+        }
     }
 
     private fun setupPreferences() {
@@ -60,6 +65,7 @@ class CustomizationFragment : PreferenceFragmentCompat() {
         updateToolbarCategoryBasedOnTabStrip(tabletAndTabStripEnabled)
         setupTabStripCategory()
         setupToolbarLayout()
+        updateToolbarShortcutBasedOnLayout()
 
         // if tab strip is enabled, swipe toolbar to switch tabs should not be enabled so the
         // preference is not shown
@@ -84,6 +90,21 @@ class CustomizationFragment : PreferenceFragmentCompat() {
         } else {
             setupToolbarCategory()
         }
+    }
+
+    private fun updateToolbarShortcutBasedOnLayout() {
+        val category = requirePreference<PreferenceCategory>(
+            R.string.pref_key_customization_category_toolbar_shortcut,
+        )
+        val settings = requireContext().settings()
+
+        category.isVisible =
+            settings.shouldShowToolbarCustomization &&
+                    Config.channel.isNightlyOrDebug &&
+                    settings.shouldUseComposableToolbar &&
+                    settings.toolbarRedesignEnabled &&
+                    isTallWindow() &&
+                    !settings.shouldUseExpandedToolbar
     }
 
     private fun setupRadioGroups() {
@@ -193,6 +214,11 @@ class CustomizationFragment : PreferenceFragmentCompat() {
         (requirePreference(R.string.pref_key_customization_category_toolbar_layout) as PreferenceCategory).apply {
             isVisible = Config.channel.isNightlyOrDebug && settings.shouldUseComposableToolbar &&
                     settings.toolbarRedesignEnabled && isTallWindow() && !isWideWindow()
+        }
+
+        val layoutToggle = requirePreference<ToggleRadioButtonPreference>(R.string.pref_key_toolbar_expanded)
+        layoutToggle.setOnToggleChanged {
+            updateToolbarShortcutBasedOnLayout()
         }
         updateToolbarLayoutIcons()
     }
