9a9b77dc0efd9cbeef95eb7eb170619fb5c63460
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-11-01 01:34:24 +0000

    Bug 1990634: Part 1 - Fix some FxBackup async and test issues r=mconley
    * Asyncs without awaits for non-async functions
      BackupService.initBackupScheduler and BackupService.uninitBackupScheduler.
    * Missing retryReadonly for backup file delete.
    * More robust handling of issue with Windows kernel briefly preventing
      deletes in tests.
    * test_BackupService_archive was no longer using the correct default directory
      when run on machines with a personal OneDrive folder, since bug 1989660.
    * Moves some exceptions from nsIRequestObserver.onStopRequest (that should
      have been no-ops -- see docs for that method) to
      nsIStreamListener.onDataAvailable (again, see the method's docs), which
      aborts the stream, as was the original intent.
    * Closes a file left open in the archive worker.
    
    Differential Revision: https://phabricator.services.mozilla.com/D267708
    
    UltraBlame original commit: 6fd22e6733b095126d22fd93507f99e16dd94aa0

--- browser/components/backup/tests/xpcshell/test_BackupService_resolveArchiveDestFolderPath.js
+++ browser/components/backup/tests/xpcshell/test_BackupService_resolveArchiveDestFolderPath.js
@@ -3,10 +3,6 @@
 
 "use strict";
 
-const { AppConstants } = ChromeUtils.importESModule(
-  "resource://gre/modules/AppConstants.sys.mjs"
-);
-
 const HOME_KEY = "Home";
 let gTestRoot;
 let gFakeHomePath;
