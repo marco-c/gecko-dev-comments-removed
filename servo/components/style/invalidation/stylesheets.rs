d77761369b137db26093188e644f0d556ce4510f
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-11-01 01:30:01 +0000

    Bug 1995774: Consider parent rules on nested declaration mutation. r=firefox-style-system-reviewers,jwatt,emilio
    Differential Revision: https://phabricator.services.mozilla.com/D269646
    
    UltraBlame original commit: 820d67bec1016ef4fbb09b9bb1688502b218eda3

--- servo/components/style/invalidation/stylesheets.rs
+++ servo/components/style/invalidation/stylesheets.rs
@@ -15,7 +15,7 @@ use crate::media_queries::Device;
 use crate::selector_parser::{SelectorImpl, Snapshot, SnapshotMap};
 use crate::shared_lock::SharedRwLockReadGuard;
 use crate::simple_buckets_map::SimpleBucketsMap;
-use crate::stylesheets::{CssRule, StylesheetInDocument};
+use crate::stylesheets::{CssRule, CssRuleRef, StylesheetInDocument};
 use crate::stylesheets::{EffectiveRules, EffectiveRulesIterator};
 use crate::values::AtomIdent;
 use crate::LocalName as SelectorLocalName;
@@ -153,6 +153,8 @@ impl StylesheetInvalidationSet {
                 device,
                 quirks_mode,
                  false,
+                
+                &[],
             );
             if self.fully_invalid {
                 break;
@@ -525,6 +527,7 @@ impl StylesheetInvalidationSet {
         device: &Device,
         quirks_mode: QuirksMode,
         change_kind: RuleChangeKind,
+        ancestors: &[CssRuleRef],
     ) where
         S: StylesheetInDocument,
     {
@@ -538,17 +541,34 @@ impl StylesheetInvalidationSet {
             return; 
         }
 
+        if ancestors
+            .iter()
+            .any(|r| !EffectiveRules::is_effective(guard, device, quirks_mode, r))
+        {
+            debug!(" > Ancestor rules not effective");
+            return;
+        }
+
         
         
         
         
         let is_generic_change = change_kind == RuleChangeKind::Generic;
-        self.collect_invalidations_for_rule(rule, guard, device, quirks_mode, is_generic_change);
+        self.collect_invalidations_for_rule(
+            rule,
+            guard,
+            device,
+            quirks_mode,
+            is_generic_change,
+            ancestors,
+        );
         if self.fully_invalid {
             return;
         }
 
-        if !is_generic_change && !EffectiveRules::is_effective(guard, device, quirks_mode, rule) {
+        if !is_generic_change
+            && !EffectiveRules::is_effective(guard, device, quirks_mode, &rule.into())
+        {
             return;
         }
 
@@ -560,6 +580,8 @@ impl StylesheetInvalidationSet {
                 device,
                 quirks_mode,
                  false,
+                
+                &[],
             );
             if self.fully_invalid {
                 break;
@@ -575,6 +597,7 @@ impl StylesheetInvalidationSet {
         device: &Device,
         quirks_mode: QuirksMode,
         is_generic_change: bool,
+        ancestors: &[CssRuleRef],
     ) {
         use crate::stylesheets::CssRule::*;
         debug!("StylesheetInvalidationSet::collect_invalidations_for_rule");
@@ -600,7 +623,9 @@ impl StylesheetInvalidationSet {
                 }
             },
             NestedDeclarations(..) => {
-                
+                if ancestors.iter().any(|r| matches!(r, CssRuleRef::Scope(_))) {
+                    self.fully_invalid = true;
+                }
             },
             Namespace(..) => {
                 
