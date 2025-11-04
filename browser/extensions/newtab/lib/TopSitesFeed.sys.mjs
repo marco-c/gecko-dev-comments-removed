76cde04df22b9700ece94c13b80a5e16e3a65fb2
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-11-04 00:09:46 +0000

    Revert "Bug 1997658 - Newtab remove discovery stream spocs from shortcuts feed r=maxx" for causing bc failures browser_no_startup_actions.js.
    This reverts commit 2c8d2fcaa31710e5194ac8ad7a326af1ec87da4e.
    
    UltraBlame original commit: c6aaeee5e3e419884b4f7430b73db320c2d83739

--- browser/extensions/newtab/lib/TopSitesFeed.sys.mjs
+++ browser/extensions/newtab/lib/TopSitesFeed.sys.mjs
@@ -2095,7 +2095,6 @@ export class TopSitesFeed {
         this.updateCustomSearchShortcuts(true /* isStartup */);
         break;
       case at.DISCOVERY_STREAM_DEV_SYSTEM_TICK:
-      case at.DISCOVERY_STREAM_DEV_EXPIRE_CACHE:
       case at.SYSTEM_TICK:
         this.refresh({ broadcast: false });
         this._contile.periodicUpdate();
@@ -2178,6 +2177,10 @@ export class TopSitesFeed {
         break;
       case at.ADS_UPDATE_TILES:
         this._contile.refresh();
+        break;
+      case at.DISCOVERY_STREAM_SPOCS_UPDATE:
+        // Refresh to update sponsored topsites.
+        this.refresh({ broadcast: true, isStartup: action.meta.isStartup });
         break;
       case at.UNINIT:
         this.uninit();
