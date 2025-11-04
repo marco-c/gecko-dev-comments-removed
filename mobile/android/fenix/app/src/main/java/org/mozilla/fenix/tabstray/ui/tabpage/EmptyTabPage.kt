3ee3563cb118fef3ab349ed6452bd8614644b1fa
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-11-04 00:12:22 +0000

    Bug 1995950 - [Tab Management Phase 1] Tabs tray should not be scrollable when the current tab list is empty. r=android-reviewers,calu,007
    Differential Revision: https://phabricator.services.mozilla.com/D269966
    
    UltraBlame original commit: 5f9ca6356f2edbbbe4d688c6a6e1674471dbbb9d

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/tabstray/ui/tabpage/EmptyTabPage.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/tabstray/ui/tabpage/EmptyTabPage.kt
@@ -9,8 +9,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
@@ -43,8 +41,7 @@ internal fun EmptyTabPage(
 
     Box(
         modifier = modifier
-            .fillMaxSize()
-            .verticalScroll(rememberScrollState()),
+            .fillMaxSize(),
         contentAlignment = if (isLandscape) {
             Alignment.Center
         } else {
