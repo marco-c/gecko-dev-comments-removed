00db43d6a3f3cae44ef27c4bfda11348933152fc
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-11-05 19:24:36 +0000

    Bug 1997375 - Add UI automated test for marketing fallback. r=android-reviewers,petru
    Differential Revision: https://phabricator.services.mozilla.com/D270703
    
    UltraBlame original commit: 1609baeea447af276dbe81488a2fe5a2b21fe64e

--- mobile/android/fenix/app/src/androidTest/java/org/mozilla/fenix/ui/AppLinksTest.kt
+++ mobile/android/fenix/app/src/androidTest/java/org/mozilla/fenix/ui/AppLinksTest.kt
@@ -4,7 +4,14 @@
 
 package org.mozilla.fenix.ui
 
+import android.app.Instrumentation
+import android.content.Intent
 import androidx.compose.ui.test.junit4.AndroidComposeTestRule
+import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.Intents.intending
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasAction
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasDataString
+import org.hamcrest.Matchers.equalTo
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -28,6 +35,7 @@ class AppLinksTest : TestSetup() {
     private val intentSchemaUrlLink = itemContainingText("Intent schema link")
     private val phoneUrlLink = itemContainingText("Telephone link")
     private val formRedirectLink = itemContainingText("Telephone post navigation link")
+    private val intentSchemeWithExampleAppLink = itemContainingText("Example app link")
 
     private val phoneSchemaLink = "tel://1234567890"
 
@@ -235,6 +243,22 @@ class AppLinksTest : TestSetup() {
         }.enterURLAndEnterToBrowser(externalLinksPage.url) {
             clickPageObject(formRedirectLink)
             verifyOpenLinkInAnotherAppPrompt(appName = "Phone")
+        }
+    }
+
+    @Test
+    fun marketingIntentWhenOpeningLinkWithoutApp() {
+        // Use ACTION_DIAL as a non-ACTION_VIEW intent to verify that the marketing flow always
+        // launches with ACTION_VIEW instead of reusing the original intent action.
+        intending(hasAction(Intent.ACTION_DIAL)).respondWith(Instrumentation.ActivityResult(0, null))
+
+        navigationToolbar {
+        }.enterURLAndEnterToBrowser(externalLinksPage.url) {
+            clickPageObject(intentSchemeWithExampleAppLink)
+            clickPageObject(itemWithResIdAndText("android:id/button1", "Open"))
+            mDevice.waitForIdle()
+            intended(hasAction(Intent.ACTION_VIEW))
+            intended(hasDataString(equalTo("market://details?id=com.example.app")))
         }
     }
 }
