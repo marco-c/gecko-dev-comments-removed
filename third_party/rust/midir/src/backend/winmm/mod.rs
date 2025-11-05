218502c18d0615a048a7c3ab273c4210f1638e4b
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-11-05 19:24:58 +0000

    Bug 1998036 - chore(midi): upgrade `midir` fork base 0.10.1 → 0.10.3 r=gsvelto,supply-chain-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D271155
    
    UltraBlame original commit: 50943e795b89149a969f6ee5ac7ad36cab3582c8

--- third_party/rust/midir/src/backend/winmm/mod.rs
+++ third_party/rust/midir/src/backend/winmm/mod.rs
@@ -654,10 +654,10 @@ impl MidiOutputConnection {
             }
 
             
-            let packet: u32 = 0;
-            let ptr = &packet as *const u32 as *mut u8;
-            for i in 0..nbytes {
-                unsafe { *ptr.offset(i as isize) = message[i] };
+            let mut packet: u32 = 0;
+            let ptr = std::ptr::addr_of_mut!(packet).cast::<u8>();
+            for (i, item) in message.iter().enumerate().take(nbytes) {
+                unsafe { *ptr.add(i) = *item };
             }
 
             
