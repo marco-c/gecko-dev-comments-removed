6ac34267669b36211c50a72d4897d2df966b52e3
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-11-07 18:14:56 +0000

    Bug 1997975 - Part 3: Align LModI/LUMod with LDivI/LUDiv on arm64. r=spidermonkey-reviewers,iain
    Align the code structure for modulus instructions to be more similar to the
    corresponding division instructions.
    
    Differential Revision: https://phabricator.services.mozilla.com/D271104
    
    UltraBlame original commit: 5c8af0a1a86df9922291a167e240e6243feb3550

--- js/src/jit/arm64/CodeGenerator-arm64.cpp
+++ js/src/jit/arm64/CodeGenerator-arm64.cpp
@@ -641,39 +641,39 @@ void CodeGenerator::visitUDivConstantI(LUDivConstantI* ins) {
 }
 
 void CodeGenerator::visitModI(LModI* ins) {
-  ARMRegister lhs = toWRegister(ins->lhs());
-  ARMRegister rhs = toWRegister(ins->rhs());
-  ARMRegister output = toWRegister(ins->output());
+  Register lhs = ToRegister(ins->lhs());
+  Register rhs = ToRegister(ins->rhs());
+
+  ARMRegister lhs32 = toWRegister(ins->lhs());
+  ARMRegister rhs32 = toWRegister(ins->rhs());
+  ARMRegister output32 = toWRegister(ins->output());
   Label done;
 
   MMod* mir = ins->mir();
 
   
   if (mir->canBeDivideByZero()) {
-    if (mir->isTruncated()) {
-      if (mir->trapOnError()) {
-        Label nonZero;
-        masm.Cbnz(rhs, &nonZero);
-        masm.wasmTrap(wasm::Trap::IntegerDivideByZero, mir->trapSiteDesc());
-        masm.bind(&nonZero);
-      } else {
-        
-        masm.Mov(output, rhs);
-        masm.Cbz(rhs, &done);
-      }
+    if (mir->trapOnError()) {
+      Label nonZero;
+      masm.Cbnz(rhs32, &nonZero);
+      masm.wasmTrap(wasm::Trap::IntegerDivideByZero, mir->trapSiteDesc());
+      masm.bind(&nonZero);
+    } else if (mir->isTruncated()) {
+      
+      masm.Mov(output32, wzr);
+      masm.Cbz(rhs32, &done);
     } else {
       
-      MOZ_ASSERT(!gen->compilingWasm());
-      masm.Cmp(rhs, Operand(0));
-      bailoutIf(Assembler::Equal, ins->snapshot());
+      MOZ_ASSERT(mir->fallible());
+      bailoutTest32(Assembler::Zero, rhs, rhs, ins->snapshot());
     }
   }
 
   
-  masm.Sdiv(output, lhs, rhs);
+  masm.Sdiv(output32, lhs32, rhs32);
 
   
-  masm.Msub(output, output, rhs, lhs);
+  masm.Msub(output32, output32, rhs32, lhs32);
 
   if (mir->canBeNegativeDividend() && !mir->isTruncated()) {
     
@@ -681,7 +681,7 @@ void CodeGenerator::visitModI(LModI* ins) {
     
     
     
-    masm.Cbnz(output, &done);
+    masm.Cbnz(output32, &done);
     bailoutCmp32(Assembler::LessThan, lhs, Imm32(0), ins->snapshot());
   }
 
@@ -1711,36 +1711,37 @@ void CodeGenerator::visitUDiv(LUDiv* ins) {
 }
 
 void CodeGenerator::visitUMod(LUMod* ins) {
-  MMod* mir = ins->mir();
-  ARMRegister lhs = toWRegister(ins->lhs());
-  ARMRegister rhs = toWRegister(ins->rhs());
-  ARMRegister output = toWRegister(ins->output());
+  Register rhs = ToRegister(ins->rhs());
+  Register output = ToRegister(ins->output());
+
+  ARMRegister lhs32 = toWRegister(ins->lhs());
+  ARMRegister rhs32 = toWRegister(ins->rhs());
+  ARMRegister output32 = toWRegister(ins->output());
   Label done;
 
+  MMod* mir = ins->mir();
+
   if (mir->canBeDivideByZero()) {
-    if (mir->isTruncated()) {
-      if (mir->trapOnError()) {
-        Label nonZero;
-        masm.Cbnz(rhs, &nonZero);
-        masm.wasmTrap(wasm::Trap::IntegerDivideByZero, mir->trapSiteDesc());
-        masm.bind(&nonZero);
-      } else {
-        
-        masm.Mov(output, rhs);
-        masm.Cbz(rhs, &done);
-      }
+    if (mir->trapOnError()) {
+      Label nonZero;
+      masm.Cbnz(rhs32, &nonZero);
+      masm.wasmTrap(wasm::Trap::IntegerDivideByZero, mir->trapSiteDesc());
+      masm.bind(&nonZero);
+    } else if (mir->isTruncated()) {
+      
+      masm.Mov(output32, wzr);
+      masm.Cbz(rhs32, &done);
     } else {
       
-      masm.Cmp(rhs, Operand(0));
-      bailoutIf(Assembler::Equal, ins->snapshot());
+      bailoutTest32(Assembler::Zero, rhs, rhs, ins->snapshot());
     }
   }
 
   
-  masm.Udiv(output, lhs, rhs);
+  masm.Udiv(output32, lhs32, rhs32);
 
   
-  masm.Msub(output, output, rhs, lhs);
+  masm.Msub(output32, output32, rhs32, lhs32);
 
   if (!mir->isTruncated()) {
     
