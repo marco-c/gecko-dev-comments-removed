6921c73dcc613e7f4bcf36a3a1df786511ede12d
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-11-07 18:12:19 +0000

    Bug 1997383 - Ensure that the backup service widget's have an ownerGlobal to avoid crashes during shutdown. r=cdupuis
    Differential Revision: https://phabricator.services.mozilla.com/D271237
    
    UltraBlame original commit: 0b00edf8c10b923e28d6e361d2aad2b5f467dfb1

--- browser/components/backup/actors/BackupUIChild.sys.mjs
+++ browser/components/backup/actors/BackupUIChild.sys.mjs
@@ -153,20 +153,22 @@ export class BackupUIChild extends JSWindowActorChild {
         this.#inittedWidgets
       );
       for (let widget of widgets) {
-        if (widget.isConnected) {
-          const state = Cu.cloneInto(message.data.state, widget.ownerGlobal);
-
-          const waivedWidget = Cu.waiveXrays(widget);
-          waivedWidget.backupServiceState = state;
-          //dispatch the event for the React listeners
-          widget.dispatchEvent(
-            new this.contentWindow.CustomEvent("BackupUI:StateWasUpdated", {
-              bubbles: true,
-              composed: true,
-              detail: { state },
-            })
-          );
+        if (!widget.isConnected || !widget.ownerGlobal) {
+          continue;
         }
+
+        const state = Cu.cloneInto(message.data.state, widget.ownerGlobal);
+
+        const waivedWidget = Cu.waiveXrays(widget);
+        waivedWidget.backupServiceState = state;
+        //dispatch the event for the React listeners
+        widget.dispatchEvent(
+          new this.contentWindow.CustomEvent("BackupUI:StateWasUpdated", {
+            bubbles: true,
+            composed: true,
+            detail: { state },
+          })
+        );
       }
     }
   }
