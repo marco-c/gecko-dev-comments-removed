cfc65b5047c201a207c100b0edf21dac945ff406
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-11-07 18:22:11 +0000

    Bug 1995169 [wpt PR 55532] - [WebDriver] navigate_to with a file URL should try loading a file, a=testonly
    Automatic update from web-platform-tests
    [WebDriver] navigate_to with a file URL should try loading a file (#55532)
    
    Previously it attempted to load a the document root of the server — a
    directory — which only works if the browser actually supports loading
    a directory. This doesn't work in all browsers, and there's nothing to
    suggest it should work in all browsers.
    
    This therefore moves us to loading an actual file.
    
    While we're at it, this gets us out of the business of implementing
    file URL serialization, by relying on pathlib to do it for us.
    --
    
    wpt-commits: ff9e8bddd38ec21b20d1d3d83e3e0607693f7005
    wpt-pr: 55532
    
    UltraBlame original commit: 9c1fbedd98aa5393cc7a73e5a179ac16253dc41b

--- testing/web-platform/tests/webdriver/tests/classic/navigate_to/file.py
+++ testing/web-platform/tests/webdriver/tests/classic/navigate_to/file.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 from tests.support.asserts import assert_success
 
 
@@ -10,15 +12,17 @@ def navigate_to(session, url):
 def test_file_protocol(session, target_platform, server_config):
     
     
-    path = server_config["doc_root"]
-    if target_platform == "windows":
-        
-        path = "/{}".format(path.replace("\\", "/"))
-    url = u"file://{}".format(path)
+    path = Path(server_config["doc_root"]) / "common" / "blank.html"
+
+    
+    
+    assert path.is_file()
+
+    
+    url = path.as_uri()
+    assert url.startswith("file://")
 
     response = navigate_to(session, url)
     assert_success(response)
 
-    if session.url.endswith('/'):
-        url += '/'
     assert session.url == url
