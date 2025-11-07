e2da0f118dde6e18c7646e2a3e82a950e7e16a66
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-11-07 18:31:00 +0000

    No Bug, mozilla-central repo-update HSTS HPKP remote-settings tld-suffixes mobile-experiments ct-logs - r=release-managers,RyanVM
    Differential Revision: https://phabricator.services.mozilla.com/D271619
    
    UltraBlame original commit: 774bcb0dce0deabb460694c7c5bd6cb52b66234d

--- security/ct/CTKnownLogs.h
+++ security/ct/CTKnownLogs.h
@@ -14,7 +14,7 @@
 
 #include <stddef.h>
 
-static const PRTime kCTExpirationTime = INT64_C(1768226955000000);
+static const PRTime kCTExpirationTime = INT64_C(1768493606000000);
 
 namespace mozilla::ct {
 
