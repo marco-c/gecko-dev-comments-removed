d5af595972169e0b6d327760bbeff68f25b7f7e7
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-11-07 18:10:54 +0000

    Bug 1988955 - [wdspec] Update mozilla specific test for max total size with request dataType r=whimboo
    Differential Revision: https://phabricator.services.mozilla.com/D267829
    
    UltraBlame original commit: cfdcf3c90ea1f756eb76f9176ea66e06d9130269

--- testing/web-platform/mozilla/tests/webdriver/bidi/network/add_data_collector/max_total_data_size.py
+++ testing/web-platform/mozilla/tests/webdriver/bidi/network/add_data_collector/max_total_data_size.py
@@ -1,34 +1,89 @@
 import pytest
+import pytest_asyncio
 from tests.bidi.network import (
+    BEFORE_REQUEST_SENT_EVENT,
     PAGE_EMPTY_HTML,
     RESPONSE_COMPLETED_EVENT,
 )
 from webdriver.bidi import error
+
+MAX_TOTAL_SIZE = 1000
+
+
+big_data = MAX_TOTAL_SIZE - 100
+half_size_data = int(MAX_TOTAL_SIZE / 2)
+max_size_data = MAX_TOTAL_SIZE
+small_data = int(MAX_TOTAL_SIZE / 100)
+too_big_data = MAX_TOTAL_SIZE + 100
+too_big_one_byte_data = MAX_TOTAL_SIZE + 1
+
+
+@pytest_asyncio.fixture
+async def send_request(wait_for_event, inline, fetch, wait_for_future_safe):
+    
+    mode_flip = False
+
+    async def _send_request(size, mode):
+        nonlocal mode_flip
+
+        
+        
+        if mode == "request or response":
+            mode_flip = not mode_flip
+            data_type = "request" if mode_flip else "response"
+        else:
+            data_type = mode
+
+        data = "".join("A" for i in range(size))
+        if data_type == "request":
+            post_data = data
+            response_data = ""
+        elif data_type == "response":
+            response_data = data
+            post_data = None
+
+        on_response_completed = wait_for_event(RESPONSE_COMPLETED_EVENT)
+        
+        
+        await fetch(url=inline(response_data, doctype="js"), post_data=post_data)
+        event = await wait_for_future_safe(on_response_completed)
+
+        
+        
+        return {"request": event["request"]["request"], "data_type": data_type}
+
+    return _send_request
 
 
 @pytest.mark.capabilities(
     {
         "moz:firefoxOptions": {
             "prefs": {
-                "remote.network.maxTotalDataSize": 1000,
+                "remote.network.maxTotalDataSize": MAX_TOTAL_SIZE,
             },
         },
     }
 )
+@pytest.mark.parametrize(
+    "mode",
+    [
+        "request",
+        "response",
+        "request or response",
+    ],
+)
 @pytest.mark.asyncio
 async def test_max_total_data_size(
     bidi_session,
-    url,
-    inline,
     setup_network_test,
     top_context,
-    wait_for_event,
-    wait_for_future_safe,
     add_data_collector,
-    fetch,
+    send_request,
+    mode,
 ):
     await setup_network_test(
         events=[
+            BEFORE_REQUEST_SENT_EVENT,
             RESPONSE_COMPLETED_EVENT,
         ]
     )
@@ -41,38 +96,19 @@ async def test_max_total_data_size(
 
     
     await add_data_collector(
-        collector_type="blob", data_types=["response"], max_encoded_data_size=1000
+        collector_type="blob",
+        data_types=["request", "response"],
+        max_encoded_data_size=MAX_TOTAL_SIZE,
     )
 
     
-    big_response = "".join("A" for i in range(900))
-    big_url = inline(big_response, doctype="js")
-
-    
-    small_response = "".join("A" for i in range(10))
-    small_url = inline(small_response, doctype="js")
-
-    
-    too_big_response = "".join("A" for i in range(1100))
-    too_big_url = inline(too_big_response, doctype="js")
-
-    
-    
-
-    async def send_request(url):
-        on_response_completed = wait_for_event(RESPONSE_COMPLETED_EVENT)
-        await fetch(url, method="GET")
-        event = await wait_for_future_safe(on_response_completed)
-        return event["request"]["request"]
-
-    
-    request_1_big = await send_request(big_url)
+    request_1_big = await send_request(size=big_data, mode=mode)
 
     await assert_request_data_available(request_1_big, bidi_session)
 
     
     
-    request_2_big = await send_request(big_url)
+    request_2_big = await send_request(size=big_data, mode=mode)
 
     
     await assert_request_data_unavailable(request_1_big, bidi_session)
@@ -80,7 +116,7 @@ async def test_max_total_data_size(
 
     
     
-    request_3_small = await send_request(small_url)
+    request_3_small = await send_request(size=small_data, mode=mode)
 
     
     await assert_request_data_available(request_2_big, bidi_session)
@@ -89,7 +125,7 @@ async def test_max_total_data_size(
     
     
     
-    request_4_big = await send_request(big_url)
+    request_4_big = await send_request(size=big_data, mode=mode)
 
     
     await assert_request_data_unavailable(request_2_big, bidi_session)
@@ -98,7 +134,7 @@ async def test_max_total_data_size(
 
     
     
-    request_5_small = await send_request(small_url)
+    request_5_small = await send_request(size=small_data, mode=mode)
 
     
     await assert_request_data_available(request_3_small, bidi_session)
@@ -110,7 +146,7 @@ async def test_max_total_data_size(
     
     
     
-    request_6_big = await send_request(big_url)
+    request_6_big = await send_request(size=big_data, mode=mode)
 
     
     await assert_request_data_unavailable(request_3_small, bidi_session)
@@ -121,7 +157,7 @@ async def test_max_total_data_size(
     
     
     
-    request_7_too_big = await send_request(too_big_url)
+    request_7_too_big = await send_request(size=too_big_data, mode=mode)
 
     
     await assert_request_data_available(request_5_small, bidi_session)
@@ -130,14 +166,14 @@ async def test_max_total_data_size(
     
     with pytest.raises(error.NoSuchNetworkDataException):
         await bidi_session.network.get_data(
-            request=request_7_too_big,
-            data_type="response",
+            request=request_7_too_big["request"],
+            data_type=request_7_too_big["data_type"],
         )
 
     
-    too_big_one_byte_response = "".join("A" for i in range(1001))
-    too_big_one_byte_url = inline(too_big_one_byte_response, doctype="js")
-    request_8_too_big_one_byte = await send_request(too_big_one_byte_url)
+    request_8_too_big_one_byte = await send_request(
+        size=too_big_one_byte_data, mode=mode
+    )
 
     
     await assert_request_data_available(request_5_small, bidi_session)
@@ -146,14 +182,12 @@ async def test_max_total_data_size(
     
     with pytest.raises(error.NoSuchNetworkDataException):
         await bidi_session.network.get_data(
-            request=request_8_too_big_one_byte,
-            data_type="response",
+            request=request_8_too_big_one_byte["request"],
+            data_type=request_8_too_big_one_byte["data_type"],
         )
 
     
-    max_size_response = "".join("A" for i in range(1000))
-    max_size_url = inline(max_size_response, doctype="js")
-    request_9_max_size = await send_request(max_size_url)
+    request_9_max_size = await send_request(size=max_size_data, mode=mode)
 
     
     await assert_request_data_unavailable(request_5_small, bidi_session)
@@ -161,10 +195,8 @@ async def test_max_total_data_size(
     await assert_request_data_available(request_9_max_size, bidi_session)
 
     
-    half_size_response = "".join("A" for i in range(500))
-    half_size_url = inline(half_size_response, doctype="js")
-    request_10_half_size = await send_request(half_size_url)
-    request_11_half_size = await send_request(half_size_url)
+    request_10_half_size = await send_request(size=half_size_data, mode=mode)
+    request_11_half_size = await send_request(size=half_size_data, mode=mode)
 
     
     await assert_request_data_unavailable(request_9_max_size, bidi_session)
@@ -174,8 +206,8 @@ async def test_max_total_data_size(
 
 async def assert_request_data_available(request, bidi_session):
     data = await bidi_session.network.get_data(
-        request=request,
-        data_type="response",
+        request=request["request"],
+        data_type=request["data_type"],
     )
     assert isinstance(data["value"], str)
 
@@ -183,6 +215,6 @@ async def assert_request_data_available(request, bidi_session):
 async def assert_request_data_unavailable(request, bidi_session):
     with pytest.raises(error.UnavailableNetworkDataException):
         await bidi_session.network.get_data(
-            request=request,
-            data_type="response",
+            request=request["request"],
+            data_type=request["data_type"],
         )
