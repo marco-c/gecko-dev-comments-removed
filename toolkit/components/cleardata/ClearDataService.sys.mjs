0fefbac77d9d54f54c656d82b79bf17420d0a298
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-11-07 18:31:01 +0000

    Bug 1998435 - Do not reset the default zoom when clearing the FFP state. r=manuel
    Differential Revision: https://phabricator.services.mozilla.com/D271446
    
    UltraBlame original commit: a7700f6a3583f49dcceedbc325b0c85efa3625eb

--- toolkit/components/cleardata/ClearDataService.sys.mjs
+++ toolkit/components/cleardata/ClearDataService.sys.mjs
@@ -438,11 +438,7 @@ const CookieBannerExecutedRecordCleaner = {
 
 // A cleaner for cleaning fingerprinting protection states.
 const FingerprintingProtectionStateCleaner = {
-  async _maybeClearSiteSpecificZoom(
-    deleteAll,
-    aSchemelessSite,
-    aOriginAttributes = {}
-  ) {
+  async _maybeClearSiteSpecificZoom(aSchemelessSite, aOriginAttributes = {}) {
     if (
       !ChromeUtils.shouldResistFingerprinting("SiteSpecificZoom", null, true)
     ) {
@@ -455,8 +451,24 @@ const FingerprintingProtectionStateCleaner = {
     const ZOOM_PREF_NAME = "browser.content.full-zoom";
 
     await new Promise((aResolve, aReject) => {
-      if (deleteAll) {
-        cps2.removeByName(ZOOM_PREF_NAME, null, {
+      aOriginAttributes =
+        ChromeUtils.fillNonDefaultOriginAttributes(aOriginAttributes);
+
+      let loadContext;
+      if (
+        aOriginAttributes.privateBrowsingId ==
+        Services.scriptSecurityManager.DEFAULT_PRIVATE_BROWSING_ID
+      ) {
+        loadContext = Cu.createLoadContext();
+      } else {
+        loadContext = Cu.createPrivateLoadContext();
+      }
+
+      cps2.removeBySubdomainAndName(
+        aSchemelessSite,
+        ZOOM_PREF_NAME,
+        loadContext,
+        {
           handleCompletion: aReason => {
             if (aReason === cps2.COMPLETE_ERROR) {
               aReject();
@@ -464,50 +476,19 @@ const FingerprintingProtectionStateCleaner = {
               aResolve();
             }
           },
-        });
-      } else {
-        aOriginAttributes =
-          ChromeUtils.fillNonDefaultOriginAttributes(aOriginAttributes);
-
-        let loadContext;
-        if (
-          aOriginAttributes.privateBrowsingId ==
-          Services.scriptSecurityManager.DEFAULT_PRIVATE_BROWSING_ID
-        ) {
-          loadContext = Cu.createLoadContext();
-        } else {
-          loadContext = Cu.createPrivateLoadContext();
         }
-
-        cps2.removeBySubdomainAndName(
-          aSchemelessSite,
-          ZOOM_PREF_NAME,
-          loadContext,
-          {
-            handleCompletion: aReason => {
-              if (aReason === cps2.COMPLETE_ERROR) {
-                aReject();
-              } else {
-                aResolve();
-              }
-            },
-          }
-        );
-      }
+      );
     });
   },
 
   async deleteAll() {
     Services.rfp.cleanAllRandomKeys();
-
-    await this._maybeClearSiteSpecificZoom(true);
   },
 
   async deleteByPrincipal(aPrincipal) {
     Services.rfp.cleanRandomKeyByPrincipal(aPrincipal);
 
     await this._maybeClearSiteSpecificZoom(
-      false,
       aPrincipal.host,
       aPrincipal.originAttributes
     );
@@ -520,7 +501,6 @@ const FingerprintingProtectionStateCleaner = {
     );
 
     await this._maybeClearSiteSpecificZoom(
-      false,
       aSchemelessSite,
       aOriginAttributesPattern
     );
@@ -532,11 +512,7 @@ const FingerprintingProtectionStateCleaner = {
       JSON.stringify(aOriginAttributesPattern)
     );
 
-    await this._maybeClearSiteSpecificZoom(
-      false,
-      aHost,
-      aOriginAttributesPattern
-    );
+    await this._maybeClearSiteSpecificZoom(aHost, aOriginAttributesPattern);
   },
 
   async deleteByOriginAttributes(aOriginAttributesString) {
