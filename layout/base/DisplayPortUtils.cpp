7787a48ec6b88c309c153e1d335d765feff1f173
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-11-13 00:41:25 +0000

    Bug 1998812. OneStepInAsyncScrollableAncestorChain should stop at menu popups like GetAsyncScrollableAncestorFrame. r=hiro
    Differential Revision: https://phabricator.services.mozilla.com/D271696
    
    UltraBlame original commit: cd11b080a539cd3dee99eff32975672c496f55df

--- layout/base/DisplayPortUtils.cpp
+++ layout/base/DisplayPortUtils.cpp
@@ -834,6 +834,9 @@ bool DisplayPortUtils::MaybeCreateDisplayPort(
 
 nsIFrame* DisplayPortUtils::OneStepInAsyncScrollableAncestorChain(
     nsIFrame* aFrame) {
+  if (aFrame->IsMenuPopupFrame()) {
+    return nullptr;
+  }
   if (aFrame->StyleDisplay()->mPosition == StylePositionProperty::Fixed &&
       nsLayoutUtils::IsReallyFixedPos(aFrame)) {
     if (nsIFrame* root = aFrame->PresShell()->GetRootScrollContainerFrame()) {
