cca8eca96e08d33157bd6967c17d1197fa00cd13
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-11-13 01:10:54 +0000

    Bug 1997328 - Update ipprotection-content element to show onboarding messages based on state - r=ip-protection-reviewers,fluent-reviewers,bolsson,kpatenio
    Differential Revision: https://phabricator.services.mozilla.com/D270912
    
    UltraBlame original commit: d5394febd4fc520fd9fbdac8b560d2714e86c12c

--- browser/components/ipprotection/content/ipprotection-message-bar.mjs
+++ browser/components/ipprotection/content/ipprotection-message-bar.mjs
@@ -3,10 +3,16 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 import { MozLitElement } from "chrome://global/content/lit-utils.mjs";
-import { html } from "chrome://global/content/vendor/lit.all.mjs";
+import { html, ifDefined } from "chrome://global/content/vendor/lit.all.mjs";
 
 // eslint-disable-next-line import/no-unassigned-import
 import "chrome://global/content/elements/moz-message-bar.mjs";
+
+const lazy = {};
+
+ChromeUtils.defineESModuleGetters(lazy, {
+  URILoadingHelper: "resource:///modules/URILoadingHelper.sys.mjs",
+});
 
 /**
  * A custom element that handles the message bar for IP Protection.
@@ -25,12 +31,15 @@ export default class IPProtectionMessageBarElement extends MozLitElement {
   static properties = {
     type: { type: String },
     messageId: { type: String },
+    messageLink: { type: String },
+    messageLinkl10nId: { type: String },
   };
 
   constructor() {
     super();
 
     this.handleDismiss = this.handleDismiss.bind(this);
+    this.handleClickSetingsLink = this.handleClickSettingsLink.bind(this);
   }
 
   connectedCallback() {
@@ -51,18 +60,24 @@ export default class IPProtectionMessageBarElement extends MozLitElement {
 
   genericErrorTemplate() {
     return html`
-      <moz-message-bar
-        type="error"
-        data-l10n-id="ipprotection-message-generic-error"
-        dismissable
-      >
+      <moz-message-bar type="error" data-l10n-id=${this.messageId} dismissable>
       </moz-message-bar>
     `;
   }
 
   infoMessageTemplate() {
     return html`
-      <moz-message-bar type="info" data-l10n-id=${this.messageId} dismissable>
+      <moz-message-bar type="info" dismissable>
+        <span
+          slot="message"
+          data-l10n-id=${this.messageId}
+          @click=${this.handleClickSettingsLink}
+        >
+          <a
+            data-l10n-name=${ifDefined(this.messageLinkl10nId)}
+            href=${ifDefined(this.messageLink)}
+          ></a>
+        </span>
       </moz-message-bar>
     `;
   }
@@ -75,6 +90,17 @@ export default class IPProtectionMessageBarElement extends MozLitElement {
         once: true,
       }
     );
+  }
+
+  handleClickSettingsLink(event) {
+    if (event.target.hasAttribute("href")) {
+      event.preventDefault();
+      lazy.URILoadingHelper.openTrustedLinkIn(window, this.messageLink, "tab");
+
+      this.dispatchEvent(
+        new CustomEvent("IPProtection:Close", { bubbles: true, composed: true })
+      );
+    }
   }
 
   render() {
