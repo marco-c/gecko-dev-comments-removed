7ed49e07442b2ea71116d7775e0114246f4a0efd
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-11-13 00:48:59 +0000

    Bug 1998610 - Set up Firefox Home settings section r=home-newtab-reviewers,fluent-reviewers,bolsson,reemhamz,akulyk,maxx
    Differential Revision: https://phabricator.services.mozilla.com/D269726
    
    UltraBlame original commit: 05f54b787a4d48d4356e7b11d76044c8c3cf0518

--- browser/components/preferences/main.js
+++ browser/components/preferences/main.js
@@ -1177,6 +1177,35 @@ let SETTINGS_CONFIG = {
       },
     ],
   },
+  home: {
+    inProgress: true,
+    headingLevel: 2,
+    l10nId: "home-prefs-content-header",
+    
+    
+    items: [
+      {
+        id: "webSearch",
+        l10nId: "home-prefs-search-header2",
+        control: "moz-toggle",
+      },
+      {
+        id: "weather",
+        l10nId: "home-prefs-weather-header",
+        control: "moz-toggle",
+      },
+      {
+        id: "lists",
+        l10nId: "home-prefs-lists-header",
+        control: "moz-toggle",
+      },
+      {
+        id: "timer",
+        l10nId: "home-prefs-timer-header",
+        control: "moz-toggle",
+      },
+    ],
+  },
   zoom: {
     
     inProgress: true,
