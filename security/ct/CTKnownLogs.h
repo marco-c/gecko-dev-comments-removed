7a9ba7affd2b5daec0ba4211d73e1346adcf8db2
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-11-15 00:30:06 +0000

    No Bug, mozilla-central repo-update HSTS HPKP remote-settings tld-suffixes mobile-experiments ct-logs - r=release-managers,RyanVM
    Differential Revision: https://phabricator.services.mozilla.com/D272428
    
    UltraBlame original commit: 6022aa30e50447bff4dcd9ac2a583c75ff4193a7

--- security/ct/CTKnownLogs.h
+++ security/ct/CTKnownLogs.h
@@ -14,7 +14,7 @@
 
 #include <stddef.h>
 
-static const PRTime kCTExpirationTime = INT64_C(1768820101000000);
+static const PRTime kCTExpirationTime = INT64_C(1769079245000000);
 
 namespace mozilla::ct {
 
--- security/manager/ssl/StaticHPKPins.h
+++ security/manager/ssl/StaticHPKPins.h
@@ -681,4 +681,4 @@ static const TransportSecurityPreload kPublicKeyPinningPreloadList[] = {
 
 static const int32_t kUnknownId = -1;
 
-static const PRTime kPreloadPKPinsExpirationTime = INT64_C(1771239261334000);
+static const PRTime kPreloadPKPinsExpirationTime = INT64_C(1771498415452000);
