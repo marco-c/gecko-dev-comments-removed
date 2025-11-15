093ada4c30d4ddf0a102d12ef299bec6770c96ac
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-11-15 00:18:54 +0000

    Bug 1995393 - Vendor libwebrtc from 2f553bf8d5
    Upstream commit: https://webrtc.googlesource.com/src/+/2f553bf8d573c90176d51559b76dfc836996c8f5
        Add a default constructor for MockIceController
    
        One-parameter constructor doesn't use the paramater, and thus tests
        shouldn't be burden to construct it.
    
        Requiring tests to construct unused IceControllerFactoryArgs complicates
        extending that struct like was attempted in
        https://webrtc-review.googlesource.com/c/src/+/407100
    
        Bug: webrtc:42223992
        Change-Id: I1d19972331e6d4b1ba3c7f671493126ca4573e50
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/407047
        Reviewed-by: Harald Alvestrand <htawebrtc.org>
        Commit-Queue: Danil Chapovalov <danilchapwebrtc.org>
        Cr-Commit-Position: refs/heads/main{#45520}
    
    UltraBlame original commit: 8020494922c5df43527f5904411c021d55415ea0

--- third_party/libwebrtc/p2p/test/mock_ice_controller.h
+++ third_party/libwebrtc/p2p/test/mock_ice_controller.h
@@ -28,6 +28,7 @@ namespace webrtc {
 
 class MockIceController : public IceControllerInterface {
  public:
+  MockIceController() = default;
   explicit MockIceController(const IceControllerFactoryArgs& ) {}
   ~MockIceController() override = default;
 
@@ -41,21 +42,18 @@ class MockIceController : public IceControllerInterface {
               (const, override));
   MOCK_METHOD(ArrayView<const Connection*>, connections, (), (const, override));
   MOCK_METHOD(bool, HasPingableConnection, (), (const, override));
-  MOCK_METHOD(IceControllerInterface::PingResult,
-              SelectConnectionToPing,
-              (int64_t),
-              (override));
+  MOCK_METHOD(PingResult, SelectConnectionToPing, (int64_t), (override));
   MOCK_METHOD(bool,
               GetUseCandidateAttr,
               (const Connection*, NominationMode, IceMode),
               (const, override));
   MOCK_METHOD(const Connection*, FindNextPingableConnection, (), (override));
   MOCK_METHOD(void, MarkConnectionPinged, (const Connection*), (override));
-  MOCK_METHOD(IceControllerInterface::SwitchResult,
+  MOCK_METHOD(SwitchResult,
               ShouldSwitchConnection,
               (IceSwitchReason, const Connection*),
               (override));
-  MOCK_METHOD(IceControllerInterface::SwitchResult,
+  MOCK_METHOD(SwitchResult,
               SortAndSwitchConnection,
               (IceSwitchReason),
               (override));
@@ -67,9 +65,9 @@ class MockIceControllerFactory : public IceControllerFactoryInterface {
   ~MockIceControllerFactory() override = default;
 
   std::unique_ptr<IceControllerInterface> Create(
-      const IceControllerFactoryArgs& args) override {
+      const IceControllerFactoryArgs& ) override {
     RecordIceControllerCreated();
-    return std::make_unique<MockIceController>(args);
+    return std::make_unique<MockIceController>();
   }
 
   MOCK_METHOD(void, RecordIceControllerCreated, ());
@@ -77,13 +75,5 @@ class MockIceControllerFactory : public IceControllerFactoryInterface {
 
 }  
 
-
-
-#ifdef WEBRTC_ALLOW_DEPRECATED_NAMESPACES
-namespace cricket {
-using ::webrtc::MockIceController;
-using ::webrtc::MockIceControllerFactory;
-}  
-#endif  
 
 #endif  
