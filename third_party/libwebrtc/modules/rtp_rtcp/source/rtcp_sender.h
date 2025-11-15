d2e5369e7a65e9127a17ed48b519a1d3135209c0
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-11-15 00:09:36 +0000

    Bug 1995393 - Vendor libwebrtc from 1d43877169
    Upstream commit: https://webrtc.googlesource.com/src/+/1d438771695d5fdb0cfef161435d474b2153aa52
        Cleanup RtcpSender from code for legacy RtpRtcp
    
        Convert configuration to RTCP one directly in ModuleRtpRtcp2
        Make scheduling callback required, update all tests to provide it.
    
        Bug: webrtc:42224904
        Change-Id: Ibe6f35ad5093978e4fac73b502f7466b723f8fb6
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/405400
        Commit-Queue: Danil Chapovalov <danilchapwebrtc.org>
        Reviewed-by: Åsa Persson <asaperssonwebrtc.org>
        Cr-Commit-Position: refs/heads/main{#45400}
    
    UltraBlame original commit: 71201710010915724cfb56f568f373f7cdc193e9

--- third_party/libwebrtc/modules/rtp_rtcp/source/rtcp_sender.h
+++ third_party/libwebrtc/modules/rtp_rtcp/source/rtcp_sender.h
@@ -13,7 +13,6 @@
 
 #include <cstddef>
 #include <cstdint>
-#include <functional>
 #include <map>
 #include <memory>
 #include <optional>
@@ -21,6 +20,7 @@
 #include <string>
 #include <vector>
 
+#include "absl/functional/any_invocable.h"
 #include "absl/strings/string_view.h"
 #include "api/array_view.h"
 #include "api/call/transport.h"
@@ -40,7 +40,6 @@
 #include "modules/rtp_rtcp/source/rtcp_packet/report_block.h"
 #include "modules/rtp_rtcp/source/rtcp_packet/tmmb_item.h"
 #include "modules/rtp_rtcp/source/rtcp_receiver.h"
-#include "modules/rtp_rtcp/source/rtp_rtcp_interface.h"
 #include "rtc_base/random.h"
 #include "rtc_base/synchronization/mutex.h"
 #include "rtc_base/thread_annotations.h"
@@ -51,11 +50,6 @@ namespace webrtc {
 class RTCPSender final {
  public:
   struct Configuration {
-    
-    
-    static Configuration FromRtpRtcpConfiguration(
-        const RtpRtcpInterface::Configuration& config);
-
     
     
     bool audio = false;
@@ -69,19 +63,14 @@ class RTCPSender final {
     
     
     bool non_sender_rtt_measurement = false;
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    std::function<void(TimeDelta)> schedule_next_rtcp_send_evaluation_function;
 
-    std::optional<TimeDelta> rtcp_report_interval;
+    
+    
+    
+    
+    absl::AnyInvocable<void(TimeDelta)> schedule_next_rtcp_send_evaluation;
+
+    TimeDelta rtcp_report_interval;
     ReceiveStatisticsProvider* receive_statistics = nullptr;
     RtcpPacketTypeCounterObserver* rtcp_packet_type_counter_observer = nullptr;
   };
@@ -247,10 +236,7 @@ class RTCPSender final {
   Transport* const transport_;
 
   const TimeDelta report_interval_;
-  
-  
-  const std::function<void(TimeDelta)>
-      schedule_next_rtcp_send_evaluation_function_;
+  absl::AnyInvocable<void(TimeDelta)> schedule_next_rtcp_send_evaluation_;
 
   mutable Mutex mutex_rtcp_sender_;
   bool sending_ RTC_GUARDED_BY(mutex_rtcp_sender_);
