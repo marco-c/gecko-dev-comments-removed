c9b1b6dddf459518b17dbaa2bc499fe709f56692
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-11-15 00:37:06 +0000

    Bug 1996818 - Remove the gpu cache. r=gw
    Differential Revision: https://phabricator.services.mozilla.com/D270818
    
    UltraBlame original commit: 0ec224d0acc302e9e596f5adacf4d1027c935bf2

--- gfx/wr/webrender_build/src/shader.rs
+++ gfx/wr/webrender_build/src/shader.rs
@@ -1,11 +1,11 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-
-
-
-
-
-
-
+//! Functionality for managing source code for shaders.
+//!
+//! This module is used during precompilation (build.rs) and regular compilation,
+//! so it has minimal dependencies.
 
 use std::borrow::Cow;
 use std::fs::File;
@@ -34,7 +34,7 @@ pub enum ShaderVersion {
 }
 
 impl ShaderVersion {
-    
+    /// Return the full variant name, for use in code generation.
     pub fn variant_name(&self) -> &'static str {
         match self {
             ShaderVersion::Gl => "ShaderVersion::Gl",
@@ -73,8 +73,8 @@ impl ShaderSourceParser {
         }
     }
 
-    
-    
+    /// Parses a shader string for imports. Imports are recursively processed, and
+    /// prepended to the output stream.
     pub fn parse<F: FnMut(&str), G: Fn(&str) -> Cow<'static, str>>(
         &mut self,
         source: Cow<'static, str>,
@@ -83,7 +83,7 @@ impl ShaderSourceParser {
     ) {
         for line in source.lines() {
             if let Some(imports) = line.strip_prefix(SHADER_IMPORT) {
-                
+                // For each import, get the source, and recurse.
                 for import in imports.split(',') {
                     if self.included.insert(import.into()) {
                         let include = get_source(import);
@@ -100,7 +100,7 @@ impl ShaderSourceParser {
     }
 }
 
-
+/// Reads a shader source file from disk into a String.
 pub fn shader_source_from_file(shader_path: &Path) -> String {
     assert!(shader_path.exists(), "Shader not found {:?}", shader_path);
     let mut source = String::new();
@@ -111,7 +111,7 @@ pub fn shader_source_from_file(shader_path: &Path) -> String {
     source
 }
 
-
+/// Creates heap-allocated strings for both vertex and fragment shaders.
 pub fn build_shader_strings<G: Fn(&str) -> Cow<'static, str>>(
     gl_version: ShaderVersion,
     features: &[&str],
@@ -141,9 +141,9 @@ pub fn build_shader_strings<G: Fn(&str) -> Cow<'static, str>>(
    (vs_source, fs_source)
 }
 
-
-
-
+/// Walks the given shader string and applies the output to the provided
+/// callback. Assuming an override path is not used, does no heap allocation
+/// and no I/O.
 pub fn do_build_shader_string<F: FnMut(&str), G: Fn(&str) -> Cow<'static, str>>(
    gl_version: ShaderVersion,
    features: &[&str],
@@ -156,8 +156,8 @@ pub fn do_build_shader_string<F: FnMut(&str), G: Fn(&str) -> Cow<'static, str>>(
    build_shader_main_string(base_filename, get_source, &mut output);
 }
 
-
-
+/// Walks the prefix section of the shader string, which manages the various
+/// defines for features etc.
 pub fn build_shader_prefix_string<F: FnMut(&str)>(
    gl_version: ShaderVersion,
    features: &[&str],
@@ -165,7 +165,7 @@ pub fn build_shader_prefix_string<F: FnMut(&str)>(
    base_filename: &str,
    output: &mut F,
 ) {
-    
+    // GLSL requires that the version number comes first.
     let gl_version_string = match gl_version {
         ShaderVersion::Gl => "#version 150\n",
         ShaderVersion::Gles if features.contains(&"TEXTURE_EXTERNAL_ESSL1") => "#version 100\n",
@@ -173,7 +173,7 @@ pub fn build_shader_prefix_string<F: FnMut(&str)>(
     };
     output(gl_version_string);
 
-    
+    // Insert the shader name to make debugging easier.
     output("// shader: ");
     output(base_filename);
     output(" ");
@@ -185,18 +185,18 @@ pub fn build_shader_prefix_string<F: FnMut(&str)>(
     }
     output("\n");
 
-    
+    // Define a constant depending on whether we are compiling VS or FS.
     let kind_string = match kind {
         ShaderKind::Vertex => "#define WR_VERTEX_SHADER\n",
         ShaderKind::Fragment => "#define WR_FRAGMENT_SHADER\n",
     };
     output(kind_string);
 
-    
+    // detect which platform we're targeting
     let is_macos = match std::env::var("CARGO_CFG_TARGET_OS") {
         Ok(os) => os == "macos",
-        
-        
+        // if this is not called from build.rs (e.g. if the optimized shader
+        // pref is disabled) we want to use the runtime value
         Err(_) => cfg!(target_os = "macos"),
     };
     let is_android = match std::env::var("CARGO_CFG_TARGET_OS") {
@@ -209,12 +209,12 @@ pub fn build_shader_prefix_string<F: FnMut(&str)>(
         output("#define PLATFORM_ANDROID\n");
     }
 
-    
+    // Define a constant for the vertex texture width.
     output("#define WR_MAX_VERTEX_TEXTURE_WIDTH ");
     output(&MAX_VERTEX_TEXTURE_WIDTH_STRING);
     output("U\n");
 
-    
+    // Add any defines for features that were passed by the caller.
     for feature in features {
         assert!(!feature.is_empty());
         output("#define WR_FEATURE_");
@@ -223,7 +223,7 @@ pub fn build_shader_prefix_string<F: FnMut(&str)>(
     }
 }
 
-
+/// Walks the main .glsl file, including any imports.
 pub fn build_shader_main_string<F: FnMut(&str), G: Fn(&str) -> Cow<'static, str>>(
    base_filename: &str,
    get_source: &G,
