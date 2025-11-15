0967590f3a00472a8b94400bd0d8d29e410d3c10
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-11-15 00:32:24 +0000

    Bug 1984751 - Assert if media playback rate is not finite. r=alwu
    Differential Revision: https://phabricator.services.mozilla.com/D266437
    
    UltraBlame original commit: 63aafea2b35930608b82c3d236b851091f101948

--- dom/media/mediaelement/HTMLMediaElement.cpp
+++ dom/media/mediaelement/HTMLMediaElement.cpp
@@ -188,6 +188,7 @@ static const double MAX_PLAYBACKRATE = 16.0;
 
 static double ClampPlaybackRate(double aPlaybackRate) {
   MOZ_ASSERT(aPlaybackRate >= 0.0);
+  MOZ_ASSERT(std::isfinite(aPlaybackRate));
 
   if (aPlaybackRate == 0.0) {
     return aPlaybackRate;
