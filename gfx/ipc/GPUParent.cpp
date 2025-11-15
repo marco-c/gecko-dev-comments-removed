25ec2061deeb24af5496db89dbe16072971e5856
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-11-15 00:42:18 +0000

    Bug 1999645 - Avoid synchronous GPUChild::SendGetDeviceStatus after GPU process channel is connected asynchronously. r=aosmond
    In the case where the GPU process channel is connected asynchronously
    we unfortunately send a synchronous GPUChild::SendGetDeviceStatus()
    message immediately afterwards. This is a regression from bug 1993758,
    which added a call to GPUChild::EnsureGPUReady() to
    GPUProcessManager::OnProcessLaunchComplete(). Ideally we'd like to
    avoid using this synchronous message unless required, and instead
    allow time for the asynchronous Init()/InitComplete() callback to
    complete.
    
    This patch achieves just that by delaying calling the listener's
    OnProcessLaunchComplete() callback until after the post-connection
    initialization is complete, rather than immediately after the channel
    has connected. This means that by the time the GPUProcessManager calls
    EnsureGPUReady(), initialization will have already completed.
    
    A new "Connected" LaunchPhase is added prior to "Complete", indicating
    that the channel has been connected but post-connection initialization
    has not yet completed. Upon connection we start asynchronous
    initialization as before, but now track its completion using a Promise
    and call OnProcessLaunchComplete when that resolves.
    
    As before, if GPUProcessManager requires the launch to complete
    synchronously it calls GPUProcessHost::WaitForLaunch(). This will now
    additionally ensure initialization has completed prior to returning by
    sending the synchronous SendGetDeviceData message.
    
    Differential Revision: https://phabricator.services.mozilla.com/D272308
    
    UltraBlame original commit: b6a51dc24ebfac43c2db386cffb8a9196e58e920

--- gfx/ipc/GPUParent.cpp
+++ gfx/ipc/GPUParent.cpp
@@ -276,7 +276,8 @@ void GPUParent::NotifyDisableRemoteCanvas() {
 mozilla::ipc::IPCResult GPUParent::RecvInit(
     nsTArray<GfxVarUpdate>&& vars, const DevicePrefs& devicePrefs,
     nsTArray<LayerTreeIdMapping>&& aMappings,
-    nsTArray<GfxInfoFeatureStatus>&& aFeatures, uint32_t aWrNamespace) {
+    nsTArray<GfxInfoFeatureStatus>&& aFeatures, uint32_t aWrNamespace,
+    InitResolver&& aInitResolver) {
   gfxVars::ApplyUpdate(vars);
 
   
@@ -397,7 +398,7 @@ mozilla::ipc::IPCResult GPUParent::RecvInit(
   
   GPUDeviceData data;
   RecvGetDeviceStatus(&data);
-  (void)SendInitComplete(data);
+  aInitResolver(data);
 
   
   
