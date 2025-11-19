d9c32825d1a0d18e470759e852982766145b8bed
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-11-19 00:39:21 +0000

    Bug 1996123 - Permit consecutive Android autoplay permission grants/denials of the same type. r=media-playback-reviewers,aosmond,alwu
    Differential Revision: https://phabricator.services.mozilla.com/D271415
    
    UltraBlame original commit: a401cff785bf8891694fa8beb425823de218c678

--- dom/media/autoplay/GVAutoplayPermissionRequest.cpp
+++ dom/media/autoplay/GVAutoplayPermissionRequest.cpp
@@ -136,9 +136,12 @@ GVAutoplayPermissionRequest::Cancel() {
   
   
   
+  
+  
   const RStatus status = GetRequestStatus(mContext, mType);
   REQUEST_LOG("Cancel, current status=%s", EnumValueToString(status));
-  MOZ_ASSERT(status == RStatus::ePENDING || status == RStatus::eUNKNOWN);
+  MOZ_ASSERT(status == RStatus::ePENDING || status == RStatus::eDENIED ||
+             status == RStatus::eUNKNOWN);
   if ((status == RStatus::ePENDING) && !mContext->IsDiscarded()) {
     SetRequestStatus(RStatus::eDENIED);
   }
@@ -153,9 +156,12 @@ GVAutoplayPermissionRequest::Allow(JS::Handle<JS::Value> aChoices) {
   
   
   
+  
+  
   const RStatus status = GetRequestStatus(mContext, mType);
   REQUEST_LOG("Allow, current status=%s", EnumValueToString(status));
-  MOZ_ASSERT(status == RStatus::ePENDING || status == RStatus::eUNKNOWN);
+  MOZ_ASSERT(status == RStatus::ePENDING || status == RStatus::eALLOWED ||
+             status == RStatus::eUNKNOWN);
   if (status == RStatus::ePENDING) {
     SetRequestStatus(RStatus::eALLOWED);
     
@@ -166,7 +172,6 @@ GVAutoplayPermissionRequest::Allow(JS::Handle<JS::Value> aChoices) {
                             nullptr);
     }
   }
-
   mContext = nullptr;
   return NS_OK;
 }
