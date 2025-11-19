d7f6468e5a71c7c3d6efa66f9132f4edb192a6ad
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-11-19 00:31:33 +0000

    Bug 1972371 - Convert Website Permissions to config-based prefs - r=emz,fluent-reviewers,akulyk,bolsson
    Differential Revision: https://phabricator.services.mozilla.com/D267470
    
    UltraBlame original commit: 4b7ddcbddd888413f23dea3a62c956198e07360e

--- browser/components/preferences/tests/browser_search_within_preferences_1.js
+++ browser/components/preferences/tests/browser_search_within_preferences_1.js
@@ -310,14 +310,10 @@ add_task(async function changing_tabs_after_searching() {
   await searchCompletedPromise;
 
   
-  let permissionsSearchHeader = gBrowser.contentDocument.querySelector(
-    "#permissionsGroup .search-header"
+  let permissionsGroup = gBrowser.contentDocument.querySelector(
+    "moz-fieldset#permissions"
   );
-  is(
-    permissionsSearchHeader.hidden,
-    false,
-    "Permissions search-header should be visible"
-  );
+  is(permissionsGroup.hidden, false, "Permissions group should be visible");
 
   let privacyCategory =
     gBrowser.contentDocument.getElementById("category-privacy");
@@ -332,13 +328,6 @@ add_task(async function changing_tabs_after_searching() {
       is(child.selected, false, "No other panel should be selected");
     }
   }
-
-  
-  is(
-    permissionsSearchHeader.hidden,
-    true,
-    "Permissions search-header should be hidden"
-  );
 
   BrowserTestUtils.removeTab(gBrowser.selectedTab);
 });
