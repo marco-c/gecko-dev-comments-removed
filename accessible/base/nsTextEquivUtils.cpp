15bef76132051fe590d38f9bc25512107f63949d
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-11-20 23:56:40 +0000

    Bug 1998242 - P5: Perform subtree name calculation in RemoteAccessible::Name. r=morgan
    Also make nsTextEquivUtils::GetNameFromSubtree remote friendly.
    
    Depends on D271312
    
    Differential Revision: https://phabricator.services.mozilla.com/D271313
    
    UltraBlame original commit: dc11f174d2b6c961092b115839c37a5d3c44f59f

--- accessible/base/nsTextEquivUtils.cpp
+++ accessible/base/nsTextEquivUtils.cpp
@@ -57,8 +57,8 @@ bool nsTextEquivUtils::HasNameRule(const Accessible* aAccessible,
   return (rule & aRule) == aRule;
 }
 
-nsresult nsTextEquivUtils::GetNameFromSubtree(
-    const LocalAccessible* aAccessible, nsAString& aName) {
+nsresult nsTextEquivUtils::GetNameFromSubtree(const Accessible* aAccessible,
+                                              nsAString& aName) {
   aName.Truncate();
 
   if (GetReferencedAccs().Contains(aAccessible)) {
@@ -71,24 +71,18 @@ nsresult nsTextEquivUtils::GetNameFromSubtree(
   }
   GetReferencedAccs().Insert(aAccessible);
 
-  if (nsIContent* content = aAccessible->GetContent()) {
-    AssociatedElementsIterator iter(aAccessible->Document(), content,
-                                    nsGkAtoms::aria_actions);
-    while (Accessible* actionTarget = iter.Next()) {
-      
-      
-      GetReferencedAccs().Insert(actionTarget);
-    }
+  Relation customActions(aAccessible->RelationByType(RelationType::ACTION));
+  while (Accessible* target = customActions.Next()) {
+    
+    
+    GetReferencedAccs().Insert(target);
   }
 
   if (HasNameRule(aAccessible, eNameFromSubtreeRule)) {
-    
-    if (aAccessible->IsContent()) {
-      nsAutoString name;
-      AppendFromAccessibleChildren(aAccessible, &name);
-      name.CompressWhitespace();
-      if (!nsCoreUtils::IsWhitespaceString(name)) aName = name;
-    }
+    nsAutoString name;
+    AppendFromAccessibleChildren(aAccessible, &name);
+    name.CompressWhitespace();
+    if (!nsCoreUtils::IsWhitespaceString(name)) aName = name;
   }
 
   
@@ -278,6 +272,24 @@ nsresult nsTextEquivUtils::AppendFromAccessible(Accessible* aAccessible,
             aString->Append(char16_t(' '));
           }
         }
+      }
+    }
+  } else if (aAccessible->IsRemote()) {
+    if (aAccessible->IsText()) {
+      
+      nsAutoString name;
+      aAccessible->Name(name);
+      aString->Append(name);
+      return NS_OK;
+    }
+    if (RefPtr<nsAtom>(aAccessible->DisplayStyle()) == nsGkAtoms::block ||
+        aAccessible->IsHTMLListItem() || aAccessible->IsTableRow() ||
+        aAccessible->IsTableCell()) {
+      
+      
+      isHTMLBlock = true;
+      if (!aString->IsEmpty()) {
+        aString->Append(char16_t(' '));
       }
     }
   }
