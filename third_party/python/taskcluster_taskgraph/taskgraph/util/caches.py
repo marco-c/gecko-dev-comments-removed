f9e16ab5bec98ebcac0a3739999bfda80754e4c4
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-11-27 20:01:37 +0000

    Bug 2000944 - Vendor taskcluster-taskgraph v18.0.0, r=taskgraph-reviewers,mach-reviewers,ahochheiden,jcristau
    Differential Revision: https://phabricator.services.mozilla.com/D273898
    
    UltraBlame original commit: b59f6bdfb23790297428f616577ab868a8dc4820

--- third_party/python/taskcluster_taskgraph/taskgraph/util/caches.py
+++ third_party/python/taskcluster_taskgraph/taskgraph/util/caches.py
@@ -37,11 +37,6 @@ def get_checkout_cache_name(config: "TransformConfig", task: dict[str, Any]) -> 
         digest = hashlib.sha256(checkout_paths_str).hexdigest()
         cache_name += f"-repos-{digest}"
 
-    
-    
-    if task["run"]["sparse-profile"]:
-        cache_name += "-sparse"
-
     return cache_name
 
 
