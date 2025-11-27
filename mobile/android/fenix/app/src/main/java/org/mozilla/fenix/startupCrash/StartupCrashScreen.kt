1d8e6fae93273a4881b8c0a5ff0636ba9801cd54
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-11-27 19:33:23 +0000

    Bug 2001345 - Fixed landscape mode for StartupCrashScreen by adding a scroll state. r=android-reviewers,boek
    Differential Revision: https://phabricator.services.mozilla.com/D273463
    
    UltraBlame original commit: 90cfae55c433fd23abf35870101aa89bf7b3115e

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/startupCrash/StartupCrashScreen.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/startupCrash/StartupCrashScreen.kt
@@ -11,11 +11,14 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CircularProgressIndicator
@@ -41,9 +44,14 @@ import mozilla.components.ui.icons.R as iconsR
 @Composable
 internal fun StartupCrashScreen(store: StartupCrashStore) {
     val state by store.observeAsComposableState { it }
+    val scrollState = rememberScrollState()
+
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier.padding(top = 74.dp, bottom = 97.dp, start = 16.dp, end = 16.dp),
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(scrollState)
+            .padding(top = 74.dp, bottom = 97.dp, start = 16.dp, end = 16.dp),
     ) {
         ScreenImg()
 
