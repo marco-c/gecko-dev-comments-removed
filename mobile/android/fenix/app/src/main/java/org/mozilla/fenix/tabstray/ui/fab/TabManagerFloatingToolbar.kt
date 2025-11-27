e2a95b31a7cdbbdc471c4a103de0cc61b99653e9
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-11-27 19:54:29 +0000

    Bug 2002067 - Make the 3-dot menu the same height as the New Tab button. r=android-reviewers,calu
    Differential Revision: https://phabricator.services.mozilla.com/D273987
    
    UltraBlame original commit: 588510b972b32595cecb5e084758f3f7a6c6b9da

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/tabstray/ui/fab/TabManagerFloatingToolbar.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/tabstray/ui/fab/TabManagerFloatingToolbar.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Card
@@ -159,7 +160,7 @@ private fun FloatingToolbarActions(
     )
 
     Card(
-        modifier = Modifier,
+        modifier = Modifier.size(56.dp),
         shape = CircleShape,
         colors = CardDefaults.cardColors(
             containerColor = MaterialTheme.colorScheme.surfaceContainer,
