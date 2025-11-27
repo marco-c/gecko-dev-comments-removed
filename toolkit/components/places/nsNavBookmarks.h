0300186d68a3fcd9ac7af43e96e1676176bc332d
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-11-27 19:05:08 +0000

    Revert "Bug 1846461: Remove FetchItemInfo(guid) from nsNavBookmarks r=places-reviewers,mak" for causing build bustages  nsNavHistoryResult.h
    This reverts commit c7e28148ef9bb237203596e0bc5dbba76e4dbb3c.
    
    Revert "Bug 1846461: Move logic that appends children of folder to nsNavHistoryResult r=places-reviewers,mak"
    
    This reverts commit e236d89e70544d8d4755c46b4a407b3ca599a030.
    
    UltraBlame original commit: 66896bac6f53507286c0570972fe6815969e2973

--- toolkit/components/places/nsNavBookmarks.h
+++ toolkit/components/places/nsNavBookmarks.h
@@ -102,6 +102,46 @@ class nsNavBookmarks final : public nsINavBookmarksService,
                    const nsAString& aLastKnownTitle);
 
   
+  
+  
+  nsresult QueryFolderChildren(int64_t aFolderId,
+                               nsNavHistoryQueryOptions* aOptions,
+                               nsCOMArray<nsNavHistoryResultNode>* children);
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  nsresult ProcessFolderNodeRow(mozIStorageValueArray* aRow,
+                                nsNavHistoryQueryOptions* aOptions,
+                                nsCOMArray<nsNavHistoryResultNode>* aChildren,
+                                int32_t& aCurrentIndex);
+
+  
+
+
+
+
+
+
+
+
+  nsresult QueryFolderChildrenAsync(nsNavHistoryFolderResultNode* aNode,
+                                    mozIStoragePendingStatement** _pendingStmt);
+
+  
 
 
 
@@ -110,6 +150,16 @@ class nsNavBookmarks final : public nsINavBookmarksService,
 
 
   nsresult FetchItemInfo(int64_t aItemId, BookmarkData& _bookmark);
+
+  
+
+
+
+
+
+
+
+  nsresult FetchItemInfo(const nsCString& aGUID, BookmarkData& _bookmark);
 
   
 
