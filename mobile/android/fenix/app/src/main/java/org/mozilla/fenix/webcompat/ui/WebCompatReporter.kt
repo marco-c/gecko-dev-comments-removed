ff1b699eaaaf453ecf48617a73f453640e3d7f3f
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-11-27 19:32:34 +0000

    Bug 1983180 - [WebCompat Reporter v2] Update the background colour to use M3 tokens r=android-reviewers,jdelorenzo
    Differential Revision: https://phabricator.services.mozilla.com/D273319
    
    UltraBlame original commit: 87bd2ca15e2a177ee5123d4ac190881476c8379c

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/webcompat/ui/WebCompatReporter.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/webcompat/ui/WebCompatReporter.kt
@@ -5,6 +5,7 @@
 package org.mozilla.fenix.webcompat.ui
 
 import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -29,6 +30,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -90,6 +92,8 @@ fun WebCompatReporter(
 
     var previewSheetVisible by remember { mutableStateOf(false) }
 
+    val scrollState = rememberScrollState()
+
     BackHandler {
         store.dispatch(WebCompatReporterAction.BackPressed)
     }
@@ -100,12 +104,14 @@ fun WebCompatReporter(
                 onBackClick = {
                     store.dispatch(WebCompatReporterAction.BackPressed)
                 },
+                scrollState = scrollState,
             )
         },
+        containerColor = MaterialTheme.colorScheme.background,
     ) { paddingValues ->
         Column(
             modifier = Modifier
-                .verticalScroll(rememberScrollState())
+                .verticalScroll(scrollState)
                 .padding(paddingValues)
                 .imePadding()
                 .padding(horizontal = 16.dp, vertical = 12.dp),
@@ -256,7 +262,6 @@ fun WebCompatReporter(
                     style = FirefoxTheme.typography.subtitle2,
 
                 )
-
                 Icon(
                     painter = painterResource(R.drawable.ic_arrowhead_right),
                     contentDescription = "",
@@ -356,6 +361,7 @@ private fun WebCompatReporterState.toDropdownItems(
 @Composable
 private fun TempAppBar(
     onBackClick: () -> Unit,
+    scrollState: ScrollState,
 ) {
     TopAppBar(
         title = {
@@ -375,6 +381,13 @@ private fun TempAppBar(
         windowInsets = WindowInsets(
             top = 0.dp,
             bottom = 0.dp,
+        ),
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = if (scrollState.canScrollBackward) {
+                MaterialTheme.colorScheme.surfaceContainerHigh
+            } else {
+                MaterialTheme.colorScheme.surface
+            },
         ),
     )
 }
