e01cf6abcbe51e80860fb3bb9500dffc21491985
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-11-27 19:50:25 +0000

    Bug 1998890 - Copy profile should copy requiresEncryption=true data. r=profiles-reviewers,fchasen,jhirsch
    Differential Revision: https://phabricator.services.mozilla.com/D272314
    
    UltraBlame original commit: ac42e3256a22ec4dc9cc7226f4b18e77449f0633

--- browser/components/profiles/tests/unit/test_copy_profile.js
+++ browser/components/profiles/tests/unit/test_copy_profile.js
@@ -6,6 +6,9 @@
 const { sinon } = ChromeUtils.importESModule(
   "resource://testing-common/Sinon.sys.mjs"
 );
+const { BackupService } = ChromeUtils.importESModule(
+  "resource:///modules/backup/BackupService.sys.mjs"
+);
 
 const execProcess = sinon.fake();
 
@@ -15,7 +18,7 @@ add_setup(async () => {
   sinon.replace(getSelectableProfileService(), "execProcess", execProcess);
 });
 
-add_task(async function test_create_profile() {
+add_task(async function test_copy_profile() {
   startProfileService();
 
   const SelectableProfileService = getSelectableProfileService();
@@ -26,14 +29,73 @@ add_task(async function test_create_profile() {
   Assert.ok(SelectableProfileService.isEnabled, "Service should be enabled");
 
   let profiles = await SelectableProfileService.getAllProfiles();
-
   Assert.equal(profiles.length, 1, "Only one selectable profile exist");
+
+  const backupServiceInstance = new BackupService();
+
+  let encState = await backupServiceInstance.loadEncryptionState(
+    SelectableProfileService.currentProfile.path
+  );
+  Assert.ok(!encState, "No encryption state before copyProfile called");
 
   let copiedProfile =
     await SelectableProfileService.currentProfile.copyProfile();
 
+  encState = await backupServiceInstance.loadEncryptionState(
+    SelectableProfileService.currentProfile.path
+  );
+  Assert.ok(!encState, "No encryption state after copyProfile called");
+
   profiles = await SelectableProfileService.getAllProfiles();
   Assert.equal(profiles.length, 2, "Two selectable profiles exist");
+
+  Assert.equal(
+    copiedProfile.avatar,
+    SelectableProfileService.currentProfile.avatar,
+    "Copied profile has the same avatar"
+  );
+
+  Assert.equal(
+    copiedProfile.theme.themeId,
+    SelectableProfileService.currentProfile.theme.themeId,
+    "Copied profile has the same theme"
+  );
+});
+
+add_task(async function test_copy_profile_with_encryption() {
+  startProfileService();
+
+  const SelectableProfileService = getSelectableProfileService();
+  const ProfilesDatastoreService = getProfilesDatastoreService();
+
+  await ProfilesDatastoreService.init();
+  await SelectableProfileService.init();
+  Assert.ok(SelectableProfileService.isEnabled, "Service should be enabled");
+
+  let profiles = await SelectableProfileService.getAllProfiles();
+  Assert.equal(profiles.length, 2, "Only two selectable profiles exist");
+
+  const backupServiceInstance = new BackupService();
+  await backupServiceInstance.enableEncryption(
+    "testCopyProfile",
+    SelectableProfileService.currentProfile.path.path
+  );
+
+  let encState = await backupServiceInstance.loadEncryptionState(
+    SelectableProfileService.currentProfile.path
+  );
+  Assert.ok(encState, "Encryption state exists before copyProfile called");
+
+  let copiedProfile =
+    await SelectableProfileService.currentProfile.copyProfile();
+
+  encState = await backupServiceInstance.loadEncryptionState(
+    SelectableProfileService.currentProfile.path
+  );
+  Assert.ok(encState, "Encryption state exists after copyProfile called");
+
+  profiles = await SelectableProfileService.getAllProfiles();
+  Assert.equal(profiles.length, 3, "Three selectable profiles exist");
 
   Assert.equal(
     copiedProfile.avatar,
