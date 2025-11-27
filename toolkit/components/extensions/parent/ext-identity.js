75330b6a9659c526259942cccff72fa2675ac753
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-11-27 19:15:25 +0000

    Bug 2000218 r=willdurand
    Differential Revision: https://phabricator.services.mozilla.com/D273456
    
    UltraBlame original commit: abd70ef80e40d0520cb77d2b7abc571fc03b16c1

--- toolkit/components/extensions/parent/ext-identity.js
+++ toolkit/components/extensions/parent/ext-identity.js
@@ -77,7 +77,7 @@ const openOAuthWindow = (details, redirectURI) => {
       
       let wrapper = ChannelWrapper.get(channel);
       if (
-        !wrapper.browserElement &&
+        !wrapper.browserElement ||
         wrapper.browserElement !== window.gBrowser.selectedBrowser
       ) {
         return;
