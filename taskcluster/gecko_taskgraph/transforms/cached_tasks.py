111ccc0af154a1a8f0445fb1a86e1898c45f9445
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-11-27 19:10:53 +0000

    Bug 2000082 - Stop sharing cached tasks between Mercurial / Git based projects, r=taskgraph-reviewers,bhearsum
    Doing this causes chain of trust failures.
    
    Differential Revision: https://phabricator.services.mozilla.com/D272922
    
    UltraBlame original commit: 9c9ea7b437f61e4eb66c714a96108d46832cea5b

--- taskcluster/gecko_taskgraph/transforms/cached_tasks.py
+++ taskcluster/gecko_taskgraph/transforms/cached_tasks.py
@@ -89,6 +89,11 @@ def cache_task(config, tasks):
             continue
 
         digest_data = cache["digest-data"] + sorted(dependency_digests)
+        
+        
+        if config.params["repository_type"] == "git":
+            digest_data.append(config.params["repository_type"])
+
         add_optimization(
             config,
             task,
