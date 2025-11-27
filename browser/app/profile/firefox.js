d3fd49f11cde8a53aee5a4b6c0af49239d85a69c
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-11-27 19:35:03 +0000

    Bug 2001395 - Set Fx Backup prefs to enabled for Windows by default r=nrishel
    Instead of defaulting the availability of Fx Backup to false, we
    default it to true on Windows. There is still a Nimbus setting
    in place that controls whether or not Fx Backup is actually
    shown to the user.
    
    Differential Revision: https://phabricator.services.mozilla.com/D273927
    
    UltraBlame original commit: 8a90d29d9973c57cf516151d0ebe5cfcbd164dd7

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -3441,8 +3441,8 @@ pref("browser.backup.scheduled.enabled", false);
 
 
 #ifdef XP_WIN
-  pref("browser.backup.archive.enabled", false);
-  pref("browser.backup.restore.enabled", false);
+  pref("browser.backup.archive.enabled", true);
+  pref("browser.backup.restore.enabled", true);
 #else
   pref("browser.backup.archive.enabled", false);
   pref("browser.backup.restore.enabled", false);
