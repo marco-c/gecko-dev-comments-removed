bcd4bf06e2da0b85db849d3b42e917ab632e5245
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-11-27 19:17:30 +0000

    Bug 1997657 - part 2: changes in handling nimbus killswitches r=hsohaney,nrishel
    Previously, our implementation of the handling for Nimbus flags
    "archiveKillswitch" and "restoreKillswitch" would only have an
    effect if the values were set to "true".
    
    Now, in order to support both phased rollout and turning off the
    feature, there are three possible states for these flags:
    
      true: feature is turned off (killed)
      undefined: feature is turned off (not rolled out)
      false: feature is turned on
    
    Differential Revision: https://phabricator.services.mozilla.com/D273526
    
    UltraBlame original commit: 0ab6dc45980ef9ea020a499962491f6ba0e49f00

--- browser/components/backup/tests/xpcshell/test_BackupService_enabled.js
+++ browser/components/backup/tests/xpcshell/test_BackupService_enabled.js
@@ -15,6 +15,8 @@ const BACKUP_ARCHIVE_ENABLED_PREF_NAME = "browser.backup.archive.enabled";
 const BACKUP_ARCHIVE_ENABLED_OVERRIDE_PREF_NAME =
   "browser.backup.archive.overridePlatformCheck";
 const BACKUP_RESTORE_ENABLED_PREF_NAME = "browser.backup.restore.enabled";
+const BACKUP_RESTORE_ENABLED_OVERRIDE_PREF_NAME =
+  "browser.backup.restore.overridePlatformCheck";
 const SANITIZE_ON_SHUTDOWN_PREF_NAME = "privacy.sanitize.sanitizeOnShutdown";
 const SELECTABLE_PROFILES_CREATED_PREF_NAME = "browser.profiles.created";
 
@@ -49,15 +51,27 @@ add_setup(async () => {
 
 add_task(async function test_archive_killswitch_enrollment() {
   let cleanupExperiment;
+  const savedPref = Services.prefs.getBoolPref(
+    BACKUP_ARCHIVE_ENABLED_OVERRIDE_PREF_NAME,
+    false
+  );
   await archiveTemplate({
     internalReason: "nimbus",
     async disable() {
+      Services.prefs.setBoolPref(
+        BACKUP_ARCHIVE_ENABLED_OVERRIDE_PREF_NAME,
+        false
+      );
       cleanupExperiment = await NimbusTestUtils.enrollWithFeatureConfig({
         featureId: "backupService",
         value: { archiveKillswitch: true },
       });
     },
     async enable() {
+      Services.prefs.setBoolPref(
+        BACKUP_ARCHIVE_ENABLED_OVERRIDE_PREF_NAME,
+        savedPref
+      );
       await cleanupExperiment();
     },
     
@@ -135,6 +149,11 @@ add_task(async function test_archive_disabled_unsupported_os() {
   );
   Services.prefs.setBoolPref(BACKUP_ARCHIVE_ENABLED_OVERRIDE_PREF_NAME, false);
   sandbox.stub(BackupService, "checkOsSupportsBackup").returns(false);
+  const cleanupExperiment = await NimbusTestUtils.enrollWithFeatureConfig({
+    featureId: "backupService",
+    value: { archiveKillswitch: false },
+  });
+
   try {
     const bs = new BackupService();
     const status = bs.archiveEnabledStatus;
@@ -146,6 +165,7 @@ add_task(async function test_archive_disabled_unsupported_os() {
       BACKUP_ARCHIVE_ENABLED_OVERRIDE_PREF_NAME,
       archiveWasEnabled
     );
+    await cleanupExperiment();
   }
 });
 
@@ -157,6 +177,10 @@ add_task(async function test_archive_enabled_supported_os() {
   );
   Services.prefs.setBoolPref(BACKUP_ARCHIVE_ENABLED_OVERRIDE_PREF_NAME, false);
   sandbox.stub(BackupService, "checkOsSupportsBackup").returns(true);
+  const cleanupExperiment = await NimbusTestUtils.enrollWithFeatureConfig({
+    featureId: "backupService",
+    value: { archiveKillswitch: false },
+  });
   try {
     const bs = new BackupService();
     const status = bs.archiveEnabledStatus;
@@ -167,20 +191,33 @@ add_task(async function test_archive_enabled_supported_os() {
       BACKUP_ARCHIVE_ENABLED_OVERRIDE_PREF_NAME,
       archiveWasEnabled
     );
+    await cleanupExperiment();
   }
 });
 
 add_task(async function test_restore_killswitch_enrollment() {
   let cleanupExperiment;
+  const savedPref = Services.prefs.getBoolPref(
+    BACKUP_RESTORE_ENABLED_OVERRIDE_PREF_NAME,
+    false
+  );
   await restoreTemplate({
     internalReason: "nimbus",
     async disable() {
+      Services.prefs.setBoolPref(
+        BACKUP_RESTORE_ENABLED_OVERRIDE_PREF_NAME,
+        false
+      );
       cleanupExperiment = await NimbusTestUtils.enrollWithFeatureConfig({
         featureId: "backupService",
         value: { restoreKillswitch: true },
       });
     },
     async enable() {
+      Services.prefs.setBoolPref(
+        BACKUP_RESTORE_ENABLED_OVERRIDE_PREF_NAME,
+        savedPref
+      );
       await cleanupExperiment();
     },
     
