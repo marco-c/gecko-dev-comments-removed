183d4ebeba02644300d64cc5c404cc5c2fa5d0ba
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-11-27 20:06:20 +0000

    Bug 1898937 - remove our UA override for www.goodyearfiaetrc.com; r=ksenia,webcompat-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D274086
    
    UltraBlame original commit: 55b4fc4266ab26cb9008bb92ab20f641d850e041

--- testing/webcompat/interventions/tests/test_1898937_goodyearfiaetrc_com.py
+++ testing/webcompat/interventions/tests/test_1898937_goodyearfiaetrc_com.py
@@ -2,21 +2,11 @@ import pytest
 
 URL = "https://www.goodyearfiaetrc.com/standings"
 
-SUPPORTED_CSS = "#result #iframe"
-UNSUPPORTED_TEXT = "Please open this Page in Chrome, Edge or Safari"
-
-
-@pytest.mark.asyncio
-@pytest.mark.with_interventions
-async def test_enabled(client):
-    await client.navigate(URL, wait="none")
-    assert client.await_css(SUPPORTED_CSS, is_displayed=True)
-    assert not client.find_text(UNSUPPORTED_TEXT, is_displayed=True)
+DEAD_TEXT = "Page not found"
 
 
 @pytest.mark.asyncio
 @pytest.mark.without_interventions
-async def test_disabled(client):
+async def test_if_site_returns(client):
     await client.navigate(URL, wait="none")
-    assert client.await_text(UNSUPPORTED_TEXT, is_displayed=True)
-    assert not client.find_css(SUPPORTED_CSS, is_displayed=True)
+    assert client.await_text(DEAD_TEXT, is_displayed=True)
