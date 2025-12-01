9ccdd013fb4d8c67cc1ea16e87e38c043187b976
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-12-01 01:26:46 +0000

    Bug 2000328: Implement prepareHashMFBT r=jandem
    Differential Revision: https://phabricator.services.mozilla.com/D273668
    
    UltraBlame original commit: d06c70b12058ccf4b1d0a539d1801b9d8ef3d594

--- js/src/jit/MacroAssembler.h
+++ js/src/jit/MacroAssembler.h
@@ -5430,6 +5430,7 @@ class MacroAssembler : public MacroAssemblerSpecific {
   void scrambleHashCode(Register result);
 
  public:
+  void hashAndScrambleValue(ValueOperand value, Register result, Register temp);
   void prepareHashNonGCThing(ValueOperand value, Register result,
                              Register temp);
   void prepareHashString(Register str, Register result, Register temp);
@@ -5442,6 +5443,10 @@ class MacroAssembler : public MacroAssemblerSpecific {
   void prepareHashValue(Register setObj, ValueOperand value, Register result,
                         Register temp1, Register temp2, Register temp3,
                         Register temp4);
+
+  
+  
+  void prepareHashMFBT(Register hashCode, bool alreadyScrambled);
 
  private:
   enum class IsBigInt { No, Yes, Maybe };
