8b50f6b1b3650f878284231cd40b36cfdf9d3645
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-12-01 01:40:25 +0000

    Bug 2002971 - Remove trailing newlines when outputting a hard-wrapped line. r=smaug
    Differential Revision: https://phabricator.services.mozilla.com/D274461
    
    UltraBlame original commit: 20f34e644e1c481429841abf435a1d5e5b06ab93

--- dom/serializers/nsPlainTextSerializer.cpp
+++ dom/serializers/nsPlainTextSerializer.cpp
@@ -1316,6 +1316,10 @@ void nsPlainTextSerializer::PerformWrapAndOutputCompleteLines(
     aLine.mContent.Truncate(goodSpace);
     
     if (!aLine.mContent.IsEmpty()) {
+      
+      if (aLine.mContent.Last() == '\n') {
+        aLine.mContent.Truncate(goodSpace - 1);
+      }
       if (!aSettings.HasFlag(nsIDocumentEncoder::OutputPreformatted)) {
         aLine.mContent.Trim(" ", false, true, false);
       }
