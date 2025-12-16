b79490d6bf3b81f30486cf074a075ef395ee2155
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-12-16 04:04:25 +0000

    Bug 2005794 - Switch archiveKillswitch handling to enable backup functionality by default on Windows 10 r=hsohaney
    Since we used an unusual method of doing a Nimbus rollout, this change
    involves modifying our handling of the archiveKillswitch Nimbus
    setting.
    
    Differential Revision: https://phabricator.services.mozilla.com/D276298
    
    UltraBlame original commit: 5611221c069e357382d0068097f172e6b13bf8d8

--- browser/components/backup/BackupService.sys.mjs
+++ browser/components/backup/BackupService.sys.mjs
@@ -660,13 +660,8 @@ export class BackupService extends EventTarget {
       BACKUP_ARCHIVE_ENABLED_OVERRIDE_PREF_NAME,
       false
     );
-    // This is explicitly checking for archiveKillswitchTriggered !== false because
-    // we now also (potentially) want to use this nimbus setting for doing staged rollout
-    // of the feature. What this means is that if the value is:
-    //     - true: feature is turned off ("killed")
-    //     - undefined: feature is turned off (not launched yet)
-    //     - false: feature is turned on
-    if (archiveKillswitchTriggered !== false && !archiveOverrideEnabled) {
+    // Only disable feature if archiveKillswitch is true.
+    if (archiveKillswitchTriggered && !archiveOverrideEnabled) {
       return {
         enabled: false,
         reason: "Archiving a profile disabled remotely.",
@@ -739,8 +734,6 @@ export class BackupService extends EventTarget {
       false
     );
 
-    // restoreKillswitch is a "normal" killswitch, in contrast
-    // to archiveKillswitch
     if (restoreKillswitchTriggered && !restoreOverrideEnabled) {
       return {
         enabled: false,
