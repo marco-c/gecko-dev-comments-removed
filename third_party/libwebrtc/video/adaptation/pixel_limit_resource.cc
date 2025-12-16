032f4165192e8e49ebdc21115eb0ddfc76fd012d
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-12-16 02:45:36 +0000

    Bug 2000941 - Vendor libwebrtc from 928102e64d
    Upstream commit: https://webrtc.googlesource.com/src/+/928102e64d3c6a7355e5e0e264701d804e54dbf1
        Add ability to toggle PixelLimitResource "on" and "off" periodically.
    
        PixelLimitResource is a testing-only resource that influences adaptation
        by periodically signaling kOveruse (= "adapt down please") and kUnderuse
        (= "adapt up please") in order to try to achieve a target resolution
        (expressed as pixel count). The old way to enable it was, for example:
        --force-fieldtrials=WebRTC-PixelLimitResource/Enabled-230400/
    
        This CL parameterizes it and adds a new parameters, for example:
        --force-fieldtrials=WebRTC-PixelLimitResource/
        target_pixels:230400,interval:1s,toggle:5s/
    
        Explanation:
        - target_pixels: Is the target pixel count, same as the old behavior.
        - interval: The interval at which to check if kOveruse/kUnderuse needs
          to be reported. This was previously hard-coded to 5s but by allowing
          overriding this value you can make the resource faster or slower.
        - toggle: If specified, toggles "on" or "off" at this interval. If not
          specified we don't toggle which was the old behavior.
    
        By toggling we can test not just what happens when we adapt down, but
        also that we can recover once a resource is no longer overused.
    
        In addition to unit tests, also tested manually at
        https://henbos.github.io/codec-quality/src/index.html using parameters
        "target_pixels:64800,interval:1s,toggle:10s". Result is rapidly
        toggling between QVGA (cpu-limited) and HD (no adaptation) every 10s.
    
        Bug: webrtc:447037084
        Change-Id: I56fe4a9c14abf38de55b4b631f401ca84656a094
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/411581
        Reviewed-by: Guido Urdaneta <guidouwebrtc.org>
        Commit-Queue: Henrik Boström <hboswebrtc.org>
        Cr-Commit-Position: refs/heads/main{#45727}
    
    UltraBlame original commit: 0deeeff3a9261a1d7a02ddaa880f4c8f6179e92b

--- third_party/libwebrtc/video/adaptation/pixel_limit_resource.cc
+++ third_party/libwebrtc/video/adaptation/pixel_limit_resource.cc
@@ -11,8 +11,10 @@
 #include "video/adaptation/pixel_limit_resource.h"
 
 #include <optional>
+#include <string>
 
 #include "api/adaptation/resource.h"
+#include "api/field_trials_view.h"
 #include "api/make_ref_counted.h"
 #include "api/scoped_refptr.h"
 #include "api/sequence_checker.h"
@@ -21,29 +23,91 @@
 #include "call/adaptation/video_stream_adapter.h"
 #include "call/adaptation/video_stream_input_state_provider.h"
 #include "rtc_base/checks.h"
+#include "rtc_base/experiments/field_trial_parser.h"
+#include "rtc_base/logging.h"
 #include "rtc_base/task_utils/repeating_task.h"
 
 namespace webrtc {
 
 namespace {
 
-constexpr TimeDelta kResourceUsageCheckIntervalMs = TimeDelta::Seconds(5);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+struct PixelLimitResourceParams {
+  static std::optional<PixelLimitResourceParams> Parse(
+      const FieldTrialsView& field_trials) {
+    std::string params_str = field_trials.Lookup("WebRTC-PixelLimitResource");
+    if (params_str.empty()) {
+      return std::nullopt;
+    }
+    PixelLimitResourceParams params;
+    ParseFieldTrial({&params.target_pixels, &params.interval, &params.toggle},
+                    params_str);
+    return params;
+  }
+
+  PixelLimitResourceParams()
+      : target_pixels("target_pixels", 0),
+        interval("interval", TimeDelta::Seconds(5)),
+        toggle("toggle") {}
+
+  FieldTrialParameter<int> target_pixels;
+  FieldTrialParameter<TimeDelta> interval;
+  FieldTrialOptional<TimeDelta> toggle;
+};
 
 }  
 
 
-scoped_refptr<PixelLimitResource> PixelLimitResource::Create(
+scoped_refptr<PixelLimitResource> PixelLimitResource::CreateIfFieldTrialEnabled(
+    const FieldTrialsView& field_trials,
     TaskQueueBase* task_queue,
     VideoStreamInputStateProvider* input_state_provider) {
-  return make_ref_counted<PixelLimitResource>(task_queue, input_state_provider);
+  std::optional<PixelLimitResourceParams> params =
+      PixelLimitResourceParams::Parse(field_trials);
+  if (!params.has_value()) {
+    return nullptr;
+  }
+  auto pixel_limit_resource = make_ref_counted<PixelLimitResource>(
+      task_queue, input_state_provider, params->target_pixels.Get(),
+      params->interval.Get(), params->toggle.GetOptional());
+  RTC_LOG(LS_INFO) << "Running with PixelLimitResource {target_pixels:"
+                   << params->target_pixels.Get()
+                   << ", interval: " << params->interval.Get() << ", toggle:"
+                   << (params->toggle ? ToString(*params->toggle)
+                                      : std::string("N/A"))
+                   << "}";
+  return pixel_limit_resource;
 }
 
 PixelLimitResource::PixelLimitResource(
     TaskQueueBase* task_queue,
-    VideoStreamInputStateProvider* input_state_provider)
+    VideoStreamInputStateProvider* input_state_provider,
+    int target_pixels,
+    TimeDelta interval,
+    std::optional<TimeDelta> toggle_interval)
     : task_queue_(task_queue),
       input_state_provider_(input_state_provider),
-      max_pixels_(std::nullopt) {
+      target_pixels_(target_pixels),
+      interval_(interval),
+      toggle_interval_(toggle_interval),
+      is_enabled_(true),
+      time_since_last_toggle_(TimeDelta::Zero()) {
   RTC_DCHECK(task_queue_);
   RTC_DCHECK(input_state_provider_);
 }
@@ -53,51 +117,57 @@ PixelLimitResource::~PixelLimitResource() {
   RTC_DCHECK(!repeating_task_.Running());
 }
 
-void PixelLimitResource::SetMaxPixels(int max_pixels) {
-  RTC_DCHECK_RUN_ON(task_queue_);
-  max_pixels_ = max_pixels;
-}
-
 void PixelLimitResource::SetResourceListener(ResourceListener* listener) {
   RTC_DCHECK_RUN_ON(task_queue_);
   listener_ = listener;
   if (listener_) {
     repeating_task_.Stop();
-    repeating_task_ = RepeatingTaskHandle::Start(task_queue_, [&] {
-      RTC_DCHECK_RUN_ON(task_queue_);
-      if (!listener_) {
-        
-        
-        return kResourceUsageCheckIntervalMs;
-      }
-      if (!max_pixels_.has_value()) {
-        
-        return kResourceUsageCheckIntervalMs;
-      }
-      std::optional<int> frame_size_pixels =
-          input_state_provider_->InputState().frame_size_pixels();
-      if (!frame_size_pixels.has_value()) {
-        
-        
-        return kResourceUsageCheckIntervalMs;
-      }
-      int current_pixels = frame_size_pixels.value();
-      int target_pixel_upper_bounds = max_pixels_.value();
-      
-      
-      
-      
-      int target_pixels_lower_bounds =
-          GetLowerResolutionThan(target_pixel_upper_bounds);
-      if (current_pixels > target_pixel_upper_bounds) {
-        listener_->OnResourceUsageStateMeasured(scoped_refptr<Resource>(this),
-                                                ResourceUsageState::kOveruse);
-      } else if (current_pixels < target_pixels_lower_bounds) {
-        listener_->OnResourceUsageStateMeasured(scoped_refptr<Resource>(this),
-                                                ResourceUsageState::kUnderuse);
-      }
-      return kResourceUsageCheckIntervalMs;
-    });
+    repeating_task_ =
+        RepeatingTaskHandle::DelayedStart(task_queue_, interval_, [&] {
+          RTC_DCHECK_RUN_ON(task_queue_);
+          if (!listener_) {
+            
+            
+            return interval_;
+          }
+          if (is_enabled_) {
+            
+            
+            
+            std::optional<int> current_pixels =
+                input_state_provider_->InputState().frame_size_pixels();
+            if (current_pixels.has_value()) {
+              
+              
+              int target_pixels_lower_bounds =
+                  GetLowerResolutionThan(target_pixels_);
+              if (*current_pixels > target_pixels_) {
+                listener_->OnResourceUsageStateMeasured(
+                    scoped_refptr<Resource>(this),
+                    ResourceUsageState::kOveruse);
+              } else if (*current_pixels < target_pixels_lower_bounds) {
+                listener_->OnResourceUsageStateMeasured(
+                    scoped_refptr<Resource>(this),
+                    ResourceUsageState::kUnderuse);
+              }
+            }
+          } else {
+            
+            listener_->OnResourceUsageStateMeasured(
+                scoped_refptr<Resource>(this), ResourceUsageState::kUnderuse);
+          }
+          
+          if (toggle_interval_.has_value()) {
+            time_since_last_toggle_ += interval_;
+            if (time_since_last_toggle_ >= toggle_interval_.value()) {
+              is_enabled_ = !is_enabled_;
+              RTC_LOG(LS_INFO) << "PixelLimitResource toggled "
+                               << (is_enabled_ ? "on" : "off");
+              time_since_last_toggle_ = TimeDelta::Zero();
+            }
+          }
+          return interval_;
+        });
   } else {
     repeating_task_.Stop();
   }
