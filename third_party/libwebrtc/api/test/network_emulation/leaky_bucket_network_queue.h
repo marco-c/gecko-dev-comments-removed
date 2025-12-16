4c7345f61661a8155816ff3407975b76cd3e2fc9
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-12-16 02:47:12 +0000

    Bug 2000941 - Vendor libwebrtc from ba391e6893
    Upstream commit: https://webrtc.googlesource.com/src/+/ba391e6893da350a48a7895a0bff8f033369a729
        Introduce FeedbackGenerator for testing Congestion controller using RFC 8888
    
        Change-Id: I02bfc545fff792e4ea73830317e3b37abd1e1424
        Bug: webrtc:447037083
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/412060
        Commit-Queue: Per Kjellander <perkjwebrtc.org>
        Reviewed-by: Danil Chapovalov <danilchapwebrtc.org>
        Reviewed-by: Mirko Bonadei <mbonadeiwebrtc.org>
        Cr-Commit-Position: refs/heads/main{#45749}
    
    UltraBlame original commit: 69c0862b155bf5bc71dfee0852eed83575cb21ea

---
+++ third_party/libwebrtc/api/test/network_emulation/leaky_bucket_network_queue.h
@@ -0,0 +1,86 @@
+
+
+
+
+
+
+
+
+
+
+#ifndef API_TEST_NETWORK_EMULATION_LEAKY_BUCKET_NETWORK_QUEUE_H_
+#define API_TEST_NETWORK_EMULATION_LEAKY_BUCKET_NETWORK_QUEUE_H_
+
+#include <cstddef>
+#include <memory>
+#include <optional>
+#include <queue>
+#include <vector>
+
+#include "api/test/network_emulation/network_queue.h"
+#include "api/test/simulated_network.h"
+#include "api/units/time_delta.h"
+#include "api/units/timestamp.h"
+#include "rtc_base/random.h"
+
+namespace webrtc {
+
+
+
+class LeakyBucketNetworkQueue : public NetworkQueue {
+ public:
+  struct Config {
+    int seed = 1;
+    
+    
+    
+    
+    TimeDelta max_ect1_sojourn_time = TimeDelta::PlusInfinity();
+    TimeDelta target_ect1_sojourn_time = TimeDelta::PlusInfinity();
+  };
+
+  LeakyBucketNetworkQueue() : LeakyBucketNetworkQueue(Config()) {}
+  explicit LeakyBucketNetworkQueue(const Config& config);
+  
+  
+  
+  void SetMaxPacketCapacity(size_t max_capactiy) override;
+
+  bool EnqueuePacket(const PacketInFlightInfo& packet_info) override;
+  std::optional<PacketInFlightInfo> PeekNextPacket() const override;
+  std::optional<PacketInFlightInfo> DequeuePacket(Timestamp time_now) override;
+  std::vector<PacketInFlightInfo> DequeueDroppedPackets() override;
+  bool empty() const override;
+
+  void DropOldestPacket();
+
+ private:
+  void MaybeMarkAsCe(Timestamp time_now, PacketInFlightInfo& packet_info);
+
+  size_t max_packet_capacity_ = kMaxPacketCapacity;
+  const TimeDelta max_ect1_sojourn_time_;
+  const TimeDelta target_ect1_sojourn_time_;
+
+  Random random_;
+  std::queue<PacketInFlightInfo> queue_;
+  std::vector<PacketInFlightInfo> dropped_packets_;
+};
+
+class LeakyBucketNetworkQueueFactory : public NetworkQueueFactory {
+ public:
+  LeakyBucketNetworkQueueFactory()
+      : LeakyBucketNetworkQueueFactory(LeakyBucketNetworkQueue::Config()) {}
+  explicit LeakyBucketNetworkQueueFactory(
+      const LeakyBucketNetworkQueue::Config& config)
+      : config_(config) {}
+
+  std::unique_ptr<NetworkQueue> CreateQueue() override {
+    return std::make_unique<LeakyBucketNetworkQueue>(config_);
+  }
+
+ private:
+  const LeakyBucketNetworkQueue::Config config_;
+};
+}  
+
+#endif  
