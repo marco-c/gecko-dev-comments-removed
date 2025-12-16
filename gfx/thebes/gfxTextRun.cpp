70d9749c9bf54ab3bc6d44b8ac046c15c5cbbe1b
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-12-16 02:11:35 +0000

    Bug 1871755 - Make gfxMissingFontRecorder::Flush safe to call from worker threads. r=gfx-reviewers,lsalzman
    The included testcase hits this crash reliably for me in local testing,
    and passes once the patch is applied.
    
    Differential Revision: https://phabricator.services.mozilla.com/D275077
    
    UltraBlame original commit: 78032ddf8e6bc0603723630d2e5967a0ebe19e9d

--- gfx/thebes/gfxTextRun.cpp
+++ gfx/thebes/gfxTextRun.cpp
@@ -3985,15 +3985,33 @@ gfxFont::Metrics gfxFontGroup::GetMetricsForCSSUnits(
   return metrics;
 }
 
-void gfxMissingFontRecorder::Flush() {
-  static bool mNotifiedFontsInitialized = false;
-  static uint32_t mNotifiedFonts[gfxMissingFontRecorder::kNumScriptBitsWords];
-  if (!mNotifiedFontsInitialized) {
-    memset(&mNotifiedFonts, 0, sizeof(mNotifiedFonts));
-    mNotifiedFontsInitialized = true;
+class DeferredNotifyMissingFonts final : public nsIRunnable {
+ public:
+  NS_DECL_THREADSAFE_ISUPPORTS
+
+  explicit DeferredNotifyMissingFonts(nsString&& aScriptList)
+      : mScriptList(std::move(aScriptList)) {}
+
+ protected:
+  virtual ~DeferredNotifyMissingFonts() {}
+
+  NS_IMETHOD Run(void) override {
+    nsCOMPtr<nsIObserverService> service = GetObserverService();
+    service->NotifyObservers(nullptr, "font-needed", mScriptList.get());
+    return NS_OK;
   }
 
+  nsString mScriptList;
+};
+
+NS_IMPL_ISUPPORTS(DeferredNotifyMissingFonts, nsIRunnable)
+
+void gfxMissingFontRecorder::Flush() {
+  static uint32_t mNotifiedFonts[gfxMissingFontRecorder::kNumScriptBitsWords];
+  static StaticMutex sNotifiedFontsMutex;
+
   nsAutoString fontNeeded;
+  sNotifiedFontsMutex.Lock();
   for (uint32_t i = 0; i < kNumScriptBitsWords; ++i) {
     mMissingFonts[i] &= ~mNotifiedFonts[i];
     if (!mMissingFonts[i]) {
@@ -4018,8 +4036,15 @@ void gfxMissingFontRecorder::Flush() {
     }
     mMissingFonts[i] = 0;
   }
+  sNotifiedFontsMutex.Unlock();
+
   if (!fontNeeded.IsEmpty()) {
-    nsCOMPtr<nsIObserverService> service = GetObserverService();
-    service->NotifyObservers(nullptr, "font-needed", fontNeeded.get());
+    if (NS_IsMainThread()) {
+      nsCOMPtr<nsIObserverService> service = GetObserverService();
+      service->NotifyObservers(nullptr, "font-needed", fontNeeded.get());
+    } else {
+      NS_DispatchToMainThread(
+          new DeferredNotifyMissingFonts(std::move(fontNeeded)));
+    }
   }
 }
