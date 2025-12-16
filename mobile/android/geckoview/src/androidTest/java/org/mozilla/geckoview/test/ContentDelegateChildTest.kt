cbd94baa47b8a671eac85ea45c04196b34c59ad5
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-12-16 04:19:58 +0000

    Bug 1809303 - Add "Copy link text" context menu item r=geckoview-reviewers,android-reviewers,android-l10n-reviewers,geckoview-api-reviewers,flod,ohall,tcampbell
    This patch introduces a "Copy link text" option to the context menu. It adds the necessary `innerText` property from Gecko to the Android components layer, allowing the context menu feature to access and copy the visible text of a link to the clipboard.
    
    Differential Revision: https://phabricator.services.mozilla.com/D275482
    
    UltraBlame original commit: 4ff69f0b1422c75385cb893869cc256ca9405454

--- mobile/android/geckoview/src/androidTest/java/org/mozilla/geckoview/test/ContentDelegateChildTest.kt
+++ mobile/android/geckoview/src/androidTest/java/org/mozilla/geckoview/test/ContentDelegateChildTest.kt
@@ -21,7 +21,6 @@ import org.hamcrest.Matchers.endsWith
 import org.hamcrest.Matchers.equalTo
 import org.hamcrest.Matchers.startsWith
 import org.junit.Assert.assertNull
-import org.junit.Assume.assumeThat
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mozilla.geckoview.GeckoSession
@@ -390,9 +389,15 @@ class ContentDelegateChildTest : BaseSessionTest() {
                         element.linkUri,
                         endsWith("hello.html"),
                     )
+                    @Suppress("DEPRECATION") 
                     assertThat(
                         "The element link text content should be the text content of the anchor.",
                         element.textContent,
+                        equalTo("Hello World"),
+                    )
+                    assertThat(
+                        "The element link text should be the link text of the anchor.",
+                        element.linkText,
                         equalTo("Hello World"),
                     )
                 }
@@ -402,7 +407,7 @@ class ContentDelegateChildTest : BaseSessionTest() {
 
     @WithDisplay(width = 100, height = 100)
     @Test
-    fun requestContextMenuOnLinkText() {
+    fun requestContextMenuOnLinkTextLimits() {
         mainSession.loadTestPath(CONTEXT_MENU_LINK_TEXT_HTML_PATH)
         mainSession.waitForPageStop()
         sendLongPress(50f, 50f)
@@ -426,10 +431,65 @@ class ContentDelegateChildTest : BaseSessionTest() {
                         element.altText?.length,
                         equalTo(4096),
                     )
+                    @Suppress("DEPRECATION") 
                     assertThat(
                         "The element link text content should not exceed a maximum of 4096 chars.",
                         element.textContent?.length,
                         equalTo(4096),
+                    )
+                    assertThat(
+                        "The element link text should not exceed a maximum of 4096 chars.",
+                        element.linkText?.length,
+                        equalTo(4096),
+                    )
+                }
+            },
+        )
+    }
+
+    @WithDisplay(width = 100, height = 100)
+    @Test
+    fun requestContextMenuOnLinkText() {
+        mainSession.loadTestPath(CONTEXT_MENU_LINK_TEXT_HTML_NORMAL_LENGTH_PATH)
+        mainSession.waitForPageStop()
+        sendLongPress(50f, 50f)
+
+        mainSession.waitUntilCalled(
+            object : ContentDelegate {
+                @AssertCalled(count = 1)
+                override fun onContextMenu(
+                    session: GeckoSession,
+                    screenX: Int,
+                    screenY: Int,
+                    element: ContextElement,
+                ) {
+                    assertThat(
+                        "Type should be none.",
+                        element.type,
+                        equalTo(ContextElement.TYPE_NONE),
+                    )
+                    assertThat(
+                        "The element link title should be the title of the anchor.",
+                        element.title,
+                        equalTo("Lorem ipsum dolor sit amet cillum amet minim."),
+                    )
+                    assertThat(
+                        "The element link URI should be the href of the anchor.",
+                        element.linkUri,
+                        endsWith("hello.html"),
+                    )
+                    @Suppress("DEPRECATION") 
+                    assertThat(
+                        "The element link text content should be the text content of the " +
+                                "anchor including white spaces.",
+                        element.textContent,
+                        equalTo("\n      Lorem ipsum dolor sit amet cillum amet minim."),
+                    )
+                    assertThat(
+                        "The element link text should be the link text of the " +
+                                "anchor without white spaces.",
+                        element.linkText,
+                        equalTo("Lorem ipsum dolor sit amet cillum amet minim."),
                     )
                 }
             },
@@ -518,9 +578,15 @@ class ContentDelegateChildTest : BaseSessionTest() {
                         element.linkUri,
                         endsWith("hello.html"),
                     )
+                    @Suppress("DEPRECATION") 
                     assertThat(
                         "The element link text content should be the text content of the anchor.",
                         element.textContent,
+                        equalTo("Hello World"),
+                    )
+                    assertThat(
+                        "The element link text should be the link text of the anchor.",
+                        element.linkText,
                         equalTo("Hello World"),
                     )
                 }
@@ -565,9 +631,15 @@ class ContentDelegateChildTest : BaseSessionTest() {
                         element.linkUri,
                         endsWith("hello.html"),
                     )
+                    @Suppress("DEPRECATION") 
                     assertThat(
                         "The element link text content should be the text content of the anchor.",
                         element.textContent,
+                        equalTo("Hello World"),
+                    )
+                    assertThat(
+                        "The element link text should be the link text of the anchor.",
+                        element.linkText,
                         equalTo("Hello World"),
                     )
                 }
