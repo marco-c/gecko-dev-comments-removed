1f41d792153487a98012d844928a548c43ba43d8
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-12-16 04:04:11 +0000

    Bug 2005449 - Handle failure to unwrap JS microtask r=bthrall
    Differential Revision: https://phabricator.services.mozilla.com/D276039
    
    UltraBlame original commit: 8c21c5966e8d4e44fec3540cd8d138b0b8474387

--- js/src/shell/js.cpp
+++ js/src/shell/js.cpp
@@ -1519,7 +1519,13 @@ static bool GlobalOfFirstJobInQueue(JSContext* cx, unsigned argc, Value* vp) {
 
     auto& genericJob = cx->microTaskQueues->microTaskQueue.front();
     JS::JSMicroTask* job = JS::ToUnwrappedJSMicroTask(genericJob);
-    MOZ_ASSERT(job);
+    if (!job) {
+      JS_ReportErrorNumberASCII(cx, GetErrorMessage, nullptr,
+                                JSMSG_DEAD_OBJECT);
+
+      return false;
+    }
+
     RootedObject global(cx, JS::GetExecutionGlobalFromJSMicroTask(job));
     if (!global) {
       JS_ReportErrorNumberASCII(cx, GetErrorMessage, nullptr,
