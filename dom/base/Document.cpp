1fbd088e23ceee2b3a23beadec0e91d627763e85
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-12-16 04:14:25 +0000

    Bug 2002289 Fix OOP-frame JSWA fullscreen problem r=edgar
    This patch fixes a problem where we have fullscreened a subtree of
    documents and now a request is made to fullscreen an element further
    down in the tree, which may be out of process, with respect to the
    already fullscreened elements and documents.
    
    The bug triggers, because a check in the parent proess will see that the
    browser tab, already has a chrome element that is fullscreen and then
    aborts. We must also not do any actual work when we hit this issue,
    because everything is already fullscreen.
    
    The fix is to resume the JSWA "messaging chain" without doing any
    Fullscreen API work and only do so when the in-process-requested
    to-be-fullscreened-element is an iframe, or if we're the parent process,
    otherwise the check should "fail" as normal and not do anything.
    
    Added wpt-like tests. I guess there's a case to be made to change the
    test testing/web-platform/tests/fullscreen/api/element-request-fullscreen-cross-origin.sub.html
    to behave like the test I've provided here. It first requests
    fullscreen inside B, which will make the body element go fullscreen,
    then when a request goes for D (its body element), the iframe in B also
    goes fullscreen and then the remainder of frames goes fullscreen. The
    point being for us, that when doing fullscreen stuff and we're stuck on
    JSWA, we must propagate the MozDOMFullscreen:Entered messages
    properly, even if no work needs to be done.
    
    Differential Revision: https://phabricator.services.mozilla.com/D274060
    
    UltraBlame original commit: 57b6ccd0754de5db2e6cc4bbec7ee52264a39d11

--- dom/base/Document.cpp
+++ dom/base/Document.cpp
@@ -16408,6 +16408,24 @@ const char* Document::GetFullscreenError(CallerType aCallerType) {
   return nullptr;
 }
 
+
+
+static inline void PropagateFullscreenRequest(Document* aDoc,
+                                              Element* aElement) {
+  nsContentUtils::DispatchEventOnlyToChrome(
+      aDoc, aElement, u"MozDOMFullscreen:Entered"_ns, CanBubble::eYes,
+      Cancelable::eNo,  nullptr);
+}
+
+static bool ElementIsRemoteFrame(Element* aElement) {
+  MOZ_ASSERT(aElement);
+  RefPtr<nsFrameLoader> loader;
+  if (RefPtr<nsFrameLoaderOwner> loaderOwner = do_QueryObject(aElement)) {
+    loader = loaderOwner->GetFrameLoader();
+  }
+  return loader && loader->IsRemoteFrame();
+}
+
 bool Document::FullscreenElementReadyCheck(FullscreenRequest& aRequest) {
   Element* elem = aRequest.Element();
   
@@ -16416,7 +16434,19 @@ bool Document::FullscreenElementReadyCheck(FullscreenRequest& aRequest) {
   
   
   Element* fullscreenElement = GetUnretargetedFullscreenElement();
-  if (elem == fullscreenElement) {
+  if (NS_WARN_IF(elem == fullscreenElement)) {
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    if (ElementIsRemoteFrame(elem)) {
+      PropagateFullscreenRequest(this, elem);
+    }
     aRequest.MayResolvePromise();
     return false;
   }
@@ -16750,10 +16780,16 @@ bool Document::ApplyFullscreen(UniquePtr<FullscreenRequest> aRequest) {
   
   
   
-  if (!previousFullscreenDoc) {
-    nsContentUtils::DispatchEventOnlyToChrome(
-        this, elem, u"MozDOMFullscreen:Entered"_ns, CanBubble::eYes,
-        Cancelable::eNo,  nullptr);
+  
+  
+  
+  
+  if (!aRequest->GetPromise() || !previousFullscreenDoc) {
+    MOZ_ASSERT(
+        (previousFullscreenDoc &&
+         ElementIsRemoteFrame(child->GetUnretargetedFullscreenElement())) ||
+        !previousFullscreenDoc);
+    PropagateFullscreenRequest(this, elem);
   }
 
   
