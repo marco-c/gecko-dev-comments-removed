3394c26fc74e2db534c946124f6661e98924eb9f
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-12-16 03:26:55 +0000

    Bug 2004511 - Part 16: Remove layerScrim in AcornColors r=android-reviewers,007
    Differential Revision: https://phabricator.services.mozilla.com/D275393
    
    UltraBlame original commit: 0edb9455885a8c10adff1ed9a98da0a00052f312

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/home/ui/MiddleSearchHomepage.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/home/ui/MiddleSearchHomepage.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
@@ -148,7 +149,7 @@ internal fun MiddleSearchHomepage(
 private fun Scrim(onDismiss: () -> Unit) {
     Box(
         modifier = Modifier
-            .background(FirefoxTheme.colors.layerScrim.copy(alpha = 0.75f))
+            .background(MaterialTheme.colorScheme.scrim.copy(alpha = 0.75f))
             .fillMaxSize()
             .pointerInput(Unit) {
                 detectTapGestures(onTap = { onDismiss() })
