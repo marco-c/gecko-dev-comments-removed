4e4773089ca73a420b3dc7b652c549125a1d1e78
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-12-16 03:32:53 +0000

    Bug 2003598 - Add Chat service with fetch with history r=tzhang,ai-models-reviewers
    Bringing over work done for Smart Assist Engine specifically the FXA and fetch with history streaming function.
    
    Differential Revision: https://phabricator.services.mozilla.com/D274803
    
    UltraBlame original commit: ce5d3136032b24136054acc03a1dca046808bdba

--- browser/base/content/test/static/browser_all_files_referenced.js
+++ browser/base/content/test/static/browser_all_files_referenced.js
@@ -337,6 +337,10 @@ var allowlist = [
   },
   
   {
+    file: "moz-src:///browser/components/aiwindow/models/Chat.sys.mjs",
+  },
+  
+  {
     file: "moz-src:///browser/components/aiwindow/models/ChatUtils.sys.mjs",
   },
   
