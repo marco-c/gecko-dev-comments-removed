4e13b3404e406291f625e31964252ad305f82a65
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-12-16 03:09:13 +0000

    Bug 2000393 - Add QuadHeader. r=gw
    Differential Revision: https://phabricator.services.mozilla.com/D272811
    
    UltraBlame original commit: 4d900247b2fcc1c078cfbba531bac6a061739a84

--- gfx/wr/webrender/src/renderer/mod.rs
+++ gfx/wr/webrender/src/renderer/mod.rs
@@ -129,7 +129,8 @@ pub use debug::DebugRenderer;
 pub use shade::{PendingShadersToPrecache, Shaders, SharedShaders};
 pub use vertex::{desc, VertexArrayKind, MAX_VERTEX_TEXTURE_WIDTH};
 pub use gpu_buffer::{GpuBuffer, GpuBufferF, GpuBufferBuilderF, GpuBufferI, GpuBufferBuilderI};
-pub use gpu_buffer::{GpuBufferHandle, GpuBufferAddress, GpuBufferBuilder, GpuBufferWriterF, GpuBufferBlockF, GpuBufferDataF};
+pub use gpu_buffer::{GpuBufferHandle, GpuBufferAddress, GpuBufferBuilder, GpuBufferWriterF};
+pub use gpu_buffer::{GpuBufferBlockF, GpuBufferDataF, GpuBufferDataI, GpuBufferWriterI};
 
 
 
