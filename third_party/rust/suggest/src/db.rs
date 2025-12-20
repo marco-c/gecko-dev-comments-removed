ffdd7d255e6593079edf5334ec03cde2a8b1f364
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-12-20 02:41:50 +0000

    Bug 2004391 - Update application-services version bump to new version 0376c542e4a31cde8d33dd0e8da17dcfbc6c58b2 r=bdk,ttran,credential-management-reviewers,dimi
    Differential Revision: https://phabricator.services.mozilla.com/D275237
    
    UltraBlame original commit: 044da25010e1d78ce1fc97bd091da3b444b58c94

--- third_party/rust/suggest/src/db.rs
+++ third_party/rust/suggest/src/db.rs
@@ -123,7 +123,7 @@ impl SuggestDb {
     
     
     
-    pub fn write_scope(&self) -> Result<WriteScope> {
+    pub fn write_scope(&self) -> Result<WriteScope<'_>> {
         Ok(WriteScope {
             conn: self.conn.lock(),
             scope: self.interrupt_handle.begin_interrupt_scope()?,
