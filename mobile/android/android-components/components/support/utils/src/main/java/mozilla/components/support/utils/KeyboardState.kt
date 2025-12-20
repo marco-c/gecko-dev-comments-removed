f4d23f51d9ae1cf9ea1050785bac9723d147d77d
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-12-20 02:26:07 +0000

    Bug 1988730 - part 1 - Improve and upstream the keyboardAsState API to A-C r=android-reviewers,moyin,Roger
    Saw in my testing that the current version of keyboardAsState is not entirely
    reliable and sometimes returning the wrong value.
    Checking Chrome sources I saw there the insets are derived from current
    Activity's root View, approach I also borrowed.
    
    Added also the `isImeVisible` convenience method already available in
    the jetpack Compose framework as the source of the data on Android 13+
    while on lower Android versions we'll use the more expensive but more
    reliable GlobalLayoutListener approach to know when to recheck and report
    the keyboard state.
    
    Differential Revision: https://phabricator.services.mozilla.com/D275988
    
    UltraBlame original commit: d9ee2f01223d910311fb11734e5b3e583906a27f

--- mobile/android/android-components/components/support/utils/src/main/java/mozilla/components/support/utils/KeyboardState.kt
+++ mobile/android/android-components/components/support/utils/src/main/java/mozilla/components/support/utils/KeyboardState.kt
@@ -2,37 +2,44 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-package org.mozilla.fenix.compose.utils
+package mozilla.components.support.utils
 
+import android.view.View
 import android.view.ViewTreeObserver
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.LocalActivity
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.platform.LocalView
-import org.mozilla.fenix.ext.isKeyboardVisible
-import java.lang.ref.WeakReference
+import mozilla.components.support.utils.ext.isKeyboardVisible
 
 /**
  * Detects if the keyboard is opened or closed and returns as a [KeyboardState].
  */
+@OptIn(ExperimentalLayoutApi::class)
 @Composable
 fun keyboardAsState(): State<KeyboardState> {
     val keyboardState = remember { mutableStateOf(KeyboardState.Closed) }
-    val view = WeakReference(LocalView.current)
-    DisposableEffect(view) {
-        val onGlobalListener = ViewTreeObserver.OnGlobalLayoutListener {
-            keyboardState.value = if (view.get()?.isKeyboardVisible() == true) {
+
+    val currentActivity = (LocalActivity.current as? ComponentActivity)
+    DisposableEffect(currentActivity) {
+        var layoutListener: ViewTreeObserver.OnGlobalLayoutListener?
+        val contentView: View? = currentActivity?.findViewById(android.R.id.content)
+
+        layoutListener = ViewTreeObserver.OnGlobalLayoutListener {
+            keyboardState.value = if (contentView?.isKeyboardVisible() == true) {
                 KeyboardState.Opened
             } else {
                 KeyboardState.Closed
             }
         }
-        view.get()?.viewTreeObserver?.addOnGlobalLayoutListener(onGlobalListener)
+        contentView?.viewTreeObserver?.addOnGlobalLayoutListener(layoutListener)
 
         onDispose {
-            view.get()?.viewTreeObserver?.removeOnGlobalLayoutListener(onGlobalListener)
+            contentView?.viewTreeObserver?.removeOnGlobalLayoutListener(layoutListener)
         }
     }
 
