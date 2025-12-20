3fcb3dcee2f05ab4bc30e3c38816e852748d9af7
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-12-20 02:25:16 +0000

    Bug 2005571 - Make sure to always set mTriggeringNavigationType. r=smaug,avandolder
    Differential Revision: https://phabricator.services.mozilla.com/D276030
    
    UltraBlame original commit: 663ee3f4482e25dd3654e2cead2871760ca8446b

--- docshell/base/CanonicalBrowsingContext.h
+++ docshell/base/CanonicalBrowsingContext.h
@@ -598,7 +598,7 @@ class CanonicalBrowsingContext final : public BrowsingContext {
   }
 
   already_AddRefed<nsDocShellLoadState> CreateLoadInfo(
-      SessionHistoryEntry* aEntry);
+      SessionHistoryEntry* aEntry, NavigationType aNavigationType);
 
   void GetContiguousEntriesForLoad(LoadingSessionHistoryInfo& aLoadingInfo,
                                    const RefPtr<SessionHistoryEntry>& aEntry);
