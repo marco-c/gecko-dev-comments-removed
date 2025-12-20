37dc2ec6770868ab5224b20aca298be6c4582588
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-12-20 02:19:51 +0000

    Bug 2004237 - [devtools] Turn devtools/shared/heapsnapshot/DominatorTreeNode.js into an ES class. r=devtools-reviewers,nchevobbe
    Differential Revision: https://phabricator.services.mozilla.com/D276751
    
    UltraBlame original commit: b9a1a0f19fa80cb69fd645cbed09086f161d61e7

--- devtools/shared/heapsnapshot/DominatorTreeNode.js
+++ devtools/shared/heapsnapshot/DominatorTreeNode.js
@@ -22,353 +22,338 @@ const DEFAULT_MAX_NUM_PATHS = 5;
 
 
 
+class LabelAndShallowSizeVisitor extends Visitor {
+  constructor() {
+    super();
+
+    
+    this._labelPieces = [];
+
+    
+    
+    this._label = undefined;
+
+    
+    
+    this._shallowSize = 0;
+  }
+  
 
 
-function DominatorTreeNode(nodeId, label, shallowSize, retainedSize) {
+  enter(breakdown, report, edge) {
+    if (this._labelPieces && edge) {
+      this._labelPieces.push(edge);
+    }
+  }
   
-  this.nodeId = nodeId;
 
-  
-  this.label = label;
 
+  exit(breakdown, report, edge) {
+    if (this._labelPieces && edge) {
+      this._labelPieces.pop();
+    }
+  }
   
-  this.shallowSize = shallowSize;
 
-  
-  this.retainedSize = retainedSize;
 
-  
-  this.parentId = undefined;
+  count(breakdown, report) {
+    if (report.count === 0) {
+      return;
+    }
 
-  
-  this.children = undefined;
+    this._label = this._labelPieces;
+    this._labelPieces = undefined;
 
+    this._shallowSize = report.bytes;
+  }
   
-  
-  this.shortestPaths = undefined;
 
+
+
+
+  label() {
+    return this._label;
+  }
   
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  this.moreChildrenAvailable = true;
+
+
+
+
+  shallowSize() {
+    return this._shallowSize;
+  }
 }
 
-DominatorTreeNode.prototype = null;
-
-module.exports = DominatorTreeNode;
 
 
 
-
-
-
-
-DominatorTreeNode.addChild = function (parent, child) {
-  if (parent.children === undefined) {
-    parent.children = [];
-  }
-
-  parent.children.push(child);
-  child.parentId = parent.nodeId;
-};
-
-
-
-
-
-function LabelAndShallowSizeVisitor() {
+class DominatorTreeNode {
   
-  this._labelPieces = [];
+
+
+
+  constructor(nodeId, label, shallowSize, retainedSize) {
+    
+    this.nodeId = nodeId;
+
+    
+    this.label = label;
+
+    
+    this.shallowSize = shallowSize;
+
+    
+    this.retainedSize = retainedSize;
+
+    
+    this.parentId = undefined;
+
+    
+    this.children = undefined;
+
+    
+    
+    this.shortestPaths = undefined;
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    this.moreChildrenAvailable = true;
+  }
 
   
+
+
+
+
+
+  static addChild(parent, child) {
+    if (parent.children === undefined) {
+      parent.children = [];
+    }
+
+    parent.children.push(child);
+    child.parentId = parent.nodeId;
+  }
+
   
-  this._label = undefined;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  static getLabelAndShallowSize(nodeId, snapshot, breakdown) {
+    const description = snapshot.describeNode(breakdown, nodeId);
+
+    const visitor = new LabelAndShallowSizeVisitor();
+    walk(breakdown, description, visitor);
+
+    return {
+      label: visitor.label(),
+      shallowSize: visitor.shallowSize(),
+    };
+  }
 
   
-  
-  this._shallowSize = 0;
-}
 
-DominatorTreeNode.LabelAndShallowSizeVisitor = LabelAndShallowSizeVisitor;
 
-LabelAndShallowSizeVisitor.prototype = Object.create(Visitor);
 
 
 
 
-LabelAndShallowSizeVisitor.prototype.enter = function (
-  breakdown,
-  report,
-  edge
-) {
-  if (this._labelPieces && edge) {
-    this._labelPieces.push(edge);
-  }
-};
 
 
 
 
-LabelAndShallowSizeVisitor.prototype.exit = function (breakdown, report, edge) {
-  if (this._labelPieces && edge) {
-    this._labelPieces.pop();
-  }
-};
 
 
 
 
-LabelAndShallowSizeVisitor.prototype.count = function (breakdown, report) {
-  if (report.count === 0) {
-    return;
-  }
 
-  this._label = this._labelPieces;
-  this._labelPieces = undefined;
 
-  this._shallowSize = report.bytes;
-};
-
-
-
-
-
-
-LabelAndShallowSizeVisitor.prototype.label = function () {
-  return this._label;
-};
-
-
-
-
-
-
-LabelAndShallowSizeVisitor.prototype.shallowSize = function () {
-  return this._shallowSize;
-};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-DominatorTreeNode.getLabelAndShallowSize = function (
-  nodeId,
-  snapshot,
-  breakdown
-) {
-  const description = snapshot.describeNode(breakdown, nodeId);
-
-  const visitor = new LabelAndShallowSizeVisitor();
-  walk(breakdown, description, visitor);
-
-  return {
-    label: visitor.label(),
-    shallowSize: visitor.shallowSize(),
-  };
-};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-DominatorTreeNode.partialTraversal = function (
-  dominatorTree,
-  snapshot,
-  breakdown,
-  maxDepth = DEFAULT_MAX_DEPTH,
-  maxSiblings = DEFAULT_MAX_SIBLINGS
-) {
-  function dfs(nodeId, depth) {
-    const { label, shallowSize } = DominatorTreeNode.getLabelAndShallowSize(
-      nodeId,
-      snapshot,
-      breakdown
-    );
-    const retainedSize = dominatorTree.getRetainedSize(nodeId);
-    const node = new DominatorTreeNode(
-      nodeId,
-      label,
-      shallowSize,
-      retainedSize
-    );
-    const childNodeIds = dominatorTree.getImmediatelyDominated(nodeId);
-
-    const newDepth = depth + 1;
-    if (newDepth < maxDepth) {
-      const endIdx = Math.min(childNodeIds.length, maxSiblings);
-      for (let i = 0; i < endIdx; i++) {
-        DominatorTreeNode.addChild(node, dfs(childNodeIds[i], newDepth));
-      }
-      node.moreChildrenAvailable = endIdx < childNodeIds.length;
-    } else {
-      node.moreChildrenAvailable = !!childNodeIds.length;
-    }
-
-    return node;
-  }
-
-  return dfs(dominatorTree.root, 0);
-};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-DominatorTreeNode.insert = function (
-  nodeTree,
-  path,
-  newChildren,
-  moreChildrenAvailable
-) {
-  function insert(tree, i) {
-    if (tree.nodeId !== path[i]) {
-      return tree;
-    }
-
-    if (i == path.length - 1) {
-      return immutableUpdate(tree, {
-        children: (tree.children || []).concat(newChildren),
-        moreChildrenAvailable,
-      });
-    }
-
-    return tree.children
-      ? immutableUpdate(tree, {
-          children: tree.children.map(c => insert(c, i + 1)),
-        })
-      : tree;
-  }
-
-  return insert(nodeTree, 0);
-};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-DominatorTreeNode.getNodeByIdAlongPath = function (id, tree, path) {
-  function find(node, i) {
-    if (!node || node.nodeId !== path[i]) {
-      return null;
-    }
-
-    if (node.nodeId === id) {
-      return node;
-    }
-
-    if (i === path.length - 1 || !node.children) {
-      return null;
-    }
-
-    const nextId = path[i + 1];
-    return find(
-      node.children.find(c => c.nodeId === nextId),
-      i + 1
-    );
-  }
-
-  return find(tree, 0);
-};
-
-
-
-
-
-
-
-
-
-
-
-DominatorTreeNode.attachShortestPaths = function (
-  snapshot,
-  breakdown,
-  start,
-  treeNodes,
-  maxNumPaths = DEFAULT_MAX_NUM_PATHS
-) {
-  const idToTreeNode = new Map();
-  const targets = [];
-  for (const node of treeNodes) {
-    const id = node.nodeId;
-    idToTreeNode.set(id, node);
-    targets.push(id);
-  }
-
-  const shortestPaths = snapshot.computeShortestPaths(
-    start,
-    targets,
-    maxNumPaths
-  );
-
-  for (const [target, paths] of shortestPaths) {
-    const deduped = deduplicatePaths(target, paths);
-    deduped.nodes = deduped.nodes.map(id => {
-      const { label } = DominatorTreeNode.getLabelAndShallowSize(
-        id,
+  static partialTraversal(
+    dominatorTree,
+    snapshot,
+    breakdown,
+    maxDepth = DEFAULT_MAX_DEPTH,
+    maxSiblings = DEFAULT_MAX_SIBLINGS
+  ) {
+    function dfs(nodeId, depth) {
+      const { label, shallowSize } = DominatorTreeNode.getLabelAndShallowSize(
+        nodeId,
         snapshot,
         breakdown
       );
-      return { id, label };
-    });
+      const retainedSize = dominatorTree.getRetainedSize(nodeId);
+      const node = new DominatorTreeNode(
+        nodeId,
+        label,
+        shallowSize,
+        retainedSize
+      );
+      const childNodeIds = dominatorTree.getImmediatelyDominated(nodeId);
 
-    idToTreeNode.get(target).shortestPaths = deduped;
+      const newDepth = depth + 1;
+      if (newDepth < maxDepth) {
+        const endIdx = Math.min(childNodeIds.length, maxSiblings);
+        for (let i = 0; i < endIdx; i++) {
+          DominatorTreeNode.addChild(node, dfs(childNodeIds[i], newDepth));
+        }
+        node.moreChildrenAvailable = endIdx < childNodeIds.length;
+      } else {
+        node.moreChildrenAvailable = !!childNodeIds.length;
+      }
+
+      return node;
+    }
+
+    return dfs(dominatorTree.root, 0);
   }
-};
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+  static insert(nodeTree, path, newChildren, moreChildrenAvailable) {
+    function insert(tree, i) {
+      if (tree.nodeId !== path[i]) {
+        return tree;
+      }
+
+      if (i == path.length - 1) {
+        return immutableUpdate(tree, {
+          children: (tree.children || []).concat(newChildren),
+          moreChildrenAvailable,
+        });
+      }
+
+      return tree.children
+        ? immutableUpdate(tree, {
+            children: tree.children.map(c => insert(c, i + 1)),
+          })
+        : tree;
+    }
+
+    return insert(nodeTree, 0);
+  }
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  static getNodeByIdAlongPath(id, tree, path) {
+    function find(node, i) {
+      if (!node || node.nodeId !== path[i]) {
+        return null;
+      }
+
+      if (node.nodeId === id) {
+        return node;
+      }
+
+      if (i === path.length - 1 || !node.children) {
+        return null;
+      }
+
+      const nextId = path[i + 1];
+      return find(
+        node.children.find(c => c.nodeId === nextId),
+        i + 1
+      );
+    }
+
+    return find(tree, 0);
+  }
+
+  
+
+
+
+
+
+
+
+
+
+  static attachShortestPaths(
+    snapshot,
+    breakdown,
+    start,
+    treeNodes,
+    maxNumPaths = DEFAULT_MAX_NUM_PATHS
+  ) {
+    const idToTreeNode = new Map();
+    const targets = [];
+    for (const node of treeNodes) {
+      const id = node.nodeId;
+      idToTreeNode.set(id, node);
+      targets.push(id);
+    }
+
+    const shortestPaths = snapshot.computeShortestPaths(
+      start,
+      targets,
+      maxNumPaths
+    );
+
+    for (const [target, paths] of shortestPaths) {
+      const deduped = deduplicatePaths(target, paths);
+      deduped.nodes = deduped.nodes.map(id => {
+        const { label } = DominatorTreeNode.getLabelAndShallowSize(
+          id,
+          snapshot,
+          breakdown
+        );
+        return { id, label };
+      });
+
+      idToTreeNode.get(target).shortestPaths = deduped;
+    }
+  }
+
+  static LabelAndShallowSizeVisitor = LabelAndShallowSizeVisitor;
+}
+
+module.exports = DominatorTreeNode;
