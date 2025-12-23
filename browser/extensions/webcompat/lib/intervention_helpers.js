fba568babf9ef343f40344123ee6558618747bfc
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-12-23 18:55:11 +0000

    Bug 2004679 - add a UA override for paybill.com; r=webcompat-reviewers,ksenia
    Differential Revision: https://phabricator.services.mozilla.com/D276948
    
    UltraBlame original commit: 280d0e66e19b8855eec753467cb9af572269c7a6

--- browser/extensions/webcompat/lib/intervention_helpers.js
+++ browser/extensions/webcompat/lib/intervention_helpers.js
@@ -267,6 +267,9 @@ var InterventionHelpers = {
     mimic_Android_Hotspot2_device: ua => {
       return UAHelpers.androidHotspot2Device(ua);
     },
+    replace_colon_in_rv_with_space: ua => {
+      return ua.replace("rv:", "rv ");
+    },
     reduce_firefox_version_by_one: ua => {
       const [head, fx, tail] = ua.split(/(firefox\/)/i);
       if (!fx || !tail) {
