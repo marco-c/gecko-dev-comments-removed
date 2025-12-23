2a9236113368ad8951667eaf528ed0886fe441ce
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-12-23 18:52:53 +0000

    Bug 1996724 - implement 2-QWACs r=jschanck
    Differential Revision: https://phabricator.services.mozilla.com/D271162
    
    UltraBlame original commit: 8c2bb2130dde9233531c663b184231628aad609b

--- security/manager/ssl/tests/unit/head_psm.js
+++ security/manager/ssl/tests/unit/head_psm.js
@@ -165,8 +165,8 @@ function arrayToString(a) {
 
 function pemToBase64(pem) {
   return pem
-    .replace(/-----BEGIN CERTIFICATE-----/, "")
-    .replace(/-----END CERTIFICATE-----/, "")
+    .replace(/-----BEGIN (CERTIFICATE|(EC )?PRIVATE KEY)-----/, "")
+    .replace(/-----END (CERTIFICATE|(EC )?PRIVATE KEY)-----/, "")
     .replace(/[\r\n]/g, "");
 }
 
