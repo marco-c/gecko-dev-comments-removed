5dd60fae8753da9f42df7da70c7b91b7f72e7e92
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-12-23 18:52:01 +0000

    Bug 2006934 - Revert bug 2001152 due to instability when failing start the GPU process. r=ipc-reviewers,gfx-reviewers,mccr8,jnicol
    This was causing infinite loops during fallback, eventually resulting
    in an OOM crash.
    
    Differential Revision: https://phabricator.services.mozilla.com/D277221
    
    UltraBlame original commit: 4e1c7f93692cd562d30420c4a3d0d92e1ac1f5d5

--- gfx/ipc/GPUParent.cpp
+++ gfx/ipc/GPUParent.cpp
@@ -46,6 +46,7 @@
 #include "mozilla/ipc/CrashReporterClient.h"
 #include "mozilla/ipc/ProcessChild.h"
 #include "mozilla/ipc/ProcessUtils.h"
+#include "mozilla/layers/APZInputBridgeParent.h"
 #include "mozilla/layers/APZPublicUtils.h"  
 #include "mozilla/layers/APZThreadUtils.h"
 #include "mozilla/layers/CompositorBridgeParent.h"
@@ -54,6 +55,7 @@
 #include "mozilla/layers/ImageBridgeParent.h"
 #include "mozilla/layers/LayerTreeOwnerTracker.h"
 #include "mozilla/layers/RemoteTextureMap.h"
+#include "mozilla/layers/UiCompositorControllerParent.h"
 #include "mozilla/layers/VideoBridgeParent.h"
 #include "mozilla/webrender/RenderThread.h"
 #include "mozilla/webrender/WebRenderAPI.h"
@@ -467,6 +469,20 @@ mozilla::ipc::IPCResult GPUParent::RecvInitVRManager(
 mozilla::ipc::IPCResult GPUParent::RecvInitVR(
     Endpoint<PVRGPUChild>&& aEndpoint) {
   gfx::VRGPUChild::InitForGPUProcess(std::move(aEndpoint));
+  return IPC_OK();
+}
+
+mozilla::ipc::IPCResult GPUParent::RecvInitUiCompositorController(
+    const LayersId& aRootLayerTreeId,
+    Endpoint<PUiCompositorControllerParent>&& aEndpoint) {
+  UiCompositorControllerParent::Start(aRootLayerTreeId, std::move(aEndpoint));
+  return IPC_OK();
+}
+
+mozilla::ipc::IPCResult GPUParent::RecvInitAPZInputBridge(
+    const LayersId& aRootLayerTreeId,
+    Endpoint<PAPZInputBridgeParent>&& aEndpoint) {
+  APZInputBridgeParent::Create(aRootLayerTreeId, std::move(aEndpoint));
   return IPC_OK();
 }
 
