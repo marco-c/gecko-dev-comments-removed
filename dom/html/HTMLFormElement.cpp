84e7cd707050ddc0b1d9e022159e372a2ceefc73
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-12-23 19:13:23 +0000

    Bug 2006613 - Fix some ASAN issues.
    Gotta love C++. See bug 2007450 for the underlying issue.
    
    ClearForm(false) is harmless so no need to reverse.
    
    UltraBlame original commit: b6b767e1beb220d6888e2631f4ad86807cbcfc1c

--- dom/html/HTMLFormElement.cpp
+++ dom/html/HTMLFormElement.cpp
@@ -1619,7 +1619,9 @@ bool HTMLFormElement::IsLastActiveElement(
     const nsGenericHTMLFormElement* aElement) const {
   MOZ_ASSERT(aElement, "Unexpected call");
 
-  for (auto* element : Reversed(mControls->mElements.AsSpan())) {
+  
+  Span elements = mControls->mElements.AsSpan();
+  for (auto* element : Reversed(elements)) {
     const auto* fc = nsIFormControl::FromNode(element);
     MOZ_ASSERT(fc);
     
@@ -1744,7 +1746,7 @@ int32_t HTMLFormElement::IndexOfContent(nsIContent* aContent) {
 }
 
 void HTMLFormElement::Clear() {
-  for (HTMLImageElement* image : Reversed(mImageElements.AsSpan())) {
+  for (HTMLImageElement* image : mImageElements.AsSpan()) {
     image->ClearForm(false);
   }
   mImageElements.Clear();
