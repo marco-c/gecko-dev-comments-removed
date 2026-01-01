8ad78dba18fefc9662c1125147d03ced12bda659
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-01-01 01:43:27 +0000

    Bug 1895717 - Part 1: Cleanup TranslationDialogFragment r=android-reviewers,petru
    Differential Revision: https://phabricator.services.mozilla.com/D277232
    
    UltraBlame original commit: 934dba6c132ba3ec38725396decf0fa28da55367

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/translations/TranslationsDialogFragment.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/translations/TranslationsDialogFragment.kt
@@ -16,13 +16,9 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.ComposeView
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.platform.ViewCompositionStrategy
-import androidx.compose.ui.unit.dp
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.google.android.material.bottomsheet.BottomSheetBehavior
@@ -121,20 +117,6 @@ class TranslationsDialogFragment : BottomSheetDialogFragment() {
                     )
                 }
 
-                var translationsHeightDp by remember {
-                    mutableStateOf(0.dp)
-                }
-
-                var translationsOptionsHeightDp by remember {
-                    mutableStateOf(0.dp)
-                }
-
-                var translationsWidthDp by remember {
-                    mutableStateOf(0.dp)
-                }
-
-                val density = LocalDensity.current
-
                 val translationsDialogState =
                     translationsDialogStore.observeAsComposableState { it }.value
 
@@ -163,43 +145,26 @@ class TranslationsDialogFragment : BottomSheetDialogFragment() {
                         showMainSheet = translationsVisibility,
                     ) { showMainPage ->
                         if (showMainPage) {
-                            Column(
-                                modifier = Modifier.onGloballyPositioned { coordinates ->
-                                    translationsHeightDp = with(density) {
-                                        coordinates.size.height.toDp()
-                                    }
-                                    translationsWidthDp = with(density) {
-                                        coordinates.size.width.toDp()
-                                    }
-                                },
-                            ) {
-                                translationsDialogState.let {
-                                    TranslationsDialogContent(
-                                        learnMoreUrl = learnMoreUrl,
-                                        showPageSettings = FxNimbus.features.translations.value().pageSettingsEnabled,
-                                        translationsDialogState = it,
-                                        onSettingClicked = {
-                                            Translations.action.record(
-                                                Translations.ActionExtra(
-                                                    "page_settings",
-                                                ),
-                                            )
-                                            translationsVisibility = false
-                                        },
-                                        onShowDownloadLanguageFileDialog = {
-                                            showDownloadLanguageFileDialog = true
-                                        },
-                                    )
-                                }
+                            Column {
+                                TranslationsDialogContent(
+                                    learnMoreUrl = learnMoreUrl,
+                                    showPageSettings = FxNimbus.features.translations.value().pageSettingsEnabled,
+                                    translationsDialogState = translationsDialogState,
+                                    onSettingClicked = {
+                                        Translations.action.record(
+                                            Translations.ActionExtra(
+                                                "page_settings",
+                                            ),
+                                        )
+                                        translationsVisibility = false
+                                    },
+                                    onShowDownloadLanguageFileDialog = {
+                                        showDownloadLanguageFileDialog = true
+                                    },
+                                )
                             }
                         } else {
-                            Column(
-                                modifier = Modifier.onGloballyPositioned { coordinates ->
-                                    translationsOptionsHeightDp = with(density) {
-                                        coordinates.size.height.toDp()
-                                    }
-                                },
-                            ) {
+                            Column {
                                 TranslationsOptionsDialogContent(
                                     learnMoreUrl = learnMoreUrl,
                                     showGlobalSettings = FxNimbus.features.translations.value().globalSettingsEnabled,
