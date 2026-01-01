68e11178ec75be97d54d32abdf04066b52857615
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-01-01 01:56:10 +0000

    Bug 2006959 - Even if there's no anchor-positioning cache, we need to save and potentially restore the target's firstTryNormalPosition. r=layout-anchor-positioning-reviewers,layout-reviewers,emilio
    Differential Revision: https://phabricator.services.mozilla.com/D277619
    
    UltraBlame original commit: e4ec9742d6dfd041438524b8bec968d02d9cd371

--- layout/generic/AbsoluteContainingBlock.cpp
+++ layout/generic/AbsoluteContainingBlock.cpp
@@ -1634,6 +1634,10 @@ void AbsoluteContainingBlock::ReflowAbsoluteFrame(
     aKidFrame->DidReflow(aPresContext, &kidReflowInput);
 
     [&]() {
+      const auto position = aKidFrame->GetPosition();
+      if (!firstTryNormalPosition) {
+        firstTryNormalPosition = Some(position);
+      }
       if (!aAnchorPosResolutionCache) {
         return;
       }
@@ -1653,12 +1657,8 @@ void AbsoluteContainingBlock::ReflowAbsoluteFrame(
             aAnchorPosResolutionCache->mDefaultAnchorCache);
       }();
       
-      const auto position = aKidFrame->GetPosition();
       
       
-      if (!firstTryNormalPosition) {
-        firstTryNormalPosition = Some(position);
-      }
       aKidFrame->SetProperty(nsIFrame::NormalPositionProperty(), position);
       if (offset != nsPoint{}) {
         aKidFrame->SetPosition(position - offset);
@@ -1734,8 +1734,7 @@ void AbsoluteContainingBlock::ReflowAbsoluteFrame(
   } while (true);
 
   [&]() {
-    if (!isOverflowingCB || !aAnchorPosResolutionCache ||
-        !firstTryNormalPosition) {
+    if (!isOverflowingCB || !firstTryNormalPosition) {
       return;
     }
     
@@ -1749,9 +1748,11 @@ void AbsoluteContainingBlock::ReflowAbsoluteFrame(
       aKidFrame->SetProperty(nsIFrame::NormalPositionProperty(),
                              normalPosition);
     }
-    const auto position =
-        normalPosition -
-        aAnchorPosResolutionCache->mReferenceData->mDefaultScrollShift;
+    auto position = normalPosition;
+    if (aAnchorPosResolutionCache) {
+      position -=
+          aAnchorPosResolutionCache->mReferenceData->mDefaultScrollShift;
+    }
     const auto oldPosition = aKidFrame->GetPosition();
     if (position == oldPosition) {
       return;
