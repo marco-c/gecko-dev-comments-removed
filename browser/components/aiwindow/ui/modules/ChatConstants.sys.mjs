3ac939bb5514ba1662821c9928fb627cad13a72f
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-01-01 01:55:56 +0000

    Bug 2007454 - Add chatHistoryView() method to support Chat History View r=mlucks,mak,ai-frontend-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D277397
    
    UltraBlame original commit: 80c2335a6c27175e0ec947621c14ee1f5f0ec6ae

--- browser/components/aiwindow/ui/modules/ChatConstants.sys.mjs
+++ browser/components/aiwindow/ui/modules/ChatConstants.sys.mjs
@@ -6,7 +6,7 @@
 /**
  * The current SQLite database schema version
  */
-export const CURRENT_SCHEMA_VERSION = 1;
+export const CURRENT_SCHEMA_VERSION = 2;
 
 /**
  * The directory that the SQLite database lives in
