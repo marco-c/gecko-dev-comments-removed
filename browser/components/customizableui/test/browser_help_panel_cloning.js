328235bf90ec490c513530e88a96a0b2c6253c9f
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-01-07 19:10:29 +0000

    Bug 2008041 - Actually fix browser_help_panel_cloning.js.
    UltraBlame original commit: bc12f24eec83198ec5ab14296ee9ee808b00d5c7

--- browser/components/customizableui/test/browser_help_panel_cloning.js
+++ browser/components/customizableui/test/browser_help_panel_cloning.js
@@ -78,7 +78,7 @@ add_task(async function test_help_panel_cloning() {
           appMenuHelpItem.getAttribute("oncommand"),
           "oncommand was properly cloned."
         );
-      } else if (attr == "command") {
+      } else if (attr == "disabled") {
         
         Assert.equal(
           helpMenuPopupItem.hasAttribute(attr),
