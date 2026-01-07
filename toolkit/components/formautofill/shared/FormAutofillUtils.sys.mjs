54b816354d6f1dba26ce81f828587ed3c596eccd
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-01-07 18:52:03 +0000

    Bug 2006756, don't treat single email fields as being a valid formfill section as those are often going to be login forms, r=credential-management-reviewers,dimi
    Differential Revision: https://phabricator.services.mozilla.com/D277122
    
    UltraBlame original commit: eb811e7c86a953814b1d4795e9e57afa0693411e

--- toolkit/components/formautofill/shared/FormAutofillUtils.sys.mjs
+++ toolkit/components/formautofill/shared/FormAutofillUtils.sys.mjs
@@ -205,8 +205,15 @@ FormAutofillUtils = {
   },
 
   isValidSection(fieldDetails) {
-    // If one of the fields has the autocomplete reason, the section is valid.
-    if (fieldDetails.some(f => f.reason == "autocomplete")) {
+    // If one of the fields has the autocomplete reason, the section is valid,
+    // except for email fields since those are often login forms.
+    // Bug 2008553 - should find a way to display an email dropdown if this
+    // isn't a login form.
+    if (
+      fieldDetails.some(
+        f => f.reason == "autocomplete" && f.fieldName != "email"
+      )
+    ) {
       return true;
     }
 
