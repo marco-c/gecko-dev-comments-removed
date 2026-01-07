1cadf3a878b6b2d44e3558f0c50fe5edcd1bf8db
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-01-07 18:53:28 +0000

    Bug 2000070 - "add note" button on tab hover preview r=dwalker,jswinarton,desktop-theme-reviewers,tabbrowser-reviewers
    When hovering over a tab with a loaded web page, the tab hover preview panel will now display an "Add note" button if the tab does not yet have a tab note. Clicking the "Add note" button will open the "Add note" tab note panel and close the tab hover preview panel.
    
    Differential Revision: https://phabricator.services.mozilla.com/D275678
    
    UltraBlame original commit: 04c71f956e93f0f1204eded27b29d6562ec1b4bb

--- browser/components/tabbrowser/content/tab-hover-preview.mjs
+++ browser/components/tabbrowser/content/tab-hover-preview.mjs
@@ -274,8 +274,15 @@ class TabPanel extends HoverPanel {
 
     this.#tab = null;
     this.#thumbnailElement = null;
+
+    this.panelElement
+      .querySelector(".tab-preview-add-note")
+      .addEventListener("click", () => this.#openTabNotePanel());
   }
 
+  /**
+   * @param {Event} e
+   */
   handleEvent(e) {
     switch (e.type) {
       case "popupshowing":
@@ -334,6 +341,11 @@ class TabPanel extends HoverPanel {
     }
   }
 
+  /**
+   * @param {MozTabbrowserTab} [leavingTab]
+   * @param {object} [options]
+   * @param {boolean} [options.force=false]
+   */
   deactivate(leavingTab = null, { force = false } = {}) {
     if (!this._prefUseTabNotes) {
       force = true;
@@ -475,6 +487,16 @@ class TabPanel extends HoverPanel {
       : "";
   }
 
+  /**
+   * Opens the tab note menu in the context of the current tab. Since only
+   * one panel should be open at a time, this also closes the tab hover preview
+   * panel.
+   */
+  #openTabNotePanel() {
+    this.win.gBrowser.tabNoteMenu.openPanel(this.#tab);
+    this.deactivate(this.#tab, { force: true });
+  }
+
   #updatePreview(tab = null) {
     if (tab) {
       this.#tab = tab;
@@ -496,10 +518,21 @@ class TabPanel extends HoverPanel {
         "";
     }
 
-    lazy.TabNotes.get(this.#tab).then(note => {
-      this.panelElement.querySelector(".tab-note-text-container").textContent =
-        note?.text || "";
-    });
+    const noteTextContainer = this.panelElement.querySelector(
+      ".tab-note-text-container"
+    );
+    const addNoteButton = this.panelElement.querySelector(
+      ".tab-preview-add-note"
+    );
+    if (this._prefUseTabNotes && lazy.TabNotes.isEligible(this.#tab)) {
+      lazy.TabNotes.get(this.#tab).then(note => {
+        noteTextContainer.textContent = note?.text || "";
+        addNoteButton.toggleAttribute("hidden", !!note);
+      });
+    } else {
+      noteTextContainer.textContent = "";
+      addNoteButton.setAttribute("hidden", "");
+    }
 
     let thumbnailContainer = this.panelElement.querySelector(
       ".tab-preview-thumbnail-container"
