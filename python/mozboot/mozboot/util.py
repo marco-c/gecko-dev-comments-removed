2d55438165f3951a6e8bdef553aeef5e617b570a
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-01-07 18:43:15 +0000

    Bug 1986626 - Update minimum Rust version to 1.87. r=firefox-build-system-reviewers,sergesanspaille
    Differential Revision: https://phabricator.services.mozilla.com/D275580
    
    UltraBlame original commit: c728cdbe8448f9f01c5f6f0eaaf9f3f3980a70ec

--- python/mozboot/mozboot/util.py
+++ python/mozboot/mozboot/util.py
@@ -13,7 +13,7 @@ from mach.site import PythonVirtualenv
 from mach.util import get_state_dir
 
 
-MINIMUM_RUST_VERSION = "1.82.0"
+MINIMUM_RUST_VERSION = "1.87.0"
 
 
 def get_tools_dir(srcdir=False):
