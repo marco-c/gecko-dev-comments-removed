d6f18d9e55a0ba201987a9b06d669fbd08b747a8
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-02-01 04:40:52 +0000

    Bug 1966744 - [devtools] Wait for storage items by default in storage cache tests r=nchevobbe
    Differential Revision: https://phabricator.services.mozilla.com/D279859
    
    UltraBlame original commit: 9fba131931182b4bae3f8ec1f6733ea417dee97b

--- devtools/client/storage/test/head.js
+++ devtools/client/storage/test/head.js
@@ -484,14 +484,30 @@ function matchVariablesViewProperty(prop, rule) {
 
 
 
-async function selectTreeItem(ids) {
+
+
+
+
+
+async function selectTreeItem(ids, options = {}) {
+  const { waitForItem = true } = options;
+
   if (gUI.tree.isSelected(ids)) {
     info(`"${ids}" is already selected, returning.`);
     return;
   }
+
   if (!gUI.tree.exists(ids)) {
-    info(`"${ids}" does not exist, returning.`);
-    return;
+    if (!waitForItem) {
+      info(`${ids} is unavailable and waitForItem=false, returning`);
+      return;
+    }
+
+    info(`Wait until ${ids} the expected item appears in the tree`);
+    await waitFor(
+      () => gUI.tree.exists(ids),
+      `Waiting until ${ids} exists in the storage tree`
+    );
   }
 
   
@@ -924,7 +940,8 @@ async function checkState(state) {
   for (const [store, names] of state) {
     const storeName = store.join(" > ");
     info(`Selecting tree item ${storeName}`);
-    await selectTreeItem(store);
+    
+    await selectTreeItem(store, { waitForItem: false });
 
     const items = gUI.table.items;
 
@@ -1044,7 +1061,9 @@ async function performAdd(store) {
   const toolbar = gPanelWindow.document.getElementById("storage-toolbar");
   const type = store[0];
 
-  await selectTreeItem(store);
+  
+  
+  await selectTreeItem(store, { waitForItem: false });
 
   const menuAdd = toolbar.querySelector("#add-button");
 
