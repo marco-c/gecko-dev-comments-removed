ea971979ce3cfa3aa2bd3f6f646b8091ebf4f636
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-02-01 01:57:36 +0000

    Revert "Bug 1957751 - Annotate some more passes." for causing chrome failures on test_moz_button.html
    This reverts commit 4b2c9698ccbd4c5523f2e16ab99e28a2b2a69916.
    
    Revert "Bug 1957751 - Serialize background-size and mask-size as two values. r=firefox-style-system-reviewers,emilio"
    
    This reverts commit 123a6b978c196a5b2df2544f4007cad9f6cb9227.
    
    UltraBlame original commit: 6ddf8396c1e7c687c3e41fa22cdbc771767e84d7

--- servo/components/style/values/generics/background.rs
+++ servo/components/style/values/generics/background.rs
@@ -31,7 +31,7 @@ pub enum GenericBackgroundSize<LengthPercent> {
         
         width: GenericLengthPercentageOrAuto<LengthPercent>,
         
-        #[css(contextual_skip_if = "width_and_height_are_auto")]
+        #[css(skip_if = "GenericLengthPercentageOrAuto::is_auto")]
         height: GenericLengthPercentageOrAuto<LengthPercent>,
     },
     
@@ -40,11 +40,6 @@ pub enum GenericBackgroundSize<LengthPercent> {
     
     #[animation(error)]
     Contain,
-}
-
-#[inline]
-fn width_and_height_are_auto<LengthPercent>(width: &GenericLengthPercentageOrAuto<LengthPercent>, height: &GenericLengthPercentageOrAuto<LengthPercent>) -> bool {
-    width.is_auto() && height.is_auto()
 }
 
 pub use self::GenericBackgroundSize as BackgroundSize;
