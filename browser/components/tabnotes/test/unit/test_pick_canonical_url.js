e453bac109613532f0909d1cf4daed2e5a29d9f7
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-02-01 04:34:57 +0000

    Bug 2009126: Tab note fallback URLs ignore trailing question mark r=sthompson,tabbrowser-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D279339
    
    UltraBlame original commit: a8ac3f7e1892fdcf0977e7c1d568d597bec4152e

--- browser/components/tabnotes/test/unit/test_pick_canonical_url.js
+++ browser/components/tabnotes/test/unit/test_pick_canonical_url.js
@@ -3,9 +3,10 @@
 
 "use strict";
 
-const { pickCanonicalUrl } = ChromeUtils.importESModule(
-  "moz-src:///browser/components/tabnotes/CanonicalURL.sys.mjs"
-);
+const { pickCanonicalUrl, getFallbackCanonicalUrl } =
+  ChromeUtils.importESModule(
+    "moz-src:///browser/components/tabnotes/CanonicalURL.sys.mjs"
+  );
 
 const LINK_REL_CANONICAL = "https://www.example.com/link_rel_canonical";
 const OPENGRAPH = "https://www.example.com/opengraph";
@@ -95,5 +96,30 @@ add_task(async function test_fallback() {
     }),
     FALLBACK,
     "should only use the fallback if nothing else was found"
+  );
+});
+
+add_task(async function test_fallback_contains_no_fragment() {
+  const fallbackUrlWithFragment = "https://example.com/a/b/c/#somefragment";
+  const document = { documentURI: fallbackUrlWithFragment };
+  const result = getFallbackCanonicalUrl(document);
+  Assert.equal(
+    result,
+    "https://example.com/a/b/c/",
+    "Should ignore fragment on fallback URLs"
+  );
+});
+
+add_task(async function test_fallback_ignores_trailing_question() {
+  
+  
+
+  const fallbackUrlWithEmptyQuery = "https://example.com/a/b/c/?";
+  const document = { documentURI: fallbackUrlWithEmptyQuery };
+  const result = getFallbackCanonicalUrl(document);
+  Assert.equal(
+    result,
+    "https://example.com/a/b/c/",
+    "Should ignore trailing question mark on fallback URLs"
   );
 });
