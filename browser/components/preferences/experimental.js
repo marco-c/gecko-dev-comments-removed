a53d3af5a66b813d3647464dc46ff452b377770d
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-02-01 05:09:59 +0000

    Bug 2011286 - Re-render Firefox Labs when Nimbus finishes updating enrollments r=mstriemer
    Differential Revision: https://phabricator.services.mozilla.com/D279579
    
    UltraBlame original commit: e1372f8650dbc693f9754523a52e9a1887072f1a

--- browser/components/preferences/experimental.js
+++ browser/components/preferences/experimental.js
@@ -14,6 +14,9 @@ const gExperimentalPane = {
   _featureGatesContainer: null,
   _firefoxLabs: null,
 
+  
+  _renderingPromise: Promise.resolve(),
+
   async init() {
     if (this.inited) {
       return;
@@ -36,10 +39,30 @@ const gExperimentalPane = {
 
     window.addEventListener("unload", () => this._removeObservers());
 
-    await this._maybeRenderLabsRecipes();
+    await this._queueRender();
+
+    Services.obs.addObserver(this, ExperimentAPI.ENROLLMENTS_UPDATED);
+  },
+
+  
+
+
+
+
+
+
+  _queueRender() {
+    this._renderingPromise = this._renderingPromise.then(() =>
+      this._maybeRenderLabsRecipes()
+    );
+    return this._renderingPromise;
   },
 
   async _maybeRenderLabsRecipes() {
+    this._featureGatesContainer
+      .querySelectorAll(".featureGate")
+      .forEach(el => el.remove());
+
     this._firefoxLabs = await FirefoxLabs.create();
 
     const shouldHide = this._firefoxLabs.count === 0;
@@ -49,6 +72,10 @@ const gExperimentalPane = {
       return;
     }
 
+    this._renderLabsRecipes();
+  },
+
+  _renderLabsRecipes() {
     const frag = document.createDocumentFragment();
 
     const groups = new Map();
@@ -112,14 +139,6 @@ const gExperimentalPane = {
     Services.obs.notifyObservers(window, "experimental-pane-loaded");
   },
 
-  _removeLabsRecipes() {
-    ExperimentAPI.manager.store.off("update", this._onNimbusUpdate);
-
-    this._featureGatesContainer
-      .querySelectorAll(".featureGate")
-      .forEach(el => el.remove());
-  },
-
   async _onCheckboxChanged(event) {
     const target = event.target;
 
@@ -166,6 +185,8 @@ const gExperimentalPane = {
 
   _removeObservers() {
     ExperimentAPI.manager.store.off("update", this._onNimbusUpdate);
+
+    Services.obs.removeObserver(this, ExperimentAPI.ENROLLMENTS_UPDATED);
   },
 
   
@@ -195,6 +216,13 @@ const gExperimentalPane = {
     ) {
       
       gotoPref("general");
+    }
+  },
+
+  observe(_subject, topic, _data) {
+    switch (topic) {
+      case ExperimentAPI.ENROLLMENTS_UPDATED:
+        void this._queueRender();
     }
   },
 };
