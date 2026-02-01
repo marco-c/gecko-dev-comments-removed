c5cc50d65c529d770b401a4170604cf36385bd78
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-02-01 01:50:12 +0000

    Bug 2008758 [wpt PR 57009] - Expose initiator-url in resourceTiming for more resources, a=testonly
    Automatic update from web-platform-tests
    Expose initiator-url in resourceTiming for more resources
    
    TaskAttribution is extended to track `initiator-url".
    
    The progress of the "initiatorUrl" reporting after this CL is the below:
    
    A) Supported:
    
       (1) Any static resource from a html file;
       (2) Resources fetched in script, in main thread, that's not
       an imported script, under the condition that the resource fetch
       happens after script loading or beyond a number of common ways
       to dispatch an asynchronous call: postTask(); fulfillments of
       a promise; queueMicrotask(); requestAnimationFrame();
       requestIdleCallback(); setInterval(); setTimeout();
       XMLHttpRequest();
    
    B) Not Supported yet but planned:
    
       (1) CSS resources that are initiated from another CSS file;
       (2) Script resources that are imported JS;
       (3) Resources fetched in the main thread, by the script that are
           dispatched via an EventHandler, or a MessageHandler;
       (4) Resource fetched by script running in a worker thread;
    
    C) Not planned to support: any rare cases that requires significant
       efforts, and any resources fetched via deprecated APIs. Example:
    
       (1) document.write();
       (2) Resources fetched in the main thread, by the script that are
           dispatched via rarely used asynchronous calls.
    
    In addition, previous tests are re-organized and refactored.
    
    Credit to previous contributor: Some "inline-script" test cases are
    merged from(with significant modification):
    https://chromium-review.googlesource.com/c/chromium/src/+/4812813 And
    For script initiators, TaskAttributionInfo is used according to this
    draft CL:
    https://chromium-review.googlesource.com/c/chromium/src/+/4931296/21
    
    PERFETTO_TESTS=`autoninja -C out/Default perfetto_diff_tests &&
    out/Default/bin/run_perfetto_diff_tests`
    
    Bug: 40919714
    Change-Id: I125e62d488eec5a85f8afa6cd61c399183c56d86
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6960132
    Commit-Queue: Guohui Deng <guohuidengmicrosoft.com>
    Reviewed-by: Scott Haseley <shaseleychromium.org>
    Reviewed-by: Mikhail Khokhlov <khokhlovgoogle.com>
    Reviewed-by: Michal Mocny <mmocnychromium.org>
    Cr-Commit-Position: refs/heads/main{#1565205}
    
    --
    
    wpt-commits: c2537699cbe35a98bd3baa0bae6186fabc7c9651
    wpt-pr: 57009
    
    UltraBlame original commit: 13499698083abc3b3b5cb45038c2ab28d4e69eeb

---
+++ testing/web-platform/tests/resource-timing/resources/loading-resource-lib.js
@@ -0,0 +1,9 @@
+
+
+async function load_image(label, img_element) {
+  const url = "/images/blue.png?"+label;
+  const response = await fetch(url);
+  blob = await response.blob();
+  const imgURL = URL.createObjectURL(blob);
+  img_element.src = imgURL;
+}
