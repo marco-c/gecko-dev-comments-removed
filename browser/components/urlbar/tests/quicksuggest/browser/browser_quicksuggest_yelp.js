a8adbf9dbfa8e7df40c3b46974865fc83c3c029f
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-02-01 06:07:46 +0000

    Bug 2010185: Standardize row UI for Yelp suggestion r=fluent-reviewers,adw,flod
    Depends on D280386
    
    Differential Revision: https://phabricator.services.mozilla.com/D280541
    
    UltraBlame original commit: 9bf5dbfa17880304920e977f6e6596665ebab4a7

--- browser/components/urlbar/tests/quicksuggest/browser/browser_quicksuggest_yelp.js
+++ browser/components/urlbar/tests/quicksuggest/browser/browser_quicksuggest_yelp.js
@@ -51,8 +51,10 @@ add_task(async function basic() {
 
     Assert.equal(UrlbarTestUtils.getResultCount(window), 2);
 
-    const details = await UrlbarTestUtils.getDetailsOfResultAt(window, 1);
-    const { element, result } = details;
+    const { element, result } = await UrlbarTestUtils.getDetailsOfResultAt(
+      window,
+      1
+    );
     Assert.equal(
       result.providerName,
       UrlbarProviderQuickSuggest.name,
@@ -63,24 +65,48 @@ add_task(async function basic() {
       result.payload.url,
       "https://www.yelp.com/search?find_desc=RaMeN&find_loc=Tokyo%2C+Tokyo-to&utm_medium=partner&utm_source=mozilla"
     );
-    const titleElement = element.row.querySelector(".urlbarView-title");
-    Assert.equal(
-      titleElement.innerHTML,
-      `Top results for <strong xmlns=\"http://www.w3.org/1999/xhtml\">RaMeN</strong> <strong xmlns=\"http://www.w3.org/1999/xhtml\">iN</strong> <strong xmlns=\"http://www.w3.org/1999/xhtml\">Tokyo</strong>, <strong xmlns=\"http://www.w3.org/1999/xhtml\">Tokyo</strong>-to`
-    );
 
-    const { row } = details.element;
-    const bottom = row.querySelector(".urlbarView-row-body-bottom");
-    Assert.ok(bottom, "Bottom text element should exist");
+    const { row } = element;
+    const icon = row.querySelector(".urlbarView-favicon");
+    Assert.equal(icon.src, "chrome://global/skin/icons/defaultFavicon.svg");
+    const title = row.querySelector(".urlbarView-title");
+    Assert.equal(title.textContent, "Top results for RaMeN iN Tokyo, Tokyo-to");
     Assert.ok(
-      BrowserTestUtils.isVisible(bottom),
-      "Bottom text element should be visible"
+      BrowserTestUtils.isVisible(
+        row.querySelector(".urlbarView-subtitle-separator")
+      )
     );
+    const subtitle = row.querySelector(".urlbarView-subtitle");
+    Assert.equal(subtitle.textContent, "Yelp");
+
+    const description = row.querySelector(".urlbarView-row-body-description");
+    Assert.equal(description.textContent, "");
+
+    const bottomLabel = row.querySelector(".urlbarView-bottom-label");
+    Assert.equal(bottomLabel.textContent, "Sponsored");
+    const bottomSeparator = row.querySelector(".urlbarView-bottom-separator");
+    Assert.ok(BrowserTestUtils.isHidden(bottomSeparator));
+    const bottomUrl = row.querySelector(".urlbarView-url");
     Assert.equal(
-      bottom.textContent,
-      "Yelp · Sponsored",
-      "Bottom text is correct"
+      bottomUrl.textContent,
+      "yelp.com/search?find_desc=RaMeN&find_loc=Tokyo,+Tokyo-to&utm_medium=partner&utm_source=mozilla"
     );
+    Assert.ok(BrowserTestUtils.isHidden(bottomUrl));
+
+    info("Simulate hover");
+    InspectorUtils.addPseudoClassLock(row, ":hover");
+    Assert.ok(BrowserTestUtils.isVisible(bottomSeparator));
+    Assert.ok(BrowserTestUtils.isVisible(bottomUrl));
+
+    InspectorUtils.removePseudoClassLock(row, ":hover");
+    Assert.ok(BrowserTestUtils.isHidden(bottomSeparator));
+    Assert.ok(BrowserTestUtils.isHidden(bottomUrl));
+
+    info("Simulate selection");
+    row.toggleAttribute("selected", true);
+    Assert.ok(BrowserTestUtils.isVisible(bottomSeparator));
+    Assert.ok(BrowserTestUtils.isVisible(bottomUrl));
+    row.toggleAttribute("selected", false);
 
     await UrlbarTestUtils.promisePopupClose(window);
     await SpecialPowers.popPrefEnv();
@@ -111,10 +137,7 @@ add_task(async function businessSubject() {
     "https://www.yelp.com/search?find_desc=the+shop&find_loc=Tokyo%2C+Tokyo-to&utm_medium=partner&utm_source=mozilla"
   );
   const titleElement = element.row.querySelector(".urlbarView-title");
-  Assert.equal(
-    titleElement.innerHTML,
-    `<strong xmlns=\"http://www.w3.org/1999/xhtml\">the</strong> <strong xmlns=\"http://www.w3.org/1999/xhtml\">shop</strong> in <strong xmlns=\"http://www.w3.org/1999/xhtml\">To</strong>kyo, <strong xmlns=\"http://www.w3.org/1999/xhtml\">To</strong>kyo-<strong xmlns=\"http://www.w3.org/1999/xhtml\">to</strong>`
-  );
+  Assert.equal(titleElement.textContent, "the shop in Tokyo, Tokyo-to");
 
   await UrlbarTestUtils.promisePopupClose(window);
   await SpecialPowers.popPrefEnv();
