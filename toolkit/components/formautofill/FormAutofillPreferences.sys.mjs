02261b0455eafe5438662de93df8ed21b56db0cb
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-02-01 05:25:33 +0000

    Revert "Bug 2001254: Add basic tests for "Manage addresses" dialog and subpage. r=mtigley,credential-management-reviewers" for causing bc failures RunWatchdog related
    This reverts commit 01bdaaa3d7cc07ec51e4760aa14eb4ca1dd2dbaf.
    
    UltraBlame original commit: e26dd0d93ce6d627c56f0969d9165437fbcda929

--- toolkit/components/formautofill/FormAutofillPreferences.sys.mjs
+++ toolkit/components/formautofill/FormAutofillPreferences.sys.mjs
@@ -346,7 +346,6 @@ export class FormAutofillPreferences {
           },
           options: [
             {
-              id: "delete-address-button",
               control: "moz-button",
               iconSrc: "chrome://global/skin/icons/delete.svg",
               type: "icon",
@@ -358,7 +357,6 @@ export class FormAutofillPreferences {
               },
             },
             {
-              id: "edit-address-button",
               control: "moz-button",
               iconSrc: "chrome://global/skin/icons/edit.svg",
               type: "icon",
