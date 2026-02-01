f0d44674a56a699cd1dba5418be17d70dc746c0b
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-02-01 04:45:06 +0000

    Bug 2011759 - Reduce rooting overhead in GenericMethod r=edgar
    Differential Revision: https://phabricator.services.mozilla.com/D279936
    
    UltraBlame original commit: d2c8d4c134b63fac0c34b9140ec352d1e3f8377e

--- dom/bindings/BindingUtils.cpp
+++ dom/bindings/BindingUtils.cpp
@@ -3285,12 +3285,15 @@ bool GenericMethod(JSContext* cx, unsigned argc, JS::Value* vp) {
     bool ok = ThisPolicy::HandleInvalidThis(cx, args, false, protoID);
     return ExceptionPolicy::HandleException(cx, args, info, ok);
   }
-  JS::Rooted<JSObject*> obj(cx, ThisPolicy::ExtractThisObject(args));
+
+  JS::RootedTuple<JSObject*, JSObject*> roots(cx);
+  JS::RootedField<JSObject*, 0> obj(roots, ThisPolicy::ExtractThisObject(args));
 
   
   
   
-  JS::Rooted<JSObject*> rootSelf(cx, ThisPolicy::MaybeUnwrapThisObject(obj));
+  JS::RootedField<JSObject*, 1> rootSelf(
+      roots, ThisPolicy::MaybeUnwrapThisObject(obj));
   void* self;
   {
     nsresult rv =
