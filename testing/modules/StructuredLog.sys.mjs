730bbb54ebd6cd3587b0714849b79d298cd2a0d5
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-02-01 05:38:14 +0000

    Bug 2012172 - Guard ChromeUtils.registerMarkerSchema call for backwards compatibility r=florian,home-newtab-reviewers,mconley
    Differential Revision: https://phabricator.services.mozilla.com/D280310
    
    UltraBlame original commit: ba1c11d0c576fb8e2decbe071c821c81141777f4

--- testing/modules/StructuredLog.sys.mjs
+++ testing/modules/StructuredLog.sys.mjs
@@ -10,6 +10,15 @@ function ensureSchemaRegistered() {
   }
   gSchemaRegistered = true;
 
+  /**
+   * @backward-compat { version 149 }
+   *
+   * Bug 2011308: Remove the following typeof / early return check
+   */
+  if (!ChromeUtils.registerMarkerSchema) {
+    return;
+  }
+
   ChromeUtils.registerMarkerSchema({
     name: "TestStatus",
     tableLabel: "{marker.data.message}",
