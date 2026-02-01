e2b6d082f32583ef63e0c37bd50fad6de0186d66
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-02-01 05:09:46 +0000

    Bug 2010481 - Do not write to StickyScrollContainer::mShouldFlatten if we're not painting to the window. r=layout-reviewers,tnikkel
    This value is only read if we're painting to the window.
    
    If we write false to it during a hit test, with retained display lists
    the next paint to the window can incorrectly use that value.
    
    Differential Revision: https://phabricator.services.mozilla.com/D280348
    
    UltraBlame original commit: 70f2a0bbad424e0fa72a3ebbd01c251dd98de90e

--- layout/generic/nsIFrame.cpp
+++ layout/generic/nsIFrame.cpp
@@ -3410,9 +3410,8 @@ void nsIFrame::BuildDisplayListForStackingContext(
   if (useStickyPosition) {
     StickyScrollContainer* stickyScrollContainer =
         StickyScrollContainer::GetOrCreateForFrame(this);
-    if (stickyScrollContainer) {
-      if (aBuilder->IsPaintingToWindow() &&
-          !aBuilder->IsInViewTransitionCapture() &&
+    if (stickyScrollContainer && aBuilder->IsPaintingToWindow()) {
+      if (!aBuilder->IsInViewTransitionCapture() &&
           stickyScrollContainer->ScrollContainer()
               ->IsMaybeAsynchronouslyScrolled()) {
         shouldFlattenStickyItem = false;
