11a84996b6919f3270fe3b81af998aa1ec685e23
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-02-01 02:02:19 +0000

    Bug 1925116 - Use cached GL Strings in gfxInfo on Android. r=gfx-reviewers,geckoview-reviewers,nical,tcampbell
    Android's gfxInfo creates a GL context and queries the vendor,
    renderer, version, and extensions strings. It then uses these for
    blocklisting. Creating a GL context can be expensive, and this occurs
    on the main thread early during startup.
    
    This patch therefore caches the queried strings. In the happy path, we
    read the strings from preferences and avoid having to initialize a GL
    context altogether. If the cache is empty, or we detect the operating
    system has been updated, then we create the context and query the
    strings as before.
    
    The relies on the assumption that the graphics driver will not be
    updated without an operating system update, and that the GPU will not
    change. This seems likely to be valid for the vast majority of users,
    but the next patch will add a mechanism to help in cases where that
    assumption does not hold.
    
    Differential Revision: https://phabricator.services.mozilla.com/D278246
    
    UltraBlame original commit: e95f12ed985251bfb2bafab739e5dc5dd155d615

--- widget/android/GfxInfo.cpp
+++ widget/android/GfxInfo.cpp
@@ -28,6 +28,20 @@ class GfxInfo::GLStrings {
   nsTArray<nsCString> mExtensions;
   bool mReady;
 
+  
+  
+  
+  constexpr static const char* kCachePrefNameFingerprint =
+      "gfxinfo.gl-strings.build-fingerprint";
+  constexpr static const char* kCachePrefNameVendor =
+      "gfxinfo.gl-strings.vendor";
+  constexpr static const char* kCachePrefNameRenderer =
+      "gfxinfo.gl-strings.renderer";
+  constexpr static const char* kCachePrefNameVersion =
+      "gfxinfo.gl-strings.version";
+  constexpr static const char* kCachePrefNameExtensions =
+      "gfxinfo.gl-strings.extensions";
+
  public:
   GLStrings() : mReady(false) {}
 
@@ -68,6 +82,59 @@ class GfxInfo::GLStrings {
       return;
     }
 
+    
+    
+    
+    if (mVendor.IsEmpty()) {
+      const char* spoofedVendor = PR_GetEnv("MOZ_GFX_SPOOF_GL_VENDOR");
+      if (spoofedVendor) {
+        mVendor.Assign(spoofedVendor);
+      }
+    }
+    if (mRenderer.IsEmpty()) {
+      const char* spoofedRenderer = PR_GetEnv("MOZ_GFX_SPOOF_GL_RENDERER");
+      if (spoofedRenderer) {
+        mRenderer.Assign(spoofedRenderer);
+      }
+    }
+    if (mVersion.IsEmpty()) {
+      const char* spoofedVersion = PR_GetEnv("MOZ_GFX_SPOOF_GL_VERSION");
+      if (spoofedVersion) {
+        mVersion.Assign(spoofedVersion);
+      }
+    }
+
+    
+    
+    
+    const nsCString fingerprint = java::sdk::Build::FINGERPRINT()->ToCString();
+    nsCString cachedFingerprint;
+    Preferences::GetCString(kCachePrefNameFingerprint, cachedFingerprint);
+
+    if (fingerprint == cachedFingerprint) {
+      if (mVendor.IsEmpty()) {
+        Preferences::GetCString(kCachePrefNameVendor, mVendor);
+      }
+      if (mRenderer.IsEmpty()) {
+        Preferences::GetCString(kCachePrefNameRenderer, mRenderer);
+      }
+      if (mVersion.IsEmpty()) {
+        Preferences::GetCString(kCachePrefNameVersion, mVersion);
+      }
+      if (mExtensions.IsEmpty()) {
+        nsCString extensions;
+        Preferences::GetCString(kCachePrefNameExtensions, extensions);
+        for (auto extension : extensions.Split(' ')) {
+          mExtensions.AppendElement(extension);
+        }
+      }
+
+      mReady = true;
+      return;
+    }
+
+    
+    
     RefPtr<gl::GLContext> gl;
     nsCString discardFailureId;
     gl = gl::GLContextProvider::CreateHeadless(
@@ -84,43 +151,41 @@ class GfxInfo::GLStrings {
     gl->MakeCurrent();
 
     if (mVendor.IsEmpty()) {
-      const char* spoofedVendor = PR_GetEnv("MOZ_GFX_SPOOF_GL_VENDOR");
-      if (spoofedVendor) {
-        mVendor.Assign(spoofedVendor);
-      } else {
-        mVendor.Assign((const char*)gl->fGetString(LOCAL_GL_VENDOR));
-      }
+      mVendor.Assign(gl->VendorString());
     }
-
     if (mRenderer.IsEmpty()) {
-      const char* spoofedRenderer = PR_GetEnv("MOZ_GFX_SPOOF_GL_RENDERER");
-      if (spoofedRenderer) {
-        mRenderer.Assign(spoofedRenderer);
-      } else {
-        mRenderer.Assign((const char*)gl->fGetString(LOCAL_GL_RENDERER));
-      }
+      mRenderer.Assign(gl->RendererString());
     }
-
     if (mVersion.IsEmpty()) {
-      const char* spoofedVersion = PR_GetEnv("MOZ_GFX_SPOOF_GL_VERSION");
-      if (spoofedVersion) {
-        mVersion.Assign(spoofedVersion);
-      } else {
-        mVersion.Assign((const char*)gl->fGetString(LOCAL_GL_VERSION));
-      }
+      mVersion.Assign(gl->VersionString());
     }
-
     if (mExtensions.IsEmpty()) {
-      nsCString rawExtensions;
-      rawExtensions.Assign((const char*)gl->fGetString(LOCAL_GL_EXTENSIONS));
-      rawExtensions.Trim(" ");
-
-      for (auto extension : rawExtensions.Split(' ')) {
-        mExtensions.AppendElement(extension);
-      }
+      mExtensions = gl->ExtensionStrings().Clone();
     }
+
+    
+    
+    
+    CacheStrings(gl->VendorString(), gl->RendererString(), gl->VersionString(),
+                 gl->ExtensionStrings());
 
     mReady = true;
+  }
+
+  void CacheStrings(const nsCString& aVendor, const nsCString& aRenderer,
+                    const nsCString& aVersion,
+                    Span<const nsCString> aExtensions) {
+    const nsCString fingerprint = java::sdk::Build::FINGERPRINT()->ToCString();
+
+    Preferences::SetCString(kCachePrefNameVendor, aVendor);
+    Preferences::SetCString(kCachePrefNameRenderer, aRenderer);
+    Preferences::SetCString(kCachePrefNameVersion, aVersion);
+    Preferences::SetCString(kCachePrefNameExtensions,
+                            StringJoin(" "_ns, aExtensions));
+    
+    
+    
+    Preferences::SetCString(kCachePrefNameFingerprint, fingerprint);
   }
 };
 
