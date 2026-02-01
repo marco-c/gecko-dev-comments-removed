78199402f0b4a1e1111cfdc95c0cfa41cd8e59c2
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-02-01 04:58:38 +0000

    Bug 2010966 - Add local search modes to the new preferences search engines table. r=scunnane,mconley
    Differential Revision: https://phabricator.services.mozilla.com/D279384
    
    UltraBlame original commit: 51a7c5a51d42798dd3d919a136828d4036cc3fa3

--- browser/components/preferences/search.js
+++ browser/components/preferences/search.js
@@ -24,6 +24,10 @@ const lazy = XPCOMUtils.declareLazy({
     "moz-src:///toolkit/components/search/UserSearchEngine.sys.mjs",
 });
 
+
+
+
+
 Preferences.addAll([
   { id: "browser.search.suggest.enabled", type: "bool" },
   { id: "browser.urlbar.suggest.searches", type: "bool" },
@@ -576,7 +580,57 @@ Preferences.addSetting(
   class extends Preferences.AsyncSetting {
     static id = "engineList";
 
+    
+
+
+    async getL10nNames() {
+      if (this.#localShortcutL10nNames) {
+        return this.#localShortcutL10nNames;
+      }
+      this.#localShortcutL10nNames = new Map();
+
+      let getIDs = (suffix = "") =>
+        lazy.UrlbarUtils.LOCAL_SEARCH_MODES.map(mode => {
+          let name = lazy.UrlbarUtils.getResultSourceName(mode.source);
+          return { id: `urlbar-search-mode-${name}${suffix}` };
+        });
+
+      try {
+        let localizedIDs = getIDs();
+        let englishIDs = getIDs("-en");
+
+        let englishSearchStrings = new Localization([
+          "preview/enUS-searchFeatures.ftl",
+        ]);
+        let localizedNames = await document.l10n.formatValues(localizedIDs);
+        let englishNames = await englishSearchStrings.formatValues(englishIDs);
+
+        lazy.UrlbarUtils.LOCAL_SEARCH_MODES.forEach(({ source }, index) => {
+          let localizedName = localizedNames[index];
+          let englishName = englishNames[index];
+
+          
+          let names =
+            localizedName === englishName
+              ? [englishName]
+              : [localizedName, englishName];
+
+          this.#localShortcutL10nNames.set(source, names);
+        });
+      } catch (ex) {
+        console.error("Error loading l10n names", ex);
+      }
+      return this.#localShortcutL10nNames;
+    }
+
+    
+
+
+
+
+
     handleDeletionOptions(engine) {
+      
       let deletionOptions;
       if (engine.isConfigEngine) {
         let toggleId = `toggleEngine-${engine.id}`;
@@ -643,7 +697,12 @@ Preferences.addSetting(
       return deletionOptions;
     }
 
+    
+
+
+
     async makeEngineList() {
+      
       let configs = [];
       for (let engine of await lazy.SearchService.getEngines()) {
         let setting = {
@@ -653,6 +712,7 @@ Preferences.addSetting(
         };
         Preferences.addSetting(setting);
 
+        
         let config = {
           id: setting.id,
           control: "moz-box-item",
@@ -693,9 +753,61 @@ Preferences.addSetting(
       return configs;
     }
 
-    async getControlConfig() {
-      return { items: await this.makeEngineList() };
+    
+
+
+
+    async makeSearchModesList() {
+      let l10nNames = await this.getL10nNames();
+
+      
+      let configs = [];
+      for (let searchMode of lazy.UrlbarUtils.LOCAL_SEARCH_MODES) {
+        let id = `searchmode-${searchMode.telemetryLabel}`;
+        Preferences.addSetting({ id });
+
+        
+        
+        let keywords = l10nNames
+          .get(searchMode.source)
+          .map(keyword => `@${keyword.toLowerCase()}`)
+          .join(", ");
+
+        
+        keywords += `, ${searchMode.restrict}`;
+
+        configs.push({
+          id,
+          control: "moz-box-item",
+          controlAttrs: {
+            label: l10nNames.get(searchMode.source)[0],
+            description: keywords,
+            layout: "large-icon",
+            iconsrc: searchMode.icon,
+            slot: "footer",
+          },
+        });
+      }
+
+      return configs;
     }
+
+    async getControlConfig() {
+      return {
+        items: [
+          ...(await this.makeEngineList()),
+          ...(await this.makeSearchModesList()),
+        ],
+      };
+    }
+
+    
+
+
+
+
+
+    #localShortcutL10nNames = null;
   }
 );
 
