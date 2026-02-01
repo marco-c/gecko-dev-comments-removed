bac3c057d7224009389ae0e7766804bdb9c73218
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-02-01 04:41:16 +0000

    Bug 2006964: Augment PositionAnchors with tree scope. r=layout-anchor-positioning-reviewers,firefox-style-system-reviewers,layout-reviewers,emilio
    Differential Revision: https://phabricator.services.mozilla.com/D278923
    
    UltraBlame original commit: e6200cd42f404c0dfdd1278851f8f9cbbd362ac8

--- layout/style/ComputedStyle.cpp
+++ layout/style/ComputedStyle.cpp
@@ -432,14 +432,14 @@ void ComputedStyle::DumpMatchedRules() const {
 
 bool ComputedStyle::HasAnchorPosReference() const {
   const auto* pos = StylePosition();
-  if (pos->mPositionAnchor.IsIdent()) {
+  if (pos->mPositionAnchor.value.IsIdent()) {
     
     
     
     return true;
   }
 
-  if (pos->mPositionAnchor.IsAuto()) {
+  if (pos->mPositionAnchor.value.IsAuto()) {
     if (!pos->mPositionArea.IsNone()) {
       
       return true;
