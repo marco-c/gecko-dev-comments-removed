180fc1def3e646ee3a6ff23332d44365ab165be5
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-02-01 02:27:28 +0000

    Bug 2006933 - Vendor libwebrtc from 5d3b06bd1a
    Upstream commit: https://webrtc.googlesource.com/src/+/5d3b06bd1a1f16bd4b7d6d91951b74f270dcea1b
        Use sigslot trampoline in ProxyBinding
    
        Bug: webrtc:42222066
        Change-Id: I12d04f2b906ab3eea7b1ad4d0230648d6d63b342
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/410580
        Reviewed-by: Tomas Gunnarsson <tommiwebrtc.org>
        Commit-Queue: Tomas Gunnarsson <tommiwebrtc.org>
        Reviewed-by: Harald Alvestrand <htawebrtc.org>
        Cr-Commit-Position: refs/heads/main{#45767}
    
    UltraBlame original commit: 64945d1ba467550c33cb4103597707faa378a63c

--- third_party/libwebrtc/rtc_base/proxy_server.cc
+++ third_party/libwebrtc/rtc_base/proxy_server.cc
@@ -10,9 +10,8 @@
 
 #include "rtc_base/proxy_server.h"
 
-#include <stddef.h>
-
 #include <algorithm>
+#include <cstddef>
 #include <memory>
 
 #include "rtc_base/checks.h"
@@ -72,7 +71,8 @@ ProxyBinding::ProxyBinding(AsyncProxyServerSocket* int_socket,
       ext_socket_(ext_socket),
       connected_(false),
       out_buffer_(kBufferSize),
-      in_buffer_(kBufferSize) {
+      in_buffer_(kBufferSize),
+      destroyed_trampoline_(this) {
   int_socket_->SignalConnectRequest.connect(this,
                                             &ProxyBinding::OnConnectRequest);
   int_socket_->SignalReadEvent.connect(this, &ProxyBinding::OnInternalRead);
@@ -152,7 +152,7 @@ void ProxyBinding::Write(Socket* socket, FifoBuffer* buffer) {
 }
 
 void ProxyBinding::Destroy() {
-  SignalDestroyed(this);
+  NotifyDestroyed(this);
 }
 
 }  
