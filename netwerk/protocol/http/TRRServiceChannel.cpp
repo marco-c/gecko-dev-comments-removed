7b16523e721951a769476bb53aeebcf64826b422
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-02-01 04:40:35 +0000

    Bug 2011643 - Enable network.trr.force_http3_first on nightly, r=necko-reviewers,valentin
    Differential Revision: https://phabricator.services.mozilla.com/D279853
    
    UltraBlame original commit: 48ad7d58c2b41fff058a882e6f79ecde2832eef1

--- netwerk/protocol/http/TRRServiceChannel.cpp
+++ netwerk/protocol/http/TRRServiceChannel.cpp
@@ -412,7 +412,8 @@ nsresult TRRServiceChannel::BeginConnect() {
            scheme, host, port, mPrivateBrowsing, OriginAttributes(),
            http2Allowed, http3Allowed,
            StaticPrefs::network_trr_force_http3_first() ||
-               TRRService::Get()->Http3FirstEnabled()))) {
+               (StaticPrefs::network_trr_allow_default_http3_first() &&
+                TRRService::Get()->Http3FirstEnabled())))) {
     LOG(("TRRServiceChannel %p Alt Service Mapping Found %s://%s:%d [%s]\n",
          this, scheme.get(), mapping->AlternateHost().get(),
          mapping->AlternatePort(), mapping->HashKey().get()));
