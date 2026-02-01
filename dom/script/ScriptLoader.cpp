3d190dbdc6ca2ac7c634d41b946488c5b600c0ea
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-02-01 03:11:35 +0000

    Bug 2007053 - Part 1: Yield to other tasks before processing long defer modules before FCP. r=nbp
    Reuse the existing logic for classic script, with moving the code to
    calculate the time to the shared code.
    
    Differential Revision: https://phabricator.services.mozilla.com/D278087
    
    UltraBlame original commit: 0431a3207ab300e8fa1340ae8791a3c362024bbc

--- dom/script/ScriptLoader.cpp
+++ dom/script/ScriptLoader.cpp
@@ -3079,11 +3079,24 @@ nsresult ScriptLoader::EvaluateScriptElement(ScriptLoadRequest* aRequest) {
   
   MOZ_ASSERT(!aRequest->IsImportMapRequest());
 
+  auto start = TimeStamp::Now();
+
+  nsresult rv;
   if (aRequest->IsModuleRequest()) {
-    return aRequest->AsModuleRequest()->EvaluateModule();
+    rv = aRequest->AsModuleRequest()->EvaluateModule();
+  } else {
+    rv = EvaluateScript(globalObject, aRequest);
   }
 
-  return EvaluateScript(globalObject, aRequest);
+  auto end = TimeStamp::Now();
+  auto duration = (end - start).ToMilliseconds();
+
+  static constexpr double LongScriptThresholdInMilliseconds = 1.0;
+  if (duration > LongScriptThresholdInMilliseconds) {
+    aRequest->SetTookLongInPreviousRuns();
+  }
+
+  return rv;
 }
 
 
@@ -3556,17 +3569,7 @@ nsresult ScriptLoader::EvaluateScript(nsIGlobalObject* aGlobalObject,
     MOZ_ASSERT(options.noScriptRval);
     TRACE_FOR_TEST(aRequest, "evaluate:classic");
 
-    auto start = TimeStamp::Now();
-
     ExecuteCompiledScript(cx, classicScript, script, erv);
-
-    auto end = TimeStamp::Now();
-    auto duration = (end - start).ToMilliseconds();
-
-    static constexpr double LongScriptThresholdInMilliseconds = 1.0;
-    if (duration > LongScriptThresholdInMilliseconds) {
-      aRequest->SetTookLongInPreviousRuns();
-    }
   }
   rv = EvaluationExceptionToNSResult(erv);
 
