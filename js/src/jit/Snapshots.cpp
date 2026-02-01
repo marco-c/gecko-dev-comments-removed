b73a1872ea17ca8619189be8497143481970b9e3
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-02-01 04:14:23 +0000

    Bug 2010943: Support reading int64 from int32 stack slot r=jandem
    Differential Revision: https://phabricator.services.mozilla.com/D279595
    
    UltraBlame original commit: f0b42fcd1bcab924abe3b603e759d7bd9fb8ee97

--- js/src/jit/Snapshots.cpp
+++ js/src/jit/Snapshots.cpp
@@ -155,6 +155,9 @@ using namespace js::jit;
 
 
 
+
+
+
 const RValueAllocation::Layout& RValueAllocation::layoutFromMode(Mode mode) {
   switch (mode) {
     case CONSTANT: {
@@ -306,6 +309,12 @@ const RValueAllocation::Layout& RValueAllocation::layoutFromMode(Mode mode) {
     case INT64_STACK: {
       static const RValueAllocation::Layout layout = {
           PAYLOAD_STACK_OFFSET, PAYLOAD_NONE, "unpacked int64"};
+      return layout;
+    }
+
+    case INT64_INT32_STACK: {
+      static const RValueAllocation::Layout layout = {
+          PAYLOAD_STACK_OFFSET, PAYLOAD_NONE, "unpacked int64 (int32)"};
       return layout;
     }
 #endif
