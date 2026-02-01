32624440d619f8352333555eed8e294ce5c4972d
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-02-01 02:51:10 +0000

    Bug 2009907 - Part 1: Update to tzdata 2025c. r=spidermonkey-reviewers,dminor
    Update to 2025c to match tzdata version in ICU 78.2.
    
    The extra changes in metaZones.txt are from CLDR 48.
    
    Differential Revision: https://phabricator.services.mozilla.com/D278746
    
    UltraBlame original commit: 0697d3fddbcce0675f2c1817bae2775a66447dd8

--- js/src/tests/non262/Intl/DateTimeFormat/timeZone_version.js
+++ js/src/tests/non262/Intl/DateTimeFormat/timeZone_version.js
@@ -2,7 +2,7 @@
 
 
 
-const tzdata = "2025b";
+const tzdata = "2025c";
 
 if (typeof getICUOptions === "undefined") {
     var getICUOptions = SpecialPowers.Cu.getJSTestingFunctions().getICUOptions;
