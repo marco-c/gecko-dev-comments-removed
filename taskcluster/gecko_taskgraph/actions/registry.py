b47fbd46e8085b121dec7fa3be99047d53fef649
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-02-01 04:48:15 +0000

    Bug 2009019 - Add additional context needed to render action tasks, r=taskgraph-reviewers,releng-reviewers,bhearsum
    This adds 'branch' and 'base_branch' to the push context, as well as
    starts passing certain params via the 'parameters' context (just
    'repository_type' for now).
    
    Differential Revision: https://phabricator.services.mozilla.com/D279003
    
    UltraBlame original commit: 5989796681395de79f2d872dd67401f504f29f5b

--- taskcluster/gecko_taskgraph/actions/registry.py
+++ taskcluster/gecko_taskgraph/actions/registry.py
@@ -190,6 +190,12 @@ def register_callback_action(
                 "base_revision": base_revision,
             }
 
+            if branch := parameters.get("head_ref"):
+                push["branch"] = branch
+
+            if (base_branch := parameters.get("base_ref")) and branch != base_branch:
+                push["base_branch"] = base_branch
+
             action = {
                 "name": name,
                 "title": title,
@@ -198,6 +204,13 @@ def register_callback_action(
                 "taskGroupId": decision_task_id,
                 "cb_name": cb_name,
                 "symbol": symbol,
+            }
+
+            
+            
+            
+            filtered_params = {
+                "repository_type": parameters["repository_type"],
             }
 
             rv = {
@@ -233,6 +246,7 @@ def register_callback_action(
                         "action": action,
                         "repository": repository,
                         "push": push,
+                        "parameters": filtered_params,
                     },
                     
                     "user": {
