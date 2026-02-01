c363eeacd08ff2ed4b77b43376000e557e5f7795
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-02-01 03:22:52 +0000

    Bug 1991050: Use OS auth even if PrP is set. r=mtigley,credential-management-reviewers,joschmidt
    Differential Revision: https://phabricator.services.mozilla.com/D266318
    
    UltraBlame original commit: ee7ea6953db440bda7699c908274560c1ae20ae6

--- browser/components/aboutlogins/tests/browser/browser_primaryPassword.js
+++ browser/components/aboutlogins/tests/browser/browser_primaryPassword.js
@@ -19,6 +19,12 @@ function waitForLoginCountToReach(browser, loginCount) {
 
 add_setup(async function () {
   await addLogin(TEST_LOGIN1);
+
+  
+  
+  await sinon.restore();
+  LoginHelper.setOSAuthEnabled(false);
+
   registerCleanupFunction(() => {
     Services.logins.removeAllUserFacingLogins();
     LoginTestUtils.primaryPassword.disable();
@@ -26,16 +32,9 @@ add_setup(async function () {
 });
 
 add_task(async function test() {
-  
-  
-  
-  Assert.equal(
-    Services.prefs.getStringPref(
-      "toolkit.osKeyStore.unofficialBuildOnlyLogin",
-      ""
-    ),
-    "",
-    "Pref should be set to default value of empty string to start the test"
+  ok(
+    !LoginHelper.getOSAuthEnabled(),
+    "OS auth must be disabled for PrP tests."
   );
   LoginTestUtils.primaryPassword.enable();
 
@@ -232,16 +231,9 @@ add_task(async function test() {
 });
 
 add_task(async function test_login_item_after_successful_auth() {
-  
-  
-  
-  Assert.equal(
-    Services.prefs.getStringPref(
-      "toolkit.osKeyStore.unofficialBuildOnlyLogin",
-      ""
-    ),
-    "",
-    "Pref should be set to default value of empty string to start the test"
+  ok(
+    !LoginHelper.getOSAuthEnabled(),
+    "OS auth must be disabled for PrP tests."
   );
   LoginTestUtils.primaryPassword.enable();
 
