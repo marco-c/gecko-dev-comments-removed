faad584d55d757aa7959473a7a3de6d8e43f796e
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-02-01 01:34:22 +0000

    Bug 1928537 - Fix raciness in browser_necko_l10n.js r=necko-reviewers,sunil
    Bug 1986649 changed the test to also check the statuspanellabel for the
    expected string, however that value is not always synchronized with
    the onStatusChange event, leading to intermittent failures.
    
    This change fixes it so that we allow both "Looking up ..." and
    "Transferring data from ..." status strings.
    It is still possible that the test would be racy (other status panel
    values are possible), but all current failures have one of these two strings.
    
    Differential Revision: https://phabricator.services.mozilla.com/D278249
    
    UltraBlame original commit: e07664913820546e1d3c0b7cb18d4e70dd3dbf71

--- netwerk/test/browser/browser_necko_l10n.js
+++ netwerk/test/browser/browser_necko_l10n.js
@@ -95,10 +95,10 @@ add_task(async function test_domain_change() {
   
   
   
-  is(
-    statuspanelLabel.value,
-    `Looking up ${DOMAIN_NAME}…`,
-    "statuspanel has expected value"
+  ok(
+    statuspanelLabel.value == `Looking up ${DOMAIN_NAME}…` ||
+      statuspanelLabel.value == `Transferring data from ${DOMAIN_NAME}…`,
+    `statuspanel has expected value. got ${statuspanelLabel.value}`
   );
   is(message, expectedMessage, "Status message was received correctly");
 });
