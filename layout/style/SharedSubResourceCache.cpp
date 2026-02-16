093609b040be05f62c9ad05ac0430773fbcfc4be
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-02-16 01:27:17 +0000

    Bug 2012948 - Part 5: Include CompleteSubResource::mNetworkMetadata in the memory reporting. r=emilio,firefox-style-system-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D280883
    
    UltraBlame original commit: 8c641fff24bc00ec7b44f38f9a67828c2a57c60e

--- layout/style/SharedSubResourceCache.cpp
+++ layout/style/SharedSubResourceCache.cpp
@@ -157,4 +157,16 @@ SubResourceNetworkMetadataHolder::SubResourceNetworkMetadataHolder(
   }
 }
 
+size_t SubResourceNetworkMetadataHolder::SizeOfExcludingThis(
+    MallocSizeOf aMallocSizeOf) const {
+  size_t n = 0;
+  if (mPerfData) {
+    n += mPerfData->SizeOfExcludingThis(aMallocSizeOf);
+  }
+  if (mResponseHead) {
+    mResponseHead->SizeOfIncludingThis(aMallocSizeOf);
+  }
+  return n;
+}
+
 }  
