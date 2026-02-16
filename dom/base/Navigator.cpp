4bfa450c7c915772463cc452a41defdfa6523364
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-02-16 02:03:34 +0000

    Revert "Bug 2012568: return TypeError for sendBeacon when used with ReadableStream r=webidl,saschanaz" for causing wpt failures
    This reverts commit cda7cdf36d4ad48318f305941b62d1ec8816d9ce.
    
    UltraBlame original commit: da15e073bb780cf213d265a0f9701e3b857f8011

--- dom/base/Navigator.cpp
+++ dom/base/Navigator.cpp
@@ -1200,17 +1200,10 @@ BeaconStreamListener::OnDataAvailable(nsIRequest* aRequest,
 }
 
 bool Navigator::SendBeacon(const nsAString& aUrl,
-                           const Nullable<BodyInit>& aData, ErrorResult& aRv) {
+                           const Nullable<fetch::BodyInit>& aData,
+                           ErrorResult& aRv) {
   if (aData.IsNull()) {
     return SendBeaconInternal(aUrl, nullptr, eBeaconTypeOther, aRv);
-  }
-
-  
-  
-  
-  if (aData.Value().IsReadableStream()) {
-    aRv.ThrowTypeError("ReadableStream body is not supported with keepalive");
-    return false;
   }
 
   if (aData.Value().IsArrayBuffer()) {
