634917e09db8ba7e5e1963d04845049b4660dbc3
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-02-16 02:37:38 +0000

    Bug 2013389 - Part 11: Make attachment arrays more consistent in StructuredCloneHolder, r=asuth
    This adds more assertions in various places, and ensures that things like
    StructuredCloneData's serialization is updated when new members are added to
    StructuredCloneHolder. This is done by adding helper methods returning
    `std::tie`-ed tuples of the various attachment array types, which can then be
    handled using `std::apply` and parameter packs.
    
    Differential Revision: https://phabricator.services.mozilla.com/D281554
    
    UltraBlame original commit: 654147a879bc64917f00319bba32d8da5bbc8999

--- dom/base/StructuredCloneHolder.cpp
+++ dom/base/StructuredCloneHolder.cpp
@@ -435,6 +435,8 @@ void StructuredCloneHolder::Write(JSContext* aCx, JS::Handle<JS::Value> aValue,
   if (aRv.Failed()) {
     return;
   }
+
+  AssertAttachmentsMatchFlags();
 }
 
 void StructuredCloneHolder::Read(JSContext* aCx,
@@ -447,17 +449,15 @@ void StructuredCloneHolder::Read(JSContext* aCx,
                                  JS::MutableHandle<JS::Value> aValue,
                                  const JS::CloneDataPolicy& aCloneDataPolicy,
                                  ErrorResult& aRv) {
+  AssertAttachmentsMatchFlags();
+
   StructuredCloneHolderBase::Read(aCx, aValue, aCloneDataPolicy, aRv);
-  if (aRv.Failed()) {
-    mTransferredPorts.Clear();
-    return;
-  }
 
   
+  
   if (mSupportsTransferring) {
-#define STMT(_member) (_member).Clear()
-    CLONED_DATA_MEMBERS
-#undef STMT
+    std::apply([](auto&... member) { (member.Clear(), ...); },
+               AttachmentArrays());
 
     Clear();
   }
@@ -2067,5 +2067,28 @@ void StructuredCloneHolder::SameProcessScopeRequired(
     *aSameProcessScopeRequired = true;
   }
 }
+
+
+static bool HasAnyOf(const auto& arrays) {
+  return std::apply(
+      [&](const auto&... member) { return (!member.IsEmpty() || ...); },
+      std::forward<decltype(arrays)>(arrays));
+}
+
+bool StructuredCloneHolder::HasClonedDOMObjects() {
+  return HasAnyOf(AttachmentArrays());
+}
+
+#ifdef DEBUG
+void StructuredCloneHolder::AssertAttachmentsMatchFlags() {
+  MOZ_ASSERT(mSupportsCloning || !HasClonedDOMObjects(),
+             "unexpected attachments");
+  MOZ_ASSERT(CloneScope() == StructuredCloneScope::SameProcess ||
+                 !HasAnyOf(InProcessCloneableAttachmentArrays()),
+             "unexpected in-process attachments");
+  MOZ_ASSERT(mSupportsTransferring || !HasAnyOf(TransferableAttachmentArrays()),
+             "unexpected transferable attachments");
+}
+#endif
 
 }  
