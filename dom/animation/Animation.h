fff1d4ab4da61a5ecabe7b57d32daa2f5e784878
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-02-16 04:33:21 +0000

    Bug 2011353 - Call Animation::PostUpdate() when invalidating the multiplier cache to update the playback rate for animations on the compositor. r=boris,layout-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D282985
    
    UltraBlame original commit: 2c92041efbddb49fc21194e4726b923889f317ab

--- dom/animation/Animation.h
+++ dom/animation/Animation.h
@@ -424,6 +424,8 @@ class Animation : public DOMEventTargetHelper,
 
   DocGroup* GetDocGroup();
 
+  void PostUpdate();
+
  protected:
   void SilentlySetCurrentTime(const TimeDuration& aNewCurrentTime);
   void CancelNoUpdate();
@@ -462,7 +464,6 @@ class Animation : public DOMEventTargetHelper,
 
 
   void FlushUnanimatedStyle() const;
-  void PostUpdate();
   void ResetFinishedPromise();
   void MaybeResolveFinishedPromise();
   void DoFinishNotification(SyncNotifyFlag aSyncNotifyFlag);
