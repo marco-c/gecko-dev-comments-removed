ae7c39ca6964ed59d76c216602cbd987151c7280
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-02-16 03:37:39 +0000

    Bug 2014787: Stop putting r=ng in application msections.
    Depends on D282003
    
    Differential Revision: https://phabricator.services.mozilla.com/D282004
    
    UltraBlame original commit: 37aceb300931281ed2fe0086b74f948947e48390

--- dom/media/webrtc/jsep/JsepSessionImpl.cpp
+++ dom/media/webrtc/jsep/JsepSessionImpl.cpp
@@ -305,9 +305,12 @@ nsresult JsepSessionImpl::CreateOfferMsection(const JsepOfferOptions& options,
           new SdpFlagAttribute(SdpAttribute::kRtcpRsizeAttribute));
     }
   }
-  
-  msection->GetAttributeList().SetAttribute(
-      new SdpFlagAttribute(SdpAttribute::kExtmapAllowMixedAttribute));
+
+  if (msection->GetMediaType() != SdpMediaSection::MediaType::kApplication) {
+    
+    msection->GetAttributeList().SetAttribute(
+        new SdpFlagAttribute(SdpAttribute::kExtmapAllowMixedAttribute));
+  }
 
   nsresult rv = AddTransportAttributes(msection, SdpSetupAttribute::kActpass);
   NS_ENSURE_SUCCESS(rv, rv);
