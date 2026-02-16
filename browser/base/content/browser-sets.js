bcf32a666ce904284827206df0eaa655b6f9c75f
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-02-16 01:26:12 +0000

    Bug 2012014 - Route Window Opener Through Auth Flow r=ai-frontend-reviewers,Gijs
    Differential Revision: https://phabricator.services.mozilla.com/D280189
    
    UltraBlame original commit: 0bc880ac7897cd3aa6da32ff0aaa29ec6ce48629

--- browser/base/content/browser-sets.js
+++ browser/base/content/browser-sets.js
@@ -228,7 +228,7 @@ document.addEventListener(
             OpenBrowserWindow({ aiWindow: false });
             break;
           case "Tools:AIWindow":
-            OpenBrowserWindow({ aiWindow: true });
+            AIWindow.launchWindow(gBrowser.selectedBrowser, true);
             break;
           case "Tools:ChatsHistory":
             
