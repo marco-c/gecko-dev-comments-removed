154d651bc4a5de554ba7d97e50d7b911f16d5d30
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-02-16 01:30:24 +0000

    Bug 1800513: Reduce no-op time in syncManagerTestChild. r=bytesized,application-update-reviewers,cdupuis
    This long-standing intermittent issue started to occur more frequently with
    the unrelated SpiderMonkey internal patches in bug 2010219.
    
    The test file "updateSyncManager.js" calls `TestUtils.waitForCondition` to wait
    for the lock to be released. `TestUtils.waitForCondition` checks for the condition
    50 times, waiting 100 ms after every failed attempt. That makes 5 seconds in total.
    
    "syncManagerTestChild.js" also waits 5 seconds until it closes itself. The test
    was relying on `TestUtils.waitForCondition` spending slightly more than 5 seconds
    due to call overhead in `setTimeout`, but that may not happen in certain test
    configurations, like for example when running under TSan.
    
    Lowering the time spend in "syncManagerTestChild.js" from 5 to 3 seconds should
    make it more likely that `TestUtils.waitForCondition` doesn't error out due to a
    timeout.
    
    Additionally change "updateSyncManager.js" to append the error message to the
    assertion, so it's easier to see in the logs when a timeout happened.
    
    Differential Revision: https://phabricator.services.mozilla.com/D280985
    
    UltraBlame original commit: d12ef132757fde3492aa2346c44c54e23f6f0ad1

--- toolkit/mozapps/update/tests/data/syncManagerTestChild.js
+++ toolkit/mozapps/update/tests/data/syncManagerTestChild.js
@@ -18,6 +18,6 @@ registerCustomDirProvider();
 resetSyncManagerLock();
 
 
-print("child process should now have the lock; will exit in 5 seconds");
-simulateNoScriptActivity(5);
+print("child process should now have the lock; will exit in 3 seconds");
+simulateNoScriptActivity(3);
 print("child process exiting now");
