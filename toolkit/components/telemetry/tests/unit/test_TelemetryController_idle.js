e42c413c270606956796b705436db722242e67d1
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-02-16 03:20:53 +0000

    Bug 1127907 - Remove gather-telemetry impl r=toolkit-telemetry-reviewers-rotation,TravisLong
    Differential Revision: https://phabricator.services.mozilla.com/D278815
    
    UltraBlame original commit: 276791cf9a9640da64bf375740d3509515b17783

--- toolkit/components/telemetry/tests/unit/test_TelemetryController_idle.js
+++ toolkit/components/telemetry/tests/unit/test_TelemetryController_idle.js
@@ -46,14 +46,14 @@ add_task(async function testSendPendingOnIdleDaily() {
   );
 
   let gatherPromise = Promise.withResolvers();
-  Services.obs.addObserver(gatherPromise.resolve, "gather-telemetry");
+  Services.obs.addObserver(gatherPromise.resolve, "idle-daily");
 
   
   TelemetrySession.observe(null, "idle-daily", null);
-  await gatherPromise.promise;
-  Assert.ok(true, "Received gather-telemetry notification.");
+  await gatherPromise.resolve;
+  Assert.ok(true, "Received idle-daily notification.");
 
-  Services.obs.removeObserver(gatherPromise.resolve, "gather-telemetry");
+  Services.obs.removeObserver(gatherPromise.resolve, "idle-daily");
 
   
   let { TelemetrySendImpl } = ChromeUtils.importESModule(
