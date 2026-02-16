bee67409ec4be7504221291de525293c1bfbe2c0
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-02-16 02:02:36 +0000

    Bug 2014296 - Fix telemetry for the `quicksuggest.online.available` pref. r=daisuke
    This fixes the bug and makes a couple of other minor changes:
    
    Remove the `newValue` param to `#recordPref`. It's unnecessary complexity and an
    unnecessary secondary code path, and it doesn't really save anything perf-wise.
    
    Don't get `prefValue` when it's not needed. `onPrefChanged` and
    `onNimbusChanged` are called when *any* urlbar pref/variable changes, not just
    the small number of prefs in `#PING_PREFS`.
    
    Differential Revision: https://phabricator.services.mozilla.com/D281692
    
    UltraBlame original commit: cc9c2fa92355c2d28619c5670b2dc9b19fcb22e2

--- browser/components/urlbar/tests/engagementTelemetry/browser/browser_glean_telemetry_record_preferences.js
+++ browser/components/urlbar/tests/engagementTelemetry/browser/browser_glean_telemetry_record_preferences.js
@@ -91,7 +91,7 @@ add_task(async function boolNimbusVariable() {
     Assert.equal(
       Glean.urlbar[glean].testGetValue(),
       initialValue,
-      `Record ${glean} when UrlbarController is initialized`
+      "Metric value should be correct initially: " + glean
     );
 
     let nimbusCleanup = await UrlbarTestUtils.initNimbusFeature({
@@ -101,7 +101,28 @@ add_task(async function boolNimbusVariable() {
     Assert.equal(
       Glean.urlbar[glean].testGetValue(),
       !initialValue,
-      `Record ${glean} when the ${variable} variable is set`
+      "Metric value should be correct after installing experiment: " + glean
+    );
+
+    
+    
+    
+    let win = await BrowserTestUtils.openNewBrowserWindow();
+    await TestUtils.waitForTick();
+
+    Assert.equal(
+      Glean.urlbar[glean].testGetValue(),
+      !initialValue,
+      "Metric value should remain correct after opening new window: " + glean
+    );
+
+    await BrowserTestUtils.closeWindow(win);
+    await TestUtils.waitForTick();
+
+    Assert.equal(
+      Glean.urlbar[glean].testGetValue(),
+      !initialValue,
+      "Metric value should remain correct after closing new window: " + glean
     );
 
     await nimbusCleanup();
@@ -109,7 +130,7 @@ add_task(async function boolNimbusVariable() {
     Assert.equal(
       Glean.urlbar[glean].testGetValue(),
       initialValue,
-      `Record ${glean} when the ${variable} variable is unset`
+      "Metric value should be reset after uninstalling experiment: " + glean
     );
   }
 });
