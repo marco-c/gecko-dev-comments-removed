5e701a07641e105571e9ed6fd9b2ed546e6ce571
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-02-16 04:12:35 +0000

    Bug 2009372 - Pipe runnable prioritization r=necko-reviewers,kershaw
    This change makes the OnInputStreamReady events delivered for the
    TRRServiceChannel transaction implement nsIRunnablePriority
    so they can be prioritized.
    
    Differential Revision: https://phabricator.services.mozilla.com/D280954
    
    UltraBlame original commit: ea6983eacec7e89c9f56958d6e003683bc7e9b93

--- netwerk/base/nsInputStreamPump.h
+++ netwerk/base/nsInputStreamPump.h
@@ -66,6 +66,14 @@ class nsInputStreamPump final : public nsIInputStreamPump,
 
   nsresult CallOnStateStop();
 
+  
+
+
+  void SetHighPriority(bool aHighPriority) {
+    mHighPriorityStream = aHighPriority;
+  }
+  bool IsHighPriority() { return mHighPriorityStream; }
+
  protected:
   enum { STATE_IDLE, STATE_START, STATE_TRANSFER, STATE_STOP, STATE_DEAD };
 
@@ -118,6 +126,9 @@ class nsInputStreamPump final : public nsIInputStreamPump,
   const bool mOffMainThread;
   
   mozilla::RecursiveMutex mMutex{"nsInputStreamPump"};
+  
+  
+  mozilla::Atomic<bool, mozilla::Relaxed> mHighPriorityStream{false};
 };
 
 #endif  
