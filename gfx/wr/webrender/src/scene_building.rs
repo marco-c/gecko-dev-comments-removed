04acd71f67d26a42d4b316369671c419a4749f19
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-02-16 04:34:32 +0000

    Revert "Bug 1978773 - Cache quad linear gradients in some cases. r=gfx-reviewers,gw" for causing failures at border-gradient-nine-patch.yaml.
    This reverts commit b44f3f4531aba5a8764631a514a1bf85bc8b8827.
    
    UltraBlame original commit: 844fb16c6ad5b79392ba216d09a4d336cd3528f3

--- gfx/wr/webrender/src/scene_building.rs
+++ gfx/wr/webrender/src/scene_building.rs
@@ -3517,11 +3517,10 @@ impl<'a> SceneBuilder<'a> {
 
         let is_tiled = prim_rect.width() > stretch_size.width
          || prim_rect.height() > stretch_size.height;
-
         
         
         
-        let cached = is_tiled && !caching_causes_artifacts;
+        let cached = (!self.config.is_software || is_tiled) && !caching_causes_artifacts;
 
         Some(LinearGradient {
             extend_mode,
