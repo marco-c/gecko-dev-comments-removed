4560c550a5fb0251138cad68c03b378e50e7f520
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-02-16 01:51:13 +0000

    Bug 1867743 - Part 8: Implement accessible relations for hint popovers r=accessibility-platform-reviewers,Jamie
    Differential Revision: https://phabricator.services.mozilla.com/D276568
    
    UltraBlame original commit: 30e34947d7b6a0ccd37eab17e887058aaccec5f5

--- accessible/generic/LocalAccessible.h
+++ accessible/generic/LocalAccessible.h
@@ -227,6 +227,12 @@ class LocalAccessible : public nsISupports, public Accessible {
 
   virtual bool NativelyUnavailable() const;
 
+  
+
+
+
+  bool IsOnlyPlainContent() const;
+
   virtual already_AddRefed<AccAttributes> Attributes() override;
 
   
@@ -1037,6 +1043,10 @@ class LocalAccessible : public nsISupports, public Accessible {
   LocalAccessible* GetPopoverTargetDetailsRelation() const;
 
   LocalAccessible* GetAnchorPositionTargetDetailsRelation() const;
+
+  LocalAccessible* GetPopoverTargetDescribedByRelation() const;
+
+  LocalAccessible* GetCommandForDescribedByRelation() const;
 };
 
 
