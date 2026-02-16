08c15bcf7f851b6f90e5dac78d8969ce6f1e171c
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-02-16 03:21:38 +0000

    Bug 2015488 - Add support for ::backdrop in <progress> / <input type=range>. r=layout-reviewers,dshin
    Differential Revision: https://phabricator.services.mozilla.com/D282500
    
    UltraBlame original commit: 173a24dd87f67508d546045448fc15268fe90ded

--- layout/forms/nsRangeFrame.h
+++ layout/forms/nsRangeFrame.h
@@ -162,10 +162,10 @@ class nsRangeFrame final : public nsContainerFrame,
   nscoord AutoCrossSize();
 
   
-  void ReflowAnonymousContent(nsPresContext* aPresContext,
-                              ReflowOutput& aDesiredSize,
-                              const mozilla::LogicalSize& aContentBoxSize,
-                              const ReflowInput& aReflowInput);
+  void ReflowChildFrames(nsPresContext* aPresContext,
+                         ReflowOutput& aDesiredSize,
+                         const mozilla::LogicalSize& aContentBoxSize,
+                         const ReflowInput& aReflowInput);
 
   void DoUpdateThumbPosition(nsIFrame* aThumbFrame,
                              const nsSize& aRangeContentBoxSize);
