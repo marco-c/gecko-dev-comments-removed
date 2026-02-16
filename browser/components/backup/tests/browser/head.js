c3fb55b2eb888f7a5a5b5e727a1548889e005231
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-02-16 03:22:45 +0000

    Bug 1995062 - Part 3: fix backup-settings tests r=cdupuis,akulyk
    Differential Revision: https://phabricator.services.mozilla.com/D279017
    
    UltraBlame original commit: 61c427338052715999cb0567d32d3b32304ce4ab

--- browser/components/backup/tests/browser/head.js
+++ browser/components/backup/tests/browser/head.js
@@ -90,3 +90,27 @@ function createMockPassInputEventPromise(inputEl, mockPassword) {
   inputEl.dispatchEvent(new Event("input"));
   return promise;
 }
+
+
+
+
+
+
+
+
+
+async function waitForBackupSettings(browser) {
+  let settingsGroup = browser.contentDocument.querySelector(
+    "setting-group[groupid='backup']"
+  );
+
+  await BrowserTestUtils.waitForMutationCondition(
+    settingsGroup,
+    { childList: true, subtree: true },
+    () => browser.contentDocument.querySelector("backup-settings")
+  );
+
+  let settings = browser.contentDocument.querySelector("backup-settings");
+  await settings.updateComplete;
+  return settings;
+}
