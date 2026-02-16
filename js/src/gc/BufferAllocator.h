a697337e697eb234e313a9603d658b53f25793c1
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-02-16 04:34:10 +0000

    Bug 2016466 - Part 3: Fix buffer allocator stats to include free regions in available chunks r=sfink
    Currently we don't report free regions in available chunks which is likely to
    be most of them.
    
    Differential Revision: https://phabricator.services.mozilla.com/D283072
    
    UltraBlame original commit: 6fc88d8eeee06a8349e6701ce67470c11855cf5e

--- js/src/gc/BufferAllocator.h
+++ js/src/gc/BufferAllocator.h
@@ -226,6 +226,8 @@ class BufferAllocator : public SlimLinkedListElement<BufferAllocator> {
 
   static constexpr size_t FullChunkSizeClass = AllocSizeClasses;
 
+  struct Stats;
+
   
   class AutoLock : public LockGuard<Mutex> {
    public:
@@ -295,6 +297,8 @@ class BufferAllocator : public SlimLinkedListElement<BufferAllocator> {
     void assertEmpty() const;
     void assertContains(size_t sizeClass, FreeRegion* region) const;
     void checkAvailable() const;
+
+    void getStats(Stats& stats);
   };
 
   class ChunkLists {
