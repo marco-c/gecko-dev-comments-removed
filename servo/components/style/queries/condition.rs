da2aeec29935e097f712f01f788e2089bd0314c2
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-02-16 04:35:04 +0000

    Bug 2014695 - patch 1 - Support for 'initial' and 'unset' keywords in container style queries. r=firefox-style-system-reviewers,emilio
    Differential Revision: https://phabricator.services.mozilla.com/D283413
    
    UltraBlame original commit: e4a60bfd71b08eeddb768b0a20bcb2055fc568f6

--- servo/components/style/queries/condition.rs
+++ servo/components/style/queries/condition.rs
@@ -9,10 +9,12 @@
 
 use super::{FeatureFlags, FeatureType, QueryFeatureExpression};
 use crate::custom_properties;
+use crate::derives::*;
+use crate::properties::CSSWideKeyword;
 use crate::stylesheets::CustomMediaEvaluator;
 use crate::values::{computed, AtomString, DashedIdent};
-use crate::{error_reporting::ContextualParseError, parser::ParserContext, parser::Parse};
-use cssparser::{Parser, SourcePosition, Token};
+use crate::{error_reporting::ContextualParseError, parser::Parse, parser::ParserContext};
+use cssparser::{match_ignore_ascii_case, Parser, SourcePosition, Token};
 use selectors::kleene_value::KleeneValue;
 use servo_arc::Arc;
 use std::fmt::{self, Write};
@@ -33,14 +35,19 @@ enum AllowOr {
     No,
 }
 
+#[derive(Clone, Debug, PartialEq, ToShmem)]
+enum StyleFeatureValue {
+    Value(Option<Arc<custom_properties::SpecifiedValue>>),
+    Keyword(CSSWideKeyword),
+}
+
 
 
 #[derive(Clone, Debug, MallocSizeOf, PartialEq, ToShmem)]
 pub struct StyleFeature {
     name: custom_properties::Name,
-    
-    #[ignore_malloc_size_of = "Arc"]
-    value: Option<Arc<custom_properties::SpecifiedValue>>,
+    #[ignore_malloc_size_of = "StyleFeatureValue has an Arc variant"]
+    value: StyleFeatureValue,
 }
 
 impl ToCss for StyleFeature {
@@ -50,9 +57,16 @@ impl ToCss for StyleFeature {
     {
         dest.write_str("--")?;
         crate::values::serialize_atom_identifier(&self.name, dest)?;
-        if let Some(ref v) = self.value {
-            dest.write_str(": ")?;
-            v.to_css(dest)?;
+        match self.value {
+            StyleFeatureValue::Keyword(k) => {
+                dest.write_str(": ")?;
+                k.to_css(dest)?;
+            },
+            StyleFeatureValue::Value(Some(ref v)) => {
+                dest.write_str(": ")?;
+                v.to_css(dest)?;
+            },
+            StyleFeatureValue::Value(None) => (),
         }
         Ok(())
     }
@@ -78,12 +92,16 @@ impl StyleFeature {
         };
         let value = if input.try_parse(|i| i.expect_colon()).is_ok() {
             input.skip_whitespace();
-            Some(Arc::new(custom_properties::SpecifiedValue::parse(
-                input,
-                &context.url_data,
-            )?))
+            if let Ok(keyword) = input.try_parse(|i| CSSWideKeyword::parse(i)) {
+                StyleFeatureValue::Keyword(keyword)
+            } else {
+                StyleFeatureValue::Value(Some(Arc::new(custom_properties::SpecifiedValue::parse(
+                    input,
+                    &context.url_data,
+                )?)))
+            }
         } else {
-            None
+            StyleFeatureValue::Value(None)
         };
         Ok(Self { name, value })
     }
@@ -99,11 +117,37 @@ impl StyleFeature {
             .inherited_custom_properties()
             .get(registration, &self.name);
         KleeneValue::from(match self.value {
-            Some(ref v) => current_value.is_some_and(|cur| {
+            StyleFeatureValue::Value(Some(ref v)) => current_value.is_some_and(|cur| {
                 custom_properties::compute_variable_value(v, registration, ctx)
                     .is_some_and(|v| v == *cur)
             }),
-            None => current_value.is_some(),
+            StyleFeatureValue::Value(None) => current_value.is_some(),
+            StyleFeatureValue::Keyword(kw) => {
+                match kw {
+                    CSSWideKeyword::Unset => current_value.is_none(),
+                    CSSWideKeyword::Initial => {
+                        if let Some(initial) = &registration.initial_value {
+                            let v = custom_properties::compute_variable_value(
+                                &initial,
+                                registration,
+                                ctx,
+                            );
+                            v == current_value.cloned()
+                        } else {
+                            current_value.is_none()
+                        }
+                    },
+                    CSSWideKeyword::Inherit => {
+                        
+                        false
+                    },
+                    
+                    
+                    
+                    
+                    CSSWideKeyword::Revert | CSSWideKeyword::RevertLayer => false,
+                }
+            },
         })
     }
 }
