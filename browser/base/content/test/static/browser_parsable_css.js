aec29a34aa11566d14eae885b503e953ab96ec56
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-02-16 01:34:37 +0000

    Revert "Bug 2005767 - Add scale for absolute/relative dimension tokens r=jules,desktop-theme-reviewers" for causing mochitests failures in browser_aboutCertError.js.
    This reverts commit 34daa969badc4640bc67f581a80ca4b6d574999d.
    
    UltraBlame original commit: 165407531c86eeb26926009a1ce6f88edd8509ba

--- browser/base/content/test/static/browser_parsable_css.js
+++ browser/base/content/test/static/browser_parsable_css.js
@@ -210,10 +210,6 @@ let propNameAllowlist = [
   
   
   { propName: /--color-[a-z]+(-alpha)?(-\d+)?/, isFromDevTools: false },
-
-  
-  
-  { propName: /--dimension(-relative)?-\d+/, isFromDevTools: false },
 ];
 
 
