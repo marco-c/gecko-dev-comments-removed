56e8894b81a604e8d1094c86a379216ebecd6087
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-02-16 02:48:45 +0000

    Bug 1979356 - Remove the "searchable" flag from marker schema fields r=necko-reviewers,profiler-reviewers,toolkit-telemetry-reviewers,canaltinova,jesup
    Differential Revision: https://phabricator.services.mozilla.com/D281769
    
    UltraBlame original commit: 85b321360cef3545bb0e17437b4b1fca0c1b211c

--- modules/libpref/Preferences.cpp
+++ modules/libpref/Preferences.cpp
@@ -540,8 +540,7 @@ struct PreferenceMarker {
   static MarkerSchema MarkerTypeDisplay() {
     using MS = MarkerSchema;
     MS schema{MS::Location::MarkerChart, MS::Location::MarkerTable};
-    schema.AddKeyLabelFormat("prefName", "Name", MS::Format::String,
-                             MS::PayloadFlags::Searchable);
+    schema.AddKeyLabelFormat("prefName", "Name", MS::Format::String);
     schema.AddKeyLabelFormat("prefKind", "Kind", MS::Format::String);
     schema.AddKeyLabelFormat("prefType", "Type", MS::Format::String);
     schema.AddKeyLabelFormat("prefValue", "Value", MS::Format::String);
--- xpcom/base/Logging.cpp
+++ xpcom/base/Logging.cpp
@@ -671,10 +671,8 @@ class LogModuleManager {
           using MS = MarkerSchema;
           MS schema{MS::Location::MarkerChart, MS::Location::MarkerTable};
           schema.SetTableLabel("({marker.data.module}) {marker.data.name}");
-          schema.AddKeyLabelFormat("module", "Module", MS::Format::String,
-                                   MS::PayloadFlags::Searchable);
-          schema.AddKeyLabelFormat("name", "Name", MS::Format::String,
-                                   MS::PayloadFlags::Searchable);
+          schema.AddKeyLabelFormat("module", "Module", MS::Format::String);
+          schema.AddKeyLabelFormat("name", "Name", MS::Format::String);
           return schema;
         }
       };
