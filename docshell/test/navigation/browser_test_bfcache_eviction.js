8614232e4cc722279064c99d397db9d4aae0dfcb
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-02-25 20:40:28 +0000

    Bug 2003179 - Remove unused docshell.shistory.testing.bfevict pref r=gregp
    Differential Revision: https://phabricator.services.mozilla.com/D284551
    
    UltraBlame original commit: 25c60f2e6e5d7f3dd6247ce7fd51ec8a557c1d56

--- docshell/test/navigation/browser_test_bfcache_eviction.js
+++ docshell/test/navigation/browser_test_bfcache_eviction.js
@@ -4,7 +4,6 @@ add_task(async function () {
   await SpecialPowers.pushPrefEnv({
     set: [
       ["browser.sessionhistory.max_total_viewers", 3],
-      ["docshell.shistory.testing.bfevict", true],
       
       ["fission.bfcacheInParent", true],
     ],
