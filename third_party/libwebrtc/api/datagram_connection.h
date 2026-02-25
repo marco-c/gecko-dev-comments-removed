4e2fd719783104c2960ce4b0e709c4b26b17c8bb
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-02-25 21:22:38 +0000

    Bug 2010312 - Vendor libwebrtc from ff2ce57a1b
    Upstream commit: https://webrtc.googlesource.com/src/+/ff2ce57a1b8122f0f1749aff3cb232ae6eed2e0e
        Add getters to DatagramConnection for ice creds
    
        Without this, users must wait for the first IceCandidate, where the
        username fragment and password are included, which is a pain.
    
        Bug: chromium:443019066
        Change-Id: I9958b0e98d43986fd61f00c727ca1dab6ec54e71
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/427364
        Auto-Submit: Tony Herre <herregoogle.com>
        Commit-Queue: Harald Alvestrand <htawebrtc.org>
        Reviewed-by: Harald Alvestrand <htawebrtc.org>
        Cr-Commit-Position: refs/heads/main{#46287}
    
    UltraBlame original commit: e4cac3c8849a83544ec224d8b6b87aded41318e6

--- third_party/libwebrtc/api/datagram_connection.h
+++ third_party/libwebrtc/api/datagram_connection.h
@@ -107,6 +107,9 @@ class RTC_EXPORT DatagramConnection : public RefCountInterface {
   
   virtual void Terminate(
       absl::AnyInvocable<void()> terminate_complete_callback) = 0;
+
+  virtual std::string_view IceUsernameFragment() = 0;
+  virtual std::string_view IcePassword() = 0;
 };
 
 }  
