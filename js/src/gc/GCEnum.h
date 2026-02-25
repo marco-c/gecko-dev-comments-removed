68e2c6e25582106e698e467df9f0e2b406866651
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-02-25 19:06:46 +0000

    Bug 2014970 - Part 11: Remove no longer used IntlOptions. r=dminor
    Differential Revision: https://phabricator.services.mozilla.com/D282127
    
    UltraBlame original commit: c9ba6fea3a2b7dc9c842f29bf7100803a2fa12d7

--- js/src/gc/GCEnum.h
+++ js/src/gc/GCEnum.h
@@ -148,7 +148,6 @@ enum class GCAbortReason {
   _(DebuggerOnStepHandler)                 \
   _(DebuggerOnPopHandler)                  \
   _(ICUObject)                             \
-  _(IntlOptions)                           \
   _(FinalizationRegistryRecordVector)      \
   _(FinalizationRegistryRegistrations)     \
   _(FinalizationRecordVector)              \
