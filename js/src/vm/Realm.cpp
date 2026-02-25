b4f714fbfa387b399b09cfb40db29238f6149798
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-02-25 20:18:44 +0000

    Bug 2014919 - Remove JS support for Shadowrealms r=dminor
    Differential Revision: https://phabricator.services.mozilla.com/D282072
    
    UltraBlame original commit: dca9a6c8974a4cca82439ff4bff0db94df5d778d

--- js/src/vm/Realm.cpp
+++ js/src/vm/Realm.cpp
@@ -13,7 +13,6 @@
 
 #include "jsfriendapi.h"
 
-#include "builtin/WrappedFunctionObject.h"
 #include "debugger/DebugAPI.h"
 #include "debugger/Debugger.h"
 #include "gc/GC.h"
@@ -838,12 +837,6 @@ JS_PUBLIC_API Realm* JS::GetFunctionRealm(JSContext* cx, HandleObject objArg) {
     if (obj->is<BoundFunctionObject>()) {
       obj = obj->as<BoundFunctionObject>().getTarget();
       continue;
-    }
-
-    
-    
-    if (obj->is<WrappedFunctionObject>()) {
-      return obj->nonCCWRealm();
     }
 
     
