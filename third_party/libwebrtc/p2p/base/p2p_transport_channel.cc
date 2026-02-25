0d50c5c11fd472183777f8a792bb9fd54c97fedc
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-02-25 21:21:17 +0000

    Bug 2010312 - Vendor libwebrtc from a05c43fa61
    Upstream commit: https://webrtc.googlesource.com/src/+/a05c43fa616dd5face74e2c17e0cf5e664c440b0
        Delete the async resolver without an async task
    
        Bug: webrtc:12651
        Change-Id: I2fba78f420b4c15f7d192fad2a226d7fed7bbfdd
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/427540
        Reviewed-by: Harald Alvestrand <htawebrtc.org>
        Commit-Queue: Harald Alvestrand <htawebrtc.org>
        Auto-Submit: Tomas Gunnarsson <tommiwebrtc.org>
        Cr-Commit-Position: refs/heads/main{#46269}
    
    UltraBlame original commit: 58316ad3bdaa5558ca1e8d44e8f3b1774bd27d30

--- third_party/libwebrtc/p2p/base/p2p_transport_channel.cc
+++ third_party/libwebrtc/p2p/base/p2p_transport_channel.cc
@@ -973,7 +973,7 @@ void P2PTransportChannel::OnCandidatesReady(
 void P2PTransportChannel::OnCandidateError(
     PortAllocatorSession* ,
     const IceCandidateErrorEvent& event) {
-  RTC_DCHECK(network_thread_ == Thread::Current());
+  RTC_DCHECK_RUN_ON(network_thread_);
   if (candidate_error_callback_) {
     candidate_error_callback_(this, event);
   }
@@ -1289,11 +1289,6 @@ void P2PTransportChannel::OnCandidateResolved(
   Candidate candidate = p->candidate;
   AddRemoteCandidateWithResult(candidate, resolver->result());
   
-  
-  
-  std::unique_ptr<AsyncDnsResolverInterface> to_delete = std::move(p->resolver);
-  
-  network_thread_->PostTask([to_delete = std::move(to_delete)] {});
   resolvers_.erase(p);
 }
 
