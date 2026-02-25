3a181eb1f703d4c25629f80a074cee1f1b498499
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-02-25 19:49:40 +0000

    Bug 2017836 - very minor frame constructor clean-ups. r=layout-reviewers,dholbert
    Noticed while looking at bug 2017711, no behavior change.
    
    Differential Revision: https://phabricator.services.mozilla.com/D284040
    
    UltraBlame original commit: d6e6f9f9226a110c5607b1c2b4fbe9ab2616ce39

--- layout/base/nsCSSFrameConstructor.h
+++ layout/base/nsCSSFrameConstructor.h
@@ -1493,16 +1493,8 @@ class nsCSSFrameConstructor final : public nsFrameManager {
   static const FrameConstructionData* FindXULTagData(const Element&,
                                                      ComputedStyle&);
   
-  static const FrameConstructionData* FindPopupGroupData(const Element&,
-                                                         ComputedStyle&);
-  static const FrameConstructionData* FindXULButtonData(const Element&,
-                                                        ComputedStyle&);
   static const FrameConstructionData* FindXULLabelOrDescriptionData(
       const Element&, ComputedStyle&);
-#ifdef XP_MACOSX
-  static const FrameConstructionData* FindXULMenubarData(const Element&,
-                                                         ComputedStyle&);
-#endif 
 
   
 
