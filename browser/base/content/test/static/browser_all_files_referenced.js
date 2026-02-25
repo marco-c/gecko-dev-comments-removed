7857af72829524e931a33703e377608c6476537a
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-02-25 19:39:34 +0000

    Bug 2010294 - (part 3) manage recovering and backing up selectable profiles. r=cdupuis,profiles-reviewers,mconley,mossop
    Differential Revision: https://phabricator.services.mozilla.com/D279609
    
    UltraBlame original commit: b10b4a0acf7ebcbe76b6916b12ac2cb36cff400a

--- browser/base/content/test/static/browser_all_files_referenced.js
+++ browser/base/content/test/static/browser_all_files_referenced.js
@@ -310,7 +310,9 @@ var allowlist = [
 
   
   { file: "chrome://browser/content/backup/BackupManifest.1.schema.json" },
+  { file: "chrome://browser/content/backup/BackupManifest.2.schema.json" },
   { file: "chrome://browser/content/backup/ArchiveJSONBlock.1.schema.json" },
+  { file: "chrome://browser/content/backup/ArchiveJSONBlock.2.schema.json" },
 
   
   {
