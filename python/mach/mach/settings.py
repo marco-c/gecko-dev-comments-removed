1fa46178d00522f1f75ca1fb91b5ef73f88f566d
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-02-25 19:03:36 +0000

    Bug 1884983 - Fix formatting of runprefs description in './mach settings' output. r=ahochheiden
    Differential Revision: https://phabricator.services.mozilla.com/D283365
    
    UltraBlame original commit: 3ace307f44a058fd8f0836bf7c9252062f861acd

--- python/mach/mach/settings.py
+++ python/mach/mach/settings.py
@@ -2,6 +2,8 @@
 
 
 
+from textwrap import dedent
+
 from mach.decorators import SettingsProvider
 
 
@@ -57,11 +59,11 @@ class MachSettings:
                 (
                     "runprefs.*",
                     "string",
-                    """
+                    dedent("""
         Pass a pref into Firefox when using `mach run`, of the form `foo.bar=value`.
         Prefs will automatically be cast into the appropriate type. Integers can be
         single quoted to force them to be strings.
-        """.strip(),
+        """),
                 ),
             ]
 
