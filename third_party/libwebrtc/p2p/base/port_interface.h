721fc55e348e783a74d602aea20c2e19782fcde9
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-02-25 21:21:21 +0000

    Bug 2010312 - Vendor libwebrtc from f0c4b14bc0
    Upstream commit: https://webrtc.googlesource.com/src/+/f0c4b14bc076d06e10c92596fce6ae26eacca379
        Remove sigslot from Port and PortInterface classes
    
        Bug: webrtc:42222066
        Change-Id: I595ad6a617cb17082286dfbd788e2696076c3c9c
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/427720
        Commit-Queue: Harald Alvestrand <htawebrtc.org>
        Reviewed-by: Tomas Gunnarsson <tommiwebrtc.org>
        Auto-Submit: Harald Alvestrand <htawebrtc.org>
        Commit-Queue: Tomas Gunnarsson <tommiwebrtc.org>
        Cr-Commit-Position: refs/heads/main{#46270}
    
    UltraBlame original commit: fe8ed1b3c54b8b32a81a139f10fbe300a31f6c45

--- third_party/libwebrtc/p2p/base/port_interface.h
+++ third_party/libwebrtc/p2p/base/port_interface.h
@@ -30,10 +30,8 @@
 #include "rtc_base/net_helper.h"
 #include "rtc_base/network.h"
 #include "rtc_base/network/sent_packet.h"
-#include "rtc_base/sigslot_trampoline.h"
 #include "rtc_base/socket.h"
 #include "rtc_base/socket_address.h"
-#include "rtc_base/third_party/sigslot/sigslot.h"
 
 namespace webrtc {
 
@@ -207,27 +205,6 @@ class PortInterface {
                                     absl::string_view remote_ufrag) = 0;
 
   virtual int16_t network_cost() const = 0;
-  
-  
-  
-  sigslot::signal6<PortInterface*,
-                   const SocketAddress&,
-                   ProtocolType,
-                   IceMessage*,
-                   const std::string&,
-                   bool>
-      SignalUnknownAddress;
-  sigslot::signal4<PortInterface*, const char*, size_t, const SocketAddress&>
-      SignalReadPacket;
-  sigslot::signal1<const SentPacketInfo&> SignalSentPacket;
-  SignalTrampoline<PortInterface, &PortInterface::SignalUnknownAddress>
-      unknown_address_trampoline_;
-  SignalTrampoline<PortInterface, &PortInterface::SignalReadPacket>
-      read_packet_trampoline_;
-  SignalTrampoline<PortInterface, &PortInterface::SignalSentPacket>
-      sent_packet_trampoline_;
-  sigslot::signal1<PortInterface*> SignalRoleConflict;
-
   
   
   friend class Connection;
