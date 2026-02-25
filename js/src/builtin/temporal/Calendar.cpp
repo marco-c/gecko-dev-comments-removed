b3d8ae1aef8f1503d5c81454da89c531fde8ae05
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-02-25 20:44:59 +0000

    Bug 2018464: Don't TimeClip when formatting Temporal plain date-time types. r=spidermonkey-reviewers,mgaudet
    Replace `ClippedTime` with `EpochMilliseconds`, because Temporal plain objects
    can represent a date-time value exceeding the valid range of `ClippedTime`.
    
    Additionally relax some assertions from `ISODateTimeWithinLimits` to
    `IsValidISODateTime`, because `GetUTCEpochNanoseconds` can now be called with
    date-time values outside the valid limits.
    
    Test262 PR: <github.com/tc39/test262/pull/4940>
    
    Differential Revision: https://phabricator.services.mozilla.com/D284430
    
    UltraBlame original commit: bc7e22e8c075bfaff4a98fed40c1a85188352fa7

--- js/src/builtin/temporal/Calendar.cpp
+++ js/src/builtin/temporal/Calendar.cpp
@@ -156,7 +156,7 @@ static int32_t WeekDay(int32_t day) {
 
 
 static int32_t ISODayOfWeek(const ISODate& isoDate) {
-  MOZ_ASSERT(ISODateWithinLimits(isoDate));
+  MOZ_ASSERT(IsValidISODate(isoDate));
 
   
   int32_t day = MakeDay(isoDate);
@@ -182,7 +182,7 @@ static constexpr auto FirstDayOfMonth(int32_t year) {
 
 
 static int32_t ISODayOfYear(const ISODate& isoDate) {
-  MOZ_ASSERT(ISODateWithinLimits(isoDate));
+  MOZ_ASSERT(IsValidISODate(isoDate));
 
   const auto& [year, month, day] = isoDate;
 
@@ -247,7 +247,7 @@ static int64_t MakeTime(const Time& time) {
 
 
 int32_t js::temporal::MakeDay(const ISODate& date) {
-  MOZ_ASSERT(ISODateWithinLimits(date));
+  MOZ_ASSERT(IsValidISODate(date));
 
   return DayFromYear(date.year) + ISODayOfYear(date) - 1;
 }
@@ -256,7 +256,7 @@ int32_t js::temporal::MakeDay(const ISODate& date) {
 
 
 int64_t js::temporal::MakeDate(const ISODateTime& dateTime) {
-  MOZ_ASSERT(ISODateTimeWithinLimits(dateTime));
+  MOZ_ASSERT(IsValidISODateTime(dateTime));
 
   
 
@@ -277,7 +277,7 @@ struct YearWeek final {
 
 
 static YearWeek ISOWeekOfYear(const ISODate& isoDate) {
-  MOZ_ASSERT(ISODateWithinLimits(isoDate));
+  MOZ_ASSERT(IsValidISODate(isoDate));
 
   
   int32_t year = isoDate.year;
