8b49f89cb333d517b25dd9b952d919ae5ecf1572
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-02-25 21:13:36 +0000

    Bug 2010312 - Vendor libwebrtc from e8fbb6ec60
    Upstream commit: https://webrtc.googlesource.com/src/+/e8fbb6ec60bc6e479ae13935b5b574100ada2a0e
        Ensure SrtpTransport::SendRtcpPacket use base class
    
        Make RtpTransport::SendPacket private and ensure SrtpTransport explicitly invoke base class when sending packets.
    
        This ensure RTCP actually is sent as ECT1 if RTP packets are received with ECN.
    
        No-Iwyu: Broken on bots
        Bug: webrtc:457646304
        Change-Id: Iab747de461a849cf2a7af298511a0f62bc98d508
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/424901
        Commit-Queue: Per Kjellander <perkjwebrtc.org>
        Reviewed-by: Danil Chapovalov <danilchapwebrtc.org>
        Cr-Commit-Position: refs/heads/main{#46169}
    
    UltraBlame original commit: dc3bebfe37f1e39281401c11869abcc89cb42b6b

--- third_party/libwebrtc/pc/rtp_transport.h
+++ third_party/libwebrtc/pc/rtp_transport.h
@@ -93,10 +93,6 @@ class RtpTransport : public RtpTransportInternal {
                    Timestamp arrival_time,
                    EcnMarking ecn);
 
-  bool SendPacket(bool rtcp,
-                  CopyOnWriteBuffer* packet,
-                  const AsyncSocketPacketOptions& options,
-                  int flags);
   flat_set<uint32_t> GetSsrcsForSink(RtpPacketSinkInterface* sink);
 
   
@@ -107,6 +103,10 @@ class RtpTransport : public RtpTransportInternal {
   virtual void OnWritableState(PacketTransportInternal* packet_transport);
 
  private:
+  bool SendPacket(bool rtcp,
+                  CopyOnWriteBuffer* packet,
+                  const AsyncSocketPacketOptions& options,
+                  int flags);
   
   void ChangePacketTransport(PacketTransportInternal* new_transport,
                              PacketTransportInternal*& transport_to_change);
