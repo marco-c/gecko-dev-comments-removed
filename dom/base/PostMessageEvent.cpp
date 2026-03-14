29a622ad86bffc96d7f402639e99c08ab420d5c6
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-03-14 03:26:10 +0000

    Bug 2003504 clear source browsing context early, if possible, r=nika,smaug
    Differential Revision: https://phabricator.services.mozilla.com/D242075
    
    UltraBlame original commit: 68b8be0d12a38ce59814967bd5aafd220c6f3973

--- dom/base/PostMessageEvent.cpp
+++ dom/base/PostMessageEvent.cpp
@@ -17,6 +17,7 @@
 #include "mozilla/dom/MessageEventBinding.h"
 #include "mozilla/dom/MessagePort.h"
 #include "mozilla/dom/RootedDictionary.h"
+#include "mozilla/dom/WindowContext.h"
 #include "nsDocShell.h"
 #include "nsGlobalWindowInner.h"
 #include "nsGlobalWindowOuter.h"
@@ -52,6 +53,12 @@ PostMessageEvent::~PostMessageEvent() = default;
 
 
 MOZ_CAN_RUN_SCRIPT_BOUNDARY NS_IMETHODIMP PostMessageEvent::Run() {
+  if (mCallerWindowID) {
+    RefPtr<WindowContext> wc = WindowContext::GetById(mCallerWindowID);
+    if (!wc || !wc->IsCurrent()) {
+      mSource = nullptr;
+    }
+  }
   
   
   
