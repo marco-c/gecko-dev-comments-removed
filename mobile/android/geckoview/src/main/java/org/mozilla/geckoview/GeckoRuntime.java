8cac669a461ec3e62410765f80bc71082ae5c4bf
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-03-14 04:03:57 +0000

    Bug 2023004 - Change how we do child process preallocation. r=tcampbell,geckoview-reviewers,android-reviewers,geckoview-api-reviewers,jonalmeida
    In the past, we were allocating a content process unconditionally
    during GeckoThread initialization. This means that we would spend
    CPU time starting up a tab process very early during startup even
    when we were only going to show the home screen. It also means that
    we can't add more processes here without affecting home screen startup
    negatively.
    
    This patch does the following:
    - Do the preallocation during GeckoEngine.warmUp(). We had an existing
      warmUp method but it was just there to trigger the instance creation
      on a lazy property. Now it has an actual purpose.
    - Replace the call to warmUp() in FenixApplication.onCreate with a simple
      property access, so that Engine instance creation is still triggered in
      the same place as before.
    - Add a call to warmUp() to the visual completeness queue.
      This means that during home view startup, we will delay the child
      process preallocation until the home view is visible.
    - Additionally, to keep preallocation happening early during applink
      startup, create a speculative Gecko session if the IntentReceiverActivity
      notices that we're about to load a page. And call warmUp() from the
      speculative Gecko session method.
    - Call setIsolatedProcessEnabled and setAppZygoteEnabled earlier so that
      warmUp creates the right content process types.
    
    As a result, we'll still allocate a tab process early during applink startup,
    but we'll delay it during homeview startup.
    
    Differential Revision: https://phabricator.services.mozilla.com/D277473
    
    UltraBlame original commit: 979858ece73c12e44a7e3eca2297636abf3b1c1e

--- mobile/android/geckoview/src/main/java/org/mozilla/geckoview/GeckoRuntime.java
+++ mobile/android/geckoview/src/main/java/org/mozilla/geckoview/GeckoRuntime.java
@@ -52,6 +52,8 @@ import org.mozilla.gecko.GeckoSystemStateListener;
 import org.mozilla.gecko.GeckoThread;
 import org.mozilla.gecko.annotation.WrapForJNI;
 import org.mozilla.gecko.crashhelper.CrashHelper;
+import org.mozilla.gecko.process.GeckoProcessManager;
+import org.mozilla.gecko.process.GeckoProcessType;
 import org.mozilla.gecko.process.MemoryController;
 import org.mozilla.gecko.util.BundleEventListener;
 import org.mozilla.gecko.util.DebugConfig;
@@ -247,6 +249,7 @@ public final class GeckoRuntime implements Parcelable {
   }
 
   private static GeckoRuntime sRuntime;
+  private static boolean sHasWarmedUpChildProcesses = false;
   private GeckoRuntimeSettings mSettings;
   private Delegate mDelegate;
   private ServiceWorkerDelegate mServiceWorkerDelegate;
@@ -451,7 +454,7 @@ public final class GeckoRuntime implements Parcelable {
     if (DEBUG) {
       Log.d(LOGTAG, "init");
     }
-    int flags = GeckoThread.FLAG_PRELOAD_CHILD;
+    int flags = 0;
 
     if (settings.getPauseForDebuggerEnabled()) {
       flags |= GeckoThread.FLAG_DEBUGGING;
@@ -459,14 +462,6 @@ public final class GeckoRuntime implements Parcelable {
 
     if (!settings.getLowMemoryDetection()) {
       flags |= GeckoThread.FLAG_DISABLE_LOW_MEMORY_DETECTION;
-    }
-
-    if (settings.getIsolatedProcessEnabled()) {
-      flags |= GeckoThread.FLAG_CONTENT_ISOLATED;
-    }
-
-    if (settings.getAppZygoteProcessEnabled()) {
-      flags |= GeckoThread.FLAG_CONTENT_ISOLATED_HAS_ZYGOTE;
     }
 
     final Class<?> crashHandler = settings.getCrashHandler();
@@ -523,6 +518,10 @@ public final class GeckoRuntime implements Parcelable {
       } catch (final FileNotFoundException e) {
       }
     }
+
+    final GeckoProcessManager pm = GeckoProcessManager.getInstance();
+    pm.setIsolatedProcessEnabled(settings.getIsolatedProcessEnabled());
+    pm.setAppZygoteEnabled(settings.getAppZygoteProcessEnabled());
 
     final int[] fds = startCrashHelper();
 
@@ -620,6 +619,21 @@ public final class GeckoRuntime implements Parcelable {
   public static @NonNull GeckoRuntime create(final @NonNull Context context) {
     ThreadUtils.assertOnUiThread();
     return create(context, new GeckoRuntimeSettings());
+  }
+
+  
+
+
+
+  @UiThread
+  public void warmUp() {
+    ThreadUtils.assertOnUiThread();
+    if (sHasWarmedUpChildProcesses) {
+      return;
+    }
+
+    GeckoProcessManager.getInstance().preload(GeckoProcessType.CONTENT);
+    sHasWarmedUpChildProcesses = true;
   }
 
   
