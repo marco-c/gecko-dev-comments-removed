2c288b9afc700042ac4234a1c31fc9cb503ef668
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-03-14 03:47:17 +0000

    Bug 2021195 [wpt PR 58274] - Allow JS Self-Profiling in Dedicated Workers, a=testonly
    Automatic update from web-platform-tests
    Allow JS Self-Profiling in Dedicated Workers
    
    Document Policy is now inherited by Dedicated Workers (see
    https://chromium-review.googlesource.com/c/chromium/src/+/7236250).
    
    This change allows the JS Self-Profiling API to be used in workers when
    Document Policy is enabled and the js-profiling feature is allowed via
    the Document-Policy: js-profiling response header.
    
    This CL also adds WPT tests to verify that Dedicated Workers can use the
    profiling API when the js-profiling feature is enabled through Document
    Policy.
    
    Spec PR (merged) - https://github.com/WICG/js-self-profiling/pull/88
    ChromeStatus - https://chromestatus.com/feature/5159559872249856 I2P -
    https://groups.google.com/a/chromium.org/d/msgid/blink-dev/69975252.050a0220.1f729.0da6.GAE%40google.com
    
    Bug: 482085416
    Change-Id: Ica61d6d2169926095f29f7a82e482acba56c7fc2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7546208
    Reviewed-by: Scott Haseley <shaseleychromium.org>
    Commit-Queue: Monica Chintala <monicachmicrosoft.com>
    Reviewed-by: Liang Zhao <lzhaomicrosoft.com>
    Cr-Commit-Position: refs/heads/main{#1594390}
    
    --
    
    wpt-commits: a8eccee92ad9f1160d03c228126a1d7bfd803c73
    wpt-pr: 58274
    
    UltraBlame original commit: 8211f39d7088ddadcebf491f58ad8a95d7c9eeac

---
+++ testing/web-platform/tests/js-self-profiling/without-document-policy/dedicated-worker-profiler-test.js
@@ -0,0 +1,7 @@
+
+try {
+  new Profiler({ sampleInterval: 10, maxBufferSize: Number.MAX_SAFE_INTEGER });
+  self.postMessage({ success: true, message: 'Profiler created' });
+} catch (e) {
+  self.postMessage({ success: false, error: e.name, message: e.message });
+}
