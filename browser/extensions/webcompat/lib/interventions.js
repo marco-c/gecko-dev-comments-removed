d3750ed29b92d71ca72efabc8f651fcb821f50bb
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-03-14 03:03:04 +0000

    Bug 2020285 - Add UA override for Eurocontrol NOP. r=webcompat-reviewers,denschub
    Differential Revision: https://phabricator.services.mozilla.com/D286217
    
    UltraBlame original commit: 0410abe82545f263a5045227a2dda8e695865bdc

--- browser/extensions/webcompat/lib/interventions.js
+++ browser/extensions/webcompat/lib/interventions.js
@@ -844,7 +844,7 @@ class Interventions {
       matches,
     };
 
-    let { all_frames, css, isolated, js, run_at } =
+    let { all_frames, css, isolated, js, match_origin_as_fallback, run_at } =
       intervention.content_scripts;
     if (!css && !js) {
       console.error(`Missing js or css for content_script in ${label}`);
@@ -859,6 +859,9 @@ class Interventions {
     }
     if (all_frames) {
       registration.allFrames = true;
+    }
+    if (match_origin_as_fallback) {
+      registration.matchOriginAsFallback = true;
     }
     if (css) {
       registration.css = css.map(item => {
