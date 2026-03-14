f59ed18162b7a51ef030a7cc7bdcd90f0709f9e5
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-03-14 01:48:30 +0000

    Bug 1969165 - remove our JS webcompat intervention for mpi.mb.ca; r=webcompat-reviewers,denschub
    Differential Revision: https://phabricator.services.mozilla.com/D285569
    
    UltraBlame original commit: c938b57cb002e360a64c08d0effa880eb179313d

--- browser/extensions/webcompat/lib/intervention_helpers.js
+++ browser/extensions/webcompat/lib/intervention_helpers.js
@@ -210,9 +210,6 @@ const GOOGLE_TLDS = [
 
 var InterventionHelpers = {
   skip_if_functions: {
-    getWeekInfo_defined: () => {
-      return !!Intl?.Locale?.prototype?.getWeekInfo;
-    },
     InstallTrigger_defined: () => {
       return "InstallTrigger" in window;
     },
