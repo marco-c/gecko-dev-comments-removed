4f05752165b12268836d35b57c9483ff5810a096
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-03-14 01:19:48 +0000

    Bug 2008662 - part 1 - Lazy load BrowsersCache in AppLinksUseCases r=android-reviewers,sfamisa
    This ensures BrowsersCache is rehydrated only when needed but also that
    we'll not query all installed browsers multiple times for multiple
    GetAppLinkRedirect calls.
    
    Differential Revision: https://phabricator.services.mozilla.com/D282649
    
    UltraBlame original commit: 477724f7e0824b6116b26fda45e68036cd737be6

--- mobile/android/android-components/components/feature/app-links/src/main/java/mozilla/components/feature/app/links/AppLinksUseCases.kt
+++ mobile/android/android-components/components/feature/app-links/src/main/java/mozilla/components/feature/app/links/AppLinksUseCases.kt
@@ -53,7 +53,7 @@ class AppLinksUseCases(
     private val context: Context,
     private var launchInApp: () -> Boolean = { false },
     private val alwaysDeniedSchemes: AlwaysDeniedSchemes = AlwaysDeniedSchemes(ALWAYS_DENY_SCHEMES),
-    private val installedBrowsers: Browsers = BrowsersCache.all(context),
+    private val installedBrowsers: () -> Browsers = { BrowsersCache.all(context) },
 ) {
     @Suppress(
         "QueryPermissionsNeeded", // We expect our browsers to have the QUERY_ALL_PACKAGES permission
@@ -107,7 +107,7 @@ class AppLinksUseCases(
             val isAppIntentHttpOrHttps = redirectData.appIntent?.data?.isHttpOrHttps ?: false
             val isEngineSupportedScheme = ENGINE_SUPPORTED_SCHEMES.contains(url.toUri().scheme)
             val isBrowserRedirect = redirectData.resolveInfo?.activityInfo?.packageName?.let { packageName ->
-                installedBrowsers.isInstalled(packageName)
+                installedBrowsers().isInstalled(packageName)
             } ?: false
 
             val appName = redirectData.resolveInfo?.let { resolveInfo ->
