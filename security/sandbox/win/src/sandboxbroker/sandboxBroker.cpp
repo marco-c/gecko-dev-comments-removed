6efc6658d21ddcfe3824781fabd61f598c911670
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-03-14 03:42:52 +0000

    Bug 2022584 - avoid closing KsecDD for Trellix DLLs on ARM64 r=bobowen
    The previous fix for this in bug 2008739 didn't have the right filenames
    for ARM64.
    
    Differential Revision: https://phabricator.services.mozilla.com/D287153
    
    UltraBlame original commit: 950bc43c7d42b854ee8b5e20e594de487f0ec2c8

--- security/sandbox/win/src/sandboxbroker/sandboxBroker.cpp
+++ security/sandbox/win/src/sandboxbroker/sandboxBroker.cpp
@@ -1117,12 +1117,16 @@ void SandboxBroker::SetSecurityLevelForContentProcess(int32_t aSandboxLevel,
   if (StaticPrefs::security_sandbox_content_close_ksecdd_handle()) {
     
     
+    bool isTrellixDllLoaded;
 #if defined(_M_X64)
-    const wchar_t* trellixDllName = L"fcagff64.dll";
+    isTrellixDllLoaded = !!::GetModuleHandleW(L"fcagff64.dll") ||
+                         !!::GetModuleHandleW(L"fcagff64hc.dll");
+#elif defined(_M_ARM64)
+    isTrellixDllLoaded = !!::GetModuleHandleW(L"fcagffarm64hc.dll");
 #else
-    const wchar_t* trellixDllName = L"fcagff.dll";
+    isTrellixDllLoaded = !!::GetModuleHandleW(L"fcagff.dll");
 #endif
-    if (!::GetModuleHandleW(trellixDllName)) {
+    if (isTrellixDllLoaded) {
       result = config->AddKernelObjectToClose(L"File", L"\\Device\\KsecDD");
       MOZ_RELEASE_ASSERT(sandbox::SBOX_ALL_OK == result,
                          "AddKernelObjectToClose should never fail.");
