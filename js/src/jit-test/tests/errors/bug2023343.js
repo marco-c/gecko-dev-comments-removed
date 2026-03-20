83f8dc23f036ec91081360ece7a36444e8eabbb6
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-03-20 00:32:04 +0000

    Bug 2023343: Align the C++ interpreter with the JITs r=jandem
    Differential Revision: https://phabricator.services.mozilla.com/D288102
    
    UltraBlame original commit: 347001a92555d46c247c311d83ed7691fbc4debf

---
+++ js/src/jit-test/tests/errors/bug2023343.js
@@ -0,0 +1,13 @@
+
+
+let g = newGlobal({newCompartment: true});
+
+function f() {
+  for (var y in {x:1}) {
+    try {
+      g.eval("throw 0")
+    } finally {}
+  }
+}
+try { f() } catch {}
+oomTest(f)
