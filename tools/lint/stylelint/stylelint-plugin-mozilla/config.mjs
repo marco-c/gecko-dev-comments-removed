badbd0cea7b54ee3b32f9cff5a68b4a71ea367b6
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-03-20 00:50:43 +0000

    Bug 2022974 - Update global-shared.css so it passes use-design-tokens rules r=desktop-theme-reviewers,emilio,dao
    While the focus of this work is to resolve stylelint issues, there are places where more semantic tokens are used rather than replicating the existing styles, and the spacing values are updated to use space tokens, which are based on `rem`, so there may be cases where pixels would be preferable.
    
    Differential Revision: https://phabricator.services.mozilla.com/D287999
    
    UltraBlame original commit: 241a46fc9aed0d4ac1ff23c1ca2fc3cb12921594

--- tools/lint/stylelint/stylelint-plugin-mozilla/config.mjs
+++ tools/lint/stylelint/stylelint-plugin-mozilla/config.mjs
@@ -4,7 +4,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-import { createRawValuesObject } from "./helpers.mjs";
+import { SYSTEM_COLORS, createRawValuesObject } from "./helpers.mjs";
 
 /**
  * @typedef {object} PropertyTypeConfig
@@ -17,32 +17,8 @@ import { createRawValuesObject } from "./helpers.mjs";
  * @property {string[]} [allowedUnits] Specific unit types allowed (e.g., ["em", "ch", "%"]). If provided, only these units are allowed when allowUnits is true
  * @property {Record<string, string>} [customFixes] Map of raw values to their token replacements for autofix
  * @property {Record<string, string>} [customSuggestions] Map of raw values to their token replacements for suggested fixes
+ * @property {boolean} [warnSystemColors] Whether to warn about system colors when there are no suitable tokens to use.
  */
-
-/**
- * The list of system colors that are valid and intended to be used for high contrast/forced colors mode situations.
- */
-export const SYSTEM_COLORS = [
-  "accentcolor",
-  "accentcolortext",
-  "activetext",
-  "buttonborder",
-  "buttonface",
-  "buttontext",
-  "canvas",
-  "canvastext",
-  "field",
-  "fieldtext",
-  "graytext",
-  "highlight",
-  "highlighttext",
-  "linktext",
-  "mark",
-  "marktext",
-  "selecteditem",
-  "selecteditemtext",
-  "visitedtext",
-];
 
 const customColorFixes = {
   "#000": "black",
@@ -184,6 +160,7 @@ const BackgroundColor = {
   aliasTokenTypes: ["color", "text-color", "border-color", "icon-color"],
   customFixes: customColorFixes,
   customSuggestions: systemColorSuggestions,
+  warnSystemColors: true,
 };
 
 /** @type {PropertyTypeConfig} */
@@ -310,6 +287,7 @@ const BorderColor = {
   aliasTokenTypes: ["color", "background-color", "text-color"],
   customFixes: customColorFixes,
   customSuggestions: systemColorSuggestions,
+  warnSystemColors: true,
 };
 
 /** @type {PropertyTypeConfig} */
@@ -382,6 +360,7 @@ const TextColor = {
   aliasTokenTypes: ["color", "background-color", "border-color"],
   customFixes: customColorFixes,
   customSuggestions: systemColorSuggestions,
+  warnSystemColors: true,
 };
 
 /** @type {PropertyTypeConfig} */
