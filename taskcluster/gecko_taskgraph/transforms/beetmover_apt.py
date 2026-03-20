0293bae4a6595b6446d1b08395e3cc2108b8fb2a
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-03-20 01:00:58 +0000

    Bug 2024583 - refactor beetmover-apt transforms. r=releng-reviewers,taskgraph-reviewers,Eijebong
    Let from_deps group dependencies by product, then filter out unwanted
    non-x86_64 localized jobs.
    
    Differential Revision: https://phabricator.services.mozilla.com/D288643
    
    UltraBlame original commit: f29e37c91fed1303328c4a9d8476247ad29b9e60

--- taskcluster/gecko_taskgraph/transforms/beetmover_apt.py
+++ taskcluster/gecko_taskgraph/transforms/beetmover_apt.py
@@ -3,7 +3,6 @@
 
 
 from taskgraph.transforms.base import TransformSequence
-from taskgraph.util.dependencies import get_primary_dependency
 
 from gecko_taskgraph.util.platforms import architecture
 from gecko_taskgraph.util.scriptworker import (
@@ -16,93 +15,57 @@ transforms = TransformSequence()
 
 
 @transforms.add
+def filter_out_extra_langpacks(config, tasks):
+    """Langpacks are architecture-independent so we only upload them from the x86_64 tasks"""
+    for task in tasks:
+        deps = list(task["dependencies"])
+        for dep in deps:
+            dep_task = config.kind_dependencies_tasks[task["dependencies"][dep]]
+            if not filter_beetmover_apt_dep(dep_task):
+                del task["dependencies"][dep]
+        yield task
+
+
+@transforms.add
 def beetmover_apt(config, tasks):
-    if config.params["project"].startswith("comm"):
-        nightly_product = "thunderbird"
-    else:
-        nightly_product = "firefox"
-
-    product = (
-        nightly_product
-        if config.params["release_type"] == "nightly"
-        else config.params["release_product"]
-    )
-    filtered_tasks = filter_beetmover_apt_tasks(config, tasks, product)
-    dependencies = {}
-    gcs_sources = []
-    for task in filtered_tasks:
-        dep = get_primary_dependency(config, task)
-        assert dep
-
-        dependencies[dep.label] = dep.label
-        gcs_sources.extend(generate_artifact_registry_gcs_sources(dep))
-
-    if not dependencies:
-        return
-
-    description = f"Beetmover APT submissions for the {product} {config.params['release_type']} .deb packages"
-    platform = f"{product}-release/opt"
-    treeherder = {
-        "platform": platform,
-        "tier": 1,
-        "kind": "other",
-        "symbol": "BM-apt",
-    }
     apt_repo_scope = get_beetmover_apt_repo_scope(config)
     repo_action_scope = get_beetmover_repo_action_scope(config)
-    attributes = {
-        "required_signoffs": ["mar-signing"],
-        "shippable": True,
-        "shipping_product": product,
-    }
-    task = {
-        "label": f"{config.kind}-{platform}",
-        "description": description,
-        "worker-type": "beetmover",
-        "treeherder": treeherder,
-        "scopes": [apt_repo_scope, repo_action_scope],
-        "attributes": attributes,
-        "shipping-phase": "ship",
-        "shipping-product": product,
-        "dependencies": dependencies,
-    }
-    worker = {
-        "implementation": "beetmover-import-from-gcs-to-artifact-registry",
-        "product": product,
-        "gcs-sources": gcs_sources,
-    }
-    task["worker"] = worker
-    yield task
 
-
-def filter_beetmover_apt_tasks(config, tasks, product):
     for task in tasks:
-        task["primary-dependency"] = get_primary_dependency(config, task)
-        if filter_beetmover_apt_task(task, product):
-            yield task
+        product = task["attributes"]["shipping_product"]
+        gcs_sources = []
+
+        for dep_label in task["dependencies"].values():
+            dep = config.kind_dependencies_tasks[dep_label]
+            gcs_sources.extend(generate_artifact_registry_gcs_sources(dep))
+
+        description = f"Beetmover APT submissions for the {product} {config.params['release_type']} .deb packages"
+        task.update({
+            "label": f"{config.kind}-{product}",
+            "description": description,
+            "scopes": [apt_repo_scope, repo_action_scope],
+        })
+        task.setdefault("worker", {}).update({
+            "product": task["attributes"]["shipping_product"],
+            "gcs-sources": gcs_sources,
+        })
+        task.setdefault("treeherder", {})["platform"] = f"{product}-release/opt"
+        yield task
 
 
-def filter_beetmover_apt_task(task, product):
+def filter_beetmover_apt_dep(task):
     
     
     
     return (
         is_x86_64_l10n_task(task) or is_not_l10n_task(task)
-    ) and is_task_for_product(task, product)
+    )
 
 
 def is_x86_64_l10n_task(task):
-    dep = task["primary-dependency"]
-    locale = dep.attributes.get("locale")
-    return locale and architecture(dep.attributes["build_platform"]) == "x86_64"
+    locale = task.attributes.get("locale")
+    return locale and architecture(task.attributes["build_platform"]) == "x86_64"
 
 
 def is_not_l10n_task(task):
-    dep = task["primary-dependency"]
-    locale = dep.attributes.get("locale")
-    return not locale
-
-
-def is_task_for_product(task, product):
-    dep = task["primary-dependency"]
-    return dep.attributes.get("shipping_product") == product
+    return not task.attributes.get("locale")
