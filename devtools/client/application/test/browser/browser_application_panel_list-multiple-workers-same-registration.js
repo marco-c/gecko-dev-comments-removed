13b8832f862ba45ccd6e5516e2d0a0b5ad107107
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-03-20 00:59:03 +0000

    Bug 2024571 - [devtools] Wait for individual workers in browser_application_panel_list-multiple-workers-same-registration.js r=bomsy,devtools-reviewers
    Alternative to D288336, maybe waiting for the actual workers to show up will fix this intermittent.
    
    Differential Revision: https://phabricator.services.mozilla.com/D288610
    
    UltraBlame original commit: 4f04f512e2ca7b10fb487de02b7326fd320563d3

--- devtools/client/application/test/browser/browser_application_panel_list-multiple-workers-same-registration.js
+++ devtools/client/application/test/browser/browser_application_panel_list-multiple-workers-same-registration.js
@@ -35,6 +35,10 @@ add_task(async function () {
   
   
   info("Check the workers data for this registration");
+  await waitFor(
+    () => registrationContainer.querySelectorAll(".js-sw-worker").length === 2,
+    "Wait until both workers are listed for the registration."
+  );
   const workers = registrationContainer.querySelectorAll(".js-sw-worker");
   is(workers.length, 2, "Registration has two workers");
   
