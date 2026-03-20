2e7c0a72dd023e9116735dab921bc0c22feb7dcb
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-03-20 00:39:22 +0000

    Revert "Bug 2023543 - Fix error message link to open sumo page correctly and styling change. r=cdupuis,desktop-theme-reviewers,sfoster" for causing bc failures at browser_settings_restore_from_backup.js
    This reverts commit 91afd84f49b12a5f414a4104d0ecbf80f7d5207b.
    
    UltraBlame original commit: 699a9918d66182cfd9cdc961b1044a07a2fbbc4c

--- browser/components/backup/content/restore-from-backup.mjs
+++ browser/components/backup/content/restore-from-backup.mjs
@@ -417,7 +417,6 @@ export default class RestoreFromBackup extends MozLitElement {
         <fieldset id="backup-filepicker-controls">
           <label
             id="backup-filepicker-label"
-            class="heading-medium"
             for="backup-filepicker-input"
             data-l10n-id="restore-from-backup-filepicker-label"
           ></label>
@@ -631,10 +630,14 @@ export default class RestoreFromBackup extends MozLitElement {
         class="field-error"
         data-l10n-id="backup-file-restore-file-validation-error"
       >
-        ${this.getSupportLinkAnchor({
-          id: "backup-generic-error-link",
-          l10nName: "restore-problems",
-        })}
+        <a
+          id="backup-generic-error-link"
+          target="_blank"
+          slot="support-link"
+          data-l10n-name="restore-problems"
+          href=${this.getSupportURLWithUTM("firefox-backup")}
+          rel="noopener noreferrer"
+        ></a>
       </span>
     `;
   }
