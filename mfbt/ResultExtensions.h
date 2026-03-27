77b3b5f2f1854698a171dd587c13a553940d2900
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-03-27 01:27:52 +0000

    Bug 2025718: Use __VA_OPT__ instead of non-standard ##__VA_ARGS__. r=sergesanspaille
    The non-standard extension `##__VA_ARGS__` can be replaced with `__VA_OPT__(, ) __VA_ARGS__`
    now that we compile with C++20.
    
    This patch only replaces `##__VA_ARGS__` in "mfbt/", but there are many more uses of
    `##__VA_ARGS__` throughout the code base which could be updated.
    
    Differential Revision: https://phabricator.services.mozilla.com/D289473
    
    UltraBlame original commit: 0288cd826ecf688c1ccbfe637dc66c40cac05187

--- mfbt/ResultExtensions.h
+++ mfbt/ResultExtensions.h
@@ -345,10 +345,11 @@ auto ToResultInvokeMember(const SmartPtr<const T>& aObj,
 
 
 
-#define MOZ_TO_RESULT_INVOKE_MEMBER(obj, methodname, ...)                \
-  ::mozilla::ToResultInvokeMember(                                       \
-      (obj), &::mozilla::detail::DerefedType<decltype(obj)>::methodname, \
-      ##__VA_ARGS__)
+#define MOZ_TO_RESULT_INVOKE_MEMBER(obj, methodname, ...)                    \
+  ::mozilla::ToResultInvokeMember(                                           \
+      (obj),                                                                 \
+      &::mozilla::detail::DerefedType<decltype(obj)>::methodname __VA_OPT__( \
+          , ) __VA_ARGS__)
 
 
 
@@ -362,7 +363,7 @@ auto ToResultInvokeMember(const SmartPtr<const T>& aObj,
   ::mozilla::ToResultInvoke<MOZ_REMOVE_PAREN(resultType)>(                  \
       ::std::mem_fn(                                                        \
           &::mozilla::detail::DerefedType<decltype(obj)>::methodname),      \
-      (obj), ##__VA_ARGS__)
+      (obj)__VA_OPT__(, ) __VA_ARGS__)
 
 }  
 
