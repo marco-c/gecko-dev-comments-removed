ab9f20ea189243f98ad6945714549175c6d8c9e7
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-03-27 01:45:24 +0000

    Bug 2025744 - Added function in mozbuild to set TASKCLUSTER_ROOT_URL env var if not already defined. r=releng-reviewers,ahal
    Differential Revision: https://phabricator.services.mozilla.com/D289638
    
    UltraBlame original commit: e7ca4e8f2658e21d154c382ef49631817e62b52b

--- tools/tryselect/task_config.py
+++ tools/tryselect/task_config.py
@@ -19,6 +19,7 @@ from textwrap import dedent
 import mozpack.path as mozpath
 import requests
 from mozbuild.base import BuildEnvironmentNotFoundException, MozbuildObject
+from mozbuild.util import set_taskcluster_root_url
 
 from .tasks import resolve_tests_by_suite
 from .util.ssh import get_ssh_user
@@ -348,6 +349,8 @@ class ExistingTasks(ParameterConfig):
     def find_decision_task(self, use_existing_tasks):
         from taskgraph.util import taskcluster
 
+        set_taskcluster_root_url()
+
         branch = "try"
         if use_existing_tasks == "last_try_push":
             
@@ -376,6 +379,8 @@ class ExistingTasks(ParameterConfig):
             return
 
         from taskgraph.util import taskcluster
+
+        set_taskcluster_root_url()
 
         if use_existing_tasks.startswith("task-id="):
             tid = use_existing_tasks[len("task-id=") :]
