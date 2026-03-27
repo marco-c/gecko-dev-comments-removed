310233eb34a37ccae0d69049eaad851bd3d80f81
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-03-27 01:38:42 +0000

    Bug 1867385 - Replace Preferences Navbar with reusable Category Navigation component r=hjones,fluent-reviewers,desktop-theme-reviewers,bolsson,flod,sfoster
    Differential Revision: https://phabricator.services.mozilla.com/D283123
    
    UltraBlame original commit: 96a7e76b45cd34175219c51457ea3924cf09a3f5

--- browser/components/preferences/preferences.js
+++ browser/components/preferences/preferences.js
@@ -376,15 +376,8 @@ function init_all() {
   gMainPane.preInit();
 
   let categories = document.getElementById("categories");
-  categories.addEventListener("select", event => gotoPref(event.target.value));
-
-  document.documentElement.addEventListener("keydown", function (event) {
-    if (event.keyCode == KeyEvent.DOM_VK_TAB) {
-      categories.setAttribute("keyboard-navigation", "true");
-    }
-  });
-  categories.addEventListener("mousedown", function () {
-    this.removeAttribute("keyboard-navigation");
+  categories.addEventListener("change-view", event => {
+    gotoPref(event.target.view);
   });
 
   maybeDisplayPoliciesNotice();
@@ -470,12 +463,16 @@ async function gotoPref(
     }
 
     item =  (
-      categories.querySelector(".category[value=" + CSS.escape(category) + "]")
+      categories.querySelector(
+        'moz-page-nav-button[view="' + CSS.escape(category) + '"]'
+      )
     );
     if (!item || item.hidden) {
       unknownCategory = true;
       category = kDefaultCategoryInternalName;
-      item = categories.querySelector(".category[value=" + category + "]");
+      item = categories.querySelector(
+        'moz-page-nav-button[view="' + category + '"]'
+      );
     }
   }
 
@@ -503,13 +500,7 @@ async function gotoPref(
   
   gLastCategory.category = category;
   gLastCategory.subcategory = subcategory;
-  if (item) {
-    
-    categories.selectedItem = item;
-  } else {
-    
-    categories.clearSelection();
-  }
+  categories.currentView = item ? item.getAttribute("view") : category;
   window.history.replaceState(category, document.title);
 
   let categoryInfo = gCategoryInits.get(category);
@@ -749,25 +740,11 @@ function appendSearchKeywords(aId, keywords) {
   element.setAttribute("searchkeywords", keywords.join(" "));
 }
 
-async function ensureScrollPadding() {
-  let stickyContainer = document.querySelector(".sticky-container");
-  let height = await window.browsingContext.topChromeWindow
-    .promiseDocumentFlushed(() => stickyContainer.clientHeight)
-    .catch(console.error); 
-
-  
-  
-  
-  
-  height += 8;
-  stickyContainer
-    .closest(".main-content")
-    .style.setProperty("scroll-padding-top", height + "px");
-}
-
 function maybeDisplayPoliciesNotice() {
   if (Services.policies.status == Services.policies.ACTIVE) {
     document.getElementById("policies-container").removeAttribute("hidden");
+    document
+      .getElementById("policies-container-content")
+      .removeAttribute("hidden");
   }
-  ensureScrollPadding();
 }
