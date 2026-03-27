b68405a9444e9844e4d245545e1eeaa2fe272f36
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-03-27 01:36:19 +0000

    No Bug, mozilla-central repo-update remote-settings ct-logs - r=release-managers,RyanVM
    Differential Revision: https://phabricator.services.mozilla.com/D289653
    
    UltraBlame original commit: f196ea6e16b5f2abf856e8e30305105939b67b2a

--- security/ct/CTKnownLogs.h
+++ security/ct/CTKnownLogs.h
@@ -14,7 +14,7 @@
 
 #include <stddef.h>
 
-static const PRTime kCTExpirationTime = INT64_C(1780258054000000);
+static const PRTime kCTExpirationTime = INT64_C(1780430588000000);
 
 namespace mozilla::ct {
 
