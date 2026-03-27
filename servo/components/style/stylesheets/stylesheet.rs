e5a2e2b098330e0709a73ee6e49a260350a06f59
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-03-27 01:54:19 +0000

    Bug 2025740 - Require all nested rules to be acceptable for sanitization. r=firefox-style-system-reviewers,dshin
    This is not ideal, but should do for now.
    
    Ideally we'd sanitize child rules individually or so, but doing so
    breaks how we sanitize and requires doing significant surgery on the
    original input, or re-serialize (which is also not wanted,
    see bug 1602843).
    
    Differential Revision: https://phabricator.services.mozilla.com/D289533
    
    UltraBlame original commit: d74794bdcbfe64723c079d536d95d298cf1f3557

--- servo/components/style/stylesheets/stylesheet.rs
+++ servo/components/style/stylesheets/stylesheet.rs
@@ -339,7 +339,19 @@ pub enum AllowImportRules {
 }
 
 impl SanitizationKind {
-    fn allows(self, rule: &CssRule) -> bool {
+    fn allows(self, rule: &CssRule, guard: &SharedRwLockReadGuard) -> bool {
+        if !self.allows_self(rule) {
+            return false;
+        }
+        for child in rule.children(guard) {
+            if !self.allows(child, guard) {
+                return false;
+            }
+        }
+        true
+    }
+
+    fn allows_self(self, rule: &CssRule) -> bool {
         debug_assert_ne!(self, SanitizationKind::None);
         
         
@@ -457,7 +469,7 @@ impl Stylesheet {
                         
                         if let Some(ref mut data) = sanitization_data {
                             if let Some(ref rule) = iter.parser.rules.last() {
-                                if !data.kind.allows(rule) {
+                                if !data.kind.allows(rule, &shared_lock.read()) {
                                     iter.parser.rules.pop();
                                     continue;
                                 }
