9ae109dfb4c65ad740d7e75981cce70ea557de31
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-03-27 01:00:23 +0000

    Bug 2017250 - Enable reification for TimingFunction; r=emilio,firefox-style-system-reviewers
    Enable reification for TimingFunction, allowing identifiers to reify into
    CSSKeywordValue for the following properties:
    - animation-timing-function
    - transition-timing-function
    
    Differential Revision: https://phabricator.services.mozilla.com/D286922
    
    UltraBlame original commit: 42131ed5e398ea819dc3cc685879b4dcde1be83d

--- servo/components/style/values/generics/easing.rs
+++ servo/components/style/values/generics/easing.rs
@@ -23,12 +23,14 @@ use crate::parser::ParserContext;
 )]
 #[value_info(ty = "TIMING_FUNCTION")]
 #[repr(u8, C)]
+#[typed_value(derive_fields)]
 pub enum TimingFunction<Integer, Number, LinearStops> {
     
     Keyword(TimingKeyword),
     
     #[allow(missing_docs)]
     #[css(comma, function)]
+    #[typed_value(skip)]
     CubicBezier {
         x1: Number,
         y1: Number,
@@ -38,12 +40,14 @@ pub enum TimingFunction<Integer, Number, LinearStops> {
     
     
     #[css(comma, function)]
+    #[typed_value(skip)]
     #[value_info(other_values = "step-start,step-end")]
     Steps(Integer, #[css(skip_if = "is_end")] StepPosition),
     
     
     
     #[css(function = "linear")]
+    #[typed_value(skip)]
     LinearFunction(LinearStops),
 }
 
@@ -61,6 +65,7 @@ pub enum TimingFunction<Integer, Number, LinearStops> {
     ToCss,
     ToResolvedValue,
     ToShmem,
+    ToTyped,
     Serialize,
     Deserialize,
 )]
