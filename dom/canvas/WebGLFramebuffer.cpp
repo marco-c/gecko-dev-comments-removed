1f1816a0c4f7bd40767f5f4af43dfe9543608a21
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-03-30 17:49:43 +0000

    Bug 2027005 - Make WebGLFramebuffer::CompletenessInfo move-friendly r=lsalzman
    Do not require manual book-keeping of the fb field by using proper
    encapsulation.
    
    Note that all the fields are still public and in RW mode, but at least
    the move-compatibility is automatically handled now.
    
    Differential Revision: https://phabricator.services.mozilla.com/D290491
    
    UltraBlame original commit: a4618a8a79844f7b10404bc286fcaac0e9ac7823

--- dom/canvas/WebGLFramebuffer.cpp
+++ dom/canvas/WebGLFramebuffer.cpp
@@ -1060,7 +1060,6 @@ FBStatus WebGLFramebuffer::CheckFramebufferStatus() const {
     }
     MOZ_ASSERT(info.width && info.height);
     mCompletenessInfo = std::move(info);
-    info.fb = nullptr;  
     return LOCAL_GL_FRAMEBUFFER_COMPLETE;
   } while (false);
 
