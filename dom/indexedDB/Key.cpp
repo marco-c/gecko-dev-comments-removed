7d5795275de2a4484cea93f1e348ad94ad5884f5
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-04-01 01:03:10 +0000

    Bug 2024440 - Reject Shared Array Buffer or Object, r=jari,dom-storage-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D289303
    
    UltraBlame original commit: 9c8ddd6cd8eb172632b3efd0a969c48b20a62c44

--- dom/indexedDB/Key.cpp
+++ dom/indexedDB/Key.cpp
@@ -921,6 +921,12 @@ Result<Ok, nsresult> Key::EncodeBinary(
     return Err(NS_ERROR_DOM_INDEXEDDB_DATA_ERR);
   }
 
+  JSObject* obj = aArrayBufferOrView.asObjectUnbarriered();
+  if (JS::IsArrayBufferViewShared(obj) ||
+      (JS::IsArrayBufferObject(obj) && JS::IsSharedArrayBufferObject(obj))) {
+    return Err(NS_ERROR_DOM_INDEXEDDB_DATA_ERR);
+  }
+
   
   
   
