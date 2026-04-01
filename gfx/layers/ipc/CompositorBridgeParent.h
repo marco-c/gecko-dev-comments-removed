aa736213e339287999b27e954d7afc843ebb744b
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-04-01 01:07:53 +0000

    Revert "Bug 2023443: Ensure ownership of external images and pipelines in various WR actors. r=gfx-reviewers,nical" for causing multiple failures
    This reverts commit 0517b85e48a400cb15fb92b1abaf0a94d58c2bff.
    
    UltraBlame original commit: 26912f25a81b289e56d67b3ff7439fadd81877eb

--- gfx/layers/ipc/CompositorBridgeParent.h
+++ gfx/layers/ipc/CompositorBridgeParent.h
@@ -153,8 +153,6 @@ class CompositorBridgeParentBase : public PCompositorBridgeParent,
   
   virtual void EnsureWebRenderBridgeParentInitialized() = 0;
 
-  bool OwnsExternalImageId(const wr::ExternalImageId& aId) const;
-
  protected:
   virtual ~CompositorBridgeParentBase();
 
