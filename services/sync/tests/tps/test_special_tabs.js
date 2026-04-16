9a9467140e289b967a3b899a8c352ab2f89fab79
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-04-16 04:59:41 +0000

    Bug 1993344: Fix some TPS tests r=markh,credential-management-reviewers,sync-reviewers,dimi
    Differential Revision: https://phabricator.services.mozilla.com/D292936
    
    UltraBlame original commit: 32ef4c3123e22f06ffc4f795504b4954e9d5d572

--- services/sync/tests/tps/test_special_tabs.js
+++ services/sync/tests/tps/test_special_tabs.js
@@ -13,11 +13,7 @@ EnableEngines(["tabs"]);
 var phases = { phase1: "profile1", phase2: "profile2" };
 
 var tabs1 = [
-  {
-    uri: "data:text/html,<html><head><title>Firefox</title></head><body>Firefox</body></html>",
-    title: "Firefox",
-    profile: "profile1",
-  },
+  { uri: "https://example.com/", title: "Example Domain", profile: "profile1" },
   { uri: "about:robots", title: "About", profile: "profile1" },
   { uri: "about:credits", title: "Credits", profile: "profile1" },
   {
@@ -25,30 +21,18 @@ var tabs1 = [
     title: "Mozilla",
     profile: "profile1",
   },
-  {
-    uri: "http://www.mozilla.com/en-US/firefox/sync/firstrun.html",
-    title: "Firstrun",
-    profile: "profile1",
-  },
+  { uri: "https://example.org/", title: "Example Domain", profile: "profile1" },
 ];
 
 var tabs2 = [
-  {
-    uri: "data:text/html,<html><head><title>Firefox</title></head><body>Firefox</body></html>",
-    title: "Firefox",
-    profile: "profile1",
-  },
-  {
-    uri: "data:text/html,<html><head><title>Mozilla</title></head><body>Mozilla</body></html>",
-    title: "Mozilla",
-    profile: "profile1",
-  },
+  { uri: "https://example.com/", title: "Example Domain", profile: "profile1" },
+  { uri: "https://example.org/", title: "Example Domain", profile: "profile1" },
 ];
 
 var tabs3 = [
   {
-    uri: "http://www.mozilla.com/en-US/firefox/sync/firstrun.html",
-    title: "Firstrun",
+    uri: "data:text/html,<html><head><title>Mozilla</title></head><body>Mozilla</body></html>",
+    title: "Mozilla",
     profile: "profile1",
   },
   { uri: "about:robots", title: "About", profile: "profile1" },
