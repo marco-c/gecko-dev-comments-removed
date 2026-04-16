5928b077e064e6e33a336ea9727209a49e8abc5c
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-04-16 03:37:51 +0000

    Bug 1976184 - pt 8. Use a RB tree for the oversize free list r=glandium
    Differential Revision: https://phabricator.services.mozilla.com/D268990
    
    UltraBlame original commit: 2b7f929c7337a87f7141f318ffa031184b5eddf1

--- memory/build/BaseAlloc.cpp
+++ memory/build/BaseAlloc.cpp
@@ -48,7 +48,7 @@ void BaseAlloc::free(void* aPtr) MOZ_EXCLUDES(mMutex) {
   if (index < NUM_LIST_SIZES) {
     mFreeLists[index].pushFront(cell);
   } else {
-    mFreeListOversize.pushFront(cell);
+    mFreeListOversize.Insert(cell);
   }
 }
 
@@ -85,13 +85,14 @@ void* BaseAlloc::alloc_from_list(base_alloc_size_t aSize) {
   }
 
   
-  for (auto& cell : mFreeListOversize) {
-    if (cell.Size() >= aSize) {
-      mFreeListOversize.remove(&cell);
-      cell.SetAllocated();
+  BaseAllocCell* cell = mFreeListOversize.SearchOrNext(aSize);
+  if (cell) {
+    MOZ_ASSERT(cell->Size() >= aSize);
+    
+    mFreeListOversize.Remove(cell);
+    cell->SetAllocated();
 
-      return cell.Ptr();
-    }
+    return cell->Ptr();
   }
 
   return nullptr;
