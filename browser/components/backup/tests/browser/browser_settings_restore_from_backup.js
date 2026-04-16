12603d37d447a3e530d0f15769114d7d486a569d
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-04-16 04:45:18 +0000

    Bug 2022413 - create backups with PROFILE_PATH to avoid picking the desktop.ini during testing. r=kpatenio
    Differential Revision: https://phabricator.services.mozilla.com/D293949
    
    UltraBlame original commit: 06a8c791287856d4ee06ddfa0934cd5b581e658d

--- browser/components/backup/tests/browser/browser_settings_restore_from_backup.js
+++ browser/components/backup/tests/browser/browser_settings_restore_from_backup.js
@@ -439,8 +439,9 @@ add_task(async function test_restore_from_backup_prefills_prior_valid_backup() {
   await SpecialPowers.pushPrefEnv({
     set: [["browser.backup.location", dir]],
   });
-  await BackupService.get().createBackup({ profilePath: TEST_PROFILE_PATH });
-  let path = (await IOUtils.getChildren(dir))[0];
+  let { archivePath: path } = await BackupService.get().createBackup({
+    profilePath: TEST_PROFILE_PATH,
+  });
   await SpecialPowers.popPrefEnv();
 
   await BrowserTestUtils.withNewTab("about:preferences#sync", async browser => {
