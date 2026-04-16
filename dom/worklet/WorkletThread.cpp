01a4f195e2428a7c3ec3640e5ac424aa578af0c9
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-04-16 04:44:10 +0000

    Bug 2031123 - Block all eval attempts in Worklets when enforcing trusted-types. r=smaug
    Differential Revision: https://phabricator.services.mozilla.com/D293830
    
    UltraBlame original commit: d27ed7a44dd9d7b4e18db2b2d34a46010c4b0251

--- dom/worklet/WorkletThread.cpp
+++ dom/worklet/WorkletThread.cpp
@@ -388,7 +388,14 @@ bool ContentSecurityPolicyAllows(
   bool reportViolation = false;
   if (OffThreadCSPContext* ctx = impl->GetCSPContext()) {
     if (aKind == JS::RuntimeCode::JS) {
-      *aOutCanCompileStrings = ctx->IsEvalAllowed(reportViolation);
+      if (ctx->CSPInfo().requireTrustedTypesForDirectiveState() ==
+          RequireTrustedTypesForDirectiveState::ENFORCE) {
+        
+        
+        *aOutCanCompileStrings = false;
+      } else {
+        *aOutCanCompileStrings = ctx->IsEvalAllowed(reportViolation);
+      }
     } else {
       *aOutCanCompileStrings = ctx->IsWasmEvalAllowed(reportViolation);
     }
