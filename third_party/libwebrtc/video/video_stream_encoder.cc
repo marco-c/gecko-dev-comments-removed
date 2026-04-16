a7cbe2a1a5823ebc8b2a2ed9e49602b817b7779b
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-04-16 04:09:07 +0000

    Bug 2030139 - Vendor libwebrtc from 15fd636394
    Upstream commit: https://webrtc.googlesource.com/src/+/15fd6363943a47bd9bf16df856113e98959f55df
        [tracing] Migrate legacy async macros in webrtc
    
        Migrating the following async event macros somewhat mechanically:
        ```
        - TRACE_EVENT(_NESTABLE)?_ASYNC_BEGIN(_WITH_TIMESTAMP)?[012](<category_group>, <name>, <id>, ...)
        -> TRACE_EVENT_BEGIN(<category_group>, <name>, <track>, ...)
    
        - TRACE_EVENT(_NESTABLE)?_ASYNC_END(_WITH_TIMESTAMP)?[012](<category_group>, <name>, <id>, ...)
        -> TRACE_EVENT_END(<category_group>, <track>, ...)
    
        - TRACE_EVENT(_NESTABLE)?_ASYNC_INSTANT(_WITH_TIMESTAMP)?[012](<category_group>, <name>, <id>, ...)
        -> TRACE_EVENT_INSTANT(<category_group>, <name>, <track>, ...)
        ```
    
        Bug: chromium:432427382
        Change-Id: I5d25d8dfd19613bcafa98c8d7a81a4050cd6534c
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/435580
        Reviewed-by: Åsa Persson <asaperssonwebrtc.org>
        Commit-Queue: Etienne Pierre-Doray <etiennepchromium.org>
        Reviewed-by: Henrik Andreassson <henrikawebrtc.org>
        Cr-Commit-Position: refs/heads/main{#46896}
    
    UltraBlame original commit: 13876d1bb361ac3ea7038f3b54765672b114e0fe

--- third_party/libwebrtc/video/video_stream_encoder.cc
+++ third_party/libwebrtc/video/video_stream_encoder.cc
@@ -868,11 +868,13 @@ void VideoStreamEncoder::AddAdaptationResource(
   
   
   
-  TRACE_EVENT_ASYNC_BEGIN0(
-      "webrtc", "VideoStreamEncoder::AddAdaptationResource(latency)", this);
+  TRACE_EVENT_BEGIN("webrtc",
+                    "VideoStreamEncoder::AddAdaptationResource(latency)",
+                    perfetto::Track::FromPointer(this));
   encoder_queue_->PostTask([this, resource = std::move(resource)] {
-    TRACE_EVENT_ASYNC_END0(
-        "webrtc", "VideoStreamEncoder::AddAdaptationResource(latency)", this);
+    TRACE_EVENT_END("webrtc",
+                    
+                    perfetto::Track::FromPointer(this));
     RTC_DCHECK_RUN_ON(encoder_queue_.get());
     additional_resources_.push_back(resource);
     stream_resource_manager_.AddResource(resource, VideoAdaptationReason::kCpu);
@@ -1730,7 +1732,8 @@ void VideoStreamEncoder::TraceFrameDropStart() {
   RTC_DCHECK_RUN_ON(encoder_queue_.get());
   
   if (!encoder_paused_and_dropped_frame_) {
-    TRACE_EVENT_ASYNC_BEGIN0("webrtc", "EncoderPaused", this);
+    TRACE_EVENT_BEGIN("webrtc", "EncoderPaused",
+                      perfetto::Track::FromPointer(this));
   }
   encoder_paused_and_dropped_frame_ = true;
 }
@@ -1739,7 +1742,8 @@ void VideoStreamEncoder::TraceFrameDropEnd() {
   RTC_DCHECK_RUN_ON(encoder_queue_.get());
   
   if (encoder_paused_and_dropped_frame_) {
-    TRACE_EVENT_ASYNC_END0("webrtc", "EncoderPaused", this);
+    TRACE_EVENT_END("webrtc", 
+                    perfetto::Track::FromPointer(this));
   }
   encoder_paused_and_dropped_frame_ = false;
 }
@@ -2198,8 +2202,8 @@ void VideoStreamEncoder::EncodeVideoFrame(const VideoFrame& video_frame,
   }
   accumulated_update_rect_is_valid_ = true;
 
-  TRACE_EVENT_ASYNC_STEP_INTO0("webrtc", "Video", video_frame.render_time_ms(),
-                               "Encode");
+  TRACE_EVENT_INSTANT("webrtc", "Encode", perfetto::Track::FromPointer(this),
+                      "render_time", video_frame.render_time_ms());
 
   stream_resource_manager_.OnEncodeStarted(out_frame, time_when_posted_us);
 
