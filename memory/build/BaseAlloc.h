95fb15c20be1cddea12067ecdf1100f40c11dfed
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-04-16 03:37:46 +0000

    Bug 1976184 - pt 7. Move some BaseAlloc structures to a new header r=glandium
    The next patch will need to access some of these structures from
    BaseAlloc.h, rather than adding them there and making the interface less
    obvious, create a new header.
    
    Differential Revision: https://phabricator.services.mozilla.com/D268989
    
    UltraBlame original commit: 22ce3787a170d800ffd5b118e03def5104dfd7d0

--- memory/build/BaseAlloc.h
+++ memory/build/BaseAlloc.h
@@ -12,11 +12,7 @@
 #include "mozilla/DoublyLinkedList.h"
 #include "mozilla/fallible.h"
 
-
-typedef uint32_t base_alloc_size_t;
-constexpr static base_alloc_size_t BASE_ALLOC_SIZE_MAX = UINT32_MAX >> 1;
-
-class BaseAllocCell;
+#include "BaseAllocInternals.h"
 
 
 
