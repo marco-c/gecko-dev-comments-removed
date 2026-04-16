5a4ac030f1b28aeeafcef1d45b5b007170cc0641
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-04-16 04:31:19 +0000

    Revert "Bug 2028765 - Remove legacy uptake Telemetry r=chutten,acottner" for causing xpcshell failures in test_uptake_telemetry.js.
    This reverts commit c291c28f680e5a4af0f6bebf7eb4e11970fa13e4.
    
    UltraBlame original commit: 31f0c01809388e59b1503bafc0ed091582467b2b

--- services/common/tests/unit/test_uptake_telemetry.js
+++ services/common/tests/unit/test_uptake_telemetry.js
@@ -2,17 +2,18 @@ const { TelemetryTestUtils } = ChromeUtils.importESModule(
   "resource://testing-common/TelemetryTestUtils.sys.mjs"
 );
 const { UptakeTelemetry } = ChromeUtils.importESModule(
-  "resource://services-settings/UptakeTelemetry.sys.mjs"
+  "resource://services-common/uptake-telemetry.sys.mjs"
 );
 
 const COMPONENT = "remotesettings";
+const GLEAN_COMPONENT = "Remotesettings";
 
 add_task(async function test_unknown_status_is_not_reported() {
   const source = "update-source";
   const startSnapshot = getUptakeTelemetrySnapshot(COMPONENT, source);
 
   try {
-    await UptakeTelemetry.report("unknown-status", { source });
+    await UptakeTelemetry.report(GLEAN_COMPONENT, "unknown-status", { source });
   } catch (e) {}
 
   const endSnapshot = getUptakeTelemetrySnapshot(COMPONENT, source);
@@ -23,7 +24,7 @@ add_task(async function test_age_is_converted_to_string_and_reported() {
   const status = UptakeTelemetry.STATUS.SUCCESS;
   const age = 42;
 
-  await UptakeTelemetry.report(status, { source: "s", age });
+  await UptakeTelemetry.report(GLEAN_COMPONENT, status, { source: "s", age });
 
   TelemetryTestUtils.assertEvents([
     [
@@ -43,7 +44,7 @@ add_task(async function test_each_status_can_be_caught_in_snapshot() {
   const expectedIncrements = {};
   for (const status of Object.values(UptakeTelemetry.STATUS)) {
     expectedIncrements[status] = 1;
-    await UptakeTelemetry.report(status, { source });
+    await UptakeTelemetry.report(GLEAN_COMPONENT, status, { source });
   }
 
   const endSnapshot = getUptakeTelemetrySnapshot(COMPONENT, source);
