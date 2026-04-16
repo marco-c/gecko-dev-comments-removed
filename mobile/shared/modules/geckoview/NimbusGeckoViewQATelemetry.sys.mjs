eb477235815b6807a64971b36173929e9da08615
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-04-16 04:54:48 +0000

    Bug 2031778 - Report user and default branch values of Nimbus QA prefs in geckoview r=geckoview-reviewers,ohall
    Differential Revision: https://phabricator.services.mozilla.com/D294245
    
    UltraBlame original commit: 412f1305ccb81fa31980569b3283e4da195e9ed8

--- mobile/shared/modules/geckoview/NimbusGeckoViewQATelemetry.sys.mjs
+++ mobile/shared/modules/geckoview/NimbusGeckoViewQATelemetry.sys.mjs
@@ -2,24 +2,67 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+function getPrefValue(branch, pref, type) {
+  switch (type) {
+    case Ci.nsIPrefBranch.PREF_STRING:
+      return branch.getStringPref(pref);
+
+    case Ci.nsIPrefBranch.PREF_INT:
+      return branch.getIntPref(pref);
+
+    case Ci.nsIPrefBranch.PREF_BOOL:
+      return branch.getBoolPref(pref);
+
+    default:
+      // Unreachable.
+      return undefined;
+  }
+}
+
+const Branches = Object.freeze({
+  USER: Services.prefs,
+  DEFAULT: Services.prefs.getDefaultBranch(null),
+});
+
+function getPrefValues(pref, type) {
+  const rv = {};
+
+  if (Services.prefs.prefHasUserValue(pref)) {
+    rv.user_branch_value = getPrefValue(Branches.USER, pref, type);
+  }
+
+  if (Services.prefs.prefHasDefaultValue(pref)) {
+    rv.default_branch_value = getPrefValue(Branches.DEFAULT, pref, type);
+  }
+
+  return rv;
+}
+
 const PREF_MAP = Object.freeze({
-  "nimbus.qa.pref-string-default": [
-    "stringDefaultValue",
-    Services.prefs.getStringPref,
-  ],
-  "nimbus.qa.pref-string-user": [
-    "stringUserValue",
-    Services.prefs.getStringPref,
-  ],
-
-  "nimbus.qa.pref-int-default": ["intDefaultValue", Services.prefs.getIntPref],
-  "nimbus.qa.pref-int-user": ["intUserValue", Services.prefs.getIntPref],
-
-  "nimbus.qa.pref-bool-default": [
-    "boolDefaultValue",
-    Services.prefs.getBoolPref,
-  ],
-  "nimbus.qa.pref-bool-user": ["boolUserValue", Services.prefs.getBoolPref],
+  "nimbus.qa.pref-string-default": {
+    metric: "stringDefault",
+    type: Ci.nsIPrefBranch.PREF_STRING,
+  },
+  "nimbus.qa.pref-string-user": {
+    metric: "stringUser",
+    type: Ci.nsIPrefBranch.PREF_STRING,
+  },
+  "nimbus.qa.pref-int-default": {
+    metric: "intDefault",
+    type: Ci.nsIPrefBranch.PREF_INT,
+  },
+  "nimbus.qa.pref-int-user": {
+    metric: "intUser",
+    type: Ci.nsIPrefBranch.PREF_INT,
+  },
+  "nimbus.qa.pref-bool-default": {
+    metric: "boolDefault",
+    type: Ci.nsIPrefBranch.PREF_BOOL,
+  },
+  "nimbus.qa.pref-bool-user": {
+    metric: "boolUser",
+    type: Ci.nsIPrefBranch.PREF_BOOL,
+  },
 });
 
 export const NimbusGeckoViewQATelemetry = new (class {
@@ -42,10 +85,10 @@ export const NimbusGeckoViewQATelemetry = new (class {
   }
 
   #recordPref(pref) {
-    const [metric, getter] = PREF_MAP[pref];
+    const { metric, type } = PREF_MAP[pref];
 
     try {
-      const value = getter(pref);
+      const value = getPrefValues(pref, type);
 
       Glean.nimbusQaPrefs[metric].set(value);
     } catch (e) {
