af4cf39cc4a322197535147e1cd71e2c6f374ef2
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-04-16 01:54:57 +0000

    Bug 2028728 - remove extraneous psm::SyncRunnableBase implementation r=jschanck
    Differential Revision: https://phabricator.services.mozilla.com/D291873
    
    UltraBlame original commit: 2397db4463ff182cdb9c2896b341695fdb362569

--- security/manager/ssl/nsNSSIOLayer.cpp
+++ security/manager/ssl/nsNSSIOLayer.cpp
@@ -12,7 +12,6 @@
 #include "NSSCertDBTrustDomain.h"
 #include "NSSErrorsService.h"
 #include "NSSSocketControl.h"
-#include "PSMRunnable.h"
 #include "SSLServerCertVerification.h"
 #include "ScopedNSSTypes.h"
 #include "TLSClientAuthCertSelection.h"
@@ -437,19 +436,6 @@ bool retryDueToTLSIntolerance(PRErrorCode err, NSSSocketControl* socketInfo) {
 
   if (StaticPrefs::security_tls_ech_disable_grease_on_fallback() &&
       socketInfo->GetEchExtensionStatus() == EchExtensionStatus::kGREASE) {
-    
-    return true;
-  }
-
-  if (!socketInfo->IsPreliminaryHandshakeDone() &&
-      !socketInfo->HasTls13HandshakeSecrets() && socketInfo->SentMlkemShare()) {
-    nsAutoCString errorName;
-    const char* prErrorName = PR_ErrorToName(err);
-    if (prErrorName) {
-      errorName.AppendASCII(prErrorName);
-    }
-    mozilla::glean::tls::xyber_intolerance_reason.Get(errorName).Add(1);
-    
     
     return true;
   }
@@ -1278,9 +1264,6 @@ static PRFileDesc* nsSSLIOLayerImportFD(PRFileDesc* fd,
       SECSuccess) {
     return nullptr;
   }
-  if (SSL_SecretCallback(sslSock, SecretCallback, infoObject) != SECSuccess) {
-    return nullptr;
-  }
   if (SSL_SetCanFalseStartCallback(sslSock, CanFalseStartCallback,
                                    infoObject) != SECSuccess) {
     return nullptr;
@@ -1615,9 +1598,7 @@ static nsresult nsSSLIOLayerSetOptions(PRFileDesc* fd, bool forSTARTTLS,
   unsigned int additional_shares =
       StaticPrefs::security_tls_client_hello_send_p256_keyshare();
   if (StaticPrefs::security_tls_enable_kyber() &&
-      range.max >= SSL_LIBRARY_VERSION_TLS_1_3 &&
-      !(infoObject->GetProviderFlags() &
-        (nsISocketProvider::BE_CONSERVATIVE | nsISocketProvider::IS_RETRY))) {
+      range.max >= SSL_LIBRARY_VERSION_TLS_1_3) {
     const SSLNamedGroup namedGroups[] = {
         ssl_grp_kem_mlkem768x25519, ssl_grp_ec_curve25519, ssl_grp_ec_secp256r1,
         ssl_grp_ec_secp384r1,       ssl_grp_ec_secp521r1,  ssl_grp_ffdhe_2048,
@@ -1627,7 +1608,6 @@ static nsresult nsSSLIOLayerSetOptions(PRFileDesc* fd, bool forSTARTTLS,
       return NS_ERROR_FAILURE;
     }
     additional_shares += 1;
-    infoObject->WillSendMlkemShare();
   } else {
     const SSLNamedGroup namedGroups[] = {
         ssl_grp_ec_curve25519, ssl_grp_ec_secp256r1, ssl_grp_ec_secp384r1,
