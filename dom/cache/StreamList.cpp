36cf704d451b27a3dff71c28f588a33dc5bfd575
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-04-16 02:17:45 +0000

    Revert "Bug 2025349: Add a deathgrip to StreamList r=dom-storage-reviewers,dom-worker-reviewers,hsingh,asuth" for casuing failures at nsTArray_base.
    This reverts commit 26ac86c3fbac131f6da742cee1f709e06f92ba2a.
    
    UltraBlame original commit: 1d94f92f5d4f73948135672f56eef36a945aa612

--- dom/cache/StreamList.cpp
+++ dom/cache/StreamList.cpp
@@ -135,7 +135,6 @@ void StreamList::NoteClosedAll() {
 
 void StreamList::CloseAll() {
   NS_ASSERT_OWNINGTHREAD(StreamList);
-  SafeRefPtr<StreamList> kungFuDeathGrip = SafeRefPtrFromThis();
 
   if (mStreamControl && mStreamControl->CanSend()) {
     
