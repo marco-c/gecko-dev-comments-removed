60f87d1c6001490f13e9cd68a85d723268916fc6
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-04-16 02:51:28 +0000

    Bug 2019299 - Decouple launching the crash helper from setting the main process' exception handler r=afranchuk
    This allows us to unset the main process exception handler without
    necessarily shutting down the crash helper. Going forward we'll handle
    this directly in the crash helper as on child process we don't have a
    way to unset exception handlers without running afoul of the sandbox.
    
    Differential Revision: https://phabricator.services.mozilla.com/D291484
    
    UltraBlame original commit: 6dadf4100a3b6e944466d89f4870692f47606928

--- js/xpconnect/src/XPCShellImpl.cpp
+++ js/xpconnect/src/XPCShellImpl.cpp
@@ -1177,6 +1177,11 @@ int XRE_XPCShellMain(int argc, char** argv, char** envp,
           aShellData->crashHelperSocket);
 #endif  
 
+      rv = CrashReporter::OOPInit(greBinDir);
+      if (NS_FAILED(rv)) {
+        printf("CrashReporter::OOPInit(): could not launch the crash helper\n");
+      }
+
       rv = CrashReporter::SetExceptionHandler(greBinDir, true);
       if (NS_FAILED(rv)) {
         printf("CrashReporter::SetExceptionHandler failed!\n");
@@ -1408,6 +1413,7 @@ int XRE_XPCShellMain(int argc, char** argv, char** envp,
   
   if (CrashReporter::GetEnabled()) {
     CrashReporter::UnsetExceptionHandler();
+    CrashReporter::OOPDeinit();
   }
 
   
