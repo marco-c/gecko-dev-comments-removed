e3066a96e39a45efb4021717d2175207e58aa072
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-04-16 03:33:42 +0000

    Bug 2006953 - Make memory reporter timeout a pref. r=mccr8
    I've occasionally run into this timeout for processes that have a lot of
    memory. It'd be nice to be able to up the timeout in those situations.
    
    Differential Revision: https://phabricator.services.mozilla.com/D277037
    
    UltraBlame original commit: 243546ca388fb62d389ccc3981ceb7466eb5f7d5

--- xpcom/base/nsMemoryReporterManager.h
+++ xpcom/base/nsMemoryReporterManager.h
@@ -229,9 +229,6 @@ class nsMemoryReporterManager final : public nsIMemoryReporterManager,
                         nsISupports* aHandleReportData, bool aAnonymize);
 
   static void TimeoutCallback(nsITimer* aTimer, void* aData);
-  
-  
-  static const uint32_t kTimeoutLengthMS = 180000;
 
   mozilla::Mutex mMutex;
   bool mIsRegistrationBlocked MOZ_GUARDED_BY(mMutex);
