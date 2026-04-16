0d076b074abd59a2866c2027ccb55db5514a4d88
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-04-16 01:23:36 +0000

    Bug 2028551 r=padenot
    Differential Revision: https://phabricator.services.mozilla.com/D291535
    
    UltraBlame original commit: c6dc8c02e63fa5f8aac68453288009f80d517af0

--- dom/media/platforms/ffmpeg/FFmpegAudioDecoder.cpp
+++ dom/media/platforms/ffmpeg/FFmpegAudioDecoder.cpp
@@ -2,8 +2,6 @@
 
 
 
-
-
 #include "FFmpegAudioDecoder.h"
 
 #include "AudioSampleFormat.h"
@@ -194,7 +192,8 @@ static AlignedAudioBuffer CopyAndPackAudio(AVFrame* aFrame,
   } else if (aFrame->format == AV_SAMPLE_FMT_FLTP) {
     
     AudioDataValue* tmp = audio.get();
-    AudioDataValue** data = reinterpret_cast<AudioDataValue**>(aFrame->data);
+    AudioDataValue** data =
+        reinterpret_cast<AudioDataValue**>(aFrame->extended_data);
     for (uint32_t frame = 0; frame < aNumAFrames; frame++) {
       for (uint32_t channel = 0; channel < aNumChannels; channel++) {
         *tmp++ = data[channel][frame];
@@ -213,7 +212,7 @@ static AlignedAudioBuffer CopyAndPackAudio(AVFrame* aFrame,
     
     
     AudioDataValue* tmp = audio.get();
-    int16_t** data = reinterpret_cast<int16_t**>(aFrame->data);
+    int16_t** data = reinterpret_cast<int16_t**>(aFrame->extended_data);
     for (uint32_t frame = 0; frame < aNumAFrames; frame++) {
       for (uint32_t channel = 0; channel < aNumChannels; channel++) {
         *tmp++ = ConvertAudioSample<float>(data[channel][frame]);
@@ -232,7 +231,7 @@ static AlignedAudioBuffer CopyAndPackAudio(AVFrame* aFrame,
     
     
     AudioDataValue* tmp = audio.get();
-    int32_t** data = reinterpret_cast<int32_t**>(aFrame->data);
+    int32_t** data = reinterpret_cast<int32_t**>(aFrame->extended_data);
     for (uint32_t frame = 0; frame < aNumAFrames; frame++) {
       for (uint32_t channel = 0; channel < aNumChannels; channel++) {
         *tmp++ = ConvertAudioSample<float>(data[channel][frame]);
@@ -251,7 +250,7 @@ static AlignedAudioBuffer CopyAndPackAudio(AVFrame* aFrame,
     
     
     AudioDataValue* tmp = audio.get();
-    uint8_t** data = reinterpret_cast<uint8_t**>(aFrame->data);
+    uint8_t** data = reinterpret_cast<uint8_t**>(aFrame->extended_data);
     for (uint32_t frame = 0; frame < aNumAFrames; frame++) {
       for (uint32_t channel = 0; channel < aNumChannels; channel++) {
         *tmp++ = ConvertAudioSample<float>(data[channel][frame]);
