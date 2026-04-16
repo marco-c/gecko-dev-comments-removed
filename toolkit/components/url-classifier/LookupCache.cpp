a6436d8a0724133d56c6c773a6d3d6e90a5ef08b
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-04-16 02:43:32 +0000

    Bug 2027331 - Removing unnecessary post-increment in LookupCache. r=dimi
    Differential Revision: https://phabricator.services.mozilla.com/D291502
    
    UltraBlame original commit: 6974c5aca7e9a32825b91c5d87c5758f38e932d1

--- toolkit/components/url-classifier/LookupCache.cpp
+++ toolkit/components/url-classifier/LookupCache.cpp
@@ -504,7 +504,7 @@ void LookupCache::GetLookupEntitylistFragments(
     if (FindCharInReadable('.', iter, end)) {
       iter++;
       nsAutoCString thirdPartyURLToAdd;
-      thirdPartyURLToAdd.Assign(Substring(iter++, end));
+      thirdPartyURLToAdd.Assign(Substring(iter, end));
 
       
       if (FindCharInReadable('.', iter, end)) {
