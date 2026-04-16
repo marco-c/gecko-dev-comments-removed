970c0b70a7437f9e359386e8d9f33bd70313888c
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-04-16 04:47:04 +0000

    Revert "Bug 1906555 - Fix getAttribute when value is set to non-canonical keyword. r=farre" for causing build bustages  HTMLBRElement
    This reverts commit a537cb55dda0bc6d7b1b71b10e9952b81eeb0d89.
    
    UltraBlame original commit: db95400305c2d1c8312551863ffb839c2b4bb3e1

--- dom/html/HTMLBRElement.cpp
+++ dom/html/HTMLBRElement.cpp
@@ -21,13 +21,11 @@ HTMLBRElement::~HTMLBRElement() = default;
 
 NS_IMPL_ELEMENT_CLONE(HTMLBRElement)
 
-enum class ClearKeyword : uint8_t { Left = 1, Right, All, Both };
-
 static constexpr nsAttrValue::EnumTableEntry kClearTable[] = {
-    {"left", ClearKeyword::Left},
-    {"right", ClearKeyword::Right},
-    {"all", ClearKeyword::All},
-    {"both", ClearKeyword::Both},
+    {"left", StyleClear::Left},
+    {"right", StyleClear::Right},
+    {"all", StyleClear::Both},
+    {"both", StyleClear::Both},
 };
 
 bool HTMLBRElement::ParseAttribute(int32_t aNamespaceID, nsAtom* aAttribute,
@@ -42,26 +40,11 @@ bool HTMLBRElement::ParseAttribute(int32_t aNamespaceID, nsAtom* aAttribute,
                                               aMaybeScriptedPrincipal, aResult);
 }
 
-static StyleClear ClearKeywordToStyleClear(ClearKeyword aAttrVal) {
-  switch (aAttrVal) {
-    case ClearKeyword::Left:
-      return StyleClear::Left;
-    case ClearKeyword::Right:
-      return StyleClear::Right;
-    case ClearKeyword::All:
-    case ClearKeyword::Both:
-      
-      return StyleClear::Both;
-  }
-}
-
 void HTMLBRElement::MapAttributesIntoRule(MappedDeclarationsBuilder& aBuilder) {
   if (!aBuilder.PropertyIsSet(eCSSProperty_clear)) {
     const nsAttrValue* value = aBuilder.GetAttr(nsGkAtoms::clear);
     if (value && value->Type() == nsAttrValue::eEnum) {
-      ClearKeyword enumValue = static_cast<ClearKeyword>(value->GetEnumValue());
-      StyleClear styleClear = ClearKeywordToStyleClear(enumValue);
-      aBuilder.SetKeywordValue(eCSSProperty_clear, styleClear);
+      aBuilder.SetKeywordValue(eCSSProperty_clear, value->GetEnumValue());
     }
   }
   nsGenericHTMLElement::MapCommonAttributesInto(aBuilder);
