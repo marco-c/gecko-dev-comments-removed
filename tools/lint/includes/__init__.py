8bb0edf13580a8f95a0fc715798efd9d37a68f5b
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-04-16 03:11:38 +0000

    Bug 2030019 - Fix include linter in presence of std namespace r=sylvestre,emilio
    Differential Revision: https://phabricator.services.mozilla.com/D292696
    
    UltraBlame original commit: bc0803bee0df43e61d5b8a11afb49b69539984a7

--- tools/lint/includes/__init__.py
+++ tools/lint/includes/__init__.py
@@ -99,10 +99,19 @@ def lint_mfbt_headers(results, path, raw_content, config, fix):
 
 
 def lint_std_headers(results, path, raw_content, config, fix):
-    if re.search(r"using\s+namespace\s+std", raw_content):
-        return
-
-    symbol_pattern = r"\bstd::{}\b"
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    if re.search(r"\bnamespace\s+std\b", raw_content):
+        symbol_pattern = r"\b{}\b"
+    else:
+        symbol_pattern = r"\bstd::{}\b"
 
     for header, symbols in std_api.items():
         headerline = rf"#\s*include <{header}>"
