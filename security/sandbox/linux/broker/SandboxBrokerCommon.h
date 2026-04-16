b14984ffa959b5780be6f3e5a919daa0bcd72fd2
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-04-16 03:53:28 +0000

    Bug 2027322 - Modernize sandbox broker operation enum. r=glandium
    Differential Revision: https://phabricator.services.mozilla.com/D291108
    
    UltraBlame original commit: c00931f859fb09ddc8c94e171af205b3aa9c474d

--- security/sandbox/linux/broker/SandboxBrokerCommon.h
+++ security/sandbox/linux/broker/SandboxBrokerCommon.h
@@ -24,7 +24,7 @@ namespace mozilla {
 
 class SandboxBrokerCommon {
  public:
-  enum Operation {
+  enum class Operation : unsigned {
     SANDBOX_FILE_OPEN,
     SANDBOX_FILE_ACCESS,
     SANDBOX_FILE_STAT,
@@ -38,9 +38,17 @@ class SandboxBrokerCommon {
     SANDBOX_FILE_READLINK,
     SANDBOX_SOCKET_CONNECT,
     SANDBOX_SOCKET_CONNECT_ABSTRACT,
+    SANDBOX_OP_MAX_VALUE = SANDBOX_SOCKET_CONNECT_ABSTRACT
   };
-  
-  static const char* OperationDescription[];
+  using enum Operation;
+
+  static bool OperationIsValid(Operation aOp) {
+    return static_cast<unsigned>(aOp) <=
+           static_cast<unsigned>(SANDBOX_OP_MAX_VALUE);
+  }
+
+  static unsigned OperationToInt(Operation);
+  static const char* OperationDescription(Operation);
 
   struct Request {
     Operation mOp;
