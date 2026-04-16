759a0118fbfabe232245ce290fd819ffdb29d961
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-04-16 02:35:44 +0000

    Bug 2029247 - Vendor libwebrtc from 6952fae6c0
    Upstream commit: https://webrtc.googlesource.com/src/+/6952fae6c00303179f5aae59443f519862626b5d
        Reduce use of deprecated GetStats implementation.
    
        Update some tests to use the "new" getstats instead of the deprecated
        method.
    
        Bug: None
        Change-Id: Ic1a09c88e5cb2503e05e34b6fd24bac78abea84e
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/445200
        Commit-Queue: Tomas Gunnarsson <tommiwebrtc.org>
        Reviewed-by: Evan Shrubsole <eshrwebrtc.org>
        Cr-Commit-Position: refs/heads/main{#46823}
    
    UltraBlame original commit: 475a8efe82326135eb22955addf9c1b3ef105be8

--- third_party/libwebrtc/pc/peer_connection_interface_unittest.cc
+++ third_party/libwebrtc/pc/peer_connection_interface_unittest.cc
@@ -1687,7 +1687,7 @@ TEST_P(PeerConnectionInterfaceTest, AddTrackBeforeConnecting) {
       CreateVideoTrack("video_track"));
   auto audio_sender = pc_->AddTrack(audio_track, std::vector<std::string>());
   auto video_sender = pc_->AddTrack(video_track, std::vector<std::string>());
-  EXPECT_TRUE(DoGetStats(nullptr));
+  EXPECT_TRUE(DoGetRTCStats());
 }
 
 TEST_P(PeerConnectionInterfaceTest, AttachmentIdIsSetOnAddTrack) {
@@ -1934,7 +1934,7 @@ TEST_P(PeerConnectionInterfaceTest, GetStatsForSpecificTrack) {
   ASSERT_LT(0u, pc_->GetReceivers().size());
   scoped_refptr<MediaStreamTrackInterface> remote_audio =
       pc_->GetReceivers()[0]->track();
-  EXPECT_TRUE(DoGetStats(remote_audio.get()));
+  EXPECT_TRUE(DoGetRTCStats());
 
   
   
@@ -1944,7 +1944,7 @@ TEST_P(PeerConnectionInterfaceTest, GetStatsForSpecificTrack) {
 
   
   
-  EXPECT_TRUE(DoGetStats(remote_audio.get()));
+  EXPECT_TRUE(DoGetRTCStats());
 }
 
 
@@ -1952,7 +1952,7 @@ TEST_P(PeerConnectionInterfaceTest, GetStatsForVideoTrack) {
   InitiateCall();
   auto video_receiver = GetFirstReceiverOfType(MediaType::VIDEO);
   ASSERT_TRUE(video_receiver);
-  EXPECT_TRUE(DoGetStats(video_receiver->track().get()));
+  EXPECT_TRUE(DoGetRTCStats());
 }
 
 
@@ -2673,7 +2673,7 @@ TEST_F(PeerConnectionInterfaceTestPlanB, CloseAndTestMethods) {
 TEST_P(PeerConnectionInterfaceTest, CloseAndGetStats) {
   InitiateCall();
   pc_->Close();
-  DoGetStats(nullptr);
+  DoGetRTCStats();
 }
 
 
