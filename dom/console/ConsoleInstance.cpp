6922180ca4322a0dcce1376209e078c3f2b5e42d
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-04-16 03:59:47 +0000

    Bug 2031122 - Extra rooting for Console r=mccr8
    Differential Revision: https://phabricator.services.mozilla.com/D293715
    
    UltraBlame original commit: 8825567c2c901336afc3a0318726ec541e1208b6

--- dom/console/ConsoleInstance.cpp
+++ dom/console/ConsoleInstance.cpp
@@ -2,8 +2,6 @@
 
 
 
-
-
 #include "mozilla/dom/ConsoleInstance.h"
 
 #include "Console.h"
@@ -184,8 +182,8 @@ METHOD(GroupCollapsed, u"groupCollapsed")
 #undef METHOD
 
 void ConsoleInstance::GroupEnd(JSContext* aCx) {
-  const Sequence<JS::Value> data;
   RefPtr<Console> console(mConsole);
+  const Sequence<JS::Value> data;
   console->MethodInternal(aCx, Console::MethodGroupEnd, u"groupEnd"_ns, data);
 }
 
@@ -258,8 +256,8 @@ void ConsoleInstance::CountReset(JSContext* aCx, const nsAString& aLabel) {
 }
 
 void ConsoleInstance::Clear(JSContext* aCx) {
-  const Sequence<JS::Value> data;
   RefPtr<Console> console(mConsole);
+  const Sequence<JS::Value> data;
   console->MethodInternal(aCx, Console::MethodClear, u"clear"_ns, data);
 }
 
