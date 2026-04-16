c63e5d30a8c5c0bfa27a2b4d78a7a4868ef83018
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-04-16 03:49:21 +0000

    Revert "Bug 2023217 - wasm: Add tests for JS-PI with stack switching. r=yury" for causing SM bustages CodeGenerator-shared.h.
    This reverts commit 2cfab4fac2c82f9ce45220308e7ab493eb2fed3b.
    
    Revert "Bug 2023217 - wasm: Add stack-switching proposal and implement JS-PI with it. r=yury,jonco"
    
    This reverts commit fda0d9ad12966f6ef929a39a76700013f1c03233.
    
    Revert "Bug 2023217 - wasm: Add validation and decoding for stack-switching opcodes. r=yury"
    
    This reverts commit 0a7b2e940e968403d78325cb88502ec98fb00c0a.
    
    Revert "Bug 2023217 - wasm: Add stack switching type system. r=yury"
    
    This reverts commit 225be1cc8924b5cb91c527f118a6417d0bfc9d77.
    
    Revert "Bug 2023217 - wasm: Add stack switching preference and feature flag. r=yury"
    
    This reverts commit bd673e76f92b2e21ebb68c3e992ae8039785f959.
    
    Revert "Bug 2023217 - wasm: Add assert* and Printf utilities to MacroAssembler. r=yury"
    
    This reverts commit 77d31ad93381679f6c69fd4b4ab6599dc2dce0da.
    
    UltraBlame original commit: 98145be0b1bd3be9df566ce676460827ce72e73b

--- js/src/wasm/WasmBuiltins.cpp
+++ js/src/wasm/WasmBuiltins.cpp
@@ -45,7 +45,6 @@
 #include "wasm/WasmGcObject.h"
 #include "wasm/WasmInstance.h"
 #include "wasm/WasmPI.h"
-#include "wasm/WasmStacks.h"
 #include "wasm/WasmStubs.h"
 
 #include "debugger/DebugAPI-inl.h"
@@ -434,25 +433,6 @@ constexpr SymbolicAddressSignature SASigArrayCopy = {
     _ThrowReported,
     7,
     {_PTR, _RoN, _I32, _RoN, _I32, _I32, _I32, _END}};
-#ifdef ENABLE_WASM_JSPI
-constexpr SymbolicAddressSignature SASigContNew = {
-    SymbolicAddress::ContNew, _RoN, _FailOnNullPtr,
-    _ThrowReported,           2,    {_PTR, _RoN, _END}};
-constexpr SymbolicAddressSignature SASigContNewEmpty = {
-    SymbolicAddress::ContNewEmpty,
-    _RoN,
-    _FailOnNullPtr,
-    _ThrowReported,
-    1,
-    {_PTR, _END}};
-constexpr SymbolicAddressSignature SASigContUnwind = {
-    SymbolicAddress::ContUnwind,
-    _VOID,
-    FailureMode::Infallible,
-    _ThrowReported,
-    2,
-    {_PTR, _PTR, _END}};
-#endif  
 
 #define VISIT_BUILTIN_FUNC(op, export, sa_name, ...)    \
   constexpr SymbolicAddressSignature SASig##sa_name = { \
@@ -464,6 +444,16 @@ constexpr SymbolicAddressSignature SASigContUnwind = {
 
 FOR_EACH_BUILTIN_MODULE_FUNC(VISIT_BUILTIN_FUNC)
 #undef VISIT_BUILTIN_FUNC
+
+#ifdef ENABLE_WASM_JSPI
+constexpr SymbolicAddressSignature SASigUpdateSuspenderState = {
+    SymbolicAddress::UpdateSuspenderState,
+    _VOID,
+    _Infallible,
+    _NoTrap,
+    3,
+    {_PTR, _PTR, _I32, _END}};
+#endif
 
 }  
 }  
@@ -555,7 +545,7 @@ static bool WasmHandleDebugTrap() {
   const Code& code = instance->code();
   MOZ_ASSERT(code.debugEnabled());
 #ifdef ENABLE_WASM_JSPI
-  MOZ_ASSERT(!cx->wasm().onContStack());
+  MOZ_ASSERT(!cx->wasm().onSuspendableStack());
 #endif
 
   
@@ -639,11 +629,7 @@ static WasmExceptionObject* GetOrWrapWasmException(JitActivation* activation,
   
   
   if (activation->isWasmTrapping() &&
-      activation->wasmTrapData().trap != Trap::ThrowReported
-#ifdef ENABLE_WASM_JSPI
-      && activation->wasmTrapData().trap != Trap::ThrowSuspendError
-#endif
-  ) {
+      activation->wasmTrapData().trap != Trap::ThrowReported) {
     return nullptr;
   }
 
@@ -797,17 +783,24 @@ static void WasmHandleRequestTierUp(Instance* instance) {
 
 void wasm::HandleExceptionWasm(JSContext* cx, JitFrameIter& iter,
                                jit::ResumeFromException* rfe) {
-  MOZ_ASSERT(!iter.done());
   MOZ_ASSERT(iter.isWasm());
   MOZ_ASSERT(CallingActivation(cx) == iter.activation());
   MOZ_ASSERT(cx->activation()->asJit()->hasWasmExitFP());
   MOZ_ASSERT(rfe->kind == ExceptionResumeKind::EntryFrame);
+
 #ifdef ENABLE_WASM_JSPI
   
   
-  MOZ_ASSERT(!cx->wasm().onContStack());
+  MOZ_ASSERT(!cx->wasm().onSuspendableStack());
 #endif
-  JitActivation* activation = CallingActivation(cx);
+
+  
+  
+  
+  
+  
+  
+  
 
 #ifdef DEBUG
   auto onExit = mozilla::MakeScopeExit([cx] {
@@ -818,43 +811,22 @@ void wasm::HandleExceptionWasm(JSContext* cx, JitFrameIter& iter,
   });
 #endif
 
-  
-  
-  
-  
+  MOZ_ASSERT(!iter.done());
+
   
   
   
   iter.asWasm().setIsLeavingFrames();
 
-  
+  JitActivation* activation = CallingActivation(cx);
   Rooted<WasmExceptionObject*> wasmExn(cx,
                                        GetOrWrapWasmException(activation, cx));
-
-#ifdef ENABLE_WASM_JSPI
-  
-  
-  wasm::ContStack* wasmPreviousStack = nullptr;
-#endif
 
   for (; !iter.done() && iter.isWasm(); ++iter) {
     
     
     WasmFrameIter& wasmFrame = iter.asWasm();
     cx->setRealmForJitExceptionHandler(wasmFrame.instance()->realm());
-
-    
-#ifdef ENABLE_WASM_JSPI
-    wasm::ContStack* wasmStack = wasmFrame.contStack();
-    if (wasmFrame.currentFrameStackSwitched()) {
-      if (wasmPreviousStack) {
-        ContStack::unwind(cx, wasmPreviousStack->handlers());
-      }
-      wasmPreviousStack = wasmStack;
-    } else {
-      MOZ_RELEASE_ASSERT(wasmStack == wasmPreviousStack);
-    }
-#endif
 
     
     if (wasmExn) {
@@ -885,71 +857,59 @@ void wasm::HandleExceptionWasm(JSContext* cx, JitFrameIter& iter,
         rfe->target = codeBlock->base() + tryNote->landingPadEntryPoint();
 
 #ifdef ENABLE_WASM_JSPI
-        if (wasmStack) {
-          rfe->stackTarget = &wasmStack->stackTarget();
-          rfe->baseHandlers = wasmStack->findBaseHandlers();
-          MOZ_ASSERT(rfe->baseHandlers);
-        } else {
-#  ifdef _WIN32
-          
-          
-          cx->wasm().updateWin32TibFields();
-#  endif  
-          rfe->stackTarget = &cx->wasm().mainStackTarget();
-          rfe->baseHandlers = nullptr;
+        wasm::SuspenderObject* destSuspender = activation->wasmExitSuspender();
+        if (destSuspender) {
+          destSuspender->enter(cx);
         }
-#endif  
+#endif
 
         
         
         if (activation->isWasmTrapping()) {
           
-          activation->finishWasmTrap();
+          activation->finishWasmTrap(false);
         } else {
           
-          activation->setWasmExitFP(nullptr);
+          activation->setWasmExitFP(nullptr, nullptr);
         }
         return;
       }
     }
 
-    if (wasmFrame.debugEnabled()) {
-      DebugFrame* frame = wasmFrame.debugFrame();
-      frame->clearReturnJSValue();
+    if (!wasmFrame.debugEnabled()) {
+      continue;
+    }
 
-      
-      
-      if (cx->isExceptionPending()) {
-        if (!DebugAPI::onExceptionUnwind(cx, AbstractFramePtr(frame))) {
-          if (cx->isPropagatingForcedReturn()) {
-            cx->clearPropagatingForcedReturn();
-            
-            
-            
-            JS_ReportErrorASCII(
-                cx, "Unexpected resumption value from onExceptionUnwind");
-            wasmExn = nullptr;
-          }
+    DebugFrame* frame = wasmFrame.debugFrame();
+    frame->clearReturnJSValue();
+
+    
+    
+    if (cx->isExceptionPending()) {
+      if (!DebugAPI::onExceptionUnwind(cx, AbstractFramePtr(frame))) {
+        if (cx->isPropagatingForcedReturn()) {
+          cx->clearPropagatingForcedReturn();
+          
+          
+          
+          JS_ReportErrorASCII(
+              cx, "Unexpected resumption value from onExceptionUnwind");
+          wasmExn = nullptr;
         }
       }
-
-      bool ok = DebugAPI::onLeaveFrame(cx, AbstractFramePtr(frame),
-                                       (const jsbytecode*)nullptr, false);
-      if (ok) {
-        
-        
-        
-        JS_ReportErrorASCII(cx, "Unexpected success from onLeaveFrame");
-        wasmExn = nullptr;
-      }
-      frame->leave(cx);
     }
-  }
 
-#ifdef ENABLE_WASM_JSPI
-  
-  MOZ_RELEASE_ASSERT(!wasmPreviousStack);
-#endif
+    bool ok = DebugAPI::onLeaveFrame(cx, AbstractFramePtr(frame),
+                                     (const jsbytecode*)nullptr, false);
+    if (ok) {
+      
+      
+      
+      JS_ReportErrorASCII(cx, "Unexpected success from onLeaveFrame");
+      wasmExn = nullptr;
+    }
+    frame->leave(cx);
+  }
 
   
   
@@ -970,7 +930,7 @@ static void* WasmHandleThrow(jit::ResumeFromException* rfe) {
   
   JSContext* cx = TlsContext.get();
 #ifdef ENABLE_WASM_JSPI
-  MOZ_ASSERT(!cx->wasm().onContStack());
+  MOZ_ASSERT(!cx->wasm().onSuspendableStack());
 #endif
   jit::HandleException(rfe);
   return cx->runtime()->jitRuntime()->getExceptionTailReturnValueCheck().value;
@@ -985,7 +945,7 @@ static void* CheckInterrupt(JSContext* cx, JitActivation* activation) {
   }
 
   void* resumePC = activation->wasmTrapData().resumePC;
-  activation->finishWasmTrap();
+  activation->finishWasmTrap(true);
   
   
   return resumePC;
@@ -1001,7 +961,7 @@ static void* WasmHandleTrap() {
   JSContext* cx = TlsContext.get();  
   JitActivation* activation = CallingActivation(cx);
 #ifdef ENABLE_WASM_JSPI
-  MOZ_ASSERT(!cx->wasm().onContStack());
+  MOZ_ASSERT(!cx->wasm().onSuspendableStack());
 #endif
 
   switch (activation->wasmTrapData().trap) {
@@ -1053,17 +1013,6 @@ static void* WasmHandleTrap() {
         return nullptr;
       }
       ReportTrapError(cx, JSMSG_OVER_RECURSED);
-      return nullptr;
-    }
-#ifdef ENABLE_WASM_JSPI
-    case Trap::ThrowSuspendError: {
-      JS_ReportErrorNumberUTF8(cx, GetErrorMessage, nullptr,
-                               JSMSG_JSPI_SUSPEND_ERROR);
-      return nullptr;
-    }
-#endif
-    case Trap::Unimplemented: {
-      ReportTrapError(cx, JSMSG_WASM_UNIMPLEMENTED);
       return nullptr;
     }
     case Trap::ThrowReported:
@@ -1414,15 +1363,6 @@ void wasm::PrintF32(float val) { fprintf(stderr, "f32(%f) ", val); }
 void wasm::PrintF64(double val) { fprintf(stderr, "f64(%lf) ", val); }
 
 void wasm::PrintText(const char* out) { fprintf(stderr, "%s", out); }
-
-void wasm::Printf(const char* out, uintptr_t value) {
-  AutoEnterOOMUnsafeRegion oomUnsafe;
-  js::UniqueChars line = JS_sprintf_append(nullptr, out, value);
-  if (!line) {
-    oomUnsafe.crash("OOM at masm.printf");
-  }
-  fprintf(stderr, "%s", line.get());
-}
 #endif
 
 void* wasm::AddressOf(SymbolicAddress imm, ABIFunctionType* abiType) {
@@ -1742,12 +1682,6 @@ void* wasm::AddressOf(SymbolicAddress imm, ABIFunctionType* abiType) {
       *abiType = Args_Int32_GeneralGeneral;
       MOZ_ASSERT(*abiType == ToABIType(SASigPostBarrierWholeCell));
       return FuncCast(Instance::postBarrierWholeCell, *abiType);
-#ifdef ENABLE_WASM_JSPI
-    case SymbolicAddress::ResumeBarrier:
-      *abiType = Args_General1;
-      return FuncCast(gc::PerformIncrementalPreWriteBarrierAllChildren,
-                      *abiType);
-#endif
     case SymbolicAddress::StructNewIL_true:
       *abiType = Args_General_GeneralInt32General;
       MOZ_ASSERT(*abiType == ToABIType(SASigStructNewIL_true));
@@ -1792,20 +1726,6 @@ void* wasm::AddressOf(SymbolicAddress imm, ABIFunctionType* abiType) {
       *abiType = Args_Int32_GeneralGeneralInt32GeneralInt32Int32Int32;
       MOZ_ASSERT(*abiType == ToABIType(SASigArrayCopy));
       return FuncCast(Instance::arrayCopy, *abiType);
-#ifdef ENABLE_WASM_JSPI
-    case SymbolicAddress::ContNew:
-      *abiType = Args_General2;
-      MOZ_ASSERT(*abiType == ToABIType(SASigContNew));
-      return FuncCast(Instance::contNew, *abiType);
-    case SymbolicAddress::ContNewEmpty:
-      *abiType = Args_General1;
-      MOZ_ASSERT(*abiType == ToABIType(SASigContNewEmpty));
-      return FuncCast(Instance::contNewEmpty, *abiType);
-    case SymbolicAddress::ContUnwind:
-      *abiType = Args_Int32_GeneralGeneral;
-      MOZ_ASSERT(*abiType == ToABIType(SASigContUnwind));
-      return FuncCast(Instance::contUnwind, *abiType);
-#endif  
     case SymbolicAddress::SlotsToAllocKindBytesTable:
       return (void*)gc::slotsToAllocKindBytes;
     case SymbolicAddress::ExceptionNew:
@@ -1816,6 +1736,13 @@ void* wasm::AddressOf(SymbolicAddress imm, ABIFunctionType* abiType) {
       *abiType = Args_Int32_GeneralGeneral;
       MOZ_ASSERT(*abiType == ToABIType(SASigThrowException));
       return FuncCast(Instance::throwException, *abiType);
+
+#ifdef ENABLE_WASM_JSPI
+    case SymbolicAddress::UpdateSuspenderState:
+      *abiType = Args_Int32_GeneralGeneralInt32;
+      MOZ_ASSERT(*abiType == ToABIType(SASigUpdateSuspenderState));
+      return FuncCast(UpdateSuspenderState, *abiType);
+#endif
 
 #ifdef WASM_CODEGEN_DEBUG
     case SymbolicAddress::PrintI32:
@@ -1833,9 +1760,6 @@ void* wasm::AddressOf(SymbolicAddress imm, ABIFunctionType* abiType) {
     case SymbolicAddress::PrintText:
       *abiType = Args_General1;
       return FuncCast(PrintText, *abiType);
-    case SymbolicAddress::Printf:
-      *abiType = Args_General2;
-      return FuncCast(Printf, *abiType);
 #endif
 #define VISIT_BUILTIN_FUNC(op, export, sa_name, abitype, needs_thunk, entry, \
                            ...)                                              \
@@ -1904,7 +1828,6 @@ bool wasm::NeedsBuiltinThunk(SymbolicAddress sym) {
     case SymbolicAddress::PrintF32:
     case SymbolicAddress::PrintF64:
     case SymbolicAddress::PrintText:
-    case SymbolicAddress::Printf:
       return false;
 #endif
 
@@ -1997,9 +1920,6 @@ bool wasm::NeedsBuiltinThunk(SymbolicAddress sym) {
     case SymbolicAddress::PostBarrierEdge:
     case SymbolicAddress::PostBarrierEdgePrecise:
     case SymbolicAddress::PostBarrierWholeCell:
-#ifdef ENABLE_WASM_JSPI
-    case SymbolicAddress::ResumeBarrier:
-#endif
     case SymbolicAddress::ExceptionNew:
     case SymbolicAddress::ThrowException:
     case SymbolicAddress::StructNewIL_true:
@@ -2014,9 +1934,7 @@ bool wasm::NeedsBuiltinThunk(SymbolicAddress sym) {
     case SymbolicAddress::ArrayInitElem:
     case SymbolicAddress::ArrayCopy:
 #ifdef ENABLE_WASM_JSPI
-    case SymbolicAddress::ContNew:
-    case SymbolicAddress::ContNewEmpty:
-    case SymbolicAddress::ContUnwind:
+    case SymbolicAddress::UpdateSuspenderState:
 #endif
       return true;
 
@@ -2034,7 +1952,19 @@ bool wasm::NeedsBuiltinThunk(SymbolicAddress sym) {
 
 static bool NeedsDynamicSwitchToMainStack(SymbolicAddress sym) {
   MOZ_ASSERT(NeedsBuiltinThunk(sym));
-  return true;
+  switch (sym) {
+#if ENABLE_WASM_JSPI
+    
+    
+    case SymbolicAddress::UpdateSuspenderState:
+    case SymbolicAddress::CurrentSuspender:
+      return false;
+#endif
+
+    
+    default:
+      return true;
+  }
 }
 
 
@@ -2231,7 +2161,6 @@ bool wasm::EnsureBuiltinThunksInitialized(
     return false;
   }
 
-  JitContext jitContext;
   LifoAlloc lifo(BUILTIN_THUNK_LIFO_SIZE, js::MallocArena);
   TempAllocator tempAlloc(&lifo);
   WasmMacroAssembler masm(tempAlloc);
@@ -2301,6 +2230,7 @@ bool wasm::EnsureBuiltinThunksInitialized(
 #ifdef DEBUG
   
   
+  JitContext jitContext;
   bool oldFlag = jitContext.setIsCompilingWasm(false);
 #endif
 
