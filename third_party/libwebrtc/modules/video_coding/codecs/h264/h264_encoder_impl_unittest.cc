4999ebbd449820bce065b4d48f4b70198ad16ca0
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-04-16 02:33:34 +0000

    Bug 2029247 - Vendor libwebrtc from 6564679131
    Upstream commit: https://webrtc.googlesource.com/src/+/6564679131a722444162f77935cdfee07304a35c
        Add support for OnFrameDropped callback in OpenH264 encoder.
    
        This CL does some minor refactoring and support for reporting of when a
        frame has been dropped by the rate controller.
    
        Bug: webrtc:467444018
        Change-Id: I0453bc2f7e0c18cc67bf19d44e36c01c69d48c58
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/444781
        Commit-Queue: Erik Språng <sprangwebrtc.org>
        Reviewed-by: Sergey Silkin <ssilkinwebrtc.org>
        Cr-Commit-Position: refs/heads/main{#46797}
    
    UltraBlame original commit: 656f128cdf63a60aa8d909d9d3115a33eaaaf2ad

--- third_party/libwebrtc/modules/video_coding/codecs/h264/h264_encoder_impl_unittest.cc
+++ third_party/libwebrtc/modules/video_coding/codecs/h264/h264_encoder_impl_unittest.cc
@@ -11,17 +11,32 @@
 
 #include "modules/video_coding/codecs/h264/h264_encoder_impl.h"
 
+#include <cstdint>
+#include <optional>
+
 #include "api/environment/environment_factory.h"
+#include "api/test/create_frame_generator.h"
+#include "api/test/frame_generator_interface.h"
+#include "api/test/mock_video_encoder.h"
+#include "api/video/video_codec_type.h"
+#include "api/video/video_frame.h"
+#include "api/video_codecs/video_codec.h"
 #include "api/video_codecs/video_encoder.h"
+#include "modules/video_coding/codecs/h264/include/h264_globals.h"
 #include "modules/video_coding/include/video_error_codes.h"
+#include "test/gmock.h"
 #include "test/gtest.h"
+
+using ::testing::AnyNumber;
+using ::testing::AtLeast;
+using ::testing::Return;
 
 namespace webrtc {
 
 namespace {
 
-const int kMaxPayloadSize = 1024;
-const int kNumCores = 1;
+constexpr int kMaxPayloadSize = 1024;
+constexpr int kNumCores = 1;
 
 const VideoEncoder::Capabilities kCapabilities(false);
 const VideoEncoder::Settings kSettings(kCapabilities,
@@ -72,6 +87,54 @@ TEST(H264EncoderImplTest, CanInitializeWithSingleNalUnitModeExplicitly) {
             encoder.InitEncode(&codec_settings, kSettings));
   EXPECT_EQ(H264PacketizationMode::SingleNalUnit,
             encoder.PacketizationModeForTesting());
+}
+
+TEST(H264EncoderImplTest, OnFrameDropped) {
+  H264EncoderImpl encoder(CreateEnvironment(), {});
+  VideoCodec codec_settings;
+  SetDefaultSettings(&codec_settings);
+  
+  codec_settings.startBitrate = 1;
+  codec_settings.maxBitrate = 1;
+
+  MockEncodedImageCallback callback;
+  EXPECT_EQ(WEBRTC_VIDEO_CODEC_OK,
+            encoder.InitEncode(&codec_settings, kSettings));
+  EXPECT_EQ(WEBRTC_VIDEO_CODEC_OK,
+            encoder.RegisterEncodeCompleteCallback(&callback));
+
+  auto frame_generator = test::CreateSquareFrameGenerator(
+      codec_settings.width, codec_settings.height,
+      test::FrameGeneratorInterface::OutputType::kI420,
+      std::nullopt);
+
+  
+  
+  const int kNumFramesToEncode = 30;
+
+  EXPECT_CALL(callback, OnEncodedImage)
+      .Times(AnyNumber())
+      .WillRepeatedly(Return(
+          EncodedImageCallback::Result(EncodedImageCallback::Result::OK)));
+
+  EXPECT_CALL(callback, OnFrameDropped)
+      .Times(AtLeast(1))
+      .WillRepeatedly([&](uint32_t rtp_timestamp, int spatial_id,
+                          bool is_end_of_temporal_unit) {
+        
+        EXPECT_EQ(spatial_id, 0);  
+                                   
+        EXPECT_TRUE(is_end_of_temporal_unit);
+      });
+
+  for (int i = 0; i < kNumFramesToEncode; ++i) {
+    VideoFrame frame =
+        VideoFrame::Builder()
+            .set_video_frame_buffer(frame_generator->NextFrame().buffer)
+            .set_rtp_timestamp(i * 90000 / codec_settings.maxFramerate)
+            .build();
+    encoder.Encode(frame, nullptr);
+  }
 }
 
 }  
