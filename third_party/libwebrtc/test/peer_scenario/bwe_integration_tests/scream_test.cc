6f1c65ab9143aa2c58d5a28e5a9365c66a8f76da
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-04-16 02:24:30 +0000

    Bug 2029247 - Vendor libwebrtc from 6367adcc43
    Upstream commit: https://webrtc.googlesource.com/src/+/6367adcc43c75201080a7719aa08e055fba859de
        Reduce EWMA coefficient for srtt in Scream
    
        Reduced in order to speed up recovery after cross traffic.
    
        Bug: webrtc:447037083
        Change-Id: If848b58efba74d6354eefcc2a69f73b8f8147e54
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/442440
        Reviewed-by: Björn Terelius <tereliuswebrtc.org>
        Commit-Queue: Per Kjellander <perkjwebrtc.org>
        Cr-Commit-Position: refs/heads/main{#46680}
    
    UltraBlame original commit: 5475af667b678181c6a68068ce5825b49fca423e

--- third_party/libwebrtc/test/peer_scenario/bwe_integration_tests/scream_test.cc
+++ third_party/libwebrtc/test/peer_scenario/bwe_integration_tests/scream_test.cc
@@ -21,6 +21,7 @@
 #include "api/stats/rtc_stats_report.h"
 #include "api/test/network_emulation/dual_pi2_network_queue.h"
 #include "api/test/network_emulation/network_config_schedule.pb.h"
+#include "api/test/network_emulation/network_emulation_interfaces.h"
 #include "api/test/network_emulation/network_queue.h"
 #include "api/test/network_emulation/schedulable_network_node_builder.h"
 #include "api/test/network_emulation/token_bucket_network_behavior_builder.h"
@@ -312,7 +313,7 @@ TEST(ScreamTest, MaybeTest(LinkCapacity600KbpsRtt100msEcn)) {
 
   SendMediaTestResult result = SendMediaInOneDirection(std::move(params), s);
   
-  EXPECT_THAT(result.caller().subview(2), Each(AvailableSendBitrateIsBetween(
+  EXPECT_THAT(result.caller().subview(3), Each(AvailableSendBitrateIsBetween(
                                               DataRate::KilobitsPerSec(350),
                                               DataRate::KilobitsPerSec(660))));
 }
@@ -697,5 +698,46 @@ TEST(ScreamTest, MaybeTest(LinkCapacity5MbitPolicedTo256Kbit)) {
                                               DataRate::KilobitsPerSec(1100))));
 }
 
+TEST(ScreamTest, MaybeTest(LinkCapacity5MbitWithCrossTrafficNoEcn)) {
+  PeerScenario s(*testing::UnitTest::GetInstance()->current_test_info());
+  SendMediaTestParams params;
+  params.test_duration = TimeDelta::Seconds(30);
+  params.caller_to_callee_path =
+      CreateNetworkPath(s,  false,
+                        DataRate::KilobitsPerSec(5000), TimeDelta::Millis(25));
+  params.callee_to_caller_path =
+      CreateNetworkPath(s,  false,
+                        DataRate::KilobitsPerSec(5000), TimeDelta::Millis(25));
+
+  
+  webrtc::TcpMessageRoute* tcp_route = s.net()->CreateTcpRoute(
+      s.net()->CreateRoute({params.caller_to_callee_path}),
+      s.net()->CreateRoute({params.callee_to_caller_path}));
+  Timestamp start_time = s.net()->Now();
+  Timestamp tcp_message_delivered_time = Timestamp::MinusInfinity();
+  s.net()->time_controller()->GetMainThread()->PostDelayedTask(
+      [&]() {
+        tcp_route->SendMessage(
+            2'000'000,
+            [&tcp_message_delivered_time, &s]() {
+              tcp_message_delivered_time = s.net()->Now();
+            });
+      },
+      TimeDelta::Seconds(3));
+
+  SendMediaTestResult result = SendMediaInOneDirection(std::move(params), s);
+  ASSERT_TRUE(tcp_message_delivered_time.IsFinite());
+
+  
+  
+  
+  int index_where_available_bitrate_should_have_recovered =
+      (tcp_message_delivered_time - start_time).seconds<int>() + 10;
+  EXPECT_THAT(
+      result.caller().subview(
+          index_where_available_bitrate_should_have_recovered),
+      Each(AvailableSendBitrateIsBetween(DataRate::KilobitsPerSec(1500),
+                                         DataRate::KilobitsPerSec(5000))));
+}
 }  
 }  
