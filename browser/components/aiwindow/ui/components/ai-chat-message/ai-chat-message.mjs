7c90aa7c8fe1531fa9d0dc2341e4bf5fab26c999
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-04-28 23:33:06 +0000

    Bug 2027029 - Part 1: Add support for markdown tables to Smart Window chat messages r=ai-frontend-reviewers,echa
    Adds support for rendering markdown tables in assistant chat messages. Table elements are wrapped by a custom element for componentization and extending functionality.
    
    Differential Revision: https://phabricator.services.mozilla.com/D294922
    
    UltraBlame original commit: 26a2153b4567ff138cd64e974796a97a0b8e8eb4

--- browser/components/aiwindow/ui/components/ai-chat-message/ai-chat-message.mjs
+++ browser/components/aiwindow/ui/components/ai-chat-message/ai-chat-message.mjs
@@ -2,16 +2,18 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+/* global Sanitizer */
+
 import { html } from "chrome://global/content/vendor/lit.all.mjs";
 import { MozLitElement } from "chrome://global/content/lit-utils.mjs";
 import {
-  defaultMarkdownParser,
-  DOMSerializer,
-} from "chrome://browser/content/multilineeditor/prosemirror.bundle.mjs";
+  parseMarkdown,
+  CHAT_WRAPPER_ELEMENTS,
+} from "chrome://browser/content/aiwindow/modules/ChatMarkdownParser.mjs";
 // eslint-disable-next-line import/no-unassigned-import
 import "chrome://browser/content/aiwindow/components/ai-chat-search-button.mjs";
-
-const SERIALIZER = DOMSerializer.fromSchema(defaultMarkdownParser.schema);
+// eslint-disable-next-line import/no-unassigned-import
+import "chrome://browser/content/aiwindow/components/ai-chat-table.mjs";
 
 /**
  * A custom element for rendering a single chat message, either a user message or an
@@ -294,21 +296,28 @@ export class AIChatMessage extends MozLitElement {
   }
 
   /**
-   * Parse markdown content to HTML using ProseMirror
+   * Custom sanitizer for chat messages.
+   *
+   * @type {Sanitizer}
+   */
+  static #chatMessageSanitizer;
+  static {
+    this.#chatMessageSanitizer = new Sanitizer();
+    for (const element of Object.values(CHAT_WRAPPER_ELEMENTS)) {
+      this.#chatMessageSanitizer.allowElement(element);
+    }
+  }
+
+  /**
+   * Parse markdown content to HTML.
    *
    * @param {string} markdown the Markdown to parse
    * @param {Element} element the element in which to insert the parsed markdown.
    */
-  parseMarkdown(markdown, element) {
-    const node = defaultMarkdownParser.parse(markdown);
-    const fragment = SERIALIZER.serializeFragment(node.content);
-
-    // Convert DocumentFragment to HTML string
-    const container = this.ownerDocument.createElement("div");
-    container.appendChild(fragment);
-
-    // Sanitize the HTML string by using "setHTML"
-    element.setHTML(container.innerHTML);
+  #parseMarkdown(markdown, element) {
+    element.setHTML(parseMarkdown(markdown), {
+      sanitizer: AIChatMessage.#chatMessageSanitizer,
+    });
   }
 
   /**
@@ -318,7 +327,7 @@ export class AIChatMessage extends MozLitElement {
    * @param {Element} element
    */
   parseUserMarkdown(markdown, element) {
-    this.parseMarkdown(markdown, element);
+    this.#parseMarkdown(markdown, element);
     this.#replaceWebsiteMentions(element);
   }
 
@@ -345,7 +354,7 @@ export class AIChatMessage extends MozLitElement {
     }
 
     // Parse the message into markdown, and unfurl any unseen links.
-    this.parseMarkdown(this.message, messageElement);
+    this.#parseMarkdown(this.message, messageElement);
     this.#unfurlUnseenLinks(messageElement);
 
     // Track the properties for memoization.
