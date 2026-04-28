ed5d36b5b6af738c5e6fdf73245d7ebffa3d0132
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-04-28 13:48:13 +0000

    Bug 2034807 - Use constexpr in BaseMarkerType::MarkerTypeDisplay to silence nonnull warnings r=mstange,profiler-reviewers
    The nullable static fields on BaseMarkerType (AllLabels, ChartLabel,
    TableLabel, TooltipLabel, ColorField, Description) default to `constexpr
    const char* = nullptr`. For marker types that don't override them,
    passing the value through the setters that take std::string trips
    clang's -Wnonnull, because libc++'s basic_string(const char*)
    constructor is annotated nonnull. The runtime `if (T::Field)` guard
    doesn't help, since -Wnonnull is a syntactic check and doesn't look at
    the enclosing condition.
    
    Using `if constexpr` drops the branch entirely when the field is null.
    
    Differential Revision: https://phabricator.services.mozilla.com/D296340
    
    UltraBlame original commit: 15bc5ff4da7fd90e3653517e270791cb66b9739b

--- mozglue/baseprofiler/public/BaseProfilerMarkersPrerequisites.h
+++ mozglue/baseprofiler/public/BaseProfilerMarkersPrerequisites.h
@@ -711,7 +711,8 @@ class MarkerSchema {
     CString,
     String,
     TimeStamp,
-    TimeDuration
+    TimeDuration,
+    Flow,
   };
 
   template <typename T>
@@ -743,6 +744,8 @@ class MarkerSchema {
       return InputType::TimeDuration;
     } else if constexpr (std::is_same_v<CleanT, ProfilerString8View>) {
       return InputType::CString;
+    } else if constexpr (std::is_same_v<CleanT, Flow>) {
+      return InputType::Flow;
     } else {
       static_assert(sizeof(T) == 0, "Unsupported type");
     }
@@ -1095,6 +1098,69 @@ struct StreamPayloadHelper<Flow, aFormat> {
   }
 };
 
+template <MarkerSchema::InputType IT>
+struct InputTypeToCpp;
+template <>
+struct InputTypeToCpp<MarkerSchema::InputType::Uint64> {
+  using Type = uint64_t;
+};
+template <>
+struct InputTypeToCpp<MarkerSchema::InputType::Uint32> {
+  using Type = uint32_t;
+};
+template <>
+struct InputTypeToCpp<MarkerSchema::InputType::Uint8> {
+  using Type = uint8_t;
+};
+template <>
+struct InputTypeToCpp<MarkerSchema::InputType::Int64> {
+  using Type = int64_t;
+};
+template <>
+struct InputTypeToCpp<MarkerSchema::InputType::Int32> {
+  using Type = int32_t;
+};
+template <>
+struct InputTypeToCpp<MarkerSchema::InputType::Int8> {
+  using Type = int8_t;
+};
+template <>
+struct InputTypeToCpp<MarkerSchema::InputType::Double> {
+  using Type = double;
+};
+template <>
+struct InputTypeToCpp<MarkerSchema::InputType::Boolean> {
+  using Type = bool;
+};
+template <>
+struct InputTypeToCpp<MarkerSchema::InputType::CString> {
+  using Type = ProfilerString8View;
+};
+template <>
+struct InputTypeToCpp<MarkerSchema::InputType::String> {
+  using Type = ProfilerString16View;
+};
+template <>
+struct InputTypeToCpp<MarkerSchema::InputType::TimeStamp> {
+  using Type = TimeStamp;
+};
+template <>
+struct InputTypeToCpp<MarkerSchema::InputType::TimeDuration> {
+  using Type = TimeDuration;
+};
+template <>
+struct InputTypeToCpp<MarkerSchema::InputType::Flow> {
+  using Type = Flow;
+};
+
+template <typename T, size_t... Is>
+auto PayloadFieldsTupleHelper(std::index_sequence<Is...>) -> std::tuple<
+    typename InputTypeToCpp<T::PayloadFields[Is].InputTy>::Type...>;
+
+template <typename T>
+using PayloadFieldsTuple = decltype(PayloadFieldsTupleHelper<T>(
+    std::make_index_sequence<std::size(T::PayloadFields)>{}));
+
 }  
 
 
@@ -1127,22 +1193,22 @@ struct BaseMarkerType {
   static MarkerSchema MarkerTypeDisplay() {
     using MS = MarkerSchema;
     MS schema{T::Locations, std::size(T::Locations)};
-    if (T::AllLabels) {
+    if constexpr (T::AllLabels) {
       schema.SetAllLabels(T::AllLabels);
     }
-    if (T::ChartLabel) {
+    if constexpr (T::ChartLabel) {
       schema.SetChartLabel(T::ChartLabel);
     }
-    if (T::TableLabel) {
+    if constexpr (T::TableLabel) {
       schema.SetTableLabel(T::TableLabel);
     }
-    if (T::TooltipLabel) {
+    if constexpr (T::TooltipLabel) {
       schema.SetTooltipLabel(T::TooltipLabel);
     }
-    if (T::IsStackBased) {
+    if constexpr (T::IsStackBased) {
       schema.SetIsStackBased();
     }
-    if (T::ColorField) {
+    if constexpr (T::ColorField) {
       schema.SetColorField(T::ColorField);
     }
     for (const MS::PayloadField field : T::PayloadFields) {
@@ -1153,7 +1219,7 @@ struct BaseMarkerType {
         schema.AddKeyFormat(field.Key, field.Fmt, field.Flags);
       }
     }
-    if (T::Description) {
+    if constexpr (T::Description) {
       schema.AddStaticLabelValue("Description", T::Description);
     }
     return schema;
