440a404a9ec739a7f64d98307c5cf232e060d401
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-04-28 12:49:27 +0000

    Revert "Bug 2033769 - [marionette] Fix "WebDriver:Navigate" and "WebDriver:Refresh" to not miss errors when triggering the actual navigation. r=Sasha" for causing wp failures at /ink-overflow-001-print.html
    This reverts commit fdf197f64ce87d0c448ec650a16dc775482d0e81.
    
    UltraBlame original commit: dcf7683ebfa31e68a4d7a184b2acee079803a2ce

--- remote/marionette/navigate.sys.mjs
+++ remote/marionette/navigate.sys.mjs
@@ -161,7 +161,7 @@ navigate.isLoadEventExpected = function (current, options = {}) {
  * @param {string} url
  *     URL to navigate to.
  */
-navigate.navigateTo = function (browsingContext, url) {
+navigate.navigateTo = async function (browsingContext, url) {
   const opts = {
     loadFlags: Ci.nsIWebNavigation.LOAD_FLAGS_IS_LINK,
     // Fake user activation.
@@ -179,7 +179,7 @@ navigate.navigateTo = function (browsingContext, url) {
  * @param {CanonicalBrowsingContext} browsingContext
  *     Browsing context to refresh.
  */
-navigate.refresh = function (browsingContext) {
+navigate.refresh = async function (browsingContext) {
   const flags = Ci.nsIWebNavigation.LOAD_FLAGS_BYPASS_CACHE;
   // Bug 2026546: As workaround use sessionHistory if available to avoid issues
   // with frames.
