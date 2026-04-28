f639abf01fe5aeebd4518ca9bda24e2a6115d531
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-04-28 14:06:39 +0000

    Revert "Bug 2014160 - Use nsTHashSet in CacheIndexIterator::mRecords r=jesup,necko-reviewers" for causing failures in PLDHashTable.h.
    This reverts commit 716b47f1859210ba2d7887852a581da435340c79.
    
    UltraBlame original commit: ccbe847c8835633abba8e5f0e728e3a0eacc7088

--- netwerk/cache2/CacheIndexIterator.cpp
+++ netwerk/cache2/CacheIndexIterator.cpp
@@ -32,14 +32,12 @@ nsresult CacheIndexIterator::GetNextHash(SHA1Sum::Hash* aHash) {
     return mStatus;
   }
 
-  if (mRecords.IsEmpty()) {
+  if (!mRecords.Length()) {
     CloseInternal(NS_ERROR_NOT_AVAILABLE);
     return mStatus;
   }
 
-  auto it = mRecords.begin();
-  memcpy(aHash, (*it)->Get()->mHash, sizeof(SHA1Sum::Hash));
-  mRecords.Remove(*it);
+  memcpy(aHash, mRecords.PopLastElement()->Get()->mHash, sizeof(SHA1Sum::Hash));
 
   return NS_OK;
 }
@@ -82,7 +80,7 @@ void CacheIndexIterator::AddRecord(CacheIndexRecordWrapper* aRecord,
                                    const StaticMutexAutoLock& aProofOfLock) {
   LOG(("CacheIndexIterator::AddRecord() [this=%p, record=%p]", this, aRecord));
 
-  mRecords.Insert(aRecord);
+  mRecords.AppendElement(aRecord);
 }
 
 bool CacheIndexIterator::RemoveRecord(CacheIndexRecordWrapper* aRecord,
@@ -90,7 +88,7 @@ bool CacheIndexIterator::RemoveRecord(CacheIndexRecordWrapper* aRecord,
   LOG(("CacheIndexIterator::RemoveRecord() [this=%p, record=%p]", this,
        aRecord));
 
-  return mRecords.EnsureRemoved(aRecord);
+  return mRecords.RemoveElement(aRecord);
 }
 
 bool CacheIndexIterator::ReplaceRecord(
