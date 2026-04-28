8aa5777a501950fbf1d4acb4de1c781f5fc37b95
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-04-28 23:29:02 +0000

    Bug 1676795. Fix Element::Animate() and Animation::Constructor to use ScrollTimeline. r=boris,webidl,smaug
    - Adds the missing KeyframeAnimationOptions.timeline WebIDL
    - Makes Element::Animate use KeyframeAnimationOptions.timeline when
      appropriate, passing it to Animation::Constructor
    
    Differential Revision: https://phabricator.services.mozilla.com/D296787
    
    UltraBlame original commit: 7340b3ea33af793bfc42ecce4c6075dedb697223

--- dom/base/Element.cpp
+++ dom/base/Element.cpp
@@ -4895,6 +4895,12 @@ already_AddRefed<Animation> Element::Animate(
 
   
   
+
+  
+
+  
+  
+  
   
   
   RefPtr<KeyframeEffect> effect =
@@ -4907,17 +4913,31 @@ already_AddRefed<Animation> Element::Animate(
   
   JSAutoRealm ar(aContext, global.Get());
 
-  AnimationTimeline* timeline = OwnerDoc()->Timeline();
-  RefPtr<Animation> animation = Animation::Constructor(
-      global, effect, Optional<AnimationTimeline*>(timeline), aError);
+  
+  
+  
+  Optional<AnimationTimeline*> timeline;
+  if (aOptions.IsKeyframeAnimationOptions()) {
+    const auto& tl = aOptions.GetAsKeyframeAnimationOptions().mTimeline;
+    if (tl.WasPassed()) {
+      timeline.Construct(tl.Value().get());
+    }
+  }
+
+  
+  RefPtr<Animation> animation =
+      Animation::Constructor(global, effect, timeline, aError);
   if (aError.Failed()) {
     return nullptr;
   }
 
+  
+  
   if (aOptions.IsKeyframeAnimationOptions()) {
     animation->SetId(aOptions.GetAsKeyframeAnimationOptions().mId);
   }
 
+  
   animation->Play(aError, Animation::LimitBehavior::AutoRewind);
   if (aError.Failed()) {
     return nullptr;
