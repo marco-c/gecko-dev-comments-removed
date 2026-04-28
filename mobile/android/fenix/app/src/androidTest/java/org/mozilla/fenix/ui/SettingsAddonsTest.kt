d86fff1fb82ad78abfaad60a1b30a0e3dc9c3640
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-04-28 12:10:59 +0000

    Bug 2033427 - Temporarily remove the verifyExtensionsButtonWithInstalledExtension test step r=dpop
    All 4 affected UI tests successfully passed 50x on Firebase ✅
    
    Differential Revision: https://phabricator.services.mozilla.com/D295259
    
    UltraBlame original commit: d0ec09c851fe7e61bdadeec018c63f86aa1982f9

--- mobile/android/fenix/app/src/androidTest/java/org/mozilla/fenix/ui/SettingsAddonsTest.kt
+++ mobile/android/fenix/app/src/androidTest/java/org/mozilla/fenix/ui/SettingsAddonsTest.kt
@@ -160,9 +160,12 @@ class SettingsAddonsTest {
         navigationToolbar(composeTestRule) {
         }.enterURLAndEnterToBrowser(webPage.toUri()) {
             verifyPageContent("Lets test!")
-        }.openThreeDotMenu {
-            verifyExtensionsButtonWithInstalledExtension(addonName)
         }
+        // Fix ComposeNotIdleException when verifying the extensions button when an extension is installed
+        // Bugzilla ticket: https://bugzilla.mozilla.org/show_bug.cgi?id=2033366
+        //     .openThreeDotMenu {
+        //     verifyExtensionsButtonWithInstalledExtension(addonName)
+        // }
     }
 
     // TestRail link: https://mozilla.testrail.io/index.php?/cases/view/875785
@@ -185,8 +188,11 @@ class SettingsAddonsTest {
         navigationToolbar(composeTestRule) {
         }.enterURLAndEnterToBrowser(webPage.toUri()) {
             verifyPageContent("Lets test!")
-        }.openThreeDotMenu {
-            verifyExtensionsButtonWithInstalledExtension(addonName)
         }
+        // Fix ComposeNotIdleException when verifying the extensions button when an extension is installed
+        // Bugzilla ticket: https://bugzilla.mozilla.org/show_bug.cgi?id=2033366
+        //     .openThreeDotMenu {
+        //     verifyExtensionsButtonWithInstalledExtension(addonName)
+        // }
     }
 }
