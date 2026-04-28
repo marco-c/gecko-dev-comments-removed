60e72a9622d564d330089e0d39ecfc9b81634717
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-04-28 12:56:27 +0000

    Revert "Bug 2032452 - enable firefox backup on all platforms. r=cdupuis" for causing several bc failures on macOS.
    This reverts commit 1dd83539cec9d964696b247e22fd0d0b2ba402b5.
    
    UltraBlame original commit: 1d0bc85d226bf31a74f26ed586a7487cc7898591

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -3478,8 +3478,14 @@ pref("browser.backup.enabled", true);
 pref("browser.backup.scheduled.enabled", false);
 
 
-pref("browser.backup.archive.enabled", true);
-pref("browser.backup.restore.enabled", true);
+
+#ifdef XP_WIN
+  pref("browser.backup.archive.enabled", true);
+  pref("browser.backup.restore.enabled", true);
+#else
+  pref("browser.backup.archive.enabled", false);
+  pref("browser.backup.restore.enabled", false);
+#endif
 
 
 pref("browser.backup.sqlite.pages_per_step", 50);
