162471dd684fddf80ec6ec8b547d762ddcfd5563
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-04-28 13:53:41 +0000

    Bug 2034798 - Refactor gfx blocklist macros to prevent common mistakes. r=gfx-reviewers,lsalzman,bradwerth
    This patch adds static asserts to prevent allowlist statuses to be
    used with non-allowlist features, as well as prevent retired
    features from being used. It also removes the name/ID split for
    most features where they are the same.
    
    Differential Revision: https://phabricator.services.mozilla.com/D296378
    
    UltraBlame original commit: a19289d2a96d480d968c64197ca77395e0a11792

--- widget/GfxInfoBase.cpp
+++ widget/GfxInfoBase.cpp
@@ -138,12 +138,18 @@ NS_IMPL_ISUPPORTS(GfxInfoBase, nsIGfxInfo, nsIObserver,
 static const char* GetPrefNameForFeature(int32_t aFeature) {
   const char* fullpref = nullptr;
   switch (aFeature) {
-#define GFXINFO_FEATURE(id, name, pref)    \
+#define GFXINFO_FEATURE(id, pref)          \
   case nsIGfxInfo::FEATURE_##id:           \
     fullpref = BLOCKLIST_PREF_BRANCH pref; \
     break;
+#define GFXINFO_FEATURE_RETIRED(id, pref)
+#define GFXINFO_FEATURE_ALLOWLIST(id, pref) GFXINFO_FEATURE(id, pref)
+#define GFXINFO_FEATURE_MISMATCHED(id, name, pref) GFXINFO_FEATURE(id, pref)
 #include "mozilla/widget/GfxInfoFeatureDefs.inc"
 #undef GFXINFO_FEATURE
+#undef GFXINFO_FEATURE_RETIRED
+#undef GFXINFO_FEATURE_ALLOWLIST
+#undef GFXINFO_FEATURE_MISMATCHED
     default:
       MOZ_ASSERT_UNREACHABLE("Unexpected nsIGfxInfo feature?!");
       break;
@@ -261,12 +267,21 @@ static already_AddRefed<const GfxDeviceFamily> BlocklistDevicesToDeviceFamily(
 
 static int32_t BlocklistFeatureToGfxFeature(const nsAString& aFeature) {
   MOZ_ASSERT(!aFeature.IsEmpty());
-#define GFXINFO_FEATURE(id, name, pref) \
-  if (aFeature.Equals(u##name##_ns)) {  \
-    return nsIGfxInfo::FEATURE_##id;    \
+#define GFXINFO_FEATURE_MISMATCHED(id, name, pref) \
+  if (aFeature.Equals(u## #name##_ns)) {           \
+    return nsIGfxInfo::FEATURE_##id;               \
   }
+#define GFXINFO_FEATURE(id, pref)      \
+  if (aFeature.Equals(u## #id##_ns)) { \
+    return nsIGfxInfo::FEATURE_##id;   \
+  }
+#define GFXINFO_FEATURE_ALLOWLIST(id, pref) GFXINFO_FEATURE(id, pref)
+#define GFXINFO_FEATURE_RETIRED(id, pref)
 #include "mozilla/widget/GfxInfoFeatureDefs.inc"
 #undef GFXINFO_FEATURE
+#undef GFXINFO_FEATURE_RETIRED
+#undef GFXINFO_FEATURE_ALLOWLIST
+#undef GFXINFO_FEATURE_MISMATCHED
 
   
   
@@ -295,7 +310,7 @@ static void GfxFeatureStatusToBlocklistFeatureStatus(int32_t aStatus,
     aStatusOut.Assign(u## #id##_ns); \
     break;
 #include "mozilla/widget/GfxInfoFeatureStatusDefs.inc"
-#undef GFXINFO_FEATURE
+#undef GFXINFO_FEATURE_STATUS
     default:
       MOZ_ASSERT_UNREACHABLE("Unexpected feature status!");
       break;
@@ -531,6 +546,11 @@ GfxInfoBase::SpoofMonitorInfo(uint32_t aScreenCount, int32_t aMinRefreshRate,
 NS_IMETHODIMP
 GfxInfoBase::GetFeatureStatus(int32_t aFeature, nsACString& aFailureId,
                               int32_t* aStatus) {
+  if (IsFeatureRetired(aFeature)) {
+    MOZ_ASSERT_UNREACHABLE("Checking retired feature!");
+    return NS_ERROR_INVALID_ARG;
+  }
+
   
 #if defined(RELEASE_OR_BETA)
   int32_t blocklistAll = 0;
@@ -601,6 +621,9 @@ nsTArray<gfx::GfxInfoFeatureStatus> GfxInfoBase::GetAllFeatures() {
     InitFeatureStatus(new nsTArray<gfx::GfxInfoFeatureStatus>());
     for (int32_t i = nsIGfxInfo::FEATURE_START; i < nsIGfxInfo::FEATURE_COUNT;
          ++i) {
+      if (IsFeatureRetired(i)) {
+        continue;
+      }
       int32_t status = nsIGfxInfo::FEATURE_STATUS_INVALID;
       nsAutoCString failureId;
       GetFeatureStatus(i, failureId, &status);
@@ -618,16 +641,6 @@ nsTArray<gfx::GfxInfoFeatureStatus> GfxInfoBase::GetAllFeatures() {
     features.AppendElement(copy);
   }
   return features;
-}
-
-inline bool MatchingAllowStatus(int32_t aStatus) {
-  switch (aStatus) {
-    case nsIGfxInfo::FEATURE_ALLOW_ALWAYS:
-    case nsIGfxInfo::FEATURE_ALLOW_QUALIFIED:
-      return true;
-    default:
-      return false;
-  }
 }
 
 
@@ -1052,10 +1065,6 @@ bool GfxInfoBase::DoesDriverVendorMatch(const nsAString& aBlocklistVendor,
              nsCaseInsensitiveStringComparator);
 }
 
-bool GfxInfoBase::IsFeatureAllowlisted(int32_t aFeature) const {
-  return aFeature == nsIGfxInfo::FEATURE_HW_DECODED_VIDEO_ZERO_COPY;
-}
-
 nsresult GfxInfoBase::GetFeatureStatusImpl(
     int32_t aFeature, int32_t* aStatus, nsAString& aSuggestedVersion,
     const nsTArray<RefPtr<GfxDriverInfo>>& aDriverInfo, nsACString& aFailureId,
@@ -1191,6 +1200,10 @@ void GfxInfoBase::EvaluateDownloadedBlocklist(
   
   for (int feature = nsIGfxInfo::FEATURE_START;
        feature < nsIGfxInfo::FEATURE_COUNT; ++feature) {
+    if (IsFeatureRetired(feature)) {
+      continue;
+    }
+
     int32_t status = nsIGfxInfo::FEATURE_STATUS_UNKNOWN;
     nsCString failureId;
     nsAutoString suggestedVersion;
