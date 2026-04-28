cc5103e133b28ee300b897f718476503bed78c36
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-04-28 12:54:49 +0000

    Revert "Bug 2009064 - Update urlbar icon as user types. r=daleharvey,urlbar-reviewers" for causing bc and TV failures at UrlbarIcons
    This reverts commit f8ddca38475212d80c04d52a12200d8c556e8c00.
    
    UltraBlame original commit: 492dd9b410365e00c940e090cf0b510d0eda773b

--- browser/components/urlbar/content/UrlbarInput.mjs
+++ browser/components/urlbar/content/UrlbarInput.mjs
@@ -1105,11 +1105,6 @@ ${
       }
     }
 
-    if (lazy.UrlbarPrefs.get("unifiedSearchButton.always")) {
-      // When user presses enter after any search, udpate icon to be its default (magnifying glass)
-      this.searchModeSwitcher?.updateSearchIcon();
-    }
-
     this.handleNavigation({ event });
   }
 
