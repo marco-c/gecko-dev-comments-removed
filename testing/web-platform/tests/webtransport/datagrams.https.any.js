f6a6912b367b67974d479a3a4aa42699fe4db0ae
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-04-28 13:31:44 +0000

    Bug 2032514 [wpt PR 58541] - WebTransport defines incomingMaxAge/outgoingMaxAge initial values to be null, a=testonly
    Automatic update from web-platform-tests
    WebTransport defines incomingMaxAge/outgoingMaxAge initial values to be null
    
    Per https://github.com/w3c/webtransport/commit/7292d72165673ac953d8790dc1852da569edf16c,
    incomingMaxAge and outgoingMaxAge are now nullable again with null as
    the initial value. Setting to 0 also maps to null per the setter
    algorithm.
    
    --
    
    wpt-commits: 7d200ef4bbfad0c5061186251ed10cb5e4ba1f88
    wpt-pr: 58541
    
    UltraBlame original commit: 6facbced4842d01c9a85923dd859fc8aaf6d52d3

--- testing/web-platform/tests/webtransport/datagrams.https.any.js
+++ testing/web-platform/tests/webtransport/datagrams.https.any.js
@@ -2,7 +2,6 @@
 
 
 
-
 async function write_datagrams(writer, signal) {
   const encoder = new TextEncoder();
   let counter = 0;
@@ -340,8 +339,8 @@ promise_test(async t => {
   const wt = new WebTransport(webtransport_url('echo.py'));
   await wt.ready;
 
-  assert_equals(wt.datagrams.incomingMaxAge, Infinity);
-  assert_equals(wt.datagrams.outgoingMaxAge, Infinity);
+  assert_equals(wt.datagrams.incomingMaxAge, null);
+  assert_equals(wt.datagrams.outgoingMaxAge, null);
 
   wt.datagrams.incomingMaxAge = 5;
   assert_equals(wt.datagrams.incomingMaxAge, 5);
@@ -354,9 +353,9 @@ promise_test(async t => {
   assert_throws_js(RangeError, () => { wt.datagrams.outgoingMaxAge = NaN; });
 
   wt.datagrams.incomingMaxAge = 0;
-  assert_equals(wt.datagrams.incomingMaxAge, Infinity);
+  assert_equals(wt.datagrams.incomingMaxAge, null);
   wt.datagrams.outgoingMaxAge = 0;
-  assert_equals(wt.datagrams.outgoingMaxAge, Infinity);
+  assert_equals(wt.datagrams.outgoingMaxAge, null);
 }, 'Datagram MaxAge getters/setters work correctly');
 
 promise_test(async t => {
