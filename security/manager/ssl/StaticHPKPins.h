43c7a273ca3a8b7793c5d09e1722a75c8bc96d2b
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-04-28 13:56:42 +0000

    No Bug, mozilla-central repo-update HSTS HPKP - r=release-managers,RyanVM
    Differential Revision: https://phabricator.services.mozilla.com/D296456
    
    UltraBlame original commit: db72bb5feba67fda98ad9831e70b227c4142e2dd

--- security/manager/ssl/StaticHPKPins.h
+++ security/manager/ssl/StaticHPKPins.h
@@ -681,4 +681,4 @@ static const TransportSecurityPreload kPublicKeyPinningPreloadList[] = {
 
 static const int32_t kUnknownId = -1;
 
-static const PRTime kPreloadPKPinsExpirationTime = INT64_C(1784319347106000);
+static const PRTime kPreloadPKPinsExpirationTime = INT64_C(1785532325967000);
