c8d71f4d264cf931afbf185473a61b51e40366fc
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-04-28 14:09:31 +0000

    Bug 2013481 - [devtools] Update the Autocompletion API's to support CM6 r=devtools-reviewers,nchevobbe
    Differential Revision: https://phabricator.services.mozilla.com/D290766
    
    UltraBlame original commit: e239d8e5522bd3d1424ddd7ca7d41d6a8ca6497f

--- devtools/client/webconsole/components/Input/JSTerm.js
+++ devtools/client/webconsole/components/Input/JSTerm.js
@@ -948,7 +948,7 @@ class JSTerm extends Component {
       ? "column"
       : "ch";
     const isAddedText =
-      from.line === to.line && from[col] === to[col] && origin === "+input";
+      from.line === to.line && from[col] == to[col] && origin === "+input";
 
     
     
