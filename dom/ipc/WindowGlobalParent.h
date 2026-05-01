c1d17d06b316cde87411d2a80e486c4f3365d03c
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-05-01 01:12:52 +0000

    Bug 2034654: Add profiler marker for pages not going into the bfcache. r=smaug
    Differential Revision: https://phabricator.services.mozilla.com/D296257
    
    UltraBlame original commit: 4cd9ed3c1a172c4fc6e1c89e2db8f35563a9072a

--- dom/ipc/WindowGlobalParent.h
+++ dom/ipc/WindowGlobalParent.h
@@ -477,6 +477,8 @@ class WindowGlobalParent final : public WindowContext,
   bool mShouldReportHasBlockedOpaqueResponse = false;
 };
 
+nsCString BFCacheStatusToString(uint32_t aFlags);
+
 }  
 }  
 
