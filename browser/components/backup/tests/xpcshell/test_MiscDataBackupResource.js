0f91ebf4b81bbce2d8c1391dd85335b0e295d9c9
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-05-01 01:27:25 +0000

    Bug 2032641 - don't backup times json. r=mconley
    Differential Revision: https://phabricator.services.mozilla.com/D295358
    
    UltraBlame original commit: cc447d38952be3ae097245e3fb92b27d114806b2

--- browser/components/backup/tests/xpcshell/test_MiscDataBackupResource.js
+++ browser/components/backup/tests/xpcshell/test_MiscDataBackupResource.js
@@ -11,10 +11,6 @@ const { ASRouterStorage } = ChromeUtils.importESModule(
   "resource:///modules/asrouter/ASRouterStorage.sys.mjs"
 );
 
-const { ProfileAge } = ChromeUtils.importESModule(
-  "resource://gre/modules/ProfileAge.sys.mjs"
-);
-
 
 
 
@@ -176,14 +172,6 @@ add_task(async function test_recover() {
     "MiscDataBackupResource-test-profile"
   );
 
-  
-  
-  let originalProfileAge = await ProfileAge(PathUtils.profileDir);
-  await originalProfileAge.computeAndPersistCreated();
-  Assert.ok(
-    await IOUtils.exists(PathUtils.join(PathUtils.profileDir, "times.json"))
-  );
-
   const simpleCopyFiles = [
     { path: "enumerate_devices.txt" },
     { path: "protections.sqlite" },
@@ -222,27 +210,6 @@ add_task(async function test_recover() {
       PathUtils.join(destProfilePath, SNIPPETS_BACKUP_FILE)
     )),
     "Snippets backup data should not have gone into the profile directory"
-  );
-
-  
-  
-  Assert.ok(
-    await IOUtils.exists(PathUtils.join(destProfilePath, "times.json"))
-  );
-  let copiedProfileAge = await ProfileAge(destProfilePath);
-  Assert.equal(
-    await originalProfileAge.created,
-    await copiedProfileAge.created,
-    "Created timestamp should match."
-  );
-  Assert.equal(
-    await originalProfileAge.firstUse,
-    await copiedProfileAge.firstUse,
-    "First use timestamp should match."
-  );
-  Assert.ok(
-    await copiedProfileAge.recoveredFromBackup,
-    "Backup recovery timestamp should have been set."
   );
 
   await maybeRemovePath(recoveryPath);
