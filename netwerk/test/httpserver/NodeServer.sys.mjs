c577c5b71091bc4ff6fab232978555ee96ea8412
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-05-01 01:13:13 +0000

    Bug 2017712 - Fix nodeServer cert path discovery r=necko-reviewers,jesup
    For mochitests the relative path to the http2 cert is slightly different.
    This change tries multiple paths to find the location of the cert.
    
    Differential Revision: https://phabricator.services.mozilla.com/D295844
    
    UltraBlame original commit: e69a44861479ab12aa18d010a368e0ea5cc4d0ac

--- netwerk/test/httpserver/NodeServer.sys.mjs
+++ netwerk/test/httpserver/NodeServer.sys.mjs
@@ -157,11 +157,27 @@ export class BaseNodeServer {
       rootDir = parent;
     }
 
-    let certFile = rootDir.clone();
-    certFile.append("netwerk");
-    certFile.append("test");
-    certFile.append("unit");
-    certFile.append(filename);
+    function findCertPath(dir) {
+      let candidates = [
+        `netwerk/test/unit/${filename}`,
+        // This one works for mochitests
+        `_tests/xpcshell/netwerk/test/unit/${filename}`,
+      ];
+      for (let candidate of candidates) {
+        let certpath = dir.clone().QueryInterface(Ci.nsIFile);
+        certpath.appendRelativePath(candidate);
+        if (certpath.exists()) {
+          return certpath;
+        }
+      }
+      return null;
+    }
+
+    let certFile = findCertPath(rootDir);
+    if (!certFile) {
+      console.log(`Error installing cert: file not found.}`);
+      return;
+    }
 
     try {
       let pem = readFile(certFile)
@@ -171,7 +187,7 @@ export class BaseNodeServer {
       certdb.addCertFromBase64(pem, "CTu,u,u");
     } catch (e) {
       let errStr = e.toString();
-      console.log(`Error installing cert ${errStr}`);
+      console.log(`Error installing cert ${errStr} path:${certFile.path}`);
       if (errStr.includes("0x805a1fe8")) {
         // Can't install the cert without a profile
         // Let's show an error, otherwise this will be difficult to diagnose.
