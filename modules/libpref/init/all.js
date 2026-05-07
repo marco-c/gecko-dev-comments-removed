5e764ddccbb3e23a3f7e4d013e49adbd29a1d700
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-05-07 02:25:12 +0000

    Bug 2036853 - Revise toolkit.zoomManager.zoomValues to allow for more fine-grained zooming. r=mconley
    Summary of changes:
    
    - Filled in missing 0.1 steps in the 1.0–1.7 range
    - Removed non-round values: .67 and 1.33
    - Extended low end: added .2, .4, and more granularity down to .3
    - Extended upper range: added 1.8, 1.9, 2.2, 2.6, 2.8 for finer steps between 1.7–3.0
    
    This gives us three clean bands: 0.1 steps (0.2–2.0), 0.2 steps (2.0–3.0), 1.0 steps (3.0–5.0)
    
    Differential Revision: https://phabricator.services.mozilla.com/D298319
    
    UltraBlame original commit: 1a43ee23f657f8fc0b95951b436c936f66937d04

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -3049,7 +3049,7 @@ pref("browser.formfill.prefixWeight",     5);
 
 
 pref("browser.zoom.full", false);
-pref("toolkit.zoomManager.zoomValues", ".3,.5,.67,.8,.9,1,1.1,1.2,1.33,1.5,1.7,2,2.4,3,4,5");
+pref("toolkit.zoomManager.zoomValues", ".2,.3,.4,.5,.6,.7,.8,.9,1,1.1,1.2,1.3,1.4,1.5,1.6,1.7,1.8,1.9,2,2.2,2.4,2.6,2.8,3,4,5");
 
 
 
