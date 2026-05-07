e1bc2e39d4c683c9483e84e9c4612e2ec073edd1
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-05-07 02:44:36 +0000

    Bug 2034462 - Refine `ApplicationExitInfo` reason mapping to identify MemoryLimiter exits. r=android-reviewers,kaya
    - Refactored `toProcessExitReason` to accept the full `ApplicationExitInfo` object instead of just an integer reason code.
    - Added a check for "MemoryLimiter:AnonSwap" within the exit description when the reason is `REASON_OTHER`, mapping it to a new "memory_limiter" metric value.
    
    Differential Revision: https://phabricator.services.mozilla.com/D297002
    
    UltraBlame original commit: 647808860f2aea37176bad7df533043501f9fa88

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/perf/ApplicationExitInfoMetrics.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/perf/ApplicationExitInfoMetrics.kt
@@ -17,6 +17,7 @@ import androidx.core.content.edit
 import org.mozilla.fenix.GleanMetrics.AppExitInfo
 import org.mozilla.fenix.R
 import org.mozilla.fenix.ext.getPreferenceKey
+import org.mozilla.fenix.perf.ApplicationExitInfoMetrics.recordProcessExits
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
@@ -56,7 +57,7 @@ object ApplicationExitInfoMetrics {
         getHistoricalProcessExits(context).map { exit ->
             ProcessExitRecord(
                 date = exit.timestamp.toDateTimeFormat(),
-                reason = exit.reason.toProcessExitReason() ?: "unknown",
+                reason = exit.toProcessExitReason() ?: "unknown",
                 processType = exit.processName.toProcessType(),
                 importance = exit.importance.toProcessImportance() ?: "unknown",
                 pssInMb = exit.pss.toValueInMB(),
@@ -122,7 +123,7 @@ object ApplicationExitInfoMetrics {
                         processType = historicalExit.processName.toProcessType(),
                         pss = historicalExit.pss.toValueInMB(),
                         rss = historicalExit.rss.toValueInMB(),
-                        reason = historicalExit.reason.toProcessExitReason(),
+                        reason = historicalExit.toProcessExitReason(),
                     ),
                 )
             }
@@ -188,15 +189,22 @@ object ApplicationExitInfoMetrics {
         return (this / KILOBYTES_TO_MEGABYTES_CONVERSION).toInt()
     }
 
-    private fun Int.toProcessExitReason(): String? {
-        return when (this) {
+    @RequiresApi(Build.VERSION_CODES.R)
+    private fun ApplicationExitInfo.toProcessExitReason(): String? {
+        return when (reason) {
             ApplicationExitInfo.REASON_ANR -> "anr"
             ApplicationExitInfo.REASON_CRASH -> "crash"
             ApplicationExitInfo.REASON_CRASH_NATIVE -> "crash_native"
             ApplicationExitInfo.REASON_LOW_MEMORY -> "low_memory"
             ApplicationExitInfo.REASON_EXCESSIVE_RESOURCE_USAGE -> "excessive_resource"
             ApplicationExitInfo.REASON_SIGNALED -> "signaled"
-            ApplicationExitInfo.REASON_OTHER -> "other"
+            ApplicationExitInfo.REASON_OTHER ->
+                if (description?.contains("MemoryLimiter:AnonSwap") == true) {
+                    "memory_limiter"
+                } else {
+                    "other"
+                }
+
             else -> null
         }
     }
