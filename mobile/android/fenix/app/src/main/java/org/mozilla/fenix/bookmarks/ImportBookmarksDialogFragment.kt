254980b559b97d7473fbd6adbb436ddb08890738
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-05-07 02:25:44 +0000

    Bug 2035948 - Adds string for the default import bookmark folder name r=android-reviewers,android-l10n-reviewers,flod,sfamisa
    Differential Revision: https://phabricator.services.mozilla.com/D297524
    
    UltraBlame original commit: fe1cec836d0e794040080d03e50bf80d12f3f497

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/bookmarks/ImportBookmarksDialogFragment.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/bookmarks/ImportBookmarksDialogFragment.kt
@@ -10,8 +10,15 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.compose.content
+import mozilla.appservices.places.BookmarkRoot
+import mozilla.components.concept.bookmark.parser.BookmarksFileParser
+import mozilla.components.concept.bookmarks.file.BookmarksFileImporter
 import mozilla.components.feature.importer.BookmarkImporter
 import mozilla.components.feature.importer.ImporterResult
+import mozilla.components.lib.bookmark.parser.jsoup.jsoupParser
+import mozilla.components.lib.bookmarks.file.htmlImporter
+import org.mozilla.fenix.R
+import org.mozilla.fenix.ext.requireComponents
 
 internal class ImportBookmarksDialogFragment : DialogFragment() {
     override fun onCreateView(
@@ -20,6 +27,14 @@ internal class ImportBookmarksDialogFragment : DialogFragment() {
         savedInstanceState: Bundle?,
     ): View = content {
         BookmarkImporter(
+            importer = BookmarksFileImporter.htmlImporter(
+                context = requireContext(),
+                parentGuid = BookmarkRoot.Mobile.id,
+                parser = BookmarksFileParser.jsoupParser(
+                    rootFolderName = requireContext().getString(R.string.bookmark_import_destination_default_name),
+                ),
+                inserter = requireComponents.core.bookmarksStorage,
+            ),
             onFinished = { result ->
                 parentFragmentManager.setFragmentResult(
                     REQUEST_KEY,
@@ -42,7 +57,7 @@ internal class ImportBookmarksDialogFragment : DialogFragment() {
             when (bundle.getString(KEY_RESULT)) {
                 RESULT_SUCCESS -> ImporterResult.Success(importCount = 0)
                 RESULT_FAILURE -> ImporterResult.Failure
-                RESULT_CANCELLED -> ImporterResult.Cancelled
+                RESULT_CANCELLED -> ImporterResult.Canceled
                 else -> null
             }
     }
@@ -51,5 +66,5 @@ internal class ImportBookmarksDialogFragment : DialogFragment() {
 private fun ImporterResult.encode(): String = when (this) {
     is ImporterResult.Success -> ImportBookmarksDialogFragment.RESULT_SUCCESS
     ImporterResult.Failure -> ImportBookmarksDialogFragment.RESULT_FAILURE
-    ImporterResult.Cancelled -> ImportBookmarksDialogFragment.RESULT_CANCELLED
+    ImporterResult.Canceled -> ImportBookmarksDialogFragment.RESULT_CANCELLED
 }
