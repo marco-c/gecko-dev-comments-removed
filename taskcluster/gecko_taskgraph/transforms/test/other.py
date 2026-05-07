e6644d5f77392e4526a18e75b71f69eb76800611
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-05-07 01:57:58 +0000

    Revert "Bug 2028933 - [geckodriver] Update releasing docs for universal builds on macOS. r=geckodriver-reviewers,jgraham" for causing performance regressions
    This reverts commit a462394bbf580063a6b19c34cf5f860d645a8062.
    
    Revert "Bug 2028933 - [geckodriver] Create universal builds for geckodriver. r=taskgraph-reviewers,releng-reviewers,ahal"
    
    This reverts commit 124a5ef03badb78ceac1c210363d4719ffefec24.
    
    UltraBlame original commit: 5947d71c524f317d2fda72d1ead387dcc9c05358

--- taskcluster/gecko_taskgraph/transforms/test/other.py
+++ taskcluster/gecko_taskgraph/transforms/test/other.py
@@ -339,10 +339,20 @@ def setup_browsertime(config, tasks):
             "by-test-platform": {
                 "android.*": ["browsertime", "linux64-geckodriver", "linux64-node"],
                 "linux.*": ["browsertime", "linux64-geckodriver", "linux64-node"],
-                "macosx.*": [
+                "macosx1470.*": [
                     "browsertime",
                     "macosx64-geckodriver",
                     "macosx64-node",
+                ],
+                "macosx1400.*": [
+                    "browsertime",
+                    "macosx64-aarch64-geckodriver",
+                    "macosx64-aarch64-node",
+                ],
+                "macosx1500.*": [
+                    "browsertime",
+                    "macosx64-aarch64-geckodriver",
+                    "macosx64-aarch64-node",
                 ],
                 "windows.*aarch64.*": [
                     "browsertime",
