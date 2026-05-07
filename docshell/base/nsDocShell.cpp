c351c160ce9189c3c4634a76cc88a8b0d4a34e34
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-05-07 02:36:54 +0000

    Bug 2035423 - Revert setting CSP context already on document creation. r=tschuster
    StartDocumentLoad calls SetRequestContextWithDocument. Since bug 543435, the
    initial about:blank load doesn't go through this code anymore. Therefore, we
    added that call also to CreateAboutBlankDocumentViewer. This is advantageous
    in general too, because transient documents will have the correct context.
    But due to bug 1899512, there are regressions.
    
    This change reverts setting the request context already at document creation.
    Instead we match regular loads and set the request context in the corresponding
    CompleteInitialAboutBlankLoad.
    
    Differential Revision: https://phabricator.services.mozilla.com/D297337
    
    UltraBlame original commit: b72b0e23a88c46eebc424c46b5b01782a8904ce8

--- docshell/base/nsDocShell.cpp
+++ docshell/base/nsDocShell.cpp
@@ -6678,14 +6678,6 @@ nsresult nsDocShell::CreateAboutBlankDocumentViewer(
         rv = Embed(viewer, aActor, true, nullptr, mCurrentURI);
         NS_ENSURE_SUCCESS(rv, rv);
 
-        if (nsIContentSecurityPolicy* csp =
-                PolicyContainer::GetCSP(blankDoc->GetPolicyContainer())) {
-          
-          
-          
-          MOZ_TRY(csp->SetRequestContextWithDocument(blankDoc));
-        }
-
         SetCurrentURI(blankDoc->GetDocumentURI(), nullptr,
                        true,
                        aIsInitialDocument,
@@ -10097,6 +10089,13 @@ nsresult nsDocShell::CompleteInitialAboutBlankLoad(
   MOZ_ASSERT(doc->GetReadyStateEnum() == Document::READYSTATE_COMPLETE);
   MOZ_ASSERT(!mIsLoadingDocument);
 
+  if (nsIContentSecurityPolicy* csp =
+          PolicyContainer::GetCSP(doc->GetPolicyContainer())) {
+    
+    
+    
+    MOZ_TRY(csp->SetRequestContextWithDocument(doc));
+  }
   doc->ApplyCspFromLoadInfo(aLoadInfo);
   doc->ApplySettingsFromCSP(false);
   doc->RecomputeResistFingerprinting();
