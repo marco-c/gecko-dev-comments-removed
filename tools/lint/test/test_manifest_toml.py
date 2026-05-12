d7c02302404bc5a5d5e71fce32cacc68da9031e2
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-05-12 13:40:02 +0000

    Bug 2038165 - Use an impossible API version in test_unknown_android_version to avoid breakage when new versions are added. r=linter-reviewers,ahal
    Differential Revision: https://phabricator.services.mozilla.com/D299346
    
    UltraBlame original commit: b904560a7711cc0c59e4c8d903c03a775e278b75

--- tools/lint/test/test_manifest_toml.py
+++ tools/lint/test/test_manifest_toml.py
@@ -6,6 +6,7 @@ from pathlib import Path
 
 import mozunit
 import pytest
+from mozinfo.platforminfo import android_os_to_api_map
 
 LINTER = "test-manifest-toml"
 fixed = 0
@@ -286,13 +287,13 @@ def test_android_os_mismatch(lint, paths, create_temp_file):
 def test_unknown_android_version(lint, paths, create_temp_file):
     """Test unknown android_version"""
 
-    contents = "[DEFAULT]\nskip-if = [\"android_version == '37'\"]"
+    contents = "[DEFAULT]\nskip-if = [\"android_version == '999'\"]"
     path = create_temp_file(contents, "unknown_android.toml")
     with pytest.raises(Exception) as e:
-        _results = lint([path], fix=True)
-    assert (
-        str(e.value)
-        == "Unknown Android API version '37'. Supported versions are dict_values(['24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36'])."
+        lint([path], fix=True)
+    assert str(e.value) == (
+        f"Unknown Android API version '999'. "
+        f"Supported versions are {android_os_to_api_map.values()}."
     )
 
 
