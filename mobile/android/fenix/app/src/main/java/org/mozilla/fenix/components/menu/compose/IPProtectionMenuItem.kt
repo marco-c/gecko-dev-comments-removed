c195b458ee40a87449b467905656501eef7e5409
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-05-12 13:37:38 +0000

    Bug 2038067 - Part 2: Add a separator the ip protection menu item r=android-reviewers,jonalmeida
    Differential Revision: https://phabricator.services.mozilla.com/D299275
    
    UltraBlame original commit: f614ff752c20a33f2ea8dac58b46355bf09028ad

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/components/menu/compose/IPProtectionMenuItem.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/components/menu/compose/IPProtectionMenuItem.kt
@@ -15,11 +15,13 @@ import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -108,6 +110,14 @@ internal fun IPProtectionMenuItem(
                 state = badgeState(state.status),
             )
         }
+
+        VerticalDivider(
+            modifier = Modifier
+                .fillMaxHeight()
+                .padding(vertical = FirefoxTheme.layout.space.static100)
+                .width(1.dp),
+            color = MaterialTheme.colorScheme.outlineVariant,
+        )
 
         Box(
             modifier = Modifier
