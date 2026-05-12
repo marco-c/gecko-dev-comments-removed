37393b5e82767b0d71f737462ea241445f0397fe
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-05-12 13:55:31 +0000

    Bug 2022156 - rust storage primary password support r=dimi,fluent-reviewers,bolsson,mtigley
    Differential Revision: https://phabricator.services.mozilla.com/D286746
    
    UltraBlame original commit: d995523bf6f25eef9a75d9446cf174d1c137043d

--- browser/components/aboutlogins/tests/browser/browser_primaryPassword.js
+++ browser/components/aboutlogins/tests/browser/browser_primaryPassword.js
@@ -18,6 +18,10 @@ function waitForLoginCountToReach(browser, loginCount) {
 }
 
 add_setup(async function () {
+  
+  await SpecialPowers.pushPrefEnv({
+    set: [["signon.rustMirror.enabled", false]],
+  });
   await addLogin(TEST_LOGIN1);
 
   
@@ -28,6 +32,7 @@ add_setup(async function () {
   registerCleanupFunction(async () => {
     await Services.logins.removeAllUserFacingLoginsAsync();
     LoginTestUtils.primaryPassword.disable();
+    await SpecialPowers.popPrefEnv();
   });
 });
 
