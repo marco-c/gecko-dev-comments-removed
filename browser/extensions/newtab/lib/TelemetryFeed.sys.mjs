ad3fd92ff3e79003c652f2881c62e1b2784efb22
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-05-12 14:07:34 +0000

    Bug 2015530 - Add widgets_enabled_list string_list telemetry metric. r=home-newtab-reviewers,mconley
    Differential Revision: https://phabricator.services.mozilla.com/D298438
    
    UltraBlame original commit: 6f837afc8fe5225660e1c5509d960bde11ad1d01

--- browser/extensions/newtab/lib/TelemetryFeed.sys.mjs
+++ browser/extensions/newtab/lib/TelemetryFeed.sys.mjs
@@ -19,6 +19,7 @@ import {
   actionTypes as at,
   actionUtils as au,
 } from "resource://newtab/common/Actions.mjs";
+import { WIDGET_REGISTRY } from "resource://newtab/common/WidgetsRegistry.mjs";
 import { Prefs } from "resource://newtab/lib/ActivityStreamPrefs.sys.mjs";
 import { classifySite } from "resource://newtab/lib/SiteClassifier.sys.mjs";
 
@@ -1592,6 +1593,7 @@ export class TelemetryFeed {
         break;
       case at.PREFS_INITIAL_VALUES:
         this.initializeGleanSession();
+        this.recordEnabledWidgets();
         break;
     }
   }
@@ -1721,7 +1723,20 @@ export class TelemetryFeed {
       }
 
       Glean.newtab.widgetsEnabled.record(payload);
+      this.recordEnabledWidgets();
     }
+  }
+
+  recordEnabledWidgets() {
+    const prefs = this.store?.getState()?.Prefs.values;
+    if (!prefs) {
+      return;
+    }
+    Glean.newtab.widgetsEnabledList.set(
+      WIDGET_REGISTRY.filter(w => prefs[w.enabledPref]).map(
+        w => w.telemetryName
+      )
+    );
   }
 
   handleWidgetsHideAll(action) {
@@ -2013,12 +2028,14 @@ export class TelemetryFeed {
           newtab_visit_id: session.session_id,
           display_status: action.data.value,
         });
+        this.recordEnabledWidgets();
         break;
       case "widgets.focusTimer.enabled":
         Glean.newtab.widgetsTimerChangeDisplay.record({
           newtab_visit_id: session.session_id,
           display_status: action.data.value,
         });
+        this.recordEnabledWidgets();
         break;
     }
   }
