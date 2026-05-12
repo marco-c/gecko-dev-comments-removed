aa8c784c9f5a83dde865e309e4dda5718de93685
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-05-12 14:03:23 +0000

    Revert "Bug 2031837 - Add new net error page illustrations with per-illustration sizing - r=niklas,rking,desktop-theme-reviewers,dao,gfx-reviewers,lsalzman" for causing reftest failures at reftests/font-face
    This reverts commit 55c035a4dc2103cd610df6d8eb34caa7b8922359.
    
    UltraBlame original commit: fc8be7aeb554bbed5a3fe65fc838c14df109cdef

--- toolkit/content/net-error-card.mjs
+++ toolkit/content/net-error-card.mjs
@@ -27,7 +27,7 @@ import {
   getResolvedErrorConfig,
   resolveErrorID,
 } from "chrome://global/content/errors/error-lookup.mjs";
-import { html, ifDefined } from "chrome://global/content/vendor/lit.all.mjs";
+import { html } from "chrome://global/content/vendor/lit.all.mjs";
 import { MozLitElement } from "chrome://global/content/lit-utils.mjs";
 import { NET_ERROR_ILLUSTRATIONS } from "chrome://global/content/errors/net-error-illustrations.mjs";
 import "chrome://global/content/elements/moz-button-group.mjs";
@@ -1053,8 +1053,7 @@ export class NetErrorCard extends MozLitElement {
     }
 
     const { bodyTitleL10nId, image } = this.errorConfig;
-    const { src, alt, className } =
-      image ?? NET_ERROR_ILLUSTRATIONS.securityError;
+    const { src, alt } = image ?? NET_ERROR_ILLUSTRATIONS.securityError;
     const title = bodyTitleL10nId ?? "fp-certerror-body-title";
 
     return html`<link
@@ -1067,12 +1066,7 @@ export class NetErrorCard extends MozLitElement {
         aria-describedby="error-intro whatCanYouDo"
       >
         <div class="img-container">
-          <img
-            src=${src}
-            class=${ifDefined(className)}
-            data-l10n-id=${alt}
-            data-l10n-attrs="alt"
-          />
+          <img src=${src} data-l10n-id=${alt} data-l10n-attrs="alt" />
         </div>
         <div class="container">
           ${this.showCustomNetErrorCard
