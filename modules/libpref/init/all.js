38bcf313e3490d67e1ff9fafa9d4f5e678e04fca
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-05-12 12:56:51 +0000

    Revert "Bug 2037099 - Remove GLS prefs from Windows r=handyman,firefox-build-system-reviewers,sergesanspaille" for causing xpc failures at test_urlformatter.js
    This reverts commit c13f1c29e14307e805b667031ab0f9f276cc3c22.
    
    UltraBlame original commit: 16010453983d67a1affa4f11fc856e61e9fbe9b9

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -3072,7 +3072,7 @@ pref("network.tcp.keepalive.idle_time", 600);
 
 
 
-#if !defined(ANDROID) && !defined(XP_WIN)
+#ifndef ANDROID
   pref("geo.provider.network.url", "https://www.googleapis.com/geolocation/v1/geolocate?key=%GOOGLE_LOCATION_SERVICE_API_KEY%&solution_channel=%OS%");
 
   
