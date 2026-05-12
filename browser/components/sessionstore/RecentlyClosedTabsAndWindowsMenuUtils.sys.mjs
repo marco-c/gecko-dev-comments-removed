91642c914fd4bf653046da63dd9471dd68311d7b
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-05-12 13:42:57 +0000

    Revert "Bug 2023691 - Nova tab group color token updates r=sthompson,desktop-theme-reviewers,tabbrowser-reviewers,urlbar-reviewers,mstriemer,adw,dao" for causing bc failures at tab-group-blue-content-invert
    This reverts commit 5697d7e6fb48e6f85a2ce105b0e659e71feae4b2.
    
    UltraBlame original commit: 3a0d55e7318e1bdfa20bbd1f2431787995fe5ea1

--- browser/components/sessionstore/RecentlyClosedTabsAndWindowsMenuUtils.sys.mjs
+++ browser/components/sessionstore/RecentlyClosedTabsAndWindowsMenuUtils.sys.mjs
@@ -331,10 +331,6 @@ function setTabGroupColorProperties(element, tabGroup) {
     "--tab-group-color-pale",
     `var(--tab-group-color-${tabGroup.color}-pale)`
   );
-  element.style.setProperty(
-    "--tab-group-background-color",
-    `var(--tab-group-${tabGroup.color})`
-  );
 }
 
 /**
