fcbbbcf491f55201d6e92ffb01c520c877237f5a
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-05-14 11:13:31 +0000

    Bug 2039058 - Enable World Cup tool call by default for Smart Window. r=tburrell
    Differential Revision: https://phabricator.services.mozilla.com/D300103
    
    UltraBlame original commit: b088183b8eb9a6406433e9fdbcf8e1c58d11d749

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -2257,7 +2257,7 @@ pref("browser.smartwindow.firstrun.explainerURL", "https://www.firefox.com/en-US
 pref("places.semanticHistory.smartwindow.featureGate", false);
 
 
-pref("browser.smartwindow.worldcup.enabled", false);
+pref("browser.smartwindow.worldcup.enabled", true);
 pref("browser.smartwindow.worldcup.endpointURL", "https://merino.services.mozilla.com");
 pref("browser.smartwindow.worldcup.timeoutMs", 2000);
 
