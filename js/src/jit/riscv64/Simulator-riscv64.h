7a2001a9389ad2b69e79b8c4428cb193fc24af84
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-05-14 11:30:03 +0000

    Bug 2039030 - Part 2: Prefer mozilla::BitwiseCast. r=iain
    The `bit_cast` function was kept to minimise the changes and because we can
    replace `mozilla::BitwiseCast` with `std::bit_cast` as soon as we require
    at least GCC11.
    
    Differential Revision: https://phabricator.services.mozilla.com/D300072
    
    UltraBlame original commit: da915159be883747158e296caab2f05a61da401f

--- js/src/jit/riscv64/Simulator-riscv64.h
+++ js/src/jit/riscv64/Simulator-riscv64.h
@@ -33,6 +33,7 @@
 #endif
 
 #include "mozilla/Atomics.h"
+#include "mozilla/Casting.h"
 
 #include <vector>
 
@@ -53,16 +54,7 @@ namespace jit {
 
 template <class Dest, class Source>
 inline Dest bit_cast(const Source& source) {
-  static_assert(sizeof(Dest) == sizeof(Source),
-                "bit_cast requires source and destination to be the same size");
-  static_assert(std::is_trivially_copyable<Dest>::value,
-                "bit_cast requires the destination type to be copyable");
-  static_assert(std::is_trivially_copyable<Source>::value,
-                "bit_cast requires the source type to be copyable");
-
-  Dest dest;
-  memcpy(&dest, &source, sizeof(dest));
-  return dest;
+  return mozilla::BitwiseCast<Dest>(source);
 }
 
 #define ASSERT_TRIVIALLY_COPYABLE(T)                  \
