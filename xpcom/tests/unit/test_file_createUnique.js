be5366b454a39f884f6bfd83284a9e1cc1d01ed6
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-05-14 11:16:00 +0000

    Revert "Bug 2029644 - Correctly check path and leaf name limits in nsLocalFile::CreateUnique. r=xpcom-reviewers,nika" for causing bc failures at browser_save_filenames.js
    This reverts commit ad9ad254277423e13c9620ba585beb09fe5e4ab0.
    
    UltraBlame original commit: 57052959fb78dbbe3705db1a948eb57026bd1134

--- xpcom/tests/unit/test_file_createUnique.js
+++ xpcom/tests/unit/test_file_createUnique.js
@@ -2,21 +2,27 @@
 
 
 
-"use strict";
-
-add_task(function test_longPath_throwsUnrecognized() {
+function run_test() {
   
+  var longLeafName = new Array(256).join("T");
+
   
   var tempFile = Services.dirsvc.get("TmpD", Ci.nsIFile);
-  var longComponent = "T".repeat(255);
-  for (let i = 0; i < 20; i++) {
-    tempFile.append(longComponent);
-  }
+  tempFile.append(longLeafName);
   tempFile.append("test.txt");
 
-  Assert.throws(
-    () => tempFile.createUnique(Ci.nsIFile.NORMAL_FILE_TYPE, 0o600),
-    /NS_ERROR_FILE_UNRECOGNIZED_PATH/,
-    "Creating an item whose path exceeds the maximum should throw"
-  );
-});
+  try {
+    tempFile.createUnique(Ci.nsIFile.NORMAL_FILE_TYPE, 0o600);
+    do_throw("Creating an item in a folder with a very long name should throw");
+  } catch (e) {
+    if (
+      !(
+        e instanceof Ci.nsIException &&
+        e.result == Cr.NS_ERROR_FILE_UNRECOGNIZED_PATH
+      )
+    ) {
+      throw e;
+    }
+    
+  }
+}
