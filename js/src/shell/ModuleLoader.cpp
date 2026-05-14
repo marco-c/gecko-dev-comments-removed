2070398c312a00c63fd4d0c49393c4633e85d0f7
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-05-14 11:12:01 +0000

    Bug 1997621 - Support wasm modules in the shell; r=arai
    Differential Revision: https://phabricator.services.mozilla.com/D297109
    
    UltraBlame original commit: 9b127dfe04b80888a89fbf5c944468ef93504977

--- js/src/shell/ModuleLoader.cpp
+++ js/src/shell/ModuleLoader.cpp
@@ -119,7 +119,8 @@ bool ModuleLoader::loadRootModule(JSContext* cx, HandleString path) {
     return false;
   }
   RootedObject moduleRequest(
-      cx, ModuleRequestObject::create(cx, specifier, JS::ModuleType::JavaScript,
+      cx, ModuleRequestObject::create(cx, specifier,
+                                      JS::ModuleType::JavaScriptOrWasm,
                                       ImportPhase::Evaluation));
   if (!moduleRequest) {
     return false;
@@ -587,6 +588,20 @@ JSObject* ModuleLoader::loadAndParse(JSContext* cx, HandleString pathArg,
   }
 
   if (module) {
+#ifdef ENABLE_SOURCE_PHASE_IMPORTS
+    
+    
+    
+    
+    
+    if (moduleRequestArg->as<ModuleRequestObject>().phase() ==
+            ImportPhase::Evaluation &&
+        module->as<ModuleObject>().moduleSource()) {
+      JS_ReportErrorNumberASCII(cx, GetErrorMessage, nullptr,
+                                JSMSG_WASM_ESM_EVAL_NOT_SUPPORTED);
+      return nullptr;
+    }
+#endif
     return module;
   }
 
@@ -618,6 +633,67 @@ JSObject* ModuleLoader::loadAndParse(JSContext* cx, HandleString pathArg,
 
     return module;
   }
+
+#ifdef ENABLE_SOURCE_PHASE_IMPORTS
+  
+  
+  if (JS::Prefs::experimental_wasm_esm_integration() &&
+      StringEndsWith(path, u".wasm")) {
+    js::ImportPhase phase = moduleRequestArg->as<ModuleRequestObject>().phase();
+    if (phase != ImportPhase::Source) {
+      JS_ReportErrorNumberASCII(cx, GetErrorMessage, nullptr,
+                                JSMSG_WASM_ESM_EVAL_NOT_SUPPORTED);
+      return nullptr;
+    }
+    RootedString resolvedPath(cx, ResolvePath(cx, path, RootRelative));
+    if (!resolvedPath) {
+      return nullptr;
+    }
+
+    UniqueChars resolvedFilename = JS_EncodeStringToUTF8(cx, resolvedPath);
+    if (!resolvedFilename) {
+      return nullptr;
+    }
+
+    FILE* file = OpenFile(cx, resolvedFilename.get(), "rb");
+    if (!file) {
+      return nullptr;
+    }
+
+    size_t fileSize;
+    if (!FileSize(cx, resolvedFilename.get(), file, &fileSize)) {
+      fclose(file);
+      return nullptr;
+    }
+
+    js::Vector<uint8_t, 0, js::MallocAllocPolicy> srcBuf;
+    if (!srcBuf.growBy(fileSize)) {
+      fclose(file);
+      ReportOutOfMemory(cx);
+      return nullptr;
+    }
+
+    if (!ReadFile(cx, resolvedFilename.get(), file,
+                  reinterpret_cast<char*>(srcBuf.begin()), fileSize)) {
+      fclose(file);
+      return nullptr;
+    }
+    fclose(file);
+
+    JS::CompileOptions options(cx);
+    options.setFileAndLine(filename.get(), 1);
+    module = JS::CompileWasmModuleAsSource(cx, options, srcBuf);
+    if (!module) {
+      return nullptr;
+    }
+
+    if (!addModuleToRegistry(cx, moduleType, path, module)) {
+      return nullptr;
+    }
+
+    return module;
+  }
+#endif
 
   JS::CompileOptions options(cx);
   options.setFileAndLine(filename.get(), 1);
@@ -651,7 +727,7 @@ JSObject* ModuleLoader::loadAndParse(JSContext* cx, HandleString pathArg,
     return nullptr;
   }
 
-  if (moduleType == JS::ModuleType::JavaScript) {
+  if (moduleType == JS::ModuleType::JavaScriptOrWasm) {
     module = JS::CompileModule(cx, options, srcBuf);
     if (!module) {
       return nullptr;
