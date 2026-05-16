d25802fe8abddcfef6fe77b610b6f3d7f42d962e
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-05-16 00:43:44 +0000

    Revert "Bug 2038222 - 'Reload all tabs' button is not triggered after switching the ETP from Standard to Strict - r=hjones" for causing bc failures  browser_privacy_status_card.js
    This reverts commit f4096d77675eabb9f12359ee60f133da6974b4f5.
    
    UltraBlame original commit: 1cb7e291af8d7dcc6cdbd112551f44b8cd1cf30c

--- browser/components/preferences/config/privacy.mjs
+++ browser/components/preferences/config/privacy.mjs
@@ -1242,18 +1242,6 @@ SettingGroupManager.registerGroups({
           },
         ],
       },
-      {
-        id: "reloadTabsHint",
-        control: "moz-message-bar",
-        l10nId: "preferences-etp-reload-tabs-hint",
-        options: [
-          {
-            control: "moz-button",
-            l10nId: "preferences-etp-reload-tabs-hint-button",
-            slot: "actions",
-          },
-        ],
-      },
     ],
   },
   etpCustomize: {
@@ -3473,9 +3461,6 @@ Preferences.addSetting({
 
     return config;
   },
-  onUserChange() {
-    PrivacySettingHelpers.maybeNotifyUserToReload();
-  },
 });
 
 Preferences.addSetting({
@@ -3616,7 +3601,6 @@ Preferences.addSetting({
   deps: ["contentBlockingCategory"],
   onUserClick(_, { contentBlockingCategory }) {
     contentBlockingCategory.value = "standard";
-    PrivacySettingHelpers.maybeNotifyUserToReload();
   },
   disabled({ contentBlockingCategory }) {
     return (
@@ -3631,7 +3615,6 @@ Preferences.addSetting({
   deps: ["contentBlockingCategory"],
   onUserClick(_, { contentBlockingCategory }) {
     contentBlockingCategory.value = "strict";
-    PrivacySettingHelpers.maybeNotifyUserToReload();
   },
   disabled({ contentBlockingCategory }) {
     return (
