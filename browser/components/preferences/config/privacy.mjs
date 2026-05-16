6ad60655470cb5b4ca6a3b38abeb491ff22a6818
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-05-16 00:42:28 +0000

    Bug 2038222 - 'Reload all tabs' button is not triggered after switching the ETP from Standard to Strict - r=hjones
    This required a few helper calls. Also, I added the same UI right below
    the reset buttons inside of the custom panel, since we really should
    be encouraging a reload there too. It wasn't in the Figma, but it must
    have been an oversight
    
    Differential Revision: https://phabricator.services.mozilla.com/D299818
    
    UltraBlame original commit: 30e97c1befc8dee1f02bf8ccb5dc43f38286a38c

--- browser/components/preferences/config/privacy.mjs
+++ browser/components/preferences/config/privacy.mjs
@@ -1242,6 +1242,18 @@ SettingGroupManager.registerGroups({
           },
         ],
       },
+      {
+        id: "reloadTabsHint",
+        control: "moz-message-bar",
+        l10nId: "preferences-etp-reload-tabs-hint",
+        options: [
+          {
+            control: "moz-button",
+            l10nId: "preferences-etp-reload-tabs-hint-button",
+            slot: "actions",
+          },
+        ],
+      },
     ],
   },
   etpCustomize: {
@@ -3461,6 +3473,9 @@ Preferences.addSetting({
 
     return config;
   },
+  onUserChange() {
+    PrivacySettingHelpers.maybeNotifyUserToReload();
+  },
 });
 
 Preferences.addSetting({
@@ -3601,6 +3616,7 @@ Preferences.addSetting({
   deps: ["contentBlockingCategory"],
   onUserClick(_, { contentBlockingCategory }) {
     contentBlockingCategory.value = "standard";
+    PrivacySettingHelpers.maybeNotifyUserToReload();
   },
   disabled({ contentBlockingCategory }) {
     return (
@@ -3615,6 +3631,7 @@ Preferences.addSetting({
   deps: ["contentBlockingCategory"],
   onUserClick(_, { contentBlockingCategory }) {
     contentBlockingCategory.value = "strict";
+    PrivacySettingHelpers.maybeNotifyUserToReload();
   },
   disabled({ contentBlockingCategory }) {
     return (
