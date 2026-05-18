5d84efb1fd616e29d9cd06609dde5dacd62aae6a
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-05-18 17:37:14 +0000

    Bug 2039129 - Add markers and flows for ScriptLoadRequest. r=nbp
    This is helful for tracking when scripts load. I used it
    to debug bug 2030614.
    
    Differential Revision: https://phabricator.services.mozilla.com/D300198
    
    UltraBlame original commit: 26884130382cb07194693b352bececf8fb1b8b93

--- js/loader/ScriptLoadRequestList.cpp
+++ js/loader/ScriptLoadRequestList.cpp
@@ -2,6 +2,8 @@
 
 
 
+#include <mozilla/FlowMarkers.h>
+
 #include "ScriptLoadRequestList.h"
 
 #include "ScriptLoadRequest.h"
@@ -35,6 +37,9 @@ bool ScriptLoadRequestList::Contains(ScriptLoadRequest* aElem) const {
 #endif  
 
 void ScriptLoadRequestList::AppendElement(ScriptLoadRequest* aElem) {
+  PROFILER_MARKER("ScriptLoadRequestList::AppendElement", JS,
+                  {mozilla::MarkerStack::Capture()}, FlowMarker,
+                  Flow::FromPointer(aElem));
   MOZ_ASSERT(!aElem->isInList());
   NS_ADDREF(aElem);
   insertBack(aElem);
