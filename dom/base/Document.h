b366c1e70268b4d1a93f2656dce7994266850991
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-05-20 13:53:08 +0000

    Revert "Bug 2039783 - Treat ScrollTimeline source as root when it resolves to the document's scrolling element. r=layout-scroll-driven-animation-reviewers,dshin" for causing wr failures  animation-with-root-scroller.html
    This reverts commit 49501542bed55eb3af354717e50d7f1fde288fc7.
    
    Revert "Bug 2039783 - Resolve ScrollTimeline anonymous sources via the document's scrolling element. r=layout-scroll-driven-animation-reviewers,dshin"
    
    This reverts commit 9c35a8997a1462e03cf6634a2b4464f4f7445f2b.
    
    Revert "Bug 2039783 - Add Document::GetScrollingElementNoFlush() for callers that must not flush. r=layout-scroll-driven-animation-reviewers,emilio,dshin"
    
    This reverts commit 6c552cb206689341e989bb0c16bffadb42772e4b.
    
    Revert "Bug 2039783 - Add a meta viewport to avoid scaling down on mobile environments. r=layout-scroll-driven-animation-reviewers,dshin"
    
    This reverts commit c38622b8e78a71b870bfa94c9c224fcd00cd0f10.
    
    UltraBlame original commit: 9bd0fbd9b57ea48b04f5c3e16dd7552343971cec

--- dom/base/Document.h
+++ dom/base/Document.h
@@ -3762,10 +3762,6 @@ class Document : public nsINode,
   
   
   
-  Element* GetScrollingElementNoFlush();
-  
-  
-  
   
   bool IsScrollingElement(Element* aElement);
 
@@ -4825,12 +4821,6 @@ class Document : public nsINode,
 
   
   bool IsPotentiallyScrollable(HTMLBodyElement* aBody);
-
-  
-  
-  enum class Flush : bool { No, Yes };
-  Element* GetScrollingElementImpl(Flush);
-  bool IsPotentiallyScrollableImpl(HTMLBodyElement* aBody, Flush);
 
   void MaybeAllowStorageForOpenerAfterUserInteraction();
 
