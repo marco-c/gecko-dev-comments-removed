ff2b8deec75fb0eefcf0685ac3ad9f70d8d6430a
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-05-26 18:14:52 +0000

    Bug 2040779 - Part 2: Integrate the IPProtection debug UI into Fenix r=mavduevskiy,android-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D301530
    
    UltraBlame original commit: af6fb6a70d2a66424197dd804866aa483b349809

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/settings/IPProtectionFragment.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/settings/IPProtectionFragment.kt
@@ -8,10 +8,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
 import androidx.fragment.app.Fragment
 import mozilla.components.ExperimentalAndroidComponentsApi
+import mozilla.components.feature.ipprotection.debug.IPProtectionStateDebugContent
 import mozilla.components.feature.ipprotection.store.IPProtectionAction
 import mozilla.components.lib.state.ext.observeAsComposableState
 import mozilla.telemetry.glean.private.NoExtras
@@ -22,10 +28,14 @@ import org.mozilla.fenix.e2e.SystemInsetsPaddedFragment
 import org.mozilla.fenix.ext.requireComponents
 import org.mozilla.fenix.ext.settings
 import org.mozilla.fenix.ext.showToolbar
+import org.mozilla.fenix.ext.showToolbarWithIconButton
 import org.mozilla.fenix.theme.FirefoxTheme
+import mozilla.components.ui.icons.R as iconsR
 
 /** Fragment hosting the IP Protection settings screen. */
 class IPProtectionFragment : Fragment(), SystemInsetsPaddedFragment {
+
+    private var showDebugDialog by mutableStateOf(false)
 
     @OptIn(ExperimentalAndroidComponentsApi::class)
     override fun onCreateView(
@@ -60,12 +70,30 @@ class IPProtectionFragment : Fragment(), SystemInsetsPaddedFragment {
                         requireComponents.ipProtection.store.dispatch(IPProtectionAction.Toggle)
                     },
                 )
+
+                if (showDebugDialog) {
+                    Dialog(
+                        onDismissRequest = { showDebugDialog = false },
+                        properties = DialogProperties(usePlatformDefaultWidth = false),
+                    ) {
+                        IPProtectionStateDebugContent(state.value)
+                    }
+                }
             }
         }
     }
 
     override fun onResume() {
         super.onResume()
-        showToolbar(getString(R.string.ip_protection_title))
+        if (requireContext().settings().showSecretDebugMenuThisSession) {
+            showToolbarWithIconButton(
+                title = getString(R.string.ip_protection_title),
+                contentDescription = getString(R.string.content_description_menu),
+                iconResId = iconsR.drawable.mozac_ic_ellipsis_vertical_24,
+                onClick = { showDebugDialog = true },
+            )
+        } else {
+            showToolbar(getString(R.string.ip_protection_title))
+        }
     }
 }
