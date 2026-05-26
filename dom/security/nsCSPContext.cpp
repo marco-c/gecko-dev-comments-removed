b53a49a4ec5b967db17d09e6b272980ceb789408
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-05-26 17:58:42 +0000

    Bug 1960363 - Record pdf.js viewer CSP violations via Glean, with a mochitest covering CSP enforcement and telemetry dispatch r=tschuster
    Differential Revision: https://phabricator.services.mozilla.com/D299990
    
    UltraBlame original commit: e92b0e8146693edf076ea71c66e1646bdda2cce7

--- dom/security/nsCSPContext.cpp
+++ dom/security/nsCSPContext.cpp
@@ -1467,12 +1467,25 @@ void nsCSPContext::HandleInternalPageViolation(
     const CSPViolationData& aCSPViolationData,
     const SecurityPolicyViolationEventInit& aInit,
     const nsAString& aViolatedDirectiveNameAndValue) {
-  if (!mSelfURI || !mSelfURI->SchemeIs("chrome")) {
+  nsCOMPtr<nsIURI> selfURI = mSelfURI;
+  if (!selfURI) {
+    return;
+  }
+  if (nsContentUtils::IsPDFJS(mLoadingPrincipal)) {
+    
+    
+    
+    
+    mLoadingPrincipal->GetURI(getter_AddRefs(selfURI));
+    if (!selfURI) {
+      return;
+    }
+  } else if (!selfURI->SchemeIs("chrome")) {
     return;
   }
 
   nsAutoCString selfURISpec;
-  mSelfURI->GetSpec(selfURISpec);
+  selfURI->GetSpec(selfURISpec);
 
   glean::security::CspViolationInternalPageExtra extra;
   extra.directive = Some(NS_ConvertUTF16toUTF8(aInit.mEffectiveDirective));
