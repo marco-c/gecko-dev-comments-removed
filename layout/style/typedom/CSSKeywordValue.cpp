4579f0cf0c64445d43cf214b7970248a30f82965
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-05-26 18:40:54 +0000

    Bug 2038579 - Add a helper factory for perspective component; r=emilio,firefox-style-system-reviewers
    Add a helper factory method for implementing the keywordish rectification
    algorithm.
    
    This helper will be used by follow-up patches implementing the WebIDL
    constructor for the perspective component and likely by additional algorithms
    in the future.
    
    Differential Revision: https://phabricator.services.mozilla.com/D300931
    
    UltraBlame original commit: 224a7906de28f56768d9001c2060612429c0c597

--- layout/style/typedom/CSSKeywordValue.cpp
+++ layout/style/typedom/CSSKeywordValue.cpp
@@ -19,9 +19,28 @@ CSSKeywordValue::CSSKeywordValue(nsCOMPtr<nsISupports> aParent,
       mValue(aValue) {}
 
 
+RefPtr<CSSKeywordValue> CSSKeywordValue::Create(nsCOMPtr<nsISupports> aParent,
+                                                const nsACString& aValue) {
+  return MakeAndAddRef<CSSKeywordValue>(std::move(aParent), aValue);
+}
+
+
+
+
+RefPtr<CSSKeywordValue> CSSKeywordValue::Create(
+    nsCOMPtr<nsISupports> aParent, const CSSKeywordish& aKeywordish) {
+  if (aKeywordish.IsCSSKeywordValue()) {
+    return &aKeywordish.GetAsCSSKeywordValue();
+  }
+
+  MOZ_DIAGNOSTIC_ASSERT(aKeywordish.IsUTF8String());
+  return Create(std::move(aParent), aKeywordish.GetAsUTF8String());
+}
+
+
 RefPtr<CSSKeywordValue> CSSKeywordValue::Create(
     nsCOMPtr<nsISupports> aParent, const StyleKeywordValue& aKeywordValue) {
-  return MakeRefPtr<CSSKeywordValue>(std::move(aParent), aKeywordValue._0);
+  return Create(std::move(aParent), aKeywordValue._0);
 }
 
 JSObject* CSSKeywordValue::WrapObject(JSContext* aCx,
