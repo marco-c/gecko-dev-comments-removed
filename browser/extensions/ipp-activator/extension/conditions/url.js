8a6ad067e31bc1bfaa4b9c8804564fdc0d72dd4c
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-05-28 19:16:17 +0000

    Bug 2042286 - IP-Protection warning message: Better URL handling r=lesleynorton
    Differential Revision: https://phabricator.services.mozilla.com/D302462
    
    UltraBlame original commit: c1597562a9c02526941a91a60d1931ec69d2f163

--- browser/extensions/ipp-activator/extension/conditions/url.js
+++ browser/extensions/ipp-activator/extension/conditions/url.js
@@ -8,11 +8,48 @@
 
 
 class ConditionUrl extends ConditionBase {
+  #url = "";
+  #onTabUpdated = null;
+  #tabId = null;
+
+  async init() {
+    await super.init();
+
+    this.#tabId = this.factory?.context?.tabId;
+    if (typeof this.#tabId !== "number") {
+      return;
+    }
+
+    try {
+      const tab = await browser.tabs.get(this.#tabId);
+      this.#url = tab?.url ?? "";
+    } catch (_) {}
+
+    this.#onTabUpdated = (tabId, changeInfo) => {
+      if (tabId !== this.#tabId || !("url" in changeInfo)) {
+        return;
+      }
+      this.#url = changeInfo.url ?? "";
+      this._notifyChange();
+    };
+    browser.tabs.onUpdated.addListener(this.#onTabUpdated, {
+      tabId: this.#tabId,
+      properties: ["url"],
+    });
+  }
+
+  uninit() {
+    if (this.#onTabUpdated) {
+      browser.tabs.onUpdated.removeListener(this.#onTabUpdated);
+      this.#onTabUpdated = null;
+    }
+    super.uninit();
+  }
+
   check() {
     try {
       const pattern = new RegExp(String(this.desc?.pattern ?? ""));
-      const url = String(this.factory?.context?.url ?? "");
-      return pattern.test(url);
+      return pattern.test(this.#url);
     } catch (e) {
       console.warn("Unable to parse the regexp", this.desc?.pattern, e);
       return false;
--- browser/extensions/ipp-activator/tests/browser/browser_conditions_url.js
+++ browser/extensions/ipp-activator/tests/browser/browser_conditions_url.js
@@ -24,3 +24,45 @@ add_task(async function test_url_pattern_no_match() {
 add_task(async function test_url_invalid_pattern() {
   await checkNotification({ type: "url", pattern: "[" }, false);
 });
+
+
+
+add_task(async function test_url_pattern_updates_on_navigation() {
+  Services.prefs.setStringPref(
+    PREF_DYNAMIC_TAB_BREAKAGES,
+    JSON.stringify([
+      {
+        domains: [TEST_DOMAIN],
+        l10nId: BREAKAGE_L10N_ID,
+        condition: {
+          type: "url",
+          pattern: `^https://${TEST_DOMAIN}/play(/|$)`,
+        },
+      },
+    ])
+  );
+
+  const tab = await BrowserTestUtils.openNewForegroundTab(
+    gBrowser,
+    `https://${TEST_DOMAIN}/play/`
+  );
+
+  await waitForNotification(tab);
+
+  BrowserTestUtils.startLoadingURIString(
+    tab.linkedBrowser,
+    `https://${TEST_DOMAIN}/news`
+  );
+  await BrowserTestUtils.browserLoaded(tab.linkedBrowser);
+  await waitForNoNotification(tab);
+
+  BrowserTestUtils.startLoadingURIString(
+    tab.linkedBrowser,
+    `https://${TEST_DOMAIN}/play/episode`
+  );
+  await BrowserTestUtils.browserLoaded(tab.linkedBrowser);
+  await waitForNotification(tab);
+
+  BrowserTestUtils.removeTab(tab);
+  resetState();
+});
