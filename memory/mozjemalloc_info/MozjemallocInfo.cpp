48e80631a8e186e428508726ae51850fde698a62
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-05-28 19:20:09 +0000

    Bug 2042123 - Add theoratical best run overhead to mozjemalloc_info r=glandium
    Differential Revision: https://phabricator.services.mozilla.com/D302303
    
    UltraBlame original commit: 2ca2e382450f4725e9f13456fead6a4f922a26f3

--- memory/mozjemalloc_info/MozjemallocInfo.cpp
+++ memory/mozjemalloc_info/MozjemallocInfo.cpp
@@ -36,15 +36,17 @@ int main() {
   printf("\n");
   printf("Run layout for each bin size\n");
   printf("----------------------------\n\n");
-  printf(" Size | Reg per run | Run size | Overhead\n");
-  printf("------|-------------|----------|----------\n");
+  printf(" Size | Reg per run | Run size | Overhead | Theoretical best \n");
+  printf("------|-------------|----------|----------|------------------\n");
   for (unsigned i = 0; i < num_bins; i++) {
     auto& bin = bin_stats[i];
     if (bin.size) {
-      printf("%5zu | %11zu | %5zuKiB | %7.2f%%\n", bin.size,
+      printf("%5zu | %11zu | %5zuKiB | %7.2f%% | %15.2f%%\n", bin.size,
              bin.regions_per_run, bin.bytes_per_run / 1024,
              float(bin.bytes_per_run - (bin.regions_per_run * bin.size)) *
-                 100.0f / float(bin.regions_per_run * bin.size));
+                 100.0f / float(bin.regions_per_run * bin.size),
+             
+             100.0f / (8.0f * float(bin.size)));
     }
   }
 
