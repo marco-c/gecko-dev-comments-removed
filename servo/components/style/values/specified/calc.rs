85045a52d1d104e865e9163179a0afa956b6a6b0
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-06-01 01:43:30 +0000

    Bug 2043078 - Typecheck color components before returning. r=sajidanwar
    Differential Revision: https://phabricator.services.mozilla.com/D302984
    
    UltraBlame original commit: 517f32cbaabce6a555e9bc6d96d8c996b48b346f

--- servo/components/style/values/specified/calc.rs
+++ servo/components/style/values/specified/calc.rs
@@ -253,6 +253,8 @@ pub struct AllowParse {
     
     units: CalcUnits,
     
+    pub color_components: bool,
+    
     additional_functions: AdditionalFunctions,
 }
 
@@ -261,6 +263,7 @@ impl AllowParse {
     pub fn new(units: CalcUnits) -> Self {
         Self {
             units,
+            color_components: false,
             additional_functions: AdditionalFunctions::empty(),
         }
     }
@@ -284,9 +287,8 @@ impl generic::CalcNodeLeaf for Leaf {
             Leaf::Angle(_) => CalcUnits::ANGLE,
             Leaf::Time(_) => CalcUnits::TIME,
             Leaf::Resolution(_) => CalcUnits::RESOLUTION,
-            Leaf::ColorComponent(_) => CalcUnits::COLOR_COMPONENT,
             Leaf::Percentage(_) => CalcUnits::PERCENTAGE,
-            Leaf::Number(_) => CalcUnits::empty(),
+            Leaf::ColorComponent(_) | Leaf::Number(_) => CalcUnits::empty(),
         }
     }
 
@@ -619,6 +621,7 @@ fn parse_anchor_function_fallback<'i, 't>(
         input,
         AllowParse {
             units: CalcUnits::LENGTH_PERCENTAGE,
+            color_components: false,
             additional_functions,
         },
     )?
@@ -767,19 +770,16 @@ impl CalcNode {
                     "-infinity" => Leaf::Number(NoCalcNumber::new(f32::NEG_INFINITY)),
                     "nan" => Leaf::Number(NoCalcNumber::new(f32::NAN)),
                     _ => {
-                        if crate::color::parsing::rcs_enabled() &&
-                            allowed.includes(CalcUnits::COLOR_COMPONENT)
-                        {
-                            if let Ok(channel_keyword) = ChannelKeyword::from_ident(&ident) {
-                                Leaf::ColorComponent(channel_keyword)
-                            } else {
-                                return Err(location
-                                    .new_unexpected_token_error(Token::Ident(ident.clone())));
-                            }
-                        } else {
+                        if !allowed.color_components {
                             return Err(
                                 location.new_unexpected_token_error(Token::Ident(ident.clone()))
                             );
+                        }
+                        if let Ok(channel_keyword) = ChannelKeyword::from_ident(&ident) {
+                            Leaf::ColorComponent(channel_keyword)
+                        } else {
+                            return Err(location
+                                .new_unexpected_token_error(Token::Ident(ident.clone())));
                         }
                     },
                 };
@@ -1114,7 +1114,7 @@ impl CalcNode {
                             } else {
                                 
                                 
-                                return if resolved.unit().contains(CalcUnits::COLOR_COMPONENT) {
+                                return if resolved.unit().is_empty() {
                                     InPlaceDivisionResult::Unchanged
                                 } else {
                                     InPlaceDivisionResult::Invalid
@@ -1307,6 +1307,7 @@ impl CalcNode {
         } else {
             AllowParse {
                 units: CalcUnits::LENGTH_PERCENTAGE,
+                color_components: false,
                 additional_functions: match allow_anchor {
                     AllowAnchorPositioningFunctions::No => unreachable!(),
                     AllowAnchorPositioningFunctions::AllowAnchorSize => {
