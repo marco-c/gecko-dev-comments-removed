c011f1d7bb3ce62868e42aaa5c5ae644a071ee54
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-06-01 02:02:49 +0000

    Bug 2014918: Follow flat-tree ancestors for finding anchor scopes. r=layout-scroll-driven-animation-reviewers,firefox-style-system-reviewers,boris
    Differential Revision: https://phabricator.services.mozilla.com/D303092
    
    UltraBlame original commit: f32418cae9846df7b14d05bd4bc2b8e140879eff

--- layout/style/TimelineManager.cpp
+++ layout/style/TimelineManager.cpp
@@ -211,8 +211,8 @@ TimelineType* TimelineManager::DoGetScopedTimeline(
 
   auto ScopeIsValid = [&](const Element* aTimelineCandidate,
                           const Element* aExpectedScope) {
-    const auto* e = aTimelineCandidate->GetParentElement();
-    for (; e && e != aExpectedScope; e = e->GetParentElement()) {
+    const auto* e = aTimelineCandidate->GetFlattenedTreeParentElement();
+    for (; e && e != aExpectedScope; e = e->GetFlattenedTreeParentElement()) {
       if (GetTimelineScope(e, aName)) {
         
         
