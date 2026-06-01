a75195c1efe7d01fff7e2871275450c75f0059ed
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-06-01 02:03:22 +0000

    Bug 2033271 - Clamp APZ visual scroll offset after applying relative ScrollPositionUpdate. r=botond
    When APZ is mid-animation and the main thread fires an instant scrollBy that
    overshoots the top or left boundary, ApplyRelativeScrollUpdateFrom can produce
    a negative visual scroll offset. Setting needToReclampScroll after the relative
    update triggers the existing post-loop clamping on both Metrics() and all
    sampled states, preventing the negative offset. As a side effect the bottom and
    right boundaries are also clamped consistently.
    
    Differential Revision: https://phabricator.services.mozilla.com/D298723
    
    UltraBlame original commit: 2809c4bede1f77ddd91e28314f007bece0aacf22

--- gfx/layers/apz/src/AsyncPanZoomController.cpp
+++ gfx/layers/apz/src/AsyncPanZoomController.cpp
@@ -576,8 +576,8 @@ UniquePtr<VelocityTracker> PlatformSpecificStateBase::CreateVelocityTracker(
 
 
 
-class MOZ_STACK_CLASS AsyncPanZoomController::AutoRecordCompositorScrollUpdate
-    final {
+class MOZ_STACK_CLASS
+AsyncPanZoomController::AutoRecordCompositorScrollUpdate final {
  public:
   AutoRecordCompositorScrollUpdate(
       AsyncPanZoomController* aApzc, CompositorScrollUpdate::Source aSource,
@@ -5999,6 +5999,7 @@ void AsyncPanZoomController::NotifyMainThreadTransaction(
       relativeDelta = Some(Metrics().ApplyRelativeScrollUpdateFrom(
           scrollUpdate, FrameMetrics::IsDefaultApzc{isDefault}));
       Metrics().RecalculateLayoutViewportOffset();
+      needToReclampScroll = true;
     } else if (scrollUpdate.GetType() == ScrollUpdateType::PureRelative) {
       APZC_LOG("%p pure-relative updating scroll offset from %s by %s\n", this,
                ToString(Metrics().GetVisualScrollOffset()).c_str(),
