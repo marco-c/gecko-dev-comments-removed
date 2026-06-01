1374d2c394e52fd6edcdca8fb21706e4670480b4
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-06-01 01:37:31 +0000

    Bug 1996495 - set associated browsing context ID for workers initiated xhr request. r=edenchuang
    Differential Revision: https://phabricator.services.mozilla.com/D288932
    
    UltraBlame original commit: 477ceeaaadd1e7c263f0c84768a8b00dfaf314cb

--- dom/xhr/XMLHttpRequestWorker.cpp
+++ dom/xhr/XMLHttpRequestWorker.cpp
@@ -765,6 +765,8 @@ bool Proxy::Init(WorkerPrivate* aWorkerPrivate) {
 
   mXHR->SetParameters(mMozAnon, mMozSystem);
   mXHR->SetClientInfoAndController(mClientInfo, mController);
+  mXHR->SetAssociatedBrowsingContextID(
+      aWorkerPrivate->AssociatedBrowsingContextID());
 
   ErrorResult rv;
   mXHRUpload = mXHR->GetUpload(rv);
