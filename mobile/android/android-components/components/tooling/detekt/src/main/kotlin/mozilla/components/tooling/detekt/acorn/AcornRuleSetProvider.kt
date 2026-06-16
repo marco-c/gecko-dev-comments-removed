17bea9a87739fa0112071d01352d57efb747fe8f
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-06-16 02:31:27 +0000

    Bug 2043856 - Add a detekt rule to enforce the compose-base Switch usage r=android-reviewers,jdelorenzo
    Differential Revision: https://phabricator.services.mozilla.com/D303628
    
    UltraBlame original commit: 8c9ea26eb17448e91874982f96370aa1079fb351

--- mobile/android/android-components/components/tooling/detekt/src/main/kotlin/mozilla/components/tooling/detekt/acorn/AcornRuleSetProvider.kt
+++ mobile/android/android-components/components/tooling/detekt/src/main/kotlin/mozilla/components/tooling/detekt/acorn/AcornRuleSetProvider.kt
@@ -20,6 +20,7 @@ class AcornRuleSetProvider : RuleSetProvider {
         rules = listOf(
             AcornPaddingRule(config),
             AcornThemeUsageRule(config),
+            MaterialSwitchUsageRule(config),
             MaterialTypographyUsageRule(config),
         ),
     )
