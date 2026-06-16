571da2e46cd4a566b723584af4c569b1614cf133
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-06-16 04:49:54 +0000

    Bug 2040608 - Part 2: Update UI test rule for onboarding changes. r=android-reviewers,rebecatudor273
    Differential Revision: https://phabricator.services.mozilla.com/D302452
    
    UltraBlame original commit: 42dcf0da3009c8a4108fee6930469ea1cb6541e1

--- mobile/android/fenix/app/src/androidTest/java/org/mozilla/fenix/ui/NimbusMessagingTriggerTest.kt
+++ mobile/android/fenix/app/src/androidTest/java/org/mozilla/fenix/ui/NimbusMessagingTriggerTest.kt
@@ -34,7 +34,7 @@ class NimbusMessagingTriggerTest {
     val fenixTestRule: FenixTestRule = FenixTestRule()
 
     @get:Rule
-    val activityTestRule = HomeActivityIntentTestRule.withDefaultSettingsOverrides(skipOnboarding = true)
+    val activityTestRule = HomeActivityIntentTestRule.withDefaultSettingsOverrides()
 
     @Before
     fun setUp() {
