53a46e6780e549ad37461c736b70fa27b641b423
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-06-16 05:43:59 +0000

    Bug 2045043 - Part 35 - Fixup final classes using _FINAL cycle collecting isupports macro in dom/messagechannel r=dom-core-reviewers,smaug
    Differential Revision: https://phabricator.services.mozilla.com/D305314
    
    UltraBlame original commit: bb6c4f04e566d1a23f399cc52f6cd5c21cd40816

--- dom/messagechannel/MessageChannel.h
+++ dom/messagechannel/MessageChannel.h
@@ -2,8 +2,6 @@
 
 
 
-
-
 #ifndef mozilla_dom_MessageChannel_h
 #define mozilla_dom_MessageChannel_h
 
@@ -23,7 +21,7 @@ class MessagePort;
 
 class MessageChannel final : public nsISupports, public nsWrapperCache {
  public:
-  NS_DECL_CYCLE_COLLECTING_ISUPPORTS
+  NS_DECL_CYCLE_COLLECTING_ISUPPORTS_FINAL
   NS_DECL_CYCLE_COLLECTION_WRAPPERCACHE_CLASS(MessageChannel)
 
   nsIGlobalObject* GetParentObject() const { return mGlobal; }
