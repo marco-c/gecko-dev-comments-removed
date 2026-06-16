7abe709f1ed4c0fefcaf943c52337c59ab5ae16b
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-06-16 04:52:53 +0000

    Bug 2046591: Use StaticRefPtr instead of manually-refcounted-raw-pointer, for refcounted globals in nsAccessibilityService. r=accessibility-platform-reviewers,Jamie
    Differential Revision: https://phabricator.services.mozilla.com/D306009
    
    UltraBlame original commit: 131b7b4042e9bbaa8268d87ac4d56660a28c9366

--- accessible/base/nsAccessibilityService.h
+++ accessible/base/nsAccessibilityService.h
@@ -12,6 +12,7 @@
 #include "mozilla/a11y/Role.h"
 #include "mozilla/a11y/SelectionManager.h"
 #include "mozilla/Preferences.h"
+#include "mozilla/StaticPtr.h"
 
 #include "nsAtomHashKeys.h"
 #include "nsIContent.h"
@@ -492,13 +493,15 @@ class nsAccessibilityService final : public mozilla::a11y::DocManager,
   
 
 
-  static nsAccessibilityService* gAccessibilityService;
+  static mozilla::StaticRefPtr<nsAccessibilityService> gAccessibilityService;
 
   
 
 
-  static mozilla::a11y::ApplicationAccessible* gApplicationAccessible;
-  static mozilla::a11y::xpcAccessibleApplication* gXPCApplicationAccessible;
+  static mozilla::StaticRefPtr<mozilla::a11y::ApplicationAccessible>
+      gApplicationAccessible;
+  static mozilla::StaticRefPtr<mozilla::a11y::xpcAccessibleApplication>
+      gXPCApplicationAccessible;
 
   
 
