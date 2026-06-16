39fb76e32598520ff278331ce1d8ec6955a43663
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-06-16 04:45:21 +0000

    Bug 2045043 - Part 25 - Fixup final classes using _FINAL cycle collecting isupports macro in dom/credentialmanagement r=dom-core-reviewers,edgar
    Differential Revision: https://phabricator.services.mozilla.com/D305304
    
    UltraBlame original commit: 9d6feca4bd531cc41fe8d38e49b8a719fe318c8d

--- dom/credentialmanagement/identity/WebIdentityHandler.h
+++ dom/credentialmanagement/identity/WebIdentityHandler.h
@@ -2,8 +2,6 @@
 
 
 
-
-
 #ifndef mozilla_dom_WebIdentityHandler_h
 #define mozilla_dom_WebIdentityHandler_h
 
@@ -18,7 +16,7 @@ namespace mozilla::dom {
 
 class WebIdentityHandler final : public AbortFollower {
  public:
-  NS_DECL_CYCLE_COLLECTING_ISUPPORTS
+  NS_DECL_CYCLE_COLLECTING_ISUPPORTS_FINAL
   NS_DECL_CYCLE_COLLECTION_CLASS(WebIdentityHandler)
 
   explicit WebIdentityHandler(nsPIDOMWindowInner* aWindow) : mWindow(aWindow) {
