5571708f02623de7932dc8e2770027cf828288f8
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-06-16 05:38:03 +0000

    Bug 1925572 - Improve parsing and validation of channel keywords in relative colors. r=firefox-style-system-reviewers,tlouw,emilio
    Differential Revision: https://phabricator.services.mozilla.com/D306373
    
    UltraBlame original commit: 45c32fcdf2487de081cbbfd8024e1041473146a5

--- servo/components/style/values/specified/calc.rs
+++ servo/components/style/values/specified/calc.rs
@@ -252,7 +252,7 @@ pub struct CalcParseFlags {
     
     units: CalcUnits,
     
-    pub color_components: bool,
+    pub color_components: ChannelKeyword,
     
     additional_functions: AdditionalFunctions,
     
@@ -265,7 +265,7 @@ impl CalcParseFlags {
     pub fn new(units: CalcUnits) -> Self {
         Self {
             units,
-            color_components: false,
+            color_components: ChannelKeyword::empty(),
             additional_functions: AdditionalFunctions::empty(),
             in_place_operations: CalcNodeParseInPlaceOperations::Yes,
         }
@@ -693,7 +693,7 @@ fn parse_anchor_function_fallback<'i, 't>(
         input,
         CalcParseFlags {
             units: CalcUnits::LENGTH_PERCENTAGE,
-            color_components: false,
+            color_components: ChannelKeyword::empty(),
             additional_functions,
             in_place_operations: CalcNodeParseInPlaceOperations::Yes,
         },
@@ -852,16 +852,9 @@ impl CalcNode {
                     "-infinity" => Leaf::Number(NoCalcNumber::new(f32::NEG_INFINITY)),
                     "nan" => Leaf::Number(NoCalcNumber::new(f32::NAN)),
                     _ => {
-                        if !flags.color_components {
-                            return Err(
-                                location.new_unexpected_token_error(Token::Ident(ident.clone()))
-                            );
-                        }
-                        if let Ok(channel_keyword) = ChannelKeyword::from_ident(&ident) {
-                            Leaf::ColorComponent(channel_keyword)
-                        } else {
-                            return Err(location
-                                .new_unexpected_token_error(Token::Ident(ident.clone())));
+                        match ChannelKeyword::from_ident(&ident) {
+                            Ok(channel_keyword) if flag.color_components.contains(channel_keyword) => Leaf::ColorComponent(channel_keyword),
+                            _ => return Err(location.new_unexpected_token_error(Token::Ident(ident.clone()))),
                         }
                     },
                 };
@@ -1418,7 +1411,7 @@ impl CalcNode {
         } else {
             CalcParseFlags {
                 units: CalcUnits::LENGTH_PERCENTAGE,
-                color_components: false,
+                color_components: ChannelKeyword::empty(),
                 additional_functions: match allow_anchor {
                     AllowAnchorPositioningFunctions::No => unreachable!(),
                     AllowAnchorPositioningFunctions::AllowAnchorSize => {
