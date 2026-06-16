778186ffbc86dca6801a570491947d23ce2a0904
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-06-16 03:35:19 +0000

    Revert "Bug 2034632: Fall back to monomorphic inlining if a trial-inlined ICScript hasn't been used r=jandem" as per request.
    This reverts commit b63e150126d7cdeaddc9c00ca085ee389f8a41e1.
    
    UltraBlame original commit: a5d7587b266982eb60c67972cf2624038a11ec69

--- js/src/jit/WarpOracle.cpp
+++ js/src/jit/WarpOracle.cpp
@@ -1356,13 +1356,6 @@ AbortReasonOr<bool> WarpScriptOracle::maybeInlineCall(
   MOZ_ASSERT_IF(!isTrialInlined, fallbackStub->trialInliningState() ==
                                      TrialInliningState::MonomorphicInlined);
 
-  if (isTrialInlined && inlineData->icScript->warmUpCount() ==
-                            JitOptions.trialInliningInitialWarmUpCount) {
-    
-    
-    isTrialInlined = false;
-  }
-
   ICScript* icScript = nullptr;
   if (isTrialInlined) {
     icScript = inlineData->icScript;
