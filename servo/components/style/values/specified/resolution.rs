a9b69b8fbd65ea6e1ed636f363b20a9ccb4e2347
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-06-16 05:31:50 +0000

    Bug 2045434 - Add ResolutionUnit::from_str helper; r=emilio,firefox-style-system-reviewers
    Add a helper for converting string resolution units into ResolutionUnit values.
    
    This centralizes resolution unit parsing and makes future callers less
    error-prone by avoiding repeated matching on raw unit strings.
    
    Differential Revision: https://phabricator.services.mozilla.com/D305098
    
    UltraBlame original commit: 42030c83c4ee50a1d7ced0193a3e2516f2031506

--- servo/components/style/values/specified/resolution.rs
+++ servo/components/style/values/specified/resolution.rs
@@ -34,6 +34,18 @@ pub enum ResolutionUnit {
 impl ResolutionUnit {
     
     #[inline]
+    pub fn from_str(unit: &str) -> Result<Self, ()> {
+        Ok(match_ignore_ascii_case! { &unit,
+            "dpi" => Self::Dpi,
+            "dppx" => Self::Dppx,
+            "dpcm" => Self::Dpcm,
+            "x" => Self::X,
+            _ => return Err(())
+        })
+    }
+
+    
+    #[inline]
     pub fn as_str(self) -> &'static str {
         match self {
             Self::Dpi => "dpi",
@@ -96,13 +108,7 @@ impl NoCalcResolution {
 
     
     pub fn parse_dimension(value: CSSFloat, unit: &str) -> Result<Self, ()> {
-        let unit = match_ignore_ascii_case! { &unit,
-            "dpi" => ResolutionUnit::Dpi,
-            "dppx" => ResolutionUnit::Dppx,
-            "dpcm" => ResolutionUnit::Dpcm,
-            "x" => ResolutionUnit::X,
-            _ => return Err(())
-        };
+        let unit = ResolutionUnit::from_str(unit)?;
         Ok(Self::new(unit, value))
     }
 }
