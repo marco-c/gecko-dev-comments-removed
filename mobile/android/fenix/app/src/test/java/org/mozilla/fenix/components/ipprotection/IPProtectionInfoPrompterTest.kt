3caad3563bfe37c5f54a955cbd67de7dc2d3a4e3
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-06-16 05:28:16 +0000

    Bug 2046194 - Show snackbar on top of the menu r=android-reviewers,jonalmeida
    Differential Revision: https://phabricator.services.mozilla.com/D304672
    
    UltraBlame original commit: cd2cdce8972c4cb35643efee4e665bb6ee805ddb

--- mobile/android/fenix/app/src/test/java/org/mozilla/fenix/components/ipprotection/IPProtectionInfoPrompterTest.kt
+++ mobile/android/fenix/app/src/test/java/org/mozilla/fenix/components/ipprotection/IPProtectionInfoPrompterTest.kt
@@ -54,7 +54,7 @@ class IPProtectionInfoPrompterTest {
 
             val snackbarState = appStore.state.snackbarState
             assertIs<SnackbarState.IPProtectionDataLimitReached>(snackbarState)
-            assertEquals(errorMessages.dataLimitReached, snackbarState.message)
+            assertEquals(errorMessages.dataLimitReached, snackbarState.title)
         }
 
     @Test
