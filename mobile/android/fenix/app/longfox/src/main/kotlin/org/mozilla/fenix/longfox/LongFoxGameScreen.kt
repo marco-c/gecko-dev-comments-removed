c381a6b20d0d497fc620b4e5c911677f65fdcee0
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-06-16 03:09:34 +0000

    Bug 2044908 - overlapping status bar icons at top of game screen r=android-reviewers,pollymce
    Differential Revision: https://phabricator.services.mozilla.com/D304779
    
    UltraBlame original commit: a31cb25cc67424277e419a7809a92807ce32a27c

--- mobile/android/fenix/app/longfox/src/main/kotlin/org/mozilla/fenix/longfox/LongFoxGameScreen.kt
+++ mobile/android/fenix/app/longfox/src/main/kotlin/org/mozilla/fenix/longfox/LongFoxGameScreen.kt
@@ -14,9 +14,12 @@ import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
@@ -185,6 +188,7 @@ fun LongFoxGameScreen() {
             )
         }
         Row(modifier = Modifier
+            .windowInsetsPadding(WindowInsets.statusBars)
             .padding(12.dp)
             .fillMaxWidth(),
             horizontalArrangement = Arrangement.SpaceBetween,
