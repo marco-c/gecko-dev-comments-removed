e24f0a70e4523f8efe57beee8ec8e73d6c980f23
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-06-16 03:51:16 +0000

    Bug 2045043 - Part 5 - Fixup final classes using _FINAL cycle collecting isupports macro in dom/webserial r=dom-core-reviewers,smaug
    Differential Revision: https://phabricator.services.mozilla.com/D305284
    
    UltraBlame original commit: 26c1c7148b66523fbae340d7480c46a52a0188a1

--- dom/webserial/SerialPortChild.h
+++ dom/webserial/SerialPortChild.h
@@ -18,7 +18,7 @@ class SerialPort;
 
 class SerialPortChild final : public PSerialPortChild, public nsISupports {
  public:
-  NS_DECL_CYCLE_COLLECTING_ISUPPORTS
+  NS_DECL_CYCLE_COLLECTING_ISUPPORTS_FINAL
   NS_DECL_CYCLE_COLLECTION_CLASS(SerialPortChild)
 
   SerialPortChild() = default;
