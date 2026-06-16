6152c8821d5c6df479dac881d876a87fe317c3ea
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-06-16 02:45:34 +0000

    Bug 2038690 - [WebCompat Reporter v3] Update description textfield r=android-reviewers,android-l10n-reviewers,flod,calu
    Differential Revision: https://phabricator.services.mozilla.com/D303814
    
    UltraBlame original commit: 64259544e81bd12de16be7e0fc84887563b7d5e6

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/webcompat/ui/WebCompatReporter.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/webcompat/ui/WebCompatReporter.kt
@@ -161,6 +161,19 @@ fun WebCompatReporter(
 
             Spacer(modifier = Modifier.height(16.dp))
 
+            Text(
+                text = stringResource(id = R.string.webcompat_reporter_label_description_2),
+                style = FirefoxTheme.typography.headline7,
+                color = MaterialTheme.colorScheme.onSurface,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(
+                        start = FirefoxTheme.layout.space.static50,
+                        bottom = FirefoxTheme.layout.space.static100,
+                        end = FirefoxTheme.layout.space.static50,
+                    ),
+            )
+
             TextField(
                 value = state.problemDescription,
                 onValueChange = {
@@ -170,11 +183,13 @@ fun WebCompatReporter(
                         ),
                     )
                 },
-                placeholder = stringResource(id = R.string.webcompat_reporter_problem_description_placeholder_text),
+                placeholder = stringResource(id = R.string.webcompat_reporter_problem_description_placeholder_text_2),
                 errorText = "",
-                label = stringResource(id = R.string.webcompat_reporter_label_description),
                 singleLine = false,
                 maxLines = PROBLEM_DESCRIPTION_MAX_LINES,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(225.dp),
             )
 
             Spacer(modifier = Modifier.height(16.dp))
