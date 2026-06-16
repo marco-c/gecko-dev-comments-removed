f15ad0474004b8d8d2f0ece05f35bb3a8ffde33f
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-06-16 05:45:07 +0000

    Bug 2045043 - Part 51 - Fixup final classes using _FINAL cycle collecting isupports macro in dom/urlpattern r=dom-core-reviewers,farre
    Differential Revision: https://phabricator.services.mozilla.com/D305329
    
    UltraBlame original commit: 489704897449ba66f9fb44fc9c23dffc2c1c511f

--- dom/urlpattern/URLPattern.h
+++ dom/urlpattern/URLPattern.h
@@ -3,7 +3,6 @@
 
 
 
-
 #ifndef mozilla_dom_URLPattern_h
 #define mozilla_dom_URLPattern_h
 
@@ -17,7 +16,7 @@ namespace mozilla::dom {
 
 class URLPattern final : public nsISupports, public nsWrapperCache {
  public:
-  NS_DECL_CYCLE_COLLECTING_ISUPPORTS
+  NS_DECL_CYCLE_COLLECTING_ISUPPORTS_FINAL
   NS_DECL_CYCLE_COLLECTION_WRAPPERCACHE_CLASS(URLPattern)
 
   explicit URLPattern(nsISupports* aParent, UrlPatternGlue aPattern,
