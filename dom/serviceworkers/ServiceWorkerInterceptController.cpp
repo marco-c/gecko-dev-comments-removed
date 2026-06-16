0bf2cc656f7132f6a1301292098fade9f032e93d
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-06-16 02:40:59 +0000

    Reapply "Bug 1201160 - Skip ServiceWorker interception for no_cors subresource fetching from CSS. r=asuth,dom-worker-reviewers" as requested in bug 2029029 for breaking interception of requests by service workers in some cases for causing build bustages at ServiceWorkerInterceptController.cpp
    This reverts commit a23856c5acb51eaa36849d0fa31025a34c585c7b.
    
    UltraBlame original commit: 2b451e361b7f820e8423972ca41757e670cfcc0c

--- dom/serviceworkers/ServiceWorkerInterceptController.cpp
+++ dom/serviceworkers/ServiceWorkerInterceptController.cpp
@@ -88,22 +88,40 @@ ServiceWorkerInterceptController::ShouldPrepareForIntercept(
       registration->MaybeScheduleTimeCheckAndUpdate();
     }
 
-    RefPtr<net::HttpBaseChannel> httpChannel = do_QueryObject(aChannel);
-
     RequestMode requestMode =
         InternalRequest::MapChannelToRequestMode(aChannel);
 
-    if (httpChannel &&
+    
+    
+    
+    RefPtr<net::HttpBaseChannel> httpChannel = do_QueryObject(aChannel);
+    if (requestMode == RequestMode::No_cors && loadInfo->GetIsMediaRequest() &&
+        httpChannel &&
         httpChannel->GetRequestHead()->HasHeader(net::nsHttp::Range)) {
-      RequestMode requestMode =
-          InternalRequest::MapChannelToRequestMode(aChannel);
       bool mayLoad = nsContentUtils::CheckMayLoad(
           loadInfo->GetLoadingPrincipal(), aChannel,
            false);
-      if (requestMode == RequestMode::No_cors && !mayLoad) {
+      if (!mayLoad) {
         *aShouldIntercept = false;
       }
-    } 
+    }
+
+    RequestDestination requestDest =
+        InternalRequest::MapContentPolicyTypeToRequestDestination(
+            loadInfo->GetExternalContentPolicyType());
+    
+    if (requestMode == RequestMode::No_cors &&
+        requestDest == RequestDestination::Style) {
+      nsCOMPtr<nsIPrincipal> triggeringPrincipal;
+      (void)loadInfo->GetTriggeringPrincipal(
+          getter_AddRefs(triggeringPrincipal));
+      MOZ_ASSERT(triggeringPrincipal);
+      bool mayLoad = nsContentUtils::CheckMayLoad(
+          triggeringPrincipal, aChannel,  false);
+      if (!mayLoad) {
+        *aShouldIntercept = false;
+      }
+    }
 
     return NS_OK;
   }
