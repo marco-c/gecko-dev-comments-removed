36b9e9dfd737dda46fe013bc4a9b7872635459cb
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-06-16 04:42:25 +0000

    Bug 2045847 - Http3Session and ZeroRttHandle cleanup, r=necko-reviewers,valentin
    Differential Revision: https://phabricator.services.mozilla.com/D305813
    
    UltraBlame original commit: 46114ef1518fdd663ef118e2981cd7fa2d30214b

--- netwerk/protocol/http/Http3Session.cpp
+++ netwerk/protocol/http/Http3Session.cpp
@@ -347,7 +347,12 @@ void Http3Session::Shutdown() {
     }
   }
 
-  for (const auto& stream : mStreamTransactionHash.Values()) {
+  nsTArray<RefPtr<Http3StreamBase>> streams;
+  streams.SetCapacity(mStreamTransactionHash.Count());
+  for (const auto& s : mStreamTransactionHash.Values()) {
+    streams.AppendElement(s);
+  }
+  for (const auto& stream : streams) {
     if (mBeforeConnectedError) {
       
       
@@ -716,12 +721,18 @@ nsresult Http3Session::ProcessEvents() {
       } break;
       case Http3Event::Tag::ConnectionConnected: {
         LOG(("Http3Session::ProcessEvents - ConnectionConnected"));
+        if (IsClosing()) {
+          break;
+        }
         bool was0RTT = mState == ZERORTT;
         mState = CONNECTED;
         SetSecInfo();
         mSocketControl->HandshakeCompleted();
         if (was0RTT) {
           Finish0Rtt(false);
+          if (IsClosing()) {
+            break;
+          }
           ZeroRttTelemetry(ZeroRttOutcome::USED_SUCCEEDED);
         }
 
@@ -1257,7 +1268,7 @@ nsresult Http3Session::ProcessOutputAndEvents(nsIUDPSocket* socket) {
     auto now = TimeStamp::Now();
     if (mTimerShouldTrigger > now) {
       
-      glean::http3::timer_delayed.AccumulateRawDuration(0);
+      glean::http3::timer_delayed.AccumulateRawDuration(nullptr);
     } else {
       glean::http3::timer_delayed.AccumulateRawDuration(now -
                                                         mTimerShouldTrigger);
@@ -2060,6 +2071,8 @@ nsresult Http3Session::WriteSegments(nsAHttpSegmentWriter* writer,
 
 nsresult Http3Session::RecvData(nsIUDPSocket* socket) {
   MOZ_ASSERT(OnSocketThread(), "not on socket thread");
+
+  RefPtr<Http3Session> self(this);
 
   
   nsresult rv = ProcessSlowConsumers();
@@ -2938,19 +2951,25 @@ void Http3Session::CloseConnectionTelemetry(CloseError& aError, bool aClosing) {
 }
 
 void Http3Session::Finish0Rtt(bool aRestart) {
-  for (size_t i = 0; i < m0RTTStreams.Length(); ++i) {
-    if (m0RTTStreams[i]) {
-      if (aRestart) {
-        
-        if (m0RTTStreams[i]->HasStreamId()) {
-          mStreamIdHash.Remove(m0RTTStreams[i]->StreamId());
-        }
-        RemoveStreamFromQueues(m0RTTStreams[i]);
-        
-        mReadyForWrite.Push(m0RTTStreams[i]);
-      }
-      m0RTTStreams[i]->Finish0RTT(aRestart);
+  nsTArray<RefPtr<Http3StreamBase>> streams;
+  for (const auto& weak : m0RTTStreams) {
+    if (RefPtr<Http3StreamBase> s = weak.get()) {
+      streams.AppendElement(std::move(s));
     }
+  }
+  m0RTTStreams.Clear();
+
+  for (const auto& stream : streams) {
+    if (aRestart) {
+      
+      if (stream->HasStreamId()) {
+        mStreamIdHash.Remove(stream->StreamId());
+      }
+      RemoveStreamFromQueues(stream);
+      
+      mReadyForWrite.Push(stream);
+    }
+    stream->Finish0RTT(aRestart);
   }
 
   for (size_t i = 0; i < mCannotDo0RTTStreams.Length(); ++i) {
@@ -2958,7 +2977,6 @@ void Http3Session::Finish0Rtt(bool aRestart) {
       mReadyForWrite.Push(mCannotDo0RTTStreams[i]);
     }
   }
-  m0RTTStreams.Clear();
   mCannotDo0RTTStreams.Clear();
   MaybeResumeSend();
 }
