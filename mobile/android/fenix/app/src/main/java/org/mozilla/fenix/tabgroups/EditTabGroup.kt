e9f333286f7624cd8a9ec50591dcb93fa5d4cc5e
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-06-16 02:47:43 +0000

    Bug 2044119 - Make the edit tab group sheet scrollable r=android-reviewers,apindiprolu,jdelorenzo
    Differential Revision: https://phabricator.services.mozilla.com/D304146
    
    UltraBlame original commit: a9dc71d3c71070db31252608c0a55b79eafa7833

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/tabgroups/EditTabGroup.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/tabgroups/EditTabGroup.kt
@@ -17,14 +17,17 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.selection.LocalTextSelectionColors
 import androidx.compose.foundation.text.selection.TextSelectionColors
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
@@ -147,9 +150,9 @@ private fun EditTabGroupContent(
     }
 
     Column(
-        modifier = Modifier.padding(
-            bottom = 12.dp,
-        ),
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState()),
     ) {
         Row(
             modifier = Modifier
@@ -195,6 +198,8 @@ private fun EditTabGroupContent(
         Spacer(modifier = Modifier.height(FirefoxTheme.layout.space.static300))
 
         TabGroupColorPicker(theme = formState.theme, onTabGroupThemeChange = onTabGroupThemeChange)
+
+        Spacer(modifier = Modifier.height(FirefoxTheme.layout.space.static150))
     }
 }
 
