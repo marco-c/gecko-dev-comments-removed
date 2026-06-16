8f304850ec72b4e47f390027185ba5c5fe251724
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-06-16 02:07:09 +0000

    Bug 2031717 - [devtools] Expose local mode settings in DevTools options panel. r=devtools-reviewers,fluent-reviewers,bolsson,nchevobbe
    Differential Revision: https://phabricator.services.mozilla.com/D294193
    
    UltraBlame original commit: 3acb6bdb3fb31a922b8b2be1f9ea37fe124f6b34

--- browser/base/content/test/static/browser_parsable_css.js
+++ browser/base/content/test/static/browser_parsable_css.js
@@ -700,7 +700,9 @@ add_task(async function checkAllTheCSS() {
     if (imageHost == "browser") {
       for (let ref of references) {
         let refHost = ref.split("/")[2];
-        if (!["builtin-addons", "newtab", "browser"].includes(refHost)) {
+        if (
+          !["builtin-addons", "newtab", "devtools", "browser"].includes(refHost)
+        ) {
           ok(
             false,
             "browser file " + image + " referenced outside browser in " + ref
