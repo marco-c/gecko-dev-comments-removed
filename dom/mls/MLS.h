0311f0856d7e9290861d55d9130564c25cf72ca9
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-06-16 05:44:07 +0000

    Bug 2045043 - Part 37 - Fixup final classes using _FINAL cycle collecting isupports macro in dom/mls r=dom-core-reviewers,smaug
    Differential Revision: https://phabricator.services.mozilla.com/D305316
    
    UltraBlame original commit: 047ee54188ceaa1508aa346843e04b7e52a91599

--- dom/mls/MLS.h
+++ dom/mls/MLS.h
@@ -2,8 +2,6 @@
 
 
 
-
-
 #ifndef mozilla_dom_MLS_h
 #define mozilla_dom_MLS_h
 
@@ -22,7 +20,7 @@ class MLSGroupView;
 
 class MLS final : public nsISupports, public nsWrapperCache {
  public:
-  NS_DECL_CYCLE_COLLECTING_ISUPPORTS
+  NS_DECL_CYCLE_COLLECTING_ISUPPORTS_FINAL
   NS_DECL_CYCLE_COLLECTION_WRAPPERCACHE_CLASS(MLS)
 
   static already_AddRefed<MLS> Constructor(GlobalObject& aGlobal,
