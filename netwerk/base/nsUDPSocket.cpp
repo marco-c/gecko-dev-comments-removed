91f9aec4652c39e318f824219c5af49fbc776351
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-06-16 02:27:09 +0000

    Bug 2042718: Remove off-STS mFD null checks from UDPSocket r=necko-reviewers,valentin
    Differential Revision: https://phabricator.services.mozilla.com/D302914
    
    UltraBlame original commit: a823f58a674aa808cace300c79e5289690f02e85

--- netwerk/base/nsUDPSocket.cpp
+++ netwerk/base/nsUDPSocket.cpp
@@ -1262,6 +1262,7 @@ nsUDPSocket::SendWithAddress(const NetAddr* aAddr, const uint8_t* aData,
 }
 
 int64_t nsUDPSocket::GetFileDescriptor() {
+  MOZ_ASSERT(OnSocketThread(), "not on socket thread");
   return PR_FileDesc2NativeHandle(mFD);
 }
 
@@ -1364,9 +1365,6 @@ nsUDPSocket::JoinMulticast(const nsACString& aAddr, const nsACString& aIface) {
   if (NS_WARN_IF(aAddr.IsEmpty())) {
     return NS_ERROR_INVALID_ARG;
   }
-  if (NS_WARN_IF(!mFD)) {
-    return NS_ERROR_NOT_INITIALIZED;
-  }
 
   PRNetAddr prAddr;
   if (PR_StringToNetAddr(PromiseFlatCString(aAddr).get(), &prAddr) !=
@@ -1389,10 +1387,6 @@ nsUDPSocket::JoinMulticast(const nsACString& aAddr, const nsACString& aIface) {
 
 NS_IMETHODIMP
 nsUDPSocket::JoinMulticastAddr(const NetAddr aAddr, const NetAddr* aIface) {
-  if (NS_WARN_IF(!mFD)) {
-    return NS_ERROR_NOT_INITIALIZED;
-  }
-
   PRNetAddr prAddr;
   NetAddrToPRNetAddr(&aAddr, &prAddr);
 
@@ -1427,9 +1421,6 @@ nsUDPSocket::LeaveMulticast(const nsACString& aAddr, const nsACString& aIface) {
   if (NS_WARN_IF(aAddr.IsEmpty())) {
     return NS_ERROR_INVALID_ARG;
   }
-  if (NS_WARN_IF(!mFD)) {
-    return NS_ERROR_NOT_INITIALIZED;
-  }
 
   PRNetAddr prAddr;
   if (PR_StringToNetAddr(PromiseFlatCString(aAddr).get(), &prAddr) !=
@@ -1452,10 +1443,6 @@ nsUDPSocket::LeaveMulticast(const nsACString& aAddr, const nsACString& aIface) {
 
 NS_IMETHODIMP
 nsUDPSocket::LeaveMulticastAddr(const NetAddr aAddr, const NetAddr* aIface) {
-  if (NS_WARN_IF(!mFD)) {
-    return NS_ERROR_NOT_INITIALIZED;
-  }
-
   PRNetAddr prAddr;
   NetAddrToPRNetAddr(&aAddr, &prAddr);
 
@@ -1492,10 +1479,6 @@ nsUDPSocket::GetMulticastLoopback(bool* aLoopback) {
 
 NS_IMETHODIMP
 nsUDPSocket::SetMulticastLoopback(bool aLoopback) {
-  if (NS_WARN_IF(!mFD)) {
-    return NS_ERROR_NOT_INITIALIZED;
-  }
-
   PRSocketOptionData opt;
 
   opt.option = PR_SockOpt_McastLoopback;
@@ -1517,10 +1500,6 @@ nsUDPSocket::GetRecvBufferSize(int* size) {
 
 NS_IMETHODIMP
 nsUDPSocket::SetRecvBufferSize(int size) {
-  if (NS_WARN_IF(!mFD)) {
-    return NS_ERROR_NOT_INITIALIZED;
-  }
-
   PRSocketOptionData opt;
 
   opt.option = PR_SockOpt_RecvBufferSize;
@@ -1542,10 +1521,6 @@ nsUDPSocket::GetDontFragment(bool* dontFragment) {
 
 NS_IMETHODIMP
 nsUDPSocket::SetDontFragment(bool dontFragment) {
-  if (NS_WARN_IF(!mFD)) {
-    return NS_ERROR_NOT_INITIALIZED;
-  }
-
   PRSocketOptionData opt;
   opt.option = PR_SockOpt_DontFrag;
   opt.value.dont_fragment = dontFragment;
@@ -1565,10 +1540,6 @@ nsUDPSocket::GetSendBufferSize(int* size) {
 
 NS_IMETHODIMP
 nsUDPSocket::SetSendBufferSize(int size) {
-  if (NS_WARN_IF(!mFD)) {
-    return NS_ERROR_NOT_INITIALIZED;
-  }
-
   PRSocketOptionData opt;
 
   opt.option = PR_SockOpt_SendBufferSize;
@@ -1594,10 +1565,6 @@ nsUDPSocket::GetMulticastInterfaceAddr(NetAddr* aIface) {
 
 NS_IMETHODIMP
 nsUDPSocket::SetMulticastInterface(const nsACString& aIface) {
-  if (NS_WARN_IF(!mFD)) {
-    return NS_ERROR_NOT_INITIALIZED;
-  }
-
   PRNetAddr prIface;
   if (aIface.IsEmpty()) {
     PR_InitializeNetAddr(PR_IpAddrAny, 0, &prIface);
@@ -1613,10 +1580,6 @@ nsUDPSocket::SetMulticastInterface(const nsACString& aIface) {
 
 NS_IMETHODIMP
 nsUDPSocket::SetMulticastInterfaceAddr(NetAddr aIface) {
-  if (NS_WARN_IF(!mFD)) {
-    return NS_ERROR_NOT_INITIALIZED;
-  }
-
   PRNetAddr prIface;
   NetAddrToPRNetAddr(&aIface, &prIface);
 
