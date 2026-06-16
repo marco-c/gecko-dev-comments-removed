170023cd520ad24130fb0929ed07a2e398e88154
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-06-16 04:53:43 +0000

    Bug 2046633 - Update gfxFontEntry::FindOrMakeFont header comments. r=gfx-reviewers,lsalzman
    The comments are inaccurate because we take, and need to take,
    strong references to the gfxFont objects. References may be held
    on the main and DOM worker threads.
    
    Differential Revision: https://phabricator.services.mozilla.com/D306140
    
    UltraBlame original commit: f620525781be6c91b3f2a5ab3e3222014d9fa9e7

--- gfx/thebes/gfxFontEntry.h
+++ gfx/thebes/gfxFontEntry.h
@@ -135,6 +135,9 @@ class gfxCharacterMap : public gfxSparseBitSet {
   explicit gfxCharacterMap(const gfxSparseBitSet& aOther)
       : gfxSparseBitSet(aOther) {}
 
+  gfxCharacterMap(const gfxCharacterMap&) = delete;
+  gfxCharacterMap& operator=(const gfxCharacterMap&) = delete;
+
   size_t SizeOfExcludingThis(mozilla::MallocSizeOf aMallocSizeOf) const {
     return gfxSparseBitSet::SizeOfExcludingThis(aMallocSizeOf);
   }
@@ -171,10 +174,6 @@ class gfxCharacterMap : public gfxSparseBitSet {
   }
 
   mozilla::ThreadSafeAutoRefCnt mRefCnt;
-
- private:
-  gfxCharacterMap(const gfxCharacterMap&) = delete;
-  gfxCharacterMap& operator=(const gfxCharacterMap&) = delete;
 };
 
 
@@ -383,18 +382,16 @@ class gfxFontEntry {
       mBlob = aFontEntry->GetFontTable(aTag);
     }
     ~AutoTable() { hb_blob_destroy(mBlob); }
+
+    AutoTable(const AutoTable&) = delete;
+    AutoTable& operator=(const AutoTable&) = delete;
+
     operator hb_blob_t*() const { return mBlob; }
 
    private:
     hb_blob_t* mBlob;
-    
-    AutoTable(const AutoTable&) = delete;
-    AutoTable& operator=(const AutoTable&) = delete;
   };
 
-  
-  
-  
   
   
   
