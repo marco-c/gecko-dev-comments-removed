b88e308deb5b3b4276a47dca6d0a864e563e0310
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-06-16 02:32:01 +0000

    Bug 2042602 - Unify early exits and parent space for sticky/scroll nodes r=botond
    Differential Revision: https://phabricator.services.mozilla.com/D303255
    
    UltraBlame original commit: 1cfe2422680bfcf85218cbd850526b11e6cfbba4

--- gfx/layers/wr/ClipManager.cpp
+++ gfx/layers/wr/ClipManager.cpp
@@ -366,11 +366,6 @@ Maybe<wr::WrSpatialId> ClipManager::DefineStickyNode(
     const ActiveScrolledRoot* aASR, nsDisplayItem* aItem) {
   nsIFrame* stickyFrame = aASR->mFrame;
 
-  if (Maybe<wr::WrSpatialId> space =
-          mBuilder->GetSpatialIdForDefinedLayer(aASR)) {
-    return space;
-  }
-
   StickyScrollContainer* stickyScrollContainer = GetStickyScrollContainer(aASR);
   if (!stickyScrollContainer) {
     
@@ -556,25 +551,27 @@ Maybe<wr::WrSpatialId> ClipManager::DefineSpatialNodes(
     return Nothing();
   }
 
-  ScrollableLayerGuid::ViewID viewId = ScrollableLayerGuid::NULL_SCROLL_ID;
-  if (aASR->mKind == ActiveScrolledRoot::ASRKind::Scroll) {
-    viewId = aASR->GetViewId();
-    Maybe<wr::WrSpatialId> space = mBuilder->GetSpatialIdForDefinedLayer(aASR);
-    if (space) {
-      
-      return space;
-    }
+  Maybe<wr::WrSpatialId> space = mBuilder->GetSpatialIdForDefinedLayer(aASR);
+  if (space) {
+    
+    return space;
   }
 
   
   Maybe<wr::WrSpatialId> ancestorSpace =
       DefineSpatialNodes(aBuilder, aASR->mParent, aItem);
 
+  Maybe<wr::WrSpatialId> parent = ancestorSpace;
+  if (parent) {
+    *parent = SpatialIdAfterOverride(*parent);
+  }
+
   if (aASR->mKind == ActiveScrolledRoot::ASRKind::Sticky) {
-    Maybe<wr::WrSpatialId> parent = ancestorSpace.map(
-        [this](wr::WrSpatialId& aId) { return SpatialIdAfterOverride(aId); });
     return ClipManager::DefineStickyNode(aBuilder, parent, aASR, aItem);
   }
+
+  MOZ_ASSERT(aASR->mKind == ActiveScrolledRoot::ASRKind::Scroll);
+  ScrollableLayerGuid::ViewID viewId = aASR->GetViewId();
 
   MOZ_ASSERT(viewId != ScrollableLayerGuid::NULL_SCROLL_ID);
 
@@ -612,10 +609,6 @@ Maybe<wr::WrSpatialId> ClipManager::DefineSpatialNodes(
       metrics.GetExpandedScrollableRect() * metrics.GetDevPixelsPerCSSPixel();
   contentRect.MoveTo(clipBounds.TopLeft());
 
-  Maybe<wr::WrSpatialId> parent = ancestorSpace;
-  if (parent) {
-    *parent = SpatialIdAfterOverride(*parent);
-  }
   
   
   
