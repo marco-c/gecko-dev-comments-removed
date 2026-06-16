5569a295fc47570296217d18f552230b44cf4a08
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-06-16 01:27:05 +0000

    Bug 2040478 - Vendor libwebrtc from 1170cc6dda
    Upstream commit: https://webrtc.googlesource.com/src/+/1170cc6dda283080bd99e3e1aaa03d10359fd0a5
        Fix network_tester compile issue.
    
        It was broken by https://webrtc-review.git.corp.google.com/c/src/+/408040 and it fails only now because of https://chromium-review.git.corp.google.com/c/chromium/tools/build/+/7734885.
    
        Change-Id: Iff0b87db7cdec809c5559d09b72b0a4b11a9bbe6
        Bug: None
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/462060
        Reviewed-by: Evan Shrubsole <eshrwebrtc.org>
        Commit-Queue: Jeremy Leconte <jlecontegoogle.com>
        Cr-Commit-Position: refs/heads/main{#47345}
    
    UltraBlame original commit: 3c8e9b211bb85466638b6aadc41ca071a6b6b59b

--- third_party/libwebrtc/rtc_tools/network_tester/jni.cc
+++ third_party/libwebrtc/rtc_tools/network_tester/jni.cc
@@ -9,13 +9,16 @@
 
 
 #include <jni.h>
-#undef JNIEXPORT
-#define JNIEXPORT __attribute__((visibility("default")))
+
+#include <cstdint>
 #include <string>
 
-#include "rtc_base/logging.h"
 #include "rtc_base/thread.h"
 #include "rtc_tools/network_tester/test_controller.h"
+#include "test/create_test_environment.h"
+
+#undef JNIEXPORT
+#define JNIEXPORT __attribute__((visibility("default")))
 
 extern "C" JNIEXPORT jlong JNICALL
 Java_com_google_media_networktester_NetworkTester_CreateTestController(
@@ -23,7 +26,8 @@ Java_com_google_media_networktester_NetworkTester_CreateTestController(
     jclass) {
   webrtc::ThreadManager::Instance()->WrapCurrentThread();
   return reinterpret_cast<intptr_t>(new webrtc::TestController(
-      0, 0, "/mnt/sdcard/network_tester_client_config.dat",
+      webrtc::CreateTestEnvironment(), 0, 0,
+      "/mnt/sdcard/network_tester_client_config.dat",
       "/mnt/sdcard/network_tester_client_packet_log.dat"));
 }
 
