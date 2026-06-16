6b8b09d22a239b6489c443ce1221a0c0b259cbdf
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-06-16 01:46:11 +0000

    Bug 2040478 - Vendor libwebrtc from f422e854ca
    Upstream commit: https://webrtc.googlesource.com/src/+/f422e854ca2eb731663b99a33852fdce167e8c4f
        Fix ASan null pointer in StartAecDump
    
        Adds a null check to PeerConnectionFactory::StartAecDump to prevent
        passing a null FILE* to WebRtcVoiceEngine, which ultimately passes it to
        a _Nonnull argument in AudioProcessing::CreateAndAttachAecDump, causing
        an ASan/UBSan invalid_null_argument runtime error.
    
        Also updates PeerConnectionFactoryDependenciesTest's
        CreatesAudioProcessingWithProvidedBuilder to use a valid temporary file
        instead of relying on undefined behavior.
    
        Fixed: webrtc:505372808
        Change-Id: I129eb0b650c0dda508e0c99cecaf33892f348bf8
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/468260
        Reviewed-by: Jeremy Leconte <jlecontegoogle.com>
        Commit-Queue: Tomas Gunnarsson <tommiwebrtc.org>
        Cr-Commit-Position: refs/heads/main{#47580}
    
    UltraBlame original commit: 4127d7eb99448fbe1974bbac8137f7a2459804f9

--- third_party/libwebrtc/pc/peer_connection_factory_unittest.cc
+++ third_party/libwebrtc/pc/peer_connection_factory_unittest.cc
@@ -14,6 +14,7 @@
 #include <cstddef>
 #include <cstdio>
 #include <memory>
+#include <string>
 #include <utility>
 #include <vector>
 
@@ -69,6 +70,7 @@
 #include "test/gmock.h"
 #include "test/gtest.h"
 #include "test/run_loop.h"
+#include "test/testsupport/file_utils.h"
 
 #ifdef WEBRTC_ANDROID
 #include "pc/test/android_test_initializer.h"
@@ -762,7 +764,14 @@ TEST(PeerConnectionFactoryDependenciesTest,
 
   scoped_refptr<PeerConnectionFactoryInterface> pcf =
       CreateModularPeerConnectionFactory(std::move(pcf_dependencies));
-  pcf->StartAecDump(nullptr, 24'242);
+  
+  
+  std::string temp_filename =
+      test::TempFilename(test::OutputPath(), "aec_dump");
+  pcf->StartAecDump(fopen(temp_filename.c_str(), "wb"), 24'242);
+  
+  pcf = nullptr;
+  test::RemoveFile(temp_filename);
 }
 
 TEST(PeerConnectionFactoryDependenciesTest, RepeatMediaEngineInitialization) {
