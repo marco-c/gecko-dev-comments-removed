4ea1f58469ef38797f4a354836466292cd01346a
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-06-16 03:23:53 +0000

    Bug 2024649 - Remove LayoutUtils.sys.mjs r=emilio,geckoview-reviewers,Sasha,jonalmeida
    After bug 1691346, LayoutUtils is a thin wrapper over a few methods on
    windowUtils. I think we can get rid of it and just call these methods
    directly now.
    
    Differential Revision: https://phabricator.services.mozilla.com/D288661
    
    UltraBlame original commit: acf5a2c80f50b5ff7822510b32461e36053f524c

--- remote/marionette/actors/MarionetteCommandsChild.sys.mjs
+++ remote/marionette/actors/MarionetteCommandsChild.sys.mjs
@@ -5,8 +5,6 @@
 const lazy = {};
 
 ChromeUtils.defineESModuleGetters(lazy, {
-  LayoutUtils: "resource://gre/modules/LayoutUtils.sys.mjs",
-
   accessibility:
     "chrome://remote/content/shared/webdriver/Accessibility.sys.mjs",
   AnimationFramePromise: "chrome://remote/content/shared/Sync.sys.mjs",
@@ -155,12 +153,12 @@ export class MarionetteCommandsChild extends JSWindowActorChild {
   #toBrowserWindowCoordinates(options, _context) {
     const { position } = options;
 
-    return lazy.LayoutUtils.rectToTopLevelWidgetRect(this.contentWindow, {
-      left: position[0],
-      top: position[1],
-      height: 0,
-      width: 0,
-    });
+    return this.contentWindow.windowUtils.toTopLevelWidgetRect(
+      position[0],
+      position[1],
+      0,
+      0
+    );
   }
 
   // eslint-disable-next-line complexity
