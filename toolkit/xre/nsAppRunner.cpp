46ff7c7ac2397393075aa7e5973c75ea38236a52
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-07-01 04:09:46 +0000

    Bug 2040673 - Also toggle the crashhelper process on appinfo.crashreporterEnabled r=gsvelto
    This wasn't a problem before as the crashhelper was always around
    anyway, but now that it can become disabled, we want to start it up
    before we set the exception handler.
    
    Differential Revision: https://phabricator.services.mozilla.com/D303761
    
    UltraBlame original commit: b4e91c41fbcf30cab794365d38fb5b728ee0b9dd

--- toolkit/xre/nsAppRunner.cpp
+++ toolkit/xre/nsAppRunner.cpp
@@ -1774,6 +1774,11 @@ nsXULAppInfo::SetEnabled(bool aEnabled) {
       return NS_ERROR_FAILURE;
     }
 
+    nsresult rv = CrashReporter::OOPInit(xreBinDirectory, true);
+    if (rv != NS_OK) {
+      return rv;
+    }
+
     return CrashReporter::SetExceptionHandler(xreBinDirectory, true);
   }
 
@@ -1782,7 +1787,14 @@ nsXULAppInfo::SetEnabled(bool aEnabled) {
     return NS_OK;
   }
 
-  return CrashReporter::UnsetExceptionHandler();
+  nsresult rv = CrashReporter::UnsetExceptionHandler();
+#if !defined(MOZ_WIDGET_ANDROID)
+  
+  
+  
+  CrashReporter::OOPDeinit();
+#endif
+  return rv;
 }
 
 NS_IMETHODIMP
