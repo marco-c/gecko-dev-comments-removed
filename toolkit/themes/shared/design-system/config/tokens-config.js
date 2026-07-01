4641350fa0a3162077cf9aa805c77b91d61f4d5a
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-07-01 02:33:09 +0000

    Bug 2045752 - Part 1: Ensure brand overrides browser-theme design tokens r=desktop-theme-reviewers,tgiles,maxx
    Differential Revision: https://phabricator.services.mozilla.com/D305457
    
    UltraBlame original commit: 7ce92fa7d30ec2c09547ec55410e7e8ee8dceb11

--- toolkit/themes/shared/design-system/config/tokens-config.js
+++ toolkit/themes/shared/design-system/config/tokens-config.js
@@ -285,7 +285,11 @@ const STATE_ORDER = [
 ];
 
 const getLayerString = () => {
-  const themeLayers = ["tokens-foundation", "tokens-browser-theme"];
+  const themeLayers = [
+    "tokens-foundation",
+    "tokens-browser-theme",
+    "tokens-foundation-brand",
+  ];
   const a11yLayers = ["tokens-prefers-contrast", "tokens-forced-colors"];
 
   const themeLayersWithOverrides = [
@@ -684,7 +688,8 @@ function formatTokens({
     componentName,
   });
 
-  let layer = `tokens-${mediaQuery ?? "foundation"}${overrideIdentifier ? `-${overrideIdentifier}` : ""}`;
+  let layer = `tokens-${mediaQuery ?? `foundation${surface == "brand" ? "-brand" : ""}`}${overrideIdentifier ? `-${overrideIdentifier}` : ""}`;
+
   
   if (mediaQuery === "browser-theme") {
     return `
@@ -757,31 +762,11 @@ function getOriginalTokenValue(token, prop, surface) {
   if (!layer || typeof layer !== "object") {
     return undefined;
   }
-
-  
-  
-  
-  if (prop === "default") {
-    return (
-      layer.nativeTheme ??
-      layer.default ??
-      (!surface ? getLightDarkPair(layer) : undefined)
-    );
+  let originalValue = layer[prop];
+  if (typeof originalValue === "object") {
+    return originalValue.default;
   }
-
-  
-  
-  if (prop === "browserTheme") {
-    const browserThemeValue = layer.default ?? getLightDarkPair(layer);
-    if (!browserThemeValue) {
-      return undefined;
-    }
-    return layer.nativeTheme || value.nativeTheme
-      ? browserThemeValue
-      : undefined;
-  }
-
-  return layer[prop];
+  return originalValue;
 }
 
 
@@ -816,6 +801,96 @@ function transformToken({ token, originalVal, dictionary, surface }) {
   return { ...token, value, comment: surfaceComment ?? token.comment };
 }
 
+const fileToComponentTypeMap = new Map();
+function isComponentToken({ filePath }) {
+  if (!fileToComponentTypeMap.has(filePath)) {
+    let componentInfo = COMPONENT_TOKEN_PATHS.find(info =>
+      filePath.startsWith(info.dir)
+    );
+    if (componentInfo) {
+      fileToComponentTypeMap.set(filePath, {
+        component: true,
+        global: !!componentInfo.isGlobal,
+      });
+    } else {
+      fileToComponentTypeMap.set(filePath, {
+        component: false,
+        global: true,
+      });
+    }
+  }
+  let info = fileToComponentTypeMap.get(filePath);
+  return info.component && !info.global;
+}
+
+
+
+
+
+
+
+
+
+
+const createBrowserThemeTransform = surface => {
+  let name = `browserThemeTransform/${surface}`;
+
+  
+  
+  let matcher = token => {
+    let match =
+      token.original.value.nativeTheme &&
+      
+      (isComponentToken(token) ? surface == "shared" : surface == "platform");
+    return match;
+  };
+
+  let getValueObject = root => {
+    if (root.light) {
+      return { light: root.light, dark: root.dark };
+    }
+    return { default: root.default };
+  };
+
+  
+  
+  let transformer = token => {
+    let value = token.original.value;
+    if (surface == "platform" && value.platform) {
+      let browserTheme = getValueObject(token.original.value.platform);
+      token.original.value.platform = {
+        default: value.nativeTheme,
+        browserTheme,
+      };
+      return token.value;
+    }
+    let browserThemeRoot = value.brand ? value.brand : value;
+    let browserTheme = getValueObject(browserThemeRoot);
+    if (isComponentToken(token)) {
+      delete token.original.value.light;
+      delete token.original.value.dark;
+      token.original.value.default = value.nativeTheme;
+      token.original.value.browserTheme = browserTheme;
+    } else {
+      token.original.value.platform = {
+        default: value.nativeTheme,
+        browserTheme,
+      };
+    }
+    return token.value;
+  };
+
+  StyleDictionary.registerTransform({
+    type: "value",
+    transitive: true,
+    name,
+    matcher,
+    transformer,
+  });
+
+  return name;
+};
+
 
 
 
@@ -828,36 +903,34 @@ function transformToken({ token, originalVal, dictionary, surface }) {
 const createLightDarkTransform = surface => {
   let name = `lightDarkTransform/${surface}`;
 
-  const getLayerValue = token =>
-    surface === "shared" ? token.original.value : token.original.value[surface];
+  function getTokenRoot(token) {
+    let isComponent = isComponentToken(token);
+    let root =
+      isComponent || surface == "shared"
+        ? token.original.value
+        : token.original.value[surface];
+    let isBrowserTheme =
+      (surface == "platform" || (isComponent && surface == "shared")) &&
+      root?.browserTheme;
+    if (isBrowserTheme) {
+      root = root.browserTheme;
+    }
+    return root;
+  }
 
   
   
   let matcher = token => {
-    const layerValue = getLayerValue(token);
-    if (
-      !layerValue ||
-      typeof layerValue !== "object" ||
-      layerValue.nativeTheme ||
-      layerValue.default ||
-      !layerValue.light ||
-      !layerValue.dark
-    ) {
-      return false;
-    }
-    return !(surface !== "shared" && token.original.value.nativeTheme);
+    let root = getTokenRoot(token);
+    return root && root.light && root.dark;
   };
 
   
   
   let transformer = token => {
-    const layerValue = getLayerValue(token);
-    const lightDarkVal = getLightDarkPair(layerValue);
-    if (surface != "shared") {
-      token.original.value[surface].default = lightDarkVal;
-      return token.value;
-    }
-    token.original.value.default = lightDarkVal;
+    let root = getTokenRoot(token);
+    let lightDarkVal = `light-dark(${root.light}, ${root.dark})`;
+    root.default = lightDarkVal;
     return lightDarkVal;
   };
 
@@ -1142,6 +1215,7 @@ module.exports = {
       },
       transforms: [
         ...StyleDictionary.transformGroup.css,
+        ...["shared", "platform", "brand"].map(createBrowserThemeTransform),
         ...["shared", "platform", "brand"].map(createLightDarkTransform),
       ],
       files: [
@@ -1178,6 +1252,7 @@ module.exports = {
       },
       transforms: [
         ...StyleDictionary.transformGroup.css,
+        ...["shared", "platform", "brand"].map(createBrowserThemeTransform),
         ...["shared", "platform", "brand"].map(createLightDarkTransform),
       ],
       files: [
