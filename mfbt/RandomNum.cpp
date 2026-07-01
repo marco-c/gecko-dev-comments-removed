1906aefd3f04c2d5e21c0ad2403c8810aa96e024
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-07-01 03:29:22 +0000

    Bug 2039226 - Use ProcessPrng in mfbt/RandomNum on Windows. r=glandium,sergesanspaille
    Differential Revision: https://phabricator.services.mozilla.com/D303791
    
    UltraBlame original commit: e672ab1f98cdea15b0f547b50a0325946ecb2501

--- mfbt/RandomNum.cpp
+++ mfbt/RandomNum.cpp
@@ -10,19 +10,8 @@
 #endif
 
 #if defined(XP_WIN)
-
-
-
-
-
-
-
-
 #  include "minwindef.h"
-#  define RtlGenRandom SystemFunction036
-extern "C" BOOLEAN NTAPI RtlGenRandom(PVOID RandomBuffer,
-                                      ULONG RandomBufferLength);
-
+#  include "mozilla/DynamicallyLinkedFunctionPtr.h"
 #endif
 
 #if defined(ANDROID) || defined(XP_DARWIN) || defined(__DragonFly__) ||    \
@@ -88,7 +77,12 @@ MFBT_API bool GenerateRandomBytesFromOS(void* aBuffer, size_t aLength) {
   MOZ_ASSERT(aLength > 0);
 
 #if defined(XP_WIN)
-  return !!RtlGenRandom(aBuffer, aLength);
+  
+  
+  static const mozilla::StaticDynamicallyLinkedFunctionPtr<BOOL(WINAPI*)(
+      PBYTE, SIZE_T)>
+      pProcessPrng(L"bcryptprimitives.dll", "ProcessPrng");
+  return pProcessPrng && !!pProcessPrng(static_cast<PBYTE>(aBuffer), aLength);
 
 #elif defined(USE_ARC4RANDOM)  
 
