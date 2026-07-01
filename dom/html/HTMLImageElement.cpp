a7d188da757e8007fa55d8309736923306714b0c
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-07-01 01:34:17 +0000

    Bug 2045513 - Make parser update form owner properly; r=hsivonen
    Differential Revision: https://phabricator.services.mozilla.com/D305669
    
    UltraBlame original commit: 2eab5f54bfbd3c10c55c9284766c7d2000e0ecaf

--- dom/html/HTMLImageElement.cpp
+++ dom/html/HTMLImageElement.cpp
@@ -612,8 +612,8 @@ HTMLFormElement* HTMLImageElement::GetFormInternal() const { return mForm; }
 
 void HTMLImageElement::SetForm(HTMLFormElement* aForm) {
   MOZ_ASSERT(aForm, "Don't pass null here");
-  NS_ASSERTION(!mForm,
-               "We don't support switching from one non-null form to another.");
+  MOZ_ASSERT(!mForm && !HasFlag(ADDED_TO_FORM),
+             "We don't support switching from one non-null form to another.");
 
   mForm = aForm;
 }
