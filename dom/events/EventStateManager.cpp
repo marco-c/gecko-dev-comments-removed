24628da2a1aa9af5837de2778d5c6bd3929cf616
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-07-01 05:02:45 +0000

    Bug 1293741 - Move active pseudo activation to pointerdown handler r=emilio,dom-core-reviewers,smaug
    Differential Revision: https://phabricator.services.mozilla.com/D308750
    
    UltraBlame original commit: f1087ed2866c4f0c38221f28ba1d66f9a1bff785

--- dom/events/EventStateManager.cpp
+++ dom/events/EventStateManager.cpp
@@ -906,6 +906,13 @@ static void HandleKeyUpInteraction(WidgetKeyboardEvent* aKeyEvent) {
   }
 }
 
+static bool NeedsActiveContentChange(const WidgetMouseEvent* aMouseEvent) {
+  
+  
+  return !aMouseEvent ||
+         aMouseEvent->mInputSource != MouseEvent_Binding::MOZ_SOURCE_TOUCH;
+}
+
 nsresult EventStateManager::PreHandleEvent(nsPresContext* aPresContext,
                                            WidgetEvent* aEvent,
                                            nsIFrame* aTargetFrame,
@@ -1180,6 +1187,17 @@ nsresult EventStateManager::PreHandleEvent(nsPresContext* aPresContext,
           NotifyTargetUserActivation(aEvent, aTargetContent);
         }
 
+        if (NeedsActiveContentChange(mouseEvent)) {
+          nsCOMPtr<nsIContent> activeContent =
+              mCurrentTarget ? mCurrentTarget->GetContent() : nullptr;
+          if (activeContent && !activeContent->IsElement()) {
+            if (nsIContent* parent = activeContent->GetFlattenedTreeParent()) {
+              activeContent = parent;
+            }
+          }
+          SetActiveManager(this, activeContent);
+        }
+
         LightDismissOpenPopovers(aEvent, aTargetContent);
         LightDismissOpenDialogs(aEvent, aTargetContent);
       }
@@ -1219,6 +1237,9 @@ nsresult EventStateManager::PreHandleEvent(nsPresContext* aPresContext,
       GenerateMouseEnterExit(mouseEvent);
       if (mouseEvent->mInputSource != MouseEvent_Binding::MOZ_SOURCE_MOUSE) {
         NotifyTargetUserActivation(aEvent, aTargetContent);
+      }
+      if (NeedsActiveContentChange(mouseEvent)) {
+        ClearGlobalActiveContent(this);
       }
       break;
     case ePointerGotCapture:
@@ -4005,13 +4026,6 @@ void EventStateManager::PostHandleKeyboardEvent(
   }
 }
 
-static bool NeedsActiveContentChange(const WidgetMouseEvent* aMouseEvent) {
-  
-  
-  return !aMouseEvent ||
-         aMouseEvent->mInputSource != MouseEvent_Binding::MOZ_SOURCE_TOUCH;
-}
-
 nsresult EventStateManager::PostHandleEvent(nsPresContext* aPresContext,
                                             WidgetEvent* aEvent,
                                             nsIFrame* aTargetFrame,
@@ -4233,24 +4247,10 @@ nsresult EventStateManager::PostHandleEvent(nsPresContext* aPresContext,
         if (mouseEvent->mButton != MouseButton::ePrimary) {
           break;
         }
-
-        
-        
-        
-        if (activeContent && !activeContent->IsElement()) {
-          if (nsIContent* par = activeContent->GetFlattenedTreeParent()) {
-            activeContent = par;
-          }
-        }
       } else {
         
         
         StopTrackingDragGesture(true);
-      }
-      
-      
-      if (NeedsActiveContentChange(mouseEvent)) {
-        SetActiveManager(this, activeContent);
       }
     } break;
     case ePointerCancel:
