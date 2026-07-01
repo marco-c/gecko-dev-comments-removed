d96dded26a04d1a2b24fbecdda98bd57d5c1426d
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-07-01 03:59:51 +0000

    No Bug, mozilla-central repo-update remote-settings tld-suffixes mobile-experiments mobile-merino-manifest ct-logs - r=release-managers,RyanVM
    Differential Revision: https://phabricator.services.mozilla.com/D308360
    
    UltraBlame original commit: eae1b13689083908b14eb3fee1b1bf6a74b0f2e6

--- security/ct/CTKnownLogs.h
+++ security/ct/CTKnownLogs.h
@@ -14,7 +14,7 @@
 
 #include <stddef.h>
 
-static const PRTime kCTExpirationTime = INT64_C(1788120330000000);
+static const PRTime kCTExpirationTime = INT64_C(1788293338000000);
 
 namespace mozilla::ct {
 
