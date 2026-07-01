8bda67da5bec938f27fb510bb5c8509bb1e0f5a6
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-07-01 04:59:02 +0000

    Bug 2043658 - Update llama.cpp to 74ade527 - Rebase patch 10 (RLIMIT_MEMLOCK solaris) r=jbowser,padenot
    Upstream added __HAIKU__ to the RLIMIT_MEMLOCK guard, so __sun__ is now
    appended alongside it and the comment updated to match. Also drops a spurious
    empty-file hunk the previous version of the patch carried.
    
    Differential Revision: https://phabricator.services.mozilla.com/D307366
    
    UltraBlame original commit: 3bd01ffe47b07886198660072ba61fe4a323cdae

--- third_party/llama.cpp/src/llama-mmap.cpp
+++ third_party/llama.cpp/src/llama-mmap.cpp
@@ -657,7 +657,7 @@ struct llama_mlock::impl {
 
         char* errmsg = std::strerror(errno);
         bool suggest = (errno == ENOMEM);
-#if defined(TARGET_OS_VISION) || defined(TARGET_OS_TV) || defined(_AIX) || defined(__HAIKU__)
+#if defined(TARGET_OS_VISION) || defined(TARGET_OS_TV) || defined(_AIX) || defined(__HAIKU__) || defined(__sun__)
         
         
         suggest = false;
