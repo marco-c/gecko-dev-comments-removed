30b97f2905943d797b36474c3d753d62693ee831
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-07-01 04:09:51 +0000

    Bug 2040673 - test that the crashhelper is properly disabled with the appropriate env variable r=gsvelto
    We use a new process with a background task in order to get a "normal"
    crash handling, as the xpcshell has its own codepaths dealing with crash
    handling that force the issue.
    
    Note that we explicitly skip Windows from PID checking in the enabled
    test case. Exporting this particular piece of information is not trivial
    and is likely costly performance-wise, which is likely not worth it just
    for the sake of testing. Instead, correctness will have to be tested
    manually.
    
    Differential Revision: https://phabricator.services.mozilla.com/D303762
    
    UltraBlame original commit: 455d5f1a45ef4c24d3781ef1c0a8a91786b9b23a

--- toolkit/crashreporter/nsExceptionHandler.cpp
+++ toolkit/crashreporter/nsExceptionHandler.cpp
@@ -3251,6 +3251,14 @@ void OOPDeinit() {
   }
 }
 
+uint32_t GetCrashHelperPid() {
+  StaticMutexAutoLock lock(gCrashHelperClientMutex);
+  if (!gCrashHelperClient) {
+    return 0;
+  }
+  return static_cast<uint32_t>(crash_helper_pid(gCrashHelperClient));
+}
+
 
 #if defined(MOZ_WIDGET_ANDROID)
 void SetCrashHelperPipes(FileHandle breakpadFd, FileHandle crashHelperFd) {
