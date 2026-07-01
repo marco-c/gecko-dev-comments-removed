4431c33697ccd25fc870852c4026d5c00d64195e
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-07-01 04:59:35 +0000

    Bug 2043658 - Update llama.cpp to 74ade527 - Improve performance of catch blocks redefined by moz-overrides.h r=padenot
    This makes the exceptions declared in the redefine of `catch` static, avoiding construction/destruction of 4 objects.
    
    Differential Revision: https://phabricator.services.mozilla.com/D308369
    
    UltraBlame original commit: bcf9b1be58468198139c667e00eba3e7439ea8e9

--- third_party/llama.cpp/moz-overrides.h
+++ third_party/llama.cpp/moz-overrides.h
@@ -31,7 +31,4 @@
 
 
 #define catch(x) \
-    if (const std::exception &e = std::exception(), err = std::exception(), \
-        error = std::exception(), ex = std::exception(); \
-        false)
-        
+    if (static const std::exception e, err, error, ex; false)
