f66020f6ab5fbba078f0f3a6e7e150fbaf4ade01
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-07-01 04:50:03 +0000

    Revert "Bug 2048699 - Ensure the browsing context can do history traversal. r=dom-core-reviewers,smaug" for causing mochitest failures at test_hashchange_popstate_event_order.html
    This reverts commit 58537468124872f56b98d22ad6545ec3dc51294c.
    
    UltraBlame original commit: 99ea8a9722b6050cb1fff53ae2f5baa17e182714

--- dom/ipc/ContentParent.cpp
+++ dom/ipc/ContentParent.cpp
@@ -7474,10 +7474,6 @@ mozilla::ipc::IPCResult ContentParent::RecvHistoryGo(
     bool aCheckForCancelation, HistoryGoResolver&& aResolveRequestedIndex) {
   if (!aContext.IsNullOrDiscarded()) {
     RefPtr<CanonicalBrowsingContext> canonical = aContext.get_canonical();
-    if (!canonical->IsOwnedByProcess(ChildID())) {
-      return IPC_OK();
-    }
-
     aResolveRequestedIndex(canonical->HistoryGo(
         aOffset, aHistoryEpoch, aRequireUserInteraction, aUserActivation,
         aCheckForCancelation, Some(ChildID())));
@@ -7491,10 +7487,6 @@ mozilla::ipc::IPCResult ContentParent::RecvNavigationTraverse(
     NavigationTraverseResolver&& aResolver) {
   if (!aContext.IsNullOrDiscarded()) {
     RefPtr<CanonicalBrowsingContext> canonical = aContext.get_canonical();
-    if (!canonical->IsOwnedByProcess(ChildID())) {
-      return IPC_OK();
-    }
-
     canonical->NavigationTraverse(aKey, aHistoryEpoch, aUserActivation,
                                   aCheckForCancelation, Some(ChildID()),
                                   aResolver);
