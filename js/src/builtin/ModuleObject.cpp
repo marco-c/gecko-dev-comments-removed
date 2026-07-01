86a7e54d56be2b92b13999cccf7f715c57129abd
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-07-01 03:09:46 +0000

    Bug 2046502 - Part 2: Update ParseModule change. r=jonco
    https://arai-a.github.io/ecma262-compare/?rev=6484b1758e11f737ee770714fd831b674f406692&id=sec-parsemodule
    
    Differential Revision: https://phabricator.services.mozilla.com/D306726
    
    UltraBlame original commit: ca2178c19fb6ffb24b12332e338fcfa0e04db9ce

--- js/src/builtin/ModuleObject.cpp
+++ js/src/builtin/ModuleObject.cpp
@@ -1850,19 +1850,18 @@ bool ModuleBuilder::buildTables(frontend::StencilModuleMetadata& metadata) {
             js::ReportOutOfMemory(fc_);
             return false;
           }
-        } else if (!importEntry->importName) {
-          
-          auto entry = frontend::StencilModuleEntry::exportNamespaceFromEntry(
-              importEntry->moduleRequest, exp.exportName, exp.lineno,
-              exp.column);
-          if (!metadata.indirectExportEntries.append(entry)) {
-            js::ReportOutOfMemory(fc_);
-            return false;
-          }
         } else {
-          auto entry = frontend::StencilModuleEntry::exportFromEntry(
-              importEntry->moduleRequest, importEntry->importName,
-              exp.exportName, exp.lineno, exp.column);
+          
+          
+          
+          frontend::StencilModuleEntry entry =
+              importEntry->importName
+                  ? frontend::StencilModuleEntry::exportFromEntry(
+                        importEntry->moduleRequest, importEntry->importName,
+                        exp.exportName, exp.lineno, exp.column)
+                  : frontend::StencilModuleEntry::exportNamespaceFromEntry(
+                        importEntry->moduleRequest, exp.exportName, exp.lineno,
+                        exp.column);
           if (!metadata.indirectExportEntries.append(entry)) {
             js::ReportOutOfMemory(fc_);
             return false;
