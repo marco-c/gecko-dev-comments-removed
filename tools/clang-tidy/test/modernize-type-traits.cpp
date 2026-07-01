71efddfb2a200967c43702b129fd68aec04ced4b
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-07-01 04:39:04 +0000

    Bug 2049899 - Do not rely on availability of standard headers when testing static analysis r=sylvestre DONTBUILD
    Follow the same practice as upstream and just mock the part we need from
    std namespace.
    
    Differential Revision: https://phabricator.services.mozilla.com/D308612
    
    UltraBlame original commit: 923aa178185221428fd03264558ad21bcf3d65c3

--- tools/clang-tidy/test/modernize-type-traits.cpp
+++ tools/clang-tidy/test/modernize-type-traits.cpp
@@ -1,3 +1,14 @@
-#include <type_traits>
+namespace std {
+    template<typename T>
+    struct remove_reference {
+      using type = T;
+    };
+    template<typename T>
+    struct remove_reference<T&> {
+      using type = T;
+    };
+    template<typename T>
+    using remove_reference_t = typename remove_reference<T>::type; 
+}
 
 using type = typename std::remove_reference<int&>::type;
