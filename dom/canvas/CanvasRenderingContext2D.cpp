ebb5c1a03bb15dbf9caa79ddd3ae4008f43ff402
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-07-01 01:34:48 +0000

    Revert "Bug 2045746 - Copy strings to avoid holding a ref to ContextState() during flush. r=gfx-reviewers,nical" for causing canvas failures
    This reverts commit 88e0f3552da60f3d4e4855053922ece7782ba172.
    
    UltraBlame original commit: 24e27500d8965cf15b4fd22ac12866135f7e0513

--- dom/canvas/CanvasRenderingContext2D.cpp
+++ dom/canvas/CanvasRenderingContext2D.cpp
@@ -4546,14 +4546,12 @@ bool CanvasRenderingContext2D::SetFontInternalDisconnected(
 }
 
 void CanvasRenderingContext2D::UpdateSpacing() {
-  
-  auto letterSpacingStr = ContextState().letterSpacingStr;
-  auto wordSpacingStr = ContextState().wordSpacingStr;
-  if (!letterSpacingStr.IsEmpty()) {
-    SetLetterSpacing(letterSpacingStr);
+  const auto& state = CurrentState();
+  if (!state.letterSpacingStr.IsEmpty()) {
+    SetLetterSpacing(state.letterSpacingStr);
   }
-  if (!wordSpacingStr.IsEmpty()) {
-    SetWordSpacing(wordSpacingStr);
+  if (!state.wordSpacingStr.IsEmpty()) {
+    SetWordSpacing(state.wordSpacingStr);
   }
 }
 
