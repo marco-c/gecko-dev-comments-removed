30a92b2528a65b8a597a9377fca7d7f00389839b
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-07-01 02:31:21 +0000

    Bug 2047630 - Mock engine manager in browser_aiwindow_smartbar_placeholder.js r=yjamora
    Differential Revision: https://phabricator.services.mozilla.com/D306823
    
    UltraBlame original commit: dc7bc1e70f1f92e1a3943cc1b08a858fdf2992e9

--- browser/components/aiwindow/ui/test/browser/browser_aiwindow_smartbar_placeholder.js
+++ browser/components/aiwindow/ui/test/browser/browser_aiwindow_smartbar_placeholder.js
@@ -7,6 +7,19 @@
 
 "use strict";
 
+const { MockEngineManager } = ChromeUtils.importESModule(
+  "resource://testing-common/AIWindowTestUtils.sys.mjs"
+);
+
+add_setup(async function () {
+  const mockEngineManager = new MockEngineManager();
+
+  registerCleanupFunction(() => {
+    mockEngineManager.rejectAllRequests();
+    mockEngineManager.cleanupMocks();
+  });
+});
+
 
 
 
